09e0473845e1c7fb3e09585f2ae417f744258b27
wtc <wtc>
1998-05-08 18:48:46 +0000

    Fixed comment.

--- nsprpub/pr/src/md/unix/darwin.c
+++ nsprpub/pr/src/md/unix/darwin.c
@@ -78,11 +78,11 @@ _MD_WAKEUP_WAITER(PRThread *thread)
     return PR_SUCCESS;
 }
 
-/* These functions should not be called for OSF1 */
+/* These functions should not be called for Rhapsody */
 void
 _MD_YIELD(void)
 {
-    PR_NOT_REACHED("_MD_YIELD should not be called for OSF1.");
+    PR_NOT_REACHED("_MD_YIELD should not be called for Rhapsody.");
 }
 
 PRStatus
@@ -94,7 +94,7 @@ _MD_CREATE_THREAD(
     PRThreadState state,
     PRUint32 stackSize)
 {
-    PR_NOT_REACHED("_MD_CREATE_THREAD should not be called for OSF1.");
+    PR_NOT_REACHED("_MD_CREATE_THREAD should not be called for Rhapsody.");
 	return PR_FAILURE;
 }
 #endif /* ! _PR_PTHREADS */
