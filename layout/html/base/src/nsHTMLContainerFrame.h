b51f07e06aab30c94bdfb833456974d2223896d9
troy <troy>
1998-05-08 04:56:47 +0000

    Added ContentChanged() member function

--- layout/html/base/src/nsHTMLContainerFrame.h
+++ layout/html/base/src/nsHTMLContainerFrame.h
@@ -51,6 +51,11 @@ public:
                              nsIContent*     aChild,
                              PRInt32         aIndexInParent);
 
+  NS_IMETHOD  ContentChanged(nsIPresShell*   aShell,
+                             nsIPresContext* aPresContext,
+                             nsIContent*     aChild,
+                             nsISupports*    aSubContent);
+
 protected:
   virtual ~nsHTMLContainerFrame();
 
