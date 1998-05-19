c6d24cd3668c4ddb4fce91800bfd32463a2bcf0f
michaelp <michaelp>
1998-05-19 22:08:46 +0000

    removed rects from region apis. fleshed out windows region implementation.

--- gfx/src/windows/nsRegionWin.cpp
+++ gfx/src/windows/nsRegionWin.cpp
@@ -25,6 +25,7 @@ nsRegionWin :: nsRegionWin()
   NS_INIT_REFCNT();
 
   mRegion = NULL;
+  mRegionType = NULLREGION;
 }
 
 nsRegionWin :: ~nsRegionWin()
@@ -38,58 +39,117 @@ NS_IMPL_RELEASE(nsRegionWin)
 
 nsresult nsRegionWin :: Init(void)
 {
+  mRegion = ::CreateRectRgn(0, 0, 0, 0);
+  mRegionType = NULLREGION;
+
   return NS_OK;
 }
 
 void nsRegionWin :: SetTo(const nsIRegion &aRegion)
 {
+  nsRegionWin *pRegion = (nsRegionWin *)&aRegion;
+
+  mRegionType = ::CombineRgn(mRegion, pRegion->mRegion, NULL, RGN_COPY);
 }
 
-void nsRegionWin :: SetTo(const nsRect &aRect)
+void nsRegionWin :: SetTo(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight)
 {
+  if (NULL != mRegion)
+    ::DeleteObject(mRegion);
+
+  mRegion = ::CreateRectRgn(aX, aY, aX + aWidth, aY + aHeight);
+
+  if ((aWidth == 0) || (aHeight == 0))
+    mRegionType = NULLREGION;
+  else
+    mRegionType = SIMPLEREGION;
 }
 
 void nsRegionWin :: Intersect(const nsIRegion &aRegion)
 {
+  nsRegionWin *pRegion = (nsRegionWin *)&aRegion;
+
+  mRegionType = ::CombineRgn(mRegion, pRegion->mRegion, mRegion, RGN_AND);
 }
 
-void nsRegionWin :: Intersect(const nsRect &aRect)
+void nsRegionWin :: Intersect(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight)
 {
+  HRGN tRegion;
+
+  tRegion = ::CreateRectRgn(aX, aY, aX + aWidth, aY + aHeight);
+  mRegionType = ::CombineRgn(mRegion, tRegion, mRegion, RGN_AND);
+
+  ::DeleteObject(tRegion);
 }
 
 void nsRegionWin :: Union(const nsIRegion &aRegion)
 {
+  nsRegionWin *pRegion = (nsRegionWin *)&aRegion;
+
+  mRegionType = ::CombineRgn(mRegion, pRegion->mRegion, mRegion, RGN_OR);
 }
 
-void nsRegionWin :: Union(const nsRect &aRect)
+void nsRegionWin :: Union(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight)
 {
+  HRGN tRegion;
+
+  tRegion = ::CreateRectRgn(aX, aY, aX + aWidth, aY + aHeight);
+  mRegionType = ::CombineRgn(mRegion, tRegion, mRegion, RGN_OR);
+
+  ::DeleteObject(tRegion);
 }
 
 void nsRegionWin :: Subtract(const nsIRegion &aRegion)
 {
+  nsRegionWin *pRegion = (nsRegionWin *)&aRegion;
+
+  mRegionType = ::CombineRgn(mRegion, mRegion, pRegion->mRegion, RGN_DIFF);
 }
 
 PRBool nsRegionWin :: IsEmpty(void)
 {
-  return PR_TRUE;
+  return (mRegionType == NULLREGION) ? PR_TRUE : PR_FALSE;
 }
 
 PRBool nsRegionWin :: IsEqual(const nsIRegion &aRegion)
 {
-  return PR_FALSE;
+  nsRegionWin *pRegion = (nsRegionWin *)&aRegion;
+
+  return ::EqualRgn(mRegion, pRegion->mRegion) ? PR_TRUE : PR_FALSE;
 }
 
-void nsRegionWin :: GetBoundingBox(nsRect &aRect)
+void nsRegionWin :: GetBoundingBox(PRInt32 *aX, PRInt32 *aY, PRInt32 *aWidth, PRInt32 *aHeight)
 {
+  RECT  bounds;
+
+  if (mRegionType != NULLREGION)
+  {
+    ::GetRgnBox(mRegion, &bounds);
+
+    *aX = bounds.left;
+    *aY = bounds.top;
+    *aWidth = bounds.right - bounds.left;
+    *aHeight = bounds.bottom - bounds.top;
+  }
+  else
+    *aX = *aY = *aWidth = *aHeight = 0;
 }
 
-void nsRegionWin :: Offset(nscoord aXOffset, nscoord aYOffset)
+void nsRegionWin :: Offset(PRInt32 aXOffset, PRInt32 aYOffset)
 {
+  ::OffsetRgn(mRegion, aXOffset, aYOffset);
 }
 
-PRBool nsRegionWin :: ContainsRect(const nsRect &aRect)
+PRBool nsRegionWin :: ContainsRect(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight)
 {
-  return PR_FALSE;
+  RECT  trect;
+
+  trect.left = aX;
+  trect.top = aY;
+  trect.right = aX + aWidth;
+  trect.bottom = aY + aHeight;
+
+  return ::RectInRegion(mRegion, &trect) ? PR_TRUE : PR_FALSE;
 }
 
 PRBool nsRegionWin :: ForEachRect(nsRectInRegionFunc *func, void *closure)
