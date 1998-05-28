25924c817281005f02f8fbc82154d4d65a2a8cd9
sfraser <sfraser>
1998-05-28 05:16:58 +0000

    *** empty log message ***

--- build/mac/TinderboxMozilla.pl
+++ build/mac/TinderboxMozilla.pl
@@ -1,7 +1,7 @@
 #!perl
 
 	use Moz;
-	use BuildList;
+	use BuildListObsolete;		# will be BuildList when we switch to the new world
 
 $DEBUG = 1;
 $MOZ_LITE = 0;		# build moz medium. This will come from a config file at some stage.
