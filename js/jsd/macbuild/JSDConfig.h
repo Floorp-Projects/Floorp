5e29da9c470f11911ac36be50d6cf08f1607fc3d
sfraser <sfraser>
1998-06-03 22:38:01 +0000

    First Checked In.

---
+++ js/jsd/macbuild/JSDConfig.h
@@ -0,0 +1,5 @@
+
+
+#include "JavaDefines.h"
+
+/* Nothing to do here. If you need JSD-specific defines, add them here */
---
+++ modules/softupdt/macbuild/SoftUpdateConfig.h
@@ -0,0 +1,5 @@
+
+/* Softupdate-specific defines. None for now */
+
+// We need ENOENT and EACCES for reg.c
+#include <OpenTransport.h>
