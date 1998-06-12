78988a9559a0cfb9e8fa33513c38677ea9b5fcb6
jwz <jwz>
1998-06-12 22:03:28 +0000

    update the lxr sources too; and log.

--- webtools/lxr/update-src.sh
+++ webtools/lxr/update-src.sh
@@ -9,9 +9,20 @@ PATH=/opt/local/bin:$PATH
 export PATH
 
 lxr_dir=.
+db_dir=`sed -n 's@^dbdir:[ 	]*\(.*\)@\1@p' < $lxr_dir/lxr.conf`
 src_dir=`sed -n 's@^sourceroot:[ 	]*\(.*\)@\1@p' < $lxr_dir/lxr.conf`
+log=$db_dir/cvs.log
 
+exec > $log 2>&1
+set -x
+
+# update the lxr sources
+pwd
+cvs -Q -d $CVSROOT update -dP
+
+# then update the Mozilla sources
 cd $src_dir
 cd ..
 cvs -Q -d $CVSROOT checkout MozillaSource
+
 exit 0
