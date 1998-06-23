fd2d85d3dff35cd9b4059d32b40aece7054f108a
norris <norris>
1998-06-23 18:20:59 +0000

    (Not part of Communicator build.) Fix array length updating for negative indices.

--- js/ref/jsarray.c
+++ js/ref/jsarray.c
@@ -64,7 +64,7 @@ IdIsIndex(jsid id, jsuint *indexp)
     jschar *cp;
 
     if (JSVAL_IS_INT(id)) {
-	jsuint i;
+	jsint i;
         i = JSVAL_TO_INT(id);
         if (i < 0)
             return JS_FALSE;
