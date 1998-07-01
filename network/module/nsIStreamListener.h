5530aff5944e8c7a30877ece42970ec1a5b4a10f
rpotts <rpotts>
1998-07-01 11:23:17 +0000

    Split the nsIStreamListner interface into nsIStreamObserver and nsIStreamListener

--- network/module/nsIStreamListener.h
+++ network/module/nsIStreamListener.h
@@ -26,37 +26,15 @@
 class nsIInputStream;
 class nsString;
 
-/* 45d234d0-c6c9-11d1-bea2-00805f8a66dc */
-#define NS_ISTREAMNOTIFICATION_IID   \
-{ 0x45d234d0, 0xc6c9, 0x11d1, \
-  {0xbe, 0xa2, 0x00, 0x80, 0x5f, 0x8a, 0x66, 0xdc} }
 
-/**
- * The nsIStreamListener interface provides the necessary notifications 
- * during both synchronous and asynchronous URL loading.  
- * This is a preliminary interface which <B>will</B> change over time!
- * <BR><BR>
- * An instance of this interface is passed to nsINetService::Open(...) to
- * allow the client to receive status and notifications during the loading 
- * of the URL. 
- * <BR><BR>
- * Over time this interface will provide the same functionality as the 
- * IBindStatusCallback interface in the MS INET-SDK...
- */
-class nsIStreamListener : public nsISupports {
+/* 97566110-ff60-11d1-beb9-00805f8a66dc */
+#define NS_ISTREAMOBSERVER_IID   \
+{ 0x97566110, 0xff60, 0x11d1, \
+  {0xbe, 0xb9, 0x00, 0x80, 0x5f, 0x8a, 0x66, 0xdc} }
+
+
+class nsIStreamObserver : public nsISupports {
 public:
-    /**
-     * Return information regarding the current URL load.<BR>
-     * 
-     * This method is currently not called.  
-     */
-    NS_IMETHOD GetBindInfo(void) = 0;
-    /**
-     * Notify the client that progress as occurred for the URL load.<BR>
-     */
-    NS_IMETHOD OnProgress(PRInt32 aProgress, PRInt32 aProgressMax, 
-                          const nsString &aMsg) = 0;
-
     /**
      * Notify the client that the URL has started to load.  This method is
      * called only once, at the beginning of a URL load.<BR><BR>
@@ -67,17 +45,10 @@ public:
     NS_IMETHOD OnStartBinding(const char *aContentType) = 0;
 
     /**
-     * Notify the client that data is available in the input stream.  This
-     * method is called whenver data is written into the input stream by the
-     * networking library...<BR><BR>
-     * 
-     * @param pIStream  The input stream containing the data.  This stream can
-     * be either a blocking or non-blocking stream.
-     * @param length    The amount of data that was just pushed into the stream.
-     * @return The return value is currently ignored.
+     * Notify the client that progress as occurred for the URL load.<BR>
      */
-    NS_IMETHOD OnDataAvailable(nsIInputStream *aIStream, 
-                               PRInt32 aLength)   = 0;
+    NS_IMETHOD OnProgress(PRInt32 aProgress, PRInt32 aProgressMax, 
+                          const nsString &aMsg) = 0;
 
     /**
      * Notify the client that the URL has finished loading.  This method is 
@@ -98,5 +69,48 @@ public:
 #define NS_BINDING_FAILED       (-1)
 #define NS_BINDING_ABORTED      (-2)
 
+
+
+
+
+/* 45d234d0-c6c9-11d1-bea2-00805f8a66dc */
+#define NS_ISTREAMLISTENER_IID   \
+{ 0x45d234d0, 0xc6c9, 0x11d1, \
+  {0xbe, 0xa2, 0x00, 0x80, 0x5f, 0x8a, 0x66, 0xdc} }
+
+/**
+ * The nsIStreamListener interface provides the necessary notifications 
+ * during both synchronous and asynchronous URL loading.  
+ * This is a preliminary interface which <B>will</B> change over time!
+ * <BR><BR>
+ * An instance of this interface is passed to nsINetService::Open(...) to
+ * allow the client to receive status and notifications during the loading 
+ * of the URL. 
+ * <BR><BR>
+ * Over time this interface will provide the same functionality as the 
+ * IBindStatusCallback interface in the MS INET-SDK...
+ */
+class nsIStreamListener : public nsIStreamObserver {
+public:
+    /**
+     * Return information regarding the current URL load.<BR>
+     * 
+     * This method is currently not called.  
+     */
+    NS_IMETHOD GetBindInfo(void) = 0;
+
+    /**
+     * Notify the client that data is available in the input stream.  This
+     * method is called whenver data is written into the input stream by the
+     * networking library...<BR><BR>
+     * 
+     * @param pIStream  The input stream containing the data.  This stream can
+     * be either a blocking or non-blocking stream.
+     * @param length    The amount of data that was just pushed into the stream.
+     * @return The return value is currently ignored.
+     */
+    NS_IMETHOD OnDataAvailable(nsIInputStream *aIStream, 
+                               PRInt32 aLength)   = 0;
+};
 
 #endif /* nsIStreamListener_h___ */
