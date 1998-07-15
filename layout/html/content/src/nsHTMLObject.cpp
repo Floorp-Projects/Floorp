ccc9c965bfdb3e63cf5e8c048bcfa869fb7f45fa
kipp <kipp>
1998-07-15 00:21:51 +0000

    Use NS_NewObjectFrame

--- layout/html/content/src/nsHTMLObject.cpp
+++ layout/html/content/src/nsHTMLObject.cpp
@@ -1,35 +1,25 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
  * The contents of this file are subject to the Netscape Public License
- * Version 1.0 (the "NPL"); you may not use this file except in
- * compliance with the NPL.  You may obtain a copy of the NPL at
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
  * http://www.mozilla.org/NPL/
  *
- * Software distributed under the NPL is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
- * for the specific language governing rights and limitations under the
- * NPL.
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
  *
- * The Initial Developer of this code under the NPL is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
- * Reserved.
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
  */
 #include "nsHTMLParts.h"
 #include "nsHTMLContainer.h"
 #include "nsFrame.h"
 #include "nsHTMLIIDs.h"
-
-// XXX temporary
-nsresult NS_NewObjectFrame(nsIFrame** aFrameResult, nsIContent* aContent,
-                           nsIFrame* aParentFrame)
-{
-  nsFrame::NewFrame(aFrameResult, aContent, aParentFrame);
-  if (nsnull == *aFrameResult) {
-    return NS_ERROR_OUT_OF_MEMORY;
-  }
-  return NS_OK;
-}
 
 #define nsHTMLObjectSuper nsHTMLContainer
 
@@ -44,7 +34,6 @@ public:
 
 protected:
   virtual ~nsHTMLObject();
-  nsString mObject;
 };
 
 nsHTMLObject::nsHTMLObject(nsIAtom* aTag)
@@ -63,7 +52,7 @@ nsHTMLObject::CreateFrame(nsIPresContext*  aPresContext,
                           nsIFrame*&       aResult)
 {
   nsIFrame* frame;
-  nsresult rv = NS_NewObjectFrame(&frame, this, aParentFrame);
+  nsresult rv = NS_NewObjectFrame(frame, this, aParentFrame);
   if (NS_OK != rv) {
     return rv;
   }
