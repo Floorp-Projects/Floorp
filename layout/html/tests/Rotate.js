f014a701dc8fa924a234be3db9e123993e142a19
vidur@netscape.com <vidur@netscape.com>
1998-07-16 23:39:08 +0000

    Rotation test revamped

--- layout/html/tests/Rotate.js
+++ layout/html/tests/Rotate.js
@@ -18,22 +18,35 @@
 
 var body = document.documentElement.childNodes[1];
 var para = body.childNodes[1];
-var image = para.childNodes[5];
+var img1 = para.childNodes[5];
+var tbody = body.childNodes[2].childNodes[1];
+var tparent = tbody.childNodes[0].childNodes[0];
+var img2 = tparent.childNodes[0].childNodes[0];
 
 var pos = 0;
 
-function rotate() {
-  dump("Removing\n");
-  para.removeChild(image);
-  dump("Getting child " + pos + "\n");
-  var child = para.childNodes[pos++];
-  if (pos == para.childNodes.length) {
-    pos = 0;
-  }
-  dump("Inserting\n");
-  para.insertBefore(image, child);
-  dump("Done\n");
+function rotate(p, c) {
+    p.removeChild(c);
+    var child = p.childNodes[pos++];
+    if (pos > p.childNodes.length) {
+      pos = 0;
+    }  
+    p.insertBefore(c, child);
 }
 
-//rotate();
-setInterval(rotate, 2000);
+function rotate2() {
+  tparent.childNodes[0].removeChild(img2);
+  if (tparent.nextSibling != null) {
+    tparent = tparent.nextSibling;
+  }
+  else if (tparent.parentNode.nextSibling != null) {
+    tparent = tparent.parentNode.nextSibling.childNodes[0];
+  }
+  else {
+    tparent = tbody.childNodes[0].childNodes[0];
+  }
+  tparent.childNodes[0].insertBefore(img2, tparent.childNodes[0].childNodes[0]);
+}
+
+var int1 = setInterval(rotate, 1000, para, img1);
+var int2 = setInterval(rotate2, 1000)
