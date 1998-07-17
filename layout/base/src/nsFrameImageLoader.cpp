43549900baab3795635112b9a2a7f6294c956b7c
troy%netscape.com <troy%netscape.com>
1998-07-17 04:41:35 +0000

    Added a #if 0 around the code to turn on transparent images

--- layout/base/src/nsFrameImageLoader.cpp
+++ layout/base/src/nsFrameImageLoader.cpp
@@ -132,9 +132,13 @@ nsFrameImageLoader::Init(nsIPresContext* aPresContext,
      cp ? cp : "(null)", mTargetFrame, mImageLoadStatus));
 
   // Start image load request
-  // XXX Use aBackgroundColor
+  // XXX SPARKY
+#if 0
+  mImageRequest = aGroup->GetImage(cp, this, aBackgroundColor, 0, 0, 0);
+#else
   nscolor bgcolor = NS_RGB(255,255,255);
   mImageRequest = aGroup->GetImage(cp, this, &bgcolor, 0, 0, 0);
+#endif
   delete cp;
 
   return NS_OK;
