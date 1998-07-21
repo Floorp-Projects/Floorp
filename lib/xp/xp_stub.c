ac396e4c83259575039c600956817dfb84fe002a
scullin%netscape.com <scullin%netscape.com>
1998-07-21 18:40:42 +0000

    Include xp strings. NGLayout build only.

--- lib/xp/xp_stub.c
+++ lib/xp/xp_stub.c
@@ -17,6 +17,9 @@
  */
 
 #include "prlog.h"
+#undef MOZILLA_CLIENT
+#define RESOURCE_STR
+#include "allxpstr.h"
 
 #ifdef DEBUG
 char * 
@@ -38,8 +41,48 @@ void XP_AssertAtLine( char *pFileName, int iLine )
  * This one takes XP string ID (which is used mainly by libnet, libsec) 
  * and loads String from resource file (netscape.rc3).
  */
-char *XP_GetString(int id)
+#ifndef MOZILLA_CLIENT
+/*
+ * This routine picks up a builtin (compiled in) numbered string.
+ * (I.e. error messages, etc.)
+ *
+ * If you are getting an unresolved symbol XP_GetString and you don't
+ * want to get strings from resources (which is how the Win, Mac and X
+ * versions of the Navigator do it), then you can define your own
+ * wrapper function like this:
+ */
+char *
+XP_GetString(int16 i)
 {
-	return "Not implemented...";
+	extern char * XP_GetBuiltinString(int16 i);
+
+	return XP_GetBuiltinString(i);
+}
+#endif /* ! MOZILLA_CLIENT */
+
+char *
+XP_GetBuiltinString(int16 i)
+{
+	static char	buf[128];
+	char		*ret;
+
+	i += RES_OFFSET;
+
+	if
+	(
+		((ret = mcom_include_merrors_i_strings (i))) ||
+		((ret = mcom_include_secerr_i_strings  (i))) ||
+		((ret = mcom_include_sec_dialog_strings(i))) ||
+		((ret = mcom_include_sslerr_i_strings  (i))) ||
+		((ret = mcom_include_xp_error_i_strings(i))) ||
+		((ret = mcom_include_xp_msg_i_strings  (i)))
+	)
+	{
+		return ret;
+	}
+
+	(void) sprintf(buf, "XP_GetBuiltinString: %d not found", i);
+
+	return buf;
 }
 
