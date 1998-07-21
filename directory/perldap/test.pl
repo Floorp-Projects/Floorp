6f5311dbdd68eb3cb7b2de6342ec42d40ff467cd
leif <leif>
1998-07-21 02:42:38 +0000

    Added the MPL license to all files.

--- directory/perldap/test.pl
+++ directory/perldap/test.pl
@@ -1,5 +1,29 @@
-# Before `make install' is performed this script should be runnable with
-# `make test'. After `make install' it should work as `perl test.pl'
+#############################################################################
+# $Id: test.pl,v 1.2 1998/07/21 02:42:37 leif Exp $
+#
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.0 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+# License for the specific language governing rights and limitations
+# under the License.
+#
+# The Original Code is PerlDap. The Initial Developer of the Original
+# Code is Netscape Communications Corp. and Clayton Donley. Portions
+# created by Netscape are Copyright (C) Netscape Communications
+# Corp., portions created by Clayton Donley are Copyright (C) Clayton
+# Donley. All Rights Reserved.
+#
+# Contributor(s):
+#
+# DESCRIPTION
+#    Before `make install' is performed this script should be runnable with
+#    `make test'. After `make install' it should work as `perl test.pl'
+#
+#############################################################################
 
 ######################### We start with some black magic to print on failure.
 
