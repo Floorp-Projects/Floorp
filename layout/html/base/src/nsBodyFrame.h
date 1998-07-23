edccbae0030dc863a8afb41ef957233a59a3aabf
troy@netscape.com <troy@netscape.com>
1998-07-23 04:22:41 +0000

    Added code to Reflow() to invalidate the damaged areas

--- layout/html/base/src/nsBodyFrame.h
+++ layout/html/base/src/nsBodyFrame.h
@@ -112,6 +112,8 @@ protected:
 
   void AddFrame(nsIFrame* aFrame);
 
+  nscoord GetConstrainingHeight(const nsReflowState&) const;
+
 private:
   nsSpaceManager* mSpaceManager;
   nsVoidArray     mAbsoluteItems;
