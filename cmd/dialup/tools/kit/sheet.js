74724548ce0ee6a414d58faf0051e5bfe6aaa76a
racham%netscape.com <racham%netscape.com>
1998-07-25 02:15:52 +0000

    Corrected NPL comment style.

--- cmd/dialup/tools/kit/sheet.js
+++ cmd/dialup/tools/kit/sheet.js
@@ -1,20 +1,21 @@
-<!--  -*- Mode: HTML; tab-width: 2; indent-tabs-mode: nil -*-
-
-   The contents of this file are subject to the Netscape Public License
-   Version 1.0 (the "NPL"); you may not use this file except in
-   compliance with the NPL.  You may obtain a copy of the NPL at
-   http://www.mozilla.org/NPL/
-
-   Software distributed under the NPL is distributed on an "AS IS" basis,
-   WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
-   for the specific language governing rights and limitations under the
-   NPL.
-
-   The Initial Developer of this code under the NPL is Netscape
-   Communications Corporation.  Portions created by Netscape are
-   Copyright (C) 1998 Netscape Communications Corporation.  All Rights
-   Reserved.
- -->
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+<!--  to hide script contents from old browsers
  //TAGS FOR ACCOUNT SETUP EDITOR DIALOGS
 
 
@@ -181,3 +182,4 @@ document.classes.bodysecondparagraph.all.fontSize='12px';
 document.classes.tabletext.fontsize='11px';
 document.classes.tabletext.fontWeight='900';
 
+// end hiding contents from old browsers  -->
