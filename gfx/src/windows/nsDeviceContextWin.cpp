af1b02b4a156effd63f4dc049e30657b058becbf
troy%netscape.com <troy%netscape.com>
1998-07-26 04:28:48 +0000

    Added DeviceContextImpl base class

--- gfx/src/windows/nsDeviceContextWin.cpp
+++ gfx/src/windows/nsDeviceContextWin.cpp
@@ -18,12 +18,9 @@
 
 #include "nsDeviceContextWin.h"
 #include "nsRenderingContextWin.h"
-#include "../nsGfxCIID.h"
-
-static NS_DEFINE_IID(kISupportsIID, NS_ISUPPORTS_IID);
-static NS_DEFINE_IID(kDeviceContextIID, NS_IDEVICE_CONTEXT_IID);
 
 nsDeviceContextWin :: nsDeviceContextWin()
+  : DeviceContextImpl()
 {
   NS_INIT_REFCNT();
 
@@ -34,78 +31,16 @@ nsDeviceContextWin :: nsDeviceContextWin()
 
   ::ReleaseDC(NULL, hdc);
 
-  mFontCache = nsnull;
-
-  mDevUnitsToAppUnits = 1.0f;
-  mAppUnitsToDevUnits = 1.0f;
-
-  mGammaValue = 1.0f;
-  mGammaTable = new PRUint8[256];
-
-  mZoom = 1.0f;
-
   mSurface = NULL;
 }
 
 nsDeviceContextWin :: ~nsDeviceContextWin()
 {
-  NS_IF_RELEASE(mFontCache);
-
   if (NULL != mSurface)
   {
     DeleteDC(mSurface);
     mSurface = NULL;
   }
-
-  if (nsnull != mGammaTable)
-  {
-    delete mGammaTable;
-    mGammaTable = nsnull;
-  }
-}
-
-NS_IMPL_QUERY_INTERFACE(nsDeviceContextWin, kDeviceContextIID)
-NS_IMPL_ADDREF(nsDeviceContextWin)
-NS_IMPL_RELEASE(nsDeviceContextWin)
-
-nsresult nsDeviceContextWin :: Init(nsNativeWidget aWidget)
-{
-  for (PRInt32 cnt = 0; cnt < 256; cnt++)
-    mGammaTable[cnt] = cnt;
-
-  mWidget = aWidget;
-
-  return NS_OK;
-}
-
-float nsDeviceContextWin :: GetTwipsToDevUnits() const
-{
-  return mTwipsToPixels;
-}
-
-float nsDeviceContextWin :: GetDevUnitsToTwips() const
-{
-  return mPixelsToTwips;
-}
-
-void nsDeviceContextWin :: SetAppUnitsToDevUnits(float aAppUnits)
-{
-  mAppUnitsToDevUnits = aAppUnits;
-}
-
-void nsDeviceContextWin :: SetDevUnitsToAppUnits(float aDevUnits)
-{
-  mDevUnitsToAppUnits = aDevUnits;
-}
-
-float nsDeviceContextWin :: GetAppUnitsToDevUnits() const
-{
-  return mAppUnitsToDevUnits;
-}
-
-float nsDeviceContextWin :: GetDevUnitsToAppUnits() const
-{
-  return mDevUnitsToAppUnits;
 }
 
 float nsDeviceContextWin :: GetScrollBarWidth() const
@@ -118,79 +53,6 @@ float nsDeviceContextWin :: GetScrollBarHeight() const
   return ::GetSystemMetrics(SM_CXHSCROLL) * mDevUnitsToAppUnits;
 }
 
-nsIRenderingContext * nsDeviceContextWin :: CreateRenderingContext(nsIView *aView)
-{
-  nsIRenderingContext *pContext = nsnull;
-  nsIWidget           *win = aView->GetWidget();
-  nsresult            rv;
-
-  static NS_DEFINE_IID(kRCCID, NS_RENDERING_CONTEXT_CID);
-  static NS_DEFINE_IID(kRCIID, NS_IRENDERING_CONTEXT_IID);
-
-  rv = NSRepository::CreateInstance(kRCCID, nsnull, kRCIID, (void **)&pContext);
-
-  if (NS_OK == rv) {
-    rv = InitRenderingContext(pContext, win);
-    if (NS_OK != rv) {
-      NS_RELEASE(pContext);
-    }
-  }
-
-  NS_IF_RELEASE(win);
-  return pContext;
-}
-
-nsresult nsDeviceContextWin :: InitRenderingContext(nsIRenderingContext *aContext, nsIWidget *aWin)
-{
-  return (aContext->Init(this, aWin));
-}
-
-nsIFontCache* nsDeviceContextWin::GetFontCache()
-{
-  if (nsnull == mFontCache) {
-    if (NS_OK != CreateFontCache()) {
-      return nsnull;
-    }
-  }
-  NS_ADDREF(mFontCache);
-  return mFontCache;
-}
-
-void nsDeviceContextWin::FlushFontCache()
-{
-  NS_IF_RELEASE(mFontCache);
-}
-
-nsresult nsDeviceContextWin::CreateFontCache()
-{
-  nsresult rv = NS_NewFontCache(&mFontCache);
-  if (NS_OK != rv) {
-    return rv;
-  }
-  mFontCache->Init(this);
-  return NS_OK;
-}
-
-nsIFontMetrics* nsDeviceContextWin::GetMetricsFor(const nsFont& aFont)
-{
-  if (nsnull == mFontCache) {
-    if (NS_OK != CreateFontCache()) {
-      return nsnull;
-    }
-  }
-  return mFontCache->GetMetricsFor(aFont);
-}
-
-void nsDeviceContextWin :: SetZoom(float aZoom)
-{
-  mZoom = aZoom;
-}
-
-float nsDeviceContextWin :: GetZoom() const
-{
-  return mZoom;
-}
-
 nsDrawingSurface nsDeviceContextWin :: GetDrawingSurface(nsIRenderingContext &aContext)
 {
   if (NULL == mSurface)
@@ -199,42 +61,4 @@ nsDrawingSurface nsDeviceContextWin :: GetDrawingSurface(nsIRenderingContext &aC
   return mSurface;
 }
 
-nsNativeWidget nsDeviceContextWin :: GetNativeWidget(void)
-{
-  return mWidget;
-}
 
-float nsDeviceContextWin :: GetGamma(void)
-{
-  return mGammaValue;
-}
-
-void nsDeviceContextWin :: SetGamma(float aGamma)
-{
-  if (aGamma != mGammaValue)
-  {
-    //we don't need to-recorrect existing images for this case
-    //so pass in 1.0 for the current gamma regardless of what it
-    //really happens to be. existing images will get a one time
-    //re-correction when they're rendered the next time. MMP
-
-    SetGammaTable(mGammaTable, 1.0f, aGamma);
-
-    mGammaValue = aGamma;
-  }
-}
-
-PRUint8 * nsDeviceContextWin :: GetGammaTable(void)
-{
-  //XXX we really need to ref count this somehow. MMP
-
-  return mGammaTable;
-}
-
-void nsDeviceContextWin :: SetGammaTable(PRUint8 * aTable, float aCurrentGamma, float aNewGamma)
-{
-  double fgval = (1.0f / aCurrentGamma) * (1.0f / aNewGamma);
-
-  for (PRInt32 cnt = 0; cnt < 256; cnt++)
-    aTable[cnt] = (PRUint8)(pow((double)cnt * (1. / 256.), fgval) * 255.99999999);
-}
--- gfx/src/windows/nsDeviceContextWin.h
+++ gfx/src/windows/nsDeviceContextWin.h
@@ -19,69 +19,27 @@
 #ifndef nsDeviceContextWin_h___
 #define nsDeviceContextWin_h___
 
-#include "nsIDeviceContext.h"
-#include "nsUnitConversion.h"
-#include "nsIFontCache.h"
-#include "nsIWidget.h"
-#include "nsIView.h"
-#include "nsIRenderingContext.h"
+#include "nsDeviceContext.h"
 #include <windows.h>
 
-class nsDeviceContextWin : public nsIDeviceContext
+class nsDeviceContextWin : public DeviceContextImpl
 {
 public:
   nsDeviceContextWin();
 
-  NS_DECL_ISUPPORTS
-
-  virtual nsresult Init(nsNativeDeviceContext aNativeDeviceContext);
-
-  virtual nsIRenderingContext * CreateRenderingContext(nsIView *aView);
-  virtual nsresult InitRenderingContext(nsIRenderingContext *aContext, nsIWidget *aWidget);
-
-  virtual float GetTwipsToDevUnits() const;
-  virtual float GetDevUnitsToTwips() const;
-
-  virtual void SetAppUnitsToDevUnits(float aAppUnits);
-  virtual void SetDevUnitsToAppUnits(float aDevUnits);
-
-  virtual float GetAppUnitsToDevUnits() const;
-  virtual float GetDevUnitsToAppUnits() const;
-
   virtual float GetScrollBarWidth() const;
   virtual float GetScrollBarHeight() const;
 
-  virtual nsIFontCache * GetFontCache();
-  virtual void FlushFontCache();
-
-  virtual nsIFontMetrics* GetMetricsFor(const nsFont& aFont);
-
-  virtual void SetZoom(float aZoom);
-  virtual float GetZoom() const;
-
+  //get a low level drawing surface for rendering. the rendering context
+  //that is passed in is used to create the drawing surface if there isn't
+  //already one in the device context. the drawing surface is then cached
+  //in the device context for re-use.
   virtual nsDrawingSurface GetDrawingSurface(nsIRenderingContext &aContext);
 
-  virtual float GetGamma(void);
-  virtual void SetGamma(float aGamma);
-  virtual PRUint8 * GetGammaTable(void);
-
-  virtual nsNativeWidget GetNativeWidget(void);
 protected:
   ~nsDeviceContextWin();
 
-  nsresult CreateFontCache();
-  void SetGammaTable(PRUint8 * aTable, float aCurrentGamma, float aNewGamma);
-
-  float             mTwipsToPixels;
-  float             mPixelsToTwips;
-  float             mAppUnitsToDevUnits;
-  float             mDevUnitsToAppUnits;
-  nsIFontCache      *mFontCache;
-  float             mZoom;
-  HDC               mSurface;
-  float             mGammaValue;
-  PRUint8           *mGammaTable;
-  nsNativeWidget    mWidget;
+  HDC mSurface;
 };
 
 #endif /* nsDeviceContextWin_h___ */
