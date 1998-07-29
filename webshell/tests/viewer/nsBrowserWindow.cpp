18ac4ccfe96bd457804c36cbca5e3df5e4603494
pierre%netscape.com <pierre%netscape.com>
1998-07-29 23:43:22 +0000

    fixing Mac build: moved NS_IMPL_IDS

--- webshell/tests/viewer/nsBrowserWindow.cpp
+++ webshell/tests/viewer/nsBrowserWindow.cpp
@@ -16,8 +16,13 @@
  * Corporation.  Portions created by Netscape are Copyright (C) 1998
  * Netscape Communications Corporation.  All Rights Reserved.
  */
+#ifdef XP_MAC
 #include "nsBrowserWindow.h"
 #define NS_IMPL_IDS
+#else
+#define NS_IMPL_IDS
+#include "nsBrowserWindow.h"
+#endif
 #include "nsIStreamListener.h"
 #include "nsIAppShell.h"
 #include "nsIWidget.h"
