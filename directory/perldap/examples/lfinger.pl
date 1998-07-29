49eb2981e0aad95234fb7b7c8faff9895ddb6b31
leif <leif>
1998-07-29 09:01:12 +0000

    *** empty log message ***

--- directory/perldap/examples/lfinger.pl
+++ directory/perldap/examples/lfinger.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl5
 #############################################################################
-# $Id: lfinger.pl,v 1.7 1998/07/29 08:36:45 leif Exp $
+# $Id: lfinger.pl,v 1.8 1998/07/29 08:59:05 leif Exp $
 #
 # The contents of this file are subject to the Mozilla Public License
 # Version 1.0 (the "License"); you may not use this file except in
@@ -38,7 +38,7 @@ use Mozilla::LDAP::Utils;		# LULU, utilities.
 # effectively with appropriate ACI/ACLs.
 #
 $APPNAM	= "lfinger";
-$USAGE	= "$APPNAM [-nv] -b base -h host -D bind -w pswd -P cert user_info";
+$USAGE	= "$APPNAM -m -b base -h host -D bind -w pswd -P cert user_info";
 
 @ATTRIBUTES = ("uid", "cn", "homedirectory", "loginshell", "pager",
 	       "telephonenumber", "facsimiletelephonenumber", "mobile");
@@ -84,7 +84,7 @@ sub printIt
 #############################################################################
 # Check arguments, and configure some parameters accordingly..
 #
-if (!getopts('nvb:h:D:p:w:P:') || !defined($ARGV[$[]))
+if (!getopts('mb:h:D:p:w:P:') || !defined($ARGV[$[]))
 {
    print "usage: $APPNAM $USAGE\n";
    exit;
@@ -96,15 +96,22 @@ $user=$ARGV[$[];
 #############################################################################
 # Instantiate an LDAP object, which also binds to the LDAP server.
 #
-$conn = new Mozilla::LDAP::Conn($ld{host}, $ld{port}, $ld{bind},
-				$ld{pswd}, $ld{cert});
+$conn = new Mozilla::LDAP::Conn(\%ld);
 die "Could't connect to LDAP server $ld{host}" unless $conn;
 
 
 #############################################################################
 # Ok, lets generate the filter, and do the search!
 #
-$search = "(&(|(cn=*$user*)(uid=*$user*)(telephonenumber=*$user*))(!$HIDE))";
+if ($opt_m)
+{
+  $search = "(&(uid=$user)(!$HIDE))";
+}
+else
+{
+  $search = "(&(|(cn=*$user*)(uid=*$user*)(telephonenumber=*$user*))(!$HIDE))";
+}
+
 $entry = $conn->search($ld{root}, "subtree", $search, 0, @ATTRIBUTES);
 while($entry)
 {
