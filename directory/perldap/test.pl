f80c8d06f312c04c82d9ee747969c0681ae2549c
clayton <clayton>
1998-07-29 03:43:34 +0000

    MANIFEST - Changed Connection.pm to Conn.pm Makefile.PL - Added Interactive and Automatic Makefile creation test.pl - Added Automatic Testing through use of environment variables.

--- directory/perldap/test.pl
+++ directory/perldap/test.pl
@@ -1,5 +1,5 @@
 #############################################################################
-# $Id: test.pl,v 1.4 1998/07/24 19:01:48 clayton Exp $
+# $Id: test.pl,v 1.5 1998/07/29 03:43:34 clayton Exp $
 #
 # The contents of this file are subject to the Mozilla Public License
 # Version 1.0 (the "License"); you may not use this file except in
@@ -39,19 +39,32 @@ print "modinit  - ok\n";
 ######################### End of black magic.
 
 
-$attrs = ["cn","telephoneNumber","mail"];
-print "\nEnter LDAP Server: ";
-chomp($ldap_host = <>);
-print "Enter Search Filter (ex. uid=abc123): ";
-chomp($filter = <>);
-print "Enter LDAP Search Base (ex. o=Org, c=US): ";
-chomp($BASEDN = <>);
-print "\n";
+$attrs = [];
+
+$ldap_host = $ENV{"LDAPHOST"};
+$filter = $ENV{"TESTFILTER"};
+$BASEDN = $ENV{"LDAPBASE"};
 
 if (!$ldap_host)
 {
-   die "Please edit \$BASEDN, \$filter and \$ldap_host in test.pl.\n";
+   print "\nEnter LDAP Server: ";
+   chomp($ldap_host = <>);
 }
+
+if (!$filter)
+{
+   print "Enter Search Filter (ex. uid=abc123): ";
+   chomp($filter = <>);
+}
+
+if (!$BASEDN)
+{
+   print "Enter LDAP Search Base (ex. o=Org, c=US): ";
+   chomp($BASEDN = <>);
+}
+
+print "\n";
+
 
 ##
 ##  Initialize LDAP Connection
