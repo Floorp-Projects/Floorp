b84b13c22b5355a1a1901727f4cdebeb43323737
troy@netscape.com <troy@netscape.com>
1998-08-03 23:29:35 +0000

    Added ASSERT for the case where we get passed a reflow reason of initial when we've already been reflowed once

--- layout/tables/nsTableOuterFrame.cpp
+++ layout/tables/nsTableOuterFrame.cpp
@@ -401,6 +401,8 @@ NS_METHOD nsTableOuterFrame::Reflow(nsIPresContext& aPresContext,
 
   } else {
     if (eReflowReason_Initial == aReflowState.reason) {
+      NS_ASSERTION(nsnull == mFirstChild, "unexpected reflow reason");
+
       // Set up our kids.  They're already present, on an overflow list, 
       // or there are none so we'll create them now
       MoveOverflowToChildList();
