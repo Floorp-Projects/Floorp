e6258c9d2d0d4e108a52d30c646b8d1640aad63e
morse%netscape.com <morse%netscape.com>
1998-08-04 00:51:34 +0000

    change for anonymous mode

--- lib/libmisc/glhist.c
+++ lib/libmisc/glhist.c
@@ -18,6 +18,7 @@
 
 /*
  */
+#include "privacy.h"
 #include "glhist.h"
 #include "xp_hash.h"
 #include "net.h"
@@ -206,6 +207,10 @@ gh_open_database(void)
 {
 #ifndef NO_DBM
 	static Bool have_tried_open=FALSE;
+
+	if (PRVCY_IsAnonymous()) {
+	    return;
+	}
 
 	if(gh_database)
 	  {
