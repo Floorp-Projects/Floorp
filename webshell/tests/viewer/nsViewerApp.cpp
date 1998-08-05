98e6fa4a35b7ce84e8b2546176b13f41c943cc1f
pierre@netscape.com <pierre@netscape.com>
1998-08-05 08:25:24 +0000

    removed NS_IMPL_IDS: it moved to nsPref.cpp

--- webshell/tests/viewer/nsViewerApp.cpp
+++ webshell/tests/viewer/nsViewerApp.cpp
@@ -16,10 +16,7 @@
  * Corporation.  Portions created by Netscape are Copyright (C) 1998
  * Netscape Communications Corporation.  All Rights Reserved.
  */
-#ifdef XP_MAC
-#define NS_IMPL_IDS
-#endif 
- 
+
 #include "nsViewerApp.h"
 #include "nsBrowserWindow.h"
 #include "nsWidgetsCID.h"
