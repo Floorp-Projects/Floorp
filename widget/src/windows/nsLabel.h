42ec04f91ce7139a04174119f02c1cf5144ba649
rods%netscape.com <rods%netscape.com>
1998-08-27 20:16:20 +0000

    Initial working checkin

---
+++ widget/src/windows/nsLabel.h
@@ -0,0 +1,68 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsLabel_h__
+#define nsLabel_h__
+
+#include "nsdefs.h"
+#include "nsWindow.h"
+#include "nsSwitchToUIThread.h"
+
+#include "nsILabel.h"
+
+/**
+ * Native Win32 Label wrapper
+ */
+
+class nsLabel :  public nsWindow,
+                 public nsILabel
+{
+
+public:
+  nsLabel(nsISupports *aOuter);
+  virtual ~nsLabel();
+
+    // nsISupports. Forward to the nsObject base class
+  BASE_SUPPORT
+
+  virtual nsresult QueryObject(const nsIID& aIID, void** aInstancePtr);
+
+    // nsIWidget interface
+  BASE_IWIDGET_IMPL
+
+    // nsILabel part
+  virtual void SetLabel(const nsString& aText);
+  virtual void GetLabel(nsString& aBuffer);
+  virtual void SetAlignment(nsLabelAlignment aAlignment);
+
+  virtual PRBool OnMove(PRInt32 aX, PRInt32 aY);
+  virtual PRBool OnPaint();
+  virtual PRBool OnResize(nsRect &aWindowRect);
+  virtual void   GetBounds(nsRect &aRect);
+  virtual void   PreCreateWidget(nsWidgetInitData *aInitData);
+
+protected:
+  nsLabelAlignment mAlignment;
+
+  virtual LPCTSTR WindowClass();
+  virtual DWORD   WindowStyle();
+  virtual DWORD   WindowExStyle();
+
+};
+
+#endif // nsLabel_h__
