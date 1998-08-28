976e98edda41c9ecc6d5059b97197093b3266bcd
cls%seawood.org <cls%seawood.org>
1998-08-28 22:40:14 +0000

    Fixed problem with makedepend under autoconf.

--- config/rules.mk
+++ config/rules.mk
@@ -789,10 +789,8 @@ $(MKDEPENDENCIES)::
 	@$(MAKE_OBJDIR)
 	touch $(MKDEPENDENCIES)
 ifdef USE_AUTOCONF
-	$(MKDEPEND) -p$(OBJDIR_NAME)/ -o'.o' -f$(MKDEPENDENCIES) $(INCLUDES) $(a
-ddprefix $(srcdir)/,$(CSRCS) $(CPPSRCS))
-	@mv depend.mk depend.mk.old && cat depend.mk.old | sed "s|^$(OBJDIR_NAME
-)/$(srcdir)/|$(OBJDIR_NAME)/|g" > depend.mk && rm -f depend.mk.old
+	$(MKDEPEND) -p$(OBJDIR_NAME)/ -o'.o' -f$(MKDEPENDENCIES) $(INCLUDES) $(addprefix $(srcdir)/,$(CSRCS) $(CPPSRCS))
+	@mv depend.mk depend.mk.old && cat depend.mk.old | sed "s|^$(OBJDIR_NAME)/$(srcdir)/|$(OBJDIR_NAME)/|g" > depend.mk && rm -f depend.mk.old
 else
 	$(MKDEPEND) -p$(OBJDIR_NAME)/ -o'.o' -f$(MKDEPENDENCIES) $(INCLUDES) $(CSRCS) $(CPPSRCS)
 endif
