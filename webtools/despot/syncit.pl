c466b207f76b12a3cbbf9456ef1d422da013b152
terry%netscape.com <terry%netscape.com>
1998-08-29 00:36:03 +0000

    Oops; wrong environment variable name.

--- webtools/despot/syncit.pl
+++ webtools/despot/syncit.pl
@@ -66,7 +66,7 @@ if ($hostname ne "lounge.mozilla.org") {
 
 $ENV{"HOME"} = glob("~$mylogin"); # CVS wants this.
 
-$ENV{"CVSPASS"} = "./.cvspass";
+$ENV{"CVS_PASSFILE"} = "./.cvspass";
 
 if ($usertoblame eq "") {
     $usertoblame = $mylogin;
