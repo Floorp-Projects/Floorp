d62ee6a1b2c05b279643eac772cc21e8d35dde07
kipp@netscape.com <kipp@netscape.com>
1998-09-01 19:07:50 +0000

    new

---
+++ layout/html/content/src/nsHTMLBody.cpp
@@ -0,0 +1,246 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+#include "nsIDOMHTMLBodyElement.h"
+#include "nsIScriptObjectOwner.h"
+#include "nsIDOMEventReceiver.h"
+#include "nsIHTMLContent.h"
+#include "nsHTMLGenericContent.h"
+#include "nsHTMLAtoms.h"
+#include "nsHTMLIIDs.h"
+#include "nsIStyleContext.h"
+#include "nsStyleConsts.h"
+#include "nsIPresContext.h"
+
+static NS_DEFINE_IID(kIDOMHTMLBodyElementIID, NS_IDOMHTMLBODYELEMENT_IID);
+
+class nsHTMLBody : public nsIDOMHTMLBodyElement,
+                   public nsIScriptObjectOwner,
+                   public nsIDOMEventReceiver,
+                   public nsIHTMLContent
+{
+public:
+  nsHTMLBody(nsIAtom* aTag);
+  ~nsHTMLBody();
+
+  // nsISupports
+  NS_DECL_ISUPPORTS
+
+  // nsIDOMNode
+  NS_IMPL_IDOMNODE_USING_GENERIC(mInner)
+
+  // nsIDOMElement
+  NS_IMPL_IDOMELEMENT_USING_GENERIC(mInner)
+
+  // nsIDOMHTMLElement
+  NS_IMPL_IDOMHTMLELEMENT_USING_GENERIC(mInner)
+
+  // nsIDOMHTMLBodyElement
+  NS_IMETHOD GetALink(nsString& aALink);
+  NS_IMETHOD SetALink(const nsString& aALink);
+  NS_IMETHOD GetBackground(nsString& aBackground);
+  NS_IMETHOD SetBackground(const nsString& aBackground);
+  NS_IMETHOD GetBgColor(nsString& aBgColor);
+  NS_IMETHOD SetBgColor(const nsString& aBgColor);
+  NS_IMETHOD GetLink(nsString& aLink);
+  NS_IMETHOD SetLink(const nsString& aLink);
+  NS_IMETHOD GetText(nsString& aText);
+  NS_IMETHOD SetText(const nsString& aText);
+  NS_IMETHOD GetVLink(nsString& aVLink);
+  NS_IMETHOD SetVLink(const nsString& aVLink);
+
+  // nsIScriptObjectOwner
+  NS_IMPL_ISCRIPTOBJECTOWNER_USING_GENERIC(mInner)
+
+  // nsIDOMEventReceiver
+  NS_IMPL_IDOMEVENTRECEIVER_USING_GENERIC(mInner)
+
+  // nsIContent
+  NS_IMPL_ICONTENT_USING_GENERIC(mInner)
+
+  // nsIHTMLContent
+  NS_IMPL_IHTMLCONTENT_USING_GENERIC(mInner)
+
+protected:
+  nsHTMLGenericContainerContent mInner;
+};
+
+nsresult
+NS_NewHTMLBody(nsIHTMLContent** aInstancePtrResult, nsIAtom* aTag)
+{
+  NS_PRECONDITION(nsnull != aInstancePtrResult, "null ptr");
+  if (nsnull == aInstancePtrResult) {
+    return NS_ERROR_NULL_POINTER;
+  }
+  nsIHTMLContent* it = new nsHTMLBody(aTag);
+  if (nsnull == it) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+  return it->QueryInterface(kIHTMLContentIID, (void**) aInstancePtrResult);
+}
+
+nsHTMLBody::nsHTMLBody(nsIAtom* aTag)
+{
+  NS_INIT_REFCNT();
+  mInner.Init(this, aTag);
+}
+
+nsHTMLBody::~nsHTMLBody()
+{
+}
+
+NS_IMPL_ADDREF(nsHTMLBody)
+
+NS_IMPL_RELEASE(nsHTMLBody)
+
+nsresult
+nsHTMLBody::QueryInterface(REFNSIID aIID, void** aInstancePtr)
+{
+  NS_IMPL_HTML_CONTENT_QUERY_INTERFACE(aIID, aInstancePtr, this)
+  if (aIID.Equals(kIDOMHTMLBodyElementIID)) {
+    nsIDOMHTMLBodyElement* tmp = this;
+    *aInstancePtr = (void*) tmp;
+    mRefCnt++;
+    return NS_OK;
+  }
+  return NS_NOINTERFACE;
+}
+
+nsresult
+nsHTMLBody::CloneNode(nsIDOMNode** aReturn)
+{
+  nsHTMLBody* it = new nsHTMLBody(mInner.mTag);
+  if (nsnull == it) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+  mInner.CopyInnerTo(this, &it->mInner);
+  return it->QueryInterface(kIDOMNodeIID, (void**) aReturn);
+}
+
+NS_IMETHODIMP
+nsHTMLBody::GetALink(nsString& aValue)
+{
+  mInner.GetAttribute(nsHTMLAtoms::alink, aValue);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsHTMLBody::SetALink(const nsString& aValue)
+{
+  return mInner.SetAttr(nsHTMLAtoms::alink, aValue, eSetAttrNotify_Render);
+}
+
+NS_IMETHODIMP
+nsHTMLBody::GetBackground(nsString& aValue)
+{
+  mInner.GetAttribute(nsHTMLAtoms::background, aValue);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsHTMLBody::SetBackground(const nsString& aValue)
+{
+  return mInner.SetAttr(nsHTMLAtoms::background, aValue,
+                        eSetAttrNotify_Render);
+}
+
+NS_IMETHODIMP
+nsHTMLBody::GetBgColor(nsString& aValue)
+{
+  mInner.GetAttribute(nsHTMLAtoms::bgcolor, aValue);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsHTMLBody::SetBgColor(const nsString& aValue)
+{
+  return mInner.SetAttr(nsHTMLAtoms::bgcolor, aValue, eSetAttrNotify_Render);
+}
+
+NS_IMETHODIMP
+nsHTMLBody::GetLink(nsString& aValue)
+{
+  mInner.GetAttribute(nsHTMLAtoms::link, aValue);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsHTMLBody::SetLink(const nsString& aValue)
+{
+  return mInner.SetAttr(nsHTMLAtoms::link, aValue, eSetAttrNotify_Render);
+}
+
+NS_IMETHODIMP
+nsHTMLBody::GetText(nsString& aValue)
+{
+  mInner.GetAttribute(nsHTMLAtoms::text, aValue);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsHTMLBody::SetText(const nsString& aValue)
+{
+  return mInner.SetAttr(nsHTMLAtoms::text, aValue, eSetAttrNotify_Render);
+}
+
+NS_IMETHODIMP
+nsHTMLBody::GetVLink(nsString& aValue)
+{
+  mInner.GetAttribute(nsHTMLAtoms::vlink, aValue);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsHTMLBody::SetVLink(const nsString& aValue)
+{
+  return mInner.SetAttr(nsHTMLAtoms::vlink, aValue, eSetAttrNotify_Render);
+}
+
+NS_IMETHODIMP
+nsHTMLBody::StringToAttribute(nsIAtom* aAttribute,
+                              const nsString& aValue,
+                              nsHTMLValue& aResult)
+{
+  return NS_CONTENT_ATTR_NOT_THERE;
+}
+
+NS_IMETHODIMP
+nsHTMLBody::AttributeToString(nsIAtom* aAttribute,
+                              nsHTMLValue& aValue,
+                              nsString& aResult) const
+{
+  return mInner.AttributeToString(aAttribute, aValue, aResult);
+}
+
+NS_IMETHODIMP
+nsHTMLBody::MapAttributesInto(nsIStyleContext* aContext,
+                              nsIPresContext* aPresContext)
+{
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsHTMLBody::HandleDOMEvent(nsIPresContext& aPresContext,
+                           nsEvent* aEvent,
+                           nsIDOMEvent** aDOMEvent,
+                           PRUint32 aFlags,
+                           nsEventStatus& aEventStatus)
+{
+  return mInner.HandleDOMEvent(aPresContext, aEvent, aDOMEvent,
+                               aFlags, aEventStatus);
+}
