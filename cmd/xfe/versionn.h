51b564df36c8e6516814ac7a27c7d4af9cbbd248
cyeh%netscape.com <cyeh%netscape.com>
1998-09-02 19:10:53 +0000

    change version number to reflect alpha reality

--- cmd/xfe/versionn.h
+++ cmd/xfe/versionn.h
@@ -20,4 +20,4 @@
 ** Set this to something like "2.1" or "2.1a1" or "2.1b1".
 ** Do *not* set it to "2.1N" or "2.1S" - that is added elsewhere.
 */
-#define VERSION_NUMBER 5.0b1
+#define VERSION_NUMBER 5.0a1
