7f7b9a4b5d65e69dfad7d00093d4b0621cd83bd2
kipp%netscape.com <kipp%netscape.com>
1998-09-04 00:52:34 +0000

    New file

---
+++ content/html/content/src/nsHTMLSpacerElement.cpp
@@ -0,0 +1,210 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+#include "nsIDOMHTMLElement.h"
+#include "nsIScriptObjectOwner.h"
+#include "nsIDOMEventReceiver.h"
+#include "nsIHTMLContent.h"
+#include "nsGenericHTMLElement.h"
+#include "nsHTMLAtoms.h"
+#include "nsHTMLIIDs.h"
+#include "nsIStyleContext.h"
+#include "nsStyleConsts.h"
+#include "nsIPresContext.h"
+
+// XXX add nsIDOMHTMLSpacer?
+
+//static NS_DEFINE_IID(kIDOMHTMLSpacerElementIID, NS_IDOMHTMLSpacerELEMENT_IID);
+
+class nsHTMLSpacerElement : public nsIDOMHTMLElement,
+                            public nsIScriptObjectOwner,
+                            public nsIDOMEventReceiver,
+                            public nsIHTMLContent
+{
+public:
+  nsHTMLSpacerElement(nsIAtom* aTag);
+  ~nsHTMLSpacerElement();
+
+  // nsISupports
+  NS_DECL_ISUPPORTS
+
+  // nsIDOMNode
+  NS_IMPL_IDOMNODE_USING_GENERIC(mInner)
+
+  // nsIDOMElement
+  NS_IMPL_IDOMELEMENT_USING_GENERIC(mInner)
+
+  // nsIDOMHTMLElement
+  NS_IMPL_IDOMHTMLELEMENT_USING_GENERIC(mInner)
+
+  // nsIScriptObjectOwner
+  NS_IMPL_ISCRIPTOBJECTOWNER_USING_GENERIC(mInner)
+
+  // nsIDOMEventReceiver
+  NS_IMPL_IDOMEVENTRECEIVER_USING_GENERIC(mInner)
+
+  // nsIContent
+  NS_IMPL_ICONTENT_USING_GENERIC(mInner)
+
+  // nsIHTMLContent
+  NS_IMPL_IHTMLCONTENT_USING_GENERIC(mInner)
+
+protected:
+  nsGenericHTMLLeafElement mInner;
+};
+
+nsresult
+NS_NewHTMLSpacerElement(nsIHTMLContent** aInstancePtrResult, nsIAtom* aTag)
+{
+  NS_PRECONDITION(nsnull != aInstancePtrResult, "null ptr");
+  if (nsnull == aInstancePtrResult) {
+    return NS_ERROR_NULL_POINTER;
+  }
+  nsIHTMLContent* it = new nsHTMLSpacerElement(aTag);
+  if (nsnull == it) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+  return it->QueryInterface(kIHTMLContentIID, (void**) aInstancePtrResult);
+}
+
+nsHTMLSpacerElement::nsHTMLSpacerElement(nsIAtom* aTag)
+{
+  NS_INIT_REFCNT();
+  mInner.Init(this, aTag);
+}
+
+nsHTMLSpacerElement::~nsHTMLSpacerElement()
+{
+}
+
+NS_IMPL_ADDREF(nsHTMLSpacerElement)
+
+NS_IMPL_RELEASE(nsHTMLSpacerElement)
+
+nsresult
+nsHTMLSpacerElement::QueryInterface(REFNSIID aIID, void** aInstancePtr)
+{
+  NS_IMPL_HTML_CONTENT_QUERY_INTERFACE(aIID, aInstancePtr, this)
+#if 0
+  if (aIID.Equals(kIDOMHTMLSpacerElementIID)) {
+    nsIDOMHTMLSpacerElement* tmp = this;
+    *aInstancePtr = (void*) tmp;
+    mRefCnt++;
+    return NS_OK;
+  }
+#endif
+  return NS_NOINTERFACE;
+}
+
+nsresult
+nsHTMLSpacerElement::CloneNode(nsIDOMNode** aReturn)
+{
+  nsHTMLSpacerElement* it = new nsHTMLSpacerElement(mInner.mTag);
+  if (nsnull == it) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+  mInner.CopyInnerTo(this, &it->mInner);
+  return it->QueryInterface(kIDOMNodeIID, (void**) aReturn);
+}
+
+NS_IMETHODIMP
+nsHTMLSpacerElement::StringToAttribute(nsIAtom* aAttribute,
+                                       const nsString& aValue,
+                                       nsHTMLValue& aResult)
+{
+  if (aAttribute == nsHTMLAtoms::size) {
+    nsGenericHTMLElement::ParseValue(aValue, 0, aResult, eHTMLUnit_Pixel);
+    return NS_CONTENT_ATTR_HAS_VALUE;
+  }
+  else if (aAttribute == nsHTMLAtoms::align) {
+    nsGenericHTMLElement::ParseAlignValue(aValue, aResult);
+    return NS_CONTENT_ATTR_HAS_VALUE;
+  }
+  else if ((aAttribute == nsHTMLAtoms::width) ||
+           (aAttribute == nsHTMLAtoms::height)) {
+    nsGenericHTMLElement::ParseValueOrPercent(aValue, aResult,
+                                              eHTMLUnit_Pixel);
+    return NS_CONTENT_ATTR_HAS_VALUE;
+  }
+  return NS_CONTENT_ATTR_NOT_THERE;
+}
+
+NS_IMETHODIMP
+nsHTMLSpacerElement::AttributeToString(nsIAtom* aAttribute,
+                                       nsHTMLValue& aValue,
+                                       nsString& aResult) const
+{
+  if (aAttribute == nsHTMLAtoms::align) {
+    if (eHTMLUnit_Enumerated == aValue.GetUnit()) {
+      nsGenericHTMLElement::AlignValueToString(aValue, aResult);
+      return NS_CONTENT_ATTR_HAS_VALUE;
+    }
+  }
+  return mInner.AttributeToString(aAttribute, aValue, aResult);
+}
+
+NS_IMETHODIMP
+nsHTMLSpacerElement::MapAttributesInto(nsIStyleContext* aContext,
+                                       nsIPresContext* aPresContext)
+{
+  if (nsnull != mInner.mAttributes) {
+    nsHTMLValue value;
+    nsStyleDisplay* display = (nsStyleDisplay*)
+      aContext->GetMutableStyleData(eStyleStruct_Display);
+    GetAttribute(nsHTMLAtoms::align, value);
+    if (eHTMLUnit_Enumerated == value.GetUnit()) {
+      switch (value.GetIntValue()) {
+      case NS_STYLE_TEXT_ALIGN_LEFT:
+        display->mFloats = NS_STYLE_FLOAT_LEFT;
+        break;
+      case NS_STYLE_TEXT_ALIGN_RIGHT:
+        display->mFloats = NS_STYLE_FLOAT_RIGHT;
+        break;
+      default:
+        break;
+      }
+    }
+
+    GetAttribute(nsHTMLAtoms::type, value);
+    if (eHTMLUnit_String == value.GetUnit()) {
+      nsAutoString tmp;
+      value.GetStringValue(tmp);
+      // XXX This list is copied in the frame code; add style consts
+      // for it?  or use the display enums?
+      if (tmp.EqualsIgnoreCase("line") ||
+          tmp.EqualsIgnoreCase("vert") ||
+          tmp.EqualsIgnoreCase("vertical")) {
+        // This is not strictly 100% compatible: if the spacer is given
+        // a width of zero then it is basically ignored.
+        display->mDisplay = NS_STYLE_DISPLAY_BLOCK;
+      }
+    }
+  }
+  return mInner.MapAttributesInto(aContext, aPresContext);
+}
+
+NS_IMETHODIMP
+nsHTMLSpacerElement::HandleDOMEvent(nsIPresContext& aPresContext,
+                                    nsEvent* aEvent,
+                                    nsIDOMEvent** aDOMEvent,
+                                    PRUint32 aFlags,
+                                    nsEventStatus& aEventStatus)
+{
+  return mInner.HandleDOMEvent(aPresContext, aEvent, aDOMEvent,
+                               aFlags, aEventStatus);
+}
