69443229640b58a649dc3f4858f3f0c5208fb19b
mjudge%netscape.com <mjudge%netscape.com>
1998-09-10 22:25:05 +0000

    IDIOT!! sorry. added 2 of 4 files

---
+++ cmd/winfe/enderwrp.h
@@ -0,0 +1,32 @@
+#ifndef ENDERWRP_H
+#define ENDERWRP_H
+#include "lo_ele.h"
+
+class CNetscapeEditView;
+class CEditToolBarController;
+
+class CEnderView: public CView
+{
+public:
+    CEnderView(CAbstractCX *p_pCx);
+    CEnderView(){};
+    ~CEnderView();
+    BOOL Create(CWnd *pParent, lo_FormElementTextareaData *pData, LO_TextAttr *pTextAttr);
+    virtual void OnDraw(CDC *pDC);
+    CNetscapeEditView *GetEditView(){return m_pEditView;}
+protected:
+    DECLARE_DYNCREATE(CEnderView)
+    //{{AFX_MSG(CEnderView)
+    afx_msg void OnDestroy();
+    afx_msg LONG OnToolController(UINT,LONG);
+	//}}AFX_MSG
+private:
+    CNetscapeEditView *m_pEditView;
+    CEditToolBarController *m_pToolBarController;
+    CAbstractCX *m_pCX;
+    DECLARE_MESSAGE_MAP()
+};
+
+
+#endif //ENDERWRP_H
+
