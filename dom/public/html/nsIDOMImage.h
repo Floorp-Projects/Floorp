de174c314b0be7c713d4a7f059c908d1a508aab5
vidur%netscape.com <vidur%netscape.com>
1998-09-11 18:31:38 +0000

    Added stopgap code for aliased constructors

---
+++ dom/public/html/nsIDOMImage.h
@@ -0,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+/* AUTO-GENERATED. DO NOT EDIT!!! */
+
+#ifndef nsIDOMImage_h__
+#define nsIDOMImage_h__
+
+#include "nsISupports.h"
+#include "nsString.h"
+#include "nsIScriptContext.h"
+
+
+#define NS_IDOMIMAGE_IID \
+{ 0x6f765329,  0xee43, 0x11d1, \
+ { 0x9b, 0xc3, 0x00, 0x60, 0x08, 0x8c, 0xa6, 0xb3 } } 
+
+class nsIDOMImage : public nsISupports {
+public:
+
+  NS_IMETHOD    Image()=0;
+};
+
+
+#define NS_DECL_IDOMIMAGE   \
+  NS_IMETHOD    Image();  \
+
+
+
+#define NS_FORWARD_IDOMIMAGE(_to)  \
+  NS_IMETHOD    Image() { return _to##Image(); }  \
+
+
+#endif // nsIDOMImage_h__
