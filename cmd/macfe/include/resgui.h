40b4fddc9ae044bdf246a079c55be7e6dfd71c6b
clu%netscape.com <clu%netscape.com>
1998-09-11 22:19:36 +0000

    #123601 - added resource number 1522 for CPageTitle.

--- cmd/macfe/include/resgui.h
+++ cmd/macfe/include/resgui.h
@@ -836,6 +836,7 @@
 #define EDITDLG_FILE_MODIFIED			5119
 #define EDITDLG_AUTOSAVE				5120
 #define EDITDLG_BROWSE_PUBLISHED		5121
+#define EDITDLG_PAGE_TITLE				5122
 
 // These are Tabbed dialog boxes used for the editor
 // Each dialog box below has a Tab with several views
