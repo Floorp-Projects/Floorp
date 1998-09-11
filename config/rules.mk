b36c0d0a3057d5df53897e8d314588f7533bd239
cls@seawood.org <cls@seawood.org>
1998-09-11 01:10:08 +0000

    Resurrected tweak_nspr kludge for --enable-nspr

--- config/rules.mk
+++ config/rules.mk
@@ -258,9 +258,6 @@ endif
 
 all:: export libs install
 
-ifdef USE_AUTOCONF
-tweak_nspr:
-else
 #
 # Maybe this should be done in config/Makefile so it only happens once...?
 #
@@ -272,6 +269,14 @@ TARGETS			+= tweak_nspr
 # Since the NSPR folks won't help, we'll fix things the sneaky way.
 #
 tweak_nspr:
+ifdef USE_AUTOCONF
+ifdef NSPR_AUTOCONF
+	@(cp $(topsrcdir)/nsprpub/config/UNIX.mk $(DEPTH)/nsprpub/config/UNIX.mk.orig; \
+		cd $(DEPTH)/nsprpub/config; \
+		awk '/^OBJDIR_NAME[ 	]*=/ { \
+			printf("OBJDIR_NAME\t= %s%s%s%s%s%s.OBJ\n","$(OS_CONFIG)","$(OS_VERSION)","$(PROCESSOR_ARCHITECTURE)","$(COMPILER)","$(IMPL_STRATEGY)","$(OBJDIR_TAG)"); next} {print}' UNIX.mk.orig > UNIX.mk)
+endif # NSPR_AUTOCONF
+else
 	@(cd $(DEPTH)/nsprpub/config; \
 		if test -f UNIX.mk.orig; then rm -f UNIX.mk; mv UNIX.mk.orig UNIX.mk; fi; \
 		mv UNIX.mk UNIX.mk.orig; \
