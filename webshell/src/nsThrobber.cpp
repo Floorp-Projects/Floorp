000bfe4efc2d64b211161624d1c7a53a5a289bdd
pierre%netscape.com <pierre%netscape.com>
1998-09-11 10:45:26 +0000

    fix crash when GetFontMetrics() returns nil, which should never happen of course

--- webshell/src/nsThrobber.cpp
+++ webshell/src/nsThrobber.cpp
@@ -238,10 +238,13 @@ HandleThrobberEvent(nsGUIEvent *aEvent)
         cx->SetColor(NS_RGB(255, 255, 255));
         cx->SetFont(tfont);
         met = cx->GetFontMetrics();
-        met->GetWidth(str, w);
-        met->GetHeight(h);
-        cx->DrawString(str, 2, (bounds.width - w) >> 1, (bounds.height - h) >> 1, 0);
-        NS_RELEASE(met);
+        if (nsnull != met)
+        {
+          met->GetWidth(str, w);
+          met->GetHeight(h);
+          cx->DrawString(str, 2, (bounds.width - w) >> 1, (bounds.height - h) >> 1, 0);
+          NS_RELEASE(met);
+        }
       }
       else
       {
