6a6d22d890164e83faecb20434b79c10fffcc09b
wtc@netscape.com <wtc@netscape.com>
1998-09-14 18:09:06 +0000

    For Strong Arm 110 (sa110), set the OS_ARCH to "arm".

--- config/Linux.mk
+++ config/Linux.mk
@@ -27,7 +27,11 @@ ARCH			:= linux
 ifeq (86,$(findstring 86,$(OS_TEST)))
 CPU_ARCH		:= x86
 else
+ifeq (,$(filter-out sa110,$(OS_TEST)))
+CPU_ARCH		:= arm
+else
 CPU_ARCH		:= $(OS_TEST)
+endif
 endif
 GFX_ARCH		:= x
 
