4d4a35797dbfb27d999cd09e067fe5b3a6c3a0b0
spider%netscape.com <spider%netscape.com>
1998-09-15 20:48:22 +0000

    New files for user and core cal canvas implementations

---
+++ calendar/modules/core/public/nsICalendarUser.h
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+#ifndef nsICalendarUser_h___
+#define nsICalendarUser_h___
+
+#include "nsISupports.h"
+
+//2fa12310-4cd8-11d2-924a-00805f8a7ab6
+#define NS_ICALENDAR_USER_IID   \
+{ 0x2fa12310, 0x4cd8, 0x11d2,    \
+{ 0x92, 0x4a, 0x00, 0x80, 0x5f, 0x8a, 0x7a, 0xb6 } }
+
+class nsICalendarUser : public nsISupports
+{
+
+public:
+  NS_IMETHOD Init() = 0;
+
+};
+
+
+#endif /* nsICalendarUser */
---
+++ calendar/modules/ui/canvas/src/nsCalCanvas.cpp
@@ -0,0 +1,61 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "nsCalCanvas.h"
+#include "nsCalUICIID.h"
+
+static NS_DEFINE_IID(kISupportsIID, NS_ISUPPORTS_IID);
+static NS_DEFINE_IID(kCalCanvasCID, NS_CAL_CANVAS_CID);
+
+nsCalCanvas :: nsCalCanvas(nsISupports* outer) : nsXPFCCanvas(outer)
+{
+  NS_INIT_REFCNT();
+}
+
+nsCalCanvas :: ~nsCalCanvas()
+{
+}
+
+nsresult nsCalCanvas::QueryInterface(REFNSIID aIID, void** aInstancePtr)      
+{                                                                        
+  if (NULL == aInstancePtr) {                                            
+    return NS_ERROR_NULL_POINTER;                                        
+  }                                                                      
+  static NS_DEFINE_IID(kISupportsIID, NS_ISUPPORTS_IID);                 
+  static NS_DEFINE_IID(kClassIID, kCalCanvasCID); 
+  if (aIID.Equals(kClassIID)) {                                          
+    *aInstancePtr = (void*) (nsCalCanvas *)this;                                        
+    AddRef();                                                            
+    return NS_OK;                                                        
+  }                                                                      
+  if (aIID.Equals(kISupportsIID)) {                                      
+    *aInstancePtr = (void*) (this);                        
+    AddRef();                                                            
+    return NS_OK;                                                        
+  }                                                                      
+  return (nsXPFCCanvas::QueryInterface(aIID, aInstancePtr));
+}
+
+NS_IMPL_ADDREF(nsCalCanvas)
+NS_IMPL_RELEASE(nsCalCanvas)
+
+nsresult nsCalCanvas :: Init()
+{
+  return NS_OK;    
+}
+
