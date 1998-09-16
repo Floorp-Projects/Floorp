7d65711475705fec77ec5dd47ca3a7276bae03da
rods@netscape.com <rods@netscape.com>
1998-09-16 17:31:02 +0000

    Changed to new interfaces

--- widget/src/motif/nsListBox.h
+++ widget/src/motif/nsListBox.h
@@ -26,15 +26,20 @@
  * Native Motif Listbox wrapper
  */
 
-class nsListBox :   public nsWindow
+class nsListBox :   public nsWindow,
+                    public nsIListWidget,
+                    public nsIListBox
 {
 
 public:
-    nsListBox(nsISupports *aOuter);
-    virtual ~nsListBox();
+  nsListBox();
+  virtual ~nsListBox();
 
-    // nsISupports. Forward to the nsObject base class
-    NS_IMETHOD QueryObject(const nsIID& aIID, void** aInstancePtr);
+  // nsISupports
+  NS_IMETHOD_(nsrefcnt) AddRef();
+  NS_IMETHOD_(nsrefcnt) Release();
+  NS_IMETHOD QueryInterface(const nsIID& aIID, void** aInstancePtr);
+
 
     void Create(nsIWidget *aParent,
               const nsRect &aRect,
@@ -53,7 +58,7 @@ public:
               nsWidgetInitData *aInitData = nsnull);
 
 
-    virtual PRBool    OnMove(PRInt32 aX, PRInt32 aY);
+    virtual PRBool OnMove(PRInt32 aX, PRInt32 aY);
     virtual PRBool OnPaint(nsPaintEvent & aEvent);
     virtual PRBool OnResize(nsSizeEvent &aEvent);
 
@@ -75,43 +80,6 @@ public:
 
 protected:
     PRBool  mMultiSelect;
-
-private:
-
-  // this should not be public
-  static PRInt32 GetOuterOffset() {
-    return offsetof(nsListBox,mAggWidget);
-  }
-
-
-  // Aggregator class and instance variable used to aggregate in the
-  // nsIListBox interface to nsListBox w/o using multiple
-  // inheritance.
-  class AggListBox : public nsIListBox {
-  public:
-    AggListBox();
-    virtual ~AggListBox();
-
-    AGGREGATE_METHOD_DEF
-
-    // nsIListBox
-    void      SetMultipleSelection(PRBool aMultipleSelections);
-    void      AddItemAt(nsString &aItem, PRInt32 aPosition);
-    PRInt32   FindItem(nsString &aItem, PRInt32 aStartPos);
-    PRInt32   GetItemCount();
-    PRBool    RemoveItemAt(PRInt32 aPosition);
-    PRBool    GetItemAt(nsString& anItem, PRInt32 aPosition);
-    void      GetSelectedItem(nsString& aItem);
-    PRInt32   GetSelectedIndex();
-    PRInt32   GetSelectedCount();
-    void      GetSelectedIndices(PRInt32 aIndices[], PRInt32 aSize);
-    void      SetSelectedIndices(PRInt32 aIndices[], PRInt32 aSize);
-    void      SelectItem(PRInt32 aPosition);
-    void      Deselect() ;
-
-  };
-  AggListBox mAggWidget;
-  friend class AggListBox;
 
 };
 
