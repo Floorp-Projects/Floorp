b5c6f03d0cbfe616babd6fc792485ca390381eda
briano%netscape.com <briano%netscape.com>
1998-09-18 02:16:44 +0000

    Got rid of the pointless LINUXy_z macros (again), and made some minor tweaks for ARM systems (NetWinder).

--- config/Linux.mk
+++ config/Linux.mk
@@ -39,21 +39,23 @@ GFX_ARCH		:= x
 # should contain lots of magic paths and flags.
 -include $(MOZILLA_DETECT_GEN)
 
-OS_INCLUDES		=\
-			$(MOZILLA_XFE_MOTIF_INCLUDE_FLAGS) \
-			$(MOZILLA_XFE_X11_INCLUDE_FLAGS)
-
+OS_INCLUDES		= $(MOZILLA_XFE_MOTIF_INCLUDE_FLAGS) $(MOZILLA_XFE_X11_INCLUDE_FLAGS)
 G++INCLUDES		= -I/usr/include/g++
-
 LOC_LIB_DIR		= /usr/lib/X11
 MOTIF			=
 MOTIFLIB		=
 OS_LIBS			= -L/lib -ldl -lc
 
-PLATFORM_FLAGS		= -ansi -Wall -pipe $(DSO_CFLAGS) -DLINUX -Dlinux -DLINUX1_2
+PLATFORM_FLAGS		= -ansi -Wall $(DASH_PIPE) $(DSO_CFLAGS) -DLINUX -Dlinux
 MOVEMAIL_FLAGS		= -DHAVE_STRERROR
 PORT_FLAGS		= -D_POSIX_SOURCE -D_BSD_SOURCE -DNEED_ENDIAN_H -DNEED_GETOPT_H -DNEED_IOCTL_H -DUSE_NODL_TABS -DHAVE_SIGNED_CHAR -DNEED_SYS_TIME_H -DHAVE_SYS_BITYPES_H -DNEED_UINT_T -DHAVE_SNPRINTF
 PDJAVA_FLAGS		= -mx128m
+
+#
+# Some systems can't handle -pipe.  They can override DASH_PIPE in
+# the version-specific section below.
+#
+DASH_PIPE		= -pipe
 
 ifdef USE_AUTOCONF
 OS_CFLAGS		= #-include $(DEPTH)/include/config.h
@@ -79,6 +81,9 @@ I2_LOCALE		= i2
 ifeq ($(CPU_ARCH),alpha)
 PLATFORM_FLAGS		+= -D__$(CPU_ARCH) -D_ALPHA_ -mieee
 PORT_FLAGS		+= -DNEED_TIME_R -DMITSHM -D_XOPEN_SOURCE
+endif
+ifeq ($(CPU_ARCH),arm)
+DASH_PIPE		=
 endif
 ifeq ($(CPU_ARCH),m68k)
 PLATFORM_FLAGS		+= -m68020-40 -D$(CPU_ARCH)
@@ -110,19 +115,12 @@ endif
 # Linux 2.x
 ifneq (,$(filter 2.0 2.1 2.2,$(OS_RELEASE)))
 PORT_FLAGS		+= -DNO_INT64_T
-PLATFORM_FLAGS		+= -DLINUX2_0
 BUILD_UNIX_PLUGINS	= 1
 MKSHLIB			= $(CC) -shared -Wl,-soname -Wl,$(@:$(OBJDIR)/%.so=%.so)
 DSO_BIND_REFERENCES	= -Wl,-Bsymbolic
 ifdef BUILD_OPT
 OPTIMIZER		= -O2
 endif
-endif
-
-# Linux 2.2 should not be any differnt than 2.0 as far as mozilla is
-# concerned.  Defined the LINUX2_2 macro just in case.
-ifneq (,$(filter 2.1 2.2,$(OS_RELEASE)))
-PLATFORM_FLAGS		+= -DLINUX2_2
 endif
 
 ######################################################################
@@ -162,4 +160,4 @@ JAVA_HOME		= /usr/local/java
 JAVAC_ZIP		= $(JAVA_HOME)/lib/classes.zip
 endif 
 
-ASFLAGS                 += -x assembler-with-cpp
+ASFLAGS			+= -x assembler-with-cpp
