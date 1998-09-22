c4c6d137c4a13dbae9cddc26705855654ec34047
beard@netscape.com <beard@netscape.com>
1998-09-22 02:29:36 +0000

    Turned off registering the native methods that are now installed by the plugin.

--- js/src/liveconnect/jsj.c
+++ js/src/liveconnect/jsj.c
@@ -276,6 +276,8 @@ static JSObject_RegisterNativeMethods(JNIEnv* jEnv)
     // Manually load the required native methods.
     static JNINativeMethod nativeMethods[] = {
         "initClass", "()V", (void*)&Java_netscape_javascript_JSObject_initClass,
+
+#if 0        
         "getMember", "(Ljava/lang/String;)Ljava/lang/Object;", (void*)&Java_netscape_javascript_JSObject_getMember,
         "getSlot", "(I)Ljava/lang/Object;", (void*)&Java_netscape_javascript_JSObject_getSlot,
         "setMember", "(Ljava/lang/String;Ljava/lang/Object;)V", (void*)&Java_netscape_javascript_JSObject_setMember,
@@ -283,11 +285,15 @@ static JSObject_RegisterNativeMethods(JNIEnv* jEnv)
         "removeMember", "(Ljava/lang/String;)V", (void*)&Java_netscape_javascript_JSObject_removeMember,
         "call", "(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/Object;", (void*)&Java_netscape_javascript_JSObject_call,
         "eval", "(Ljava/lang/String;)Ljava/lang/Object;", (void*)&Java_netscape_javascript_JSObject_eval,
-        
+#endif
+
         "toString", "()Ljava/lang/String;", (void*)&Java_netscape_javascript_JSObject_toString,
+
+#if 0        
         "getWindow", "(Ljava/applet/Applet;)Lnetscape/javascript/JSObject;", (void*)&Java_netscape_javascript_JSObject_getWindow,
         "finalize", "()V", (void*)&Java_netscape_javascript_JSObject_finalize,
         /* "equals", "(Ljava/lang/Object;)Z", (void*)&Java_netscape_javascript_JSObject_equals */
+#endif
     };
     (*jEnv)->RegisterNatives(jEnv, njJSObject, nativeMethods, sizeof(nativeMethods) / sizeof(JNINativeMethod));
     if ((*jEnv)->ExceptionOccurred(jEnv)) {
