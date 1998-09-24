fbceedcd4db35d33afc5e6032e947b6feecb8908
troy@netscape.com <troy@netscape.com>
1998-09-24 03:49:45 +0000

    Removed some XXX CONSTRUCTION code that was work-in-progress, but is no longer needed

--- layout/generic/nsPlaceholderFrame.h
+++ layout/generic/nsPlaceholderFrame.h
@@ -50,14 +50,6 @@ public:
                    nsIRenderingContext& aRenderingContext,
                    const nsRect& aDirtyRect);
 
-// XXX CONSTRUCTION
-#if 0
-  NS_IMETHOD  ContentInserted(nsIPresShell*   aShell,
-                              nsIPresContext* aPresContext,
-                              nsIContent*     aContainer,
-                              nsIContent*     aChild,
-                              PRInt32         aIndexInParent);
-#endif
   NS_IMETHOD  ContentReplaced(nsIPresShell*   aShell,
                               nsIPresContext* aPresContext,
                               nsIContent*     aContainer,
