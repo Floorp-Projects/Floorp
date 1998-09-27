444e1022636587715add45de7673470b3ef95254
michaelp%netscape.com <michaelp%netscape.com>
1998-09-27 22:24:54 +0000

    added support for plugintaginfo2 and jvmtaginfo.

--- modules/plugin/base/src/nsPluginInstancePeer.cpp
+++ modules/plugin/base/src/nsPluginInstancePeer.cpp
@@ -48,7 +48,9 @@ NS_IMPL_ADDREF(nsPluginInstancePeerImpl);
 NS_IMPL_RELEASE(nsPluginInstancePeerImpl);
 
 static NS_DEFINE_IID(kIPluginTagInfoIID, NS_IPLUGINTAGINFO_IID); 
+static NS_DEFINE_IID(kIPluginTagInfo2IID, NS_IPLUGINTAGINFO2_IID); 
 static NS_DEFINE_IID(kIPluginInstancePeerIID, NS_IPLUGININSTANCEPEER_IID); 
+static NS_DEFINE_IID(kIJVMPluginTagInfoIID, NS_IJVMPLUGINTAGINFO_IID); 
 static NS_DEFINE_IID(kISupportsIID, NS_ISUPPORTS_IID);
 
 nsresult nsPluginInstancePeerImpl :: QueryInterface(const nsIID& iid, void** instance)
@@ -66,6 +68,20 @@ nsresult nsPluginInstancePeerImpl :: QueryInterface(const nsIID& iid, void** ins
     if (iid.Equals(kIPluginTagInfoIID))
     {
         *instance = (void *)(nsIPluginTagInfo *)this;
+        AddRef();
+        return NS_OK;
+    }
+
+    if (iid.Equals(kIPluginTagInfo2IID))
+    {
+        *instance = (void *)(nsIPluginTagInfo2 *)this;
+        AddRef();
+        return NS_OK;
+    }
+
+    if (iid.Equals(kIJVMPluginTagInfoIID))
+    {
+        *instance = (void *)(nsIJVMPluginTagInfo *)this;
         AddRef();
         return NS_OK;
     }
@@ -112,19 +128,35 @@ printf("instance peer newstream called\n");
 
 NS_IMETHODIMP nsPluginInstancePeerImpl :: ShowStatus(const char* message)
 {
-printf("instance peer showstatus called\n");
-  return NS_OK;
+  if (nsnull != mOwner)
+    return mOwner->ShowStatus(message);
+  else
+    return NS_ERROR_FAILURE;
 }
 
 NS_IMETHODIMP nsPluginInstancePeerImpl :: GetAttributes(PRUint16& n, const char*const*& names, const char*const*& values)
 {
   if (nsnull != mOwner)
-    return mOwner->GetAttributes(n, names, values);
+  {
+    nsIPluginTagInfo  *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfoIID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetAttributes(n, names, values);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
   else
   {
     n = 0;
     names = nsnull;
     values = nsnull;
+
     return NS_ERROR_FAILURE;
   }
 }
@@ -132,10 +164,434 @@ NS_IMETHODIMP nsPluginInstancePeerImpl :: GetAttributes(PRUint16& n, const char*
 NS_IMETHODIMP nsPluginInstancePeerImpl :: GetAttribute(const char* name, const char* *result)
 {
   if (nsnull != mOwner)
-    return mOwner->GetAttribute(name, result);
+  {
+    nsIPluginTagInfo  *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfoIID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetAttribute(name, result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
   else
   {
     *result = "";
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetTagType(nsPluginTagType *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetTagType(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = nsPluginTagType_Unknown;
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetTagText(const char* *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetTagText(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = "";
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetParameters(PRUint16& n, const char*const*& names, const char*const*& values)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetParameters(n, names, values);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    n = 0;
+    names = nsnull;
+    values = nsnull;
+
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetParameter(const char* name, const char* *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetParameter(name, result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = "";
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetDocumentBase(const char* *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetDocumentBase(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = "";
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetDocumentEncoding(const char* *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetDocumentEncoding(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = "";
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetAlignment(const char* *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetAlignment(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = "";
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetWidth(PRUint32 *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetWidth(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = 0;
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetHeight(PRUint32 *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetHeight(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = 0;
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetBorderVertSpace(PRUint32 *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetBorderVertSpace(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = 0;
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetBorderHorizSpace(PRUint32 *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetBorderHorizSpace(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = 0;
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetUniqueID(PRUint32 *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIPluginTagInfo2 *tinfo;
+    nsresult          rv;
+
+    rv = mOwner->QueryInterface(kIPluginTagInfo2IID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetUniqueID(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = 0;
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetCode(const char* *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIJVMPluginTagInfo *tinfo;
+    nsresult            rv;
+
+    rv = mOwner->QueryInterface(kIJVMPluginTagInfoIID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetCode(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = 0;
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetCodeBase(const char* *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIJVMPluginTagInfo *tinfo;
+    nsresult            rv;
+
+    rv = mOwner->QueryInterface(kIJVMPluginTagInfoIID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetCodeBase(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = 0;
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetArchive(const char* *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIJVMPluginTagInfo *tinfo;
+    nsresult            rv;
+
+    rv = mOwner->QueryInterface(kIJVMPluginTagInfoIID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetArchive(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = 0;
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetName(const char* *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIJVMPluginTagInfo *tinfo;
+    nsresult            rv;
+
+    rv = mOwner->QueryInterface(kIJVMPluginTagInfoIID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetName(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = 0;
+    return NS_ERROR_FAILURE;
+  }
+}
+
+NS_IMETHODIMP nsPluginInstancePeerImpl :: GetMayScript(PRBool *result)
+{
+  if (nsnull != mOwner)
+  {
+    nsIJVMPluginTagInfo *tinfo;
+    nsresult            rv;
+
+    rv = mOwner->QueryInterface(kIJVMPluginTagInfoIID, (void **)&tinfo);
+
+    if (NS_OK == rv) 
+    {
+      rv = tinfo->GetMayScript(result);
+      NS_RELEASE(tinfo);
+    }
+
+    return rv;
+  }
+  else
+  {
+    *result = 0;
     return NS_ERROR_FAILURE;
   }
 }
