1f074267724ea4762a317931ca32fb93817fb055
rpotts@netscape.com <rpotts@netscape.com>
1998-10-01 21:46:24 +0000

    Pass a null nsIURLGroup into the ImageGroup initialize method...

--- gfx/tests/btest/BitTest.cpp
+++ gfx/tests/btest/BitTest.cpp
@@ -850,7 +850,7 @@ char *str;
     if (gImageGroup == NULL) 
       {
       nsIDeviceContext *deviceCtx = gWindow->GetDeviceContext();
-      if (NS_NewImageGroup(&gImageGroup) != NS_OK || gImageGroup->Init(deviceCtx) != NS_OK) 
+      if (NS_NewImageGroup(&gImageGroup) != NS_OK || gImageGroup->Init(deviceCtx, nsnull) != NS_OK) 
         {
         ::MessageBox(NULL, "Couldn't create image group",class1Name, MB_OK);
         NS_RELEASE(deviceCtx);
