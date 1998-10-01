91a2b19cceee7c656770437f430e54c6ecb846ad
warren%netscape.com <warren%netscape.com>
1998-10-01 03:01:39 +0000

    Removed unnecessary field for new networking stuff.

--- modules/plugin/src/nppriv.h
+++ modules/plugin/src/nppriv.h
@@ -141,9 +141,6 @@ struct _np_urlsnode {
     void* notifyData;
     XP_Bool cached;
     XP_Bool notify;
-#ifdef NEW_PLUGIN_STREAM_API
-    uint16 streamType;
-#endif
 };
 
 /* MWContext.pluginReconnect -> np_reconnect */
