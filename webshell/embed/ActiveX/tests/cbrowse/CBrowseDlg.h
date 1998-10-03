80ee698ecce321cad3e6ce9bbca23e656642f1cc
locka%iol.ie <locka%iol.ie>
1998-10-03 23:14:15 +0000

    *** empty log message ***

---
+++ webshell/embed/ActiveX/tests/cbrowse/CBrowseDlg.h
@@ -0,0 +1,50 @@
+// CBrowseDlg.h : header file
+//
+
+#if !defined(AFX_CBROWSEDLG_H__5121F5E6_5324_11D2_93E1_000000000000__INCLUDED_)
+#define AFX_CBROWSEDLG_H__5121F5E6_5324_11D2_93E1_000000000000__INCLUDED_
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+
+/////////////////////////////////////////////////////////////////////////////
+// CBrowseDlg dialog
+
+class CBrowseDlg : public CDialog
+{
+// Construction
+public:
+	CWnd *m_pWndBrowser;
+	CBrowseDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CBrowseDlg)
+	enum { IDD = IDD_CBROWSE_DIALOG };
+	CString	m_sURL;
+	//}}AFX_DATA
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CBrowseDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	//{{AFX_MSG(CBrowseDlg)
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg void OnGo();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Developer Studio will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_CBROWSEDLG_H__5121F5E6_5324_11D2_93E1_000000000000__INCLUDED_)
