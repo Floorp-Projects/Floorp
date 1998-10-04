a0cdac813b9bea227bf4f5fa29b79bb574d357e3
locka%iol.ie <locka%iol.ie>
1998-10-04 14:36:06 +0000

    Improved diagnostic macros

--- webshell/embed/ActiveX/BrowserDiagnostics.h
+++ webshell/embed/ActiveX/BrowserDiagnostics.h
@@ -2,25 +2,22 @@
 #define BROWSER_DIAGNOSTICS_H
 
 #ifndef _DEBUG
-#define NG_ASSERT(X)
-#define NG_ASSERT_POINTER(p, type) \
+#  include <assert.h>
+#  define NG_ASSERT(expr) assert(expr)
+#  define NG_ASSERT_POINTER(p, type) \
 	NG_ASSERT(((p) != NULL) && NgIsValidAddress((p), sizeof(type), FALSE))
-
-#define NG_ASSERT_NULL_OR_POINTER(p, type) \
+#  define NG_ASSERT_NULL_OR_POINTER(p, type) \
 	NG_ASSERT(((p) == NULL) || NgIsValidAddress((p), sizeof(type), FALSE))
 #else
-#define NG_ASSERT(X)
-#define NG_ASSERT_POINTER(p, type)
-#define NG_ASSERT_NULL_OR_POINTER(p, type)
+#  define NG_ASSERT(X)
+#  define NG_ASSERT_POINTER(p, type)
+#  define NG_ASSERT_NULL_OR_POINTER(p, type)
 #endif
 
 inline BOOL NgIsValidAddress(const void* lp, UINT nBytes, BOOL bReadWrite = TRUE)
 {
-	if (lp == NULL)
-	{
-		return FALSE;
-	}
-	return TRUE;
+	return (lp != NULL && !IsBadReadPtr(lp, nBytes) &&
+		(!bReadWrite || !IsBadWritePtr((LPVOID)lp, nBytes)));
 }
 
 #endif
