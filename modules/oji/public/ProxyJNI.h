eb203966cc3235db204918a55e951d8ab50fab08
beard@netscape.com <beard@netscape.com>
1998-10-13 21:14:53 +0000

    First Checked In.

---
+++ modules/oji/public/ProxyJNI.h
@@ -0,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/*
+	ProxyJNI.h
+ */
+
+#ifndef PROXY_JNI_H
+#define PROXY_JNI_H
+
+#ifndef JNI_H
+#include <jni.h>
+#endif
+
+class nsIJVMPlugin;
+
+/**
+ * Creates a proxy JNIEnv using the given JVM plugin.
+ */
+JNIEnv* CreateProxyJNI(nsIJVMPlugin* jvmPlugin);
+
+/**
+ * Deletes the proxy JNIEnv. Releases the connection
+ * to the underlying JVM.
+ */
+void DeleteProxyJNI(JNIEnv* env);
+
+#endif /* PROXY_JNI_H */
