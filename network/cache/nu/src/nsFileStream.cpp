8eb4d30a1b0489652761788c105bab758ad7213f
gagan%netscape.com <gagan%netscape.com>
1998-10-16 01:29:26 +0000

    NuCache Streams now hold the filename for smoother reopening. Not in build as yet.

--- network/cache/nu/src/nsFileStream.cpp
+++ network/cache/nu/src/nsFileStream.cpp
@@ -18,16 +18,27 @@
  */
 
 #include "nsFileStream.h"
+#include "plstr.h"
 #include "prlog.h"
 
-nsFileStream::nsFileStream(PRFileDesc* i_pFile):m_pFile(i_pFile)
+nsFileStream::nsFileStream(const char* i_Filename):m_pFile(0),m_pFilename(0)
 {
-    PR_ASSERT(m_pFile);
+    PR_ASSERT(i_Filename);
+    if (i_Filename)
+    {
+        m_pFilename = new char[PL_strlen(i_Filename)+1];
+        PL_strcpy(m_pFilename, i_Filename);
+        Open();
+    }
 }
 
 nsFileStream::~nsFileStream()
 {
-    //close the file if not closed. todo
+    if (m_pFilename)
+    {
+        delete[] m_pFilename;
+        m_pFilename = 0;
+    }
     if (m_pFile)
         PR_Close(m_pFile);
 }
@@ -54,27 +65,40 @@ nsresult nsFileStream::QueryInterface(const nsIID& aIID,
 }
 */
 
-PRInt32 nsFileStream::Read(void* o_Buffer, PRUint32 i_Len)
+PRBool nsFileStream::Open()
 {
     if (m_pFile)
+        PR_Close(m_pFile);
+    PR_ASSERT(m_pFilename);
+    if (m_pFilename)
     {
-        return PR_Read(m_pFile, o_Buffer, i_Len);
+        m_pFile = PR_Open(
+                m_pFilename, 
+                PR_CREATE_FILE | PR_RDWR,
+                600);// Read and write by owner only
+
+        PR_ASSERT(m_pFile);
+        if (m_pFile)
+            return PR_TRUE;
     }
-    return 0;
+    return PR_FALSE;
+}
+
+PRInt32 nsFileStream::Read(void* o_Buffer, PRUint32 i_Len)
+{
+    if (!m_pFile && !Open())
+        return 0;
+    return PR_Read(m_pFile, o_Buffer, i_Len);
 }
 
 void nsFileStream::Reset()
 {
-	return;
-    if (m_pFile)
-        PR_Close(m_pFile);
+    Open();
 }
 
 PRInt32 nsFileStream::Write(const void* i_Buffer, PRUint32 i_Len)
 {
-    if (m_pFile)
-    {
-        return PR_Write(m_pFile, i_Buffer, i_Len);
-    }
-    return 0;
+    if (!m_pFile && !Open())
+        return 0;
+    return PR_Write(m_pFile, i_Buffer, i_Len);
 }
