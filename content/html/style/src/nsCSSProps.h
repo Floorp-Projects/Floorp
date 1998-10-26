4006485933ff2056b68473bc0f1d8a6f5598a5c7
peterl@netscape.com <peterl@netscape.com>
1998-10-26 23:16:27 +0000

    added CSS2 properties

--- content/html/style/src/nsCSSProps.h
+++ content/html/style/src/nsCSSProps.h
@@ -43,6 +43,56 @@ public:
   static const NameTableEntry kNameTable[];
 
   static const PRInt32  kHintTable[];
+
+  // Keyword/Enum value tables
+  static const PRInt32 kAzimuthKTable[];
+  static const PRInt32 kBackgroundAttachmentKTable[];
+  static const PRInt32 kBackgroundColorKTable[];
+  static const PRInt32 kBackgroundRepeatKTable[];
+  static const PRInt32 kBorderCollapseKTable[];
+  static const PRInt32 kBorderColorKTable[];
+  static const PRInt32 kBorderStyleKTable[];
+  static const PRInt32 kBorderWidthKTable[];
+  static const PRInt32 kCaptionSideKTable[];
+  static const PRInt32 kClearKTable[];
+  static const PRInt32 kContentKTable[];
+  static const PRInt32 kCursorKTable[];
+  static const PRInt32 kDirectionKTable[];
+  static const PRInt32 kDisplayKTable[];
+  static const PRInt32 kElevationKTable[];
+  static const PRInt32 kEmptyCellsKTable[];
+  static const PRInt32 kFloatKTable[];
+  static const PRInt32 kFontKTable[];
+  static const PRInt32 kFontSizeKTable[];
+  static const PRInt32 kFontStretchKTable[];
+  static const PRInt32 kFontStyleKTable[];
+  static const PRInt32 kFontVariantKTable[];
+  static const PRInt32 kFontWeightKTable[];
+  static const PRInt32 kListStylePositionKTable[];
+  static const PRInt32 kListStyleKTable[];
+  static const PRInt32 kOutlineColorKTable[];
+  static const PRInt32 kOverflowKTable[];
+  static const PRInt32 kPageBreakKTable[];
+  static const PRInt32 kPageBreakInsideKTable[];
+  static const PRInt32 kPageMarksKTable[];
+  static const PRInt32 kPageSizeKTable[];
+  static const PRInt32 kPitchKTable[];
+  static const PRInt32 kPlayDuringKTable[];
+  static const PRInt32 kPositionKTable[];
+  static const PRInt32 kSpeakKTable[];
+  static const PRInt32 kSpeakHeaderKTable[];
+  static const PRInt32 kSpeakNumeralKTable[];
+  static const PRInt32 kSpeakPunctuationKTable[];
+  static const PRInt32 kSpeechRateKTable[];
+  static const PRInt32 kTableLayoutKTable[];
+  static const PRInt32 kTextAlignKTable[];
+  static const PRInt32 kTextDecorationKTable[];
+  static const PRInt32 kTextTransformKTable[];
+  static const PRInt32 kUnicodeBidiKTable[];
+  static const PRInt32 kVerticalAlignKTable[];
+  static const PRInt32 kVisibilityKTable[];
+  static const PRInt32 kVolumeKTable[];
+  static const PRInt32 kWhitespaceKTable[];
 };
 
 #endif /* nsCSSProps_h___ */
