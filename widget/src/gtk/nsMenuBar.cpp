f480fc68130f06e89b8515e42e41b4ac292183cf
pavlov%pavlov.net <pavlov%pavlov.net>
1998-10-31 17:20:45 +0000

    add menu files which do compile

---
+++ widget/src/gtk/nsMenuBar.cpp
@@ -0,0 +1,125 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include <gtk/gtk.h>
+
+#include "nsMenuBar.h"
+#include "nsIMenu.h"
+#include "nsIWidget.h"
+
+#include "nsString.h"
+#include "nsStringUtil.h"
+
+static NS_DEFINE_IID(kMenuBarIID, NS_IMENUBAR_IID);
+NS_IMPL_ISUPPORTS(nsMenuBar, kMenuBarIID)
+
+
+//-------------------------------------------------------------------------
+//
+// nsMenuBar constructor
+//
+//-------------------------------------------------------------------------
+nsMenuBar::nsMenuBar() : nsIMenuBar()
+{
+  NS_INIT_REFCNT();
+  mNumMenus = 0;
+  mMenu     = nsnull;
+  mParent   = nsnull;
+  mIsMenuBarAdded = PR_FALSE;
+}
+
+//-------------------------------------------------------------------------
+//
+// nsMenuBar destructor
+//
+//-------------------------------------------------------------------------
+nsMenuBar::~nsMenuBar()
+{
+  NS_IF_RELEASE(mParent);
+}
+
+//-------------------------------------------------------------------------
+//
+// Create the proper widget
+//
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuBar::Create(nsIWidget *aParent)
+{
+  mParent = aParent;
+  NS_ADDREF(mParent);
+  GtkWidget *parentWidget = (GtkWidget*)mParent->GetNativeData(NS_NATIVE_WIDGET);
+#if 0
+  GtkWidget *mainWindow = XtParent(parentWidget);
+#endif
+  mMenu = gtk_menu_bar_new();
+  return NS_OK;
+
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuBar::GetParent(nsIWidget *&aParent)
+{
+ // XXX: Shouldn't this do an addref here? or is this just internal
+  aParent = mParent;
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuBar::AddMenu(nsIMenu * aMenu)
+{
+  // XXX add to internal data structure
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuBar::GetMenuCount(PRUint32 &aCount)
+{
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuBar::GetMenuAt(const PRUint32 aCount, nsIMenu *& aMenu)
+{
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuBar::InsertMenuAt(const PRUint32 aCount, nsIMenu *& aMenu)
+{
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuBar::RemoveMenu(const PRUint32 aCount)
+{
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuBar::RemoveAll()
+{
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuBar::GetNativeData(void *& aData)
+{
+  aData = (void *)mMenu;
+  return NS_OK;
+}
+
---
+++ widget/src/gtk/nsMenuItem.cpp
@@ -0,0 +1,224 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include <gtk/gtk.h>
+
+#include "nsMenuItem.h"
+#include "nsIMenu.h"
+#include "nsIMenuBar.h"
+#include "nsIWidget.h"
+
+#include "nsStringUtil.h"
+#include "nsGtkEventHandler.h"
+
+#include "nsIPopUpMenu.h"
+
+static NS_DEFINE_IID(kIMenuIID,     NS_IMENU_IID);
+static NS_DEFINE_IID(kIMenuBarIID,  NS_IMENUBAR_IID);
+static NS_DEFINE_IID(kISupportsIID, NS_ISUPPORTS_IID);
+static NS_DEFINE_IID(kIPopUpMenuIID, NS_IPOPUPMENU_IID);
+static NS_DEFINE_IID(kIMenuItemIID, NS_IMENUITEM_IID);
+NS_IMPL_ISUPPORTS(nsMenuItem, kIMenuItemIID)
+
+
+//-------------------------------------------------------------------------
+//
+// nsMenuItem constructor
+//
+//-------------------------------------------------------------------------
+nsMenuItem::nsMenuItem() : nsIMenuItem()
+{
+  NS_INIT_REFCNT();
+  mMenu        = nsnull;
+  mMenuParent  = nsnull;
+  mPopUpParent = nsnull;
+  mTarget      = nsnull;
+}
+
+//-------------------------------------------------------------------------
+//
+// nsMenuItem destructor
+//
+//-------------------------------------------------------------------------
+nsMenuItem::~nsMenuItem()
+{
+  NS_IF_RELEASE(mMenuParent);
+  NS_IF_RELEASE(mPopUpParent);
+  NS_IF_RELEASE(mTarget);
+}
+
+//-------------------------------------------------------------------------
+void nsMenuItem::Create(nsIWidget      *aMBParent, 
+                        GtkWidget      *aParent, 
+                        const nsString &aLabel, 
+                        PRUint32        aCommand)
+{
+#if 0
+  mTarget  = aMBParent;
+  mCommand = aCommand;
+  mLabel   = aLabel;
+
+  if (NULL == aParent || nsnull == aMBParent) {
+    return;
+  }
+
+  mTarget = aMBParent;
+  char * nameStr = mLabel.ToNewCString();
+  Widget parentMenuHandle = GetNativeParent();
+  mMenu = XtVaCreateManagedWidget(nameStr, xmCascadeButtonGadgetClass,
+                                          parentMenuHandle,
+                                          NULL);
+  XtAddCallback(mMenu, XmNactivateCallback, nsXtWidget_Menu_Callback, 
+                (nsIMenuItem *)this);
+  delete[] nameStr;
+#endif
+}
+
+//-------------------------------------------------------------------------
+GtkWidget *nsMenuItem::GetNativeParent()
+{
+  void * voidData;
+  if (nsnull != mMenuParent) {
+    mMenuParent->GetNativeData(voidData);
+  } else if (nsnull != mPopUpParent) {
+    mPopUpParent->GetNativeData(voidData);
+  } else {
+    return NULL;
+  }
+  return (GtkWidget*)voidData;
+}
+
+
+//-------------------------------------------------------------------------
+nsIWidget * nsMenuItem::GetMenuBarParent(nsISupports * aParent)
+{
+  nsIWidget    * widget  = nsnull; // MenuBar's Parent
+  nsIMenu      * menu    = nsnull;
+  nsIMenuBar   * menuBar = nsnull;
+  nsIPopUpMenu * popup   = nsnull;
+  nsISupports  * parent  = aParent;
+
+  // Bump the ref count on the parent, since it gets released unconditionally..
+  NS_ADDREF(parent);
+  while (1) {
+    if (NS_OK == parent->QueryInterface(kIMenuIID,(void**)&menu)) {
+      NS_RELEASE(parent);
+      if (NS_OK != menu->GetParent(parent)) {
+        NS_RELEASE(menu);
+        return nsnull;
+      }
+      NS_RELEASE(menu);
+
+    } else if (NS_OK == parent->QueryInterface(kIPopUpMenuIID,(void**)&popup)) {
+      if (NS_OK != popup->GetParent(widget)) {
+        widget =  nsnull;
+      } 
+      NS_RELEASE(parent);
+      NS_RELEASE(popup);
+      return widget;
+
+    } else if (NS_OK == parent->QueryInterface(kIMenuBarIID,(void**)&menuBar)) {
+      if (NS_OK != menuBar->GetParent(widget)) {
+        widget =  nsnull;
+      } 
+      NS_RELEASE(parent);
+      NS_RELEASE(menuBar);
+      return widget;
+    } else {
+      NS_RELEASE(parent);
+      return nsnull;
+    }
+  }
+  return nsnull;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuItem::Create(nsIMenu        *aParent, 
+                             const nsString &aLabel, 
+                             PRUint32       aCommand)
+                            
+{
+#if 0
+  if (nsnull == aParent) {
+    return NS_ERROR_FAILURE;
+  }
+
+  mMenuParent = aParent;
+  NS_ADDREF(mMenuParent);
+
+  nsIWidget   * widget  = nsnull; // MenuBar's Parent
+  nsISupports * sups;
+  if (NS_OK == aParent->QueryInterface(kISupportsIID,(void**)&sups)) {
+    widget = GetMenuBarParent(sups);
+    NS_RELEASE(sups);
+  }
+
+  Create(widget, GetNativeParent(), aLabel, aCommand);
+  aParent->AddItem(this);
+#endif
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuItem::Create(nsIPopUpMenu   *aParent, 
+                             const nsString &aLabel,  
+                             PRUint32        aCommand)
+{
+#if 0
+  mPopUpParent = aParent;
+  NS_ADDREF(mPopUpParent);
+
+  nsIWidget * widget = nsnull;
+  if (NS_OK != aParent->GetParent(widget)) {
+    widget = nsnull;
+  }
+
+  Create(widget, GetNativeParent(), aLabel, aCommand);
+  aParent->AddItem(this);
+#endif
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuItem::GetLabel(nsString &aText)
+{
+  aText = mLabel;
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuItem::GetCommand(PRUint32 & aCommand)
+{
+  aCommand = mCommand;
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuItem::GetTarget(nsIWidget *& aTarget)
+{
+  aTarget = mTarget;
+  return NS_OK;
+}
+
+//-------------------------------------------------------------------------
+NS_METHOD nsMenuItem::GetNativeData(void *& aData)
+{
+  aData = (void *)mMenu;
+  return NS_OK;
+}
+
