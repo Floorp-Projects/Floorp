08ccae2b7d544ab0fda0e6526789ba867211a40f
pavlov%pavlov.net <pavlov%pavlov.net>
1998-10-31 17:54:41 +0000

    just about finish setting up all the files needed for gtk to compile

---
+++ widget/src/gtk/nsScrollbar.h
@@ -0,0 +1,86 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsScrollbar_h__
+#define nsScrollbar_h__
+
+#include "nsWindow.h"
+
+#include "nsIScrollbar.h"
+
+/**
+ * Native Motif scrollbar wrapper. 
+ */
+
+class nsScrollbar : public nsWindow,
+                    public nsIScrollbar
+{
+
+public:
+                            nsScrollbar(PRBool aIsVertical);
+    virtual                 ~nsScrollbar();
+
+    // nsISupports
+    NS_IMETHOD_(nsrefcnt) AddRef();
+    NS_IMETHOD_(nsrefcnt) Release();
+    NS_IMETHOD QueryInterface(const nsIID& aIID, void** aInstancePtr);
+
+    // nsIScrollBar implementation
+    NS_IMETHOD SetMaxRange(PRUint32 aEndRange);
+    NS_IMETHOD GetMaxRange(PRUint32& aMaxRange);
+    NS_IMETHOD SetPosition(PRUint32 aPos);
+    NS_IMETHOD GetPosition(PRUint32& aPos);
+    NS_IMETHOD SetThumbSize(PRUint32 aSize);
+    NS_IMETHOD GetThumbSize(PRUint32& aSize);
+    NS_IMETHOD SetLineIncrement(PRUint32 aSize);
+    NS_IMETHOD GetLineIncrement(PRUint32& aSize);
+    NS_IMETHOD SetParameters(PRUint32 aMaxRange, PRUint32 aThumbSize,
+                               PRUint32 aPosition, PRUint32 aLineIncrement);
+
+
+  NS_IMETHOD Create(nsIWidget *aParent,
+              const nsRect &aRect,
+              EVENT_CALLBACK aHandleEventFunction,
+              nsIDeviceContext *aContext = nsnull,
+              nsIAppShell *aAppShell = nsnull,
+              nsIToolkit *aToolkit = nsnull,
+              nsWidgetInitData *aInitData = nsnull);
+
+  NS_IMETHOD Create(nsNativeWidget aParent,
+              const nsRect &aRect,
+              EVENT_CALLBACK aHandleEventFunction,
+              nsIDeviceContext *aContext = nsnull,
+              nsIAppShell *aAppShell = nsnull,
+              nsIToolkit *aToolkit = nsnull,
+              nsWidgetInitData *aInitData = nsnull);
+
+
+    virtual PRBool    OnPaint(nsPaintEvent & aEvent);
+    virtual PRBool    OnScroll(nsScrollbarEvent & aEvent, PRUint32 cPos);
+    virtual PRBool    OnResize(nsSizeEvent &aEvent);
+
+private:
+
+  PRUint32 mLineIncrement;
+  int      mOrientation;
+
+  int AdjustScrollBarPosition(int aPosition);
+
+};
+
+#endif // nsScrollbar_
