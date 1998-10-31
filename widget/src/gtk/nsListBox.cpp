b96ab2420fc6504885f907fd0b630039d8e8d2e8
pavlov%pavlov.net <pavlov%pavlov.net>
1998-10-31 20:21:19 +0000

    oops, forgot that we didn't want the column titles displayed

--- widget/src/gtk/nsListBox.cpp
+++ widget/src/gtk/nsListBox.cpp
@@ -355,6 +355,9 @@ NS_METHOD nsListBox::Create(nsIWidget *aParent,
   } else {
     gtk_clist_set_selection_mode(GTK_CLIST(mWidget), GTK_SELECTION_BROWSE);
   }
+
+  gtk_clist_column_titles_hide(GTK_CLIST(mWidget);
+
 /*
   mWidget = ::XtVaCreateManagedWidget("",
                                     xmListWidgetClass,
