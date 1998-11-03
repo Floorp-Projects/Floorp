970c85b53d3ec00a990036159d46eec211531e85
rpotts@netscape.com <rpotts@netscape.com>
1998-11-03 10:11:33 +0000

    Removed the definition of the nsIDocumentLoadInfo IID

--- xpcom/src/IIDS.h
+++ xpcom/src/IIDS.h
@@ -137,7 +137,7 @@ nsIConnectionInfo = { /* a6cf9062-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-nsIDocumentLoadInfo = { /* a6cf9063-15b3-11d2-932e-00805f8add32 */
+INTERFACENAME = { /* a6cf9063-15b3-11d2-932e-00805f8add32 */
     0xa6cf9063,
     0x15b3,
     0x11d2,
