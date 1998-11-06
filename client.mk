083b21a30d30d9acb166acc21db634d4ae73c1a2
mcafee%netscape.com <mcafee%netscape.com>
1998-11-06 08:39:52 +0000

    Initial version

---
+++ client.mk
@@ -0,0 +1,76 @@
+#!gmake
+# The contents of this file are subject to the Netscape Public License
+# Version 1.0 (the "NPL"); you may not use this file except in
+# compliance with the NPL.  You may obtain a copy of the NPL at
+# http://www.mozilla.org/NPL/
+#
+# Software distributed under the NPL is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+# for the specific language governing rights and limitations under the
+# NPL.
+#
+# The Initial Developer of this code under the NPL is Netscape
+# Communications Corporation.  Portions created by Netscape are
+# Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+# Reserved.
+
+#
+# Build the Mozilla client.
+#
+# This needs CVSROOT set to work, e.g.
+# setenv CVSROOT :pserver:$(USER)%netscape.com@cvs.mozilla.org:/cvsroot
+#
+# Usage:
+# Pull the source:
+#   cvs update mozilla/client.mk
+#   gmake -f mozilla/client.mk checkout
+#
+
+DEPTH=mozilla
+
+# Allow for cvs flags
+ifndef CVS_FLAGS
+CVS_CFLAGS = -q -z 3
+endif
+
+CVSCO		= cvs $(CVS_FLAGS) co -P
+MAKE		= gmake
+AUTOCONF	= autoconf
+TARGETS		= export libs install
+MKDIR		= mkdir
+SH			= /bin/sh
+
+-include $(DEPTH)/config/config.mk
+
+all: checkout
+
+.PHONY: checkout
+
+# List branches here.
+#
+
+checkout:
+# Pull the core layout stuff.
+	$(CVSCO) mozilla/nglayout.mk
+	(cd mozilla; $(MAKE) -f nglayout.mk pull_all)
+
+# Pull xpfe
+	$(CVSCO) mozilla/xpfe
+
+
+# Build with autoconf
+build:
+	PWD=`pwd`
+	(cd mozilla; $(AUTOCONF))
+
+# Do an autoconf build, this isn't working yet. -mcafee
+#
+#	if test ! -d mozilla/$(FOO); then (cd mozilla; $(MKDIR) $(AUTODIR)); fi
+#	@echo cd mozilla/obj-`build/autoconf/config.guess`; ../configure --with-nspr=$(PWD)/$(DIST)
+
+
+# Build & install nspr.  Classic build, no autoconf.
+# Linux/RPM available.
+nspr:
+	(cd mozilla/nsprpub; $(MAKE))
+
