dee9328bdfa444c07cfe6a0b19e5bd9d22c5d118
pavlov%pavlov.net <pavlov%pavlov.net>
1998-11-08 20:45:53 +0000

    add 'this' to the userdata of the widget so that callbacks will have the info they need

--- widget/src/gtk/nsScrollbar.cpp
+++ widget/src/gtk/nsScrollbar.cpp
@@ -80,6 +80,7 @@ NS_METHOD nsScrollbar::Create(nsIWidget* aParent,
   gtk_layout_put(GTK_LAYOUT(parentWidget), mWidget, aRect.x, aRect.y);
   gtk_widget_set_usize(mWidget, aRect.width, aRect.height);
 
+  gtk_object_set_user_data(GTK_OBJECT(mWidget), this);
   gtk_widget_show(mWidget);
 
   // save the event callback function
