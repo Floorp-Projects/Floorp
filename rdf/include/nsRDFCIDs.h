d8ddfb0631dbbd9b2ab2a35608b52c0fb8c4c22b
waterson%netscape.com <waterson%netscape.com>
1998-11-12 02:09:56 +0000

    Added nsRDFCIDs.h, which contains class IDs for RDF objects.

---
+++ rdf/include/nsRDFCIDs.h
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsRDFCIDs_h__
+#define nsRDFCIDs_h__
+
+// {2564E6E0-79C9-11d2-B51D-000000000000}
+#define NS_RDFTREEDATAMODEL_CID \
+{ 0x2564e6e0, 0x79c9, 0x11d2, { 0xb5, 0x1d, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 } }
+
+// {6B13A9C0-79C9-11d2-B51D-000000000000}
+#define NS_RDFTOOLBARDATAMODEL_CID \
+{ 0x6b13a9c0, 0x79c9, 0x11d2, { 0xb5, 0x1d, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 } }
+
+
+#endif // nsRDFCIDs_h__
