cee4a316e2650605a1a7842d693d432fbb83df22
mcmullen@netscape.com <mcmullen@netscape.com>
1998-11-13 22:06:10 +0000

    Commenting out previous change for now.

--- build/mac/Moz.pm
+++ build/mac/Moz.pm
@@ -204,8 +204,8 @@ sub log_recent_errors($)
 					{
 						if ( /^Error/ || /^Couldn’t find project file/ )
 							{
-								if (!$found_errors)
-									print $_; # put the first error in the perl console.
+#								if (!$found_errors)
+#									print $_;
 								$found_errors = 1;
 							}
 						print ERROR_LOG $_;
