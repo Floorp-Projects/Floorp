3d822da6c800046e05c6f45b0cde311d707662ad
locka@iol.ie <locka@iol.ie>
1998-11-15 17:09:20 +0000

    Added NPL header Fixed problem caused by typedef int32 disappearing from winsock.h

--- webshell/embed/ActiveX/StdAfx.h
+++ webshell/embed/ActiveX/StdAfx.h
@@ -1,3 +1,21 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
 // stdafx.h : include file for standard system include files,
 //      or project specific include files that are used frequently,
 //      but are changed infrequently
@@ -23,16 +41,25 @@ extern CComModule _Module;
 #include <atlcom.h>
 #include <atlctl.h>
 
+#include <winsock2.h>
+
 // STL headers
 #include <vector>
+#include <string>
+
+// New winsock2.h doesn't define this anymore
+typedef long int32;
+
+#include "jstypes.h"
+#include "prtypes.h"
 
 // Mozilla headers
-
+//#define USE_NGPREF
 #ifdef USE_NGPREF
 #include "nsIPref.h"
 #endif
 
-#include "prtypes.h"
+
 #include "xp_core.h"
 #include "jscompat.h"
 
@@ -58,10 +85,14 @@ extern CComModule _Module;
 // Mozilla control headers
 #include "resource.h"
 
+// Define some string classes
+typedef std::basic_string<TCHAR> tstring;
+
 #include "BrowserDiagnostics.h"
 #include "MozillaControl.h"
 #include "MozillaBrowser.h"
 #include "WebShellContainer.h"
+
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Developer Studio will insert additional declarations immediately before the previous line.
