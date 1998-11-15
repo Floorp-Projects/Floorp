79ded2d136449afee3ca38afb78dbad16c216aca
locka%iol.ie <locka%iol.ie>
1998-11-15 17:04:58 +0000

    Fixed trace bug Added NPL header

--- webshell/embed/ActiveX/BrowserDiagnostics.h
+++ webshell/embed/ActiveX/BrowserDiagnostics.h
@@ -1,7 +1,24 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
 #ifndef BROWSER_DIAGNOSTICS_H
 #define BROWSER_DIAGNOSTICS_H
 
-#ifndef _DEBUG
+#ifdef _DEBUG
 #  include <assert.h>
 #  define NG_TRACE ATLTRACE
 #  define NG_ASSERT(expr) assert(expr)
@@ -10,6 +27,7 @@
 #  define NG_ASSERT_NULL_OR_POINTER(p, type) \
 	NG_ASSERT(((p) == NULL) || NgIsValidAddress((p), sizeof(type), FALSE))
 #else
+#  define NG_TRACE ATLTRACE
 #  define NG_ASSERT(X)
 #  define NG_ASSERT_POINTER(p, type)
 #  define NG_ASSERT_NULL_OR_POINTER(p, type)
