a8d3c8f20f645fcc72b65bf0ae93ae3ad1d93063
vidur@netscape.com <vidur@netscape.com>
1998-11-17 02:37:19 +0000

    New interface nsIDOMNSDocument

--- xpcom/base/IIDS.h
+++ xpcom/base/IIDS.h
@@ -773,7 +773,7 @@ nsIHTMLContentContainer = { /* a6cf90cc-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-INTERFACENAME = { /* a6cf90cd-15b3-11d2-932e-00805f8add32 */
+nsIDOMNSDocument = { /* a6cf90cd-15b3-11d2-932e-00805f8add32 */
     0xa6cf90cd,
     0x15b3,
     0x11d2,
