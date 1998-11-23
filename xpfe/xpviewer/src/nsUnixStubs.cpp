8c50aa8388fe7164d511c3b8527fbcae524383c8
cls%seawood.org <cls%seawood.org>
1998-11-23 17:13:39 +0000

    Use MOZ_USER_DIR since it's already setup.

--- xpfe/xpviewer/src/nsUnixStubs.cpp
+++ xpfe/xpviewer/src/nsUnixStubs.cpp
@@ -6,6 +6,10 @@
 #include "xp_linebuf.h"
 #include "mkbuf.h"
 
+#ifndef MOZ_USER_DIR
+#define MOZ_USER_DIR ".mozilla"
+#endif
+
 extern "C" XP_Bool ValidateDocData(MWContext *window_id) 
 { 
   printf("ValidateDocData not implemented, stubbed in webshell/tests/viewer/nsStubs.cpp\n"); 
@@ -42,12 +46,13 @@ extern "C" char *fe_GetConfigDir(void) {
   char *home = getenv("HOME");
   if (home) {
     int len = strlen(home);
-    len += strlen("/.netscape") + 1;
+    len += strlen("/") + strlen(MOZ_USER_DIR) + 1;
 
     char* config_dir = (char *)XP_CALLOC(len, sizeof(char));
     // we really should use XP_STRN*_SAFE but this is MODULAR_NETLIB
     XP_STRCPY(config_dir, home);
-    XP_STRCAT(config_dir, "/.netscape");
+    XP_STRCAT(config_dir, "/");
+    XP_STRCAT(config_dir, MOZ_USER_DIR); 
     return config_dir;
   }
   
