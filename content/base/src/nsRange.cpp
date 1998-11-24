3a2b2f6096981aa3cc50431265a992463a2e4617
akkana%netscape.com <akkana%netscape.com>
1998-11-24 21:20:11 +0000

    Implementation file for nsIDOMRange

---
+++ content/base/src/nsRange.cpp
@@ -0,0 +1,156 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+/* AUTO-GENERATED. DO NOT EDIT!!! */
+
+/*
+ * nsRange.cpp: Implementation of the nsIDOMRange object.
+ */
+
+class nsRange : public nsIDOMRange
+{
+public:
+
+  NS_IMETHOD    GetIsPositioned(PRBool* aIsPositioned)=0;
+  NS_IMETHOD    SetIsPositioned(PRBool aIsPositioned)=0;
+
+  NS_IMETHOD    GetStartParent(nsIDOMElement** aStartParent)=0;
+  NS_IMETHOD    SetStartParent(nsIDOMElement* aStartParent)=0;
+
+  NS_IMETHOD    GetStartOffset(PRInt32* aStartOffset)=0;
+  NS_IMETHOD    SetStartOffset(PRInt32 aStartOffset)=0;
+
+  NS_IMETHOD    GetEndParent(nsIDOMElement** aEndParent)=0;
+  NS_IMETHOD    SetEndParent(nsIDOMElement* aEndParent)=0;
+
+  NS_IMETHOD    GetEndOffset(PRInt32* aEndOffset)=0;
+  NS_IMETHOD    SetEndOffset(PRInt32 aEndOffset)=0;
+
+  NS_IMETHOD    GetIsCollapsed(PRBool* aIsCollapsed)=0;
+  NS_IMETHOD    SetIsCollapsed(PRBool aIsCollapsed)=0;
+
+  NS_IMETHOD    GetCommonParent(nsIDOMNode** aCommonParent)=0;
+  NS_IMETHOD    SetCommonParent(nsIDOMNode* aCommonParent)=0;
+
+  NS_IMETHOD    SetStart(nsIDOMNode* aParent, PRInt32 aOffset)=0;
+
+  NS_IMETHOD    SetEnd(nsIDOMNode* aParent, PRInt32 aOffset)=0;
+
+  NS_IMETHOD    Collapse(PRBool aToStart)=0;
+
+  NS_IMETHOD    Unposition()=0;
+
+  NS_IMETHOD    SelectNode(nsIDOMNode* aN)=0;
+
+  NS_IMETHOD    SelectNodeContents(nsIDOMNode* aN)=0;
+
+  NS_IMETHOD    DeleteContents()=0;
+
+  NS_IMETHOD    ExtractContents(nsIDOMDocumentFragment** aReturn)=0;
+
+  NS_IMETHOD    CopyContents(nsIDOMDocumentFragment** aReturn)=0;
+
+  NS_IMETHOD    InsertNode(nsIDOMNode* aN)=0;
+
+  NS_IMETHOD    SurroundContents(nsIDOMNode* aN)=0;
+
+  NS_IMETHOD    Clone(nsIDOMRange** aReturn)=0;
+
+  NS_IMETHOD    ToString(nsString& aReturn)=0;
+};
+
+NS_IMETHOD nsRange::GetIsPositioned(PRBool* aIsPositioned)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SetIsPositioned(PRBool aIsPositioned)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::GetStartParent(nsIDOMElement** aStartParent)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SetStartParent(nsIDOMElement* aStartParent)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::GetStartOffset(PRInt32* aStartOffset)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SetStartOffset(PRInt32 aStartOffset)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::GetEndParent(nsIDOMElement** aEndParent)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SetEndParent(nsIDOMElement* aEndParent)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::GetEndOffset(PRInt32* aEndOffset)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SetEndOffset(PRInt32 aEndOffset)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::GetIsCollapsed(PRBool* aIsCollapsed)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SetIsCollapsed(PRBool aIsCollapsed)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::GetCommonParent(nsIDOMNode** aCommonParent)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SetCommonParent(nsIDOMNode* aCommonParent)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SetStart(nsIDOMNode* aParent, PRInt32 aOffset)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SetEnd(nsIDOMNode* aParent, PRInt32 aOffset)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::Collapse(PRBool aToStart)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::Unposition()
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SelectNode(nsIDOMNode* aN)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SelectNodeContents(nsIDOMNode* aN)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::DeleteContents()
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::ExtractContents(nsIDOMDocumentFragment** aReturn)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::CopyContents(nsIDOMDocumentFragment** aReturn)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::InsertNode(nsIDOMNode* aN)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::SurroundContents(nsIDOMNode* aN)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::Clone(nsIDOMRange** aReturn)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
+NS_IMETHOD nsRange::ToString(nsString& aReturn)
+{ return NS_ERROR_NOT_IMPLEMENTED; }
+
