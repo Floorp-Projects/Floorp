a254a6fbca03abc6f092e60253fcef9121ffbd08
akkana%netscape.com <akkana%netscape.com>
1998-11-24 01:39:47 +0000

    Range specification

--- xpcom/src/IIDS.h
+++ xpcom/src/IIDS.h
@@ -779,7 +779,7 @@ nsIDOMNSDocument = { /* a6cf90cd-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-INTERFACENAME = { /* a6cf90ce-15b3-11d2-932e-00805f8add32 */
+nsIRange = { /* a6cf90ce-15b3-11d2-932e-00805f8add32 */
     0xa6cf90ce,
     0x15b3,
     0x11d2,
