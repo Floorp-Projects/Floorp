b47f47475b6d652c380b7b5c79726bf8f6d14b11
akkana%netscape.com <akkana%netscape.com>
1998-11-25 23:20:29 +0000

    Implementation of the easy parts

---
+++ content/base/src/nsRange.h
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/*
+ * nsRange.cpp: Implementation of the nsIDOMRange object.
+ *
+ * This header file is intended to live inside layout/base/src;
+ * if you find yourself wanting to export it, that's a sign that
+ * it's time to implement a COM-legal factory method for nsRange.
+ */
+
+#include "nsIDOMRange.h"
+
+extern "C" nsresult
+NS_NewRange(nsIRange** aRange);
+
