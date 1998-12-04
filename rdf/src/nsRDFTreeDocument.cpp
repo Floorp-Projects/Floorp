c637ece4b19c417987a65c192ba05beb124575d4
waterson@netscape.com <waterson@netscape.com>
1998-12-04 10:00:13 +0000

    Initial revision.

---
+++ rdf/src/nsRDFTreeDocument.cpp
@@ -0,0 +1,41 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+#include "nsIRDFContent.h"
+#include "nsIRDFCursor.h"
+#include "nsIRDFDataBase.h"
+#include "nsIRDFNode.h"
+#include "nsIRDFResourceManager.h"
+#include "nsIServiceManager.h"
+#include "nsISupportsArray.h"
+#include "nsLayoutCID.h"
+#include "nsRDFCID.h"
+#include "nsRDFDocument.h"
+#include "rdfutil.h"
+
+// XXX just a stub for now.
+
+nsresult NS_NewRDFTreeDocument(nsIRDFDocument** result)
+{
+    *result = nsnull;
+    return NS_NOINTERFACE;
+}
+
+
+
---
+++ rdf/src/rdfutil.h
@@ -0,0 +1,55 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+#ifndef rdfutil_h__
+#define rdfutil_h__
+
+#include "prtypes.h"
+
+class nsIRDFDataBase;
+class nsIRDFNode;
+class nsIRDFResourceManager;
+class nsString;
+
+/**
+ * Returns PR_TRUE if the URI is an RDF ordinal property; e.g., rdf:_1,
+ * rdf:_2, etc.
+ */
+PRBool
+rdf_IsOrdinalProperty(const nsString& uri);
+
+
+/**
+ * Returns PR_TRUE if the resource is a container resource; e.g., an
+ * rdf:Bag.
+ */
+PRBool
+rdf_IsContainer(nsIRDFResourceManager* mgr,
+                nsIRDFDataBase* db,
+                nsIRDFNode* resource);
+
+/**
+ * Tries to guess whether the specified node is a resource or a literal.
+ */
+PRBool
+rdf_IsResource(nsIRDFNode* node);
+
+#endif // rdfutil_h__
+
+
