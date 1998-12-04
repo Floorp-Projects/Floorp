3cdcf1b8c01644157120dfc385fed1da4f6c76d0
dcone@netscape.com <dcone@netscape.com>
1998-12-04 15:02:26 +0000

    First Checked In.

---
+++ gfx/src/mac/nsDeviceContextSpecFactoryM.cpp
@@ -0,0 +1,77 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "nsDeviceContextSpecFactoryM.h"
+#include "nsDeviceContextSpecMac.h"
+#include "nsGfxCIID.h"
+#include "plstr.h"
+
+/** -------------------------------------------------------
+ *  Constructor
+ *  @update   dc 12/02/98
+ */
+nsDeviceContextSpecFactoryMac :: nsDeviceContextSpecFactoryMac()
+{
+}
+
+/** -------------------------------------------------------
+ *  Destructor
+ *  @update   dc 12/02/98
+ */
+nsDeviceContextSpecFactoryMac :: ~nsDeviceContextSpecFactoryMac()
+{
+}
+
+static NS_DEFINE_IID(kDeviceContextSpecFactoryIID, NS_IDEVICE_CONTEXT_SPEC_FACTORY_IID);
+static NS_DEFINE_IID(kIDeviceContextSpecIID, NS_IDEVICE_CONTEXT_SPEC_IID);
+static NS_DEFINE_IID(kDeviceContextSpecCID, NS_DEVICE_CONTEXT_SPEC_CID);
+
+NS_IMPL_QUERY_INTERFACE(nsDeviceContextSpecFactoryMac, kDeviceContextSpecFactoryIID)
+NS_IMPL_ADDREF(nsDeviceContextSpecFactoryMac)
+NS_IMPL_RELEASE(nsDeviceContextSpecFactoryMac)
+
+/** -------------------------------------------------------
+ *  Initialize the device context spec factory
+ *  @update   dc 12/02/98
+ */
+NS_IMETHODIMP nsDeviceContextSpecFactoryMac :: Init(void)
+{
+  return NS_OK;
+}
+
+/** -------------------------------------------------------
+ *  Get a device context specification
+ *  @update   dc 12/02/98
+ */
+NS_IMETHODIMP nsDeviceContextSpecFactoryMac :: CreateDeviceContextSpec(nsIDeviceContextSpec *aOldSpec,
+                                                                       nsIDeviceContextSpec *&aNewSpec,
+                                                                       PRBool aQuiet)
+{
+nsresult  						rv = NS_ERROR_FAILURE;
+nsIDeviceContextSpec  *devSpec = nsnull;
+
+	nsRepository::CreateInstance(kDeviceContextSpecCID, nsnull, kIDeviceContextSpecIID, (void **)&devSpec);
+
+	if (nsnull != devSpec){
+	  if (NS_OK == ((nsDeviceContextSpecMac *)devSpec)->Init(aQuiet)){
+	    aNewSpec = devSpec;
+	    rv = NS_OK;
+	  }
+	}
+	return rv;
+}
---
+++ gfx/src/mac/nsDeviceContextSpecMac.cpp
@@ -0,0 +1,103 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "nsDeviceContextSpecMac.h"
+#include "prmem.h"
+#include "plstr.h"
+
+/** -------------------------------------------------------
+ *  Construct the nsDeviceContextSpecMac
+ *  @update   dc 12/02/98
+ */
+nsDeviceContextSpecMac :: nsDeviceContextSpecMac()
+{
+  NS_INIT_REFCNT();
+	mPrtRec = nsnull;
+	mPrintManagerOpen = PR_FALSE;
+	
+}
+
+/** -------------------------------------------------------
+ *  Destroy the nsDeviceContextSpecMac
+ *  @update   dc 12/02/98
+ */
+nsDeviceContextSpecMac :: ~nsDeviceContextSpecMac()
+{
+
+	if(mPrtRec != nsnull){
+		::DisposeHandle((Handle)mPrtRec);
+		mPrtRec = nsnull;
+		ClosePrintManager();
+	}
+
+}
+
+static NS_DEFINE_IID(kDeviceContextSpecIID, NS_IDEVICE_CONTEXT_SPEC_IID);
+
+NS_IMPL_QUERY_INTERFACE(nsDeviceContextSpecMac, kDeviceContextSpecIID)
+NS_IMPL_ADDREF(nsDeviceContextSpecMac)
+NS_IMPL_RELEASE(nsDeviceContextSpecMac)
+
+/** -------------------------------------------------------
+ *  Initialize the nsDeviceContextSpecMac
+ *  @update   dc 12/02/98
+ */
+NS_IMETHODIMP nsDeviceContextSpecMac :: Init(PRBool	aQuiet)
+{
+nsresult  theResult = NS_ERROR_FAILURE;
+THPrint		prtRec;
+GrafPtr		oldport;
+
+	// open the printing manager
+	::PrOpen();
+	if(::PrError() == noErr){
+		mPrintManagerOpen = PR_TRUE;
+		prtRec = (THPrint) NewHandle(sizeof(TPrint));
+		if(prtRec!=nsnull){
+			::PrintDefault(prtRec);
+		
+			// standard print dialog, if true print
+			if(PrJobDialog(prtRec)){
+				// have the print record
+				theResult = NS_OK;
+				mPrtRec = prtRec;
+			}else{
+				// don't print,
+				::DisposeHandle((Handle)prtRec);
+				::SetPort(oldport); 
+			}
+		}
+	}
+  return theResult;
+}
+
+/** -------------------------------------------------------
+ * Closes the printmanager if it is open.
+ * @update   dc 12/03/98
+ */
+NS_IMETHODIMP nsDeviceContextSpecMac :: ClosePrintManager()
+{
+PRBool	isPMOpen;
+
+	this->PrintManagerOpen(&isPMOpen);
+	if(isPMOpen){
+		::PrClose();
+	}
+	
+	return NS_OK;
+}  
