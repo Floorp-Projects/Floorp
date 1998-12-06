f55cbf30b226e762edd88cece64a3a4b4c6866c0
troy@netscape.com <troy@netscape.com>
1998-12-06 00:30:51 +0000

    Added nsIPrintStatusCallback

--- xpcom/src/IIDS.h
+++ xpcom/src/IIDS.h
@@ -809,7 +809,7 @@ nsIPageSequenceFrame = { /* a6cf90d2-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-INTERFACENAME = { /* a6cf90d3-15b3-11d2-932e-00805f8add32 */
+nsIPrintStatusCallback = { /* a6cf90d3-15b3-11d2-932e-00805f8add32 */
     0xa6cf90d3,
     0x15b3,
     0x11d2,
