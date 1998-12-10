b49d65a85a69485a701c9009ddd95ef7a2b07736
kin%netscape.com <kin%netscape.com>
1998-12-10 18:23:04 +0000

    Initial checkin of file.

---
+++ editor/txmgr/public/nsTransactionManagerCID.h
@@ -0,0 +1,27 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsTransactionManagerCID_h__
+#define nsTransactionManagerCID_h__
+
+#define NS_TRANSACTION_MANAGER_FACTORY_CID          \
+{ /* 9C8F9601-801A-11d2-98BA-00805F297D89 */        \
+0x9c8f9601, 0x801a, 0x11d2,                         \
+{ 0x98, 0xba, 0x0, 0x80, 0x5f, 0x29, 0x7d, 0x89 } }
+
+#endif /* nsTransactionManagerCID_h__ */
