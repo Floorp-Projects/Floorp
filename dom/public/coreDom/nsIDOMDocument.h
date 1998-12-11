d1656fdf2e8326b668c47e7625131dd8a3cf198c
mjudge@netscape.com <mjudge@netscape.com>
1998-12-11 23:35:46 +0000

    adding IID for now, auto generate will duplicate work that is ok

--- dom/public/coreDom/nsIDOMDocument.h
+++ dom/public/coreDom/nsIDOMDocument.h
@@ -44,6 +44,8 @@ class nsIDOMNodeList;
 class nsIDOMDocument : public nsIDOMNode {
 public:
 
+  static const nsIID& IID() { static nsIID iid = NS_IDOMDOCUMENT_IID; return iid; }
+
   NS_IMETHOD    GetDoctype(nsIDOMDocumentType** aDoctype)=0;
 
   NS_IMETHOD    GetImplementation(nsIDOMDOMImplementation** aImplementation)=0;
