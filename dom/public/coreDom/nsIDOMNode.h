c01fe7ab143ae9582234c90c603a6cc574822871
mjudge%netscape.com <mjudge%netscape.com>
1998-12-11 23:35:46 +0000

    adding IID for now, auto generate will duplicate work that is ok

--- dom/public/coreDom/nsIDOMNode.h
+++ dom/public/coreDom/nsIDOMNode.h
@@ -35,6 +35,7 @@ class nsIDOMNodeList;
 
 class nsIDOMNode : public nsISupports {
 public:
+  static const nsIID& IID() { static nsIID iid = NS_IDOMNODE_IID; return iid; }
   enum {
     ELEMENT_NODE = 1,
     ATTRIBUTE_NODE = 2,
