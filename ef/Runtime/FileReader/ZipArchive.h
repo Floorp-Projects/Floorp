5ca08c04d2c57c0d6ae7f8b7bbdfaa58bd0342f0
rth%cygnus.com <rth%cygnus.com>
1998-12-12 05:23:36 +0000

    Insert directory entries into a SortedTree for searching.

--- ef/Runtime/FileReader/ZipArchive.h
+++ ef/Runtime/FileReader/ZipArchive.h
@@ -20,40 +20,67 @@
 
 #include "Fundamentals.h"
 #include "prio.h"
+#include "plstr.h"
+#include "Tree.h"
 
 /*
  * Central zip directory entry
  */
-typedef struct {
-  char *fn;                     /* file name */
-  Uint32 len;                   /* file size */
-  Uint32 size;                  /* file compressed size */
-  Int32 method;                 /* Compression method */
-  Int32 mod;                    /* file modification time */
-  Int32 off;                    /* file contents offset */
-} DirectoryEntry;
 
-/* An object that represents a zip archive */
+class DirectoryEntry_Key {
+private:
+  const char * const str;
+
+public:
+  explicit DirectoryEntry_Key(const char *s) : str(s) { }
+  DirectoryEntry_Key(const DirectoryEntry_Key &other) : str(other.str) { }
+
+  bool operator== (const DirectoryEntry_Key &other) const {
+    return PL_strcasecmp(str, other.str) == 0;
+  }
+
+  bool operator< (const DirectoryEntry_Key &other) const {
+    return PL_strcasecmp(str, other.str) < 0;
+  }
+};
+
+struct DirectoryEntry : public TreeNode<DirectoryEntry> {
+  const char *fn;               // file name
+  Uint32 len;                   // file size
+  Uint32 size;                  // file compressed size
+  Int32 method;                 // compression method
+  Int32 mod;                    // file modification time
+  Int32 off;                    // file contents offset
+
+  // Bits to interface with the tree code.
+  DirectoryEntry_Key getKey() const {
+    return DirectoryEntry_Key(fn);
+  }
+};
+
+
+/*
+ * An object that represents a zip archive.
+ */
+
 class ZipArchive {
 public:
-  /* Create a zip archive reader from a zip archive whose
-   * canonical pathname is archiveName. Pool is used to allocate
-   * memory for data structures. Sets status to true if it was
-   * able to successfully open the archive; returns false if
-   * it could not.
-   */
+  // Create a zip archive reader from a zip archive whose
+  // canonical pathname is archiveName. Pool is used to allocate
+  // memory for data structures. Sets status to true if it was
+  // able to successfully open the archive; returns false if
+  // it could not.
   ZipArchive(const char *archiveName, Pool &pool, bool &status);
 
   ~ZipArchive();
 
-  /* Read a file given by fileName from the zip archive into
-   * buf, allocating as much memory as necessary. This memory
-   * is allocated using the pool passed into the constructor
-   * and will go away when the pool is destroyed. On success,
-   * returns true and sets len to the length of the file read.
-   * Returns false if the file was not found in the archive, or
-   * if there was an error.
-   */
+  // Read a file given by fileName from the zip archive into
+  // buf, allocating as much memory as necessary. This memory
+  // is allocated using the pool passed into the constructor
+  // and will go away when the pool is destroyed. On success,
+  // returns true and sets len to the length of the file read.
+  // Returns false if the file was not found in the archive, or
+  // if there was an error.
   bool get(const char *fileName, char *&buf, Int32 &len) {
     const DirectoryEntry *entry = lookup(fileName);
 
@@ -63,53 +90,45 @@ public:
     return get(entry, buf, len);
   }
   
-  /* Exactly like get() above, but works with a directory entry
-   * obtained via lookup().
-   */
+  // Exactly like get() above, but works with a directory entry
+  // obtained via lookup().
   bool get(const DirectoryEntry *entry, char *&buf, Int32 &len);
 
-  /* returns true if the given file exists in the archive, false
-   * otherwise.
-   */
+  // Returns true if the given file exists in the archive, false
+  // otherwise.
   bool exists(const char *fileName) {
     return (lookup(fileName) != 0);
   }
 
-  /* lookup a file and return it's directory entry if it exists.
-   * If not, return false.
-   */
-  const DirectoryEntry *lookup(const char *fileName);
+  // Lookup a file and return it's directory entry if it exists.
+  // If not, return false.
+  const DirectoryEntry *lookup(const char *fileName) {
+    return dir.find(DirectoryEntry_Key(fileName));
+  }
 
-  /* Return number of elements in the zip archive whose filenames
-   * have the suffix indicated by fileNameSuffix.
-   */
+  // Return number of elements in the zip archive whose filenames
+  // have the suffix indicated by fileNameSuffix.
   Uint32 getNumElements(const char *fileNameSuffix);
   
-  /* Gets the names of all elements in the zip archive whose filenames
-   * have the suffix indicated by fileNameSuffix. Returns the number
-   * of matching elements. Memory for buf is allocated using the
-   * pool passed into the constructor and is destroyed when the
-   * pool is destroyed.
-   */
+  // Gets the names of all elements in the zip archive whose filenames
+  // have the suffix indicated by fileNameSuffix. Returns the number
+  // of matching elements. Memory for buf is allocated using the
+  // pool passed into the constructor and is destroyed when the
+  // pool is destroyed.
   Uint32 listElements(const char *fileNameSuffix, char **&buf);
 
-
 private:
-  Pool &pool;     /* Pool used to allocate internal memory */
-
-  /* File descriptor */
-  PRFileDesc *fp;
-
-  DirectoryEntry *dir;	/* zip file directory */
-  Uint32 nel;		/* number of directory entries */
-  Uint32 cenoff;	/* Offset of central directory (CEN) */
-  Uint32 endoff;	/* Offset of end-of-central-directory record */
+  Pool &pool;                   // Pool used to allocate internal memory
+  PRFileDesc *fp;               // File descriptor
+  SortedTree<DirectoryEntry,DirectoryEntry_Key> dir; // zip file directory
+  Uint32 nel;                   // number of directory entries
+  Uint32 cenoff;                // Offset of central directory (CEN)
+  Uint32 endoff;                // Offset of end-of-central-directory record
 
   bool initReader();
   bool findEnd();
   bool readFully(void *buf, Int32 len);
   bool inflateFully(Uint32 size, void *buf, Uint32 len);
 };		      
-
 
 #endif /* _ZIP_ARCHIVE_H_ */
