6bfbf3005ed79985f89adae10be84bf97b0e94c6
rpotts@netscape.com <rpotts@netscape.com>
1999-01-04 08:49:49 +0000

    This is the implementation of NS_GetFactory(...) for the xpcom DLL.

---
+++ xpcom/src/nsXPComFactory.cpp
@@ -0,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+#include "nsXPComFactory.h"
+#include "nsXPComCIID.h"
+
+static NS_DEFINE_IID(kIFactoryIID,          NS_IFACTORY_IID);
+static NS_DEFINE_IID(kEventQueueServiceCID, NS_EVENTQUEUESERVICE_CID);
+
+nsresult NS_NewEventQueueServiceFactory(nsIFactory** aResult);
+
+/*
+ * Define the global NSGetFactory(...) entry point for the xpcom DLL...
+ */
+#if defined(XP_MAC) && defined(MAC_STATIC)
+extern "C" NS_EXPORT nsresult NSGetFactory_XPCOM_DLL(const nsCID& aClass, nsIFactory** aFactory)
+#else
+extern "C" NS_EXPORT nsresult NSGetFactory(const nsCID& aClass, nsIFactory** aFactory)
+#endif
+{
+  nsresult rv = NS_OK;
+
+  if (NULL == aFactory) {
+    return NS_ERROR_NULL_POINTER;
+  }
+
+  if (aClass.Equals(kEventQueueServiceCID)) {
+    rv = NS_NewEventQueueServiceFactory(aFactory);
+  }
+
+  return rv;
+}
+
