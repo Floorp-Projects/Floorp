d1d64a68ee33a1b2b75f1627e39485a4bd39f491
vidur@netscape.com <vidur@netscape.com>
1999-01-06 02:05:25 +0000

    Fixed bug 1956. Generic jsval to nsISupports conversion routine checks for valid object.

--- dom/src/base/nsJSUtils.cpp
+++ dom/src/base/nsJSUtils.cpp
@@ -164,14 +164,21 @@ nsJSUtils::nsConvertJSValToObject(nsISupports** aSupports,
     *aSupports = nsnull;
   }
   else if (JSVAL_IS_OBJECT(aValue)) {
-    JSObject *jsobj = JSVAL_TO_OBJECT(aValue); 
-    nsISupports *supports = (nsISupports *)JS_GetPrivate(aContext, jsobj);
-    if (NS_OK != supports->QueryInterface(aIID, (void **)aSupports)) {
-      char buf[128];
-      char typeName[128];
-      aTypeName.ToCString(typeName, sizeof(typeName));
-      sprintf(buf, "Parameter must of type %s", typeName);
-      JS_ReportError(aContext, buf);
+    JSObject* jsobj = JSVAL_TO_OBJECT(aValue); 
+    JSClass* jsclass = JS_GetClass(aContext, jsobj);
+    if ((nsnull != jsclass) && (jsclass->flags & JSCLASS_HAS_PRIVATE)) {
+      nsISupports *supports = (nsISupports *)JS_GetPrivate(aContext, jsobj);
+      if (NS_OK != supports->QueryInterface(aIID, (void **)aSupports)) {
+        char buf[128];
+        char typeName[128];
+        aTypeName.ToCString(typeName, sizeof(typeName));
+        sprintf(buf, "Parameter must of type %s", typeName);
+        JS_ReportError(aContext, buf);
+        return JS_FALSE;
+      }
+    }
+    else {
+      JS_ReportError(aContext, "Parameter isn't a object");
       return JS_FALSE;
     }
   }
