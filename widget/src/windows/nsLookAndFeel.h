f2786e1093296db940d38ffb3a7ecd8f5521805d
rods@netscape.com <rods@netscape.com>
1999-01-19 17:36:19 +0000

    Added new Method for Querying System Fonts

--- widget/src/windows/nsLookAndFeel.h
+++ widget/src/windows/nsLookAndFeel.h
@@ -28,6 +28,7 @@ public:
 
   NS_DECL_ISUPPORTS
 
+  NS_IMETHOD GetFont(const nsFontID aID, nsFont &aFont);
   NS_IMETHOD GetColor(const nsColorID aID, nscolor &aColor);
   NS_IMETHOD GetMetric(const nsMetricID aID, PRInt32 & aMetric);
   NS_IMETHOD GetMetric(const nsMetricFloatID aID, float & aMetric);
