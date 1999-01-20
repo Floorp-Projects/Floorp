562c3ea1058db48c85d840d9dd5ca698995739cd
kipp%netscape.com <kipp%netscape.com>
1999-01-20 17:53:09 +0000

    allocated an IID

--- xpcom/src/IIDS.h
+++ xpcom/src/IIDS.h
@@ -881,7 +881,7 @@ nsIAreaFrame = { /* a6cf90de-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-INTERFACENAME = { /* a6cf90df-15b3-11d2-932e-00805f8add32 */
+nsBlockFrame = { /* a6cf90df-15b3-11d2-932e-00805f8add32 */
     0xa6cf90df,
     0x15b3,
     0x11d2,
