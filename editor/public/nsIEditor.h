739a14ec87a680f2f7538af2c23edc1eda077b71
buster@netscape.com <buster@netscape.com>
1999-01-21 01:53:10 +0000

    starting to move towards a slimmed down nsIEditor.h

--- editor/public/nsIEditor.h
+++ editor/public/nsIEditor.h
@@ -21,6 +21,7 @@
 #include "nsISupports.h"
 
 class nsIDOMElement;
+class nsITransaction;
 
 /*
 Editor interface to outside world
@@ -36,22 +37,22 @@ Editor interface to outside world
 0xe2f4c7f1, 0x864a, 0x11d2, \
 { 0x8f, 0x38, 0x0, 0x60, 0x8, 0x31, 0x1, 0x94 } }
 
-enum PROPERTIES{NONE = 0,BOLD = 1,NUMPROPERTIES};
 
 class nsIDOMDocument;
 class nsIPresShell;
 class nsString;
 
 /**
- * An editor specific interface. 
+ * A generic editor interface. 
  * <P>
- * It's implemented by an object that has to execute specific editor
- * behavior.
- * <P>
- * The specific behavior is still in design this is an unfinished interface
+ * nsIEditor is the base interface used by applications to communicate with the editor.  
+ * It makes no assumptions about the kind of content it is editing, 
+ * other than the content being a DOM tree. 
  */
 class nsIEditor  : public nsISupports{
 public:
+
+  typedef enum {NONE = 0,BOLD = 1,NUMPROPERTIES} Properties;
 
   typedef enum {eLTR=0, eRTL=1} Direction;
 
@@ -76,20 +77,21 @@ public:
    *
    * @param aProperty An enum that lists the various properties that can be applied, bold, ect.
    */
-  virtual nsresult SetProperties(PROPERTIES aProperty)=0;
+  virtual nsresult SetProperties(Properties aProperties)=0;
 
   /**
    * SetProperties() sets the properties of the current selection
    *
    * @param aProperty An enum that will recieve the various properties that can be applied from the current selection.
    */
-  virtual nsresult GetProperties(PROPERTIES **aProperty)=0;
+  virtual nsresult GetProperties(Properties **aProperties)=0;
 
   /**
-   * SetAttribute() sets the attribute of the current node.
+   * SetAttribute() sets the attribute of aElement.
    * No checking is done to see if aAttribute is a legal attribute of the node,
    * or if aValue is a legal value of aAttribute.
    *
+   * @param aElement    the content element to operate on
    * @param aAttribute  the string representation of the attribute to set
    * @param aValue      the value to set aAttribute to
    */
@@ -98,8 +100,9 @@ public:
                                 const nsString& aValue)=0;
 
   /**
-   * GetAttributeValue() retrieves the attribute's value for the current node.
+   * GetAttributeValue() retrieves the attribute's value for aElement.
    *
+   * @param aElement      the content element to operate on
    * @param aAttribute    the string representation of the attribute to get
    * @param aResultValue  the value of aAttribute.  only valid if aResultIsSet is PR_TRUE
    * @param aResultIsSet  PR_TRUE if aAttribute is set on the current node, PR_FALSE if it is not.
@@ -109,6 +112,13 @@ public:
                                      nsString&       aResultValue, 
                                      PRBool&         aResultIsSet)=0;
 
+  /**
+   * RemoveAttribute() deletes aAttribute from the attribute list of aElement.
+   * If aAttribute is not an attribute of aElement, nothing is done.
+   *
+   * @param aElement      the content element to operate on
+   * @param aAttribute    the string representation of the attribute to get
+   */
   virtual nsresult RemoveAttribute(nsIDOMElement * aElement, 
                                    const nsString& aAttribute)=0;
 
@@ -126,6 +136,47 @@ public:
    * @param aCtrlKey  was the CtrlKey down?
    */
   virtual nsresult Commit(PRBool aCtrlKey)=0;
+
+  /** Do fires a transaction.  It is provided here so clients can create their own transactions.
+    * Clients need no knowledge of whether the editor has a transaction manager or not.
+    * If a transaction manager is present, it is used.  
+    * Otherwise, the transaction is just executed directly.
+    *
+    * @param aTxn the transaction to execute
+    */
+  virtual nsresult Do(nsITransaction *aTxn)=0;
+
+  /** Undo reverses the effects of the last ExecuteTransaction operation
+    * It is provided here so clients need no knowledge of whether the editor has a transaction manager or not.
+    * If a transaction manager is present, it is told to undo and the result of
+    * that undo is returned.  
+    * Otherwise, the Undo request is ignored.
+    *
+    */
+  virtual nsresult Undo(PRUint32 aCount)=0;
+
+  /** Redo reverses the effects of the last Undo operation
+    * It is provided here so clients need no knowledge of whether the editor has a transaction manager or not.
+    * If a transaction manager is present, it is told to redo and the result of
+    * that redo is returned.  
+    * Otherwise, the Redo request is ignored.
+    *
+    */
+  virtual nsresult Redo(PRUint32 aCount)=0;
+
+  /** BeginUpdate is a signal from the caller to the editor that the caller will execute multiple updates
+    * to the content tree that should be treated in the most efficient way possible.
+    * EndUpdate must be called after BeginUpdate.
+    * Calls to BeginUpdate can be nested, as long as EndUpdate is called once per BeginUpdate.
+    */
+  virtual nsresult BeginUpdate()=0;
+
+  /** EndUpdate is a signal from the caller to the editor that the caller is finished updating the content model.
+    * BeginUpdate must be called before EndUpdate is called.
+    * Calls to BeginUpdate can be nested, as long as EndUpdate is called once per BeginUpdate.
+    */
+  virtual nsresult EndUpdate()=0;
+
 
 };
 
