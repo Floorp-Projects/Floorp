86c9f5ca8dbddfb4c676f53abfc816795f3cc657
nhotta%netscape.com <nhotta%netscape.com>
1999-01-21 01:28:27 +0000

    Initial check in.

---
+++ intl/locale/src/windows/nsLocaleFactoryWin.cpp
@@ -0,0 +1,173 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "nscore.h"
+#include "nsISupports.h"
+#include "nsIFactory.h"
+#include "nsCollationWin.h"
+#include "nsDateTimeFormatWin.h"
+
+
+NS_DEFINE_IID(kISupportsIID, NS_ISUPPORTS_IID);
+NS_DEFINE_IID(kIFactoryIID,  NS_IFACTORY_IID);
+NS_DEFINE_IID(kICollationFactoryIID, NS_ICOLLATIONFACTORY_IID);                                                         
+NS_DEFINE_IID(kICollationIID, NS_ICOLLATION_IID);                                                         
+NS_DEFINE_IID(kIDateTimeFormatIID, NS_IDATETIMEFORMAT_IID);
+
+
+class nsLocaleWinFactory : public nsIFactory
+{   
+  public:   
+    // nsISupports methods   
+    NS_IMETHOD QueryInterface(const nsIID &aIID,    
+                              void **aResult);   
+    NS_IMETHOD_(nsrefcnt) AddRef(void);   
+    NS_IMETHOD_(nsrefcnt) Release(void);   
+
+    // nsIFactory methods   
+    NS_IMETHOD CreateInstance(nsISupports *aOuter,   
+                              const nsIID &aIID,   
+                              void **aResult);   
+
+    NS_IMETHOD LockFactory(PRBool aLock);   
+
+    nsLocaleWinFactory(const nsCID &aClass);   
+    ~nsLocaleWinFactory();   
+
+  private:   
+    nsrefcnt  mRefCnt;   
+    nsCID     mClassID;
+};
+
+nsLocaleWinFactory::nsLocaleWinFactory(const nsCID &aClass)   
+{   
+  mRefCnt = 0;
+  mClassID = aClass;
+}   
+
+nsLocaleWinFactory::~nsLocaleWinFactory()   
+{   
+  NS_ASSERTION(mRefCnt == 0, "non-zero refcnt at destruction");   
+}   
+
+nsresult nsLocaleWinFactory::QueryInterface(const nsIID &aIID,   
+                                      void **aResult)   
+{   
+  if (aResult == NULL) {   
+    return NS_ERROR_NULL_POINTER;   
+  }   
+
+  // Always NULL result, in case of failure   
+  *aResult = NULL;   
+
+  if (aIID.Equals(kISupportsIID)) {   
+    *aResult = (void *)(nsISupports*)this;   
+  } else if (aIID.Equals(kIFactoryIID)) {   
+    *aResult = (void *)(nsIFactory*)this;   
+  }   
+
+  if (*aResult == NULL) {   
+    return NS_NOINTERFACE;   
+  }   
+
+  NS_ADDREF_THIS(); // Increase reference count for caller   
+  return NS_OK;   
+}   
+
+nsrefcnt nsLocaleWinFactory::AddRef()   
+{   
+  return ++mRefCnt;   
+}   
+
+nsrefcnt nsLocaleWinFactory::Release()   
+{   
+  if (--mRefCnt == 0) {   
+    delete this;   
+    return 0; // Don't access mRefCnt after deleting!   
+  }   
+  return mRefCnt;   
+}  
+
+nsresult nsLocaleWinFactory::CreateInstance(nsISupports *aOuter,  
+                                         const nsIID &aIID,  
+                                         void **aResult)  
+{
+  if (aResult == NULL) {  
+    return NS_ERROR_NULL_POINTER;  
+  }  
+
+  *aResult = NULL;  
+
+  nsISupports *inst = NULL;
+
+  if (aIID.Equals(kICollationFactoryIID)) {
+     NS_NEWXPCOM(inst, nsCollationFactory);
+  }
+  else if (aIID.Equals(kICollationIID)) {
+     NS_NEWXPCOM(inst, nsCollationWin);
+  }
+  else if (aIID.Equals(kIDateTimeFormatIID)) {
+     NS_NEWXPCOM(inst, nsDateTimeFormatWin);
+  }
+  else 
+  {
+    return NS_NOINTERFACE;
+  }
+
+  if (NULL == inst) {
+    return NS_ERROR_OUT_OF_MEMORY;  
+  }
+  
+  nsresult res = inst->QueryInterface(aIID, aResult);
+  
+  if(NS_FAILED(res)) {
+    delete inst;
+  }
+
+  return res;
+}
+
+nsresult nsLocaleWinFactory::LockFactory(PRBool aLock)  
+{  
+  // Not implemented in simplest case.  
+  return NS_OK;
+}  
+
+// return the proper factory to the caller
+//
+extern "C" NS_EXPORT nsresult NSGetFactory(const nsCID &aCID, nsIFactory **aFactory)
+{
+  if (NULL == aFactory) {
+    return NS_ERROR_NULL_POINTER;
+  }
+
+  nsIFactory *inst = NULL;
+
+  inst = new nsLocaleWinFactory(aCID);
+  if(NULL == inst) {
+    return NS_ERROR_OUT_OF_MEMORY;  
+  }
+
+  nsresult res = inst->QueryInterface(kIFactoryIID, (void**)aFactory);
+  if (NS_FAILED(res)) {
+    *aFactory = NULL;
+    delete inst;
+  }
+
+  return res;
+}
