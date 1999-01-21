759e9adfd343e49a90d20fde9a625b6c2eba76ef
rpotts@netscape.com <rpotts@netscape.com>
1999-01-21 07:33:32 +0000

    CIDs for browser components...

---
+++ xpfe/browser/public/nsBrowserCIDs.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+#ifndef nsBrowserCIDs_h__
+#define nsBrowserCIDs_h__
+
+
+// XXX: BOGUS 43147b80-8a39-11d2-9938-0080c7cb1081
+#define NS_BROWSERCONTROLLER_CID \
+{ 0x43147b80, 0x8a39, 0x11d2, \
+  {0x99, 0x38, 0x00, 0x80, 0xc7, 0xcb, 0x10, 0x81} }
+
+
+#endif /* nsBrowserCIDs_h__ */
