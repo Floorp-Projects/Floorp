317d0d7c4e97a9617af071f99e8ff85a24e41c95
harishd%netscape.com <harishd%netscape.com>
1999-01-22 22:25:00 +0000

    Passing stylecontext into Paintborder()

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -3975,7 +3975,7 @@ nsBaseIBFrame::Paint(nsIPresContext&      aPresContext,
     nsCSSRendering::PaintBackground(aPresContext, aRenderingContext, this,
                                     aDirtyRect, rect, *color, *spacing, 0, 0);
     nsCSSRendering::PaintBorder(aPresContext, aRenderingContext, this,
-                                aDirtyRect, rect, *spacing, skipSides);
+                                aDirtyRect, rect, *spacing, mStyleContext, skipSides);
   }
 
   // If overflow is hidden then set the clip rect so that children
