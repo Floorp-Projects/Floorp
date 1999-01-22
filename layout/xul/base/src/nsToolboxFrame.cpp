cb51ba1c2a94a16f5265c7c68ab66b433d2c6cab
harishd@netscape.com <harishd@netscape.com>
1999-01-22 22:38:34 +0000

    StyleContext passed onto PaintBorder()

--- layout/xul/base/src/nsToolboxFrame.cpp
+++ layout/xul/base/src/nsToolboxFrame.cpp
@@ -96,7 +96,7 @@ nsToolboxFrame :: Paint ( nsIPresContext& aPresContext,
       nsCSSRendering::PaintBackground(aPresContext, aRenderingContext, this,
                                       aDirtyRect, rect, *color, *spacing, 0, 0);
       nsCSSRendering::PaintBorder(aPresContext, aRenderingContext, this,
-                                  aDirtyRect, rect, *spacing, skipSides);
+                                  aDirtyRect, rect, *spacing, mStyleContext, skipSides);
     }
   }
 
