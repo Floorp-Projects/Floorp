d506b30655e1995735a2737ffc538c7a1565e2a7
alecf%netscape.com <alecf%netscape.com>
1999-01-23 02:40:27 +0000

    add new nsINNTPCategoryContainer interface

---
+++ mailnews/news/public/nsINNTPCategoryContainer.h
@@ -0,0 +1,35 @@
+/*
+ * DO NOT EDIT.  THIS FILE IS GENERATED FROM nsINNTPCategoryContainer.idl
+ */
+
+#ifndef __gen_nsINNTPCategoryContainer_h__
+#define __gen_nsINNTPCategoryContainer_h__
+
+#include "nsISupports.h" /* interface nsISupports */
+#include "nsINNTPNewsgroup.h" /* interface nsINNTPNewsgroup */
+
+
+/* starting interface nsINNTPCategoryContainer */
+
+/* {cb36d510-b249-11d2-b7ef-00805f05ffa5} */
+#define NS_INNTPCATEGORYCONTAINER_IID_STR "cb36d510-b249-11d2-b7ef-00805f05ffa5"
+#define NS_INNTPCATEGORYCONTAINER_IID \
+  {0xcb36d510, 0xb249, 0x11d2, \
+    { 0xb7, 0xef, 0x00, 0x80, 0x5f, 0x05, 0xff, 0xa5 }}
+
+class nsINNTPCategoryContainer : public nsISupports {
+ private:
+  void operator delete(void *); // NOT TO BE IMPLEMENTED
+
+ public: 
+  static const nsIID& IID() {
+    static nsIID iid = NS_INNTPCATEGORYCONTAINER_IID;
+    return iid;
+  }
+
+  /*  <IDL>  */
+  NS_IMETHOD GetRootCategory(nsINNTPNewsgroup * *aRootCategory) = 0;
+  NS_IMETHOD SetRootCategory(nsINNTPNewsgroup * aRootCategory) = 0;
+};
+
+#endif /* __gen_nsINNTPCategoryContainer_h__ */
