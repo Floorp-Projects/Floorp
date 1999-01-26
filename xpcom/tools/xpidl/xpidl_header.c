a39aee01fcce12153b27e1f50a9131a9b82d3838
alecf%netscape.com <alecf%netscape.com>
1999-01-26 18:20:30 +0000

    stop using IsXXX for boolean Getters

--- xpcom/tools/xpidl/xpidl_header.c
+++ xpcom/tools/xpidl/xpidl_header.c
@@ -331,11 +331,6 @@ static gboolean
 attr_accessor(TreeState *state, gboolean getter)
 {
     char *attrname = ATTR_IDENT(state->tree).str;
-    if (getter && (ATTR_TYPE(state->tree) == IDLN_TYPE_BOOLEAN)) {
-        fprintf(state->file, "  NS_IMETHOD Is%c%s(PRBool *aIs%c%s) = 0;\n",
-                toupper(attrname[0]), attrname + 1,
-                toupper(attrname[0]), attrname + 1);
-    } else {
         IDL_tree orig = state->tree;
         fprintf(state->file, "  NS_IMETHOD %cet%c%s(",
                 getter ? 'G' : 'S',
@@ -348,7 +343,6 @@ attr_accessor(TreeState *state, gboolean getter)
                 (STARRED_TYPE(orig) ? "" : " "),
                 getter ? "*" : "",
                 toupper(attrname[0]), attrname + 1);
-    }
     return TRUE;
 }
 
