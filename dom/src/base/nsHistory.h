a9464f8dbb0922d39029f22673762558d345a0d0
joki@netscape.com <joki@netscape.com>
1999-01-28 22:24:29 +0000

    Adding new files for window.history, window.screen

---
+++ dom/src/base/nsHistory.h
@@ -0,0 +1,55 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+#ifndef nsHistory_h___
+#define nsHistory_h___
+
+#include "nsIScriptObjectOwner.h"
+#include "nsIDOMHistory.h"
+#include "nsISupports.h"
+#include "nscore.h"
+#include "nsIScriptContext.h"
+
+class nsIWebShell;
+
+// Script "History" object
+class HistoryImpl : public nsIScriptObjectOwner, public nsIDOMHistory {
+public:
+  HistoryImpl();
+  ~HistoryImpl();
+
+  NS_DECL_ISUPPORTS
+
+  NS_IMETHOD GetScriptObject(nsIScriptContext *aContext, void** aScriptObject);
+  NS_IMETHOD SetScriptObject(void *aScriptObject);
+
+  NS_IMETHOD_(void)       SetWebShell(nsIWebShell *aWebShell);
+
+  NS_IMETHOD    GetLength(PRInt32* aLength);
+  NS_IMETHOD    GetCurrent(nsString& aCurrent);
+  NS_IMETHOD    GetPrevious(nsString& aPrevious);
+  NS_IMETHOD    GetNext(nsString& aNext);
+  NS_IMETHOD    Back();
+  NS_IMETHOD    Forward();
+  NS_IMETHOD    Go(PRInt32 aIndex);
+
+protected:
+  nsIWebShell* mWebShell;
+  void *mScriptObject;
+};
+
+#endif /* nsHistory_h___ */
---
+++ dom/src/base/nsJSScreen.cpp
@@ -0,0 +1,376 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+/* AUTO-GENERATED. DO NOT EDIT!!! */
+
+#include "jsapi.h"
+#include "nsJSUtils.h"
+#include "nscore.h"
+#include "nsIScriptContext.h"
+#include "nsIJSScriptObject.h"
+#include "nsIScriptObjectOwner.h"
+#include "nsIScriptGlobalObject.h"
+#include "nsIPtr.h"
+#include "nsString.h"
+#include "nsIDOMScreen.h"
+
+
+static NS_DEFINE_IID(kIScriptObjectOwnerIID, NS_ISCRIPTOBJECTOWNER_IID);
+static NS_DEFINE_IID(kIJSScriptObjectIID, NS_IJSSCRIPTOBJECT_IID);
+static NS_DEFINE_IID(kIScriptGlobalObjectIID, NS_ISCRIPTGLOBALOBJECT_IID);
+static NS_DEFINE_IID(kIScreenIID, NS_IDOMSCREEN_IID);
+
+NS_DEF_PTR(nsIDOMScreen);
+
+//
+// Screen property ids
+//
+enum Screen_slots {
+  SCREEN_WIDTH = -1,
+  SCREEN_HEIGHT = -2,
+  SCREEN_PIXELDEPTH = -3,
+  SCREEN_COLORDEPTH = -4,
+  SCREEN_AVAILWIDTH = -5,
+  SCREEN_AVAILHEIGHT = -6,
+  SCREEN_AVAILLEFT = -7,
+  SCREEN_AVAILTOP = -8
+};
+
+/***********************************************************************/
+//
+// Screen Properties Getter
+//
+PR_STATIC_CALLBACK(JSBool)
+GetScreenProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+  nsIDOMScreen *a = (nsIDOMScreen*)JS_GetPrivate(cx, obj);
+
+  // If there's no private data, this must be the prototype, so ignore
+  if (nsnull == a) {
+    return JS_TRUE;
+  }
+
+  if (JSVAL_IS_INT(id)) {
+    switch(JSVAL_TO_INT(id)) {
+      case SCREEN_WIDTH:
+      {
+        PRInt32 prop;
+        if (NS_OK == a->GetWidth(&prop)) {
+          *vp = INT_TO_JSVAL(prop);
+        }
+        else {
+          return JS_FALSE;
+        }
+        break;
+      }
+      case SCREEN_HEIGHT:
+      {
+        PRInt32 prop;
+        if (NS_OK == a->GetHeight(&prop)) {
+          *vp = INT_TO_JSVAL(prop);
+        }
+        else {
+          return JS_FALSE;
+        }
+        break;
+      }
+      case SCREEN_PIXELDEPTH:
+      {
+        PRInt32 prop;
+        if (NS_OK == a->GetPixelDepth(&prop)) {
+          *vp = INT_TO_JSVAL(prop);
+        }
+        else {
+          return JS_FALSE;
+        }
+        break;
+      }
+      case SCREEN_COLORDEPTH:
+      {
+        PRInt32 prop;
+        if (NS_OK == a->GetColorDepth(&prop)) {
+          *vp = INT_TO_JSVAL(prop);
+        }
+        else {
+          return JS_FALSE;
+        }
+        break;
+      }
+      case SCREEN_AVAILWIDTH:
+      {
+        PRInt32 prop;
+        if (NS_OK == a->GetAvailWidth(&prop)) {
+          *vp = INT_TO_JSVAL(prop);
+        }
+        else {
+          return JS_FALSE;
+        }
+        break;
+      }
+      case SCREEN_AVAILHEIGHT:
+      {
+        PRInt32 prop;
+        if (NS_OK == a->GetAvailHeight(&prop)) {
+          *vp = INT_TO_JSVAL(prop);
+        }
+        else {
+          return JS_FALSE;
+        }
+        break;
+      }
+      case SCREEN_AVAILLEFT:
+      {
+        PRInt32 prop;
+        if (NS_OK == a->GetAvailLeft(&prop)) {
+          *vp = INT_TO_JSVAL(prop);
+        }
+        else {
+          return JS_FALSE;
+        }
+        break;
+      }
+      case SCREEN_AVAILTOP:
+      {
+        PRInt32 prop;
+        if (NS_OK == a->GetAvailTop(&prop)) {
+          *vp = INT_TO_JSVAL(prop);
+        }
+        else {
+          return JS_FALSE;
+        }
+        break;
+      }
+      default:
+        return nsJSUtils::nsCallJSScriptObjectGetProperty(a, cx, id, vp);
+    }
+  }
+  else {
+    return nsJSUtils::nsCallJSScriptObjectGetProperty(a, cx, id, vp);
+  }
+
+  return PR_TRUE;
+}
+
+/***********************************************************************/
+//
+// Screen Properties Setter
+//
+PR_STATIC_CALLBACK(JSBool)
+SetScreenProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+  nsIDOMScreen *a = (nsIDOMScreen*)JS_GetPrivate(cx, obj);
+
+  // If there's no private data, this must be the prototype, so ignore
+  if (nsnull == a) {
+    return JS_TRUE;
+  }
+
+  if (JSVAL_IS_INT(id)) {
+    switch(JSVAL_TO_INT(id)) {
+      case 0:
+      default:
+        return nsJSUtils::nsCallJSScriptObjectSetProperty(a, cx, id, vp);
+    }
+  }
+  else {
+    return nsJSUtils::nsCallJSScriptObjectSetProperty(a, cx, id, vp);
+  }
+
+  return PR_TRUE;
+}
+
+
+//
+// Screen finalizer
+//
+PR_STATIC_CALLBACK(void)
+FinalizeScreen(JSContext *cx, JSObject *obj)
+{
+  nsJSUtils::nsGenericFinalize(cx, obj);
+}
+
+
+//
+// Screen enumerate
+//
+PR_STATIC_CALLBACK(JSBool)
+EnumerateScreen(JSContext *cx, JSObject *obj)
+{
+  return nsJSUtils::nsGenericEnumerate(cx, obj);
+}
+
+
+//
+// Screen resolve
+//
+PR_STATIC_CALLBACK(JSBool)
+ResolveScreen(JSContext *cx, JSObject *obj, jsval id)
+{
+  return nsJSUtils::nsGenericResolve(cx, obj, id);
+}
+
+
+/***********************************************************************/
+//
+// class for Screen
+//
+JSClass ScreenClass = {
+  "Screen", 
+  JSCLASS_HAS_PRIVATE,
+  JS_PropertyStub,
+  JS_PropertyStub,
+  GetScreenProperty,
+  SetScreenProperty,
+  EnumerateScreen,
+  ResolveScreen,
+  JS_ConvertStub,
+  FinalizeScreen
+};
+
+
+//
+// Screen class properties
+//
+static JSPropertySpec ScreenProperties[] =
+{
+  {"width",    SCREEN_WIDTH,    JSPROP_ENUMERATE | JSPROP_READONLY},
+  {"height",    SCREEN_HEIGHT,    JSPROP_ENUMERATE | JSPROP_READONLY},
+  {"pixelDepth",    SCREEN_PIXELDEPTH,    JSPROP_ENUMERATE | JSPROP_READONLY},
+  {"colorDepth",    SCREEN_COLORDEPTH,    JSPROP_ENUMERATE | JSPROP_READONLY},
+  {"availWidth",    SCREEN_AVAILWIDTH,    JSPROP_ENUMERATE | JSPROP_READONLY},
+  {"availHeight",    SCREEN_AVAILHEIGHT,    JSPROP_ENUMERATE | JSPROP_READONLY},
+  {"availLeft",    SCREEN_AVAILLEFT,    JSPROP_ENUMERATE | JSPROP_READONLY},
+  {"availTop",    SCREEN_AVAILTOP,    JSPROP_ENUMERATE | JSPROP_READONLY},
+  {0}
+};
+
+
+//
+// Screen class methods
+//
+static JSFunctionSpec ScreenMethods[] = 
+{
+  {0}
+};
+
+
+//
+// Screen constructor
+//
+PR_STATIC_CALLBACK(JSBool)
+Screen(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+  return JS_FALSE;
+}
+
+
+//
+// Screen class initialization
+//
+nsresult NS_InitScreenClass(nsIScriptContext *aContext, void **aPrototype)
+{
+  JSContext *jscontext = (JSContext *)aContext->GetNativeContext();
+  JSObject *proto = nsnull;
+  JSObject *constructor = nsnull;
+  JSObject *parent_proto = nsnull;
+  JSObject *global = JS_GetGlobalObject(jscontext);
+  jsval vp;
+
+  if ((PR_TRUE != JS_LookupProperty(jscontext, global, "Screen", &vp)) ||
+      !JSVAL_IS_OBJECT(vp) ||
+      ((constructor = JSVAL_TO_OBJECT(vp)) == nsnull) ||
+      (PR_TRUE != JS_LookupProperty(jscontext, JSVAL_TO_OBJECT(vp), "prototype", &vp)) || 
+      !JSVAL_IS_OBJECT(vp)) {
+
+    proto = JS_InitClass(jscontext,     // context
+                         global,        // global object
+                         parent_proto,  // parent proto 
+                         &ScreenClass,      // JSClass
+                         Screen,            // JSNative ctor
+                         0,             // ctor args
+                         ScreenProperties,  // proto props
+                         ScreenMethods,     // proto funcs
+                         nsnull,        // ctor props (static)
+                         nsnull);       // ctor funcs (static)
+    if (nsnull == proto) {
+      return NS_ERROR_FAILURE;
+    }
+
+  }
+  else if ((nsnull != constructor) && JSVAL_IS_OBJECT(vp)) {
+    proto = JSVAL_TO_OBJECT(vp);
+  }
+  else {
+    return NS_ERROR_FAILURE;
+  }
+
+  if (aPrototype) {
+    *aPrototype = proto;
+  }
+  return NS_OK;
+}
+
+
+//
+// Method for creating a new Screen JavaScript object
+//
+extern "C" NS_DOM nsresult NS_NewScriptScreen(nsIScriptContext *aContext, nsISupports *aSupports, nsISupports *aParent, void **aReturn)
+{
+  NS_PRECONDITION(nsnull != aContext && nsnull != aSupports && nsnull != aReturn, "null argument to NS_NewScriptScreen");
+  JSObject *proto;
+  JSObject *parent;
+  nsIScriptObjectOwner *owner;
+  JSContext *jscontext = (JSContext *)aContext->GetNativeContext();
+  nsresult result = NS_OK;
+  nsIDOMScreen *aScreen;
+
+  if (nsnull == aParent) {
+    parent = nsnull;
+  }
+  else if (NS_OK == aParent->QueryInterface(kIScriptObjectOwnerIID, (void**)&owner)) {
+    if (NS_OK != owner->GetScriptObject(aContext, (void **)&parent)) {
+      NS_RELEASE(owner);
+      return NS_ERROR_FAILURE;
+    }
+    NS_RELEASE(owner);
+  }
+  else {
+    return NS_ERROR_FAILURE;
+  }
+
+  if (NS_OK != NS_InitScreenClass(aContext, (void **)&proto)) {
+    return NS_ERROR_FAILURE;
+  }
+
+  result = aSupports->QueryInterface(kIScreenIID, (void **)&aScreen);
+  if (NS_OK != result) {
+    return result;
+  }
+
+  // create a js object for this class
+  *aReturn = JS_NewObject(jscontext, &ScreenClass, proto, parent);
+  if (nsnull != *aReturn) {
+    // connect the native object to the js object
+    JS_SetPrivate(jscontext, (JSObject *)*aReturn, aScreen);
+  }
+  else {
+    NS_RELEASE(aScreen);
+    return NS_ERROR_FAILURE; 
+  }
+
+  return NS_OK;
+}
