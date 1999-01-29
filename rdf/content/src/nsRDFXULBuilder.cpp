4d61f00b094603766823484923dab752c0f6cf3b
waterson@netscape.com <waterson@netscape.com>
1999-01-29 00:41:42 +0000

    Added nsRDFXULBuilder.cpp; this is not currently in the build.

---
+++ rdf/content/src/nsRDFXULBuilder.cpp
@@ -0,0 +1,144 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+/*
+
+  An nsIRDFDocument implementation that builds a XUL content model.
+  
+
+ */
+
+#include "nsDebug.h"
+#include "nsIAtom.h"
+#include "nsIDocument.h"
+#include "nsIRDFContent.h"
+#include "nsIRDFContentModelBuilder.h"
+#include "nsIRDFCursor.h"
+#include "nsIRDFCompositeDataSource.h"
+#include "nsIRDFDocument.h"
+#include "nsIRDFNode.h"
+#include "nsIRDFService.h"
+#include "nsIServiceManager.h"
+#include "nsINameSpaceManager.h"
+#include "nsIServiceManager.h"
+#include "nsISupportsArray.h"
+#include "nsRDFCID.h"
+#include "nsRDFContentUtils.h"
+#include "nsString.h"
+#include "rdf.h"
+#include "rdfutil.h"
+
+////////////////////////////////////////////////////////////////////////
+
+static NS_DEFINE_IID(kIDocumentIID,               NS_IDOCUMENT_IID);
+static NS_DEFINE_IID(kIRDFResourceIID,            NS_IRDFRESOURCE_IID);
+static NS_DEFINE_IID(kIRDFLiteralIID,             NS_IRDFLITERAL_IID);
+static NS_DEFINE_IID(kIRDFContentIID,             NS_IRDFCONTENT_IID);
+static NS_DEFINE_IID(kIRDFContentModelBuilderIID, NS_IRDFCONTENTMODELBUILDER_IID);
+static NS_DEFINE_IID(kIRDFServiceIID,             NS_IRDFSERVICE_IID);
+
+static NS_DEFINE_CID(kRDFServiceCID,              NS_RDFSERVICE_CID);
+
+////////////////////////////////////////////////////////////////////////
+
+class RDFXULBuilderImpl : public nsIRDFContentModelBuilder
+{
+public:
+    RDFXULBuilderImpl();
+    virtual ~RDFXULBuilderImpl();
+
+    // nsISupports interface
+    NS_DECL_ISUPPORTS
+
+    // nsIRDFContentModelBuilder interface
+    NS_IMETHOD SetDocument(nsIRDFDocument* aDocument);
+    NS_IMETHOD CreateRoot(nsIRDFResource* aResource);
+    NS_IMETHOD OnAssert(nsIRDFContent* aElement, nsIRDFResource* aProperty, nsIRDFNode* aValue);
+    NS_IMETHOD OnUnassert(nsIRDFContent* aElement, nsIRDFResource* aProperty, nsIRDFNode* aValue);
+};
+
+////////////////////////////////////////////////////////////////////////
+
+nsresult
+NS_NewRDFXULBuilder(nsIRDFContentModelBuilder** result)
+{
+    NS_PRECONDITION(result != nsnull, "null ptr");
+    if (! result)
+        return NS_ERROR_NULL_POINTER;
+
+    RDFXULBuilderImpl* builder = new RDFXULBuilderImpl();
+    if (! builder)
+        return NS_ERROR_OUT_OF_MEMORY;
+
+    NS_ADDREF(builder);
+    *result = builder;
+    return NS_OK;
+}
+
+
+
+RDFXULBuilderImpl::RDFXULBuilderImpl(void)
+{
+    NS_INIT_REFCNT();
+}
+
+RDFXULBuilderImpl::~RDFXULBuilderImpl(void)
+{
+}
+
+////////////////////////////////////////////////////////////////////////
+
+NS_IMPL_ISUPPORTS(RDFXULBuilderImpl, kIRDFContentModelBuilderIID);
+
+////////////////////////////////////////////////////////////////////////
+// nsIRDFContentModelBuilder methods
+
+NS_IMETHODIMP
+RDFXULBuilderImpl::SetDocument(nsIRDFDocument* aDocument)
+{
+    NS_NOTYETIMPLEMENTED("write me!");
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+NS_IMETHODIMP
+RDFXULBuilderImpl::CreateRoot(nsIRDFResource* aResource)
+{
+    NS_NOTYETIMPLEMENTED("write me!");
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+
+NS_IMETHODIMP
+RDFXULBuilderImpl::OnAssert(nsIRDFContent* parent,
+                             nsIRDFResource* property,
+                             nsIRDFNode* value)
+{
+    NS_NOTYETIMPLEMENTED("write me!");
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+
+NS_IMETHODIMP
+RDFXULBuilderImpl::OnUnassert(nsIRDFContent* aElement,
+                               nsIRDFResource* aProperty,
+                               nsIRDFNode* aValue)
+{
+    NS_NOTYETIMPLEMENTED("write me!");
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
