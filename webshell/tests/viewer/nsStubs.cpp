5cfdb48fafac5bef710e2dac6365f2e256a08476
mcafee@netscape.com <mcafee@netscape.com>
1999-02-01 11:06:22 +0000

    Adding license; moved XP_Rebuffer() and XP_Trace() stubs to lib/xp/xp_stubs.c

--- webshell/tests/viewer/nsStubs.cpp
+++ webshell/tests/viewer/nsStubs.cpp
@@ -1,5 +1,20 @@
-
-
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
 
 #include "xp_mcom.h"
 #include "net.h"
@@ -11,6 +26,11 @@ extern "C" XP_Bool ValidateDocData(MWContext *window_id)
   printf("ValidateDocData not implemented, stubbed in webshell/tests/viewer/nsStubs.cpp\n"); 
   return PR_TRUE; 
 }
+
+/* Unix: Moved these stubs to lib/xp/xp_stubs.c so we don't keep
+   copying this stub function around.  Mac & Win32 should follow suit. */
+
+#ifndef XP_UNIX
 
 /* dist/public/xp/xp_linebuf.h */
 extern "C" int XP_ReBuffer (const char *net_buffer, int32 net_buffer_size,
@@ -28,14 +48,12 @@ extern "C" int XP_ReBuffer (const char *net_buffer, int32 net_buffer_size,
 }
 
 
-
 /* mozilla/include/xp_trace.h */
 
 extern "C" void XP_Trace( const char *, ... ) 
 { 
   printf("XP_Trace not implemented, stubbed in webshell/tests/viewer/nsStubs.cpp\n"); 
 
-
 } 
 
-
+#endif
