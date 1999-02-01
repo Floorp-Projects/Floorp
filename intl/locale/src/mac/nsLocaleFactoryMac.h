492f8339a646a59139326460c9b2a67530ac46a3
tague@netscape.com <tague@netscape.com>
1999-02-01 04:32:55 +0000

    First Checked In.

---
+++ intl/locale/src/mac/nsLocaleFactoryMac.h
@@ -0,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "nscore.h"
+#include "nsISupports.h"
+#include "nsIFactory.h"
+#include "nsCollationWin.h"
+#include "nsDateTimeFormatWin.h"
+
+
+class nsLocaleMacFactory : public nsIFactory
+{   
+  public:   
+    // nsISupports methods   
+    NS_IMETHOD QueryInterface(const nsIID &aIID,    
+                              void **aResult);   
+    NS_IMETHOD_(nsrefcnt) AddRef(void);   
+    NS_IMETHOD_(nsrefcnt) Release(void);   
+
+    // nsIFactory methods   
+    NS_IMETHOD CreateInstance(nsISupports *aOuter,   
+                              const nsIID &aIID,   
+                              void **aResult);   
+
+    NS_IMETHOD LockFactory(PRBool aLock);   
+
+    nsLocaleMacFactory(const nsCID &aClass);   
+    ~nsLocaleMacFactory();   
+
+  private:   
+    nsrefcnt  mRefCnt;   
+    nsCID     mClassID;
+};
