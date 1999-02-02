eb5d84d267053300028b2e4af223adfaeee34ce6
rhp%netscape.com <rhp%netscape.com>
1999-02-02 01:10:00 +0000

    New files for accessing XP-COM interface for libmime internals

---
+++ mailnews/mime/cthandlers/calendar/mimexpcom.h
@@ -0,0 +1,107 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/* 
+ * This is the definitions for the Content Type Handler plugins to 
+ * access internals of libmime via XP-COM calls
+ */ 
+#ifndef _MIMEXPCOM_H_
+#define _MIMEXPCOM_H_
+
+/*
+  This header exposes functions that are necessary to access the 
+  object heirarchy for the mime chart. The class hierarchy is:
+
+     MimeObject (abstract)
+      |
+      |--- MimeContainer (abstract)
+      |     |
+      |     |--- MimeMultipart (abstract)
+      |     |     |
+      |     |     |--- MimeMultipartMixed
+      |     |     |
+      |     |     |--- MimeMultipartDigest
+      |     |     |
+      |     |     |--- MimeMultipartParallel
+      |     |     |
+      |     |     |--- MimeMultipartAlternative
+      |     |     |
+      |     |     |--- MimeMultipartRelated
+      |     |     |
+      |     |     |--- MimeMultipartAppleDouble
+      |     |     |
+      |     |     |--- MimeSunAttachment
+      |     |     |
+      |     |     |--- MimeMultipartSigned (abstract)
+      |     |          |
+      |     |          |--- MimeMultipartSigned
+      |     |
+      |     |--- MimeXlateed (abstract)
+      |     |     |
+      |     |     |--- MimeXlateed
+      |     |
+      |     |--- MimeMessage
+      |     |
+      |     |--- MimeUntypedText
+      |
+      |--- MimeLeaf (abstract)
+      |     |
+      |     |--- MimeInlineText (abstract)
+      |     |     |
+      |     |     |--- MimeInlineTextPlain
+      |     |     |
+      |     |     |--- MimeInlineTextHTML
+      |     |     |
+      |     |     |--- MimeInlineTextRichtext
+      |     |     |     |
+      |     |     |     |--- MimeInlineTextEnriched
+      |     |	    |
+      |     |	    |--- MimeInlineTextVCard
+      |     |	    |
+      |     |	    |--- MimeInlineTextCalendar
+      |     |
+      |     |--- MimeInlineImage
+      |     |
+      |     |--- MimeExternalObject
+      |
+      |--- MimeExternalBody
+ */
+
+/*
+ * These functions are exposed by libmime to be used by content type
+ * handler plugins for processing stream data. 
+ */
+/*
+ * This is the write call for outputting processed stream data.
+ */ 
+extern int  COM_MimeObject_write(void *mimeObject, char *data, 
+                                      PRInt32 length, 
+                                      PRBool user_visible_p);
+/*
+ * The following group of calls expose the pointers for the object
+ * system within libmime. 
+ */                                                        
+extern void *COM_GetmimeInlineTextClass(void);
+extern void *COM_GetmimeLeafClass(void);
+extern void *COM_GetmimeObjectClass(void);
+extern void *COM_GetmimeContainerClass(void);
+extern void *COM_GetmimeMultipartClass(void);
+extern void *COM_GetmimeMultipartSignedClass(void);
+
+
+#endif /* _MIMEXPCOM_H_ */
