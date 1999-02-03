42b51c73c628d1f6aa8dd914dc5ba7e458d038d6
racham%netscape.com <racham%netscape.com>
1999-02-03 16:34:01 +0000

    Root property sheet header file

---
+++ cck/driver/PropSheet.h
@@ -0,0 +1,58 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+// PropSheet.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CPropSheet
+#include "WizardUI.h"
+
+class CPropSheet : public CPropertySheet
+{
+	DECLARE_DYNAMIC(CPropSheet)
+
+// Construction
+public:
+	CPropSheet(UINT nIDCaption, CWnd* pParentWnd = NULL, UINT iSelectPage = 0);
+	CPropSheet(LPCTSTR pszCaption, CWnd* pParentWnd = NULL, UINT iSelectPage = 0);
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CPropSheet)
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CPropSheet();
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CPropSheet)
+		// NOTE - the ClassWizard will add and remove member functions here.
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
