0e426c6cfab6b1a3a6afc83365a0ad39c259280b
jefft%netscape.com <jefft%netscape.com>
1999-02-04 22:25:04 +0000

    added nsIPop3Sink.h & nsIPop3URL.h

---
+++ mailnews/local/public/nsIPop3Sink.h
@@ -0,0 +1,58 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsIPop3Sink_h__
+#define nsIPop3Sink_h__
+
+#include "nsISupports.h"
+
+/* starting interface nsIPop3Sink */
+
+/* {EC54D490-BB00-11D2-AB5E-00805F8AC968} */
+#define NS_IPOP3SINK_IID_STR "EC54D490-BB00-11D2-AB5E-00805F8AC968"
+#define NS_IPOP3SINK_IID \
+  {0xEC54D490, 0xBB00, 0x11D2, \
+			{0xAB, 0x5E, 0x00, 0x80, 0x5F, 0x8A, 0xC9, 0x68}}
+
+class nsIPop3Sink : public nsISupports 
+{
+public:
+		static const nsIID& IID() {
+				static nsIID iid = NS_IPOP3SINK_IID;
+				return iid;
+		}
+
+    NS_IMETHOD SetUserAuthenticated(PRBool authed) = 0;
+    NS_IMETHOD SetMailAccountURL(const char* urlString) = 0;
+    NS_IMETHOD BeginMailDelivery(PRBool *aBool) = 0;
+    NS_IMETHOD EndMailDelivery() = 0;
+    NS_IMETHOD AbortMailDelivery() = 0;
+    NS_IMETHOD IncorporateBegin(const char* uidlString, nsIURL* aURL, 
+                                PRUint32 flags, void** closure) = 0;
+    NS_IMETHOD IncorporateWrite(void* closure, const char* block, 
+                                PRInt32 length) = 0;
+    NS_IMETHOD IncorporateComplete(void* closure) = 0;
+    NS_IMETHOD IncorporateAbort(void* closure, PRInt32 status) = 0;
+    NS_IMETHOD BiffGetNewMail() = 0;
+    NS_IMETHOD SetBiffStateAndUpdateFE(PRUint32 aBiffState) = 0;
+    NS_IMETHOD SetSenderAuthedFlag(void* closure, PRBool authed) = 0;
+    NS_IMETHOD SetMailDirectory(const char* mailDirectory) = 0;
+    NS_IMETHOD GetMailDirectory(const char** mailDirectory) = 0;
+};
+
+#endif /*  nsIPop3Sink_h__ */
---
+++ mailnews/local/public/nsIPop3URL.h
@@ -0,0 +1,56 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsIPop3URL_h___
+#define nsIPop3URL_h___
+
+#include "nscore.h"
+#include "nsIURL.h"
+#include "nsIPop3Sink.h"
+
+#include "nsISupports.h"
+
+/* include all of our event sink interfaces */
+
+/* 73c043d0-b7e2-11d2-ab5c-00805f8ac968 */
+
+#define NS_IPOP3URL_IID                         \
+{ 0x73c043d0, 0xb7e2, 0x11d2,                   \
+    { 0xab, 0x5c, 0x0, 0x80, 0x5f, 0x8a, 0xc9, 0x68 } }
+
+class nsIPop3URL : public nsIURL
+{
+public:
+	/////////////////////////////////////////////////////////////////////////////// 
+	// Getters and Setters for the news specific event sinks to bind to to your url
+	///////////////////////////////////////////////////////////////////////////////
+
+	// mscott: this interface really belongs in nsIURL and I will move it there after talking
+	// it over with core netlib. This error message replaces the err_msg which was in the 
+	// old URL_struct. Also, it should probably be a nsString or a PRUnichar *. I don't know what
+	// XP_GetString is going to return in mozilla. 
+
+    NS_IMETHOD SetPop3Sink(nsIPop3Sink* aPop3Sink) = 0;
+    NS_IMETHOD GetPop3Sink(nsIPop3Sink** aPop3Sink) const = 0;
+
+	NS_IMETHOD SetErrorMessage (char * errorMessage) = 0;
+	// caller must free using PR_FREE
+	NS_IMETHOD GetErrorMessage (char ** errorMessage) const = 0;
+};
+
+#endif /* nsIPop3URL_h___ */
