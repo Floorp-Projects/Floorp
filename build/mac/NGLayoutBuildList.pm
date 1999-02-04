920bbaf85c0be50b06f92cdbf173bba192c2d61b
waterson@netscape.com <waterson@netscape.com>
1999-02-04 11:16:57 +0000

    Added mozilla:layout:html:document:src to manifest dirs.

--- build/mac/NGLayoutBuildList.pm
+++ build/mac/NGLayoutBuildList.pm
@@ -278,6 +278,7 @@ sub BuildDist()
 	#LAYOUT
     InstallFromManifest(":mozilla:layout:build:MANIFEST",							"$distdirectory:layout:");
     InstallFromManifest(":mozilla:layout:base:public:MANIFEST",						"$distdirectory:layout:");
+        InstallFromManifest(":mozilla:layout:html:document:src:MANIFEST",                               "$distdirectory:layout:");
     InstallFromManifest(":mozilla:layout:html:style:public:MANIFEST",				"$distdirectory:layout:");
     InstallFromManifest(":mozilla:layout:html:style:src:MANIFEST",					"$distdirectory:layout:");
     InstallFromManifest(":mozilla:layout:html:base:src:MANIFEST",					"$distdirectory:layout:");
