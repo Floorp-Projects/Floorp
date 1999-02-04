25de9dca4e83c9703c9567d12e7e293ade655b0e
nhotta@netscape.com <nhotta@netscape.com>
1999-02-04 23:23:04 +0000

    Changed to use strxfrm for a key generation.

--- intl/locale/src/unix/nsCollationUnix.cpp
+++ intl/locale/src/unix/nsCollationUnix.cpp
@@ -16,7 +16,7 @@
  * Reserved.
  */
 
-
+#include "prmem.h"
 #include "nsCollationUnix.h"
 
 
@@ -41,10 +41,11 @@ nsresult nsCollationUnix::Initialize(nsILocale* locale)
 {
   mCollation = new nsCollation;
   if (mCollation == NULL) {
+    NS_ASSERTION(0, "mCollation creation failed");
     return NS_ERROR_OUT_OF_MEMORY;
   }
 
-  // locale -> LCID
+  // store platform locale
 
   return NS_OK;
 };
@@ -53,38 +54,53 @@ nsresult nsCollationUnix::Initialize(nsILocale* locale)
 nsresult nsCollationUnix::GetSortKeyLen(const nsCollationStrength strength, 
                            const nsString& stringIn, PRUint32* outLen)
 {
-  *outLen = stringIn.Length() * sizeof(PRUnichar);
-  return NS_OK;
+  nsresult res = NS_OK;
+
+  // this may not necessary because collation key length 
+  // probably will not change by this normalization
+  nsAutoString stringNormalized(stringIn);
+  if (strength != kCollationCaseSensitive) {
+    res = mCollation->NormalizeString(stringNormalized);
+  }
+
+  // convert unicode to charset
+  nsString aCharset("ISO-8859-1");	//TODO: need to get this from locale
+  char *str;
+
+  res = mCollation->UnicodeToChar(stringNormalized, &str, aCharset);
+  if (NS_SUCCEEDED(res) && str != NULL) {
+    // TODO: setlocale()
+    // call strxfrm to calculate a key length 
+    int len = strxfrm(NULL, str, 0) + 1;
+    *outLen = (len == -1) ? 0 : (PRUint32)len;
+    PR_Free(str);
+  }
+
+  return res;
 }
 
 nsresult nsCollationUnix::CreateSortKey(const nsCollationStrength strength, 
                            const nsString& stringIn, PRUint8* key, PRUint32* outLen)
 {
-  // temporary implementation, call FE eventually
-  PRUint32 byteLenIn = stringIn.Length() * sizeof(PRUnichar);
+  nsresult res = NS_OK;
 
-  if (byteLenIn > *outLen) {
-    *outLen = 0;
+  nsAutoString stringNormalized(stringIn);
+  if (strength != kCollationCaseSensitive) {
+    res = mCollation->NormalizeString(stringNormalized);
   }
-  else {
-    if (mCollation != NULL) {
-      mCollation->NormalizeString(stringIn);
-    }
-    if (strength != kCollationCaseSensitive) {
-      nsString *stringLower = new nsString(stringIn);
-      if (NULL == stringLower)
-        return NS_ERROR_OUT_OF_MEMORY;
+  // convert unicode to charset
+  nsString aCharset("ISO-8859-1");	//TODO: need to get this from locale
+  char *str;
 
-      stringLower->ToLowerCase();
-      memcpy((void *) key, (void *) stringLower->GetUnicode(), byteLenIn);
-      delete stringLower;
-    }
-   else {
-      memcpy((void *) key, (void *) stringIn.GetUnicode(), byteLenIn);
-   }
-   *outLen = byteLenIn;
+  res = mCollation->UnicodeToChar(stringNormalized, &str, aCharset);
+  if (NS_SUCCEEDED(res) && str != NULL) {
+    // TODO: setlocale()
+    // call strxfrm to generate a key 
+    int len = strxfrm((char *) key, str, strlen(str));
+   *outLen = (len == -1) ? 0 : (PRUint32)len;
+    PR_Free(str);
   }
 
-  return NS_OK;
+  return res;
 }
 
