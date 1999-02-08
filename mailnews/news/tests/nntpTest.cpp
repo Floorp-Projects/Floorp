524107ae905e2ffd3ca30cd1cd2db6d0bec441c9
alecf%netscape.com <alecf%netscape.com>
1999-02-08 23:05:05 +0000

    make this newsheader valid for now

--- mailnews/news/tests/nntpTest.cpp
+++ mailnews/news/tests/nntpTest.cpp
@@ -575,6 +575,13 @@ nsNntpTestDriver::OnPostMessage()
         post->AddNewsgroup(newsgroup);
         post->SetBody(message);
         post->SetSubject(subject);
+
+        // fake out these headers so that it's a valid post
+        post->SetDate("Mon Feb  8 11:41:38 PST 1999");
+        post->SetFrom("userid@somewhere.com");
+        post->SetPath("");
+        post->SetRelayVersion("1.0");
+        post->SetPostingVersion("5.0");
     }
     
     m_url->SetMessageToPost(post);
