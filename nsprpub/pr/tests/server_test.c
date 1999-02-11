dd83ab951049534712fc6f18dbd71af27cb1ba07
wtc@netscape.com <wtc@netscape.com>
1999-02-11 00:43:30 +0000

    Declare Test_Result as returning void.  Changed AF_INET and INADDR_XXX to PR_AF_INET and PR_INADDR_XXX.

--- nsprpub/pr/tests/server_test.c
+++ nsprpub/pr/tests/server_test.c
@@ -94,7 +94,7 @@ PRCondVar *ServerStateCV;
 ***********************************************************************/
 
 
-static Test_Result (int result)
+static void Test_Result (int result)
 {
 	switch (result)
 	{
@@ -265,9 +265,9 @@ ServerSetup(void)
     }
 
     memset(&serverAddr, 0, sizeof(PRNetAddr));
-    serverAddr.inet.family = AF_INET;
+    serverAddr.inet.family = PR_AF_INET;
     serverAddr.inet.port = PR_htons(PORT);
-    serverAddr.inet.ip = PR_htonl(INADDR_ANY);
+    serverAddr.inet.ip = PR_htonl(PR_INADDR_ANY);
 
     if ( PR_Bind(listenSocket, &serverAddr) == PR_FAILURE) {
         if (debug_mode) printf("\tServer error binding to server address: OS error %d\n",
@@ -361,9 +361,9 @@ ClientThreadFunc(void *unused)
         if (debug_mode) printf("\tClient could not malloc space!?\n");
 
     memset(&serverAddr, 0, sizeof(PRNetAddr));
-    serverAddr.inet.family = AF_INET;
+    serverAddr.inet.family = PR_AF_INET;
     serverAddr.inet.port = PR_htons(PORT);
-    serverAddr.inet.ip = PR_htonl(INADDR_LOOPBACK);
+    serverAddr.inet.ip = PR_htonl(PR_INADDR_LOOPBACK);
 
     while(numRequests > 0) {
 
