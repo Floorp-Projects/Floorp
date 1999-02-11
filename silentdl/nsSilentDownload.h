e36c3880a0025d61bc9f2af522832d871f832668
dougt%netscape.com <dougt%netscape.com>
1999-02-11 20:51:37 +0000

    removing extra semicolons so that the mac build does not complain.

--- silentdl/nsSilentDownload.h
+++ silentdl/nsSilentDownload.h
@@ -15,7 +15,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-  };
+  }
 
 #define NS_SilentDownloadFactory_CID                 \
 { /* 18c2f984-b09f-11d2-bcde-00805f0e1353 */         \
@@ -23,7 +23,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-  };
+  }
 
 #define NS_SilentDownloadTaskFactory_CID             \
 { /* 18c2f985-b09f-11d2-bcde-00805f0e1353 */         \
@@ -31,6 +31,6 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-  };
+  }
 
 #endif /* nsSilentDownload_h___ */
