323f34a628b996d4d35b53282e1bfad1d55c1e6d
ducarroz%netscape.com <ducarroz%netscape.com>
1999-02-11 01:01:47 +0000

    Initial check in

---
+++ mailnews/compose/build/nsMsgCompCID.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsMessageBaseCID_h__
+#define nsMessageBaseCID_h__
+
+#include "nsISupports.h"
+#include "nsIFactory.h"
+#include "nsRepository.h"
+
+#define NS_MSGCOMPOSE_CID						  \
+{ /* EB5BDAF8-BBC6-11d2-A6EC-0060B0EB39B5 */      \
+ 0xeb5bdaf8, 0xbbc6, 0x11d2,                      \
+ {0xa6, 0xec, 0x0, 0x60, 0xb0, 0xeb, 0x39, 0xb5}}
+
+#define NS_MSGCOMPFIELDS_CID						  \
+{ /* 6D222BA0-BD46-11d2-8293-000000000000 */      \
+ 0x6d222ba0, 0xbd46, 0x11d2,                      \
+ {0x82, 0x93, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}}
+
+#endif // nsMessageBaseCID_h__
