d5146eecc9f7afbc85a0a797273732b2aed9eb2d
dcone%netscape.com <dcone%netscape.com>
1999-02-12 17:36:28 +0000

    added a manifest for Layout/html/table

--- build/mac/NGLayoutBuildList.pm
+++ build/mac/NGLayoutBuildList.pm
@@ -283,6 +283,7 @@ sub BuildDist()
     InstallFromManifest(":mozilla:layout:html:style:src:MANIFEST",					"$distdirectory:layout:");
     InstallFromManifest(":mozilla:layout:html:base:src:MANIFEST",					"$distdirectory:layout:");
 	InstallFromManifest(":mozilla:layout:html:forms:public:MANIFEST",				"$distdirectory:layout:");
+    InstallFromManifest(":mozilla:layout:html:table:public:MANIFEST",		"$distdirectory:layout:");
     InstallFromManifest(":mozilla:layout:base:src:MANIFEST",						"$distdirectory:layout:");
 	InstallFromManifest(":mozilla:layout:events:public:MANIFEST",					"$distdirectory:layout:");
 	InstallFromManifest(":mozilla:layout:events:src:MANIFEST",						"$distdirectory:layout:");
