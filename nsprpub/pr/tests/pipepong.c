174ea791ce997b7a9115bf449c0e00f5fe194f95
wtc@netscape.com <wtc@netscape.com>
1999-02-12 18:41:52 +0000

    Minor improvements.

--- nsprpub/pr/tests/pipepong.c
+++ nsprpub/pr/tests/pipepong.c
@@ -44,25 +44,28 @@ int main()
     size_t nBytes;
     int idx;
 
-    memset(buf, 0, sizeof(buf));
-
     for (idx = 0; idx < NUM_ITERATIONS; idx++) {
+        memset(buf, 0, sizeof(buf));
         nBytes = fread(buf, 1, 5, stdin);
-        fprintf(stderr, "pipepong: received \"%s\"\n", buf);
+        fprintf(stderr, "pong process: received \"%s\"\n", buf);
         if (nBytes != 5) {
-            fprintf(stderr, "pipepong: expected 5 bytes but got %d bytes\n",
+            fprintf(stderr, "pong process: expected 5 bytes but got %d bytes\n",
                     nBytes);
             exit(1);
         }
         if (strcmp(buf, "ping") != 0) {
-            fprintf(stderr, "pipepong: expected \"ping\" but got \"%s\"\n",
+            fprintf(stderr, "pong process: expected \"ping\" but got \"%s\"\n",
                     buf);
             exit(1);
         }
 
         strcpy(buf, "pong");
-        fprintf(stderr, "pipepong: sending \"%s\"\n", buf);
-        printf("%s", buf);
+        fprintf(stderr, "pong process: sending \"%s\"\n", buf);
+        nBytes = fwrite(buf, 1, 5, stdout);
+        if (nBytes != 5) {
+            fprintf(stderr, "pong process: fwrite failed\n");
+            exit(1);
+        }
         fflush(stdout);
     }
 
