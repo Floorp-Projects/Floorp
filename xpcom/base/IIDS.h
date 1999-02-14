15f91384af572dbd843417f84a9fbc1a83c8b595
jfrancis%netscape.com <jfrancis%netscape.com>
1999-02-14 06:47:43 +0000

    grabbed an  id for nsContentSubtreeIterator_CID

--- xpcom/base/IIDS.h
+++ xpcom/base/IIDS.h
@@ -917,7 +917,7 @@ nsIContentIterator = { /* a6cf90e4-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-INTERFACENAME = { /* a6cf90e5-15b3-11d2-932e-00805f8add32 */
+nsContentSubtreeIterator_CID = { /* a6cf90e5-15b3-11d2-932e-00805f8add32 */
     0xa6cf90e5,
     0x15b3,
     0x11d2,
