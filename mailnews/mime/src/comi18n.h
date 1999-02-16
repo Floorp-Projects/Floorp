a271778c4fde7ff7aa70abe44de6059c3fb07cd0
nhotta%netscape.com <nhotta%netscape.com>
1999-02-16 18:37:10 +0000

    Initial check in.

---
+++ mailnews/mime/src/comi18n.h
@@ -0,0 +1,108 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#define kMIME_ENCODED_WORD_SIZE 72
+#define kMAX_CSNAME 64
+
+/**
+ * If a header is MIME encoded then decode a header and sets a charset name.
+ * This is a replacement for INTL_DecodeMimePartIIStr.
+ * Unlike INTL_DecodeMimePartIIStr, this does not apply any charset conversion.
+ * Use MIME_ConvertCharset if the decoded string needs a conversion.
+ *
+ *
+ * @param header      [IN] A header to decode.
+ * @param charset     [OUT] Charset name (in C string) from a MIME header is set.
+ *                    Caller should allocate at least 65 bytes (kMAX_CSNAME + 1) for a charset name.
+ * @return            Decoded buffer (in C string) or return NULL if the header is not MIME encoded.
+ */
+char *MIME_DecodeMimePartIIStr(const char *header, char *charset);
+
+/**
+ * Convert the unicode string into the encoding used in internet message and encode 
+ * them into RFC 2047 form.
+ *
+ *
+ * @param inHeaderUni     [IN] Unicode string.
+ * @param inCharLen       [IN] Character length of the unicode string.
+ * @param mailCharset     [IN] Charset name (in C string) to convert.
+ * @param encodedWordSize [IN] Byte lenght limit of the output, ususally 72 (use kMIME_ENCODED_WORD_SIZE).
+ * @return            Encoded buffer (in C string) or NULL in case of error (e.g. converter unavailable).
+ */
+char *MIME_EncodeMimePartIIStr(const void* inHeaderUni, const PRInt32 inCharLen, 
+                               const char* mailCharset, const PRInt32 encodedWordSize);
+
+/**
+ * Apply charset conversion to a given buffer.
+ * This is a replacement for INTL_ConvertLineWithoutAutoDetect.
+ *
+ *
+ * @param from_charset[IN] A charset name in C string.
+ * @param to_charset  [IN] A charset name in C string.
+ * @param inBuffer    [IN] Input buffer to convert.
+ * @param inLength    [IN] Input buffer length.
+ * @param outBuffer   [OUT] Converted buffer (in C string) is set. Allocated buffer should be freed by PR_FREE.
+ * @return            nsresult, 0 is success, otherwise error.
+ */
+PRUint32 MIME_ConvertCharset(const char* from_charset, const char* to_charset,
+                             const char* inBuffer, const PRInt32 inLength,
+                             char** outBuffer);
+
+/**
+ * Convert an input buffer with a charset into unicode.
+ *
+ *
+ * @param from_charset [IN] A charset name in C string.
+ * @param aBuffer      [IN] Input buffer to convert.
+ * @param aLength      [IN] Input buffer length.
+ * @param uniBuffer    [OUT] Output unicode buffer is set. Allocated buffer should be freed by PR_FREE.
+ * @param uniLength    [OUT] Output unicode buffer character length is set.
+ * @return             nsresult, 0 is success, otherwise error.
+ */
+PRUint32 MIME_ConvertToUnicode(const char* from_charset, const char* aBuffer, const PRInt32 aLength,
+                               void** uniBuffer, PRInt32* uniLength);
+
+/**
+ * Conver an unicode buffer to a given charset.
+ *
+ *
+ * @param to_charset   [IN] A charset name in C string.
+ * @param uniBuffer    [IN] Input unicode buffer to convert.
+ * @param uniLength    [IN] Input unicode buffer character length.
+ * @param aBuffer      [OUT] Output buffer (in C string) is set. Allocated buffer should be freed by PR_FREE.
+ * @return             nsresult, 0 is success, otherwise error.
+ */
+PRUint32 MIME_ConvertFromUnicode(const char* to_charset, const void* uniBuffer, const PRInt32 uniLength,
+                                 char** aBuffer);
+
+
+/*
+ * To be removed. Existing for the backword compatibility. 
+ */
+char *INTL_DecodeMimePartIIStr(const char *header, int16 wincsid, PRBool dontConvert);
+char *INTL_EncodeMimePartIIStr(char *subject, int16 wincsid,PRBool bUseMime);
+char *INTL_EncodeMimePartIIStr_VarLen(char *subject, int16 wincsid, PRBool bUseMime, int encodedWordSize);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
