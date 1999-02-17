ca621b1215984b7c1bc6b298467dbb8a95e01b2d
waterson%netscape.com <waterson%netscape.com>
1999-02-17 08:17:50 +0000

    Create RDF_19990217_BRANCH from RDF_DOM_19990216_BRANCH to pick up nsCOMPtr changes before doing major whackage.

---
+++ rdf/content/src/nsJSXULDocument.cpp
@@ -0,0 +1,312 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+/* AUTO-GENERATED. DO NOT EDIT!!! */
+
+#include "jsapi.h"
+#include "nsJSUtils.h"
+#include "nscore.h"
+#include "nsIScriptContext.h"
+#include "nsIJSScriptObject.h"
+#include "nsIScriptObjectOwner.h"
+#include "nsIScriptGlobalObject.h"
+#include "nsIPtr.h"
+#include "nsString.h"
+#include "nsIDOMNode.h"
+#include "nsIDOMXULDocument.h"
+
+
+static NS_DEFINE_IID(kIScriptObjectOwnerIID, NS_ISCRIPTOBJECTOWNER_IID);
+static NS_DEFINE_IID(kIJSScriptObjectIID, NS_IJSSCRIPTOBJECT_IID);
+static NS_DEFINE_IID(kIScriptGlobalObjectIID, NS_ISCRIPTGLOBALOBJECT_IID);
+static NS_DEFINE_IID(kINodeIID, NS_IDOMNODE_IID);
+static NS_DEFINE_IID(kIXULDocumentIID, NS_IDOMXULDOCUMENT_IID);
+
+NS_DEF_PTR(nsIDOMNode);
+NS_DEF_PTR(nsIDOMXULDocument);
+
+
+/***********************************************************************/
+//
+// XULDocument Properties Getter
+//
+PR_STATIC_CALLBACK(JSBool)
+GetXULDocumentProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+  nsIDOMXULDocument *a = (nsIDOMXULDocument*)JS_GetPrivate(cx, obj);
+
+  // If there's no private data, this must be the prototype, so ignore
+  if (nsnull == a) {
+    return JS_TRUE;
+  }
+
+  if (JSVAL_IS_INT(id)) {
+    switch(JSVAL_TO_INT(id)) {
+      case 0:
+      default:
+        return nsJSUtils::nsCallJSScriptObjectGetProperty(a, cx, id, vp);
+    }
+  }
+  else {
+    return nsJSUtils::nsCallJSScriptObjectGetProperty(a, cx, id, vp);
+  }
+
+  return PR_TRUE;
+}
+
+/***********************************************************************/
+//
+// XULDocument Properties Setter
+//
+PR_STATIC_CALLBACK(JSBool)
+SetXULDocumentProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+  nsIDOMXULDocument *a = (nsIDOMXULDocument*)JS_GetPrivate(cx, obj);
+
+  // If there's no private data, this must be the prototype, so ignore
+  if (nsnull == a) {
+    return JS_TRUE;
+  }
+
+  if (JSVAL_IS_INT(id)) {
+    switch(JSVAL_TO_INT(id)) {
+      case 0:
+      default:
+        return nsJSUtils::nsCallJSScriptObjectSetProperty(a, cx, id, vp);
+    }
+  }
+  else {
+    return nsJSUtils::nsCallJSScriptObjectSetProperty(a, cx, id, vp);
+  }
+
+  return PR_TRUE;
+}
+
+
+//
+// XULDocument finalizer
+//
+PR_STATIC_CALLBACK(void)
+FinalizeXULDocument(JSContext *cx, JSObject *obj)
+{
+  nsJSUtils::nsGenericFinalize(cx, obj);
+}
+
+
+//
+// XULDocument enumerate
+//
+PR_STATIC_CALLBACK(JSBool)
+EnumerateXULDocument(JSContext *cx, JSObject *obj)
+{
+  return nsJSUtils::nsGenericEnumerate(cx, obj);
+}
+
+
+//
+// XULDocument resolve
+//
+PR_STATIC_CALLBACK(JSBool)
+ResolveXULDocument(JSContext *cx, JSObject *obj, jsval id)
+{
+  return nsJSUtils::nsGenericResolve(cx, obj, id);
+}
+
+
+//
+// Native method GetElementByID
+//
+PR_STATIC_CALLBACK(JSBool)
+XULDocumentGetElementByID(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+  nsIDOMXULDocument *nativeThis = (nsIDOMXULDocument*)JS_GetPrivate(cx, obj);
+  JSBool rBool = JS_FALSE;
+  nsIDOMNode* nativeRet;
+  nsAutoString b0;
+
+  *rval = JSVAL_NULL;
+
+  // If there's no private data, this must be the prototype, so ignore
+  if (nsnull == nativeThis) {
+    return JS_TRUE;
+  }
+
+  if (argc >= 1) {
+
+    nsJSUtils::nsConvertJSValToString(b0, cx, argv[0]);
+
+    if (NS_OK != nativeThis->GetElementByID(b0, &nativeRet)) {
+      return JS_FALSE;
+    }
+
+    nsJSUtils::nsConvertObjectToJSVal(nativeRet, cx, rval);
+  }
+  else {
+    JS_ReportError(cx, "Function getElementByID requires 1 parameters");
+    return JS_FALSE;
+  }
+
+  return JS_TRUE;
+}
+
+
+/***********************************************************************/
+//
+// class for XULDocument
+//
+JSClass XULDocumentClass = {
+  "XULDocument", 
+  JSCLASS_HAS_PRIVATE,
+  JS_PropertyStub,
+  JS_PropertyStub,
+  GetXULDocumentProperty,
+  SetXULDocumentProperty,
+  EnumerateXULDocument,
+  ResolveXULDocument,
+  JS_ConvertStub,
+  FinalizeXULDocument
+};
+
+
+//
+// XULDocument class properties
+//
+static JSPropertySpec XULDocumentProperties[] =
+{
+  {0}
+};
+
+
+//
+// XULDocument class methods
+//
+static JSFunctionSpec XULDocumentMethods[] = 
+{
+  {"getElementByID",          XULDocumentGetElementByID,     1},
+  {0}
+};
+
+
+//
+// XULDocument constructor
+//
+PR_STATIC_CALLBACK(JSBool)
+XULDocument(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+  return JS_FALSE;
+}
+
+
+//
+// XULDocument class initialization
+//
+nsresult NS_InitXULDocumentClass(nsIScriptContext *aContext, void **aPrototype)
+{
+  JSContext *jscontext = (JSContext *)aContext->GetNativeContext();
+  JSObject *proto = nsnull;
+  JSObject *constructor = nsnull;
+  JSObject *parent_proto = nsnull;
+  JSObject *global = JS_GetGlobalObject(jscontext);
+  jsval vp;
+
+  if ((PR_TRUE != JS_LookupProperty(jscontext, global, "XULDocument", &vp)) ||
+      !JSVAL_IS_OBJECT(vp) ||
+      ((constructor = JSVAL_TO_OBJECT(vp)) == nsnull) ||
+      (PR_TRUE != JS_LookupProperty(jscontext, JSVAL_TO_OBJECT(vp), "prototype", &vp)) || 
+      !JSVAL_IS_OBJECT(vp)) {
+
+    if (NS_OK != NS_InitDocumentClass(aContext, (void **)&parent_proto)) {
+      return NS_ERROR_FAILURE;
+    }
+    proto = JS_InitClass(jscontext,     // context
+                         global,        // global object
+                         parent_proto,  // parent proto 
+                         &XULDocumentClass,      // JSClass
+                         XULDocument,            // JSNative ctor
+                         0,             // ctor args
+                         XULDocumentProperties,  // proto props
+                         XULDocumentMethods,     // proto funcs
+                         nsnull,        // ctor props (static)
+                         nsnull);       // ctor funcs (static)
+    if (nsnull == proto) {
+      return NS_ERROR_FAILURE;
+    }
+
+  }
+  else if ((nsnull != constructor) && JSVAL_IS_OBJECT(vp)) {
+    proto = JSVAL_TO_OBJECT(vp);
+  }
+  else {
+    return NS_ERROR_FAILURE;
+  }
+
+  if (aPrototype) {
+    *aPrototype = proto;
+  }
+  return NS_OK;
+}
+
+
+//
+// Method for creating a new XULDocument JavaScript object
+//
+extern "C" NS_DOM nsresult NS_NewScriptXULDocument(nsIScriptContext *aContext, nsISupports *aSupports, nsISupports *aParent, void **aReturn)
+{
+  NS_PRECONDITION(nsnull != aContext && nsnull != aSupports && nsnull != aReturn, "null argument to NS_NewScriptXULDocument");
+  JSObject *proto;
+  JSObject *parent;
+  nsIScriptObjectOwner *owner;
+  JSContext *jscontext = (JSContext *)aContext->GetNativeContext();
+  nsresult result = NS_OK;
+  nsIDOMXULDocument *aXULDocument;
+
+  if (nsnull == aParent) {
+    parent = nsnull;
+  }
+  else if (NS_OK == aParent->QueryInterface(kIScriptObjectOwnerIID, (void**)&owner)) {
+    if (NS_OK != owner->GetScriptObject(aContext, (void **)&parent)) {
+      NS_RELEASE(owner);
+      return NS_ERROR_FAILURE;
+    }
+    NS_RELEASE(owner);
+  }
+  else {
+    return NS_ERROR_FAILURE;
+  }
+
+  if (NS_OK != NS_InitXULDocumentClass(aContext, (void **)&proto)) {
+    return NS_ERROR_FAILURE;
+  }
+
+  result = aSupports->QueryInterface(kIXULDocumentIID, (void **)&aXULDocument);
+  if (NS_OK != result) {
+    return result;
+  }
+
+  // create a js object for this class
+  *aReturn = JS_NewObject(jscontext, &XULDocumentClass, proto, parent);
+  if (nsnull != *aReturn) {
+    // connect the native object to the js object
+    JS_SetPrivate(jscontext, (JSObject *)*aReturn, aXULDocument);
+  }
+  else {
+    NS_RELEASE(aXULDocument);
+    return NS_ERROR_FAILURE; 
+  }
+
+  return NS_OK;
+}
