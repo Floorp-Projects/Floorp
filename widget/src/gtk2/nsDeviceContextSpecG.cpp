3a17d6e7b2e046cfb353d5edfaf6b6eef653b9cd
dcone%netscape.com <dcone%netscape.com>
1999-02-18 22:35:11 +0000

    Added the printer support files for GTK

---
+++ widget/src/gtk2/nsDeviceContextSpecG.cpp
@@ -0,0 +1,65 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "nsDeviceContextSpecG.h"
+//#include "prmem.h"
+//#include "plstr.h"
+
+/** -------------------------------------------------------
+ *  Construct the nsDeviceContextSpecMac
+ *  @update   dc 12/02/98
+ */
+nsDeviceContextSpecGTK :: nsDeviceContextSpecGTK()
+{
+  NS_INIT_REFCNT();
+	
+}
+
+/** -------------------------------------------------------
+ *  Destroy the nsDeviceContextSpecMac
+ *  @update   dc 2/15/98
+ */
+nsDeviceContextSpecGTK :: ~nsDeviceContextSpecGTK()
+{
+
+
+}
+
+static NS_DEFINE_IID(kDeviceContextSpecIID, NS_IDEVICE_CONTEXT_SPEC_IID);
+
+NS_IMPL_QUERY_INTERFACE(nsDeviceContextSpecGTK, kDeviceContextSpecIID)
+NS_IMPL_ADDREF(nsDeviceContextSpecGTK)
+NS_IMPL_RELEASE(nsDeviceContextSpecGTK)
+
+/** -------------------------------------------------------
+ *  Initialize the nsDeviceContextSpecGTK
+ *  @update   dc 2/15/98
+ */
+NS_IMETHODIMP nsDeviceContextSpecGTK :: Init(PRBool	aQuiet)
+{
+  return NS_OK;
+}
+
+/** -------------------------------------------------------
+ * Closes the printmanager if it is open.
+ *  @update   dc 2/15/98
+ */
+NS_IMETHODIMP nsDeviceContextSpecGTK :: ClosePrintManager()
+{
+	return NS_OK;
+}  
