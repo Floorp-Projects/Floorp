4aeb615bb6af2dc8a1f2e88909981d8dd3008c8c
evaughan%netscape.com <evaughan%netscape.com>
1999-02-19 20:12:08 +0000

    Fixed out of scope compile bug.

--- layout/xul/base/src/nsTitledButtonFrame.cpp
+++ layout/xul/base/src/nsTitledButtonFrame.cpp
@@ -511,7 +511,8 @@ nsTitledButtonFrame::CalculateTitleForWidth(nsIPresContext& aPresContext, nsIRen
 		   nscoord cwidth;
 		   nscoord twidth = 0;
            int length = mTitle.Length();
-           for (int i=0; i < length; i++)
+		   int i = 0;
+           for (i = 0; i < length; i++)
            {
               PRUnichar ch = mTitle[i];
               aRenderingContext.GetWidth(ch,cwidth);
@@ -537,7 +538,8 @@ nsTitledButtonFrame::CalculateTitleForWidth(nsIPresContext& aPresContext, nsIRen
 		   nscoord cwidth;
 		   nscoord twidth = 0;
            int length = mTitle.Length();
-           for (int i=length-1; i >= 0; i--)
+		   int i = length-1;
+           for (i=length-1; i >= 0; i--)
            {
               PRUnichar ch = mTitle[i];
               aRenderingContext.GetWidth(ch,cwidth);
