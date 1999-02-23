0436b7ef10c867ba867ce567181478b16ac04851
vidur%netscape.com <vidur%netscape.com>
1999-02-23 19:18:34 +0000

    Added nsIStyleSheetLinkingElement interface

--- xpcom/src/IIDS.h
+++ xpcom/src/IIDS.h
@@ -941,7 +941,7 @@ nsIFile = { /* a6cf90e8-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-INTERFACENAME = { /* a6cf90e9-15b3-11d2-932e-00805f8add32 */
+nsILinkingElement = { /* a6cf90e9-15b3-11d2-932e-00805f8add32 */
     0xa6cf90e9,
     0x15b3,
     0x11d2,
