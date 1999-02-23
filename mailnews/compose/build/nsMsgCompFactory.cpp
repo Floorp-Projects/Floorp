9f9ed78fb1bcfa82a2cbc91aa3d7da4dd79891c7
mscott%netscape.com <mscott%netscape.com>
1999-02-23 01:02:49 +0000

    Added DLL self registration code. Ability to create smtp service object. Changed class name to reflect the fact that this is a factory for compose....

--- mailnews/compose/build/nsMsgCompFactory.cpp
+++ mailnews/compose/build/nsMsgCompFactory.cpp
@@ -16,12 +16,16 @@
  * Reserved.
  */
 
+#include "msgCore.h"
+
 #include "nsIFactory.h"
 #include "nsISupports.h"
-#include "msgCore.h"
+#include "pratom.h"
 #include "nsMsgCompCID.h"
 
 /* Include all of the interfaces our factory can generate components for */
+#include "nsISmtpService.h"
+#include "nsSmtpService.h"
 #include "nsMsgComposeFact.h"
 #include "nsMsgCompFieldsFact.h"
 #include "nsMsgSendFact.h"
@@ -31,42 +35,45 @@ static NS_DEFINE_IID(kIFactoryIID, NS_IFACTORY_IID);
 static NS_DEFINE_CID(kCMsgComposeCID, NS_MSGCOMPOSE_CID);
 static NS_DEFINE_CID(kCMsgCompFieldsCID, NS_MSGCOMPFIELDS_CID);
 static NS_DEFINE_CID(kCMsgSendCID, NS_MSGSEND_CID);
+static NS_DEFINE_CID(kCSmtpServiceCID, NS_SMTPSERVICE_CID);
 
 ////////////////////////////////////////////////////////////
 //
 ////////////////////////////////////////////////////////////
+static PRInt32 g_InstanceCount = 0;
+static PRInt32 g_LockCount = 0;
 
-class nsMsgFactory : public nsIFactory
+class nsMsgComposeFactory : public nsIFactory
 {   
 public:
 	// nsISupports methods
 	NS_DECL_ISUPPORTS 
 
-    nsMsgFactory(const nsCID &aClass); 
+    nsMsgComposeFactory(const nsCID &aClass); 
 
     // nsIFactory methods   
     NS_IMETHOD CreateInstance(nsISupports *aOuter, const nsIID &aIID, void **aResult);   
     NS_IMETHOD LockFactory(PRBool aLock);   
 
   protected:
-    virtual ~nsMsgFactory();   
+    virtual ~nsMsgComposeFactory();   
 
   private:  
     nsCID     mClassID;
 };   
 
-nsMsgFactory::nsMsgFactory(const nsCID &aClass)   
+nsMsgComposeFactory::nsMsgComposeFactory(const nsCID &aClass)   
 {   
 	NS_INIT_REFCNT();
 	mClassID = aClass;
 }   
 
-nsMsgFactory::~nsMsgFactory()   
+nsMsgComposeFactory::~nsMsgComposeFactory()   
 {   
 	NS_ASSERTION(mRefCnt == 0, "non-zero refcnt at destruction");   
 }   
 
-nsresult nsMsgFactory::QueryInterface(const nsIID &aIID, void **aResult)   
+nsresult nsMsgComposeFactory::QueryInterface(const nsIID &aIID, void **aResult)   
 {   
   if (aResult == NULL)  
     return NS_ERROR_NULL_POINTER;  
@@ -87,10 +94,10 @@ nsresult nsMsgFactory::QueryInterface(const nsIID &aIID, void **aResult)
   return NS_OK;   
 }   
 
-NS_IMPL_ADDREF(nsMsgFactory)
-NS_IMPL_RELEASE(nsMsgFactory)
+NS_IMPL_ADDREF(nsMsgComposeFactory)
+NS_IMPL_RELEASE(nsMsgComposeFactory)
 
-nsresult nsMsgFactory::CreateInstance(nsISupports *aOuter, const nsIID &aIID, void **aResult)  
+nsresult nsMsgComposeFactory::CreateInstance(nsISupports *aOuter, const nsIID &aIID, void **aResult)  
 {  
 	nsresult res = NS_OK;
 
@@ -103,7 +110,20 @@ nsresult nsMsgFactory::CreateInstance(nsISupports *aOuter, const nsIID &aIID, vo
 
 	// ClassID check happens here
 	// Whenever you add a new class that supports an interface, plug it in here!!!
+
+	// mscott --> I've noticed that all of the "convience" functions (NS_*) used to 
+	// create components are adding a ref count to the returned component...then at the end of
+	// CreateInstance we ref count again when we query interface for the desired interface. 
+	// That's why we need to be careful and make sure we release the first ref count before returning...
+	// So, if you add a component here and it doesn't ref count when you create it then you might
+	// have a problem....
 	
+	if (mClassID.Equals(kCSmtpServiceCID))
+	{
+		nsSmtpService * smtpService = new nsSmtpService();
+		// okay now turn around and give inst a handle on it....
+		smtpService->QueryInterface(kISupportsIID, (void **) &inst);
+	}
 	// do they want an Message Compose interface ?
 	if (mClassID.Equals(kCMsgComposeCID))
 	{
@@ -134,7 +154,7 @@ nsresult nsMsgFactory::CreateInstance(nsISupports *aOuter, const nsIID &aIID, vo
 		// so we now have the class that supports the desired interface...we need to turn around and
 		// query for our desired interface.....
 		res = inst->QueryInterface(aIID, aResult);
-		NS_RELEASE(inst);
+		NS_RELEASE(inst); // because creating picked up an extra ref count...
 		if (res != NS_OK)  // if the query interface failed for some reason, then the object did not get ref counted...delete it.
 			delete inst;
 	}
@@ -144,10 +164,14 @@ nsresult nsMsgFactory::CreateInstance(nsISupports *aOuter, const nsIID &aIID, vo
   return res;  
 }  
 
-nsresult nsMsgFactory::LockFactory(PRBool aLock)  
+nsresult nsMsgComposeFactory::LockFactory(PRBool aLock)  
 {  
-  // Not implemented in simplest case.  
-  return NS_OK;
+	if (aLock) 
+		PR_AtomicIncrement(&g_LockCount); 
+	else
+		PR_AtomicDecrement(&g_LockCount); 
+	
+	return NS_OK;
 }  
 
 // return the proper factory to the caller. 
@@ -159,8 +183,8 @@ extern "C" NS_EXPORT nsresult NSGetFactory(const nsCID &aClass,
 		return NS_ERROR_NULL_POINTER;
 
 	// If we decide to implement multiple factories in the msg.dll, then we need to check the class
-	// type here and create the appropriate factory instead of always creating a nsMsgFactory...
-	*aFactory = new nsMsgFactory(aClass);
+	// type here and create the appropriate factory instead of always creating a nsMsgComposeFactory...
+	*aFactory = new nsMsgComposeFactory(aClass);
 
 	if (aFactory)
 		return (*aFactory)->QueryInterface(nsIFactory::IID(), (void**)aFactory); // they want a Factory Interface so give it to them
@@ -168,3 +192,32 @@ extern "C" NS_EXPORT nsresult NSGetFactory(const nsCID &aClass,
 		return NS_ERROR_OUT_OF_MEMORY;
 }
 
+extern "C" NS_EXPORT PRBool NSCanUnload() 
+{
+    return PRBool(g_InstanceCount == 0 && g_LockCount == 0);
+}
+
+extern "C" NS_EXPORT nsresult NSRegisterSelf(const char* path)
+{
+	nsresult ret;
+
+	ret = nsRepository::RegisterFactory(kCMsgComposeCID, path, PR_TRUE, PR_TRUE);
+	ret = nsRepository::RegisterFactory(kCSmtpServiceCID, path, PR_TRUE, PR_TRUE);
+	ret = nsRepository::RegisterFactory(kCMsgCompFieldsCID, path, PR_TRUE, PR_TRUE);
+	ret = nsRepository::RegisterFactory(kCMsgSendCID, path, PR_TRUE, PR_TRUE);
+
+	return ret;
+}
+
+extern "C" NS_EXPORT nsresult
+NSUnregisterSelf(const char* path)
+{
+	nsresult ret;
+
+	ret = nsRepository::UnregisterFactory(kCMsgComposeCID, path);
+	ret = nsRepository::UnregisterFactory(kCSmtpServiceCID, path);
+    ret = nsRepository::UnregisterFactory(kCMsgCompFieldsCID, path);
+	ret = nsRepository::UnregisterFactory(kCMsgSendCID, path);
+
+	return ret;
+}
