05fe972b0c4a33e00fc3f3154508c275491c16aa
putterman%netscape.com <putterman%netscape.com>
1999-02-23 07:08:56 +0000

    Added build directory.

---
+++ mailnews/local/build/nsMsgLocalCID.h
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsMsgLocalCID_h__
+#define nsMsgLocalCID_h__
+
+#include "nsISupports.h"
+#include "nsIFactory.h"
+#include "nsRepository.h"
+
+#define NS_MSGLOCALDATASOURCE_CID                    \
+{ /* 992732A0-CABC-11d2-94BE-006097222B83 */      \
+0x992732a0, 0xcabc, 0x11d2,                      \
+ {0x94, 0xbe, 0x0, 0x60, 0x97, 0x22, 0x2b, 0x83}}
+
+#endif // nsMsgLocal_h__
