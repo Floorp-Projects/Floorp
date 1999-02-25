d3e44cdc45b13f3fb7ac78ba7965a77fd9351939
mscott%netscape.com <mscott%netscape.com>
1999-02-25 01:10:27 +0000

    header file for mailbox service implementation.

---
+++ mailnews/local/src/nsMailboxService.h
@@ -0,0 +1,61 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsMailboxService_h___
+#define nsMailboxService_h___
+
+#include "nscore.h"
+#include "nsISupports.h"
+
+#include "nsIMailboxService.h"
+#include "nsIURL.h"
+#include "nsIStreamListener.h"
+#include "nsFileSpec.h"
+
+////////////////////////////////////////////////////////////////////////////////////////
+// The Mailbox Service is an interface designed to make building and running mailbox urls
+// easier. I'm not sure if this service will go away when the new networking model comes
+// on line (as part of the N2 project). So I reserve the right to change my mind and take
+// this service away =).
+////////////////////////////////////////////////////////////////////////////////////////
+
+class nsMailboxService : public nsIMailboxService
+{
+public:
+
+	nsMailboxService();
+	virtual ~nsMailboxService();
+	
+	NS_DECL_ISUPPORTS
+
+	////////////////////////////////////////////////////////////////////////////////////////
+	// we suppport the nsIMailboxService Interface 
+	////////////////////////////////////////////////////////////////////////////////////////
+
+	NS_IMETHOD ParseMailbox(const nsFilePath& aMailboxPath, nsIStreamListener * aMailboxParser, nsIURL ** aURL);
+
+	NS_IMETHOD DisplayMessage(const nsFilePath& aMailboxPath, PRUint32 aStartByte, PRUint32 aEndByte, 
+		nsISupports * aDisplayConsumer, nsIURL ** aURL);
+
+	////////////////////////////////////////////////////////////////////////////////////////
+	// End suppport for the nsIMailboxService Interface
+	////////////////////////////////////////////////////////////////////////////////////////
+
+};
+
+#endif /* nsMailboxService_h___ */
