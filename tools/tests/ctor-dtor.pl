7bb42a461d73ae9562a6b16953a1edeedc8baea7
cyeh%netscape.com <cyeh%netscape.com>
1999-02-25 19:35:05 +0000

    little perl script that searches for global constructors/destructors in build objects in the tree

---
+++ tools/tests/ctor-dtor.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use Cwd;
+
+$curdir = cwd();
+
+open( LOG, ">ctor-dtor-report" ) || print "can't open $?\n";
+
+@path_fields = split(/\//,$curdir);
+
+pop(@path_fields);
+pop(@path_fields);
+
+$path = join ("/",@path_fields);
+
+open (REPORT, "find $path -name \"*.o\" -print | xargs nm -Bno | egrep \"_GLOBAL_\.[ID]\" 2>&1 |" ) || die "open: $! \n";
+
+while (<REPORT>) {
+    print $_;
+    print LOG $_;
+}
+close(REPORT);
+
+
