1259553bfd02c31d70eb2cf47bc047eac35ceeeb
troy@netscape.com <troy@netscape.com>
1999-02-25 05:41:05 +0000

    Fixed it so we clear the NS_FRAME_SYNC_FRAME_AND_VIEW bit, because the default is that it's set

--- layout/generic/nsSplittableFrame.cpp
+++ layout/generic/nsSplittableFrame.cpp
@@ -39,8 +39,8 @@ nsSplittableFrame::Init(nsIPresContext&  aPresContext,
     // Make sure the general flags bits are the same
     nsFrameState  state;
     aPrevInFlow->GetFrameState(&state);
-    if (state & NS_FRAME_SYNC_FRAME_AND_VIEW) {
-      mState |= NS_FRAME_SYNC_FRAME_AND_VIEW;
+    if ((state & NS_FRAME_SYNC_FRAME_AND_VIEW) == 0) {
+      mState &= ~NS_FRAME_SYNC_FRAME_AND_VIEW;
     }
     if (state & NS_FRAME_REPLACED_ELEMENT) {
       mState |= NS_FRAME_REPLACED_ELEMENT;
