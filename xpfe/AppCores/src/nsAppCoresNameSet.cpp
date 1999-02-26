8d7455246a30a8f2e7ec331709eb05bae8e04483
rjc%netscape.com <rjc%netscape.com>
1999-02-26 02:14:51 +0000

    Add in RDFCore.

--- xpfe/AppCores/src/nsAppCoresNameSet.cpp
+++ xpfe/AppCores/src/nsAppCoresNameSet.cpp
@@ -27,6 +27,7 @@
 #include "nsIDOMAppCoresManager.h"
 #include "nsIDOMToolkitCore.h"
 #include "nsIDOMMailCore.h"
+#include "nsIDOMRDFCore.h"
 #include "nsIDOMToolbarCore.h"
 #include "nsIDOMBrowserAppCore.h"
 #include "nsIDOMEditorAppCore.h"
@@ -37,6 +38,7 @@ static NS_DEFINE_IID(kIScriptExternalNameSetIID, NS_ISCRIPTEXTERNALNAMESET_IID);
 static NS_DEFINE_IID(kAppCoresCID,           NS_APPCORESMANAGER_CID);
 static NS_DEFINE_IID(kToolkitCoreCID,        NS_TOOLKITCORE_CID);
 static NS_DEFINE_IID(kMailCoreCID,           NS_MAILCORE_CID);
+static NS_DEFINE_IID(kRDFCoreCID,            NS_RDFCORE_CID);
 static NS_DEFINE_IID(kToolbarCoreCID,        NS_TOOLBARCORE_CID);
 static NS_DEFINE_IID(kBrowserAppCoreCID,     NS_BROWSERAPPCORE_CID);
 static NS_DEFINE_IID(kEditorAppCoreCID,      NS_EDITORAPPCORE_CID);
@@ -68,6 +70,7 @@ nsAppCoresNameSet::InitializeClasses(nsIScriptContext* aScriptContext)
     result = NS_InitBrowserAppCoreClass(aScriptContext, nsnull);
     result = NS_InitEditorAppCoreClass(aScriptContext, nsnull);
     result = NS_InitToolkitCoreClass(aScriptContext, nsnull);
+    result = NS_InitRDFCoreClass(aScriptContext, nsnull);
 
     return result;
 }
@@ -86,6 +89,12 @@ nsAppCoresNameSet::AddNameSet(nsIScriptContext* aScriptContext)
     {
         result = manager->RegisterGlobalName("MailCore", 
                                              kMailCoreCID, 
+                                             PR_TRUE);
+
+        if (NS_OK != result) return result;
+
+        result = manager->RegisterGlobalName("RDFCore", 
+                                             kRDFCoreCID, 
                                              PR_TRUE);
 
         if (NS_OK != result) return result;
