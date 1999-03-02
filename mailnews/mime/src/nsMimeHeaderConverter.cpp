2f3dddf297b579631e794f6841cd198388401d48
rhp%netscape.com <rhp%netscape.com>
1999-03-02 16:10:28 +0000

    Adding new header converter interface.

---
+++ mailnews/mime/src/nsMimeHeaderConverter.cpp
@@ -0,0 +1,125 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+#include "stdio.h"
+#include "mimecom.h"
+#include "nscore.h"
+#include "nsIFactory.h"
+#include "nsISupports.h"
+#include "nsMimeHeaderConverter.h"
+
+/* 
+ * This function will be used by the factory to generate an 
+ * mime object class object....
+ */
+nsresult NS_NewMimeHeaderConverter(nsIMimeHeaderConverter ** aInstancePtrResult)
+{
+	/* note this new macro for assertions...they can take 
+     a string describing the assertion */
+	nsresult result = NS_OK;
+	NS_PRECONDITION(nsnull != aInstancePtrResult, "nsnull ptr");
+	if (nsnull != aInstancePtrResult)
+	{
+		nsMimeObjectClassAccess *obj = new nsMimeObjectClassAccess();
+		if (obj)
+			return obj->QueryInterface(nsIMimeObjectClassAccess::IID(), (void**) aInstancePtrResult);
+		else
+			return NS_ERROR_OUT_OF_MEMORY; /* we couldn't allocate the object */
+	}
+	else
+		return NS_ERROR_NULL_POINTER; /* aInstancePtrResult was NULL....*/
+}
+
+/* 
+ * The following macros actually implement addref, release and 
+ * query interface for our component. 
+ */
+NS_IMPL_ADDREF(nsMimeObjectClassAccess)
+NS_IMPL_RELEASE(nsMimeObjectClassAccess)
+NS_IMPL_QUERY_INTERFACE(nsMimeObjectClassAccess, nsIMimeObjectClassAccess::IID()); /* we need to pass in the interface ID of this interface */
+
+/*
+ * nsMimeObjectClassAccess definitions....
+ */
+
+/* 
+ * Inherited methods for nsMimeObjectClassAccess
+ */
+nsMimeObjectClassAccess::nsMimeObjectClassAccess()
+{
+  /* the following macro is used to initialize the ref counting data */
+  NS_INIT_REFCNT();
+}
+
+nsMimeObjectClassAccess::~nsMimeObjectClassAccess()
+{
+}
+
+nsresult
+nsMimeObjectClassAccess::MimeObjectWrite(void *mimeObject, 
+                                char *data, 
+                                PRInt32 length, 
+                                PRBool user_visible_p)
+{
+  int rc = XPCOM_MimeObject_write(mimeObject, data, length, user_visible_p);
+  if (rc < 0)
+    return NS_ERROR_FAILURE;
+  else
+    return NS_OK;
+}
+
+nsresult 
+nsMimeObjectClassAccess::GetmimeInlineTextClass(void **ptr)
+{
+  *ptr = XPCOM_GetmimeInlineTextClass();
+  return NS_OK;
+}
+
+nsresult 
+nsMimeObjectClassAccess::GetmimeLeafClass(void **ptr)
+{
+  *ptr = XPCOM_GetmimeLeafClass();
+  return NS_OK;
+}
+
+nsresult 
+nsMimeObjectClassAccess::GetmimeObjectClass(void **ptr)
+{
+  *ptr = XPCOM_GetmimeObjectClass();
+  return NS_OK;
+}
+
+nsresult 
+nsMimeObjectClassAccess::GetmimeContainerClass(void **ptr)
+{
+  *ptr = XPCOM_GetmimeContainerClass();
+  return NS_OK;
+}
+
+nsresult 
+nsMimeObjectClassAccess::GetmimeMultipartClass(void **ptr)
+{
+  *ptr = XPCOM_GetmimeMultipartClass();
+  return NS_OK;
+}
+
+nsresult 
+nsMimeObjectClassAccess::GetmimeMultipartSignedClass(void **ptr)
+{
+  *ptr = XPCOM_GetmimeMultipartSignedClass();
+  return NS_OK;
+}
---
+++ mailnews/mime/src/nsMimeHeaderConverter.h
@@ -0,0 +1,61 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+ 
+/*
+ * This interface is implemented by libmime. This interface is used by 
+ * a Content-Type handler "Plug In" (i.e. vCard) for accessing various 
+ * internal information about the object class system of libmime. When 
+ * libmime progresses to a C++ object class, this would probably change.
+ */
+#ifndef nsMimeHeaderConverter_h_
+#define nsMimeHeaderConverter_h_
+
+#include "prtypes.h"
+#include "nsIMimeHeaderConverter.h"
+
+// {866A1E11-D0B9-11d2-B373-525400E2D63A}
+#define NS_MIME_HEADER_CONVERTER_CID   \
+        { 0x866a1e11, 0xd0b9, 0x11d2,         \
+        { 0xb3, 0x73, 0x52, 0x54, 0x0, 0xe2, 0xd6, 0x3a } };
+
+class nsMimeHeaderConverter : public nsIMimeHeaderConverter {
+public: 
+  nsMimeHeaderConverter();
+  virtual ~nsMimeHeaderConverter();
+       
+  /* this macro defines QueryInterface, AddRef and Release for this class */
+  NS_DECL_ISUPPORTS 
+
+  // These methods are all implemented by libmime to be used by 
+  // content type handler plugins for processing stream data. 
+
+  // Decode routine
+  NS_IMETHOD DecodeMimePartIIStr(const char *header, 
+                                 const char *charset, 
+                                 char *decodedString);
+  // Encode routine
+  NS_IMETHOD EncodeMimePartIIStr(const char *header, 
+                                 const char *mailCharset, 
+                                 PRInt32     encodedWordSize, 
+                                 char       *encodedString);
+}; 
+
+/* this function will be used by the factory to generate an class access object....*/
+extern nsresult NS_NewMimeHeaderConverter(nsIMimeHeaderConverter **aInstancePtrResult);
+
+#endif /* nsMimeHeaderConverter_h_ */
