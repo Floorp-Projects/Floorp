e11424ecba338db5366c512b2618888f2b1ed311
waterson%netscape.com <waterson%netscape.com>
1999-03-05 11:12:24 +0000

    Added C++-specific #include of nscore.h to make sure that PRUnichar gets included.

--- rdf/base/public/nsRDFInterfaces.h
+++ rdf/base/public/nsRDFInterfaces.h
@@ -6,10 +6,20 @@
 #define __gen_nsRDFInterfaces_h__
 
 #include "nsISupports.h" /* interface nsISupports */
+#include "nsID.h" /* interface nsID */
 
 #ifdef XPIDL_JS_STUBS
 #include "jsapi.h"
 #endif
+#include "nsDebug.h"
+#include "nsTraceRefcnt.h"
+#include "nsID.h"
+#include "nsIID.h"
+#include "nsError.h"
+#include "nsISupportsUtils.h"
+
+#include "nscore.h" // for PRUnichar
+
 
 /* starting interface nsIRDFNode */
 
