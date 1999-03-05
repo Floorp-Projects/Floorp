a9007127b9f1920e603c71fcf79002abcb9eb47d
dcone%netscape.com <dcone%netscape.com>
1999-03-05 14:48:44 +0000

    Update the surfaces to include graphics state

--- gfx/src/mac/nsDrawingSurfaceMac.h
+++ gfx/src/mac/nsDrawingSurfaceMac.h
@@ -21,9 +21,41 @@
 
 #include "nsIDrawingSurface.h"
 #include "nsIDrawingSurfaceMac.h"
+#include "nsFontMetricsMac.h"
 
 
-class GraphicsState;
+class GraphicState
+{
+public:
+  GraphicState();
+  GraphicState(GraphicState* aGS);
+  ~GraphicState();
+
+  NS_DECL_AND_IMPL_ZEROING_OPERATOR_NEW
+
+	void				Clear();
+	void				Init(nsDrawingSurface aSurface);
+	void				Init(GrafPtr aPort);
+	void				Init(nsIWidget* aWindow);
+	void				Duplicate(GraphicState* aGS);	// would you prefer an '=' operator?
+
+protected:
+	RgnHandle		DuplicateRgn(RgnHandle aRgn);
+
+public:
+  nsTransform2D * 			mTMatrix; 					// transform that all the graphics drawn here will obey
+
+	PRInt32               mOffx;
+  PRInt32               mOffy;
+
+  RgnHandle							mMainRegion;
+  RgnHandle			    		mClipRegion;
+
+  nscolor               mColor;
+  PRInt32               mFont;
+  nsIFontMetrics * 			mFontMetrics;
+	PRInt32               mCurrFontHandle;
+};
 
 
 class nsDrawingSurfaceMac : public nsIDrawingSurface,
@@ -45,25 +77,20 @@ public:
   NS_IMETHOD GetPixelFormat(nsPixelFormat *aFormat);
 
   //nsIDrawingSurfaceMac interface
+  NS_IMETHOD Init(nsDrawingSurface aDS);
   NS_IMETHOD Init(GrafPtr aThePort);
   NS_IMETHOD Init(nsIWidget *aTheWidget);
   NS_IMETHOD Init(PRUint32 aDepth,PRUint32 aWidth, PRUint32 aHeight,PRUint32 aFlags);
 	NS_IMETHOD GetGrafPtr(GrafPtr	*aTheGrafPtr)   	{*aTheGrafPtr = mPort;return NS_OK;}
-	NS_IMETHOD GetOffset(PRInt32	*aXOffset,PRInt32 *aYOffset) {*aXOffset=mOffx;*aYOffset=mOffy;return NS_OK;}
 
   // locals
-	RgnHandle	GetMainRgn(void) {return mMainRegion;}
-	RgnHandle	GetClipRgn(void) {return mClipRegion;}
+	GraphicState*	GetGS(void) {return mGS;}
 
 private:
   ~nsDrawingSurfaceMac();
 
 	GrafPtr					mPort;						// the onscreen or offscreen GrafPtr;	
 	
-	PRInt32         mOffx;
-  PRInt32         mOffy;
-  RgnHandle				mMainRegion;
-  RgnHandle			  mClipRegion;
   PRUint32      	mWidth;
   PRUint32      	mHeight;
   PRInt32       	mLockOffset;
@@ -71,6 +98,7 @@ private:
   PRUint32      	mLockFlags;
 	PRBool					mIsOffscreen;	
 
+	GraphicState*		mGS;						// a graphics state for the surface
 };
 
 #endif
