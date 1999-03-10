52b5df7ff4538e6ff374759cfc0dee1f4633588c
dougt@netscape.com <dougt@netscape.com>
1999-03-10 18:36:40 +0000

    Removing extra ; to get rid of warning.THIS IS NOT PART OF TREE IS NOT PART OF THE BUILD SYSTEM YET

--- xpinstall/public/nsSoftwareUpdateIIDs.h
+++ xpinstall/public/nsSoftwareUpdateIIDs.h
@@ -7,7 +7,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-};
+}
 
 #define NS_SoftwareUpdateFactory_CID                 \
 { /* 18c2f98a-b09f-11d2-bcde-00805f0e1353 */         \
@@ -15,7 +15,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-};
+}
 
 #define NS_SoftwareUpdateInstall_CID                 \
 { /* 18c2f98b-b09f-11d2-bcde-00805f0e1353 */         \
@@ -23,7 +23,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-};
+}
 
 #define NS_SoftwareUpdateInstallFactory_CID          \
 { /* 18c2f98c-b09f-11d2-bcde-00805f0e1353 */         \
@@ -31,7 +31,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-};
+}
 
 #define NS_SoftwareUpdateInstallTrigger_CID          \
 { /* 18c2f98d-b09f-11d2-bcde-00805f0e1353 */         \
@@ -39,7 +39,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-};
+}
 
 #define NS_SoftwareUpdateInstallTriggerFactory_CID   \
 { /* 18c2f98e-b09f-11d2-bcde-00805f0e1353 */         \
@@ -47,7 +47,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-};
+}
 
 #define NS_SoftwareUpdateInstallVersion_CID          \
 { /* 18c2f98f-b09f-11d2-bcde-00805f0e1353 */         \
@@ -55,7 +55,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-};
+}
 
 #define NS_SoftwareUpdateInstallVersionFactory_CID   \
 { /* 18c2f990-b09f-11d2-bcde-00805f0e1353 */         \
@@ -63,7 +63,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-};
+}
 
 #define NS_SoftwareUpdateInstallFolder_CID          \
 { /* 18c2f991-b09f-11d2-bcde-00805f0e1353 */         \
@@ -71,7 +71,7 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-};
+}
 
 #define NS_SoftwareUpdateInstallFolderFactory_CID   \
 { /* 18c2f992-b09f-11d2-bcde-00805f0e1353 */         \
@@ -79,6 +79,6 @@
     0xb09f,                                          \
     0x11d2,                                          \
     {0xbc, 0xde, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} \
-};
+}
 
 #endif /* nsSoftwareUpdateIIDs_h___ */
