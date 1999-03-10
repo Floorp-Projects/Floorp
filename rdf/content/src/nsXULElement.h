040e6291190ec680654db7ee21455fac4a33440e
waterson%netscape.com <waterson%netscape.com>
1999-03-10 05:37:31 +0000

    Landed on tip.

---
+++ rdf/content/src/nsXULElement.h
@@ -0,0 +1,71 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+#ifndef nsXULElement_h__
+#define nsXULElement_h__
+
+#include "nsIDOMXULElement.h"
+class nsXULElement : public nsISupports
+{
+protected:
+    nsIDOMXULElement* mOuter;
+    nsXULElement(nsIDOMXULElement* aOuter) : mOuter(aOuter) {}
+    
+public:
+    virtual ~nsXULElement() {};
+
+    // nsISupports interface. Subclasses should use the
+    // NS_DECL/IMPL_ISUPPORTS_INHERITED macros to implement the
+    // nsISupports interface.
+    NS_IMETHOD_(nsrefcnt) AddRef() {
+        return mOuter->AddRef();
+    }
+
+    NS_IMETHOD_(nsrefcnt) Release() {
+        return mOuter->Release();
+    }
+
+    NS_IMETHOD QueryInterface(REFNSIID aIID, void** aResult) {
+        return mOuter->QueryInterface(aIID, aResult);
+    }
+};
+
+#define NS_IMPL_XULELEMENT_ISUPPORTS                            \
+    NS_IMETHOD_(nsrefcnt) AddRef() {                            \
+        return mOuter->AddRef();                                \
+    }                                                           \
+                                                                \
+    NS_IMETHOD_(nsrefcnt) Release() {                           \
+        return mOuter->Release();                               \
+    }                                                           \
+                                                                \
+    NS_IMETHOD QueryInterface(REFNSIID aIID, void** aResult) {  \
+        if (aIID.Equals(GetDOMIID())) {                         \
+            *aResult = this;                                    \
+            NS_ADDREF(this);                                    \
+            return NS_OK;                                       \
+        }                                                       \
+        else {                                                  \
+            return mOuter->QueryInterface(aIID, aResult);       \
+        }                                                       \
+    }
+
+
+
+#endif // nsXULElement_h__
