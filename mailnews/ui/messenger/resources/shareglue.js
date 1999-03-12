7838849ad7e63a48ed02f4b2435e220cd6e2e1bf
mscott%netscape.com <mscott%netscape.com>
1999-03-12 04:13:53 +0000

    Changes for naoki to implement set character set function.

--- mailnews/ui/messenger/resources/shareglue.js
+++ mailnews/ui/messenger/resources/shareglue.js
@@ -8,6 +8,23 @@ function Print() {}
 function Close() {}
 function Exit() {}
 function CharacterSet() {}
+function SetDocumentCharacterSet(aCharset)
+{
+  var appCore = XPAppCoresManager.Find("MsgAppCore");  
+  if (appCore == null) {
+    dump("StartUp: Creating AppCore\n");
+    appCore = new MsgAppCore();
+  }
+  if (appCore != null) {
+    dump(aCharset);dump("\n");
+    appCore.Init("MsgAppCore");
+    appCore.SetWindow(window);
+    appCore.SetDocumentCharset(aCharset);
+  } else {
+    dump("MsgAppCore has not been created!\n");
+  }
+}
+
 function NavigatorWindow() {}
 function MessengerWindow() {}
 function ComposerWindow() {}
