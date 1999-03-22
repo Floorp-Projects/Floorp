bf82500ce5be32744c5e1bfbd75a46c135aeba69
rickg%netscape.com <rickg%netscape.com>
1999-03-22 22:26:17 +0000

    fixed link error

--- xpcom/ds/nsCRT.cpp
+++ xpcom/ds/nsCRT.cpp
@@ -91,7 +91,7 @@ static const unsigned char kLower2Upper[256] = {
 };
 
 // XXX bug: this doesn't map 0x80 to 0x9f properly
-static const PRUnichar kIsoLatin1ToUCS2[256] = {
+ const PRUnichar kIsoLatin1ToUCS2[256] = {
     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
    16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
    32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
