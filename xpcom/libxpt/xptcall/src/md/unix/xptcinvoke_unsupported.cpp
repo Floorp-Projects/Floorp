9a923d21309493cd73853bf7ed7aeae8681aa15c
jband%netscape.com <jband%netscape.com>
1999-03-23 22:16:14 +0000

    add implementation files for unsupported platforms to allow clean build on all platforms even though we'll have failure at runtime on unsupported platforms

---
+++ xpcom/libxpt/xptcall/src/md/unix/xptcinvoke_unsupported.cpp
@@ -0,0 +1,29 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/* Platform specific code to invoke XPCOM methods on native objects */
+
+#include "xptcprivate.h"
+
+XPTC_PUBLIC_API(nsresult)
+XPTC_InvokeByIndex(nsISupports* that, PRUint32 methodIndex,
+                   PRUint32 paramCount, nsXPTCVariant* params)
+{
+    NS_ASSERTION(0,"XPTC_InvokeByIndex called on unsupported platform");
+    return NS_ERROR_NOT_IMPLEMENTED;
+}    
