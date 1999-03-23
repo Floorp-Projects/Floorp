ef0f434cf41a1caba69096a8913b026b4f10feb3
nhotta@netscape.com <nhotta@netscape.com>
1999-03-23 22:57:11 +0000

    Changed error code checking in NSRegisterSelf as suggested by ftang.

--- intl/locale/src/unix/nsLocaleSO.cpp
+++ intl/locale/src/unix/nsLocaleSO.cpp
@@ -120,28 +120,28 @@ extern "C" NS_EXPORT nsresult NSRegisterSelf(nsISupports* aServMgr, const char *
   //
   rv = compMgr->RegisterComponent(kLocaleFactoryCID,NULL,NULL,path,PR_TRUE,PR_TRUE);
   NS_ASSERTION(rv==NS_OK,"nsLocaleTest: RegisterFactory failed.");
-  if (rv!=NS_OK) goto done;
+  if(NS_FAILED(rv) && (NS_ERROR_FACTORY_EXISTS != rv)) goto done;
 
   //
   // register the collation factory
   //
   rv = compMgr->RegisterComponent(kCollationFactoryCID, NULL, NULL, path, PR_TRUE, PR_TRUE);
   NS_ASSERTION(rv==NS_OK,"nsLocaleTest: Register CollationFactory failed.");
-  if (NS_FAILED(rv)) goto done;
+  if(NS_FAILED(rv) && (NS_ERROR_FACTORY_EXISTS != rv)) goto done;
   
   //
   // register the collation interface
   //
   rv = compMgr->RegisterComponent(kCollationCID, NULL, NULL, path, PR_TRUE, PR_TRUE);
   NS_ASSERTION(rv==NS_OK,"nsLocaleTest: Register Collation failed.");
-  if (NS_FAILED(rv)) goto done;
+  if(NS_FAILED(rv) && (NS_ERROR_FACTORY_EXISTS != rv)) goto done;
   
   //
   // register the date time formatter
   //
   rv = compMgr->RegisterComponent(kDateTimeFormatCID, NULL, NULL, path, PR_TRUE, PR_TRUE);
   NS_ASSERTION(rv==NS_OK,"nsLocaleTest: Register DateTimeFormat failed.");
-  if (NS_FAILED(rv)) goto done;
+  if(NS_FAILED(rv) && (NS_ERROR_FACTORY_EXISTS != rv)) goto done;
 
   done:
   (void)servMgr->ReleaseService(kComponentManagerCID, compMgr);
@@ -162,16 +162,16 @@ extern "C" NS_EXPORT nsresult NSUnregisterSelf(nsISupports* aServMgr, const char
   if (NS_FAILED(rv)) return rv;
 
   rv = compMgr->UnregisterFactory(kLocaleFactoryCID, path);
-  if (rv!=NS_OK) goto done;
+  if (NS_FAILED(rv)) goto done;
 
   rv = compMgr->UnregisterFactory(kCollationFactoryCID, path);
-  if (rv!=NS_OK) goto done;
+  if (NS_FAILED(rv)) goto done;
 
   rv = compMgr->UnregisterFactory(kCollationCID, path);
-  if (rv!=NS_OK) goto done;
+  if (NS_FAILED(rv)) goto done;
 
   rv = compMgr->UnregisterFactory(kDateTimeFormatCID, path);
-  if (rv!=NS_OK) goto done;
+  if (NS_FAILED(rv)) goto done;
 
   done:
   (void)servMgr->ReleaseService(kComponentManagerCID, compMgr);
