32f4f055deff8506b4c6856c93193d3254b4e523
chuang%netscape.com <chuang%netscape.com>
1999-03-24 08:33:08 +0000

    Update messenger start up url

--- xpfe/bootstrap/nsAppRunner.cpp
+++ xpfe/bootstrap/nsAppRunner.cpp
@@ -180,7 +180,7 @@ int main(int argc, char* argv[])
       if (NS_SUCCEEDED(rv))
       {
         if (cmdResult && (strcmp("1",cmdResult)==0))
-          urlstr = "resource:/res/samples/mailshell.xul";
+          urlstr = "resource:/res/mailnews/messenger/mailshell.xul";
       }
     }
     if (nsnull == urlstr)
