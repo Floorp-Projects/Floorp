49414179b21f1cbeda531bed21869d37c5070f5b
dougt%netscape.com <dougt%netscape.com>
1999-03-24 02:16:41 +0000

    Added ScheduledTasks (rename and delete).

---
+++ xpinstall/src/ScheduledTasks.h
@@ -0,0 +1,40 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Communicator client code, 
+ * released March 31, 1998. 
+ *
+ * The Initial Developer of the Original Code is Netscape Communications 
+ * Corporation.  Portions created by Netscape are 
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ *
+ * Contributors:
+ *     Daniel Veditz <dveditz@netscape.com>
+ *     Douglas Turner <dougt@netscape.com>
+ */
+
+
+#ifndef __SCHEDULEDTASKS_H__
+#define __SCHEDULEDTASKS_H__
+
+
+#include "NSReg.h"
+
+REGERR DeleteFileLater(const char * filename);
+REGERR ReplaceFileLater(const char *tmpfile, const char *target );
+
+void DeleteScheduledFiles(void);
+void ReplaceScheduledFiles(void);
+
+
+#endif
