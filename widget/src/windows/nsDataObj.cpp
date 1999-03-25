2bbd629399203ba651812e01ad030efa40a4593a
mcafee@netscape.com <mcafee@netscape.com>
1999-03-25 04:38:25 +0000

    Adding Mozilla license

--- widget/src/windows/nsDataObj.cpp
+++ widget/src/windows/nsDataObj.cpp
@@ -1,15 +1,20 @@
-/*Copyright Notice ===============================================*
-	This file contains proprietary information of Digital Style
-	Corporation. Copying or reproducing without prior written
-	approval is prohibited.
-                                       
-	Copyright (c) 1995           
-	Digital Style Corporation
-	16885 Via Del Campo Court #202
-	San Diego, CA 92127
-	(619)-673-5050
-
- *Copyright Notice ===============================================*/
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
 
 #include "Ddcomm.h"
 //#include "DragDrop.h"
