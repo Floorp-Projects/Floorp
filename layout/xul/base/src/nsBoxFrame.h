14d0e6458ab8fa6870341d23922f4eaa01bc6a87
evaughan@netscape.com <evaughan@netscape.com>
1999-03-26 18:51:08 +0000

    Added box layout system.

---
+++ layout/xul/base/src/nsBoxFrame.h
@@ -0,0 +1,95 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+/**
+
+  Eric D Vaughan
+  This class lays out its children either vertically or horizontally
+ 
+**/
+
+#include "nsHTMLContainerFrame.h"
+class nsBoxDataSpring;
+
+class nsBoxFrame : public nsHTMLContainerFrame
+{
+public:
+
+  friend nsresult NS_NewBoxFrame(nsIFrame*& aNewFrame);
+
+  NS_IMETHOD FlowChildAt(nsIFrame* frame, 
+                     nsIPresContext& aPresContext,
+                     nsHTMLReflowMetrics&     aDesiredSize,
+                     const nsHTMLReflowState& aReflowState,
+                     nsReflowStatus&          aStatus,
+                     const nsSize& size);
+
+
+    NS_IMETHOD  Init(nsIPresContext&  aPresContext,
+                   nsIContent*      aContent,
+                   nsIFrame*        aParent,
+                   nsIStyleContext* aContext,
+                   nsIFrame*        asPrevInFlow);
+
+   NS_IMETHOD  ReResolveStyleContext ( nsIPresContext* aPresContext, 
+                                      nsIStyleContext* aParentContext,
+                                      PRInt32 aParentChange,
+                                      nsStyleChangeList* aChangeList,
+                                      PRInt32* aLocalChange) ;
+
+  NS_IMETHOD AttributeChanged(nsIPresContext* aPresContext,
+                              nsIContent* aChild,
+                              nsIAtom* aAttribute,
+                              PRInt32 aHint);
+
+
+    // nsIHTMLReflow overrides
+  NS_IMETHOD Reflow(nsIPresContext&          aPresContext,
+                    nsHTMLReflowMetrics&     aDesiredSize,
+                    const nsHTMLReflowState& aReflowState,
+                    nsReflowStatus&          aStatus);
+
+  NS_IMETHOD  Paint(nsIPresContext& aPresContext,
+                    nsIRenderingContext& aRenderingContext,
+                    const nsRect& aDirtyRect,
+                    nsFramePaintLayer aWhichLayer);
+
+  NS_IMETHOD HandleEvent(nsIPresContext& aPresContext, 
+                         nsGUIEvent* aEvent,
+                         nsEventStatus& aEventStatus);
+
+ 
+protected:
+
+	  virtual void GetDesiredSize(nsIPresContext* aPresContext,
+                              const nsHTMLReflowState& aReflowState,
+                              nsHTMLReflowMetrics& aDesiredSize);
+
+    virtual PRIntn GetSkipSides() const { return 0; }
+
+    nsBoxFrame();
+
+    virtual void Stretch(nsBoxDataSpring* springs, PRInt32 nSprings, nscoord& size);
+
+private: 
+
+  PRBool mHorizontal;
+ 
+}; // class nsBoxFrame
+
