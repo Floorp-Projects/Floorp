7c06b41bc49dc641b228155504febb601b76cdeb
amusil%netscape.com <amusil%netscape.com>
1999-03-29 22:11:06 +0000

    Code to handle locating and loading plugin info.

---
+++ modules/plugin/base/src/nsPluginsDirUNIX.cpp
@@ -0,0 +1,79 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/*
+	nsPluginsDirUNIX.cpp
+	
+	Windows implementation of the nsPluginsDir/nsPluginsFile classes.
+	
+	by Alex Musil
+ */
+
+#include "nsPluginsDir.h"
+#include "prlink.h"
+#include "plstr.h"
+#include "prmem.h"
+
+/* nsPluginsDir implementation */
+
+nsPluginsDir::nsPluginsDir()
+{
+
+}
+
+nsPluginsDir::~nsPluginsDir()
+{
+	// do nothing
+}
+
+PRBool nsPluginsDir::IsPluginFile(const nsFileSpec& fileSpec)
+{
+	return false;
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+/* nsPluginFile implementation */
+
+nsPluginFile::nsPluginFile(const nsFileSpec& spec)
+	:	nsFileSpec(spec)
+{
+	// nada
+}
+
+nsPluginFile::~nsPluginFile()
+{
+	// nada
+}
+
+/**
+ * Loads the plugin into memory using NSPR's shared-library loading
+ * mechanism. Handles platform differences in loading shared libraries.
+ */
+nsresult nsPluginFile::LoadPlugin(PRLibrary* &outLibrary)
+{
+	return NS_ERROR_FAILURE;
+}
+
+/**
+ * Obtains all of the information currently available for this plugin.
+ */
+nsresult nsPluginFile::GetPluginInfo(nsPluginInfo& info)
+{
+	return NS_ERROR_FAILURE;
+}
