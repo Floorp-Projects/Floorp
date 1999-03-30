b1bf2fdb3e7cc26d6cdabcab3387f2002cea6cd2
rhp%netscape.com <rhp%netscape.com>
1999-03-30 02:19:05 +0000

    New XML emitter for libmime

---
+++ mailnews/mime/emitters/xml/nsEmitterFactory.cpp
@@ -0,0 +1,242 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "nsIFactory.h"
+#include "nsISupports.h"
+#include "msgCore.h"
+#include "nsCOMPtr.h"
+#include "pratom.h"
+
+/* Include all of the interfaces our factory can generate components for */
+#include "nsIMimeEmitter.h"
+#include "nsMimeEmitter.h"
+
+
+static NS_DEFINE_CID(kComponentManagerCID, NS_COMPONENTMANAGER_CID);
+static NS_DEFINE_CID(kMimeEmitterCID, NS_XML_MIME_EMITTER_CID);
+
+////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////
+static PRInt32 g_InstanceCount = 0;
+static PRInt32 g_LockCount = 0;
+
+class nsMsgFactory : public nsIFactory
+{   
+public:
+	// nsISupports methods
+	NS_DECL_ISUPPORTS 
+
+  nsMsgFactory(const nsCID &aClass,
+               const char* aClassName,
+               const char* aProgID,
+               nsISupports*);
+
+  // nsIFactory methods   
+  NS_IMETHOD CreateInstance(nsISupports *aOuter, const nsIID &aIID, void **aResult);   
+  NS_IMETHOD LockFactory(PRBool aLock);   
+
+protected:
+  virtual ~nsMsgFactory();   
+
+  nsCID mClassID;
+  char* mClassName;
+  char* mProgID;
+  nsIServiceManager* mServiceManager;
+};   
+
+nsMsgFactory::nsMsgFactory(const nsCID &aClass,
+                           const char* aClassName,
+                           const char* aProgID,
+                           nsISupports *compMgrSupports)
+  : mClassID(aClass),
+    mClassName(nsCRT::strdup(aClassName)),
+    mProgID(nsCRT::strdup(aProgID))
+{
+	NS_INIT_REFCNT();
+
+  // store a copy of the 
+  compMgrSupports->QueryInterface(nsIServiceManager::GetIID(),
+                                  (void **)&mServiceManager);
+}   
+
+nsMsgFactory::~nsMsgFactory()   
+{
+	NS_ASSERTION(mRefCnt == 0, "non-zero refcnt at destruction");
+  
+  NS_IF_RELEASE(mServiceManager);
+  delete[] mClassName;
+  delete[] mProgID;
+}   
+
+nsresult
+nsMsgFactory::QueryInterface(const nsIID &aIID, void **aResult)   
+{   
+  if (aResult == NULL)  
+    return NS_ERROR_NULL_POINTER;  
+
+  // Always NULL result, in case of failure   
+  *aResult = NULL;   
+
+  // we support two interfaces....nsISupports and nsFactory.....
+  if (aIID.Equals(::nsISupports::GetIID()))    
+    *aResult = (void *)(nsISupports*)this;   
+  else if (aIID.Equals(nsIFactory::GetIID()))   
+    *aResult = (void *)(nsIFactory*)this;   
+
+  if (*aResult == NULL)
+    return NS_NOINTERFACE;
+
+  AddRef(); // Increase reference count for caller   
+  return NS_OK;   
+}   
+
+NS_IMPL_ADDREF(nsMsgFactory)
+NS_IMPL_RELEASE(nsMsgFactory)
+
+nsresult
+nsMsgFactory::CreateInstance(nsISupports *aOuter,
+                             const nsIID &aIID,
+                             void **aResult)  
+{  
+	nsresult res = NS_OK;
+
+	if (aResult == NULL)  
+		return NS_ERROR_NULL_POINTER;  
+
+	*aResult = NULL;  
+  
+	nsISupports *inst = nsnull;
+
+	// ClassID check happens here
+	// Whenever you add a new class that supports an interface, plug it in here!!!
+	
+	// do they want a mime emitter interface ?
+	if (mClassID.Equals(kMimeEmitterCID)) 
+	{
+		res = NS_NewMimeEmitter((nsIMimeEmitter **) &inst);
+		if (NS_FAILED(res))  // was there a problem creating the object ?
+		  return res;   
+	}
+
+	// End of checking the interface ID code....
+	if (inst) 
+	{
+		// so we now have the class that supports the desired interface...we need to turn around and
+		// query for our desired interface.....
+		res = inst->QueryInterface(aIID, aResult);
+		if (res != NS_OK)  // if the query interface failed for some reason, then the object did not get ref counted...delete it.
+			delete inst; 
+	}
+	else
+		res = NS_ERROR_OUT_OF_MEMORY;
+
+  return res;  
+}  
+
+nsresult
+nsMsgFactory::LockFactory(PRBool aLock)  
+{  
+	if (aLock)
+		PR_AtomicIncrement(&g_LockCount); 
+	else
+		PR_AtomicDecrement(&g_LockCount);
+
+	return NS_OK;
+}  
+
+////////////////////////////////////////////////////////////////////////////////
+
+// return the proper factory to the caller. 
+extern "C" NS_EXPORT nsresult NSGetFactory(nsISupports* aServMgr,
+                                           const nsCID &aClass,
+                                           const char *aClassName,
+                                           const char *aProgID,
+                                           nsIFactory **aFactory)
+{
+	if (nsnull == aFactory)
+		return NS_ERROR_NULL_POINTER;
+
+  *aFactory = new nsMsgFactory(aClass, aClassName, aProgID, aServMgr);
+  if (aFactory)
+    return (*aFactory)->QueryInterface(nsIFactory::GetIID(),
+                                       (void**)aFactory);
+  else
+    return NS_ERROR_OUT_OF_MEMORY;
+}
+
+extern "C" NS_EXPORT PRBool NSCanUnload(nsISupports* aServMgr) 
+{
+	return PRBool(g_InstanceCount == 0 && g_LockCount == 0);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+extern "C" NS_EXPORT nsresult
+NSRegisterSelf(nsISupports* aServMgr, const char* path)
+{
+  nsresult rv;
+
+  nsCOMPtr<nsIServiceManager> servMgr(do_QueryInterface(aServMgr, &rv));
+  if (NS_FAILED(rv)) return rv;
+
+  nsIComponentManager* compMgr;
+  rv = servMgr->GetService(kComponentManagerCID, 
+                           nsIComponentManager::GetIID(), 
+                           (nsISupports**)&compMgr);
+  if (NS_FAILED(rv)) return rv;
+
+  rv = compMgr->RegisterComponent(kMimeEmitterCID,
+                                       "RFC822 Parser",
+                                       nsnull,
+                                       path, PR_TRUE, PR_TRUE);
+  if (NS_FAILED(rv)) goto done;
+
+#ifdef NS_DEBUG
+  printf("*** Register XML MIME Emitter...\n");
+#endif
+
+  done:
+  (void)servMgr->ReleaseService(kComponentManagerCID, compMgr);
+  return rv;
+}
+
+extern "C" NS_EXPORT nsresult
+NSUnregisterSelf(nsISupports* aServMgr, const char* path)
+{
+  nsresult rv;
+
+  nsCOMPtr<nsIServiceManager> servMgr(do_QueryInterface(aServMgr, &rv));
+  if (NS_FAILED(rv)) return rv;
+
+  nsIComponentManager* compMgr;
+  rv = servMgr->GetService(kComponentManagerCID, 
+                           nsIComponentManager::GetIID(), 
+                           (nsISupports**)&compMgr);
+  if (NS_FAILED(rv)) return rv;
+
+  rv = compMgr->UnregisterComponent(kMimeEmitterCID, path);
+  if (NS_FAILED(rv)) goto done;
+
+  done:
+  (void)servMgr->ReleaseService(kComponentManagerCID, compMgr);
+  return rv;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
---
+++ mailnews/mime/emitters/xml/nsMimeEmitter.cpp
@@ -0,0 +1,367 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+#include "stdio.h"
+#include "nsMimeRebuffer.h"
+#include "nsMimeEmitter.h"
+#include "plstr.h"
+#include "nsEmitterUtils.h"
+#include "nsMailHeaders.h"
+
+/* 
+ * This function will be used by the factory to generate an 
+ * mime object class object....
+ */
+nsresult NS_NewMimeEmitter(nsIMimeEmitter ** aInstancePtrResult)
+{
+	/* note this new macro for assertions...they can take 
+     a string describing the assertion */
+	nsresult result = NS_OK;
+	NS_PRECONDITION(nsnull != aInstancePtrResult, "nsnull ptr");
+	if (nsnull != aInstancePtrResult)
+	{
+		nsMimeEmitter *obj = new nsMimeEmitter();
+		if (obj)
+			return obj->QueryInterface(nsIMimeEmitter::GetIID(), (void**) aInstancePtrResult);
+		else
+			return NS_ERROR_OUT_OF_MEMORY; /* we couldn't allocate the object */
+	}
+	else
+		return NS_ERROR_NULL_POINTER; /* aInstancePtrResult was NULL....*/
+}
+
+/* 
+ * The following macros actually implement addref, release and 
+ * query interface for our component. 
+ */
+NS_IMPL_ADDREF(nsMimeEmitter)
+NS_IMPL_RELEASE(nsMimeEmitter)
+NS_IMPL_QUERY_INTERFACE(nsMimeEmitter, nsIMimeEmitter::GetIID()); /* we need to pass in the interface ID of this interface */
+
+/*
+ * nsIMimeEmitter definitions....
+ */
+nsMimeEmitter::nsMimeEmitter()
+{
+  /* the following macro is used to initialize the ref counting data */
+  NS_INIT_REFCNT();
+  
+  mOutStream = NULL;
+  mBufferMgr = NULL;
+  mTotalWritten = 0;
+  mTotalRead = 0;
+  mDocHeader = PR_FALSE;
+  mXMLHeaderStarted = PR_FALSE;
+  mAttachCount = 0;
+
+#ifdef NS_DEBUG
+  mLogFile = NULL;    /* Temp file to put generated XML into. */
+  mReallyOutput = PR_FALSE;
+#endif
+}
+
+nsMimeEmitter::~nsMimeEmitter(void)
+{
+  if (mBufferMgr)
+    delete mBufferMgr;
+}
+
+// Set the output stream for processed data.
+nsresult
+nsMimeEmitter::SetOutputStream(nsINetOStream *outStream)
+{
+  return NS_OK;
+}
+
+// Note - these is setup only...you should not write
+// anything to the stream since these may be image data
+// output streams, etc...
+nsresult       
+nsMimeEmitter::Initialize(nsINetOStream *outStream)
+{
+  mOutStream = outStream;
+
+  // Create rebuffering object
+  mBufferMgr = new MimeRebuffer();
+
+  // Counters for output stream
+  mTotalWritten = 0;
+  mTotalRead = 0;
+
+#ifdef DEBUG
+  PR_Delete("C:\\mail.xml");
+  mLogFile = PR_Open("C:\\mail.xml", PR_RDWR | PR_CREATE_FILE | PR_TRUNCATE, 493);
+#endif /* DEBUG */
+
+  return NS_OK;
+}
+
+// Note - this is teardown only...you should not write
+// anything to the stream since these may be image data
+// output streams, etc...
+nsresult
+nsMimeEmitter::Complete()
+{
+#ifdef DEBUG
+  mReallyOutput = PR_TRUE;
+#endif
+
+  UtilityWrite("</message>");
+
+  // If we are here and still have data to write, we should try
+  // to flush it...if we try and fail, we should probably return
+  // an error!
+  PRUint32      written; 
+  if (mBufferMgr->GetSize() > 0)
+    Write("", 0, &written);
+
+  printf("TOTAL WRITTEN = %d\n", mTotalWritten);
+  printf("LEFTOVERS     = %d\n", mBufferMgr->GetSize());
+
+#ifdef DEBUG
+  if (mLogFile) 
+    PR_Close(mLogFile);
+#endif
+
+  return NS_OK;
+}
+
+nsresult
+nsMimeEmitter::WriteXMLHeader(const char *msgID)
+{
+
+  UtilityWrite("<?xml version=\"1.0\"?>");
+  UtilityWrite("<?xml-stylesheet href=\"resource:/res/mail.css\" type=\"text/css\"?>");
+
+  UtilityWrite("<message id=\"");
+  UtilityWrite(msgID);
+  UtilityWrite("\">");
+
+  mXMLHeaderStarted = PR_TRUE;
+  return NS_OK;
+}
+
+nsresult
+nsMimeEmitter::WriteXMLTag(const char *tagName, const char *value)
+{
+  UtilityWrite("<");
+  UtilityWrite(tagName);
+  UtilityWrite(">");
+
+  UtilityWrite(tagName);
+  UtilityWrite(": ");
+  UtilityWrite(value);
+
+  UtilityWrite("</");
+  UtilityWrite(tagName);
+  UtilityWrite(">");
+  return NS_OK;
+}
+
+
+// Header handling routines.
+nsresult
+nsMimeEmitter::StartHeader(PRBool rootMailHeader, PRBool headerOnly, const char *msgID)
+{
+#ifdef DEBUG
+  mReallyOutput = PR_TRUE;
+#endif
+
+  mDocHeader = rootMailHeader;
+  WriteXMLHeader(msgID);
+  UtilityWrite("<mailheader>");
+
+  return NS_OK;
+}
+
+nsresult
+nsMimeEmitter::AddHeaderField(const char *field, const char *value)
+{
+#ifdef DEBUG
+  mReallyOutput = PR_TRUE;
+#endif
+
+  if ( (!field) || (!value) )
+    return NS_OK;
+
+  WriteXMLTag(field, value);
+  return NS_OK;
+}
+
+nsresult
+nsMimeEmitter::EndHeader()
+{
+#ifdef DEBUG
+  mReallyOutput = PR_TRUE;
+#endif
+
+  UtilityWrite("</mailheader>");
+  return NS_OK;
+}
+
+// Attachment handling routines
+nsresult
+nsMimeEmitter::StartAttachment(const char *name, const char *contentType, const char *url)
+{
+  char    buf[128];
+#ifdef DEBUG
+  mReallyOutput = PR_TRUE;
+#endif
+
+  ++mAttachCount;
+
+  sprintf(buf, "<mailattachment id=%d>", mAttachCount);
+  UtilityWrite(buf);
+
+  return NS_OK;
+}
+
+nsresult
+nsMimeEmitter::AddAttachmentField(const char *field, const char *value)
+{
+#ifdef DEBUG
+  mReallyOutput = PR_TRUE;
+#endif
+
+  WriteXMLTag(field, value);
+  return NS_OK;
+}
+
+nsresult
+nsMimeEmitter::EndAttachment()
+{
+#ifdef DEBUG
+  mReallyOutput = PR_TRUE;
+#endif
+
+  UtilityWrite("</mailattachment>");
+  return NS_OK;
+}
+
+// Attachment handling routines
+nsresult
+nsMimeEmitter::StartBody(PRBool bodyOnly, const char *msgID)
+{
+#ifdef DEBUG
+  mReallyOutput = PR_TRUE;
+#endif
+
+  if (bodyOnly && !mXMLHeaderStarted)
+    WriteXMLHeader(msgID);
+
+  UtilityWrite("<mailbody>");
+  return NS_OK;
+}
+
+nsresult
+nsMimeEmitter::WriteBody(const char *buf, PRUint32 size, PRUint32 *amountWritten)
+{
+#ifdef DEBUG
+  mReallyOutput = PR_TRUE;
+#endif
+
+  Write(buf, size, amountWritten);
+  return NS_OK;
+}
+
+nsresult
+nsMimeEmitter::EndBody()
+{
+#ifdef DEBUG
+  mReallyOutput = PR_TRUE;
+#endif
+
+  UtilityWrite("</mailbody>");
+  return NS_OK;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// These are routines necessary for the C based routines in libmime
+// to access the new world streams.
+////////////////////////////////////////////////////////////////////////////////
+nsresult
+nsMimeEmitter::Write(const char *buf, PRUint32 size, PRUint32 *amountWritten)
+{
+  unsigned int        written = 0;
+  PRUint32            rc, aReadyCount = 0;
+
+#ifdef DEBUG
+  if ((mLogFile) && (mReallyOutput))
+    PR_Write(mLogFile, buf, size);
+#endif
+
+  //
+  // Make sure that the buffer we are "pushing" into has enough room
+  // for the write operation. If not, we have to buffer, return, and get
+  // it on the next time through
+  //
+  *amountWritten = 0;
+  rc = mOutStream->WriteReady(&aReadyCount);
+
+  // First, handle any old buffer data...
+  if (mBufferMgr->GetSize() > 0)
+  {
+    if (aReadyCount >= mBufferMgr->GetSize())
+    {
+      rc += mOutStream->Write(mBufferMgr->GetBuffer(), 
+                            mBufferMgr->GetSize(), &written);
+      mTotalWritten += written;
+      mBufferMgr->ReduceBuffer(written);
+    }
+    else
+    {
+      rc += mOutStream->Write(mBufferMgr->GetBuffer(),
+                             aReadyCount, &written);
+      mTotalWritten += written;
+      mBufferMgr->ReduceBuffer(written);
+      mBufferMgr->IncreaseBuffer(buf, size);
+      return rc;
+    }
+  }
+
+  // Now, deal with the new data the best way possible...
+  rc = mOutStream->WriteReady(&aReadyCount);
+  if (aReadyCount >= size)
+  {
+    rc += mOutStream->Write(buf, size, &written);
+    mTotalWritten += written;
+    *amountWritten = written;
+    return rc;
+  }
+  else
+  {
+    rc += mOutStream->Write(buf, aReadyCount, &written);
+    mTotalWritten += written;
+    mBufferMgr->IncreaseBuffer(buf+written, (size-written));
+    *amountWritten = written;
+    return rc;
+  }
+}
+
+nsresult
+nsMimeEmitter::UtilityWrite(const char *buf)
+{
+  PRInt32     tmpLen = PL_strlen(buf);
+  PRUint32    written;
+
+  Write(buf, tmpLen, &written);
+#ifdef DEBUG
+//  Write("\r\n", 2, &written);
+#endif
+
+  return NS_OK;
+}
