559d5946ebf2d268d743f6debd8c9e461330547e
kipp@netscape.com <kipp@netscape.com>
1999-03-31 04:08:07 +0000

    Added eStyleUnit_Chars

--- layout/style/nsStyleCoord.h
+++ layout/style/nsStyleCoord.h
@@ -33,7 +33,8 @@ enum nsStyleUnit {
   eStyleUnit_Coord        = 20,     // (nscoord) value is twips
   eStyleUnit_Integer      = 30,     // (int) value is simple integer
   eStyleUnit_Proportional = 31,     // (int) value has proportional meaning
-  eStyleUnit_Enumerated   = 32      // (int) value has enumerated meaning
+  eStyleUnit_Enumerated   = 32,     // (int) value has enumerated meaning
+  eStyleUnit_Chars        = 33      // (int) value is number of characters
 };
 
 typedef union {
@@ -131,9 +132,11 @@ inline PRInt32 nsStyleCoord::GetIntValue(void) const
 {
   NS_ASSERTION((mUnit == eStyleUnit_Proportional) ||
                (mUnit == eStyleUnit_Enumerated) ||
+               (mUnit == eStyleUnit_Chars) ||
                (mUnit == eStyleUnit_Integer), "not an int value");
   if ((mUnit == eStyleUnit_Proportional) ||
       (mUnit == eStyleUnit_Enumerated) ||
+      (mUnit == eStyleUnit_Chars) ||
       (mUnit == eStyleUnit_Integer)) {
     return mValue.mInt;
   }
