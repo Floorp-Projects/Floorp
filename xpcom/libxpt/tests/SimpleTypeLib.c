3ea763aa505f5228da8c8532daf00b15ce0665b5
beard@netscape.com <beard@netscape.com>
1999-04-04 00:13:28 +0000

    added default args for XP_MAC.

--- xpcom/libxpt/tests/SimpleTypeLib.c
+++ xpcom/libxpt/tests/SimpleTypeLib.c
@@ -64,7 +64,15 @@ main(int argc, char **argv)
     PRBool ok;
 
     td_void.prefix.flags = TD_VOID;
-    
+
+#ifdef XP_MAC
+	if (argc == 0) {
+		static char* args[] = { "SimpleTypeLib", "simple.xpt", NULL };
+		argc = 2;
+		argv = args;
+	}
+#endif
+
     if (argc != 2) {
 	fprintf(stderr, "Usage: %s <filename.xpt>\n"
 		"       Creates a simple typelib file.\n", argv[0]);
