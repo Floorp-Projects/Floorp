94c6c9aa952f8e3d5ec3a8c68ae73cc080cc4604
cata%netscape.com <cata%netscape.com>
1999-04-06 23:36:14 +0000

    Converters added.

--- intl/uconv/src/registryhack2.h
+++ intl/uconv/src/registryhack2.h
@@ -17,7 +17,7 @@
  * Netscape Communications Corporation.  All Rights Reserved.
  */
 
-  mDecSize           = 7;
+  mDecSize           = 11;
   mDecArray          = new ConverterInfo [mDecSize];
 
   mDecArray[0].mCID  = &kLatin1ToUnicodeCID;
@@ -27,11 +27,19 @@
   mDecArray[4].mCID  = &kISO2022JPToUnicodeCID;
   mDecArray[5].mCID  = &kEUCJPToUnicodeCID;
   mDecArray[6].mCID  = &kUTF8ToUnicodeCID;
+  mDecArray[7].mCID  = &kBIG5ToUnicodeCID;
+  mDecArray[8].mCID  = &kEUCTWToUnicodeCID;
+  mDecArray[9].mCID  = &kGB2312ToUnicodeCID;
+  mDecArray[10].mCID  = &kEUCKRToUnicodeCID;
   
-  mEncSize           = 4;
+  mEncSize           = 8;
   mEncArray          = new ConverterInfo [mEncSize];
 
   mEncArray[0].mCID  = &kUnicodeToLatin1CID;
   mEncArray[1].mCID  = &kUnicodeToUTF8CID;
   mEncArray[2].mCID  = &kUnicodeToSJISCID;
   mEncArray[3].mCID  = &kUnicodeToEUCJPCID;
+  mEncArray[4].mCID  = &kUnicodeToBIG5CID;
+  mEncArray[5].mCID  = &kUnicodeToEUCTWCID;
+  mEncArray[6].mCID  = &kUnicodeToGB2312CID;
+  mEncArray[7].mCID  = &kUnicodeToEUCKRCID;
