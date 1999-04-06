3f5584563ef09ff479cdb3c63001cdeac20280d8
ftang@netscape.com <ftang@netscape.com>
1999-04-06 21:27:42 +0000

    add nsStringUtil.cpp - fix bustage

---
+++ widget/src/windows/nsStringUtil.cpp
@@ -0,0 +1,37 @@
+/* -*- Mode: c++; tab-width: 2; indent-tabs-mode: nil; -*- */
+/*
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "nsStringUtil.h"
+#include <windows.h>
+
+
+char* GetACPString(nsString& aStr)
+{
+   int acplen = aStr.Length() * 2 + 1;
+   char * acp = new char[acplen];
+   if(acp)
+   {
+      int outlen = ::WideCharToMultiByte( CP_ACP, 0, 
+                      aStr.GetUnicode(), aStr.Length(),
+                      acp, acplen, NULL, NULL);
+      if ( outlen > 0)
+         acp[outlen] = '\0';  // null terminate
+   }
+   return acp;
+}
+
