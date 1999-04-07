10e2cd2d97fef3eb5e07f71a2914b1348433ac74
cata@netscape.com <cata@netscape.com>
1999-04-07 00:21:28 +0000

    Converters added.

--- intl/uconv/ucvlatin/nsUCvLatinCID.h
+++ intl/uconv/ucvlatin/nsUCvLatinCID.h
@@ -62,6 +62,26 @@ NS_DECLARE_ID(kCP1253ToUnicodeCID,
 NS_DECLARE_ID(kCP1254ToUnicodeCID, 
   0x7c657d17, 0xec5e, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
 
+// Class ID for our MacRomanToUnicode charset converter
+// {7B8556A1-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kMacRomanToUnicodeCID, 
+  0x7b8556a1, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our MacRomanToUnicode charset converter
+// {7B8556A2-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kMacCEToUnicodeCID, 
+  0x7b8556a2, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our MacGreekToUnicode charset converter
+// {7B8556A3-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kMacGreekToUnicodeCID, 
+  0x7b8556a3, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our MacTurkishToUnicode charset converter
+// {7B8556A4-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kMacTurkishToUnicodeCID, 
+  0x7b8556a4, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
 // Class ID for our UTF8ToUnicode charset converter
 // {5534DDC0-DD96-11d2-8AAC-00600811A836}
 NS_DECLARE_ID(kUTF8ToUnicodeCID, 
@@ -71,6 +91,61 @@ NS_DECLARE_ID(kUTF8ToUnicodeCID,
 // {920307B0-C6E8-11d2-8AA8-00600811A836}
 NS_DECLARE_ID(kUnicodeToLatin1CID, 
   0x920307b0, 0xc6e8, 0x11d2, 0x8a, 0xa8, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToISO88592 charset converter
+// {7B8556A6-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToISO88592CID, 
+  0x7b8556a6, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToISO88597 charset converter
+// {7B8556A8-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToISO88597CID, 
+  0x7b8556a8, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToISO88599 charset converter
+// {7B8556A9-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToISO88599CID, 
+  0x7b8556a9, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToCP1250 charset converter
+// {7B8556AA-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToCP1250CID, 
+  0x7b8556aa, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToCP1252 charset converter
+// {7B8556AC-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToCP1252CID, 
+  0x7b8556ac, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToCP1253 charset converter
+// {7B8556AD-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToCP1253CID, 
+  0x7b8556ad, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToCP1254 charset converter
+// {7B8556AE-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToCP1254CID, 
+  0x7b8556ae, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToMacRoman charset converter
+// {7B8556AF-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToMacRomanCID, 
+  0x7b8556af, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToMacCE charset converter
+// {7B8556B0-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToMacCECID, 
+  0x7b8556b0, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToMacGreek charset converter
+// {7B8556B1-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToMacGreekCID, 
+  0x7b8556b1, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
+
+// Class ID for our UnicodeToMacTurkish charset converter
+// {7B8556B2-EC79-11d2-8AAC-00600811A836}
+NS_DECLARE_ID(kUnicodeToMacTurkishCID, 
+  0x7b8556b2, 0xec79, 0x11d2, 0x8a, 0xac, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36);
 
 // Class ID for our UnicodeToUTF8 charset converter
 // {7C657D18-EC5E-11d2-8AAC-00600811A836}
