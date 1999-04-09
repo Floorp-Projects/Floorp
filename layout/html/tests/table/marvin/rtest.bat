8fac3a943020701915e4f1e3466a39e167dc3382
karnaze%netscape.com <karnaze%netscape.com>
1999-04-09 21:12:34 +0000

    more/better table regression scripts

---
+++ layout/html/tests/table/marvin/rtest.bat
@@ -0,0 +1,16 @@
+@echo off
+if %1==baseline goto baseline
+
+:verify
+if not exist verify mkdir verify
+s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\marvin\verify\ -rd s:\mozilla\layout\html\tests\table\marvin -f s:\mozilla\layout\html\tests\table\marvin\file_list.txt
+goto done
+
+:baseline
+s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\marvin\ -f s:\mozilla\layout\html\tests\table\marvin\file_list.txt
+goto done
+
+:error
+echo syntax: rtest (baseline verify) 
+
+:done
