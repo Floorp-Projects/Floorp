1384b0e2b29e499acd67e380e1cb4fcb7bca1f2b
gagan%netscape.com <gagan%netscape.com>
1999-04-09 02:37:28 +0000

    nsFileCID.h first checkin

---
+++ netwerk/protocols/file/src/nsFileCID.h
@@ -0,0 +1,35 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/*
+
+  XPCOM Class ID for the File Handler that can be created with the
+  factory.
+
+  -Gagan Saksena 03/24/99
+
+*/
+
+#ifndef nsFileCID_h__
+#define nsFileCID_h__
+
+// {81F34A60-EE24-11d2-B019-006097BFC036}
+#define NS_FILE_HANDLER_FACTORY_CID \
+    { 0x81f34a60, 0xee24, 0x11d2, { 0xb0, 0x19, 0x0, 0x60, 0x97, 0xbf, 0xc0, 0x36 } }
+
+#endif // nsFileCID_h__
