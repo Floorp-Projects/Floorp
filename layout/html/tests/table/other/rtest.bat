ab4264ea34707184f2d292135d3523d967e78741
karnaze@netscape.com <karnaze@netscape.com>
1999-04-09 21:12:34 +0000

    more/better table regression scripts

---
+++ layout/html/tests/table/other/rtest.bat
@@ -0,0 +1,15 @@
+@echo off
+if %1==baseline goto baseline
+
+:verify
+if not exist verify mkdir verify
+s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\other\verify\ -rd s:\mozilla\layout\html\tests\table\other -f s:\mozilla\layout\html\tests\table\other\file_list.txt
+goto done
+
+:baseline
+s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\other\ -f s:\mozilla\layout\html\tests\table\other\file_list.txt
+goto done
+
+:error
+echo syntax: rtest (baseline verify) 
+:done
--- layout/html/tests/table/rtest.bat
+++ layout/html/tests/table/rtest.bat
@@ -1,21 +1,16 @@
 @echo off
-if %1==baseline goto baseline
 
-:verify
-s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\viewer_tests\verify\ -rd s:\mozilla\layout\html\tests\table\viewer_tests -f s:\mozilla\layout\html\tests\table\viewer_tests\file_list.txt
-s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\bugs\verify\ -rd s:\mozilla\layout\html\tests\table\bugs -f s:\mozilla\layout\html\tests\table\bugs\file_list.txt
-s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\marvin\verify\ -rd s:\mozilla\layout\html\tests\table\marvin -f s:\mozilla\layout\html\tests\table\marvin\file_list.txt
-s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\other\verify\ -rd s:\mozilla\layout\html\tests\table\other -f s:\mozilla\layout\html\tests\table\other\file_list.txt
-goto done
+cd viewer_tests
+call rtest.bat %1
 
-:baseline
-s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\viewer_tests\ -f s:\mozilla\layout\html\tests\table\viewer_tests\file_list.txt
-s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\bugs\ -f s:\mozilla\layout\html\tests\table\bugs\file_list.txt
-s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\marvin\ -f s:\mozilla\layout\html\tests\table\marvin\file_list.txt
-s:\mozilla\dist\win32_d.obj\bin\viewer -o s:\mozilla\layout\html\tests\table\other\ -f s:\mozilla\layout\html\tests\table\other\file_list.txt
-goto done
+cd ..\bugs
+call rtest.bat %1
 
-:error
-echo syntax: rtest (baseline verify) (viewer_tests bugs marvin all)
+cd ..\marvin
+call rtest.bat %1
 
-:done
+cd ..\other
+call rtest.bat %1
+
+cd ..
+
