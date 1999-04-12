8ea2b42583166285217b30ab7b8a12c163e8d6e2
mccabe%netscape.com <mccabe%netscape.com>
1999-04-12 08:57:06 +0000

    Fixed initial copyright date.
    Thanks to Tim Rowley <tor@cs.brown.edu> for this SunOS fix.

--- js/src/config/SunOS5.7.mk
+++ js/src/config/SunOS5.7.mk
@@ -11,7 +11,7 @@
 #
 # The Initial Developer of this code under the NPL is Netscape
 # Communications Corporation.  Portions created by Netscape are
-# Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+# Copyright (C) 1999 Netscape Communications Corporation.  All Rights
 # Reserved.
 #
 
