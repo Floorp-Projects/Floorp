1a6e34fc7408ef69c92a986a2a511dd8e6e79891
beard%netscape.com <beard%netscape.com>
1999-04-15 17:10:37 +0000

    First Checked In.

---
+++ plugin/oji/MRJ/plugin/Source/MRJPlugin.java
@@ -0,0 +1,30 @@
+/*
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/*
+	MRJPlugin.java
+ */
+
+import netscape.plugin.Plugin;
+
+class MRJPlugin extends Plugin {
+	MRJPlugin() {}
+
+	public native Object getField(String name, String signature);	
+	public native void setField(String name, String signature, Object value);
+	public native Object callMethod(String name, String signature, Object[] args);
+}
