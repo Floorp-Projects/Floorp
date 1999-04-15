f9fe92034ccf060c257edb56d52a9c80c1cf4a6b
beard@netscape.com <beard@netscape.com>
1999-04-15 21:54:14 +0000

    regenerated from Navigator.idl

--- dom/public/base/nsIDOMNavigator.h
+++ dom/public/base/nsIDOMNavigator.h
@@ -24,6 +24,8 @@
 #include "nsString.h"
 #include "nsIScriptContext.h"
 
+class nsIDOMPluginArray;
+class nsIDOMMimeTypeArray;
 
 #define NS_IDOMNAVIGATOR_IID \
  { 0xa6cf906e, 0x15b3, 0x11d2, \
@@ -33,44 +35,52 @@ class nsIDOMNavigator : public nsISupports {
 public:
   static const nsIID& GetIID() { static nsIID iid = NS_IDOMNAVIGATOR_IID; return iid; }
 
-  NS_IMETHOD    GetUserAgent(nsString& aUserAgent)=0;
-
   NS_IMETHOD    GetAppCodeName(nsString& aAppCodeName)=0;
-
-  NS_IMETHOD    GetAppVersion(nsString& aAppVersion)=0;
 
   NS_IMETHOD    GetAppName(nsString& aAppName)=0;
 
+  NS_IMETHOD    GetAppVersion(nsString& aAppVersion)=0;
+
   NS_IMETHOD    GetLanguage(nsString& aLanguage)=0;
+
+  NS_IMETHOD    GetMimeTypes(nsIDOMMimeTypeArray** aMimeTypes)=0;
 
   NS_IMETHOD    GetPlatform(nsString& aPlatform)=0;
 
+  NS_IMETHOD    GetPlugins(nsIDOMPluginArray** aPlugins)=0;
+
   NS_IMETHOD    GetSecurityPolicy(nsString& aSecurityPolicy)=0;
+
+  NS_IMETHOD    GetUserAgent(nsString& aUserAgent)=0;
 
   NS_IMETHOD    JavaEnabled(PRBool* aReturn)=0;
 };
 
 
 #define NS_DECL_IDOMNAVIGATOR   \
-  NS_IMETHOD    GetUserAgent(nsString& aUserAgent);  \
   NS_IMETHOD    GetAppCodeName(nsString& aAppCodeName);  \
-  NS_IMETHOD    GetAppVersion(nsString& aAppVersion);  \
   NS_IMETHOD    GetAppName(nsString& aAppName);  \
+  NS_IMETHOD    GetAppVersion(nsString& aAppVersion);  \
   NS_IMETHOD    GetLanguage(nsString& aLanguage);  \
+  NS_IMETHOD    GetMimeTypes(nsIDOMMimeTypeArray** aMimeTypes);  \
   NS_IMETHOD    GetPlatform(nsString& aPlatform);  \
+  NS_IMETHOD    GetPlugins(nsIDOMPluginArray** aPlugins);  \
   NS_IMETHOD    GetSecurityPolicy(nsString& aSecurityPolicy);  \
+  NS_IMETHOD    GetUserAgent(nsString& aUserAgent);  \
   NS_IMETHOD    JavaEnabled(PRBool* aReturn);  \
 
 
 
 #define NS_FORWARD_IDOMNAVIGATOR(_to)  \
-  NS_IMETHOD    GetUserAgent(nsString& aUserAgent) { return _to##GetUserAgent(aUserAgent); } \
   NS_IMETHOD    GetAppCodeName(nsString& aAppCodeName) { return _to##GetAppCodeName(aAppCodeName); } \
-  NS_IMETHOD    GetAppVersion(nsString& aAppVersion) { return _to##GetAppVersion(aAppVersion); } \
   NS_IMETHOD    GetAppName(nsString& aAppName) { return _to##GetAppName(aAppName); } \
+  NS_IMETHOD    GetAppVersion(nsString& aAppVersion) { return _to##GetAppVersion(aAppVersion); } \
   NS_IMETHOD    GetLanguage(nsString& aLanguage) { return _to##GetLanguage(aLanguage); } \
+  NS_IMETHOD    GetMimeTypes(nsIDOMMimeTypeArray** aMimeTypes) { return _to##GetMimeTypes(aMimeTypes); } \
   NS_IMETHOD    GetPlatform(nsString& aPlatform) { return _to##GetPlatform(aPlatform); } \
+  NS_IMETHOD    GetPlugins(nsIDOMPluginArray** aPlugins) { return _to##GetPlugins(aPlugins); } \
   NS_IMETHOD    GetSecurityPolicy(nsString& aSecurityPolicy) { return _to##GetSecurityPolicy(aSecurityPolicy); } \
+  NS_IMETHOD    GetUserAgent(nsString& aUserAgent) { return _to##GetUserAgent(aUserAgent); } \
   NS_IMETHOD    JavaEnabled(PRBool* aReturn) { return _to##JavaEnabled(aReturn); }  \
 
 
