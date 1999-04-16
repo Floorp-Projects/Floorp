f1d9108370e40aa29b933419cf28c0a5de5465de
sspitzer@netscape.com <sspitzer@netscape.com>
1999-04-16 00:44:36 +0000

    correctly fix bustage

--- xpfe/appshell/src/nsFileLocations.cpp
+++ xpfe/appshell/src/nsFileLocations.cpp
@@ -55,9 +55,7 @@
 static NS_DEFINE_IID(kIFileLocatorIID, NS_IFILELOCATOR_IID);
 
 // for profile manager
-#if defined(NS_USING_PROFILES) || 1
 static NS_DEFINE_CID(kProfileCID,           NS_PROFILE_CID);
-#endif // NS_USING_PROFILES
 
 #ifdef XP_MAC
 #pragma export on
