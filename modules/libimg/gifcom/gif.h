6f48cabec3898cd7095004c476692da0968fc672
pnunn%netscape.com <pnunn%netscape.com>
1999-04-19 19:35:16 +0000

    gif decoder. created. pnunn.

---
+++ modules/libimg/gifcom/gif.h
@@ -0,0 +1,28 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+
+/* gif.h */
+
+extern int  il_gif_init(il_container *ic);
+extern int  il_gif_write(il_container *, const uint8 *, int32);
+extern void il_gif_complete(il_container *ic);
+extern unsigned int il_gif_write_ready(il_container *ic);
+extern void il_gif_abort(il_container *ic);
+extern void gif_delay_time_callback(void *closure);
