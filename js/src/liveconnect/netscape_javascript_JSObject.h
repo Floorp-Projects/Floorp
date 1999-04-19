cbd133ead1fb5e4598fcf8f568e520c59941ee46
fur%netscape.com <fur%netscape.com>
1999-04-19 05:12:25 +0000

    Sync up with SpiderMonkey140_BRANCH

--- js/src/liveconnect/netscape_javascript_JSObject.h
+++ js/src/liveconnect/netscape_javascript_JSObject.h
@@ -1,6 +1,30 @@
-/* -*- Mode: C; tab-width: 8 -*-
- * Copyright (C) 1998 Netscape Communications Corporation, All Rights Reserved.
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
  */
+
+/*
+ * This file is part of the Java-vendor-neutral implementation of LiveConnect
+ *
+ * Declarations of private (internal) functions/data/types for
+ * JavaScript <==> Java communication.
+ *
+ */
+
 #include <jni.h>
 /* Header for class netscape_javascript_JSObject */
 
