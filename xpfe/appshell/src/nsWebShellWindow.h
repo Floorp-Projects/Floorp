a2bf9cc89de577ccd54b9b85f5e297e12382d6ed
saari%netscape.com <saari%netscape.com>
1999-04-19 08:23:18 +0000

    First of a series of menu checkins

--- xpfe/appshell/src/nsWebShellWindow.h
+++ xpfe/appshell/src/nsWebShellWindow.h
@@ -198,6 +198,7 @@ protected:
   PRInt32 GetDocHeight(nsIDocument * aDoc);
  
   void LoadMenus(nsIDOMDocument * aDOMDoc, nsIWidget * aParentWindow);
+  void DynamicLoadMenus(nsIDOMDocument * aDOMDoc, nsIWidget * aParentWindow);
   nsCOMPtr<nsIDOMNode>     FindNamedParentFromDoc(nsIDOMDocument * aDomDoc, const nsString &aName);
   nsCOMPtr<nsIDOMNode>     FindNamedDOMNode(const nsString &aName, nsIDOMNode * aParent, PRInt32 & aCount, PRInt32 aEndCount);
   nsCOMPtr<nsIDOMDocument> GetNamedDOMDoc(const nsString & aWebShellName);
