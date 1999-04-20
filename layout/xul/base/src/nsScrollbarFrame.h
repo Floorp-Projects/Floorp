3aeac2fe48995b1c5c3da208a46a8566b0996ed9
pinkerton%netscape.com <pinkerton%netscape.com>
1999-04-20 21:39:52 +0000

    First Checked In.

---
+++ layout/xul/base/src/nsScrollbarFrame.h
@@ -0,0 +1,60 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+//
+// nsScrollbarFrame
+//
+
+#ifndef nsScrollbarFrame_h__
+#define nsScrollbarFrame_h__
+
+
+#include "nsLeafFrame.h"
+#include "prtypes.h"
+#include "nsIAtom.h"
+#include "nsCOMPtr.h"
+
+class nsString;
+
+
+nsresult NS_NewScrollbarFrame(nsIFrame*& aResult) ;
+
+
+class nsScrollbarFrame : public nsLeafFrame
+{
+public:
+  nsScrollbarFrame();
+
+    // nsIFrame overrides
+  NS_IMETHOD GetFrameName(nsString& aResult) const {
+    return MakeFrameName("ScrollbarFrame", aResult);
+  }
+  NS_IMETHOD Paint(nsIPresContext& aPresContext,
+                   nsIRenderingContext& aRenderingContext,
+                   const nsRect& aDirtyRect,
+                   nsFramePaintLayer aWhichLayer);
+  
+protected:
+
+  virtual void GetDesiredSize(nsIPresContext* aPresContext,
+                              const nsHTMLReflowState& aReflowState,
+                              nsHTMLReflowMetrics& aDesiredSize) ;
+
+}; // class nsScrollbarFrame
+
+#endif
