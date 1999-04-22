90a2cb20209a71160ade12bba751fe5647a20461
locka@iol.ie <locka@iol.ie>
1999-04-22 23:28:09 +0000

    Approved: leaf@mozilla.org New Mozilla control registering tool

---
+++ webshell/embed/ActiveX/tests/RegMozCtl/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by RegMozCtl.rc
+//
+#define IDD_REGMOZCTL_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDI_ICON2                       130
+#define IDC_REGISTER                    1001
+#define IDC_UNREGISTER                  1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
