051ea43bd1443ff7112bf3fe437e5931743fe161
sfraser%netscape.com <sfraser%netscape.com>
1999-04-24 05:00:32 +0000

    c2pstr on a "inline" string is bad, with VM on. Fixed.

--- widget/src/mac/nsMenu.cpp
+++ widget/src/mac/nsMenu.cpp
@@ -247,7 +247,7 @@ NS_METHOD nsMenu::AddMenu(nsIMenu * aMenu)
   aMenu->GetLabel(label);
   char* menuLabel = label.ToNewCString();
   mNumMenuItems++;
-  ::InsertMenuItem(mMacMenuHandle, c2pstr("a"), mNumMenuItems);
+  ::InsertMenuItem(mMacMenuHandle, "\p ", mNumMenuItems);
   ::SetMenuItemText(mMacMenuHandle, mNumMenuItems, c2pstr(menuLabel));
   delete[] menuLabel;
   
