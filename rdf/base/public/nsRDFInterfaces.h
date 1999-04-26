87294a22bb0a8c05988bc1a8bcd3d0f80b20885d
waterson@netscape.com <waterson@netscape.com>
1999-04-26 18:45:59 +0000

    Added nsIRDFContainer and nsIRDFContainerUtils to replace the old C versions in rdfutil.

--- rdf/base/public/nsRDFInterfaces.h
+++ rdf/base/public/nsRDFInterfaces.h
@@ -10,6 +10,7 @@
 #include "nsICollection.h" /* interface nsICollection */
 #include "nsIEnumerator.h" /* interface nsIEnumerator */
 #include "nsrootidl.h" /* interface nsrootidl */
+#include "nsISimpleEnumerator.h" /* interface nsISimpleEnumerator */
 
 #ifdef XPIDL_JS_STUBS
 #include "jsapi.h"
@@ -306,6 +307,93 @@ class nsIRDFService : public nsISupports {
 #ifdef XPIDL_JS_STUBS
   static NS_EXPORT_(JSObject *) InitJSClass(JSContext *cx);
   static NS_EXPORT_(JSObject *) GetJSObject(JSContext *cx, nsIRDFService *priv);
+#endif
+};
+
+/* starting interface:    nsIRDFContainer */
+
+/* {D4214E90-FB94-11D2-BDD8-00104BDE6048} */
+#define NS_IRDFCONTAINER_IID_STR "D4214E90-FB94-11D2-BDD8-00104BDE6048"
+#define NS_IRDFCONTAINER_IID \
+  {0xD4214E90, 0xFB94, 0x11D2, \
+    { 0xBD, 0xD8, 0x00, 0x10, 0x4B, 0xDE, 0x60, 0x48 }}
+
+class nsIRDFContainer : public nsISupports {
+ public: 
+  NS_DEFINE_STATIC_IID_ACCESSOR(NS_IRDFCONTAINER_IID)
+
+  /* void Init (in nsIRDFDataSource aDataSource, in nsIRDFResource aContainer); */
+  NS_IMETHOD Init(nsIRDFDataSource *aDataSource, nsIRDFResource *aContainer) = 0;
+
+  /* long GetCount (); */
+  NS_IMETHOD GetCount(PRInt32 *_retval) = 0;
+
+  /* nsISimpleEnumerator GetElements (); */
+  NS_IMETHOD GetElements(nsISimpleEnumerator **_retval) = 0;
+
+  /* void AppendElement (in nsIRDFNode aElement); */
+  NS_IMETHOD AppendElement(nsIRDFNode *aElement) = 0;
+
+  /* void RemoveElement (in nsIRDFNode aElement, in boolean aRenumber); */
+  NS_IMETHOD RemoveElement(nsIRDFNode *aElement, PRBool aRenumber) = 0;
+
+  /* void InsertElementAt (in nsIRDFNode aElement, in long aIndex, in boolean aRenumber); */
+  NS_IMETHOD InsertElementAt(nsIRDFNode *aElement, PRInt32 aIndex, PRBool aRenumber) = 0;
+
+  /* long IndexOf (in nsIRDFNode aElement); */
+  NS_IMETHOD IndexOf(nsIRDFNode *aElement, PRInt32 *_retval) = 0;
+
+#ifdef XPIDL_JS_STUBS
+  static NS_EXPORT_(JSObject *) InitJSClass(JSContext *cx);
+  static NS_EXPORT_(JSObject *) GetJSObject(JSContext *cx, nsIRDFContainer *priv);
+#endif
+};
+
+/* starting interface:    nsIRDFContainerUtils */
+
+/* {D4214E91-FB94-11D2-BDD8-00104BDE6048} */
+#define NS_IRDFCONTAINERUTILS_IID_STR "D4214E91-FB94-11D2-BDD8-00104BDE6048"
+#define NS_IRDFCONTAINERUTILS_IID \
+  {0xD4214E91, 0xFB94, 0x11D2, \
+    { 0xBD, 0xD8, 0x00, 0x10, 0x4B, 0xDE, 0x60, 0x48 }}
+
+class nsIRDFContainerUtils : public nsISupports {
+ public: 
+  NS_DEFINE_STATIC_IID_ACCESSOR(NS_IRDFCONTAINERUTILS_IID)
+
+  /* boolean IsOrdinalProperty (in nsIRDFResource aProperty); */
+  NS_IMETHOD IsOrdinalProperty(nsIRDFResource *aProperty, PRBool *_retval) = 0;
+
+  /* nsIRDFResource IndexToOrdinalResource (in long aIndex); */
+  NS_IMETHOD IndexToOrdinalResource(PRInt32 aIndex, nsIRDFResource **_retval) = 0;
+
+  /* long OrdinalResourceToIndex (in nsIRDFResource aOrdinal); */
+  NS_IMETHOD OrdinalResourceToIndex(nsIRDFResource *aOrdinal, PRInt32 *_retval) = 0;
+
+  /* boolean IsContainer (in nsIRDFDataSource aDataSource, in nsIRDFResource aResource); */
+  NS_IMETHOD IsContainer(nsIRDFDataSource *aDataSource, nsIRDFResource *aResource, PRBool *_retval) = 0;
+
+  /* boolean IsBag (in nsIRDFDataSource aDataSource, in nsIRDFResource aResource); */
+  NS_IMETHOD IsBag(nsIRDFDataSource *aDataSource, nsIRDFResource *aResource, PRBool *_retval) = 0;
+
+  /* boolean IsSeq (in nsIRDFDataSource aDataSource, in nsIRDFResource aResource); */
+  NS_IMETHOD IsSeq(nsIRDFDataSource *aDataSource, nsIRDFResource *aResource, PRBool *_retval) = 0;
+
+  /* boolean IsAlt (in nsIRDFDataSource aDataSource, in nsIRDFResource aResource); */
+  NS_IMETHOD IsAlt(nsIRDFDataSource *aDataSource, nsIRDFResource *aResource, PRBool *_retval) = 0;
+
+  /* nsIRDFContainer MakeBag (in nsIRDFDataSource aDataSource, in nsIRDFResource aResource); */
+  NS_IMETHOD MakeBag(nsIRDFDataSource *aDataSource, nsIRDFResource *aResource, nsIRDFContainer **_retval) = 0;
+
+  /* nsIRDFContainer MakeSeq (in nsIRDFDataSource aDataSource, in nsIRDFResource aResource); */
+  NS_IMETHOD MakeSeq(nsIRDFDataSource *aDataSource, nsIRDFResource *aResource, nsIRDFContainer **_retval) = 0;
+
+  /* nsIRDFContainer MakeAlt (in nsIRDFDataSource aDataSource, in nsIRDFResource aResource); */
+  NS_IMETHOD MakeAlt(nsIRDFDataSource *aDataSource, nsIRDFResource *aResource, nsIRDFContainer **_retval) = 0;
+
+#ifdef XPIDL_JS_STUBS
+  static NS_EXPORT_(JSObject *) InitJSClass(JSContext *cx);
+  static NS_EXPORT_(JSObject *) GetJSObject(JSContext *cx, nsIRDFContainerUtils *priv);
 #endif
 };
 
