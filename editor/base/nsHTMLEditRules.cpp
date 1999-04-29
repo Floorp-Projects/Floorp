457a1555e78de601067972446fdb1c4d587e2126
buster%netscape.com <buster%netscape.com>
1999-04-29 00:14:05 +0000

    approved bug fix for infinite loop when typing a space in a document with only a single text node

--- editor/base/nsHTMLEditRules.cpp
+++ editor/base/nsHTMLEditRules.cpp
@@ -986,7 +986,7 @@ nsHTMLEditRules::NextNodeInBlock(nsIDOMNode *aNode, IterDirection aDir)
   	if (NS_FAILED(iter->CurrentNode(getter_AddRefs(content)))) return nullNode;
     // ignore nodes that aren't elements or text, or that are the block parent 
     node = do_QueryInterface(content);
-    if (node && IsTextOrElementNode(node) && (node != blockParent))
+    if (node && IsTextOrElementNode(node) && (node != blockParent) && (node!=aNode))
       return node;
     
     if (aDir == kIterForward)
