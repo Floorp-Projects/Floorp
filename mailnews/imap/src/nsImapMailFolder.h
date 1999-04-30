c2e9fead94ae1f3d0fbeaf31c65adf3e00ba0d1e
jefft%netscape.com <jefft%netscape.com>
1999-04-30 21:38:53 +0000

    Part of fixing bug 5781 - fixed creating multiple mail folders problem

--- mailnews/imap/src/nsImapMailFolder.h
+++ mailnews/imap/src/nsImapMailFolder.h
@@ -266,8 +266,6 @@ protected:
 	nsresult AddSubfolder(nsAutoString name, nsIMsgFolder **child);
 
 	nsresult GetDatabase();
-    static PRBool FindAndSelectFolder(nsISupports* aElement,
-                                                      void* aData);
 
     nsNativeFileSpec m_pathName;
 	nsIMsgDatabase* m_mailDatabase;
