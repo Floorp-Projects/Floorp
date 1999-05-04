7983b823f0499c1729b755b91d8ddd2e8bc77ff2
locka%iol.ie <locka%iol.ie>
1999-05-04 22:52:40 +0000

    New control event sink

---
+++ webshell/embed/ActiveX/tests/cbrowse/ControlEventSink.cpp
@@ -0,0 +1,110 @@
+// ControlEventSink.cpp : Implementation of CControlEventSink
+#include "stdafx.h"
+#include "Cbrowse.h"
+#include "ControlEventSink.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// CControlEventSink
+
+HRESULT STDMETHODCALLTYPE CControlEventSink::GetTypeInfoCount( 
+    /* [out] */ UINT __RPC_FAR *pctinfo)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT STDMETHODCALLTYPE CControlEventSink::GetTypeInfo( 
+    /* [in] */ UINT iTInfo,
+    /* [in] */ LCID lcid,
+    /* [out] */ ITypeInfo __RPC_FAR *__RPC_FAR *ppTInfo)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT STDMETHODCALLTYPE CControlEventSink::GetIDsOfNames( 
+    /* [in] */ REFIID riid,
+    /* [size_is][in] */ LPOLESTR __RPC_FAR *rgszNames,
+    /* [in] */ UINT cNames,
+    /* [in] */ LCID lcid,
+    /* [size_is][out] */ DISPID __RPC_FAR *rgDispId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT STDMETHODCALLTYPE CControlEventSink:: Invoke( 
+    /* [in] */ DISPID dispIdMember,
+    /* [in] */ REFIID riid,
+    /* [in] */ LCID lcid,
+    /* [in] */ WORD wFlags,
+    /* [out][in] */ DISPPARAMS __RPC_FAR *pDispParams,
+    /* [out] */ VARIANT __RPC_FAR *pVarResult,
+    /* [out] */ EXCEPINFO __RPC_FAR *pExcepInfo,
+    /* [out] */ UINT __RPC_FAR *puArgErr)
+{
+	CString szEvent;
+
+	switch (dispIdMember)
+	{
+	case 0x66:
+		szEvent = _T("StatusTextChange");
+		break;
+	case 0x6c:
+		szEvent = _T("ProgressChange");
+		break;
+	case 0x69:
+		szEvent = _T("CommandStateChange");
+		break;
+	case 0x6a:
+		szEvent = _T("DownloadBegin");
+		break;
+	case 0x68:
+		szEvent = _T("DownloadComplete");
+		break;
+	case 0x71:
+		szEvent = _T("TitleChange");
+		break;
+	case 0x70:
+		szEvent = _T("PropertyChange");
+		break;
+	case 0xfa:
+		szEvent = _T("BeforeNavigate2");
+		break;
+	case 0xfb:
+		szEvent = _T("NewWindow2");
+		break;
+	case 0xfc:
+		szEvent = _T("NavigateComplete2");
+		break;
+	case 0x103:
+		szEvent = _T("DocumentComplete");
+		break;
+	case 0xfd:
+		szEvent = _T("OnQuit");
+		break;
+	case 0xfe:
+		szEvent = _T("OnVisible");
+		break;
+	case 0xff:
+		szEvent = _T("OnToolBar");
+		break;
+	case 0x100:
+		szEvent = _T("OnMenuBar");
+		break;
+	case 0x101:
+		szEvent = _T("OnStatusBar");
+		break;
+	case 0x102:
+		szEvent = _T("OnFullScreen");
+		break;
+	case 0x104:
+		szEvent = _T("OnTheaterMode");
+		break;
+	default:
+		szEvent.Format(_T("%d"), dispIdMember);
+	}
+
+	if (m_pBrowseDlg)
+	{
+		m_pBrowseDlg->OutputString(_T("Event %s"), szEvent);
+	}
+	return S_OK;
+}
