3355650659a8adae37938e83d8b85f2073921e48
cmanske@netscape.com <cmanske@netscape.com>
1999-05-05 23:30:21 +0000

    Split JavaScript out of XUL into separate files

---
+++ editor/ui/dialogs/content/EdCharacterProps.js
@@ -0,0 +1,27 @@
+  // dialog initialization code
+var appCore;
+var toolkitCore;
+var insertNew = true;
+var selectionIsCollapsed = false;
+var undoCount = 0;
+
+function Statup()
+{
+    dump("Doing Character Props Startup...\n");
+    toolkitCore = XPAppCoresManager.Find("ToolkitCore");
+    if (!toolkitCore) {
+      toolkitCore = new ToolkitCore();
+      if (toolkitCore)
+        toolkitCore.Init("ToolkitCore");
+    }
+    if(!toolkitCore) {
+      dump("toolkitCore not found!!! And we can't close the dialog!\n");
+    }
+
+    // NEVER create an appcore here - we must find parent editor's
+    appCore = XPAppCoresManager.Find("EditorAppCoreHTML");  
+    if(!appCore || !toolkitCore) {
+      dump("EditorAppCore not found!!!\n");
+      toolkitCore.CloseWindow(window);
+    }
+}
