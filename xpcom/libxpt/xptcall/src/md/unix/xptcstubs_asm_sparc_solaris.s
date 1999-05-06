748f7a4f12dc84d8cb6e0774cfd3e61691920460
mcafee%netscape.com <mcafee%netscape.com>
1999-05-06 00:49:02 +0000

    Linker didn't seem to like _ prefix for function names, removing them fixes link errors.  Adding license file.

--- xpcom/libxpt/xptcall/src/md/unix/xptcstubs_asm_sparc_solaris.s
+++ xpcom/libxpt/xptcall/src/md/unix/xptcstubs_asm_sparc_solaris.s
@@ -1,11 +1,30 @@
-        .global _SharedStub
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1999 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+        .global SharedStub
+
 /*
     in the frame for the function that called SharedStub are the
     rest of the parameters we need
 
 */
 
-_SharedStub:
+SharedStub:
 ! we don't create a new frame yet, but work within the frame of the calling
 ! function to give ourselves the other parameters we want
 
@@ -29,7 +48,7 @@ _SharedStub:
         mov     %i0, %o0
         mov     %i1, %o1
         mov     %i2, %o2
-        call    _PrepareAndDispatch
+        call    PrepareAndDispatch
         nop
         mov     %o0,%i0             ! propogate return value
         b .LL1
