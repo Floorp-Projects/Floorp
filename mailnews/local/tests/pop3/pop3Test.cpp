5f301a002500ba1e7991d1c4588e1e6ea53ba894
mscott%netscape.com <mscott%netscape.com>
1999-05-06 01:35:40 +0000

    Bug 5991 --> Fix crash in pop3Test caused by the event queue carpool yesterday. We need to make sure the event queue is registered.

--- mailnews/local/tests/pop3/pop3Test.cpp
+++ mailnews/local/tests/pop3/pop3Test.cpp
@@ -45,6 +45,7 @@
 #include "nsINetService.h"
 #include "nsIServiceManager.h"
 #include "nsIEventQueueService.h"
+#include "nsIEventQueue.h"
 #include "nsXPComCIID.h"
 
 #include "nsIMsgIdentity.h"
@@ -78,6 +79,7 @@ static NS_DEFINE_CID(kEventQueueServiceCID, NS_EVENTQUEUESERVICE_CID);
 static NS_DEFINE_CID(kPop3ServiceCID, NS_POP3SERVICE_CID);
 static NS_DEFINE_CID(kPrefCID, NS_PREF_CID);
 static NS_DEFINE_IID(kFileLocatorCID, NS_FILELOCATOR_CID);
+static NS_DEFINE_IID(kEventQueueCID, NS_EVENTQUEUE_CID);
 
 /////////////////////////////////////////////////////////////////////////////////
 // Define default values to be used to drive the test
@@ -473,6 +475,7 @@ int main()
 
 	nsComponentManager::RegisterComponent(kNetServiceCID, NULL, NULL, NETLIB_DLL, PR_FALSE, PR_FALSE);
 	nsComponentManager::RegisterComponent(kEventQueueServiceCID, NULL, NULL, XPCOM_DLL, PR_FALSE, PR_FALSE);
+	nsComponentManager::RegisterComponent(kEventQueueCID, NULL, NULL, XPCOM_DLL, PR_FALSE, PR_FALSE);
 	nsComponentManager::RegisterComponent(kPrefCID, nsnull, nsnull, PREF_DLL, PR_TRUE, PR_TRUE);
 	nsComponentManager::RegisterComponent(kFileLocatorCID,  NULL, NULL, APPSHELL_DLL, PR_FALSE, PR_FALSE);
 
