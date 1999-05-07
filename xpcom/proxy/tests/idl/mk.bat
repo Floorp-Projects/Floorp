935ef516549c840710c1f5e0f730659c5dd5d9df
dougt%netscape.com <dougt%netscape.com>
1999-05-07 20:07:50 +0000

    removing stuff that is throw away.

--- xpcom/proxy/tests/idl/mk.bat
+++
@@ -1,5 +0,0 @@
-xpidl -w -I ../../../../js/src/xpconnect/idl/ -m header nsITestProxyFoo.idl
-xpidl -w -I ../../../../js/src/xpconnect/idl/ -m typelib nsITestProxyFoo.idl
-
-cp nsITestProxyFoo.xpt ../../../../dist/win32_d.obj/bin/components
-
