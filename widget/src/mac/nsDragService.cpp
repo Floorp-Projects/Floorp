14e5a06aead8e59e075d7e50342f352468dea0fd
pinkerton@netscape.com <pinkerton@netscape.com>
1999-05-07 19:52:15 +0000

    new drag session interface and lots of work to get d&d going.

--- widget/src/mac/nsDragService.cpp
+++ widget/src/mac/nsDragService.cpp
@@ -52,19 +52,21 @@
 
 #include "nsDragService.h"
 
+
+
 #include "nsITransferable.h"
 
+#include "nsIDataFlavor.h"
 
+#include "nsMimeMapper.h"
 
 #include "nsWidgetsCID.h"
 
 #include "nsClipboard.h"
 
+#include "nsIRegion.h"
 
 
-
-
-static NS_DEFINE_IID(kIDragServiceIID,   NS_IDRAGSERVICE_IID);
 
 
 
@@ -84,9 +86,9 @@ NS_IMPL_RELEASE_INHERITED(nsDragService, nsBaseDragService)
 
 nsDragService::nsDragService()
 
-{
+  : mDragRef(0)
 
-  NS_INIT_REFCNT();
+{
 
 }
 
@@ -142,7 +144,7 @@ nsDragService::QueryInterface(const nsIID& aIID, void** aInstancePtr)
 
 
 
-  if (aIID.Equals(kIDragServiceIID)) {
+  if (aIID.Equals(nsIDragService::GetIID())) {
 
     *aInstancePtr = (void*) ((nsIDragService*)this);
 
@@ -174,17 +176,235 @@ nsDragService::QueryInterface(const nsIID& aIID, void** aInstancePtr)
 
 NS_IMETHODIMP
 
-nsDragService::StartDragSession (nsITransferable * aTransferable, PRUint32 aActionType)
-
-
+nsDragService :: InvokeDragSession (nsISupportsArray * anArrayTransferables, nsIRegion * aDragRgn, PRUint32 aActionType)
 
 {
 
-  return NS_ERROR_FAILURE;
+  //еее what should we do about |mDragRef| from the previous drag?? Does anyone
+
+  //еее outside of this method need it? Should it really be a member variable?
+
+  
+
+  OSErr result = ::NewDrag(&mDragRef);
+
+  if ( !result )
+
+    return NS_ERROR_FAILURE;
+
+
+
+
+
+  //еее add the flavors from the transferables
+
+
+
+
+
+  // create the drag region. Pull out the native mac region from the nsIRegion we're
+
+  // given, copy it, inset it one pixel, and subtract them so we're left with just an
+
+  // outline. Too bad we can't do this with gfx api's.
+
+  //
+
+  // At the end, we are left with an outline of the region in global coordinates.
+
+  RgnHandle dragRegion = nsnull;
+
+  aDragRgn->GetNativeRegion(dragRegion);
+
+  RgnHandle insetDragRegion = ::NewRgn();
+
+  if ( dragRegion && insetDragRegion ) {
+
+    ::CopyRgn ( dragRegion, insetDragRegion );
+
+    ::InsetRgn ( insetDragRegion, 1, 1 );
+
+    ::DiffRgn ( dragRegion, insetDragRegion, insetDragRegion );
+
+  
+
+    // now shift the region into global coordinates.
+
+    Point offsetFromLocalToGlobal = { 0, 0 };
+
+    ::LocalToGlobal ( &offsetFromLocalToGlobal );
+
+    ::OffsetRgn ( insetDragRegion, offsetFromLocalToGlobal.h, offsetFromLocalToGlobal.v );
+
+  }
+
+  
+
+  
+
+  //еее register drag send procs
+
+
+
+
+
+  // we have to synthesize the native event because we may be called from JavaScript
+
+  // through XPConnect. In that case, we only have a DOM event and no way to
+
+  // get to the native event. As a consequence, we just always fake it.
+
+  Point globalMouseLoc;
+
+  ::GetMouse(&globalMouseLoc);
+
+  ::LocalToGlobal(&globalMouseLoc);
+
+  WindowPtr theWindow = nsnull;;
+
+  if ( ::FindWindow(globalMouseLoc, &theWindow) != inContent ) {
+
+    // debugging sanity check
+
+    #ifdef NS_DEBUG
+
+    DebugStr("\pAbout to start drag, but FindWindow() != inContent");
+
+    #endif
+
+  }
+
+  EventRecord theEvent;
+
+  theEvent.what = mouseDown;
+
+  theEvent.message = reinterpret_cast<UInt32>(theWindow);
+
+  theEvent.when = 0;
+
+  theEvent.where = globalMouseLoc;
+
+  theEvent.modifiers = 0;
+
+  
+
+  // start the drag
+
+  result = ::TrackDrag ( mDragRef, &theEvent, insetDragRegion );
+
+  
+
+  // clean up after ourselves 
+
+  ::DisposeRgn ( insetDragRegion );
+
+  ::DisposeDrag ( mDragRef );
+
+  
+
+  return NS_OK;
 
   
 
 } // StartDragSession
+
+
+
+
+
+//
+
+// GetData
+
+//
+
+// Pull data out of the OS drag items and stash it into the given transferable
+
+//
+
+NS_IMETHODIMP
+
+nsDragService :: GetData (nsITransferable * aTransferable)
+
+{
+
+  printf("nsDragService::GetData -- unimplemented on MacOS\n");
+
+  return NS_ERROR_FAILURE;
+
+}
+
+
+
+
+
+//
+
+// IsDataFlavorSupported
+
+//
+
+// Check the OS to see if the given drag flavor is in the list. Oddly returns
+
+// NS_OK for success and NS_ERROR_FAILURE if flavor is not present.
+
+//
+
+NS_IMETHODIMP
+
+nsDragService :: IsDataFlavorSupported(nsIDataFlavor * aDataFlavor)
+
+{
+
+  nsresult flavorSupported = NS_ERROR_FAILURE;
+
+  
+
+  // convert to 4 character MacOS type
+
+  nsAutoString mimeType;
+
+  aDataFlavor->GetMimeType(mimeType);
+
+  FlavorType macFlavor = nsMimeMapperMac::MapMimeTypeToMacOSType(mimeType);
+
+  
+
+  // search through all drag items looking for something with this flavor
+
+  unsigned short numDragItems = 0;
+
+  ::CountDragItems ( mDragRef, &numDragItems );
+
+  for ( int i = 0; i < numDragItems; ++i ) {
+
+    ItemReference currItem;
+
+    OSErr res = ::GetDragItemReferenceNumber ( mDragRef, i, &currItem );
+
+    if ( res != noErr )
+
+      return NS_ERROR_FAILURE;
+
+    
+
+    FlavorFlags ignored;
+
+    char foundFlavor = ::GetFlavorFlags(mDragRef, currItem, macFlavor, &ignored) == noErr;
+
+    if ( foundFlavor )
+
+      flavorSupported = NS_OK;
+
+  } // for each item in drag
+
+  
+
+  return flavorSupported;
+
+
+
+} // IsDataFlavorSupported
 
 
 
