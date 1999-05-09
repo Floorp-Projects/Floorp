2996899c0bfb6487fd40de21c76e70d2cfc98680
beard%netscape.com <beard%netscape.com>
1999-05-09 03:19:07 +0000

    NPL

--- xpcom/typelib/xpidl/macplugin/mac_stdlib.cpp
+++ xpcom/typelib/xpidl/macplugin/mac_stdlib.cpp
@@ -1,7 +1,24 @@
 /*
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/*
 	mac_stdlib.cpp
 
-	replacement functions for the Metrowerks plugin.
+	replacement functions for the CodeWarrior plugin.
 	
 	by Patrick C. Beard.
  */
@@ -15,5 +32,3 @@ void std::exit(int status)
 {
 	throw status;
 }
-
-
