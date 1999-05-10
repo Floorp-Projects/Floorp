ffdab9561750cf88d9ca044036e398bcaf70ad7e
rhp%netscape.com <rhp%netscape.com>
1999-05-10 22:17:13 +0000

    Changes for new message send testing

--- mailnews/compose/tests/compose/test.cpp
+++ mailnews/compose/tests/compose/test.cpp
@@ -1,140 +1,206 @@
 #include <stdio.h>
+#ifdef XP_PC
+#include <windows.h>
+#endif
+
 #include "nsIComponentManager.h" 
 #include "nsMsgCompCID.h"
 #include "nsIMsgCompose.h"
 #include "nsIMsgCompFields.h"
 #include "nsIMsgSend.h"
+#include "nsIPref.h"
+#include "nsIServiceManager.h"
+#include "nscore.h"
+#include "nsIMsgMailSession.h"
 
+
+#include "nsINetService.h"
+#include "nsIComponentManager.h"
+#include "nsString.h"
+
+#include "nsISmtpService.h"
+#include "nsISmtpUrl.h"
+#include "nsIUrlListener.h"
+#include "nsIEventQueueService.h"
+#include "nsIEventQueue.h"
+#include "nsXPComCIID.h"
+#include "nsIFileLocator.h"
+
+#ifdef XP_PC
+#define NETLIB_DLL "netlib.dll"
+#define XPCOM_DLL  "xpcom32.dll"
+#define PREF_DLL   "xppref32.dll"
+#define APPSHELL_DLL "nsappshell.dll"
+#else
+#ifdef XP_MAC
+#include "nsMacRepository.h"
+#else
+#define NETLIB_DLL "libnetlib.so"
+#define XPCOM_DLL  "libxpcom.so"
+#define PREF_DLL   "libpref.so"
+#define APPCORES_DLL  "libappcores.so"
+#define APPSHELL_DLL "libnsappshell.so"
+#endif
+#endif
+
+/////////////////////////////////////////////////////////////////////////////////
+// Define keys for all of the interfaces we are going to require for this test
+/////////////////////////////////////////////////////////////////////////////////
+
+static NS_DEFINE_CID(kNetServiceCID, NS_NETSERVICE_CID);
+static NS_DEFINE_CID(kEventQueueServiceCID, NS_EVENTQUEUESERVICE_CID);
+static NS_DEFINE_CID(kSmtpServiceCID, NS_SMTPSERVICE_CID);
+static NS_DEFINE_IID(kFileLocatorCID, NS_FILELOCATOR_CID);
+static NS_DEFINE_IID(kEventQueueCID, NS_EVENTQUEUE_CID);
 static NS_DEFINE_IID(kIMsgComposeIID, NS_IMSGCOMPOSE_IID); 
 static NS_DEFINE_CID(kMsgComposeCID, NS_MSGCOMPOSE_CID); 
-
 static NS_DEFINE_IID(kIMsgCompFieldsIID, NS_IMSGCOMPFIELDS_IID); 
 static NS_DEFINE_CID(kMsgCompFieldsCID, NS_MSGCOMPFIELDS_CID); 
-
 static NS_DEFINE_IID(kIMsgSendIID, NS_IMSGSEND_IID); 
 static NS_DEFINE_CID(kMsgSendCID, NS_MSGSEND_CID); 
+static NS_DEFINE_CID(kPrefCID, NS_PREF_CID);
+static NS_DEFINE_CID(kCMsgMailSessionCID, NS_MSGMAILSESSION_CID); 
 
-/* This is just a testing stub added by mscott. All I'm doing is loading a component,
-   and querying it for a particular interface.
+nsresult OnIdentityCheck()
+{
+	nsresult result = NS_OK;
+	NS_WITH_SERVICE(nsIMsgMailSession, mailSession, kCMsgMailSessionCID, &result); 
+	if (NS_SUCCEEDED(result) && mailSession)
+	{
+		// mscott: we really don't check an identity, we check
+		// for an outgoing 
+		nsIMsgIncomingServer * incomingServer = nsnull;
+		result = mailSession->GetCurrentServer(&incomingServer);
+		if (NS_SUCCEEDED(result) && incomingServer)
+		{
+			char * value = nsnull;
+			incomingServer->GetPrettyName(&value);
+			printf("Server pretty name: %s\n", value ? value : "");
+			incomingServer->GetUserName(&value);
+			printf("User Name: %s\n", value ? value : "");
+			incomingServer->GetHostName(&value);
+			printf("Pop Server: %s\n", value ? value : "");
+			incomingServer->GetPassword(&value);
+			printf("Pop Password: %s\n", value ? value : "");
 
-   It is its only purpose / use....
-
- */
-
- int main(int argc, char *argv[]) 
- { 
-	nsIMsgCompose *pMsgCompose; 
-	nsIMsgCompFields *pMsgCompFields;
-	nsIMsgSend *pMsgSend;
-	nsresult res;
-
-	// register our dll
-	nsComponentManager::RegisterComponent(kMsgComposeCID, NULL, NULL, "msgcompose.dll", PR_FALSE, PR_FALSE);
-	nsComponentManager::RegisterComponent(kMsgCompFieldsCID, NULL, NULL, "msgcompose.dll", PR_FALSE, PR_FALSE);
-	nsComponentManager::RegisterComponent(kMsgSendCID, NULL, NULL, "msgcompose.dll", PR_FALSE, PR_FALSE);
-
-	res = nsComponentManager::CreateInstance(kMsgCompFieldsCID, 
-                                           NULL, 
-                                           nsIMsgCompFields::GetIID(), 
-                                           (void **) &pMsgCompFields); 
-
-	if (res == NS_OK && pMsgCompFields) { 
-		printf("We succesfully obtained a nsIMsgCompFields interface....\n");
-
-/*
-		const char * value;
-		pMsgCompFields->SetFrom("field #01 FROM", NULL);
-		pMsgCompFields->SetReplyTo("field #02 REPLY_TO", NULL);
-		pMsgCompFields->SetTo("field #03 TO", NULL);
-		pMsgCompFields->SetCc("field #04 CC", NULL);
-		pMsgCompFields->SetBcc("field #05 BCC", NULL);
-		pMsgCompFields->SetFcc("field #06 FCC", NULL);
-		pMsgCompFields->SetNewsFcc("field #07 NEWS_FCC", NULL);
-		pMsgCompFields->SetNewsBcc("field #08 NEWS_BCC", NULL);
-		pMsgCompFields->SetNewsgroups("field #09 NEWSGROUPS", NULL);
-		pMsgCompFields->SetFollowupTo("field #10 FOLLOWUP_TO", NULL);
-		pMsgCompFields->SetSubject("field #11 SUBJECT", NULL);
-		pMsgCompFields->SetAttachments("field #12 ATTACHMENTS", NULL);
-		pMsgCompFields->SetOrganization("field #13 ORGANIZATION", NULL);
-		pMsgCompFields->SetReferences("field #14 REFERENCES", NULL);
-		pMsgCompFields->SetOtherRandomHeaders("field #15 OTHET_RANDOM_HEADEARS", NULL);
-		pMsgCompFields->SetNewspostUrl("field #16 NEWS_POST_URL", NULL);
-		pMsgCompFields->SetDefaultBody("field #17 DEFAULT_BODY", NULL);
-		pMsgCompFields->SetPriority("field #18 PRIORITY", NULL);
-		pMsgCompFields->SetMessageEncoding("field #19 MESSAGE_ENCODING", NULL);
-		pMsgCompFields->SetCharacterSet("field #20 CHARACTER_SET", NULL);
-		pMsgCompFields->SetMessageId("field #21 MESSAGE_ID", NULL);
-		pMsgCompFields->SetHTMLPart("field #22 HTML_PART", NULL);
-		pMsgCompFields->SetTemplateName("field #23 TEMPLATE_NAME", NULL);
-		pMsgCompFields->SetReturnReceipt(PR_TRUE, NULL);
-		pMsgCompFields->SetAttachVCard(PR_TRUE, NULL);
-
-		nsIMsgCompFields * pCopyFields;
-		res = nsComponentManager::CreateInstance(kMsgCompFieldsCID, 
-												NULL, 
-												nsIMsgCompFields::GetIID(), 
-												(void **) &pCopyFields); 
-
-		if (res == NS_OK && pCopyFields) { 
-			printf("We succesfully obtained a second nsIMsgCompFields interface....\n");
-
-			pCopyFields->Copy(pMsgCompFields);
-			pMsgCompFields->GetFrom((char **)&value);
-			printf("%s\n", value);
-			pMsgCompFields->GetReplyTo((char **)&value);
-			printf("%s\n", value);
-			pMsgCompFields->GetTo((char **)&value);
-			printf("%s\n", value);
-			pMsgCompFields->GetCc((char **)&value);
-			printf("%s\n", value);
-			pCopyFields->Release();
+			NS_RELEASE(incomingServer);
 		}
-*/
-
-	 printf("Releasing the interface now...\n");
-     pMsgCompFields->Release(); 
-   } 
-
-    res = nsComponentManager::CreateInstance(kMsgComposeCID, 
-                                               NULL, 
-                                               nsIMsgCompose::GetIID(), 
-                                               (void **) &pMsgCompose); 
-
-   if (res == NS_OK && pMsgCompose) {
-	 printf("We succesfully obtained a nsIMsgCompose interface....\n");
-#if 0
-	 pMsgCompose->CreateAndInitialize(NULL, NULL, NULL, NULL, NULL);
-#endif
-	 printf("Releasing the interface now...\n");
-     pMsgCompose->Release(); 
-   } 
- 
-	res = nsComponentManager::CreateInstance(kMsgSendCID, 
-                                               NULL, 
-                                               kIMsgSendIID, 
-                                               (void **) &pMsgSend); 
-
-	if (res == NS_OK && pMsgSend) { 
-		printf("We succesfully obtained a nsIMsgSend interface....\n");
-
-		res = nsComponentManager::CreateInstance(kMsgCompFieldsCID, 
-													NULL, 
-													kIMsgCompFieldsIID, 
-													(void **) &pMsgCompFields); 
-		if (res == NS_OK && pMsgCompFields) { 
-			pMsgCompFields->SetFrom(", qatest02@netscape.com, ", NULL);
-			pMsgCompFields->SetTo("qatest02@netscape.com", NULL);
-			pMsgCompFields->SetSubject("[spam] test", NULL);
-			pMsgCompFields->SetBody("Sample message sent with Mozilla\n\nPlease do not reply, thanks\n\nJean-Francois\n", NULL);
-
-			pMsgSend->SendMessage(pMsgCompFields, "");
-
-			pMsgCompFields->Release(); 
-		}
-
-		printf("Releasing the interface now...\n");
-		pMsgSend->Release(); 
+		else
+			printf("Unable to retrieve the outgoing server interface....\n");
 	}
+	else
+		printf("Unable to retrieve the mail session service....\n");
 
-	return 0; 
- }
+	return result;
+}
+
+/* 
+ * This is a test stub for mail composition. This will be enhanced as the
+ * development continues for message send functions. 
+ */
+int main(int argc, char *argv[]) 
+{ 
+  nsIMsgCompose *pMsgCompose; 
+  nsIMsgCompFields *pMsgCompFields;
+  nsIMsgSend *pMsgSend;
+  nsresult rv = NS_OK;
+
+  nsComponentManager::RegisterComponent(kNetServiceCID, NULL, NULL, NETLIB_DLL, PR_FALSE, PR_FALSE);
+	nsComponentManager::RegisterComponent(kEventQueueServiceCID, NULL, NULL, XPCOM_DLL, PR_FALSE, PR_FALSE);
+	nsComponentManager::RegisterComponent(kEventQueueCID, NULL, NULL, XPCOM_DLL, PR_FALSE, PR_FALSE);
+	nsComponentManager::RegisterComponent(kPrefCID, nsnull, nsnull, PREF_DLL, PR_TRUE, PR_TRUE);
+	nsComponentManager::RegisterComponent(kFileLocatorCID,  NULL, NULL, APPSHELL_DLL, PR_FALSE, PR_FALSE);
+
+  	// Create the Event Queue for this thread...
+	NS_WITH_SERVICE(nsIEventQueueService, pEventQService, kEventQueueServiceCID, &rv); 
+
+	if (NS_FAILED(rv)) 
+  {
+    printf("Failed to get event queue\n");
+    return rv;
+  }
+
+  rv = pEventQService->CreateThreadEventQueue();
+	if (NS_FAILED(rv)) 
+  {
+    printf("Failed to create event queue\n");
+    return rv;
+  }
+
+
+
+	// make sure prefs get initialized and loaded..
+	// mscott - this is just a bad bad bad hack right now until prefs
+	// has the ability to take nsnull as a parameter. Once that happens,
+	// prefs will do the work of figuring out which prefs file to load...
+	NS_WITH_SERVICE(nsIPref, prefs, kPrefCID, &rv); 
+    if (NS_FAILED(rv) || (prefs == nsnull)) {
+        exit(rv);
+    }
+
+  NS_WITH_SERVICE(nsIMsgMailSession, mailSession, kCMsgMailSessionCID, &rv);
+  if (NS_FAILED(rv)) 
+  {
+    printf("Failure on Mail Session Init!\n");
+    return rv;
+  }  
+
+  OnIdentityCheck();
+
+
+  rv = nsComponentManager::CreateInstance(kMsgCompFieldsCID, NULL, 
+                                           nsIMsgCompFields::GetIID(), (void **) &pMsgCompFields);   
+  if (rv == NS_OK && pMsgCompFields) { 
+    printf("We succesfully obtained a nsIMsgCompFields interface....\n");
+    printf("Releasing the interface now...\n");
+    pMsgCompFields->Release(); 
+  } 
+  
+  rv = nsComponentManager::CreateInstance(kMsgComposeCID, NULL, 
+                                           nsIMsgCompose::GetIID(), (void **) &pMsgCompose);
+  if (rv == NS_OK && pMsgCompose)
+  {
+    printf("We succesfully obtained a nsIMsgCompose interface....\n");
+    printf("Releasing the interface now...\n");
+    pMsgCompose->Release(); 
+  } 
+
+  rv = nsComponentManager::CreateInstance(kMsgSendCID, NULL, kIMsgSendIID, (void **) &pMsgSend); 
+  if (rv == NS_OK && pMsgSend) 
+  { 
+    printf("We succesfully obtained a nsIMsgSend interface....\n");    
+    rv = nsComponentManager::CreateInstance(kMsgCompFieldsCID, NULL, kIMsgCompFieldsIID, 
+                                             (void **) &pMsgCompFields); 
+    if (rv == NS_OK && pMsgCompFields)
+    { 
+      pMsgCompFields->SetFrom(", rhp@netscape.com, ", NULL);
+      pMsgCompFields->SetTo("rhp@netscape.com", NULL);
+      pMsgCompFields->SetSubject("[spam] test", NULL);
+      pMsgCompFields->SetBody("Sample message sent with Mozilla\n\nPlease do not reply, thanks\n\nRich Pizzarro\n", NULL);
+      pMsgCompFields->SetAttachments("c:\\boxster.jpg", NULL);
+
+      pMsgSend->SendMessage(pMsgCompFields, "");
+    }
+    
+  }
+
+#ifdef XP_PC
+  printf("Sitting in an event processing loop ...Hit Cntl-C to exit...");
+  while (1)
+  {
+    MSG msg;
+    if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+    {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+  }
+#endif
+
+  printf("Releasing the interface now...\n");
+  pMsgSend->Release(); 
+  pMsgCompFields->Release(); 
+
+
+  return 0; 
+}
