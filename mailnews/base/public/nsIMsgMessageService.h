6d0c5b64666c34c456ae3f7d835ea8d0ba72b835
alecf%netscape.com <alecf%netscape.com>
1999-05-12 07:55:16 +0000

    remove entire nsIURL #include

--- mailnews/base/public/nsIMsgMessageService.h
+++ mailnews/base/public/nsIMsgMessageService.h
@@ -14,7 +14,6 @@
 class nsIURL; /* forward decl */
 class nsIUrlListener; /* forward decl */
 class nsIStreamListener; /* forward decl */
-#include "nsIURL.h"
 #include "nsIStreamListener.h"
     
 
