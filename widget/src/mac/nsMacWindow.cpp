c0c5d27106fe2a243c48dfec9353cf0bf813aca7
pinkerton%netscape.com <pinkerton%netscape.com>
1999-05-12 00:28:20 +0000

    fix bustage.

--- widget/src/mac/nsMacWindow.cpp
+++ widget/src/mac/nsMacWindow.cpp
@@ -55,10 +55,14 @@ const short kWindowMarginWidth = 6;
 const short kDialogTitleBarHeight = 26;
 const short kDialogMarginWidth = 6;
 
-
+#if TARGET_CARBON
+// quickest fix. not sure why this doesn't compile w/out carbon.
 DragTrackingHandlerUPP nsMacWindow::sDragTrackingHandlerUPP = ::NewDragTrackingHandlerProc(DragTrackingHandler);
 DragReceiveHandlerUPP nsMacWindow::sDragReceiveHandlerUPP = ::NewDragReceiveHandlerProc(DragReceiveHandler);
-
+#else
+DragTrackingHandlerUPP nsMacWindow::sDragTrackingHandlerUPP = NULL;
+DragReceiveHandlerUPP nsMacWindow::sDragReceiveHandlerUPP = NULL;
+#endif
 
 //еее this should probably go into the drag session as a static
 pascal OSErr
