9e5a8ce12874ba10cd61dced12b65cdfdffcae10
pinkerton@netscape.com <pinkerton@netscape.com>
1999-05-13 00:13:06 +0000

    somehow, these files got extra blank lines in them between every source file line. Removing them. Also removing duplicate implementations of addref, release, and QI, since we inherit them from nsBaseDragService.

--- widget/src/mac/nsDragService.h
+++ widget/src/mac/nsDragService.h
@@ -1,125 +1,66 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
-
  *
-
  * The contents of this file are subject to the Netscape Public License
-
  * Version 1.0 (the "NPL"); you may not use this file except in
-
  * compliance with the NPL.  You may obtain a copy of the NPL at
-
  * http://www.mozilla.org/NPL/
-
  *
-
  * Software distributed under the NPL is distributed on an "AS IS" basis,
-
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
-
  * for the specific language governing rights and limitations under the
-
  * NPL.
-
  *
-
  * The Initial Developer of this code under the NPL is Netscape
-
  * Communications Corporation.  Portions created by Netscape are
-
  * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
-
  * Reserved.
-
  */
 
 
-
 //
-
 // Mike Pinkerton
-
 // Netscape Communications
-
 //
-
 // The MacOS native implementation of  nsIDragService.
-
 //
-
-
-
 
 
 #ifndef nsDragService_h__
-
 #define nsDragService_h__
 
 
-
 #include "nsBaseDragService.h"
-
 #include <Drag.h>
-
 
 
 class  nsNativeDragTarget;
 
 
-
-
-
 //
-
 // Native MacOS DragService/DragSession implementation
-
 //
-
 class nsDragService : public nsBaseDragService
-
 {
-
 public:
-
   nsDragService();
-
   virtual ~nsDragService();
 
-
-
   //nsISupports
-
-  NS_DECL_ISUPPORTS_INHERITED
-
+  //NS_DECL_ISUPPORTS_INHERITED
   
-
   //nsIDragService
-
   NS_IMETHOD InvokeDragSession (nsISupportsArray * anArrayTransferables, nsIRegion * aRegion, PRUint32 aActionType);
-
   //NS_IMETHOD InvokeDragSessionSingle (nsITransferable * aTransferable,  nsIRegion * aRegion, PRUint32 aActionType);
 
-
-
   //nsIDragSession
-
   NS_IMETHOD GetData (nsITransferable * aTransferable);
-
   NS_IMETHOD IsDataFlavorSupported(nsIDataFlavor * aDataFlavor);
-
-
 
 private:
 
-
-
   DragReference mDragRef;    // reference to _the_ drag. There can be only one.
 
-  
-
 }; // class nsDragService
-
-
-
 
 
 #endif // nsDragService_h__
