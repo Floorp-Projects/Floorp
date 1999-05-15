0f22a2972350fc9fc908834c1ffdef1be275879f
law%netscape.com <law%netscape.com>
1999-05-15 01:01:29 +0000

    Got one that will make everybody happy (if Mac goes red, I'll scream)

--- xpfe/appshell/src/nsAppShellService.cpp
+++ xpfe/appshell/src/nsAppShellService.cpp
@@ -221,7 +221,7 @@ nsAppShellService::Initialize( nsICmdLineService *aCmdLineService )
   }
 
   // Initialize each registered component.
-  EnumerateComponents( &InitializeComponent );
+  EnumerateComponents( &nsAppShellService::InitializeComponent );
 
 done:
   return rv;
@@ -397,7 +397,7 @@ nsAppShellService::Shutdown(void)
 
 #if 1
   // Shutdown all components.
-  EnumerateComponents( &ShutdownComponent );
+  EnumerateComponents( &nsAppShellService::ShutdownComponent );
 
   mAppShell->Exit();
 #else
