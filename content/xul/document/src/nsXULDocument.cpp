2ce85ee0fa375d1c3b0932ef39103a892d4b85b1
hyatt%netscape.com <hyatt%netscape.com>
1999-05-18 10:21:11 +0000

    Disabling the cloned style sheets. This is causing trouble.  Will probably have to force them to be reparsed.

--- content/xul/document/src/nsXULDocument.cpp
+++ content/xul/document/src/nsXULDocument.cpp
@@ -2949,12 +2949,13 @@ XULDocumentImpl::CreatePopupDocument(nsIContent* aPopupElement, nsIDocument** aR
       return rv;
     }
     // Now we need to copy all of the style sheets from the parent document
-    PRInt32 count = mStyleSheets.Count();
+    /*PRInt32 count = mStyleSheets.Count();
     for (PRInt32 i = 0; i < count; i++) {
       // Don't bother addrefing. We don't live as long as our parent document
         nsIStyleSheet* sheet = (nsIStyleSheet*) mStyleSheets.ElementAt(i);
+        sheet->SetOwningDocument(popupDoc);
         popupDoc->mStyleSheets.AppendElement(sheet);  
-    }
+    }*/
 
     // We don't share builders, but we do share the DB that the current
     // XUL builder has.
