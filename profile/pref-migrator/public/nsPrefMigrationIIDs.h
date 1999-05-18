50a9e17c7700571e13b8df8c163dca54a94ef2fc
dbragg%netscape.com <dbragg%netscape.com>
1999-05-18 00:03:44 +0000

    This file was in the wrong place

--- profile/pref-migrator/public/nsPrefMigrationIIDs.h
+++ profile/pref-migrator/public/nsPrefMigrationIIDs.h
@@ -1,23 +1,31 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+
 #ifndef nsPrefMigrationIIDs_h___
-
 #define nsPrefMigrationIIDs_h___
-
-
 
 #define NS_IPrefMigration_IID { 0xc29db6a1, 0xd195, 0x11d2, 0xbc, 0xf1, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53}
 
-
-
 #define NS_PrefMigration_CID  { 0xc29db6a2, 0xd195, 0x11d2, 0xbc, 0xf1, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53}
-
-
 
 #define NS_PrefMigrationFactory_CID {  0xc29db6a3, 0xd195,  0x11d2, 0xbc, 0xf1, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53} 
 
-
-
 #define NS_IPrefMigrationFactory_IID { 0xc29db6a4, 0xd195, 0x11d2, 0xbc, 0xf1, 0x00, 0x80, 0x5f, 0x0e, 0x13, 0x53}
-
-
 
 #endif /* nsPrefMigrationIIDs_h___ */
