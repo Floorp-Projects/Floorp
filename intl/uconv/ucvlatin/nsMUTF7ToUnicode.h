20a749ea5266ac2a52892c8a63660fda2edda342
cata@netscape.com <cata@netscape.com>
1999-05-18 22:36:25 +0000

    Created.

---
+++ intl/uconv/ucvlatin/nsMUTF7ToUnicode.h
@@ -0,0 +1,49 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+#ifndef nsMUTF7ToUnicode_h___
+#define nsMUTF7ToUnicode_h___
+
+#include "nsUCvLatinSupport.h"
+
+//----------------------------------------------------------------------
+// Class nsMUTF7ToUnicode [declaration]
+
+/**
+ * A character set converter from MUTF7 to Unicode.
+ *
+ * @created         18/May/1999
+ * @author  Catalin Rotaru [CATA]
+ */
+class nsMUTF7ToUnicode : public nsOneByteDecoderSupport
+{
+public:
+
+  /**
+   * Class constructor.
+   */
+  nsMUTF7ToUnicode();
+
+  /**
+   * Static class constructor.
+   */
+  static nsresult CreateInstance(nsISupports **aResult);
+};
+
+#endif /* nsMUTF7ToUnicode_h___ */
