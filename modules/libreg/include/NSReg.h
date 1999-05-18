0b0798908ef0762f1cd2cfd3649896009b604311
dveditz@netscape.com <dveditz@netscape.com>
1999-05-18 14:46:02 +0000

    NSReg.h

--- modules/libreg/include/NSReg.h
+++ modules/libreg/include/NSReg.h
@@ -165,6 +165,9 @@ VR_INTERFACE(REGERR) NR_RegClose(
  *
  * Parameters:
  *    hReg     - handle of open registry to query
+ * Returns:
+ *    REGERR_OK if writable, REGERR_READONLY if not, possibly
+ *    other errors for an invalid hReg
  * ---------------------------------------------------------------------
  */
 VR_INTERFACE(REGERR) NR_RegIsWritable(
