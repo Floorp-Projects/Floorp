7361fdede5e9a5694d23838a5372c5a08cb0a049
dveditz%netscape.com <dveditz%netscape.com>
1999-05-19 04:06:40 +0000

    new file

---
+++ modules/libjar/zipstub.h
@@ -0,0 +1,71 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Communicator client code, 
+ * released March 31, 1998. 
+ *
+ * The Initial Developer of the Original Code is Netscape Communications 
+ * Corporation.  Portions created by Netscape are 
+ * Copyright (C) 1998-1999 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ *
+ * Contributors:
+ *     Daniel Veditz <dveditz@netscape.com>
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <malloc.h>
+
+#define PR_ASSERT         assert
+#define PR_Malloc         malloc
+#define PR_FREEIF(x)      do { if (x) free(x); } while(0)
+#define PL_strfree        free
+#define PL_strcmp         strcmp
+#define PL_strdup         strdup
+#define PL_strcpy         strcpy
+
+#define PR_Open(a,b,c)    fopen((a),(b))
+#define PR_Read(f,d,n)    fread((d),1,(n),(f))
+#define PR_Write(f,s,n)   fwrite((s),1,(n),(f))
+#define PR_Close          fclose
+#define PR_Seek           fseek
+
+#define PR_BEGIN_EXTERN_C       extern "C" {
+#define PR_END_EXTERN_C         }
+
+#define PR_EXTERN(__type)       extern _declspec(dllexport) __type
+#define PR_PUBLIC_API(__type)   _declspec(dllexport) __type
+
+#define NS_STATIC_CAST(__type, __ptr)      ((__type)(__ptr))
+
+#define PRFileDesc          FILE
+typedef long                PRInt32;
+typedef unsigned long       PRUint32;
+typedef short               PRInt16;
+typedef unsigned short      PRUint16;
+typedef char                PRBool;
+typedef unsigned char       PRUint8;
+
+#define PR_TRUE             1
+#define PR_FALSE            0
+
+#define INVALID_SXP   -1
+#define NON_SXP       -2
+#define VALID_SXP     1
+
+#define PR_RDONLY     "rb"
+#define PR_SEEK_SET   SEEK_SET
+
+#define XP_RegExpValid(a)       NON_SXP
+#define XP_RegExpMatch(a,b,c)   PR_FALSE
