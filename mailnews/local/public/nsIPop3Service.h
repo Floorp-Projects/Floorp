26053eb932e0de9807869ba03c2bde073db7b959
alecf%netscape.com <alecf%netscape.com>
1999-05-27 04:53:32 +0000

    IDL-ify these interfaces move CIDs into nsMsgLocalCID

--- mailnews/local/public/nsIPop3Service.h
+++ mailnews/local/public/nsIPop3Service.h
@@ -1,78 +1,34 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- *
- * The contents of this file are subject to the Netscape Public License
- * Version 1.0 (the "NPL"); you may not use this file except in
- * compliance with the NPL.  You may obtain a copy of the NPL at
- * http://www.mozilla.org/NPL/
- *
- * Software distributed under the NPL is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
- * for the specific language governing rights and limitations under the
- * NPL.
- *
- * The Initial Developer of this code under the NPL is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
- * Reserved.
+/*
+ * DO NOT EDIT.  THIS FILE IS GENERATED FROM nsIPop3Service.idl
  */
 
-#ifndef nsIPop3Service_h___
-#define nsIPop3Service_h___
+#ifndef __gen_nsIPop3Service_h__
+#define __gen_nsIPop3Service_h__
 
-#include "nscore.h"
-#include "nsISupports.h"
-#include "nsIPop3IncomingServer.h"
-#include "MailNewsTypes.h"
-#include "nsFileSpec.h"
+#include "nsISupports.h" /* interface nsISupports */
+#include "nsIPop3IncomingServer.h" /* interface nsIPop3IncomingServer */
+#include "nsrootidl.h" /* interface nsrootidl */
+#include "nsIUrlListener.h" /* interface nsIUrlListener */
+class nsIURL; /* forward decl */
+class nsIStreamListener; /* forward decl */
 
-/* 3BB459E0-D746-11d2-806A-006008128C4E */
+/* starting interface:    nsIPop3Service */
 
-#define NS_IPOP3SERVICE_IID								\
-{ 0x3bb459e0, 0xd746, 0x11d2,							\
-    { 0x80, 0x6a, 0x0, 0x60, 0x8, 0x12, 0x8c, 0x4e } }
+/* {3BB459E0-D746-11d2-806A-006008128C4E} */
+#define NS_IPOP3SERVICE_IID_STR "3BB459E0-D746-11d2-806A-006008128C4E"
+#define NS_IPOP3SERVICE_IID \
+  {0x3BB459E0, 0xD746, 0x11d2, \
+    { 0x80, 0x6A, 0x00, 0x60, 0x08, 0x12, 0x8C, 0x4E }}
 
-#define NS_POP3SERVICE_CID								\
-{ /* 3BB459E3-D746-11d2-806A-006008128C4E} */			\
- 0x3bb459e3, 0xd746, 0x11d2,							\
-  { 0x80, 0x6a, 0x0, 0x60, 0x8, 0x12, 0x8c, 0x4e }}
+class nsIPop3Service : public nsISupports {
+ public: 
+  NS_DEFINE_STATIC_IID_ACCESSOR(NS_IPOP3SERVICE_IID)
 
-////////////////////////////////////////////////////////////////////////////////////////
-// The Pop3 Service is an interface designed to make building and running pop3 urls
-// easier. I'm not sure if this service will go away when the new networking model comes
-// on line (as part of the N2 project). So I reserve the right to change my mind and take
-// this service away =).
-////////////////////////////////////////////////////////////////////////////////////////
-class nsIURL;
-class nsIStreamListener;
-class nsIUrlListener;
+  /* nsIURL GetNewMail (in nsIUrlListener aUrlListener, in nsIPop3IncomingServer popServer); */
+  NS_IMETHOD GetNewMail(nsIUrlListener *aUrlListener, nsIPop3IncomingServer *popServer, nsIURL **_retval) = 0;
 
-class nsIPop3Service : public nsISupports
-{
-public:
-    static const nsIID& GetIID() { static nsIID iid = NS_IPOP3SERVICE_IID; return iid; }
-
-	/////////////////////////////////////////////////////////////////////////////////////////////
-	// All of these functions build pop3 urls and run them. If you want a handle 
-	// on the running task, pass in a valid nsIURL ptr. You can later interrupt this action by 
-	// asking the netlib service manager to interrupt the url you are given back. Remember to release 
-	// aURL when you are done with it. Pass nsnull in for aURL if you don't care about 
-	// the returned URL.
-	/////////////////////////////////////////////////////////////////////////////////////////////
-	
-	/////////////////////////////////////////////////////////////////////////////////////////////
-	// right now getting new mail doesn't require any user specific data. We use the default current
-	// identity for this information. I suspect that we'll eventually pass in an identity to this 
-	// call so you can get mail on different pop3 accounts....
-	//////////////////////////////////////////////////////////////////////////////////////////////
-
-	NS_IMETHOD GetNewMail(nsIUrlListener * aUrlListener,
-                          nsIPop3IncomingServer *popServer,
-                          nsIURL ** aURL) = 0;
-
-	NS_IMETHOD CheckForNewMail(nsIUrlListener * aUrlListener,
-                               nsIPop3IncomingServer *popServer,
-                               nsIURL ** aURL) = 0;
-
+  /* nsIURL CheckForNewMail (in nsIUrlListener aUrlListener, in nsIPop3IncomingServer popServer); */
+  NS_IMETHOD CheckForNewMail(nsIUrlListener *aUrlListener, nsIPop3IncomingServer *popServer, nsIURL **_retval) = 0;
 };
 
-#endif /* nsIPop3Service_h___ */
+#endif /* __gen_nsIPop3Service_h__ */
