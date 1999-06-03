4b98dfedf6b2f702a68f8e530c1239b3f4ee3942
ssu@netscape.com <ssu@netscape.com>
1999-06-03 18:07:29 +0000

    initial checkin of file operation functions for xpInstall

---
+++ xpinstall/src/nsInstallFileOpEnums.h
@@ -0,0 +1,38 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsInstallFileOpEnums_h__
+#define nsInstallFileOpEnums_h__
+
+typedef enum nsInstallFileOpEnums {
+  NS_FOP_DIR_CREATE = 0,
+  NS_FOP_DIR_REMOVE = 1,
+  NS_FOP_DIR_RENAME = 2,
+  NS_FOP_FILE_COPY = 3,
+  NS_FOP_FILE_DELETE = 4,
+  NS_FOP_FILE_EXECUTE = 5,
+  NS_FOP_FILE_MOVE = 6,
+  NS_FOP_FILE_RENAME = 7,
+  NS_FOP_WIN_SHORTCUT_CREATE = 8,
+  NS_FOP_MAC_ALIAS_CREATE = 9,
+  NS_FOP_UNIX_LINK_CREATE = 10,
+  NS_FOP_FILE_SET_STAT = 11
+
+} nsInstallFileOpEnums;
+
+#endif /* nsInstallFileOpEnums_h__ */
---
+++ xpinstall/src/nsInstallFileOpItem.h
@@ -0,0 +1,111 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsInstallFileOpItem_h__
+#define nsInstallFileOpItem_h__
+
+#include "prtypes.h"
+
+#include "nsFileSpec.h"
+#include "nsSoftwareUpdate.h"
+#include "nsInstallObject.h"
+#include "nsInstall.h"
+
+class nsInstallFileOpItem : public nsInstallObject
+{
+  public:
+    /* Public Fields */
+
+    /* Public Methods */
+    // used by:
+    //   FileOpFileDelete()
+    nsInstallFileOpItem(nsInstall*    installObj,
+                        PRInt32       aCommand,
+                        nsFileSpec&   aTarget,
+                        PRInt32       aFlags,
+                        PRInt32*      aReturn);
+
+    // used by:
+    //   FileOpDirRemove()
+    //   FileOpDirRename()
+    //   FileOpFileCopy()
+    //   FileOpFileMove()
+    //   FileOpFileRename()
+    nsInstallFileOpItem(nsInstall*    installObj,
+                        PRInt32       aCommand,
+                        nsFileSpec&   aSrc,
+                        nsFileSpec&   aTarget,
+                        PRInt32*      aReturn);
+
+    // used by:
+    //   FileOpDirCreate()
+    nsInstallFileOpItem(nsInstall*    aInstallObj,
+                        PRInt32       aCommand,
+                        nsFileSpec&   aTarget,
+                        PRInt32*      aReturn);
+
+    // used by:
+    //   FileOpFileExecute()
+    nsInstallFileOpItem(nsInstall*    aInstallObj,
+                        PRInt32       aCommand,
+                        nsFileSpec&   aTarget,
+                        nsString&     aParams,
+                        PRInt32*      aReturn);
+
+    ~nsInstallFileOpItem();
+
+    PRInt32       Prepare(void);
+    PRInt32       Complete();
+    char*         toString();
+    void          Abort();
+    float         GetInstallOrder();
+    
+  /* should these be protected? */
+    PRBool        CanUninstall();
+    PRBool        RegisterPackageNode();
+      
+  private:
+    
+    /* Private Fields */
+    
+    nsInstall*    mIObj;        // initiating Install object
+    nsFileSpec*   mSrc;
+    nsFileSpec*   mTarget;
+    nsString*     mParams;
+    long          mFStat;
+    PRInt32       mFlags;
+    PRInt32       mCommand;
+    
+    /* Private Methods */
+
+    PRInt32       NativeFileOpDirCreate(nsFileSpec* aTarget);
+    PRInt32       NativeFileOpDirRemove(nsFileSpec* aTarget, PRInt32 aFlags);
+    PRInt32       NativeFileOpDirRename(nsFileSpec* aSrc, nsFileSpec* aTarget);
+    PRInt32       NativeFileOpFileCopy(nsFileSpec* aSrc, nsFileSpec* aTarget);
+    PRInt32       NativeFileOpFileDelete(nsFileSpec* aTarget, PRInt32 aFlags);
+    PRInt32       NativeFileOpFileExecute(nsFileSpec* aTarget, nsString* aParams);
+    PRInt32       NativeFileOpFileMove(nsFileSpec* aSrc, nsFileSpec* aTarget);
+    PRInt32       NativeFileOpFileRename(nsFileSpec* aSrc, nsFileSpec* aTarget);
+    PRInt32       NativeFileOpWinShortcutCreate();
+    PRInt32       NativeFileOpMacAliasCreate();
+    PRInt32       NativeFileOpUnixLinkCreate();
+
+};
+
+#endif /* nsInstallFileOpItem_h__ */
+
