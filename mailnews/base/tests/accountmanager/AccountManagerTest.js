aeeae419e08a9b93257e6bb99157251defec03f8
alecf%netscape.com <alecf%netscape.com>
1999-06-07 22:53:34 +0000

    new arrayDescribe() to describe() all elements of an array

--- mailnews/base/tests/accountmanager/AccountManagerTest.js
+++ mailnews/base/tests/accountmanager/AccountManagerTest.js
@@ -7,53 +7,30 @@ function displayAccounts() {
   var am = msg.accountManager;
 //  describe(am, "AccountManager");
   var servers = am.allServers;
-
-  for (var i=0; i< servers.Count(); i++) {
-    var serverSupports = servers.GetElementAt(i);
-    var server = serverSupports.QueryInterface(Components.interfaces.nsIMsgIncomingServer);
-
-    dump("Server: " + server.key + "\n");
-    describe(server, "\t" + server.key);
-
-    var identities = am.GetIdentitiesForServer(server);
-    
-    dump("\tThis server has " + identities.Count() + " identities:\n");
-    for (var id=0; i<identities.Count; i++) {
-      var identity =
-        identities.GetElementAt(id).QueryInterface(Components.interfaces.nsIMsgIdentities);
-
-      dump(identity.key + ":\n");
-    }
-  }
-
+  arrayDescribe(servers, Components.interfaces.nsIMsgIncomingServer, "Server");
+  
   var identities = am.allIdentities;
+  arrayDescribe(identities, Components.interfaces.nsIMsgIdentity, "Identity");
 
-  dump("There are " + identities.Count() + " identities\n");
-  for (var i=0; i< identities.Count(); i++) {
-    var identity = identities.GetElementAt(i).QueryInterface(Components.interfaces.nsIMsgIdentity);
-
-    dump("Identity: " + identity.key + "\n");
-    describe(identity, "\t" + identity.key);
-
-    var servers = am.GetServersForIdentity(identity);
-    
-    dump("\tThis identity has " + servers.Count() + " servers:\n");
-    for (var sd=0; i<servers.Count; i++) {
-      var server =
-        servers.GetElementAt(id).QueryInterface(Components.interfaces.nsIMsgIdentities);
-
-      dump(server.key + ":\n");
-    }
-  }
-
+  var accounts = am.accounts;
+  arrayDescribe(accounts, Components.interfaces.nsIMsgAccount, "Account");
   
 }
 
 
+function arrayDescribe(array, iface, name) {
+    dump("There are " + array.Count() + " " + name + "(s).\n");
+    for (var i=0; i<array.Count(); i++) {
+      var obj = array.GetElementAt(i).QueryInterface(iface);
+      dump(name + " #" + i + ":\n");
+      describe(obj, "\t" + name);
+    }
+}
+
 function describe(object, name) {
   for (var i in object) {
     var value = object[i];
-    if (object[i] == "function")
+    if (typeof(object[i]) == "function")
       value = "[function]";
     
     dump(name + "." + i + " = " + value + "\n");
