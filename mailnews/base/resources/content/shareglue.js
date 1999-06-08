15ba149a078727176b46a5b5c4131090ca939312
alecf%netscape.com <alecf%netscape.com>
1999-06-08 00:59:02 +0000

    add mozilla license, rename OnLoad/etc to OnLoadMessenger/etc

--- mailnews/base/resources/content/shareglue.js
+++ mailnews/base/resources/content/shareglue.js
@@ -1,3 +1,26 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/*
+ * code in here is generic, shared utility code across all messenger
+ * components. There should be no command or widget specific code here
+ */
+
 function NewBrowserWindow() {}
 function NewBlankPage() {} 
 function TemplatePage() {}
@@ -6,18 +29,20 @@ function PageSetup() {}
 function PrintPreview() {}
 function Print() {}
 
-function OnLoad()
+function OnLoadMessenger()
 {
 	messenger.SetWindow(window);
+    window.frames["messagepane"].location =
+      "http://www.mozilla.org/mailnews/prefs-info.html";
 }
 
-function OnUnload()
+function OnUnloadMessenger()
 {
 	dump("\nOnUnload from XUL\nClean up ...\n");
 	messenger.OnUnload();
 }
 
-function Close() 
+function CloseMessenger() 
 {
 	dump("\nClose from XUL\nDo something...\n");
 	messenger.Close();
--- mailnews/base/resources/content/widgetglue.js
+++ mailnews/base/resources/content/widgetglue.js
@@ -1,3 +1,28 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/*
+ * widget-specific wrapper glue. There should be one function for every
+ * widget/menu item, which gets some context (like the current selection)
+ * and then calls a function/command in commandglue
+ */
+
+
 function MsgLoadNewsMessage(url)
 {
   dump("\n\nMsgLoadNewsMessage from XUL\n");
