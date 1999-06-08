6ddd8d4031dec89fb4c4e22a5db476b9b70ee182
hyatt@netscape.com <hyatt@netscape.com>
1999-06-08 06:05:53 +0000

    Adding code to force the user into STANDARD mode.  Otherwise the chrome malfunctions.

--- xpfe/appshell/src/nsWebShellWindow.cpp
+++ xpfe/appshell/src/nsWebShellWindow.cpp
@@ -332,6 +332,14 @@ nsresult nsWebShellWindow::Initialize(nsIWebShellWindow* aParent,
                                     nsIPref::GetIID(), 
                                     (nsISupports **)&prefs);
   if (NS_SUCCEEDED(rv)) {
+    // XXX Enforce the STANDARD compatibility mode? Nav Quirks causes
+    // the chrome to malfunction. Having this be a general pref applied
+    // everywhere seems bogus to me.  We certainly don't want it on for
+    // the chrome.
+    prefs->SetIntPref("nglayout.compatibility.mode", eCompatibility_Standard);
+    prefs->SavePrefFile();
+
+    // Set the prefs in the outermost webshell.
     mWebShell->SetPrefs(prefs);
     nsServiceManager::ReleaseService(kPrefCID, prefs);
   }
