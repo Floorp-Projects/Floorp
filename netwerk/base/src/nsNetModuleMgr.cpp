4c567bcb0dea8625bb46b30ee89048ec1cb07957
valeski@netscape.com <valeski@netscape.com>
1999-06-09 14:30:20 +0000

    added nsNetModuleMgr.h to the src dir and added the Create method to the modMgr impl so it can be in the factory

--- netwerk/base/src/nsNetModuleMgr.cpp
+++ netwerk/base/src/nsNetModuleMgr.cpp
@@ -191,3 +191,18 @@ nsNetModuleMgr::~nsNetModuleMgr() {
     }
     //PR_DestroyLock(mLock);
 }
+
+NS_METHOD
+nsNetModuleMgr::Create(nsISupports *aOuter, REFNSIID aIID, void **aResult)
+{
+    if (aOuter)
+        return NS_ERROR_NO_AGGREGATION;
+
+    nsNetModuleMgr* mgr = new nsNetModuleMgr();
+    if (mgr == nsnull)
+        return NS_ERROR_OUT_OF_MEMORY;
+    NS_ADDREF(mgr);
+    nsresult rv = mgr->QueryInterface(aIID, aResult);
+    NS_RELEASE(mgr);
+    return rv;
+}
