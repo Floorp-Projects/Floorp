f615fcb71ee0abcd30ed0bc9d0ca290d87c600eb
ftang@netscape.com <ftang@netscape.com>
1999-06-10 23:31:51 +0000

    add implementation for ISO-8859-10

--- intl/uconv/ucvlatin/nsISO885910ToUnicode.h
+++ intl/uconv/ucvlatin/nsISO885910ToUnicode.h
@@ -1,17 +1,49 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
  * The contents of this file are subject to the Netscape Public License
- * Version 1.0 (the "NPL"); you may not use this file except in
- * compliance with the NPL.  You may obtain a copy of the NPL at
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
  * http://www.mozilla.org/NPL/
  *
- * Software distributed under the NPL is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
- * for the specific language governing rights and limitations under the
- * NPL.
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
  *
- * The Initial Developer of this code under the NPL is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
- * Reserved.
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
  */
+
+#ifndef nsISO885910ToUnicode_h___
+#define nsISO885910ToUnicode_h___
+
+#include "nsUCvLatinSupport.h"
+
+//----------------------------------------------------------------------
+// Class nsISO885910ToUnicode [declaration]
+
+/**
+ * A character set converter from ISO885910 to Unicode.
+ *
+ * @created         23/Nov/1998
+ * @author  Catalin Rotaru [CATA]
+ */
+class nsISO885910ToUnicode : public nsOneByteDecoderSupport
+{
+public:
+
+  /**
+   * Class constructor.
+   */
+  nsISO885910ToUnicode();
+
+  /**
+   * Static class constructor.
+   */
+  static nsresult CreateInstance(nsISupports **aResult);
+};
+
+#endif /* nsISO885910ToUnicode_h___ */
