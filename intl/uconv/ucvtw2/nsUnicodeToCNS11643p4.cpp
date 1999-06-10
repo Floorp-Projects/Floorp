8cc989368496daac533c5720a05351fd715d7467
ftang%netscape.com <ftang%netscape.com>
1999-06-10 14:36:31 +0000

    add real implementation for Unicode To CNS p3-p7 converters

--- intl/uconv/ucvtw2/nsUnicodeToCNS11643p4.cpp
+++ intl/uconv/ucvtw2/nsUnicodeToCNS11643p4.cpp
@@ -1,17 +1,59 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
  * The contents of this file are subject to the Netscape Public License
- * Version 1.0 (the "NPL"); you may not use this file except in
- * compliance with the NPL.  You may obtain a copy of the NPL at
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
  * http://www.mozilla.org/NPL/
  *
- * Software distributed under the NPL is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
- * for the specific language governing rights and limitations under the
- * NPL.
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
  *
- * The Initial Developer of this code under the NPL is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
- * Reserved.
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
  */
+
+#include "nsUnicodeToCNS11643p4.h"
+#include "nsUCvTW2Dll.h"
+
+//----------------------------------------------------------------------
+// Global functions and data [declaration]
+
+static PRInt16 g_ShiftTable[] =  {
+  0, u2BytesCharset,
+  ShiftCell(0, 0, 0, 0, 0, 0, 0, 0),
+};
+
+//----------------------------------------------------------------------
+// Class nsUnicodeToCNS11643p4 [implementation]
+
+nsUnicodeToCNS11643p4::nsUnicodeToCNS11643p4() 
+: nsTableEncoderSupport((uShiftTable*) &g_ShiftTable, 
+                        (uMappingTable*) &g_ufCNS4MappingTable)
+{
+}
+
+nsresult nsUnicodeToCNS11643p4::CreateInstance(nsISupports ** aResult) 
+{
+  nsIUnicodeEncoder *p = new nsUnicodeToCNS11643p4();
+  if(p) {
+   *aResult = p;
+   return NS_OK;
+  }
+  return NS_ERROR_OUT_OF_MEMORY;
+}
+
+//----------------------------------------------------------------------
+// Subclassing of nsTableEncoderSupport class [implementation]
+
+NS_IMETHODIMP nsUnicodeToCNS11643p4::GetMaxLength(const PRUnichar * aSrc, 
+                                              PRInt32 aSrcLength,
+                                              PRInt32 * aDestLength)
+{
+  *aDestLength = 2 * aSrcLength;
+  return NS_OK_UENC_EXACTLENGTH;
+}
