b2103bbb5481d8336fbaf7fb7b4d0e1bcecd0bfe
ftang@netscape.com <ftang@netscape.com>
1999-06-10 14:14:50 +0000

    add CNS 11643 p3-7 to EUCTW

--- intl/uconv/ucvtw2/nsEUCTWToUnicode.cpp
+++ intl/uconv/ucvtw2/nsEUCTWToUnicode.cpp
@@ -39,23 +39,58 @@ static PRInt16 g_CNS2ShiftTable[] =  {
   0, u2BytesGRPrefix8EA2Charset,  
   ShiftCell(0, 0, 0, 0, 0, 0, 0, 0),
 };
+static PRInt16 g_CNS3ShiftTable[] =  {
+  0, u2BytesGRPrefix8EA3Charset,  
+  ShiftCell(0, 0, 0, 0, 0, 0, 0, 0),
+};
+static PRInt16 g_CNS4ShiftTable[] =  {
+  0, u2BytesGRPrefix8EA4Charset,  
+  ShiftCell(0, 0, 0, 0, 0, 0, 0, 0),
+};
+static PRInt16 g_CNS5ShiftTable[] =  {
+  0, u2BytesGRPrefix8EA5Charset,  
+  ShiftCell(0, 0, 0, 0, 0, 0, 0, 0),
+};
+static PRInt16 g_CNS6ShiftTable[] =  {
+  0, u2BytesGRPrefix8EA6Charset,  
+  ShiftCell(0, 0, 0, 0, 0, 0, 0, 0),
+};
+static PRInt16 g_CNS7ShiftTable[] =  {
+  0, u2BytesGRPrefix8EA7Charset,  
+  ShiftCell(0, 0, 0, 0, 0, 0, 0, 0),
+};
 
 
 static PRInt16 *g_EUCTWShiftTableSet [] = {
   g_ASCIIShiftTable,
   g_CNS1ShiftTable,
-  g_CNS2ShiftTable
+  g_CNS2ShiftTable,
+  g_CNS3ShiftTable,
+  g_CNS4ShiftTable,
+  g_CNS5ShiftTable,
+  g_CNS6ShiftTable,
+  g_CNS7ShiftTable
 };
 
 static PRUint16 *g_EUCTWMappingTableSet [] ={
   g_ASCIIMappingTable,
-  g_CNS1MappingTable,
-  g_CNS2MappingTable
+  g_utCNS1MappingTable,
+  g_utCNS2MappingTable,
+  g_utCNS3MappingTable,
+  g_utCNS4MappingTable,
+  g_utCNS5MappingTable,
+  g_utCNS6MappingTable,
+  g_utCNS7MappingTable
 };
 
 static uRange g_EUCTWRanges[] = {
   { 0x00, 0x7E },
   { 0xA1, 0xFE },
+  { 0x8E, 0x8E },
+  { 0x8E, 0x8E },
+  { 0x8E, 0x8E },
+  { 0x8E, 0x8E },
+  { 0x8E, 0x8E },
   { 0x8E, 0x8E }
 };
 
@@ -64,7 +99,7 @@ static uRange g_EUCTWRanges[] = {
 // Class nsEUCTWToUnicode [implementation]
 
 nsEUCTWToUnicode::nsEUCTWToUnicode() 
-: nsMultiTableDecoderSupport(3, 
+: nsMultiTableDecoderSupport(8, 
                         (uRange*) &g_EUCTWRanges, 
                         (uShiftTable**) &g_EUCTWShiftTableSet, 
                         (uMappingTable**) &g_EUCTWMappingTableSet)
