1662f221c047f01fac311fdbccaac12bab856af6
dp%netscape.com <dp%netscape.com>
1999-06-12 07:54:05 +0000

    Removing obsoleted files from xpcom/ post xpcom2.0 landing

--- xpcom/libxpt/xptcall/src/md/test/mk_invoke.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@echo deleing old output
-if exist invoke_test.obj del invoke_test.obj > NUL
-if exist invoke_test.ilk del invoke_test.ilk > NUL
-if exist *.pdb           del *.pdb           > NUL
-if exist invoke_test.exe del invoke_test.exe > NUL
-
-@echo building...
-cl /nologo -Zi -DWIN32 invoke_test.cpp
