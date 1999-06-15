ef252a2bebbc39a53216cff0d5d76fe37afe7f81
rjc@netscape.com <rjc@netscape.com>
1999-06-15 05:14:37 +0000

    Check-in search UI.

---
+++ xpfe/components/search/resources/search.js
@@ -0,0 +1,40 @@
+
+function loadPage(thePage)
+{
+	var	content="", results="";
+
+	if (thePage == "find")
+	{
+		content="find.xul";
+		results="findresults.xul";
+	}
+	else if (thePage == "internet")
+	{
+		content="internet.xul";
+		results="internetresults.xul";
+	}
+	else if (thePage == "mailnews")
+	{
+		content="about:blank";
+		results="about:blank";
+	}
+	else if (thePage == "addressbook")
+	{
+		content="about:blank";
+		results="about:blank";
+	}
+	if ((content != "") && (results != ""))
+	{
+		var	contentFrame = document.getElementById("content");
+		if (contentFrame)
+		{
+			contentFrame.setAttribute("src", content);
+		}
+		var	resultsFrame = document.getElementById("results");
+		if (resultsFrame)
+		{
+			resultsFrame.setAttribute("src", results);
+		}
+	}
+	return(true);
+}
