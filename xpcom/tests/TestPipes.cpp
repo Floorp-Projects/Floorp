a00f9dda7ea0ebead625cc85dd7d0075e5202795
warren%netscape.com <warren%netscape.com>
1999-06-15 08:36:27 +0000

    Fixed buffer tests.

--- xpcom/tests/TestPipes.cpp
+++ xpcom/tests/TestPipes.cpp
@@ -169,7 +169,7 @@ main(int argc, char* argv[])
     }
 #endif
     // test for small buffers
-    rv = NS_NewPipe(&in, &out, 12, 24, PR_FALSE, nsnull);
+    rv = NS_NewPipe(&in, &out, 12, 24, PR_TRUE, nsnull);
     if (NS_FAILED(rv)) {
         printf("NewPipe failed\n");
         return -1;
@@ -184,7 +184,7 @@ main(int argc, char* argv[])
     }
 
     // test for large buffers
-    rv = NS_NewPipe(&in, &out, 4096, 4096*16, PR_FALSE, nsnull);
+    rv = NS_NewPipe(&in, &out, 4096, 4096*16, PR_TRUE, nsnull);
     if (NS_FAILED(rv)) {
         printf("NewPipe failed\n");
         return -1;
