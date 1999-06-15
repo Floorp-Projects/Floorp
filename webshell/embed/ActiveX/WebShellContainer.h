189e68b0d8b23b9ef4abc090ab0e2803f60037f8
rpotts%netscape.com <rpotts%netscape.com>
1999-06-15 19:33:29 +0000

    Fix build bustage...  nsIDocumentLoaderObserver interface was changed :-(

--- webshell/embed/ActiveX/WebShellContainer.h
+++ webshell/embed/ActiveX/WebShellContainer.h
@@ -69,7 +69,7 @@ public:
 
 	// nsIDocumentLoaderObserver 
 	NS_IMETHOD OnStartDocumentLoad(nsIDocumentLoader* loader, nsIURL* aURL, const char* aCommand);
-	NS_IMETHOD OnEndDocumentLoad(nsIDocumentLoader* loader, nsIURL *aUrl, PRInt32 aStatus);
+	NS_IMETHOD OnEndDocumentLoad(nsIDocumentLoader* loader, nsIURL *aUrl, PRInt32 aStatus, nsIDocumentLoaderObserver* aObserver);
 	NS_IMETHOD OnStartURLLoad(nsIDocumentLoader* loader, nsIURL* aURL, const char* aContentType, nsIContentViewer* aViewer);
 	NS_IMETHOD OnProgressURLLoad(nsIDocumentLoader* loader, nsIURL* aURL, PRUint32 aProgress, PRUint32 aProgressMax);
 	NS_IMETHOD OnStatusURLLoad(nsIDocumentLoader* loader, nsIURL* aURL, nsString& aMsg);
