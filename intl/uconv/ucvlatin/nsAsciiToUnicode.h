027585440cdb0f1ecd9d5a7623f145c2356287c3
ftang@netscape.com <ftang@netscape.com>
1999-06-15 06:37:33 +0000

    add ASCII converter and unix makefile

---
+++ intl/uconv/ucvlatin/nsAsciiToUnicode.h
@@ -0,0 +1,49 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+#ifndef nsAsciiToUnicode_h___
+#define nsAsciiToUnicode_h___
+
+#include "nsUCvLatinSupport.h"
+
+//----------------------------------------------------------------------
+// Class nsAsciiToUnicode [declaration]
+
+/**
+ * A character set converter from Ascii to Unicode.
+ *
+ * @created         23/Nov/1998
+ * @author  Catalin Rotaru [CATA]
+ */
+class nsAsciiToUnicode : public nsOneByteDecoderSupport
+{
+public:
+
+  /**
+   * Class constructor.
+   */
+  nsAsciiToUnicode();
+
+  /**
+   * Static class constructor.
+   */
+  static nsresult CreateInstance(nsISupports **aResult);
+};
+
+#endif /* nsAsciiToUnicode_h___ */
---
+++ intl/uconv/ucvlatin/nsUnicodeToAscii.cpp
@@ -0,0 +1,59 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+#include "nsUnicodeToAscii.h"
+
+//----------------------------------------------------------------------
+// Global functions and data [declaration]
+
+static PRUint16 g_ufMappingTable[] = {
+  0x0001, 0x0004, 0x0005, 0x0008, 0x0000, 0x0000, 0x007F, 0x0000
+};
+
+static PRInt16 g_ufShiftTable[] =  {
+  0, u1ByteCharset ,
+  ShiftCell(0,0,0,0,0,0,0,0)
+};
+
+//----------------------------------------------------------------------
+// Class nsUnicodeToAscii [implementation]
+
+nsUnicodeToAscii::nsUnicodeToAscii() 
+: nsTableEncoderSupport((uShiftTable*) &g_ufShiftTable, 
+                        (uMappingTable*) &g_ufMappingTable)
+{
+}
+
+nsresult nsUnicodeToAscii::CreateInstance(nsISupports ** aResult) 
+{
+  *aResult = (nsIUnicodeEncoder *) new nsUnicodeToAscii();
+  return (*aResult == NULL)? NS_ERROR_OUT_OF_MEMORY : NS_OK;
+}
+
+//----------------------------------------------------------------------
+// Subclassing of nsTableEncoderSupport class [implementation]
+
+NS_IMETHODIMP nsUnicodeToAscii::GetMaxLength(const PRUnichar * aSrc, 
+                                              PRInt32 aSrcLength,
+                                              PRInt32 * aDestLength)
+{
+  *aDestLength = aSrcLength;
+  return NS_OK_UENC_EXACTLENGTH;
+}
+
