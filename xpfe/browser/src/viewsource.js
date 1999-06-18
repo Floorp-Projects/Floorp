dbcd4010aaabcca925992dd93ccd84c2b1a256b0
law%netscape.com <law%netscape.com>
1999-06-18 00:39:43 +0000

    Fix for bug #8357; approved by chofmann@netscape.com; reviewed by don@netscape.com

--- xpfe/browser/src/viewsource.js
+++ xpfe/browser/src/viewsource.js
@@ -1,13 +1,10 @@
-  var coreName;
-
-
   function StartupViewSource() {
-    // Generate unique name.
-    coreName = "ViewSource." + ( new Date() ).getTime().toString();
+    // Generate unique name (var appCoreName declared in navigator.js).
+    appCoreName = "ViewSource." + ( new Date() ).getTime().toString();
     
     // Create and initialize the browser app core.
     appCore = new BrowserAppCore();
-    appCore.Init( coreName );
+    appCore.Init( appCoreName );
     appCore.setContentWindow(window.frames[0]);
     appCore.setWebShellWindow(window);
     appCore.setToolbarWindow(window);
@@ -16,5 +13,5 @@
     var url = document.getElementById("args").getAttribute("value");
 
     // Load the source (the app core will magically know what to do).
-    XPAppCoresManager.Find( coreName ).loadUrl(url);
+    appCore.loadUrl(url);
   }
