47a49e41f383ca9b8d1759eff3729ae5341261dc
jefft%netscape.com <jefft%netscape.com>
1999-06-21 23:13:59 +0000

    adding new files nsLocalUndoTxn.h nsLocalUndoTxn.cpp

---
+++ mailnews/local/src/nsLocalUndoTxn.cpp
@@ -0,0 +1,176 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998, 1999 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "nsLocalUndoTxn.h"
+
+static NS_DEFINE_CID(kMailboxServiceCID, NS_IMAILBOXSERVICE_IID);
+
+nsLocalMoveCopyMsgTxn::nsLocalMoveCopyMsgTxn()
+{
+	Init(nsnull, nsnull, PR_FALSE);
+}
+
+nsLocalMoveCopyMsgTxn::nsLocalMoveCopyMsgTxn(nsIMsgFolder* srcFolder,
+											nsIMsgFolder* dstFolder,
+											PRBool isMove)
+{
+	Init(srcFolder, dstFolder, isMove);
+}
+
+nsLocalMoveCopyMsgTxn::~nsLocalMoveCopyMsgTxn()
+{
+}
+
+NS_IMPL_ISUPPORTS_INHERITED(nsLocalMoveCopyMsgTxn, nsMsgTxn, nsLocalMoveCopyMsgTxn)
+
+nsresult
+nsLocalMoveCopyMsgTxn::Init(nsIMsgFolder* srcFolder, nsIMsgFolder* dstFolder,
+							PRBool isMove)
+{
+	nsresult rv;
+	rv = SetSrcFolder(srcFolder);
+	rv = SetDstFolder(dstFolder);
+	m_isMove = isMove;
+	return NS_OK;
+}
+
+nsresult
+nsLocalMoveCopyMsgTxn::SetSrcFolder(nsIMsgFolder* srcFolder)
+{
+	nsresult rv = NS_ERROR_NULL_POINTER;
+	if (srcFolder)
+		m_srcFolder = do_QueryInterface(srcFolder, &rv);
+	return rv;
+}
+
+nsresult
+nsLocalMoveCopyMsgTxn::SetDstFolder(nsIMsgFolder* dstFolder)
+{
+	nsresult rv = NS_ERROR_NULL_POINTER;
+	if (dstFolder)
+		m_dstFolder = do_QueryInterface(dstFolder, &rv);
+	return rv;
+}
+
+nsresult
+nsLocalMoveCopyMsgTxn::AddSrcKey(nsMsgKey aKey)
+{
+	m_srcKeyArray.Add(aKey);
+	return NS_OK;
+}
+
+nsresult
+nsLocalMoveCopyMsgTxn::AddDstKey(nsMsgKey aKey)
+{
+	m_dstKeyArray.Add(aKey);
+	return NS_OK;
+}
+
+nsresult
+nsLocalMoveCopyMsgTxn::SetUndoString(nsString *aString)
+{
+	if (!aString) return NS_ERROR_NULL_POINTER;
+	m_undoString = *aString;
+	return NS_OK;
+}
+
+nsresult
+nsLocalMoveCopyMsgTxn::SetRedoString(nsString* aString)
+{
+	if (!aString) return NS_ERROR_NULL_POINTER;
+	m_redoString = *aString;
+	return NS_OK;
+}
+
+NS_IMETHODIMP
+nsLocalMoveCopyMsgTxn::GetUndoString(nsString* aString)
+{
+	if (!aString) return NS_ERROR_NULL_POINTER;
+	*aString = m_undoString;
+	return NS_OK;
+}
+
+NS_IMETHODIMP
+nsLocalMoveCopyMsgTxn::GetRedoString(nsString* aString)
+{
+	if (!aString) return NS_ERROR_NULL_POINTER;
+	*aString = m_redoString;
+	return NS_OK;
+}
+
+NS_IMETHODIMP
+nsLocalMoveCopyMsgTxn::Undo()
+{
+    nsresult rv = NS_ERROR_FAILURE;
+    nsCOMPtr<nsIMsgDatabase> srcDB;
+    nsCOMPtr<nsIMsgDatabase> dstDB;
+    
+    rv = m_srcFolder->GetMsgDatabase(getter_AddRefs(srcDB));
+    if(NS_FAILED(rv)) return rv;
+    rv = m_dstFolder->GetMsgDatabase(getter_AddRefs(dstDB));
+    if (NS_FAILED(rv)) return rv;
+    {
+        PRUint32 count = m_srcKeyArray.GetSize();
+        PRUint32 i;
+        nsCOMPtr<nsIMsgDBHdr> oldHdr;
+        nsCOMPtr<nsIMsgDBHdr> newHdr;
+        for (i=0; i<count; i++)
+        {
+            rv = dstDB->GetMsgHdrForKey(m_dstKeyArray.GetAt(i), 
+                                        getter_AddRefs(oldHdr));
+            if (m_isMove && NS_SUCCEEDED(rv))
+                rv = srcDB->CopyHdrFromExistingHdr(m_srcKeyArray.GetAt(i),
+                                                   oldHdr,
+                                                   getter_AddRefs(newHdr));
+            if (oldHdr)
+                rv = dstDB->DeleteHeader(oldHdr, nsnull, PR_TRUE, PR_TRUE);
+        }
+    }
+    return rv;
+}
+
+NS_IMETHODIMP
+nsLocalMoveCopyMsgTxn::Redo()
+{
+    nsresult rv = NS_ERROR_FAILURE;
+    nsCOMPtr<nsIMsgDatabase> srcDB;
+    nsCOMPtr<nsIMsgDatabase> dstDB;
+    
+    rv = m_srcFolder->GetMsgDatabase(getter_AddRefs(srcDB));
+    if(NS_FAILED(rv)) return rv;
+    rv = m_dstFolder->GetMsgDatabase(getter_AddRefs(dstDB));
+    if (NS_FAILED(rv)) return rv;
+    {
+        PRUint32 count = m_srcKeyArray.GetSize();
+        PRUint32 i;
+        nsCOMPtr<nsIMsgDBHdr> oldHdr;
+        nsCOMPtr<nsIMsgDBHdr> newHdr;
+        for (i=0; i<count; i++)
+        {
+            rv = srcDB->GetMsgHdrForKey(m_srcKeyArray.GetAt(i), 
+                                        getter_AddRefs(oldHdr));
+            if (NS_SUCCEEDED(rv))
+                rv = dstDB->CopyHdrFromExistingHdr(m_dstKeyArray.GetAt(i),
+                                                   oldHdr,
+                                                   getter_AddRefs(newHdr));
+            if (m_isMove && oldHdr)
+                rv = srcDB->DeleteHeader(oldHdr, nsnull, PR_TRUE, PR_TRUE);
+        }
+    }
+    return rv;
+}
---
+++ mailnews/local/src/nsLocalUndoTxn.h
@@ -0,0 +1,74 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998, 1999 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsLocalUndoTxn_h__
+#define nsLocalUndoTxn_h__
+
+#include "msgCore.h"
+#include "nsIMsgFolder.h"
+#include "nsMailboxService.h"
+#include "nsMsgTxn.h"
+#include "nsMsgKeyArray.h"
+#include "nsCOMPtr.h"
+
+#define NS_LOCALMOVECOPYMSGTXN_IID \
+{ /* 874363b4-242e-11d3-afad-001083002da8 */ \
+	0x874363b4, 0x242e, 0x11d3, \
+	{ 0xaf, 0xad, 0x00, 0x10, 0x83, 0x00, 0x2d, 0xa8 } }
+
+class nsLocalMoveCopyMsgTxn : public nsMsgTxn
+{
+public:
+    NS_DEFINE_STATIC_IID_ACCESSOR(NS_LOCALMOVECOPYMSGTXN_IID)
+
+	nsLocalMoveCopyMsgTxn();
+    nsLocalMoveCopyMsgTxn(nsIMsgFolder *srcFolder,
+						  nsIMsgFolder *dstFolder,
+						  PRBool isMove);
+    virtual ~nsLocalMoveCopyMsgTxn();
+
+    NS_DECL_ISUPPORTS_INHERITED
+
+    // overloading nsITransaction methods
+	NS_IMETHOD Undo(void);
+    NS_IMETHOD Redo(void);
+    NS_IMETHOD GetUndoString(nsString *aString);
+    NS_IMETHOD GetRedoString(nsString *aString);
+
+    // helper
+    nsresult SetUndoString(nsString *aString);
+    nsresult SetRedoString(nsString *aString);
+    nsresult AddSrcKey(nsMsgKey aKey);
+    nsresult AddDstKey(nsMsgKey aKey);
+    nsresult SetSrcFolder(nsIMsgFolder* srcFolder);
+    nsresult SetDstFolder(nsIMsgFolder* dstFolder);
+    nsresult Init(nsIMsgFolder* srcFolder,
+				  nsIMsgFolder* dstFolder,
+				  PRBool isMove);
+  
+private:
+    nsCOMPtr<nsIMsgFolder> m_srcFolder;
+    nsMsgKeyArray m_srcKeyArray;
+    nsCOMPtr<nsIMsgFolder> m_dstFolder;
+    nsMsgKeyArray m_dstKeyArray;
+    nsString m_undoString;
+    nsString m_redoString;
+    PRBool m_isMove;
+};
+
+#endif
