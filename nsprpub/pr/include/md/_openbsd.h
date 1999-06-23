6d127c289e798bb14ee930f5af4184aaf5283c20
srinivas@netscape.com <srinivas@netscape.com>
1999-06-23 19:43:41 +0000

    Checkin for briano@netscape.com. Bugzilla #8600.

--- nsprpub/pr/include/md/_openbsd.h
+++ nsprpub/pr/include/md/_openbsd.h
@@ -21,8 +21,8 @@
 
 #include <sys/syscall.h>
 
-#define PR_LINKER_ARCH "openbsd"
-#define _PR_SI_SYSNAME  "OpenBSD"
+#define PR_LINKER_ARCH	"openbsd"
+#define _PR_SI_SYSNAME  "OPENBSD"
 #if defined(__i386__)
 #define _PR_SI_ARCHITECTURE "x86"
 #elif defined(__alpha__)
@@ -37,24 +37,26 @@
 #define _PR_SI_ARCHITECTURE "arm32"
 #endif
 
-#define PR_DLL_SUFFIX          ".so.1.0"
+#define PR_DLL_SUFFIX		".so.1.0"
 
 #define _PR_VMBASE              0x30000000
-#define _PR_STACK_VMBASE       0x50000000
-#define _MD_DEFAULT_STACK_SIZE 65536L
+#define _PR_STACK_VMBASE	0x50000000
+#define _MD_DEFAULT_STACK_SIZE	65536L
 #define _MD_MMAP_FLAGS          MAP_PRIVATE
 
 #undef  HAVE_STACK_GROWING_UP
 #define HAVE_DLL
 #define USE_DLFCN
 #define _PR_HAVE_SOCKADDR_LEN
+#define _PR_NO_LARGE_FILES
+#define _PR_STAT_HAS_ST_ATIMESPEC
 
 #define USE_SETJMP
 
 #ifndef _PR_PTHREADS
 #include <setjmp.h>
 
-#define PR_CONTEXT_TYPE        sigjmp_buf
+#define PR_CONTEXT_TYPE	sigjmp_buf
 
 #define CONTEXT(_th) ((_th)->md.context)
 
@@ -64,7 +66,7 @@
 #define JB_SP_INDEX 34
 #elif defined(__arm32__)
 /*
- * On the arm32, the jmpbuf regs underwent a namechange after NetBSD 1.3
+ * On the arm32, the jmpbuf regs underwent a name change after OpenBSD 1.3.
  */
 #ifdef JMPBUF_REG_R13
 #define JB_SP_INDEX JMPBUF_REG_R13
@@ -76,7 +78,7 @@
 #endif
 #define _MD_GET_SP(_th)    (_th)->md.context[JB_SP_INDEX]
 
-#define PR_NUM_GCREGS  _JBLEN
+#define PR_NUM_GCREGS	_JBLEN
 
 /*
 ** Initialize a thread context to run "_main()" when started
@@ -134,6 +136,42 @@ struct _MDSegment {
     PRInt8 notused;
 };
 
+/*
+ * md-specific cpu structure field
+ */
+#define _PR_MD_MAX_OSFD FD_SETSIZE
+
+struct _MDCPU_Unix {
+    PRCList ioQ;
+    PRUint32 ioq_timeout;
+    PRInt32 ioq_max_osfd;
+    PRInt32 ioq_osfd_cnt;
+#ifndef _PR_USE_POLL
+    fd_set fd_read_set, fd_write_set, fd_exception_set;
+    PRInt16 fd_read_cnt[_PR_MD_MAX_OSFD],fd_write_cnt[_PR_MD_MAX_OSFD],
+				fd_exception_cnt[_PR_MD_MAX_OSFD];
+#else
+	struct pollfd *ioq_pollfds;
+	int ioq_pollfds_size;
+#endif	/* _PR_USE_POLL */
+};
+
+#define _PR_IOQ(_cpu)			((_cpu)->md.md_unix.ioQ)
+#define _PR_ADD_TO_IOQ(_pq, _cpu) PR_APPEND_LINK(&_pq.links, &_PR_IOQ(_cpu))
+#define _PR_FD_READ_SET(_cpu)		((_cpu)->md.md_unix.fd_read_set)
+#define _PR_FD_READ_CNT(_cpu)		((_cpu)->md.md_unix.fd_read_cnt)
+#define _PR_FD_WRITE_SET(_cpu)		((_cpu)->md.md_unix.fd_write_set)
+#define _PR_FD_WRITE_CNT(_cpu)		((_cpu)->md.md_unix.fd_write_cnt)
+#define _PR_FD_EXCEPTION_SET(_cpu)	((_cpu)->md.md_unix.fd_exception_set)
+#define _PR_FD_EXCEPTION_CNT(_cpu)	((_cpu)->md.md_unix.fd_exception_cnt)
+#define _PR_IOQ_TIMEOUT(_cpu)		((_cpu)->md.md_unix.ioq_timeout)
+#define _PR_IOQ_MAX_OSFD(_cpu)		((_cpu)->md.md_unix.ioq_max_osfd)
+#define _PR_IOQ_OSFD_CNT(_cpu)		((_cpu)->md.md_unix.ioq_osfd_cnt)
+#define _PR_IOQ_POLLFDS(_cpu)		((_cpu)->md.md_unix.ioq_pollfds)
+#define _PR_IOQ_POLLFDS_SIZE(_cpu)	((_cpu)->md.md_unix.ioq_pollfds_size)
+
+#define _PR_IOQ_MIN_POLLFDS_SIZE(_cpu)	32
+
 struct _MDCPU {
     struct _MDCPU_Unix md_unix;
 };
@@ -157,7 +195,7 @@ struct _MDCPU {
 #endif /* ! _PR_PTHREADS */
 
 #define _MD_EARLY_INIT                  _MD_EarlyInit
-#define _MD_FINAL_INIT                 _PR_UnixInit
+#define _MD_FINAL_INIT			_PR_UnixInit
 #define _MD_GET_INTERVAL                  _PR_UNIX_GetInterval
 #define _MD_INTERVAL_PER_SEC              _PR_UNIX_TicksPerSecond
 
@@ -167,5 +205,9 @@ struct _MDCPU {
  */
 #define _MD_SELECT(nfds,r,w,e,tv) syscall(SYS_select,nfds,r,w,e,tv)
 #define _MD_POLL(fds,nfds,timeout) syscall(SYS_poll,fds,nfds,timeout)
+
+#if OpenBSD1_3 == 1L
+typedef unsigned int nfds_t;
+#endif
 
 #endif /* nspr_openbsd_defs_h___ */
