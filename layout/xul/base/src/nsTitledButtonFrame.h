45cd09c53094d85678477b73def9a68459166a8b
evaughan%netscape.com <evaughan%netscape.com>
1999-06-23 01:13:58 +0000

    Images will now change on hover on titledbuttons.

--- layout/xul/base/src/nsTitledButtonFrame.h
+++ layout/xul/base/src/nsTitledButtonFrame.h
@@ -66,6 +66,7 @@ public:
 
 
   virtual void UpdateAttributes(nsIPresContext&  aPresContext);
+  virtual void UpdateImage(nsIPresContext&  aPresContext);
 
   // nsIHTMLReflow overrides
   NS_IMETHOD Reflow(nsIPresContext&          aPresContext,
