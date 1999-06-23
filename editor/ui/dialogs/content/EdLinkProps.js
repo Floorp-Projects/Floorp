7e8b37648ce4024cacfb4a55b2dbd507aad28755
cmanske%netscape.com <cmanske%netscape.com>
1999-06-23 21:24:49 +0000

    Tweaked size of HLine dialog and removed 'dump(element)' that gives and error, preventing insert link from working

--- editor/ui/dialogs/content/EdLinkProps.js
+++ editor/ui/dialogs/content/EdLinkProps.js
@@ -137,7 +137,7 @@ function onOK()
 
   // Set the HREF directly on the editor document's anchor node
   //  or on the newly-created node if insertNew is true
-  dump(anchorElement + "\n");
+//  dump(anchorElement + "\n");
   anchorElement.setAttribute("href",dialog.hrefInput.value);
 
   // Get text to use for a new link
