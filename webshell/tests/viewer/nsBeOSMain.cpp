4d8cd637a98408863ccf9ba4bfa53fe54212159c
mcafee@netscape.com <mcafee@netscape.com>
1999-06-25 11:08:07 +0000

    BeOS landing; thanks to Duncan Wilcox (duncan@be.com) for these changes.

---
+++ webshell/tests/viewer/nsBeOSMain.cpp
@@ -0,0 +1,100 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+#include "nsViewerApp.h"
+#include "nsBrowserWindow.h"
+//#include "JSConsole.h"
+#include "nsViewerApp.h"
+#include "nsBrowserWindow.h"
+#include "nsIImageManager.h"
+#include <stdlib.h>
+#include "plevent.h"
+
+//#include "nsTimer.h"
+//#include "nsITimer.h"
+
+nsNativeViewerApp::nsNativeViewerApp()
+{
+}
+
+nsNativeViewerApp::~nsNativeViewerApp()
+{
+}
+
+int
+nsNativeViewerApp::Run() 
+{
+  OpenWindow();
+
+	if(mAppShell)
+		mAppShell->Run();
+
+	return NS_OK;
+}
+
+//----------------------------------------------------------------------
+
+nsNativeBrowserWindow::nsNativeBrowserWindow()
+{
+}
+
+nsNativeBrowserWindow::~nsNativeBrowserWindow()
+{
+}
+
+nsresult
+nsNativeBrowserWindow::InitNativeWindow()
+{
+	// override to do something special with platform native windows
+  return NS_OK;
+}
+
+nsresult
+nsNativeBrowserWindow::CreateMenuBar(PRInt32 aWidth)
+{
+	printf("nsNativeBrowserWindow:: CreateMenuBar not implemented\n");
+#if 0
+  HMENU menu = ::LoadMenu(gInstance, "Viewer");
+  HWND hwnd = (HWND)mWindow->GetNativeData(NS_NATIVE_WIDGET);
+  ::SetMenu(hwnd, menu);
+#endif
+
+  return NS_OK;
+}
+
+nsEventStatus
+nsNativeBrowserWindow::DispatchMenuItem(PRInt32 aID)
+{
+  // Dispatch windows-only menu code goes here
+
+  // Dispatch xp menu items
+  return nsBrowserWindow::DispatchMenuItem(aID);
+}
+
+//----------------------------------------------------------------------
+
+int main(int argc, char **argv)
+{
+  nsViewerApp* app = new nsNativeViewerApp();
+  NS_ADDREF(app);
+  app->Initialize(argc, argv);
+  app->Run();
+  NS_RELEASE(app);
+
+  return 0;
+}
