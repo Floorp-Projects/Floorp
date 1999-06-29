a022bcbbcfc2f6900a87a16bedd2a980f47ac2c0
jband%netscape.com <jband%netscape.com>
1999-06-29 21:34:50 +0000

    doing my part for bug 8227

--- js/src/xpconnect/tests/components/xpctest_echo.cpp
+++ js/src/xpconnect/tests/components/xpctest_echo.cpp
@@ -288,8 +288,18 @@ xpctest::ConstructEcho(nsISupports *aOuter, REFNSIID aIID, void **aResult)
     nsresult rv;
     NS_ASSERTION(aOuter == nsnull, "no aggregation");
     xpctestEcho* obj = new xpctestEcho();
-    rv = obj->QueryInterface(aIID, aResult);
-    NS_ASSERTION(NS_SUCCEEDED(rv), "unable to find correct interface");
-    NS_RELEASE(obj);
+
+    if(obj)
+    {
+        rv = obj->QueryInterface(aIID, aResult);
+        NS_ASSERTION(NS_SUCCEEDED(rv), "unable to find correct interface");
+        NS_RELEASE(obj);
+    }
+    else
+    {
+        *aResult = nsnull;
+        rv = NS_ERROR_OUT_OF_MEMORY;
+    }
+
     return rv;
 }        
