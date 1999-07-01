b2f17b856e5de70232f2d9e8b5f622703022233d
nhotta%netscape.com <nhotta%netscape.com>
1999-07-01 18:29:35 +0000

    Bug fix 8899, changed to use nsIStringCharsetDetector.

--- mailnews/mime/src/comi18n.cpp
+++ mailnews/mime/src/comi18n.cpp
@@ -30,6 +30,7 @@
 #define NS_IMPL_IDS
 #include "nsIServiceManager.h"
 #include "nsICharsetConverterManager.h"
+#include "nsIStringCharsetDetector.h"
 
 // BEGIN EXTERNAL DEPENDANCY
 
@@ -1258,184 +1259,6 @@ static PRInt32 INTL_ConvertFromUnicode(const char* to_charset, const void* uniBu
   return NS_SUCCEEDED(res) ? 0 : -1;
 }
 ////////////////////////////////////////////////////////////////////////////////
-#define USE_NONXPCOM_AUTODETECTION
-// TODO: This part should be entirly replaced by XPCOM version of charset detection.
-#ifdef USE_NONXPCOM_AUTODETECTION
-class nsCharsetDetect {
-public:
-  PRBool AutoCharsetDetectionAvailable(const nsString& aCharset);
-  nsresult AutoCharsetDetectBuffer(const char* aBuffer, const PRInt32 aLen, PRBool& bDetected,
-                                   const nsString& aCharsetIn, nsString& aCharsetDetected);
-};
-					/* values for EUC shift chars	*/
-#define SS2		0x8E		/* Single Shift 2		*/
-#define SS3		0x8F		/* Single Shift 3		*/
-#define IsRoman(c)			((c) < 0x80)
-#define IsSJIS2ndByte(c)	(((c) > 0x3F) && ((c) < 0xFD))
-#define IsLoSJIS2ndByte(c)	(((c) > 0x3F) && ((c) < 0xA1))
-#define IsHiSJIS2ndByte(c)	(((c) > 0xA0) && ((c) < 0xFD))
-#define IsEUCJPKana(b1)		(((b1) > 0xA0) && ((b1) < 0xE0))
-#define IsEUCJPKanji(b1or2)	(((b1or2) > 0xA0) && ((b1or2) < 0xFF))
-
-#define	YES		1
-#define NO		0
-#define	MAYBE	-1
-
-static int
-isSJIS(const unsigned char *cp, PRInt32 len)
-{
-	while (len) {
-		if (IsRoman(*cp)) {
-			cp++, len--;
-		} else if (*cp == 0x80) {		/* illegal SJIS 1st byte			*/
-			return NO;
-		} else if ((*cp < 0xA0)) {		/* byte 1 of 2byte SJIS 1st range	*/
-			if (len > 1) {
-				if (IsSJIS2ndByte(cp[1])) {
-					if ((*cp != 0x8E && *cp != 0x8F) || (*(cp+1) <= 0xA0))
-						return YES;
-					cp += 2, len -= 2;	/* valid 2 byte SJIS				*/
-				} else {
-					return NO;			/* invalid SJIS	2nd byte			*/
-				}
-			} else
-				break;						/* buffer ended w/1of2 byte SJIS */
-		} else if (*cp == 0xA0) {			/* illegal EUCJP byte		*/
-#if ALLOW_NBSP
-			cp++, len--; /* allow nbsp */
-#endif
-		} else if (*cp < 0xE0) {		/* SJIS half-width kana				*/
-			cp++, len--;
-		} else if (*cp < 0xF0) {		/* byte 1 of 2byte SJIS	 2nd range	*/
-			if (len > 1) {
-				if (IsSJIS2ndByte(cp[1])) {
-					cp += 2, len -= 2;	/* valid 2 byte SJIS				*/
-				} else {
-					return NO;			/* invalid SJIS						*/
-				}
-			} else
-				break;					/* buffer ended w/1of2 byte SJIS	*/
-		} else {
-			return NO;					/* invalid SJIS 1st byte			*/
-		}
-	}
-	return MAYBE;						/* No illegal SJIS values found		*/
-}
-
-static int
-isEUCJP(const unsigned char *cp, PRInt32 len)
-{
-	while (len) {
-		if (IsRoman(*cp)) {			/* Roman						*/
-			cp++, len--;
-		} else if (*cp == SS2) {		/* EUCJP JIS201 half-width kana */
-			if (len > 1) {
-				if (IsEUCJPKana(cp[1]))
-					cp += 2, len -= 2;		/* valid half-width kana */
-				else
-					return NO;				/* invalid 2of3 byte EUC */ 
-			} else
-				break;						/* buffer ended w/1of2 byte EUC	*/
-		} else if (*cp == SS3) {			/* EUCJP JIS212					*/
-			 if (len > 1) {
-			 	if (IsEUCJPKanji(cp[1])) {
-			 		if (len > 2) {
-				 		if (IsEUCJPKanji(cp[2]))
-							cp += 2, len -= 2;	/* valid 3 byte EUCJP		*/
-						else
-							return NO;		/* invalid 3of3 byte EUCJP	*/
-					} else
-						break;				/* buffer ended w/2of3 byte EUCJP */
-				} else
-					return NO;				/* invalid 2of3 byte EUCJP	*/
-			} else
-				break;						/* buffer ended w/1of3 byte EUCJP */
-		} else if (*cp == 0xA0) {			/* illegal EUCJP byte		*/
-#if ALLOW_NBSP
-			cp++, len--; /* allow nbsp */
-#else
-			return NO;
-#endif
-		} else if (*cp < 0xF0) {		/* EUCJP JIS208 (overlaps SJIS)		*/
-			if (len > 1) {
-			 	if (IsEUCJPKanji(cp[1]))
-					cp += 2, len -= 2;		/* valid 2 byte EUCJP		*/
-				else
-					return NO;				/* invalid 2of2 byte EUCJP	*/
-			} else
-				break;						/* buffer ended w/1of2 byte EUCJP */
-		} else if (*cp < 0xFF) {		/* EUCJP JIS208 only:			*/
-			if (len > 1) {
-			 	if (IsEUCJPKanji(cp[1]))
-					return YES;			/* valid 2 byte EUCJP, invalid SJIS	*/
-				else
-					return NO;				/* invalid 2of2 byte EUCJP	*/
-			} else
-				break;						/* buffer ended w/1of2 byte EUCJP */
-		} else {
-			return NO;					/* invalid EUCJP 1st byte: 0xFF	*/
-		}
-	}
-	return MAYBE;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-PRBool nsCharsetDetect::AutoCharsetDetectionAvailable(const nsString& aCharset)
-{
-  if (!aCharset.EqualsIgnoreCase("Shift_JIS") &&
-      !aCharset.EqualsIgnoreCase("EUC-JP") &&
-      !aCharset.EqualsIgnoreCase("ISO-2022-JP")) {
-    return PR_FALSE;
-  }
-  return PR_TRUE;
-}
-
-nsresult nsCharsetDetect::AutoCharsetDetectBuffer(const char* aBuffer, const PRInt32 aLen, PRBool& bDetected,
-                                                  const nsString& aCharsetIn, nsString& aCharsetDetected)
-{
-  PRBool doAutoDetect = PR_FALSE;
-
-  if (!AutoCharsetDetectionAvailable(aCharsetIn)) {
-    bDetected = PR_FALSE;
-    return NS_OK;
-  }
-  // check 7 bit only or ESC
-  for (int i = 0; i < aLen; i++) {
-    if ((unsigned char) aBuffer[i] > 127 || aBuffer[i] == 0x1B) {
-      if (aBuffer[i] == 0x1B) {
-        aCharsetDetected.SetString("ISO-2022-JP");
-      }
-      doAutoDetect = PR_TRUE;
-      break;
-    }
-  }
-
-  if (!doAutoDetect) {
-    aCharsetDetected.SetString("ISO-8859-1");
-  }
-  else {
-   if (!aCharsetDetected.EqualsIgnoreCase("ISO-2022-JP")) {
-    // use old japanese auto detect code
-      int euc, sjis;
-      aCharsetDetected.SetString("ISO-8859-1");
-      euc = isEUCJP((unsigned char *) aBuffer, aLen);
-      if (YES == euc || MAYBE == euc) {
-        aCharsetDetected.SetString("EUC-JP");
-      }
-      else {
-        sjis = isSJIS((unsigned char *) aBuffer, aLen);
-        if (YES == sjis || MAYBE == sjis) {
-          aCharsetDetected.SetString("Shift_JIS");
-        }
-      }
-    }
-  }
-  bDetected = PR_TRUE;
-
-  return NS_OK;
-}
-#endif//USE_NONXPCOM_AUTODETECTION
 
 class MimeCharsetConverterClass {
 public:
@@ -1467,6 +1290,7 @@ private:
   PRBool mAutoDetect;                   // true if apply auto detection
   nsString mInputCharset;               // input charset for auto detection hint as well as need conversion check
   nsString mOutputCharset;              // output charset for need conversion check
+  nsIStringCharsetDetector *mDetector;  // charset detector
 };
 
 MimeCharsetConverterClass::MimeCharsetConverterClass()
@@ -1477,6 +1301,7 @@ MimeCharsetConverterClass::MimeCharsetConverterClass()
   mMaxNumCharsDetect = -1;
   mNumChars = 0;
   mAutoDetect = PR_FALSE;
+  mDetector = NULL;
 }
 
 MimeCharsetConverterClass::~MimeCharsetConverterClass()
@@ -1484,6 +1309,7 @@ MimeCharsetConverterClass::~MimeCharsetConverterClass()
   NS_IF_RELEASE(mDecoder);
   NS_IF_RELEASE(mEncoder);
   NS_IF_RELEASE(mDecoderDetected);
+  NS_IF_RELEASE(mDetector);
 }
 
 PRInt32 MimeCharsetConverterClass::Initialize(const char* from_charset, const char* to_charset, 
@@ -1498,11 +1324,21 @@ PRInt32 MimeCharsetConverterClass::Initialize(const char* from_charset, const ch
   mAutoDetect = autoDetect;
   mMaxNumCharsDetect = maxNumCharsDetect;
 
-  // Check if auto detection is available for the input charset
-  nsCharsetDetect aCharsetDetect;
-  if (mAutoDetect && !aCharsetDetect.AutoCharsetDetectionAvailable(from_charset)) {
-    mAutoDetect = PR_FALSE;
+  //TODO: For now, restrict to Japanese only
+  if (mAutoDetect &&
+      (mInputCharset.EqualsIgnoreCase("ISO-2022-JP") ||
+      mInputCharset.EqualsIgnoreCase("EUC-JP") ||
+      mInputCharset.EqualsIgnoreCase("Shift_JIS"))) {
+    char aComponent[128];
+    PL_strcpy(aComponent, NS_STRCDETECTOR_PROGID_BASE);
+    PL_strcat(aComponent, "japsm"); //TODO: get this from pref
+    res = nsComponentManager::CreateInstance(aComponent, nsnull, 
+                                             nsIStringCharsetDetector::GetIID(), (void**)&mDetector);
+    if (NS_FAILED(res)) {
+      mDetector = NULL;   // no charset detector is available
+    }
   }
+
   // No need to do the conversion then do not create converters. 
   if (!mAutoDetect && !NeedCharsetConversion(mInputCharset, mOutputCharset)) {
     return 0;
@@ -1554,12 +1390,14 @@ PRInt32 MimeCharsetConverterClass::Convert(const char* inBuffer, const PRInt32 i
   nsIUnicodeEncoder* encoder = mEncoder;
 
   // try auto detection for this string
-  if (mAutoDetect && (mMaxNumCharsDetect == -1 || mMaxNumCharsDetect > mNumChars)) {
-    nsCharsetDetect aCharsetDetect;   //TODO: replace this by XPCOM interface
-    PRBool bDetected;
+  if (mAutoDetect && NULL != mDetector && (mMaxNumCharsDetect == -1 || mMaxNumCharsDetect > mNumChars)) {
     nsString aCharsetDetected;
-    res = aCharsetDetect.AutoCharsetDetectBuffer(inBuffer, inLength, bDetected, mInputCharset, aCharsetDetected);
-    if (NS_SUCCEEDED(res) && bDetected) {
+    const char* oCharset;
+    nsDetectionConfident oConfident;
+    res = mDetector->DoIt(inBuffer, inLength, &oCharset, oConfident);
+    if (NS_SUCCEEDED(res) && (eBestAnswer == oConfident || eSureAnswer == oConfident)) {
+      aCharsetDetected.SetString(oCharset);
+
       // Check if need a conversion.
       if (!NeedCharsetConversion(aCharsetDetected, mOutputCharset)) {
         *outBuffer = (char *) PR_Malloc(inLength+1);
