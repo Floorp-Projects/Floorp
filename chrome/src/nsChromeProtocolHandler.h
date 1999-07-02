2c1893b5831dbe9fbabc14ebb99f51f189f2bce3
warren@netscape.com <warren@netscape.com>
1999-07-02 04:15:43 +0000

    Added chrome protocol handler

---
+++ chrome/src/nsChromeProtocolHandler.h
@@ -0,0 +1,61 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsChromeProtocolHandler_h___
+#define nsChromeProtocolHandler_h___
+
+#include "nsIProtocolHandler.h"
+
+#define NS_CHROMEPROTOCOLHANDLER_CID                 \
+{ /* 61ba33c0-3031-11d3-8cd0-0060b0fc14a3 */         \
+    0x61ba33c0,                                      \
+    0x3031,                                          \
+    0x11d3,                                          \
+    {0x8c, 0xd0, 0x00, 0x60, 0xb0, 0xfc, 0x14, 0xa3} \
+}
+
+class nsChromeProtocolHandler : public nsIProtocolHandler
+{
+public:
+    NS_DECL_ISUPPORTS
+
+    // nsIProtocolHandler methods:
+    NS_IMETHOD GetScheme(char * *aScheme);
+    NS_IMETHOD GetDefaultPort(PRInt32 *aDefaultPort);
+    NS_IMETHOD MakeAbsolute(const char *aRelativeSpec, nsIURI *aBaseURI,
+                            char **_retval);
+    NS_IMETHOD NewURI(const char *aSpec, nsIURI *aBaseURI,
+                      nsIURI **_retval);
+    NS_IMETHOD NewChannel(const char* verb, nsIURI* url,
+                          nsIEventSinkGetter *eventSinkGetter,
+                          nsIEventQueue *eventQueue,
+                          nsIChannel **_retval);
+
+    // nsChromeProtocolHandler methods:
+    nsChromeProtocolHandler();
+    virtual ~nsChromeProtocolHandler();
+
+    static NS_METHOD
+    Create(nsISupports *aOuter, REFNSIID aIID, void **aResult);
+
+    nsresult Init();
+
+protected:
+};
+
+#endif /* nsChromeProtocolHandler_h___ */
