781c1f3fe4ddc3aa396d5b7639c316a55ffd0ec1
edwin%woudt.nl <edwin%woudt.nl>
1999-07-02 14:20:06 +0000

    Including the MozillaZine animation contest winner from Klaus Malorny.

--- grendel/ui/GeneralFrame.java
+++ grendel/ui/GeneralFrame.java
@@ -142,7 +142,7 @@ public class GeneralFrame extends JFrame
     fPanel = getContentPane();
 
     fAnimation = new Animation();
-    fAnimation.setImageTemplate("/grendel/ui/images/animation/GrendelAnim{0,number,00}.gif",
+    fAnimation.setImageTemplate("/grendel/ui/images/animation/Contest{0,number,00}.gif",
                                 30);
 
     fToolBarPanel = new CollapsiblePanel(true);
