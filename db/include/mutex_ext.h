b638dace7052fbcde93ba41e0ec8648c94e1c730
brendan@mozilla.org <brendan@mozilla.org>
1999-07-07 08:02:17 +0000

    Removing mozilla/db due to incompatible license.

--- db/include/mutex_ext.h
+++
@@ -1,7 +0,0 @@
-/* DO NOT EDIT: automatically built by dist/distrib. */
-#ifndef _mutex_ext_h_
-#define _mutex_ext_h_
-int __db_mutex_init __P((db_mutex_t *, u_int32_t));
-int __db_mutex_lock __P((db_mutex_t *, int));
-int __db_mutex_unlock __P((db_mutex_t *, int));
-#endif /* _mutex_ext_h_ */
