a16588ba99d9d7e76d8df35a36ef0ece72530bf9
edburns%acm.org <edburns%acm.org>
1999-07-08 20:25:53 +0000

    Removed non ifdef'd debugging statements.

--- modules/plugin/base/src/ns4xPluginInstance.cpp
+++ modules/plugin/base/src/ns4xPluginInstance.cpp
@@ -94,7 +94,6 @@ NS_IMETHODIMP
 ns4xPluginStreamListener::OnStartBinding(nsIPluginStreamInfo* pluginInfo)
 {
 
-    printf("debug: edburns: ns4xPluginStreamListener::OnStartBinding()\n");
 	NPP npp;
 	const NPPluginFuncs *callbacks;
 	PRBool seekable;
@@ -114,9 +113,7 @@ ns4xPluginStreamListener::OnStartBinding(nsIPluginStreamInfo* pluginInfo)
     mInst->GetCallbacks(&callbacks);
     mInst->GetNPP(&npp);
 
-    printf("debug: edburns: ns4xPluginStreamListener::OnStartBinding() done GetNPP()\n");
 #if !TARGET_CARBON
-    printf("debug: edburns: ns4xPluginStreamListener::OnStartBinding() about to call CallNPP_NewStreamProc: callbacks->newstream: %p\n", callbacks->newstream);
 // pinkerton
 // relies on routine descriptors, not present in carbon. We need to fix this.
     error = CallNPP_NewStreamProc(callbacks->newstream,
@@ -142,7 +139,6 @@ ns4xPluginStreamListener::OnStartBinding(nsIPluginStreamInfo* pluginInfo)
 		default: return NS_ERROR_FAILURE;
 	}
 
-    printf("debug: edburns: ns4xPluginStreamListener::OnStartBinding() done switch\n");
 	
     return NS_OK;
 }
