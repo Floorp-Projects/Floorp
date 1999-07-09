e15c8cb3adafafe864683f1b7f6ab32a0063c2c0
pavlov@pavlov.net <pavlov@pavlov.net>
1999-07-09 01:01:23 +0000

    fix bug 9446 and 9464 complete resizing work needed to turn intrinsic sizing on under linux (bugs 9443, 4439) except for one other problem not related to this code.  also fixes 9461

--- widget/src/gtk/nsWindow.h
+++ widget/src/gtk/nsWindow.h
@@ -66,18 +66,22 @@ public:
     NS_IMETHOD           Show(PRBool aShow);
     NS_IMETHOD           ShowMenuBar(PRBool aShow);
     NS_IMETHOD           Move(PRUint32 aX, PRUint32 aY);
+    NS_IMETHOD           IsMenuBarVisible(PRBool *aVisible);
+
     NS_IMETHOD           Resize(PRUint32 aWidth, PRUint32 aHeight, PRBool aRepaint);
     NS_IMETHOD           Resize(PRUint32 aX, PRUint32 aY, PRUint32 aWidth,
                                 PRUint32 aHeight, PRBool aRepaint);
-    NS_IMETHOD           IsMenuBarVisible(PRBool *aVisible);
+
+    NS_IMETHOD           Invalidate(PRBool aIsSynchronous);
+    NS_IMETHOD           Invalidate(const nsRect &aRect, PRBool aIsSynchronous);
 
     NS_IMETHOD           SetTooltips(PRUint32 aNumberOfTips,nsRect* aTooltipAreas[]);
     NS_IMETHOD           UpdateTooltips(nsRect* aNewTips[]);
     NS_IMETHOD           RemoveTooltips();
 
-    NS_IMETHOD            BeginResizingChildren(void);
-    NS_IMETHOD            EndResizingChildren(void);
-    NS_IMETHOD            Destroy(void);
+    NS_IMETHOD           BeginResizingChildren(void);
+    NS_IMETHOD           EndResizingChildren(void);
+    NS_IMETHOD           Destroy(void);
 
 
     virtual PRBool IsChild() const;
