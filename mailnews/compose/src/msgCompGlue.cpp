fd1e447bc8a95ea200356177df502af625c0faad
rhp%netscape.com <rhp%netscape.com>
1999-07-13 19:38:45 +0000

    cleanup

--- mailnews/compose/src/msgCompGlue.cpp
+++
@@ -1,8 +0,0 @@
-//
-// All the definition here after are temporary...we are close to being done with 
-// this file
-//
-
-README_NOW:
-THIS FILE IS NO LONGER NEEDED BY THE BUILD. IF THIS IS BEING BUILT IN YOUR
-TREE, THEN YOUR MAKEFILES/PROJECT FILES ARE NOT CORRECT.
