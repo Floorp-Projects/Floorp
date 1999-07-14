acac84a1f26cf14945ee74eadd7ae1c19481e74c
nhotta@netscape.com <nhotta@netscape.com>
1999-07-14 16:42:36 +0000

    Initial check in (not part of the main build).

---
+++ intl/chardet/src/windows/nsNativeCharDetDll.cpp
@@ -0,0 +1,143 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#define NS_IMPL_IDS
+#include "nsICharsetAlias.h"
+#undef NS_IMPL_IDS
+
+#include "pratom.h"
+#include "nsNativeCharDetDll.h"
+#include "nsISupports.h"
+#include "nsIComponentManager.h"
+#include "nsIFactory.h"
+#include "nsIServiceManager.h"
+#include "nsCOMPtr.h"
+#include "nsICharsetDetector.h"
+#include "nsIStringCharsetDetector.h"
+#include "nsNativeDetectors.h"
+
+
+static NS_DEFINE_CID(kJANativeDetectorCID,         NS_JA_NATIVE_DETECTOR_CID);
+static NS_DEFINE_CID(kJANativeStringDetectorCID,   NS_JA_NATIVE_STRING_DETECTOR_CID);
+static NS_DEFINE_CID(kKONativeDetectorCID,         NS_KO_NATIVE_DETECTOR_CID);
+static NS_DEFINE_CID(kKONativeStringDetectorCID,   NS_KO_NATIVE_STRING_DETECTOR_CID);
+static NS_DEFINE_CID(kComponentManagerCID, NS_COMPONENTMANAGER_CID);
+static NS_DEFINE_IID(kFactoryIID, NS_IFACTORY_IID);
+
+PRInt32 g_InstanceCount = 0;
+PRInt32 g_LockCount = 0;
+
+
+extern "C" NS_EXPORT nsresult NSGetFactory(nsISupports* aServMgr,
+                                           const nsCID &aClass,
+                                           const char *aClassName,
+                                           const char *aProgID,
+                                           nsIFactory **aFactory)
+{
+  if (aFactory == NULL) {
+    return NS_ERROR_NULL_POINTER;
+  }
+
+  nsIFactory *factory = nsnull;
+  if (aClass.Equals(kJANativeDetectorCID)) {
+    factory = NEW_JA_NATIVEDETECTOR_FACTORY();
+  } else if (aClass.Equals(kJANativeStringDetectorCID)) {
+    factory = NEW_JA_STRING_NATIVEDETECTOR_FACTORY();
+  } else if (aClass.Equals(kKONativeDetectorCID)) {
+    factory = NEW_KO_NATIVEDETECTOR_FACTORY();
+  } else if (aClass.Equals(kKONativeStringDetectorCID)) {
+    factory = NEW_KO_STRING_NATIVEDETECTOR_FACTORY();
+  }
+
+  if(nsnull != factory) {
+    nsresult res = factory->QueryInterface(kFactoryIID, (void **) aFactory);
+    if (NS_FAILED(res)) {
+      *aFactory = NULL;
+      delete factory;
+    }
+    return res;
+  }
+  return NS_NOINTERFACE;
+}
+
+extern "C" NS_EXPORT PRBool NSCanUnload(nsISupports* aServMgr) {
+  return PRBool(g_InstanceCount == 0 && g_LockCount == 0);
+}
+extern "C" NS_EXPORT nsresult NSRegisterSelf(nsISupports* aServMgr, const char *path)
+{
+  nsresult rv;
+
+  nsCOMPtr<nsIServiceManager> servMgr(do_QueryInterface(aServMgr, &rv));
+  if (NS_FAILED(rv)) return rv;
+
+  nsIComponentManager* compMgr;
+  rv = servMgr->GetService(kComponentManagerCID, 
+                           nsIComponentManager::GetIID(), 
+                           (nsISupports**)&compMgr);
+  if (NS_FAILED(rv)) return rv;
+
+  rv = compMgr->RegisterComponent(kJANativeDetectorCID, 
+                                  "Native JA Charset Detector", 
+                                  NS_CHARSET_DETECTOR_PROGID_BASE "jams", 
+                                  path,
+                                  PR_TRUE, PR_TRUE);
+
+  rv = compMgr->RegisterComponent(kJANativeStringDetectorCID, 
+                                  "Native JA String Charset Detector", 
+                                  NS_STRCDETECTOR_PROGID_BASE "jams", 
+                                  path,
+                                  PR_TRUE, PR_TRUE);
+
+  rv = compMgr->RegisterComponent(kKONativeDetectorCID, 
+                                  "Native KO Charset Detector", 
+                                  NS_CHARSET_DETECTOR_PROGID_BASE "koms", 
+                                  path,
+                                  PR_TRUE, PR_TRUE);
+
+  rv = compMgr->RegisterComponent(kKONativeStringDetectorCID, 
+                                  "Native KO String Charset Detector", 
+                                  NS_STRCDETECTOR_PROGID_BASE "koms", 
+                                  path,
+                                  PR_TRUE, PR_TRUE);
+
+  (void)servMgr->ReleaseService(kComponentManagerCID, compMgr);
+  return rv;
+}
+
+extern "C" NS_EXPORT nsresult NSUnregisterSelf(nsISupports* aServMgr, const char *path)
+{
+  nsresult rv;
+
+  nsCOMPtr<nsIServiceManager> servMgr(do_QueryInterface(aServMgr, &rv));
+  if (NS_FAILED(rv)) return rv;
+
+  nsIComponentManager* compMgr;
+  rv = servMgr->GetService(kComponentManagerCID, 
+                           nsIComponentManager::GetIID(), 
+                           (nsISupports**)&compMgr);
+  if (NS_FAILED(rv)) return rv;
+
+  rv = compMgr->UnregisterComponent(kJANativeDetectorCID, path);
+  rv = compMgr->UnregisterComponent(kJANativeStringDetectorCID, path);
+  rv = compMgr->UnregisterComponent(kKONativeDetectorCID, path);
+  rv = compMgr->UnregisterComponent(kKONativeStringDetectorCID, path);
+
+  (void)servMgr->ReleaseService(kComponentManagerCID, compMgr);
+  return rv;
+}
+
---
+++ intl/chardet/src/windows/nsNativeDetectors.h
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+
+#ifndef nsNativeDetectors_h__
+#define nsNativeDetectors_h__
+
+#include "nsIFactory.h"
+
+// {4FE25140-3944-11d3-9142-006008A6EDF6}
+#define NS_JA_NATIVE_DETECTOR_CID \
+{ 0x4fe25140, 0x3944, 0x11d3, { 0x91, 0x42, 0x0, 0x60, 0x8, 0xa6, 0xed, 0xf6 } };
+
+// {57296530-3644-11d3-9142-006008A6EDF6}
+#define NS_JA_NATIVE_STRING_DETECTOR_CID \
+{ 0x57296530, 0x3644, 0x11d3, { 0x91, 0x42, 0x0, 0x60, 0x8, 0xa6, 0xed, 0xf6 } }
+
+// {4FE25141-3944-11d3-9142-006008A6EDF6}
+#define NS_KO_NATIVE_DETECTOR_CID \
+{ 0x4fe25141, 0x3944, 0x11d3, { 0x91, 0x42, 0x0, 0x60, 0x8, 0xa6, 0xed, 0xf6 } };
+
+// {4FE25142-3944-11d3-9142-006008A6EDF6}
+#define NS_KO_NATIVE_STRING_DETECTOR_CID \
+{ 0x4fe25142, 0x3944, 0x11d3, { 0x91, 0x42, 0x0, 0x60, 0x8, 0xa6, 0xed, 0xf6 } };
+
+nsIFactory* NEW_JA_NATIVEDETECTOR_FACTORY();
+nsIFactory* NEW_JA_STRING_NATIVEDETECTOR_FACTORY();
+nsIFactory* NEW_KO_NATIVEDETECTOR_FACTORY();
+nsIFactory* NEW_KO_STRING_NATIVEDETECTOR_FACTORY();
+
+#endif /* nsNativeDetectors_h__ */
