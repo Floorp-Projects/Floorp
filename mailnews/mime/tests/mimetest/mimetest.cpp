4d3844ddd08e3582cbfe565d26e1afa5fd75c23c
sspitzer%netscape.com <sspitzer%netscape.com>
1999-07-17 02:00:27 +0000

    fix warning for rhp.

--- mailnews/mime/tests/mimetest/mimetest.cpp
+++ mailnews/mime/tests/mimetest/mimetest.cpp
@@ -297,7 +297,7 @@ nsresult
 FileInputStreamImpl::PumpFileStream()
 {
   if (mInFile->eof())
-    NS_BASE_STREAM_EOF;
+    return NS_BASE_STREAM_EOF;
   
   mBufLen = mInFile->read(mBuf, sizeof(mBuf));
   if (mBufLen > 0)
