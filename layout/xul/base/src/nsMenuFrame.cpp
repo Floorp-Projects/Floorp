a92ef1a85c5615b0186d7c4cdd369ba382eaaecf
hyatt%netscape.com <hyatt%netscape.com>
1999-07-18 06:36:37 +0000

    Adding the menu frame files.

---
+++ layout/xul/base/src/nsMenuFrame.cpp
@@ -0,0 +1,58 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+
+#include "nsMenuFrame.h"
+
+#include "nsIContent.h"
+#include "prtypes.h"
+#include "nsIAtom.h"
+#include "nsIPresContext.h"
+#include "nsIStyleContext.h"
+#include "nsCSSRendering.h"
+#include "nsINameSpaceManager.h"
+
+
+//
+// NS_NewMenuFrame
+//
+// Wrapper for creating a new menu popup container
+//
+nsresult
+NS_NewMenuFrame(nsIFrame** aNewFrame)
+{
+  NS_PRECONDITION(aNewFrame, "null OUT ptr");
+  if (nsnull == aNewFrame) {
+    return NS_ERROR_NULL_POINTER;
+  }
+  nsMenuFrame* it = new nsMenuFrame;
+  if ( !it )
+    return NS_ERROR_OUT_OF_MEMORY;
+  *aNewFrame = it;
+  return NS_OK;
+}
+
+
+//
+// nsMenuFrame cntr
+//
+nsMenuFrame::nsMenuFrame()
+{
+
+} // cntr
+
