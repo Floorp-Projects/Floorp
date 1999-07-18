b4443307361704bef9e2f806462ccb3ea9cf14c4
hangas%netscape.com <hangas%netscape.com>
1999-07-18 19:43:44 +0000

    Still writing globalOverlay.  Not yet part of build.

--- xpfe/global/resources/content/globalOverlay.js
+++ xpfe/global/resources/content/globalOverlay.js
@@ -1,1 +1,34 @@
-// more to come soon
+function NavigatorWindow()
+{
+	var toolkitCore = XPAppCoresManager.Find("ToolkitCore");
+	if (!toolkitCore)
+	{
+		toolkitCore = new ToolkitCore();
+		if (toolkitCore)
+		{
+			toolkitCore.Init("ToolkitCore");
+		}
+    }
+
+    if (toolkitCore)
+	{
+      toolkitCore.ShowWindow("chrome://navigator/content/",
+                             window);
+    }
+}
+
+
+function MessengerWindow()
+{
+	window.open("chrome://messenger/content/", "messenger", "chrome");
+}
+
+
+function AddressBook() 
+{
+	var wind = window.open("chrome://addressbook/content/addressbook.xul",
+						   "addressbook", "chrome");
+	return wind;
+}
+
+
