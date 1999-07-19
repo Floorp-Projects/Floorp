69024458b57fcf6259dc876c8f5a65109f0f1416
hangas%netscape.com <hangas%netscape.com>
1999-07-19 03:24:04 +0000

    Split up globalOverlay into three files

--- xpfe/global/resources/content/globalOverlay.js
+++ xpfe/global/resources/content/globalOverlay.js
@@ -1,45 +1,4 @@
-function NavigatorWindow()
-{
-	// FIX ME - Really need to find the front most navigator window
-	//          and bring it all the way to the front
-	
-	// FIX ME - This code needs to be updated to use window.open()
-	var toolkitCore = XPAppCoresManager.Find("ToolkitCore");
-	if (!toolkitCore)
-	{
-		toolkitCore = new ToolkitCore();
-		if (toolkitCore)
-		{
-			toolkitCore.Init("ToolkitCore");
-		}
-    }
-
-    if (toolkitCore)
-	{
-      toolkitCore.ShowWindow("chrome://navigator/content/",
-                             window);
-    }
-}
-
-
-function MessengerWindow()
-{
-	// FIX ME - Really need to find the front most messenger window
-	//          and bring it all the way to the front
-	
-	window.open("chrome://messenger/content/", "messenger", "chrome");
-}
-
-
-function AddressBook() 
-{
-	var wind = window.open("chrome://addressbook/content/addressbook.xul",
-						   "addressbook", "chrome");
-	return wind;
-}
-
-
-function QuitApplication()
+function goQuitApplication()
 {
 	var editorShell = Components.classes["component://netscape/editor/editorshell"].createInstance();
 	editorShell = editorShell.QueryInterface(Components.interfaces.nsIEditorShell);
@@ -47,4 +6,3 @@ function QuitApplication()
 	if ( editorShell )
 		editorShell.Exit();
 }
-
