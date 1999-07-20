ed261f87738fb806126bee5dc532d4cf808da142
ramiro%netscape.com <ramiro%netscape.com>
1999-07-20 06:18:42 +0000

    Move the timers to their final resting ground.  Im sorry.  I screwed up last time by putting them on the top level.  Ive learned my lesson.  Dont scrweup unless you can really get away with it.

---
+++ widget/timer/src/unix/motif/nsTimerMotifFactory.cpp
@@ -0,0 +1,167 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+
+#include "nsTimerMotif.h"
+
+#include "nsTimerCIID.h"
+
+#include "nsIFactory.h"
+#include "nsIComponentManager.h"
+#include "nsIServiceManager.h"
+#include "nsCOMPtr.h"
+
+static NS_DEFINE_CID(kCTimerMotif, NS_TIMER_MOTIF_CID);
+static NS_DEFINE_CID(kComponentManagerCID, NS_COMPONENTMANAGER_CID);
+
+class nsTimerMotifFactory : public nsIFactory
+{
+public:
+  NS_DECL_ISUPPORTS
+
+  NS_IMETHOD CreateInstance(nsISupports *aOuter,
+                            const nsIID &aIID,
+                            void **aResult);
+
+  NS_IMETHOD LockFactory(PRBool aLock);
+
+  nsTimerMotifFactory(const nsCID &aClass);
+  virtual ~nsTimerMotifFactory();
+
+private:
+  nsCID mClassID;
+
+};
+
+nsTimerMotifFactory::nsTimerMotifFactory(const nsCID &aClass) :
+  mRefCnt(0),
+  mClassID(aClass)
+{   
+}   
+
+nsTimerMotifFactory::~nsTimerMotifFactory()   
+{   
+  NS_ASSERTION(mRefCnt == 0, "non-zero refcnt at destruction");   
+}   
+
+NS_IMPL_ISUPPORTS(nsTimerMotifFactory, nsIFactory::GetIID())
+
+
+NS_IMETHODIMP
+nsTimerMotifFactory::CreateInstance(nsISupports *aOuter,  
+                                const nsIID &aIID,  
+                                void **aResult)  
+{  
+  if (aResult == nsnull)
+    return NS_ERROR_NULL_POINTER;  
+
+  *aResult = nsnull;  
+  
+  nsISupports *inst = nsnull;
+
+  if (mClassID.Equals(kCTimerMotif)) 
+  {
+    inst = (nsISupports *)(nsTimerMotif *) new nsTimerMotif();
+  }
+
+  if (inst == nsnull) 
+    return NS_ERROR_OUT_OF_MEMORY;
+  
+  nsresult rv = inst->QueryInterface(aIID, aResult);
+
+  if (rv != NS_OK) 
+    delete inst;
+  
+  return rv;
+}
+
+nsresult nsTimerMotifFactory::LockFactory(PRBool aLock)
+{
+  // Not implemented in simplest case.  
+  return NS_OK;
+}
+
+nsresult
+NSGetFactory(nsISupports* servMgr,
+             const nsCID &aClass,
+             const char *aClassName,
+             const char *aProgID,
+             nsIFactory **aFactory)
+{
+  if (nsnull == aFactory) {
+    return NS_ERROR_NULL_POINTER;
+  }
+
+  *aFactory = new nsTimerMotifFactory(aClass);
+
+  if (nsnull == aFactory) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+
+  return (*aFactory)->QueryInterface(nsIFactory::GetIID(), (void**)aFactory);
+  
+}
+
+PRBool
+NSCanUnload(nsISupports* aServMgr)
+{
+  return PR_FALSE;
+}
+
+nsresult
+NSRegisterSelf(nsISupports* aServMgr, const char *fullpath)
+{
+  nsresult rv;
+
+#ifdef NS_DEBUG
+  printf("*** Registering MOTIF timer\n");
+#endif
+
+  nsCOMPtr<nsIServiceManager>
+    serviceManager(do_QueryInterface(aServMgr, &rv));
+  if (NS_FAILED(rv)) return rv;
+  
+  NS_WITH_SERVICE(nsIComponentManager, compMgr, kComponentManagerCID, &rv);
+  if (NS_FAILED(rv)) return rv;
+
+  rv = compMgr->RegisterComponent(kCTimerMotif,
+                                  "MOTIF timer",
+                                  "component://netscape/timer/unix/motif",
+                                  fullpath,
+                                  PR_TRUE, 
+                                  PR_TRUE);
+  
+  return rv;
+}
+
+nsresult
+NSUnregisterSelf(nsISupports* aServMgr, const char *fullpath)
+{
+
+  nsresult rv;
+  nsCOMPtr<nsIServiceManager>
+    serviceManager(do_QueryInterface(aServMgr, &rv));
+  if (NS_FAILED(rv)) return rv;
+  
+  NS_WITH_SERVICE(nsIComponentManager, compMgr, kComponentManagerCID, &rv);
+  if (NS_FAILED(rv)) return rv;
+
+  compMgr->UnregisterComponent(kCTimerMotif, fullpath);
+  
+  return NS_OK;
+}
---
+++ widget/timer/src/unix/xlib/nsTimerXlib.h
@@ -0,0 +1,67 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef __nsTimerXlib_h
+#define __nsTimerXlib_h
+
+#include "nsITimer.h"
+#include "nsITimerCallback.h"
+
+#include <sys/time.h>
+
+class nsTimerXlib : public nsITimer
+{
+public:
+  static nsTimerXlib *gTimerList;
+  static struct timeval gTimer;
+  static struct timeval gNextFire;
+  static void ProcessTimeouts(struct timeval *aNow);
+  nsTimerXlib();
+  virtual ~nsTimerXlib();
+
+  virtual nsresult Init(nsTimerCallbackFunc aFunc,
+                        void *aClosure,
+                        PRUint32 aDelay);
+
+  virtual nsresult Init(nsITimerCallback *aCallback,
+                        PRUint32 aDelay);
+
+  NS_DECL_ISUPPORTS
+
+  virtual void Cancel();
+  void Fire(struct timeval *aNow);
+  
+  virtual PRUint32 GetDelay() { return 0; };
+  virtual void SetDelay(PRUint32 aDelay) {};
+  virtual void *GetClosure() { return mClosure; }
+
+  // this needs to be public so that the mainloop can
+  // find the next fire time...
+  struct timeval       mFireTime;
+  nsTimerXlib *        mNext;
+
+private:
+  nsresult Init(PRUint32 aDelay);
+
+  nsTimerCallbackFunc  mFunc;
+  void *               mClosure;
+  PRUint32             mDelay;
+  nsITimerCallback *   mCallback;
+};
+
+#endif // __nsTimerXlib_h
