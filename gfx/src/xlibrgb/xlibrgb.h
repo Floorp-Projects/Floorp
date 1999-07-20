36fea549af734a621bdd90258af818874f4b6d06
ramiro@netscape.com <ramiro@netscape.com>
1999-07-20 11:26:35 +0000

    Add missing prototype.

--- gfx/src/xlibrgb/xlibrgb.h
+++ gfx/src/xlibrgb/xlibrgb.h
@@ -186,6 +186,9 @@ xlib_rgb_get_screen (void);
 extern void
 xlib_set_xt_app_context(void * app_context);
 
+extern void *
+xlib_get_xt_app_context();
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
