4a473e2d642fc92d60337b4c73a87d6a98bfb101
slamm%netscape.com <slamm%netscape.com>
1999-07-21 00:00:14 +0000

    Add bookmark popup code

---
+++ xpfe/components/bookmarks/resources/bookmark-popup.js
@@ -0,0 +1,46 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.1 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1999 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/*
+  Script for the bookmarks popup window
+*/
+function BookmarkPopupFillTree(popup,node) {
+  if (gBookmarkPopup) {
+    gBookmarkPopup.close();
+  }
+
+  var uri = node.getAttribute('id');
+  var treechildren = document.getElementById('cascade:treechildren');
+  treechildren.setAttribute('ref', uri);
+  gBookmarkPopup = popup;
+}
+
+function BookmarkPopupSingleClick(popup,node) {
+  if (node.getAttribute('container') == "true")
+  {
+    return false;
+  }
+
+  OpenBookmarkURL(node);
+
+  gBookmarkPopup = null;
+
+  popup.close();
+
+  return true;
+}
