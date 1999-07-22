35d437f8aef9c3d2cfdc9a50d1615a6e68481a84
davidm@netscape.com <davidm@netscape.com>
1999-07-22 00:27:14 +0000

    Modify navigator.xul to use taskoverly. Updated overlay to have all the functionality that existed in navigator.xul

--- xpfe/browser/resources/content/navigator.js
+++ xpfe/browser/resources/content/navigator.js
@@ -452,21 +452,7 @@
     }
   }
   
-  function BrowserNewTextEditorWindow()
-  {
-    core = XPAppCoresManager.Find("toolkitCore");
-    if ( !core ) {
-        core = new ToolkitCore();
-        if ( core ) {
-            core.Init("toolkitCore");
-        }
-    }
-    if ( core ) {
-        core.ShowWindowWithArgs( "chrome://editor/content/TextEditorAppShell.xul", window, "chrome://editor/content/EditorInitPagePlain.html" );
-    } else {
-        dump("Error; can't create toolkitCore\n");
-    }
-  }
+
   
   function BrowserEditPage(url)
   {
@@ -553,19 +539,8 @@
     }
   }
 
-  function OpenHistoryView()
-  {
-    var toolkitCore = XPAppCoresManager.Find("toolkitCore");
-    if (!toolkitCore) {
-      toolkitCore = new ToolkitCore();
-      if (toolkitCore) {
-        toolkitCore.Init("toolkitCore");
-      }
-    }
-    if (toolkitCore) {
-      toolkitCore.ShowWindow("resource:/res/samples/history.xul",window);
-    }
-  }
+
+
 
   function BrowserPrintPreview()
   {
@@ -799,19 +774,7 @@
     }
   }
 
-  function OpenEditor()
-  {
-    var toolkitCore = XPAppCoresManager.Find("ToolkitCore");
-    if (!toolkitCore) {
-      toolkitCore = new ToolkitCore();
-      if (toolkitCore) {
-        toolkitCore.Init("ToolkitCore");
-      }
-    }
-    if (toolkitCore) {
-      toolkitCore.ShowWindowWithArgs("chrome://editor/content/EditorAppShell.xul",window,"chrome://editor/content/EditorInitPage.html");
-    }
-  }
+
         var bindCount = 0;
         function onStatus() {
             var status = document.getElementById("Browser:Status");
@@ -904,81 +867,5 @@
             dump( "meter value=" + meter.getAttribute("value") + "\n" );
         }
 
-function ShowWindowFromResource( node )
-{
-	var windowManager = Components.classes['component://netscape/rdf/datasource?name=window-mediator'].getService();
-	dump("got window Manager \n");
-	var	windowManagerInterface = windowManager.QueryInterface( Components.interfaces.nsIWindowMediator);
-    dump("got interface \n");
-    
-    var desiredWindow = null;
-    var url = node.getAttribute('id');
-    dump( url +" finding \n" );
-	desiredWindow = windowManagerInterface.GetWindowForResource( url );
-	dump( "got window \n");
-	if ( desiredWindow )
-	{
-		dump("focusing \n");
-		desiredWindow.focus();
-	}
-}
 
-function CycleWindow( inType, inChromeURL )
-{
-	var windowManager = Components.classes['component://netscape/rdf/datasource?name=window-mediator'].getService();
-	dump("got window Manager \n");
-	var	windowManagerInterface = windowManager.QueryInterface( Components.interfaces.nsIWindowMediator);
-    dump("got interface \n");
-    
-    var desiredWindow = null;
-    
-	topWindowOfType = windowManagerInterface.GetMostRecentWindow( inType );
-	topWindow = windowManagerInterface.GetMostRecentWindow( null );
-	dump( "got windows \n");
-	if ( topWindowOfType != topWindow )
-	{
-		dump( "first not top so give focus \n");
-		topWindowOfType.focus();
-		return;
-	}
-	
-	if ( topWindowOfType == null )
-	{
-		dump( " no windows of this type so create a new one \n");
-		window.open( inChromeURL, "","chrome" );
-	}
-	var enumerator = windowManagerInterface.GetEnumerator( inType );
-	firstWindow = windowManagerInterface.ConvertISupportsToDOMWindow ( enumerator.GetNext() );
-	if ( firstWindow == topWindowOfType )
-	{
-		dump( "top most window is first window \n");
-		firstWindow = null;
-	}
-	else
-	{
-		dump("find topmost window \n");
-		while ( enumerator.HasMoreElements() )
-		{
-			var nextWindow = windowManagerInterface.ConvertISupportsToDOMWindow ( enumerator.GetNext() );
-			if ( nextWindow == topWindowOfType )
-				break;
-		}	
-	}
-	desiredWindow = firstWindow;
-	if ( enumerator.HasMoreElements() )
-	{
-		dump( "Give focus to next window in the list \n");
-		desiredWindow = windowManagerInterface.ConvertISupportsToDOMWindow ( enumerator.GetNext() );		
-	}
-	
-	if ( desiredWindow )
-	{
-		desiredWindow.focus();
-		dump("focusing window \n");
-	}
-	else
-	{
-		dump("open window \n");
-		window.open( inChromeURL, "","chrome" );
-	}
-}
+
