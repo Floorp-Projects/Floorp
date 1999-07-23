b6cfdd484255e3bea230d190dd0b5476f3aca2db
ramiro@netscape.com <ramiro@netscape.com>
1999-07-23 12:24:09 +0000

    Add IDS for unix toolkit timers.

---
+++ widget/timer/src/unix/nsUnixTimerCIID.h
@@ -0,0 +1,38 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsUnixTimerCIID_h__
+#define nsUnixTimerCIID_h__
+
+#include "nsISupports.h"
+
+// {90F55E53-40EB-11d3-B219-000064657374}
+#define NS_TIMER_MOTIF_CID \
+{ 0x90f55e53, 0x40eb, 0x11d3, \
+  { 0xb2, 0x19, 0x0, 0x0, 0x64, 0x65, 0x73, 0x74 } }
+
+// {90F55E54-40EB-11d3-B219-000064657374}
+#define NS_TIMER_GTK_CID \
+{ 0x90f55e54, 0x40eb, 0x11d3, \
+  { 0xb2, 0x19, 0x0, 0x0, 0x64, 0x65, 0x73, 0x74 } }
+
+// {90F55E57-40EB-11d3-B219-000064657374}
+#define NS_TIMER_XLIB_CID \
+{ 0x90f55e57, 0x40eb, 0x11d3, \
+  { 0xb2, 0x19, 0x0, 0x0, 0x64, 0x65, 0x73, 0x74 } }
+#endif // nsUnixTimerCIID_h__
