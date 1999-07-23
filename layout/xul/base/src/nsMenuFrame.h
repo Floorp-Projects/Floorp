7a255b6ee5f60fabb0dbde54fa590e5eecd95f18
hyatt@netscape.com <hyatt@netscape.com>
1999-07-23 07:49:43 +0000

    Teaching the menus about the ESC key (which will close up each level of menu until you hit the menu bar).

--- layout/xul/base/src/nsMenuFrame.h
+++ layout/xul/base/src/nsMenuFrame.h
@@ -79,6 +79,7 @@ public:
 
   void KeyboardNavigation(PRUint32 aDirection, PRBool& aHandledFlag);
   void ShortcutNavigation(PRUint32 aLetter, PRBool& aHandledFlag);
+  void Escape();
 
   void ToggleMenuState();
   void SelectMenu(PRBool aActivateFlag);
