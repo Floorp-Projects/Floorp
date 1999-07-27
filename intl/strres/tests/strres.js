c079aad3eb28aca5fbc5f00f8bbc5f9e2918af83
tao%netscape.com <tao%netscape.com>
1999-07-27 01:22:53 +0000

    Add test cases.

---
+++ intl/strres/tests/strres.js
@@ -0,0 +1,30 @@
+var strBundleService = null;
+function JS_GetStrBundle(path, locale)
+{
+  var strBundle = null;
+
+  strBundleService =
+    Components.classes["component://netscape/intl/stringbundle"].createInstance(); 
+
+  if (strBundleService) {
+    strBundleService = 
+      strBundleService.QueryInterface(Components.interfaces.nsIStringBundleService);
+
+    if (strBundleService) {
+      strBundle = strBundleService.CreateBundle(path, locale); 
+      if (!strBundle) {
+        dump("\n--** strbundle  createInstance failed **--\n");
+		return null;
+      }
+    }
+    else {
+      dump("\n--** strBundleService createInstance 2 **--\n");
+	  return null;
+    }
+  }
+  else {
+    dump("\n--** strBundleService createInstance 1 failed **--\n");
+	return null;
+  }
+  return strBundle;
+}
