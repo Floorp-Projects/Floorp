2281825b564f7a220e69abc8c4a3fc6c7d8e6545
warren@netscape.com <warren@netscape.com>
1999-07-28 04:02:07 +0000

    Fixed UMR / refcounting problem (purify).

--- netwerk/mime/src/nsMIMEInfoImpl.cpp
+++ netwerk/mime/src/nsMIMEInfoImpl.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
  * The contents of this file are subject to the Netscape Public License
  * Version 1.0 (the "NPL"); you may not use this file except in
@@ -25,6 +25,7 @@ NS_IMPL_ISUPPORTS(nsMIMEInfoImpl, nsCOMTypeInfo<nsIMIMEInfo>::GetIID());
 
 // nsMIMEInfoImpl methods
 nsMIMEInfoImpl::nsMIMEInfoImpl(const char *aMIMEType, const char *aFileExtensions, const char *aDescription) {
+    NS_INIT_REFCNT();
     mMIMEType = NS_NewAtom(aMIMEType);
     mFileExtensions.SetString(aFileExtensions);
     mDescription.SetString(aDescription);
