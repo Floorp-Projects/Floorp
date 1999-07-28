14ca7569f89511a7adc4b18e375fdcf9d4b674ab
warren%netscape.com <warren%netscape.com>
1999-07-28 08:27:38 +0000

    Fully initialized arena so that it won't fail in destructor without first calling Init.

--- xpcom/ds/nsArena.h
+++ xpcom/ds/nsArena.h
@@ -27,7 +27,7 @@
 // Simple arena implementation layered on plarena
 class ArenaImpl : public nsIArena {
 public:
-  ArenaImpl(void) { NS_INIT_REFCNT(); }
+  ArenaImpl(void);
   virtual ~ArenaImpl();
 
   NS_DECL_ISUPPORTS
