23bf95375f329392d32cd3fe41cf3c55ce921099
tao%netscape.com <tao%netscape.com>
1999-07-31 16:25:34 +0000

    Rename JS_GetStrBundle() to srGetStrBundle().

--- intl/strres/tests/strres-test.js
+++ intl/strres/tests/strres-test.js
@@ -109,7 +109,7 @@ function help() {
 }
 
 function strresTest() {
-  var Bundle = JS_GetStrBundle("resource:/res/strres.properties", null);
+  var Bundle = srGetStrBundle("resource:/res/strres.properties", null);
   var	ostr1 = Bundle.GetStringFromName("file");
   dump("\n--** JS strBundle GetStringFromName file=" + ostr1 +
       "len=" + ostr1.length + "**--\n");
