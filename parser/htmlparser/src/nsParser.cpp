81a3d839ccf4fedb97c3b5fe883ea90960ab11af
harishd%netscape.com <harishd%netscape.com>
1999-07-31 20:16:30 +0000

    Fix for bug#s 10530, and 10596. Conditioning the raw buffer by removing the unwanted null chars.

--- parser/htmlparser/src/nsParser.cpp
+++ parser/htmlparser/src/nsParser.cpp
@@ -1138,6 +1138,15 @@ nsresult nsParser::OnDataAvailable(nsIURI* aURL, nsIInputStream *pIStream, PRUin
       if(mParserFilter)
          mParserFilter->RawBuffer(mParserContext->mTransferBuffer, &theNumRead);
 
+      // XXX Hack --- NULL character(s) is(are) seen in the middle of the buffer!!!
+      // For now, I'm conditioning the raw buffer by removing the unwanted null chars.
+      // Problem could be NECKO related
+
+      for(PRUint32 i=0;i<theNumRead;i++) {
+        if(mParserContext->mTransferBuffer[i]==kNullCh)
+          mParserContext->mTransferBuffer[i]=kSpace;
+      }
+
 #ifdef  NS_DEBUG
       int index=0;
       for(index=0;index<10;index++)
