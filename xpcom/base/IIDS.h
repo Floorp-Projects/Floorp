7e2c77ad535384572a0d8f7f3bba0e2fd81c9540
troy@netscape.com <troy@netscape.com>
1999-08-03 22:35:42 +0000

    Reserved IID for nsIFrameManager

--- xpcom/base/IIDS.h
+++ xpcom/base/IIDS.h
@@ -1121,7 +1121,7 @@ nsIFormSubmitObserver = { /* a6cf9106-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-INTERFACENAME = { /* a6cf9107-15b3-11d2-932e-00805f8add32 */
+nsIFrameManager = { /* a6cf9107-15b3-11d2-932e-00805f8add32 */
     0xa6cf9107,
     0x15b3,
     0x11d2,
