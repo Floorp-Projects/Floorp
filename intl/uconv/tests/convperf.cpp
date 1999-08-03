e2867d6fff706d8c49cfcd91f2837f2f739225aa
ftang@netscape.com <ftang@netscape.com>
1999-08-03 21:05:59 +0000

    add nsconv and convperf

---
+++ intl/uconv/tests/convperf.cpp
@@ -0,0 +1,192 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
+#include <windows.h>
+#include <winnls.h>
+
+#include "nscore.h"
+#include "nsString.h"
+#include "nsIServiceManager.h"
+#include "nsICharsetConverterManager.h"
+#include "nsIUnicodeEncoder.h"
+#include "nsIUnicodeDecoder.h"
+static NS_DEFINE_CID(kCharsetConverterManagerCID, NS_ICHARSETCONVERTERMANAGER_CID);
+static NS_DEFINE_IID(kICharsetConverterManagerIID, NS_ICHARSETCONVERTERMANAGER_IID);
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void usage()
+{
+  printf(
+"convperf -f fromcode -t tocode [file]\n"
+  );
+}
+int fromcodeind = 0;
+int tocodeind = 0;
+FILE* infile = 0;
+#define INBUFSIZE (1024*16)
+#define MEDBUFSIZE (1024*16*2)
+#define OUTBUFSIZE (1024*16*8)
+char inbuffer[INBUFSIZE];
+char outbuffer[OUTBUFSIZE];
+PRUnichar  medbuffer[MEDBUFSIZE];
+nsIUnicodeEncoder* encoder = nsnull;
+nsIUnicodeDecoder* decoder = nsnull;
+UINT incp = 932;
+UINT outcp = 932;
+
+void memcpyDecode(const char* src, PRInt32 srclen, char* dest)
+{
+   ::memcpy(dest, src, srclen);
+}
+void memcpyEncode(const char* src, PRInt32 srclen, char* dest)
+{
+   ::memcpy(dest, src, srclen);
+}
+
+void WideDecode(const char* src, 
+              PRInt32 srclen, PRUnichar *dest, PRInt32 *destLen)
+{
+   const char* end = src+srclen ;
+   while(src < end)
+     *dest++ = (PRUnichar) *src++;
+   *destLen = srclen;
+}
+void NarrowEncode(const PRUnichar *src, 
+              PRInt32 srclen, char* dest, PRInt32* destLen)
+{
+   const PRUnichar* end = src+srclen ;
+   while(src < end)
+     *dest++ = (char) *src++;
+   *destLen = srclen;
+}
+void msDecode(UINT cp, const char* src, 
+              PRInt32 srclen, PRUnichar *dest, PRInt32 *destLen)
+{
+   *destLen = ::MultiByteToWideChar(cp, 0,src, srclen, (LPWSTR)dest, *destLen);
+   if(*destLen <= 0)
+      fprintf(stderr, "problem in ::MultiByteToWideChar\n");
+}
+void msEncode(UINT cp, const PRUnichar *src, 
+              PRInt32 srcLen, char* dest, PRInt32* destLen)
+{
+   *destLen = ::WideCharToMultiByte(cp, 0, src, srcLen, (LPSTR)dest, *destLen, 
+                (LPCSTR)" ", FALSE);
+   if(*destLen <= 0)
+      fprintf(stderr, "problem in ::WideCharToMultiByte\n");
+}
+     
+int main(int argc, const char** argv)
+{
+   nsresult res= NS_OK;
+   nsICharsetConverterManager* ccMain=nsnull;
+   // get ccMain;
+   nsServiceManager::GetService(kCharsetConverterManagerCID,
+       kICharsetConverterManagerIID, (nsISupports**) &ccMain);
+   if(NS_FAILED(res))
+   {
+	 fprintf(stderr, "Cannot get Character Converter Manager %x\n", res);
+   }
+   int i;
+   if(argc > 5)
+   {
+     for(i =0; i < argc; i++)
+     {
+       if(strcmp(argv[i], "-t") == 0)
+       {
+             tocodeind = i+1;
+             nsAutoString str(argv[tocodeind]);
+             res = ccMain->GetUnicodeDecoder(&str, &decoder);
+             if(NS_FAILED(res)) {
+	     	fprintf(stderr, "Cannot get Unicode decoder %s %x\n", 
+			argv[tocodeind],res);
+  		return -1;
+	     }
+
+       }
+       if(strcmp(argv[i], "-f") == 0)
+       {
+             fromcodeind = i+1;
+             nsAutoString str(argv[fromcodeind]);
+             res = ccMain->GetUnicodeEncoder(&str, &encoder);
+             if(NS_FAILED(res)) {
+	     	fprintf(stderr, "Cannot get Unicode encoder %s %x\n", 
+			argv[fromcodeind],res);
+  		return -1;
+	     }
+       }
+     }
+     if(argc == 6)
+     {
+        infile = fopen(argv[5], "rb");
+        if(NULL == infile) 
+        {  
+           usage();
+           fprintf(stderr,"cannot open file %s\n", argv[5]);
+           return -1; 
+        }
+     }
+     else
+     {
+        infile = stdin;
+     }
+    
+     PRInt32 insize,medsize,outsize;
+     while((insize=fread(inbuffer, 1,INBUFSIZE,infile)) > 0)
+     {
+        medsize=MEDBUFSIZE;
+        
+	res = decoder->Convert(medbuffer, 0, &medsize,inbuffer,0,&insize);
+        if(NS_FAILED(res)) {
+            fprintf(stderr, "failed in decoder->Convert %x\n",res);
+	    return -1;
+	}
+        outsize = OUTBUFSIZE;
+	res = encoder->Convert(medbuffer, &medsize, outbuffer,&outsize);
+        if(NS_FAILED(res)) {
+            fprintf(stderr, "failed in encoder->Convert %x\n",res);
+	    return -1;
+	}
+        fwrite(outbuffer, 1, outsize, stdout);
+
+        memcpyDecode(inbuffer, insize, outbuffer);
+
+        memcpyEncode(inbuffer, insize, outbuffer);
+
+        medsize = MEDBUFSIZE;
+        msDecode(incp, inbuffer, insize, medbuffer, &medsize);
+
+        outsize = OUTBUFSIZE;
+        msEncode(outcp, medbuffer, medsize, outbuffer, &outsize);
+
+        medsize = MEDBUFSIZE;
+        WideDecode( inbuffer, insize, medbuffer, &medsize);
+
+        outsize = OUTBUFSIZE;
+        NarrowEncode( medbuffer, medsize, outbuffer, &outsize);
+     }
+     
+     fclose(infile);
+     fclose(stdout);
+     fprintf(stderr, "Done!\n");
+     return 0;
+   }
+   usage();
+   return -1;
+}
