3cc0227c64f2a885c9d3523f674333dbefd46328
rogerl%netscape.com <rogerl%netscape.com>
1999-08-04 22:26:07 +0000

    Missing '\' before \n. Trailing '|' in expected output was not correct.

--- js/tests/ecma_2/RegExp/properties-001.js
+++ js/tests/ecma_2/RegExp/properties-001.js
@@ -17,10 +17,10 @@
     AddRegExpCases( /.*/,       ".*", false, false, false, 0 );
     AddRegExpCases( /[\d]{5}/g, "[\\d]{5}", true, false, false, 0 );
     AddRegExpCases( /[\S]?$/i,  "[\\S]?$", false, true, false, 0 );
-    AddRegExpCases( /^([a-z]*)[^\w\s\f\n\r]+/m,  "^([a-z]*)[^\\w\\s\\f\n\\r]+", false, false, true, 0 );
+    AddRegExpCases( /^([a-z]*)[^\w\s\f\n\r]+/m,  "^([a-z]*)[^\\w\\s\\f\\n\\r]+", false, false, true, 0 );
     AddRegExpCases( /[\D]{1,5}[\ -][\d]/gi,      "[\\D]{1,5}[\\ -][\\d]", true, true, false, 0 );
     AddRegExpCases( /[a-zA-Z0-9]*/gm, "[a-zA-Z0-9]*", true, false, true, 0 );
-    AddRegExpCases( /x|y|z/gim, "x|y|z|", true, true, true, 0 );
+    AddRegExpCases( /x|y|z/gim, "x|y|z", true, true, true, 0 );
 
     AddRegExpCases( /\u0051/im, "\\u0051", false, true, true, 0 );
     AddRegExpCases( /\x45/gm, "\\x45", true, false, true, 0 );
