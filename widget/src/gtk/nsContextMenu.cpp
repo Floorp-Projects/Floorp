de5383fa93f9c0601a8371cde755364fd4e5c4f7
pavlov%netscape.com <pavlov%netscape.com>
1999-08-05 23:13:07 +0000

    lets try this again

--- widget/src/gtk/nsContextMenu.cpp
+++ widget/src/gtk/nsContextMenu.cpp
@@ -437,10 +437,10 @@ nsEventStatus nsContextMenu::MenuItemSelected(const nsMenuEvent & aMenuEvent)
   return nsEventStatus_eIgnore;
 }
 
-void nsContextMenu::MenuPosFunc(GtkMenu *menu,
-                                  gint *x,
-                                  gint *y,
-                                  gpointer data)
+void menu_popup_position(GtkMenu *menu,
+                         gint *x,
+                         gint *y,
+                         gpointer data)
 {
   nsContextMenu *cm = (nsContextMenu*)data;
   *x = cm->GetX();
@@ -452,7 +452,7 @@ nsEventStatus nsContextMenu::MenuSelected(const nsMenuEvent & aMenuEvent)
   GtkWidget *parent = GTK_WIDGET(mParent->GetNativeData(NS_NATIVE_WIDGET));
   gtk_menu_popup (GTK_MENU(mMenu),
                   parent, NULL,
-                  nsContextMenu::MenuPosFunc,
+                  menu_popup_position,
                   this, 1, GDK_CURRENT_TIME);
 
   if (nsnull != mListener) {
--- widget/src/gtk/nsWidgetFactory.cpp
+++ widget/src/gtk/nsWidgetFactory.cpp
@@ -41,9 +41,7 @@
 #include "nsMenu.h"
 #include "nsMenuItem.h"
 #include "nsPopUpMenu.h"
-#if 0
 #include "nsContextMenu.h"
-#endif
 #include "nsTabWidget.h"
 #include "nsFontRetrieverService.h"
 
@@ -236,11 +234,9 @@ nsresult nsWidgetFactory::CreateInstance(nsISupports *aOuter,
     else if (mClassID.Equals(kCPopUpMenu)) {
         inst = (nsISupports*)new nsPopUpMenu();
     }
-#if 0
     else if (mClassID.Equals(kCContextMenu)) {
         inst = (nsISupports*)(nsIContextMenu*)new nsContextMenu();
     }
-#endif
     else if (mClassID.Equals(kCSound)) {
     	nsISound* aSound = nsnull;
     	NS_NewSound(&aSound);
