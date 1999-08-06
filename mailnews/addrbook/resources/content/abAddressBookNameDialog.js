f3f79c0067c7865507cf2a8f3da9bc98fe6aa7dd
hangas%netscape.com <hangas%netscape.com>
1999-08-06 04:19:42 +0000

    First Checked In.

---
+++ mailnews/addrbook/resources/content/abAddressBookNameDialog.js
@@ -0,0 +1,32 @@
+var okCallback = 0;
+
+function abNameOnLoad()
+{
+	doSetOKCancel(abNameOKButton, 0);
+
+	// look in arguments[0] for parameters
+	if (window.arguments && window.arguments[0])
+	{
+		if ( window.arguments[0].title )
+		{
+			dump("title = " + window.arguments[0].title + "\n");
+			var title = window.arguments[0].title;
+			top.window.title = title;
+		}
+		
+		if ( window.arguments[0].okCallback )
+			top.okCallback = window.arguments[0].okCallback;
+	}
+}
+
+function abNameOKButton()
+{
+	if ( top.okCallback )
+	{
+		var name = document.getElementById('name').value;
+	
+		top.okCallback(name);
+	}
+	
+	return true;
+}
