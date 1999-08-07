b9731e91b1059399d25371feae07fdee9d823169
putterman%netscape.com <putterman%netscape.com>
1999-08-07 00:20:10 +0000

    Added locale:en-US MANIFEST

--- build/mac/NGLayoutBuildList.pm
+++ build/mac/NGLayoutBuildList.pm
@@ -1142,6 +1142,7 @@ sub MakeResourceAliases()
         
         _InstallResources(":mozilla:mailnews:base:resources:content:MANIFEST",           "$messenger_chrome_dir:content:default:", 0);
         _InstallResources(":mozilla:mailnews:base:resources:skin:MANIFEST",              "$messenger_chrome_dir:skin:default:", 0);
+        _InstallResources(":mozilla:mailnews:base:resources:locale:en-US:MANIFEST",      "$messenger_chrome_dir:locale:en-US:", 0);
         _InstallResources(":mozilla:mailnews:base:prefs:resources:content:MANIFEST",     "$messenger_chrome_dir:content:default:", 0);
         _InstallResources(":mozilla:mailnews:base:prefs:resources:locale:en-US:MANIFEST", "$messenger_chrome_dir:locale:en-US:", 0);
         _InstallResources(":mozilla:mailnews:base:prefs:resources:skin:MANIFEST",        "$messenger_chrome_dir:skin:default:", 0);
