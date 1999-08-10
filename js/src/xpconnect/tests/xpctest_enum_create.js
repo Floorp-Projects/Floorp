a06a9f277e33aafe73372e6a810b2e96376c1eb2
jband@netscape.com <jband@netscape.com>
1999-08-10 06:12:06 +0000

    updating tests

--- js/src/xpconnect/tests/xpctest_enum_create.js
+++ js/src/xpconnect/tests/xpctest_enum_create.js
@@ -133,42 +133,56 @@ dumpln ("** Enumerated " + totalIfaces + " interfaces");
 dumpln (" * " + strangeIfaces + " with strange names");
 
 var progIDsTo_NOT_Create = [
-    "component://netscape/messenger/maildb", // calling a property - nsMsgDatabase::m_newSet used but not set
+
+// fixed    "component://netscape/prefwindow", // nsPrefWindow::~nsPrefWindow() releases null service
+// fixed /* BUG 11511 */    "component://netscape/rdf/datasource?name=addresscard",      // nsAbRDFDataSource::~nsAbRDFDataSource calls mRDFService->UnregisterDataSource(this); even though it was not registered
+// fixed /* BUG 11511 */    "component://netscape/rdf/datasource?name=addressdirectory", // nsAbRDFDataSource::~nsAbRDFDataSource calls mRDFService->UnregisterDataSource(this); even though it was not registered
+// fixed    "component://netscape/addressbook/directoryproperty",  // fails to init refcount
+
+// fixed    "component://netscape/rdf/datasource?name=local-store", //NS_NewLocalStore calls stuff that asserts
+// fixed    "component://netscape/rdf/datasource?name=xpinstall-update-notifier", //RDFXMLDataSourceImpl::Refresh does CreateInstance of parser but fails to check the result (I think I've seen this elsewhere)
+// fixed    "component://netscape/rdf/xul-template-builder",    //RDFXMLDataSourceImpl::Refresh does CreateInstance of parser but fails to check the result (I think I've seen this elsewhere)
+
+
+/* dp claims to have fixes coming */   "component://netscape/image/decoder&type=image/",  // PNGDecoder::QueryInterface and JPGDecoder::QueryInterface, do NS_INIT_REFCNT() in QueryInterface! (npunn)
+/* works now ??? */    "component://netscape/messenger/maildb", // calling a property - nsMsgDatabase::m_newSet used but not set
+
+/* BUG 11507 */    "component://netscape/wallet", // WalletFactoryImpl::CreateInstance broken : calls "delete inst" then calls "NS_IF_RELEASE(inst)"
+
+/* BUG 11509 */    "component://netscape/messengercompose/compose",              // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service)
+/* BUG 11509 */    "component://netscape/messenger",                             // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service)
+/* BUG 11509 */    "component://netscape/rdf/datasource?name=msgaccountmanager", // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service) 
+/* BUG 11509 */    "component://netscape/rdf/datasource?name=mailnewsfolders",   // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service)
+/* BUG 11509 */    "component://netscape/rdf/datasource?name=msgnotifications",  // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service)
+/* BUG 11509 */    "component://netscape/rdf/datasource?name=mailnewsmessages",  // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service)
+
+
+
+/* BUG 11510 */    "component://netscape/rdf/xul-key-listener",   // RDFFactoryImpl::CreateInstance asserts when creating if requested interface is nsISupports!
+/* BUG 11510 */    "component://netscape/rdf/xul-popup-listener", // RDFFactoryImpl::CreateInstance asserts when creating if requested interface is nsISupports!
+/* BUG 11510 */    "component://netscape/rdf/xul-focus-tracker",  // RDFFactoryImpl::CreateInstance asserts when creating if requested interface is nsISupports!
+
+
+/* BUG 11512 */    "component://netscape/rdf/datasource?name=files", // FileSystemDataSource::~FileSystemDataSource calls gRDFService->UnregisterDataSource(this); even though it was not registered
+
+
+/* BUG 11514 */    "component://netscape/rdf/datasource?name=msgaccounts", // nsMsgAccountDataSource::QueryInterface is COMPLETELY <censor>screwed</censor> up
+
+/* BUG 11516 */    "component://netscape/rdf/xul-sort-service",  // XULSortServiceImpl releases itself in its own destructor!
+
+// this bug filing business is hard work! I'll file the rest later...
+
     "component://netscape/rdf/resource-factory",  // calling a property - trying to copy a null value in nsRDFResource::GetValue
-    "component://netscape/addressbook/directoryproperty",  // fails to init refcount
-    "component://netscape/image/decoder&type=image/",  // PNGDecoder::QueryInterface and JPGDecoder::QueryInterface, do NS_INIT_REFCNT() in QueryInterface! (npunn)
     "component://netscape/messenger/identity",  // calling a property - nsMsgIdentity::getCharPref  is null
     "component://netscape/messenger/account",   // calling a property - nsMsgAccount::GetIncomingServer asserts because m_accountKey is null
     "component://netscape/messenger/server&type=", // calling a property - nsMsgIncomingServer::getCharPref uses m_prefs which is null
-    "component://netscape/prefwindow", // nsPrefWindow::~nsPrefWindow() releases null service
-    "component://netscape/rdf/xul-sort-service",  // XULSortServiceImpl releases itself in its own destructor!
-    "component://netscape/rdf/datasource?name=msgaccounts", // nsMsgAccountDataSource::QueryInterface is COMPLETELY fucked up
     "componment://netscape/intl/charsetconvertermanager", // another case where CreateInstance returned NS_OK, but the instance pointer was null!
-    "component://netscape/wallet", // WalletFactoryImpl::CreateInstance broken : calls "delete inst" then calls "NS_IF_RELEASE(inst)"
     "component://netscape/rdf/datasource?name=mail-messageview", // nsMessageViewDataSource::RemoveDataSource uses mDataSource without checking for validity
 
-    "component://netscape/rdf/datasource?name=local-store", //NS_NewLocalStore calls stuff that asserts
-    "component://netscape/rdf/datasource?name=xpinstall-update-notifier", //RDFXMLDataSourceImpl::Refresh does CreateInstance of parser but fails to check the result (I think I've seen this elsewhere)
-    "component://netscape/rdf/xul-template-builder",    //RDFXMLDataSourceImpl::Refresh does CreateInstance of parser but fails to check the result (I think I've seen this elsewhere)
-
-    "component://netscape/rdf/xul-key-listener",   // RDFFactoryImpl::CreateInstance asserts when creating if requested interface is nsISupports!
-    "component://netscape/rdf/xul-popup-listener", // RDFFactoryImpl::CreateInstance asserts when creating if requested interface is nsISupports!
-    "component://netscape/rdf/xul-focus-tracker",  // RDFFactoryImpl::CreateInstance asserts when creating if requested interface is nsISupports!
-
-    "component://netscape/messengercompose/compose",              // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service)
-    "component://netscape/messenger",                             // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service)
-    "component://netscape/rdf/datasource?name=msgaccountmanager", // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service) 
-    "component://netscape/rdf/datasource?name=mailnewsfolders",   // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service)
-    "component://netscape/rdf/datasource?name=msgnotifications",  // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service)
-    "component://netscape/rdf/datasource?name=mailnewsmessages",  // ASSERTION in nsMsgAccountManager::prefService() (needs prefs service)
 
     "component://netscape/rdf/xul-content-sink", // nsXULContentSink.cpp, XULContentSinkImpl::XULContentSinkImpl needs to init mNameSpaceManager
     "component://netscape/rdf/datasource?name=find", // FindDataSource::~FindDataSource calls gRDFService->UnregisterDataSource(this) even if it was not actaully registered.
 
-    "component://netscape/rdf/datasource?name=addresscard",      // nsAbRDFDataSource::~nsAbRDFDataSource calls mRDFService->UnregisterDataSource(this); even though it was not registered
-    "component://netscape/rdf/datasource?name=addressdirectory", // nsAbRDFDataSource::~nsAbRDFDataSource calls mRDFService->UnregisterDataSource(this); even though it was not registered
-
-    "component://netscape/rdf/datasource?name=files", // FileSystemDataSource::~FileSystemDataSource calls gRDFService->UnregisterDataSource(this); even though it was not registered
 
 
 ];
