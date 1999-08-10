cd64c3ebee4a8dd3b284339a4cb5073d3e438bcf
rhp%netscape.com <rhp%netscape.com>
1999-08-10 03:15:23 +0000

    fixing a non-intentional change

--- mailnews/compose/src/nsMsgI18N.cpp
+++ mailnews/compose/src/nsMsgI18N.cpp
@@ -187,7 +187,7 @@ char * nsMsgI18NGetDefaultMailCharset()
 	  if (NS_SUCCEEDED(res)) 
 	  {
   		//TODO: map to mail charset (e.g. Shift_JIS -> ISO-2022-JP) bug#3941.
-  		 retVal = PL_strdup("us-ascii");
+  		 retVal = prefValue;
 	  }
 	  else 
 		  retVal = PL_strdup("iso-8859-1");
