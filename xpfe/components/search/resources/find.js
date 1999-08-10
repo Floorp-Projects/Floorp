9987e3f2102d5707b7005bd08be66c20f9ea1517
rjc@netscape.com <rjc@netscape.com>
1999-08-10 06:07:53 +0000

    Now we can save find queries in bookmarks.

--- xpfe/components/search/resources/find.js
+++ xpfe/components/search/resources/find.js
@@ -1,6 +1,16 @@
 
+var	gDatasourceName = "";
+var	gMatchName = "";
+var	gMethodName = "";
+var	gTextName = "";
+
 function doFind()
 {
+	gDatasourceName = "";
+	gMatchName = "";
+	gMethodName = "";
+	gTextName = "";
+
 	// get RDF datasource to query
 	var datasourceNode = document.getElementById("datasource");
 	if (!datasourceNode)	return(false);
@@ -8,7 +18,8 @@ function doFind()
 	if (x < 0)		return(false);
 	var datasource = datasourceNode.options[x].value;
 	if (!datasource)	return(false);
-	dump("Datasource: " + datasource + "\n");
+	gDatasourceName = datasourceNode.options[x].text;
+	dump("Datasource: " + gDatasourceName + "\n");
 
 	// get match
 	var matchNode = document.getElementById("match");
@@ -17,7 +28,8 @@ function doFind()
 	if (x < 0)		return(false);
 	var match = matchNode.options[x].value;
 	if (!match)		return(false);
-	dump("Match: " + match + "\n");
+	gMatchName = matchNode.options[x].text;
+	dump("Match: " + gMatchName + "\n");
 
 	// get method
 	var methodNode = document.getElementById("method");
@@ -26,6 +38,7 @@ function doFind()
 	if (x < 0)		return(false);
 	var method = methodNode.options[x].value;
 	if (!method)		return(false);
+	gMethodName = methodNode.options[x].text;
 	dump("Method: " + method + "\n");
 
 	// get user text to find
@@ -33,6 +46,7 @@ function doFind()
 	if (!textNode)	return(false);
 	var text = textNode.value;
 	if (!text)	return(false);
+	gTextName = text;
 	dump("Find text: " + text + "\n");
 
 	// construct find URL
@@ -46,7 +60,34 @@ function doFind()
 	var resultsTree = parent.frames[1].document.getElementById("findresultstree");
 	if (!resultsTree)	return(false);
         resultsTree.setAttribute("ref", url);
+
+	// enable "Save Search" button
+	var searchButton = document.getElementById("SaveSearch");
+	if (searchButton)
+	{
+		searchButton.removeAttribute("disabled", "false");
+	}
+
 	dump("doFind done.\n");
+	return(true);
+}
+
+
+
+function saveSearch()
+{
+	var resultsTree = parent.frames[1].document.getElementById("findresultstree");
+	if (!resultsTree)	return(false);
+	var searchURL = resultsTree.getAttribute("ref");
+	if ((!searchURL) || (searchURL == ""))		return(false);
+
+	dump("Bookmark search URL: " + searchURL + "\n");
+	var searchTitle = "Search: " + gMatchName + " " + gMethodName + " '" + gTextName + "' in " + gDatasourceName;
+	dump("Title: " + searchTitle + "\n\n");
+
+	var bmks = Components.classes["component://netscape/browser/bookmarks-service"].getService();
+	if (bmks)	bmks = bmks.QueryInterface(Components.interfaces.nsIBookmarksService);
+	if (bmks)	bmks.AddBookmark(searchURL, searchTitle);
 
 	return(true);
 }
