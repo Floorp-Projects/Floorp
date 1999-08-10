af2910b895894124ec361890ed114d67727e4076
matt@netscape.com <matt@netscape.com>
1999-08-10 02:48:56 +0000

    Adding prefs in boxes

--- xpfe/components/prefwindow/resources/content/PrefsWindow.js
+++ xpfe/components/prefwindow/resources/content/PrefsWindow.js
@@ -1,3 +1,25 @@
+/* 
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *  
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *  
+ * The Original Code is Mozilla Communicator client code, released
+ * March 31, 1998.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation. Portions created by Netscape are
+ * Copyright (C) 1998-1999 Netscape Communications Corporation. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): 
+ */
+
 var prefwindow = null;
 
 function StartUp(windowName)
@@ -18,3 +40,12 @@ function StartUp(windowName)
 		prefwindow.panelLoaded(window);
 	}
 }
+
+
+ function choose() {
+    var toolkit;
+    var browser;
+         /* Use existing browser "open" logic. */
+         browser.openWindow();
+         toolkit.CloseWindow( window );
+ }
