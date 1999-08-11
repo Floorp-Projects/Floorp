a80bd0341440c0b3b172307c09b2be3f9b775f24
alecf%netscape.com <alecf%netscape.com>
1999-08-11 02:40:43 +0000

    fix account manager to load pages when people click on the appropriate category

--- mailnews/base/prefs/resources/content/AccountManager.js
+++ mailnews/base/prefs/resources/content/AccountManager.js
@@ -5,37 +5,40 @@ var pageTagArc = RDF.GetResource("http://home.netscape.com/NC-rdf#PageTag");
 
 function showPage(event) {
 
-  var node = event.target.parentNode;
+  var node = event.target.parentNode.parentNode;
   var uri = node.getAttribute('id');
 
-  var server = node.parentNode.parentNode;
+  var server = node.parentNode.parentNode.parentNode;
   var serveruri = server.getAttribute('id');
 
   var pageResource = RDF.GetResource(uri);
 
   // we have to do this extra QI because of a bug in XPConnect
-  var db = document.getElementById("accounttree").database;
+  var accounttree = window.frames["accounttree"];
+  var db = accounttree.document.getElementById("accounttree").database;
   var db2 = db.QueryInterface(Components.interfaces.nsIRDFDataSource);
 
   // convert node->literal
   var newPageNode = db2.GetTarget(pageResource, pageTagArc, true);
   var newPageResource = newPageNode.QueryInterface(Components.interfaces.nsIRDFLiteral);
-  
   var pageURI = newPageResource.Value;
-
-  var destFrame = window.parent.frames["prefframe"];
-
-  var oldLocation = new String(destFrame.location);
-  var lastSlashPos = oldLocation.lastIndexOf('/');
-
-  if (lastSlashPos >= 0) {
-    var newLocation = oldLocation.slice(0, lastSlashPos+1);
-    newLocation += pageURI;
-  }
   
-  if (oldLocation != newLocation) {
+  var destFrame = window.frames["prefframe"];
+
+  // this is a quick hack to make this work for now.
+  newLocation = pageURI;
+  
+  //  var lastSlashPos = oldLocation.lastIndexOf('/');
+  //  var lastSlashPos=0;
+  
+  //  if (lastSlashPos >= 0) {
+  //    newLocation = oldLocation.slice(0, lastSlashPos+1);
+  //    newLocation += pageURI;
+  //  }
+
+  // if (oldLocation != newLocation) {
     destFrame.location = newLocation;
-  }
+  // }
 }
 
 
