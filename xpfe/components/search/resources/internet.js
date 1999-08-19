6cc3e0e601a993f1cc1f8610b6811d215d7fb8c5
rjc%netscape.com <rjc%netscape.com>
1999-08-19 19:58:26 +0000

    Search UI tweaks.

--- xpfe/components/search/resources/internet.js
+++ xpfe/components/search/resources/internet.js
@@ -2,6 +2,166 @@
 var	gText = "";
 var	gSites = "";
 
+
+
+function loadPage()
+{
+	try
+	{
+		var rdf = Components.classes["component://netscape/rdf/rdf-service"].getService();
+		if (rdf)   rdf = rdf.QueryInterface(Components.interfaces.nsIRDFService);
+		if (rdf)
+		{
+			var localStore = rdf.GetDataSource("rdf:local-store");
+			if (localStore)
+			{
+				// XXX activate last selected list of search engines
+				var treeNode = document.getElementById("NC:SearchEngineRoot");
+				var treeChildrenNode = null;
+				var numChildren = treeNode.childNodes.length;
+				for (var x = 0; x<numChildren; x++)
+				{
+					if (treeNode.childNodes[x].tagName == "treechildren")
+					{
+						treeChildrenNode = treeNode.childNodes[x];
+						break;
+					}
+				}
+				if (treeChildrenNode != null)
+				{
+					var numEngines = treeChildrenNode.childNodes.length;
+					dump("LoadPage(): " + numEngines + " engines.\n");
+
+					var checkedProperty = rdf.GetResource("http://home.netscape.com/NC-rdf#checked", true);
+
+					for (var x = 0; x<numEngines; x++)
+					{
+						var treeItem = treeChildrenNode.childNodes[x];
+						if (!treeItem)	continue;
+
+						var engineURI = treeItem.getAttribute("id");
+						var src = rdf.GetResource(engineURI, true);
+						var target = localStore.GetTarget(src, checkedProperty, true);
+						if (target)	target = target.QueryInterface(Components.interfaces.nsIRDFLiteral);
+						if (target)	target = target.Value;
+						if (target == "true")
+						{
+							dump("loadPage(): Checking '" + engineURI + "\n");
+							treeItem.childNodes[0].childNodes[0].childNodes[0].checked = true;
+							treeItem.childNodes[0].childNodes[0].childNodes[0].setAttribute("checked", "1");
+						}
+					}
+				}
+			}
+		}
+	}
+	catch(ex)
+	{
+		dump("\nloadPage(): Exception.\n");
+	}
+
+	// check and see if we need to do an automatic search
+	if (window.parent)
+	{
+		var searchText = window.parent.getSearchText();
+		if (searchText)
+		{
+			var textNode = document.getElementById("searchtext");
+			if (textNode)
+			{
+				textNode.setAttribute("value", searchText);
+				doSearch();
+			}
+		}
+	}
+}
+
+
+
+function unloadPage()
+{
+	try
+	{
+		var rdf = Components.classes["component://netscape/rdf/rdf-service"].getService();
+		if (rdf)   rdf = rdf.QueryInterface(Components.interfaces.nsIRDFService);
+		if (rdf)
+		{
+			var localStore = rdf.GetDataSource("rdf:local-store");
+			if (localStore)
+			{
+				// remember last selected list of search engines
+				var treeNode = document.getElementById("NC:SearchEngineRoot");
+				var treeChildrenNode = null;
+				var numChildren = treeNode.childNodes.length;
+				for (var x = 0; x<numChildren; x++)
+				{
+					if (treeNode.childNodes[x].tagName == "treechildren")
+					{
+						treeChildrenNode = treeNode.childNodes[x];
+						break;
+					}
+				}
+				if (treeChildrenNode != null)
+				{
+					var numEngines = treeChildrenNode.childNodes.length;
+					dump("unloadPage(): " + numEngines + " engines.\n");
+
+					var checkedProperty = rdf.GetResource("http://home.netscape.com/NC-rdf#checked", true);
+					var trueLiteral = rdf.GetLiteral("true");
+					var checkedFlag = false;
+
+					for (var x = 0; x<numEngines; x++)
+					{
+						var treeItem = treeChildrenNode.childNodes[x];
+						if (!treeItem)
+						{
+							dump("unloadPage(): Huh? treeitem is null.\n");
+							continue;
+						}
+						
+						checkedFlag = false;
+
+						if (treeItem.childNodes[0].childNodes[0].childNodes[0].checked == true)
+						{
+							checkedFlag = true;
+						}
+						else if (treeItem.childNodes[0].childNodes[0].childNodes[0].getAttribute("checked") == "1")
+						{
+							checkedFlag = true;
+						}
+
+						var engineURI = treeItem.getAttribute("id");
+						var src = rdf.GetResource(engineURI, true);
+						if (checkedFlag == true)
+						{
+							dump("unloadPage(): Saving '" + engineURI + "'\n");
+							localStore.Assert(src, checkedProperty, trueLiteral, true);
+						}
+						else
+						{
+							// unassert it just in case its in the localstore, as we don't want to remember it
+							localStore.Unassert(src, checkedProperty, trueLiteral, true);
+						}
+					}
+
+					// XXX for now, need to force a flush
+					flushableStore = localStore.QueryInterface(Components.interfaces.nsIRDFRemoteDataSource);
+					if (flushableStore)
+					{
+						flushableStore.Flush();
+					}
+				}
+			}
+		}
+	}
+	catch(ex)
+	{
+		dump("\nunloadPage(): Exception.\n");
+	}
+}
+
+
+
 function doSearch()
 {
 	gText = "";
@@ -35,18 +195,36 @@ function doSearch()
 	var foundEngine = false;
 
 	var numEngines = treeChildrenNode.childNodes.length;
-	dump("Found treebody, it has " + numEngines + " kids\n");
+	dump("doSearch(): " + numEngines + " engines.\n");
+
 	for (var x = 0; x<numEngines; x++)
 	{
+		dump("Engine: #" + x + "\n");
+
 		var treeItem = treeChildrenNode.childNodes[x];
-		if (!treeItem)	continue;
-		// XXX when its fully implemented, instead use
-		//     var engines = document.getElementsByTagName("checkbox");
-		if (treeItem.childNodes[0].childNodes[0].childNodes[0].getAttribute("value") == "1")
+		if (!treeItem)
+		{
+			dump("doSearch(): huh? treeitem is null.\n");
+			continue;
+		}
+
+		var checkedFlag = false;
+
+		if (treeItem.childNodes[0].childNodes[0].childNodes[0].checked == true)
+		{
+			checkedFlag = true;
+		}
+		else if (treeItem.childNodes[0].childNodes[0].childNodes[0].getAttribute("checked") == "1")
+		{
+			checkedFlag = true;
+		}
+
+		if (checkedFlag == true)
 		{
 			var engineURI = treeItem.getAttribute("id");
 			if (!engineURI)	continue;
-			dump ("# " + x + ":  " + engineURI + "\n");
+
+			dump ("doSearch(): Checked: # " + x + ":  " + engineURI + "\n");
 
 			var searchEngineName = treeItem.childNodes[0].childNodes[1].childNodes[0].getAttribute("value");
 			if (searchEngineName != "")
@@ -73,7 +251,7 @@ function doSearch()
 	if (foundEngine == false)	return(false);
 
 	searchURL += "&text=" + escape(text);
-	dump("Internet Search URL: " + searchURL + "\n");
+	dump("\nInternet Search URL: " + searchURL + "\n");
 
 	// set text in results pane
 	var summaryNode = parent.frames[1].document.getElementById("internetresultssummary");
@@ -87,7 +265,7 @@ function doSearch()
 	// load find URL into results pane
 	var resultsTree = parent.frames[1].document.getElementById("internetresultstree");
 	if (!resultsTree)	return(false);
-        resultsTree.setAttribute("ref", searchURL);
+	resultsTree.setAttribute("ref", searchURL);
 
 	// enable "Save Search" button
 	var searchButton = document.getElementById("SaveSearch");
@@ -101,7 +279,28 @@ function doSearch()
 	return(true);
 }
 
-function doUncheckAll()
+
+
+function doCheck(node)
+{
+	/* XXX due to a bug, on a mouse-click we also set the
+	       checked attribute in the DOM so that it persists */
+
+	if (node.checked == true)
+	{
+		node.setAttribute("checked", "1");
+	}
+	else
+	{
+		node.removeAttribute("checked");
+//		node.setAttribute("checked", "0");
+	}
+	return(false);
+}
+
+
+
+function doCheckAll(activeFlag)
 {
 	// get selected search engines
 	var treeNode = document.getElementById("NC:SearchEngineRoot");
@@ -119,20 +318,44 @@ function doUncheckAll()
 	if (treeChildrenNode == null)	return(false);
 
 	var numEngines = treeChildrenNode.childNodes.length;
-	dump("Found treebody, it has " + numEngines + " kids\n");
+	dump("doCheckAll():  " + numEngines + " engines.\n");
+
 	for (var x = 0; x<numEngines; x++)
 	{
 		var treeItem = treeChildrenNode.childNodes[x];
-		if (!treeItem)	continue;
-		// XXX when its fully implemented, instead use
-		//     var engines = document.getElementsByTagName("checkbox");
-		if (treeItem.childNodes[0].childNodes[0].childNodes[0].getAttribute("value") == "1")
+		if (!treeItem)
 		{
-			treeItem.childNodes[0].childNodes[0].childNodes[0].setAttribute("value", "0");
+			dump("doCheckAll(): huh? treeItem is null.\n");
+			continue;
+		}
+
+		var checkedFlag = false;
+
+		if (treeItem.childNodes[0].childNodes[0].childNodes[0].checked == true)
+		{
+			checkedFlag = true;
+		}
+		else if (treeItem.childNodes[0].childNodes[0].childNodes[0].getAttribute("checked") == "1")
+		{
+			checkedFlag = true;
+		}
+
+		if (checkedFlag != activeFlag)
+		{
+			treeItem.childNodes[0].childNodes[0].childNodes[0].checked = activeFlag;
+			if (activeFlag)
+			{
+				treeItem.childNodes[0].childNodes[0].childNodes[0].setAttribute("checked", "1");
+			}
+			else
+			{
+				treeItem.childNodes[0].childNodes[0].childNodes[0].removeAttribute("checked");
+//				treeItem.childNodes[0].childNodes[0].childNodes[0].setAttribute("checked", "0");
+			}
 		}
 	}
 
-	dump("doUncheckAll() done.\n");
+	dump("doCheckAll() done.\n");
 
 	return(true);
 }
