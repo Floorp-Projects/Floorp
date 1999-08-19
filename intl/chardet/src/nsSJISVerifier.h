afca1a857f5085830d3f78da0279c40e70590f30
ftang@netscape.com <ftang@netscape.com>
1999-08-19 21:11:07 +0000

    fix euc-jp detection bugs by remove 0x85,0x86,0xeb,0xec from the legal char list. change size of array  which won't break build

--- intl/chardet/src/nsSJISVerifier.h
+++ intl/chardet/src/nsSJISVerifier.h
@@ -40,7 +40,7 @@ PCK4BITS(2,2,2,2,2,2,2,2),  // 60 - 67
 PCK4BITS(2,2,2,2,2,2,2,2),  // 68 - 6f 
 PCK4BITS(2,2,2,2,2,2,2,2),  // 70 - 77 
 PCK4BITS(2,2,2,2,2,2,2,1),  // 78 - 7f 
-PCK4BITS(3,3,3,3,3,0,0,3),  // 80 - 87 
+PCK4BITS(3,3,3,3,3,5,5,3),  // 80 - 87 
 PCK4BITS(3,3,3,3,3,3,3,3),  // 88 - 8f 
 PCK4BITS(3,3,3,3,3,3,3,3),  // 90 - 97 
 PCK4BITS(3,3,3,3,3,3,3,3),  // 98 - 9f 
@@ -53,7 +53,7 @@ PCK4BITS(2,2,2,2,2,2,2,2),  // c8 - cf
 PCK4BITS(2,2,2,2,2,2,2,2),  // d0 - d7 
 PCK4BITS(2,2,2,2,2,2,2,2),  // d8 - df 
 PCK4BITS(3,3,3,3,3,3,3,3),  // e0 - e7 
-PCK4BITS(3,3,3,0,0,3,3,3),  // e8 - ef 
+PCK4BITS(3,3,3,5,5,3,3,3),  // e8 - ef 
 PCK4BITS(3,3,3,3,3,3,3,3),  // f0 - f7 
 PCK4BITS(3,3,3,3,3,0,0,0)   // f8 - ff 
 };
@@ -61,8 +61,8 @@ PCK4BITS(3,3,3,3,3,0,0,0)   // f8 - ff
 
 static PRUint32 SJIS_st [ 3] = {
 PCK4BITS(eError,eStart,eStart,     3,eError,eError,eError,eError),//00-07 
-PCK4BITS(eError,eError,eItsMe,eItsMe,eItsMe,eItsMe,eItsMe,eError),//08-0f 
-PCK4BITS(eError,eStart,eStart,eStart,eStart,eStart,eStart,eStart) //10-17 
+PCK4BITS(eError,eError,eError,eError,eItsMe,eItsMe,eItsMe,eItsMe),//08-0f 
+PCK4BITS(eItsMe,eItsMe,eError,eError,eStart,eStart,eStart,eStart) //10-17 
 };
 
 
@@ -75,7 +75,7 @@ static nsVerifier nsSJISVerifier = {
        eUnitMsk4bits, 
        SJIS_cls 
     },
-    5,
+    6,
     {
        eIdxSft4bits, 
        eSftMsk4bits, 
