41f203ce4002a3f87047eeba176f62beb27bcf2b
mgleeson1@netscape.com <mgleeson1@netscape.com>
1999-08-19 20:36:06 +0000

    HP-UX additions

---
+++ xpcom/reflect/xptcall/src/md/unix/xptcinvoke_pa32.cpp
@@ -0,0 +1,161 @@
+
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1999 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "xptcprivate.h"
+
+#if _HPUX
+#error "This code is for HP-PA RISC 32 bit mode only"
+#endif
+
+#include <alloca.h>
+
+typedef unsigned nsXPCVariant;
+
+extern "C" PRInt32
+invoke_count_bytes(nsISupports* that,
+  const PRUint32 paramCount, const nsXPTCVariant* s)
+{
+  PRInt32 result = 4; /* variant records do not include self pointer */
+
+  /* counts the number of bytes required by the argument stack,
+     64 bit integer, and double requires 8 bytes.  All else requires
+     4 bytes.
+   */
+
+  {
+    PRUint32 indx;
+    for (indx = paramCount; indx > 0; --indx, ++s)
+    {
+      if (! s->IsPtrData())
+      {
+        if (s->type == nsXPTType::T_I64 || s->type == nsXPTType::T_U64 ||
+            s->type == nsXPTType::T_DOUBLE)
+        {
+          /* 64 bit integer and double aligned on 8 byte boundaries */
+          result += (result & 4) + 8;
+          continue;
+        }
+      }
+      result += 4; /* all other cases use 4 bytes */
+    }
+  }
+  result -= 72; /* existing stack buffer is 72 bytes */
+  if (result < 0)
+    return 0;
+  {
+    /* round up to 64 bytes boundary */
+    PRInt32 remainder = result & 63;
+    return (remainder == 0) ? result : (result + 64 - remainder);
+  }
+}
+
+extern "C" PRUint32
+invoke_copy_to_stack(PRUint32* d,
+  const PRUint32 paramCount, nsXPTCVariant* s)
+{
+
+  typedef struct
+  {
+    PRUint32 hi;
+    PRUint32 lo;
+  } DU;
+
+  PRUint32* dest = d;
+  nsXPTCVariant* source = s;
+  /* we clobber param vars by copying stuff on stack, have to use local var */
+
+  PRUint32 floatflags = 0;
+  /* flag indicating which floating point registers to load */
+
+  PRUint32 regwords = 1; /* register 26 is reserved for ptr to 'that' */
+  PRUint32 indx;
+
+  for (indx = paramCount; indx > 0; --indx, --dest, ++source)
+  {
+    if (source->IsPtrData())
+    {
+      *((void**) dest) = source->ptr;
+      ++regwords;
+      continue;
+    }
+    switch (source->type)
+    {
+    case nsXPTType::T_I8    : *((PRInt32*) dest) = source->val.i8;  break;
+    case nsXPTType::T_I16   : *((PRInt32*) dest) = source->val.i16; break;
+    case nsXPTType::T_I32   : *((PRInt32*) dest) = source->val.i32; break;
+    case nsXPTType::T_I64   :
+    case nsXPTType::T_U64   :
+      if (regwords & 1)
+      {
+        /* align on double word boundary */
+        --dest;
+        ++regwords;
+      }
+      *((uint32*) dest) = ((DU *) source)->lo;
+      *((uint32*) --dest) = ((DU *) source)->hi;
+      /* big endian - hi word in low addr */
+      regwords += 2;
+      continue;
+    case nsXPTType::T_DOUBLE :
+      if (regwords & 1)
+      {
+        /* align on double word boundary */
+        --dest;
+        ++regwords;
+      }
+      switch (regwords) /* load double precision float register */
+      {
+      case 2:
+        floatflags |= 1;
+      }
+      *((uint32*) dest) = ((DU *) source)->lo;
+      *((uint32*) --dest) = ((DU *) source)->hi;
+      /* big endian - hi word in low addr */
+      regwords += 2;
+      continue;
+    case nsXPTType::T_FLOAT :
+      switch (regwords) /* load single precision float register */
+      {
+      case 1:
+        floatflags |= 2;
+        break;
+      case 2:
+        floatflags |= 4;
+        break;
+      case 3:
+        floatflags |= 8;
+      }
+      *((float*) dest) = source->val.f;
+      break;
+    case nsXPTType::T_U8    : *((PRUint32*) (dest)) = source->val.u8; break;
+    case nsXPTType::T_U16   : *((PRUint32*) (dest)) = source->val.u16; break;
+    case nsXPTType::T_U32   : *((PRUint32*) (dest)) = source->val.u32; break;
+    case nsXPTType::T_BOOL  : *((PRBool*)   (dest)) = source->val.b; break;
+    case nsXPTType::T_CHAR  : *((PRUint32*) (dest)) = source->val.c; break;
+    case nsXPTType::T_WCHAR : *((PRInt32*)  (dest)) = source->val.wc; break;
+
+    default:
+      // all the others are plain pointer types
+      *((void**) dest) = source->val.p;
+    }
+    ++regwords;
+  }
+  return floatflags;
+}
+
---
+++ xpcom/reflect/xptcall/src/md/unix/xptcstubs_pa32.cpp
@@ -0,0 +1,156 @@
+
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1999 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/* Implement shared vtbl methods. */
+
+#include "xptcprivate.h"
+
+#if _HPUX
+#error "This code is for HP-PA RISC 32 bit mode only"
+#endif
+
+extern "C" nsresult
+PrepareAndDispatch(nsXPTCStubBase* self, PRUint32 methodIndex,
+  PRUint32* args, PRUint32* floatargs)
+{
+
+  typedef struct {
+    uint32 hi;
+    uint32 lo;
+  } DU;
+
+#define PARAM_BUFFER_COUNT     16
+
+  nsXPTCMiniVariant paramBuffer[PARAM_BUFFER_COUNT];
+  nsXPTCMiniVariant* dispatchParams = NULL;
+  nsIInterfaceInfo* iface_info = NULL;
+  const nsXPTMethodInfo* info;
+  PRInt32 regwords = 1; /* self pointer is not in the variant records */
+  nsresult result = NS_ERROR_FAILURE;
+  PRUint8 paramCount;
+  PRUint8 i;
+
+  NS_ASSERTION(self,"no self");
+
+  self->GetInterfaceInfo(&iface_info);
+  NS_ASSERTION(iface_info,"no interface info");
+
+  iface_info->GetMethodInfo(PRUint16(methodIndex), &info);
+  NS_ASSERTION(info,"no interface info");
+
+  paramCount = info->GetParamCount();
+
+  // setup variant array pointer
+  if(paramCount > PARAM_BUFFER_COUNT)
+    dispatchParams = new nsXPTCMiniVariant[paramCount];
+  else
+    dispatchParams = paramBuffer;
+  NS_ASSERTION(dispatchParams,"no place for params");
+
+  for(i = 0; i < paramCount; ++i, --args)
+  {
+    const nsXPTParamInfo& param = info->GetParam(i);
+    const nsXPTType& type = param.GetType();
+    nsXPTCMiniVariant* dp = &dispatchParams[i];
+
+    if(param.IsOut() || !type.IsArithmetic())
+    {
+      dp->val.p = (void*) *args;
+      ++regwords;
+      continue;
+    }
+    switch(type)
+    {
+    case nsXPTType::T_I8     : dp->val.i8  = *((PRInt32*) args); break;
+    case nsXPTType::T_I16    : dp->val.i16 = *((PRInt32*) args); break;
+    case nsXPTType::T_I32    : dp->val.i32 = *((PRInt32*) args); break;
+    case nsXPTType::T_DOUBLE :
+                               if (regwords & 1)
+                               {
+                                 ++regwords; /* align on double word */
+                                 --args;
+                               }
+                               if (regwords == 0 || regwords == 2)
+                               {
+                                 dp->val.d=*((double*) (floatargs + regwords));
+                                 --args;
+                               }
+                               else
+                               {
+                                 dp->val.d = *((double*) --args);
+                               }
+                               regwords += 2;
+                               continue;
+    case nsXPTType::T_U64    :
+    case nsXPTType::T_I64    :
+                               if (regwords & 1)
+                               {
+                                 ++regwords; /* align on double word */
+                                 --args;
+                               }
+                               ((DU *)dp)->lo = *((PRUint32*) args);
+                               ((DU *)dp)->hi = *((PRUint32*) --args);
+                               regwords += 2;
+                               continue;
+    case nsXPTType::T_FLOAT  :
+                               if (regwords >= 4)
+                                 dp->val.f = *((float*) args);
+                               else
+                                 dp->val.f = *((float*) floatargs+4+regwords);
+                               break;
+    case nsXPTType::T_U8     : dp->val.u8  = *((PRUint32*) args); break;
+    case nsXPTType::T_U16    : dp->val.u16 = *((PRUint32*) args); break;
+    case nsXPTType::T_U32    : dp->val.u32 = *((PRUint32*) args); break;
+    case nsXPTType::T_BOOL   : dp->val.b   = *((PRBool*)   args); break;
+    case nsXPTType::T_CHAR   : dp->val.c   = *((PRUint32*) args); break;
+    case nsXPTType::T_WCHAR  : dp->val.wc  = *((PRInt32*)  args); break;
+    default:
+      NS_ASSERTION(0, "bad type");
+      break;
+    }
+    ++regwords;
+  }
+
+  result = self->CallMethod((PRUint16) methodIndex, info, dispatchParams);
+
+  NS_RELEASE(iface_info);
+
+  if(dispatchParams != paramBuffer)
+    delete [] dispatchParams;
+
+  return result;
+}
+
+extern "C" int SharedStub(int);
+
+#define STUB_ENTRY(n)       \
+nsresult nsXPTCStubBase::Stub##n()  \
+{                           \
+    return SharedStub(n);   \
+}
+
+#define SENTINEL_ENTRY(n) \
+nsresult nsXPTCStubBase::Sentinel##n() \
+{ \
+    NS_ASSERTION(0,"nsXPTCStubBase::Sentinel called"); \
+    return NS_ERROR_NOT_IMPLEMENTED; \
+}
+
+#include "xptcstubsdef.inc"
+
