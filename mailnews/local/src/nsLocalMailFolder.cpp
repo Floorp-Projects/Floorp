c3875f14b92bcc7be990632fa40dd2cb7a2c4ae3
alecf%netscape.com <alecf%netscape.com>
1999-08-20 07:23:20 +0000

    fix reloading of accounts by precaching each folder's path (this prevents GetPath from calling nsLocalURI2Path, which reloads the accountmanager)

--- mailnews/local/src/nsLocalMailFolder.cpp
+++ mailnews/local/src/nsLocalMailFolder.cpp
@@ -104,6 +104,11 @@ nsMsgLocalMailFolder::nsMsgLocalMailFolder(void)
     mInitialized(PR_FALSE), mCopyState(nsnull)
 {
 	mPath = nsnull;
+  
+  // temp hack to precache the local path
+  nsCOMPtr<nsIFileSpec> path;
+  GetPath(getter_AddRefs(path));
+  
 //  NS_INIT_REFCNT(); done by superclass
 }
 
