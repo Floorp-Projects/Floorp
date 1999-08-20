58311478a10cb13fb0b7fe7df204c60d61fccda1
alecf%netscape.com <alecf%netscape.com>
1999-08-20 02:38:02 +0000

    updates to account manager now create a <deck> for each server, and put an <iframe> in each one

--- mailnews/base/prefs/resources/content/AccountManager.js
+++ mailnews/base/prefs/resources/content/AccountManager.js
@@ -5,40 +5,55 @@ var pageTagArc = RDF.GetResource("http://home.netscape.com/NC-rdf#PageTag");
 
 function showPage(event) {
 
+  // the url to load is stored in the PageTag attribute
+  // of the current treeitem
   var node = event.target.parentNode.parentNode;
-  var uri = node.getAttribute('id');
 
-  var server = node.parentNode.parentNode.parentNode;
-  var serveruri = server.getAttribute('id');
-
-  var pageResource = RDF.GetResource(uri);
-
-  // we have to do this extra QI because of a bug in XPConnect
-  var accounttree = window.frames["accounttree"];
-  var db = accounttree.document.getElementById("accounttree").database;
-  var db2 = db.QueryInterface(Components.interfaces.nsIRDFDataSource);
-
-  // convert node->literal
-  var newPageNode = db2.GetTarget(pageResource, pageTagArc, true);
-  var newPageResource = newPageNode.QueryInterface(Components.interfaces.nsIRDFLiteral);
-  var pageURI = newPageResource.Value;
+  if (node.tagName != "treeitem") return;
   
-  var destFrame = window.frames["prefframe"];
+  var pageUrl = node.getAttribute('PageTag');
+
+  // the server's URI the parent treeitem
+  var servernode = node.parentNode.parentNode;
+
+  // for toplevel treeitems, we just use the current treeitem
+  if (servernode.tagName != "treeitem") {
+    servernode = node;
+  }
+  var serveruri = servernode.getAttribute('id');
+
+  // find the IFRAME that corresponds to this server
+  var destFrame = top.window.frames[serveruri];
+
+  var newLocation="";
+  var oldLocation = new String(destFrame.location);
 
   // this is a quick hack to make this work for now.
-  newLocation = pageURI;
-  
-  //  var lastSlashPos = oldLocation.lastIndexOf('/');
-  //  var lastSlashPos=0;
-  
-  //  if (lastSlashPos >= 0) {
-  //    newLocation = oldLocation.slice(0, lastSlashPos+1);
-  //    newLocation += pageURI;
-  //  }
+  //  newLocation = pageURI;
 
-  // if (oldLocation != newLocation) {
+  var lastSlashPos = oldLocation.lastIndexOf('/');
+  
+  if (lastSlashPos >= 0) {
+    newLocation = oldLocation.slice(0, lastSlashPos+1);
+  }
+  newLocation += pageUrl;
+  
+  if (oldLocation != newLocation) {
     destFrame.location = newLocation;
-  // }
+  }
+
+  // now see if we need to bring this server into view
+
+  // find the box's index in the <deck> and bring it forward
+  var deckBox= top.document.getElementById(serveruri);
+  var deck = deckBox.parentNode;
+  var children = deck.childNodes;
+  var i;
+  for (i=0; i<children.length; i++) {
+    if (children[i] == deckBox) break;
+  }
+
+  deck.setAttribute("value", i-1);
 }
 
 
