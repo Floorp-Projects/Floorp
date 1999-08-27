dd1a65aa4c75475f8c4cb1de540a1d484c608ef5
alecf%netscape.com <alecf%netscape.com>
1999-08-27 08:01:19 +0000

    updated account manager - prepare to prepopulate pages

--- mailnews/base/prefs/resources/content/AccountManager.js
+++ mailnews/base/prefs/resources/content/AccountManager.js
@@ -1,29 +1,78 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * The Original Code is Mozilla Communicator client code, released
+ * March 31, 1998.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation. Portions created by Netscape are
+ * Copyright (C) 1998-1999 Netscape Communications Corporation. All
+ * Rights Reserved.
+ */
 
-var RDF = Components.classes['component://netscape/rdf/rdf-service'].getService();
-RDF = RDF.QueryInterface(Components.interfaces.nsIRDFService);
-var pageTagArc = RDF.GetResource("http://home.netscape.com/NC-rdf#PageTag");
+var serverArray;
 
-function showPage(event) {
+var accounts;
+// called when the whole document loads
+function onLoad() {
+  
+  serverArray = new Array;
 
-  // the url to load is stored in the PageTag attribute
-  // of the current treeitem
+  var mailsession =
+    Components.classes["component://netscape/messenger/services/session"].getService(Components.interfaces.nsIMsgMailSession);
+
+  var am = mailsession.accountManager;
+  accounts = am.accounts;
+}
+
+// called when a prefs page is done loading
+function onPageLoad(event, name) {
+  // page needs to be filled with values.
+  // how do we determine which account we should be using?
+  dump("loaded " + name + "\n");
+}
+
+// called when someone clicks on a 
+function onAccountClick(event) {
+
+  // get the page to load
+  // (stored in the PageTag attribute of this node)
   var node = event.target.parentNode.parentNode;
-
   if (node.tagName != "treeitem") return;
   
   var pageUrl = node.getAttribute('PageTag');
 
-  // the server's URI the parent treeitem
-  var servernode = node.parentNode.parentNode;
 
+  // get the server's ID
+  // (stored in the ID attribute of the server node)
+  var servernode = node.parentNode.parentNode;
+  
   // for toplevel treeitems, we just use the current treeitem
+  dump("servernode is " + servernode + "\n");
   if (servernode.tagName != "treeitem") {
     servernode = node;
   }
-  var serveruri = servernode.getAttribute('id');
+  var serverid = servernode.getAttribute('id');
+  
+  dump("before showPage(" + serverid + "," + pageUrl + ");\n");
+  showPage(serverid, pageUrl);
 
+}
+// load pageUrl in the deck for the given server
+
+function showPage(serverid, pageUrl) {
+
+  dump("showPage(" + serverid + "," + pageUrl + ");\n");
   // find the IFRAME that corresponds to this server
-  var destFrame = top.window.frames[serveruri];
+  var destFrame = top.window.frames[serverid];
 
   var newLocation="";
   var oldLocation = new String(destFrame.location);
@@ -45,7 +94,7 @@ function showPage(event) {
   // now see if we need to bring this server into view
 
   // find the box's index in the <deck> and bring it forward
-  var deckBox= top.document.getElementById(serveruri);
+  var deckBox= top.document.getElementById(serverid);
   var deck = deckBox.parentNode;
   var children = deck.childNodes;
   var i;
@@ -55,3 +104,5 @@ function showPage(event) {
 
   deck.setAttribute("value", i-1);
 }
+
+
