0c51b40f8a51ed447c2bd418d8904bb738b898f9
brade@netscape.com <brade@netscape.com>
1999-08-31 12:52:50 +0000

    View Source (contribution by Sammy Ford); fix year for copyright

--- editor/ui/composer/content/EditorCommands.js
+++ editor/ui/composer/content/EditorCommands.js
@@ -12,12 +12,17 @@
  *
  * The Initial Developer of this code under the NPL is Netscape
  * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Copyright (C) 1998-1999 Netscape Communications Corporation.  All Rights
  * Reserved.
+ *
+ * Contributors:
+ *    Sammy Ford
  */
 
 /* Main Composer window UI control */
 
+var EdAppCore = null;
+var EdAppCoreName = "";
 var toolbar;
 var documentModified;
 var EditorDisplayStyle = true;
@@ -356,6 +361,23 @@ function EditorShowClipboard()
 }
 
 // --------------------------- View menu ---------------------------
+
+function EditorViewSource() 
+{
+   var toolkitCore = XPAppCoresManager.Find("ToolkitCore");
+   if (!toolkitCore) {
+     toolkitCore = new ToolkitCore();
+     if (toolkitCore) {
+       toolkitCore.Init("ToolkitCore");
+     }
+   }
+   if (toolkitCore) {
+     var url = window.content.location;
+     dump("Opening view of source for" + url + "\n");
+     toolkitCore.ShowWindowWithArgs("chrome://editor/content/viewSource.xul", window, url);
+   }
+}
+
 
 function EditorSetDocumentCharacterSet(aCharset)
 {
