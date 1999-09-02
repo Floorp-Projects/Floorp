2338164ba2049fc3a6b48d33e27f8e54312cf5b5
law%netscape.com <law%netscape.com>
1999-09-02 04:08:22 +0000

    Cleanup so it can be used with window.openDialog

--- xpfe/browser/resources/content/viewsource.js
+++ xpfe/browser/resources/content/viewsource.js
@@ -1,17 +1,41 @@
-  function StartupViewSource() {
-    // Generate unique name (var appCoreName declared in navigator.js).
-    appCoreName = "ViewSource." + ( new Date() ).getTime().toString();
-    
-    // Create and initialize the browser app core.
-    appCore = new BrowserAppCore();
-    appCore.Init( appCoreName );
-    appCore.setContentWindow(window.frames[0]);
-    appCore.setWebShellWindow(window);
-    appCore.setToolbarWindow(window);
-    
-    // Get url whose source to view.
-    var url = document.getElementById("args").getAttribute("value");
+  function onLoadViewSource() {
+    if ( !useOldAppCore ) {
+        StartupViewSource();
+    }
+  }
 
-    // Load the source (the app core will magically know what to do).
-    appCore.loadUrl(url);
+  function StartupViewSource() {
+    if ( useOldAppCore ) {
+        // Generate unique name (var appCoreName declared in navigator.js).
+        appCoreName = "ViewSource." + ( new Date() ).getTime().toString();
+    
+        // Create and initialize the browser app core.
+        appCore = new BrowserAppCore();
+        appCore.Init( appCoreName );
+        appCore.setContentWindow(window.frames[0]);
+        appCore.setWebShellWindow(window);
+        appCore.setToolbarWindow(window);
+    
+        // Get url whose source to view.
+        var url = document.getElementById("args").getAttribute("value");
+    
+        // Load the source (the app core will magically know what to do).
+        appCore.loadUrl(url);
+    } else {
+        // Create and initialize the browser instance.
+        createBrowserInstance();
+    
+        if ( appCore ) {
+            appCore.isViewSource = true;
+            appCore.setContentWindow(window.frames[0]);
+            appCore.setWebShellWindow(window);
+            appCore.setToolbarWindow(window);
+        }
+        
+        // Get url whose source to view.
+        var url = window.arguments[0];
+    
+        // Load the source (the app core will magically know what to do).
+        appCore.loadUrl( url );
+    }
   }
