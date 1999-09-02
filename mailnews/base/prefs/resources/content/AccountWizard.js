eb182b184dbee4e27074ba42b2e7d7c39a944376
alecf%netscape.com <alecf%netscape.com>
1999-09-02 21:48:26 +0000

    account wizard now creates new accounts (first rough cut)

--- mailnews/base/prefs/resources/content/AccountWizard.js
+++ mailnews/base/prefs/resources/content/AccountWizard.js
@@ -104,6 +104,8 @@ function nextPage(win) {
     dump("Loading " + getUrlFromTag(nextPageTag) + "\n");
     if (nextPageTag)
         win.location=getUrlFromTag(nextPageTag);
+    else
+        onFinish();
 }
 
 function previousPage(win) {
@@ -113,13 +115,12 @@ function previousPage(win) {
 }
 
 function initializePage(win, hash) {
-    var doc = win.document;
-    for (var i in hash) {
-        var formElement=doc.getElementById(i);
-        if (formElement) {
-            formElement.value = hash[i];
-        }
-
+    var inputs= win.document.getElementsByTagName("FORM")[0].elements;
+    dump("initialize: There are " + inputs.length + " input tags\n");
+    for (var i=0; i<inputs.length; i++) {
+        dump("restoring input " + i + "\n");
+        restoreValue(hash, inputs[i]);
+        dump("done.\n");
     }
 }
 
@@ -127,14 +128,49 @@ function initializePage(win, hash) {
 function saveContents(win, hash) {
 
     var inputs = win.document.getElementsByTagName("FORM")[0].elements;
-    dump("There are " + inputs.length + " input tags\n");
+    dump("save: There are " + inputs.length + " input tags\n");
     for (var i=0 ; i<inputs.length; i++) {
-        dump("Saving: ");
-        dump("ID=" + inputs[i].name + " Value=" + inputs[i].value + "..");
-        hash[inputs[i].name] = inputs[i].value;
-        dump("done.\n");
+        saveValue(hash, inputs[i])
     }
 
+}
+
+function restoreValue(hash, element) {
+    if (!hash[element.name]) return;
+    dump("Restoring " + element.name + "...");
+    if (element.type=="radio") {
+        if (hash[element.name] == element.value)
+            element.checked=true;
+        else
+            element.checked=false;
+        dump(element.checked);
+    } else if (element.type=="checkbox") {
+        element.checked=hash[element.name];
+        dump(element.checked);
+    } else {
+        element.value=hash[element.name];
+        dump(element.value);
+    }
+    dump("\n");
+}
+
+function saveValue(hash, element) {
+
+    dump("Saving " + element.name + "...");
+    if (element.type=="radio") {
+        if (element.checked) {
+            hash[element.name] = element.value;
+            dump(element.value);
+        }
+    } else if (element.type == "checkbox") {
+        hash[element.name] = element.checked;
+        dump(element.checked);
+    }
+    else {
+        hash[element.name] = element.value;
+        dump(element.value);
+    }
+    dump("\n");
 }
 
 function validateIdentity(win, hash) {
@@ -156,11 +192,45 @@ function validateSmtp(win, hash) {
     return true;
 }
 
-function onFinish(event) {
+function onFinish() {
     var i;
     dump("There are " + wizardContents.length + " elements\n");
     for (i in wizardContents) {
         dump("wizardContents[" + i + "] = " + wizardContents[i] + "\n");
     }
-    
+    createAccount(wizardContents);
 }
+
+function createAccount(hash) {
+
+    var mail = Components.classes["component://netscape/messenger/services/session"].getService(Components.interfaces.nsIMsgMailSession);
+    var am = mail.accountManager;
+
+    var account = am.createAccount();
+
+    dump("Created account " + account.key + "\n");
+
+    var server = am.createIncomingServer(hash["server.type"]);
+
+    dump("Created server " + server.key + "\n");
+    
+    var identity = am.createIdentity();
+
+    dump("Created identity " + identity.key + "\n");
+
+    account.incomingServer = server;
+    account.addIdentity(identity);
+
+    for (var i in hash) {
+        var vals = i.split(".");
+        var type = vals[0];
+        var slot = vals[1];
+
+        if (type == "identity")
+            identity[slot] = hash[i];
+        else if (type == "server")
+            server[slot] = hash[i];
+    }
+
+}
+
