98e701014035a1a43f922524a1a6c2d1ca13b2e5
shaver%netscape.com <shaver%netscape.com>
1999-09-04 19:57:49 +0000

    JSRuntime management service

---
+++ js/src/xpconnect/src/xpcruntimesvc.cpp
@@ -0,0 +1,77 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "MPL"); you may not use this file except in
+ * compliance with the MPL.  You may obtain a copy of the MPL at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the MPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the MPL
+ * for the specific language governing rights and limitations under the
+ * MPL.
+ *
+ * The Initial Developer of this code under the MPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1999 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#include "xpcprivate.h"
+
+/*
+ * This object holds state that we don't want to lose!
+ *
+ * The plan is that once created this object never goes away. We do an
+ * intentional extra addref at construction to keep it around even if no one
+ * is using it.
+ */
+
+nsJSRuntimeServiceImpl::nsJSRuntimeServiceImpl() :
+    mRuntime(0)
+{
+    NS_INIT_ISUPPORTS();
+    NS_ADDREF_THIS();
+}
+
+nsJSRuntimeServiceImpl::~nsJSRuntimeServiceImpl() { }
+
+NS_IMPL_ISUPPORTS1(nsJSRuntimeServiceImpl, nsIJSRuntimeService);
+
+nsJSRuntimeServiceImpl*
+nsJSRuntimeServiceImpl::GetSingleton()
+{
+    static nsJSRuntimeServiceImpl *singleton = nsnull;
+    if (!singleton) {
+	if ((singleton = new nsJSRuntimeServiceImpl()) != nsnull)
+	    NS_ADDREF(singleton);
+    }
+    NS_IF_ADDREF(singleton);
+    return singleton;
+}
+
+/* attribute JSRuntime runtime; */
+NS_IMETHODIMP
+nsJSRuntimeServiceImpl::GetRuntime(JSRuntime **runtime)
+{
+    if (!runtime)
+	return NS_ERROR_NULL_POINTER;
+
+    *runtime = mRuntime;
+#ifdef DEBUG_shaver
+    fprintf(stderr, "nJRSI: returning %p\n", mRuntime);
+#endif
+    return NS_OK;
+}
+
+NS_IMETHODIMP
+nsJSRuntimeServiceImpl::SetRuntime(JSRuntime *runtime)
+{
+#ifdef DEBUG_shaver
+    fprintf(stderr, "nJRSI: setting %p over %p\n", runtime, mRuntime);
+#endif
+    NS_ASSERTION(!mRuntime || !runtime, "can\'t switch runtimes");
+    if (mRuntime && runtime)
+        return NS_ERROR_INVALID_ARG;
+    mRuntime = runtime;
+    return NS_OK;
+}
