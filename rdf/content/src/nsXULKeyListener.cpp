5feefa758950cfedabf473dee00a4e0d1a48b316
saari%netscape.com <saari%netscape.com>
1999-09-04 01:25:55 +0000

    Adding support for full keyboard to XUL keybinds

--- rdf/content/src/nsXULKeyListener.cpp
+++ rdf/content/src/nsXULKeyListener.cpp
@@ -34,6 +34,121 @@
 #include "nsIXULKeyListener.h"
 #include "nsRDFCID.h"
 
+  enum {
+    VK_CANCEL = 3,
+    VK_BACK = 8,
+    VK_TAB = 9,
+    VK_CLEAR = 12,
+    VK_RETURN = 13,
+    VK_ENTER = 14,
+    VK_SHIFT = 16,
+    VK_CONTROL = 17,
+    VK_ALT = 18,
+    VK_PAUSE = 19,
+    VK_CAPS_LOCK = 20,
+    VK_ESCAPE = 27,
+    VK_SPACE = 32,
+    VK_PAGE_UP = 33,
+    VK_PAGE_DOWN = 34,
+    VK_END = 35,
+    VK_HOME = 36,
+    VK_LEFT = 37,
+    VK_UP = 38,
+    VK_RIGHT = 39,
+    VK_DOWN = 40,
+    VK_PRINTSCREEN = 44,
+    VK_INSERT = 45,
+    VK_DELETE = 46,
+    VK_0 = 48,
+    VK_1 = 49,
+    VK_2 = 50,
+    VK_3 = 51,
+    VK_4 = 52,
+    VK_5 = 53,
+    VK_6 = 54,
+    VK_7 = 55,
+    VK_8 = 56,
+    VK_9 = 57,
+    VK_SEMICOLON = 59,
+    VK_EQUALS = 61,
+    VK_A = 65,
+    VK_B = 66,
+    VK_C = 67,
+    VK_D = 68,
+    VK_E = 69,
+    VK_F = 70,
+    VK_G = 71,
+    VK_H = 72,
+    VK_I = 73,
+    VK_J = 74,
+    VK_K = 75,
+    VK_L = 76,
+    VK_M = 77,
+    VK_N = 78,
+    VK_O = 79,
+    VK_P = 80,
+    VK_Q = 81,
+    VK_R = 82,
+    VK_S = 83,
+    VK_T = 84,
+    VK_U = 85,
+    VK_V = 86,
+    VK_W = 87,
+    VK_X = 88,
+    VK_Y = 89,
+    VK_Z = 90,
+    VK_NUMPAD0 = 96,
+    VK_NUMPAD1 = 97,
+    VK_NUMPAD2 = 98,
+    VK_NUMPAD3 = 99,
+    VK_NUMPAD4 = 100,
+    VK_NUMPAD5 = 101,
+    VK_NUMPAD6 = 102,
+    VK_NUMPAD7 = 103,
+    VK_NUMPAD8 = 104,
+    VK_NUMPAD9 = 105,
+    VK_MULTIPLY = 106,
+    VK_ADD = 107,
+    VK_SEPARATOR = 108,
+    VK_SUBTRACT = 109,
+    VK_DECIMAL = 110,
+    VK_DIVIDE = 111,
+    VK_F1 = 112,
+    VK_F2 = 113,
+    VK_F3 = 114,
+    VK_F4 = 115,
+    VK_F5 = 116,
+    VK_F6 = 117,
+    VK_F7 = 118,
+    VK_F8 = 119,
+    VK_F9 = 120,
+    VK_F10 = 121,
+    VK_F11 = 122,
+    VK_F12 = 123,
+    VK_F13 = 124,
+    VK_F14 = 125,
+    VK_F15 = 126,
+    VK_F16 = 127,
+    VK_F17 = 128,
+    VK_F18 = 129,
+    VK_F19 = 130,
+    VK_F20 = 131,
+    VK_F21 = 132,
+    VK_F22 = 133,
+    VK_F23 = 134,
+    VK_F24 = 135,
+    VK_NUM_LOCK = 144,
+    VK_SCROLL_LOCK = 145,
+    VK_COMMA = 188,
+    VK_PERIOD = 190,
+    VK_SLASH = 191,
+    VK_BACK_QUOTE = 192,
+    VK_OPEN_BRACKET = 219,
+    VK_BACK_SLASH = 220,
+    VK_CLOSE_BRACKET = 221,
+    VK_QUOTE = 222
+  };
+
 ////////////////////////////////////////////////////////////////////////
 
 static NS_DEFINE_IID(kXULKeyListenerCID,      NS_XULKEYLISTENER_CID);
@@ -103,6 +218,7 @@ protected:
 
 private:
     nsresult DoKey(nsIDOMEvent* aKeyEvent, eEventType aEventType);
+    PRBool   IsMatchingKey(PRUint32 theChar, nsString keyName);
 
     nsIDOMElement* element; // Weak reference. The element will go away first.
     nsIDOMDocument* mDOMDocument; // Weak reference.
@@ -292,7 +408,8 @@ nsresult nsXULKeyListenerImpl::DoKey(nsIDOMEvent* aKeyEvent, eEventType aEventTy
 		         // NOTE - convert theChar and keyName to upper
 		         keyName.ToUpperCase();
 		         tempChar2.ToUpperCase();
-		        if (tempChar2 == keyName) {
+		        //if (tempChar2 == keyName) {
+		        if ( IsMatchingKey(theChar, keyName) ) {
 			      keyElement->GetAttribute(nsAutoString("command"), modCommand);
 			      keyElement->GetAttribute(nsAutoString("control"), modControl);
 			      keyElement->GetAttribute(nsAutoString("shift"),   modShift);
@@ -426,6 +543,468 @@ nsresult nsXULKeyListenerImpl::DoKey(nsIDOMEvent* aKeyEvent, eEventType aEventTy
   } // end if(aKeyEvent && mDOMDocument) 
   executingKeyBind = PR_FALSE;
   return NS_ERROR_BASE;
+}
+
+PRBool nsXULKeyListenerImpl::IsMatchingKey(PRUint32 theChar, nsString keyName)
+{
+  PRBool ret = PR_FALSE;
+  
+  //printf("theChar = %d \n", theChar);
+  //printf("keyName = %s \n", keyName.ToNewCString());
+  //printf("\n");
+  
+  switch ( theChar ) {
+    case VK_CANCEL:
+      if(keyName == "VK_CANCEL")
+        ret = PR_TRUE;
+        break;
+    case VK_BACK:
+      if(keyName == "VK_BACK")
+        ret = PR_TRUE;
+        break;
+    case VK_TAB:
+      if(keyName == "VK_TAB")
+        ret = PR_TRUE;
+        break;
+    case VK_CLEAR:
+      if(keyName == "VK_CLEAR")
+        ret = PR_TRUE;
+        break;
+    case VK_RETURN:
+      if(keyName == "VK_RETURN")
+        ret = PR_TRUE;
+        break;
+    case VK_ENTER:
+      if(keyName == "VK_ENTER")
+        ret = PR_TRUE;
+        break;
+    case VK_SHIFT:
+      if(keyName == "VK_SHIFT")
+        ret = PR_TRUE;
+        break;
+    case VK_CONTROL:
+      if(keyName == "VK_CONTROL")
+        ret = PR_TRUE;
+        break;
+    case VK_ALT:
+      if(keyName == "VK_ALT")
+        ret = PR_TRUE;
+        break;
+    case VK_PAUSE:
+      if(keyName == "VK_PAUSE")
+        ret = PR_TRUE;
+        break;
+    case VK_CAPS_LOCK:
+      if(keyName == "VK_CAPS_LOCK")
+        ret = PR_TRUE;
+        break;
+    case VK_ESCAPE:
+      if(keyName == "VK_ESCAPE")
+        ret = PR_TRUE;
+        break;
+    case VK_SPACE:
+      if(keyName == "VK_SPACE")
+        ret = PR_TRUE;
+        break;
+    case VK_PAGE_UP:
+      if(keyName == "VK_PAGE_UP")
+        ret = PR_TRUE;
+        break;
+    case VK_PAGE_DOWN:
+      if(keyName == "VK_PAGE_DOWN")
+        ret = PR_TRUE;
+        break;
+    case VK_END:
+      if(keyName == "VK_END")
+        ret = PR_TRUE;
+        break;
+    case VK_HOME:
+      if(keyName == "VK_HOME")
+        ret = PR_TRUE;
+        break;
+    case VK_LEFT:
+      if(keyName == "VK_LEFT")
+        ret = PR_TRUE;
+        break;
+    case VK_UP:
+      if(keyName == "VK_UP")
+        ret = PR_TRUE;
+        break;
+    case VK_RIGHT:
+      if(keyName == "VK_RIGHT")
+        ret = PR_TRUE;
+        break;
+    case VK_DOWN:
+      if(keyName == "VK_DOWN")
+        ret = PR_TRUE;
+        break;
+    case VK_PRINTSCREEN:
+      if(keyName == "VK_PRINTSCREEN")
+        ret = PR_TRUE;
+        break;
+    case VK_INSERT:
+      if(keyName == "VK_INSERT")
+        ret = PR_TRUE;
+        break;
+    case VK_DELETE:
+      if(keyName == "VK_DELETE")
+        ret = PR_TRUE;
+        break;
+    case VK_0:
+      if(keyName == "VK_0")
+        ret = PR_TRUE;
+        break;
+    case VK_1:
+      if(keyName == "VK_1")
+        ret = PR_TRUE;
+        break;
+    case VK_2:
+      if(keyName == "VK_2")
+        ret = PR_TRUE;
+        break;
+    case VK_3:
+      if(keyName == "VK_3")
+        ret = PR_TRUE;
+        break;
+    case VK_4:
+      if(keyName == "VK_4")
+        ret = PR_TRUE;
+        break;
+    case VK_5:
+      if(keyName == "VK_5")
+        ret = PR_TRUE;
+        break;
+    case VK_6:
+      if(keyName == "VK_6")
+        ret = PR_TRUE;
+        break;
+    case VK_7:
+      if(keyName == "VK_7")
+        ret = PR_TRUE;
+        break;
+    case VK_8:
+      if(keyName == "VK_8")
+        ret = PR_TRUE;
+        break;
+    case VK_9:
+      if(keyName == "VK_9")
+        ret = PR_TRUE;
+        break;
+    case VK_SEMICOLON:
+      if(keyName == "VK_SEMICOLON")
+        ret = PR_TRUE;
+        break;
+    case VK_EQUALS:
+      if(keyName == "VK_EQUALS")
+        ret = PR_TRUE;
+        break;
+    case VK_A:
+      if(keyName == "VK_A"  || keyName == "A" || keyName == "a")
+        ret = PR_TRUE;
+        break;
+    case VK_B:
+      if(keyName == "VK_B" || keyName == "B" || keyName == "b")
+        ret = PR_TRUE;
+    break;
+    case VK_C:
+      if(keyName == "VK_C"  || keyName == "C" || keyName == "c")
+        ret = PR_TRUE;
+        break;
+    case VK_D:
+      if(keyName == "VK_D"  || keyName == "D" || keyName == "d")
+        ret = PR_TRUE;
+        break;
+    case VK_E:
+      if(keyName == "VK_E"  || keyName == "E" || keyName == "e")
+        ret = PR_TRUE;
+        break;
+    case VK_F:
+      if(keyName == "VK_F"  || keyName == "F" || keyName == "f")
+        ret = PR_TRUE;
+        break;
+    case VK_G:
+      if(keyName == "VK_G"  || keyName == "G" || keyName == "g")
+        ret = PR_TRUE;
+        break;
+    case VK_H:
+      if(keyName == "VK_H"  || keyName == "H" || keyName == "h")
+        ret = PR_TRUE;
+        break;
+    case VK_I:
+      if(keyName == "VK_I"  || keyName == "I" || keyName == "i")
+        ret = PR_TRUE;
+        break;
+    case VK_J:
+      if(keyName == "VK_J"  || keyName == "J" || keyName == "j")
+        ret = PR_TRUE;
+        break;
+    case VK_K:
+      if(keyName == "VK_K"  || keyName == "K" || keyName == "k")
+        ret = PR_TRUE;
+        break;
+    case VK_L:
+      if(keyName == "VK_L"  || keyName == "L" || keyName == "l")
+        ret = PR_TRUE;
+        break;
+    case VK_M:
+      if(keyName == "VK_M"  || keyName == "M" || keyName == "m")
+        ret = PR_TRUE;
+        break;
+    case VK_N:
+      if(keyName == "VK_N"  || keyName == "N" || keyName == "n")
+        ret = PR_TRUE;
+        break;
+    case VK_O:
+      if(keyName == "VK_O"  || keyName == "O" || keyName == "o")
+        ret = PR_TRUE;
+        break;
+    case VK_P:
+      if(keyName == "VK_P"  || keyName == "P" || keyName == "p")
+        ret = PR_TRUE;
+        break;
+    case VK_Q:
+      if(keyName == "VK_Q"  || keyName == "Q" || keyName == "q")
+        ret = PR_TRUE;
+        break;
+    case VK_R:
+      if(keyName == "VK_R"  || keyName == "R" || keyName == "r")
+        ret = PR_TRUE;
+        break;
+    case VK_S:
+      if(keyName == "VK_S"  || keyName == "S" || keyName == "s")
+        ret = PR_TRUE;
+        break;
+    case VK_T:
+      if(keyName == "VK_T"  || keyName == "T" || keyName == "t")
+        ret = PR_TRUE;
+        break;
+    case VK_U:
+      if(keyName == "VK_U"  || keyName == "U" || keyName == "u")
+        ret = PR_TRUE;
+        break;
+    case VK_V:
+      if(keyName == "VK_V"  || keyName == "V" || keyName == "v")
+        ret = PR_TRUE;
+        break;
+    case VK_W:
+      if(keyName == "VK_W"  || keyName == "W" || keyName == "w")
+        ret = PR_TRUE;
+        break;
+    case VK_X:
+      if(keyName == "VK_X"  || keyName == "X" || keyName == "x")
+        ret = PR_TRUE;
+        break;
+    case VK_Y:
+      if(keyName == "VK_Y"  || keyName == "Y" || keyName == "y")
+        ret = PR_TRUE;
+        break;
+    case VK_Z:
+      if(keyName == "VK_Z"  || keyName == "Z" || keyName == "z")
+        ret = PR_TRUE;
+        break;
+    case VK_NUMPAD0:
+      if(keyName == "VK_NUMPAD0")
+        ret = PR_TRUE;
+        break;
+    case VK_NUMPAD1:
+      if(keyName == "VK_NUMPAD1")
+        ret = PR_TRUE;
+        break;
+    case VK_NUMPAD2:
+      if(keyName == "VK_NUMPAD2")
+        ret = PR_TRUE;
+        break;
+    case VK_NUMPAD3:
+      if(keyName == "VK_NUMPAD3")
+        ret = PR_TRUE;
+        break;
+    case VK_NUMPAD4:
+      if(keyName == "VK_NUMPAD4")
+        ret = PR_TRUE;
+        break;
+    case VK_NUMPAD5:
+      if(keyName == "VK_NUMPAD5")
+        ret = PR_TRUE;
+        break;
+    case VK_NUMPAD6:
+      if(keyName == "VK_NUMPAD6")
+        ret = PR_TRUE;
+        break;
+    case VK_NUMPAD7:
+      if(keyName == "VK_NUMPAD7")
+        ret = PR_TRUE;
+        break;
+    case VK_NUMPAD8:
+      if(keyName == "VK_NUMPAD8")
+        ret = PR_TRUE;
+        break;
+    case VK_NUMPAD9:
+      if(keyName == "VK_NUMPAD9")
+        ret = PR_TRUE;
+        break;
+    case VK_MULTIPLY:
+      if(keyName == "VK_MULTIPLY")
+        ret = PR_TRUE;
+        break;
+    case VK_ADD:
+      if(keyName == "VK_ADD")
+        ret = PR_TRUE;
+        break;
+    case VK_SEPARATOR:
+      if(keyName == "VK_SEPARATOR")
+        ret = PR_TRUE;
+        break;
+    case VK_SUBTRACT:
+      if(keyName == "VK_SUBTRACT")
+        ret = PR_TRUE;
+        break;
+    case VK_DECIMAL:
+      if(keyName == "VK_DECIMAL")
+        ret = PR_TRUE;
+        break;
+    case VK_DIVIDE:
+      if(keyName == "VK_DIVIDE")
+        ret = PR_TRUE;
+        break;
+    case VK_F1:
+      if(keyName == "VK_F1")
+        ret = PR_TRUE;
+        break;
+    case VK_F2:
+      if(keyName == "VK_F2")
+        ret = PR_TRUE;
+        break;
+    case VK_F3:
+      if(keyName == "VK_F3")
+        ret = PR_TRUE;
+        break;
+    case VK_F4:
+      if(keyName == "VK_F4")
+        ret = PR_TRUE;
+        break;
+    case VK_F5:
+      if(keyName == "VK_F5")
+        ret = PR_TRUE;
+        break;
+    case VK_F6:
+      if(keyName == "VK_F6")
+        ret = PR_TRUE;
+        break;
+    case VK_F7:
+      if(keyName == "VK_F7")
+        ret = PR_TRUE;
+        break;
+    case VK_F8:
+      if(keyName == "VK_F8")
+        ret = PR_TRUE;
+        break;
+    case VK_F9:
+      if(keyName == "VK_F9")
+        ret = PR_TRUE;
+        break;
+    case VK_F10:
+      if(keyName == "VK_F10")
+        ret = PR_TRUE;
+        break;
+    case VK_F11:
+      if(keyName == "VK_F11")
+        ret = PR_TRUE;
+        break;
+    case VK_F12:
+      if(keyName == "VK_F12")
+        ret = PR_TRUE;
+        break;
+    case VK_F13:
+      if(keyName == "VK_F13")
+        ret = PR_TRUE;
+        break;
+    case VK_F14:
+      if(keyName == "VK_F14")
+        ret = PR_TRUE;
+        break;
+    case VK_F15:
+      if(keyName == "VK_F15")
+        ret = PR_TRUE;
+        break;
+    case VK_F16:
+      if(keyName == "VK_F16")
+        ret = PR_TRUE;
+        break;
+    case VK_F17:
+      if(keyName == "VK_F17")
+        ret = PR_TRUE;
+        break;
+    case VK_F18:
+      if(keyName == "VK_F18")
+        ret = PR_TRUE;
+        break;
+    case VK_F19:
+      if(keyName == "VK_F19")
+        ret = PR_TRUE;
+        break;
+    case VK_F20:
+      if(keyName == "VK_F20")
+        ret = PR_TRUE;
+        break;
+    case VK_F21:
+      if(keyName == "VK_F21")
+        ret = PR_TRUE;
+        break;
+    case VK_F22:
+      if(keyName == "VK_F22")
+        ret = PR_TRUE;
+        break;
+    case VK_F23:
+      if(keyName == "VK_F23")
+        ret = PR_TRUE;
+        break;
+    case VK_F24:
+      if(keyName == "VK_F24")
+        ret = PR_TRUE;
+        break;
+    case VK_NUM_LOCK:
+      if(keyName == "VK_NUM_LOCK")
+        ret = PR_TRUE;
+        break;
+    case VK_SCROLL_LOCK:
+      if(keyName == "VK_SCROLL_LOCK")
+        ret = PR_TRUE;
+        break;
+    case VK_COMMA:
+      if(keyName == "VK_COMMA")
+        ret = PR_TRUE;
+        break;
+    case VK_PERIOD:
+      if(keyName == "VK_PERIOD")
+        ret = PR_TRUE;
+        break;
+    case VK_SLASH:
+      if(keyName == "VK_SLASH")
+        ret = PR_TRUE;
+        break;
+    case VK_BACK_QUOTE:
+      if(keyName == "VK_BACK_QUOTE")
+        ret = PR_TRUE;
+        break;
+    case VK_OPEN_BRACKET:
+      if(keyName == "VK_OPEN_BRACKET")
+        ret = PR_TRUE;
+        break;
+    case VK_BACK_SLASH:
+      if(keyName == "VK_BACK_SLASH")
+        ret = PR_TRUE;
+        break;
+    case VK_CLOSE_BRACKET:
+      if(keyName == "VK_CLOSE_BRACKET")
+        ret = PR_TRUE;
+        break;
+    case VK_QUOTE:
+      if(keyName == "VK_QUOTE")
+        ret = PR_TRUE;
+        break;
+  }
+  
+  return ret;
 }
 
 ////////////////////////////////////////////////////////////////
