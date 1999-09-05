d60201e832634f173db8a1943b25c481e40a8621
cls%seawood.org <cls%seawood.org>
1999-09-05 20:46:57 +0000

    When cross-compiling, jscpucfg needs to grab the values from NSPR rather than calling sizeof().

--- js/src/jscpucfg.c
+++ js/src/jscpucfg.c
@@ -21,6 +21,11 @@
  */
 #include <stdio.h>
 
+#ifdef CROSS_COMPILE
+#include <prtypes.h>
+#define INT64 PRInt64
+#else
+
 #ifdef __MWERKS__
 #define XP_MAC 1
 #endif
@@ -34,7 +39,11 @@
 #else
 #ifdef XP_PC
 #ifdef WIN32
+#if defined(__GNUC__)
+#define INT64   long long
+#else
 #define INT64	_int64
+#endif /* __GNUC__ */
 #else
 #define INT64	long
 #endif
@@ -46,6 +55,8 @@
 #endif
 #endif
 #endif
+
+#endif /* CROSS_COMPILE */
 
 typedef void *prword;
 
@@ -159,6 +170,12 @@ static void BitsPerByte(void)
 
 int main(int argc, char **argv)
 {
+    int sizeof_char, sizeof_short, sizeof_int, sizeof_int64, sizeof_long,
+        sizeof_float, sizeof_double, sizeof_word, sizeof_dword;
+    int bits_per_int64_log2, align_of_short, align_of_int, align_of_long,
+        align_of_int64, align_of_float, align_of_double, align_of_pointer,
+        align_of_word;
+
     BitsPerByte();
 
     printf("#ifndef js_cpucfg___\n");
@@ -166,60 +183,117 @@ int main(int argc, char **argv)
 
     printf("/* AUTOMATICALLY GENERATED - DO NOT EDIT */\n\n");
 
+#ifdef CROSS_COMPILE
+#if defined(IS_LITTLE_ENDIAN)
+    printf("#define IS_LITTLE_ENDIAN 1\n");
+    printf("#undef  IS_BIG_ENDIAN\n\n");
+#elif defined(IS_BIG_ENDIAN)
+    printf("#undef  IS_LITTLE_ENDIAN\n");
+    printf("#define IS_BIG_ENDIAN 1\n\n");
+#else
+#error "Endianess not defined."
+#endif
+
+    sizeof_char		= PR_BYTES_PER_BYTE;
+    sizeof_short	= PR_BYTES_PER_SHORT;
+    sizeof_int		= PR_BYTES_PER_INT;
+    sizeof_int64	= PR_BYTES_PER_INT64;
+    sizeof_long		= PR_BYTES_PER_LONG;
+    sizeof_float	= PR_BYTES_PER_FLOAT;
+    sizeof_double	= PR_BYTES_PER_DOUBLE;
+    sizeof_word		= PR_BYTES_PER_WORD;
+    sizeof_dword	= PR_BYTES_PER_DWORD;
+
+    bits_per_int64_log2 = PR_BITS_PER_INT64_LOG2;
+
+    align_of_short	= PR_ALIGN_OF_SHORT;
+    align_of_int	= PR_ALIGN_OF_INT;
+    align_of_long	= PR_ALIGN_OF_LONG;
+    align_of_int64	= PR_ALIGN_OF_INT64;
+    align_of_float	= PR_ALIGN_OF_FLOAT;
+    align_of_double	= PR_ALIGN_OF_DOUBLE;
+    align_of_pointer	= PR_ALIGN_OF_POINTER;
+    align_of_word	= PR_ALIGN_OF_WORD;
+
+#else /* !CROSS_COMPILE */
     if (sizeof(long) == 8) {
 	do64();
     } else {
 	do32();
     }
-    printf("#define JS_BYTES_PER_BYTE   %dL\n", sizeof(char));
-    printf("#define JS_BYTES_PER_SHORT  %dL\n", sizeof(short));
-    printf("#define JS_BYTES_PER_INT    %dL\n", sizeof(int));
-    printf("#define JS_BYTES_PER_INT64  %dL\n", 8);
-    printf("#define JS_BYTES_PER_LONG   %dL\n", sizeof(long));
-    printf("#define JS_BYTES_PER_FLOAT  %dL\n", sizeof(float));
-    printf("#define JS_BYTES_PER_DOUBLE %dL\n", sizeof(double));
-    printf("#define JS_BYTES_PER_WORD   %dL\n", sizeof(prword));
-    printf("#define JS_BYTES_PER_DWORD  %dL\n", 8);
+
+    sizeof_char		= sizeof(char);
+    sizeof_short	= sizeof(short);
+    sizeof_int		= sizeof(int);
+    sizeof_int64	= 8;
+    sizeof_long		= sizeof(long);
+    sizeof_float	= sizeof(float);
+    sizeof_double	= sizeof(double);
+    sizeof_word		= sizeof(prword);
+    sizeof_dword	= 8;
+
+    bits_per_int64_log2 = 6;
+
+    align_of_short	= ALIGN_OF(short);
+    align_of_int	= ALIGN_OF(int);
+    align_of_long	= ALIGN_OF(long);
+    if (sizeof(INT64) < 8) {
+	/* this machine doesn't actually support int64's */
+        align_of_int64	= ALIGN_OF(fakelonglong);
+    } else {
+        align_of_int64	= ALIGN_OF(int64);
+    }
+    align_of_float	= ALIGN_OF(float);
+    align_of_double	= ALIGN_OF(double);
+    align_of_pointer	= ALIGN_OF(pointer);
+    align_of_word	= ALIGN_OF(prword);
+
+#endif /* CROSS_COMPILE */
+
+    printf("#define JS_BYTES_PER_BYTE   %dL\n", sizeof_char);
+    printf("#define JS_BYTES_PER_SHORT  %dL\n", sizeof_short);
+    printf("#define JS_BYTES_PER_INT    %dL\n", sizeof_int);
+    printf("#define JS_BYTES_PER_INT64  %dL\n", sizeof_int64);
+    printf("#define JS_BYTES_PER_LONG   %dL\n", sizeof_long);
+    printf("#define JS_BYTES_PER_FLOAT  %dL\n", sizeof_float);
+    printf("#define JS_BYTES_PER_DOUBLE %dL\n", sizeof_double);
+    printf("#define JS_BYTES_PER_WORD   %dL\n", sizeof_word);
+    printf("#define JS_BYTES_PER_DWORD  %dL\n", sizeof_dword);
     printf("\n");
 
     printf("#define JS_BITS_PER_BYTE    %dL\n", bpb);
-    printf("#define JS_BITS_PER_SHORT   %dL\n", bpb * sizeof(short));
-    printf("#define JS_BITS_PER_INT     %dL\n", bpb * sizeof(int));
-    printf("#define JS_BITS_PER_INT64   %dL\n", bpb * 8);
-    printf("#define JS_BITS_PER_LONG    %dL\n", bpb * sizeof(long));
-    printf("#define JS_BITS_PER_FLOAT   %dL\n", bpb * sizeof(float));
-    printf("#define JS_BITS_PER_DOUBLE  %dL\n", bpb * sizeof(double));
-    printf("#define JS_BITS_PER_WORD    %dL\n", bpb * sizeof(prword));
+    printf("#define JS_BITS_PER_SHORT   %dL\n", bpb * sizeof_short);
+    printf("#define JS_BITS_PER_INT     %dL\n", bpb * sizeof_int);
+    printf("#define JS_BITS_PER_INT64   %dL\n", bpb * sizeof_int64);
+    printf("#define JS_BITS_PER_LONG    %dL\n", bpb * sizeof_long);
+    printf("#define JS_BITS_PER_FLOAT   %dL\n", bpb * sizeof_float);
+    printf("#define JS_BITS_PER_DOUBLE  %dL\n", bpb * sizeof_double);
+    printf("#define JS_BITS_PER_WORD    %dL\n", bpb * sizeof_word);
     printf("\n");
 
     printf("#define JS_BITS_PER_BYTE_LOG2   %dL\n", Log2(bpb));
-    printf("#define JS_BITS_PER_SHORT_LOG2  %dL\n", Log2(bpb * sizeof(short)));
-    printf("#define JS_BITS_PER_INT_LOG2    %dL\n", Log2(bpb * sizeof(int)));
-    printf("#define JS_BITS_PER_INT64_LOG2  %dL\n", 6);
-    printf("#define JS_BITS_PER_LONG_LOG2   %dL\n", Log2(bpb * sizeof(long)));
-    printf("#define JS_BITS_PER_FLOAT_LOG2  %dL\n", Log2(bpb * sizeof(float)));
-    printf("#define JS_BITS_PER_DOUBLE_LOG2 %dL\n", Log2(bpb * sizeof(double)));
-    printf("#define JS_BITS_PER_WORD_LOG2   %dL\n", Log2(bpb * sizeof(prword)));
+    printf("#define JS_BITS_PER_SHORT_LOG2  %dL\n", Log2(bpb * sizeof_short));
+    printf("#define JS_BITS_PER_INT_LOG2    %dL\n", Log2(bpb * sizeof_int));
+    printf("#define JS_BITS_PER_INT64_LOG2  %dL\n", bits_per_int64_log2);
+    printf("#define JS_BITS_PER_LONG_LOG2   %dL\n", Log2(bpb * sizeof_long));
+    printf("#define JS_BITS_PER_FLOAT_LOG2  %dL\n", Log2(bpb * sizeof_float));
+    printf("#define JS_BITS_PER_DOUBLE_LOG2 %dL\n", Log2(bpb * sizeof_double));
+    printf("#define JS_BITS_PER_WORD_LOG2   %dL\n", Log2(bpb * sizeof_word));
     printf("\n");
 
-    printf("#define JS_ALIGN_OF_SHORT   %dL\n", ALIGN_OF(short));
-    printf("#define JS_ALIGN_OF_INT     %dL\n", ALIGN_OF(int));
-    printf("#define JS_ALIGN_OF_LONG    %dL\n", ALIGN_OF(long));
-    if (sizeof(INT64) < 8) {
-	/* this machine doesn't actually support int64's */
-	printf("#define JS_ALIGN_OF_INT64   %dL\n", ALIGN_OF(fakelonglong));
-    } else {
-	printf("#define JS_ALIGN_OF_INT64   %dL\n", ALIGN_OF(int64));
-    }
-    printf("#define JS_ALIGN_OF_FLOAT   %dL\n", ALIGN_OF(float));
-    printf("#define JS_ALIGN_OF_DOUBLE  %dL\n", ALIGN_OF(double));
-    printf("#define JS_ALIGN_OF_POINTER %dL\n", ALIGN_OF(pointer));
-    printf("#define JS_ALIGN_OF_WORD    %dL\n", ALIGN_OF(prword));
+    printf("#define JS_ALIGN_OF_SHORT   %dL\n", align_of_short);
+    printf("#define JS_ALIGN_OF_INT     %dL\n", align_of_int);
+    printf("#define JS_ALIGN_OF_LONG    %dL\n", align_of_long);
+    printf("#define JS_ALIGN_OF_INT64   %dL\n", align_of_int64);
+    printf("#define JS_ALIGN_OF_FLOAT   %dL\n", align_of_float);
+    printf("#define JS_ALIGN_OF_DOUBLE  %dL\n", align_of_double);
+    printf("#define JS_ALIGN_OF_POINTER %dL\n", align_of_pointer);
+    printf("#define JS_ALIGN_OF_WORD    %dL\n", align_of_word);
     printf("\n");
 
-    printf("#define JS_BYTES_PER_WORD_LOG2   %dL\n", Log2(sizeof(prword)));
-    printf("#define JS_BYTES_PER_DWORD_LOG2  %dL\n", Log2(8));
-    printf("#define JS_WORDS_PER_DWORD_LOG2  %dL\n", Log2(8/sizeof(prword)));
+    printf("#define JS_BYTES_PER_WORD_LOG2   %dL\n", Log2(sizeof_word));
+    printf("#define JS_BYTES_PER_DWORD_LOG2  %dL\n", Log2(sizeof_dword));
+    printf("#define JS_WORDS_PER_DWORD_LOG2  %dL\n", Log2(sizeof_dword/sizeof_word));
     printf("\n");
 
     printf("#endif /* js_cpucfg___ */\n");
