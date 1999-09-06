71d227c25f069fa3648dbc79a3a1de580f2d7690
cls%seawood.org <cls%seawood.org>
1999-09-06 20:24:44 +0000

    Removed unnecessary USE_AUTOCONF ifdef.

--- include/np.h
+++ include/np.h
@@ -43,13 +43,6 @@ typedef enum { NP_FullPage = 1, NP_Embedded } NPPageType;
 typedef void* NPReference;
 #define NPRefFromStart ((NPReference)NULL)
 
-/* This ifdef should probably be replaced by something more generic like
- * #if defined(JAVA_OR_OJI) || defined(XP_MAC) -cls
- */
-#ifndef USE_AUTOCONF
-#define ANTHRAX
-#endif
-
 /* it's lame that this is supposed to support more than plugins but
 it has plugin specific junk (wdata) in it -jg */
 
