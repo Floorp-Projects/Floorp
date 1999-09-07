8b9df615a45263d16b4ac506e287386a0382f96f
ssu%netscape.com <ssu%netscape.com>
1999-09-07 23:19:23 +0000

    updated makefile to use non_mt libs. update license info for the other files.
    This is not part of the seamonkey build, yet.

--- xpinstall/wizard/windows/nsinstall/resource.h
+++ xpinstall/wizard/windows/nsinstall/resource.h
@@ -19,7 +19,7 @@
  *
  * The Initial Developer of the Original Code is Netscape Communications
  * Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Copyright (C) 1999 Netscape Communications Corporation.  All Rights
  * Reserved.
  *
  * Contributors:
