c22aca29286ac96a19a31a20128b01e4760d0c06
ftang@netscape.com <ftang@netscape.com>
1999-09-08 13:41:11 +0000

    placeholder for some late night hack....

---
+++ intl/unicharutil/public/nsArabicBasicLToPFormBVCID.h
@@ -0,0 +1,35 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+#ifndef nsArabicBasicLToPFormBVCID_h__
+#define nsArabicBasicLToPFormBVCID_h__
+
+
+#include "nsITextTransform.h"
+#include "nsISupports.h"
+#include "nscore.h"
+
+
+// {6A381F92-65F1-11d3-B3C5-00805F8A6670}
+#define NS_ARABIC_BASICL_TO_PFORMBV_CID \
+{ 0x6a381f92, 0x65f1, 0x11d3, \
+  { 0xb3, 0xc5, 0x0, 0x80, 0x5f, 0x8a, 0x66, 0x70} }
+
+#define NS_ARABIC_BASICL_TO_PFORMBV_PROGID NS_TEXTTRANSFORM_PROGID_BASE "ArabicBasicLToPFormBV"
+
+
+#endif
