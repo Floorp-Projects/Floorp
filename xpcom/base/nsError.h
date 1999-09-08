cd30fe89a8d000d8cf5937952fbe2bf24093f168
vidur%netscape.com <vidur%netscape.com>
1999-09-08 23:11:52 +0000

    Added error base for DOM module

--- xpcom/base/nsError.h
+++ xpcom/base/nsError.h
@@ -65,6 +65,7 @@ typedef PRUint32 nsresult;
 #define NS_ERROR_MODULE_UCONV      11
 #define NS_ERROR_MODULE_REG        12
 #define NS_ERROR_MODULE_FILES      13
+#define NS_ERROR_MODULE_DOM        14
 
 #define NS_ERROR_MODULE_MAILNEWS   16
 #define NS_ERROR_MODULE_EDITOR     17
