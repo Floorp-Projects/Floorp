c4384f1c7c109310d6d24a21c18736b2b501bb66
pnunn@netscape.com <pnunn@netscape.com>
1999-09-09 19:32:16 +0000

    Chkins for newt@pobox.com.

--- modules/libimg/pngcom/pngdec.h
+++ modules/libimg/pngcom/pngdec.h
@@ -18,11 +18,10 @@
  */
 
 
-/* png.h */
+/* pngdec.h */
 
 extern int  il_png_init(il_container *ic);
 extern int  il_png_write(il_container *, const uint8 *, int32);
-extern int il_png_complete(il_container *ic);
+extern int  il_png_complete(il_container *ic);
+extern int  il_png_abort(il_container *ic);
 //extern unsigned int il_png_write_ready(il_container *ic);
-extern int il_png_abort(il_container *ic);
-extern void png_delay_time_callback(void *closure);
