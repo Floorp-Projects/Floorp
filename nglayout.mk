f74a9cb62f612fad4749e03119967808445753d3
briano@netscape.com <briano@netscape.com>
1999-09-11 04:50:16 +0000

    General cleanup.

--- nglayout.mk
+++ nglayout.mk
@@ -1,4 +1,4 @@
-#!gmake
+#
 # The contents of this file are subject to the Netscape Public License
 # Version 1.0 (the "NPL"); you may not use this file except in
 # compliance with the NPL.  You may obtain a copy of the NPL at
@@ -13,77 +13,63 @@
 # Communications Corporation.  Portions created by Netscape are
 # Copyright (C) 1998 Netscape Communications Corporation.  All Rights
 # Reserved.
-
-DEPTH=.
-
-#
-# Command macro defines
 #
 
-ifdef MOZ_DATE
-CVSCO = cvs -q co -P -D $(MOZ_DATE)
-else
-CVSCO = cvs -q co -P
-endif
-
-THIS_MAKEFILE = nglayout.mk
+DEPTH		= .
+THIS_MAKEFILE	= nglayout.mk
 
 # Branch tags we use
-NETLIB_BRANCH = 
-LIBPREF_BRANCH = XPCOM_BRANCH
-PLUGIN_BRANCH =
-
-# CVS commands to pull the appropriate branch versions
-CVSCO_IMGLIB = $(CVSCO)
-CVSCO_NETLIB = $(CVSCO)
-CVSCO_NGLAYOUT = $(CVSCO)
-CVSCO_LIZARD = $(CVSCO)
+LIBPREF_BRANCH	= XPCOM_BRANCH
+NETLIB_BRANCH	=
+PLUGIN_BRANCH	=
 
 ifdef MOZ_DATE
-CVSCO_LIBPREF = $(CVSCO) -r $(LIBPREF_BRANCH)
-CVSCO_PLUGIN = $(CVSCO)
+CVSCO		= cvs -q co -P -D $(MOZ_DATE)
+CVSCO_PLUGIN	= $(CVSCO)
 else
-CVSCO_LIBPREF = $(CVSCO) -r $(LIBPREF_BRANCH)
-CVSCO_PLUGIN = $(CVSCO) -A
+CVSCO		= cvs -q co -P
+CVSCO_PLUGIN	= $(CVSCO) -A
 endif
+
+CVSCO_IMGLIB	= $(CVSCO)
+CVSCO_LIBPREF	= $(CVSCO) -r $(LIBPREF_BRANCH)
+CVSCO_LIZARD	= $(CVSCO)
+CVSCO_NETLIB	= $(CVSCO)
+CVSCO_NGLAYOUT	= $(CVSCO)
 
 # The list of directories that need to be built to build the standalone
 # nglayout test program. The order is important.
-DIRS =				\
-  config			\
-  dbm				\
-  nsprpub			\
-  jpeg				\
-  modules/libreg		\
-  xpcom				\
-  modules/zlib			\
-  modules/libutil		\
-  sun-java			\
-  nav-java			\
-  js				\
-  modules/security/freenav	\
-  modules/libpref		\
-  modules/libimg		\
-  modules/oji		\
-  modules/plugin		\
-  base				\
-  caps				\
-  lib/xp			\
-  lib/libpwcac			\
-  network			\
-  lib/liblayer/include		\
-  htmlparser			\
-  gfx				\
-  dom				\
-  view				\
-  widget			\
-  layout			\
-  editor                \
-  webshell
-
-#-include $(DEPTH)/config/config.mk
-
-#-include $(DEPTH)/config/rules.mk
+DIRS		= \
+		config \
+		dbm \
+		nsprpub \
+		jpeg \
+		modules/libreg \
+		xpcom \
+		modules/zlib \
+		modules/libutil \
+		sun-java \
+		nav-java \
+		js \
+		modules/security/freenav \
+		modules/libpref \
+		modules/libimg \
+		modules/oji \
+		modules/plugin \
+		base \
+		caps \
+		lib/xp \
+		lib/libpwcac \
+		network \
+		lib/liblayer/include \
+		htmlparser \
+		gfx \
+		dom \
+		view \
+		widget \
+		layout \
+		editor \
+		webshell
 
 real_all: all
 
@@ -100,7 +86,6 @@ real_depend: depend
 #
 # Rules for pulling the source from the cvs repository
 #
-
 pull_all: pull_lizard pull_xpcom pull_imglib pull_netlib pull_nglayout pull_editor pull_plugin pull_autoconf
 
 pull_lizard:
@@ -179,15 +164,15 @@ pull_autoconf:
 #
 # Build tarball
 
-DATE_CMD=date
-DATE=$(shell $(DATE_CMD) +%Y%m%d)
+DATE_CMD	= date
+DATE		= $(shell $(DATE_CMD) +%Y%m%d)
 
-TAR = tar
-GZIP = gzip
+TAR		= tar
+GZIP		= gzip
 
-TARBALL = $(MOZ_SRC)/unix-$(DATE).tar
+TARBALL		= $(MOZ_SRC)/unix-$(DATE).tar
 
-TARFILES = mozilla README/nglayout
+TARFILES	= mozilla README/nglayout
 
 tarball: pull_all pull_doc clobber clobber_all real_tar
 
@@ -199,3 +184,4 @@ real_tar:
 	@echo Making gzip of $(TARBALL); \
 	cd $(DEPTH)/..; \
 	$(GZIP) -9 -q $(TARBALL)
+
