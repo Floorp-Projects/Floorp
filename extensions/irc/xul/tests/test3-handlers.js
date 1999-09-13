aa9055643a819a6694c64d6b35d5d446fe5adb4d
rginda%ndcico.com <rginda%ndcico.com>
1999-09-13 23:23:53 +0000

    Added test3.xul and friends

---
+++ extensions/irc/xul/tests/test3-handlers.js
@@ -0,0 +1,484 @@
+
+function onLoad()
+{
+
+    initStatic();
+    initHost(client);
+    mainStep();
+    
+}
+
+function onUnload()
+{
+
+    client.quit ("re-load");
+    
+}
+
+/* toolbaritem click */
+function onTBIClick (id)
+{
+    var tbi = document.getElementById (id);
+    var view = client.viewsArray[tbi.getAttribute("viewKey")];
+
+    setCurrentObject (view);
+    
+}
+
+function onInputKeyUp (e)
+{
+    
+    switch (e.which)
+    {        
+        case 13: /* CR */
+            e.line = e.target.value;
+            onInputCompleteLine (e);
+            break;
+
+        case 38: /* up */
+            break;
+
+        case 40: /* down */
+            break;
+            
+    }
+
+}
+
+function onInputCompleteLine(e)
+{
+
+    if (e.target.getAttribute ("expanded") != "YES")
+    {
+        if (e.line[0] == client.COMMAND_CHAR)
+        {
+            var ary = e.line.substr(1, e.line.length).match (/(\S+)? ?(.*)/);
+            var command = ary[1];
+            
+            if (command[0].search (/[\[\{\(]/) == 0) /* request to expand */
+            {
+                e.target.setAttribute("expanded", "YES");
+                switch (command[0])
+                {
+                    case "[":
+                        e.target.setAttribute("collapseChar", "]");
+                        break;
+
+                    case "{":
+                        e.target.setAttribute("collapseChar", "}");
+                        break;
+                        
+                    case "(":
+                        e.target.setAttribute("collapseChar", ")");
+                        break;        
+                }
+                e.target.style.height = client.EXPAND_HEIGHT;
+            }
+            else /* normal command */
+            {
+                var destMethod = "onInput" + command[0].toUpperCase() +
+                    command.substr (1, command.length).toLowerCase();
+
+                if (typeof client[destMethod] != "function")
+                {
+                    client.display ("Unknown command '" + command + "'.",
+                                    "ERROR");
+                    return false;
+                }
+                
+                var type = "input-" + command.toLowerCase();
+                var ev = new CEvent ("client", type, client, destMethod);
+
+                ev.command = command;
+                ev.inputData =  ary[2] ? ary[2] : "";
+
+                ev.target = client.currentObject;
+
+                switch (client.currentObject.TYPE) /* set up event props */
+                {
+                    case "IRCChannel":
+                        ev.channel = ev.target;
+                        ev.server = ev.channel.parent;
+                        ev.network = ev.server.parent;
+                        break;
+
+                    case "IRCUser":
+                        ev.user = ev.target;
+                        ev.server = ev.user.parent;
+                        ev.network = ev.server.parent;
+                        break;
+
+                    case "IRCChanUser":
+                        ev.user = ev.target;
+                        ev.channel = ev.user.parent;
+                        ev.server = ev.channel.parent;
+                        ev.network = ev.server.parent;
+                        break;
+
+                    case "IRCNetwork":
+                        ev.network = client.currentObject;
+                        if (ev.network.isConnected())
+                            ev.server = ev.network.primServ;
+                        break;
+
+                    case "IRCClient":
+                        if (client.lastNetwork)
+                        {
+                            ev.network = client.lastNetwork;
+                            if (ev.network.isConnected())
+                                ev.server = ev.network.primServ;
+                        }
+                        break;
+
+                    default:
+                        /* no setup for unknown object */
+                        
+                }
+
+                client.eventPump.addEvent (ev);
+            }
+        }
+        else /* plain text */
+        {
+            client.sayToCurrentTarget (e.target.value);
+            e.target.value = "";            
+        }
+    }
+    else /* input-box is expanded */
+    {
+        var lines = e.target.value.split("\n");
+        for (i in lines)
+            if (lines[i] == "")
+                arrayRemoveAt (lines, i);
+        var lastLine = lines[lines.length - 1];
+
+        dd ("lines: " + lines);
+        
+        if (lastLine.replace(/s*$/,"") ==
+            e.target.getAttribute ("collapseChar"))
+        {
+            dd ("collapsing...");
+            
+            e.target.setAttribute("expanded", "NO");
+            e.target.style.height = client.COLLAPSE_HEIGHT;
+            e.target.value = "";
+            for (var i = 1; i < lines.length - 1; i++)
+                client.sayToCurrentTarget (lines[i]);
+        }
+    }
+    
+}
+
+client.onInputNetwork =
+function clie_inetwork (e)
+{
+    if (!e.inputData)
+    {
+        client.display ("network <network-name>", "USAGE");
+        return false;
+    }
+
+    var net = client.networks[e.inputData];
+    
+    if (net)
+        client.lastNetwork = net;
+    else
+        client.display ("Unknown network '" + e.inputData + "'", "ERROR");
+
+    return true;
+    
+}
+
+client.onInputConnect =
+function cli_iconnect (e)
+{
+    var net;
+    
+    if (!e.inputData)
+    {
+        if (client.lastNetwork)
+        {        
+            client.display ("No network specified network, " +
+                            "Using '" + client.lastNetwork.name + "'",
+                            "NOTICE");
+            net = client.lastNetwork;
+        }
+        else
+        {
+            client.display ("No network specified, and no default network " +
+                            "is in place.", "ERROR");
+            client.display ("connect <network-name>.", "USAGE");
+            return false;
+        }
+    }
+    else
+    {
+        net = client.networks[e.inputData];
+        if (!net)
+            client.display ("Unknown network '" + e.inputData + "'", "ERROR");
+        client.lastNetwork = net;
+    }
+
+    net.connect();
+    net.display ("Connecting...", "INFO");
+    setCurrentObject(net);
+    
+}
+    
+client.onInputMe =
+function cli_ime (e)
+{
+    if (e.channel)
+    {
+        e.channel.act (e.inputData);
+        client.channel.display (e.inputData, "ACTION", "!ME");
+    }
+}
+
+client.onInputNick =
+function cli_inick (e)
+{
+
+    if (!e.inputData)
+        return false;
+    
+    if (e.server) 
+        e.server.sendData ('NICK ' + e.inputData + '\n');
+    else
+        CIRCNetwork.prototype.INITIAL_NICK = e.inputData;
+    
+}
+
+client.onInputName =
+function cli_iname (e)
+{
+
+    if (!e.inputData)
+        return false;
+    
+    CIRCNetwork.prototype.INITIAL_NAME = e.inputData;
+    
+}
+
+client.onInputDesc =
+function cli_idesc (e)
+{
+
+    if (!e.inputData)
+        return false;
+    
+    CIRCNetwork.prototype.INITIAL_DESC = e.inputData;
+    
+}
+
+client.onInputRaw =
+function cli_iraw (e)
+{
+
+    client.primNet.primServ.sendData (e.inputData + "\n");
+    
+}
+
+client.onInputEval =
+function cli_ieval (e)
+{
+
+    try
+    {
+        rv = String(eval (e.inputData));
+        if (rv.indexOf ("\n") == -1)
+            client.display ("(" + e.inputData + ") " + rv, "EVAL");
+        else
+            client.display ("(" + e.inputData + ")\n" + rv, "EVAL");
+    }
+    catch (ex)
+    {
+        client.display (String(ex), "ERROR");
+    }
+    
+}
+    
+client.onInputJ = client.onInputJoin =
+function cli_ijoin (e)
+{
+    if (!e.network || !e.network.isConnected())
+    {
+        if (!e.network)
+            client.display ("No network selected.", "ERROR");
+        else
+            client.display ("Network '" + e.network.name + " is not connected.",
+                            "ERROR");        
+        return false;
+    }
+    
+    var name = e.inputData.match(/\S+/);
+    if (!name)
+    {
+        client.display ("join [#|&]<channel-name>", "USAGE");
+        return false;
+    }
+
+    if ((name[0] != "#") && (name[0] != "&"))
+        name = "#" + name;
+
+    e.channel = e.server.addChannel (String(name));
+    e.channel.join();
+    e.channel.display ("Joining...", "INFO");
+    setCurrentObject(e.channel);
+    
+}
+
+client.onInputP = client.onInputPart = client.onInputLeave =
+function cli_ipart (e)
+{
+    if (!e.channel)
+        return false;
+
+    e.channel.part();    
+    
+}
+
+CIRCNetwork.prototype.onNotice =
+function my_notice (e)
+{
+
+    this.display (e.meat, "NOTICE");
+    
+}
+
+CIRCChannel.prototype.onPrivmsg =
+function my_cprivmsg (e)
+{
+    
+    e.user.display (e.meat, "PRIVMSG");
+    
+    if (e.meat.indexOf (client.prefix) == 0)
+    {
+        try
+        {
+            var v = eval(e.meat.substring (client.prefix.length,
+                                           e.meat.length));
+        }
+        catch (ex)
+        {
+            this.say (e.user.nick + ": " + String(ex));
+            return false;
+        }
+        
+        if (typeof (v) != "undefined")
+        {						
+            if (v != null)                
+                v = String(v);
+            else
+                v = "null";
+            
+            var rsp = e.user.nick + ", your result is,";
+            
+            if (v.indexOf ("\n") != -1)
+                rsp += "\n";
+            else
+                rsp += " ";
+            
+            this.say (rsp + v);
+        }
+    }
+
+    return true;
+    
+}
+
+/* end of names */
+CIRCChannel.prototype.on366 =
+function my_366 (e)
+{
+
+    if (!this.list)
+        this.list = new CListBox();
+    else
+        this.list.clear();
+
+    for (var u in this.users)
+        this.list.add (this.users[u].getDecoratedNick());
+    
+}    
+    
+CIRCChannel.prototype.onNotice =
+function my_notice (e)
+{
+
+    e.user.display (e.meat, "NOTICE", e.user.nick);
+    
+}
+
+CIRCChannel.prototype.onCTCPAction =
+function my_caction (e)
+{
+
+    e.user.display (e.CTCPData, "ACTION");
+
+}
+
+CIRCChannel.prototype.onJoin =
+function my_cjoin (e)
+{
+
+    this.display(e.user.properNick + " has joined " + e.channel.name,
+                 "JOIN");
+    this.list.add (e.user.getDecoratedNick());
+    
+}
+
+CIRCChannel.prototype.onPart =
+function my_cpart (e)
+{
+
+    this.display (e.user.properNick + " has left " + e.channel.name, "PART");
+    this.list.remove (e.user.getDecoratedNick());
+    
+}
+
+CIRCChannel.prototype.onKick =
+function my_ckick (e)
+{
+
+    this.display (e.lamer.properNick + " was booted from " + e.channel.name +
+                  " by " + e.user.properNick + " (" + e.reason + ")", "KICK");
+    this.list.listContainer.removeChild (e.user.getDecoratedNick());
+
+}
+
+CIRCChannel.prototype.onNick =
+function my_cnick (e)
+{
+
+    this.display (e.oldNick + " is now known as " + e.user.properNick, "NICK");
+    e.user.updateDecoratedNick();
+    
+}
+
+
+CIRCChannel.prototype.onQuit =
+function my_cquit (e)
+{
+
+    this.display (e.user.properNick + " has left " + e.server.parent.name +
+                  " (" + e.reason + ")", "QUIT");
+    this.list.listContainer.removeChild (e.user.getDecoratedNick());
+    
+}
+
+CIRCUser.prototype.onPrivmsg =
+function my_cprivmsg (e)
+{
+    
+    this.display (e.meat, "PRIVMSG");
+    
+}
+
+CIRCUser.prototype.onNotice =
+function my_notice (e)
+{
+
+    this.display (e.meat, "NOTICE");
+    
+}
---
+++ extensions/irc/xul/tests/test3-static.js
@@ -0,0 +1,488 @@
+var client = new Object();
+
+client.COMMAND_CHAR = "/";
+client.STEP_TIMEOUT = 500;
+client.UPDATE_DELAY = 500;
+client.EXPAND_HEIGHT = "200px";
+client.COLLAPSE_HEIGHT = "25px";
+client.TYPE = "IRCClient";
+client.OP1_IMG = "g_blue_on.gif";    /* user is op image */
+client.OP0_IMG = "g_blue.gif"; /* user isnt op image */
+client.V1_IMG = "g_grey_on.gif";    /* user is voice image */
+client.V0_IMG = "g_grey.gif"; /* user isnt voide image */
+client.ACT_IMG = "green-on.gif";   /* view has activity image */
+client.NACT_IMG = "green-off.gif"; /* view has no activity image */
+client.CUR_IMG = "yellow-on.gif"; /* view is currently displayed */
+client.name = "*client*";
+client.viewsArray = new Array();
+client.currentObject = client;
+client.lastListType = "chan-users";
+
+CIRCNetwork.prototype.INITIAL_NICK = "IRCMonkey";
+CIRCNetwork.prototype.INITIAL_NAME = "chatzilla";
+CIRCNetwork.prototype.INITIAL_DESC = "New Now Know How";
+CIRCNetwork.prototype.INITIAL_CHANNEL = "";
+CIRCNetwork.prototype.MAX_MESSAGES = 50;
+
+CIRCServer.prototype.READ_TIMEOUT = 0;
+
+CIRCUser.prototype.MAX_MESSAGES = 100;
+
+CIRCChannel.prototype.MAX_MESSAGES = 200;
+
+CIRCChanUser.prototype.MAX_MESSAGES = 100;
+
+
+function initStatic()
+{
+    var obj;
+    
+    obj = document.getElementById("input");
+    obj.onkeyup = onInputKeyUp;
+
+    obj = document.getElementById("tb[*client*]");
+    
+    client.quickList = new CListBox(document.getElementById("quickList"));
+
+    client.display ("Welcome to ChatZilla", "HELP");
+    setCurrentObject (client);
+    
+}
+
+function initHost(obj)
+{
+    
+    obj.networks = new Object();
+    obj.eventPump = new CEventPump (10);
+    
+    obj.networks["efnet"] =
+	new CIRCNetwork ("efnet", [{name: "irc.primenet.com", port: 6667},
+                                  {name: "irc.cs.cmu.edu",   port: 6667}],
+                         obj.eventPump);
+    obj.networks["linuxnet"] =
+	new CIRCNetwork ("linuxnet", [{name: "irc.mozilla.org", port: 6667}],
+                         obj.eventPump);
+    obj.networks["hybridnet"] =
+        new CIRCNetwork ("hybridnet", [{name: "irc.ssc.net", port: 6667}],
+                         obj.eventPump);
+
+    obj.primNet = obj.networks["efnet"];
+
+    if (DEBUG)
+        /* hook all events EXCEPT server.poll and *.event-end types
+         * (the 4th param inverts the match) */
+        obj.eventPump.addHook ([{type: "poll", set: /^(server|dcc-chat)$/},
+                               {type: "event-end"}], event_tracer,
+                               "event-tracer", true /* negate */);
+    
+}
+
+/* timer-based mainloop */
+function mainStep()
+{
+
+    client.eventPump.stepEvents();
+    setTimeout ("mainStep()", client.STEP_TIMEOUT);
+    
+}
+
+function newInlineText (data, className)
+{
+    var a = document.createElement ("html:a");
+
+    if (data)
+        a.appendChild (document.createTextNode (data));
+    if (typeof className != "undefined")
+        a.setAttribute ("class", className);
+
+    return a;
+    
+}
+
+function setCurrentObject (obj)
+{
+    if (!obj.messages)
+    {
+        dd ("** INVALID OBJECT passed to setCurrentObject **");
+        return false;
+    }
+
+    var tb = getTBForObject(client.currentObject);
+    tb.setAttribute ("src", client.NACT_IMG);
+
+    var output = document.getElementById ("output");
+
+    output.removeChild (output.firstChild);
+    output.appendChild (obj.messages);
+
+    var quickList = document.getElementById ("quickList");
+    if (!obj.list)
+        obj.list = new CListBox();
+    
+    quickList.removeChild (quickList.firstChild);
+    quickList.appendChild (obj.list.listContainer);
+        
+    client.currentObject = obj;
+    tb = getTBForObject(obj);
+    tb.setAttribute ("src", client.CUR_IMG);
+    
+}
+
+function addHistory (source, obj)
+{
+
+    if (!source.messages)
+    {
+        source.messages = document.createElement ("span");
+        source.messages.setAttribute ("class", "chat-view");
+        source.messages.setAttribute ("type", source.TYPE);
+        
+        switch (source.TYPE)
+        {
+            case "IRCChanUser":
+            case "IRCUser":
+                source.messages.setAttribute ("nick", source.nick);
+                break;
+
+            case "IRCNetwork":
+            case "IRCChannel":
+            case "IRCClient":
+                source.messages.setAttribute ("name", source.name);
+                break;
+
+            default:
+                dd ("** 'source' has INVALID TYPE in addHistory **");
+                break;
+        }       
+    }
+
+    if (client.PRINT_DIRECTION == 1)
+        source.messages.appendChild (obj);
+    else
+        source.messages.insertBefore (obj, source.messages.firstChild);
+    
+    if (source.MAX_MESSAGES)
+    {
+        if (typeof source.messageCount != "number")
+            source.messageCount = 1;
+        else
+            source.messageCount++;
+
+        if (source.messageCount > source.MAX_MESSAGES)
+            source.messages.removeChild (source.messages.lastChild);
+    }
+    
+}
+
+function notifyActivity (source)
+{
+    var tb = getTBForObject (source);
+    
+    if (client.currentObject != source)
+        tb.setAttribute ("src", "green-on.gif");
+    
+}
+
+/* gets the toolbutton associated with an object
+ * creating it if it doesn't exist */
+function getTBForObject (source)
+{
+    var name, tb;
+
+    switch (source.TYPE)
+    {
+        case "IRCChanUser":
+        case "IRCUser":
+            name = source.nick;
+            break;
+            
+        case "IRCNetwork":
+        case "IRCChannel":
+        case "IRCClient":
+            name = source.name;
+            break;
+    }
+
+    var id = "tb[" + name + "]";
+    
+    if (!(tb = document.getElementById (id)))
+    {
+        var views = document.getElementById ("views-tbar");
+        var tbi = document.createElement ("toolbaritem");
+        tbi.setAttribute ("class", "activity-button");
+        tbi.setAttribute ("onclick", "onTBIClick('" + id + "')");
+        
+        var tb = document.createElement ("titledbutton");
+        tb.setAttribute ("id", id);
+        client.viewsArray.push (source);
+        tb.setAttribute ("viewKey", client.viewsArray.length - 1);
+        tb.setAttribute ("value", name);
+        tbi.appendChild (tb);
+        views.appendChild (tbi);
+    }
+
+    return tb;
+    
+}
+
+client.sayToCurrentTarget =
+function cli_say(msg)
+{
+
+    dd ("sayToCurrentTarget '" + msg + "'");
+    switch (client.currentObject.TYPE)
+    {
+        case "IRCChannel":
+        case "IRCUser":
+        case "IRCChanUser":
+            client.currentObject.say (msg);
+            break;
+
+        case "IRCClient":
+            client.onInputEval ({inputData: msg});
+            break;
+
+        default:
+            client.display ("No default action for objects of type '" +
+                            client.currentObject.TYPE + "'", "ERROR");
+            break;
+    }
+
+}
+
+client.display =
+function cli_display (message, msgtype)
+{
+    var ary = message.split ("\n");
+
+    var msgContainer = newInlineText ("", "msg");
+    msgContainer.setAttribute ("network", "{LOCAL}");
+    msgContainer.setAttribute ("msgtype", msgtype);
+    
+    var msg = newInlineText ("[" + msgtype + "]", "msg-type");
+    msg.setAttribute ("network", "{LOCAL}");
+    msg.setAttribute ("msgtype", msgtype);
+    msgContainer.appendChild(msg);
+
+    for (var l in ary)
+    {
+        var msg = newInlineText (ary[l], "msg-data");
+        msg.setAttribute ("network", "{LOCAL}");
+        msg.setAttribute ("msgtype", msgtype);
+        msgContainer.appendChild(msg);
+        msgContainer.appendChild (document.createElement ("html:br"));
+    }
+
+    addHistory (this, msgContainer);        
+    notifyActivity (this);
+
+}
+
+client.quit =
+function cli_quit (reason)
+{
+    
+    for (var n in client.networks)
+        if (client.networks[n].primServ)
+            client.networks[n].quit (reason);
+    
+}
+
+CIRCNetwork.prototype.display =
+function net_display (message, msgtype)
+{
+    var ary = message.split ("\n");
+
+    var msgContainer = newInlineText ("", "msg");
+    msgContainer.setAttribute ("network", this.name);
+    msgContainer.setAttribute ("msgtype", msgtype);
+
+    var msg = newInlineText ("[" + msgtype + "]", "msg-type");
+    msg.setAttribute ("network", this.name);
+    msg.setAttribute ("msgtype", msgtype);
+    msgContainer.appendChild(msg);
+        
+    for (var l in ary)
+    {
+        var msg = newInlineText (ary[l], "msg-data");
+        msg.setAttribute ("network", this.name);
+        msg.setAttribute ("msgtype", msgtype);
+        msgContainer.appendChild(msg);
+        msgContainer.appendChild (document.createElement ("html:br"));
+    }
+
+    addHistory (this, msgContainer);
+    notifyActivity (this);
+            
+}
+
+CIRCUser.prototype.getDecoratedNick =
+function usr_decoratednick()
+{
+
+    if (!this.decoNick)
+    {
+        var pfx;
+        var el = newInlineText();
+        
+        if (this.TYPE == "IRCChanUser")
+        {
+            var img = document.createElement ("html:img", "option-graphic");
+            img.setAttribute ("src", this.isOp ? client.OP1_IMG :
+                              client.OP0_IMG);
+            el.appendChild (img);
+            
+            img = document.createElement ("html:img", "option-graphic");
+            img.setAttribute ("src", this.isVoice ? client.V1_IMG :
+                              client.V0_IMG);
+            el.appendChild (img);
+        }
+        
+        el.appendChild (newInlineText (this.properNick, "option-text"));
+
+        this.decoNick = el;
+    }
+    
+    return this.decoNick;
+
+}
+
+CIRCUser.prototype.updateDecoratedNick =
+function usr_updnick()
+{
+    var decoNick = this.getDecoratedNick();
+
+    if (!decoNick)
+        return;
+    
+    if (this.TYPE == "IRCChanUser")
+    {
+        var obj = decoNick.firstChild;
+        obj.setAttribute ("src", this.isOp ? client.OP1_IMG :
+                          client.OP0_IMG);
+
+        obj = obj.nextSibling;
+        obj.setAttribute ("src", this.isVoice ? client.V1_IMG :
+                          client.V0_IMG);
+        
+        obj = obj.nextSibling;
+        obj.firstChild.data = this.properNick;
+    }
+    
+}
+
+CIRCUser.prototype.display =
+function user_display(message, msgtype)
+{
+    var ary = message.split ("\n");
+
+    dd ("user.display");
+
+    var msgContainer = newInlineText ("", "msg");
+    msgContainer.setAttribute ("network", this.parent.parent.name);
+    msgContainer.setAttribute ("user", this.nick);
+    msgContainer.setAttribute ("msgtype", msgtype);
+
+    var msg = newInlineText ("[" + msgtype + "]", "msg-type");
+    msg.setAttribute ("network", this.parent.parent.name);
+    msg.setAttribute ("user", this.nick);
+    msg.setAttribute ("msgtype", msgtype);
+    msgContainer.appendChild(msg);
+    
+    if (this.TYPE == "IRCUser")
+    {
+        for (var l in ary)
+        {
+            var msg = newInlineText (ary[l], "msg-data");
+            msg.setAttribute ("network", this.parent.parent.name);
+            msg.setAttribute ("user", this.nick);
+            msg.setAttribute ("msgtype", msgtype);
+            msgContainer.appendChild(msg);
+            msgContainer.appendChild (document.createElement ("html:br"));
+        }
+        addHistory (this, msgContainer);
+        notifyActivity (this);
+
+    }
+    else
+    {
+        this.parent.display (message, msgtype, this.nick);
+    }
+    
+}
+
+CIRCChannel.prototype.display =
+function chan_display (message, msgtype, nick)
+{
+    var ary = message.split ("\n");
+
+    dd ("channel.display");
+    
+    var msgContainer = newInlineText ("", "msg");
+    msgContainer.setAttribute ("network", this.parent.parent.name);
+    msgContainer.setAttribute ("channel", this.name);
+    msgContainer.setAttribute ("user", nick);
+    msgContainer.setAttribute ("msgtype", msgtype);
+
+    var msg = newInlineText ("[" + msgtype + "]", "msg-type");
+    msg.setAttribute ("network", this.parent.parent.name);
+    msg.setAttribute ("channel", this.name);
+    msg.setAttribute ("user", nick);
+    msg.setAttribute ("msgtype", msgtype);
+    msgContainer.appendChild (msg);
+    
+    for (var l in ary)
+    {
+        var msg = newInlineText (ary[l], "msg-data");
+        msg.setAttribute ("network", this.parent.parent.name);
+        msg.setAttribute ("channel", this.name);
+        msg.setAttribute ("user", nick);
+        msg.setAttribute ("msgtype", msgtype);
+        
+        if (nick)
+        {
+            var nickText;
+            var realNick = (nick == "!ME") ? this.parent.me.properNick
+                : this.users[nick].properNick;
+            
+            switch (msgtype)
+            {
+                
+                case "PRIVMSG":
+                    nickText = newInlineText ("<" + realNick + "> ",
+                                              "msg-user");
+                    break;
+                    
+                case "ACTION":
+                    nickText = newInlineText ("*" + realNick + "* ",
+                                              "msg-user");
+                    break;
+                    
+                case "NOTICE":
+                    nickText = newInlineText ("[" + realNick + "] ",
+                                              "msg-user");
+                    break;
+                    
+            }
+
+            if (nickText)
+            {
+                nickText.setAttribute ("network", this.parent.parent.name);
+                nickText.setAttribute ("channel", this.name);
+                nickText.setAttribute ("user", nick);
+                nickText.setAttribute ("msgtype", msgtype);
+                msgContainer.appendChild (nickText);
+            }
+
+            addHistory (this.users[nick], msgContainer);
+            
+        }
+
+        msgContainer.appendChild (msg);
+        msgContainer.appendChild (document.createElement ("html:br"));
+    }
+
+    addHistory (this, msgContainer);
+    notifyActivity (this);
+
+}
