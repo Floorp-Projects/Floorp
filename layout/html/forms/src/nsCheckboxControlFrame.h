2cb62e9b2e8992d72954de5a085b76b5614a8ac9
pollmann%netscape.com <pollmann%netscape.com>
1999-09-14 19:12:08 +0000

    Bug 13058: Implement nsIStatefulFrame for checkboxes

--- layout/html/forms/src/nsCheckboxControlFrame.h
+++ layout/html/forms/src/nsCheckboxControlFrame.h
@@ -19,6 +19,7 @@
 #define nsCheckboxControlFrame_h___
 
 #include "nsNativeFormControlFrame.h"
+#include "nsIStatefulFrame.h"
 
 //
 // nsCheckboxControlFrame
@@ -40,7 +41,9 @@
 // become checked since "mixed" doesn't exist on normal checkboxes.
 //
 
-class nsCheckboxControlFrame : public nsNativeFormControlFrame {
+class nsCheckboxControlFrame : public nsNativeFormControlFrame,
+                               public nsIStatefulFrame
+{
 private:
 	typedef nsNativeFormControlFrame Inherited;
 
@@ -84,9 +87,15 @@ public:
                          nsGUIEvent* aEvent,
                          nsEventStatus& aEventStatus);
 
-    // this should be protected, but VC6 is lame.
+   // this should be protected, but VC6 is lame.
   enum CheckState { eOff, eOn, eMixed } ;
-  
+
+   // nsIStatefulFrame
+  NS_IMETHOD GetStateType(StateType* aStateType);
+  NS_IMETHOD SaveState(nsISupports** aState);
+  NS_IMETHOD RestoreState(nsISupports* aState);
+  NS_IMETHOD QueryInterface(const nsIID& aIID, void** aInstancePtr);
+
 protected:
 
     // native/gfx implementations need to implement needs.
@@ -114,6 +123,9 @@ protected:
   static nsIAtom* GetTristateAtom() ;
   static nsIAtom* GetTristateValueAtom() ;
 
+private:
+  NS_IMETHOD_(nsrefcnt) AddRef() { return NS_OK; }
+  NS_IMETHOD_(nsrefcnt) Release() { return NS_OK; }
 }; // class nsCheckboxControlFrame
 
 #endif
