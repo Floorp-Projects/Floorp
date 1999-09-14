652decc4c420963b1a302bf55afe7b9ede84318a
pollmann%netscape.com <pollmann%netscape.com>
1999-09-14 01:58:49 +0000

    Bug 13058: Tweaks to StatefulFrame interface

--- layout/base/public/nsILayoutHistoryState.h
+++ layout/base/public/nsILayoutHistoryState.h
@@ -2,6 +2,7 @@
 #define _nsILayoutHistoryState_h
 
 #include "nsISupports.h"
+#include "nsIStatefulFrame.h" // Get StateType enum
 
 #define NS_ILAYOUTHISTORYSTATE_IID_STR "306c8ca0-5f0c-11d3-a9fb-000064657374"
 
@@ -9,14 +10,12 @@
 {0x306c8ca0, 0x5f0c, 0x11d3, \
 {0xa9, 0xfb, 0x00, 0x00, 0x64, 0x65, 0x73, 0x74}}
 
-#define NS_HISTORY_STATE_TYPE_NONE -1
-
 class nsILayoutHistoryState : public nsISupports {
  public: 
   NS_DEFINE_STATIC_IID_ACCESSOR(NS_ILAYOUTHISTORYSTATE_IID)
 
-  NS_IMETHOD AddState(PRUint32 aContentID, nsISupports* aState, PRInt32 aStateType) = 0;
-  NS_IMETHOD GetState(PRUint32 aContentID, nsISupports** aState, PRInt32 aStateType) = 0;
+  NS_IMETHOD AddState(PRUint32 aContentID, nsISupports* aState, StateType aStateType) = 0;
+  NS_IMETHOD GetState(PRUint32 aContentID, nsISupports** aState, StateType aStateType) = 0;
 };
 
 nsresult
--- layout/base/public/nsIStatefulFrame.h
+++ layout/base/public/nsIStatefulFrame.h
@@ -9,11 +9,16 @@
 {0x306c8ca0, 0x5f0c, 0x11d3, \
 {0xa9, 0xfb, 0x00, 0x00, 0x64, 0x65, 0x73, 0x74}}
 
+// If you implement nsIStatefulFrame, add an entry to this enum and use it
+// in your GetStateType method to prevent collisions.
+enum StateType {eNoType=-1, eCheckboxType, eFileType, eRadioType, eSelectType,
+                eTextType, eNumStateTypes};
+
 class nsIStatefulFrame : public nsISupports {
  public: 
   NS_DEFINE_STATIC_IID_ACCESSOR(NS_ISTATEFULFRAME_IID)
 
-  NS_IMETHOD GetStateType(PRInt32* aStateType) = 0;
+  NS_IMETHOD GetStateType(StateType* aStateType) = 0;
   NS_IMETHOD SaveState(nsISupports** aState) = 0;
   NS_IMETHOD RestoreState(nsISupports* aState) = 0;
 
