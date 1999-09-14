187950012003601f5dc83ff9bcfcfbeeccdc86a9
law@netscape.com <law@netscape.com>
1999-09-14 21:28:34 +0000

    Clean up

--- xpfe/browser/resources/content/viewsource.js
+++ xpfe/browser/resources/content/viewsource.js
@@ -1,41 +1,21 @@
   function onLoadViewSource() {
-    if ( !useOldAppCore ) {
-        StartupViewSource();
-    }
-  }
+    // Create and initialize the browser instance.
+    createBrowserInstance();
 
-  function StartupViewSource() {
-    if ( useOldAppCore ) {
-        // Generate unique name (var appCoreName declared in navigator.js).
-        appCoreName = "ViewSource." + ( new Date() ).getTime().toString();
-    
-        // Create and initialize the browser app core.
-        appCore = new BrowserAppCore();
-        appCore.Init( appCoreName );
+    if ( appCore ) {
+        appCore.isViewSource = true;
         appCore.setContentWindow(window.frames[0]);
         appCore.setWebShellWindow(window);
         appCore.setToolbarWindow(window);
-    
-        // Get url whose source to view.
-        var url = document.getElementById("args").getAttribute("value");
-    
-        // Load the source (the app core will magically know what to do).
-        appCore.loadUrl(url);
-    } else {
-        // Create and initialize the browser instance.
-        createBrowserInstance();
-    
-        if ( appCore ) {
-            appCore.isViewSource = true;
-            appCore.setContentWindow(window.frames[0]);
-            appCore.setWebShellWindow(window);
-            appCore.setToolbarWindow(window);
-        }
-        
+
         // Get url whose source to view.
         var url = window.arguments[0];
     
         // Load the source (the app core will magically know what to do).
         appCore.loadUrl( url );
+    } else {
+        // Give up.
+        alert( "Error creating browser instance\n" );
     }
+    
   }
