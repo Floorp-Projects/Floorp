2a3a1917bfce45013355e1d8466d5bdc1e8da15b
valeski%netscape.com <valeski%netscape.com>
1999-09-17 18:05:11 +0000

    14143

--- netwerk/streamconv/src/nsStreamConverterService.cpp
+++ netwerk/streamconv/src/nsStreamConverterService.cpp
@@ -343,9 +343,10 @@ nsStreamConverterService::FindConverter(const char *aProgID, nsVoidArray **aEdge
     if (!source) return NS_ERROR_OUT_OF_MEMORY;
 
     SCTableData *data = (SCTableData*)lBFSTable.Get(source);
+    NS_ASSERTION(data, "tried to convert a stream who's from type wasn't registered");
+    if (!data) return NS_ERROR_FAILURE;
+
     BFSState *state = (BFSState*)data->data;
-    // XXX probably don't need this check.
-    if (!state) return NS_ERROR_FAILURE;
 
     state->color = gray;
     state->distance = 0;
