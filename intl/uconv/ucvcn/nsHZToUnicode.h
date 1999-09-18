dbe706448c8868b2a6e7b1f304843499bc34cae5
ftang%netscape.com <ftang%netscape.com>
1999-09-18 00:05:27 +0000

    check in HZ to Unicode contributed by Xu, Yueheng <yueheng.xu@intel.com>

--- intl/uconv/ucvcn/nsHZToUnicode.h
+++ intl/uconv/ucvcn/nsHZToUnicode.h
@@ -15,3 +15,61 @@
  * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
  * Reserved.
  */
+
+#ifndef nsHZToUnicode_h___
+#define nsHZToUnicode_h___
+
+#include "nsUCvCnSupport.h"
+
+//----------------------------------------------------------------------
+// Class nsHZToUnicode [declaration]
+
+/**
+ * A character set converter from GBK to Unicode.
+ * 
+ *
+ * @created         08/Sept/1999
+ * @author  Yueheng Xu, Yueheng.Xu@intel.com
+ */
+class nsHZToUnicode : public nsBufferDecoderSupport
+{
+public:
+		
+  /**
+   * Class constructor.
+   */
+  nsHZToUnicode() {};
+  virtual ~nsHZToUnicode() {};
+
+  /**
+   * Static class constructor.
+   */
+  static nsresult CreateInstance(nsISupports **aResult);
+
+
+protected:
+
+  //--------------------------------------------------------------------
+  // Subclassing of nsDecoderSupport class [declaration]
+  NS_IMETHOD ConvertNoBuff(const char* aSrc,
+											   PRInt32 * aSrcLength,
+											   PRUnichar *aDest,
+										       PRInt32 * aDestLength);
+
+  NS_IMETHOD GetMaxLength(const char * aSrc, PRInt32 aSrcLength, 
+      PRInt32 * aDestLength);
+
+private:
+
+typedef struct
+{
+    char leftbyte;
+    char rightbyte;
+
+} DByte;
+  void HZToUnicode(DByte *pGBCode, PRUnichar * pUnicode);
+  void GBToUnicode(DByte *pGBCode, PRUnichar * pUnicode);
+
+};
+
+#endif /* nsHZToUnicode_h___ */
