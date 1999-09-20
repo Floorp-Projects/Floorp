666c4ceea032a5597fb4d531c1cac4dbaa7f4f38
locka@iol.ie <locka@iol.ie>
1999-09-20 22:00:30 +0000

    Added safe for scripting support

--- webshell/embed/ActiveX/ControlSite.h
+++ webshell/embed/ActiveX/ControlSite.h
@@ -20,6 +20,9 @@
 
 #include "IOleCommandTargetImpl.h"
 
+// If you created a class derived from CControlSite, use the following macro
+// in the interface map of the derived class to include all the necessary
+// interfaces.
 #define CCONTROLSITE_INTERFACES() \
 	COM_INTERFACE_ENTRY(IOleWindow) \
 	COM_INTERFACE_ENTRY(IOleClientSite) \
@@ -35,6 +38,29 @@
 	COM_INTERFACE_ENTRY(IOleCommandTarget)
 
 
+//
+// Class for hosting an ActiveX control
+//
+// This class supports both windowed and windowless classes. The normal
+// steps to hosting a control are this:
+//
+//   CControlSiteInstance *pSite = NULL;
+//   CControlSiteInstance::CreateInstance(&pSite);
+//   pSite->AddRef();
+//   pSite->Create(clsidControlToCreate);
+//   pSite->Attach(hwndParentWindow, rcPosition);
+//
+// Where propertyList is a named list of values to initialise the new object
+// with, hwndParentWindow is the window in which the control is being created,
+// and rcPosition is the position in window coordinates where the control will
+// be rendered.
+//
+// Destruction is this:
+//
+//   pSite->Detach();
+//   pSite->Release();
+//   pSite = NULL;
+
 class CControlSite :	public CComObjectRootEx<CComSingleThreadModel>,
 						public IOleClientSite,
 						public IOleInPlaceSiteWindowless,
@@ -43,6 +69,8 @@ class CControlSite :	public CComObjectRootEx<CComSingleThreadModel>,
 						public IDispatch,
 						public IOleCommandTargetImpl<CControlSite>
 {
+protected:
+// Site management values
 	// Handle to parent window
 	HWND m_hWndParent;
 	// Position of the site and the contained object
@@ -64,18 +92,17 @@ class CControlSite :	public CComObjectRootEx<CComSingleThreadModel>,
 	// Flag indicating if only safely scriptable controls are allowed
 	unsigned m_bSafeForScriptingObjectsOnly:1;
 
+// Pointers to object interfaces
 	// Raw pointer to the object
 	CComPtr<IUnknown> m_spObject;
 	// Pointer to objects IViewObject interface
 	CComQIPtr<IViewObject, &IID_IViewObject> m_spIViewObject;
 	// Pointer to object's IOleObject interface
 	CComQIPtr<IOleObject, &IID_IOleObject> m_spIOleObject;
-
 	// Pointer to object's IOleInPlaceObject interface
 	CComQIPtr<IOleInPlaceObject, &IID_IOleInPlaceObject> m_spIOleInPlaceObject;
 	// Pointer to object's IOleInPlaceObjectWindowless interface
 	CComQIPtr<IOleInPlaceObjectWindowless, &IID_IOleInPlaceObjectWindowless> m_spIOleInPlaceObjectWindowless;
-
 	// Name of this control
 	tstring m_szName;
 	// CLSID of the control
@@ -83,8 +110,7 @@ class CControlSite :	public CComObjectRootEx<CComSingleThreadModel>,
 	// Parameter list
 	PropertyList m_ParameterList;
 
-	// Double buffer drawing variables used for windowless controls
-	
+// Double buffer drawing variables used for windowless controls
 	// Area of buffer
 	RECT m_rcBuffer;
 	// Bitmap to buffer
@@ -98,9 +124,7 @@ class CControlSite :	public CComObjectRootEx<CComSingleThreadModel>,
 	// Flags indicating how the buffer was painted
 	DWORD m_dwBufferFlags;
 
-
-	// Ambient properties
-
+// Ambient properties
 	// Locale ID
 	LCID m_nAmbientLocale;
 	// Foreground colour
@@ -119,6 +143,7 @@ protected:
 	virtual void FireAmbientPropertyChange(DISPID id);
 
 public:
+// Construction and destruction
 	// Constructor
 	CControlSite();
 	// Destructor
@@ -131,79 +156,92 @@ END_COM_MAP()
 BEGIN_OLECOMMAND_TABLE()
 END_OLECOMMAND_TABLE()
 
+	// List of controls
+	static std::list<CControlSite *> m_cControlList;
+
+	// Helper method
+	static HRESULT ClassImplementsCategory(const CLSID &clsid, const CATID &catid);
+
 	// Returns the window used when processing ole commands
 	HWND GetCommandTargetWindow()
 	{
 		return NULL; // TODO
 	}
 
-	// List of controls
-	static std::list<CControlSite *> m_cControlList;
-
-	virtual HRESULT Create(REFCLSID clsid, PropertyList &pl, const tstring szName = _T(""));
-	virtual HRESULT Attach(HWND hwndParent, const RECT &rcPos, IUnknown *pInitStream);
+// Object creation and management functions
+	// Creates and initialises an object
+	virtual HRESULT Create(REFCLSID clsid, PropertyList &pl = PropertyList(), const tstring szName = _T(""));
+	// Attaches the object to the site
+	virtual HRESULT Attach(HWND hwndParent, const RECT &rcPos, IUnknown *pInitStream = NULL);
+	// Detaches the object from the site
 	virtual HRESULT Detach();
+	// Returns the IUnknown pointer for the object
 	virtual HRESULT GetControlUnknown(IUnknown **ppObject);
+	// Sets the bounding rectangle for the object
 	virtual HRESULT SetPosition(const RECT &rcPos);
+	// Draws the object using the provided DC
 	virtual HRESULT Draw(HDC hdc);
+	// Performs the specified action on the object
 	virtual HRESULT DoVerb(LONG nVerb, LPMSG lpMsg = NULL);
+	// Sets an advise sink up for changes to the object
 	virtual HRESULT Advise(IUnknown *pIUnkSink, const IID &iid, DWORD *pdwCookie);
+	// Removes an advise sink
 	virtual HRESULT Unadvise(const IID &iid, DWORD dwCookie);
 
-	// Methods to set ambient properties
+// Methods to set ambient properties
 	virtual void SetAmbientUserMode(BOOL bUser);
 
-	// Inline methods
-
+// Inline helper methods
+	// Returns the object's CLSID
 	virtual const CLSID &GetObjectCLSID() const
 	{
 		return m_clsid;
 	}
-
+	// Returns the name of the object
 	virtual const tstring &GetObjectName() const
 	{
 		return m_szName;
 	}
-
+	// Tests if the object is valid or not
 	virtual BOOL IsObjectValid() const
 	{
 		return (m_spObject) ? TRUE : FALSE;
 	}
-
+	// Returns the parent window to this one
 	virtual HWND GetParentWindow() const
 	{
 		return m_hWndParent;
 	}
-
+	// Returns the inplace active state of the object
 	virtual BOOL IsInPlaceActive() const
 	{
 		return m_bInPlaceActive;
 	}
 
-	// IDispatch
+// IDispatch
 	virtual HRESULT STDMETHODCALLTYPE GetTypeInfoCount(/* [out] */ UINT __RPC_FAR *pctinfo);
 	virtual HRESULT STDMETHODCALLTYPE GetTypeInfo(/* [in] */ UINT iTInfo, /* [in] */ LCID lcid, /* [out] */ ITypeInfo __RPC_FAR *__RPC_FAR *ppTInfo);
 	virtual HRESULT STDMETHODCALLTYPE GetIDsOfNames(/* [in] */ REFIID riid, /* [size_is][in] */ LPOLESTR __RPC_FAR *rgszNames, /* [in] */ UINT cNames, /* [in] */ LCID lcid, /* [size_is][out] */ DISPID __RPC_FAR *rgDispId);
 	virtual /* [local] */ HRESULT STDMETHODCALLTYPE Invoke(/* [in] */ DISPID dispIdMember, /* [in] */ REFIID riid, /* [in] */ LCID lcid, /* [in] */ WORD wFlags, /* [out][in] */ DISPPARAMS __RPC_FAR *pDispParams, /* [out] */ VARIANT __RPC_FAR *pVarResult, /* [out] */ EXCEPINFO __RPC_FAR *pExcepInfo, /* [out] */ UINT __RPC_FAR *puArgErr);
 
-	// IAdviseSink implementation
+// IAdviseSink implementation
     virtual /* [local] */ void STDMETHODCALLTYPE OnDataChange(/* [unique][in] */ FORMATETC __RPC_FAR *pFormatetc, /* [unique][in] */ STGMEDIUM __RPC_FAR *pStgmed);
     virtual /* [local] */ void STDMETHODCALLTYPE OnViewChange(/* [in] */ DWORD dwAspect, /* [in] */ LONG lindex);
     virtual /* [local] */ void STDMETHODCALLTYPE OnRename(/* [in] */ IMoniker __RPC_FAR *pmk);
     virtual /* [local] */ void STDMETHODCALLTYPE OnSave(void);
     virtual /* [local] */ void STDMETHODCALLTYPE OnClose(void);
 
-	// IAdviseSink2
+// IAdviseSink2
 	virtual /* [local] */ void STDMETHODCALLTYPE OnLinkSrcChange(/* [unique][in] */ IMoniker __RPC_FAR *pmk);
 
-	// IAdviseSinkEx implementation
+// IAdviseSinkEx implementation
     virtual /* [local] */ void STDMETHODCALLTYPE OnViewStatusChange(/* [in] */ DWORD dwViewStatus);
 
-	// IOleWindow implementation
+// IOleWindow implementation
 	virtual /* [input_sync] */ HRESULT STDMETHODCALLTYPE GetWindow(/* [out] */ HWND __RPC_FAR *phwnd);
 	virtual HRESULT STDMETHODCALLTYPE ContextSensitiveHelp(/* [in] */ BOOL fEnterMode);
 
-	// IOleClientSite implementation
+// IOleClientSite implementation
 	virtual HRESULT STDMETHODCALLTYPE SaveObject(void);
 	virtual HRESULT STDMETHODCALLTYPE GetMoniker(/* [in] */ DWORD dwAssign, /* [in] */ DWORD dwWhichMoniker, /* [out] */ IMoniker __RPC_FAR *__RPC_FAR *ppmk);
 	virtual HRESULT STDMETHODCALLTYPE GetContainer(/* [out] */ IOleContainer __RPC_FAR *__RPC_FAR *ppContainer);
@@ -211,7 +249,7 @@ END_OLECOMMAND_TABLE()
 	virtual HRESULT STDMETHODCALLTYPE OnShowWindow(/* [in] */ BOOL fShow);
 	virtual HRESULT STDMETHODCALLTYPE RequestNewObjectLayout(void);
 
-	// IOleInPlaceSite implementation
+// IOleInPlaceSite implementation
 	virtual HRESULT STDMETHODCALLTYPE CanInPlaceActivate(void);
 	virtual HRESULT STDMETHODCALLTYPE OnInPlaceActivate(void);
 	virtual HRESULT STDMETHODCALLTYPE OnUIActivate(void);
@@ -223,12 +261,12 @@ END_OLECOMMAND_TABLE()
 	virtual HRESULT STDMETHODCALLTYPE DeactivateAndUndo(void);
 	virtual HRESULT STDMETHODCALLTYPE OnPosRectChange(/* [in] */ LPCRECT lprcPosRect);
 
-	// IOleInPlaceSiteEx implementation
+// IOleInPlaceSiteEx implementation
 	virtual HRESULT STDMETHODCALLTYPE OnInPlaceActivateEx(/* [out] */ BOOL __RPC_FAR *pfNoRedraw, /* [in] */ DWORD dwFlags);
 	virtual HRESULT STDMETHODCALLTYPE OnInPlaceDeactivateEx(/* [in] */ BOOL fNoRedraw);
 	virtual HRESULT STDMETHODCALLTYPE RequestUIActivate(void);
 
-	// IOleInPlaceSiteWindowless implementation
+// IOleInPlaceSiteWindowless implementation
 	virtual HRESULT STDMETHODCALLTYPE CanWindowlessActivate(void);
 	virtual HRESULT STDMETHODCALLTYPE GetCapture(void);
 	virtual HRESULT STDMETHODCALLTYPE SetCapture(/* [in] */ BOOL fCapture);
@@ -242,7 +280,7 @@ END_OLECOMMAND_TABLE()
 	virtual HRESULT STDMETHODCALLTYPE AdjustRect(/* [out][in] */ LPRECT prc);
 	virtual HRESULT STDMETHODCALLTYPE OnDefWindowMessage(/* [in] */ UINT msg, /* [in] */ WPARAM wParam, /* [in] */ LPARAM lParam, /* [out] */ LRESULT __RPC_FAR *plResult);
 
-	// IOleControlSite implementation
+// IOleControlSite implementation
 	virtual HRESULT STDMETHODCALLTYPE OnControlInfoChanged(void);
 	virtual HRESULT STDMETHODCALLTYPE LockInPlaceActive(/* [in] */ BOOL fLock);
 	virtual HRESULT STDMETHODCALLTYPE GetExtendedControl(/* [out] */ IDispatch __RPC_FAR *__RPC_FAR *ppDisp);
@@ -253,5 +291,7 @@ END_OLECOMMAND_TABLE()
 };
 
 typedef CComObject<CControlSite> CControlSiteInstance;
+
+
 
 #endif
