dd2595e91174bddb3848be458c5344723b190526
brade@netscape.com <brade@netscape.com>
1999-09-21 22:29:44 +0000

    allow txn to get/set string and id

--- editor/base/EditAggregateTxn.h
+++ editor/base/EditAggregateTxn.h
@@ -12,7 +12,7 @@
  *
  * The Initial Developer of this code under the NPL is Netscape
  * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Copyright (C) 1998-1999 Netscape Communications Corporation.  All Rights
  * Reserved.
  */
 
@@ -80,6 +80,8 @@ public:
 
   /** get the name assigned to this txn */
   NS_IMETHOD GetName(nsIAtom **aName);
+
+  enum { kTransactionID = 11210 };
 
 protected:
 
