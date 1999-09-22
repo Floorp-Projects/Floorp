ad97c5b35685f92f211d8eaedd11ed65722d2c2c
alecf%netscape.com <alecf%netscape.com>
1999-09-22 03:40:07 +0000

    fix GetPassword() bustage

--- mailnews/compose/tests/sendlater/test.cpp
+++ mailnews/compose/tests/sendlater/test.cpp
@@ -108,7 +108,7 @@ nsresult OnIdentityCheck()
 			printf("User Name: %s\n", value ? value : "");
 			incomingServer->GetHostName(&value);
 			printf("Pop Server: %s\n", value ? value : "");
-			incomingServer->GetPassword(PR_FALSE, &value);
+			incomingServer->GetPassword(&value);
 			printf("Pop Password: %s\n", value ? value : "");
 
 			NS_RELEASE(incomingServer);
