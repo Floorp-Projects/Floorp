ec0d89055e177ef803e7792bf5312c7ad902eb46
norris%netscape.com <norris%netscape.com>
1999-09-24 18:20:08 +0000

    Implement the "cast" of java class objects so that js objects in java adapters can use 'this' for the implemented java interface.

--- js/rhino/org/mozilla/javascript/NativeJavaClass.java
+++ js/rhino/org/mozilla/javascript/NativeJavaClass.java
@@ -118,6 +118,21 @@ public class NativeJavaClass extends NativeJavaObject implements Function {
                        Object[] args)
         throws JavaScriptException
     {
+        // If it looks like a "cast" of an object to this class type,
+        // walk the prototype chain to see if there's a wrapper of a 
+        // object that's an instanceof this class.
+        if (args.length == 1 && args[0] instanceof Scriptable) {
+            Class c = getClassObject();
+            Scriptable p = (Scriptable) args[0];
+            do {
+                if (p instanceof Wrapper) {
+                    Object o = ((Wrapper) p).unwrap();
+                    if (c.isInstance(o))
+                        return p;
+                }
+                p = p.getPrototype();
+            } while (p != null);
+        }
         return construct(cx, scope, args);
     }
 
