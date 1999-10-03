7501e2d29603c497c66fecf9d3c296abc5647e75
hyatt%netscape.com <hyatt%netscape.com>
1999-10-03 01:41:06 +0000

    Fixing apprunner startup bustage on mac. r=beard

--- build/mac/NGLayoutBuildList.pm
+++ build/mac/NGLayoutBuildList.pm
@@ -1236,7 +1236,7 @@ sub MakeResourceAliases()
     _InstallResources(":mozilla:xpfe:global:resources:content:MANIFEST",			"$global_chrome_dir:content:default");
     _InstallResources(":mozilla:xpfe:global:resources:content:mac:MANIFEST",		"$global_chrome_dir:content:default");
     _InstallResources(":mozilla:xpfe:global:resources:skin:MANIFEST",               "$global_chrome_dir:skin:default");
-	_InstallResources(":mozilla:xpfe:global:resources:skin:MANIFEST_CHROME",        "$global_chrome_dir");
+	_InstallResources(":mozilla:xpfe:global:resources:skin:MANIFEST_CHROME",        "$chrome_dir");
 	_InstallResources(":mozilla:xpfe:global:resources:locale:MANIFEST",				"$global_chrome_dir:locale:en-US:", 0);
 	_InstallResources(":mozilla:xpfe:global:resources:locale:mac:MANIFEST",			"$global_chrome_dir:locale:en-US:", 0);
 
