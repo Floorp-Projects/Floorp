70bbf31062adbd9c7ca58ba8b6385eb26a4f2446
dougt@netscape.com <dougt@netscape.com>
1999-10-05 21:15:30 +0000

    Change pref name Added Preferences for software notification timer (see 14729) r. samir g.

--- xpinstall/public/xpinstall.js
+++ xpinstall/public/xpinstall.js
@@ -1,3 +1,6 @@
 pref("xpinstall.enabled",       true);
 pref("xpinstall.manual_confirm",  true);
-pref("xpinstall.notifications_enabled",  true);
+
+pref("xpinstall.notifications.enabled",  true);
+pref("xpinstall.notifications.interval", 1);
+pref("xpinstall.notifications.lastDate", 0);
