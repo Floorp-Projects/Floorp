8e2a51ab025c7f3fdbad9e59cb8fa4fba5c3ef97
jband%netscape.com <jband%netscape.com>
1999-10-05 21:07:23 +0000

    the JS engine part of the fix for bug 14738. This does two things: 1) expose the JSClass for JSError so that it is possible to detect from the outside if a given JSObject is of that JSError class, and 2) expose an api call via which the JSErrorReport can be extracted from a JSError (if present). This change required renaming the JSClass used for JSError, so many lines were touched

--- js/src/jsexn.h
+++ js/src/jsexn.h
@@ -77,9 +77,12 @@ js_ErrorToException(JSContext *cx, const char *message, JSErrorReport *reportp);
 extern JSBool
 js_ReportUncaughtException(JSContext *cx);
 
-extern JSClass exn_class;
+extern JS_FRIEND_DATA(JSClass) js_ErrorClass;
 extern JSBool
 js_exnHasInstance(JSContext *cx, JSObject *obj, jsval v, JSBool *bp);
+
+extern JSErrorReport *
+js_ErrorFromException(JSContext *cx, jsval exn);
 
 JS_END_EXTERN_C
 
