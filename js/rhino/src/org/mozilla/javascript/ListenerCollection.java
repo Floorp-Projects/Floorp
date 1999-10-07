6757acf35ace0bb340d372f5457087dda3466477
norris%netscape.com <norris%netscape.com>
1999-10-07 16:57:01 +0000

    Subject:             Re: NPL vs. MPL        Date:             Wed, 06 Oct 1999 18:30:34 -0400       From:             "Ian D. Stewart" <idstewart@softhome.net>         To:             Norris Boyd <norris@netscape.com>  References:             1 , 2 , 3
    Norris Boyd wrote:
    
      Great. So I'd like to change this copyright text
    
      /* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset:
      4 -*-
       *
       * The contents of this file are subject to the Mozilla Public License
       * Version 1.0 (the "MozPL"); you may not use this file except in
       * compliance with the MozPL.  You may obtain a copy of the MozPL at
       * http://www.mozilla.org/NPL/
       *
       * Software distributed under the MozPL is distributed on an "AS IS"
      basis,
       * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
      MozPL
       * for the specific language governing rights and limitations under the
       * MozPL.
       *
       * The Initial Developer of this code under the MozPL is Ian D. Stewart.
    
       * Portions created by Ian D. Stewart are Copyright (C) 1998, 1999
       * Ian D. Stewart.
       * All Rights Reserved.
       */
    
      to this:
    
      /* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset:
      4 -*-
       *
       * The contents of this file are subject to the Netscape Public
       * License Version 1.1 (the "License"); you may not use this file
       * except in compliance with the License. You may obtain a copy of
       * the License at http://www.mozilla.org/NPL/
       *
       * Software distributed under the License is distributed on an "AS
       * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express oqr
       * implied. See the License for the specific language governing
       * rights and limitations under the License.
       *
       * The Original Code is ListenerCollection, released
       * May 15, 1998.
       *
       * The Initial Developer of the Original Code is Ian D. Stewart.
       * Portions created by Ian D. Stewart are Copyright (C) 1998, 1999
       * Ian D. Stewart.
       * Rights Reserved.
       *
       * Contributor(s):
       * Ian D. Stewart
       *
       * Alternatively, the contents of this file may be used under the
       * terms of the GNU Public License (the "GPL"), in which case the
       * provisions of the GPL are applicable instead of those above.
       * If you wish to allow use of your version of this file only
       * under the terms of the GPL and not to allow others to use your
       * version of this file under the NPL, indicate your decision by
       * deleting the provisions above and replace them with the notice
       * and other provisions required by the GPL.  If you do not delete
       * the provisions above, a recipient may use your version of this
       * file under either the NPL or the GPL.
       */
    
      Can you give me your approval for this change?
    
    Make it so.
    
    
    Ian

--- js/rhino/src/org/mozilla/javascript/ListenerCollection.java
+++ js/rhino/src/org/mozilla/javascript/ListenerCollection.java
@@ -1,20 +1,38 @@
 /* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.0 (the "MozPL"); you may not use this file except in
- * compliance with the MozPL.  You may obtain a copy of the MozPL at
- * http://www.mozilla.org/NPL/
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
  *
- * Software distributed under the MozPL is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the MozPL
- * for the specific language governing rights and limitations under the
- * MozPL.
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express oqr
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
  *
- * The Initial Developer of this code under the MozPL is Ian D. Stewart.
- * Portions created by Ian D. Stewart are Copyright (C) 1998, 1999 
+ * The Original Code is ListenerCollection, released
+ * May 15, 1998.
+ *
+ * The Initial Developer of the Original Code is Ian D. Stewart.
+ * Portions created by Ian D. Stewart are Copyright (C) 1998, 1999
  * Ian D. Stewart.
- * All Rights Reserved.
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ * Ian D. Stewart
+ *
+ * Alternatively, the contents of this file may be used under the
+ * terms of the GNU Public License (the "GPL"), in which case the
+ * provisions of the GPL are applicable instead of those above.
+ * If you wish to allow use of your version of this file only
+ * under the terms of the GPL and not to allow others to use your
+ * version of this file under the NPL, indicate your decision by
+ * deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL.  If you do not delete
+ * the provisions above, a recipient may use your version of this
+ * file under either the NPL or the GPL.
  */
+
 
 /* This class provides a series of methods for accessing event listeners. */
 
