1cc282ce093acc732f1aa82ea1ea3f7d1258cabf
rogerl@netscape.com <rogerl@netscape.com>
1999-10-11 22:38:40 +0000

    NOT A PART OF SEAMONKEY BUILD - removed redundant test & fixed test for 'super' to detect syntax error

--- js/tests/ecma_2/LexicalConventions/keywords-001.js
+++ js/tests/ecma_2/LexicalConventions/keywords-001.js
@@ -16,13 +16,19 @@
     // Regular Expression Literals may not be empty; // should be regarded
     // as a comment, not a RegExp literal.
 
-    var result = "passed";
+    var result = "failed";
 
-    super;
+    try {
+		eval("super;");
+	} 
+	catch (x) {
+		if (x instanceof SyntaxError)
+			result = x.toString();
+	}
 
     AddTestCase(
         "using the expression \"super\" shouldn't cause js to crash",
-        "passed" ,
+        "SyntaxError: super is a reserved identifier" ,
         result );
 
     test();
