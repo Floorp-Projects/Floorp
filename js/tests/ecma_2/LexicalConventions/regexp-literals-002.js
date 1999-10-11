0ac59822f476cce690d826ff5c3c208b572f95a2
rogerl@netscape.com <rogerl@netscape.com>
1999-10-11 22:59:25 +0000

    NOT A PART OF SEAMONKEY BUILD - fixed test ECMA references, - added test to -002

--- js/tests/ecma_2/LexicalConventions/regexp-literals-002.js
+++ js/tests/ecma_2/LexicalConventions/regexp-literals-002.js
@@ -1,12 +1,12 @@
 /**
- *  File Name:          LexicalConventions/regexp-literals-001.js
- *  ECMA Section:       7.7.5
- *  Description:        Based on ECMA 2 Draft 7 February 1999
+ *  File Name:          LexicalConventions/regexp-literals-002.js
+ *  ECMA Section:       7.8.5
+ *  Description:        Based on ECMA 2 Draft 8 October 1999
  *
  *  Author:             christine@netscape.com
  *  Date:               19 February 1999
  */
-    var SECTION = "LexicalConventions/regexp-literals-001.js";
+    var SECTION = "LexicalConventions/regexp-literals-002.js";
     var VERSION = "ECMA_2";
     var TITLE   = "Regular Expression Literals";
 
@@ -14,5 +14,9 @@
 
     // A regular expression literal represents an object of type RegExp.
 
-    Object.prototype.getInternalClass = Object.prototype.toString;
+    AddTestCase(
+        "// A regular expression literal represents an object of type RegExp.",
+        "true",
+        (/x*/ instanceof RegExp).toString() );
 
+    test();
