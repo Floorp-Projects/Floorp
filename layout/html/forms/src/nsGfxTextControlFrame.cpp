be2e4b8643ae02a4859808f8ae23abfa5654912e
kmcclusk%netscape.com <kmcclusk%netscape.com>
1999-10-12 21:42:31 +0000

    Set state of checkbox and Radio button on MouseUp instead of MouseDown so JavaScript onclick event handlers can retreive the correct state of the form element. bug 15366; r=rods@netscape.com
    Submit the form when a text field is clicked on only if the JavaScript event handler
    has not consumed the event. bug 15949; r=rods@netscape.com

--- layout/html/forms/src/nsGfxTextControlFrame.cpp
+++ layout/html/forms/src/nsGfxTextControlFrame.cpp
@@ -315,18 +315,20 @@ nsGfxTextControlFrame::EnterPressed(nsIPresContext& aPresContext)
   if (mFormFrame && mFormFrame->CanSubmit(*this)) {
     nsIContent *formContent = nsnull;
 
+    nsEventStatus status = nsEventStatus_eIgnore;
+
     mFormFrame->GetContent(&formContent);
     if (nsnull != formContent) {
       nsEvent event;
-      nsEventStatus status = nsEventStatus_eIgnore;
-
       event.eventStructType = NS_EVENT;
       event.message = NS_FORM_SUBMIT;
       formContent->HandleDOMEvent(aPresContext, &event, nsnull, NS_EVENT_FLAG_INIT, status);
       NS_RELEASE(formContent);
     }
 
-    mFormFrame->OnSubmit(&aPresContext, this);
+    if (nsEventStatus_eConsumeNoDefault != status) {
+      mFormFrame->OnSubmit(&aPresContext, this);
+    }
   }
 }
 
