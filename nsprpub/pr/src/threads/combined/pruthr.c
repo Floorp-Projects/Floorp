547b778f83bb9f34297ad5d8c4b359d32b24dbf3
sdagley@netscape.com <sdagley@netscape.com>
1999-10-14 23:33:13 +0000

    Enforce _MD_DEFAULT_STACK_SIZE as the minimum stack size on Mac. r=beard

--- nsprpub/pr/src/threads/combined/pruthr.c
+++ nsprpub/pr/src/threads/combined/pruthr.c
@@ -1113,6 +1113,11 @@ PR_IMPLEMENT(PRThread*) _PR_CreateThread(PRThreadType type,
     native = (((scope == PR_GLOBAL_THREAD)|| (scope == PR_GLOBAL_BOUND_THREAD))
 							&& _PR_IS_NATIVE_THREAD_SUPPORTED());
 
+#ifdef XP_MAC
+    if (stackSize < _MD_DEFAULT_STACK_SIZE)
+    	stackSize = _MD_DEFAULT_STACK_SIZE;
+#endif
+
     _PR_ADJUST_STACKSIZE(stackSize);
 
     if (native) {
