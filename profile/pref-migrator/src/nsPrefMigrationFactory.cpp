a257a77a114e95b0c56888379b935c9547ce231f
neeti%netscape.com <neeti%netscape.com>
1999-10-15 04:06:43 +0000

    Converted to use nsIModule to  prepare for memory leaks fixing. Bug# 14034. r=sspitzer

--- profile/pref-migrator/src/nsPrefMigrationFactory.cpp
+++ profile/pref-migrator/src/nsPrefMigrationFactory.cpp
@@ -31,87 +31,229 @@
 /* Pref Migration Factory routines                                         */
 /*-------------------------------------------------------------------------*/
 static NS_DEFINE_IID(kISupportsIID, NS_ISUPPORTS_IID);  /* Need to remove these defines from this */
-static NS_DEFINE_IID(kIFactoryIID, NS_IFACTORY_IID);    /* file they should be global to the      */
-                                                        /* prefmigration source files.            */
+static NS_DEFINE_IID(kPrefMigrationCID,  NS_PREFMIGRATION_CID);
 
-static PRInt32 gPrefMigrationInstanceCnt = 0;
-static PRInt32 gPrefMigrationLock        = 0;
+//----------------------------------------------------------------------
 
+// Functions used to create new instances of a given object by the
+// generic factory.
 
-nsPrefMigrationFactory::nsPrefMigrationFactory(void)
-{
-  mRefCnt=0;
-  PR_AtomicIncrement(&gPrefMigrationInstanceCnt);
+                          
+static NS_IMETHODIMP  
+CreateNewPrefMigration(nsISupports* aOuter, REFNSIID aIID, void **aResult)
+{                                                  
+    if (!aResult) {                                                  
+        return NS_ERROR_NULL_POINTER;                             
+    }                                                                
+    if (aOuter) {                                                    
+        *aResult = nsnull;                                           
+        return NS_ERROR_NO_AGGREGATION;                              
+    }                                                                
+    nsPrefMigration* inst = nsPrefMigration::GetInstance();   
+    if (inst == nsnull)
+      return NS_ERROR_OUT_OF_MEMORY;
+                               
+    nsresult rv = inst->QueryInterface(aIID, aResult);                        
+    if (NS_FAILED(rv)) {                                             
+        *aResult = nsnull;                                           
+    }                                                                
+    return rv;                                                       
 }
 
-nsPrefMigrationFactory::~nsPrefMigrationFactory(void)
+//----------------------------------------------------------------------
+
+nsPrefMigrationModule::nsPrefMigrationModule()
+    : mInitialized(PR_FALSE)
 {
-  PR_AtomicDecrement(&gPrefMigrationInstanceCnt);
+    NS_INIT_ISUPPORTS();
 }
 
-NS_IMETHODIMP 
-nsPrefMigrationFactory::QueryInterface(REFNSIID aIID, void** aInstancePtr)
+nsPrefMigrationModule::~nsPrefMigrationModule()
 {
-  if (aInstancePtr == NULL)
-  {
-    return NS_ERROR_NULL_POINTER;
-  }
-
-  // Always NULL result, in case of failure
-  *aInstancePtr = NULL;
-
-  if ( aIID.Equals(kISupportsIID) )
-  {
-    *aInstancePtr = (void*) this;
-  }
-  else if ( aIID.Equals(kIFactoryIID) )
-  {
-    *aInstancePtr = (void*) this;
-  }
-
-  if (aInstancePtr == NULL)
-  {
-    return NS_ERROR_NO_INTERFACE;
-  }
-
-  AddRef();
-  return NS_OK;
+    Shutdown();
 }
 
+NS_IMPL_ISUPPORTS(nsPrefMigrationModule, NS_GET_IID(nsIModule))
 
-NS_IMPL_ADDREF(nsPrefMigrationFactory);
-NS_IMPL_RELEASE(nsPrefMigrationFactory);
-/*------------------------------------------------------------------------*/
-/* The PrefMigration CreateInstance Method                                */
-/*------------------------------------------------------------------------*/
+// Perform our one-time intialization for this module
+nsresult
+nsPrefMigrationModule::Initialize()
+{
+    if (mInitialized) {
+        return NS_OK;
+    }
+    mInitialized = PR_TRUE;
+    return NS_OK;
+}
+
+// Shutdown this module, releasing all of the module resources
+void
+nsPrefMigrationModule::Shutdown()
+{
+    // Release the factory object
+    mFactory = nsnull;
+}
+
+// Create a factory object for creating instances of aClass.
 NS_IMETHODIMP
-nsPrefMigrationFactory::CreateInstance(nsISupports *aOuter, 
-                                       REFNSIID aIID, 
-                                       void **aResult)
+nsPrefMigrationModule::GetClassObject(nsIComponentManager *aCompMgr,
+                               const nsCID& aClass,
+                               const nsIID& aIID,
+                               void** r_classObj)
 {
-  if (aResult == NULL)
-  {
-    return NS_ERROR_NULL_POINTER;
-  }
+    nsresult rv;
 
-  *aResult = NULL;
-  
-  nsPrefMigration *pPrefMigration = nsPrefMigration::GetInstance();
+    // Defensive programming: Initialize *r_classObj in case of error below
+    if (!r_classObj) {
+        return NS_ERROR_INVALID_POINTER;
+    }
+    *r_classObj = NULL;
 
-  if (pPrefMigration == NULL)
-    return NS_ERROR_OUT_OF_MEMORY;
+    // Do one-time-only initialization if necessary
+    if (!mInitialized) {
+        rv = Initialize();
+        if (NS_FAILED(rv)) {
+            // Initialization failed! yikes!
+            return rv;
+        }
+    }
 
-  return pPrefMigration->QueryInterface(aIID, aResult);
+    // Choose the appropriate factory, based on the desired instance
+    // class type (aClass).
+    nsCOMPtr<nsIGenericFactory> fact;
+    if (aClass.Equals(kPrefMigrationCID)) {
+        if (!mFactory) {
+            // Create and save away the factory object for creating
+            // new instances of prefMigration. This way if we are called
+            // again for the factory, we won't need to create a new
+            // one.
+            rv = NS_NewGenericFactory(getter_AddRefs(mFactory),
+                                      CreateNewPrefMigration);
+        }
+        fact = mFactory;
+    }
+    else {
+        rv = NS_ERROR_FACTORY_NOT_REGISTERED;
+#ifdef DEBUG
+        char* cs = aClass.ToString();
+        printf("+++ nsPrefMigrationModule: unable to create factory for %s\n", cs);
+        nsCRT::free(cs);
+#endif
+    }
+
+    if (fact) {
+        rv = fact->QueryInterface(aIID, r_classObj);
+    }
+
+    return rv;
+}
+
+//----------------------------------------
+
+struct Components {
+    const char* mDescription;
+    const nsID* mCID;
+    const char* mProgID;
+};
+
+// The list of components we register
+static Components gComponents[] = {
+    { "Profile Migration", &kPrefMigrationCID,
+      NS_PROFILEMIGRATION_PROGID, },
+};
+#define NUM_COMPONENTS (sizeof(gComponents) / sizeof(gComponents[0]))
+
+NS_IMETHODIMP
+nsPrefMigrationModule::RegisterSelf(nsIComponentManager *aCompMgr,
+                             nsIFileSpec* aPath,
+                             const char* registryLocation,
+                             const char* componentType)
+{
+    nsresult rv = NS_OK;
+
+#ifdef DEBUG
+    printf("*** Registering PrefMigration components\n");
+#endif
+
+    Components* cp = gComponents;
+    Components* end = cp + NUM_COMPONENTS;
+    while (cp < end) {
+        rv = aCompMgr->RegisterComponentSpec(*cp->mCID, cp->mDescription,
+                                             cp->mProgID, aPath, PR_TRUE,
+                                             PR_TRUE);
+        if (NS_FAILED(rv)) {
+#ifdef DEBUG
+            printf("nsPrefMigrationModule: unable to register %s component => %x\n",
+                   cp->mDescription, rv);
+#endif
+            break;
+        }
+        cp++;
+    }
+
+    return rv;
 }
 
 NS_IMETHODIMP
-nsPrefMigrationFactory::LockFactory(PRBool aLock)
+nsPrefMigrationModule::UnregisterSelf(nsIComponentManager* aCompMgr,
+                               nsIFileSpec* aPath,
+                               const char* registryLocation)
 {
-  if (aLock)
-    PR_AtomicIncrement(&gPrefMigrationLock);
-  else
-    PR_AtomicDecrement(&gPrefMigrationLock);
+#ifdef DEBUG
+    printf("*** Unregistering PrefMigration components\n");
+#endif
+    Components* cp = gComponents;
+    Components* end = cp + NUM_COMPONENTS;
+    while (cp < end) {
+        nsresult rv = aCompMgr->UnregisterComponentSpec(*cp->mCID, aPath);
+        if (NS_FAILED(rv)) {
+#ifdef DEBUG
+            printf("nsPrefMigrationModule: unable to unregister %s component => %x\n",
+                   cp->mDescription, rv);
+#endif
+        }
+        cp++;
+    }
 
-  return NS_OK;
+    return NS_OK;
+}
+
+NS_IMETHODIMP
+nsPrefMigrationModule::CanUnload(nsIComponentManager *aCompMgr, PRBool *okToUnload)
+{
+    if (!okToUnload) {
+        return NS_ERROR_INVALID_POINTER;
+    }
+    *okToUnload = PR_FALSE;
+    return NS_ERROR_FAILURE;
+}
+
+//----------------------------------------------------------------------
+
+static nsPrefMigrationModule *gModule = NULL;
+
+extern "C" NS_EXPORT nsresult NSGetModule(nsIComponentManager *servMgr,
+                                          nsIFileSpec* location,
+                                          nsIModule** return_cobj)
+{
+    nsresult rv = NS_OK;
+
+    NS_ENSURE_ARG_POINTER(return_cobj);
+    NS_ENSURE_NOT(gModule, NS_ERROR_FAILURE);
+
+    // Create and initialize the module instance
+    nsPrefMigrationModule *m = new nsPrefMigrationModule();
+    if (!m) {
+        return NS_ERROR_OUT_OF_MEMORY;
+    }
+
+    // Increase refcnt and store away nsIModule interface to m in return_cobj
+    rv = m->QueryInterface(NS_GET_IID(nsIModule), (void**)return_cobj);
+    if (NS_FAILED(rv)) {
+        delete m;
+        m = nsnull;
+    }
+    gModule = m;                  // WARNING: Weak Reference
+    return rv;
 }
 
