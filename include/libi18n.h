25aba30f43f0583cbc32ceb9f811cf814635099e
ramiro%fateware.com <ramiro%fateware.com>
1999-10-22 07:11:43 +0000

    Removing the badly rotten MOZ_STRIP_NOT_EXPORTED hack.  We now have a better way to accomplish the same thing using linker version scripts. r=cls@seawood.org

--- include/libi18n.h
+++ include/libi18n.h
@@ -36,9 +36,6 @@
 #ifdef XP_WIN32
 #define UNICVTAPI __declspec(dllexport)
 
-#elif defined(MOZ_STRIP_NOT_EXPORTED)
-#define UNICVTAPI __attribute__ ((dllexport))
-
 #else
 #define UNICVTAPI
 #endif
