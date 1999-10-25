f819b0635db85ab0eb2c659a7f76b219992a433b
tbogard@aol.net <tbogard@aol.net>
1999-10-25 04:50:22 +0000

    Framed out the nsHTMLDocShell implementation.  Setup to build a library.

---
+++ docshell/html/nsHTMLDocShell.h
@@ -0,0 +1,52 @@
+/* -*- Mode: IDL; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * The Original Code is the Mozilla browser.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications, Inc.  Portions created by Netscape are
+ * Copyright (C) 1999, Mozilla.  All Rights Reserved.
+ * 
+ * Contributor(s):
+ *   Travis Bogard <travis@netscape.com>
+ */
+
+#ifndef nsHTMLDocShell_h__
+#define nsHTMLDocShell_h__
+
+#include "nsCHTMLDocShell.h"
+
+class nsHTMLDocShell : public nsIDocShell, public nsIHTMLDocShell, 
+   public nsIDocShellEdit, public nsIDocShellFile, public nsIGenericWindow,
+   public nsIScrollable, public nsITextScroll
+{
+public:
+   NS_DECL_ISUPPORTS
+
+   NS_DECL_NSIDOCSHELL
+   NS_DECL_NSIHTMLDOCSHELL
+   NS_DECL_NSIDOCSHELLEDIT
+   NS_DECL_NSIDOCSHELLFILE
+   NS_DECL_NSIGENERICWINDOW
+   NS_DECL_NSISCROLLABLE
+   NS_DECL_NSITEXTSCROLL
+
+   static NS_METHOD Create(nsISupports* aOuter, const nsIID& aIID, void** ppv);
+
+protected:
+   nsHTMLDocShell();
+   virtual ~nsHTMLDocShell();
+
+protected:
+};
+
+#endif /* nsHTMLDocShell_h__ */
