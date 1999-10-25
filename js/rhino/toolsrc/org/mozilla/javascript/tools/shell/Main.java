83dbb689c32e3e39a1bf79f7d22f87b5660fc0c2
norris%netscape.com <norris%netscape.com>
1999-10-25 16:40:30 +0000

    Add contributors.

--- js/rhino/toolsrc/org/mozilla/javascript/tools/shell/Main.java
+++ js/rhino/toolsrc/org/mozilla/javascript/tools/shell/Main.java
@@ -19,6 +19,9 @@
  * Rights Reserved.
  *
  * Contributor(s): 
+ * Patrick Beard
+ * Norris Boyd
+ * Kurt Westerfield
  *
  * Alternatively, the contents of this file may be used under the
  * terms of the GNU Public License (the "GPL"), in which case the
