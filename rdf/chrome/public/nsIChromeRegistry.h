a25a8c01f157cadd26be2500132afd163e58af5a
hyatt%netscape.com <hyatt%netscape.com>
1999-10-26 08:13:25 +0000

    stubbing out dynamic overlay loading and converting chrome registry to IDL r=pavlov

--- rdf/chrome/public/nsIChromeRegistry.h
+++ rdf/chrome/public/nsIChromeRegistry.h
@@ -1,49 +1,54 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- *
- * The contents of this file are subject to the Netscape Public License
- * Version 1.0 (the "NPL"); you may not use this file except in
- * compliance with the NPL.  You may obtain a copy of the NPL at
- * http://www.mozilla.org/NPL/
- *
- * Software distributed under the NPL is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
- * for the specific language governing rights and limitations under the
- * NPL.
- *
- * The Initial Developer of this code under the NPL is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
- * Reserved.
+/*
+ * DO NOT EDIT.  THIS FILE IS GENERATED FROM nsIChromeRegistry.idl
  */
 
-#ifndef nsIChromeRegistry_h__
-#define nsIChromeRegistry_h__
+#ifndef __gen_nsIChromeRegistry_h__
+#define __gen_nsIChromeRegistry_h__
 
 #include "nsISupports.h"
-#include "nsString.h"
+#include "nsIURI.h"
+#include "nsrootidl.h"
 #include "nsIURL.h"
 
-// {D8C7D8A1-E84C-11d2-BF87-00105A1B0627}
-#define NS_ICHROMEREGISTRY_IID \
-{ 0xd8c7d8a1, 0xe84c, 0x11d2, { 0xbf, 0x87, 0x0, 0x10, 0x5a, 0x1b, 0x6, 0x27 } }
+/* starting interface:    nsIChromeRegistry */
 
+#define NS_ICHROMEREGISTRY_IID_STR "d8c7d8a1-e84c-11d2-bf87-00105a1b0627"
+
+#define NS_ICHROMEREGISTRY_IID \
+  {0xd8c7d8a1, 0xe84c, 0x11d2, \
+    { 0xbf, 0x87, 0x00, 0x10, 0x5a, 0x1b, 0x06, 0x27 }}
 
 class nsIChromeRegistry : public nsISupports {
-public:
-    static const nsIID& GetIID() { static nsIID iid = NS_ICHROMEREGISTRY_IID; return iid; }
-    
-    NS_IMETHOD ConvertChromeURL(nsIURI* aChromeURL) = 0;
-    NS_IMETHOD InitRegistry() = 0;
+ public: 
+  NS_DEFINE_STATIC_IID_ACCESSOR(NS_ICHROMEREGISTRY_IID)
+
+  /* void convertChromeURL (in nsIURI aChromeURL); */
+  NS_IMETHOD ConvertChromeURL(nsIURI *aChromeURL) = 0;
+
+  /* long getOverlayCount (in wstring aChromeURL); */
+  NS_IMETHOD GetOverlayCount(const PRUnichar *aChromeURL, PRInt32 *_retval) = 0;
+
+  /* wstring getOverlayAt (in wstring aChromeURL, in long aIndex); */
+  NS_IMETHOD GetOverlayAt(const PRUnichar *aChromeURL, PRInt32 aIndex, PRUnichar **_retval) = 0;
 };
+
+/* Use this macro when declaring classes that implement this interface. */
+#define NS_DECL_NSICHROMEREGISTRY \
+  NS_IMETHOD ConvertChromeURL(nsIURI *aChromeURL); \
+  NS_IMETHOD GetOverlayCount(const PRUnichar *aChromeURL, PRInt32 *_retval); \
+  NS_IMETHOD GetOverlayAt(const PRUnichar *aChromeURL, PRInt32 aIndex, PRUnichar **_retval); 
+
+/* Use this macro to declare functions that forward the behavior of this interface to another object. */
+#define NS_FORWARD_NSICHROMEREGISTRY(_to) \
+  NS_IMETHOD ConvertChromeURL(nsIURI *aChromeURL) { return _to ## ConvertChromeURL(aChromeURL); } \
+  NS_IMETHOD GetOverlayCount(const PRUnichar *aChromeURL, PRInt32 *_retval) { return _to ## GetOverlayCount(aChromeURL, _retval); } \
+  NS_IMETHOD GetOverlayAt(const PRUnichar *aChromeURL, PRInt32 aIndex, PRUnichar **_retval) { return _to ## GetOverlayAt(aChromeURL, aIndex, _retval); } 
 
 // for component registration
 // {D8C7D8A2-E84C-11d2-BF87-00105A1B0627}
 #define NS_CHROMEREGISTRY_CID \
 { 0xd8c7d8a2, 0xe84c, 0x11d2, { 0xbf, 0x87, 0x0, 0x10, 0x5a, 0x1b, 0x6, 0x27 } }
-
-////////////////////////////////////////////////////////////////////////////////
-
 extern nsresult
 NS_NewChromeRegistry(nsIChromeRegistry* *aResult);
 
-#endif // nsIChromeRegistry_h__
+#endif /* __gen_nsIChromeRegistry_h__ */
