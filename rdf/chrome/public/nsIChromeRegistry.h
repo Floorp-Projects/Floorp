c8bad066a0891f41d93f7e067bb6fa07fc7a1c74
hyatt%netscape.com <hyatt%netscape.com>
1999-10-26 08:21:32 +0000

    change from strings to URIs r=pavlov

--- rdf/chrome/public/nsIChromeRegistry.h
+++ rdf/chrome/public/nsIChromeRegistry.h
@@ -25,24 +25,24 @@ class nsIChromeRegistry : public nsISupports {
   /* void convertChromeURL (in nsIURI aChromeURL); */
   NS_IMETHOD ConvertChromeURL(nsIURI *aChromeURL) = 0;
 
-  /* long getOverlayCount (in wstring aChromeURL); */
-  NS_IMETHOD GetOverlayCount(const PRUnichar *aChromeURL, PRInt32 *_retval) = 0;
+  /* long getOverlayCount (in nsIURI aChromeURL); */
+  NS_IMETHOD GetOverlayCount(nsIURI *aChromeURL, PRInt32 *_retval) = 0;
 
-  /* wstring getOverlayAt (in wstring aChromeURL, in long aIndex); */
-  NS_IMETHOD GetOverlayAt(const PRUnichar *aChromeURL, PRInt32 aIndex, PRUnichar **_retval) = 0;
+  /* nsIURI getOverlayAt (in nsIURI aChromeURL, in long aIndex); */
+  NS_IMETHOD GetOverlayAt(nsIURI *aChromeURL, PRInt32 aIndex, nsIURI **_retval) = 0;
 };
 
 /* Use this macro when declaring classes that implement this interface. */
 #define NS_DECL_NSICHROMEREGISTRY \
   NS_IMETHOD ConvertChromeURL(nsIURI *aChromeURL); \
-  NS_IMETHOD GetOverlayCount(const PRUnichar *aChromeURL, PRInt32 *_retval); \
-  NS_IMETHOD GetOverlayAt(const PRUnichar *aChromeURL, PRInt32 aIndex, PRUnichar **_retval); 
+  NS_IMETHOD GetOverlayCount(nsIURI *aChromeURL, PRInt32 *_retval); \
+  NS_IMETHOD GetOverlayAt(nsIURI *aChromeURL, PRInt32 aIndex, nsIURI **_retval); 
 
 /* Use this macro to declare functions that forward the behavior of this interface to another object. */
 #define NS_FORWARD_NSICHROMEREGISTRY(_to) \
   NS_IMETHOD ConvertChromeURL(nsIURI *aChromeURL) { return _to ## ConvertChromeURL(aChromeURL); } \
-  NS_IMETHOD GetOverlayCount(const PRUnichar *aChromeURL, PRInt32 *_retval) { return _to ## GetOverlayCount(aChromeURL, _retval); } \
-  NS_IMETHOD GetOverlayAt(const PRUnichar *aChromeURL, PRInt32 aIndex, PRUnichar **_retval) { return _to ## GetOverlayAt(aChromeURL, aIndex, _retval); } 
+  NS_IMETHOD GetOverlayCount(nsIURI *aChromeURL, PRInt32 *_retval) { return _to ## GetOverlayCount(aChromeURL, _retval); } \
+  NS_IMETHOD GetOverlayAt(nsIURI *aChromeURL, PRInt32 aIndex, nsIURI **_retval) { return _to ## GetOverlayAt(aChromeURL, aIndex, _retval); } 
 
 // for component registration
 // {D8C7D8A2-E84C-11d2-BF87-00105A1B0627}
