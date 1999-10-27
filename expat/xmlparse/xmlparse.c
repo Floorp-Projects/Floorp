fe3262deaa304a38d07dd1db6ac9efc2d26bbf38
nisheeth@netscape.com <nisheeth@netscape.com>
1999-10-27 07:39:57 +0000

    Fix for bug 15633.  Fixing build warning with the patch submitted by James Clark (jjc@jclark.com).  r=nisheeth

--- expat/xmlparse/xmlparse.c
+++ expat/xmlparse/xmlparse.c
@@ -2009,8 +2009,7 @@ initializeEncoding(XML_Parser parser)
     int i;
     for (i = 0; protocolEncodingName[i]; i++) {
       if (i == sizeof(encodingBuf) - 1
-	  || protocolEncodingName[i] >= 0x80
-	  || protocolEncodingName[i] < 0) {
+	  || (protocolEncodingName[i] & ~0x7f) != 0) {
 	encodingBuf[0] = '\0';
 	break;
       }
