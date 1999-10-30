4efbfcbdfbba5e31507d2dd9621faabf3650bf75
alecf%netscape.com <alecf%netscape.com>
1999-10-30 07:32:23 +0000

    fix a warning r=pavlov

--- mailnews/local/src/nsPop3Protocol.cpp
+++ mailnews/local/src/nsPop3Protocol.cpp
@@ -362,8 +362,8 @@ net_pop3_free_state(Pop3UidlHost* host)
 // nsPop3Protocol class implementation
 
 nsPop3Protocol::nsPop3Protocol(nsIURI* aURL)
-    : nsMsgLineBuffer(NULL, PR_FALSE),
-      nsMsgProtocol(aURL, aURL)
+    : nsMsgProtocol(aURL, aURL),
+      nsMsgLineBuffer(NULL, PR_FALSE)
 {
 	SetLookingForCRLF(MSG_LINEBREAK_LEN == 2);
 	Initialize(aURL);
