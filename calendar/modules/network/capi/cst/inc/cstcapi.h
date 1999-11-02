0101ea8f825f7a72b16cdb18a4b8e16d4377faa2
dmose%mozilla.org <dmose%mozilla.org>
1999-11-02 06:13:48 +0000

    updating license boilerplate

--- calendar/modules/network/capi/cst/inc/cstcapi.h
+++ calendar/modules/network/capi/cst/inc/cstcapi.h
@@ -1,14 +1,18 @@
 /*
 
-  The contents of this file are subject to the Mozilla Public License Version
-  1.0 //(the "MPL"); you may not use this file except in compliance with the
-  MPL. You //may obtain a copy of the NPL at http://www.mozilla.org/MPL/
+  The contents of this file are subject to the Mozilla Public
+  License Version 1.1 (the "License"); you may not use this file
+  except in compliance with the License. You may obtain a copy of
+  the License at http://www.mozilla.org/MPL/
 
-  Software distributed under the MPL is distributed on an "AS IS" basis,
-  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the MPL for the
-  specific language governing rights and limitations under the MPL.
+  Software distributed under the License is distributed on an "AS
+  IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+  implied. See the License for the specific language governing
+  rights and limitations under the License.
 
-  The Initial Developer of this code under the MPL is Corporate Software &
+  The Original Code is this file as it was released on September 3, 1998.
+
+  The Initial Developer of the Original Code is Corporate Software &
   Technologies Int, Inc. Portions created by Corporate Software & Technologies
   Int, Inc are Copyright (C) 1998 Corporate Software & Technologies Int,
   Inc. All Rights Reserved.
