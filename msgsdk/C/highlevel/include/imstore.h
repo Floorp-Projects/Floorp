b6f1a2eb04650659d492f68b51b2febed26022ca
dmose%mozilla.org <dmose%mozilla.org>
1999-11-02 06:20:05 +0000

    updating license boilerplate to xPL v1.1

--- msgsdk/C/highlevel/include/imstore.h
+++ msgsdk/C/highlevel/include/imstore.h
@@ -1,21 +1,22 @@
 /*
- *	The contents of this file are subject to the Netscape Public License 
- *	Version 1.0 (the "License"); you may not use this file except in compliance 
- *	with the License. You may obtain a copy of the License at 
- *	http://www.mozilla.org/NPL/. 
+ *	 The contents of this file are subject to the Netscape Public
+ *	 License Version 1.1 (the "License"); you may not use this file
+ *	 except in compliance with the License. You may obtain a copy of
+ *	 the License at http://www.mozilla.org/NPL/
  *	
- *	Software distributed under the License is distributed on an "AS IS" basis, 
- *	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
- *	for the specific language governing rights and limitations under the License. 
+ *	 Software distributed under the License is distributed on an "AS
+ *	 IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ *	 implied. See the License for the specific language governing
+ *	 rights and limitations under the License.
  *	
- *	The Original Code is the Netscape Messaging Access SDK Version 3.5 code, 
- *	released on or about June 15, 1998. 
+ *	 The Original Code is the Netscape Messaging Access SDK Version 3.5 code, 
+ *	released on or about June 15, 1998.  *	
+ *	 The Initial Developer of the Original Code is Netscape Communications 
+ *	 Corporation. Portions created by Netscape are
+ *	 Copyright (C) 1998 Netscape Communications Corporation. All
+ *	 Rights Reserved.
  *	
- *	The Initial Developer of the Original Code is Netscape Communications 
- *	Corporation. Portions created by Netscape are Copyright (C) 1998 Netscape 
- *	Communications Corporation. All Rights Reserved. 
- *	
- *	Contributor(s): ______________________________________. 
+ *	 Contributor(s): ______________________________________. 
 */
 
 /*
