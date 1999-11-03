b8688c8b7fe9845403caa9a9b1a1afb623b23948
sgehani%netscape.com <sgehani%netscape.com>
1999-11-03 07:54:28 +0000

    Chnage MIW target name to Mozilla Installer [r=sfraser].

--- build/mac/NGLayoutBuildList.pm
+++ build/mac/NGLayoutBuildList.pm
@@ -1486,7 +1486,7 @@ sub BuildLayoutProjects()
     
     BuildOneProject(":mozilla:xpinstall:macbuild:xpistub.mcp",                  "xpistub$D.shlb", "", 1, $main::ALIAS_SYM_FILES, 0);
     
-    BuildOneProject(":mozilla:xpinstall:wizard:mac:macbuild:MIW.mcp",           "MIW$D", "", 0, 0, 0);
+    BuildOneProject(":mozilla:xpinstall:wizard:mac:macbuild:MIW.mcp",           "Mozilla Installer$D", "", 0, 0, 0);
 
 	print("--- Layout projects complete ---\n")
 }
