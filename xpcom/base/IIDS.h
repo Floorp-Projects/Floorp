55c83355dab6c649f86910e300e0d90c31bdeb21
erik%netscape.com <erik%netscape.com>
1999-11-18 16:45:59 +0000

    Grabbing the next available ID. Does not affect build.

--- xpcom/base/IIDS.h
+++ xpcom/base/IIDS.h
@@ -1208,7 +1208,7 @@ nsIFontEnumerator = { /* a6cf9114-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-INTERFACENAME = { /* a6cf9115-15b3-11d2-932e-00805f8add32 */
+nsFontEnumerator_CID = { /* a6cf9115-15b3-11d2-932e-00805f8add32 */
     0xa6cf9115,
     0x15b3,
     0x11d2,
