c36b6edecb524195360b61379df67209d77af958
dp%netscape.com <dp%netscape.com>
1999-11-20 07:34:03 +0000

    - Converting to GenericModule - Using NS_IMPL_ISUPPORTS1 - Removing a bunch of NS_DEFINE_IID

--- modules/libpref/src/nsPref.cpp
+++ modules/libpref/src/nsPref.cpp
@@ -58,12 +58,6 @@
 
 #include "prefapi_private_data.h"
 
-static NS_DEFINE_IID(kIPrefIID, NS_IPREF_IID);
-static NS_DEFINE_IID(kIFileLocatorIID,      NS_IFILELOCATOR_IID);
-static NS_DEFINE_CID(kProfileCID, NS_PROFILE_CID);
-
-static NS_DEFINE_CID(kPrefCID, NS_PREF_CID);
-static NS_DEFINE_CID(kComponentManagerCID, NS_COMPONENTMANAGER_CID);
 static NS_DEFINE_CID(kFileLocatorCID,       NS_FILELOCATOR_CID);
 
 //========================================================================================
@@ -236,10 +230,7 @@ nsPref* nsPref::GetInstance()
     return gInstance;
 } // nsPref::GetInstance
 
-NS_IMPL_ISUPPORTS(nsPref, kIPrefIID);
-
-
-
+NS_IMPL_ISUPPORTS1(nsPref, nsIPref);
 
 //========================================================================================
 // nsIPref Implementation
@@ -1267,199 +1258,13 @@ CreateNewPref(nsISupports *aDelegate, REFNSIID aIID, void **aResult)
     return res;
 }
 
-//----------------------------------------------------------------------------------------
-
-NS_IMPL_ISUPPORTS(nsPrefModule, NS_GET_IID(nsIModule))
-
-nsPrefModule::nsPrefModule()
-    : mInitialized(PR_FALSE)
-{
-    NS_INIT_ISUPPORTS();
-}
-
-nsPrefModule::~nsPrefModule()
-{
-    Shutdown();
-}
-
-// Perform our one-time intialization for this module
-nsresult
-nsPrefModule::Initialize()
-{
-    if (mInitialized) {
-        return NS_OK;
-    }
-    mInitialized = PR_TRUE;
-    return NS_OK;
-}
-
-// Shutdown this module, releasing all of the module resources
-void
-nsPrefModule::Shutdown()
-{
-    // Release the factory object
-    mFactory = nsnull;
-}
-
-// Create a factory object for creating instances of aClass.
-NS_IMETHODIMP
-nsPrefModule::GetClassObject(nsIComponentManager *aCompMgr,
-                               const nsCID& aClass,
-                               const nsIID& aIID,
-                               void** r_classObj)
-{
-    nsresult rv;
-
-    // Defensive programming: Initialize *r_classObj in case of error below
-    if (!r_classObj) {
-        return NS_ERROR_INVALID_POINTER;
-    }
-    *r_classObj = NULL;
-
-    // Do one-time-only initialization if necessary
-    if (!mInitialized) {
-        rv = Initialize();
-        if (NS_FAILED(rv)) {
-            // Initialization failed! yikes!
-            return rv;
-        }
-    }
-
-    // Choose the appropriate factory, based on the desired instance
-    // class type (aClass).
-    nsCOMPtr<nsIGenericFactory> fact;
-    if (aClass.Equals(kPrefCID)) {
-        if (!mFactory) {
-            // Create and save away the factory object for creating
-            // new instances of Pref. This way if we are called
-            // again for the factory, we won't need to create a new
-            // one.
-            rv = NS_NewGenericFactory(getter_AddRefs(mFactory),
-                                      CreateNewPref);
-        }
-        fact = mFactory;
-    }
-    else {
-		rv = NS_ERROR_FACTORY_NOT_REGISTERED;
-#ifdef DEBUG
-        char* cs = aClass.ToString();
-        printf("+++ nsPrefModule: unable to create factory for %s\n", cs);
-        nsCRT::free(cs);
-#endif
-    }
-
-    if (fact) {
-        rv = fact->QueryInterface(aIID, r_classObj);
-    }
-
-    return rv;
-}
-
-//----------------------------------------
-
-struct Components {
-    const char* mDescription;
-    const nsID* mCID;
-    const char* mProgID;
-};
-
 // The list of components we register
-static Components gComponents[] = {
-    { "Preferences Service", &kPrefCID,
-      "component://netscape/preferences", },
+static nsModuleComponentInfo components[] = {
+    { NS_PREF_CLASSNAME, NS_PREF_CID, NS_PREF_PROGID, CreateNewPref, },
 };
-#define NUM_COMPONENTS (sizeof(gComponents) / sizeof(gComponents[0]))
 
-NS_IMETHODIMP
-nsPrefModule::RegisterSelf(nsIComponentManager *aCompMgr,
-                          nsIFileSpec* aPath,
-                          const char* registryLocation,
-                          const char* componentType)
-{
-    nsresult rv = NS_OK;
-
-#ifdef DEBUG
-    printf("*** Registering pref components\n");
-#endif
-
-    Components* cp = gComponents;
-    Components* end = cp + NUM_COMPONENTS;
-    while (cp < end) {
-        rv = aCompMgr->RegisterComponentSpec(*cp->mCID, cp->mDescription,
-                                             cp->mProgID, aPath, PR_TRUE,
-                                             PR_TRUE);
-        if (NS_FAILED(rv)) {
-#ifdef DEBUG
-            printf("nsPrefModule: unable to register %s component => %x\n",
-                   cp->mDescription, rv);
-#endif
-            break;
-        }
-        cp++;
-    }
-
-    return rv;
-}
-
-NS_IMETHODIMP
-nsPrefModule::UnregisterSelf(nsIComponentManager* aCompMgr,
-                            nsIFileSpec* aPath,
-                            const char* registryLocation)
-{
-#ifdef DEBUG
-    printf("*** Unregistering pref components\n");
-#endif
-    Components* cp = gComponents;
-    Components* end = cp + NUM_COMPONENTS;
-    while (cp < end) {
-        nsresult rv = aCompMgr->UnregisterComponentSpec(*cp->mCID, aPath);
-        if (NS_FAILED(rv)) {
-#ifdef DEBUG
-            printf("nsPrefModule: unable to unregister %s component => %x\n",
-                   cp->mDescription, rv);
-#endif
-        }
-        cp++;
-    }
-
-    return NS_OK;
-}
-
-NS_IMETHODIMP
-nsPrefModule::CanUnload(nsIComponentManager *aCompMgr, PRBool *okToUnload)
-{
-    if (!okToUnload) {
-        return NS_ERROR_INVALID_POINTER;
-    }
-    *okToUnload = PRBool(g_InstanceCount == 0);
-    return NS_ERROR_FAILURE;
-}
-
-//----------------------------------------------------------------------
-
-static nsPrefModule *gModule = NULL;
-
-extern "C" NS_EXPORT nsresult NSGetModule(nsIComponentManager *servMgr,
-                                          nsIFileSpec* location,
-                                          nsIModule** return_cobj)
-{
-    nsresult rv = NS_OK;
-
-    NS_ENSURE_ARG_POINTER(return_cobj);
-    NS_ENSURE_FALSE(gModule, NS_ERROR_FAILURE);
-
-    // Create an initialize the layout module instance
-    nsPrefModule *m = new nsPrefModule();
-    if (!m) {
-        return NS_ERROR_OUT_OF_MEMORY;
-    }
-
-    // Increase refcnt and store away nsIModule interface to m in return_cobj
-    rv = m->QueryInterface(nsIModule::GetIID(), (void**)return_cobj);
-    if (NS_FAILED(rv)) {
-        delete m;
-        m = nsnull;
-    }
-    gModule = m;                  // WARNING: Weak Reference
-    return rv;
-}
+////////////////////////////////////////////////////////////////////////
+// Implement the NSGetModule() exported function for your module
+// and the entire implementation of the module object.
+//
+NS_IMPL_NSGETMODULE("nsPrefModule", components)
