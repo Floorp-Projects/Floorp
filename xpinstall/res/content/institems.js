b8070edf632701a8307c2dcb7cf8a758e7242e77
dbragg%netscape.com <dbragg%netscape.com>
1999-11-29 21:11:26 +0000

    New confirmation dialog for xpinstall.  Fixes bug #17506.  r=dougt

---
+++ xpinstall/res/content/institems.js
@@ -0,0 +1,93 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is Mozilla Communicator client code, released March
+ * 31, 1998.
+ * 
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation. Portions created by Netscape are Copyright (C) 1998
+ * Netscape Communications Corporation. All Rights Reserved.
+ */
+
+var toolkit;
+var browser;
+var dialog;
+
+// dialog param block
+var param;
+
+function addTreeItem(num, modName, url)
+{
+  dump("Adding element " + num + " : " + name + "\n");
+  var body = document.getElementById("theTreeBody");
+
+  var newitem = document.createElement('treeitem');
+  var newrow = document.createElement('treerow');
+  
+  newrow.setAttribute("rowNum", num);
+  newrow.setAttribute("rowName", modName);
+
+  var elem = document.createElement('treecell');
+  var text = document.createTextNode(modName);
+  elem.appendChild(text);
+  newrow.appendChild(elem);
+
+  var elem = document.createElement('treecell');
+  var text = document.createTextNode(url);
+  elem.appendChild(text);
+  newrow.appendChild(elem);
+
+  newitem.appendChild(newrow);
+  body.appendChild(newitem);
+}
+
+
+function onLoad() 
+{
+	param = window.arguments[0].QueryInterface( Components.interfaces.nsIDialogParamBlock  );
+	if( !param )
+	{
+		dump( " error getting param block interface\n" );
+		return;
+	}
+
+    var i = 0;
+	var row = 0;
+	var moduleName;
+	var URL;
+	while (moduleName != "")
+	{
+		moduleName = param.GetString(i++);
+		URL = param.GetString(i++);
+        addTreeItem(row++, moduleName, URL);
+	}
+}
+
+
+function onOk() 
+{
+   // set the okay button in the param block
+   if (param)
+	param.SetInt(0, 0 );
+
+   window.close();
+}
+
+function cancel() 
+{
+	// set the cancel button in the param block
+	if (param)
+	  param.SetInt(0, 1 );
+    
+	window.close();
+}
+
