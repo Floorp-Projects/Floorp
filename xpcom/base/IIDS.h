d163749ccd90c8f9b0a7d659337db858b5b8017a
jfrancis%netscape.com <jfrancis%netscape.com>
1999-11-30 14:31:18 +0000

    iid for nsIAbsorbingTransaction

--- xpcom/base/IIDS.h
+++ xpcom/base/IIDS.h
@@ -1214,7 +1214,7 @@ nsFontEnumerator_CID = { /* a6cf9115-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-INTERFACENAME = { /* a6cf9116-15b3-11d2-932e-00805f8add32 */
+nsIAbsorbingTransaction  = { /* a6cf9116-15b3-11d2-932e-00805f8add32 */
     0xa6cf9116,
     0x15b3,
     0x11d2,
