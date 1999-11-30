5bd0548f65edf3da39ecf28c0aaec774fab3d026
dougt%netscape.com <dougt%netscape.com>
1999-11-30 00:16:09 +0000

    fix for 18078. r=mstoltz,warren

--- extensions/wallet/src/wallet.cpp
+++ extensions/wallet/src/wallet.cpp
@@ -223,7 +223,7 @@ NS_NewURItoFile(const char *in, nsFileSpec dirSpec, const char *out)
     rv = eventQService->CreateThreadEventQueue();
     if (NS_FAILED(rv)) return rv;
 
-    rv = eventQService->GetThreadEventQueue(PR_CurrentThread(), &gEventQ);
+    rv = eventQService->GetThreadEventQueue(NS_CURRENT_THREAD, &gEventQ);
     if (NS_FAILED(rv)) return rv;
 
     NS_WITH_SERVICE(nsIIOService, serv, kIOServiceCID, &rv);
