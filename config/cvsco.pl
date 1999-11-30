2daf232b141a3fdeda19131971945d94bdf5ca47
slamm%netscape.com <slamm%netscape.com>
1999-11-30 00:48:33 +0000

    Rewrite script to check for cvs conflicts and log output to a file. r=mcafee

--- config/cvsco.pl
+++ config/cvsco.pl
@@ -1,9 +1,9 @@
-#!perl
+#!/usr/bin/env perl
 #
-# The contents of this file are subject to the Netscape Public
+# The contents of this file are subject to the Mozilla Public
 # License Version 1.1 (the "License"); you may not use this file
 # except in compliance with the License. You may obtain a copy of
-# the License at http://www.mozilla.org/NPL/
+# the License at http://www.mozilla.org/MPL/
 #
 # Software distributed under the License is distributed on an "AS
 # IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
@@ -14,16 +14,53 @@
 #
 # The Initial Developer of the Original Code is Netscape
 # Communications Corporation.  Portions created by Netscape are
-# Copyright (C) 1998 Netscape Communications Corporation. All
+# Copyright (C) 1999 Netscape Communications Corporation. All
 # Rights Reserved.
 #
-# Contributor(s): 
+# Contributor(s): Stephen Lamm
 #
 
-chdir($ENV{'MOZ_SRC'});
+sub usage {
+  warn "usage: cvsco.pl <checkout_command>\n"
+      ."       <checkout_command> is the entire cvs co command\n"
+      ."             (e.g. cvs -q -z3 co SeaMonkeyAll).\n";
+}
 
-$cmd = "cvs -q co ".$ARGV[0]." ".$ARGV[1];
+usage(), die "Error: Not enough arguments\n" if $#ARGV < 0;
+usage(), die "Error: Wrong cwd. Must chdir to mozilla/..\n" 
+  unless -r 'mozilla/config/cvsco.pl';
 
-print "Excecuting ".$cmd."...\n";
-system($cmd);
+$co_command = join ' ', @ARGV;
+
+$logfile     = 'cvslog.txt';
+$old_logfile = 'cvslog-old.txt';
+
+sub dblprint {
+  print LOG @_;
+  print STDERR @_;
+}
+
+if (-r $logfile) {
+  rename $logfile, $old_logfile;
+  print "rename $logfile, $old_logfile\n";
+}
+
+open LOG,   ">$logfile";
+open CVSCO, "$co_command|";
+
+dblprint "\ncheckout start: ".scalar(localtime)."\n";
+dblprint "$co_command | tee cvslog.txt\n";
+
+while (<CVSCO>) {
+  dblprint $_;
+  push @conflicts, $_ if /^C /;
+}
+
+if (@conflicts) {
+  print "Error: cvs conflicts during checkout:\n";
+  die join('', @conflicts);
+}
+
+close(CVSCO) or die "cvs error.\n";
+dblprint 'checkout finish: '.scalar(localtime)."\n";
 
