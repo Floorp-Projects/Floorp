2fc65eda6d2724583ef86973a20f84ee6c04cb37
rginda%netscape.com <rginda%netscape.com>
1999-12-01 06:07:28 +0000

    Added new ecma_3 suite. along with 'new style' (hopefully simpler) shell.js, and a new test (which fails already!)

--- js/tests/lc3/ConvertJSObject/ToBoolean-001.js
+++ js/tests/lc3/ConvertJSObject/ToBoolean-001.js
@@ -29,7 +29,7 @@
     var SECTION = "Preferred argument conversion:  boolean";
     var VERSION = "1_4";
     var TITLE   = "LiveConnect 3.0 JavaScript to Java Data Type Conversion " +
-                    SECTION;
+                   SECTION;
     startTest();
 
     var TEST_CLASS = new Packages.com.netscape.javascript.qa.lc3.jsobject.JSObject_004;
