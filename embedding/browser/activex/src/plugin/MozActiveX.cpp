c75c3aa38f014595272d9474ad421110d0514d3d
locka@iol.ie <locka@iol.ie>
1999-12-02 22:17:18 +0000

    Made ActiveX plugin buildable

---
+++ embedding/browser/activex/src/plugin/MozActiveX.cpp
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ */
+
+// npmozax.cpp : Implementation of DLL Exports.
+
+
+// Note: Proxy/Stub Information
+//		To build a separate proxy/stub DLL, 
+//		run nmake -f MozillaControlps.mk in the project directory.
+
+#include "stdafx.h"
+#include "resource.h"
+#include "initguid.h"
+
+CComModule _Module;
+
+//BEGIN_OBJECT_MAP(ObjectMap)
+//END_OBJECT_MAP()
