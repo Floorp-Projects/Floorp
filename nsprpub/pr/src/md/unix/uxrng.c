64618950047d3087a110ba3450fd43317a257e60
cls%seawood.org <cls%seawood.org>
1999-12-05 07:26:46 +0000

    I've been told that NetBSD has /dev/random as well.  At worst, it should return 0 rather than fail with that undefined error anyways.

--- nsprpub/pr/src/md/unix/uxrng.c
+++ nsprpub/pr/src/md/unix/uxrng.c
@@ -102,7 +102,7 @@ GetHighResClock(void *buf, size_t maxbytes)
     return 0;
 }
 
-#elif (defined(LINUX) || defined(__FreeBSD__))
+#elif (defined(LINUX) || defined(__FreeBSD__) || defined(NETBSD))
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
