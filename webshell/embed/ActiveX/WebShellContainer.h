5049bdb20f76e0c7fd174d63425a4cb836a02e53
locka@iol.ie <locka@iol.ie>
1999-12-05 18:31:35 +0000

    Fixed broken declaration of GetTitle(). Hopefully this is the last of this problem.

--- webshell/embed/ActiveX/WebShellContainer.h
+++ webshell/embed/ActiveX/WebShellContainer.h
@@ -67,7 +67,7 @@ public:
 	NS_IMETHOD SetChrome(PRUint32 aNewChromeMask);
 	NS_IMETHOD GetChrome(PRUint32& aChromeMaskResult);
 	NS_IMETHOD SetTitle(const PRUnichar* aTitle);
-    NS_IMETHOD GetTitle(PRUnichar** aResult) = 0;
+    NS_IMETHOD GetTitle(PRUnichar** aResult);
 	NS_IMETHOD SetStatus(const PRUnichar* aStatus);
 	NS_IMETHOD GetStatus(const PRUnichar** aResult);
 	NS_IMETHOD SetDefaultStatus(const PRUnichar* aStatus);
