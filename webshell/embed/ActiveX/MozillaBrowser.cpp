253cb2b7ccf4da44aa5be27b679bba14610e321e
locka%iol.ie <locka%iol.ie>
1999-12-07 23:01:59 +0000

    Fixed bad const casting

--- webshell/embed/ActiveX/MozillaBrowser.cpp
+++ webshell/embed/ActiveX/MozillaBrowser.cpp
@@ -1858,16 +1858,16 @@ HRESULT STDMETHODCALLTYPE CMozillaBrowser::get_LocationName(BSTR __RPC_FAR *Loca
 	}
 
 	// Get the url from the web shell
-   nsXPIDLString  locationName;
-	m_pIWebShell->GetTitle(getter_Copies(locationName));
-	if (locationName == nsnull)
+	nsXPIDLString szLocationName;
+	m_pIWebShell->GetTitle(getter_Copies(szLocationName));
+	if (nsnull == (const PRUnichar *) szLocationName)
 	{
 		RETURN_E_UNEXPECTED();
 	}
 
 	// Convert the string to a BSTR
 	USES_CONVERSION;
-	LPOLESTR pszConvertedLocationName = W2OLE(const_cast<PRUnichar *>(locationName));
+	LPCOLESTR pszConvertedLocationName = W2COLE((const PRUnichar *) szLocationName);
 	*LocationName = SysAllocString(pszConvertedLocationName);
 
 	return S_OK;
