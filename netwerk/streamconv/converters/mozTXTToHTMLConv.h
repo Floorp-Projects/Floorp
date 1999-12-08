ad4360fbc533b0beb6daa8dea0adf9adc63e7d5d
rhp%netscape.com <rhp%netscape.com>
1999-12-08 02:10:57 +0000

    Fix for TXT - HTML conversion - r: rhp - external contributor

--- netwerk/streamconv/converters/mozTXTToHTMLConv.h
+++ netwerk/streamconv/converters/mozTXTToHTMLConv.h
@@ -33,8 +33,8 @@
  * Contributor(s):
  */
 
-/*
-  Description: Currently only functions to enhance plain text with HTML tags. See mozITXTToHTMLConv.
+/**
+  Description: Currently only functions to enhance plain text with HTML tags. See mozITXTToHTMLConv. Stream conversion is defunct.
 */
 
 #ifndef _mozTXTToHTMLConv_h__
@@ -45,90 +45,42 @@
 
 static NS_DEFINE_CID(kTXTToHTMLConvCID, MOZITXTTOHTMLCONV_CID);
 
-class mozTXTToHTMLConv : public mozITXTToHTMLConv { 
+class mozTXTToHTMLConv : public mozITXTToHTMLConv
+{
+
 public:
+
   mozTXTToHTMLConv();
   virtual ~mozTXTToHTMLConv();
   NS_DECL_ISUPPORTS
-  static const nsIID& GetIID(void) { static nsIID iid = MOZITXTTOHTMLCONV_IID; return iid; }
+  static const nsIID& GetIID(void)
+       { static nsIID iid = MOZITXTTOHTMLCONV_IID; return iid; }
 
   NS_DECL_MOZITXTTOHTMLCONV
   NS_DECL_NSISTREAMOBSERVER
   NS_DECL_NSISTREAMLISTENER
   NS_DECL_NSISTREAMCONVERTER
 
-/*
-  see ScanTXT
+/**
+  see mozITXTToHTMLConv::ScanTXT
  */
   static nsAutoString ScanTXT(const nsAutoString& text, PRUint32 whattodo);
 
-/*
-  see ScanHTML
+/**
+  see mozITXTToHTMLConv::ScanHTML
  */
   static nsAutoString ScanHTML(const nsAutoString& text, PRUint32 whattodo);
 
-/*
-  see CiteLevelTXT
+/**
+  see mozITXTToHTMLConv::CiteLevelTXT
  */
   static PRInt32 CiteLevelTXT(const nsAutoString& line,PRUint32& logLineStart);
 
+///////////////////////////////////////////////////////////////////
 protected:
+///////////////////////////////////////////////////////////////////
 
-/*
-  Currently only changes "<", ">" and "&". All others stay as they are.<p>
-  "Char" in function name to avoid side effects with nsString(char)
-  constructors.
-  @param ch (in)
-  @return ch in its HTML representation
-*/
-  static nsAutoString EscapeChar(const PRUnichar ch);
-
-/*
-  See EscapeChar
-*/
-  static nsAutoString EscapeStr(const nsAutoString& aString);
-
-/*
-  Currently only reverts "<", ">" and "&". All others stay as they are.<p>
-  @param aString (in) HTML string
-  @return aString in its plain text representation
-*/
-  static nsAutoString UnescapeStr(const nsAutoString& aString);
-
-/*
-  Completes<ul>
-  <li>1. "mozilla@bucksch.org" to "mailto:mozilla@bucksch.org"
-  <li>2. "www.mozilla.org" to "http://www.mozilla.org"
-  <li>3. "ftp.mozilla.org" to "ftp://www.mozilla.org"
-  </ul>
-  It does no check, if the resulting URL is valid.
-  @param text (in): abbreviated URL
-  @param pos (in): position of "@" (case 1) or first "." (case 2 and 3)
-  @return Completed URL at success and empty string at failure
- */
-  static nsAutoString CompleteAbbreviatedURL(const nsAutoString& text,
-       const PRUint32 pos);
-
-/*
-  <p><em>Note:</em> replaceBefore + replaceAfter + 1 (for char at pos) chars
-  in text should be replaced by outputHTML.</p>
-  <p><em>Note:</em> This function should be able to process a URL on multiple
-  lines, but currently, ScanForURLs is called for every line, so it can't.</p>
-  @param text (in): includes possibly a URL
-  @param pos (in): position in text, where either ":", "." or "@" are found
-  @param whathasbeendone (in): What the calling ScanTXT did/has to do with the
-              (not-linkified) text. (Needed to calculate replaceBefore.)
-              NOT what will be done with the content of the link.
-  @param outputHTML (out): URL with HTML-a tag
-  @param replaceBefore (out): Number of chars of URL before pos
-  @param replaceAfter (out): Number of chars of URL after pos
-  @return URL found
-*/
-  static PRBool FindURL(const nsAutoString& text, const PRUint32 pos,
-       const PRUint32 whathasbeendone, nsAutoString& outputHTML,
-       PRInt32& replaceBefore, PRInt32& replaceAfter);
-
-/*
+/**
   @param text (in): the source string
   @param start (in): offset of text specifying the start of the new object
   @return a new (local) object containing the substring
@@ -143,11 +95,12 @@ protected:
     LT_DIGIT
   };
 
-/*
+/**
   @param text (in): the string to search through.<p>
          If before = IGNORE,<br>
            rep is compared starting at 1. char of text (text[0]),<br>
            else starting at 2. char of text (text[1]).
+         Chars after "after"-delimiter are ignored.
   @param rep (in): the string to look for
   @param before (in): limitation before rep
   @param after (in): limitation after rep
@@ -156,20 +109,81 @@ protected:
   static PRBool ItMatchesDelimited(const nsAutoString& text,
        const nsAutoString& rep, LIMTYPE before, LIMTYPE after);
 
-/*
+/**
   @param see ItMatchesDelimited
   @return Number of ItMatchesDelimited in text
 */
   static PRUint32 NumberOfMatches(const nsAutoString& text,
        const nsAutoString& rep, LIMTYPE before, LIMTYPE after);
 
-/*
+/**
+  Currently only changes "<", ">" and "&". All others stay as they are.<p>
+  "Char" in function name to avoid side effects with nsString(ch)
+  constructors.
+  @param ch (in)
+  @return ch in its HTML representation
+*/
+  static nsAutoString EscapeChar(const PRUnichar ch);
+
+/**
+  See EscapeChar
+*/
+  static nsAutoString EscapeStr(const nsAutoString& aString);
+
+/**
+  Currently only reverts "<", ">" and "&". All others stay as they are.<p>
+  @param aString (in) HTML string
+  @return aString in its plain text representation
+*/
+  static nsAutoString UnescapeStr(const nsAutoString& aString);
+
+/**
+  Completes<ul>
+  <li>Case 1: "mozilla@bucksch.org" to "mailto:mozilla@bucksch.org"
+  <li>Case 2: "www.mozilla.org" to "http://www.mozilla.org"
+  <li>Case 3: "ftp.mozilla.org" to "ftp://www.mozilla.org"
+  </ul>
+  It does no check, if the resulting URL is valid.
+  @param text (in): abbreviated URL
+  @param pos (in): position of "@" (case 1) or first "." (case 2 and 3)
+  @return Completed URL at success and empty string at failure
+ */
+  static nsAutoString CompleteAbbreviatedURL(const nsAutoString& text,
+       const PRUint32 pos);
+
+/**
+  <em>Note</em>: I use different strategies to pass context between the
+  functions (full text and pos vs. cutted text and col0, glphyTextLen vs.
+  replaceBefore/-After). It makes some sense, but is hard to understand
+  (maintain) :-(. If somebody has an elegant way, tell me.
+*/
+
+/**
+  <p><em>Note:</em> replaceBefore + replaceAfter + 1 (for char at pos) chars
+  in text should be replaced by outputHTML.</p>
+  <p><em>Note:</em> This function should be able to process a URL on multiple
+  lines, but currently, ScanForURLs is called for every line, so it can't.</p>
+  @param text (in): includes possibly a URL
+  @param pos (in): position in text, where either ":", "." or "@" are found
+  @param whathasbeendone (in): What the calling ScanTXT did/has to do with the
+              (not-linkified) text. (Needed to calculate replaceBefore.)
+              NOT what will be done with the content of the link.
+  @param outputHTML (out): URL with HTML-a tag
+  @param replaceBefore (out): Number of chars of URL before pos
+  @param replaceAfter (out): Number of chars of URL after pos
+  @return URL found
+*/
+  static PRBool FindURL(const nsAutoString& text, PRUint32 pos,
+       const PRUint32 whathasbeendone, nsAutoString& outputHTML,
+       PRInt32& replaceBefore, PRInt32& replaceAfter);
+
+/**
   @param text (in): line of text possibly with tagTXT.<p>
               if col0 is true,
                 starting with tagTXT<br>
               else
                 starting one char before tagTXT
-  @param col0 (in): tagTXT is on the beginning of the logical line.
+  @param col0 (in): tagTXT is on the beginning of the line (or paragraph).
               open must be 0 then.
   @param tagTXT (in): Tag in plaintext to search for, e.g. "*"
   @param tagHTML (in): HTML-Tag to replace tagTXT with,
@@ -185,7 +199,7 @@ protected:
        const nsAutoString tagHTML, const nsAutoString attributeHTML,
        nsAutoString& outputHTML, PRUint32& openTags);
 
-/*
+/**
   @param text (in), col0 (in): see GlyphHit
   @param tagTXT (in): Smily, see also StructPhraseHit
   @param tagHTML (in): see StructPhraseHit
@@ -195,13 +209,17 @@ protected:
        const nsAutoString tagTXT, const nsAutoString tagHTML,
        nsAutoString& outputHTML, PRInt32& glyphTextLen);
 
-/*
+/**
   Checks, if we can replace some chars at the start of line with prettier HTML
   code.<p>
   If success is reported, replace the first glyphTextLen chars with outputHTML
 
-  @param text (in): line in msg, starting at char to investigate
-  @param col0 (in): text starts at the beginning of the logical line
+  @param text (in): line of text possibly with Glyph.<p>
+              If col0 is true,
+                starting with Glyph <br><!-- (br not part of text) -->
+              else
+                starting one char before Glyph
+  @param col0 (in): text starts at the beginning of the line (or paragraph)
   @param outputHTML (out): see StructPhraseHit
   @param glyphTextLen (out): Length of original text to replace
   @return see StructPhraseHit
@@ -209,14 +227,17 @@ protected:
   static PRBool GlyphHit(const nsAutoString& text, PRBool col0,
        nsAutoString& outputHTML, PRInt32& glyphTextLen);
 
+//////////////////////////////////////////////////////////
 private:
+//////////////////////////////////////////////////////////
+
   enum modetype {
-         unknown = 0,
+         unknown,
          RFC1738,          /* Check, if RFC1738, APPENDIX compliant,
                               like "<URL:http://www.mozilla.org>". */
          RFC2396E,         /* RFC2396, APPENDIX E allows anglebrackets (like
                               "<http://www.mozilla.org>") (without "URL:") or
-                              quotation marks (like""http://www.mozilla.org"").
+                              quotation marks(like ""http://www.mozilla.org"").
                               Also allow email addresses without scheme,
                               e.g. "<mozilla@bucksch.org>" */
          freetext,         /* assume heading scheme
@@ -226,15 +247,16 @@ private:
                               (including linebreaks) end the URL.
                               Other certain (punctation) characters (see code)
                               at the end are stripped off. */
-         abbreviated       /* "www.mozilla.org", "ftp.mozilla.org" and
-                              "mozilla@bucksch.org". Similar to freetext. */
+         abbreviated       /* Similar to freetext, but without scheme, e.g.
+	                      "www.mozilla.org", "ftp.mozilla.org" and
+                              "mozilla@bucksch.org". */
       /* RFC1738 and RFC2396E type URLs may use multiple lines,
          whitespace is stripped. Special characters like "," stay intact.*/
   };
 
 /**
  * @param text (in), pos (in): see FindURL
- * @param check (in): Start must be conform with this modetype
+ * @param check (in): Start must be conform with this mode
  * @param start (out): Position in text, where URL (including brackets or
  *             similar) starts
  * @return |check|-conform start has been found
@@ -244,7 +266,7 @@ private:
 
 /**
  * @param text (in), pos (in): see FindURL
- * @param check (in): End must be conform with this modetype
+ * @param check (in): End must be conform with this mode
  * @param start (in): see FindURLStart
  * @param end (out): Similar to |start| param of FindURLStart
  * @return |check|-conform end has been found
@@ -254,9 +276,10 @@ private:
 
 /**
  * @param text (in), pos (in), whathasbeendone (in): see FindURL
+ * @param check (in): Current mode
  * @param start (in), end (in): see FindURLEnd
  * @param txtURL (out): Guessed (raw) URL.
-               Without whitespace, but not completed.
+ *             Without whitespace, but not completed.
  * @param desc (out): Link as shown to the user, but already escaped.
  *             Should be placed between the <a> and </a> tags.
  * @param replaceBefore(out), replaceAfter (out): see FindURL
@@ -277,9 +300,9 @@ private:
        nsAutoString& outputHTML);
 };
 
-// BenB: I had to move these to here to get stuff compiling 
-//       correctly on Win32 & Mac
-const PRInt32 lastMode = 4;  // Needed (only) by FindURL
-const PRUint32 numberOfModes = 4;       // dito
+// It's said, that Win32 and Mac don't like static const members
+const PRInt32 mozTXTToHTMLConv_lastMode = 4;
+	                        // Needed (only) by mozTXTToHTMLConv::FindURL
+const PRInt32 mozTXTToHTMLConv_numberOfModes = 4;  // dito; unknown not counted
 
 #endif
