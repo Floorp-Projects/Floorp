761d2d7c8094a0d386bc82fe4f4d32d33c2312d3
srinivas@netscape.com <srinivas@netscape.com>
1999-12-22 20:51:26 +0000

    Fixed PR_SetThreadAffinityMask, which returned random value on Irix. Bugzilla #21621.

--- nsprpub/pr/src/threads/prcthr.c
+++ nsprpub/pr/src/threads/prcthr.c
@@ -298,6 +298,8 @@ PR_IMPLEMENT(PRInt32) PR_SetThreadAffinityMask(PRThread *thread, PRUint32 mask )
 #ifdef HAVE_THREAD_AFFINITY
 #ifndef IRIX
     return _PR_MD_SETTHREADAFFINITYMASK(thread, mask);
+#else
+	return 0;
 #endif
 #else
 
