2900d5c882728aca1aa2607d3381d0fc7e05a1c5
valeski@netscape.com <valeski@netscape.com>
1999-12-22 03:37:57 +0000

    r=waterson. 22306. symbolic links weren't clickable in dir list view. now they are.

--- xpfe/components/directory/directory.js
+++ xpfe/components/directory/directory.js
@@ -77,7 +77,7 @@ function OnClick(event)
         // directories.
         var type = treeitem.getAttribute('type');
 
-        if (targetclass == 'filename' && type == 'FILE') {
+        if (targetclass == 'filename' && (type == 'FILE' || type == 'SYM-LINK') ) {
             var url = treeitem.getAttribute('id');
 
             debug('navigating to ' + url + '\n');
