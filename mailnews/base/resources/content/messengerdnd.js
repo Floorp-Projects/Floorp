4a766f01563d644bd24a63d0f268bba52dd26639
sspitzer%netscape.com <sspitzer%netscape.com>
1999-12-28 19:17:36 +0000

    the very humble beginnings of drag and drop for messenger.

---
+++ mailnews/base/resources/content/messengerdnd.js
@@ -0,0 +1,18 @@
+function DragOverTree(event)
+{
+	dump("DragOverTree\n");
+	dump(event + "\n");
+	return true;
+}
+function BeginDragTree(event)
+{
+	dump("BeginDragTree\n");
+	dump(event + "\n");
+	return true;
+}
+function DropOnTree(event)
+{
+	dump("DropOnTree\n");
+	dump(event + "\n");
+	return true;
+}
