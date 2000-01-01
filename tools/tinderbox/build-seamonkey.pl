ae9ed20b4432620d25307ed4d23786654072ec89
cls%seawood.org <cls%seawood.org>
2000-01-01 22:11:35 +0000

    Workaround for the *ahem* Y2K bug in date. ;)

--- tools/tinderbox/build-seamonkey.pl
+++ tools/tinderbox/build-seamonkey.pl
@@ -6,7 +6,7 @@ use Sys::Hostname;
 use POSIX "sys_wait_h";
 use Cwd;
 
-$Version = '$Revision: 1.28 $ ';
+$Version = '$Revision: 1.29 $ ';
 
 
 sub PrintUsage {
@@ -386,7 +386,7 @@ sub BuildIt {
     $StartTime = time;
     
     if ($UseTimeStamp) {
-      $BuildStart = `date '+%D %H:%M'`;
+      $BuildStart = `date '+%m/%d/%Y %H:%M'`;
       chomp($BuildStart);
       $CVSCO .= " -D '$BuildStart'";
     }
