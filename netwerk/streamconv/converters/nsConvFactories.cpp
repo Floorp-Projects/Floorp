4d1a1b38ed466eca80e0cf5ce33c1c2d6a4833ac
dougt%netscape.com <dougt%netscape.com>
2000-01-03 23:37:30 +0000

    Converting to use nsIModule macro.  r=dp.

--- netwerk/streamconv/converters/nsConvFactories.cpp
+++ netwerk/streamconv/converters/nsConvFactories.cpp
@@ -32,32 +32,6 @@ nsresult NS_NewFTPDirListingConv(nsFTPDirListingConv** result);
 nsresult NS_NewMultiMixedConv(nsMultiMixedConv** result);
 nsresult MOZ_NewTXTToHTMLConv(mozTXTToHTMLConv** result);
 
-// Module implementation
-class nsConvModule : public nsIModule
-{
-public:
-    nsConvModule();
-    virtual ~nsConvModule();
-
-    NS_DECL_ISUPPORTS
-
-    NS_DECL_NSIMODULE
-
-protected:
-    nsresult Initialize();
-
-    void Shutdown();
-
-    PRBool mInitialized;
-    nsCOMPtr<nsIGenericFactory> mFTPDirListingConvFactory;
-    nsCOMPtr<nsIGenericFactory> mMultiMixedConvFactory;
-    nsCOMPtr<nsIGenericFactory> mTXTToHTMLConvFactory;
-};
-
-//----------------------------------------------------------------------
-
-// Functions used to create new instances of a given object by the
-// generic factory.
 
 static NS_IMETHODIMP                 
 CreateNewFTPDirListingConv(nsISupports* aOuter, REFNSIID aIID, void **aResult) 
@@ -131,230 +105,32 @@ CreateNewTXTToHTMLConvFactory(nsISupports* aOuter, REFNSIID aIID, void **aResult
     return rv;              
 }
 
-
-//----------------------------------------------------------------------
-
-nsConvModule::nsConvModule()
-    : mInitialized(PR_FALSE)
-{
-    NS_INIT_ISUPPORTS();
-}
-
-nsConvModule::~nsConvModule()
-{
-    Shutdown();
-}
-
-NS_IMPL_ISUPPORTS(nsConvModule, NS_GET_IID(nsIModule))
-
-// Perform our one-time intialization for this module
-nsresult
-nsConvModule::Initialize()
-{
-    if (mInitialized) {
-        return NS_OK;
-    }
-    mInitialized = PR_TRUE;
-    return NS_OK;
-}
-
-// Shutdown this module, releasing all of the module resources
-void
-nsConvModule::Shutdown()
-{
-    // Release the factory objects
-    mFTPDirListingConvFactory = nsnull;
-    mMultiMixedConvFactory = nsnull;
-    mTXTToHTMLConvFactory = nsnull;
-}
-
-// Create a factory object for creating instances of aClass.
-NS_IMETHODIMP
-nsConvModule::GetClassObject(nsIComponentManager *aCompMgr,
-                               const nsCID& aClass,
-                               const nsIID& aIID,
-                               void** r_classObj)
-{
-    nsresult rv;
-
-    // Defensive programming: Initialize *r_classObj in case of error below
-    if (!r_classObj) {
-        return NS_ERROR_INVALID_POINTER;
-    }
-    *r_classObj = NULL;
-
-    // Do one-time-only initialization if necessary
-    if (!mInitialized) {
-        rv = Initialize();
-        if (NS_FAILED(rv)) {
-            // Initialization failed! yikes!
-            return rv;
-        }
-    }
-
-    // Choose the appropriate factory, based on the desired instance
-    // class type (aClass).
-    nsCOMPtr<nsIGenericFactory> fact;
-    if (aClass.Equals(kFTPDirListingConverterCID)) {
-        if (!mFTPDirListingConvFactory) {
-            // Create and save away the factory object for creating
-            // new instances of FTPDirListingConv. This way if we are called
-            // again for the factory, we won't need to create a new
-            // one.
-            rv = NS_NewGenericFactory(getter_AddRefs(mFTPDirListingConvFactory),
-                                      CreateNewFTPDirListingConv);
-        }
-        fact = mFTPDirListingConvFactory;
-    }
-    else if (aClass.Equals(kMultiMixedConverterCID)) {
-        if (!mMultiMixedConvFactory) {
-            // Create and save away the factory object for creating
-            // new instances of MultiMixedConv. This way if we are called
-            // again for the factory, we won't need to create a new
-            // one.
-            rv = NS_NewGenericFactory(getter_AddRefs(mMultiMixedConvFactory),
-                                      CreateNewMultiMixedConvFactory);
-        }
-        fact = mMultiMixedConvFactory;
-    }
-    else if (aClass.Equals(kTXTToHTMLConvCID)) {
-        if (!mTXTToHTMLConvFactory) {
-            // Create and save away the factory object for creating
-            // new instances of MultiMixedConv. This way if we are called
-            // again for the factory, we won't need to create a new
-            // one.
-            rv = NS_NewGenericFactory(getter_AddRefs(mTXTToHTMLConvFactory),
-                                      CreateNewTXTToHTMLConvFactory);
-        }
-        fact = mTXTToHTMLConvFactory;
-    }
-    else {
-        rv = NS_ERROR_FACTORY_NOT_REGISTERED;
-#ifdef DEBUG
-        char* cs = aClass.ToString();
-        printf("+++ nsConvModule: unable to create factory for %s\n", cs);
-        nsCRT::free(cs);
-#endif
-    }
-
-    if (fact) {
-        rv = fact->QueryInterface(aIID, r_classObj);
-    }
-
-    return rv;
-}
-
-//----------------------------------------
-
-struct Components {
-    const char* mDescription;
-    const nsID* mCID;
-    const char* mProgID;
-};
-
 // The list of components we register
-static Components gComponents[] = {
-    { "FTPDirListingConverter", &kFTPDirListingConverterCID,
-      NS_ISTREAMCONVERTER_KEY "?from=text/ftp-dir-unix?to=application/http-index-format", },
-    { "FTPDirListingConverter", &kFTPDirListingConverterCID,
-      NS_ISTREAMCONVERTER_KEY "?from=text/ftp-dir-nt?to=application/http-index-format", },
-    { "MultiMixedConverter", &kMultiMixedConverterCID,
-      NS_ISTREAMCONVERTER_KEY "?from=multipart/x-mixed-replace?to=text/html", },
-    { "TXTToHTMLConverter", &kTXTToHTMLConvCID,
-    NS_ISTREAMCONVERTER_KEY "?from=text/plain?to=text/html", },
+static nsModuleComponentInfo components[] = 
+{
+    { "FTPDirListingConverter", 
+      NS_FTPDIRLISTINGCONVERTER_CID,
+      NS_ISTREAMCONVERTER_KEY "?from=text/ftp-dir-unix?to=application/http-index-format", 
+      CreateNewFTPDirListingConv
+    },
+
+    { "FTPDirListingConverter", 
+      NS_FTPDIRLISTINGCONVERTER_CID,
+      NS_ISTREAMCONVERTER_KEY "?from=text/ftp-dir-nt?to=application/http-index-format", 
+      CreateNewFTPDirListingConv
+    },
+    
+    { "MultiMixedConverter", 
+      NS_MULTIMIXEDCONVERTER_CID,
+      NS_ISTREAMCONVERTER_KEY "?from=multipart/x-mixed-replace?to=text/html", 
+      CreateNewMultiMixedConvFactory
+    },
+
+    { "TXTToHTMLConverter", 
+      MOZITXTTOHTMLCONV_CID,
+      NS_ISTREAMCONVERTER_KEY "?from=text/plain?to=text/html", 
+      CreateNewTXTToHTMLConvFactory
+    },
 };
-#define NUM_COMPONENTS (sizeof(gComponents) / sizeof(gComponents[0]))
 
-NS_IMETHODIMP
-nsConvModule::RegisterSelf(nsIComponentManager *aCompMgr,
-                             nsIFileSpec* aPath,
-                             const char* registryLocation,
-                             const char* componentType)
-{
-    nsresult rv = NS_OK;
-
-#ifdef DEBUG
-    printf("*** Registering Conv components\n");
-#endif
-
-    Components* cp = gComponents;
-    Components* end = cp + NUM_COMPONENTS;
-    while (cp < end) {
-        rv = aCompMgr->RegisterComponentSpec(*cp->mCID, cp->mDescription,
-                                             cp->mProgID, aPath, PR_TRUE,
-                                             PR_TRUE);
-        if (NS_FAILED(rv)) {
-#ifdef DEBUG
-            printf("nsConvModule: unable to register %s component => %x\n",
-                   cp->mDescription, rv);
-#endif
-            break;
-        }
-        cp++;
-    }
-
-    return rv;
-}
-
-NS_IMETHODIMP
-nsConvModule::UnregisterSelf(nsIComponentManager* aCompMgr,
-                               nsIFileSpec* aPath,
-                               const char* registryLocation)
-{
-#ifdef DEBUG
-    printf("*** Unregistering Conv components\n");
-#endif
-    Components* cp = gComponents;
-    Components* end = cp + NUM_COMPONENTS;
-    while (cp < end) {
-        nsresult rv = aCompMgr->UnregisterComponentSpec(*cp->mCID, aPath);
-        if (NS_FAILED(rv)) {
-#ifdef DEBUG
-            printf("nsConvModule: unable to unregister %s component => %x\n",
-                   cp->mDescription, rv);
-#endif
-        }
-        cp++;
-    }
-
-    return NS_OK;
-}
-
-NS_IMETHODIMP
-nsConvModule::CanUnload(nsIComponentManager *aCompMgr, PRBool *okToUnload)
-{
-    if (!okToUnload) {
-        return NS_ERROR_INVALID_POINTER;
-    }
-    *okToUnload = PR_FALSE;
-    return NS_ERROR_FAILURE;
-}
-
-//----------------------------------------------------------------------
-
-static nsConvModule *gModule = NULL;
-
-extern "C" NS_EXPORT nsresult NSGetModule(nsIComponentManager *servMgr,
-                                          nsIFileSpec* location,
-                                          nsIModule** return_cobj)
-{
-    nsresult rv = NS_OK;
-
-    NS_ENSURE_ARG_POINTER(return_cobj);
-    NS_ENSURE_FALSE(gModule, NS_ERROR_FAILURE);
-
-    // Create and initialize the module instance
-    nsConvModule *m = new nsConvModule();
-    if (!m) {
-        return NS_ERROR_OUT_OF_MEMORY;
-    }
-
-    // Increase refcnt and store away nsIModule interface to m in return_cobj
-    rv = m->QueryInterface(NS_GET_IID(nsIModule), (void**)return_cobj);
-    if (NS_FAILED(rv)) {
-        delete m;
-        m = nsnull;
-    }
-    gModule = m;                  // WARNING: Weak Reference
-    return rv;
-}
+NS_IMPL_NSGETMODULE("nsConvModule", components);
