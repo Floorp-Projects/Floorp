ea36a0b1bed7e374d91e9d7905c10ad35552f92f
dougt@netscape.com <dougt@netscape.com>
2000-01-03 22:59:05 +0000

    Converting to use nsIModule macro.  r=dp.

--- db/mork/build/nsMorkFactory.cpp
+++ db/mork/build/nsMorkFactory.cpp
@@ -39,192 +39,21 @@ public:
 
 };
 
-
-// include files for components this factory creates...
-static NS_DEFINE_CID(kComponentManagerCID, NS_COMPONENTMANAGER_CID);
-static NS_DEFINE_CID(kCMorkFactory, NS_MORK_CID);
-
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsMorkFactoryFactory)
 
-////////////////////////////////////////////////////////////
-//
-class nsMorkModule : public nsIModule
-{   
-public:
-
-	nsMorkModule(); 
-	virtual ~nsMorkModule();
-
-	// nsISupports methods
-	NS_DECL_ISUPPORTS 
-
-    NS_DECL_NSIMODULE
-
-protected:
-    nsresult Initialize();
-
-    void Shutdown();
-
-    PRBool mInitialized;
-	nsCOMPtr<nsIGenericFactory> mMorkFactory;
-};   
-
-nsMorkModule::nsMorkModule()
-    : mInitialized(PR_FALSE)
-{   
-	NS_INIT_REFCNT();
-}   
-
-
-NS_IMPL_ISUPPORTS(nsMorkModule, NS_GET_IID(nsIModule))
-
-nsMorkModule::~nsMorkModule()
+static nsModuleComponentInfo components[] =
 {
-    Shutdown();
-}
-
-// Perform our one-time intialization for this module
-nsresult nsMorkModule::Initialize()
-{
-    if (mInitialized)
-        return NS_OK;
-
-    mInitialized = PR_TRUE;
-    return NS_OK;
-}
-
-// Shutdown this module, releasing all of the module resources
-void nsMorkModule::Shutdown()
-{
-    // Release the factory objects
-    mMorkFactory = null_nsCOMPtr();
-}
-
-// Create a factory object for creating instances of aClass.
-NS_IMETHODIMP nsMorkModule::GetClassObject(nsIComponentManager *aCompMgr,
-                               const nsCID& aClass,
-                               const nsIID& aIID,
-                               void** r_classObj)
-{
-    nsresult rv;
-
-    // Defensive programming: Initialize *r_classObj in case of error below
-    if (!r_classObj)
-        return NS_ERROR_INVALID_POINTER;
-
-    *r_classObj = NULL;
-
-    // Do one-time-only initialization if necessary
-    if (!mInitialized) 
-    {
-        rv = Initialize();
-        if (NS_FAILED(rv)) // Initialization failed! yikes!
-            return rv;
+    { "Mork Factory", 
+      NS_MORK_CID, 
+      nsnull,
+      nsMorkFactoryFactoryConstructor 
     }
-
-    // Choose the appropriate factory, based on the desired instance
-    // class type (aClass).
-    nsCOMPtr<nsIGenericFactory> fact;
-
-    if (aClass.Equals(kCMorkFactory))
-    {
-        if (!mMorkFactory)
-            rv = NS_NewGenericFactory(getter_AddRefs(mMorkFactory), &nsMorkFactoryFactoryConstructor);
-        fact = mMorkFactory;
-    }
-    if (fact)
-        rv = fact->QueryInterface(aIID, r_classObj);
-
-    return rv;
-}
-
-
-struct Components {
-    const char* mDescription;
-    const nsID* mCID;
-    const char* mProgID;
 };
 
-// The list of components we register
-static Components gComponents[] = {
-    { "Mork Factory", &kCMorkFactory,
-      nsnull },
-};
-#define NUM_COMPONENTS (sizeof(gComponents) / sizeof(gComponents[0]))
+NS_IMPL_NSGETMODULE("nsMorkModule", components);
 
-NS_IMETHODIMP nsMorkModule::RegisterSelf(nsIComponentManager *aCompMgr,
-                          nsIFileSpec* aPath,
-                          const char* registryLocation,
-                          const char* componentType)
-{
-    nsresult rv = NS_OK;
 
-    Components* cp = gComponents;
-    Components* end = cp + NUM_COMPONENTS;
-    while (cp < end) 
-    {
-        rv = aCompMgr->RegisterComponentSpec(*cp->mCID, cp->mDescription,
-                                             cp->mProgID, aPath, PR_TRUE,
-                                             PR_TRUE);
-        if (NS_FAILED(rv)) 
-            break;
-        cp++;
-    }
 
-    return rv;
-}
-
-NS_IMETHODIMP nsMorkModule::UnregisterSelf(nsIComponentManager* aCompMgr,
-                            nsIFileSpec* aPath,
-                            const char* registryLocation)
-{
-    Components* cp = gComponents;
-    Components* end = cp + NUM_COMPONENTS;
-    while (cp < end) 
-    {
-        aCompMgr->UnregisterComponentSpec(*cp->mCID, aPath);
-        cp++;
-    }
-
-    return NS_OK;
-}
-
-NS_IMETHODIMP nsMorkModule::CanUnload(nsIComponentManager *aCompMgr, PRBool *okToUnload)
-{
-    if (!okToUnload)
-        return NS_ERROR_INVALID_POINTER;
-
-    *okToUnload = PR_FALSE;
-    return NS_ERROR_FAILURE;
-}
-
-//----------------------------------------------------------------------
-static nsMorkModule *gModule = NULL;
-
-extern "C" NS_EXPORT nsresult NSGetModule(nsIComponentManager *servMgr,
-                                          nsIFileSpec* location,
-                                          nsIModule** return_cobj)
-{
-    nsresult rv = NS_OK;
-
-    NS_ASSERTION(return_cobj, "Null argument");
-    NS_ASSERTION(gModule == NULL, "nsMorkModule: Module already created.");
-
-    // Create an initialize the imap module instance
-    nsMorkModule *module = new nsMorkModule();
-    if (!module)
-        return NS_ERROR_OUT_OF_MEMORY;
-
-    // Increase refcnt and store away nsIModule interface to m in return_cobj
-    rv = module->QueryInterface(nsIModule::GetIID(), (void**)return_cobj);
-    if (NS_FAILED(rv)) 
-    {
-        delete module;
-        module = nsnull;
-    }
-    gModule = module;                  // WARNING: Weak Reference
-    return rv;
-}
 
 static nsIMdbFactory *gMDBFactory = nsnull;
 
