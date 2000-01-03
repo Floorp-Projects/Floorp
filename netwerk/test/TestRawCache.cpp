0ec364d36abe0ba1e6a8bdf64b70591b7abd62c1
dougt@netscape.com <dougt@netscape.com>
2000-01-03 23:34:15 +0000

    Including nsCRT.h/nsIFileSpec directly.  This prep work for the nsIFile landing.

--- netwerk/test/TestRawCache.cpp
+++ netwerk/test/TestRawCache.cpp
@@ -36,8 +36,7 @@
 #include "nsIPref.h"
 #include "prenv.h"
 #include "nsIFileStream.h"
-
-
+#include "nsIFileSpec.h"
 // Number of test entries to be placed in the cache
 #define NUM_CACHE_ENTRIES  250
 
