ccadd5d62d306f10ee86b7ef45c0d3ab01566fbe
alecf@netscape.com <alecf@netscape.com>
2000-01-06 07:35:25 +0000

    add treeframe-level interface to EnsureRowIsVisible, and add a new nsITreeFrame interface so that EnsureRowIsVisible can be called from outside of the layout DLL. first part of fix to #12895 r=bryner

---
+++ layout/xul/base/public/nsITreeFrame.h
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ */
+
+#ifndef nsITreeFrame_h__
+#define nsITreeFrame_h__
+
+#include "nsISupports.h"
+
+#define BLAH_IID_STR "06b8921c-1dd2-11b2-96f2-d5925c3d48ca"
+
+#define NS_ITREEFRAME_IID \
+  {0x06b8921c, 0x1dd2, 0x11b2, \
+    { 0x96, 0xf2, 0xd5, 0x92, 0x5c, 0x3d, 0x48, 0xca }}
+
+
+class nsITreeFrame : public nsISupports {
+public:
+
+  NS_DEFINE_STATIC_IID_ACCESSOR(NS_ITREEFRAME_IID)
+
+  NS_IMETHOD EnsureRowIsVisible(PRInt32 aRowIndex) = 0;
+
+};
+
+#endif
