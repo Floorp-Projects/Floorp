839d9b1798b2b14092f0eed47049d5475b2f2a8a
hyatt@netscape.com <hyatt@netscape.com>
2000-01-13 02:23:54 +0000

    more xbl work.

---
+++ content/xbl/public/nsIXBLBinding.h
@@ -0,0 +1,60 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ */
+
+/*
+
+  Private interface to the XBL Binding
+
+*/
+
+#ifndef nsIXBLBinding_h__
+#define nsIXBLBinding_h__
+
+#include "nsString.h"
+#include "nsISupports.h"
+
+class nsIContent;
+class nsISupportsArray;
+
+// {DDDBAD20-C8DF-11d3-97FB-00400553EEF0}
+#define NS_IXBLBINDING_IID \
+{ 0xdddbad20, 0xc8df, 0x11d3, { 0x97, 0xfb, 0x0, 0x40, 0x5, 0x53, 0xee, 0xf0 } }
+
+class nsIXBLBinding : public nsISupports
+{
+public:
+  static const nsIID& GetIID() { static nsIID iid = NS_IXBLBINDING_IID; return iid; }
+
+  NS_IMETHOD GetBaseBinding(nsIXBLBinding** aResult) = 0;
+  NS_IMETHOD SetBaseBinding(nsIXBLBinding* aBinding) = 0;
+
+  NS_IMETHOD GetAnonymousContent(nsIContent** aParent) = 0;
+  NS_IMETHOD SetAnonymousContent(nsIContent* aParent) = 0;
+
+  NS_IMETHOD GetBindingElement(nsIContent** aResult) = 0;
+  NS_IMETHOD SetBindingElement(nsIContent* aElement) = 0;
+};
+
+extern nsresult
+NS_NewXBLBinding(nsIXBLBinding** aResult);
+
+#endif // nsIXBLBinding_h__
---
+++ content/xbl/src/nsXBLBinding.cpp
@@ -0,0 +1,123 @@
+#include "nsCOMPtr.h"
+#include "nsIXBLBinding.h"
+#include "nsIInputStream.h"
+#include "nsINameSpaceManager.h"
+#include "nsHashtable.h"
+#include "nsIURI.h"
+#include "nsIURL.h"
+#include "nsIChannel.h"
+#include "nsXPIDLString.h"
+#include "nsIParser.h"
+#include "nsParserCIID.h"
+#include "nsNetUtil.h"
+#include "plstr.h"
+#include "nsIContent.h"
+#include "nsIDocument.h"
+#include "nsIXMLContentSink.h"
+#include "nsLayoutCID.h"
+#include "nsXMLDocument.h"
+
+#include "nsIXBLBinding.h"
+
+// Static IIDs/CIDs. Try to minimize these.
+// None
+
+
+class nsXBLBinding: public nsIXBLBinding
+{
+  NS_DECL_ISUPPORTS
+
+  NS_IMETHOD GetBaseBinding(nsIXBLBinding** aResult);
+  NS_IMETHOD SetBaseBinding(nsIXBLBinding* aBinding);
+
+  NS_IMETHOD GetAnonymousContent(nsIContent** aParent);
+  NS_IMETHOD SetAnonymousContent(nsIContent* aParent);
+
+  NS_IMETHOD GetBindingElement(nsIContent** aResult);
+  NS_IMETHOD SetBindingElement(nsIContent* aElement);
+
+public:
+  nsXBLBinding();
+  virtual ~nsXBLBinding();
+
+// MEMBER VARIABLES
+protected:
+  nsCOMPtr<nsIContent> binding; // Strong. As long as we're around, the binding can't go away.
+  nsCOMPtr<nsIContent> content; // Strong. Our anonymous content stays around with us.
+  nsCOMPtr<nsIXBLBinding> nextBinding; // Strong. The derived binding owns the base class bindings.
+};
+
+
+// Implementation /////////////////////////////////////////////////////////////////
+
+// Implement our nsISupports methods
+NS_IMPL_ISUPPORTS1(nsXBLBinding, nsIXBLBinding)
+
+// Constructors/Destructors
+nsXBLBinding::nsXBLBinding(void)
+{
+  NS_INIT_REFCNT();
+}
+
+nsXBLBinding::~nsXBLBinding(void)
+{
+}
+
+// nsIXBLBinding Interface ////////////////////////////////////////////////////////////////
+
+NS_IMETHODIMP
+nsXBLBinding::GetBaseBinding(nsIXBLBinding** aResult)
+{
+  *aResult = nextBinding;
+  NS_IF_ADDREF(*aResult);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsXBLBinding::SetBaseBinding(nsIXBLBinding* aBinding)
+{
+  nextBinding = aBinding; // Comptr handles rel/add
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsXBLBinding::GetAnonymousContent(nsIContent** aResult)
+{
+  *aResult = content;
+  NS_IF_ADDREF(*aResult);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsXBLBinding::SetAnonymousContent(nsIContent* aParent)
+{
+  content = aParent;
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsXBLBinding::GetBindingElement(nsIContent** aResult)
+{
+  *aResult = binding;
+  NS_IF_ADDREF(*aResult);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsXBLBinding::SetBindingElement(nsIContent* aElement)
+{
+  binding = aElement;
+  return NS_OK;
+}
+
+// Creation Routine ///////////////////////////////////////////////////////////////////////
+
+nsresult
+NS_NewXBLBinding(nsIXBLBinding** aResult)
+{
+  *aResult = new nsXBLBinding;
+  if (!*aResult)
+    return NS_ERROR_OUT_OF_MEMORY;
+  NS_ADDREF(*aResult);
+  return NS_OK;
+}
