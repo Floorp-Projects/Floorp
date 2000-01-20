92c954fff2ae5132635d8549f46f44662b9b8a53
matt@netscape.com <matt@netscape.com>
2000-01-20 03:08:44 +0000

    fixing for font pref emergency r=erik brendan okayed

--- xpfe/components/prefwindow/resources/content/pref-fonts.js
+++ xpfe/components/prefwindow/resources/content/pref-fonts.js
@@ -53,29 +53,34 @@ function selectLang()
 
 
 	lang = document.getElementById("selectLangs").value;
-	var fontSizePrefstring = 'font.size.' + lang;
-	document.getElementById('size').setAttribute('prefstring', fontSizePrefstring);	
+	var fontSizePrefint = 'font.size.variable.' + lang;
+	var fontSizePrefFontint = 'font.size.fixed.' + lang;
+	document.getElementById('size').setAttribute('prefstring', fontSizePrefint);	
+	document.getElementById('sizeFixed').setAttribute('prefstring', fontSizePrefFontint);	
+
 	for (i = 0; i < generics.length; i++)  {
 		var select = document.getElementById(generics[i]);
 		var selectParent = select.parentNode;
 		var selectNew = select.cloneNode(false);
 		var fontPrefstring = 'font.name.' + generics[i] + '.' + lang;
-        select.setAttribute('prefstring', fontPrefstring);
+        selectNew.setAttribute('prefstring', fontPrefstring);
+	//	dump('selectNew:' + selectNew.getAttribute('prefstring'));
 		fonts = enumerator.EnumerateFonts(lang, generics[i], fontCount);
 
 		
 		for (j = 0; j < fonts.length; j++) {
 			var option = document.createElement("html:option");
 			var text = document.createTextNode(fonts[j]);
-			option.appendChild(text);;
+			option.appendChild(text);
 			option.setAttribute('value' , fonts[j]);
-			selectNew.appendChild(option)
+			selectNew.appendChild(option);
 	//		dump(fonts[j] + " || ");
 
 		}
 
 		selectParent.replaceChild(selectNew , select);
-
+       	//dump('select:' + document.getElementById(generics[i]).getAttribute('prefstring') + '\n');
+		
 	}
 }
 
