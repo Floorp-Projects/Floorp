17ef68d3a9ff4fb12169b7734e3990d8bb6f4be3
rginda@netscape.com <rginda@netscape.com>
2000-01-20 20:35:39 +0000

    Fixed testcase

--- js/tests/ecma_3/ExecutionContexts/regress-23346.js
+++ js/tests/ecma_3/ExecutionContexts/regress-23346.js
@@ -46,7 +46,7 @@ function test()
     printStatus ("ECMA Section: 10.1.3: Variable Instantiation.");
     printBugNumber (23346);
 
-    reportCompare ("PASSED", f(), 
+    reportCompare ("PASSED", f(true), 
                    "Unqualified reference should not see Function.prototype");
 
     exitFunc("test");        
