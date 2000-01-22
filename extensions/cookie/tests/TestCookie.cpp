77b30382ada2b5b44938d7b3a3e3967cb394acf0
dp%netscape.com <dp%netscape.com>
2000-01-22 22:46:13 +0000

    Preparing cookies for merging with necko. - Eliminated interdepency betn CookieService and CookieHTTPNotify - CID/Progid for CookieHTTPNotify

--- extensions/cookie/tests/TestCookie.cpp
+++ extensions/cookie/tests/TestCookie.cpp
@@ -1,29 +1,35 @@
-#define NS_IMPL_IDS
-#include "nsISupports.h"
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ */
+
 #include "nsIServiceManager.h"
 #include "nsICookieService.h"
-
-
-static NS_DEFINE_IID(kICookieServiceIID, NS_ICOOKIESERVICE_IID);
-static NS_DEFINE_IID(kCookieServiceCID, NS_COOKIESERVICE_CID);
-
-
 
 int main(PRInt32 argc, char *argv[])
 {
 
-    nsICookieService *cookieService = NULL;
-
     nsresult rv;
-
-    rv = nsServiceManager::GetService(kCookieServiceCID,
-                                    kICookieServiceIID,
-                                    (nsISupports **)&cookieService);
-
-	if (rv == NS_OK) {
-     
-	}
-     
-	return 0;
+    nsCOMPtr<nsICookieService> cookieService = do_GetService(NS_COOKIESERVICE_PROGID, &rv);
+	if (NS_FAILED(rv))
+        return -1;
+    return 0;
 }
 
