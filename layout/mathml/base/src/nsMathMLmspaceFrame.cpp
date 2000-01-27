6b203918571e2234704997cf1ecc13dd365be1d7
rbs%maths.uq.edu.au <rbs%maths.uq.edu.au>
2000-01-27 12:31:12 +0000

    First checkin

---
+++ layout/mathml/base/src/nsMathMLmspaceFrame.cpp
@@ -0,0 +1,148 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Mozilla MathML Project.
+ *
+ * The Initial Developer of the Original Code is The University Of
+ * Queensland.  Portions created by The University Of Queensland are
+ * Copyright (C) 1999 The University Of Queensland.  All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Roger B. Sidje <rbs@maths.uq.edu.au>
+ */
+
+
+#include "nsCOMPtr.h"
+#include "nsHTMLParts.h"
+#include "nsIHTMLContent.h"
+#include "nsFrame.h"
+#include "nsLineLayout.h"
+#include "nsHTMLIIDs.h"
+#include "nsIPresContext.h"
+#include "nsHTMLAtoms.h"
+#include "nsUnitConversion.h"
+#include "nsIStyleContext.h"
+#include "nsStyleConsts.h"
+#include "nsINameSpaceManager.h"
+#include "nsIRenderingContext.h"
+#include "nsIFontMetrics.h"
+#include "nsStyleUtil.h"
+
+#include "nsMathMLmspaceFrame.h"
+
+
+//
+// <mspace> -- space - implementation
+//
+
+nsresult
+NS_NewMathMLmspaceFrame(nsIPresShell* aPresShell, nsIFrame** aNewFrame)
+{
+  NS_PRECONDITION(aNewFrame, "null OUT ptr");
+  if (nsnull == aNewFrame) {
+    return NS_ERROR_NULL_POINTER;
+  }
+  nsMathMLmspaceFrame* it = new (aPresShell) nsMathMLmspaceFrame;
+  if (nsnull == it) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+  *aNewFrame = it;
+  return NS_OK;
+}
+
+nsMathMLmspaceFrame::nsMathMLmspaceFrame()
+{
+}
+
+nsMathMLmspaceFrame::~nsMathMLmspaceFrame()
+{
+}
+
+NS_IMETHODIMP
+nsMathMLmspaceFrame::Init(nsIPresContext*  aPresContext,
+                          nsIContent*      aContent,
+                          nsIFrame*        aParent,
+                          nsIStyleContext* aContext,
+                          nsIFrame*        aPrevInFlow)
+{
+  nsresult rv = nsMathMLContainerFrame::Init(aPresContext, aContent, aParent, aContext, aPrevInFlow);
+
+//  mEmbellishData.flags = NS_MATHML_SPACELIKE;
+
+  /*
+  parse the attributes
+
+  width  = number h-unit 
+  height = number v-unit 
+  depth  = number v-unit 
+  */
+
+  // XXX Need to check what will happen in case of style changes
+  // These may have to be moved in Reflow() to capture style changes
+
+  nsAutoString value;
+  nsCSSValue cssValue;
+
+  // width 
+  mWidth = 0;
+  if (NS_CONTENT_ATTR_HAS_VALUE == GetAttribute(mContent, mPresentationData.mstyle,
+                   nsMathMLAtoms::width_, value)) {
+    if (ParseNumericValue(value, cssValue) && cssValue.IsLengthUnit()) {
+      mWidth = CalcLength(aPresContext, mStyleContext, cssValue);
+    }
+  }
+
+  // height
+  mHeight = 0;
+  if (NS_CONTENT_ATTR_HAS_VALUE == GetAttribute(mContent, mPresentationData.mstyle,
+                   nsMathMLAtoms::height_, value)) {
+    if (ParseNumericValue(value, cssValue) && cssValue.IsLengthUnit()) {
+      mHeight = CalcLength(aPresContext, mStyleContext, cssValue);
+    }
+  }
+
+  // depth
+  mDepth = 0;
+  if (NS_CONTENT_ATTR_HAS_VALUE == GetAttribute(mContent, mPresentationData.mstyle,
+                   nsMathMLAtoms::depth_, value)) {
+    if (ParseNumericValue(value, cssValue) && cssValue.IsLengthUnit()) {
+      mHeight = CalcLength(aPresContext, mStyleContext, cssValue);
+    }
+  }
+
+  return rv;
+}
+
+NS_IMETHODIMP
+nsMathMLmspaceFrame::Reflow(nsIPresContext*          aPresContext,
+                            nsHTMLReflowMetrics&     aDesiredSize,
+                            const nsHTMLReflowState& aReflowState,
+                            nsReflowStatus&          aStatus)
+{ 
+  mBoundingMetrics.Clear();
+  mBoundingMetrics.width = mWidth;
+  mBoundingMetrics.ascent = mHeight;
+  mBoundingMetrics.descent = mDepth;
+
+  aDesiredSize.ascent = mHeight;
+  aDesiredSize.descent = mDepth;
+  aDesiredSize.width = mWidth;
+  aDesiredSize.height = aDesiredSize.ascent + aDesiredSize.descent;
+  if (nsnull != aDesiredSize.maxElementSize) {
+    aDesiredSize.maxElementSize->width = aDesiredSize.width;
+    aDesiredSize.maxElementSize->height = aDesiredSize.height;
+  }
+  // Also return our bounding metrics
+  aDesiredSize.mBoundingMetrics = mBoundingMetrics;
+
+  aStatus = NS_FRAME_COMPLETE;
+  return NS_OK;
+}
---
+++ layout/mathml/base/src/nsMathMLmspaceFrame.h
@@ -0,0 +1,61 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * The Original Code is Mozilla MathML Project.
+ * 
+ * The Initial Developer of the Original Code is The University Of 
+ * Queensland.  Portions created by The University Of Queensland are
+ * Copyright (C) 1999 The University Of Queensland.  All Rights Reserved.
+ * 
+ * Contributor(s): 
+ *   Roger B. Sidje <rbs@maths.uq.edu.au>
+ */
+
+#ifndef nsMathMLmspaceFrame_h___
+#define nsMathMLmspaceFrame_h___
+
+#include "nsCOMPtr.h"
+#include "nsMathMLContainerFrame.h"
+
+//
+// <mspace> -- space
+//
+
+class nsMathMLmspaceFrame : public nsMathMLContainerFrame {
+public:
+  friend nsresult NS_NewMathMLmspaceFrame(nsIPresShell* aPresShell, nsIFrame** aNewFrame);
+  
+  NS_IMETHOD
+  Init(nsIPresContext*  aPresContext,
+       nsIContent*      aContent,
+       nsIFrame*        aParent,
+       nsIStyleContext* aContext,
+       nsIFrame*        aPrevInFlow);
+
+  NS_IMETHOD
+  Reflow(nsIPresContext*          aPresContext,
+         nsHTMLReflowMetrics&     aDesiredSize,
+         const nsHTMLReflowState& aReflowState,
+         nsReflowStatus&          aStatus);
+  
+protected:
+  nsMathMLmspaceFrame();
+  virtual ~nsMathMLmspaceFrame();
+  
+  virtual PRIntn GetSkipSides() const { return 0; }
+
+private:
+  nscoord mWidth;
+  nscoord mHeight;
+  nscoord mDepth;
+};
+
+#endif /* nsMathMLmspaceFrame_h___ */
