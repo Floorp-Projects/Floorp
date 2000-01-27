ea07dae499a6ec676d81998c1bd6fa0fc5e9c2bd
shaver@netscape.com <shaver@netscape.com>
2000-01-27 09:40:17 +0000

    system information service (not yet part of build)

---
+++ xpcom/base/nsSystemInfo.cpp
@@ -0,0 +1,78 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 2000 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ *   Mike Shaver <shaver@mozilla.org>
+ */
+
+#include "prsystem.h"
+#include "nsSystemInfo.h"
+#include "nsAllocator.h"
+
+nsSystemInfo::nsSystemInfo()
+{
+    NS_INIT_REFCNT();
+}
+
+nsSystemInfo::~nsSystemInfo()
+{
+}
+
+NS_IMPL_ISUPPORTS1(nsSystemInfo, nsISystemInfo)
+
+#define SYSINFO_GETTER(name, cmd)                               \
+NS_IMETHODIMP                                                   \
+nsSystemInfo::Get##name(char **_retval)                         \
+{                                                               \
+    NS_ENSURE_ARG_POINTER(_retval);                             \
+    char *buf = (char *)nsAllocator::Alloc(256);                \
+    if (!buf)                                                   \
+	return NS_ERROR_OUT_OF_MEMORY;                          \
+    if (PR_GetSystemInfo((cmd), buf, 256) == PR_FAILURE) {      \
+	nsAllocator::Free(buf);                                 \
+	return NS_ERROR_FAILURE;                                \
+    }                                                           \
+    *_retval = buf;                                             \
+    return NS_OK;                                               \
+}
+
+SYSINFO_GETTER(Hostname, PR_SI_HOSTNAME)
+SYSINFO_GETTER(OSName, PR_SI_SYSNAME)
+SYSINFO_GETTER(OSVersion, PR_SI_RELEASE)
+SYSINFO_GETTER(Architecture, PR_SI_ARCHITECTURE)
+
+NS_METHOD
+nsSystemInfo::Create(nsISupports *outer, const nsIID &aIID, void **aInstancePtr)
+{
+    nsresult rv;
+    if (outer)
+        return NS_ERROR_NO_AGGREGATION;
+
+    nsSystemInfo *it = new nsSystemInfo();
+    if (!it)
+        return NS_ERROR_OUT_OF_MEMORY;
+
+    rv = it->QueryInterface(aIID, aInstancePtr);
+    if (NS_FAILED(rv)) {
+        delete it;
+        *aInstancePtr = 0;
+    }
+
+    return rv;
+}
---
+++ xpcom/base/nsSystemInfo.h
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 2000 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ *   Mike Shaver <shaver@mozilla.org>
+ */
+
+#include "nsISystemInfo.h"
+
+#define NS_SYSTEMINFO_CID_STR "3c443856-1dd2-11b2-bfa1-83125c3f3887"
+
+#define NS_SYSTEMINFO_CID \
+  {0x3c443856, 0x1dd2, 0x11b2, \
+    { 0xbf, 0xa1, 0x83, 0x12, 0x5c, 0x3f, 0x38, 0x87 }}
+
+#define NS_SYSTEMINFO_CLASSNAME "System Info Service"
+#define NS_SYSTEMINFO_PROGID    "mozilla.sysinfo.1"
+
+class nsSystemInfo : public nsISystemInfo
+{
+  NS_DECL_ISUPPORTS
+public:
+  NS_DECL_NSISYSTEMINFO
+
+  nsSystemInfo();
+  virtual ~nsSystemInfo();
+  static NS_METHOD Create(nsISupports *outer, const nsIID& aIID,
+			  void **aInstancePtr);
+};
+
