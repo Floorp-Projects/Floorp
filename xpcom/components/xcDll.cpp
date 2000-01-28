0c427aebb75497ba348077e27a306db298a0b080
dp%netscape.com <dp%netscape.com>
2000-01-28 11:04:51 +0000

    Using asm() only for linux.

--- xpcom/components/xcDll.cpp
+++ xpcom/components/xcDll.cpp
@@ -484,7 +484,7 @@ void nsDll::BreakAfterLoad(const char *nsprPath)
             // Loading a dll that we want to break on
             // Put your breakpoint here
             printf("...Loading module %s\n", nsprPath);
-#ifdef XP_UNIX
+#ifdef __linux
             // Break in the debugger here.
             asm("int $3");
 #endif
