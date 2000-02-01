620faed34831f3dff363eeca59a3f0e9b065fc30
dp%netscape.com <dp%netscape.com>
2000-02-01 08:03:42 +0000

    Fixing build breakage.

--- extensions/wallet/src/nsBasicStreamGenerator.cpp
+++ extensions/wallet/src/nsBasicStreamGenerator.cpp
@@ -69,7 +69,7 @@ NS_IMETHODIMP nsBasicStreamGenerator::GetByte(PRUint32 offset, PRUint8 *retval)
 {
     NS_ENSURE_ARG_POINTER(retval);
     nsresult rv = NS_OK;
-    if (!((const char *)mPassword))
+    if (mPassword.Length() == 0)
     {
         // First time we need the password. Get it.
         if (!mPasswordSink) return NS_ERROR_FAILURE;
