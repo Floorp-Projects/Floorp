349e796f16787a23e19571929e13ece6de4da4ce
bryner%uiuc.edu <bryner%uiuc.edu>
2000-02-02 22:48:55 +0000

    Adding finger protocol.  To use it, enter a URL such as "finger:user@host".  This is not hooked up to the build yet. r=valeski.

---
+++ extensions/finger/nsFingerHandler.h
@@ -0,0 +1,52 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ */
+
+// The finger protocol handler creates "finger" URIs of the form
+// "finger:user@host" or "finger:host".
+
+#ifndef nsFingerHandler_h___
+#define nsFingerHandler_h___
+
+#include "nsIProtocolHandler.h"
+
+#define FINGER_PORT 79
+
+// {0x76d6d5d8-1dd2-11b2-b361-850ddf15ef07}
+#define NS_FINGERHANDLER_CID     \
+{ 0x76d6d5d8, 0x1dd2, 0x11b2, \
+   {0xb3, 0x61, 0x85, 0x0d, 0xdf, 0x15, 0xef, 0x07} }
+
+class nsFingerHandler : public nsIProtocolHandler
+{
+public:
+    NS_DECL_ISUPPORTS
+    NS_DECL_NSIPROTOCOLHANDLER
+
+    // nsFingerHandler methods:
+    nsFingerHandler();
+    virtual ~nsFingerHandler();
+
+    // Define a Create method to be used with a factory:
+    static NS_METHOD Create(nsISupports* aOuter, const nsIID& aIID, void* *aResult);
+};
+
+#endif /* nsFingerHandler_h___ */
---
+++ netwerk/protocol/finger/src/nsFingerHandler.cpp
@@ -0,0 +1,127 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ */
+
+#include "nspr.h"
+#include "nsFingerChannel.h"
+#include "nsFingerHandler.h"
+#include "nsIURL.h"
+#include "nsCRT.h"
+#include "nsIComponentManager.h"
+#include "nsIServiceManager.h"
+#include "nsIInterfaceRequestor.h"
+#include "nsIProgressEventSink.h"
+
+// Using StandardURL instead of SimpleURI so that we can get the
+// username and port parsed for us.  We ignore the path.
+
+static NS_DEFINE_CID(kSimpleURICID, NS_SIMPLEURI_CID);
+
+////////////////////////////////////////////////////////////////////////////////
+
+nsFingerHandler::nsFingerHandler() {
+    NS_INIT_REFCNT();
+}
+
+nsFingerHandler::~nsFingerHandler() {
+}
+
+NS_IMPL_ISUPPORTS(nsFingerHandler, NS_GET_IID(nsIProtocolHandler));
+
+NS_METHOD
+nsFingerHandler::Create(nsISupports* aOuter, const nsIID& aIID, void* *aResult) {
+
+    nsFingerHandler* ph = new nsFingerHandler();
+    if (ph == nsnull)
+        return NS_ERROR_OUT_OF_MEMORY;
+    NS_ADDREF(ph);
+    nsresult rv = ph->QueryInterface(aIID, aResult);
+    NS_RELEASE(ph);
+    return rv;
+}
+    
+////////////////////////////////////////////////////////////////////////////////
+// nsIProtocolHandler methods:
+
+NS_IMETHODIMP
+nsFingerHandler::GetScheme(char* *result) {
+    *result = nsCRT::strdup("finger");
+    if (!*result) return NS_ERROR_OUT_OF_MEMORY;
+    return NS_OK;
+}
+
+NS_IMETHODIMP
+nsFingerHandler::GetDefaultPort(PRInt32 *result) {
+    *result = FINGER_PORT;
+    return NS_OK;
+}
+
+NS_IMETHODIMP
+nsFingerHandler::NewURI(const char *aSpec, nsIURI *aBaseURI,
+                             nsIURI **result) {
+    nsresult rv;
+
+    // no concept of a relative finger url
+    NS_ASSERTION(!aBaseURI, "base url passed into finger protocol handler");
+
+    nsIURI* url;
+    rv = nsComponentManager::CreateInstance(kSimpleURICID, nsnull,
+                                            NS_GET_IID(nsIURI),
+                                            (void**)&url);
+    if (NS_FAILED(rv)) return rv;
+    rv = url->SetSpec((char*)aSpec);
+    if (NS_FAILED(rv)) {
+        NS_RELEASE(url);
+        return rv;
+    }
+
+    *result = url;
+    return rv;
+}
+
+NS_IMETHODIMP
+nsFingerHandler::NewChannel(const char* verb, nsIURI* url,
+                          nsILoadGroup* aLoadGroup,
+                          nsIInterfaceRequestor* notificationCallbacks,
+                          nsLoadFlags loadAttributes,
+                          nsIURI* originalURI,
+                          PRUint32 bufferSegmentSize,
+                          PRUint32 bufferMaxSize,
+                          nsIChannel* *result)
+{
+    nsresult rv;
+    
+    nsFingerChannel* channel;
+    rv = nsFingerChannel::Create(nsnull, NS_GET_IID(nsIChannel), (void**)&channel);
+    if (NS_FAILED(rv)) return rv;
+
+    rv = channel->Init(verb, url, aLoadGroup, notificationCallbacks,
+                       loadAttributes, originalURI, bufferSegmentSize, bufferMaxSize);
+    if (NS_FAILED(rv)) {
+        NS_RELEASE(channel);
+        return rv;
+    }
+
+    *result = channel;
+    return NS_OK;
+}
+
+////////////////////////////////////////////////////////////////////////////////
