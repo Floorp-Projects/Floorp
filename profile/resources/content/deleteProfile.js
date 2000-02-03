8ed4b97cd155a5b3b6b64139193f53105b437ffa
sfraser@netscape.com <sfraser@netscape.com>
2000-02-03 23:04:50 +0000

    Workaround for bug 26265, to handle failure to load string bundle at startup. r=ben, a=leaf

--- profile/resources/content/deleteProfile.js
+++ profile/resources/content/deleteProfile.js
@@ -6,12 +6,20 @@ function Startup()
   var okButton = document.getElementById("ok");
   var Button2 = document.getElementById("Button2");
   var cancelButton = document.getElementById("cancel");
-  okButton.setAttribute( "value", bundle.GetStringFromName("dontDeleteFiles") );
+  
+  try {
+    okButton.setAttribute( "value", bundle.GetStringFromName("dontDeleteFiles") );
+    Button2.setAttribute( "value", bundle.GetStringFromName("deleteFiles") );
+    cancelButton.setAttribute( "value", bundle.GetStringFromName("cancel") );
+  } catch(e) {
+    okButton.setAttribute( "value", "Don't Delete Files Yah" );
+    Button2.setAttribute( "value", "Delete Files Yah" );
+    cancelButton.setAttribute( "value", "Cancel Yah" );
+  }
+
   okButton.setAttribute( "class", ( okButton.getAttribute("class") + " padded" ) );
-  Button2.setAttribute( "value", bundle.GetStringFromName("deleteFiles") );
   Button2.setAttribute( "class", ( okButton.getAttribute("class") + " padded" ) );
   Button2.setAttribute( "style", "display: inherit;" );
-  cancelButton.setAttribute( "value", bundle.GetStringFromName("cancel") );
   cancelButton.setAttribute( "class", ( cancelButton.getAttribute("class") + " padded" ) );
 }
 
