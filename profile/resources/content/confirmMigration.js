f2c023e1c4ebe889161882255306fe194f18f101
ben@netscape.com <ben@netscape.com>
2000-02-05 03:32:13 +0000

    cleaned up confirm migration dialog r=jst@citec.fi

--- profile/resources/content/confirmMigration.js
+++ profile/resources/content/confirmMigration.js
@@ -1,3 +1,4 @@
+var bundle;
 var profile = Components.classes["component://netscape/profile/manager"].getService();
 profile = profile.QueryInterface(Components.interfaces.nsIProfile);
 
@@ -14,5 +15,16 @@ function handleCancelButton()
 
 function onLoad()
 {
+  bundle = srGetStrBundle("chrome://profile/locale/migration.properties");
 	doSetOKCancel(handleOKButton, handleCancelButton);
+  var okButton = document.getElementById("ok");
+  var cancelButton = document.getElementById("cancel");
+  if( !okButton || !cancelButton )
+    return false;
+  okButton.setAttribute( "value", bundle.GetStringFromName( "migrate" ) );
+  okButton.setAttribute( "class", ( okButton.getAttribute( "class" ) + " padded" ) );
+  cancelButton.setAttribute( "value", bundle.GetStringFromName( "newprofile" ) );
+  cancelButton.setAttribute( "class", ( cancelButton.getAttribute( "class" ) + " padded" ) );
+  centerWindowOnScreen();
 }
+
