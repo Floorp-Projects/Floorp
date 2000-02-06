c6c8dddea7c92e702df8c9d5ec14f0b0ff1e4b6d
sspitzer%netscape.com <sspitzer%netscape.com>
2000-02-06 01:12:47 +0000

    fix warnings, make it build on linux.

--- xpcom/components/nsCategoryManager.cpp
+++ xpcom/components/nsCategoryManager.cpp
@@ -129,7 +129,7 @@ class nsCategoryManager
   {
     public:
       nsCategoryManager();
-      ~nsCategoryManager();
+      virtual ~nsCategoryManager();
 
       NS_DECL_ISUPPORTS
       NS_DECL_NSICATEGORYMANAGER
