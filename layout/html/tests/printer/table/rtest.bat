61563b5c2013c6c22a0211615f10ad5e525b6011
dcone@netscape.com <dcone@netscape.com>
2000-02-11 14:40:32 +0000

    Added printer regression test files and directories

---
+++ layout/html/tests/printer/table/rtest.bat
@@ -0,0 +1,16 @@
+@echo off
+if %1==baseline goto baseline
+
+:verify
+if not exist verify mkdir verify
+s:\mozilla\dist\win32_d.obj\bin\viewer -Prt 1 -o s:\mozilla\layout\html\tests\printer\table\verify\ -rd s:\mozilla\layout\html\tests\printer\table -f s:\mozilla\layout\html\tests\printer\images\file_list.txt
+goto done
+
+:baseline
+s:\mozilla\dist\win32_d.obj\bin\viewer -Prt 1 -o s:\mozilla\layout\html\tests\printer\table\ -f s:\mozilla\layout\html\tests\printer\table\file_list.txt
+goto done
+
+:error
+echo syntax: rtest (baseline verify) 
+
+:done
