b603af808b847b630cae72acc5bb38e3872e53a1
alecf%netscape.com <alecf%netscape.com>
2000-02-15 01:17:45 +0000

    add ispUtils.js and accountUtils.js to the build (not part of the product yet)

---
+++ mailnews/base/prefs/resources/content/accountUtils.js
@@ -0,0 +1,47 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ * Alec Flett <alecf@netscape.com>
+ */
+
+// returns the first account with an invalid server or identity
+
+function getFirstInvalidAccount(accounts)
+{
+    var numAccounts = accounts.Count();
+    for (var i=0; i<numAccounts; i++) {
+        var account = accounts.QueryElementAt(i, Components.interfaces.nsIMsgAccount);
+        if (!account.incomingServer.valid)
+            return account;
+
+        var identities = account.identities;
+        var numIdentities = identities.Count();
+
+        for (var j=0; j<numIdentities; j++) {
+            var identity = identities.QueryElementAt(j, Components.interfaces.nsIMsgIdentity);
+            if (!identity.valid)
+                return account
+        }
+    }
+
+    // none found
+    return null;
+}
+
