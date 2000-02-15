27c07b4b326002f044c9aab4ee3dd3e20e7b4660
alecf%netscape.com <alecf%netscape.com>
2000-02-15 01:14:23 +0000

    move the choose file/set as current to a global spot so other pref panes can get to them, for #27630, r=sspitzer

--- xpfe/components/prefwindow/resources/content/pref-navigator.js
+++ xpfe/components/prefwindow/resources/content/pref-navigator.js
@@ -1,19 +1,2 @@
-var bundle = srGetStrBundle("chrome://pref/locale/prefutilities.properties");
 
-function prefNavSelectFile()
-{
-  var folderField = document.getElementById("browserStartupHomepage");
-  var url = getFileOrFolderURL( bundle.GetStringFromName("choosehomepage"), false );
-  if( url != -1 )
-    folderField.value = url;
-}
 
-function setHomePageToCurrentPage()
-{
-  if( !parent.opener.appCore )
-    return false;
-  var homePageField = document.getElementById("browserStartupHomepage");
-  var url = parent.opener.content.location.href;
-  if( url )
-    homePageField.value = url;
-}
