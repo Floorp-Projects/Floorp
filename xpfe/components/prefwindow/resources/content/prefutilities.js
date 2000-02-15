1d12dd9fcbf1a892bafc7d5406bbfb702872547f
alecf@netscape.com <alecf@netscape.com>
2000-02-15 01:14:23 +0000

    move the choose file/set as current to a global spot so other pref panes can get to them, for #27630, r=sspitzer

--- xpfe/components/prefwindow/resources/content/prefutilities.js
+++ xpfe/components/prefwindow/resources/content/prefutilities.js
@@ -1,3 +1,4 @@
+var bundle = srGetStrBundle("chrome://pref/locale/prefutilities.properties");
 
 function getFileOrFolderURL( aTitle, aFolder )
 {
@@ -15,14 +16,35 @@ function getFileOrFolderURL( aTitle, aFolder )
     return false;
   }
   try {
-    if( aFolder )
-      fileSpecWithUI.path = fileSpecWithUI.chooseDirectory( aTitle );
+    var value;
+    if( aFolder ) 
+      value = fileSpecWithUI.chooseDirectory( aTitle );
     else 
-      fileSpecWithUI.path = fileSpecWithUI.chooseFile( aTitle );
+      value = fileSpecWithUI.chooseFile( aTitle );
+    dump("filespecWithUI.path = " + value + "\n");
+    fileSpecWithUI.URLString = value;
   }
   catch(e) {
+    dump("Error: " + e + "\n");
     return -1;
   }
   return fileSpecWithUI.nativePath;
 }
 
+function prefNavSelectFile(folderFieldId, stringId)
+{
+  var folderField = document.getElementById(folderFieldId);
+  var url = getFileOrFolderURL( bundle.GetStringFromName(stringId), false );
+  if( url != -1 )
+    folderField.value = url;
+}
+
+function setHomePageToCurrentPage(folderFieldId)
+{
+  if( !parent.opener.appCore )
+    return false;
+  var homePageField = document.getElementById(folderFieldId);
+  var url = parent.opener.content.location.href;
+  if( url )
+    homePageField.value = url;
+}
