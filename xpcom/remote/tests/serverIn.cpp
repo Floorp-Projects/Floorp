66e690d999d5971fb5b96ae10158b6cc4d46e2da
idk@eng.sun.com <idk@eng.sun.com>
2000-02-15 21:34:28 +0000

    Updated remote-xpcom tests. a=leilag@eng.sun.com r=idk@eng.sun.com

--- xpcom/remote/tests/serverIn.cpp
+++ xpcom/remote/tests/serverIn.cpp
@@ -1,31 +1,16 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * The contents of this file are subject to the Mozilla Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
- *
- * The Original Code is mozilla.org code.
- *
- * The Initial Developer of the Original Code is Sun Microsystems,
- * Inc. Portions created by Sun are
- * Copyright (C) 1999 Sun Microsystems, Inc. All
- * Rights Reserved.
- *
- * Contributor(s): 
- */
 #include <stdio.h>
+#include <stream.h>
+#include <fstream.h>
+#include <stdlib.h>
 #include "RPCServerService.h"
 #include "nsISupports.h"
 #include "nsIJVMManager.h"
 #include "nsIRPCTestIn.h"
-#include <unistd.h>
 #include "IDispatcher.h"
 #include "nsIThread.h"
+#include "deftest.h"
+#include "proto.h"
+
 
 class nsRPCTestInImpl : public  nsIRPCTestIn {
     NS_DECL_ISUPPORTS 
@@ -33,127 +18,287 @@ class nsRPCTestInImpl : public  nsIRPCTestIn {
 	NS_INIT_REFCNT();
     }
 
-    NS_IMETHOD Test1(PRBool bool) {
-        printf("Test1 this=%p\n", this);
+    NS_IMETHOD TestIn1(PRBool bool) {
+        printf("TestIn1 this=%p\n", this);
 	printf("--nsRPCTestInImpl::Test1 %d\n",bool);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn1 %d\n",bool);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test2(PRUint8 octet) {
-        printf("Test2 this=%p\n", this);
+    NS_IMETHOD TestIn2(PRUint8 octet) {
+        printf("TestIn2 this=%p\n", this);
 	printf("--nsRPCTestInImpl::Test2 %d\n",octet);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn2 %o\n",octet);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test3(PRInt16 sInt) {
-        printf("Test3 this=%p\n", this);
+    NS_IMETHOD TestIn3(PRInt16 sInt) {
+        printf("TestIn3 this=%p\n", this);
 	printf("--nsRPCTestInImpl::Test3 %d\n",sInt);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn3 %d\n",sInt);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test4(PRInt32 lInt) {
-        printf("Test4 this=%p\n", this);
+    NS_IMETHOD TestIn4(PRInt32 lInt) {
+        printf("TestIn4 this=%p\n", this);
 	printf("--nsRPCTestInImpl::Test4 %d\n",lInt);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn4 %d\n",lInt);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test5(PRInt64 llInt) {
-        printf("Test5 this=%p\n", this);
+    NS_IMETHOD TestIn5(PRInt64 llInt) {
+        printf("TestIn5 this=%p\n", this);
 	printf("--nsRPCTestInImpl::Test5 %d\n",llInt);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn5 %d\n",llInt);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test6(PRUint16 usInt) {
-        printf("Test6 this=%p\n", this);
-	printf("--nsRPCTestInImpl::Test6 %15u\n",usInt);
+    NS_IMETHOD TestIn6(PRUint16 usInt) {
+        printf("TestIn6 this=%p\n", this);
+	printf("--nsRPCTestInImpl::Test6 %u\n",usInt);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn6 %u\n",usInt);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test7(PRUint32 ulInt) {
-        printf("Test7 this=%p\n", this);
-	printf("--nsRPCTestInImpl::Test7 %15u\n",ulInt);
+    NS_IMETHOD TestIn7(PRUint32 ulInt) {
+        printf("TestIn7 this=%p\n", this);
+	printf("--nsRPCTestInImpl::Test7 %u\n",ulInt);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn7 %u\n",ulInt);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test8(PRUint64 ullInt) {
-        printf("Test8 this=%p\n", this);
-	printf("--nsRPCTestInImpl::Test8 %15u\n",ullInt);
+    NS_IMETHOD TestIn8(PRUint64 ullInt) {
+        printf("TestIn8 this=%p\n", this);
+	printf("--nsRPCTestInImpl::Test8 %u\n",ullInt);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn8 %u\n",ullInt);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test9(float f) {
-        printf("Test9 this=%p\n", this);
+    NS_IMETHOD TestIn9(float f) {
+        printf("TestIn9 this=%p\n", this);
 	printf("--nsRPCTestInImpl::Test9 %.50f\n",f);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn9 %.50f\n",f);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test10(double d) {
-        printf("Test10 this=%p\n", this);
+    NS_IMETHOD TestIn10(double d) {
+        printf("TestIn10 this=%p\n", this);
 	printf("--nsRPCTestInImpl::Test10 %.50f\n",d);
+        char *tmpstr = new char[500];
+        sprintf(tmpstr,"TestIn10 %.50f\n",d);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test11(char c) {
-        printf("Test11 this=%p\n", this);
+    NS_IMETHOD TestIn11(char c) {
+        printf("TestIn11 this=%p\n", this);
 	printf("--nsRPCTestInImpl::Test11 %c\n",c);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn11 %c\n",c);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test12(PRUnichar unic) {
-        printf("Test12 this=%p\n", this);
+    NS_IMETHOD TestIn12(PRUnichar unic) {
+        printf("TestIn12 this=%p\n", this);
 	printf("--nsRPCTestInImpl::Test12 %c\n",unic);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn12 %c\n",unic);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test13(const char* s) {
-        printf("Test13 this=%p\n", this);
+    NS_IMETHOD TestIn13(const char* s) {
+        printf("TestIn13 this=%p\n", this);
 	printf("--nsRPCTestInImpl::Test13 %s\n",s);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn13 %s\n",s);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-//  NS_IMETHOD Test14(PRUnichar* unis) {
-//      printf("Test14 this=%p\n", this);
-//	printf("--nsRPCTestInImpl::Test14 %s\n",unis);
-//	return NS_OK;
-//    }
-  
+    NS_IMETHOD TestIn14(const PRUnichar* unis) {
+        printf("TestIn14 this=%p\n", this);
+	printf("--nsRPCTestInImpl::Test14 %s\n",unis);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn14 %s\n",unis);
+        writeResult(SERVER_IN_RES, tmpstr);
+        delete tmpstr;
+	return NS_OK;
+    }
+
+    NS_IMETHOD TestIn15(PRUint32 count, const char **valueArray) {
+        printf("--TestIn15\n");
+        printf("count %d\n",count);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr, "TestIn15 count %d\n", count);
+        writeResult(SERVER_IN_RES, tmpstr);
+        for (int i = 0; i < count; i++) {
+            printf("TestIn15 Array[%d]=%s\n",i,valueArray[i]);
+            sprintf(tmpstr,"TestIn15 Array[%d]=%s\n",i,valueArray[i]);
+            writeResult(SERVER_IN_RES, tmpstr);
+        }
+        delete tmpstr;
+        return NS_OK;
+    }
+
+    NS_IMETHOD TestIn16(PRUint32 count, PRInt32 *longArray) {
+        printf("--TestIn16\n");
+        printf("count %d\n",count);
+        char *tmpstr = new char[200];
+        sprintf(tmpstr, "TestIn16 count %d\n", count);
+        writeResult(SERVER_IN_RES, tmpstr);
+        for (int i = 0; i < count; i++) {
+            printf("TestIn16 Array[%d]=%d\n",i,longArray[i]);
+            sprintf(tmpstr,"TestIn16 Array[%d]=%d\n",i,longArray[i]);
+            writeResult(SERVER_IN_RES, tmpstr);
+        }
+        delete tmpstr;
+        return NS_OK;
+    }
+
+    NS_IMETHOD TestIn17(PRBool bBool, char cChar, PRUint8 nByte, 
+                        PRInt16 nShort, PRUint16 nUShort, 
+                        PRInt32 nLong,PRUint32 nULong, 
+                        PRInt64 nHyper, PRUint64 nUHyper, 
+                        float fFloat, double fDouble, const char *aString, 
+                        PRUint32 count, PRInt32 *longArray) 
+    {
+
+        printf("TestIn17 this=%p\n", this) ;
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestIn17 PRBool %d\n",bBool);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 PRUint8 %o\n",nByte);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 PRInt16 %d\n",nShort);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 PRInt32 %d\n",nLong);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 PRInt64 %d\n",nHyper);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 PRUint16 %u\n",nUShort);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 PRUint32 %u\n",nULong);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 PRUint64 %u\n",nUHyper);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 float %.50f\n",fFloat);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 double %.50f\n",fDouble);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 char %c\n",cChar);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 string %s\n",aString);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn17 count %d\n", count);
+        writeResult(SERVER_IN_RES, tmpstr);
+        for (int i = 0; i < count; i++) {
+             printf("TestIn17 Array[%d]=%d\n",i,longArray[i]);
+             sprintf(tmpstr,"TestIn17 Array[%d]=%d\n",i,longArray[i]);
+             writeResult(SERVER_IN_RES, tmpstr);
+        }
+
+        delete tmpstr;
+        return NS_OK;
+        
+    }
+
+    NS_IMETHOD TestIn18(char cChar, PRUint8 nByte, 
+                        PRInt16 nShort, PRUint16 nUShort, 
+                        PRInt32 nLong,PRUint32 nULong, 
+                        PRInt64 nHyper, PRUint64 nUHyper, 
+                        float fFloat, double fDouble, const char *aString, 
+                        PRUint32 count, PRInt32 *longArray) 
+    {
+        printf("TestIn18 this=%p\n", this);
+        char *tmpstr = new char[200];
+
+        sprintf(tmpstr,"TestIn18 PRUint8 %o\n",nByte);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 PRInt16 %d\n",nShort);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 PRInt32 %d\n",nLong);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 PRInt64 %d\n",nHyper);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 PRUint16 %u\n",nUShort);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 PRUint32 %u\n",nULong);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 PRUint64 %u\n",nUHyper);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 float %.50f\n",fFloat);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 double %.50f\n",fDouble);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 char %c\n",cChar);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 string %s\n",aString);
+        writeResult(SERVER_IN_RES, tmpstr);
+        sprintf(tmpstr,"TestIn18 count %d\n", count);
+        writeResult(SERVER_IN_RES, tmpstr);
+        for (int i = 0; i < count; i++) {
+             printf("TestIn18 Array[%d]=%d\n",i,longArray[i]);
+             sprintf(tmpstr,"TestIn18 Array[%d]=%d\n",i,longArray[i]);
+             writeResult(SERVER_IN_RES, tmpstr);
+        }
+
+        delete tmpstr;
+        return NS_OK;
+        
+    }
+
 };
 
 NS_IMPL_ISUPPORTS(nsRPCTestInImpl, NS_GET_IID(nsIRPCTestIn));
 int main(int argc, char **args) {
-    printf("%x\n",getpid());
-    nsRPCTestInImpl * test1 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test2 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test3 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test4 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test5 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test6 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test7 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test8 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test9 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test10 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test11 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test12 = new nsRPCTestInImpl();
-    nsRPCTestInImpl * test13 = new nsRPCTestInImpl();
+   int i;
+    const short num = 2;
+    nsRPCTestInImpl * test[num];
+
+    setProcessId(IN_FDATA);
+    for(i = 0; i < num; i++) {
+       test[i] = new nsRPCTestInImpl();
+    }
     RPCServerService * rpcService = RPCServerService::GetInstance();
     IDispatcher *dispatcher;
     rpcService->GetDispatcher(&dispatcher);
-    dispatcher->RegisterWithOID(test1, 1);
-    dispatcher->RegisterWithOID(test2, 5);
-    dispatcher->RegisterWithOID(test3, 7);
-    dispatcher->RegisterWithOID(test4, 9);
-    dispatcher->RegisterWithOID(test5, 11);
-    dispatcher->RegisterWithOID(test6, 13);
-    dispatcher->RegisterWithOID(test7, 15);
-    dispatcher->RegisterWithOID(test8, 17);
-    dispatcher->RegisterWithOID(test9, 19);
-    dispatcher->RegisterWithOID(test10, 21);
-    dispatcher->RegisterWithOID(test11, 23);
-    dispatcher->RegisterWithOID(test12, 25);
-    dispatcher->RegisterWithOID(test13, 27);
+    for(i = 0; i < num; i++) {
+       dispatcher->RegisterWithOID(test[i], i + 1);
+    }
     while(1) {
 	PR_Sleep(100);
     }
-    
 }
-
--- xpcom/remote/tests/serverOut.cpp
+++ xpcom/remote/tests/serverOut.cpp
@@ -1,173 +1,274 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * The contents of this file are subject to the Mozilla Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
- *
- * The Original Code is mozilla.org code.
- *
- * The Initial Developer of the Original Code is Sun Microsystems,
- * Inc. Portions created by Sun are
- * Copyright (C) 1999 Sun Microsystems, Inc. All
- * Rights Reserved.
- *
- * Contributor(s): 
- */
 #include <stdio.h>
 #include <stream.h>
+#include <fstream.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
 #include "RPCServerService.h"
 #include "nsISupports.h"
 #include "nsIJVMManager.h"
 #include "nsIRPCTestOut.h"
-#include <unistd.h>
 #include "IDispatcher.h"
 #include "nsIThread.h"
+#include "deftest.h"
+#include "proto.h"
 
-class nsRPCTestOutImpl : public  nsIRPCTestOut {
+class nsRPCTestOutImpl  : public  nsIRPCTestOut {
     NS_DECL_ISUPPORTS 
-    nsRPCTestOutImpl() {
+    nsRPCTestOutImpl () {
 	NS_INIT_REFCNT();
     }
 
-    NS_IMETHOD Test1(PRBool *bool) {
-        printf("Test1 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test1 %d\n",*bool);
+    NS_IMETHOD TestOut1(PRBool *bool) {
+        printf("TestOut1 this=%p\n", this);
         *bool = PR_FALSE;
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut1 %d\n",*bool);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test2(PRUint8 *octet) {
-        printf("Test2 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test2 %d\n",*octet);
+    NS_IMETHOD TestOut2(PRUint8 *octet) {
+        printf("TestOut2 this=%p\n", this);
         *octet = 88; 
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut2 %o\n",*octet);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test3(PRInt16 *sInt) {
-        printf("Test3 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test3 %d\n",*sInt);
-        *sInt = 9999;
+    NS_IMETHOD TestOut3(PRInt16 *sInt) {
+        printf("TestOut3 this=%p\n", this);
+        *sInt = SHRT_MAX;
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut3 %d\n",*sInt);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test4(PRInt32 *lInt) {
-        printf("Test4 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test4 %d\n",*lInt);
-        *lInt = 99999;
+    NS_IMETHOD TestOut4(PRInt32 *lInt) {
+        printf("TestOut4 this=%p\n", this);
+        *lInt = LONG_MAX;
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut4 %d\n",*lInt);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test5(PRInt64 *llInt) {
-        printf("Test5 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test5 %d\n",*llInt);
-        *llInt = 999999;
+    NS_IMETHOD TestOut5(PRInt64 *llInt) {
+        printf("TestOut5 this=%p\n", this);
+        *llInt = LONG_MAX;
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut5 %d\n",*llInt);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test6(PRUint16 *usInt) {
-        printf("Test6 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test6 %15u\n",*usInt);
-        *usInt = 9999;
+    NS_IMETHOD TestOut6(PRUint16 *usInt) {
+        printf("TestOut6 this=%p\n", this);
+        *usInt = USHRT_MAX;
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut6 %u\n",*usInt);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test7(PRUint32 *ulInt) {
-        printf("Test7 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test7 %15u\n",*ulInt);
-        *ulInt = 99999;
+    NS_IMETHOD TestOut7(PRUint32 *ulInt) {
+        printf("TestOut7 this=%p\n", this);
+        *ulInt = ULONG_MAX;
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut7 %u\n",*ulInt);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test8(PRUint64 *ullInt) {
-        printf("Test8 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test8 %15u\n",*ullInt);
-        *ullInt = 9999999;
+    NS_IMETHOD TestOut8(PRUint64 *ullInt) {
+        printf("TestOut8 this=%p\n", this);
+        *ullInt = ULONG_MAX;
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut8 %u\n",*ullInt);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test9(float *f) {
-        printf("Test9 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test9 %.50f\n",*f);
-        *f = 999.999;
+    NS_IMETHOD TestOut9(float *f) {
+        printf("TestOut9 this=%p\n", this);
+        *f = FLT_MAX;
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut9 %.50f\n",*f);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test10(double *d) {
-        printf("Test10 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test10 %.50f\n",*d);
-        *d = 9999999.9999999;
+    NS_IMETHOD TestOut10(double *d) {
+        printf("TestOut10 this=%p\n", this);
+        *d = DBL_MAX;
+        char *tmpstr = new char[500];
+        sprintf(tmpstr,"TestOut10 %.50f\n",*d);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test11(char *c) {
-        printf("Test11 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test11 %c\n",*c);
+    NS_IMETHOD TestOut11(char *c) {
+        printf("TestOut11 this=%p\n", this);
         *c = 'G';
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut11 %c\n",*c);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test12(PRUnichar *unic) {
-        printf("Test12 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test12 %c\n",*unic);
+    NS_IMETHOD TestOut12(PRUnichar *unic) {
+        printf("TestOut12 this=%p\n", this);
         *unic = 'G';
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut12 %c\n",*unic);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-    NS_IMETHOD Test13(char **s) {
-        printf("Test13 this=%p\n", this);
-	printf("--nsRPCTestOutImpl::Test13 %s\n",*s);
+    NS_IMETHOD TestOut13(char **s) {
+        printf("TestOut13 this=%p\n", this);
         *s = "remote ipc";
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut13 %s\n",*s);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
 	return NS_OK;
     }
 
-//  NS_IMETHOD Test14(PRUnichar **unis) {
-//      printf("Test14 this=%p\n", this);
-//	printf("--nsRPCTestOutImpl::Test14 %s\n",*unis);
-//	return NS_OK;
-//    }
+    NS_IMETHOD TestOut14(PRUnichar **unis) {
+        printf("TestOut14 this=%p\n", this);
+        //*unis = "remote ipc";
+        *unis = new PRUnichar[50];
+        for (int i = 0; i < 10; i++) {
+           (*unis)[i] = 'a' + 1;
+        }
+        (*unis)[i] = '\0';
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut14 %s\n",*unis);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
+	return NS_OK;
+    }
+
+    NS_IMETHOD TestOut15(PRUint32 *count, char **valueArray) {
+        printf("--TestOut15\n");
+        *valueArray = "This is a remote ipc test.";
+        *count = strlen(*valueArray);
+        char *tmpstr = new char[200];
+        printf("TestOut15 count %d\n", *count);
+        sprintf(tmpstr, "TestOut15 count %d\n", *count);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        printf("TestOut15 valueArray %s\n", *valueArray);
+        sprintf(tmpstr,"TestOut15 Array=%s\n",*valueArray);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        delete tmpstr;
+        return NS_OK;
+    }
+
+    NS_IMETHOD TestOut16(PRBool *bBool, char *cChar, PRUint8 *nByte,
+                        PRInt16 *nShort, PRUint16 *nUShort,
+                        PRInt32 *nLong,PRUint32 *nULong,
+                        PRInt64 *nHyper, PRUint64 *nUHyper,
+                        float *fFloat, double *fDouble, char **aString)
+    {
+        *bBool = PR_TRUE;
+        *nByte = 777; 
+        *cChar = 'A';
+        *nShort = 1;
+        *nUShort = 2;
+        *nLong = 3; 
+        *nULong = 4;
+        *nHyper = 5;
+        *nUHyper = 6;
+        *fFloat = 7;
+        *fDouble = 8;
+        *aString = "Remote ipc test.";
+
+        printf("TestOut16 this=%p\n", this) ;
+        char *tmpstr = new char[200];
+        sprintf(tmpstr,"TestOut16 PRBool %d\n",*bBool);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 PRUint8 %o\n",*nByte);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 PRInt16 %d\n",*nShort);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 PRInt32 %d\n",*nLong);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 PRInt64 %d\n",*nHyper);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 PRUint16 %u\n",*nUShort);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 PRUint32 %u\n",*nULong);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 PRUint64 %u\n",*nUHyper);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 float %.50f\n",*fFloat);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 double %.50f\n",*fDouble);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 char %c\n",*cChar);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        sprintf(tmpstr,"TestOut16 string %s\n",*aString);
+        writeResult(SERVER_OUT_RES, tmpstr);
+
+        delete tmpstr;
+        return NS_OK;
+    }
+
+    NS_IMETHOD TestOut17(PRUint32 *count, PRInt32 **longArray) {
+        printf("--TestOut17\n");
+        char *tmpstr = new char[200];
+        *count = 5;
+        sprintf(tmpstr, "TestOut17 count %d\n", *count);
+        writeResult(SERVER_OUT_RES, tmpstr);
+        *longArray = new PRInt32[*count];
+        for (int i = 0; i < *count; i++) {
+            (*longArray)[i] = 55555 + i;
+            printf("TestOut17 Array[%d]=%d\n",i,(*longArray)[i]);
+            sprintf(tmpstr,"TestOut17 Array[%d]=%d\n",i,(*longArray)[i]);
+            writeResult(SERVER_OUT_RES, tmpstr);
+        }
+        delete tmpstr;
+        return NS_OK;
+    }
+
+
+
 
 };
 
-NS_IMPL_ISUPPORTS(nsRPCTestOutImpl, NS_GET_IID(nsIRPCTestOut));
+NS_IMPL_ISUPPORTS(nsRPCTestOutImpl , NS_GET_IID(nsIRPCTestOut));
 int main(int argc, char **args) {
-    printf("%x\n",getpid());
-    nsRPCTestOutImpl * test1 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test2 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test3 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test4 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test5 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test6 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test7 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test8 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test9 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test10 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test11 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test12 = new nsRPCTestOutImpl();
-    nsRPCTestOutImpl * test13 = new nsRPCTestOutImpl();
+   int i;
+    const short num = 5;
+    nsRPCTestOutImpl * test[num];
+
+    setProcessId(OUT_FDATA);
+    for(i = 0; i < num; i++) {
+       test[i] = new nsRPCTestOutImpl();
+    }
     RPCServerService * rpcService = RPCServerService::GetInstance();
     IDispatcher *dispatcher;
     rpcService->GetDispatcher(&dispatcher);
-    dispatcher->RegisterWithOID(test1, 1);
-    dispatcher->RegisterWithOID(test2, 5);
-    dispatcher->RegisterWithOID(test3, 7);
-    dispatcher->RegisterWithOID(test4, 9);
-    dispatcher->RegisterWithOID(test5, 11);
-    dispatcher->RegisterWithOID(test6, 13);
-    dispatcher->RegisterWithOID(test7, 15);
-    dispatcher->RegisterWithOID(test8, 17);
-    dispatcher->RegisterWithOID(test9, 19);
-    dispatcher->RegisterWithOID(test10, 21);
-    dispatcher->RegisterWithOID(test11, 23);
-    dispatcher->RegisterWithOID(test12, 25);
-    dispatcher->RegisterWithOID(test13, 27);
-    while(1) {
-	PR_Sleep(100);
+    for(i = 0; i < num; i++) {
+       dispatcher->RegisterWithOID(test[i], i + 1);
     }
-    
+    while(1) {
+        PR_Sleep(100);
+    }
 }
 
