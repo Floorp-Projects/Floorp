31f6ffde6d88ccb1f28bbb8aac72d8b8d30ea64c
davidm%netscape.com <davidm%netscape.com>
2000-02-16 07:52:43 +0000

    20893  Cache preferences need to be hooked up r = ben@netscape.com

--- xpfe/components/prefwindow/resources/content/pref-cache.js
+++ xpfe/components/prefwindow/resources/content/pref-cache.js
@@ -1,9 +1,26 @@
 
 function prefCacheSelectFolder()
 {
+  dump("prefCacheSelectFolder \n");
   bundle = srGetStrBundle("chrome://pref/locale/prefutilities.properties");
   var folderField = document.getElementById("browserCacheDirectory");
   var url = getFileOrFolderURL( bundle.GetStringFromName("cachefolder"), true );
   if( url != -1 )
     folderField.value = url;
+}
+
+function prefClearMemCache()
+{
+	dump("prefClearMemCache \n");
+	var cache = Components.classes['component://netscape/network/cache?name=manager'].getService();
+  var	cacheService = cache.QueryInterface( Components.interfaces.nsINetDataCacheManager);
+	cacheService.Clear( 2 );
+}
+
+function prefClearDiskCache()
+{
+	dump("prefClearDiskCache \n");
+	var cache = Components.classes['component://netscape/network/cache?name=manager'].getService();
+  var	cacheService = cache.QueryInterface( Components.interfaces.nsINetDataCacheManager);
+	cacheService.Clear( 12 );
 }
