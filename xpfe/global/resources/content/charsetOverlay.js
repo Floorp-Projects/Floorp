91fb4ab34b238917000e55920302789eb648735c
cata@netscape.com <cata@netscape.com>
2000-02-16 07:26:47 +0000

    Charset menu cache. r=nhotta bug #24030

--- xpfe/global/resources/content/charsetOverlay.js
+++ xpfe/global/resources/content/charsetOverlay.js
@@ -5,15 +5,41 @@ function MultiplexHandler(event)
 
   if (name == 'detectorGroup') {
     SelectDetector(event);
+  } else if (name == 'charsetGroup') {
+    charset = node.getAttribute('id');
+    charset = charset.substring('charset.'.length, charset.length)
+    SetDefaultCharacterSet(charset);
   } else {
-    SetDefaultCharacterSet(node);
+    SetDefaultCharacterSet(node.getAttribute('id'));
   }
 }
 
-function SetDefaultCharacterSet(node)
+function SetDefaultCharacterSet(charset)
 {
-	dump("Charset Overlay menu item pressed: " + node.getAttribute('id') + "\n");
-    BrowserSetDefaultCharacterSet(node.getAttribute('id'));
+	dump("Charset Overlay menu item pressed: " + charset + "\n");
+    BrowserSetDefaultCharacterSet(charset);
+}
+
+function SelectDetector(event)
+{
+	dump("Charset Detector menu item pressed: " + event.node.getAttribute('id') + "\n");
+
+    uri =  event.target.getAttribute("id");
+    prefvalue = uri.substring('charsetDetector.'.length, uri.length);
+    if("off" == prefvalue) { // "off" is special value to turn off the detectors
+        prefvalue = "";
+    }
+
+    pref = Components.classes['component://netscape/preferences'];
+    if (pref) {
+        pref = pref.getService();
+        pref = pref.QueryInterface(Components.interfaces.nsIPref);
+    }
+ 
+    if (pref) {
+        pref.SetCharPref("intl.charset.detector", prefvalue);
+        window.content.location.reload();
+    }
 }
 
 function UpdateCurrentCharset()
@@ -32,30 +58,10 @@ function UpdateCurrentCharset()
         menu.SetCurrentCharset(charset);
     }
 
-    menuitem = document.getElementById(charset);
+    menuitem = document.getElementById('charset.' + charset);
 
     if (menuitem) {
         menuitem.setAttribute('checked', 'true');
-    }
-}
-
-function SelectDetector(event)
-{
-    uri =  event.target.getAttribute("id");
-    prefvalue = uri.substring('charsetDetector.'.length, uri.length);
-    if("off" == prefvalue) { // "off" is special value to turn off the detectors
-        prefvalue = "";
-    }
-
-    pref = Components.classes['component://netscape/preferences'];
-    if (pref) {
-        pref = pref.getService();
-        pref = pref.QueryInterface(Components.interfaces.nsIPref);
-    }
- 
-    if (pref) {
-        pref.SetCharPref("intl.charset.detector", prefvalue);
-        window.content.location.reload();
     }
 }
 
