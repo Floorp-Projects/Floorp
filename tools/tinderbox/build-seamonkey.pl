9f0b8c08c21fa7cbfaf04def9dac4207567bc4af
slamm%netscape.com <slamm%netscape.com>
2000-02-17 01:44:12 +0000

    Got the signs incorrect. Try again.

--- tools/tinderbox/build-seamonkey.pl
+++ tools/tinderbox/build-seamonkey.pl
@@ -10,7 +10,7 @@ use Sys::Hostname;
 use POSIX qw(sys_wait_h strftime);
 use Cwd;
 
-$Version = '$Revision: 1.53 $ ';
+$Version = '$Revision: 1.54 $ ';
 
 
 sub PrintUsage {
@@ -148,7 +148,7 @@ sub BuildIt {
       $begin = 0 * 60; # Starts 0 minutes after the hour.
       $lag = 2 * 60;   # Takes 2 minute to update.
 
-      $StartTime = int(($StartTime + $begin - $lag) / $cycle) * $cycle - $begin;
+      $StartTime = int(($StartTime - $begin - $lag) / $cycle) * $cycle + $begin;
       $UseTimeStamp = 1;
       $ENV{MOZ_CO_USE_MIRROR} = 1;
     }
