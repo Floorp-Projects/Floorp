42390b4c210d7eb27a0b758309fa993ac35cd693
kvisco%ziplink.net <kvisco%ziplink.net>
2000-02-18 00:11:24 +0000

    I finally included the FreeBSD changes from Eric Du with respect to floating point handling, sorry for the delay

--- extensions/transformiix/source/base/primitives.h
+++ extensions/transformiix/source/base/primitives.h
@@ -3,28 +3,32 @@
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
  * the License at http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS
  * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
  * implied. See the License for the specific language governing
  * rights and limitations under the License.
- * 
+ *
  * The Original Code is TransforMiiX XSLT processor.
- * 
+ *
  * The Initial Developer of the Original Code is The MITRE Corporation.
  * Portions created by MITRE are Copyright (C) 1999 The MITRE Corporation.
  *
  * Portions created by Keith Visco as a Non MITRE employee,
  * (C) 1999 Keith Visco. All Rights Reserved.
- * 
- * Contributor(s): 
+ *
+ * Contributor(s):
  *
  * Keith Visco, kvisco@ziplink.net
  *    -- original author.
  *
  * Larry Fitzpatrick, OpenText, lef@opentext.com
- *    -- 
- * $Id: primitives.h,v 1.2 1999/11/15 07:12:43 nisheeth%netscape.com Exp $
+ *    --
+ *
+ * Eric Du, duxy@leyou.com.cn
+ *   -- added fix for FreeBSD
+ *
+ * $Id: primitives.h,v 1.3 2000/02/18 00:11:24 kvisco%ziplink.net Exp $
  */
 
 
@@ -38,6 +42,11 @@
 
 #ifdef WIN32
 #include <float.h>
+#endif
+
+//A trick to handle IEEE floating point exceptions on FreeBSD - E.D.
+#ifdef __FreeBSD__
+#include <floatingpoint.h>
 #endif
 
 /**
