dc8bfc9921c1fca370e79e9ba939cc1058e70abc
attinasi@netscape.com <attinasi@netscape.com>
2000-02-23 17:58:22 +0000

    Updated scripts to handle Clock-time and to deal with running Mozilla or Viewer

--- tools/performance/layout/perf.pl
+++ tools/performance/layout/perf.pl
@@ -28,32 +28,53 @@
 # 5) Run the script history.pl to generate the history chart
 # 6) Move the files to a directory just for them...
 
+$UseViewer = 1;    # set to 0 to use Mozilla
+$UseClockTime=0;   # if third argument is 'clock' this gets set to 1
 
 # Get and check the arguments
 @ARGV;
 $ID = $ARGV[0];
 $bldRoot = $ARGV[1];
-$profID = $ARGV[2];
+$clockTimeArg = $ARGV[2];
+$profID = $ARGV[3];
 $arg=0;
 $cmdLineArg[$arg++] = "-f ";
 $cmdLineArg[$arg++] = "S:\\mozilla\\tools\\performance\\layout\\40-url.txt ";
-#$cmdLineArg[$arg++] = "-ftimeout ";
-#$cmdLineArg[$arg++] = "15 ";
-#$cmdLineArg[$arg++] = "-P ";
-#$cmdLineArg[$arg++] = "$profID ";
+if($UseViewer==0){
+  $cmdLineArg[$arg++] = "-ftimeout ";
+  $cmdLineArg[$arg++] = "10 ";
+  $cmdLineArg[$arg++] = "-P ";
+  $cmdLineArg[$arg++] = "$profID ";
+}
 
-if(!$ID || !$bldRoot || !$profID){
-  die "ID, Build Root and Profile Name must be provided. \n - Example: 'perl perf.pl Daily_021400 s:\\moz\\daily\\0214 Attinasi' \n";
+if(!$UseViewer){
+  if(!$ID || !$bldRoot || !$profID || !$clockTimeArg){
+    die "ID, Build Root, Profile Name, and clock OR CPU must be provided. \n - Example: 'perl perf.pl Daily_021400 s:\\moz\\daily\\0214  clock|cpu Attinasi' \n";
+  }
+} else {
+  if(!$ID || !$bldRoot || !$clockTimeArg){
+    die "ID, Build Root, Profile Name, and clock OR CPU must be provided. \n - Example: 'perl perf.pl Daily_021400 s:\\moz\\daily\\0214 clock|CPU' \n";
+  }
 }
 
 # build up a full path and argument strings for the invocation
 $mozPath;
 $logName;
-#$mozPath=$bldRoot."\\bin\\Mozilla.exe";
-$mozPath=$bldRoot."\\bin\\Viewer.exe";
+if($UseViewer==0){
+  $mozPath=$bldRoot."\\bin\\Mozilla.exe";
+} else {
+  $mozPath=$bldRoot."\\bin\\Viewer.exe";
+}
 $logName="Logs\\".$ID."-combined.dat";
 
 -e $mozPath or die "$mozPath could not be found\n";
+
+if($clockTimeArg=="clock"){
+  $UseClockTime=1;
+  print( "Charting Clock time...\n");
+} else {
+  print( "Charting CPU time...\n");
+}
 
 # run it
 $commandLine = $mozPath." ".$cmdLineArg[0].$cmdLineArg[1].$cmdLineArg[2].$cmdLineArg[3].$cmdLineArg[4].$cmdLineArg[5]."> $logName";
@@ -65,8 +86,13 @@ print("Breaking result into log files...\n");
 system( ("perl", "uncombine.pl", "$logName") ) == 0 or die "Error uncombining the output\n";
 
 # generate the chart from the logs
-print("Generating performance table...\n");
-system( ("perl", "genfromlogs.pl", "$ID", "$bldRoot") ) == 0 or die "Error generating the chart from the logs\n";
+if($UseClockTime==1){
+  print("Generating performance table... (ClockTime)\n");
+  system( ("perl", "genfromlogs.pl", "$ID", "$bldRoot", "clock") ) == 0 or die "Error generating the clock-time chart from the logs\n";
+} else {
+  print("Generating performance table...(CPUTime)\n");
+  system( ("perl", "genfromlogs.pl", "$ID", "$bldRoot") ) == 0 or die "Error generating the cpu-time chart from the logs\n";
+}
 
 # rename the output
 system( ("copy", "table.html", "Tables\\$ID\.html") ) == 0 or die "Error copying table.html to Tables\\$ID\.html\n";
