31196f5ef05668e5eb1564baf59c439e12fe5c3a
slamm%netscape.com <slamm%netscape.com>
2000-02-26 02:24:24 +0000

    Trim some time off the alive tests.

--- tools/tinderbox/build-seamonkey.pl
+++ tools/tinderbox/build-seamonkey.pl
@@ -11,7 +11,7 @@ use POSIX qw(sys_wait_h strftime);
 use Cwd;
 use File::Basename; # for basename();
 
-$::Version = '$Revision: 1.73 $ ';
+$::Version = '$Revision: 1.74 $ ';
 
 sub PrintUsage {
     die <<END_USAGE
@@ -321,13 +321,13 @@ sub run_tests {
 
     # Mozilla alive test
     print_log "Running AliveTest ...\n";
-    my $build_status = RunAliveTest($build_dir, $binary, 60);
+    my $build_status = RunAliveTest($build_dir, $binary, 45);
     return $build_status if $build_status ne 'success';
 
     # Viewer alive test
     if ($Settings::ViewerTest) {
         print_log "Running ViewerTest ...\n";
-        $build_status = RunAliveTest($build_dir, "$binary_dir/viewer", 60);
+        $build_status = RunAliveTest($build_dir, "$binary_dir/viewer", 45);
         return $build_status if $build_status ne 'success';
     }
     
