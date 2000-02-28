e68701c14aa135e5cd0dc1108ac4d120df7e86c5
dcone%netscape.com <dcone%netscape.com>
2000-02-28 14:27:50 +0000

    Update printer regression tests.  r=karnaze a=rickg

--- layout/html/tests/printer/table/rtest.bat
+++ layout/html/tests/printer/table/rtest.bat
@@ -3,7 +3,7 @@ if %1==baseline goto baseline
 
 :verify
 if not exist verify mkdir verify
-s:\mozilla\dist\win32_d.obj\bin\viewer -Prt 1 -o s:\mozilla\layout\html\tests\printer\table\verify\ -rd s:\mozilla\layout\html\tests\printer\table -f s:\mozilla\layout\html\tests\printer\images\file_list.txt
+s:\mozilla\dist\win32_d.obj\bin\viewer -Prt 1 -o s:\mozilla\layout\html\tests\printer\table\verify\ -rd s:\mozilla\layout\html\tests\printer\table -f s:\mozilla\layout\html\tests\printer\table\file_list.txt
 goto done
 
 :baseline
