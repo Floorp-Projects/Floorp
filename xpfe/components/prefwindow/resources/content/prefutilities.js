1d15d01f5f972fea62e80d3187a05ee17a44ad39
alecf%netscape.com <alecf%netscape.com>
2000-02-29 00:52:51 +0000

    fix for #29168 r=sspitzer a=phil allow us to switch between nativePath and URLString when using the picker in prefs/account manager

--- xpfe/components/prefwindow/resources/content/prefutilities.js
+++ xpfe/components/prefwindow/resources/content/prefutilities.js
@@ -1,6 +1,29 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ * Alec Flett <alecf@netscape.com>
+ */
+
 var bundle = srGetStrBundle("chrome://pref/locale/prefutilities.properties");
 
-function getFileOrFolderURL( aTitle, aFolder )
+function getFileOrFolderSpec( aTitle, aFolder )
 {
   try {
     var fileSpecWithUI = Components.classes["component://netscape/filespecwithui"].createInstance();
@@ -24,15 +47,19 @@ function getFileOrFolderURL( aTitle, aFolder )
     dump("Error: " + e + "\n");
     return -1;
   }
-  return fileSpecWithUI.URLString;
+  return fileSpecWithUI;
 }
 
-function prefNavSelectFile(folderFieldId, stringId)
+function prefNavSelectFile(folderFieldId, stringId, useNative)
 {
   var folderField = document.getElementById(folderFieldId);
-  var url = getFileOrFolderURL( bundle.GetStringFromName(stringId), false );
-  if( url != -1 )
-    folderField.value = url;
+  var spec = getFileOrFolderSpec( bundle.GetStringFromName(stringId), false );
+  if( spec != -1 ) {
+      if (useNative)
+          folderField.value = spec.nativePath;
+      else
+          folderField.value = spec.URLString;
+  }
 }
 
 function setHomePageToCurrentPage(folderFieldId)
