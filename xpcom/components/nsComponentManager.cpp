830c0c6f08d41a1242b219913cd1738dc5857fe4
dp@netscape.com <dp@netscape.com>
2000-03-02 02:13:11 +0000

    Eliminating 11 hits to the registry for Progid if we already have prepopulated the registry. This improves startup performance. a=jevering, r=dveditz

--- xpcom/components/nsComponentManager.cpp
+++ xpcom/components/nsComponentManager.cpp
@@ -1160,7 +1160,9 @@ nsComponentManagerImpl::ProgIDToClassID(const char *aProgID, nsCID *aClass)
     else {
         // This is the first time someone has asked for this
         // ProgID. Go to the registry to find the CID.
-        res = PlatformProgIDToCLSID(aProgID, aClass);
+        if (!mPrePopulationDone) {
+            res = PlatformProgIDToCLSID(aProgID, aClass);
+        }
 
         if (NS_SUCCEEDED(res)) {
             // Found it. So put it into the cache.
