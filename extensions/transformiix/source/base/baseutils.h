5604b7caf5891e668f4c34bab1d835c0fc2eae55
kvisco@ziplink.net <kvisco@ziplink.net>
2000-03-02 09:22:29 +0000

    updated to support TxObject and ArrayList

--- extensions/transformiix/source/base/baseutils.h
+++ extensions/transformiix/source/base/baseutils.h
@@ -1,24 +1,4 @@
 /*
- * (C) Copyright The MITRE Corporation 1999  All rights reserved.
- *
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * The program provided "as is" without any warranty express or
- * implied, including the warranty of non-infringement and the implied
- * warranties of merchantibility and fitness for a particular purpose.
- * The Copyright owner will not be liable for any damages suffered by
- * you as a result of using the Program. In no event will the Copyright
- * owner be liable for any special, indirect or consequential damages or
- * lost profits even if the Copyright owner has been advised of the
- * possibility of their occurrence.
- *
- * Please see release.txt distributed with this file for more information.
- *
- */
-/*
  * The contents of this file are subject to the Mozilla Public
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
@@ -41,20 +21,17 @@
  * Keith Visco, kvisco@ziplink.net
  *    -- original author.
  *
- * $Id: baseutils.h,v 1.3 2000/02/02 18:17:30 tomk%mitre.org Exp $
+ * Tom Kneeland, tomk@mitre.org
+ *    -- added UInt32 to provide a common unsigned integer
+ *
+ * $Id: baseutils.h,v 1.4 2000/03/02 09:22:29 kvisco%ziplink.net Exp $
  */
 
 // Basic Definitions used throughout many of these classes
 
-//
-// Modification History:
-// Who  When        What
-// TK   01/31/2000  Added UInt32 to provide a common unsigned integer.
-//
 
-
-#ifndef MITRE_BASEUTILS_H
-#define MITRE_BASEUTILS_H
+#ifndef TRANSFRMX_BASEUTILS_H
+#define TRANSFRMX_BASEUTILS_H
 
 typedef int Int32;
 typedef unsigned int UInt32;
