48da3455deca52e4da6298b2ca1cc9f9e3a8ef77
warren%netscape.com <warren%netscape.com>
2000-03-05 21:36:54 +0000

    Bug 21556: Making linux be thread-safe. Additional diffs from Andreas. r=warren,a=jar

--- netwerk/base/src/nsAuthURLParser.cpp
+++ netwerk/base/src/nsAuthURLParser.cpp
@@ -23,8 +23,7 @@
 #include "nsString.h"
 #include "prprf.h"
 
-NS_IMPL_ISUPPORTS1(nsAuthURLParser, 
-                   nsIURLParser)
+NS_IMPL_THREADSAFE_ISUPPORTS(nsAuthURLParser, NS_GET_IID(nsIURLParser))
 
 nsAuthURLParser::~nsAuthURLParser()
 {
@@ -298,7 +297,7 @@ nsresult
 nsAuthURLParser::ParseAtPath(const char* i_Spec, char* *o_Path)
 {
     // Just write the path and check for a starting /
-    nsAutoString dir;
+    nsCAutoString dir;
     if ('/' != *i_Spec)
         dir += "/";
     
