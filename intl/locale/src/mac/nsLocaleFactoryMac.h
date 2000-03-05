d20e79f69502e3c7f8d5c0a0aa89682e1ceb65ae
warren%netscape.com <warren%netscape.com>
2000-03-05 23:23:23 +0000

    Fixing mac bustage. Need to use NS_DECL_ISUPPORTS here.

--- intl/locale/src/mac/nsLocaleFactoryMac.h
+++ intl/locale/src/mac/nsLocaleFactoryMac.h
@@ -27,24 +27,13 @@
 
 class nsLocaleMacFactory : public nsIFactory
 {   
-  public:   
-    // nsISupports methods   
-    NS_IMETHOD QueryInterface(const nsIID &aIID,    
-                              void **aResult);   
-    NS_IMETHOD_(nsrefcnt) AddRef(void);   
-    NS_IMETHOD_(nsrefcnt) Release(void);   
-
-    // nsIFactory methods   
-    NS_IMETHOD CreateInstance(nsISupports *aOuter,   
-                              const nsIID &aIID,   
-                              void **aResult);   
-
-    NS_IMETHOD LockFactory(PRBool aLock);   
+  public:
+    NS_DECL_ISUPPORTS
+    NS_DECL_NSIFACTORY
 
     nsLocaleMacFactory(const nsCID &aClass);   
     ~nsLocaleMacFactory();   
 
   private:   
-    nsrefcnt  mRefCnt;   
     nsCID     mClassID;
 };
