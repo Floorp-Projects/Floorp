f0398a8427e4fdc1df2be821166d49880f0e0b7e
jevering%netscape.com <jevering%netscape.com>
2000-03-10 06:25:13 +0000

    this is valeski in jevering's tree. a=jevering. r=ltabb (nice!), r=vidur. forcing cache bypass for inline JS url loads. this is a workaround, not a final fix

--- content/html/document/src/nsHTMLContentSink.cpp
+++ content/html/document/src/nsHTMLContentSink.cpp
@@ -4327,7 +4327,8 @@ HTMLContentSink::ProcessSCRIPTTag(const nsIParserNode& aNode)
       nsIStreamLoader* loader;
 
       mDocument->GetDocumentLoadGroup(getter_AddRefs(loadGroup));
-      rv = NS_NewStreamLoader(&loader, mScriptURI, this, nsnull, loadGroup);
+      rv = NS_NewStreamLoader(&loader, mScriptURI, this, nsnull, loadGroup,
+                              nsnull, nsIChannel::FORCE_RELOAD);
       if (NS_OK == rv) {
         rv = NS_ERROR_HTMLPARSER_BLOCK;
       }
