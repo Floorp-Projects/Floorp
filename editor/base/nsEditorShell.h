d393cebef9c733b758c795a8b64c10b99ab9624e
tbogard@aol.net <tbogard@aol.net>
2000-03-11 01:01:39 +0000

    Use the XPIDL generated macro to declare the methods for nsIDocumentLoaderObserver interface.

--- editor/base/nsEditorShell.h
+++ editor/base/nsEditorShell.h
@@ -90,20 +90,7 @@ class nsEditorShell :   public nsIEditorShell,
     NS_DECL_NSIEDITORSPELLCHECK
 
     // nsIDocumentLoaderObserver
-    NS_IMETHOD OnStartDocumentLoad(nsIDocumentLoader* loader, nsIURI* aURL, const char* aCommand);
-
-    NS_IMETHOD OnEndDocumentLoad(nsIDocumentLoader* loader, nsIChannel* channel, nsresult aStatus);
-    NS_IMETHOD OnStartURLLoad(nsIDocumentLoader* loader, nsIChannel* channel);
-    NS_IMETHOD OnProgressURLLoad(nsIDocumentLoader* loader, nsIChannel* channel, PRUint32 aProgress,
-                               PRUint32 aProgressMax);
-    NS_IMETHOD OnStatusURLLoad(nsIDocumentLoader* loader, nsIChannel* channel, nsString& aMsg);
-
-    NS_IMETHOD OnEndURLLoad(nsIDocumentLoader* loader, nsIChannel* channel, nsresult aStatus);
-
-    NS_IMETHOD HandleUnknownContentType(nsIDocumentLoader* loader,
-                                        nsIChannel* channel,
-                                        const char *aContentType,
-                                        const char *aCommand );
+    NS_DECL_NSIDOCUMENTLOADEROBSERVER
 
   protected:
     nsIDOMWindow       *mToolbarWindow;				// weak reference
