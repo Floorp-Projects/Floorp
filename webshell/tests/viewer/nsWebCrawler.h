34f4cfd0641371cf5f02871858077fef05cf1553
tbogard@aol.net <tbogard@aol.net>
2000-03-11 00:19:38 +0000

    nsIDocumentLoaderObserver function declarations now use the XPIDL macro.

--- webshell/tests/viewer/nsWebCrawler.h
+++ webshell/tests/viewer/nsWebCrawler.h
@@ -48,21 +48,7 @@ public:
   NS_DECL_ISUPPORTS
 
   // nsIDocumentLoaderObserver
-  NS_IMETHOD OnStartDocumentLoad(nsIDocumentLoader* loader, nsIURI* aURL,
-                                 const char* aCommand);
-  NS_IMETHOD OnEndDocumentLoad(nsIDocumentLoader* loader, nsIChannel* channel,
-                               nsresult aStatus);
-  NS_IMETHOD OnStartURLLoad(nsIDocumentLoader* loader, nsIChannel* channel);
-  NS_IMETHOD OnProgressURLLoad(nsIDocumentLoader* loader, nsIChannel* channel,
-                               PRUint32 aProgress, PRUint32 aProgressMax);
-  NS_IMETHOD OnStatusURLLoad(nsIDocumentLoader* loader, nsIChannel* channel,
-                             nsString& aMsg);
-  NS_IMETHOD OnEndURLLoad(nsIDocumentLoader* loader, nsIChannel* channel,
-                          nsresult aStatus);
-  NS_IMETHOD HandleUnknownContentType(nsIDocumentLoader* loader,
-                                      nsIChannel* channel,
-                                      const char *aContentType,
-                                      const char *aCommand);
+  NS_DECL_NSIDOCUMENTLOADEROBSERVER
 
   // Add a url to load
   void AddURL(const nsString& aURL);
