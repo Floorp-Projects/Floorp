cfb0e95a01f2c3f63c552b103cc89cad1a9d11ab
tbogard%aol.net <tbogard%aol.net>
2000-03-11 01:22:23 +0000

    Changed to use the XPIDL generated macro to declare the methods for nsIDocumentLoaderObserver.

--- extensions/wallet/src/nsWalletService.h
+++ extensions/wallet/src/nsWalletService.h
@@ -75,22 +75,7 @@ public:
   NS_IMETHOD Notify(nsIContent* formNode);
 
   // nsIDocumentLoaderObserver
-  NS_IMETHOD OnStartDocumentLoad
-    (nsIDocumentLoader* loader, nsIURI* aURL, const char* aCommand);
-  NS_IMETHOD OnEndDocumentLoad
-    (nsIDocumentLoader* loader, nsIChannel* channel, nsresult aStatus);
-  NS_IMETHOD OnStartURLLoad
-    (nsIDocumentLoader* loader, nsIChannel* channel);
-  NS_IMETHOD OnProgressURLLoad
-    (nsIDocumentLoader* loader, nsIChannel* channel, PRUint32 aProgress,
-     PRUint32 aProgressMax);
-  NS_IMETHOD OnStatusURLLoad
-    (nsIDocumentLoader* loader, nsIChannel* channel, nsString& aMsg);
-  NS_IMETHOD OnEndURLLoad
-    (nsIDocumentLoader* loader, nsIChannel* channel, nsresult aStatus);
-  NS_IMETHOD HandleUnknownContentType
-    (nsIDocumentLoader* loader, nsIChannel* channel, const char *aContentType,
-     const char *aCommand );		
+  NS_DECL_NSIDOCUMENTLOADEROBSERVER
 
   // nsIPasswordSink
   NS_DECL_NSIPASSWORDSINK
