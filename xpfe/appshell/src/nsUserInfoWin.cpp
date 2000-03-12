e66c382543514a39690e6d62deffe9a271c333f1
sspitzer%netscape.com <sspitzer%netscape.com>
2000-03-12 11:49:28 +0000

    we now have an implementation of nsIUserInfo for windows. only GetUsername is implemented.

--- xpfe/appshell/src/nsUserInfoWin.cpp
+++ xpfe/appshell/src/nsUserInfoWin.cpp
@@ -19,11 +19,14 @@
  *
  * Contributor(s): 
  *   Doug Turner <dougt@netscape.com>
+ *   Seth Spitzer <sspitzer@netscape.com>
  */
 
 #include "nsUserInfo.h"
 #include "nsString.h"
 #include "windows.h"
+#include "nsCRT.h"
+#include "nsXPIDLString.h"
 
 nsUserInfo::nsUserInfo()
 {
@@ -37,9 +40,9 @@ nsUserInfo::~nsUserInfo()
 NS_IMPL_ISUPPORTS1(nsUserInfo,nsIUserInfo);
 
 NS_IMETHODIMP
-nsUserInfo::GetFullname(PRUnichar **aFullname)
+nsUserInfo::GetUsername(char **aUsername)
 {
-    *aFullname = nsnull;
+    *aUsername = nsnull;
 
     TCHAR username[256];
     DWORD size = 256;
@@ -47,29 +50,30 @@ nsUserInfo::GetFullname(PRUnichar **aFullname)
     if (!GetUserName(username, &size))
         return NS_ERROR_FAILURE;
     
-    nsAutoString fullname(username);
-    *aFullname = fullname.ToNewUnicode();
+    *aUsername = nsCRT::strdup(username);
     
-    if (*aFullname)
-        return NS_OK;
+    if (*aUsername) return NS_OK;
 
     return NS_ERROR_FAILURE;
 }
 
-NS_IMETHODIMP
-nsUserInfo::GetEmailAddress(char * *aEmailAddress)
-{
-    return NS_ERROR_NOT_IMPLEMENTED;
-}
-
 NS_IMETHODIMP 
-nsUserInfo::GetUsername(char * *aUsername)
+nsUserInfo::GetFullname(PRUnichar **aFullname)
 {
+    *aFullname = nsnull;
     return NS_ERROR_NOT_IMPLEMENTED;
 }
 
 NS_IMETHODIMP 
 nsUserInfo::GetDomain(char * *aDomain)
+{ 
+    *aDomain = nsnull;
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+NS_IMETHODIMP 
+nsUserInfo::GetEmailAddress(char * *aEmailAddress)
 {
+    *aEmailAddress = nsnull;
     return NS_ERROR_NOT_IMPLEMENTED;
 }
