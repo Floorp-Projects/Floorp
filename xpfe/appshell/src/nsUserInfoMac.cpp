969f8bbd2b7d5314eb05027e62ea0f974ec9cbb1
sspitzer@netscape.com <sspitzer@netscape.com>
2000-03-12 11:52:02 +0000

    minor tweakage, not part of the build yet.

--- xpfe/appshell/src/nsUserInfoMac.cpp
+++ xpfe/appshell/src/nsUserInfoMac.cpp
@@ -36,24 +36,28 @@ NS_IMPL_ISUPPORTS1(nsUserInfo,nsIUserInfo);
 NS_IMETHODIMP
 nsUserInfo::GetFullname(PRUnichar **aFullname)
 {
+    *aFullname = nsnull;
     return NS_ERROR_NOT_IMPLEMENTED;
 }
 
 NS_IMETHODIMP 
 nsUserInfo::GetEmailAddress(char * *aEmailAddress)
 {
+    *aEmailAddress = nsnull;
     return NS_ERROR_NOT_IMPLEMENTED;
 }
 
 NS_IMETHODIMP 
 nsUserInfo::GetUsername(char * *aUsername)
 {
+    *aUsername = nsnull;
     return NS_ERROR_NOT_IMPLEMENTED;
 }
 
 NS_IMETHODIMP 
 nsUserInfo::GetDomain(char * *aDomain)
 {
+    *aDomain = nsnull;
     return NS_ERROR_NOT_IMPLEMENTED;
 }
 
