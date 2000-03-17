5ba3a9bed3deab89574705e1ae6c3f3537558723
sspitzer%netscape.com <sspitzer%netscape.com>
2000-03-17 23:08:51 +0000

    more changes for the new subscribe dialog.  not part of the build yet.

--- mailnews/base/resources/content/subscribe.js
+++ mailnews/base/resources/content/subscribe.js
@@ -1,15 +1,53 @@
-var gSubscribetree;
-var gServername;
+var gSubscribetree = null;
+var gCurrentServer = null;
+var okCallback = null;
 
 function SubscribeOnLoad()
 {
 	dump("subscribeOnLoad()\n");
 
     gSubscribetree = document.getElementById('subscribetree');
-    gServername = document.getElementById('servername');
+    gCurrentServer = document.getElementById('currentserver');
 
-	gSubscribetree.setAttribute('ref','news://news.mozilla.org');
-	gServername.value = "news.mozilla.org";
+	doSetOKCancel(subscribeOK,subscribeCancel);
+
+	// look in arguments[0] for parameters
+	if (window.arguments && window.arguments[0]) {
+		if ( window.arguments[0].title ) {
+			top.window.title = window.arguments[0].title;
+		}
+		
+		if ( window.arguments[0].okCallback ) {
+			top.okCallback = window.arguments[0].okCallback;
+		}
+	}
+	
+	// pre select the folderPicker, based on what they selected in the folder pane
+	if (window.arguments[0].preselectedURI) {
+		var folder = GetMsgFolderFromUri(window.arguments[0].preselectedURI);
+		var server = folder.server;
+		
+		gSubscribetree.setAttribute('ref',server.serverURI);
+		gCurrentServer.value = server.hostName;	// use gServer.prettyName?
+	}
+}
+
+function subscribeOK()
+{
+	dump("in subscribeOK()\n")
+	if (top.okCallback) {
+		// we stored the uri as the ref, now get it back
+    	var tree = document.getElementById('subscribetree');
+		var uri = tree.getAttribute('ref');
+		top.okCallback(uri);
+	}
+	return true;
+}
+
+function subscribeCancel()
+{
+	dump("in subscribeCancel()\n")
+	return true;
 }
 
 function SubscribeButtonClicked()
