952216ff6e1427807024a092e1a9823f1581f2d1
mscott%netscape.com <mscott%netscape.com>
2000-03-20 00:20:20 +0000

    Bug #7807 --> make nsImapservice scriptable.

--- mailnews/imap/src/nsImapService.h
+++ mailnews/imap/src/nsImapService.h
@@ -48,144 +48,12 @@ public:
 	virtual ~nsImapService();
 	
 	NS_DECL_ISUPPORTS
-    NS_DECL_NSIMSGPROTOCOLINFO
+  NS_DECL_NSIMSGPROTOCOLINFO
 
 	////////////////////////////////////////////////////////////////////////////////////////
 	// we suppport the nsIImapService interface 
 	////////////////////////////////////////////////////////////////////////////////////////
-
-	NS_IMETHOD SelectFolder(nsIEventQueue * aClientEventQueue, 
-                            nsIMsgFolder *aImapMailFolder, 
-                            nsIUrlListener * aUrlListener, 
-							nsIMsgWindow *aMsgWindow,
-                            nsIURI ** aURL);	
-	NS_IMETHOD LiteSelectFolder(nsIEventQueue * aClientEventQueue, 
-                                nsIMsgFolder * aImapMailFolder, 
-                                nsIUrlListener * aUrlListener, 
-                                nsIURI ** aURL);
-	NS_IMETHOD FetchMessage(
-                            nsIImapUrl * aImapUrl,
-                            nsImapAction aImapAction,
-                            nsIMsgFolder * aImapMailFolder, 
-                            nsIImapMessageSink * aImapMessage,
-                            nsIURI ** aURL,
-							              nsISupports *aConsumer,
-                            const char *messageIdentifierList,
-                            PRBool messageIdsAreUID);
-	NS_IMETHOD Noop(nsIEventQueue * aClientEventQueue, 
-                    nsIMsgFolder * aImapMailFolder,
-                    nsIUrlListener * aUrlListener, 
-                    nsIURI ** aURL);
-	NS_IMETHOD GetHeaders(nsIEventQueue * aClientEventQueue, 
-                          nsIMsgFolder * aImapMailFolder, 
-                          nsIUrlListener * aUrlListener, 
-                          nsIURI ** aURL,
-                          const char *messageIdentifierList,
-                          PRBool messageIdsAreUID);
-	NS_IMETHOD Expunge(nsIEventQueue * aClientEventQueue, 
-                       nsIMsgFolder * aImapMailFolder,
-                       nsIUrlListener * aUrlListener,
-                       nsIURI ** aURL);
-	NS_IMETHOD Biff(nsIEventQueue * aClientEventQueue, 
-                    nsIMsgFolder * aImapMailFolder,
-                    nsIUrlListener * aUrlListener,
-                    nsIURI ** aURL,
-                    PRUint32 uidHighWater);
-	NS_IMETHOD DeleteMessages(nsIEventQueue * aClientEventQueue,
-                              nsIMsgFolder * aImapMailFolder, 
-                              nsIUrlListener * aUrlListener,
-                              nsIURI ** aURL,
-                              const char *messageIdentifierList,
-                              PRBool messageIdsAreUID);
-	NS_IMETHOD DeleteAllMessages(nsIEventQueue * aClientEventQueue, 
-                                 nsIMsgFolder * aImapMailFolder,
-                                 nsIUrlListener * aUrlListener,
-                                 nsIURI ** aURL);
-	NS_IMETHOD AddMessageFlags(nsIEventQueue * aClientEventQueue,
-                               nsIMsgFolder * aImapMailFolder, 
-                               nsIUrlListener * aUrlListener,
-                               nsIURI ** aURL,
-                               const char *messageIdentifierList,
-                               imapMessageFlagsType flags,
-                               PRBool messageIdsAreUID);
-	NS_IMETHOD SubtractMessageFlags(nsIEventQueue * aClientEventQueue,
-                                    nsIMsgFolder * aImapMailFolder, 
-                                    nsIUrlListener * aUrlListener,
-                                    nsIURI ** aURL,
-                                    const char *messageIdentifierList,
-                                    imapMessageFlagsType flags,
-                                    PRBool messageIdsAreUID);
-	NS_IMETHOD SetMessageFlags(nsIEventQueue * aClientEventQueue,
-                               nsIMsgFolder * aImapMailFolder, 
-                               nsIUrlListener * aUrlListener, 
-                               nsIURI ** aURL,
-                               const char *messageIdentifierList,
-                               imapMessageFlagsType flags,
-                               PRBool messageIdsAreUID);
-
-    NS_IMETHOD DiscoverAllFolders(nsIEventQueue* aClientEventQueue,
-                                  nsIMsgFolder* aImapMailFolder,
-                                  nsIUrlListener* aUrlListener,
-                                  nsIURI** aURL);
-    NS_IMETHOD DiscoverAllAndSubscribedFolders(nsIEventQueue*
-                                               aClientEventQueue,
-                                               nsIMsgFolder* aImapMailFolder,
-                                               nsIUrlListener* aUrlListener,
-                                               nsIURI** aURL);
-    NS_IMETHOD DiscoverChildren(nsIEventQueue* aClientEventQueue,
-                                nsIMsgFolder* aImapMailFolder,
-                                nsIUrlListener* aUrlListener,
-                                nsIURI** aURL);
-    NS_IMETHOD DiscoverLevelChildren(nsIEventQueue* aClientEventQueue,
-                                     nsIMsgFolder* aImapMailFolder,
-                                     nsIUrlListener* aUrlListener,
-                                     PRInt32 level,
-                                     nsIURI** aURL);
-    NS_IMETHOD OnlineMessageCopy(nsIEventQueue* aClientEventQueue,
-                                 nsIMsgFolder* aSrcFolder,
-                                 const char* messageIds,
-                                 nsIMsgFolder* aDstFolder,
-                                 PRBool idsAreUids,
-                                 PRBool isMove,
-                                 nsIUrlListener* aUrlListener,
-                                 nsIURI** aURL,
-                                 nsISupports* copyState);
-    NS_IMETHOD AppendMessageFromFile(nsIEventQueue* aClientEventQ,
-                                     nsIFileSpec* aFileSpec,
-                                     nsIMsgFolder* aDstFolder,
-                                     const char* messageId, // to replace with
-                                     PRBool idsAreUids,
-                                     PRBool inSelectedState, // needs to be in
-                                     nsIUrlListener* aUrlListener,
-                                     nsIURI** aURL,
-                                     nsISupports* copyState);
-    NS_IMETHOD MoveFolder(nsIEventQueue* eventQueue,
-                          nsIMsgFolder* srcFolder,
-                          nsIMsgFolder* dstFolder,
-                          nsIUrlListener* urlListener,
-                          nsIURI** url);
-    NS_IMETHOD RenameLeaf(nsIEventQueue* eventQueue,
-                          nsIMsgFolder* srcFolder,
-                          const PRUnichar* leafName,
-                          nsIUrlListener* urlListener,
-                          nsIURI** url);
-    NS_IMETHOD DeleteFolder(nsIEventQueue* eventQueue,
-                            nsIMsgFolder* srcFolder,
-                            nsIUrlListener* urlListener,
-                            nsIURI** url);
-    NS_IMETHOD CreateFolder(nsIEventQueue* eventQueue,
-                          nsIMsgFolder* parent,
-                          const PRUnichar* leafName,
-                          nsIUrlListener* urlListener,
-                          nsIURI** url);
-
-	NS_IMETHOD ListFolder(nsIEventQueue* aClientEventQueue,
-                                nsIMsgFolder* aImapMailFolder,
-                                nsIUrlListener* aUrlListener,
-                                nsIURI** aURL);
-	////////////////////////////////////////////////////////////////////////////////////////
-	// End support of nsIImapService interface 
-	////////////////////////////////////////////////////////////////////////////////////////
+  NS_DECL_NSIIMAPSERVICE
 
 	////////////////////////////////////////////////////////////////////////////////////////
 	// we suppport the nsIMsgMessageService Interface 
@@ -196,9 +64,6 @@ public:
 	// we suppport the nsIProtocolHandler interface 
 	////////////////////////////////////////////////////////////////////////////////////////
     NS_DECL_NSIPROTOCOLHANDLER
-	////////////////////////////////////////////////////////////////////////////////////////
-	// End support of nsIProtocolHandler interface 
-	////////////////////////////////////////////////////////////////////////////////////////
 
 protected:
     PRUnichar GetHierarchyDelimiter(nsIMsgFolder* aMsgFolder);
