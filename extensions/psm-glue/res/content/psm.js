52a11759cff0d4f9c8e8f1a520c61aac4c35bc86
dougt%netscape.com <dougt%netscape.com>
2000-03-22 03:10:05 +0000

    removing psm.js.  it is now included directly in the xul file.

--- extensions/psm-glue/res/content/psm.js
+++
@@ -1,7 +0,0 @@
-function displaySecurityAdvisor()
-{
-    var psm = Components.classes["component://netscape/psm"].getService();
-    psm = psm.QueryInterface(Components.interfaces.nsIPSMComponent);
-    psm.DisplaySecurityAdvisor( null );
-}
-
