aa365309bbc1278ab18ebde32f258f44d72536a0
rbs@maths.uq.edu.au <rbs@maths.uq.edu.au>
2000-03-23 09:31:16 +0000

    [#ifdef MOZ_MATHML - NOT PART OF THE DEFAULT BUILD]. Adding the ucvmath module --for Mathematical charsets (symbols for MathML/scientific documents) r:cata@nestcape.com

---
+++ intl/uconv/ucvmath/nsUnicodeToMathematica1.cpp
@@ -0,0 +1,72 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ *   Roger B. Sidje <rbs@maths.uq.edu.au>
+ *
+ * This Original Code has been modified by Roger B. Sidje.
+ * Modifications made by Roger B. Sidje described herein are
+ * Copyright (C) 2000 The University Of Queensland.
+ *
+ * Modifications to Mozilla code or documentation
+ * identified per MPL Section 3.3
+ *
+ * Date            Modified by     Description of modification
+ * 08/March/2000   RBS.            Support for Mathematical fonts.
+ */
+
+#include "nsUnicodeToMathematica1.h"
+
+//----------------------------------------------------------------------
+// Global functions and data [declaration]
+
+static PRUint16 g_ufMappingTable[] = {
+#include "mathematica1.uf"
+};
+
+static PRInt16 g_ufShiftTable[] =  {
+  0, u1ByteCharset ,
+  ShiftCell(0,0,0,0,0,0,0,0)
+};
+
+//----------------------------------------------------------------------
+// Class nsUnicodeToMathematica1 [implementation]
+
+nsUnicodeToMathematica1::nsUnicodeToMathematica1() 
+: nsTableEncoderSupport((uShiftTable*) &g_ufShiftTable, 
+                        (uMappingTable*) &g_ufMappingTable)
+{
+}
+
+nsresult nsUnicodeToMathematica1::CreateInstance(nsISupports ** aResult) 
+{
+  *aResult = (nsIUnicodeEncoder*) new nsUnicodeToMathematica1();
+  return (*aResult == NULL)? NS_ERROR_OUT_OF_MEMORY : NS_OK;
+}
+
+//----------------------------------------------------------------------
+// Subclassing of nsTableEncoderSupport class [implementation]
+
+NS_IMETHODIMP nsUnicodeToMathematica1::GetMaxLength(const PRUnichar * aSrc, 
+                                              PRInt32 aSrcLength,
+                                              PRInt32 * aDestLength)
+{
+  *aDestLength = aSrcLength;
+  return NS_OK_UENC_EXACTLENGTH;
+}
---
+++ intl/uconv/ucvmath/nsUnicodeToTeXCMSYt1.h
@@ -0,0 +1,69 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ *   Roger B. Sidje <rbs@maths.uq.edu.au>
+ *
+ * This Original Code has been modified by Roger B. Sidje.
+ * Modifications made by Roger B. Sidje described herein are
+ * Copyright (C) 2000 The University Of Queensland.
+ *
+ * Modifications to Mozilla code or documentation
+ * identified per MPL Section 3.3
+ *
+ * Date            Modified by     Description of modification
+ * 08/March/2000   RBS.            Support for Mathematical fonts.
+ */
+
+#ifndef nsUnicodeToTeXCMSYt1_h___
+#define nsUnicodeToTeXCMSYt1_h___
+
+#include "nsUCvMathSupport.h"
+
+//----------------------------------------------------------------------
+// Class nsUnicodeToTeXCMSYt1 [declaration]
+
+/**
+ * A character set converter from Unicode to TeXCMSYt1 (TeX's Computer Modern Symbol).
+ *
+ */
+class nsUnicodeToTeXCMSYt1 : public nsTableEncoderSupport
+{
+public:
+
+  /**
+   * Class constructor.
+   */
+  nsUnicodeToTeXCMSYt1();
+
+  /**
+   * Static class constructor.
+   */
+  static nsresult CreateInstance(nsISupports **aResult);
+
+protected:
+
+  //--------------------------------------------------------------------
+  // Subclassing of nsEncoderSupport class [declaration]
+
+  NS_IMETHOD GetMaxLength(const PRUnichar * aSrc, PRInt32 aSrcLength, 
+      PRInt32 * aDestLength);
+};
+
+#endif /* nsUnicodeToTeXCMSYt1_h___ */
