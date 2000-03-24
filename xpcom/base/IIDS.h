997e630d35f19238f9f6e5c35063e8b648d6cbff
jfrancis@netscape.com <jfrancis@netscape.com>
2000-03-24 00:43:38 +0000

    nsIEditRules IID

--- xpcom/base/IIDS.h
+++ xpcom/base/IIDS.h
@@ -1244,7 +1244,7 @@ nsIDOMNSHTMLAnchorElement = { /* a6cf911c-15b3-11d2-932e-00805f8add32 */
     0x11d2,
     {0x93, 0x2e, 0x00, 0x80, 0x5f, 0x8a, 0xdd, 0x32}
   };
-INTERFACENAME = { /* a6cf911d-15b3-11d2-932e-00805f8add32 */
+nsIEditRules = { /* a6cf911d-15b3-11d2-932e-00805f8add32 */
     0xa6cf911d,
     0x15b3,
     0x11d2,
