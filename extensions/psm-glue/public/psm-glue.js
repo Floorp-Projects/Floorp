6281edae8574ff90bad0d4231cff631b396c586b
dougt%netscape.com <dougt%netscape.com>
2000-03-28 12:24:17 +0000

    Adding psm-glue preferences javascript.  These are the prefs that we should have defaults for.

---
+++ extensions/psm-glue/public/psm-glue.js
@@ -0,0 +1,11 @@
+pref("general.useragent.security",       "U");
+
+pref("security.enable_ssl2",             true);
+pref("security.enable_ssl3",             true);
+pref("security.default_personal_cert",   "Select Automatically");
+pref("security.ask_for_password",        0);
+pref("security.password_lifetime",       30);
+pref("security.warn_entering_secure",    true);
+pref("security.warn_leaving_secure",     true);
+pref("security.warn_viewing_mixed",      true);
+pref("security.warn_submit_insecure",    true);
