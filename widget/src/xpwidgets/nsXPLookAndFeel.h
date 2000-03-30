1c0f3bd108d0beba5e7502138b8bcf826d079690
akkana@netscape.com <akkana@netscape.com>
2000-03-30 22:32:18 +0000

    Let the user look-and-feel paramaters with prefs, bugs 24514 and 33417

---
+++ widget/src/xpwidgets/nsXPLookAndFeel.h
@@ -0,0 +1,90 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ */
+
+#ifndef __nsXPLookAndFeel
+#define __nsXPLookAndFeel
+
+#include "nsILookAndFeel.h"
+
+#include "nsCOMPtr.h"
+#include "nsIPref.h"
+
+struct nsLookAndFeelIntPref
+{
+  char* name;
+  nsILookAndFeel::nsMetricID id;
+  PRPackedBool isSet;
+  PRPackedBool isFloat; // default (PR_FALSE) is PRInt32
+  PRInt32 intVar;
+};
+
+struct nsLookAndFeelFloatPref
+{
+  char* name;
+  nsILookAndFeel::nsMetricFloatID id;
+  PRPackedBool isSet;
+  PRPackedBool isFloat; // default (PR_FALSE) is PRInt32
+  float floatVar;
+};
+
+class nsXPLookAndFeel: public nsILookAndFeel
+{
+public:
+  nsXPLookAndFeel();
+  virtual ~nsXPLookAndFeel();
+
+  NS_DECL_ISUPPORTS
+
+  void Init();
+
+  //
+  // All these routines will return NS_OK if they have a value,
+  // in which case the nsLookAndFeel should use that value;
+  // otherwise we'll return NS_ERROR_NOT_AVAILABLE, in which case, the
+  // platform-specific nsLookAndFeel should use its own values instead.
+  //
+  NS_IMETHOD GetColor(const nsColorID aID, nscolor &aColor);
+  NS_IMETHOD GetMetric(const nsMetricID aID, PRInt32 & aMetric);
+  NS_IMETHOD GetMetric(const nsMetricFloatID aID, float & aMetric);
+
+#ifdef NS_DEBUG
+  NS_IMETHOD GetNavSize(const nsMetricNavWidgetID aWidgetID,
+                        const nsMetricNavFontID   aFontID, 
+                        const PRInt32             aFontSize, 
+                        nsSize &aSize);
+#endif
+
+protected:
+  nsresult InitFromPref(nsLookAndFeelIntPref* aPref);
+  nsresult InitFromPref(nsLookAndFeelFloatPref* aPref);
+
+  nsCOMPtr<nsIPref> mPrefService;
+
+  static PRBool sInitialized;
+
+  static nsLookAndFeelIntPref sIntPrefs[22];
+  static nsLookAndFeelFloatPref sFloatPrefs[8];
+};
+
+extern nsresult NS_NewXPLookAndFeel(nsILookAndFeel**);
+
+#endif
