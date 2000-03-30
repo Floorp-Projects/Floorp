c7825191f2ecee786706854fffcd81336ce36633
dcone%netscape.com <dcone%netscape.com>
2000-03-30 15:00:45 +0000

    Added some methods for tiling.

--- gfx/public/nsRenderingContextImpl.h
+++ gfx/public/nsRenderingContextImpl.h
@@ -32,8 +32,37 @@ public:
   nsRenderingContextImpl();
 
 
+
+  /** ---------------------------------------------------
+   *  See documentation in nsIRenderingContext.h
+   *	@update 03/29/00 dwc
+   */
+  NS_IMETHOD DrawTile(nsIImage *aImage,nscoord aX0,nscoord aY0,nscoord aX1,nscoord aY1,nscoord aWidth,nscoord aHeight);
+
+
 protected:
   virtual ~nsRenderingContextImpl();
+
+  /** ---------------------------------------------------
+   *  Check to see if the given size of tile can be imaged by the RenderingContext
+   *	@update 03/29/00 dwc
+   *  @param aWidth The width of the tile
+   *  @param aHeight The height of the tile
+   *  @return PR_TRUE the RenderingContext can handle this tile
+   */
+  virtual PRBool CanTile(nscoord aWidth,nscoord aHeight) { return PR_FALSE; }
+
+  
+  /** ---------------------------------------------------
+   *  A bit blitter to tile images to the background recursively
+   *	@update 3/29/00 dwc
+   *  @param aDS -- Target drawing surface for the rendering context
+   *  @param aSrcRect -- Rectangle we are build with the image
+   *  @param aHeight -- height of the tile
+   *  @param aWidth -- width of the tile
+   */
+  void  TileImage(nsDrawingSurface  aDS,nsRect &aSrcRect,PRInt16 aWidth,PRInt16 aHeight);
+
 
 public:
 
