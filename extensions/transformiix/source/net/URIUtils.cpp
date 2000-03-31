a42d39354dc3559a3be60df1b525fc99f686b0f4
kvisco@ziplink.net <kvisco@ziplink.net>
2000-03-31 07:07:24 +0000

    updated to support changes in String.cpp

--- extensions/transformiix/source/net/URIUtils.cpp
+++ extensions/transformiix/source/net/URIUtils.cpp
@@ -3,33 +3,33 @@
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
  * the License at http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS
  * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
  * implied. See the License for the specific language governing
  * rights and limitations under the License.
- * 
+ *
  * The Original Code is TransforMiiX XSLT processor.
- * 
+ *
  * The Initial Developer of the Original Code is The MITRE Corporation.
  * Portions created by MITRE are Copyright (C) 1999 The MITRE Corporation.
  *
  * Portions created by Keith Visco as a Non MITRE employee,
  * (C) 1999 Keith Visco. All Rights Reserved.
- * 
- * Contributor(s): 
+ *
+ * Contributor(s):
  * Keith Visco, kvisco@ziplink.net
  *    -- original author.
  * Larry Fitzpatrick, OpenText, lef@opentext.com
  *   -- 19990806
- *     -- moved initialization of constant shorts and chars from 
+ *     -- moved initialization of constant shorts and chars from
  *        URIUtils.cpp to here
  *
  * Peter Van der Beken
  *   -- 20000326
  *     -- added Mozilla integration code
- * 
- * $Id: URIUtils.cpp,v 1.3 2000/03/27 07:11:55 kvisco%ziplink.net Exp $
+ *
+ * $Id: URIUtils.cpp,v 1.4 2000/03/31 07:07:24 kvisco%ziplink.net Exp $
  */
 
 #include "URIUtils.h"
@@ -38,7 +38,7 @@
  * URIUtils
  * A set of utilities for handling URIs
  * @author <a href="mailto:kvisco@ziplink.net">Keith Visco</a>
- * @version $Revision: 1.3 $ $Date: 2000/03/27 07:11:55 $
+ * @version $Revision: 1.4 $ $Date: 2000/03/31 07:07:24 $
 **/
 
 //- Constants -/
@@ -98,11 +98,11 @@ istream* URIUtils::getInputStream
     else {
         // Try local files
         char* fchars = new char[xHref.length()+1];
-        ifstream* inFile = new ifstream(xHref.toChar(fchars), ios::in);
+        ifstream* inFile = new ifstream(xHref.toCharArray(fchars), ios::in);
         delete fchars;
         if ( ! *inFile ) {
             fchars = new char[href.length()+1];
-            (*inFile).open(href.toChar(fchars), ios::in);
+            (*inFile).open(href.toCharArray(fchars), ios::in);
             delete fchars;
         }
         inStream = inFile;
@@ -225,7 +225,7 @@ void URIUtils::resolveHref(String& href, String& documentBase, String& dest) {
     else {
         // Try local files
         char* xHrefChars = new char[xHref.length()+1];
-        ifstream inFile(xHref.toChar(xHrefChars), ios::in);
+        ifstream inFile(xHref.toCharArray(xHrefChars), ios::in);
         if ( inFile ) dest.append(xHref);
         else dest.append(href);
         inFile.close();
@@ -244,7 +244,7 @@ istream* URIUtils::openStream(ParsedURI* uri) {
     istream* inStream = 0;
     if ( FILE_PROTOCOL.isEqual(uri->protocol) ) {
         char* fchars = new char[uri->path.length()+1];
-        ifstream* inFile = new ifstream(uri->path.toChar(fchars), ios::in);
+        ifstream* inFile = new ifstream(uri->path.toCharArray(fchars), ios::in);
         delete fchars;
         inStream = inFile;
     }
