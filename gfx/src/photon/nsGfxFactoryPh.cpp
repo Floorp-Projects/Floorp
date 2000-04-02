16d4e3ac8a1113f2b7e25acca0941cccfb5e02ad
Jerry.Kirk%Nexwarecorp.com <Jerry.Kirk%Nexwarecorp.com>
2000-04-02 14:42:15 +0000

    Remove local copy of nsIRenderingContextImpl.h and inherit from gfx/public/nsRenderingContextImpl.h like everyone else. Photon Only.

--- gfx/src/photon/nsGfxFactoryPh.cpp
+++ gfx/src/photon/nsGfxFactoryPh.cpp
@@ -12,222 +12,154 @@
  *
  * The Original Code is mozilla.org code.
  *
- * The Initial Developer of the Original Code is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation. All
- * Rights Reserved.
+ * The Initial Developer of the Original Code is Christopher
+ * Blizzard.  Portions created by Christopher Blizzard are
+ * Copyright (C) 2000 Christopher Blizzard. All Rights Reserved.
  *
  * Contributor(s): 
+ *   Christopher Blizzzard <blizzard@mozilla.org>
  */
 
-#include "nscore.h"
-#include "nsIFactory.h"
-#include "nsISupports.h"
+#include "nsIGenericFactory.h"
+#include "nsIModule.h"
+#include "nsCOMPtr.h"
 #include "nsGfxCIID.h"
+
+#include "nsBlender.h"
 #include "nsFontMetricsPh.h"
 #include "nsRenderingContextPh.h"
-#include "nsImagePh.h"
-#include "nsDeviceContextPh.h"
-#include "nsRegionPh.h"
-#include "nsScriptableRegion.h"
-#include "nsBlender.h"
+// aka    nsDeviceContextSpecPh.h
 #include "nsDeviceContextSpecPh.h"
+// aka    nsDeviceContextSpecFactoryPh.h
 #include "nsDeviceContextSpecFactoryP.h"
+#include "nsScreenManagerPh.h"
+#include "nsScriptableRegion.h"
 #include "nsIImageManager.h"
-#include "nsPhGfxLog.h"
+#include "nsDeviceContextPh.h"
 
-static NS_DEFINE_IID(kCFontMetrics, NS_FONT_METRICS_CID);
-static NS_DEFINE_IID(kCFontEnumerator, NS_FONT_ENUMERATOR_CID);
-static NS_DEFINE_IID(kCRenderingContext, NS_RENDERING_CONTEXT_CID);
-static NS_DEFINE_IID(kCImage, NS_IMAGE_CID);
-static NS_DEFINE_IID(kCDeviceContext, NS_DEVICE_CONTEXT_CID);
-static NS_DEFINE_IID(kCRegion, NS_REGION_CID);
-static NS_DEFINE_IID(kCScriptableRegion, NS_SCRIPTABLE_REGION_CID);
+// objects that just require generic constructors
 
-static NS_DEFINE_IID(kCBlender, NS_BLENDER_CID);
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsFontMetricsPh)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsDeviceContextPh)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsRenderingContextPh)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsImagePh)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsBlender)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsRegionPh)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsDeviceContextSpecPh)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsDeviceContextSpecFactoryPh)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsFontEnumeratorPh)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsScreenManagerPh)
 
-static NS_DEFINE_IID(kISupportsIID, NS_ISUPPORTS_IID);
-static NS_DEFINE_IID(kIFactoryIID, NS_IFACTORY_IID);
+// our custom constructors
 
-static NS_DEFINE_IID(kCDeviceContextSpec, NS_DEVICE_CONTEXT_SPEC_CID);
-static NS_DEFINE_IID(kCDeviceContextSpecFactory, NS_DEVICE_CONTEXT_SPEC_FACTORY_CID);
-static NS_DEFINE_IID(kImageManagerImpl, NS_IMAGEMANAGER_CID);
-
-
-
-class nsGfxFactoryPh : public nsIFactory
-{   
-  public:   
-    // nsISupports methods
-    NS_DECL_ISUPPORTS
-
-    NS_DECL_NSIFACTORY
-
-    nsGfxFactoryPh(const nsCID &aClass);   
-    ~nsGfxFactoryPh();   
-
-  private:   
-    nsCID     mClassID;
-};   
-
-nsGfxFactoryPh::nsGfxFactoryPh(const nsCID &aClass)   
-{   
-  NS_INIT_ISUPPORTS();
-  mClassID = aClass;
-}   
-
-nsGfxFactoryPh::~nsGfxFactoryPh()   
-{   
-  PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::~nsGfxFactoryPh Destructor\n"));
-}   
-
-nsresult nsGfxFactoryPh::QueryInterface(const nsIID &aIID,   
-                                         void **aResult)   
-{   
-  if (aResult == NULL) {   
-    return NS_ERROR_NULL_POINTER;   
-  }   
-
-  // Always NULL result, in case of failure   
-  *aResult = NULL;   
-
-  if (aIID.Equals(kISupportsIID)) {   
-    *aResult = (void *)(nsISupports*)this;   
-  } else if (aIID.Equals(kIFactoryIID)) {   
-    *aResult = (void *)(nsIFactory*)this;   
-  }   
-
-  if (*aResult == NULL) {   
-    return NS_NOINTERFACE;   
-  }   
-
-  AddRef(); // Increase reference count for caller   
-  return NS_OK;   
-}   
-
-NS_IMPL_ADDREF(nsGfxFactoryPh);
-NS_IMPL_RELEASE(nsGfxFactoryPh);
-
-nsresult nsGfxFactoryPh::CreateInstance(nsISupports *aOuter,  
-                                          const nsIID &aIID,  
-                                          void **aResult)  
-{  
-  nsresult res;
-  
-  if (aResult == NULL) {  
-    return NS_ERROR_NULL_POINTER;  
-  }  
-
-  *aResult = NULL;  
-  
-  nsISupports *inst = nsnull;
-  PRBool already_addreffed = PR_FALSE;
-  
-  if (mClassID.Equals(kCFontMetrics))
-  {
-    PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::CreateInstance asking for nsFontMetricsPh.\n"));
-    inst = (nsISupports *) new nsFontMetricsPh();
-  }
-  else if (mClassID.Equals(kCDeviceContext)) {
-    PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::CreateInstance asking for nsDeviceContextPh.\n"));
-    inst = (nsISupports *)new nsDeviceContextPh();
-  }
-  else if (mClassID.Equals(kCRenderingContext)) {
-    PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::CreateInstance asking for nsRenderingContextPh.\n"));
-    inst = (nsISupports *)new nsRenderingContextPh();
-  }
-  else if (mClassID.Equals(kCImage)) {
-    PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::CreateInstance asking for nsImagePh.\n"));
-    inst = (nsISupports *)new nsImagePh();
-  }
-  else if (mClassID.Equals(kCRegion)) {
-    PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::CreateInstance asking for nsRegionPh.\n"));
-    nsRegionPh*  region;
-    NS_NEWXPCOM(region, nsRegionPh);
-    inst = (nsISupports *)region;
-  }
-  else if (mClassID.Equals(kCScriptableRegion)) {
-    PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::CreateInstance asking for kCScriptableRegion nsRegionPh.\n"));
-    nsCOMPtr<nsIRegion> rgn;
-    NS_NEWXPCOM(rgn, nsRegionPh);
-    if (rgn != nsnull) {
-      nsCOMPtr<nsIScriptableRegion> scriptableRgn = new nsScriptableRegion(rgn);
-      inst = scriptableRgn;
-    }
-  }
-  else if (mClassID.Equals(kCBlender)) {
-    inst = (nsISupports *)new nsBlender;
-  }
-  else if (mClassID.Equals(kCDeviceContextSpec)) {
-    PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::CreateInstance asking for nsDeviceContextSpecPh.\n"));
-    nsDeviceContextSpecPh* dcs;
-    NS_NEWXPCOM(dcs, nsDeviceContextSpecPh);
-    inst = (nsISupports *)dcs;
-  }
-  else if (mClassID.Equals(kCDeviceContextSpecFactory)) {
-    PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::CreateInstance asking for nsDeviceContextSpecFactoryPh.\n"));
-    nsDeviceContextSpecFactoryPh* dcs;
-    NS_NEWXPCOM(dcs, nsDeviceContextSpecFactoryPh);
-    inst = (nsISupports *)dcs;
-  }
-  else if (mClassID.Equals(kImageManagerImpl)) {
-    PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::CreateInstance asking for ImageManagerImpl.\n"));
-    nsCOMPtr<nsIImageManager> iManager;
-    res = NS_NewImageManager(getter_AddRefs(iManager));
-    already_addreffed = PR_TRUE;
-    if (NS_SUCCEEDED(res))
-    {
-      res = iManager->QueryInterface(NS_GET_IID(nsISupports), (void**)&inst);
-    }
-  }          
-  else if (mClassID.Equals(kCFontEnumerator)) {
-    PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::CreateInstance asking for FontEnumerator.\n"));
-    nsFontEnumeratorPh* fe;
-    NS_NEWXPCOM(fe, nsFontEnumeratorPh);
-    inst = (nsISupports *)fe;
-  } 
-
-  if (inst == NULL)
-  {  
-    PR_LOG(PhGfxLog, PR_LOG_ERROR,("nsGfxFactoryPh::CreateInstance Failed.\n"));
-    return NS_ERROR_OUT_OF_MEMORY;  
-  }  
-
-  if (already_addreffed == PR_FALSE)
-   NS_ADDREF(inst);
-
-  res = inst->QueryInterface(aIID, aResult);
-  NS_RELEASE(inst);
-  
-  return res;  
-}  
-
-nsresult nsGfxFactoryPh::LockFactory(PRBool aLock)  
-{  
- PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::LockFactory - Not Implmented\n"));
-
-  // Not implemented in simplest case.  
-  return NS_OK;
-}  
-
-// return the proper factory to the caller
-extern "C" NS_GFXNONXP nsresult NSGetFactory(nsISupports* servMgr,
-                                             const nsCID &aClass,
-                                             const char *aClassName,
-                                             const char *aProgID,
-                                             nsIFactory **aFactory)
+static nsresult nsScriptableRegionConstructor(nsISupports *aOuter, REFNSIID aIID, void **aResult)
 {
- PR_LOG(PhGfxLog, PR_LOG_DEBUG,("nsGfxFactoryPh::NSGetFactory\n"));
+  nsresult rv;
 
-  if (nsnull == aFactory) {
-    return NS_ERROR_NULL_POINTER;
+  nsIScriptableRegion *inst;
+
+  if ( NULL == aResult )
+  {
+    rv = NS_ERROR_NULL_POINTER;
+    return rv;
   }
-
-  *aFactory = new nsGfxFactoryPh(aClass);
-
-  if (nsnull == aFactory) {
-    return NS_ERROR_OUT_OF_MEMORY;
+  *aResult = NULL;
+  if (NULL != aOuter)
+  {
+    rv = NS_ERROR_NO_AGGREGATION;
+    return rv;
   }
+  // create an nsRegionPh and get the scriptable region from it
+  nsCOMPtr <nsIRegion> rgn;
+  NS_NEWXPCOM(rgn, nsRegionPh);
+  if (rgn != nsnull)
+  {
+    nsCOMPtr<nsIScriptableRegion> scriptableRgn = new nsScriptableRegion(rgn);
+    inst = scriptableRgn;
+  }
+  if (NULL == inst)
+  {
+    rv = NS_ERROR_OUT_OF_MEMORY;
+    return rv;
+  }
+  NS_ADDREF(inst);
+  rv = inst->QueryInterface(aIID, aResult);
+  NS_RELEASE(inst);
 
-  return (*aFactory)->QueryInterface(kIFactoryIID, (void**)aFactory);
+  return rv;
 }
+
+static nsresult nsImageManagerConstructor(nsISupports *aOuter, REFNSIID aIID, void **aResult)
+{
+    nsresult rv;
+
+  if ( NULL == aResult )
+  {
+    rv = NS_ERROR_NULL_POINTER;
+    return rv;
+  }
+  *aResult = NULL;
+  if (NULL != aOuter)
+  {
+    rv = NS_ERROR_NO_AGGREGATION;
+    return rv;
+  }
+  // this will return an image manager with a count of 1
+  rv = NS_NewImageManager((nsIImageManager **)aResult);
+  return rv;
+}
+
+static nsModuleComponentInfo components[] =
+{
+  { "Ph Font Metrics",
+    NS_FONT_METRICS_CID,
+    "mozilla.gfx.font_metrics.Ph.1",
+    nsFontMetricsPhConstructor },
+  { "Ph Device Context",
+    NS_DEVICE_CONTEXT_CID,
+    "mozilla.gfx.device_context.Ph.1",
+    nsDeviceContextPhConstructor },
+  { "Ph Rendering Context",
+    NS_RENDERING_CONTEXT_CID,
+    "mozilla.gfx.rendering_context.Ph.1",
+    nsRenderingContextPhConstructor },
+  { "Ph Image",
+    NS_IMAGE_CID,
+    "mozilla.gfx.image.Ph.1",
+    nsImagePhConstructor },
+  { "Ph Region",
+    NS_REGION_CID,
+    "mozilla.gfx.region.Ph.1",
+    nsRegionPhConstructor },
+  { "Scriptable Region",
+    NS_SCRIPTABLE_REGION_CID,
+    "mozilla.gfx.scriptable_region.1",
+    nsScriptableRegionConstructor },
+  { "Blender",
+    NS_BLENDER_CID,
+    "mozilla.gfx.blender.1",
+    nsBlenderConstructor },
+  { "Ph Device Context Spec",
+    NS_DEVICE_CONTEXT_SPEC_CID,
+    "mozilla.gfx.device_context_spec.Ph.1",
+    nsDeviceContextSpecPhConstructor },
+  { "Ph Device Context Spec Factory",
+    NS_DEVICE_CONTEXT_SPEC_FACTORY_CID,
+    "mozilla.gfx.device_context_spec_factory.Ph.1",
+    nsDeviceContextSpecFactoryPhConstructor },
+  { "Image Manager",
+    NS_IMAGEMANAGER_CID,
+    "mozilla.gfx.image_manager.1",
+    nsImageManagerConstructor },
+   { "Ph Font Enumerator",
+    NS_FONT_ENUMERATOR_CID,
+    "mozilla.gfx.font_enumerator.Ph.1",
+    nsFontEnumeratorPhConstructor },
+  { "Ph Screen Manager",
+    NS_SCREENMANAGER_CID,
+    "mozilla.gfx.screenmanager.Ph.1",
+    nsScreenManagerPhConstructor }
+};
+
+NS_IMPL_NSGETMODULE("nsGfxPhModule", components)
+
