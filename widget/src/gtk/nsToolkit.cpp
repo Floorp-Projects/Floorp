261cd2559dab9bcb1065912e5f48a67d0a9dd768
pavlov%netscape.com <pavlov%netscape.com>
2000-04-04 23:51:59 +0000

    fix for 34473

--- widget/src/gtk/nsToolkit.cpp
+++ widget/src/gtk/nsToolkit.cpp
@@ -49,11 +49,10 @@ nsToolkit::~nsToolkit()
 {
   if (mSharedGC) {
     gdk_gc_unref(mSharedGC);
-    gdk_gc_destroy(mSharedGC);
   }
 
-    // Remove the TLS reference to the toolkit...
-    PR_SetThreadPrivate(gToolkitTLSIndex, nsnull);
+  // Remove the TLS reference to the toolkit...
+  PR_SetThreadPrivate(gToolkitTLSIndex, nsnull);
 }
 
 //-------------------------------------------------------------------------
@@ -71,10 +70,9 @@ void nsToolkit::CreateSharedGC(void)
   if (mSharedGC)
     return;
 
-  pixmap = ::gdk_pixmap_new (NULL, 1, 1, gdk_rgb_get_visual()->depth);
-  mSharedGC = ::gdk_gc_new (pixmap);
-  gdk_pixmap_unref (pixmap);
-  mSharedGC = gdk_gc_ref(mSharedGC);
+  pixmap = gdk_pixmap_new(NULL, 1, 1, gdk_rgb_get_visual()->depth);
+  mSharedGC = gdk_gc_new(pixmap);
+  gdk_pixmap_unref(pixmap);
 }
 
 GdkGC *nsToolkit::GetSharedGC(void)
