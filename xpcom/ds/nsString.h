e29764292e2537fc0c5a4a78a878cdf9fb386f5f
scc@netscape.com <scc@netscape.com>
2000-04-22 08:36:28 +0000

    Added a signature previously covered by |Assign(const nsStr&)|, now |Assign( const nsString& )|.

--- xpcom/ds/nsString.h
+++ xpcom/ds/nsString.h
@@ -403,6 +403,7 @@ public:
    */
 
   void AssignWithConversion(const PRUnichar*,PRInt32=-1);
+  void AssignWithConversion( const nsString& aString )          { AssignWithConversion(aString.GetUnicode(), aString.Length()); }
   void AssignWithConversion(PRUnichar);
 
 #ifndef NEW_STRING_APIS
