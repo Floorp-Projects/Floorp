4911aacc4e9a3d76e31648ed7418e0e31b33e7f9
ducarroz%netscape.com <ducarroz%netscape.com>
2000-04-22 03:29:35 +0000

    Fix for bug 16354. Landing of the new autocomplete in mailnews. R=mscott

--- mailnews/addrbook/src/nsAbAutoCompleteSession.h
+++ mailnews/addrbook/src/nsAbAutoCompleteSession.h
@@ -23,11 +23,11 @@
 #ifndef nsAbAutoCompleteSession_h___
 #define nsAbAutoCompleteSession_h___
 
-#include "nsIAbAutoCompleteSession.h"
-#include "nsIAbAutoCompleteListener.h"
-#include "nsIMsgHeaderParser.h"
-#include "nsCOMPtr.h"
 #include "msgCore.h"
+#include "nsCOMPtr.h"
+#include "nsIMsgHeaderParser.h"
+#include "nsIAbDirectory.h"
+#include "nsIAbAutoCompleteSession.h"
 
 
 typedef struct
@@ -39,27 +39,83 @@ typedef struct
 
 #define MAX_ENTRIES 100
 
+
 class nsAbAutoCompleteSession : public nsIAbAutoCompleteSession
 {
 public:
 	NS_DECL_ISUPPORTS
+    NS_DECL_NSIAUTOCOMPLETESESSION
     NS_DECL_NSIABAUTOCOMPLETESESSION
-	nsAbAutoCompleteSession();
-	virtual ~nsAbAutoCompleteSession();
 
-protected:
-  nsresult InitializeTable();
-  nsresult PopulateTableWithAB(nsIEnumerator * aABCards); // enumerates through the cards and adds them to the table
-  PRUnichar* BuildSearchResult(PRUint32 nIndex, nsIMsgHeaderParser* parser);
-  
-  nsCOMPtr<nsIAbAutoCompleteListener> m_resultListener;
-  PRBool	m_tableInitialized;
-  nsAbStubEntry m_searchNameCompletionEntryTable[MAX_ENTRIES];
-  PRInt32   m_numEntries;
+	nsAbAutoCompleteSession();
+	virtual ~nsAbAutoCompleteSession(); 
+
+    typedef enum
+    {
+        DEFAULT_MATCH           = 0,
+        NICKNAME_EXACT_MATCH,
+        NAME_EXACT_MATCH,
+        EMAIL_EXACT_MATCH,
+        NICKNAME_MATCH,
+        NAME_MATCH,
+        EMAIL_MATCH,
+        LAST_MATCH_TYPE
+    } MatchType;
+
+protected:    
+    void ResetMatchTypeConters();
+    PRBool ItsADuplicate(PRUnichar* fullAddrStr, nsIAutoCompleteResults* results);
+    void AddToResult(const PRUnichar* pNickNameStr, const PRUnichar* pNameStr, const PRUnichar*pEmailStr, MatchType type, nsIAutoCompleteResults* results);
+    nsresult SearchCards(nsIAbDirectory* directory, const PRUnichar* searchStr, nsIAutoCompleteResults* results);
+    nsresult SearchDirectory(nsString& fileName, const PRUnichar* searchStr, nsIAutoCompleteResults* results, PRBool searchSubDirectory = PR_FALSE);
+    nsresult SearchPreviousResults(const PRUnichar *uSearchString, nsIAutoCompleteResults *previousSearchResult);
+
+    nsCOMPtr<nsIMsgHeaderParser> mParser;
+    nsString mDefaultDomain;
+    PRUint32 mMatchTypeConters[LAST_MATCH_TYPE];
 };
 
-// factory method
-extern nsresult NS_NewAbAutoCompleteSession(const nsIID &aIID, void ** aInstancePtrResult);
+
+#define NS_ABAUTOCOMPLETEPARAM_IID_STR "a3093c00-1469-11d4-a449-e55aa307c5bc"
+#define NS_ABAUTOCOMPLETEPARAM_IID \
+  {0xa3093c00, 0x1469, 0x11d4, \
+    { 0xa4, 0x49, 0xe5, 0x5a, 0xa3, 0x07, 0xc5, 0xbc }}
+
+class nsAbAutoCompleteParam : public nsISupports
+{
+public:
+	NS_DECL_ISUPPORTS
+    NS_DEFINE_STATIC_IID_ACCESSOR(NS_ABAUTOCOMPLETEPARAM_IID)
+	
+	nsAbAutoCompleteParam(const PRUnichar* nickName, const PRUnichar* userName, const PRUnichar* emailAddress, nsAbAutoCompleteSession::MatchType type)
+	{
+	    NS_INIT_REFCNT();
+	    mNickName = nsCRT::strdup(nickName);
+	    mUserName = nsCRT::strdup(userName);
+	    mEmailAddress = nsCRT::strdup(emailAddress);
+	    mType = type;
+	}
+	
+	virtual ~nsAbAutoCompleteParam()
+	{
+	    if (mNickName)
+	        nsCRT::free(mNickName);
+	    if (mUserName)
+	        nsCRT::free(mUserName);
+	    if (mEmailAddress)
+	        nsCRT::free(mEmailAddress);
+	};
+	
+protected:
+    PRUnichar* mNickName;
+    PRUnichar* mUserName;
+    PRUnichar* mEmailAddress;
+    nsAbAutoCompleteSession::MatchType  mType;
+
+public:
+    friend class nsAbAutoCompleteSession;
+};
+
 
 #endif /* nsAbAutoCompleteSession_h___ */
 
