14539e2d9fb6fc3bf0cc2f157673bc362c8c4b1b
alecf%netscape.com <alecf%netscape.com>
2000-04-24 08:04:06 +0000

    fix strres js so that the string bundle service is retrieved via getService, not createInstance() (otherwise there were 2 createinstances for every creation of a stringbundle!)

--- xpfe/global/resources/content/strres.js
+++ xpfe/global/resources/content/strres.js
@@ -1,21 +1,21 @@
 var strBundleService = null;
+var localeService = null;
 
 function srGetAppLocale()
 {
-  var localeService = null;
   var applicationLocale = null;
 
-  localeService = Components.classes["component://netscape/intl/nslocaleservice"].createInstance();
   if (!localeService) {
-    dump("\n--** localeService createInstance 1 failed **--\n");
-	return null;
+      try {
+          localeService = Components.classes["component://netscape/intl/nslocaleservice"].getService();
+      
+          localeService = localeService.QueryInterface(Components.interfaces.nsILocaleService);
+      } catch (ex) {
+          dump("\n--** localeService failed: " + ex + "\n");
+          return null;
+      }
   }
-
-  localeService = localeService.QueryInterface(Components.interfaces.nsILocaleService);
-  if (!localeService) {
-    dump("\n--** localeService createInstance 2 failed **--\n");
-	return null;
-  }
+  
   applicationLocale = localeService.GetApplicationLocale();
   if (!applicationLocale) {
     dump("\n--** localeService.GetApplicationLocale failed **--\n");
@@ -27,21 +27,17 @@ function srGetStrBundleWithLocale(path, locale)
 {
   var strBundle = null;
 
-  strBundleService =
-    Components.classes["component://netscape/intl/stringbundle"].createInstance(); 
-
   if (!strBundleService) {
-    dump("\n--** strBundleService createInstance 1 failed **--\n");
-	return null;
+      try {
+          strBundleService =
+              Components.classes["component://netscape/intl/stringbundle"].getService(); 
+          strBundleService = 
+              strBundleService.QueryInterface(Components.interfaces.nsIStringBundleService);
+      } catch (ex) {
+          dump("\n--** strBundleService failed: " + ex + "\n");
+          return null;
+      }
   }
-
-  strBundleService = 
-  	strBundleService.QueryInterface(Components.interfaces.nsIStringBundleService);
-
-  if (!strBundleService) {
-	dump("\n--** strBundleService createInstance 2 failed **--\n");
-	return null;
-  }	
 
   strBundle = strBundleService.CreateBundle(path, locale); 
   if (!strBundle) {
