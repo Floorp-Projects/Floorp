b2d83b40ae20a303342f9eeb4b8dcb66b31517af
slamm@netscape.com <slamm@netscape.com>
2000-04-27 01:02:48 +0000

    Sidebar panels file url changed from sidebar-rdf.netscape.com to sidebar.netscape.com.

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -556,7 +556,7 @@ pref("custtoolbar.Composer.Formatting_Toolbar.position", 1);
 pref("custtoolbar.Composer.Formatting_Toolbar.showing", true);
 pref("custtoolbar.Composer.Formatting_Toolbar.open", true);
 
-pref("sidebar.customize.all_panels.url", "http://sidebar-rdf.netscape.com/%LOCALE%/sidebar-rdf/%SIDEBAR_VERSION%/all-panels.rdf");
+pref("sidebar.customize.all_panels.url", "http://sidebar.netscape.com/%LOCALE%/sidebar-rdf/%SIDEBAR_VERSION%/all-panels.rdf");
 
 pref("prefs.converted-to-utf8",false);
 
