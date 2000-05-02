fb9792026fe5a1abd9c53306535343d2b5b4dbab
sfraser@netscape.com <sfraser@netscape.com>
2000-05-02 23:26:01 +0000

    First Checked In.

---
+++ xpfe/appshell/src/nsUserInfoMac.h
@@ -0,0 +1,56 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ * Seth Spitzer <sspitzer@netscape.com>
+ */
+#ifndef __nsUserInfoMac_h
+#define __nsUserInfoMac_h
+
+#include "nsIUserInfo.h"
+
+
+#include <InternetConfig.h>
+
+class nsUserInfo: public nsIUserInfo
+
+{
+public:
+              nsUserInfo(void);
+  virtual     ~nsUserInfo();
+
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIUSERINFO
+
+  
+protected:
+
+  nsresult    EnsureInitted();
+
+  static OSType      GetAppCreatorCode();
+  static PRUnichar*  PStringToNewUCS2(ConstStr255Param str);
+  
+protected:
+
+  ICInstance    mInstance;
+  PRBool        mInitted;
+  
+};
+
+#endif /* __nsUserInfo_h */
