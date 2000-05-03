bcdad23ae6522d952ecba257a6cf39d15b141135
blizzard%redhat.com <blizzard%redhat.com>
2000-05-03 21:21:18 +0000

    add some signal tests

--- embedding/browser/gtk/tests/TestGtkEmbed.cpp
+++ embedding/browser/gtk/tests/TestGtkEmbed.cpp
@@ -12,6 +12,18 @@ destroy_cb(GtkWidget *widget, gpointer data);
 static void
 activate_cb(GtkEditable *widget, gpointer data);
 
+void
+link_message_cb(GtkMozEmbed *embed, gpointer data);
+
+void
+js_status_cb(GtkMozEmbed *embed, gpointer data);
+
+void
+location_cb(GtkMozEmbed *embed, gpointer data);
+
+void
+title_cb(GtkMozEmbed *embed, gpointer data);
+
 int
 main(int argc, char **argv)
 {
@@ -36,13 +48,23 @@ main(int argc, char **argv)
 		     GTK_SIGNAL_FUNC(activate_cb), NULL);
 
   moz_embed = gtk_moz_embed_new();
+
+  gtk_signal_connect(GTK_OBJECT(moz_embed), "link_message",
+		     GTK_SIGNAL_FUNC(link_message_cb), NULL);
+  gtk_signal_connect(GTK_OBJECT(moz_embed), "js_status",
+		     GTK_SIGNAL_FUNC(js_status_cb), NULL);
+  gtk_signal_connect(GTK_OBJECT(moz_embed), "location",
+		     GTK_SIGNAL_FUNC(location_cb), NULL);
+  gtk_signal_connect(GTK_OBJECT(moz_embed), "title",
+		     GTK_SIGNAL_FUNC(title_cb), NULL);
+
   gtk_widget_set_usize(moz_embed, 100, 100);
   gtk_box_pack_end(GTK_BOX(box), moz_embed, TRUE, TRUE, 1);
   gtk_widget_show(moz_embed);
 
   if (argc > 1)
   {
-	  gtk_moz_embed_load_url(GTK_WIDGET (moz_embed), argv[1]);
+    gtk_moz_embed_load_url(GTK_WIDGET (moz_embed), argv[1]);
   }
 
   gtk_widget_show(toplevelwindow);
@@ -65,4 +87,28 @@ activate_cb(GtkEditable *widget, gpointer data)
   g_print("activate_cb.  data is %s\n", text);
   gtk_moz_embed_load_url(moz_embed, text);
   g_free(text);
+}
+
+void
+link_message_cb(GtkMozEmbed *embed, gpointer data)
+{
+  g_print("link_message_cb\n");
+}
+
+void
+js_status_cb(GtkMozEmbed *embed, gpointer data)
+{
+  g_print("js_status_cb\n");
+}
+
+void
+location_cb(GtkMozEmbed *embed, gpointer data)
+{
+  g_print("location_cb\n");
+}
+
+void
+title_cb(GtkMozEmbed *embed, gpointer data)
+{
+  g_print("title_cb\n");
 }
