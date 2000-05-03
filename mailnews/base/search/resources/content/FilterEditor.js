07ea4fe2885d9a988aaaf3b6020d8090e86f7e95
alecf%netscape.com <alecf%netscape.com>
2000-05-03 21:38:23 +0000

    initialize the dialog with all filter terms (not working just yet)

--- mailnews/base/search/resources/content/FilterEditor.js
+++ mailnews/base/search/resources/content/FilterEditor.js
@@ -84,5 +84,64 @@ function initializeDialog(filter)
     // now test by initializing the psuedo <searchterm>
     var searchTerm = document.getElementById("searchTerm");
     dump("initializing " + searchTerm + "\n");
-    searchTerm.initialize(filter, 0);
+    
+    var filterRowContainer = document.getElementById("filterListItem");
+    var numTerms = filter.numTerms;
+    for (var i=0; i<numTerms; i++) {
+      var filterRow = createFilterRow(filter, i);
+      filterRowContainer.appendChild(filterRow);
+    }
+}
+
+function createFilterRow(filter, index)
+{
+    var searchAttr = document.createElement("searchattribute");
+    var searchOp = document.createElement("searchoperator");
+    var searchVal = document.createElement("searchvalue");
+
+    // now set up ids:
+    searchAttr.id = "searchAttr" + index;
+    searchOp.id  = "searchOp" + index;
+    searchVal.id = "searchVal" + index;
+
+    searchAttr.setAttribute("for", searchOp.id + "," + searchVal.id);
+
+    var rowdata = new Array(searchAttr, searchOp, searchVal);
+    var searchrow = constructRow(rowdata);
+
+    searchrow.id = "searchRow" + index;
+
+    // should this be done with XBL or just straight JS?
+    // probably straight JS but I don't know how that's done.
+    var searchtermContainer = document.getElementById("searchterms");
+    var searchTerm = document.createElement("searchterm");
+    try { 
+      dump("created searchTerm: " + searchTerm + "\n");
+      dump("searchTerm tagname = " + searchTerm.tagName + "\n");
+    } catch (ex) {
+      dump("searchTerm dump Error: " + ex + "\n");
+    }
+    searchTerm.id = "searchTerm" + index;
+    searchTerm.searchattribute = searchAttr;
+    searchTerm.searchattribute = searchOp;
+    searchTerm.searchvalue = searchVal;
+    searchTerm.initialize(filter, index);
+
+    searchtermContainer.appendChild(searchTerm);
+
+    // now return the row
+    return searchrow;
+}
+
+// creates a <treerow> using the array treeCellChildren as 
+// the children of each treecell
+function constructRow(treeCellChildren)
+{
+    var row = document.createElement("treerow");
+    for (var i = 0; i<treeCellChildren.length; i++) {
+      var treecell = document.createElement("treecell");
+      treecell.appendChild(treeCellChildren[i]);
+      row.appendChild(treecell);
+    }
+    return row;
 }
