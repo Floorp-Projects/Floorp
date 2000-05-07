93340115b073235d54a685b09777de83403605e8
jbetak@netscape.com <jbetak@netscape.com>
2000-05-07 21:50:32 +0000

    32477 Accept-Language UI, changing location and name of the language string bundle, r=erik, ftang

--- build/mac/NGLayoutBuildList.pm
+++ build/mac/NGLayoutBuildList.pm
@@ -457,6 +457,8 @@ sub MakeResourceAliases()
 	_MakeAlias(":mozilla:intl:uconv:src:charsetTitles.properties",						"$resource_dir");
 	_MakeAlias(":mozilla:intl:uconv:src:acceptlanguage.properties",						"$resource_dir");
 	_MakeAlias(":mozilla:intl:locale:src:langGroups.properties",						"$resource_dir");
+	_MakeAlias(":mozilla:intl:locale:src:language.properties",						"$resource_dir");
+
 	_InstallResources(":mozilla:gfx:src:MANIFEST",										"$resource_dir"."gfx:");
 
 	_InstallResources(":mozilla:extensions:wallet:src:MANIFEST",						"$resource_dir");
