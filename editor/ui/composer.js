8d2c90368029b54752640bdbac37fe18487ec162
brade@netscape.com <brade@netscape.com>
2000-05-09 13:26:38 +0000

    clean up of editor prefs

--- editor/ui/composer.js
+++ editor/ui/composer.js
@@ -14,41 +14,43 @@
  *
  * The Initial Developer of the Original Code is Netscape
  * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Copyright (C) 1998-2000 Netscape Communications Corporation. All
  * Rights Reserved.
  *
  * Contributor(s): 
  */
 
-pref("editor.html_editor",                  "");
-pref("editor.image_editor",                 "");
 pref("editor.author",                       "");
+
+pref("editor.use_custom_colors",            false);
+pref("editor.text_color",                   "#000000");
+pref("editor.link_color",                   "#0000FF");
+pref("editor.active_link_color",            "#000088");
+pref("editor.followed_link_color",          "#FF0000");
+pref("editor.background_color",             "#FFFFFF");
+pref("editor.use_background_image",         false);
+localDefPref("editor.background_image",     "");
+pref("editor.use_custom_default_colors", 1);
+
+localDefPref("editor.hrule.height",         2);
+localDefPref("editor.hrule.width",          100);
+pref("editor.hrule.width_percent",          true);
+pref("editor.hrule.shading",                true);
+pref("editor.hrule.align",                  0);         // match types in edttypes.h ED_ALIGN_CENTER
+
+pref("editor.table.maintain_structure", true);
+pref("editor.table.delete_key", 0);
+
+pref("editor.prettyprint_html", 1);
+
+pref("editor.throbber.url","http://www.mozilla.org");
+
+pref("editor.auto_save",                    false);
 pref("editor.auto_save_delay",              10);    // minutes
-pref("editor.returnkey_mapping",            1);
-pref("editor.enterkey_mapping",             1);
-pref("editor.template_location",            "http://home.netscape.com/home/gold3.0_templates.html");
-pref("editor.default_template_location",    "http://home.netscape.com/home/gold3.0_templates.html");
-pref("editor.template_last_loc",            "");
-pref("editor.template_history_0",           "");
-pref("editor.template_history_1",           "");
-pref("editor.template_history_2",           "");
-pref("editor.template_history_3",           "");
-pref("editor.template_history_4",           "");
-pref("editor.template_history_5",           "");
-pref("editor.template_history_6",           "");
-pref("editor.template_history_7",           "");
-pref("editor.template_history_8",           "");
-pref("editor.template_history_9",           "");
-pref("editor.template_history_10",          "");
-pref("editor.template_history_11",          "");
-pref("editor.template_history_12",          "");
-pref("editor.template_history_13",          "");
-pref("editor.template_history_14",          "");
-pref("editor.template_history_15",          "");
-pref("editor.template_history_16",          "");
-pref("editor.template_history_17",          "");
-pref("editor.template_history_18",          "");
-pref("editor.template_history_19",          "");
+pref("editor.use_html_editor",              0);
+pref("editor.html_editor",                  "");
+pref("editor.use_image_editor",             0);
+pref("editor.image_editor",                 "");
 
 pref("editor.url_history.TITLE_0", "");
 pref("editor.url_history.TITLE_1", "");
@@ -71,109 +73,3 @@ pref("editor.url_history.URL_6", "");
 pref("editor.url_history.URL_7", "");
 pref("editor.url_history.URL_8", "");
 pref("editor.url_history.URL_9", "");
-
-localDefPref("editor.html_directory",               "");
-localDefPref("editor.image_directory",              "");
-localDefPref("editor.background_image",             "");
-
-pref("editor.last_pref_page",               0);
-pref("editor.last_text_page",               0);
-pref("editor.last_doc_page",                0);
-pref("editor.last_table_page",              0);
-pref("editor.use_custom_colors",            false);
-pref("editor.use_background_image",         false);
-pref("editor.publish_keep_links",           true);
-pref("editor.publish_keep_images",          true);
-pref("editor.show_copyright",               true);
-pref("editor.show_character_toolbar",       true);
-pref("editor.show_paragraph_toolbar",       true);
-pref("editor.text_color",                   "#000000");
-pref("editor.link_color",                   "#0000FF");
-pref("editor.active_link_color",            "#000088");
-pref("editor.followed_link_color",          "#FF0000");
-pref("editor.background_color",             "#FFFFFF");
-pref("editor.auto_save",                    false);
-pref("editor.javascript_color",             "#FF0000");
-pref("editor.color_scheme",                 "Netscape Default Colors");
-
-pref("editor.hints.bookmark",               true);
-pref("editor.hints.image",                  true);
-pref("editor.hints.sitemanager",            true);
-pref("editor.hints.copyright",              true);
-pref("editor.hints.removelinks",            true);
-
-localDefPref("editor.hrule.height",                 2);
-localDefPref("editor.hrule.width",                  100);
-pref("editor.hrule.width_percent",          true);
-pref("editor.hrule.shading",                true);
-pref("editor.hrule.align",                  0);         // match types in edttypes.h ED_ALIGN_CENTER
-
-pref("editor.publish_username",             "");
-pref("editor.publish_password",             "");
-pref("editor.publish_save_password",        false);
-pref("editor.publish_location",             "");
-pref("editor.publish_browse_location",      "");
-pref("editor.publish_last_loc",             "");
-pref("editor.publish_last_pass",            "");
-pref("editor.publish_history_0",            "");
-pref("editor.publish_history_1",            "");
-pref("editor.publish_history_2",            "");
-pref("editor.publish_history_3",            "");
-pref("editor.publish_history_4",            "");
-pref("editor.publish_history_5",            "");
-pref("editor.publish_history_6",            "");
-pref("editor.publish_history_7",            "");
-pref("editor.publish_history_8",            "");
-pref("editor.publish_history_9",            "");
-pref("editor.publish_history_10",           "");
-pref("editor.publish_history_11",           "");
-pref("editor.publish_history_12",           "");
-pref("editor.publish_history_13",           "");
-pref("editor.publish_history_14",           "");
-pref("editor.publish_history_15",           "");
-pref("editor.publish_history_16",           "");
-pref("editor.publish_history_17",           "");
-pref("editor.publish_history_18",           "");
-pref("editor.publish_history_19",           "");
-pref("editor.publish_password_0",           "");
-pref("editor.publish_password_1",           "");
-pref("editor.publish_password_2",           "");
-pref("editor.publish_password_3",           "");
-pref("editor.publish_password_4",           "");
-pref("editor.publish_password_5",           "");
-pref("editor.publish_password_6",           "");
-pref("editor.publish_password_7",           "");
-pref("editor.publish_password_8",           "");
-pref("editor.publish_password_9",           "");
-pref("editor.publish_password_10",          "");
-pref("editor.publish_password_11",          "");
-pref("editor.publish_password_12",          "");
-pref("editor.publish_password_13",          "");
-pref("editor.publish_password_14",          "");
-pref("editor.publish_password_15",          "");
-pref("editor.publish_password_16",          "");
-pref("editor.publish_password_17",          "");
-pref("editor.publish_password_18",          "");
-pref("editor.publish_password_19",          "");
-
-pref("editor.custom_color_0", "0,0,0");
-pref("editor.custom_color_1", "99,0,0");
-pref("editor.custom_color_2", "CC,0,0");
-pref("editor.custom_color_3", "FF,0,0");
-pref("editor.custom_color_4", "0,99,0");
-pref("editor.custom_color_5", "0,CC,0");
-pref("editor.custom_color_6", "0,FF,0");
-pref("editor.custom_color_7", "0,0,99");
-pref("editor.custom_color_8", "0,0,CC");
-pref("editor.custom_color_9", "0,0,FF");
-
-pref("editor.last_color_picked", "0,0,0");
-pref("editor.last_background_color_picked", "255,255,255");
-pref("editor.new_cell_has_space", true);
-
-pref("editor.throbber.url","http://www.mozilla.org");
-
-pref("editor.maintain_table_structure", true);
-pref("editor.table_delete_key", 0);
-
-pref("editor.use_custom_default_colors", 1);
