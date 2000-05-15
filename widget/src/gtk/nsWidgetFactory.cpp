a79e1873ce710f18dc15c5a67ca087bd8e98c662
pavlov%netscape.com <pavlov%netscape.com>
2000-05-15 22:32:01 +0000

    make unix widget code a component as well

--- widget/src/gtk/nsWidgetFactory.cpp
+++ widget/src/gtk/nsWidgetFactory.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
  * The contents of this file are subject to the Netscape Public
  * License Version 1.1 (the "License"); you may not use this file
@@ -12,210 +12,220 @@
  *
  * The Original Code is mozilla.org code.
  *
- * The Initial Developer of the Original Code is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation. All
- * Rights Reserved.
+ * The Initial Developer of the Original Code is Christopher
+ * Blizzard.  Portions created by Christopher Blizzard are
+ * Copyright (C) 2000 Christopher Blizzard. All Rights Reserved.
  *
  * Contributor(s): 
+ *   Christopher Blizzzard <blizzard@mozilla.org>
+ *   Stuart Parmenter <pavlov@netscape.com>
  */
 
-#include "nsIFactory.h"
-#include "nsISupports.h"
-#include "nsIButton.h"
-#include "nsITextWidget.h"
+#include "nsIGenericFactory.h"
+#include "nsIModule.h"
+#include "nsCOMPtr.h"
 #include "nsWidgetsCID.h"
 
-#include "nsToolkit.h"
 #include "nsWindow.h"
-#include "nsAppShell.h"
 #include "nsButton.h"
-#include "nsScrollbar.h"
 #include "nsCheckButton.h"
-#include "nsTextWidget.h"
+#include "nsComboBox.h"
+#include "nsRadioButton.h"
 #include "nsFileWidget.h"
-#include "nsFileSpecWithUIImpl.h"
+#include "nsListBox.h"
+#include "nsTextAreaWidget.h"
+#include "nsTextWidget.h"
+#include "nsAppShell.h"
+#include "nsToolkit.h"
 #include "nsLookAndFeel.h"
 #include "nsLabel.h"
-#include "nsFontRetrieverService.h"
-
-// Drag & Drop, Clipboard
-#include "nsClipboard.h"
 #include "nsTransferable.h"
+#include "nsClipboard.h"
 #include "nsXIFFormatConverter.h"
+#include "nsFontRetrieverService.h"
 #include "nsDragService.h"
-
+#include "nsFileSpecWithUIImpl.h"
+#include "nsScrollbar.h"
 #include "nsSound.h"
 
-static NS_DEFINE_IID(kCWindow,        NS_WINDOW_CID);
-static NS_DEFINE_IID(kCChild,         NS_CHILD_CID);
-static NS_DEFINE_IID(kCButton,        NS_BUTTON_CID);
-static NS_DEFINE_IID(kCCheckButton,   NS_CHECKBUTTON_CID);
-static NS_DEFINE_IID(kCFileOpen,      NS_FILEWIDGET_CID);
-static NS_DEFINE_IID(kCHorzScrollbar, NS_HORZSCROLLBAR_CID);
-static NS_DEFINE_IID(kCVertScrollbar, NS_VERTSCROLLBAR_CID);
-static NS_DEFINE_IID(kCTextField,     NS_TEXTFIELD_CID);
-static NS_DEFINE_IID(kCAppShell,      NS_APPSHELL_CID);
-static NS_DEFINE_IID(kCToolkit,       NS_TOOLKIT_CID);
-static NS_DEFINE_IID(kCLookAndFeel,   NS_LOOKANDFEEL_CID);
-static NS_DEFINE_IID(kCLabel,         NS_LABEL_CID);
-static NS_DEFINE_IID(kCFontRetrieverService,    NS_FONTRETRIEVERSERVICE_CID);
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsWindow)
+NS_GENERIC_FACTORY_CONSTRUCTOR(ChildWindow)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsButton)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsCheckButton)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsComboBox)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsRadioButton)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsFileWidget)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsListBox)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsTextAreaWidget)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsTextWidget)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsAppShell)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsToolkit)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsLookAndFeel)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsLabel)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsTransferable)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsClipboard)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsXIFFormatConverter)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsFontRetrieverService)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsDragService)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsFileSpecWithUIImpl)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsSound)
 
-// Drag & Drop, Clipboard
-static NS_DEFINE_IID(kCDataObj,       NS_DATAOBJ_CID);
-static NS_DEFINE_IID(kCClipboard,     NS_CLIPBOARD_CID);
-static NS_DEFINE_IID(kCTransferable,  NS_TRANSFERABLE_CID);
-static NS_DEFINE_IID(kCDataFlavor,    NS_DATAFLAVOR_CID);
-static NS_DEFINE_IID(kCXIFFormatConverter,  NS_XIFFORMATCONVERTER_CID);
-static NS_DEFINE_IID(kCDragService,   NS_DRAGSERVICE_CID);
-
-static NS_DEFINE_IID(kISupportsIID,   NS_ISUPPORTS_IID);
-static NS_DEFINE_IID(kIFactoryIID,    NS_IFACTORY_IID);
-
-// Sound services (just Beep for now)
-static NS_DEFINE_CID(kCSound,   NS_SOUND_CID);
-static NS_DEFINE_CID(kCFileSpecWithUI,   NS_FILESPECWITHUI_CID);
-
-
-class nsWidgetFactory : public nsIFactory
-{   
-public:   
-
-    NS_DECL_ISUPPORTS
-
-    NS_DECL_NSIFACTORY
-
-    nsWidgetFactory(const nsCID &aClass);   
-    virtual ~nsWidgetFactory();   
-private:
-  nsCID mClassID;
-
-};   
-
-
-
-nsWidgetFactory::nsWidgetFactory(const nsCID &aClass)   
-{   
-    NS_INIT_ISUPPORTS();
-    mClassID = aClass;
-}   
-
-nsWidgetFactory::~nsWidgetFactory()   
-{   
-}   
-
-NS_IMPL_ISUPPORTS(nsWidgetFactory, NS_GET_IID(nsIFactory))
-
-nsresult nsWidgetFactory::CreateInstance(nsISupports *aOuter,  
-                                          const nsIID &aIID,  
-                                          void **aResult)  
-{  
-    if (aResult == NULL) {  
-        return NS_ERROR_NULL_POINTER;  
-    }  
-
-    *aResult = NULL;  
-
-    if (nsnull != aOuter)
-      return NS_ERROR_NO_AGGREGATION;
-  
-    nsISupports *inst = nsnull;
-    if (mClassID.Equals(kCWindow)) {
-      inst = (nsISupports *)(nsBaseWidget*)new nsWindow();
-    }
-    else if (mClassID.Equals(kCChild)) {
-      inst = (nsISupports *)(nsBaseWidget*)new ChildWindow();
-    }
-    else if (mClassID.Equals(kCButton)) {
-      inst = (nsISupports*)(nsBaseWidget*)(nsWidget *)new nsButton();
-    }
-    else if (mClassID.Equals(kCCheckButton)) {
-        inst = (nsISupports*)(nsBaseWidget*)(nsWidget *)new nsCheckButton();
-    }
-    else if (mClassID.Equals(kCFileOpen)) {
-        inst = (nsISupports*)new nsFileWidget();
-    }
-    else if (mClassID.Equals(kCHorzScrollbar)) {
-        inst = (nsISupports*)(nsBaseWidget*)(nsWidget *)new nsScrollbar(PR_FALSE);
-    }
-    else if (mClassID.Equals(kCVertScrollbar)) {
-        inst = (nsISupports*)(nsBaseWidget*)(nsWidget *)new nsScrollbar(PR_TRUE);
-    }
-    else if (mClassID.Equals(kCTextField)) {
-        inst = (nsISupports*)(nsBaseWidget*)(nsWidget *)new nsTextWidget();
-    }
-    else if (mClassID.Equals(kCAppShell)) {
-        inst = (nsISupports*)new nsAppShell();
-    }
-    else if (mClassID.Equals(kCToolkit)) {
-        inst = (nsISupports*)(nsBaseWidget*)(nsWidget *)new nsToolkit();
-    }
-    else if (mClassID.Equals(kCLookAndFeel)) {
-        inst = (nsISupports*)(nsBaseWidget*)(nsWidget *)new nsLookAndFeel();
-    }
-    else if (mClassID.Equals(kCLabel)) {
-        inst = (nsISupports*)(nsBaseWidget*)(nsWidget *)new nsLabel();
-    }
-    else if (mClassID.Equals(kCSound)) {
-    	nsISound* aSound = nsnull;
-    	NS_NewSound(&aSound);
-        inst = (nsISupports*) aSound;
-    }
-    else if (mClassID.Equals(kCTransferable)) {
-        inst = (nsISupports*)new nsTransferable();
-    }
-    else if (mClassID.Equals(kCClipboard)) {
-        inst = (nsISupports*)new nsClipboard();
-    }
-    else if (mClassID.Equals(kCXIFFormatConverter))
-        inst = (nsISupports*)new nsXIFFormatConverter();
-    else if (mClassID.Equals(kCFontRetrieverService))
-        inst = (nsISupports*)(nsIFontRetrieverService *) new nsFontRetrieverService();
-    else if (mClassID.Equals(kCDragService))
-        inst = (nsISupports*) (nsIDragService *) new nsDragService();
-    else if (mClassID.Equals(kCFileSpecWithUI))
-    	inst = (nsISupports*) (nsIFileSpecWithUI *) new nsFileSpecWithUIImpl;
-    else {
-        printf("nsWidgetFactory::CreateInstance(), unhandled class.\n");
-    }
-  
-    if (inst == NULL) {  
-        return NS_ERROR_OUT_OF_MEMORY;  
-    }  
-
-    NS_ADDREF(inst);
-    nsresult res = inst->QueryInterface(aIID, aResult);
-    NS_RELEASE(inst);
-
-    return res;
-
-}  
-
-nsresult nsWidgetFactory::LockFactory(PRBool aLock)  
-{  
-    // Not implemented in simplest case.  
-    return NS_OK;
-}  
-
-// return the proper factory to the caller
-extern "C" NS_WIDGET nsresult 
-NSGetFactory(nsISupports* serviceMgr,
-             const nsCID &aClass,
-             const char *aClassName,
-             const char *aProgID,
-             nsIFactory **aFactory)
+static nsresult nsHorizScrollbarConstructor (nsISupports *aOuter, REFNSIID aIID, void **aResult)
 {
-    if (nsnull == aFactory) {
-        return NS_ERROR_NULL_POINTER;
-    }
+  nsresult rv;
+  nsISupports *inst = nsnull;
 
-    *aFactory = new nsWidgetFactory(aClass);
+  if ( NULL == aResult )
+  {
+    rv = NS_ERROR_NULL_POINTER;
+    return rv;
+  }
+  *aResult = NULL;
+  if (NULL != aOuter)
+  {
+    rv = NS_ERROR_NO_AGGREGATION;
+    return rv;
+  }
+  
+  inst = (nsISupports *)(nsBaseWidget *)(nsWidget *)new nsScrollbar(PR_FALSE);
+  if (inst == NULL)
+  {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+  NS_ADDREF(inst);
+  rv = inst->QueryInterface(aIID, aResult);
+  NS_RELEASE(inst);
 
-    if (nsnull == aFactory) {
-        return NS_ERROR_OUT_OF_MEMORY;
-    }
-
-    return (*aFactory)->QueryInterface(kIFactoryIID, (void**)aFactory);
+  return rv;
 }
 
+static nsresult nsVertScrollbarConstructor (nsISupports *aOuter, REFNSIID aIID, void **aResult)
+{
+  nsresult rv;
+  nsISupports *inst = nsnull;
 
+  if ( NULL == aResult )
+  {
+    rv = NS_ERROR_NULL_POINTER;
+    return rv;
+  }
+  *aResult = NULL;
+  if (NULL != aOuter)
+  {
+    rv = NS_ERROR_NO_AGGREGATION;
+    return rv;
+  }
+  
+  inst = (nsISupports *)(nsBaseWidget *)(nsWidget *)new nsScrollbar(PR_TRUE);
+  if (inst == NULL)
+  {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+  NS_ADDREF(inst);
+  rv = inst->QueryInterface(aIID, aResult);
+  NS_RELEASE(inst);
+
+  return rv;
+}
+
+static nsModuleComponentInfo components[] =
+{
+  { "Gtk nsWindow",
+    NS_WINDOW_CID,
+    "mozilla.widgets.window.gtk.1",
+    nsWindowConstructor },
+  { "Gtk Child nsWindow",
+    NS_CHILD_CID,
+    "mozilla.widgets.child_window.gtk.1",
+    ChildWindowConstructor },
+  { "Gtk Button",
+    NS_BUTTON_CID,
+    "mozilla.widgets.button.gtk.1",
+    nsButtonConstructor },
+  { "Gtk Check Button",
+    NS_CHECKBUTTON_CID,
+    "mozilla.widgets.checkbutton.gtk.1",
+    nsCheckButtonConstructor },
+  { "Gtk Combobox",
+    NS_COMBOBOX_CID,
+    "mozilla.widgets.combobox.gtk.1",
+    nsComboBoxConstructor },
+  { "Gtk Radio Button",
+    NS_RADIOBUTTON_CID,
+    "mozilla.widgets.radiobutton.gtk.1",
+    nsRadioButtonConstructor },
+  { "Gtk File Widget",
+    NS_FILEWIDGET_CID,
+    "mozilla.widgets.filewidget.gtk.1",
+    nsFileWidgetConstructor },
+  { "Gtk List Box",
+    NS_LISTBOX_CID,
+    "mozilla.widgets.listbox.gtk.1",
+    nsListBoxConstructor },
+  { "Gtk Horiz Scrollbar",
+    NS_HORZSCROLLBAR_CID,
+    "mozilla.widgets.horizscroll.gtk.1",
+    nsHorizScrollbarConstructor },
+  { "Gtk Vert Scrollbar",
+    NS_VERTSCROLLBAR_CID,
+    "mozilla.widgets.vertscroll.gtk.1",
+    nsVertScrollbarConstructor },
+  { "Gtk Text Area",
+    NS_TEXTAREA_CID,
+    "mozilla.widgets.textarea.gtk.1",
+    nsTextAreaWidgetConstructor },
+  { "Gtk Text Widget",
+    NS_TEXTFIELD_CID,
+    "mozilla.widgets.textwidget.gtk.1",
+    nsTextWidgetConstructor },
+  { "Gtk AppShell",
+    NS_APPSHELL_CID,
+    "mozilla.widget.appshell.gtk.1",
+    nsAppShellConstructor },
+  { "Gtk Toolkit",
+    NS_TOOLKIT_CID,
+    "mozilla.widget.toolkit.gtk.1",
+    nsToolkitConstructor },
+  { "Gtk Look And Feel",
+    NS_LOOKANDFEEL_CID,
+    "mozilla.widget.lookandfeel.gtk.1",
+    nsLookAndFeelConstructor },
+  { "Gtk Label",
+    NS_LABEL_CID,
+    "mozilla.widget.label.gtk.1",
+    nsLabelConstructor },
+  { "Gtk Sound",
+    NS_SOUND_CID,
+    //    "mozilla.widget.sound.gtk.1"
+    "component://netscape/sound",
+    nsSoundConstructor },
+  { "Transferrable",
+    NS_TRANSFERABLE_CID,
+    //    "mozilla.widget.transferrable.gtk.1",
+    "component://netscape/widget/transferable",
+    nsTransferableConstructor },
+  { "Gtk Clipboard",
+    NS_CLIPBOARD_CID,
+    //    "mozilla.widget.clipboard.gtk.1",
+    "component://netscape/widget/clipboard",
+    nsClipboardConstructor },
+  { "XIF Format Converter",
+    NS_XIFFORMATCONVERTER_CID,
+    "mozilla.widget.xifformatconverter.gtk.1",
+    nsXIFFormatConverterConstructor },
+  { "Gtk Font Retriever Service",
+    NS_FONTRETRIEVERSERVICE_CID,
+    "mozilla.widget.fontretrieverservice.gtk.1",
+    nsFontRetrieverServiceConstructor },
+  { "Gtk Drag Service",
+    NS_DRAGSERVICE_CID,
+    //    "mozilla.widget.dragservice.gtk.1",
+    "component://netscape/widget/dragservice",
+    nsDragServiceConstructor },
+  { "File Spec with UI",
+    NS_FILESPECWITHUI_CID,
+    "mozilla.widget.filespecwithui.gtk.1",
+    nsFileSpecWithUIImplConstructor }
+};
+
+NS_IMPL_NSGETMODULE("nsWidgetGTKModule", components)
