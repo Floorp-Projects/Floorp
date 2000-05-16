3d8b1164e9b3eb2d2238eb9c923ad7b4f5712bf1
kvisco%ziplink.net <kvisco%ziplink.net>
2000-05-16 09:34:01 +0000

    Fixed a seg fault when '/' is used as a div operator, which is an error so I also added an error message for this case

--- extensions/transformiix/source/xpath/parser.mk
+++ extensions/transformiix/source/xpath/parser.mk
@@ -1,30 +1,38 @@
 
 CC := g++ -g
 
-ROOT_PATH    = ../..
+ROOT_PATH    = ..
 BASE_PATH    = $(ROOT_PATH)/base
 XML_PATH     = $(ROOT_PATH)/xml
 DOM_PATH     = $(XML_PATH)/dom
-XSL_PATH     = $(ROOT_PATH)/xsl
-XSLUTIL_PATH  = $(XSL_PATH)/util
+XSLT_PATH    = $(ROOT_PATH)/xslt
 
-INCLUDE_PATHS = -I. \
-                -I$(BASE_PATH) \
-                -I$(XSLUTIL_PATH) \
-                -I$(DOM_PATH) \
-                -I$(XML_PATH) \
-                -I$(XSL_PATH) \
+INCLUDE_PATHS = -I.             \
+                -I$(BASE_PATH)  \
+                -I$(DOM_PATH)   \
+                -I$(XML_PATH)   \
+                -I$(XSLT_PATH)  \
                 -I-
 
-OBJS = *.o \
-       $(BASE_PATH)/*.o \
-       $(XSLUTIL_PATH)/*.o \
-       $(DOM_PATH)/*.o \
-       $(XML_PATH)/*.o \
-       $(XSL_PATH)/Names.o
+OBJS = *.o                    \
+       $(BASE_PATH)/*.o       \
+       $(DOM_PATH)/*.o        \
+       $(XML_PATH)/*.o        \
+       $(XSLT_PATH)/Names.o
+
 
 
 
 target:
-	$(CC) $(INCLUDE_PATHS) $(OBJS) parser.cpp -o parser.exe
+	$(CC) $(INCLUDE_PATHS) $(OBJS) Parser.cpp -o parser.exe
+
+
+
+
+
+
+
+
+
+
 
