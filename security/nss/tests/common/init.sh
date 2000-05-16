6308342d98e49f6c6ed42542f71b6330e970a0a1
relyea@netscape.com <relyea@netscape.com>
2000-05-16 17:50:52 +0000

    Add a mini-framework to allow us to test NSS releases without using tet.

---
+++ security/nss/tests/common/init.sh
@@ -0,0 +1,59 @@
+#! /bin/sh
+#
+#  Initialize a bunch of variables that may tests would be interested in
+#
+#
+mozilla_root=`(cd ../../../..; pwd)`
+common=`(cd ../common; pwd)`
+MOZILLA_ROOT=${SECURITY_ROOT-$mozilla_root}
+COMMON=${TEST_COMMON-$common}
+DIST=${DIST-${MOZILLA_ROOT}/dist}
+SECURITY_ROOT=${DIST-${MOZILLA_ROOT}/security/nss}
+TESTDIR=${TESTDIR-${MOZILLA_ROOT}/tests_results/security}
+. ../common/arch.sh
+OBJDIR=${OBJDIR_NAME}
+PATH=${DIST}/${OBJDIR}/bin:${DIST}/${OBJDIR}/lib:$PATH
+export PATH
+LD_LIBRARY_PATH=${DIST}/${OBJDIR}/lib
+export LD_LIBRARY_PATH
+echo "Creating ${TESTDIR}"
+if [ ! -d ${TESTDIR} ]; then
+   mkdir -p ${TESTDIR}
+fi
+
+if [ ! -s "${HOSTDIR}" ]; then
+    version=1
+    if [ -f ${TESTDIR}/${HOST} ]; then
+	version=`cat ${TESTDIR}/${HOST}`
+    fi
+    expr $version + 1 > ${TESTDIR}/${HOST}
+
+    HOSTDIR=${TESTDIR}/${HOST}'.'$version
+fi
+
+if [ ! -d ${HOSTDIR} ]; then
+   mkdir -p ${HOSTDIR}
+fi
+
+RESULTS=${HOSTDIR}/results.html
+if [ ! -f "${RESULTS}" ]; then
+
+	cp ${COMMON}/results_header.html ${RESULTS}
+	echo "<H4>Platform: ${OBJDIR}<BR>" >> ${RESULTS}
+	echo "Test Run: ${HOST}.$version</H4>" >> ${RESULTS}
+	echo "<HR><BR>" >> ${RESULTS}
+fi
+
+echo "********************************************"
+echo "   Platform: ${OBJDIR}"
+echo "   Results: ${HOST}.$version"
+echo "********************************************"
+
+KILL="kill"
+if  [ ${OS_ARCH} = "Linux" ]; then
+        KILL="kill -kill"
+fi
+
+export  KILL
+
+
