89b088a4e9f8c823665dfc3f1e814cba3fe37aad
pollmann@netscape.com <pollmann@netscape.com>
2000-05-16 22:53:35 +0000

    Adding html iframe regression tests, not part of any build

--- layout/html/tests/frameset/runtests.sh
+++ layout/html/tests/frameset/runtests.sh
@@ -3,10 +3,15 @@
 # create test file to use first; since we don't know where the tree
 # is, and we need full pathnames in the file, we create it on the fly.
 
-viewer=$MOZ_SRC/mozilla/dist/bin/viewer
-testsfile=/tmp/$$-tests.txt
+if test "$ARCH"x = 'winx'; then
+ viewer=$MOZ_SRC/mozilla/dist/WIN32_D.OBJ/bin/viewer.exe
+ testsfile="file_list.txt"
+else
+ viewer=$MOZ_SRC/mozilla/dist/bin/viewer
+ testsfile=$$-tests.txt
+ sed -e "s@file:///s|@file:$MOZ_SRC@" < file_list.txt > $testsfile
+fi
 
-sed -e "s@file:///s|@file:$MOZ_SRC@" < file_list.txt > $testsfile
 
 if test "$1"x = "baselinex"; then
   rm -r -f baseline
