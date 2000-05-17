9d2a9a852d1442b8879e227d0c2d33bae1c6f225
zuperdee@penguinpowered.com <zuperdee@penguinpowered.com>
2000-05-17 04:47:00 +0000

    Temporary fix for bug 33801.  r=shaver

--- gfx/src/beos/nsDeviceContextSpecB.cpp
+++ gfx/src/beos/nsDeviceContextSpecB.cpp
@@ -78,9 +78,9 @@ NS_IMETHODIMP nsDeviceContextSpecBeOS :: Init(PRBool	aQuiet)
 
   if ( ( path = getenv( "PWD" ) ) == (char *) NULL ) 
 	if ( ( path = getenv( "HOME" ) ) == (char *) NULL )
-  		strcpy( mPrData.path, "netscape.ps" );
+  		strcpy( mPrData.path, "mozilla.ps" );
   if ( path != (char *) NULL )
-	sprintf( mPrData.path, "%s/netscape.ps", path );
+	sprintf( mPrData.path, "%s/mozilla.ps", path );
   else
 	return NS_ERROR_FAILURE;
 
