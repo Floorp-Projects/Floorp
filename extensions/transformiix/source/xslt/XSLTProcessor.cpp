0fbd2b2a0e78309fff1d0654b79a2abee1346167
kvisco@ziplink.net <kvisco@ziplink.net>
2000-05-18 03:59:17 +0000

    Added special case for handling xsl:copy-of when the node is a document node, this is a fix to a bug reported by Olivier Gerardin

--- extensions/transformiix/source/xslt/XSLTProcessor.cpp
+++ extensions/transformiix/source/xslt/XSLTProcessor.cpp
@@ -35,7 +35,7 @@
  * Nathan Pride, npride@wavo.com
  *    -- fixed a document base issue
  *
- * $Id: XSLTProcessor.cpp,v 1.11 2000/05/02 22:03:54 Peter.VanderBeken%pandora.be Exp $
+ * $Id: XSLTProcessor.cpp,v 1.12 2000/05/18 03:59:17 kvisco%ziplink.net Exp $
  */
 
 #include "XSLTProcessor.h"
@@ -48,7 +48,7 @@
 /**
  * XSLTProcessor is a class for Processing XSL styelsheets
  * @author <a href="mailto:kvisco@ziplink.net">Keith Visco</a>
- * @version $Revision: 1.11 $ $Date: 2000/05/02 22:03:54 $
+ * @version $Revision: 1.12 $ $Date: 2000/05/18 03:59:17 $
 **/
 
 /**
@@ -1568,7 +1568,17 @@ void XSLTProcessor::xslCopyOf(ExprResult* exprResult, ProcessorState* ps) {
             NodeSet* nodes = (NodeSet*)exprResult;
             for (int i = 0; i < nodes->size();i++) {
                 Node* node = nodes->get(i);
-                ps->addToResultTree(XMLDOMUtils::copyNode(node, resultDoc));
+                //-- handle special case of copying another document into
+                //-- the result tree
+                if (node->getNodeType() == Node::DOCUMENT_NODE) {
+                    Node* child = node->getFirstChild();
+                    while (child) {
+                        ps->addToResultTree(XMLDOMUtils::copyNode(child, resultDoc));
+                        child = child->getNextSibling();
+                    }
+                }
+                //-- otherwise just copy node
+                else ps->addToResultTree(XMLDOMUtils::copyNode(node, resultDoc));
             }
             break;
         }
