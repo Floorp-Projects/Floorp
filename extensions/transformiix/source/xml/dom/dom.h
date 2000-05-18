b8757de4233e059689a175d7fd2d4c6aa5abd491
kvisco%ziplink.net <kvisco%ziplink.net>
2000-05-18 09:17:41 +0000

    this file is used to point to the correct dom.h

---
+++ extensions/transformiix/source/xml/dom/dom.h
@@ -0,0 +1,28 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Transformiix XSLT processor.
+ *
+ * Contributor(s):
+ *
+ * Peter Van der Beken
+ *    -- original author
+ *
+ * $Id: dom.h,v 1.6 2000/05/18 09:17:41 kvisco%ziplink.net Exp $
+ */
+
+#ifndef MOZILLA
+#include "standalone/dom.h"
+#else
+#include "mozImpl/dom.h"
+#endif
+
+
