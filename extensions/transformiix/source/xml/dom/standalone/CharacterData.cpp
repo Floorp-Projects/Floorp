d45443a8378b2ff1a40a178c936c902d81e60904
kvisco@ziplink.net <kvisco@ziplink.net>
2000-05-18 08:31:46 +0000

    moved from source/dom

---
+++ extensions/transformiix/source/xml/dom/standalone/CharacterData.cpp
@@ -0,0 +1,113 @@
+/*
+ * (C) Copyright The MITRE Corporation 1999  All rights reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * The program provided "as is" without any warranty express or
+ * implied, including the warranty of non-infringement and the implied
+ * warranties of merchantibility and fitness for a particular purpose.
+ * The Copyright owner will not be liable for any damages suffered by
+ * you as a result of using the Program. In no event will the Copyright
+ * owner be liable for any special, indirect or consequential damages or
+ * lost profits even if the Copyright owner has been advised of the
+ * possibility of their occurrence.
+ *
+ * Please see release.txt distributed with this file for more information.
+ *
+ */
+// Tom Kneeland (3/29/99)
+//
+//  Implementation of the Document Object Model Level 1 Core
+//    Implementation of the CharacterData class
+//
+// Modification History:
+// Who  When      What
+// TK   03/29/99  Created
+//
+
+#include "dom.h"
+
+//
+//Protected constructor.  Just pass parameters onto NodeDefinition.
+//
+CharacterData::CharacterData(NodeType type, const DOMString& name,
+                             const DOMString& value, Document* owner) :
+               NodeDefinition(type, name, value, owner)
+{
+}
+
+//
+//Return a constant reference to the data stored by this object.
+//
+const DOMString& CharacterData::getData() const
+{
+  return nodeValue;
+}
+
+//
+//Set the data stored by this object to the string represented by "source".
+//
+void CharacterData::setData(const DOMString& source)
+{
+  nodeValue = source;
+}
+
+//
+//Returns the length of the data object.
+//
+Int32 CharacterData::getLength() const
+{
+  return nodeValue.length();
+}
+
+//
+//Retreive the substring starting at offset anc ending count number of
+//characters away.
+//    NOTE:  An empty string will be returned in the event of an error.
+//
+DOMString& CharacterData::substringData(Int32 offset, Int32 count, DOMString& dest)
+{
+  if ((offset >= 0) && (offset < nodeValue.length()) && (count > 0))
+    return nodeValue.subString(offset, offset+count, dest);
+  else
+    {
+    dest.clear();
+    return dest;
+    }
+}
+
+void CharacterData::appendData(const DOMString& arg)
+{
+  nodeValue.append(arg);
+}
+
+void CharacterData::insertData(Int32 offset, const DOMString& arg)
+{
+  if ((offset >= 0) && (offset < nodeValue.length()))
+    nodeValue.insert(offset, arg);
+}
+
+void CharacterData::deleteData(Int32 offset, Int32 count)
+{
+  if ((offset >= 0) && (offset < nodeValue.length()) && (count > 0))
+    nodeValue.deleteChars(offset, count);
+}
+
+void CharacterData::replaceData(Int32 offset, Int32 count, const DOMString& arg)
+{
+  DOMString tempString;
+
+  if ((offset >= 0) && (offset < nodeValue.length()) && (count > 0))
+    {
+      if (count < arg.length())
+        {
+        tempString = arg.subString(0, count, tempString);
+        nodeValue.replace(offset, tempString);
+        }
+      else
+        nodeValue.replace(offset, arg);
+    }
+}
---
+++ extensions/transformiix/source/xml/dom/standalone/Text.cpp
@@ -0,0 +1,93 @@
+/*
+ * (C) Copyright The MITRE Corporation 1999  All rights reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * The program provided "as is" without any warranty express or
+ * implied, including the warranty of non-infringement and the implied
+ * warranties of merchantibility and fitness for a particular purpose.
+ * The Copyright owner will not be liable for any damages suffered by
+ * you as a result of using the Program. In no event will the Copyright
+ * owner be liable for any special, indirect or consequential damages or
+ * lost profits even if the Copyright owner has been advised of the
+ * possibility of their occurrence.
+ *
+ * Please see release.txt distributed with this file for more information.
+ *
+ */
+// Tom Kneeland (3/29/99)
+//
+//  Implementation of the Document Object Model Level 1 Core
+//    Implementation of the Text class
+//
+// Modification History:
+// Who  When      What
+// TK   03/29/99  Created
+//
+
+#include "dom.h"
+
+//
+//Construct a text object with the specified document owner and data
+//
+Text::Text(const DOMString& theData, Document* owner) :
+      CharacterData(Node::TEXT_NODE, "#text", theData, owner)
+{
+}
+
+//
+//Protected constructor for children of the Text Class.  Currently only
+//CDATASection needs to use this function.
+Text::Text(NodeType type, const DOMString& name, const DOMString& value,
+         Document* owner) :
+      CharacterData(type, name, value, owner)
+{
+}
+
+
+//
+//Split the text node at Offset into two siblings.  Return a pointer to the new
+//sibling.
+//
+Text* Text::splitText(Int32 offset)
+{
+  Text* newTextSibling = NULL;
+  DOMString newData;
+
+  if ((offset >= 0) && (offset < nodeValue.length()))
+    {
+      newTextSibling = getOwnerDocument()->createTextNode(nodeValue.subString(offset, newData));
+      getParentNode()->insertBefore(newTextSibling, getNextSibling());
+      nodeValue.deleteChars(offset, nodeValue.length() - offset);
+    }
+
+  return newTextSibling;
+}
+
+//
+//Text nodes can not have any children, so just return null from all child
+//manipulation functions.
+//
+
+Node* Text::insertBefore(Node* newChild, Node* refChild)
+{
+  return NULL;
+}
+
+Node* Text::replaceChild(Node* newChild, Node* oldChild)
+{
+  return NULL;
+}
+
+Node* Text::removeChild(Node* oldChild)
+{
+  return NULL;
+}
+
+Node* Text::appendChild(Node* newChild)
+{
+  return NULL;
+}
