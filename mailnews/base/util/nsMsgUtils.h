8ca2d0d7ca10214b24c6fdad947f5ba49c953c93
sspitzer%netscape.com <sspitzer%netscape.com>
2000-05-19 22:38:07 +0000

    last bit of the fix for #39172, i18n friendly subscribe dialog

--- mailnews/base/util/nsMsgUtils.h
+++ mailnews/base/util/nsMsgUtils.h
@@ -69,7 +69,7 @@ NS_MSG_BASE nsresult NS_MsgHashIfNecessary(nsCAutoString &name);
 
 NS_MSG_BASE nsresult NS_MsgCreatePathStringFromFolderURI(const char *folderURI, nsCString& pathString);
 
-nsresult CreateUnicodeStringFromUtf7(const char *aSourceString, PRUnichar **result);
+NS_MSG_BASE nsresult CreateUnicodeStringFromUtf7(const char *aSourceString, PRUnichar **result);
 
 #endif
 
