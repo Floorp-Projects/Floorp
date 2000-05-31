5243fc1acda6c8eee5093ae9d7136244aba37079
relyea%netscape.com <relyea%netscape.com>
2000-05-31 22:34:07 +0000

    Fix Environment variable overrides.

--- security/nss/tests/common/init.sh
+++ security/nss/tests/common/init.sh
@@ -5,10 +5,10 @@
 #
 mozilla_root=`(cd ../../../..; pwd)`
 common=`(cd ../common; pwd)`
-MOZILLA_ROOT=${SECURITY_ROOT-$mozilla_root}
+MOZILLA_ROOT=${MOZILLA_ROOT-$mozilla_root}
 COMMON=${TEST_COMMON-$common}
 DIST=${DIST-${MOZILLA_ROOT}/dist}
-SECURITY_ROOT=${DIST-${MOZILLA_ROOT}/security/nss}
+SECURITY_ROOT=${SECURITY_ROOT-${MOZILLA_ROOT}/security/nss}
 TESTDIR=${TESTDIR-${MOZILLA_ROOT}/tests_results/security}
 . ../common/arch.sh
 OBJDIR=${OBJDIR_NAME}
@@ -42,12 +42,13 @@ if [ ! -f "${RESULTS}" ]; then
 	echo "<H4>Platform: ${OBJDIR}<BR>" >> ${RESULTS}
 	echo "Test Run: ${HOST}.$version</H4>" >> ${RESULTS}
 	echo "<HR><BR>" >> ${RESULTS}
+
+	echo "********************************************"
+	echo "   Platform: ${OBJDIR}"
+	echo "   Results: ${HOST}.$version"
+	echo "********************************************"
 fi
 
-echo "********************************************"
-echo "   Platform: ${OBJDIR}"
-echo "   Results: ${HOST}.$version"
-echo "********************************************"
 
 KILL="kill"
 if  [ ${OS_ARCH} = "Linux" ]; then
