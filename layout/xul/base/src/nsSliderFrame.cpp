44f76a39df7ecce257c1dba25fd5397a4ab8cfb7
evaughan@netscape.com <evaughan@netscape.com>
2000-06-03 02:12:19 +0000

    Just adding comments.

--- layout/xul/base/src/nsSliderFrame.cpp
+++ layout/xul/base/src/nsSliderFrame.cpp
@@ -17,7 +17,11 @@
  * Copyright (C) 1998 Netscape Communications Corporation. All
  * Rights Reserved.
  *
+ * AUTHOR: Eric Vaughan
+ *
  * Contributor(s): 
+ * Tomi Leppikangas 
+ * Davor Cubranic
  */
 
 //
