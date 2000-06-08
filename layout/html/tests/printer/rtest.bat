ec2aba8883ecf2aa768952d5fb73dc3a27b89159
dcone@netscape.com <dcone@netscape.com>
2000-06-08 13:45:15 +0000

    Update printing regression tests.  r=McCluskey

--- layout/html/tests/printer/rtest.bat
+++ layout/html/tests/printer/rtest.bat
@@ -1,13 +1,14 @@
 @echo off
 
+cd general
+call rtest.bat %1
+cd..
+
 cd table
 call rtest.bat %1
 cd..
 
-cd images
-call rtest.bat %1
-cd..
+rem cd images
+rem call rtest.bat %1
+rem cd..
 
-cd general
-call rtest.bat %1
-cd..
