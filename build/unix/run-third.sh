d7eec2b255ed55c6e2f49d43de590cb10c8a9245
jim_nance@yahoo.com <jim_nance@yahoo.com>
2000-06-10 10:56:51 +0000

    Tru64 helper scripts for ATOM tools. Not part of the build.

--- build/unix/run-third.sh
+++ build/unix/run-third.sh
@@ -6,13 +6,13 @@ export LD_LIBRARY_PATH
 PROG=mozilla-bin
 PLIBS="-L."
 
-TOOL=hiprof
+TOOL=third
 
 for l in ./*.so components/*.so; do
     PLIBS="$PLIBS -incobj $l"
 done
 
-$ECHO atom $PROG -tool $TOOL -env threads -toolargs="-calltime -systime" -all $PLIBS
+$ECHO atom $PROG -tool $TOOL -env threads -g -all $PLIBS -toolargs="-leaks all -before NS_ShutdownXPCOM"
 
 cd components && (
     for f in lib*.so; do
