09ca9cf1cedc8027ebb6768e456d7bcae06bc818
axel%pike.org <axel%pike.org>
2000-06-13 12:10:09 +0000

    Code cleanup, not part of build, needed for SUN Workshop compilers

--- extensions/transformiix/source/xml/parser/xmltok/xmltok.h
+++ extensions/transformiix/source/xml/parser/xmltok/xmltok.h
@@ -190,15 +190,12 @@ literals, comments and processing instructions.
 
 #define XmlPrologTok(enc, ptr, end, nextTokPtr) \
   (((enc)->scanners[XML_PROLOG_STATE]) (enc, ptr, end, nextTokPtr))
-//   XmlTok(enc, XML_PROLOG_STATE, ptr, end, nextTokPtr)
 
 #define XmlContentTok(enc, ptr, end, nextTokPtr) \
   (((enc)->scanners[XML_CONTENT_STATE]) (enc, ptr, end, nextTokPtr))
-//   XmlTok(enc, XML_CONTENT_STATE, ptr, end, nextTokPtr)
 
 #define XmlCdataSectionTok(enc, ptr, end, nextTokPtr) \
   (((enc)->scanners[XML_CDATA_SECTION_STATE]) (enc, ptr, end, nextTokPtr))
-//   XmlTok(enc, XML_CDATA_SECTION_STATE, ptr, end, nextTokPtr)
 
 /* This is used for performing a 2nd-level tokenization on
 the content of a literal that has already been returned by XmlTok. */ 
@@ -208,11 +205,9 @@ the content of a literal that has already been returned by XmlTok. */
 
 #define XmlAttributeValueTok(enc, ptr, end, nextTokPtr) \
   (((enc)->literalScanners[XML_ATTRIBUTE_VALUE_LITERAL])(enc, ptr, end, nextTokPtr))
-//   XmlLiteralTok(enc, XML_ATTRIBUTE_VALUE_LITERAL, ptr, end, nextTokPtr)
 
 #define XmlEntityValueTok(enc, ptr, end, nextTokPtr) \
   (((enc)->literalScanners[XML_ENTITY_VALUE_LITERAL])(enc, ptr, end, nextTokPtr))
-//   XmlLiteralTok(enc, XML_ENTITY_VALUE_LITERAL, ptr, end, nextTokPtr)
 
 #define XmlSameName(enc, ptr1, ptr2) (((enc)->sameName)(enc, ptr1, ptr2))
 
