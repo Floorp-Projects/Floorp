c5b8b86241182929cb34f23aae250b3527444282
mccabe%netscape.com <mccabe%netscape.com>
2000-06-16 08:23:28 +0000

    Build nsIScriptablePlugin.idl on the mac; remaining part of fix for 39911.  a=beard,r=don

--- build/mac/NGLayoutBuildList.pm
+++ build/mac/NGLayoutBuildList.pm
@@ -1492,6 +1492,7 @@ sub BuildIDLProjects()
     BuildIDLProject(":mozilla:modules:libpref:macbuild:libprefIDL.mcp",             "libpref");
     BuildIDLProject(":mozilla:modules:libutil:macbuild:libutilIDL.mcp",             "libutil");
     BuildIDLProject(":mozilla:modules:libjar:macbuild:libjarIDL.mcp",               "libjar");
+    BuildIDLProject(":mozilla:modules:plugin:macbuild:pluginIDL.mcp",               "plugin");
     BuildIDLProject(":mozilla:modules:oji:macbuild:ojiIDL.mcp",                     "oji");
     BuildIDLProject(":mozilla:js:macbuild:XPConnectIDL.mcp",                        "xpconnect");
     BuildIDLProject(":mozilla:dom:macbuild:domIDL.mcp",                             "dom");
