632069aef7b3e87c25fdfd4299874a9a87495dd8
akkana@netscape.com <akkana@netscape.com>
2000-06-16 21:23:16 +0000

    Make table tests run on linux; add comments explaining how to run them. Needed for testing 41916.  r=karnaze

--- layout/html/tests/table/runtests.sh
+++ layout/html/tests/table/runtests.sh
@@ -3,7 +3,8 @@
 # create test file to use first; since we don't know where the tree
 # is, and we need full pathnames in the file, we create it on the fly.
 
-viewer=$MOZ_SRC/mozilla/dist/bin/viewer
+viewer=$MOZILLA_FIVE_HOME/viewer
+echo viewer: $viewer
 testsfile=/tmp/$$-tests.txt
 
 sed -e "s@file:///s|@file:$MOZ_SRC@" < file_list.txt > $testsfile
@@ -11,11 +12,11 @@ sed -e "s@file:///s|@file:$MOZ_SRC@" < file_list.txt > $testsfile
 if test "$1"x = "baselinex"; then
   rm -r -f baseline
   mkdir baseline
-  $viewer -o baseline/ -f $testsfile
+  $viewer -d 1 -o baseline/ -f $testsfile
 elif test "$1"x = "verifyx"; then
   rm -r -f verify
   mkdir verify
-  $viewer -o verify/ -rd baseline/ -f $testsfile
+  $viewer -d 1 -o verify/ -rd baseline/ -f $testsfile
 elif test "$1"x = "cleanx"; then
   rm -r -f verify baseline  
 else
