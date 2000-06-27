888cb9091a27a2a4decb07046b052b8708e837a5
dougt%netscape.com <dougt%netscape.com>
2000-06-27 01:30:20 +0000

    simplify, simplify, simplify...
    added editor support.
    added multiple document support.
    reduced what interfaces were needed by the WebBrowser class.
    
    
    Not Part of build!

--- embedding/tests/winEmbed/WebBrowser.cpp
+++ embedding/tests/winEmbed/WebBrowser.cpp
@@ -34,18 +34,42 @@
 #include "nsIDocShell.h"
 #include "nsIContentViewer.h"
 #include "nsIContentViewerFile.h"
+#include "nsIDocShell.h"
+#include "nsIWebNavigation.h"
+#include "nsIEditorShell.h"
+#include "nsIDOMWindow.h"
+#include "nsIScriptGlobalObject.h"
+#include "nsIInterfaceRequestor.h"
 
-//*****************************************************************************
-//***    WebBrowser: Object Management
-//*****************************************************************************
 
-WebBrowser::WebBrowser()
+nsresult
+ConvertDocShellToDOMWindow(nsIDocShell* aDocShell, nsIDOMWindow** aDOMWindow)
 {
-	NS_INIT_REFCNT();
+  if (!aDOMWindow)
+    return NS_ERROR_FAILURE;
+
+  *aDOMWindow = nsnull;
+
+  nsCOMPtr<nsIScriptGlobalObject> scriptGlobalObject(do_GetInterface(aDocShell));
+
+  nsCOMPtr<nsIDOMWindow> domWindow(do_QueryInterface(scriptGlobalObject));
+  if (!domWindow)
+    return NS_ERROR_FAILURE;
+
+  *aDOMWindow = domWindow.get();
+  NS_ADDREF(*aDOMWindow);
+
+  return NS_OK;
 }
 
+
+
+WebBrowser::WebBrowser(){}
 WebBrowser::~WebBrowser()
 {
+    PRBool duh;
+    if (mEditor)  // not good place for it!
+        mEditor->SaveDocument(PR_FALSE, PR_FALSE, &duh);
 }
 
 nsresult 
@@ -61,24 +85,18 @@ WebBrowser::Init(nsNativeWidget widget)
     mBaseWindow = do_QueryInterface(mWebBrowser);
         
     rv = mBaseWindow->InitWindow( widget,
-                                    nsnull, 
-                                    0, 
-                                    32, 
-                                    450, 
-                                    450);
+                                  nsnull, 
+                                  0, 
+                                  0, 
+                                  100, 
+                                  100);
 
+    mWebBrowser->SetTopLevelWindow(nsnull);
 
-    mWebBrowser->SetTopLevelWindow(this);
-    
     mBaseWindow->Create();
-
-    nsCOMPtr <nsIDocShell> rootDocShell;
-    mWebBrowser->GetDocShell(getter_AddRefs(rootDocShell));
-    rootDocShell->SetAllowPlugins(PR_TRUE);
-
     mBaseWindow->SetVisibility(PR_TRUE);
 
-   return rv;
+    return rv;
 }
 
 nsresult 
@@ -89,7 +107,6 @@ WebBrowser::GetIWebBrowser(nsIWebBrowser **outBrowser)
     return NS_OK;
 }
 
-
 nsresult 
 WebBrowser::GoTo(char* url)
 {
@@ -97,6 +114,26 @@ WebBrowser::GoTo(char* url)
     return webNav->LoadURI(NS_ConvertASCIItoUCS2(url).GetUnicode());
 }
 
+nsresult 
+WebBrowser::Edit(char* url)
+{
+    nsresult rv;
+    mEditor = do_CreateInstance("component://netscape/editor/editorshell", &rv);
+    
+	if (NS_FAILED(rv)) return rv;
+    
+    nsCOMPtr <nsIDocShell> rootDocShell;
+    mWebBrowser->GetDocShell(getter_AddRefs(rootDocShell));
+    
+    nsCOMPtr<nsIDOMWindow> domWindow;
+    ConvertDocShellToDOMWindow(rootDocShell, getter_AddRefs(domWindow));
+   
+    mEditor->Init();
+    mEditor->SetEditorType(NS_ConvertASCIItoUCS2("html").GetUnicode());
+    mEditor->SetWebShellWindow(domWindow);
+    mEditor->SetContentWindow(domWindow);
+    return mEditor->LoadUrl(NS_ConvertASCIItoUCS2(url).GetUnicode());
+}
 
 nsresult 
 WebBrowser::Print(void)
@@ -118,296 +155,9 @@ WebBrowser::Print(void)
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// an empty implementation.
-
-
-//*****************************************************************************
-// WebBrowser::nsISupports
-//*****************************************************************************   
-
-NS_IMPL_ADDREF(WebBrowser)
-NS_IMPL_RELEASE(WebBrowser)
-
-NS_INTERFACE_MAP_BEGIN(WebBrowser)
-   NS_INTERFACE_MAP_ENTRY_AMBIGUOUS(nsISupports, nsIWebBrowserChrome)
-   NS_INTERFACE_MAP_ENTRY(nsIInterfaceRequestor)
-   NS_INTERFACE_MAP_ENTRY(nsIWebBrowserChrome)
-   NS_INTERFACE_MAP_ENTRY(nsIWebProgressListener)
-   NS_INTERFACE_MAP_ENTRY(nsIBaseWindow)
-   NS_INTERFACE_MAP_ENTRY(nsIPrompt)
-NS_INTERFACE_MAP_END
-
-//*****************************************************************************
-// WebBrowser::nsIInterfaceRequestor
-//*****************************************************************************   
-
-NS_IMETHODIMP WebBrowser::GetInterface(const nsIID &aIID, void** aInstancePtr)
+nsresult 
+WebBrowser::SetPositionAndSize(int x, int y, int cx, int cy)
 {
-   return QueryInterface(aIID, aInstancePtr);
+    return mBaseWindow->SetPositionAndSize(x, y, cx, cy, true);
 }
 
-//*****************************************************************************
-// WebBrowser::nsIWebBrowserChrome
-//*****************************************************************************   
-
-NS_IMETHODIMP WebBrowser::SetJSStatus(const PRUnichar* aStatus)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetJSDefaultStatus(const PRUnichar* aStatus)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetOverLink(const PRUnichar* aLink)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::GetWebBrowser(nsIWebBrowser** aWebBrowser)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetWebBrowser(nsIWebBrowser* aWebBrowser)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::GetChromeMask(PRUint32* aChromeMask)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetChromeMask(PRUint32 aChromeMask)
-{
-   return NS_OK;
-}
-
-extern nsresult CreateNativeWindowWidget(nsIWebBrowser **outBrowser);
-
-NS_IMETHODIMP WebBrowser::GetNewBrowser(PRUint32 chromeMask, nsIWebBrowser **outBrowser)
-{    
-    CreateNativeWindowWidget(outBrowser);
-    return NS_OK;
-}
-
-
-NS_IMETHODIMP WebBrowser::FindNamedBrowserItem(const PRUnichar* aName,
-                                                  	  nsIDocShellTreeItem ** aWebBrowser)
-{
-   return NS_ERROR_FAILURE;
-}
-
-NS_IMETHODIMP WebBrowser::SizeBrowserTo(PRInt32 aCX, PRInt32 aCY)
-{
-   return NS_OK;
-}
-
-
-NS_IMETHODIMP WebBrowser::ShowAsModal(void)
-{
-   return NS_OK;
-}
-
-
-//*****************************************************************************
-// WebBrowser::nsIWebProgressListener
-//*****************************************************************************   
-
-NS_IMETHODIMP WebBrowser::OnStateChange(nsIWebProgress *aWebProgress, nsIRequest *aRequest, PRInt32 aStateFlags, PRUint32 aStatus)
-{
-	return NS_OK;
-}
-NS_IMETHODIMP WebBrowser::OnProgressChange(nsIWebProgress *aWebProgress, nsIRequest *aRequest, PRInt32 aCurSelfProgress, PRInt32 aMaxSelfProgress, PRInt32 aCurTotalProgress, PRInt32 aMaxTotalProgress)
-{
-	return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::OnLocationChange(nsIURI *location)
-{
-	return NS_OK;
-}
-
-
-//*****************************************************************************
-// WebBrowser::nsIBaseWindow
-//*****************************************************************************   
-
-NS_IMETHODIMP WebBrowser::InitWindow(nativeWindow aParentNativeWindow,
-   nsIWidget* parentWidget, PRInt32 x, PRInt32 y, PRInt32 cx, PRInt32 cy)   
-{
-    return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::Create()
-{
-   return NS_ERROR_UNEXPECTED;
-}
-
-NS_IMETHODIMP WebBrowser::Destroy()
-{
-   return NS_ERROR_UNEXPECTED;
-}
-
-NS_IMETHODIMP WebBrowser::SetPosition(PRInt32 x, PRInt32 y)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::GetPosition(PRInt32* x, PRInt32* y)
-{
-   NS_ENSURE_ARG_POINTER(x && y);
-
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetSize(PRInt32 cx, PRInt32 cy, PRBool fRepaint)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::GetSize(PRInt32* cx, PRInt32* cy)
-{
-   NS_ENSURE_ARG_POINTER(cx && cy);
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetPositionAndSize(PRInt32 x, PRInt32 y, PRInt32 cx,
-   PRInt32 cy, PRBool fRepaint)
-{
-    return mBaseWindow->SetPositionAndSize(x, y,cx, cy,fRepaint);
-}
-
-NS_IMETHODIMP WebBrowser::GetPositionAndSize(PRInt32* x, PRInt32* y, PRInt32* cx,
-   PRInt32* cy)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::Repaint(PRBool aForce)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::GetParentWidget(nsIWidget** aParentWidget)
-{
-   NS_ENSURE_ARG_POINTER(aParentWidget);
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetParentWidget(nsIWidget* aParentWidget)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::GetParentNativeWindow(nativeWindow* aParentNativeWindow)
-{
-   NS_ENSURE_ARG_POINTER(aParentNativeWindow);
-    return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetParentNativeWindow(nativeWindow aParentNativeWindow)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::GetVisibility(PRBool* aVisibility)
-{
-   NS_ENSURE_ARG_POINTER(aVisibility);
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetVisibility(PRBool aVisibility)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::GetMainWidget(nsIWidget** aMainWidget)
-{
-   NS_ENSURE_ARG_POINTER(aMainWidget);
-
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetFocus()
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::FocusAvailable(nsIBaseWindow* aCurrentFocus, 
-   PRBool* aTookFocus)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::GetTitle(PRUnichar** aTitle)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::SetTitle(const PRUnichar* aTitle)
-{
-   return NS_OK;
-}
-
-//*****************************************************************************
-// WebBrowser::nsIPrompt
-//*****************************************************************************   
-
-NS_IMETHODIMP WebBrowser::Alert(const PRUnichar *dialogTitle, const PRUnichar *text)
-{
-    return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::Confirm(const PRUnichar *dialogTitle, const PRUnichar *text, PRBool *_retval)
-{
-    return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::ConfirmCheck(const PRUnichar *dialogTitle, const PRUnichar *text, const PRUnichar *checkMsg, PRBool *checkValue, PRBool *_retval)
-{
-    return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::Prompt(const PRUnichar *dialogTitle, const PRUnichar *text, const PRUnichar *passwordRealm, PRUint32 savePassword, const PRUnichar *defaultText, PRUnichar **result, PRBool *_retval)
-{
-    return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::PromptUsernameAndPassword(const PRUnichar *dialogTitle, const PRUnichar *text, const PRUnichar *passwordRealm, PRUint32 savePassword, PRUnichar **user, PRUnichar **pwd, PRBool *_retval)
-{
-    return NS_OK;
-}
-
-
-NS_IMETHODIMP WebBrowser::PromptPassword(const PRUnichar *dialogTitle, const PRUnichar *text, const PRUnichar *passwordRealm, PRUint32 savePassword, PRUnichar **user, PRBool *_retval)
-{
-    return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::Select(const PRUnichar *dialogTitle, const PRUnichar *text, PRUint32 count, const PRUnichar **selectList, PRInt32 *outSelection, PRBool *_retval)
-{
-   return NS_OK;
-}
-
-NS_IMETHODIMP WebBrowser::UniversalDialog(const PRUnichar *inTitleMessage, const PRUnichar *inDialogTitle, const PRUnichar *inMsg, const PRUnichar *inCheckboxMsg, const PRUnichar *inButton0Text, const PRUnichar *inButton1Text, const PRUnichar *inButton2Text, const PRUnichar *inButton3Text, const PRUnichar *inEditfield1Msg, const PRUnichar *inEditfield2Msg, PRUnichar **inoutEditfield1Value, PRUnichar **inoutEditfield2Value, const PRUnichar *inIConURL, PRBool *inoutCheckboxState, PRInt32 inNumberButtons, PRInt32 inNumberEditfields, PRInt32 inEditField1Password, PRInt32 *outButtonPressed)
-{
-   return NS_OK;
-}
