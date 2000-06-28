a53d61ddd30cafff7967071ef98cd2156b8c4f2c
locka@iol.ie <locka@iol.ie>
2000-06-28 20:40:10 +0000

    Added property dialogs

--- embedding/browser/activex/src/control/resource.h
+++ embedding/browser/activex/src/control/resource.h
@@ -5,35 +5,69 @@
 #define IDS_PROJNAME                    100
 #define IDR_MOZILLABROWSER              101
 #define IDI_MOZILLABROWSER              201
+#define IDC_CHECKMSG                    201
 #define IDR_POPUP_PAGE                  202
+#define IDC_BUTTON1                     202
+#define IDR_POPUP_DOCUMENT              202
 #define IDR_POPUP_LINK                  203
+#define IDC_BUTTON2                     203
 #define IDR_POPUP_CLIPBOARD             204
+#define IDC_QUESTION                    204
+#define IDR_POPUP_TEXT                  204
+#define IDD_CONFIRMCHECK                205
+#define IDC_MESSAGE                     205
+#define IDD_PROMPT                      206
+#define IDR_POPUP_IMAGE                 206
+#define IDC_PPAGE_MARKER                206
+#define IDD_PROMPTUSERNAMEANDPASSWORD   207
+#define IDD_PROMPTPASSWORD              208
+#define IDC_TYPE                        208
+#define IDD_PROPERTIES                  209
+#define IDC_PROTOCOL                    209
+#define IDD_PPAGE_LINK                  210
+#define IDC_ADDRESS                     210
 #define IDS_LOCATEMOZILLA               1000
 #define IDS_LOCATEMOZILLATITLE          1001
 #define IDS_CANNOTCREATEPREFS           1002
 #define ID_BROWSE_BACK                  32768
+#define ID_DOCUMENT_BACK                32768
 #define ID_BROWSE_FORWARD               32769
+#define ID_DOCUMENT_FORWARD             32769
 #define ID_FILE_REFRESH                 32772
+#define ID_DOCUMENT_REFRESH             32772
 #define ID_FILE_PRINT                   32773
+#define ID_DOCUMENT_PRINT               32773
 #define ID_PAGE_PROPERTIES              32774
+#define ID_DOCUMENT_PROPERTIES          32774
 #define ID_FILE_VIEWSOURCE              32775
+#define ID_DOCUMENT_VIEWSOURCE          32775
 #define ID_FILE_OPEN                    32776
+#define ID_LINK_OPEN                    32776
 #define ID_FILE_OPENINNEWWINDOW         32777
+#define ID_LINK_OPENINNEWWINDOW         32777
 #define ID_EDIT_COPYSHORTCUT            32778
+#define ID_LINK_COPYSHORTCUT            32778
 #define ID_LINK_PROPERTIES              32779
 #define ID_EDIT_CUT                     32780
 #define ID_EDIT_COPY                    32781
 #define ID_EDIT_PASTE                   32782
 #define ID_EDIT_SELECTALL               32783
 #define ID_SELECTIONPOPUP_PRINT         32784
+#define ID_DOCUMENT_SELECTALL           32785
+#define ID_DOCUMENT_PASTE               32786
+#define ID_TEXT_CUT                     32787
+#define ID_TEXT_COPY                    32788
+#define ID_TEXT_PASTE                   32789
+#define ID_TEXT_SELECTALL               32790
+#define ID_TEXT_PRINT                   32791
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        205
-#define _APS_NEXT_COMMAND_VALUE         32785
-#define _APS_NEXT_CONTROL_VALUE         201
+#define _APS_NEXT_RESOURCE_VALUE        211
+#define _APS_NEXT_COMMAND_VALUE         32792
+#define _APS_NEXT_CONTROL_VALUE         211
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
