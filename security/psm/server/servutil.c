366f6b877ec5c20c51210471d42a654f720b9a35
javi%netscape.com <javi%netscape.com>
2000-06-29 18:19:03 +0000

    Don't stop sending text to a log file just because one Write failed.
    This way subsequent DEBUG messages may still get printed.

--- security/psm/server/servutil.c
+++ security/psm/server/servutil.c
@@ -422,7 +422,7 @@ void SSM_DebugP(char *fmt, ...)
             numWritten = PR_Write(sock, tmp2, len);
         if (numWritten < len)
         {
-            SSM_Remove(logSockets, sock);
+            /*SSM_Remove(logSockets, sock);*/
             /*PR_Close(sock);*/
             /*PR_Shutdown(sock, PR_SHUTDOWN_BOTH);*/
             rv = 0;
