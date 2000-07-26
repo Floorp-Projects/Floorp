a02b66a615139320f850c75f53dce53eca346dda
mkaply@us.ibm.com <mkaply@us.ibm.com>
2000-07-26 14:07:24 +0000

    #45872 r=mkaply, a=brendan Make nsDeviceContextSpecOS2 look more like Windows

--- gfx/src/os2/nsDeviceContextSpecOS2.h
+++ gfx/src/os2/nsDeviceContextSpecOS2.h
@@ -1,21 +1,25 @@
-/*
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
  *
- * The Original Code is the Mozilla OS/2 libraries.
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
  *
- * The Initial Developer of the Original Code is John Fairhurst,
- * <john_fairhurst@iname.com>.  Portions created by John Fairhurst are
- * Copyright (C) 1999 John Fairhurst. All Rights Reserved.
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
  *
  * Contributor(s): 
+ *   John Fairhurst <john_fairhurst@iname.com>
+ *   IBM Corp.
  */
 
 #ifndef _nsDeviceContextSpecOS2_h
@@ -28,18 +32,18 @@
 
 class nsDeviceContextSpecOS2 : public nsIDeviceContextSpec
 {
- public:
-   nsDeviceContextSpecOS2();
+public:
+  nsDeviceContextSpecOS2();
 
-   NS_DECL_ISUPPORTS
+  NS_DECL_ISUPPORTS
 
-   NS_IMETHOD Init( PRTQUEUE *pq);
-   NS_IMETHOD GetPRTQUEUE( PRTQUEUE *&p);
+  NS_IMETHOD Init(PRTQUEUE *pq);
+  NS_IMETHOD GetPRTQUEUE(PRTQUEUE *&p);
 
- protected:
-   virtual ~nsDeviceContextSpecOS2();
+protected:
+  virtual ~nsDeviceContextSpecOS2();
 
-   PRTQUEUE *mQueue;
+  PRTQUEUE *mQueue;
 };
 
 #endif
