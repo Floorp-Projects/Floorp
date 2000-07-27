d25c0905f6927303853f4e1c3a81567643d18609
dbaron@fas.harvard.edu <dbaron@fas.harvard.edu>
2000-07-27 23:41:28 +0000

    Bring mozilla-viewer.sh all the enhancements made to mozilla (the shell script) in the past year or so, plus one more (-- to turn off command line handling).  This is needed to get block regression tests to run easily.  r=pollmann

--- webshell/tests/viewer/mozilla-viewer.sh
+++ webshell/tests/viewer/mozilla-viewer.sh
@@ -32,46 +32,44 @@
 ## the viewer work.
 ##
 
-dist_bin=""
-
-# Running from dist/bin
-if [ -d components -a -d res ]
-then
-  dist_bin="./"
-else
-  # Running from source dir
-  if [ -f Makefile.in ]
-  then
-    depth=`perl -ne '/^DEPTH\s*=(.*)/ and print $1' Makefile.in`
-    dist_bin=$depth/dist/bin
-  fi
-fi
-
+dist_bin=`dirname $0`
 script_args=""
+moreargs=""
+MOZILLA_BIN="viewer"
+pass_all_args=0
 
 while [ $# -gt 0 ]
 do
-  case $1 in
-    -h | --help)
-      script_args="$script_args -h"
-      shift
-      ;;
-    -g | --debug)
-      script_args="$script_args -g"
-      shift
-      ;;
-    -d | --debugger)
-      script_args="$script_args -d $2"
-      shift 2
-      ;;
-    -*)
-      echo "Unknown option: $1" 1>&2
-      exit 1
-      ;;
-    *)
-      break
-      ;;
- esac
+  if [ $pass_all_args -ne 0 ]
+  then
+    moreargs="$moreargs \"$1\""
+    shift
+  else
+    case $1 in
+      -h | --help)
+        script_args="$script_args -h"
+        shift
+        ;;
+      -g | --debug)
+        script_args="$script_args -g"
+        shift
+        ;;
+      -d | --debugger)
+        script_args="$script_args -d $2"
+        shift 2
+        ;;
+      --)
+        shift
+        pass_all_args=1
+        ;;
+      *)
+        moreargs="$moreargs \"$1\""
+        shift 1
+        ;;
+    esac
+  fi
 done
 
-$dist_bin/run-mozilla.sh $script_args ./viewer ${1+"$@"}
+eval "set -- $moreargs"
+echo $dist_bin/run-mozilla.sh $script_args $dist_bin/$MOZILLA_BIN "$@"
+$dist_bin/run-mozilla.sh $script_args $dist_bin/$MOZILLA_BIN "$@"
