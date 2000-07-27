fe2b60447cf1563406826e5f3f11e43211cc468a
larryh@netscape.com <larryh@netscape.com>
2000-07-27 21:20:48 +0000

    BugZilla: 25982. Add function PR_PutEnv()

--- nsprpub/pr/include/prenv.h
+++ nsprpub/pr/include/prenv.h
@@ -46,14 +46,105 @@
 PR_BEGIN_EXTERN_C
 
 /*
-** Lookup a variable in the environment. Return NULL if it's not found,
-** otherwise return it's value.
+** PR_GetEnv() -- Retrieve value of environment variable
+** 
+** Description:
+** PR_GetEnv() is modeled on Unix getenv().
+** 
+** 
+** Inputs: 
+**   var -- The name of the environment variable
+** 
+** Outputs: see Returns.
+** 
+** Returns:
+**   The value of the environment variable 'var' or NULL if
+** the variable is undefined.
+** 
+** Restrictions:
+**   You'd think that a POSIX setenv(), putenv() would be
+**   consistently implemented everywhere. Surprise! It is not. On
+**   some platforms, a putenv() where the argument is of
+**   the form "name"  causes the named environment variable to
+**   be un-set; that is: a subsequent getenv() returns NULL. On
+**   other platforms, the putenv() fails, on others, it is a
+**   no-op. Similarly, a putenv() where the argument is of the
+**   form "name=" causes the named environment variable to be
+**   un-set; a subsequent call to getenv() returns NULL. On
+**   other platforms, a subsequent call to getenv() returns a
+**   pointer to a null-string (a byte of zero).
+** 
+**   NSPR's PR_SetEnv(), PR_PutEnv() provide a consistent
+**   behavior across all supported platforms. There are,
+**   however, some restrictions and some practices you must use
+**   to achieve consistent results everywhere.
+** 
+**   When manipulating the environment there is no way to un-set
+**   an environment variable across all platforms. We suggest
+**   you interpret the return of a pointer to null-string to
+**   mean the same as a return of NULL from PR_GetEnv().
+** 
+**   A call to PR_PutEnv() where the parameter is of the form
+**   "name" will return PR_FAILURE; the environment remains
+**   unchanged. A call to PR_PutEnv() where the parameter is
+**   of the form "name=" may un-set the envrionment variable on
+**   some platforms; on others it may set the value of the
+**   environment variable to the null-string.
+** 
+**   For example, to test for NULL return or return of the
+**   null-string from PR_GetEnv(), use the following code
+**   fragment:
+** 
+**      char *val = PR_GetEnv("foo");
+**      if ((NULL == val) || (0x00 == *val)) {
+**         /* interpret this as un-set ... 
+**      }
+** 
+**   The caller must ensure that the string passed
+**   to PR_PutEnv() is persistent. That is: The string should
+**   not be on the stack, where it can be overwritten
+**   on return from the function calling PR_PutEnv().
+**   Similarly, the string passed to PR_PutEnv() must not be
+**   overwritten by other actions of the process. ... Some
+**   platforms use the string by reference rather than copying
+**   it into the environment space. ... You have been warned!
+** 
+**   Use of platform-native functions that manipulate the
+**   environment (getenv(), putenv(), 
+**   SetEnvironmentVariable(), etc.) must not be used with
+**   NSPR's similar functions. The platform-native functions
+**   may not be thread safe and/or may operate on different
+**   conceptual environment space than that operated upon by
+**   NSPR's functions or other environment manipulating
+**   functions on the same platform. (!)
+** 
 */
 NSPR_API(char*) PR_GetEnv(const char *var);
 
-#ifdef XP_MAC
-NSPR_API(PRIntn) PR_PutEnv(const char *string);
-#endif
+/*
+** PR_PutEnv() -- set, unset or change an environment variable
+** 
+** Description:
+** PR_PutEnv() is modeled on the Unix putenv() function.
+** 
+** Inputs: 
+**   string -- pointer to a caller supplied
+**   constant, persistent string of the form name=value. Where
+**   name is the name of the environment variable to be set or
+**   changed; value is the value assigned to the variable.
+**
+** Outputs: The environment variable is set or changed.
+** 
+** Returns: 
+**   PRStatus.
+** 
+** Restrictions: 
+**   See the Restrictions documented in the description of
+**   PR_GetEnv() in this header file.
+** 
+** 
+*/
+NSPR_API(PRStatus) PR_PutEnv(const char *string);
 
 PR_END_EXTERN_C
 
