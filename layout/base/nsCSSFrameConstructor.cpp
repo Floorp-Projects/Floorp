c9c32a64e4c04b2c0ce7932fb4f88a31d9a895a0
attinasi%netscape.com <attinasi%netscape.com>
2000-07-28 21:54:49 +0000

    Fix for bug 36849: Script animation that changes location and clip simultaneously no longer will flicker. ApplyRenderingChangeToTree now always does a NO_SYNC update of the views where it used to sometimes do a SYNC update. b=36849 r=kmcclusk

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -9712,20 +9712,7 @@ ApplyRenderingChangeToTree(nsIPresContext* aPresContext,
   }
 
   if (viewManager) {
-    nsCOMPtr<nsIPresShell> presShell;
-    nsresult rv = aPresContext->GetShell(getter_AddRefs(presShell));
-    if (NS_SUCCEEDED(rv)) {
-      PRBool isReflowLocked = PR_FALSE;
-      presShell->IsReflowLocked(&isReflowLocked);
-      if (isReflowLocked) {
-        viewManager->EndUpdateViewBatch(NS_VMREFRESH_NO_SYNC);
-      } else {
-        viewManager->EndUpdateViewBatch(NS_VMREFRESH_IMMEDIATE);
-      }
-    } else {
-      viewManager->EndUpdateViewBatch(NS_VMREFRESH_NO_SYNC);
-    }
-//    viewManager->Composite();
+    viewManager->EndUpdateViewBatch(NS_VMREFRESH_NO_SYNC);
     NS_RELEASE(viewManager);
   }
 }
