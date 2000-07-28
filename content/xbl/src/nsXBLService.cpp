222aae83d5a1eaf2e6f8f9cf20a7abdf7626921c
hyatt@netscape.com <hyatt@netscape.com>
2000-07-28 02:22:59 +0000

    Fix for 43778. r=ben

--- content/xbl/src/nsXBLService.cpp
+++ content/xbl/src/nsXBLService.cpp
@@ -240,7 +240,10 @@ nsXBLService::LoadBindings(nsIContent* aContent, const nsString& aURL, PRBool aA
       styleBinding->GetBindingURI(uri);
       if (uri.Equals(aURL))
         return NS_OK;
-      else FlushStyleBindings(aContent);
+      else {
+        FlushStyleBindings(aContent);
+        binding = nsnull;
+      }
     }
   }
 
