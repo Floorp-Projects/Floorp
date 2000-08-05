d5d42fefcdccb75bd72e28fa136856420edb406e
pierre@netscape.com <pierre@netscape.com>
2000-08-05 04:40:22 +0000

    40325 (subtracting dev2app in some cases)

--- gfx/src/mac/nsFontMetricsMac.cpp
+++ gfx/src/mac/nsFontMetricsMac.cpp
@@ -242,7 +242,7 @@ nsFontMetricsMac :: GetXHeight(nscoord& aResult)
 {
   float  dev2app;
   mContext->GetDevUnitsToAppUnits(dev2app);
-  aResult = NSToCoordRound(float(mMaxAscent * 0.71f) - dev2app);		// 0.71 = 5 / 7
+  aResult = NSToCoordRound(float(mMaxAscent * 0.71f));		// 0.71 = 5 / 7
   return NS_OK;
 }
 
