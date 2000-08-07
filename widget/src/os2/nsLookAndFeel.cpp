3f1f91e50317c7d309a21e95f42a689a5a53d446
mkaply@us.ibm.com <mkaply@us.ibm.com>
2000-08-07 20:48:09 +0000

    #47874 r=mkaply, a=brendan Bring nsLookAndFeel up to par with the Windows code

--- widget/src/os2/nsLookAndFeel.cpp
+++ widget/src/os2/nsLookAndFeel.cpp
@@ -1,37 +1,35 @@
-/*
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
  *
- * The Original Code is the Mozilla OS/2 libraries.
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
  *
- * The Initial Developer of the Original Code is John Fairhurst,
- * <john_fairhurst@iname.com>.  Portions created by John Fairhurst are
- * Copyright (C) 1999 John Fairhurst. All Rights Reserved.
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
  *
  * Contributor(s): 
- *   Pierre Phaneuf <pp@ludusdesign.com>
- *
- * This Original Code has been modified by IBM Corporation. Modifications made by IBM 
- * described herein are Copyright (c) International Business Machines Corporation, 2000.
- * Modifications to Mozilla code or documentation identified per MPL Section 3.3
- *
- * Date             Modified by     Description of modification
- * 04/20/2000       IBM Corp.       Make more like Windows.
- * 06/21/2000       IBM Corp.       Add SubmenuDelay case in GetMetric
+ *  John Fairhurst <john_fairhurst@iname.com>
+ *  Michael Lowe <michael.lowe@bigfoot.com>
+ *  Pierre Phaneuf <pp@ludusdesign.com>
+ *  IBM Corp.
  */
 
 #include "nsLookAndFeel.h"
-#include "nsWidgetDefs.h"
-
 #include "nsXPLookAndFeel.h"
-
+#define INCL_WIN
+#include <os2.h>
+#include "nsFont.h"
+ 
 static NS_DEFINE_IID(kILookAndFeelIID, NS_ILOOKANDFEEL_IID);
 
 //NS_IMPL_ISUPPORTS(nsLookAndFeel, NS_ILOOKANDFEEL_IID)
@@ -62,129 +60,129 @@ NS_IMETHODIMP nsLookAndFeel::GetColor(const nsColorID aID, nscolor &aColor)
   int idx;
   switch (aID) {
     case eColor_WindowBackground:
-        idx = SYSCLR_WINDOW;          // white
+        idx = SYSCLR_WINDOW;
         break;
     case eColor_WindowForeground:
-        idx = SYSCLR_WINDOWTEXT;   // black
+        idx = SYSCLR_WINDOWTEXT;
         break;
     case eColor_WidgetBackground:
-        idx = SYSCLR_BUTTONMIDDLE;  // light gray 
+        idx = SYSCLR_BUTTONMIDDLE;
         break;
     case eColor_WidgetForeground:
-        idx = SYSCLR_WINDOWTEXT;    // black
+        idx = SYSCLR_WINDOWTEXT; 
         break;
     case eColor_WidgetSelectBackground:
-        idx = SYSCLR_HILITEBACKGROUND;    // dark gray (items selected in a control)
+        idx = SYSCLR_HILITEBACKGROUND;
         break;
     case eColor_WidgetSelectForeground:
-        idx = SYSCLR_HILITEFOREGROUND;    // white (text of items selected in a control)
+        idx = SYSCLR_HILITEFOREGROUND;
         break;
     case eColor_Widget3DHighlight:
-        idx = SYSCLR_BUTTONLIGHT;   // white (highlight color for 3d for edges facing light source)
+        idx = SYSCLR_BUTTONLIGHT;
         break;
     case eColor_Widget3DShadow:
-        idx = SYSCLR_BUTTONDARK;      // dark gray (for edges facing away from light source)
+        idx = SYSCLR_BUTTONDARK;
         break;
     case eColor_TextBackground:
-        idx = SYSCLR_WINDOW;          // white
+        idx = SYSCLR_WINDOW;
         break;
     case eColor_TextForeground:
-        idx = SYSCLR_WINDOWTEXT;    // black
+        idx = SYSCLR_WINDOWTEXT;
         break;
     case eColor_TextSelectBackground:
-        idx = SYSCLR_HILITEBACKGROUND;         // dark gray
+        idx = SYSCLR_HILITEBACKGROUND;
         break;
     case eColor_TextSelectForeground:
-        idx = SYSCLR_HILITEFOREGROUND;        // white
+        idx = SYSCLR_HILITEFOREGROUND;
         break;
 
     // New CSS 2 Color definitions
     case eColor_activeborder:
-      idx = SYSCLR_ACTIVEBORDER;         // pale yellow
+      idx = SYSCLR_ACTIVEBORDER;
       break;
     case eColor_activecaption:
-      idx = SYSCLR_ACTIVETITLETEXT;   // teal (active window title bar)
+      idx = SYSCLR_ACTIVETITLETEXT;
       break;
     case eColor_appworkspace:
-      idx = SYSCLR_APPWORKSPACE;   // off-white
+      idx = SYSCLR_APPWORKSPACE;
       break;
     case eColor_background:
-      idx = SYSCLR_BACKGROUND;      // light gray
+      idx = SYSCLR_BACKGROUND;
       break;
     case eColor_buttonface:
-      idx = SYSCLR_BUTTONMIDDLE;    // light gray
+      idx = SYSCLR_BUTTONMIDDLE;
       break;
     case eColor_buttonhighlight:
-      idx = SYSCLR_BUTTONLIGHT;        // white
+      idx = SYSCLR_BUTTONLIGHT;
       break;
     case eColor_buttonshadow:
-      idx = SYSCLR_BUTTONDARK;        // dark gray
+      idx = SYSCLR_BUTTONDARK;
       break;
     case eColor_buttontext:
-      idx = SYSCLR_BUTTONDEFAULT;    // black
+      idx = SYSCLR_BUTTONDEFAULT;
       break;
     case eColor_captiontext:
-      idx = SYSCLR_WINDOWTEXT;          // black???
+      idx = SYSCLR_WINDOWTEXT;
       break;
     case eColor_graytext:
-      idx = SYSCLR_MENUDISABLEDTEXT;         // dark gray
+      idx = SYSCLR_MENUDISABLEDTEXT;
       break;
     case eColor_highlight:
-      idx = SYSCLR_HILITEFOREGROUND;    // white???
+      idx = SYSCLR_HILITEFOREGROUND;
       break;
     case eColor_highlighttext:
-      idx = SYSCLR_WINDOWTEXT;    // black ???
+      idx = SYSCLR_WINDOWTEXT;
       break;
     case eColor_inactiveborder:
-      idx = SYSCLR_INACTIVEBORDER;    // light gray
+      idx = SYSCLR_INACTIVEBORDER;
       break;
     case eColor_inactivecaption:
-      idx = SYSCLR_INACTIVETITLE;   // light gray???
+      idx = SYSCLR_INACTIVETITLE;
       break;
     case eColor_inactivecaptiontext:
-      idx = SYSCLR_INACTIVETITLE;      // light gray???
+      idx = SYSCLR_INACTIVETITLE;
       break;
     case eColor_infobackground:
-      idx = SYSCLR_INACTIVETITLE;      // light gray???
+      idx = SYSCLR_INACTIVETITLE;
       break;
     case eColor_infotext:
-      idx = SYSCLR_WINDOWTEXT;    // black ???
+      idx = SYSCLR_WINDOWTEXT;
       break;
     case eColor_menu:
-      idx = SYSCLR_MENU;     // light gray
+      idx = SYSCLR_MENU;
       break;
     case eColor_menutext:
-      idx = SYSCLR_MENUTEXT;        // black
+      idx = SYSCLR_MENUTEXT;
       break;
     case eColor_scrollbar:
-      idx = SYSCLR_SCROLLBAR;        // pale gray
+      idx = SYSCLR_SCROLLBAR;
       break;
     case eColor_threeddarkshadow:
-      idx = SYSCLR_SHADOW;    // dark gray???
+      idx = SYSCLR_SHADOW;
       break;
     case eColor_threedface:
-      idx = SYSCLR_BUTTONMIDDLE;    // light gray???
+      idx = SYSCLR_BUTTONMIDDLE;
       break;
     case eColor_threedhighlight:
-      idx = SYSCLR_BUTTONLIGHT;   // white???
+      idx = SYSCLR_BUTTONLIGHT;
       break;
     case eColor_threedlightshadow:
-      idx = SYSCLR_BUTTONMIDDLE;    // light gray???
+      idx = SYSCLR_BUTTONMIDDLE;
       break;
     case eColor_threedshadow:
-      idx = SYSCLR_SHADOW;    // dark gray???
+      idx = SYSCLR_SHADOW;
       break;
     case eColor_window:
-      idx = SYSCLR_WINDOW;    // white
+      idx = SYSCLR_WINDOW;
       break;
     case eColor_windowframe:
-      idx = SYSCLR_WINDOWFRAME;   // dark gray
+      idx = SYSCLR_WINDOWFRAME;
       break;
     case eColor_windowtext:
-      idx = SYSCLR_WINDOWTEXT;    // black
+      idx = SYSCLR_WINDOWTEXT;
       break;
     default:
-        idx = SYSCLR_WINDOW;    // white
+        idx = SYSCLR_WINDOW;
         break;
     }
 
@@ -198,7 +196,7 @@ NS_IMETHODIMP nsLookAndFeel::GetColor(const nsColorID aID, nscolor &aColor)
 
   return res;
 }
-
+  
 NS_IMETHODIMP nsLookAndFeel::GetMetric(const nsMetricID aID, PRInt32 & aMetric)
 {
   nsresult res = NS_OK;
@@ -208,30 +206,26 @@ NS_IMETHODIMP nsLookAndFeel::GetMetric(const nsMetricID aID, PRInt32 & aMetric)
     res = mXPLookAndFeel->GetMetric(aID, aMetric);
     if (NS_SUCCEEDED(res))
       return res;
+    res = NS_OK;
   }
-
-  long svalue = 0;
-  aMetric = 0;
-  ULONG ulPels = 0;
 
   switch (aID) {
     case eMetric_WindowTitleHeight:
-        svalue = SV_CYTITLEBAR; 
+        aMetric = WinQuerySysValue( HWND_DESKTOP, SV_CYTITLEBAR);
         break;
     case eMetric_WindowBorderWidth:
-        svalue = SV_CXSIZEBORDER;
+        aMetric = WinQuerySysValue( HWND_DESKTOP, SV_CXSIZEBORDER);
         break;
     case eMetric_WindowBorderHeight:
-        svalue = SV_CYSIZEBORDER; 
+        aMetric = WinQuerySysValue( HWND_DESKTOP, SV_CYSIZEBORDER);
         break;
     case eMetric_Widget3DBorder:
-        svalue = SV_CXBORDER; 
+        aMetric = WinQuerySysValue( HWND_DESKTOP, SV_CXBORDER);
         break;
     case eMetric_TextFieldBorder:
         aMetric = 3;
         break;
     case eMetric_TextFieldHeight:
-        // ??? gModuleData.lHtEntryfield; 
         aMetric = 24;
         break;
     case eMetric_ButtonHorizontalInsidePaddingNavQuirks:
@@ -270,41 +264,27 @@ NS_IMETHODIMP nsLookAndFeel::GetMetric(const nsMetricID aID, PRInt32 & aMetric)
     case eMetric_ListVerticalInsidePadding:
         aMetric = 0;
         break;
+    case eMetric_CaretBlinkTime:
+        aMetric = WinQuerySysValue( HWND_DESKTOP, SV_CURSORRATE);
+        break;
+    case eMetric_SingleLineCaretWidth:
+        aMetric = 1;
+        break;
+    case eMetric_MultiLineCaretWidth:
+        aMetric = 2;
+        break;
     case eMetric_SubmenuDelay:
         aMetric = 300;
         break;
-    case eMetric_CaretBlinkTime:
-        svalue = SV_CURSORRATE;
+    case eMetric_MenusCanOverlapOSBar:
+        // we want XUL popups to be able to overlap the task bar.
+        aMetric = 1;
         break;
-    case eMetric_SingleLineCaretWidth:
-    case eMetric_MultiLineCaretWidth:
-        // Sigh - this is in 'twips', should be in 'app units', but there's no
-        // DC anyway so we can't work it out!
 
-        // The reason for this is that there is some rounding weirdness,
-        // such that the 2-pixel caret requested by the windows guys
-        // got rounded down to a 1-pixel caret for drawing, because
-        // of errors in conversion to twips and back.
-        
-        ulPels = WinQuerySysValue( HWND_DESKTOP, SV_CYBORDER);
-
-        // With luck, either:
-        //   * these metrics will go into nsILookAndFeel, or
-        //   * we'll get an nsIDeviceContext here
-        //
-        // For now, lets assume p2t = 20.
-        aMetric = 1 * ulPels;
-        break;
-// Windows     aMetric = 30;
-        break;
     default:
         aMetric = -1;
         res = NS_ERROR_FAILURE;
     }
-
-  if( svalue != 0)
-     aMetric = WinQuerySysValue( HWND_DESKTOP, svalue);
-
   return res;
 }
 
@@ -317,6 +297,7 @@ NS_IMETHODIMP nsLookAndFeel::GetMetric(const nsMetricFloatID aID, float & aMetri
     res = mXPLookAndFeel->GetMetric(aID, aMetric);
     if (NS_SUCCEEDED(res))
       return res;
+    res = NS_OK;
   }
 
   switch (aID) {
@@ -368,7 +349,43 @@ NS_IMETHODIMP nsLookAndFeel::GetNavSize(const nsMetricNavWidgetID aWidgetID,
 
   aSize.width  = 0;
   aSize.height = 0;
-  printf("nsLookAndFeel::GetNavSize not implemented\n");   // OS2TODO
-  return NS_ERROR_NOT_IMPLEMENTED;  
+
+  if (aFontSize < 1 || aFontSize > 7) {
+    return NS_ERROR_FAILURE;
+  }
+
+  PRInt32 kTextFieldWidths[2][7] = {
+    {106,147,169,211,253,338,506}, // Courier
+    {152,214,237,281,366,495,732}  // sans-serif
+  };
+
+  PRInt32 kTextFieldHeights[2][7] = {
+    {18,21,24,27,33,45,63}, // Courier
+    {18,21,24,27,34,48,67}  // sans-serif
+  };
+
+  PRInt32 kTextAreaWidths[2][7] = {
+    {121,163,184,226,268,352,520}, // Courier
+    {163,226,247,289,373,499,730}  // sans-serif
+  };
+
+  PRInt32 kTextAreaHeights[2][7] = {
+    {40,44,48,52,60,76,100}, // Courier
+    {40,44,48,52,62,80,106}  // sans-serif
+  };
+
+  switch (aWidgetID) {
+    case eMetricSize_TextField:
+      aSize.width  = kTextFieldWidths[aFontID][aFontSize-1];
+      aSize.height = kTextFieldHeights[aFontID][aFontSize-1];
+      break;
+    case eMetricSize_TextArea:
+      aSize.width  = kTextAreaWidths[aFontID][aFontSize-1];
+      aSize.height = kTextAreaHeights[aFontID][aFontSize-1];
+      break;
+  } //switch
+
+  return NS_OK;
+
 }
 #endif
--- widget/src/os2/nsLookAndFeel.h
+++ widget/src/os2/nsLookAndFeel.h
@@ -1,28 +1,23 @@
-/*
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
  *
- * The Original Code is the Mozilla OS/2 libraries.
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
  *
- * The Initial Developer of the Original Code is John Fairhurst,
- * <john_fairhurst@iname.com>.  Portions created by John Fairhurst are
- * Copyright (C) 1999 John Fairhurst. All Rights Reserved.
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
  *
  * Contributor(s): 
- * This Original Code has been modified by IBM Corporation. Modifications made by IBM 
- * described herein are Copyright (c) International Business Machines Corporation, 2000.
- * Modifications to Mozilla code or documentation identified per MPL Section 3.3
- *
- * Date             Modified by     Description of modification
- * 04/20/2000       IBM Corp.      Make more like Windows.
- *
  */
 
 #ifndef __nsLookAndFeel
