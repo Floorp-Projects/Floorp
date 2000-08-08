f4c2ca507319ee352799bd2c62dd7c2a1e97459b
scc%mozilla.org <scc%mozilla.org>
2000-08-08 00:14:35 +0000

    not part of the build; initial work on multi-fragment strings

--- string/public/nsFragmentedString.h
+++ string/public/nsFragmentedString.h
@@ -1,20 +1,20 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /*
- * The contents of this file are subject to the Netscape Public
+ * The contents of this file are subject to the Mozilla Public
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/NPL/
- *
+ * the License at http://www.mozilla.org/MPL/
+ * 
  * Software distributed under the License is distributed on an "AS
  * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
  * implied. See the License for the specific language governing
  * rights and limitations under the License.
- *
- * The Original Code is mozilla.org code.
- *
+ * 
+ * The Original Code is Mozilla XPCOM.
+ * 
  * The Initial Developer of the Original Code is Netscape
  * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Copyright (C) 2000 Netscape Communications Corporation. All
  * Rights Reserved.
  *
  * Original Author:
@@ -39,8 +39,35 @@ class basic_nsFragmentedString
       ...
     */
   {
+    protected:
+      virtual const void* Implementation() const;
+
+      virtual const CharT* GetReadableFragment( nsReadableFragment<CharT>&, nsFragmentRequest, PRUint32 ) const;
+      virtual CharT* GetWritableFragment( nsWritableFragment<CharT>&, nsFragmentRequest, PRUint32 );
+
     public:
+      virtual PRUint32 Length() const;
+
+      virtual void SetCapacity( PRUint32 aNewCapacity );
+      virtual void SetLength( PRUint32 aNewLength );
+
+    private:
+      
       // ...
   };
+
+  /**
+   * |SetLength|
+   */
+
+
+  /**
+   * |SetCapacity|.
+   *
+   * If a client tries to increase the capacity of multi-fragment string, perhaps a single
+   * empty fragment of the appropriate size should be appended.
+   */
+
+
 
 #endif // !defined(nsFragmentedString_h___)
