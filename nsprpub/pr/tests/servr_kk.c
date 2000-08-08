c5183c19227a9d15cdeb97411f270151bffd61f2
wtc@netscape.com <wtc@netscape.com>
2000-08-08 21:33:51 +0000

    Make main() return int explicitly.

--- nsprpub/pr/tests/servr_kk.c
+++ nsprpub/pr/tests/servr_kk.c
@@ -541,7 +541,7 @@ static void Measure(void (*func)(void), const char *msg)
 }
 
 
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	/* The command line argument: -d is used to determine if the test is being run
 	in debug mode. The regress tool requires only one line output:PASS or FAIL.
