01d9696e304bf2dce061b503708152db79564fc3
conrad@ingress.com <conrad@ingress.com>
2000-08-14 03:03:05 +0000

    Not part of build.Removed MMozillaApp mixin class from project. This is now handled by EmbedAPI lib.

--- embedding/browser/powerplant/source/CBrowserApp.h
+++ embedding/browser/powerplant/source/CBrowserApp.h
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
  * The contents of this file are subject to the Mozilla Public
  * License Version 1.1 (the "License"); you may not use this file
@@ -29,52 +29,31 @@
 #include "nsError.h"
 #endif
 
-class nsIEventQueueService;
-class nsIServiceManager;
-
-// ---------------------------------------------------------------------------
-//		MMozillaApp - A class that can be mixed into an application class. It
-//    handles the startup and shutdown of mozilla.
-// ---------------------------------------------------------------------------
-
-class MMozillaApp
-{
-  protected:
-                          MMozillaApp();
-    virtual               ~MMozillaApp();
-    
-		nsresult		          InitWebShellApp();
-		void			            TermWebShellApp();
-		
-		nsIEventQueueService  *mpIEventQueueService;
-		nsIServiceManager     *mpIServiceManager;
-};                  
-
-// WARNING: CBrowserApp must descend from these in this order - we need
-// the destructor for MMozillaApp to be called AFTER that of LApplication
  
-class	CBrowserApp : public MMozillaApp,
-                       public PP_PowerPlant::LApplication
+class	CBrowserApp : public PP_PowerPlant::LApplication
 {
 
 public:
-							    CBrowserApp();	// constructor registers PPobs
-		virtual 			~CBrowserApp();	// stub destructor
-	
+					        CBrowserApp();	// constructor registers PPobs
+	virtual 			    ~CBrowserApp();	// stub destructor
 
-	  virtual void		ProcessNextEvent();
-	
-		// this overriding method handles application commands
-		virtual Boolean		ObeyCommand(PP_PowerPlant::CommandT inCommand, void* ioParam);	
-	
-	
-		// this overriding method returns the status of menu items
-		virtual void		FindCommandStatus(PP_PowerPlant::CommandT inCommand,
-									Boolean &outEnabled, Boolean &outUsesMark,
-									PP_PowerPlant::Char16 &outMark, Str255 outName);
+
+    virtual void            ProcessNextEvent();
+
+	// this overriding method handles application commands
+	virtual Boolean		    ObeyCommand(PP_PowerPlant::CommandT inCommand, void* ioParam);	
+
+
+	// this overriding method returns the status of menu items
+	virtual void            FindCommandStatus(PP_PowerPlant::CommandT inCommand,
+            								  Boolean &outEnabled, Boolean &outUsesMark,
+            								  PP_PowerPlant::Char16 &outMark, Str255 outName);
+								
+   virtual Boolean          AttemptQuitSelf(SInt32 inSaveOption);
+
 									
 protected:
 		
-		virtual void			    StartUp();			// override startup functions
+	virtual void			StartUp();			// override startup functions
 
 };
