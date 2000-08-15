68ddf0eb55cddc8f272e8731f9a1ae00a07ed369
hyatt%netscape.com <hyatt%netscape.com>
2000-08-15 22:46:09 +0000

    Fix for bug #49066, r=saari

--- xpfe/global/resources/content/commonDialog.js
+++ xpfe/global/resources/content/commonDialog.js
@@ -114,7 +114,7 @@ function commonDialogOnLoad()
             
         var label = param.GetString(4);
         if (label) setElementText(labelID, label);
-        field.focus();
+        field.focusTextField();
 
         break;
     case 1:
@@ -136,7 +136,7 @@ function commonDialogOnLoad()
       unHideElementByID(containerID);
       var field = document.getElementById(fieldID);
       field.value = param.GetString(6);
-      field.focus();
+      field.focusTextField();
       break;
   }	
 
