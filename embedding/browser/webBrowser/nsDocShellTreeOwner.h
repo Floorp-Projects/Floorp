e5a98a4a360833d98a63fb5f409f5f8c6cbfac42
locka@iol.ie <locka@iol.ie>
2000-08-16 08:51:58 +0000

    Added mouse DOM listener to listen for context menu events and notify the embedding client. b=48270 a=valeski

--- embedding/browser/webBrowser/nsDocShellTreeOwner.h
+++ embedding/browser/webBrowser/nsDocShellTreeOwner.h
@@ -33,32 +33,69 @@
 #include "nsIInterfaceRequestor.h"
 #include "nsIWebBrowserChrome.h"
 #include "nsIWebProgressListener.h"
+#include "nsIDOMMouseListener.h"
+#include "nsIDOMDocument.h"
+
+#include "nsCommandHandler.h"
 
 class nsWebBrowser;
+
+// {6D10C180-6888-11d4-952B-0020183BF181}
+#define NS_ICDOCSHELLTREEOWNER_IID \
+{ 0x6d10c180, 0x6888, 0x11d4, { 0x95, 0x2b, 0x0, 0x20, 0x18, 0x3b, 0xf1, 0x81 } }
+
+/*
+ * This is a fake 'hidden' interface that nsDocShellTreeOwner implements.
+ * Classes such as nsCommandHandler can QI for this interface to be
+ * sure that they're dealing with a valid nsDocShellTreeOwner and not some
+ * other object that implements nsIDocShellTreeOwner.
+ */
+class nsICDocShellTreeOwner : public nsISupports
+{
+public:
+    NS_DEFINE_STATIC_IID_ACCESSOR(NS_ICDOCSHELLTREEOWNER_IID)
+};
+
 
 class nsDocShellTreeOwner : public nsIDocShellTreeOwner,
                             public nsIBaseWindow,
                             public nsIInterfaceRequestor,
-                            public nsIWebProgressListener
+                            public nsIWebProgressListener,
+                            public nsIDOMMouseListener,
+                            public nsICDocShellTreeOwner
 {
 friend class nsWebBrowser;
+friend class nsCommandHandler;
 
 public:
-   NS_DECL_ISUPPORTS
+    NS_DECL_ISUPPORTS
 
-   NS_DECL_NSIBASEWINDOW
-   NS_DECL_NSIDOCSHELLTREEOWNER
-   NS_DECL_NSIINTERFACEREQUESTOR
-   NS_DECL_NSIWEBPROGRESSLISTENER
+    NS_DECL_NSIBASEWINDOW
+    NS_DECL_NSIDOCSHELLTREEOWNER
+    NS_DECL_NSIINTERFACEREQUESTOR
+    NS_DECL_NSIWEBPROGRESSLISTENER
+
+	// nsIDOMMouseListener
+	virtual nsresult HandleEvent(nsIDOMEvent* aEvent);
+	virtual nsresult MouseDown(nsIDOMEvent* aMouseEvent);
+	virtual nsresult MouseUp(nsIDOMEvent* aMouseEvent);
+	virtual nsresult MouseClick(nsIDOMEvent* aMouseEvent);
+	virtual nsresult MouseDblClick(nsIDOMEvent* aMouseEvent);
+	virtual nsresult MouseOver(nsIDOMEvent* aMouseEvent);
+	virtual nsresult MouseOut(nsIDOMEvent* aMouseEvent);
 
 protected:
-   nsDocShellTreeOwner();
-   virtual ~nsDocShellTreeOwner();
+    nsDocShellTreeOwner();
+    virtual ~nsDocShellTreeOwner();
 
-   void WebBrowser(nsWebBrowser* aWebBrowser);
-   nsWebBrowser* WebBrowser();
-   NS_IMETHOD SetTreeOwner(nsIDocShellTreeOwner* aTreeOwner);
-   NS_IMETHOD SetWebBrowserChrome(nsIWebBrowserChrome* aWebBrowserChrome);
+    void WebBrowser(nsWebBrowser* aWebBrowser);
+    nsWebBrowser* WebBrowser();
+    NS_IMETHOD SetTreeOwner(nsIDocShellTreeOwner* aTreeOwner);
+    NS_IMETHOD SetWebBrowserChrome(nsIWebBrowserChrome* aWebBrowserChrome);
+
+    nsCOMPtr<nsIDOMDocument> mLastDOMDocument;
+    NS_IMETHOD AddMouseListener();
+    NS_IMETHOD RemoveMouseListener();
 
 protected:
 
