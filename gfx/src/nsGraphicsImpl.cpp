bd6db38e681fd16a507bb430705f7d8631e99b85
scc@mozilla.org <scc@mozilla.org>
2000-08-19 22:01:24 +0000

    attempt to fix linux bustage

--- gfx/src/nsGraphicsImpl.cpp
+++ gfx/src/nsGraphicsImpl.cpp
@@ -142,7 +142,8 @@ NS_IMETHODIMP nsGraphicsImpl::DrawString(const PRUnichar *text, nscoord x, nscoo
 NS_IMETHODIMP nsGraphicsImpl::SetFont(const PRUnichar *name, nscoord size)
 {
 	size *= mDev2App;
-	nsFont font(nsAutoString(name), NS_FONT_STYLE_NORMAL, NS_FONT_VARIANT_NORMAL, NS_FONT_WEIGHT_NORMAL, NS_FONT_DECORATION_NONE, size);
+	nsAutoString nameString(name);
+	nsFont font(nameString, NS_FONT_STYLE_NORMAL, NS_FONT_VARIANT_NORMAL, NS_FONT_WEIGHT_NORMAL, NS_FONT_DECORATION_NONE, size);
 	return mRenderer->SetFont(font);
 }
 
