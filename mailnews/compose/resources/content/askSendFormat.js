54158fa72c888db62d8a10b755db27625373b7ec
ducarroz%netscape.com <ducarroz%netscape.com>
2000-08-22 23:40:59 +0000

    Fix for bug 49016. Create an HTML text node to hold the alert message. Also fix the alert position. R=putterman

--- mailnews/compose/resources/content/askSendFormat.js
+++ mailnews/compose/resources/content/askSendFormat.js
@@ -4,37 +4,48 @@ var param = null;
 function Startup()
 {
 	if (window.arguments && window.arguments[0] && window.arguments[0])
-    {
+  {
 		param = window.arguments[0];
 		param.abort = true;         //if the user hit the close box, we will abort.
 		if (param.action)
 		{
-		    //Set the default radio array value
-		    var group = document.getElementById("mailDefaultHTMLAction");
-		    var element = document.getElementById("SendPlainTextAndHtml");
+      // display the main text
+      var messageParent = document.getElementById("info.box");
+    	var messageText = messageParent.getAttribute("value");
+      var htmlNode = document.createElement("html");
+      var text = document.createTextNode(messageText);
+      htmlNode.appendChild(text);
+      messageParent.appendChild(htmlNode);
+
+		  //Set the default radio array value
+	    var group = document.getElementById("mailDefaultHTMLAction");
+	    var element = document.getElementById("SendPlainTextAndHtml");
 			group.selectedItem= element;
 			group.data = element.data;
 
-		    //change the button label
-		    labels = document.getElementById("okCancelButtons");
-		    element = document.getElementById("ok");
-		    element.setAttribute("value", labels.getAttribute("button1Label"));
-		    element = document.getElementById("cancel");
-		    element.setAttribute("value", labels.getAttribute("button2Label"));
+	    //change the button label
+	    labels = document.getElementById("okCancelButtons");
+	    element = document.getElementById("ok");
+	    element.setAttribute("value", labels.getAttribute("button1Label"));
+	    element = document.getElementById("cancel");
+	    element.setAttribute("value", labels.getAttribute("button2Label"));
 /*
-		    element = document.getElementById("Button2");
-		    element.setAttribute("value", labels.getAttribute("button3Label"));
-            element.removeAttribute("hidden");
-		    element.setAttribute("disabled", "true");
-		    element = document.getElementById("Button3");
-		    element.setAttribute("value", labels.getAttribute("button4Label"));
-            element.removeAttribute("hidden");
-		    element.setAttribute("disabled", "true");
+	    element = document.getElementById("Button2");
+	    element.setAttribute("value", labels.getAttribute("button3Label"));
+      element.removeAttribute("hidden");
+	    element.setAttribute("disabled", "true");
+	    element = document.getElementById("Button3");
+	    element.setAttribute("value", labels.getAttribute("button4Label"));
+      element.removeAttribute("hidden");
+	    element.setAttribute("disabled", "true");
 */		    
-		    //set buttons action
-		    doSetOKCancel(Send, DontSend, Recipients, Help);
+	    //set buttons action
+	    doSetOKCancel(Send, DontSend, Recipients, Help);
 
-		   moveToAlertPosition();
+      //At this point, we cannot position the window because it still doesn't have a width and a height.
+      //Let move it out of the screen and then move it back at the right position, this after the first refresh.
+      window.moveTo(32000, 32000);
+		  setTimeout("moveToAlertPosition();", 0);
 		}
 	}
 	else 
