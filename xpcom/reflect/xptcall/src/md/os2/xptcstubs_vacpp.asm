e9cc7884ede483db537d3edd4ff4afb86a533343
mkaply%us.ibm.com <mkaply%us.ibm.com>
2000-08-24 20:20:49 +0000

    #50203 r=mkaply, a=brendan Patch from pedemont - xptcstubs didn't handle one parameter correctly

--- xpcom/reflect/xptcall/src/md/os2/xptcstubs_vacpp.asm
+++ xpcom/reflect/xptcall/src/md/os2/xptcstubs_vacpp.asm
@@ -30,16 +30,37 @@ COMMENT | -*- Mode: asm; tab-width: 8; c-basic-offset: 4 -*-
 
             ; Optlink puts stubidx in eax
 SetEntryFromIndex__Fi       PROC OPTLINK EXPORT stubidx
-        mov     dword ptr [esp+20h], ecx    ; move arg into stack slot
-        mov	    dword ptr [esp+1ch], edx    ; ditto
-        lea     ecx, dword ptr [esp+1ch]    ; load args address
-        mov	    edx, eax                    ; index
-        mov     eax, dword ptr [ebp+18h]    ; this
-        sub	    esp, 0ch                    ; room for params
-        ;call    dword ptr [ebp+10h]         ; PrepareAndDispatch
-        call    PrepareAndDispatch__FP14nsXPTCStubBaseUiPUi
-        add	    esp, 10h                    ; clean up
-        ret
+
+            ; hack to check for only two params in previous stack frame.
+            ; in optlink, space is allocated on stack for three params
+            ; (an implied this, and the first two params in the list).
+            ; here, the sixth and seventh commands write the values in
+            ; edx and ecx back to the space on stack allocated for them
+            ; however, if there is only one actual param (eax = 'this',
+            ; edx = param, ecx = garbage), then writing back ecx in the
+            ; sixth instruction here will overwrite the 'saved EBP' and
+            ; corrupt the stack.  
+            ; this code walks up the ebp-chain.  if there is only one
+            ; param and the 'this' pointer in the stack frame, then
+            ; there will be another saved EBP at current-EBP + 20h. if
+            ; this is the case, do not save ECX as it will overwrite
+            ; the saved EBP there.
+          mov     ebx, [ebp]                  ; walk EBP chain...
+          mov     ebx, [ebx]                  ; ...and again
+          sub     ebx, 20h                      
+          cmp     ebx, ebp                    ; check if found another EBP
+          je     skipload
+
+          mov     dword ptr [esp+20h], ecx    ; move arg into stack slot
+ skipload:mov	    dword ptr [esp+1ch], edx    ; ditto
+          lea     ecx, dword ptr [esp+1ch]    ; load args address
+          mov	    edx, eax                    ; index
+          mov     eax, dword ptr [ebp+18h]    ; this
+          sub	    esp, 0ch                    ; room for params
+          ;call    dword ptr [ebp+10h]         ; PrepareAndDispatch
+          call    PrepareAndDispatch__FP14nsXPTCStubBaseUiPUi
+          add	    esp, 0ch                    ; clean up
+          ret
 SetEntryFromIndex__Fi       ENDP
         
         END
