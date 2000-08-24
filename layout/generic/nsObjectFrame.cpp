45432962fea4052722334ea6fde1ef49706f76a8
akkana@netscape.com <akkana@netscape.com>
2000-08-24 20:52:44 +0000

    Fix build bustage

--- layout/generic/nsObjectFrame.cpp
+++ layout/generic/nsObjectFrame.cpp
@@ -1712,7 +1712,7 @@ NS_IMETHODIMP nsPluginInstanceOwner::GetURL(const char *aURL, const char *aTarge
             rv = lh->OnLinkClick(content, eLinkVerb_Replace, 
                                  fullurl.GetUnicode(), 
                                  unitarget.GetUnicode(), 
-                                 postDataStream ? postDataStream : nsnull);
+                                 postDataStream ? postDataStream.get() : nsnull);
             NS_IF_RELEASE(content);
           }
           NS_RELEASE(lh);
