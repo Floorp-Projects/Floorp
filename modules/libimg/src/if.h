b1bbbb7b88112ac1115057544acbb8d0fcf92d62
pnunn@netscape.com <pnunn@netscape.com>
2000-08-26 18:17:25 +0000

    bug#46704: chkin for syd. r:pnunn. keep single frame chrome in imgcache.

--- modules/libimg/src/if.h
+++ modules/libimg/src/if.h
@@ -22,7 +22,7 @@
 
 /*   if.h --- Top-level image library internal routines
  *
- * $Id: if.h,v 3.23 2000/08/16 03:16:46 waterson%netscape.com Exp $
+ * $Id: if.h,v 3.24 2000/08/26 18:17:25 pnunn%netscape.com Exp $
  */
 
 #ifndef _if_h
@@ -112,6 +112,11 @@ extern PRLogModuleInfo *il_log_module;
 #endif
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 
+/* types that can be used to, for example, differentiate chrome
+   images from others */
+
+#define TYPE_DEFAULT 0		// unspecified
+#define TYPE_CHROME 1		// chrome URL
 
 /* Conversion of imglib errors to XPCOM errors */
 #define NS_CONVERT_ERROR_CODE(e)  \
@@ -189,6 +194,7 @@ struct il_container_struct {
     enum icstate state;
     int sized;
 
+    int moz_type;		/* TYPE_CHROME, etc. */
     int is_alone;               /* only image on a page */
     int is_in_use;              /* Used by some context */
     int32 loop_count;           /* Remaining number of times to repeat image,
