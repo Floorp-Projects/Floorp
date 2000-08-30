8147a96d3e28785cbee434860fcfd7965c64bc7c
kestes%staff.mail.com <kestes%staff.mail.com>
2000-08-30 02:23:08 +0000

    fix bug in html missing '/' in <> index files are new parametrized

--- webtools/tinderbox2/src/lib/Summaries.pm
+++ webtools/tinderbox2/src/lib/Summaries.pm
@@ -8,8 +8,8 @@
 # The only external interface to this library is summary_pages() and
 # create_global_index() these functions are only called by tinder.cgi.
 
-# $Revision: 1.2 $ 
-# $Date: 2000/08/11 00:26:05 $ 
+# $Revision: 1.3 $ 
+# $Date: 2000/08/30 02:23:08 $ 
 # $Author: kestes%staff.mail.com $ 
 # $Source: /home/hwine/cvs_conversion/cvsroot/mozilla/webtools/tinderbox2/src/lib/Summaries.pm,v $ 
 # $Name:  $ 
@@ -117,8 +117,7 @@ sub summary_pages {
                      );
   }
 
-  my ($index_file) = (FileStructure::get_filename($tree,'tree_HTML').
-                      "/index.html");
+  my ($index_file) = FileStructure::get_filename($tree,'index');
 
   push @index_html, (
                      "<html>\n\t<head><title>Tree: $tree</title></head>\n".
@@ -293,7 +292,10 @@ EOF
 EOF
   }
 
-  my $global_index_file = "$FileStructure::TINDERBOX_DIR/index.html";
+  my $global_index_file = (
+                           $FileStructure::TINDERBOX_DIR.
+                           "/".$FileStructure::GLOBAL_INDEX_FILE
+                          );
   
   main::overwrite_file ($global_index_file, $out);
 
@@ -345,7 +347,7 @@ EOF
                                      "/status\.html"),
                           );
   
-  $body .= "\t\t</tr><tr>\n\n";
+  $body .= "\t\t</th></tr><tr>\n\n";
   
   for ($i=0; $i <= $#BUILD_NAMES; $i++) {
     my ($buildname) = $BUILD_NAMES[$i];
