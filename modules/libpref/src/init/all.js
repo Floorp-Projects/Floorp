a4d5d3263b5432e92f234b65e6169093309641cd
slamm@netscape.com <slamm@netscape.com>
2000-08-30 23:41:17 +0000

    Sidebar customize dialog: More panels link (47585). r=mcafee

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -503,6 +503,7 @@ pref("custtoolbar.Composer.Formatting_Toolbar.showing", true);
 pref("custtoolbar.Composer.Formatting_Toolbar.open", true);
 
 pref("sidebar.customize.all_panels.url", "http://sidebar-rdf.netscape.com/%LOCALE%/sidebar-rdf/%SIDEBAR_VERSION%/all-panels.rdf");
+pref("sidebar.customize.more_panels.url", "http://dmoz.org/Netscape/Sidebar/");
 
 pref("prefs.converted-to-utf8",false);
 
