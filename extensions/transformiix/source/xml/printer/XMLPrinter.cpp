d8380d7556ec0f49d9ea31e13eab6a85e1c8dba1
kvisco%ziplink.net <kvisco%ziplink.net>
2000-09-07 03:40:24 +0000

    Not part of regular build, a=leaf. Added the missing char casts after doing the UTF-8 encoding.

--- extensions/transformiix/source/xml/printer/XMLPrinter.cpp
+++ extensions/transformiix/source/xml/printer/XMLPrinter.cpp
@@ -23,7 +23,7 @@
  * Bob Miller, kbob@oblix.com
  *    -- plugged core leak.
  *
- * $Id: XMLPrinter.cpp,v 1.6 2000/09/06 09:48:12 kvisco%ziplink.net Exp $
+ * $Id: XMLPrinter.cpp,v 1.7 2000/09/07 03:40:24 kvisco%ziplink.net Exp $
  */
 
 #include "printers.h"
@@ -36,7 +36,7 @@
  * A class for printing XML nodes.
  * This class was ported from XSL:P Java source
  * @author <a href="kvisco@ziplink.net">Keith Visco</a>
- * @version $Revision: 1.6 $ $Date: 2000/09/06 09:48:12 $
+ * @version $Revision: 1.7 $ $Date: 2000/09/07 03:40:24 $
 **/
 
 /**
@@ -385,14 +385,14 @@ void XMLPrinter::printUTF8Char(DOM_CHAR ch) const {
     */
     //-- 0x0080-0x07FF
     else if (ch < 2048) {
-        out << (192+(ch/64));   // 0xC0 + x/64
-        out << (128+(ch%64));   // 0x80 + x%64
+        out << (char) (192+(ch/64));        // 0xC0 + x/64
+        out << (char) (128+(ch%64));        // 0x80 + x%64
     }
     //-- 0x800-0xFFFF
     else {
-        out << (224+(ch/4096));      // 0xE0 + x/64^2
-        out << (128+((ch/64)%64));   // 0x80 + (x/64)%64
-        out << (128+(ch%64));        // 0x80 + x%64
+        out << (char) (224+(ch/4096));      // 0xE0 + x/64^2
+        out << (char) (128+((ch/64)%64));   // 0x80 + (x/64)%64
+        out << (char) (128+(ch%64));        // 0x80 + x%64
     }
 } //-- printUTF8Char
 
@@ -420,14 +420,14 @@ void XMLPrinter::printUTF8Chars(const String& data) {
         */
         //-- 0x0080-0x07FF
         else if (ch < 2048) {
-            out << (192+(ch/64));   // 0xC0 + x/64
-            out << (128+(ch%64));   // 0x80 + x%64
+            out << (char)(192+(ch/64));        // 0xC0 + x/64
+            out << (char)(128+(ch%64));        // 0x80 + x%64
         }
         //-- 0x800-0xFFFF
         else {
-            out << (224+(ch/4096));      // 0xE0 + x/64^2
-            out << (128+((ch/64)%64));   // 0x80 + (x/64)%64
-            out << (128+(ch%64));        // 0x80 + x%64
+            out << (char)(224+(ch/4096));      // 0xE0 + x/64^2
+            out << (char)(128+((ch/64)%64));   // 0x80 + (x/64)%64
+            out << (char)(128+(ch%64));        // 0x80 + x%64
         }
     }
 
