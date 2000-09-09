f9628a5ea2836b7144f31acd31980f20abd18536
scc%mozilla.org <scc%mozilla.org>
2000-09-09 06:42:45 +0000

    nobody uses |nsFragmentedString| as yet ... changing it now to be based on the new work from |nsSlidingString|/|nsSlidingSubstring| for the parser

--- string/src/nsFragmentedString.cpp
+++ string/src/nsFragmentedString.cpp
@@ -25,3 +25,138 @@
 
 #include "nsFragmentedString.h"
 
+
+const PRUnichar*
+nsFragmentedString::GetReadableFragment( nsReadableFragment<PRUnichar>& aFragment, nsFragmentRequest aRequest, PRUint32 aOffset ) const
+  {
+    const nsSharedBufferList::Buffer* buffer = 0;
+    switch ( aRequest )
+      {
+        case kPrevFragment:
+          buffer = NS_STATIC_CAST(const nsSharedBufferList::Buffer*, aFragment.mFragmentIdentifier)->mPrev;
+          break;
+
+        case kFirstFragment:
+          buffer = mBufferList.GetFirstBuffer();
+          break;
+
+        case kLastFragment:
+          buffer = mBufferList.GetLastBuffer();
+          break;
+
+        case kNextFragment:
+          buffer = NS_STATIC_CAST(const nsSharedBufferList::Buffer*, aFragment.mFragmentIdentifier)->mNext;
+          break;
+
+        case kFragmentAt:
+          // ...work...
+          break;
+      }
+
+    if ( buffer )
+      {
+        aFragment.mStart  = buffer->DataStart();
+        aFragment.mEnd    = buffer->DataEnd();
+        aFragment.mFragmentIdentifier = buffer;
+        return aFragment.mStart + aOffset;
+      }
+
+    return 0;
+  }
+
+
+PRUnichar*
+nsFragmentedString::GetWritableFragment( nsWritableFragment<PRUnichar>& aFragment, nsFragmentRequest aRequest, PRUint32 aOffset )
+  {
+    nsSharedBufferList::Buffer* buffer = 0;
+    switch ( aRequest )
+      {
+        case kPrevFragment:
+          buffer = NS_STATIC_CAST(nsSharedBufferList::Buffer*, aFragment.mFragmentIdentifier)->mPrev;
+          break;
+
+        case kFirstFragment:
+          buffer = mBufferList.GetFirstBuffer();
+          break;
+
+        case kLastFragment:
+          buffer = mBufferList.GetLastBuffer();
+          break;
+
+        case kNextFragment:
+          buffer = NS_STATIC_CAST(nsSharedBufferList::Buffer*, aFragment.mFragmentIdentifier)->mNext;
+          break;
+
+        case kFragmentAt:
+          // ...work...
+          break;
+      }
+
+    if ( buffer )
+      {
+        aFragment.mStart  = buffer->DataStart();
+        aFragment.mEnd    = buffer->DataEnd();
+        aFragment.mFragmentIdentifier = buffer;
+        return aFragment.mStart + aOffset;
+      }
+
+    return 0;
+  }
+
+  /**
+   * ...
+   */
+PRUint32
+nsFragmentedString::Length() const
+  {
+    return PRUint32(mBufferList.GetDataLength());
+  }
+
+  /**
+   * |SetLength|
+   */
+void
+nsFragmentedString::SetLength( PRUint32 aNewLength )
+  {
+    // according to the current interpretation of |SetLength|,
+    //  cut off characters from the end, or else add unitialized space to fill
+
+    if ( aNewLength < mBufferList.GetDataLength() )
+      {
+//      if ( aNewLength )
+          mBufferList.DiscardSuffix(mBufferList.GetDataLength()-aNewLength);
+//      else
+//        mBufferList.DestroyBuffers();
+      }
+
+// temporarily... eliminate as soon as our munging routines don't need this form of |SetLength|
+    else if ( aNewLength > mBufferList.GetDataLength() )
+      {
+        size_t empty_space_to_add = aNewLength - mBufferList.GetDataLength();
+        nsSharedBufferList::Buffer* new_buffer = nsSharedBufferList::NewBuffer(0, 0, empty_space_to_add);
+        new_buffer->DataEnd(new_buffer->DataStart()+empty_space_to_add);
+        mBufferList.LinkBuffer(mBufferList.GetLastBuffer(), new_buffer, 0);
+      }
+  }
+
+
+#if 0
+  /**
+   * |SetCapacity|.
+   *
+   * If a client tries to increase the capacity of multi-fragment string, perhaps a single
+   * empty fragment of the appropriate size should be appended.
+   */
+void
+nsFragmentedString::SetCapacity( PRUint32 aNewCapacity )
+  {
+    if ( !aNewCapacity )
+      {
+        // |SetCapacity(0)| is special and means ``release all storage''.
+      }
+    else if ( aNewCapacity > ... )
+      {
+        
+      }
+  }
+#endif
