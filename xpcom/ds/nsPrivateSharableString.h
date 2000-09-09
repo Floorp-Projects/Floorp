0f79c4507d203fbdefa258c8041191ac0e284b7c
scc@mozilla.org <scc@mozilla.org>
2000-09-09 00:02:48 +0000

    adding license and include guards

--- xpcom/ds/nsPrivateSharableString.h
+++ xpcom/ds/nsPrivateSharableString.h
@@ -1,3 +1,30 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * The Original Code is Mozilla strings.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 2000 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ *   Scott Collins <scc@mozilla.org> (original author)
+ *
+ */
+
+#ifndef nsPrivateSharableString_h___
+#define nsPrivateSharableString_h___
+
 #include "nsBufferHandle.h"
 
   /**
@@ -24,3 +51,5 @@ nsPrivateSharableString<CharT>::GetSharedBufferHandle() const
   {
     return 0;
   }
+
+#endif // !defined(nsPrivateSharableString_h___)
--- xpcom/string/public/nsSlidingString.h
+++ xpcom/string/public/nsSlidingString.h
@@ -1,3 +1,30 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * The Original Code is Mozilla strings.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 2000 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ *   Scott Collins <scc@mozilla.org> (original author)
+ *
+ */
+
+#ifndef nsSlidingString_h___
+#define nsSlidingString_h___
+
 #include "nsAReadableString.h"
 #include "nsSharedBufferList.h"
 
@@ -89,3 +116,5 @@ class nsSlidingString
 
 typedef nsSlidingString     nsParserString;
 typedef nsSlidingSubstring  nsParserSubstring;
+
+#endif // !defined(nsSlidingString_h___)
