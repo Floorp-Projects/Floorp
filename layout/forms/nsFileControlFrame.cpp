ecfca9724db85a2f920801b0c4143ca912ee2bfd
rods%netscape.com <rods%netscape.com>
2000-09-09 22:13:12 +0000

    wasn't setting status to NS_FRAME_COMPLETE b=49010 r=buster

--- layout/forms/nsFileControlFrame.cpp
+++ layout/forms/nsFileControlFrame.cpp
@@ -346,6 +346,8 @@ NS_IMETHODIMP nsFileControlFrame::Reflow(nsIPresContext*          aPresContext,
 {
   DO_GLOBAL_REFLOW_COUNT("nsFileControlFrame", aReflowState.reason);
 
+  aStatus = NS_FRAME_COMPLETE;
+
   if (mFormFrame == nsnull && eReflowReason_Initial == aReflowState.reason) {
     // add ourself as an nsIFormControlFrame
     nsFormFrame::AddFormControlFrame(aPresContext, *NS_STATIC_CAST(nsIFrame*, this));
