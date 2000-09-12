bc4ba277fb2199d162a5d74479ccf59ccb383b4c
shaver%mozilla.org <shaver%mozilla.org>
2000-09-12 02:35:29 +0000

    Part 1.1 of 18352 (remove extensions/ dependencies from non-extensions/ code):   Teach netwerk about nsIPasswordManager.   (Craves better interfaces, current versions just C&P'd from wallet's.) r=valeski,a=brendan

---
+++ netwerk/base/public/nsIPasswordManagerUtils.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Zero-Knowledge
+ * Systems, Inc.  Portions created by Zero-Knowledge are Copyright
+ * (C) 2000 Zero-Knowledge Systems, Inc. All Rights Reserved.
+ *
+ * Contributor(s):
+ */
+
+#ifndef NSIPASSWORDMANAGERUTILS_H__
+#define NSIPASSWORDMANAGERUTILS_H__
+
+#include "nsIPasswordManager.h"
+
+/* XXX love me, rayw, love me true */
+#define PASSWORDMANAGER_PROGID "mozilla.network.password-manager.1"
+
+#endif /* NSIPASSWORDMANAGERUTILS_H__ */
