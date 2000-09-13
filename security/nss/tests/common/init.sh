2d7c63b11e7f9b04eff543f464c619e0970a329a
wtc@netscape.com <wtc@netscape.com>
2000-09-13 19:19:05 +0000

    Get OBJDIR_NAME and OS_ARCH from the makefile as opposed to the arch.sh script.  arch.sh should be cvs removed when we are sure about this change. Checkin on behalf of Sonja Mirtitsch <sonmi@netscape.com>. Modified file: init.sh Added file: Makefile

--- security/nss/tests/common/init.sh
+++ security/nss/tests/common/init.sh
@@ -10,8 +10,8 @@ COMMON=${TEST_COMMON-$common}
 DIST=${DIST-${MOZILLA_ROOT}/dist}
 SECURITY_ROOT=${SECURITY_ROOT-${MOZILLA_ROOT}/security/nss}
 TESTDIR=${TESTDIR-${MOZILLA_ROOT}/tests_results/security}
-. ../common/arch.sh
-OBJDIR=${OBJDIR_NAME}
+OBJDIR=`cd ../common; gmake objdir_name` 
+OS_ARCH=`cd ../common; gmake os_arch`
 if [ ${OS_ARCH} = "WINNT" ]; then
 PATH=${DIST}/${OBJDIR}/bin\;${DIST}/${OBJDIR}/lib\;$PATH
 else
