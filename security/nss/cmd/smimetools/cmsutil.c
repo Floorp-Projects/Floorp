0983b2f44a9a97aab0a39241264efb94ac503be4
chrisk@netscape.com <chrisk@netscape.com>
2000-09-19 19:34:46 +0000

    Fix bug # 53221: Call RNG_SystemInfoForRNG to provide RNG with enough entropy for proper operation.

--- security/nss/cmd/smimetools/cmsutil.c
+++ security/nss/cmd/smimetools/cmsutil.c
@@ -34,7 +34,7 @@
 /*
  * cmsutil -- A command to work with CMS data
  *
- * $Id: cmsutil.c,v 1.5 2000/06/23 16:40:30 chrisk%netscape.com Exp $
+ * $Id: cmsutil.c,v 1.6 2000/09/19 19:34:46 chrisk%netscape.com Exp $
  */
 
 #include "nspr.h"
@@ -836,7 +836,8 @@ main(int argc, char **argv)
     /* Call the libsec initialization routines */
     PR_Init(PR_SYSTEM_THREAD, PR_PRIORITY_NORMAL, 1);
     SECU_PKCS11Init(PR_FALSE);
-    SEC_Init();
+    RNG_SystemInfoForRNG();     /* SECU_PKCS11Init does not call this */
+    SEC_Init();                 /* this does nothing, actually */
 
     /* open cert database */
     options.certHandle = OpenCertDB(progName);
