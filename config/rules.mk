7ea00d25146d56848fa1c7626153dd79dc9f1f36
warren%netscape.com <warren%netscape.com>
2000-09-20 21:28:30 +0000

    Taking stab at change -- need to use topsrcdir for objDir build system.

--- config/rules.mk
+++ config/rules.mk
@@ -1132,17 +1132,14 @@ endif
 ################################################################################
 # CHROME PACKAGING
 
-JAR_MANIFEST := jar.mn
+JAR_MANIFEST := $(srcdir)/jar.mn
 
 chrome::
-	@if test -f $(JAR_MANIFEST); then $(PERL) $(MOZILLA_DIR)/config/make-jars.pl -c -d $(DIST)/bin/chrome < $(JAR_MANIFEST); fi
+	@if test -f $(JAR_MANIFEST); then $(PERL) $(topsrcdir)/config/make-jars.pl -c -d $(DIST)/bin/chrome < $(JAR_MANIFEST); fi
 
 install:: chrome
 
-REGCHROME = echo $(PERL) $(MOZILLA_DIR)/config/add-chrome.pl $(DIST)/bin/chrome/installed-chrome.txt; \
-	    pwd; \
-	    ls $(MOZILLA_DIR)/config; \
-	    $(PERL) $(MOZILLA_DIR)/config/add-chrome.pl $(DIST)/bin/chrome/installed-chrome.txt
+REGCHROME = $(PERL) $(topsrcdir)/config/add-chrome.pl $(DIST)/bin/chrome/installed-chrome.txt
 
 ##############################################################################
 
