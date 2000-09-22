6db32983b4291d148e82c2d44f4a6bd3f354e477
scc%mozilla.org <scc%mozilla.org>
2000-09-22 07:51:47 +0000

    bug #36908: checking in for endico, r=scc, a=brendan

---
+++ netwerk/protocol/about/src/nsAboutPlugins.cpp
@@ -0,0 +1,59 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): Sammy Ford <sford@swbell.net>
+ *                 Dawn Endico <endico@mozilla.org>
+ */
+
+#include "nsAboutPlugins.h"
+#include "nsIIOService.h"
+#include "nsIServiceManager.h"
+#include "nsCOMPtr.h"
+#include "nsIURI.h"
+
+static NS_DEFINE_CID(kIOServiceCID, NS_IOSERVICE_CID);
+
+NS_IMPL_ISUPPORTS(nsAboutPlugins, NS_GET_IID(nsIAboutModule));
+
+static const char kPluginsPage[] = "chrome://global/content/plugins.html";
+
+NS_IMETHODIMP
+nsAboutPlugins::NewChannel(nsIURI *aURI, nsIChannel **result)
+{
+    nsresult rv;
+    NS_WITH_SERVICE(nsIIOService, ioService, kIOServiceCID, &rv);
+    if (NS_FAILED(rv))
+        return rv;
+    rv = ioService->NewChannel(kPluginsPage, nsnull, result);
+    return rv;
+}
+
+NS_METHOD
+nsAboutPlugins::Create(nsISupports *aOuter, REFNSIID aIID, void **aResult)
+{
+    nsAboutPlugins* about = new nsAboutPlugins();
+    if (about == nsnull)
+        return NS_ERROR_OUT_OF_MEMORY;
+    NS_ADDREF(about);
+    nsresult rv = about->QueryInterface(aIID, aResult);
+    NS_RELEASE(about);
+    return rv;
+}
+
+////////////////////////////////////////////////////////////////////////////////
