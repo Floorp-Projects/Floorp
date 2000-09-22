2e1119547d26358d7a3994cd517374fcc949fb98
jim_nance%yahoo.com <jim_nance%yahoo.com>
2000-09-22 09:18:40 +0000

    Fixed 50782.  Multiply defined symbols cause Tru64 5.0 to crash. Found and fixed by harbaugh.  Fix improved by tor, r=pnunn, a=brendan

--- modules/libimg/gifcom/gif.cpp
+++ modules/libimg/gifcom/gif.cpp
@@ -77,9 +77,6 @@ mailing address.
 #define HOWMANY(x, r)     (((x) + ((r) - 1)) / (r))
 #define ROUNDUP(x, r)     (HOWMANY(x, r) * (r))
 
-int il_debug;
-PRLogModuleInfo *il_log_module = NULL;
-
 #ifndef MAX
 #    define MAX(x, y)   (((x) > (y)) ? (x) : (y))
 #endif
