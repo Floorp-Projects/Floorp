b4fbcf40560fd99cc0857bdfbc9309147e0c1cc2
waldemar@netscape.com <waldemar@netscape.com>
2000-09-22 08:14:56 +0000

    Removed "constructor" keyword

--- js/semantics/JS20/Lexer.lisp
+++ js/semantics/JS20/Lexer.lisp
@@ -236,7 +236,7 @@
        (%print-actions)
        
        (define reserved-words (vector string)
-         (vector "abstract" "break" "case" "catch" "class" "const" "constructor" "continue" "debugger" "default" "delete" "do" "else" "enum"
+         (vector "abstract" "break" "case" "catch" "class" "const" "continue" "debugger" "default" "delete" "do" "else" "enum"
                  "eval" "export" "extends" "false" "final" "finally" "for" "function" "goto" "if" "implements" "import" "in" "include"
                  "instanceof" "interface" "namespace" "native" "new" "null" "package" "private" "protected" "public" "return" "static" "super"
                  "switch" "synchronized" "this" "throw" "throws" "transient" "true" "try" "typeof" "use" "var" "volatile" "while" "with"))
