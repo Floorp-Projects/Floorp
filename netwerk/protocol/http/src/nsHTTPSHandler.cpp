355a21abced286a275eed8c55784a94d1d6a440c
rpotts%netscape.com <rpotts%netscape.com>
2000-10-03 04:49:51 +0000

    bug #54239 (r=dougt).  PSM can be initialized on the wrong thread...

--- netwerk/protocol/http/src/nsHTTPSHandler.cpp
+++ netwerk/protocol/http/src/nsHTTPSHandler.cpp
@@ -24,12 +24,24 @@
 #include "nsHTTPSHandler.h"
 #include "nsISocketTransportService.h"
 #include "nsIServiceManager.h"
+#include "nsIPSMComponent.h"
 
 static NS_DEFINE_CID(kSocketTransportServiceCID, NS_SOCKETTRANSPORTSERVICE_CID);
 
 nsHTTPSHandler::nsHTTPSHandler()
 : nsHTTPHandler()
 {
+    nsresult rv;
+    nsISupports *psm = nsnull;
+    //
+    // Initialize the PSM component.
+    // This is to ensure that PSM is initialized on the main UI thread.
+    // 
+    rv = nsServiceManager::GetService( PSM_COMPONENT_CONTRACTID,
+                                       NS_GET_IID(nsIPSMComponent), 
+                                       (nsISupports**)&psm);  
+
+    NS_IF_RELEASE(psm);
 }
 
 nsHTTPSHandler::~nsHTTPSHandler()
