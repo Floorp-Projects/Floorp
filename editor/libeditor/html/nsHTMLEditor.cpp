4ddd828e505dcb2558be74269d409cee63dfa174
kin%netscape.com <kin%netscape.com>
2000-10-05 19:38:46 +0000

    Fix for bug #50570 ([MLK] Leaking PRUnichar buffer in nsHTMLEditor::InsertFromTransferable()) Added calls to nsMemory::Free() to free text in PasteAsPlainTextQuotation() and InsertFromTransferable(). r=brade@netscape.com sr=sfraser@netscape.com

--- editor/libeditor/html/nsHTMLEditor.cpp
+++ editor/libeditor/html/nsHTMLEditor.cpp
@@ -4922,6 +4922,8 @@ NS_IMETHODIMP nsHTMLEditor::InsertFromTransferable(nsITransferable *transferable
         stuffToPaste.Assign ( text, len / 2 );
         nsAutoEditBatch beginBatching(this);
         rv = InsertHTML(stuffToPaste);
+        if (text)
+          nsMemory::Free(text);
       }
     }
     else if (flavor.EqualsWithConversion(kUnicodeMime))
@@ -4936,6 +4938,8 @@ NS_IMETHODIMP nsHTMLEditor::InsertFromTransferable(nsITransferable *transferable
         // pasting does not inherit local inline styles
         RemoveAllInlineProperties();
         rv = InsertText(stuffToPaste);
+        if (text)
+          nsMemory::Free(text);
       }
     }
     else if (flavor.EqualsWithConversion(kFileMime))
@@ -5518,6 +5522,8 @@ NS_IMETHODIMP nsHTMLEditor::PasteAsPlaintextQuotation(PRInt32 aSelectionType)
         stuffToPaste.Assign ( text, len / 2 );
         nsAutoEditBatch beginBatching(this);
         rv = InsertAsPlaintextQuotation(stuffToPaste, 0);
+        if (text)
+          nsMemory::Free(text);
       }
     }
     nsCRT::free(flav);
