f93e79c545ca83f25a44e1aed0e7040430c818bb
jst@netscape.com <jst@netscape.com>
2000-10-07 12:28:41 +0000

    Removing old XIF tests (patch from akkana)

--- parser/htmlparser/tests/outsinks/TestOutSinks.pl
+++ parser/htmlparser/tests/outsinks/TestOutSinks.pl
@@ -30,29 +30,29 @@ $ENV{LD_LIBRARY_PATH} .= ":.";
 
 $errmsg = "";
 
-print "Testing simple html to html ...\n";
-$status = system("TestOutput -i text/html -o text/html -f 0 -c OutTestData/simple.html OutTestData/simple.html");
-if ($status != 0) {
-  print "Simple html to html failed.\n";
-  $errmsg = "$errmsg simple.html";
-}
+# print "Testing simple html to html ...\n";
+# $status = system("./TestOutput -i text/html -o text/html -f 0 -c OutTestData/simple.html OutTestData/simple.html");
+# if ($status != 0) {
+#   print "Simple html to html failed.\n";
+#   $errmsg = "$errmsg simple.html";
+# }
 
 print "Testing simple copy case ...\n";
-$status = system("TestOutput -i text/html -o text/plain -f 0 -w 0 -c OutTestData/simplecopy.out OutTestData/simple.html");
+$status = system("./TestOutput -i text/html -o text/plain -f 0 -w 0 -c OutTestData/simplecopy.out OutTestData/simple.html");
 if ($status != 0) {
   print "Simple copy test failed.\n";
   $errmsg = "$errmsg simplecopy.out";
 }
 
 print "Testing simple html to plaintext formatting ...\n";
-$status = system("TestOutput -i text/html -o text/plain -f 34 -w 70 -c OutTestData/simplefmt.out OutTestData/simple.html");
+$status = system("./TestOutput -i text/html -o text/plain -f 34 -w 70 -c OutTestData/simplefmt.out OutTestData/simple.html");
 if ($status != 0) {
   print("Simple formatting test failed.\n");
   $errmsg = "$errmsg simplefmt.out ";
 }
 
 print "Testing non-wrapped plaintext in preformatted mode ...\n";
-$status = system("TestOutput -i text/html -o text/plain -f 16 -c OutTestData/plainnowrap.out OutTestData/plain.html");
+$status = system("./TestOutput -i text/html -o text/plain -f 16 -c OutTestData/plainnowrap.out OutTestData/plain.html");
 if ($status != 0) {
   print "Non-wrapped plaintext test failed.\n";
   $errmsg = "$errmsg plainnowrap.out";
@@ -66,52 +66,24 @@ if ($status != 0) {
 # }
 
 print "Testing mail quoting ...\n";
-$status = system("TestOutput -i text/html -o text/plain -f 2 -w 50 -c OutTestData/mailquote.out OutTestData/mailquote.html");
+$status = system("./TestOutput -i text/html -o text/plain -f 2 -w 50 -c OutTestData/mailquote.out OutTestData/mailquote.html");
 if ($status != 0) {
   print "Mail quoting test failed.\n";
   $errmsg = "$errmsg mailquote.out";
 }
 
 print "Testing format=flowed output ...\n";
-$status = system("TestOutput -i text/html -o text/plain -f 66 -w 50 -c OutTestData/simplemail.out OutTestData/simplemail.html");
+$status = system("./TestOutput -i text/html -o text/plain -f 66 -w 50 -c OutTestData/simplemail.out OutTestData/simplemail.html");
 if ($status != 0) {
   print "Format=flowed test failed.\n";
   $errmsg = "$errmsg simplemail.out";
 }
 
-print "Testing conversion of XIF entities ...\n";
-$status = system("TestOutput -i text/xif -o text/plain -c OutTestData/entityxif.out OutTestData/entityxif.xif");
-if ($status != 0) {
-  print "XIF entity conversion test failed.\n";
-  $errmsg = "$errmsg entityxif.out";
-}
-
-print "Testing XIF to HTML ...\n";
-$status = system("TestOutput -i text/xif -o text/html -c OutTestData/xifstuff.out OutTestData/xifstuff.xif");
-if ($status != 0) {
-  print "XIF to HTML conversion test failed.\n";
-  $errmsg = "$errmsg xifstuff.out";
-}
-
 print "Testing HTML Table to Text ...\n";
-$status = system("TestOutput -i text/html -o text/plain -f 2 -c OutTestData/htmltable.out OutTestData/htmltable.html");
+$status = system("./TestOutput -i text/html -o text/plain -f 2 -c OutTestData/htmltable.out OutTestData/htmltable.html");
 if ($status != 0) {
   print "HTML Table to Plain text failed.\n";
   $errmsg = "$errmsg htmltable.out";
-}
-
-print "Testing XIF to plain with doctype (bug 28447) ...\n";
-$status = system("TestOutput -i text/xif -o text/plain -f 2 -c OutTestData/xifdtplain.out OutTestData/doctype.xif");
-if ($status != 0) {
-  print "XIF to plain with doctype failed.\n";
-  $errmsg = "$errmsg xifdtplain.out";
-}
-
-print "Testing XIF to html with doctype ...\n";
-$status = system("TestOutput -i text/xif -o text/html -f 0 -c OutTestData/xifdthtml.out OutTestData/doctype.xif");
-if ($status != 0) {
-  print "XIF to html with doctype failed.\n";
-  $errmsg = "$errmsg xifdthtml.out";
 }
 
 if ($errmsg ne "") {
