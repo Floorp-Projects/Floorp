620595269346a5a13b82aed1991a6dd82f7ef03b
mcgreer@netscape.com <mcgreer@netscape.com>
2000-10-11 00:56:32 +0000

    add a new function, SECU_TextFileToItem, for reading text files.

--- security/nss/cmd/lib/secutil.h
+++ security/nss/cmd/lib/secutil.h
@@ -193,6 +193,7 @@ extern const char * SECU_Strerror(PRErrorCode errNum);
 
 /* Read the contents of a file into a SECItem */
 extern SECStatus SECU_FileToItem(SECItem *dst, PRFileDesc *src);
+extern SECStatus SECU_TextFileToItem(SECItem *dst, PRFileDesc *src);
 
 /* Read in a DER from a file, may be ascii  */
 extern SECStatus 
