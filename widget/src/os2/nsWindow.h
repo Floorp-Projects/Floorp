451bf70b7574e8ae1e33830387ac6ecde6000c4f
mkaply@us.ibm.com <mkaply@us.ibm.com>
2000-10-16 22:30:06 +0000

    #56770 r=mkaply, a=blizzard More attempts to fix keyboard stuff.

--- widget/src/os2/nsWindow.h
+++ widget/src/os2/nsWindow.h
@@ -295,6 +295,24 @@ protected:
 #define PM2NS_PARENT NS2PM_PARENT
 #define PM2NS NS2PM
 
+#define PMSCAN_PAD7         0x47
+#define PMSCAN_PAD8         0x48
+#define PMSCAN_PAD9         0x49
+#define PMSCAN_PADMINUS     0x4A
+#define PMSCAN_PAD4         0x4B
+#define PMSCAN_PAD5         0x4C
+#define PMSCAN_PAD6         0x4D
+#define PMSCAN_PADPLUS      0x4E
+#define PMSCAN_PAD1         0x4F
+#define PMSCAN_PAD2         0x50
+#define PMSCAN_PAD3         0x51
+#define PMSCAN_PAD0         0x52
+
+#define isNumPadScanCode(scanCode) !( (scanCode < PMSCAN_PAD7) ||      \
+                                      (scanCode > PMSCAN_PAD0) ||      \
+                                      (scanCode == PMSCAN_PADMINUS) || \
+                                      (scanCode == PMSCAN_PADPLUS) )
+
 extern PRUint32 WMChar2KeyCode( MPARAM mp1, MPARAM mp2);
 
 extern nsWindow *NS_HWNDToWindow( HWND hwnd);
