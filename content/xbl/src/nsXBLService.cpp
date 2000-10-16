5ffab9368c4b1620705f13c05d33e75e90a693f8
hyatt@netscape.com <hyatt@netscape.com>
2000-10-16 21:46:59 +0000

    Fix for 56235. r=saari, a=waterson

--- content/xbl/src/nsXBLService.cpp
+++ content/xbl/src/nsXBLService.cpp
@@ -949,6 +949,10 @@ NS_IMETHODIMP nsXBLService::GetBindingInternal(nsIContent* aBoundElement,
   else
     protoBinding->GetBindingElement(getter_AddRefs(child));
 
+  NS_ASSERTION(protoBinding, "Unable to locate an XBL binding.");
+  if (!protoBinding)
+    return NS_ERROR_FAILURE;
+
   // If our prototype already has a base, then don't check for an "extends" attribute.
   nsCOMPtr<nsIXBLBinding> baseBinding;
   nsCOMPtr<nsIXBLPrototypeBinding> baseProto;
