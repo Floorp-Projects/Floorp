3107bd8ed63383e250ac59061498906d9fe38ecc
pierre%netscape.com <pierre%netscape.com>
2000-10-25 04:23:37 +0000

    56950 "SeaMonkey crash in css code". Check error codes when constructing a DocElementTableFrame. r=attinasi, sr=buster, a=phil.

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -3188,6 +3188,10 @@ nsCSSFrameConstructor::ConstructDocElementTableFrame(nsIPresShell*        aPresS
 
   ConstructFrame(aPresShell, aPresContext, state, aDocElement, aParentFrame, frameItems);
   aNewTableFrame = frameItems.childList;
+  if (!aNewTableFrame) {
+    NS_WARNING("cannot get table contentFrame");
+    return NS_ERROR_FAILURE;
+  }
   return NS_OK;
 }
 
@@ -3274,6 +3278,8 @@ nsCSSFrameConstructor::ConstructDocElementFrame(nsIPresShell*        aPresShell,
           
 */    
 
+  aNewFrame = nsnull;
+
   if (!mTempFrameTreeState)
     aPresShell->CaptureHistoryState(getter_AddRefs(mTempFrameTreeState));
 
@@ -3342,12 +3348,16 @@ nsCSSFrameConstructor::ConstructDocElementFrame(nsIPresShell*        aPresShell,
 
   nsIFrame* contentFrame = nsnull;
   PRBool isBlockFrame = PR_FALSE;
+  nsresult rv;
 
   if (docElemIsTable) {
       // if the document is a table then just populate it.
-      ConstructDocElementTableFrame(aPresShell, aPresContext, aDocElement, 
+      rv = ConstructDocElementTableFrame(aPresShell, aPresContext, aDocElement, 
                                     aParentFrame, contentFrame,
                                     aState.mFrameState);
+      if (NS_FAILED(rv)) {
+        return rv;
+      }
       contentFrame->GetStyleContext(getter_AddRefs(styleContext));
   } else {
         // otherwise build a box or a block
@@ -3355,12 +3365,18 @@ nsCSSFrameConstructor::ConstructDocElementFrame(nsIPresShell*        aPresShell,
         PRInt32 nameSpaceID;
         if (NS_SUCCEEDED(aDocElement->GetNameSpaceID(nameSpaceID)) &&
             nameSpaceID == nsXULAtoms::nameSpaceID) {
-          NS_NewBoxFrame(aPresShell, &contentFrame, PR_TRUE);
+          rv = NS_NewBoxFrame(aPresShell, &contentFrame, PR_TRUE);
+          if (NS_FAILED(rv)) {
+            return rv;
+          }
         }
         else
 #endif 
         {
-          NS_NewDocumentElementFrame(aPresShell, &contentFrame);
+          rv = NS_NewDocumentElementFrame(aPresShell, &contentFrame);
+          if (NS_FAILED(rv)) {
+            return rv;
+          }
           isBlockFrame = PR_TRUE;
 
           // Since we always create a block frame, we need to make sure that the 
