7aef21cf2fc1a8d695a51259f400eabc50a01b05
sspitzer@netscape.com <sspitzer@netscape.com>
2000-10-26 03:36:21 +0000

    fix for #56002.  for security, add a "salt" directory between the profile directory and the profile data.  r=racham, sr=alecf

--- profile/src/nsProfile.h
+++ profile/src/nsProfile.h
@@ -75,7 +75,8 @@ private:
 	nsresult EnsureProfileFileExists(nsIFile *aFile);
 								
 	nsresult CloneProfileDirectorySpec(nsILocalFile **aLocalFile);
-	
+    nsresult AddLevelOfIndirection(nsIFile *aDir);
+
     PRBool mAutomigrate;
     PRBool mOutofDiskSpace;
     PRBool mDiskSpaceErrorQuitCalled;
