c1c578e54aa683e3e7cd1bd5ae900aef066aa9b7
warren@netscape.com <warren@netscape.com>
2000-10-27 23:41:27 +0000

    Removed extra 0 arg from macro

--- xpcom/io/nsLocalFileOS2.h
+++ xpcom/io/nsLocalFileOS2.h
@@ -42,7 +42,7 @@
 #include "nsLocalFile.h"
 #include "nslog.h"
 
-NS_IMPL_LOG(nsLocalFileOS2Log, 0)
+NS_IMPL_LOG(nsLocalFileOS2Log)
 #define PRINTF NS_LOG_PRINTF(nsLocalFileOS2Log)
 #define FLUSH  NS_LOG_FLUSH(nsLocalFileOS2Log)
 
