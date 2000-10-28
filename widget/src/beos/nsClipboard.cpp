bf11d32788019123bbeda0fbdb26b7803e2f5e5f
warren%netscape.com <warren%netscape.com>
2000-10-28 10:03:09 +0000

    Fixed printfs for beos

--- widget/src/beos/nsClipboard.cpp
+++ widget/src/beos/nsClipboard.cpp
@@ -31,9 +31,14 @@
 #include "nsIWidget.h"
 #include "nsIServiceManager.h"
 #include "nsWidgetsCID.h"
-#include "nslog.h"
 
 #include <View.h>
+
+#include "nslog.h"
+
+NS_IMPL_LOG(nsClipboardLog)
+#define PRINTF NS_LOG_PRINTF(nsClipboardLog)
+#define FLUSH  NS_LOG_FLUSH(nsClipboardLog)
 
 // The class statics:
 BView *nsClipboard::sView = 0;
@@ -56,7 +61,7 @@ static NS_DEFINE_CID(kCClipboardCID,       NS_CLIPBOARD_CID);
 nsClipboard::nsClipboard() : nsBaseClipboard()
 {
 #ifdef DEBUG_CLIPBOARD
-  PRINTF(("  nsClipboard::nsClipboard()\n"));
+  PRINTF("  nsClipboard::nsClipboard()\n");
 #endif /* DEBUG_CLIPBOARD */
 
   //NS_INIT_REFCNT();
@@ -75,7 +80,7 @@ nsClipboard::nsClipboard() : nsBaseClipboard()
 nsClipboard::~nsClipboard()
 {
 #ifdef DEBUG_CLIPBOARD
-  PRINTF(("  nsClipboard::~nsClipboard()\n"));  
+  PRINTF("  nsClipboard::~nsClipboard()\n");  
 #endif /* DEBUG_CLIPBOARD */
 
 //  // Remove all our event handlers:
@@ -125,13 +130,13 @@ void nsClipboard::SetTopLevelView(BView *v)
   if(v == 0 || v->Window() == 0)
   {
 #ifdef DEBUG_CLIPBOARD
-    PRINTF(("  nsClipboard::SetTopLevelView: widget passed in is null or has no window!\n"));
+    PRINTF("  nsClipboard::SetTopLevelView: widget passed in is null or has no window!\n");
 #endif /* DEBUG_CLIPBOARD */
     return;
   }
 
 #ifdef DEBUG_CLIPBOARD
-  PRINTF(("  nsClipboard::SetTopLevelView\n"));
+  PRINTF("  nsClipboard::SetTopLevelView\n");
 #endif /* DEBUG_CLIPBOARD */
   
 //  // If we're changing from one widget to another
@@ -147,7 +152,7 @@ void nsClipboard::SetTopLevelView(BView *v)
 //  NS_WITH_SERVICE(nsIClipboard, clipboard, kCClipboardCID, &rv);
 //
 //  if (!NS_SUCCEEDED(rv)) {
-//    PRINTF(("Couldn't get clipboard service!\n"));
+//    PRINTF("Couldn't get clipboard service!\n");
 //    return;
 //  }
 //
@@ -198,12 +203,12 @@ NS_IMETHODIMP nsClipboard::SetNativeClipboardData(PRInt32 aWhichClipboard)
   mIgnoreEmptyNotification = PR_TRUE;
 
 #ifdef DEBUG_CLIPBOARD
-  PRINTF(("  nsClipboard::SetNativeClipboardData()\n"));
+  PRINTF("  nsClipboard::SetNativeClipboardData()\n");
 #endif /* DEBUG_CLIPBOARD */
 
   // make sure we have a good transferable
   if (nsnull == mTransferable) {
-    PRINTF(("  SetNativeClipboardData: no transferable!\n"));
+    PRINTF("  SetNativeClipboardData: no transferable!\n");
     return NS_ERROR_FAILURE;
   }
 
@@ -241,12 +246,12 @@ nsClipboard::GetNativeClipboardData(nsITransferable * aTransferable, PRInt32 aWh
   nsresult rv = NS_OK;
 
 #ifdef DEBUG_CLIPBOARD
-  PRINTF(("  nsClipboard::GetNativeClipboardData()\n"));
+  PRINTF("  nsClipboard::GetNativeClipboardData()\n");
 #endif /* DEBUG_CLIPBOARD */
 
   // make sure we have a good transferable
   if (nsnull == aTransferable) {
-    PRINTF(("  GetNativeClipboardData: Transferable is null!\n"));
+    PRINTF("  GetNativeClipboardData: Transferable is null!\n");
     return NS_ERROR_FAILURE;
   }
 
@@ -332,7 +337,7 @@ nsClipboard::GetNativeClipboardData(nsITransferable * aTransferable, PRInt32 aWh
 //  // Set a flag saying that we're blocking waiting for the callback:
 //  mBlocking = PR_TRUE;
 //#ifdef DEBUG_CLIPBOARD
-//  PRINTF(("Waiting for the callback\n"));
+//  PRINTF("Waiting for the callback\n");
 //#endif /* DEBUG_CLIPBOARD */
 //
 //  // Now we need to wait until the callback comes in ...
@@ -343,7 +348,7 @@ nsClipboard::GetNativeClipboardData(nsITransferable * aTransferable, PRInt32 aWh
 //  }
 //
 //#ifdef DEBUG_CLIPBOARD
-//  PRINTF(("Got the callback: '%s', %d\n",
+//  PRINTF("Got the callback: '%s', %d\n",
 //         mSelectionData.data, mSelectionData.length);
 //#endif /* DEBUG_CLIPBOARD */
 //
@@ -365,7 +370,7 @@ nsClipboard::GetNativeClipboardData(nsITransferable * aTransferable, PRInt32 aWh
 //  mSelectionData.length = 0;
 
   return NS_OK;
-})
+}
 
 //
 // Called when the data from a paste comes in:
@@ -376,7 +381,7 @@ nsClipboard::GetNativeClipboardData(nsITransferable * aTransferable, PRInt32 aWh
 //                                  gpointer aData)
 //{
 //#ifdef DEBUG_CLIPBOARD
-//  PRINTF(("  nsClipboard::SelectionReceivedCB\n"));
+//  PRINTF("  nsClipboard::SelectionReceivedCB\n");
 //#endif /* DEBUG_CLIPBOARD */
 //
 //  // ARGHH!  GTK doesn't pass the arg to the callback, so we can't
@@ -385,12 +390,12 @@ nsClipboard::GetNativeClipboardData(nsITransferable * aTransferable, PRInt32 aWh
 //  NS_WITH_SERVICE(nsIClipboard, iclipboard, kCClipboardCID, &rv);
 //
 //  if (NS_FAILED(rv)) {
-//    PRINTF(("Couldn't get clipboard service!\n"));
+//    PRINTF("Couldn't get clipboard service!\n");
 //    return;
 //  }
 //  nsClipboard* clipboard = (nsClipboard*)iclipboard;
 //  if (!clipboard) {
-//    PRINTF(("couldn't convert nsIClipboard to nsClipboard\n"));
+//    PRINTF("couldn't convert nsIClipboard to nsClipboard\n");
 //    return;
 //  }
 //
@@ -405,7 +410,7 @@ nsClipboard::GetNativeClipboardData(nsITransferable * aTransferable, PRInt32 aWh
 //
 //  if (aSelectionData->length < 0)
 //  {
-//    PRINTF(("Error retrieving selection: length was %d\n",
+//    PRINTF("Error retrieving selection: length was %d\n",
 //           aSelectionData->length));
 //    return;
 //  }
@@ -424,8 +429,8 @@ nsClipboard::GetNativeClipboardData(nsITransferable * aTransferable, PRInt32 aWh
 //      return;
 //
 //    default:
-//      PRINTF(("Can't convert type %s (%ld) to string\n",
-//             gdk_atom_name (aSelectionData->type), aSelectionData->type));
+//      PRINTF("Can't convert type %s (%ld) to string\n",
+//             gdk_atom_name (aSelectionData->type), aSelectionData->type);
 //      return;
 //  }
 //}
@@ -437,7 +442,7 @@ nsClipboard::GetNativeClipboardData(nsITransferable * aTransferable, PRInt32 aWh
 NS_IMETHODIMP nsClipboard::ForceDataToClipboard()
 {
 #ifdef DEBUG_CLIPBOARD
-PRINTF(("  nsClipboard::ForceDataToClipboard()\n"));
+  PRINTF("  nsClipboard::ForceDataToClipboard()\n");
 #endif /* DEBUG_CLIPBOARD */
 
   // make sure we have a good transferable
@@ -459,7 +464,7 @@ PRINTF(("  nsClipboard::ForceDataToClipboard()\n"));
 //                                 gpointer   aData)
 //{ 
 //#ifdef DEBUG_CLIPBOARD
-//  PRINTF(("  nsClipboard::SelectionGetCB\n")); 
+//  PRINTF("  nsClipboard::SelectionGetCB\n"); 
 //#endif /* DEBUG_CLIPBOARD */
 //
 //  nsClipboard *clipboard = (nsClipboard *)aData;
@@ -470,7 +475,7 @@ PRINTF(("  nsClipboard::ForceDataToClipboard()\n"));
 //
 //  // Make sure we have a transferable:
 //  if (!clipboard->mTransferable) {
-//    PRINTF(("Clipboard has no transferable!\n"));
+//    PRINTF("Clipboard has no transferable!\n");
 //    return;
 //  }
 //
@@ -491,7 +496,7 @@ PRINTF(("  nsClipboard::ForceDataToClipboard()\n"));
 //                           dataLength);
 //  }
 //  else
-//    PRINTF(("Transferable didn't support the data flavor\n"));
+//    PRINTF("Transferable didn't support the data flavor\n");
 //}
 //
 //
@@ -502,7 +507,7 @@ PRINTF(("  nsClipboard::ForceDataToClipboard()\n"));
 //                                   gpointer data)
 //{
 //#ifdef DEBUG_CLIPBOARD
-//  PRINTF(("  nsClipboard::SelectionClearCB\n"));
+//  PRINTF("  nsClipboard::SelectionClearCB\n");
 //#endif /* DEBUG_CLIPBOARD */
 //}
 //
@@ -514,7 +519,7 @@ PRINTF(("  nsClipboard::ForceDataToClipboard()\n"));
 //                                 gpointer aData)
 //{
 //#ifdef DEBUG_CLIPBOARD
-//  PRINTF(("  nsClipboard::SelectionRequestCB\n"));
+//  PRINTF("  nsClipboard::SelectionRequestCB\n");
 //#endif /* DEBUG_CLIPBOARD */
 //}
 //
@@ -524,6 +529,6 @@ PRINTF(("  nsClipboard::ForceDataToClipboard()\n"));
 //                                  gpointer aData)
 //{
 //#ifdef DEBUG_CLIPBOARD
-//   PRINTF(("  nsClipboard::SelectionNotifyCB\n"));
+//   PRINTF("  nsClipboard::SelectionNotifyCB\n");
 //#endif /* DEBUG_CLIPBOARD */
 //}
