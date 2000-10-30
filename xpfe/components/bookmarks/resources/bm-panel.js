ec44a2aa5bf19f4f5e46e8467c6fca29080cc7fa
disttsc@bart.nl <disttsc@bart.nl>
2000-10-30 20:50:33 +0000

    Remove obsoleted "OpenBookmarkURL" function from bm-panel.js. bug=58366, r=matt, a=ben

--- xpfe/components/bookmarks/resources/bm-panel.js
+++ xpfe/components/bookmarks/resources/bm-panel.js
@@ -26,113 +26,38 @@
   Code for the Bookmarks Sidebar Panel
  */
 
-// get handle to the BrowserAppCore in the content area.
-var appCore = window._content.appCore;
-
-
-
 function clicked(event, target)
 {
 	if ((event.button != 1) || (event.detail != 2) || (target.nodeName != "treeitem"))
-		return(false);
+		return false;
 
     if (event.altKey)
     {
         // if altKey is down then just open the Bookmark Properties dialog
         BookmarkProperties();
-        return(true);
+        return true;
     }
 
 	if (target.getAttribute("container") == "true")
-		return(false);
+		return false;
 
 	sidebarOpenURL(event, target, document.getElementById('bookmarksTree').database);
-	return(true);
+	return true;
 }
 
-
-
-function OpenBookmarkURL(event, node, datasources)
-{
-	if (node.getAttribute("container") == "true")
-	{
-		return(false);
-	}
-
-	var url = node.getAttribute("id");
-	try
-	{
-		// add support for IE favorites under Win32, and NetPositive URLs under BeOS
-		var rdf = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService();
-		if (rdf) rdf = rdf.QueryInterface(Components.interfaces.nsIRDFService);
-		if (rdf && datasources) {
-			var src = rdf.GetResource(url, true);
-			var prop = rdf.GetResource("http://home.netscape.com/NC-rdf#URL", true);
-			var target = datasources.GetTarget(src, prop, true);
-			if (target)	target = target.QueryInterface(Components.interfaces.nsIRDFLiteral);
-			if (target)	target = target.Value;
-			if (target)	url = target;
-		}
-	}
-	catch(ex)
-	{
-	}
-
-	// Ignore "NC:" urls.
-	if (url.substring(0, 3) == "NC:")
-	{
-		return(false);
-	}
-  
-	if (!window._content)
-	{
-        const WM_CONTRACTID = "@mozilla.org/rdf/datasource;1?name=window-mediator";
-        var wm = nsJSComponentManager.getService(WM_CONTRACTID, "nsIWindowMediator");
-        if (wm)
-        {
-          navWindow = wm.getMostRecentWindow("navigator:browser");
-          if (navWindow)
-          {
-            navWindow.appCore.loadUrl(url);
-          }
-        }
-        else
-        {
-      		window.openDialog( getBrowserURL(), "_blank", "chrome,all,dialog=no", url );
-        }
-	}
-	else if (event.metaKey)
-	{
-	    // if metaKey is down, open in a new browser window
-  		window.openDialog( getBrowserURL(), "_blank", "chrome,all,dialog=no", url ); 
-	}
-	else
-	{
-		if(appCore)
-		{
-		    // support session history (if appCore is available)
-            appCore.loadUrl(url);
-		}
-		else
-		{
-		    // fallback case (if appCore isn't available)
-            window._content.location = url;
-		}
-  	}
-}
 
 function sidebarOpenURL(event, treeitem, root)
 {
  
 	if (treeitem.getAttribute("container") == "true")
-		return(false);
+		return false;
 
 	if (treeitem.getAttribute("type") == "http://home.netscape.com/NC-rdf#BookmarkSeparator")
-		return(false);
+		return false;
 
 	var id = treeitem.getAttribute("id");
 	if (!id)
-		return(false);
+		return false;
 
 	// rjc: add support for anonymous resources; if the node has
 	// a "#URL" property, use it, otherwise default to using the id
@@ -157,7 +82,7 @@ function sidebarOpenURL(event, treeitem, root)
     // Ignore "NC:" urls.
 	if (id.substring(0, 3) == "NC:")
 	{
-		return(false);
+		return false;
 	}	
 	if (event.metaKey) 
 	  {
