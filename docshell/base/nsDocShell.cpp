11ada38f227a520efe00c4ef2d7ed9fefc045600
mscott@netscape.com <mscott@netscape.com>
2000-11-01 08:03:58 +0000

    Bug #56938 --> Break out the code which parses and sets up refresh urls into a separate method so we can expose it as an interface method via nsIRefreshURI. r=sspitzer,sr=rpotts

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -3772,6 +3772,17 @@ nsDocShell::OnNewURI(nsIURI *aURI, nsIChannel *aChannel, PRUint32 aLoadType)
   }
 
    SetCurrentURI(aURI);
+   // if there's a refresh header in the channel, this method
+   // will set it up for us. 
+   SetupRefreshURI(aChannel);
+   
+   mInitialPageLoad = PR_FALSE;
+   return NS_OK;
+}
+
+NS_IMETHODIMP 
+nsDocShell::SetupRefreshURI(nsIChannel * aChannel)
+{
    nsCOMPtr<nsIHTTPChannel> httpChannel(do_QueryInterface(aChannel));
    if(httpChannel)
    {
@@ -3854,7 +3865,6 @@ nsDocShell::OnNewURI(nsIURI *aURI, nsIChannel *aChannel, PRUint32 aLoadType)
       }
    }
 
-   mInitialPageLoad = PR_FALSE;
    return NS_OK;
 }
 
