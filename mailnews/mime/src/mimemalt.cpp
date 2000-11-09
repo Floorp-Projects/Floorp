5537e0086a1770fec0c2dc0d8baabd9b717fd7fe
jefft%netscape.com <jefft%netscape.com>
2000-11-09 20:34:12 +0000

    fixed bug 59470 -- failed to handle multipart/alternative message; r=rhp, sr=mscott

--- mailnews/mime/src/mimemalt.cpp
+++ mailnews/mime/src/mimemalt.cpp
@@ -213,7 +213,7 @@ MimeMultipartAlternative_display_part_p(MimeObject *self,
 	 are themselves unknown.
    */
 
-  MimeObjectClass *clazz = mime_find_class (ct, sub_hdrs, self->options, PR_TRUE);
+  MimeObjectClass *clazz = mime_find_class (ct, sub_hdrs, self->options, PR_FALSE);
   PRBool result = (clazz
 					? clazz->displayable_inline_p(clazz, sub_hdrs)
 					: PR_FALSE);
