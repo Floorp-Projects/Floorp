0c23e63e43357da4cbd5a7efc3f6560bd51cad43
sspitzer%netscape.com <sspitzer%netscape.com>
2000-11-11 14:58:52 +0000

    fix uninitialized variable. #59673. sr=alecf

--- mailnews/db/msgdb/build/nsMsgDBFactory.cpp
+++ mailnews/db/msgdb/build/nsMsgDBFactory.cpp
@@ -103,7 +103,7 @@ NS_IMETHODIMP nsMsgDBModule::GetClassObject(nsIComponentManager *aCompMgr,
                                const nsIID& aIID,
                                void** r_classObj)
 {
-    nsresult rv;
+    nsresult rv = NS_OK;
 
     // Defensive programming: Initialize *r_classObj in case of error below
     if (!r_classObj)
