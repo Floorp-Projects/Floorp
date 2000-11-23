b17a3dd940eaf8c04183f2b4f612e021d3d6d331
blakeross%telocity.com <blakeross%telocity.com>
2000-11-23 05:47:04 +0000

    Fix 59707: extract navigator's context menu and allow it to be used elsewhere in the app (and to help in embedding). r=jag a=alecf

--- xpfe/communicator/resources/content/contentAreaUtils.js
+++ xpfe/communicator/resources/content/contentAreaUtils.js
@@ -19,130 +19,31 @@
  *
  * Contributors:
  * 
- *   Alec Flett      <alecf@netscape.com>
- *   Ben Goodger     <ben@netscape.com>
- *   Mike Pinkerton  <pinkerton@netscape.com>
- *   Blake Ross      <blakeross@telocity.com>
  */
 
-  var pref = null;
-  pref = Components.classes["@mozilla.org/preferences;1"];
-  pref = pref.getService();
-  pref = pref.QueryInterface(Components.interfaces.nsIPref);
-  
-  function findParentNode(node, parentNode)
-  {
-    while (node) {
-      var nodeName = node.localName;
-      if (!nodeName)
-        return null;
-      nodeName = nodeName.toLowerCase();
-      if (nodeName == "body" || nodeName == "html" ||
-          nodeName == "#document") {
-        return null;
-      }
-      if (nodeName == parentNode)
-        return node;
-      node = node.parentNode;
-    }
-    return null;
-  }
-
-  // Called whenever the user clicks in the content area,
-  // except when left-clicking on links (special case)
-  // should always return true for click to go through
-  function contentAreaClick(event) 
-  {
-    var target = event.originalTarget;
-    var linkNode;
-    switch (target.localName.toLowerCase()) {
-      case "a":
-        linkNode = event.target;
-        break;
-      case "area":
-        if (event.target.href) 
-          linkNode = event.target;
-        break;
-      default:
-        linkNode = findParentNode(target, "a");
-        break;
-    }
-    if (linkNode) {
-      handleLinkClick(event, linkNode.href);
-      return true;
-    }
-    if (pref && event.button == 2 &&
-        !findParentNode(target, "scrollbar") &&
-        pref.GetBoolPref("middlemouse.paste")) {
-      middleMousePaste(event);
-    }
-    return true;
-  }
-
-  function handleLinkClick(event, href)
-  {
-    switch (event.button) {                                   
-      case 1:                                                         // if left button clicked
-        if (event.metaKey || event.ctrlKey) {                         // and meta or ctrl are down
-          openNewWindowWith(href);                                    // open link in new window
-          event.preventBubble();
-          return true;
-        } 
-        if (event.shiftKey) {                                         // if shift is down
-          savePage(href);                                             // save the link
-          return true;
-        }
-        if (event.altKey)                                             // if alt is down
-          return true;                                                // do nothing
-        return false;
-      case 2:                                                         // if middle button clicked
-        if (pref && pref.GetBoolPref("middlemouse.openNewWindow")) {  // and the pref is on
-          openNewWindowWith(href);                                    // open link in new window
-          event.preventBubble();
-          return true;
-        }
-        break;
-    }
-    return false;
-  }
-
-  function middleMousePaste(event)
-  {
-      var url = readFromClipboard();
-      //dump ("Loading URL on clipboard: '" + url + "'; length = " + url.length + "\n");
-      if (url) {
-        var urlBar = document.getElementById("urlbar");
-        urlBar.value = url;
-        BrowserLoadURL();
-        event.preventBubble();
-        return true;
-    }
-    return false;
-  }
-
-   function openNewWindowWith( url ) {
-     var newWin;
-     var wintype = document.firstChild.getAttribute('windowtype');
+  function openNewWindowWith( url ) {
+    var newWin;
+    var wintype = document.firstChild.getAttribute('windowtype');
      
-     // if and only if the current window is a browser window and it has a document with a character
-     // set, then extract the current charset menu setting from the current document and use it to
-     // initialize the new browser window...
-     if (window && (wintype == "navigator:browser") &&
-         window._content && window._content.document) {
-       var DocCharset = window._content.document.characterSet;
-       charsetArg = "charset="+DocCharset;
-       dump("*** Current document charset: " + DocCharset + "\n");
+    // if and only if the current window is a browser window and it has a document with a character
+    // set, then extract the current charset menu setting from the current document and use it to
+    // initialize the new browser window...
+    if (window && (wintype == "navigator:browser") &&
+      window._content && window._content.document) {
+      var DocCharset = window._content.document.characterSet;
+      charsetArg = "charset="+DocCharset;
+      dump("*** Current document charset: " + DocCharset + "\n");
  
-       //we should "inherit" the charset menu setting in a new window
-       newWin = window.openDialog( getBrowserURL(), "_blank", "chrome,all,dialog=no", url, charsetArg );
-     }
-     else { // forget about the charset information.
-       newWin = window.openDialog( getBrowserURL(), "_blank", "chrome,all,dialog=no", url );
-     }
+      //we should "inherit" the charset menu setting in a new window
+      newWin = window.openDialog( getBrowserURL(), "_blank", "chrome,all,dialog=no", url, charsetArg );
+    }
+    else { // forget about the charset information.
+      newWin = window.openDialog( getBrowserURL(), "_blank", "chrome,all,dialog=no", url );
+    }
  
-     // Fix new window.    
-     newWin.saveFileAndPos = true;
-   }
+    // Fix new window.    
+    newWin.saveFileAndPos = true;
+  }
 
   function savePage(url) 
   {
@@ -164,4 +65,27 @@
     }
 
     return true;
-  } 
+  }
+
+  function editPage(url)
+  {
+    window.openDialog( "chrome://editor/content", "_blank", "chrome,all,dialog=no", url );
+  }
+
+  function findParentNode(node, parentNode)
+  {
+    while (node) {
+      var nodeName = node.localName;
+      if (!nodeName)
+        return null;
+      nodeName = nodeName.toLowerCase();
+      if (nodeName == "body" || nodeName == "html" ||
+          nodeName == "#document") {
+        return null;
+      }
+      if (nodeName == parentNode)
+        return node;
+      node = node.parentNode;
+    }
+    return null;
+  }
