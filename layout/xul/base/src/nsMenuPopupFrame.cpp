3223edbcad2bf755a2f4d529338ba6bbcf4a0ba4
ben@netscape.com <ben@netscape.com>
2000-11-27 22:04:58 +0000

    fix for bug 57951, submenus of scrolling menus positioned incorrectly. r=pinkerton@netscape.com, a=hyatt@netscape.com

--- layout/xul/base/src/nsMenuPopupFrame.cpp
+++ layout/xul/base/src/nsMenuPopupFrame.cpp
@@ -215,8 +215,10 @@ nsMenuPopupFrame::GetViewOffset(nsIViewManager* aManager, nsIView* aView,
   parent = aView;
   while (nsnull != parent) {
     parent->GetBounds(bounds);
-    aPoint.x += bounds.x;
-    aPoint.y += bounds.y;
+    if (bounds.y >= 0) {
+      aPoint.x += bounds.x;
+      aPoint.y += bounds.y;
+    }
     parent->GetParent(parent);
   }
 }
@@ -507,6 +509,16 @@ nsMenuPopupFrame::SyncViewWithFrame(nsIPresContext* aPresContext,
   nsIView* containingView = nsnull;
   nsPoint offset;
   aFrame->GetOffsetFromView(aPresContext, offset, &containingView);
+
+  // We add the bounds of the containing view to the offset as 
+  // in the case of a scrolled view (autoscrolling menus), the offset
+  // y dimension includes matter that is not onscreen and should not
+  // be taken into account when positioning submenus. The y dimension
+  // on the bounds of the containing view is negative if any content
+  // is offscreen. For most menus it is 0 so this is a no-op. 
+  nsRect bounds;
+  containingView->GetBounds(bounds);
+  offset += nsPoint(bounds.x, bounds.y);
   
   nsRect parentRect;
   aFrame->GetRect(parentRect);
