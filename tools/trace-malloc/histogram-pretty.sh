ebc5489904e769329b2ca93ee0a146f2657fb517
waterson@netscape.com <waterson@netscape.com>
2000-12-13 00:42:29 +0000

    Add pretty print for one histogram file.

--- tools/trace-malloc/histogram-pretty.sh
+++ tools/trace-malloc/histogram-pretty.sh
@@ -27,10 +27,10 @@
 #
 # Usage:
 #
-#   histogram-diff.sh [-c <count>] <base> <incr>
+#   histogram-pretty.sh [-c <count>] <file>
 #
-# Compute incremental memory growth from histogram in file <base> to
-# histogram in file <incr>, displaying at most <count> rows.
+# Pretty-print the histogram in file <file>, displaying at most
+# <count> rows.
 
 # How many rows are we gonna show?
 COUNT=20
@@ -46,50 +46,30 @@ while [ $# -gt 0 ]; do
     esac
 done
 
-BASE=$1
-INCR=$2
+FILE=$1
 
-# Sort the base and incremental files so that we can `join' them on
-# the type name
-sort $BASE > /tmp/$$.left
-sort $INCR > /tmp/$$.right
+# The first `awk' script computes a `TOTAL' row. Then, we sort by the
+# larges delta in bytes.
+awk '{ tobj += $2; tbytes += $3; } END { print "TOTAL", tobj, tbytes; }' ${FILE} > /tmp/$$.sorted
 
-# Do the join. The `awk' script computes the difference between
-# the base and the incremental files.
-join /tmp/$$.left /tmp/$$.right \
-    | awk '{ print $1, $2, $3, $4, $5, $4 - $2, $5 - $3; }' \
-    > /tmp/$$.joined
-
-rm -f /tmp/$$.left /tmp/$$.right
-
-# Now compute a `TOTAL' row.
-awk '{ tobj1 += $2; tbytes1 += $3; tobj2 += $4; tbytes2 += $5; tdobj += $6; tdbytes += $7; } END { print "TOTAL", tobj1, tbytes1, tobj2, tbytes2, tdobj, tdbytes; }' /tmp/$$.joined \
-    > /tmp/$$.sorted
-
-# Then, we sort by the largest delta in bytes.
-sort -nr +6 /tmp/$$.joined >> /tmp/$$.sorted
-
-rm -f /tmp/$$.joined
+sort -nr +2 ${FILE} >> /tmp/$$.sorted
 
 # Pretty-print, including percentages
 cat <<EOF > /tmp/$$.awk
 BEGIN {
-  print "                        ---- Base ----   ---- Incr ----   ----- Difference ----";
-  print "Type                    Count    Bytes   Count    Bytes   Count    Bytes %Total";
+  print "Type                    Count    Bytes %Total";
   }
 \$1 == "TOTAL" {
-  tbytes = \$7;
+  tbytes = \$3;
   }
 NR <= $COUNT {
-  printf "%-22s %6d %8d  %6d %8d  %6d %8d %6.2lf\n", \$1, \$2, \$3, \$4, \$5, \$6, \$7, 100.0 * \$7 / tbytes;
+  printf "%-22s %6d %8d %6.2lf\n", \$1, \$2, \$3, 100.0 * \$3 / tbytes;
   }
 NR > $COUNT {
-  oobjs1 += \$2;  obytes1 += \$3;
-  oobjs2 += \$4;  obytes2 += \$5;
-  odobjs += \$6;  odbytes += \$7;
+  oobjs += \$2;  obytes += \$3;
   }
 END {
-  printf "%-22s %6d %8d  %6d %8d  %6d %8d %6.2lf\n", "OTHER", oobjs1, obytes1, oobjs2, obytes2, odobjs, odbytes, odbytes * 100.0 / tbytes;
+  printf "%-22s %6d %8d %6.2lf\n", "OTHER", oobjs, obytes, obytes * 100.0 / tbytes;
   }
 EOF
 
