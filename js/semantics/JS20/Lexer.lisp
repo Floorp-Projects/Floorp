666b8c7b812b08c3d28a15d4fdcc5c08997d5ab7
waldemar%netscape.com <waldemar%netscape.com>
2000-12-19 01:56:36 +0000

    Removed 'operator' non-reserved word

--- js/semantics/JS20/Lexer.lisp
+++ js/semantics/JS20/Lexer.lisp
@@ -241,7 +241,7 @@
                  "instanceof" "interface" "namespace" "native" "new" "null" "package" "private" "protected" "public" "return" "static" "super"
                  "switch" "synchronized" "this" "throw" "throws" "transient" "true" "try" "typeof" "use" "var" "volatile" "while" "with"))
        (define non-reserved-words (vector string)
-         (vector "exclude" "get" "include" "operator" "set"))
+         (vector "exclude" "get" "include" "set"))
        (define keywords (vector string)
          (append reserved-words non-reserved-words))
        
