d67e0d81fce01a6078b96d747c75293bfe8b881f
cls%seawood.org <cls%seawood.org>
2000-12-30 05:44:01 +0000

    Preparations for building on win32 with autoconf Makefile.ins.

--- nsprpub/pr/src/md/windows/objs.mk
+++ nsprpub/pr/src/md/windows/objs.mk
@@ -34,6 +34,7 @@
 
 ifeq (WINNT,$(MOZ_TARGET))
 CSRCS = ntmisc.c \
+	ntsec.c \
 	ntsem.c \
 	ntinrval.c \
 	ntgc.c \
@@ -41,10 +42,14 @@ CSRCS = ntmisc.c \
 	ntthread.c \
 	ntdllmn.c \
 	win32_errors.c \
-	w32poll.c
+	w32ipcsem.c \
+	w32poll.c \
+	w32rng.c \
+	w32shm.c
 else
 ifeq (WIN95,$(MOZ_TARGET))
 CSRCS =	ntmisc.c \
+	ntsec.c \
 	ntsem.c \
 	ntinrval.c \
 	ntgc.c \
@@ -53,7 +58,10 @@ CSRCS =	ntmisc.c \
 	w95cv.c \
 	w95sock.c \
 	win32_errors.c \
+	w32ipcsem.c \
 	w32poll.c \
+	w32rng.c \
+	w32shm.c \
 	w95dllmain.c
 else
 ifeq (WIN16,$(MOZ_TARGET))
