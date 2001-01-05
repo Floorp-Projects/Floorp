0298ab90eb856689c876f74fcd1612b185d2d0c8
nboyd%atg.com <nboyd%atg.com>
2001-01-05 20:00:47 +0000

    For == use .equals after unwrapping.

--- js/rhino/src/org/mozilla/javascript/ScriptRuntime.java
+++ js/rhino/src/org/mozilla/javascript/ScriptRuntime.java
@@ -1582,11 +1582,8 @@ public class ScriptRuntime {
                 if (typeX == ScriptableClass) {
                     if (x == y)
                         return true;
-                    if (x instanceof Wrapper &&
-                        y instanceof Wrapper)
-                    {
-                        return ((Wrapper) x).unwrap() ==
-                               ((Wrapper) y).unwrap();
+                    if (x instanceof Wrapper && y instanceof Wrapper) {
+                        return ((Wrapper) x).unwrap().equals(((Wrapper) y).unwrap());
                     }
                     return false;
                 }
