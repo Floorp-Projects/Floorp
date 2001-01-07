68049fe0cef27f6b5764c2b7108b084e9effa9de
nelsonb%netscape.com <nelsonb%netscape.com>
2001-01-07 09:21:25 +0000

    Eliminate some warnings found when compiling on Unix boxen.

--- security/nss/lib/certdb/certt.h
+++ security/nss/lib/certdb/certt.h
@@ -33,7 +33,7 @@
 /*
  * certt.h - public data structures for the certificate library
  *
- * $Id: certt.h,v 1.3 2001/01/07 08:12:48 nelsonb%netscape.com Exp $
+ * $Id: certt.h,v 1.4 2001/01/07 09:21:23 nelsonb%netscape.com Exp $
  */
 #ifndef _CERTT_H_
 #define _CERTT_H_
@@ -804,11 +804,11 @@ extern const SEC_ASN1Template CERT_SetOfAttributeTemplate[];
 /* These functions simply return the address of the above-declared templates.
 ** This is necessary for Windows DLLs.  Sigh.
 */
-SEC_ASN1_CHOOSER_DECLARE(CERT_CertificateRequestTemplate);
-SEC_ASN1_CHOOSER_DECLARE(CERT_CertificateTemplate);
-SEC_ASN1_CHOOSER_DECLARE(CERT_CrlTemplate);
-SEC_ASN1_CHOOSER_DECLARE(CERT_IssuerAndSNTemplate);
-SEC_ASN1_CHOOSER_DECLARE(CERT_SetOfSignedCrlTemplate);
-SEC_ASN1_CHOOSER_DECLARE(CERT_SignedDataTemplate);
+SEC_ASN1_CHOOSER_DECLARE(CERT_CertificateRequestTemplate)
+SEC_ASN1_CHOOSER_DECLARE(CERT_CertificateTemplate)
+SEC_ASN1_CHOOSER_DECLARE(CERT_CrlTemplate)
+SEC_ASN1_CHOOSER_DECLARE(CERT_IssuerAndSNTemplate)
+SEC_ASN1_CHOOSER_DECLARE(CERT_SetOfSignedCrlTemplate)
+SEC_ASN1_CHOOSER_DECLARE(CERT_SignedDataTemplate)
 
 #endif /* _CERTT_H_ */
