3c987d940d55230b225dad25641bd25847b3e2dd
jgmyers%netscape.com <jgmyers%netscape.com>
2001-01-10 07:42:59 +0000

    back out cleanup fix

--- mailnews/addrbook/src/nsDirPrefs.cpp
+++ mailnews/addrbook/src/nsDirPrefs.cpp
@@ -22,7 +22,6 @@
 
 /* directory server preferences (used to be dirprefs.c in 4.x) */
 
-#include <ctype.h>
 #include "nsIPref.h"
 #include "nsVoidArray.h"
 #include "nsIServiceManager.h"
@@ -2839,8 +2838,7 @@ char * dir_ConvertDescriptionToPrefName(DIR_Server * server)
 		numSrcBytes = PL_strlen(descr);
 		while (srcIndex < numSrcBytes && destIndex < MAX_PREF_NAME_SIZE-1)
 		{
-			if (isascii(descr[srcIndex]) &&
-                (isdigit(descr[srcIndex]) || isalpha(descr[srcIndex]) ))
+			if (XP_IS_DIGIT(descr[srcIndex]) ||XP_IS_ALPHA(descr[srcIndex]) )
 			{
 				fileNameBuf[destIndex] = descr[srcIndex];
 				destIndex++;
@@ -4507,10 +4505,10 @@ char *DIR_Unescape (const char *src, PRBool makeHtml)
 				 */
 				PRBool didEscape = PR_FALSE;
 				char c1 = *(tmpSrc + 1);
-				if (c1 && isascii(c1) && (isdigit(c1) || isalpha(c1)))
+				if (c1 && (XP_IS_DIGIT(c1) || XP_IS_ALPHA(c1)))
 				{
 					char c2 = *(tmpSrc + 2);
-					if (c2 && isascii(c2) && (isdigit(c2) || isalpha(c2)))
+					if (c2 && (XP_IS_DIGIT(c2) || XP_IS_ALPHA(c2)))
 					{
 						*tmpDst++ = (UNHEX(c1) << 4) | UNHEX(c2);
 						tmpSrc +=2;
