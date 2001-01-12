2714ab69679da4f1cd1fccae3192a329c01dea58
pavlov@netscape.com <pavlov@netscape.com>
2001-01-12 08:23:22 +0000

    some code that should in theory write a bmp whenever GetBits is called to c:\whatever.bmp... this however produces a blank bmp at the moment...

--- gfx2/src/windows/nsImage.cpp
+++ gfx2/src/windows/nsImage.cpp
@@ -49,6 +49,8 @@ NS_IMETHODIMP nsImage::Init(gfx_dimension aWidth, gfx_dimension aHeight, gfx_for
     return NS_ERROR_FAILURE;
   }
 
+  delete[] mBits;
+
   mSize.SizeTo(aWidth, aHeight);
   mFormat = aFormat;
 
@@ -137,13 +139,197 @@ NS_IMETHODIMP nsImage::GetBitsLength(PRUint32 *aBitsLength)
   return NS_OK;
 }
 
+#include <windows.h>
+
+void errhandler(char *foo, void *a) {}
+
+PBITMAPINFO CreateBitmapInfoStruct(HWND hwnd, HBITMAP hBmp)
+{ 
+    BITMAP bmp; 
+    PBITMAPINFO pbmi; 
+    WORD    cClrBits; 
+
+    // Retrieve the bitmap's color format, width, and height. 
+    if (!GetObject(hBmp, sizeof(BITMAP), (LPSTR)&bmp)) 
+        errhandler("GetObject", hwnd); 
+
+    // Convert the color format to a count of bits. 
+    cClrBits = (WORD)(bmp.bmPlanes * bmp.bmBitsPixel); 
+    if (cClrBits == 1) 
+        cClrBits = 1; 
+    else if (cClrBits <= 4) 
+        cClrBits = 4; 
+    else if (cClrBits <= 8) 
+        cClrBits = 8; 
+    else if (cClrBits <= 16) 
+        cClrBits = 16; 
+    else if (cClrBits <= 24) 
+        cClrBits = 24; 
+    else cClrBits = 32; 
+
+    // Allocate memory for the BITMAPINFO structure. (This structure 
+    // contains a BITMAPINFOHEADER structure and an array of RGBQUAD 
+    // data structures.) 
+
+     if (cClrBits != 24) 
+         pbmi = (PBITMAPINFO) LocalAlloc(LPTR, 
+                    sizeof(BITMAPINFOHEADER) + 
+                    sizeof(RGBQUAD) * (1<< cClrBits)); 
+
+     // There is no RGBQUAD array for the 24-bit-per-pixel format. 
+
+     else 
+         pbmi = (PBITMAPINFO) LocalAlloc(LPTR, 
+                    sizeof(BITMAPINFOHEADER)); 
+
+    // Initialize the fields in the BITMAPINFO structure. 
+
+    pbmi->bmiHeader.biSize = sizeof(BITMAPINFOHEADER); 
+    pbmi->bmiHeader.biWidth = bmp.bmWidth; 
+    pbmi->bmiHeader.biHeight = bmp.bmHeight; 
+    pbmi->bmiHeader.biPlanes = bmp.bmPlanes; 
+    pbmi->bmiHeader.biBitCount = bmp.bmBitsPixel; 
+    if (cClrBits < 24) 
+        pbmi->bmiHeader.biClrUsed = (1<<cClrBits); 
+
+    // If the bitmap is not compressed, set the BI_RGB flag. 
+    pbmi->bmiHeader.biCompression = BI_RGB; 
+
+    // Compute the number of bytes in the array of color 
+    // indices and store the result in biSizeImage. 
+    // Width must be DWORD aligned unless bitmap is RLE compressed.
+    pbmi->bmiHeader.biSizeImage = (pbmi->bmiHeader.biWidth + 15) /16 
+                                  * pbmi->bmiHeader.biHeight 
+                                  * cClrBits;
+    // Set biClrImportant to 0, indicating that all of the 
+    // device colors are important. 
+     pbmi->bmiHeader.biClrImportant = 0; 
+     return pbmi; 
+ } 
+
+void CreateBMPFile(HWND hwnd, LPTSTR pszFile, PBITMAPINFO pbi, 
+                  HBITMAP hBMP, HDC hDC) 
+ { 
+     HANDLE hf;                 // file handle 
+    BITMAPFILEHEADER hdr;       // bitmap file-header 
+    PBITMAPINFOHEADER pbih;     // bitmap info-header 
+    LPBYTE lpBits;              // memory pointer 
+    DWORD dwTotal;              // total count of bytes 
+    DWORD cb;                   // incremental count of bytes 
+    BYTE *hp;                   // byte pointer 
+    DWORD dwTmp; 
+
+    pbih = (PBITMAPINFOHEADER) pbi; 
+    lpBits = (LPBYTE) GlobalAlloc(GMEM_FIXED, pbih->biSizeImage);
+
+    if (!lpBits) 
+         errhandler("GlobalAlloc", hwnd); 
+
+    // Retrieve the color table (RGBQUAD array) and the bits 
+    // (array of palette indices) from the DIB. 
+    if (!GetDIBits(hDC, hBMP, 0, (WORD) pbih->biHeight, lpBits, pbi, 
+        DIB_RGB_COLORS)) 
+    {
+        errhandler("GetDIBits", hwnd); 
+    }
+
+    // Create the .BMP file. 
+    hf = CreateFile(pszFile, 
+                   GENERIC_READ | GENERIC_WRITE, 
+                   (DWORD) 0, 
+                    NULL, 
+                   CREATE_ALWAYS, 
+                   FILE_ATTRIBUTE_NORMAL, 
+                   (HANDLE) NULL); 
+    if (hf == INVALID_HANDLE_VALUE) 
+        errhandler("CreateFile", hwnd); 
+    hdr.bfType = 0x4d42;        // 0x42 = "B" 0x4d = "M" 
+    // Compute the size of the entire file. 
+    hdr.bfSize = (DWORD) (sizeof(BITMAPFILEHEADER) + 
+                 pbih->biSize + pbih->biClrUsed 
+                 * sizeof(RGBQUAD) + pbih->biSizeImage); 
+    hdr.bfReserved1 = 0; 
+    hdr.bfReserved2 = 0; 
+
+    // Compute the offset to the array of color indices. 
+    hdr.bfOffBits = (DWORD) sizeof(BITMAPFILEHEADER) + 
+                    pbih->biSize + pbih->biClrUsed 
+                    * sizeof (RGBQUAD); 
+
+    // Copy the BITMAPFILEHEADER into the .BMP file. 
+    if (!WriteFile(hf, (LPVOID) &hdr, sizeof(BITMAPFILEHEADER), 
+        (LPDWORD) &dwTmp,  NULL)) 
+    {
+       errhandler("WriteFile", hwnd); 
+    }
+
+    // Copy the BITMAPINFOHEADER and RGBQUAD array into the file. 
+    if (!WriteFile(hf, (LPVOID) pbih, 
+                   sizeof(BITMAPINFOHEADER) + pbih->biClrUsed * sizeof (RGBQUAD), 
+                  (LPDWORD) &dwTmp,
+                  (NULL)
+                  ) 
+       )
+        errhandler("WriteFile", hwnd); 
+
+    // Copy the array of color indices into the .BMP file. 
+    dwTotal = cb = pbih->biSizeImage; 
+    hp = lpBits; 
+    if (!WriteFile(hf, (LPSTR) hp, (int) cb, (LPDWORD) &dwTmp,NULL)) 
+           errhandler("WriteFile", hwnd); 
+
+    // Close the .BMP file. 
+     if (!CloseHandle(hf)) 
+           errhandler("CloseHandle", hwnd); 
+
+    // Free memory. 
+    GlobalFree((HGLOBAL)lpBits);
+}
+
+
 /* readonly attribute PRUint8 bits; */
 NS_IMETHODIMP nsImage::GetBits(PRUint8 *aBits)
 {
   if (!mBits)
     return NS_ERROR_NOT_INITIALIZED;
 
-  aBits = mBits;
+
+  HWND bg = GetDesktopWindow();
+  HDC memDC = GetDC(bg);
+
+//  HBITMAP memBM = CreateCompatibleBitmap(memDC, GFXCoordToIntCeil(mSize.width), GFXCoordToIntCeil(mSize.height));
+
+
+  LPBITMAPINFOHEADER mBHead = (LPBITMAPINFOHEADER)new char[sizeof(BITMAPINFO)];
+
+  mBHead->biSize = sizeof(BITMAPINFOHEADER);
+	mBHead->biWidth = GFXCoordToIntCeil(mSize.width);
+	mBHead->biHeight = GFXCoordToIntCeil(mSize.height);
+	mBHead->biPlanes = 1;
+	mBHead->biBitCount = 24;
+	mBHead->biCompression = BI_RGB;
+	mBHead->biSizeImage = 0;            // not compressed, so we dont need this to be set
+	mBHead->biXPelsPerMeter = 0;
+	mBHead->biYPelsPerMeter = 0;
+	mBHead->biClrUsed = 0;
+	mBHead->biClrImportant = 0;
+
+  HBITMAP memBM = ::CreateDIBitmap(memDC,mBHead,CBM_INIT,mBits,(LPBITMAPINFO)mBHead,
+				                      DIB_RGB_COLORS);
+
+  PBITMAPINFO info = CreateBitmapInfoStruct(bg, memBM);
+
+
+  CreateBMPFile(bg, "c:\\whatever.bmp", info, 
+                  memBM, memDC) ;
+
+  DeleteObject(memBM);
+
+  FILE *f = fopen("C:\\image.data", "w");
+  fwrite(mBits, mBitsLength, 1, f);
+  fclose(f);
+
+
   return NS_OK;
 }
 
@@ -156,7 +342,7 @@ NS_IMETHODIMP nsImage::SetBits(const PRUint8 *data, PRUint32 length, PRInt32 off
   PRUint32 i;
   PRInt32 off;
   // XXX i could unroll this loop a bit :-)
-  for (i=0, off = offset; i <= length; ++i, ++offset) {
+  for (i=0, off = offset; i <= length; ++i, ++off) {
     mBits[off] = data[i];
   }
   return NS_OK;
