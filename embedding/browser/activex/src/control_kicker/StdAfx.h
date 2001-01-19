d2f5f5a6fd158162b4f7f5d938cb5c0e8f609834
locka%iol.ie <locka%iol.ie>
2001-01-19 13:17:28 +0000

    New mozctlx.dll for Mozilla ActiveX control. Register this DLL instead of mozctl.dll and it will ensure the PATH environment variable in the host process includes the Mozilla bin directory everytime the control is created. NOT PART OF BUILD

---
+++ embedding/browser/activex/src/control_kicker/StdAfx.h
@@ -0,0 +1,26 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__7F2751C5_DFC7_4E43_BE16_785116B26B4A__INCLUDED_)
+#define AFX_STDAFX_H__7F2751C5_DFC7_4E43_BE16_785116B26B4A__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// Insert your headers here
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <windows.h>
+#include <tchar.h>
+#include <wtypes.h>
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__7F2751C5_DFC7_4E43_BE16_785116B26B4A__INCLUDED_)
