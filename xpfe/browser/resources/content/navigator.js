07ee87e6cf1ead07d188c5e1bb5dc53295a302b6
disttsc%bart.nl <disttsc%bart.nl>
2001-01-19 01:19:07 +0000

    More appCore killing. bug=46200, r=ben, a=alecf

--- xpfe/browser/resources/content/navigator.js
+++ xpfe/browser/resources/content/navigator.js
@@ -451,7 +451,7 @@ function Startup()
       var arrayArgComponents = window.arguments[1].split("=");
       if (arrayArgComponents) {
         //we should "inherit" the charset menu setting in a new window
-        appCore.SetDocumentCharset(arrayArgComponents[1]);
+        BrowserSetDefaultCharacterSet(arrayArgComponents[1]);
       }
     }
   }
@@ -807,8 +807,7 @@ function BrowserEditBookmarks()
 
 function BrowserPrintPreview()
 {
-  // this is currently a do-nothing on appCore which is going to die
-  // ???.printPreview();
+  // implement me
 }
 
 function BrowserPrint()
@@ -819,18 +818,21 @@ function BrowserPrint()
 
 function BrowserSetDefaultCharacterSet(aCharset)
 {
-  appCore.SetDocumentCharset(aCharset);
+  getMarkupDocumentViewer().defaultCharacterSet = aCharset;
   getWebNavigation().reload(nsIWebNavigation.LOAD_FLAGS_NONE);
 }
 
 function BrowserSetForcedCharacterSet(aCharset)
 {
-  appCore.SetForcedCharset(aCharset);
+  var charsetConverterManager = Components.classes["@mozilla.org/charset-converter-manager;1"]
+                                          .getService(Components.interfaces.nsICharsetConverterManager2);
+  var characterSet = charsetConverterManager.GetCharsetAtom(aCharset);
+  getBrowser().documentCharsetInfo.forcedCharset = characterSet;
 }
 
 function BrowserSetForcedDetector()
 {
-  appCore.SetForcedDetector();
+  getBrowser().documentCharsetInfo.forcedDetector = true;
 }
 
 function BrowserClose()
@@ -1126,11 +1128,7 @@ function checkForDirectoryListing()
   if ("HTTPIndex" in _content &&
       typeof _content.HTTPIndex == "object" &&
       !_content.HTTPIndex.constructor) {
-    // Give directory .xul/.js access to browser instance.
-    // XXX The following line leaks (bug 61821), so the next line is a hack
-    // to avoid the leak.
-    // _content.defaultCharacterset = getBrowser().markupDocumentViewer.defaultCharacterSet;
-    _content.defaultCharacterset = getBrowser().docShell.contentViewer.QueryInterface(Components.interfaces.nsIMarkupDocumentViewer).defaultCharacterSet;
+    _content.defaultCharacterset = getMarkupDocumentViewer().defaultCharacterSet;
     _content.parentWindow = window;
   }
 }
