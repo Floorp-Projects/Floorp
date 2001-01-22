06868b8c1424614fd95014aff16a7004cdda16b1
pavlov%netscape.com <pavlov%netscape.com>
2001-01-22 07:32:41 +0000

    some documentation changes

--- gfx2/src/windows/nsImageContainer.cpp
+++ gfx2/src/windows/nsImageContainer.cpp
@@ -14,7 +14,7 @@
  * 
  * The Initial Developer of the Original Code is Netscape
  * Communications Corporation. Portions created by Netscape are
- * Copyright (C) 2000-2001 Netscape Communications Corporation. All
+ * Copyright (C) 2001 Netscape Communications Corporation. All
  * Rights Reserved.
  * 
  * Contributor(s): 
