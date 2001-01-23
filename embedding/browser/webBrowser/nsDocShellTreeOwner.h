6a6393d15e88e027a6ff26306a1f234c6d58193e
pinkerton@netscape.com <pinkerton@netscape.com>
2001-01-23 00:47:02 +0000

    Support tooltips in embedding code with new nsITooltipListener interface. Refactor listeners into separate object to simplify ownership and removed Release() hack. r=saari, sr=sfraser

--- embedding/browser/webBrowser/nsDocShellTreeOwner.h
+++ embedding/browser/webBrowser/nsDocShellTreeOwner.h
@@ -37,10 +37,14 @@
 #include "nsIDOMDocument.h"
 #include "nsIChromeEventHandler.h"
 #include "nsIDOMEventReceiver.h"
+#include "nsIDOMKeyListener.h"
+#include "nsIDOMMouseMotionListener.h"
+#include "nsITimer.h"
 
 #include "nsCommandHandler.h"
 
 class nsWebBrowser;
+class ChromeListener;
 
 // {6D10C180-6888-11d4-952B-0020183BF181}
 #define NS_ICDOCSHELLTREEOWNER_IID \
@@ -63,7 +67,6 @@ class nsDocShellTreeOwner : public nsIDocShellTreeOwner,
                             public nsIBaseWindow,
                             public nsIInterfaceRequestor,
                             public nsIWebProgressListener,
-                            public nsIDOMMouseListener,
                             public nsICDocShellTreeOwner
 {
 friend class nsWebBrowser;
@@ -77,15 +80,6 @@ public:
     NS_DECL_NSIINTERFACEREQUESTOR
     NS_DECL_NSIWEBPROGRESSLISTENER
 
-	// nsIDOMMouseListener
-	virtual nsresult HandleEvent(nsIDOMEvent* aEvent);
-	virtual nsresult MouseDown(nsIDOMEvent* aMouseEvent);
-	virtual nsresult MouseUp(nsIDOMEvent* aMouseEvent);
-	virtual nsresult MouseClick(nsIDOMEvent* aMouseEvent);
-	virtual nsresult MouseDblClick(nsIDOMEvent* aMouseEvent);
-	virtual nsresult MouseOver(nsIDOMEvent* aMouseEvent);
-	virtual nsresult MouseOut(nsIDOMEvent* aMouseEvent);
-
 protected:
     nsDocShellTreeOwner();
     virtual ~nsDocShellTreeOwner();
@@ -96,9 +90,7 @@ protected:
     NS_IMETHOD SetTreeOwner(nsIDocShellTreeOwner* aTreeOwner);
     NS_IMETHOD SetWebBrowserChrome(nsIWebBrowserChrome* aWebBrowserChrome);
 
-    nsCOMPtr<nsIDOMEventReceiver> mEventReceiver;
-    NS_IMETHOD AddMouseListener();
-    NS_IMETHOD RemoveMouseListener();
+    NS_IMETHOD AddChromeListeners();
 
 protected:
 
@@ -111,8 +103,114 @@ protected:
    nsIWebProgressListener* mOwnerProgressListener;
    nsIBaseWindow*          mOwnerWin;
    nsIInterfaceRequestor*  mOwnerRequestor;
-   PRBool                  mMouseListenerActive;
+   
+    // the object that listens for chrome events like context menus and tooltips. 
+    // It is a separate object to avoid circular references between |this|
+    // and the DOM. This is a strong, owning ref.
+   ChromeListener*         mChromeListener;
 };
+
+
+//
+// class ChromeListener
+//
+// The class that listens to the chrome events and tells the embedding
+// chrome to show things like context menus or tooltips, as appropriate.
+// Handles registering itself with the DOM with AddChromeListeners()
+// and removing itself with RemoveChromeListeners().
+//
+// NOTE: Because of a leak/bug in the EventListenerManager having to do with
+//       a single listener with multiple IID's, we need to addref/release
+//       this class instead of just owning it and calling delete when we
+//       know we're done. This also forces us to make RemoveChromeListeners()
+//       public when it doesn't need to be. This is a reminder to fix when I
+//       fix the ELM bug. (pinkerton)
+//
+class ChromeListener : public nsIDOMMouseListener,
+                       public nsIDOMKeyListener,
+                       public nsIDOMMouseMotionListener
+{
+public:
+  NS_DECL_ISUPPORTS
+  
+  ChromeListener ( nsWebBrowser* inBrowser, nsIWebBrowserChrome* inChrome ) ;
+  ~ChromeListener ( ) ;
+
+	  // nsIDOMMouseListener
+	virtual nsresult HandleEvent(nsIDOMEvent* aEvent) {	return NS_OK; }
+	virtual nsresult MouseDown(nsIDOMEvent* aMouseEvent);
+	virtual nsresult MouseUp(nsIDOMEvent* aMouseEvent);
+	virtual nsresult MouseClick(nsIDOMEvent* aMouseEvent);
+	virtual nsresult MouseDblClick(nsIDOMEvent* aMouseEvent);
+	virtual nsresult MouseOver(nsIDOMEvent* aMouseEvent);
+	virtual nsresult MouseOut(nsIDOMEvent* aMouseEvent);
+
+    // nsIDOMMouseMotionListener
+  virtual nsresult MouseMove(nsIDOMEvent* aMouseEvent);
+  virtual nsresult DragMove(nsIDOMEvent* aMouseEvent) { return NS_OK; };
+
+    // nsIDOMKeyListener
+  virtual nsresult KeyDown(nsIDOMEvent* aKeyEvent) ;
+  virtual nsresult KeyUp(nsIDOMEvent* aKeyEvent) ;
+  virtual nsresult KeyPress(nsIDOMEvent* aKeyEvent) ;
+
+    // Add/remove the relevant listeners, based on what interfaces
+    // the embedding chrome implements.
+  NS_IMETHOD AddChromeListeners();
+  NS_IMETHOD RemoveChromeListeners();
+
+private:
+
+  NS_IMETHOD AddContextMenuListener();
+  NS_IMETHOD AddTooltipListener();
+  NS_IMETHOD RemoveContextMenuListener();
+  NS_IMETHOD RemoveTooltipListener();
+
+  nsWebBrowser* mWebBrowser;
+  nsIWebBrowserChrome* mWebBrowserChrome;
+  nsCOMPtr<nsIDOMEventReceiver> mEventReceiver;
+
+  PRPackedBool mContextMenuListenerInstalled;
+  PRPackedBool mTooltipListenerInstalled;
+
+private:
+
+    // various delays for tooltips
+  enum {
+    kTooltipAutoHideTime = 5000,       // 5000ms = 5 seconds
+    kTooltipShowTime = 500             // 500ms = 0.5 seconds
+  };
+
+  NS_IMETHOD ShowTooltip ( PRInt32 inXCoords, PRInt32 inYCoords, const nsAReadableString & inTipText ) ;
+  NS_IMETHOD HideTooltip ( ) ;
+
+    // Determine if there is a TITLE attribute. Returns |PR_TRUE| if there is,
+    // and sets the text in |outText|.
+  PRBool FindTitleText ( nsIDOMNode* inNode, nsAWritableString & outText ) ;
+
+  nsCOMPtr<nsITimer> mTooltipTimer;
+  static void sTooltipCallback ( nsITimer* aTimer, void* aListener ) ;
+  PRInt32 mMouseClientX, mMouseClientY;       // mouse coordinates for tooltip event
+  PRBool mShowingTooltip;
+
+    // a timer for auto-hiding the tooltip after a certain delay
+  nsCOMPtr<nsITimer> mAutoHideTimer;
+  static void sAutoHideCallback ( nsITimer* aTimer, void* aListener ) ;
+  void CreateAutoHideTimer ( ) ;
+
+    // The node hovered over that fired the timer. This may turn into the node that
+    // triggered the tooltip, but only if the timer ever gets around to firing.
+    // This is a strong reference, because the tooltip content can be destroyed while we're
+    // waiting for the tooltip to pup up, and we need to detect that.
+    // It's set only when the tooltip timer is created and launched. The timer must
+    // either fire or be cancelled (or possibly released?), and we release this
+    // reference in each of those cases. So we don't leak.
+  nsCOMPtr<nsIDOMNode> mPossibleTooltipNode;
+
+}; // ChromeListener
+
+
+
 
 #endif /* nsDocShellTreeOwner_h__ */
 
