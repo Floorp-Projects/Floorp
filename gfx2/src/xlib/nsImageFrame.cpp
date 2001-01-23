d8f78afc03711a70301a36b8040ed9e30a09f93a
pavlov%netscape.com <pavlov%netscape.com>
2001-01-23 20:46:36 +0000

    adding imageframe/container to xlib

--- gfx2/src/xlib/nsImageFrame.cpp
+++ gfx2/src/xlib/nsImageFrame.cpp
@@ -14,47 +14,44 @@
  * 
  * The Initial Developer of the Original Code is Netscape
  * Communications Corporation. Portions created by Netscape are
- * Copyright (C) 2000-2001 Netscape Communications Corporation. All
+ * Copyright (C) 2001 Netscape Communications Corporation. All
  * Rights Reserved.
  * 
  * Contributor(s): 
  *   Stuart Parmenter <pavlov@netscape.com>
  */
 
-#include "nsImage.h"
+#include "nsImageFrame.h"
 
 #include "nsUnitConverters.h"
 
-NS_IMPL_ISUPPORTS1(nsImage, nsIImage2)
+NS_IMPL_ISUPPORTS1(nsImageFrame, nsIImageFrame)
 
-nsImage::nsImage()
+nsImageFrame::nsImageFrame() :
+  mBits(nsnull)
 {
   NS_INIT_ISUPPORTS();
   /* member initializers and constructor code */
+}
 
+nsImageFrame::~nsImageFrame()
+{
+  /* destructor code */
+  delete[] mBits;
   mBits = nsnull;
 }
 
-nsImage::~nsImage()
-{
-#if 0
-  /* destructor code */
-  if (mBits) {
-    delete[] mBits;
-    mBits = nsnull;
-  }
-#endif
-}
-
-/* void init (in gfx_dimension aWidth, in gfx_dimension aHeight, in gfx_format aFormat); */
-NS_IMETHODIMP nsImage::Init(gfx_dimension aWidth, gfx_dimension aHeight, gfx_format aFormat)
+/* void init (in gfx_coord aX, in gfx_coord aY, in gfx_dimension aWidth, in gfx_dimension aHeight, in gfx_format aFormat); */
+NS_IMETHODIMP nsImageFrame::Init(gfx_coord aX, gfx_coord aY, gfx_dimension aWidth, gfx_dimension aHeight, gfx_format aFormat)
 {
   if (aWidth <= 0 || aHeight <= 0) {
     printf("error - negative image size\n");
     return NS_ERROR_FAILURE;
   }
 
-  mSize.SizeTo(aWidth, aHeight);
+  delete[] mBits;
+
+  mRect.SetRect(aX, aY, aWidth, aHeight);
   mFormat = aFormat;
 
   switch (aFormat) {
@@ -71,7 +68,7 @@ NS_IMETHODIMP nsImage::Init(gfx_dimension aWidth, gfx_dimension aHeight, gfx_for
     break;
   }
 
-  PRInt32 ceilWidth(GFXCoordToIntCeil(mSize.width));
+  PRInt32 ceilWidth(GFXCoordToIntCeil(mRect.width));
 
   mBytesPerRow = (ceilWidth * mDepth) >> 5;
 
@@ -79,67 +76,78 @@ NS_IMETHODIMP nsImage::Init(gfx_dimension aWidth, gfx_dimension aHeight, gfx_for
     mBytesPerRow++;
   mBytesPerRow <<= 2;
 
-  mBitsLength = mBytesPerRow * GFXCoordToIntCeil(mSize.height);
+  mBitsLength = mBytesPerRow * GFXCoordToIntCeil(mRect.height);
 
   mBits = new PRUint8[mBitsLength];
 
   return NS_OK;
 }
 
-/* void initFromDrawable (in nsIDrawable aDrawable, in gfx_coord aX, in gfx_coord aY, in gfx_dimension aWidth, in gfx_dimension aHeight); */
-NS_IMETHODIMP nsImage::InitFromDrawable(nsIDrawable *aDrawable, gfx_coord aX, gfx_coord aY, gfx_dimension aWidth, gfx_dimension aHeight)
-{
-  return NS_ERROR_NOT_IMPLEMENTED;
-}
-
-/* readonly attribute gfx_dimension width; */
-NS_IMETHODIMP nsImage::GetWidth(gfx_dimension *aWidth)
+/* readonly attribute gfx_coord x; */
+NS_IMETHODIMP nsImageFrame::GetX(gfx_coord *aX)
 {
   if (!mBits)
     return NS_ERROR_NOT_INITIALIZED;
 
-  *aWidth = mSize.width;
+  *aX = mRect.x;
+  return NS_OK;
+}
+
+/* readonly attribute gfx_coord y; */
+NS_IMETHODIMP nsImageFrame::GetY(gfx_coord *aY)
+{
+  if (!mBits)
+    return NS_ERROR_NOT_INITIALIZED;
+
+  *aY = mRect.y;
+  return NS_OK;
+}
+
+
+/* readonly attribute gfx_dimension width; */
+NS_IMETHODIMP nsImageFrame::GetWidth(gfx_dimension *aWidth)
+{
+  if (!mBits)
+    return NS_ERROR_NOT_INITIALIZED;
+
+  *aWidth = mRect.width;
   return NS_OK;
 }
 
 /* readonly attribute gfx_dimension height; */
-NS_IMETHODIMP nsImage::GetHeight(gfx_dimension *aHeight)
+NS_IMETHODIMP nsImageFrame::GetHeight(gfx_dimension *aHeight)
 {
   if (!mBits)
     return NS_ERROR_NOT_INITIALIZED;
 
-  *aHeight = mSize.height;
+  *aHeight = mRect.height;
   return NS_OK;
 }
 
-#include <gdk/gdk.h>
-#include <gdk/gdkrgb.h>
-#include <gdk/gdkx.h>
-#include <gdk/gdkprivate.h>
-
-/* readonly attribute gfx_format format; */
-NS_IMETHODIMP nsImage::GetFormat(gfx_format *aFormat)
+/* readonly attribute nsRect2 rect; */
+NS_IMETHODIMP nsImageFrame::GetRect(nsRect2 **aRect)
 {
+  return NS_ERROR_NOT_IMPLEMENTED;
+
   if (!mBits)
     return NS_ERROR_NOT_INITIALIZED;
 
-  GdkWindow *rw = gdk_window_lookup(GDK_ROOT_WINDOW());
+//  *aRect = mRect;
+  return NS_OK;
+}
 
-  GdkGC *gc = gdk_gc_new(rw);
-  gdk_draw_rgb_image(rw, gc,
-                     0, 0, mSize.width, mSize.height,
-                     GDK_RGB_DITHER_MAX,
-                     mBits, 3*mSize.width);
-  gdk_gc_unref(gc);
-
-
+/* readonly attribute gfx_format format; */
+NS_IMETHODIMP nsImageFrame::GetFormat(gfx_format *aFormat)
+{
+  if (!mBits)
+    return NS_ERROR_NOT_INITIALIZED;
 
   *aFormat = mFormat;
   return NS_OK;
 }
 
 /* readonly attribute unsigned long bytesPerRow; */
-NS_IMETHODIMP nsImage::GetBytesPerRow(PRUint32 *aBytesPerRow)
+NS_IMETHODIMP nsImageFrame::GetBytesPerRow(PRUint32 *aBytesPerRow)
 {
   if (!mBits)
     return NS_ERROR_NOT_INITIALIZED;
@@ -149,7 +157,7 @@ NS_IMETHODIMP nsImage::GetBytesPerRow(PRUint32 *aBytesPerRow)
 }
 
 /* readonly attribute unsigned long bitsLength; */
-NS_IMETHODIMP nsImage::GetBitsLength(PRUint32 *aBitsLength)
+NS_IMETHODIMP nsImageFrame::GetBitsLength(PRUint32 *aBitsLength)
 {
   if (!mBits)
     return NS_ERROR_NOT_INITIALIZED;
@@ -159,7 +167,7 @@ NS_IMETHODIMP nsImage::GetBitsLength(PRUint32 *aBitsLength)
 }
 
 /* void getBits([array, size_is(length)] out PRUint8 bits, out unsigned long length); */
-NS_IMETHODIMP nsImage::GetBits(PRUint8 **aBits, PRUint32 *length)
+NS_IMETHODIMP nsImageFrame::GetBits(PRUint8 **aBits, PRUint32 *length)
 {
   if (!mBits)
     return NS_ERROR_NOT_INITIALIZED;
@@ -171,12 +179,25 @@ NS_IMETHODIMP nsImage::GetBits(PRUint8 **aBits, PRUint32 *length)
 }
 
 /* void setBits ([array, size_is (length), const] in PRUint8 data, in unsigned long length, in long offset); */
-NS_IMETHODIMP nsImage::SetBits(const PRUint8 *data, PRUint32 length, PRInt32 offset)
+NS_IMETHODIMP nsImageFrame::SetBits(const PRUint8 *data, PRUint32 length, PRInt32 offset)
 {
   if (!mBits)
     return NS_ERROR_NOT_INITIALIZED;
 
+  if (((PRUint32)offset + length) > mBitsLength)
+    return NS_ERROR_FAILURE;
+
   memcpy(mBits + offset, data, length);
 
   return NS_OK;
+}
+
+/* attribute long timeout; */
+NS_IMETHODIMP nsImageFrame::GetTimeout(PRInt32 *aTimeout)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+NS_IMETHODIMP nsImageFrame::SetTimeout(PRInt32 aTimeout)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
 }
