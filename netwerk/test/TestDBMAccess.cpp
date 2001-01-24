7b3171213e39802bb39bc8583f11e394d92302c6
darin@netscape.com <darin@netscape.com>
2001-01-24 01:18:23 +0000

    Ported to WIN32

--- netwerk/test/TestDBMAccess.cpp
+++ netwerk/test/TestDBMAccess.cpp
@@ -28,10 +28,19 @@
 #include "mcom_db.h"
 #include "nsString.h"
 
+#ifdef WIN32
+#define TMPDIR "c:\\temp\\"
+#define DIRSEP "\\"
+#else
+#define TMPDIR "/tmp/"
+#define DIRSEP "/"
+#endif
+
 #define DATASIZE 512
 #define ENTRYCOUNT 512
 #define USE_ENTRY_ID 1
 #define EXTRA_LOOKUP 1
+#define NUM_CYCLES 2
 
 DB* myDB;
 HASHINFO hash_info = {
@@ -47,12 +56,13 @@ HASHINFO hash_info = {
 
 int writeDBM(int cycles)
 {
+    printf("writeDBM\n");
     DBT db_key, db_data ;
     PRIntervalTime time = PR_IntervalNow();
 
     while (cycles--) {        
         // create database file
-        myDB = dbopen("/tmp/foodb",
+        myDB = dbopen(TMPDIR "foodb",
                       O_RDWR | O_CREAT ,
                       0600 ,
                       DB_HASH ,
@@ -105,6 +115,7 @@ int writeDBM(int cycles)
 int
 readDBM(int cycles)
 {
+    printf("readDBM\n");
     // begin timing "lookups"
     int status = 0 ;
     DBT db_key, db_data ;
@@ -159,8 +170,9 @@ readDBM(int cycles)
 int
 writeFile(int cycles)
 {
-    FILE* file;
-    int fStatus;
+    printf("writeFile\n");
+    PRFileDesc *fd;
+    PRInt32 fStatus;
     int x;
     char * data = (char*) malloc(DATASIZE);
     for (x=0; x<DATASIZE; x++)
@@ -168,86 +180,115 @@ writeFile(int cycles)
 
     PRIntervalTime time = PR_IntervalNow();
     while (cycles--) {
+        printf("writeFile [cycle=%d]\n", cycles);
+
         // create "cache" directories
-        PR_MkDir("/tmp/foo", 0755);
+        PR_MakeDir(TMPDIR "foo", 0755);
         
         for (x=0; x<32; x++) {
-            nsCAutoString filename; filename.Assign("/tmp/foo/");
+            nsCAutoString filename; filename.Assign(TMPDIR "foo" DIRSEP);
             filename.AppendInt(x);
-            PR_MkDir(filename, 0755);
+            PR_MakeDir(filename, 0755);
         }
         
         // create "cache" files
         for (x=1; x<=ENTRYCOUNT; x++) {
-            nsCAutoString filename; filename.Assign("/tmp/foo/");
+            nsCAutoString filename; filename.Assign(TMPDIR "foo" DIRSEP);
             filename.AppendInt( x % 32 );
-            filename.Append("/");
+            filename.Append(DIRSEP);
             filename.AppendInt( x );
-            
-            file = fopen(filename, "w");
-            if (!file)
+
+            PRIntervalTime i1, i2, i3;
+            i1 = PR_IntervalNow();
+
+            fd = PR_OpenFile(filename, PR_WRONLY|PR_TRUNCATE, 0644);
+            if (!fd)
                 printf("bad filename?  %s\n", (char*)filename);
-            
-            fStatus = fwrite (data, DATASIZE, 1, file);  
+
+            i2 = PR_IntervalNow();
+
+            fStatus = PR_Write(fd, data, DATASIZE);  
             if (fStatus == -1) {
-                printf("Bad fStatus %d\n", errno);
+                printf("Bad fStatus %d\n", PR_GetError());
                 exit(1);
             } 
-            fclose(file);
+            i3 = PR_IntervalNow();
+
+            PR_Close(fd);
+
+/*
+            printf(" - writing %s [topen=%d twrite=%d]\n", 
+                (const char *) filename,
+                PR_IntervalToMilliseconds(i2 - i1), 
+                PR_IntervalToMilliseconds(i3 - i2));
+*/
         }
     }
-    return PR_IntervalToMilliseconds( PR_IntervalNow() - time);
+    return PR_IntervalToMilliseconds(PR_IntervalNow() - time);
 }
 
 
 int
 readFile(int cycles)
 {
-    FILE* file;
-    int fStatus;
+    printf("readFile\n");
+    PRFileDesc* fd;
+    PRInt32 fStatus;
     
     // begin timing "lookups"
     PRIntervalTime time = PR_IntervalNow();
     while (cycles--) {
         for (int x=1; x<=ENTRYCOUNT; x++) {
-            nsCAutoString filename; filename.Assign("/tmp/foo/");
+            nsCAutoString filename; filename.Assign(TMPDIR "foo" DIRSEP);
             filename.AppendInt( x % 32 );
-            filename.Append("/");
+            filename.Append(DIRSEP);
             filename.AppendInt( x );
             
-            file = fopen(filename, "a+");
+            fd = PR_OpenFile(filename, PR_RDONLY, 0);
             
-            int size = ftell(file);
-            rewind(file);
+            PRInt32 size = PR_Available(fd);
             
             char* fdBuffer = (char*) malloc (size);
             
-            fStatus = fread (fdBuffer, size, 1, file);  
+            PRIntervalTime i1, i2, i3;
+            i1 = PR_IntervalNow();
+
+            fStatus = PR_Read(fd, fdBuffer, size);  
+
+            i2 = PR_IntervalNow();
             
             if (fStatus == -1) {
-                printf("Bad fStatus %d\n", errno);
+                printf("Bad fStatus %d\n", PR_GetError());
                 exit(1);
             } 
+            i3 = PR_IntervalNow();
             
-            fclose(file);
-            free (fdBuffer);
+            PR_Close(fd);
+            free(fdBuffer);
+
+/*
+            printf(" - reading %s [topen=%d tread=%d]\n", 
+                (const char *) filename,
+                PR_IntervalToMilliseconds(i2 - i1), 
+                PR_IntervalToMilliseconds(i3 - i2));
+*/
         }
     }
-    return PR_IntervalToMilliseconds( PR_IntervalNow() - time);
+    return PR_IntervalToMilliseconds(PR_IntervalNow() - time);
 }
 
 
 int
 main(void)
 {
-    int totalDBMTime  = writeDBM(32);
-    int totalFileTime = writeFile(32);
+    int totalDBMTime  = writeDBM(NUM_CYCLES);
+    int totalFileTime = writeFile(NUM_CYCLES);
 
     printf("total write dbm IO  ---- > (%d) milliseconds\n", totalDBMTime);
     printf("total write file IO ---- > (%d) milliseconds\n", totalFileTime);
 
-    totalDBMTime  = readDBM(32);
-    totalFileTime = readFile(32);
+    totalDBMTime  = readDBM(NUM_CYCLES);
+    totalFileTime = readFile(NUM_CYCLES);
 
     printf("\n");
     printf("total read dbm IO  ---- > (%d) milliseconds\n", totalDBMTime);
