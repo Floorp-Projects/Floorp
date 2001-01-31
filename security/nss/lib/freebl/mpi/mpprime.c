28904c3b7b1f16ec2830767db8ca3bc92cab2ef2
nelsonb@netscape.com <nelsonb@netscape.com>
2001-01-31 00:37:40 +0000

    Fix memory leak in mpp_make_prime.  Bug 67094.

--- security/nss/lib/freebl/mpi/mpprime.c
+++ security/nss/lib/freebl/mpi/mpprime.c
@@ -566,6 +566,7 @@ mp_err mpp_make_prime(mp_int *start, mp_size nBits, mp_size strong,
     mp_exch(&trial, start);
 CLEANUP:
   mp_clear(&trial);
+  mp_clear(&q);
   if (nTries)
     *nTries += i;
 #ifdef macintosh
