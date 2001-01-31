9d0048988ef5a487e088f51ae2c680f3b2d2e766
alecf@netscape.com <alecf@netscape.com>
2001-01-31 20:21:37 +0000

    fix for #64060 - bulk delete history entries by hostname & domainname also add a new history controller for future history coolness a=ben, r=jag, and a bit of r=timeless

--- xpfe/components/history/resources/history.js
+++ xpfe/components/history/resources/history.js
@@ -23,17 +23,148 @@
 
 // The history window uses JavaScript in bookmarks.js too.
 
-function HistoryInit() {
-    var tree = document.getElementById("historyTree");
-    var historyController = new nsTreeController(tree);
+function debug(msg)
+{
+    // Uncomment for noise
+    //dump(msg+"\n");
+}
 
+var gHistoryTree;
+var gLastHostname;
+var gLastDomain;
+var gGlobalHistory;
+
+var gDeleteByHostname;
+var gDeleteByDomain;
+var gHistoryBundle;
+
+function HistoryInit() {
+
+    gHistoryTree =      document.getElementById("historyTree");
+    gDeleteByHostname = document.getElementById("cmd_deleteByHostname");
+    gDeleteByDomain =   document.getElementById("cmd_deleteByDomain");
+    gHistoryBundle =    document.getElementById("historyBundle");
+    
+    var treeController = new nsTreeController(gHistoryTree);
+    var historyController = new nsHistoryController;
+    gHistoryTree.controllers.appendController(historyController);
+
+    gGlobalHistory = Components.classes["@mozilla.org/browser/global-history;1"].getService(Components.interfaces.nsIGlobalHistory);
+    
     var children = document.getElementById('treechildren-bookmarks');
     if (children.firstChild)
-        tree.selectItem(children.firstChild);
-    tree.focus();
+        gHistoryTree.selectItem(children.firstChild);
+    gHistoryTree.focus();
 
+    dump("Updating sort..\n");
     // do a sort
     RefreshSort();
+}
+
+function updateHistoryCommands()
+{
+    dump("Updating history commands..\n");
+    goUpdateCommand("cmd_deleteByHostname");
+    goUpdateCommand("cmd_deleteByDomain");
+}
+
+function historyOnSelect(event)
+{
+    dump("History selection has changed..\n");
+    // every time selection changes, save the last hostname
+    gLastHostname = "";
+    gLastDomain = "";
+    var selection = gHistoryTree.selectedItems;
+    if (selection && selection.length > 0) {
+        var url = selection[0].id;
+        // matches scheme://(hostname)...
+        var match = url.match(/.*:\/\/([^\/:]*)/);
+        
+        if (match && match.length>1)
+            gLastHostname = match[1];
+    }
+
+    if (gLastHostname) {
+        // matches the last foo.bar in foo.bar or baz.foo.bar
+        var match = gLastHostname.match(/([^.]+\.[^.]+$)/);
+        if (match)
+            gLastDomain = match[1];
+    }
+
+
+    document.commandDispatcher.updateCommands("select");
+}
+
+function nsHistoryController()
+{
+
+}
+
+nsHistoryController.prototype =
+{
+    supportsCommand: function(command)
+    {
+        switch(command) {
+        case "cmd_deleteByHostname":
+        case "cmd_deleteByDomain":
+            return true;
+        default:
+            return false;
+        }
+    },
+
+    isCommandEnabled: function(command)
+    {
+        var enabled = false;
+        var stringId;
+        var text;
+        switch(command) {
+        case "cmd_deleteByHostname":
+            dump("Updating cmd_deleteByHostname..\n");
+            if (gLastHostname) {
+                stringId = "deleteHost";
+                enabled = true;
+            } else {
+                stringId = "deleteHostNoSelection";
+            }
+            text =
+                gHistoryBundle.stringBundle.formatStringFromName(stringId,
+                                                                 [ gLastHostname ], 1);
+            dump("Setting value to " + text + "\n");
+            gDeleteByHostname.setAttribute("value", text);
+            
+            break;
+        case "cmd_deleteByDomain":
+            if (gLastDomain) {
+                stringId = "deleteDomain";
+                enabled = true;
+            } else {
+                stringId = "deleteDomainNoSelection";
+            }
+            text = gHistoryBundle.stringBundle.formatStringFromName(stringId,
+                                                                    [ gLastDomain ], 1);
+            gDeleteByDomain.setAttribute("value", text);
+        }
+        return enabled;
+    },
+
+    doCommand: function(command)
+    {
+        switch(command) {
+        case "cmd_deleteByHostname":
+            gGlobalHistory.RemovePagesFromHost(gLastHostname, false)
+            return true;
+            
+        case "cmd_deleteByDomain":
+            gGlobalHistory.RemovePagesFromHost(gLastDomain, true)
+            return true;
+            
+        default:
+            return false;
+        }
+
+    }
+
 }
 
 var historyDNDObserver = {
