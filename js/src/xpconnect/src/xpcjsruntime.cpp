a36df8834a346e18c21f152bd689c41d495ae926
mkaply%us.ibm.com <mkaply%us.ibm.com>
2001-02-02 13:54:03 +0000

    OS/2 TB breakage Need cast to pass C++ function

--- js/src/xpconnect/src/xpcjsruntime.cpp
+++ js/src/xpconnect/src/xpcjsruntime.cpp
@@ -258,7 +258,7 @@ XPCJSRuntime::XPCJSRuntime(nsXPConnect* aXPConnect,
 
     NS_ASSERTION(!gOldJSGCCallback, "XPCJSRuntime created more than once");
     if(mJSRuntime)
-        gOldJSGCCallback = JS_SetGCCallbackRT(mJSRuntime, GCCallback);
+        gOldJSGCCallback = JS_SetGCCallbackRT(mJSRuntime, (JSGCCallback)GCCallback);
 
     // Install a JavaScript 'debugger' keyword handler in debug builds only
 #ifdef DEBUG
