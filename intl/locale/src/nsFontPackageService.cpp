5f0e58f384ad00ad5573398e72f0f2c89c206dbe
ftang%netscape.com <ftang%netscape.com>
2001-02-06 02:34:46 +0000

    not part of the build yet check in support class for "font download" work sr=erik

---
+++ intl/locale/src/nsFontPackageService.cpp
@@ -0,0 +1,72 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 2000 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ *  Frank Yung-Fong Tang <ftang@netscape.com>
+ */
+
+#include "nsFontPackageService.h"
+
+
+NS_IMPL_THREADSAFE_ISUPPORTS2(nsFontPackageService, 
+   nsIFontPackageService,
+   nsIFontPackageProxy);
+
+/* from nsIFontPackageSercice.h */
+nsFontPackageService::nsFontPackageService() : mHandler(nsnull)
+{
+  NS_INIT_REFCNT();
+  /* member initializers and constructor code */
+}
+
+nsFontPackageService::~nsFontPackageService()
+{
+  /* destructor code */
+}
+
+/* void SetHandler (in nsIFontPackageHandler aHandler); */
+NS_IMETHODIMP nsFontPackageService::SetHandler(nsIFontPackageHandler *aHandler)
+{
+    mHandler = aHandler;
+    return NS_OK;
+}
+
+/* void FontPackageHandled (in boolean aSuccess, in boolean aReloadPages, in string aFontPackID); */
+NS_IMETHODIMP nsFontPackageService::FontPackageHandled(PRBool aSuccess, PRBool aReloadPages, const char *aFontPackID)
+{
+    // add implementation later
+    return NS_OK;
+}
+
+/* from nsIFontPackageProxy.h */
+
+/* void NeedFontPackage (in string aFontPackID); */
+NS_IMETHODIMP nsFontPackageService::NeedFontPackage(const char *aFontPackID)
+{
+    if(mHandler)
+       return mHandler->NeedFontPackage(aFontPackID);
+#ifdef _DEBUG
+    printf("We need to download the font package for %s\n", aFontPackID);
+    printf("But we don't have a nsIFontPackageHandler set up yet.\n");
+    printf("Embedding application, please implement a nsIFontPackageHandler\n");
+    printf("and call nsIFontPackageService::SetHandler to register it. Thanks\n");
+#endif 
+    NS_ASSERTION(0, "no handler for font package");
+    return NS_OK;
+}
