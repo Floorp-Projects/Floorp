51e70dc58ad44fd05abc1706b00d70bc46384366
ftang%netscape.com <ftang%netscape.com>
2001-02-06 23:44:14 +0000

    First Checked In.

---
+++ intl/uconv/ucvibm/nsUnicodeToCP864i.h
@@ -0,0 +1,130 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+
+ *
+
+ * The contents of this file are subject to the Netscape Public
+
+ * License Version 1.1 (the "License"); you may not use this file
+
+ * except in compliance with the License. You may obtain a copy of
+
+ * the License at http://www.mozilla.org/NPL/
+
+ *
+
+ * Software distributed under the License is distributed on an "AS
+
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+
+ * implied. See the License for the specific language governing
+
+ * rights and limitations under the License.
+
+ *
+
+ * The Original Code is Mozilla Communicator client code.
+
+ *
+
+ * The Initial Developer of the Original Code is Netscape Communications
+
+ * Corporation.  Portions created by Netscape are
+
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+
+ * Rights Reserved.
+
+ *
+
+ * Contributor(s): 
+
+ * IBM Corporation
+
+ *
+
+ * This Original Code has been modified by IBM Corporation.
+
+ * Modifications made by IBM described herein are
+
+ * Copyright (c) International Business Machines
+
+ * Corporation, 1999
+
+ *
+
+ * Modifications to Mozilla code or documentation
+
+ * identified per MPL Section 3.3
+
+ *
+
+ * Date         Modified by     Description of modification
+
+ * 12/09/1999   IBM Corp.       Support for IBM codepages - 864i
+
+ *
+
+ */
+
+
+
+#ifndef nsUnicodeToCP864i_h___
+
+#define nsUnicodeToCP864i_h___
+
+
+
+#include "nsUCvIBMSupport.h"
+
+
+
+//----------------------------------------------------------------------
+
+// Class nsUnicodeToCP864i [declaration]
+
+
+
+/**
+
+ * A character set converter from Unicode to CP864i.
+
+ */
+
+class nsUnicodeToCP864i : public nsTableEncoderSupport
+
+{
+
+public:
+
+
+
+  /**
+
+   * Class constructor.
+
+   */
+
+  nsUnicodeToCP864i();
+
+
+
+protected:
+
+
+
+  //--------------------------------------------------------------------
+
+  // Subclassing of nsEncoderSupport class [declaration]
+
+
+
+  NS_IMETHOD GetMaxLength(const PRUnichar * aSrc, PRInt32 aSrcLength, 
+
+      PRInt32 * aDestLength);
+
+};
+
+
+
+#endif /* nsUnicodeToCP864i_h___ */
+
