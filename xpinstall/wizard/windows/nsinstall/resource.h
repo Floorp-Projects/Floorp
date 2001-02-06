9f3ebcdd73250d62ba8770d1d7924073794a5d43
ssu@netscape.com <ssu@netscape.com>
2001-02-06 07:15:13 +0000

    fixing bug 49456 - installer memory usage too high.  r=dveditz, sr=mscott.  windows platform only.

--- xpinstall/wizard/windows/nsinstall/resource.h
+++ xpinstall/wizard/windows/nsinstall/resource.h
@@ -22,19 +22,19 @@
  * Copyright (C) 1999 Netscape Communications Corporation. All
  * Rights Reserved.
  *
- * Contributor(s): 
+ * Contributor(s):
  *     Troy Chevalier <troy@netscape.com>
  *     Sean Su <ssu@netscape.com>
  */
 
 #include "winuser.h"
 #include "winresrc.h"
-
 #define IDS_PROMPT                      1
 #define IDS_STATUS_EXTRACTING           2
 #define IDS_STATUS_LAUNCHING_SETUP      3
 #define IDS_ERROR_FILE_WRITE            4
 #define IDS_TITLE                       5
+#define IDS_ERROR_OUT_OF_MEMORY         6
 #define IDD_EXTRACTING                  101
 #define IDC_STATUS                      1001
 #define IDC_GAUGE                       1002
@@ -43,10 +43,10 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
-#define _APS_NO_MFC                     1
 #endif
 #endif
