0502a8f84e436a8fc189004cdd73055d623a0428
bienvenu%netscape.com <bienvenu%netscape.com>
2001-02-07 19:27:00 +0000

    add files to build so Seth and I can work on them NOT PART OF BUILD

---
+++ mailnews/base/src/nsMsgDBView.cpp
@@ -0,0 +1,75 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 2001 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ */
+
+#include "msgCore.h"
+#include "nsMsgDBView.h"
+#include "nsISupports.h"
+/* Implementation file */
+
+NS_IMPL_ISUPPORTS1(nsMsgDBView, nsIMsgDBView)
+
+nsMsgDBView::nsMsgDBView()
+{
+  NS_INIT_ISUPPORTS();
+  /* member initializers and constructor code */
+	m_sortValid = PR_TRUE;
+  m_sortOrder = nsMsgViewSortOrder::none;
+//	m_viewFlags = (ViewFlags) 0;
+}
+
+nsMsgDBView::~nsMsgDBView()
+{
+  /* destructor code */
+}
+
+/* void open (in nsIMsgDatabase msgDB, in nsMsgViewSortType viewType); */
+NS_IMETHODIMP nsMsgDBView::Open(nsIMsgDatabase *msgDB, nsMsgViewSortType *viewType)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void close (); */
+NS_IMETHODIMP nsMsgDBView::Close()
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void init (out long pCount); */
+NS_IMETHODIMP nsMsgDBView::Init(PRInt32 *pCount)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void addKeys (out long pKeys, out long pFlags, in string pLevels, in nsMsgViewSortType sortType, in long numKeysToAdd); */
+NS_IMETHODIMP nsMsgDBView::AddKeys(PRInt32 *pKeys, PRInt32 *pFlags, const char *pLevels, nsMsgViewSortType *sortType, PRInt32 numKeysToAdd)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void sort (in nsMsgViewSortType sortType, in nsMsgViewSortOrder sortOrder); */
+NS_IMETHODIMP nsMsgDBView::Sort(nsMsgViewSortType *sortType, nsMsgViewSortOrder *sortOrder)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+
+
---
+++ mailnews/base/src/nsMsgDBView.h
@@ -0,0 +1,53 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 2001 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ */
+
+#ifndef _nsMsgDBView_H_
+#define _nsMsgDBView_H_
+
+#include "nsIMsgDBView.h"
+#include "nsIMsgDatabase.h"
+#include "nsMsgLineBuffer.h" // for nsByteArray
+#include "nsMsgKeyArray.h"
+
+// I think this will be an abstract implementation class.
+// The classes that implement the outliner support will probably
+// inherit from this class.
+class nsMsgDBView : public nsIMsgDBView
+{
+public:
+  nsMsgDBView();
+  virtual ~nsMsgDBView();
+
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIMSGDBVIEW
+
+protected:
+  nsMsgKeyArray m_keys;
+  nsUInt32Array m_flags;
+  nsByteArray   m_levels;
+
+  nsCOMPtr <nsIMsgDatabase> m_db;
+  PRBool		m_sortValid;
+  nsMsgViewSortOrderValue m_sortOrder;
+};
+
+#endif
