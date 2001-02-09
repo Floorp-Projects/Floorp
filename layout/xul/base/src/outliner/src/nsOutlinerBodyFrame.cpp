8232c29b9b654f4f020537b766341e79769c73de
hyatt@netscape.com <hyatt@netscape.com>
2001-02-09 07:21:01 +0000

    Not part of build.

--- layout/xul/base/src/outliner/src/nsOutlinerBodyFrame.cpp
+++ layout/xul/base/src/outliner/src/nsOutlinerBodyFrame.cpp
@@ -27,43 +27,53 @@
 #include "nsIPresContext.h"
 #include "nsOutlinerBodyFrame.h"
 #include "nsXULAtoms.h"
+#include "nsHTMLAtoms.h"
+#include "nsINameSpaceManager.h"
 #include "nsIContent.h"
 #include "nsIStyleContext.h"
 #include "nsIDOMElement.h"
 #include "nsIDOMNodeList.h"
 #include "nsIContent.h"
 #include "nsICSSStyleRule.h"
+#include "nsCSSRendering.h"
 
 // The style context cache impl
 nsresult 
 nsOutlinerStyleCache::GetStyleContext(nsICSSPseudoComparator* aComparator,
                                       nsIPresContext* aPresContext, nsIContent* aContent, 
-                                      nsIStyleContext* aContext, nsISupportsArray* aInputWord,
+                                      nsIStyleContext* aContext, nsIAtom* aPseudoElement,
+                                      nsISupportsArray* aInputWord,
                                       nsIStyleContext** aResult)
 {
   *aResult = nsnull;
   
-  nsCOMPtr<nsIStyleContext> nextContext;
-  nsIAtom* pseudo = NS_NewAtom(":-moz-outliner-row");
-  aPresContext->ResolvePseudoStyleWithComparator(aContent, pseudo,
-                                             aContext, PR_FALSE,
-                                             aComparator, getter_AddRefs(nextContext));
-
   PRUint32 count;
   aInputWord->Count(&count);
   nsDFAState startState(0);
   nsDFAState* currState = &startState;
-  nsCOMPtr<nsIStyleContext> currContext = aContext;
+
+  // Go ahead and init the transition table.
+  if (!mTransitionTable) {
+    // Automatic miss. Build the table
+    mTransitionTable = new nsHashtable;
+  }
+
+  // The first transition is always made off the supplied pseudo-element.
+  nsTransitionKey key(currState->GetStateID(), aPseudoElement);
+  currState = NS_STATIC_CAST(nsDFAState*, mTransitionTable->Get(&key));
+
+  if (!currState) {
+    // We had a miss. Make a new state and add it to our hash.
+    mNextState++;
+    currState = new nsDFAState(mNextState);
+    mTransitionTable->Put(&key, currState);
+  }
+
   for (PRUint32 i = 0; i < count; i++)
   {
     nsCOMPtr<nsIAtom> pseudo = getter_AddRefs(NS_STATIC_CAST(nsIAtom*, aInputWord->ElementAt(i)));
     nsTransitionKey key(currState->GetStateID(), pseudo);
-    if (!mTransitionTable) {
-      // Automatic miss. Build the table
-      mTransitionTable = new nsHashtable;
-    }
-    else
-      currState = NS_STATIC_CAST(nsDFAState*, mTransitionTable->Get(&key));
+    currState = NS_STATIC_CAST(nsDFAState*, mTransitionTable->Get(&key));
 
     if (!currState) {
       // We had a miss. Make a new state and add it to our hash.
@@ -71,36 +81,75 @@ nsOutlinerStyleCache::GetStyleContext(nsICSSPseudoComparator* aComparator,
       mNextState++;
       mTransitionTable->Put(&key, currState);
     }
-
-    // Look up our style context at this step of the computation.
-    nsCOMPtr<nsIStyleContext> nextContext;
-    if (mCache)
-      nextContext = getter_AddRefs(NS_STATIC_CAST(nsIStyleContext*, mCache->Get(currState)));
-    if (!nextContext) {
-      // We missed. Resolve this pseudo-style.
-      aPresContext->ResolvePseudoStyleWithComparator(aContent, pseudo,
-                                                     currContext, PR_FALSE,
-                                                     aComparator,
-                                                     getter_AddRefs(nextContext));
-      // Put it in our table.
-      if (!mCache)
-        mCache = new nsSupportsHashtable;
-      mCache->Put(currState, nextContext);
-    }
-
-    // Advance to the next context.
-    currContext = nextContext;
   }
 
-  if (currState == &startState)
-    // No input word supplied.
-    return NS_OK;
-
-  // We're in some final state. Return the last context resolved.
-  *aResult = currContext;
-  NS_IF_ADDREF(*aResult);
+  // We're in a final state.
+  // Look up our style context for this state.
+  nsCOMPtr<nsIStyleContext> currContext;
+  if (mCache)
+    *aResult = NS_STATIC_CAST(nsIStyleContext*, mCache->Get(currState)); // Addref occurs on *aResult.
+  if (!*aResult) {
+    // We missed the cache. Resolve this pseudo-style.
+    aPresContext->ResolvePseudoStyleWithComparator(aContent, aPseudoElement,
+                                                   currContext, PR_FALSE,
+                                                   aComparator,
+                                                   aResult); // Addref occurs on *aResult.
+    // Put it in our table.
+    if (!mCache)
+      mCache = new nsSupportsHashtable;
+    mCache->Put(currState, *aResult);
+  }
 
   return NS_OK;
+}
+
+// Column class that caches all the info about our column.
+nsOutlinerColumn::nsOutlinerColumn(nsIContent* aColElement, nsIFrame* aFrame)
+{
+  mColFrame = aFrame;
+  mColElement = aColElement; 
+
+  // Fetch the ID.
+  mColElement->GetAttribute(kNameSpaceID_None, nsHTMLAtoms::id, mID);
+
+  // Fetch the crop style.
+  mCropStyle = 0;
+  nsAutoString crop;
+  mColElement->GetAttribute(kNameSpaceID_None, nsXULAtoms::crop, crop);
+  if (crop.EqualsIgnoreCase("middle"))
+    mCropStyle = 1;
+  else if (crop.EqualsIgnoreCase("right"))
+    mCropStyle = 2;
+
+  // Cache our text alignment policy.
+  nsCOMPtr<nsIStyleContext> styleContext;
+  aFrame->GetStyleContext(getter_AddRefs(styleContext));
+
+  const nsStyleText* textStyle =
+        (const nsStyleText*)styleContext->GetStyleData(eStyleStruct_Text);
+
+  mTextAlignment = textStyle->mTextAlign;
+
+  // Figure out if we're the primary column (that has to have indentation
+  // and twisties drawn.
+  mIsPrimaryCol = PR_FALSE;
+  nsAutoString primary;
+  mColElement->GetAttribute(kNameSpaceID_None, nsXULAtoms::primary, primary);
+  if (primary.EqualsIgnoreCase("true"))
+    mIsPrimaryCol = PR_TRUE;
+
+  // Figure out if we're a cycling column (one that doesn't cause a selection
+  // to happen).
+  mIsCyclerCol = PR_FALSE;
+  nsAutoString cycler;
+  mColElement->GetAttribute(kNameSpaceID_None, nsXULAtoms::cycler, cycler);
+  if (cycler.EqualsIgnoreCase("true"))
+    mIsCyclerCol = PR_TRUE;
+}
+
+nscoord nsOutlinerColumn::GetColumnWidth()
+{
+  return 0;
 }
 
 //
@@ -137,6 +186,10 @@ nsOutlinerBodyFrame::nsOutlinerBodyFrame(nsIPresShell* aPresShell)
 nsOutlinerBodyFrame::~nsOutlinerBodyFrame()
 {
   delete mColumns;
+  
+  // Drop our ref to the view.
+  mView->SetOutliner(nsnull);
+  mView = nsnull;
 }
 
 NS_IMETHODIMP_(nsrefcnt) 
@@ -160,11 +213,16 @@ NS_IMETHODIMP nsOutlinerBodyFrame::GetView(nsIOutlinerView * *aView)
 
 NS_IMETHODIMP nsOutlinerBodyFrame::SetView(nsIOutlinerView * aView)
 {
+  // Outliner, meet the view.
   mView = aView;
   
-  // Changing the store causes us to refetch our data.  This will
+  // View, meet the outliner.
+  mView->SetOutliner(this);
+
+  // Changing the view causes us to refetch our data.  This will
   // necessarily entail a full invalidation of the outliner.
   mTopRowIndex = 0;
+  delete mColumns;
   Invalidate();
   
   return NS_OK;
@@ -234,29 +292,38 @@ NS_IMETHODIMP nsOutlinerBodyFrame::RowsRemoved(PRInt32 index, PRInt32 count)
 
 PRInt32 nsOutlinerBodyFrame::GetRowHeight(nsIPresContext* aPresContext)
 {
-  // Look up the correct height in our cache.
+  // Look up the correct height.  It is equal to the specified height
+  // + the specified margins.
   nsCOMPtr<nsIStyleContext> rowContext;
   mScratchArray->Clear();
-  mScratchArray->AppendElement(nsXULAtoms::mozoutlinerrow);
-  GetPseudoStyleContext(aPresContext, getter_AddRefs(rowContext));
+  GetPseudoStyleContext(aPresContext, nsXULAtoms::mozoutlinerrow, getter_AddRefs(rowContext));
   if (rowContext) {
     const nsStylePosition* myPosition = (const nsStylePosition*)
           rowContext->GetStyleData(eStyleStruct_Position);
     if (myPosition->mHeight.GetUnit() == eStyleUnit_Coord)  {
       PRInt32 val = myPosition->mHeight.GetCoordValue();
-      if (val > 0) return val;
+      if (val > 0) {
+        // Inflate the height by our margins.
+        nsRect rowRect(0,0,0,val);
+        const nsStyleMargin* rowMarginData = (const nsStyleMargin*)rowContext->GetStyleData(eStyleStruct_Margin);
+        nsMargin rowMargin;
+        rowMarginData->GetMargin(rowMargin);
+        rowRect.Inflate(rowMargin);
+        val = rowRect.height;
+      }
+      return val;
     }
   }
-  return 16;
+  return 16; // As good a default as any.
 }
 
 nsRect nsOutlinerBodyFrame::GetInnerBox()
 {
   nsRect r(0,0,mRect.width, mRect.height);
   nsMargin m(0,0,0,0);
-  const nsStyleSpacing* mySpacing = (const nsStyleSpacing*)
-        mStyleContext->GetStyleData(eStyleStruct_Spacing);
-  mySpacing->GetBorderPadding(m);
+  const nsStyleBorderPadding* borderpadding = (const nsStyleBorderPadding*)
+      mStyleContext->GetStyleData(eStyleStruct_BorderPaddingShortcut);
+  borderpadding->GetBorderPadding(m);
   r.Deflate(m);
   return r;
 }
@@ -292,18 +359,49 @@ NS_IMETHODIMP nsOutlinerBodyFrame::Paint(nsIPresContext*      aPresContext,
     nsRect rowRect(0, mRowHeight*(i-mTopRowIndex), mInnerBox.width, mRowHeight);
     nsRect dirtyRect;
     if (dirtyRect.IntersectRect(aDirtyRect, rowRect))
-      PaintRow(i, aPresContext, aRenderingContext, aDirtyRect, aWhichLayer);
+      PaintRow(i, rowRect, aPresContext, aRenderingContext, aDirtyRect, aWhichLayer);
   }
 
   return NS_OK;
 }
 
-NS_IMETHODIMP nsOutlinerBodyFrame::PaintRow(int aRowIndex, 
+NS_IMETHODIMP nsOutlinerBodyFrame::PaintRow(int aRowIndex, const nsRect& aRowRect,
                                             nsIPresContext*      aPresContext,
                                             nsIRenderingContext& aRenderingContext,
                                             const nsRect&        aDirtyRect,
                                             nsFramePaintLayer    aWhichLayer)
 {
+  // We have been given a rect for our row.  We treat this row like a full-blown
+  // frame, meaning that it can have borders, margins, padding, and a background.
+  
+  // Without a view, we have no data. Check for this up front.
+  if (!mView)
+    return NS_OK;
+
+  // Now obtain the properties for our row.
+  mScratchArray->Clear();
+  mView->GetRowProperties(aRowIndex, mScratchArray);
+
+  // Resolve style for the row.  It contains all the info we need to lay ourselves
+  // out and to paint.
+  nsCOMPtr<nsIStyleContext> rowContext;
+  GetPseudoStyleContext(aPresContext, nsXULAtoms::mozoutlinerrow, getter_AddRefs(rowContext));
+
+  // Obtain the margins for the row and then deflate our rect by that 
+  // amount.  The row is assumed to be contained within the deflated rect.
+  nsRect rowRect(aRowRect);
+  const nsStyleMargin* rowMarginData = (const nsStyleMargin*)rowContext->GetStyleData(eStyleStruct_Margin);
+  nsMargin rowMargin;
+  rowMarginData->GetMargin(rowMargin);
+  rowRect.Deflate(rowMargin);
+
+  // If the layer is the background layer, we must paint our borders and background for our
+  // row rect.
+  if (NS_FRAME_PAINT_LAYER_BACKGROUND == aWhichLayer)
+    PaintBackgroundLayer(rowContext, aPresContext, aRenderingContext, rowRect, aDirtyRect);
+
+  // Now loop over our cells. Only paint a cell if it intersects with our dirty rect.
+
   return NS_OK;
 }
   
@@ -317,11 +415,40 @@ NS_IMETHODIMP nsOutlinerBodyFrame::PaintCell(int aRowIndex,
   return NS_OK;
 }
 
+NS_IMETHODIMP
+nsOutlinerBodyFrame::PaintBackgroundLayer(nsIStyleContext* aStyleContext, nsIPresContext* aPresContext, 
+                                          nsIRenderingContext& aRenderingContext, 
+                                          const nsRect& aRect, const nsRect& aDirtyRect)
+{
+
+  const nsStyleDisplay* disp = (const nsStyleDisplay*)
+      aStyleContext->GetStyleData(eStyleStruct_Display);
+  const nsStyleColor* myColor = (const nsStyleColor*)
+      aStyleContext->GetStyleData(eStyleStruct_Color);
+  const nsStyleBorder* myBorder = (const nsStyleBorder*)
+      aStyleContext->GetStyleData(eStyleStruct_Border);
+  const nsStyleOutline* myOutline = (const nsStyleOutline*)
+      aStyleContext->GetStyleData(eStyleStruct_Outline);
+  
+  nsCSSRendering::PaintBackground(aPresContext, aRenderingContext, this,
+                                  aDirtyRect, aRect, *myColor, *myBorder, 0, 0);
+
+  nsCSSRendering::PaintBorder(aPresContext, aRenderingContext, this,
+                              aDirtyRect, aRect, *myBorder, mStyleContext, 0);
+
+  nsCSSRendering::PaintOutline(aPresContext, aRenderingContext, this,
+                              aDirtyRect, aRect, *myBorder, *myOutline, aStyleContext, 0);
+
+  return NS_OK;
+}
+
 // The style cache.
 nsresult 
-nsOutlinerBodyFrame::GetPseudoStyleContext(nsIPresContext* aPresContext, nsIStyleContext** aResult)
+nsOutlinerBodyFrame::GetPseudoStyleContext(nsIPresContext* aPresContext, nsIAtom* aPseudoElement, 
+                                           nsIStyleContext** aResult)
 {
-  return mStyleCache.GetStyleContext(this, aPresContext, mContent, mStyleContext, mScratchArray, aResult);
+  return mStyleCache.GetStyleContext(this, aPresContext, mContent, mStyleContext, aPseudoElement,
+                                     mScratchArray, aResult);
 }
 
 // Our comparator for resolving our complex pseudos
@@ -329,10 +456,26 @@ NS_IMETHODIMP
 nsOutlinerBodyFrame::PseudoMatches(nsIAtom* aTag, nsCSSSelector* aSelector, PRBool* aResult)
 {
   if (aSelector->mTag == aTag) {
-    printf("LA!");
+    // Iterate the pseudoclass list.  For each item in the list, see if
+    // it is contained in our scratch array.  If we have a miss, then
+    // we aren't a match.  If all items in the pseudoclass list are
+    // present in the scratch array, then we have a match.
+    nsAtomList* curr = aSelector->mPseudoClassList;
+    while (curr) {
+      PRInt32 index;
+      mScratchArray->GetIndexOf(curr->mAtom, &index);
+      if (index == -1) {
+        *aResult = PR_FALSE;
+        break;
+      }
+      curr = curr->mNext;
+    }
+    *aResult = PR_TRUE;
   }
-  return NS_OK;
+  else 
+    *aResult = PR_FALSE;
 
+  return NS_OK;
 }
 
 void
