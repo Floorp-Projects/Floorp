c9c554fa5c9af396304eaa03d8ebd0bb5396f345
danm%netscape.com <danm%netscape.com>
2001-02-17 02:24:53 +0000

    implementation of WindowCreator callback. bug 67368 r=hyatt,pinkerton

---
+++ embedding/tests/winEmbed/WindowCreator.cpp
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 2001 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ */
+
+#include "nsIWebBrowserChrome.h"
+#include "WindowCreator.h"
+#include "winEmbed.h"
+
+WindowCreator::WindowCreator() {
+  NS_INIT_REFCNT();
+}
+
+WindowCreator::~WindowCreator() {
+}
+
+NS_IMPL_ISUPPORTS1(WindowCreator, nsIWindowCreator)
+
+NS_IMETHODIMP
+WindowCreator::CreateWindow(nsIWebBrowserChrome *parent,
+                            PRUint32 chromeFlags,
+                            nsIWebBrowserChrome **_retval)
+{
+  NS_ENSURE_ARG_POINTER(_retval);
+  ::CreateBrowserWindow(PRInt32(chromeFlags), parent, _retval);
+  return *_retval ? NS_OK : NS_ERROR_FAILURE;
+}
