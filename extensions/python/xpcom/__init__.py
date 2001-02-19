16db44849b018c1b16ffc1f8b1f2b598a76c0b6b
markh@activestate.com <markh@activestate.com>
2001-02-19 08:31:46 +0000

    Update the way the license is expressed and minor makefile mods.
    Still not building pending a decision on bug 66610.

--- extensions/python/xpcom/__init__.py
+++ extensions/python/xpcom/__init__.py
@@ -1,5 +1,19 @@
-# Copyright (c) 2000-2001 ActiveState Tool Corporation.
-# See the file LICENSE.txt for licensing information.
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+# the specific language governing rights and limitations under the License.
+#
+# The Original Code is the Python XPCOM language bindings.
+#
+# The Initial Developer of the Original Code is ActiveState Tool Corp.
+# Portions created by ActiveState Tool Corp. are Copyright (C) 2000, 2001
+# ActiveState Tool Corp.  All Rights Reserved.
+#
+# Contributor(s): Mark Hammond <MarkH@ActiveState.com> (original author)
+#
 
 # The XPCOM (Cross Platform COM) package.
 import exceptions
