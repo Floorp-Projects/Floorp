52a04a142afe8d490f48b772e1bfa1c6168e0dc3
oeschger%netscape.com <oeschger%netscape.com>
2001-02-23 14:57:34 +0000

    updates to help chrome, NOT PART OF BUILD

--- extensions/help/resources/content/help.js
+++ extensions/help/resources/content/help.js
@@ -1,6 +1,15 @@
 //-------- global variables
 
 var gBrowser;
+var key {
+ "?mail": "chrome://help/locale/mail_help.html",
+ "?nav":  "chrome://help/locale/nav_help.html",
+ "?im:    "chrome://help/locale/im_help.html",
+ "?sec:   "chrome://help/locale/security_help.html",
+ "?cust:  "chrome://help/locale/customize_help.html",
+ "?comp:  "chrome://help/locale/composer_help.html",
+}
+
 
 function init()
 {
@@ -18,11 +27,31 @@ function init()
 
   getWebNavigation().sessionHistory = sessionHistory;
 
-  if ("argument" in window && window.arguments.length >= 1) {
-    browser.loadURI(window.arguments[0]);
+  //if ("argument" in window && window.arguments.length >= 1) {
+  //  browser.loadURI(window.arguments[0]);
+  //} else {
+  //  goHome(); // should be able to do browser.goHome();
+  //}
+
+  if (window.location.search) {
+      browser.loadURI(key[window.location.search]);
+      selectTOC(key[window.location.search]);
   } else {
-    goHome(); // should be able to do browser.goHome();
-  }
+      goHome();
+}
+
+function selectTOC(link_attr) {
+  var items = document.getElementsByAttribute("helplink", link_attr);
+  if (items.length >= 1) {
+	var parentRow = items[0].parentNode;
+  	var selectableNode = parentRow.parentNode; 	// helplink is an attribute
+  								// on a treecell, which cannot be selected
+  	var tree = document.getElementById("help-toc-tree");
+  	tree.selectItem(selectableNode);
+  } 
+}
+
+
 }
 
 function getWebNavigation()
@@ -52,7 +81,7 @@ function goForward()
 
 function goHome() {
   // load "Welcome" page
-  loadURI("chrome://help/locale/maincont.html");
+  loadURI("chrome://help/locale/welcome_help.html");
 }
 
 function print()
