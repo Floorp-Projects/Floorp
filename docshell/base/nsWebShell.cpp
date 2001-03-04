bb38a1e1ac3eabcde6beda62fd93efde52249a83
dbaron@fas.harvard.edu <dbaron@fas.harvard.edu>
2001-03-04 19:43:55 +0000

    Fix bad vi modeline that caused text to wrap at 2 characters.  r=gagan  (Comment changes only.)

--- docshell/base/nsWebShell.cpp
+++ docshell/base/nsWebShell.cpp
@@ -1,5 +1,5 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * vi:tw=2:ts=2:et:sw=2:
+ * vim:ts=2:et:sw=2:
  *
  * The contents of this file are subject to the Netscape Public
  * License Version 1.1 (the "License"); you may not use this file
