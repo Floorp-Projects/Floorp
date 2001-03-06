dc4d7e81c25b45840f5f86c1a89b21fac66358af
timeless%mac.com <timeless%mac.com>
2001-03-06 15:51:05 +0000

    fix bug 46127 -console option missing from -help listing patch by anedah-9@sm.luth.se r=timeless, sr=alecf

--- xpfe/bootstrap/nsAppRunner.cpp
+++ xpfe/bootstrap/nsAppRunner.cpp
@@ -1057,6 +1057,9 @@ static void DumpHelp(char *appname)
   printf("%s-ProfileWizard%sStart with profile wizard.\n",HELP_SPACER_1,HELP_SPACER_2);
   printf("%s-ProfileManager%sStart with profile manager.\n",HELP_SPACER_1,HELP_SPACER_2);
   printf("%s-SelectProfile%sStart with profile selection dialog.\n",HELP_SPACER_1,HELP_SPACER_2);
+#ifdef XP_WIN32
+  printf("%s-console%sStart Mozilla with a debugging console.\n",HELP_SPACER_1,HELP_SPACER_2);
+#endif
 #ifdef MOZ_ENABLE_XREMOTE
   printf("%s-remote <command>%sExecute <command> in an already running\n"
          "%sMozilla process.  For more info, see:\n"
