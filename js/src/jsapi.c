b44b4774707c3ed9cd07672ad834b5934e812c17
pschwartau%netscape.com <pschwartau%netscape.com>
2001-03-09 07:36:48 +0000

    Updated JS_GetImplementationVersion() to date of latest JS release (67111, r=rginda, sr=jband).

--- js/src/jsapi.c
+++ js/src/jsapi.c
@@ -1010,7 +1010,7 @@ JS_ToggleOptions(JSContext *cx, uint32 options)
 JS_PUBLIC_API(const char *)
 JS_GetImplementationVersion(void)
 {
-    return "JavaScript-C 1.5 pre-release 1 1999 10 31";
+    return "JavaScript-C 1.5 pre-release 3 2001-03-07";
 }
 
 
