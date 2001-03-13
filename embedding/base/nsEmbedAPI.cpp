edb0425331b47201f127d8cdc989467bed7eed9f
attinasi%netscape.com <attinasi%netscape.com>
2001-03-13 08:39:37 +0000

    Trying to fix bustage: removing references to appstartup stuff.

--- embedding/base/nsEmbedAPI.cpp
+++ embedding/base/nsEmbedAPI.cpp
@@ -25,7 +25,6 @@
 #include "nsIServiceManager.h"
 #include "nsIEventQueueService.h"
 #include "nsIChromeRegistry.h"
-#include "nsIAppStartupNotifier.h"
 #include "nsIStringBundle.h"
 
 #include "nsIDirectoryService.h"
@@ -111,11 +110,6 @@ nsresult NS_InitEmbedding(nsILocalFile *mozBinDirectory,
 
         sRegistryInitializedFlag = PR_TRUE;
     }
-
-	nsCOMPtr<nsIObserver> mStartupNotifier = do_CreateInstance(NS_APPSTARTUPNOTIFIER_CONTRACTID, &rv);
-	if(NS_FAILED(rv))
-		return rv;
-	mStartupNotifier->Observe(nsnull, APPSTARTUP_TOPIC, nsnull);
 
     // Create the Event Queue for the UI thread...
     //
