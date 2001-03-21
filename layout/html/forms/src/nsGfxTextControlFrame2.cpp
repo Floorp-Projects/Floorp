69b6cfdc47a3f1c17876e80441f814a45ab96dd5
blakeross%telocity.com <blakeross%telocity.com>
2001-03-21 22:37:59 +0000

    Backing out to fix bustage.

--- layout/html/forms/src/nsGfxTextControlFrame2.cpp
+++ layout/html/forms/src/nsGfxTextControlFrame2.cpp
@@ -3080,7 +3080,7 @@ nsGfxTextControlFrame2::SetTextControlFrameState(const nsAReadableString& aValue
       else {
         nsCOMPtr<nsIPlaintextEditor> textEditor = do_QueryInterface(mEditor);
         if (textEditor)
-          textEditor->InsertText(currentValue);
+          textEditor->InsertText(currentValue.GetUnicode());
       }
       mEditor->SetFlags(savedFlags);
       if (selPriv)
