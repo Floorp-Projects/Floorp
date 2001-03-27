a3239107a68c694bae8575d8c388e0654e85ca5b
pschwartau%netscape.com <pschwartau%netscape.com>
2001-03-27 07:43:17 +0000

    Minor fixes to code and whitespace.

--- js/tests/ecma_3/RegExp/regress-67773.js
+++ js/tests/ecma_3/RegExp/regress-67773.js
@@ -19,12 +19,14 @@
 * Contributor(s): pschwartau@netscape.com  
 * Date: 06 February 2001
 *
-* SUMMARY:  Arose from Bugzilla bug 67773: 
+* SUMMARY:  Arose from Bugzilla bug 67773:
 * "Regular subexpressions followed by + failing to run to completion"
 *
-*  See http://bugzilla.mozilla.org/show_bug.cgi?id=67773
+* See http://bugzilla.mozilla.org/show_bug.cgi?id=67773
+* See http://bugzilla.mozilla.org/show_bug.cgi?id=69989
 */
 //-------------------------------------------------------------------------------------------------
+var i = 0;
 var bug = 67773;
 var summary = 'Testing regular subexpressions followed by ? or +\n';
 var cnSingleSpace = ' ';
@@ -37,104 +39,104 @@ var expectedmatch = ''; var expectedmatches = new Array();
 pattern = /^(\S+)?( ?)(B+)$/;  //single space before second ? character
     string = 'AAABBB AAABBB ';  //single space at middle and at end -
     actualmatch = string.match(pattern);
-    expectedmatch = null;  
-    addThis(); 
+    expectedmatch = null;
+    addThis();
 
     string = 'AAABBB BBB';  //single space in the middle
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string,  'AAABBB', cnSingleSpace,  'BBB');  
-    addThis(); 
+    expectedmatch = Array(string,  'AAABBB', cnSingleSpace,  'BBB');
+    addThis();
 
     string = 'AAABBB AAABBB';  //single space in the middle
     actualmatch = string.match(pattern);
-    expectedmatch = null;  
-    addThis(); 
+    expectedmatch = null;
+    addThis();
 
 
 pattern = /^(A+B)+$/;
     string = 'AABAAB';
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string,  'AAB');  
+    expectedmatch = Array(string,  'AAB');
     addThis();
 
     string = 'ABAABAAAAAAB';
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string,  'AAAAAAB');  
+    expectedmatch = Array(string,  'AAAAAAB');
     addThis();
 
     string = 'ABAABAABAB';
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string,  'AB');  
-    addThis(); 
+    expectedmatch = Array(string,  'AB');
+    addThis();
 
     string = 'ABAABAABABB';
     actualmatch = string.match(pattern);
-    expectedmatch = null;   // because string doesn't match at end   
-    addThis(); 
+    expectedmatch = null;   // because string doesn't match at end
+    addThis();
 
 
 pattern = /^(A+1)+$/;
     string = 'AA1AA1';
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string,  'AA1');  
-    addThis(); 
+    expectedmatch = Array(string,  'AA1');
+    addThis();
 
 
 pattern = /^(\w+\-)+$/;
     string = '';
     actualmatch = string.match(pattern);
-    expectedmatch = null;  
-    addThis(); 
+    expectedmatch = null;
+    addThis();
 
     string = 'bla-';
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string, string);  
+    expectedmatch = Array(string, string);
     addThis();
 
-    string = 'bla-bla';  // hyphen missing at end - 
+    string = 'bla-bla';  // hyphen missing at end -
     actualmatch = string.match(pattern);
     expectedmatch = null;  //because string doesn't match at end
-    addThis(); 
+    addThis();
 
     string = 'bla-bla-';
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string, 'bla-');  
-    addThis(); 
+    expectedmatch = Array(string, 'bla-');
+    addThis();
 
 
 pattern = /^(\S+)+(A+)$/;
     string = 'asdldflkjAAA';
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string, 'asdldflkjAA', 'A');  
-    addThis(); 
+    expectedmatch = Array(string, 'asdldflkjAA', 'A');
+    addThis();
 
     string = 'asdldflkj AAA'; // space in middle
     actualmatch = string.match(pattern);
     expectedmatch = null;  //because of the space
-    addThis(); 
+    addThis();
 
 
 pattern = /^(\S+)+(\d+)$/;
     string = 'asdldflkj122211';
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string, 'asdldflkj12221', '1');  
-    addThis(); 
+    expectedmatch = Array(string, 'asdldflkj12221', '1');
+    addThis();
 
-    string = 'asdldflkj1111111aaa1'; 
+    string = 'asdldflkj1111111aaa1';
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string, 'asdldflkj1111111aaa', '1'); 
+    expectedmatch = Array(string, 'asdldflkj1111111aaa', '1');
     addThis();
 
 
 /* 
- * This one comes from Stephen Ostermiller. 
+ * This one comes from Stephen Ostermiller.
  * See http://bugzilla.mozilla.org/show_bug.cgi?id=69989
  */
 pattern = /^[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)+$/;
     string = 'some.host.tld';
     actualmatch = string.match(pattern);
-    expectedmatch = Array(string, '.tld', '.'); 
-    addThis(); 
+    expectedmatch = Array(string, '.tld', '.');
+    addThis();
 
 
 
@@ -144,21 +146,21 @@ test();
 
 
 
-function addThis() 
+function addThis()
 {
-  i++;
   patterns[i] = pattern;
   strings[i] = string;
   actualmatches[i] = actualmatch;
   expectedmatches[i] = expectedmatch;
+  i++;
 }
 
 
-function test() 
+function test()
 {
-  enterFunc ('test'); 
+  enterFunc ('test');
   printBugNumber (bug);
-  printStatus (summary);  
+  printStatus (summary);
   testRegExp(patterns, strings, actualmatches, expectedmatches);
   exitFunc ('test');
 }
