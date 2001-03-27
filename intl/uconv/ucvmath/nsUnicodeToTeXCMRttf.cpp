cc0b33f08bfa2337116a37f140aa33e741c12803
rbs@maths.uq.edu.au <rbs@maths.uq.edu.au>
2001-03-27 12:41:48 +0000

    Follow up to bug 72161 - sync:ing ucvmath with the painstaking datasets needed for the MathFont Property Files

---
+++ intl/uconv/ucvmath/nsUnicodeToTeXCMRttf.cpp
@@ -0,0 +1,66 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ *   Roger B. Sidje <rbs@maths.uq.edu.au>
+ *
+ * This Original Code has been modified by Roger B. Sidje.
+ * Modifications made by Roger B. Sidje described herein are
+ * Copyright (C) 2001 The University Of Queensland.
+ *
+ * Modifications to Mozilla code or documentation
+ * identified per MPL Section 3.3
+ *
+ * Date            Modified by     Description of modification
+ * 21/March/2001   RBS.            Support for Mathematical fonts.
+ */
+
+#include "nsUnicodeToTeXCMRttf.h"
+
+//----------------------------------------------------------------------
+// Global functions and data [declaration]
+
+static PRUint16 g_ufMappingTable[] = {
+#include "texcmr-ttf.uf"
+};
+
+static PRInt16 g_ufShiftTable[] =  {
+  0, u1ByteCharset ,
+  ShiftCell(0,0,0,0,0,0,0,0)
+};
+
+//----------------------------------------------------------------------
+// Class nsUnicodeToTeXCMRttf [implementation]
+
+nsUnicodeToTeXCMRttf::nsUnicodeToTeXCMRttf() 
+: nsTableEncoderSupport((uShiftTable*) &g_ufShiftTable, 
+                        (uMappingTable*) &g_ufMappingTable)
+{
+}
+
+//----------------------------------------------------------------------
+// Subclassing of nsTableEncoderSupport class [implementation]
+
+NS_IMETHODIMP nsUnicodeToTeXCMRttf::GetMaxLength(const PRUnichar * aSrc, 
+                                              PRInt32 aSrcLength,
+                                              PRInt32 * aDestLength)
+{
+  *aDestLength = aSrcLength;
+  return NS_OK_UENC_EXACTLENGTH;
+}
