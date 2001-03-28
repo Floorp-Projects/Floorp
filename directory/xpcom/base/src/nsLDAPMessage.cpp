f50b8ea04a507059577394b6d0bcfb4d5ac1ec5b
dmose%mozilla.org <dmose%mozilla.org>
2001-03-28 23:17:44 +0000

    Workaround bug in Solaris WorkShop 6 Update 1 that crashes the compiler by ifdefing out the offending code on that compiler (bug 73834).  r=rich.burridge@sun.com, sr=sfraser@netscape.com

--- directory/xpcom/base/src/nsLDAPMessage.cpp
+++ directory/xpcom/base/src/nsLDAPMessage.cpp
@@ -58,10 +58,16 @@ nsLDAPMessage::nsLDAPMessage()
 nsLDAPMessage::~nsLDAPMessage(void)
 {
 
-
     if (mMsgHandle) {
         int rc = ldap_msgfree(mMsgHandle);
 
+// for some reason, the following switch statement is causing Sun's Forte 6
+// Update 1 (CC -v sez "CC: Sun WorkShop 6 update 1 C++ 5.2 Patch 109508-01
+// 2001/01/31") to crash during the compile.  Since all it does is log 
+// something which is pretty unlikely to happen anyway, I'm gonna ifdef this 
+// code out on that compiler.
+// 
+#if __SUNPRO_CC != 0x520  
         switch(rc) {
         case LDAP_RES_BIND:
         case LDAP_RES_SEARCH_ENTRY:
@@ -91,6 +97,7 @@ nsLDAPMessage::~nsLDAPMessage(void)
                     "failed: %s\n", ldap_err2string(rc)));
             break;
         }
+#endif /* __SUNPRO_CC */
     }
 
     if (mMatchedDn) {
