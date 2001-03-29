8052c8e89b0f0abefabbce3a5f5609b2298c5ee2
bnesse@netscape.com <bnesse@netscape.com>
2001-03-29 00:46:49 +0000

    Initial versions based on feedback of initial prefservice and prefbranch feedback. [Not Part of the Build]

---
+++ modules/libpref/src/nsPrefsFactory.cpp
@@ -0,0 +1,52 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ * Brian Nesse <bnesse@netscape.com>
+ */
+
+#include "nsIGenericFactory.h"
+#include "nsPrefService.h"
+#include "nsIPref.h"
+
+// remove this when nsPref goes away
+extern "C" nsresult nsPrefConstructor(nsISupports *aOuter, REFNSIID aIID, void **aResult);
+
+
+NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsPrefService, Init)
+
+// The list of components we register
+static nsModuleComponentInfo components[] = 
+{
+  {
+    NS_PREFSERVICE_CLASSNAME, 
+    NS_PREFSERVICE_CID,
+    NS_PREFSERVICE_CONTRACTID, 
+    nsPrefServiceConstructor
+  },
+
+  { // remove this when nsPref goes away
+    NS_PREF_CLASSNAME, 
+    NS_PREF_CID,
+    NS_PREF_CONTRACTID, 
+    nsPrefConstructor
+  },
+};
+
+NS_IMPL_NSGETMODULE("nsPrefModule", components);
