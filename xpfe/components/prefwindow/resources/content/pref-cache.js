e5cf20f80ec510495a7b0327cc30c067d97d7052
disttsc@bart.nl <disttsc@bart.nl>
2001-04-01 05:02:07 +0000

    Removing prefutilities.js, moving the code in it to where it belongs. bug=73355, r=mao, sr=ben

--- xpfe/components/prefwindow/resources/content/pref-cache.js
+++ xpfe/components/prefwindow/resources/content/pref-cache.js
@@ -1,23 +1,43 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ */
+
+const nsIFilePicker          = Components.interfaces.nsIFilePicker;
+const nsINetDataCacheManager = Components.interfaces.nsINetDataCacheManager;
+
+const FILEPICKER_CONTRACTID  = "@mozilla.org/filepicker;1";
+
 function prefCacheSelectFolder()
 {
+  var fp = Components.classes[FILEPICKER_CONTRACTID]
+                     .createInstance(nsIFilePicker);
+
   var prefutilitiesBundle = document.getElementById("bundle_prefutilities");
-  var folderField = document.getElementById("browserCacheDirectory");
-  var file = getFileOrFolderSpec(prefutilitiesBundle.getString("cachefolder"),
-                                 true);
-  if ( file && file != -1 )
-    folderField.value = file.unicodePath;
+  var title = prefutilitiesBundle.getString("cachefolder");
+  fp.init(window, title, nsIFilePicker.modeGetFolder);
+  fp.appendFilters(nsIFilePicker.filterAll);
+
+  var ret = fp.show();
+  if (ret == nsIFilePicker.returnOK) {
+    var folderField = document.getElementById("browserCacheDirectory");
+    folderField.value = fp.file.unicodePath;
+  }
+}
+
+function prefClearCache(aType)
+{
+  var cacheService = Components.classes['@mozilla.org/network/cache;1?name=manager']
+                               .getService(nsINetDataCacheManager);
+  cacheService.clear(aType);
 }
 
 function prefClearMemCache()
 {
-  var cache = Components.classes['@mozilla.org/network/cache;1?name=manager'].getService();
-  var cacheService = cache.QueryInterface( Components.interfaces.nsINetDataCacheManager);
-  cacheService.clear( 2 );
+  prefClearCache(nsINetDataCacheManager.MEM_CACHE);
 }
 
 function prefClearDiskCache()
 {
-  var cache = Components.classes['@mozilla.org/network/cache;1?name=manager'].getService();
-  var cacheService = cache.QueryInterface( Components.interfaces.nsINetDataCacheManager);
-  cacheService.clear( 12 );
+  prefClearCache(nsINetDataCacheManager.FILE_CACHE |
+                 nsINetDataCacheManager.FLAT_CACHE);
 }
