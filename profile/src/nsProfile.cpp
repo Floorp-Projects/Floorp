480d442d90fc7e6139d9769e3f4510946de19efd
danm@netscape.com <danm@netscape.com>
2001-04-09 02:25:52 +0000

    oops

--- profile/src/nsProfile.cpp
+++ profile/src/nsProfile.cpp
@@ -114,8 +114,10 @@
 // we want everyone to have the debugging info to the console for now
 // to help track down profile manager problems
 // when we ship, we'll turn this off
+#define DEBUG_profile 1
 #undef DEBUG_profile_verbose
 #ifdef DEBUG_seth
+#define DEBUG_profile_verbose 1
 #endif
 
 // ProfileAccess varaible (gProfileDataAccess) to access registry operations
