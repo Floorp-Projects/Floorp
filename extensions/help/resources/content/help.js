a3820544984c653437d5a87731813b80af65e108
cotter%netscape.com <cotter%netscape.com>
2001-04-09 18:09:19 +0000

    Added first batch of help targets for PSM 2.0

--- extensions/help/resources/content/help.js
+++ extensions/help/resources/content/help.js
@@ -7,7 +7,36 @@ var key = {
  "?im":    "chrome://help/locale/im_help.html",
  "?sec":   "chrome://help/locale/security_help.html",
  "?cust":  "chrome://help/locale/customize_help.html",
- "?comp":  "chrome://help/locale/composer_help.html"
+ "?comp":  "chrome://help/locale/composer_help.html",
+
+ "?my_certs":  "chrome://help/locale/certs_help.html#My_Certificates",
+ "?cert_backup_pwd":  "chrome://help/locale/certs_help.html#Choose_a_Certificate_Backup_Password",
+ "?delete_my_certs":  "chrome://help/locale/certs_help.html#Delete_My_Certificate",
+ "?change_pwd":  "chrome://help/locale/passwords_help.html#Change_Master_Password",
+ "?web_certs":  "chrome://help/locale/certs_help.html#Web_Site_Certificates",
+ "?edit_web_certs":  "chrome://help/locale/certs_help.html#Edit_Web_Site_Certificate_Settings",
+ "?delete_web_certs":  "chrome://help/locale/certs_help.html#Delete_Web_Site_Certificate",
+ "?ca_certs":  "chrome://help/locale/certs_help.html#CA_Certificates",
+ "?edit_ca_certs":  "chrome://help/locale/certs_help.html#Edit_CA_Certificate_Settings",
+ "?delete_ca_certs":  "chrome://help/locale/certs_help.html#Delete_CA_Certificate",
+ "?sec_devices":  "chrome://help/locale/certs_help.html#Security_Devices",
+
+ "?cert_details":  "chrome://help/locale/cert_dialog_help.html#Certificate_Details",
+ "?which_token":  "chrome://help/locale/cert_dialog_help.html#Choose_Security_Device",
+ "?priv_key_copy":  "chrome://help/locale/cert_dialog_help.html#Encryption_Key_Copy",
+ "?backup_your_cert":  "chrome://help/locale/cert_dialog_help.html#Certificate_Backup",
+ "?which_cert":  "chrome://help/locale/cert_dialog_help.html#User_Identification_Request",
+ "?no_cert":  "chrome://help/locale/cert_dialog_help.html#User_Identification_Request",
+ "?new_ca":  "chrome://help/locale/cert_dialog_help.html#New_Certificate_Authority",
+ "?new_web_cert":  "chrome://help/locale/cert_dialog_help.html#New_Web_Site_Certificate",
+ "?exp_web_cert":  "chrome://help/locale/cert_dialog_help.html#Expired_Web_Site_Certificate",
+ "?not_yet_web_cert":  "chrome://help/locale/cert_dialog_help.html#Web_Site_Certificate_Not_Yet_Valid",
+ "?bad_name_web_cert":  "chrome://help/locale/cert_dialog_help.html#Unexpected_Certificate_Name",
+
+ "?sec_gen":  "chrome://help/locale/privsec_help.html#privsec_help_first",
+ "?ssl_prefs":  "chrome://help/locale/ssl_help.html#ssl_first",
+ "?validation_prefs":  "chrome://help/locale/ssl_help.html#ssl_first",
+ "?passwords_prefs":  "chrome://help/locale/ssl_help.html#ssl_first"
 }
 
 
