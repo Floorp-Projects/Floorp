20db5d04683c00ece915c8dd05b0af562188e5cc
waterson%netscape.com <waterson%netscape.com>
2001-04-13 03:18:01 +0000

    Fix bustage.

--- uriloader/base/nsDocLoader.cpp
+++ uriloader/base/nsDocLoader.cpp
@@ -70,7 +70,7 @@ void GetURIStringFromRequest(nsIRequest* request, nsXPIDLCString &aStr)
     request->GetName(getter_Copies(name));
 
     if (name)
-        *getter_Copies(aStr) = ToNewUTF8String(name);
+        *getter_Copies(aStr) = ToNewUTF8String(name.get());
     else
         *getter_Shares(aStr) = "???";
 }
