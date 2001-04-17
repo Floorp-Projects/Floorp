ee008beed72c67e0acf2737d7f21a7eb038ac3bc
pinkerton@netscape.com <pinkerton@netscape.com>
2001-04-17 04:35:56 +0000

    get rid of warning about non-virtual dtor with virtual methods.

--- widget/src/mac/nsWidgetFactory.cpp
+++ widget/src/mac/nsWidgetFactory.cpp
@@ -136,7 +136,7 @@ public:
     NS_IMETHOD LockFactory(PRBool aLock);
 
     nsWidgetFactory(const nsCID &aClass);
-    ~nsWidgetFactory();
+    virtual ~nsWidgetFactory();
 private:
   nsCID mClassID;
 
