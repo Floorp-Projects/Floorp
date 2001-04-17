cd87be9612ed391bd7ef04bda38e1089dbc56c2b
joki@netscape.com <joki@netscape.com>
2001-04-17 09:53:37 +0000

    Fix for smoketest block 76304.  r:jst

--- content/events/src/nsEventListenerManager.cpp
+++ content/events/src/nsEventListenerManager.cpp
@@ -1051,6 +1051,9 @@ nsresult nsEventListenerManager::HandleEvent(nsIPresContext* aPresContext,
 {
   NS_ENSURE_ARG_POINTER(aEventStatus);
   nsresult ret = NS_OK;
+
+  mListenersRemoved = PR_FALSE;
+
   if (aFlags & NS_EVENT_FLAG_INIT) {
     aFlags |= (NS_EVENT_FLAG_BUBBLE | NS_EVENT_FLAG_CAPTURE);
   }
@@ -2070,6 +2073,8 @@ nsresult nsEventListenerManager::HandleEvent(nsIPresContext* aPresContext,
     (aEvent->flags & NS_EVENT_FLAG_NO_DEFAULT)) {
     *aEventStatus = nsEventStatus_eConsumeNoDefault;
   }
+
+  mListenersRemoved = PR_FALSE;
 
   return NS_OK;
 }
