4a078cc78a14888b6f806ccc613e4b3a1ae3bd13
joki@netscape.com <joki@netscape.com>
2001-04-23 05:33:07 +0000

    Fix for bug 71283, null pointer check for event listeners passed into AddEventListener.  a:choffmann

--- content/events/src/nsEventListenerManager.cpp
+++ content/events/src/nsEventListenerManager.cpp
@@ -358,6 +358,8 @@ nsresult nsEventListenerManager::AddEventListener(nsIDOMEventListener *aListener
                                                   nsHashKey* aKey,
                                                   PRInt32 aFlags)
 {
+  NS_ENSURE_TRUE(aListener, NS_ERROR_FAILURE);
+
   nsVoidArray* listeners = GetListenersByType(aType, aKey, PR_TRUE);
 
   //We asked the GetListenersByType to create the array if it had to.  If it didn't
