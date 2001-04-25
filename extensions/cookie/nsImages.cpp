10bac9098d650c3ccaa33670a4f76c234023a6bd
shaver@mozilla.org <shaver@mozilla.org>
2001-04-25 16:24:24 +0000

    backing out unauthorized checkin to closed tree, as per Asa

--- extensions/cookie/nsImages.cpp
+++ extensions/cookie/nsImages.cpp
@@ -168,12 +168,8 @@ IMAGE_CheckForPermission
   /* use common routine to make decision */
   PRUnichar * message = CKutil_Localize(NS_LITERAL_STRING("PermissionToAcceptImage").get());
   PRUnichar * new_string = nsTextFormatter::smprintf(message, hostname ? hostname : "");
-  if (NS_SUCCEEDED(PERMISSION_Read())) {
-    *permission = Permission_Check(0, hostname, IMAGEPERMISSION,
-                                   image_GetWarningPref(), new_string);
-  } else {
-    *permission = PR_TRUE;
-  }
+  *permission = Permission_Check(0, hostname, IMAGEPERMISSION,
+                                image_GetWarningPref(), new_string);
   PR_FREEIF(new_string);
   Recycle(message);
   return NS_OK;
