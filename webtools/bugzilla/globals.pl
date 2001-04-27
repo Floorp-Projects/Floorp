4f193d1f182a94b41d20ea3eb4476026159c582b
justdave%syndicomm.com <justdave%syndicomm.com>
2001-04-27 19:36:20 +0000

    rolling version number to 2.13 for development

--- webtools/bugzilla/globals.pl
+++ webtools/bugzilla/globals.pl
@@ -70,7 +70,7 @@ use Date::Parse;               # For str2time().
 use RelationSet;
 
 # Contains the version string for the current running Bugzilla.
-$::param{'version'} = '2.12';
+$::param{'version'} = '2.13';
 
 $::dontchange = "--do_not_change--";
 $::chooseone = "--Choose_one:--";
