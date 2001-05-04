2b2b6f9f6940bb57fa3f1800827016d5256387c2
rginda%netscape.com <rginda%netscape.com>
2001-05-04 22:25:37 +0000

    - not built - wiring for break and step commands

--- extensions/venkman/resources/content/venkman-commands.js
+++ extensions/venkman/resources/content/venkman-commands.js
@@ -43,6 +43,7 @@ function initCommands(commandObject)
         console._commands.add (name, func, usage, help);
     }
 
+    add (CMD_BREAK,  "onInputBreak",     CMD_BREAK_PARAMS,  CMD_BREAK_HELP);
     add (CMD_CMDS,   "onInputCommands",  CMD_CMDS_PARAMS,   CMD_CMDS_HELP);
     add (CMD_CONT,   "onInputCont",      CMD_CONT_PARAMS,   CMD_CONT_HELP);
     add (CMD_EVAL,   "onInputEval",      CMD_EVAL_PARAMS,   CMD_EVAL_HELP);
@@ -52,6 +53,7 @@ function initCommands(commandObject)
     add (CMD_PROPS,  "onInputProps",     CMD_PROPS_PARAMS,  CMD_PROPS_HELP);
     add (CMD_PROPSD, "onInputPropsD",    CMD_PROPSD_PARAMS, CMD_PROPSD_HELP);
     add (CMD_SCOPE,  "onInputScope",     CMD_SCOPE_PARAMS,  CMD_SCOPE_HELP);
+    add (CMD_STEP,   "onInputStep",      CMD_STEP_PARAMS,   CMD_STEP_HELP);
     add (CMD_WHERE,  "onInputWhere",     CMD_WHERE_PARAMS,  CMD_WHERE_HELP);
     add (CMD_QUIT,   "onInputQuit",      CMD_QUIT_PARAMS,   CMD_QUIT_HELP);
     
