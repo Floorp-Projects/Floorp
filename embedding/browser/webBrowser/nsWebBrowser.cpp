de1e7c79afcf1e9c2faa74eaa3ec85777513276c
ccarlen%netscape.com <ccarlen%netscape.com>
2001-05-04 14:46:16 +0000

    Bug 72230 (fixing side affect of its fix) WebBrowser window was painting black as background on Mac. r=adamlock, sr=blizzard.

--- embedding/browser/webBrowser/nsWebBrowser.cpp
+++ embedding/browser/webBrowser/nsWebBrowser.cpp
@@ -788,10 +788,6 @@ NS_IMETHODIMP nsWebBrowser::Create()
    info.mColor = &mBackgroundColor;
    mDC->GetSystemAttribute(eSystemAttr_Color_WindowBackground, &info);
 
-   // set the foreground color of our rendering context so we don't
-   // have to do it later.
-   mRC->SetColor(mBackgroundColor);
-
    nsCOMPtr<nsIDocShell> docShell(do_CreateInstance(kWebShellCID));
    NS_ENSURE_SUCCESS(SetDocShell(docShell), NS_ERROR_FAILURE);
 
@@ -1309,6 +1305,7 @@ NS_IMETHODIMP nsWebBrowser::EnsureDocShellTreeOwner()
 
 NS_IMETHODIMP nsWebBrowser::FillBackground(const nsRect &aRect)
 {
+    mRC->SetColor(mBackgroundColor);
     mRC->FillRect(aRect);
     return NS_OK;
 }
