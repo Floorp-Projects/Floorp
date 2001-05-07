a83816927a135c21f6ef887dd294c948a4bb192c
bienvenu%netscape.com <bienvenu%netscape.com>
2001-05-07 23:30:36 +0000

    fix problem where synchronize for offline didn't bring up offline dialog r=mohanb, sr=sspitzer  79183

--- mailnews/base/resources/content/commandglue.js
+++ mailnews/base/resources/content/commandglue.js
@@ -961,20 +961,6 @@ function MsgToggleWorkOffline()
   }
 
 
-function  MsgSynchronizeOffline()
-  {
-  // this is just code for my testing purposes, and doesn't have the proper UI, as in the offline spec.
-  // we could use the account manager, or add a new service, the offline manager.
-  // what the heck, might as well bite the bullet and add a new service.
-  var offlineManager = Components.classes["@mozilla.org/messenger/offline-manager;1"].getService(Components.interfaces.nsIMsgOfflineManager);
-  if (offlineManager) {
-    // should use progress window here. params are:
-    // download news, download mail, send unsent messages, go offline when done, msg window
-    messenger.SetWindow(window, msgWindow);
-    offlineManager.synchronizeForOffline(false, true, true, false, msgWindow);
-  }
-}
-
 function MsgSettingsOffline()
 {
 }
