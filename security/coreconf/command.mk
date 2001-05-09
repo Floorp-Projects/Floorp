a22f48674337814c3606675ccda43e1eb473e48b
wtc@netscape.com <wtc@netscape.com>
2001-05-09 00:13:57 +0000

    Bugzilla bug #77788: backed out the fix because it breaks mozilla/security/nss/lib/fortcrypt/swfort/pkcs11.  The manifest.mn file in that directory defines CFLAGS, before command.mk is included by the Makefile.

--- security/coreconf/command.mk
+++ security/coreconf/command.mk
@@ -42,7 +42,7 @@ CCF           = $(CC) $(CFLAGS)
 LINK_DLL      = $(LINK) $(OS_DLLFLAGS) $(DLLFLAGS)
 LINK_EXE      = $(LINK) $(OS_LFLAGS) $(LFLAGS)
 NFSPWD        = $(NSINSTALL_DIR)/nfspwd
-CFLAGS        = $(OPTIMIZER) $(OS_CFLAGS) $(XP_DEFINE) $(DEFINES) $(INCLUDES) \
+CFLAGS       += $(OPTIMIZER) $(OS_CFLAGS) $(XP_DEFINE) $(DEFINES) $(INCLUDES) \
 		$(XCFLAGS)
 RANLIB        = echo
 TAR           = /bin/tar
