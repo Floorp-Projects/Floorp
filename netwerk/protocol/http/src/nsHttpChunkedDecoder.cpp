9dd5b9c92eb63ba60c3eab77633cf3bf444128a9
darin@netscape.com <darin@netscape.com>
2001-05-14 10:15:06 +0000

    Fixes blocker bug 80457: r=bbaetz,dr

--- netwerk/protocol/http/src/nsHttpChunkedDecoder.cpp
+++ netwerk/protocol/http/src/nsHttpChunkedDecoder.cpp
@@ -73,56 +73,46 @@ nsHttpChunkedDecoder::HandleChunkedContent(char *buf,
 nsresult
 nsHttpChunkedDecoder::ParseChunkRemaining(char *buf,
                                           PRUint32 count,
-                                          PRUint32 *countRead)
+                                          PRUint32 *bytesConsumed)
 {
     NS_PRECONDITION(mChunkRemaining == 0, "chunk remaining should be zero");
+    NS_PRECONDITION(count, "unexpected");
 
-    char *p;
-
-    // we may have a partial line from the last time we were called
-    if (mLineBuf.IsEmpty()) {
-        p = PL_strstr(buf, "\r\n");
-        if (p) {
-            *p = 0;
+    *bytesConsumed = 0;
+    
+    char *p = PL_strnchr(buf, '\n', count);
+    if (p) {
+        *p = 0;
+        *bytesConsumed = p - buf + 1;
+        if (mLineBuf.IsEmpty())
             p = buf;
-        }
-    }
-    else {
-        p = PL_strstr(buf, "\r\n");
-        // it would be really sad if this ever returned null, b/c it
-        // would mean we were reading really small chunks from the net.
-        if (p) {
-            *p = 0;
+        else {
             // append to the line buf and use that as the buf to parse.
             mLineBuf.Append(buf);
             p = (char *) mLineBuf.get();
         }
     }
-
-    if (!p) {
+    else {
         mLineBuf.Append(buf, count);
-        *countRead = count;
+        *bytesConsumed = count;
         return NS_OK;
     }
 
-    // indicate that the entire line has been consumed... the \r has been
-    // nulled out, so we have to add 2 to include the \r\n in the count read.
-    *countRead = PL_strlen(buf) + 2;
+    if (*p && *p != '\r') {
+        buf = p;
 
-    buf = p;
-    if ((p = PL_strchr(buf, ';')) != nsnull) {
         // ignore any chunk-extensions
-        *p = 0;
-    }
+        if ((p = PL_strchr(buf, ';')) != nsnull)
+            *p = 0;
 
-    if (!sscanf(buf, "%x", &mChunkRemaining)) {
-        LOG(("sscanf failed parsing hex on string [%s]\n", buf));
-        return NS_ERROR_UNEXPECTED;
-    }
+        if (!sscanf(buf, "%x", &mChunkRemaining)) {
+            LOG(("sscanf failed parsing hex on string [%s]\n", buf));
+            return NS_ERROR_UNEXPECTED;
+        }
 
-    if ((mChunkRemaining == 0) && (*buf != 0)) {
         // XXX need to add code to consume "trailer" headers
-        mReachedEOF = PR_TRUE;
+        if ((mChunkRemaining == 0) && (*buf != 0))
+            mReachedEOF = PR_TRUE;
     }
 
     // clear the line buffer if it is not already empty
