868879270103a25eb3e30c543ff3ba48cd58dcac
dveditz%netscape.com <dveditz%netscape.com>
2001-05-14 12:38:15 +0000

    fix Mac build problem for bug 7022; thanks to sgehani@netscape.com for tracking down

--- xpfe/appshell/public/nsBuildID.h
+++
@@ -1,8 +0,0 @@
-/* Build ID file.
-*
-* If building MOZILLLA_OFFICIAL (release build) NS_BUILD_ID will be updated
-* to a current build id. This will be used to determine if we need to 
-* re-register components.
-*
-*/
-#define NS_BUILD_ID 0000000000
