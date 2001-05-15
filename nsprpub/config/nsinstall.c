17680c472cb46b4c67615e22152801e42720d31e
wtc%netscape.com <wtc%netscape.com>
2001-05-15 22:57:39 +0000

    Verified that the autoconf build system is equivalent to the classic build system on AIX, HP-UX, Linux, and Solaris.  The 'release' makefile target still doesn't quite work. Modified Files: 	configure configure.in config/Makefile.in 	config/autoconf.mk.in config/config.mk config/nsinstall.c 	config/rules.mk lib/ds/Makefile.in lib/libc/src/Makefile.in 	pr/include/md/Makefile.in pr/src/misc/Makefile.in

--- nsprpub/config/nsinstall.c
+++ nsprpub/config/nsinstall.c
@@ -57,17 +57,6 @@
 
 #include "pathsub.h"
 
-#ifndef HAVE_LCHOWN
-#define HAVE_LCHOWN
-#endif
-
-#if defined(AIX) || defined(BSDI) || defined(HPUX) || defined(LINUX) \
-    || defined(SUNOS4) || defined(SCO) || defined(UNIXWARE) \
-    || defined(RHAPSODY) || defined(NEXTSTEP) || defined(QNX) \
-    || defined(BEOS) || defined(VMS)
-#undef HAVE_LCHOWN
-#endif
-
 #define HAVE_FCHMOD
 
 #if defined(BEOS)
