c31db345adaa1db0cf6f91773fa0f9c18837f10b
beard%netscape.com <beard%netscape.com>
2001-05-17 00:32:20 +0000

    [not part of build] experimental mechanism to load Java classes from same location as the referring document.

---
+++ modules/oji/src/ProxyClassLoader.cpp
@@ -0,0 +1,151 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):  Patrick Beard <beard@netscape.com>
+ */
+
+#include "ProxyClassLoader.h"
+
+#include "jsapi.h"
+#include "jsjava.h"
+#include "prprf.h"
+
+#include "nsIServiceManager.h"
+#include "nsIJSContextStack.h"
+#include "nsIPrincipal.h"
+#include "nsICodebasePrincipal.h"
+#include "nsIScriptContext.h"
+#include "nsIScriptGlobalObject.h"
+#include "nsIScriptObjectPrincipal.h"
+#include "nsNetUtil.h"
+#include "nsIByteBuffer.h"
+
+static nsresult getScriptCodebase(JSContext* cx, nsIURI* *result)
+{
+    nsIScriptContext* scriptContext = NS_REINTERPRET_CAST(nsIScriptContext*, JS_GetContextPrivate(cx));
+    if (scriptContext) {
+        nsCOMPtr<nsIScriptGlobalObject> scriptGlobal = dont_AddRef(scriptContext->GetGlobalObject());
+        nsCOMPtr<nsIScriptObjectPrincipal> scriptObjectPrincipal = do_QueryInterface(scriptGlobal);
+        if (scriptObjectPrincipal) {
+            nsCOMPtr<nsIPrincipal> principal;
+            scriptObjectPrincipal->GetPrincipal(getter_AddRefs(principal));
+            if (principal) {
+                nsCOMPtr<nsICodebasePrincipal> codebasePrincipal = do_QueryInterface(principal);
+                if (codebasePrincipal)
+                    return codebasePrincipal->GetURI(result);
+            }
+        }
+    }
+    return NS_ERROR_FAILURE;
+}
+
+static nsresult getScriptClassLoader(JNIEnv* env, jobject* classloader)
+{
+    // get the current JSContext from the context stack service.
+    nsresult rv;
+    nsCOMPtr<nsIJSContextStack> contexts = do_GetService("@mozilla.org/js/xpc/ContextStack;1", &rv);
+    if (NS_FAILED(rv)) return rv;
+    JSContext* cx;
+    rv = contexts->Peek(&cx);
+    if (NS_FAILED(rv)) return rv;
+    
+    // lookup "window.navigator.javaclasses", if it exists, this is the class loader bound
+    // to this page.
+    JSObject* window = JS_GetGlobalObject(cx);
+    if (!window) return NS_ERROR_FAILURE;
+
+    jsval navigator;
+    if (!JS_GetProperty(cx, window, "navigator", &navigator)) return NS_ERROR_FAILURE;
+    
+    jsval javaclasses;
+    if (JS_GetProperty(cx, JSVAL_TO_OBJECT(navigator), "javaclasses", &javaclasses)) {
+        // unwrap this, the way LiveConnect does it.
+        if (JSJ_ConvertJSValueToJavaObject(cx, javaclasses, classloader))
+            return NS_OK;
+    }
+
+    nsCOMPtr<nsIURI> codebase;
+    rv = getScriptCodebase(cx, getter_AddRefs(codebase));
+    if (NS_FAILED(rv)) return rv;
+    
+    // create a netscape.oji.ProxyClassLoader instance.
+    nsXPIDLCString spec;
+    rv = codebase->GetSpec(getter_Copies(spec));
+    if (NS_FAILED(rv)) return rv;
+    
+    jstring jspec = env->NewStringUTF(spec);
+    if (!spec) {
+        env->ExceptionClear();
+        return NS_ERROR_FAILURE;
+    }
+    jclass netscape_oji_ProxyClassLoader = env->FindClass("netscape/oji/ProxyClassLoader");
+    if (!netscape_oji_ProxyClassLoader) {
+        env->ExceptionClear();
+        return NS_ERROR_FAILURE;
+    }
+    jmethodID constructorID = env->GetMethodID(netscape_oji_ProxyClassLoader, "<init>", "(Ljava/lang/String;)V");
+    if (!constructorID) {
+        env->ExceptionClear();
+        return NS_ERROR_FAILURE;
+    }
+    *classloader = env->NewObject(netscape_oji_ProxyClassLoader, constructorID, jspec);
+    if (!*classloader) {
+        env->ExceptionClear();
+        return NS_ERROR_FAILURE;
+    }
+
+    env->DeleteLocalRef(jspec);
+    env->DeleteLocalRef(netscape_oji_ProxyClassLoader);
+    
+    // now, cache the class loader in "window.navigator.javaclasses"
+    if (JSJ_ConvertJavaObjectToJSValue(cx, *classloader, &javaclasses)) {
+        JS_SetProperty(cx, JSVAL_TO_OBJECT(navigator), "javaclasses", &javaclasses);
+    }
+    
+    return NS_OK;
+}
+
+/**
+ * Obtain the URL of the document base of the current JSContext. This will
+ * be used as the default location to download classes from.
+ */
+jclass ProxyFindClass(JNIEnv* env, const char* name)
+{
+    do {
+        nsresult rv;
+        jobject classloader;
+        rv = getScriptClassLoader(env, &classloader);
+        if (NS_FAILED(rv)) break;
+
+        jclass netscape_oji_ProxyClassLoader = env->GetObjectClass(classloader);
+        jmethodID loadClassID = env->GetMethodID(netscape_oji_ProxyClassLoader, "loadClass",
+                                                 "(Ljava/lang/String;Z)Ljava/lang/Class;");
+        if (!loadClassID) {
+            env->ExceptionClear();
+            break;
+        }
+        env->DeleteLocalRef(netscape_oji_ProxyClassLoader);
+        jstring jname = env->NewStringUTF(name);
+        jvalue jargs[2]; jargs[0].l = jname, jargs[1].z = JNI_TRUE;
+        jclass c = (jclass) env->CallObjectMethodA(classloader, loadClassID, jargs);
+        env->DeleteLocalRef(jname);
+        return c;
+    } while (0);
+    return 0;
+}
