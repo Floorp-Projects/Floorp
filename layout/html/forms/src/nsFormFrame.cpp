ea686d7396d7f7a8cf33c92e2dc71579ffc5737e
karnaze%netscape.com <karnaze%netscape.com>
2001-05-17 23:15:22 +0000

    bug 62811 - ensure that <form>s are always display:block, sr=attinasi, r=pollmann

--- layout/html/forms/src/nsFormFrame.cpp
+++ layout/html/forms/src/nsFormFrame.cpp
@@ -50,6 +50,7 @@
 #include "nsIPresShell.h"
 #include "nsIPresContext.h"
 #include "nsIStyleContext.h"
+#include "nsIMutableStyleContext.h"
 #include "nsCSSRendering.h"
 #include "nsHTMLIIDs.h"
 #include "nsDebug.h"
@@ -164,6 +165,16 @@ nsFormFrame::~nsFormFrame()
     fcFrame->SetFormFrame(nsnull);
     mFormControls.RemoveElement(fcFrame);
   }
+}
+
+NS_METHOD nsFormFrame::DidSetStyleContext(nsIPresContext* aPresContext)
+{
+  // we can't yet support anything but display:block on a form, 
+  // so make sure that the display is block
+  nsMutableStyleDisplay displayData(mStyleContext);
+  displayData->mDisplay = NS_STYLE_DISPLAY_BLOCK;
+
+  return NS_OK;
 }
 
 PRBool 
