d7ab9eaafa534a62cc7b392444b2933a5b7e88d7
bstell@netscape.com <bstell@netscape.com>
2001-05-18 21:38:53 +0000

    bug 78273, r=ftang/jbetak, sr=blizzard for the embedding API add "get font list". Used to fill the font pref panel

---
+++ gfx/src/nsFontList.h
@@ -0,0 +1,55 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Mozilla Communicator client code, 
+ * released March 31, 1998. 
+ *
+ * The Initial Developer of the Original Code is Netscape Communications 
+ * Corporation.  Portions created by Netscape are
+ * Copyright (C) 2001 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ *     Brian Stell <bstell@netscape.com>
+ */
+
+/*
+ * Implementation of nsIFontList
+ */
+
+#ifndef _nsFontList_H_
+#define _nsFontList_H_
+
+#include "nsIFontList.h"
+
+#define NS_FONTLIST_CONTRACTID "@mozilla.org/gfx/fontlist;1"
+
+
+class nsFontList : public nsIFontList
+{
+public:
+  NS_DEFINE_STATIC_CID_ACCESSOR(NS_IFONTLIST_IID)
+    
+  nsFontList();
+  virtual ~nsFontList();
+
+  // nsISupports
+  NS_DECL_ISUPPORTS
+
+  // nsIFontList
+  NS_DECL_NSIFONTLIST
+
+protected:
+
+};
+
+#endif /* _nsFontList_H_ */
