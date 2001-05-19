b85bd3a44b28ebdeed5605fa4a4cb87c88d63d0e
peterv@netscape.com <peterv@netscape.com>
2001-05-19 00:21:26 +0000

    Adding two new files. Not built. Part of fix for bug 7515 (dynamically adding LINK-ed or pi-ed style sheets does nothing).

---
+++ content/base/src/nsStyleLinkElement.cpp
@@ -0,0 +1,328 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * 
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *  
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *  
+ * The Original Code is Mozilla Communicator client code, released
+ * March 31, 1998.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation. Portions created by Netscape are
+ * Copyright (C) 1998-1999 Netscape Communications Corporation. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): 
+ */
+
+#include "nsStyleLinkElement.h"
+
+#include "nsIContent.h"
+#include "nsICSSLoader.h"
+#include "nsIDocument.h"
+#include "nsIDOMNode.h"
+#include "nsIDOMStyleSheet.h"
+#include "nsIHTMLContentContainer.h"
+#include "nsINameSpaceManager.h"
+#include "nsIParser.h"
+
+#include "nsNetUtil.h"
+#include "nsVoidArray.h"
+
+nsStyleLinkElement::nsStyleLinkElement() :
+    mDontLoadStyle(PR_FALSE),
+    mUpdatesEnabled(PR_TRUE),
+    mAppend(PR_FALSE)
+{
+}
+
+nsStyleLinkElement::~nsStyleLinkElement()
+{
+}
+
+NS_IMETHODIMP 
+nsStyleLinkElement::SetStyleSheet(nsIStyleSheet* aStyleSheet)
+{
+  mStyleSheet = aStyleSheet;
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP 
+nsStyleLinkElement::GetStyleSheet(nsIStyleSheet*& aStyleSheet)
+{
+  aStyleSheet = mStyleSheet;
+  NS_IF_ADDREF(aStyleSheet);
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP 
+nsStyleLinkElement::InitStyleLinkElement(nsIParser* aParser, PRBool aDontLoadStyle)
+{
+  mParser = aParser;
+  mDontLoadStyle = aDontLoadStyle;
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsStyleLinkElement::GetSheet(nsIDOMStyleSheet** aSheet)
+{
+  CallQueryInterface(mStyleSheet, aSheet);
+
+  // Always return NS_OK to avoid throwing JS exceptions if mStyleSheet 
+  // is not a nsIDOMStyleSheet
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsStyleLinkElement::SetEnableUpdates(PRBool aEnableUpdates)
+{
+  mUpdatesEnabled = aEnableUpdates;
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsStyleLinkElement::StyleSheetLoaded(nsICSSStyleSheet*aSheet, PRBool aNotify)
+{
+  mParser = nsnull;
+  return NS_OK;
+}
+
+void nsStyleLinkElement::ParseLinkTypes(const nsAReadableString& aTypes,
+                                        nsStringArray& aResult)
+{
+  nsReadingIterator<PRUnichar> current;
+  nsReadingIterator<PRUnichar> done;
+
+  aTypes.BeginReading(current);
+  aTypes.EndReading(done);
+  if (current == done)
+    return;
+
+  nsReadingIterator<PRUnichar> start;
+  PRBool inString = !nsCRT::IsAsciiSpace(*current);
+
+  aTypes.BeginReading(start);
+  while (current != done) {
+    if (nsCRT::IsAsciiSpace(*start)) {
+      if (inString) {
+        aResult.AppendString(Substring(start, current));
+        inString = false;
+      }
+    }
+    else {
+      if (!inString) {
+        start = current;
+        inString = true;
+      }
+    }
+    current++;
+  }
+  if (inString) {
+    aResult.AppendString(Substring(start, current));
+    inString = false;
+  }
+}
+
+void nsStyleLinkElement::SplitMimeType(const nsString& aValue, nsString& aType,
+                                       nsString& aParams)
+{
+  aType.Truncate();
+  aParams.Truncate();
+  PRInt32 semiIndex = aValue.FindChar(PRUnichar(';'));
+  if (-1 != semiIndex) {
+    aValue.Left(aType, semiIndex);
+    aValue.Right(aParams, (aValue.Length() - semiIndex) - 1);
+    aParams.StripWhitespace();
+  }
+  else {
+    aType = aValue;
+  }
+  aType.StripWhitespace();
+}
+
+#ifdef ALLOW_ASYNCH_STYLE_SHEETS
+const PRBool kBlockByDefault=PR_FALSE;
+#else
+const PRBool kBlockByDefault=PR_TRUE;
+#endif
+
+NS_IMETHODIMP
+nsStyleLinkElement::UpdateStyleSheet(PRBool aNotify, nsIDocument *aOldDocument, PRInt32 aDocIndex)
+{
+  if (!mUpdatesEnabled)
+    return NS_OK;
+
+  nsCOMPtr<nsIContent> thisContent;
+  QueryInterface(NS_GET_IID(nsIContent), getter_AddRefs(thisContent));
+
+  if (!thisContent)
+    return NS_ERROR_FAILURE;
+
+  nsCOMPtr<nsIDocument> doc;
+  thisContent->GetDocument(*getter_AddRefs(doc));
+
+  if (aNotify && mStyleSheet && !doc && aOldDocument) {
+    // We're removing the link element from the document, unload the stylesheet.
+    aOldDocument->RemoveStyleSheet(mStyleSheet);
+    mStyleSheet = nsnull;
+
+    return NS_OK;
+  }
+
+  if (!doc || mDontLoadStyle)
+    return NS_OK;
+
+  nsresult rv = NS_OK;
+
+  nsAutoString url, title, type, media;
+  PRBool isAlternate;
+
+  GetStyleSheetInfo(url, title, type, media, &isAlternate);
+
+  nsCOMPtr<nsIDOMStyleSheet> styleSheet(do_QueryInterface(mStyleSheet));
+
+  if (styleSheet) {
+    nsAutoString oldHref;
+
+    styleSheet->GetHref(oldHref);
+
+    if (oldHref.Equals(url))
+      return NS_OK;
+  }
+
+  if (mStyleSheet) {
+    doc->RemoveStyleSheet(mStyleSheet);
+    mStyleSheet = nsnull;
+  }
+
+  if (url.IsEmpty()) {
+    // if href is empty then just bail
+    return rv;
+  }
+
+  if (!type.EqualsIgnoreCase("text/css")) {
+    return NS_OK;
+  }
+
+  nsCOMPtr<nsIURI> uri;
+
+  rv = NS_NewURI(getter_AddRefs(uri), url);
+
+  if (NS_FAILED(rv)) {
+    return NS_OK; // The URL is bad, move along, don't propogate the error (for now)
+  }
+
+  nsCOMPtr<nsIHTMLContentContainer> htmlContainer(do_QueryInterface(doc));
+  nsCOMPtr<nsICSSLoader> loader;
+
+  if (htmlContainer) {
+    htmlContainer->GetCSSLoader(*getter_AddRefs(loader));
+  }
+
+  if (!loader) {
+    return NS_OK;
+  }
+
+  PRBool blockParser = kBlockByDefault;
+  if (isAlternate) {
+    blockParser = PR_FALSE;
+  }
+
+  /* NOTE: no longer honoring the important keyword to indicate blocking
+           as it is proprietary and unnecessary since all non-alternate 
+           will block the parser now  -mja
+    if (-1 != linkTypes.IndexOf("important")) {
+      blockParser = PR_TRUE;
+    }
+  */
+
+  // The way we determine the stylesheet's position in the cascade is by looking
+  // at the first of the previous siblings that are style linking elements, and
+  // insert just after that one. I'm not sure this is correct for every case for
+  // XML documents (it seems to be all right for HTML). The sink should disable
+  // this search by directly specifying a position.
+  PRInt32 insertionPoint;
+
+  if (aDocIndex > -1) {
+    insertionPoint = aDocIndex;
+  }
+  else {
+    // We're not getting them in document order, look for where to insert.
+    nsCOMPtr<nsIDOMNode> parentNode;
+    PRUint16 nodeType = 0;
+    nsCOMPtr<nsIDOMNode> thisNode(do_QueryInterface(thisContent));
+    nsCOMPtr<nsIStyleSheet> prevSheet;
+
+    thisNode->GetParentNode(getter_AddRefs(parentNode));
+    if (parentNode)
+      parentNode->GetNodeType(&nodeType);
+    if (nodeType == nsIDOMNode::DOCUMENT_NODE) {
+      nsCOMPtr<nsIDocument> parent(do_QueryInterface(parentNode));
+      if (parent) {
+        PRInt32 index;
+        nsCOMPtr<nsIContent> prevNode;
+        nsCOMPtr<nsIStyleSheetLinkingElement> previousLink;
+
+        parent->IndexOf(thisContent, index);
+        while (index-- > 0) {
+          parent->ChildAt(index, *getter_AddRefs(prevNode));
+          previousLink = do_QueryInterface(prevNode);
+          if (previousLink) {
+            previousLink->GetStyleSheet(*getter_AddRefs(prevSheet));
+            if (prevSheet)
+              // Found the first previous sibling that is a style linking element.
+              break;
+          }
+        }
+      }
+    }
+    else {
+      nsCOMPtr<nsIContent> parent(do_QueryInterface(parentNode));
+      if (parent) {
+        PRInt32 index;
+        nsCOMPtr<nsIContent> prevNode;
+        nsCOMPtr<nsIStyleSheetLinkingElement> previousLink;
+
+        parent->IndexOf(thisContent, index);
+        while (index-- > 0) {
+          parent->ChildAt(index, *getter_AddRefs(prevNode));
+          previousLink = do_QueryInterface(prevNode);
+          if (previousLink) {
+            previousLink->GetStyleSheet(*getter_AddRefs(prevSheet));
+            if (prevSheet)
+              // Found the first previous sibling that is a style linking element.
+              break;
+          }
+        }
+      }
+    }
+    if (prevSheet)
+      insertionPoint = doc->GetIndexOfStyleSheet(prevSheet) + 1;
+    else
+      insertionPoint = 0;
+  }
+
+  PRBool doneLoading;
+  rv = loader->LoadStyleLink(thisContent, uri, title, media,
+                             kNameSpaceID_Unknown,
+                             insertionPoint, 
+                             ((blockParser) ? mParser.get() : nsnull),
+                             doneLoading, 
+                             this);
+
+  if (NS_SUCCEEDED(rv) && blockParser && !doneLoading) {
+    rv = NS_ERROR_HTMLPARSER_BLOCK;
+  }
+
+  return rv;
+}
+
---
+++ content/base/src/nsStyleLinkElement.h
@@ -0,0 +1,78 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * 
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *  
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *  
+ * The Original Code is Mozilla Communicator client code, released
+ * March 31, 1998.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation. Portions created by Netscape are
+ * Copyright (C) 1998-1999 Netscape Communications Corporation. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): 
+ */
+#ifndef nsStyleLinkElement_h___
+#define nsStyleLinkElement_h___
+
+#include "nsCOMPtr.h"
+#include "nsIDOMLinkStyle.h"
+#include "nsIStyleSheetLinkingElement.h"
+#include "nsICSSStyleSheet.h"
+#include "nsICSSLoaderObserver.h"
+
+class nsIParser;
+class nsIDocument;
+class nsStringArray;
+
+class nsStyleLinkElement : public nsIDOMLinkStyle,
+                           public nsIStyleSheetLinkingElement,
+                           public nsICSSLoaderObserver
+{
+public:
+  nsStyleLinkElement();
+  virtual ~nsStyleLinkElement();
+
+  NS_IMETHOD QueryInterface(REFNSIID aIID, void** aInstancePtr) = 0;
+
+  // nsIDOMLinkStyle
+  NS_DECL_NSIDOMLINKSTYLE
+
+  // nsIStyleSheetLinkingElement  
+  NS_IMETHOD SetStyleSheet(nsIStyleSheet* aStyleSheet);
+  NS_IMETHOD GetStyleSheet(nsIStyleSheet*& aStyleSheet);
+  NS_IMETHOD InitStyleLinkElement(nsIParser *aParser, PRBool aDontLoadStyle);
+  NS_IMETHOD UpdateStyleSheet(PRBool aNotify, nsIDocument *aOldDocument = nsnull, PRInt32 aDocIndex = -1);
+  NS_IMETHOD SetEnableUpdates(PRBool aEnableUpdates);
+
+  // nsICSSLoaderObserver
+  NS_IMETHOD StyleSheetLoaded(nsICSSStyleSheet*aSheet, PRBool aNotify);
+
+  static void ParseLinkTypes(const nsAReadableString& aTypes, nsStringArray& aResult);
+  static void SplitMimeType(const nsString& aValue, nsString& aType, nsString& aParams);
+
+protected:
+  virtual void GetStyleSheetInfo(nsAWritableString& aUrl,
+                                 nsAWritableString& aTitle,
+                                 nsAWritableString& aType,
+                                 nsAWritableString& aMedia,
+                                 PRBool* aIsAlternate) = 0;
+
+
+  nsCOMPtr<nsIStyleSheet> mStyleSheet;
+  nsCOMPtr<nsIParser> mParser;
+  PRBool mDontLoadStyle;
+  PRBool mUpdatesEnabled;
+  PRBool mAppend;
+};
+
+#endif /* nsStyleLinkElement_h___ */
+
