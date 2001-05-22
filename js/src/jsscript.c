3fac8310471d25f26bf9356902b5f8d133382def
rginda%netscape.com <rginda%netscape.com>
2001-05-22 23:57:37 +0000

    create a dummy stack frame before calling out to the new-script-hook.  The dummy frame prevents the script from being affected by any GC the hook may trigger.  See bug 77636.  author=shaver, r=me, sr=brendan

--- js/src/jsscript.c
+++ js/src/jsscript.c
@@ -774,8 +774,25 @@ js_NewScriptFromCG(JSContext *cx, JSCodeGenerator *cg, JSFunction *fun)
     rt = cx->runtime;
     hook = rt->newScriptHook;
     if (hook) {
+        /*
+         * We use a dummy stack frame to protect the script from a GC caused
+         * by debugger-hook execution.
+         *
+         * XXX We really need a way to manage local roots and such more
+         * XXX automatically, at which point we can remove this one-off hack
+         * XXX and others within the engine.  See bug 40757 for discussion.
+         */
+        JSStackFrame dummy;
+        
+        memset(&dummy, 0, sizeof dummy);
+        dummy.down = cx->fp;
+        dummy.script = script;
+        cx->fp = &dummy;
+
         (*hook)(cx, cg->filename, cg->firstLine, script, fun,
                 rt->newScriptHookData);
+
+        cx->fp = dummy.down;
     }
     return script;
 }
