bba5567773dd8cc890940a1d41eda0294dd6dd11
cls@seawood.org <cls@seawood.org>
2001-06-05 00:08:21 +0000

    Handle cygwin & mks uname output on win32. Bug #72154 r=pschwartau@netscape.com a=asa@mozilla.org

--- js/src/config.mk
+++ js/src/config.mk
@@ -56,6 +56,30 @@ ifeq ($(OS_ARCH),IRIX64)
 OS_ARCH         := IRIX
 endif
 
+# Handle output from win32 unames other than Netscape's version
+ifeq (,$(filter-out Windows_95 Windows_98 CYGWIN_95-4.0 CYGWIN_98-4.10, $(OS_ARCH)))
+	OS_ARCH   := WIN95
+endif
+ifeq ($(OS_ARCH),WIN95)
+	OS_ARCH	   := WINNT
+	OS_RELEASE := 4.0
+endif
+ifeq ($(OS_ARCH), Windows_NT)
+	OS_ARCH    := WINNT
+	OS_MINOR_RELEASE := $(shell uname -v)
+	ifeq ($(OS_MINOR_RELEASE),00)
+		OS_MINOR_RELEASE = 0
+	endif
+	OS_RELEASE := $(OS_RELEASE).$(OS_MINOR_RELEASE)
+endif
+ifeq (CYGWIN_NT,$(findstring CYGWIN_NT,$(OS_ARCH)))
+	OS_RELEASE := $(patsubst CYGWIN_NT-%,%,$(OS_ARCH))
+	OS_ARCH    := WINNT
+endif
+ifeq ($(OS_ARCH), CYGWIN32_NT)
+	OS_ARCH    := WINNT
+endif
+
 # Virtually all Linux versions are identical.
 # Any distinctions are handled in linux.h
 ifeq ($(OS_ARCH),Linux)
