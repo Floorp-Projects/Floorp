64a1440a5feba2cdbc1d0be9b466398dd4172fcb
mkaply@us.ibm.com <mkaply@us.ibm.com>
2001-06-08 14:54:10 +0000

    #50203 r=mkaply, aaronr, pedemont sr=blizzard OS/2 only - while investigating optimizer issues, we came up with a better fix for this problem

--- xpcom/reflect/xptcall/src/md/os2/xptcinvoke_icc.cpp
+++ xpcom/reflect/xptcall/src/md/os2/xptcinvoke_icc.cpp
@@ -208,7 +208,8 @@ XPTC_InvokeByIndex(nsISupports *that, PRUint32 index,
    int   result = NS_OK;
 
    // Find size in bytes necessary for call and load param structs
-   ibytes = 4 * invoke_count_words(paramcount, params);
+   // Always allocate at least space for one additional parameter on the stack
+   ibytes = 4 * (invoke_count_words(paramcount, params)+1);
 
    // XXXX DO REST FROM xptcall_vacpp.asm
    // pStack = get_stack() + ibytes;
