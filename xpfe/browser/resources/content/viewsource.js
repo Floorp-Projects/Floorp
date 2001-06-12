41860f4bd1d85da1b85bd96e5a9e8c72142e7258
hwaara%chello.se <hwaara%chello.se>
2001-06-12 20:05:16 +0000

    fix for bug 63026, r=jag, sr=ben, a=asa - adds a menu to the viewsource window - also fixes 22022 39389 50877 59489 69036 74862 76250 79518 81102 82663.
    patch by doronr@naboonline.com

--- xpfe/browser/resources/content/viewsource.js
+++ xpfe/browser/resources/content/viewsource.js
@@ -1,10 +1,32 @@
-/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * The Original Code is mozilla.org code.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) Netscape Communications Corporation.  All
+ * Rights Reserved.
+ * 
+ * Contributor(s): 
+ *    Doron Rosenberg (doronr@naboonline.com) 
+ */
+
 var gBrowser = null;
 var appCore = null;
 
 function onLoadViewSource() 
 {
   viewSource(window.arguments[0]);
+  window._content.focus();
 }
 
 function getBrowser()
@@ -50,35 +72,15 @@ function viewSource(url)
   return true;
 }
 
-function BrowserClose()
-{
-  window.close();
-}
-
 function getMarkupDocumentViewer()
 {
   return getBrowser().markupDocumentViewer;
 }
 
-function BrowserFind()
+// Strips the |view-source:| for editPage()
+function ViewSourceEditPage()
 {
-  var focusedWindow = document.commandDispatcher.focusedWindow;
-  if (!focusedWindow || focusedWindow == window)
-    focusedWindow = window._content;
-
-  findInPage(getBrowser(), window._content, focusedWindow)
-}
-
-function BrowserFindAgain()
-{
-  var focusedWindow = document.commandDispatcher.focusedWindow;
-  if (!focusedWindow || focusedWindow == window)
-    focusedWindow = window._content;
-
-  findAgainInPage(getBrowser(), window._content, focusedWindow)
-}
-
-function BrowserCanFindAgain()
-{
-  return canFindAgainInPage();
+  var url = window._content.location.href;
+  url = url.substring(12,url.length);
+  editPage(url,window, false);
 }
