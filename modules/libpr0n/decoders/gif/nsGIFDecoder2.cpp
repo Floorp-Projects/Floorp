fb5cea14ff50c9c4704450949d0ff9d334a75ce2
pavlov%netscape.com <pavlov%netscape.com>
2001-06-13 01:13:06 +0000

    fixing image rendering color problems on photon.  patch from briane@qnx.com.  r=pavlov sr=blizzard a=asa

--- modules/libpr0n/decoders/gif/nsGIFDecoder2.cpp
+++ modules/libpr0n/decoders/gif/nsGIFDecoder2.cpp
@@ -402,7 +402,7 @@ int HaveDecodedRow(
       format = gfxIFormats::RGB_A1;
     }
 
-#if defined(XP_PC) || defined(XP_BEOS)
+#if defined(XP_PC) || defined(XP_BEOS) || defined(MOZ_WIDGET_PHOTON)
     // XXX this works...
     format += 1; // RGB to BGR
 #endif
@@ -511,7 +511,7 @@ int HaveDecodedRow(
         PRUint32 iwidth = (PRUint32)width;
         for (PRUint32 x=0; x<iwidth; x++) {
           if (*rowBufIndex != decoder->mGIFStruct->tpixel) {
-#if defined(XP_PC) || defined(XP_BEOS)
+#if defined(XP_PC) || defined(XP_BEOS) || defined(MOZ_WIDGET_PHOTON)
             *rgbRowIndex++ = cmap[PRUint8(*rowBufIndex)].blue;
             *rgbRowIndex++ = cmap[PRUint8(*rowBufIndex)].green;
             *rgbRowIndex++ = cmap[PRUint8(*rowBufIndex)].red;
