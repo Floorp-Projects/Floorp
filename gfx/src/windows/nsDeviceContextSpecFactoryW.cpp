93180e504bfb7474324985a10ebb3120a61287a4
rods@netscape.com <rods@netscape.com>
2001-06-14 01:07:08 +0000

    We will now be using the native print dialog and extending it with the Frame printing options The localization is still done, but only on the new controls that are added. Bug 78319 r=kmcclusk sr=attinasi a=asa

--- gfx/src/windows/nsDeviceContextSpecFactoryW.cpp
+++ gfx/src/windows/nsDeviceContextSpecFactoryW.cpp
@@ -61,7 +61,11 @@ NS_IMETHODIMP nsDeviceContextSpecFactoryWin :: Init(void)
 
 //----------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------
-static UINT gFrameSelectedRadioBtn = NULL;
+// Identifies whcih new radio btn was lciked on
+static UINT         gFrameSelectedRadioBtn = NULL;
+
+// Indicates whether the native print dialog was successfully extended
+static PRPackedBool gDialogWasExtended     = PR_FALSE;
 
 #define PRINTDLG_PROPERTIES "chrome://communicator/locale/gfx/printdialog.properties"
 #define TEMPLATE_NAME "PRINTDLGNEW"
@@ -101,7 +105,7 @@ static void SetRadio(HWND         aParent,
                      PRBool       aIsSet,
                      PRBool       isEnabled = PR_TRUE) 
 {
-  HWND wnd = GetDlgItem (aParent, aId);
+  HWND wnd = ::GetDlgItem (aParent, aId);
   if (!wnd) {
     return;
   }
@@ -114,6 +118,20 @@ static void SetRadio(HWND         aParent,
 }
 
 //--------------------------------------------------------
+static void SetRadioOfGroup(HWND aDlg, int aRadId)
+{
+  int radioIds[] = {rad4, rad5, rad6};
+  int numRads = 3;
+
+  for (int i=0;i<numRads;i++) {
+    HWND radWnd = ::GetDlgItem(aDlg, radioIds[i]);
+    if (radWnd != NULL) {
+      ::SendMessage(radWnd, BM_SETCHECK, (WPARAM)(radioIds[i] == aRadId), (LPARAM)0);
+    }
+  }
+}
+
+//--------------------------------------------------------
 typedef struct {
   char * mKeyStr;
   long   mKeyId;
@@ -122,63 +140,267 @@ typedef struct {
 // These are the control ids used in the dialog and 
 // defined by MS-Windows in commdlg.h
 static PropKeyInfo gAllPropKeys[] = {
-    {"Printer", grp4},
-    {"Name", stc6},
-    {"Properties", psh2},
-    {"Status", stc8},
-    {"Type", stc7},
-    {"Where", stc10},
-    {"Comment", stc9},
-    {"Printtofile", chx1},
-    {"Printrange", grp1},
-    {"All", rad1},
-    {"Pages", rad3},
-    {"Selection", rad2},
-    {"from", stc2},
-    {"to", stc3},
-    {"Copies", grp2},
-    {"Numberofcopies", stc5},
-    {"Collate", chx2},
     {"PrintFrames", grp3},
     {"Aslaid", rad4},
     {"selectedframe", rad5},
     {"Eachframe", rad6},
-    {"OK", IDOK},
-    {"Cancel", IDCANCEL},
     {NULL, NULL}};
+
+    // This is a list of other controls Ids in the native dialog
+    // I am leaving these here for documentation purposes
+    //
+    //{"Printer", grp4},
+    //{"Name", stc6},
+    //{"Properties", psh2},
+    //{"Status", stc8},
+    //{"Type", stc7},
+    //{"Where", stc10},
+    //{"Comment", stc9},
+    //{"Printtofile", chx1},
+    //{"Printrange", grp1},
+    //{"All", rad1},
+    //{"Pages", rad3},
+    //{"Selection", rad2},
+    //{"from", stc2},
+    //{"to", stc3},
+    //{"Copies", grp2},
+    //{"Numberofcopies", stc5},
+    //{"Collate", chx2},
+    //{"OK", IDOK},
+    //{"Cancel", IDCANCEL},
     //{"stc11", stc11},
     //{"stc12", stc12},
     //{"stc14", stc14},
     //{"stc13", stc13},
 
 //--------------------------------------------------------
+//--------------------------------------------------------
+//--------------------------------------------------------
+//--------------------------------------------------------
+// Get the absolute coords of the child windows relative
+// to its parent window
+static void GetLocalRect(HWND aWnd, RECT& aRect, HWND aParent)
+{
+  RECT wr;
+  ::GetWindowRect(aParent, &wr);
+
+  RECT cr;
+  ::GetClientRect(aParent, &cr);
+
+  ::GetWindowRect(aWnd, &aRect);
+
+  int borderH = (wr.bottom-wr.top+1) - (cr.bottom-cr.top+1);
+  int borderW = ((wr.right-wr.left+1) - (cr.right-cr.left+1))/2;
+  aRect.top    -= wr.top+borderH-borderW;
+  aRect.left   -= wr.left+borderW;
+  aRect.right  -= wr.left+borderW;
+  aRect.bottom -= wr.top+borderH-borderW;
+}
+
+//--------------------------------------------------------
+// Show or Hide the control
+void Show(HWND aWnd, PRBool bState)
+{
+  if (aWnd) {
+    ::ShowWindow(aWnd, bState?SW_SHOW:SW_HIDE);
+  }
+}
+
+//--------------------------------------------------------
+// Create a child window "control"
+static HWND CreateControl(LPCTSTR         aType,
+                          DWORD           aStyle,
+                          HINSTANCE       aHInst, 
+                          HWND            aHdlg, 
+                          int             aId, 
+                          const nsString& aStr, 
+                          const nsRect&   aRect)
+{
+  char* pStr = nsDeviceContextWin::GetACPString(aStr);
+  if (pStr == NULL) return NULL;
+
+  HWND hWnd = ::CreateWindow (aType, pStr,
+                              WS_CHILD | WS_CLIPSIBLINGS | WS_VISIBLE | aStyle,
+                              aRect.x, aRect.y, aRect.width, aRect.height,
+                              (HWND)aHdlg, (HMENU)aId,
+                              aHInst, NULL);
+  if (hWnd == NULL) return NULL;
+
+  delete [] pStr;
+
+  // get the native font for the dialog and 
+  // set it into the new control
+  HFONT hFont = (HFONT)::SendMessage(aHdlg, WM_GETFONT, (WPARAM)0, (LPARAM)0);
+  if (hFont != NULL) {
+    ::SendMessage(hWnd, WM_SETFONT, (WPARAM) hFont, (LPARAM)0);
+  }
+  return hWnd;
+}
+
+//--------------------------------------------------------
+// Create a Radio Button
+static HWND CreateRadioBtn(HINSTANCE       aHInst, 
+                           HWND            aHdlg, 
+                           int             aId, 
+                           const nsString& aStr, 
+                           const nsRect&   aRect)
+{
+  return CreateControl("BUTTON", BS_RADIOBUTTON, aHInst, aHdlg, aId, aStr, aRect);
+}
+
+//--------------------------------------------------------
+// Create a Group Box
+static HWND CreateGroupBox(HINSTANCE       aHInst, 
+                           HWND            aHdlg, 
+                           int             aId, 
+                           const nsString& aStr, 
+                           const nsRect&   aRect)
+{
+  return CreateControl("BUTTON", BS_GROUPBOX, aHInst, aHdlg, aId, aStr, aRect);
+}
+
+//--------------------------------------------------------
+// Special Hook Procedure for handling the print dialog messages
 UINT CALLBACK PrintHookProc(HWND hdlg, UINT uiMsg, WPARAM wParam, LPARAM lParam) 
 {
+
   if (uiMsg == WM_COMMAND) {
     UINT id = LOWORD(wParam);
     if (id == rad4 || id == rad5 || id == rad6) {
       gFrameSelectedRadioBtn = id;
+      SetRadioOfGroup(hdlg, id);
     }
 
   } else if (uiMsg == WM_INITDIALOG) {
-    PRINTDLG *   printDlg           = (PRINTDLG *)lParam;
-    PRInt16      howToEnableFrameUI = (PRBool)printDlg->lCustData;
-    HWND         collateChk         = GetDlgItem (hdlg, chx2);
-    if (collateChk) {
-      ::ShowWindow(collateChk, SW_SHOW);
+    PRINTDLG * printDlg = (PRINTDLG *)lParam;
+    if (printDlg == NULL) return 0L;
+
+    PRInt16 howToEnableFrameUI = (PRBool)printDlg->lCustData;
+
+    HINSTANCE hInst = (HINSTANCE)::GetWindowLong(hdlg, GWL_HINSTANCE);
+    if (hInst == NULL) return 0L;
+
+    // Start by getting the local rects of several of the controls
+    // so we can calculate where the new controls are
+    HWND wnd = ::GetDlgItem(hdlg, grp1);
+    if (wnd == NULL) return 0L;
+    RECT dlgRect;
+    GetLocalRect(wnd, dlgRect, hdlg);
+
+    wnd = ::GetDlgItem(hdlg, rad1); // this is the top control "All"
+    if (wnd == NULL) return 0L;
+    RECT rad1Rect;
+    GetLocalRect(wnd, rad1Rect, hdlg);
+
+    wnd = ::GetDlgItem(hdlg, rad2); // this is the bottom control "Selection"
+    if (wnd == NULL) return 0L;
+    RECT rad2Rect;
+    GetLocalRect(wnd, rad2Rect, hdlg);
+
+    wnd = ::GetDlgItem(hdlg, rad3); // this is the middle control "Pages"
+    if (wnd == NULL) return 0L;
+    RECT rad3Rect;
+    GetLocalRect(wnd, rad3Rect, hdlg);
+
+    HWND okWnd = ::GetDlgItem(hdlg, IDOK);
+    if (okWnd == NULL) return 0L;
+    RECT okRect;
+    GetLocalRect(okWnd, okRect, hdlg);
+
+    wnd = ::GetDlgItem(hdlg, grp4); // this is the "Print range" groupbox
+    if (wnd == NULL) return 0L;
+    RECT prtRect;
+    GetLocalRect(wnd, prtRect, hdlg);
+
+
+    // calculate various different "gaps" for layout purposes
+
+    int rbGap     = rad3Rect.top - rad1Rect.bottom;     // gap between radiobtns
+    int grpBotGap = dlgRect.bottom - rad2Rect.bottom;   // gap from bottom rb to bottom of grpbox
+    int grpGap    = dlgRect.top - prtRect.bottom ;      // gap between group boxes
+    int top       = dlgRect.bottom + grpGap;            
+    int radHgt    = rad1Rect.bottom - rad1Rect.top + 1; // top of new group box
+    int y         = top+(rad1Rect.top-dlgRect.top);     // starting pos of first radio
+    int rbWidth   = dlgRect.right - rad1Rect.left - 5;  // measure from rb left to the edge of the groupbox
+                                                        // (5 is arbitrary)
+    nsRect rect;
+
+    // Create and position the radio buttons
+    //
+    // If any one control cannot be created then 
+    // hide the others and bail out
+    //
+    rect.SetRect(rad1Rect.left, y, rbWidth,radHgt);
+    HWND rad4Wnd = CreateRadioBtn(hInst, hdlg, rad4, nsAutoString(NS_ConvertASCIItoUCS2("As &laid out on the screen")), rect);
+    if (rad4Wnd == NULL) return 0L;
+    y += radHgt + rbGap;
+
+    rect.SetRect(rad1Rect.left, y, rbWidth, radHgt);
+    HWND rad5Wnd = CreateRadioBtn(hInst, hdlg, rad5, nsAutoString(NS_ConvertASCIItoUCS2("The selected &frame")), rect);
+    if (rad5Wnd == NULL) {
+      Show(rad4Wnd, FALSE); // hide
+      return 0L;
+    }
+    y += radHgt + rbGap;
+
+    rect.SetRect(rad1Rect.left, y, rbWidth, radHgt);
+    HWND rad6Wnd = CreateRadioBtn(hInst, hdlg, rad6, nsAutoString(NS_ConvertASCIItoUCS2("&Each frame separately")), rect);
+    if (rad6Wnd == NULL) {
+      Show(rad4Wnd, FALSE); // hide
+      Show(rad5Wnd, FALSE); // hide
+      return 0L;
+    }
+    y += radHgt + grpBotGap;
+
+    // Create and position the group box
+    rect.SetRect (dlgRect.left, top, dlgRect.right-dlgRect.left+1, y-top+1);
+    HWND grpBoxWnd = CreateGroupBox(hInst, hdlg, grp3, nsAutoString(NS_ConvertASCIItoUCS2("Print Frame")), rect);
+    if (grpBoxWnd == NULL) {
+      Show(rad4Wnd, FALSE); // hide
+      Show(rad5Wnd, FALSE); // hide
+      Show(rad6Wnd, FALSE); // hide
+      return 0L;
     }
 
-    // Localize the print dialog
+    // Here we figure out the old height of the dlg
+    // then figure it's gap from the old grpbx to the bottom
+    // then size the dlg
+    RECT pr, cr; 
+    ::GetWindowRect(hdlg, &pr);
+    ::GetClientRect(hdlg, &cr);
+
+    int dlgHgt = (cr.bottom - cr.top) + 1;
+    int bottomGap = dlgHgt - okRect.bottom;
+    pr.bottom += (dlgRect.bottom-dlgRect.top) + grpGap + 1 - (dlgHgt-dlgRect.bottom) + bottomGap;
+
+    ::SetWindowPos(hdlg, NULL, pr.left, pr.top, pr.right-pr.left+1, pr.bottom-pr.top+1, 
+                   SWP_NOMOVE|SWP_NOREDRAW|SWP_NOZORDER);
+
+    // figure out the new height of the dialog
+    ::GetClientRect(hdlg, &cr);
+    dlgHgt = (cr.bottom - cr.top) + 1;
+ 
+    // Reposition the OK and Cancel btns
+    int okHgt = okRect.bottom - okRect.top + 1;
+    ::SetWindowPos(okWnd, NULL, okRect.left, dlgHgt-bottomGap-okHgt, 0, 0, 
+                   SWP_NOSIZE|SWP_NOREDRAW|SWP_NOZORDER);
+
+    HWND cancelWnd = ::GetDlgItem(hdlg, IDCANCEL);
+    if (cancelWnd == NULL) return 0L;
+
+    RECT cancelRect;
+    GetLocalRect(cancelWnd, cancelRect, hdlg);
+    int cancelHgt = cancelRect.bottom - cancelRect.top + 1;
+    ::SetWindowPos(cancelWnd, NULL, cancelRect.left, dlgHgt-bottomGap-cancelHgt, 0, 0, 
+                   SWP_NOSIZE|SWP_NOREDRAW|SWP_NOZORDER);
+
+    // Localize the new controls in the print dialog
     nsCOMPtr<nsIStringBundle> strBundle;
     if (NS_SUCCEEDED(DeviceContextImpl::GetLocalizedBundle(PRINTDLG_PROPERTIES, getter_AddRefs(strBundle)))) {
       PRInt32 i = 0;
       while (gAllPropKeys[i].mKeyStr != NULL) {
         SetText(hdlg, gAllPropKeys[i].mKeyId, strBundle, gAllPropKeys[i].mKeyStr);
         i++;
-      }
-      nsAutoString titleStr;
-      if (NS_SUCCEEDED(DeviceContextImpl::GetLocalizedString(strBundle, "title", titleStr))) {
-        SetTextOnWnd(hdlg, titleStr);
       }
     }
 
@@ -206,9 +428,13 @@ UINT CALLBACK PrintHookProc(HWND hdlg, UINT uiMsg, WPARAM wParam, LPARAM lParam)
       SetRadio(hdlg, rad5, PR_FALSE, PR_FALSE); 
       SetRadio(hdlg, rad6, PR_FALSE, PR_FALSE); 
     }
+
+    // Looks like we were able to extend the dialog
+    gDialogWasExtended = PR_TRUE;
   }
   return 0L;
 }
+
 
 //XXX this method needs to do what the API says...
 
@@ -221,19 +447,19 @@ NS_IMETHODIMP nsDeviceContextSpecFactoryWin :: CreateDeviceContextSpec(nsIWidget
   nsresult  rv = NS_ERROR_FAILURE;
   NS_WITH_SERVICE(nsIPrintOptions, printService, kPrintOptionsCID, &rv);
 
+  gDialogWasExtended  = PR_FALSE;
+
   PRINTDLG  prntdlg;
   memset(&prntdlg, 0, sizeof(PRINTDLG));
-
   HWND      hWnd      = (HWND)aWidget->GetNativeData(NS_NATIVE_WINDOW);
   HINSTANCE hInstance = (HINSTANCE)::GetWindowLong(hWnd, GWL_HINSTANCE);
 
   prntdlg.lStructSize = sizeof(prntdlg);
-  prntdlg.hwndOwner = NULL;               //XXX need to find a window here. MMP
-  prntdlg.hDevMode = NULL;
-  prntdlg.hDevNames = NULL;
-  prntdlg.hDC = NULL;
-  prntdlg.Flags = PD_ALLPAGES | PD_RETURNIC | PD_HIDEPRINTTOFILE | 
-                  PD_ENABLEPRINTTEMPLATE | PD_ENABLEPRINTHOOK | PD_USEDEVMODECOPIESANDCOLLATE;
+  prntdlg.hwndOwner   = NULL;               //XXX need to find a window here. MMP
+  prntdlg.hDevMode    = NULL;
+  prntdlg.hDevNames   = NULL;
+  prntdlg.hDC         = NULL;
+  prntdlg.Flags       = PD_ALLPAGES | PD_RETURNIC | PD_HIDEPRINTTOFILE | PD_USEDEVMODECOPIESANDCOLLATE;
 
   // if there is a current selection then enable the "Selection" radio button
   PRInt16 howToEnableFrameUI = nsIPrintOptions::kFrameEnableNone;
@@ -246,40 +472,47 @@ NS_IMETHODIMP nsDeviceContextSpecFactoryWin :: CreateDeviceContextSpec(nsIWidget
     printService->GetHowToEnableFrameUI(&howToEnableFrameUI);
   }
 
-  // Get the template name from the string bundle
-  nsAutoString templateName;
+  // Determine whether we have a completely native dialog
+  // or whether we cshould extend it
+  // true  - do only the native
+  // false - extend the dialog
+  PRPackedBool doExtend = PR_FALSE;
   nsCOMPtr<nsIStringBundle> strBundle;
-  nsresult res = DeviceContextImpl::GetLocalizedBundle(PRINTDLG_PROPERTIES, getter_AddRefs(strBundle));
-  if (NS_SUCCEEDED(res)) {
-    res = DeviceContextImpl::GetLocalizedString(strBundle, "templateName", templateName);
+  if (NS_SUCCEEDED(DeviceContextImpl::GetLocalizedBundle(PRINTDLG_PROPERTIES, getter_AddRefs(strBundle)))) {
+    nsAutoString doExtendStr;
+    if (NS_SUCCEEDED(DeviceContextImpl::GetLocalizedString(strBundle, "extend", doExtendStr))) {
+      doExtend = doExtendStr.EqualsIgnoreCase("true");
+    }
   }
-  if (NS_FAILED(res)) {
-    templateName.AssignWithConversion(TEMPLATE_NAME);
-  }
-
-  char * tmpStr = templateName.ToNewCString();
 
   prntdlg.nFromPage           = 0xFFFF;
   prntdlg.nToPage             = 0xFFFF;
   prntdlg.nMinPage            = 1;
   prntdlg.nMaxPage            = 0xFFFF;
   prntdlg.nCopies             = 1;
-  prntdlg.hInstance           = hInstance;
-  prntdlg.lCustData           = (DWORD)howToEnableFrameUI;
-  prntdlg.lpfnPrintHook       = PrintHookProc;
   prntdlg.lpfnSetupHook       = NULL;
-  prntdlg.lpPrintTemplateName = (LPCTSTR)tmpStr?tmpStr:TEMPLATE_NAME;
   prntdlg.lpSetupTemplateName = NULL;
   prntdlg.hPrintTemplate      = NULL;
   prntdlg.hSetupTemplate      = NULL;
+
+  prntdlg.hInstance           = NULL;
+  prntdlg.lpPrintTemplateName = NULL;
+
+  if (!doExtend || aQuiet) {
+    prntdlg.lCustData         = NULL;
+    prntdlg.lpfnPrintHook     = NULL;
+  } else {
+    // Set up print dialog "hook" procedure for extending the dialog
+    prntdlg.lCustData         = (DWORD)howToEnableFrameUI;
+    prntdlg.lpfnPrintHook     = PrintHookProc;
+    prntdlg.Flags            |= PD_ENABLEPRINTHOOK;
+  }
 
   if(PR_TRUE == aQuiet){
     prntdlg.Flags = PD_ALLPAGES | PD_RETURNDEFAULT | PD_RETURNIC | PD_USEDEVMODECOPIESANDCOLLATE;
   }
 
   BOOL result = ::PrintDlg(&prntdlg);
-
-  nsMemory::Free(tmpStr); // free template name
 
   if (TRUE == result)
   {
@@ -312,18 +545,25 @@ NS_IMETHODIMP nsDeviceContextSpecFactoryWin :: CreateDeviceContextSpec(nsIWidget
       }
 
       if (howToEnableFrameUI != nsIPrintOptions::kFrameEnableNone) {
-        // check to see about the frame radio buttons
-        switch (gFrameSelectedRadioBtn) {
-          case rad4: 
-            printService->SetPrintFrameType(nsIPrintOptions::kFramesAsIs);
-            break;
-          case rad5: 
-            printService->SetPrintFrameType(nsIPrintOptions::kSelectedFrame);
-            break;
-          case rad6: 
-            printService->SetPrintFrameType(nsIPrintOptions::kEachFrameSep);
-            break;
-        } // switch 
+        // make sure the dialog got extended
+        if (gDialogWasExtended) {
+          // check to see about the frame radio buttons
+          switch (gFrameSelectedRadioBtn) {
+            case rad4: 
+              printService->SetPrintFrameType(nsIPrintOptions::kFramesAsIs);
+              break;
+            case rad5: 
+              printService->SetPrintFrameType(nsIPrintOptions::kSelectedFrame);
+              break;
+            case rad6: 
+              printService->SetPrintFrameType(nsIPrintOptions::kEachFrameSep);
+              break;
+          } // switch
+        } else {
+          // if it didn't get extended then have it default to printing
+          // each frame separately
+          printService->SetPrintFrameType(nsIPrintOptions::kEachFrameSep);
+        }
       } else {
         printService->SetPrintFrameType(nsIPrintOptions::kNoFrames);
       }
