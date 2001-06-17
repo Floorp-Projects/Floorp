d408641d27ed9d64729f75c877ad841df1c95be4
dbaron@fas.harvard.edu <dbaron@fas.harvard.edu>
2001-06-17 07:31:49 +0000

    Fix bustage from scc's checkin for bug 85271.  r=jag,scc

--- extensions/inspector/base/src/nsCSSDecDataSource.cpp
+++ extensions/inspector/base/src/nsCSSDecDataSource.cpp
@@ -148,7 +148,7 @@ nsCSSDecDataSource::GetURI(char* *aURI)
   NS_PRECONDITION(aURI != nsnull, "null ptr");
   if (! aURI) return NS_ERROR_NULL_POINTER;
 
-  *aURI = nsXPIDLCString::Copy("rdf:" NS_CSSDECDATASOURCE_ID);
+  *aURI = nsCRT::strdup("rdf:" NS_CSSDECDATASOURCE_ID);
 	if (! *aURI) return NS_ERROR_OUT_OF_MEMORY;
 
 	return NS_OK;
