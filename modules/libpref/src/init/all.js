431a60134b923d6cd81f863a581e0529e8330706
dbaron%fas.harvard.edu <dbaron%fas.harvard.edu>
2001-06-19 22:43:10 +0000

    Change default for using system colors as document colors to false, and change default document background color to white rather than light gray.  b=69053  r=attinasi  sr=ben  a=roc

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -56,9 +56,9 @@ pref("browser.cache.check_doc_frequency",   0);
 
 pref("browser.display.use_document_fonts",  1);  // 0 = never, 1 = quick, 2 = always
 pref("browser.display.use_document_colors", true);
-pref("browser.display.use_system_colors",true);
+pref("browser.display.use_system_colors",   false);
 pref("browser.display.foreground_color",    "#000000");
-pref("browser.display.background_color",    "#C0C0C0");
+pref("browser.display.background_color",    "#FFFFFF");
 pref("browser.anchor_color",                "#0000EE");
 pref("browser.visited_color",               "#551A8B");
 pref("browser.underline_anchors",           true);
