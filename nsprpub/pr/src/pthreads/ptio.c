7a7efef943b7ff566f8b7adb3070ee50ff486116
wtc@netscape.com <wtc@netscape.com>
2001-06-20 22:53:38 +0000

    Bugzilla bug #86941: worked around a TCP socket close() bug in Tru64 UNIX V5.1.

--- nsprpub/pr/src/pthreads/ptio.c
+++ nsprpub/pr/src/pthreads/ptio.c
@@ -1142,8 +1142,26 @@ static PRStatus pt_Close(PRFileDesc *fd)
     {
         if (-1 == close(fd->secret->md.osfd))
         {
+#ifdef OSF1
+            /*
+             * Bug 86941: On Tru64 UNIX V5.0A and V5.1, the close()
+             * system call, when called to close a TCP socket, may
+             * return -1 with errno set to EINVAL but the system call
+             * does close the socket successfully.  An application
+             * may safely ignore the EINVAL error.  This bug is fixed
+             * on Tru64 UNIX V5.1A and later.  The defect tracking
+             * number is QAR 81431.
+             */
+            if (PR_DESC_SOCKET_TCP != fd->methods->file_type
+            || EINVAL != errno)
+            {
+                pt_MapError(_PR_MD_MAP_CLOSE_ERROR, errno);
+                return PR_FAILURE;
+            }
+#else
             pt_MapError(_PR_MD_MAP_CLOSE_ERROR, errno);
             return PR_FAILURE;
+#endif
         }
         fd->secret->state = _PR_FILEDESC_CLOSED;
     }
