cc19c1e8d322b299c26d6acd7901f6bc3b26d07b
nhotta%netscape.com <nhotta%netscape.com>
2001-07-02 22:24:09 +0000

    Moved "font.size." prefs from all.js to platform files and changed macpfref.js for .ja default faces to "HeiseiMincho", "HeiseiKakuGothic", default sizes to 14. bug 87871, r=ftang, sr=blizzard.

--- modules/libpref/src/unix/unix.js
+++ modules/libpref/src/unix/unix.js
@@ -140,6 +140,49 @@ pref("font.name.monospace.zh-CN", "isas-song ti-gb2312.1980-0");
 
 // zh-TW
 
+pref("font.default", "serif");
+pref("font.size.variable.ar", 16);
+pref("font.size.fixed.ar", 13);
+
+pref("font.size.variable.el", 16);
+pref("font.size.fixed.el", 13);
+
+pref("font.size.variable.he", 16);
+pref("font.size.fixed.he", 13);
+
+pref("font.size.variable.ja", 16);
+pref("font.size.fixed.ja", 16);
+
+pref("font.size.variable.ko", 16);
+pref("font.size.fixed.ko", 16);
+
+pref("font.size.variable.th", 16);
+pref("font.size.fixed.th", 13);
+
+pref("font.size.variable.tr", 16);
+pref("font.size.fixed.tr", 13);
+
+pref("font.size.variable.x-baltic", 16);
+pref("font.size.fixed.x-baltic", 13);
+
+pref("font.size.variable.x-central-euro", 16);
+pref("font.size.fixed.x-central-euro", 13);
+
+pref("font.size.variable.x-cyrillic", 16);
+pref("font.size.fixed.x-cyrillic", 13);
+
+pref("font.size.variable.x-unicode", 16);
+pref("font.size.fixed.x-unicode", 13);
+
+pref("font.size.variable.x-western", 16);
+pref("font.size.fixed.x-western", 13);
+
+pref("font.size.variable.zh-CN", 16);
+pref("font.size.fixed.zh-CN", 16);
+
+pref("font.size.variable.zh-TW", 16);
+pref("font.size.fixed.zh-TW", 16);
+
 // below a certian pixel size scaled fonts produce poor results
 pref("font.scale.outline.min",      6);
 pref("font.scale.bitmap.min",       12);
