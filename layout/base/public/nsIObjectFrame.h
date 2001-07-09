cdfddbfa3a420e7fb254e052c27068468a597d48
waterson%netscape.com <waterson%netscape.com>
2001-07-09 23:33:28 +0000

    Bug 89076. Remove unneccessary methods. r=dbaron, sr=attinasi.

--- layout/base/public/nsIObjectFrame.h
+++ layout/base/public/nsIObjectFrame.h
@@ -26,7 +26,6 @@
 
 class nsIPluginInstance;
 
-
 // {6D10B07D-E75B-11d4-9885-00C04FA0CF4B}
 #define NS_IOBJECTFRAME_IID \
 { 0x6d10b07d, 0xe75b, 0x11d4, { 0x98, 0x85, 0x0, 0xc0, 0x4f, 0xa0, 0xcf, 0x4b } }
@@ -34,41 +33,6 @@ class nsIPluginInstance;
 class nsIObjectFrame : public nsISupports {
 public:
   NS_DEFINE_STATIC_IID_ACCESSOR(NS_IOBJECTFRAME_IID)
-
-  NS_IMETHOD SetInitialChildList(nsIPresContext* aPresContext,
-                                 nsIAtom*        aListName,
-                                 nsIFrame*       aChildList) = 0;
-  NS_IMETHOD Init(nsIPresContext*  aPresContext,
-                  nsIContent*      aContent,
-                  nsIFrame*        aParent,
-                  nsIStyleContext* aContext,
-                  nsIFrame*        aPrevInFlow) = 0;
-  NS_IMETHOD Reflow(nsIPresContext*          aPresContext,
-                    nsHTMLReflowMetrics&     aDesiredSize,
-                    const nsHTMLReflowState& aReflowState,
-                    nsReflowStatus&          aStatus) = 0;
-  NS_IMETHOD DidReflow(nsIPresContext* aPresContext,
-                       nsDidReflowStatus aStatus) = 0;
-  NS_IMETHOD Paint(nsIPresContext* aPresContext,
-                   nsIRenderingContext& aRenderingContext,
-                   const nsRect& aDirtyRect,
-                   nsFramePaintLayer aWhichLayer) = 0;
-
-  NS_IMETHOD  HandleEvent(nsIPresContext* aPresContext,
-                          nsGUIEvent*     aEvent,
-                          nsEventStatus*  aEventStatus) = 0;
-
-  NS_IMETHOD Scrolled(nsIView *aView) = 0;
-  NS_IMETHOD GetFrameType(nsIAtom** aType) const = 0;
-#ifdef DEBUG
-  NS_IMETHOD GetFrameName(nsString& aResult) const = 0;
-#endif
-
-  NS_IMETHOD Destroy(nsIPresContext* aPresContext) = 0;
-
-  NS_IMETHOD ContentChanged(nsIPresContext* aPresContext,
-                            nsIContent*     aChild,
-                            nsISupports*    aSubContent) = 0;
 
   NS_IMETHOD GetPluginInstance(nsIPluginInstance*& aPluginInstance) = 0;
 };
