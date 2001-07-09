ae30a5c661a363f3a5f060372647015d35d265ab
pschwartau@netscape.com <pschwartau@netscape.com>
2001-07-09 01:09:27 +0000

    Correcting a misattribution, and improving readability.

--- js/tests/js1_5/Regress/regress-89474.js
+++ js/tests/js1_5/Regress/regress-89474.js
@@ -24,12 +24,12 @@
 * See http://bugzilla.mozilla.org/show_bug.cgi?id=89474
 *
 * This test used to crash the JS shell. This was discovered
-* by Mike Epstein <darren.deridder@icarusproject.com
+* by Darren DeRidder <darren.deridder@icarusproject.com
 */
 //-------------------------------------------------------------------------------------------------
 var bug = 89474;
 var summary = "Testing the JS shell doesn't crash on it.item()";
-var sTest = 'it.item()';
+var cnTest = 'it.item()';
 
 
 //-------------------------------------------------------------------------------------------------
@@ -43,7 +43,7 @@ function test()
   printBugNumber (bug);
   printStatus (summary);
 
-  tryThis(sTest); // Just testing that we don't crash on this
+  tryThis(cnTest); // Just testing that we don't crash on this
 
   exitFunc ('test');
 }
