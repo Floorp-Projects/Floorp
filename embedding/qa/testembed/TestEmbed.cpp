19428b48926ccb00dfe8c83ea45ef8aea1012d36
depstein@netscape.com <depstein@netscape.com>
2001-07-10 01:57:23 +0000

    Changed mfcEmbedComponents to testEmbedComponents in TestEmbed.cpp & makefiles.

--- embedding/qa/testembed/TestEmbed.cpp
+++ embedding/qa/testembed/TestEmbed.cpp
@@ -63,7 +63,7 @@ static char THIS_FILE[] = __FILE__;
 
 // this is for overriding the Mozilla default PromptService component
 #include "PromptService.h"
-#define kComponentsLibname "mfcEmbedComponents.dll"
+#define kComponentsLibname "testEmbedComponents.dll"
 #define NS_PROMPTSERVICE_CID \
  {0xa2112d6a, 0x0e28, 0x421f, {0xb4, 0x6a, 0x25, 0xc0, 0xb3, 0x8, 0xcb, 0xd0}}
 static NS_DEFINE_CID(kPromptServiceCID, NS_PROMPTSERVICE_CID);
