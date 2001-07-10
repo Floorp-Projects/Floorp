a3703928fcbee46fe10975737da55aac93c14571
peterlubczynski%netscape.com <peterlubczynski%netscape.com>
2001-07-10 03:59:39 +0000

    Slightly change pluign timer interval to fix url bar on Mac bug 88936 r=bnesse sr=sfraser

--- modules/plugin/nglsrc/nsPluginViewer.cpp
+++ modules/plugin/nglsrc/nsPluginViewer.cpp
@@ -988,7 +988,7 @@ NS_IMETHODIMP pluginInstanceOwner :: CreateWidget(void)
           // start a periodic timer to provide null events to the plugin instance.
           mPluginTimer = do_CreateInstance("@mozilla.org/timer;1", &rv);
           if (rv == NS_OK)
-            rv = mPluginTimer->Init(this, 1000 / 60, NS_PRIORITY_NORMAL, NS_TYPE_REPEATING_SLACK);
+            rv = mPluginTimer->Init(this, 1020 / 60, NS_PRIORITY_NORMAL, NS_TYPE_REPEATING_SLACK);
 #endif
 
 
@@ -1244,7 +1244,7 @@ NS_IMETHODIMP_(void) pluginInstanceOwner::Notify(nsITimer* /* timer */)
   nsresult rv;
   mPluginTimer = do_CreateInstance("@mozilla.org/timer;1", &rv);
   if (NS_SUCCEEDED(rv))
-    mPluginTimer->Init(this, 1000 / 60);
+    mPluginTimer->Init(this, 1020 / 60);
 #endif  // REPEATING_TIMERS
 #endif // XP_MAC
 }
