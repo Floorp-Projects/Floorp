e4f5812f4e48ce9b4f47e9b283da9f8c1e33321a
locka%iol.ie <locka%iol.ie>
2001-07-11 11:57:04 +0000

    NOT PART OF BUILD. Minor update adds a few missing chrome files.

--- embedding/config/gen_mn.pl
+++ embedding/config/gen_mn.pl
@@ -27,48 +27,52 @@ if ($showhelp) {
                "  -help            Show this help.\n",
                "  -mozpath <path>  Specify the path to Mozilla.\n",
                "  -chrome <path>   Specify the path to the chrome.\n",
-               "  -locale <value>  Specify the chrome locale to use.\n";
+               "  -locale <value>  Specify the locale to use. (e.g. en-US)\n";
   exit 1;
 }
 
-
 %embed_files = (
-  "content/global/", "$chrome/toolkit/content/global/",
-  "skin/clasic/global/", "$chrome/classic/skin/classic/global/",
-  "skin/classic/communicator/contents.rdf", "$chrome/classic/skin/classic/communicator/contents.rdf",
-#  "skin/classic/communicator/dialogOverlay.css", "$chrome/classic/skin/classic/communicator/dialogOverlay.css",
-#  "skin/classic/communicator/smallheader-bg.gif", "$chrome/classic/skin/classic/communicator/smallheader-bg.gif",
+  "content/global/", "toolkit/content/global/",
+  "skin/clasic/global/", "classic/skin/classic/global/",
+  "skin/classic/communicator/contents.rdf", "classic/skin/classic/communicator/contents.rdf",
+  "skin/classic/communicator/dialogOverlay.css", "classic/skin/classic/communicator/dialogOverlay.css",
+  "skin/classic/communicator/smallheader-bg.gif", "classic/skin/classic/communicator/smallheader-bg.gif",
   # Locale stuff
-  "locale/$locale/global/", "$chrome/$locale/local/$locale/global/",
-  "locale/$locale/necko/contents.rdf", "$chrome/$locale/locale/$locale/necko/contents.rdf",
-  "locale/$locale/necko/necko.properties", "$chrome/$locale/locale/$locale/necko/necko.properties",
-  "locale/$locale/necko/redirect_loop.dtd", "$chrome/$locale/locale/$locale/necko/redirect_loop.dtd",
-  "locale/$locale/communicator/contents.rdf", "$chrome/$locale/locale/$locale/communicator/contents.rdf",
-  "locale/$locale/communicator/security.properties", "$chrome/$locale/locale/$locale/communicator/security.properties",
+  "locale/$locale/global/", "$locale/local/$locale/global/",
+  "locale/$locale/necko/contents.rdf", "$locale/locale/$locale/necko/contents.rdf",
+  "locale/$locale/necko/necko.properties", "$locale/locale/$locale/necko/necko.properties",
+  "locale/$locale/necko/redirect_loop.dtd", "$locale/locale/$locale/necko/redirect_loop.dtd",
+  "locale/$locale/communicator/contents.rdf", "$locale/locale/$locale/communicator/contents.rdf",
+  "locale/$locale/communicator/security.properties", "$locale/locale/$locale/communicator/security.properties",
+  "locale/$locale/navigator-platform/", "en-unix/locale/$locale/navigator-platform/",
+  "locale/$locale/global-platform/", "en-unix/locale/$locale/global-platform/",
   # Help stuff
-#  "content/help/", "help/content/help/",
-#  "skin/classic/communicator/help.css", "classic/skin/classic/communicator/help.css",
+  "content/help/", "help/content/help/",
+  "skin/classic/communicator/help.css", "classic/skin/classic/communicator/help.css",
 );
 
-print "embed.jar:\n";
-while (($key, $value) = each %embed_files) {
-  open (FILES, "ls -1 $value 2>/dev/null |") or die("Cannot list \"$value\"\n");
-  while (<FILES>) {
-    chomp;
-      
-      
-    if (substr($value, $#value) eq '/') {
-      $chrome_file = "$key$_";
-      $real_file = "$value$_";
-    }
-    else {
-      $chrome_file = $key;
-      $real_file = $value;
-    }
+dump_manifest();
 
-    if (! -d $real_file) {
-      $real_file =~ s/$chrome\///g;
-      print "  $chrome_file   ($real_file)\n";
+sub dump_manifest() {
+  print "embed.jar:\n";
+  while (($key, $value) = each %embed_files) {
+    # Run ls on the dir/file to ensure it's there and to get a file list back
+    open (FILES, "ls -1 $chrome/$value 2>/dev/null |") or die("Cannot list \"$value\"\n");
+    while (<FILES>) {
+      chomp;
+      # If the value ends in a '/' it's a whole dir
+      if (substr($value, $#value) eq '/') {
+	$chrome_file = "$key$_";
+	$real_file = "$value$_";
+      }
+      else {
+	$chrome_file = $key;
+	$real_file = $value;
+      }
+      # Ignore directories which are returned
+      if (! -d "$chrome/$real_file") {
+	print "  $chrome_file   ($real_file)\n";
+      }
     }
   }
 }
