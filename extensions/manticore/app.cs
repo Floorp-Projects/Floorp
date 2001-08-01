d026e97c8ceb1d15e2627a8f89fb499ba6598769
ben%netscape.com <ben%netscape.com>
2001-08-01 03:49:21 +0000

    none of the previous changes are part of the build, of course!

--- extensions/manticore/app.cs
+++ extensions/manticore/app.cs
@@ -1,5 +1,5 @@
 /* -*- Mode: C#; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- *
+ * 
  * The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
