20a797cb3e9a20b6e99fe7f5c15b372596167fee
shanjian%netscape.com <shanjian%netscape.com>
2001-08-02 00:02:59 +0000

    #92339 Universal Auto Chardetector doesn't work. (This is yokoyama@netscape.com 's bug. ) r= shanjian  sr=blizzard@mozilla.org
    his fix also added some comment for:
    #33337 Universal Charset Autodetection for mozilla tree
    The code is almost the same as in commercial tree, 3rd party detector is removed, and name has been changed by roy.
    
    r=shanjian/ftang  sr=blizzard

--- extensions/universalchardet/src/nsUniversalCharDetModule.cpp
+++ extensions/universalchardet/src/nsUniversalCharDetModule.cpp
@@ -1,16 +1,21 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
- * CONFIDENTIAL AND PROPRIETARY SOURCE CODE
- * OF NETSCAPE COMMUNICATIONS CORPORATION
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
- * Copyright (c) 2000 Netscape Communications Corporation.
- * All Rights Reserved.
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
  *
- * Use of this Source Code is subject to the terms of the applicable
- * license agreement from Netscape Communications Corporation.
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
  *
- * The copyright notice(s) in this Source Code does not indicate actual
- * or intended publication of this Source Code.
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
  */
 
 #define NS_IMPL_IDS
@@ -62,9 +67,9 @@ static NS_METHOD nsUniversalCharDetectorRegistrationProc(nsIComponentManager *aC
   }
 
   rv = registry -> AddSubtree(nsIRegistry::Common, 
-                              NS_CHARSET_DETECTOR_REG_BASE "Universal_charset_detector" ,&key);
+                              NS_CHARSET_DETECTOR_REG_BASE "universal_charset_detector" ,&key);
   if (NS_SUCCEEDED(rv)) {
-    rv = registry-> SetStringUTF8(key, "type", "Universal_charset_detector");
+    rv = registry-> SetStringUTF8(key, "type", "universal_charset_detector");
     rv = registry-> SetStringUTF8(key, "defaultEnglishText", "UniversalDetector");
   }
 
