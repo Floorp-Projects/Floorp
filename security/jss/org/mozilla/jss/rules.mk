eb3e9f941c619b14106f2c159918241a3fab229a
nicolson%netscape.com <nicolson%netscape.com>
2001-08-03 07:17:54 +0000

    Make the javadoc target work

--- security/jss/org/mozilla/jss/rules.mk
+++ security/jss/org/mozilla/jss/rules.mk
@@ -31,65 +31,46 @@
 # GPL.
 # 
 
-ifeq ($(JAVADOC_NSS),1)
-JAVADOC_TARGETS=                                    				\
-					org.mozilla.jss.NSSInit						\
-					org.mozilla.jss.KeyDatabaseException			\
-					org.mozilla.jss.CertDatabaseException			\
-					org.mozilla.jss.crypto.AlreadyInitializedException \
-					org.mozilla.jss.util.Password					\
-					org.mozilla.jss.util.PasswordCallback 			\
-					org.mozilla.jss.util.PasswordCallbackInfo		\
-					org.mozilla.jss.util.ConsolePasswordCallback 	\
-					org.mozilla.jss.ssl.PrintOutputStreamWriter	\
-					org.mozilla.jss.ssl.SSLHandshakeCompletedEvent	\
-					org.mozilla.jss.ssl.SSLHandshakeCompletedListener\
-					org.mozilla.jss.ssl.SSLInputStream				\
-					org.mozilla.jss.ssl.SSLOutputStream			\
-					org.mozilla.jss.ssl.SSLSecurityStatus			\
-					org.mozilla.jss.ssl.SSLServerSocket			\
-					org.mozilla.jss.ssl.SSLSocket					\
-					org.mozilla.jss.ssl.SSLSocketImpl				\
-					$(NULL)
-else
-JAVADOC_TARGETS=                                    \
-					org.mozilla.jss.util.Assert	\
-					org.mozilla.jss.util.AssertionException	\
-					org.mozilla.jss.util.Password	\
-					org.mozilla.jss.util.PasswordCallback \
-					org.mozilla.jss.util.IncorrectPasswordException \
-					org.mozilla.jss.util.NotImplementedException \
-					org.mozilla.jss.util.NullPasswordCallback\
-					org.mozilla.jss.util.ConsolePasswordCallback\
-					org.mozilla.jss.util.PasswordCallbackInfo\
-					org.mozilla.jss.util.UTF8Converter \
-					util/Debug.java					\
-                    org.mozilla.jss.crypto         \
-					org.mozilla.jss					\
-					org.mozilla.jss.pkcs11.PK11Module	\
-                    org.mozilla.jss.ssl            \
-                    org.mozilla.jss.asn1                           \
-                    org.mozilla.jss.pkix.primitive                 \
-                    org.mozilla.jss.pkix.cert                      \
-                    org.mozilla.jss.pkix.crmf                      \
-					org.mozilla.jss.pkix.cmmf							\
-                    org.mozilla.jss.pkcs7 							\
-                    org.mozilla.jss.pkcs12 						\
+JAVADOC_TARGETS=                                                        \
+                    org.mozilla.jss                                     \
+                    org.mozilla.jss.asn1                                \
+                    org.mozilla.jss.crypto                              \
+                    org.mozilla.jss.pkcs7                               \
+                    org.mozilla.jss.pkcs10                              \
+                    org.mozilla.jss.pkcs11                              \
+                    org.mozilla.jss.pkcs12                              \
+                    org.mozilla.jss.pkix.primitive                      \
+                    org.mozilla.jss.pkix.cert                           \
+                    org.mozilla.jss.pkix.cmc                            \
+                    org.mozilla.jss.pkix.cmmf                           \
+                    org.mozilla.jss.pkix.cms                            \
+                    org.mozilla.jss.pkix.crmf                           \
+                    org.mozilla.jss.provider                            \
+                    org.mozilla.jss.ssl                                 \
+                    org.mozilla.jss.tests                               \
+                    org.mozilla.jss.util.Assert                         \
+                    org.mozilla.jss.util.AssertionException             \
+                    org.mozilla.jss.util.Base64OutputStream             \
+                    org.mozilla.jss.util.ConsolePasswordCallback        \
+                    org.mozilla.jss.util.IncorrectPasswordException     \
+                    org.mozilla.jss.util.InvalidNicknameException       \
+                    org.mozilla.jss.util.NativeProxy                    \
+                    org.mozilla.jss.util.NotImplementedException        \
+                    org.mozilla.jss.util.NullPasswordCallback           \
+                    org.mozilla.jss.util.Password                       \
+                    org.mozilla.jss.util.PasswordCallback               \
+                    org.mozilla.jss.util.PasswordCallbackInfo           \
+                    org.mozilla.jss.util.Tunnel                         \
+                    org.mozilla.jss.util.UTF8Converter                  \
+                    util/Debug.java                                     \
                     $(NULL)
+
+ifneq ($(HTML_HEADER),)
+HTML_HEADER_OPT=-header '$(HTML_HEADER)'
 endif
 
-ifndef NUKE_JAVA_FILES
 javadoc:
-	@echo \
-"Generating Javadoc will destroy any .java files in the top-level"
-	@echo \
-"ns/ninja/org/mozilla/jss directory.  You must set the NUKE_JAVA_FILES"
-	@echo \
-"environment variable to confirm that this is OK."
-else
-javadoc:
-	cp manage/*.java .
-	if [ ! -d "$(CORE_DEPTH)/ninja/doc" ] ; then mkdir $(CORE_DEPTH)/ninja/doc; fi
-	/share/builds/components/cms_jdk/SunOS/1.2.2_05a/bin/javadoc -native -public -sourcepath $(CORE_DEPTH)/ninja -d $(CORE_DEPTH)/ninja/doc  $(JAVADOC_TARGETS)
-	rm *.java
-endif
+	cp -i manage/*.java .
+	if [ ! -d "$(DIST)/jssdoc" ] ; then mkdir -p $(CORE_DEPTH)/jssdoc ; fi
+	$(JAVADOC) -native -private -sourcepath $(CORE_DEPTH)/jss -d $(CORE_DEPTH)/jssdoc $(HTML_HEADER_OPT) $(JAVADOC_TARGETS)
+	rm -i *.java
