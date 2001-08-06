9ebab332fcba221eb2725de401791e15663ff72f
bryner%netscape.com <bryner%netscape.com>
2001-08-06 21:19:11 +0000

    Bug 93467 - removing remaining references to native combobox and nsIComboBox interface.  r=pavlov, sr=blizzard.

--- widget/src/build/nsWidgetSupport.cpp
+++ widget/src/build/nsWidgetSupport.cpp
@@ -27,7 +27,6 @@
 #include "nsIFileWidget.h"
 #include "nsIAppShell.h"
 #include "nsIButton.h"
-#include "nsIComboBox.h"
 #include "nsIEventListener.h"
 #include "nsILabel.h"
 #include "nsIListBox.h"
@@ -248,37 +247,6 @@ NS_CreateScrollBar(nsISupports* aParent,
 NS_WIDGET nsresult 
 NS_CreateListBox(nsISupports* aParent, 
 									nsIListBox* aWidget, 
-									const nsRect& aRect, 
-                  EVENT_CALLBACK aHandleEventFunction,
-                  const nsFont* aFont)
-{
-	nsIWidget* parent = nsnull;
-	if (aParent != nsnull)
-    aParent->QueryInterface(kIWidgetIID,(void**)&parent);
-
-  nsIWidget* 	widget = nsnull;
-	if (NS_OK == aWidget->QueryInterface(kIWidgetIID,(void**)&widget)) {
-	  widget->Create(parent, aRect, aHandleEventFunction, NULL);
-	  widget->Show(PR_TRUE);
-    if (aFont != nsnull)
-	  	widget->SetFont(*aFont);
-    NS_IF_RELEASE(widget);
-	}
-  else
-  {
-    NS_ERROR("Called QueryInterface on a non kIWidgetIID supported object");
-  }
-
-	if (aParent)
-	  NS_IF_RELEASE(parent);
-
-  return NS_OK;
-}
-
-
-NS_WIDGET nsresult 
-NS_CreateComboBox(nsISupports* aParent, 
-									nsIComboBox* aWidget, 
 									const nsRect& aRect, 
                   EVENT_CALLBACK aHandleEventFunction,
                   const nsFont* aFont)
