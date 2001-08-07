edead61a96f4c7dcd66351ede06f4a6ba1c26866
rogerl%netscape.com <rogerl%netscape.com>
2001-08-07 01:21:11 +0000

    Merging DIKDIK_BRANCH into main line.

--- js2/common.mk
+++ js2/common.mk
@@ -6,11 +6,15 @@ JS2_DIR = $(top_srcdir)/src/
 LIBJS2 = $(JS2_DIR)/libjs2.a
 
 WFLAGS = -Wmissing-prototypes -Wstrict-prototypes -Wunused \
-         -Wswitch
-
+         -Wswitch -Wall -Wconversion
 
 if DEBUG
-CXXFLAGS = -DXP_UNIX -g -DDEBUG -DNEW_PARSER $(WFLAGS)
+CXXFLAGS = -DXP_UNIX -g -DDEBUG $(WFLAGS)
+JS1x_BINDIR = Linux_All_DBG.OBJ
 else
-CXXFLAGS = -DXP_UNIX -O2 -DNEW_PARSER -Wuninitialized $(WFLAGS)
+CXXFLAGS = -DXP_UNIX -O2 -Wuninitialized $(WFLAGS)
+JS1x_BINDIR = Linux_All_OPT.OBJ
 endif
+
+FDLIBM_DIR = $(top_srcdir)/../js/src/fdlibm/$(JS1x_BINDIR)
+LIBFDLIBM = $(FDLIBM_DIR)/libfdm.a
