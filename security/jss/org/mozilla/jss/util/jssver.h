9f0b7149a8e540a6f3ec3ab8241c3e842dccc72d
nicolson%netscape.com <nicolson%netscape.com>
2001-08-09 03:02:23 +0000

    update version string to JSS 3.1, RTM.

--- security/jss/org/mozilla/jss/util/jssver.h
+++ security/jss/org/mozilla/jss/util/jssver.h
@@ -41,10 +41,10 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>] [<Beta>]"
  */
-#define JSS_VERSION  "3.1 Beta"
+#define JSS_VERSION  "3.1"
 #define JSS_VMAJOR   3
 #define JSS_VMINOR   1
 #define JSS_VPATCH   0
-#define JSS_BETA     PR_TRUE
+#define JSS_BETA     PR_FALSE
 
 #endif
