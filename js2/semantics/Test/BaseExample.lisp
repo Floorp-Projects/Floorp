0974f0ff722eaa3d1acc10b17052242582e446fa
waldemar%netscape.com <waldemar%netscape.com>
2001-08-11 06:53:35 +0000

    Brought up to date

--- js2/semantics/Test/BaseExample.lisp
+++ js2/semantics/Test/BaseExample.lisp
@@ -5,11 +5,11 @@
      '((lexer base-example-lexer
               :lalr-1
               :numeral
-              ((:digit (#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)
-                       ((value $digit-value))))
+              ((:digit (#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9) ((value $digit-value))))
               (($digit-value integer digit-value digit-char-36)))
        
-       (deftype semantic-exception (oneof syntax-error))
+       (deftag syntax-error)
+       (deftype semantic-exception (tag syntax-error))
        
        (%charclass :digit)
        
@@ -17,26 +17,29 @@
                       (base-value (-> (integer) integer)))
          (production :digits (:digit) digits-first
            (decimal-value (value :digit))
-           ((base-value (base integer))
-            (let ((d integer (value :digit)))
-              (if (< d base) d (throw (oneof syntax-error))))))
+           ((base-value base)
+            (const d integer (value :digit))
+            (if (< d base)
+              (return d)
+              (throw syntax-error))))
          (production :digits (:digits :digit) digits-rest
            (decimal-value (+ (* 10 (decimal-value :digits)) (value :digit)))
-           ((base-value (base integer))
-            (let ((d integer (value :digit)))
-              (if (< d base)
-                (+ (* base ((base-value :digits) base)) d)
-                (throw (oneof syntax-error)))))))
+           ((base-value base)
+            (const d integer (value :digit))
+            (if (< d base)
+              (return (+ (* base ((base-value :digits) base)) d))
+              (throw syntax-error)))))
        
        (rule :numeral ((value integer))
          (production :numeral (:digits) numeral-digits
            (value (decimal-value :digits)))
          (production :numeral (:digits #\# :digits) numeral-digits-and-base
            (value
-            (let ((base integer (decimal-value :digits 2)))
-              (if (and (>= base 2) (<= base 10))
-                ((base-value :digits 1) base)
-                (throw (oneof syntax-error)))))))
+            (begin
+             (const base integer (decimal-value :digits 2))
+             (if (and (>= base 2) (<= base 10))
+               (return ((base-value :digits 1) base))
+               (throw syntax-error))))))
        (%print-actions)
        )))
   
@@ -44,19 +47,19 @@
   (defparameter *beg* (lexer-grammar *bel*)))
 
 #|
-(depict-rtf-to-local-file
- "Test/BaseExampleSemantics.rtf"
- "Base Example Semantics"
- #'(lambda (rtf-stream)
-     (depict-world-commands rtf-stream *bew*)))
-
-(depict-html-to-local-file
- "Test/BaseExampleSemantics.html"
- "Base Example Semantics"
- t
- #'(lambda (html-stream)
-     (depict-world-commands html-stream *bew*))
- :external-link-base "")
+(values
+  (depict-rtf-to-local-file
+   "Test/BaseExampleSemantics.rtf"
+   "Base Example Semantics"
+   #'(lambda (rtf-stream)
+       (depict-world-commands rtf-stream *bew*)))
+  (depict-html-to-local-file
+   "Test/BaseExampleSemantics.html"
+   "Base Example Semantics"
+   t
+   #'(lambda (html-stream)
+       (depict-world-commands html-stream *bew*))
+   :external-link-base ""))
 
 
 (lexer-pparse *bel* "37")
