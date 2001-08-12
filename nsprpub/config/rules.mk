cd2765dc3cb017fd66fba027e8ae2fc439f23262
cls@seawood.org <cls@seawood.org>
2001-08-12 03:40:04 +0000

    Adding --enable-strip option. Bug #88045 r=wtc sr=waterson

--- nsprpub/config/rules.mk
+++ nsprpub/config/rules.mk
@@ -283,7 +283,7 @@ else
 	$(CC) -o $@ $(CFLAGS) $(OBJS) $(LDFLAGS)
 endif
 endif
-ifdef BUILD_OPT
+ifdef ENABLE_STRIP
 	$(STRIP) $@
 endif
 
@@ -339,10 +339,9 @@ endif	# OpenVMS
 endif   # OS2
 endif	# WINNT
 endif	# AIX 4.1
-ifdef BUILD_OPT
+ifdef ENABLE_STRIP
 	$(STRIP) $@
 endif
-
 
 ifeq (,$(filter-out WINNT OS2,$(OS_ARCH)))
 $(RES): $(RESNAME)
