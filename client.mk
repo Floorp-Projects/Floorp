4b340c20c99ea53da8d70d8c09b0ae5f7e37cb79
wtc@netscape.com <wtc@netscape.com>
2001-08-14 18:09:04 +0000

    Bugzilla bug 78471: have Mozilla pull the latest version of NSPR (NSPR 4.2 Beta).  The tag we are using now is the static tag NSPRPUB_CLIENT_TAG. We may change the tag to "HEAD" (the tip of NSPR) or some client-specific branch tag soon.  This landing has been approved by drivers@mozilla.org for mozilla0.9.4. Modified files: mozilla/client.mk mozilla/client.mak mozilla/build/mac/build_scripts/MozillaCheckoutList.txt

--- client.mk
+++ client.mk
@@ -53,7 +53,7 @@
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
 #MOZ_CO_TAG = <tag>
-NSPR_CO_TAG = NSPRPUB_CLIENT_BRANCH
+NSPR_CO_TAG = NSPRPUB_CLIENT_TAG
 PSM_CO_TAG = #We will now build PSM from the tip instead of a branch.
 NSS_CO_TAG = NSS_CLIENT_TAG
 LDAPCSDK_CO_TAG = LDAPCSDK_40_BRANCH
