0fa8d70e3e876819a66667f16e479ff0771ffa4b
pschwartau%netscape.com <pschwartau%netscape.com>
2001-08-14 02:19:08 +0000

    Added many new cases to test.

--- js2/tests/js/js2_0/Class/class-010.js
+++ js2/tests/js/js2_0/Class/class-010.js
@@ -20,95 +20,164 @@
 * Date: 2001-06-26
 *
 * SUMMARY: Testing that classes can see global variables
+*          Will compare objX, or objX.prop, to expected values...
 */
-//-------------------------------------------------------------------------------------------------
-var UBound = 0;
-var bug = '(none)';
-var summary = 'Testing that classes can see global variables';
-var status = '';
-var statusitems = [];
-var actual = '';
-var actualvalues = [];
-var expect= '';
-var expectedvalues = [];
-var self = this; // capture a reference to the global object
-var objX = {};
-var objY = {}
+//-----------------------------------------------------------------------------
+var UBound:Integer = 0;
+var bug:String = '(none)';
+var summary:String = 'Testing that classes can see global variables';
+var status:String = '';
+var statusitems:Array = [];
+var actual:String = '';
+var actualvalues:Array = [];
+var expect:String= '';
+var expectedvalues:Array = [];
+var cnA:String = 'A';
+var cnC:String = 'C';
+var cnX:String = 'X';
+var cnY:String = 'Y';
+var cnZ:String = 'Z';
+var self:Object = this;
+var objX:Object = {prop:cnX};
+var objY:Object = {prop:cnY};
 
 
-class A 
+class A
 {
-}
+  var prop:String;
 
-class B 
-{
-  function B() 
+  prototype function A()
   {
-    objX = objA;  // global variable defined below -
+    prop = cnA;
+    objX = objY;
   }
 }
 
-status = inSection(1);
-actual = objX;
-expect = self;
-addThis();
 
-
-/******************************************************
-
-class C 
+class B
 {
+  prototype function B()
+  {
+    // objA is defined below, but BEFORE any instances of B are made -
+    objX = objA;
+  }
+}
+
+
+class C
+{
+  var prop:String;
+
+  prototype function C()
+  {
+    prop = cnC;
+  }
+
   function changeX() 
   {
     objX = self;
   }
 }
 
-class CC extends C 
+
+// Here is a CLASS-level "objX" to resolve...
+class CC extends C
 {
-  var m = test;
+  var objX:Object;
+
+  prototype function CC()
+  {
+    objX = this;
+  }
 }
 
 
 class D
 {
-  function changeX() 
+  prototype function D()
   {
-    objX = objA;
+  }
+
+  static function changeX()
+  {
+    objX.prop = cnZ;
   }
 }
+
 
 class E
 {
-  function E (obj:B)
+  prototype function E (obj:C)
   {
-    objX = obj;
+    objX= obj;
+  }
+
+  function changeX()
+  {
+    objX = new A;
   }
 }
 
-******************************************************/
 
+
+status = inSection(1);
+actual = objX.prop;
+expect = cnX;
+addThis();
+
+status = inSection(2);
 var objA = new A;
+actual = objX.prop;
+expect = cnY;
+addThis();
+
+status = inSection(3);
 var objB = new B;
+actual = objX;
+expect = objA;
+addThis();
 
-/******************************************************
-
+status = inSection(4);
 var objC = new C;
-var objCC = new CC;
-var objD = new D;
-var objE = new E(objB);
+actual = objX.prop;
+expect = cnA;
+addThis();
 
-
-objB.changeX();
+status = inSection(5);
 objC.changeX();
-objCC.m();
-objD.changeX();
+actual = objX.prop;
+expect = undefined;
+addThis();
 
-******************************************************/
+status = inSection(6);
+var objCC = new CC;
+actual = objX.prop;
+expect = cnC; // expect objX == objCC, which inherits .prop from C
+addThis();
 
-//-------------------------------------------------------------------------------------------------
+status = inSection(7);
+D.changeX(); // static method
+actual = objX.prop;
+expect = cnZ;
+addThis();
+
+status = inSection(8);
+var objE = new E(objC);
+actual = objX.prop;
+expect = cnC;
+addThis();
+
+status = inSection(9);
+objE.changeX();
+actual = objX.prop;
+expect = cnA;
+addThis();
+
+
+
+//-----------------------------------------------------------------------------
 test();
-//-------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
 
 function addThis()
@@ -125,5 +194,11 @@ function test()
   enterFunc ('test');
   printBugNumber (bug);
   printStatus (summary);
+
+  for (var i=0; i<UBound; i++)
+  {
+    reportCompare(expectedvalues[i], actualvalues[i], statusitems[i]);
+  }
+
   exitFunc ('test');
 }
