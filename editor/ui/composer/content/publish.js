e0cd7c40c9ee6096de1b14aa61781f60a1550b44
brade@netscape.com <brade@netscape.com>
2001-08-17 22:34:11 +0000

    not in the build

--- editor/ui/composer/content/publish.js
+++ editor/ui/composer/content/publish.js
@@ -1,7 +1,22 @@
+var gPublishHandler = null;
+
 function EditorPublish(destinationDirectoryLocation, fileName, login, password)
 {
   post_current_editor_contents_to_server(destinationDirectoryLocation, fileName, login, password);
 //  PublishCopyFile(srcDirectoryLocation, destinationDirectoryLocation, fileName);
+}
+
+var gPublishIOService;
+function GetIOService()
+{
+  if (gPublishIOService)
+    return gPublishIOService;
+
+  gPublishIOService = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+  if (!gPublishIOService)
+    dump("failed to get io service\n");
+  
+  return gPublishIOService;
 }
 
 // this function takes a login and password and adds them to the destination url
@@ -9,24 +24,40 @@ function get_destination_channel(destinationDirectoryLocation, fileName, login, 
 {
   try
   {
-    var ioService = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+    var ioService = GetIOService();
     if (!ioService)
-    {
-      dump("failed to get io service\n");
       return;
-    }
 
     // create a channel for the destination location
-    destChannel = create_channel_from_url(ioService, destinationDirectoryLocation + fileName, login, password);
+    var fullurl = destinationDirectoryLocation + fileName;
+    dump("location is "+fullurl+"\n");
+    destChannel = create_channel_from_url(ioService, fullurl, login, password);
     if (!destChannel)
     {
       dump("can't create dest channel\n");
       return;
     }
-    // destChannel.SetNotificationCallbacks();
+    try {
+    dump("about to set callbacks\n");
+    destChannel.notificationCallbacks = window.docshell;  // docshell
+    dump("notification callbacks set\n");
+    }
+    catch(e) {dump(e+"\n");}
 
     var ftpChannel = destChannel.QueryInterface(Components.interfaces.nsIFTPChannel);
-    return ftpChannel;
+    if (ftpChannel) dump("ftp channel found\n");
+    if (ftpChannel)
+      return ftpChannel;
+    var httpChannel = destChannel.QueryInterface(Components.interfaces.nsIHTTPChannel);
+    if (httpChannel) dump("http channel found\n");
+    if (httpChannel)
+      return httpChannel;
+    var httpsChannel = destChannel.QueryInterface(Components.interfaces.nsIHTTPSChannel);
+    if (httpsChannel) dump("https channel found\n");
+    if (httpsChannel)
+      return httpsChannel;
+    else
+      return null;
   }
   catch (e)
   {
@@ -36,14 +67,24 @@ function get_destination_channel(destinationDirectoryLocation, fileName, login, 
 
 function output_current_editor_to_stream()
 {
-  var formatType = editorShell.editorType;
-  dump("formatType is " + formatType + "\n");
+  var streamwrapper = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
+  if (!streamwrapper)
+  {
+    dump("streamwrapper could not be created\n");
+    return null;
+  }
+  
+  var formatType = 'text/' + editorShell.editorType;
   var flags = 256; // nsIDocumentEncoder::OutputEncodeEntities
   var charset = editorShell.GetDocumentCharacterSet();
-  dump("charset is " + charset + "\n");
-  // XXX we need to create an nsIOutputStream here
-  var stream = editorShell.editor.OutputToStream('text/'+formatType, charset, flags); 
-  return stream;
+  editorShell.editor.OutputToStream(streamwrapper, formatType, charset, flags); 
+  if (!streamwrapper)
+    return null;
+  var inputstream = streamwrapper.QueryInterface(Components.interfaces.nsIInputStream);
+  dump("input stream is: " + inputstream + "\n");
+  var outputstream = streamwrapper.QueryInterface(Components.interfaces.nsIOutputStream);
+  dump("output stream is: " + outputstream + "\n");
+  return streamwrapper;
 }
 
 function post_current_editor_contents_to_server(newLocation, fileName, login, password)
@@ -57,14 +98,14 @@ function post_current_editor_contents_to_server(newLocation, fileName, login, pa
       return;
     }
 
-    var ftpChannel = get_destination_channel(newLocation, fileName, login, password);
-    if (!ftpChannel)
+    var protocolChannel = get_destination_channel(newLocation, fileName, login, password);
+    if (!protocolChannel)
     {
       dump("failed to get a destination channel\n");
       return;
     }
-    ftpChannel.uploadStream = contentsStream;
-    ftpChannel.asyncOpen(null, null);
+    protocolChannel.uploadStream = contentsStream;
+    protocolChannel.asyncOpen(gPublishingListener, null);
     dump("done\n");
   }
   catch (e)
@@ -108,12 +149,9 @@ function PublishCopyFile(srcDirectoryLocation, destinationDirectoryLocation, fil
   try
   {
     // grab an io service
-    var ioService = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+    var ioService = GetIOService();
     if (!ioService)
-    {
-      dump("failed to get io service\n");
       return;
-    }
 
     // create a channel for the source location
     srcChannel = create_channel_from_url(ioService, srcDirectoryLocation + fileName, null, null);
@@ -139,3 +177,33 @@ function PublishCopyFile(srcDirectoryLocation, destinationDirectoryLocation, fil
     dump("an error occurred: " + e + "\n");
   }
 }
+
+var gPublishingListener =
+{
+  QueryInterface: function(aIId, instance)
+  {
+    if (aIId.equals(Components.interfaces.nsIStreamListener))
+      return this;
+    if (aIId.equals(Components.interfaces.nsISupports))
+      return this;
+    
+    dump("QueryInterface " + aIId + "\n");
+    throw Components.results.NS_NOINTERFACE;
+  },
+
+  onStartRequest: function(request, ctxt)
+  {
+    dump("onStartRequest status = " + request.status + "\n");
+  },
+
+  onStopRequest: function(request, ctxt, status, errorMsg)
+  {
+    dump("onStopRequest status = " + request.status + " " + errorMsg + "\n");
+  },
+
+  onDataAvailable: function(request, ctxt, inStream, sourceOffset, count)
+  {
+    dump("onDataAvailable status = " + request.status + " " + count + "\n");
+  }
+}
+
