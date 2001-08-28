e8f99474b0d9f9186a7fff69286bc4fc847cde35
dcone@netscape.com <dcone@netscape.com>
2001-08-28 20:54:35 +0000

    bug 12037.  A small fix for a regression that was and r=peterl sr=attinasi and a=dbaron.  I forgot to checkin the check to see if I handled the print.  This fixes my regression.

--- gfx/src/windows/nsImageWin.cpp
+++ gfx/src/windows/nsImageWin.cpp
@@ -482,6 +482,8 @@ nsImageWin :: Draw(nsIRenderingContext &aContext, nsDrawingSurface aSurface,
 
   if (nsnull != TheHDC){
     if (!IsOptimized() || nsnull==mHBitmap){
+      PRBool  didComposite = PR_FALSE;
+
       rop = SRCCOPY;
 
       if (nsnull != mAlphaBits){
@@ -490,6 +492,7 @@ nsImageWin :: Draw(nsIRenderingContext &aContext, nsDrawingSurface aSurface,
             MONOBITMAPINFO  bmi(mAlphaWidth, mAlphaHeight);
               CompositeBitsInMemory(TheHDC,aDX,aDY,aDWidth,aDHeight,aSX,aSY,aSWidth,aSHeight,srcy,
                     mAlphaBits,&bmi,mImageBits,mBHead,mNumPaletteColors);
+            didComposite = PR_TRUE;
           } else {
 
             MONOBITMAPINFO  bmi(mAlphaWidth, mAlphaHeight);
@@ -502,7 +505,7 @@ nsImageWin :: Draw(nsIRenderingContext &aContext, nsDrawingSurface aSurface,
       }
 
       
-      if (canRaster != DT_RASPRINTER){
+      if (PR_FALSE == didComposite){
         if (8==mAlphaDepth) {              
            DrawComposited(TheHDC, aDX, aDY, aDWidth, aDHeight,
              aSX, srcy, aSWidth, aSHeight);
