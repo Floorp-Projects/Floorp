31780763c5582f1320bb5909fdfa13585e1c1384
tao%netscape.com <tao%netscape.com>
2001-08-29 03:00:27 +0000

    8124: default charset should be set in ja-unix.jar -> move this pref to en-unix.jar.(r=jbetak,sr=sspitzer).

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -389,7 +389,7 @@ pref("intl.charsetmenu.mailview.cache",     "");
 pref("intl.charsetmenu.composer.cache",     "");
 pref("intl.charsetmenu.browser.cache.size", 5);
 pref("intl.charset.detector",               "chrome://navigator/locale/navigator.properties");
-pref("intl.charset.default",                "chrome://navigator/locale/navigator.properties");
+pref("intl.charset.default",                "chrome://navigator-platform/locale/navigator.properties");
 pref("intl.content.langcode",               "chrome://communicator-region/locale/region.properties");
 pref("font.language.group",                 "chrome://navigator/locale/navigator.properties");
 
