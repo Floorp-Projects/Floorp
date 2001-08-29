562e7bdf22cc38c7144644c3e82e2ee5d0b5307d
tao%netscape.com <tao%netscape.com>
2001-08-29 02:55:47 +0000

    8120: intl.collationOption should be defined in ja-unix.jar (r=jbetak,sr=mscott) move this pref to en-unix.jar.

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -375,7 +375,7 @@ pref("wallet.miscPanel.hide",               false);
 pref("imageblocker.enabled",                true);
 pref("intl.accept_languages",               "chrome://navigator/locale/navigator.properties");
 pref("intl.accept_charsets",                "iso-8859-1,*,utf-8");
-pref("intl.collationOption",                "chrome://navigator/locale/navigator.properties");
+pref("intl.collationOption",                "chrome://navigator-platform/locale/navigator.properties");
 pref("intl.menuitems.alwaysappendacceskeys","chrome://navigator/locale/navigator.properties");
 pref("intl.charsetmenu.browser.static",     "chrome://navigator/locale/navigator.properties");
 pref("intl.charsetmenu.browser.more1",      "chrome://navigator/locale/navigator.properties");
