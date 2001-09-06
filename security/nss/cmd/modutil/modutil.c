5214f9119721079f248f1c006ba45d45373339fa
relyea@netscape.com <relyea@netscape.com>
2001-09-06 21:16:51 +0000

    Fix Updating the slot in modutil. Add option to dump the raw PKCS #11 strings.

--- security/nss/cmd/modutil/modutil.c
+++ security/nss/cmd/modutil/modutil.c
@@ -56,6 +56,7 @@ typedef enum {
 	FIPS_COMMAND,
 	JAR_COMMAND,
 	LIST_COMMAND,
+	RAW_LIST_COMMAND,
 	UNDEFAULT_COMMAND
 } Command;
 
@@ -72,6 +73,7 @@ static char *commandNames[] = {
 	"-fips",
 	"-jar",
 	"-list",
+	"-rawlist",
 	"-undefault"
 };
 
@@ -93,6 +95,7 @@ typedef enum {
 	JAR_ARG,
 	LIBFILE_ARG,
 	LIST_ARG,
+	RAW_LIST_ARG,
 	MECHANISMS_ARG,
 	NEWPWFILE_ARG,
 	PWFILE_ARG,
@@ -100,6 +103,7 @@ typedef enum {
 	UNDEFAULT_ARG,
 	INSTALLDIR_ARG,
 	TEMPDIR_ARG,
+	SECMOD_ARG,
 	NOCERTDB_ARG,
 
 	NUM_ARGS	/* must be last */
@@ -122,6 +126,7 @@ static char *optionStrings[] = {
 	"-jar",
 	"-libfile",
 	"-list",
+	"-rawlist",
 	"-mechanisms",
 	"-newpwfile",
 	"-pwfile",
@@ -129,6 +134,7 @@ static char *optionStrings[] = {
 	"-undefault",
 	"-installdir",
 	"-tempdir",
+	"-secmod",
 	"-nocertdb"
 };
 
@@ -145,6 +151,7 @@ static char* pwFile = NULL;
 static char* newpwFile = NULL;
 static char* moduleName = NULL;
 static char* slotName = NULL;
+static char* secmodName = NULL;
 static char* tokenName = NULL;
 static char* libFile = NULL;
 static char* dbdir = NULL;
@@ -380,6 +387,17 @@ parse_args(int argc, char *argv[])
 					moduleName = argv[++i];
 			}
 			break;
+		case RAW_LIST_ARG:
+			if(command != NO_COMMAND) {
+				PR_fprintf(PR_STDERR, errStrings[MULTIPLE_COMMAND_ERR], arg);
+				return MULTIPLE_COMMAND_ERR;
+			}
+			command = RAW_LIST_COMMAND;
+			/* This option may or may not have an argument */
+			if( (i+1 < argc) && (argv[i+1][0] != '-') ) {
+					moduleName = argv[++i];
+			}
+			break;
 		case MECHANISMS_ARG:
 			if(mechanisms != NULL) {
 				PR_fprintf(PR_STDERR, errStrings[DUPLICATE_OPTION_ERR], arg);
@@ -423,6 +441,17 @@ parse_args(int argc, char *argv[])
 				return OPTION_NEEDS_ARG_ERR;
 			}
 			slotName = argv[i];
+			break;
+		case SECMOD_ARG:
+			if(secmodName != NULL) {
+				PR_fprintf(PR_STDERR, errStrings[DUPLICATE_OPTION_ERR], arg);
+				return DUPLICATE_OPTION_ERR;
+			}
+			if(TRY_INC(i, argc)) {
+				PR_fprintf(PR_STDERR, errStrings[OPTION_NEEDS_ARG_ERR], arg);
+				return OPTION_NEEDS_ARG_ERR;
+			}
+			secmodName = argv[i];
 			break;
 		}
 	}
@@ -469,6 +498,7 @@ verify_params()
 		}
 		break;
 	case LIST_COMMAND:
+	case RAW_LIST_COMMAND:
 		break;
 	case UNDEFAULT_COMMAND:
 	case DEFAULT_COMMAND:
@@ -508,6 +538,9 @@ init_crypto(PRBool create, PRBool readOnly)
 	SECStatus rv;
 	PRUint32 flags = 0;
 
+	if (secmodName == NULL) {
+	    secmodName = "secmod.db";
+	}
 
 	if(SECU_ConfigDirectory(dbdir)[0] == '\0') {
 		PR_fprintf(PR_STDERR, errStrings[NO_DBDIR_ERR]);
@@ -622,7 +655,7 @@ init_crypto(PRBool create, PRBool readOnly)
 	if (readOnly) flags |= NSS_INIT_READONLY;
 	if (nocertdb) flags |= NSS_INIT_NOCERTDB;
 	rv = NSS_Initialize(SECU_ConfigDirectory(NULL), dbprefix, dbprefix,
-	               "secmod.db", flags);
+	               secmodName, flags);
 	if (rv != SECSuccess) {
 	    SECU_PrintPRandOSError(progName);
 	    retval=NSS_INITIALIZE_FAILED_ERR;
@@ -693,6 +726,7 @@ usage()
 "-dbprefix prefix                 Prefix for the security databases\n"
 "-nocertdb                        Do not load certificate or key databases. No\n"
 "                                 verification will be performed on JAR files.\n"
+"-secmod secmodName               Name of the security modules file\n"
 "---------------------------------------------------------------------------\n"
 "\n"
 "Mechanism lists are colon-separated.  The following mechanisms are recognized:\n"
@@ -741,6 +775,20 @@ main(int argc, char *argv[])
 		usage();
 		errcode = INVALID_USAGE_ERR;
 		goto loser;
+	}
+
+        if (command == RAW_LIST_COMMAND) {
+	    if(!moduleName) {
+		if (!secmodName) secmodName="secmod.db";
+		if (!dbprefix) dbprefix = "";
+		SECU_ConfigDirectory(dbdir);
+
+		moduleName=PR_smprintf("name=\"NSS default Module DB\" parameters=\"configdir=%s certPrefix=%s keyPrefix=%s secmod=%s flags=readOnly%s\" NSS=\"flags=internal,moduleDB,moduleDBOnly,critical\"",
+			SECU_ConfigDirectory(NULL),dbprefix,
+				dbprefix,secmodName, nocertdb?",noCertDB":"");
+	    }
+	    errcode = RawListModule(moduleName);
+	    goto loser;
 	}
 
 	/* Set up crypto stuff */
