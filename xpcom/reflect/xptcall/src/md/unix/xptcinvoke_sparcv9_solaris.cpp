983d7ffd94f4e0cdb810165be457dddcde5457a2
pavlov%netscape.com <pavlov%netscape.com>
2001-09-06 08:18:24 +0000

    Adding a few more comments about what is going on and fixing the couple of issues brought up by brendan.  Work for bug 91840.  not part of build

--- xpcom/reflect/xptcall/src/md/unix/xptcinvoke_sparcv9_solaris.cpp
+++ xpcom/reflect/xptcall/src/md/unix/xptcinvoke_sparcv9_solaris.cpp
@@ -39,19 +39,14 @@ extern "C" PRUint64
 invoke_copy_to_stack(PRUint64* d, PRUint32 paramCount, nsXPTCVariant* s)
 {
   /*
-      We need to copy the parameters for this function to locals and use them
-      from there since the parameters occupy the same stack space as the stack
-      we're trying to populate.
-    */
+    We need to copy the parameters for this function to locals and use them
+    from there since the parameters occupy the same stack space as the stack
+    we're trying to populate.
+  */
   PRUint64 *l_d = d;
   nsXPTCVariant *l_s = s;
   PRUint64 l_paramCount = paramCount;
   PRUint64 regCount = 0;  // return the number of registers to load from the stack
-
-  typedef struct {
-    float hi;
-    float lo;
-  } DU;
 
   for(PRUint64 i = 0; i < l_paramCount; i++, l_d++, l_s++)
   {
@@ -64,25 +59,26 @@ invoke_copy_to_stack(PRUint64* d, PRUint32 paramCount, nsXPTCVariant* s)
     }
     switch (l_s->type)
     {
-      case nsXPTType::T_I8     : *((PRInt64*)l_d)  = l_s->val.i8;    break;
-      case nsXPTType::T_I16    : *((PRInt64*)l_d)  = l_s->val.i16;   break;
-      case nsXPTType::T_I32    : *((PRInt64*)l_d)  = l_s->val.i32;   break;
-      case nsXPTType::T_I64    : *((PRInt64*)l_d)  = l_s->val.i64;   break;
+      case nsXPTType::T_I8    : *((PRInt64*)l_d)     = l_s->val.i8;    break;
+      case nsXPTType::T_I16   : *((PRInt64*)l_d)     = l_s->val.i16;   break;
+      case nsXPTType::T_I32   : *((PRInt64*)l_d)     = l_s->val.i32;   break;
+      case nsXPTType::T_I64   : *((PRInt64*)l_d)     = l_s->val.i64;   break;
       
-      case nsXPTType::T_U8     : *((PRUint64*)l_d) = l_s->val.u8;    break;
-      case nsXPTType::T_U16    : *((PRUint64*)l_d) = l_s->val.u16;   break;
-      case nsXPTType::T_U32    : *((PRUint64*)l_d) = l_s->val.u32;   break;
-      case nsXPTType::T_U64    : *((PRUint64*)l_d) = l_s->val.u64;   break;
-      
-      case nsXPTType::T_FLOAT  : {
-        ((DU *)l_d)->lo = l_s->val.f;
-        ((DU *)l_d)->hi = l_s->val.f;
-        break;
-      }
-      case nsXPTType::T_DOUBLE : *((double*)l_d)   = l_s->val.d;     break;
-      case nsXPTType::T_BOOL   : *((PRBool*)l_d)   = l_s->val.b;     break;
-      case nsXPTType::T_CHAR   : *((PRUint64*)l_d) = l_s->val.c;     break;
-      case nsXPTType::T_WCHAR  : *((PRInt64*)l_d)  = l_s->val.wc;    break;
+      case nsXPTType::T_U8    : *((PRUint64*)l_d)    = l_s->val.u8;    break;
+      case nsXPTType::T_U16   : *((PRUint64*)l_d)    = l_s->val.u16;   break;
+      case nsXPTType::T_U32   : *((PRUint64*)l_d)    = l_s->val.u32;   break;
+      case nsXPTType::T_U64   : *((PRUint64*)l_d)    = l_s->val.u64;   break;
+
+      /* in the case of floats, we want to put the bits in to the
+         64bit space right justified... floats in the paramter array on
+         sparcv9 use odd numbered registers.. %f1, %f3, so we have to skip
+         the space that would be occupied by %f0, %f2, etc.
+      */
+      case nsXPTType::T_FLOAT : *(((float*)l_d) + 1) = l_s->val.f;     break;
+      case nsXPTType::T_DOUBLE: *((double*)l_d)      = l_s->val.d;     break;
+      case nsXPTType::T_BOOL  : *((PRBool*)l_d)      = l_s->val.b;     break;
+      case nsXPTType::T_CHAR  : *((PRUint64*)l_d)    = l_s->val.c;     break;
+      case nsXPTType::T_WCHAR : *((PRInt64*)l_d)     = l_s->val.wc;    break;
 
       default:
         // all the others are plain pointer types
