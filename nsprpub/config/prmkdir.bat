d346727adf3642d40f0737b66a8d8629407fc53b
wtc%netscape.com <wtc%netscape.com>
2001-09-07 18:37:58 +0000

    Bugzilla bug 98741: replaced NPL by MPL/GPL. Modified files: prerr.et prmkdir.bat pr/src/md/os2/objs.mk

--- nsprpub/config/prmkdir.bat
+++ nsprpub/config/prmkdir.bat
@@ -1,18 +1,34 @@
-REM
-REM The contents of this file are subject to the Netscape Public License
-REM Version 1.1 (the "NPL"); you may not use this file except in
-REM compliance with the NPL.  You may obtain a copy of the NPL at
-REM http://www.mozilla.org/NPL/
 REM 
-REM Software distributed under the NPL is distributed on an "AS IS" basis,
-REM WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
-REM for the specific language governing rights and limitations under the
-REM NPL.
+REM The contents of this file are subject to the Mozilla Public
+REM License Version 1.1 (the "License"); you may not use this file
+REM except in compliance with the License. You may obtain a copy of
+REM the License at http://www.mozilla.org/MPL/
 REM 
-REM The Initial Developer of this code under the NPL is Netscape
-REM Communications Corporation.  Portions created by Netscape are
-REM Copyright (C) 1998 Netscape Communications Corporation.  All Rights
-REM Reserved.
-REM
+REM Software distributed under the License is distributed on an "AS
+REM IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+REM implied. See the License for the specific language governing
+REM rights and limitations under the License.
+REM 
+REM The Original Code is the Netscape Portable Runtime (NSPR).
+REM 
+REM The Initial Developer of the Original Code is Netscape
+REM Communications Corporation.  Portions created by Netscape are 
+REM Copyright (C) 1998-2000 Netscape Communications Corporation.  All
+REM Rights Reserved.
+REM 
+REM Contributor(s):
+REM 
+REM Alternatively, the contents of this file may be used under the
+REM terms of the GNU General Public License Version 2 or later (the
+REM "GPL"), in which case the provisions of the GPL are applicable 
+REM instead of those above.  If you wish to allow use of your 
+REM version of this file only under the terms of the GPL and not to
+REM allow others to use your version of this file under the MPL,
+REM indicate your decision by deleting the provisions above and
+REM replace them with the notice and other provisions required by
+REM the GPL.  If you do not delete the provisions above, a recipient
+REM may use your version of this file under either the MPL or the
+REM GPL.
+REM 
 
 mkdir %1
