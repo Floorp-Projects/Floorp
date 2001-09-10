9904d904dfe307f72ba273795e2e7389d8501462
dbaron%fas.harvard.edu <dbaron%fas.harvard.edu>
2001-09-10 19:39:25 +0000

    Change NS_SUCCEEDED check to null-check since GetDocument may return a null document.  r=jag  sr=waterson  b=99056

--- layout/generic/nsBulletFrame.cpp
+++ layout/generic/nsBulletFrame.cpp
@@ -1515,8 +1515,8 @@ nsBulletFrame::GetBaseURI(nsIURI **aURI)
   }
   else {
     nsCOMPtr<nsIDocument> doc;
-    rv = mContent->GetDocument(*getter_AddRefs(doc));
-    if (NS_SUCCEEDED(rv)) {
+    mContent->GetDocument(*getter_AddRefs(doc));
+    if (doc) {
       doc->GetBaseURL(*getter_AddRefs(baseURI));
     }
   }
