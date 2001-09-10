d28253c5193ffde5404705669f34f27a6d4e483d
mcafee%netscape.com <mcafee%netscape.com>
2001-09-10 03:27:27 +0000

    Fixing some logic errors

--- tools/tinderbox/build-galeon.pl
+++ tools/tinderbox/build-galeon.pl
@@ -43,13 +43,15 @@ sub main {
 
   # Pending a config file, stuff things here.
   my $post_status = 'success';  # Success until we report a failure.
-  my $status = 0;
+  my $status = 0;  # 0 = success
   my $galeon_alive_test = 1;
   my $galeon_test8_test = 1;
   my $galeon_dir = "$mozilla_build_dir/galeon";
+  my $galeon_binary = "galeon-bin";
 
   # Checkout/update the galeon code.
   $status = checkout($mozilla_build_dir);
+  TinderUtils::print_log "Status from checkout: $status\n";
   if ($status != 0) {
   	$post_status = 'busted';
   }
@@ -63,24 +65,33 @@ sub main {
 	# Make sure we have a configure
 	unless (-e "configure" and $post_status ne 'busted') {
 	  $status = TinderUtils::run_shell_command("./autogen.sh");
-	  
+	  TinderUtils::print_log "Status from autogen: $status\n";
 	}
 
 	# Not sure how to only do this when we need to.
 	# Force a configure for now.
-	if ($status != 0) {
+	if ($status == 0) {
+	  TinderUtils::print_log "configuring...\n";
 	  $status = TinderUtils::run_shell_command("./configure --sysconfdir=/etc --with-mozilla-libs=$mozilla_build_dir/mozilla/dist/bin --with-mozilla-includes=$mozilla_build_dir/mozilla/dist/include --with-nspr-includes=$mozilla_build_dir/mozilla/dist/include/nspr --with-mozilla-home=$mozilla_build_dir/mozilla/dist/bin");
+	  TinderUtils::print_log "Status from configure: $status\n";
 	}
 
-	if ($status != 0) {
-	  TinderUtils::DeleteBinary("$galeon_dir/src/galeon-bin");
+	if ($status == 0) {
+	  TinderUtils::print_log "Deleting, gmake...\n";
+	  TinderUtils::DeleteBinary("$galeon_dir/src/$galeon_binary");
+
+	  my $foo = Cwd::getcwd();
+	  TinderUtils::print_log "cwd = $foo\n";
 	  $status = TinderUtils::run_shell_command("gmake");
+	  TinderUtils::print_log "Status from gmake: $status\n";
 	}
 
+	TinderUtils::print_log "Testing build status...\n";
 	if ($status != 0) {
+	  TinderUtils::print_log "busted, gmake status non-zero\n";
 	  $post_status = 'busted';
-	} elsif (not TinderUtils::BinaryExists("$galeon_dir/src/galeon-bin")) {
-	  TinderUtils::print_log "Error: binary not found: $galeon_dir/src/galeon-bin\n";
+	} elsif (not TinderUtils::BinaryExists("$galeon_dir/src/$galeon_binary")) {
+	  TinderUtils::print_log "Error: binary not found: $galeon_dir/src/$galeon_binary\n";
 	  $post_status = 'busted';
 	} else {
 	  $post_status = 'success';
@@ -93,7 +104,6 @@ sub main {
   #
   # Also, have to set crash_recovery=0 in ~/.gnome/galeon to avoid
   # getting the crash dialog after the alive test times out.
-  # Looks like --disable-crash-dialog is not implemented yet.
   #
 
   # Test galeon, about:blank
