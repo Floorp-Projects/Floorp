4bd49ff576464e340b7c1d5554c022d65c53219e
dbaron%fas.harvard.edu <dbaron%fas.harvard.edu>
2001-09-11 00:42:52 +0000

    Removing commented-out remnant of old hand-crafted nsIFactory implementation.  b=97491  r=edburns  sr=waterson

--- modules/oji/src/nsCJVMManagerFactory.cpp
+++ modules/oji/src/nsCJVMManagerFactory.cpp
@@ -21,11 +21,15 @@
  *   Pierre Phaneuf <pp@ludusdesign.com>
  */
 
-#if 1
-
 #include "nsIModule.h"
 #include "nsIGenericFactory.h"
 #include "nsJVMManager.h"
+
+/*
+ * Note:  In revision 1.17 of this file (and earlier) there was a
+ * commented out implementation of nsCJVMManagerFactory, a hand-crafted
+ * implementation of nsIFactory.
+ */
 
 // The list of components we register
 static nsModuleComponentInfo components[] = 
@@ -38,178 +42,3 @@ static nsModuleComponentInfo components[] =
 };
 
 NS_IMPL_NSGETMODULE(nsCJVMManagerModule, components);
-
-#else
-
-#include "prtypes.h"
-#include "nspr.h"
-#include "prmem.h"
-#include "prmon.h"
-#include "prlog.h"
-
-
-
-
-#include "nsJVMManager.h"
-#include "nsCJVMManagerFactory.h"
-#include "nsIComponentManager.h"
-#include "nsCOMPtr.h"
-#include "nsIServiceManager.h"
-
-static NS_DEFINE_IID(kISupportsIID,    NS_ISUPPORTS_IID);
-static NS_DEFINE_IID(kIServiceManagerIID,    NS_ISERVICEMANAGER_IID);
-static NS_DEFINE_IID(kIFactoryIID,     NS_IFACTORY_IID);
-static NS_DEFINE_CID(kJVMManagerCID, NS_JVMMANAGER_CID);
-static NS_DEFINE_CID(kComponentManagerCID, NS_COMPONENTMANAGER_CID);
-
-///////////////////////////////////////////////////////////////////////////////
-// Auto-registration functions START
-///////////////////////////////////////////////////////////////////////////////
-
-extern "C" NS_EXPORT nsresult NSRegisterSelf(nsISupports* aServMgr, 
-                                             const char *path)
-{
-    nsresult rv;
-
-    nsCOMPtr<nsIServiceManager> servMgr(do_QueryInterface(aServMgr, &rv));
-    if (NS_FAILED(rv)) {
-        return rv;
-    }
-
-    nsIComponentManager* compMgr;
-    rv = servMgr->GetService(kComponentManagerCID, 
-                             NS_GET_IID(nsIComponentManager), 
-                             (nsISupports**)&compMgr);
-    if (NS_FAILED(rv)) {
-        return rv;
-    }
-
-    rv = compMgr->RegisterComponent(kJVMManagerCID,
-                                    "JVM Manager Service",
-                                    "@mozilla.org/oji/jvm-mgr;1",
-                                    path, 
-                                    PR_TRUE, PR_TRUE);
-
-    (void)servMgr->ReleaseService(kComponentManagerCID, compMgr);
-    return rv;
-}
-
-extern "C" NS_EXPORT nsresult NSUnregisterSelf(nsISupports* aServMgr, 
-                                               const char *path)
-{
-    nsresult rv;
-    
-    nsCOMPtr<nsIServiceManager> servMgr(do_QueryInterface(aServMgr, &rv));
-    if (NS_FAILED(rv)) {
-        return rv;
-    }
-
-    nsIComponentManager* compMgr;
-    rv = servMgr->GetService(kComponentManagerCID, 
-                             NS_GET_ID(nsIComponentManager), 
-                             (nsISupports**)&compMgr);
-    if (NS_FAILED(rv)) {
-        return rv;
-    }
-    
-    rv = compMgr->UnregisterComponent(kJVMManagerCID, path);
-
-    (void)servMgr->ReleaseService(kComponentManagerCID, compMgr);
-    return rv;
-}
-
-/*+++++++++++++++++++++++++++++++++++++++++++++++++
- * NSGetFactory:
- * Provides entry point to liveconnect dll.
- +++++++++++++++++++++++++++++++++++++++++++++++++*/
-
-extern "C" NS_EXPORT nsresult
-NSGetFactory(nsISupports* serviceMgr,
-             const nsCID &aClass,
-             const char *aClassName,
-             const char *aContractID,
-             nsIFactory **aFactory)
-{
-    if (!aClass.Equals(kJVMManagerCID)) {
-        return NS_ERROR_FACTORY_NOT_LOADED;     // XXX right error?
-    }
-
-    nsCJVMManagerFactory* factory = new nsCJVMManagerFactory();
-    if (factory == NULL) {
-        return NS_ERROR_OUT_OF_MEMORY;
-    }
-    
-    NS_ADDREF(factory);
-    *aFactory = factory;
-    
-    return NS_OK;
-}
-
-extern "C" NS_EXPORT PRBool
-NSCanUnload(nsISupports* serviceMgr)
-{
-    return PR_FALSE;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Auto-registration functions END
-///////////////////////////////////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////////////////////////////
-// from nsISupports 
-
-#if 1
-NS_METHOD
-nsCJVMManagerFactory::QueryInterface(const nsIID& aIID, void** aInstancePtr) 
-{
-    PR_ASSERT(NULL != aInstancePtr); 
-    if (NULL == aInstancePtr) {
-        return NS_ERROR_NULL_POINTER; 
-    } 
-    if (aIID.Equals(kIFactoryIID) ||
-        aIID.Equals(kISupportsIID)) {
-        *aInstancePtr = (void*) this; 
-        AddRef(); 
-        return NS_OK; 
-    }
-    return NS_NOINTERFACE; 
-}
-
-NS_IMPL_ADDREF(nsCJVMManagerFactory)
-NS_IMPL_RELEASE(nsCJVMManagerFactory)
-#else
-NS_IMPL_ISUPPORTS1(nsCJVMManagerFactory, nsIFactory)
-#endif
-
-////////////////////////////////////////////////////////////////////////////
-// from nsIFactory:
-
-NS_METHOD
-nsCJVMManagerFactory::CreateInstance(nsISupports *aOuter, REFNSIID aIID, void **aResult)
-{
-	return nsJVMManager::Create(aOuter, aIID, aResult);
-}
-
-NS_METHOD
-nsCJVMManagerFactory::LockFactory(PRBool aLock)
-{
-   return NS_OK;
-}
-
-
-
-////////////////////////////////////////////////////////////////////////////
-// from nsCJVMManagerFactory:
-
-nsCJVMManagerFactory::nsCJVMManagerFactory(void)
-{
-      NS_INIT_REFCNT();
-}
-
-nsCJVMManagerFactory::~nsCJVMManagerFactory()
-{
-}
-
-
-#endif
