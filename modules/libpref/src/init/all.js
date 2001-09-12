bbd9d9b807f963ce3e9c37b89e72d2b29a011521
bzbarsky@mit.edu <bzbarsky@mit.edu>
2001-09-12 20:16:32 +0000

    Add line-wrapping capability to the view source window.  Bug 22022, r=bzbarsky, sr=alecf

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -142,6 +142,7 @@ pref("browser.forms.submit.backwards_compatible", true);
 
 // view source
 pref("view_source.syntax_highlight", true);
+pref("view_source.wrap_long_lines", false);
 
 // gfx widgets
 pref("nglayout.widget.mode", 2);
