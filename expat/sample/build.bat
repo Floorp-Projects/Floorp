39142c356b4400f2535847b79efe57d5a4d4f307
gerv@gerv.net <gerv@gerv.net>
2001-09-19 21:50:17 +0000

    More license whackage. We are relicensing the expat makefiles to be compatible with the LGPL and GPL but making it clear that expat itself is under James Clakrk's MIT/X license. Bug 98089.

--- expat/sample/build.bat
+++ expat/sample/build.bat
@@ -1,3 +1,6 @@
+rem Copyright (c) 1998, 1999 Thai Open Source Software Center Ltd
+rem See the file COPYING for copying permission.
+
 @echo off
 set LIB=..\xmlparse\Release;..\lib;%LIB%
 cl /nologo /DXMLTOKAPI=__declspec(dllimport) /DXMLPARSEAPI=__declspec(dllimport) /I..\xmlparse /Fe..\bin\elements elements.c xmlparse.lib
