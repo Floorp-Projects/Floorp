1856815ff1c4a505a0c384fbf42fc02df5c5c5eb
gerv%gerv.net <gerv%gerv.net>
2001-09-20 00:02:59 +0000

    Oops.

--- expat/xmlwf/codepage.c
+++ expat/xmlwf/codepage.c
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 1998, 1999 Thai Open Source Software Center Ltd
-See the file COPYING for copying permission.
+See the file copying.txt for copying permission.
 */
 
 #include "codepage.h"
--- parser/expat/lib/xmltok_ns.c
+++ parser/expat/lib/xmltok_ns.c
@@ -1,8 +1,3 @@
-/*
-Copyright (c) 1998, 1999 Thai Open Source Software Center Ltd
-See the file COPYING for copying permission.
-*/
-
 const ENCODING *NS(XmlGetUtf8InternalEncoding)(void)
 {
   return &ns(internal_utf8_encoding).enc;
