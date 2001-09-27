f748f11fe5176ed015b31fe1c144155f19787e5d
srilatha%netscape.com <srilatha%netscape.com>
2001-09-27 03:51:23 +0000

    Checkin for implementation of MAPILogon and MAPILogoff. Checking in for kkhandrika@netscape.com Not part of the build. a=chofmann

---
+++ mailnews/mapi/mapihook/src/msgMapiFactory.h
@@ -0,0 +1,69 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corp.
+ * Portions created by the Initial Developer are Copyright (C) 2001
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s): Krishna Mohan Khandrika (kkhandrika@netscape.com)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#ifndef MSG_MAPI_FACTORY_H
+#define MSG_MAPI_FACTORY_H
+
+#include <windows.h>
+#include <objbase.h>
+#include "nspr.h"
+
+
+class nsMapiFactory : public IClassFactory
+{
+public :
+
+    // IUnknown
+
+    STDMETHODIMP            QueryInterface (REFIID aIid, void** aPpv);
+    STDMETHODIMP_(ULONG)    AddRef(void);
+    STDMETHODIMP_(ULONG)    Release(void);
+
+    // IClassFactory
+
+    STDMETHODIMP        CreateInstance (LPUNKNOWN aUnkOuter, REFIID aIid, void **aPpv);
+    STDMETHODIMP        LockServer (BOOL aLock);
+
+    nsMapiFactory();
+    ~nsMapiFactory();
+
+private :
+
+    PRInt32 m_cRef;
+};
+
+#endif  // MSG_MAPI_FACTORY_H
+
