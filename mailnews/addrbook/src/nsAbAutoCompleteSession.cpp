52a95a415695f5ebb6ec4539d21c9e602241cd68
mscott%netscape.com <mscott%netscape.com>
2001-09-29 04:31:59 +0000

    Bug #101498 --> auto complete preselects the default domain OVER the first ldap match r=sspitzer,ducarroz sr=hewitt

--- mailnews/addrbook/src/nsAbAutoCompleteSession.cpp
+++ mailnews/addrbook/src/nsAbAutoCompleteSession.cpp
@@ -646,9 +646,9 @@ NS_IMETHODIMP nsAbAutoCompleteSession::OnStartLookup(const PRUnichar *uSearchStr
               status = nsIAutoCompleteStatus::matchFound;
               if (addedDefaultItem)
               {
-                // always make the first search result the default item. 
-                // if we dont' have any matches then use pos 0 which is the default item....
-                  results->SetDefaultItemIndex(nbrOfItems > 1 ? 1 : 0);
+                // If we have at least one REAL match then make it the default item. If we don't have any matches,
+                // just the default domain, then don't install a default item index on the widget.
+                results->SetDefaultItemIndex(nbrOfItems > 1 ? 1 : -1);
               }
               else
                 results->SetDefaultItemIndex(0);  
