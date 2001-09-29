a276cfdb53e169ec62ecb72a773c93b6085ec292
gerv%gerv.net <gerv%gerv.net>
2001-09-29 00:10:42 +0000

    License changes Round 2. Expat. Updating licensing to be consistent with latest version of expat (MIT/X); relicensing NS-contributed code under expat's license (except the build system, which is tri-licensed.) Removing outdated licensing information and adding new COPYING file. This has been checked with James Clark. Bug 98089.

--- expat/xmltok/nametab.h
+++ expat/xmltok/nametab.h
@@ -1,3 +1,8 @@
+/*
+Copyright (c) 1998, 1999 Thai Open Source Software Center Ltd
+See the file COPYING for copying permission.
+*/
+
 static const unsigned namingBitmap[] = {
 0x00000000, 0x00000000, 0x00000000, 0x00000000,
 0x00000000, 0x00000000, 0x00000000, 0x00000000,
--- parser/expat/lib/latin1tab.h
+++ parser/expat/lib/latin1tab.h
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 1998, 1999 Thai Open Source Software Center Ltd
-See the file copying.txt for copying permission.
+See the file COPYING for copying permission.
 */
 
 /* 0x80 */ BT_OTHER, BT_OTHER, BT_OTHER, BT_OTHER,
