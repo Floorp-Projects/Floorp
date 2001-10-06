55a770a93a1e4bb6bd0c1e1b4016c7ac2d40dfa5
bienvenu%netscape.com <bienvenu%netscape.com>
2001-10-06 00:53:01 +0000

    fix UMR r=naving, sr=sspitzer 103134

--- mailnews/addrbook/src/nsAbMDBDirectory.cpp
+++ mailnews/addrbook/src/nsAbMDBDirectory.cpp
@@ -71,7 +71,8 @@ nsAbMDBDirectory::nsAbMDBDirectory(void)
   :  nsAbMDBRDFResource(),
      mInitialized(PR_FALSE),
 	 mIsMailingList(-1),
-     mPerformingQuery(PR_FALSE)
+     mPerformingQuery(PR_FALSE),
+     mIsQueryURI(PR_FALSE)
 {
 	NS_NewISupportsArray(getter_AddRefs(mSubDirectories));
 }
