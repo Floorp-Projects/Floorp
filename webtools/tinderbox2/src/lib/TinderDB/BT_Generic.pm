29cf401afdd8f1d7587ccf5cefdfc5a204c6542a
kestes%walrus.com <kestes%walrus.com>
2001-10-10 15:07:41 +0000

    typo in new BT_NAME parameter, did not get default from TinderboxConfig

--- webtools/tinderbox2/src/lib/TinderDB/BT_Generic.pm
+++ webtools/tinderbox2/src/lib/TinderDB/BT_Generic.pm
@@ -74,7 +74,7 @@ use TreeData;
 use VCDisplay;
 
 
-$VERSION = ( qw $Revision: 1.11 $ )[1];
+$VERSION = ( qw $Revision: 1.12 $ )[1];
 
 @ISA = qw(TinderDB::BasicTxtDB);
 
@@ -86,7 +86,7 @@ push @TinderDB::HTML_COLUMNS, TinderDB::BT->new();
 
 
 # name of the bug tracking system
-$BT_NAME = $TinderDB::BT_NAME || "BT";
+$BT_NAME = $TinderConfig::BT_NAME || "BT";
 
 # remove all records from the database which are older then last_time.
 
