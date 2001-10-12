aa09fc1fea256ddf9916da21d932116c1796b1ec
mcafee@netscape.com <mcafee@netscape.com>
2001-10-12 00:02:32 +0000

    usage, allow for different exe names, e.g. mozilla or netscape or galeon

--- tools/performance/startup/startup-unix.pl
+++ tools/performance/startup/startup-unix.pl
@@ -2,7 +2,14 @@
 
 #
 # Script to time mozilla startup.
+# Feeds in start time as url argument, startup-test.html
+# takes this arg and computes the time difference.
+# So something like this happens:
 #
+# mozilla file:/foo/startup-test.html?begin=T
+#   where T = ms since 1970, e.g.:
+# mozilla file:/foo/startup-test.html?begin=999456977124
+# 
 
 require 5.003;
 
@@ -11,20 +18,22 @@ require "gettime.pl";
 use strict;
 use Cwd;
 
-# 
-# mozilla file:/foo/startup-test.html?begin=T
-#   where T = seconds since 1970, e.g.:
-# mozilla file:/foo/startup-test.html?begin=999456977000
-# 
+sub PrintUsage {
+  die <<END_USAGE
+  usage: startup-unix.pl <exe>
+END_USAGE
+}
 
 {
-	# Build up command string.
-	my $cwd = Cwd::getcwd();
-	my $time = Time::PossiblyHiRes::getTime();
-	my $cmd = "mozilla \"file:$cwd/startup-test.html?begin=" . $time . "\"";	
-	print "cmd = $cmd\n";
+  PrintUsage() if $#ARGV != 0;
 
-	# Run the command.
-	exec $cmd;
+  # Build up command string.
+  my $cwd = Cwd::getcwd();
+  my $time = Time::PossiblyHiRes::getTime();
+  my $cmd = "@ARGV[0] \"file:$cwd/startup-test.html?begin=" . $time . "\"";	
+  print "cmd = $cmd\n";
+  
+  # Run the command.
+  exec $cmd;
 }
 
