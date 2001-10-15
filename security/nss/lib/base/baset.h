ac8983809513150b1a50ac84b54c8bc84b9f144b
ian.mcgreer@sun.com <ian.mcgreer@sun.com>
2001-10-15 16:50:01 +0000

    add ordering capability to the list

--- security/nss/lib/base/baset.h
+++ security/nss/lib/base/baset.h
@@ -35,7 +35,7 @@
 #define BASET_H
 
 #ifdef DEBUG
-static const char BASET_CVS_ID[] = "@(#) $RCSfile: baset.h,v $ $Revision: 1.5 $ $Date: 2001/09/20 20:33:26 $ $Name:  $";
+static const char BASET_CVS_ID[] = "@(#) $RCSfile: baset.h,v $ $Revision: 1.6 $ $Date: 2001/10/15 16:50:01 $ $Name:  $";
 #endif /* DEBUG */
 
 /*
@@ -102,6 +102,7 @@ typedef struct nssArenaMarkStr nssArenaMark;
 typedef struct nssListStr nssList;
 typedef struct nssListIteratorStr nssListIterator;
 typedef PRBool (* nssListCompareFunc)(void *a, void *b);
+typedef PRIntn (* nssListSortFunc)(void *a, void *b);
 typedef void (* nssListElementDestructorFunc)(void *el);
 
 typedef struct nssHashStr nssHash;
