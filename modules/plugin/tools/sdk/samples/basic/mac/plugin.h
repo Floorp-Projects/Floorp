5254993ab2894e8e2b86b4219d656d3a43d15436
peterlubczynski%netscape.com <peterlubczynski%netscape.com>
2001-10-17 23:37:52 +0000

    Plugin SDK basic plugin port for Mac initial check-in. not part of the build

---
+++ modules/plugin/tools/sdk/samples/basic/mac/plugin.h
@@ -0,0 +1,68 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: NPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is 
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1998
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or 
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the NPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the NPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#ifndef __PLUGIN_H__
+#define __PLUGIN_H__
+
+#include "pluginbase.h"
+
+class nsPluginInstance : public nsPluginInstanceBase
+{
+public:
+  nsPluginInstance(NPP aInstance);
+  ~nsPluginInstance();
+
+  NPBool init(NPWindow* aWindow);
+  void shut();
+  NPBool isInitialized();
+  NPError SetWindow(NPWindow* pNPWindow);
+  uint16  HandleEvent(void* event);
+
+  // locals
+  const char * getVersion();
+  void DoDraw();
+  NPWindow * mWindow;
+
+private:
+  NPBool StartDraw(NPWindow* window);
+  void EndDraw(NPWindow* window);
+
+  NPP mInstance;
+  NPBool mInitialized;
+};
+
+#endif // __PLUGIN_H__
