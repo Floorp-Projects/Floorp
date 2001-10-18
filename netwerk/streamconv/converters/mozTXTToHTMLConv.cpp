0ceaca6f0dd71499357fb7892e8e6db7f56db2f6
dbaron%fas.harvard.edu <dbaron%fas.harvard.edu>
2001-10-18 03:21:31 +0000

    Correct incorrect bustage fix that was causing some odd text substitutions in mail messages.  r=jag  sr=alecf, hyatt  b=104693

--- netwerk/streamconv/converters/mozTXTToHTMLConv.cpp
+++ netwerk/streamconv/converters/mozTXTToHTMLConv.cpp
@@ -542,7 +542,7 @@ mozTXTToHTMLConv::ItMatchesDelimited(const PRUnichar * aInString,
           nsCRT::IsAsciiDigit(textAfterPos) ||
           textAfterPos == *rep
         ) ||
-        !(before == !Compare(nsDependentString(aInString + (LT_IGNORE ? 0 : 1) ), nsDependentString(rep, aRepLen), nsCaseInsensitiveStringComparator()))
+        Compare(nsDependentString(aInString + (before == LT_IGNORE ? 0 : 1) ), nsDependentString(rep, aRepLen), nsCaseInsensitiveStringComparator())
     )
     return PR_FALSE;
 
