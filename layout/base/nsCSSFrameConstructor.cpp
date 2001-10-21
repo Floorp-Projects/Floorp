9168581312e5c1a6255fa4bc198bfb8fa27a49d0
jaggernaut@netscape.com <jaggernaut@netscape.com>
2001-10-21 08:35:34 +0000

    Bug 89998: move nsTitledBoxFrame.cpp to nsGroupBoxFrame.cpp (and the classnames in it) as the final step in this XUL 1.0 bug. r=jag,FrodoB.

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -253,7 +253,7 @@ nsresult
 NS_NewTextBoxFrame ( nsIPresShell* aPresShell, nsIFrame** aNewFrame );
 
 nsresult
-NS_NewTitledBoxFrame ( nsIPresShell* aPresShell, nsIFrame** aNewFrame );
+NS_NewGroupBoxFrame ( nsIPresShell* aPresShell, nsIFrame** aNewFrame );
 
 nsresult
 NS_NewButtonBoxFrame ( nsIPresShell* aPresShell, nsIFrame** aNewFrame);
@@ -5496,7 +5496,7 @@ nsCSSFrameConstructor::ConstructXULFrame(nsIPresShell*            aPresShell,
       }
       // End of DECK CONSTRUCTION logic
       else if (display->mDisplay == NS_STYLE_DISPLAY_GROUPBOX) {
-        rv = NS_NewTitledBoxFrame(aPresShell, &newFrame);
+        rv = NS_NewGroupBoxFrame(aPresShell, &newFrame);
         processChildren = PR_TRUE;
         isReplaced = PR_TRUE;
 
