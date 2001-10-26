5acdd1e260f0eec1265b6ca053f51008bd345ccf
mike+mozilla@meer.net <mike+mozilla@meer.net>
2001-10-26 09:38:43 +0000

    Initial checkin of build file for Darwin1.4.mk.  It just includes Darwin1.3.mk.  Because I happened to be fooling around with an OS X system.

---
+++ js/src/config/Darwin1.4.mk
@@ -0,0 +1,37 @@
+# -*- Mode: makefile -*-
+#
+# The contents of this file are subject to the Netscape Public
+# License Version 1.1 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at http://www.mozilla.org/NPL/
+#
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express oqr
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
+#
+# The Original Code is Mozilla Communicator client code, released
+# March 31, 1998.
+#
+# The Initial Developer of the Original Code is Netscape
+# Communications Corporation.  Portions created by Netscape are
+# Copyright (C) 1998 Netscape Communications Corporation. All
+# Rights Reserved.
+#
+# Contributor(s): 
+#
+#  Mike McCabe <mike+mozilla@meer.net>
+#
+# Alternatively, the contents of this file may be used under the
+# terms of the GNU Public License (the "GPL"), in which case the
+# provisions of the GPL are applicable instead of those above.
+# If you wish to allow use of your version of this file only
+# under the terms of the GPL and not to allow others to use your
+# version of this file under the NPL, indicate your decision by
+# deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL.  If you do not delete
+# the provisions above, a recipient may use your version of this
+# file under either the NPL or the GPL.
+#
+
+include $(DEPTH)/config/Darwin1.3.mk
