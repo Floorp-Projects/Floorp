e03c6793022009fd82ddf80453f949e059b09bfc
dougt%netscape.com <dougt%netscape.com>
2001-10-30 22:03:05 +0000

    Moving nsISimpleEnumeratory interface into a seperate idl file. bug 99136. r=alecf@netscape.com, sr=rpotts@netscape.com

--- xpcom/tests/TestShutdown.cpp
+++ xpcom/tests/TestShutdown.cpp
@@ -71,6 +71,9 @@ void main(int argc, char* argv[])
         else {
             printf("Failed to create %s (%x)\n", cidStr, rv);
         }
+
+        nsComponentManager::FreeLibraries();
+        
     }
 
     rv = NS_ShutdownXPCOM(servMgr);
