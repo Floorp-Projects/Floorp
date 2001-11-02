0718a3a4dee4f38c04ea1b49dd5cc09af69e19d4
pschwartau@netscape.com <pschwartau@netscape.com>
2001-11-02 02:17:57 +0000

    Improving readability.

--- js/tests/js1_5/String/regress-107771.js
+++ js/tests/js1_5/String/regress-107771.js
@@ -38,36 +38,36 @@ var str = '';
 var k = -9999;
 
 
+status = inSection(1);
 str = "AAA//BBB/CCC/";
 k = str.lastIndexOf('/');
-status = inSection(1);
 actual = k;
 expect = 12;
 
+status = inSection(2);
 str = str.substring(0, k);
 k = str.lastIndexOf('/');
-status = inSection(2);
 actual = k;
 expect = 8;
 addThis();
 
+status = inSection(3);
 str = str.substring(0, k);
 k = str.lastIndexOf('/');
-status = inSection(3);
 actual = k;
 expect = 4;
 addThis();
 
+status = inSection(4);
 str = str.substring(0, k);
 k = str.lastIndexOf('/');
-status = inSection(4);
 actual = k;
 expect = 3;
 addThis();
 
+status = inSection(5);
 str = str.substring(0, k);
 k = str.lastIndexOf('/');
-status = inSection(5);
 actual = k;
 expect = -1;
 addThis();
