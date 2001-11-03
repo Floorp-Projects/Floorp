ad87ffac202145f2f97164345dc355bdd3def568
rods@netscape.com <rods@netscape.com>
2001-11-03 13:00:40 +0000

    Fixed up nsIPrintSettings interface and add a Get/Set method to nsIPrintOptions (only the setter is implemented at this time) Bug 107399 r=kmcclusk sr=alexf/attinasi

--- gfx/src/nsPrintOptionsImpl.h
+++ gfx/src/nsPrintOptionsImpl.h
@@ -26,6 +26,7 @@
 #include "nsIPrintOptions.h"  
 
 class nsIPref;
+class nsIPrintSettings;
 
 //*****************************************************************************
 //***    nsPrintOptions
