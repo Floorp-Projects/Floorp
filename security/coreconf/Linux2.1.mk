f4bc0d165ce9283103947979209cd4364f95bebf
ian.mcgreer@sun.com <ian.mcgreer@sun.com>
2001-11-07 20:58:09 +0000

    fix linux 2.1 also

--- security/coreconf/Linux2.1.mk
+++ security/coreconf/Linux2.1.mk
@@ -40,5 +40,10 @@ ifeq ($(OS_RELEASE),2.1)
         ifdef BUILD_OPT
                 OPTIMIZER       = -O2
         endif
+	ifdef MAPFILE
+		MKSHLIB += -Wl,--version-script,$(MAPFILE)
+	endif
+	PROCESS_MAP_FILE = grep -v ';-' $(LIBRARY_NAME).def | \
+       	 sed -e 's,;+,,' -e 's; DATA ;;' -e 's,;;,,' -e 's,;.*,;,' > $@
 endif
 
