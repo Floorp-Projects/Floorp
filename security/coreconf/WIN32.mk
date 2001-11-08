9e592855e538562a9b7a7bd2e6782843fefb5acf
ian.mcgreer%sun.com <ian.mcgreer%sun.com>
2001-11-08 16:20:54 +0000

    use nsinstall not copy on windows

--- security/coreconf/WIN32.mk
+++ security/coreconf/WIN32.mk
@@ -95,7 +95,7 @@ ifdef MAPFILE
 # Add LD options to restrict exported symbols to those in the map file
 endif
 # Change PROCESS to put the mapfile in the correct format for this platform
-PROCESS_MAP_FILE = copy $(LIBRARY_NAME).def $@
+PROCESS_MAP_FILE = $(INSTALL) -m 444 $(LIBRARY_NAME).def $@
 
 
 #
