9ae226ea813706e1e36ade77cfb9c83b6e597639
law%netscape.com <law%netscape.com>
2001-11-10 02:40:13 +0000

    New tools for measuring startup with a minimalist 'Navigator'

---
+++ tools/performance/startup/bigNav.bat
@@ -0,0 +1,8 @@
+@mozilla.exe -P "Default User" -url chrome://navigator/content/quit.html
+@mozilla.exe -P "Default User" -url chrome://navigator/content/quit.html
+@grep main1 "%NS_TIMELINE_LOG_FILE%"
+@mozilla.exe -P "Default User" -url chrome://navigator/content/quit.html
+@grep main1 "%NS_TIMELINE_LOG_FILE%"
+@mozilla.exe -P "Default User" -url chrome://navigator/content/quit.html
+@grep main1 "%NS_TIMELINE_LOG_FILE%"
+@copy "%NS_TIMELINE_LOG_FILE%" bigNav.log
