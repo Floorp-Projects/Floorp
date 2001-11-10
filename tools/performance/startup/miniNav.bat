809d966a348c61a3568468c65e2b60c6bf1b791f
law@netscape.com <law@netscape.com>
2001-11-10 02:40:13 +0000

    New tools for measuring startup with a minimalist 'Navigator'

---
+++ tools/performance/startup/miniNav.bat
@@ -0,0 +1,8 @@
+@mozilla.exe -P "Default User" -chrome chrome://navigator/content/miniNav.xul
+@mozilla.exe -P "Default User" -chrome chrome://navigator/content/miniNav.xul
+@grep main1 "%NS_TIMELINE_LOG_FILE%"
+@mozilla.exe -P "Default User" -chrome chrome://navigator/content/miniNav.xul
+@grep main1 "%NS_TIMELINE_LOG_FILE%"
+@mozilla.exe -P "Default User" -chrome chrome://navigator/content/miniNav.xul
+@grep main1 "%NS_TIMELINE_LOG_FILE%"
+@copy "%NS_TIMELINE_LOG_FILE%" miniNav.log
