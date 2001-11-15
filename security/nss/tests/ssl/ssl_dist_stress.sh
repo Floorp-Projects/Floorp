4a8b2ae277e71a35103955188bfbc2466fb45499
sonja.mirtitsch%sun.com <sonja.mirtitsch%sun.com>
2001-11-15 23:27:20 +0000

    modernized... changed servers, clients, mastebuild, 10 minutes wait now

--- security/nss/tests/ssl/ssl_dist_stress.sh
+++ security/nss/tests/ssl/ssl_dist_stress.sh
@@ -243,8 +243,9 @@ ssl_ds_dist_stress()
                trex-10
                compaqtor-10"
 
-  #clientlist=" box-2 washer-5" #FIXME ADJUST
-  clientlist="  box-200 washer-200"
+  #clientlist=" huey-2 dewey-2 hornet-2 shabadoo-2" #FIXME ADJUST
+  clientlist="  box-200 washer-200 huey-200 dewey-200 hornet-200 shabadoo-200 "
+  #clientlist="washer-200 huey-200 dewey-200 hornet-200 "
 
   html_head "SSL Distributed Stress Test"
 
@@ -255,7 +256,7 @@ ssl_ds_dist_stress()
   if [ -z "CD_QADIR_SSL" ] ; then
       CD_QADIR_SSL="cd $QADIR/ssl"
   else
-      cp -r $HOSTDIR $HOSTDIR/../../../../../y2sun2_Solaris8/mozilla/tests_results/security
+      cp -r $HOSTDIR $HOSTDIR/../../../../../booboo_Solaris8/mozilla/tests_results/security
   fi
 
   #sparam=" -t 128 -D -r "
@@ -284,7 +285,7 @@ ssl_ds_dist_stress()
   echo cd "${CLIENTDIR}"
   cd "${CLIENTDIR}"
 
-  sleep 300 # give the clients time to finish #FIXME ADJUST
+  sleep 600 # give the clients time to finish #FIXME ADJUST
  
   echo "GET /stop HTTP/1.0\n\n" > stdin.txt #check to make sure it has /r/n
   echo "tstclnt -h $HOSTADDR -p  8443 -d ${CLIENTDIR} -n TestUser0 "
