a4ce4d18984595d0f36387de0b69d5449d46f0c4
bbaetz%cs.mcgill.ca <bbaetz%cs.mcgill.ca>
2001-11-17 07:19:46 +0000

    Bug 101560 - clear some more env vars so that system calls will run in taint mode. The perforce integration stuff needs this.
    r=gerv,mattyt

--- webtools/bugzilla/globals.pl
+++ webtools/bugzilla/globals.pl
@@ -74,8 +74,7 @@ use Date::Parse;               # For str2time().
 use RelationSet;
 
 # Some environment variables are not taint safe
-delete $ENV{PATH};
-delete $ENV{BASH_ENV};
+delete $ENV{qw(PATH IFS CDPATH ENV BASH_ENV)};
 
 # Contains the version string for the current running Bugzilla.
 $::param{'version'} = '2.15';
