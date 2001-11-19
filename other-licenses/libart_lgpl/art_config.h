25d77eb313408db3f93606b3e187a0d6fa72656e
bbaetz%cs.mcgill.ca <bbaetz%cs.mcgill.ca>
2001-11-19 22:12:29 +0000

    Don't use gen_art_config - supply art_config.h instead (made using nspr). Makes the build stuff easier.

---
+++ other-licenses/libart_lgpl/art_config.h
@@ -0,0 +1,12 @@
+/* This isn't pregenerated - use nspr, instead */
+
+#include "prtypes.h"
+
+#define ART_SIZEOF_CHAR PR_BYTES_PER_BYTE
+#define ART_SIZEOF_SHORT PR_BYTES_PER_SHORT
+#define ART_SIZEOF_INT PR_BYTES_PER_ING
+#define ART_SIZEOF_LONG PR_BYTES_PER_LONG
+
+typedef PRUint8 art_u8;
+typedef PRUint16 art_u16;
+typedef PRUint32 art_u32;
