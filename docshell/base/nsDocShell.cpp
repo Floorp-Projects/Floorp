9cc9e43e59e35f57008197a50995d782aa65c67f
jst%netscape.com <jst%netscape.com>
2001-11-28 06:09:40 +0000

    nsDependentString(nsnull) is illegal and asserts. sr=rpotts@netscape.com

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -898,7 +898,7 @@ nsresult nsDocShell::FindTarget(const PRUnichar *aWindowTarget,
 
         rv = parentWindow->Open(NS_LITERAL_STRING(""),    // URL to load
                                 name,                     // Window name
-                                nsDependentString(nsnull),  // Window features
+                                NS_LITERAL_STRING(""),    // Window features
                                 getter_AddRefs(newWindow));
         if (NS_FAILED(rv)) return rv;
 
