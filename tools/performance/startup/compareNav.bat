d3444a0a9270b3b055ae5fd8d4db7c1ef24762b9
law@netscape.com <law@netscape.com>
2001-12-01 03:54:14 +0000

    Enhance compareNav so it handles jar/nojar configurations

--- tools/performance/startup/compareNav.bat
+++ tools/performance/startup/compareNav.bat
@@ -24,6 +24,8 @@
 @echo ----- %1/%profile1% -----
 @rem For each *.jar.<a>, save *.jar and replace it.
 @for %%x in ( chrome\*.jar ) do @if exist %%x.%1 (copy %%x %%x.save >nul & copy %%x.%1 %%x >nul)
+@rem Do likewise for these special files: installed-chrome.txt chrome.rdf
+@for %%x in ( chrome\installed-chrome.txt chrome\chrome.rdf ) do @if exist %%x.%1 (copy %%x %%x.save >nul & copy %%x.%1 %%x >nul)
 @%prog% -P "%profile1%" -url chrome://navigator/content/quit.html > nul
 @%prog% -P "%profile1%" -url chrome://navigator/content/quit.html > nul
 @grep \"Navigator Window visible now" "%NS_TIMELINE_LOG_FILE%"
@@ -34,10 +36,14 @@
 @copy "%NS_TIMELINE_LOG_FILE%" "%1.%profile1%.log" > nul
 @rem Restore *.jar files.
 @for %%x in ( chrome\*.jar ) do @if exist %%x.%1 @(copy %%x.save %%x >nul & del %%x.save)
+@rem Restore special files.
+@for %%x in ( chrome\installed-chrome.txt chrome\chrome.rdf ) do @if exist %%x.%1 @(copy %%x.save %%x >nul & del %%x.save)
 :skip1
 @echo ----- %2/%profile2% -----
 @rem For each *.jar.<b>, save *.jar and replace it.
 @for %%x in ( chrome\*.jar ) do @if exist %%x.%2 @(copy %%x %%x.save >nul & copy %%x.%2 %%x >nul)
+@rem Do likewise for these special files: installed-chrome.txt chrome.rdf
+@for %%x in ( chrome\installed-chrome.txt chrome\chrome.rdf ) do @if exist %%x.%2 (copy %%x %%x.save >nul & copy %%x.%2 %%x >nul)
 @%prog% -P "%profile2%" -url chrome://navigator/content/quit.html > nul
 @%prog% -P "%profile2%" -url chrome://navigator/content/quit.html > nul
 @grep \"Navigator Window visible now" "%NS_TIMELINE_LOG_FILE%"
@@ -48,11 +54,10 @@
 @copy "%NS_TIMELINE_LOG_FILE%" "%2.%profile2%.log" > nul
 @rem Restore *.jar files.
 @for %%x in ( chrome\*.jar ) do @if exist %%x.%2 @(copy %%x.save %%x >nul & del %%x.save)
+@rem Restore special files.
+@for %%x in ( chrome\installed-chrome.txt chrome\chrome.rdf ) do @if exist %%x.%2 @(copy %%x.save %%x >nul & del %%x.save)
 :skip2
-@copy chrome\comm.jar.save chrome\comm.jar > nul
-@copy chrome\modern.jar.save chrome\modern.jar > nul
-@copy chrome\classic.jar.save chrome\classic.jar > nul
-@goto done
+@goto :done
 :error
 @echo Syntax: compareNav ^<a^> ^<b^> ^<profile1^> ^<profile2^>
 @echo     where ^<a^> and ^<b^> are flavors of comm.jar
@@ -60,10 +65,5 @@
 @echo     and   ^<profile1^> and ^<profile2^> are optional profile names
 @echo           to use; e.g., "classic" or "modern"; profile1 defaults to
 @echo           "Default User", profile2 defaults to profile1
-@goto done
-:errorMissing1
-@echo missing chrome\comm.jar.%1
-@goto done
-:errorMissing2
-@echo missing chrome\comm.jar.%2
+@goto :done
 :done
