a1cf6c3dbf8929add09b0a4ceb55ab66fd620f2e
mcafee%netscape.com <mcafee%netscape.com>
2001-12-05 03:32:29 +0000

    Updated tooltip comment to reflect xul test change

--- tools/tinderbox/build-seamonkey-util.pl
+++ tools/tinderbox/build-seamonkey-util.pl
@@ -20,7 +20,7 @@ use File::Basename; # for basename();
 use Config; # for $Config{sig_name} and $Config{sig_num}
 
 
-$::UtilsVersion = '$Revision: 1.102 $ ';
+$::UtilsVersion = '$Revision: 1.103 $ ';
 
 package TinderUtils;
 
@@ -955,7 +955,7 @@ sub run_all_tests {
 			$test_result = 'success';
 
 			print_log "TinderboxPrint:" .
-			  "<a title=\"Avg time to open 10 browser windows\" href=\"http://tegu.mozilla.org/graph/xulwinopen/query.cgi?tbox=" .
+			  "<a title=\"Best nav open time of 9 runs\" href=\"http://tegu.mozilla.org/graph/xulwinopen/query.cgi?tbox=" .
 				::hostname() . "&autoscale=0&days=0\">Txul:$open_time" . "ms</a>\n";
 
 			if($Settings::TestsPhoneHome) {
