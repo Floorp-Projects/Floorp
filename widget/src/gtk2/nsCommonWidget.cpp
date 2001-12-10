c1baa75a23b2a5573f88ed8523f1828a857df665
blizzard%redhat.com <blizzard%redhat.com>
2001-12-10 17:37:36 +0000

    Checkpointing changes.  Expose events, window destruction and window close are now handled.  Not part of the build.

---
+++ widget/src/gtk2/nsCommonWidget.cpp
@@ -0,0 +1,122 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code mozilla.org code.
+ *
+ * The Initial Developer of the Original Code Christopher Blizzard
+ * <blizzard@mozilla.org>.  Portions created by the Initial Developer
+ * are Copyright (C) 2001 the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#include "nsCommonWidget.h"
+
+nsCommonWidget::nsCommonWidget()
+{
+  mOnDestroyCalled = PR_FALSE;
+}
+
+nsCommonWidget::~nsCommonWidget()
+{
+}
+
+nsIWidget *
+nsCommonWidget::GetParent(void)
+{
+  nsIWidget *retval;
+  retval = mParent;
+  NS_IF_ADDREF(retval);
+  return retval;
+}
+
+void
+nsCommonWidget::CommonCreate(nsIWidget *aParent)
+{
+  mParent = aParent;
+}
+
+void
+nsCommonWidget::InitPaintEvent(nsPaintEvent &aEvent)
+{
+  memset(&aEvent, 0, sizeof(nsPaintEvent));
+  aEvent.eventStructType = NS_PAINT_EVENT;
+  aEvent.message = NS_PAINT;
+  aEvent.widget = NS_STATIC_CAST(nsIWidget *, this);
+}
+
+void nsCommonWidget::InitSizeEvent(nsSizeEvent &aEvent)
+{
+  memset(&aEvent, 0, sizeof(nsSizeEvent));
+  aEvent.eventStructType = NS_SIZE_EVENT;
+  aEvent.message = NS_SIZE;
+  aEvent.widget = NS_STATIC_CAST(nsIWidget *, this);
+}
+
+void nsCommonWidget::InitGUIEvent(nsGUIEvent &aEvent, PRUint32 aMsg)
+{
+  memset(&aEvent, 0, sizeof(nsGUIEvent));
+  aEvent.eventStructType = NS_GUI_EVENT;
+  aEvent.message = aMsg;
+  aEvent.widget = NS_STATIC_CAST(nsIWidget *, this);
+}
+
+NS_IMETHODIMP
+nsCommonWidget::DispatchEvent(nsGUIEvent *aEvent,
+			      nsEventStatus &aStatus)
+{
+  aStatus = nsEventStatus_eIgnore;
+
+  // send it to the standard callback
+  if (mEventCallback)
+    aStatus = (* mEventCallback)(aEvent);
+
+  // dispatch to event listener if event was not consumed
+  if ((aStatus != nsEventStatus_eIgnore) && mEventListener)
+    aStatus = mEventListener->ProcessEvent(*aEvent);
+
+  return NS_OK;
+}
+
+void
+nsCommonWidget::OnDestroy(void)
+{
+  if (mOnDestroyCalled)
+    return;
+
+  mOnDestroyCalled = PR_TRUE;
+
+  // release references to children, device context, toolkit + app shell
+  nsBaseWidget::OnDestroy();
+
+  nsCOMPtr<nsIWidget> kungFuDeathGrip = this;
+
+  nsGUIEvent event;
+  InitGUIEvent(event, NS_DESTROY);
+  
+  nsEventStatus status;
+  DispatchEvent(&event, status);
+}
--- widget/src/gtk2/nsWindow.cpp
+++ widget/src/gtk2/nsWindow.cpp
@@ -1,40 +1,65 @@
-/*
- * The contents of this file are subject to the Mozilla Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
- * 
- * The Original Code is mozilla.org code.
- * 
- * The Initial Developer of the Original Code is Christopher Blizzard.
- * Portions created by Christopher Blizzard are Copyright (C)
- * Christopher Blizzard.  All Rights Reserved.
- * 
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code mozilla.org code.
+ *
+ * The Initial Developer of the Original Code Christopher Blizzard
+ * <blizzard@mozilla.org>.  Portions created by the Initial Developer
+ * are Copyright (C) 2001 the Initial Developer. All Rights Reserved.
+ *
  * Contributor(s):
- *   Christopher Blizzard <blizzard@mozilla.org>
- */
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 #include "nsWindow.h"
 #include "nsToolkit.h"
+#include "nsIRenderingContext.h"
 
 #include <gtk/gtkwindow.h>
 #include <gdk/gdkx.h>
+
+static gboolean expose_event_cb   (GtkWidget *widget,
+				   GdkEventExpose *event);
+static gboolean configure_event_cb(GtkWidget *widget,
+				   GdkEventConfigure *event);
+static gboolean delete_event_cb   (GtkWidget *widget,
+				   GdkEventAny *event);
 
 nsWindow::nsWindow()
 {
   mContainer       = nsnull;
   mDrawingarea     = nsnull;
+  mShell           = nsnull;
   mIsTopLevel      = PR_FALSE;
+  mIsDestroyed     = PR_FALSE;
   mWindowType      = eWindowType_child;
 }
 
 nsWindow::~nsWindow()
 {
+  Destroy();
 }
 
 NS_IMETHODIMP
@@ -46,7 +71,7 @@ nsWindow::Create(nsIWidget        *aParent,
 		 nsIToolkit       *aToolkit,
 		 nsWidgetInitData *aInitData)
 {
-  return CommonCreate(aParent, nsnull, aRect, aHandleEventFunction,
+  return NativeCreate(aParent, nsnull, aRect, aHandleEventFunction,
 		      aContext, aAppShell, aToolkit, aInitData);
 }
 
@@ -59,23 +84,33 @@ nsWindow::Create(nsNativeWidget aParent,
 		 nsIToolkit       *aToolkit,
 		 nsWidgetInitData *aInitData)
 {
-  return CommonCreate(nsnull, aParent, aRect, aHandleEventFunction,
+  return NativeCreate(nsnull, aParent, aRect, aHandleEventFunction,
 		      aContext, aAppShell, aToolkit, aInitData);
 }
 
 NS_IMETHODIMP
 nsWindow::Destroy(void)
 {
-  return NS_ERROR_NOT_IMPLEMENTED; 
-}
+  if (mIsDestroyed)
+    return NS_OK;
 
-nsIWidget*
-nsWindow::GetParent(void)
-{
-  nsIWidget *retval;
-  retval = mParent;
-  NS_IF_ADDREF(retval);
-  return retval;
+  mIsDestroyed = PR_TRUE;
+
+  if (mShell) {
+    gtk_widget_destroy(mShell);
+    mShell = nsnull;
+  }
+
+  mContainer = nsnull;
+
+  if (mDrawingarea) {
+    g_object_unref(mDrawingarea);
+    mDrawingarea = nsnull;
+  }
+
+  OnDestroy();
+
+  return NS_OK;
 }
 
 NS_IMETHODIMP
@@ -83,8 +118,14 @@ nsWindow::Show(PRBool aState)
 {
   if (mIsTopLevel) {
     moz_drawingarea_set_visibility(mDrawingarea, aState);
-    gtk_widget_show(GTK_WIDGET(mContainer));
-    gtk_widget_show(mShell);
+    if (aState) {
+      gtk_widget_show(GTK_WIDGET(mContainer));
+      gtk_widget_show(mShell);
+    }
+    else {
+      gtk_widget_hide(GTK_WIDGET(mShell));
+      gtk_widget_hide(GTK_WIDGET(mContainer));
+    }
   }
   else {
     moz_drawingarea_set_visibility(mDrawingarea, aState);
@@ -124,7 +165,7 @@ nsWindow::Move(PRInt32 aX,
 
   // XXX do we need to handle the popup crud here?
   if (mIsTopLevel)
-    gtk_widget_set_uposition(mShell, aX, aY);
+    gtk_window_move(GTK_WINDOW(mShell), aX, aY);
   else
     moz_drawingarea_move(mDrawingarea, aX, aY);
   
@@ -139,12 +180,20 @@ nsWindow::Resize(PRInt32 aWidth,
   mBounds.width = aWidth;
   mBounds.height = aHeight;
 
-  if (mIsTopLevel) {
-    gtk_window_set_default_size(GTK_WINDOW(mShell), aWidth, aHeight);
-    gtk_widget_set_size_request(GTK_WIDGET(mContainer), aWidth, aHeight);
-  }
+  printf("nsWindow::Resize [%p] %d %d\n", (void *)this,
+	 aWidth, aHeight);
+
+  if (mIsTopLevel)
+    gtk_window_resize(GTK_WINDOW(mShell), aWidth, aHeight);
 
   moz_drawingarea_resize (mDrawingarea, aWidth, aHeight);
+
+  // synthesize a resize event if this isn't a toplevel
+  if (!mIsTopLevel) {
+    nsRect rect(mBounds.x, mBounds.y, aWidth, aHeight);
+    nsEventStatus status;
+    SendResizeEvent(rect, status);
+  }
 
   return NS_OK;
 }
@@ -161,14 +210,20 @@ nsWindow::Resize(PRInt32 aX,
   mBounds.width = aWidth;
   mBounds.height = aHeight;
 
+  printf("nsWindow::Resize [%p] %d %d %d %d\n", (void *)this,
+	 aX, aY, aWidth, aHeight);
+
   if (mIsTopLevel) {
-    gtk_window_set_default_size(GTK_WINDOW(mShell), aWidth, aHeight);
-    gtk_widget_set_uposition(mShell, aX, aY);
-    gtk_widget_set_size_request(GTK_WIDGET(mContainer), aWidth, aHeight);
+    gtk_window_move(GTK_WINDOW(mShell), aX, aY);
+    gtk_window_resize(GTK_WINDOW(mShell), aWidth, aHeight);
     moz_drawingarea_resize(mDrawingarea, aWidth, aHeight);
   }
   else {
     moz_drawingarea_move_resize(mDrawingarea, aX, aY, aWidth, aHeight);
+    // synthesize a resize event
+    nsRect rect(aX, aY, aWidth, aHeight);
+    nsEventStatus status;
+    SendResizeEvent(rect, status);
   }
   return NS_OK;
 }
@@ -327,7 +382,14 @@ nsWindow::SetBorderStyle(nsBorderStyle aBorderStyle)
 NS_IMETHODIMP
 nsWindow::SetTitle(const nsString& aTitle)
 {
-  return NS_ERROR_NOT_IMPLEMENTED;
+  if (!mShell)
+    return NS_OK;
+
+  // convert the string into utf8 and set the title.
+  NS_ConvertUCS2toUTF8 utf8title(aTitle);
+  gtk_window_set_title(GTK_WINDOW(mShell), (const char *)utf8title.get());
+
+  return NS_OK;
 }
 
 NS_IMETHODIMP
@@ -382,13 +444,6 @@ nsWindow::GetPreferredSize(PRInt32 &aWidth,
 NS_IMETHODIMP
 nsWindow::SetPreferredSize(PRInt32 aWidth,
 			   PRInt32 aHeight)
-{
-  return NS_ERROR_NOT_IMPLEMENTED;
-}
-
-NS_IMETHODIMP
-nsWindow::DispatchEvent(nsGUIEvent*    event,
-			nsEventStatus &aStatus)
 {
   return NS_ERROR_NOT_IMPLEMENTED;
 }
@@ -451,8 +506,93 @@ nsWindow::GetAttention()
   return NS_ERROR_NOT_IMPLEMENTED;
 }
 
+gboolean
+nsWindow::OnExposeEvent(GtkWidget *aWidget, GdkEventExpose *aEvent)
+{
+  // handle exposes for the inner window only
+  if (aEvent->window != mDrawingarea->inner_window)
+    return FALSE;
+
+  printf("sending expose event [%p] %d %d %d %d\n",
+	 (void *)this,
+	 aEvent->area.x, aEvent->area.y,
+	 aEvent->area.width, aEvent->area.height);
+
+  // ok, send out the paint event
+  // XXX figure out the region/rect stuff!
+  nsRect rect(aEvent->area.x, aEvent->area.y,
+	      aEvent->area.width, aEvent->area.height);
+  nsPaintEvent event;
+
+  InitPaintEvent(event);
+
+  event.point.x = aEvent->area.x;
+  event.point.y = aEvent->area.y;
+  event.rect = &rect;
+  // XXX fix this!
+  event.region = nsnull;
+  // XXX fix this!
+  event.renderingContext = GetRenderingContext();
+
+  nsEventStatus status;
+  DispatchEvent(&event, status);
+
+  NS_RELEASE(event.renderingContext);
+
+  // check the return value!
+  return TRUE;
+}
+
+gboolean
+nsWindow::OnConfigureEvent(GtkWidget *aWidget, GdkEventConfigure *aEvent)
+{
+  printf("configure event [%p] %p %d %d %d %d\n", (void *)this,
+	 (void *)aEvent->window,
+	 aEvent->x, aEvent->y, aEvent->width, aEvent->height);
+
+  nsRect rect(aEvent->x, aEvent->y, aEvent->width, aEvent->height);
+
+  // update the size of the drawing area
+  moz_drawingarea_resize (mDrawingarea, aEvent->width, aEvent->height);
+
+  nsEventStatus status;
+  SendResizeEvent(rect, status);
+
+  return FALSE;
+}
+
+void
+nsWindow::OnDeleteEvent(GtkWidget *aWidget, GdkEventAny *aEvent)
+{
+  nsGUIEvent event;
+
+  InitGUIEvent(event, NS_XUL_CLOSE);
+
+  event.point.x = 0;
+  event.point.y = 0;
+
+  nsEventStatus status;
+  DispatchEvent(&event, status);
+}
+
+void
+nsWindow::SendResizeEvent(nsRect &aRect, nsEventStatus &aStatus)
+{
+  nsSizeEvent event;
+  InitSizeEvent(event);
+
+  event.windowSize = &aRect;
+  event.point.x = aRect.x;
+  event.point.y = aRect.y;
+  event.mWinWidth = aRect.width;
+  event.mWinHeight = aRect.height;
+
+  nsEventStatus status;
+  DispatchEvent(&event, status); 
+}
+
 nsresult
-nsWindow::CommonCreate(nsIWidget        *aParent,
+nsWindow::NativeCreate(nsIWidget        *aParent,
 		       nsNativeWidget    aNativeParent,
 		       const nsRect     &aRect,
 		       EVENT_CALLBACK    aHandleEventFunction,
@@ -471,9 +611,9 @@ nsWindow::CommonCreate(nsIWidget        *aParent,
   // initialize all the common bits of this class
   BaseCreate(baseParent, aRect, aHandleEventFunction, aContext,
 	     aAppShell, aToolkit, aInitData);
-
-  // save a ref to our parent
-  mParent = aParent;
+  
+  // and do our common creation
+  CommonCreate(aParent);
 
   // save our bounds
   mBounds = aRect;
@@ -572,7 +712,87 @@ nsWindow::CommonCreate(nsIWidget        *aParent,
   if (mContainer)
     g_object_set_data(G_OBJECT(mContainer), "nsWindow", this);
 
+  if (mShell)
+    g_object_set_data(G_OBJECT(mShell), "nsWindow", this);
+
+  // attach listeners for events
+  if (mShell) {
+    g_signal_connect(G_OBJECT(mShell), "configure_event",
+		     G_CALLBACK(configure_event_cb), NULL);
+    g_signal_connect(G_OBJECT(mShell), "delete_event",
+		     G_CALLBACK(delete_event_cb), NULL);
+  }
+  if (mContainer) {
+    g_signal_connect(G_OBJECT(mContainer), "expose_event",
+		     G_CALLBACK(expose_event_cb), NULL);
+
+  }
+
+  printf("nsWindow [%p]\n", (void *)this);
+  if (mShell) 
+    printf("\tmShell %p %p %lx\n", (void *)mShell, (void *)mShell->window,
+	   GDK_WINDOW_XWINDOW(mShell->window));
+  if (mContainer)
+    printf("\tmContainer %p %p %lx\n", (void *)mContainer,
+	   (void *)GTK_WIDGET(mContainer)->window,
+	   GDK_WINDOW_XWINDOW(GTK_WIDGET(mContainer)->window));
+  if (mDrawingarea)
+    printf("\tmDrawingarea %p %p %p %lx %lx\n", (void *)mDrawingarea,
+	   (void *)mDrawingarea->clip_window,
+	   (void *)mDrawingarea->inner_window,
+	   GDK_WINDOW_XWINDOW(mDrawingarea->clip_window),
+	   GDK_WINDOW_XWINDOW(mDrawingarea->inner_window));
+
   return NS_OK;
+}
+
+// gtk callbacks
+
+/* static */
+gboolean expose_event_cb(GtkWidget *widget, GdkEventExpose *event)
+{
+  gpointer user_data;
+  user_data = g_object_get_data(G_OBJECT(event->window), "nsWindow");
+
+  if (!user_data)
+    return FALSE;
+
+  nsWindow *window = NS_STATIC_CAST(nsWindow *, user_data);
+
+  return window->OnExposeEvent(widget, event);
+}
+
+/* static */
+gboolean
+configure_event_cb(GtkWidget *widget,
+		   GdkEventConfigure *event)
+{
+  gpointer user_data;
+  user_data = g_object_get_data(G_OBJECT(widget), "nsWindow");
+
+  if (!user_data)
+    return FALSE;
+
+  nsWindow *window = NS_STATIC_CAST(nsWindow *, user_data);
+  
+  return window->OnConfigureEvent(widget, event);
+}
+
+/* static */
+gboolean
+delete_event_cb(GtkWidget *widget, GdkEventAny *event)
+{
+  gpointer user_data;
+  user_data = g_object_get_data(G_OBJECT(widget), "nsWindow");
+  
+  if (!user_data)
+    return FALSE;
+
+  nsWindow *window = NS_STATIC_CAST(nsWindow *, user_data);
+
+  window->OnDeleteEvent(widget, event);
+
+  return TRUE;
 }
 
 // nsChildWindow class
