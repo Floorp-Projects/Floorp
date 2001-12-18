9aebf62faf288a5f2db81532db3990b991b1a95b
timeless@mac.com <timeless@mac.com>
2001-12-18 13:40:06 +0000

    Bug 115594 Support .CUR files patch by cbiesinger@web.de r=pavlov sr=tor

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -106,7 +106,7 @@ static nsDefaultMimeTypeEntry defaultMimeEntries [] =
   { IMAGE_JPG, "jpeg,jpg", "JPEG Image", 0, 0 },
   { IMAGE_PNG, "png", "PNG Image", 0, 0 },
   { IMAGE_BMP, "bmp", "BMP Image", 0, 0 },
-  { IMAGE_ICO, "ico", "ICO Image", 0, 0 },
+  { IMAGE_ICO, "ico,cur", "ICO Image", 0, 0 },
   { IMAGE_MNG, "mng", "MNG Image", 0, 0 },
   { IMAGE_JNG, "jng", "JNG Image", 0, 0 },
   { APPLICATION_XPINSTALL, "xpi", "XPInstall Install", 'xpi*','MOSS' },
