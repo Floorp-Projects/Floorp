0bd35411c2bcbe46cfcb4b725c32834d17c92102
wtc%netscape.com <wtc%netscape.com>
2001-12-18 03:49:31 +0000

    Updated the test for the NSPR 4.2 release.

--- nsprpub/pr/tests/vercheck.c
+++ nsprpub/pr/tests/vercheck.c
@@ -49,12 +49,12 @@
 #include <stdlib.h>
 
 /*
- * This release (4.1) is backward compatible with the
- * 4.0.x releases.  It, of course, is compatible with
- * itself.
+ * This release (4.2) is backward compatible with the
+ * 4.0.x and 4.1.x releases.  It, of course, is compatible
+ * with itself.
  */
 static char *compatible_version[] = {
-    "4.0", "4.0.1", "4.0.2", PR_VERSION
+    "4.0", "4.0.1", "4.1", "4.1.1", "4.1.2", "4.1.3", PR_VERSION
 };
 
 /*
@@ -69,8 +69,8 @@ static char *incompatible_version[] = {
     "3.0", "3.0.1",
     "3.1", "3.1.1", "3.1.2", "3.1.3",
     "3.5", "3.5.1",
-    "4.1.3",
-    "4.2", "4.2.1",
+    "4.2.3",
+    "4.3", "4.3.1",
     "10.0", "11.1", "12.14.20"
 };
 
