69db99bf055ac9b0f88b31773d3e75512c20e2ba
seawood@netscape.com <seawood@netscape.com>
2002-01-01 00:44:07 +0000

    Recent versions of freebsd have a threadsafe poll. Use sched_get_priority_* to get actual priority min & max rather than hardcoding defaults. Patches from freebsd ports system. Bug #112987 r=rjesup@wgate.com

--- nsprpub/pr/include/md/_freebsd.h
+++ nsprpub/pr/include/md/_freebsd.h
@@ -37,6 +37,7 @@
 
 #include "prthread.h"
 
+#include <sys/param.h>
 #include <sys/syscall.h>
 
 #define PR_LINKER_ARCH	"freebsd"
@@ -65,7 +66,7 @@
 #define _PR_HAVE_SOCKADDR_LEN
 #define _PR_STAT_HAS_ST_ATIMESPEC
 #define _PR_NO_LARGE_FILES
-#if ( __FreeBSD__ > 2 )
+#if ( __FreeBSD_version >= 220000 ) && ( __FreeBSD_version < 400008 )
 #if !defined(_PR_PTHREADS)
 /*
  * libc_r doesn't have poll().  Although libc has poll(), it is not
