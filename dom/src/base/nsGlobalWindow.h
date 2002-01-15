55fa92e68ba3c5feb96a5a10b9c51d18109dd273
rods@netscape.com <rods@netscape.com>
2002-01-15 01:11:16 +0000

    added printWithSettings method for printing with PrintSettings Bug 118352 r=dcone sr=jst

--- dom/src/base/nsGlobalWindow.h
+++ dom/src/base/nsGlobalWindow.h
@@ -237,7 +237,7 @@ protected:
   nsresult CheckSecurityWidthAndHeight(PRInt32* width, PRInt32* height);
   nsresult CheckSecurityLeftAndTop(PRInt32* left, PRInt32* top);
 
-  nsresult DoPrint(PRBool aDoPreview);
+  nsresult DoPrint(PRBool aDoPreview, nsIPrintSettings* aPS = nsnull);
 
   // Helper for window.find()
   nsresult FindInternal(nsAReadableString& aStr, PRBool caseSensitive,
