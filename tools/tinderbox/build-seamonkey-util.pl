f4694b34f9158e5e68676362eacc76b9c5deabe7
mcafee%netscape.com <mcafee%netscape.com>
2002-01-16 02:50:15 +0000

    Oops, forgot about the 2x try on layout, adding parameter for other url

--- tools/tinderbox/build-seamonkey-util.pl
+++ tools/tinderbox/build-seamonkey-util.pl
@@ -21,7 +21,7 @@ use File::Basename; # for basename();
 use Config; # for $Config{sig_name} and $Config{sig_num}
 
 
-$::UtilsVersion = '$Revision: 1.137 $ ';
+$::UtilsVersion = '$Revision: 1.138 $ ';
 
 package TinderUtils;
 
@@ -1024,7 +1024,7 @@ sub run_all_tests {
       # load tests instead of hard-coding to localhost.
       $test_result =
         FileBasedTest("LayoutPerformanceTest", $build_dir, $binary_dir,
-                      $binary . " -P $Settings::MozProfileName \"http://localhost/page-loader/loader.pl?delay=1000&nocache=0&maxcyc=4&timeout=15000&auto=1\"",
+                      $binary . " -P $Settings::MozProfileName \"http://localhost/page-loader/loader.pl?delay=1000&nocache=0&maxcyc=4&timeout=$Settings::LayoutPerformanceTestPageTimeout&auto=1\"",
                       $Settings::LayoutPerformanceTestTimeout,
                       "_x_x_mozilla_page_load", 1, 0);
       
