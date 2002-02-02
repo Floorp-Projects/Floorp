d404d26bad0c536e4b436ca485945e9b6e467691
darin@netscape.com <darin@netscape.com>
2002-02-02 02:45:25 +0000

    fixes bug 86917 "socket transport should try all ip addresses returned by DNS service" r=gordon, sr=rpotts

--- netwerk/base/src/nsSocketTransport.cpp
+++ netwerk/base/src/nsSocketTransport.cpp
@@ -143,6 +143,7 @@ nsSocketTransport::nsSocketTransport():
     mHostName(nsnull),
     mPort(-1),
     mMonitor(nsnull),
+    mNetAddress(nsnull),
     mOperation(eSocketOperation_None),
     mProxyPort(-1),
     mProxyHost(nsnull),
@@ -181,12 +182,6 @@ nsSocketTransport::nsSocketTransport():
     
     SetReadType (eSocketRead_None);
     SetWriteType(eSocketWrite_None);
-    
-    //
-    // Set up Internet defaults...
-    //
-    memset(&mNetAddress, 0, sizeof(mNetAddress));
-    PR_SetNetAddr(PR_IpAddrAny, PR_AF_INET6, 0, &mNetAddress);
     
     //
     // Initialize the global connect timeout value if necessary...
@@ -512,6 +507,18 @@ nsresult nsSocketTransport::Process(PRInt16 aSelectFlags)
                 mHostName, mPort, this));
             mStatus = doConnection(aSelectFlags);
 
+            // on connection failure, reuse next address if one exists
+            if (mStatus == NS_ERROR_CONNECTION_REFUSED) {
+                mNetAddress = mNetAddrList.GetNext(mNetAddress);
+                if (mNetAddress) {
+                    LOG(("connection failed... trying next address\n"));
+                    PR_Close(mSocketFD);
+                    mSocketFD = nsnull;
+                    // remain in this state
+                    mStatus = NS_BASE_STREAM_WOULD_BLOCK;
+                }
+            }
+
             // Send status message
             OnStatus_Locked(NS_NET_STATUS_CONNECTING_TO);
             break;
@@ -641,7 +648,7 @@ nsresult nsSocketTransport::doResolveHost(void)
 
     NS_ASSERTION(eSocketState_WaitDNS == mCurrentState, "Wrong state.");
 
-    LOG(("nsSocketTransport: Entering doResolveHost() [host=%s:%d this=%x].\n", 
+    LOG(("nsSocketTransport: Entering doResolveHost() [host=%s:%d %x].\n", 
         mHostName, mPort, this));
 
     // 
@@ -653,15 +660,7 @@ nsresult nsSocketTransport::doResolveHost(void)
     //
     // The hostname has not been resolved yet...
     //
-    if (PR_IsNetAddrType(&mNetAddress, PR_IpAddrAny)) {
-        //
-        // Initialize the port used for the connection...
-        //
-        // XXX: The list of ports must be restricted - see net_bad_ports_table[] in 
-        //      mozilla/network/main/mkconect.c
-        //
-        mNetAddress.ipv6.port = PR_htons(((mProxyPort != -1 && !mProxyTransparent) ? mProxyPort : mPort));
-
+    if (mNetAddress == nsnull) {
         PR_ExitMonitor(mMonitor);
 
         nsIDNSService* pDNSService = mService->GetCachedDNSService();
@@ -673,9 +672,7 @@ nsresult nsSocketTransport::doResolveHost(void)
         // Give up the SocketTransport lock.  This allows the DNS thread to call the
         // nsIDNSListener notifications without blocking...
         //
-        rv = pDNSService->Lookup((mProxyHost && !mProxyTransparent) ? mProxyHost : mHostName, 
-                                 this, 
-                                 nsnull, 
+        rv = pDNSService->Lookup(GetSocketHost(), this, nsnull, 
                                  getter_AddRefs(mDNSRequest));
         //
         // Aquire the SocketTransport lock again...
@@ -686,7 +683,7 @@ nsresult nsSocketTransport::doResolveHost(void)
             //
             // The DNS lookup has finished...  It has either failed or succeeded.
             //
-            if (NS_FAILED(mStatus) || !PR_IsNetAddrType(&mNetAddress, PR_IpAddrAny)) {
+            if (NS_FAILED(mStatus) || mNetAddress) {
                 mDNSRequest = 0;
                 rv = mStatus;
             } 
@@ -701,10 +698,8 @@ nsresult nsSocketTransport::doResolveHost(void)
         }
     }
 
-    LOG(("nsSocketTransport: Leaving doResolveHost() [%s:%d %x].\t"
-        "rv = %x.\n\n",
+    LOG(("nsSocketTransport: Leaving doResolveHost() [%s:%d %x] rv = %x\n",
         mHostName, mPort, this, rv));
-
     return rv;
 }
 
@@ -846,7 +841,7 @@ nsresult nsSocketTransport::doConnection(PRInt16 aSelectFlags)
         //    This is only done the first time doConnection(...) is called.
         //
         if (NS_SUCCEEDED(rv)) {
-            status = PR_Connect(mSocketFD, &mNetAddress, gConnectTimeout);
+            status = PR_Connect(mSocketFD, mNetAddress, gConnectTimeout);
             if (PR_SUCCESS != status) {
                 PRErrorCode code = PR_GetError();
                 //
@@ -946,14 +941,17 @@ nsSocketTransport::doBlockingConnection()
     //
     // The hostname has not been resolved yet...
     //
-    if (PR_IsNetAddrType(&mNetAddress, PR_IpAddrAny)) {
+    if (mNetAddress == nsnull) {
         nsIDNSService* pDNSService = mService->GetCachedDNSService();
         if (!pDNSService) {
             return NS_ERROR_UNEXPECTED;
         }
 
         nsXPIDLCString result;
-        const char *host = (mProxyHost && !mProxyTransparent) ? mProxyHost : mHostName;
+        const char *host = GetSocketHost();
+
+        // XXX DNS service does not provide a synchronous resolver that returns
+        // the entire nsHostEnt struct.
         rv = pDNSService->Resolve(host, getter_Copies(result));
         if (NS_FAILED(rv)) return rv;
 
@@ -967,16 +965,18 @@ nsSocketTransport::doBlockingConnection()
             return NS_ERROR_FAILURE;
         }
 
-        if (addr.raw.family == PR_AF_INET)
-            PR_ConvertIPv4AddrToIPv6(addr.inet.ip, &mNetAddress.ipv6.ip);
-        else
-            memcpy(&mNetAddress.ipv6.ip, &addr.ipv6.ip, sizeof(mNetAddress.ipv6.ip));
+        mNetAddrList.Init(1);
+        mNetAddress = mNetAddrList.GetNext(nsnull);
 
-        mNetAddress.ipv6.port
-            = PR_htons(((mProxyPort != -1 && !mProxyTransparent) ? mProxyPort : mPort));
+        PR_SetNetAddr(PR_IpAddrAny, PR_AF_INET6, GetSocketPort(), mNetAddress);
+
+        if (addr.raw.family == PR_AF_INET)
+            PR_ConvertIPv4AddrToIPv6(addr.inet.ip, &mNetAddress->ipv6.ip);
+        else
+            memcpy(&mNetAddress->ipv6.ip, &addr.ipv6.ip, sizeof(mNetAddress->ipv6.ip));
 
         LOG(("address { family=%hu, port=%hu }\n",
-            mNetAddress.ipv6.family, PR_ntohs(mNetAddress.ipv6.port)));
+            mNetAddress->ipv6.family, PR_ntohs(mNetAddress->ipv6.port)));
     }
 
     //
@@ -1497,23 +1497,44 @@ nsSocketTransport::OnFound(nsISupports *aContext,
     nsAutoMonitor mon(mMonitor);
     nsresult rv = NS_OK;
 
-    if (aHostEnt->hostEnt.h_addr_list && aHostEnt->hostEnt.h_addr_list[0]) {
-        if (aHostEnt->hostEnt.h_addrtype == PR_AF_INET6)
-            memcpy(&mNetAddress.ipv6.ip, aHostEnt->hostEnt.h_addr_list[0], sizeof(mNetAddress.ipv6.ip));
-        else
-            PR_ConvertIPv4AddrToIPv6(*(PRUint32*)aHostEnt->hostEnt.h_addr_list[0], &mNetAddress.ipv6.ip);
+    char **addrList = aHostEnt->hostEnt.h_addr_list;
+    
+    if (addrList && addrList[0]) {
+        PRUint32 len = 0;
+
+        LOG(("nsSocketTransport::OnFound [%s:%d %x] lookup succeeded [FQDN=%s]\n",
+            mHostName, mPort, this, aHostEnt->hostEnt.h_name));
+
+        // determine the number of address in the list
+        for (; *addrList; ++addrList)
+            ++len;
+        addrList -= len;
+
+        // allocate space for the addresses
+        mNetAddrList.Init(len);
+
+        // populate the address list
+        PRNetAddr *addr = nsnull;
+        while ((addr = mNetAddrList.GetNext(addr)) != nsnull) {
+            PR_SetNetAddr(PR_IpAddrAny, PR_AF_INET6, GetSocketPort(), addr);
+            if (aHostEnt->hostEnt.h_addrtype == PR_AF_INET6)
+                memcpy(&addr->ipv6.ip, *addrList, sizeof(addr->ipv6.ip));
+            else
+                PR_ConvertIPv4AddrToIPv6(*(PRUint32 *)(*addrList), &addr->ipv6.ip);
+            ++addrList;
 #if defined(PR_LOGGING)
-        char addrbuf[50];
-        PR_NetAddrToString(&mNetAddress, addrbuf, sizeof(addrbuf));
-        LOG(("nsSocketTransport: OnFound(...) [%s:%d %x]."
-            "  DNS lookup succeeded => %s (%s)\n",
-            mHostName, mPort, this,
-            aHostEnt->hostEnt.h_name,
-            addrbuf));
+            char buf[50];
+            PR_NetAddrToString(addr, buf, sizeof(buf));
+            LOG(("  => %s\n", buf));
 #endif
-    } else {
-        // XXX: What should happen here?  The GetHostByName(...) succeeded but 
-        //      there are *no* A records...
+        }
+
+        // start with first address in list
+        mNetAddress = mNetAddrList.GetNext(nsnull);
+    }
+    else {
+        // XXX: What should happen here?  The DNS lookup succeeded, but there
+        //      are no address records...
         rv = NS_ERROR_FAILURE;
 
         LOG(("nsSocketTransport: OnFound(...) [%s:%d %x]."
@@ -1543,7 +1564,7 @@ nsSocketTransport::OnStopLookup(nsISupports *aContext,
     // If the lookup failed, set the status...
     if (NS_FAILED(aStatus))
         mStatus = aStatus;
-    else if (PR_IsNetAddrType(&mNetAddress, PR_IpAddrAny))
+    else if (mNetAddress == nsnull)
         mStatus = NS_ERROR_ABORT;
 
     // Start processing the transport again - if necessary...
@@ -1864,7 +1885,7 @@ nsSocketTransport::GetIPStr(PRUint32 aLen, char **_retval)
     *_retval = (char*)nsMemory::Alloc(aLen);
     if (!*_retval) return NS_ERROR_FAILURE;
 
-    PRStatus status = PR_NetAddrToString(&mNetAddress, *_retval, aLen);
+    PRStatus status = PR_NetAddrToString(mNetAddress, *_retval, aLen);
 
     if (PR_FAILURE == status) {
         nsMemory::Free(*_retval);
@@ -1980,6 +2001,37 @@ nsSocketTransport::ClearSocketBS(nsSocketBS *blockingStream)
     
     if (--mSocketRef == 0)
         CloseConnection();
+}
+
+//
+//----------------------------------------------------------------------------
+// nsSocketTransport::nsNetAddrList
+//----------------------------------------------------------------------------
+//
+
+nsresult nsSocketTransport::
+nsNetAddrList::Init(PRUint32 len)
+{
+    NS_ASSERTION(!mList, "already initialized");
+    mList = new PRNetAddr[len];
+    if (!mList)
+        return NS_ERROR_OUT_OF_MEMORY;
+    mLen = len;
+    return NS_OK;
+}
+
+PRNetAddr *nsSocketTransport::
+nsNetAddrList::GetNext(PRNetAddr *addr)
+{
+    if (!addr)
+        return mList;
+
+    PRUint32 offset = addr - mList;
+    NS_ASSERTION(offset < mLen, "invalid address");
+    if (offset + 1 < mLen)
+        return addr + 1;
+
+    return nsnull;
 }
 
 //
--- netwerk/base/src/nsSocketTransport.h
+++ netwerk/base/src/nsSocketTransport.h
@@ -176,7 +176,7 @@ public:
     PRFileDesc* GetSocket(void)      { return mSocketFD;    }
     PRInt16     GetSelectFlags(void) { return mSelectFlags; }
     PRCList*    GetListNode(void)    { return &mListLink;   }
-    
+
     static nsSocketTransport* GetInstance(PRCList* qp) { return (nsSocketTransport*)((char*)qp - offsetof(nsSocketTransport, mListLink)); }
     
     PRBool CanBeReused() { return 
@@ -237,8 +237,27 @@ protected:
     inline void ClearFlag(nsSocketReadWriteInfo aFlag) {
         mReadWriteState &= ~aFlag;
     } 
+
+    PRInt32     GetSocketPort(void)  { return (mProxyPort != -1 && !mProxyTransparent) ? mProxyPort : mPort; }
+    const char *GetSocketHost(void)  { return (mProxyHost && !mProxyTransparent) ? mProxyHost : mHostName; }
     
 protected:
+    class nsNetAddrList {
+    public:
+        nsNetAddrList() : mList(nsnull), mLen(0) {}
+       ~nsNetAddrList() { delete[] mList; }
+
+        // allocate space for the address list
+        nsresult Init(PRUint32 len);
+
+        // given a net addr in the list, return the next addr.
+        // if given NULL, then return the first addr in the list.
+        // returns NULL if given addr is the last addr.
+        PRNetAddr *GetNext(PRNetAddr *currentAddr);
+    private:
+        PRNetAddr *mList;
+        PRUint32   mLen;
+    };
     
     nsSocketState                   mCurrentState;
     nsCOMPtr<nsIRequest>            mDNSRequest;
@@ -249,7 +268,8 @@ protected:
     PRIntervalTime                  mLastActiveTime;
     PRCList                         mListLink;
     PRMonitor*                      mMonitor;
-    PRNetAddr                       mNetAddress;
+    nsNetAddrList                   mNetAddrList;
+    PRNetAddr                      *mNetAddress;
     nsSocketOperation               mOperation;
     nsCOMPtr<nsISupports>           mSecurityInfo;
 
