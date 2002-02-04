9ede75fd505d8bf83bce65fcd2458f9e44cb37dd
waldemar%netscape.com <waldemar%netscape.com>
2002-02-04 21:09:05 +0000

    Removed license

--- js2/semantics/JSECMA/Lexer.lisp
+++ js2/semantics/JSECMA/Lexer.lisp
@@ -1,22 +1,3 @@
-;;; The contents of this file are subject to the Mozilla Public
-;;; License Version 1.1 (the "License"); you may not use this file
-;;; except in compliance with the License. You may obtain a copy of
-;;; the License at http://www.mozilla.org/MPL/
-;;; 
-;;; Software distributed under the License is distributed on an "AS
-;;; IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-;;; implied. See the License for the specific language governing
-;;; rights and limitations under the License.
-;;; 
-;;; The Original Code is the Language Design and Prototyping Environment.
-;;; 
-;;; The Initial Developer of the Original Code is Netscape Communications
-;;; Corporation.  Portions created by Netscape Communications Corporation are
-;;; Copyright (C) 1999 Netscape Communications Corporation.  All
-;;; Rights Reserved.
-;;; 
-;;; Contributor(s):   Waldemar Horwat <waldemar@acm.org>
-
 ;;;
 ;;; ECMAScript sample lexer
 ;;;
