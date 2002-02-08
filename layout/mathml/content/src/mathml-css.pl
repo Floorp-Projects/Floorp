eeb5c2d6346e410b6a2f8c6794da4ddd81d8c838
rbs@maths.uq.edu.au <rbs@maths.uq.edu.au>
2002-02-08 03:34:57 +0000

    since bad end of lines cause troubles on some platforms, extend the little perl magic for dos2unix to dos2{unix or mac} so that the conditional addition of mathml.css to the ua.css works reliably everywhere

--- layout/mathml/content/src/mathml-css.pl
+++ layout/mathml/content/src/mathml-css.pl
@@ -10,8 +10,9 @@ close(UA);
 
 if (!($css =~ m|\@import.*mathml\.css|)) 
 {
-  $css =~ s/\cM\n/\n/g; # dos2unix end of line
-  $css =~ s|(\@import[^\@]+\;)\n\n|$1\n\@import url\(resource:/res/mathml\.css\);\n\n|;
+  # since bad end of lines cause troubles on some platforms
+  # do a little perl magic for dos2{unix or mac} here
+  $css =~ s#(\@import[^\@]+\;)(\cM?)(\n\cM?\n)#$1$2\n\@import url\(resource:/res/mathml\.css\);$2$3#;
   open(UA, ">$ua");
   print UA $css;
   close(UA);
