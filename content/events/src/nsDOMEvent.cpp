a26bc9da87f685c520a0192d6f63c95a4f38802c
jst%netscape.com <jst%netscape.com>
2002-02-09 01:49:25 +0000

    Backing out v1.130 of this file since it caused crasher bug 120237. rs=hewitt@netscape.com

--- content/events/src/nsDOMEvent.cpp
+++ content/events/src/nsDOMEvent.cpp
@@ -647,9 +647,7 @@ NS_METHOD nsDOMEvent::GetScreenY(PRInt32* aScreenY)
 NS_METHOD nsDOMEvent::GetClientX(PRInt32* aClientX)
 {
   if (!mEvent || 
-       (mEvent->eventStructType != NS_MOUSE_EVENT 
-        && mEvent->eventStructType != NS_EVENT 
-        && mEvent->eventStructType != NS_DRAGDROP_EVENT) ||
+       (mEvent->eventStructType != NS_MOUSE_EVENT && mEvent->eventStructType != NS_DRAGDROP_EVENT) ||
       !mPresContext) {
     *aClientX = 0;
     return NS_OK;
@@ -700,9 +698,7 @@ NS_METHOD nsDOMEvent::GetClientX(PRInt32* aClientX)
 NS_METHOD nsDOMEvent::GetClientY(PRInt32* aClientY)
 {
   if (!mEvent || 
-       (mEvent->eventStructType != NS_MOUSE_EVENT 
-        && mEvent->eventStructType != NS_EVENT 
-        && mEvent->eventStructType != NS_DRAGDROP_EVENT) ||
+       (mEvent->eventStructType != NS_MOUSE_EVENT && mEvent->eventStructType != NS_DRAGDROP_EVENT) ||
       !mPresContext) {
     *aClientY = 0;
     return NS_OK;
