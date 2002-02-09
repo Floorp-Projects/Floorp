52ade1c793dbe7e25b572a431626220c4c406ec9
wtc%netscape.com <wtc%netscape.com>
2002-02-09 03:26:51 +0000

    Define DARWIN instead of XP_MACOSX.

--- security/coreconf/Darwin.mk
+++ security/coreconf/Darwin.mk
@@ -57,7 +57,7 @@ endif
 # definitions so that the linker can catch multiply-defined symbols.
 # Also, common symbols are not allowed with Darwin dynamic libraries.
 
-OS_CFLAGS	= $(DSO_CFLAGS) $(OS_REL_CFLAGS) -Wmost -fpascal-strings -traditional-cpp -fno-common -pipe -DXP_MACOSX -DHAVE_STRERROR -DHAVE_BSD_FLOCK
+OS_CFLAGS	= $(DSO_CFLAGS) $(OS_REL_CFLAGS) -Wmost -fpascal-strings -traditional-cpp -fno-common -pipe -DDARWIN -DHAVE_STRERROR -DHAVE_BSD_FLOCK
 
 ARCH		= darwin
 
