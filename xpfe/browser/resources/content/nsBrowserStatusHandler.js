31e46e3083f21403ae9dc0dc0bb1436de1ad461b
cbiesinger@web.de <cbiesinger@web.de>
2002-02-17 22:26:05 +0000

    backing out patch for bug 122144 because Txul is 1% up a=shaver

--- xpfe/browser/resources/content/nsBrowserStatusHandler.js
+++ xpfe/browser/resources/content/nsBrowserStatusHandler.js
@@ -109,7 +109,6 @@ nsBrowserStatusHandler.prototype =
     this.stopMenu        = document.getElementById("menuitem-stop");
     this.stopContext     = document.getElementById("context-stop");
     this.statusTextField = document.getElementById("statusbar-display");
-    this.translateMenu   = document.getElementById("menuitem-translate");
 
   },
 
@@ -123,7 +122,6 @@ nsBrowserStatusHandler.prototype =
     this.stopMenu        = null;
     this.stopContext     = null;
     this.statusTextField = null;
-    this.translateMenu   = null;
     this.userTyped       = null;
   },
 
@@ -200,7 +198,6 @@ nsBrowserStatusHandler.prototype =
     const nsIWebProgressListener = Components.interfaces.nsIWebProgressListener;
     const nsIChannel = Components.interfaces.nsIChannel;
     var domWindow;
-    var ctype;
     if (aStateFlags & nsIWebProgressListener.STATE_START) {
       if (aStateFlags & nsIWebProgressListener.STATE_IS_NETWORK) {
         // Remember when loading commenced.
@@ -217,7 +214,6 @@ nsBrowserStatusHandler.prototype =
         this.stopButton.disabled = false;
         this.stopMenu.removeAttribute('disabled');
         this.stopContext.removeAttribute('disabled');
-        this.translateMenu.removeAttribute('disabled');
 
         // Initialize the progress stuff...
         this.useRealProgressFlag = false;
@@ -251,9 +247,6 @@ nsBrowserStatusHandler.prototype =
           }
           this.status = "";
           this.setDefaultStatus(msg);
-          ctype = aRequest.QueryInterface(nsIChannel).contentType;
-          if (ctype.match(/^image\//))
-            this.translateMenu.setAttribute('disabled', 'true');
         }
 
         // Turn the progress meter and throbber off.
@@ -270,7 +263,7 @@ nsBrowserStatusHandler.prototype =
     }
     else if (aStateFlags & nsIWebProgressListener.STATE_TRANSFERRING) {
       if (aStateFlags & nsIWebProgressListener.STATE_IS_DOCUMENT) {
-        ctype = aRequest.QueryInterface(nsIChannel).contentType;
+        var ctype = aRequest.QueryInterface(nsIChannel).contentType;
 
         if (ctype != "text/html")
           this.useRealProgressFlag = true;
