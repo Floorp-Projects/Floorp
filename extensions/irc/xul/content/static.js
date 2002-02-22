77cb503dbdccb92df1aefa9991cdedc23ac742d0
samuel%sieb.net <samuel%sieb.net>
2002-02-22 07:09:32 +0000

    incrementing version number r=rginda a=asa chatzilla only

--- extensions/irc/xul/content/static.js
+++ extensions/irc/xul/content/static.js
@@ -36,7 +36,7 @@ const MSG_UNKNOWN   = getMsg ("unknown");
 
 client.defaultNick = getMsg( "defaultNick" );
 
-client.version = "0.8.5";
+client.version = "0.8.6";
 
 client.TYPE = "IRCClient";
 client.COMMAND_CHAR = "/";
