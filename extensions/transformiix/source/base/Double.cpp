650adde1f980ddc0a648283c8e7b7571fcd2707b
seawood@netscape.com <seawood@netscape.com>
2002-02-26 07:59:00 +0000

    Apply patches from FreeBSD's Ports system. Thanks to Jeremy Lea <reg@shale.csir.co.za> for the patch. Bug #112987 r=cls a=asa

--- extensions/transformiix/source/base/Double.cpp
+++ extensions/transformiix/source/base/Double.cpp
@@ -41,7 +41,7 @@
 #endif
 //A trick to handle IEEE floating point exceptions on FreeBSD - E.D.
 #ifdef __FreeBSD__
-#include <floatingpoint.h>
+#include <ieeefp.h>
 #endif
 #ifndef TX_EXE
 #include "prdtoa.h"
@@ -56,7 +56,11 @@
 
 //A trick to handle IEEE floating point exceptions on FreeBSD - E.D.
 #ifdef __FreeBSD__
+#ifdef __alpha__
+fp_except_t allmask = FP_X_INV|FP_X_OFL|FP_X_UFL|FP_X_DZ|FP_X_IMP;
+#else
 fp_except_t allmask = FP_X_INV|FP_X_OFL|FP_X_UFL|FP_X_DZ|FP_X_IMP|FP_X_DNML;
+#endif
 fp_except_t oldmask = fpsetmask(~allmask);
 #endif
 
