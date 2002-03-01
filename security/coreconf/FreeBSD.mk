d0c71c0e9d6750e1bb8886b266f4c96bcd72926d
wtc%netscape.com <wtc%netscape.com>
2002-03-01 16:08:12 +0000

    Bugzilla bug 112987: merged changes contributed by FreeBSD developer Jeremy <reg@shale.csir.co.za>.  r=wtc.

--- security/coreconf/FreeBSD.mk
+++ security/coreconf/FreeBSD.mk
@@ -43,19 +43,21 @@ RANLIB			= ranlib
 ifeq ($(OS_TEST),alpha)
 CPU_ARCH		= alpha
 else
-OS_REL_CFLAGS		= -Di386
 CPU_ARCH		= x86
 endif
 
-OS_CFLAGS		= $(DSO_CFLAGS) $(OS_REL_CFLAGS) -ansi -Wall -pipe $(THREAD_FLAG) -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
+OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
+
+DSO_CFLAGS		= -fPIC
+DSO_LDOPTS		= -shared -Wl,-soname -Wl,$(notdir $@)
 
 #
 # The default implementation strategy for FreeBSD is pthreads.
 #
 ifndef CLASSIC_NSPR
 USE_PTHREADS		= 1
-DEFINES			+= -D_THREAD_SAFE
-THREAD_FLAG		= -pthread
+DEFINES			+= -D_THREAD_SAFE -D_REENTRANT
+DSO_LDOPTS		+= -pthread
 endif
 
 ARCH			= freebsd
@@ -68,11 +70,7 @@ else
 DLL_SUFFIX		= so.1.0
 endif
 
-DSO_CFLAGS		= -fPIC
-DSO_LDOPTS		= -Bshareable
-DSO_LDFLAGS		=
-
-MKSHLIB			= $(LD) $(DSO_LDOPTS)
+MKSHLIB			= $(CC) $(DSO_LDOPTS)
 ifdef MAPFILE
 # Add LD options to restrict exported symbols to those in the map file
 endif
