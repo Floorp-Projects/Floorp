99ed8e625026c75cf2928909512079c694d5d302
av@netscape.com <av@netscape.com>
2002-03-06 03:37:15 +0000

    Threaded plugin sample code, not part of the build

--- modules/plugin/samples/npthread/windows/dbg.h
+++ modules/plugin/samples/npthread/windows/dbg.h
@@ -34,3 +34,32 @@
  * the terms of any one of the NPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
+
+#ifndef __DBG_HPP_
+#define __DBG_HPP_
+
+#ifdef NTRACE
+
+#define dbgOut1(x)        ((void)0)
+#define dbgOut2(x,y)      ((void)0)
+#define dbgOut3(x,y,z)      ((void)0)
+#define dbgOut4(x,y,z,t)    ((void)0)
+#define dbgOut5(x,y,z,t,u)    ((void)0)
+#define dbgOut6(x,y,z,t,u,v)  ((void)0)
+#define dbgOut7(x,y,z,t,u,v,a)  ((void)0)
+#define dbgOut8(x,y,z,t,u,v,a,b)  ((void)0)
+
+#else
+
+void __cdecl dbgOut(LPSTR format, ...);
+#define dbgOut1(x)        dbgOut(x)
+#define dbgOut2(x,y)      dbgOut(x, y)
+#define dbgOut3(x,y,z)      dbgOut(x, y, z)
+#define dbgOut4(x,y,z,t)    dbgOut(x, y, z, t)
+#define dbgOut5(x,y,z,t,u)    dbgOut(x, y, z, t, u)
+#define dbgOut6(x,y,z,t,u,v)  dbgOut(x, y, z, t, u, v)
+#define dbgOut7(x,y,z,t,u,v, a)  dbgOut(x, y, z, t, u, v, a)
+#define dbgOut8(x,y,z,t,u,v, a, b)  dbgOut(x, y, z, t, u, v, a, b)
+
+#endif
+#endif
--- modules/plugin/samples/npthread/windows/thread.h
+++ modules/plugin/samples/npthread/windows/thread.h
@@ -34,3 +34,57 @@
  * the terms of any one of the NPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
+
+#ifndef __THREAD_H__
+#define __THREAD_H__
+
+typedef enum {
+  ts_dead = 0,
+  ts_ready,
+  ts_busy
+
+} ThreadState;
+
+class CThread {
+private:
+  HANDLE mThread;
+  DWORD mID;
+  HANDLE mEventThreadInitializationFinished;
+  HANDLE mEventThreadShutdownFinished;
+  DWORD mInitTimeOut;
+  DWORD mShutTimeOut;
+  HANDLE mEventDo;
+  ThreadState mState;
+
+protected:
+  int mAction;
+
+public:
+  BOOL setInitEvent();
+  BOOL setShutEvent();
+  BOOL isAlive();
+  BOOL isBusy();
+  HANDLE getHandle();
+
+  void run();
+
+  // outside thread interface
+  CThread(DWORD aInitTimeOut = INFINITE, DWORD aShutTimeOut = INFINITE);
+  virtual ~CThread();
+
+  BOOL open(CThread * aThread);
+  void close(CThread * aThread);
+
+  BOOL tryAction(int aAction);
+  BOOL doAction(int aAction);
+
+  // virtuals
+  virtual BOOL init() = 0;
+  virtual void shut() = 0; 
+  virtual void dispatch() = 0;
+};
+
+BOOL InitThreadLib(HINSTANCE aInstance);
+void ShutThreadLib(HINSTANCE aInstance);
+
+#endif // THREAD_HPP__
