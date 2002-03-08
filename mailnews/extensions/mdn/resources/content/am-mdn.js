d24f02af8de34bf078a78036b57a7e01def8212b
shliang%netscape.com <shliang%netscape.com>
2002-03-08 22:09:45 +0000

    128962 - account manager extension for MDN return receipts - not part of build - r/sr=sspitzer,a=asa

---
+++ mailnews/extensions/mdn/resources/content/am-mdn.js
@@ -0,0 +1,108 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation. Portions created by Netscape are
+ * Copyright (C) 1998-2001 Netscape Communications Corporation. All
+ * Rights Reserved.
+ * 
+ * Contributors:
+ */
+
+var useCustomPrefs;
+var requestReceipt;
+var leaveInInbox;
+var moveToSent;
+var receiptSend;
+var neverReturn;
+var returnSome;
+var notInToCcPref;
+var notInToCcLabel;
+var outsideDomainPref;
+var outsideDomainLabel;
+var otherCasesPref;
+var otherCasesLabel;
+var receiptArriveLabel;
+var receiptRequestLabel;
+
+function onInit() 
+{
+  useCustomPrefs = document.getElementById("identity.use_custom_prefs");
+  requestReceipt = document.getElementById("identity.request_return_receipt_on");
+  leaveInInbox = document.getElementById("leave_in_inbox");
+  moveToSent = document.getElementById("move_to_sent");
+  receiptSend = document.getElementById("identity.return_some_receipts");
+  neverReturn = document.getElementById("never_return");
+  returnSome = document.getElementById("return_some");
+  notInToCcPref = document.getElementById("server.mdn_not_in_to_cc");
+  notInToCcLabel = document.getElementById("notInToCcLabel");
+  outsideDomainPref = document.getElementById("server.mdn_outside_domain");
+  outsideDomainLabel = document.getElementById("outsideDomainLabel");
+  otherCasesPref = document.getElementById("server.mdn_other");
+  otherCasesLabel = document.getElementById("otherCasesLabel");
+  receiptArriveLabel = document.getElementById("receiptArriveLabel");
+  receiptRequestLabel = document.getElementById("receiptRequestLabel");
+  
+  EnableDisableCustomSettings();
+        
+  return true;
+}
+
+function onSave()
+{
+
+}
+
+function EnableDisableCustomSettings() {
+  if (useCustomPrefs && (useCustomPrefs.getAttribute("value") == 0)) {
+    requestReceipt.setAttribute("disabled", "true");
+    leaveInInbox.setAttribute("disabled", "true");
+    moveToSent.setAttribute("disabled", "true");
+    neverReturn.setAttribute("disabled", "true");
+    returnSome.setAttribute("disabled", "true");
+    receiptArriveLabel.setAttribute("disabled", "true");
+    receiptRequestLabel.setAttribute("disabled", "true");
+  }
+  else {
+    requestReceipt.removeAttribute("disabled");
+    leaveInInbox.removeAttribute("disabled");
+    moveToSent.removeAttribute("disabled");
+    neverReturn.removeAttribute("disabled");
+    returnSome.removeAttribute("disabled");
+    receiptArriveLabel.removeAttribute("disabled");
+    receiptRequestLabel.removeAttribute("disabled");
+  }
+  EnableDisableAllowedReceipts();
+  return true;
+}
+
+function EnableDisableAllowedReceipts() {
+  if (receiptSend) {
+    if (!neverReturn.getAttribute("disabled") && (receiptSend.getAttribute("value") != 0)) {
+      notInToCcPref.removeAttribute("disabled");
+      notInToCcLabel.removeAttribute("disabled");
+      outsideDomainPref.removeAttribute("disabled");
+      outsideDomainLabel.removeAttribute("disabled");
+      otherCasesPref.removeAttribute("disabled");
+      otherCasesLabel.removeAttribute("disabled");
+    }
+    else {
+      notInToCcPref.setAttribute("disabled", "true");
+      notInToCcLabel.setAttribute("disabled", "true");
+      outsideDomainPref.setAttribute("disabled", "true");
+      outsideDomainLabel.setAttribute("disabled", "true");
+      otherCasesPref.setAttribute("disabled", "true");
+      otherCasesLabel.setAttribute("disabled", "true");
+    }
+  }
+  return true;
+}
---
+++ mailnews/extensions/mdn/src/mdn-service.js
@@ -0,0 +1,115 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * The Original Code is mozilla.org code.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1999 Netscape Communications Corporation.  All
+ * Rights Reserved.
+ * 
+ * Contributor(s): 
+ * Seth Spitzer <sspitzer@netscape.com>
+ */
+
+
+/* components defined in this file */
+const MDN_EXTENSION_SERVICE_CONTRACTID =
+    "@mozilla.org/accounmanager/extension;1?name=mdn";
+const MDN_EXTENSION_SERVICE_CID =
+    Components.ID("{e007d92e-1dd1-11b2-a61e-dc962c9b8571}");
+
+/* interafces used in this file */
+const nsIMsgAccountManagerExtension  = Components.interfaces.nsIMsgAccountManagerExtension;
+const nsICategoryManager = Components.interfaces.nsICategoryManager;
+const nsISupports        = Components.interfaces.nsISupports;
+
+function MDNService()
+{}
+
+MDNService.prototype.name = "mdn";
+MDNService.prototype.showPanel =
+
+function (server)
+
+{
+  // don't show the panel for news accounts
+  return (server.type != "nntp");
+}
+
+/* factory for command line handler service (MDNService) */
+var MDNFactory = new Object();
+
+MDNFactory.createInstance =
+function (outer, iid) {
+  if (outer != null)
+    throw Components.results.NS_ERROR_NO_AGGREGATION;
+
+  if (!iid.equals(nsIMsgAccountManagerExtension) && !iid.equals(nsISupports))
+    throw Components.results.NS_ERROR_INVALID_ARG;
+
+  return new MDNService();
+}
+
+
+var MDNModule = new Object();
+
+MDNModule.registerSelf =
+function (compMgr, fileSpec, location, type)
+{
+  dump("*** Registering mdn account manager extension.\n");
+  compMgr = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+  compMgr.registerFactoryLocation(MDN_EXTENSION_SERVICE_CID,
+                                  "MDN Account Manager Extension Service",
+                                  MDN_EXTENSION_SERVICE_CONTRACTID, 
+                                  fileSpec,
+                                  location, 
+                                  type);
+  catman = Components.classes["@mozilla.org/categorymanager;1"].getService(nsICategoryManager);
+  catman.addCategoryEntry("mailnews-accountmanager-extensions",
+                            "mdn account manager extension",
+                            MDN_EXTENSION_SERVICE_CONTRACTID, true, true);
+}
+
+MDNModule.unregisterSelf =
+function(compMgr, fileSpec, location)
+{
+  compMgr = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+  compMgr.unregisterFactoryLocation(MDN_EXTENSION_SERVICE_CID, fileSpec);
+  catman = Components.classes["@mozilla.org/categorymanager;1"].getService(nsICategoryManager);
+  catman.deleteCategoryEntry("mailnews-accountmanager-extensions",
+                             MDN_EXTENSION_SERVICE_CONTRACTID, true);
+}
+
+MDNModule.getClassObject =
+function (compMgr, cid, iid) {
+  if (cid.equals(MDN_EXTENSION_SERVICE_CID))
+    return MDNFactory;
+
+
+  if (!iid.equals(Components.interfaces.nsIFactory))
+    throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+
+  throw Components.results.NS_ERROR_NO_INTERFACE;    
+}
+
+MDNModule.canUnload =
+function(compMgr)
+{
+  return true;
+}
+
+/* entrypoint */
+function NSGetModule(compMgr, fileSpec) {
+  return MDNModule;
+}
+
+
