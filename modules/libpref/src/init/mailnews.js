3897d9fd99df6438bc9995c81731cc3720557e73
shliang@netscape.com <shliang@netscape.com>
2002-03-08 22:17:03 +0000

    128962 - pref panel for MDN return receipts - not part of build - r/sr=sspitzer,a=asa

--- modules/libpref/src/init/mailnews.js
+++ modules/libpref/src/init/mailnews.js
@@ -130,14 +130,19 @@ pref("mail.compose.other.header",	    "");
 localDefPref("mail.fcc_folder",                     "");
 pref("mail.encrypt_outgoing_mail",          false);
 pref("mail.crypto_sign_outgoing_mail",      false);
-pref("mail.default_html_action", 0);    // 0=ask, 1=plain, 2=html, 3=both
-pref("mail.smtp.ssl",                       0); // 0 = no, 1 = try, 2 = must use SSL
-pref("mail.mdn.report.not_in_to_cc", 0);      // 0: Never 1: Always 2: Ask me
-pref("mail.mdn.report.outside_domain", 2);    // 0: Never 1: Always 2: Ask me
-pref("mail.mdn.report.other", 2);   // 0: Never 1: Always 2: Ask me 3: Denial
-pref("mail.incorporate.return_receipt", 1); // 0: inbox/filter 1: Sent folder
-pref("mail.request.return_receipt", 2);     // 1: DSN 2: MDN 3: Both
-pref("mail.receipt.request_header_type", 0); // 0: MDN-DNT header  1: RRT header 2: Both (MC)
+pref("mail.default_html_action", 0);          // 0=ask, 1=plain, 2=html, 3=both
+pref("mail.smtp.ssl",0);                      // 0 = no, 1 = try, 2 = must use SSL
+
+pref("mail.mdn.report.not_in_to_cc", 2);               // 0: Never 1: Always 2: Ask me
+pref("mail.mdn.report.outside_domain", 2);             // 0: Never 1: Always 2: Ask me
+pref("mail.mdn.report.other", 2);                      // 0: Never 1: Always 2: Ask me 3: Denial
+
+pref("mail.incorporate.return_receipt", 0);            // 0: Inbox/filter 1: Sent folder
+pref("mail.request.return_receipt", 2);                // 1: DSN 2: MDN 3: Both
+pref("mail.receipt.request_header_type", 0);           // 0: MDN-DNT header  1: RRT header 2: Both (MC)
+pref("mail.receipt.request_return_receipt_on", false);
+pref("mail.receipt.return_some_receipts", 1);          // 0: Never send 1: Send sometimes
+
 
 pref("news.default_cc",                     "");
 pref("news.default_fcc",                    ""); // mailbox:URL or Imap://Host/OnlineFolderName
