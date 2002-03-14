8d1e284ba231a9358e36f893ac01e4237281b4bd
peterv@netscape.com <peterv@netscape.com>
2002-03-14 12:57:37 +0000

    Fix for bug 103235 (Implement DOM Level 3 XPath WD). r=sicking, sr=jst, a=dbaron.

--- extensions/transformiix/build/XSLTProcessorModule.cpp
+++ extensions/transformiix/build/XSLTProcessorModule.cpp
@@ -23,16 +23,21 @@
  *
  */
 
-#include "nsIGenericFactory.h"
 #include "nsICategoryManager.h"
 #include "nsIDOMClassInfo.h"
-#include "nsIServiceManager.h"
+#include "nsIExceptionService.h"
+#include "nsIGenericFactory.h"
 #include "nsIScriptNameSpaceManager.h"
+#include "nsIServiceManager.h"
 #include "nsSyncLoader.h"
+#include "nsXPathEvaluator.h"
+#include "nsXPathException.h"
+#include "nsXPathExpression.h"
+#include "nsXPathNSResolver.h"
+#include "nsXPathResult.h"
 #include "nsXPIDLString.h"
 #include "txAtoms.h"
 #include "XSLTProcessor.h"
-#include "XPathProcessor.h"
 
 /* 1c1a3c01-14f6-11d6-a7f2-ea502af815dc */
 #define TRANSFORMIIX_DOMCI_EXTENSION_CID   \
@@ -46,26 +51,48 @@ NS_DOMCI_EXTENSION(Transformiix)
     static NS_DEFINE_CID(kXSLTProcessorCID, TRANSFORMIIX_XSLT_PROCESSOR_CID);
     NS_DOMCI_EXTENSION_ENTRY_BEGIN(XSLTProcessor)
         NS_DOMCI_EXTENSION_ENTRY_INTERFACE(nsIDocumentTransformer)
-    NS_DOMCI_EXTENSION_ENTRY_END(XSLTProcessor, nsIDocumentTransformer, PR_FALSE, &kXSLTProcessorCID)
+    NS_DOMCI_EXTENSION_ENTRY_END(XSLTProcessor, nsIDocumentTransformer,
+                                 PR_FALSE, &kXSLTProcessorCID)
 
-    static NS_DEFINE_CID(kXPathProcessorCID, TRANSFORMIIX_XPATH_PROCESSOR_CID);
-    NS_DOMCI_EXTENSION_ENTRY_BEGIN(XPathProcessor)
-        NS_DOMCI_EXTENSION_ENTRY_INTERFACE(nsIXPathNodeSelector)
-    NS_DOMCI_EXTENSION_ENTRY_END(XPathProcessor, nsIXPathNodeSelector, PR_FALSE, &kXPathProcessorCID)
+    static NS_DEFINE_CID(kXPathEvaluatorCID, TRANSFORMIIX_XPATH_EVALUATOR_CID);
+    NS_DOMCI_EXTENSION_ENTRY_BEGIN(XPathEvaluator)
+        NS_DOMCI_EXTENSION_ENTRY_INTERFACE(nsIDOMXPathEvaluator)
+    NS_DOMCI_EXTENSION_ENTRY_END(XPathEvaluator, nsIDOMXPathEvaluator, PR_TRUE,
+                                 &kXPathEvaluatorCID)
 
-    NS_DOMCI_EXTENSION_ENTRY_BEGIN(NodeSet)
-        NS_DOMCI_EXTENSION_ENTRY_INTERFACE(nsIDOMNodeList)
-    NS_DOMCI_EXTENSION_ENTRY_END(NodeSet, nsIDOMNodeList, PR_FALSE, nsnull)
+    NS_DOMCI_EXTENSION_ENTRY_BEGIN(XPathException)
+        NS_DOMCI_EXTENSION_ENTRY_INTERFACE(nsIDOMXPathException)
+        NS_DOMCI_EXTENSION_ENTRY_INTERFACE(nsIException)
+    NS_DOMCI_EXTENSION_ENTRY_END(XPathException, nsIDOMXPathException, PR_TRUE,
+                                 nsnull)
+
+    NS_DOMCI_EXTENSION_ENTRY_BEGIN(XPathExpression)
+        NS_DOMCI_EXTENSION_ENTRY_INTERFACE(nsIDOMXPathExpression)
+    NS_DOMCI_EXTENSION_ENTRY_END(XPathExpression, nsIDOMXPathExpression,
+                                 PR_TRUE, nsnull)
+
+    NS_DOMCI_EXTENSION_ENTRY_BEGIN(XPathNSResolver)
+        NS_DOMCI_EXTENSION_ENTRY_INTERFACE(nsIDOMXPathNSResolver)
+    NS_DOMCI_EXTENSION_ENTRY_END(XPathNSResolver, nsIDOMXPathNSResolver,
+                                 PR_TRUE, nsnull)
+
+    NS_DOMCI_EXTENSION_ENTRY_BEGIN(XPathResult)
+        NS_DOMCI_EXTENSION_ENTRY_INTERFACE(nsIDOMXPathResult)
+    NS_DOMCI_EXTENSION_ENTRY_END(XPathResult, nsIDOMXPathResult, PR_TRUE,
+                                 nsnull)
 NS_DOMCI_EXTENSION_END
 
 // Factory Constructor
 NS_GENERIC_FACTORY_CONSTRUCTOR(XSLTProcessor)
-NS_GENERIC_FACTORY_CONSTRUCTOR(XPathProcessor)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsXPathEvaluator)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsSyncLoader)
 
 NS_DECL_DOM_CLASSINFO(XSLTProcessor)
-NS_DECL_DOM_CLASSINFO(XPathProcessor)
-NS_DECL_DOM_CLASSINFO(NodeSet)
+NS_DECL_DOM_CLASSINFO(XPathEvaluator)
+NS_DECL_DOM_CLASSINFO(XPathException)
+NS_DECL_DOM_CLASSINFO(XPathExpression)
+NS_DECL_DOM_CLASSINFO(XPathNSResolver)
+NS_DECL_DOM_CLASSINFO(XPathResult)
 
 static NS_METHOD 
 RegisterTransformiix(nsIComponentManager *aCompMgr,
@@ -74,101 +101,135 @@ RegisterTransformiix(nsIComponentManager *aCompMgr,
                      const char *componentType,
                      const nsModuleComponentInfo *info)
 {
-  nsresult rv = NS_OK;
+    nsresult rv = NS_OK;
 
-  nsCOMPtr<nsICategoryManager> catman =
-    do_GetService(NS_CATEGORYMANAGER_CONTRACTID, &rv);
+    nsCOMPtr<nsICategoryManager> catman =
+        do_GetService(NS_CATEGORYMANAGER_CONTRACTID, &rv);
 
-  if (NS_FAILED(rv))
+    if (NS_FAILED(rv))
+        return rv;
+
+    nsXPIDLCString previous;
+    rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_CLASS,
+                                  "XSLTProcessor",
+                                  TRANSFORMIIX_DOMCI_EXTENSION_CONTRACTID,
+                                  PR_TRUE, PR_TRUE, getter_Copies(previous));
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_CLASS,
+                                  "XPathEvaluator",
+                                  TRANSFORMIIX_DOMCI_EXTENSION_CONTRACTID,
+                                  PR_TRUE, PR_TRUE, getter_Copies(previous));
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_CLASS,
+                                  "XPathException",
+                                  TRANSFORMIIX_DOMCI_EXTENSION_CONTRACTID,
+                                  PR_TRUE, PR_TRUE, getter_Copies(previous));
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_CLASS,
+                                  "XPathExpression",
+                                  TRANSFORMIIX_DOMCI_EXTENSION_CONTRACTID,
+                                  PR_TRUE, PR_TRUE, getter_Copies(previous));
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_CLASS,
+                                  "XPathNSResolver",
+                                  TRANSFORMIIX_DOMCI_EXTENSION_CONTRACTID,
+                                  PR_TRUE, PR_TRUE, getter_Copies(previous));
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_CLASS,
+                                  "XPathResult",
+                                  TRANSFORMIIX_DOMCI_EXTENSION_CONTRACTID,
+                                  PR_TRUE, PR_TRUE, getter_Copies(previous));
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_INTERFACE,
+                                  "nsIDocumentTransformer",
+                                  NS_GET_IID(nsIDocumentTransformer).ToString(),
+                                  PR_TRUE, PR_TRUE, getter_Copies(previous));
+
     return rv;
-
-  nsXPIDLCString previous;
-  rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_CLASS,
-                                "XSLTProcessor",
-                                TRANSFORMIIX_DOMCI_EXTENSION_CONTRACTID,
-                                PR_TRUE, PR_TRUE, getter_Copies(previous));
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_CLASS,
-                                "XPathProcessor",
-                                TRANSFORMIIX_DOMCI_EXTENSION_CONTRACTID,
-                                PR_TRUE, PR_TRUE, getter_Copies(previous));
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_CLASS,
-                                "NodeSet",
-                                TRANSFORMIIX_DOMCI_EXTENSION_CONTRACTID,
-                                PR_TRUE, PR_TRUE, getter_Copies(previous));
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_INTERFACE,
-                                "nsIDocumentTransformer",
-                                NS_GET_IID(nsIDocumentTransformer).ToString(),
-                                PR_TRUE, PR_TRUE, getter_Copies(previous));
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  rv = catman->AddCategoryEntry(JAVASCRIPT_DOM_INTERFACE,
-                                "nsIXPathNodeSelector",
-                                NS_GET_IID(nsIXPathNodeSelector).ToString(),
-                                PR_TRUE, PR_TRUE, getter_Copies(previous));
-
-  return rv;
 }
 
 static PRBool gInitialized = PR_FALSE;
+static nsIExceptionProvider *sXPathExceptionProvider = 0;
 
 // Perform our one-time intialization for this module
 PR_STATIC_CALLBACK(nsresult)
 Initialize(nsIModule* aSelf)
 {
-  NS_PRECONDITION(!gInitialized, "module already initialized");
-  if (gInitialized)
+    NS_PRECONDITION(!gInitialized, "module already initialized");
+    if (gInitialized)
+        return NS_OK;
+
+    gInitialized = PR_TRUE;
+
+    sXPathExceptionProvider = new nsXPathExceptionProvider();
+    if (!sXPathExceptionProvider)
+        return NS_ERROR_OUT_OF_MEMORY;
+    NS_ADDREF(sXPathExceptionProvider);
+    nsCOMPtr<nsIExceptionService> xs =
+        do_GetService(NS_EXCEPTIONSERVICE_CONTRACTID);
+    if (xs)
+        xs->RegisterExceptionProvider(sXPathExceptionProvider,
+                                      NS_ERROR_MODULE_DOM_XPATH);
+
+    if (!txXMLAtoms::init())
+        return NS_ERROR_OUT_OF_MEMORY;
+    if (!txXPathAtoms::init())
+        return NS_ERROR_OUT_OF_MEMORY;
+    if (!txXSLTAtoms::init())
+        return NS_ERROR_OUT_OF_MEMORY;
+    if (!txHTMLAtoms::init())
+        return NS_ERROR_OUT_OF_MEMORY;
     return NS_OK;
-
-  gInitialized = PR_TRUE;
-
-  if (!txXMLAtoms::init())
-    return NS_ERROR_OUT_OF_MEMORY;
-  if (!txXPathAtoms::init())
-    return NS_ERROR_OUT_OF_MEMORY;
-  if (!txXSLTAtoms::init())
-    return NS_ERROR_OUT_OF_MEMORY;
-  if (!txHTMLAtoms::init())
-    return NS_ERROR_OUT_OF_MEMORY;
-  return NS_OK;
 }
 
 // Shutdown this module, releasing all of the module resources
 PR_STATIC_CALLBACK(void)
 Shutdown(nsIModule* aSelf)
 {
-  NS_PRECONDITION(gInitialized, "module not initialized");
-  if (!gInitialized)
-    return;
+    NS_PRECONDITION(gInitialized, "module not initialized");
+    if (!gInitialized)
+        return;
 
-  gInitialized = PR_FALSE;
+    gInitialized = PR_FALSE;
+    if (sXPathExceptionProvider) {
+        nsCOMPtr<nsIExceptionService> xs =
+            do_GetService(NS_EXCEPTIONSERVICE_CONTRACTID);
+        if (xs)
+            xs->UnregisterExceptionProvider(sXPathExceptionProvider,
+                                            NS_ERROR_MODULE_DOM_XPATH);
+        NS_RELEASE(sXPathExceptionProvider);
+    }
 
-  NS_IF_RELEASE(NS_CLASSINFO_NAME(XSLTProcessor));
-  NS_IF_RELEASE(NS_CLASSINFO_NAME(XPathProcessor));
-  NS_IF_RELEASE(NS_CLASSINFO_NAME(NodeSet));
+    NS_IF_RELEASE(NS_CLASSINFO_NAME(XSLTProcessor));
+    NS_IF_RELEASE(NS_CLASSINFO_NAME(XPathEvaluator));
+    NS_IF_RELEASE(NS_CLASSINFO_NAME(XPathException));
+    NS_IF_RELEASE(NS_CLASSINFO_NAME(XPathExpression));
+    NS_IF_RELEASE(NS_CLASSINFO_NAME(XPathNSResolver));
+    NS_IF_RELEASE(NS_CLASSINFO_NAME(XPathResult));
 
-  txXMLAtoms::shutdown();
-  txXPathAtoms::shutdown();
-  txXSLTAtoms::shutdown();
-  txHTMLAtoms::shutdown();
+    txXMLAtoms::shutdown();
+    txXPathAtoms::shutdown();
+    txXSLTAtoms::shutdown();
+    txHTMLAtoms::shutdown();
 }
 
 // Component Table
 static const nsModuleComponentInfo gComponents[] = {
-    { "Transformiix XSLT Processor",
+    { "XSLTProcessor",
       TRANSFORMIIX_XSLT_PROCESSOR_CID,
       TRANSFORMIIX_XSLT_PROCESSOR_CONTRACTID,
       XSLTProcessorConstructor,
       RegisterTransformiix },
-    { "Transformiix XPath Processor",
-      TRANSFORMIIX_XPATH_PROCESSOR_CID,
-      TRANSFORMIIX_XPATH_PROCESSOR_CONTRACTID,
-      XPathProcessorConstructor },
+    { "XPathEvaluator",
+      TRANSFORMIIX_XPATH_EVALUATOR_CID,
+      TRANSFORMIIX_XPATH_EVALUATOR_CONTRACTID,
+      nsXPathEvaluatorConstructor },
     { "Transformiix Synchronous Loader",
       TRANSFORMIIX_SYNCLOADER_CID,
       TRANSFORMIIX_SYNCLOADER_CONTRACTID,
