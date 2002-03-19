3f29510ad55da1fa7d9d5b4ec118a1f1b6c233b4
darin@netscape.com <darin@netscape.com>
2002-03-19 22:30:55 +0000

    fixes bug 126755 "try next ip address on connection timeout" r=dougt, sr=rpotts, a=asa

--- netwerk/base/src/nsSocketTransport.h
+++ netwerk/base/src/nsSocketTransport.h
@@ -208,6 +208,8 @@ protected:
     nsresult doReadWrite(PRInt16 aSelectFlags);
     nsresult doResolveHost();
 
+    PRBool TryNextAddress();
+
     void CompleteAsyncRead();
     void CompleteAsyncWrite();
 
@@ -378,7 +380,7 @@ public:
 
     void        SetSocket(PRFileDesc *aSock) { mSock = aSock; }
     PRUint32    GetOffset() { return mOffset; }
-    void        SetOffset(PRUint32 o) { mOffset = o; }
+    void        SetOffset(PRUint32 offset) { mOffset = offset; }
     PRBool      GotWouldBlock() { return mError == PR_WOULD_BLOCK_ERROR; }
     PRBool      GotError() { return mError != 0; }
     PRErrorCode GetError() { return mError; }
