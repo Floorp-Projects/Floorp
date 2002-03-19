55a33a709e1195b13fa2cb1b767adb5b6f2c7895
darin%netscape.com <darin%netscape.com>
2002-03-19 22:30:55 +0000

    fixes bug 126755 "try next ip address on connection timeout" r=dougt, sr=rpotts, a=asa

--- netwerk/base/src/nsSocketTransport.cpp
+++ netwerk/base/src/nsSocketTransport.cpp
@@ -531,21 +531,8 @@ nsresult nsSocketTransport::Process(PRInt16 aSelectFlags)
 
             // on connection failure, reuse next address if one exists
             if (mStatus == NS_ERROR_CONNECTION_REFUSED) {
-                mNetAddress = mNetAddrList.GetNext(mNetAddress);
-                if (mNetAddress) {
-#if defined(PR_LOGGING)
-                    char buf[50];
-                    PR_NetAddrToString(mNetAddress, buf, sizeof(buf));
-                    LOG(("connection failed... trying %s\n", buf));
-#endif
-                    PR_Close(mSocketFD);
-                    mSocketFD = nsnull;
-
-                    // mask error status so we'll return to this state
-                    mStatus = NS_OK;
-
-                    // need to re-enter Process() asynchronously
-                    mService->AddToWorkQ(this);
+                LOG(("connection failed [this=%x error=%x]\n", this, mStatus));
+                if (TryNextAddress()) {
                     done = PR_TRUE;
                     continue;
                 }
@@ -563,6 +550,23 @@ nsresult nsSocketTransport::Process(PRInt16 aSelectFlags)
             LOG(("nsSocketTransport: Transport [host=%s:%d this=%x] is in Timeout state.\n",
                 mHostName, mPort, this));
             mStatus = NS_ERROR_NET_TIMEOUT;
+
+            // on timeout, reuse next address if one exists ... do this only
+            // if we haven't already fired OnStartRequest.
+            if (mReadRequest || mWriteRequest) {
+                PRBool firedOnStart = PR_TRUE; // initial value doesn't matter
+                if (mReadRequest)
+                    firedOnStart = mReadRequest->IsInitialized();
+                if (!firedOnStart && mWriteRequest)
+                    firedOnStart = mWriteRequest->IsInitialized();
+                if (!firedOnStart && TryNextAddress()) {
+                    // a little bit of hackery here so we'll end up in the
+                    // WaitConnect state...
+                    mCurrentState = eSocketState_WaitConnect;
+                    done = PR_TRUE;
+                    continue;
+                }
+            }
             break;
 
         default:
@@ -609,6 +613,29 @@ nsSocketTransport::Cancel(nsresult status)
     if (mWriteRequest)
         mWriteRequest->Cancel(status);
     return NS_OK;
+}
+
+PRBool
+nsSocketTransport::TryNextAddress()
+{
+    mNetAddress = mNetAddrList.GetNext(mNetAddress);
+    if (mNetAddress) {
+#if defined(PR_LOGGING)
+        char buf[64];
+        PR_NetAddrToString(mNetAddress, buf, sizeof(buf));
+        LOG(("  ...trying next address: %s\n", buf));
+#endif
+        PR_Close(mSocketFD);
+        mSocketFD = nsnull;
+
+        // mask error status so we'll return to this state
+        mStatus = NS_OK;
+
+        // need to re-enter Process() asynchronously
+        mService->AddToWorkQ(this);
+        return PR_TRUE;
+    }
+    return PR_FALSE;
 }
 
 void
