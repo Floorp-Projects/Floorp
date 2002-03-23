7bd297cecef019025cb499dc1143b3f58a9258d7
rods@netscape.com <rods@netscape.com>
2002-03-23 18:07:51 +0000

    Add event listeners to "prevent" ContextMenu, Key, Mouse and MouseMotion events from being processed. Bug 128449 r=dcone sr=attinasi a=asa

---
+++ content/base/src/nsPrintPreviewListener.h
@@ -0,0 +1,95 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * The Original Code is the Mozilla browser.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications, Inc.  Portions created by Netscape are
+ * Copyright (C) 1999, Mozilla.  All Rights Reserved.
+ * 
+ * Contributor(s):
+ *   
+ */
+
+#ifndef nsPrintPreviewListener_h__
+#define nsPrintPreviewListener_h__
+
+// Interfaces needed to be included
+#include "nsIDOMContextMenuListener.h"
+#include "nsIDOMKeyListener.h"
+#include "nsIDOMMouseListener.h"
+#include "nsIDOMMouseMotionListener.h"
+// Helper Classes
+#include "nsIDOMEventReceiver.h"
+#include "nsCOMPtr.h"
+
+#define REG_NONE_LISTENER        0x00
+#define REG_CONTEXTMENU_LISTENER 0x01
+#define REG_KEY_LISTENER         0x02
+#define REG_MOUSE_LISTENER       0x04
+#define REG_MOUSEMOTION_LISTENER 0x08
+
+//
+// class nsPrintPreviewListener
+//
+// The class that listens to the chrome events and tells the embedding
+// chrome to show context menus, as appropriate. Handles registering itself
+// with the DOM with AddChromeListeners() and removing itself with
+// RemoveChromeListeners().
+//
+class nsPrintPreviewListener : public nsIDOMContextMenuListener,
+                                public nsIDOMKeyListener,
+                                public nsIDOMMouseListener,
+                                public nsIDOMMouseMotionListener
+
+{
+public:
+  NS_DECL_ISUPPORTS
+  
+  nsPrintPreviewListener(nsIDOMEventReceiver* aEVRec);
+
+  // nsIDOMContextMenuListener
+  NS_IMETHOD HandleEvent(nsIDOMEvent* aEvent)        {	return NS_OK; }
+  NS_IMETHOD ContextMenu (nsIDOMEvent* aEvent)       { printf("preventing ContextMenu\n"); aEvent->PreventDefault(); return NS_OK; }
+
+  // nsIDOMKeyListener
+  NS_IMETHOD KeyDown(nsIDOMEvent* aKeyEvent);
+  NS_IMETHOD KeyUp(nsIDOMEvent* aKeyEvent);
+  NS_IMETHOD KeyPress(nsIDOMEvent* aKeyEvent);
+
+  // nsIDOMMouseListener
+  NS_IMETHOD MouseDown(nsIDOMEvent* aMouseEvent)     { printf("preventing MouseDown\n"); aMouseEvent->StopPropagation();aMouseEvent->PreventDefault(); return NS_OK; }
+  NS_IMETHOD MouseUp(nsIDOMEvent* aMouseEvent)       { printf("preventing MouseUp\n"); aMouseEvent->StopPropagation();aMouseEvent->PreventDefault(); return NS_OK; }
+  NS_IMETHOD MouseClick(nsIDOMEvent* aMouseEvent)    { printf("preventing MouseClick\n"); aMouseEvent->StopPropagation();aMouseEvent->PreventDefault(); return NS_OK; }
+  NS_IMETHOD MouseDblClick(nsIDOMEvent* aMouseEvent) { printf("preventing MouseDblClick\n"); aMouseEvent->StopPropagation();aMouseEvent->PreventDefault(); return NS_OK; }
+  NS_IMETHOD MouseOver(nsIDOMEvent* aMouseEvent)     { printf("preventing MouseOver\n"); aMouseEvent->StopPropagation();aMouseEvent->PreventDefault(); return NS_OK; }
+  NS_IMETHOD MouseOut(nsIDOMEvent* aMouseEvent)      { printf("preventing MouseOut\n"); aMouseEvent->StopPropagation();aMouseEvent->PreventDefault(); return NS_OK; }
+
+  // nsIDOMMouseMotionListener
+  NS_IMETHOD DragMove(nsIDOMEvent* aMouseEvent)      { return NS_OK; };
+  NS_IMETHOD MouseMove(nsIDOMEvent* aMouseEvent)     { aMouseEvent->PreventDefault(); return NS_OK; }
+
+  // Add/remove the relevant listeners, based on what interfaces
+  // the embedding chrome implements.
+  nsresult AddListeners();
+  nsresult RemoveListeners();
+
+private:
+
+  nsCOMPtr<nsIDOMEventReceiver> mEventReceiver;
+  PRInt8 mRegFlags;
+
+}; // class nsPrintPreviewListener
+
+
+
+#endif /* nsPrintPreviewListener_h__ */
