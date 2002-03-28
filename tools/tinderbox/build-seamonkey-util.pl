5cc73440af71fbc7da6baf788e056b4715ded022
darin%netscape.com <darin%netscape.com>
2002-03-28 00:34:23 +0000

    fix mach-o build problem (CXX=c++) r=mcafee

--- tools/tinderbox/build-seamonkey-util.pl
+++ tools/tinderbox/build-seamonkey-util.pl
@@ -22,7 +22,7 @@ use File::Path;     # for rmtree();
 use Config;         # for $Config{sig_name} and $Config{sig_num}
 
 
-$::UtilsVersion = '$Revision: 1.149 $ ';
+$::UtilsVersion = '$Revision: 1.150 $ ';
 
 package TinderUtils;
 
@@ -346,7 +346,7 @@ sub SetupPath {
 
     if ($Settings::OS eq 'Darwin') {
         $ENV{PATH} = "/bin:/usr/bin:$ENV{PATH}";
-        $Settings::ConfigureEnvArgs = 'CC=cc CXX=cpp';
+        $Settings::ConfigureEnvArgs = 'CC=cc CXX=c++';
         $Settings::Compiler = 'cc';
     }
 
