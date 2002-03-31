3d8368473da8255f531612448428cf76ac4eecc4
av%netscape.com <av%netscape.com>
2002-03-31 02:58:00 +0000

    Bug 133662 -- update Tester plugin, not part of the build

--- modules/plugin/tools/tester/windows/winentry.cpp
+++ modules/plugin/tools/tester/windows/winentry.cpp
@@ -42,7 +42,13 @@
 /*********************************************************/
 #include <windows.h>
 
+#include "xp.h"
+#include "guiprefs.h"
+#include "loadstatus.h"
+#include "winutils.h"
+
 HINSTANCE hInst = NULL;
+HWND hWndLoadStatus = NULL;
 
 BOOL WINAPI DllMain(HINSTANCE hDLL, DWORD dwReason, LPVOID lpReserved)
 {
@@ -50,8 +56,21 @@ BOOL WINAPI DllMain(HINSTANCE hDLL, DWORD dwReason, LPVOID lpReserved)
 
   switch (dwReason) {
     case DLL_PROCESS_ATTACH:
+    {
+      char szFileName[_MAX_PATH];
+      GetINIFileName(hInst, szFileName, sizeof(szFileName));
+      char sz[256];
+      XP_GetPrivateProfileString(SECTION_PREFERENCES, KEY_LOADSTATUS_WINDOW, ENTRY_NO, sz, sizeof(sz), szFileName);
+      if (strcmpi(sz, ENTRY_YES) == 0)
+        hWndLoadStatus = ShowLoadStatus("Tester dll is loaded");
+      break;
+    }
     case DLL_THREAD_ATTACH:
+      break;
     case DLL_PROCESS_DETACH:
+      if (hWndLoadStatus)
+        DestroyLoadStatus(hWndLoadStatus);
+      break;
     case DLL_THREAD_DETACH:
       break;
     default:
