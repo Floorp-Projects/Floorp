32297f75baf64f99282ba87023ae3e000328a54b
jdunn@netscape.com <jdunn@netscape.com>
2002-04-01 11:28:26 +0000

    Adding 64bit support for AIX Patches courtesy of pkw@austin.ibm.com r=jdunn@netscape.com sr=jband@netscape.com a=asa@mozilla.org # 133622

--- xpcom/reflect/xptcall/src/md/unix/xptcstubs_ppc_aix64.cpp
+++ xpcom/reflect/xptcall/src/md/unix/xptcstubs_ppc_aix64.cpp
@@ -1,37 +1,36 @@
-/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* ***** BEGIN LICENSE BLOCK *****
- * Version: NPL 1.1/GPL 2.0/LGPL 2.1
+ * Version: MPL 1.1/LGPL 2.1/GPL 2.0
  *
- * The contents of this file are subject to the Netscape Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/NPL/
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
  *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
  *
  * The Original Code is mozilla.org code.
  *
- * The Initial Developer of the Original Code is 
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1999
- * the Initial Developer. All Rights Reserved.
+ * The Initial Developer of the Original Code is IBM Corporation.
+ * Portions created by IBM are
+ *   Copyright (C) 2002, International Business Machines Corporation.
+ *   All Rights Reserved.
  *
  * Contributor(s):
  *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or 
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the NPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the NPL, the GPL or the LGPL.
+ *  Alternatively, the contents of this file may be used under the terms of
+ *  either of the GNU General Public License Version 2 or later (the "GPL"),
+ *  or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ *  in which case the provisions of the GPL or the LGPL are applicable instead
+ *  of those above. If you wish to allow use of your version of this file only
+ *  under the terms of either the GPL or the LGPL, and not to allow others to
+ *  use your version of this file under the terms of the MPL, indicate your
+ *  decision by deleting the provisions above and replace them with the notice
+ *  and other provisions required by the LGPL or the GPL. If you do not delete
+ *  the provisions above, a recipient may use your version of this file under
+ *  the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
 
@@ -48,12 +47,8 @@
         arriving in registers
 */
 extern "C" nsresult
-PrepareAndDispatch(nsXPTCStubBase* self, PRUint32 methodIndex, PRUint32* args, PRUint32 *gprData, double *fprData)
+PrepareAndDispatch(nsXPTCStubBase* self, PRUint64 methodIndex, PRUint64* args, PRUint64 *gprData, double *fprData)
 {
-    typedef struct {
-        uint32 hi;
-        uint32 lo;      // have to move 64 bit entities as 32 bit halves since
-    } DU;               // stack slots are not guaranteed 16 byte aligned
 
 #define PARAM_BUFFER_COUNT     16
 #define PARAM_GPR_COUNT         7  
@@ -83,7 +78,7 @@ PrepareAndDispatch(nsXPTCStubBase* self, PRUint32 methodIndex, PRUint32* args, P
         dispatchParams = paramBuffer;
     NS_ASSERTION(dispatchParams,"no place for params");
 
-    PRUint32* ap = args;
+    PRUint64* ap = args;
     PRUint32 iCount = 0;
     PRUint32 fpCount = 0;
     for(i = 0; i < paramCount; i++)
@@ -119,13 +114,9 @@ PrepareAndDispatch(nsXPTCStubBase* self, PRUint32 methodIndex, PRUint32* args, P
                                          dp->val.i32  = (PRInt32)  *ap++;
                                      break;
         case nsXPTType::T_I64     :  if (iCount < PARAM_GPR_COUNT)
-                                         ((DU *)dp)->hi  = (PRInt32) gprData[iCount++];
+                                         dp->val.i64  = (PRInt64) gprData[iCount++];
                                      else
-                                         ((DU *)dp)->hi  = (PRInt32)  *ap++;
-                                     if (iCount < PARAM_GPR_COUNT)
-                                         ((DU *)dp)->lo  = (PRUint32) gprData[iCount++];
-                                     else
-                                         ((DU *)dp)->lo  = (PRUint32)  *ap++;
+                                         dp->val.i64  = (PRInt64) *ap++;
                                      break;
         case nsXPTType::T_U8      :  if (iCount < PARAM_GPR_COUNT)
                                          dp->val.u8  = (PRUint8) gprData[iCount++];
@@ -143,13 +134,9 @@ PrepareAndDispatch(nsXPTCStubBase* self, PRUint32 methodIndex, PRUint32* args, P
                                          dp->val.u32  = (PRUint32)  *ap++;
                                      break;
         case nsXPTType::T_U64     :  if (iCount < PARAM_GPR_COUNT)
-                                         ((DU *)dp)->hi  = (PRUint32) gprData[iCount++];
+                                         dp->val.u64  = (PRUint64) gprData[iCount++];
                                      else
-                                         ((DU *)dp)->hi  = (PRUint32)  *ap++;
-                                     if (iCount < PARAM_GPR_COUNT)
-                                         ((DU *)dp)->lo  = (PRUint32) gprData[iCount++];
-                                     else
-                                         ((DU *)dp)->lo  = (PRUint32)  *ap++;
+                                         dp->val.u64  = (PRUint64)  *ap++;
                                      break;
         case nsXPTType::T_FLOAT   :  if (fpCount < 13) {
                                          dp->val.f  = (float) fprData[fpCount++];
