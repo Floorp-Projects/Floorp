8228bd056db5c26bfeb41dfd65c18b670a4182fd
ashishbhatt%netscape.com <ashishbhatt%netscape.com>
2002-04-08 18:57:01 +0000

    fixed nsIObserServ.cpp and .h CV: ----------------------------------------------------------------------

--- embedding/qa/testembed/Tests.h
+++ embedding/qa/testembed/Tests.h
@@ -142,6 +142,7 @@ protected:
 	afx_msg void OnTestsRemovehistorylistener();
 	afx_msg void OnInterfacesNsiwebnav();
 	afx_msg void OnInterfacesNsiclipboardcommands();
+	afx_msg void OnInterfacesNsiobserverservice();
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 
