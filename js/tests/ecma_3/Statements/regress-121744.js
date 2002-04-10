774ab94e935262fb7a266dbb3199d52f1051227f
pschwartau%netscape.com <pschwartau%netscape.com>
2002-04-10 22:52:26 +0000

    Adding an early return for SpiderMonkey; we only want to run this test in Rhino.

--- js/tests/ecma_3/Statements/regress-121744.js
+++ js/tests/ecma_3/Statements/regress-121744.js
@@ -35,13 +35,18 @@
 *
 *
 * Date:    30 Jan 2002
+* Revised: 10 Apr 2002
 * SUMMARY: JS should error on |for(i in undefined)|, |for(i in null)|
 * See http://bugzilla.mozilla.org/show_bug.cgi?id=121744
 *
-* Also see the ECMA-262 3rd Edition Final spec:
-* > Section 12.6.4  The for-in Statement
-* > Section 9.9     ToObject
+* ECMA-262 3rd Edition Final spec says such statements should error. See:
+* Section 12.6.4  The for-in Statement
+* Section 9.9     ToObject
 *
+* BUT: SpiderMonkey has decided NOT to follow this; it's a bug in the spec.
+* See http://bugzilla.mozilla.org/show_bug.cgi?id=131348
+*
+* So adding an early return for SpiderMonkey; will continue to use for Rhino.
 */
 //-----------------------------------------------------------------------------
 var UBound = 0;
@@ -56,6 +61,12 @@ var actual = '';
 var actualvalues = [];
 var expect= '';
 var expectedvalues = [];
+
+/*
+ * As of 10 Apr 2002, we only want to run this test in Rhino
+ */
+if (!inRhino())
+  quit();
 
 
 status = inSection(1);
@@ -110,7 +121,7 @@ status = inSection(3);
 expect = TEST_PASSED;
 actual = TEST_FAILED_BADLY;
 /*
- * Variable names that cannot be looked up gererate ReferenceErrors; however,
+ * Variable names that cannot be looked up generate ReferenceErrors; however,
  * property names like obj.ZZZ that cannot be looked up are set to |undefined|
  *
  * Therefore, this should indirectly test | for (var i in undefined) |
