ba4614c159d9716e7acb14b2ed05919e340e3c31
mikep%oeone.com <mikep%oeone.com>
2002-04-11 15:12:18 +0000

    Adding in ability to update build ids.

--- calendar/createXPI.sh
+++ calendar/createXPI.sh
@@ -1,1 +1,1 @@
-cd linux; zip -r calendar_linux.xpi components/ install.js resources/; mv calendar_linux.xpi ../; cd ../windows; zip -r calendar_windows.xpi components/ bin/ install.js resources/; mv calendar_windows.xpi ../; cd ..
+./createBuildId.pl resources/content/calendar.js; cd linux; zip -r calendar_linux.xpi components/ install.js resources/; mv calendar_linux.xpi ../; cd ../windows; zip -r calendar_windows.xpi components/ bin/ install.js resources/; mv calendar_windows.xpi ../; cd ..
