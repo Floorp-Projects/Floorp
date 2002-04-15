0ea32af066a81aa07eb9283b07413b850eee955c
mkaply@us.ibm.com <mkaply@us.ibm.com>
2002-04-15 22:54:51 +0000

    #135795 r=cls, a=asa Disable mkdepend on OS/2 for now.

--- config/config.mk
+++ config/config.mk
@@ -658,7 +658,9 @@ else
 MKDEPEND_DIR	= $(CONFIG_TOOLS)/mkdepend
 MKDEPEND	= $(MKDEPEND_DIR)/mkdepend$(BIN_SUFFIX)
 ifndef COMPILER_DEPEND
+ifneq ($(OS_ARCH),OS2)
 MKDEPEND_BUILTIN = $(MKDEPEND_DIR)/mkdepend$(BIN_SUFFIX)
+endif
 endif
 endif
 
