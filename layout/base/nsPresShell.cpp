1be2855d7e0aca705a342da378096954ddac3a85
waterson%netscape.com <waterson%netscape.com>
2002-04-16 21:19:32 +0000

    Bug 137378. Stop ancestor frame notification at a frame that is an NS_FRAME_REFLOW_ROOT. r=karnaze, sr=kin.

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -3576,8 +3576,17 @@ NotifyAncestorFramesOfReflowCommand(nsIPresShell*        aPresShell,
     if (target) {
       nsIFrame* ancestor;
       target->GetParent(&ancestor);
-      while(ancestor) {
+      while (ancestor) {
         ancestor->ReflowCommandNotify(aPresShell, aRC, aCommandAdded);
+
+        // Stop if we encounter a reflow root: when dispatched, the
+        // reflow command will never visit any of the frames above the
+        // it.
+        nsFrameState state;
+        ancestor->GetFrameState(&state);
+        if (state & NS_FRAME_REFLOW_ROOT)
+          break;
+
         ancestor->GetParent(&ancestor);
       }
     }
