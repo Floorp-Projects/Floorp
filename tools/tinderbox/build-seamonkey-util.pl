8457b274da9f70b4e496b181f7263165c4c91d32
cltbld%netscape.com <cltbld%netscape.com>
2002-04-19 18:24:10 +0000

    printf for deleting profile, for debugging.  -mcafee

--- tools/tinderbox/build-seamonkey-util.pl
+++ tools/tinderbox/build-seamonkey-util.pl
@@ -23,7 +23,7 @@ use Config;         # for $Config{sig_name} and $Config{sig_num}
 use File::Find ();
 
 
-$::UtilsVersion = '$Revision: 1.163 $ ';
+$::UtilsVersion = '$Revision: 1.164 $ ';
 
 package TinderUtils;
 
@@ -944,6 +944,7 @@ sub run_all_tests {
             # Recreate profile if we have $Settings::CleanProfile set.
             if ($Settings::CleanProfile) {
                 print_log "Creating clean profile ...\n";
+                print_log "Deleting $build_dir/.mozilla ...\n";
                 File::Path::rmtree("$build_dir/.mozilla", 0, 0);
                 if (-e "$build_dir/.mozilla") {
                     print_log "Error: rmtree('$build_dir/.mozilla', 0, 0) failed.\n";
