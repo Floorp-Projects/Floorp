926b5e4e9083870c42882139937b3eb3b32ff352
hyatt%netscape.com <hyatt%netscape.com>
2002-04-20 23:04:29 +0000

    Personal pane commit.

---
+++ camino/PreferencePanes/Personal/PersonalPane.h
@@ -0,0 +1,9 @@
+#import <Cocoa/Cocoa.h>
+#import <PreferencePanes/NSPreferencePane.h>
+
+@interface PersonalPane : NSPreferencePane
+{
+
+}
+
+@end
---
+++ camino/PreferencePanes/Personal/PersonalPane.m
@@ -0,0 +1,19 @@
+#import "PersonalPane.h"
+
+@implementation PersonalPane
+
+
+- (id) initWithBundle:(NSBundle *) bundle {
+    self = [super initWithBundle:bundle] ;
+    return self ;
+}
+
+- (void)awakeFromNib
+{
+}
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
