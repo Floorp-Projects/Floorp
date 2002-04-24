3c9c1af1f74ff5e796f5970b290b3c131104f273
sicking@bigfoot.com <sicking@bigfoot.com>
2002-04-24 22:08:05 +0000

    bug 137399: Make removeAttribute work again for html-elements not in main doc-tree
    r=glazou sr=jst

--- content/html/content/src/nsGenericHTMLElement.cpp
+++ content/html/content/src/nsGenericHTMLElement.cpp
@@ -1983,7 +1983,6 @@ nsGenericHTMLElement::UnsetAttr(PRInt32 aNameSpaceID, nsIAtom* aAttribute, PRBoo
     }
   }
 
-  nsCOMPtr<nsIHTMLStyleSheet> sheet;
   PRInt32 impact = NS_STYLE_HINT_UNKNOWN;
   if (mDocument) {
     if (aNotify) {
@@ -2031,8 +2030,9 @@ nsGenericHTMLElement::UnsetAttr(PRInt32 aNameSpaceID, nsIAtom* aAttribute, PRBoo
     }
   }
 
-  sheet = dont_AddRef(GetAttrStyleSheet(mDocument));
-  if (sheet && mAttributes) {
+  if (mAttributes) {
+    nsCOMPtr<nsIHTMLStyleSheet> sheet
+         = dont_AddRef(GetAttrStyleSheet(mDocument));
     PRInt32 count;
     result = mAttributes->UnsetAttributeFor(aAttribute, aNameSpaceID, this,
                                             sheet, count);
