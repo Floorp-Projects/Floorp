c99d697275b63b28c54818ec17c42fa2b2886474
peterv%netscape.com <peterv%netscape.com>
2002-04-26 11:36:26 +0000

    Fix for bug 139380 (duplicate code in nsFontMetricsXlib.cpp/nsFontMetricsGTK.cpp). Patch by Roland.Mainz@informatik.med.uni-giessen.de. r=bstell, sr=attinasi.

--- gfx/src/gtk/nsFontMetricsGTK.cpp
+++ gfx/src/gtk/nsFontMetricsGTK.cpp
@@ -4125,7 +4125,6 @@ FFRESubstituteEncoding(nsACString &aFFREName,
 {
   PRInt32 encodingHyphen = aFFREName.FindChar('-');
   encodingHyphen = aFFREName.FindChar('-', encodingHyphen + 1);
-  encodingHyphen = aFFREName.FindChar('-', encodingHyphen + 1);
   aFFREName.Truncate(encodingHyphen+1);
   aFFREName.Append(aReplacementEncoding);
 }
