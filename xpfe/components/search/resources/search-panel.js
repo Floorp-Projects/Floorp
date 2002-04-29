7698983fa9d725ac86f700e3959f3c3b1806973a
blakeross@telocity.com <blakeross@telocity.com>
2002-04-29 03:00:57 +0000

    32034 - sidebar search fails if no navigator windows are open. r=bryner sr=ben

--- xpfe/components/search/resources/search-panel.js
+++ xpfe/components/search/resources/search-panel.js
@@ -675,10 +675,16 @@ function doStop()
 
 function doSearch()
 {
-  var searchMode = nsPreferences.getIntPref("browser.search.mode", 0);
+  var navWindow = getNavigatorWindow(true);
+  if (navWindow._content)
+    return onNavWindowLoad();
+  navWindow.addEventListener("load", onNavWindowLoad, false);
+}
+
+function onNavWindowLoad() {
+  var navWindow = getNavigatorWindow(true);
 
   // hide various columns
-  var navWindow = getNavigatorWindow(false);
   if (navWindow && "_content" in navWindow && "isMozillaSearchWindow" in navWindow._content) {
     colNode = navWindow._content.document.getElementById("RelevanceColumn");
     if (colNode)
@@ -701,6 +707,7 @@ function doSearch()
     return;
   }
 
+  var searchMode = nsPreferences.getIntPref("browser.search.mode", 0);
   var engineURIs = [];
   if (searchMode > 0) {
     var foundEngine = false;
