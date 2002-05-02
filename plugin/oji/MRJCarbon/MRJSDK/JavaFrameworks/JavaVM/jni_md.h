24ad4c4d23af53ac404fe7a902ebcd767c4c8802
bnesse%netscape.com <bnesse%netscape.com>
2002-05-02 21:56:05 +0000

    [Not part of build] Initial checkin of header files required to build the MRJ Plugin.

---
+++ plugin/oji/MRJCarbon/MRJSDK/JavaFrameworks/JavaVM/jni_md.h
@@ -0,0 +1,22 @@
+/*
+ * @(#)jni_md.h	1.12 00/02/02
+ *
+ * Copyright 1996-2000 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+
+#ifndef _JAVASOFT_JNI_MD_H_
+#define _JAVASOFT_JNI_MD_H_
+
+#define JNIEXPORT 
+#define JNIIMPORT
+#define JNICALL
+
+typedef long jint;
+typedef long long jlong;
+typedef signed char jbyte;
+
+#endif /* !_JAVASOFT_JNI_MD_H_ */
