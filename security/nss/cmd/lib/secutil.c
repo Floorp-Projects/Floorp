41ba83e2834a70714b8ce78eaa5f8cc1e5569fb9
jpierre%netscape.com <jpierre%netscape.com>
2002-05-06 23:10:40 +0000

    Fix for 142658 - fix certutil crash with NULL nickname

--- security/nss/cmd/lib/secutil.c
+++ security/nss/cmd/lib/secutil.c
@@ -1593,6 +1593,9 @@ SECU_PrintCertNickname(CERTCertificate *cert, void *data)
 	if ( name == NULL ) {
 	    name = cert->emailAddr;
 	}
+	if ( name == NULL ) {
+	    name = "(NULL)";
+	}
 	
         trust = cert->trust;
 	if (trust) {
