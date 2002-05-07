13a6a4f74d14d872e850044464b72c5a31c635e6
locka@iol.ie <locka@iol.ie>
2002-05-07 21:54:29 +0000

    NOT PART OF BUILD. More work on LiveConnect. It actually works now(!) but is a bit crashy for some undetermined reason.

--- embedding/browser/activex/src/plugin/MozAxPlugin.java
+++ embedding/browser/activex/src/plugin/MozAxPlugin.java
@@ -1,14 +1,65 @@
 import netscape.plugin.Plugin;
 
-class MozAxPlugin extends Plugin
+public class MozAxPlugin extends Plugin
 {
-    native  Object getProperty(String dispid);
-    native  void setProperty(String dispid, String property);
-    native  void setProperty(String dispid, Object property);
+    ///////////////////////////////////////////////////////////////////////////
+    // Public methods that are exposed through LiveConnect
 
-    native  Object invoke(String dispid);
-    native  Object invoke(String dispid, Object param1);
-    native  Object invoke(String dispid, Object param1, Object param2);
-    native  Object invoke(String dispid, Object param1, Object param2, Object param3);
-    native  Object invoke(String dispid, Object param1, Object param2, Object param3, Object param4);
+    public Object getProperty(String dispid)
+    {
+      return xgetProperty(dispid);
+    }
+    public void setProperty(String dispid, String property)
+    {
+      xsetProperty1(dispid, property);
+    }
+    public void setProperty(String dispid, Object property)
+    {
+      xsetProperty2(dispid, property);
+    }
+
+    public Object invoke(String dispid)
+    {
+      return xinvoke(dispid);
+    }
+
+    public Object invoke(String dispid, Object p1)
+    {
+      return xinvoke1(dispid, p1);
+    }
+    public Object invoke(String dispid, Object p1, Object p2)
+    {
+      return xinvoke2(dispid, p1, p2);
+    }
+    public Object invoke(String dispid, Object p1, Object p2, Object p3)
+    {
+      return xinvoke3(dispid, p1, p2, p3);
+    }
+    public Object invoke(String dispid, Object p1, Object p2, Object p3, Object p4)
+    {
+      return xinvoke4(dispid, p1, p2, p3, p4);
+    }
+    public Object invoke(String dispid, Object params[])
+    {
+      return xinvokeX(dispid, params);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Native implementations of the above methods.
+    //
+    // Note: These methods are not overloaded like the public versions above
+    //       because javah generates bad code which doesn't compile if you try.
+
+    // TODO remove - this is just a sanity test
+    public native void x(int timeout);
+
+    private native Object xgetProperty(String dispid);
+    private native void xsetProperty1(String dispid, String property);
+    private native void xsetProperty2(String dispid, Object property);
+    private native Object xinvoke(String dispid);
+    private native Object xinvoke1(String dispid, Object p1);
+    private native Object xinvoke2(String dispid, Object p1, Object p2);
+    private native Object xinvoke3(String dispid, Object p1, Object p2, Object p3);
+    private native Object xinvoke4(String dispid, Object p1, Object p2, Object p3, Object p4);
+    private native Object xinvokeX(String dispid, Object params[]);
 };
