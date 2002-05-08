b5bf4ad943a9f54fe667b238406c9dc7673328eb
locka%iol.ie <locka%iol.ie>
2002-05-08 14:09:13 +0000

    NOT PART OF BUILD. Throws exceptions for errors

--- embedding/browser/activex/src/plugin/javastubs.c
+++ embedding/browser/activex/src/plugin/javastubs.c
@@ -5,6 +5,8 @@
 #define IMPLEMENT_MozAxPlugin
 #include "_stubs/MozAxPlugin.c"
 #include "_stubs/netscape_plugin_Plugin.c"
+#include "_stubs/java_lang_Throwable.c"
+#include "_stubs/java_lang_Error.c"
 #include "_stubs/java_lang_String.c"
 #include "_stubs/java_lang_Boolean.c"
 #include "_stubs/java_lang_Number.c"
