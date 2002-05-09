cd3088bdb096694e53ee6de7e93bbda963c66ce6
justdave%syndicomm.com <justdave%syndicomm.com>
2002-05-09 05:19:30 +0000

    Bumping version number to 2.16rc1

--- webtools/bugzilla/globals.pl
+++ webtools/bugzilla/globals.pl
@@ -98,7 +98,7 @@ $::SIG{TERM} = 'IGNORE';
 $::SIG{PIPE} = 'IGNORE';
 
 # Contains the version string for the current running Bugzilla.
-$::param{'version'} = '2.15';
+$::param{'version'} = '2.16rc1';
 
 $::dontchange = "--do_not_change--";
 $::chooseone = "--Choose_one:--";
