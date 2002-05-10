007f38f808f8ef072040120493b03f182505c3f0
darin@netscape.com <darin@netscape.com>
2002-05-10 18:38:09 +0000

    fixes bug 143311 "nsStreamListenerTee should completely drop mSink during OnStopRequest" r=gordon,dougt sr=rpotts

--- netwerk/base/src/nsStreamListenerTee.cpp
+++ netwerk/base/src/nsStreamListenerTee.cpp
@@ -1,3 +1,40 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2002
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Darin Fisher <darin@netscape.com> (original author)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 #include "nsStreamListenerTee.h"
 
 NS_IMPL_ISUPPORTS3(nsStreamListenerTee,
@@ -19,7 +56,11 @@ nsStreamListenerTee::OnStopRequest(nsIRequest *request,
                                    nsresult status)
 {
     NS_ENSURE_TRUE(mListener, NS_ERROR_NOT_INITIALIZED);
-    mInputTee = 0;
+    // it is critical that we close out the input stream tee
+    if (mInputTee) {
+        mInputTee->SetSink(nsnull);
+        mInputTee = 0;
+    }
     mSink = 0;
     return mListener->OnStopRequest(request, context, status);
 }
@@ -46,7 +87,7 @@ nsStreamListenerTee::OnDataAvailable(nsIRequest *request,
     }
     else {
         // re-initialize the input tee since the input stream may have changed.
-        rv = mInputTee->Init(input, mSink);
+        rv = mInputTee->SetSource(input);
         if (NS_FAILED(rv)) return rv;
 
         tee = do_QueryInterface(mInputTee, &rv);
