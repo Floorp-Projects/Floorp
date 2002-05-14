305ee08f028ab53463a2d43fdde52aa6ef8e8d0a
shliang%netscape.com <shliang%netscape.com>
2002-05-14 00:36:48 +0000

    141064 - focus for new mailing list dialog should be in list name field. r=hewitt, sr=sspitzer.

--- mailnews/addrbook/resources/content/abMailListDialog.js
+++ mailnews/addrbook/resources/content/abMailListDialog.js
@@ -245,7 +245,7 @@ function OnLoadNewMailList()
   // focus on first name
   var listName = document.getElementById('ListName');
   if ( listName )
-    listName.focus();
+    setTimeout( function(firstTextBox) { firstTextBox.focus(); }, 0, listName );
 
   moveToAlertPosition();
 }
