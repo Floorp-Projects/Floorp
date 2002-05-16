f87893977ff4ff3843b9dff804ab540e13d7b8f1
varada%netscape.com <varada%netscape.com>
2002-05-16 22:57:12 +0000

    fix for 130036;in offline mode ctrl-enter should do send later;r=ducarroz;sr=bienvenu

--- mailnews/compose/resources/content/MsgComposeCommands.js
+++ mailnews/compose/resources/content/MsgComposeCommands.js
@@ -116,7 +116,7 @@ function InitializeGlobalVariables()
   defaultSaveOperation = "draft";
   gSendOrSaveOperationInProgress = false;
   gCloseWindowAfterSave = false;
-  gIsOffline = false;
+  gIsOffline = gIOService.offline;
   gSessionAdded = false;
   gCurrentAutocompleteDirectory = null;
   gAutocompleteSession = null;
