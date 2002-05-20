eee7fc1d5fad29056c4e8ef00077e03c9ba547b9
nhotta%netscape.com <nhotta%netscape.com>
2002-05-20 19:51:16 +0000

    Change not to set the composing mail charset as attachment's chaset. bug 144917, r=ducarroz, sr=bienvenu.

--- mailnews/compose/src/nsMsgSend.cpp
+++ mailnews/compose/src/nsMsgSend.cpp
@@ -2272,8 +2272,6 @@ nsMsgComposeAndSend::AddCompFieldRemoteAttachments(PRUint32   aStartLocation,
 
           nsMsgNewURL(getter_AddRefs(m_attachments[newLoc].mURL), url.get());
 
-          PR_FREEIF(m_attachments[newLoc].m_charset);
-          m_attachments[newLoc].m_charset = PL_strdup(mCompFields->GetCharacterSet());
           PR_FREEIF(m_attachments[newLoc].m_encoding);
           m_attachments[newLoc].m_encoding = PL_strdup ("7bit");
 
