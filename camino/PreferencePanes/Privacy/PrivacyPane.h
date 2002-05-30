43ca5b70d17847fbbad11e3c6cbd746f2ec75818
pinkerton%netscape.com <pinkerton%netscape.com>
2002-05-30 19:12:35 +0000

    Updated nibs for new classnames. Made Privacy pane set prefs as soon as you click the control, not only when the panel goes away.

--- camino/PreferencePanes/Privacy/PrivacyPane.h
+++ camino/PreferencePanes/Privacy/PrivacyPane.h
@@ -1,6 +1,8 @@
 #import <Cocoa/Cocoa.h>
 #import <PreferencePanes/NSPreferencePane.h>
 
+class nsIPref;
+
 @interface OrgMozillaChimeraPreferencePrivacy : NSPreferencePane
 {
   IBOutlet id mCookies;
@@ -8,9 +10,16 @@
   
   IBOutlet NSButton* mEnableJS;
   IBOutlet NSButton* mEnableJava;
+  
+  nsIPref* mPrefService;					// strong ref, but can't use comPtr here
 }
 
 -(IBAction) clearCookies:(id)aSender;
 -(IBAction) clearDiskCache:(id)aSender;
+
+-(IBAction) clickPromptForCookie:(id)sender;
+-(IBAction) clickEnableCookies:(id)sender;
+-(IBAction) clickEnableJS:(id)sender;
+-(IBAction) clickEnableJava:(id)sender;
 
 @end
--- chimera/PreferencePanes/Privacy/PrivacyPane.mm
+++ chimera/PreferencePanes/Privacy/PrivacyPane.mm
@@ -10,75 +10,51 @@
 
 - (void) dealloc
 {
-  NSLog(@"Going away from Privacy Panel!!!!");
-
+  NS_IF_RELEASE(mPrefService);
 }
 
-- (id) initWithBundle:(NSBundle *) bundle {
+- (id) initWithBundle:(NSBundle *) bundle
+{
   self = [super initWithBundle:bundle] ;
+  
+  nsCOMPtr<nsIPref> prefService ( do_GetService(NS_PREF_CONTRACTID) );
+  NS_ASSERTION(prefService, "Could not get pref service, pref panel left uninitialized");
+  mPrefService = prefService.get();
+  NS_IF_ADDREF(mPrefService);
+  
   return self;
 }
 
 - (void)awakeFromNib
 {
-  NSLog(@"PrivacyPane awoke from nib");
 }
 
 - (void)mainViewDidLoad
 {
-  nsCOMPtr<nsIPref> prefService ( do_GetService(NS_PREF_CONTRACTID) );
-  NS_ASSERTION(prefService, "Could not get pref service, pref panel left uninitialized");
-  if ( !prefService )
+  if ( !mPrefService )
     return;
     
   // Hookup cookie prefs. Relies on the tags of the radio buttons in the matrix being
   // set such that "enable all" is 0 and "disable all" is 2.
   PRInt32 acceptCookies = 0;
-  prefService->GetIntPref("network.accept_cookies", &acceptCookies);
+  mPrefService->GetIntPref("network.accept_cookies", &acceptCookies);
   if ( acceptCookies == 1 )					// be safe in case of importing a mozilla profile
     acceptCookies = 2;
   if ( [mCookies selectCellWithTag:acceptCookies] != YES )
     NS_WARNING("Bad value for network.accept_cookies");
     
   PRBool warnAboutCookies = PR_TRUE;
-  prefService->GetBoolPref("network.cookie.warnAboutCookies", &warnAboutCookies);
+  mPrefService->GetBoolPref("network.cookie.warnAboutCookies", &warnAboutCookies);
   [mPromptForCookie setState:(warnAboutCookies ? NSOnState : NSOffState)];
   
   PRBool jsEnabled = PR_TRUE;
-  prefService->GetBoolPref("javascript.enabled", &jsEnabled);
+  mPrefService->GetBoolPref("javascript.enabled", &jsEnabled);
   [mEnableJS setState:(jsEnabled ? NSOnState : NSOffState)];
 
   PRBool javaEnabled = PR_TRUE;
-  prefService->GetBoolPref("security.enable_java", &javaEnabled);
+  mPrefService->GetBoolPref("security.enable_java", &javaEnabled);
   [mEnableJava setState:(javaEnabled ? NSOnState : NSOffState)];
 }
-
-
-//
-// didUnselect
-//
-// Called when our panel is no longer the current panel, or when the window is going
-// away. Use this as an opportunity to save the current values.
-//
-- (void)didUnselect
-{
-  nsCOMPtr<nsIPref> prefService ( do_GetService(NS_PREF_CONTRACTID) );
-  NS_ASSERTION(prefService, "Could not get pref service, pref panel values not saved");
-  if ( !prefService )
-    return;
-
-  // Save cookie prefs. Relies on the tags of the radio buttons in the matrix being
-  // set such that "enable all" is 0 and "disable all" is 2.
-  prefService->SetIntPref("network.accept_cookies", [[mCookies selectedCell] tag]);
-  prefService->SetBoolPref("network.cookie.warnAboutCookies",
-                            [mPromptForCookie state] == NSOnState ? PR_TRUE : PR_FALSE);
-  
-  prefService->SetBoolPref("javascript.enabled",
-                            [mEnableJS state] == NSOnState ? PR_TRUE : PR_FALSE);
-  prefService->SetBoolPref("security.enable_java",
-                            [mEnableJava state] == NSOnState ? PR_TRUE : PR_FALSE);
-}
-
 
 //
 // clearCookies:
@@ -92,7 +68,6 @@
     cookieMonster->RemoveAll();
 }
 
-
 //
 // clearDiskCache:
 //
@@ -103,6 +78,58 @@
   nsCOMPtr<nsICacheService> cacheServ ( do_GetService("@mozilla.org/network/cache-service;1") );
   if ( cacheServ )
     cacheServ->EvictEntries(nsICache::STORE_ON_DISK);
+}
+
+//
+// clickPromptForCookie:
+//
+// Set if the user should be prompted for each cookie
+//
+-(IBAction) clickPromptForCookie:(id)sender
+{
+  if ( !mPrefService )
+    return;
+  mPrefService->SetBoolPref("network.cookie.warnAboutCookies",
+                            [mPromptForCookie state] == NSOnState ? PR_TRUE : PR_FALSE);
+}
+
+//
+// clickEnableCookies:
+//
+// Set cookie prefs. Relies on the tags of the radio buttons in the matrix being
+// set such that "enable all" is 0 and "disable all" is 2.
+//
+-(IBAction) clickEnableCookies:(id)sender
+{
+  if ( !mPrefService )
+    return;
+  mPrefService->SetIntPref("network.accept_cookies", [[mCookies selectedCell] tag]);
+}
+
+//
+// clickEnableJS
+//
+// Set pref if JavaScript is enabled
+//
+-(IBAction) clickEnableJS:(id)sender
+{
+  if ( !mPrefService )
+    return;
+  mPrefService->SetBoolPref("javascript.enabled",
+                            [mEnableJS state] == NSOnState ? PR_TRUE : PR_FALSE);
+}
+
+//
+// clickEnableJava
+//
+// Set pref if Java is enabled
+//
+-(IBAction) clickEnableJava:(id)sender
+{
+  if ( !mPrefService )
+    return;
+  mPrefService->SetBoolPref("security.enable_java",
+                            [mEnableJava state] == NSOnState ? PR_TRUE : PR_FALSE);
 }
 
 @end
