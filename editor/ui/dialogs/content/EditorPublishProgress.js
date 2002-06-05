e36353b91a2ef0eab990640640bc9f187f19f3b6
cmanske%netscape.com <cmanske%netscape.com>
2002-06-05 01:17:33 +0000

    Improve Publishing progress and error notification, b=148425, r=brade, sr=hewitt

--- editor/ui/dialogs/content/EditorPublishProgress.js
+++ editor/ui/dialogs/content/EditorPublishProgress.js
@@ -26,11 +26,55 @@ var gPublishData;
 var gPersistObj;
 var gTotalFileCount = 0;
 var gSucceededCount = 0;
-var gFinishedCount = 0;
 var gFinished = false;
-var gFinalMessage="";
+var gPublishingFailed = false;
+var gFileNotFound = false;
+var gStatusMessage="";
+
 var gTimerID;
 var gAllowEnterKey = false;
+
+// Publishing error codes
+//   These are translated from C++ error code strings like this:
+//   kFileNotFound = "FILE_NOT_FOUND",
+const kNetReset = 2152398868; // nsISocketTransportService.idl
+const kFileNotFound = 2152857618;
+const kNotConnected = 2152398860; // in netCore.h
+const kConnectionRefused = 2152398861; // nsISocketTransportService.idl
+const kNetTimeout = 2152398862; // nsISocketTransportService.idl
+const kNoConnectionOrTimeout = 2152398878;
+const kPortAccessNotAllowed = 2152398867; // netCore.h
+const kOffline = 2152398865; // netCore.h
+const kDiskFull = 2152857610;
+const kNoDeviceSpace = 2152857616;
+const kNameTooLong = 2152857617;
+const kAccessDenied = 2152857621;
+
+// These are more errors that I don't think we encounter during publishing,
+//  so we don't have error strings yet. Let's keep them here for future reference
+//const kUnrecognizedPath = 2152857601;
+//const kUnresolvableSymlink = 2152857602;
+//const kUnknownType = 2152857604;
+//const kDestinationNotDir = 2152857605;
+//const kTargetDoesNotExist = 2152857606;
+//const kAlreadyExists = 2152857608;
+//const kInvalidPath = 2152857609;
+//const kNotDirectory = 2152857612;
+//const kIsDirectory = 2152857613;
+//const kIsLocked = 2152857614;
+//const kTooBig = 2152857615;
+//const kReadOnly = 2152857619;
+//const kDirectoryNotEmpty = 2152857620;
+//const kErrorBindingRedirected = 2152398851;
+//const kAlreadyConnected = 2152398859; // in netCore.h
+//const kInProgress = 2152398863; // netCore.h
+//const kNoContent = 2152398865; // netCore.h
+//const kUnknownProtocol = 2152398866 // netCore.h
+//const kFtpLogin = 2152398869; // ftpCore.h
+//const kFtpCWD = 2152398870; // ftpCore.h
+//const kFtpPasv = 2152398871; // ftpCore.h
+//const kFtpPwd = 2152398872; // ftpCore.h
+
 
 function Startup()
 {
@@ -45,10 +89,11 @@ function Startup()
     return;
   }
 
-  gDialog.FileList          = document.getElementById("FileList");
-  gDialog.StatusMessage     = document.getElementById("StatusMessage");
-  gDialog.KeepOpen          = document.getElementById("KeepOpen");
-  gDialog.Close             = document.documentElement.getButton("cancel");
+  gDialog.FileList           = document.getElementById("FileList");
+  gDialog.FinalStatusMessage = document.getElementById("FinalStatusMessage");
+  gDialog.StatusMessage      = document.getElementById("StatusMessage");
+  gDialog.KeepOpen           = document.getElementById("KeepOpen");
+  gDialog.Close              = document.documentElement.getButton("cancel");
 
   SetWindowLocation();
   window.title = GetString("PublishProgressCaption").replace(/%title%/, editorShell.GetDocumentTitle());
@@ -148,42 +193,109 @@ function SetProgressStatus(filename, status)
 
 function SetProgressFinished(filename, networkStatus)
 {
+  var abortPublishing = false;
   if (filename)
   {
     var status = networkStatus ? "failed" : "done";
     if (networkStatus == 0)
       gSucceededCount++;
 
-    if (SetProgressStatus(filename, status))
-      gFinishedCount++;
+    SetProgressStatus(filename, status);
   }
 
-  if (networkStatus != 0)
+  if (networkStatus != 0) // Error condition
   {
-    // XXX Interpret networkStatus and call SetStatusMessage() with 
-    //  appropriate error description.
-    if (!gPublishData.publishOtherFiles || filename == gPublishData.filename)
-      gFinalMessage = GetString("PublishFailed");
-    else
-      gFinalMessage = GetString("PublishSomeFileFailed");
+    // We abort on all errors except if image file was not found
+    abortPublishing = networkStatus != kFileNotFound;
+
+    // Mark all remaining files as "failed"
+    if (abortPublishing)
+    {
+      gPublishingFailed = true;
+      SetProgressStatusCancel();
+      gDialog.FinalStatusMessage.value = GetString("PublishFailed");
+    }
+
+    switch (networkStatus)
+    {
+      case kFileNotFound:
+        gFileNotFound = true;
+        if (filename)
+          gStatusMessage = GetString("FileNotFound").replace(/%file%/, filename);
+        break;
+      case kNetReset:
+        // We get this when subdir doesn't exist AND
+        //   if filename used is same as an existing subdir 
+        var dir = (gPublishData.filename == filename) ? 
+                     gPublishData.docDir : gPublishData.otherDir;
+
+        if (dir)
+        {
+          // This is the ambiguous case when we can't tell if subdir or filename is bad
+          // Remove terminal "/" from dir string and insert into message
+          gStatusMessage = GetString("SubdirDoesNotExist").replace(/%dir%/, dir.slice(0, dir.length-1));
+          gStatusMessage = gStatusMessage.replace(/%file%/, filename);
+
+          // Remove directory from saved prefs
+          // XXX Note that if subdir is good, 
+          //     but filename = next level subdirectory name, 
+          //     we really shouldn't remove subdirectory, 
+          //     but it's impossible to differentiate this case!
+          RemovePublishSubdirectoryFromPrefs(gPublishData, dir);
+        }
+        else if (filename)
+          gStatusMessage = GetString("FilenameIsSubdir").replace(/%file%/, filename);
+
+        break;
+      case kNotConnected:
+      case kConnectionRefused:
+      case kNetTimeout:
+      case kNoConnectionOrTimeout:
+      case kPortAccessNotAllowed:
+        gStatusMessage = GetString("ServerNotAvailable");
+        break;
+      case kOffline:
+        gStatusMessage = GetString("Offline");
+        break;
+      case kDiskFull:
+      case kNoDeviceSpace:
+        if (filename)
+          gStatusMessage = GetString("DiskFull").replace(/%file%/, filename);
+        break;
+      case kNameTooLong:
+        if (filename)
+          gStatusMessage = GetString("NameTooLong").replace(/%file%/, filename);
+        break;
+      case kAccessDenied:
+        if (filename)
+          gStatusMessage = GetString("AccessDenied").replace(/%file%/, filename);
+        break;
+      case kUnknownType:
+      default:
+        gStatusMessage = GetString("UnknownPublishError")
+        break;
+    }
   }
-  else if (gFinishedCount == gTotalFileCount || !filename)
+  else if (!filename)
   {
     gFinished = true;
     gDialog.Close.setAttribute("label", GetString("Close"));
-    gFinalMessage = GetString("PublishCompleted");
+    if (!gStatusMessage)
+      gStatusMessage = GetString(gPublishingFailed ? "UnknownPublishError" : "AllFilesPublished");
 
     // Now allow "Enter/Return" key to close the dialog
     AllowDefaultButton();
-  }
-  if (gFinalMessage)
-    SetStatusMessage(gFinalMessage);
-}
 
-function SetStatusMessage(message)
-{
-  gDialog.StatusMessage.value = message;
-  window.sizeToContent();
+    if (gPublishingFailed || gFileNotFound)
+    {
+      // Show "Troubleshooting" button to help solving problems
+      //  and key for successful / failed files
+      document.getElementById("failureBox").hidden = false;
+    }
+  }
+
+  if (gStatusMessage)
+    SetStatusMessage(gStatusMessage);
 }
 
 function CheckKeepOpen()
@@ -222,6 +334,11 @@ function AllowDefaultButton()
   gAllowEnterKey = true;
 }
 
+function doTroubleshooting()
+{
+  openHelp("comp-doc-publish-troubleshooting");
+}
+
 function onEnterKey()
 {
   if (gAllowEnterKey)
@@ -237,12 +354,42 @@ function RequestCloseDialog()
     // Leave window open a minimum amount of time 
     gTimerID = setTimeout("CloseDialog();", 3000);
   }
+  // Finish progress messages, settings buttons etc.
+  SetProgressFinished(null,0);
 
-  // Now allow "Enter/Return" key to close the dialog
-  AllowDefaultButton();
+  // Set "completed" message if we succeeded
+  // (Some image files may have failed,
+  //  but we don't abort publishing for that)
+  if (!gPublishingFailed)
+  {
+    gDialog.FinalStatusMessage.value = GetString("PublishCompleted");
+    if (gFileNotFound && gTotalFileCount-gSucceededCount)
+    {
+      // Show number of files that failed to upload
+      gStatusMessage = 
+        (GetString("FailedFileMsg").replace(/%x%/,(gTotalFileCount-gSucceededCount)))
+          .replace(/%total%/,gTotalFileCount);
 
-  // If window remains open, be sure final message is set
-  SetStatusMessage(gFinalMessage);
+      SetStatusMessage(gStatusMessage);
+    }
+  }
+}
+
+function SetStatusMessage(message)
+{
+  // Status message is a child of <description> element
+  //  so text can wrap to multiple lines if necessary
+  if (gDialog.StatusMessage.firstChild)
+  {
+    gDialog.StatusMessage.firstChild.data = message;
+  }
+  else
+  {
+    var textNode = document.createTextNode(message);
+    if (textNode)
+      gDialog.StatusMessage.appendChild(textNode);
+  }
+  window.sizeToContent();
 }
 
 function CloseDialog()
@@ -253,4 +400,3 @@ function CloseDialog()
     window.close();
   } catch (e) {}
 }
-
