e7ad8ac35b5f1a6386a74ee9a2aaaf7066ed1359
dbaron%fas.harvard.edu <dbaron%fas.harvard.edu>
2002-06-12 03:30:32 +0000

    Fix crash when looking at style rules of element matching no rules.  b=141022  r=hewitt  sr=waterson

--- extensions/inspector/base/src/inDOMUtils.cpp
+++ extensions/inspector/base/src/inDOMUtils.cpp
@@ -99,14 +99,11 @@ inDOMUtils::GetStyleRules(nsIDOMElement *aElement, nsISupportsArray **_retval)
   styleContext->GetRuleNode(&ruleNode);
   
   nsCOMPtr<nsIStyleRule> srule;
-  PRBool isRoot;
-  do {
+  for (PRBool isRoot; mCSSUtils->IsRuleNodeRoot(ruleNode, &isRoot), !isRoot;
+       mCSSUtils->GetRuleNodeParent(ruleNode, &ruleNode)) {
     mCSSUtils->GetRuleNodeRule(ruleNode, getter_AddRefs(srule));
     rules->InsertElementAt(srule, 0);
-    
-    mCSSUtils->GetRuleNodeParent(ruleNode, &ruleNode);
-    mCSSUtils->IsRuleNodeRoot(ruleNode, &isRoot);
-  } while (!isRoot);
+  }
 
   *_retval = rules;
   NS_IF_ADDREF(*_retval);
