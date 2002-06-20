ba543e2c3bbf097e9b896eff2815cdf98d191e59
srilatha%netscape.com <srilatha%netscape.com>
2002-06-20 23:48:54 +0000

    Fix for bug # 130403. When teh new card dialog is opened from selected addresses dialog, there is no selectedab. Make personal addressbook selectedab, if there is no selected addressbook. r=varada, sr=bienvenu

--- mailnews/addrbook/resources/content/abCommon.js
+++ mailnews/addrbook/resources/content/abCommon.js
@@ -790,6 +790,9 @@ function GetSelectedAddressBookDirID(abListItem)
   if (!selectedAB && abDirEntries && abDirEntries.localName == "menulist" && abDirEntries.selectedItem)
     selectedAB = abDirEntries.selectedItem.getAttribute("id");
 
+  // if we do not have a selected ab still, use personal addressbook
+  if (!selectedAB)
+    selectedAB = kPersonalAddressbookURI;
   return selectedAB;
 }
 
