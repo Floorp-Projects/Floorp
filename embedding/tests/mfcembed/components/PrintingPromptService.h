a874cc426a0763d89a786eb1b78442e0ca44a00a
rods@netscape.com <rods@netscape.com>
2002-07-01 20:30:18 +0000

    Implement nsPrintingPromptService for MfcEmbed Bug 154244 r=chak sr=rpotts

---
+++ embedding/tests/mfcembed/components/PrintingPromptService.h
@@ -0,0 +1,61 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: NPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is 
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2001
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the NPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the NPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#ifndef __PrintingPromptService_h
+#define __PrintingPromptService_h
+
+#include "nsError.h"
+
+// this component is for an MFC app; it's Windows. make sure this is defined.
+#ifndef XP_WIN
+#define XP_WIN
+#endif
+
+class nsIFactory;
+
+// factory creator, in hard and soft link formats
+extern "C" NS_EXPORT nsresult NS_NewPrintingPromptServiceFactory(nsIFactory** aFactory);
+typedef nsresult (__cdecl *MakeFactoryType)(nsIFactory **);
+#define kPrintingPromptServiceFactoryFuncName "NS_NewPrintingPromptServiceFactory"
+
+// initialization function, in hard and soft link formats
+extern "C" NS_EXPORT void InitPrintingPromptService(HINSTANCE instance);
+typedef nsresult (__cdecl *InitPrintingPromptServiceType)(HINSTANCE instance);
+#define kPrintingPromptServiceInitFuncName "InitPrintingPromptService"
+
+#endif
