bef018bda930e5ec526f47be6c45ce06436fd920
pete.zha@sun.com <pete.zha@sun.com>
2002-07-02 02:52:22 +0000

    bug 152279:Transport shouldn't fire status and progress after stop button been clicked for stopping a document load patch=antonio.xu@sun.com r=jkeiser@netscape.com sr=darin@netscape.com

--- netwerk/protocol/http/src/nsHttpChannel.cpp
+++ netwerk/protocol/http/src/nsHttpChannel.cpp
@@ -3036,7 +3036,7 @@ NS_IMETHODIMP
 nsHttpChannel::OnStatus(nsIRequest *req, nsISupports *ctx, nsresult status,
                         const PRUnichar *statusText)
 {
-    if (mProgressSink)
+    if (mProgressSink && !mCanceled)
         mProgressSink->OnStatus(this, mListenerContext, status, statusText);
 
     return NS_OK;
@@ -3047,7 +3047,7 @@ NS_IMETHODIMP
 nsHttpChannel::OnProgress(nsIRequest *req, nsISupports *ctx,
                           PRUint32 progress, PRUint32 progressMax)
 {
-    if (mProgressSink)
+    if (mProgressSink && !mCanceled)
         mProgressSink->OnProgress(this, mListenerContext, progress, progressMax);
 
     return NS_OK;
