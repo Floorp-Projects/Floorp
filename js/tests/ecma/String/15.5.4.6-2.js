601c4951e871681ac307c234a03d1f6d5d11355e
pschwartau%netscape.com <pschwartau%netscape.com>
2002-07-14 23:57:03 +0000

    Correcting errors in the test. See bug 155289.

--- js/tests/ecma/String/15.5.4.6-2.js
+++ js/tests/ecma/String/15.5.4.6-2.js
@@ -56,8 +56,13 @@
                         Therefore it can be transferred to other kinds of objects
                         for use as a method.
 
-    Author:             christine@netscape.com
-    Date:               2 october 1997
+    Author:             christine@netscape.com, pschwartau@netscape.com
+    Date:               02 October 1997
+    Modified:           14 July 2002
+    Reason:             See http://bugzilla.mozilla.org/show_bug.cgi?id=155289
+                        ECMA-262 Ed.3  Section 15.5.4.7
+                        The length property of the indexOf method is 1
+*
 */
     var SECTION = "15.5.4.6-2";
     var VERSION = "ECMA_1";
@@ -96,10 +101,10 @@ function getTestCases() {
     array[item++] = new TestCase( SECTION, "function f() { return this; }; function g() { var h = f; return h(); }; g().toString()",    GLOBAL,  g().toString() );
 
 
-    array[item++] = new TestCase( SECTION, "String.prototype.indexOf.length",                                               2,     String.prototype.indexOf.length );
-    array[item++] = new TestCase( SECTION, "String.prototype.indexOf.length = null; String.prototype.indexOf.length",       2,     eval("String.prototype.indexOf.length = null; String.prototype.indexOf.length") );
+    array[item++] = new TestCase( SECTION, "String.prototype.indexOf.length",                                               1,     String.prototype.indexOf.length );
+    array[item++] = new TestCase( SECTION, "String.prototype.indexOf.length = null; String.prototype.indexOf.length",       1,     eval("String.prototype.indexOf.length = null; String.prototype.indexOf.length") );
     array[item++] = new TestCase( SECTION, "delete String.prototype.indexOf.length",                                        false,  delete String.prototype.indexOf.length );
-    array[item++] = new TestCase( SECTION, "delete String.prototype.indexOf.length; String.prototype.indexOf.length",       2,      eval("delete String.prototype.indexOf.length; String.prototype.indexOf.length") );
+    array[item++] = new TestCase( SECTION, "delete String.prototype.indexOf.length; String.prototype.indexOf.length",       1,      eval("delete String.prototype.indexOf.length; String.prototype.indexOf.length") );
 
     array[item++] = new TestCase( SECTION, "var s = new String(); s.indexOf()",     -1,     eval("var s = new String(); s.indexOf()") );
 
