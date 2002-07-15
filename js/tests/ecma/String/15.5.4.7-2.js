39c85345875e8702e1e6481e5c29d73a90ec11b8
pschwartau%netscape.com <pschwartau%netscape.com>
2002-07-15 00:04:22 +0000

    Correcting errors in the test. See bug 155289.

--- js/tests/ecma/String/15.5.4.7-2.js
+++ js/tests/ecma/String/15.5.4.7-2.js
@@ -51,8 +51,13 @@
     Note that the lastIndexOf function is intentionally generic; it does not require that its this value be a
     String object. Therefore it can be transferred to other kinds of objects for use as a method.
 
-    Author:             christine@netscape.com
-    Date:               2 october 1997
+    Author:             christine@netscape.com, pschwartau@netscape.com
+    Date:               02 October 1997
+    Modified:           14 July 2002
+    Reason:             See http://bugzilla.mozilla.org/show_bug.cgi?id=155289
+                        ECMA-262 Ed.3  Section 15.5.4.8
+                        The length property of the lastIndexOf method is 1
+*
 */
     var SECTION = "15.5.4.7-2";
     var VERSION = "ECMA_1";
@@ -69,9 +74,9 @@ function getTestCases() {
     var array = new Array();
     var item = 0;
 
-    array[item++] = new TestCase( SECTION, "String.prototype.lastIndexOf.length",           2,          String.prototype.lastIndexOf.length );
+    array[item++] = new TestCase( SECTION, "String.prototype.lastIndexOf.length",           1,          String.prototype.lastIndexOf.length );
     array[item++] = new TestCase( SECTION, "delete String.prototype.lastIndexOf.length",    false,      delete String.prototype.lastIndexOf.length );
-    array[item++] = new TestCase( SECTION, "delete String.prototype.lastIndexOf.length; String.prototype.lastIndexOf.length",   2,  eval("delete String.prototype.lastIndexOf.length; String.prototype.lastIndexOf.length" ) );
+    array[item++] = new TestCase( SECTION, "delete String.prototype.lastIndexOf.length; String.prototype.lastIndexOf.length",   1,  eval("delete String.prototype.lastIndexOf.length; String.prototype.lastIndexOf.length" ) );
 
     array[item++] = new TestCase( SECTION, "var s = new String(''); s.lastIndexOf('', 0)",          LastIndexOf("","",0),  eval("var s = new String(''); s.lastIndexOf('', 0)") );
     array[item++] = new TestCase( SECTION, "var s = new String(''); s.lastIndexOf('')",             LastIndexOf("",""),  eval("var s = new String(''); s.lastIndexOf('')") );
