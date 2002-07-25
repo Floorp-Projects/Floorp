ac32f38594e318f70d5183978e0c91c53ad3fa79
jrgm%netscape.com <jrgm%netscape.com>
2002-07-25 00:38:23 +0000

    bug 158072: 'remove dead function selectLocale from strres.js', r=timeless, sr=jag, a=asa

--- xpfe/global/resources/content/strres.js
+++ xpfe/global/resources/content/strres.js
@@ -22,31 +22,3 @@ function srGetStrBundle(path)
   }
   return strBundle;
 }
-
-function selectLocale(event)
-{
-  try {
-    var node = event.target;
-    var langcode = node.getAttribute('value');
-    if (langcode)
-    {
-      var chromeRegistry = Components.classes["@mozilla.org/chrome/chrome-registry;1"].getService(Components.interfaces.nsIXULChromeRegistry);
-      chromeRegistry.selectLocale(langcode, true);
-      var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
-      observerService.notifyObservers(null, "locale-selected", null);
-      var prefUtilBundle = srGetStrBundle("chrome://communicator/locale/pref/prefutilities.properties");
-      var brandBundle = srGetStrBundle("chrome://global/locale/brand.properties");
-      var alertText = prefUtilBundle.GetStringFromName("languageAlert");
-      var titleText = prefUtilBundle.GetStringFromName("languageTitle");
-      alertText = alertText.replace(/%brand%/g, brandBundle.GetStringFromName("brandShortName"));
-      var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService();
-      promptService = promptService.QueryInterface(Components.interfaces.nsIPromptService)
-      promptService.alert(window, titleText, alertText);
-    }
-  }
-  catch(e) {
-    return false;
-  }
-  return true;	
-}
-
