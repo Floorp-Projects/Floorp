a064c6edca206299f3ccec06a4045e536f1cf0d8
dcone%netscape.com <dcone%netscape.com>
2002-07-29 17:48:22 +0000

    Backing out changes.  Something is interacting wrong with scroll bars.. so I will investigate.

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -3113,13 +3113,14 @@ nsCSSRendering::PaintBackgroundWithSC(nsIPresContext* aPresContext,
       y1 = y0 + tileHeight;
     }
 
+    nsRect tileRect(x0,y0,(x1-x0),(y1-y0));
+
 #ifdef XP_WIN
-    PRInt32 xOffset = dirtyRect.x - x0,
-            yOffset = dirtyRect.y - y0;
-    aRenderingContext.DrawTile(image,xOffset,yOffset,&dirtyRect);
+    PRInt32 xOffset = tileRect.x - x0,
+            yOffset = tileRect.y - y0;
+    aRenderingContext.DrawTile(image,xOffset,yOffset,&tileRect);
 #else
     nsRect drawRect;
-    nsRect tileRect(x0,y0,(x1-x0),(y1-y0));
 
     if (drawRect.IntersectRect(tileRect, dirtyRect)) {
       PRInt32 xOffset = drawRect.x - x0,
