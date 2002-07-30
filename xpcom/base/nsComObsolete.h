d0cfcc1da2ca8e3163558fdfb1c612cffd233e50
bryner%netscape.com <bryner%netscape.com>
2002-07-30 21:30:05 +0000

    Remove obsolete NS_BASE and _IMPL_NS_BASE, and unused/obsolete source files nsStdFileStream.* (bug 160052).  r=cls, sr=alecf, a=asa.

--- xpcom/base/nsComObsolete.h
+++ xpcom/base/nsComObsolete.h
@@ -37,12 +37,6 @@
 #define nsComObsolete_h__
 
 /* These _IMPL_NS_*  defines should move into their own directories.  */
-#ifdef _IMPL_NS_BASE
-#define NS_BASE NS_EXPORT
-#else
-#define NS_BASE NS_IMPORT
-#endif
-
 #ifdef _IMPL_NS_DOM
 #define NS_DOM NS_EXPORT
 #else
