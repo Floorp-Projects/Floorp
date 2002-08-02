ca494f80d1cb227859fc4de5163d49582d7c7650
sfraser%netscape.com <sfraser%netscape.com>
2002-08-02 00:51:08 +0000

    Fix for bug 157837 -- fix a problem with click-hold scrolling in scrollbars that caused the slightest mouse move to stop the scrolling; patch fixes a coordinate issue that made us think we'd left the widget. r=pinkerton, sr=bryner, a=asa.

--- widget/src/mac/nsMacEventHandler.cpp
+++ widget/src/mac/nsMacEventHandler.cpp
@@ -1672,7 +1672,12 @@ PRBool nsMacEventHandler::HandleMouseMoveEvent( EventRecord& aOSEvent )
 	if (lastWidgetHit)
 	{
 		Point macPoint = aOSEvent.where;
-		::GlobalToLocal(&macPoint);
+		WindowRef wind = reinterpret_cast<WindowRef>(mTopLevelWidget->GetNativeData(NS_NATIVE_DISPLAY));
+		nsGraphicsUtils::SafeSetPortWindowPort(wind);
+		{
+			StOriginSetter  originSetter(wind);
+			::GlobalToLocal(&macPoint);
+		}
 		PRBool inWidgetHit = lastWidgetHit->PointInWidget(macPoint);
 		if (sMouseInWidgetHit != inWidgetHit)
 		{
