178cd7c22bff876e2e08aaeef6492b95c1ab23c9
glen.beasley%sun.com <glen.beasley%sun.com>
2002-08-06 16:41:43 +0000

    commented debug statement

--- security/jss/org/mozilla/jss/ssl/SocketProxy.java
+++ security/jss/org/mozilla/jss/ssl/SocketProxy.java
@@ -1,27 +1,27 @@
-/* 
+/*
  * The contents of this file are subject to the Mozilla Public
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
  * the License at http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS
  * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
  * implied. See the License for the specific language governing
  * rights and limitations under the License.
- * 
+ *
  * The Original Code is the Netscape Security Services for Java.
- * 
+ *
  * The Initial Developer of the Original Code is Netscape
- * Communications Corporation.  Portions created by Netscape are 
+ * Communications Corporation.  Portions created by Netscape are
  * Copyright (C) 2001 Netscape Communications Corporation.  All
  * Rights Reserved.
- * 
+ *
  * Contributor(s):
- * 
+ *
  * Alternatively, the contents of this file may be used under the
  * terms of the GNU General Public License Version 2 or later (the
- * "GPL"), in which case the provisions of the GPL are applicable 
- * instead of those above.  If you wish to allow use of your 
+ * "GPL"), in which case the provisions of the GPL are applicable
+ * instead of those above.  If you wish to allow use of your
  * version of this file only under the terms of the GPL and not to
  * allow others to use your version of this file under the MPL,
  * indicate your decision by deleting the provisions above and
@@ -42,7 +42,7 @@ class SocketProxy extends org.mozilla.jss.util.NativeProxy {
     protected native void releaseNativeResources();
 
     protected void finalize() throws Throwable {
-        System.out.println("In SocketProxy.finalize");
+//        System.out.println("In SocketProxy.finalize");
         super.finalize();
     }
 }
