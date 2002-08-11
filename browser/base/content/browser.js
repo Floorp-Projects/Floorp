324510a9fa3f894a9ed1d6102d966ca8b2170f36
blakeross%telocity.com <blakeross%telocity.com>
2002-08-11 01:25:32 +0000

    Add min-height to personal toolbar.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -390,6 +390,7 @@ function LoadBookmarksCallback()
     personalToolbar.builder.rebuild();
   } catch (e) {
   }
+  var ptoolbar = document.getElementById("PersonalToolbar");
 }
 
 function WindowFocusTimerCallback(element)
