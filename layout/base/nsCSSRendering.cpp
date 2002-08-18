53276bf5e025bc95284d1f91ae7eb63270a51326
dcone%netscape.com <dcone%netscape.com>
2002-08-18 00:46:00 +0000

    b=148598 r=mcclusky,sr=kin  Speed up scrolling

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -3143,11 +3143,6 @@ nsCSSRendering::PaintBackgroundWithSC(nsIPresContext* aPresContext,
 
     // Take the intersection again to paint only the required area
     nsRect tileRect(x0,y0,(x1-x0),(y1-y0));
-#ifdef XP_WIN
-    PRInt32 xOffset = tileRect.x - x0,
-            yOffset = tileRect.y - y0;
-    aRenderingContext.DrawTile(image,xOffset,yOffset,&tileRect);
-#else
     nsRect drawRect;
 
     if (drawRect.IntersectRect(tileRect, dirtyRect)) {
@@ -3155,7 +3150,6 @@ nsCSSRendering::PaintBackgroundWithSC(nsIPresContext* aPresContext,
               yOffset = drawRect.y - y0;
       aRenderingContext.DrawTile(image,xOffset,yOffset,&drawRect);
     }
-#endif
 
 #if (!defined(XP_UNIX) && !defined(XP_BEOS)) || defined(XP_MACOSX)
     // Restore clipping
