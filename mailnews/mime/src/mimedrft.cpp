e4bf934f3fef1ef3d861c567fd159889271f24cd
ducarroz%netscape.com <ducarroz%netscape.com>
2002-08-19 21:40:23 +0000

    Fix for bug 162961. Don't set decrypt_p to true if smime is not enable. R=varada, SR=bienvenu

--- mailnews/mime/src/mimedrft.cpp
+++ mailnews/mime/src/mimedrft.cpp
@@ -1293,7 +1293,7 @@ mime_parse_stream_complete (nsMIMESession *stream)
     {
       // save the override flag before it's unavailable
       charsetOverride = mdd->options->override_charset;
-      if ((!mdd->mailcharset || charsetOverride) && mdd->options->default_charset)
+      if ((!mdd->mailcharset || charsetOverride) && mdd->options->default_charset) 
       {
         PR_Free(mdd->mailcharset);
         mdd->mailcharset = nsCRT::strdup(mdd->options->default_charset);
@@ -2092,12 +2092,14 @@ mime_bridge_create_draft_stream(
   if (! (mdd->options->prefs && NS_SUCCEEDED(rv)))
     goto FAIL;
 
+#ifdef ENABLE_SMIME
   /* If we're attaching a message (for forwarding) then we must eradicate all
    traces of xlateion from it, since forwarding someone else a message
    that wasn't xlated for them doesn't work.  We have to dexlate it
    before sending it.
    */
   mdd->options->decrypt_p = PR_TRUE;
+#endif /* ENABLE_SMIME */
 
   obj = mime_new ( (MimeObjectClass *) &mimeMessageClass, (MimeHeaders *) NULL, MESSAGE_RFC822 );
   if ( !obj ) 
