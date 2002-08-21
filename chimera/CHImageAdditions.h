5899a398af75a6c96d8ed2f67780fac6240ab0c0
sfraser@netscape.com <sfraser@netscape.com>
2002-08-21 22:59:47 +0000

    Removing files which have been moved to other locations under mozilla/chimera

--- chimera/CHImageAdditions.h
+++
@@ -1,7 +0,0 @@
-#import <AppKit/NSImage.h>
-
-@interface NSImage (CHImageAdditions)
-- (void) drawFlippedInRect:(NSRect) rect operation:(NSCompositingOperation) op fraction:(float) delta;
-- (void) drawFlippedInRect:(NSRect) rect operation:(NSCompositingOperation) op;
-- (void)applyBadge:(NSImage*)badge withAlpha:(float)alpha scale:(float)scale;
-@end
