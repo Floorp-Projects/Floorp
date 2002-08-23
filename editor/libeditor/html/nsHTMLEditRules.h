a7a210c2d7c4ea706b6c8b2f46e917b870ca8bbe
jfrancis@netscape.com <jfrancis@netscape.com>
2002-08-23 18:14:30 +0000

    fix for 129763:  edit this page and delete Personals section causes stray bullet r=glazman; sr=kin

--- editor/libeditor/html/nsHTMLEditRules.h
+++ editor/libeditor/html/nsHTMLEditRules.h
@@ -186,6 +186,7 @@ protected:
                                       PRBool *aHandled);
   nsresult CheckForInvisibleBR(nsIDOMNode *aBlock, nsHTMLEditRules::BRLocation aWhere, 
                                nsCOMPtr<nsIDOMNode> *outBRNode, PRInt32 aOffset=0);
+  PRBool ExpandSelectionForDeletion(nsISelection *aSelection);
   PRBool IsFirstNode(nsIDOMNode *aNode);
   PRBool IsLastNode(nsIDOMNode *aNode);
 #ifdef XXX_DEAD_CODE
