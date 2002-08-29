32844f7719324eafc51b120b59e57d335acee066
jkeiser%netscape.com <jkeiser%netscape.com>
2002-08-29 04:05:39 +0000

    Make anonymous content inaccessible to web content (bug 164086), r=sicking@bigfoot.com, sr=jst@netscape.com

--- content/html/content/src/nsAttributeContent.cpp
+++ content/html/content/src/nsAttributeContent.cpp
@@ -97,6 +97,8 @@ public:
   NS_IMETHOD SetDocument(nsIDocument* aDocument, PRBool aDeep, PRBool aCompileEventHandlers);
   NS_IMETHOD GetParent(nsIContent*& aResult) const;
   NS_IMETHOD SetParent(nsIContent* aParent);
+  NS_IMETHOD_(PRBool) IsNativeAnonymous() const { return PR_TRUE; }
+  NS_IMETHOD_(void) SetNativeAnonymous(PRBool aAnonymous) { }
 
   NS_IMETHOD GetNameSpaceID(PRInt32& aID) const {
     aID = kNameSpaceID_None;
