71be1a82e34cab92e26f7bc805df07d5a04d7bd1
katakai@japan.sun.com <katakai@japan.sun.com>
2002-09-03 08:18:17 +0000

    bug 166152 hindShaper should be hindiShaper in allmakefiles.sh r=cls,sr=bzbarsky

--- allmakefiles.sh
+++ allmakefiles.sh
@@ -203,7 +203,7 @@ intl/ctl/public/Makefile
 intl/ctl/src/Makefile
 intl/ctl/src/pangoLite/Makefile
 intl/ctl/src/thaiShaper/Makefile
-intl/ctl/src/hindShaper/Makefile
+intl/ctl/src/hindiShaper/Makefile
 "
 fi
 
