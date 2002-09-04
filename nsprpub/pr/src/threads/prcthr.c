e48c896a05490d482e47cc0fb45e034d408cb2d4
wtc%netscape.com <wtc%netscape.com>
2002-09-04 01:27:00 +0000

    Bug 166257: removed an unused variable.  Thanks to timeless@bemail.org for the fix.

--- nsprpub/pr/src/threads/prcthr.c
+++ nsprpub/pr/src/threads/prcthr.c
@@ -55,8 +55,6 @@ extern PRLock *_pr_sleeplock;  /* allocated and initialized in prinit */
 */
 void _PR_CleanupThread(PRThread *thread)
 {
-    PRUintn i;
-
     /* Free up per-thread-data */
     _PR_DestroyThreadPrivate(thread);
 
