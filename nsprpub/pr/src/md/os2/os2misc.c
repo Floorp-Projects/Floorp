da69ffc0f97e5f6d56d48d1b6435f299fd2c6d74
wtc@netscape.com <wtc@netscape.com>
2002-09-06 01:37:32 +0000

    Bug 166716: do not assert in the unimplemented memory-mapped file functions.  It is sufficient for them to fail with the not implemented error. Modified Files: bmmap.c macio.c os2misc.c

--- nsprpub/pr/src/md/os2/os2misc.c
+++ nsprpub/pr/src/md/os2/os2misc.c
@@ -452,14 +452,12 @@ _PR_MD_WAKEUP_CPUS( void )
 
 PRStatus _MD_CreateFileMap(PRFileMap *fmap, PRInt64 size)
 {
-    PR_ASSERT(!"Not implemented");
     PR_SetError(PR_NOT_IMPLEMENTED_ERROR, 0);
     return PR_FAILURE;
 }
 
 PRInt32 _MD_GetMemMapAlignment(void)
 {
-    PR_ASSERT(!"Not implemented");
     PR_SetError(PR_NOT_IMPLEMENTED_ERROR, 0);
     return -1;
 }
@@ -469,21 +467,18 @@ void * _MD_MemMap(
     PROffset64 offset,
     PRUint32 len)
 {
-    PR_ASSERT(!"Not implemented");
     PR_SetError(PR_NOT_IMPLEMENTED_ERROR, 0);
     return NULL;
 }
 
 PRStatus _MD_MemUnmap(void *addr, PRUint32 len)
 {
-    PR_ASSERT(!"Not implemented");
     PR_SetError(PR_NOT_IMPLEMENTED_ERROR, 0);
     return PR_FAILURE;
 }
 
 PRStatus _MD_CloseFileMap(PRFileMap *fmap)
 {
-    PR_ASSERT(!"Not implemented");
     PR_SetError(PR_NOT_IMPLEMENTED_ERROR, 0);
     return PR_FAILURE;
 }
