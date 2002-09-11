04efa431f3119992a6860a95564be78ab278c042
katakai%japan.sun.com <katakai%japan.sun.com>
2002-09-11 01:52:34 +0000

    bug 167040 CTL: caret does not move to next/prev line on text area r=Roland.Mainz@informatik.med.uni-giessen.de,sr=scc (CTL is not part of default build)

--- layout/html/base/src/nsTextFrame.cpp
+++ layout/html/base/src/nsTextFrame.cpp
@@ -4058,11 +4058,16 @@ nsTextFrame::PeekOffset(nsIPresContext* aPresContext, nsPeekOffsetStruct *aPos)
 #ifdef SUNCTL
         }
         else {
-          PRInt32 mPreviousOffset;
-          mCtlObj->PrevCluster((const PRUnichar*)paintBuffer.mBuffer,
+          if (aPos->mStartOffset < 1) {
+            // go to prev
+            i = -1;
+          } else {
+            PRInt32 mPreviousOffset;
+            mCtlObj->PrevCluster(NS_REINTERPRET_CAST(const PRUnichar*, paintBuffer.mBuffer),
                                textLength,aPos->mStartOffset, 
                                &mPreviousOffset);
-          aPos->mContentOffset = i = mPreviousOffset;
+            aPos->mContentOffset = i = mPreviousOffset;
+          }
         }
 #endif /* SUNCTL */
 
@@ -4102,11 +4107,16 @@ nsTextFrame::PeekOffset(nsIPresContext* aPresContext, nsPeekOffsetStruct *aPos)
 #ifdef SUNCTL
         }
         else {
-          PRInt32 mNextOffset;
-          mCtlObj->NextCluster((const PRUnichar*)paintBuffer.mBuffer,
+          if (aPos->mStartOffset >= textLength) {
+            // go to next
+            i = mContentLength + 1;
+          } else {
+            PRInt32 mNextOffset;
+            mCtlObj->NextCluster(NS_REINTERPRET_CAST(const PRUnichar*, paintBuffer.mBuffer),
                                textLength, aPos->mStartOffset,
                                &mNextOffset);
-          aPos->mContentOffset = i = mNextOffset;
+            aPos->mContentOffset = i = mNextOffset;
+          }
         }
 #endif /* SUNCTL */
 
