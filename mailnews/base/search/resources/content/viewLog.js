ab849a173f3dfca24f48ef14b8f6b953eb7c4451
sspitzer%netscape.com <sspitzer%netscape.com>
2002-09-12 06:50:04 +0000

    more work on filter log viewer.  not part of the build yet.

--- mailnews/base/search/resources/content/viewLog.js
+++ mailnews/base/search/resources/content/viewLog.js
@@ -4,10 +4,15 @@ var gLogFilters;
 function onLoad()
 {
   gFilterList = window.arguments[0].filterList;
+
   gLogFilters = document.getElementById("logFilters");
   gLogFilters.checked = gFilterList.loggingEnabled;
 
   var logView = document.getElementById("logView");
+
+  // for security, disable JS
+  logView.docShell.allowJavascript = false;
+
   // won't work yet
   // Security Error: Content at chrome://messenger/content/filterLog.xul 
   // may not load or link to file:///C|/Documents%20and%20Settings/Administrator/Application%20Data/Mozilla/Profiles/sspitzer/js48p4bs.slt/ImapMail/nsmail-1/filterlog.html.
@@ -16,9 +21,14 @@ function onLoad()
 
 function toggleLogFilters()
 {
-   gFilterList.loggingEnabled = gLogFilters.checked;
+  gFilterList.enableLogging = gLogFilters.checked;
 }
 
 function clearLog()
 {
+  // var logURL = gFilterList.logURL;
+  // gFilterList.logStream = null;
+  // get file for logURL;
+  // truncate file
+  // reload blank log file in viewer
 }
