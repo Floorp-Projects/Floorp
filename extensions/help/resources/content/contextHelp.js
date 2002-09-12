f0331f90f8da831c58dc689e329dcdb204495c79
oeschger%netscape.com <oeschger%netscape.com>
2002-09-12 23:53:51 +0000

    adding flag to make help window float on top, r=danm, sr=alecf, bug=136647

--- extensions/help/resources/content/contextHelp.js
+++ extensions/help/resources/content/contextHelp.js
@@ -1,3 +1,23 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * The Original Code is Mozilla Communicator client code, released
+ * March 31, 1998.
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation. Portions created by Netscape are
+ * Copyright (C) 1998-1999 Netscape Communications Corporation. All
+ * Rights Reserved.
+ */
+
 const MOZ_HELP_URI = "chrome://help/content/help.xul";
 const MOZILLA_HELP = "chrome://help/locale/mozillahelp.rdf";
 var helpFileURI = MOZILLA_HELP;
@@ -11,7 +31,7 @@ function openHelp(topic) {
     topWindow.displayTopic(topic);
   } else {
       var encodedURI = encodeURIComponent(helpFileURI + "?" + ((topic)?topic:""));  
-      window.open(MOZ_HELP_URI + "?" + encodedURI, "_blank", "chrome,menubar,toolbar,dialog=no,resizable,scrollbars");
+      window.open(MOZ_HELP_URI + "?" + encodedURI, "_blank", "chrome,menubar,toolbar,dialog=no,resizable,scrollbars,alwaysRaised");
   }
 }
 
