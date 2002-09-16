3558a42662ad314c30f1db6aea377e967ee63670
jpierre%netscape.com <jpierre%netscape.com>
2002-09-16 09:32:09 +0000

    Undo check-in that broke QA

--- security/nss/lib/freebl/rijndael.c
+++ security/nss/lib/freebl/rijndael.c
@@ -30,7 +30,7 @@
  * may use your version of this file under either the MPL or the
  * GPL.
  *
- * $Id: rijndael.c,v 1.13 2002/09/14 06:42:39 jpierre%netscape.com Exp $
+ * $Id: rijndael.c,v 1.14 2002/09/16 09:32:09 jpierre%netscape.com Exp $
  */
 
 #include "prinit.h"
@@ -839,7 +839,7 @@ static SECStatus
 rijndael_encryptECB(AESContext *cx, unsigned char *output,
                     unsigned int *outputLen, unsigned int maxOutputLen,
                     const unsigned char *input, unsigned int inputLen, 
-                    unsigned int blocksize)
+                    int blocksize)
 {
     SECStatus rv;
     AESBlockFunc *encryptor;
@@ -861,9 +861,9 @@ static SECStatus
 rijndael_encryptCBC(AESContext *cx, unsigned char *output,
                     unsigned int *outputLen, unsigned int maxOutputLen,
                     const unsigned char *input, unsigned int inputLen, 
-                    unsigned int blocksize)
+                    int blocksize)
 {
-    unsigned int j;
+    int j;
     SECStatus rv;
     AESBlockFunc *encryptor;
     unsigned char *lastblock;
@@ -897,7 +897,7 @@ static SECStatus
 rijndael_decryptECB(AESContext *cx, unsigned char *output,
                     unsigned int *outputLen, unsigned int maxOutputLen,
                     const unsigned char *input, unsigned int inputLen, 
-                    unsigned int blocksize)
+                    int blocksize)
 {
     SECStatus rv;
     AESBlockFunc *decryptor;
@@ -919,13 +919,13 @@ static SECStatus
 rijndael_decryptCBC(AESContext *cx, unsigned char *output,
                     unsigned int *outputLen, unsigned int maxOutputLen,
                     const unsigned char *input, unsigned int inputLen, 
-                    unsigned int blocksize)
+                    int blocksize)
 {
     SECStatus rv;
     AESBlockFunc *decryptor;
     const unsigned char *in;
     unsigned char *out;
-    unsigned int j;
+    int j;
     unsigned char newIV[RIJNDAEL_MAX_BLOCKSIZE];
 
     if (!inputLen) 
