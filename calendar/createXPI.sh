4d2ae7c37364a061b6290e014ee3bbc7375b8999
mostafah%oeone.com <mostafah%oeone.com>
2002-09-17 15:28:06 +0000

    Cleaned up xpi script

--- calendar/createXPI.sh
+++ calendar/createXPI.sh
@@ -1,1 +1,30 @@
-./createBuildId.pl resources/content/about.html; cd linux/components/; chmod 644 *.*; cd ..; find components/ resources/ install.js \( -name CVS -o -name Makefile -o -name makefile.win -o -name Makefile.in -o -name .cvsignore \) -prune -o -print | xargs zip calendar_linux.xpi; mv calendar_linux.xpi ../; cd ../mac; cd components; chmod 644 *.*; cd ..; find components/ resources/ install.js \( -name CVS -o -name Makefile -o -name makefile.win -o -name Makefile.in -o -name .cvsignore \) -prune -o -print | xargs zip calendar_mac.xpi; mv calendar_mac.xpi ../; cd ../windows; find components/ resources/ bin/ install.js \( -name CVS -o -name Makefile -o -name makefile.win -o -name Makefile.in -o -name .cvsignore \) -prune -o -print | xargs zip calendar_windows.xpi; mv calendar_windows.xpi ../; cd ..
+#!/bin/bash
+rm -f calendar_windows.xpi
+rm -f calendar_linux.xpi
+./createBuildId.pl resources/content/about.html
+
+#Linux
+echo "Building Linux xpi..."
+cd linux/components
+chmod 644 *.*
+cd ..
+find components/ resources/ install.js \( -name CVS -o -name Makefile -o -name makefile.win -o -name Makefile.in -o -name .cvsignore \) -prune -o -print | xargs zip calendar_linux.xpi
+mv calendar_linux.xpi ../
+echo "Done."
+
+#Windows
+echo "Building Windows xpi..."
+cd ../windows
+find components/ resources/ bin/ install.js \( -name CVS -o -name Makefile -o -name makefile.win -o -name Makefile.in -o -name .cvsignore \) -prune -o -print | xargs zip calendar_windows.xpi
+mv calendar_windows.xpi ../
+echo "Done."
+cd ..
+
+#Mac
+#cd ../mac
+#cd components
+#chmod 644 *.*
+#cd ..
+#find components/ resources/ install.js \( -name CVS -o -name Makefile -o -name makefile.win -o -name Makefile.in -o -name .cvsignore \) -prune -o -print | xargs zip calendar_mac.xpi
+#mv calendar_mac.xpi ../
+
