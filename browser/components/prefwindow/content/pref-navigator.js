1a81e4574fe43d1b92954bbb82b0daace921ee97
blakeross%telocity.com <blakeross%telocity.com>
2002-09-18 04:04:01 +0000

    Prefs tweaks in preparation for 0.1

--- browser/components/prefwindow/content/pref-navigator.js
+++ browser/components/prefwindow/content/pref-navigator.js
@@ -11,40 +11,6 @@ const FILEPICKER_CONTRACTID     = "@mozilla.org/filepicker;1";
 const WINDOWMEDIATOR_CONTRACTID = "@mozilla.org/appshell/window-mediator;1";
 const PREFSERVICE_CONTRACTID    = "@mozilla.org/preferences-service;1";
 
-function Startup()
-{
-  var clearHistButton = document.getElementById("browserClearHistory");
-  try {
-    var urlBarHist = Components.classes["@mozilla.org/browser/urlbarhistory;1"]
-                               .getService(Components.interfaces.nsIUrlbarHistory);
-    var isBtnLocked = parent.hPrefWindow.getPrefIsLocked(clearHistButton.getAttribute("prefstring"));
-    var globalHistory = Components.classes["@mozilla.org/browser/global-history;1"]
-                                  .getService(Components.interfaces.nsIBrowserHistory);
-    clearHistButton.disabled = ( urlBarHist.count == 0 && globalHistory.count == 0) || isBtnLocked;
-  }
-  catch(ex) {
-  }
-}
-
-function selectFile()
-{
-  var fp = Components.classes[FILEPICKER_CONTRACTID]
-                     .createInstance(nsIFilePicker);
-
-  var prefutilitiesBundle = document.getElementById("bundle_prefutilities");
-  var title = prefutilitiesBundle.getString("choosehomepage");
-  fp.init(window, title, nsIFilePicker.modeOpen);
-  fp.appendFilters(nsIFilePicker.filterAll | nsIFilePicker.filterText |
-                   nsIFilePicker.filterXML | nsIFilePicker.filterHTML |
-                   nsIFilePicker.filterImages);
-
-  var ret = fp.show();
-  if (ret == nsIFilePicker.returnOK) {
-    var folderField = document.getElementById("browserStartupHomepage");
-    folderField.value = fp.fileURL.spec;
-  }
-}
-
 function setHomePageToCurrentPage()
 {
   var windowManager = Components.classes[WINDOWMEDIATOR_CONTRACTID]
@@ -70,14 +36,4 @@ function setHomePageToDefaultPage()
                                   nsIPrefLocalizedString).data;
   var homePageField = document.getElementById("browserStartupHomepage");
   homePageField.value = url;
-}
-
-function prefClearGlobalHistory()
-{
-  var globalHistory = Components.classes["@mozilla.org/browser/global-history;1"]
-                                .getService(Components.interfaces.nsIBrowserHistory);
-  globalHistory.removeAllPages();
-  var urlBarHistory = Components.classes["@mozilla.org/browser/urlbarhistory;1"]
-                                .getService(Components.interfaces.nsIUrlbarHistory);
-  urlBarHistory.clearHistory();
 }
