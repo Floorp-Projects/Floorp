4c2de189a2ddcbd78143ad985d18c2f80fd5e036
bstell@ix.netcom.com <bstell@ix.netcom.com>
2002-09-27 07:13:49 +0000

    bug 169695 r=smontagu, sr=kin@netscape.com move gdk performance code out of nsRenderingContextGTK.cpp since it is not directly related to the rendering context

--- gfx/src/gtk/nsRenderingContextGTK.cpp
+++ gfx/src/gtk/nsRenderingContextGTK.cpp
@@ -40,6 +40,7 @@
 
 #include "nsFontMetricsGTK.h"
 #include "nsRenderingContextGTK.h"
+#include "nsGdkUtils.h"
 #include "nsRegionGTK.h"
 #include "nsImageGTK.h"
 #include "nsGraphicsStateGTK.h"
@@ -2632,99 +2633,6 @@ nsRenderingContextGTK::GetBoundingMetrics(const PRUnichar*   aString,
   return NS_OK;
 }
 #endif /* MOZ_MATHML */
-
-
-/* static */ void
-nsRenderingContextGTK::my_gdk_draw_text (GdkDrawable *drawable,
-                                         GdkFont     *font,
-                                         GdkGC       *gc,
-                                         gint         x,
-                                         gint         y,
-                                         const gchar *text,
-                                         gint         text_length)
-{
-#ifdef MOZ_WIDGET_GTK
-  GdkWindowPrivate *drawable_private;
-  GdkFontPrivate *font_private;
-  GdkGCPrivate *gc_private;
-#endif /* MOZ_WIDGET_GTK */
-
-  g_return_if_fail (drawable != NULL);
-  g_return_if_fail (font != NULL);
-  g_return_if_fail (gc != NULL);
-  g_return_if_fail (text != NULL);
-
-#ifdef MOZ_WIDGET_GTK
-  drawable_private = (GdkWindowPrivate*) drawable;
-  if (drawable_private->destroyed)
-    return;
-
-  gc_private = (GdkGCPrivate*) gc;
-  font_private = (GdkFontPrivate*) font;
-#endif /* MOZ_WIDGET_GTK */
-#ifdef MOZ_WIDGET_GTK2
-  if (GDK_IS_WINDOW(drawable) && GDK_WINDOW_OBJECT(drawable)->destroyed)
-    return;
-#endif /* MOZ_WIDGET_GTK2 */
-
-  if (font->type == GDK_FONT_FONT)
-  {
-#ifdef MOZ_WIDGET_GTK
-    XFontStruct *xfont = (XFontStruct *) font_private->xfont;
-#endif /* MOZ_WIDGET_GTK */
-#ifdef MOZ_WIDGET_GTK2
-    XFontStruct *xfont = (XFontStruct *) GDK_FONT_XFONT(font);
-#endif /* MOZ_WIDGET_GTK2 */
-
-    // gdk does this... we don't need it..
-    //    XSetFont(drawable_private->xdisplay, gc_private->xgc, xfont->fid);
-
-    // We clamp the sizes down to 32768 which is the maximum width of
-    // a window.  Even if a font was 1 pixel high and started at the
-    // left, the maximum size of a draw request could only be 32k.
-
-    if ((xfont->min_byte1 == 0) && (xfont->max_byte1 == 0))
-    {
-#ifdef MOZ_WIDGET_GTK
-      XDrawString (drawable_private->xdisplay, drawable_private->xwindow,
-                   gc_private->xgc, x, y, text, MIN(text_length, 32768));
-#endif /* MOZ_WIDGET_GTK */
-#ifdef MOZ_WIDGET_GTK2
-      XDrawString (GDK_WINDOW_XDISPLAY(drawable), GDK_DRAWABLE_XID(drawable),
-                   GDK_GC_XGC(gc), x, y, text, MIN(text_length, 32768));
-#endif /* MOZ_WIDGET_GTK2 */
-    }
-    else
-    {
-#ifdef MOZ_WIDGET_GTK
-      XDrawString16 (drawable_private->xdisplay, drawable_private->xwindow,
-                     gc_private->xgc, x, y, (XChar2b *) text, 
-                     MIN((text_length / 2), 32768));
-#endif /* MOZ_WIDGET_GTK */
-#ifdef MOZ_WIDGET_GTK2
-      XDrawString16 (GDK_WINDOW_XDISPLAY(drawable), GDK_DRAWABLE_XID(drawable),
-                     GDK_GC_XGC(gc), x, y, (XChar2b *) text, 
-                     MIN((text_length / 2), 32768));
-#endif /* MOZ_WIDGET_GTK2 */
-    }
-  }
-  else if (font->type == GDK_FONT_FONTSET)
-  {
-#ifdef MOZ_WIDGET_GTK
-    XFontSet fontset = (XFontSet) font_private->xfont;
-    XmbDrawString (drawable_private->xdisplay, drawable_private->xwindow,
-                   fontset, gc_private->xgc, x, y, text, text_length);
-#endif /* MOZ_WIDGET_GTK */
-#ifdef MOZ_WIDGET_GTK2
-    XFontSet fontset = (XFontSet) GDK_FONT_XFONT(font);
-    XmbDrawString (GDK_WINDOW_XDISPLAY(drawable), GDK_DRAWABLE_XID(drawable),
-                   fontset, GDK_GC_XGC(gc), x, y, text, text_length);
-#endif /* MOZ_WIDGET_GTK2 */
-  }
-  else
-    g_error("undefined font type\n");
-}
-
 
 
 NS_IMETHODIMP nsRenderingContextGTK::DrawImage(imgIContainer *aImage, const nsRect * aSrcRect, const nsPoint * aDestPoint)
