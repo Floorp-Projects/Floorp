64fc82bbf852fb868d15a5e8a0f31edc5d7d9a46
hyatt%netscape.com <hyatt%netscape.com>
2002-10-08 22:05:33 +0000

    Fix script panel.

--- browser/components/prefwindow/content/pref-scripts.js
+++ browser/components/prefwindow/content/pref-scripts.js
@@ -44,7 +44,6 @@ function changeDisabledState(state){
   //Set the states of the groupbox children state based on the "javascript enabled" checkbox value
   document.getElementById("allowScripts").disabled = state;
   document.getElementById("allowWindowMoveResize").disabled = state;
-  document.getElementById("allowWindowOpen").disabled = state;
   document.getElementById("allowImageSrcChange").disabled = state;
   document.getElementById("allowDocumentCookieSet").disabled = state;
   document.getElementById("allowDocumentCookieGet").disabled = state;
@@ -82,8 +81,7 @@ function Startup(){
 
   //If scriptData does not exist, then it is the first time the panel was shown and we default to false 
   if (!("scriptData" in data)){
-    var changedList = ["allowWindowOpenChanged",
-                       "allowWindowMoveResizeChanged",
+    var changedList = ["allowWindowMoveResizeChanged",
                        "allowWindowStatusChangeChanged",
                        "allowWindowFlipChanged",
                        "allowDocumentCookieSetChanged",
@@ -97,7 +95,6 @@ function Startup(){
       data.scriptData[ changedList[run] ].value = false;
     }
 
-    document.getElementById("allowWindowOpen").checked = getPrefValueForCheckbox("dom.disable_open_during_load");
     document.getElementById("allowWindowMoveResize").checked =  getPrefValueForCheckbox("dom.disable_window_move_resize");
     document.getElementById("allowWindowFlip").checked = getPrefValueForCheckbox("dom.disable_window_flip");
     document.getElementById("allowWindowStatusChange").checked = getPrefValueForCheckbox("dom.disable_window_status_change");
@@ -131,11 +128,6 @@ function doOnOk(){
   }
 
   var data = parent.hPrefWindow.wsm.dataManager.pageData["chrome://browser/content/pref/pref-scripts.xul"];
- 
-  if (data.scriptData["allowWindowOpenChanged"].value){
-    parent.hPrefWindow.setPref("bool", "dom.disable_open_during_load",
-      !getCheckboxValue('allowWindowOpen'));
-  }
 
   if (data.scriptData["allowWindowMoveResizeChanged"].value){
     parent.hPrefWindow.setPref("bool", "dom.disable_window_move_resize",
