db070df0d9c7901ac9bc31c9c3c54c9e21121120
sspitzer@netscape.com <sspitzer@netscape.com>
2002-10-08 06:00:10 +0000

    comment out the pref for junk button, until it's ready.

--- modules/libpref/src/init/mailnews.js
+++ modules/libpref/src/init/mailnews.js
@@ -277,7 +277,7 @@ pref("mailnews.remember_selected_message", true);
 
 pref("mail.toolbars.showbutton.file", true);
 pref("mail.toolbars.showbutton.next", true);
-pref("mail.toolbars.showbutton.junk", false); // not yet...
+/* not ready yet: pref("mail.toolbars.showbutton.junk", false); */
 pref("mail.toolbars.showbutton.print", true);
 pref("mail.toolbars.showbutton.stop", true);
 
