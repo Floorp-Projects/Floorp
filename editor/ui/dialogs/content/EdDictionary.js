db5eca39247aa189efa0f578eeed4295c7fe0343
akkana@netscape.com <akkana@netscape.com>
2002-10-11 23:46:57 +0000

    168999: Make nsEditorSpellCheck an independant object instead of part of editorshell. r=brade sr=kin a=asa

--- editor/ui/dialogs/content/EdDictionary.js
+++ editor/ui/dialogs/content/EdDictionary.js
@@ -1,3 +1,4 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 /* 
  * The contents of this file are subject to the Netscape Public
  * License Version 1.1 (the "License"); you may not use this file
