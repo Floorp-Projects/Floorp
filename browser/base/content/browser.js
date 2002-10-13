3adf8bb37c66dc10ef5f7d4ed2f9de2dbce288ca
blakeross%telocity.com <blakeross%telocity.com>
2002-10-13 16:35:19 +0000

    fold contentAreAclick.js into browser.js; speed optimizations.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -219,7 +219,7 @@ function UpdatePageReport(event)
       displayPageReportFirstTime();
 
       // Now set the pref.
-      pref.setBoolPref("privacy.popups.firstTime", "false");
+      gPrefService.setBoolPref("privacy.popups.firstTime", "false");
     }
   }
   else
@@ -4130,3 +4130,239 @@ nsDefaultEngine.prototype =
         }
     }
 }
+
+/*
+ * - [ Dependencies ] ---------------------------------------------------------
+ *  utilityOverlay.js:
+ *    - gatherTextUnder
+ */
+
+  // Prefill a single text field
+  function prefillTextBox(target) {
+
+    // obtain values to be used for prefilling
+    var walletService = Components.classes["@mozilla.org/wallet/wallet-service;1"].getService(Components.interfaces.nsIWalletService);
+    var value = walletService.WALLET_PrefillOneElement(window._content, target);
+    if (value) {
+
+      // result is a linear sequence of values, each preceded by a separator character
+      // convert linear sequence of values into an array of values
+      var separator = value[0];
+      var valueList = value.substring(1, value.length).split(separator);
+
+      target.value = valueList[0];
+/*
+ * Following code is a replacement for above line.  In the case of multiple values, it
+ * presents the user with a dialog containing a list from which he can select the value
+ * he wants.  However it is being commented out for now because of several problems, namely
+ *
+ *   1. There is no easy way to put localizable strings for the title and message of
+ *      the dialog without introducing a .properties file which currently doesn't exist
+ *   2. Using blank title and messages as shown below have a problem because a zero-length
+ *      title is being displayed as some garbage characters (which is why the code below
+ *      has a title of " " instead of "").  This is a separate bug which will have to be
+ *      investigated further.
+ *   3. The current wallet tables present alternate values for items such as shipping
+ *      address -- namely billing address and home address.  Up until now, these alternate
+ *      values have never been a problem because the preferred value is always first and is
+ *      all the user sees when doing a prefill.  However now he will be presented with a
+ *      list showing all these values and asking him to pick one, even though the wallet
+ *      code was clearly able to determine that he meant shipping address and not billing
+ *      address.
+ *   4. There is a relatively long delay before the dialog come up whereas values are
+ *      filled in quickly when no dialog is involved.
+ *
+ * Once this feature is checked in, a separate bug will be opened asking that the above
+ * problems be examined and this dialog turned on
+ 
+      if (valueList.length == 1) {
+        // only one value, use it for prefilling
+        target.value = valueList[0];
+      } else {
+
+        // more than one value, have user select the one he wants
+        var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService();
+        promptService = promptService.QueryInterface(Components.interfaces.nsIPromptService);
+        var position = {};
+        var title = " ";
+        var message = "";
+        var ok =
+          promptService.select
+            (window, title, message, valueList.length, valueList, position)
+        if (ok) {
+          target.value = valueList[position.value];
+        }
+      }
+
+ * End of commented out code
+ */
+    }
+  }
+  
+  // Called whenever the user clicks in the content area,
+  // except when left-clicking on links (special case)
+  // should always return true for click to go through
+  function contentAreaClick(event) 
+  {
+    var target = event.target;
+    var linkNode;
+
+    var local_name = target.localName;
+
+    if (local_name) {
+      local_name = local_name.toLowerCase();
+    }
+
+    switch (local_name) {
+      case "a":
+      case "area":
+      case "link":
+        if (target.hasAttribute("href")) 
+          linkNode = target;
+        break;
+      case "input":
+        if ((event.target.type.toLowerCase() == "text" || event.target.type == "") // text field
+            && event.detail == 2 // double click
+            && event.button == 0 // left mouse button
+            && event.target.value.length == 0) { // no text has been entered
+          prefillTextBox(target); // prefill the empty text field if possible
+        }
+        break;
+      default:
+        linkNode = findParentNode(event.originalTarget, "a");
+        // <a> cannot be nested.  So if we find an anchor without an
+        // href, there is no useful <a> around the target
+        if (linkNode && !linkNode.hasAttribute("href"))
+          linkNode = null;
+        break;
+    }
+    if (linkNode) {
+      handleLinkClick(event, linkNode.href, linkNode);
+      return true;
+    } else {
+      // Try simple XLink
+      var href;
+      linkNode = target;
+      while (linkNode) {
+        if (linkNode.nodeType == Node.ELEMENT_NODE) {
+          href = linkNode.getAttributeNS("http://www.w3.org/1999/xlink", "href");
+          break;
+        }
+        linkNode = linkNode.parentNode;
+      }
+      if (href && href != "") {
+        href = makeURLAbsolute(target.baseURI,href);
+        handleLinkClick(event, href, null);
+        return true;
+      }
+    }
+    if (gPrefService && event.button == 1 &&
+        !findParentNode(event.originalTarget, "scrollbar") &&
+        gPrefService.getBoolPref("middlemouse.contentLoadURL")) {
+      if (middleMousePaste(event)) {
+        event.preventBubble();
+      }
+    }
+    return true;
+  }
+
+  function openNewTabOrWindow(event, href, linkNode)
+  {
+    // should we open it in a new tab?
+    var result = false;
+    if (gPrefService && gPrefService.getBoolPref("browser.tabs.opentabfor.middleclick") &&
+        ("getBrowser" in window) && getBrowser().localName == "tabbrowser") {
+      var loadInBackground = gPrefService.getBoolPref("browser.tabs.loadInBackground");
+      if (event.shiftKey)
+        loadInBackground = !loadInBackground;
+      var theTab = getBrowser().addTab(href, getReferrer(document));
+      if (!loadInBackground)
+        getBrowser().selectedTab = theTab;
+      event.preventBubble();
+      result = true;
+    }
+    else
+    // should we open it in a new window?
+    if (gPrefService && gPrefService.getBoolPref("middlemouse.openNewWindow")) {
+      openNewWindowWith(href);
+      event.preventBubble();
+      result = true;
+    }
+
+    if (result && linkNode) {
+      var globalHistory = Components.classes["@mozilla.org/browser/global-history;1"]
+                                  .getService(Components.interfaces.nsIGlobalHistory);
+      if (!globalHistory.isVisited(href)) {
+        globalHistory.addPage(href);
+        var oldHref = linkNode.href;
+        linkNode.href = "";
+        linkNode.href = oldHref;
+      }
+    }
+        
+    // let someone else deal with it
+    return result;
+  }
+
+  function handleLinkClick(event, href, linkNode)
+  {
+    switch (event.button) {                                   
+      case 0:                                                         // if left button clicked
+        if (event.metaKey || event.ctrlKey) {                         // and meta or ctrl are down
+          if (openNewTabOrWindow(event, href, linkNode))
+            return true;
+        } 
+        var saveModifier = true;
+        if (pref) {
+          try {
+            saveModifier = gPrefService.getBoolPref("ui.key.saveLink.shift");
+          }
+          catch(ex) {            
+          }
+        }
+        saveModifier = saveModifier ? event.shiftKey : event.altKey;
+          
+        if (saveModifier) {                                           // if saveModifier is down
+          saveURL(href, linkNode ? gatherTextUnder(linkNode) : "");
+          return true;
+        }
+        if (event.altKey)                                             // if alt is down
+          return true;                                                // do nothing
+        return false;
+      case 1:                                                         // if middle button clicked
+        if (openNewTabOrWindow(event, href, linkNode))
+          return true;
+        break;
+    }
+    return false;
+  }
+
+  function middleMousePaste( event )
+  {
+    var url = readFromClipboard();
+    if (!url)
+      return false;
+    url = getShortcutOrURI(url);
+    if (!url)
+      return false;
+
+    // On ctrl-middleclick, open in new window or tab.
+    if (event.ctrlKey)
+      return openNewTabOrWindow(event, url, null);
+
+    // If ctrl wasn't down, then just load the url in the current win/tab.
+    loadURI(url);
+    event.preventBubble();
+    return true;
+  }
+
+  function makeURLAbsolute( base, url ) 
+  {
+    // Construct nsIURL.
+    var ioService = Components.classes["@mozilla.org/network/io-service;1"]
+                  .getService(Components.interfaces.nsIIOService);
+    var baseURI  = ioService.newURI(base, null, null);
+
+    return ioService.newURI(baseURI.resolve(url), null, null).spec;
+  }
+
