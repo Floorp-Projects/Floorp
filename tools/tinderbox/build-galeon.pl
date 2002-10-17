2577185ec512a27f75d6936f38840c82d5d6cda6
cltbld%netscape.com <cltbld%netscape.com>
2002-10-17 19:46:06 +0000

    Whitespace changes, no tabs.

--- tools/tinderbox/build-galeon.pl
+++ tools/tinderbox/build-galeon.pl
@@ -67,46 +67,46 @@ sub main {
 
   # Build galeon if we passed the checkout command.
   if ($post_status ne 'busted') {
-	# Build galeon.
-
-	chdir $galeon_dir;
-	
-	# Make sure we have a configure
-	unless (-e "configure" and $post_status ne 'busted') {
-	  $status = TinderUtils::run_shell_command("./autogen.sh");
-	  TinderUtils::print_log "Status from autogen: $status\n";
-	}
-
-	# Not sure how to only do this when we need to.
-	# Force a configure for now.
-	if ($status == 0) {
-	  TinderUtils::print_log "configuring...\n";
-	  $status = TinderUtils::run_shell_command("./configure --sysconfdir=/etc --with-mozilla-libs=$mozilla_build_dir/mozilla/dist/bin --with-mozilla-includes=$mozilla_build_dir/mozilla/dist/include --with-nspr-includes=$mozilla_build_dir/mozilla/dist/include/nspr --with-mozilla-home=$mozilla_build_dir/mozilla/dist/bin");
-	  TinderUtils::print_log "Status from configure: $status\n";
-	}
-
-	if ($status == 0) {
-	  TinderUtils::print_log "Deleting, gmake...\n";
-	  TinderUtils::DeleteBinary("$galeon_dir/src/$galeon_binary");
-
-	  my $foo = Cwd::getcwd();
-	  TinderUtils::print_log "cwd = $foo\n";
-	  $status = TinderUtils::run_shell_command("gmake");
-	  TinderUtils::print_log "Status from gmake: $status\n";
-	}
-
-	TinderUtils::print_log "Testing build status...\n";
-	if ($status != 0) {
-	  TinderUtils::print_log "busted, gmake status non-zero\n";
-	  $post_status = 'busted';
-	} elsif (not TinderUtils::BinaryExists("$galeon_dir/src/$galeon_binary")) {
-	  TinderUtils::print_log "Error: binary not found: $galeon_dir/src/$galeon_binary\n";
-	  $post_status = 'busted';
-	} else {
-	  $post_status = 'success';
-	}
+    # Build galeon.
+    
+    chdir $galeon_dir;
+    
+    # Make sure we have a configure
+    unless (-e "configure" and $post_status ne 'busted') {
+      $status = TinderUtils::run_shell_command("./autogen.sh");
+      TinderUtils::print_log "Status from autogen: $status\n";
+    }
+    
+    # Not sure how to only do this when we need to.
+    # Force a configure for now.
+    if ($status == 0) {
+      TinderUtils::print_log "configuring...\n";
+      $status = TinderUtils::run_shell_command("./configure --sysconfdir=/etc --with-mozilla-libs=$mozilla_build_dir/mozilla/dist/bin --with-mozilla-includes=$mozilla_build_dir/mozilla/dist/include --with-nspr-includes=$mozilla_build_dir/mozilla/dist/include/nspr --with-mozilla-home=$mozilla_build_dir/mozilla/dist/bin");
+      TinderUtils::print_log "Status from configure: $status\n";
+    }
+    
+    if ($status == 0) {
+      TinderUtils::print_log "Deleting, gmake...\n";
+      TinderUtils::DeleteBinary("$galeon_dir/src/$galeon_binary");
+      
+      my $foo = Cwd::getcwd();
+      TinderUtils::print_log "cwd = $foo\n";
+      $status = TinderUtils::run_shell_command("gmake");
+      TinderUtils::print_log "Status from gmake: $status\n";
+    }
+    
+    TinderUtils::print_log "Testing build status...\n";
+    if ($status != 0) {
+      TinderUtils::print_log "busted, gmake status non-zero\n";
+      $post_status = 'busted';
+    } elsif (not TinderUtils::BinaryExists("$galeon_dir/src/$galeon_binary")) {
+      TinderUtils::print_log "Error: binary not found: $galeon_dir/src/$galeon_binary\n";
+      $post_status = 'busted';
+    } else {
+      $post_status = 'success';
+    }
   }
-
+  
   #
   # Need a way to bypass initial setup dialog stuff, or
   # we'll need to manually run this by hand the first time.
@@ -114,23 +114,33 @@ sub main {
   # Also, have to set crash_recovery=0 in ~/.gnome/galeon to avoid
   # getting the crash dialog after the alive test times out.
   #
-
+  
   # Test galeon, about:blank
   if ($galeon_alive_test and $post_status eq 'success') {
-	$post_status = TinderUtils::AliveTest("GaleonAliveTest",
-										  "$galeon_dir/src",
-										  "galeon",
-										  "about:blank",
-										  45);
+    $post_status = TinderUtils::AliveTest("GaleonAliveTest",
+					  "$galeon_dir/src",
+					  "galeon",
+					  "about:blank",
+					  45);
   }
-
+  
   # Test galeon, test8
   if ($galeon_test8_test and $post_status eq 'success') {
-	$post_status = TinderUtils::AliveTest("GaleonTest8Test",
-										  "$galeon_dir/src",
-										  "galeon",
-										  "resource:///res/samples/test8.html",
-										  45);
+    $post_status = TinderUtils::AliveTest("GaleonTest8Test",
+					  "$galeon_dir/src",
+					  "galeon",
+					  "resource:///res/samples/test8.html",
+					  45);
+  }
+  
+  # Startup test
+  if ($galeon_startup_test and $post_status eq 'success') {
+    TinderUtils::StartupPerformanceTest("GaleonStartupPerformanceTest",
+					"galeon",
+					"$galeon_dir/src",
+					"",
+					"$galeon_dir/src/../../../startup-test.html");     
+    
   }
 
   # Pass our status back to calling script.
