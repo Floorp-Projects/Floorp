40f5500436d6d2938001b47a5dfe437e2929219b
jfrancis@netscape.com <jfrancis@netscape.com>
2002-10-22 21:59:34 +0000

    mac port of layout-debug idl, plugin, and component

---
+++ extensions/layout-debug/mac/DbgPrefixCarbon.h
@@ -0,0 +1,10 @@
+#define XP_MAC 1
+#define TARGET_CARBON	1
+#define DEBUG 1
+
+/* Read the common configuration file */
+#ifndef MacConfigInclude_h_
+#include "MacConfigInclude.h"
+#endif
+
+
---
+++ extensions/layout-debug/mac/PluginConfig.h
@@ -0,0 +1,7 @@
+#define XP_MAC 1
+#define DEBUG 1
+
+/* Read the common configuration file */
+#ifndef MacConfigInclude_h_
+#include "MacConfigInclude.h"
+#endif
