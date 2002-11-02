94f8219b202014165b97e2a794ab1dd13d654005
blakeross%telocity.com <blakeross%telocity.com>
2002-11-02 22:09:49 +0000

    Fix pref name.

--- browser/app/profile/firebird.js
+++ browser/app/profile/firebird.js
@@ -120,7 +120,7 @@ pref("browser.download.progressDnldDialog.keepAlive", true); // keep the dnload 
 pref("browser.download.progressDnldDialog.enable_launch_reveal_buttons", true);
 pref("browser.download.useProgressDialogs", true);
 pref("browser.download.openSidebar", false);
-pref("browser.download.prompt", true);
+pref("browser.download.promptWithFilepicker", true);
 
 // various default search settings
 pref("browser.search.defaulturl", "chrome://browser-region/locale/region.properties");
