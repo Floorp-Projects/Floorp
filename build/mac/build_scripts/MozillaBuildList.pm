bd45d0e465012e7b86e2fbe6a643743e6de79bf8
bryner@netscape.com <bryner@netscape.com>
2002-11-08 06:09:17 +0000

    Stop building and packaging XBL form controls, since they aren't being actively worked on and aren't slated to be turned on anytime soon (bug 172288). r=jkeiser, sr=blake.

--- build/mac/build_scripts/MozillaBuildList.pm
+++ build/mac/build_scripts/MozillaBuildList.pm
@@ -609,7 +609,6 @@ sub ProcessJarManifests()
     CreateJarFromManifest(":mozilla:extensions:wallet:jar.mn", $chrome_dir, \%jars);
     CreateJarFromManifest(":mozilla:intl:uconv:src:jar.mn", $chrome_dir, \%jars);
     CreateJarFromManifest(":mozilla:htmlparser:src:jar.mn", $chrome_dir, \%jars);
-    CreateJarFromManifest(":mozilla:layout:html:forms:resources:jar.mn", $chrome_dir, \%jars);
     CreateJarFromManifest(":mozilla:layout:html:document:src:xbl-marquee:resources:jar.mn", $chrome_dir, \%jars);
     CreateJarFromManifest(":mozilla:layout:html:forms:src:jar.mn", $chrome_dir, \%jars);
     CreateJarFromManifest(":mozilla:layout:html:base:src:jar.mn", $chrome_dir, \%jars);
