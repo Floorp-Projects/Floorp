57c2e908dc123e1135255063219065506995b40c
jkeiser%netscape.com <jkeiser%netscape.com>
2002-11-09 01:03:36 +0000

    Fix copyright date.  Bug 133354.

--- xpcom/ds/nsDoubleHashtable.h
+++ xpcom/ds/nsDoubleHashtable.h
@@ -16,7 +16,7 @@
  *
  * The Initial Developer of the Original Code is
  * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1998
+ * Portions created by the Initial Developer are Copyright (C) 2002
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
