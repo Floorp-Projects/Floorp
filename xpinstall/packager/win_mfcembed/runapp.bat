4f7a6efa1b65d8c0d325125aa118cf767e905398
curt%netscape.com <curt%netscape.com>
2002-11-09 05:16:52 +0000

    Changing version string.   (Bug #178716, r=chak, sr=dveditz)

--- xpinstall/packager/win_mfcembed/runapp.bat
+++ xpinstall/packager/win_mfcembed/runapp.bat
@@ -2,7 +2,7 @@
 REM --------------------------------------------------------
 REM Modify GRE_DIR below to match your GRE install location
 REM --------------------------------------------------------
-set GRE_VERSION=1.2b
+set GRE_VERSION=1.3a
 set GRE_DIR=c:\Program Files\mozilla.org\GRE\%GRE_VERSION%
 
 set PATH=%PATH%;%GRE_DIR%;%GRE_DIR%\components
