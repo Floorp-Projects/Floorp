6e29f0865bc670804f2135021094895ec206941c
pkw%us.ibm.com <pkw%us.ibm.com>
2002-11-12 19:38:15 +0000

    Fix xptcinvoke code on AIX when compiling in 64-bit mode. Specifically, passing PRBools through xptcinvoke was broken. Bug 178499: r=jdunn, sr=jst

--- xpcom/reflect/xptcall/src/md/unix/xptcinvoke_ppc_aix64.cpp
+++ xpcom/reflect/xptcall/src/md/unix/xptcinvoke_ppc_aix64.cpp
@@ -38,9 +38,7 @@
 
 #include "xptcprivate.h"
 
-#ifndef AIX
-#error "This code is for PowerPC only"
-#endif
+#ifdef _AIX
 
 extern "C" void
 invoke_copy_to_stack(PRUint64* d, PRUint32 paramCount, nsXPTCVariant* s, double *fprData)
@@ -59,36 +57,39 @@ invoke_copy_to_stack(PRUint64* d, PRUint32 paramCount, nsXPTCVariant* s, double 
     {
         if(l_s->IsPtrData())
         {
-            *((void**)l_d) = l_s->ptr;
+            *l_d = (PRUint64)l_s->ptr;
             continue;
         }
         switch(l_s->type)
         {
-        case nsXPTType::T_I8     : *((PRInt64*)  l_d) = l_s->val.i8;    break;
-        case nsXPTType::T_I16    : *((PRInt64*)  l_d) = l_s->val.i16;   break;
-        case nsXPTType::T_I32    : *((PRInt64*)  l_d) = l_s->val.i32;   break;
-        case nsXPTType::T_I64    : *((PRInt64*)  l_d) = l_s->val.i64;   break;
-        case nsXPTType::T_U8     : *((PRUint64*) l_d) = l_s->val.u8;    break;
-        case nsXPTType::T_U16    : *((PRUint64*) l_d) = l_s->val.u16;   break;
-        case nsXPTType::T_U32    : *((PRUint64*) l_d) = l_s->val.u32;   break;
-        case nsXPTType::T_U64    : *((PRUint64*) l_d) = l_s->val.u64;   break;
-        case nsXPTType::T_DOUBLE :
-            *((double*) l_d) = l_s->val.d;
+        case nsXPTType::T_I8:    *l_d = (PRUint64)l_s->val.i8;   break;
+        case nsXPTType::T_I16:   *l_d = (PRUint64)l_s->val.i16;  break;
+        case nsXPTType::T_I32:   *l_d = (PRUint64)l_s->val.i32;  break;
+        case nsXPTType::T_I64:   *l_d = (PRUint64)l_s->val.i64;  break;
+        case nsXPTType::T_U8:    *l_d = (PRUint64)l_s->val.u8;   break;
+        case nsXPTType::T_U16:   *l_d = (PRUint64)l_s->val.u16;  break;
+        case nsXPTType::T_U32:   *l_d = (PRUint64)l_s->val.u32;  break;
+        case nsXPTType::T_U64:   *l_d = (PRUint64)l_s->val.u64;  break;
+        case nsXPTType::T_BOOL:  *l_d = (PRUint64)l_s->val.b;    break;
+        case nsXPTType::T_CHAR:  *l_d = (PRUint64)l_s->val.c;    break;
+        case nsXPTType::T_WCHAR: *l_d = (PRUint64)l_s->val.wc;   break;
+
+        case nsXPTType::T_DOUBLE:
+            *((double*)l_d) = l_s->val.d;
             if(fpCount < 13)
                 l_fprData[fpCount++] = l_s->val.d;
             break;
-        case nsXPTType::T_FLOAT  :
-            *((float*)  l_d) = l_s->val.f;
+        case nsXPTType::T_FLOAT:
+            *((float*)l_d) = l_s->val.f;
             if(fpCount < 13)
                 l_fprData[fpCount++] = l_s->val.f;
             break;
-        case nsXPTType::T_BOOL   : *((PRBool*)   l_d) = l_s->val.b;     break;
-        case nsXPTType::T_CHAR   : *((PRUint64*) l_d) = l_s->val.c;     break;
-        case nsXPTType::T_WCHAR  : *((PRInt64*)  l_d) = l_s->val.wc;    break;
         default:
             // all the others are plain pointer types
-            *((void**)l_d) = l_s->val.p;
+            *l_d = (PRUint64)l_s->val.p;
             break;
         }
     }
 }
+#endif
+
