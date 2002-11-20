e0650a06fd51e2e2339d494d42cb939a75c675fd
danm@netscape.com <danm@netscape.com>
2002-11-20 03:25:08 +0000

    prevent untrusted script from opening modal windows in general while still allowing alerts and the like. bug 180048 r=jst,mstoltz

--- embedding/components/windowwatcher/src/nsWindowWatcher.h
+++ embedding/components/windowwatcher/src/nsWindowWatcher.h
@@ -96,7 +96,8 @@ private:
   static void       CheckWindowName(nsString& aName);
   static PRUint32   CalculateChromeFlags(const char *aFeatures,
                                          PRBool aFeaturesSpecified,
-                                         PRBool aDialog);
+                                         PRBool aDialog,
+                                         PRBool aChromeURL);
   static PRInt32    WinHasOption(const char *aOptions, const char *aName,
                                  PRInt32 aDefault, PRBool *aPresenceFlag);
   static nsresult   ReadyOpenedDocShellItem(nsIDocShellTreeItem *aOpenedItem,
