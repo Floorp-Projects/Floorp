42ca5f2672a015c86b66ae49bdc6a1b113b48c26
bienvenu%netscape.com <bienvenu%netscape.com>
2002-11-28 03:32:17 +0000

    fix problem with folder permissions and imap acl r/sr=sspitzer 170597

--- mailnews/imap/src/nsImapMailFolder.cpp
+++ mailnews/imap/src/nsImapMailFolder.cpp
@@ -5248,9 +5248,6 @@ PRBool nsMsgIMAPFolderACL::SetFolderRightsForUser(const char *userName, const ch
 
 const char *nsMsgIMAPFolderACL::GetRightsStringForUser(const char *inUserName)
 {
-  if (!inUserName)
-    return nsnull;
-
   nsXPIDLCString userName;
   userName.Assign(inUserName);
   if (!userName.Length())
