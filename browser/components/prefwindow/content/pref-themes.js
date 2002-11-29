cca557a5294eb33a747ca45db7a514493902b61d
blakeross@telocity.com <blakeross@telocity.com>
2002-11-29 04:50:50 +0000

    Cut more comm dependencies; remove some unused files.

--- browser/components/prefwindow/content/pref-themes.js
+++ browser/components/prefwindow/content/pref-themes.js
@@ -51,7 +51,7 @@ catch(e) {}
 
 function Startup()
 {
-  gData = parent.hPrefWindow.wsm.dataManager.pageData["chrome://communicator/content/pref/pref-themes.xul"];
+  gData = parent.hPrefWindow.wsm.dataManager.pageData["chrome://browser/content/pref/pref-themes.xul"];
   var list = document.getElementById( "skinsList" );
   if ("loaded" in gData && "themeIndex" in gData) {
     list.selectedIndex = gData.themeIndex;    
@@ -101,7 +101,7 @@ function Startup()
 
 function applyTheme()
 {
-  var data = parent.hPrefWindow.wsm.dataManager.pageData["chrome://communicator/content/pref/pref-themes.xul"];
+  var data = parent.hPrefWindow.wsm.dataManager.pageData["chrome://browser/content/pref/pref-themes.xul"];
   if (data.name == null)
     return;
 
