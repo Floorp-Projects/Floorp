62b8516bb903b57886bcfcc5adc6bebabf43766d
nelsonb%netscape.com <nelsonb%netscape.com>
2002-12-05 22:15:36 +0000

    Don't compile the .c files in lib/pki1 on the trunk.  These files are used only in Stan.

--- security/nss/lib/pki1/config.mk
+++ security/nss/lib/pki1/config.mk
@@ -30,7 +30,7 @@
 # may use your version of this file under either the MPL or the
 # GPL.
 #
-CONFIG_CVS_ID = "@(#) $RCSfile: config.mk,v $ $Revision: 1.2 $ $Date: 2001/10/11 17:41:47 $ $Name:  $"
+CONFIG_CVS_ID = "@(#) $RCSfile: config.mk,v $ $Revision: 1.3 $ $Date: 2002/12/05 22:15:36 $ $Name:  $"
 
 ifdef BUILD_IDG
 DEFINES += -DNSSDEBUG
@@ -41,7 +41,7 @@ endif
 #  are specifed as dependencies within rules.mk.
 #
 
-TARGETS        = $(LIBRARY)
+TARGETS        = 
 SHARED_LIBRARY =
 IMPORT_LIBRARY =
 PROGRAM        =
