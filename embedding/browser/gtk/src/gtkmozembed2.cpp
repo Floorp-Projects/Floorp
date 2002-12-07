18cbe9dd425bbb45530f9aa098901ebbe4de2af4
blizzard%redhat.com <blizzard%redhat.com>
2002-12-07 20:02:34 +0000

    Bug #183912.  embedding widget crashes with gtk 2.1.3.  Make sure that the types for the signal marshalers match. r=timeless, sr=bryner, a=asa

--- embedding/browser/gtk/src/gtkmozembed2.cpp
+++ embedding/browser/gtk/src/gtkmozembed2.cpp
@@ -327,7 +327,7 @@ gtk_moz_embed_class_init(GtkMozEmbedClass *klass)
 		   GET_OBJECT_CLASS_TYPE(klass),
 		   GTK_SIGNAL_OFFSET(GtkMozEmbedClass, progress_all),
 		   gtkmozembed_VOID__POINTER_INT_INT,
-		   GTK_TYPE_NONE, 3, GTK_TYPE_STRING,
+		   GTK_TYPE_NONE, 3, GTK_TYPE_POINTER,
 		   GTK_TYPE_INT, GTK_TYPE_INT);
   moz_embed_signals[NET_STATE] =
     gtk_signal_new("net_state",
@@ -342,7 +342,7 @@ gtk_moz_embed_class_init(GtkMozEmbedClass *klass)
 		   GET_OBJECT_CLASS_TYPE(klass),
 		   GTK_SIGNAL_OFFSET(GtkMozEmbedClass, net_state_all),
 		   gtkmozembed_VOID__POINTER_INT_UINT,
-		   GTK_TYPE_NONE, 3, GTK_TYPE_STRING,
+		   GTK_TYPE_NONE, 3, GTK_TYPE_POINTER,
 		   GTK_TYPE_INT, GTK_TYPE_UINT);
   moz_embed_signals[NET_START] =
     gtk_signal_new("net_start",
@@ -385,7 +385,7 @@ gtk_moz_embed_class_init(GtkMozEmbedClass *klass)
 		   GET_OBJECT_CLASS_TYPE(klass),
 		   GTK_SIGNAL_OFFSET(GtkMozEmbedClass, open_uri),
 		   gtk_marshal_BOOL__POINTER,
-		   GTK_TYPE_BOOL, 1, GTK_TYPE_STRING);
+		   GTK_TYPE_BOOL, 1, GTK_TYPE_POINTER);
   moz_embed_signals[SIZE_TO] =
     gtk_signal_new("size_to",
 		   GTK_RUN_LAST,
