5660552c739247ad17d088b1ec83de53e08012ea
cltbld@netscape.com <cltbld@netscape.com>
2002-12-17 00:25:49 +0000

    Updating trunk to 1.3b, Bug#184465 r=lpham/sr=kysmith. ycalonje checking in as cltbld

--- xpinstall/wizard/mac/rsrc/MacInstallerVersion.r
+++ xpinstall/wizard/mac/rsrc/MacInstallerVersion.r
@@ -35,10 +35,10 @@ resource 'vers' (1, "XPInstall version") {
 resource 'vers' (2, "Mozilla Installer version") {
         1,
         0x30,
-        alpha,	// {alpha, beta, release}
+        beta,	// {alpha, beta, release}
         0,			// alpha or beta number; 0 for release
         0,			// language
-        "1.3a",		// short version string
-        "Mozilla 1.3a Installer"	// long version string
+        "1.3b",		// short version string
+        "Mozilla 1.3b Installer"	// long version string
 };
 
