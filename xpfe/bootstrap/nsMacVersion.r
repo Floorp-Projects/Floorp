38e5df9a45241b2a92bb79098ce6b4824997d74a
asasaki@netscape.com <asasaki@netscape.com>
2002-12-18 00:35:58 +0000

    184464: update mac desktop getinfo for 1.3b. r=leaf sr=granrose

--- xpfe/bootstrap/nsMacVersion.r
+++ xpfe/bootstrap/nsMacVersion.r
@@ -29,13 +29,13 @@
 
 #define		VERSION_MAJOR			1
 #define		VERSION_MINOR			0x30	// revision & fix in BCD
-#define		VERSION_KIND			alpha	// alpha, beta, or final
+#define		VERSION_KIND			beta	// alpha, beta, or final
 #define		VERSION_MICRO			0		// internal stage: alpha or beta number
 
 
 // Version Strings (Finder's Get Info dialog box) //
 
-#define		VERSION_STRING			"1.3a"
+#define		VERSION_STRING			"1.3b"
 //#define 	VERSION_LANG			"en"	// e.g. en, ja, de, fr
 //#define 	VERSION_COUNTRY			"_US"	// e.g.,  _JP, _DE, _FR, _US
 //#define	VERSION_LOCALE			"[" VERSION_LANG "_" VERSION_COUNTRY "]"
