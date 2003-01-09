2d8b897ec990f205fe8bb379ba386031aae8fa7a
aaronl@netscape.com <aaronl@netscape.com>
2003-01-09 20:03:41 +0000

    Bug 187301 - tab nav prefs not dynamically changeable, no prefs observer. r=akkana, sr=bryner.

--- content/events/src/nsEventStateManager.h
+++ content/events/src/nsEventStateManager.h
@@ -74,9 +74,10 @@ class nsEventStateManager : public nsSupportsWeakReference,
   // Tab focus model bit field:
   enum nsTabFocusModel {
     eTabFocus_unset = 0,                  // unset, check preferences
-    eTabFocus_textControlsMask = (1<<0),  // text elements
+  //eTabFocus_textControlsMask = (1<<0),  // unused - textboxes always tabbable
     eTabFocus_formElementsMask = (1<<1),  // non-text form elements
-    eTabFocus_linksMask = (1<<2)          // links
+    eTabFocus_linksMask = (1<<2),         // links
+    eTabFocus_any = 1 | (1<<1) | (1<<2),  // everything that can be focused
   };
 
   enum nsTextfieldSelectModel {
@@ -212,6 +213,7 @@ protected:
                          PRBool scrollPage, PRBool aUseTargetFrame);
   void ForceViewUpdate(nsIView* aView);
   nsresult getPrefBranch();
+  void ResetObservedPrefs();
   nsresult ChangeTextSize(PRInt32 change);
   // end mousewheel functions
 
