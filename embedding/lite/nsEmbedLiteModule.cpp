36cc6b60bde073e31735da9f0f8882f86b0e76df
ccarlen@netscape.com <ccarlen@netscape.com>
2003-01-11 03:02:33 +0000

    Bug 70714 - visited link doesn't change color in embedding apps. The patch turns on the building of the embed-lite component which implements global history with less footprint and is exported to Embed dist. r=chak/sr=alecf

--- embedding/lite/nsEmbedLiteModule.cpp
+++ embedding/lite/nsEmbedLiteModule.cpp
@@ -42,27 +42,27 @@
 #include "nsEmbedGlobalHistory.h"
 #include "nsChromeProtocolHandler.h"
 
-NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsEmbedChromeRegistry, Init)
+//NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsEmbedChromeRegistry, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsEmbedGlobalHistory, Init)
 
 static const nsModuleComponentInfo components[] =
 {
-#if 1 // Don't hook up until implementation is finished
+#if 0 // Don't hook up until implementation is finished
     { "Chrome Registry", 
       NS_EMBEDCHROMEREGISTRY_CID,
       "@mozilla.org/chrome/chrome-registry;1", 
       nsEmbedChromeRegistryConstructor,
+    },
+    { "Chrome Protocol Handler",
+      NS_CHROMEPROTOCOLHANDLER_CID,
+      NS_NETWORK_PROTOCOL_CONTRACTID_PREFIX "chrome",
+      nsChromeProtocolHandler::Create
     },
 #endif
     { "Global History", 
       NS_EMBEDGLOBALHISTORY_CID,
       "@mozilla.org/browser/global-history;1", 
       nsEmbedGlobalHistoryConstructor,
-    },
-    { "Chrome Protocol Handler", 
-      NS_CHROMEPROTOCOLHANDLER_CID,
-      NS_NETWORK_PROTOCOL_CONTRACTID_PREFIX "chrome", 
-      nsChromeProtocolHandler::Create
     }
 };
 
