c88812690afc78034d017c8af5667397cf2f6ebc
mikep%oeone.com <mikep%oeone.com>
2003-01-13 15:36:45 +0000

    FIxing bug 188417 by adding message about installing calendar as root if Mozilla was installed as root.

--- calendar/install.js
+++ calendar/install.js
@@ -8,8 +8,10 @@ addDirectory( "resources" );
 
 addDirectory("", "components", getFolder( "Components" ), "" );
 
-addDirectory( "", "", "icons", getFolder( "Chrome", "icons" ), "", true );
+addDirectory( "", "icons", getFolder( "Chrome", "icons" ), "", true );
 
+err = addFile( "", "calendar_pref.js", getFolder( getFolder( "Program", "defaults" ), "pref" ), "" );
+alert( err );
 var err = getLastError();
   
 if ( err == SUCCESS ) { 
@@ -31,7 +33,7 @@ if ( err == SUCCESS ) {
    }
    else if( err == "999" )
    {
-      alert("The Mozilla Calendar has been installed. \n You must restart your browser to continue.");
+      alert("The Mozilla Calendar has been installed. \n Please restart your browser to continue.");
    }
    
    else { 
@@ -41,9 +43,11 @@ if ( err == SUCCESS ) {
    }
 } 
 else { 
-  alert("Failed to create directory. \n"
+   alert("Failed to create directory. \n"
     +"You probably don't have appropriate permissions \n"
     +"(write access to mozilla/chrome directory). \n"
+    +"If you installed Mozilla as root then you need to install calendar as root as well.\n"
+    +"Or, you can change ownership of your Mozilla directory to yourself and install calendar."
     +"_____________________________\nError code:" + err);
     cancelInstall(err);
 }
