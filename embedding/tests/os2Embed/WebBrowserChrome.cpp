3845ffd9ebb01b9750091b2c3217100f8bf56108
mkaply@us.ibm.com <mkaply@us.ibm.com>
2003-01-22 04:01:57 +0000

    No bug r=mkaply, sr=blizzard OS/2 only - initial checkin of os2Embed sample

---
+++ embedding/tests/os2Embed/WebBrowserChrome.cpp
@@ -0,0 +1,569 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: Mozilla-sample-code 1.0
+ *
+ * Copyright (c) 2002 Netscape Communications Corporation and
+ * other contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this Mozilla sample software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+// Mozilla Includes
+#include "nsIGenericFactory.h"
+#include "nsIComponentManager.h"
+#include "nsString.h"
+#include "nsXPIDLString.h"
+#include "nsIURI.h"
+#include "nsIWebProgress.h"
+#include "nsIDocShellTreeItem.h"
+#include "nsIDOMWindow.h"
+#include "nsIDOMWindowInternal.h"
+#include "nsIInterfaceRequestor.h"
+#include "nsIInterfaceRequestorUtils.h"
+#include "nsIRequest.h"
+#include "nsIChannel.h"
+#include "nsCWebBrowser.h"
+#include "nsWidgetsCID.h"
+#include "nsIProfileChangeStatus.h"
+#include "nsCRT.h"
+
+// Local includes
+#include "resource.h"
+
+#include "os2Embed.h"
+#include "WebBrowserChrome.h"
+
+
+WebBrowserChrome::WebBrowserChrome()
+{
+    NS_INIT_ISUPPORTS();
+    mNativeWindow = nsnull;
+    mSizeSet = PR_FALSE;
+}
+
+WebBrowserChrome::~WebBrowserChrome()
+{
+    WebBrowserChromeUI::Destroyed(this);
+}
+
+nsresult WebBrowserChrome::CreateBrowser(PRInt32 aX, PRInt32 aY,
+                                         PRInt32 aCX, PRInt32 aCY,
+                                         nsIWebBrowser **aBrowser)
+{
+    NS_ENSURE_ARG_POINTER(aBrowser);
+    *aBrowser = nsnull;
+
+    mWebBrowser = do_CreateInstance(NS_WEBBROWSER_CONTRACTID);
+    
+    if (!mWebBrowser)
+        return NS_ERROR_FAILURE;
+
+    (void)mWebBrowser->SetContainerWindow(NS_STATIC_CAST(nsIWebBrowserChrome*, this));
+
+    nsCOMPtr<nsIDocShellTreeItem> dsti = do_QueryInterface(mWebBrowser);
+    dsti->SetItemType(nsIDocShellTreeItem::typeContentWrapper);
+
+    nsCOMPtr<nsIBaseWindow> browserBaseWindow = do_QueryInterface(mWebBrowser);
+
+    mNativeWindow = WebBrowserChromeUI::CreateNativeWindow(NS_STATIC_CAST(nsIWebBrowserChrome*, this));
+
+    if (!mNativeWindow)
+        return NS_ERROR_FAILURE;
+
+    browserBaseWindow->InitWindow( mNativeWindow,
+                             nsnull, 
+                             aX, aY, aCX, aCY);
+    browserBaseWindow->Create();
+
+    nsCOMPtr<nsIWebProgressListener> listener(NS_STATIC_CAST(nsIWebProgressListener*, this));
+    nsCOMPtr<nsIWeakReference> thisListener(dont_AddRef(NS_GetWeakReference(listener)));
+    (void)mWebBrowser->AddWebBrowserListener(thisListener, 
+        NS_GET_IID(nsIWebProgressListener));
+
+    // The window has been created. Now register for history notifications
+    mWebBrowser->AddWebBrowserListener(thisListener, NS_GET_IID(nsISHistoryListener));
+
+    if (mWebBrowser)
+    {
+      *aBrowser = mWebBrowser;
+      NS_ADDREF(*aBrowser);
+      return NS_OK;
+    }
+    return NS_ERROR_FAILURE;
+}
+
+//*****************************************************************************
+// WebBrowserChrome::nsISupports
+//*****************************************************************************   
+
+NS_IMPL_ADDREF(WebBrowserChrome)
+NS_IMPL_RELEASE(WebBrowserChrome)
+
+NS_INTERFACE_MAP_BEGIN(WebBrowserChrome)
+   NS_INTERFACE_MAP_ENTRY_AMBIGUOUS(nsISupports, nsIWebBrowserChrome)
+   NS_INTERFACE_MAP_ENTRY(nsIInterfaceRequestor)
+   NS_INTERFACE_MAP_ENTRY(nsIWebBrowserChrome)
+   NS_INTERFACE_MAP_ENTRY(nsIEmbeddingSiteWindow)
+   NS_INTERFACE_MAP_ENTRY(nsIWebProgressListener) // optional
+   NS_INTERFACE_MAP_ENTRY(nsISHistoryListener)
+   NS_INTERFACE_MAP_ENTRY(nsISupportsWeakReference)
+   NS_INTERFACE_MAP_ENTRY(nsIObserver)
+   NS_INTERFACE_MAP_ENTRY(nsIContextMenuListener)
+   NS_INTERFACE_MAP_ENTRY(nsITooltipListener)
+NS_INTERFACE_MAP_END
+
+//*****************************************************************************
+// WebBrowserChrome::nsIInterfaceRequestor
+//*****************************************************************************   
+
+NS_IMETHODIMP WebBrowserChrome::GetInterface(const nsIID &aIID, void** aInstancePtr)
+{
+    NS_ENSURE_ARG_POINTER(aInstancePtr);
+
+    *aInstancePtr = 0;
+    if (aIID.Equals(NS_GET_IID(nsIDOMWindow)))
+    {
+        if (mWebBrowser)
+        {
+            return mWebBrowser->GetContentDOMWindow((nsIDOMWindow **) aInstancePtr);
+        }
+        return NS_ERROR_NOT_INITIALIZED;
+    }
+    return QueryInterface(aIID, aInstancePtr);
+}
+
+//*****************************************************************************
+// WebBrowserChrome::nsIWebBrowserChrome
+//*****************************************************************************   
+
+NS_IMETHODIMP WebBrowserChrome::SetStatus(PRUint32 aType, const PRUnichar* aStatus)
+{
+    WebBrowserChromeUI::UpdateStatusBarText(this, aStatus);
+    return NS_OK;
+}
+
+NS_IMETHODIMP WebBrowserChrome::GetWebBrowser(nsIWebBrowser** aWebBrowser)
+{
+    NS_ENSURE_ARG_POINTER(aWebBrowser);
+    *aWebBrowser = mWebBrowser;
+    NS_IF_ADDREF(*aWebBrowser);
+    return NS_OK;
+}
+
+NS_IMETHODIMP WebBrowserChrome::SetWebBrowser(nsIWebBrowser* aWebBrowser)
+{
+    mWebBrowser = aWebBrowser;
+    return NS_OK;
+}
+
+NS_IMETHODIMP WebBrowserChrome::GetChromeFlags(PRUint32* aChromeMask)
+{
+    *aChromeMask = mChromeFlags;
+    return NS_OK;
+}
+
+NS_IMETHODIMP WebBrowserChrome::SetChromeFlags(PRUint32 aChromeMask)
+{
+    mChromeFlags = aChromeMask;
+    return NS_OK;
+}
+
+NS_IMETHODIMP WebBrowserChrome::DestroyBrowserWindow(void)
+{
+    WebBrowserChromeUI::Destroy(this);
+    return NS_OK;
+}
+
+
+// IN: The desired browser client area dimensions.
+NS_IMETHODIMP WebBrowserChrome::SizeBrowserTo(PRInt32 aWidth, PRInt32 aHeight)
+{
+  /* This isn't exactly correct: we're setting the whole window to
+     the size requested for the browser. At time of writing, though,
+     it's fine and useful for os2Embed's purposes. */
+  WebBrowserChromeUI::SizeTo(this, aWidth, aHeight);
+  mSizeSet = PR_TRUE;
+  return NS_OK;
+}
+
+
+NS_IMETHODIMP WebBrowserChrome::ShowAsModal(void)
+{
+  if (mDependentParent)
+    EnableChromeWindow(mDependentParent, PR_FALSE);
+
+  mContinueModalLoop = PR_TRUE;
+  RunEventLoop(mContinueModalLoop);
+
+  if (mDependentParent)
+    EnableChromeWindow(mDependentParent, PR_TRUE);
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP WebBrowserChrome::IsWindowModal(PRBool *_retval)
+{
+    *_retval = PR_FALSE;
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+NS_IMETHODIMP WebBrowserChrome::ExitModalEventLoop(nsresult aStatus)
+{
+  mContinueModalLoop = PR_FALSE;
+  return NS_OK;
+}
+
+//*****************************************************************************
+// WebBrowserChrome::nsIWebBrowserChromeFocus
+//*****************************************************************************
+
+NS_IMETHODIMP WebBrowserChrome::FocusNextElement()
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+NS_IMETHODIMP WebBrowserChrome::FocusPrevElement()
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+//*****************************************************************************
+// WebBrowserChrome::nsIWebProgressListener
+//*****************************************************************************   
+
+NS_IMETHODIMP WebBrowserChrome::OnProgressChange(nsIWebProgress *progress, nsIRequest *request,
+                                                  PRInt32 curSelfProgress, PRInt32 maxSelfProgress,
+                                                  PRInt32 curTotalProgress, PRInt32 maxTotalProgress)
+{
+    WebBrowserChromeUI::UpdateProgress(this, curTotalProgress, maxTotalProgress);
+    return NS_OK;
+}
+
+NS_IMETHODIMP WebBrowserChrome::OnStateChange(nsIWebProgress *progress, nsIRequest *request,
+                                               PRUint32 progressStateFlags, nsresult status)
+{
+    if ((progressStateFlags & STATE_START) && (progressStateFlags & STATE_IS_DOCUMENT))
+    {
+        WebBrowserChromeUI::UpdateBusyState(this, PR_TRUE);
+    }
+
+    if ((progressStateFlags & STATE_STOP) && (progressStateFlags & STATE_IS_DOCUMENT))
+    {
+        WebBrowserChromeUI::UpdateBusyState(this, PR_FALSE);
+        WebBrowserChromeUI::UpdateProgress(this, 0, 100);
+        WebBrowserChromeUI::UpdateStatusBarText(this, nsnull);
+        ContentFinishedLoading();
+    }
+
+    return NS_OK;
+}
+
+
+NS_IMETHODIMP WebBrowserChrome::OnLocationChange(nsIWebProgress* aWebProgress,
+                                                 nsIRequest* aRequest,
+                                                 nsIURI *location)
+{
+  PRBool isSubFrameLoad = PR_FALSE; // Is this a subframe load
+  if (aWebProgress) {
+    nsCOMPtr<nsIDOMWindow>  domWindow;
+    nsCOMPtr<nsIDOMWindow>  topDomWindow;
+    aWebProgress->GetDOMWindow(getter_AddRefs(domWindow));
+    if (domWindow) { // Get root domWindow
+      domWindow->GetTop(getter_AddRefs(topDomWindow));
+    }
+    if (domWindow != topDomWindow)
+      isSubFrameLoad = PR_TRUE;
+  }
+  if (!isSubFrameLoad)
+    WebBrowserChromeUI::UpdateCurrentURI(this);
+  return NS_OK;
+}
+
+NS_IMETHODIMP 
+WebBrowserChrome::OnStatusChange(nsIWebProgress* aWebProgress,
+                                 nsIRequest* aRequest,
+                                 nsresult aStatus,
+                                 const PRUnichar* aMessage)
+{
+    WebBrowserChromeUI::UpdateStatusBarText(this, aMessage);
+    return NS_OK;
+}
+
+
+
+NS_IMETHODIMP 
+WebBrowserChrome::OnSecurityChange(nsIWebProgress *aWebProgress, 
+                                    nsIRequest *aRequest, 
+                                    PRUint32 state)
+{
+    return NS_OK;
+}
+
+//*****************************************************************************
+// WebBrowserChrome::nsISHistoryListener
+//*****************************************************************************   
+
+NS_IMETHODIMP
+WebBrowserChrome::OnHistoryNewEntry(nsIURI * aNewURI)
+{
+    return SendHistoryStatusMessage(aNewURI, "add");
+}
+
+NS_IMETHODIMP
+WebBrowserChrome::OnHistoryGoBack(nsIURI * aBackURI, PRBool * aContinue)
+{
+    // For now, let the operation continue
+    *aContinue = PR_TRUE;
+    return SendHistoryStatusMessage(aBackURI, "back");
+}
+
+
+NS_IMETHODIMP
+WebBrowserChrome::OnHistoryGoForward(nsIURI * aForwardURI, PRBool * aContinue)
+{
+    // For now, let the operation continue
+    *aContinue = PR_TRUE;
+    return SendHistoryStatusMessage(aForwardURI, "forward");
+}
+
+
+NS_IMETHODIMP
+WebBrowserChrome::OnHistoryGotoIndex(PRInt32 aIndex, nsIURI * aGotoURI, PRBool * aContinue)
+{
+    // For now, let the operation continue
+    *aContinue = PR_TRUE;
+    return SendHistoryStatusMessage(aGotoURI, "goto", aIndex);
+}
+
+NS_IMETHODIMP
+WebBrowserChrome::OnHistoryReload(nsIURI * aURI, PRUint32 aReloadFlags, PRBool * aContinue)
+{
+    // For now, let the operation continue
+    *aContinue = PR_TRUE;
+    return SendHistoryStatusMessage(aURI, "reload", 0 /* no info to pass here */, aReloadFlags);
+}
+
+NS_IMETHODIMP
+WebBrowserChrome::OnHistoryPurge(PRInt32 aNumEntries, PRBool *aContinue)
+{
+    // For now let the operation continue
+    *aContinue = PR_FALSE;
+    return SendHistoryStatusMessage(nsnull, "purge", aNumEntries);
+}
+
+nsresult
+WebBrowserChrome::SendHistoryStatusMessage(nsIURI * aURI, char * operation, PRInt32 info1, PRUint32 aReloadFlags)
+{
+    nsCAutoString uriCStr;
+    if (aURI)
+    {
+        aURI->GetSpec(uriCStr);
+    }
+
+    nsString uriAStr;
+
+    if(!(nsCRT::strcmp(operation, "back")))
+    {
+        // Going back. XXX Get string from a resource file
+        uriAStr.Append(NS_LITERAL_STRING("Going back to url:"));
+        uriAStr.Append(NS_ConvertUTF8toUCS2(uriCStr));
+    }
+    else if (!(nsCRT::strcmp(operation, "forward")))
+    {
+        // Going forward. XXX Get string from a resource file
+        uriAStr.Append(NS_LITERAL_STRING("Going forward to url:"));
+        uriAStr.Append(NS_ConvertUTF8toUCS2(uriCStr));
+    }
+    else if (!(nsCRT::strcmp(operation, "reload")))
+    {
+        // Reloading. XXX Get string from a resource file
+        if (aReloadFlags & nsIWebNavigation::LOAD_FLAGS_BYPASS_PROXY && 
+            aReloadFlags & nsIWebNavigation::LOAD_FLAGS_BYPASS_CACHE)
+        {
+            uriAStr.Append(NS_LITERAL_STRING("Reloading url,(bypassing proxy and cache) :"));
+        }
+        else if (aReloadFlags & nsIWebNavigation::LOAD_FLAGS_BYPASS_PROXY)
+        {
+            uriAStr.Append(NS_LITERAL_STRING("Reloading url, (bypassing proxy):"));
+        }
+        else if (aReloadFlags & nsIWebNavigation::LOAD_FLAGS_BYPASS_CACHE)
+        {
+            uriAStr.Append(NS_LITERAL_STRING("Reloading url, (bypassing cache):"));
+        }
+        else
+        {
+            uriAStr.Append(NS_LITERAL_STRING("Reloading url, (normal):"));
+        }
+        uriAStr.Append(NS_ConvertASCIItoUCS2(uriCStr));
+    }
+    else if (!(nsCRT::strcmp(operation, "add")))
+    {
+        // Adding new entry. XXX Get string from a resource file
+        uriAStr.Append(NS_ConvertASCIItoUCS2(uriCStr));
+        uriAStr.Append(NS_LITERAL_STRING(" added to session History"));
+    }
+    else if (!(nsCRT::strcmp(operation, "goto")))
+    {
+        // Goto. XXX Get string from a resource file
+        uriAStr.Append(NS_LITERAL_STRING("Going to HistoryIndex:"));
+        uriAStr.AppendInt(info1);
+        uriAStr.Append(NS_LITERAL_STRING(" Url:"));
+        uriAStr.Append(NS_ConvertASCIItoUCS2(uriCStr));
+    }
+    else if (!(nsCRT::strcmp(operation, "purge")))
+    {
+        // Purging old entries
+        uriAStr.AppendInt(info1);
+        uriAStr.Append(NS_LITERAL_STRING(" purged from Session History"));
+    }
+
+    WebBrowserChromeUI::UpdateStatusBarText(this, uriAStr.get());
+
+    return NS_OK;
+}
+
+void WebBrowserChrome::ContentFinishedLoading()
+{
+  // if it was a chrome window and no one has already specified a size,
+  // size to content
+  if (mWebBrowser && !mSizeSet &&
+     (mChromeFlags & nsIWebBrowserChrome::CHROME_OPENAS_CHROME)) {
+    nsCOMPtr<nsIDOMWindow> contentWin;
+    mWebBrowser->GetContentDOMWindow(getter_AddRefs(contentWin));
+    if (contentWin)
+        contentWin->SizeToContent();
+    WebBrowserChromeUI::ShowWindow(this, PR_TRUE);
+  }
+}
+
+
+//*****************************************************************************
+// WebBrowserChrome::nsIEmbeddingSiteWindow
+//*****************************************************************************   
+
+NS_IMETHODIMP WebBrowserChrome::SetDimensions(PRUint32 aFlags, PRInt32 x, PRInt32 y, PRInt32 cx, PRInt32 cy)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+NS_IMETHODIMP WebBrowserChrome::GetDimensions(PRUint32 aFlags, PRInt32 *x, PRInt32 *y, PRInt32 *cx, PRInt32 *cy)
+{
+    if (aFlags & nsIEmbeddingSiteWindow::DIM_FLAGS_POSITION)
+    {
+        *x = 0;
+        *y = 0;
+    }
+    if (aFlags & nsIEmbeddingSiteWindow::DIM_FLAGS_SIZE_INNER ||
+        aFlags & nsIEmbeddingSiteWindow::DIM_FLAGS_SIZE_OUTER)
+    {
+        *cx = 0;
+        *cy = 0;
+    }
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void setFocus (); */
+NS_IMETHODIMP WebBrowserChrome::SetFocus()
+{
+    WebBrowserChromeUI::SetFocus(this);
+    return NS_OK;
+}
+
+/* attribute wstring title; */
+NS_IMETHODIMP WebBrowserChrome::GetTitle(PRUnichar * *aTitle)
+{
+   NS_ENSURE_ARG_POINTER(aTitle);
+
+   *aTitle = nsnull;
+   
+   return NS_ERROR_NOT_IMPLEMENTED;
+}
+NS_IMETHODIMP WebBrowserChrome::SetTitle(const PRUnichar * aTitle)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* attribute boolean visibility; */
+NS_IMETHODIMP WebBrowserChrome::GetVisibility(PRBool * aVisibility)
+{
+    NS_ENSURE_ARG_POINTER(aVisibility);
+    *aVisibility = PR_TRUE;
+    return NS_OK;
+}
+NS_IMETHODIMP WebBrowserChrome::SetVisibility(PRBool aVisibility)
+{
+    return NS_OK;
+}
+
+/* attribute nativeSiteWindow siteWindow */
+NS_IMETHODIMP WebBrowserChrome::GetSiteWindow(void * *aSiteWindow)
+{
+   NS_ENSURE_ARG_POINTER(aSiteWindow);
+
+   *aSiteWindow = mNativeWindow;
+   return NS_OK;
+}
+
+
+//*****************************************************************************
+// WebBrowserChrome::nsIObserver
+//*****************************************************************************   
+
+NS_IMETHODIMP WebBrowserChrome::Observe(nsISupports *aSubject, const char *aTopic, const PRUnichar *someData)
+{
+    nsresult rv = NS_OK;
+    if (nsCRT::strcmp(aTopic, "profile-change-teardown") == 0)
+    {
+        // A profile change means death for this window
+        WebBrowserChromeUI::Destroy(this);
+    }
+    return rv;
+}
+
+//*****************************************************************************
+// WebBrowserChrome::nsIContextMenuListener
+//*****************************************************************************   
+
+/* void OnShowContextMenu (in unsigned long aContextFlags, in nsIDOMEvent aEvent, in nsIDOMNode aNode); */
+NS_IMETHODIMP WebBrowserChrome::OnShowContextMenu(PRUint32 aContextFlags, nsIDOMEvent *aEvent, nsIDOMNode *aNode)
+{
+    WebBrowserChromeUI::ShowContextMenu(this, aContextFlags, aEvent, aNode);
+    return NS_OK;
+}
+
+//*****************************************************************************
+// WebBrowserChrome::nsITooltipListener
+//*****************************************************************************   
+
+/* void OnShowTooltip (in long aXCoords, in long aYCoords, in wstring aTipText); */
+NS_IMETHODIMP WebBrowserChrome::OnShowTooltip(PRInt32 aXCoords, PRInt32 aYCoords, const PRUnichar *aTipText)
+{
+    WebBrowserChromeUI::ShowTooltip(this, aXCoords, aYCoords, aTipText);
+    return NS_OK;
+}
+
+/* void OnHideTooltip (); */
+NS_IMETHODIMP WebBrowserChrome::OnHideTooltip()
+{
+    WebBrowserChromeUI::HideTooltip(this);
+    return NS_OK;
+}
---
+++ embedding/tests/os2Embed/WebBrowserChrome.h
@@ -0,0 +1,121 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: Mozilla-sample-code 1.0
+ *
+ * Copyright (c) 2002 Netscape Communications Corporation and
+ * other contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this Mozilla sample software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#ifndef __WebBrowserChrome__
+#define __WebBrowserChrome__
+
+#include "nsCOMPtr.h"
+#include "nsIGenericFactory.h"
+#include "nsString.h"
+#include "nsIWebBrowserChrome.h"
+#include "nsIWebBrowserChromeFocus.h"
+
+#include "nsIDocShell.h"
+#include "nsIContentViewer.h"
+#include "nsIContentViewerFile.h"
+#include "nsIBaseWindow.h"
+#include "nsIEmbeddingSiteWindow.h"
+#include "nsIWebNavigation.h"
+#include "nsIWebProgressListener.h"
+#include "nsIInterfaceRequestor.h"
+#include "nsIInterfaceRequestorUtils.h"
+#include "nsIWebBrowser.h"
+#include "nsIObserver.h"
+#include "nsWeakReference.h"
+#include "nsISHistoryListener.h"
+#include "nsIContextMenuListener.h"
+#include "nsITooltipListener.h"
+
+class WebBrowserChromeUI
+{
+public:
+    static nativeWindow CreateNativeWindow(nsIWebBrowserChrome* chrome);
+    static void Destroy(nsIWebBrowserChrome* chrome);
+    static void Destroyed(nsIWebBrowserChrome* chrome);
+    static void SetFocus(nsIWebBrowserChrome *chrome);
+    static void UpdateStatusBarText(nsIWebBrowserChrome *aChrome, const PRUnichar* aStatusText);
+    static void UpdateCurrentURI(nsIWebBrowserChrome *aChrome);
+    static void UpdateBusyState(nsIWebBrowserChrome *aChrome, PRBool aBusy);
+    static void UpdateProgress(nsIWebBrowserChrome *aChrome, PRInt32 aCurrent, PRInt32 aMax);
+    static void GetResourceStringById(PRInt32 aID, char ** aReturn);
+    static void ShowContextMenu(nsIWebBrowserChrome *aChrome, PRUint32 aContextFlags, nsIDOMEvent *aEvent, nsIDOMNode *aNode);
+    static void ShowTooltip(nsIWebBrowserChrome *aChrome, PRInt32 aXCoords, PRInt32 aYCoords, const PRUnichar *aTipText);
+    static void HideTooltip(nsIWebBrowserChrome *aChrome);
+    static void ShowWindow(nsIWebBrowserChrome *aChrome, PRBool aShow);
+    static void SizeTo(nsIWebBrowserChrome *aChrome, PRInt32 aWidth, PRInt32 aHeight);
+};
+
+class WebBrowserChrome   : public nsIWebBrowserChrome,
+                           public nsIWebBrowserChromeFocus,
+                           public nsIWebProgressListener,
+                           public nsIEmbeddingSiteWindow,
+                           public nsIInterfaceRequestor,
+                           public nsISHistoryListener,
+                           public nsIObserver,
+                           public nsIContextMenuListener,
+                           public nsITooltipListener,
+                           public nsSupportsWeakReference
+
+{
+public:
+    WebBrowserChrome();
+    virtual ~WebBrowserChrome();
+
+    NS_DECL_ISUPPORTS
+    NS_DECL_NSIWEBBROWSERCHROME
+    NS_DECL_NSIWEBBROWSERCHROMEFOCUS
+    NS_DECL_NSIWEBPROGRESSLISTENER
+    NS_DECL_NSIEMBEDDINGSITEWINDOW
+    NS_DECL_NSIINTERFACEREQUESTOR
+    NS_DECL_NSISHISTORYLISTENER
+    NS_DECL_NSIOBSERVER
+    NS_DECL_NSICONTEXTMENULISTENER
+    NS_DECL_NSITOOLTIPLISTENER
+
+    nsresult CreateBrowser(PRInt32 aX, PRInt32 aY, PRInt32 aCX, PRInt32 aCY,
+                           nsIWebBrowser **aBrowser);
+
+    void     SetParent(nsIWebBrowserChrome *aParent)
+               { mDependentParent = aParent; }
+   
+protected:
+    nsresult SendHistoryStatusMessage(nsIURI * aURI, char * operation, PRInt32 info1=0, PRUint32 info2=0);
+
+    void ContentFinishedLoading();
+
+    nativeWindow mNativeWindow;
+    PRUint32     mChromeFlags;
+    PRBool       mContinueModalLoop;
+    PRBool       mSizeSet;
+
+    nsCOMPtr<nsIWebBrowser> mWebBrowser;
+    nsCOMPtr<nsIWebBrowserChrome> mDependentParent; // opener (for dependent windows only)
+};
+
+#endif /* __WebBrowserChrome__ */
