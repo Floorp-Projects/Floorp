6f4d2d75e1f4a969d94970de7a5e090cfdb58e2f
wtc@netscape.com <wtc@netscape.com>
2003-01-23 17:16:32 +0000

    Bug 90010: added support for Linux for s390 and s390x.  The patch is contributed by Gerhard Tonn <GerhardTonn@swol.de>.

--- security/coreconf/Linux.mk
+++ security/coreconf/Linux.mk
@@ -78,8 +78,18 @@ ifeq (,$(filter-out arm% sa110,$(OS_TEST)))
 	OS_REL_CFLAGS   = -DLINUX1_2 -D_XOPEN_SOURCE
 	CPU_ARCH        = arm
 else
+ifeq ($(OS_TEST),s390)
+	OS_REL_CFLAGS   = -DLINUX1_2 -D_XOPEN_SOURCE
+	CPU_ARCH        = s390
+else
+ifeq ($(OS_TEST),s390x)
+	OS_REL_CFLAGS   = -DLINUX1_2 -D_XOPEN_SOURCE
+	CPU_ARCH        = s390x
+else
 	OS_REL_CFLAGS	= -DLINUX1_2 -Di386 -D_XOPEN_SOURCE
 	CPU_ARCH	= x86
+endif
+endif
 endif
 endif
 endif
