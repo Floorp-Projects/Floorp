a66a4d539efe0964e27d63998d67835e9eba9967
wtc%netscape.com <wtc%netscape.com>
2003-01-23 17:03:22 +0000

    Bug 90010: added support for Linux for s390 and s390x.  The patch is contributed by Gerhard Tonn <GerhardTonn@swol.de>. Modified Files: _linux.cfg _linux.h prprf.c

--- nsprpub/pr/include/md/_linux.h
+++ nsprpub/pr/include/md/_linux.h
@@ -59,6 +59,10 @@
 #define _PR_SI_ARCHITECTURE "mips"
 #elif defined(__arm__)
 #define _PR_SI_ARCHITECTURE "arm"
+#elif defined(__s390__)
+#define _PR_SI_ARCHITECTURE "s390"
+#elif defined(__s390x__)
+#define _PR_SI_ARCHITECTURE "s390x"
 #else
 #error "Unknown CPU architecture"
 #endif
