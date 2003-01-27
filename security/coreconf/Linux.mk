a6be0bf311392758aca30df193bd98e8c2248126
wtc%netscape.com <wtc%netscape.com>
2003-01-27 22:36:06 +0000

    Bug 90010: added support for parisc64.  I received this patch from Christopher Blizzard <blizzard@mozilla.org>.

--- security/coreconf/Linux.mk
+++ security/coreconf/Linux.mk
@@ -78,6 +78,10 @@ ifeq (,$(filter-out arm% sa110,$(OS_TEST)))
 	OS_REL_CFLAGS   = -DLINUX1_2 -D_XOPEN_SOURCE
 	CPU_ARCH        = arm
 else
+ifeq ($(OS_TEST),parisc64)
+	OS_REL_CFLAGS   = -DLINUX1_2 -D_XOPEN_SOURCE
+	CPU_ARCH        = hppa
+else
 ifeq ($(OS_TEST),s390)
 	OS_REL_CFLAGS   = -DLINUX1_2 -D_XOPEN_SOURCE
 	CPU_ARCH        = s390
@@ -88,6 +92,7 @@ ifeq ($(OS_TEST),s390x)
 else
 	OS_REL_CFLAGS	= -DLINUX1_2 -Di386 -D_XOPEN_SOURCE
 	CPU_ARCH	= x86
+endif
 endif
 endif
 endif
