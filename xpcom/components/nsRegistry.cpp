1953de132a0814a6da128b9cc8efbb9bfa01a0aa
dougt%netscape.com <dougt%netscape.com>
2003-01-28 22:07:53 +0000

    xpcom is unable to always load .js component file.  r=ssu, sr=alecf, a=rjesup, b=190560 build/nsXPComInit.cpp

--- xpcom/components/nsRegistry.cpp
+++ xpcom/components/nsRegistry.cpp
@@ -1992,7 +1992,7 @@ nsRegistryFactory::LockFactory(PRBool aLock)
 
 // This is a temporary hack; needs work to support dynamic binding
 // via nsComponentManager and support for multiple factories per DLL.
-extern "C" NS_EXPORT nsresult
+nsresult
 NS_RegistryGetFactory(nsIFactory** aFactory ) {
     nsresult rv = NS_OK;
 
