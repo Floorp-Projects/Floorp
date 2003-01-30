5a1321d1da84a092a06905af0fbd402bf842cecf
pschwartau%netscape.com <pschwartau%netscape.com>
2003-01-30 02:25:51 +0000

    We've decided to follow Perl, rather than ECMA, and allow unescaped braces in regexp patterns, evan if they are not part of a quantifier. See bug 190685.

--- js/tests/ecma_3/RegExp/regress-188206.js
+++ js/tests/ecma_3/RegExp/regress-188206.js
@@ -39,6 +39,7 @@
 *
 * See http://bugzilla.mozilla.org/show_bug.cgi?id=188206
 * and http://bugzilla.mozilla.org/show_bug.cgi?id=85721#c48 etc.
+* and http://bugzilla.mozilla.org/show_bug.cgi?id=190685
 *
 */
 //-----------------------------------------------------------------------------
@@ -48,6 +49,8 @@ var summary = 'Invalid use of regexp quantifiers should generate SyntaxErrors';
 var TEST_PASSED = 'SyntaxError';
 var TEST_FAILED = 'Generated an error, but NOT a SyntaxError!';
 var TEST_FAILED_BADLY = 'Did not generate ANY error!!!';
+var CHECK_PASSED = 'Should not generate an error';
+var CHECK_FAILED = 'Generated an error!';
 var status = '';
 var statusitems = [];
 var actual = '';
@@ -87,43 +90,88 @@ testThis(' /a???/ ');
 status = inSection(6);
 testThis(' /a????/ ');
 
-/*
- * Misusing the {DecmalDigits} quantifier -
- */
-status = inSection(7);
-testThis(' /a*{/ ');
-
-status = inSection(8);
-testThis(' /a{}/ ');
-
 
 /*
  * Now do some weird things on the left side of the regexps -
  */
-status = inSection(9);
+status = inSection(7);
 testThis(' /*a/ ');
 
-status = inSection(10);
+status = inSection(8);
 testThis(' /**a/ ');
 
-status = inSection(11);
+status = inSection(9);
 testThis(' /+a/ ');
 
-status = inSection(12);
+status = inSection(10);
 testThis(' /++a/ ');
 
-status = inSection(13);
+status = inSection(11);
 testThis(' /?a/ ');
 
-status = inSection(14);
+status = inSection(12);
 testThis(' /??a/ ');
 
+
+/*
+ * Misusing the {DecmalDigits} quantifier - according to ECMA.
+ *
+ * ECMA-262 Edition 3 prohibits the use of unescaped braces in
+ * regexp patterns, unless they form part of a quantifier
+ *
+ * Hovever, Perl does not prohibit this. If not used as part
+ * of a quantifer, Perl treats braces literally.
+ *
+ * We decided to follow Perl on this for backward compatibility.
+ * See http://bugzilla.mozilla.org/show_bug.cgi?id=190685.
+ *
+ * Therefore NONE of the following ECMA violations should generate
+ * a SyntaxError. Note we use checkThis() instead of testThis().
+ */
+status = inSection(13);
+checkThis(' /a*{/ ');
+
+status = inSection(14);
+checkThis(' /a{}/ ');
+
 status = inSection(15);
-testThis(' /{a/ ');
+checkThis(' /{a/ ');
 
 status = inSection(16);
-testThis(' /}a/ ');
+checkThis(' /}a/ ');
 
+status = inSection(17);
+checkThis(' /x{abc}/ ');
+
+status = inSection(18);
+checkThis(' /{{0}/ ');
+
+status = inSection(19);
+checkThis(' /{{1}/ ');
+
+status = inSection(20);
+checkThis(' /x{{0}/ ');
+
+status = inSection(21);
+checkThis(' /x{{1}/ ');
+
+status = inSection(22);
+checkThis(' /x{{0}}/ ');
+
+status = inSection(23);
+checkThis(' /x{{1}}/ ');
+
+status = inSection(24);
+checkThis(' /x{{0}}/ ');
+
+status = inSection(25);
+checkThis(' /x{{1}}/ ');
+
+status = inSection(26);
+checkThis(' /x{{0}}/ ');
+
+status = inSection(27);
+checkThis(' /x{{1}}/ ');
 
 
 
@@ -133,6 +181,9 @@ test();
 
 
 
+/*
+ * Invalid syntax should generate a SyntaxError
+ */
 function testThis(sInvalidSyntax)
 {
   expect = TEST_PASSED;
@@ -148,6 +199,30 @@ function testThis(sInvalidSyntax)
       actual = TEST_PASSED;
     else
       actual = TEST_FAILED;
+  }
+
+  statusitems[UBound] = status;
+  expectedvalues[UBound] = expect;
+  actualvalues[UBound] = actual;
+  UBound++;
+}
+
+
+/*
+ * Allowed syntax shouldn't generate any errors
+ */
+function checkThis(sAllowedSyntax)
+{
+  expect = CHECK_PASSED;
+  actual = CHECK_PASSED;
+
+  try
+  {
+    eval(sAllowedSyntax);
+  }
+  catch(e)
+  {
+    actual = CHECK_FAILED;
   }
 
   statusitems[UBound] = status;
