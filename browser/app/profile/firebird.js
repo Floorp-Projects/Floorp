6f2421f4eade2b36a7bb0e9704b61140d9d97403
asa%mozilla.org <asa%mozilla.org>
2003-02-02 06:37:10 +0000

    bump rv to 1.3b

--- browser/app/profile/firebird.js
+++ browser/app/profile/firebird.js
@@ -46,7 +46,7 @@ pref("keyword.URL", "http://www.google.com/search?btnI=I%27m+Feeling+Lucky&q=");
 
 pref("general.useragent.locale", "chrome://navigator/locale/navigator.properties");
 pref("general.useragent.contentlocale", "chrome://navigator-region/locale/region.properties");
-pref("general.useragent.misc", "rv:1.3a");
+pref("general.useragent.misc", "rv:1.3b");
 pref("general.useragent.vendor", "Phoenix");
 pref("general.useragent.vendorSub", "0.5");
 
