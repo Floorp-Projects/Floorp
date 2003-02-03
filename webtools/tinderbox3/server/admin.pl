d39f63c26454d9bd6767e5f07b0adbe0d670ebc5
johnkeis <johnkeis>
2003-02-03 03:46:42 +0000

    Support logins, show more useful information

--- webtools/tinderbox3/server/admin.pl
+++ webtools/tinderbox3/server/admin.pl
@@ -4,19 +4,27 @@ use strict;
 use CGI;
 use Tinderbox3::Header;
 use Tinderbox3::DB;
+use Tinderbox3::Login;
 
 my $p = new CGI;
 my $dbh = get_dbh();
-header($p, "Global Admin");
 
-print "<h2>Administrate Tinderbox</h2>\n";
+my ($login, $cookie) = check_session($p, $dbh);
+header($p, $login, $cookie, "Administrate Tinderbox");
 
 print "<table class=editlist><tr><th>Trees</th></tr>\n";
-foreach my $tree (@{$dbh->selectcol_arrayref("SELECT tree_name FROM tbox_tree")}) {
-  print "<tr><td><a href='admintree.pl?tree=$tree'>$tree</a></td></tr>\n";
+my $sth = $dbh->prepare("SELECT tree_name FROM tbox_tree");
+$sth->execute();
+while (my $tree_info = $sth->fetchrow_arrayref()) {
+  my $tree = $tree_info->[0];
+  my @actions;
+  push @actions, "<a href='sheriff.pl?tree=$tree'>Sheriff</a>";
+  push @actions, "<a href='admintree.pl?tree=$tree'>Edit</a>";
+  push @actions, "<a href='showbuilds.pl?tree=$tree'>View</a>";
+  print "<tr><td><a href='showbuilds.pl?tree=$tree'>$tree</a> (" . join(' | ', @actions) . ")</td></tr>\n";
 }
 
-print "<tr><td><a href='admintree.pl?tree='>Add Tree</a></td></tr>\n";
+print "<tr><td><a href='admintree.pl'>Add Tree</a></td></tr>\n";
 print "</table>\n";
 
 footer($p);
