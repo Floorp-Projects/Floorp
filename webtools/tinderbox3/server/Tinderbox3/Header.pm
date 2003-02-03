6c1a977eb91a446e246916280415dff10ebb5dab
johnkeis <johnkeis>
2003-02-03 03:48:58 +0000

    Add login functionality, lose absolute links

--- webtools/tinderbox3/server/Tinderbox3/Header.pm
+++ webtools/tinderbox3/server/Tinderbox3/Header.pm
@@ -9,16 +9,19 @@ our @ISA = qw(Exporter);
 our @EXPORT = qw(header footer);
 
 sub header {
-  my ($p, $title) = @_;
-  print $p->header;
+  my ($p, $login, $cookie, $title, $tree, $machine_id, $machine_name) = @_;
+  print $p->header(-cookie => $cookie);
   print <<EOM;
 <html>
 <head>
 <title>Tinderbox - $title</title>
-<link rel="stylesheet" type="text/css" href="/~jkeiser/tbox3.css">
+<link rel="stylesheet" type="text/css" href="tbox3-admin.css">
 </head>
 <body>
+<h2>$title</h2>
 EOM
+
+  nav_links($login, $tree, $machine_id, $machine_name);
 }
 
 sub footer {
@@ -26,6 +29,24 @@ sub footer {
 </body>
 </html>
 EOM
+}
+
+sub nav_links {
+  my ($login, $tree, $machine_id, $machine_name) = @_;
+  print "<p>\n";
+  if ($login) {
+    print "Logged in as: $login (<a href='admin.pl?-logout=1'>Log out</a>)\n";
+  } else {
+    print "Not Logged In (<a href='login.pl'>Log in</a>)\n";
+  }
+  print "<br><strong><a href='admin.pl'>Administrate Tinderbox</a></strong>";
+  if ($tree) {
+    print "<br>Tree $tree: <a href='sheriff.pl?tree=$tree'>Sheriff Tree</a> | <a href='admintree.pl?tree=$tree'>Edit Tree</a> | <a href='showbuilds.pl?tree=$tree'>View Tree</a>\n";
+    if ($machine_id) {
+      print "<br>$machine_name: <a href='adminmachine.pl?machine_id=$machine_id'>Edit</a> | <a href='adminmachine.pl?action=kick_machine&machine_id=$machine_id'>Kick</a>\n";
+    }
+  }
+  print "</strong></p>\n";
 }
 
 1
