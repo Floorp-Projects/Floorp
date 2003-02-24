62d505a2a6ed62d13b94728d273b7c1d0d205923
mkaply%us.ibm.com <mkaply%us.ibm.com>
2003-02-24 21:33:12 +0000

    #194298 r=pedemont, sr=blizzard (platform specific), a=dveditz OS/2 only - rewrite of xpicleanup

--- xpinstall/cleanup/InstallCleanupOS2.cpp
+++ xpinstall/cleanup/InstallCleanupOS2.cpp
@@ -20,23 +20,29 @@
  *
  * Contributor(s): 
  *   Don Bragg <dbragg@netscape.com>
+ *   IBM Corp.
  */
 
 #include "InstallCleanup.h"
-
+#include "InstallCleanupDefines.h"
+#define INCL_DOSERRORS
+#define INCL_DOS
+#include <os2.h>
+#include <string.h>
+#include <sys/stat.h>
 //----------------------------------------------------------------------------
 // Native OS/2 file deletion function
 //----------------------------------------------------------------------------
 int NativeDeleteFile(const char* aFileToDelete)
 {
-    struct stat fileStack;
-    if (stat(aFileToDelete, &fileStack)!= 0)
+    struct stat st;
+    if (stat(aFileToDelete, &st) != 0)
     {
       return DONE;// No file to delete, do nothing.
     }
     else 
     {
-        if(remove(aFileToDelete)!= 0)
+        if(DosDelete(aFileToDelete) != NO_ERROR)
           return TRY_LATER;
     }
     return DONE;
@@ -47,47 +53,69 @@ int NativeDeleteFile(const char* aFileToDelete)
 //----------------------------------------------------------------------------
 int NativeReplaceFile(const char* replacementFile, const char* doomedFile )
 {
-    struct stat fileStack;
-
     // replacement file must exist, doomed file doesn't have to
-    if (stat(replacementFile, &fileStack) != 0)
+    struct stat st;
+    if (stat(replacementFile, &st) != 0)
         return DONE;
 
     // don't have to do anything if the files are the same
-    if (strcmp(replacementFile, doomedFile))
+    if (stricmp(replacementFile, doomedFile) == 0)
         return DONE;
 
-    if (stat(doomedFile,&fileStack) != 0)
+    if (DosDelete(doomedFile) != NO_ERROR)
     {
-      // doomedFile is gone move new file into place
-      if (!rename(replacementFile, doomedFile))
-          return TRY_LATER; // this shouldn't happen
+        if (stat(doomedFile, &st) == 0)
+            return TRY_LATER;
     }
-    else
-    {
-      //remove failed
-      return TRY_LATER;
-    }
+    
+    // doomedFile is gone move new file into place
+    if (DosMove(replacementFile, doomedFile) != NO_ERROR)
+        return TRY_LATER; // this shouldn't happen
 
     return DONE;
 }
 
-int main()
+int main(int argc, char *argv[], char *envp[])
 {
-    HREG reg;
+    HREG  reg;
+    BOOL foundSpecialFile = FALSE;
+    struct stat st;
+
     int status = DONE;
 
     if ( REGERR_OK == NR_StartupRegistry())
     {
-        if ( REGERR_OK == NR_RegOpen("", &reg) )
+        char regFilePath[CCHMAXPATH];
+
+        strcpy(regFilePath, argv[0]);
+        char* lastSlash = strrchr(regFilePath, '\\');
+        lastSlash++; 
+        *lastSlash = 0;//strip of the executable name
+        strcat(regFilePath, CLEANUP_REGISTRY); //append reg file name
+    
+        if (stat(regFilePath, &st) != 0)
+          strcpy(regFilePath, ""); // an empty reg file tells RegOpen to get the "default"
+        else
+          foundSpecialFile = TRUE;
+
+        if ( REGERR_OK == NR_RegOpen(regFilePath, &reg) )
         {
-            do {
+            int numAttempts = 0;
+            do 
+            {
                 status = PerformScheduledTasks(reg);
-                if (status != DONE)
-                   DosSleep(1000);
-            } while (status == TRY_LATER);
+                if (status != DONE) {
+                    DosSleep(1000); // Sleep for 1 second
+                    numAttempts++;
+                }
+            } while ((status == TRY_LATER) && numAttempts <= 5);
+
+            NR_RegClose(&reg);
         }
         NR_ShutdownRegistry();
+        if (status == DONE) {
+            DosDelete(regFilePath);
+        }
     }
     return(0);
 }
