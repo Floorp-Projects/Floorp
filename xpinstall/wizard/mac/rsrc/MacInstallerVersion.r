098f5364a401eb2e4d9f735f62166451679a8fe3
twalker%netscape.com <twalker%netscape.com>
2003-02-25 15:41:10 +0000

    Version updated to 1.4a. bug# 194085. r=lpham sr=jj

--- xpinstall/wizard/mac/rsrc/MacInstallerVersion.r
+++ xpinstall/wizard/mac/rsrc/MacInstallerVersion.r
@@ -34,11 +34,11 @@ resource 'vers' (1, "XPInstall version") {
 
 resource 'vers' (2, "Mozilla Installer version") {
         1,
-        0x30,
-        beta,	// {alpha, beta, release}
+        0x40,
+        alpha,	// {alpha, beta, release}
         0,			// alpha or beta number; 0 for release
         0,			// language
-        "1.3b",		// short version string
-        "Mozilla 1.3b Installer"	// long version string
+        "1.4a",		// short version string
+        "Mozilla 1.4a Installer"	// long version string
 };
 
