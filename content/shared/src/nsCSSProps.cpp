d204e4c941c625bdae0e0580fd8511fdc3399ce0
sfraser@netscape.com <sfraser@netscape.com>
2003-02-26 03:34:15 +0000

    Declare kCSSRawProperties and kCSSRawKeywords as 'extern' so that tests code can link with the .a, and get them.

--- content/shared/src/nsCSSProps.cpp
+++ content/shared/src/nsCSSProps.cpp
@@ -47,6 +47,9 @@
 #include "nsReadableUtils.h"
 #include "nsStaticNameTable.h"
 
+// required to make the symbol external, so that TestCSSPropertyLookup.cpp can link with it
+extern const char* const kCSSRawProperties[];
+
 // define an array of all CSS properties
 #define CSS_PROP(_name, _id, _method, _hint) #_name,
 const char* const kCSSRawProperties[] = {
--- layout/style/nsCSSKeywords.cpp
+++ layout/style/nsCSSKeywords.cpp
@@ -41,6 +41,9 @@
 #include "nsStaticNameTable.h"
 #include "nsReadableUtils.h"
 
+// required to make the symbol external, so that TestCSSPropertyLookup.cpp can link with it
+extern const char* const kCSSRawKeywords[];
+
 // define an array of all CSS keywords
 #define CSS_KEY(_name,_id) #_name,
 const char* const kCSSRawKeywords[] = {
