b0a1568c8a665b13683a4b7de40b753cd35759a1
smontagu@netscape.com <smontagu@netscape.com>
2003-02-27 01:51:04 +0000

    Whitespace change to note that the previous checkin was for bug 188759.

--- layout/html/base/src/nsTextTransformer.cpp
+++ layout/html/base/src/nsTextTransformer.cpp
@@ -14,7 +14,7 @@
  *
  * The Original Code is Mozilla Communicator client code.
  *
- * The Initial Developer of the Original Code is 
+ * The Initial Developer of the Original Code is
  * Netscape Communications Corporation.
  * Portions created by the Initial Developer are Copyright (C) 1998
  * the Initial Developer. All Rights Reserved.
