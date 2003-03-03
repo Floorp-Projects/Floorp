d2f3e65fc575a356ed2098348f21cff5357f49d0
chanial%noos.fr <chanial%noos.fr>
2003-03-03 06:41:51 +0000

    some cleanup

--- toolkit/content/nsWidgetStateManager.js
+++ toolkit/content/nsWidgetStateManager.js
@@ -1,28 +1,28 @@
-/* -*- Mode: Java; tab-width: 2; c-basic-offset: 2; -*-
- *
- * The contents of this file are subject to the Netscape Public License
- * Version 1.0 (the "NPL"); you may not use this file except in
- * compliance with the NPL.  You may obtain a copy of the NPL at
- * http://www.mozilla.org/NPL/
- *
- * Software distributed under the NPL is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
- * for the specific language governing rights and limitations under the
- * NPL.
- *
- * The Initial Developer of this code under the NPL is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
- * Reserved.
- *
- * Contributor(s):
- *   Ben Goodger <ben@netscape.com> (Original Author)
- */
-
-/** Presenting widgetStateManager the Third.
- *   a production of ye olde bard Ben Goodger and his Merry XUL Widget Crewe.
- *  =>> MODIFICATIONS MUST BE REVIEWED BY ben@netscape.com!!! <<=
- **/
+# -*- Mode: Java; tab-width: 2; c-basic-offset: 2; -*-
+#
+# The contents of this file are subject to the Netscape Public License
+# Version 1.0 (the "NPL"); you may not use this file except in
+# compliance with the NPL.  You may obtain a copy of the NPL at
+# http://www.mozilla.org/NPL/
+#
+# Software distributed under the NPL is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+# for the specific language governing rights and limitations under the
+# NPL.
+#
+# The Initial Developer of this code under the NPL is Netscape
+# Communications Corporation.  Portions created by Netscape are
+# Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+# Reserved.
+#
+# Contributor(s):
+#   Ben Goodger <ben@netscape.com> (Original Author)
+#
+#
+# ** Presenting widgetStateManager the Third.
+# *   a production of ye olde bard Ben Goodger and his Merry XUL Widget Crewe.
+# *  =>> MODIFICATIONS MUST BE REVIEWED BY ben@netscape.com!!! <<=
+# **
 
 var wsm;
 
