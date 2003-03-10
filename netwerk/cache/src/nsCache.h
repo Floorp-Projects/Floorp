61cb379246e4f004690eeff3e5c7e64ac0fc6756
gordon@netscape.com <gordon@netscape.com>
2003-03-10 23:19:05 +0000

    for bug 103851 "move cache deletion to another thread", r=bryner, sr=darin.

--- netwerk/cache/src/nsCache.h
+++ netwerk/cache/src/nsCache.h
@@ -31,6 +31,7 @@
 #define _nsCache_h_
 
 #include "nsISupports.h"
+#include "nsIFile.h"
 #include "nsAString.h"
 #include "prtime.h"
 #include "nsError.h"
@@ -40,17 +41,23 @@
 #if defined(PR_LOGGING)
 extern PRLogModuleInfo * gCacheLog;
 void   CacheLogInit();
+void   CacheLogPrintPath(PRLogModuleLevel level,
+                         char *           format,
+                         nsIFile *        item);
 #define CACHE_LOG_INIT()        CacheLogInit()
 #define CACHE_LOG_ALWAYS(args)  PR_LOG(gCacheLog, PR_LOG_ALWAYS, args)
 #define CACHE_LOG_ERROR(args)   PR_LOG(gCacheLog, PR_LOG_ERROR, args)
 #define CACHE_LOG_WARNING(args) PR_LOG(gCacheLog, PR_LOG_WARNING, args)
 #define CACHE_LOG_DEBUG(args)   PR_LOG(gCacheLog, PR_LOG_DEBUG, args)
+#define CACHE_LOG_PATH(level, format, item) \
+                                CacheLogPrintPath(level, format, item)
 #else
 #define CACHE_LOG_INIT()        {}
 #define CACHE_LOG_ALWAYS(args)  {}
 #define CACHE_LOG_ERROR(args)   {}
 #define CACHE_LOG_WARNING(args) {}
 #define CACHE_LOG_DEBUG(args)   {}
+#define CACHE_LOG_PATH(level, format, item)  {}
 #endif
 
 
