f3792006e16fcfe38cfead9d47cf94a11bcd881e
seawood@netscape.com <seawood@netscape.com>
2003-03-16 06:49:44 +0000

    Force xpcom_compat to be shared again duue to win32 static build bustage.

--- config/static-config.mk
+++ config/static-config.mk
@@ -100,6 +100,9 @@ endif
 # Component Makefile always brings in this.
 # STATIC_EXTRA_LIBS	+= $(TK_LIBS)
 
+# Some random modules require this
+STATIC_EXTRA_LIBS	+= $(MOZ_XPCOM_OBSOLETE_LIBS)
+
 ifeq ($(OS_ARCH),WINNT)
 STATIC_EXTRA_LIBS += $(call EXPAND_LIBNAME,comctl32 comdlg32 uuid shell32 ole32 oleaut32 version winspool)
 endif
