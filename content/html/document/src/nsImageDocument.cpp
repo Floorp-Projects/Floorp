38a936671157d85a1616ae25f86c45d3875e95d3
bzbarsky@mit.edu <bzbarsky@mit.edu>
2003-03-20 02:03:13 +0000

    Fix crash when viewing standalone images if images are blocked in mailnews. Bug 198301, r=jkeiser, sr=jst

--- content/html/document/src/nsImageDocument.cpp
+++ content/html/document/src/nsImageDocument.cpp
@@ -287,12 +287,6 @@ nsImageDocument::StartDocumentLoad(const char*         aCommand,
     }
   }
 
-  // Create synthetic document
-  rv = CreateSyntheticDocument();
-  if (NS_FAILED(rv)) {
-    return rv;
-  }
-
   NS_ASSERTION(aDocListener, "null aDocListener");
   *aDocListener = new ImageListener(this);
   if (!*aDocListener)
@@ -324,7 +318,21 @@ nsImageDocument::SetScriptGlobalObject(nsIScriptGlobalObject* aScriptGlobalObjec
       target->RemoveEventListener(NS_LITERAL_STRING("keypress"), this, PR_FALSE);
     }
   }
-  else {
+
+  // Set the script global object on the superclass before doing
+  // anything that might require it....
+  nsresult rv = nsHTMLDocument::SetScriptGlobalObject(aScriptGlobalObject);
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+  
+  if (aScriptGlobalObject) {
+    // Create synthetic document
+    rv = CreateSyntheticDocument();
+    if (NS_FAILED(rv)) {
+      return rv;
+    }
+
     if (mImageResizingEnabled) {
       nsCOMPtr<nsIDOMEventTarget> target = do_QueryInterface(mImageElement);
       target->AddEventListener(NS_LITERAL_STRING("click"), this, PR_FALSE);
@@ -335,7 +343,7 @@ nsImageDocument::SetScriptGlobalObject(nsIScriptGlobalObject* aScriptGlobalObjec
     }
   }
 
-  return nsHTMLDocument::SetScriptGlobalObject(aScriptGlobalObject);
+  return NS_OK;
 }
 
 
