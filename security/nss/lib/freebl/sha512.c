e60662c0725fb78f4e2971d3f038fa09e60c861c
wtc%netscape.com <wtc%netscape.com>
2003-03-21 15:10:11 +0000

    Resurrected the workaround in revision 1.2.  It may be difficult for some OSF1 users to upgrade their C compiler to the required versions.

--- security/nss/lib/freebl/sha512.c
+++ security/nss/lib/freebl/sha512.c
@@ -32,7 +32,7 @@
  * may use your version of this file under either the MPL or the
  * GPL.
  *
- * $Id: sha512.c,v 1.4 2002/11/20 05:25:58 nelsonb%netscape.com Exp $
+ * $Id: sha512.c,v 1.5 2003/03/21 15:10:11 wtc%netscape.com Exp $
  */
 #include "prcpucfg.h"
 #if defined(_X86_) || defined(SHA_NO_LONG_LONG)
@@ -557,6 +557,48 @@ SHA256_Resurrect(unsigned char *space, void *arg)
 
 /* SHA-384 and SHA-512 constants, K512. */
 static const PRUint64 K512[80] = {
+#if PR_BYTES_PER_LONG == 8
+     0x428a2f98d728ae22UL ,  0x7137449123ef65cdUL , 
+     0xb5c0fbcfec4d3b2fUL ,  0xe9b5dba58189dbbcUL ,
+     0x3956c25bf348b538UL ,  0x59f111f1b605d019UL , 
+     0x923f82a4af194f9bUL ,  0xab1c5ed5da6d8118UL ,
+     0xd807aa98a3030242UL ,  0x12835b0145706fbeUL , 
+     0x243185be4ee4b28cUL ,  0x550c7dc3d5ffb4e2UL ,
+     0x72be5d74f27b896fUL ,  0x80deb1fe3b1696b1UL , 
+     0x9bdc06a725c71235UL ,  0xc19bf174cf692694UL ,
+     0xe49b69c19ef14ad2UL ,  0xefbe4786384f25e3UL , 
+     0x0fc19dc68b8cd5b5UL ,  0x240ca1cc77ac9c65UL ,
+     0x2de92c6f592b0275UL ,  0x4a7484aa6ea6e483UL , 
+     0x5cb0a9dcbd41fbd4UL ,  0x76f988da831153b5UL ,
+     0x983e5152ee66dfabUL ,  0xa831c66d2db43210UL , 
+     0xb00327c898fb213fUL ,  0xbf597fc7beef0ee4UL ,
+     0xc6e00bf33da88fc2UL ,  0xd5a79147930aa725UL , 
+     0x06ca6351e003826fUL ,  0x142929670a0e6e70UL ,
+     0x27b70a8546d22ffcUL ,  0x2e1b21385c26c926UL , 
+     0x4d2c6dfc5ac42aedUL ,  0x53380d139d95b3dfUL ,
+     0x650a73548baf63deUL ,  0x766a0abb3c77b2a8UL , 
+     0x81c2c92e47edaee6UL ,  0x92722c851482353bUL ,
+     0xa2bfe8a14cf10364UL ,  0xa81a664bbc423001UL , 
+     0xc24b8b70d0f89791UL ,  0xc76c51a30654be30UL ,
+     0xd192e819d6ef5218UL ,  0xd69906245565a910UL , 
+     0xf40e35855771202aUL ,  0x106aa07032bbd1b8UL ,
+     0x19a4c116b8d2d0c8UL ,  0x1e376c085141ab53UL , 
+     0x2748774cdf8eeb99UL ,  0x34b0bcb5e19b48a8UL ,
+     0x391c0cb3c5c95a63UL ,  0x4ed8aa4ae3418acbUL , 
+     0x5b9cca4f7763e373UL ,  0x682e6ff3d6b2b8a3UL ,
+     0x748f82ee5defb2fcUL ,  0x78a5636f43172f60UL , 
+     0x84c87814a1f0ab72UL ,  0x8cc702081a6439ecUL ,
+     0x90befffa23631e28UL ,  0xa4506cebde82bde9UL , 
+     0xbef9a3f7b2c67915UL ,  0xc67178f2e372532bUL ,
+     0xca273eceea26619cUL ,  0xd186b8c721c0c207UL , 
+     0xeada7dd6cde0eb1eUL ,  0xf57d4f7fee6ed178UL ,
+     0x06f067aa72176fbaUL ,  0x0a637dc5a2c898a6UL , 
+     0x113f9804bef90daeUL ,  0x1b710b35131c471bUL ,
+     0x28db77f523047d84UL ,  0x32caab7b40c72493UL , 
+     0x3c9ebe0a15c9bebcUL ,  0x431d67c49c100d4cUL ,
+     0x4cc5d4becb3e42b6UL ,  0x597f299cfc657e2aUL , 
+     0x5fcb6fab3ad6faecUL ,  0x6c44198c4a475817UL 
+#else
     ULLC(428a2f98,d728ae22), ULLC(71374491,23ef65cd), 
     ULLC(b5c0fbcf,ec4d3b2f), ULLC(e9b5dba5,8189dbbc),
     ULLC(3956c25b,f348b538), ULLC(59f111f1,b605d019), 
@@ -597,6 +639,7 @@ static const PRUint64 K512[80] = {
     ULLC(3c9ebe0a,15c9bebc), ULLC(431d67c4,9c100d4c),
     ULLC(4cc5d4be,cb3e42b6), ULLC(597f299c,fc657e2a), 
     ULLC(5fcb6fab,3ad6faec), ULLC(6c44198c,4a475817)
+#endif
 };
 
 struct SHA512ContextStr {
@@ -613,10 +656,17 @@ struct SHA512ContextStr {
 
 /* SHA-512 initial hash values */
 static const PRUint64 H512[8] = {
+#if PR_BYTES_PER_LONG == 8
+     0x6a09e667f3bcc908UL ,  0xbb67ae8584caa73bUL , 
+     0x3c6ef372fe94f82bUL ,  0xa54ff53a5f1d36f1UL , 
+     0x510e527fade682d1UL ,  0x9b05688c2b3e6c1fUL , 
+     0x1f83d9abfb41bd6bUL ,  0x5be0cd19137e2179UL 
+#else
     ULLC(6a09e667,f3bcc908), ULLC(bb67ae85,84caa73b), 
     ULLC(3c6ef372,fe94f82b), ULLC(a54ff53a,5f1d36f1), 
     ULLC(510e527f,ade682d1), ULLC(9b05688c,2b3e6c1f), 
     ULLC(1f83d9ab,fb41bd6b), ULLC(5be0cd19,137e2179)
+#endif
 };
 
 
@@ -1124,10 +1174,17 @@ SHA512_Resurrect(unsigned char *space, void *arg)
 
 /* SHA-384 initial hash values */
 static const PRUint64 H384[8] = {
+#if PR_BYTES_PER_LONG == 8
+     0xcbbb9d5dc1059ed8UL ,  0x629a292a367cd507UL , 
+     0x9159015a3070dd17UL ,  0x152fecd8f70e5939UL , 
+     0x67332667ffc00b31UL ,  0x8eb44a8768581511UL , 
+     0xdb0c2e0d64f98fa7UL ,  0x47b5481dbefa4fa4UL 
+#else
     ULLC(cbbb9d5d,c1059ed8), ULLC(629a292a,367cd507), 
     ULLC(9159015a,3070dd17), ULLC(152fecd8,f70e5939), 
     ULLC(67332667,ffc00b31), ULLC(8eb44a87,68581511), 
     ULLC(db0c2e0d,64f98fa7), ULLC(47b5481d,befa4fa4)
+#endif
 };
 
 SHA384Context *
