d88650e5f128f7af341698044a612783a9a184bb
sspitzer%netscape.com <sspitzer%netscape.com>
2003-03-22 00:08:56 +0000

    back out that last change from 3/20/2003 it was not part of bug #195287 (as the checkin comment claimed) it was something from neil that I had in my tree that I didn't want to check in.

--- mailnews/base/resources/content/mailWindowOverlay.js
+++ mailnews/base/resources/content/mailWindowOverlay.js
@@ -164,8 +164,7 @@ function view_init()
   {
       var message_menuitem_hidden = message_menuitem.getAttribute("hidden");
       if(message_menuitem_hidden != "true"){
-          message_menuitem.setAttribute('checked', !IsMessagePaneCollapsed());
-          message_menuitem.setAttribute('disabled', gAccountCentralLoaded);
+          message_menuitem.setAttribute('checked',!IsThreadAndMessagePaneSplitterCollapsed());
       }
   }
   var threadColumn = document.getElementById('ThreadColumnHeader');
@@ -1067,8 +1066,12 @@ function MsgOpenNewWindowForFolder(uri, key)
     // will return the message that is highlighted.
     uriToOpen = GetSelectedFolderURI();
 
-  if (uriToOpen)
-    window.openDialog("chrome://messenger/content/", "_blank", "chrome,all,dialog=no", uriToOpen, keyToSelect);
+  if (uriToOpen) {
+   // get the messenger window open service and ask it to open a new window for us
+   var mailWindowService = Components.classes["@mozilla.org/messenger/windowservice;1"].getService(Components.interfaces.nsIMessengerWindowService);
+   if (mailWindowService)
+     mailWindowService.openMessengerWindowWithUri("mail:3pane", uriToOpen, keyToSelect);
+  }
 }
 
 // passing in the view, so this will work for search and the thread pane
