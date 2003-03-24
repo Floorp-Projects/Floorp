0354e102b12a5c3546de24824947e9cd56bb56b3
pinkerton%netscape.com <pinkerton%netscape.com>
2003-03-24 03:49:02 +0000

    from dts

---
+++ camino/src/extensions/ImageAndTextCell.h
@@ -0,0 +1,20 @@
+//
+//  ImageAndTextCell.h
+//
+//  Copyright (c) 2001-2002, Apple. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ImageAndTextCell : NSTextFieldCell {
+@private
+    NSImage	*image;
+}
+
+- (void)setImage:(NSImage *)anImage;
+- (NSImage *)image;
+
+- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView;
+- (NSSize)cellSize;
+
+@end
