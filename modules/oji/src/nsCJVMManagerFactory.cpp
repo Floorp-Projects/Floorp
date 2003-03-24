07283b7c36a939b2f8adc400d3fee8e57925039a
louie.zhao%sun.com <louie.zhao%sun.com>
2003-03-24 06:25:06 +0000

    OJI part of bug 60304 "Basic Auth doesn't work for Java" r = peterlubczynski; sr = darin patch = joshua.xia@sun.com

--- modules/oji/src/nsCJVMManagerFactory.cpp
+++ modules/oji/src/nsCJVMManagerFactory.cpp
@@ -38,6 +38,7 @@
 
 #include "nsIModule.h"
 #include "nsIGenericFactory.h"
+#include "nsJVMAuthTools.h"
 #include "nsJVMManager.h"
 
 /*
@@ -53,6 +54,12 @@ static const nsModuleComponentInfo components[] =
       NS_JVMMANAGER_CID,  
       "@mozilla.org/oji/jvm-mgr;1", 
       nsJVMManager::Create
+    },
+
+    { "JVM Authentication Service", 
+      NS_JVMAUTHTOOLS_CID,  
+      "@mozilla.org/oji/jvm-auth-tools;1", 
+      nsJVMAuthTools::Create
     },
 };
 
