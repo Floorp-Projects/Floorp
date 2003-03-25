e122991476444b8905645184d7fd50a3d5b0f99b
seawood@netscape.com <seawood@netscape.com>
2003-03-25 06:46:49 +0000

    Fix signature of standalone embedstring version of |Distance|. Bug #198276 r=jag sr=dbaron

--- xpcom/string/embed/nsEmbedString.cpp
+++ xpcom/string/embed/nsEmbedString.cpp
@@ -490,13 +490,13 @@ Distance_Impl( const nsReadingIterator<CharT>& aStart,
   }
 
 NS_COM
-size_t Distance( const nsAString::const_iterator& aStart, const nsAString::const_iterator& aEnd )
+size_t Distance( const nsReadingIterator<PRUnichar>& aStart, const nsReadingIterator<PRUnichar>& aEnd )
   {
     return Distance_Impl(aStart, aEnd);
   }
 
 NS_COM
-size_t Distance( const nsACString::const_iterator& aStart, const nsACString::const_iterator& aEnd )
+size_t Distance( const nsReadingIterator<char>& aStart, const nsReadingIterator<char>& aEnd )
   {
     return Distance_Impl(aStart, aEnd);
   }
