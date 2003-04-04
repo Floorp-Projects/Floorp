dcaf858b0d4ab19cd81d57a316efdd3db2fd0a50
alecf%netscape.com <alecf%netscape.com>
2003-04-04 02:19:25 +0000

    fix for bug 200524, use arenas to allocate pref names r=dveditz, sr=jag

--- modules/libpref/src/prefapi.cpp
+++ modules/libpref/src/prefapi.cpp
@@ -41,6 +41,9 @@
 #include "jsapi.h"
 #include "nsCRT.h"
 
+#define PL_ARENA_CONST_ALIGN_MASK 3
+#include "plarena.h"
+
 #if defined(XP_MAC)
   #include <stat.h>
 #else
@@ -86,7 +89,7 @@ clearPrefEntry(PLDHashTable *table, PLDHashEntryHdr *entry)
         PR_FREEIF(pref->userPref.stringVal);
     }
     // don't need to free this as it's allocated in memory owned by
-    // the global PrefNameBuffer
+    // gPrefNameArena
     pref->key = nsnull;
     memset(entry, 0, table->entrySize);
 }
@@ -127,6 +130,7 @@ global_resolve(JSContext *cx, JSObject *obj, jsval id)
 JSContext *       gMochaContext = NULL;
 PRBool              gErrorOpeningUserPrefs = PR_FALSE;
 PLDHashTable        gHashTable = { nsnull };
+static PLArenaPool  gPrefNameArena;
 PRBool              gDirty = PR_FALSE;
 
 static JSRuntime *       gMochaTaskState = NULL;
@@ -171,16 +175,14 @@ static PLDHashTableOps     pref_HashTableOps = {
     nsnull,
 };
     
-class PrefNameBuffer;
-
 // PR_ALIGN_OF_WORD is only defined on some platforms.  ALIGN_OF_WORD has
 // already been defined to PR_ALIGN_OF_WORD everywhere
 #ifndef PR_ALIGN_OF_WORD
 #define PR_ALIGN_OF_WORD PR_ALIGN_OF_POINTER
 #endif
 
-// making PrefNameBuffer exactly 8k for nice allocation
-#define PREFNAME_BUFFER_LEN (8192 - (sizeof(PrefNameBuffer*) + sizeof(char*)))
+// making PrefName arena 8k for nice allocation
+#define PREFNAME_ARENA_SIZE 8192
 
 #define WORD_ALIGN_MASK (PR_ALIGN_OF_WORD - 1)
 
@@ -189,84 +191,16 @@ class PrefNameBuffer;
 #error "PR_ALIGN_OF_WORD must be a power of 2!"
 #endif
 
-class PrefNameBuffer {
- public:
-    PrefNameBuffer(PrefNameBuffer* aNext) : mNext(aNext), mNextFree(0)
-        {}
-    static const char *StrDup(const char*);
-    static void FreeAllBuffers();
-
- private:
-    char *Alloc(PRInt32 len);
-
-    static PrefNameBuffer *gRoot;
-
-    // member variables
-    class PrefNameBuffer* mNext;
-    PRUint32 mNextFree;
-    char mBuf[PREFNAME_BUFFER_LEN];
-};
-
-PrefNameBuffer *PrefNameBuffer::gRoot = nsnull;
-
-char*
-PrefNameBuffer::Alloc(PRInt32 len)
-{
-    NS_ASSERTION(this == gRoot, "Can only allocate on the root!\n");
-    NS_ASSERTION(len < PREFNAME_BUFFER_LEN, "Can only allocate short strings\n");
-
-    // check for space in the current buffer
-    if ((mNextFree + len) > PREFNAME_BUFFER_LEN) {
-        // allocate and update the root
-        gRoot = new PrefNameBuffer(this);
-        return gRoot->Alloc(len);
-    }
-
-    // ok, we have space.
-    char *result = &mBuf[mNextFree];
-    
-    mNextFree += len;
-
-    // now align the next free allocation
-    mNextFree = (mNextFree + WORD_ALIGN_MASK) & ~WORD_ALIGN_MASK;
-
-    return result;
-}
-
 // equivalent to strdup() - does no error checking,
 // we're assuming we're only called with a valid pointer
-const char *
-PrefNameBuffer::StrDup(const char *str)
+static char *ArenaStrDup(const char* str, PLArenaPool* aArena)
 {
-    if (!gRoot) {
-        gRoot = new PrefNameBuffer(nsnull);
-        
-        // ack, no memory
-        if (!gRoot)
-            return nsnull;
-    }
-
-    // extra byte for null-termination
-    PRInt32 len = strlen(str) + 1;
-    
-    char *buf = gRoot->Alloc(len);
-
-    memcpy(buf, str, len);
-    
-    return buf;
-}
-
-void
-PrefNameBuffer::FreeAllBuffers()
-{
-    PrefNameBuffer *curr = gRoot;
-    PrefNameBuffer *next;
-    while (curr) {
-        next = curr->mNext;
-        delete curr;
-        curr = next;
-    }
-    gRoot = nsnull;
+    void* mem;
+    PRUint32 len = strlen(str);
+    PL_ARENA_ALLOCATE(mem, aArena, len+1);
+    if (mem)
+        memcpy(mem, str, len+1);
+    return NS_STATIC_CAST(char*, mem);
 }
 
 /*---------------------------------------------------------------------------*/
@@ -306,6 +240,9 @@ PRBool PREF_Init(const char *filename)
         if (!PL_DHashTableInit(&gHashTable, &pref_HashTableOps, nsnull,
                                sizeof(PrefHashEntry), 1024))
             gHashTable.ops = nsnull;
+        
+        PL_INIT_ARENA_POOL(&gPrefNameArena, "PrefNameArena",
+                           PREFNAME_ARENA_SIZE);
     }
         
     if (!gMochaTaskState)
@@ -419,10 +356,10 @@ void PREF_CleanupPrefs()
     if (gHashTable.ops) {
         PL_DHashTableFinish(&gHashTable);
         gHashTable.ops = nsnull;
+        PL_FinishArenaPool(&gPrefNameArena);
     }
 
-    PrefNameBuffer::FreeAllBuffers();
-    
+
     if (gSavedLine)
         free(gSavedLine);
     gSavedLine = NULL;
@@ -998,7 +935,7 @@ PrefResult pref_HashPref(const char *key, PrefValue value, PrefType type, PrefAc
         
         // initialize the pref entry
         pref->flags = type;
-        pref->key = PrefNameBuffer::StrDup(key);
+        pref->key = ArenaStrDup(key, &gPrefNameArena);
         pref->defaultPref.intVal = 0;
         pref->userPref.intVal = 0;
         
