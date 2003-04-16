8a3cc05edb98d01e3ad830e3e20bf601058e5dfd
mkaply@us.ibm.com <mkaply@us.ibm.com>
2003-04-16 03:09:19 +0000

    #201807 r=wtc Corrupt databases on GCC because we were opening the file in the wrong mode

--- dbm/src/hash.c
+++ dbm/src/hash.c
@@ -185,7 +185,7 @@ __hash_open(const char *file, int flags, int mode, const HASHINFO *info, int dfl
 
 	if (file) {				 
 
-#if defined(_WIN32) || defined(_WINDOWS) || defined (macintosh)  || defined(XP_OS2_VACPP)
+#if defined(_WIN32) || defined(_WINDOWS) || defined (macintosh)  || defined(XP_OS2)
 		if ((hashp->fp = DBFILE_OPEN(file, flags | O_BINARY, mode)) == -1)
 			RETURN_ERROR(errno, error0);
 #else
