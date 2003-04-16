2fc43892edc5d99ba7ccd186e1849f73692627aa
jaggernaut%netscape.com <jaggernaut%netscape.com>
2003-04-16 02:36:49 +0000

    Bug 197671: make defaults for what page to load on startup, new window, and new tab be what they used to be. Patch by ian@arlen.demon.co.uk, r=/sr=jag

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -198,13 +198,13 @@ pref("browser.tabs.opentabfor.urlbar", false);
 pref("browser.tabs.opentabfor.windowopen", false);
 pref("browser.tabs.opentabfor.bookmarks", false);
 
- // lets new tab/window load something different than first window
- // -1 - use navigator startup preference
- //  0 - loads blank page
- //  1 - loads home page
- //  2 - loads last page visited
- pref("browser.tabs.loadOnNewTab", 0);
- pref("browser.windows.loadOnNewWindow", 0);
+// lets new tab/window load something different than first window
+// -1 - use navigator startup preference
+//  0 - loads blank page
+//  1 - loads home page
+//  2 - loads last page visited
+pref("browser.tabs.loadOnNewTab", 0);
+pref("browser.windows.loadOnNewWindow", -1);
 
 // view source
 pref("view_source.syntax_highlight", true);
