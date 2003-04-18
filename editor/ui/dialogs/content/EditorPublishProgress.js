3121dbe55cb6c63fb71425cf42ac4a567997dfb4
jaggernaut%netscape.com <jaggernaut%netscape.com>
2003-04-18 12:59:52 +0000

    Bug 145243: warning should be presented if user attempts cancellation of publish. r=kaie, sr=hewitt, moa=brade

--- editor/ui/dialogs/content/EditorPublishProgress.js
+++ editor/ui/dialogs/content/EditorPublishProgress.js
@@ -309,6 +309,25 @@ function CheckKeepOpen()
 
 function onClose()
 {
+  if (!gFinished)
+  {
+    const nsIPromptService = Components.interfaces.nsIPromptService;
+    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                                  .getService(nsIPromptService);
+    const buttonFlags = (nsIPromptService.BUTTON_TITLE_IS_STRING *
+                         nsIPromptService.BUTTON_POS_0) +
+                        (nsIPromptService.BUTTON_TITLE_CANCEL *
+                         nsIPromptService.BUTTON_POS_1);
+    var button = promptService.confirmEx(window,
+                                         GetString("CancelPublishTitle"),
+                                         GetString("CancelPublishMessage"),
+                                         buttonFlags,
+                                         GetString("CancelPublishContinue"),
+                                         null, null, null, {});
+    if (button == 0)
+      return false;
+  }
+
   if (gTimerID)
   {
     clearTimeout(gTimerID);
