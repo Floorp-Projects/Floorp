4986e32875c7b25dee41e6f1c38e2153f3c23b80
scott%scott-macgregor.org <scott%scott-macgregor.org>
2003-04-20 20:28:01 +0000

    Bug #179533 --> add vccard library to the static mail build. (not part of the build)

--- mailnews/build/nsMailModule.cpp
+++ mailnews/build/nsMailModule.cpp
@@ -272,6 +272,11 @@
 #include "nsMsgMdnCID.h"
 #include "nsMsgMdnGenerator.h"
 
+///////////////////////////////////////////////////////////////////////////////
+// vcard includes
+///////////////////////////////////////////////////////////////////////////////
+#include "nsMimeContentTypeHandler.h"
+
 ////////////////////////////////////////////////////////////////////////////////
 // mailnews base factories
 ////////////////////////////////////////////////////////////////////////////////
@@ -495,6 +500,44 @@ NS_GENERIC_FACTORY_CONSTRUCTOR(nsMsgMailViewList);
 // mdn factories
 ////////////////////////////////////////////////////////////////////////////////
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsMsgMdnGenerator);
+
+////////////////////////////////////////////////////////////////////////////////
+// vcard factories 
+////////////////////////////////////////////////////////////////////////////////
+
+// XXX this vcard stuff needs cleaned up to use a generic factory constructor
+extern "C" MimeObjectClass *
+MIME_VCardCreateContentTypeHandlerClass(const char *content_type, 
+                                        contentTypeHandlerInitStruct *initStruct);
+
+static NS_IMETHODIMP nsVCardMimeContentTypeHandlerConstructor(nsISupports *aOuter,
+                                         REFNSIID aIID,
+                                         void **aResult)
+{
+  nsresult rv;
+  nsMimeContentTypeHandler *inst = nsnull;
+
+  if (NULL == aResult) 
+  {
+    rv = NS_ERROR_NULL_POINTER;
+    return rv;
+  }
+  *aResult = NULL;
+  if (NULL != aOuter) 
+  {
+    rv = NS_ERROR_NO_AGGREGATION;
+    return rv;
+  }
+  inst = new nsMimeContentTypeHandler("text/x-vcard", &MIME_VCardCreateContentTypeHandlerClass);
+  if (inst == NULL)
+    return NS_ERROR_OUT_OF_MEMORY;
+
+  NS_ADDREF(inst);
+  rv = inst->QueryInterface(aIID,aResult);
+  NS_RELEASE(inst);
+
+  return rv;
+}
 
 // The list of components we register
 static const nsModuleComponentInfo gComponents[] = {
@@ -1026,10 +1069,16 @@ static const nsModuleComponentInfo gComponents[] = {
        NS_MSGMAILVIEWLIST_CONTRACTID, nsMsgMailViewListConstructor },
     
     ////////////////////////////////////////////////////////////////////////////////
-    // mail view components
+    // mdn  components
     ////////////////////////////////////////////////////////////////////////////////
     { "Msg Mdn Generator", NS_MSGMDNGENERATOR_CID,
-      NS_MSGMDNGENERATOR_CONTRACTID, nsMsgMdnGeneratorConstructor }
+      NS_MSGMDNGENERATOR_CONTRACTID, nsMsgMdnGeneratorConstructor },
+    
+    ////////////////////////////////////////////////////////////////////////////////
+    // mdn  components
+    ////////////////////////////////////////////////////////////////////////////////
+    { "MIME VCard Handler", NS_VCARD_CONTENT_TYPE_HANDLER_CID, "@mozilla.org/mimecth;1?type=text/x-vcard",
+       nsVCardMimeContentTypeHandlerConstructor, }
 };
 
 PR_STATIC_CALLBACK(void) nsMailModuleDtor(nsIModule* self)
