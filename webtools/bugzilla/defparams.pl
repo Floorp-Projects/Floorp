86e24733ff01b534a6f406f6729deaeea7be229d
bbaetz%acm.org <bbaetz%acm.org>
2003-04-24 22:41:07 +0000

    Bug 197153 - Add wording schange requested by reviewer which wasn't in the patch on the bug, and so wasn't checked in. r,a=justdave

--- webtools/bugzilla/defparams.pl
+++ webtools/bugzilla/defparams.pl
@@ -100,7 +100,7 @@ sub check_webdotbase {
         if(-e "data/webdot/.htaccess") {
             open HTACCESS, "data/webdot/.htaccess";
             if(! grep(/ \\\.png\$/,<HTACCESS>)) {
-                return "Dependency graph images are not accessible.\nDelete data/webdot/.htaccess and re-run checksetup.pl to rectify.\n";
+                return "Dependency graph images are not accessible.\nAssuming that you have not modified the file, delete data/webdot/.htaccess and re-run checksetup.pl to rectify.\n";
             }
             close HTACCESS;
         }
