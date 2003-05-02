f5377a89694909856e2b4e1756dd5eeaeefd52a2
dbradley@netscape.com <dbradley@netscape.com>
2003-05-02 04:09:12 +0000

    Bug 203093 - Move ActiveXObject --> NSActiveXObject -> GeckActiveXObject. r=adamlock, sr=alecf, a=sspitzer

--- js/src/xpconnect/src/xpcjsruntime.cpp
+++ js/src/xpconnect/src/xpcjsruntime.cpp
@@ -53,7 +53,7 @@ const char* XPCJSRuntime::mStrings[] = {
     "prototype",            // IDX_PROTOTYPE
     "createInstance"        // IDX_CREATE_INSTANCE
 #ifdef XPC_IDISPATCH_SUPPORT
-    , "NSActiveXObject"     // IDX_ACTIVEX_OBJECT
+    , "GeckoActiveXObject"  // IDX_ACTIVEX_OBJECT
     , "COMObject"           // IDX_COMOBJECT
 #endif
 };
