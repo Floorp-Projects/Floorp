c4c7b8a308aae09a169dc9f9bc0b2fcd05a0b069
jshin%mailaps.org <jshin%mailaps.org>
2003-05-15 03:42:50 +0000

    bug 203052. Enable Thai shaping in Mozilla-Xft by adding a thin wrapper (x-thaittf-0 converter) over TIS620-2 converter offered by the CTL module (r=prabhat, sr=rbs)

--- intl/ctl/src/nsCtlLEModule.cpp
+++ intl/ctl/src/nsCtlLEModule.cpp
@@ -34,6 +34,8 @@
 #include "nsICharsetConverterManager.h"
 #include "nsUnicodeToTIS620.h"
 #include "nsUnicodeToSunIndic.h"
+#include "nsUnicodeToThaiTTF.h"
+#include "nsUnicodeToSunIndicTTF.h"
 
 //----------------------------------------------------------------------------
 // Global functions and data [declaration]
@@ -50,12 +52,14 @@ PRInt32 g_LockCount = 0;
 
 NS_CONVERTER_REGISTRY_START
 NS_UCONV_REG_UNREG_ENCODER("tis620-2", NS_UNICODETOTIS620_CID)
+NS_UCONV_REG_UNREG_ENCODER("x-thaittf-0", NS_UNICODETOTHAITTF_CID)
 NS_UCONV_REG_UNREG_ENCODER("x-sun-unicode-india-0", NS_UNICODETOSUNINDIC_CID)
 NS_CONVERTER_REGISTRY_END
 
 NS_IMPL_NSUCONVERTERREGSELF
 
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsUnicodeToTIS620);
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsUnicodeToThaiTTF);
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsUnicodeToSunIndic);
 
 static const nsModuleComponentInfo components[] =
@@ -63,6 +67,10 @@ static const nsModuleComponentInfo components[] =
   { ENCODER_NAME_BASE "tis620-2" , NS_UNICODETOTIS620_CID,
     NS_UNICODEENCODER_CONTRACTID_BASE "tis620-2",
     nsUnicodeToTIS620Constructor,
+    nsUConverterRegSelf, nsUConverterUnregSelf },
+  { ENCODER_NAME_BASE "x-thaittf-0" , NS_UNICODETOTHAITTF_CID,
+    NS_UNICODEENCODER_CONTRACTID_BASE "x-thaittf-0",
+    nsUnicodeToThaiTTFConstructor,
     nsUConverterRegSelf, nsUConverterUnregSelf },
   { ENCODER_NAME_BASE "x-sun-unicode-india-0" , NS_UNICODETOSUNINDIC_CID,
     NS_UNICODEENCODER_CONTRACTID_BASE "x-sun-unicode-india-0",
