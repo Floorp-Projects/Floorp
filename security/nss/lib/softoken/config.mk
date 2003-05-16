71c6479eb3b4261fec32ee35caeca6e71ceeb349
nelsonb@netscape.com <nelsonb@netscape.com>
2003-05-16 20:21:20 +0000

    Remove MOZILLA_BSAFE_BUILD.  Bug 193090.

--- security/nss/lib/softoken/config.mk
+++ security/nss/lib/softoken/config.mk
@@ -63,9 +63,6 @@ EXTRA_SHARED_LIBS += \
 	-lnspr4 \
 	$(NULL)
 else # ! NS_USE_GCC
-ifdef MOZILLA_BSAFE_BUILD
-	EXTRA_LIBS+=$(DIST)/lib/bsafe$(BSAFEVER).lib
-endif
 
 EXTRA_SHARED_LIBS += \
 	$(DIST)/lib/$(NSPR31_LIB_PREFIX)plc4.lib \
@@ -75,10 +72,6 @@ EXTRA_SHARED_LIBS += \
 endif # NS_USE_GCC
 
 else
-
-ifdef MOZILLA_BSAFE_BUILD
-	EXTRA_LIBS+=$(DIST)/lib/$(LIB_PREFIX)bsafe.$(LIB_SUFFIX)
-endif
 
 # $(PROGRAM) has NO explicit dependencies on $(EXTRA_SHARED_LIBS)
 # $(EXTRA_SHARED_LIBS) come before $(OS_LIBS), except on AIX.
