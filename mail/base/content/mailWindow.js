906da9f0a6f1463de02f61f2c55315d1399d0ae4
scott%scott-macgregor.org <scott%scott-macgregor.org>
2003-06-07 01:49:14 +0000

    synch with the trunk.

--- mail/base/content/mailWindow.js
+++ mail/base/content/mailWindow.js
@@ -577,8 +577,10 @@ function OpenInboxForServer(server)
         var inboxFolder = GetInboxFolder(server);
         SelectFolder(inboxFolder.URI);
 
-        if(CheckOnline())
-            GetMessagesForInboxOnServer(server);
+        if(CheckOnline())	{
+            if (server.type != "imap")
+                GetMessagesForInboxOnServer(server);
+        }
         else {
             var option = PromptGetMessagesOffline();
             if(option == 0) {
