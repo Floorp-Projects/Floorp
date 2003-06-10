b28ce0a5302cc72d3a3e3ad1b7ca7e6f9404b77d
seawood%netscape.com <seawood%netscape.com>
2003-06-10 20:12:33 +0000

    Removing old cfm build files. Use the CFM_LAST_RITES tag to resurrect. r=macdev

--- extensions/universalchardet/macbuild/UniversalchardetPrefix.h
+++
@@ -1,3 +0,0 @@
-
-
-#include "MacSharedPrefix.h"
--- xpcom/typelib/xpidl/macbuild/xpidl_mpw_prefix.h
+++
@@ -1,9 +0,0 @@
-#include <ansi_prefix.mac.h>
-
-#define XP_MAC
-
-#define LIBIDL_LIBRARY_VERSION		"0.6.5"
-#define CPP_PROGRAM					"Foobar"
-
-#define OTUNIXERRORS	1
-#include <OpenTransport.h>
