1b8786a7a7f751688b6a118c6def7ed94aa4950e
seawood@netscape.com <seawood@netscape.com>
2003-06-10 18:41:52 +0000

    Removing cfm specific versions of glib & libidl. Bug #98811 r=macdev

--- xpcom/typelib/xpidl/libidl/libIDL-0.6.5/libIDLConf.sh
+++
@@ -1,6 +0,0 @@
-#
-# Configuration file for using the libIDL library in GNOME applications
-#
-LIBIDL_INCLUDEDIR="-I/usr/lib/glib/include -I/usr/local/include"
-LIBIDL_LIBDIR="-L/usr/local/lib"
-LIBIDL_LIBS="-lIDL -L/usr/lib -lglib"
