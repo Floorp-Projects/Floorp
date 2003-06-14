b266715c3db5f58fb66178b62fe43a2edf5f50de
brendan@mozilla.org <brendan@mozilla.org>
2003-06-14 00:10:09 +0000

    Try to fix HPUX bustage.

--- js/src/js.c
+++ js/src/js.c
@@ -2010,7 +2010,7 @@ env_setProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
         return JS_FALSE;
     name = JS_GetStringBytes(idstr);
     value = JS_GetStringBytes(valstr);
-#ifdef XP_WIN
+#if defined XP_WIN || defined HPUX
     {
         char *waste = JS_smprintf("%s=%s", name, value);
         if (!waste) {
@@ -2018,7 +2018,10 @@ env_setProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
             return JS_FALSE;
         }
         rv = putenv(waste);
+#ifndef HPUX
+        /* XXX hpux9 at least still has the bad old non-copying putenv */
         free(waste);
+#endif
     }
 #else
     rv = setenv(name, value, 1);
