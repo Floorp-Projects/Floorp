78e42e37232a6a9e8d6e7956455e6b702f79e55e
dbaron%dbaron.org <dbaron%dbaron.org>
2003-06-15 00:55:00 +0000

    Fix wrong parent style context warnings (and incorrect reresolution) with HRs or other leaf frames with :before or :after generated content.  Move some code back together that was separated.  Also fix case of float whose placeholder is the child of a block within an inline.  b=209359  r+sr=bzbarsky

--- layout/html/base/src/nsFrame.h
+++ layout/html/base/src/nsFrame.h
@@ -369,14 +369,11 @@ public:
   // Do the work for getting the parent style context frame so that
   // other frame's |GetParentStyleContextFrame| methods can call this
   // method on *another* frame.  (This function handles out-of-flow
-  // frames by using the frame manager's placeholder map and it handles
-  // block-within-inline by calling |GetIBSpecialParent|.)
+  // frames by using the frame manager's placeholder map and it also
+  // handles block-within-inline and generated content wrappers.)
   nsresult DoGetParentStyleContextFrame(nsIPresContext* aPresContext,
                                         nsIFrame**      aProviderFrame,
                                         PRBool*         aIsChild);
-
-  nsresult GetIBSpecialParent(nsIPresContext* aPresContext,
-                              nsIFrame** aSpecialParent);
 
   // Return the previously stored overflow area, if the frame does not 
   // overflow and a creation is not requested it will return nsnull
