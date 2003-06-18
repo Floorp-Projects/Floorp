717316bfb032bc57d5378b026e4ae2c3d1d21012
rogerl%netscape.com <rogerl%netscape.com>
2003-06-18 20:02:09 +0000

    Adding Pandora

---
+++ js2/src/epimetheus.h
@@ -0,0 +1,41 @@
+// -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+//
+// The contents of this file are subject to the Netscape Public
+// License Version 1.1 (the "License"); you may not use this file
+// except in compliance with the License. You may obtain a copy of
+// the License at http://www.mozilla.org/NPL/
+//
+// Software distributed under the License is distributed on an "AS
+// IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// rights and limitations under the License.
+//
+// The Original Code is the JavaScript 2 Prototype.
+//
+// The Initial Developer of the Original Code is Netscape
+// Communications Corporation.  Portions created by Netscape are
+// Copyright (C) 1998 Netscape Communications Corporation. All
+// Rights Reserved.
+
+#ifdef _WIN32
+#include "msvc_pragma.h"
+#endif
+
+#include <algorithm>
+#include <assert.h>
+
+#include "world.h"
+#include "utilities.h"
+#include "js2value.h"
+
+#include <map>
+#include <algorithm>
+#include <list>
+#include <stack>
+
+#include "reader.h"
+#include "parser.h"
+#include "regexp.h"
+#include "js2engine.h"
+#include "bytecodecontainer.h"
+#include "js2metadata.h"
