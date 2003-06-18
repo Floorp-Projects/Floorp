bc9eb3ea3716e575e3202006b44cc8db244e3bdf
jshin%mailaps.org <jshin%mailaps.org>
2003-06-18 09:07:58 +0000

    bug 209513 : be generous to broken servers emiitng 'filename=...' without specifying the dispositin type spec in Content-Disposition header. (r/sr=bzbarsky)

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -1002,11 +1002,12 @@ void nsExternalAppHandler::ExtractSuggestedFileNameFromChannel(nsIChannel* aChan
                                   nsnull, dispToken);
     // RFC 2183, section 2.8 says that an unknown disposition
     // value should be treated as "attachment"
-    if (NS_FAILED(rv) || !dispToken.EqualsIgnoreCase("inline") &&
+    if (NS_FAILED(rv) || 
+        (!dispToken.EqualsIgnoreCase("inline") &&
         // Broken sites just send
         // Content-Disposition: filename="file"
         // without a disposition token... screen those out.
-        !dispToken.EqualsIgnoreCase("filename")) 
+        !dispToken.EqualsIgnoreCase("filename", 8))) 
     {
       // We have a content-disposition of "attachment" or unknown
       mHandlingAttachment = PR_TRUE;
