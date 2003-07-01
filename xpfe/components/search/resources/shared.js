87d22480ed4de190fe8815bd1d542479fd0477d9
cbiesinger@web.de <cbiesinger@web.de>
2003-07-01 20:04:30 +0000

    205440 patch by kd-moz@tprac.de r=shliang sr=jag JavaScript error in internetresults.xul/.js

--- xpfe/components/search/resources/shared.js
+++ xpfe/components/search/resources/shared.js
@@ -1,5 +1,39 @@
-
-
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Robert John Churchill.
+ * Portions created by the Initial Developer are Copyright (C) 2002
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s): 
+ *    Robert John Churchill   <rjc@netscape.com> (Original Author)
+ *    Karsten Duesterloh      <kd-moz@tprac.de>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 function fillContextMenu(name, treeName)
 {
@@ -367,3 +401,48 @@ function setInitialSort(node, sortDirection)
   }
   return(true);
 }
+
+
+function switchTab(aPageIndex)
+{
+  var deck = document.getElementById("advancedDeck");
+  if (!deck)
+  { // get the search-panel deck the hard way 
+    // otherwise the switchTab call from internetresults.xul would fail
+    var oSearchPanel = getNavigatorWindow(false).sidebarObj.panels.get_panel_from_id("urn:sidebar:panel:search");
+    if (!oSearchPanel)
+      return;
+    deck = oSearchPanel.get_iframe().contentDocument.getElementById("advancedDeck")
+  }
+  deck.setAttribute("selectedIndex", aPageIndex);
+}
+
+
+// retrieves the most recent navigator window
+function getNavigatorWindow(aOpenFlag)
+{
+  var navigatorWindow;
+
+  // if this is a browser window, just use it
+  if ("document" in top) {
+    var possibleNavigator = top.document.getElementById("main-window");
+    if (possibleNavigator &&
+        possibleNavigator.getAttribute("windowtype") == "navigator:browser")
+      navigatorWindow = top;
+  }
+
+  // if not, get the most recently used browser window
+  if (!navigatorWindow) {
+    var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                       .getService(Components.interfaces.nsIWindowMediator);
+    navigatorWindow = wm.getMostRecentWindow("navigator:browser");
+  }
+
+  // if no browser window available and it's ok to open a new one, do so
+  if (!navigatorWindow && aOpenFlag) {
+    var navigatorChromeURL = search_getBrowserURL();
+    navigatorWindow = openDialog(navigatorChromeURL, "_blank", "chrome,all,dialog=no");
+  }
+  return navigatorWindow;
+}
+
