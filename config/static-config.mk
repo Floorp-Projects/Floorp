713c038628fd2035604710e503049b90247b2aad
dougt@meer.net <dougt@meer.net>
2003-07-05 19:34:19 +0000

    Adding support for static builds of minimo. r=pavlov. b=211645

--- config/static-config.mk
+++ config/static-config.mk
@@ -102,7 +102,9 @@ endif
 # STATIC_EXTRA_LIBS	+= $(TK_LIBS)
 
 # Some random modules require this
+ifndef MINIMO
 STATIC_EXTRA_LIBS	+= $(MOZ_XPCOM_OBSOLETE_LIBS)
+endif
 
 ifeq ($(OS_ARCH),WINNT)
 STATIC_EXTRA_LIBS += $(call EXPAND_LIBNAME,comctl32 comdlg32 uuid shell32 ole32 oleaut32 version winspool)
