47c65ca78c7edd583974cf543bc52e9337f6a884
mkaply%us.ibm.com <mkaply%us.ibm.com>
2003-07-25 18:48:02 +0000

    #56301 r=mkaply, pedemont, jblanco, sr=kin@netscape.com, a=drivers Turn on spellchecker for extensions=all builds only - not part of any default builds

--- allmakefiles.sh
+++ allmakefiles.sh
@@ -1208,6 +1208,13 @@ for extension in $MOZ_EXTENSIONS; do
             extensions/pref/autoconfig/src/Makefile
             extensions/pref/autoconfig/resources/Makefile
             " ;;
+        spellcheck ) MAKEFILES_extensions="$MAKEFILES_extensions
+            extensions/spellcheck/Makefile
+            extensions/spellcheck/idl/Makefile
+            extensions/spellcheck/myspell/Makefile
+            extensions/spellcheck/myspell/dictionaries/Makefile
+            extensions/spellcheck/src/Makefile
+            " ;;
         transformiix ) MAKEFILES_extensions="$MAKEFILES_extensions
             $MAKEFILES_transformiix"
             ;;
