03a12c4259c194dbde3a72f027ccbc74edb45317
hyatt%mozilla.org <hyatt%mozilla.org>
2003-08-10 08:04:58 +0000

    Initial commit of alternate stylesheet support in Firebird.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -58,6 +58,7 @@ var gRDF = null;
 var gGlobalHistory = null;
 var gURIFixup = null;
 var gReportButton = null;
+var gPageThemeButton = null;
 var gCharsetMenu = null;
 var gLastBrowserCharset = null;
 var gPrevCharset = null;
@@ -123,6 +124,7 @@ function loadEventHandlers(event)
     UpdateBookmarksLastVisitedDate(event);
     checkForDirectoryListing();
     charsetLoadListener(event);
+    updatePageTheme();
   }
 }
 
@@ -323,7 +325,8 @@ function prepareForStartup()
   gNavigatorBundle = document.getElementById("bundle_browser");
   gProgressMeterPanel = document.getElementById("statusbar-progresspanel");
   gBrowser.addEventListener("DOMUpdatePageReport", UpdatePageReport, false);
-
+  gBrowser.addEventListener("DOMUpdatePageTheme", updatePageTheme, false);
+  
   var webNavigation;
   try {
     // Create the browser instance component.
@@ -4313,4 +4316,154 @@ function ShowWindowFromResource( node )
 	}
 }
 #endif
+
+/* Begin Page Theme Functions */
+function getStyleSheetArray(frame)
+{
+  var styleSheets = frame.document.styleSheets;
+  var styleSheetsArray = new Array(styleSheets.length);
+  for (var i = 0; i < styleSheets.length; i++) {
+    styleSheetsArray[i] = styleSheets[i];
+  }
+  return styleSheetsArray;
+}
+
+function getAllStyleSheets(frameset)
+{
+  var styleSheetsArray = getStyleSheetArray(frameset);
+  for (var i = 0; i < frameset.frames.length; i++) {
+    var frameSheets = getAllStyleSheets(frameset.frames[i]);
+    styleSheetsArray = styleSheetsArray.concat(frameSheets);
+  }
+  return styleSheetsArray;
+}
+
+function stylesheetFillPopup(menuPopup)
+{
+  var plainStyle = menuPopup.firstChild;
+  var normalStyle = plainStyle.nextSibling;
+  var sep = normalStyle.nextSibling;
+  while (sep.nextSibling)
+    menuPopup.removeChild(sep.nextSibling);
+
+  var noOptionalStyles = true;
+  var styleSheets = getAllStyleSheets(window._content);
+  var currentStyleSheets = [];
+
+  var allDisabled = true;
+  var haveAltSheets = false;
+  var haveAnyNonAltSheets = false;
+  
+  for (var i = 0; i < styleSheets.length; ++i) {
+    var currentStyleSheet = styleSheets[i];
+    
+    // Skip any stylesheets that don't match the screen media type.
+    var media = currentStyleSheet.media.mediaText.toLowerCase();
+    if (media && (media.indexOf("screen") == -1) && (media.indexOf("all") == -1))
+        continue;
+        
+    if (!currentStyleSheet.disabled)
+        allDisabled = false;
+        
+    if (currentStyleSheet.title) {
+      if (!currentStyleSheet.disabled)
+        noOptionalStyles = false;
+
+      haveAltSheets = true;
+      
+      var lastWithSameTitle = null;
+      if (currentStyleSheet.title in currentStyleSheets)
+        lastWithSameTitle = currentStyleSheets[currentStyleSheet.title];
+
+      if (!lastWithSameTitle) {
+        var menuItem = document.createElement("menuitem");
+        menuItem.setAttribute("type", "radio");
+        menuItem.setAttribute("label", currentStyleSheet.title);
+        menuItem.setAttribute("data", currentStyleSheet.title);
+        menuItem.setAttribute("checked", !currentStyleSheet.disabled);
+        menuPopup.appendChild(menuItem);
+        currentStyleSheets[currentStyleSheet.title] = menuItem;
+      } else {
+        if (currentStyleSheet.disabled)
+          lastWithSameTitle.removeAttribute("checked");
+      }
+    }
+    else 
+       haveAnyNonAltSheets = true;
+  }
+  
+  normalStyle.setAttribute("checked", noOptionalStyles && (!haveAnyNonAltSheets || !allDisabled));
+  plainStyle.setAttribute("checked", allDisabled && haveAnyNonAltSheets);
+  
+  plainStyle.hidden = !haveAnyNonAltSheets;
+  sep.hidden = !haveAltSheets;
+}
+
+function stylesheetInFrame(frame, title) {
+  var docStyleSheets = frame.document.styleSheets;
+
+  for (var i = 0; i < docStyleSheets.length; ++i) {
+    if (docStyleSheets[i].title == title)
+      return true;
+  }
+  return false;
+}
+
+function stylesheetSwitchFrame(frame, title) {
+  var docStyleSheets = frame.document.styleSheets;
+
+  for (var i = 0; i < docStyleSheets.length; ++i) {
+    var docStyleSheet = docStyleSheets[i];
+
+    if (title == "_nostyle")
+      docStyleSheet.disabled = true;
+    else if (docStyleSheet.title)
+      docStyleSheet.disabled = (docStyleSheet.title != title);
+    else if (docStyleSheet.disabled)
+      docStyleSheet.disabled = false;
+  }
+}
+
+function stylesheetSwitchAll(frameset, title) {
+  if (!title || title == "_nostyle" || stylesheetInFrame(frameset, title)) {
+    stylesheetSwitchFrame(frameset, title);
+  }
+  for (var i = 0; i < frameset.frames.length; i++) {
+    stylesheetSwitchAll(frameset.frames[i], title);
+  }
+}
+
+function updatePageTheme(evt)
+{
+  if (!gPageThemeButton)
+    gPageThemeButton = document.getElementById("page-theme-button");
+
+  var hasThemes = false;
+  var stylesheets = window._content.document.styleSheets;
+  for (var i = 0; i < stylesheets.length; ++i) {
+    var currentStyleSheet = stylesheets[i];
+    
+    if (currentStyleSheet.title) {
+        // Skip any stylesheets that don't match the screen media type.
+        var media = currentStyleSheet.media.mediaText.toLowerCase();
+        if (media && (media.indexOf("screen") == -1) && (media.indexOf("all") == -1))
+            continue;
+        hasThemes = true;
+        break;
+    }
+  }
+
+  if (hasThemes) {
+    gPageThemeButton.setAttribute("themes", "true");
+    //if (gPrefService && gPrefService.getBoolPref("privacy.popups.firstTime")) {
+    //  displayPageReportFirstTime();
+
+      // Now set the pref.
+      //gPrefService.setBoolPref("privacy.popups.firstTime", "false");
+   // }
+  }
+  else
+    gPageThemeButton.removeAttribute("themes");
+}
+/* End of the Page Theme functions */
 
