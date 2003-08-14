9c7d150b186a7099e0633e9d74c702c928912a69
mkaply%us.ibm.com <mkaply%us.ibm.com>
2003-08-14 21:53:50 +0000

    Bug #211834 r=bryner, sr=blizzard, a=mkaply Crash in GTK embedding with no parent window

--- embedding/browser/gtk/src/GtkPromptService.cpp
+++ embedding/browser/gtk/src/GtkPromptService.cpp
@@ -279,6 +279,9 @@ GtkPromptService::GetGtkWindowForDOMWindow(nsIDOMWindow* aDOMWindow)
 {
     nsCOMPtr<nsIWindowWatcher> wwatch = do_GetService("@mozilla.org/embedcomp/window-watcher;1");
 
+    if (!aDOMWindow)
+        return NULL;
+
     nsCOMPtr<nsIWebBrowserChrome> chrome;
     wwatch->GetChromeForWindow(aDOMWindow, getter_AddRefs(chrome));
     nsCOMPtr<nsIEmbeddingSiteWindow> siteWindow = do_QueryInterface(chrome);
