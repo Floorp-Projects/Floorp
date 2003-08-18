08a739f6d183178861de18a8619a1fa15f17628b
chanial%noos.fr <chanial%noos.fr>
2003-08-18 10:52:16 +0000

    bug 213228: new toolkit independence bug, part 11: fork remaining files in toolkit/content

---
+++ toolkit/content/charsetOverlay.js
@@ -0,0 +1,285 @@
+function MultiplexHandler(event)
+{
+    var node = event.target;
+    var name = node.getAttribute('name');
+
+    if (name == 'detectorGroup') {
+        SetForcedDetector(true);
+        SelectDetector(event, false);
+    } else if (name == 'charsetGroup') {
+        var charset = node.getAttribute('id');
+        charset = charset.substring('charset.'.length, charset.length)
+        SetForcedCharset(charset);
+        SetDefaultCharacterSet(charset);
+    } else if (name == 'charsetCustomize') {
+        //do nothing - please remove this else statement, once the charset prefs moves to the pref window
+    } else {
+        SetForcedCharset(node.getAttribute('id'));
+        SetDefaultCharacterSet(node.getAttribute('id'));
+    }
+}
+
+function MailMultiplexHandler(event)
+{
+    var node = event.target;
+    var name = node.getAttribute('name');
+
+    if (name == 'detectorGroup') {
+        SelectDetector(event, true);
+    } else if (name == 'charsetGroup') {
+        var charset = node.getAttribute('id');
+        charset = charset.substring('charset.'.length, charset.length)
+        MessengerSetDefaultCharacterSet(charset);
+    } else if (name == 'charsetCustomize') {
+        //do nothing - please remove this else statement, once the charset prefs moves to the pref window
+    } else {
+        MessengerSetDefaultCharacterSet(node.getAttribute('id'));
+    }
+}
+
+function ComposerMultiplexHandler(event)
+{
+    var node = event.target;
+    var name = node.getAttribute('name');
+
+    if (name == 'detectorGroup') {
+        ComposerSelectDetector(event, true);
+    } else if (name == 'charsetGroup') {
+        var charset = node.getAttribute('id');
+        charset = charset.substring('charset.'.length, charset.length)
+        EditorSetDocumentCharacterSet(charset);
+    } else if (name == 'charsetCustomize') {
+        //do nothing - please remove this else statement, once the charset prefs moves to the pref window
+    } else {
+        SetForcedEditorCharset(node.getAttribute('id'));
+    }
+}
+
+function SetDefaultCharacterSet(charset)
+{
+    dump("Charset Overlay menu item pressed: " + charset + "\n");
+    BrowserSetDefaultCharacterSet(charset);
+}
+
+function SelectDetector(event, doReload)
+{
+    dump("Charset Detector menu item pressed: " + event.target.getAttribute('id') + "\n");
+
+    var uri =  event.target.getAttribute("id");
+    var prefvalue = uri.substring('chardet.'.length, uri.length);
+    if ("off" == prefvalue) { // "off" is special value to turn off the detectors
+        prefvalue = "";
+    }
+
+    try {
+        var pref = Components.classes["@mozilla.org/preferences-service;1"]
+                             .getService(Components.interfaces.nsIPrefBranch);
+        var str =  Components.classes["@mozilla.org/supports-string;1"]
+                             .createInstance(Components.interfaces.nsISupportsString);
+
+        str.data = prefvalue;
+        pref.setComplexValue("intl.charset.detector",
+                             Components.interfaces.nsISupportsString, str);
+        if (doReload) window._content.location.reload();
+    }
+    catch (ex) {
+        dump("Failed to set the intl.charset.detector preference.\n");
+    }
+}
+
+function ComposerSelectDetector(event)
+{
+    //dump("Charset Detector menu item pressed: " + event.target.getAttribute('id') + "\n");
+
+    var uri =  event.target.getAttribute("id");
+    var prefvalue = uri.substring('chardet.'.length, uri.length);
+    if ("off" == prefvalue) { // "off" is special value to turn off the detectors
+        prefvalue = "";
+    }
+
+    try {
+        var pref = Components.classes["@mozilla.org/preferences-service;1"]
+                             .getService(Components.interfaces.nsIPrefBranch);
+        var str =  Components.classes["@mozilla.org/supports-string;1"]
+                             .createInstance(Components.interfaces.nsISupportsString);
+
+        str.data = prefvalue;
+        pref.setComplexValue("intl.charset.detector",
+                             Components.interfaces.nsISupportsString, str);
+        EditorLoadUrl(GetDocumentUrl());    
+    }
+    catch (ex) {
+        dump("Failed to set the intl.charset.detector preference.\n");
+    }
+}
+
+function SetForcedDetector(doReload)
+{
+    BrowserSetForcedDetector(doReload);
+}
+
+function SetForcedCharset(charset)
+{
+    BrowserSetForcedCharacterSet(charset);
+}
+
+var gPrevCharset = null;
+function UpdateCurrentCharset()
+{
+    var menuitem = null;
+
+    // exctract the charset from DOM
+    var wnd = document.commandDispatcher.focusedWindow;
+    if ((window == wnd) || (wnd == null)) wnd = window._content;
+    menuitem = document.getElementById('charset.' + wnd.document.characterSet);
+
+    if (menuitem) {
+        // uncheck previously checked item to workaround Mac checkmark problem
+        // bug 98625
+        if (gPrevCharset) {
+            var pref_item = document.getElementById('charset.' + gPrevCharset);
+            if (pref_item)
+              pref_item.setAttribute('checked', 'false');
+        }
+        menuitem.setAttribute('checked', 'true');
+    }
+}
+
+function UpdateCurrentMailCharset()
+{
+    var charset = msgWindow.mailCharacterSet;
+    dump("Update current mail charset: " + charset + " \n");
+
+    var menuitem = document.getElementById('charset.' + charset);
+
+    if (menuitem) {
+        menuitem.setAttribute('checked', 'true');
+    }
+}
+
+function UpdateCharsetDetector()
+{
+    var prefvalue;
+
+    try {
+        var pref = Components.classes["@mozilla.org/preferences-service;1"]
+                             .getService(Components.interfaces.nsIPrefBranch);
+        prefvalue = pref.getComplexValue("intl.charset.detector",
+                                         Components.interfaces.nsIPrefLocalizedString).data;
+    }
+    catch (ex) {
+        prefvalue = "";
+    }
+
+    if (prefvalue == "") prefvalue = "off";
+    dump("intl.charset.detector = "+ prefvalue + "\n");
+
+    prefvalue = 'chardet.' + prefvalue;
+    var menuitem = document.getElementById(prefvalue);
+
+    if (menuitem) {
+        menuitem.setAttribute('checked', 'true');
+    }
+}
+
+function UpdateMenus(event)
+{
+    // use setTimeout workaround to delay checkmark the menu
+    // when onmenucomplete is ready then use it instead of oncreate
+    // see bug 78290 for the detail
+    UpdateCurrentCharset();
+    setTimeout("UpdateCurrentCharset()", 0);
+    UpdateCharsetDetector();
+    setTimeout("UpdateCharsetDetector()", 0);
+}
+
+function CreateMenu(node)
+{
+  var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
+  observerService.notifyObservers(null, "charsetmenu-selected", node);
+}
+
+function UpdateMailMenus(event)
+{
+    // use setTimeout workaround to delay checkmark the menu
+    // when onmenucomplete is ready then use it instead of oncreate
+    // see bug 78290 for the detail
+    UpdateCurrentMailCharset();
+    setTimeout("UpdateCurrentMailCharset()", 0);
+    UpdateCharsetDetector();
+    setTimeout("UpdateCharsetDetector()", 0);
+}
+
+var gCharsetMenu = Components.classes['@mozilla.org/rdf/datasource;1?name=charset-menu'].getService().QueryInterface(Components.interfaces.nsICurrentCharsetListener);
+var gLastBrowserCharset = null;
+
+function charsetLoadListener (event)
+{
+    var charset = window._content.document.characterSet;
+
+    if (charset.length > 0 && (charset != gLastBrowserCharset)) {
+        gCharsetMenu.SetCurrentCharset(charset);
+        gPrevCharset = gLastBrowserCharset;
+        gLastBrowserCharset = charset;
+    }
+}
+
+
+function composercharsetLoadListener (event)
+{
+    var charset = window._content.document.characterSet;
+
+ 
+    if (charset.length > 0 ) {
+       gCharsetMenu.SetCurrentComposerCharset(charset);
+    }
+ }
+
+function SetForcedEditorCharset(charset)
+{
+    if (charset.length > 0 ) {
+       gCharsetMenu.SetCurrentComposerCharset(charset);
+    }
+    EditorSetDocumentCharacterSet(charset);
+}
+
+
+var gLastMailCharset = null;
+
+function mailCharsetLoadListener (event)
+{
+    if (msgWindow) {
+        var charset = msgWindow.mailCharacterSet;
+        if (charset.length > 0 && (charset != gLastMailCharset)) {
+            gCharsetMenu.SetCurrentMailCharset(charset);
+            gLastMailCharset = charset;
+            dump("mailCharsetLoadListener: " + charset + " \n");
+        }
+    }
+}
+
+var wintype = document.firstChild.getAttribute('windowtype');
+if (window && (wintype == "navigator:browser"))
+{
+    var contentArea = window.document.getElementById("appcontent");
+    if (contentArea)
+        contentArea.addEventListener("load", charsetLoadListener, true);
+}
+else
+{
+    var arrayOfStrings = wintype.split(":");
+    if (window && arrayOfStrings[0] == "mail") 
+    {
+        var messageContent = window.document.getElementById("messagepane");
+        if (messageContent)
+            messageContent.addEventListener("load", mailCharsetLoadListener, true);
+    }
+    else
+    if (window && arrayOfStrings[0] == "composer") 
+    {
+        contentArea = window.document.getElementById("appcontent");
+        if (contentArea)
+            contentArea.addEventListener("load", composercharsetLoadListener, true);
+    }
+
+}
---
+++ toolkit/content/selectDialog.js
@@ -0,0 +1,152 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: NPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is 
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1998
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Alec Flett <alecf@netscape.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or 
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the NPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the NPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+var elements = [];
+var numItems;
+var list;
+var param;
+
+function selectDialogOnLoad() {
+  param = window.arguments[0].QueryInterface( Components.interfaces.nsIDialogParamBlock  );
+  if( !param )
+  dump( " error getting param block interface\n" );
+
+  var messageText = param.GetString( 1 );
+  {
+    var messageFragment;
+
+    // Let the caller use "\n" to cause breaks
+    // Translate these into <br> tags
+    var messageParent = (document.getElementById("info.txt"));
+    var done = false;
+    while (!done) {
+      var breakIndex = messageText.indexOf('\n');
+      if (breakIndex == 0) {
+        // Ignore break at the first character
+        messageText = messageText.slice(1);
+        messageFragment = "";
+      } else if (breakIndex > 0) {
+        // The fragment up to the break
+        messageFragment = messageText.slice(0, breakIndex);
+
+        // Chop off fragment we just found from remaining string
+        messageText = messageText.slice(breakIndex+1);
+      } else {
+        // "\n" not found. We're done
+        done = true;
+        messageFragment = messageText;
+      }
+      messageParent.setAttribute("value", messageFragment);
+    }
+  }
+
+  var windowTitle = param.GetString( 0 );
+  window.title = windowTitle;
+
+  list = document.getElementById("list");
+  numItems = param.GetInt( 2 )
+
+  var i;
+  for ( i = 2; i <= numItems+1; i++ ) {
+    var newString = param.GetString( i );
+    if (newString == "") {
+      newString = "<>";
+    }
+    elements[i-2] = AppendStringToListbox(list, newString);
+  }
+  list.selectItem(elements[0]);
+  list.focus();
+
+  // resize the window to the content
+  window.sizeToContent();
+
+  // Move to the right location
+  moveToAlertPosition();
+  param.SetInt(0, 1 );
+  centerWindowOnScreen();
+}
+
+function commonDialogOnOK() {
+  for (var i=0; i<numItems; i++) {
+    if (elements[i] == list.selectedItems[0]) {
+      param.SetInt(2, i );
+      break;
+    }
+  }
+  param.SetInt(0, 0 );
+  return true;
+}
+
+function commonDialogOnCancel() {
+  for (var i=0; i<numItems; i++) {
+    if (elements[i]) {
+      param.SetInt(2, i );
+      break;
+    }
+  }
+  param.SetInt(0, 1 );
+  return true;
+}
+
+function commonDialogOnDoubleClick() {
+  commonDialogOnOK();
+  window.close();
+}
+
+// following routine should really be in a global utilities package
+
+function AppendStringToListbox(tree, string)
+{
+  if (tree)
+  {
+    var listbox = document.getElementById('list');
+
+    var listitem = document.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "listitem");
+    if (listitem)
+    {
+      listitem.setAttribute("label", string);
+      listitem.setAttribute("ondblclick","commonDialogOnDoubleClick()");
+      listbox.appendChild(listitem)
+      var len = Number(tree.getAttribute("length"));
+      if (!len) len = -1;
+      tree.setAttribute("length",len+1);
+      return listitem;
+    }
+  }
+  return null;
+}
