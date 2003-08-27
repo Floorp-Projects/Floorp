a74e13944b240b667b5e995561fdf24b7057ba2d
scott%scott-macgregor.org <scott%scott-macgregor.org>
2003-08-27 04:45:36 +0000

    Bug #217405 --> New helper app dialog causes JS exception for unknown content types
    r=ben

--- toolkit/mozapps/downloads/content/nsHelperAppDlg.js
+++ toolkit/mozapps/downloads/content/nsHelperAppDlg.js
@@ -378,8 +378,15 @@ nsUnknownContentTypeDialog.prototype = {
         
         if (typeString == "") {
           // 2. If there is none, use the extension to identify the file, e.g. "ZIP file"
-          if (mimeInfo.primaryExtension != "")
-            typeString = mimeInfo.primaryExtension.toUpperCase() + " file";
+          var primaryExtension = "";
+          try {
+            primaryExtension = mimeInfo.primaryExtension;
+          }
+          catch (ex) {
+          }
+
+          if (primaryExtension != "")
+            typeString = primaryExtension.toUpperCase() + " file";
           // 3. If we can't even do that, just give up and show the MIME type. 
           else
             typeString = mimeInfo.MIMEType;
