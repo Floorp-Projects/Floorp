233eea5135ebdeb6e8696bfa6e7d6ee6ff01801d
scott%scott-macgregor.org <scott%scott-macgregor.org>
2003-09-04 23:23:14 +0000

    bump trunk UA to 0.3a

--- mail/app/profile/all.js
+++ mail/app/profile/all.js
@@ -49,7 +49,7 @@ pref("general.useragent.locale", "chrome://navigator/locale/navigator.properties
 pref("general.useragent.contentlocale", "chrome://navigator-region/locale/region.properties");
 pref("general.useragent.misc", "rv:1.5b");
 pref("general.useragent.vendor", "Thunderbird");
-pref("general.useragent.vendorSub", "0.2a");
+pref("general.useragent.vendorSub", "0.3a");
 
 pref("general.startup.browser",             false);
 pref("general.startup.mail",                false);
