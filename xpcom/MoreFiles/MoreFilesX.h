ebee086a83b3d9fa4a1f5aa819cc9e18baa8b668
darin@meer.net <darin@meer.net>
2003-09-09 00:13:29 +0000

    fixes bug 215089 "update version of MoreFilesX to 1.0.1" r=ccarlen sr=bryner

--- xpcom/MoreFiles/MoreFilesX.h
+++ xpcom/MoreFiles/MoreFilesX.h
@@ -4,17 +4,44 @@
 	Contains:	A collection of useful high-level File Manager routines
 				which use the HFS Plus APIs wherever possible.
 
-	Version:	MoreFilesX 1.0
+	Version:	MoreFilesX 1.0.1
 
 	Copyright:	© 1992-2002 by Apple Computer, Inc., all rights reserved.
 
-	You may incorporate this sample code into your applications without
-	restriction, though the sample code has been provided "AS IS" and the
-	responsibility for its operation is 100% yours.	 However, what you are
-	not permitted to do is to redistribute the source as "DSC Sample Code"
-	after having made changes. If you're going to re-distribute the source,
-	we require that you make it clear in the source that the code was
-	descended from Apple Sample Code, but that you've made changes.
+	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc.
+				("Apple") in consideration of your agreement to the following terms, and your
+				use, installation, modification or redistribution of this Apple software
+				constitutes acceptance of these terms.  If you do not agree with these terms,
+				please do not use, install, modify or redistribute this Apple software.
+
+				In consideration of your agreement to abide by the following terms, and subject
+				to these terms, Apple grants you a personal, non-exclusive license, under Apple’s
+				copyrights in this original Apple software (the "Apple Software"), to use,
+				reproduce, modify and redistribute the Apple Software, with or without
+				modifications, in source and/or binary forms; provided that if you redistribute
+				the Apple Software in its entirety and without modifications, you must retain
+				this notice and the following text and disclaimers in all such redistributions of
+				the Apple Software.  Neither the name, trademarks, service marks or logos of
+				Apple Computer, Inc. may be used to endorse or promote products derived from the
+				Apple Software without specific prior written permission from Apple.  Except as
+				expressly stated in this notice, no other rights or licenses, express or implied,
+				are granted by Apple herein, including but not limited to any patent rights that
+				may be infringed by your derivative works or by other works in which the Apple
+				Software may be incorporated.
+
+				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO
+				WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED
+				WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+				PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE OR IN
+				COMBINATION WITH YOUR PRODUCTS.
+
+				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR
+				CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+				GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+				ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION
+				OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER UNDER THEORY OF CONTRACT, TORT
+				(INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN
+				ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 	File Ownership:
 
@@ -32,6 +59,8 @@
 
 	Change History (most recent first):
 
+		 <3>	 4/19/02	JL		[2853905]  Fixed #if test around header includes.
+		 <2>	 4/19/02	JL		[2853901]  Updated standard disclaimer.
 		 <1>	 1/25/02	JL		MoreFilesX 1.0
 	
 	Notes:
@@ -58,20 +87,12 @@
 #ifndef __MOREFILESX__
 #define __MOREFILESX__
 
-#ifndef __MACTYPES__
-#include <MacTypes.h>
-#endif
-
-#ifndef __FINDER__
-#include <Finder.h>
-#endif
-
-#ifndef __FILES__
-#include <Files.h>
-#endif
-
-#ifndef __TEXTCOMMON__
-#include <TextCommon.h>
+#ifndef __CARBON__
+	#if defined(__MACH__)
+		#include <Carbon/Carbon.h>
+	#else
+		#include <Carbon.h>
+	#endif
 #endif
 
 #if PRAGMA_ONCE
