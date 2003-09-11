8b5794a66c7f627857b2b0029f2b83783c85ae5d
wtc%netscape.com <wtc%netscape.com>
2003-09-11 00:04:38 +0000

    Bugzilla bug 208971: remove obsolete Mac CFM build files from NSS.

--- security/nss/macbuild/NSSCommon.h
+++
@@ -1,6 +0,0 @@
-/* Defines common to all versions of NSS */
-
-#define NSPR20 1
-#define MP_API_COMPATIBLE 1
-#define NSS_3_4_CODE 1 /* Remove this when we start building NSS 4.0 by default */
-#define SHLIB_SUFFIX "shlb"
