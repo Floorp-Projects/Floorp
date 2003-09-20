c1499f4a0885b86f99163ecc5a7b835716ca7873
wtc%netscape.com <wtc%netscape.com>
2003-09-20 00:27:30 +0000

    Bugzilla bug 219756: fixed a CERTCertificate reference leak.

--- security/jss/org/mozilla/jss/CryptoManager.c
+++ security/jss/org/mozilla/jss/CryptoManager.c
@@ -181,6 +181,7 @@ int ConfigureOCSP(
                         result = SECFailure;
                         goto loser;
 	               }
+                        CERT_DestroyCertificate(cert);
 	}
         status =
             CERT_SetOCSPDefaultResponder(   certdb,
