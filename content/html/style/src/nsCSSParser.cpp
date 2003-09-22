6a67774fa3cc098f05b40972378f1bdb976b0bfa
dbaron@dbaron.org <dbaron@dbaron.org>
2003-09-22 23:52:16 +0000

    Fix --disable-xul bustage.  b=219058

--- content/html/style/src/nsCSSParser.cpp
+++ content/html/style/src/nsCSSParser.cpp
@@ -2212,8 +2212,11 @@ void CSSParserImpl::ParsePseudoSelector(PRInt32&  aDataMask,
     // various -moz-* pseudo-elements) must have |parsingPseudoElement|
     // set.
     if (!parsingPseudoElement &&
-        !nsCSSPseudoElements::IsCSS2PseudoElement(pseudo) &&
-        !IsTreePseudoElement(pseudo)) {
+        !nsCSSPseudoElements::IsCSS2PseudoElement(pseudo)
+#ifdef MOZ_XUL
+        && !IsTreePseudoElement(pseudo)
+#endif
+        ) {
       REPORT_UNEXPECTED_TOKEN(NS_LITERAL_STRING("This pseudo-element must use the \"::\" form: "));
       UngetToken();
       aParsingStatus = SELECTOR_PARSING_STOPPED_ERROR;
