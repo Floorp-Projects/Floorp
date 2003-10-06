d15e9d5a22a6ed09f87e71fefb1fa3d8ec26c624
bzbarsky%mit.edu <bzbarsky%mit.edu>
2003-10-06 02:16:20 +0000

    Fix debug DumpRegressionData method to not crash on null uris for things like mBinding, mBackgroundImage, etc

--- content/base/src/nsStyleContext.cpp
+++ content/base/src/nsStyleContext.cpp
@@ -538,7 +538,11 @@ nsStyleContext::Mark()
 class URICString : public nsCAutoString {
 public:
   URICString(nsIURI* aURI) {
-    aURI->GetSpec(*this);
+    if (aURI) {
+      aURI->GetSpec(*this);
+    } else {
+      Assign("[no background image]");
+    }
   }
 
   URICString& operator=(const URICString& aOther) {
