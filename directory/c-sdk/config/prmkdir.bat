ffa0613f0baccecffe2fb2e8c53df9e7b9e1f868
mcs%netscape.com <mcs%netscape.com>
2003-10-10 15:03:52 +0000

    Fix bug # 205419 - Relicense LDAP C SDK (directory/c-sdk).   Replace existing licenses with a triple license (NPL/GPL/LGPL or     MPL/GPL/LGPL).   Add licenses to files that had no license.   Remove two unused files.

--- directory/c-sdk/config/prmkdir.bat
+++ directory/c-sdk/config/prmkdir.bat
@@ -1,18 +1,38 @@
 REM
+REM ***** BEGIN LICENSE BLOCK *****
+REM Version: NPL 1.1/GPL 2.0/LGPL 2.1
+REM 
 REM The contents of this file are subject to the Netscape Public License
-REM Version 1.1 (the "NPL"); you may not use this file except in
-REM compliance with the NPL.  You may obtain a copy of the NPL at
+REM Version 1.1 (the "License"); you may not use this file except in
+REM compliance with the License. You may obtain a copy of the License at
 REM http://www.mozilla.org/NPL/
 REM 
-REM Software distributed under the NPL is distributed on an "AS IS" basis,
-REM WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+REM Software distributed under the License is distributed on an "AS IS" basis,
+REM WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 REM for the specific language governing rights and limitations under the
-REM NPL.
+REM License.
 REM 
-REM The Initial Developer of this code under the NPL is Netscape
-REM Communications Corporation.  Portions created by Netscape are
-REM Copyright (C) 1998 Netscape Communications Corporation.  All Rights
-REM Reserved.
-REM
+REM The Original Code is mozilla.org code.
+REM 
+REM The Initial Developer of the Original Code is
+REM Netscape Communications Corporation.
+REM Portions created by the Initial Developer are Copyright (C) 1998
+REM the Initial Developer. All Rights Reserved.
+REM 
+REM Contributor(s):
+REM 
+REM Alternatively, the contents of this file may be used under the terms of
+REM either of the GNU General Public License Version 2 or later (the "GPL"),
+REM or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+REM in which case the provisions of the GPL or the LGPL are applicable instead
+REM of those above. If you wish to allow use of your version of this file only
+REM under the terms of either the GPL or the LGPL, and not to allow others to
+REM use your version of this file under the terms of the NPL, indicate your
+REM decision by deleting the provisions above and replace them with the notice
+REM and other provisions required by the GPL or the LGPL. If you do not delete
+REM the provisions above, a recipient may use your version of this file under
+REM the terms of any one of the NPL, the GPL or the LGPL.
+REM 
+REM ***** END LICENSE BLOCK ***** 
 
 mkdir %1
