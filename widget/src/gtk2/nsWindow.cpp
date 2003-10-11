ab5812dc0363d0fe2c1c5bd9ea2bc349d429e593
darin@meer.net <darin@meer.net>
2003-10-11 23:38:38 +0000

    fixes bug 216800 "drag-n-drop to a GTK2 mozilla build often fails" patch=glen@imodulo.com r+sr=blizzard

--- widget/src/gtk2/nsWindow.cpp
+++ widget/src/gtk2/nsWindow.cpp
@@ -1841,7 +1841,10 @@ nsWindow::OnDragDropEvent(GtkWidget *aWidget,
 
     // clear any drag leave timer that might be pending so that it
     // doesn't get processed when we actually go out to get data.
-    mDragLeaveTimer = 0;
+    if (mDragLeaveTimer) {
+        mDragLeaveTimer->Cancel();
+        mDragLeaveTimer = 0;
+    }
 
     // set the last window to this
     mLastDragMotionWindow = innerMostWidget;
