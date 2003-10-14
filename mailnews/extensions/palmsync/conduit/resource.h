7e58ad3b4218848dc0e7871796a5e04308fa2d7f
bienvenu%nventure.com <bienvenu%nventure.com>
2003-10-14 17:19:55 +0000

    more palm sync ui, allow configuration of conduit through hot sync conduit ui 221413 patch by kteuscher@myrealbox.com, r/sr=bienvenu

---
+++ mailnews/extensions/palmsync/conduit/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+//
+#define IDS_CONDUIT_NAME                1
+#define IDB_SYNC                        103
+#define IDB_DONOTHING                   104
+#define IDD_CONDUIT_ACTION              399
+#define IDC_ACTIONGROUPBOXTEXT          403
+#define IDC_RADIO_SYNC                  404
+#define IDC_RADIO_DONOTHING             405
+#define IDC_MAKEDEFAULT                 411
+#define IDC_SYNC                        1000
+#define IDC_DONOTHING                   1001
+#define IDC_STATIC_PERMANENT            1002
+#define IDC_STATIC_TEMPORARY            1003
+#define IDC_CURRENT_SETTINGS_GROUP      1004
+#define IDS_CONNECTION                  3311
+#define IDS_CURRENT_SETTINGS_GROUP      8004
+#define IDS_SYNC_FILES                  8005
+#define IDS_DO_NOTHING                  8006
+#define IDS_SYNC_ACTION_TEXT            8007
+
