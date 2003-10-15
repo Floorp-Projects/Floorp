cdd10143f2bf26d8d9307f91c7f89cc3fb99e856
mcs%netscape.com <mcs%netscape.com>
2003-10-15 17:26:10 +0000

    Additional fix for bug # 205419 - Relicense LDAP C SDK (directory/c-sdk).   Replace NPL/GPL/LGPL licenses with MPL/GPL/LGPL.

--- directory/c-sdk/aclocal.m4
+++ directory/c-sdk/aclocal.m4
@@ -1,11 +1,11 @@
 dnl
 dnl ***** BEGIN LICENSE BLOCK *****
-dnl Version: NPL 1.1/GPL 2.0/LGPL 2.1
+dnl Version: MPL 1.1/GPL 2.0/LGPL 2.1
 dnl 
-dnl The contents of this file are subject to the Netscape Public License
-dnl Version 1.1 (the "License"); you may not use this file except in
-dnl compliance with the License. You may obtain a copy of the License at
-dnl http://www.mozilla.org/NPL/
+dnl The contents of this file are subject to the Mozilla Public License Version 
+dnl 1.1 (the "License"); you may not use this file except in compliance with 
+dnl the License. You may obtain a copy of the License at 
+dnl http://www.mozilla.org/MPL/
 dnl 
 dnl Software distributed under the License is distributed on an "AS IS" basis,
 dnl WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
@@ -28,11 +28,11 @@ dnl or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
 dnl in which case the provisions of the GPL or the LGPL are applicable instead
 dnl of those above. If you wish to allow use of your version of this file only
 dnl under the terms of either the GPL or the LGPL, and not to allow others to
-dnl use your version of this file under the terms of the NPL, indicate your
+dnl use your version of this file under the terms of the MPL, indicate your
 dnl decision by deleting the provisions above and replace them with the notice
 dnl and other provisions required by the GPL or the LGPL. If you do not delete
 dnl the provisions above, a recipient may use your version of this file under
-dnl the terms of any one of the NPL, the GPL or the LGPL.
+dnl the terms of any one of the MPL, the GPL or the LGPL.
 dnl 
 dnl ***** END LICENSE BLOCK ***** 
 
--- directory/c-sdk/config/prmkdir.bat
+++ directory/c-sdk/config/prmkdir.bat
@@ -1,11 +1,11 @@
 REM
 REM ***** BEGIN LICENSE BLOCK *****
-REM Version: NPL 1.1/GPL 2.0/LGPL 2.1
+REM Version: MPL 1.1/GPL 2.0/LGPL 2.1
 REM 
-REM The contents of this file are subject to the Netscape Public License
-REM Version 1.1 (the "License"); you may not use this file except in
-REM compliance with the License. You may obtain a copy of the License at
-REM http://www.mozilla.org/NPL/
+REM The contents of this file are subject to the Mozilla Public License Version 
+REM 1.1 (the "License"); you may not use this file except in compliance with 
+REM the License. You may obtain a copy of the License at 
+REM http://www.mozilla.org/MPL/
 REM 
 REM Software distributed under the License is distributed on an "AS IS" basis,
 REM WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
@@ -27,11 +27,11 @@ REM or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
 REM in which case the provisions of the GPL or the LGPL are applicable instead
 REM of those above. If you wish to allow use of your version of this file only
 REM under the terms of either the GPL or the LGPL, and not to allow others to
-REM use your version of this file under the terms of the NPL, indicate your
+REM use your version of this file under the terms of the MPL, indicate your
 REM decision by deleting the provisions above and replace them with the notice
 REM and other provisions required by the GPL or the LGPL. If you do not delete
 REM the provisions above, a recipient may use your version of this file under
-REM the terms of any one of the NPL, the GPL or the LGPL.
+REM the terms of any one of the MPL, the GPL or the LGPL.
 REM 
 REM ***** END LICENSE BLOCK ***** 
 
