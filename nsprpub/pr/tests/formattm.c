9f4b826366df89054bd52b414ead1d1654480090
wchang0222%aol.com <wchang0222%aol.com>
2003-10-24 01:17:42 +0000

    Added a missing printf statement.

--- nsprpub/pr/tests/formattm.c
+++ nsprpub/pr/tests/formattm.c
@@ -51,5 +51,6 @@ int main()
     printf("%s\n", buffer);
     (void)PR_FormatTimeUSEnglish(buffer, sizeof(buffer),
         "%a %b %d %H:%M:%S %Z %Y", &tod);
+    printf("%s\n", buffer);
     return 0;
 }
