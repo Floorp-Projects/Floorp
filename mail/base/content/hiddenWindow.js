4797f90886365e1978a99aa03bd9707318a09dd9
scott%scott-macgregor.org <scott%scott-macgregor.org>
2003-11-03 04:32:44 +0000

    Implement a thunderbird version of hiddenWindow.xul on the MAC. This fixes a huge OSX usability issue where we are DOA when you close all open thunderbird windows. This was contributed by Don Crandall. Many thanks Don!

---
+++ mail/base/content/hiddenWindow.js
@@ -0,0 +1,64 @@
+# -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+# ***** BEGIN LICENSE BLOCK *****
+# Version: NPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Netscape Public License
+# Version 1.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/NPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Mozilla Communicator client code.
+#
+# The Initial Developer of the Original Code is 
+# Netscape Communications Corporation.
+# Portions created by the Initial Developer are Copyright (C) 1998
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Don Crandall (macdoc@interx.net)
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or 
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the NPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the NPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK ***** 
+
+function hiddenWindowStartup()
+{
+  // focus the hidden window
+  window.focus();
+
+  // Disable menus which are not appropriate
+  var disabledItems = ['newNewMsgCmd', 'menu_newFolder', 'newAccountMenuItem', 'menu_close',
+                       'menu_sendunsentmsgs', 'menu_subscribe', 'menu_renameFolder', 'menu_selectAll',
+                       'menu_showMessengerToolbar', 'menu_showSearchToolbar', 'menu_showTaskbar', 'menu_customizeToolbar',
+                       'sortByDateMenuitem', 'sortByFlagMenuitem', 'sortByOrderReceivedMenuitem', 'sortByPriorityMenuitem',
+                       'messagePaneClassic', 'messagePaneWide', 'messagePaneVertical', 'menu_showMessage',
+                       'sortBySenderOrRecipientMenuitem', 'sortBySizeMenuitem', 'sortByStatusMenuitem', 'sortBySubjectMenuitem',
+                       'sortByThreadMenuitem', 'sortByUnreadMenuitem', 'sortByLabelMenuitem', 'sortByJunkStatusMenuitem',
+                       'sortAscending', 'sortDescending', 'viewThreaded', 'viewAll',
+		                   'viewUnread', 'viewLabelMenuItem1', 'viewLabelMenuItem2', 'viewLabelMenuItem3',
+                       'viewLabelMenuItem4', 'viewLabelMenuItem5', 'viewCreateCustomView', 'viewallheaders',
+		                   'viewnormalheaders', 'bodyAllowHTML', 'bodySanitized', 'bodyAsPlaintext',
+                       'viewAttachmentsInlineMenuitem', 'goStartPage', 'newMsgCmd',
+		                   'searchMailCmd', 'searchAddressesCmd', 'filtersCmd', 'junkMailCmd'];
+  for (var id in disabledItems) 
+  {
+    var broadcaster = document.getElementById(disabledItems[id]);
+    if (broadcaster)
+      broadcaster.setAttribute("disabled", "true");
+  }
+}
