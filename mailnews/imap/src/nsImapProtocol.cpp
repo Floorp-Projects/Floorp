8a71fdb9c88cec8c17177c645e07dbc1a7f6aa0a
scott%scott-macgregor.org <scott%scott-macgregor.org>
2003-11-14 05:18:48 +0000

    Bug #225651 --> fix regression in reporting progress in the progressmeter for loading of imap messages.
    r/sr=bienvenu

--- mailnews/imap/src/nsImapProtocol.cpp
+++ mailnews/imap/src/nsImapProtocol.cpp
@@ -8045,7 +8045,7 @@ nsImapMockChannel::OnTransportStatus(nsITransport *transport, nsresult status,
     if (status == nsISocketTransport::STATUS_RECEIVING_FROM ||
         status == nsISocketTransport::STATUS_SENDING_TO)
     {
-      mProgressEventSink->OnProgress(this, nsnull, progress, progressMax);
+      // do nothing....do NOT report socket transport progress bytes either
     }
     else
     {
