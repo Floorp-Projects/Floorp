4aaa6048ab4d64a969eb038c2d2d67b4379df1e4
scott%scott-macgregor.org <scott%scott-macgregor.org>
2003-11-25 02:28:10 +0000

    Bug #226392 -> When displaying a vcard, hide fields in the address book card which are not used by vcard

--- mail/components/addrbook/content/abCardOverlay.js
+++ mail/components/addrbook/content/abCardOverlay.js
@@ -108,6 +108,8 @@ function OnLoadNewCard()
       gOkCallback = window.arguments[0].okCallback;
 
     if ("escapedVCardStr" in window.arguments[0]) {
+	  // hide non vcard values
+	  HideNonVcardFields(); 
       var addressbook = Components.classes["@mozilla.org/addressbook;1"].createInstance(Components.interfaces.nsIAddressBook);
       editCard.card = addressbook.escapedVCardToAbCard(window.arguments[0].escapedVCardStr);
     }
@@ -449,6 +451,18 @@ function GetCardValues(cardproperty, doc)
   }
 }
 
+// when the ab card dialog is being loaded to show a vcard, hide the fields which aren't supported
+// by vcard so the user does not try to edit them.
+function HideNonVcardFields()
+{
+  document.getElementById('nickNameContainer').collapsed = true;
+  document.getElementById('secondaryEmailContainer').collapsed = true;
+  document.getElementById('screenNameContainer').collapsed = true;
+  document.getElementById('homeAddressGroup').collapsed = true;
+  document.getElementById('customFields').collapsed = true;
+
+}
+
 // Move the data from the dialog to the cardproperty to be stored in the database
 function SetCardValues(cardproperty, doc)
 {
@@ -569,7 +583,12 @@ function DisplayNameChanged()
   SetCardDialogTitle(displayName);
 }
 
+
 function SetCardDialogTitle(displayName)
+
 {
+
    top.window.title = displayName ? gAddressBookBundle.getFormattedString(editCard.titleProperty + "WithDisplayName", [displayName]) : gAddressBookBundle.getString(editCard.titleProperty);
+
 }
+
