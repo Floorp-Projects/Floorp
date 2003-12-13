42aa9da4c019284017c9975d1a7d89bdf53d4983
pschwartau%netscape.com <pschwartau%netscape.com>
2003-12-13 23:52:12 +0000

    Correcting an error.

--- js/tests/ecma_3/RegExp/regress-228087.js
+++ js/tests/ecma_3/RegExp/regress-228087.js
@@ -120,7 +120,7 @@ string = 'abccccc{3 }c{ 3}c{3, }c{3 ,}de';
   addThis();
 
   status = inSection(9);
-  pattern = /c{3[.]}/;
+  pattern = /c{3[ ]}/;
   actualmatch = string.match(pattern);
   expectedmatch = Array('c{3 }');
   addThis();
