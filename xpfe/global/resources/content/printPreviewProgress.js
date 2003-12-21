f8716171dea7dce10b468c625bb39b8123170dac
timeless@mozdev.org <timeless@mozdev.org>
2003-12-21 04:04:25 +0000

    Bug 228529 JS Warnings in printing/print preview r=neil sr=bz

--- xpfe/global/resources/content/printPreviewProgress.js
+++ xpfe/global/resources/content/printPreviewProgress.js
@@ -45,12 +45,10 @@ function elipseString(aStr, doFront)
   if (aStr.length > fixedLen) {
     if (doFront) {
       var endStr = aStr.substr(aStr.length-fixedLen, fixedLen);
-      var str = "..." + endStr;
-      return str;
+      return "..." + endStr;
     } else {
       var frontStr = aStr.substr(0, fixedLen);
-      var str = frontStr + "...";
-      return str;
+      return frontStr + "...";
     }
   }
   return aStr;
