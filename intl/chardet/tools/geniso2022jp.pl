ce5088ba8fab3308e406aa20efc2f94854acd1a4
jgmyers%speakeasy.net <jgmyers%speakeasy.net>
2004-01-03 21:45:21 +0000

    Accept halfwidth katakana in iso-2022-jp: bug 172393 r=ftang sr=roc

--- intl/chardet/tools/geniso2022jp.pl
+++ intl/chardet/tools/geniso2022jp.pl
@@ -8,6 +8,7 @@ my(@iso2022jp_cls);
 my(@iso2022jp_st);
 my($iso2022jp_ver);
 
+# 1:ESC 3:'(' 4:'B' 5:'J' 6:'@' 7:'$' 8:'D' 9:'I' 
 @iso2022jp_cls = (
  [ 0x0e , 0x0f , 2 ],
  [ 0x28 , 0x28 , 3 ],
@@ -15,6 +16,8 @@ my($iso2022jp_ver);
  [ 0x4a , 0x4a , 5 ],
  [ 0x40 , 0x40 , 6 ],
  [ 0x24 , 0x24 , 7 ],
+ [ 0x44 , 0x44 , 8 ],
+ [ 0x49 , 0x49 , 9 ],
  [ 0x01 , 0x1a , 0 ],
  [ 0x1c , 0x7f , 0 ],
  [ 0x1b , 0x1b , 1 ],
@@ -24,17 +27,18 @@ my($iso2022jp_ver);
 
 package genverifier;
 @iso2022jp_st = (
-# 0  1  2  3  4  5  6  7
-  0, 3, 1, 0, 0, 0, 0, 0, # Start State - 0
-  1, 1, 1, 1, 1, 1, 1, 1, # Error State - 1
-  2, 2, 2, 2, 2, 2, 2, 2, # ItsMe State - 2
-  1, 1, 1, 5, 1, 1, 1, 4, # got ESC
-  1, 1, 1, 1, 2, 1, 2, 1, # got ESC $
-  1, 1, 1, 1, 2, 2, 1, 1, # got ESC (
+# 0  1  2  3  4  5  6  7  8  9
+  0, 3, 1, 0, 0, 0, 0, 0, 0, 0, # Start State - 0
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, # Error State - 1
+  2, 2, 2, 2, 2, 2, 2, 2, 2, 2, # ItsMe State - 2
+  1, 1, 1, 5, 1, 1, 1, 4, 1, 1, # got ESC
+  1, 1, 1, 6, 2, 1, 2, 1, 1, 1, # got ESC $
+  1, 1, 1, 1, 2, 2, 1, 1, 1, 2, # got ESC (
+  1, 1, 1, 1, 1, 1, 1, 1, 2, 1, # got ESC $ (
 );
 
 $iso2022jp_ver = genverifier::GenVerifier("ISO2022JP", "ISO-2022-JP", 
-      \@iso2022jp_cls, 8, \@iso2022jp_st);
+      \@iso2022jp_cls, 10, \@iso2022jp_st);
 print $iso2022jp_ver;
 
 
