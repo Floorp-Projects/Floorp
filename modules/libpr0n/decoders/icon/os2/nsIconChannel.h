971c3396b608ba131aa86787d8d364661ba84e56
mkaply@us.ibm.com <mkaply@us.ibm.com>
2004-01-08 23:35:07 +0000

    #223128 r=biesinger, sr=blizzard (platform specific) OS/2 only - don't call streamlistener methods directly

--- modules/libpr0n/decoders/icon/os2/nsIconChannel.h
+++ modules/libpr0n/decoders/icon/os2/nsIconChannel.h
@@ -16,7 +16,7 @@
  * Portions created by Brian Ryner are Copyright (C) 2000 Brian Ryner.
  * All Rights Reserved.
  *
- * Contributor(s):
+ * Contributor(s): 
  *  Scott MacGregor <mscott@netscape.com>
  *  IBM Corp.
  */
@@ -31,15 +31,19 @@
 #include "nsIInterfaceRequestor.h"
 #include "nsIInterfaceRequestorUtils.h"
 #include "nsIURI.h"
+#include "nsIInputStreamPump.h"
+#include "nsIStreamListener.h"
 
 class nsIFile;
 
-class nsIconChannel : public nsIChannel
+class nsIconChannel : public nsIChannel, public nsIStreamListener
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIREQUEST
   NS_DECL_NSICHANNEL
+  NS_DECL_NSIREQUESTOBSERVER
+  NS_DECL_NSISTREAMLISTENER
 
   nsIconChannel();
   virtual ~nsIconChannel();
@@ -49,14 +53,16 @@ public:
 protected:
   nsCOMPtr<nsIURI> mUrl;
   nsCOMPtr<nsIURI> mOriginalURI;
-  PRUint32         mLoadAttributes;
   PRInt32          mContentLength;
   nsCOMPtr<nsILoadGroup> mLoadGroup;
   nsCOMPtr<nsIInterfaceRequestor> mCallbacks;
-  nsCOMPtr<nsISupports>  mOwner;
-  nsresult mStatus;
+  nsCOMPtr<nsISupports>  mOwner; 
 
-nsresult ExtractIconInfoFromUrl(nsIFile ** aLocalFile, PRUint32 * aDesiredImageSize, nsACString &aContentType, nsACString &aFileExtension);
+  nsCOMPtr<nsIInputStreamPump> mPump;
+  nsCOMPtr<nsIStreamListener>  mListener;
+
+  nsresult ExtractIconInfoFromUrl(nsIFile ** aLocalFile, PRUint32 * aDesiredImageSize, nsACString &aContentType, nsACString &aFileExtension);
+  nsresult MakeInputStream(nsIInputStream** _retval, PRBool nonBlocking);
 };
 
 #define ID_ICO_FILE    5800
