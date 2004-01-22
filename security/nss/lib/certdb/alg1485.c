61d56ab306153b49a8fe67d6e538536b29fb14e4
nelsonb%netscape.com <nelsonb%netscape.com>
2004-01-22 23:45:30 +0000

    When a name attribute's value exceeds the maximum allowed length, display a truncated version of it, followed by ellipsis.  Bug 220855. r=wtc

--- security/nss/lib/certdb/alg1485.c
+++ security/nss/lib/certdb/alg1485.c
@@ -695,11 +695,19 @@ AppendAVA(stringBuf *bufp, CERTAVA *ava)
     }
 
     /* Check value length */
-    if (avaValue->len > maxLen) {
-	if (unknownTag) PR_smprintf_free(unknownTag);
-	SECITEM_FreeItem(avaValue, PR_TRUE);
-	PORT_SetError(SEC_ERROR_INVALID_AVA);
-	return SECFailure;
+    if (avaValue->len > maxLen + 3) {  /* must be room for "..." */
+	/* avaValue is a UTF8 string, freshly allocated and returned to us 
+	** by CERT_DecodeAVAValue just above, so we can modify it here.
+	** See if we're in the middle of a multi-byte UTF8 character.
+	*/
+	while (((avaValue->data[maxLen] & 0xc0) == 0x80) && maxLen > 0) {
+	   maxLen--;
+	}
+	/* add elipsis to signify truncation. */
+	avaValue->data[maxLen++] = '.'; 
+	avaValue->data[maxLen++] = '.';
+	avaValue->data[maxLen++] = '.';
+	avaValue->len = maxLen;
     }
 
     len = PORT_Strlen(tagName);
