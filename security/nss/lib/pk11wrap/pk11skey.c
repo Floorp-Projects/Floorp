ce75f8d873f6fca592affff9ae8239714c1674a1
nelsonb%netscape.com <nelsonb%netscape.com>
2004-01-22 02:19:42 +0000

    Move an extern function declaration to the header file where it belongs. Bug 229212. r=relyea.

--- security/nss/lib/pk11wrap/pk11skey.c
+++ security/nss/lib/pk11wrap/pk11skey.c
@@ -72,10 +72,6 @@ static PK11SymKey *pk11_DeriveWithTemplate(PK11SymKey *baseKey,
 static PRBool pk11_FindAttrInTemplate(CK_ATTRIBUTE *attr,
 	unsigned int numAttrs, CK_ATTRIBUTE_TYPE target);
 
-#ifdef NSS_ENABLE_ECC
-extern int SECKEY_ECParamsToKeySize(const SECItem *params);
-#endif /* NSS_ENABLE_ECC */
-
 /*
  * strip leading zero's from key material
  */
