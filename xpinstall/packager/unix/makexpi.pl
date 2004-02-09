51aea1c159d72d112d5126466dc300754e3e8c57
cbiesinger%web.de <cbiesinger%web.de>
2004-02-09 18:39:25 +0000

    Bug 217531 mozilla .xpi files' install.js has filesize too big patch by Andrew Schultz <ajschult@eos.ncsu.edu> r=bsmedberg sr=leaf

--- xpinstall/packager/unix/makexpi.pl
+++ xpinstall/packager/unix/makexpi.pl
@@ -66,6 +66,17 @@ sub RecursiveStrip
     system("strip @libraryList") if (defined(@libraryList));
 }
 
+sub MakeJsFile
+{
+  my($componentName) = @_;
+
+  # Make .js file
+  if(system("perl makejs.pl $componentName.jst $inDefaultVersion $inStagePath/$componentName install.js") != 0)
+  {
+    exit(1);
+  }
+}
+
 sub find_libraries
 {
     /\.so$/ && push @libraryList, $File::Find::name;
@@ -85,17 +96,12 @@ if($#ARGV < 2)
 $inComponentName  = $ARGV[0];
 $inStagePath      = $ARGV[1];
 $inDestPath       = $ARGV[2];
+$inDefaultVersion = $ARGV[3];
 
 # check for existence of staging component path
 if(!(-e "$inStagePath/$inComponentName"))
 {
   die "invalid path: $inStagePath/$inComponentName\n";
-}
-
-# check for existence of .js script
-if(!(-e "$inComponentName.js"))
-{
-  die "missing .js script: $inComponentName.js\n";
 }
 
 # delete component .xpi file
@@ -135,7 +141,9 @@ RecursiveStrip(cwd());
 system("zip -r -y $inDestPath/$inComponentName.xpi *");
 chdir("$saveCwdir");
 
-system("cp $inComponentName.js install.js");
+# Make .js file
+MakeJsFile($inComponentName);
+
 system("zip -g $inDestPath/$inComponentName.xpi install.js");
 
 # delete install.js
