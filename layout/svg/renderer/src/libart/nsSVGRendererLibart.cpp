9e593a8d7ab7de140982aaed2fc58e0f15d026e2
bzbarsky@mit.edu <bzbarsky@mit.edu>
2004-02-19 06:33:51 +0000

    Fix libart bustage from bug 231995 by making the code more sane.  r=darin

--- layout/svg/renderer/src/libart/nsSVGRendererLibart.cpp
+++ layout/svg/renderer/src/libart/nsSVGRendererLibart.cpp
@@ -120,15 +120,15 @@ NS_NewSVGRendererLibart(nsISVGRenderer** aResult)
 
     if (!nsSVGLibartFreetype::Init()) {
       nsCOMPtr<nsIPromptService> prompter(do_GetService(prompt_service.get()));
-      nsXPIDLString title(NS_LITERAL_STRING("Freetype error"));
-      nsXPIDLString msg(NS_LITERAL_STRING("The Libart/Freetype SVG rendering engine could not initialize the freetype library. Please go to http://www.mozilla.org/projects/fonts/unix/enabling_truetype.html and follow steps 2-7."));
-      prompter->Alert(nsnull, title, msg);
+      NS_NAMED_LITERAL_STRING(title, "Freetype error");
+      NS_NAMED_LITERAL_STRING(msg, "The Libart/Freetype SVG rendering engine could not initialize the freetype library. Please go to http://www.mozilla.org/projects/fonts/unix/enabling_truetype.html and follow steps 2-7.");
+      prompter->Alert(nsnull, title.get(), msg.get());
     }
     else if (!nsSVGLibartFreetype::HasSuitableFonts()) {
       nsCOMPtr<nsIPromptService> prompter(do_GetService(prompt_service.get()));
-      nsXPIDLString title(NS_LITERAL_STRING("Font Configuration Error"));
-      nsXPIDLString msg(NS_LITERAL_STRING("The Libart/Freetype SVG rendering engine can't find any truetype fonts on your system. Please go to http://www.mozilla.org/projects/fonts/unix/enabling_truetype.html and follow steps 2-7."));
-      prompter->Alert(nsnull, title, msg);
+      NS_NAMED_LITERAL_STRING(title, "Font Configuration Error");
+      NS_NAMED_LITERAL_STRING(msg, "The Libart/Freetype SVG rendering engine can't find any truetype fonts on your system. Please go to http://www.mozilla.org/projects/fonts/unix/enabling_truetype.html and follow steps 2-7.");
+      prompter->Alert(nsnull, title.get(), msg.get());
       nsSVGLibartFreetype::Shutdown();
     }
     else {
