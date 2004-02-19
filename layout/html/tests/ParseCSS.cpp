8ecd5f619a04c6fb38eca07951ed36e1835e9594
timeless@mozdev.org <timeless@mozdev.org>
2004-02-19 09:49:47 +0000

    Bug 231789 Make ParseCSS more tinderbox friendly r=bz sr=dbaron

--- layout/html/tests/ParseCSS.cpp
+++ layout/html/tests/ParseCSS.cpp
@@ -57,47 +57,65 @@
 static NS_DEFINE_CID(kCSSLoaderCID, NS_CSS_LOADER_CID);
 
 static already_AddRefed<nsIURI>
-FileToURI(const char *aFilename)
+FileToURI(const char *aFilename, nsresult *aRv = 0)
 {
-    nsCOMPtr<nsILocalFile> lf(do_CreateInstance(NS_LOCAL_FILE_CONTRACTID));
+    nsCOMPtr<nsILocalFile> lf(do_CreateInstance(NS_LOCAL_FILE_CONTRACTID, aRv));
+    NS_ENSURE_TRUE(lf, nsnull);
     // XXX Handle relative paths somehow.
     lf->InitWithNativePath(nsDependentCString(aFilename));
 
     nsIURI *uri = nsnull;
-    NS_NewFileURI(&uri, lf);
+    nsresult rv = NS_NewFileURI(&uri, lf);
+    if (aRv)
+        *aRv = rv;
     return uri;
 }
 
-static void
+static int
 ParseCSSFile(nsIURI *aSheetURI)
 {
     nsCOMPtr<nsICSSLoader> loader(do_CreateInstance(kCSSLoaderCID));
     nsCOMPtr<nsICSSStyleSheet> sheet;
     loader->LoadAgentSheet(aSheetURI, getter_AddRefs(sheet));
     NS_ASSERTION(sheet, "sheet load failed");
+    /* This can happen if the file can't be found (e.g. you
+     * ask for a relative path and xpcom/io rejects it)
+     */
+    if (!sheet)
+        return -1;
     PRBool complete;
     sheet->GetComplete(complete);
     NS_ASSERTION(complete, "synchronous load did not complete");
+    if (!complete)
+        return -2;
+    return 0;
 }
 
 int main(int argc, char** argv)
 {
-    NS_InitXPCOM2(nsnull, nsnull, nsnull);
+    if (argc < 2) {
+        fprintf(stderr, "%s [FILE]...\n", argv[0]);
+    }
+    nsresult rv = NS_InitXPCOM2(nsnull, nsnull, nsnull);
+    if (NS_FAILED(rv))
+        return (int)rv;
 
+    int res = 0;
     for (int i = 1; i < argc; ++i) {
         const char *filename = argv[i];
 
         printf("\nParsing %s.\n", filename);
 
-        nsCOMPtr<nsIURI> uri = FileToURI(filename);
-        if (!uri) {
+        nsCOMPtr<nsIURI> uri = FileToURI(filename, &rv);
+        if (rv == NS_ERROR_OUT_OF_MEMORY) {
             fprintf(stderr, "Out of memory.\n");
             return 1;
         }
-        ParseCSSFile(uri);
+        if (uri)
+            res = ParseCSSFile(uri);
     }
 
     NS_ShutdownXPCOM(nsnull);
 
-    return 0;
+    return res;
 }
