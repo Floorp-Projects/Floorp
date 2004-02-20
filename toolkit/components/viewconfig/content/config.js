14c74c41d8ff4fef7fb4e24fce9e56a65acddcda
blakeross%telocity.com <blakeross%telocity.com>
2004-02-20 21:09:03 +0000

    Fix 234948: sync our config.js with the trunk's.

--- toolkit/components/viewconfig/content/config.js
+++ toolkit/components/viewconfig/content/config.js
@@ -297,7 +297,8 @@ function fetchPref(prefName, prefIndex)
       case gPrefBranch.PREF_STRING:
         pref.valueCol = gPrefBranch.getComplexValue(prefName, nsISupportsString).data;
         // Try in case it's a localized string (will throw an exception if not)
-        if (pref.lockCol == PREF_IS_DEFAULT_VALUE)
+        if (pref.lockCol == PREF_IS_DEFAULT_VALUE &&
+            /^chrome:\/\/.+\/locale\/.+\.properties/.test(pref.valueCol))
           pref.valueCol = gPrefBranch.getComplexValue(prefName, nsIPrefLocalizedString).data;
         break;
     }
@@ -364,7 +365,7 @@ function onConfigUnload()
 
 function FilterPrefs()
 {
-  var substring = document.getElementById("textbox").value.toString();
+  var substring = document.getElementById("textbox").value.toLowerCase();
   var prefCol = view.selection.currentIndex < 0 ? null : gPrefView[view.selection.currentIndex].prefCol;
   var array = gPrefView;
   gPrefView = gPrefArray;
