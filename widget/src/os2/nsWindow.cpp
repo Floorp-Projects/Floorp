1f08909699af2f8586c61acf7b869cb6b5a3e612
mkaply%us.ibm.com <mkaply%us.ibm.com>
2004-02-24 23:20:12 +0000

    #231839 r=pedemont, sr=blizzard (platform specific code) Add default when there is no scan code so that IMEs work

--- widget/src/os2/nsWindow.cpp
+++ widget/src/os2/nsWindow.cpp
@@ -3728,7 +3728,7 @@ PRUint32 WMChar2KeyCode( MPARAM mp1, MPARAM mp2)
          {
            rc = rc - '0' + NS_VK_0;
          }
-         else {
+         else if (CHAR4FROMMP(mp1) != 0) {
            /* For some characters, map the scan code to the NS_VK value */
            /* This only happens in the char case NOT the VK case! */
            switch (CHAR4FROMMP(mp1)) {
@@ -3812,13 +3812,18 @@ PRUint32 WMChar2KeyCode( MPARAM mp1, MPARAM mp2)
                rc = NS_VK_DIVIDE;
                break;
 
-             case 0x0C:
+            case 0x0C:
                rc = NS_VK_SUBTRACT; /* THIS IS WRONG! */
                break;
 
              default:
                break;
            }
+         } else {
+           /* If we got here, it is an IME input */
+           /* Just set rc to 0 so that it is ignored */
+           /* Other values cause bad things to happen */
+           rc = 0;
          }
       }
    }
