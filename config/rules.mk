d36d42f46fe23404f89ace1daed01a3bab9c0d87
cls@seawood.org <cls@seawood.org>
2004-02-26 07:34:27 +0000

    Use LD instead of CC when linking C libs on HPUX

--- config/rules.mk
+++ config/rules.mk
@@ -471,10 +471,10 @@ ifeq ($(GNU_CC)$(GNU_CXX),)
 EXTRA_DSO_LDOPTS += -Wl,-Bsymbolic
 ifneq ($(HAS_EXTRAEXPORTS),1)
 MKSHLIB  += -Wl,+eNSGetModule -Wl,+eerrno
-MKCSHLIB += -Wl,+eNSGetModule -Wl,+eerrno
+MKCSHLIB += +eNSGetModule +eerrno
 ifneq ($(OS_TEST),ia64)
 MKSHLIB  += -Wl,+e_shlInit
-MKCSHLIB += -Wl,+e_shlInit
+MKCSHLIB += +e_shlInit
 endif # !ia64
 endif # !HAS_EXTRAEXPORTS
 endif # non-gnu compilers
