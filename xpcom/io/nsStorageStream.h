210feddcf85b751fd5df4c76e084a4f2b5a32401
timeless@mozdev.org <timeless@mozdev.org>
2004-03-02 01:11:06 +0000

    Bug 235744 Expose StorageStream class to Javascript patch by nsayer@kfu.com r=darin sr=darin

--- xpcom/io/nsStorageStream.h
+++ xpcom/io/nsStorageStream.h
@@ -34,6 +34,17 @@
 #include "nsIOutputStream.h"
 #include "nsMemory.h"
 
+#define NS_STORAGESTREAM_CID                       \
+{ /* 669a9795-6ff7-4ed4-9150-c34ce2971b63 */       \
+  0x669a9795,                                      \
+  0x6ff7,                                          \
+  0x4ed4,                                          \
+  {0x91, 0x50, 0xc3, 0x4c, 0xe2, 0x97, 0x1b, 0x63} \
+}
+
+#define NS_STORAGESTREAM_CONTRACTID "@mozilla.org/storagestream;1"
+#define NS_STORAGESTREAM_CLASSNAME "Storage Stream"
+
 class nsSegmentedBuffer;
 
 class nsStorageStream : public nsIStorageStream,
