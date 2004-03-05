bd67aaaf11f311af7fff2b5ba6066ffdcdc08ebe
ben%bengoodger.com <ben%bengoodger.com>
2004-03-05 00:46:54 +0000

    (215094) Move migration-started observer notification to the correct place to fix automigration

--- browser/components/migration/src/nsSeamonkeyProfileMigrator.cpp
+++ browser/components/migration/src/nsSeamonkeyProfileMigrator.cpp
@@ -88,12 +88,12 @@ nsSeamonkeyProfileMigrator::Migrate(PRUint32 aItems, PRBool aReplace, const PRUn
 {
   nsresult rv = NS_OK;
 
-  NOTIFY_OBSERVERS(MIGRATION_STARTED, nsnull);
-
   if (!mTargetProfile) 
     GetTargetProfile(aProfile, aReplace);
   if (!mSourceProfile)
     GetSourceProfile(aProfile);
+
+  NOTIFY_OBSERVERS(MIGRATION_STARTED, nsnull);
 
   COPY_DATA(CopyPreferences,  aReplace, nsIBrowserProfileMigrator::SETTINGS);
   COPY_DATA(CopyCookies,      aReplace, nsIBrowserProfileMigrator::COOKIES);
