40aaba9057c779fea760a9187ccc3e509a99f204
cls%seawood.org <cls%seawood.org>
2004-03-10 07:46:38 +0000

    Update openbsd support for openbsd >= 3.4 Thanks to Peter Str��mberg <wilfried@spamcop.net> for pulling these patches out of openbsd's ports tree. Bug #236599 r+sr=dmose

--- directory/c-sdk/ldap/libraries/liblber/lber-int.h
+++ directory/c-sdk/ldap/libraries/liblber/lber-int.h
@@ -251,15 +251,15 @@ void ber_err_print( char *data );
 	     (((_l)&0xff0000)>>8) + (((_l)&0xff000000)>>24))
 #define LBER_NTOHL(_l) LBER_HTONL(_l)
 
-#elif !defined(__alpha) || defined(VMS)
+#elif (!defined(__alpha) || defined(VMS)) && !defined(__amd64__)
 
 #define LBER_HTONL( l )	htonl( l )
 #define LBER_NTOHL( l )	ntohl( l )
 
-#else /* __alpha */
+#else /* __alpha || __amd64__ */
 /*
- * htonl and ntohl on the DEC Alpha under OSF 1 seem to only swap the
- * lower-order 32-bits of a (64-bit) long, so we define correct versions
+ * htonl and ntohl on the 64 bit UNIX platforms only swap the lower-order
+ * 32-bits of a (64-bit) long, so we define correct versions
  * here.
  */
 #define LBER_HTONL( l )	(((long)htonl( (l) & 0x00000000FFFFFFFF )) << 32 \
@@ -267,7 +267,7 @@ void ber_err_print( char *data );
 
 #define LBER_NTOHL( l )	(((long)ntohl( (l) & 0x00000000FFFFFFFF )) << 32 \
     			| ntohl( ( (l) & 0xFFFFFFFF00000000 ) >> 32 ))
-#endif /* __alpha */
+#endif /* __alpha || __amd64__ */
 
 
 /* function prototypes */
