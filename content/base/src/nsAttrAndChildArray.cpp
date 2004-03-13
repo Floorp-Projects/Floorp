89e98879e1ae2be2dc606deef8fb18bde0b8ae8c
mconnor@myrealbox.com <mconnor@myrealbox.com>
2004-03-13 01:17:02 +0000

    bug 237262 nsAttrAndChildArray::ReplaceChildAt() should addref before releasing, patch by Wladimir Palant (trev@gtchat.de), r=sicking, sr=bz, a=dbaron

--- content/base/src/nsAttrAndChildArray.cpp
+++ content/base/src/nsAttrAndChildArray.cpp
@@ -167,9 +167,11 @@ nsAttrAndChildArray::ReplaceChildAt(nsIContent* aChild, PRUint32 aPos)
   NS_ASSERTION(aPos < ChildCount(), "out-of-bounds");
   void** pos = mImpl->mBuffer + AttrSlotsSize() + aPos;
   nsIContent* child = NS_STATIC_CAST(nsIContent*, *pos);
-  NS_RELEASE(child);
   *pos = aChild;
+
+  // Make sure to addref first, in case aChild == child
   NS_ADDREF(aChild);
+  NS_RELEASE(child);
 }
 
 PRInt32
