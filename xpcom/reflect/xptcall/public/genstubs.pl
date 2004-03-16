3ff36bd0c7b33a8845e9a808cf5db3a258c49a16
cls@seawood.org <cls@seawood.org>
2004-03-16 08:07:25 +0000

    Fix mingw 3.3.1/3.3.3 bustage. Follow MSVC as much as possible by requiring proper dllimport/dllexport declarations. Remove dllimport/dllexport declarations from all nsXPTCStubBase functions and just export the class itself. Bug #226609 r=shaver/swalker sr=dbaron a=chofmann

--- xpcom/reflect/xptcall/public/genstubs.pl
+++ xpcom/reflect/xptcall/public/genstubs.pl
@@ -40,11 +40,11 @@ print OUTFILE "*  2 is Release\n";
 print OUTFILE "*/\n";
 print OUTFILE "#if !defined(__ia64) || (!defined(__hpux) && !defined(__linux__))\n";
 for($i = 0; $i < $entry_count; $i++) {
-    print OUTFILE "XPTC_EXPORT NS_IMETHOD Stub",$i+3,"();\n";
+    print OUTFILE "NS_IMETHOD Stub",$i+3,"();\n";
 }
 print OUTFILE "#else\n";
 for($i = 0; $i < $entry_count; $i++) {
-    print OUTFILE "XPTC_EXPORT NS_IMETHOD Stub",$i+3,"(PRUint64,\n";
+    print OUTFILE "NS_IMETHOD Stub",$i+3,"(PRUint64,\n";
     print OUTFILE " PRUint64,PRUint64,PRUint64,PRUint64,PRUint64,PRUint64);\n";
 
 }
@@ -53,7 +53,7 @@ print OUTFILE "#endif\n";
 print OUTFILE "\n/* declarations of sentinel stubs */\n";
 
 for($i = 0; $i < $sentinel_count; $i++) {
-    print OUTFILE "XPTC_EXPORT NS_IMETHOD Sentinel",$i,"();\n";
+    print OUTFILE "NS_IMETHOD Sentinel",$i,"();\n";
 }
 close(OUTFILE);
 
