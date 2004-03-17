f040fa2436dcd49254fad867fcf46554f3c392ef
igor%mir2.org <igor%mir2.org>
2004-03-17 18:36:57 +0000

    Added RUnit Software

--- js/rhino/docs/owner.js
+++ js/rhino/docs/owner.js
@@ -1,3 +1,15 @@
-function owner() {
-	return '<a href="mailto:nboyd'+'@'+ 'atg.com">Norris Boyd</a>'
+function owner() 
+{
+	return email("Norris Boyd", "nboyd", "atg.com");
 }
+
+function email(name, prefix, suffix) 
+{
+	return "<a href='mailto:"+prefix+"@"+suffix+"'>"+name+"</a>";
+}
+
+function write_email(name, prefix, suffix) 
+{
+	document.write(email(name, prefix, suffix));
+}
+
