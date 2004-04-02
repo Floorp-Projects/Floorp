adeee71a3e4b3edddcc4370b442c5c94852c4110
bienvenu%nventure.com <bienvenu%nventure.com>
2004-04-02 01:07:04 +0000

    fix 239283 reading offline messages in folder that was open when you do an offline sync fails, sr=mscott, a=chofmann

--- mailnews/imap/src/nsImapMailFolder.cpp
+++ mailnews/imap/src/nsImapMailFolder.cpp
@@ -4327,6 +4327,7 @@ nsImapMailFolder::GetMessageSizeFromDB(const char *id, PRBool idIsUid, PRUint32 
   nsresult rv = NS_ERROR_FAILURE;
   NS_ENSURE_ARG(size);
   *size = 0;
+  (void) GetDatabase(nsnull);
   if (id && mDatabase)
   {
     PRUint32 key = atoi(id);
