4f074469038d5f516495ff89c7fcf9ae2466296b
dbaron%dbaron.org <dbaron%dbaron.org>
2004-04-07 23:25:06 +0000

    Add -moz- prefix to :first-node and :last-node.  b=141397  r+sr=bzbarsky  a=chofmann

--- layout/style/nsCSSPseudoClassList.h
+++ layout/style/nsCSSPseudoClassList.h
@@ -70,7 +70,7 @@ CSS_PSEUDO_CLASS(mozDragOver, ":-moz-drag-over")
 CSS_PSEUDO_CLASS(target, ":target")
 
 CSS_PSEUDO_CLASS(firstChild, ":first-child")
-CSS_PSEUDO_CLASS(firstNode, ":first-node")
+CSS_PSEUDO_CLASS(firstNode, ":-moz-first-node")
 CSS_PSEUDO_CLASS(lastChild, ":last-child")
-CSS_PSEUDO_CLASS(lastNode, ":last-node")
+CSS_PSEUDO_CLASS(lastNode, ":-moz-last-node")
 
