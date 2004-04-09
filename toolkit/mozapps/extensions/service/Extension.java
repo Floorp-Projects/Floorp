cae940de58f3119620d221231eafdbe789ac5737
ben@bengoodger.com <ben@bengoodger.com>
2004-04-09 21:50:26 +0000

    add extension return type for update checking web service

---
+++ toolkit/mozapps/extensions/service/Extension.java
@@ -0,0 +1,15 @@
+package org.mozilla.update.extensions;
+
+public class Extension
+{
+  public Extension()
+  {
+  }
+
+  public int row;
+  public String id;
+  public String version;
+  public String name;
+  public String xpiURL;
+}
+
