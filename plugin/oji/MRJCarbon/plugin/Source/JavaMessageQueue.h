dfb29e9236aff2f210574878e1f30f43b8dd9f6c
gerv%gerv.net <gerv%gerv.net>
2004-04-17 18:33:16 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license.

--- plugin/oji/MRJCarbon/plugin/Source/JavaMessageQueue.h
+++ plugin/oji/MRJCarbon/plugin/Source/JavaMessageQueue.h
@@ -1,4 +1,4 @@
-/* ----- BEGIN LICENSE BLOCK -----
+/* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
  * The contents of this file are subject to the Mozilla Public License Version
@@ -18,11 +18,12 @@
  * Portions created by the Initial Developer are Copyright (C) 2001
  * the Initial Developer. All Rights Reserved.
  *
- * Contributor(s):  Patrick C. Beard <beard@netscape.com>
+ * Contributor(s):
+ *   Patrick C. Beard <beard@netscape.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
@@ -32,7 +33,7 @@
  * the provisions above, a recipient may use your version of this file under
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
- * ----- END LICENSE BLOCK ----- */
+ * ***** END LICENSE BLOCK ***** */
 
 /*
 	JavaMessageQueue.h
