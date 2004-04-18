c0cd46fc51978ceea0c4cd64023dc77347393da2
gerv@gerv.net <gerv@gerv.net>
2004-04-18 14:21:17 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license.

--- editor/ui/composer.js
+++ editor/ui/composer.js
@@ -20,11 +20,11 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- * Benjamin Smedberg <bsmedberg@covad.net>
+ *   Benjamin Smedberg <bsmedberg@covad.net>
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
--- intl/uconv/ucvko/nsUnicodeToX11Johab.cpp
+++ intl/uconv/ucvko/nsUnicodeToX11Johab.cpp
@@ -1,54 +1,44 @@
 /*
  * @(#)CharToByteX11Johab.java  1.1 99/08/28
  *
- * The contents of this file are subject to the Mozilla Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/MPL/
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
  *
- * The Original Code is CharToByteX11Johab.java
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- * The Initial Developer of the Original Code is Deogtae Kim
- * <dtkim@calab.kaist.ac.kr> (98/05/03) and
- * Based on: Hanterm source code adapted by Jungshik Shin
- * <jshin@pantheon.yale.edu>
+ * The Original Code is CharToByteX11Johab.java.
+ *
+ * The Initial Developer of the Original Code is
+ * Deogtae Kim <dtkim@calab.kaist.ac.kr> (98/05/03) and Based on: Hanterm source code adapted by Jungshik Shin <jshin@pantheon.yale.edu>.
+ * Portions created by the Initial Developer are Copyright (C) 2001
+ * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
  *   Deogtae Kim <dtkim@calab.kaist.ac.kr> (99/08/28)
  *   Jungshik Shin <jshin@pantheon.yale.edu>
  *   Pierre Phaneuf <pp@ludusdesign.com>
  *
- * - Purposes:
- *   1. Enable displaying all 11,172 Modern hangul syllables with Hanterm
- *      johab fonts on Unix
- *   2. Enable displaying some of Unicode 2.0 ancient hangul syllables
- *      with Hanterm johab fonts on Unix
- *   3. Enable displaying all of Unicode 2.0 ancient hangul syllables with
- *      possible future extended Hanterm johab fonts on Unix
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
  *
- * - Installation Instructions:
- *   1. Install Hanterm Johab fonts and a proper font property file to Unix system.
- *      (Confer http://calab.kaist.ac.kr/~dtkim/java/)
- *   2. Make a directory "jdk1.x.x/classes/"
- *   3. Compile this class into "jdk1.x.x/classes/"
- *
- * - Hangule syllable break rule algorithm descriptions:
- *   The algorithm used here is slightly different from that of the Unicode standard 2.0.
- *   The standard saids that a (canonical) hangul syllable block is a sequence of leading
- *   consonants and a sequence of vowels and an optional sequence of trailing
- *   consonants.
- *   I think this unncessarily complicate matters.
- *   So, the algorithm chosen here is:
- *   A (non-canonical) hangul syllable block is the longest sequence comprised of
- *   an optional leading consonant and an optional vowel and an optional trailing consonant.
- *   The skipped leading consonants and vowels are considered as fillers.
- *   The merit of this algorithm is that any sequence of jamos can be composed validly.
- */
+ * ***** END LICENSE BLOCK ***** */
 #include "nsUnicodeToX11Johab.h"
 #include "nsUCvKODll.h"
 
