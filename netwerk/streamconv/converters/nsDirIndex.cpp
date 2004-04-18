8b8dde7d95653a78516e3a5bb1365bc9fc543c43
gerv%gerv.net <gerv%gerv.net>
2004-04-18 22:01:16 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license.

--- netwerk/streamconv/converters/nsDirIndex.cpp
+++ netwerk/streamconv/converters/nsDirIndex.cpp
@@ -1,5 +1,5 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/* ----- BEGIN LICENSE BLOCK -----
+/* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
  * The contents of this file are subject to the Mozilla Public License Version
@@ -14,26 +14,27 @@
  *
  * The Original Code is the directory index parsing code.
  *
- * The Initial Developer of the Original Code is Bradley Baetz.
- * Portions created by Bradley Baetz are
- * Copyright (C) 2001 Bradley Baetz.
- * All Rights Reserved.
+ * The Initial Developer of the Original Code is
+ * Bradley Baetz.
+ * Portions created by the Initial Developer are Copyright (C) 2001
+ * the Initial Developer. All Rights Reserved.
  *
- * Contributor(s): Bradley Baetz <bbaetz@cs.mcgill.ca>
+ * Contributor(s):
+ *   Bradley Baetz <bbaetz@cs.mcgill.ca>
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
  * use your version of this file under the terms of the MPL, indicate your
  * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the LGPL or the GPL. If you do not delete
+ * and other provisions required by the GPL or the LGPL. If you do not delete
  * the provisions above, a recipient may use your version of this file under
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
- * ----- END LICENSE BLOCK ----- */
+ * ***** END LICENSE BLOCK ***** */
 
 #include "nsDirIndex.h"
 #include "nsReadableUtils.h"
