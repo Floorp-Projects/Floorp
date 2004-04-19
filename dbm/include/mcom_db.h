d9f5283be074d14a19a3db2eb51878d7f209bedb
mkaply%us.ibm.com <mkaply%us.ibm.com>
2004-04-19 16:23:39 +0000

    #239663 r=mkaply, sr=blizzard (platform specific) OS/2 only - bring back VACPP support for NSPR and DBM mainly so VACPP can still be used with NSS

--- dbm/include/mcom_db.h
+++ dbm/include/mcom_db.h
@@ -190,7 +190,7 @@
 #define LITTLE_ENDIAN   1234
 #endif
 
-#if defined(_WINDOWS)
+#if defined(_WINDOWS) || defined(XP_OS2_VACPP)
 #ifdef BYTE_ORDER
 #undef BYTE_ORDER
 #endif
@@ -220,6 +220,14 @@
 
 #ifndef XP_OS2 
 #define MAXPATHLEN 	1024               
+#endif
+
+#ifdef XP_OS2_VACPP
+#include <os2.h>
+#define	MAXPATHLEN	CCHMAXPATH
+#define	EPERM		EINVAL
+#define	ENOTDIR		EBADPOS
+#define	S_ISDIR(s)	((s) & S_IFDIR)
 #endif
 
 #define	EFTYPE		EINVAL		/* POSIX 1003.1 format errno. */
