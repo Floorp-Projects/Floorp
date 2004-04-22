e863a0a087f2711386e1e8cf27bf9021c8735cb2
caillon@redhat.com <caillon@redhat.com>
2004-04-22 19:19:11 +0000

    Bug 241247 scroll lock and num lock need keycodes. r+sr=blizzard

--- widget/src/gtk/nsGtkEventHandler.cpp
+++ widget/src/gtk/nsGtkEventHandler.cpp
@@ -159,6 +159,8 @@ struct nsKeyConverter nsKeycodes[] = {
   { NS_VK_DECIMAL,    GDK_KP_Decimal },
   { NS_VK_DIVIDE,     GDK_KP_Divide },
   { NS_VK_RETURN,     GDK_KP_Enter },
+  { NS_VK_NUM_LOCK,   GDK_Num_Lock },
+  { NS_VK_SCROLL_LOCK,GDK_Scroll_Lock },
 
   { NS_VK_COMMA,      GDK_comma },
   { NS_VK_PERIOD,     GDK_period },
