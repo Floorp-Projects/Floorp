8e710656a3f9da8116726e291b598c1c048a2dfa
gerv@gerv.net <gerv@gerv.net>
2004-04-25 21:07:34 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license.

--- tools/memory/bloattable.pl
+++ tools/memory/bloattable.pl
@@ -1,25 +1,42 @@
 #!/usr/bin/perl -w
 #
-# The contents of this file are subject to the Mozilla Public
-# License Version 1.1 (the "License"); you may not use this file
-# except in compliance with the License. You may obtain a copy of
-# the License at http://www.mozilla.org/MPL/
-# 
-# Software distributed under the License is distributed on an "AS
-# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# rights and limitations under the License.
-# 
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
 # The Original Code is Waldemar's Perl Utilities.
-# 
-# The Initial Developer of the Original Code is Netscape Communications
-# Corporation.  Portions created by Netscape Communications Corporation are
-# Copyright (C) 2000 Netscape Communications Corporation.  All
-# Rights Reserved.
-# 
-# Contributor(s):   Waldemar Horwat <waldemar@acm.org>
 #
+# The Initial Developer of the Original Code is
+# Netscape Communications Corporation.
+# Portions created by the Initial Developer are Copyright (C) 2000
+# the Initial Developer. All Rights Reserved.
 #
+# Contributor(s):
+#   Waldemar Horwat <waldemar@acm.org>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
 # bloattable [-debug] [-source] [-byte n|-obj n|-ref n] <file1> <file2> ... <filen> > <html-file>
 #
 # file1, file2, ... filen should be successive BloatView files generated from the same run.
@@ -31,7 +48,6 @@
 #    respectively, and sort by the nth column (n is zero-based, so the first column has n==0).
 #
 # See http://lxr.mozilla.org/mozilla/source/xpcom/doc/MemoryTools.html
-#
 
 use 5.004;
 use strict;
