409ad99e6fb3a3b2b017e3f936cf80913d1a499f
rlk%trfenv.com <rlk%trfenv.com>
2004-04-28 23:25:33 +0000

    Landing Firefox Help 1.1. Permission to checkin from ben.

--- toolkit/components/help/content/contextHelp.js
+++ toolkit/components/help/content/contextHelp.js
@@ -1,68 +1,71 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla Application Suite.
- *
- * The Initial Developer of the Original Code is
- * Ian Oeschger.
- * Portions created by the Initial Developer are Copyright (C) 2003
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *      brantgurganus2001@cherokeescouting.org
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Mozilla Application Suite.
+#
+# The Initial Developer of the Original Code is
+# Ian Oeschger.
+# Portions created by the Initial Developer are Copyright (C) 2003
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#      brantgurganus2001@cherokeescouting.org
+#      Jswalden86@netzero.net
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
 
+#ifndef MOZ_THUNDERBIRD
 const MOZILLA_CONTENT_PACK = "chrome://help/locale/firebirdhelp.rdf";
-// Set the default content pack to the Mozilla content pack. Use the
-// setHelpFileURI function to set this value.
+#else
+const MOZILLA_CONTENT_PACK = "chrome://help/locale/thunderbirdhelp.rdf";
+#endif
+
+# Set the default content pack to the Mozilla content pack. Use the
+# setHelpFileURI function to set this value.
 var helpFileURI = MOZILLA_CONTENT_PACK;
 
-// openHelp - Opens up the Mozilla Help Viewer with the specified
-//    topic and content pack.
-// see http://www.mozilla.org/projects/help-viewer/content_packs.html
+# openHelp - Opens up the Mozilla Help Viewer with the specified
+#    topic and content pack.
+# see http://www.mozilla.org/projects/help-viewer/content_packs.html
 function openHelp(topic, contentPack)
 {
-  //cp is the content pack to use in this function. If the contentPack
-  //parameter was set, we will use that content pack. If not, we will
-  //use the default content pack set by setHelpFileURI().
-  var cp = contentPack || helpFileURI;
+# helpFileURI is the content pack to use in this function. If contentPack is defined,
+# use that and set the helpFileURI to that value so that it will be the default.
+  helpFileURI = contentPack || helpFileURI;
 
-  // Try to find previously opened help.
-  var topWindow = locateHelpWindow(cp);
+# Try to find previously opened help.
+  var topWindow = locateHelpWindow(helpFileURI);
 
   if ( topWindow ) {
-    // Open topic in existing window.
+# Open topic in existing window.
     topWindow.focus();
     topWindow.displayTopic(topic);
   } else {
-    // Open topic in new window.
+# Open topic in new window.
     const params = Components.classes["@mozilla.org/embedcomp/dialogparam;1"]
                              .createInstance(Components.interfaces.nsIDialogParamBlock);
     params.SetNumberStrings(2);
-    params.SetString(0, cp);
+    params.SetString(0, helpFileURI);
     params.SetString(1, topic);
     const ww = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
                          .getService(Components.interfaces.nsIWindowWatcher);
@@ -70,13 +73,13 @@ function openHelp(topic, contentPack)
   }
 }
 
-// setHelpFileURI - Sets the default content pack to use in the Help Viewer
+# setHelpFileURI - Sets the default content pack to use in the Help Viewer
 function setHelpFileURI(rdfURI)
 {
   helpFileURI = rdfURI;
 }
 
-// Locate existing help window for this content pack.
+# Locate existing help window for this content pack.
 function locateHelpWindow(contentPack) {
     const windowManagerInterface = Components
         .classes['@mozilla.org/appshell/window-mediator;1'].getService()
@@ -85,8 +88,8 @@ function locateHelpWindow(contentPack) {
     var topWindow = null;
     var aWindow;
 
-    // Loop through help windows looking for one with selected content
-    // pack.
+# Loop through help windows looking for one with selected content
+# pack.
     while (iterator.hasMoreElements()) {
         aWindow = iterator.getNext();
         if (aWindow.getHelpFileURI() == contentPack) {
--- toolkit/components/help/content/toolbarCustomization.js
+++ toolkit/components/help/content/toolbarCustomization.js
@@ -1,43 +1,43 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Firebird Help code.
- *
- * The Initial Developer of the Original Code is
- * R.J. Keller
- * Portions created by the Initial Developer are Copyright (C) 2003
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
-//CustomizeToolbar() - Enables the Customize Toolbar dialog and sets toolbar
-//	and toolbar button properties approprately.
-//
-//    id - The id of the toolbar to enable customization for.
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Firebird Help code.
+#
+# The Initial Developer of the Original Code is
+# R.J. Keller
+# Portions created by the Initial Developer are Copyright (C) 2003
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+#
+#
+# CustomizeToolbar() - Enables the Customize Toolbar dialog and sets toolbar
+#	and toolbar button properties approprately.
+#
+#    id - The id of the toolbar to enable customization for.
 function CustomizeToolbar(id)
 {
   var customizePopup = document.getElementById("cmd_CustomizeToolbars");
@@ -47,21 +47,21 @@ function CustomizeToolbar(id)
                     "chrome,all,dependent", document.getElementById(id));
 }
 
-//ToolboxCustomizeDone() - Resets the toolbar back to its default state. Reenables
-//	toolbar buttons and the "Customize Toolbar" command.
-//
-//    aToolboxChanged - boolean value of whether or not the toolbar was changed.
+# ToolboxCustomizeDone() - Resets the toolbar back to its default state. Reenables
+#	toolbar buttons and the "Customize Toolbar" command.
+#
+#    aToolboxChanged - boolean value of whether or not the toolbar was changed.
 function ToolboxCustomizeDone(aToolboxChanged)
 {
-  // Update global UI elements that may have been added or removed
+# Update global UI elements that may have been added or removed
   var customizePopup = document.getElementById("cmd_CustomizeToolbars");
   customizePopup.removeAttribute("disabled");
 
-  // make sure our toolbar buttons have the correct enabled state restored to them...
+# make sure our toolbar buttons have the correct enabled state restored to them...
   if (this.UpdateToolbar != undefined)
     UpdateToolbar(focus);
 
-  // Set the Sidebar Toolbar Button's label appropriately.
+# Set the Sidebar Toolbar Button's label appropriately.
   var sidebarButton = document.getElementById("sidebar-button");
   var strBundle = document.getElementById("bundle_help");
   if (document.getElementById("helpsidebar-box").hidden) {
@@ -73,14 +73,14 @@ function ToolboxCustomizeDone(aToolboxChanged)
 
 function UpdateToolbar(caller)
 {
-  //dump("XXX update help-toolbar " + caller + "\n");
+# dump("XXX update help-toolbar " + caller + "\n");
   document.commandDispatcher.updateCommands('help-toolbar');
 
-  //re-enable toolbar customization command
+# re-enable toolbar customization command
   var customizePopup = document.getElementById("cmd_CustomizeToolbars");
   customizePopup.removeAttribute("disabled");
 
-  // hook for extra toolbar items
+# hook for extra toolbar items
   var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
   observerService.notifyObservers(window, "help:updateToolbarItems", null);
 }
