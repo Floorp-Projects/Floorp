41cf9146bfa4b3839180099c8c6f564c99b62b37
peterv@propagandism.org <peterv@propagandism.org>
2004-05-05 13:50:27 +0000

    Fix for bug 235747 (Move parser stuff into mozilla/parser). Removing files from old location, new location is mozilla/parser/expat. r=cls, sr=jst.

--- expat/sample/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set LIB=..\xmlparse\Release;..\lib;%LIB%
-cl /nologo /DXMLTOKAPI=__declspec(dllimport) /DXMLPARSEAPI=__declspec(dllimport) /I..\xmlparse /Fe..\bin\elements elements.c xmlparse.lib
-@echo Run it using: ..\bin\elements ^<..\expat.html
