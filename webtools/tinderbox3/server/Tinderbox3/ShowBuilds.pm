9eb1d71925328ef7b0d34d49da6ea98c9b46e33b
john%johnkeiser.com <john%johnkeiser.com>
2004-05-19 15:49:34 +0000

    Add license

--- webtools/tinderbox3/server/Tinderbox3/ShowBuilds.pm
+++ webtools/tinderbox3/server/Tinderbox3/ShowBuilds.pm
@@ -1,3 +1,27 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Tinderbox 3.
+#
+# The Initial Developer of the Original Code is
+# John Keiser (john@johnkeiser.com).
+# Portions created by the Initial Developer are Copyright (C) 2004
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#
+# ***** END LICENSE BLOCK *****
+
 package Tinderbox3::ShowBuilds;
 
 use strict;
