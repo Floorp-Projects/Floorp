dd9db43d16b51773643f54e68ad2ceb017c7f5cb
bzbarsky%mit.edu <bzbarsky%mit.edu>
2004-05-19 04:22:05 +0000

    Fix permissions check for CSS background images, again.  Bug 243948, patch by mvl@exedo.nl, r+sr=bzbarsky

--- content/html/style/src/nsCSSValue.cpp
+++ content/html/style/src/nsCSSValue.cpp
@@ -376,7 +376,7 @@ nsCSSValue::Image::Image(nsIURI* aURI, const PRUnichar* aString,
     : (PRInt32)nsIRequest::LOAD_BACKGROUND;
 
   if (mURI &&
-      NS_SUCCEEDED(nsContentUtils::CanLoadImage(mURI, nsnull, aDocument))) {
+      nsContentUtils::CanLoadImage(mURI, aDocument, aDocument)) {
     nsContentUtils::LoadImage(mURI, aDocument, nsnull,
                               loadFlag,
                               getter_AddRefs(mRequest));
