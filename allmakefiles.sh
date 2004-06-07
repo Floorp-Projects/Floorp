65e8d2282f9e2175af257b08cbeeeb6855640953
ben@bengoodger.com <ben@bengoodger.com>
2004-06-07 06:00:52 +0000

    back out this change since it was superfluous and seems to have broken a lot of things

--- allmakefiles.sh
+++ allmakefiles.sh
@@ -881,7 +881,6 @@ browser/components/shell/public/Makefile
 browser/installer/Makefile
 browser/installer/unix/Makefile
 browser/installer/windows/Makefile
-browser/base/content/contents-region.rdf
 "
 
 MAKEFILES_xulapp="
