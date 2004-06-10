bb1df186cfa83c0a9b29a16703deaeb246a099a3
jst@mozilla.jstenback.com <jst@mozilla.jstenback.com>
2004-06-10 00:11:38 +0000

    Fixing bug 140690. Patch by m.wargers@hccnet.nl. Making element.innerHTML not do newline conversion. r+sr=jst@mozilla.jstenback.com

--- content/html/content/src/nsGenericHTMLElement.cpp
+++ content/html/content/src/nsGenericHTMLElement.cpp
@@ -859,7 +859,8 @@ nsGenericHTMLElement::GetInnerHTML(nsAString& aInnerHTML)
   NS_ENSURE_TRUE(docEncoder, NS_ERROR_FAILURE);
 
   docEncoder->Init(doc, NS_LITERAL_STRING("text/html"),
-                   nsIDocumentEncoder::OutputEncodeBasicEntities);
+                   nsIDocumentEncoder::OutputEncodeBasicEntities |
+                   nsIDocumentEncoder::OutputLFLineBreak);
 
   nsCOMPtr<nsIDOMRange> range(new nsRange);
   NS_ENSURE_TRUE(range, NS_ERROR_OUT_OF_MEMORY);
