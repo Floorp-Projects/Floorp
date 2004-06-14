b7ba86ffa201486c5b24c91901f523d0133af8a7
bsmedberg@covad.net <bsmedberg@covad.net>
2004-06-14 00:17:48 +0000

    Landing semi-single-profile on the trunk (merge from the AVIARY_1_0_20040515_BRANCH).

---
+++ toolkit/profile/content/createProfileWizard.js
@@ -0,0 +1,224 @@
+/*
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Mozilla Communicator client code, released
+ * March 31, 1998.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation. Portions created by Netscape are
+ * Copyright (C) 1998-1999 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ *   Ben Goodger (30/09/99)
+ *   Brant Gurganus (23/03/03)
+ *   Stefan Borggraefe (17/10/03)
+ *   Benjamin Smedberg <bsmedberg@covad.net> - 8-Apr-2004
+ */ 
+
+const C = Components.classes;
+const I = Components.interfaces;
+
+const ToolkitProfileService = "@mozilla.org/toolkit/profile-service;1";
+
+var gProfileService;
+var gProfileManagerBundle;
+
+var gDefaultProfileParent;
+var gOldProfileName;
+
+// The directory where the profile will be created.
+var gProfileRoot;
+
+// Text node to display the location and name of the profile to create.
+var gProfileDisplay;
+
+// Called once when the wizard is opened.
+function initWizard()
+{ 
+  try {
+    gProfileService = C[ToolkitProfileService].getService(I.nsIToolkitProfileService);
+    gProfileManagerBundle = document.getElementById("bundle_profileManager");
+
+    var dirService = C["@mozilla.org/file/directory_service;1"].getService(I.nsIProperties);
+    gDefaultProfileParent = dirService.get("DefProfRt", I.nsIFile);
+
+    gOldProfileName = document.getElementById("profileName").value;
+
+    // Initialize the profile location display.
+    gProfileDisplay = document.getElementById("profileDisplay").firstChild;
+    setDisplayToDefaultFolder();
+  }
+  catch(e) {
+    window.close();
+    throw (e);
+  }
+}
+
+// Called every time the second wizard page is displayed.
+function initSecondWizardPage() 
+{
+  var profileName = document.getElementById("profileName");
+  profileName.select();
+  profileName.focus();
+
+  // Initialize profile name validation.
+  checkCurrentInput(profileName.value);
+}
+
+function setDisplayToDefaultFolder()
+{
+  var defaultProfileDir = gDefaultProfileParent.clone();
+  defaultProfileDir.append(document.getElementById("profileName").value);
+  gProfileRoot = defaultProfileDir;
+  document.getElementById("useDefault").disabled = true;
+}
+
+function updateProfileDisplay()
+{
+  gProfileDisplay.data = gProfileRoot.path;
+}
+
+// Invoke a folder selection dialog for choosing the directory of profile storage.
+function chooseProfileFolder()
+{
+  var newProfileRoot;
+  
+  var dirChooser = C["@mozilla.org/filepicker;1"].createInstance(I.nsIFilePicker);
+  dirChooser.init(window, gProfileManagerBundle.getString("chooseFolder"),
+                  I.nsIFilePicker.modeGetFolder);
+  dirChooser.appendFilters(I.nsIFilePicker.filterAll);
+  dirChooser.show();
+  newProfileRoot = dirChooser.file;
+
+  // Disable the "Default Folder..." button when the default profile folder
+  // was selected manually in the File Picker.
+  // This is always false on Windows, until bug 221872 is fixed.
+  document.getElementById("useDefault").disabled =
+    (newProfileRoot.parent.equals(gDefaultProfileParent));
+
+  gProfileRoot = newProfileRoot;
+  updateProfileDisplay();
+}
+
+// Checks the current user input for validity and triggers an error message accordingly.
+function checkCurrentInput(currentInput)
+{
+  var finishButton = document.documentElement.getButton("finish");
+  var finishText = document.getElementById("finishText");
+  var canAdvance;
+
+  var errorMessage = checkProfileName(currentInput);
+
+  if (!errorMessage) {
+    finishText.className = "";
+    finishText.firstChild.data = gProfileManagerBundle.getString("profileFinishText");
+    canAdvance = true;
+  }
+  else {
+    finishText.className = "error";
+    finishText.firstChild.data = errorMessage;
+    canAdvance = false;
+  }
+
+  document.documentElement.canAdvance = canAdvance;
+  finishButton.disabled = !canAdvance;
+
+  updateProfileDisplay();
+}
+
+function updateProfileName(aNewName) {
+  checkCurrentInput(aNewName);
+
+  if (gProfileRoot.leafName == gOldProfileName) {
+    gProfileRoot.leafName = aNewName;
+    updateProfileDisplay();
+  }
+  gOldProfileName = aNewName;
+}
+
+// Checks whether the given string is a valid profile name.
+// Returns an error message describing the error in the name or "" when it's valid.
+function checkProfileName(profileNameToCheck)
+{
+  // Check for emtpy profile name.
+  if (!/\S/.test(profileNameToCheck))
+    return gProfileManagerBundle.getString("profileNameEmpty");
+
+  // Check whether all characters in the profile name are allowed.
+  if (/([\\*:?<>|\/\"])/.test(profileNameToCheck))
+    return gProfileManagerBundle.getFormattedString("invalidChar", [RegExp.$1]);
+
+  // Check whether a profile with the same name already exists.
+  if (profileExists(profileNameToCheck))
+    return gProfileManagerBundle.getString("profileExists");
+
+  // profileNameToCheck is valid.
+  return "";
+}
+
+function profileExists(aName)
+{
+  var profiles = gProfileService.profiles;
+  while (profiles.hasMoreElements()) {
+    var profile = profiles.getNext().QueryInterface(I.nsIToolkitProfile);
+    if (profile.name.toLowerCase() == aName.toLowerCase())
+      return true;
+  }
+
+  return false;
+}
+
+// Called when the first wizard page is shown.
+function enableNextButton()
+{
+  document.documentElement.canAdvance = true;
+}
+
+function onFinish() 
+{
+  var profileName = document.getElementById("profileName").value;
+  var profile;
+
+  // Create profile named profileName in profileRoot.
+  try {
+    profile = gProfileService.createProfile(gProfileRoot, profileName);
+  }
+  catch (e) {
+    var profileCreationFailed =
+      gProfileManagerBundle.getString("profileCreationFailed");
+    var profileCreationFailedTitle =
+      gProfileManagerBundle.getString("profileCreationFailedTitle");
+    var promptService = C["@mozilla.org/embedcomp/prompt-service;1"].
+      getService(I.nsIPromptService);
+    promptService.alert(window, profileCreationFailedTitle,
+                        profileCreationFailed + "\n" + e);
+
+    return false;
+  }
+
+  // window.opener is false if the Create Profile Wizard was opened from the
+  // command line.
+  if (window.opener) {
+    // Add new profile to the list in the Profile Manager.
+    window.opener.CreateProfile(profile);
+  }
+  else {
+    // Use the newly created Profile.
+    var profileLock = profile.lock();
+
+    var dialogParams = window.arguments[0].QueryInterface(I.nsIDialogParamBlock);
+    dialogParams.objects.insertElementAt(profileLock, 0, false);
+  }
+
+  // Exit the wizard.
+  return true;
+}
---
+++ toolkit/profile/content/profileSelection.js
@@ -0,0 +1,257 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Mozilla Communicator client code, released
+ * March 31, 1998.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation. Portions created by Netscape are
+ * Copyright (C) 1998-1999 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ *   Ben Goodger (03/01/00)
+ *   Seth Spitzer (28/10/99)
+ *   Dan Veditz <dveditz@netscape.com>
+ *   Benjamin Smedberg <bsmedberg@covad.net>
+ */
+
+const C = Components.classes;
+const I = Components.interfaces;
+
+const ToolkitProfileService = "@mozilla.org/toolkit/profile-service;1";
+const PromptService = "@mozilla.org/embedcomp/prompt-service;1";
+
+var gDialogParams;
+var gProfileManagerBundle;
+var gBrandBundle;
+var gProfileService;
+var gPromptService;
+
+function startup()
+{
+  try {
+    gDialogParams = window.arguments[0].
+      QueryInterface(I.nsIDialogParamBlock);
+
+    gProfileService = C[ToolkitProfileService].getService(I.nsIToolkitProfileService);
+
+    gProfileManagerBundle = document.getElementById("bundle_profileManager");
+    gBrandBundle = document.getElementById("bundle_brand");
+
+    gPromptService = C[PromptService].getService(I.nsIPromptService);
+
+    document.documentElement.centerWindowOnScreen();
+
+    var profilesElement = document.getElementById("profiles");
+
+    var profileList = gProfileService.profiles;
+    while (profileList.hasMoreElements()) {
+      var profile = profileList.getNext().QueryInterface(I.nsIToolkitProfile);
+
+      var listitem = profilesElement.appendItem(profile.name, "");
+
+      var tooltiptext =
+        gProfileManagerBundle.getFormattedString("profileTooltip", [profile.name, profile.rootDir.path]);
+      listitem.setAttribute("tooltiptext", tooltiptext);
+      listitem.setAttribute("class", "listitem-iconic");
+      listitem.profile = profile;
+      try {
+        if (profile === gProfileService.selectedProfile) {
+          profilesElement.selectedItem = listitem;
+        }
+      }
+      catch(e) { }
+    }
+
+    var autoSelectLastProfile = document.getElementById("autoSelectLastProfile");
+    autoSelectLastProfile.checked = gProfileService.startWithLastProfile;
+    profilesElement.focus();
+  }
+  catch(e) {
+    window.close();
+    throw (e);
+  }
+}
+
+function acceptDialog()
+{
+  var appName = gBrandBundle.getString("brandShortName");
+
+  var profilesElement = document.getElementById("profiles");
+  var selectedProfile = profilesElement.selectedItem;
+  if (!selectedProfile) {
+    var pleaseSelectTitle = gProfileManagerBundle.getString("pleaseSelectTitle");
+    var pleaseSelect =
+      gProfileManagerBundle.getFormattedString("pleaseSelect", [appName]);
+    gPromptService.alert(window, pleaseSelectTitle, pleaseSelect);
+
+    return false;
+  }
+
+  var profileLock;
+
+  try {
+    profileLock = selectedProfile.profile.lock();
+  }
+  catch (e) {
+    var lockedTitle = gProfileManagerBundle.getString("profileLockedTitle");
+    var locked =
+      gProfileManagerBundle.getFormattedString("profileLocked", [appName, selectedProfile.profile.name]);
+    gPromptService.alert(window, lockedTitle, locked);
+
+    return false;
+  }
+  gDialogParams.objects.insertElementAt(profileLock.nsIProfileLock, 0, false);
+
+  var autoSelectLastProfile = document.getElementById("autoSelectLastProfile");
+  gProfileService.startWithLastProfile = autoSelectLastProfile.checked;
+  gProfileService.selectedProfile = selectedProfile.profile;
+
+  gProfileService.startOffline = document.getElementById("offlineState").checked;
+
+  gDialogParams.SetInt(0, 1);
+
+  return true;
+}
+
+// handle key event on listboxes
+function onProfilesKey(aEvent)
+{
+  switch( aEvent.keyCode ) 
+  {
+  case 46:
+    confirmDelete();
+    break;
+  case "VK_F2":
+    renameProfile();
+    break;
+  }
+}
+
+function onProfilesDblClick(aEvent)
+{
+  if(aEvent.target.localName == "listitem")
+    document.documentElement.acceptDialog();
+}
+
+// invoke the createProfile Wizard
+function CreateProfileWizard()
+{
+  window.openDialog('chrome://mozapps/content/profile/createProfileWizard.xul',
+                    '', 'centerscreen,chrome,modal,titlebar', gProfileService);
+}
+
+/**
+ * Called from createProfileWizard to update the display.
+ */
+function CreateProfile(aProfile)
+{
+  var profilesElement = document.getElementById("profiles");
+
+  var listitem = profilesElement.appendItem(aProfile.name, "");
+
+  var tooltiptext =
+    gProfileManagerBundle.getFormattedString("profileTooltip", [aProfile.name, aProfile.rootDir.path]);
+  listitem.setAttribute("tooltiptext", tooltiptext);
+  listitem.setAttribute("class", "listitem-iconic");
+  listitem.profile = aProfile;
+
+  profilesElement.ensureElementIsVisible(listitem);
+  profilesElement.selectItem(listitem);
+}
+
+// rename the selected profile
+function RenameProfile()
+{
+  var profilesElement = document.getElementById("profiles");
+  var selectedItem = profilesElement.selectedItem;
+  if (!selectedItem) {
+    return false;
+  }
+
+  var selectedProfile = selectedItem.profile;
+
+  var oldName = selectedProfile.name;
+  var newName = {value: oldName};
+
+  var dialogTitle = gProfileManagerBundle.getString("renameProfileTitle");
+  var msg =
+    gProfileManagerBundle.getFormattedString("renameProfilePrompt", [oldName]);
+
+  if (gPromptService.prompt(window, dialogTitle, msg, newName, null, {value:0})) {
+    newName = newName.value;
+
+    // User hasn't changed the profile name. Treat as if cancel was pressed.
+    if (newName == oldName)
+      return false;
+
+    try {
+      selectedProfile.name = newName;
+    }
+    catch (e) {
+      var alTitle = gProfileManagerBundle.getString("profileNameInvalidTitle");
+      var alMsg = gProfileManagerBundle.getFormattedString("profileNameInvalid", [newName]);
+      gPromptService.alert(window, alTitle, alMsg);
+      return false;
+    }
+
+    selectedItem.label = newName;
+    var tooltiptext =
+      gProfileManagerBundle.getFormattedString("profileTooltip", [newName, aProfile.rootDir.path]);
+    listitem.setAttribute("tooltiptext", tooltiptext);
+
+    return true;
+  }
+
+  return false;
+}
+
+function ConfirmDelete()
+{
+  var deleteButton = document.getElementById("delbutton");
+  var profileList = document.getElementById( "profiles" );
+
+  var selectedItem = profileList.selectedItem;
+  if (!selectedItem) {
+    return false;
+  }
+
+  var selectedProfile = selectedItem.profile;
+  var deleteFiles = false;
+
+  if (selectedProfile.rootDir.exists()) {
+    var dialogTitle = gProfileManagerBundle.getString("deleteTitle");
+    var dialogText =
+      gProfileManagerBundle.getFormattedString("deleteProfile",
+                                               [selectedProfile.rootDir.path]);
+
+    var buttonPressed = gPromptService.confirmEx(window, dialogTitle, dialogText,
+                          (gPromptService.BUTTON_TITLE_IS_STRING * gPromptService.BUTTON_POS_0) +
+                          (gPromptService.BUTTON_TITLE_CANCEL * gPromptService.BUTTON_POS_1) +
+                          (gPromptService.BUTTON_TITLE_IS_STRING * gPromptService.BUTTON_POS_2),
+                          gProfileManagerBundle.getString("dontDeleteFiles"),
+                          null,
+                          gProfileManagerBundle.getString("deleteFiles"),
+                          null, {value:0});
+    if (buttonPressed == 1)
+      return false;
+
+    if (buttonPressed == 2)
+      deleteFiles = true;
+  }
+  
+  selectedProfile.remove(deleteFiles);
+  selectedItem.parentNode.removeChild(selectedItem);
+
+  return true;
+}
