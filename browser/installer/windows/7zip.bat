00a1d8c0ff4aa768338d837597528a1986b984d7
ben%bengoodger.com <ben%bengoodger.com>
2004-06-16 07:31:53 +0000

    make firefox smaller thanks to Igor Pavlov

--- browser/installer/windows/7zip.bat
+++ browser/installer/windows/7zip.bat
@@ -1,5 +1,5 @@
 cd 7zstage
-7z a -t7z ..\7z\app.7z *.* -m0=LZMA:d25m:fb128 -mx9
+7z a -t7z ..\7z\app.7z *.* -mx -m0=BCJ2 -m1=LZMA:d24 -m2=LZMA:d19 -m3=LZMA:d19  -mb0:1 -mb0s1:2 -mb0s2:3
 cd ..\7z
 upx -9 7zSD.sfx
 copy /b 7zSD.sfx+app.tag+app.7z SetupGeneric.exe
