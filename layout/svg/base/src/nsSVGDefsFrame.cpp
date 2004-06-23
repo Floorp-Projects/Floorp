ea17cdf5c2242ee599b1b50eeea397db08fc6467
tor@cs.brown.edu <tor@cs.brown.edu>
2004-06-23 18:14:04 +0000

    Bug 246857 - implement <svg:defs>.  r=afri.  Not in default build.

---
+++ layout/svg/base/src/nsSVGDefsFrame.cpp
@@ -0,0 +1,344 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ----- BEGIN LICENSE BLOCK -----
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the Mozilla SVG project.
+ *
+ * The Initial Developer of the Original Code is 
+ * Crocodile Clips Ltd..
+ * Portions created by the Initial Developer are Copyright (C) 2001
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *    Alex Fritze <alex.fritze@crocodile-clips.com> (original author)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or 
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the NPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ----- END LICENSE BLOCK ----- */
+
+#include "nsIDOMSVGRect.h"
+#include "nsIDOMSVGGElement.h"
+#include "nsIPresContext.h"
+#include "nsISVGOuterSVGFrame.h"
+#include "nsISVGRendererCanvas.h"
+#include "nsISVGValue.h"
+#include "nsIDOMSVGTransformable.h"
+#include "nsIDOMSVGAnimTransformList.h"
+#include "nsSVGDefsFrame.h"
+
+//----------------------------------------------------------------------
+// Implementation
+
+nsresult
+NS_NewSVGDefsFrame(nsIPresShell* aPresShell, nsIContent* aContent, nsIFrame** aNewFrame)
+{
+  *aNewFrame = nsnull;
+  
+  nsCOMPtr<nsIDOMSVGTransformable> transformable = do_QueryInterface(aContent);
+  if (!transformable) {
+#ifdef DEBUG
+    printf("warning: trying to construct an SVGDefsFrame for a content element that doesn't support the right interfaces\n");
+#endif
+    return NS_ERROR_FAILURE;
+  }
+  
+  nsSVGDefsFrame* it = new (aPresShell) nsSVGDefsFrame;
+  if (nsnull == it)
+    return NS_ERROR_OUT_OF_MEMORY;
+
+  *aNewFrame = it;
+
+  return NS_OK;
+}
+
+nsSVGDefsFrame::nsSVGDefsFrame()
+{
+}
+
+nsSVGDefsFrame::~nsSVGDefsFrame()
+{
+  nsCOMPtr<nsIDOMSVGTransformable> transformable = do_QueryInterface(mContent);
+  NS_ASSERTION(transformable, "wrong content element");
+  nsCOMPtr<nsIDOMSVGAnimatedTransformList> transforms;
+  transformable->GetTransform(getter_AddRefs(transforms));
+  nsCOMPtr<nsISVGValue> value = do_QueryInterface(transforms);
+  NS_ASSERTION(value, "interface not found");
+  if (value)
+    value->RemoveObserver(this);
+}
+
+nsresult nsSVGDefsFrame::Init()
+{
+  nsCOMPtr<nsIDOMSVGTransformable> transformable = do_QueryInterface(mContent);
+  NS_ASSERTION(transformable, "wrong content element");
+  nsCOMPtr<nsIDOMSVGAnimatedTransformList> transforms;
+  transformable->GetTransform(getter_AddRefs(transforms));
+  nsCOMPtr<nsISVGValue> value = do_QueryInterface(transforms);
+  NS_ASSERTION(value, "interface not found");
+  if (value)
+    value->AddObserver(this);
+  return NS_OK;
+}
+
+//----------------------------------------------------------------------
+// nsISupports methods
+
+NS_INTERFACE_MAP_BEGIN(nsSVGDefsFrame)
+  NS_INTERFACE_MAP_ENTRY(nsISVGChildFrame)
+  NS_INTERFACE_MAP_ENTRY(nsISVGContainerFrame)
+  NS_INTERFACE_MAP_ENTRY(nsISupportsWeakReference)
+  NS_INTERFACE_MAP_ENTRY(nsISVGValueObserver)
+NS_INTERFACE_MAP_END_INHERITING(nsSVGDefsFrameBase)
+
+
+//----------------------------------------------------------------------
+// nsIFrame methods
+NS_IMETHODIMP
+nsSVGDefsFrame::Init(nsIPresContext*  aPresContext,
+                  nsIContent*      aContent,
+                  nsIFrame*        aParent,
+                  nsStyleContext*  aContext,
+                  nsIFrame*        aPrevInFlow)
+{
+  nsresult rv;
+  rv = nsSVGDefsFrameBase::Init(aPresContext, aContent, aParent,
+                                aContext, aPrevInFlow);
+
+  Init();
+  
+  return rv;
+}
+
+NS_IMETHODIMP
+nsSVGDefsFrame::AppendFrames(nsIPresContext* aPresContext,
+                          nsIPresShell&   aPresShell,
+                          nsIAtom*        aListName,
+                          nsIFrame*       aFrameList)
+{
+  // append == insert at end:
+  return InsertFrames(aPresContext, aPresShell, aListName,
+                      mFrames.LastChild(), aFrameList);  
+}
+
+NS_IMETHODIMP
+nsSVGDefsFrame::InsertFrames(nsIPresContext* aPresContext,
+                          nsIPresShell&   aPresShell,
+                          nsIAtom*        aListName,
+                          nsIFrame*       aPrevFrame,
+                          nsIFrame*       aFrameList)
+{
+  // memorize last new frame
+  nsIFrame* lastNewFrame = nsnull;
+  {
+    nsFrameList tmpList(aFrameList);
+    lastNewFrame = tmpList.LastChild();
+  }
+  
+  // Insert the new frames
+  mFrames.InsertFrames(this, aPrevFrame, aFrameList);
+
+  // call InitialUpdate() on all new frames:
+  nsIFrame* end = nsnull;
+  if (lastNewFrame)
+    end = lastNewFrame->GetNextSibling();
+  
+  for (nsIFrame* kid = aFrameList; kid != end;
+       kid = kid->GetNextSibling()) {
+    nsISVGChildFrame* SVGFrame=nsnull;
+    kid->QueryInterface(NS_GET_IID(nsISVGChildFrame),(void**)&SVGFrame);
+    if (SVGFrame) {
+      SVGFrame->InitialUpdate(); 
+    }
+  }
+  
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsSVGDefsFrame::RemoveFrame(nsIPresContext* aPresContext,
+                     nsIPresShell&   aPresShell,
+                     nsIAtom*        aListName,
+                     nsIFrame*       aOldFrame)
+{
+  nsCOMPtr<nsISVGRendererRegion> dirty_region;
+  
+  nsISVGChildFrame* SVGFrame=nsnull;
+  aOldFrame->QueryInterface(NS_GET_IID(nsISVGChildFrame),(void**)&SVGFrame);
+
+  if (SVGFrame)
+    dirty_region = SVGFrame->GetCoveredRegion();
+
+  PRBool result = mFrames.DestroyFrame(aPresContext, aOldFrame);
+
+  nsISVGOuterSVGFrame* outerSVGFrame = GetOuterSVGFrame();
+  NS_ASSERTION(outerSVGFrame, "no outer svg frame");
+  if (dirty_region && outerSVGFrame)
+    outerSVGFrame->InvalidateRegion(dirty_region, PR_TRUE);
+
+  NS_ASSERTION(result, "didn't find frame to delete");
+  return result ? NS_OK : NS_ERROR_FAILURE;
+}
+
+NS_IMETHODIMP
+nsSVGDefsFrame::ReplaceFrame(nsIPresContext* aPresContext,
+                          nsIPresShell&   aPresShell,
+                          nsIAtom*        aListName,
+                          nsIFrame*       aOldFrame,
+                          nsIFrame*       aNewFrame)
+{
+  NS_NOTYETIMPLEMENTED("write me!");
+  return NS_ERROR_UNEXPECTED;
+}
+
+//----------------------------------------------------------------------
+// nsISVGValueObserver methods:
+
+NS_IMETHODIMP
+nsSVGDefsFrame::WillModifySVGObservable(nsISVGValue* observable)
+{
+  return NS_OK;
+}
+
+
+NS_IMETHODIMP
+nsSVGDefsFrame::DidModifySVGObservable (nsISVGValue* observable)
+{
+  for (nsIFrame* kid = mFrames.FirstChild(); kid;
+       kid = kid->GetNextSibling()) {
+    nsISVGChildFrame* SVGFrame=nsnull;
+    kid->QueryInterface(NS_GET_IID(nsISVGChildFrame),(void**)&SVGFrame);
+    if (SVGFrame)
+      SVGFrame->NotifyCTMChanged();
+  }  
+  return NS_OK;
+}
+
+
+//----------------------------------------------------------------------
+// nsISVGChildFrame methods
+
+NS_IMETHODIMP
+nsSVGDefsFrame::Paint(nsISVGRendererCanvas* canvas, const nsRect& dirtyRectTwips)
+{
+  // defs don't paint
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsSVGDefsFrame::GetFrameForPoint(float x, float y, nsIFrame** hit)
+{
+  *hit = nsnull;
+  
+  return NS_OK;
+}
+
+NS_IMETHODIMP_(already_AddRefed<nsISVGRendererRegion>)
+nsSVGDefsFrame::GetCoveredRegion()
+{
+  nsISVGRendererRegion *accu_region=nsnull;
+  
+  return accu_region;
+}
+
+NS_IMETHODIMP
+nsSVGDefsFrame::InitialUpdate()
+{
+  nsIFrame* kid = mFrames.FirstChild();
+  while (kid) {
+    nsISVGChildFrame* SVGFrame=0;
+    kid->QueryInterface(NS_GET_IID(nsISVGChildFrame),(void**)&SVGFrame);
+    if (SVGFrame) {
+      SVGFrame->InitialUpdate();
+    }
+    kid = kid->GetNextSibling();
+  }
+  return NS_OK;
+}  
+
+NS_IMETHODIMP
+nsSVGDefsFrame::NotifyCTMChanged()
+{
+  for (nsIFrame* kid = mFrames.FirstChild(); kid;
+       kid = kid->GetNextSibling()) {
+    nsISVGChildFrame* SVGFrame=nsnull;
+    kid->QueryInterface(NS_GET_IID(nsISVGChildFrame),(void**)&SVGFrame);
+    if (SVGFrame) {
+      SVGFrame->NotifyCTMChanged();
+    }
+  }
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsSVGDefsFrame::NotifyRedrawSuspended()
+{
+  for (nsIFrame* kid = mFrames.FirstChild(); kid;
+       kid = kid->GetNextSibling()) {
+    nsISVGChildFrame* SVGFrame=nsnull;
+    kid->QueryInterface(NS_GET_IID(nsISVGChildFrame),(void**)&SVGFrame);
+    if (SVGFrame) {
+      SVGFrame->NotifyRedrawSuspended();
+    }
+  }
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsSVGDefsFrame::NotifyRedrawUnsuspended()
+{
+  for (nsIFrame* kid = mFrames.FirstChild(); kid;
+       kid = kid->GetNextSibling()) {
+    nsISVGChildFrame* SVGFrame=nsnull;
+    kid->QueryInterface(NS_GET_IID(nsISVGChildFrame),(void**)&SVGFrame);
+    if (SVGFrame) {
+      SVGFrame->NotifyRedrawUnsuspended();
+    }
+  }
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsSVGDefsFrame::GetBBox(nsIDOMSVGRect **_retval)
+{
+  *_retval = nsnull;
+  return NS_ERROR_FAILURE;
+}
+
+//----------------------------------------------------------------------
+// nsISVGContainerFrame methods:
+
+NS_IMETHODIMP_(nsISVGOuterSVGFrame *)
+nsSVGDefsFrame::GetOuterSVGFrame()
+{
+  NS_ASSERTION(mParent, "null parent");
+  
+  nsISVGContainerFrame *containerFrame;
+  mParent->QueryInterface(NS_GET_IID(nsISVGContainerFrame), (void**)&containerFrame);
+  if (!containerFrame) {
+    NS_ERROR("invalid container");
+    return nsnull;
+  }
+
+  return containerFrame->GetOuterSVGFrame();  
+}
