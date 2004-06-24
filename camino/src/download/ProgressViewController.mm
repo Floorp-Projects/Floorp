bcf6f0916a81ac846056447650577294761f780d
pinkerton%aol.net <pinkerton%aol.net>
2004-06-24 15:04:12 +0000

    use user-chosen save name as displayed download name (bug 247968)

--- camino/src/download/ProgressViewController.mm
+++ camino/src/download/ProgressViewController.mm
@@ -269,7 +269,7 @@ enum {
 -(void)refreshDownloadInfo
 {
   NSView* curView = [self view];
-  NSString* filename = [mSourceURL lastPathComponent];
+  NSString* filename = [mDestPath lastPathComponent];
   id iconLabel = [curView viewWithTag:kLabelTagIcon];
   
   id filenameLabel = [curView viewWithTag:kLabelTagFilename];
