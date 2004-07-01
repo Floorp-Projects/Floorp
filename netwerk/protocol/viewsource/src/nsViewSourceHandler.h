5bacf5acce7fdd1164d3a0534fce6aae5e4382c8
darin@meer.net <darin@meer.net>
2004-07-01 23:45:34 +0000

    fixes bug 201040 "Unable to view page source of the page that uses IDN" r=biesi

--- netwerk/protocol/viewsource/src/nsViewSourceHandler.h
+++ netwerk/protocol/viewsource/src/nsViewSourceHandler.h
@@ -40,28 +40,12 @@
 #define nsViewSourceHandler_h___
 
 #include "nsIProtocolHandler.h"
-#include "nsString.h"
-
-// {0x9c7ec5d1-23f9-11d5-aea8-8fcc0793e97f}
-#define NS_VIEWSOURCEHANDLER_CID     \
-{ 0x9c7ec5d1, 0x23f9, 0x11d5,   \
-    { 0xae, 0xa8, 0x8f, 0xcc, 0x7, 0x93, 0xe9, 0x7f } }
 
 class nsViewSourceHandler : public nsIProtocolHandler
 {
 public:
     NS_DECL_ISUPPORTS
     NS_DECL_NSIPROTOCOLHANDLER
-
-    // nsViewSourceHandler methods:
-    nsViewSourceHandler();
-    virtual ~nsViewSourceHandler();
-
-    // Define a Create method to be used with a factory:
-    static NS_METHOD Create(nsISupports* aOuter, const nsIID& aIID, void* *aResult);
-
-    nsresult Init();
-
 };
 
-#endif /* nsViewSourceHandler_h___ */
+#endif /* !defined( nsViewSourceHandler_h___ ) */
