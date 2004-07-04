15426336fd1cc1d81bcdf164b7527c2ea97f3371
igor%mir2.org <igor%mir2.org>
2004-07-04 14:12:01 +0000

    Normilizing numeration of Token constants after removal of few no longer used constants in the previous patches

--- js/rhino/src/org/mozilla/javascript/Token.java
+++ js/rhino/src/org/mozilla/javascript/Token.java
@@ -66,150 +66,146 @@ public class Token
 
     public final static int
     // start enum
-        ERROR       = -1, // well-known as the only code < EOF
-        EOF         = 0,  // end of file token - (not EOF_CHAR)
-        EOL         = 1,  // end of line
+        ERROR          = -1, // well-known as the only code < EOF
+        EOF            = 0,  // end of file token - (not EOF_CHAR)
+        EOL            = 1,  // end of line
 
         // Interpreter reuses the following as bytecodes
-        FIRST_BYTECODE_TOKEN = 2,
+        FIRST_BYTECODE_TOKEN    = 2,
 
-// XXX removed unused POPV
-        ENTERWITH   = 3,
-        LEAVEWITH   = 4,
-        RETURN      = 5,
-        GOTO        = 6,
-        IFEQ        = 7,
-        IFNE        = 8,
-        SETNAME     = 9,
-        BITOR       = 10,
-        BITXOR      = 11,
-        BITAND      = 12,
-        EQ          = 13,
-        NE          = 14,
-        LT          = 15,
-        LE          = 16,
-        GT          = 17,
-        GE          = 18,
-        LSH         = 19,
-        RSH         = 20,
-        URSH        = 21,
-        ADD         = 22,
-        SUB         = 23,
-        MUL         = 24,
-        DIV         = 25,
-        MOD         = 26,
-        NOT         = 27,
-        BITNOT      = 28,
-        POS         = 29,
-        NEG         = 30,
-        NEW         = 31,
-        DELPROP     = 32,
-        TYPEOF      = 33,
-        GETPROP     = 34,
-        SETPROP     = 35,
-        GETELEM     = 36,
-        SETELEM     = 37,
-        CALL        = 38,
-        NAME        = 39,
-        NUMBER      = 40,
-        STRING      = 41,
-        ZERO        = 42,
-        ONE         = 43,
-        NULL        = 44,
-        THIS        = 45,
-        FALSE       = 46,
-        TRUE        = 47,
-        SHEQ        = 48,   // shallow equality (===)
-        SHNE        = 49,   // shallow inequality (!==)
-        REGEXP      = 50,
-// XXX removed unused POP
-        BINDNAME    = 52,
-        THROW       = 53,
-        IN          = 54,
-        INSTANCEOF  = 55,
-        LOCAL_SAVE  = 56,
-        LOCAL_LOAD  = 57,
-        GETVAR      = 58,
-        SETVAR      = 59,
-// XXX removed unused UNDEFINED
-        CATCH_SCOPE = 61,
-        ENUM_INIT   = 62,
-        ENUM_NEXT   = 63,
-        ENUM_ID     = 64,
-        THISFN      = 65,
-        RETURN_RESULT = 66, // to return result stored as popv in functions
-        ARRAYLIT    = 67, // array literal
-        OBJECTLIT   = 68, // object literal
-        GET_REF     = 69, // *reference
-        SET_REF     = 70, // *reference = something
-        REF_CALL    = 71, // f(args) = something or f(args)++
-        SPECIAL_REF = 72, // reference for special properties like __proto
-        GENERIC_REF = 73, // generic reference to generate runtime ref errors
+        ENTERWITH      = 2,
+        LEAVEWITH      = 3,
+        RETURN         = 4,
+        GOTO           = 5,
+        IFEQ           = 6,
+        IFNE           = 7,
+        SETNAME        = 8,
+        BITOR          = 9,
+        BITXOR         = 10,
+        BITAND         = 11,
+        EQ             = 12,
+        NE             = 13,
+        LT             = 14,
+        LE             = 15,
+        GT             = 16,
+        GE             = 17,
+        LSH            = 18,
+        RSH            = 19,
+        URSH           = 20,
+        ADD            = 21,
+        SUB            = 22,
+        MUL            = 23,
+        DIV            = 24,
+        MOD            = 25,
+        NOT            = 26,
+        BITNOT         = 27,
+        POS            = 28,
+        NEG            = 29,
+        NEW            = 30,
+        DELPROP        = 31,
+        TYPEOF         = 32,
+        GETPROP        = 33,
+        SETPROP        = 34,
+        GETELEM        = 35,
+        SETELEM        = 36,
+        CALL           = 37,
+        NAME           = 38,
+        NUMBER         = 39,
+        STRING         = 40,
+        ZERO           = 41,
+        ONE            = 42,
+        NULL           = 43,
+        THIS           = 44,
+        FALSE          = 45,
+        TRUE           = 46,
+        SHEQ           = 47,   // shallow equality (===)
+        SHNE           = 48,   // shallow inequality (!==)
+        REGEXP         = 49,
+        BINDNAME       = 50,
+        THROW          = 51,
+        IN             = 52,
+        INSTANCEOF     = 53,
+        LOCAL_SAVE     = 54,
+        LOCAL_LOAD     = 55,
+        GETVAR         = 56,
+        SETVAR         = 57,
+        CATCH_SCOPE    = 58,
+        ENUM_INIT      = 59,
+        ENUM_NEXT      = 60,
+        ENUM_ID        = 61,
+        THISFN         = 62,
+        RETURN_RESULT  = 63, // to return prevoisly stored return result
+        ARRAYLIT       = 64, // array literal
+        OBJECTLIT      = 65, // object literal
+        GET_REF        = 66, // *reference
+        SET_REF        = 67, // *reference    = something
+        REF_CALL       = 68, // f(args)    = something or f(args)++
+        SPECIAL_REF    = 69, // reference for special properties like __proto
+        GENERIC_REF    = 70, // generic reference to generate runtime ref errors
 
-        LAST_BYTECODE_TOKEN = 73,
+        LAST_BYTECODE_TOKEN    = 70,
         // End of interpreter bytecodes
 
-        TRY         = 74,
-        SEMI        = 75,  // semicolon
-        LB          = 76,  // left and right brackets
-        RB          = 77,
-        LC          = 78,  // left and right curlies (braces)
-        RC          = 79,
-        LP          = 80,  // left and right parentheses
-        RP          = 81,
-        COMMA       = 82,  // comma operator
-        ASSIGN      = 83, // simple assignment  (=)
-        ASSIGNOP    = 84, // assignment with operation (+= -= etc.)
-        HOOK        = 85, // conditional (?:)
-        COLON       = 86,
-        OR          = 87, // logical or (||)
-        AND         = 88, // logical and (&&)
-        INC         = 89, // increment/decrement (++ --)
-        DEC         = 90,
-        DOT         = 91, // member operator (.)
-        FUNCTION    = 92, // function keyword
-        EXPORT      = 93, // export keyword
-        IMPORT      = 94, // import keyword
-        IF          = 95, // if keyword
-        ELSE        = 96, // else keyword
-        SWITCH      = 97, // switch keyword
-        CASE        = 98, // case keyword
-        DEFAULT     = 99, // default keyword
-        WHILE       = 100, // while keyword
-        DO          = 101, // do keyword
-        FOR         = 102, // for keyword
-        BREAK       = 103, // break keyword
-        CONTINUE    = 104, // continue keyword
-        VAR         = 105, // var keyword
-        WITH        = 106, // with keyword
-        CATCH       = 107, // catch keyword
-        FINALLY     = 108, // finally keyword
-        VOID        = 109, // void keyword
-        RESERVED    = 110, // reserved keywords
+        TRY            = 71,
+        SEMI           = 72,  // semicolon
+        LB             = 73,  // left and right brackets
+        RB             = 74,
+        LC             = 75,  // left and right curlies (braces)
+        RC             = 76,
+        LP             = 77,  // left and right parentheses
+        RP             = 78,
+        COMMA          = 79,  // comma operator
+        ASSIGN         = 80, // simple assignment  (=)
+        ASSIGNOP       = 81, // assignment with operation (+= -= etc.)
+        HOOK           = 82, // conditional (?:)
+        COLON          = 83,
+        OR             = 84, // logical or (||)
+        AND            = 85, // logical and (&&)
+        INC            = 86, // increment/decrement (++ --)
+        DEC            = 87,
+        DOT            = 88, // member operator (.)
+        FUNCTION       = 89, // function keyword
+        EXPORT         = 90, // export keyword
+        IMPORT         = 91, // import keyword
+        IF             = 92, // if keyword
+        ELSE           = 93, // else keyword
+        SWITCH         = 94, // switch keyword
+        CASE           = 95, // case keyword
+        DEFAULT        = 96, // default keyword
+        WHILE          = 97, // while keyword
+        DO             = 98, // do keyword
+        FOR            = 99, // for keyword
+        BREAK          = 100, // break keyword
+        CONTINUE       = 101, // continue keyword
+        VAR            = 102, // var keyword
+        WITH           = 103, // with keyword
+        CATCH          = 104, // catch keyword
+        FINALLY        = 105, // finally keyword
+        VOID           = 106, // void keyword
+        RESERVED       = 107, // reserved keywords
 
-        EMPTY       = 111,
+        EMPTY          = 108,
 
         /* types used for the parse tree - these never get returned
          * by the scanner.
          */
 
-        BLOCK       = 112, // statement block
-        LABEL       = 113, // label
-        TARGET      = 114,
-        LOOP        = 115,
-        EXPR_VOID   = 116,
-        EXPR_RESULT = 117,
-        JSR         = 118,
-        SCRIPT      = 119,   // top-level node for entire script
-        TYPEOFNAME  = 120,  // for typeof(simple-name)
-        USE_STACK   = 121,
-        SETPROP_OP  = 122, // x.y op= something
-        SETELEM_OP  = 123, // x[y] op= something
-// XXX removed unused INIT_LIST
-        LOCAL_BLOCK = 125,
-        SET_REF_OP  = 126, // *reference op= something
+        BLOCK          = 109, // statement block
+        LABEL          = 110, // label
+        TARGET         = 111,
+        LOOP           = 112,
+        EXPR_VOID      = 113, // expression statement in functions
+        EXPR_RESULT    = 114, // expression statement in scripts
+        JSR            = 115,
+        SCRIPT         = 116, // top-level node for entire script
+        TYPEOFNAME     = 117, // for typeof(simple-name)
+        USE_STACK      = 118,
+        SETPROP_OP     = 119, // x.y op= something
+        SETELEM_OP     = 120, // x[y] op= something
+        LOCAL_BLOCK    = 121,
+        SET_REF_OP     = 122, // *reference op= something
 
-        LAST_TOKEN  = 126;
+        LAST_TOKEN     = 122;
 
     public static String name(int token)
     {
