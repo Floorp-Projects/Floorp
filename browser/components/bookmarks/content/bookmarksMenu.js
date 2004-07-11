d44bebbdef82f372d22194eb1af62a5040d403a5
ben%bengoodger.com <ben%bengoodger.com>
2004-07-11 22:53:35 +0000

    174390 - bookmarks toolbar overflow doesn't account for toolbar items to the right of the bookmarks toolbar. patch by dkoppenh@null.net, r=ben

--- browser/components/bookmarks/content/bookmarksMenu.js
+++ browser/components/bookmarks/content/bookmarksMenu.js
@@ -872,6 +872,14 @@ var BookmarksToolbar =
       return;
     var chevron = document.getElementById("bookmarks-chevron");
     var width = window.innerWidth;
+    var myToolbar = buttons.parentNode.parentNode.parentNode;
+    for (var i = myToolbar.childNodes.length-1; i >= 0; i--){
+      var anItem = myToolbar.childNodes[i];
+      if (anItem.id == "personal-bookmarks") {
+        break;
+      }
+      width -= anItem.boxObject.width;
+    }
     var chevronWidth = 0;
     chevron.collapsed = false;
     chevronWidth = chevron.boxObject.width;
