9bf73dcc6b8676841c6255727e422e249a57f685
jst@mozilla.jstenback.com <jst@mozilla.jstenback.com>
2004-07-12 16:38:06 +0000

    Fixing Mac build bustage.

--- modules/plugin/base/public/npapi.h
+++ modules/plugin/base/public/npapi.h
@@ -37,7 +37,7 @@
 
 
 /*
- *  npapi.h $Revision: 3.35 $
+ *  npapi.h $Revision: 3.36 $
  *  Netscape client plug-in API spec
  */
 
@@ -186,7 +186,7 @@ typedef unsigned short uint16;
 #endif
 
 #ifndef _UINT32
-#    if defined(__alpha) || defined(__amd64__) defined(__x86_64__)
+#    if defined(__alpha) || defined(__amd64__) || defined(__x86_64__)
 typedef unsigned int uint32;
 #    else  /* __alpha */
 typedef unsigned long uint32;
