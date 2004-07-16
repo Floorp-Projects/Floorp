286ee8179602b0d255a48af90fa64aa9563fb411
dwitte@stanford.edu <dwitte@stanford.edu>
2004-07-16 20:08:08 +0000

    quick fix to get cookie/permission file saving working on trunk again, while we hash out a real fix.
    b=251648, r+sr=darin

--- extensions/cookie/nsPermissionManager.cpp
+++ extensions/cookie/nsPermissionManager.cpp
@@ -931,15 +931,24 @@ nsPermissionManager::Write()
   }
 
   delete[] hostList;
-  mChangedList = PR_FALSE;
+
+  // XXX hack for bug 251648, to avoid dataloss:
+  // flush the buffered stream's data first
+  rv = bufferedOutputStream->Flush();
 
   // All went ok. Maybe except for problems in Write(), but the stream detects
   // that for us
   nsCOMPtr<nsISafeFileOutputStream> safeStream = do_QueryInterface(fileOutputStream);
-  if (safeStream)
-    safeStream->Finish();
+  if (NS_SUCCEEDED(rv) && safeStream)
+    rv = safeStream->Finish();
 
-  return NS_OK;
+  if (NS_FAILED(rv)) {
+    NS_WARNING("failed to save permissions file! possible dataloss");
+    return rv;
+  }
+
+  mChangedList = PR_FALSE;
+  return rv;
 }
 
 nsresult
