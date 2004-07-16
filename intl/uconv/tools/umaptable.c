9f35e25052938d0e9221160ffc8ff3d44fce3840
gerv%gerv.net <gerv%gerv.net>
2004-07-16 18:48:35 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license.

--- intl/uconv/tools/umaptable.c
+++ intl/uconv/tools/umaptable.c
@@ -23,8 +23,8 @@
  * Contributor(s):
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
@@ -182,23 +182,42 @@ void AddFormat0(uint16 srcBegin, uint16 srcEnd)
 void printnpl()
 {
 printf(
-"/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-\n"
-"*\n"
-"* The contents of this file are subject to the Netscape Public License\n"
-"* Version 1.0 (the \"NPL\"); you may not use this file except in\n"
-"* compliance with the NPL.  You may obtain a copy of the NPL at\n"
-"* http://www.mozilla.org/NPL/\n"
-"*\n"
-"* Software distributed under the NPL is distributed on an \"AS IS\" basis, \n"
-"* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL \n"
-"* for the specific language governing rights and limitations under the \n"
-"* NPL. \n"
-"*\n"
-"* The Initial Developer of this code under the NPL is Netscape\n"
-"* Communications Corporation.  Portions created by Netscape are\n"
-"* Copyright (C) 2001 Netscape Communications Corporation.  All Rights\n"
-"* Reserved.\n"
-"*/\n ");
+"/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */\n"
+"/* ***** BEGIN LICENSE BLOCK *****\n"
+" * Version: MPL 1.1/GPL 2.0/LGPL 2.1\n"
+" *\n"
+" * The contents of this file are subject to the Mozilla Public License Version\n"
+" * 1.1 (the \"License\"); you may not use this file except in compliance with\n"
+" * the License. You may obtain a copy of the License at\n"
+" * http://www.mozilla.org/MPL/\n"
+" *\n"
+" * Software distributed under the License is distributed on an \"AS IS\" basis,\n"
+" * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License\n"
+" * for the specific language governing rights and limitations under the\n"
+" * License.\n"
+" *\n"
+" * The Original Code is mozilla.org code.\n"
+" *\n"
+" * The Initial Developer of the Original Code is\n"
+" * Netscape Communications Corporation.\n"
+" * Portions created by the Initial Developer are Copyright (C) 2001\n"
+" * the Initial Developer. All Rights Reserved.\n"
+" *\n"
+" * Contributor(s):\n"
+" *\n"
+" * Alternatively, the contents of this file may be used under the terms of\n"
+" * either the GNU General Public License Version 2 or later (the \"GPL\"), or\n"
+" * the GNU Lesser General Public License Version 2.1 or later (the \"LGPL\"),\n"
+" * in which case the provisions of the GPL or the LGPL are applicable instead\n"
+" * of those above. If you wish to allow use of your version of this file only\n"
+" * under the terms of either the GPL or the LGPL, and not to allow others to\n"
+" * use your version of this file under the terms of the MPL, indicate your\n"
+" * decision by deleting the provisions above and replace them with the notice\n"
+" * and other provisions required by the GPL or the LGPL. If you do not delete\n"
+" * the provisions above, a recipient may use your version of this file under\n"
+" * the terms of any one of the MPL, the GPL or the LGPL.\n"
+" *\n"
+" * ***** END LICENSE BLOCK ***** */\n");
 }
 void gentable()
 {
--- mailnews/mime/cthandlers/smimestub/nsSMIMEStubFactory.cpp
+++ mailnews/mime/cthandlers/smimestub/nsSMIMEStubFactory.cpp
@@ -23,8 +23,8 @@
  *   Pierre Phaneuf <pp@ludusdesign.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
@@ -35,28 +35,6 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
-
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- *
- * The contents of this file are subject to the Netscape Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/NPL/
- *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
- *
- * The Original Code is mozilla.org code.
- *
- * The Initial Developer of the Original Code is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation. All
- * Rights Reserved.
- *
- * Contributor(s): 
- */
 
 #include "nsIGenericFactory.h"
 
