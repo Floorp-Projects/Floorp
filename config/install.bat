ba2432c07e3062eb62fef54da706ab92cb4a6063
gerv%gerv.net <gerv%gerv.net>
2004-08-25 23:02:50 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license.

--- config/install.bat
+++ config/install.bat
@@ -1,22 +1,40 @@
 @echo off
-rem The contents of this file are subject to the Netscape Public
-rem License Version 1.1 (the "License"); you may not use this file
-rem except in compliance with the License. You may obtain a copy of
-rem the License at http://www.mozilla.org/NPL/
+rem ***** BEGIN LICENSE BLOCK *****
+rem Version: MPL 1.1/GPL 2.0/LGPL 2.1
 rem
-rem Software distributed under the License is distributed on an "AS
-rem IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-rem implied. See the License for the specific language governing
-rem rights and limitations under the License.
+rem The contents of this file are subject to the Mozilla Public License Version
+rem 1.1 (the "License"); you may not use this file except in compliance with
+rem the License. You may obtain a copy of the License at
+rem http://www.mozilla.org/MPL/
+rem
+rem Software distributed under the License is distributed on an "AS IS" basis,
+rem WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+rem for the specific language governing rights and limitations under the
+rem License.
 rem
 rem The Original Code is mozilla.org code.
 rem
-rem The Initial Developer of the Original Code is Netscape
-rem Communications Corporation.  Portions created by Netscape are
-rem Copyright (C) 1998 Netscape Communications Corporation. All
-rem Rights Reserved.
+rem The Initial Developer of the Original Code is
+rem Netscape Communications Corporation.
+rem Portions created by the Initial Developer are Copyright (C) 1998
+rem the Initial Developer. All Rights Reserved.
 rem
-rem Contributor(s): 
+rem Contributor(s):
+rem
+rem Alternatively, the contents of this file may be used under the terms of
+rem either the GNU General Public License Version 2 or later (the "GPL"), or
+rem the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+rem in which case the provisions of the GPL or the LGPL are applicable instead
+rem of those above. If you wish to allow use of your version of this file only
+rem under the terms of either the GPL or the LGPL, and not to allow others to
+rem use your version of this file under the terms of the MPL, indicate your
+rem decision by deleting the provisions above and replace them with the notice
+rem and other provisions required by the GPL or the LGPL. If you do not delete
+rem the provisions above, a recipient may use your version of this file under
+rem the terms of any one of the MPL, the GPL or the LGPL.
+rem
+rem ***** END LICENSE BLOCK ***** */
+
 @echo on
 
 @if not exist %2\nul mkdir %2
