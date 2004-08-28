e922de549e32971a8600e2179b1e843cba5805f8
jocuri%softhome.net <jocuri%softhome.net>
2004-08-28 09:05:55 +0000

    Patch for bug 254146: make the error message clear if servertest.pl failed to find the GID for HTTPD; patch by Byron Jones (glob) <bugzilla@glob.com.au>; r=joel, a=justdave.

--- webtools/bugzilla/testserver.pl
+++ webtools/bugzilla/testserver.pl
@@ -43,7 +43,7 @@ if ($^O !~ /MSWin32/i) {
     foreach my $pscmd (@pscmds) {
         open PH, "$pscmd 2>/dev/null |";
         while (my $line = <PH>) {
-            if ($line =~ /^(?:\S*\/)?httpd\s+(\d+)$/) {
+            if ($line =~ /^(?:\S*\/)?(?:httpd|apache)\s+(\d+)$/) {
                 $sgid = $1 if $1 > $sgid;
             }
         }
@@ -76,7 +76,9 @@ Please refer to the webserver configuration section of the Bugzilla guide.
 If you are using virtual hosts or suexec, this warning may not apply.\n";
     }
 } elsif ($^O !~ /MSWin32/i) {
-   print "TEST-???? Could not identify group webserver is using.\n";
+   print
+"TEST-WARNING Failed to find the GID for the 'httpd' process, unable
+to validate webservergroup.\n";
 }
 
 
