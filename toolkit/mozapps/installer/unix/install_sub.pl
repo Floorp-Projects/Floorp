9472d28025047325e98850eca42926aaac8b5658
dbaron@dbaron.org <dbaron@dbaron.org>
2004-08-30 19:57:52 +0000

    Fix ownership and permissions in installer .tar.gz.  b=254303  r+sr=bryner

--- toolkit/mozapps/installer/unix/install_sub.pl
+++ toolkit/mozapps/installer/unix/install_sub.pl
@@ -37,7 +37,7 @@ sub BuildPlatformInstaller
     rename ("$instRoot/mozilla-installer", "$instRoot/$mainExe-installer");
     rename ("$instRoot/mozilla-installer-bin", "$instRoot/$mainExe-installer-bin");
 
-    system ("cd $gDirDistInstall/sea && tar zcvf $seiFileNameSpecific.tar.gz $mainExe-installer");
+    system ("cd $gDirDistInstall/sea && tar -zcv --owner=0 --group=0 --numeric-owner --mode='go-w' -f $seiFileNameSpecific.tar.gz $mainExe-installer");
     return 0;
 }
 
