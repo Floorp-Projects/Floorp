af3442d1b29b5c77cd3146799eb9e033a1561d57
jst%mozilla.jstenback.com <jst%mozilla.jstenback.com>
2004-08-30 19:05:11 +0000

    Fixing build bustage on BeOS (bug 257442). Patch by thesuckiestemail@yahoo.se, r+sr=jst@mozilla.org

--- modules/plugin/base/public/nptypes.h
+++ modules/plugin/base/public/nptypes.h
@@ -80,6 +80,8 @@
     #include <inttypes.h>
     #include <stdbool.h>
   #endif
+#elif defined(BEOS)
+  #include <inttypes.h>
 #else
   /*
    * For those that ship a standard C99 stdint.h header file, include
