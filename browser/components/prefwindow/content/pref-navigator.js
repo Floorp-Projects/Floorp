04ec2e5a6be894af2216b7117fd7e0ed61800d4b
p_ch%verizon.net <p_ch%verizon.net>
2004-08-31 13:38:40 +0000

    Bug 256029: OK doesn't work after changing home page and switching to another category. r=bryner

--- browser/components/prefwindow/content/pref-navigator.js
+++ browser/components/prefwindow/content/pref-navigator.js
@@ -35,7 +35,7 @@
 
 var _elementIDs = ["browserStartupHomepage", "checkForDefault"];
     
-const nsIPrefService    = Components.interfaces.nsIPrefService;
+const nsIPrefService         = Components.interfaces.nsIPrefService;
 const nsIPrefLocalizedString = Components.interfaces.nsIPrefLocalizedString;
 
 const PREFSERVICE_CONTRACTID    = "@mozilla.org/preferences-service;1";
@@ -98,10 +98,6 @@ function onOK()
     if (homeButton)
       homeButton.setAttribute("tooltiptext", parent.homepage);
   }
-
-  var shell = getShellService();
-  if (shell && "shouldBeDefaultBrowser" in parent && parent.shouldBeDefautBrowser)
-    shell.setDefaultBrowser(true, false);
 }
 
 function Startup()
@@ -119,11 +115,12 @@ function Startup()
     useButton.disabled = true;
   }
  
-#ifdef MOZ_WIDGET_GTK2
-  var shell = getShellService();
-  if (!shell)
+  try {
+    var shellSvc = Components.classes["@mozilla.org/browser/shell-service;1"]
+                             .getService(Components.interfaces.nsIShellService);
+  } catch (e) {
     document.getElementById("defaultBrowserPrefs").hidden = true;
-#endif
+  }
 
   parent.hPrefWindow.registerOKCallbackFunc(onOK);
 }
@@ -213,10 +210,11 @@ function saveFontPrefs()
   }
 }
 
-#ifdef HAVE_SHELL_SERVICE
+# this function is only reached if the shell service is defined
 function checkNow()
 {
-  var shell = getShellService();
+  var shellSvc = Components.classes["@mozilla.org/browser/shell-service;1"]
+                           .getService(Components.interfaces.nsIShellService);
   var brandBundle = document.getElementById("bundle_brand");
   var shellBundle = document.getElementById("bundle_shell");
   var brandShortName = brandBundle.getString("brandShortName");
@@ -225,7 +223,7 @@ function checkNow()
   const IPS = Components.interfaces.nsIPromptService;
   var psvc = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
                        .getService(IPS);
-  if (!shell.isDefaultBrowser(false)) {
+  if (!shellSvc.isDefaultBrowser(false)) {
     promptMessage = shellBundle.getFormattedString("setDefaultBrowserMessage", 
                                                    [brandShortName]);
     var rv = psvc.confirmEx(window, promptTitle, promptMessage, 
@@ -233,7 +231,7 @@ function checkNow()
                             (IPS.BUTTON_TITLE_NO * IPS.BUTTON_POS_1),
                             null, null, null, null, { });
     if (rv == 0)
-      shell.setDefaultBrowser(true, false);
+      shellSvc.setDefaultBrowser(true, false);
   }
   else {
     promptMessage = shellBundle.getFormattedString("alreadyDefaultBrowser",
@@ -241,7 +239,6 @@ function checkNow()
     psvc.alert(window, promptTitle, promptMessage);
   }
 }
-#endif
 
 function showLanguages()
 {
