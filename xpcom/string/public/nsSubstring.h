60809809d5c0bde3666b382545b3e2ddd84f8fce
bryner%brianryner.com <bryner%brianryner.com>
2004-09-05 22:55:34 +0000

    HPUX aCC A.01.21 can't deal with the literal string template, so disable it, at least for this compiler version.  Bug 239465, r=dbaron.

--- xpcom/string/public/nsSubstring.h
+++ xpcom/string/public/nsSubstring.h
@@ -48,7 +48,7 @@
 // If some platform(s) can't handle our template that matches literal strings,
 // then we'll disable it on those platforms.
 #ifndef NS_DISABLE_LITERAL_TEMPLATE
-#  if (defined(_MSC_VER) && (_MSC_VER < 1310)) || (defined(__SUNPRO_CC) & (__SUNPRO_CC < 0x560))
+#  if (defined(_MSC_VER) && (_MSC_VER < 1310)) || (defined(__SUNPRO_CC) & (__SUNPRO_CC < 0x560)) || (defined(__HP_aCC) && (__HP_aCC <= 012100))
 #    define NS_DISABLE_LITERAL_TEMPLATE
 #  endif
 #endif /* !NS_DISABLE_LITERAL_TEMPLATE */
