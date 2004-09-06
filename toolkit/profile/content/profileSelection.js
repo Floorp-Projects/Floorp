f29788e99314ae636aa5b40fb8331eb22efbcf17
bsmedberg@covad.net <bsmedberg@covad.net>
2004-09-06 17:08:42 +0000

    Bug 257777 - ffox profile manager shows offline mode checkbox, patch by Gavin Sharp <gavin_sharp+bugzilla@nospin.org> r=me a=asa

--- toolkit/profile/content/profileSelection.js
+++ toolkit/profile/content/profileSelection.js
@@ -117,7 +117,10 @@ function acceptDialog()
   gProfileService.startWithLastProfile = autoSelectLastProfile.checked;
   gProfileService.selectedProfile = selectedProfile.profile;
 
+  /* Bug 257777 */
+#ifndef MOZ_PHOENIX
   gProfileService.startOffline = document.getElementById("offlineState").checked;
+#end if
 
   gDialogParams.SetInt(0, 1);
 
