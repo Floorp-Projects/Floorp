19532676d4a22a8c2c9daadf253b61cc419e49ad
dbaron%dbaron.org <dbaron%dbaron.org>
2004-09-13 03:22:46 +0000

    Convert mCurrentEventContent and mCurrentEventContentStack to use nsCOMPtr/nsCOMArray.  b=258667  r+sr=jst

--- layout/html/base/src/nsPresShell.cpp
+++ layout/html/base/src/nsPresShell.cpp
@@ -72,6 +72,7 @@
 #include "prprf.h"
 #include "prinrval.h"
 #include "nsVoidArray.h"
+#include "nsCOMArray.h"
 #include "nsHashtable.h"
 #include "nsIViewObserver.h"
 #include "nsContainerFrame.h"
@@ -1393,9 +1394,9 @@ protected:
   PRPackedBool mHaveShutDown;
 
   nsIFrame*   mCurrentEventFrame;
-  nsIContent* mCurrentEventContent;
+  nsCOMPtr<nsIContent> mCurrentEventContent;
   nsVoidArray mCurrentEventFrameStack;
-  nsVoidArray mCurrentEventContentStack;
+  nsCOMArray<nsIContent> mCurrentEventContentStack;
   nsSupportsHashtable* mAnonymousContentTable;
 
 #ifdef NS_DEBUG
@@ -1641,7 +1642,7 @@ PresShell::~PresShell()
   delete mStyleSet;
   delete mFrameConstructor;
 
-  NS_IF_RELEASE(mCurrentEventContent);
+  mCurrentEventContent = nsnull;
 
   // if we allocated any stack memory free it.
   FreeDynamicStack();
@@ -3764,9 +3765,7 @@ PresShell::ClearFrameRefs(nsIFrame* aFrame)
   }
   
   if (aFrame == mCurrentEventFrame) {
-    NS_IF_RELEASE(mCurrentEventContent);
     mCurrentEventContent = aFrame->GetContent();
-    NS_IF_ADDREF(mCurrentEventContent);
     mCurrentEventFrame = nsnull;
   }
 
@@ -3775,8 +3774,7 @@ PresShell::ClearFrameRefs(nsIFrame* aFrame)
       //One of our stack frames was deleted.  Get its content so that when we
       //pop it we can still get its new frame from its content
       nsIContent *currentEventContent = aFrame->GetContent();
-      NS_IF_ADDREF(currentEventContent);
-      mCurrentEventContentStack.ReplaceElementAt((void*)currentEventContent, i);
+      mCurrentEventContentStack.ReplaceObjectAt(currentEventContent, i);
       mCurrentEventFrameStack.ReplaceElementAt(nsnull, i);
     }
   }
@@ -5491,24 +5489,23 @@ PresShell::PushCurrentEventInfo(nsIFrame* aFrame, nsIContent* aContent)
 {
   if (mCurrentEventFrame || mCurrentEventContent) {
     mCurrentEventFrameStack.InsertElementAt((void*)mCurrentEventFrame, 0);
-    mCurrentEventContentStack.InsertElementAt((void*)mCurrentEventContent, 0);
+    mCurrentEventContentStack.InsertObjectAt(mCurrentEventContent, 0);
   }
   mCurrentEventFrame = aFrame;
   mCurrentEventContent = aContent;
-  NS_IF_ADDREF(aContent);
 }
 
 void
 PresShell::PopCurrentEventInfo()
 {
   mCurrentEventFrame = nsnull;
-  NS_IF_RELEASE(mCurrentEventContent);
+  mCurrentEventContent = nsnull;
 
   if (0 != mCurrentEventFrameStack.Count()) {
     mCurrentEventFrame = (nsIFrame*)mCurrentEventFrameStack.ElementAt(0);
     mCurrentEventFrameStack.RemoveElementAt(0);
-    mCurrentEventContent = (nsIContent*)mCurrentEventContentStack.ElementAt(0);
-    mCurrentEventContentStack.RemoveElementAt(0);
+    mCurrentEventContent = mCurrentEventContentStack.ObjectAt(0);
+    mCurrentEventContentStack.RemoveObjectAt(0);
   }
 }
 
@@ -5651,8 +5648,7 @@ PresShell::HandleEvent(nsIView         *aView,
 
       nsIEventStateManager *esm = mPresContext->EventStateManager();
 
-      NS_IF_RELEASE(mCurrentEventContent);
-      esm->GetFocusedContent(&mCurrentEventContent);
+      esm->GetFocusedContent(getter_AddRefs(mCurrentEventContent));
 
       esm->GetFocusedFrame(&mCurrentEventFrame);
       if (!mCurrentEventFrame) {
@@ -5678,7 +5674,7 @@ PresShell::HandleEvent(nsIView         *aView,
                 focusController->GetFocusedElement(getter_AddRefs(focusedElement));
                 if (focusedElement) {
                   // get mCurrentEventContent from focusedElement
-                  CallQueryInterface(focusedElement, &mCurrentEventContent);
+                  mCurrentEventContent = do_QueryInterface(focusedElement);
                 }
               }
             }
@@ -5686,7 +5682,7 @@ PresShell::HandleEvent(nsIView         *aView,
         }
 #endif /* defined(MOZ_X11) */
         if (!mCurrentEventContent) {
-          NS_IF_ADDREF(mCurrentEventContent = mDocument->GetRootContent());
+          mCurrentEventContent = mDocument->GetRootContent();
         }
         mCurrentEventFrame = nsnull; // XXXldb Isn't it already?
       }
@@ -5774,12 +5770,10 @@ PresShell::HandleEvent(nsIView         *aView,
 
           // If we found an element, target it.  Otherwise, target *nothing*.
           if (!targetElement) {
-            NS_IF_RELEASE(mCurrentEventContent);
+            mCurrentEventContent = nsnull;
             mCurrentEventFrame = nsnull;
           } else if (targetElement != mCurrentEventContent) {
-            NS_IF_RELEASE(mCurrentEventContent);
             mCurrentEventContent = targetElement;
-            NS_ADDREF(mCurrentEventContent);
           }
         }
       }
