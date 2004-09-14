163e105f1f0d67a636fc9e13bfb675ae1a1a33d8
bryner%brianryner.com <bryner%brianryner.com>
2004-09-14 20:43:45 +0000

    Bug 245653 - don't try to use dogbert migrator on BeOS.  Patch by thesuckiestemail@yahoo.se, r=sergei_d@fi.tartu.ee.

--- browser/components/build/nsModule.cpp
+++ browser/components/build/nsModule.cpp
@@ -48,7 +48,9 @@
 #include "nsGNOMEShellService.h"
 #endif
 #include "nsProfileMigrator.h"
+#if !defined(XP_BEOS)
 #include "nsDogbertProfileMigrator.h"
+#endif
 #if !defined(XP_OS2)
 #include "nsOperaProfileMigrator.h"
 #endif
@@ -74,8 +76,9 @@ NS_GENERIC_FACTORY_CONSTRUCTOR(nsMacShellService)
 #elif defined(MOZ_WIDGET_GTK2)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsGNOMEShellService, Init)
 #endif
-
+#if !defined(XP_BEOS)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsDogbertProfileMigrator)
+#endif
 #if !defined(XP_OS2)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsOperaProfileMigrator)
 #endif
@@ -164,6 +167,13 @@ static const nsModuleComponentInfo components[] =
     nsOperaProfileMigratorConstructor },
 #endif
 
+#if !defined(XP_BEOS)
+  { "Netscape 4.x Profile Migrator",
+    NS_DOGBERTPROFILEMIGRATOR_CID,
+    NS_BROWSERPROFILEMIGRATOR_CONTRACTID_PREFIX "dogbert",
+    nsDogbertProfileMigratorConstructor },
+#endif
+
   { "Phoenix Profile Migrator",
     NS_PHOENIXPROFILEMIGRATOR_CID,
     NS_BROWSERPROFILEMIGRATOR_CONTRACTID_PREFIX "phoenix",
@@ -172,12 +182,8 @@ static const nsModuleComponentInfo components[] =
   { "Seamonkey Profile Migrator",
     NS_SEAMONKEYPROFILEMIGRATOR_CID,
     NS_BROWSERPROFILEMIGRATOR_CONTRACTID_PREFIX "seamonkey",
-    nsSeamonkeyProfileMigratorConstructor },
+    nsSeamonkeyProfileMigratorConstructor }
 
-  { "Netscape 4.x Profile Migrator",
-    NS_DOGBERTPROFILEMIGRATOR_CID,
-    NS_BROWSERPROFILEMIGRATOR_CONTRACTID_PREFIX "dogbert",
-    nsDogbertProfileMigratorConstructor }
 
 };
 
