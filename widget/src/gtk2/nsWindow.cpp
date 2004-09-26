ae1f1fb9f72ad3d92e540ed930e4a33578d4dbce
bzbarsky%mit.edu <bzbarsky%mit.edu>
2004-09-26 18:13:02 +0000

    Fix context menu keyboard shortcut on Linux.  Bug 261043, patch by Kyle Yuan <kyle.yuan@sun.com>, r=robin.lu, sr=bryner, a=dbaron

--- widget/src/gtk2/nsWindow.cpp
+++ widget/src/gtk2/nsWindow.cpp
@@ -4003,6 +4003,7 @@ key_event_to_context_menu_event(const nsKeyEvent* aKeyEvent,
                                 nsMouseEvent* aCMEvent)
 {
     memcpy(aCMEvent, aKeyEvent, sizeof(nsInputEvent));
+    aCMEvent->eventStructType = NS_MOUSE_EVENT;
     aCMEvent->message = NS_CONTEXTMENU_KEY;
     aCMEvent->isShift = aCMEvent->isControl = PR_FALSE;
     aCMEvent->isAlt = aCMEvent->isMeta = PR_FALSE;
