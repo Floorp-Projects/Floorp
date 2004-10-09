221a4aeda78dbfe33462c49194304b4f7a7156f4
bmlk@gmx.de <bmlk@gmx.de>
2004-10-09 12:04:52 +0000

    reorganize CalCDominantBorder, reduce the number of arguments, change the border structure. make the rules handling more evident, bug 229883, With more than a little help from my friends (fantasai, boris) r/sr=bzbarsky

--- layout/base/nsStyleConsts.h
+++ layout/base/nsStyleConsts.h
@@ -250,7 +250,7 @@
 #define NS_STYLE_BORDER_STYLE_BG_SOLID          12
 // a bit ORed onto the style for table border collapsing indicating that the style was 
 // derived from a table with its rules attribute set
-#define NS_STYLE_BORDER_STYLE_RULES_MASK      0x10  
+#define NS_STYLE_BORDER_STYLE_RULES_MARKER      0x10  
 
 // See nsStyleDisplay
 #define NS_STYLE_CLEAR_NONE                     0
