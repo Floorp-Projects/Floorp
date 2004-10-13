9e8e359ed56c415a803d0d82b0e1c76b9155dfa4
zack@kde.org <zack@kde.org>
2004-10-13 21:01:24 +0000

    New approach, better approach, popups disappearing after a click, just cooler... Like it still won't get me chicks but is closer to that goal (i got into mozilla because of false promises of women)

---
+++ widget/src/qt/mozqwidget.h
@@ -0,0 +1,21 @@
+#ifndef MOZQWIDGET_H
+#define MOZQWIDGET_H
+
+#include <qwidget.h>
+
+class QEvent;
+class nsCommonWidget;
+
+class MozQWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    MozQWidget(nsCommonWidget *receiver, QWidget *parent,
+               const char *name, WFlags f);
+protected:
+    virtual bool event(QEvent *ev);
+private:
+    nsCommonWidget *mReceiver;
+};
+
+#endif
