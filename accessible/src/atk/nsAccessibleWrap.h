f9dc6bdf0eae2246b1a6ad95134206bfc64de3d2
bryner%brianryner.com <bryner%brianryner.com>
2004-10-14 02:31:58 +0000

    Use GType as the return type for GetMaiAtkType since it is not the same as PRUint32 on all architectures.  Bug 256603, patch by pkwarren@gmail.com, r=Louie.Zhao@sun.com, sr=dbaron.

--- accessible/src/atk/nsAccessibleWrap.h
+++ accessible/src/atk/nsAccessibleWrap.h
@@ -91,11 +91,6 @@ protected:
 private:
     nsresult CreateMaiInterfaces(void);
     nsresult AddMaiInterface(MaiInterface *aMaiIface);
-    PRUint32 GetMaiAtkType(void);
-
-    static const char * GetUniqueMaiAtkTypeName(void);
-    static PRUint32 mAtkTypeNameIndex;
-
 
     MaiInterface **mInterfaces;
     PRUint32 mInterfaceCount;
