49287c937ec74ade31957b6b8b31e7c67b87ae50
cltbld <cltbld>
2004-10-18 21:57:15 +0000

    save a copy of the dated dir that was copied to ftp server and remove any previously saved builds older than 7 days (granrose)

--- tools/tinderbox/post-mozilla-rel.pl
+++ tools/tinderbox/post-mozilla-rel.pl
@@ -648,8 +648,11 @@ sub main {
     return returnStatus("Pushing package $upload_directory failed", ("testfailed"));
   }
 
-
   if ($cachebuild) { 
+    # remove saved builds older than a week and save current build
+    TinderUtils::run_shell_command "find $mozilla_build_dir -type d -name \"200*-*\" -prune -mtime +7 -print | xargs rm -rf";
+    TinderUtils::run_shell_command "cp -rdpf $upload_directory $mozilla_build_dir/$datestamp";
+
     unlink "last-built";
     open BLAH, ">last-built"; 
     close BLAH;
