d2f5d85f1ab30676ff4278161c89ed471987ac0d
psychoticwolf%carolina.rr.com <psychoticwolf%carolina.rr.com>
2004-10-29 11:54:52 +0000

    Remove OS Manager. OSes are mostly static, DB can be edited directly to add new ones.

--- webtools/update/developers/inc_sidebar.php
+++ webtools/update/developers/inc_sidebar.php
@@ -7,21 +7,23 @@ if ($_SESSION["level"] == "user") {
 <li><A HREF="usermanager.php">Your Profile</A></li>
 
 <?php
-} else if ($_SESSION["level"] == "moderator") {
+} else if ($_SESSION["level"] == "editor") {
 ?>
-<li><A HREF="usermanager.php">Your Profile</A></li>
+<li><A HREF="usermanager.php?function=edituser&userid=<?php echo"$_SESSION[uid]"; ?>">Your Profile</A></li>
+<li><A HREF="approval.php">Approval Queue</A></li>
 <li><A HREF="listmanager.php?type=T">Themes list</A></li>
 <li><A HREF="listmanager.php?type=E">Extensions list</A></li>
+<li><A HREF="usermanager.php">Users Manager</A></li>
 <?php
 } else {
 ?>
 <li><A HREF="usermanager.php?function=edituser&userid=<?php echo"$_SESSION[uid]"; ?>">Your Profile</A></li>
+<li><A HREF="approval.php">Approval Queue</A></li>
 <li><A HREF="listmanager.php?type=T">Themes list</A></li>
 <li><A HREF="listmanager.php?type=E">Extensions list</A></li>
 <li><A HREF="usermanager.php">Users Manager</A></li>
 <li><a href="appmanager.php">Application Manager</a></li>
 <li><a href="categorymanager.php">Category Manager</A></li>
-<li><a href="osmanager.php">OS Manager</a></li>
 <li><a href="faqmanager.php">FAQ Manager</A></li>
 <?php } ?>
 <li><a href="logout.php">Logout</A></li>
