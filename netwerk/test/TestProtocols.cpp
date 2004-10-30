117511a6aa330ad0744db7924b6f013a5254d75c
bsmedberg%covad.net <bsmedberg%covad.net>
2004-10-30 04:30:48 +0000

    Revert timeless' incorrect bustage-fix.

--- netwerk/test/TestProtocols.cpp
+++ netwerk/test/TestProtocols.cpp
@@ -476,7 +476,7 @@ InputTestConsumer::OnDataAvailable(nsIRequest *request,
 
 PR_STATIC_CALLBACK(void) DecrementDestroyHandler(PLEvent *self) 
 {
-    PR_DELETE(((void*)self));
+    PR_DELETE(self);
 }
 
 
