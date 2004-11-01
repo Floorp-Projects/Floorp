69bd85dbcefb6254419a251681d767ff68e84295
psychoticwolf%carolina.rr.com <psychoticwolf%carolina.rr.com>
2004-11-01 17:14:39 +0000

    Finish it before checking it in... Bug fixes for Approval Queue file downloader.

--- webtools/update/developers/approvalfile.php
+++ webtools/update/developers/approvalfile.php
@@ -1,5 +1,18 @@
 <?php
+require"core/sessionconfig.php";
 require"../core/config.php";
+
+if ($_SESSION["level"]=="admin" or $_SESSION["level"]=="editor") {
+    //Do Nothing, they're good. :-)
+} else {
+    echo"<h1>Access Denied</h1>\n";
+    echo"You do not have access to the Approval Queue.";
+    exit;
+}
+
+
+
+
 $filename = basename($_SERVER["PATH_INFO"]);
 $file = "$repositorypath/approval/$filename";
 header('Content-Description: File Transfer');
