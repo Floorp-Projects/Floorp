30aadb249fc8814c1ec7d216c1ea8f31aad10f0e
darin@meer.net <darin@meer.net>
2004-11-06 01:35:01 +0000

    fixes bug 267263, r=cneberg sr=dveditz

--- netwerk/protocol/http/src/nsHttpTransaction.cpp
+++ netwerk/protocol/http/src/nsHttpTransaction.cpp
@@ -140,6 +140,7 @@ nsHttpTransaction::nsHttpTransaction()
     , mReceivedData(PR_FALSE)
     , mStatusEventPending(PR_FALSE)
     , mHasRequestBody(PR_FALSE)
+    , mSSLConnectFailed(PR_FALSE)
 {
     LOG(("Creating nsHttpTransaction @%x\n", this));
 }
