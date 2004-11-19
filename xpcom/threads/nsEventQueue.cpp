6fbd0918080fcc8e17bb5dea74fa48e46a6ed500
darin@meer.net <darin@meer.net>
2004-11-19 19:06:58 +0000

    alternate fix for bug 234620 "Unknown random SEGV/seg fault/core dumps/crashes, only thing on is Mail/IMAP" in an attempt to avoid crash bug 269585, r=dbaron sr=bienvenu a=asa

--- xpcom/threads/nsEventQueue.cpp
+++ xpcom/threads/nsEventQueue.cpp
@@ -219,9 +219,13 @@ nsEventQueueImpl::StopAcceptingEvents()
 void
 nsEventQueueImpl::NotifyObservers(const char *aTopic)
 {
-  // we must not invoke the observer service from a background thread!
-  if (!nsIThread::IsMainThread())
+  // only send out this notification for native event queues
+  if (!PL_IsQueueNative(mEventQueue))
     return;
+
+  // we should not call out to the observer service from background threads!
+  NS_ASSERTION(nsIThread::IsMainThread(),
+               "Native event queues should only be used on the main thread");
 
   nsresult rv;
   nsCOMPtr<nsIObserverService> os = do_GetService("@mozilla.org/observer-service;1", &rv);
