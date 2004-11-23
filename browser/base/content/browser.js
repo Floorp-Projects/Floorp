36264e9bdeb3b5fc81a07fca6174029da0b5ff87
timeless%mozdev.org <timeless%mozdev.org>
2004-11-23 20:40:23 +0000

    Bug 221161 customize domain endings (TLDs) for ctrl+enter etc completions of the URL bar patch by hassman@czilla.cz r=varga sr=shaver

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1552,7 +1552,16 @@ function canonizeUrl(aTriggeringEvent, aPostDataRef)
 
     else if (aTriggeringEvent && 'ctrlKey' in aTriggeringEvent &&
         aTriggeringEvent.ctrlKey)
-      suffix = ".com/";
+    {
+      try {
+        suffix = gPrefService.getCharPref("browser.fixup.alternate.suffix");
+        if (suffix.charAt(suffix.length - 1) != '/')
+          suffix += "/";
+      }
+      catch(e) {
+        suffix = ".com/";
+      }
+    }
 
     else if (aTriggeringEvent && 'shiftKey' in aTriggeringEvent &&
         aTriggeringEvent.shiftKey)
