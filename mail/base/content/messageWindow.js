dc9ef46d02c50842d3237abb547d82fa0408e742
bienvenu%nventure.com <bienvenu%nventure.com>
2004-12-01 17:44:10 +0000

    fix 271717 opening messages from search results sometimes doesn't work if original folder is threaded, sr=mscott

--- mail/base/content/messageWindow.js
+++ mail/base/content/messageWindow.js
@@ -315,9 +315,9 @@ function OnLoadMessageWindowDelayed(loadCustomMessage)
   if (loadCustomMessage)
     gDBView.loadMessageByUrl(gCurrentMessageUri);
   else
-{
-  var msgKey = extractMsgKeyFromURI(gCurrentMessageUri); 
-  gDBView.loadMessageByMsgKey(msgKey); 
+  {
+    var msgKey = extractMsgKeyFromURI(gCurrentMessageUri); 
+    LoadMessageByViewIndex(gDBView.findIndexFromKey(msgKey, true));
   }
   gNextMessageViewIndexAfterDelete = gDBView.msgToSelectAfterDelete; 
   UpdateStandAloneMessageCounts();
@@ -1111,10 +1111,7 @@ function GetDBView()
 
 function LoadMessageByMsgKey(messageKey)
 {
-  gDBView.loadMessageByMsgKey(messageKey);
-  // we only want to update the toolbar if there was no previous selected message.
-  if (nsMsgKey_None == gDBView.keyForFirstSelectedMessage)
-    UpdateMailToolbar("update toolbar for message Window");
+  LoadMessageByViewIndex(gDBView.findIndexFromKey(messageKey, true));
 }
 
 function LoadMessageByViewIndex(viewIndex)
