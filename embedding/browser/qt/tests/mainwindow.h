c42dbda58c8a16f18bc19ced430263b83def388d
zack%kde.org <zack%kde.org>
2004-12-06 02:50:30 +0000

    making the embedding example actually do something nice

--- embedding/browser/qt/tests/mainwindow.h
+++ embedding/browser/qt/tests/mainwindow.h
@@ -4,6 +4,7 @@
 #include <qmainwindow.h>
 
 class QGeckoEmbed;
+class QLineEdit;
 
 class MyMainWindow : public QMainWindow
 {
@@ -14,8 +15,20 @@ public:
 public slots:
     void fileOpen();
     void startURIOpen(const QString &, bool &);
+    void changeLocation();
+    void goBack();
+    void goForward();
+    void stop();
+
 public:
     QGeckoEmbed *qecko;
+
+private slots:
+    void slotProgress(int, int);
+    void slotProgress(const QString &, int, int);
+
+private:
+    QLineEdit *location;
 };
 
 #endif
