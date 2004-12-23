8302bf088acba21ac67f7e6e9b1254f6bf97acbd
psychoticwolf%carolina.rr.com <psychoticwolf%carolina.rr.com>
2004-12-23 18:10:37 +0000

    Extensions should be above themes on index. Bug 275829.

--- webtools/update/index.php
+++ webtools/update/index.php
@@ -85,10 +85,10 @@ if ($securitywarning=="true") {
     $uriparams_skip="application";
     ?>
 	<dl>
-		<dt>Themes</dt>
-		<dd>Themes allow you to change the way your Mozilla program looks. New graphics and colors. Browse themes for: <a href="/themes/?<?php echo"".uriparams()."&amp;"; ?>application=firefox">Firefox</a>, <a href="/themes/?<?php echo"".uriparams()."&amp;"; ?>application=thunderbird">Thunderbird</a>, <a href="/themes/?<?php echo"".uriparams()."&amp;"; ?>application=mozilla">Mozilla&nbsp;Suite</a></dd>
 		<dt>Extensions</dt>
 		<dd>Extensions are small add-ons that add new functionality to your Mozilla program. They can add anything from a toolbar button to a completely new feature. Browse extensions for: <a href="/extensions/?<?php echo"".uriparams()."&amp;"; ?>application=firefox">Firefox</a>, <a href="/extensions/?<?php echo"".uriparams()."&amp;"; ?>application=thunderbird">Thunderbird</a>, <a href="/extensions/?<?php echo"".uriparams()."&amp;"; ?>application=mozilla">Mozilla&nbsp;Suite</a></dd>
+		<dt>Themes</dt>
+		<dd>Themes allow you to change the way your Mozilla program looks. New graphics and colors. Browse themes for: <a href="/themes/?<?php echo"".uriparams()."&amp;"; ?>application=firefox">Firefox</a>, <a href="/themes/?<?php echo"".uriparams()."&amp;"; ?>application=thunderbird">Thunderbird</a>, <a href="/themes/?<?php echo"".uriparams()."&amp;"; ?>application=mozilla">Mozilla&nbsp;Suite</a></dd>
 		<dt>Plugins</dt>
 		<dd>Plugins are programs that allow websites to provide content to you and have it appear in your browser. Examples of Plugins are Flash, RealPlayer, and Java. Browse plug-ins for <a href="/plugins/">Mozilla Suite and Firefox</a></dd>
 		<!--<dt>Search Engines</dt>
@@ -133,47 +133,6 @@ if ($securitywarning=="true") {
     <?php } ?>
 	</div>
 	<div id="side" class="right">
-	<h2>Most Popular <?php echo ucwords($application); ?> Themes</h2>
-	<ol class="popularlist">
-
-        <?php
-        $i=0;
-        $sql = "SELECT TM.ID, TV.vID,TM.Name, TV.Version, TM.TotalDownloads, TM.downloadcount
-            FROM  `main` TM
-            INNER  JOIN version TV ON TM.ID = TV.ID
-            INNER  JOIN applications TA ON TV.AppID = TA.AppID
-            INNER  JOIN os TOS ON TV.OSID = TOS.OSID
-            WHERE  `Type`  =  'T' AND `AppName` = '$application' AND `minAppVer_int` <='$currentver' AND `maxAppVer_int` >= '$currentver' AND (`OSName` = '$OS' OR `OSName` = 'ALL') AND `downloadcount` > '0' AND `approved` = 'YES' ORDER BY `downloadcount` DESC ";
-        $sql_result = mysql_query($sql, $connection) or trigger_error("MySQL Error ".mysql_errno().": ".mysql_error()."", E_USER_NOTICE);
-            if (mysql_num_rows($sql_result)=="0") {
-                echo"        <li>No Popular Themes</li>\n";
-            }
-            while ($row = mysql_fetch_array($sql_result)) {
-                $i++;
-                $id = $row["ID"];
-                $vid = $row["vID"];
-                $name = $row["Name"];
-                $version = $row["Version"];
-                $downloadcount = $row["downloadcount"];
-                $totaldownloads = $row["TotalDownloads"];
-                $typename="themes";
-                if ($lastname == $name) {
-                    $i--;
-                    continue;
-                }
-
-                echo"		<li>";
-                echo"<a href=\"/$typename/moreinfo.php?".uriparams()."&amp;id=$id\">$name</a>";
-                echo"<span class=\"downloads\"> ($downloadcount downloads)</span>";
-                echo"</li>\n";
-
-                $lastname = $name;
-                if ($i >= "5") {
-                    break;
-                }
-            }
-        ?>
-	</ol>
 	<h2>Most Popular <?php echo ucwords($application); ?> Extensions</h2>
 	<ol class="popularlist">
 
@@ -216,6 +175,48 @@ if ($securitywarning=="true") {
         ?>
 	</ol>
 	<a href="/rss/?application=<?php echo"$application"; ?>&amp;list=newest"><img src="images/rss.png" width="16" height="16" class="rss" alt="News Additions in RSS"></a>
+	<h2>Most Popular <?php echo ucwords($application); ?> Themes</h2>
+	<ol class="popularlist">
+
+        <?php
+        $i=0;
+        $sql = "SELECT TM.ID, TV.vID,TM.Name, TV.Version, TM.TotalDownloads, TM.downloadcount
+            FROM  `main` TM
+            INNER  JOIN version TV ON TM.ID = TV.ID
+            INNER  JOIN applications TA ON TV.AppID = TA.AppID
+            INNER  JOIN os TOS ON TV.OSID = TOS.OSID
+            WHERE  `Type`  =  'T' AND `AppName` = '$application' AND `minAppVer_int` <='$currentver' AND `maxAppVer_int` >= '$currentver' AND (`OSName` = '$OS' OR `OSName` = 'ALL') AND `downloadcount` > '0' AND `approved` = 'YES' ORDER BY `downloadcount` DESC ";
+        $sql_result = mysql_query($sql, $connection) or trigger_error("MySQL Error ".mysql_errno().": ".mysql_error()."", E_USER_NOTICE);
+            if (mysql_num_rows($sql_result)=="0") {
+                echo"        <li>No Popular Themes</li>\n";
+            }
+            while ($row = mysql_fetch_array($sql_result)) {
+                $i++;
+                $id = $row["ID"];
+                $vid = $row["vID"];
+                $name = $row["Name"];
+                $version = $row["Version"];
+                $downloadcount = $row["downloadcount"];
+                $totaldownloads = $row["TotalDownloads"];
+                $typename="themes";
+                if ($lastname == $name) {
+                    $i--;
+                    continue;
+                }
+
+                echo"		<li>";
+                echo"<a href=\"/$typename/moreinfo.php?".uriparams()."&amp;id=$id\">$name</a>";
+                echo"<span class=\"downloads\"> ($downloadcount downloads)</span>";
+                echo"</li>\n";
+
+                $lastname = $name;
+                if ($i >= "5") {
+                    break;
+                }
+            }
+        ?>
+	</ol>
+
 	<h2>New Additions</h2>
 	<ol class="popularlist">
 
