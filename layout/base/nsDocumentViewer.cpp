d1da171a4f4737cb7902389b650c790a3fe27d75
dbaron%dbaron.org <dbaron%dbaron.org>
2004-12-24 22:27:57 +0000

    Restore old code that prevents printing or previewing XUL documents, since it still doesn't work.  b=240490  r+sr=roc

--- layout/base/nsDocumentViewer.cpp
+++ layout/base/nsDocumentViewer.cpp
@@ -109,7 +109,7 @@
 #include "nsCopySupport.h"
 #include "nsIDOMHTMLFrameSetElement.h"
 #ifdef MOZ_XUL
-#include "nsIXULDocument.h"  // Temporary code for Bug 136185
+#include "nsIXULDocument.h"
 #endif
 
 #include "nsIClipboardHelper.h"
@@ -3028,6 +3028,15 @@ DocumentViewerImpl::Print(nsIPrintSettings*       aPrintSettings,
 #ifdef NS_PRINTING
   INIT_RUNTIME_ERROR_CHECKING();
 
+#ifdef MOZ_XUL
+  // Temporary code for Bug 136185 / Bug 240490
+  nsCOMPtr<nsIXULDocument> xulDoc(do_QueryInterface(mDocument));
+  if (xulDoc) {
+    nsPrintEngine::ShowPrintErrorDialog(NS_ERROR_GFX_PRINTER_NO_XUL);
+    return NS_ERROR_FAILURE;
+  }
+#endif
+
   nsCOMPtr<nsIDocShell> docShell(do_QueryInterface(mContainer));
   NS_ASSERTION(docShell, "This has to be a docshell");
 
@@ -3119,6 +3128,16 @@ DocumentViewerImpl::PrintPreview(nsIPrintSettings* aPrintSettings,
     nsPrintEngine::CloseProgressDialog(aWebProgressListener);
     return NS_ERROR_FAILURE;
   }
+
+#ifdef MOZ_XUL
+  // Temporary code for Bug 136185 / Bug 240490
+  nsCOMPtr<nsIXULDocument> xulDoc(do_QueryInterface(mDocument));
+  if (xulDoc) {
+    nsPrintEngine::CloseProgressDialog(aWebProgressListener);
+    nsPrintEngine::ShowPrintErrorDialog(NS_ERROR_GFX_PRINTER_NO_XUL, PR_FALSE);
+    return NS_ERROR_FAILURE;
+  }
+#endif
 
   if (!mPrintEngine) {
     mPrintEngine = new nsPrintEngine();
