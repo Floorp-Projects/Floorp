3cc5699f9685b2c5da9b99fcb18062148c4f992a
scott%scott-macgregor.org <scott%scott-macgregor.org>
2005-01-07 01:29:20 +0000

    Work of fixing Bug #276918 --> Thunderbird installer and zip builds are DOA after the aviary landing to mozilla\toolkit with toolkit entity errors.
    Locale restructuring to match work done on the aviary 1.0 branch.

--- mail/app/profile/all-thunderbird.js
+++ mail/app/profile/all-thunderbird.js
@@ -39,7 +39,8 @@ pref("general.useragent.vendor", "Thunderbird");
 pref("general.useragent.vendorSub",
 #expand __APP_VERSION__
 );
-pref("general.useragent.locale", "en-US");
+
+#expand pref("general.useragent.locale", "__AB_CD__");
 pref("general.skins.selectedSkin", "classic/1.0");
 
 pref("app.extensions.version", "0.6");
