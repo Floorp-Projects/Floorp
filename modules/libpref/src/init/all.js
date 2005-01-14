21b5b07d9fa063afdb00780bd9bbabf29ce4e146
dwitte@stanford.edu <dwitte@stanford.edu>
2005-01-14 03:24:13 +0000

    revert default cookie pref from 'use p3p' to 'accept all'. b=225287, r=mkaply, sr=dveditz

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -632,7 +632,7 @@ pref("network.proxy.socks_remote_dns",      false);
 pref("network.proxy.no_proxies_on",         "localhost, 127.0.0.1");
 pref("network.proxy.failover_timeout",      1800); // 30 minutes
 pref("network.online",                      true); //online/offline
-pref("network.cookie.cookieBehavior",       3); // 0-Accept, 1-dontAcceptForeign, 2-dontUse, 3-p3p
+pref("network.cookie.cookieBehavior",       0); // 0-Accept, 1-dontAcceptForeign, 2-dontUse, 3-p3p
 pref("network.cookie.disableCookieForMailNews", true); // disable all cookies for mail
 pref("network.cookie.lifetimePolicy",       0); // accept normally, 1-askBeforeAccepting, 2-acceptForSession,3-acceptForNDays
 pref("network.cookie.alwaysAcceptSessionCookies", false);
