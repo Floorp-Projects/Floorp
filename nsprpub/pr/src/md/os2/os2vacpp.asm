e60531b5f27217f38af2bdd86ab0a52ba3eea78b
gerv@gerv.net <gerv@gerv.net>
2005-02-02 22:28:27 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license.

--- nsprpub/pr/src/md/os2/os2vacpp.asm
+++ nsprpub/pr/src/md/os2/os2vacpp.asm
@@ -1,78 +1,51 @@
-COMMENT | -*- Mode: asm; tab-width: 8; c-basic-offset: 4 -*-
- ***** BEGIN LICENSE BLOCK *****
- Version: MPL 1.1/GPL 2.0/LGPL 2.1
+; -*- Mode: asm; tab-width: 8; c-basic-offset: 4 -*-
 
- The contents of this file are subject to the Mozilla Public License Version
- 1.1 (the "License"); you may not use this file except in compliance with
- the License. You may obtain a copy of the License at
- http://www.mozilla.org/MPL/
+; ***** BEGIN LICENSE BLOCK *****
+; Version: MPL 1.1/GPL 2.0/LGPL 2.1
+;
+; The contents of this file are subject to the Mozilla Public License Version
+; 1.1 (the "License"); you may not use this file except in compliance with
+; the License. You may obtain a copy of the License at
+; http://www.mozilla.org/MPL/
+;
+; Software distributed under the License is distributed on an "AS IS" basis,
+; WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+; for the specific language governing rights and limitations under the
+; License.
+;
+; The Original Code is the NetScape Portable Runtime (NSPR).
+;
+; The Initial Developer of the Original Code is
+; IBM Corporation.
+; Portions created by the Initial Developer are Copyright (C) 2001
+; the Initial Developer. All Rights Reserved.
+;
+; Contributor(s):
+;
+; Alternatively, the contents of this file may be used under the terms of
+; either the GNU General Public License Version 2 or later (the "GPL"), or
+; the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+; in which case the provisions of the GPL or the LGPL are applicable instead
+; of those above. If you wish to allow use of your version of this file only
+; under the terms of either the GPL or the LGPL, and not to allow others to
+; use your version of this file under the terms of the MPL, indicate your
+; decision by deleting the provisions above and replace them with the notice
+; and other provisions required by the GPL or the LGPL. If you do not delete
+; the provisions above, a recipient may use your version of this file under
+; the terms of any one of the MPL, the GPL or the LGPL.
+;
+; ***** END LICENSE BLOCK *****
 
- Software distributed under the License is distributed on an "AS IS" basis,
- WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- for the specific language governing rights and limitations under the
- License.
+;        Windows uses inline assembly for their atomic functions, so we have
+;        created an assembly file for VACPP on OS/2.
+;
+;        This assembly file also contains an implementation of RAM semaphores.
+;
+;        Notes:
+;          The ulTIDPID element of the RAMSEM structure is overloaded in the 386
+;          implementation to hold the TID:PID in the lower 31 bits and the lock
+;          bit in the high bit
 
- The Original Code is mozilla.org Code.
-
- The Initial Developer of the Original Code is
- Netscape Communications Corporation.
- Portions created by the Initial Developer are Copyright (C) 2001
- the Initial Developer. All Rights Reserved.
-
- Contributor(s):
-
- Alternatively, the contents of this file may be used under the terms of
- either the GNU General Public License Version 2 or later (the "GPL"), or
- the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- in which case the provisions of the GPL or the LGPL are applicable instead
- of those above. If you wish to allow use of your version of this file only
- under the terms of either the GPL or the LGPL, and not to allow others to
- use your version of this file under the terms of the MPL, indicate your
- decision by deleting the provisions above and replace them with the notice
- and other provisions required by the GPL or the LGPL. If you do not delete
- the provisions above, a recipient may use your version of this file under
- the terms of any one of the MPL, the GPL or the LGPL.
-
- ***** END LICENSE BLOCK *****
-        License Version 1.1 (the "License"); you may not use this file
-        except in compliance with the License. You may obtain a copy of
-        the License at http://www.mozilla.org/MPL/
-
-        Software distributed under the License is distributed on an "AS
-        IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-        implied. See the License for the specific language governing
-        rights and limitations under the License.
-
-        The Original Code is the Netscape Portable Runtime (NSPR).
-
-        The Initial Developer of the Original Code is IBM Corporation.
-        Portions created by IBM are Copyright (C) 2001 IBM Corporation.
-        All Rights Reserved.
-
-        Contributor(s):
-
-        Alternatively, the contents of this file may be used under the
-        terms of the GNU General Public License Version 2 or later (the
-        "GPL"), in which case the provisions of the GPL are applicable 
-        instead of those above.  If you wish to allow use of your 
-        version of this file only under the terms of the GPL and not to
-        allow others to use your version of this file under the MPL,
-        indicate your decision by deleting the provisions above and
-        replace them with the notice and other provisions required by
-        the GPL.  If you do not delete the provisions above, a recipient
-        may use your version of this file under either the MPL or the
-        GPL.
-
-        Windows uses inline assembly for their atomic functions, so we have
-        created an assembly file for VACPP on OS/2.
-
-        This assembly file also contains an implementation of RAM semaphores.
-
-        Notes:
-          The ulTIDPID element of the RAMSEM structure is overloaded in the 386
-          implementation to hold the TID:PID in the lower 31 bits and the lock
-          bit in the high bit
-        |
         page ,132
 
         .486P
