70dc80657ef9b74ea1ad951c2125ca8107a9a02c
gerv%gerv.net <gerv%gerv.net>
2005-02-13 00:07:56 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license.

--- js/tests/js1_5/Scope/scope-001.js
+++ js/tests/js1_5/Scope/scope-001.js
@@ -1,35 +1,39 @@
-/*
- * The contents of this file are subject to the Netscape Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/NPL/
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
  * The Original Code is mozilla.org code.
  *
- * The Initial Developer of the Original Code is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation. All
- * Rights Reserved.
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1998
+ * the Initial Developer. All Rights Reserved.
  *
- * Contributor(s): pschwartau@netscape.com
+ * Contributor(s):
+ *   pschwartau@netscape.com
  *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
  *
- * The idea behind bug 53268 is as follows. The item 'five' below is defined
- * as const, hence is a read-only property of the global object. So if we set
- * obj.__proto__  = this,  'five' should become a read-only propery of obj.
- *
- * If we then change obj.__proto__  to null, obj.five should initially be
- * undefined. We should be able to define obj.five to whatever we want,
- * and be able to access this value as obj.five.
- *
- * Bug 53268 was filed because obj.five could not be set or accessed after
- * obj.__proto__  had been set to the global object and then to null.
- */
+ * ***** END LICENSE BLOCK ***** */
 //-----------------------------------------------------------------------------
 var bug = '53268';
 var status = 'Testing scope after changing obj.__proto__';
