90ce901e4cfe4a77633ee5b7cc9142843528eab7
bienvenu%nventure.com <bienvenu%nventure.com>
2005-03-18 20:41:07 +0000

    potential fix for going online after saving imap draft offline, sr=mscott 285791

--- mailnews/imap/src/nsImapOfflineSync.cpp
+++ mailnews/imap/src/nsImapOfflineSync.cpp
@@ -378,7 +378,7 @@ nsImapOfflineSync::ProcessAppendMsgOperation(nsIMsgOfflineImapOperation *current
                         m_window);
                   }
                   else
-                    m_curTempFile->Delete(PR_FALSE);
+                    tempFileSpec->Delete(PR_FALSE);
                 }
                 currentOp->ClearOperation(nsIMsgOfflineImapOperation::kAppendDraft);
                 m_currentDB->DeleteHeader(mailHdr, nsnull, PR_TRUE, PR_TRUE);
