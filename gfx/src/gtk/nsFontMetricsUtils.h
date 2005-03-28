36d8dd0018591e4c854bf72b419103d0684fb21c
dbaron%dbaron.org <dbaron%dbaron.org>
2005-03-28 02:14:17 +0000

    Fix crashes when core X fonts are disable:  ignore the pref to disable Xft when core fonts are disabled so we can't get into a state where we have zero acceptable font backends.  b=287187  r+sr=bryner

--- gfx/src/gtk/nsFontMetricsUtils.h
+++ gfx/src/gtk/nsFontMetricsUtils.h
@@ -40,15 +40,23 @@
 #ifndef __nsFontMetricsUtils_h
 #define __nsFontMetricsUtils_h
 
-extern PRUint32 NS_FontMetricsGetHints    (void);
+extern PRUint32 NS_FontMetricsGetHints    ();
 extern nsresult NS_FontMetricsFamilyExists(nsIDeviceContext *aDevice,
                                            const nsString &aName);
 #ifdef MOZ_ENABLE_XFT
-extern PRBool   NS_IsXftEnabled(void);
+#ifdef MOZ_ENABLE_COREXFONTS
+extern PRBool NS_IsXftEnabled();
+#else
+inline PRBool NS_IsXftEnabled() { return PR_TRUE; }
+#endif
 #endif
 
 #ifdef MOZ_ENABLE_PANGO
-extern PRBool   NS_IsPangoEnabled(void);
+#if defined(MOZ_ENABLE_XFT) || defined(MOZ_ENABLE_COREXFONTS)
+extern PRBool NS_IsPangoEnabled();
+#else
+inline PRBool NS_IsPangoEnabled() { return PR_TRUE; }
+#endif
 #endif
 
 #endif /* __nsFontMetricsUtils_h */
