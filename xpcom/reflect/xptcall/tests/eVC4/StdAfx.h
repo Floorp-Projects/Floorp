56c7da41f5b3ac3ff40e36032546db0484201ffc
dougt@meer.net <dougt@meer.net>
2005-03-30 06:16:30 +0000

    xptcall windows ce tests for arm.  submitted by john wolfe <wolfe@lobo.us>

---
+++ xpcom/reflect/xptcall/tests/eVC4/StdAfx.h
@@ -0,0 +1,31 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__B8B182B3_2D2B_404C_A7E2_C8FAED541358__INCLUDED_)
+#define AFX_STDAFX_H__B8B182B3_2D2B_404C_A7E2_C8FAED541358__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#if (_WIN32_WCE <= 211)
+#error This project can not be built for H/PC Pro 2.11 or earlier platforms.
+#endif
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#endif
+
+// Windows Header Files:
+#include <windows.h>
+
+// Local Header Files
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft eMbedded Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__B8B182B3_2D2B_404C_A7E2_C8FAED541358__INCLUDED_)
