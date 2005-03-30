82e39ad92ca12f2d985fcd033fa813fcb5bf6878
dougt%meer.net <dougt%meer.net>
2005-03-30 06:25:12 +0000

    Initial Implementation of WindowCE ARM XPTCALL code.  patch by John Wolfe wolfe@lobo.us.

--- xpcom/reflect/xptcall/src/md/win32/xptcinvokece.cpp
+++ xpcom/reflect/xptcall/src/md/win32/xptcinvokece.cpp
@@ -1,4 +1,3 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
@@ -12,18 +11,17 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is mozilla.org code.
+ * The Original Code is Windows CE Reflect
  *
- * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1998
+ * The Initial Developer of the Original Code is John Wolfe
+ * Portions created by the Initial Developer are Copyright (C) 2005
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
@@ -39,9 +37,105 @@
 
 #include "xptcprivate.h"
 
+extern "C" nsresult
+asmXPTC_InvokeByIndex( nsISupports* that,
+                       PRUint32 methodIndex,
+  					   PRUint32 paramCount,
+					   nsXPTCVariant* params,
+					   PRUint32 pfn_CopyToStack,
+					   PRUint32 pfn_CountWords);  
+
+// NOTE: This does the actual copying into the stack - based upon the
+// pointer "d" -- which is placed at the lowest location on the block
+// of memory that has been allocated on the stack
+
+extern "C" static void
+invoke_copy_to_stack(PRUint32* d, PRUint32 paramCount, nsXPTCVariant* s)
+{
+  for(; paramCount > 0; paramCount--, d++, s++)
+  {
+    if(s->IsPtrData())
+    {
+      *((void**)d) = s->ptr;
+      continue;
+    }
+    switch(s->type)
+    {
+      case nsXPTType::T_I8     : *((PRInt8*)  d) = s->val.i8;          break;
+      case nsXPTType::T_I16    : *((PRInt16*) d) = s->val.i16;         break;
+      case nsXPTType::T_I32    : *((PRInt32*) d) = s->val.i32;         break;
+      case nsXPTType::T_I64    : *((PRInt64*) d) = s->val.i64; d++;    break;
+      case nsXPTType::T_U8     : *((PRUint8*) d) = s->val.u8;          break;
+      case nsXPTType::T_U16    : *((PRUint16*)d) = s->val.u16;         break;
+      case nsXPTType::T_U32    : *((PRUint32*)d) = s->val.u32;         break;
+      case nsXPTType::T_U64    : *((PRUint64*)d) = s->val.u64; d++;    break;
+      case nsXPTType::T_FLOAT  : *((float*)   d) = s->val.f;           break;
+      case nsXPTType::T_DOUBLE : *((double*)  d) = s->val.d;   d++;    break;
+      case nsXPTType::T_BOOL   : *((PRBool*)  d) = s->val.b;           break;
+      case nsXPTType::T_CHAR   : *((char*)    d) = s->val.c;           break;
+      case nsXPTType::T_WCHAR  : *((wchar_t*) d) = s->val.wc;          break;
+      default:
+        // all the others are plain pointer types
+        *((void**)d) = s->val.p;
+        break;
+    }
+  }
+}
+
+
+extern "C" static uint32
+invoke_count_words(PRUint32 paramCount, nsXPTCVariant* s)
+{
+  uint32 nCnt = 0;
+  
+  for(; paramCount > 0; paramCount--, s++)
+  {
+    if(s->IsPtrData())
+    {
+      nCnt++;
+      continue;
+    }
+    switch(s->type)
+    {
+      case nsXPTType::T_I64    : 
+      case nsXPTType::T_U64    : 
+      case nsXPTType::T_DOUBLE : 
+        nCnt += 2;
+        break;
+        
+      case nsXPTType::T_I8     : 
+      case nsXPTType::T_I16    : 
+      case nsXPTType::T_I32    : 
+      case nsXPTType::T_U8     : 
+      case nsXPTType::T_U16    : 
+      case nsXPTType::T_U32    : 
+      case nsXPTType::T_FLOAT  : 
+      case nsXPTType::T_BOOL   : 
+      case nsXPTType::T_CHAR   : 
+      case nsXPTType::T_WCHAR  : 
+      default:
+        nCnt++;
+        break;
+    }
+  }
+  
+  // 4 Bytes for everything that uses 4 bytes or less, 8 bytes for
+  // everything else.
+  return nCnt;
+}
+
+
 XPTC_PUBLIC_API(nsresult)
 XPTC_InvokeByIndex(nsISupports* that, PRUint32 methodIndex, PRUint32 paramCount, nsXPTCVariant* params)
 {
-  return NS_OK;
+  return asmXPTC_InvokeByIndex(that,
+                               methodIndex,
+                               paramCount,
+                               params,
+                               (PRUint32) &invoke_copy_to_stack,
+                               (PRUint32) &invoke_count_words);
 }
+
+
+int g_xptcinvokece_marker;
 
