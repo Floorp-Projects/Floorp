8416d968ca05cf37f096c172a7878ca4ded50bc5
cbiesinger@web.de <cbiesinger@web.de>
2005-04-03 17:22:58 +0000

    Bug 288011 eliminate nsISSLSocketProvider r=darin sr=bz

--- security/manager/ssl/src/nsSSLSocketProvider.h
+++ security/manager/ssl/src/nsSSLSocketProvider.h
@@ -40,19 +40,19 @@
 #ifndef _NSSSLSOCKETPROVIDER_H_
 #define _NSSSLSOCKETPROVIDER_H_
 
-#include "nsISSLSocketProvider.h"
+#include "nsISocketProvider.h"
 
+#define NS_SSLSOCKETPROVIDER_CLASSNAME  "Mozilla SSL Socket Provider Component"
 /* 217d014a-1dd2-11b2-999c-b0c4df79b324 */
 #define NS_SSLSOCKETPROVIDER_CID   \
 { 0x217d014a, 0x1dd2, 0x11b2, {0x99, 0x9c, 0xb0, 0xc4, 0xdf, 0x79, 0xb3, 0x24}}
 
 
-class nsSSLSocketProvider : public nsISSLSocketProvider
+class nsSSLSocketProvider : public nsISocketProvider
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSISOCKETPROVIDER
-  NS_DECL_NSISSLSOCKETPROVIDER
   
   // nsSSLSocketProvider methods:
   nsSSLSocketProvider();
--- security/manager/ssl/src/nsTLSSocketProvider.h
+++ security/manager/ssl/src/nsTLSSocketProvider.h
@@ -40,9 +40,10 @@
 #ifndef _NSTLSSOCKETPROVIDER_H_
 #define _NSTLSSOCKETPROVIDER_H_
 
-#include "nsISSLSocketProvider.h"
+#include "nsISocketProvider.h"
 
 
+#define NS_STARTTLSSOCKETPROVIDER_CLASSNAME  "Mozilla STARTTLS Capable Socket Provider Component"
 #define NS_STARTTLSSOCKETPROVIDER_CID \
 { /* b9507aec-1dd1-11b2-8cd5-c48ee0c50307 */         \
      0xb9507aec,                                     \
@@ -51,13 +52,12 @@
     {0x8c, 0xd5, 0xc4, 0x8e, 0xe0, 0xc5, 0x03, 0x07} \
 }
 
-class nsTLSSocketProvider : public nsISSLSocketProvider
+class nsTLSSocketProvider : public nsISocketProvider
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSISOCKETPROVIDER
-  NS_DECL_NSISSLSOCKETPROVIDER
-  
+
   // nsTLSSocketProvider methods:
   nsTLSSocketProvider();
   virtual ~nsTLSSocketProvider();
