0ce974602e4c04c84ebebb9469270cb2f70cb234
bryner@brianryner.com <bryner@brianryner.com>
2005-04-04 19:57:46 +0000

    fixing gtk2 bustage

--- accessible/src/atk/nsMaiInterfaceHypertext.cpp
+++ accessible/src/atk/nsMaiInterfaceHypertext.cpp
@@ -139,9 +139,8 @@ getLinkCB(AtkHypertext *aText, gint aLinkIndex)
     // we take hyperlink->get() as Id for the MaiHyperlink.
     // release our ref to the previous one
 
-    nsCOMPtr<nsIAccessibleDocument> accessibleDoc;
-    nsAccessNode::GetDocAccessibleFor(weakShell,
-                                      getter_AddRefs(accessibleDoc));
+    nsCOMPtr<nsIAccessibleDocument> accessibleDoc =
+        nsAccessNode::GetDocAccessibleFor(weakShell);
     if (!accessibleDoc) {
         NS_WARNING("No accessible document for this presshell");
         return nsnull;
