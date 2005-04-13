482ad5b12db64994d5c3d53e1e5fbc94cb01af6d
scott%scott-macgregor.org <scott%scott-macgregor.org>
2005-04-13 21:45:13 +0000

    Bug #284699 --> add some null ptr protection to avoid a crash
    sr=bienvenu

--- mail/components/migration/src/nsSeamonkeyProfileMigrator.cpp
+++ mail/components/migration/src/nsSeamonkeyProfileMigrator.cpp
@@ -362,21 +362,25 @@ nsSeamonkeyProfileMigrator::FillProfileDataFromSeamonkeyRegistry()
   nsCOMPtr<nsILocalFile> seamonkeyRegistry;
 #ifdef XP_WIN
   fileLocator->Get(NS_WIN_APPDATA_DIR, NS_GET_IID(nsILocalFile), getter_AddRefs(seamonkeyRegistry));
+  NS_ENSURE_TRUE(seamonkeyRegistry, NS_ERROR_FAILURE);
 
   seamonkeyRegistry->Append(NS_LITERAL_STRING("Mozilla"));
   seamonkeyRegistry->Append(NS_LITERAL_STRING("registry.dat"));
 #elif defined(XP_MACOSX)
   fileLocator->Get(NS_MAC_USER_LIB_DIR, NS_GET_IID(nsILocalFile), getter_AddRefs(seamonkeyRegistry));
+  NS_ENSURE_TRUE(seamonkeyRegistry, NS_ERROR_FAILURE);
   
   seamonkeyRegistry->Append(NS_LITERAL_STRING("Mozilla"));
   seamonkeyRegistry->Append(NS_LITERAL_STRING("Application Registry"));
 #elif defined(XP_UNIX)
   fileLocator->Get(NS_UNIX_HOME_DIR, NS_GET_IID(nsILocalFile), getter_AddRefs(seamonkeyRegistry));
+  NS_ENSURE_TRUE(seamonkeyRegistry, NS_ERROR_FAILURE);
   
   seamonkeyRegistry->Append(NS_LITERAL_STRING(".mozilla"));
   seamonkeyRegistry->Append(NS_LITERAL_STRING("appreg"));
 #elif defined(XP_OS2)
   fileLocator->Get(NS_OS2_HOME_DIR, NS_GET_IID(nsILocalFile), getter_AddRefs(seamonkeyRegistry));
+  NS_ENSURE_TRUE(seamonkeyRegistry, NS_ERROR_FAILURE);
   
   seamonkeyRegistry->Append(NS_LITERAL_STRING("Mozilla"));
   seamonkeyRegistry->Append(NS_LITERAL_STRING("registry.dat"));
