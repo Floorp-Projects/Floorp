76ebcc8cbe9b41b28470b651dcbd61ee397d111f
bob%bclary.com <bob%bclary.com>
2005-04-18 20:02:20 +0000

    Additional tests by Martin Honnen, Bug 290481

--- js/tests/e4x/XMLList/13.5.4.9.js
+++ js/tests/e4x/XMLList/13.5.4.9.js
@@ -25,6 +25,7 @@
  *   Igor Bukanov
  *   Ethan Hugg
  *   Milen Nankov
+ *   Martin Honnen
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -43,6 +44,39 @@
 START("13.5.4.9 - XMLList descendants()");
 
 TEST(1, true, XMLList.prototype.hasOwnProperty("descendants"));
-    
+
+// From Martin Honnen    
+
+var gods = <gods>
+  <god>
+    <name>Kibo</name>
+  </god>
+  <god>
+    <name>Xibo</name>
+  </god>
+</gods>;
+
+var godList = gods.god;
+
+var expect;
+var actual;
+var node;
+var descendants = godList.descendants();
+
+expect = 4;
+actual = descendants.length();
+TEST(2, expect, actual)
+
+expect = 'nodeKind(): element, name(): name;\n' + 
+         'nodeKind(): text, name(): null;\n' +
+         'nodeKind(): element, name(): name;\n' + 
+         'nodeKind(): text, name(): null;\n';
+
+actual = '';
+
+for each (var xml in descendants) {
+  actual += 'nodeKind(): ' + xml.nodeKind() + ', name(): ' + xml.name() + ';\n';
+}
+TEST(4, expect, actual);
 
 END();
