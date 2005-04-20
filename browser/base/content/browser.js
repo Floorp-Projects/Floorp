348f9e2659e796021109e360aff016b242805940
doronr@us.ibm.com <doronr@us.ibm.com>
2005-04-20 17:13:54 +0000

    Bug 258277 - Add menu item to Check for Updates. r=mconnor,a=mkaply

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -5799,3 +5799,12 @@ missingPluginInstaller.prototype.observe = function(aSubject, aTopic, aData){
   }
 }
 var gMissingPluginInstaller = new missingPluginInstaller();
+
+function checkForUpdates() {
+  var updates = Components.classes["@mozilla.org/updates/update-service;1"]
+                          .getService(Components.interfaces.nsIUpdateService);
+  updates.checkForUpdates([], 0, Components.interfaces.nsIUpdateItem.TYPE_ANY, 
+                          Components.interfaces.nsIUpdateService.SOURCE_EVENT_USER,
+                          window);
+}
+
