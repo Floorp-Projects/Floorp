5cee8c04b5d0b5c55f882d0fd72faedfd126e68e
ben@bengoodger.com <ben@bengoodger.com>
2005-04-23 04:26:12 +0000

    <mconnor> it's splas.rc all over again!

--- browser/app/nsBrowserApp.cpp
+++ browser/app/nsBrowserApp.cpp
@@ -45,7 +45,7 @@
 
 static const nsXREAppData kAppData = {
   "Mozilla",
-  "Firefox Debug",
+  "Firefox",
   NS_STRINGIFY(APP_VERSION),
   NS_STRINGIFY(BUILD_ID),
   // ec8030f7-c20a-464f-9b0e-13a3a9e97384
