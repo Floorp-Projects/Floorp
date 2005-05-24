3a142f36bfbceef584dbe4da74afc1d7f729fbe1
robert%accettura.com <robert%accettura.com>
2005-05-24 01:54:23 +0000

    Bug 292696 point reporter to production instance.  All tests should be done with extensions.reporter.serviceURL set to reporter-dev.mozilla.org

--- extensions/reporter/resources/content/prefs/reporter.js
+++ extensions/reporter/resources/content/prefs/reporter.js
@@ -1,4 +1,4 @@
 // Make sure there are trailing slashes!
-pref("extensions.reporter.privacyURL", "http://reporter-test.mozilla.org/privacy/");
-pref("extensions.reporter.serviceURL", "http://reporter-test.mozilla.org/service/");
+pref("extensions.reporter.privacyURL", "http://reporter.mozilla.org/privacy/");
+pref("extensions.reporter.serviceURL", "http://reporter.mozilla.org/service/");
 
