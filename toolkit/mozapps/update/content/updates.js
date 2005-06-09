5d669856c0ed839411fac6c6595aec28d3607dd4
ben@bengoodger.com <ben@bengoodger.com>
2005-06-09 01:06:45 +0000

    fall back to downloading a complete download package if the verification of the patch fails

--- toolkit/mozapps/update/content/updates.js
+++ toolkit/mozapps/update/content/updates.js
@@ -1,5 +1,43 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the Update Service.
+ *
+ * The Initial Developer of the Original Code is Google Inc.
+ * Portions created by the Initial Developer are Copyright (C) 2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Ben Goodger <ben@mozilla.org> (Original Author)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 const nsIUpdateItem = Components.interfaces.nsIUpdateItem;
 const nsIIncrementalDownload = Components.interfaces.nsIIncrementalDownload;
+const XMLNS_XUL = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
 
 /**
  * Logs a string to the error console. 
@@ -149,8 +187,7 @@ var gUpdatesAvailablePage = {
 var gDownloadingPage = {
   onPageShow: function() {
     // Build the UI for the active download
-    var update = document.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",  
-                                          "update");
+    var update = document.createElementNS(XMLNS_XUL, "update");
     update.setAttribute("state", "downloading");
     update.setAttribute("name", "Firefox 1.0.4");
     update.setAttribute("status", "Blah");
@@ -159,16 +196,29 @@ var gDownloadingPage = {
     var updatesView = document.getElementById("updatesView");
     updatesView.appendChild(update);
     
+    updatesView.addEventListener("update-pause", this.onPause, false);
+    
     // Add this UI as a listener for active downloads
     var updates = 
-        Components.classes["@mozilla.org/updates/update-service;1"]
-                  .getService(Components.interfaces.nsIApplicationUpdateService);
-    LOG("goats");
+        Components.classes["@mozilla.org/updates/update-service;1"].
+        getService(Components.interfaces.nsIApplicationUpdateService);
+    LOG("intergoat");
     updates.downloadUpdate(gUpdates.update, false);
     updates.addDownloadListener(this);
     
     // Build the UI for previously installed updates
     // ...
+  },
+  
+  _paused: false,
+  onPause: function() {
+    var updates = 
+        Components.classes["@mozilla.org/updates/update-service;1"].
+        getService(Components.interfaces.nsIApplicationUpdateService);
+    if (this._paused)
+      updates.downloadUpdate(gUpdates.update, false);
+    else
+      updates.pauseDownload();
   },
   
   onClose: function() {
@@ -193,7 +243,7 @@ var gDownloadingPage = {
   
   onProgress: function(request, context, progress, maxProgress) {
     request.QueryInterface(nsIIncrementalDownload);
-    LOG("gDownloadingPage.onProgress: " + request.URI.spec + ", " + progress + "/" + maxProgress);
+    //LOG("gDownloadingPage.onProgress: " + request.URI.spec + ", " + progress + "/" + maxProgress);
     
     var active = document.getElementById("activeDownloadItem");
     active.setAttribute("progress", Math.floor(100 * (progress/maxProgress)));
@@ -207,6 +257,19 @@ var gDownloadingPage = {
   onStopRequest: function(request, context, status) {
     request.QueryInterface(nsIIncrementalDownload);
     LOG("gDownloadingPage.onStopRequest: " + request.URI.spec + ", status = " + status);
+    var updates = 
+        Components.classes["@mozilla.org/updates/update-service;1"]
+                  .getService(Components.interfaces.nsIApplicationUpdateService);
+    if (status == Components.results.NS_ERROR_UNEXPECTED &&
+        !gUpdates.update.isCompleteUpdate) {
+      // If we were downloading a patch and the patch verification phase 
+      // failed, log this and then commence downloading the complete update.
+      LOG("Verification of patch failed, downloading complete update");
+      gUpdates.update.isCompleteUpdate = true;
+      updates.downloadUpdate(gUpdates.update, false);
+      return;
+    }
+    updates.removeDownloadListener(this);
   },
    
   /**
