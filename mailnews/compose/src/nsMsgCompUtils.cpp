4ecec2d1811d7eafa60c3f6b99e781e4b193a709
scott%scott-macgregor.org <scott%scott-macgregor.org>
2005-06-10 19:02:05 +0000

    Bug #297325 --> Consider application/pgp-keys as plain text
    Patch by patrick.brunschwig@gmx.net
    
    r=mscott
    sr=bienvenu
    a=asa

--- mailnews/compose/src/nsMsgCompUtils.cpp
+++ mailnews/compose/src/nsMsgCompUtils.cpp
@@ -1530,6 +1530,7 @@ mime_type_requires_b64_p (const char *type)
     static const char *app_and_image_types_which_are_really_text[] = {
     "application/mac-binhex40",   /* APPLICATION_BINHEX */
     "application/pgp",        /* APPLICATION_PGP */
+    "application/pgp-keys",
     "application/x-pgp-message",  /* APPLICATION_PGP2 */
     "application/postscript",   /* APPLICATION_POSTSCRIPT */
     "application/x-uuencode",   /* APPLICATION_UUENCODE */
