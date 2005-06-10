b18529a8a4113a12b91875d79aba44f5a189452b
bsmedberg@covad.net <bsmedberg@covad.net>
2005-06-10 19:21:27 +0000

    Bug 224305 part "windows" - Separate GRE (xulrunner) registration from the installer scripts, r=darin a=asa

--- xpcom/glue/standalone/nsGREDirServiceProvider.cpp
+++ xpcom/glue/standalone/nsGREDirServiceProvider.cpp
@@ -21,6 +21,7 @@
  *
  * Contributor(s):
  *   Sean Su <ssu@netscape.com>
+ *   Benjamin Smedberg <benjamin@smedbergs.us>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -82,9 +83,7 @@
 
 #include "nsGREDirServiceProvider.h"
 
-PRBool GRE_GetCurrentProcessDirectory(char* buffer);
-PRBool GRE_GetPathFromConfigDir(const char* dirname, char* buffer);
-PRBool GRE_GetPathFromConfigFile(const char* dirname, char* buffer);
+static PRBool GRE_GetCurrentProcessDirectory(char* buffer);
 
 //*****************************************************************************
 // nsGREDirServiceProvider::nsISupports
@@ -295,177 +294,11 @@ GRE_GetGREPath()
     }
   }
 
-  // if GRE_HOME is in the environment, use that GRE
-  const char* env = PR_GetEnv("GRE_HOME");
-  if (env && *env) {
-#if XP_UNIX
-    if (!realpath(env, sGRELocation))
-      strcpy(sGRELocation, env);
-#elif XP_WIN32
-    if (!_fullpath(sGRELocation, env, MAXPATHLEN))
-      strcpy(sGRELocation, env);
-#else
-    // hope for the best
-    strcpy(sGRELocation, env);
-#endif
-    // xxxbsmedberg: it would help that other platforms had a "make absolute" function
+  GRE_GetGREPathForVersion(GRE_BUILD_ID, sGRELocation, MAXPATHLEN);
+  if (*sGRELocation)
     return sGRELocation;
-  }
-
-  // the Gecko bits that sit next to the application or in the LD_LIBRARY_PATH
-  env = PR_GetEnv("USE_LOCAL_GRE");
-  if (env && *env)
-    return nsnull;
-
-#if XP_UNIX
-  // check in the HOME directory
-  env = PR_GetEnv("HOME");
-  if (env && *env) {
-    sprintf(buffer, "%s" XPCOM_FILE_PATH_SEPARATOR GRE_CONF_NAME, env);
-    
-    if (GRE_GetPathFromConfigFile(buffer, sGRELocation)) {
-      return sGRELocation;
-    }
-  }
-#endif
-
-  env = PR_GetEnv("MOZ_GRE_CONF");
-  if (env) {
-    if (GRE_GetPathFromConfigFile(env, sGRELocation)) {
-      return sGRELocation;
-    }
-  }
-
-#if XP_UNIX
-  // Look for a group of config files in /etc/gre.d/
-  if (GRE_GetPathFromConfigDir(GRE_CONF_DIR, sGRELocation)) {
-    return sGRELocation;
-  }
-
-  // Look for a global /etc/gre.conf file
-  if (GRE_GetPathFromConfigFile(GRE_CONF_PATH, sGRELocation)) {
-    return sGRELocation;
-  }
-#endif
-  
-#if XP_WIN32
-  char szKey[256];
-  HKEY hRegKey = NULL;
-  DWORD dwLength = MAXPATHLEN;
-    
-  // A couple of key points here:
-  // 1. Note the usage of the "Software\\Mozilla\\GRE" subkey - this allows
-  //    us to have multiple versions of GREs on the same machine by having
-  //    subkeys such as 1.0, 1.1, 2.0 etc. under it.
-  // 2. In this sample below we're looking for the location of GRE version 1.2
-  //    i.e. we're compatible with GRE 1.2 and we're trying to find it's install
-  //    location.
-  //
-  // Please see http://www.mozilla.org/projects/embedding/GRE.html for
-  // more info.
-  //
-  strcpy(szKey, GRE_WIN_REG_LOC GRE_BUILD_ID);
-    
-  if (::RegOpenKeyEx(HKEY_CURRENT_USER, szKey, 0, KEY_QUERY_VALUE, &hRegKey) == ERROR_SUCCESS) {
-    if (::RegQueryValueEx(hRegKey, "GreHome", NULL, NULL, (BYTE *)sGRELocation, &dwLength) != ERROR_SUCCESS) {
-      *sGRELocation = '\0';
-    }
-    ::RegCloseKey(hRegKey);
-
-    if (*sGRELocation)
-      return sGRELocation;
-  }
-
-  if (::RegOpenKeyEx(HKEY_LOCAL_MACHINE, szKey, 0, KEY_QUERY_VALUE, &hRegKey) == ERROR_SUCCESS) {
-    if (::RegQueryValueEx(hRegKey, "GreHome", NULL, NULL, (BYTE *)sGRELocation, &dwLength) != ERROR_SUCCESS) {
-      *sGRELocation = '\0';
-    }
-    ::RegCloseKey(hRegKey);
-
-    if (*sGRELocation)
-      return sGRELocation;
-  }
-#endif
 
   return nsnull;
-}
-
-PRBool
-GRE_GetPathFromConfigDir(const char* dirname, char* buffer)
-{
-  // Open the directory provided and try to read any files in that
-  // directory that end with .conf.  We look for an entry that might
-  // point to the GRE that we're interested in.
-  PRDir *dir = PR_OpenDir(dirname);
-  if (!dir)
-    return nsnull;
-
-  PRBool found = PR_FALSE;
-  PRDirEntry *entry;
-
-  while (!found && (entry = PR_ReadDir(dir, PR_SKIP_BOTH))) {
-
-    // Only look for files that end in .conf
-    char *offset = PL_strrstr(entry->name, ".conf");
-    if (!offset)
-      continue;
-
-    if (offset != entry->name + strlen(entry->name) - 5)
-      continue;
-
-    nsEmbedCString fullPath;
-    NS_CStringAppendData(fullPath, dirname);
-    NS_CStringAppendData(fullPath, XPCOM_FILE_PATH_SEPARATOR);
-    NS_CStringAppendData(fullPath, entry->name);
-
-    found = GRE_GetPathFromConfigFile(fullPath.get(), buffer);
-  }
-
-  PR_CloseDir(dir);
-
-  return found;
-}
-
-PRBool
-GRE_GetPathFromConfigFile(const char* filename, char* pathBuffer)
-{
-  *pathBuffer = '\0';
-  char buffer[1024];
-  FILE *cfg;
-  PRBool foundHeader = PR_FALSE;
-  PRInt32 versionLen = sizeof(GRE_BUILD_ID)-1;
-
-  if((cfg=fopen(filename,"r"))==nsnull) {
-    return nsnull;
-  }
-
-  while (fgets(buffer, 1024, cfg) != nsnull) {
-    // skip over comment lines and blank lines
-    if (buffer[0] == '#' || buffer[0] == '\n') {
-      continue;
-    }
-
-    // we found a section heading, check to see if it is the one we are intersted in.
-    if (buffer[0] == '[') {
-      if (!strncmp (buffer+1, GRE_BUILD_ID, versionLen)) {
-        foundHeader = PR_TRUE;
-      }
-      continue;
-    }
-
-    if (foundHeader && !strncmp (buffer, "GRE_PATH=", 9)) {
-      strcpy(pathBuffer, buffer + 9);
-      // kill the line feed if any
-      PRInt32 len = strlen(pathBuffer);
-      len--;
-
-      if (pathBuffer[len] == '\n')
-        pathBuffer[len] = '\0';
-      break;
-    }
-  }
-  fclose(cfg);
-  return (*pathBuffer != '\0');
 }
 
 extern "C" nsresult
