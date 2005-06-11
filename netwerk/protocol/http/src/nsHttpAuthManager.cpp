734beae4632f68574b7c010f083f3dafcc534601
gandalf%firefox.pl <gandalf%firefox.pl>
2005-06-11 13:12:55 +0000

    Bug 296456: Fix license headers on some HTTP source files; r=darin,sr=dveditz, a=asa

--- netwerk/protocol/http/src/nsHttpAuthManager.cpp
+++ netwerk/protocol/http/src/nsHttpAuthManager.cpp
@@ -27,11 +27,11 @@
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the NPL, indicate your
+ * use your version of this file under the terms of the MPL, indicate your
  * decision by deleting the provisions above and replace them with the notice
  * and other provisions required by the GPL or the LGPL. If you do not delete
  * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the NPL, the GPL or the LGPL.
+ * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
 
