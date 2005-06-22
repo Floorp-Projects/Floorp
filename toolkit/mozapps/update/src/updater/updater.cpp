fa9b3fbffb9c000e9adf0da8112efbdb9807b514
cls%seawood.org <cls%seawood.org>
2005-06-22 15:55:32 +0000

    Use HOST_EXTRA_LIBS for linking HOST_PROGRAMS. Do not define WinMain() for gcc builds. Bug #298417 r=bsmedberg a=chofmann

--- toolkit/mozapps/update/src/updater/updater.cpp
+++ toolkit/mozapps/update/src/updater/updater.cpp
@@ -1071,7 +1071,7 @@ int DoUpdate()
   return rv;
 }
 
-#if defined(XP_WIN) && !defined(DEBUG)
+#if defined(XP_WIN) && !defined(DEBUG) && !defined(__GNUC__)
 // We need WinMain in order to not be a console app.  This function is unused
 // if we are a console application.
 int WINAPI WinMain( HINSTANCE, HINSTANCE, LPSTR args, int )
