63e30c663ef8bd6d8c2d57485a6e08c5cc7d3f47
wtchang@redhat.com <wtchang@redhat.com>
2005-06-23 23:41:29 +0000

    Bugzilla Bug 298537: replaced hardcoded 3 and 8 with the NSS_VMAJOR and NSS_VMINOR macros defined in nss.h. r=relyea,nelsonb.

--- security/nss/lib/softoken/pkcs11.c
+++ security/nss/lib/softoken/pkcs11.c
@@ -62,6 +62,7 @@
 #include "secport.h"
 #include "pcert.h"
 #include "secrng.h"
+#include "nss.h"
 
 #include "keydbi.h" 
 
@@ -2911,8 +2912,8 @@ CK_RV  NSC_GetInfo(CK_INFO_PTR pInfo)
     pInfo->cryptokiVersion.major = 2;
     pInfo->cryptokiVersion.minor = 11;
     PORT_Memcpy(pInfo->manufacturerID,manufacturerID,32);
-    pInfo->libraryVersion.major = 3;
-    pInfo->libraryVersion.minor = 8;
+    pInfo->libraryVersion.major = NSS_VMAJOR;
+    pInfo->libraryVersion.minor = NSS_VMINOR;
     PORT_Memcpy(pInfo->libraryDescription,libraryDescription,32);
     pInfo->flags = 0;
     return CKR_OK;
@@ -2953,8 +2954,8 @@ CK_RV NSC_GetSlotInfo(CK_SLOT_ID slotID, CK_SLOT_INFO_PTR pInfo)
     pInfo->flags = CKF_TOKEN_PRESENT;
     /* ok we really should read it out of the keydb file. */
     /* pInfo->hardwareVersion.major = NSSLOWKEY_DB_FILE_VERSION; */
-    pInfo->hardwareVersion.major = 3;
-    pInfo->hardwareVersion.minor = 8;
+    pInfo->hardwareVersion.major = NSS_VMAJOR;
+    pInfo->hardwareVersion.minor = NSS_VMINOR;
     return CKR_OK;
 }
 
