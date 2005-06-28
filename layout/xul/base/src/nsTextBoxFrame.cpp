e248ecf0c9dff7cb38fd06f8c81d910506af0e76
masayuki%d-toybox.com <masayuki%d-toybox.com>
2005-06-28 17:21:21 +0000

    Bug 298742 intl.properties's "intl.menuitems.alwaysappendaccesskeys" is ignored always r=jst, sr=shaver, a=benjamin

--- layout/xul/base/src/nsTextBoxFrame.cpp
+++ layout/xul/base/src/nsTextBoxFrame.cpp
@@ -198,8 +198,9 @@ nsTextBoxFrame::AlwaysAppendAccessKey()
   {
     gAccessKeyPrefInitialized = PR_TRUE;
 
-    gAlwaysAppendAccessKey =
-        nsContentUtils::GetCharPref("intl.menuitems.alwaysappendaccesskeys").Equals("true");
+    const char* prefName = "intl.menuitems.alwaysappendaccesskeys";
+    nsAdoptingString val = nsContentUtils::GetLocalizedStringPref(prefName);
+    gAlwaysAppendAccessKey = val.Equals(NS_LITERAL_STRING("true"));
   }
   return gAlwaysAppendAccessKey;
 }
