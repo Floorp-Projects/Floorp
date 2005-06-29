73bd167fb9f30b1aafb3ef9fdeb17f94c4c65131
bryner@brianryner.com <bryner@brianryner.com>
2005-06-29 03:57:49 +0000

    Enable fastback (saving 3 content viewers).  Bug 298293, r+sr=darin, a=bsmedberg

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -215,6 +215,7 @@ pref("browser.search.basic.min_ver", "0.0");
 
 pref("browser.history.grouping", "day");
 pref("browser.sessionhistory.max_entries", 50);
+pref("browser.sessionhistory.max_viewers", 3);
 
 // handle external links
 // 0=default window, 1=current window/tab, 2=new window, 3=new tab in most recent window
