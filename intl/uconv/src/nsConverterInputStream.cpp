187c46383e132faa4a7eb647e8b0fba05eaec291
cbiesinger@web.de <cbiesinger@web.de>
2005-07-05 14:32:59 +0000

    no effect either, undoing again

--- intl/uconv/src/nsConverterInputStream.cpp
+++ intl/uconv/src/nsConverterInputStream.cpp
@@ -245,8 +245,7 @@ nsConverterInputStream::Fill(nsresult * aErrorCode)
       NS_ASSERTION(0 < mUnicharData->GetBufferSize() - mUnicharDataLength,
                    "Decoder returned an error but filled the output buffer! "
                    "Should not happen.");
-      mUnicharData->GetBuffer()[mUnicharDataLength++] =
-                                             PRUnichar(mReplacementChar);
+      mUnicharData->GetBuffer()[mUnicharDataLength++] = mReplacementChar;
       ++srcConsumed;
       // XXX this is needed to make sure we don't underrun our buffer;
       // bug 160784 again
