58d2456ca34d77bb861190c3f04066ee93c7e86e
shaver%mozilla.org <shaver%mozilla.org>
2005-07-06 16:03:09 +0000

    Fix event handling for todo list. r=finally-getting-the-hang-of-this

--- calendar/lightning/content/todo-list.js
+++ calendar/lightning/content/todo-list.js
@@ -1,7 +1,26 @@
+function eventToTodo(event)
+{
+    try {
+        return event.originalTarget.selectedItem.todo;
+    } catch (e) {
+        return null;
+    }
+}
+
+function handleTodoListEvent(event)
+{
+    var todo = eventToTodo(event);
+    var name = todo ? ('"' + todo.title + '"') : "<none>";
+    dump("Todo list event: " + event.type + " (" + name + ")\n");
+}
+
 function initializeTodoList()
 {
     var todoList = document.getElementById("calendar-todo-list");
     todoList.calendar = getCompositeCalendar();
+    todoList.addEventListener("todo-item-open", handleTodoListEvent, false);
+    todoList.addEventListener("todo-item-delete", handleTodoListEvent, false);
+    todoList.addEventListener("todo-empty-dblclick", handleTodoListEvent, false);
 }
 
 window.addEventListener("load", initializeTodoList, false);
