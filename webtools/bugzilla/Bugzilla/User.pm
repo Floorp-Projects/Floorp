cc906a851f1c2c489458f2d9fc1f393550142baf
mkanat%kerio.com <mkanat%kerio.com>
2005-07-11 06:30:05 +0000

    Bug 300334: Bugzilla::User has no need to "use Bugzilla::BugMail" Patch By Max Kanat-Alexander <mkanat@bugzilla.org> r=justdave, a=justdave

--- webtools/bugzilla/Bugzilla/User.pm
+++ webtools/bugzilla/Bugzilla/User.pm
@@ -42,7 +42,6 @@ use Bugzilla::Util;
 use Bugzilla::Constants;
 use Bugzilla::User::Setting;
 use Bugzilla::Auth;
-use Bugzilla::BugMail;
 
 use base qw(Exporter);
 @Bugzilla::User::EXPORT = qw(insert_new_user is_available_username
