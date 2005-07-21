252be2d441da529c5c7a7cf26b4b62c978da5ee7
wtchang%redhat.com <wtchang%redhat.com>
2005-07-21 23:48:30 +0000

    Bugzilla Bug 288647: enable building NSS with an NSPR binary distribution. Introduced NSPR_INCLUDE_DIR and NSPR_LIB_DIR make variables. Portions of the patch were contributed by Chris Seawood <cls@seawood.org>. r=relyea. Modified Files: 	coreconf/OS2.mk coreconf/OpenVMS.mk coreconf/location.mk 	nss/cmd/platlibs.mk nss/cmd/shlibsign/Makefile 	nss/cmd/shlibsign/sign.cmd nss/cmd/shlibsign/sign.sh 	nss/lib/ckfw/builtins/Makefile 	nss/lib/fortcrypt/swfort/pkcs11/Makefile nss/lib/nss/config.mk 	nss/lib/smime/config.mk nss/lib/softoken/config.mk 	nss/lib/ssl/config.mk

--- security/nss/lib/ssl/config.mk
+++ security/nss/lib/ssl/config.mk
@@ -52,6 +52,7 @@ ifdef NS_USE_GCC
 EXTRA_SHARED_LIBS += \
 	-L$(DIST)/lib \
 	-lnss3 \
+	-L$(NSPR_LIB_DIR) \
 	-lplc4 \
 	-lplds4 \
 	-lnspr4 \
@@ -59,9 +60,9 @@ EXTRA_SHARED_LIBS += \
 else # ! NS_USE_GCC
 EXTRA_SHARED_LIBS += \
 	$(DIST)/lib/nss3.lib \
-	$(DIST)/lib/$(NSPR31_LIB_PREFIX)plc4.lib \
-	$(DIST)/lib/$(NSPR31_LIB_PREFIX)plds4.lib \
-	$(DIST)/lib/$(NSPR31_LIB_PREFIX)nspr4.lib \
+	$(NSPR_LIB_DIR)/$(NSPR31_LIB_PREFIX)plc4.lib \
+	$(NSPR_LIB_DIR)/$(NSPR31_LIB_PREFIX)plds4.lib \
+	$(NSPR_LIB_DIR)/$(NSPR31_LIB_PREFIX)nspr4.lib \
 	$(NULL)
 endif # NS_USE_GCC
 
@@ -71,8 +72,9 @@ else
 # $(PROGRAM) has NO explicit dependencies on $(EXTRA_SHARED_LIBS)
 # $(EXTRA_SHARED_LIBS) come before $(OS_LIBS), except on AIX.
 EXTRA_SHARED_LIBS += \
-	-L$(DIST)/lib/ \
+	-L$(DIST)/lib \
 	-lnss3 \
+	-L$(NSPR_LIB_DIR) \
 	-lplc4 \
 	-lplds4 \
 	-lnspr4 \
