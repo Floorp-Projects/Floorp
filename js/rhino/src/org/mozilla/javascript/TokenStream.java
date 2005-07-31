5839d1a3e274eab02874d28db86631ba75104cf3
igor%mir2.org <igor%mir2.org>
2005-07-31 13:48:46 +0000

    Fixing bug 291591
    \<LineTerminator> in string literals is removed from the resulting string to follow SpiderMonkey and C/C++

--- js/rhino/src/org/mozilla/javascript/TokenStream.java
+++ js/rhino/src/org/mozilla/javascript/TokenStream.java
@@ -574,6 +574,12 @@ class TokenStream
                             }
                             break;
 
+                        case '\n':
+                            // Remove line terminator after escape to follow
+                            // SpiderMonkey and C/C++
+                            c = getChar();
+                            continue strLoop;
+
                         default:
                             if ('0' <= c && c < '8') {
                                 int val = c - '0';
