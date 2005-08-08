11fda53577d4afac015875284291cdd5b30b01f9
caillon@redhat.com <caillon@redhat.com>
2005-08-08 19:43:31 +0000

    Bug 300226, unset window userdata before destroying p=chpe@gnome.org r=mpgritti@gnome.org sr=roc a=asa

--- widget/src/gtk2/mozdrawingarea.c
+++ widget/src/gtk2/mozdrawingarea.c
@@ -168,7 +168,9 @@ moz_drawingarea_finalize (GObject *object)
 
     drawingarea = MOZ_DRAWINGAREA(object);
 
+    gdk_window_set_user_data(drawingarea->inner_window, NULL);
     gdk_window_destroy(drawingarea->inner_window);
+    gdk_window_set_user_data(drawingarea->clip_window, NULL);
     gdk_window_destroy(drawingarea->clip_window);
 
     (* parent_class->finalize) (object);
