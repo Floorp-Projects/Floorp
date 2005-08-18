075a33a7c483b2432fec133bad4ffdd0a690d6b8
scott@scott-macgregor.org <scott@scott-macgregor.org>
2005-08-18 21:10:58 +0000

    Bug #302569 --> moz-icon urls broken on Mac OS X
    r=biesi,pavlov

--- modules/libpr0n/decoders/icon/mac/nsIconChannel.h
+++ modules/libpr0n/decoders/icon/mac/nsIconChannel.h
@@ -46,17 +46,21 @@
 #include "nsILoadGroup.h"
 #include "nsIInterfaceRequestor.h"
 #include "nsIInterfaceRequestorUtils.h"
+#include "nsIInputStreamPump.h"
+#include "nsIStreamListener.h"
 #include "nsIURI.h"
 #include <Icons.h>
 
 class nsIFile;
 
-class nsIconChannel : public nsIChannel
+class nsIconChannel : public nsIChannel, public nsIStreamListener, public nsIStreamListener
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIREQUEST
   NS_DECL_NSICHANNEL
+  NS_DECL_NSIREQUESTOBSERVER
+  NS_DECL_NSISTREAMLISTENER
 
   nsIconChannel();
   virtual ~nsIconChannel();
@@ -66,12 +70,15 @@ public:
 protected:
   nsCOMPtr<nsIURI> mUrl;
   nsCOMPtr<nsIURI> mOriginalURI;
-  PRUint32         mLoadAttributes;
   PRInt32          mContentLength;
   nsCOMPtr<nsILoadGroup> mLoadGroup;
   nsCOMPtr<nsIInterfaceRequestor> mCallbacks;
   nsCOMPtr<nsISupports>  mOwner; 
-  nsresult mStatus;
+  
+  nsCOMPtr<nsIInputStreamPump> mPump;
+  nsCOMPtr<nsIStreamListener>  mListener;
+  
+  nsresult MakeInputStream(nsIInputStream** _retval, PRBool nonBlocking);
   
   nsresult ExtractIconInfoFromUrl(nsIFile ** aLocalFile, PRUint32 * aDesiredImageSize,
                            nsACString &aContentType, nsACString &aFileExtension);
