f4c806de713f2368bd4282d4f463cfa433f49cd9
cbiesinger@web.de <cbiesinger@web.de>
2005-08-22 18:10:23 +0000

    bug 285872 GIF Decoder: replace gathering buffer with dynamic malloc to fixed 256 bytes hold patch by Alfred Kayser <alfredkayser@nl.ibm.com> r/sr=tor/pavlov

--- modules/libpr0n/decoders/gif/nsGIFDecoder2.cpp
+++ modules/libpr0n/decoders/gif/nsGIFDecoder2.cpp
@@ -223,14 +223,9 @@ nsresult nsGIFDecoder2::ProcessData(unsigned char *data, PRUint32 count, PRUint3
 {
   // Push the data to the GIF decoder
   
-  // First we ask if the gif decoder is ready for more data, and if so, push it.
-  // In the new decoder, we should always be able to process more data since
-  // we don't wait to decode each frame in an animation now.
-  if (gif_write_ready(mGIFStruct)) {
-    PRStatus result = gif_write(mGIFStruct, data, count);
-    if (result != PR_SUCCESS)
-      return NS_ERROR_FAILURE;
-  }
+  PRStatus result = gif_write(mGIFStruct, data, count);
+  if (result != PR_SUCCESS)
+    return NS_ERROR_FAILURE;
 
   if (mImageFrame && mObserver) {
     FlushImageData();
