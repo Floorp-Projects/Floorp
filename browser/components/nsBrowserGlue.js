e1400c9125f1ce96798a8c3ff2c3e918c4b7163a
gavin%gavinsharp.com <gavin%gavinsharp.com>
2005-08-23 16:38:28 +0000

    Bug 284086: "Sanitize on shutdown" fails if the last closed window is not a browser window, patch by Giorgio Maone <g.maone@informaction.com>, r=mconnor

---
+++ browser/components/nsBrowserGlue.js
@@ -0,0 +1,228 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the Firefox Browser Glue Service.
+ *
+ * The Initial Developer of the Original Code is
+ * Giorgio Maone
+ * Portions created by the Initial Developer are Copyright (C) 2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Giorgio Maone <g.maone@informaction.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+
+
+// Constructor
+
+function BrowserGlue() {
+  this._init();
+}
+
+BrowserGlue.prototype = {
+  QueryInterface: function(iid) 
+  {
+     xpcomCheckInterfaces(iid, kServiceIIds, Components.results.NS_ERROR_NO_INTERFACE);
+     return this;
+  }
+,
+  // nsIObserver implementation 
+  observe: function(subject, topic, data) 
+  {
+    switch(topic) {
+      case "xpcom-shutdown":
+        this._dispose();
+        break;
+      case "profile-before-change": 
+        this._onProfileShutdown();
+        break;
+      case "profile-after-change":
+        this._onProfileStartup();
+        break;
+    }
+  }
+, 
+  // initialization (called on application startup) 
+  _init: function() 
+  {
+    // observer registration
+    const osvr = Components.classes['@mozilla.org/observer-service;1']
+                           .getService(Components.interfaces.nsIObserverService);
+    osvr.addObserver(this, "profile-before-change", false);
+    osvr.addObserver(this, "xpcom-shutdown", false);
+    osvr.addObserver(this, "profile-after-change", false);
+  },
+
+  // cleanup (called on application shutdown)
+  _dispose: function() 
+  {
+    // observer removal 
+    const osvr = Components.classes['@mozilla.org/observer-service;1']
+                           .getService(Components.interfaces.nsIObserverService);
+    osvr.removeObserver(this, "profile-before-change");
+    osvr.removeObserver(this, "xpcom-shutdown");
+    osvr.removeObserver(this, "profile-after-change");
+  },
+
+  // profile startup handler (contains profile initialization routines)
+  _onProfileStartup: function() 
+  {
+    this.Sanitizer.onStartup();
+  },
+
+  // profile shutdown handler (contains profile cleanup routines)
+  _onProfileShutdown: function() 
+  {
+    his.Sanitizer.onShutdown();
+  },
+
+  // returns the (cached) Sanitizer constructor
+  get Sanitizer() 
+  {
+    if(typeof(Sanitizer) != "function") { // we should dynamically load the script
+      Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+                .getService(Components.interfaces.mozIJSSubScriptLoader)
+                .loadSubScript("chrome://browser/content/sanitize.js", null);
+    }
+    return Sanitizer;
+  },
+  
+  // ------------------------------
+  // public nsIBrowserGlue members
+  // ------------------------------
+  
+  sanitize: function(aParentWindow) 
+  {
+    this.Sanitizer.sanitize(aParentWindow);
+  }
+
+}
+
+
+// XPCOM Scaffolding code
+
+// component defined in this file
+
+const kServiceName = "Firefox Browser Glue Service";
+const kServiceId = "{eab9012e-5f74-4cbc-b2b5-a590235513cc}";
+const kServiceCtrId = "@mozilla.org/browser/browserglue;1";
+const kServiceConstructor = BrowserGlue;
+
+const kServiceCId = Components.ID(kServiceId);
+
+// interfaces implemented by this component
+const kServiceIIds = [ 
+  Components.interfaces.nsIObserver,
+  Components.interfaces.nsISupports,
+  Components.interfaces.nsISupportsWeakReference,
+  Components.interfaces.nsIBrowserGlue
+  ];
+
+// categories which this component is registered in
+const kServiceCats = ["app-startup"];
+
+// Factory object
+const kServiceFactory = {
+  _instance: null,
+  createInstance: function (outer, iid) 
+  {
+    if (outer != null) throw Components.results.NS_ERROR_NO_AGGREGATION;
+
+    xpcomCheckInterfaces(iid, kServiceIIds, 
+                          Components.results.NS_ERROR_INVALID_ARG);
+    return this._instance == null ?
+      this._instance = new kServiceConstructor() : this._instance;
+  }
+};
+
+function xpcomCheckInterfaces(iid, iids, ex) {
+  for (var j = iids.length; j-- >0;) {
+    if (iid.equals(iids[j])) return true;
+  }
+  throw ex;
+}
+
+// Module
+
+var Module = {
+  registered: false,
+  
+  registerSelf: function(compMgr, fileSpec, location, type) 
+  {
+    if (!this.registered) {
+      compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar)
+             .registerFactoryLocation(kServiceCId,
+                                      kServiceName,
+                                      kServiceCtrId, 
+                                      fileSpec,
+                                      location, 
+                                      type);
+      const catman = Components.classes['@mozilla.org/categorymanager;1']
+                               .getService(Components.interfaces.nsICategoryManager);
+      var len = kServiceCats.length;
+      for (var j = 0; j < len; j++) {
+        catman.addCategoryEntry(kServiceCats[j],
+          kServiceCtrId, kServiceCtrId, true, true, null);
+      }
+      this.registered = true;
+    } 
+  },
+  
+  unregisterSelf: function(compMgr, fileSpec, location) 
+  {
+    compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar)
+           .unregisterFactoryLocation(kServiceCId, fileSpec);
+    const catman = Components.classes['@mozilla.org/categorymanager;1']
+                             .getService(Components.interfaces.nsICategoryManager);
+    for (var j = 0, len = kServiceCats.length; j < len; j++) {
+      catman.deleteCategoryEntry(kServiceCats[j], kServiceCtrId, true);
+    }
+  },
+  
+  getClassObject: function(compMgr, cid, iid) 
+  {
+    if(cid.equals(kServiceCId))
+      return kServiceFactory;
+    
+    throw Components.results[
+      iid.equals(Components.interfaces.nsIFactory)
+      ? "NS_ERROR_NO_INTERFACE"
+      : "NS_ERROR_NOT_IMPLEMENTED"
+    ];
+    
+  },
+  
+  canUnload: function(compMgr) 
+  {
+    return true;
+  }
+};
+
+// entrypoint
+function NSGetModule(compMgr, fileSpec) {
+  return Module;
+}
