a08d7d1306e99a8bbf5664e5c43051ed57c5e3f9
mgalli%geckonnection.com <mgalli%geckonnection.com>
2005-08-23 15:21:09 +0000

    Clipboard copy and paste for text unicode.

--- minimo/chrome/content/minimo.js
+++ minimo/chrome/content/minimo.js
@@ -340,24 +340,37 @@ function BrowserResetZoomMinus() {
 }
 
 /* 
-  Testing the SMS and Call Services 
-*/
-function BrowserTestSendCall(toCall) {
-  var phoneInterface= Components.classes["@mozilla.org/phone/support;1"].createInstance(Components.interfaces.nsIPhoneSupport);
-  phoneInterface.makeCall(toCall,"");
-}
-
-/* 
   We want to intercept before it shows, 
   to evaluate when the selected content area is a phone number, 
   thus mutate the popup menu to the right make call item 
 */ 
 function BrowserPopupShowing () {
-	var selectedRange=getBrowser().selectedBrowser.contentDocument.getSelection();
-    document.getElementById("item_call").label="Call \""+ selectedRange + " \"";
-    document.getElementById("item_call").setAttribute("oncommand","BrowserTestSendCall("+selectedRange+")");
+  var selectedRange=getBrowser().selectedBrowser.contentDocument.getSelection();
+  document.getElementById("item-call").label="Call \""+ selectedRange + " \"";
+  document.getElementById("item-call").setAttribute("oncommand","DoTestSendCall("+selectedRange+")");
+  
+  /* Enable Copy */
+  if(selectedRange.toString()) {
+    document.getElementById("item-copy").style.display="block";
+  }
+  
+  /* Enable Paste - Can paste only if the focused element has a value attribute. :) 
+    THis may affect XHTML nodes. Users may be able to paste things within XML nodes. 
+  */
+  if(document.commandDispatcher.focusedElement.nodeName=="INPUT"||document.commandDispatcher.focusedElement.nodeName=="TEXTAREA") {
+    if(DoClipCheckPaste()) {
+      document.getElementById("item-paste").style.display="block";	
+    }
+  }
 }
 
+/* 
+  Testing the SMS and Call Services 
+*/
+function DoTestSendCall(toCall) {
+  var phoneInterface= Components.classes["@mozilla.org/phone/support;1"].createInstance(Components.interfaces.nsIPhoneSupport);
+  phoneInterface.makeCall(toCall,"");
+}
 
 function DoFullScreen()
 {
@@ -370,7 +383,61 @@ function DoFullScreen()
   window.fullScreen = gFullScreen;  
 }
 
+/* 
 
+ */
+function DoClipCopy()
+{
+  var copytext=getBrowser().selectedBrowser.contentDocument.getSelection().toString();
+  var str = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString);
+  if (!str) return false;
+  str.data = copytext;
+  var trans = Components.classes["@mozilla.org/widget/transferable;1"].createInstance(Components.interfaces.nsITransferable);
+  if (!trans) return false;
+  trans.addDataFlavor("text/unicode");
+  trans.setTransferData("text/unicode",str,copytext.length * 2);
+  var clipid = Components.interfaces.nsIClipboard;
+  var clip = Components.classes["@mozilla.org/widget/clipboard;1"].getService(clipid);
+  if (!clip) return false;
+  clip.setData(trans,null,clipid.kGlobalClipboard);
+}
+
+/* 
+ Currently supports text/unicode. 
+ */
+function DoClipCheckPaste()
+{
+  var clip = Components.classes["@mozilla.org/widget/clipboard;1"].getService(Components.interfaces.nsIClipboard);
+  if (!clip) return false;
+  var trans = Components.classes["@mozilla.org/widget/transferable;1"].createInstance(Components.interfaces.nsITransferable);
+  if (!trans) return false;
+  trans.addDataFlavor("text/unicode");
+  clip.getData(trans,clip.kGlobalClipboard);
+  var str = new Object();
+  var strLength = new Object();
+  var pastetext = null;
+  trans.getTransferData("text/unicode",str,strLength);
+  if (str) str = str.value.QueryInterface(Components.interfaces.nsISupportsString);
+  if (str) pastetext = str.data.substring(0,strLength.value / 2);
+  if(pastetext) {
+    return pastetext;
+  } else return false;
+}
+
+/* 
+ Currently supports pasting text/unicode within INPUT and TEXTAREA
+ */
+function DoClipPaste()
+{
+
+  var pasteTxt=DoClipCheckPaste();
+
+  /* With 007 does not fail. Maybe now focus is on the context menu */
+  if(document.commandDispatcher.focusedElement) {
+
+    document.commandDispatcher.focusedElement.value=pasteTxt;
+  }
+}
 
 function addToUrlbarHistory()
 {
@@ -436,3 +503,4 @@ function BrowserToogleSiteSSR()
 
   BrowserReload(); // hack until this is done by ssr itself
 }
+
