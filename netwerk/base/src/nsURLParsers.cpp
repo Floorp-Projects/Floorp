6bb445b61e7eccb34f8e40321dc5fd743284d713
andreas.otte@debitel.net <andreas.otte@debitel.net>
2005-08-25 19:25:29 +0000

    Backing out the preliminary fix for bug 304904 it breaks thunderbird local folders

--- netwerk/base/src/nsURLParsers.cpp
+++ netwerk/base/src/nsURLParsers.cpp
@@ -21,7 +21,6 @@
  *
  * Contributor(s):
  *   Darin Fisher (original author)
- *   Andreas Otte
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -577,8 +576,7 @@ nsAuthURLParser::ParseServerInfo(const char *serverinfo, PRInt32 serverinfoLen,
                     colon = p;
                 break;
             case ' ':
-            case '%':
-                // hostname must not contain a space or a %
+                // hostname must not contain a space
                 NS_WARNING("malformed hostname");
                 return NS_ERROR_MALFORMED_URI;
         }
