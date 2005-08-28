45ac052fd93e9272d90a81751e2f499415992f84
igor%mir2.org <igor%mir2.org>
2005-08-28 19:49:21 +0000

    Changing the example to use the supported syntax, the older syntax did not work since 1.5R1 release or so.

--- js/rhino/examples/enum.js
+++ js/rhino/examples/enum.js
@@ -1,5 +1,5 @@
-/* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- *
+/* -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
+ *      
  * The contents of this file are subject to the Netscape Public
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
@@ -34,36 +34,32 @@
  */
 
 /*
-	enum.js
+Implementing the interface java.util.Enumeration passing the object
+with JavaScript implementation directly to the constructor.
+This is a shorthand for JavaAdapter constructor:
 
-	Implementing the interface java.util.Enumeration using the new syntax.
-	Note that this syntax is experimental only, and hasn't been approved
-	by ECMA.
-	The same functionality can be had without the new syntax using the
-	uglier syntax:
-
-		var elements = new JavaAdapter(java.util.Enumeration, {
-						index: 0, elements: array,
-						hasMoreElements: function ...
-						nextElement: function ...
+elements = new JavaAdapter(java.util.Enumeration, {
+        index: 0, 
+        elements: array,
+	hasMoreElements: function ...
+        nextElement: function ...
 		});
-
-	by Patrick C. Beard.
  */
 
 // an array to enumerate.
 var array = [0, 1, 2];
 
 // create an array enumeration.
-var elements = new java.util.Enumeration() {
-	index: 0, elements: array,
-	hasMoreElements: function() {
-		return (this.index < this.elements.length);
-	},
-	nextElement: function() {
-		return this.elements[this.index++];
+var elements = new java.util.Enumeration({
+        index: 0,
+        elements: array,
+        hasMoreElements: function() {
+                return (this.index < this.elements.length);
+	},      
+        nextElement: function() {
+                return this.elements[this.index++];
 	}
-};
+    });
 
 // now print out the array by enumerating through the Enumeration
 while (elements.hasMoreElements())
