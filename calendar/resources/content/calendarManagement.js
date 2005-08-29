4bcb11b5ca46fd4053dbe889d71f6cc1fa627415
jminta%gmail.com <jminta%gmail.com>
2005-08-29 16:43:26 +0000

    Backing out patch from Bug 306241, can break getContrastingTextColor r=mvl

--- calendar/resources/content/calendarManagement.js
+++ calendar/resources/content/calendarManagement.js
@@ -323,7 +323,8 @@ function updateStyleSheetForObject(object) {
     if (object.uri) {
         color = getCalendarManager().getCalendarPref(object, 'color');
         rule.style.backgroundColor = color;
-        rule.style.color = getContrastingTextColor(color);
+        if (color)
+            rule.style.color = getContrastingTextColor(color);
         return;
     }
     var categoryPrefBranch = prefService.getBranch("calendar.category.color.");
