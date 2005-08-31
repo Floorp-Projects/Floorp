6232847ccaeb4095f58516f35988a7fb285d2789
pavlov%pavlov.net <pavlov%pavlov.net>
2005-08-31 22:15:33 +0000

    adding support for image encoding.  bug 245684.  patches from Brett Wilson <brettw@gmail.com>.  r=me, sr=vlad

---
+++ modules/libpr0n/encoders/png/nsPNGEncoder.h
@@ -0,0 +1,78 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Oracle Corporation code.
+ *
+ * The Initial Developer of the Original Code is Oracle Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Stuart Parmenter <pavlov@pavlov.net>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#include "imgIEncoder.h"
+
+#include <png.h>
+
+#define NS_PNGENCODER_CID \
+{ /* 38d1592e-b81e-432b-86f8-471878bbfe07 */         \
+     0x38d1592e,                                     \
+     0xb81e,                                         \
+     0x432b,                                         \
+    {0x86, 0xf8, 0x47, 0x18, 0x78, 0xbb, 0xfe, 0x07} \
+}
+
+// Provides PNG encoding functionality. Use InitFromData() to do the
+// encoding. See that function definition for encoding options.
+
+class nsPNGEncoder : public imgIEncoder
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_IMGIENCODER
+  NS_DECL_NSIINPUTSTREAM
+
+  nsPNGEncoder();
+
+private:
+  ~nsPNGEncoder();
+
+protected:
+  void ConvertHostARGBRow(const PRUint8* aSrc, PRUint8* aDest,
+                          PRUint32 aPixelWidth, PRBool aUseTransparency);
+  void StripAlpha(const PRUint8* aSrc, PRUint8* aDest,
+                  PRUint32 aPixelWidth);
+  static void WriteCallback(png_structp png, png_bytep data, png_size_t size);
+
+  // image buffer
+  PRUint8* mImageBuffer;
+  PRUint32 mImageBufferSize;
+  PRUint32 mImageBufferUsed;
+
+  PRUint32 mImageBufferReadPoint;
+};
