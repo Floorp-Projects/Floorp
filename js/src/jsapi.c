37d151a259bcbabf5169c312dd3afec5cb7be6d7
bzbarsky%mit.edu <bzbarsky%mit.edu>
2005-08-31 04:26:08 +0000

    Fix bug 305959 -- only return "function" from typeof for Scripts, functions, and regexps.  Patch by Blake Kaplan <mrbkap@gmail.com>, r=brendan

--- js/src/jsapi.c
+++ js/src/jsapi.c
@@ -621,7 +621,9 @@ JS_TypeOfValue(JSContext *cx, jsval v)
                  */
                 clasp = OBJ_GET_CLASS(cx, obj);
                 if ((ops == &js_ObjectOps)
-                    ? clasp == &js_FunctionClass || clasp->call
+                    ? (clasp->call
+                       ? (clasp == &js_RegExpClass || clasp == &js_ScriptClass)
+                       : clasp == &js_FunctionClass)
                     : ops->call != NULL) {
                     type = JSTYPE_FUNCTION;
                 } else {
