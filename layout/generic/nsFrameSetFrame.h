0cfd6a71cad338565eed352deb533c56fefc84b9
bzbarsky%mit.edu <bzbarsky%mit.edu>
2005-09-06 02:14:36 +0000

    Make addition or removal of frames in a frameset actually change the rendering.  Bug 306660, r=jst, sr=roc

--- layout/generic/nsFrameSetFrame.h
+++ layout/generic/nsFrameSetFrame.h
@@ -152,6 +152,8 @@ public:
                     const nsHTMLReflowState& aReflowState,
                     nsReflowStatus&          aStatus);
 
+  virtual nsIAtom* GetType() const;
+
   virtual PRBool IsLeaf() const;
   
   NS_IMETHOD  VerifyTree() const;
