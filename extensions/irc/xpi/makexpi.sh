ec980a61b5ac68dec54cfac1f779b70405bb0e9b
silver%warwickcompsoc.co.uk <silver%warwickcompsoc.co.uk>
2005-09-12 19:00:59 +0000

    Bug 302907 - Support FreeBSD in XPI generation script. ChatZilla only. r=rginda p=bugmail@millibyte.net (Marco Perez)

--- extensions/irc/xpi/makexpi.sh
+++ extensions/irc/xpi/makexpi.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/sh
 
 # Set up paths for finding files.
 if [ -z "$FEDIR" ]; then FEDIR=$PWD/..; fi
@@ -8,7 +8,7 @@ if [ -z "$XPIROOT" ]; then XPIROOT=$PWD/xpi-tree; fi
 if [ -z "$JARROOT" ]; then JARROOT=$PWD/jar-tree; fi
 
 
-if [ "$1" == "clean" ]; then
+if [ "$1" = "clean" ]; then
   echo -n "Cleaning up files"
   echo -n .
   rm -rf "$XPIROOT"
@@ -32,7 +32,7 @@ fi
 
 
 # Extract version number.
-VERSION=`grep "const __cz_version" "$FEDIR/xul/content/static.js" | sed "s|.*\"\([^\"]\+\)\".*|\1|"`
+VERSION=`grep "const __cz_version" "$FEDIR/xul/content/static.js" | sed "s|.*\"\([^\"]\{1,\}\)\".*|\1|"`
 
 echo Begining build of ChatZilla $VERSION...
 
@@ -80,7 +80,7 @@ echo -n .
 sed "s|@REVISION@|$VERSION|g" < "$XPIFILES/install.js" > "$XPIROOT/install.js"
 echo -n .
 mv "$FEDIR/xul/content/contents.rdf" "$FEDIR/xul/content/contents.rdf.in"
-sed "s|@MOZILLA_VERSION@|cz-$VERSION|g;s|\(chrome:displayName=\)\"[^\"]\+\"|\1\"ChatZilla $VERSION\"|g" < "$FEDIR/xul/content/contents.rdf.in" > "$FEDIR/xul/content/contents.rdf"
+sed "s|@MOZILLA_VERSION@|cz-$VERSION|g;s|\(chrome:displayName=\)\"[^\"]\{1,\}\"|\1\"ChatZilla $VERSION\"|g" < "$FEDIR/xul/content/contents.rdf.in" > "$FEDIR/xul/content/contents.rdf"
 rm "$FEDIR/xul/content/contents.rdf.in"
 echo -n .
 mv "$FEDIR/xul/locale/en-US/contents.rdf" "$FEDIR/xul/locale/en-US/contents.rdf.in"
@@ -92,7 +92,8 @@ echo   ".   done"
 # Create JAR.
 echo -n "  Constructing JAR package"
 echo -n .
-pushd "$CONFIGDIR" >/dev/null
+OLDPWD=`pwd`
+cd "$CONFIGDIR"
 echo -n .
 perl make-jars.pl -v -z zip -p preprocessor.pl -s "$FEDIR" -d "$JARROOT" < "$FEDIR/jar.mn" 1>log.tmp 2>&1
 if [ "$?" != "0" ]; then echo ERROR; cat log.tmp; exit 1; fi
@@ -103,7 +104,7 @@ echo -n .
 perl make-jars.pl -v -z zip -p preprocessor.pl -s "$FEDIR/ff" -d "$JARROOT" < "$FEDIR/ff/jar.mn" 1>log.tmp 2>&1
 if [ "$?" != "0" ]; then echo ERROR; cat log.tmp; exit 1; fi
 echo -n .
-popd >/dev/null
+cd "$OLDPWD"
 echo   ".         done"
 
 
@@ -120,7 +121,10 @@ chmod 664 "$XPIROOT/chrome/chatzilla.jar"
 echo -n .
 chmod 664 "$XPIROOT/components/chatzilla-service.js"
 echo -n .
-pushd "$XPIROOT" >/dev/null; zip -vr ../chatzilla-$VERSION.xpi * 1>log.tmp 2>&1; popd >/dev/null
+OLDPWD=`pwd`
+cd "$XPIROOT"
+zip -vr ../chatzilla-$VERSION.xpi * 1>log.tmp 2>&1
+cd "$OLDPWD"
 if [ "$?" != "0" ]; then echo ERROR; cat log.tmp; exit 1; fi
 echo   ".         done"
 
