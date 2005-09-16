b287f8e5edcf03fa3e9c57e87ff9f0866c154d86
timeless@mozdev.org <timeless@mozdev.org>
2005-09-16 07:23:16 +0000

    Adding support to spidermonkey standalone (not part of gecko build) for mingw:
    uname -a
    MINGW32_NT-5.1 PROFILER 1.0.10(0.46/3/2) 2004-03-15 07:17 i686 unknown
    
    uname -r
    1.0.10(0.46/3/2)
    
    uname -m
    i686
    
    uname -s
    MINGW32_NT-5.1
    
    mingw is really mean to /'s in commands, so since cl and friends are happy
    to use - instead, i'm switching the flags to do so.

--- js/src/config/WINNT5.0.mk
+++ js/src/config/WINNT5.0.mk
@@ -52,29 +52,29 @@ CPU_ARCH = x86 # XXX fixme
 GFX_ARCH = win32
 
 # MSVC compiler options for both debug/optimize
-# /nologo  - suppress copyright message
-# /W3      - Warning level 3
-# /Gm      - enable minimal rebuild
-# /Z7      - put debug info into the executable, not in .pdb file
-# /YX      - automatic precompiled headers
-# /GX      - enable C++ exception support
-WIN_CFLAGS = /nologo /W3 /Fp$(OBJDIR)/js.pch
+# -nologo  - suppress copyright message
+# -W3      - Warning level 3
+# -Gm      - enable minimal rebuild
+# -Z7      - put debug info into the executable, not in .pdb file
+# -YX      - automatic precompiled headers
+# -GX      - enable C++ exception support
+WIN_CFLAGS = -nologo -W3 -Fp$(OBJDIR)/js.pch
 
 # MSVC compiler options for debug builds linked to MSVCRTD.DLL
-# /MDd     - link with MSVCRTD.LIB (Dynamically-linked, multi-threaded, debug C-runtime)
-# /Od      - minimal optimization
-WIN_IDG_CFLAGS = /MDd /Od /Z7 
+# -MDd     - link with MSVCRTD.LIB (Dynamically-linked, multi-threaded, debug C-runtime)
+# -Od      - minimal optimization
+WIN_IDG_CFLAGS = -MDd -Od -Z7 
 
 # MSVC compiler options for debug builds linked to MSVCRT.DLL
-# /MD      - link with MSVCRT.LIB (Dynamically-linked, multi-threaded, debug C-runtime)
-# /Od      - minimal optimization
-WIN_DEBUG_CFLAGS = /MD /Od /Z7 
+# -MD      - link with MSVCRT.LIB (Dynamically-linked, multi-threaded, debug C-runtime)
+# -Od      - minimal optimization
+WIN_DEBUG_CFLAGS = -MD -Od -Z7 
 
 # MSVC compiler options for release (optimized) builds
-# /MD      - link with MSVCRT.LIB (Dynamically-linked, multi-threaded, C-runtime)
-# /O2      - Optimize for speed
-# /G5      - Optimize for Pentium
-WIN_OPT_CFLAGS = /MD /O2
+# -MD      - link with MSVCRT.LIB (Dynamically-linked, multi-threaded, C-runtime)
+# -O2      - Optimize for speed
+# -G5      - Optimize for Pentium
+WIN_OPT_CFLAGS = -MD -O2
 
 ifdef BUILD_OPT
 OPTIMIZER = $(WIN_OPT_CFLAGS)
@@ -94,16 +94,16 @@ PREBUILT_CPUCFG = 1
 USE_MSVC = 1
 
 LIB_LINK_FLAGS=kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib\
- advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib oldnames.lib /nologo\
- /subsystem:windows /dll /debug /pdb:none\
- /machine:I386\
- /opt:ref /opt:noicf
+ advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib oldnames.lib -nologo\
+ -subsystem:windows -dll -debug -pdb:none\
+ -machine:I386\
+ -opt:ref -opt:noicf
 
 EXE_LINK_FLAGS=kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib\
- advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib oldnames.lib /nologo\
- /subsystem:console /debug /pdb:none\
- /machine:I386\
- /opt:ref /opt:noicf
+ advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib oldnames.lib -nologo\
+ -subsystem:console -debug -pdb:none\
+ -machine:I386\
+ -opt:ref -opt:noicf
 
 # CAFEDIR = t:/cafe
 # JCLASSPATH = $(CAFEDIR)/Java/Lib/classes.zip
