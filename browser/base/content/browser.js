a28e6a52e3e8ab262e8dc4bf377d0a229a59ab65
gavin@gavinsharp.com <gavin@gavinsharp.com>
2005-09-18 21:31:34 +0000

    Bug 294383: Disable certain menu options (view source, find, text zoom, etc) when viewing non-text content (images, plugins), r=mconnor

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -3208,6 +3208,7 @@ nsBrowserStatusHandler.prototype =
     this.statusTextField        = document.getElementById("statusbar-display");
     this.securityButton         = document.getElementById("security-button");
     this.urlBar                 = document.getElementById("urlbar");
+    this.isImage                = document.getElementById("isImage");
 
     // Initialize the security button's state and tooltip text
     var securityUI = getBrowser().securityUI;
@@ -3263,6 +3264,14 @@ nsBrowserStatusHandler.prototype =
       this.statusTextField.label = text;
       this.statusText = text;
     }
+  },
+  
+  mimeTypeIsTextBased : function(contentType)
+  {
+    return /^text\/|\+xml$/.test(contentType) ||
+           contentType == "application/x-javascript" ||
+           contentType == "application/xml" ||
+           contentType == "mozilla.application/cached-xul";
   },
 
   onLinkIconAvailable : function(aBrowser)
@@ -3370,6 +3379,12 @@ nsBrowserStatusHandler.prototype =
           }
           this.status = "";
           this.setDefaultStatus(msg);
+
+          // Disable menu entries for images, enable otherwise
+          if (content.document && this.mimeTypeIsTextBased(content.document.contentType))
+            this.isImage.removeAttribute('disabled');
+          else
+            this.isImage.setAttribute('disabled', 'true');
         }
 
         // Turn the progress meter and throbber off.
@@ -3412,6 +3427,12 @@ nsBrowserStatusHandler.prototype =
       }
     }
     selectedBrowser.lastURI = aLocation;
+
+    // Disable menu entries for images, enable otherwise
+    if (content.document && this.mimeTypeIsTextBased(content.document.contentType))
+      this.isImage.removeAttribute('disabled');
+    else
+      this.isImage.setAttribute('disabled', 'true');
 
     this.setOverLink("", null);
 
