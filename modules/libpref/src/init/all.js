4d23e7b3e2c562a4706f08e8a39109262f62fd96
gerv@gerv.net <gerv@gerv.net>
2005-09-20 09:10:23 +0000

    Bug 308344: add .vn to list of IDN TLDs. Patch by gerv; a=asa.

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -593,6 +593,7 @@ pref("network.IDN.whitelist.sh", true);
 pref("network.IDN.whitelist.th", true);
 pref("network.IDN.whitelist.tm", true);
 pref("network.IDN.whitelist.tw", true);
+pref("network.IDN.whitelist.vn", true);
 
 // non-ccTLDs
 pref("network.IDN.whitelist.info", true);
