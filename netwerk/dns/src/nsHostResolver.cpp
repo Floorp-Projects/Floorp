8583616baf4c14820cb0a33402e117eed75905df
darin@meer.net <darin@meer.net>
2005-09-22 21:38:12 +0000

    Fixes bug 304904 "Necko should refuse to look up invalid hostnames containing '%'" patch by andreas.otte@debitel.net, r=biesi, sr=darin

--- netwerk/dns/src/nsHostResolver.cpp
+++ netwerk/dns/src/nsHostResolver.cpp
@@ -64,6 +64,7 @@
 #include "prlog.h"
 #include "pldhash.h"
 #include "plstr.h"
+#include "nsURLHelper.h"
 
 //----------------------------------------------------------------------------
 
@@ -382,6 +383,11 @@ nsHostResolver::ResolveHost(const char            *host,
     NS_ENSURE_TRUE(host && *host, NS_ERROR_UNEXPECTED);
 
     LOG(("nsHostResolver::ResolveHost [host=%s]\n", host));
+
+    // ensure that we are working with a valid hostname before proceeding.  see
+    // bug 304904 for details.
+    if (!net_IsValidHostName(nsDependentCString(host)))
+        return NS_ERROR_UNKNOWN_HOST;
 
     // if result is set inside the lock, then we need to issue the
     // callback before returning.
