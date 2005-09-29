3cc631ed601be8579d35b60a696e2d6dda2d6282
mozilla.mano@sent.com <mozilla.mano@sent.com>
2005-09-29 01:47:43 +0000

    Bug 310373, r=mconnor.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2617,8 +2617,10 @@ var newTabButtonObserver = {
     {
       var xferData = aXferData.data.split("\n");
       var url = xferData[0] ? xferData[0] : xferData[1];
-      if (url)
+      if (url) {
+        getBrowser().dragDropSecurityCheck(aEvent, aDragSession, url);
         openNewTabWith(url, null, null, aEvent);
+      }
     },
   getSupportedFlavours: function ()
     {
@@ -2648,8 +2650,10 @@ var newWindowButtonObserver = {
     {
       var xferData = aXferData.data.split("\n");
       var url = xferData[0] ? xferData[0] : xferData[1];
-      if (url)
+      if (url) {
+        getBrowser().dragDropSecurityCheck(aEvent, aDragSession, url);
         openNewWindowWith(url, null, null);
+      }
     },
   getSupportedFlavours: function ()
     {
