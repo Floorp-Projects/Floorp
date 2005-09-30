50e60565321bd2fcf75b72bbafba54049676d598
dbaron@dbaron.org <dbaron@dbaron.org>
2005-09-30 19:25:42 +0000

    Back out bug 281709 since it makes tabbed browsing unusable on at least GTK2.

--- view/src/nsView.cpp
+++ view/src/nsView.cpp
@@ -427,16 +427,6 @@ void nsView::ResetWidgetBounds(PRBool aRecurse, PRBool aMoveOnly,
         mWindow->Resize(newBounds.width, newBounds.height, aInvalidateChangedSize);
       } // else do nothing!
     }
-    PRBool vis = PR_FALSE;
-    mWindow->IsVisible(vis);
-    if (vis != (mVis == nsViewVisibility_kShow)) {
-#ifndef HIDE_ALL_WIDGETS
-      if (mVis == nsViewVisibility_kShow)
-        mWindow->Show(PR_TRUE);
-      else
-#endif
-        mWindow->Show(PR_FALSE);
-    }
   } else if (aRecurse) {
     // reposition any widgets under this view
     for (nsView* v = GetFirstChild(); v; v = v->GetNextSibling()) {
@@ -475,7 +465,15 @@ NS_IMETHODIMP nsView::SetVisibility(nsViewVisibility aVisibility)
     DropMouseGrabbing();
   }
 
-  ResetWidgetBounds(PR_FALSE, PR_FALSE, PR_FALSE);
+  if (nsnull != mWindow)
+  {
+#ifndef HIDE_ALL_WIDGETS
+    if (mVis == nsViewVisibility_kShow)
+      mWindow->Show(PR_TRUE);
+    else
+#endif
+      mWindow->Show(PR_FALSE);
+  }
 
   return NS_OK;
 }
