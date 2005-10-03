645f7af5f577f7a5f1d03fd14730f54d89b4a1a7
bugzilla%standard8.demon.co.uk <bugzilla%standard8.demon.co.uk>
2005-10-03 17:53:32 +0000

    Fix bug 212221 XUL error pages do not play nicely with IM button in Addressbook (SeaMonkey patch) r=mnyromyr,sr=neil.parkwaycc.co.uk

--- mailnews/addrbook/resources/content/addressbook.js
+++ mailnews/addrbook/resources/content/addressbook.js
@@ -216,6 +216,12 @@ function OnLoadAddressBook()
 
   var dirTree = GetDirTree();
   dirTree.addEventListener("click",DirPaneClick,true);
+
+  // Ensure we don't load xul error pages into the main window
+  window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+        .getInterface(Components.interfaces.nsIWebNavigation)
+        .QueryInterface(Components.interfaces.nsIDocShell)
+        .useErrorPages = false;
 }
 
 function OnLoadDirTree() {
@@ -848,9 +854,11 @@ function IsCardViewAndAbResultsPaneSplitterCollapsed()
 
 function LaunchUrl(url)
 {
-  var messenger = Components.classes["@mozilla.org/messenger;1"].createInstance(Components.interfaces.nsIMessenger);
-  messenger.SetWindow(window, null);
-  messenger.OpenURL(url);
+  // Doesn't matter if this bit fails, window.location contains its own prompts
+  try {
+    window.location = url;
+  }
+  catch (ex) {}
 }
 
 function AbIMSelected()
