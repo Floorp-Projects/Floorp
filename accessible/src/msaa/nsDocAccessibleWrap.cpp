51dd2d7ff89990b7bc5f3493dcb2cdbbe6598fc3
aaronleventhal%moonset.net <aaronleventhal%moonset.net>
2005-10-09 02:36:27 +0000

    Bug 311430. Not firing EVENT_REORDER for frames/iframes that finish loading. r=parente, sr=jst

--- accessible/src/msaa/nsDocAccessibleWrap.cpp
+++ accessible/src/msaa/nsDocAccessibleWrap.cpp
@@ -450,16 +450,6 @@ NS_IMETHODIMP nsDocAccessibleWrap::FireDocLoadingEvent(PRBool aIsFinished)
 
   nsDocAccessible::FireDocLoadingEvent(aIsFinished);
 
-  nsCOMPtr<nsIDocShellTreeItem> treeItem = GetDocShellTreeItemFor(mDOMNode);
-  if (!treeItem) {
-    return NS_OK;
-  }
-  nsCOMPtr<nsIDocShellTreeItem> sameTypeRoot;
-  treeItem->GetSameTypeRootTreeItem(getter_AddRefs(sameTypeRoot));
-  if (sameTypeRoot != treeItem) {
-    return NS_OK; // We only fire MSAA doc loading events for root content frame
-  }
-
   if (aIsFinished) {
     // Use short timer before firing state change event for finished doc,
     // because the window is made visible asynchronously by Microsoft Windows
@@ -472,6 +462,17 @@ NS_IMETHODIMP nsDocAccessibleWrap::FireDocLoadingEvent(PRBool aIsFinished)
     }
   }
   else {
+    nsCOMPtr<nsIDocShellTreeItem> treeItem = GetDocShellTreeItemFor(mDOMNode);
+    if (!treeItem) {
+      return NS_OK;
+    }
+
+    nsCOMPtr<nsIDocShellTreeItem> sameTypeRoot;
+    treeItem->GetSameTypeRootTreeItem(getter_AddRefs(sameTypeRoot));
+    if (sameTypeRoot != treeItem) {
+      return NS_OK; // We only fire MSAA doc loading events for root content frame
+    }
+
     FireToolkitEvent(nsIAccessibleEvent::EVENT_STATE_CHANGE, this, nsnull);
   }
 
