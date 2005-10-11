6065a42885b53059e0073da789ef3833071185b0
bob@bclary.com <bob@bclary.com>
2005-10-11 19:03:02 +0000

    Fix expected value for CDATA section nodeKind

--- js/tests/e4x/Expressions/11.4.1-02.js
+++ js/tests/e4x/Expressions/11.4.1-02.js
@@ -40,7 +40,7 @@ START("11.1.4 - XML Initializer");
 var bug = 257679;
 var summary = 'XML Initializer should accept single CDATA Section';
 var actual = '';
-var expect = 'processing-instruction';
+var expect = 'text';
 
 printBugNumber (bug);
 printStatus (summary);
