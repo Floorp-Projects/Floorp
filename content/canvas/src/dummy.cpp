15df5b5c47e9e731fb3411ec892e0e188814fced
jst@mozilla.jstenback.com <jst@mozilla.jstenback.com>
2005-10-17 23:39:20 +0000

    Landing fix for bug 311143, patch by jeffh@activestate.com. Make the canvas code linkable with Sun's linker. r=vladimir@pobox.com, sr=jst@mozilla.com

--- content/canvas/src/dummy.cpp
+++ content/canvas/src/dummy.cpp
@@ -1,4 +1,8 @@
 /*
  * This file only exists because some platforms can't handle building an empty .a
  * file, or .lib, or whatever (e.g. OS X, possibly Win32).
+ *
+ * Need at least one external symbol for some linkers to create a proper
+ * archive file: https://bugzilla.mozilla.org/show_bug.cgi?id=311143
  */
+void concvsdummy(void) {}
