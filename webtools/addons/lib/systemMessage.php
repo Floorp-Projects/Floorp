045c44e98873570b4c95a36472389fbeb4d4d18f
mike.morgan%oregonstate.edu <mike.morgan%oregonstate.edu>
2005-10-17 17:47:40 +0000

    Fix for bug 312611.

--- webtools/addons/lib/systemMessage.php
+++ webtools/addons/lib/systemMessage.php
@@ -12,8 +12,8 @@
 
 // these values can be done as a DB table so they can be controlled and edited by site admins without access to the webserver itself
 function getSystemMessageData() {
-  $messageData['header'] = "Want to get involved?"
-  $messageData['text'] = "We are looking for volunteers to help us with UMO. We are in need of PHP developers to help with redesigning the site, and people to review extensions and themes that get submitted to UMO. We especially need Mac and Thunderbird users. If you are interested in being a part of this exciting project, please send information such as your full name, timezone and experience to <a href="mailto:umo-reviewer@mozilla.org?subject=Review%20Application%20for%20[Name Here]">umo-reviewer@mozilla.org</a>. Also, please join us in #umo on irc.mozilla.org to start getting a feeling for what's up or for a more informal chat.";
+  $messageData['header'] = 'Want to get involved?';
+  $messageData['text'] = 'We are looking for volunteers to help us with UMO. We are in need of PHP developers to help with redesigning the site, and people to review extensions and themes that get submitted to UMO. We especially need Mac and Thunderbird users. If you are interested in being a part of this exciting project, please send information such as your full name, timezone and experience to <a href="mailto:umo-reviewer@mozilla.org?subject=Review%20Application%20for%20[Name Here]">umo-reviewer@mozilla.org</a>. Also, please join us in #umo on irc.mozilla.org to start getting a feeling for what\'s up or for a more informal chat.';
   $messageData['pageType'] = 1;
 
   return $messageData;
