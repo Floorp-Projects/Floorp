0af8f2186f5fe1f7b06ed999722ceb53aafb3b95
brendan@mozilla.org <brendan@mozilla.org>
2005-10-20 18:58:24 +0000

    Use local roots effectively (312278, r=igor).

--- js/src/jsarray.c
+++ js/src/jsarray.c
@@ -363,7 +363,6 @@ array_join_sub(JSContext *cx, JSObject *obj, JSString *sep, JSBool literalize,
                jsval *rval, JSBool localeString)
 {
     JSBool ok;
-    jsval v;
     jsuint length, index;
     jschar *chars, *ochars;
     size_t nchars, growth, seplen, tmplen;
@@ -444,6 +443,9 @@ array_join_sub(JSContext *cx, JSObject *obj, JSString *sep, JSBool literalize,
     sepstr = NULL;
     seplen = JSSTRING_LENGTH(sep);
 
+    /* Use rval to locally root each element value as we loop and convert. */
+#define v (*rval)
+
     v = JSVAL_NULL;
     for (index = 0; index < length; index++) {
         ok = JS_GetElement(cx, obj, index, &v);
@@ -517,6 +519,8 @@ array_join_sub(JSContext *cx, JSObject *obj, JSString *sep, JSBool literalize,
             free(chars);
         return ok;
     }
+
+#undef v
 
   make_string:
     if (!chars) {
