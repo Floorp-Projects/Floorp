61980520fdaf1be9c5b3feaba9b7badffa43014d
darin%meer.net <darin%meer.net>
2005-10-21 19:07:22 +0000

    fixes bug 309311 "Add yet more characters to the IDN blacklist" patch by Neil Harris (usenet@tonal.clara.co.uk), r=gerv, sr=dveditz

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -605,11 +605,8 @@ pref("network.IDN.whitelist.museum", true);
 // If a domain includes any of the following characters, it may be a spoof 
 // attempt and so we always display the domain name as punycode. This would 
 // override the settings "network.IDN_show_punycode" and 
-// "netword.IDN.whitelist.*".
-// The list currently contains the characters:
-// U+2044 FRACTION SLASH
-// U+2215 DIVISION SLASH
-pref("network.IDN.blacklist_chars", "⁄∕");
+// "network.IDN.whitelist.*".
+pref("network.IDN.blacklist_chars", "\u0020\u00A0\u00BC\u00BD\u01C3\u0337\u0338\u05C3\u05F4\u06D4\u0702\u115F\u1160\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u200B\u2024\u2027\u2028\u2029\u202F\u2039\u203A\u2044\u205F\u2154\u2155\u2156\u2159\u215A\u215B\u215F\u2215\u23AE\u29F6\u29F8\u2AFB\u2AFD\u2FF0\u2FF1\u2FF2\u2FF3\u2FF4\u2FF5\u2FF6\u2FF7\u2FF8\u2FF9\u2FFA\u2FFB\u3000\u3002\u3014\u3015\u3033\u3164\u321D\u321E\u33AE\u33AF\u33C6\u33DF\uFE14\uFE15\uFE3F\uFE5D\uFE5E\uFEFF\uFF0E\uFF0F\uFF61\uFFA0\uFFF9\uFFFA\uFFFB\uFFFC\uFFFD");
 
 // This preference specifies a list of domains for which DNS lookups will be
 // IPv4 only. Works around broken DNS servers which can't handle IPv6 lookups
