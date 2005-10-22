2713ae519049561c78e53bc6278ecb629314b512
allan@beaufour.dk <allan@beaufour.dk>
2005-10-22 15:54:44 +0000

    Remove extensions/xforms/packageMakefile. Bug 298431, r=benjamin

--- allmakefiles.sh
+++ allmakefiles.sh
@@ -1512,7 +1512,6 @@ for extension in $MOZ_EXTENSIONS; do
             " ;;
         xforms ) MAKEFILES_extensions="$MAKEFILES_extensions
             extensions/xforms/Makefile
-            extensions/xforms/package/Makefile
             " ;;
         xmlextras ) MAKEFILES_extensions="$MAKEFILES_extensions
             extensions/xmlextras/Makefile
