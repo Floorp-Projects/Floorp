d96a42353462d66f29db725dcfde740bac7faf3b
rjesup%wgate.com <rjesup%wgate.com>
2005-11-02 16:04:50 +0000

    Bug 94243: VoidArray usage patch.  a=asa sr=brendan r=waterson r=bienvenu r=pierre r=jband/dbradley  Also fixes some whitespace issues.

--- xpcom/glue/nsQuickSort.h
+++ xpcom/glue/nsQuickSort.h
@@ -36,6 +36,18 @@
  */
 NS_BEGIN_EXTERN_C
 
+/**
+ * Parameters:
+ *  1. the array to sort
+ *  2. the number of elements in the array
+ *  3. the size of the array
+ *  4. comparison function taking two elements and parameter #5 and
+ *     returning an integer:
+ *      + less than zero if the first element should be before the second
+ *      + 0 if the order of the elements does not matter
+ *      + greater than zero if the second element should be before the first
+ *  5. extra data to pass to comparison function
+ */
 PR_EXTERN(void) NS_QuickSort(void *, unsigned int, unsigned int,
                              int (*)(const void *, const void *, void *), 
                              void *);
