f04213829ff6f4f2821f29161c4e7106c5496bc1
alecf@netscape.com <alecf@netscape.com>
2005-11-02 16:04:35 +0000

    match up some reviewers comments: - tri-license with MPL, not NPL - be consistent with policy on null in the array (it is allowed) - move Clear() into implementation file, and hide static callback there too - make sure to AddRef() on the way out of nsArray::IndexOf() - handle failure of ReplaceElementAt more gracefully bug 162115, not part of build yet

--- xpcom/glue/nsCOMArray.h
+++ xpcom/glue/nsCOMArray.h
@@ -1,37 +1,38 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /* ***** BEGIN LICENSE BLOCK *****
- * Version: NPL 1.1/GPL 2.0/LGPL 2.1
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * The contents of this file are subject to the Netscape Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/NPL/
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
  *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is mozilla.org code.
+ * The Original Code is a COM aware array class.
  *
- * The Initial Developer of the Original Code is 
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1998
+ * The Initial Developer of the Original Code
+ * Netscape Communications Corp.
+ * Portions created by the Initial Developer are Copyright (C) 2002
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
+ *   Alec Flett <alecf@netscape.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or 
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
  * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the NPL, indicate your
+ * use your version of this file under the terms of the MPL, indicate your
  * decision by deleting the provisions above and replace them with the notice
  * and other provisions required by the GPL or the LGPL. If you do not delete
  * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the NPL, the GPL or the LGPL.
+ * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
 
@@ -62,14 +63,9 @@ protected:
     }
 
     // override nsVoidArray stuff so that they can be accessed by
-    // other methods
+    // consumers of nsCOMArray
     PRInt32 Count() const {
         return mArray.Count();
-    }
-
-    void Clear() {
-        mArray.EnumerateForwards(ClearObjectsCallback, nsnull);
-        mArray.Clear();
     }
 
     PRBool EnumerateForwards(nsVoidArrayEnumFunc aFunc, void* aData) {
@@ -79,6 +75,7 @@ protected:
     // any method which is not a direct forward to mArray should
     // avoid inline bodies, so that the compiler doesn't inline them
     // all over the place
+    void Clear();
     PRBool InsertObjectAt(nsISupports* aObject, PRInt32 aIndex);
     PRBool ReplaceObjectAt(nsISupports* aObject, PRInt32 aIndex);
     PRBool AppendObject(nsISupports *aObject);
@@ -87,11 +84,10 @@ protected:
 
  private:
     
-    static PRBool ClearObjectsCallback(void *aElement, void*);
-    
     // the actual storage
     nsVoidArray mArray;
-    
+
+    // don't implement these, defaults will muck with refcounts!
     nsCOMArray_base(const nsCOMArray_base& other);
     nsCOMArray_base& operator=(const nsCOMArray_base& other);
 };
@@ -157,7 +153,8 @@ class nsCOMArray : protected nsCOMArray_base
 
 
  private:
-    
+
+    // don't implement these!
     nsCOMArray(const nsCOMArray& other);
     nsCOMArray& operator=(const nsCOMArray& other);
 };
