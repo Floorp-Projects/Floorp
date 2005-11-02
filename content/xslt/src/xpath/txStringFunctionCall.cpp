2a217dfae0858148e9303210f5287e717caa0980
sicking@bigfoot.com <sicking@bigfoot.com>
2005-11-02 07:37:38 +0000

    Bug 140687: Make substring() for all start-indexes
    r=Pike, sr=jst

--- content/xslt/src/xpath/txStringFunctionCall.cpp
+++ content/xslt/src/xpath/txStringFunctionCall.cpp
@@ -169,7 +169,7 @@ ExprResult* StringFunctionCall::evaluate(Node* aContext, ContextState* aCs)
             // check for NaN or +/-Inf
             if (Double::isNaN(start) ||
                 Double::isInfinite(start) ||
-                start + 0.5 >= src.length())
+                start >= src.length() + 0.5)
                 return new StringResult();
 
             start = floor(start + 0.5) - 1;
