f7ee2f6f5d1ba5efa69fabbb34e5ad2afc05abc9
jshin%mailaps.org <jshin%mailaps.org>
2005-11-02 16:57:26 +0000

    separate xpcom wrapper and charset detectors : patch by shanjian with some updates by me (r=smontagu, sr=alecf)

--- extensions/universalchardet/src/base/nsUniversalDetector.h
+++ extensions/universalchardet/src/base/nsUniversalDetector.h
@@ -38,21 +38,6 @@
 #ifndef nsUniversalDetector_h__
 #define nsUniversalDetector_h__
 
-#include "nsISupports.h"
-#include "nsICharsetDetector.h"
-#include "nsIStringCharsetDetector.h"
-#include "nsICharsetDetectionObserver.h"
-
-#include "nsIFactory.h"
-
-// {374E0CDE-F605-4259-8C92-E639C6C2EEEF}
-#define NS_UNIVERSAL_DETECTOR_CID \
-{ 0x374e0cde, 0xf605, 0x4259, { 0x8c, 0x92, 0xe6, 0x39, 0xc6, 0xc2, 0xee, 0xef } }
-
-// {6EE5301A-3981-49bd-85F8-1A2CC228CF3E}
-#define NS_UNIVERSAL_STRING_DETECTOR_CID \
-{ 0x6ee5301a, 0x3981, 0x49bd, { 0x85, 0xf8, 0x1a, 0x2c, 0xc2, 0x28, 0xcf, 0x3e } }
-
 class nsCharSetProber;
 
 #define NUM_OF_CHARSET_PROBERS  3
@@ -84,42 +69,6 @@ protected:
 
    nsCharSetProber  *mCharSetProbers[NUM_OF_CHARSET_PROBERS];
    nsCharSetProber  *mEscCharSetProber;
-};
-
-//=====================================================================
-class nsUniversalXPCOMDetector :  
-      public nsUniversalDetector,
-      public nsICharsetDetector
-{
-  NS_DECL_ISUPPORTS
-  public:
-     nsUniversalXPCOMDetector();
-     virtual ~nsUniversalXPCOMDetector();
-     NS_IMETHOD Init(nsICharsetDetectionObserver* aObserver);
-     NS_IMETHOD DoIt(const char* aBuf, PRUint32 aLen, PRBool *oDontFeedMe);
-     NS_IMETHOD Done();
-  protected:
-     virtual void Report(const char* aCharset);
-  private:
-     nsICharsetDetectionObserver* mObserver;
-};
-
-//=====================================================================
-class nsUniversalXPCOMStringDetector :  
-      public nsUniversalDetector,
-      public nsIStringCharsetDetector
-{
-  NS_DECL_ISUPPORTS
-  public:
-     nsUniversalXPCOMStringDetector();
-     virtual ~nsUniversalXPCOMStringDetector();
-     NS_IMETHOD DoIt(const char* aBuf, PRUint32 aLen, 
-                     const char** oCharset, nsDetectionConfident &oConf);
-  protected:
-     virtual void Report(const char* aCharset);
-  private:
-     nsICharsetDetectionObserver* mObserver;
-     const char* mResult;
 };
 
 #endif
