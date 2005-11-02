59332e92b8943b81d7f083a8c043231e3605a299
axel@pike.org <axel@pike.org>
2005-11-02 07:34:17 +0000

    removing windows linefeeds, still reminents from the old days, not part of build

--- content/xslt/src/main/transformiix.cpp
+++ content/xslt/src/main/transformiix.cpp
@@ -31,7 +31,7 @@
  *    -- read XML from stdin when -i is omitted
  *    -- accept '-' to specify stdin/stdout on command line
  *
- * $Id: transformiix.cpp,v 1.6 2005/11/02 07:33:37 kvisco%ziplink.net Exp $
+ * $Id: transformiix.cpp,v 1.7 2005/11/02 07:33:38 axel%pike.org Exp $
  */
 
 
