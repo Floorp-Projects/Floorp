1f11edb2f3bfab085eb90a7b02ffa598ce57f2d5
gavin%gavinsharp.com <gavin%gavinsharp.com>
2005-11-02 06:03:07 +0000

    Bug 314647: remove onDownloadShowOptions, it's unused and browser specific, r=mconnor

--- toolkit/mozapps/downloads/content/downloads.js
+++ toolkit/mozapps/downloads/content/downloads.js
@@ -754,20 +754,6 @@ var gDownloadViewController = {
   }
 };
 
-function onDownloadShowOptions()
-{
-  var windowManager = Components.classes['@mozilla.org/appshell/window-mediator;1'].getService();
-  var windowManagerInterface = windowManager.QueryInterface(Components.interfaces.nsIWindowMediator);
-  var topWindow = windowManagerInterface.getMostRecentWindow("Browser:Options");
-  if (topWindow) {
-    topWindow.focus();
-    topWindow.switchPage("catDownloadsButton");
-  }
-  else
-    openDialog("chrome://browser/content/pref/pref.xul", "PrefWindow",
-              "chrome,titlebar,resizable,modal", "catDownloadsButton");
-}
-
 function onDownloadShowInfo()
 {
   if (gDownloadsView.selectedItem)
