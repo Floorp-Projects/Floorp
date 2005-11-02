d795692f0f06d1573649967ed2df846d132acf5f
seawood@netscape.com <seawood@netscape.com>
2005-11-02 07:39:27 +0000

    Purge XP_PC. Bug #56767 r=mkaply sr=alecf

--- content/xslt/src/xpath/txMultiplicativeExpr.cpp
+++ content/xslt/src/xpath/txMultiplicativeExpr.cpp
@@ -84,7 +84,7 @@ ExprResult* MultiplicativeExpr::evaluate(txIEvalContext* aContext)
     switch ( op ) {
         case DIVIDE:
             if (rightDbl == 0) {
-#ifdef XP_PC
+#if defined(XP_WIN) || defined(XP_OS2)
                 /* XXX MSVC miscompiles such that (NaN == 0) */
                 if (Double::isNaN(rightDbl))
                     result = Double::NaN;
@@ -105,7 +105,7 @@ ExprResult* MultiplicativeExpr::evaluate(txIEvalContext* aContext)
                 result = Double::NaN;
             }
             else {
-#ifdef XP_PC
+#if defined(XP_WIN) || defined(XP_OS2)
                 /* Workaround MS fmod bug where 42 % (1/0) => NaN, not 42. */
                 if (!Double::isInfinite(leftDbl) && Double::isInfinite(rightDbl))
                     result = leftDbl;
