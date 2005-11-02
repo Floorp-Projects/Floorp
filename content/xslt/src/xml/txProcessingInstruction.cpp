2dfc63a8c0f4292b26fb3eb4467c33a12beca4ab
kvisco%ziplink.net <kvisco%ziplink.net>
2005-11-02 07:33:52 +0000

    moved from source/dom

---
+++ content/xslt/src/xml/txProcessingInstruction.cpp
@@ -0,0 +1,93 @@
+/*
+ * (C) Copyright The MITRE Corporation 1999  All rights reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * The program provided "as is" without any warranty express or
+ * implied, including the warranty of non-infringement and the implied
+ * warranties of merchantibility and fitness for a particular purpose.
+ * The Copyright owner will not be liable for any damages suffered by
+ * you as a result of using the Program. In no event will the Copyright
+ * owner be liable for any special, indirect or consequential damages or
+ * lost profits even if the Copyright owner has been advised of the
+ * possibility of their occurrence.
+ *
+ * Please see release.txt distributed with this file for more information.
+ *
+ */
+// Tom Kneeland (3/29/99)
+//
+//  Implementation of the Document Object Model Level 1 Core
+//    Implementation of the ProcessingInstruction class
+//
+// Modification History:
+// Who  When      What
+// TK   03/29/99  Created
+//
+
+#include "dom.h"
+
+//
+//Construct a text object with the specified document owner and data
+//
+ProcessingInstruction::ProcessingInstruction(const DOMString& theTarget,
+                                             const DOMString& theData,
+                                             Document* owner) :
+                       NodeDefinition(Node::PROCESSING_INSTRUCTION_NODE,
+                                      theTarget, theData, owner)
+{
+}
+
+//
+//Return the Target of the processing instruction.  This is simply the
+//nodeName.
+//
+const DOMString& ProcessingInstruction::getTarget() const
+{
+  return nodeName;
+}
+
+//
+//Return the Data of the processing instruction.  This is simply the value
+//of the node, "nodeValue"
+//
+const DOMString& ProcessingInstruction::getData() const
+{
+  return nodeValue;
+}
+
+//
+//Set the Data element of the processing instruction.
+void ProcessingInstruction::setData(const DOMString& theData)
+{
+  nodeValue = theData;
+}
+
+
+//
+//ProcessingInstruction nodes can not have any children, so just return null
+//from all child manipulation functions.
+//
+
+Node* ProcessingInstruction::insertBefore(Node* newChild, Node* refChild)
+{
+  return NULL;
+}
+
+Node* ProcessingInstruction::replaceChild(Node* newChild, Node* oldChild)
+{
+  return NULL;
+}
+
+Node* ProcessingInstruction::removeChild(Node* oldChild)
+{
+  return NULL;
+}
+
+Node* ProcessingInstruction::appendChild(Node* newChild)
+{
+  return NULL;
+}
