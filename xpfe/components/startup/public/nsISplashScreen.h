d180662aaf26b7a6bfe9b23dc7974b63a01e5365
bsmedberg@covad.net <bsmedberg@covad.net>
2005-11-11 15:55:43 +0000

    Build bustage, bug 313309

--- xpfe/components/startup/public/nsISplashScreen.h
+++ xpfe/components/startup/public/nsISplashScreen.h
@@ -64,9 +64,11 @@
  */
 class nsISplashScreen : public nsISupports {
 public:
-    NS_DEFINE_STATIC_IID_ACCESSOR( NS_ISPLASHSCREEN_IID )
+    NS_DECLARE_STATIC_IID_ACCESSOR(NS_ISPLASHSCREEN_IID)
     NS_IMETHOD Show() = 0;
     NS_IMETHOD Hide() = 0;
 }; // class nsISplashScreen
+
+NS_DECLARE_STATIC_IID_ACCESSOR(nsISplashScreen, NS_ISPLASHSCREEN_IID)
 
 #endif // nsISplashScreen_h__
