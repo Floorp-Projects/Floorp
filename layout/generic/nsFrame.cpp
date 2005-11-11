ac3f79fa07c6bcc2b3172aee4a7b311fd9ba7b67
bmlk%gmx.de <bmlk%gmx.de>
2005-11-11 05:30:31 +0000

    More robust handling of the overflow property, bug 315210 r/sr=roc

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -4268,6 +4268,7 @@ nsIFrame::GetOverflowAreaProperty(PRBool aCreateIfNecessary)
     return overflow;
   }
 
+  NS_NOTREACHED("Frame abuses NS_FRAME_OUTSIDE_CHILDREN flag");
   return nsnull;
 }
 
