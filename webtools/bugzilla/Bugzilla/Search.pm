5340e68cb9e09b9851fb9c2569cde2662fba3b76
bugreport%peshkin.net <bugreport%peshkin.net>
2005-11-15 20:47:01 +0000

    Bug 306130 Search for attachment creation fails Patch by Joel Peshkin <bugreport@peshkin.net> r,a=justfave

--- webtools/bugzilla/Bugzilla/Search.pm
+++ webtools/bugzilla/Bugzilla/Search.pm
@@ -771,6 +771,11 @@ sub init {
                     "ON groups_$chartid.id = bug_group_map_$chartid.group_id");
             $f = "groups_$chartid.name";
          },
+         "^attach_data\.thedata,changed" => sub {
+            # Searches for attachment data's change must search
+            # the creation timestamp of the attachment instead.
+            $f = "attachments.whocares";
+         },
          "^attach_data\.thedata," => sub {
              my $atable = "attachments_$chartid";
              my $dtable = "attachdata_$chartid";
