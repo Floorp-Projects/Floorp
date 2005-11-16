7036789bd9cbd94977b0a6a1e0f3d0b191b750e7
rbs@maths.uq.edu.au <rbs@maths.uq.edu.au>
2005-11-16 23:22:28 +0000

    Use ATS instead of FM to enumerate fonts so that Mozilla won't miss the Symbol font. Also cleanup obsolete codes. Patch by YAMASHITA Makoto <makotoy@ms.u-tokyo.ac.jp>, b=213702, r=jshin, sr=rbs

--- gfx/src/mac/nsMacUnicodeFontInfo.cpp
+++ gfx/src/mac/nsMacUnicodeFontInfo.cpp
@@ -604,16 +604,14 @@ PRBool nsMacUnicodeFontInfo::HasGlyphFor(PRUnichar aChar)
   if (0xfffd == aChar)
     return PR_FALSE;
 
-  // MacOS 8.6 do not have FMxxx etc so we have to check
-  if (nsDeviceContextMac::HaveFontManager90()) {       
-    if (!gCCMap) 
-      gCCMap = InitGlobalCCMap();
+  if (!gCCMap) 
+    gCCMap = InitGlobalCCMap();
 
-    NS_ASSERTION(gCCMap, "cannot init global ccmap");
+  NS_ASSERTION(gCCMap, "cannot init global ccmap");
     
-    if (gCCMap)
-      return CCMAP_HAS_CHAR(gCCMap, aChar);
- }
+  if (gCCMap)
+    return CCMAP_HAS_CHAR(gCCMap, aChar);
+
   return PR_FALSE;
 }
 
