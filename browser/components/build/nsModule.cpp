d9bb356977d12b2abc97cd27bf7026c5fa680155
brettw%gmail.com <brettw%gmail.com>
2005-11-17 18:36:40 +0000

    Bug 316077, r=annie.sullivan, sr=darin Protocol handler allowing access to binary annotations. Also: forgotten registration part of bug 306640 for the annotation service.

--- browser/components/build/nsModule.cpp
+++ browser/components/build/nsModule.cpp
@@ -42,6 +42,8 @@
 #include "nsBookmarksService.h"
 #include "nsForwardProxyDataSource.h"
 #ifdef MOZ_PLACES
+#include "nsAnnoProtocolHandler.h"
+#include "nsAnnotationService.h"
 #include "nsNavHistory.h"
 #include "nsNavBookmarks.h"
 #endif
@@ -78,6 +80,8 @@ NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsBookmarksService, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsForwardProxyDataSource, Init)
 #ifdef MOZ_PLACES
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsNavHistory, Init)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsAnnoProtocolHandler);
+NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsAnnotationService, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsNavBookmarks, Init)
 #endif
 #ifdef XP_WIN
@@ -139,6 +143,16 @@ static const nsModuleComponentInfo components[] =
     NS_NAVHISTORY_CID,
     "@mozilla.org/autocomplete/search;1?name=history",
     nsNavHistoryConstructor },
+
+  { "Page Annotation Service",
+    NS_ANNOTATIONSERVICE_CID,
+    NS_ANNOTATIONSERVICE_CONTRACTID,
+    nsAnnotationServiceConstructor },
+
+  { "Annotation Protocol Handler",
+    NS_ANNOPROTOCOLHANDLER_CID,
+    NS_NETWORK_PROTOCOL_CONTRACTID_PREFIX "moz-anno",
+    nsAnnoProtocolHandlerConstructor },
 
   { "Browser Bookmarks Service",
     NS_NAVBOOKMARKSSERVICE_CID,
