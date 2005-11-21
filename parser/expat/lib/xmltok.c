1cf6be5ef779fb1092cb6c6d16359e802f68a9ae
peterv%propagandism.org <peterv%propagandism.org>
2005-11-21 14:59:12 +0000

    Backout due to tree orange.

--- parser/expat/lib/xmltok.c
+++ parser/expat/lib/xmltok.c
@@ -2,8 +2,6 @@
    See the file COPYING for copying permission.
 */
 
-#include <stddef.h>
-
 #ifdef COMPILED_FROM_DSP
 #include "winconfig.h"
 #elif defined(MACOS_CLASSIC)
@@ -14,7 +12,6 @@
 #endif
 #endif /* ndef COMPILED_FROM_DSP */
 
-#include "expat_external.h"
 #include "internal.h"
 #include "xmltok.h"
 #include "nametab.h"
@@ -1249,7 +1246,7 @@ XmlUtf16Encode(int charNum, unsigned short *buf)
 
 struct unknown_encoding {
   struct normal_encoding normal;
-  CONVERTER convert;
+  int (*convert)(void *userData, const char *p);
   void *userData;
   unsigned short utf16[256];
   char utf8[256][4];
