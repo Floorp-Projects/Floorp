c034e6fe1c701ce76644557a42802271d485e914
timeless%mozdev.org <timeless%mozdev.org>
2005-12-11 02:38:15 +0000

    Bug 319003 remove unused variable gSession from TestCacheService r=darin sr=darin

--- netwerk/test/TestCacheService.cpp
+++ netwerk/test/TestCacheService.cpp
@@ -61,7 +61,6 @@ static NS_DEFINE_CID(kCacheServiceCID,           NS_CACHESERVICE_CID);
 
 nsCOMPtr<nsIEventQueue>   gEventQ;
 nsCOMPtr<nsICacheService> gCacheService;
-nsCOMPtr<nsICacheSession> gSession;
 
 class AsyncCacheRequest
 {
