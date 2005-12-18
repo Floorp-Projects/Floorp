a141d0cb9175458689e2cfd07d0e1a5c21eddc3e
peterv@propagandism.org <peterv@propagandism.org>
2005-12-18 10:39:23 +0000

    Now that bug 320375 is fixed, trying once more to check in fix for bug 274777 (Update to Expat 1.95.8). r=bsmedberg, sr=bz.

--- parser/expat/lib/internal.h
+++ parser/expat/lib/internal.h
@@ -20,7 +20,10 @@
          and therefore subject to change.
 */
 
+/* BEGIN MOZILLA CHANGE (https://bugzilla.mozilla.org/show_bug.cgi?id=297604) */
+// #if defined(__GNUC__) && defined(__i386__)
 #if defined(__GNUC__) && defined(__i386__) && !defined(XP_MACOSX)
+/* END MOZILLA CHANGE */
 /* We'll use this version by default only where we know it helps.
 
    regparm() generates warnings on Solaris boxes.   See SF bug #692878.
