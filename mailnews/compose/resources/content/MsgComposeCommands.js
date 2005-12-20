1dac91758562942b8cf8adea90d29e943e3be637
cbiesinger%web.de <cbiesinger%web.de>
2005-12-20 19:40:14 +0000

    Bug 320650 regression from Bug 294140: rename entity. fixes send after ctrl+e patch by Frank Wein <bugzilla@mcsmurf.de> r=iann sr=bienvenu

--- mailnews/compose/resources/content/MsgComposeCommands.js
+++ mailnews/compose/resources/content/MsgComposeCommands.js
@@ -1737,7 +1737,7 @@ function GenericSendMessage( msgType )
             var checkbox = {value:false};
             var okToProceed = gPromptService.confirmCheck(
                                   window,
-                                  sComposeMsgsBundle.getString("subjectDlogTitle"),
+                                  sComposeMsgsBundle.getString("sendMsgTitle"),
                                   sComposeMsgsBundle.getString("recipientDlogMessage"),
                                   sComposeMsgsBundle.getString("CheckMsg"),
                                   checkbox);
