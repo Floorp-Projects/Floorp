a5f5b5628f7c8f1c438dad1d3f8c72b6df7beb53
gerv@gerv.net <gerv@gerv.net>
2005-12-31 16:02:07 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license.

--- modules/oji/src/nsJVMAuthTools.cpp
+++ modules/oji/src/nsJVMAuthTools.cpp
@@ -14,10 +14,11 @@
  *
  * The Original Code is mozilla.org code.
  *
- * The Initial Developer of the Original Code is Sun Microsystem.
- * Portions created by Sun Microsystem are Copyright (C) 2003 by
- * Sun Microsystem. All Rights Reserved.
- *    
+ * The Initial Developer of the Original Code is
+ * Sun Microsystem.
+ * Portions created by the Initial Developer are Copyright (C) 2003
+ * the Initial Developer. All Rights Reserved.
+ *
  * Contributor(s):
  *   Joshua Xia <joshua.xia@sun.com>
  *
@@ -27,11 +28,11 @@
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the NPL, indicate your
+ * use your version of this file under the terms of the MPL, indicate your
  * decision by deleting the provisions above and replace them with the notice
  * and other provisions required by the GPL or the LGPL. If you do not delete
  * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the NPL, the GPL or the LGPL.
+ * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
 
