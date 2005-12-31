eb50dd44e7a0b1008b3bd6888b62f0bd0001e3b0
gerv%gerv.net <gerv%gerv.net>
2005-12-31 16:02:07 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license.

--- toolkit/components/printing/content/printdialog.js
+++ toolkit/components/printing/content/printdialog.js
@@ -19,7 +19,7 @@
 # Portions created by the Initial Developer are Copyright (C) 2002
 # the Initial Developer. All Rights Reserved.
 #
-# Contributor(s): 
+# Contributor(s):
 #   Masaki Katakai <katakai@japan.sun.com>
 #   Jessica Blanco <jblanco@us.ibm.com>
 #   Asko Tontti <atontti@cc.hut.fi>
@@ -27,16 +27,16 @@
 #   Peter Weilbacher <mozilla@weilbacher.org>
 #
 # Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or 
+# either the GNU General Public License Version 2 or later (the "GPL"), or
 # the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
 # in which case the provisions of the GPL or the LGPL are applicable instead
 # of those above. If you wish to allow use of your version of this file only
 # under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the NPL, indicate your
+# use your version of this file under the terms of the MPL, indicate your
 # decision by deleting the provisions above and replace them with the notice
 # and other provisions required by the GPL or the LGPL. If you do not delete
 # the provisions above, a recipient may use your version of this file under
-# the terms of any one of the NPL, the GPL or the LGPL.
+# the terms of any one of the MPL, the GPL or the LGPL.
 #
 # ***** END LICENSE BLOCK *****
 
