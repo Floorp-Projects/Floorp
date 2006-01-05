64f850cdaf73899e8628708d47f23015ef9c7a42
gavin%gavinsharp.com <gavin%gavinsharp.com>
2006-01-05 11:03:47 +0000

    Bug 311779: Connections fail when hosts/domains in 'No proxy for' are separated by semicolon, patch by Henrik Skupin <hskupin@gmail.com>, r=mconnor

--- browser/components/preferences/connection.js
+++ browser/components/preferences/connection.js
@@ -44,7 +44,7 @@ var gConnectionsDialog = {
       this.doAutoconfigURLFixup();
       return true;
     }
-    
+
     if (proxyTypePref.value != 1)
       return true;
 
@@ -64,6 +64,9 @@ var gConnectionsDialog = {
         proxyPortPref.value = httpProxyPortPref.value;
       }
     }
+    
+    var noProxiesPref = document.getElementById("network.proxy.no_proxies_on");
+    noProxiesPref.value = noProxiesPref.value.replace(/[;]/g,',');
     
     return true;
   },
