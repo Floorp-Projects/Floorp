eec0e5a3b514a9f4ac8d341f777587352960eca2
ajschult%verizon.net <ajschult%verizon.net>
2006-01-06 03:25:11 +0000

    Bustage fix (assertion failure) for bug 319004, handle empty title

--- toolkit/components/history/src/nsGlobalHistory.cpp
+++ toolkit/components/history/src/nsGlobalHistory.cpp
@@ -1119,7 +1119,8 @@ nsGlobalHistory::SetPageTitle(nsIURI *aURI, const nsAString& aTitle)
   NS_ENSURE_ARG_POINTER(aURI);
 
   nsAutoString titleString(StringHead(aTitle, HISTORY_TITLE_LENGTH_MAX));
-  if (IS_HIGH_SURROGATE(titleString.Last()))
+  if (titleString.Length() < aTitle.Length() &&
+      IS_HIGH_SURROGATE(titleString.Last()))
     titleString.Truncate(titleString.Length()-1);
 
   // skip about: URIs to avoid reading in the db (about:blank, especially)
