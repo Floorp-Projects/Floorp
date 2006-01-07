c5566c3eff7d0f04f726666eb353f3b351e9a392
gavin@gavinsharp.com <gavin@gavinsharp.com>
2006-01-07 20:37:12 +0000

    Bug 317568: Stub executable opens console window, patch by Dave Townsend (Mossop) <mossop@blueprintit.co.uk>, r=bsmedberg

--- xulrunner/stub/nsXULStub.cpp
+++ xulrunner/stub/nsXULStub.cpp
@@ -209,3 +209,13 @@ main(int argc, char **argv)
   return 1;
 #endif
 }
+
+#if defined( XP_WIN ) && defined( WIN32 ) && !defined(__GNUC__)
+// We need WinMain in order to not be a console app.  This function is
+// unused if we are a console application.
+int WINAPI WinMain( HINSTANCE, HINSTANCE, LPSTR args, int )
+{
+  // Do the real work.
+  return main( __argc, __argv );
+}
+#endif
