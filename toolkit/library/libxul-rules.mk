09f650974d8a982110aa0925ae359cb266b20433
bsmedberg@covad.net <bsmedberg@covad.net>
2006-01-18 17:38:58 +0000

    Bug 321173 - Always build javaxpcom into the mini-libxul - r=jhpedemonte

--- toolkit/library/libxul-rules.mk
+++ toolkit/library/libxul-rules.mk
@@ -84,11 +84,3 @@ dlldeps.cpp: $(topsrcdir)/xpcom/build/dlldeps.cpp
 
 dlldeps-obs.cpp: $(topsrcdir)/xpcom/obsolete/dlldeps-obs.cpp
 	$(INSTALL) $^ .
-
-ifdef MOZ_JAVAXPCOM
-export:: dlldeps-javaxpcom.cpp
-
-dlldeps-javaxpcom.cpp: $(topsrcdir)/extensions/java/xpcom/src/dlldeps-javaxpcom.cpp
-	$(INSTALL) $^ .
-endif
-
