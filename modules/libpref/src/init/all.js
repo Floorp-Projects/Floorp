a329e374b1a76af7a81700a6b3ba8e57037744ce
dbaron%dbaron.org <dbaron%dbaron.org>
2006-01-20 20:06:33 +0000

    Fix regression of default DPI preference.  b=233082  r+sr=roc

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -1791,7 +1791,7 @@ pref("browser.download.dir", "/boot/home/Downloads");
 pref("network.hosts.smtp_server", "localhost");
 pref("network.hosts.pop_server", "pop");
 pref("network.protocol-handler.warn-external.file", false);
-pref("browser.display.screen_resolution", 0); // System setting
+pref("browser.display.screen_resolution", -1); // max(96dpi, System setting)
 pref("browser.drag_out_of_frame_style", 1);
 pref("editor.singleLine.pasteNewlines", 0);
 
