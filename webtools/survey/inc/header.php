6381ee10f2b254844e28903b7ad61dcc43ec794b
mike.morgan%oregonstate.edu <mike.morgan%oregonstate.edu>
2006-01-26 17:22:31 +0000

    Template work, finished everything but the SQL inserts.

---
+++ webtools/survey/inc/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en" dir="ltr">
+<head>
+    <title>Uninstall Survey | Mozilla Corporation</title>
+    <link rel="stylesheet" type="text/css" href="<?=WEB_PATH?>/css/base/template.css" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="<?=WEB_PATH?>/css/rustico/template.css" title="Cavendish" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="<?=WEB_PATH?>/css/rustico/content.css" title="Cavendish" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="<?=WEB_PATH?>/css/survey.css" media="screen"/>
+</head>
+<body id="mozilla-com">
+    <div id="header">
+        <div>
+            <h1><a href="<?=WEB_PATH?>/" title="Return to home page" accesskey="1"><img src="http://mozilla.com/images/template/moz-com-logo.png" height="38" width="89" alt="Mozilla Corporation"></a></h1>
+            <ul>
+                <li id="menu-products"><a href="/products/">Products</a></li>
+                <li id="menu-extensions"><a href="/extensions/">Extensions</a></li>
+                <li id="menu-support"><a href="/support/">Support</a></li>
+                <li id="menu-developers"><a href="/developers/">Developers</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div id="breadcrumbs"><div><span><a href="http://mozilla.com/">Home</a></span> &raquo; <span>Uninstall Survey</span></div></div>
+
+    <!-- Sidebar goes here. We will have none by default for now;
+    The site should be navigatable without a sidebar, and we
+    want to encourage people to build up the basics with this
+    in mind. -->
+
+    <hr class="hide"/>
+
+    <div id="content">
--- webtools/survey/webroot/index.php
+++ webtools/survey/webroot/index.php
@@ -7,12 +7,20 @@
 $intends = $app->getIntends();
 $issues = $app->getIssues();
 
-if (empty($_POST['submit'])) {
-echo '<form action="./" method="post">';
+if (!empty($_POST['submit'])) {
+// Do the inserts and stuff.
+
+// Redirect to thank you page.
+header('Location: http://'.$_SERVER['HTTP_HOST'].WEB_PATH.'/thanks.php');
+exit;
+
+} else {
+require_once(HEADER);
+echo '<form action="./" method="post" id="surveyform">';
 
 // Create intend block.
 echo '<h2>How did you intend to use Firefox when you installed it?</h2>';
-echo '<ul>';
+echo '<ul class="survey">';
 foreach ($intends as $id=>$text) {
     echo '<li><input type="radio" name="intend_id" id="int'.$id.'" value="'.$id.'" /> <label for="int'.$id.'">'.$text.'</label></li>';
 }
@@ -21,17 +29,18 @@ echo '</ul>';
 
 // Create issue block.
 echo '<h2>What issues, if any, did you have? (select all that apply)</h2>';
-echo '<ul>';
+echo '<ul class="survey">';
 foreach ($issues as $id=>$text) {
-    echo '<li><input type="checkbox" name="issue_id[]" id="iss'.$id.'" value="'.$id.'" /> <label for="iss'.$id.'">'.$text.'</label></li>';
+    echo '<li><input type="checkbox" name="issue_id[]" id="iss'.$id.'" value="'.$id.'" /> <label for="isstext'.$id.'" onclick="getElementById(\'iss'.$id.'\').checked=true;">'.$text.'</label> <input type="text" id="isstext'.$id.'" name="'.$id.'_text"/></li>';
 }
 echo '</ul>';
 
 echo '<h2>Other comments or suggestions?</h2>';
-echo '<textarea name="comments"></textarea>';
+echo '<textarea name="comments" rows="11" cols="80"></textarea>';
 
 
-echo '<div><input name="submit" type="submit" class="submit" value="Submit"/></div>';
+echo '<div><input name="submit" type="submit" class="submit" value="Submit &raquo;"/></div>';
 echo '</form>';
+require_once(FOOTER);
 }
 ?>
