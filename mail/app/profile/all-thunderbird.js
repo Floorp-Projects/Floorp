2ebd85fd9ed17196b8ee3cf42581d196dcbdb45f
scott%scott-macgregor.org <scott%scott-macgregor.org>
2006-01-31 22:03:15 +0000

    Bug #239136 --> Thunderbird needs to integrate with the new download manager in toolkit
    Stop building the old one for thunderbird in xpfe\components.
    
    sr=bsmedberg

--- mail/app/profile/all-thunderbird.js
+++ mail/app/profile/all-thunderbird.js
@@ -337,15 +337,18 @@ pref("browser.chrome.toolbar_style",        2);
 
 pref("browser.xul.error_pages.enabled", true);
 
-// Dialog modality issues
-pref("browser.show_about_as_stupid_modal_window", false);
-
-pref("browser.download.progressDnldDialog.keepAlive", true); // keep the dnload progress dialog up after dnload is complete
-pref("browser.download.progressDnldDialog.enable_launch_reveal_buttons", true);
+// Attachment download manager settings
 pref("browser.download.useDownloadDir", false);
 pref("browser.download.folderList", 0);
-pref("browser.download.autoDownload", false);
-pref("browser.download.lastLocation", false);
+pref("browser.download.manager.showAlertOnComplete", false);
+pref("browser.download.manager.showAlertInterval", 2000);
+pref("browser.download.manager.retention", 1);
+pref("browser.download.manager.showWhenStarting", true);
+pref("browser.download.manager.useWindow", true);
+pref("browser.download.manager.closeWhenDone", false);
+pref("browser.download.manager.openDelay", 0);
+pref("browser.download.manager.focusWhenStarting", false);
+pref("browser.download.manager.flashCount", 2);
 
 // various default search settings
 pref("browser.search.defaulturl", "chrome://navigator-region/locale/region.properties");
