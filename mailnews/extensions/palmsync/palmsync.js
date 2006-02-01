a89135165ea501cc2a897da3137fc72fcfc71d4a
scott%scott-macgregor.org <scott%scott-macgregor.org>
2006-02-01 00:47:27 +0000

    missing palm sync extension files that I forgot to cvs add when this made it onto the trunk.

---
+++ mailnews/extensions/palmsync/palmsync.js
@@ -0,0 +1,7 @@
+/*
+   Default pref values for the PalmSync Extension
+*/
+
+// global pref used to keep track of whether we have run PalmSyncInstall.exe 
+// for the current installation of the extension.
+pref("extensions.palmsync.conduitRegistered", false);
