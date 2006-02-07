beb67be3556a87cb0d4a7b6c5b8908d48fe67ca8
wtchang%redhat.com <wtchang%redhat.com>
2006-02-07 01:21:00 +0000

    Bugzilla bug 326110: use PR_IMPLEMENT with two exported functions. Modified files: prfile.c ptio.c

--- nsprpub/pr/src/io/prfile.c
+++ nsprpub/pr/src/io/prfile.c
@@ -412,7 +412,7 @@ PR_IMPLEMENT(PRFileDesc*) PR_OpenFile(
     return fd;
 }
 
-PRInt32 PR_GetSysfdTableMax(void)
+PR_IMPLEMENT(PRInt32) PR_GetSysfdTableMax(void)
 {
 #if defined(XP_UNIX) && !defined(AIX) && !defined(NEXTSTEP) && !defined(QNX)
     struct rlimit rlim;
@@ -445,7 +445,7 @@ PRInt32 PR_GetSysfdTableMax(void)
 #endif
 }
 
-PRInt32 PR_SetSysfdTableSize(int table_size)
+PR_IMPLEMENT(PRInt32) PR_SetSysfdTableSize(int table_size)
 {
 #if defined(XP_UNIX) && !defined(AIX) && !defined(NEXTSTEP) && !defined(QNX)
     struct rlimit rlim;
