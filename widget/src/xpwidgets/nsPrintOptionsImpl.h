8ec4c47576c0e510614383d27ed90e2c247e34f6
timeless%mozdev.org <timeless%mozdev.org>
2006-02-07 01:15:14 +0000

    Bug 297277 potential OOM crash/mlk [@ nsPrintOptions::_CreatePrintSettings] patch by b.jacques@planet.nl r=timeless sr=dmose a=bsmedberg

--- widget/src/xpwidgets/nsPrintOptionsImpl.h
+++ widget/src/xpwidgets/nsPrintOptionsImpl.h
@@ -42,24 +42,31 @@
 #define nsPrintOptionsImpl_h__
 
 #include "nsCOMPtr.h"
-#include "nsIPrintOptions.h"  
-#include "nsIPrintSettingsService.h"  
+#include "nsIPrintOptions.h"
+#include "nsIPrintSettingsService.h"
 #include "nsIPrefBranch.h"
-
-//class nsIPrintSettings;
 
 #undef  IMETHOD_VISIBILITY
 #define IMETHOD_VISIBILITY NS_VISIBILITY_DEFAULT
 
-//*****************************************************************************
-//***    nsPrintOptions
-//*****************************************************************************
-class NS_GFX nsPrintOptions : public nsIPrintOptions, public nsIPrintSettingsService
+/**
+ *   Class nsPrintOptions
+ */
+class NS_GFX nsPrintOptions : public nsIPrintOptions, public
+                              nsIPrintSettingsService
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIPRINTOPTIONS
   NS_DECL_NSIPRINTSETTINGSSERVICE
+
+  /**
+   * method Init
+   *  Initializes member variables. Every consumer that does manual
+   *  creation (instead of do_CreateInstance) needs to call this method
+   *  immediately after instantiation.
+   */
+  virtual nsresult Init();
 
   nsPrintOptions();
   virtual ~nsPrintOptions();
@@ -67,36 +74,65 @@ public:
 protected:
   void ReadBitFieldPref(const char * aPrefId, PRInt32 anOption);
   void WriteBitFieldPref(const char * aPrefId, PRInt32 anOption);
-  void ReadJustification(const char * aPrefId, PRInt16& aJust, PRInt16 aInitValue);
+  void ReadJustification(const char * aPrefId, PRInt16& aJust,
+                         PRInt16 aInitValue);
   void WriteJustification(const char * aPrefId, PRInt16 aJust);
-  void ReadInchesToTwipsPref(const char * aPrefId, nscoord&  aTwips, const char * aMarginPref);
+  void ReadInchesToTwipsPref(const char * aPrefId, nscoord&  aTwips,
+                             const char * aMarginPref);
   void WriteInchesFromTwipsPref(const char * aPrefId, nscoord aTwips);
 
-  nsresult ReadPrefString(const char * aPrefId, nsString& aString);
-  nsresult WritePrefString(const char * aPrefId, nsString& aString);
+  nsresult ReadPrefString(const char * aPrefId, nsAString& aString);
+  /**
+   * method WritePrefString
+   *   Writes PRUnichar* to Prefs and deletes the contents of aString
+   */
+  nsresult WritePrefString(const char * aPrefId, const nsAString& aString);
   nsresult WritePrefString(PRUnichar*& aStr, const char* aPrefId);
   nsresult ReadPrefDouble(const char * aPrefId, double& aVal);
   nsresult WritePrefDouble(const char * aPrefId, double aVal);
 
-  virtual nsresult ReadPrefs(nsIPrintSettings* aPS, const nsString& aPrefName, PRUint32 aFlags);
-  virtual nsresult WritePrefs(nsIPrintSettings* aPS, const nsString& aPrefName, PRUint32 aFlags);
-  const char* GetPrefName(const char *     aPrefName, 
-                          const nsString&  aPrinterName);
-  
-  // May be implemented by the platform-specific derived class                       
+  /**
+   * method ReadPrefs
+   * @param aPS          a pointer to the printer settings
+   * @param aPrinterName the name of the printer for which to read prefs
+   * @param aFlags       flag specifying which prefs to read
+   */
+  virtual nsresult ReadPrefs(nsIPrintSettings* aPS, const nsAString&
+                             aPrinterName, PRUint32 aFlags);
+  /**
+   * method WritePrefs
+   * @param aPS          a pointer to the printer settings
+   * @param aPrinterName the name of the printer for which to write prefs
+   * @param aFlags       flag specifying which prefs to read
+   */
+  virtual nsresult WritePrefs(nsIPrintSettings* aPS, const nsAString& aPrefName,
+                              PRUint32 aFlags);
+  const char* GetPrefName(const char *     aPrefName,
+                          const nsAString&  aPrinterName);
+
+  /**
+   * method _CreatePrintSettings
+   *   May be implemented by the platform-specific derived class
+   *
+   * @return             printer settings instance
+   */
   virtual nsresult _CreatePrintSettings(nsIPrintSettings **_retval);
 
-  // Members 
+  // Members
   nsCOMPtr<nsIPrintSettings> mGlobalPrintSettings;
 
-  nsCAutoString mPrefName;
+  nsCString mPrefName;
 
   nsCOMPtr<nsIPrefBranch> mPrefBranch;
 
-  static nsFont* sDefaultFont;
+  nsFont* mDefaultFont;
+private:
+  // These are not supported and are not implemented!
+  nsPrintOptions(const nsPrintOptions& x);
+  nsPrintOptions& operator=(const nsPrintOptions& x);
 };
 
 #undef  IMETHOD_VISIBILITY
 #define IMETHOD_VISIBILITY NS_VISIBILITY_HIDDEN
 
-#endif /* nsPrintOptions_h__ */
+#endif /* nsPrintOptionsImpl_h__ */
