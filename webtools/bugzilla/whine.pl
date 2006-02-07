d05857fa373585fac0a15c4de43589cd8892ca0b
lpsolit%gmail.com <lpsolit%gmail.com>
2006-02-07 09:29:27 +0000

    Bug 313806: Whine E-mails do not "Order" the Queries Correctly - Patch by Fr�d�ric Buclin <LpSolit@gmail.com> r=wurblzap a=justdave

--- webtools/bugzilla/whine.pl
+++ webtools/bugzilla/whine.pl
@@ -51,7 +51,7 @@ my @seen_schedules = ();
 # allow the database to keep their SQL compiled.
 my $sth_run_queries =
     $dbh->prepare("SELECT " .
-                  "id, query_name, title, onemailperbug " .
+                  "query_name, title, onemailperbug " .
                   "FROM whine_queries " .
                   "WHERE eventid=? " .
                   "ORDER BY sortkey");
@@ -425,18 +425,19 @@ sub run_queries {
     my $return_queries = [];
 
     $sth_run_queries->execute($args->{'eventid'});
-    my $queries = {};
+    my @queries = ();
     for (@{$sth_run_queries->fetchall_arrayref}) {
-        $queries->{$_->[0]} = {
-            'name'          => $_->[1],
-            'title'         => $_->[2],
-            'onemailperbug' => $_->[3],
-            'bugs'          => [],
-        };
+        push(@queries,
+            {
+              'name'          => $_->[0],
+              'title'         => $_->[1],
+              'onemailperbug' => $_->[2],
+              'bugs'          => [],
+            }
+        );
     }
 
-    for my $query_id (keys %{$queries}) {
-        my $thisquery = $queries->{$query_id};
+    foreach my $thisquery (@queries) {
         next unless $thisquery->{'name'};   # named query is blank
 
         my $savedquery = get_query($thisquery->{'name'}, $args->{'author'});
