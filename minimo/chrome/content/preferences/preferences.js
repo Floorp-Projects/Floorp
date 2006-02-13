48de71cc26bd1af9b1c94ad045983af1e893852d
mgalli%geckonnection.com <mgalli%geckonnection.com>
2006-02-13 21:14:30 +0000

    useDownloadDir pref and dependency UI.

--- minimo/chrome/content/preferences/preferences.js
+++ minimo/chrome/content/preferences/preferences.js
@@ -86,6 +86,12 @@ function UIdependencyCheck() {
 	document.getElementById("networkProxyHTTP_Port").disabled=false;
   }
 
+  if(!document.getElementById("useDownloadDir").checked) {
+	document.getElementById("downloadDir").disabled=true;
+  } else {
+	document.getElementById("downloadDir").disabled=false;
+  }
+
 }
 
 
