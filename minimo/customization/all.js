1cbd876b8e373d027d4363f58be3cdec06f3e2ab
mgalli%geckonnection.com <mgalli%geckonnection.com>
2006-02-16 20:18:30 +0000

    New entry to homebar.

--- minimo/customization/all.js
+++ minimo/customization/all.js
@@ -667,4 +667,4 @@ pref("dom.max_script_run_time", 60);
 /* Bookmark store as a pref for now, homebar section as well */
 
 pref("browser.bookmark.store","<bm></bm>");
-pref("browser.bookmark.homebar","<bm><li iconsrc='chrome://minimo/skin/extensions/icon-star.png'>chrome://minimo/content/rssview/rssload.xhtml?url=http://del.icio.us/rss/tag/mobile-sites</li><li iconsrc='chrome://minimo/skin/extensions/icon-google.png'>http://www.google.com/xhtml</li><li iconsrc='http://m.technorati.com/favicon.ico'>http://m.technorati.com/</li><li iconsrc='http://mobile.wunderground.com/favicon.ico'>http://mobile.wunderground.com/</li></bm>");
+pref("browser.bookmark.homebar","<bm><li iconsrc='chrome://minimo/skin/extensions/icon-star.png'>chrome://minimo/content/rssview/rssload.xhtml?url=http://del.icio.us/rss/tag/mobile-sites</li><li iconsrc='chrome://minimo/skin/extensions/icon-google.png'>http://www.google.com/xhtml</li><li iconsrc='http://m.technorati.com/favicon.ico'>http://m.technorati.com/</li><li iconsrc='http://mobile.wunderground.com/favicon.ico'>http://mobile.wunderground.com/</li><li iconsrc="chrome://minimo/skin/extensions/icon-shopping.png">http://yokel.com/</li></bm>");
