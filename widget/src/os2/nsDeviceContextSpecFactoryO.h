8970e5d085add5f98afa84511a55a9466b8ab157
rods@netscape.com <rods@netscape.com>
2006-02-16 21:32:05 +0000

    backing out unwanted changes

--- widget/src/os2/nsDeviceContextSpecFactoryO.h
+++ widget/src/os2/nsDeviceContextSpecFactoryO.h
@@ -54,7 +54,7 @@ public:
   NS_IMETHOD CreateDeviceContextSpec(nsIWidget *aWidget,
                                      nsIPrintSettings* aPrintSettings,
                                      nsIDeviceContextSpec *&aNewSpec,
-                                     PRBool aIsPrintPreview);
+                                     PRBool aQuiet);
 
 protected:
   ~nsDeviceContextSpecFactoryOS2();
