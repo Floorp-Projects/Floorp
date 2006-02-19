1f326d0a1175ea75cb45e8e58f9ec33b93598525
mark%moxienet.com <mark%moxienet.com>
2006-02-19 22:41:13 +0000

    327850/327682 Bump version numbers

--- camino/resources/application/all-camino.js
+++ camino/resources/application/all-camino.js
@@ -62,7 +62,7 @@ pref("chimera.log_js_to_console", false);
 
 // Identify Camino in the UA string
 pref("general.useragent.vendor", "Camino");
-pref("general.useragent.vendorSub", "1.0+");
+pref("general.useragent.vendorSub", "1.2+");
 
 pref("browser.chrome.favicons", true);
 pref("browser.urlbar.autocomplete.enabled", true);
