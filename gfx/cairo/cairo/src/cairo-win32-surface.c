361c23430b03623dd20d4f2d6df2028ce88c2277
pavlov%pavlov.net <pavlov%pavlov.net>
2006-02-21 21:14:32 +0000

    fix windows printing. bug 328072. r=vlad

--- gfx/cairo/cairo/src/cairo-win32-surface.c
+++ gfx/cairo/cairo/src/cairo-win32-surface.c
@@ -31,6 +31,7 @@
  *
  * Contributor(s):
  *	Owen Taylor <otaylor@redhat.com>
+ *	Stuart Parmenter <stuart@mozilla.com>
  */
 
 #include <stdio.h>
@@ -390,8 +391,18 @@ _cairo_win32_surface_get_subimage (cairo_win32_surface_t  *surface,
 		 width, height,
 		 surface->dc,
 		 x, y,
-		 SRCCOPY))
-	goto FAIL;
+		 SRCCOPY)) {
+	/* If we fail to BitBlt here, most likely the source is a printer.
+	 * You can't reliably get bits from a printer DC, so just fill in
+	 * the surface as white (common case for printing).
+	 */
+
+	RECT r;
+	r.left = r.top = 0;
+	r.right = width;
+	r.bottom = height;
+	FillRect(local->dc, &r, (HBRUSH)GetStockObject(WHITE_BRUSH));
+    }
 
     GdiFlush();
 
@@ -611,6 +622,8 @@ _composite_alpha_blend (cairo_win32_surface_t *dst,
     }
 
     if (alpha_blend == NULL)
+	return CAIRO_INT_STATUS_UNSUPPORTED;
+    if (GetDeviceCaps(dst->dc, SHADEBLENDCAPS) == SB_NONE)
 	return CAIRO_INT_STATUS_UNSUPPORTED;
     
     blend_function.BlendOp = AC_SRC_OVER;
@@ -997,19 +1010,23 @@ cairo_win32_surface_create (HDC hdc)
 	return &_cairo_surface_nil;
     }
 
-    depth = GetDeviceCaps(hdc, BITSPIXEL);
-    if (depth == 32)
-        format = CAIRO_FORMAT_ARGB32;
-    else if (depth == 24)
-        format = CAIRO_FORMAT_RGB24;
-    else if (depth == 8)
-        format = CAIRO_FORMAT_A8;
-    else if (depth == 1)
-        format = CAIRO_FORMAT_A1;
-    else {
-        _cairo_win32_print_gdi_error("cairo_win32_surface_create(bad BITSPIXEL)");
-        _cairo_error (CAIRO_STATUS_NO_MEMORY);
-        return &_cairo_surface_nil;
+	depth = GetDeviceCaps(hdc, BITSPIXEL);
+    if (GetDeviceCaps(hdc, TECHNOLOGY) == DT_RASDISPLAY) {
+	if (depth == 32)
+	    format = CAIRO_FORMAT_ARGB32;
+	else if (depth == 24)
+	    format = CAIRO_FORMAT_RGB24;
+	else if (depth == 8)
+	    format = CAIRO_FORMAT_A8;
+	else if (depth == 1)
+	    format = CAIRO_FORMAT_A1;
+	else {
+	    _cairo_win32_print_gdi_error("cairo_win32_surface_create(bad BITSPIXEL)");
+	    _cairo_error (CAIRO_STATUS_NO_MEMORY);
+	    return &_cairo_surface_nil;
+	}
+    } else {
+	format = CAIRO_FORMAT_RGB24;
     }
 
     surface = malloc (sizeof (cairo_win32_surface_t));
