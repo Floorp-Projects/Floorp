3323e66e045bcb3916d5f7b847709e0e09a7e53f
silver%warwickcompsoc.co.uk <silver%warwickcompsoc.co.uk>
2006-02-21 18:34:54 +0000

    Bump version number. ChatZilla only.

--- extensions/irc/xul/content/static.js
+++ extensions/irc/xul/content/static.js
@@ -39,11 +39,11 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-const __cz_version   = "0.9.70";
+const __cz_version   = "0.9.71";
 const __cz_condition = "green";
 const __cz_suffix    = "";
 const __cz_guid      = "59c81df5-4b7a-477b-912d-4e0fdf64e5f2";
-const __cz_locale    = "0.9.70.0";
+const __cz_locale    = "0.9.71.0";
 
 var warn;
 var ASSERT;
