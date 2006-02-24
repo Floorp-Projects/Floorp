41557f2f1f4cf3f0304145cda1d35c3ee7ee753f
tor%cs.brown.edu <tor%cs.brown.edu>
2006-02-24 16:41:39 +0000

    Bug 325728 - changes to markerWidth/markerHeight and orient not handled properly.  Patch by longsonr@gmail.com, r=tor, sr=roc.

--- content/svg/content/src/nsSVGSVGElement.cpp
+++ content/svg/content/src/nsSVGSVGElement.cpp
@@ -1449,10 +1449,6 @@ NS_IMETHODIMP
 nsSVGSVGElement::WillModifySVGObservable(nsISVGValue* observable,
                                          nsISVGValue::modificationType aModType)
 {
-#ifdef DEBUG
-  printf("viewport/viewbox/preserveAspectRatio will be changed\n");
-#endif
-
   if (mDispatchEvent) {
     // Modification isn't due to calling SetCurrent[Scale]Translate, so if
     // currentScale or currentTranslate is about to change we must record their
@@ -1533,10 +1529,6 @@ nsSVGSVGElement::DidModifySVGObservable (nsISVGValue* observable,
 #endif
   }
   
-  
-#ifdef DEBUG
-  printf("viewport/viewbox/preserveAspectRatio have been changed\n");
-#endif
   return NS_OK;
 }
 
