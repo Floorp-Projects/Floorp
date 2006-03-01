f95ae18fe7e034c876317c75c54e0bac8570177b
rrelyea%redhat.com <rrelyea%redhat.com>
2006-03-01 17:09:17 +0000

    Remove mp_init/mp_clear calls (and potential mallocs,frees and zeros) in tight loops for bug #326482
    r=nelson

--- security/nss/lib/freebl/ecl/ecp_jm.c
+++ security/nss/lib/freebl/ecl/ecp_jm.c
@@ -41,6 +41,8 @@
 #include "mplogic.h"
 #include <stdlib.h>
 
+#define MAX_SCRATCH 6
+
 /* Computes R = 2P.  Elliptic curve points P and R can be identical.  Uses 
  * Modified Jacobian coordinates.
  *
@@ -51,19 +53,19 @@
 mp_err
 ec_GFp_pt_dbl_jm(const mp_int *px, const mp_int *py, const mp_int *pz,
 				 const mp_int *paz4, mp_int *rx, mp_int *ry, mp_int *rz,
-				 mp_int *raz4, const ECGroup *group)
+				 mp_int *raz4, mp_int scratch[], const ECGroup *group)
 {
 	mp_err res = MP_OKAY;
-	mp_int t0, t1, M, S;
+	mp_int *t0, *t1, *M, *S;
 
-	MP_DIGITS(&t0) = 0;
-	MP_DIGITS(&t1) = 0;
-	MP_DIGITS(&M) = 0;
-	MP_DIGITS(&S) = 0;
-	MP_CHECKOK(mp_init(&t0));
-	MP_CHECKOK(mp_init(&t1));
-	MP_CHECKOK(mp_init(&M));
-	MP_CHECKOK(mp_init(&S));
+	t0 = &scratch[0];
+	t1 = &scratch[1];
+	M = &scratch[2];
+	S = &scratch[3];
+
+#if MAX_SCRATCH < 4
+#error "Scratch array defined too small "
+#endif
 
 	/* Check for point at infinity */
 	if (ec_GFp_pt_is_inf_jac(px, py, pz) == MP_YES) {
@@ -74,44 +76,42 @@ ec_GFp_pt_dbl_jm(const mp_int *px, const mp_int *py, const mp_int *pz,
 	}
 
 	/* M = 3 (px^2) + a*(pz^4) */
-	MP_CHECKOK(group->meth->field_sqr(px, &t0, group->meth));
-	MP_CHECKOK(group->meth->field_add(&t0, &t0, &M, group->meth));
-	MP_CHECKOK(group->meth->field_add(&t0, &M, &t0, group->meth));
-	MP_CHECKOK(group->meth->field_add(&t0, paz4, &M, group->meth));
+	MP_CHECKOK(group->meth->field_sqr(px, t0, group->meth));
+	MP_CHECKOK(group->meth->field_add(t0, t0, M, group->meth));
+	MP_CHECKOK(group->meth->field_add(t0, M, t0, group->meth));
+	MP_CHECKOK(group->meth->field_add(t0, paz4, M, group->meth));
 
 	/* rz = 2 * py * pz */
-	MP_CHECKOK(group->meth->field_mul(py, pz, rz, group->meth));
-	MP_CHECKOK(group->meth->field_add(rz, rz, rz, group->meth));
+	MP_CHECKOK(group->meth->field_mul(py, pz, S, group->meth));
+	MP_CHECKOK(group->meth->field_add(S, S, rz, group->meth));
 
 	/* t0 = 2y^2 , t1 = 8y^4 */
-	MP_CHECKOK(group->meth->field_sqr(py, &t0, group->meth));
-	MP_CHECKOK(group->meth->field_add(&t0, &t0, &t0, group->meth));
-	MP_CHECKOK(group->meth->field_sqr(&t0, &t1, group->meth));
-	MP_CHECKOK(group->meth->field_add(&t1, &t1, &t1, group->meth));
+	MP_CHECKOK(group->meth->field_sqr(py, t0, group->meth));
+	MP_CHECKOK(group->meth->field_add(t0, t0, t0, group->meth));
+	MP_CHECKOK(group->meth->field_sqr(t0, t1, group->meth));
+	MP_CHECKOK(group->meth->field_add(t1, t1, t1, group->meth));
 
 	/* S = 4 * px * py^2 = 2 * px * t0 */
-	MP_CHECKOK(group->meth->field_mul(px, &t0, &S, group->meth));
-	MP_CHECKOK(group->meth->field_add(&S, &S, &S, group->meth));
+	MP_CHECKOK(group->meth->field_mul(px, t0, S, group->meth));
+	MP_CHECKOK(group->meth->field_add(S, S, S, group->meth));
+
 
 	/* rx = M^2 - 2S */
-	MP_CHECKOK(group->meth->field_sqr(&M, rx, group->meth));
-	MP_CHECKOK(group->meth->field_sub(rx, &S, rx, group->meth));
-	MP_CHECKOK(group->meth->field_sub(rx, &S, rx, group->meth));
+	MP_CHECKOK(group->meth->field_sqr(M, rx, group->meth));
+	MP_CHECKOK(group->meth->field_sub(rx, S, rx, group->meth));
+	MP_CHECKOK(group->meth->field_sub(rx, S, rx, group->meth));
 
 	/* ry = M * (S - rx) - t1 */
-	MP_CHECKOK(group->meth->field_sub(&S, rx, ry, group->meth));
-	MP_CHECKOK(group->meth->field_mul(ry, &M, ry, group->meth));
-	MP_CHECKOK(group->meth->field_sub(ry, &t1, ry, group->meth));
+	MP_CHECKOK(group->meth->field_sub(S, rx, S, group->meth));
+	MP_CHECKOK(group->meth->field_mul(S, M, ry, group->meth));
+	MP_CHECKOK(group->meth->field_sub(ry, t1, ry, group->meth));
 
 	/* ra*z^4 = 2*t1*(apz4) */
-	MP_CHECKOK(group->meth->field_mul(paz4, &t1, raz4, group->meth));
+	MP_CHECKOK(group->meth->field_mul(paz4, t1, raz4, group->meth));
 	MP_CHECKOK(group->meth->field_add(raz4, raz4, raz4, group->meth));
 
+
   CLEANUP:
-	mp_clear(&t0);
-	mp_clear(&t1);
-	mp_clear(&M);
-	mp_clear(&S);
 	return res;
 }
 
@@ -124,23 +124,21 @@ mp_err
 ec_GFp_pt_add_jm_aff(const mp_int *px, const mp_int *py, const mp_int *pz,
 					 const mp_int *paz4, const mp_int *qx,
 					 const mp_int *qy, mp_int *rx, mp_int *ry, mp_int *rz,
-					 mp_int *raz4, const ECGroup *group)
+					 mp_int *raz4, mp_int scratch[], const ECGroup *group)
 {
 	mp_err res = MP_OKAY;
-	mp_int A, B, C, D, C2, C3;
+	mp_int *A, *B, *C, *D, *C2, *C3;
 
-	MP_DIGITS(&A) = 0;
-	MP_DIGITS(&B) = 0;
-	MP_DIGITS(&C) = 0;
-	MP_DIGITS(&D) = 0;
-	MP_DIGITS(&C2) = 0;
-	MP_DIGITS(&C3) = 0;
-	MP_CHECKOK(mp_init(&A));
-	MP_CHECKOK(mp_init(&B));
-	MP_CHECKOK(mp_init(&C));
-	MP_CHECKOK(mp_init(&D));
-	MP_CHECKOK(mp_init(&C2));
-	MP_CHECKOK(mp_init(&C3));
+	A = &scratch[0];
+	B = &scratch[1];
+	C = &scratch[2];
+	D = &scratch[3];
+	C2 = &scratch[4];
+	C3 = &scratch[5];
+
+#if MAX_SCRATCH < 6
+#error "Scratch array defined too small "
+#endif
 
 	/* If either P or Q is the point at infinity, then return the other
 	 * point */
@@ -161,53 +159,46 @@ ec_GFp_pt_add_jm_aff(const mp_int *px, const mp_int *py, const mp_int *pz,
 	}
 
 	/* A = qx * pz^2, B = qy * pz^3 */
-	MP_CHECKOK(group->meth->field_sqr(pz, &A, group->meth));
-	MP_CHECKOK(group->meth->field_mul(&A, pz, &B, group->meth));
-	MP_CHECKOK(group->meth->field_mul(&A, qx, &A, group->meth));
-	MP_CHECKOK(group->meth->field_mul(&B, qy, &B, group->meth));
+	MP_CHECKOK(group->meth->field_sqr(pz, A, group->meth));
+	MP_CHECKOK(group->meth->field_mul(A, pz, B, group->meth));
+	MP_CHECKOK(group->meth->field_mul(A, qx, A, group->meth));
+	MP_CHECKOK(group->meth->field_mul(B, qy, B, group->meth));
 
 	/* C = A - px, D = B - py */
-	MP_CHECKOK(group->meth->field_sub(&A, px, &C, group->meth));
-	MP_CHECKOK(group->meth->field_sub(&B, py, &D, group->meth));
+	MP_CHECKOK(group->meth->field_sub(A, px, C, group->meth));
+	MP_CHECKOK(group->meth->field_sub(B, py, D, group->meth));
 
 	/* C2 = C^2, C3 = C^3 */
-	MP_CHECKOK(group->meth->field_sqr(&C, &C2, group->meth));
-	MP_CHECKOK(group->meth->field_mul(&C, &C2, &C3, group->meth));
+	MP_CHECKOK(group->meth->field_sqr(C, C2, group->meth));
+	MP_CHECKOK(group->meth->field_mul(C, C2, C3, group->meth));
 
 	/* rz = pz * C */
-	MP_CHECKOK(group->meth->field_mul(pz, &C, rz, group->meth));
+	MP_CHECKOK(group->meth->field_mul(pz, C, rz, group->meth));
 
 	/* C = px * C^2 */
-	MP_CHECKOK(group->meth->field_mul(px, &C2, &C, group->meth));
+	MP_CHECKOK(group->meth->field_mul(px, C2, C, group->meth));
 	/* A = D^2 */
-	MP_CHECKOK(group->meth->field_sqr(&D, &A, group->meth));
+	MP_CHECKOK(group->meth->field_sqr(D, A, group->meth));
 
 	/* rx = D^2 - (C^3 + 2 * (px * C^2)) */
-	MP_CHECKOK(group->meth->field_add(&C, &C, rx, group->meth));
-	MP_CHECKOK(group->meth->field_add(&C3, rx, rx, group->meth));
-	MP_CHECKOK(group->meth->field_sub(&A, rx, rx, group->meth));
+	MP_CHECKOK(group->meth->field_add(C, C, rx, group->meth));
+	MP_CHECKOK(group->meth->field_add(C3, rx, rx, group->meth));
+	MP_CHECKOK(group->meth->field_sub(A, rx, rx, group->meth));
 
 	/* C3 = py * C^3 */
-	MP_CHECKOK(group->meth->field_mul(py, &C3, &C3, group->meth));
+	MP_CHECKOK(group->meth->field_mul(py, C3, C3, group->meth));
 
 	/* ry = D * (px * C^2 - rx) - py * C^3 */
-	MP_CHECKOK(group->meth->field_sub(&C, rx, ry, group->meth));
-	MP_CHECKOK(group->meth->field_mul(&D, ry, ry, group->meth));
-	MP_CHECKOK(group->meth->field_sub(ry, &C3, ry, group->meth));
+	MP_CHECKOK(group->meth->field_sub(C, rx, ry, group->meth));
+	MP_CHECKOK(group->meth->field_mul(D, ry, ry, group->meth));
+	MP_CHECKOK(group->meth->field_sub(ry, C3, ry, group->meth));
 
 	/* raz4 = a * rz^4 */
 	MP_CHECKOK(group->meth->field_sqr(rz, raz4, group->meth));
 	MP_CHECKOK(group->meth->field_sqr(raz4, raz4, group->meth));
 	MP_CHECKOK(group->meth->
 			   field_mul(raz4, &group->curvea, raz4, group->meth));
-
-  CLEANUP:
-	mp_clear(&A);
-	mp_clear(&B);
-	mp_clear(&C);
-	mp_clear(&D);
-	mp_clear(&C2);
-	mp_clear(&C3);
+CLEANUP:
 	return res;
 }
 
@@ -226,6 +217,7 @@ ec_GFp_pt_mul_jm_wNAF(const mp_int *n, const mp_int *px, const mp_int *py,
 	mp_err res = MP_OKAY;
 	mp_int precomp[16][2], rz, tpx, tpy;
 	mp_int raz4;
+	mp_int scratch[MAX_SCRATCH];
 	signed char *naf = NULL;
 	int i, orderBitSize;
 
@@ -236,6 +228,9 @@ ec_GFp_pt_mul_jm_wNAF(const mp_int *n, const mp_int *px, const mp_int *py,
 	for (i = 0; i < 16; i++) {
 		MP_DIGITS(&precomp[i][0]) = 0;
 		MP_DIGITS(&precomp[i][1]) = 0;
+	}
+	for (i = 0; i < MAX_SCRATCH; i++) {
+		MP_DIGITS(&scratch[i]) = 0;
 	}
 
 	ARGCHK(group != NULL, MP_BADARG);
@@ -250,6 +245,9 @@ ec_GFp_pt_mul_jm_wNAF(const mp_int *n, const mp_int *px, const mp_int *py,
 	for (i = 0; i < 16; i++) {
 		MP_CHECKOK(mp_init(&precomp[i][0]));
 		MP_CHECKOK(mp_init(&precomp[i][1]));
+	}
+	for (i = 0; i < MAX_SCRATCH; i++) {
+		MP_CHECKOK(mp_init(&scratch[i]));
 	}
 
 	/* Set out[8] = P */
@@ -295,12 +293,13 @@ ec_GFp_pt_mul_jm_wNAF(const mp_int *n, const mp_int *px, const mp_int *py,
 	/* wNAF method */
 	for (i = orderBitSize; i >= 0; i--) {
 		/* R = 2R */
-		ec_GFp_pt_dbl_jm(rx, ry, &rz, &raz4, rx, ry, &rz, &raz4, group);
+		ec_GFp_pt_dbl_jm(rx, ry, &rz, &raz4, rx, ry, &rz, 
+					     &raz4, scratch, group);
 		if (naf[i] != 0) {
 			ec_GFp_pt_add_jm_aff(rx, ry, &rz, &raz4,
 								 &precomp[(naf[i] + 15) / 2][0],
 								 &precomp[(naf[i] + 15) / 2][1], rx, ry,
-								 &rz, &raz4, group);
+								 &rz, &raz4, scratch, group);
 		}
 	}
 
@@ -308,6 +307,9 @@ ec_GFp_pt_mul_jm_wNAF(const mp_int *n, const mp_int *px, const mp_int *py,
 	MP_CHECKOK(ec_GFp_pt_jac2aff(rx, ry, &rz, rx, ry, group));
 
   CLEANUP:
+	for (i = 0; i < MAX_SCRATCH; i++) {
+		mp_clear(&scratch[i]);
+	}
 	for (i = 0; i < 16; i++) {
 		mp_clear(&precomp[i][0]);
 		mp_clear(&precomp[i][1]);
