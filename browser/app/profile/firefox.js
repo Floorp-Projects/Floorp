f6e7a1304708bb4199c04bf4de2695f70ba56c12
gavin@gavinsharp.com <gavin@gavinsharp.com>
2006-03-03 20:34:56 +0000

    Revert the change from bug 307407 now that bug 300453 is fixed on the branch (change dom.disable_window_flip to true), r=mconnor

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -271,7 +271,7 @@ pref("dom.disable_window_status_change",          true);
 pref("dom.disable_window_move_resize",            false);
 // prevent JS from monkeying with window focus, etc
 pref("dom.disable_window_flip",                   true);
- 
+
 // popups.policy 1=allow,2=reject
 pref("privacy.popups.policy",               1);
 pref("privacy.popups.usecustom",            true);
