b5c250b92c21449b9bc180ee52db9e083a669f71
benjamin%smedbergs.us <benjamin%smedbergs.us>
2006-03-03 21:08:51 +0000

    Add hack define for branch.

--- browser/components/places/src/nsMaybeWeakPtr.h
+++ browser/components/places/src/nsMaybeWeakPtr.h
@@ -43,6 +43,10 @@
 #include "nsWeakReference.h"
 #include "nsTArray.h"
 
+#ifdef MOZILLA_1_8_BRANCH
+#define NS_GET_TEMPLATE_IID NS_GET_IID
+#endif
+
 // nsMaybeWeakPtr is a helper object to hold a strong-or-weak reference
 // to the template class.  It's pretty minimal, but sufficient.
 
