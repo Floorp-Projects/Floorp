7932c781db84de8867f7744b9321a8569b1f805e
jminta%gmail.com <jminta%gmail.com>
2006-03-03 19:51:32 +0000

    Bug 327877 multiple entries for same task are created in agenda when editing task. r=dmose

--- calendar/lightning/content/agenda-tree.js
+++ calendar/lightning/content/agenda-tree.js
@@ -430,9 +430,6 @@ function observer_onAddItem(item)
 agendaTreeView.calendarObserver.onDeleteItem =
 function observer_onDeleteItem(item, rebuildFlag)
 {
-    if (!(item instanceof Components.interfaces.calIEvent))
-        return;
-
     var occs = item.getOccurrencesBetween(this.agendaTreeView.today.start,
                                           this.agendaTreeView.soon.end, {});
     occs.forEach(this.agendaTreeView.deleteItem, this.agendaTreeView);
