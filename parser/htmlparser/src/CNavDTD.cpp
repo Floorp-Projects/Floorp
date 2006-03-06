c2535c88cd50b7c251e5fefe778731be7ab41abd
mrbkap%gmail.com <mrbkap%gmail.com>
2006-03-06 22:13:41 +0000

    Protect against our table container being closed before we're ready by using the same logic to find the close tag that HandleEndToken does. bug 329364, r+sr=jst

--- parser/htmlparser/src/CNavDTD.cpp
+++ parser/htmlparser/src/CNavDTD.cpp
@@ -1750,7 +1750,11 @@ CNavDTD::HandleSavedTokens(PRInt32 anIndex)
             // EndContext(). Ex: <center><table><a></center>.
             // In the Ex. above </center> should not close <center> above table.
             // Doing so will cause the current context to get closed prematurely.
-            PRInt32 theIndex = mBodyContext->LastOf(theTag);
+            eHTMLTags closed = FindAutoCloseTargetForEndTag(theTag, *mBodyContext,
+                                                            mDTDMode);
+            PRInt32 theIndex = closed != eHTMLTag_unknown
+                               ? mBodyContext->LastOf(closed)
+                               : kNotFound;
 
             if (theIndex != kNotFound &&
                 theIndex <= mBodyContext->mContextTopIndex) {
