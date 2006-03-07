9e051c299bb9fc48d8e4d214bc3fa898af9769cb
mark%moxienet.com <mark%moxienet.com>
2006-03-07 01:26:13 +0000

    326668 Add NS_NewLocalFileWithFSRef function to create nsLocalFiles from FSRefs on Mac prior to xpcom startup.  r=josh sr=dougt

--- xpcom/io/nsLocalFileOSX.cpp
+++ xpcom/io/nsLocalFileOSX.cpp
@@ -2123,6 +2123,24 @@ nsresult NS_NewLocalFileWithFSSpec(const FSSpec* inSpec, PRBool followLinks, nsI
     return NS_OK;
 }
 
+nsresult NS_NewLocalFileWithFSRef(const FSRef* aFSRef, PRBool aFollowLinks, nsILocalFileMac** result)
+{
+    nsLocalFile* file = new nsLocalFile();
+    if (file == nsnull)
+        return NS_ERROR_OUT_OF_MEMORY;
+    NS_ADDREF(file);
+
+    file->SetFollowLinks(aFollowLinks);
+
+    nsresult rv = file->InitWithFSRef(aFSRef);
+    if (NS_FAILED(rv)) {
+        NS_RELEASE(file);
+        return rv;
+    }
+    *result = file;
+    return NS_OK;
+}
+
 //*****************************************************************************
 //  Static Functions
 //*****************************************************************************
