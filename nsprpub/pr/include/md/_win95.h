7909073244be49f1e59866f79f47d91b8281c1dc
darin%meer.net <darin%meer.net>
2006-03-10 05:38:31 +0000

    fixes bug 326168 "Add a UTF-16 API to load a library" patch by jshin1987@gmail.com r=wtc,darin

--- nsprpub/pr/include/md/_win95.h
+++ nsprpub/pr/include/md/_win95.h
@@ -273,8 +273,9 @@ extern PRInt32 _MD_CloseFile(PROsfd osfd);
 #define _MD_TLOCKFILE                 _PR_MD_TLOCKFILE
 #define _MD_UNLOCKFILE                _PR_MD_UNLOCKFILE
 
-#ifdef MOZ_UNICODE
 /* --- UTF16 IO stuff --- */
+extern PRBool _pr_useUnicode;
+#ifdef MOZ_UNICODE
 #define _MD_OPEN_FILE_UTF16           _PR_MD_OPEN_FILE_UTF16
 #define _MD_OPEN_DIR_UTF16            _PR_MD_OPEN_DIR_UTF16
 #define _MD_READ_DIR_UTF16            _PR_MD_READ_DIR_UTF16
