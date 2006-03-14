3b7803cf1f50dab0255554c99ada54b94195471d
joe@retrovirus.com <joe@retrovirus.com>
2006-03-14 22:58:59 +0000

    Changes a few remaining ASSERT() calls to NS_ASSERT() calls that I had missed when working on bug 327349.
    bug=330489
    r=annie.sullivan@gmail.com
    sr=ben@mozilla.org

--- browser/components/places/content/places.js
+++ browser/components/places/content/places.js
@@ -805,18 +805,20 @@ var ViewMenu = {
    */
   _clean: function VM__clean(popup, startID, endID) {
     if (endID) 
-      ASSERT(startID, "meaningless to have valid endID and null startID");
+      NS_ASSERT(startID, "meaningless to have valid endID and null startID");
     if (startID) {
       var startElement = document.getElementById(startID);
-      ASSERT(startElement.parentNode == popup, "startElement is not in popup");
-      ASSERT(startElement, 
-             "startID does not correspond to an existing element");
+      NS_ASSERT(startElement.parentNode == 
+                popup, "startElement is not in popup");
+      NS_ASSERT(startElement, 
+                "startID does not correspond to an existing element");
       var endElement = null;
       if (endID) {
         endElement = document.getElementById(endID);
-        ASSERT(endElement.parentNode == popup, "endElement is not in popup");
-        ASSERT(endElement, 
-               "endID does not correspond to an existing element");
+        NS_ASSERT(endElement.parentNode == popup, 
+                  "endElement is not in popup");
+        NS_ASSERT(endElement, 
+                  "endID does not correspond to an existing element");
       }
       while (startElement.nextSibling != endElement)
         popup.removeChild(startElement.nextSibling);
@@ -920,7 +922,8 @@ var ViewMenu = {
     const PREFIX = "menucol_";
     var columnID = element.id.substr(PREFIX.length, element.id.length);
     var column = document.getElementById(columnID);
-    ASSERT(column, "menu item for column that doesn't exist?! id = " + element.id);
+    NS_ASSERT(column, 
+              "menu item for column that doesn't exist?! id = " + element.id);
 
     var splitter = column.nextSibling;
     if (splitter && splitter.localName != "splitter")
