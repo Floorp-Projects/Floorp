4fdf8cf29658f5694b4ef086bebdb7f6507251c9
mkaply%us.ibm.com <mkaply%us.ibm.com>
2006-03-14 21:52:14 +0000

    #301064 r=mkaply, sr=mkaply (OS/2 only) Patch from Andy Willis - add _declspec(dllexport/dllimport) for OS/2 Instead of adding NS_GFX to nsRegionImpl, moving region code into OS/2 (we're the only consumer)

--- gfx/src/os2/nsRegionOS2.cpp
+++ gfx/src/os2/nsRegionOS2.cpp
@@ -34,22 +34,14 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include "nsGfxDefs.h"
 #include "nsRegionOS2.h"
-
-
-NS_IMPL_ISUPPORTS1(nsRegionOS2, nsIRegion)
+#include "nsGfxDefs.h"
 
 nsRegionOS2::nsRegionOS2() 
 {  
 }
 
-// Do not use GetNativeRegion on OS/2. Regions are device specific. Use GetHRGN () instead.
-nsresult nsRegionOS2::GetNativeRegion (void *&aRegion) const
-{
-  aRegion = (void*)RGN_ERROR;
-  return NS_OK;
-}
+NS_IMPL_ISUPPORTS1(nsRegionOS2, nsIRegion)
 
 PRUint32 nsRegionOS2::NumOfRects (HPS aPS, HRGN aRegion) const
 {
@@ -115,5 +107,164 @@ nsresult nsRegionOS2::InitWithHRGN (HRGN SrcRegion, PRUint32 SrcHeight, HPS SrcP
     delete [] pRects;
   }
 
+  return NS_OK;
+}
+
+nsresult nsRegionOS2::Init (void)
+{
+  mRegion.SetEmpty ();
+  return NS_OK;
+}
+
+void nsRegionOS2::SetTo (const nsIRegion &aRegion)
+{
+  const nsRegionOS2* pRegion = NS_STATIC_CAST (const nsRegionOS2*, &aRegion);
+  mRegion = pRegion->mRegion;
+}
+
+void nsRegionOS2::SetTo (PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight)
+{
+  mRegion = nsRect (aX, aY, aWidth, aHeight);
+}
+
+void nsRegionOS2::Intersect (const nsIRegion &aRegion)
+{
+  const nsRegionOS2* pRegion = NS_STATIC_CAST (const nsRegionOS2*, &aRegion);
+  mRegion.And (mRegion, pRegion->mRegion);
+}
+
+void nsRegionOS2::Intersect (PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight)
+{
+  mRegion.And (mRegion, nsRect (aX, aY, aWidth, aHeight));
+}
+
+void nsRegionOS2::Union (const nsIRegion &aRegion)
+{
+  const nsRegionOS2* pRegion = NS_STATIC_CAST (const nsRegionOS2*, &aRegion);
+  mRegion.Or (mRegion, pRegion->mRegion);
+}
+
+void nsRegionOS2::Union (PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight)
+{
+  mRegion.Or (mRegion, nsRect (aX, aY, aWidth, aHeight));
+}
+
+void nsRegionOS2::Subtract (const nsIRegion &aRegion)
+{
+  const nsRegionOS2* pRegion = NS_STATIC_CAST (const nsRegionOS2*, &aRegion);
+  mRegion.Sub (mRegion, pRegion->mRegion);
+}
+
+void nsRegionOS2::Subtract (PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight)
+{
+  mRegion.Sub (mRegion, nsRect (aX, aY, aWidth, aHeight));
+}
+
+PRBool nsRegionOS2::IsEmpty (void)
+{
+  return mRegion.IsEmpty ();
+}
+
+PRBool nsRegionOS2::IsEqual (const nsIRegion &aRegion)
+{
+  const nsRegionOS2* pRegion = NS_STATIC_CAST (const nsRegionOS2*, &aRegion);
+  return mRegion.IsEqual (pRegion->mRegion);
+}
+
+void nsRegionOS2::GetBoundingBox (PRInt32 *aX, PRInt32 *aY, PRInt32 *aWidth, PRInt32 *aHeight)
+{
+  const nsRect& BoundRect = mRegion.GetBounds();
+  *aX = BoundRect.x;
+  *aY = BoundRect.y;
+  *aWidth  = BoundRect.width;
+  *aHeight = BoundRect.height;
+}
+
+void nsRegionOS2::Offset (PRInt32 aXOffset, PRInt32 aYOffset)
+{
+  mRegion.MoveBy (aXOffset, aYOffset);
+}
+
+PRBool nsRegionOS2::ContainsRect (PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight)
+{
+  nsRegion TmpRegion;
+  TmpRegion.And (mRegion, nsRect (aX, aY, aWidth, aHeight));
+  return (!TmpRegion.IsEmpty ());
+}
+
+nsresult nsRegionOS2::GetRects (nsRegionRectSet **aRects)
+{
+  if (!aRects)
+    return NS_ERROR_NULL_POINTER;
+
+  nsRegionRectSet* pRegionSet = *aRects;
+  PRUint32 NumRects = mRegion.GetNumRects ();
+
+  if (!pRegionSet)                          // Not yet allocated
+  {
+    PRUint8* pBuf = new PRUint8 [sizeof (nsRegionRectSet) + NumRects * sizeof (nsRegionRect)];
+    pRegionSet = NS_REINTERPRET_CAST (nsRegionRectSet*, pBuf);
+    pRegionSet->mRectsLen = NumRects + 1;
+  } else                                    // Already allocated in previous call
+  {
+    if (NumRects > pRegionSet->mRectsLen)   // passed array is not big enough - reallocate it.
+    {
+      delete [] NS_REINTERPRET_CAST (PRUint8*, pRegionSet);
+      PRUint8* pBuf = new PRUint8 [sizeof (nsRegionRectSet) + NumRects * sizeof (nsRegionRect)];
+      pRegionSet = NS_REINTERPRET_CAST (nsRegionRectSet*, pBuf);
+      pRegionSet->mRectsLen = NumRects + 1;
+    }
+  }
+  pRegionSet->mNumRects = NumRects;
+  *aRects = pRegionSet;
+
+
+  nsRegionRectIterator ri (mRegion);
+  nsRegionRect* pDest = &pRegionSet->mRects [0];
+  const nsRect* pSrc;
+
+  while ((pSrc = ri.Next ()) != nsnull)
+  {
+    pDest->x = pSrc->x;
+    pDest->y = pSrc->y;
+    pDest->width  = pSrc->width;
+    pDest->height = pSrc->height;
+
+    ++pDest;
+  }
+
+  return NS_OK;
+}
+
+nsresult nsRegionOS2::FreeRects (nsRegionRectSet *aRects)
+{
+  if (!aRects)
+    return NS_ERROR_NULL_POINTER;
+
+  delete [] NS_REINTERPRET_CAST (PRUint8*, aRects);
+  return NS_OK;
+}
+
+nsresult nsRegionOS2::GetNativeRegion (void *&aRegion) const
+{
+  aRegion = RGN_ERROR;
+  return NS_OK;
+}
+
+nsresult nsRegionOS2::GetRegionComplexity (nsRegionComplexity &aComplexity) const
+{
+  switch (mRegion.GetNumRects ())
+  {
+    case 0:   aComplexity = eRegionComplexity_empty;    break;
+    case 1:   aComplexity = eRegionComplexity_rect;     break;
+    default:  aComplexity = eRegionComplexity_complex;  break;
+  }
+
+  return NS_OK;
+}
+
+nsresult nsRegionOS2::GetNumRects (PRUint32 *aRects) const
+{
+  *aRects = mRegion.GetNumRects ();
   return NS_OK;
 }
--- gfx/src/os2/nsRegionOS2.h
+++ gfx/src/os2/nsRegionOS2.h
@@ -38,10 +38,12 @@
 #ifndef _nsRegionOS2_h
 #define _nsRegionOS2_h
 
-#include "nsRegionImpl.h"
+#include "nsiRegion.h"
+#include "nsRegion.h"
+#define INCL_GPI
+#include <os2.h>
 
-
-class nsRegionOS2 : public nsRegionImpl
+class nsRegionOS2 : public nsIRegion
 {
   PRUint32 NumOfRects (HPS aPS, HRGN aRegion) const;
 
@@ -50,7 +52,27 @@ public:
 
   NS_DECL_ISUPPORTS
 
-  NS_IMETHOD GetNativeRegion(void *&aRegion) const;    // Don't use this on OS/2 - Use GetHRGN () instead
+  virtual nsresult Init();
+
+  virtual void SetTo(const nsIRegion &aRegion);
+  virtual void SetTo(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight);
+  virtual void Intersect(const nsIRegion &aRegion);
+  virtual void Intersect(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight);
+  virtual void Union(const nsIRegion &aRegion);
+  virtual void Union(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight);
+  virtual void Subtract(const nsIRegion &aRegion);
+  virtual void Subtract(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight);
+  virtual PRBool IsEmpty(void);
+  virtual PRBool IsEqual(const nsIRegion &aRegion);
+  virtual void GetBoundingBox(PRInt32 *aX, PRInt32 *aY, PRInt32 *aWidth, PRInt32 *aHeight);
+  virtual void Offset(PRInt32 aXOffset, PRInt32 aYOffset);
+  virtual PRBool ContainsRect(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight);
+  NS_IMETHOD GetRects(nsRegionRectSet **aRects);
+  NS_IMETHOD FreeRects(nsRegionRectSet *aRects);
+  // Don't use this on OS/2 - Use GetHRGN () instead	
+  NS_IMETHOD GetNativeRegion(void *&aRegion) const;
+  NS_IMETHOD GetRegionComplexity(nsRegionComplexity &aComplexity) const;
+  NS_IMETHOD GetNumRects(PRUint32 *aRects) const;
  
   // OS/2 specific
   // get region in widget's coord space for given device
@@ -58,6 +80,8 @@ public:
 
   // copy from another region defined in aWidget's space for a given device
   nsresult InitWithHRGN (HRGN copy, PRUint32 ulHeight, HPS hps);
+private:
+  nsRegion mRegion;
 };
 
 #endif
