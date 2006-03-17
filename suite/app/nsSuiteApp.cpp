05c4d21c50c7eb384dd0f79e93480706e413c9d2
bugzilla%standard8.demon.co.uk <bugzilla%standard8.demon.co.uk>
2006-03-17 14:08:31 +0000

    Bug 330053 Make SeaMonkey capable of generating a reasonable build with MOZ_XUL_APP set. Part 1 - Add suite/app for starting up with toolkit v3. r=jag,sr=neil

---
+++ suite/app/nsSuiteApp.cpp
@@ -0,0 +1,75 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * the mozilla.org SeaMonkey project.
+ * Portions created by the Initial Developer are Copyright (C) 2006
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Mark Banner <bugzilla@standard8.demon.co.uk>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#include "nsXULAppAPI.h"
+#ifdef XP_WIN
+#include <windows.h>
+#include <stdlib.h>
+#endif
+#include "nsBuildID.h"
+#include "prtypes.h"
+
+static const nsXREAppData kAppData = {
+  sizeof(nsXREAppData),
+  nsnull,
+  "Mozilla", // XXX This should just be nsnull, but we need it here to get our
+             // profile dir in the right place for the time being.
+  "SeaMonkey", // XXX If we get a sensible profile dir setting, this should be
+               // changed to MOZ_APP_DISPLAYNAME
+  NS_STRINGIFY(APP_VERSION),
+  NS_STRINGIFY(BUILD_ID),
+  "{92650c4d-4b8e-4d2a-b7eb-24ecf4f6b63a}",
+  "Copyright (c) 1998-2006 mozilla.org",
+  NS_XRE_ENABLE_PROFILE_MIGRATOR |
+  NS_XRE_ENABLE_EXTENSION_MANAGER
+};
+
+int main(int argc, char* argv[])
+{
+  return XRE_main(argc, argv, &kAppData);
+}
+
+#if defined( XP_WIN ) && defined( WIN32 ) && !defined(__GNUC__)
+// We need WinMain in order to not be a console app.  This function is
+// unused if we are a console application.
+int WINAPI WinMain( HINSTANCE, HINSTANCE, LPSTR args, int )
+{
+  // Do the real work.
+  return main( __argc, __argv );
+}
+#endif
