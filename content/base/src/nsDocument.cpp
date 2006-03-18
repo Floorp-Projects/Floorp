d7bd93f921f9c100da5ab3a2d3c7c316af3c8791
neil%parkwaycc.co.uk <neil%parkwaycc.co.uk>
2006-03-18 14:14:08 +0000

    Ensure box objects are obtained from the correct document b=330181 r+sr=jst

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -3217,6 +3217,8 @@ nsDocument::GetBoxObjectFor(nsIDOMElement* aElement, nsIBoxObject** aResult)
 {
   nsCOMPtr<nsIContent> content(do_QueryInterface(aElement));
   NS_ENSURE_TRUE(content, NS_ERROR_UNEXPECTED);
+  NS_ENSURE_TRUE(content->GetCurrentDoc() == this,
+                 NS_ERROR_DOM_WRONG_DOCUMENT_ERR);
   
   nsresult rv;
 
