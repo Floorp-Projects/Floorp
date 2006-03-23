ace148d610145c854a71b3644773c3f5fcad1831
dbaron%dbaron.org <dbaron%dbaron.org>
2006-03-23 23:21:27 +0000

    Workaround brokenness of __builtin_frame_address(0) on gcc 4.1 (as shipped with FC5, at least).  b=331436  r=brendan

--- gc/boehm/os_dep.c
+++ gc/boehm/os_dep.c
@@ -2442,7 +2442,16 @@ struct stack_frame {
 static stack_frame* getStackFrame()
 {
   stack_frame* currentFrame;
+#if defined(__i386) 
+  __asm__( "movl %%ebp, %0" : "=g"(currentFrame));
+#elif defined(__x86_64__)
+  __asm__( "movq %%rbp, %0" : "=g"(currentFrame));
+#else
+  // It would be nice if this worked uniformly, but at least on i386 and
+  // x86_64, it stopped working with gcc 4.1, because it points to the
+  // end of the saved registers instead of the start.
   currentFrame = (stack_frame*)__builtin_frame_address(0);
+#endif
   currentFrame = currentFrame->next;
   return currentFrame;
 }
