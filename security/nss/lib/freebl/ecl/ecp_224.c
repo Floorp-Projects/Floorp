0304782833fcd2c55c84c57ab62bbee53b2f5d33
rrelyea@redhat.com <rrelyea@redhat.com>
2006-03-24 22:55:51 +0000

    Bug 321350 Implement optimized code for NIST Suite B elliptic curves r=douglas r=vipul

--- security/nss/lib/freebl/ecl/ecp_224.c
+++ security/nss/lib/freebl/ecl/ecp_224.c
@@ -42,6 +42,8 @@
 #include "mpi-priv.h"
 #include <stdlib.h>
 
+#define ECP224_DIGITS ECL_CURVE_DIGITS(224)
+
 /* Fast modular reduction for p224 = 2^224 - 2^96 + 1.  a can be r. Uses
  * algorithm 7 from Brown, Hankerson, Lopez, Menezes. Software
  * Implementation of the NIST Elliptic Curves over Prime Fields. */
@@ -51,213 +53,254 @@ ec_GFp_nistp224_mod(const mp_int *a, mp_int *r, const GFMethod *meth)
 	mp_err res = MP_OKAY;
 	mp_size a_used = MP_USED(a);
 
-	/* s is a statically-allocated mp_int of exactly the size we need */
-	mp_int s;
-
+	int    r3b;
+	mp_digit carry;
 #ifdef ECL_THIRTY_TWO_BIT
-	mp_digit sa[8];
-	mp_digit a13 = 0, a12 = 0, a11 = 0, a10, a9 = 0, a8, a7;
-
-	MP_SIGN(&s) = MP_ZPOS;
-	MP_ALLOC(&s) = 8;
-	MP_USED(&s) = 7;
-	MP_DIGITS(&s) = sa;
+        mp_digit a6a = 0, a6b = 0,
+                a5a = 0, a5b = 0, a4a = 0, a4b = 0, a3a = 0, a3b = 0;
+        mp_digit r0a, r0b, r1a, r1b, r2a, r2b, r3a;
 #else
-	mp_digit sa[4];
-	mp_digit a6 = 0, a5 = 0, a4 = 0, a3 = 0;
-
-	MP_SIGN(&s) = MP_ZPOS;
-	MP_ALLOC(&s) = 4;
-	MP_USED(&s) = 4;
-	MP_DIGITS(&s) = sa;
+	mp_digit a6 = 0, a5 = 0, a4 = 0, a3b = 0, a5a = 0;
+        mp_digit a6b = 0, a6a_a5b = 0, a5b = 0, a5a_a4b = 0, a4a_a3b = 0;
+        mp_digit r0, r1, r2, r3;
 #endif
 
 	/* reduction not needed if a is not larger than field size */
-#ifdef ECL_THIRTY_TWO_BIT
-	if (a_used < 8) {
-#else
-	if (a_used < 4) {
-#endif
+	if (a_used < ECP224_DIGITS) {
+		if (a == r) return MP_OKAY;
 		return mp_copy(a, r);
 	}
-#ifdef ECL_THIRTY_TWO_BIT
 	/* for polynomials larger than twice the field size, use regular
 	 * reduction */
-	if (a_used > 14) {
+	if (a_used > ECL_CURVE_DIGITS(224*2)) {
 		MP_CHECKOK(mp_mod(a, &meth->irr, r));
 	} else {
+#ifdef ECL_THIRTY_TWO_BIT
 		/* copy out upper words of a */
 		switch (a_used) {
 		case 14:
-			a13 = MP_DIGIT(a, 13);
+			a6b = MP_DIGIT(a, 13);
 		case 13:
-			a12 = MP_DIGIT(a, 12);
+			a6a = MP_DIGIT(a, 12);
 		case 12:
-			a11 = MP_DIGIT(a, 11);
+			a5b = MP_DIGIT(a, 11);
 		case 11:
-			a10 = MP_DIGIT(a, 10);
+			a5a = MP_DIGIT(a, 10);
 		case 10:
-			a9 = MP_DIGIT(a, 9);
+			a4b = MP_DIGIT(a, 9);
 		case 9:
-			a8 = MP_DIGIT(a, 8);
+			a4a = MP_DIGIT(a, 8);
 		case 8:
-			a7 = MP_DIGIT(a, 7);
+			a3b = MP_DIGIT(a, 7);
+		}
+		r3a = MP_DIGIT(a, 6);
+		r2b= MP_DIGIT(a, 5);
+		r2a= MP_DIGIT(a, 4);
+		r1b = MP_DIGIT(a, 3);
+		r1a = MP_DIGIT(a, 2);
+		r0b = MP_DIGIT(a, 1);
+		r0a = MP_DIGIT(a, 0);
+
+
+		/* implement r = (a3a,a2,a1,a0)
+			+(a5a, a4,a3b,  0)
+			+(  0, a6,a5b,  0)
+			-(  0	 0,    0|a6b, a6a|a5b )
+			-(  a6b, a6a|a5b, a5a|a4b, a4a|a3b ) */
+		MP_ADD_CARRY (r1b, a3b, r1b, 0,     carry);
+		MP_ADD_CARRY (r2a, a4a, r2a, carry, carry);
+		MP_ADD_CARRY (r2b, a4b, r2b, carry, carry);
+		MP_ADD_CARRY (r3a, a5a, r3a, carry, carry);
+		r3b = carry;
+		MP_ADD_CARRY (r1b, a5b, r1b, 0,     carry);
+		MP_ADD_CARRY (r2a, a6a, r2a, carry, carry);
+		MP_ADD_CARRY (r2b, a6b, r2b, carry, carry);
+		MP_ADD_CARRY (r3a,   0, r3a, carry, carry);
+		r3b += carry;
+		MP_SUB_BORROW(r0a, a3b, r0a, 0,     carry);
+		MP_SUB_BORROW(r0b, a4a, r0b, carry, carry);
+		MP_SUB_BORROW(r1a, a4b, r1a, carry, carry);
+		MP_SUB_BORROW(r1b, a5a, r1b, carry, carry);
+		MP_SUB_BORROW(r2a, a5b, r2a, carry, carry);
+		MP_SUB_BORROW(r2b, a6a, r2b, carry, carry);
+		MP_SUB_BORROW(r3a, a6b, r3a, carry, carry);
+		r3b -= carry;
+		MP_SUB_BORROW(r0a, a5b, r0a, 0,     carry);
+		MP_SUB_BORROW(r0b, a6a, r0b, carry, carry);
+		MP_SUB_BORROW(r1a, a6b, r1a, carry, carry);
+		if (carry) {
+			MP_SUB_BORROW(r1b, 0, r1b, carry, carry);
+			MP_SUB_BORROW(r2a, 0, r2a, carry, carry);
+			MP_SUB_BORROW(r2b, 0, r2b, carry, carry);
+			MP_SUB_BORROW(r3a, 0, r3a, carry, carry);
+			r3b -= carry;
+		}
+
+		while (r3b > 0) {
+			int tmp;
+			MP_ADD_CARRY(r1b, r3b, r1b, 0,     carry);
+			if (carry) {
+				MP_ADD_CARRY(r2a,  0, r2a, carry, carry);
+				MP_ADD_CARRY(r2b,  0, r2b, carry, carry);
+				MP_ADD_CARRY(r3a,  0, r3a, carry, carry);
+			}
+			tmp = carry;
+			MP_SUB_BORROW(r0a, r3b, r0a, 0,     carry);
+			if (carry) {
+				MP_SUB_BORROW(r0b, 0, r0b, carry, carry);
+				MP_SUB_BORROW(r1a, 0, r1a, carry, carry);
+				MP_SUB_BORROW(r1b, 0, r1b, carry, carry);
+				MP_SUB_BORROW(r2a, 0, r2a, carry, carry);
+				MP_SUB_BORROW(r2b, 0, r2b, carry, carry);
+				MP_SUB_BORROW(r3a, 0, r3a, carry, carry);
+				tmp -= carry;
+			}
+			if (tmp < 0) {
+				printf("gone from pos to neg\n");
+			}
+			r3b = tmp;
+		}
+
+		while (r3b < 0) {
+			mp_digit maxInt = MP_DIGIT_MAX;
+                	MP_ADD_CARRY (r0a, 1, r0a, 0,     carry);
+                	MP_ADD_CARRY (r0b, 0, r0b, carry, carry);
+                	MP_ADD_CARRY (r1a, 0, r1a, carry, carry);
+                	MP_ADD_CARRY (r1b, maxInt, r1b, carry, carry);
+                	MP_ADD_CARRY (r2a, maxInt, r2a, carry, carry);
+                	MP_ADD_CARRY (r2b, maxInt, r2b, carry, carry);
+                	MP_ADD_CARRY (r3a, maxInt, r3a, carry, carry);
+			r3b += carry;
+		}
+		/* check for final reduction */
+		/* now the only way we are over is if the top 4 words are all ones */
+		if ((r3a == MP_DIGIT_MAX) && (r2b == MP_DIGIT_MAX)
+			&& (r2a == MP_DIGIT_MAX) && (r1b == MP_DIGIT_MAX) &&
+			 ((r1a != 0) || (r0b != 0) || (r0a != 0)) ) {
+			/* one last subraction */
+			MP_SUB_BORROW(r0a, 1, r0a, 0,     carry);
+			MP_SUB_BORROW(r0b, 0, r0b, carry, carry);
+			MP_SUB_BORROW(r1a, 0, r1a, carry, carry);
+			r1b = r2a = r2b = r3a = 0;
+		}
+
+
+		if (a != r) {
+			MP_CHECKOK(s_mp_pad(r, 7));
 		}
 		/* set the lower words of r */
-		if (a != r) {
-			MP_CHECKOK(s_mp_pad(r, 8));
-			MP_DIGIT(r, 6) = MP_DIGIT(a, 6);
-			MP_DIGIT(r, 5) = MP_DIGIT(a, 5);
-			MP_DIGIT(r, 4) = MP_DIGIT(a, 4);
-			MP_DIGIT(r, 3) = MP_DIGIT(a, 3);
-			MP_DIGIT(r, 2) = MP_DIGIT(a, 2);
-			MP_DIGIT(r, 1) = MP_DIGIT(a, 1);
-			MP_DIGIT(r, 0) = MP_DIGIT(a, 0);
-		}
+		MP_SIGN(r) = MP_ZPOS;
 		MP_USED(r) = 7;
-		switch (a_used) {
-		case 14:
-		case 13:
-		case 12:
-		case 11:
-			sa[6] = a10;
-		case 10:
-			sa[5] = a9;
-		case 9:
-			sa[4] = a8;
-		case 8:
-			sa[3] = a7;
-			sa[2] = sa[1] = sa[0] = 0;
-			MP_USED(&s) = a_used - 4;
-			if (MP_USED(&s) > 7)
-				MP_USED(&s) = 7;
-			MP_CHECKOK(mp_add(r, &s, r));
-		}
-		switch (a_used) {
-		case 14:
-			sa[5] = a13;
-		case 13:
-			sa[4] = a12;
-		case 12:
-			sa[3] = a11;
-			sa[2] = sa[1] = sa[0] = 0;
-			MP_USED(&s) = a_used - 8;
-			MP_CHECKOK(mp_add(r, &s, r));
-		}
-		switch (a_used) {
-		case 14:
-			sa[6] = a13;
-		case 13:
-			sa[5] = a12;
-		case 12:
-			sa[4] = a11;
-		case 11:
-			sa[3] = a10;
-		case 10:
-			sa[2] = a9;
-		case 9:
-			sa[1] = a8;
-		case 8:
-			sa[0] = a7;
-			MP_USED(&s) = a_used - 7;
-			MP_CHECKOK(mp_sub(r, &s, r));
-		}
-		switch (a_used) {
-		case 14:
-			sa[2] = a13;
-		case 13:
-			sa[1] = a12;
-		case 12:
-			sa[0] = a11;
-			MP_USED(&s) = a_used - 11;
-			MP_CHECKOK(mp_sub(r, &s, r));
-		}
-		/* there might be 1 or 2 bits left to reduce; use regular
-		 * reduction for this */
-		MP_CHECKOK(mp_mod(r, &meth->irr, r));
-	}
+		MP_DIGIT(r, 6) = r3a;
+		MP_DIGIT(r, 5) = r2b;
+		MP_DIGIT(r, 4) = r2a;
+		MP_DIGIT(r, 3) = r1b;
+		MP_DIGIT(r, 2) = r1a;
+		MP_DIGIT(r, 1) = r0b;
+		MP_DIGIT(r, 0) = r0a;
 #else
-	/* for polynomials larger than twice the field size, use regular
-	 * reduction */
-	if (a_used > 7) {
-		MP_CHECKOK(mp_mod(a, &meth->irr, r));
-	} else {
 		/* copy out upper words of a */
 		switch (a_used) {
 		case 7:
 			a6 = MP_DIGIT(a, 6);
+			a6b = a6 >> 32;
+			a6a_a5b = a6 << 32;
 		case 6:
 			a5 = MP_DIGIT(a, 5);
+			a5b = a5 >> 32;
+			a6a_a5b |= a5b;
+			a5b = a5b << 32;
+			a5a_a4b = a5 << 32;
+			a5a = a5 & 0xffffffff;
 		case 5:
 			a4 = MP_DIGIT(a, 4);
+			a5a_a4b |= a4 >> 32;
+			a4a_a3b = a4 << 32;
 		case 4:
-			a3 = MP_DIGIT(a, 3) >> 32;
+			a3b = MP_DIGIT(a, 3) >> 32;
+			a4a_a3b |= a3b;
+			a3b = a3b << 32;
+		}
+
+		r3 = MP_DIGIT(a, 3) & 0xffffffff;
+		r2 = MP_DIGIT(a, 2);
+		r1 = MP_DIGIT(a, 1);
+		r0 = MP_DIGIT(a, 0);
+
+		/* implement r = (a3a,a2,a1,a0)
+			+(a5a, a4,a3b,  0)
+			+(  0, a6,a5b,  0)
+			-(  0	 0,    0|a6b, a6a|a5b )
+			-(  a6b, a6a|a5b, a5a|a4b, a4a|a3b ) */
+		MP_ADD_CARRY (r1, a3b, r1, 0,     carry);
+		MP_ADD_CARRY (r2, a4 , r2, carry, carry);
+		MP_ADD_CARRY (r3, a5a, r3, carry, carry);
+		MP_ADD_CARRY (r1, a5b, r1, 0,     carry);
+		MP_ADD_CARRY (r2, a6 , r2, carry, carry);
+		MP_ADD_CARRY (r3,   0, r3, carry, carry);
+
+		MP_SUB_BORROW(r0, a4a_a3b, r0, 0,     carry);
+		MP_SUB_BORROW(r1, a5a_a4b, r1, carry, carry);
+		MP_SUB_BORROW(r2, a6a_a5b, r2, carry, carry);
+		MP_SUB_BORROW(r3, a6b    , r3, carry, carry);
+		MP_SUB_BORROW(r0, a6a_a5b, r0, 0,     carry);
+		MP_SUB_BORROW(r1, a6b    , r1, carry, carry);
+		if (carry) {
+			MP_SUB_BORROW(r2, 0, r2, carry, carry);
+			MP_SUB_BORROW(r3, 0, r3, carry, carry);
+		}
+
+
+		/* if the value is negative, r3 has a 2's complement 
+		 * high value */
+		r3b = (int)(r3 >>32);
+		while (r3b > 0) {
+			r3 &= 0xffffffff;
+			MP_ADD_CARRY(r1,((mp_digit)r3b) << 32, r1, 0, carry);
+			if (carry) {
+				MP_ADD_CARRY(r2,  0, r2, carry, carry);
+				MP_ADD_CARRY(r3,  0, r3, carry, carry);
+			}
+			MP_SUB_BORROW(r0, r3b, r0, 0, carry);
+			if (carry) {
+				MP_SUB_BORROW(r1, 0, r1, carry, carry);
+				MP_SUB_BORROW(r2, 0, r2, carry, carry);
+				MP_SUB_BORROW(r3, 0, r3, carry, carry);
+			}
+			r3b = (int)(r3 >>32);
+		}
+
+		while (r3b < 0) {
+                	MP_ADD_CARRY (r0, 1, r0, 0,     carry);
+                	MP_ADD_CARRY (r1, MP_DIGIT_MAX <<32, r1, carry, carry);
+                	MP_ADD_CARRY (r2, MP_DIGIT_MAX, r2, carry, carry);
+                	MP_ADD_CARRY (r3, MP_DIGIT_MAX >> 32, r3, carry, carry);
+			r3b = (int)(r3 >>32);
+		}
+		/* check for final reduction */
+		/* now the only way we are over is if the top 4 words are all ones */
+		if ((r3 == (MP_DIGIT_MAX >> 32)) && (r2 == MP_DIGIT_MAX)
+			&& ((r1 & MP_DIGIT_MAX << 32)== MP_DIGIT_MAX << 32) &&
+			 ((r1 != MP_DIGIT_MAX << 32 ) || (r0 != 0)) ) {
+			/* one last subraction */
+			MP_SUB_BORROW(r0, 1, r0, 0,     carry);
+			MP_SUB_BORROW(r1, 0, r1, carry, carry);
+			r2 = r3 = 0;
+		}
+
+
+		if (a != r) {
+			MP_CHECKOK(s_mp_pad(r, 4));
 		}
 		/* set the lower words of r */
-		if (a != r) {
-			MP_CHECKOK(s_mp_pad(r, 5));
-			MP_DIGIT(r, 3) = MP_DIGIT(a, 3) & 0xFFFFFFFF;
-			MP_DIGIT(r, 2) = MP_DIGIT(a, 2);
-			MP_DIGIT(r, 1) = MP_DIGIT(a, 1);
-			MP_DIGIT(r, 0) = MP_DIGIT(a, 0);
-		} else {
-			MP_DIGIT(r, 3) &= 0xFFFFFFFF;
-		}
+		MP_SIGN(r) = MP_ZPOS;
 		MP_USED(r) = 4;
-		switch (a_used) {
-		case 7:
-		case 6:
-			sa[3] = a5 & 0xFFFFFFFF;
-		case 5:
-			sa[2] = a4;
-		case 4:
-			sa[1] = a3 << 32;
-			sa[0] = 0;
-			MP_USED(&s) = a_used - 2;
-			if (MP_USED(&s) == 5)
-				MP_USED(&s) = 4;
-			MP_CHECKOK(mp_add(r, &s, r));
-		}
-		switch (a_used) {
-		case 7:
-			sa[2] = a6;
-		case 6:
-			sa[1] = (a5 >> 32) << 32;
-			sa[0] = 0;
-			MP_USED(&s) = a_used - 4;
-			MP_CHECKOK(mp_add(r, &s, r));
-		}
-		sa[2] = sa[1] = sa[0] = 0;
-		switch (a_used) {
-		case 7:
-			sa[3] = a6 >> 32;
-			sa[2] = a6 << 32;
-		case 6:
-			sa[2] |= a5 >> 32;
-			sa[1] = a5 << 32;
-		case 5:
-			sa[1] |= a4 >> 32;
-			sa[0] = a4 << 32;
-		case 4:
-			sa[0] |= a3;
-			MP_USED(&s) = a_used - 3;
-			MP_CHECKOK(mp_sub(r, &s, r));
-		}
-		sa[0] = 0;
-		switch (a_used) {
-		case 7:
-			sa[1] = a6 >> 32;
-			sa[0] = a6 << 32;
-		case 6:
-			sa[0] |= a5 >> 32;
-			MP_USED(&s) = a_used - 5;
-			MP_CHECKOK(mp_sub(r, &s, r));
-		}
-		/* there might be 1 or 2 bits left to reduce; use regular
-		 * reduction for this */
-		MP_CHECKOK(mp_mod(r, &meth->irr, r));
-	}
+		MP_DIGIT(r, 3) = r3;
+		MP_DIGIT(r, 2) = r2;
+		MP_DIGIT(r, 1) = r1;
+		MP_DIGIT(r, 0) = r0;
 #endif
+	}
 
   CLEANUP:
 	return res;
@@ -292,6 +335,31 @@ ec_GFp_nistp224_mul(const mp_int *a, const mp_int *b, mp_int *r,
 	return res;
 }
 
+/* Divides two field elements. If a is NULL, then returns the inverse of
+ * b. */
+mp_err
+ec_GFp_nistp224_div(const mp_int *a, const mp_int *b, mp_int *r,
+		   const GFMethod *meth)
+{
+	mp_err res = MP_OKAY;
+	mp_int t;
+
+	/* If a is NULL, then return the inverse of b, otherwise return a/b. */
+	if (a == NULL) {
+		return  mp_invmod(b, &meth->irr, r);
+	} else {
+		/* MPI doesn't support divmod, so we implement it using invmod and 
+		 * mulmod. */
+		MP_CHECKOK(mp_init(&t));
+		MP_CHECKOK(mp_invmod(b, &meth->irr, &t));
+		MP_CHECKOK(mp_mul(a, &t, r));
+		MP_CHECKOK(ec_GFp_nistp224_mod(r, r, meth));
+	  CLEANUP:
+		mp_clear(&t);
+		return res;
+	}
+}
+
 /* Wire in fast field arithmetic and precomputation of base point for
  * named curves. */
 mp_err
@@ -301,6 +369,7 @@ ec_group_set_gfp224(ECGroup *group, ECCurveName name)
 		group->meth->field_mod = &ec_GFp_nistp224_mod;
 		group->meth->field_mul = &ec_GFp_nistp224_mul;
 		group->meth->field_sqr = &ec_GFp_nistp224_sqr;
+		group->meth->field_div = &ec_GFp_nistp224_div;
 	}
 	return MP_OKAY;
 }
