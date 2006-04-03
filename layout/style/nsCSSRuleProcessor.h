5dae0dcf6bfd06fcc47ed445397c53a6fccd14f2
dbaron%dbaron.org <dbaron%dbaron.org>
2006-04-03 23:52:17 +0000

    Split the nsCSSRuleProcessor half of nsCSSStyleSheet.cpp into nsCSSRuleProcessor.cpp.  b=332136  r+sr=bzbarsky

--- layout/style/nsCSSRuleProcessor.h
+++ layout/style/nsCSSRuleProcessor.h
@@ -46,9 +46,9 @@
 #define nsCSSRuleProcessor_h_
 
 #include "nsIStyleRuleProcessor.h"
+#include "nsCSSStyleSheet.h"
 
 struct RuleCascadeData;
-class nsICSSStyleSheet;
 
 /**
  * The CSS style rule processor provides a mechanism for sibling style
