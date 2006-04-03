f4d94d08ac097678c2aec7581698eaac5a3c320f
dougt@meer.net <dougt@meer.net>
2006-04-03 18:52:20 +0000

    IME interrupts scrolling. wince only r/a=ere. b=332592

--- widget/src/windows/nsWindow.cpp
+++ widget/src/windows/nsWindow.cpp
@@ -4667,10 +4667,11 @@ PRBool nsWindow::ProcessMessage(UINT msg, WPARAM wParam, LPARAM lParam, LRESULT 
           break;
         }
       }
-#endif
+#else
       // check whether IME window do mouse operation
       if (IMEMouseHandling(NS_MOUSE_LEFT_BUTTON_DOWN, IMEMOUSE_LDOWN, lParam))
         break;
+#endif
       result = DispatchMouseEvent(NS_MOUSE_LEFT_BUTTON_DOWN, wParam, lParam);
       DispatchPendingEvents();
     }
