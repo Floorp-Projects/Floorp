69801323f299bfc142170ee39cdeefee57d8a9d1
wtchang@redhat.com <wtchang@redhat.com>
2006-04-05 21:02:25 +0000

    Bug 301064 r=wtc, sr=mkaply (OS/2 only) Patch from Andy Willis - add _declspec(dllexport/dllimport) for OS/2

--- nsprpub/pr/include/prtypes.h
+++ nsprpub/pr/include/prtypes.h
@@ -161,6 +161,22 @@
 #define PR_CALLBACK_DECL
 #define PR_STATIC_CALLBACK(__x) static __x
 
+#elif defined(XP_OS2) && defined(__declspec)
+
+#define PR_EXPORT(__type) extern __declspec(dllexport) __type
+#define PR_EXPORT_DATA(__type) extern __declspec(dllexport) __type
+#define PR_IMPORT(__type) extern  __declspec(dllimport) __type
+#define PR_IMPORT_DATA(__type) extern __declspec(dllimport) __type
+
+#define PR_EXTERN(__type) extern __declspec(dllexport) __type
+#define PR_IMPLEMENT(__type) __declspec(dllexport) __type
+#define PR_EXTERN_DATA(__type) extern __declspec(dllexport) __type
+#define PR_IMPLEMENT_DATA(__type) __declspec(dllexport) __type
+
+#define PR_CALLBACK
+#define PR_CALLBACK_DECL
+#define PR_STATIC_CALLBACK(__x) static __x
+
 #elif defined(XP_OS2_VACPP) 
 
 #define PR_EXPORT(__type) extern __type
