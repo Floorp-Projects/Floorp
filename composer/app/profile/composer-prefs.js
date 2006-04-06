726a6859ec77da849dda0ec58798b0e1e9a66b46
daniel%glazman.org <daniel%glazman.org>
2006-04-06 11:35:11 +0000

    NOT PART OF THE BUILD package files needed for xulrunner

---
+++ composer/app/profile/composer-prefs.js
@@ -0,0 +1,22 @@
+pref("toolkit.defaultChromeURI", "chrome://composer/content/composer.xul");
+
+/* debugging prefs */
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
+
+pref("general.useragent.extra.mybrowser", "MyBrowser/0.1");
+
+pref("extensions.update.enabled", true);
+pref("extensions.update.url", "chrome://mozapps/locale/extensions/extensions.properties");
+pref("extensions.update.interval", 86400);  // Check for updates to Extensions and 
+                                            // Themes every week
+// Non-symmetric (not shared by extensions) extension-specific [update] preferences
+pref("extensions.getMoreExtensionsURL", "chrome://mozapps/locale/extensions/extensions.properties");
+pref("extensions.getMoreThemesURL", "chrome://mozapps/locale/extensions/extensions.properties");
+pref("extensions.dss.enabled", false);          // Dynamic Skin Switching                                               
+pref("extensions.dss.switchPending", false);    // Non-dynamic switch pending after next
+                                                // restart.
+                                                
