da8e786aad2b42a155d2d9c4fddf727f26993377
bugzilla%micropipes.com <bugzilla%micropipes.com>
2006-04-07 02:10:46 +0000

    fixed injection issue (bug 333034)

--- webtools/addons/public/htdocs/js/auth.js
+++ webtools/addons/public/htdocs/js/auth.js
@@ -31,5 +31,5 @@ function readCookie(name)
 /* Someone should make this a js builtin */
 function htmlEntities(str)
 {
-    return str.replace('&','&amp;').replace('<','&lt;').replace('>','&gt;').replace('"','&quot;').replace('+','&nbsp;');
+    return str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/\+/g,'&nbsp;');
 }
