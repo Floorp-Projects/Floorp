dd6ec36444a040b66380a152d47d1b455ea2cbc1
masayuki@d-toybox.com <masayuki@d-toybox.com>
2006-04-10 15:27:29 +0000

    Bug 201071 signature file containing accented characters has become corrupted / Bug 153855 Composer does not display a UTF-8 signature in the right way. r=jshin, sr=bienvenu

--- intl/uconv/public/nsIPlatformCharset.h
+++ intl/uconv/public/nsIPlatformCharset.h
@@ -62,7 +62,8 @@ typedef enum {
      kPlatformCharsetSel_4xBookmarkFile = 3,
      kPlatformCharsetSel_KeyboardInput = 4,
      kPlatformCharsetSel_WindowManager = 5,
-     kPlatformCharsetSel_4xPrefsJS = 6
+     kPlatformCharsetSel_4xPrefsJS = 6,
+     kPlatformCharsetSel_PlainTextInFile = 7
 } nsPlatformCharsetSel;
 
 class nsIPlatformCharset : public nsISupports
