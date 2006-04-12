4a5f940a0f6257a614717d2a9c0e404defff2bc7
benjamin@smedbergs.us <benjamin@smedbergs.us>
2006-04-12 13:56:58 +0000

    Bug 333410 - don't install elf-dynstr-gc to dist/bin, patch by Mike Hommey (mh+mozilla@glandium.org), r=me

--- config/config.mk
+++ config/config.mk
@@ -661,7 +661,7 @@ endif
 ifndef CROSS_COMPILE
 ifdef USE_ELF_DYNSTR_GC
 ifdef MOZ_COMPONENTS_VERSION_SCRIPT_LDFLAGS
-ELF_DYNSTR_GC 	= $(DIST)/bin/elf-dynstr-gc
+ELF_DYNSTR_GC 	= $(DEPTH)/config/elf-dynstr-gc
 endif
 endif
 endif
