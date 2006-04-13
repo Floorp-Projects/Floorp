66e592306045f5d32b739f93470c6da77d8d3b61
dougt%meer.net <dougt%meer.net>
2006-04-13 22:38:06 +0000

    Adding native SDR.  wince/minimo only.

--- minimo/components/ssl/nsNativeSSL.cpp
+++ minimo/components/ssl/nsNativeSSL.cpp
@@ -1,3 +1,39 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Minimo.
+ *
+ * The Initial Developer of the Original Code is
+ * Doug Turner <dougt@meer.net>.
+ * Portions created by the Initial Developer are Copyright (C) 2006
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 #include "nsIServiceManager.h"
 #include "nsIGenericFactory.h"
 
@@ -18,6 +54,8 @@
 #include "nsIWebProgressListener.h"
 
 #include "nsSecureBrowserUIImpl.h"
+
+#include "nsNativeSDR.h"
 
 #include <Winsock2.h>
 #include <sslsock.h>
@@ -470,8 +508,18 @@ nsWINCESSLSocketProvider::AddToSocket(PRInt32 family,
     {0xbc, 0xa9, 0xda, 0x81, 0x70, 0xfd, 0x8b, 0x58}   \
 }
 
+
+#define NS_WINCENATIVESDR_CID                          \
+{   0xcba2f351,                                        \
+    0x1993,                                            \
+    0x4b27,                                            \
+    {0xbf, 0x60, 0x4e, 0xbf, 0xce, 0x9a, 0xa3, 0x58}   \
+}
+
+
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsWINCESSLSocketProvider)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsSecureBrowserUIImpl)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsNativeSDR)
 
 static const nsModuleComponentInfo components[] =
 {
@@ -486,6 +534,13 @@ static const nsModuleComponentInfo components[] =
       NS_SECURE_BROWSER_UI_CID,
       NS_SECURE_BROWSER_UI_CONTRACTID,
       nsSecureBrowserUIImplConstructor
+    },
+
+    {
+      "Native SDR",
+      NS_WINCENATIVESDR_CID,
+      "@mozilla.org/security/sdr;1",
+      nsNativeSDRConstructor
     },
 
 };
