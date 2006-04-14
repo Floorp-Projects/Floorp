47118369b1ff7eb3aca4e7c3c71f684a9f811439
gavin@gavinsharp.com <gavin@gavinsharp.com>
2006-04-14 19:12:31 +0000

    Bug 331749: chrome://pippki/content/newserver.js is not UTF-8, patch by Philip Withnall <bugzilla@tecnocode.co.uk>, r=kaie

--- security/manager/pki/resources/content/newserver.js
+++ security/manager/pki/resources/content/newserver.js
@@ -22,7 +22,7 @@
  *
  * Contributor(s):
  *   Javier Delgadillo <javi@netscape.com>
- *   Håkan Waara <hwaara@chello.se>
+ *   HÃ¥kan Waara <hwaara@chello.se>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
