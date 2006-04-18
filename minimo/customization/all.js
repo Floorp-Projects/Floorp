28df8d86866cb75e4087421b667df604ded75dbc
mgalli%geckonnection.com <mgalli%geckonnection.com>
2006-04-18 18:49:56 +0000

    Upddate to URL homebar, mozilla minimo update service.

--- minimo/customization/all.js
+++ minimo/customization/all.js
@@ -664,12 +664,12 @@ pref("dom.max_script_run_time", 60);
 /* Bookmark store as a pref for now, homebar section as well */
 
 pref("browser.bookmark.store","<bm></bm>");
-pref("browser.bookmark.homebar","<bm><li iconsrc='chrome://minimo/skin/extensions/icon-star.png'>chrome://minimo/content/rssview/rssload.xhtml?url=http://del.icio.us/rss/tag/mobile-sites</li><li iconsrc='chrome://minimo/skin/extensions/icon-google.png'>http://www.google.com/xhtml</li><li iconsrc='chrome://minimo/skin/extensions/icon-shopping.png'>http://www.yokel.com</li><li iconsrc='http://m.technorati.com/favicon.ico'>http://m.technorati.com/</li><li iconsrc='http://mobile.wunderground.com/favicon.ico'>http://mobile.wunderground.com/</li><li iconsrc='http://www.yelp.com/favicon.ico'>http://www.yelp.com</li><li iconsrc='http://www.mozilla.org/projects/minimo/images/favicon.ico'>http://www.mozilla.org/projects/minimo/</li></bm>");
+pref("browser.bookmark.homebar","<bm><li iconsrc='chrome://minimo/skin/extensions/icon-star.png'>chrome://minimo/content/rssview/rssload.xhtml?url=http://del.icio.us/rss/tag/mobile-sites</li><li iconsrc='chrome://minimo/skin/extensions/icon-google.png'>http://www.google.com/xhtml</li><li iconsrc='chrome://minimo/skin/extensions/icon-cash.png'>chrome://minimo/content/rssview/rssload.xhtml?url=http://www.xanadb.com/ticker/goog+yhoo+msft+nok+fte.pa</li><li iconsrc='chrome://minimo/skin/extensions/icon-shopping.png'>http://www.yokel.com</li><li iconsrc='http://m.technorati.com/favicon.ico'>http://m.technorati.com/</li><li iconsrc='http://mobile.wunderground.com/favicon.ico'>http://mobile.wunderground.com/</li><li iconsrc='http://www.yelp.com/favicon.ico'>http://www.yelp.com</li><li iconsrc='http://www.meer.net/dougt/minimo_ce/start/update.cgi'>http://www.mozilla.org/projects/minimo/update/latest-update.xhtml</li></bm>");
 
 pref("browser.download.dir", "\\Storage Card");
 pref("browser.download.progressDnlgDialog.dontAskForLaunch", false);
 pref("browser.cache.disk.parent_directory","\\Storage Card");
 pref("browser.cache.disk.enable",false);
 
-
-
+pref("signon.rememberSignons", true);
+pref("signon.SignonFileName", "signons.txt");
