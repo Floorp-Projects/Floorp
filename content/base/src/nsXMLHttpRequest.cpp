e3bad2f0d38790259d29355d9b9cb412fc4b6fc0
shanjian%netscape.com <shanjian%netscape.com>
2006-04-20 03:37:43 +0000

    While I was checking in fix of 88363 yesterday, a conflict mess got into my check in. Remove that mess.

--- content/base/src/nsXMLHttpRequest.cpp
+++ content/base/src/nsXMLHttpRequest.cpp
@@ -2,31 +2,16 @@
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: NPL 1.1/GPL 2.0/LGPL 2.1
  *
-<<<<<<< nsXMLHttpRequest.cpp
- * The contents of this file are subject to the Netscape Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/NPL/
-=======
  * The contents of this file are subject to the Netscape Public License
  * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.mozilla.org/NPL/
->>>>>>> 1.43
  *
-<<<<<<< nsXMLHttpRequest.cpp
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
- *
-=======
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
  *
->>>>>>> 1.43
  * The Original Code is mozilla.org code.
  *
  * The Initial Developer of the Original Code is 
