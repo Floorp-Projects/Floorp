5401d9a016d46ad8bcf6ea482ea988eeb0391312
heikki%netscape.com <heikki%netscape.com>
2006-04-20 03:38:25 +0000

    include no longer needed.

--- content/base/src/nsXMLHttpRequest.h
+++ content/base/src/nsXMLHttpRequest.h
@@ -50,7 +50,6 @@
 #include "nsIHttpChannel.h"
 #include "nsIDocument.h"
 #include "nsIStreamListener.h"
-#include "nsIWebBrowserChrome.h"
 #include "nsIEventQueueService.h"
 #include "nsWeakReference.h"
 #include "nsISupportsArray.h"
