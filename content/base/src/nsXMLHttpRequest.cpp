fbef8a4091b9620d6d3127de610854eb24e973bd
henry.jia%sun.com <henry.jia%sun.com>
2006-04-20 03:38:06 +0000

    4th patch for bug 158080 Description: replace the hardcode of "@mozilla.org/embedcomp/window-watcher;1" with NS_WINDOWWATCHER_CONTRACTID Patch by Henry.Jia@sun.com r=anto, sr=alecf

--- content/base/src/nsXMLHttpRequest.cpp
+++ content/base/src/nsXMLHttpRequest.cpp
@@ -1503,7 +1503,7 @@ nsXMLHttpRequest::GetInterface(const nsIID & aIID, void **aResult)
     *aResult = nsnull;
 
     nsresult rv;
-    nsCOMPtr<nsIWindowWatcher> ww(do_GetService("@mozilla.org/embedcomp/window-watcher;1", &rv));
+    nsCOMPtr<nsIWindowWatcher> ww(do_GetService(NS_WINDOWWATCHER_CONTRACTID, &rv));
     if (NS_FAILED(rv))
       return rv;
 
