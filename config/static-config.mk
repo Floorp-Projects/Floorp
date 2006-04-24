8990ac7a728f3e35e8875eb3bbaffefd2d97f387
mark%moxienet.com <mark%moxienet.com>
2006-04-24 18:27:37 +0000

    332170 gtk2 Xft static builds may fail with unresolved symbols from libXft when gtk2's library list includes libpangocairo instead of libpangoxft.  r=bsmedberg

--- config/static-config.mk
+++ config/static-config.mk
@@ -115,6 +115,10 @@ ifdef MOZ_ENABLE_XPRINT
 STATIC_EXTRA_LIBS	+= $(MOZ_XPRINT_LDFLAGS)
 endif
 
+ifdef MOZ_ENABLE_XFT
+STATIC_EXTRA_LIBS	+= $(MOZ_XFT_LIBS)
+endif
+
 # Component Makefile always brings in this.
 # STATIC_EXTRA_LIBS	+= $(TK_LIBS)
 
