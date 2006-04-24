32712e68abab7a966cc7f2fc69e2f6ca31dd731c
wtchang@redhat.com <wtchang@redhat.com>
2006-04-24 20:07:15 +0000

    Bugzilla bug 335275: compile with the GCC flag -Werror-implicit-function-declaration. r=nelsonb. Modified files: Linux.mk SunOS5.mk

--- security/coreconf/SunOS5.mk
+++ security/coreconf/SunOS5.mk
@@ -94,7 +94,7 @@ DEFAULT_COMPILER = cc
 
 ifdef NS_USE_GCC
 	CC         = gcc
-	OS_CFLAGS += -Wall -Wno-format
+	OS_CFLAGS += -Wall -Wno-format -Werror-implicit-function-declaration
 	CCC        = g++
 	CCC       += -Wall -Wno-format
 	ASFLAGS	  += -x assembler-with-cpp
