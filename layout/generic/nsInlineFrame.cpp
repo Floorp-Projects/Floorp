fc7797defdd6a368f90dc3719723eeae8429b76b
aaronleventhal%moonset.net <aaronleventhal%moonset.net>
2006-04-27 16:23:09 +0000

    Bug 326090. Expose <hr> as ROLE_SEPARATOR. Patch by Ming (Mike) Gao. r=aaronlev, sr=roc

--- layout/generic/nsInlineFrame.cpp
+++ layout/generic/nsInlineFrame.cpp
@@ -818,8 +818,7 @@ NS_IMETHODIMP nsInlineFrame::GetAccessible(nsIAccessible** aAccessible)
   *aAccessible = nsnull;
   nsIAtom *tagAtom = mContent->Tag();
   if ((tagAtom == nsHTMLAtoms::img || tagAtom == nsHTMLAtoms::input || 
-       tagAtom == nsHTMLAtoms::label || tagAtom == nsHTMLAtoms::hr) &&
-      mContent->IsContentOfType(nsIContent::eHTML)) {
+       tagAtom == nsHTMLAtoms::label) && mContent->IsContentOfType(nsIContent::eHTML)) {
     // Only get accessibility service if we're going to use it
     nsCOMPtr<nsIAccessibilityService> accService(do_GetService("@mozilla.org/accessibilityService;1"));
     if (!accService)
@@ -830,8 +829,6 @@ NS_IMETHODIMP nsInlineFrame::GetAccessible(nsIAccessible** aAccessible)
       return accService->CreateHTMLImageAccessible(NS_STATIC_CAST(nsIFrame*, this), aAccessible);
     else if (tagAtom == nsHTMLAtoms::label)  // Creat accessible for <label>
       return accService->CreateHTMLLabelAccessible(NS_STATIC_CAST(nsIFrame*, this), aAccessible);
-    // Create accessible for <hr>
-    return accService->CreateHTMLHRAccessible(NS_STATIC_CAST(nsIFrame*, this), aAccessible);
   }
 
   return NS_ERROR_FAILURE;
