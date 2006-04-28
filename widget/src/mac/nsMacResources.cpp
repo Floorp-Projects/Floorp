a6a3bdea542af1e18c4aab70368b76367f2f6f7c
joshmoz@gmail.com <joshmoz@gmail.com>
2006-04-28 21:58:04 +0000

    remove unused Mac widget code, detab the file. no bug.

--- widget/src/mac/nsMacResources.cpp
+++ widget/src/mac/nsMacResources.cpp
@@ -39,53 +39,12 @@
 #include <Resources.h>
 #include <Processes.h>
 
-short nsMacResources::mRefNum				= kResFileNotOpened;
-short nsMacResources::mSaveResFile	= 0;
+short nsMacResources::mRefNum      = kResFileNotOpened;
+short nsMacResources::mSaveResFile = 0;
 
-#if !defined(XP_MACOSX)
-pascal OSErr __NSInitialize(const CFragInitBlock *theInitBlock);
-pascal OSErr __initializeResources(const CFragInitBlock *theInitBlock);
-
-pascal void __NSTerminate(void);
-pascal void __terminateResources(void);
-
-//----------------------------------------------------------------------------------------
-//
-//----------------------------------------------------------------------------------------
-pascal OSErr __initializeResources(const CFragInitBlock *theInitBlock)
-{
-    OSErr err = __NSInitialize(theInitBlock);
-    if (err)
-    	return err;
-
-	short saveResFile = ::CurResFile();
-
-	short refNum = FSpOpenResFile(theInitBlock->fragLocator.u.onDisk.fileSpec, fsRdPerm);
-	nsMacResources::SetLocalResourceFile(refNum);
-
-	::UseResFile(saveResFile);
-
-	return ::ResError();
-}
-
-
-//----------------------------------------------------------------------------------------
-//
-//----------------------------------------------------------------------------------------
-pascal void __terminateResources(void)
-{
-	::CloseResFile(nsMacResources::GetLocalResourceFile());
-    __NSTerminate();
-}
-#endif /*!XP_MACOSX*/
-
-//----------------------------------------------------------------------------------------
-//
-//----------------------------------------------------------------------------------------
 
 nsresult nsMacResources::OpenLocalResourceFile()
 {
-#ifdef XP_MACOSX
   if (mRefNum == kResFileNotOpened) {
     ProcessSerialNumber PSN;
     ProcessInfoRec pinfo;
@@ -104,28 +63,23 @@ nsresult nsMacResources::OpenLocalResourceFile()
         FSOpenResourceFile(&resRef, 0, NULL, fsRdPerm, &mRefNum);
     }
   }
-#endif
-	if (mRefNum == kResFileNotOpened)
-		return NS_ERROR_NOT_INITIALIZED;
+  if (mRefNum == kResFileNotOpened)
+    return NS_ERROR_NOT_INITIALIZED;
 
-	mSaveResFile = ::CurResFile();
-	::UseResFile(mRefNum);
+  mSaveResFile = ::CurResFile();
+  ::UseResFile(mRefNum);
 
-	return (::ResError() == noErr ? NS_OK : NS_ERROR_FAILURE);
+  return (::ResError() == noErr ? NS_OK : NS_ERROR_FAILURE);
 }
 
 
-//----------------------------------------------------------------------------------------
-//
-//----------------------------------------------------------------------------------------
-
 nsresult nsMacResources::CloseLocalResourceFile()
 {
-	if (mRefNum == kResFileNotOpened)
-		return NS_ERROR_NOT_INITIALIZED;
+  if (mRefNum == kResFileNotOpened)
+    return NS_ERROR_NOT_INITIALIZED;
 
-	::UseResFile(mSaveResFile);
+  ::UseResFile(mSaveResFile);
 
-	return (::ResError() == noErr ? NS_OK : NS_ERROR_FAILURE);
+  return (::ResError() == noErr ? NS_OK : NS_ERROR_FAILURE);
 }
 
