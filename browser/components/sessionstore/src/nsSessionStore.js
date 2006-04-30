b7405a4f5b4e4113b500a572c41fa0b03f895d71
mconnor@steelgryphon.com <mconnor@steelgryphon.com>
2006-04-30 05:02:43 +0000

    bug 328159 - take 2, r=me

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -149,7 +149,6 @@ function debug(aMsg) {
 
 /* :::::::: The Service ::::::::::::::: */
 
-//var SessionStoreService = {
 function SessionStoreService() {
 }
 
@@ -234,14 +233,14 @@ SessionStoreService.prototype = {
         }, this);
         delete this._initialState.Window[0].hidden;
       }
-      catch (ex) { debug(ex); } // invalid .INI file - nothing can be restored
+      catch (ex) { debug("The session file is invalid: " + ex); } // invalid .INI file - nothing can be restored
     }
     
     // if last session crashed, backup the session
     // and try to restore the disk cache
     if (this._lastSessionCrashed) {
       try {
-        this._writeFile(this._getSessionFile(true), aState, true);
+        this._writeFile(this._getSessionFile(true), iniString);
       }
       catch (ex) { } // nothing else we can do here
       try {
@@ -1670,7 +1669,7 @@ SessionStoreService.prototype = {
       "state=" + (this._loadState == STATE_RUNNING ? STATE_RUNNING_STR : STATE_STOPPED_STR),
       this._getCurrentState(),
       ""
-    ].join("\n").replace(/\n\[/g, "\n$&"), aUpdateAll);
+    ].join("\n").replace(/\n\[/g, "\n$&"));
     this._lastSaveTime = Date.now();
   },
 
@@ -1991,27 +1990,12 @@ SessionStoreService.prototype = {
    *        nsIFile
    * @param aData
    *        String data
-   * @param aThisThread
-   *        bool Write in current thread
    */
-  _writeFile: function sss_writeFile(aFile, aData, aThisThread) {
+  _writeFile: function sss_writeFile(aFile, aData) {
     // save the file in the current thread
     // (making sure we don't get killed at shutdown)
-    if (aThisThread) {
-      (new FileWriter(aFile, aData)).run();
-      return;
-    }
-    
-    // save file in new thread
-    var nsIThread = Ci.nsIThread;
-    var thread = Cc["@mozilla.org/thread;1"].createInstance(Ci.nsIThread);
-    thread.init(
-      new FileWriter(aFile, aData),
-      128 * 1024,
-      nsIThread.PRIORITY_NORMAL,
-      nsIThread.SCOPE_GLOBAL,
-      nsIThread.STATE_UNJOINABLE
-    );
+    (new FileWriter(aFile, aData)).run();
+    return;
   },
 
 /* ........ QueryInterface .............. */
@@ -2268,7 +2252,6 @@ const SessionStoreFactory = {
       return null;
     }
     
-    //return SessionStoreService.QueryInterface(aIID);
     return (new SessionStoreService()).QueryInterface(aIID);
   },
 
