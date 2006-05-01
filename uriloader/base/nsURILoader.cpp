2b40b06af8f781698b80515e627ffc1746d75fb0
cbiesinger@web.de <cbiesinger@web.de>
2006-05-01 18:36:30 +0000

    bug 336108 add OOM check. r+sr+a=bz

--- uriloader/base/nsURILoader.cpp
+++ uriloader/base/nsURILoader.cpp
@@ -962,6 +962,8 @@ nsresult nsURILoader::OpenChannel(nsIChannel* channel,
       listener->GetLoadCookie(getter_AddRefs(cookie));
       if (!cookie) {
         nsRefPtr<nsDocLoader> newDocLoader = new nsDocLoader();
+        if (!newDocLoader)
+          return NS_ERROR_OUT_OF_MEMORY;
         nsresult rv = newDocLoader->Init();
         if (NS_FAILED(rv))
           return rv;
