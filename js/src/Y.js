27b5f3a944480f54b5e03344467c2a011edb3fed
mrbkap%gmail.com <mrbkap%gmail.com>
2006-05-02 00:20:31 +0000

    Tab patrol

--- js/src/Y.js
+++ js/src/Y.js
@@ -2,16 +2,16 @@
 
 function factorial(proc) {
     return function (n) {
-	return (n <= 1) ? 1 : n * proc(n-1);
+        return (n <= 1) ? 1 : n * proc(n-1);
     }
 }
 
 function Y(outer) {
     function inner(proc) {
-	function apply(arg) {
-	    return proc(proc)(arg);
-	}
-	return outer(apply);
+        function apply(arg) {
+            return proc(proc)(arg);
+        }
+        return outer(apply);
     }
     return inner(inner);
 }
