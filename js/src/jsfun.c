081c184c2ccb4156fb87523dce0006b100cfc680
mrbkap@gmail.com <mrbkap@gmail.com>
2006-05-05 23:17:22 +0000

    Expand the checks to more cases. bug 336601, r+a181=brendan a=dveditz

--- js/src/jsfun.c
+++ js/src/jsfun.c
@@ -1543,12 +1543,6 @@ fun_call(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
     for (i = 0; i < argc; i++)
         *sp++ = argv[i];
 
-    obj = js_SafeComputeThis(cx, obj, sp - argc);
-    if (!obj) {
-        ok = JS_FALSE;
-        goto out;
-    }
-
     /* Lift current frame to include the args and do the call. */
     fp = cx->fp;
     oldsp = fp->sp;
@@ -1558,7 +1552,6 @@ fun_call(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
     /* Store rval and pop stack back to our frame's sp. */
     *rval = fp->sp[-1];
     fp->sp = oldsp;
-out:
     js_FreeStack(cx, mark);
     return ok;
 }
@@ -1637,12 +1630,6 @@ fun_apply(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
         if (!ok)
             goto out;
         sp++;
-    }
-
-    obj = js_SafeComputeThis(cx, obj, sp - argc);
-    if (!obj) {
-        ok = JS_FALSE;
-        goto out;
     }
 
     /* Lift current frame to include the args and do the call. */
