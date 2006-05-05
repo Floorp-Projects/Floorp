605be74d73e65abff94ae2631a97798483e98dcc
mconnor%steelgryphon.com <mconnor%steelgryphon.com>
2006-05-05 18:01:35 +0000

    bug 328159 - the one that should work, or I'm buying the build team drinks

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -110,6 +110,9 @@ const DEFAULT_RESUME_SESSION = false;
 // resume the current session at startup just this once
 const DEFAULT_RESUME_SESSION_ONCE = false;
 
+// resume the current session at startup if it had previously crashed
+const DEFAULT_RESUME_FROM_CRASH = false;
+
 // global notifications observed
 const OBSERVING = [
   "domwindowopened", "domwindowclosed",
@@ -149,7 +152,6 @@ function debug(aMsg) {
 
 /* :::::::: The Service ::::::::::::::: */
 
-//var SessionStoreService = {
 function SessionStoreService() {
 }
 
@@ -234,14 +236,14 @@ SessionStoreService.prototype = {
         }, this);
         delete this._initialState.Window[0].hidden;
       }
-      catch (ex) { debug(ex); } // invalid .INI file - nothing can be restored
+      catch (ex) { "The session file is invalid: " + debug(ex); } // invalid .INI file - nothing can be restored
     }
     
     // if last session crashed, backup the session
     // and try to restore the disk cache
     if (this._lastSessionCrashed) {
       try {
-        this._writeFile(this._getSessionFile(true), aState, true);
+        this._writeFile(this._getSessionFile(true), iniString);
       }
       catch (ex) { } // nothing else we can do here
       try {
@@ -1670,7 +1672,7 @@ SessionStoreService.prototype = {
       "state=" + (this._loadState == STATE_RUNNING ? STATE_RUNNING_STR : STATE_STOPPED_STR),
       this._getCurrentState(),
       ""
-    ].join("\n").replace(/\n\[/g, "\n$&"), aUpdateAll);
+    ].join("\n").replace(/\n\[/g, "\n$&"));
     this._lastSaveTime = Date.now();
   },
 
@@ -1769,6 +1771,10 @@ SessionStoreService.prototype = {
    * @returns bool
    */
   _doRecoverSession: function sss_doRecoverSession() {
+    // do not prompt or resume, post-crash
+    if (!this._getPref("sessionstore.resume_from_crash", DEFAULT_RESUME_FROM_CRASH))
+      return false;
+
     // if the prompt fails, recover anyway
     var recover = true;
     // allow extensions to hook in a more elaborate restore prompt
@@ -1991,27 +1997,12 @@ SessionStoreService.prototype = {
    *        nsIFile
    * @param aData
    *        String data
-   * @param aThisThread
-   *        bool Write in current thread
    */
-  _writeFile: function sss_writeFile(aFile, aData, aThisThread) {
+  _writeFile: function sss_writeFile(aFile, aData) {
     // save the file in the current thread
     // (making sure we don't get killed at shutdown)
-    if (aThisThread) {
-      (new FileWriter(aFile, aData)).run();
-      return;
-    }
-    
-    // save file in new thread
-    var nsIThread = Ci.nsIThread;
-    var thread = Cc["@mozilla.org/thread;1"].createInstance(Ci.nsIThread);
-    thread.init(
-      new FileWriter(aFile, aData),
-      128 * 1024,
-      nsIThread.PRIORITY_NORMAL,
-      nsIThread.SCOPE_GLOBAL,
-      nsIThread.STATE_UNJOINABLE
-    );
+    (new FileWriter(aFile, aData)).run();
+    return;
   },
 
 /* ........ QueryInterface .............. */
@@ -2268,7 +2259,6 @@ const SessionStoreFactory = {
       return null;
     }
     
-    //return SessionStoreService.QueryInterface(aIID);
     return (new SessionStoreService()).QueryInterface(aIID);
   },
 
