bf9b45e4c0739fc41aad43503e3e7238249475cc
bzbarsky@mit.edu <bzbarsky@mit.edu>
2006-05-10 04:48:18 +0000

    Temporarily disable session restore to test effect on Ts.  Bug 337320

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -89,7 +89,7 @@ const PRIVACY_FULL = 2;
 /* :::::::: Pref Defaults :::::::::::::::::::: */
 
 // whether the service is enabled
-const DEFAULT_ENABLED = true;
+const DEFAULT_ENABLED = false;
 
 // minimal interval between two save operations (in milliseconds)
 const DEFAULT_INTERVAL = 10000;
