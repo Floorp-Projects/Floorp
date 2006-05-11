eb33c382db6b149b0efd1d82a8289845399bfeeb
timeless@mozdev.org <timeless@mozdev.org>
2006-05-11 13:20:37 +0000

    Bug 336479 oomish crash [@ cryptojs_GetScriptPrincipal] because of a missing * r=kaie sr=bz

--- security/manager/ssl/src/nsCrypto.cpp
+++ security/manager/ssl/src/nsCrypto.cpp
@@ -289,7 +289,7 @@ cryptojs_GetScriptPrincipal(JSContext *cx, JSScript *script,
   }
   nsJSPrincipals *nsJSPrin = NS_STATIC_CAST(nsJSPrincipals *, jsp);
   *result = nsJSPrin->nsIPrincipalPtr;
-  if (!result) {
+  if (!*result) {
     return NS_ERROR_FAILURE;
   }
   NS_ADDREF(*result);
