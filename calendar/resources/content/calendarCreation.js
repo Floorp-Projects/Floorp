5fc64db25458a4137fc58d324befffbea0b68db6
jminta%gmail.com <jminta%gmail.com>
2006-05-13 19:36:44 +0000

    Bug 336167 Make it easier for extensions to add new providers.  patch by garyvdm@gmail.com, r=mvl

--- calendar/resources/content/calendarCreation.js
+++ calendar/resources/content/calendarCreation.js
@@ -19,7 +19,7 @@
  * Portions created by the Initial Developer are Copyright (C) 2005
  * the Initial Developer. All Rights Reserved.
  *
- * Contributor(s):
+ * Contributor(s): Gary van der Merwe <garyvdm@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -70,11 +70,7 @@ function doCreateCalendar()
         uri = 'moz-profile-calendar://?id=2';
     } else {
         uri = document.getElementById("calendar-uri").value;
-        var format = document.getElementById('calendar-format').selectedItem.value;
-        if (format == 'webdav')
-            provider = 'ics';
-        else
-            provider = 'caldav';
+        provider = document.getElementById('calendar-format').selectedItem.value;
     }
         
     var calManager = getCalendarManager();
