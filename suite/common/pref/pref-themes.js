c87fa4abf8b8349e2a6828ea38ad109300da86d7
ben%netscape.com <ben%netscape.com>
2006-05-17 02:27:16 +0000

    Fix for 44025 - clean up UI of themes pref panel; 49763 - enable 'restore default' theme

--- suite/common/pref/pref-themes.js
+++ suite/common/pref/pref-themes.js
@@ -2,95 +2,86 @@
 // Theme Selector
 // ( 05/09/2000, Ben Goodger <ben@netscape.com> )
 
+var bundle = srGetStrBundle("chrome://communicator/locale/pref/prefutilities.properties");
+
 const DEBUG_USE_PROFILE = true;
 
 
-try 
-  {
-    var chromeRegistry = Components.classes["component://netscape/chrome/chrome-registry"].getService();
-    if ( chromeRegistry )
-      chromeRegistry = chromeRegistry.QueryInterface( Components.interfaces.nsIChromeRegistry );
-  }
-catch(e) 
-  {
-  }
+try {
+  var chromeRegistry = Components.classes["component://netscape/chrome/chrome-registry"].getService();
+  if ( chromeRegistry )
+    chromeRegistry = chromeRegistry.QueryInterface( Components.interfaces.nsIChromeRegistry );
+}
+catch(e) {}
 
-function selectSkin()
-  {
+function Startup()
+{
+  var tree = document.getElementById( "skinsTree" );
+  var theSkinKids = document.getElementById("theSkinKids");
+  if (theSkinKids.hasChildNodes() && theSkinKids.firstChild)
+    tree.selectItem(theSkinKids.firstChild);
+}
+
+function applySkin()
+{
+  var tree = document.getElementById( "skinsTree" );
+  var selectedSkinItem = tree.selectedItems[0];
+  var skinName = selectedSkinItem.getAttribute( "name" );
+  chromeRegistry.selectSkin( skinName, DEBUG_USE_PROFILE ); 
+  chromeRegistry.refreshSkins();
+}  
+ 
+function deselectSkin()
+{
+  var tree = document.getElementById( "skinsTree" );
+  var selectedSkinItem = tree.selectedItems[0];
+  var skinName = selectedSkinItem.getAttribute( "name" );
+  chromeRegistry.deselectSkin( skinName, DEBUG_USE_PROFILE );
+  chromeRegistry.refreshSkins();
+}
+  
+// XXX DEBUG ONLY. DO NOT LOCALIZE
+function installSkin()
+{
+  var themeURL = prompt( "Enter URL for a skin to install:","");
+  chromeRegistry.installSkin( themeURL, DEBUG_USE_PROFILE, false );
+}
+
+function themeSelect()
+{
+  var tree = document.getElementById("skinsTree");
+  var selectedItem = tree.selectedItems.length ? tree.selectedItems[0] : null;
+  if (selectedItem && selectedItem.getAttribute("skin") == "true") {
+    var themeName = selectedItem.getAttribute("displayName");
+    var nameField = document.getElementById("displayName");
+    nameField.setAttribute("value", themeName);
+    var author = document.getElementById("author");
+    author.setAttribute("value", selectedItem.getAttribute("author"));
+    var image = document.getElementById("previewImage");
+    image.setAttribute("src", selectedItem.getAttribute("image"));
+    var descText = document.createTextNode(selectedItem.getAttribute("description"));
+    var description = document.getElementById("description");
+    while (description.hasChildNodes())
+      description.removeChild(description.firstChild);
+    description.appendChild(descText);
+    var applyButton = document.getElementById("applySkin");
+    var applyLabel = bundle.GetStringFromName("applyThemePrefix");
+    applyLabel = applyLabel.replace(/%theme_name%/, themeName);
+    applyButton.value = applyLabel;
+  }
+}
+
+function restoreDefaults()
+{
+  var theSkinKids = document.getElementById("theSkinKids");
+  if (theSkinKids.hasChildNodes()) {
+    var defaultTheme = theSkinKids.firstChild;
     var tree = document.getElementById( "skinsTree" );
-    
-    var commonDialogs = nsJSComponentManager.getService("component://netscape/appshell/commonDialogs", 
-                                                        "nsICommonDialogs");
-   
     var selectedSkinItem = tree.selectedItems[0];
     var skinName = selectedSkinItem.getAttribute( "name" );
     chromeRegistry.selectSkin( skinName, DEBUG_USE_PROFILE ); 
     chromeRegistry.refreshSkins();
-  }  
- 
-function deselectSkin()
-  {
-    var tree = document.getElementById( "skinsTree" );
-    var selectedSkinItem = tree.selectedItems[0];
-    var skinName = selectedSkinItem.getAttribute( "name" );
-    chromeRegistry.deselectSkin( skinName, DEBUG_USE_PROFILE );
-    chromeRegistry.refreshSkins();
   }
+}
   
-// XXX DEBUG ONLY. DO NOT LOCALIZE
-function installSkin()
-  {
-    var themeURL = prompt( "Enter URL for a skin to install:","");
-    chromeRegistry.installSkin( themeURL, DEBUG_USE_PROFILE, false );
-  }
-
-function themeSelect()
-  {
-    var tree = document.getElementById("skinsTree");
-    var selectedItem = tree.selectedItems.length ? tree.selectedItems[0] : null;
-    if (selectedItem && selectedItem.getAttribute("skin") == "true") 
-      {
-        var displayName = document.getElementById("displayName");
-        displayName.setAttribute("value", selectedItem.getAttribute("displayName"));
-        var author = document.getElementById("author");
-        author.setAttribute("value", selectedItem.getAttribute("author"));
-        var email = document.getElementById("email");
-        var address = selectedItem.getAttribute("email");
-        if (address)
-          {
-            email.removeAttribute("disabled");
-            email.setAttribute("emailaddress", address);
-            email.setAttribute("tooltiptext", address);
-            email.setAttribute("emailtitle", selectedItem.getAttribute("displayName"));
-          }
-        else
-          email.setAttribute("disabled", "true");
-        var website = document.getElementById("website");
-        var url = selectedItem.getAttribute("website");
-        if (url)  
-          {
-            website.removeAttribute("disabled");
-            website.setAttribute("website", url);
-            website.setAttribute("tooltiptext", url);
-          }
-        else 
-          website.setAttribute("disabled", "true");
-        var image = document.getElementById("previewImage");
-        image.setAttribute("src", selectedItem.getAttribute("image"));
-      }
-  }
-  
-function gotoWebsite(aElement)
-  {
-    openTopWin(aElement.getAttribute("website"));
-  } 
-  
-function sendEmail(aElement)
-  {
-    window.openDialog( "chrome://messenger/content/messengercompose/messengercompose.xul", "_blank", 
-                     "chrome,all,dialog=no", "subject='" + aElement.getAttribute('displayName') + "',bodyislink=true");
-    
-  } 
-  
-var bundle = srGetStrBundle("chrome://communicator/locale/pref/prefutilities.properties");
   
