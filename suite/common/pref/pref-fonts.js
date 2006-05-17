45ee07f94fcdf702faa4e587a36caa9e69baad56
matt%netscape.com <matt%netscape.com>
2006-05-17 02:23:53 +0000

    Adding select lists for dynamic font menu Removing dynamic from the font pane. r=don 24830, 26077, 26176, 24753, 24754

--- suite/common/pref/pref-fonts.js
+++ suite/common/pref/pref-fonts.js
@@ -24,15 +24,15 @@ langs = [
 ];
 generics = [
   "serif",
-  "sans-serif",
-  "cursive",
-  "fantasy",
+ // "sans-serif",
+ // "cursive",
+ // "fantasy",
   "monospace"
 ];
 
 function startUp()
 {
- 
+    
 	selectLangs = document.getElementById("selectLangs");
 	for (i = 0; i < langs.length; i++) {
 		option = document.createElement("html:option");
@@ -40,48 +40,147 @@ function startUp()
 		option.appendChild(text);
 		selectLangs.appendChild(option);
 		option.setAttribute('value' , langs[i]);
- 
   } 
-
+    selectLangs.value = "x-western";
 	selectLang();
 }
 
 
 
+function get() {
+    dump('\n pref: ' + document.getElementById('serif').getAttribute('pref')  + '\n');
+    dump('\n pref: ' + document.getElementById('serif').getAttribute('preftype')  + '\n');
+    dump('\n element: ' + document.getElementById('serif').getAttribute('prefstring') + '\n');
+    dump('***');
+	dump('\n Size pref: ' + document.getElementById('size').getAttribute('pref')  + '\n');
+    dump('\n Size pref: ' + document.getElementById('size').getAttribute('preftype')  + '\n');
+    dump('\n Size element: ' + document.getElementById('size').getAttribute('prefstring') + '\n');
+    dump('\n Value element: ' + document.getElementById('size').value + '\n');
+  }
+
+  
+//Clears select list
+ function ClearList(list)
+ {
+   if (list) {
+     dump(list.length + " \n");
+     list.selectedIndex = -1;
+     for( j=list.length-1; j >= 0; j--) {
+       dump(list + " \n");
+       list.remove(j);
+	  }
+   }
+ }
+
+
+
+ function AppendStringToListByID(list, stringID) 
+ { 
+   AppendStringToList(list, editorShell.GetString(stringID)); 
+ } 
+
+ function AppendStringToList(list, string) 
+ { 
+  // THIS DOESN'T WORK! Result is a XULElement -- namespace problem 
+  // optionNode1 = document.createElement("option"); 
+  // This works - Thanks to Vidur! Params = name, value 
+   optionNode = new Option(string, string); 
+
+   if (optionNode) { 
+     list.add(optionNode, null); 
+   } else { 
+     dump("Failed to create OPTION node. String content="+string+"\n"); 
+   } 
+ } 
+
+
 function selectLang()
 	{
+    //get pref services
+	var pref = null;
+	try
+	{
+		pref = Components.classes["component://netscape/preferences"];
+		if (pref)	pref = pref.getService();
+		if (pref)	pref = pref.QueryInterface(Components.interfaces.nsIPref);
+	}
+	catch(ex)
+	{
+		dump("failed to get prefs service!\n");
+		pref = null;
+	}
 
-
+    //Getting variables needed
 	lang = document.getElementById("selectLangs").value;
-	var fontSizePrefint = 'font.size.variable.' + lang;
-	var fontSizePrefFontint = 'font.size.fixed.' + lang;
-	document.getElementById('size').setAttribute('prefstring', fontSizePrefint);	
-	document.getElementById('sizeFixed').setAttribute('prefstring', fontSizePrefFontint);	
+	//set prefstring of size
+	var fontVarPref = 'font.size.variable.' + lang;
+	var fontfixPref = 'font.size.fixed.' + lang;
+	var sizeVar =  document.getElementById('size');
+	var sizeFix =  document.getElementById('sizeFixed');
+	sizeVar.setAttribute('prefstring', fontVarPref);	
+	sizeFix.setAttribute('prefstring', fontfixPref);
+
+	//Get size from the preferences
+	var fixedFont = null;
+	try
+	{
+		if (pref)
+		{
+			var fontVarInt = pref.GetIntPref(fontVarPref);
+			var fontfixInt = pref.GetIntPref(fontfixPref);
+		}
+	}
+	catch(ex)
+	{
+	    //Default to 16 and 14 if no size is in the pref
+		fixedFont = null;
+		fontVarInt = "16"
+		fontfixInt = "14"
+	}
+
+	dump(fixedFont + "\n");
+
+
+		
 
 	for (i = 0; i < generics.length; i++)  {
 		var select = document.getElementById(generics[i]);
 		var selectParent = select.parentNode;
-		var selectNew = select.cloneNode(false);
+		//create name of font prefstring
 		var fontPrefstring = 'font.name.' + generics[i] + '.' + lang;
-        selectNew.setAttribute('prefstring', fontPrefstring);
-	//	dump('selectNew:' + selectNew.getAttribute('prefstring'));
-		fonts = enumerator.EnumerateFonts(lang, generics[i], fontCount);
-
+		select.setAttribute('prefstring', fontPrefstring);
+		//Clear the select list
+		ClearList(select);
 		
+		
+		fonts = enumerator.EnumerateFonts(lang, generics[i], fontCount);		
+		//Append to the select list.  Build the new select list
 		for (j = 0; j < fonts.length; j++) {
-			var option = document.createElement("html:option");
-			var text = document.createTextNode(fonts[j]);
-			option.appendChild(text);
-			option.setAttribute('value' , fonts[j]);
-			selectNew.appendChild(option);
-	//		dump(fonts[j] + " || ");
-
+		    AppendStringToList(select, fonts[j]) 
+		}
+        
+		//Get the pref value of the font
+		var fixedFont = null;
+		try
+		{
+			if (pref)
+			{
+				var selectVal = pref.CopyCharPref(fontPrefstring);
+			}
+		}
+		catch(ex)
+		{
+			selectVal = fonts[0];
 		}
 
-		selectParent.replaceChild(selectNew , select);
-       	//dump('select:' + document.getElementById(generics[i]).getAttribute('prefstring') + '\n');
+	    dump(fixedFont + "\n");
+		//select the value of the string
+		select.value = selectVal;
+		dump('select:' + select.getAttribute('prefstring') + "\n");
 		
 	}
+    //set the value of the sizes
+	sizeVar.value = fontVarInt;
+    sizeFix.value = fontfixInt;
 }
-
 
