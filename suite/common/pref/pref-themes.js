90218a3814bc5b0ecf4e17d23453ae055f580a99
alecf%netscape.com <alecf%netscape.com>
2006-05-17 02:29:25 +0000

    back out previous checkin - it's only going in on the branch

--- suite/common/pref/pref-themes.js
+++ suite/common/pref/pref-themes.js
@@ -26,6 +26,17 @@ function Startup()
     tree.selectItem(theSkinKids.firstChild);
 }
 
+function applySkin()
+{
+  var tree = document.getElementById( "skinsTree" );
+  var selectedSkinItem = tree.selectedItems[0];
+  var skinName = selectedSkinItem.getAttribute( "name" );
+  if (!chromeRegistry.isSkinSelected(skinName, DEBUG_USE_PROFILE)) {
+    chromeRegistry.selectSkin( skinName, DEBUG_USE_PROFILE );
+    chromeRegistry.refreshSkins();
+  }
+}
+
 function deselectSkin()
 {
   var tree = document.getElementById( "skinsTree" );
