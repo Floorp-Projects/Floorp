fb620b8ceca63972bed6aff1d3767c253f0cf9ed
brettw%gmail.com <brettw%gmail.com>
2006-05-19 16:25:40 +0000

    Bug 336230 r+a=vlad Bug in ASYNC_SYNC

--- storage/src/mozStorageAsyncIO.cpp
+++ storage/src/mozStorageAsyncIO.cpp
@@ -1308,8 +1308,7 @@ ProcessOneMessage(AsyncMessage* aMessage)
 
     case ASYNC_SYNC:
       NS_ASSERTION(pBase, "Must have base writer for writing");
-      NS_ASSERTION(sqliteOrigTruncate, "No seek pointer");
-      rc = sqliteOrigSeek(pBase, aMessage->mOffset);
+      rc = sqliteOrigSync(pBase, aMessage->mBytes);
       break;
 
     case ASYNC_TRUNCATE:
