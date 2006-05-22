425c3a44dc11173590d4d848dc5b52c00c22270b
allan%beaufour.dk <allan%beaufour.dk>
2006-05-22 14:47:02 +0000

    Validation chokes on empty xsd:date. Bug 326530, r=doronr

--- extensions/schema-validation/src/nsSchemaValidator.cpp
+++ extensions/schema-validation/src/nsSchemaValidator.cpp
@@ -2512,6 +2512,9 @@ nsSchemaValidator::IsValidSchemaDate(const nsAString & aNodeValue,
 
   LOG(("  Validating Date:"));
 
+  if (aNodeValue.IsEmpty())
+    return PR_FALSE;
+
   nsAutoString dateString(aNodeValue); 
   if (dateString.First() == '-') {
     aResult->isNegative = PR_TRUE;
