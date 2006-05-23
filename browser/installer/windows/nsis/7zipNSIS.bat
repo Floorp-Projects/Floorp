4f23af14f10d5f2f373ea5e2be077f4f5fe94792
rob_strong%exchangecode.com <rob_strong%exchangecode.com>
2006-05-23 12:02:59 +0000

    One last try to get NSIS building correctly for Bug 326580 - Firefox 2.0 Windows Installer

--- browser/installer/windows/nsis/7zipNSIS.bat
+++ browser/installer/windows/nsis/7zipNSIS.bat
@@ -1,3 +1,3 @@
-7z a -t7z app.7z setup.exe config ..\..\..\installer-stage\* -mx -m0=BCJ2 -m1=LZMA:d24 -m2=LZMA:d19 -m3=LZMA:d19  -mb0:1 -mb0s1:2 -mb0s2:3
+7z a -t7z app.7z setup.exe config "%1/*" -mx -m0=BCJ2 -m1=LZMA:d24 -m2=LZMA:d19 -m3=LZMA:d19  -mb0:1 -mb0s1:2 -mb0s2:3
 upx --best 7zSD.sfx
 copy /b 7zSD.sfx+app.tag+app.7z SetupGeneric.exe
