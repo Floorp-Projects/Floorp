b435ac8f5390299fa6bc0f2e5be2f1140f26626e
cst@andrew.cmu.edu <cst@andrew.cmu.edu>
2006-05-24 22:29:20 +0000

    Bug 327873 Make return-to-parent tab work for tabs opened by redirected target="_blank" links and certain things dragged to the tab strip r=jag sr=neil

--- xpfe/browser/resources/content/navigator.js
+++ xpfe/browser/resources/content/navigator.js
@@ -451,9 +451,8 @@ nsBrowserAccess.prototype = {
         return window.openDialog(getBrowserURL(), "_blank", "all,dialog=no",
                                  uri, null, referrer);
       case nsIBrowserDOMWindow.OPEN_NEWTAB:
-        var newTab = gBrowser.addTab("about:blank");
-        if (!pref.getBoolPref("browser.tabs.loadDivertedInBackground"))
-          gBrowser.selectedTab = newTab;
+        var newTab = gBrowser.addTab("about:blank", null, null,
+                                     !pref.getBoolPref("browser.tabs.loadDivertedInBackground"));
         var browser = gBrowser.getBrowserForTab(newTab);
         try {
           browser.loadURIWithFlags(uri, loadflags, referrer);
