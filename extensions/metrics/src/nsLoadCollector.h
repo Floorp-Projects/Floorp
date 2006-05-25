52f69657b5f717a484a3ec47d1864c4ed9d60459
bryner%brianryner.com <bryner%brianryner.com>
2006-05-25 20:05:06 +0000

    Add a document destroy event, and number documents so that it can be matched up with the load event (bug 335842) r=marria

--- extensions/metrics/src/nsLoadCollector.h
+++ extensions/metrics/src/nsLoadCollector.h
@@ -68,29 +68,51 @@
 #include "nsWeakReference.h"
 #include "nsDataHashtable.h"
 #include "nsAutoPtr.h"
+#include "nsIDocumentObserver.h"
+#include "nsPtrHashKey.h"
+
+class nsIDocument;
 
 class nsLoadCollector : public nsIMetricsCollector,
                         public nsIWebProgressListener,
+                        public nsIDocumentObserver,
                         public nsSupportsWeakReference
 {
  public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIMETRICSCOLLECTOR
   NS_DECL_NSIWEBPROGRESSLISTENER
+  NS_DECL_NSIDOCUMENTOBSERVER
 
   nsLoadCollector();
   nsresult Init();
 
  private:
-  ~nsLoadCollector();
-
   struct RequestEntry {
     nsCOMPtr<nsIWritablePropertyBag2> properties;
     PRTime startTime;
   };
 
+  struct DocumentEntry {
+    PRUint32 docID;
+    PRUint32 windowID;
+  };
+
+  ~nsLoadCollector();
+
+  // Callback for removing a document observer and map entry
+  static PLDHashOperator PR_CALLBACK
+  RemoveDocumentFromMap(const nsIDocument *document,
+                        DocumentEntry &entry, void *userData);
+
   // Hash table mapping nsIRequest objects to their event properties.
   nsDataHashtable<nsISupportsHashKey, RequestEntry> mRequestMap;
+
+  // Documents we're currently listening to, and their associated ids
+  nsDataHashtable< nsPtrHashKey<nsIDocument>, DocumentEntry > mDocumentMap;
+
+  // The next document id we'll assign
+  PRUint32 mNextDocID;
 };
 
 #define NS_LOADCOLLECTOR_CLASSNAME "Load Collector"
