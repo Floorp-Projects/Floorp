35cc3243539e8f0b72383d51e164d4752c610dbc
joshmoz@gmail.com <joshmoz@gmail.com>
2006-05-30 15:54:28 +0000

    add the Carbon icon decoder back, alongside the Cocoa icon decoder. Use Carbon decoder for Carbon builds, Cocoa for Cocoa builds. We can't use the Cocoa API from Carbon due to runtime conflicts (that aren't completely resolved by calling NSApplicationLoad). Also, removing call to NSApplicationLoad. b=339389 r=mento sr=stuart

--- widget/src/mac/nsAppShell.cpp
+++ widget/src/mac/nsAppShell.cpp
@@ -49,10 +49,6 @@
 
 #include <Carbon/Carbon.h>
 
-extern "C" {
-  int NSApplicationLoad();
-}
-
 enum {
   kEventClassMoz = 'MOZZ',
   kEventMozNull  = 0,
@@ -88,11 +84,6 @@ nsAppShell::~nsAppShell()
 nsresult
 nsAppShell::Init()
 {
-  // We call NSApplicationLoad() to initialize Cocoa. If we don't initialize Cocoa
-  // before we call into it (either through some mozilla component or a plugin)
-  // then we get bad app behavior like in bug 337334.
-  NSApplicationLoad();
-
   // The message pump is only used for its EventRecord dispatcher.  It is
   // used by the transitional WaitNextEvent handler.
 
