65cc318f4e711c2466a653d4dfb573a4e7034d5b
tony@ponderer.org <tony@ponderer.org>
2006-06-13 23:39:29 +0000

    bug 340603: getting table updates consumes large amounts of memory; interface for streaming updates r+a=darin

--- toolkit/components/build/nsToolkitCompsModule.cpp
+++ toolkit/components/build/nsToolkitCompsModule.cpp
@@ -56,6 +56,7 @@
 
 #ifdef MOZ_URL_CLASSIFIER
 #include "nsUrlClassifierDBService.h"
+#include "nsUrlClassifierStreamUpdater.h"
 #endif
 
 #ifdef MOZ_FEEDS
@@ -84,6 +85,7 @@ NS_GENERIC_FACTORY_CONSTRUCTOR(nsTypeAheadFind)
 #ifdef MOZ_URL_CLASSIFIER
 NS_GENERIC_FACTORY_SINGLETON_CONSTRUCTOR(nsUrlClassifierDBService,
                                          nsUrlClassifierDBService::GetInstance)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsUrlClassifierStreamUpdater)
 #endif
 
 #ifdef MOZ_FEEDS
@@ -132,6 +134,10 @@ static const nsModuleComponentInfo components[] =
     NS_URLCLASSIFIERDBSERVICE_CID,
     NS_URLCLASSIFIERDBSERVICE_CONTRACTID,
     nsUrlClassifierDBServiceConstructor },
+  { "Url Classifier Stream Updater",
+    NS_URLCLASSIFIERSTREAMUPDATER_CID,
+    NS_URLCLASSIFIERSTREAMUPDATER_CONTRACTID,
+    nsUrlClassifierStreamUpdaterConstructor },
 #endif
 #ifdef MOZ_FEEDS
   { "Unescape HTML",
