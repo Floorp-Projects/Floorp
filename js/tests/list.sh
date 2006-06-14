fc0292aa490fb3624c07d3bcc1f9a4ccb0f6a987
bclary%bclary.com <bclary%bclary.com>
2006-06-14 23:14:14 +0000

    JavaScript Test Library - miscellaneous fixes for test automation, no bug, not part of the build, a=bsmedberg for checkin to closed tree

--- js/tests/list.sh
+++ js/tests/list.sh
@@ -1,3 +1,5 @@
+trap 'echo Unexpected error: $?; exit 2' ERR
+
 if [[ -z $TEST_HTTP ]]; then
     export TEST_HTTP=test.mozilla.com
 fi
@@ -7,6 +9,9 @@ if [[ -z $1 ]]; then
 else
     dirs=$@
 fi
+
+currdir=`pwd`
+jsdir=`basename $currdir`
 
 find $dirs \
     -mindepth 2 -name '*.js' -print | \
@@ -20,15 +25,16 @@ find $dirs \
   if [[ -z $result ]]; then
       result=`echo $jsfile | sed 's/.*js\([0-9]\)_\([0-9]\).*/\1.\2/'`
       case $result in
-	  1.1) version=";version=1.1";;
-	  1.2) version=";version=1.2";;
-	  1.3) version=";version=1.3";;
-	  1.4) version=";version=1.4";;
+#	  1.1) version=";version=1.1";;
+#	  1.2) version=";version=1.2";;
+#	  1.3) version=";version=1.3";;
+#	  1.4) version=";version=1.4";;
 	  1.5) version=";version=1.5";;
 	  1.6) version=";version=1.6";;
+	  1.7) version=";version=1.7";;
 	  *) version="";;
       esac
       
-      echo "http://${TEST_HTTP}/tests/mozilla.org/js/js-test-driver-standards.html?test=$jsfile;language=type;text/javascript$version"
+      echo "http://${TEST_HTTP}/tests/mozilla.org/$jsdir/js-test-driver-standards.html?test=$jsfile;language=type;text/javascript$version"
   fi
 done
