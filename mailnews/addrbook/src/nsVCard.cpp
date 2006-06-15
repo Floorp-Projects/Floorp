a38acad4f06a0a440fffea300c78bfb647c68a86
bienvenu%nventure.com <bienvenu%nventure.com>
2006-06-15 15:24:47 +0000

    fix a base64 parsing problem in v-cards, r=sspitzer, sr=dveditz, 339740

--- mailnews/addrbook/src/nsVCard.cpp
+++ mailnews/addrbook/src/nsVCard.cpp
@@ -934,7 +934,7 @@ static char * lexGetDataFromBase64()
 		b = 62;
 	    else if (c == '/')
 		b = 63;
-	    else if (c == '=') {
+      else if (c == '=' && (quadIx == 2 || quadIx == 3)) {
 		b = 0;
 		pad++;
 	    } else if ((c == ' ') || (c == '\t')) {
@@ -989,6 +989,7 @@ static char * lexGetDataFromBase64()
 		}
 		trip = 0;
 		quadIx = 0;
+        pad = 0;
 		}
 	    }
 	} /* while */
