90ce32e3d24b973c7576c613e14ed63179af4be6
mhammond%skippinet.com.au <mhammond%skippinet.com.au>
2006-06-15 03:06:30 +0000

    Correct the change to the fast-load version.  r/sr=bz

--- content/xul/document/public/nsIXULPrototypeCache.h
+++ content/xul/document/public/nsIXULPrototypeCache.h
@@ -129,7 +129,7 @@ const char XUL_FASTLOAD_FILE_BASENAME[] = "XUL";
 // Increase the subtractor when changing version, say when changing the
 // (opaque to FastLoad code) format of JS script, function, regexp, etc.
 // XDR serializations.
-#define XUL_FASTLOAD_FILE_VERSION       (0xfeedbeef - 12)
+#define XUL_FASTLOAD_FILE_VERSION       (0xfeedbeef - 13)
 
 #define XUL_SERIALIZATION_BUFFER_SIZE   (64 * 1024)
 #define XUL_DESERIALIZATION_BUFFER_SIZE (8 * 1024)
