153505e51e31950d038e613de1a6369c8c26a885
mrbkap%gmail.com <mrbkap%gmail.com>
2006-06-20 18:56:03 +0000

    Hold a true weak reference to the location's docshell since the window won't always be able to clear the docshell out. bug 338288, r=jst sr=bzbarsky

--- dom/src/base/nsGlobalWindow.h
+++ dom/src/base/nsGlobalWindow.h
@@ -747,10 +747,7 @@ public:
   NS_DECL_ISUPPORTS
 
   void SetDocShell(nsIDocShell *aDocShell);
-  nsIDocShell *GetDocShell()
-  {
-    return mDocShell;
-  }
+  nsIDocShell *GetDocShell();
 
   // nsIDOMLocation
   NS_DECL_NSIDOMLOCATION
@@ -777,7 +774,7 @@ protected:
   nsresult CheckURL(nsIURI *url, nsIDocShellLoadInfo** aLoadInfo);
   nsresult FindUsableBaseURI(nsIURI * aBaseURI, nsIDocShell * aParent, nsIURI ** aUsableURI);
 
-  nsIDocShell *mDocShell; // Weak Reference
+  nsWeakPtr mDocShell;
 };
 
 /* factory function */
