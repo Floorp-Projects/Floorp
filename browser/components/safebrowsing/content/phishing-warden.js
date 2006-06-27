81c75c15880244b93c39d78b5828a70900614ee8
tony@ponderer.org <tony@ponderer.org>
2006-06-27 06:00:50 +0000

    Bug 341946: Safe browsing considers all jar: URI's safe patch: -jar, -javascript, +about r=mmchew,sr=bryner

--- browser/components/safebrowsing/content/phishing-warden.js
+++ browser/components/safebrowsing/content/phishing-warden.js
@@ -454,8 +454,7 @@ PROT_PhishingWarden.prototype.checkUrl_ = function(url, callback) {
     callback();
     return;
   }
-  if (!this.isSpurious_(url))
-    this.isEvilURL_(url, callback);
+  this.isEvilURL_(url, callback);
 }
 
 /**
@@ -491,21 +490,4 @@ PROT_PhishingWarden.prototype.checkRemoteData = function(callback,
   } else {
     G_Debug(this, "Remote blacklist miss");
   }
-}
-
-/**
- * Helper function to determine whether a given URL is "spurious" for some
- * definition of "spurious".
- *
- * @param url String containing the URL to check
- * 
- * @returns Boolean indicating whether Fritz thinks it's too boring to notice
- */ 
-PROT_PhishingWarden.prototype.isSpurious_ = function(url) {
-  return (url == "about:blank" ||
-          url == "about:config" ||  
-          url.startsWith("chrome://") ||
-          url.startsWith("file://") ||
-          url.startsWith("jar:") ||
-          url.startsWith("javascript:"));
 }
