dddd123d98d06e040fd7e3e339ad32a37eca755d
gavin@gavinsharp.com <gavin@gavinsharp.com>
2006-06-28 16:16:12 +0000

    Bug 342540: fix mistake that broke plugins that don't use explicit <Param>s (technorati search was broken), r=mconnor

--- browser/components/search/nsSearchService.js
+++ browser/components/search/nsSearchService.js
@@ -791,7 +791,7 @@ function EngineURL(aType, aMethod, aTemplate) {
     // Disable these for now, see bug 295018
     // case "file":
     // case "resource":
-      this.template = templateURI.spec;
+      this.template = aTemplate;
       break;
     default:
       ENSURE(false, "new EngineURL: template uses invalid scheme!",
