bdeb521557e31a2cb2a97a0c0260bd52dee19e8b
masayuki%d-toybox.com <masayuki%d-toybox.com>
2006-07-12 04:15:42 +0000

    Bug 312831 scroll wheel scrolls page after reaching end of a <select> box r=mats, sr=roc

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -815,6 +815,11 @@ pref("middlemouse.scrollbarPosition", false);
 // Clipboard behavior
 pref("clipboard.autocopy", false);
 
+// mouse wheel scroll transaction period of time (in milliseconds)
+pref("mousewheel.transaction.timeout", 3000);
+// mouse wheel scroll transaction is held even if the mouse cursor is moved.
+pref("mousewheel.transaction.ignoremovedelay", 500);
+
 // 0=lines, 1=pages, 2=history , 3=text size
 pref("mousewheel.withnokey.action",0);
 pref("mousewheel.withnokey.numlines",1);	
