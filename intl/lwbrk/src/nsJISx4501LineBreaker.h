ab8ee052c929a2c53036c9a28e7f76fd9bd8b744
masayuki%d-toybox.com <masayuki%d-toybox.com>
2006-07-13 20:25:46 +0000

    Bug 255990 Characters below U+0100 are not subject to line-breaking rules at all r=jshin+mrbkap sr=roc

--- intl/lwbrk/src/nsJISx4501LineBreaker.h
+++ intl/lwbrk/src/nsJISx4501LineBreaker.h
@@ -48,6 +48,9 @@ public:
   nsJISx4051LineBreaker();
   virtual ~nsJISx4051LineBreaker();
 
+  PRBool CanBreakBetweenLatin1(PRUnichar aChar1,
+                               PRUnichar aChar2);
+
   PRBool BreakInBetween( const PRUnichar* aText1 , PRUint32 aTextLen1,
                          const PRUnichar* aText2 , PRUint32 aTextLen2);
 
