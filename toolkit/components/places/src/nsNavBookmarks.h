b65f40c506e1b708a1ec762e55d0a57b03d6cfc3
benjamin%smedbergs.us <benjamin%smedbergs.us>
2006-07-18 15:00:46 +0000

    History-based bookmarks implementation for Places.  Bug 314553, r=brettw.
    This file was copied in CVS from the following location:
    mozilla/browser/components/places/src/nsNavBookmarks.h
    Original committer: bryner%brianryner.com
    Original revision: 1.1
    Original date: 2005/11/16 22:53:47

---
+++ toolkit/components/places/src/nsNavBookmarks.h
@@ -0,0 +1,123 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Places.
+ *
+ * The Initial Developer of the Original Code is
+ * Google Inc.
+ * Portions created by the Initial Developer are Copyright (C) 2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Brian Ryner <bryner@brianryner.com> (original author)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#include "nsINavBookmarksService.h"
+#include "nsNavHistory.h"
+#include "nsBrowserCompsCID.h"
+
+class nsNavBookmarks;
+
+class nsNavFolderResultNode : public nsNavHistoryResultNode
+{
+public:
+  nsNavFolderResultNode() :
+    nsNavHistoryResultNode(), mQueriedChildren(PR_FALSE) { }
+
+  NS_IMETHOD GetChildCount(PRInt32 *aChildCount);
+  NS_IMETHOD GetChild(PRInt32 aIndex, nsINavHistoryResultNode **aChild);
+
+private:
+  friend class nsNavBookmarks;
+  PRBool mQueriedChildren;
+};
+
+class nsNavBookmarks : public nsINavBookmarksService,
+                       public nsINavHistoryObserver
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSINAVBOOKMARKSSERVICE
+  NS_DECL_NSINAVHISTORYOBSERVER
+
+  nsNavBookmarks();
+  nsresult Init();
+
+  static nsNavBookmarks* GetBookmarksService() {
+    if (!sInstance) {
+      nsresult rv;
+      nsCOMPtr<nsINavBookmarksService> serv(do_GetService(NS_NAVBOOKMARKSSERVICE_CONTRACTID, &rv));
+      NS_ENSURE_SUCCESS(rv, nsnull);
+      NS_ASSERTION(sInstance, "Should have static instance pointer now");
+    }
+    return sInstance;
+  }
+
+private:
+  static nsNavBookmarks *sInstance;
+
+  ~nsNavBookmarks();
+
+  nsresult AdjustIndices(PRInt64 aFolder,
+                         PRInt32 aStartIndex, PRInt32 aEndIndex,
+                         PRInt32 aDelta);
+  nsresult ResultNodeForFolder(PRInt64 aFolder, const nsString &aTitle,
+                               nsNavHistoryResultNode **aNode);
+  nsresult FillFolderChildren(nsNavFolderResultNode *aNode);
+  PRInt32 FolderCount(PRInt64 aFolder);
+
+  nsNavHistory* History() { return nsNavHistory::GetHistoryService(); }
+
+  mozIStorageStatement* DBGetURLPageInfo()
+  { return History()->DBGetURLPageInfo(); }
+
+  mozIStorageConnection* DBConn() { return History()->GetStorageConnection(); }
+
+  nsCOMArray<nsINavBookmarkObserver> mObservers;
+  PRInt64 mRoot;
+
+  nsCOMPtr<mozIStorageStatement> mDBGetFolderInfo;    // kGetFolderInfoIndex_* results
+  static const PRInt32 kGetFolderInfoIndex_FolderID;
+  static const PRInt32 kGetFolderInfoIndex_Title;
+
+  nsCOMPtr<mozIStorageStatement> mDBGetChildren;       // kGetInfoIndex_* results + kGetChildrenIndex_* results
+
+  static const PRInt32 kGetChildrenIndex_Position;
+  static const PRInt32 kGetChildrenIndex_ItemChild;
+  static const PRInt32 kGetChildrenIndex_FolderChild;
+  static const PRInt32 kGetChildrenIndex_FolderTitle;
+
+  nsCOMPtr<mozIStorageStatement> mDBFindURIBookmarks;  // kFindBookmarksIndex_* results
+  static const PRInt32 kFindBookmarksIndex_ItemChild;
+  static const PRInt32 kFindBookmarksIndex_FolderChild;
+  static const PRInt32 kFindBookmarksIndex_Parent;
+  static const PRInt32 kFindBookmarksIndex_Position;
+
+  nsCOMPtr<mozIStorageStatement> mDBFolderCount;
+
+  friend class nsNavFolderResultNode;
+};
