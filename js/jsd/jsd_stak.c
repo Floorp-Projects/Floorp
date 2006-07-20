3ba02d2f8f1c7f8d46d586f411a0bc3d68f7d22b
silver%warwickcompsoc.co.uk <silver%warwickcompsoc.co.uk>
2006-07-20 15:25:32 +0000

    Bug 343511 - Don't assert more than necessary. r=rginda

--- js/jsd/jsd_stak.c
+++ js/jsd/jsd_stak.c
@@ -172,7 +172,7 @@ jsd_DestroyThreadState(JSDContext* jsdc, JSDThreadState* jsdthreadstate)
     JSDStackFrameInfo* jsdframe;
     JSCList* list;
 
-    JSD_ASSERT_VALID_THREAD_STATE(jsdthreadstate);
+    JS_ASSERT(jsdthreadstate);
     JS_ASSERT(JSD_CURRENT_THREAD() == jsdthreadstate->thread);
 
     JSD_LOCK_THREADSTATES(jsdc);
