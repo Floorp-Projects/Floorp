ed173147f2f4015d245a6cc41fbdc5f02750cd5c
vladimir@pobox.com <vladimir@pobox.com>
2006-07-20 23:28:23 +0000

    cairo-on-mac compilation fixes, r=stuart

--- gfx/src/thebes/nsThebesRenderingContext.cpp
+++ gfx/src/thebes/nsThebesRenderingContext.cpp
@@ -1223,7 +1223,7 @@ nsThebesRenderingContext::GetTextDimensionsInternal(const PRUnichar* aString,
   return GetWidth(aString, aLength, aDimensions.width, aFontID);
 }
 
-#if defined(_WIN32) || defined(XP_OS2) || defined(MOZ_X11) || defined(XP_BEOS)
+#if defined(_WIN32) || defined(XP_OS2) || defined(MOZ_X11) || defined(XP_BEOS) || defined(XP_MACOSX)
 NS_IMETHODIMP
 nsThebesRenderingContext::GetTextDimensionsInternal(const char*       aString,
                                                     PRInt32           aLength,
