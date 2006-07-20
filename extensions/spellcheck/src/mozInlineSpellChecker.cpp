05bffd1f79ed8a35c89c038c3409e7dba5212a04
brettw@gmail.com <brettw@gmail.com>
2006-07-20 00:15:07 +0000

    Bug 345054 r+sr=roc Don't flush pending notifications in the spellchecker

--- extensions/spellcheck/src/mozInlineSpellChecker.cpp
+++ extensions/spellcheck/src/mozInlineSpellChecker.cpp
@@ -313,7 +313,6 @@ mozInlineSpellChecker::SpellCheckAfterEditorChange(
   rv = wordUtil.Init(mEditor);
   if (NS_FAILED(rv))
     return NS_OK; // editor doesn't like us
-  wordUtil.GetDocument()->FlushPendingNotifications(Flush_Style);
 
   nsCOMPtr<nsIDOMRange> wordRange;
   rv = wordUtil.GetRangeForWord(anchorNode, anchorOffset,
