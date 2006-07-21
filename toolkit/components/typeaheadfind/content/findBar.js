a9a9e9bfa28711d7c26891826797e0f03e28567a
pamg.bugs@gmail.com <pamg.bugs@gmail.com>
2006-07-21 00:54:59 +0000

    Bug 345379: Case-sensitivity in the find bar is broken.  Patch by pkasting@google.com. r=beng

--- toolkit/components/typeaheadfind/content/findBar.js
+++ toolkit/components/typeaheadfind/content/findBar.js
@@ -638,7 +638,7 @@ var gFindBar = {
     if (this.mTypeAheadCaseSensitive == 1)
       return true;
 
-    return (str != str.toLowerCase() && str != str.toUpperCase());
+    return (str != str.toLowerCase());
   },
 
   onFindBarFocus: function ()
