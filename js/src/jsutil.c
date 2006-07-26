ded4c15946d297439dd4f356b55d4c9d4bde00a2
bclary@bclary.com <bclary@bclary.com>
2006-07-26 02:38:58 +0000

    Force JS_Assert to exit with code 3 on Windows, debug only, r=mrbkap,sr=brendan, bug 345118

--- js/src/jsutil.c
+++ js/src/jsutil.c
@@ -56,6 +56,7 @@ JS_PUBLIC_API(void) JS_Assert(const char *s, const char *file, JSIntn ln)
     fprintf(stderr, "Assertion failure: %s, at %s:%d\n", s, file, ln);
 #if defined(WIN32)
     DebugBreak();
+    exit(3);
 #elif defined(XP_OS2) || (defined(__GNUC__) && defined(__i386))
     asm("int $3");
 #endif
