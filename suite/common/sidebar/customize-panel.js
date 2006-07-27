92d581dd76d0aaba69545789a40a5b0c24a6fca4
slamm%netscape.com <slamm%netscape.com>
2006-07-27 14:48:58 +0000

    Resurrect this to make simple chromeless window.

--- suite/common/sidebar/customize-panel.js
+++ suite/common/sidebar/customize-panel.js
@@ -21,10 +21,7 @@
 
 function Init()
 {
-  var panel_name_div  = document.getElementById('panelname');
-  var customize_frame = document.getElementById('customizeframe');
-  var panel_text      = document.createTextNode(panel_name)
-  panel_name_div.appendChild(panel_text);
-  // The customize page currently is not loading because of a redirect bug.
-  customize_frame.setAttribute('src', panel_customize_URL);
+  var customize_url = window.arguments[0];
+  var customize_frame = document.getElementById('customize_frame');
+  customize_frame.setAttribute('src', customize_url);
 }
