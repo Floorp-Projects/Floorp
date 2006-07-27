fb47207c889b94b00e17821f5e2e4b7189fec42f
slamm%netscape.com <slamm%netscape.com>
2006-07-27 14:51:49 +0000

    oops, use MPL instead of NPL (50594). r=mcafee. Last changes were r=mcafee too.

--- suite/common/related/related-panel.js
+++ suite/common/related/related-panel.js
@@ -1,9 +1,9 @@
 /* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- * The contents of this file are subject to the Netscape Public
+ * The contents of this file are subject to the Mozilla Public
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/NPL/
+ * the License at http://www.mozilla.org/MPL/
  *
  * Software distributed under the License is distributed on an "AS
  * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
