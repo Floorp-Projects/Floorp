6f60d1e57dce4e32522c97eab9330ec445cca0a7
morse%netscape.com <morse%netscape.com>
2006-07-27 14:49:12 +0000

    new file, not yet part of build

---
+++ suite/common/permissions/permissionsOverlay.js
@@ -0,0 +1,33 @@
+/* -*- Mode: Java; tab-width: 4; c-basic-offset: 4; -*-
+ * 
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ *
+ * Contributor(s):
+ */
+
+function viewImages() {
+  window.openDialog("chrome://communicator/content/wallet/CookieViewer.xul","",
+                    "modal=yes,chrome,resizable=yes", 2 );
+}
+
+function viewCookies() {
+  window.openDialog("chrome://communicator/content/wallet/CookieViewer.xul","","modal=yes,chrome,resizable=yes", 0);
+}  
+
+function viewTutorial() {
+  window.openDialog
+    ("chrome://communicator/content/wallet/privacy.xul","","modal=yes,chrome,resizable=yes,height=400,width=600", 0);
+}  
