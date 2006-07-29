34f8dbe632eab35dd636917f9017fb77b041c69b
mnyromyr%tprac.de <mnyromyr%tprac.de>
2006-07-29 08:48:31 +0000

    Bug 229211: Remove browser.related.enabled; p=benc@meer.net/Frank Wein <bugzilla@mcsmurf.de>, r/sr=neil

--- suite/browser/browser-prefs.js
+++ suite/browser/browser-prefs.js
@@ -139,7 +139,6 @@ pref("browser.translation.service", "chrome://navigator-region/locale/region.pro
 pref("browser.translation.serviceDomain", "chrome://navigator-region/locale/region.properties");
   
 // Smart Browsing prefs
-pref("browser.related.enabled", true);
 pref("browser.related.autoload", 1);  // 0 = Always, 1 = After first use, 2 = Never
 pref("browser.related.provider", "http://www-rl.netscape.com/wtgn?");
 pref("browser.related.disabledForDomains", "");
