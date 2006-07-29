abe627512e1a16342316a1b28fbb2d689bdb017d
blakeross%telocity.com <blakeross%telocity.com>
2006-07-29 05:36:59 +0000

    Links dragged from content area to personal toolbar have no title (84976). r=kerz sr=ben a=asa

--- suite/common/contentAreaDD.js
+++ suite/common/contentAreaDD.js
@@ -144,8 +144,13 @@ var contentAreaDNDObserver = {
       aXferData.data = new TransferData();
       aXferData.data.addDataForFlavour("text/unicode", isAnchor ? urlstring :  titlestring);
       aXferData.data.addDataForFlavour("text/html", htmlstring);
-      if (urlstring)
+      if (urlstring) {
         aXferData.data.addDataForFlavour("text/x-moz-url", urlstring + "\n" + titlestring);
+        aXferData.data.addDataForFlavour("moz/rdfitem", urlstring + "\n" + titlestring);
+      }
+      else {
+        aXferData.data.addDataForFlavour("moz/rdfitem", titlestring);
+      }
       // we use the url for text/unicode data if an anchor is being dragged, rather than
       // the title text of the link or the alt text for an anchor image. 
     },
