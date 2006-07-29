da635f18d570ac435e6f63652589fc41966bdefb
bugzilla%arlen.demon.co.uk <bugzilla%arlen.demon.co.uk>
2006-07-29 05:44:03 +0000

    Bug 311028 Print Preview window is partly overlayed by background and is missing scrollbars p=me r=neil.parkwaycc.co.uk sr=bienvenu

--- suite/common/printing.js
+++ suite/common/printing.js
@@ -38,123 +38,273 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-var gPrintSettings = null;
 var gPrintSettingsAreGlobal = false;
 var gSavePrintSettings = false;
 
-function setPrinterDefaultsForSelectedPrinter(aPrintService)
-{
-  if (gPrintSettings.printerName == "") {
-    gPrintSettings.printerName = aPrintService.defaultPrinterName;
-  }
+var PrintUtils = {
 
-  // First get any defaults from the printer 
-  aPrintService.initPrintSettingsFromPrinter(gPrintSettings.printerName, gPrintSettings);
-
-  // now augment them with any values from last time
-  aPrintService.initPrintSettingsFromPrefs(gPrintSettings, true, gPrintSettings.kInitSaveAll);
-}
-
-function GetPrintSettings()
-{
-  try {
-    if (gPrintSettings == null) {
-      var pref = Components.classes["@mozilla.org/preferences-service;1"]
-                           .getService(Components.interfaces.nsIPrefBranch);
-      if (pref) {
-        gPrintSettingsAreGlobal = pref.getBoolPref("print.use_global_printsettings", false);
-        gSavePrintSettings = pref.getBoolPref("print.save_print_settings", false);
-      }
-
-      var printService = Components.classes["@mozilla.org/gfx/printsettings-service;1"]
-                                        .getService(Components.interfaces.nsIPrintSettingsService);
-      if (gPrintSettingsAreGlobal) {
-        gPrintSettings = printService.globalPrintSettings;
-        setPrinterDefaultsForSelectedPrinter(printService);
-      } else {
-        gPrintSettings = printService.newPrintSettings;
-      }
-    }
-  } catch (e) {
-    dump("GetPrintSettings() "+e+"\n");
-  }
-
-  return gPrintSettings;
-}
-
-function goPageSetup(domwin, printSettings)
-{
-  try {
-    if (printSettings == null) {
-      dump("***************** PrintSettings arg is null!");
+  showPageSetup: function ()
+  {
+    try {
+      var printSettings = this.getPrintSettings();
+      var printingPromptService = Components.classes["@mozilla.org/embedcomp/printingprompt-service;1"]
+                                            .getService(Components.interfaces.nsIPrintingPromptService);
+      printingPromptService.showPageSetup(window, printSettings, null);
+    } catch (e) {
+      dump("showPageSetup "+e+"\n");
       return false;
     }
+    return true;
+  },
 
-    // This code calls the printoptions service to bring up the printoptions
-    // dialog.  This will be an xp dialog if the platform did not override
-    // the ShowPrintSetupDialog method.
+  print: function ()
+  {
+    var webBrowserPrint = this.getWebBrowserPrint();
+    var printSettings = this.getPrintSettings();
+    try {
+      webBrowserPrint.print(printSettings, null);
+      if (gPrintSettingsAreGlobal && gSavePrintSettings) {
+        var printService = Components.classes["@mozilla.org/gfx/printsettings-service;1"]
+                                     .getService(Components.interfaces.nsIPrintSettingsService);
+        printService.savePrintSettingsToPrefs(printSettings, true, 
+                                              printSettings.kInitSaveAll);
+        printService.savePrintSettingsToPrefs(printSettings, false,
+                                              printSettings.kInitSavePrinterName);
+      }
+    } catch (e) {
+      // Pressing cancel is expressed as an NS_ERROR_ABORT return value,
+      // causing an exception to be thrown which we catch here.
+      // Unfortunately this will also consume helpful failures, so add a
+      // dump("print: "+e+"\n"); // if you need to debug
+    }
+  },
+
+  printPreview: function (aEnterPPCallback, aExitPPCallback)
+  {
+    // if we're already in PP mode, don't set the callbacks; chances
+    // are they're null because someone is calling printPreview() to
+    // get us to refresh the display.
+    var pptoolbar = document.getElementById("print-preview-toolbar");
+    if (!pptoolbar) {
+      this._onEnterPP = aEnterPPCallback;
+      this._onExitPP  = aExitPPCallback;
+    } else {
+      // collapse the browser - it will be shown in
+      // onEnterPrintPreview; this forces a reflow which fixes display
+      // issues in bug 267422.
+      var browser = getPPBrowser();
+      if (browser)
+        browser.collapsed = true;
+    }
+
+    this._webProgressPP = {};
+    var ppParams        = {};
+    var notifyOnOpen    = {};
+    var webBrowserPrint = this.getWebBrowserPrint();
+    var printSettings   = this.getPrintSettings();
+    // Here we get the PrintingPromptService so we can display the PP Progress from script
+    // For the browser implemented via XUL with the PP toolbar we cannot let it be
+    // automatically opened from the print engine because the XUL scrollbars in the PP window
+    // will layout before the content window and a crash will occur.
+    // Doing it all from script, means it lays out before hand and we can let printing do it's own thing
     var printingPromptService = Components.classes["@mozilla.org/embedcomp/printingprompt-service;1"]
-                                             .getService(Components.interfaces.nsIPrintingPromptService);
-    printingPromptService.showPageSetup(domwin, printSettings, null);
-  } catch(e) {
-    // Note: Pressing Cancel gets here too.
+                                          .getService(Components.interfaces.nsIPrintingPromptService);
+    // just in case we are already printing, 
+    // an error code could be returned if the Progress Dialog is already displayed
+    try {
+      printingPromptService.showProgress(this, webBrowserPrint, printSettings, this._obsPP, false,
+                                         this._webProgressPP, ppParams, notifyOnOpen);
+      if (ppParams.value) {
+        var webNav = getWebNavigation();
+        ppParams.value.docTitle = webNav.document.title;
+        ppParams.value.docURL   = webNav.currentURI.spec;
+      }
 
-    return false; 
+      // this tells us whether we should continue on with PP or 
+      // wait for the callback via the observer
+      if (!notifyOnOpen.value.valueOf() || this._webProgressPP.value == null)
+        this.enterPrintPreview();
+    } catch (e) {
+      this.enterPrintPreview();
+    }
+  },
+
+  getWebBrowserPrint: function ()
+  {
+    return getEngineWebBrowserPrint();
+  },
+
+  ////////////////////////////////////////
+  // "private" methods. Don't use them. //
+  ////////////////////////////////////////
+
+  setPrinterDefaultsForSelectedPrinter: function (aPrintService, aPrintSettings)
+  {
+    if (!aPrintSettings.printerName)
+      aPrintSettings.printerName = aPrintService.defaultPrinterName;
+
+    // First get any defaults from the printer 
+    aPrintService.initPrintSettingsFromPrinter(aPrintSettings.printerName, aPrintSettings);
+    // now augment them with any values from last time
+    aPrintService.initPrintSettingsFromPrefs(aPrintSettings, true, aPrintSettings.kInitSaveAll);
+  },
+
+  getPrintSettings: function ()
+  {
+    var pref = Components.classes["@mozilla.org/preferences-service;1"]
+                         .getService(Components.interfaces.nsIPrefBranch);
+    if (pref) {
+      gPrintSettingsAreGlobal = pref.getBoolPref("print.use_global_printsettings", false);
+      gSavePrintSettings = pref.getBoolPref("print.save_print_settings", false);
+    }
+
+    var printSettings;
+    try {
+      var printService = Components.classes["@mozilla.org/gfx/printsettings-service;1"]
+                                   .getService(Components.interfaces.nsIPrintSettingsService);
+      if (gPrintSettingsAreGlobal) {
+        printSettings = printService.globalPrintSettings;
+        this.setPrinterDefaultsForSelectedPrinter(printService, printSettings);
+      } else {
+        printSettings = printService.newPrintSettings;
+      }
+    } catch (e) {
+      dump("getPrintSettings: "+e+"\n");
+    }
+    return printSettings;
+  },
+
+  _chromeState: {},
+  _closeHandlerPP: null,
+  _webProgressPP: null,
+  _onEnterPP: null,
+  _onExitPP: null,
+
+  // This observer is called once the progress dialog has been "opened"
+  _obsPP: 
+  {
+    observe: function(aSubject, aTopic, aData)
+    {
+      // delay the print preview to show the content of the progress dialog
+      setTimeout(function () { PrintUtils.enterPrintPreview(); }, 0);
+    },
+
+    QueryInterface : function(iid)
+    {
+      if (iid.equals(Components.interfaces.nsIObserver) ||
+          iid.equals(Components.interfaces.nsISupportsWeakReference) ||
+          iid.equals(Components.interfaces.nsISupports))
+        return this;   
+      throw Components.results.NS_NOINTERFACE;
+    }
+  },
+
+  enterPrintPreview: function ()
+  {
+    var webBrowserPrint = this.getWebBrowserPrint();
+    var printSettings   = this.getPrintSettings();
+    try {
+      webBrowserPrint.printPreview(printSettings, null, this._webProgressPP.value);
+    } catch (e) {
+      // Pressing cancel is expressed as an NS_ERROR_ABORT return value,
+      // causing an exception to be thrown which we catch here.
+      // Unfortunately this will also consume helpful failures, so add a
+      // dump(e); // if you need to debug
+      return;
+    }
+
+    var printPreviewTB = document.getElementById("print-preview-toolbar");
+    if (printPreviewTB) {
+      printPreviewTB.updateToolbar();
+      var browser = getPPBrowser();
+      if (browser)
+        browser.collapsed = false;
+      return;
+    }
+
+    // show the toolbar after we go into print preview mode so
+    // that we can initialize the toolbar with total num pages
+    const XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+    printPreviewTB = document.createElementNS(XUL_NS, "toolbar");
+    printPreviewTB.setAttribute("printpreview", true);
+    printPreviewTB.setAttribute("id", "print-preview-toolbar");
+
+    var navToolbox = getNavToolbox();
+    navToolbox.parentNode.insertBefore(printPreviewTB, navToolbox);
+
+    // copy the window close handler
+    if (document.documentElement.hasAttribute("onclose"))
+      this._closeHandlerPP = document.documentElement.getAttribute("onclose");
+    else
+      this._closeHandlerPP = null;
+    document.documentElement.setAttribute("onclose", "PrintUtils.exitPrintPreview(); return false;");
+
+    // disable chrome shortcuts...
+    window.addEventListener("keypress", this.onKeyPressPP, true);
+ 
+    // _content.focus();
+    navToolbox.focus();
+
+    // on Enter PP Call back
+    if (this._onEnterPP) {
+      this._onEnterPP();
+      this._onEnterPP = null;
+    }
+  },
+
+  exitPrintPreview: function ()
+  {
+    window.removeEventListener("keypress", this.onKeyPressPP, true);
+
+    // restore the old close handler
+    document.documentElement.setAttribute("onclose", this._closeHandlerPP);
+    this._closeHandlerPP = null;
+
+    var webBrowserPrint = this.getWebBrowserPrint();
+    webBrowserPrint.exitPrintPreview(); 
+
+    // remove the print preview toolbar
+    var navToolbox = getNavToolbox();
+    var printPreviewTB = document.getElementById("print-preview-toolbar");
+    navToolbox.parentNode.removeChild(printPreviewTB);
+
+    // _content.focus();
+    navToolbox.focus();
+
+    // on Exit PP Call back
+    if (this._onExitPP) {
+      this._onExitPP();
+      this._onExitPP = null;
+    }
+  },
+
+  onKeyPressPP: function (aEvent)
+  {
+    var closeKey;
+    try {
+      closeKey = document.getElementById("key_close")
+                         .getAttribute("key");
+      closeKey = aEvent["DOM_VK_"+closeKey];
+    } catch (e) {}
+    var isModif = aEvent.ctrlKey || aEvent.metaKey;
+    // ESC and Ctrl-W exits the PP
+    if (aEvent.keyCode == aEvent.DOM_VK_ESCAPE || isModif &&
+        (aEvent.charCode == closeKey || aEvent.charCode == closeKey + 32))
+      PrintUtils.exitPrintPreview();
+    // cancel shortkeys
+    if (isModif) {
+      aEvent.preventDefault();
+      aEvent.stopPropagation();
+    }
   }
-  return true;
 }
 
 function NSPrintSetup()
 {
-  var didOK = false;
-  try {
-    gPrintSettings = GetPrintSettings();
-
-    var webBrowserPrint = null;
-    if (content) {
-      webBrowserPrint = content
-        .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-        .getInterface(Components.interfaces.nsIWebBrowserPrint);
-    }
-
-    didOK = goPageSetup(window, gPrintSettings);
-    if (didOK) {
-      if (webBrowserPrint) {
-        if (gPrintSettingsAreGlobal && gSavePrintSettings) {
-          var psService = Components.classes["@mozilla.org/gfx/printsettings-service;1"]
-                                            .getService(Components.interfaces.nsIPrintSettingsService);
-          psService.savePrintSettingsToPrefs(gPrintSettings, false, gPrintSettings.kInitSaveNativeData);
-        }
-      }
-    }
-  } catch (e) {
-    dump("NSPrintSetup() " + e + "\n");
-  }
-  return didOK;
+  PrintUtils.showPageSetup();
 }
 
 function NSPrint()
 {
-  try {
-    var webBrowserPrint = content
-          .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-          .getInterface(Components.interfaces.nsIWebBrowserPrint);
-    if (webBrowserPrint) {
-      gPrintSettings = GetPrintSettings();
-      webBrowserPrint.print(gPrintSettings, null);
-      if (gPrintSettingsAreGlobal && gSavePrintSettings) {
-        var psService = Components.classes["@mozilla.org/gfx/printsettings-service;1"]
-                                          .getService(Components.interfaces.nsIPrintSettingsService);
-        psService.savePrintSettingsToPrefs(gPrintSettings, true,
-                                           gPrintSettings.kInitSaveAll);
-        psService.savePrintSettingsToPrefs(gPrintSettings, false,
-                                           gPrintSettings.kInitSavePrinterName);
-      }
-    }
-  } catch (e) {
-    // Pressing cancel is expressed as an NS_ERROR_ABORT return value,
-    // causing an exception to be thrown which we catch here.
-    // Unfortunately this will also consume helpful failures, so add a
-    // dump(e); // if you need to debug
-  }
+  PrintUtils.print();
 }
