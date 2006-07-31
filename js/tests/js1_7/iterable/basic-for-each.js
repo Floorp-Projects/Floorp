518b61e43c8b02f5987a630d46550fcb122a1e54
bclary%bclary.com <bclary%bclary.com>
2006-07-31 08:09:10 +0000

    JavaScript Test Library - check that everything was iterated, by Jeff Walden

--- js/tests/js1_7/iterable/basic-for-each.js
+++ js/tests/js1_7/iterable/basic-for-each.js
@@ -35,7 +35,7 @@
  *
  * ***** END LICENSE BLOCK ***** */
 //-----------------------------------------------------------------------------
-var bug     = "(none)";
+var bug     = "346582";
 var summary = "Basic support for iterable objects and for-each";
 var actual, expect;
 
@@ -88,6 +88,9 @@ try
       throw "for-each iteration failed on vals[\"" + index + "\"]";
     index++;
   }
+  if (index != keys.length)
+    throw "not everything iterated!  index=" + index +
+          ", keys.length=" + keys.length;
 
   if (iterable.persistedProp != 17)
     throw "iterable.persistedProp not persisted!";
