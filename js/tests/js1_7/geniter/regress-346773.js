75940be05ab82177f656d10a7267c31887fb98f9
bclary%bclary.com <bclary%bclary.com>
2006-08-04 18:32:45 +0000

    JavaScript Test Library - correct test to prevent syntax error through eval, bug 346773

--- js/tests/js1_7/geniter/regress-346773.js
+++ js/tests/js1_7/geniter/regress-346773.js
@@ -50,24 +50,32 @@ function test()
   enterFunc ('test');
   printBugNumber (bug);
   printStatus (summary);
-  
-  var it = {foo:"5"};
-  it.__iterator__ =
-    function(valsOnly)
-    {
-      var gen =
-        function()
-        {
-          for (var i = 0; i < keys.length; i++)
-          {
-            if (valsOnly)
-              yield vals[i];
-            else
-              yield [keys[i], vals[i]];
-          }
-          return gen();
-        }
-    }
+
+  try
+  {
+    var src = 
+      '    var it = {foo:"5"};' + 
+      '    it.__iterator__ =' + 
+      '      function(valsOnly)' + 
+      '      {' + 
+      '        var gen =' + 
+      '        function()' + 
+      '        {' + 
+      '          for (var i = 0; i < keys.length; i++)' + 
+      '          {' + 
+      '            if (valsOnly)' + 
+      '              yield vals[i];' + 
+      '            else' + 
+      '              yield [keys[i], vals[i]];' + 
+      '          }' + 
+      '          return gen();' + 
+      '        }' + 
+      '      }';
+    eval(src);
+  }
+  catch(ex)
+  {
+  }
 
   reportCompare(expect, actual, summary);
 
