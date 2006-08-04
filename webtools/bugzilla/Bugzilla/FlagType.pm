3400c6d894ddd130ec193accabf92e855dc2235b
lpsolit%gmail.com <lpsolit%gmail.com>
2006-08-04 14:09:26 +0000

    Bug 347360: Incorrect field name in FlagType.pm - Patch by Fr�d�ric Buclin <LpSolit@gmail.com> a=myk

--- webtools/bugzilla/Bugzilla/FlagType.pm
+++ webtools/bugzilla/Bugzilla/FlagType.pm
@@ -235,8 +235,8 @@ sub request_group {
 sub flag_count {
     my $self = shift;
 
-    if (!defined $self->{'bug_count'}) {
-        $self->{'bug_count'} =
+    if (!defined $self->{'flag_count'}) {
+        $self->{'flag_count'} =
             Bugzilla->dbh->selectrow_array('SELECT COUNT(*) FROM flags
                                             WHERE type_id = ?', undef, $self->{'id'});
     }
