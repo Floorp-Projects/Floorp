b288728ac8f7f05fcc303b4825b7ea7fe0b59af8
bugzilla%standard8.demon.co.uk <bugzilla%standard8.demon.co.uk>
2006-08-05 19:41:14 +0000

    Follow up to Bug 282669 First ("To:") address field not focused in new mail when last mail sent with first address field in focus - TB correction to use awGetInputElement(1) instead of top.awInputElement. Patch by Wayne Woods <w.woods@optusnet.com.au>,r=bienvenu,sr=mscott

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -232,8 +232,7 @@ var gComposeRecyclingListener = {
     ReleaseGlobalVariables();
 
     // Clear the focus
-    if (top.awInputElement.getAttribute('focused') != '')
-      top.awInputElement.removeAttribute('focused');
+    awGetInputElement(1).removeAttribute('focused');
 
     //Reset Boxes size    
     document.getElementById("headers-box").removeAttribute("height");
