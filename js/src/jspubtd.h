72e2197d21aae711c13d34946f128a58bb3232c2
brendan%mozilla.org <brendan%mozilla.org>
2006-08-11 23:26:18 +0000

    Revise try/catch/finally AST to pave way for block scope and destructuring (336379, r=mrbkap).

--- js/src/jspubtd.h
+++ js/src/jspubtd.h
@@ -595,9 +595,14 @@ typedef enum JSExnType {
 } JSExnType;
 
 typedef struct JSErrorFormatString {
-    const char *format;  /* the error message (may be UTF-8 if compiled with JS_C_STRINGS_ARE_UTF8) */
-    uint16 argCount;     /* the number of arguments to convert in the error message */
-    int16 exnType;       /* One of the JSExnType constants above */
+    /* The error format string (UTF-8 if JS_C_STRINGS_ARE_UTF8 is defined). */
+    const char *format;
+
+    /* The number of arguments to expand in the formatted error message. */
+    uint16 argCount;
+
+    /* One of the JSExnType constants above. */
+    int16 exnType;
 } JSErrorFormatString;
 
 typedef const JSErrorFormatString *
