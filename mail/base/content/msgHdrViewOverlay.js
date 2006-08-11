f996e6bb3d62ea6b621f12c050d7cd75a9257cfe
gavin%gavinsharp.com <gavin%gavinsharp.com>
2006-08-11 20:51:45 +0000

    Bug 251279: No tooltip for "From:" in collapsed/brief header if sender isn't in Address Book, patch by Ziga Sancin <bisi@pikslar.com>, r=mscott

--- mail/base/content/msgHdrViewOverlay.js
+++ mail/base/content/msgHdrViewOverlay.js
@@ -932,9 +932,10 @@ function AddExtraAddressProcessing(emailAddress, addressNode)
   if (parentElementId == "expandedfromBox" || parentElementId == "expandedreply-toBox")
     condenseName = false;
 
-  if (condenseName && gShowCondensedEmailAddresses && displayName && useDisplayNameForAddress(mailAddress))
+  if (condenseName && gShowCondensedEmailAddresses && displayName)
   {
-    addressNode.setAttribute('label', displayName); 
+    useDisplayNameForAddress(emailAddress);
+    addressNode.setAttribute("label", displayName);
     addressNode.setAttribute("tooltiptext", mailAddress);
     addressNode.setAttribute("tooltip", "emailAddressTooltip");
   }
