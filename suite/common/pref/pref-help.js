6960805494afcc240586c8a0a6285e150d08f0ea
mnyromyr%tprac.de <mnyromyr%tprac.de>
2006-08-12 23:12:25 +0000

    Bug 342560: tag help; r=IanN

--- suite/common/pref/pref-help.js
+++ suite/common/pref/pref-help.js
@@ -24,7 +24,7 @@ var fm = {
   "chrome://messenger/content/addressbook/pref-addressing.xul": "mail_prefs_addressing",
   "chrome://messenger/content/pref-offline.xul": "mail_prefs_offline",
   "chrome://messenger/content/pref-junk.xul": "mail-prefs-junk",
-  "chrome://messenger/content/pref-labels.xul": "mail-prefs-labels",
+  "chrome://messenger/content/pref-labels.xul": "mail-prefs-tags",
   "chrome://messenger/content/pref-receipts.xul": "mail-prefs-receipts",
   "chrome://editor/content/pref-composer.xul": "composer_prefs_general",
   "chrome://editor/content/pref-editing.xul":  "composer_prefs_newpage",
