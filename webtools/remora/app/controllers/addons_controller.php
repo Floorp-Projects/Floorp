9fb599fead807fb67b9a791bd69bdadfe122955f
fligtar%gmail.com <fligtar%gmail.com>
2006-08-16 22:20:20 +0000

    Preliminary add-on controller stuff

--- webtools/remora/app/controllers/addons_controller.php
+++ webtools/remora/app/controllers/addons_controller.php
@@ -3,13 +3,48 @@
 class AddonsController extends AppController
 {
     var $name = 'Addons';
-    var $scaffold;
+    var $components = array('Rdf', 'Versioncompare');
 
-    function index()
-    {
+   /**
+    *
+    */
+    function index() {
         $this->layout = 'developers';
         $this->set('addons', $this->Addon->findall());
     }
+
+   /**
+    * Add new or new version of an add-on
+    * @param int $id
+    */
+    function add($id = 0) {
+
+    }
+
+   /**
+    * Manage an add-on
+    * @param int $id
+    */
+    function manage($id) {
+
+    }
+
+   /**
+    * Edit add-on
+    * @param int $id
+    */
+    function edit($id) {
+
+    }
+
+   /**
+    * Edit a version
+    * @param int $id
+    */
+    function editVersion($id) {
+
+    }
+
 }
 
 ?>
