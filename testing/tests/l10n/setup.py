7e79034167a9c405da59a4b0292abbbaee53006a
axel@pike.org <axel@pike.org>
2006-08-17 11:56:38 +0000

    bug 348554, add verify-search to scripts

--- testing/tests/l10n/setup.py
+++ testing/tests/l10n/setup.py
@@ -3,7 +3,7 @@ setup(name="l10n-tools",
       version="0.1",
       author="Axel Hecht",
       author_email="l10n@mozilla.com",
-      scripts=['scripts/compare-locales'],
+      scripts=['scripts/compare-locales', 'scripts/verify-search'],
       package_dir={'': 'lib'},
       packages=['Mozilla']
       )
