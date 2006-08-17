fe793d490592314ab407de21a4499fb76bb9e40c
longsonr@gmail.com <longsonr@gmail.com>
2006-08-17 10:36:40 +0000

    Bug 348610 - Cairo degenerate path values are in device units. r=tor, sr=roc

--- layout/svg/base/src/nsSVGPathGeometryFrame.h
+++ layout/svg/base/src/nsSVGPathGeometryFrame.h
@@ -135,12 +135,19 @@ private:
 
   /*
    * Check for what cairo returns for the fill extents of a degenerate path
+   *
+   * @param xmin the minimum x value in device units
+   * @param ymin the minimum y value in device units
+   * @param xmax the maximum x value in device units
+   * @param ymax the maximum y value in device units
+   *
+   * @return PR_TRUE if the path is degenerate
    */
   static PRBool
   IsDegeneratePath(double xmin, double ymin, double xmax, double ymax)
   {
-    return (xmin ==  32767 && ymin ==  32767 &&
-            xmax == -32768 && ymax == -32768);
+    return (fabs(xmin - 32767) < 1 && fabs(ymin - 32767) < 1 &&
+            fabs(xmax + 32768) < 1 && fabs(ymax + 32768) < 1);
   }
 
   nsSVGMarkerProperty *GetMarkerProperty();
