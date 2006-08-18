78f43a7e1d42e6f2ae784b978d001b9528557f2e
bclary%bclary.com <bclary%bclary.com>
2006-08-18 15:15:03 +0000

    JavaScript Library - improved regression test for bug 348986, by Igor Bukanov

--- js/tests/js1_5/Function/regress-348986.js
+++ js/tests/js1_5/Function/regress-348986.js
@@ -51,45 +51,108 @@ function test()
   printBugNumber (bug);
   printStatus (summary);
   
-// Number of nested function f(){function f(){function f(){...}}}
-  var N = 700;
-  var prefix="function f(){";
-  var suffix="}";
-  var source = Array(N+1).join(prefix) + Array(N+1).join(suffix);
-  var F = Function(source);
+// Construct f(){function f(){function f(){...}}} with maximum
+// nested function declaration still does not hit recursion limit.
 
-// We successfully parsed the source, now call F.toSource() from a deeply
-// recursive code when already consumed C stack plus the stack to perform
-// F.toSource() would exceed the stack required to parse F.
+  var deepestFunction;
 
-  var testObj = { 	
-	recursionLevel: N*0.7,
-	get propertyWithGetter() {
-      if (this.recursionLevel <= 0)
-        return underDeepRecursion();
-      this.recursionLevel--;
-      return this.propertyWithGetter;
-    } 
-  };
+  var n = findActionMax(function(n) {
+    var prefix="function f(){";
+    var suffix="}";
+    var source = Array(n+1).join(prefix) + Array(n+1).join(suffix);
+    try {
+      deepestFunction = Function(source);
+      return true; 
+    } catch (e) {
+      if (!(e instanceof InternalError))
+			throw e;
+      return false; 
+    }
 
-  function underDeepRecursion()
-  {
-    return F.toSource();
-  }
+  });
 
-  var success = false;
+  if (n == 0)
+    throw "unexpected";
+
+  writeLineToLog("Max nested function leveles:"+n);
+
+  n = findActionMax(function(n) {
+    try {
+      callAfterConsumingCStack(n, function() {});
+      return true;
+    } catch (e) {
+      if (!(e instanceof InternalError))
+			throw e;
+      return false;	
+    }
+  });
+
+  writeLineToLog("Max callAfterConsumingCStack levels:"+n);
+
+// Here n is max possible value when callAfterConsumingCStack(n, emptyFunction)
+// does not trigger stackOverflow. Decrease it slightly to give some C stack space
+// for deepestFunction.toSource()
+  
+  n = Math.max(0, n - 10);
   try {
-    testObj.propertyWithGetter;
-    success = true;
+    var src = callAfterConsumingCStack(n, function() {
+      return deepestFunction.toSource();
+    });
+    throw "Test failed to hit the recursion limit.";
   } catch (e) {
-    success = (e instanceof InternalError);
+    if (!(e instanceof InternalError))
+      throw e;
   }
-
-  writeLineToLog("success="+success);
 
   expect = true;
-  actual = success;
-  reportCompare(expect, actual, summary);
+  actual = true;
+  reportCompare(expect, true, summary);
 
   exitFunc ('test');
+}
+
+function callAfterConsumingCStack(n, action)
+{
+  var testObj = { 	
+    get propertyWithGetter() {
+      if (n == 0)
+      return action();
+      n--;
+      return this.propertyWithGetter;
+    }
+  };
+  return testObj.propertyWithGetter;
+}
+
+
+// Return the maximum positive value of N where action(N) still returns true 
+// or 0 if no such value exists.
+function findActionMax(action)
+{
+  var N, next, increase;
+
+  n = 0;
+  for (;;) {
+    var next = (n == 0 ? 1 : n * 2);
+    if (!isFinite(next) || !action(next))
+      break;
+    n = next;
+  }
+  if (n == 0)
+    return 0;
+		
+  var increase = n / 2;
+  for (;;) {
+    var next = n + increase;
+    if (next == n)
+      break;
+    if (isFinite(next) && action(next)) {
+      n = next;	
+    } else if (increase == 1) {
+      break;
+    } else {
+      increase = increase / 2;
+    }		
+  }
+  return n;
 }
