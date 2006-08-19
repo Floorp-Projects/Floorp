b098867a10147a27312204dd92454c6fec7a4d58
kairo%kairo.at <kairo%kairo.at>
2006-08-19 20:49:04 +0000

    bug 339415 - fix pref-locales to not use builtinURLs, r+sr=Neil

--- suite/browser/browser-prefs.js
+++ suite/browser/browser-prefs.js
@@ -282,6 +282,7 @@ pref("extensions.update.interval", 86400);  // Check for updates to Extensions a
 // Non-symmetric (not shared by extensions) extension-specific [update] preferences
 pref("extensions.getMoreExtensionsURL", "chrome://mozapps/locale/extensions/extensions.properties");
 pref("extensions.getMoreThemesURL", "chrome://mozapps/locale/extensions/extensions.properties");
+pref("extensions.getMoreLocalesURL", "chrome://branding/locale/brand.properties");
 pref("extensions.dss.enabled", false);          // Dynamic Skin Switching                                               
 pref("extensions.dss.switchPending", false);    // Non-dynamic switch pending after next
                                                 // restart.
