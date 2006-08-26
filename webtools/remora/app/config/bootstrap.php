f8e3869444ec301b742ba26f7e7d8e2b4f6d30d2
wclouser%mozilla.com <wclouser%mozilla.com>
2006-08-26 03:29:13 +0000

    adding l10n files

--- webtools/remora/app/config/bootstrap.php
+++ webtools/remora/app/config/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-/* SVN FILE: $Id: bootstrap.php,v 1.3 2006/08/18 22:05:09 fligtar%gmail.com Exp $ */
+/* SVN FILE: $Id: bootstrap.php,v 1.4 2006/08/26 03:29:13 wclouser%mozilla.com Exp $ */
 /**
  * Short description for file.
  *
@@ -21,9 +21,9 @@
  * @package			cake
  * @subpackage		cake.app.config
  * @since			CakePHP v 0.10.8.2117
- * @version			$Revision: 1.3 $
+ * @version			$Revision: 1.4 $
  * @modifiedby		$LastChangedBy: phpnut $
- * @lastmodified	$Date: 2006/08/18 22:05:09 $
+ * @lastmodified	$Date: 2006/08/26 03:29:13 $
  * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 /**
@@ -46,6 +46,21 @@
 //Path defines - no trailing slashes
 define('REPO_PATH', WWW_ROOT.'/files'); // XPI/JAR repository path
 define('FTP_URL', 'http://ftp.mozilla.org/pub/mozilla.org'); // FTP
+
+
+// Required for translating data from the database
+require_once 'Translation2.php';
+
+// Required for translating the templates (using gettext)
+require_once 'language.php';
+
+// Set's up all the gettext functions for our language (passed in $_GET).  
+$language_config = new LANGUAGE_CONFIG(true);
+
+// For other functions/classes
+define('LANG', $language_config->getCurrentLanguage());
+
+unset($language_config);
 
 //EOF
 ?>
