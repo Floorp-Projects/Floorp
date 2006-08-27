85e077fcfd227f4418e320dccc1e5c8b2dc326c6
mattwillis%gmail.com <mattwillis%gmail.com>
2006-08-27 12:52:35 +0000

    bug 348796 - Strips braces from UIDs when using CalDAV. patch by bbbrowning, r1=jminta, r2=dmose

--- calendar/providers/caldav/calDavCalendar.js
+++ calendar/providers/caldav/calDavCalendar.js
@@ -175,9 +175,11 @@ calDavCalendar.prototype = {
         if (this.readOnly) {
             throw Components.interfaces.calIErrors.CAL_IS_READONLY;
         }
-
+        
+        // CalDAV UIDs are used to construct filenames, so we strip braces to avoid
+        // problems with servers which do not support names with {}
         if (aItem.id == null && aItem.isMutable)
-            aItem.id = getUUID();
+            aItem.id = getUUID().replace(/[{}]/g, '');
 
         if (aItem.id == null) {
             if (aListener)
