f04fe1e3620fa06505a3767725a023162e104a1e
gavin@gavinsharp.com <gavin@gavinsharp.com>
2006-08-28 19:36:26 +0000

    Bug 350453: move branding-specific prefs to a special branding prefs file, to avoid ifdef OFFICIAL_BRANDING in firefox.js, r=bsmedberg

---
+++ other-licenses/branding/firefox/pref/firefox-branding.js
@@ -0,0 +1,11 @@
+pref("startup.homepage_override_url","http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/%VERSION%/whatsnew/");
+pref("startup.homepage_welcome_url","http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/%VERSION%/firstrun/");
+// URL user can browse to manually if for some reason all update installation
+// attempts fail.
+pref("app.update.url.manual", "http://%LOCALE%.www.mozilla.com/%LOCALE%/products/%APP%/");
+// A default value for the "More information about this update" link
+// supplied in the "An update is available" page of the update wizard. 
+pref("app.update.url.details", "http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/releases/");
+
+// Release notes URL
+pref("app.releaseNotesURL", "http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/%VERSION%/releasenotes/");
