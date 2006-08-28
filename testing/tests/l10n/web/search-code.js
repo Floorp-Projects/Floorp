d076e64b5e6db90a09901c169236f24d94afa532
axel%pike.org <axel%pike.org>
2006-08-28 22:24:57 +0000

    bug 348731, 348554, unify test harness, one ring to test them all, one page to watch it

--- testing/tests/l10n/web/search-code.js
+++ testing/tests/l10n/web/search-code.js
@@ -35,105 +35,104 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-var controller, view;
 
-const Tier1 = ['en-GB', 'fr', 'de', 'ja', 'ja-JP-mac', 'pl', 'es-ES'];
-const Tier2 = ['zh-CN', 'zh-TW', 'cs', 'da', 'nl', 'fi', 'hu', 'it', 'ko', 'pt-BR', 'pt-PT', 'ru', 'es-AR', 'sv-SE', 'tr'];
-tierMap = {};
-var loc;
-for each (loc in Tier1) tierMap[loc] = 'Tier-1';
-for each (loc in Tier2) tierMap[loc] = 'Tier-2';
-
-var view;
-view = {
-  hashes: {},
-  updateView: function() {
-    this._gView = document.getElementById("view");
-    var cmp = function(l,r) {
-      var lName = results.details[l].ShortName;
-      var rName =  results.details[r].ShortName;
-      if (lName) lName = lName.toLowerCase();
-      if (rName) rName = rName.toLowerCase();
-      return lName < rName ? -1 : lName > rName ? 1 : 0;
-    }
-    for (loc in results.locales) {
-      var row = document.createElement("tr");
-      if (tierMap[loc])
-        row.className += ' ' + tierMap[loc];
-      else
-        row.className += ' Tier-3';
-      this._gView.appendChild(row);
-      var lst = results.locales[loc].list;
-      lst.sort(cmp);
-      var orders = results.locales[loc].orders;
-      if (orders) {
-        var explicit = [];
-        var implicit = [];
-        for (var sn in orders) {
-          if (explicit[sn]) {
-            fatal = true;
-            break;
-          }
-          explicit[sn] = orders[sn];
-        }
-        explicit = [];
-        for each (var path in lst) {
-          var shortName = results.details[path].ShortName;
-          if (orders[shortName]) {
-            explicit[orders[shortName] - 1] = path;
-          }
-          else {
-            implicit.push(path);
-          }
-        }
-        lst = explicit.concat(implicit);
+var searchController = {
+  __proto__: baseController,
+  beforeSelect: function() {
+    this.hashes = {};
+    searchView.setUpHandlers();
+    var _t = this;
+    var callback = function(obj) {
+      delete _t.req;
+      if (view != searchView) {
+        // ignore, we have switched again
+        return;
       }
-      row.innerHTML = '<td class="locale"><a href="https://bugzilla.mozilla.org/buglist.cgi?query_format=advanced&amp;short_desc_type=regexp&amp;short_desc=' + loc + '%5B%20-%5D&amp;chfieldto=Now&amp;field0-0-0=blocked&amp;type0-0-0=substring&amp;value0-0-0=347914">' + loc + '</a></td>';
-      for each (var path in lst) {
-        //YAHOO.widget.Logger.log('testing ' + path);
-        var innerContent;
-        var cl = '';
-        if (path.match(/^mozilla/)) {
-          cl += ' enUS';
+      _t.result = obj;
+      searchView.updateView(keys(_t.result.locales));
+    };
+    this.req = JSON.get('results/' + this.tag + '/search-results.json',
+                        callback);
+  },
+  beforeUnSelect: function() {
+    searchView.destroyHandlers();
+  },
+  showView: function(aClosure) {
+    // json onload handler does this;
+  },
+  getContent: function(aLoc) {
+    var row = document.createDocumentFragment();
+    var lst = this.result.locales[aLoc].list;
+    var _t = this;
+    lst.sort(function(a,b){return _t.cmp.apply(_t,[a,b])});
+    var orders = this.result.locales[aLoc].orders;
+    if (orders) {
+      var explicit = [];
+      var implicit = [];
+      for (var sn in orders) {
+        if (explicit[sn]) {
+          fatal = true;
+          break;
         }
-        var localName = path.substr(path.lastIndexOf('/') + 1)
-        if (results.details[path].error) {
-          innerContent = 'error in ' + localName;
-          cl += ' error';
+        explicit[sn] = orders[sn];
+      }
+      explicit = [];
+      for each (var path in lst) {
+        var shortName = this.result.details[path].ShortName;
+        if (orders[shortName]) {
+          explicit[orders[shortName] - 1] = path;
         }
         else {
-          var shortName = results.details[path].ShortName;
-          var img = results.details[path].Image;
-          if (results.locales[loc].orders && results.locales[loc].orders[shortName]) {
-            cl += " ordered";
-          }
-          innerContent = '<img src="' + img + '">' + shortName;
+          implicit.push(path);
         }
-        var td = document.createElement('td');
-        td.className = 'searchplugin' + cl;
-        td.innerHTML = innerContent;
-        row.appendChild(td);
-        td.details = results.details[path];
-        // test the hash code
-        if (td.details.error) {
-          // ignore errorenous plugins
-          continue;
+      }
+      lst = explicit.concat(implicit);
+    }
+    row.innerHTML = '<td class="locale"><a href="https://bugzilla.mozilla.org/buglist.cgi?query_format=advanced&amp;short_desc_type=regexp&amp;short_desc=' + aLoc + '%5B%20-%5D&amp;chfieldto=Now&amp;field0-0-0=blocked&amp;type0-0-0=substring&amp;value0-0-0=347914">bug</a></td>';
+    for each (var path in lst) {
+      //YAHOO.widget.Logger.log('testing ' + path);
+      var innerContent;
+      var cl = '';
+      if (path.match(/^mozilla/)) {
+        cl += ' enUS';
+      }
+      var localName = path.substr(path.lastIndexOf('/') + 1);
+      if (this.result.details[path].error) {
+        innerContent = 'error in ' + localName;
+        cl += ' error';
+      }
+      else {
+        var shortName = this.result.details[path].ShortName;
+        var img = this.result.details[path].Image;
+        if (this.result.locales[aLoc].orders && this.result.locales[aLoc].orders[shortName]) {
+          cl += " ordered";
         }
-        if (this.hashes[localName]) {
-          this.hashes[localName].nodes.push(td);
-          if (this.hashes[localName].conflict) {
+        innerContent = '<img src="' + img + '">' + shortName;
+      }
+      var td = document.createElement('td');
+      td.className = 'searchplugin' + cl;
+      td.innerHTML = innerContent;
+      row.appendChild(td);
+      td.details = this.result.details[path];
+      // test the hash code
+      if (td.details.error) {
+        // ignore errorenous plugins
+        continue;
+      }
+      if (this.hashes[localName]) {
+        this.hashes[localName].nodes.push(td);
+        if (this.hashes[localName].conflict) {
+          td.className += ' conflict';
+        }
+        else if (this.hashes[localName].key != td.details.md5) {
+          this.hashes[localName].conflict = true;
+          for each (td in this.hashes[localName].nodes) {
             td.className += ' conflict';
           }
-          else if (this.hashes[localName].key != td.details.md5) {
-            this.hashes[localName].conflict = true;
-            for each (td in this.hashes[localName].nodes) {
-              td.className += ' conflict';
-            }
-          }
         }
-        else {
-          this.hashes[localName] =  {key: td.details.md5, nodes: [td]};
-        }
+      }
+      else {
+        this.hashes[localName] =  {key: td.details.md5, nodes: [td]};
       }
     }
     for (localName in this.hashes) {
@@ -146,6 +145,31 @@ view = {
       }
       YAHOO.widget.Logger.log('difference in ' + localName + ' for ' + locs.join(', '));
     }
+    return row;
+  },
+  cmp: function(l,r) {
+    var lName = this.result.details[l].ShortName;
+    var rName =  this.result.details[r].ShortName;
+    if (lName) lName = lName.toLowerCase();
+    if (rName) rName = rName.toLowerCase();
+    return lName < rName ? -1 : lName > rName ? 1 : 0;
+  }
+};
+var searchView = {
+  __proto__: baseView,
+  setUpHandlers: function() {
+    _t = this;
+    this.omv = function(event){_t.onMouseOver.apply(_t, [event]);};
+    this.omo = function(event){_t.onMouseOut.apply(_t, [event]);};
+    this.content.addEventListener("mouseover", this.omv, true);
+    this.content.addEventListener("mouseout", this.omv, true);
+  },
+  destroyHandlers: function() {
+    this.content.removeEventListener("mouseover", this.omv, true);
+    this.content.removeEventListener("mouseout", this.omv, true);
+    if (this._dv) {
+      this._dv.hide();
+    }
   },
   onMouseOver: function(event) {
     if (!event.target.details)
@@ -155,30 +179,30 @@ view = {
       clientX: event.clientX,
       clientY: event.clientY
     };
-    view.pending = setTimeout(function(){view.showDetail(_e);}, 500);
+    this.pending = setTimeout(function(){searchView.showDetail(_e);}, 500);
   },
   onMouseOut: function(event) {
-    if (view.pending) {
-      clearTimeout(view.pending);
-      delete view.pending;
+    if (this.pending) {
+      clearTimeout(this.pending);
+      delete this.pending;
       return;
     }
     if (!event.target.details)
       return;
   },
   showDetail: function(event) {
-    delete view.pending;
-    if (!view._dv) {
-      view._dv = new YAHOO.widget.Panel('dv', {visible:true,draggable:true,constraintoviewport:true});
-      view._dv.beforeHideEvent.subscribe(function(){delete view._dv;}, null);
+    delete this.pending;
+    if (!this._dv) {
+      this._dv = new YAHOO.widget.Panel('dv', {visible:true,draggable:true,constraintoviewport:true});
+      this._dv.beforeHideEvent.subscribe(function(){delete this._dv;}, null);
     }
     var dt = event.details;
     if (dt.error) {
-      view._dv.setHeader("Error");
-      view._dv.setBody(dt.error);
+      this._dv.setHeader("Error");
+      this._dv.setBody(dt.error);
     }
     else {
-      view._dv.setHeader("Details");
+      this._dv.setHeader("Details");
       var c = '';
       var q = 'test';
       var len = 0;
@@ -193,41 +217,12 @@ view = {
         c += '<button onclick="window.open(this.textContent)">' + uc + '</button><br>';
         len  = len < c.length ? c.length : len;
       }
-      view._dv.setBody(c);
+      this._dv.setBody(c);
     }
-    view._dv.render(document.body);
-    view._dv.moveTo(event.clientX + window.scrollX, event.clientY + window.scrollY);
-  },
-  onClickDisplay: function(event) {
-    if (event.target.localName != 'INPUT') {
-      return false;
-    }
-    YAHOO.widget.Logger.log('clicking ' + event);
-    var handler = function() {return function(){view.onCDReal(event.target)}};
-    window.setTimeout(handler(),0);
-    return false;
-  },
-  onCDReal: function(target) {
-    var dsp = target.checked;
-    target.checked = dsp;
-    dsp = dsp ? 'table-row' : 'none';
-    var layout = document.styleSheets[document.styleSheets.length - 1];
-    var i = Number(target.name);
-    layout.deleteRule(i);
-    layout.insertRule('.Tier-' + (i+1) + ' {display: ' + dsp + ';}', i);
-    return false;
+    this._dv.render(document.body);
+    this._dv.moveTo(event.clientX + window.scrollX, event.clientY + window.scrollY);
+    this._dv.show();
   }
 };
-
-controller = {
-  kPrefix: 'results/',
-}
-
-function onLoad(event) {
-  document.removeEventListener("load", onLoad, true);
-  document.getElementById("checks").addEventListener("click", view.onClickDisplay, true);
-  view.updateView();
-  document.getElementById("view").addEventListener("mouseover", view.onMouseOver, true);
-  document.getElementById("view").addEventListener("mouseout", view.onMouseOut, true);
-  return true;
-}
+controller.addPane('Search', 'search', searchController, searchView);
+//controller.addLocales(keys(results.locales));
