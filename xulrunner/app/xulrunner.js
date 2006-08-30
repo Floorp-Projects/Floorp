763cb4ad06c18b05e752b720afa16990d941987d
bent.mozilla%gmail.com <bent.mozilla%gmail.com>
2006-08-30 19:14:28 +0000

    Bug 350528 - "XULRunner needs to ship with some additional preferences". r=bsmedberg.

--- xulrunner/app/xulrunner.js
+++ xulrunner/app/xulrunner.js
@@ -60,3 +60,13 @@ pref("xpinstall.dialog.progress.skin", "chrome://mozapps/content/extensions/exte
 pref("xpinstall.dialog.progress.type.chrome", "Extension:Manager");
 pref("xpinstall.dialog.progress.type.skin", "Extension:Manager");
 pref("xpinstall.enabled", true);
+#ifdef XP_WIN
+pref("browser.preferences.instantApply", false);
+#else
+pref("browser.preferences.instantApply", true);
+#endif
+#ifdef XP_MACOSX
+pref("browser.preferences.animateFadeIn", true);
+#else
+pref("browser.preferences.animateFadeIn", false);
+#endif
