172291270faf681e3a6543054d21888879c11a14
brendan%mozilla.org <brendan%mozilla.org>
2006-09-07 16:57:59 +0000

    CheckSideEffects needs to do more work for delete operands, to match the code generator for delete (346915, r=mrbkap).

--- js/src/jsemit.c
+++ js/src/jsemit.c
@@ -2180,7 +2180,6 @@ CheckSideEffects(JSContext *cx, JSTreeContext *tc, JSParseNode *pn,
 
       case PN_UNARY:
         if (pn->pn_type == TOK_INC || pn->pn_type == TOK_DEC ||
-            pn->pn_type == TOK_DELETE ||
             pn->pn_type == TOK_THROW ||
 #if JS_HAS_GENERATORS
             pn->pn_type == TOK_YIELD ||
@@ -2188,6 +2187,25 @@ CheckSideEffects(JSContext *cx, JSTreeContext *tc, JSParseNode *pn,
             pn->pn_type == TOK_DEFSHARP) {
             /* All these operations have effects that we must commit. */
             *answer = JS_TRUE;
+        } else if (pn->pn_type == TOK_DELETE) {
+            pn2 = pn->pn_kid;
+            switch (pn2->pn_type) {
+              case TOK_NAME:
+              case TOK_DOT:
+#if JS_HAS_XML_SUPPORT
+              case TOK_DBLDOT:
+#endif
+#if JS_HAS_LVALUE_RETURN
+              case TOK_LP:
+#endif
+              case TOK_LB:
+                /* All these delete addressing modes have effects too. */
+                *answer = JS_TRUE;
+                break;
+              default:
+                ok = CheckSideEffects(cx, tc, pn2, answer);
+                break;
+            }
         } else {
             ok = CheckSideEffects(cx, tc, pn->pn_kid, answer);
         }
