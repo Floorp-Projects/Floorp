3363e38fe7a3af6377dd5f3e9fa3849dedda21bc
bclary@bclary.com <bclary@bclary.com>
2006-09-10 09:26:16 +0000

    JavaScript Test - update test to reflect current behavior

--- js/tests/js1_7/block/regress-349493.js
+++ js/tests/js1_7/block/regress-349493.js
@@ -51,7 +51,7 @@ function test()
   printBugNumber (bug);
   printStatus (summary);
 
-  expect = 'function (x) {\n    return x ? 0 : let (a = 3) a;\n}';
+  expect = 'function (x) {\n    return x ? 0 : (let (a = 3) a);\n}';
 
   var f = function (x) { return x ? 0 : let (a = 3) a; }
   actual = f.toString();
