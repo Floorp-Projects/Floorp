346a97c59451ae803a1c370bfa24ae36f2cba33e
rob_strong%exchangecode.com <rob_strong%exchangecode.com>
2006-09-11 17:55:36 +0000

    Bug 351611 - when restarting after installing an extension, I'm not warned about closing tabs. r=sspitzer

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -1419,11 +1419,36 @@ function installUpdatesAll() {
 function restartApp() {
   const nsIAppStartup = Components.interfaces.nsIAppStartup;
 
-  if (gUpdatesOnly)
+  if (gUpdatesOnly) {
     window.close();
-  else if (canQuitApplication())
-    Components.classes["@mozilla.org/toolkit/app-startup;1"].getService(nsIAppStartup)
-              .quit(nsIAppStartup.eRestart | nsIAppStartup.eAttemptQuit);
+    return;
+  }
+
+  // Notify all windows that an application quit has been requested.
+  var os = Components.classes["@mozilla.org/observer-service;1"]
+                     .getService(Components.interfaces.nsIObserverService);
+  var cancelQuit = Components.classes["@mozilla.org/supports-PRBool;1"]
+                             .createInstance(Components.interfaces.nsISupportsPRBool);
+  os.notifyObservers(cancelQuit, "quit-application-requested", null);
+
+  // Something aborted the quit process. 
+  if (cancelQuit.data)
+    return;
+
+  // Notify all windows that an application quit has been granted.
+  os.notifyObservers(null, "quit-application-granted", null);
+
+  // Enumerate all windows and call shutdown handlers
+  var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                     .getService(Components.interfaces.nsIWindowMediator);
+  var windows = wm.getEnumerator(null);
+  while (windows.hasMoreElements()) {
+    var win = windows.getNext();
+    if (("tryToClose" in win) && !win.tryToClose())
+      return;
+  }
+  Components.classes["@mozilla.org/toolkit/app-startup;1"].getService(nsIAppStartup)
+            .quit(nsIAppStartup.eRestart | nsIAppStartup.eAttemptQuit);
 }
 
 function installWithFilePicker() {
