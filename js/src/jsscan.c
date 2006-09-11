f162d5ee4d8c438d15c5f2f933bff54294080051
brendan%mozilla.org <brendan%mozilla.org>
2006-09-11 19:54:18 +0000

    Fix bogus assertion (352208, r/a=me).

--- js/src/jsscan.c
+++ js/src/jsscan.c
@@ -1012,6 +1012,7 @@ js_PeekTokenSameLine(JSContext *cx, JSTokenStream *ts)
     JSTokenType tt;
 
     JS_ASSERT(ts->lookahead == 0 ||
+              (ts->flags & TSF_ERROR) ||
               ON_CURRENT_LINE(ts, CURRENT_TOKEN(ts).pos) ||
               ts->tokens[(ts->cursor + ts->lookahead) & NTOKENS_MASK].type
                   == TOK_EOL);
