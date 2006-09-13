c3f54982f1853ae13efc331af8acfeb412f019c9
myk@mozilla.org <myk@mozilla.org>
2006-09-13 23:15:08 +0000

    bug 352584: remove "products/" from the app.update.url.manual preference in official branding Patch by Reed Loden. r=beltzner

--- other-licenses/branding/firefox/pref/firefox-branding.js
+++ other-licenses/branding/firefox/pref/firefox-branding.js
@@ -2,7 +2,7 @@ pref("startup.homepage_override_url","http://%LOCALE%.www.mozilla.com/%LOCALE%/%
 pref("startup.homepage_welcome_url","http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/%VERSION%/firstrun/");
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
-pref("app.update.url.manual", "http://%LOCALE%.www.mozilla.com/%LOCALE%/products/%APP%/");
+pref("app.update.url.manual", "http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/");
 // A default value for the "More information about this update" link
 // supplied in the "An update is available" page of the update wizard. 
 pref("app.update.url.details", "http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/releases/");
