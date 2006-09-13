c8569c85b5780dc8f60ee5043fde2ad04f5df587
davel%mozilla.com <davel%mozilla.com>
2006-09-13 16:33:39 +0000

    b=352305, on behalf of asqueella r=davel Mark more clearly the parts of the xpcshell unit test example that need to be customized, and convert the existing example to an in-place runnable example

--- tools/test-harness/xpcshell-simple/example/unit/test_sample.js
+++ tools/test-harness/xpcshell-simple/example/unit/test_sample.js
@@ -36,10 +36,18 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-// This is the most basic testcase.  It just sets a timeout, and then exits the
-// test harness when that timeout fires.  This is meant to demonstrate that
-// there is a complete event system available to test scripts.
+/* This is the most basic testcase.  It makes some trivial assertions, 
+ * then sets a timeout, and exits the test harness when that timeout 
+ * fires. This is meant to demonstrate that there is a complete event 
+ * system available to test scripts.
+ * Available functions are described at:
+ * http://developer.mozilla.org/en/docs/Writing_xpcshell-based_unit_tests
+ */
 function run_test() {
+  do_check_eq(57, 57)
+  do_check_neq(1, 2)
+  do_check_true(true);
+
   do_test_pending();
-  do_timeout(1000, "do_test_finished();");
+  do_timeout(100, "do_test_finished();");
 }
