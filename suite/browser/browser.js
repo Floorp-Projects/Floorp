132176e970244e00605d9aee367a55a36f9668ec
aaronl%netscape.com <aaronl%netscape.com>
2006-09-14 06:06:31 +0000

    Bug 167921 - allow web pages to use preventDefault so that typeaheadfind doesn't interfere with them. r=kyle, sr=brendan. Also fixes bug 165315 - typeaheadfind shouldn't hardcode kbd shortcuts, bug 77704  - add accelerator for find prev (also added menu item). Also fixes bug 167783 - ctrl+g for typeaheadfind after repeating characters. Also fixes bug 168408 - backspace after bad character typed should do the right thing, but don't add bad characters to buffer. Also fixes bug 71832 - add F3 for find next (also shift+F3). Bug 157669 - Alt+D to select location bar (IE keyboard compatibility), r=sgehani, sr=hewitt

--- suite/browser/browser.js
+++ suite/browser/browser.js
@@ -382,13 +382,13 @@ function BrowserFind()
   findInPage(getBrowser(), window._content, focusedWindow)
 }
 
-function BrowserFindAgain()
+function BrowserFindAgain(reverse)
 {
     var focusedWindow = document.commandDispatcher.focusedWindow;
     if (!focusedWindow || focusedWindow == window)
       focusedWindow = window._content;
 
-  findAgainInPage(getBrowser(), window._content, focusedWindow)
+  findAgainInPage(getBrowser(), window._content, focusedWindow, reverse)
 }
 
 function BrowserCanFindAgain()
