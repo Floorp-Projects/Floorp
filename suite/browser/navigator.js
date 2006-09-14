40599fe98fb514a9e39a413b677876e4b21f1476
bnesse%netscape.com <bnesse%netscape.com>
2006-09-14 06:00:40 +0000

    Fix for bug 106159. Force getComplexValue calls to return string values. r=sgehani, sr=alecf.

--- suite/browser/navigator.js
+++ suite/browser/navigator.js
@@ -168,7 +168,7 @@ function getHomePage()
   var url;
   try {
     url = pref.getComplexValue("browser.startup.homepage",
-                               Components.interfaces.nsIPrefLocalizedString);
+                               Components.interfaces.nsIPrefLocalizedString).data;
   } catch (e) {
   }
 
@@ -629,7 +629,7 @@ function readRDFString(aDS,aRes,aProp)
 function ensureDefaultEnginePrefs(aRDF,aDS) 
 {
   var mPrefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-  var defaultName = mPrefs.getComplexValue("browser.search.defaultenginename" , Components.interfaces.nsIPrefLocalizedString);
+  var defaultName = mPrefs.getComplexValue("browser.search.defaultenginename", Components.interfaces.nsIPrefLocalizedString).data;
   var kNC_Root = aRDF.GetResource("NC:SearchEngineRoot");
   var kNC_child = aRDF.GetResource("http://home.netscape.com/NC-rdf#child");
   var kNC_Name = aRDF.GetResource("http://home.netscape.com/NC-rdf#Name");
@@ -694,7 +694,7 @@ function OpenSearch(tabName, forceDialogFlag, searchStr)
   try {
     autoOpenSearchPanel = pref.getBoolPref("browser.search.opensidebarsearchpanel");
     defaultSearchURL = pref.getComplexValue("browser.search.defaulturl",
-                                            Components.interfaces.nsIPrefLocalizedString);
+                                            Components.interfaces.nsIPrefLocalizedString).data;
   } catch (ex) {
   }
 
