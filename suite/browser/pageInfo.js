71ea09db7f08a2988fd6fd882855493a4cbc8ddb
db48x%yahoo.com <db48x%yahoo.com>
2006-09-14 06:09:16 +0000

    bug 239472: Page Info -> media -> Save As... doesn't work r=db48x, sr=jag

--- suite/browser/pageInfo.js
+++ suite/browser/pageInfo.js
@@ -715,16 +715,6 @@ function onBeginLinkDrag(event,urlField,descField)
 }
 
 //******** Image Stuff
-function getSource(item)
-{
-  // Return the correct source without strict warnings
-  if ("href" in item && item.href)
-    return item.href;
-  if ("src" in item && item.src)
-    return item.src;
-  return null;
-}
-
 function getSelectedImage(tree)
 {
   if (!imageView.rowCount) return null;
@@ -739,10 +729,10 @@ function saveMedia()
 {
   var tree = document.getElementById("imagetree");
   var item = getSelectedImage(tree);
-  var url = getAbsoluteURL(getSource(item), item);
+  var url = imageView.data[tree.currentIndex][0];
 
   if (url)
-    saveURL(url, null, 'SaveImageTitle', false );
+    saveURL(url, null, 'SaveImageTitle', false, makeURI(item.baseURI));
 }
 
 function onImageSelect()
