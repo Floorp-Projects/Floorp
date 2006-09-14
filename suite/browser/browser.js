3124d37813af11931c0c38af3a73a3d6a4aae944
rods%netscape.com <rods%netscape.com>
2006-09-14 06:03:53 +0000

    Adds a new arg to the PP call. The new arg is not being used, it is for future and is needed for the API freeze. Bug 144716 r=dcone sr=attinasi

--- suite/browser/browser.js
+++ suite/browser/browser.js
@@ -252,7 +252,7 @@ function BrowserPrintPreview()
       Components.interfaces.nsIWebBrowserPrint);     
     if (webBrowserPrint) {
       gPrintSettings = GetPrintSettings();
-      webBrowserPrint.printPreview(gPrintSettings, null);
+      webBrowserPrint.printPreview(gPrintSettings, null, null);
     }
 
     // show the toolbar after we go into print preview mode so
@@ -289,7 +289,7 @@ function BrowserPrintSetup()
           psService.savePrintSettingsToPrefs(gPrintSettings, false, gPrintSettings.kInitSaveNativeData);
         }
         if (webBrowserPrint.doingPrintPreview) {
-          webBrowserPrint.printPreview(gPrintSettings, null);
+          webBrowserPrint.printPreview(gPrintSettings, null, null);
         }
       }
     }
