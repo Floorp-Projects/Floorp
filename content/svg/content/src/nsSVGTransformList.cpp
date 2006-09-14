eace625dd0bec7f5a4bbeca0661153d227704689
tor@cs.brown.edu <tor@cs.brown.edu>
2006-09-14 13:48:36 +0000

    Bug 294137 - mozilla accepts illegal syntax on transform attribute. Patch by wellsk@us.ibm.com, tor.  r+sr=roc

--- content/svg/content/src/nsSVGTransformList.cpp
+++ content/svg/content/src/nsSVGTransformList.cpp
@@ -38,6 +38,7 @@
  * ***** END LICENSE BLOCK ***** */
 
 #include "nsSVGTransformList.h"
+#include "nsSVGTransformListParser.h"
 #include "nsSVGTransform.h"
 #include "nsSVGMatrix.h"
 #include "nsDOMError.h"
@@ -130,119 +131,17 @@ NS_IMETHODIMP
 nsSVGTransformList::SetValueString(const nsAString& aValue)
 {
   // XXX: we don't implement the _exact_ BNF given in the
-  // specs. 
-  
+  // specs.
+
   nsresult rv = NS_OK;
 
-  char *str = ToNewCString(aValue);
-  
-  char* rest = str;
-  char* keyword;
-  char* args;
-  const char* delimiters1 = "\x20\x9\xD\xA,(";
-  const char* delimiters2 = "()";
+  // parse transform attribute value
   nsCOMArray<nsIDOMSVGTransform> xforms;
-    
-  while ((keyword = nsCRT::strtok(rest, delimiters1, &rest))) {
+  nsSVGTransformListParser parser(&xforms);
+  rv = parser.Parse(aValue);
 
-    while (rest && isspace(*rest))
-      ++rest;
-
-    if (!(args = nsCRT::strtok(rest, delimiters2, &rest))) {
-      rv = NS_ERROR_FAILURE;
-      break; // parse error
-    }
-    nsCOMPtr<nsIDOMSVGTransform> transform;
-    NS_NewSVGTransform(getter_AddRefs(transform));
-    if (!transform) {
-      rv = NS_ERROR_OUT_OF_MEMORY;
-      break;
-    }
-    
-    nsCOMPtr<nsIAtom> keyatom = do_GetAtom(keyword);
-    
-    if (keyatom == nsSVGAtoms::translate) {
-      // tx [ty=0]
-      float t[2] = { 0.f };
-      PRInt32 num_parsed = ParseParameterList(args, t, 2);
-      if (num_parsed != 1 && num_parsed != 2) {
-        rv = NS_ERROR_FAILURE;
-        break; // parse error
-      }
-
-      transform->SetTranslate(t[0], t[1]);
-    }
-    else if (keyatom == nsSVGAtoms::scale) { 
-      // sx [sy=sx]
-      float s[2] = { 0.f };
-      PRInt32 num_parsed = ParseParameterList(args, s, 2);
-      if (num_parsed != 1 && num_parsed != 2) {
-        rv = NS_ERROR_FAILURE;
-        break; // parse error
-      }
-
-      if (num_parsed == 1)
-        s[1] = s[0];
-
-      transform->SetScale(s[0], s[1]);
-    }
-    else if (keyatom == nsSVGAtoms::rotate) {
-      // r [x0=0 y0=0]
-      float r[3] = { 0.f };
-      PRInt32 num_parsed = ParseParameterList(args, r, 3);
-      if (num_parsed != 1 && num_parsed != 3) {
-        rv = NS_ERROR_FAILURE;
-        break; // parse error
-      }
-
-      transform->SetRotate(r[0], r[1], r[2]);
-    }
-    else if (keyatom == nsSVGAtoms::skewX) {
-      // x-angle
-      float angle;
-      PRInt32 num_parsed = ParseParameterList(args, &angle, 1);
-      if (num_parsed != 1) {
-        rv = NS_ERROR_FAILURE;
-        break; // parse error
-      }
-
-      transform->SetSkewX(angle);
-    }
-    else if (keyatom == nsSVGAtoms::skewY) {
-      // y-angle
-      float angle;
-      PRInt32 num_parsed = ParseParameterList(args, &angle, 1);
-      if (num_parsed != 1) {
-        rv = NS_ERROR_FAILURE;
-        break; // parse error
-      }
-
-      transform->SetSkewY(angle);
-    }
-    else if (keyatom == nsSVGAtoms::matrix) {
-      // a b c d e f
-      float m[6];
-      PRInt32 num_parsed = ParseParameterList(args, m, 6);
-      if (num_parsed != 6) {
-        rv = NS_ERROR_FAILURE;
-        break; // parse error
-      }
-
-      nsCOMPtr<nsIDOMSVGMatrix> matrix;
-      NS_NewSVGMatrix(getter_AddRefs(matrix),
-                      m[0], m[1], m[2], m[3], m[4], m[5]);
-      NS_ASSERTION(matrix, "couldn't create matrix");
-      transform->SetMatrix(matrix);
-    }
-    else { // parse error
-      rv = NS_ERROR_FAILURE;
-      break;
-    }
-    xforms.AppendObject(transform);
-  }
-
-  if (keyword || NS_FAILED(rv)) { 
-    // there was a parse error. 
+  if (NS_FAILED(rv)) {
+    // there was a parse error.
     rv = NS_ERROR_FAILURE;
   }
   else {
@@ -255,57 +154,7 @@ nsSVGTransformList::SetValueString(const nsAString& aValue)
     DidModify();
   }
 
-  nsMemory::Free(str);
-  
   return rv;
-}
-
-// helper for SetValueString
-// parse up to nvars comma-separated parameters into vars, returning
-// the number of variables actually provided.
-//
-// -1 will be returned if any of the arguments can't be converted to a
-// float.  The return value may be higher than nvars, if more
-// arguments were provided; no attempt is made to actually parse any
-// more arguments than nvars.
-//
-// note that this would accept ",,," and will just return 0
-// arguments found, instead of -1.  This is because the numeric
-// values can have spaces surrounding them, but the spaces can also
-// be used as delimiters.  strtok doesn't tell us what the
-// delimiter is, so we have no way to distinguish
-// ' 20,20 ' from ',,20,20,,' (or from ',,20 20,,', for that matter).
-//
-// XXX If someone knows for sure that it's not ok to mix commas and
-// spaces as delimiters, then we can scan the string for a comma,
-// and if found use a delimiter of just ",", otherwise use a set of
-// spaces (without a comma).
-PRInt32
-nsSVGTransformList::ParseParameterList(char *paramstr, float *vars, PRInt32 nvars)
-{
-  if (!paramstr)
-    return 0;
-
-  char *arg, *argend, *argrest = paramstr;
-  int num_args_found = 0;
-  float f;
-
-  const char* arg_delimiters = "\x20\x09\x0d\x0a,";
-
-  while ((arg = nsCRT::strtok(argrest, arg_delimiters, &argrest))) {
-    if (num_args_found < nvars) {
-      f = (float) PR_strtod(arg, &argend);
-      if (arg == argend || *argend != '\0')
-        return -1;
-
-      vars[num_args_found] = f;
-    }
-
-    arg = argrest;
-    num_args_found++;
-  }
-
-  return num_args_found;
 }
 
 NS_IMETHODIMP
