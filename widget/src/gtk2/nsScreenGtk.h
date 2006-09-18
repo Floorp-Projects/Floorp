88bdf640e66293b73653dc68312d65506dd3c2ea
dbaron@dbaron.org <dbaron@dbaron.org>
2006-09-18 23:37:21 +0000

    Pick up dynamic changes in screen size (although some other things still cache them per-window).  b=256646  r+sr=roc

--- widget/src/gtk2/nsScreenGtk.h
+++ widget/src/gtk2/nsScreenGtk.h
@@ -40,9 +40,10 @@
 
 #include "nsIScreen.h"
 #include "nsRect.h"
+#include "gdk/gdk.h"
+#include <X11/Xlib.h>
 
 #ifdef MOZ_ENABLE_XINERAMA
-#include <X11/Xlib.h>
 #include <X11/extensions/Xinerama.h>
 #endif // MOZ_ENABLE_XINERAMA
 
@@ -57,13 +58,18 @@ public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSISCREEN
 
-  void Init();
+  void Init(PRBool aReInit = PR_FALSE);
+  void ReInit() { Init(PR_TRUE); }
 #ifdef MOZ_ENABLE_XINERAMA
   void Init(XineramaScreenInfo *aScreenInfo);
 #endif
 
+  Atom NetWorkareaAtom() { return mNetWorkareaAtom; }
+
 private:
+  GdkWindow *mRootWindow;
   PRUint32 mScreenNum;
+  Atom mNetWorkareaAtom;
   nsRect mRect; // in pixels, not twips
   nsRect mAvailRect; // in pixels, not twips
 };
