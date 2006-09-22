f7a6f598e3bd7ee669f8d6bcba40c82e4f106fa6
bienvenu%nventure.com <bienvenu%nventure.com>
2006-09-22 17:19:48 +0000

    show done compacting status message when compacting a single folder, sr=mscott 275199

--- mailnews/base/src/nsMsgFolderCompactor.cpp
+++ mailnews/base/src/nsMsgFolderCompactor.cpp
@@ -464,6 +464,8 @@ nsFolderCompactState::FinishCompact()
 
   if (m_compactAll)
     rv = CompactNextFolder();
+  else 
+    ShowDoneStatus();
       
   return rv;
 }
@@ -486,7 +488,7 @@ void nsFolderCompactState::ShowDoneStatus()
   if (m_folder)
   {
     nsXPIDLString statusString;
-    nsresult rv = m_folder->GetStringWithFolderNameFromBundle("compactingDone", getter_Copies(statusString));
+    nsresult rv = m_folder->GetStringWithFolderNameFromBundle("doneCompacting", getter_Copies(statusString));
     if (statusString && NS_SUCCEEDED(rv))
       ShowStatusMsg(statusString);
   }
