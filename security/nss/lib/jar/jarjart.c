28d374a498747e80599036432862aad0fd7c5873
nelson@bolyard.com <nelson@bolyard.com>
2006-09-25 19:20:37 +0000

    Fix leaks in jarfile.c (bug 338453), jarjart.c (bug 351408), and jarver.c (bug 337361). Patch contributed by ehsan.akhgari@gmail.com

--- security/nss/lib/jar/jarjart.c
+++ security/nss/lib/jar/jarjart.c
@@ -311,8 +311,6 @@ void *JAR_JAR_end_hash (int alg, void *cookie)
 int JAR_JAR_sign_archive 
       (char *nickname, char *password, char *sf, char *outsig)
   {
-  char *out_fn;
-
   int status = JAR_ERR_GENERAL;
   JAR_FILE sf_fp; 
   JAR_FILE out_fp;
@@ -322,6 +320,11 @@ int JAR_JAR_sign_archive
 
   CERTCertificate *cert;
 
+  if (PORT_Strlen (sf) < 5)
+    {
+    return JAR_ERR_GENERAL;
+    }
+
   /* open cert and key databases */
 
   certdb = JAR_open_database();
@@ -330,12 +333,10 @@ int JAR_JAR_sign_archive
 
   keydb = jar_open_key_database();
   if (keydb == NULL)
+    {
+    JAR_close_database(certdb);
     return JAR_ERR_GENERAL;
-
-  out_fn = PORT_Strdup (sf);
-
-  if (out_fn == NULL || PORT_Strlen (sf) < 5)
-    return JAR_ERR_GENERAL;
+    }
 
   sf_fp = JAR_FOPEN (sf, "rb");
   out_fp = JAR_FOPEN (outsig, "wb");
