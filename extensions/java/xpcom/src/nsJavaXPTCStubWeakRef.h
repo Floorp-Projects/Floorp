ba8d925b4e62c9cc276ac22c2cba585a2c3d83b6
pedemont%us.ibm.com <pedemont%us.ibm.com>
2006-09-27 15:08:47 +0000

    A weak reference implementation. Not part of default build.
    This file was copied in CVS from the following location:
    mozilla/extensions/java/xpcom/nsJavaXPTCStubWeakRef.h
    Original committer: pedemont%us.ibm.com
    Original revision: 1.1
    Original date: 2004/09/15 21:50:41

---
+++ extensions/java/xpcom/src/nsJavaXPTCStubWeakRef.h
@@ -0,0 +1,58 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Java XPCOM Bindings.
+ *
+ * The Initial Developer of the Original Code is
+ * IBM Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2004
+ * IBM Corporation. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Javier Pedemonte (jhpedemonte@gmail.com)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#ifndef _nsJavaXPTCStubWeakRef_h_
+#define _nsJavaXPTCStubWeakRef_h_
+
+#include "jni.h"
+#include "nsIWeakReference.h"
+
+
+class nsJavaXPTCStubWeakRef : public nsIWeakReference
+{
+public:
+	nsJavaXPTCStubWeakRef(JNIEnv* env, jobject aJavaObject);
+	virtual ~nsJavaXPTCStubWeakRef();
+	NS_DECL_ISUPPORTS
+  NS_DECL_NSIWEAKREFERENCE;
+
+protected:
+  JNIEnv* mJavaEnv;
+  jweak   mWeakRef;
+};
+
+#endif // _nsJavaXPTCStubWeakRef_h_
