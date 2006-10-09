49561096a42c196c6a2ff89999398a04b4b55ef9
Olli.Pettay@helsinki.fi <Olli.Pettay@helsinki.fi>
2006-10-09 17:58:11 +0000

    Bug 356007, Crashes [@ nsFormFillController::OnTextEntered], r=bryner

--- toolkit/components/satchel/src/nsFormFillController.cpp
+++ toolkit/components/satchel/src/nsFormFillController.cpp
@@ -438,36 +438,34 @@ nsFormFillController::OnSearchComplete()
 }
 
 NS_IMETHODIMP
-nsFormFillController::OnTextEntered(PRBool *_retval)
+nsFormFillController::OnTextEntered(PRBool* aPrevent)
 {
+  NS_ENSURE_ARG(aPrevent);
+  NS_ENSURE_TRUE(mFocusedInput, NS_OK);
   // Fire off a DOMAutoComplete event
   nsCOMPtr<nsIDOMDocument> domDoc;
   mFocusedInput->GetOwnerDocument(getter_AddRefs(domDoc));
 
   nsCOMPtr<nsIDOMDocumentEvent> doc = do_QueryInterface(domDoc);
+  NS_ENSURE_STATE(doc);
 
   nsCOMPtr<nsIDOMEvent> event;
   doc->CreateEvent(NS_LITERAL_STRING("Events"), getter_AddRefs(event));
-  if (!event) {
-    NS_ERROR("Could not create DOM Event");
-    return NS_ERROR_FAILURE;
-  }
+  nsCOMPtr<nsIPrivateDOMEvent> privateEvent(do_QueryInterface(event));
+  NS_ENSURE_STATE(privateEvent);
 
   event->InitEvent(NS_LITERAL_STRING("DOMAutoComplete"), PR_TRUE, PR_TRUE);
 
-  nsCOMPtr<nsIPrivateDOMEvent> privateEvent(do_QueryInterface(event));
-  if (privateEvent) {
-    // XXXjst: We mark this event as a trusted event, it's up to the
-    // callers of this to ensure that it's only called from trusted
-    // code.
-    privateEvent->SetTrusted(PR_TRUE);
-  }
+  // XXXjst: We mark this event as a trusted event, it's up to the
+  // callers of this to ensure that it's only called from trusted
+  // code.
+  privateEvent->SetTrusted(PR_TRUE);
 
   nsCOMPtr<nsIDOMEventTarget> targ = do_QueryInterface(mFocusedInput);
 
   PRBool defaultActionEnabled;
   targ->DispatchEvent(event, &defaultActionEnabled);
-
+  *aPrevent = !defaultActionEnabled;
   return NS_OK;
 }
 
