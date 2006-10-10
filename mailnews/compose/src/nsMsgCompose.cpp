48d4604b0979ac7d6fc973bc568ec32ecce33292
gavin%gavinsharp.com <gavin%gavinsharp.com>
2006-10-10 13:15:44 +0000

    Bug 278663: cite attribute of blockquote misses colon in URL, patch by Magnus Melin <mkmelin+mozilla@iki.fi>, r+sr=bienvenu

--- mailnews/compose/src/nsMsgCompose.cpp
+++ mailnews/compose/src/nsMsgCompose.cpp
@@ -1981,7 +1981,7 @@ QuotingOutputStreamListener::QuotingOutputStreamListener(const char * originalMs
         if (!myGetter.IsEmpty())
         {
           nsCAutoString buf;
-          mCiteReference.AssignLiteral("mid");
+          mCiteReference.AssignLiteral("mid:");
           AppendASCIItoUTF16(NS_EscapeURL(myGetter, esc_FileBaseName | esc_Forced, buf),
                              mCiteReference);
         }
