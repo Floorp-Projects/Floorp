92d75f6d9f6e8c159a0fc6b2cf271b4175ca456d
gavin@gavinsharp.com <gavin@gavinsharp.com>
2006-10-13 03:11:49 +0000

    Bug 356083: Incorrect decompilation for ({this setter: function () { } }), patch by Brian Crowder <crowder@fiverocks.com>, r=brendan

--- js/src/jsopcode.c
+++ js/src/jsopcode.c
@@ -3860,7 +3860,9 @@ Decompile(SprintStack *ss, jsbytecode *pc, intN nb)
                               rval);
 #else
                 if (lastop == JSOP_GETTER || lastop == JSOP_SETTER) {
-                    if (strncmp(rval, js_function_str, 8) || rval[8] != ' ') {
+                    if (!ATOM_IS_IDENTIFIER(atom) ||
+                        strncmp(rval, js_function_str, 8) ||
+                        rval[8] != ' ') {
                         todo = Sprint(&ss->sprinter, "%s%s%s%s%s:%s", lval,
                                       (lval[1] != '\0') ? ", " : "", xval,
                                       (lastop == JSOP_GETTER ||
