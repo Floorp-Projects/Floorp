ddeec62efb67c93a40e385379e8a233f5129b437
ccooper%deadsquid.com <ccooper%deadsquid.com>
2006-10-20 20:11:53 +0000

    - point at blat

--- tools/tinderbox-configs/xulrunner/win32/tinder-config.pl
+++ tools/tinderbox-configs/xulrunner/win32/tinder-config.pl
@@ -124,7 +124,7 @@ $Make          = 'make';       # Must be GNU make
 #$CVSCO         = 'checkout -P';
 
 # win32 usually doesn't have /bin/mail
-$blat           = 'c:/moztools/bin/blat';
+$blat           = 'd:/moztools/bin/blat.exe';
 $use_blat       = 1;
 
 # Set moz_cvsroot to something like:
