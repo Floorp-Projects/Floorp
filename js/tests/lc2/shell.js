d186d0067710bbabf641699f29048d4a9c3d955d
cbiesinger%web.de <cbiesinger%web.de>
2006-10-20 09:58:28 +0000

    Bug 341270 use the pref service's contract ID for nsIPrefService/nsIPrefBranch, instead of the nsPref one patch by Ryan Jones <sciguyryan+bugzilla@gmail.com> r=bclary

--- js/tests/lc2/shell.js
+++ js/tests/lc2/shell.js
@@ -316,10 +316,10 @@ function JavaScriptOptions()
     // browser
     netscape.security.PrivilegeManager.enablePrivilege(this.privileges);
 
-    var preferences = Components.classes['@mozilla.org/preferences;1'];
+    var preferences = Components.classes['@mozilla.org/preferences-service;1'];
     if (!preferences)
     {
-      throw 'JavaScriptOptions: unable to get @mozilla.org/preference;1';
+      throw 'JavaScriptOptions: unable to get @mozilla.org/preferences-service;1';
     }
 
     var prefService = preferences.
@@ -373,10 +373,10 @@ function (optionName, optionValue)
     // browser
     netscape.security.PrivilegeManager.enablePrivilege(this.privileges);
 
-    var preferences = Components.classes['@mozilla.org/preferences;1'];
+    var preferences = Components.classes['@mozilla.org/preferences-service;1'];
     if (!preferences)
     {
-      throw 'setOption: unable to get @mozilla.org/preference;1';
+      throw 'setOption: unable to get @mozilla.org/preferences-service;1';
     }
 
     var prefService = preferences.
