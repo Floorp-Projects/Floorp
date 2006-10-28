438f44ddc9126cefb09c8a8ca4011a59d9d2691c
sayrer@gmail.com <sayrer@gmail.com>
2006-10-28 21:46:33 +0000

    Add xul mime type to server.py

--- testing/mochitest/server.py
+++ testing/mochitest/server.py
@@ -9,6 +9,7 @@ PORT = 8888
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 Handler.extensions_map[".xhtml"] = "application/xhtml+xml"
 Handler.extensions_map[".svg"] = "image/svg+xml"
+Handler.extensions_map[".xul"] = "application/vnd.mozilla.xul+xml"
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 print "serving at port", PORT
