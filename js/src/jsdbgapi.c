1a9c312b037564c56dafba1e4f6ae9cb609ef907
mrbkap@gmail.com <mrbkap@gmail.com>
2006-11-07 05:09:53 +0000

    Set up the pseudo frame more carefully. bug 354978, r=brendan

--- js/src/jsdbgapi.c
+++ js/src/jsdbgapi.c
@@ -366,12 +366,14 @@ js_watch_set(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
                  */
                 JSObject *funobj = (JSObject *) wp->closure;
                 JSFunction *fun = (JSFunction *) JS_GetPrivate(cx, funobj);
+                jsval argv[2] = { OBJECT_TO_JSVAL(funobj), JSVAL_NULL };
                 JSStackFrame frame;
 
                 memset(&frame, 0, sizeof(frame));
                 frame.script = FUN_SCRIPT(fun);
                 frame.fun = fun;
                 frame.down = cx->fp;
+                frame.argv = argv + 2;
                 cx->fp = &frame;
                 ok = !wp->setter ||
                      ((sprop->attrs & JSPROP_SETTER)
