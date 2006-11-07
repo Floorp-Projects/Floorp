0de8aa2fe67d0f5f8b2badad48700c425504997b
sayrer@gmail.com <sayrer@gmail.com>
2006-11-07 00:56:50 +0000

    Add XUL and XHTML templates, add handlers for gen_template.pl

--- testing/mochitest/gen_template.pl
+++ testing/mochitest/gen_template.pl
@@ -2,15 +2,28 @@
 #
 #  gen_template.pl
 #  Makes test case templates.
-#  Takes one argument: a bug number
+#  Takes two arguments:
 #
-#  perl gen_template.pl 345876
+#  -b : a bugnumber
+#  -type : template type. {html|xhtml|xul}. defaults to html.  
+#
+#  perl gen_template.pl -b 345876 -type xul
 #
 #  sends a test case template for bug 345876 to stdout
+use Switch;
+use Getopt::Long;
+GetOptions("b=i"=> \$bug_number,
+           "type:s"=> \$template_type);
 
-$bug_number = $ARGV[0];
+if ($template_type == "xul") {
+  $template_type = "static/xul.template.txt";
+} elsif ($template_type == "xhtml") {
+  $template_type = "static/xhtml.template.txt";
+} else {
+  $template_type = "static/test.template.txt";
+}
 
-open(IN,"static/test.template.txt") or die("Failed to open myfile for reading.");
+open(IN,$template_type) or die("Failed to open myfile for reading.");
 while((defined(IN)) && ($line = <IN>)) {
         $line =~ s/{BUGNUMBER}/$bug_number/g;
         print STDOUT $line;
