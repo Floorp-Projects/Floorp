4716aa4d176efd74794fe987bc45e424cf62a747
michael.buettner%sun.com <michael.buettner%sun.com>
2006-11-13 12:01:47 +0000

    Bug #357147 cloned proxies forget about attendee list r=lilmatt

--- calendar/base/src/calItemBase.js
+++ calendar/base/src/calItemBase.js
@@ -653,9 +653,11 @@ calItemBase.prototype = {
 
         if (this.mOrganizer)
             icalcomp.addProperty(this.mOrganizer.icalProperty);
-        if (this.mAttendees) {
-            for (var i = 0; i < this.mAttendees.length; i++)
-                icalcomp.addProperty(this.mAttendees[i].icalProperty);
+        var attendees = this.getAttendees({});
+        if (attendees.length > 0) {
+          for (var i = 0; i < attendees.length; i++) {
+            icalcomp.addProperty(attendees[i].icalProperty);
+          }
         }
 
         if (this.mGeneration) {
