3ef9f3a54860e8481056828a2b9bf630d8347900
mconnor%steelgryphon.com <mconnor%steelgryphon.com>
2006-11-22 02:21:20 +0000

    adding Gran Paradiso branding directory, NPOTB and non-working for now

---
+++ browser/branding/unofficial/pref/firefox-branding.js
@@ -0,0 +1,16 @@
+pref("startup.homepage_override_url","http://www.mozilla.org/projects/%APP%/%VERSION%/whatsnew/");
+pref("startup.homepage_welcome_url","http://www.mozilla.org/projects/%APP%/%VERSION%/firstrun/");
+// URL user can browse to manually if for some reason all update installation
+// attempts fail.
+pref("app.update.url.manual", "http://www.mozilla.org/products/%APP%/");
+// A default value for the "More information about this update" link
+// supplied in the "An update is available" page of the update wizard. 
+pref("app.update.url.details", "http://www.mozilla.org/projects/%APP%/");
+
+// Release notes URL
+pref("app.releaseNotesURL", "http://www.mozilla.org/projects/%APP%/%VERSION%/releasenotes/");
+
+// Search codes belong only in builds with official branding
+pref("browser.search.param.yahoo-fr", "");
+pref("browser.search.param.yahoo-fr-cjkt", "");
+pref("browser.search.param.yahoo-f-CN", "");
