cf1e09c384c11d10e11cb437b1682439489c8f2a
hwaara@gmail.com <hwaara@gmail.com>
2006-11-24 14:37:00 +0000

    expose the "mixed" value of checkbox, and add license headers to these files. r=surkov, b=361736

--- accessible/src/mac/mozActionElements.mm
+++ accessible/src/mac/mozActionElements.mm
@@ -1,8 +1,53 @@
+/* -*- Mode: Objective-C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2006
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Original Author: Håkan Waara <hwaara@gmail.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 #import "mozActionElements.h"
 #import "nsIAccessible.h"
 
 extern const NSString *kInstanceDescriptionAttribute; // NSAccessibilityDescriptionAttribute
 extern const NSString *kTopLevelUIElementAttribute;   // NSAccessibilityTopLevelUIElementAttribute
+
+enum CheckboxValue {
+  // these constants correspond to the values in the OS
+  kUnchecked = 0,
+  kChecked = 1,
+  kMixed = 2
+};
 
 @implementation mozButtonAccessible
 
@@ -73,7 +118,7 @@ extern const NSString *kTopLevelUIElementAttribute;   // NSAccessibilityTopLevel
 - (NSString*)accessibilityActionDescription:(NSString*)action 
 {
   if ([action isEqualToString:NSAccessibilityPressAction]) {
-    if ([self isChecked])
+    if ([self isChecked] != kUnchecked)
       return @"uncheck checkbox"; // XXX: localize this later?
     
     return @"check checkbox"; // XXX: localize this later?
@@ -82,17 +127,22 @@ extern const NSString *kTopLevelUIElementAttribute;   // NSAccessibilityTopLevel
   return nil;
 }
 
-- (BOOL)isChecked
+- (int)isChecked
 {
   PRUint32 state = 0;
   mGeckoAccessible->GetState(&state);
-  return ((state & nsIAccessible::STATE_CHECKED) != 0);
+  
+  // check if we're checked or in a mixed state
+  if (state & nsIAccessible::STATE_CHECKED) {
+    return (state & nsIAccessible::STATE_MIXED) ? kMixed : kChecked;
+  }
+  
+  return kUnchecked;
 }
 
 - (id)value
 {
-  // need to handle mixed state here too (return 2)
-  return [NSNumber numberWithInt:([self isChecked] ? 1 : 0)];
+  return [NSNumber numberWithInt:[self isChecked]];
 }
 
 @end
