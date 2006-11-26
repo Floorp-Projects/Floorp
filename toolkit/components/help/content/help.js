97cb9cf2e746c0466f123ec31c96b7d4e91f7cfb
mozilla.mano@sent.com <mozilla.mano@sent.com>
2006-11-26 22:47:46 +0000

    Bug 288254 - Findbar XBL widget. r=gavin,masayuki,bienvenu.

--- toolkit/components/help/content/help.js
+++ toolkit/components/help/content/help.js
@@ -140,8 +140,6 @@ function init() {
   strBundle = document.getElementById("bundle_help");
   emptySearchText = strBundle.getString("emptySearchText");
 
-  gFindBar.initFindBar();
-
   // Get the content pack, base URL, and help topic
   var helpTopic = defaultTopic;
   if ("arguments" in window && 
