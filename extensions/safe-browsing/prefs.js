e4ec1b41e9fd758fecba597bffc4e01691a70075
tony@ponderer.org <tony@ponderer.org>
2006-12-03 08:29:45 +0000

    Bug 337336: Remove safebrowsing code from /extensions/safebrowsing part 2, actually remove all the safe browsing files r=bsmedbergs

--- extensions/safe-browsing/prefs.js
+++
@@ -1,4 +0,0 @@
-// We want to be off by default
-pref("extensions.safebrowsing.enabled", false);
-// But when we turn on, we want advanced protection
-pref("extensions.safebrowsing.advancedprotection", true);
