f8a250145b18823ee9fa67053c96598e5411c73d
timeless%mozdev.org <timeless%mozdev.org>
2006-12-06 03:00:09 +0000

    Bug 322806 ARM crash [@ XPTC_InvokeByIndex] patch by apm@dna.fi eabi arm assembly didn't warn that a variable was a memory operand so the newer gcc compilers (e.g. gcc4) started optimizing things away...
    I tried to get someone to vouch for this code but the people I poked
    never got back to me.
    
    moa=timeless

--- xpcom/reflect/xptcall/src/md/unix/xptcinvoke_arm.cpp
+++ xpcom/reflect/xptcall/src/md/unix/xptcinvoke_arm.cpp
@@ -212,7 +212,7 @@ XPTC_InvokeByIndex(nsISupports* that, PRUint32 methodIndex,
     "add	sp, sp, r4	\n\t"	/* restore stack pointer		*/
     "mov	%0, r0		\n\t"	/* the result...			*/
     : "=r" (result)
-    : "r" (&my_params)
+    : "r" (&my_params), "m" (my_params)
     : "r0", "r1", "r2", "r3", "r4", "ip", "lr", "sp"
     );
     
