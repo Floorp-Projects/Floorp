fb6bef7f39aafd0d87a54b5f83fa17a06133f596
bclary%bclary.com <bclary%bclary.com>
2006-12-08 21:08:09 +0000

    Bug 309169 - update JS_GetImplementationVersion, r=brendan

--- js/src/jsapi.c
+++ js/src/jsapi.c
@@ -1106,7 +1106,7 @@ JS_ToggleOptions(JSContext *cx, uint32 options)
 JS_PUBLIC_API(const char *)
 JS_GetImplementationVersion(void)
 {
-    return "JavaScript-C 1.7 pre-release 1 2006-04-04";
+    return "JavaScript-C 1.7 pre-release 2 2006-11-19";
 }
 
 
