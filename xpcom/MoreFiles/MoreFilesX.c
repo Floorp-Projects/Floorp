c57afc54421dd2b69cb2b391f0c13fca1487ada7
joshmoz@gmail.com <joshmoz@gmail.com>
2006-12-08 07:52:18 +0000

    Don't compile FSVolumeMount in MoreFilesX. It exists on Mac OS X 10.5 now, we can't re-define it in MoreFilesX. Since we don't use it anyway, just don't ever compile it. b=363092 r=mento sr=wtc

--- xpcom/MoreFiles/MoreFilesX.c
+++ xpcom/MoreFiles/MoreFilesX.c
@@ -2316,6 +2316,9 @@ BadParameter:
 
 /*****************************************************************************/
 
+// This function exists in Mac OS X 10.5, we cannot re-define it here.
+// We don't use this function, so just don't compile it.
+#if 0
 OSErr
 FSVolumeMount(
 	const void *volMountInfo,
@@ -2339,6 +2342,7 @@ BadParameter:
 
 	return ( result );
 }
+#endif
 
 /*****************************************************************************/
 
