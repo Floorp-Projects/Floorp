069dc4a90c1bcff377eba2355f206e78cc62c2a1
mattwillis%gmail.com <mattwillis%gmail.com>
2006-12-13 18:34:08 +0000

    Fix lightning's CVSROOT

--- tools/tinderbox-configs/lightning/macosx/tinder-config.pl
+++ tools/tinderbox-configs/lightning/macosx/tinder-config.pl
@@ -132,8 +132,7 @@ $ProductName              = "Thunderbird";
 # :pserver:$ENV{USER}%netscape.com@cvs.mozilla.org:/cvsroot
 
 #$moz_cvsroot   = $ENV{CVSROOT};
-#$moz_cvsroot   = ":ext:cltbld\@cvs.mozilla.org:/cvsroot";
-$moz_cvsroot   = ":pserver:anonymous\@cvs-mirror.mozilla.org:/cvsroot";
+$moz_cvsroot   = ':ext:cltbld@cvs.mozilla.org:/cvsroot';
 
 #- Set these proper values for your tinderbox server
 #$Tinderbox_server = 'tinderbox-daemon@tinderbox.mozilla.org';
