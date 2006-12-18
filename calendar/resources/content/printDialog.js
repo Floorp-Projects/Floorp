271f95870f5ef8d0ee5ac0f08273b214243ab951
daniel.boelzle%sun.com <daniel.boelzle%sun.com>
2006-12-18 10:51:09 +0000

    Bug 361057: minding asynchronous provider responses

--- calendar/resources/content/printDialog.js
+++ calendar/resources/content/printDialog.js
@@ -90,76 +90,66 @@ function unloadCalendarPrintDialog()
 
 /**
  * Gets the settings from the dialog's UI widgets.
- * @returns an Object with title, layoutCId, eventList, start, and end
+ * notifies an Object with title, layoutCId, eventList, start, and end
  *          properties containing the appropriate values.
  */
-function getDialogSettings()
+function getEventsAndDialogSettings(receiverFunc)
 {
     var settings = new Object();
-    var ccalendar = window.opener.getCompositeCalendar();
+    var tempTitle = document.getElementById("title-field").value;
+    settings.title = (tempTitle || calGetString("calendar", "Untitled"));
+    settings.layoutCId = document.getElementById("layout-field").value;
+    settings.start = null;
+    settings.end = null;
+    settings.eventList = null;
 
-    var listener = {
-        mEventArray: new Array(),
-
-        onOperationComplete: function (aCalendar, aStatus, aOperationType, aId, aDateTime) {
-            settings.eventList = listener.mEventArray;
-            settings.start = start;
-            settings.end = end;
-        },
-
-        onGetResult: function (aCalendar, aStatus, aItemType, aDetail, aCount, aItems) {
-            for (var i = 0; i < aCount; i++) {
-                listener.mEventArray.push(aItems[i]);
-            }
-        }
-    };
-
-    var filter = ccalendar.ITEM_FILTER_TYPE_EVENT | 
-                 ccalendar.ITEM_FILTER_CLASS_OCCURRENCES;
-
-    var start;
-    var end;
-    var eventList;
     var theView = window.opener.currentView();
     switch (document.getElementById("view-field").selectedItem.value) {
-
         case 'currentview':
         case '': //just in case
-            start = theView.startDay;
-            end   = theView.endDay;
+            settings.start = theView.startDay;
+            settings.end   = theView.endDay;
             break;
         case 'selected':
-            eventList = theView.getSelectedItems({});
+            settings.eventList = theView.getSelectedItems({});
             break;
         case 'custom':
             // We return the time from the timepickers using the selected
             // timezone, as not doing so in timezones with a positive offset
             // from UTC may cause the printout to include the wrong days.
             var currentTimezone = calendarDefaultTimezone();
-            start = jsDateToDateTime(document.getElementById("start-date-picker").value);
-            start = start.getInTimezone(currentTimezone);
-            end   = jsDateToDateTime(document.getElementById("end-date-picker").value);
-            end   = end.getInTimezone(currentTimezone);
+            settings.start = jsDateToDateTime(document.getElementById("start-date-picker").value);
+            settings.start = settings.start.getInTimezone(currentTimezone);
+            settings.end   = jsDateToDateTime(document.getElementById("end-date-picker").value);
+            settings.end   = settings.end.getInTimezone(currentTimezone);
             break ;
         default :
             dump("Error : no case in printDialog.js::printCalendar()");
     }
-    if (!eventList) {
-        // end isn't exclusive, so we need to add one day
-        end = end.clone();
-        end.day = end.day + 1;
-        end.normalize();
-        ccalendar.getItems(filter, 0, start, end, listener);
-    } else {
-        settings.eventList = eventList;
-        settings.start = null;
-        settings.end = null;
-    }
 
-    var tempTitle = document.getElementById("title-field").value;
-    settings.title = (tempTitle || calGetString("calendar", "Untitled"));
-    settings.layoutCId = document.getElementById("layout-field").value;
-    return settings;
+    if (settings.eventList) {
+        receiverFunc(settings);
+    } else {
+        // end isn't exclusive, so we need to add one day
+        settings.end = settings.end.clone();
+        settings.end.day = settings.end.day + 1;
+        settings.end.normalize();
+        settings.eventList = [];
+        var listener = {
+            onOperationComplete:
+            function onOperationComplete(aCalendar, aStatus, aOperationType, aId, aDateTime) {
+                receiverFunc(settings);
+            },
+            onGetResult:
+            function onGetResult(aCalendar, aStatus, aItemType, aDetail, aCount, aItems) {
+                settings.eventList = settings.eventList.concat(aItems);
+            }
+        };
+        window.opener.getCompositeCalendar().getItems(
+            Components.interfaces.calICalendar.ITEM_FILTER_TYPE_EVENT | 
+            Components.interfaces.calICalendar.ITEM_FILTER_CLASS_OCCURRENCES,
+            0, settings.start, settings.end, listener);
+    }
 }
 
 /**
@@ -169,49 +159,52 @@ function getDialogSettings()
  */
 function refreshHtml()
 {
-    var settings = getDialogSettings();
+    getEventsAndDialogSettings(
+        function getEventsAndDialogSettings_response(settings) {
+            // calGetString can't do "formatStringFromName".
+            var sbs = Components.classes["@mozilla.org/intl/stringbundle;1"]
+                                .getService(Components.interfaces.nsIStringBundleService);
+            var props = sbs.createBundle("chrome://calendar/locale/calendar.properties");
+            document.title = props.formatStringFromName("PrintPreviewWindowTitle", [settings.title], 1);
 
-    // I'd like to use calGetString, but it can't do "formatStringFromName".
-    var sbs = Components.classes["@mozilla.org/intl/stringbundle;1"]
-                        .getService(Components.interfaces.nsIStringBundleService);
+            var printformatter = Components.classes[settings.layoutCId]
+                                           .createInstance(Components.interfaces.calIPrintFormatter);
 
-    var props = sbs.createBundle("chrome://calendar/locale/calendar.properties");
-    document.title = props.formatStringFromName("PrintPreviewWindowTitle",
-                                                [settings.title], 1);
+            // Fail-safe check to not init twice, to prevent leaking files
+            if (gTempFile) {
+                gTempFile.remove(false);
+            }
+            const nsIFile = Components.interfaces.nsIFile;
+            var dirService = Components.classes["@mozilla.org/file/directory_service;1"]
+                                       .getService(Components.interfaces.nsIProperties);
+            gTempFile = dirService.get("TmpD", nsIFile);
+            gTempFile.append("calendarPrint.html");
+            gTempFile.createUnique(nsIFile.NORMAL_FILE_TYPE, 0600); // 0600 = -rw-------
+            var ioService = Components.classes["@mozilla.org/network/io-service;1"]
+                                      .getService(Components.interfaces.nsIIOService);
+            var tempUri = ioService.newFileURI(gTempFile); 
 
-    var printformatter = Components.classes[settings.layoutCId]
-                                   .createInstance(Components.interfaces.calIPrintFormatter);
+            var stream = Components.classes["@mozilla.org/network/file-output-stream;1"]
+                                   .createInstance(Components.interfaces.nsIFileOutputStream);
 
-    // Fail-safe check to not init twice, to prevent leaking files
-    if (gTempFile) {
-        gTempFile.remove(false);
-    }
-    const nsIFile = Components.interfaces.nsIFile;
-    var dirService = Components.classes["@mozilla.org/file/directory_service;1"]
-                               .getService(Components.interfaces.nsIProperties);
-    gTempFile = dirService.get("TmpD", nsIFile);
-    gTempFile.append("calendarPrint.html");
-    gTempFile.createUnique(nsIFile.NORMAL_FILE_TYPE, 0600); // 0600 = -rw-------
-    var ioService = Components.classes["@mozilla.org/network/io-service;1"]
-                              .getService(Components.interfaces.nsIIOService);
-    var tempUri = ioService.newFileURI(gTempFile); 
+            try {
+                // 0x2A = PR_TRUNCATE, PR_CREATE_FILE, PR_WRONLY
+                // 0600 = -rw-------
+                stream.init(gTempFile, 0x2A, 0600, 0);
+                printformatter.formatToHtml(stream,
+                                            settings.start,
+                                            settings.end,
+                                            settings.eventList.length,
+                                            settings.eventList,
+                                            settings.title);
+                stream.close();
+            } catch (e) {
+                dump("printDialog::refreshHtml:" + e + "\n");
+            }
 
-    var stream = Components.classes["@mozilla.org/network/file-output-stream;1"]
-                           .createInstance(Components.interfaces.nsIFileOutputStream);
-
-    try {
-        // 0x2A = PR_TRUNCATE, PR_CREATE_FILE, PR_WRONLY
-        // 0600 = -rw-------
-        stream.init(gTempFile, 0x2A, 0600, 0);
-        printformatter.formatToHtml(stream, settings.start, settings.end,
-                                    settings.eventList.length,
-                                    settings.eventList, settings.title);
-        stream.close();
-    } catch (e) {
-        dump("printDialog::refreshHtml:"+e+"\n");
-    }
-
-    document.getElementById("content").contentWindow.location = tempUri.spec;
+            document.getElementById("content").contentWindow.location = tempUri.spec;
+        }
+    );
 }
 
 /**
