444ac56f568e3fc09d0eab8d2e79c8b870098d1c
ginn.chen@sun.com <ginn.chen@sun.com>
2006-12-18 07:02:03 +0000

    Bug 359790 set text doesn't work correctly for XUL entry patch by nian.liu at sun.com r=ginn.chen

--- accessible/src/html/nsHyperTextAccessible.cpp
+++ accessible/src/html/nsHyperTextAccessible.cpp
@@ -1357,10 +1357,16 @@ nsresult nsHyperTextAccessible::GetSelections(nsISelectionController **aSelCon, 
   
   nsCOMPtr<nsIEditor> editor = GetEditor();
   if (editor) {
-    editor->GetSelectionController(aSelCon);
-    NS_ENSURE_TRUE(aSelCon, NS_ERROR_FAILURE);
-    editor->GetSelection(aDomSel);
-    NS_ENSURE_TRUE(aDomSel, NS_ERROR_FAILURE);
+    if (aSelCon) {
+      editor->GetSelectionController(aSelCon);
+      NS_ENSURE_TRUE(aSelCon, NS_ERROR_FAILURE);
+    }
+
+    if (aDomSel) {
+      editor->GetSelection(aDomSel);
+      NS_ENSURE_TRUE(aDomSel, NS_ERROR_FAILURE);
+    }
+
     return NS_OK;
   }
 
