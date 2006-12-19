180767e149eb28d443df1737f000970fa587475d
bzbarsky@mit.edu <bzbarsky@mit.edu>
2006-12-19 15:32:46 +0000

    Fix bug 363235 -- REJECT_SERVER is taken to mean "content is blocked by user", and that's not what we want here.  r+sr=sicking

--- content/base/src/nsNoDataProtocolContentPolicy.cpp
+++ content/base/src/nsNoDataProtocolContentPolicy.cpp
@@ -95,7 +95,7 @@ nsNoDataProtocolContentPolicy::ShouldLoad(PRUint32 aContentType,
       do_QueryInterface(handler);
 
     if (extHandler) {
-      *aDecision = nsIContentPolicy::REJECT_SERVER;
+      *aDecision = nsIContentPolicy::REJECT_REQUEST;
     }
   }
 
