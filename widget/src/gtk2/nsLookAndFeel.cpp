1a49158efbcce6dccd2e509503ef3912a36f3d57
neil%parkwaycc.co.uk <neil%parkwaycc.co.uk>
2006-12-23 23:32:09 +0000

    Back out bug 350766's scrollbar metrics r+sr=roc

--- widget/src/gtk2/nsLookAndFeel.cpp
+++ widget/src/gtk2/nsLookAndFeel.cpp
@@ -509,14 +509,6 @@ NS_IMETHODIMP nsLookAndFeel::GetMetric(const nsMetricID aID, PRInt32 & aMetric)
     case eMetric_ScrollSliderStyle:
         aMetric = eMetric_ScrollThumbStyleProportional;
         break;
-    case eMetric_ScrollbarWidth:
-    case eMetric_ScrollbarHeight:
-        {
-            MozGtkScrollbarMetrics metrics;
-            moz_gtk_get_scrollbar_metrics(&metrics);
-            aMetric = metrics.stepper_size;
-        }
-        break;
     case eMetric_TreeOpenDelay:
         aMetric = 1000;
         break;
