8dfe5d455029f5f69171bdcb42f9f7ea40952480
rhelmer@mozilla.com <rhelmer@mozilla.com>
2007-01-03 17:55:13 +0000

    add check for read-only files r=tfullhart sr=rcampbell

--- testing/release/l10n/verify_l10n.sh
+++ testing/release/l10n/verify_l10n.sh
@@ -15,11 +15,15 @@ do
   rm -rf source/*
   # unpack_build platform dir_name pkg_file
   unpack_build $platform source $release/*.en-US.${platform}.*
+  # check for read-only files
+  find "./source" -not -perm -u=w -exec echo "FAIL read-only file" {} \;
   locales=`ls $release/*.${platform}.* | grep -v en-US | cut -d\. -f5`
   for locale in $locales
   do
     rm -rf target/*
     unpack_build ${platform} target $release/*.${locale}.${platform}.*
+    # check for read-only files
+    find "./target" -not -perm -u=w -exec echo "FAIL read-only file" {} \;
     mkdir -p $release/diffs
     diff -r source target > $release/diffs/${platform}.${locale}.diff
   done
