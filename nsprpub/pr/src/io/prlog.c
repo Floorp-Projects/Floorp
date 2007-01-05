0ccd39fbd279799e3c5889a2e65b648de05cf794
wtchang%redhat.com <wtchang%redhat.com>
2007-01-05 22:26:27 +0000

    Bug 365703: turn off logging and tracing if the effective group ID is different from the real group ID.  The patch is suggested by Mike Hommey <mh+mozilla@glandium.org>.  r=nelsonb,wtc. Modified files: prlog.c prtrace.c

--- nsprpub/pr/src/io/prlog.c
+++ nsprpub/pr/src/io/prlog.c
@@ -256,7 +256,7 @@ void _PR_InitLog(void)
         PR_SetLogBuffering(isSync ? bufSize : 0);
 
 #ifdef XP_UNIX
-        if (getuid() != geteuid()) {
+        if (getuid() != geteuid() || getgid() != getegid()) {
             return;
         }
 #endif /* XP_UNIX */
