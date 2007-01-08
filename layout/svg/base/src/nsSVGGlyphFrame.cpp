d9e328002774f6c688b4de401edf217211ca0ba3
longsonr@gmail.com <longsonr@gmail.com>
2007-01-08 10:10:13 +0000

    Bug 358032 - SVG text too big when minimum font size set. r=tor, sr=roc

--- layout/svg/base/src/nsSVGGlyphFrame.cpp
+++ layout/svg/base/src/nsSVGGlyphFrame.cpp
@@ -1249,7 +1249,8 @@ void nsSVGGlyphFrame::SelectFont(gfxContext *aContext)
 {
   cairo_t *ctx = aContext->GetCairo();
 
-  nsFont font = GetStyleFont()->mFont;
+  const nsStyleFont* fontData = GetStyleFont();
+  nsFont font = fontData->mFont;
 
   // XXX eventually we will have to treat decorations separately from
   // fonts, because they can have a different color than the current
@@ -1306,7 +1307,7 @@ void nsSVGGlyphFrame::SelectFont(gfxContext *aContext)
   presContext->DeviceContext()->GetCanonicalPixelScale(pixelScale);
   float textZoom = presContext->TextZoom();
 
-  cairo_set_font_size(ctx, font.size * pxPerTwips / pixelScale / textZoom);
+  cairo_set_font_size(ctx, fontData->mSize * pxPerTwips / pixelScale / textZoom);
 }
 
 void nsSVGGlyphFrame::UpdateGeometry(PRBool bRedraw,
