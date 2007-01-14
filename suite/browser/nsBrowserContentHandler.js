4aa67134e8bd80a66da70d5a84c891054314939b
neil%parkwaycc.co.uk <neil%parkwaycc.co.uk>
2007-01-14 12:33:27 +0000

    Typo'd my own name, of course I blame the reviewers for not spotting it ;-)

--- suite/browser/nsBrowserContentHandler.js
+++ suite/browser/nsBrowserContentHandler.js
@@ -20,7 +20,7 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *   Neil Rashbook <neil@parkwaycc.co.uk>
+ *   Neil Rashbrook <neil@parkwaycc.co.uk>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
