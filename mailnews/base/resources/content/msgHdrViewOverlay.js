a25e0b1d09941ec6751a0b566dc0c9cf16e48a68
neil%parkwaycc.co.uk <neil%parkwaycc.co.uk>
2007-01-23 13:56:00 +0000

    Bug 365362 msgHdrSink's properties is null r=mscott

--- mailnews/base/resources/content/msgHdrViewOverlay.js
+++ mailnews/base/resources/content/msgHdrViewOverlay.js
@@ -502,31 +502,31 @@ var messageHeaderSink = {
 
     mSecurityInfo  : null,
     mSaveHdr: null,
-    getSecurityInfo: function()
+    get securityInfo()
     {
       return this.mSecurityInfo;
     },
-    setSecurityInfo: function(aSecurityInfo)
+    set securityInfo(aSecurityInfo)
     {
       this.mSecurityInfo = aSecurityInfo;
     },
 
     mDummyMsgHeader: null,
 
-    getDummyMsgHeader: function()
+    get dummyMsgHeader()
     {
       if (!this.mDummyMsgHeader)
         this.mDummyMsgHeader = new nsDummyMsgHeader();
       return this.mDummyMsgHeader;
     },
     mProperties: null,
-    getProperties: function()
-      {
+    get properties()
+    {
       if (!this.mProperties)
         this.mProperties = Components.classes["@mozilla.org/hash-property-bag;1"].
           createInstance(Components.interfaces.nsIWritablePropertyBag2);
       return this.mProperties;
-      }
+    }
 };
 
 // Private method which generates a space delimited list of tag keys for the
