5d4451d39af32a2db3e60497e271e97d4d1a5b8e
justdave%bugzilla.org <justdave%bugzilla.org>
2007-02-02 22:23:23 +0000

    [SECURITY] Bug 367071: mod_perl initialization script forgets to allow .htaccess files to prohibit file access. r=mkanat, a=mkanat

--- webtools/bugzilla/mod_perl.pl
+++ webtools/bugzilla/mod_perl.pl
@@ -56,6 +56,7 @@ my $conf = <<EOT;
     PerlCleanupHandler  Bugzilla::ModPerl::CleanupHandler
     PerlOptions +ParseHeaders
     Options +ExecCGI
+    AllowOverride Limit
     DirectoryIndex index.cgi index.html
 </Directory>
 EOT
