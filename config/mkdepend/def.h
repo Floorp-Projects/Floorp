7a522c0451945bcbd0965507fae49ed852317ccd
cls%seawood.org <cls%seawood.org>
2007-02-06 05:09:08 +0000

    Bump the number of directories that can be passed to mkdepend via -I to 1024. Bug #367860 r=bsmedberg

--- config/mkdepend/def.h
+++ config/mkdepend/def.h
@@ -48,7 +48,7 @@ in this Software without prior written authorization from The Open Group.
 #define MAXDEFINES	512
 #define MAXFILES	1024
 #define MAXINCFILES	128	/* "-include" files */
-#define MAXDIRS		64
+#define MAXDIRS		1024
 #define SYMTABINC	10	/* must be > 1 for define() to work right */
 #define	TRUE		1
 #define	FALSE		0
