127fa1409962d745f695d514298f1b92b9997e2f
ccooper%deadsquid.com <ccooper%deadsquid.com>
2007-02-07 17:19:24 +0000

    b=369604 - turning on codesize tests for SeaMonkey

--- tools/tinderbox-configs/seamonkey/linux/tinder-config.pl
+++ tools/tinderbox-configs/seamonkey/linux/tinder-config.pl
@@ -65,8 +65,8 @@ $RunMozillaTests          = 1;  # Allow turning off of all tests if needed.
 #$BloatTest2               = 0;  # dbaron memory bloat test, require tracemalloc
 #$DomToTextConversionTest  = 0;  
 #$XpcomGlueTest            = 0;
-#$CodesizeTest             = 0;  # Z,  require mozilla/tools/codesighs
-#$EmbedCodesizeTest        = 0;  # mZ, require mozilla/tools/codesigns
+$CodesizeTest             = 1;  # Z,  require mozilla/tools/codesighs
+$EmbedCodesizeTest        = 1;  # mZ, require mozilla/tools/codesigns
 #$MailBloatTest            = 0;
 #$EmbedTest                = 0;  # Assumes you wanted $BuildEmbed=1
 $LayoutPerformanceTest    = 1;  # Tp
