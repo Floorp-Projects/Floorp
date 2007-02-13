9f9ef10f7fabf7c8c94727de24f0fb8444804463
sharparrow1%yahoo.com <sharparrow1%yahoo.com>
2007-02-13 23:00:39 +0000

    Bustage fix for bug 369834.

--- layout/printing/nsPrintEngine.cpp
+++ layout/printing/nsPrintEngine.cpp
@@ -935,7 +935,13 @@ nsPrintEngine::PrintPreview(nsIPrintSettings* aPrintSettings,
   // we want to view every page in PrintPreview each time
   mPrt->mPrintSettings->SetPrintRange(nsIPrintSettings::kRangeAllPages);
 
-  mPrt->mPrintDC = ppDC ? ppDC : mDeviceContext;
+  if (ppDC)
+    mPrt->mPrintDC = ppDC;
+  else
+    mPrt->mPrintDC = mDeviceContext;
+
+  if (!mPrt->mPrintDC)
+    return NS_ERROR_FAILURE;
 
   if (aWebProgressListener != nsnull) {
     mPrt->mPrintProgressListeners.AppendObject(aWebProgressListener);
