742a6a685836fd16257d991d7b90ee5459960b3c
jonas@sicking.cc <jonas@sicking.cc>
2007-02-13 21:18:25 +0000

    Bug 370230: unbreak --enable-perf-metrics. Patch by me and Mats Palmgren. r/sr=jst

--- content/base/src/nsContentSink.h
+++ content/base/src/nsContentSink.h
@@ -61,7 +61,7 @@
 #include "nsIContentSink.h"
 #include "prlog.h"
 #include "nsIRequest.h"
-
+#include "nsTimer.h"
 
 class nsIDocument;
 class nsIURI;
@@ -269,6 +269,9 @@ protected:
   PRInt32 mInMonolithicContainer;
 
   PRInt32 mInNotification;
+
+  // Measures content model creation time for current document
+  MOZ_TIMER_DECLARE(mWatch)
 };
 
 
