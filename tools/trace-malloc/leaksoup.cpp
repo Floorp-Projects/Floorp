4ad1c23ce07cdc23b586147702dc87a09ee2f2b7
dbaron@dbaron.org <dbaron@dbaron.org>
2007-02-14 07:18:26 +0000

    Change my old email address to my new one (files missed when I did a mass-change in January 2003).

--- tools/trace-malloc/leaksoup.cpp
+++ tools/trace-malloc/leaksoup.cpp
@@ -19,7 +19,7 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *   L. David Baron <dbaron@fas.harvard.edu> (original author)
+ *   L. David Baron <dbaron@dbaron.org> (original author)
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
