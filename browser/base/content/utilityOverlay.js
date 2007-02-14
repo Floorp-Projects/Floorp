4653d980b1245eb308363f522a3543e0ab4f350c
mozilla.mano%sent.com <mozilla.mano%sent.com>
2007-02-14 21:28:26 +0000

    Bug 368918 - browser.xul should use editMenuOverlay rather than duplicating its code. r=gavin.

--- browser/base/content/utilityOverlay.js
+++ browser/base/content/utilityOverlay.js
@@ -270,42 +270,6 @@ function closeMenus(node)
   }
 }
 
-// update menu items that rely on focus
-function goUpdateGlobalEditMenuItems()
-{
-  goUpdateCommand('cmd_undo');
-  goUpdateCommand('cmd_redo');
-  goUpdateCommand('cmd_cut');
-  goUpdateCommand('cmd_copy');
-  goUpdateCommand('cmd_paste');
-  goUpdateCommand('cmd_selectAll');
-  goUpdateCommand('cmd_delete');
-  if (gBidiUI)
-    goUpdateCommand('cmd_switchTextDirection');
-}
-
-// update menu items that rely on the current selection
-function goUpdateSelectEditMenuItems()
-{
-  goUpdateCommand('cmd_cut');
-  goUpdateCommand('cmd_copy');
-  goUpdateCommand('cmd_delete');
-  goUpdateCommand('cmd_selectAll');
-}
-
-// update menu items that relate to undo/redo
-function goUpdateUndoEditMenuItems()
-{
-  goUpdateCommand('cmd_undo');
-  goUpdateCommand('cmd_redo');
-}
-
-// update menu items that depend on clipboard contents
-function goUpdatePasteMenuItems()
-{
-  goUpdateCommand('cmd_paste');
-}
-
 // Gather all descendent text under given document node.
 function gatherTextUnder ( root ) 
 {
