33fe39ae2fb001cf9cd17f8f3c76351f47387d26
bclary@bclary.com <bclary@bclary.com>
2007-02-16 03:29:20 +0000

    JavaScript Test - move SpiderMonkey extension ecma_2/RegExp/function-001.js to ecma_2/extensions/function-001.js, bug 368585, r+sr=brendan

--- js/tests/ecma_2/extensions/function-001.js
+++ js/tests/ecma_2/extensions/function-001.js
@@ -28,34 +28,9 @@ RegExp.prototype.getClassProperty = Object.prototype.toString;
 var re = new RegExp();
 
 AddTestCase(
-  "RegExp.prototype.getClassProperty = Object.prototype.toString; " +
-  "(new RegExp()).getClassProperty()",
-  "[object RegExp]",
-  re.getClassProperty() );
-
-AddTestCase(
-  "(new RegExp()).source",
-  "",
-  re.source );
-
-AddTestCase(
-  "(new RegExp()).global",
-  false,
-  re.global );
-
-AddTestCase(
-  "(new RegExp()).ignoreCase",
-  false,
-  re.ignoreCase );
-
-AddTestCase(
-  "(new RegExp()).multiline",
-  false,
-  re.multiline );
-
-AddTestCase(
-  "(new RegExp()).lastIndex",
-  0,
-  re.lastIndex );
+  "new RegExp().__proto__",
+  RegExp.prototype,
+  re.__proto__
+  );
 
 test()
