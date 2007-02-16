08929fd64bab3ac7fc05c224354c169a49260deb
bclary%bclary.com <bclary%bclary.com>
2007-02-16 03:54:41 +0000

    JavaScript Test - move SpiderMonkey extension ecma/Types/8.6.2.1-1.js to ecma/extensions/8.6.2.1-1.js, bug 368585, r+sr=brendan

--- js/tests/ecma/extensions/8.6.2.1-1.js
+++ js/tests/ecma/extensions/8.6.2.1-1.js
@@ -62,15 +62,35 @@ startTest();
 
 writeHeaderToLog( SECTION + " [[Get]] (Value)");
 
-new TestCase( SECTION,  "var OBJ = new MyObject(true); OBJ.valueOf()",              true,           eval("var OBJ = new MyObject(true); OBJ.valueOf()") );
+new TestCase( SECTION,  "var OBJ = new MyValuelessObject(true); OBJ.valueOf()",     true,           eval("var OBJ = new MyValuelessObject(true); OBJ.valueOf()") );
+//    new TestCase( SECTION,  "var OBJ = new MyProtoValuelessObject(true); OBJ + ''",     "undefined",    eval("var OBJ = new MyProtoValuelessObject(); OBJ + ''") );
+new TestCase( SECTION,  "var OBJ = new MyProtolessObject(true); OBJ.valueOf()",     true,           eval("var OBJ = new MyProtolessObject(true); OBJ.valueOf()") );
 
-new TestCase( SECTION,  "var OBJ = new MyObject(Number.POSITIVE_INFINITY); OBJ.valueOf()",              Number.POSITIVE_INFINITY,           eval("var OBJ = new MyObject(Number.POSITIVE_INFINITY); OBJ.valueOf()") );
+new TestCase( SECTION,  "var OBJ = new MyValuelessObject(Number.POSITIVE_INFINITY); OBJ.valueOf()",     Number.POSITIVE_INFINITY,           eval("var OBJ = new MyValuelessObject(Number.POSITIVE_INFINITY); OBJ.valueOf()") );
+//    new TestCase( SECTION,  "var OBJ = new MyProtoValuelessObject(Number.POSITIVE_INFINITY); OBJ + ''",     "undefined",                        eval("var OBJ = new MyProtoValuelessObject(); OBJ + ''") );
+new TestCase( SECTION,  "var OBJ = new MyProtolessObject(Number.POSITIVE_INFINITY); OBJ.valueOf()",     Number.POSITIVE_INFINITY,           eval("var OBJ = new MyProtolessObject(Number.POSITIVE_INFINITY); OBJ.valueOf()") );
 
-new TestCase( SECTION,  "var OBJ = new MyObject('string'); OBJ.valueOf()",              'string',           eval("var OBJ = new MyObject('string'); OBJ.valueOf()") );
+new TestCase( SECTION,  "var OBJ = new MyValuelessObject('string'); OBJ.valueOf()",     'string',           eval("var OBJ = new MyValuelessObject('string'); OBJ.valueOf()") );
+//    new TestCase( SECTION,  "var OBJ = new MyProtoValuelessObject('string'); OJ + ''",     "undefined",      eval("var OBJ = new MyProtoValuelessObject(); OBJ + ''") );
+new TestCase( SECTION,  "var OBJ = new MyProtolessObject('string'); OBJ.valueOf()",     'string',           eval("var OBJ = new MyProtolessObject('string'); OBJ.valueOf()") );
 
 test();
 
-function MyObject( value ) {
+function MyProtoValuelessObject(value) {
+  this.valueOf = new Function ( "" );
+  this.__proto__ = null;
+}
+
+function MyProtolessObject( value ) {
   this.valueOf = new Function( "return this.value" );
+  this.__proto__ = null;
+  this.value = value;
+}
+function MyValuelessObject(value) {
+  this.__proto__ = new MyPrototypeObject(value);
+}
+function MyPrototypeObject(value) {
+  this.valueOf = new Function( "return this.value;" );
+  this.toString = new Function( "return (this.value + '');" );
   this.value = value;
 }
