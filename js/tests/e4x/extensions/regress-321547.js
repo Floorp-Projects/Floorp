88bcf3bd85cf182b76c94a987d86686a8d1fad49
bclary%bclary.com <bclary%bclary.com>
2007-02-16 03:26:51 +0000

    JavaScript Test - move SpiderMonkey extension e4x/Regress/regress-321547.js to e4x/extensions/regress-321547.js, bug 368585, r+sr=brendan

--- js/tests/e4x/extensions/regress-321547.js
+++ js/tests/e4x/extensions/regress-321547.js
@@ -51,41 +51,11 @@ function a(){
   return x..c;
 }
 
-actual = a.toString();
-expect = 'function a() {\n' + 
-         '    var x = <a><b><c>value c</c></b></a>;\n' +
-         '    return x..c;\n' +
-         '}';
+actual = a.toSource();
+expect = 'function a() {var x = <a><b><c>value c</c></b></a>;return x..c;}';
 actual = actual.replace(/[\n ]+/mg, ' ');
 expect = expect.replace(/[\n ]+/mg, ' ');
 
-TEST(1, expect, actual);
-
-actual = String(a.valueOf());
-expect = 'function a() {\n' + 
-         '    var x = <a><b><c>value c</c></b></a>;\n' +
-         '    return x..c;\n' +
-         '}';
-actual = actual.replace(/[\n ]+/mg, ' ');
-expect = expect.replace(/[\n ]+/mg, ' ');
-
-TEST(3, expect, actual);
-
-actual = String(a);
-expect = 'function a() {\n' + 
-         '    var x = <a><b><c>value c</c></b></a>;\n' +
-         '    return x..c;\n' +
-         '}';
-actual = actual.replace(/[\n ]+/mg, ' ');
-expect = expect.replace(/[\n ]+/mg, ' ');
-
-TEST(4, expect, actual);
-
-actual = a();
-expect = <c>value c</c>;
-actual = actual.replace(/[\n ]+/mg, ' ');
-expect = expect.replace(/[\n ]+/mg, ' ');
-
-TEST(5, expect, actual);
+TEST(2, expect, actual);
 
 END();
