b436f4cfc062554f50e439d11d54074623cc96d8
bclary%bclary.com <bclary%bclary.com>
2007-02-16 03:51:09 +0000

    JavaScript Test - move SpiderMonkey extension ecma/String/15.5.4.2.js to ecma/extensions/15.5.4.2.js, bug 368585, r+sr=brendan

--- js/tests/ecma/extensions/15.5.4.2.js
+++ js/tests/ecma/extensions/15.5.4.2.js
@@ -51,34 +51,6 @@ var TITLE   = "String.prototype.tostring";
 
 writeHeaderToLog( SECTION + " "+ TITLE);
 
-new TestCase(   SECTION,
-		"String.prototype.toString() == String.prototype.valueOf()",
-		true,
-		String.prototype.toString() == String.prototype.valueOf() );
-
-new TestCase(   SECTION, "String.prototype.toString()",     "",     String.prototype.toString() );
-new TestCase(   SECTION, "String.prototype.toString.length",    0,  String.prototype.toString.length );
-
-
-new TestCase(   SECTION,
-		"TESTSTRING = new String();TESTSTRING.valueOf() == TESTSTRING.toString()",
-		true,
-		eval("TESTSTRING = new String();TESTSTRING.valueOf() == TESTSTRING.toString()") );
-new TestCase(   SECTION,
-		"TESTSTRING = new String(true);TESTSTRING.valueOf() == TESTSTRING.toString()",
-		true,
-		eval("TESTSTRING = new String(true);TESTSTRING.valueOf() == TESTSTRING.toString()") );
-new TestCase(   SECTION,
-		"TESTSTRING = new String(false);TESTSTRING.valueOf() == TESTSTRING.toString()",
-		true,
-		eval("TESTSTRING = new String(false);TESTSTRING.valueOf() == TESTSTRING.toString()") );
-new TestCase(   SECTION,
-		"TESTSTRING = new String(Math.PI);TESTSTRING.valueOf() == TESTSTRING.toString()",
-		true,
-		eval("TESTSTRING = new String(Math.PI);TESTSTRING.valueOf() == TESTSTRING.toString()") );
-new TestCase(   SECTION,
-		"TESTSTRING = new String();TESTSTRING.valueOf() == TESTSTRING.toString()",
-		true,
-		eval("TESTSTRING = new String();TESTSTRING.valueOf() == TESTSTRING.toString()") );
+new TestCase( SECTION, "String.prototype.toString.__proto__",  Function.prototype, String.prototype.toString.__proto__ );
 
 test();
