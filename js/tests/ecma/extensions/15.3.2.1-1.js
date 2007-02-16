d4d57bc88a33707d228b526cc461e3cb3e70044f
bclary%bclary.com <bclary%bclary.com>
2007-02-16 03:41:15 +0000

    JavaScript Test - move SpiderMonkey extension ecma/FunctionObjects/15.3.2.1-1.js to ecma/extensions/15.3.2.1-1.js, bug 368585, r+sr=brendan

--- js/tests/ecma/extensions/15.3.2.1-1.js
+++ js/tests/ecma/extensions/15.3.2.1-1.js
@@ -61,69 +61,9 @@ writeHeaderToLog( SECTION + " "+ TITLE);
 
 var MyObject = new Function( "value", "this.value = value; this.valueOf = new Function( 'return this.value' ); this.toString = new Function( 'return String(this.value);' )" );
 
-var myfunc = new Function();
-
-//    not going to test toString here since it is implementation dependent.
-//    new TestCase( SECTION,  "myfunc.toString()",     "function anonymous() { }",    myfunc.toString() );
-
-myfunc.toString = Object.prototype.toString;
-
-new TestCase( SECTION,  "myfunc = new Function(); myfunc.toString = Object.prototype.toString; myfunc.toString()",
-	      "[object Function]",
-	      myfunc.toString() );
-
-new TestCase( SECTION,  
-	      "myfunc.length",                            
-	      0,                      
-	      myfunc.length );
-
 new TestCase( SECTION,
-	      "myfunc.prototype.toString()", 
-	      "[object Object]",  
-	      myfunc.prototype.toString() );
-
-new TestCase( SECTION,
-	      "myfunc.prototype.constructor",    
-	      myfunc,    
-	      myfunc.prototype.constructor );
-
-new TestCase( SECTION, 
-	      "myfunc.arguments",   
-	      null,   
-	      myfunc.arguments );
-
-new TestCase( SECTION, 
-	      "var OBJ = new MyObject(true); OBJ.valueOf()", 
-	      true,  
-	      eval("var OBJ = new MyObject(true); OBJ.valueOf()") );
-
-new TestCase( SECTION,
-	      "OBJ.toString()",   
-	      "true",  
-	      OBJ.toString() );
-
-new TestCase( SECTION, 
-	      "OBJ.toString = Object.prototype.toString; OBJ.toString()", "[object Object]", 
-	      eval("OBJ.toString = Object.prototype.toString; OBJ.toString()") );
-
-new TestCase( SECTION,  
-	      "MyObject.toString = Object.prototype.toString; MyObject.toString()", 
-	      "[object Function]",  
-	      eval("MyObject.toString = Object.prototype.toString; MyObject.toString()") );
-
-new TestCase( SECTION,
-	      "MyObject.length",  
-	      1,
-	      MyObject.length );
-
-new TestCase( SECTION,
-	      "MyObject.prototype.constructor", 
-              MyObject, 
-	      MyObject.prototype.constructor );
-
-new TestCase( SECTION,
-	      "MyObject.arguments",   
-	      null,  
-	      MyObject.arguments );
+	      "MyObject.__proto__ == Function.prototype",    
+	      true, 
+	      MyObject.__proto__ == Function.prototype );
 
 test();
