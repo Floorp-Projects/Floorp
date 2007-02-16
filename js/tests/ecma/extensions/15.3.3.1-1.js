3c896b848b3e43f5c910d329263255ad548e3a83
bclary@bclary.com <bclary@bclary.com>
2007-02-16 03:44:26 +0000

    JavaScript Test - move SpiderMonkey extension ecma/FunctionObjects/15.3.3.1-1.js to ecma/extensions/15.3.3.1-1.js, bug 368585, r+sr=brendan

--- js/tests/ecma/extensions/15.3.3.1-1.js
+++ js/tests/ecma/extensions/15.3.3.1-1.js
@@ -60,6 +60,6 @@ var TITLE   = "Function.prototype";
 
 writeHeaderToLog( SECTION + " "+ TITLE);
 
-new TestCase( SECTION, "Function.prototype == Function.proto",    true, Function.__proto__ == Function.prototype );
+new TestCase( SECTION, "Function.prototype == Function.__proto__",    true, Function.__proto__ == Function.prototype );
 
 test();
