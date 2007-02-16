83fcce472069eff96056d3775874a344a7495066
martijn.martijn@gmail.com <martijn.martijn@gmail.com>
2007-02-16 12:12:44 +0000

    Bug 369326 � Allow marquee inside p, r=mrbkap, sr=dbaron

--- parser/htmlparser/src/nsElementTable.cpp
+++ parser/htmlparser/src/nsElementTable.cpp
@@ -820,7 +820,7 @@ const nsHTMLElement gHTMLElements[] = {
     /*req-parent excl-parent*/          eHTMLTag_unknown,eHTMLTag_unknown,
     /*rootnodes,endrootnodes*/          &gRootTags,&gRootTags,
     /*autoclose starttags and endtags*/ 0,0,0,0,
-    /*parent,incl,exclgroups*/          kBlock, (kSelf|kFlowEntity), kNone,
+    /*parent,incl,exclgroups*/          kSpecial, (kSelf|kFlowEntity), kNone,
     /*special props, prop-range*/       kRequiresBody, kDefaultPropRange,
     /*special parents,kids,skip*/       0,0,eHTMLTag_unknown,
     /*contain-func*/                    0
