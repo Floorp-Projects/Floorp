cc9d20f78b4f865de70f062766b8ecfbda8db349
rob_strong%exchangecode.com <rob_strong%exchangecode.com>
2007-02-20 21:24:32 +0000

    Bug 370574: Remove unused xpinstall installer bits. r=bsmedberg

--- browser/installer/windows/7zip.bat
+++
@@ -1,6 +0,0 @@
-cd 7zstage
-7z a -t7z ..\7z\app.7z *.* -mx -m0=BCJ2 -m1=LZMA:d24 -m2=LZMA:d19 -m3=LZMA:d19  -mb0:1 -mb0s1:2 -mb0s2:3
-cd ..\7z
-upx --best 7zSD.sfx
-copy /b 7zSD.sfx+app.tag+app.7z SetupGeneric.exe
-cd ..
