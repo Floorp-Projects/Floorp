d79c9c49e28f8d1207e9a4931cb82345734f5219
anton.bobrov%sun.com <anton.bobrov%sun.com>
2007-02-23 12:56:40 +0000

    new package.mk to make tarballs for binary dist, ftp or otherwise.

--- directory/c-sdk/package.mk
+++ directory/c-sdk/package.mk
@@ -43,156 +43,180 @@ MOD_DEPTH = .
 include $(NSPR_TREE)/config/rules.mk
 include build.mk
 
-MMDD	= $(shell date +%m.%d)
-INSTDIR = ../../dist/$(MMDD)/$(OBJDIR_NAME)
-LIBDIR  = ../../dist/$(OBJDIR_NAME)/lib
-INCDIR  = ../../dist/public/ldap
-PRIVATEINCDIR  = ../../dist/public/ldap-private
-NSPRINCDIR  = ../../dist/$(OBJDIR_NAME)/include
-BINDIR  = ../../dist/$(OBJDIR_NAME)/bin
-ETCDIR  = ../../dist/$(OBJDIR_NAME)/etc
-EXPDIR  = ldap/examples
-
-# defaults
-PKG_PRIVATE_HDRS=1
-PKG_PRIVATE_LIBS=1
-PKG_DEP_LIBS=1
+NSINSTALL = ./config/nsinstall
+ifeq ($(USE_64), 1)
+OBJDIR_NAME = $(OS_ARCH)$(OS_RELEASE)_$(CPU_ARCH)_64$(OBJDIR_TAG)
+else
+OBJDIR_NAME = $(OS_ARCH)$(OS_RELEASE)_$(CPU_ARCH)$(OBJDIR_TAG)
+endif
+BASEINSTDIR = ../../dist/pkg/$(OBJDIR_NAME)
+PKG_NAME_PREFIX = ldapcsdk-
+PKG_VERSION = $(shell expr "scale=2; $(VENDOR_VERSION)/100" | bc)
+INSTDIR = ../../dist/pkg/$(OBJDIR_NAME)/$(PKG_NAME_PREFIX)$(PKG_VERSION)-$(OBJDIR_NAME)
+LDAP_DIST = ../../dist
+SASL_DIST = ../../dist
+SEC_DIST = ../../dist/$(OBJDIR_NAME)
+SVRCORE_DIST = ../../dist
+NSPRINCDIR  = $(SEC_DIST)/include
+NSSINCDIR  = ../../dist/public/nss
+SECLIBDIR  = $(SEC_DIST)/lib
+SASLLIBDIR = $(SASL_DIST)/lib
+SASLINCDIR = $(SASL_DIST)/include
+ifeq ($(OS_ARCH), WINNT)
+SASL_BASENAME = libsasl
+else
+SASL_BASENAME = libsasl2
+endif
+SVRCORELIBDIR = $(SVRCORE_DIST)/lib
+SVRCOREINCDIR = $(SVRCORE_DIST)/include
+LDAPLIBDIR  = $(LDAP_DIST)/lib
+LDAPINCDIR  = $(LDAP_DIST)/public/ldap
+LDAPPRIVATEINCDIR  = $(LDAP_DIST)/public/ldap-private
+LDAPBINDIR  = $(LDAP_DIST)/bin
+LDAPETCDIR  = $(LDAP_DIST)/etc
+LDAPEXPDIR  = ldap/examples
+FREEBL_LIBNAME  = freebl*
 
 all::   FORCE
 	$(NSINSTALL) -D $(INSTDIR)
-
 	@echo "Installing libraries"
 	$(NSINSTALL) -D $(INSTDIR)/lib
 # Windows
 ifeq ($(OS_ARCH), WINNT)
-	$(NSINSTALL) $(LIBDIR)/$(LDAP_LIBNAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/$(SSLDAP_LIBNAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/$(PRLDAP_LIBNAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/$(LBER_LIBNAME).lib $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/$(LDIF_LIBNAME).lib $(INSTDIR)/lib
-ifeq ($(PKG_PRIVATE_LIBS),1)
-	$(NSINSTALL) $(LIBDIR)/$(IUTIL_LIBNAME).lib $(INSTDIR)/lib
-endif
-	$(NSINSTALL) $(LIBDIR)/$(UTIL_LIBNAME).lib $(INSTDIR)/lib
-ifeq ($(PKG_DEP_LIBS),1)
-	$(NSINSTALL) $(LIBDIR)/$(NSS_LIBNAME).* $(INSTDIR)/lib
-ifeq ($(NSS_DYNAMIC_SOFTOKN),1)
-	$(NSINSTALL) $(LIBDIR)/$(SOFTOKN_LIBNAME).* $(INSTDIR)/lib
-endif
-	$(NSINSTALL) $(LIBDIR)/$(SSL_LIBNAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/$(PLC_BASENAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/$(PLDS_BASENAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/$(NSPR_BASENAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/$(LDAP_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/$(SSLDAP_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/$(PRLDAP_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/$(LDIF_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/$(LBER_LIBNAME).lib $(INSTDIR)/lib
+	$(NSINSTALL) $(SVRCORELIBDIR)/$(SVRCORE_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/$(NSS_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/$(SSL_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/$(SOFTOKN_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/$(PLC_BASENAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/$(PLDS_BASENAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/$(NSPR_BASENAME).* $(INSTDIR)/lib
+ifeq ($(HAVE_SASL), 1)
+	$(NSINSTALL) $(SASLLIBDIR)/$(SASL_BASENAME)* $(INSTDIR)/lib
 endif
 # UNIX
 else
-	$(NSINSTALL) $(LIBDIR)/lib$(LDAP_LIBNAME).$(DLL_SUFFIX) $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/lib$(SSLDAP_LIBNAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/lib$(PRLDAP_LIBNAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/lib$(LDAP_LIBNAME).$(LIB_SUFFIX) $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/lib$(LBER_LIBNAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/lib$(LDIF_LIBNAME).* $(INSTDIR)/lib
-ifeq ($(PKG_PRIVATE_LIBS),1)
-	$(NSINSTALL) $(LIBDIR)/lib$(IUTIL_LIBNAME).* $(INSTDIR)/lib
-endif
-ifeq ($(PKG_DEP_LIBS),1)
-	$(NSINSTALL) $(LIBDIR)/lib$(NSS_LIBNAME).* $(INSTDIR)/lib
-ifeq ($(NSS_DYNAMIC_SOFTOKN),1)
-	$(NSINSTALL) $(LIBDIR)/lib$(SOFTOKN_LIBNAME).* $(INSTDIR)/lib
-endif
-	$(NSINSTALL) $(LIBDIR)/lib$(SSL_LIBNAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/$(PLC_BASENAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/$(PLDS_BASENAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/$(NSPR_BASENAME).* $(INSTDIR)/lib
-ifneq ($(USE_64), 1)
-ifeq ($(OS_ARCH), SunOS)
-ifneq ($(OS_TEST),i86pc)
-	$(NSINSTALL) $(LIBDIR)/lib$(HYBRID_LIBNAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/lib$(PURE32_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/lib$(LDAP_LIBNAME).$(DLL_SUFFIX) $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/lib$(SSLDAP_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/lib$(PRLDAP_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/lib$(LDAP_LIBNAME).$(LIB_SUFFIX) $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/lib$(LBER_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(LDAPLIBDIR)/lib$(LDIF_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SVRCORELIBDIR)/lib$(SVRCORE_LIBNAME).$(DLL_SUFFIX)* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/lib$(NSS_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/lib$(SSL_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/lib$(SOFTOKN_LIBNAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/lib$(PLC_BASENAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/lib$(PLDS_BASENAME).* $(INSTDIR)/lib
+	$(NSINSTALL) $(SECLIBDIR)/lib$(NSPR_BASENAME).* $(INSTDIR)/lib
+ifeq ($(HAVE_SASL), 1)
+        ifeq ($(OS_ARCH), HP-UX)
+		$(NSINSTALL) $(SASLLIBDIR)/$(SASL_BASENAME).sl* $(INSTDIR)/lib
+		$(NSINSTALL) $(SASLLIBDIR)/sasl2/*.sl $(INSTDIR)/lib
+        else
+		$(NSINSTALL) $(SASLLIBDIR)/$(SASL_BASENAME).so* $(INSTDIR)/lib
+		$(NSINSTALL) $(SASLLIBDIR)/sasl2/*.so* $(INSTDIR)/lib
+        endif
 endif
 endif
-ifeq ($(OS_ARCH), HP-UX)
-	$(NSINSTALL) $(LIBDIR)/lib$(HYBRID_LIBNAME).* $(INSTDIR)/lib
-	$(NSINSTALL) $(LIBDIR)/lib$(PURE32_LIBNAME).* $(INSTDIR)/lib
+
+ifeq ($(COPYFREEBL), 1)
+ifeq ($(OS_ARCH), WINNT)
+	$(NSINSTALL) $(SECLIBDIR)/$(FREEBL_LIBNAME).* $(INSTDIR)/lib
+else
+	$(NSINSTALL) $(SECLIBDIR)/lib$(FREEBL_LIBNAME).* $(INSTDIR)/lib
 endif
 endif
+
+ifneq ($(OS_ARCH), WINNT)
+	rm -f $(INSTDIR)/lib/*.a
 endif
-endif
-ifeq ($(BUILDCLU), 1)
+
 	@echo "Installing tools"
-	$(NSINSTALL) -D $(INSTDIR)/tools
-	$(NSINSTALL) $(BINDIR)/ldapsearch$(EXE_SUFFIX) $(INSTDIR)/tools
-	$(NSINSTALL) $(BINDIR)/ldapdelete$(EXE_SUFFIX) $(INSTDIR)/tools
-	$(NSINSTALL) $(BINDIR)/ldapmodify$(EXE_SUFFIX) $(INSTDIR)/tools
-	$(NSINSTALL) $(BINDIR)/ldapcmp$(EXE_SUFFIX) $(INSTDIR)/tools
-	$(NSINSTALL) $(BINDIR)/ldapcompare$(EXE_SUFFIX) $(INSTDIR)/tools
-endif
+	$(NSINSTALL) -D $(INSTDIR)/bin
+	$(NSINSTALL) $(LDAPBINDIR)/ldapsearch$(EXE_SUFFIX) $(INSTDIR)/bin
+	$(NSINSTALL) $(LDAPBINDIR)/ldapdelete$(EXE_SUFFIX) $(INSTDIR)/bin
+	$(NSINSTALL) $(LDAPBINDIR)/ldapmodify$(EXE_SUFFIX) $(INSTDIR)/bin
+	$(NSINSTALL) $(LDAPBINDIR)/ldapcmp$(EXE_SUFFIX) $(INSTDIR)/bin
+	$(NSINSTALL) $(LDAPBINDIR)/ldapcompare$(EXE_SUFFIX) $(INSTDIR)/bin
+	$(NSINSTALL) $(LDAPBINDIR)/ldappasswd$(EXE_SUFFIX) $(INSTDIR)/bin
 
-	@echo "Installing includes"
+	@echo "Installing header files"
 	$(NSINSTALL) -D $(INSTDIR)/include
-	$(NSINSTALL) $(INCDIR)/disptmpl.h $(INSTDIR)/include
-ifeq ($(PKG_PRIVATE_LIBS),1)
-	$(NSINSTALL) $(INCDIR)/iutil.h $(INSTDIR)/include
-endif
-	$(NSINSTALL) $(INCDIR)/lber.h $(INSTDIR)/include
-	$(NSINSTALL) $(INCDIR)/ldap.h $(INSTDIR)/include
-	$(NSINSTALL) $(INCDIR)/ldap-standard.h $(INSTDIR)/include
-	$(NSINSTALL) $(INCDIR)/ldap-platform.h $(INSTDIR)/include
-	$(NSINSTALL) $(INCDIR)/ldap-extension.h $(INSTDIR)/include
-	$(NSINSTALL) $(INCDIR)/ldap-deprecated.h $(INSTDIR)/include
-	$(NSINSTALL) $(INCDIR)/ldap-to-be-deprecated.h $(INSTDIR)/include
-	$(NSINSTALL) $(INCDIR)/ldap_ssl.h $(INSTDIR)/include
-	$(NSINSTALL) $(INCDIR)/ldappr.h $(INSTDIR)/include
-	$(NSINSTALL) $(INCDIR)/srchpref.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/disptmpl.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/lber.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/ldap.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/ldif.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/ldap-standard.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/ldap-platform.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/ldap-extension.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/ldap-deprecated.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/ldap-to-be-deprecated.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/ldap_ssl.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/ldappr.h $(INSTDIR)/include
+	$(NSINSTALL) $(LDAPINCDIR)/srchpref.h $(INSTDIR)/include
 
-ifeq ($(PKG_PRIVATE_HDRS),1)
-	@echo "Installing private include files"
-	$(NSINSTALL) -D $(INSTDIR)/include-private
-	$(NSINSTALL) -D $(INSTDIR)/include-private/liblber
-	$(NSINSTALL) $(PRIVATEINCDIR)/lber-int.h $(INSTDIR)/include-private/liblber
-	$(NSINSTALL) $(PRIVATEINCDIR)/ldap-int.h $(INSTDIR)/include-private
-	$(NSINSTALL) $(PRIVATEINCDIR)/portable.h $(INSTDIR)/include-private
-	$(NSINSTALL) $(PRIVATEINCDIR)/ldaprot.h $(INSTDIR)/include-private
-	$(NSINSTALL) $(PRIVATEINCDIR)/ldaplog.h $(INSTDIR)/include-private
+	$(NSINSTALL) -D $(INSTDIR)/include
+	cp -r $(SVRCOREINCDIR)/svrcore.h $(INSTDIR)/include
 
-	@echo "Installing nspr header files"
-	$(NSINSTALL) -D $(INSTDIR)/include-nspr
-	cp -r $(NSPRINCDIR)/* $(INSTDIR)/include-nspr
+	$(NSINSTALL) -D $(INSTDIR)/include
+	cp -r $(NSPRINCDIR)/* $(INSTDIR)/include
+	rm -rf $(INSTDIR)/include/md $(INSTDIR)/include/obsolete $(INSTDIR)/include/private
+
+	$(NSINSTALL) -D $(INSTDIR)/include
+	cp -r $(NSSINCDIR)/* $(INSTDIR)/include
+
+ifeq ($(HAVE_SASL), 1)
+	$(NSINSTALL) $(SASLINCDIR)/sasl/*.h $(INSTDIR)/include
 endif
 
 	@echo "Installing etc files"
 	$(NSINSTALL) -D $(INSTDIR)/etc
-	$(NSINSTALL) $(ETCDIR)/ldapfilter.conf $(INSTDIR)/etc
-	$(NSINSTALL) $(ETCDIR)/ldapfriendly $(INSTDIR)/etc
-	$(NSINSTALL) $(ETCDIR)/ldapsearchprefs.conf $(INSTDIR)/etc
-	$(NSINSTALL) $(ETCDIR)/ldaptemplates.conf $(INSTDIR)/etc
+	$(NSINSTALL) $(LDAPETCDIR)/ldapfilter.conf $(INSTDIR)/etc
+	$(NSINSTALL) $(LDAPETCDIR)/ldapfriendly $(INSTDIR)/etc
+	$(NSINSTALL) $(LDAPETCDIR)/ldapsearchprefs.conf $(INSTDIR)/etc
+	$(NSINSTALL) $(LDAPETCDIR)/ldaptemplates.conf $(INSTDIR)/etc
 
 	@echo "Installing example files"
 	$(NSINSTALL) -D $(INSTDIR)/examples
-	$(NSINSTALL) $(EXPDIR)/*.c $(INSTDIR)/examples
-	$(NSINSTALL) $(EXPDIR)/*.h $(INSTDIR)/examples
-	$(NSINSTALL) $(EXPDIR)/README $(INSTDIR)/examples
-	$(NSINSTALL) $(EXPDIR)/Makefile $(INSTDIR)/examples
-	$(NSINSTALL) $(EXPDIR)/xmplflt.conf $(INSTDIR)/examples
+	$(NSINSTALL) $(LDAPEXPDIR)/*.c $(INSTDIR)/examples
+	$(NSINSTALL) $(LDAPEXPDIR)/*.h $(INSTDIR)/examples
+	$(NSINSTALL) $(LDAPEXPDIR)/README $(INSTDIR)/examples
+ifneq ($(OS_ARCH), WINNT)
+	$(NSINSTALL) $(LDAPEXPDIR)/Makefile $(INSTDIR)/examples
+endif
+ifeq ($(OS_ARCH), WINNT)
+	$(NSINSTALL) $(LDAPEXPDIR)/win32.mak $(INSTDIR)/examples
+endif
+	$(NSINSTALL) $(LDAPEXPDIR)/xmplflt.conf $(INSTDIR)/examples
 
-ifdef DOCDIR
-	@echo "Installing doc files"
-	$(NSINSTALL) -D $(INSTDIR)/docs
-	$(NSINSTALL) $(DOCDIR)/README $(INSTDIR)
-	$(NSINSTALL) $(DOCDIR)/redist.txt $(INSTDIR)
-	$(NSINSTALL) $(DOCDIR)/*.htm $(INSTDIR)
-	$(NSINSTALL) $(DOCDIR)/*.gif $(INSTDIR)
-	$(NSINSTALL) $(DOCDIR)/README $(INSTDIR)/docs
-	$(NSINSTALL) $(DOCDIR)/redist.txt $(INSTDIR)/docs
+	@echo "Creating tarball in $(BASEINSTDIR) directory"
+ifeq ($(OS_ARCH), WINNT)
+	cd $(BASEINSTDIR) && zip -r -X $(PKG_NAME_PREFIX)$(PKG_VERSION)-$(OBJDIR_NAME).zip \
+		$(PKG_NAME_PREFIX)$(PKG_VERSION)-$(OBJDIR_NAME)
+else
+	cd $(BASEINSTDIR) && tar -cvf $(PKG_NAME_PREFIX)$(PKG_VERSION)-$(OBJDIR_NAME).tar \
+		$(PKG_NAME_PREFIX)$(PKG_VERSION)-$(OBJDIR_NAME) \
+		&& gzip -c $(PKG_NAME_PREFIX)$(PKG_VERSION)-$(OBJDIR_NAME).tar > \
+		    $(PKG_NAME_PREFIX)$(PKG_VERSION)-$(OBJDIR_NAME).tgz && rm -f \
+			  $(PKG_NAME_PREFIX)$(PKG_VERSION)-$(OBJDIR_NAME).tar
 endif
 
 ifdef BUILD_SHIP
-	@echo "Copying files to $(BUILD_SHIP) directory"
-	cp -r $(INSTDIR) $(BUILD_SHIP)
+	@echo "Copying tarball to $(BUILD_SHIP) directory"
+ifeq ($(OS_ARCH), WINNT)
+	cp $(BASEINSTDIR)/$(PKG_NAME_PREFIX)$(PKG_VERSION)-$(OBJDIR_NAME).zip $(BUILD_SHIP)
+else
+	cp $(BASEINSTDIR)/$(PKG_NAME_PREFIX)$(PKG_VERSION)-$(OBJDIR_NAME).tgz $(BUILD_SHIP)
 endif
-	
+endif
+
 clean:: FORCE
 	@echo "Cleaning up old install"
-	$(RM) -rf $(INSTDIR)
+	$(RM) -rf $(BASEINSTDIR)
 
 FORCE::
