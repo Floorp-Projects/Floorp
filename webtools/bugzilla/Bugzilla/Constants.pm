dde5608e263cf7eb9a3d9f90927c120763facc6e
mkanat%bugzilla.org <mkanat%bugzilla.org>
2007-02-26 06:21:37 +0000

    Bump version number on the trunk with the branching of 3.0.

--- webtools/bugzilla/Bugzilla/Constants.pm
+++ webtools/bugzilla/Bugzilla/Constants.pm
@@ -146,7 +146,7 @@ use File::Basename;
 # CONSTANTS
 #
 # Bugzilla version
-use constant BUGZILLA_VERSION => "2.23.4+";
+use constant BUGZILLA_VERSION => "3.1";
 
 #
 # ControlMap constants for group_control_map.
