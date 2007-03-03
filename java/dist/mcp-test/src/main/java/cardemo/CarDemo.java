8db49728f7b00c2852f49a80f6b8a89f831c295d
edburns%acm.org <edburns%acm.org>
2007-03-03 21:06:39 +0000

    M build.xml A mcp-test/src/main/java/cardemo/CarDemo.java A mcp-test/src/test/java/cardemo/CarDemoTest.java
    - include automated test to use MCP to test the Ajax cardemo

---
+++ java/dist/mcp-test/src/main/java/cardemo/CarDemo.java
@@ -0,0 +1,29 @@
+/*
+ * CarDemo.java
+ *
+ * Created on March 3, 2007, 2:58 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package cardemo;
+
+/**
+ *
+ * @author edburns
+ */
+public class CarDemo {
+    
+    /** Creates a new instance of CarDemo */
+    public CarDemo() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ java/dist/mcp-test/src/test/java/cardemo/CarDemoTest.java
@@ -0,0 +1,43 @@
+/*
+ * CarDemoTest.java
+ * JUnit based test
+ *
+ * Created on March 3, 2007, 2:31 PM
+ */
+
+package cardemo;
+
+import org.mozilla.mcp.MCP;
+import org.mozilla.webclient.WebclientTestCase;
+
+/**
+ *
+ * @author edburns
+ */
+public class CarDemoTest extends WebclientTestCase  {
+    
+    private MCP mcp = null;    
+    
+    public CarDemoTest(String testName) {
+        super(testName);
+    }
+
+    public void setUp() {
+        super.setUp();
+
+        mcp = new MCP();
+	try {
+	    mcp.setAppData(getBrowserBinDir());
+	}
+	catch (Exception e) {
+	    fail();
+	}
+        
+    }
+    
+    public void testCardemo() throws Exception {
+        mcp.getRealizedVisibleBrowserWindow();
+        mcp.blockingLoad("http://webdev1.sun.com/jsf-ajax-cardemo/faces/chooseLocale.jsp");
+    }
+    
+}
