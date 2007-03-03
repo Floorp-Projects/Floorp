0032301777b6ff11d5905faf0b9ea2df32313f28
sayrer@gmail.com <sayrer@gmail.com>
2007-03-03 17:43:01 +0000

    Bug 371611. crash nsIScriptableDateFormat::FormatDate for undefined year, month, day. Patch by Ryan Jones <sciguyryan@gmail.com>. r=smontagu, sr=biesi

---
+++ intl/locale/tests/unit/test_bug371611.js
@@ -0,0 +1,26 @@
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+
+function test_formatdatetime_return()
+{
+  var dateConv = Cc["@mozilla.org/intl/scriptabledateformat;1"].
+    getService(Ci.nsIScriptableDateFormat);
+
+  /* Testing if we throw instead of crashing when we are passed 0s. */
+  var x = false;
+  try {
+    dateConv.FormatDate("", Ci.nsIScriptableDateFormat.dateFormatLong,
+                        0, 0, 0);
+  }
+  catch (e if (e.result == Cr.NS_ERROR_INVALID_ARG)) {
+    x = true;
+  }
+  if (!x)
+    do_throw("FormatDate didn't throw when passed 0 for its arguments.");
+}
+
+function run_test()
+{
+  test_formatdatetime_return();
+}
