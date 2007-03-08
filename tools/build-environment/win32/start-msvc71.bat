07bf92b7bca5aa7f7b827786ed3940e09cfcb8eb
bent.mozilla%gmail.com <bent.mozilla%gmail.com>
2007-03-08 02:28:30 +0000

    Bug 369241 - "MozillaBuild should prepend the PlatformSDK's paths if available for vc71". r=bsmedberg.

--- tools/build-environment/win32/start-msvc71.bat
+++ tools/build-environment/win32/start-msvc71.bat
@@ -26,5 +26,21 @@ SET PATH=%PATH%;%MOZ_TOOLS%\bin
 rem Prepend MSVC paths
 call "%VC71DIR%\Bin\vcvars32.bat"
 
+rem Prepend SDK paths if MOZBUILD_USE_SDK is defined and the SDK exists
+if defined MOZBUILD_USE_SDK (
+    if not "%SDKDIR%"=="" (
+        rem Prepend SDK paths - Don't use the SDK SetEnv.cmd because it pulls in
+        rem random VC paths which we don't want.
+
+        rem Don't add '%SDKDIR%include\atl' to INCLUDE because VC7 has ATL headers
+        rem which don't require atlthunk.lib. This also makes it unnecessary to
+        rem append the atlthunk compat lib to LIB.
+
+        SET PATH=%SDKDIR%\bin;%PATH%
+        SET LIB=%SDKDIR%\lib;%LIB%
+        SET INCLUDE=%SDKDIR%\include;%INCLUDE%
+    )
+)
+
 cd "%USERPROFILE%"
 start "MSYS Shell - MSVC7.1 Environment" "%MOZILLABUILD%\msys\bin\rxvt" -backspacekey  -sl 2500 -fg %FGCOLOR% -bg %BGCOLOR% -sr -fn "Lucida Console" -tn msys -geometry 80x25 -e /bin/sh --login -i
