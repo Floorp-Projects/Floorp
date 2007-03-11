1fa0de744d6a33cc64f812b242c509285542484e
sayrer%gmail.com <sayrer%gmail.com>
2007-03-11 01:05:06 +0000

    Bug 366936. disable tests to see if mac bustage is affected by that.

--- parser/htmlparser/tests/mochitest/parser_web_testrunner.js
+++ parser/htmlparser/tests/mochitest/parser_web_testrunner.js
@@ -106,10 +106,13 @@ var framesLoaded = [];
 function frameLoaded(e) {
   framesLoaded.push(e.target);
   if (framesLoaded.length == parserDatFiles.length) {
+    /*
     var tests = [scrapeText(ifr.contentDocument)
 		 for each (ifr in framesLoaded)];
     testcases = test_parser(tests);    
     nextTest($("testframe"));
+    */
+    SimpleTest.finish();
   }
 }
 
