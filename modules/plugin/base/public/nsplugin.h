1b83fa2b5129ff282d571d4a609de510be2a43d5
joshmoz%gmail.com <joshmoz%gmail.com>
2007-03-13 22:18:04 +0000

    remove backward adapter plugin code for 3.0/4.0 plugin compat. nobody should be using it any more and in any case it doesn't belong in the samples directory. b=373155 sr=jst

--- modules/plugin/base/public/nsplugin.h
+++ modules/plugin/base/public/nsplugin.h
@@ -39,9 +39,7 @@
  *
  * This interface was an attempt to supersede npapi.h, npupp.h but it failed. It
  * would have eliminated the need for glue files: npunix.c, npwin.cpp and
- * npmac.cpp that get linked with the plugin. You would however have needed to
- * link with the "backward adapter" (badapter.cpp) in order to allow your
- * plugin to run in pre-5.0 browsers.
+ * npmac.cpp that get linked with the plugin.
  *
  * This is the master header file that includes most of the other headers
  * you'd have needed to write a plugin.
