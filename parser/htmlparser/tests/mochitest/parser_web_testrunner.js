4ab0c86a7952d33dc41160381181cf7e872c3b54
sayrer@gmail.com <sayrer@gmail.com>
2007-03-20 21:15:24 +0000

    Add times to parser test logging to examine linux test harness timeouts.

--- parser/htmlparser/tests/mochitest/parser_web_testrunner.js
+++ parser/htmlparser/tests/mochitest/parser_web_testrunner.js
@@ -72,17 +72,17 @@ function makeTestChecker(input, expected, errors) {
     var domAsString = docToTestOutput(e.target.contentDocument);
     // It's possible we need to reorder attributes to get these to match
     if (expected == domAsString) {
-      is(domAsString, expected, "HTML5 expected success.");
+      is(domAsString, expected, "HTML5 expected success. " + new Date());
     } else {
       var reorderedDOM = reorderToMatchExpected(domAsString, expected);
       if (html5Exceptions[input]) {
-	todo(reorderedDOM == expected, "HTML5 expected failure.");
-	writeErrorSummary(input, expected, reorderedDOM, true);
+        todo(reorderedDOM == expected, "HTML5 expected failure. " + new Date());
+        writeErrorSummary(input, expected, reorderedDOM, true);
       } else {
-	is(reorderedDOM, expected, "HTML5 unexpected failure.");
-	if (reorderedDOM != expected) {
-	  writeErrorSummary(input, expected, reorderedDOM, false);
-	}
+	      is(reorderedDOM, expected, "HTML5 unexpected failure. " + new Date());
+	      if (reorderedDOM != expected) {
+	        writeErrorSummary(input, expected, reorderedDOM, false);
+        }
       }
     }
     nextTest(e.target);
