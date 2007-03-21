df9df44f4988832f656563d451ec113ca0198d51
pavlov%pavlov.net <pavlov%pavlov.net>
2007-03-21 19:12:00 +0000

    removing CRCs from the acTl chunk and making all the chunks not-safe-for-copy. bug 257197. r=asmith15@learn.senecac.on.ca

--- modules/libpr0n/decoders/png/nsPNGDecoder.cpp
+++ modules/libpr0n/decoders/png/nsPNGDecoder.cpp
@@ -101,7 +101,7 @@ void nsPNGDecoder::CreateFrame(png_uint_32 x_offset, png_uint_32 y_offset,
   if (NS_FAILED(rv))
     longjmp(mPNG->jmpbuf, 5); // NS_ERROR_OUT_OF_MEMORY
 
-  if (png_get_valid(mPNG, mInfo, PNG_INFO_acTl))
+  if (png_get_valid(mPNG, mInfo, PNG_INFO_acTL))
     SetAnimFrameInfo();
   
   mImage->AppendFrame(mFrame);
@@ -376,7 +376,7 @@ info_callback(png_structp png_ptr, png_infop info_ptr)
     }
   }
 
-  if (png_get_valid(png_ptr, info_ptr, PNG_INFO_acTl))
+  if (png_get_valid(png_ptr, info_ptr, PNG_INFO_acTL))
     png_set_progressive_frame_fn(png_ptr, frame_info_callback, NULL);
   
   if (png_first_frame_is_hidden(png_ptr, info_ptr)) {
@@ -554,7 +554,7 @@ end_callback(png_structp png_ptr, png_infop info_ptr)
 
   nsPNGDecoder *decoder = NS_STATIC_CAST(nsPNGDecoder*, png_get_progressive_ptr(png_ptr));
   
-  if (png_get_valid(png_ptr, info_ptr, PNG_INFO_acTl)) {
+  if (png_get_valid(png_ptr, info_ptr, PNG_INFO_acTL)) {
     PRInt32 num_iterations = png_get_num_iterations(png_ptr, info_ptr);
     if (num_iterations <= 0) /* forever */
       num_iterations = -1;
