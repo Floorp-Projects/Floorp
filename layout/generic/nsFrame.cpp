cf9f0977a58894263e670b4d99458b50c5835bcc
asqueella@gmail.com <asqueella@gmail.com>
2007-03-24 22:30:59 -0700

    Bug 374486 - "WARNING: Nonempty list left over?" in visual debugging mode p=Sylvain Pasche <sylvain.pasche@gmail.com>r+sr=roc

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -1416,11 +1416,11 @@ nsIFrame::BuildDisplayListForStackingContext(nsDisplayListBuilder* aBuilder,
   // The element's outline items need to all come before any child outline
   // items.
   set.Outlines()->SortByContentOrder(aBuilder, GetContent());
-  resultList.AppendToTop(set.Outlines());
-
 #ifdef NS_DEBUG
   DisplayDebugBorders(aBuilder, this, set);
 #endif
+  resultList.AppendToTop(set.Outlines());
+
 
   if (applyAbsPosClipping) {
     nsAbsPosClipWrapper wrapper(absPosClip);
