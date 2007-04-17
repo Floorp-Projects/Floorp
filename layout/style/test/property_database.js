28f0026e6366d4b3812d58da0af511ed27a7617b
dbaron@dbaron.org <dbaron@dbaron.org>
2007-04-17 11:01:17 -0700

    Better express that failures in one of the system font cases depend on what the system fonts actually are.

--- layout/style/test/property_database.js
+++ layout/style/test/property_database.js
@@ -843,8 +843,8 @@ var gCSSProperties = {
 		domProp: "fontWeight",
 		inherited: true,
 		type: CSS_TYPE_LONGHAND,
-		initial_values: [ "normal" ],
-		other_values: [ "bold", "100", "200", "300", "400", "500", "600", "700", "800", "900" ],
+		initial_values: [ "normal", "400" ],
+		other_values: [ "bold", "100", "200", "300", "500", "600", "700", "800", "900" ],
 		invalid_values: [ "107", "399", "401", "699", "710" ]
 	},
 	"height": {
