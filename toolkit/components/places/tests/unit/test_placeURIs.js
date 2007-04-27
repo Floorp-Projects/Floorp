835710045fb23391e74d72bd013254ff354cf6af
mozilla.mano@sent.com <mozilla.mano@sent.com>
2007-04-27 03:04:13 -0700

    Bug 378828 - Support sortingAnnotation in place: URIs. r=dietrich.

--- toolkit/components/places/tests/unit/test_placeURIs.js
+++ toolkit/components/places/tests/unit/test_placeURIs.js
@@ -49,9 +49,24 @@ function run_test() {
   // XXX Full testing coverage for QueriesToQueryString and
   // QueryStringToQueries
 
+  const NHQO = Ci.nsINavHistoryQueryOptions;
   // Bug 376798
   var query = histsvc.getNewQuery();
   query.setFolders([1],1);
   do_check_eq(histsvc.queriesToQueryString([query], 1, histsvc.getNewQueryOptions()),
               "place:folder=1");
+
+  // Bug 378828
+  var options = histsvc.getNewQueryOptions();
+  options.sortingAnnotation = "test anno";
+  options.sortingMode = NHQO.SORT_BY_ANNOTATION_DESCENDING;
+  var placeURI =
+    "place:folder=1&sort=" + NHQO.SORT_BY_ANNOTATION_DESCENDING +
+    "&sortingAnnotation=test%20anno";
+  do_check_eq(histsvc.queriesToQueryString([query], 1, options),
+              placeURI);
+  var options = {};
+  histsvc.queryStringToQueries(placeURI, { }, {}, options);
+  do_check_eq(options.value.sortingAnnotation, "test anno");
+  do_check_eq(options.value.sortingMode, NHQO.SORT_BY_ANNOTATION_DESCENDING);
 }
