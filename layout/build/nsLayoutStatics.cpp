594d259432ed151529686f8e10aa01ba66702ed8
jonas@sicking.cc <jonas@sicking.cc>
2007-05-07 18:03:08 -0700

    back out last patch since leaks went through the roof

--- layout/build/nsLayoutStatics.cpp
+++ layout/build/nsLayoutStatics.cpp
@@ -78,7 +78,6 @@
 #include "txMozillaXSLTProcessor.h"
 #include "nsDOMStorage.h"
 #include "nsCellMap.h"
-#include "nsCCUncollectableMarker.h"
 
 #ifdef MOZ_XUL
 #include "nsXULContentUtils.h"
@@ -207,12 +206,6 @@ nsLayoutStatics::Initialize()
   rv = nsDOMStorageManager::Initialize();
   if (NS_FAILED(rv)) {
     NS_ERROR("Could not initialize nsDOMStorageManager");
-    return rv;
-  }
-
-  rv = nsCCUncollectableMarker::Init();
-  if (NS_FAILED(rv)) {
-    NS_ERROR("Could not initialize nsCCUncollectableMarker");
     return rv;
   }
 
