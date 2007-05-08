fe03d7db2dda8d4cc387ca7ac8e9cfccefb24b69
aaronleventhal@moonset.net <aaronleventhal@moonset.net>
2007-05-08 14:44:04 -0700

    Bug 301621. Expose elements with onclick from addEventListener(). r=smaug

--- accessible/src/base/nsAccessibilityService.cpp
+++ accessible/src/base/nsAccessibilityService.cpp
@@ -39,6 +39,7 @@
 // NOTE: alphabetically ordered
 #include "nsAccessibilityAtoms.h"
 #include "nsAccessibilityService.h"
+#include "nsAccessibilityUtils.h"
 #include "nsCURILoader.h"
 #include "nsDocAccessible.h"
 #include "nsHTMLAreaAccessible.h"
@@ -532,7 +533,8 @@ nsAccessibilityService::CreateHyperTextAccessible(nsISupports *aFrame, nsIAccess
 
   nsCOMPtr<nsIContent> content(do_QueryInterface(node));
   NS_ENSURE_TRUE(content, NS_ERROR_FAILURE);
-  if (content->HasAttr(kNameSpaceID_None, nsAccessibilityAtoms::onclick)) {
+  
+  if (nsAccessibilityUtils::HasListener(content, NS_LITERAL_STRING("click"))) {
     // nsLinkableAccessible inherits from nsHyperTextAccessible, but
     // it also includes code for dealing with the onclick
     *aAccessible = new nsLinkableAccessible(node, weakShell);
@@ -1249,7 +1251,7 @@ NS_IMETHODIMP nsAccessibilityService::GetAccessible(nsIDOMNode *aNode,
   // correspond to the doc accessible and will be created in any case
   if (!newAcc && content->Tag() != nsAccessibilityAtoms::body && content->GetParent() && 
       (content->IsFocusable() ||
-       content->HasAttr(kNameSpaceID_None, nsAccessibilityAtoms::onclick) ||
+       nsAccessibilityUtils::HasListener(content, NS_LITERAL_STRING("click")) ||
        content->HasAttr(kNameSpaceID_WAIProperties, nsAccessibilityAtoms::describedby) ||
        content->HasAttr(kNameSpaceID_WAIProperties, nsAccessibilityAtoms::labelledby) ||
        content->HasAttr(kNameSpaceID_WAIProperties, nsAccessibilityAtoms::required) ||
