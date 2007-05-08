3b3a28f255d99160f8c7077b43a7f7c191b1eb29
sspitzer@mozilla.org <sspitzer@mozilla.org>
2007-05-08 14:19:39 -0700

    fix for bug #372051:  problems when moving a livemark containerr=mano.  Thanks to onemen.one@gmail.com for the patch.

--- toolkit/components/places/src/nsLivemarkService.js
+++ toolkit/components/places/src/nsLivemarkService.js
@@ -423,13 +423,13 @@ LivemarkService.prototype = {
     var oldURI = this._livemarks[index].folderURI;
     var feedURIString = this._ans.getAnnotationString(oldURI, LMANNO_FEEDURI);
     this._ans.removeAnnotation(oldURI, LMANNO_FEEDURI);
-    this._ans.setAnnotation(newURI, LMANNO_FEEDURI, feedURIString, 0, 
+    this._ans.setAnnotationString(newURI, LMANNO_FEEDURI, feedURIString, 0, 
                             this._ans.EXPIRE_NEVER);
     
     // Update the annotation that maps the folder URI to the livemark site URI
     var siteURIString;
     try {
-      siteURIString = this._ans.GetAnnotationString(oldURI, LMANNO_SITEURI);
+      siteURIString = this._ans.getAnnotationString(oldURI, LMANNO_SITEURI);
     }
     catch (ex) {
       // will throw if no annotation
@@ -437,7 +437,7 @@ LivemarkService.prototype = {
 
     if (siteURIString) {
       this._ans.removeAnnotation(oldURI, LMANNO_SITEURI);
-      this._ans.setAnnotation(newURI, LMANNO_SITEURI, siteURIString, 0,
+      this._ans.setAnnotationString(newURI, LMANNO_SITEURI, siteURIString, 0,
                               this._ans.EXPIRE_NEVER);
     }
   },
