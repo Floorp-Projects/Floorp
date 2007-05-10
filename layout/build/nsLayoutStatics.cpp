eedb90c553fc15327f26df7c00934d51125edf30
jst@mozilla.org <jst@mozilla.org>
2007-05-09 17:42:33 -0700

    Backing out Jonas' last changes due to tinderbox orange, per Jonas' request.

--- layout/build/nsLayoutStatics.cpp
+++ layout/build/nsLayoutStatics.cpp
@@ -78,7 +78,6 @@
 #include "txMozillaXSLTProcessor.h"
 #include "nsDOMStorage.h"
 #include "nsCellMap.h"
-#include "nsCCUncollectableMarker.h"
 #include "nsTextFrameTextRunCache.h"
 
 #ifdef MOZ_XUL
@@ -214,12 +213,6 @@ nsLayoutStatics::Initialize()
   rv = nsDOMStorageManager::Initialize();
   if (NS_FAILED(rv)) {
     NS_ERROR("Could not initialize nsDOMStorageManager");
-    return rv;
-  }
-
-  rv = nsCCUncollectableMarker::Init();
-  if (NS_FAILED(rv)) {
-    NS_ERROR("Could not initialize nsCCUncollectableMarker");
     return rv;
   }
 
