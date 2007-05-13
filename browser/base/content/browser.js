66932e16e53c724bf9c6f2088e8f608b79a665db
asqueella@gmail.com <asqueella@gmail.com>
2007-05-13 08:47:05 -0700

    Bug 338864 - ctrl+enter doesn't work if the url starts with wwwp=Simon Bunzli <zeniko@gmail.com>r=gavin

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2330,7 +2330,7 @@ function canonizeUrl(aTriggeringEvent, aPostDataRef) {
   // Since this function is called from handleURLBarCommand, which receives
   // both mouse (from the go button) and keyboard events, we also make sure not
   // to do the fixup unless we get a keyboard event, to match user expectations.
-  if (!/^(www|http)|\/\s*$/i.test(url) &&
+  if (!/^(www|https?)\b|\/\s*$/i.test(url) &&
       (aTriggeringEvent instanceof KeyEvent)) {
 #ifdef XP_MACOSX
     var accel = aTriggeringEvent.metaKey;
