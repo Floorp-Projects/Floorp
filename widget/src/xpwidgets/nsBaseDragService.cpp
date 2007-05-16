f53073946d2ec527c101b12d3ffae880397f0f21
enndeakin@sympatico.ca <enndeakin@sympatico.ca>
2007-05-16 11:05:23 -0700

    Bug 376238, add a pref to disable drag images, r=sr=roc

--- widget/src/xpwidgets/nsBaseDragService.cpp
+++ widget/src/xpwidgets/nsBaseDragService.cpp
@@ -69,12 +69,15 @@
 #include "nsIViewObserver.h"
 #include "nsRegion.h"
 #include "nsGUIEvent.h"
+#include "nsIPrefService.h"
 
 #ifdef MOZ_CAIRO_GFX
 #include "gfxContext.h"
 #include "gfxImageSurface.h"
 
 #endif
+
+#define DRAGIMAGES_PREF "nglayout.enable_drag_images"
 
 NS_IMPL_ADDREF(nsBaseDragService)
 NS_IMPL_RELEASE(nsBaseDragService)
@@ -412,8 +415,14 @@ nsBaseDragService::DrawDrag(nsIDOMNode* aDOMNode,
   if (!presShell)
     return NS_ERROR_FAILURE;
 
+  // check if drag images are disabled
+  PRBool enableDragImages = PR_TRUE;
+  nsCOMPtr<nsIPrefBranch> prefs(do_GetService(NS_PREFSERVICE_CONTRACTID));
+  if (prefs)
+    prefs->GetBoolPref(DRAGIMAGES_PREF, &enableDragImages);
+
   // didn't want an image, so just set the screen rectangle to the frame size
-  if (!mHasImage) {
+  if (!enableDragImages || !mHasImage) {
     // if a region was specified, set the screen rectangle to the area that
     // the region occupies
     if (aRegion) {
