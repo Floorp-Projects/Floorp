ed68018f383116d14363562b4927bcb295d864b3
sspitzer@mozilla.org <sspitzer@mozilla.org>
2007-05-27 20:57:20 -0700

    fix for bug #381750: with lots of bookmarks, opening up the bookmark manager dialog is slowr=dietrich

--- browser/components/places/content/controller.js
+++ browser/components/places/content/controller.js
@@ -41,7 +41,7 @@ const NHRVO = Ci.nsINavHistoryResultViewObserver;
 
 // XXXmano: we should move most/all of these constants to PlacesUtils
 const ORGANIZER_ROOT_HISTORY_UNSORTED = "place:beginTime=-2592000000000&beginTimeRef=1&endTime=7200000000&endTimeRef=2&type=1";
-const ORGANIZER_ROOT_BOOKMARKS = "place:folder=2&group=3&excludeItems=1";
+const ORGANIZER_ROOT_BOOKMARKS = "place:folder=2&group=3&excludeItems=1&queryType=1";
 const ORGANIZER_SUBSCRIPTIONS_QUERY = "place:annotation=livemark%2FfeedURI";
 
 // No change to the view, preserve current selection
