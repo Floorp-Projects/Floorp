8f99183f16a4a99a28c1063742f8fe1aa561e904
mcsmurf@mcsmurf.de <mcsmurf@mcsmurf.de>
2007-05-29 12:56:24 -0700

    Add other-licenses/7zstub/seamonkey to default checkout for suite - Bug 351917, r=bsmedberg via IRC

--- client.mk
+++ client.mk
@@ -247,6 +247,7 @@ MODULES_suite :=                                \
   mozilla/mailnews                              \
   mozilla/themes                                \
   mozilla/suite                                 \
+  mozilla/other-licenses/7zstub/seamonkey       \
   $(NULL)
 
 LOCALES_suite :=                                \
