72244a2fb8931df9c5515dc51e84f979446babc2
asqueella@gmail.com <asqueella@gmail.com>
2007-06-07 15:34:06 -0700

    Bug 381499 - Components.utils.import reports NS_ERROR_FAILURE when the file not existsswitch to NS_ERROR_FILE_NOT_FOUND and update testsr=sayrer, sr=brendan

--- tools/test-harness/xpcshell-simple/head.js
+++ tools/test-harness/xpcshell-simple/head.js
@@ -142,7 +142,7 @@ function do_import_script(topsrcdirRelativePath) {
   load(scriptPath);
 }
 
-function do_get_file(path) {
+function do_get_file(path, allowInexistent) {
   var comps = path.split("/");
   try {
     // The following always succeeds on Windows because we use cygpath with
@@ -168,7 +168,12 @@ function do_get_file(path) {
       lf.append(comps[i]);
   }
 
-  do_check_true(lf.exists());
+  if (!allowInexistent) {
+    if (!lf.exists()) {
+      print(lf.path + " doesn't exist\n");
+    }
+    do_check_true(lf.exists());
+  }
 
   return lf;
 }
