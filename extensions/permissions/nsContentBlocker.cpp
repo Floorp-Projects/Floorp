4f3b4ab01c19fa0db8ae70ac836610e25dd5296e
kherron@fmailbox.com <kherron@fmailbox.com>
2007-06-17 06:50:50 -0700

    Bug 375314 - discriminate ping and xmlhttprequests in content policy check. Patch by Wladimir Palant <trev.moz@adblockplus.org>, r=jst, sr=peterv.

--- extensions/permissions/nsContentBlocker.cpp
+++ extensions/permissions/nsContentBlocker.cpp
@@ -60,7 +60,11 @@ static const char *kTypeString[NUMBER_OF_TYPES] = {"other",
                                                    "object",
                                                    "document",
                                                    "subdocument",
-                                                   "refresh"};
+                                                   "refresh",
+                                                   "xbl",
+                                                   "ping",
+                                                   "xmlhttprequest",
+                                                   "objectsubrequest"};
 
 
 NS_IMPL_ISUPPORTS3(nsContentBlocker, 
