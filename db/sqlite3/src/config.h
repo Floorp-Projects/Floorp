f9cbdb055ee19eb1fe06a019762d081abb69b563
sdwilsh@shawnwilsher.com <sdwilsh@shawnwilsher.com>
2007-06-19 16:47:38 -0700

    Backout of Bug 341137

---
+++ db/sqlite3/src/config.h
@@ -0,0 +1,11 @@
+
+#ifndef _sqlite3_config_h
+#define _sqlite3_config_h
+
+#include "prcpucfg.h"
+
+#define HAVE_USLEEP 1
+
+#define SQLITE_PTR_SZ PR_BYTES_PER_WORD
+
+#endif /* _sqlite3_config_h */
