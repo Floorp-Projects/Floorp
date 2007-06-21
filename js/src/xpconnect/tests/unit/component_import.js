7cde8caad2d964051af0159557f19f00a02c47a3
sayrer@gmail.com <sayrer@gmail.com>
2007-06-20 21:53:35 -0700

    Bug 381651. Tweak XPCOMUtils. Patch by Nickolay_Ponomarev <asqueella@gmail.com>. r=sayrer, sr=bsmedberg

--- js/src/xpconnect/tests/unit/component_import.js
+++ js/src/xpconnect/tests/unit/component_import.js
@@ -34,36 +34,32 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
 function FooComponent() {
+  this.wrappedJSObject = this;
+  this.postRegisterCalled = gPostRegisterCalled;
 }
 FooComponent.prototype =
 {
+  // nsIClassInfo + information for XPCOM registration code in XPCOMUtils.jsm
+  classDescription:  "Foo Component",
+  classID:           Components.ID("{6b933fe6-6eba-4622-ac86-e4f654f1b474}"),
+  contractID:       "@mozilla.org/tests/module-importer;1",
+
   // nsIClassInfo
+  implementationLanguage: Components.interfaces.nsIProgrammingLanguage.JAVASCRIPT,
+  flags: 0,
+
   getInterfaces: function getInterfaces(aCount) {
     var interfaces = [Components.interfaces.nsIClassInfo];
     aCount.value = interfaces.length;
     return interfaces;
   },
 
-  // nsIClassInfo
   getHelperForLanguage: function getHelperForLanguage(aLanguage) {
     return null;
   },
-
-  // nsIClassInfo
-  classDescription: "Module importer test 1",
-
-  // nsIClassInfo
-  classID: Components.ID("{6b933fe6-6eba-4622-ac86-e4f654f1b474}"),
-
-  // nsIClassInfo
-  contractID: "@mozilla.org/tests/module-importer;1",
-
-  // nsIClassInfo
-  implementationLanguage: Components.interfaces.nsIProgrammingLanguage.JAVASCRIPT,
-
-  // nsIClassInfo
-  flags: 0,
 
   // nsISupports
   QueryInterface: function QueryInterface(aIID) {
@@ -79,50 +75,59 @@ function BarComponent() {
 }
 BarComponent.prototype =
 {
+  // nsIClassInfo + information for XPCOM registration code in XPCOMUtils.jsm
+  classDescription: "Module importer test 2",
+  classID: Components.ID("{708a896a-b48d-4bff-906e-fc2fd134b296}"),
+  contractID: "@mozilla.org/tests/module-importer;2",
+
   // nsIClassInfo
+  implementationLanguage: Components.interfaces.nsIProgrammingLanguage.JAVASCRIPT,
+  flags: 0,
+
   getInterfaces: function getInterfaces(aCount) {
     var interfaces = [Components.interfaces.nsIClassInfo];
     aCount.value = interfaces.length;
     return interfaces;
   },
 
-  // nsIClassInfo
   getHelperForLanguage: function getHelperForLanguage(aLanguage) {
     return null;
   },
 
-  // nsIClassInfo
-  classDescription: "Module importer test 2",
-
-  // nsIClassInfo
-  classID: Components.ID("{708a896a-b48d-4bff-906e-fc2fd134b296}"),
-
-  // nsIClassInfo
-  contractID: "@mozilla.org/tests/module-importer;2",
-
-  // nsIClassInfo
-  implementationLanguage: Components.interfaces.nsIProgrammingLanguage.JAVASCRIPT,
-
-  // nsIClassInfo
-  flags: 0
+  // nsISupports
+  QueryInterface: XPCOMUtils.generateQI([Components.interfaces.nsIClassInfo])
 };
 
-Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+function do_check_true(cond, text) {
+  // we don't have the test harness' utilities in this scope, so we need this
+  // little helper. In the failure case, the exception is propagated to the
+  // caller in the main run_test() function, and the test fails.
+  if (!cond)
+    throw "Failed check: " + text;
+}
 
-var NSGetModule = XPCOMUtils.generateNSGetModule([
-  {
-    className:  FooComponent.prototype.classDescription,
-    cid:        FooComponent.prototype.classID,
-    contractID: FooComponent.prototype.contractID,
-    factory: XPCOMUtils.generateFactory(FooComponent)
-  },
-  {
-    className:  BarComponent.prototype.classDescription,
-    cid:        BarComponent.prototype.classID,
-    contractID: BarComponent.prototype.contractID,
-    factory: XPCOMUtils.generateFactory(
-      BarComponent,
-      [Components.interfaces.nsIClassInfo]
-    )
-  }
-], null, null);
+function postRegister(componentManager, file, componentsArray) {
+  const Ci = Components.interfaces;
+  do_check_true(componentManager instanceof Ci.nsIComponentManager,
+               "postRegister: componentManager param is ok");
+  do_check_true(file instanceof Ci.nsIFile,
+                "postRegister: file param is ok");
+  do_check_true(componentsArray === gComponentsArray,
+                "postRegister: componentsArray param is ok");
+  gPostRegisterCalled = true;
+}
+
+function preUnregister(componentManager, file, componentsArray) {
+  const Ci = Components.interfaces;
+  do_check_true(componentManager instanceof Ci.nsIComponentManager,
+               "postRegister: componentManager param is ok");
+  do_check_true(file instanceof Ci.nsIFile,
+                "postRegister: file param is ok");
+  do_check_true(componentsArray === gComponentsArray,
+                "postRegister: componentsArray param is ok");
+}
+
+var gPostRegisterCalled = false;
+var gComponentsArray = [FooComponent, BarComponent];
+var NSGetModule = XPCOMUtils.generateNSGetModule(gComponentsArray,
+                    postRegister, preUnregister);
