084647c4b254d7c931030fae5a1f5089b22a1a7c
scott@scott-macgregor.org <scott@scott-macgregor.org>
2007-07-02 11:20:31 -0700

    fix a character encoding issue for nemeth's name in the licenses.
    patch by Ryan VanderMeulen <ryanvm@gmail.com>
    
    NPOTB

--- extensions/spellcheck/hunspell/src/mozHunspell.h
+++ extensions/spellcheck/hunspell/src/mozHunspell.h
@@ -12,14 +12,14 @@
  * License.
  * 
  * The Initial Developers of the Original Code are Kevin Hendricks (MySpell)
- * and Laszlo Nemeth (Hunspell). Portions created by the Initial Developers
+ * and László Németh (Hunspell). Portions created by the Initial Developers
  * are Copyright (C) 2002-2005 the Initial Developers. All Rights Reserved.
  * 
  * Contributor(s): Kevin Hendricks (kevin.hendricks@sympatico.ca)
- *                 László Németh (nemethl@gyorsposta.hu)
  *                 David Einstein (deinst@world.std.com)
  *                 Michiel van Leeuwen (mvl@exedo.nl)
  *                 Caolan McNamara (cmc@openoffice.org)
+ *                 László Németh (nemethl@gyorsposta.hu)
  *                 Davide Prina
  *                 Giuseppe Modugno
  *                 Gianluca Turconi
