7f47643f523f31afbfee3dad17d7b6f74696fe4f
bzbarsky@mit.edu <bzbarsky@mit.edu>
2007-07-10 20:42:11 -0700

    Don't start layout for XML just because the root has been opened.  And don't start layout until <html:head> closes if <html:head> gets opened.  For XHTML documents, this should prevent flashes of unstyled content.
    Also, don't flush tags unless forced to if a sheet is pending.
    
    Bug 380028, r+sr=sicking

--- content/base/src/nsContentSink.h
+++ content/base/src/nsContentSink.h
@@ -211,6 +211,10 @@ protected:
 
   void TryToScrollToRef();
 
+  // Later on we might want to make this more involved somehow
+  // (e.g. stop waiting after some timeout or whatnot).
+  PRBool WaitForPendingSheets() { return mPendingSheetCount > 0; }
+
 private:
   // People shouldn't be allocating this class directly.  All subclasses should
   // be allocated using a zeroing operator new.
@@ -276,7 +280,9 @@ protected:
   PRUint8 mHaveOfflineResources : 1;
   // true if offline-resource links should be saved to the offline cache
   PRUint8 mSaveOfflineResources : 1;
-
+  // If true, we deferred notifications until sheets load
+  PRUint8 mDeferredFlushTags : 1;
+  
   // -- Can interrupt parsing members --
   PRUint32 mDelayTimerStart;
 
