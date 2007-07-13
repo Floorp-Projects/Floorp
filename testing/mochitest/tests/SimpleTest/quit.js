96d6e020923c890374cbb4ba5d16ce15133ac0cd
jwalden@mit.edu <jwalden@mit.edu>
2007-07-12 20:38:31 -0700

    I think I figured out the problem with bug 384192; the code added to quit.js is also used in the browser tests, but it triggers an undefined-variable exception there, and I think the tests are getting killed by test timeout code that doesn't print a helpful 'tests timed out' message to give a little help determining the cause of the failure.  We shall see...

--- testing/mochitest/tests/SimpleTest/quit.js
+++ testing/mochitest/tests/SimpleTest/quit.js
@@ -100,11 +100,9 @@ function goQuitApplication()
     return false;
   }
   
-/*
   // shut down cross-domain mapping (really reverts preference values)
-  if (crossDomain)
+  if (typeof crossDomain != "undefined")
     crossDomain.disable();
-*/
 
   const kAppStartup = '@mozilla.org/toolkit/app-startup;1';
   const kAppShell   = '@mozilla.org/appshell/appShellService;1';
