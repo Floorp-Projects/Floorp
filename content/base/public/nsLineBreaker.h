6ac430a20ed5c49415c03bc69e62c8b202b47900
roc+@cs.cmu.edu <roc+@cs.cmu.edu>
2007-07-18 20:26:51 -0700

    Bug 336959. Use Pango to break inside Thai/Lao runs. Patch by Theppitak Karoonboonyanan, r+sr=roc

--- content/base/public/nsLineBreaker.h
+++ content/base/public/nsLineBreaker.h
@@ -96,11 +96,12 @@ public:
   static inline PRBool IsComplexChar(PRUnichar u)
   {
     return IsComplexASCIIChar(u) ||
-           (0x1100 <= u && u <= 0x11ff) ||
-           (0x2000 <= u && u <= 0x21ff) ||
-           (0x2e80 <= u && u <= 0xd7ff) ||
-           (0xf900 <= u && u <= 0xfaff) ||
-           (0xff00 <= u && u <= 0xffef);
+           (0x0e01 <= u && u <= 0x0edf) || // Thai & Lao
+           (0x1100 <= u && u <= 0x11ff) || // Hangul Jamo
+           (0x2000 <= u && u <= 0x21ff) || // Punctuations and Symbols
+           (0x2e80 <= u && u <= 0xd7ff) || // several CJK blocks
+           (0xf900 <= u && u <= 0xfaff) || // CJK Compatibility Idographs
+           (0xff00 <= u && u <= 0xffef);   // Halfwidth and Fullwidth Forms
   }
 
   // Normally, break opportunities exist at the end of each run of whitespace
