1b506091b9d94c39c99c3ed5b6ce71f259acc208
sdwilsh@shawnwilsher.com <sdwilsh@shawnwilsher.com>
2007-07-22 11:57:08 -0700

    Bustage fix

--- uriloader/exthandler/unix/nsOSHelperAppService.h
+++ uriloader/exthandler/unix/nsOSHelperAppService.h
@@ -61,7 +61,7 @@ public:
   already_AddRefed<nsIMIMEInfo> GetMIMEInfoFromOS(const nsACString& aMimeType,
                                                   const nsACString& aFileExt,
                                                   PRBool     *aFound);
-  already_AddRefed<nsIHanderInfo> GetProtocolInfoFromOS(const nsACString &aScheme);
+  already_AddRefed<nsIHandlerInfo> GetProtocolInfoFromOS(const nsACString &aScheme);
 
   // override nsIExternalProtocolService methods
   nsresult OSProtocolHandlerExists(const char * aProtocolScheme, PRBool * aHandlerExists);
