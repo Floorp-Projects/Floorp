d813830a5fea5a2f3c48167e96dcf67000bb2bfa
sdwilsh@shawnwilsher.com <sdwilsh@shawnwilsher.com>
2007-07-22 11:40:24 -0700

    Bug 388701 - nsExternalHelperAppService::GetProtocolHandlerInfo should get OS Specific implementation.  r=cbiesinger, sr=dmose

--- uriloader/exthandler/win/nsOSHelperAppService.cpp
+++ uriloader/exthandler/win/nsOSHelperAppService.cpp
@@ -587,3 +587,25 @@ already_AddRefed<nsIMIMEInfo> nsOSHelperAppService::GetMIMEInfoFromOS(const nsAC
   }
   return mi;
 }
+
+already_AddRefed<nsIHandlerInfo>
+nsOSHelperAppService::GetProtocolInfoFromOS(const nsACString &aScheme)
+{
+  NS_ASSERTION(!aScheme.IsEmpty(), "No scheme was specified!");
+
+  PRBool exists;
+  nsresult rv = OSProtocolHandlerExists(nsPromiseFlatCString(aScheme).get(),
+                                        &exists);
+  NS_ENSURE_SUCCESS(rv, nsnull);
+
+  nsMIMEInfoWin *handlerInfo = new nsMIMEInfoWin();
+  NS_ENSURE_TRUE(handlerInfo, nsnull);
+  NS_ADDREF(handlerInfo);
+
+  nsAutoString desc;
+  GetApplicationDescription(aScheme, desc);
+  handlerInfo->SetDefaultDescription(desc);
+
+  return handlerInfo;
+}
+
