91bbdaa821355dfd0bf5e011e53df8eefcc7d5b3
sspitzer@mozilla.org <sspitzer@mozilla.org>
2007-07-28 20:19:33 -0700

    fix for bug #384897:  in-product links to AMO should be https:// (and not http://)
    patch by Simon 'sipaq' Paquet <bugzilla@babylonsounds.com>
    
    r=sspitzer, morgamic
    a=mconnor for M7

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -148,8 +148,8 @@ pref("extensions.update.url", "chrome://mozapps/locale/extensions/extensions.pro
 pref("extensions.update.interval", 86400);  // Check for updates to Extensions and 
                                             // Themes every day
 // Non-symmetric (not shared by extensions) extension-specific [update] preferences
-pref("extensions.getMoreExtensionsURL", "http://%LOCALE%.add-ons.mozilla.com/%LOCALE%/%APP%/%VERSION%/extensions/");
-pref("extensions.getMoreThemesURL", "http://%LOCALE%.add-ons.mozilla.com/%LOCALE%/%APP%/%VERSION%/themes/");
+pref("extensions.getMoreExtensionsURL", "https://%LOCALE%.add-ons.mozilla.com/%LOCALE%/%APP%/%VERSION%/extensions/");
+pref("extensions.getMoreThemesURL", "https://%LOCALE%.add-ons.mozilla.com/%LOCALE%/%APP%/%VERSION%/themes/");
 pref("extensions.dss.enabled", false);          // Dynamic Skin Switching                                               
 pref("extensions.dss.switchPending", false);    // Non-dynamic switch pending after next
                                                 // restart.
