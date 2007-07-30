66eb28821ec7c48d629f30012f235699c3ea7ef2
sspitzer@mozilla.org <sspitzer@mozilla.org>
2007-07-29 21:11:52 -0700

    fix for bug #389202:  errors when I attempt to add a new folder with a description (new folder dialog doesn't dismiss)
    r=dietrich, a=mconnor for M7

--- browser/components/places/tests/unit/test_placesTxn.js
+++ browser/components/places/tests/unit/test_placesTxn.js
@@ -121,11 +121,22 @@ var bmStartIndex = 1;
 
 // main
 function run_test() {
+  const DESCRIPTION_ANNO = "bookmarkProperties/description";
+  var testDescription = "this is my test description";
+  var annotationService = Cc["@mozilla.org/browser/annotation-service;1"].
+                          getService(Ci.nsIAnnotationService);
 
-  //Test creating a folder
-  var txn1 = ptSvc.createFolder("Testing folder", root, bmStartIndex);
+  //Test creating a folder with a description
+  var annos = [{ name: DESCRIPTION_ANNO,
+                 type: Ci.nsIAnnotationService.TYPE_STRING,
+                flags: 0,
+                value: testDescription,
+              expires: Ci.nsIAnnotationService.EXPIRE_NEVER }];
+  var txn1 = ptSvc.createFolder("Testing folder", root, bmStartIndex, annos);
   txn1.doTransaction();
   var folderId = bmsvc.getChildFolder(root, "Testing folder");
+  do_check_eq(testDescription, 
+              annotationService.getItemAnnotation(folderId, DESCRIPTION_ANNO));
   do_check_eq(observer._itemAddedIndex, bmStartIndex);
   do_check_eq(observer._itemAddedParent, root);
   do_check_eq(observer._itemAddedId, folderId);
