4a6e81a98b88a9df71de1452c6c2f39ab37e9293
bzbarsky@mit.edu <bzbarsky@mit.edu>
2007-08-10 15:55:21 -0700

    Back out bug 390425 to fix performance regression.

--- layout/base/nsCSSFrameConstructor.h
+++ layout/base/nsCSSFrameConstructor.h
@@ -111,7 +111,8 @@ public:
   nsresult ContentInserted(nsIContent*            aContainer,
                            nsIContent*            aChild,
                            PRInt32                aIndexInContainer,
-                           nsILayoutHistoryState* aFrameState);
+                           nsILayoutHistoryState* aFrameState,
+                           PRBool                 aInReinsertContent);
 
   nsresult ContentRemoved(nsIContent*     aContainer,
                           nsIContent*     aChild,
@@ -753,16 +754,7 @@ private:
 
   nsresult RecreateFramesForContent(nsIContent*      aContent);
 
-  // If removal of aFrame from the frame tree requires reconstruction of some
-  // containing block (either of aFrame or of its parent) due to {ib} splits,
-  // recreate the relevant containing block.  The return value indicates
-  // whether this happened.  If this method returns true, *aResult is the
-  // return value of ReframeContainingBlock.  If this method returns false, the
-  // value of *aResult is no affected.  aFrame and aResult must not be null.
-  // aFrame must be the result of a GetPrimaryFrameFor() call (which means its
-  // parent is also not null).
-  PRBool MaybeRecreateContainerForIBSplitterFrame(nsIFrame* aFrame,
-                                                  nsresult* aResult);
+  PRBool MaybeRecreateContainerForIBSplitterFrame(nsIFrame* aFrame, nsresult* aResult);
 
   nsresult CreateContinuingOuterTableFrame(nsIPresShell*    aPresShell, 
                                            nsPresContext*  aPresContext,
@@ -846,9 +838,17 @@ private:
   PRBool AreAllKidsInline(nsIFrame* aFrameList);
 
   PRBool WipeContainingBlock(nsFrameConstructorState& aState,
-                             nsIFrame*                aContainingBlock,
+                             nsIFrame*                blockContent,
                              nsIFrame*                aFrame,
-                             const nsFrameItems&      aFrameList);
+                             nsIFrame*                aFrameList);
+
+  PRBool NeedSpecialFrameReframe(nsIContent*      aParent1,
+                                 nsIContent*      aParent2,
+                                 nsIFrame*&       aParentFrame,
+                                 nsIContent*      aChild,
+                                 PRInt32          aIndexInContainer,
+                                 nsIFrame*&       aPrevSibling,
+                                 nsIFrame*        aNextSibling);
 
   nsresult ReframeContainingBlock(nsIFrame* aFrame);
 
