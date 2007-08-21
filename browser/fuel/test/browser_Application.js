6eaf77cad11fb44d69a47d949fdad8a36f87bc04
mark.finkle@gmail.com <mark.finkle@gmail.com>
2007-08-21 12:59:46 -0700

    b=391902. r=gavin. FUEL Application.console.open() fails

--- browser/fuel/test/browser_Application.js
+++ browser/fuel/test/browser_Application.js
@@ -1,3 +1,6 @@
+const Ci = Components.interfaces;
+const Cc = Components.classes;
+
 function test() {
   ok(Application, "Check global access to Application");
   
@@ -5,4 +8,19 @@ function test() {
   ok(Application.id, "Check to see if an ID exists for the Application");
   ok(Application.name, "Check to see if a name exists for the Application");
   ok(Application.version, "Check to see if a version exists for the Application");
+  
+  var wMediator = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+  var console = wMediator.getMostRecentWindow("global:console");
+  if (!console) {
+    Application.console.open();
+    setTimeout(checkConsole, 500);
+  }
+}
+
+function checkConsole() {
+  var wMediator = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+  var console = wMediator.getMostRecentWindow("global:console");
+  ok(console, "Check to see if the console window opened");
+  if (console)
+    console.close();
 }
