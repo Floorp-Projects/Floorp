d85686cb70068a8e6954c39a8af1beb242b5e5b6
Dan Mills <thunder@mozilla.com>
2007-09-24 00:21:22 -0700

    Add code to disable any auth prompt and turn them into failures (from the microsummary svc). Get/save sync service password in the password manager (ui missing). Fix a bug in the reconciler that prevented it from returning any conflicts. Other misc cleanup.

--- services/sync/nsBookmarksSyncService.js
+++ services/sync/nsBookmarksSyncService.js
@@ -92,15 +92,15 @@ BookmarksSyncService.prototype = {
   },
 
   _init: function BSS__init() {
-    var serverUrl = "http://sync.server.url/";
+    let serverURL = 'https://dotmoz.mozilla.org/';
     try {
-      var branch = Cc["@mozilla.org/preferences-service;1"].
+      let branch = Cc["@mozilla.org/preferences-service;1"].
         getService(Ci.nsIPrefBranch);
-      serverUrl = branch.getCharPref("browser.places.sync.serverUrl");
+      serverURL = branch.getCharPref("browser.places.sync.serverURL");
     }
     catch (ex) { /* use defaults */ }
-    LOG("Bookmarks sync server: " + serverUrl);
-    this._dav = new DAVCollection(serverUrl);
+    LOG("Bookmarks login server: " + serverURL);
+    this._dav = new DAVCollection(serverURL);
   },
 
   _wrapNode: function BSS__wrapNode(node) {
@@ -159,7 +159,7 @@ BookmarksSyncService.prototype = {
   },
 
   _nodeParentsInt: function BSS__nodeParentsInt(guid, tree, parents) {
-    if (tree[guid].parentGuid == null)
+    if (tree[guid] && tree[guid].parentGuid == null)
       return parents;
     parents.push(tree[guid].parentGuid);
     return this._nodeParentsInt(tree[guid].parentGuid, tree, parents);
@@ -292,12 +292,26 @@ BookmarksSyncService.prototype = {
     }
   },
 
+  // FIXME: todo: change the conflicts data structure to hold more
+  // information about which specific command pars conflict; an what
+  // commands would apply once those are resolved.  Perhaps something
+  // like:
+
+  // [[direct-conflictA, 2nd-degree-conflictA1, ...],
+  //  [direct-conflictB, 2nd-degree-conflict-B1, ...], ...]
+
+  // possible problem: a 2nd-degree conflict could show up in multiple
+  // lists (that is, there are multiple direct conflicts that prevent
+  // another command pair from cleanly applying). maybe:
+
+  // [[[dcA1, dcB1], [dcA2. dcB2], ...],
+  //  [2dcA1, 2dcA2, ...], [2dcB1, 2dcB2, ...]]
+
   _reconcile: function BSS__reconcile(onComplete, commandLists) {
     let generator = yield;
     this._timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
-    let callback = bind2(this,
-                         function(event) { handleEvent(generator, event); });
-    let listener = new EventListener(callback);
+    let handlers = this._handlersForGenerator(generator);
+    let listener = new EventListener(handlers['complete']);
 
     let [listA, listB] = commandLists;
     let propagations = [[], []];
@@ -335,10 +349,10 @@ BookmarksSyncService.prototype = {
       for (let j = 0; j < listB.length; j++) {
         if (this._conflicts(listA[i], listB[j]) ||
             this._conflicts(listB[j], listA[i])) {
-          if (conflicts[0].some(
+          if (!conflicts[0].some(
             function(elt) { return elt.guid == listA[i].guid }))
             conflicts[0].push(listA[i]);
-          if (conflicts[1].some(
+          if (!conflicts[1].some(
             function(elt) { return elt.guid == listB[j].guid }))
             conflicts[1].push(listB[j]);
         }
@@ -563,10 +577,6 @@ BookmarksSyncService.prototype = {
       var propagations = [server['updates'], localUpdates];
       var conflicts = [[],[]];
 
-      // reconciliation was wrapped in this - why?
-      //if (server['status'] == 1 && localUpdates.length > 0) {
-      //}
-
       LOG("Reconciling updates");
       asyncRun(bind2(this, this._reconcile),
                handlers['complete'], [localUpdates, server.updates]);
@@ -590,15 +600,13 @@ BookmarksSyncService.prototype = {
       }
 
       if (conflicts && conflicts[0] && conflicts[0].length) {
-        //var combinedCommands = this._combineCommands(propagations[0]);
         LOG("\nWARNING: Conflicts found, but we don't resolve conflicts yet!\n");
-        LOG("Conflicts(1) " + uneval(this._combineCommands(conflicts[0])));
+        LOG("Conflicts(1) " + uneval(conflicts[0]));
       }
 
       if (conflicts && conflicts[1] && conflicts[1].length) {
-        //var combinedCommands = this._combineCommands(propagations[0]);
         LOG("\nWARNING: Conflicts found, but we don't resolve conflicts yet!\n");
-        LOG("Conflicts(2) " + uneval(this._combineCommands(conflicts[1])));
+        LOG("Conflicts(2) " + uneval(conflicts[1]));
       }
 
       // 3.1) Apply server changes to local store
@@ -793,13 +801,14 @@ BookmarksSyncService.prototype = {
   },
 
   _handlersForGenerator: function BSS__handlersForGenerator(generator) {
-    var h = {load: bind2(this, function(event) { handleEvent(generator, event); }),
+    var h = {load: bind2(this, function(event) { continueGenerator(generator, event); }),
              error: bind2(this, function(event) { LOG("Request failed: " + uneval(event)); })};
     h['complete'] = h['load'];
     return h;
   },
 
   _onLogin: function BSS__onLogin(event) {
+    LOG("Bookmarks sync server: " + this._dav.baseURL);
     this._os.notifyObservers(null, "bookmarks-sync:login", "");
   },
 
@@ -823,8 +832,7 @@ BookmarksSyncService.prototype = {
   sync: function BSS_sync() { asyncRun(bind2(this, this._doSync)); },
 
   login: function BSS_login() {
-    this._dav.login("nobody@mozilla.com", "password", // FIXME
-                    {load: bind2(this, this._onLogin),
+    this._dav.login({load: bind2(this, this._onLogin),
                      error: bind2(this, this._onLoginError)});
   },
 
@@ -834,15 +842,39 @@ BookmarksSyncService.prototype = {
   }
 };
 
+function makeFile(path) {
+  var file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+  file.initWithPath(path);
+  return file;
+}
+
+function makeURI(uriString) {
+  var ioservice = Cc["@mozilla.org/network/io-service;1"].
+                  getService(Ci.nsIIOService);
+  return ioservice.newURI(uriString, null, null);
+}
+
+function LOG(aText) {
+  dump(aText + "\n");
+  var consoleService = Cc["@mozilla.org/consoleservice;1"].
+                       getService(Ci.nsIConsoleService);
+  consoleService.logStringMessage(aText);
+}
+
+function bind2(object, method) {
+  return function innerBind() { return method.apply(object, arguments); }
+}
+
 function asyncRun(func, handler, data) {
   var generator = func(handler, data);
   generator.next();
   generator.send(generator);
 }
 
-function handleEvent(generator, data) {
+function continueGenerator(generator, data) {
   try { generator.send(data); }
   catch (e) {
+    generator.close();
     if (e instanceof StopIteration)
       generator = null;
     else
@@ -874,11 +906,16 @@ EventListener.prototype = {
   }
 };
 
-function DAVCollection(baseUrl) {
-  this._baseUrl = baseUrl;
+function DAVCollection(baseURL) {
+  this._baseURL = baseURL;
+  this._authProvider = new DummyAuthProvider();
 }
 DAVCollection.prototype = {
   _loggedIn: false,
+
+  get baseURL() {
+    return this._baseURL;
+  },
 
   __base64: {},
   __vase64loaded: false,
@@ -914,13 +951,15 @@ DAVCollection.prototype = {
     this._addHandler(request, handlers, "error");
   
     request = request.QueryInterface(Ci.nsIXMLHttpRequest);
-    request.open(op, this._baseUrl + path, true);
+    request.open(op, this._baseURL + path, true);
   
     if (headers) {
       for (var key in headers) {
         request.setRequestHeader(key, headers[key]);
       }
     }
+
+    request.channel.notificationCallbacks = this._authProvider;
 
     return request;
   },
@@ -947,14 +986,41 @@ DAVCollection.prototype = {
 
   // Login / Logout
 
-  login: function DC_login(username, password, handlers) {
+  login: function DC_login(handlers) {
     this._loginHandlers = handlers;
     internalHandlers = {load: bind2(this, this._onLogin),
                         error: bind2(this, this._onLoginError)};
 
-    this._authString = "Basic " +
-      this._base64.Base64.encode(username + ":" + password);
-    headers = {'Authorization': this._authString};
+    try {
+      let uri = makeURI(this._baseURL);
+      let username = 'nobody@mozilla.com';
+      let password;
+
+      let branch = Cc["@mozilla.org/preferences-service;1"].
+        getService(Ci.nsIPrefBranch);
+      username = branch.getCharPref("browser.places.sync.username");
+
+      // fixme: make a request and get the realm
+      let lm = Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager);
+      let logins = lm.findLogins({}, uri.hostPort, null,
+                                 'Use your ldap username/password - dotmoz');
+      LOG("Found " + logins.length + " logins");
+
+      for (let i = 0; i < logins.length; i++) {
+        if (logins[i].username == username) {
+          password = logins[i].password;
+          break;
+        }
+      }
+
+      // FIXME: should we regen this each time to prevent it from staying in memory?
+      this._authString = "Basic " +
+        this._base64.Base64.encode(username + ":" + password);
+      headers = {'Authorization': this._authString};
+    } catch (e) {
+      // try without the auth header?
+      // fixme: may want to just fail here
+    }
 
     let request = this._makeRequest("GET", "", internalHandlers, headers);
     request.send(null);
@@ -968,16 +1034,17 @@ DAVCollection.prototype = {
     //LOG("logged in (" + event.target.status + "):\n" +
     //    event.target.responseText + "\n");
 
-    if (event.target.status != 200) {
+    if (this._authProvider._authFailed || event.target.status >= 400) {
       this._onLoginError(event);
       return;
     }
 
+    // fixme: hacktastic
     let hello = /Hello (.+)@mozilla.com/.exec(event.target.responseText)
     if (hello) {
       this._currentUserPath = hello[1];	
       this._currentUser = this._currentUserPath + "@mozilla.com";
-      this._baseUrl = "http://dotmoz.mozilla.org/~" +
+      this._baseURL = "http://dotmoz.mozilla.org/~" +
         this._currentUserPath + "/";
     }
 
@@ -1047,28 +1114,141 @@ DAVCollection.prototype = {
   }
 };
 
-function makeFile(path) {
-  var file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
-  file.initWithPath(path);
-  return file;
-}
 
-function makeURI(uriString) {
-  var ioservice = Cc["@mozilla.org/network/io-service;1"].
-                  getService(Ci.nsIIOService);
-  return ioservice.newURI(uriString, null, null);
-}
+// Taken from nsMicrosummaryService.js and massaged slightly
+function DummyAuthProvider() {}
+DummyAuthProvider.prototype = {
+  // Implement notification callback interfaces so we can suppress UI
+  // and abort loads for bad SSL certs and HTTP authorization requests.
+  
+  // Interfaces this component implements.
+  interfaces: [Ci.nsIBadCertListener,
+               Ci.nsIAuthPromptProvider,
+               Ci.nsIAuthPrompt,
+               Ci.nsIPrompt,
+               Ci.nsIInterfaceRequestor,
+               Ci.nsISupports],
 
-function bind2(object, method) {
-  return function innerBind() { return method.apply(object, arguments); }
-}
+  // Auth requests appear to succeed when we cancel them (since the server
+  // redirects us to a "you're not authorized" page), so we have to set a flag
+  // to let the load handler know to treat the load as a failure.
+  get _authFailed()         { return this.__authFailed; },
+  set _authFailed(newValue) { return this.__authFailed = newValue },
 
-function LOG(aText) {
-  dump(aText + "\n");
-  var consoleService = Cc["@mozilla.org/consoleservice;1"].
-                       getService(Ci.nsIConsoleService);
-  consoleService.logStringMessage(aText);
-}
+  // nsISupports
+
+  QueryInterface: function DAP_QueryInterface(iid) {
+    if (!this.interfaces.some( function(v) { return iid.equals(v) } ))
+      throw Cr.NS_ERROR_NO_INTERFACE;
+
+    // nsIAuthPrompt and nsIPrompt need separate implementations because
+    // their method signatures conflict.  The other interfaces we implement
+    // within DummyAuthProvider itself.
+    switch(iid) {
+    case Ci.nsIAuthPrompt:
+      return this.authPrompt;
+    case Ci.nsIPrompt:
+      return this.prompt;
+    default:
+      return this;
+    }
+  },
+
+  // nsIInterfaceRequestor
+  
+  getInterface: function DAP_getInterface(iid) {
+    return this.QueryInterface(iid);
+  },
+
+  // nsIBadCertListener
+
+  // Suppress UI and abort secure loads from servers with bad SSL certificates.
+  
+  confirmUnknownIssuer: function DAP_confirmUnknownIssuer(socketInfo, cert, certAddType) {
+    return false;
+  },
+
+  confirmMismatchDomain: function DAP_confirmMismatchDomain(socketInfo, targetURL, cert) {
+    return false;
+  },
+
+  confirmCertExpired: function DAP_confirmCertExpired(socketInfo, cert) {
+    return false;
+  },
+
+  notifyCrlNextupdate: function DAP_notifyCrlNextupdate(socketInfo, targetURL, cert) {
+  },
+
+  // nsIAuthPromptProvider
+  
+  getAuthPrompt: function(aPromptReason, aIID) {
+    this._authFailed = true;
+    throw Cr.NS_ERROR_NOT_AVAILABLE;
+  },
+
+  // HTTP always requests nsIAuthPromptProvider first, so it never needs
+  // nsIAuthPrompt, but not all channels use nsIAuthPromptProvider, so we
+  // implement nsIAuthPrompt too.
+
+  // nsIAuthPrompt
+
+  get authPrompt() {
+    var resource = this;
+    return {
+      QueryInterface: XPCOMUtils.generateQI([Ci.nsIPrompt]),
+      prompt: function(dialogTitle, text, passwordRealm, savePassword, defaultText, result) {
+        resource._authFailed = true;
+        return false;
+      },
+      promptUsernameAndPassword: function(dialogTitle, text, passwordRealm, savePassword, user, pwd) {
+        resource._authFailed = true;
+        return false;
+      },
+      promptPassword: function(dialogTitle, text, passwordRealm, savePassword, pwd) {
+        resource._authFailed = true;
+        return false;
+      }
+    };
+  },
+
+  // nsIPrompt
+
+  get prompt() {
+    var resource = this;
+    return {
+      QueryInterface: XPCOMUtils.generateQI([Ci.nsIPrompt]),
+      alert: function(dialogTitle, text) {
+        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+      },
+      alertCheck: function(dialogTitle, text, checkMessage, checkValue) {
+        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+      },
+      confirm: function(dialogTitle, text) {
+        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+      },
+      confirmCheck: function(dialogTitle, text, checkMessage, checkValue) {
+        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+      },
+      confirmEx: function(dialogTitle, text, buttonFlags, button0Title, button1Title, button2Title, checkMsg, checkValue) {
+        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+      },
+      prompt: function(dialogTitle, text, value, checkMsg, checkValue) {
+        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+      },
+      promptPassword: function(dialogTitle, text, password, checkMsg, checkValue) {
+        resource._authFailed = true;
+        return false;
+      },
+      promptUsernameAndPassword: function(dialogTitle, text, username, password, checkMsg, checkValue) {
+        resource._authFailed = true;
+        return false;
+      },
+      select: function(dialogTitle, text, count, selectList, outSelection) {
+        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+      }
+    };
+  }
+};
 
 function NSGetModule(compMgr, fileSpec) {
   return XPCOMUtils.generateModule([BookmarksSyncService]);
