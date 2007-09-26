d4c815234ec8527b5bdaca2e71214ac80bc093a8
cbeard@h-135.office.mozilla.org <cbeard@h-135.office.mozilla.org>
2007-09-26 12:08:53 -0700

    added preferences pane to main pref dialog and wired everything up (except the password bit) added autoconnect pref and actions removing sync-engine.js as it doesn't look like we're using it anymore

--- services/sync/services-sync.js
+++ services/sync/services-sync.js
@@ -2,4 +2,5 @@ pref("browser.places.sync.serverURL", "http://dotmoz.mozilla.org/");
 pref("browser.places.sync.username", "nobody@mozilla.com");
 pref("extensions.sync.lastversion", "firstrun");
 pref("extensions.sync.lastsync", "");
+pref("extensions.sync.remember", false);
 
