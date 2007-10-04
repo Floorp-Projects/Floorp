fbf4f8759fbfa0364e0ec18b541a5dae4ecbebf7
surkov.alexander@gmail.com <surkov.alexander@gmail.com>
2007-10-04 00:20:37 -0700

    Bug 398176 - dont count invisible tabs in pageInfo screen, r=aaronlev, a=dsicore

--- accessible/src/base/nsAccessibilityUtils.cpp
+++ accessible/src/base/nsAccessibilityUtils.cpp
@@ -38,12 +38,13 @@
 
 #include "nsAccessibilityUtils.h"
 
+#include "nsIAccessibleStates.h"
 #include "nsIAccessibleTypes.h"
 #include "nsPIAccessible.h"
 #include "nsPIAccessNode.h"
 #include "nsAccessibleEventData.h"
 
-#include "nsAccessNode.h"
+#include "nsAccessible.h"
 #include "nsARIAMap.h"
 #include "nsIDocument.h"
 #include "nsIDOMAbstractView.h"
@@ -175,12 +176,30 @@ nsAccUtils::SetAccAttrsForXULSelectControlItem(nsIDOMNode *aNode,
   if (!control)
     return;
 
-  PRUint32 itemsCount;
+  PRUint32 itemsCount = 0;
   control->GetItemCount(&itemsCount);
-  PRInt32 indexOf;
+
+  PRInt32 indexOf = 0;
   control->GetIndexOfItem(item, &indexOf);
 
-  SetAccGroupAttrs(aAttributes, 0, indexOf + 1, itemsCount);
+  PRUint32 setSize = itemsCount, posInSet = indexOf;
+  for (PRUint32 index = 0; index < itemsCount; index++) {
+    nsCOMPtr<nsIDOMXULSelectControlItemElement> currItem;
+    control->GetItemAtIndex(index, getter_AddRefs(currItem));
+    nsCOMPtr<nsIDOMNode> currNode(do_QueryInterface(currItem));
+
+    nsCOMPtr<nsIAccessible> itemAcc;
+    nsAccessNode::GetAccService()->GetAccessibleFor(currNode,
+                                                    getter_AddRefs(itemAcc));
+    if (!itemAcc ||
+        nsAccessible::State(itemAcc) & nsIAccessibleStates::STATE_INVISIBLE) {
+      setSize--;
+      if (index < indexOf)
+        posInSet--;
+    }
+  }
+
+  SetAccGroupAttrs(aAttributes, 0, posInSet + 1, setSize);
 }
 
 PRBool
