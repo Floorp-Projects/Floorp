48541412c120b39629abd7fbdcda42492dc0c4f2
bzbarsky@mit.edu <bzbarsky@mit.edu>
2007-10-05 17:35:00 -0700

    Fix a few issues in bfcache and XBL when javascript is being toggled on and off.  Bug 398668, r+sr+a=jst

--- content/xbl/src/nsXBLProtoImplMethod.cpp
+++ content/xbl/src/nsXBLProtoImplMethod.cpp
@@ -287,7 +287,13 @@ nsXBLProtoImplMethod::Traverse(nsCycleCollectionTraversalCallback &cb) const
 nsresult
 nsXBLProtoImplAnonymousMethod::Execute(nsIContent* aBoundElement)
 {
-  NS_PRECONDITION(mIsCompiled, "Can't execute uncompiled method");
+  if (!mIsCompiled) {
+    // Someone might have enabled script between when the binding would have
+    // been compiled and now.  If this is the constructor we shouldn't run it
+    // because our other scripted stuff is not set up, and if it's the
+    // destructor then the constructor never ran.  In either case, bail out.
+    return NS_OK;
+  }
   
   if (!mJSMethodObject) {
     // Nothing to do here
