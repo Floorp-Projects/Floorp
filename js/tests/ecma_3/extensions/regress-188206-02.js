77b3313925b98ee53c83360de6268eae4ec00df7
bclary@bclary.com <bclary@bclary.com>
2007-10-10 18:36:33 -0700

    JavaScript Tests - move tests of non-standard into extensions, bug 370585

--- js/tests/ecma_3/extensions/regress-188206-02.js
+++ js/tests/ecma_3/extensions/regress-188206-02.js
@@ -36,24 +36,11 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-/*
- *
- * Date:    21 January 2003
- * SUMMARY: Invalid use of regexp quantifiers should generate SyntaxErrors
- *
- * See http://bugzilla.mozilla.org/show_bug.cgi?id=188206
- * and http://bugzilla.mozilla.org/show_bug.cgi?id=85721#c48 etc.
- * and http://bugzilla.mozilla.org/show_bug.cgi?id=190685
- * and http://bugzilla.mozilla.org/show_bug.cgi?id=197451
- */
 //-----------------------------------------------------------------------------
-var gTestfile = 'regress-188206.js';
+var gTestfile = 'regress-188206-02.js';
 var UBound = 0;
 var BUGNUMBER = 188206;
 var summary = 'Invalid use of regexp quantifiers should generate SyntaxErrors';
-var TEST_PASSED = 'SyntaxError';
-var TEST_FAILED = 'Generated an error, but NOT a SyntaxError!';
-var TEST_FAILED_BADLY = 'Did not generate ANY error!!!';
 var CHECK_PASSED = 'Should not generate an error';
 var CHECK_FAILED = 'Generated an error!';
 var status = '';
@@ -65,119 +52,71 @@ var expectedvalues = [];
 
 
 /*
- * All the following are invalid uses of regexp quantifiers and
- * should generate SyntaxErrors. That's what we're testing for.
+ * Misusing the {DecmalDigits} quantifier - according to ECMA,
+ * but not according to Perl.
  *
- * To allow the test to compile and run, we have to hide the errors
- * inside eval strings, and check they are caught at run-time -
+ * ECMA-262 Edition 3 prohibits the use of unescaped braces in
+ * regexp patterns, unless they form part of a quantifier.
  *
- */
-status = inSection(1);
-testThis(' /a**/ ');
-
-status = inSection(2);
-testThis(' /a***/ ');
-
-status = inSection(3);
-testThis(' /a++/ ');
-
-status = inSection(4);
-testThis(' /a+++/ ');
-
-/*
- * The ? quantifier, unlike * or +, may appear twice in succession.
- * Thus we need at least three in a row to provoke a SyntaxError -
- */
-
-status = inSection(5);
-testThis(' /a???/ ');
-
-status = inSection(6);
-testThis(' /a????/ ');
-
-
-/*
- * Now do some weird things on the left side of the regexps -
- */
-status = inSection(9);
-testThis(' /+a/ ');
-
-status = inSection(10);
-testThis(' /++a/ ');
-
-status = inSection(11);
-testThis(' /?a/ ');
-
-status = inSection(12);
-testThis(' /??a/ ');
-
-
-/*
- * Misusing the {DecmalDigits} quantifier - according to BOTH ECMA and Perl.
+ * Hovever, Perl does not prohibit this. If not used as part
+ * of a quantifer, Perl treats braces literally.
  *
- * Just as with the * and + quantifiers above, can't have two {DecmalDigits}
- * quantifiers in succession - it's a SyntaxError.
+ * We decided to follow Perl on this for backward compatibility.
+ * See http://bugzilla.mozilla.org/show_bug.cgi?id=190685.
+ *
+ * Therefore NONE of the following ECMA violations should generate
+ * a SyntaxError. Note we use checkThis() instead of testThis().
  */
-status = inSection(28);
-testThis(' /x{1}{1}/ ');
+status = inSection(13);
+checkThis(' /a*{/ ');
 
-status = inSection(29);
-testThis(' /x{1,}{1}/ ');
+status = inSection(14);
+checkThis(' /a{}/ ');
 
-status = inSection(30);
-testThis(' /x{1,2}{1}/ ');
+status = inSection(15);
+checkThis(' /{a/ ');
 
-status = inSection(31);
-testThis(' /x{1}{1,}/ ');
+status = inSection(16);
+checkThis(' /}a/ ');
 
-status = inSection(32);
-testThis(' /x{1,}{1,}/ ');
+status = inSection(17);
+checkThis(' /x{abc}/ ');
 
-status = inSection(33);
-testThis(' /x{1,2}{1,}/ ');
+status = inSection(18);
+checkThis(' /{{0}/ ');
 
-status = inSection(34);
-testThis(' /x{1}{1,2}/ ');
+status = inSection(19);
+checkThis(' /{{1}/ ');
 
-status = inSection(35);
-testThis(' /x{1,}{1,2}/ ');
+status = inSection(20);
+checkThis(' /x{{0}/ ');
 
-status = inSection(36);
-testThis(' /x{1,2}{1,2}/ ');
+status = inSection(21);
+checkThis(' /x{{1}/ ');
 
+status = inSection(22);
+checkThis(' /x{{0}}/ ');
+
+status = inSection(23);
+checkThis(' /x{{1}}/ ');
+
+status = inSection(24);
+checkThis(' /x{{0}}/ ');
+
+status = inSection(25);
+checkThis(' /x{{1}}/ ');
+
+status = inSection(26);
+checkThis(' /x{{0}}/ ');
+
+status = inSection(27);
+checkThis(' /x{{1}}/ ');
 
 
 //-----------------------------------------------------------------------------
 test();
 //-----------------------------------------------------------------------------
 
-
-
-/*
- * Invalid syntax should generate a SyntaxError
- */
-function testThis(sInvalidSyntax)
-{
-  expect = TEST_PASSED;
-  actual = TEST_FAILED_BADLY;
-
-  try
-  {
-    eval(sInvalidSyntax);
-  }
-  catch(e)
-  {
-    if (e instanceof SyntaxError)
-      actual = TEST_PASSED;
-    else
-      actual = TEST_FAILED;
-  }
-
-  statusitems[UBound] = status;
-  expectedvalues[UBound] = expect;
-  actualvalues[UBound] = actual;
-  UBound++;
-}
 
 
 /*
