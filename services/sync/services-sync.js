22b53dfe57e6d197a4d9cd91638a675d2572d7dc
Dan Mills <thunder@mozilla.com>
2007-10-15 19:59:54 -0700

    add resetServer, fix schedule pref

--- services/sync/services-sync.js
+++ services/sync/services-sync.js
@@ -4,5 +4,5 @@ pref("browser.places.sync.rememberpassword", true);
 pref("browser.places.sync.autoconnect", true);
 pref("browser.places.sync.enabled", true);
 pref("browser.places.sync.bookmarks", true);
-pref("browser.places.sync.schedule", 1)
+pref("browser.places.sync.schedule", 1);
 pref("browser.places.sync.lastsync", "0");
