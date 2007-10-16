252b29b6b266524f8fbe26f1b9786d04c82d562f
kaie@kuix.de <kaie@kuix.de>
2007-10-15 17:37:03 -0700

    Bug 399022, Leak-prone code in nsCertTree r=bzbarsky, a=sayrer

--- security/manager/ssl/src/nsCertTree.h
+++ security/manager/ssl/src/nsCertTree.h
@@ -154,9 +154,10 @@ private:
   nsCOMPtr<nsICertOverrideService> mOverrideService;
 
   treeArrayEl *GetThreadDescAtIndex(PRInt32 _index);
-  nsIX509Cert *GetCertAtIndex(PRInt32 _index, PRInt32 *outAbsoluteCertOffset = nsnull);
-  nsCertTreeDispInfo *GetDispInfoAtIndex(PRInt32 index,  
-                                         PRInt32 *outAbsoluteCertOffset = nsnull);
+  already_AddRefed<nsIX509Cert> 
+    GetCertAtIndex(PRInt32 _index, PRInt32 *outAbsoluteCertOffset = nsnull);
+  already_AddRefed<nsCertTreeDispInfo> 
+    GetDispInfoAtIndex(PRInt32 index, PRInt32 *outAbsoluteCertOffset = nsnull);
   void FreeCertArray();
   nsresult UpdateUIContents();
 
