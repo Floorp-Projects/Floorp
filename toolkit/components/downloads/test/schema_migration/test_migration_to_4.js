61c9f47139650728809e27c135a538ac079a65ba
edward.lee@engineering.uiuc.edu <edward.lee@engineering.uiuc.edu>
2007-11-04 20:43:18 -0800

    Bug 402493 - Incorrect license boilerplate for download manager schema migration test code. rs=sdwilsh

--- toolkit/components/downloads/test/schema_migration/test_migration_to_4.js
+++ toolkit/components/downloads/test/schema_migration/test_migration_to_4.js
@@ -29,8 +29,8 @@
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
  * use your version of this file under the terms of the MPL, indicate your
- * decision by declaring the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not declare
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
  * the provisions above, a recipient may use your version of this file under
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
