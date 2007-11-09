b570b85cc8257faa80b8d4c487dc6df4351b43c1
reed@reedloden.com <reed@reedloden.com>
2007-11-08 18:19:30 -0800

    Bug 392870 - "Easy discoverability of Tabbed Browsing for new users" [p=dao r=mconnor r=Neil a=blocking-firefox3+]

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -266,6 +266,10 @@ pref("browser.link.open_newwindow", 3);
 pref("browser.link.open_newwindow.restriction", 2);
 
 // Tab browser preferences.
+pref("browser.tabs.autoHide", false);
+pref("browser.tabs.warnOnClose", true);
+pref("browser.tabs.warnOnOpen", true);
+pref("browser.tabs.maxOpenBeforeWarn", 15);
 pref("browser.tabs.loadInBackground", true);
 pref("browser.tabs.loadFolderAndReplace", true);
 pref("browser.tabs.opentabfor.middleclick", true);
