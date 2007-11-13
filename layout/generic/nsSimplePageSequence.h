661226c56a655ed6310ac8dcb3113e7bcd0ee71e
reed@reedloden.com <reed@reedloden.com>
2007-11-13 11:14:07 -0800

    Bug 396278 - "should take edge values from print settings instead of prefs" [p=chpe r+sr=roc r=gavin a1.9=schrep]

--- layout/generic/nsSimplePageSequence.h
+++ layout/generic/nsSimplePageSequence.h
@@ -149,7 +149,6 @@ protected:
   void SetDateTimeStr(PRUnichar * aDateTimeStr);
   void SetPageNumberFormat(PRUnichar * aFormatStr, PRBool aForPageNumOnly);
 
-  void GetEdgePaperMarginCoord(const char* aPrefName, nscoord& aCoord);
   void GetEdgePaperMargin(nsMargin& aMargin);
 
   NS_IMETHOD_(nsrefcnt) AddRef(void) {return nsContainerFrame::AddRef();}
