e5861c00a22d8aa84c9b43b0ba9e59a10ad49360
jonas@sicking.cc <jonas@sicking.cc>
2007-11-28 16:27:05 -0800

    Bug 403835: Make talos force cycle collection between each pageload. r=alice sr=rcampbell

--- layout/tools/pageloader/pageloader.js
+++ layout/tools/pageloader/pageloader.js
@@ -60,6 +60,7 @@ var noisy = false;
 var timeout = -1;
 var timeoutEvent = -1;
 var running = false;
+var forceCC = true;
 
 var content;
 
@@ -93,7 +94,15 @@ function plInit() {
     if (args.filter) pageFilterRegexp = new RegExp(args.filter);
     if (args.noisy) noisy = true;
     if (args.timeout) timeout = parseInt(args.timeout);
+    forceCC = !args.noForceCC;
     doRenderTest = args.doRender;
+
+    if (forceCC &&
+        !window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+               .getInterface(Components.interfaces.nsIDOMWindowUtils)
+               .garbageCollect) {
+      forceCC = false;
+    }
 
     gIOS = Cc["@mozilla.org/network/io-service;1"]
       .getService(Ci.nsIIOService);
@@ -220,6 +229,15 @@ function loadFail() {
 function plNextPage() {
   if (pageIndex < pages.length-1) {
     pageIndex++;
+
+    if (forceCC) {
+      var tccstart = new Date();
+      window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+            .getInterface(Components.interfaces.nsIDOMWindowUtils)
+            .garbageCollect(); 
+      var tccend = new Date();
+      report.recordCCTime(tccend - tccstart);
+    }
 
     setTimeout(plLoadPage, 250);
   } else {
