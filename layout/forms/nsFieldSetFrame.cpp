f6a0bf6aa0fe94fae65e943dc499ba9bbf6201b2
roc+@cs.cmu.edu <roc+@cs.cmu.edu>
2007-12-01 00:32:51 -0800

    Bug 405305. Fix z-ordering of legend background so it's in the block-backgrounds layer. r+sr=mats

--- layout/forms/nsFieldSetFrame.cpp
+++ layout/forms/nsFieldSetFrame.cpp
@@ -240,7 +240,9 @@ nsFieldSetFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
     NS_ENSURE_SUCCESS(rv, rv);
   }
   if (mLegendFrame) {
-    nsDisplayListSet set(aLists, aLists.Content());
+    // The legend's background goes on our BlockBorderBackgrounds list because
+    // it's a block child.
+    nsDisplayListSet set(aLists, aLists.BlockBorderBackgrounds());
     nsresult rv = BuildDisplayListForChild(aBuilder, mLegendFrame, aDirtyRect, set);
     NS_ENSURE_SUCCESS(rv, rv);
   }
