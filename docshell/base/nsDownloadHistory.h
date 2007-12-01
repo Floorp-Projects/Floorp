e1e7a813fa34a0a1fc2a9d3f5a1ed341da2577fb
reed@reedloden.com <reed@reedloden.com>
2007-11-30 23:18:43 -0800

    Bug 405241 - "Don't register nsDownloadHistory if it's not needed" [p=comrade693+bmo@gmail.com (Shawn Wilsher [sdwilsh]) r=biesi sr=bzbarsky a1.9=damons]

--- docshell/base/nsDownloadHistory.h
+++ docshell/base/nsDownloadHistory.h
@@ -41,6 +41,7 @@
 #define __nsDownloadHistory_h__
 
 #include "nsIDownloadHistory.h"
+#include "nsIGenericFactory.h"
 
 #define NS_DOWNLOADHISTORY_CID \
   {0x2ee83680, 0x2af0, 0x4bcb, {0xbf, 0xa0, 0xc9, 0x70, 0x5f, 0x65, 0x54, 0xf1}}
@@ -50,6 +51,14 @@ class nsDownloadHistory : public nsIDownloadHistory
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIDOWNLOADHISTORY
+
+  static NS_METHOD RegisterSelf(nsIComponentManager *aCompMgr,
+                                nsIFile *aPath,
+                                const char *aLoaderStr,
+                                const char *aType,
+                                const nsModuleComponentInfo *aInfo);
+
+  NS_DEFINE_STATIC_CID_ACCESSOR(NS_DOWNLOADHISTORY_CID)
 };
 
 #endif // __nsDownloadHistory_h__
