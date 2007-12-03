430205474086fa6a1098894ea4c7f0f42e4a9640
bzbarsky@mit.edu <bzbarsky@mit.edu>
2007-12-03 14:09:00 -0800

    Fix bug 402937 -- don't allow recursion into Instantiate().  r=biesi, sr=jst, a=dsicore.

--- layout/generic/nsObjectFrame.h
+++ layout/generic/nsObjectFrame.h
@@ -193,11 +193,10 @@ private:
   nsPluginInstanceOwner *mInstanceOwner;
   nsRect                mWindowlessRect;
 
-#ifdef DEBUG
   // For assertions that make it easier to determine if a crash is due
-  // to the underlying problem described in bug 136927.
+  // to the underlying problem described in bug 136927, and to prevent
+  // reentry into instantiation.
   PRBool mInstantiating;
-#endif
 };
 
 
