8c9defe99458a9062410c4917ffb144717cbac1e
Dan Mills <thunder@mozilla.com>
2007-12-11 11:57:13 -0800

    assume in loaded modules that the resource:// alias has already been added - remove all the templatey code to do that

--- services/sync/modules/weave.js
+++ services/sync/modules/weave.js
@@ -41,25 +41,8 @@ const Ci = Components.interfaces;
 const Cr = Components.results;
 const Cu = Components.utils;
 
-addModuleAlias("weave", "{340c2bbc-ce74-4362-90b5-7c26312808ef}");
 Cu.import("resource://weave/service.js");
 
 let Weave = {};
 //Weave.Crypto = new WeaveCrypto();
 Weave.Service = new WeaveSyncService();
-
-function addModuleAlias(alias, extensionId) {
-  let ioSvc = Cc["@mozilla.org/network/io-service;1"]
-    .getService(Ci.nsIIOService);
-  let resProt = ioSvc.getProtocolHandler("resource")
-    .QueryInterface(Ci.nsIResProtocolHandler);
-
-  if (!resProt.hasSubstitution(alias)) {
-    let extMgr = Cc["@mozilla.org/extensions/manager;1"]
-      .getService(Ci.nsIExtensionManager);
-    let loc = extMgr.getInstallLocation(extensionId);
-    let extD = loc.getItemLocation(extensionId);
-    extD.append("modules");
-    resProt.setSubstitution(alias, ioSvc.newFileURI(extD));
-  }
-}
