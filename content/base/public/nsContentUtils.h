27a55e7707a2bba81a7890a573c8f5e00d042c66
reed@reedloden.com <reed@reedloden.com>
2007-12-14 00:55:08 -0800

    Back out patch from bug 406047, as it is causing orange on Linux and Tp regression on Mac.

--- content/base/public/nsContentUtils.h
+++ content/base/public/nsContentUtils.h
@@ -55,9 +55,6 @@
 #include "nsDataHashtable.h"
 #include "nsIScriptRuntime.h"
 #include "nsIScriptGlobalObject.h"
-#include "nsIDOMEvent.h"
-
-struct nsNativeKeyEvent; // Don't include nsINativeKeyBindings.h here: it will force strange compilation error!
 
 class nsIDOMScriptObjectFactory;
 class nsIXPConnect;
@@ -1128,13 +1125,6 @@ public:
    * Return the localized ellipsis for UI.
    */
   static const nsDependentString GetLocalizedEllipsis();
-  
-   /*The routine GetNativeEvent is used to fill nsNativeKeyEvent nsNativeKeyEvent and
-  it's used in DOMEventToNativeKeyEvent. See Bug 406407 page for details. */
-  static nsEvent* GetNativeEvent(nsIDOMEvent* aDOMEvent);
-  static PRBool DOMEventToNativeKeyEvent(nsIDOMEvent* aDOMEvent,
-                                         nsNativeKeyEvent* aNativeEvent,
-                                         PRBool aGetCharCode);
 
 private:
 
