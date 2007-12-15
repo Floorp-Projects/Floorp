5395a457d4e3eb87ab191c410b8ececed12ae1cc
florian@queze.net <florian@queze.net>
2007-12-14 16:20:48 -0800

    Bug 407193: Restore animateFadeIn behavior for Mac prefpanes (revert fix for bug 394881). r=mconnor, a=blocking-firefox3+

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -454,7 +454,11 @@ pref("browser.preferences.instantApply", false);
 #else
 pref("browser.preferences.instantApply", true);
 #endif
+#ifdef XP_MACOSX
+pref("browser.preferences.animateFadeIn", true);
+#else
 pref("browser.preferences.animateFadeIn", false);
+#endif
 
 pref("browser.download.show_plugins_in_list", true);
 pref("browser.download.hide_plugins_without_extensions", true);
