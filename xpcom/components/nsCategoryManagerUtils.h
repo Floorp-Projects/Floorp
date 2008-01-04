c76cefc88d967071b308d40f5dc11518a1f0871b
bugzilla@standard8.plus.com <bugzilla@standard8.plus.com>
2008-01-04 10:08:34 -0800

    Bug 410326 do_CreateInstanceFromCategory is no longer used. r/sr=benjamin,a1.9=dsicore

--- xpcom/components/nsCategoryManagerUtils.h
+++ xpcom/components/nsCategoryManagerUtils.h
@@ -39,51 +39,10 @@
 #define nsCategoryManagerUtils_h__
 
 #include "nsICategoryManager.h"
-#include "nsCOMPtr.h"
 
 NS_COM nsresult
 NS_CreateServicesFromCategory(const char *category,
                               nsISupports *origin,
                               const char *observerTopic);
-
-class NS_COM nsCreateInstanceFromCategory : public nsCOMPtr_helper
-{
-public:
-    nsCreateInstanceFromCategory(const char *aCategory, const char *aEntry,
-                                 nsISupports *aOuter, nsresult *aErrorPtr)
-        : mCategory(aCategory),
-          mEntry(aEntry),
-          mOuter(aOuter),
-          mErrorPtr(aErrorPtr)
-    {
-        // nothing else to do;
-    }
-    virtual nsresult NS_FASTCALL operator()( const nsIID& aIID, void** aInstancePtr) const;
-
-private:
-    const char *mCategory;  // Do not free.  This char * is not owned.
-    const char *mEntry;     // Do not free.  This char * is not owned.
-
-    nsISupports *mOuter;
-    nsresult *mErrorPtr;
-
-};
-
-inline
-const nsCreateInstanceFromCategory
-do_CreateInstanceFromCategory( const char *aCategory, const char *aEntry,
-                               nsresult *aErrorPtr = 0)
-{
-    return nsCreateInstanceFromCategory(aCategory, aEntry, 0, aErrorPtr);
-}
-
-inline
-const nsCreateInstanceFromCategory
-do_CreateInstanceFromCategory( const char *aCategory, const char *aEntry,
-                               nsISupports *aOuter, nsresult *aErrorPtr = 0)
-{
-    return nsCreateInstanceFromCategory(aCategory, aEntry, aOuter, aErrorPtr);
-}
-
 
 #endif
