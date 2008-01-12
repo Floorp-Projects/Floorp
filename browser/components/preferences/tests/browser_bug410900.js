ea011cdfd64209b788805bc249a40d098beb03c3
reed@reedloden.com <reed@reedloden.com>
2008-01-12 04:28:03 -0800

    Enable it because it passes now? ugh

--- browser/components/preferences/tests/browser_bug410900.js
+++ browser/components/preferences/tests/browser_bug410900.js
@@ -35,7 +35,7 @@ function runTest() {
     ok(rbox, "handlersView is present");
 
     var items = rbox && rbox.getElementsByTagName("richlistitem");
-    //ok(items && items.length > 0, "App handler list populated");
+    ok(items && items.length > 0, "App handler list populated");
 
     win.close();
   }
