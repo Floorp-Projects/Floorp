55194785a85f5bf065d976e15cad1196027bbf76
bent.mozilla@gmail.com <bent.mozilla@gmail.com>
2008-01-15 10:23:46 -0800

    Bug 352791 - "Permission denied to get property XULElement.ownerDocument". Patch by Blake Kaplan <mrbkap@gmail.com>. r=jst,bzbarsky. sr=brendan. a=blocking1.9.

--- js/src/xpconnect/src/xpcwrappednativescope.cpp
+++ js/src/xpconnect/src/xpcwrappednativescope.cpp
@@ -152,6 +152,12 @@ XPCWrappedNativeScope::XPCWrappedNativeScope(XPCCallContext& ccx,
 
         mNext = gScopes;
         gScopes = this;
+
+        // Grab the XPCContext associated with our context.
+        mContext = mRuntime->GetContextMap()->Find(ccx.GetJSContext());
+        NS_ASSERTION(mContext, "Context map is not synchronized");
+
+        mContext->AddScope(this);
     }
 
     if(aGlobal)
@@ -273,6 +279,9 @@ XPCWrappedNativeScope::~XPCWrappedNativeScope()
         NS_ASSERTION(0 == mWrapperMap->Count(), "scope has non-empty map");
         delete mWrapperMap;
     }
+
+    if(mContext)
+        mContext->RemoveScope(this);
 
     // XXX we should assert that we are dead or that xpconnect has shutdown
     // XXX might not want to do this at xpconnect shutdown time???
