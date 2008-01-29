07cdba90e60bda13567c7e53289629008f1b97b4
sdwilsh@shawnwilsher.com <sdwilsh@shawnwilsher.com>
2008-01-28 20:58:31 -0800

    Bug 414161 - Quit & cancelation confirmation should not be displayed if downloads are resumable.  r=Mardak, a=damons

--- toolkit/components/downloads/src/nsDownloadManager.cpp
+++ toolkit/components/downloads/src/nsDownloadManager.cpp
@@ -1640,8 +1640,14 @@ nsDownloadManager::Observe(nsISupports *aSubject,
                            const char *aTopic,
                            const PRUnichar *aData)
 {
-  // Count active downloads that aren't real-paused
   PRInt32 currDownloadCount = mCurrentDownloads.Count();
+
+  // If we don't need to cancel all the downloads on quit, only count the ones
+  // that aren't resumable.
+  if (GetQuitBehavior() != QUIT_AND_CANCEL)
+    for (PRInt32 i = currDownloadCount - 1; i >= 0; --i)
+      if (mCurrentDownloads[i]->IsResumable())
+        currDownloadCount--;
 
   nsresult rv;
   if (strcmp(aTopic, "oncancel") == 0) {
