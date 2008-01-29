444ed4227e27891661c719c40cbd79cc95046eb3
gavin@gavinsharp.com <gavin@gavinsharp.com>
2008-01-29 11:35:31 -0800

    Bug 412990: Sherlock engine conversion code fails to set icons, r=mano, a=schrep

--- browser/components/search/nsSearchService.js
+++ browser/components/search/nsSearchService.js
@@ -2539,7 +2539,7 @@ SearchService.prototype = {
         bStream.close();
 
         // Convert the byte array to a base64-encoded string
-        var str = b64(bytes);
+        var str = btoa(String.fromCharCode.apply(null, bytes));
 
         aEngine._iconURI = makeURI(ICON_DATAURL_PREFIX + str);
         LOG("_importSherlockEngine: Set sherlock iconURI to: \"" +
