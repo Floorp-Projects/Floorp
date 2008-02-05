58454da1d0bc5b72189228e59e87cdc8288eef7d
Dan Mills <thunder@mozilla.com>
2008-02-05 14:15:53 -0800

    add openssl support

--- services/sync/modules/crypto.js
+++ services/sync/modules/crypto.js
@@ -139,12 +139,16 @@ WeaveCrypto.prototype = {
       if (!algorithm)
         algorithm = this.defaultAlgorithm;
 
+      if (algorithm != "none")
+        this._log.debug("Encrypting data");
+
       switch (algorithm) {
       case "none":
         ret = data;
+        break;
+
       case "XXXTEA": // Weave 0.1.12.10 and below had this typo
       case "XXTEA": {
-        this._log.debug("Encrypting data");
         let gen = this._xxtea.encrypt(data, identity.password);
         ret = gen.next();
         while (typeof(ret) == "object") {
@@ -153,11 +157,22 @@ WeaveCrypto.prototype = {
           ret = gen.next();
         }
         gen.close();
-        this._log.debug("Done encrypting data");
       } break;
+
+      case "aes-128-cbc":
+      case "aes-192-cbc":
+      case "aes-256-cbc":
+      case "bf-cbc":
+      case "des-ede3-cbc":
+        ret = openssl("-e", algorithm, data, identity.password);
+        break;
+
       default:
         throw "Unknown encryption algorithm: " + algorithm;
       }
+
+      if (algorithm != "none")
+        this._log.debug("Done encrypting data");
 
     } catch (e) {
       this._log.error("Exception caught: " + (e.message? e.message : e));
@@ -180,13 +195,16 @@ WeaveCrypto.prototype = {
       if (!algorithm)
         algorithm = this.defaultAlgorithm;
 
+      if (algorithm != "none")
+        this._log.debug("Decrypting data");
+
       switch (algorithm) {
       case "none":
         ret = data;
         break;
+
       case "XXXTEA": // Weave 0.1.12.10 and below had this typo
       case "XXTEA": {
-        this._log.debug("Decrypting data");
         let gen = this._xxtea.decrypt(data, identity.password);
         ret = gen.next();
         while (typeof(ret) == "object") {
@@ -195,11 +213,22 @@ WeaveCrypto.prototype = {
           ret = gen.next();
         }
         gen.close();
-        this._log.debug("Done decrypting data");
       } break;
+
+      case "aes-128-cbc":
+      case "aes-192-cbc":
+      case "aes-256-cbc":
+      case "bf-cbc":
+      case "des-ede3-cbc":
+        ret = openssl("-d", algorithm, data, identity.password);
+        break;
+
       default:
         throw "Unknown encryption algorithm: " + algorithm;
       }
+
+      if (algorithm != "none")
+        this._log.debug("Done decrypting data");
 
     } catch (e) {
       this._log.error("Exception caught: " + (e.message? e.message : e));
@@ -212,3 +241,62 @@ WeaveCrypto.prototype = {
     this._log.warn("generator not properly closed");
   }
 };
+
+function openssl(op, algorithm, input, password) {
+  let extMgr = Components.classes["@mozilla.org/extensions/manager;1"]
+    .getService(Components.interfaces.nsIExtensionManager);
+  let loc = extMgr.getInstallLocation("{340c2bbc-ce74-4362-90b5-7c26312808ef}");
+
+  let wrap = loc.getItemLocation("{340c2bbc-ce74-4362-90b5-7c26312808ef}");
+  wrap.append("openssl");
+  let bin;
+
+  let os = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime).OS;
+  switch(os) {
+  case "WINNT":
+    wrap.append("win32");
+    wrap.append("exec.bat");
+    bin = wrap.parent.path + "\openssl.exe";
+    dump("FIXME TESTING path: " + bin + "\n");
+    break;
+  case "Linux":
+  case "Darwin":
+    wrap.append("unix");
+    wrap.append("exec.sh");
+    bin = "openssl";
+    break;
+  default:
+    throw "encryption not supported on this platform: " + os;
+  }
+
+  let inputFile = getTmp("input");
+  let [inputFOS] = open(inputFile, ">");
+  inputFOS.write(input, input.length);
+  inputFOS.close();
+
+  let outputFile = getTmp("output");
+  if (outputFile.exists())
+    outputFile.remove(false);
+
+  let passFile = getTmp("pass");
+  let [passFOS] = open(passFile, ">", PERMS_PASSFILE);
+  passFOS.write(password, password.length);
+  passFOS.close();
+
+  try {
+    runCmd(wrap, getTmp().path, bin, algorithm, op, "-a", "-salt",
+           "-in", "input", "-out", "output", "-pass", "file:pass");
+  } catch (e) {
+    throw e;
+  } finally {
+    passFile.remove(false);
+    inputFile.remove(false);
+  }
+
+  let [outputFIS] = open(outputFile, "<");
+  let ret = readStream(outputFIS);
+  outputFIS.close();
+  outputFile.remove(false);
+
+  return ret;
+}
