56b7cd34277af735ff5d233c122ee08555e9543c
reed@reedloden.com <reed@reedloden.com>
2008-02-07 01:08:17 -0800

    Bug 415078 - "nsWebShell.cpp attempts to fixup immutable URIs" [p=graememcc_firefox@graeme-online.co.uk (graememcc) r+sr=bzbarsky a1.9=schrep]

--- docshell/base/nsWebShell.cpp
+++ docshell/base/nsWebShell.cpp
@@ -1161,10 +1161,6 @@ nsresult nsWebShell::EndPageLoad(nsIWebProgress *aProgress,
           newURI->GetSpec(newSpec);
           NS_ConvertUTF8toUTF16 newSpecW(newSpec);
 
-          // This seems evil, since it is modifying the original URL
-          rv = url->SetSpec(newSpec);
-          if (NS_FAILED(rv)) return rv;
-
           return LoadURI(newSpecW.get(),      // URI string
                          LOAD_FLAGS_NONE, // Load flags
                          nsnull,          // Referring URI
