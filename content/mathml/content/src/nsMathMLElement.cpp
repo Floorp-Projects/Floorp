e2f32405c12bb8bf202247e698b422d61243730e
roc+@cs.cmu.edu <roc+@cs.cmu.edu>
2008-02-06 20:09:03 -0800

    Bug 412237. Don't parse negative sizes for the MathML fontsize attribute. Make sure we do continue to parse '-0px', since that's not actually negative. r+sr=dbaron

--- content/mathml/content/src/nsMathMLElement.cpp
+++ content/mathml/content/src/nsMathMLElement.cpp
@@ -222,7 +222,7 @@ Implementation here:
 /* static */ PRBool
 nsMathMLElement::ParseNumericValue(const nsString& aString,
                                    nsCSSValue&     aCSSValue,
-                                   PRBool          aRequireLengthUnit)
+                                   PRUint32        aFlags)
 {
   nsAutoString str(aString);
   str.CompressWhitespace(); // aString is const in this code...
@@ -267,19 +267,21 @@ nsMathMLElement::ParseNumericValue(const nsString& aString,
   float floatValue = number.ToFloat(&errorCode);
   if (NS_FAILED(errorCode))
     return PR_FALSE;
+  if (floatValue < 0 && !(aFlags & PARSE_ALLOW_NEGATIVE))
+    return PR_FALSE;
 
   nsCSSUnit cssUnit;
   if (unit.IsEmpty()) {
-    if (aRequireLengthUnit) {
-      // We are supposed to have a length unit, but there isn't one.
+    if (aFlags & PARSE_ALLOW_UNITLESS) {
+      // no explicit unit, this is a number that will act as a multiplier
+      cssUnit = eCSSUnit_Number;
+    } else {
+      // We are supposed to have a unit, but there isn't one.
       // If the value is 0 we can just call it "pixels" otherwise
       // this is illegal.
       if (floatValue != 0.0)
         return PR_FALSE;
       cssUnit = eCSSUnit_Pixel;
-    } else {
-      // no explicit unit, this is a number that will act as a multiplier
-      cssUnit = eCSSUnit_Number;
     }
   }
   else if (unit.EqualsLiteral("%")) {
@@ -328,7 +330,7 @@ nsMathMLElement::MapMathMLAttributesInto(const nsMappedAttributes* aAttributes,
     if (value && value->Type() == nsAttrValue::eString &&
         aData->mFontData->mScriptMinSize.GetUnit() == eCSSUnit_Null) {
       ParseNumericValue(value->GetStringValue(),
-                        aData->mFontData->mScriptMinSize, PR_TRUE);
+                        aData->mFontData->mScriptMinSize, 0);
     }
 
     value = aAttributes->GetAttr(nsGkAtoms::scriptlevel_);
@@ -363,7 +365,7 @@ nsMathMLElement::MapMathMLAttributesInto(const nsMappedAttributes* aAttributes,
     if (value && value->Type() == nsAttrValue::eString &&
         aData->mFontData->mSize.GetUnit() == eCSSUnit_Null) {
       nsAutoString str(value->GetStringValue());
-      if (!ParseNumericValue(str, aData->mFontData->mSize, PR_TRUE) &&
+      if (!ParseNumericValue(str, aData->mFontData->mSize, 0) &&
           parseSizeKeywords) {
         static const char sizes[3][7] = { "small", "normal", "big" };
         static const PRInt32 values[NS_ARRAY_LENGTH(sizes)] = {
