4f785df705764b4443de1242ac165264bdedd7df
reed@reedloden.com <reed@reedloden.com>
2008-02-13 03:45:22 -0800

    Add missing parentheses to complete cast and fix bustage.

--- content/base/src/nsDOMAttributeMap.h
+++ content/base/src/nsDOMAttributeMap.h
@@ -105,7 +105,7 @@ public:
       if (!aKey)
         return 0;
 
-      return PR_ROTATE_LEFT32(static_cast<PRUint32>aKey->mNamespaceID, 4) ^
+      return PR_ROTATE_LEFT32(static_cast<PRUint32>(aKey->mNamespaceID), 4) ^
              NS_PTR_TO_INT32(aKey->mLocalName);
     }
   enum { ALLOW_MEMMOVE = PR_TRUE };
