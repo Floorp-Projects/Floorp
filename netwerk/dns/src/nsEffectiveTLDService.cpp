2f3c57196e4dba973649a3cf1630a57f8a0d57b1
jwalden@mit.edu <jwalden@mit.edu>
2008-02-14 14:57:20 -0800

    Bug 414122 - Preprocess effective TLD data into C++ code (eliminate file I/O to read, move parsing out of C++, remove an arena, etc.).  r=dwitte, sr=bsmedberg, a=blocker

--- netwerk/dns/src/nsEffectiveTLDService.cpp
+++ netwerk/dns/src/nsEffectiveTLDService.cpp
@@ -22,6 +22,7 @@
  * Contributor(s):
  *   Pamela Greene <pamg.bugs@gmail.com> (original author)
  *   Daniel Witte <dwitte@stanford.edu>
+ *   Jeff Walden <jwalden+code@mit.edu>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -42,79 +43,48 @@
 // http://wiki.mozilla.org/Gecko:Effective_TLD_Service
 
 #include "nsEffectiveTLDService.h"
-#include "nsAppDirectoryServiceDefs.h"
-#include "nsDirectoryServiceUtils.h"
-#include "nsDirectoryServiceDefs.h"
-#include "nsFileStreams.h"
-#include "nsIFile.h"
 #include "nsIIDNService.h"
 #include "nsNetUtil.h"
 #include "prnetdb.h"
-
-// The file name of the list of TLD-like names.  A file with this name in the
-// system "res" directory will always be used.  In addition, if a file with
-// the same name is present in the user's profile directory, its contents will
-// also be used, as though those rules were appended to the system file.
-#define EFF_TLD_FILENAME NS_LITERAL_CSTRING("effective_tld_names.dat")
 
 NS_IMPL_ISUPPORTS1(nsEffectiveTLDService, nsIEffectiveTLDService)
 
 // ----------------------------------------------------------------------
 
-#define PL_ARENA_CONST_ALIGN_MASK 3
-#include "plarena.h"
-
-static PLArenaPool *gArena = nsnull;
-
-#define ARENA_SIZE 512
-
-// equivalent to strdup() - does no error checking,
-// we're assuming we're only called with a valid pointer
-static char *
-ArenaStrDup(const char* str, PLArenaPool* aArena)
-{
-  void *mem;
-  PRUint32 size = strlen(str) + 1;
-  PL_ARENA_ALLOCATE(mem, aArena, size);
-  if (mem)
-    memcpy(mem, str, size);
-  return static_cast<char*>(mem);
-}
-
-nsDomainEntry::nsDomainEntry(const char *aDomain)
- : mDomain(ArenaStrDup(aDomain, gArena))
- , mIsNormal(PR_FALSE)
- , mIsException(PR_FALSE)
- , mIsWild(PR_FALSE)
-{
-}
+static const ETLDEntry gEntries[] =
+#include "etld_data.inc"
+;
 
 // ----------------------------------------------------------------------
 
 nsresult
 nsEffectiveTLDService::Init()
 {
-  if (!mHash.Init())
+  // We'll probably have to rehash at least once, since nsTHashtable doesn't
+  // use a perfect hash, but at least we'll save a few rehashes along the way.
+  // Next optimization here is to precompute the hash using something like
+  // gperf, but one step at a time.  :-)
+  if (!mHash.Init(NS_ARRAY_LENGTH(gEntries) - 1))
     return NS_ERROR_OUT_OF_MEMORY;
 
   nsresult rv;
   mIDNService = do_GetService(NS_IDNSERVICE_CONTRACTID, &rv);
   if (NS_FAILED(rv)) return rv;
 
-  return LoadEffectiveTLDFiles();
-}
-
-nsEffectiveTLDService::nsEffectiveTLDService()
-{
-}
-
-nsEffectiveTLDService::~nsEffectiveTLDService()
-{
-  if (gArena) {
-    PL_FinishArenaPool(gArena);
-    delete gArena;
+  // Initialize eTLD hash from static array
+  for (PRUint32 i = 0; i < NS_ARRAY_LENGTH(gEntries) - 1; i++) {
+#ifdef DEBUG
+    nsDependentCString name(gEntries[i].domain);
+    nsCAutoString normalizedName(gEntries[i].domain);
+    NS_ASSERTION(NS_SUCCEEDED(NormalizeHostname(normalizedName)),
+                 "normalization failure!");
+    NS_ASSERTION(name.Equals(normalizedName), "domain not normalized!");
+#endif
+    nsDomainEntry *entry = mHash.PutEntry(gEntries[i].domain);
+    NS_ENSURE_TRUE(entry, NS_ERROR_OUT_OF_MEMORY);
+    entry->SetData(&gEntries[i]);
   }
-  gArena = nsnull;
+  return NS_OK;
 }
 
 // External function for dealing with URI's correctly.
@@ -212,8 +182,8 @@ nsEffectiveTLDService::GetBaseDomainInternal(nsCString  &aHostname,
   if (result == PR_SUCCESS)
     return NS_ERROR_HOST_IS_IP_ADDRESS;
 
-  // walk up the domain tree, most specific to least specific,
-  // looking for matches at each level. note that a given level may
+  // Walk up the domain tree, most specific to least specific,
+  // looking for matches at each level.  Note that a given level may
   // have multiple attributes (e.g. IsWild() and IsNormal()).
   const char *prevDomain = nsnull;
   const char *currDomain = aHostname.get();
@@ -276,186 +246,18 @@ nsEffectiveTLDService::GetBaseDomainInternal(nsCString  &aHostname,
   return NS_OK;
 }
 
-// Normalizes characters of hostname. ASCII/ACE names are lower-cased,
-// and UTF8 names are normalized per RFC 3454 and converted to ACE.
+// Normalizes the given hostname, component by component.  ASCII/ACE
+// components are lower-cased, and UTF-8 components are normalized per
+// RFC 3454 and converted to ACE.
 nsresult
 nsEffectiveTLDService::NormalizeHostname(nsCString &aHostname)
 {
-  if (IsASCII(aHostname)) {
-    ToLowerCase(aHostname);
-    return NS_OK;
-  }
-
-  return mIDNService->ConvertUTF8toACE(aHostname, aHostname);
-}
-
-// Adds the given domain name rule to the effective-TLD hash.
-// CAUTION: As a side effect, the domain name rule will be normalized.
-// see NormalizeHostname().
-nsresult
-nsEffectiveTLDService::AddEffectiveTLDEntry(nsCString &aDomainName)
-{
-  // lazily init the arena pool
-  if (!gArena) {
-    gArena = new PLArenaPool;
-    NS_ENSURE_TRUE(gArena, NS_ERROR_OUT_OF_MEMORY);
-    PL_INIT_ARENA_POOL(gArena, "eTLDArena", ARENA_SIZE);
-  }
-
-  PRBool isException = PR_FALSE, isWild = PR_FALSE;
-
-  // Is this node an exception?
-  if (aDomainName.First() == '!') {
-    isException = PR_TRUE;
-    aDomainName.Cut(0, 1);
-
-  // ... or wild?
-  } else if (StringBeginsWith(aDomainName, NS_LITERAL_CSTRING("*."))) {
-    isWild = PR_TRUE;
-    aDomainName.Cut(0, 2);
-
-    NS_ASSERTION(!StringBeginsWith(aDomainName, NS_LITERAL_CSTRING("*.")),
-                 "only one wildcard level supported!");
-  }
-
-  // Normalize the domain name.
-  nsresult rv = NormalizeHostname(aDomainName);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  nsDomainEntry *entry = mHash.PutEntry(aDomainName.get());
-  NS_ENSURE_TRUE(entry, NS_ERROR_FAILURE);
-
-  // check for arena string alloc failure
-  if (!entry->GetKey()) {
-    mHash.RawRemoveEntry(entry);
-    return NS_ERROR_OUT_OF_MEMORY;
-  }
-
-  // add the new flags, without stomping existing ones
-  entry->IsWild() |= isWild;
-  entry->IsException() |= isException;
-  // note: isWild also implies isNormal (e.g. *.co.nz also implies the co.nz eTLD)
-  entry->IsNormal() |= isWild || !isException;
-
-  return NS_OK;
-}
-
-// Locates the effective-TLD file.  If aUseProfile is true, uses the file from
-// the user's profile directory; otherwise uses the one from the system "res"
-// directory.  Places nsnull in foundFile if the desired file was not found.
-nsresult
-LocateEffectiveTLDFile(nsCOMPtr<nsIFile>& foundFile, PRBool aUseProfile)
-{
-  foundFile = nsnull;
-
-  nsCOMPtr<nsIFile> effTLDFile = nsnull;
-  nsresult rv = NS_OK;
-  PRBool exists = PR_FALSE;
-  if (aUseProfile) {
-    // Look for the file in the user's profile directory.
-    rv = NS_GetSpecialDirectory(NS_APP_USER_PROFILE_50_DIR,
-                                getter_AddRefs(effTLDFile));
-    // We allow a nonfatal error so that this component can be tested in an
-    // xpcshell with no profile present.
+  if (!IsASCII(aHostname)) {
+    nsresult rv = mIDNService->ConvertUTF8toACE(aHostname, aHostname);
     if (NS_FAILED(rv))
       return rv;
   }
-  else {
-    // Look for the file in the application "res" directory.
-    rv = NS_GetSpecialDirectory(NS_OS_CURRENT_PROCESS_DIR,
-                                getter_AddRefs(effTLDFile));
-    NS_ENSURE_SUCCESS(rv, rv);
-    
-    rv = effTLDFile->AppendNative(NS_LITERAL_CSTRING("res"));
-    NS_ENSURE_SUCCESS(rv, rv);
-  }
 
-  rv = effTLDFile->AppendNative(EFF_TLD_FILENAME);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  rv = effTLDFile->Exists(&exists);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  if (exists)
-    foundFile = effTLDFile;
-
-  return rv;
-}
-
-void
-TruncateAtWhitespace(nsCString &aString)
-{
-  // Searching for a space or tab one byte at a time is fine since UTF-8 is a
-  // superset of 7-bit ASCII.
-  nsASingleFragmentCString::const_char_iterator begin, iter, end;
-  aString.BeginReading(begin);
-  aString.EndReading(end);
-
-  for (iter = begin; iter != end; ++iter) {
-    if (*iter == ' ' || *iter == '\t') {
-      aString.Truncate(iter - begin);
-      break;
-    }
-  }
-}
-
-// Loads the contents of the given effective-TLD file, building the tree as it
-// goes.
-nsresult
-nsEffectiveTLDService::LoadOneEffectiveTLDFile(nsCOMPtr<nsIFile>& effTLDFile)
-{
-  // Open the file as an input stream.
-  nsCOMPtr<nsIInputStream> fileStream;
-  nsresult rv = NS_NewLocalFileInputStream(getter_AddRefs(fileStream),
-                                           effTLDFile,
-                                           0x01, // read-only mode
-                                           -1,   // all permissions
-                                           nsIFileInputStream::CLOSE_ON_EOF);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  nsCOMPtr<nsILineInputStream> lineStream = do_QueryInterface(fileStream, &rv);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  nsCAutoString lineData;
-  PRBool isMore;
-  NS_NAMED_LITERAL_CSTRING(kCommentMarker, "//");
-  while (NS_SUCCEEDED(lineStream->ReadLine(lineData, &isMore)) && isMore) {
-    if (StringBeginsWith(lineData, kCommentMarker))
-      continue;
-
-    TruncateAtWhitespace(lineData);
-    if (!lineData.IsEmpty()) {
-      rv = AddEffectiveTLDEntry(lineData);
-      NS_WARN_IF_FALSE(NS_SUCCEEDED(rv), "Error adding effective TLD to list");
-    }
-  }
-
+  ToLowerCase(aHostname);
   return NS_OK;
-}
-
-// Loads the contents of the system and user effective-TLD files.
-nsresult
-nsEffectiveTLDService::LoadEffectiveTLDFiles()
-{
-  nsCOMPtr<nsIFile> effTLDFile;
-  nsresult rv = LocateEffectiveTLDFile(effTLDFile, PR_FALSE);
-
-  // If we didn't find any system effective-TLD file, warn but keep trying.  We
-  // can struggle along using the base TLDs.
-  if (NS_FAILED(rv) || nsnull == effTLDFile) {
-    NS_WARNING("No effective-TLD file found in system res directory");
-  }
-  else {
-    rv = LoadOneEffectiveTLDFile(effTLDFile);
-    NS_ENSURE_SUCCESS(rv, rv);
-  }
-
-  rv = LocateEffectiveTLDFile(effTLDFile, PR_TRUE);
-
-  // Since the profile copy isn't strictly needed, ignore any errors trying to
-  // find or read it, in order to allow testing using xpcshell.
-  if (NS_FAILED(rv) || nsnull == effTLDFile)
-    return NS_OK;
-
-  return LoadOneEffectiveTLDFile(effTLDFile);
 }
