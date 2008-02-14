7d712ce15a826ac09cdc1b02316e03be8dc136fb
Dan Mills <thunder@mozilla.com>
2008-02-13 16:07:11 -0800

    clean up prefs; add logging prefs; check weave is enabled on scheduled sync

--- services/sync/services-sync.js
+++ services/sync/services-sync.js
@@ -1,11 +1,21 @@
 pref("extensions.weave.serverURL", "https://services.mozilla.com/");
 pref("extensions.weave.username", "nobody@mozilla.com");
+
+pref("extensions.weave.encryption", "aes-256-cbc");
+
 pref("extensions.weave.lastversion", "firstrun");
+pref("extensions.weave.lastsync", "0");
+
 pref("extensions.weave.rememberpassword", true);
 pref("extensions.weave.autoconnect", true);
 pref("extensions.weave.enabled", true);
 pref("extensions.weave.bookmarks", true);
 pref("extensions.weave.history", true);
 pref("extensions.weave.schedule", 1);
-pref("extensions.weave.lastsync", "0");
-pref("extensions.weave.encryption", "aes-256-cbc");
+
+
+pref("extensions.weave.log.rootLogger", "Config");
+pref("extensions.weave.log.appender.console", "Warn");
+pref("extensions.weave.log.appender.dump", "Error");
+pref("extensions.weave.log.appender.briefLog", "Info");
+pref("extensions.weave.log.appender.debugLog", "Config");
