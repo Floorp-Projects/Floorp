2e9ea3f98341d31d4429a4a94d3bb40fa2042c59
roc+@cs.cmu.edu <roc+@cs.cmu.edu>
2008-02-27 04:10:24 -0800

    Backing out patch for bug 415163 to try to fix centos5 orange

--- widget/src/gtk2/nsNativeThemeGTK.cpp
+++ widget/src/gtk2/nsNativeThemeGTK.cpp
@@ -899,8 +899,7 @@ nsNativeThemeGTK::GetWidgetPadding(nsIDeviceContext* aContext,
       aWidgetType == NS_THEME_TOOLBAR_BUTTON ||
       aWidgetType == NS_THEME_TOOLBAR_DUAL_BUTTON ||
       aWidgetType == NS_THEME_TAB_SCROLLARROW_BACK ||
-      aWidgetType == NS_THEME_TAB_SCROLLARROW_FORWARD ||
-      aWidgetType == NS_THEME_DROPDOWN_BUTTON) {
+      aWidgetType == NS_THEME_TAB_SCROLLARROW_FORWARD) {
     aResult->SizeTo(0, 0, 0, 0);
     return PR_TRUE;
   }
