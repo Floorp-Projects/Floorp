670a8c71c31079efb765afd3acec81fb1e050c51
brendan@mozilla.org <brendan@mozilla.org>
2008-02-28 16:29:59 -0800

    Unbitrot JS_OPMETER (363529, r=shaver, a=beltzner/sayrer).

--- js/src/jsinterp.c
+++ js/src/jsinterp.c
@@ -2196,9 +2196,9 @@ UnwindScope(JSContext *cx, JSStackFrame *fp, jsint stackDepth,
  * are written to JS_OPMETER_HIST, defaulting to /tmp/ops.hist.
  */
 #ifndef JS_OPMETER
-# define METER_OP_INIT(op)      /* nothing */
-# define METER_OP_PAIR(op1,op2) /* nothing */
-# define METER_SLOT_OP(op,slot) /* nothing */
+# define METER_OP_INIT(op)      ((void)0)
+# define METER_OP_PAIR(op1,op2) ((void)0)
+# define METER_SLOT_OP(op,slot) ((void)0)
 #else
 
 # include <stdlib.h>
@@ -2264,12 +2264,12 @@ js_DumpOpMeters()
 
     graph = (Edge *) calloc(nedges, sizeof graph[0]);
     for (i = nedges = 0; i < JSOP_LIMIT; i++) {
-        from = js_CodeSpec[i].name;
+        from = js_CodeName[i];
         for (j = 0; j < JSOP_LIMIT; j++) {
             count = succeeds[i][j];
             if (count != 0 && SIGNIFICANT(count, total)) {
                 graph[nedges].from = from;
-                graph[nedges].to = js_CodeSpec[j].name;
+                graph[nedges].to = js_CodeName[j];
                 graph[nedges].count = count;
                 ++nedges;
             }
@@ -2315,7 +2315,7 @@ js_DumpOpMeters()
         for (j = 0; j < HIST_NSLOTS; j++) {
             if (slot_ops[i][j] != 0) {
                 /* Reuse j in the next loop, since we break after. */
-                fprintf(fp, "%-8.8s", js_CodeSpec[i].name);
+                fprintf(fp, "%-8.8s", js_CodeName[i]);
                 for (j = 0; j < HIST_NSLOTS; j++)
                     fprintf(fp, " %7lu", (unsigned long)slot_ops[i][j]);
                 putc('\n', fp);
