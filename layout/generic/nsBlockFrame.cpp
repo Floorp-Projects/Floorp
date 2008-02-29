828e13b8da0599fe45214740c4c10daa89cc0609
dholbert@cs.stanford.edu <dholbert@cs.stanford.edu>
2008-02-29 13:45:43 -0800

    Bug 418766: Use width, not height, as a quick proxy for whether a line is empty. r+sr=roc, a=beltzner

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -1923,7 +1923,7 @@ nsBlockFrame::ReflowDirtyLines(nsBlockReflowState& aState)
       // to the result of nextLine->ShouldApplyTopMargin.
       if (line.next() != end_lines()) {
         PRBool maybeWasEmpty = oldY == line.next()->mBounds.y;
-        PRBool isEmpty = line->mBounds.height == 0 && line->CachedIsEmpty();
+        PRBool isEmpty = line->mBounds.width == 0 && line->CachedIsEmpty();
         if (maybeReflowingForFirstTime /*1*/ ||
             (isEmpty || maybeWasEmpty) /*2/3/4*/) {
           line.next()->MarkPreviousMarginDirty();
