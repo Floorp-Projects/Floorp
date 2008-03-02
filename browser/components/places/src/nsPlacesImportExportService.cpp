82d7179e1a9534468008319d3e8ba80eccdbb738
reed@reedloden.com <reed@reedloden.com>
2008-03-02 00:56:21 -0800

    Bug 419654 - "Importing bookmark file corrupts heap (reproducible)" (Fix heap corruption and increase initial capacity) [p=ondrej@allpeers.com (Ondrej Brablc) r=dietrich a1.9b4=schrep a=blocking-firefox3+]

--- browser/components/places/src/nsPlacesImportExportService.cpp
+++ browser/components/places/src/nsPlacesImportExportService.cpp
@@ -329,6 +329,8 @@ nsPlacesImportExportService::~nsPlacesImportExportService()
 class BookmarkContentSink : public nsIHTMLContentSink
 {
 public:
+  BookmarkContentSink();
+
   nsresult Init(PRBool aAllowRootChanges,
                 nsINavBookmarksService* bookmarkService,
                 PRInt64 aFolder,
@@ -435,6 +437,10 @@ protected:
   }
 #endif
 };
+
+BookmarkContentSink::BookmarkContentSink() : mFrames(16)
+{
+}
 
 // BookmarkContentSink::Init
 //
@@ -1222,9 +1228,11 @@ BookmarkContentSink::NewFrame()
     // don't clear last-modified, in case there's a description
   }
 
+  frame.mPreviousId = ourID;
+
   if (!mFrames.AppendElement(BookmarkImportFrame(ourID)))
     return NS_ERROR_OUT_OF_MEMORY;
-  frame.mPreviousId = ourID;
+
   return NS_OK;
 }
 
