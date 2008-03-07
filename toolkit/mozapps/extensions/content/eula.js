a78b3f47297abe87e58d88106aa4047fc1c2b7a9
dtownsend@oxymoronical.com <dtownsend@oxymoronical.com>
2008-03-06 17:56:20 -0800

    Bug 414481: Get Add-ons Tab: make EULA text selectable. r=robstrong, a=beltzner

--- toolkit/mozapps/extensions/content/eula.js
+++ toolkit/mozapps/extensions/content/eula.js
@@ -39,5 +39,5 @@ function Startup() {
   var bundle = document.getElementById("extensionsStrings");
   var label = document.createTextNode(bundle.getFormattedString("eulaHeader", [window.arguments[0].name]));
   document.getElementById("heading").appendChild(label);
-  document.getElementById("eula").appendChild(document.createTextNode(window.arguments[0].text));
+  document.getElementById("eula").value = window.arguments[0].text;
 }
