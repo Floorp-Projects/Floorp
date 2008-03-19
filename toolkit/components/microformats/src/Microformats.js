b911805c550406165cf44ab84de728a7fdbc5783
mkaply@us.ibm.com <mkaply@us.ibm.com>
2008-03-19 12:52:54 -0700

    Bug 420882 - r=sayrer, a=schrep - do some validation of geo lat/long

--- toolkit/components/microformats/src/Microformats.js
+++ toolkit/components/microformats/src/Microformats.js
@@ -524,7 +524,10 @@ var Microformats = {
           result = Microformats.parser.HTMLGetter(node, parentnode);
           break;
         case "float":
-          result = parseFloat(Microformats.parser.textGetter(node, parentnode));
+          var asText = Microformats.parser.textGetter(node, parentnode);
+          if (!isNaN(asText)) {
+            result = parseFloat(asText);
+          }
           break;
         case "custom":
           result = prop.customGetter(node, parentnode);
@@ -1572,11 +1575,20 @@ function geo(node, validate) {
   }
 }
 geo.prototype.toString = function() {
-  if (this.latitude && this.longitude) {
+  if (this.latitude != undefined) {
+    if (!isFinite(this.latitude) || (this.latitude > 360) || (this.latitude < -360)) {
+      return;
+    }
+  }
+  if (this.longitude != undefined) {
+    if (!isFinite(this.longitude) || (this.longitude > 360) || (this.longitude < -360)) {
+      return;
+    }
+  }
+
+  if ((this.latitude != undefined) && (this.longitude != undefined)) {
     var s;
-    if ((this.node.localName.toLowerCase() != "abbr") && (this.node.localName.toLowerCase() != "html:abbr")) {
-      s = Microformats.parser.textGetter(this.node);
-    } else {
+    if ((this.node.localName.toLowerCase() == "abbr") || (this.node.localName.toLowerCase() == "html:abbr")) {
       s = this.node.textContent;
     }
 
@@ -1626,7 +1638,9 @@ var geo_definition = {
         if (value.match(';')) {
           latlong = value.split(';');
           if (latlong[0]) {
-            return parseFloat(latlong[0]);
+            if (!isNaN(latlong[0])) {
+              return parseFloat(latlong[0]);
+            }
           }
         }
       }
@@ -1641,11 +1655,32 @@ var geo_definition = {
         if (value.match(';')) {
           latlong = value.split(';');
           if (latlong[1]) {
-            return parseFloat(latlong[1]);
+            if (!isNaN(latlong[1])) {
+              return parseFloat(latlong[1]);
+            }
           }
         }
       }
     }
+  },
+  validate: function(node) {
+    var latitude = Microformats.parser.getMicroformatProperty(node, "geo", "latitude");
+    var longitude = Microformats.parser.getMicroformatProperty(node, "geo", "longitude");
+    if (latitude != undefined) {
+      if (!isFinite(latitude) || (latitude > 360) || (latitude < -360)) {
+        throw("Invalid latitude");
+      }
+    } else {
+      throw("No latitude specified");
+    }
+    if (longitude != undefined) {
+      if (!isFinite(longitude) || (longitude > 360) || (longitude < -360)) {
+        throw("Invalid longitude");
+      }
+    } else {
+      throw("No longitude specified");
+    }
+    return true;
   }
 };
 
