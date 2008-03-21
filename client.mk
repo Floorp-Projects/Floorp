721c1a17afb8eb9d36ce32d465cf9c01ee632643
dbaron@dbaron.org <dbaron@dbaron.org>
2008-03-21 13:46:50 -0700

    Put something non-empty that changes (the output of date) into the .nss.checkout file so it is better reflected when mirroring a CVS checkout into other version control systems.  b=421861  r=bsmedberg  a1.9b5=schrep

--- client.mk
+++ client.mk
@@ -812,7 +812,7 @@ real_checkout:
 # update the NSS checkout timestamp, if we checked PSM out
 	@if test -d $(TOPSRCDIR)/security/manager -a \
 		 `egrep -c '^(U|C) mozilla/security/(nss|coreconf)' $(CVSCO_LOGFILE) 2>/dev/null` != 0; then \
-		touch $(TOPSRCDIR)/security/manager/.nss.checkout; \
+		date > $(TOPSRCDIR)/security/manager/.nss.checkout; \
 	fi
 ifdef RUN_AUTOCONF_LOCALLY
 	cd $(ROOTDIR) && \
