2d7e70b9f1356e33774f15777e12ed74757a21f4
smontagu@smontagu.org <smontagu@smontagu.org>
2008-03-25 13:19:34 -0700

    Remove unnecessary and obsolete code. Bug 424916, r+sr=dbaron, a=damons

--- extensions/universalchardet/src/base/nsEUCTWProber.cpp
+++ extensions/universalchardet/src/base/nsEUCTWProber.cpp
@@ -52,11 +52,6 @@ nsProbingState nsEUCTWProber::HandleData(const char* aBuf, PRUint32 aLen)
   for (PRUint32 i = 0; i < aLen; i++)
   {
     codingState = mCodingSM->NextState(aBuf[i]);
-    if (codingState == eError)
-    {
-      mState = eNotMe;
-      break;
-    }
     if (codingState == eItsMe)
     {
       mState = eFoundIt;
