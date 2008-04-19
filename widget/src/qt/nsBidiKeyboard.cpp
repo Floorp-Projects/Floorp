13e8d9e3339ca3bbdea9395e2e0b3a5a91f789df
pekka.vanhoja <>
2008-04-19 18:41:10 +0300

    Added qt bidikeyboard implementation

--- widget/src/qt/nsBidiKeyboard.cpp
+++ widget/src/qt/nsBidiKeyboard.cpp
@@ -66,6 +66,5 @@ NS_IMETHODIMP nsBidiKeyboard::IsLangRTL(PRBool *aIsRTL)
 
 NS_IMETHODIMP nsBidiKeyboard::SetLangFromBidiLevel(PRUint8 aLevel)
 {
-    // XXX Insert platform specific code to set keyboard language
-    return NS_ERROR_NOT_IMPLEMENTED;
+    return NS_OK;
 }
