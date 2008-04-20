887661e35ad011eff3c917db3e2b39a645ce6831
Vladimir Vukicevic <vladimir@pobox.com>
2008-04-20 06:54:27 -0700

    Comment out X11-specific code to enable Qt Embedded builds

--- widget/src/qt/nsIdleServiceQt.cpp
+++ widget/src/qt/nsIdleServiceQt.cpp
@@ -37,12 +37,14 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include <QX11Info>
-
 #include "nsIdleServiceQt.h"
 #include "nsIServiceManager.h"
 #include "nsDebug.h"
 #include "prlink.h"
+
+
+#ifdef Q_WS_X11
+#include <QX11Info>
 
 typedef PRBool (*_XScreenSaverQueryExtension_fn)(Display* dpy, int* event_base,
                                                  int* error_base);
@@ -132,3 +134,27 @@ nsIdleServiceQt::GetIdleTime(PRUint32 *aTimeDiff)
 
     return NS_ERROR_FAILURE;
 }
+
+#else
+
+NS_IMPL_ISUPPORTS1(nsIdleServiceQt, nsIIdleService)
+
+nsIdleServiceQt::nsIdleServiceQt()
+{
+}
+
+static void Initialize()
+{
+}
+
+nsIdleServiceQt::~nsIdleServiceQt()
+{
+}
+
+NS_IMETHODIMP
+nsIdleServiceQt::GetIdleTime(PRUint32 *aTimeDiff)
+{
+    return NS_ERROR_FAILURE;
+}
+
+#endif
