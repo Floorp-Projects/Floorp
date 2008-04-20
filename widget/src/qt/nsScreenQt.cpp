3be4c64d4849e1a5ea6da3687ceca4f8e78af483
vlad <>
2008-04-20 11:16:40 +0300

    Qt: widget/gfx work
    - Fix crash on window close
      - Qt expects the QWidget to remain available even after it has
    processed a close event. So, use deleteLater() instead of
    deleting outright.
      - Also carry around the MozQWidget pointer (though we need to rework
    CreateQWidget and ::Initialize) so that we can call dropReceiver
    
    - Fix screen <-> widget coordinate translation
      - Fixes popup menu positioning
    
    - Honor mayWait in nsAppShell::ProcessNextNativeEvent
      - Prevents massive busy-waiting
    
    - Don't pretend to support translucent windows
    
    - Fix embedding widget build failure
    
    - cairo-qpainter: invert source matrix in paint() to get proper
    transform (proper positioning of elements drawn using paint())
    
    - Cleaner event handling in mozqwidget -- return full nsEventStatus,
    not just boolean, and decide whether to call QWidget::event() based
    on that.

--- widget/src/qt/nsScreenQt.cpp
+++ widget/src/qt/nsScreenQt.cpp
@@ -46,8 +46,8 @@
 #include <qapplication.h>
 
 nsScreenQt::nsScreenQt(int aScreen)
+    : mScreen(aScreen)
 {
-    screen = aScreen;
     // nothing else to do. I guess we could cache a bunch of information
     // here, but we want to ask the device at runtime in case anything
     // has changed.
@@ -61,11 +61,11 @@ nsScreenQt::~nsScreenQt()
 // addref, release, QI
 NS_IMPL_ISUPPORTS1(nsScreenQt, nsIScreen)
 
-    NS_IMETHODIMP
+NS_IMETHODIMP
 nsScreenQt::GetRect(PRInt32 *outLeft,PRInt32 *outTop,
                     PRInt32 *outWidth,PRInt32 *outHeight)
 {
-    QRect r = QApplication::desktop()->screenGeometry(screen);
+    QRect r = QApplication::desktop()->screenGeometry(mScreen);
     *outTop = r.x();
     *outLeft = r.y();
     *outWidth = r.width();
@@ -78,7 +78,7 @@ NS_IMETHODIMP
 nsScreenQt::GetAvailRect(PRInt32 *outLeft,PRInt32 *outTop,
                          PRInt32 *outWidth,PRInt32 *outHeight)
 {
-    QRect r = QApplication::desktop()->availableGeometry(screen);
+    QRect r = QApplication::desktop()->availableGeometry(mScreen);
     *outTop = r.x();
     *outLeft = r.y();
     *outWidth = r.width();
