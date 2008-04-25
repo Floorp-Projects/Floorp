99f3f29d4323436c7df756de807ba40d1c2b4e65
Olli.Pettay@helsinki.fi <Olli.Pettay@helsinki.fi>
2008-04-25 02:45:58 -0700

    Bug 430276, new XMLHttpRequest() after document.write throws an exception, r+sr=jst, a=beltzner

--- dom/src/base/nsDOMClassInfo.cpp
+++ dom/src/base/nsDOMClassInfo.cpp
@@ -4999,14 +4999,19 @@ nsDOMConstructor::Create(const PRUnichar* aName,
                          nsDOMConstructor** aResult)
 {
   *aResult = nsnull;
-  if (!aOwner->IsOuterWindow()) {
-    *aResult = new nsDOMConstructor(aName, aNameStruct, aOwner);
-  } else if (!nsContentUtils::CanCallerAccess(aOwner)) {
+  // Prevent creating a constructor if
+  // - aOwner is inner window which doesn't have outer window or
+  // - outer window doesn't have inner window or
+  // - caller can't access outer window's inner window.
+  nsPIDOMWindow* outerWindow = aOwner->GetOuterWindow();
+  nsPIDOMWindow* currentInner =
+    outerWindow ? outerWindow->GetCurrentInnerWindow() : nsnull;
+  if (!currentInner ||
+      (aOwner != currentInner &&
+       !nsContentUtils::CanCallerAccess(currentInner))) {
     return NS_ERROR_DOM_SECURITY_ERR;
-  } else {
-    *aResult =
-      new nsDOMConstructor(aName, aNameStruct, aOwner->GetCurrentInnerWindow());
   }
+  *aResult = new nsDOMConstructor(aName, aNameStruct, currentInner);
   NS_ENSURE_TRUE(*aResult, NS_ERROR_OUT_OF_MEMORY);
   NS_ADDREF(*aResult);
   return NS_OK;
