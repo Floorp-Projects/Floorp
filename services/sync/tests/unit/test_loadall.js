b9fca1f94d23fa7e16936727ee4c92b93caa2d92
Dan Mills <thunder@mozilla.com>
2008-04-30 13:01:17 -0700

    add a simple unit test harness based on xpcshell; add two tests for PBE and to load all the modules; add some hacks to the component's makefile to make tests work correctly

---
+++ services/sync/tests/unit/test_loadall.js
@@ -0,0 +1,15 @@
+function run_test() {
+  Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+  Components.utils.import("resource://weave/constants.js");
+  Components.utils.import("resource://weave/log4moz.js");
+  Components.utils.import("resource://weave/util.js");
+  Components.utils.import("resource://weave/async.js");
+  Components.utils.import("resource://weave/crypto.js");
+  Components.utils.import("resource://weave/identity.js");
+  Components.utils.import("resource://weave/dav.js");
+  Components.utils.import("resource://weave/wrap.js");
+  Components.utils.import("resource://weave/stores.js");
+  Components.utils.import("resource://weave/syncCores.js");
+  Components.utils.import("resource://weave/engines.js");
+  Components.utils.import("resource://weave/service.js");
+}
