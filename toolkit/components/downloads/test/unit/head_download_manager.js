1b4ae4fc79f3394b127e24fdfe43649c0c2f9c74
bugzilla@standard8.plus.com <bugzilla@standard8.plus.com>
2008-05-04 06:50:25 -0700

    Bug 431124 xpcshell test test_bug_395092.js fails on Thunderbird. r=sdwilsh, test-only change

--- toolkit/components/downloads/test/unit/head_download_manager.js
+++ toolkit/components/downloads/test/unit/head_download_manager.js
@@ -135,14 +135,12 @@ function addDownload(aResultFileName)
 
   var destFile = dirSvc.get("ProfD", Ci.nsIFile);
   destFile.append(aResultFileName || "download.result");
-  var srcFile = dirSvc.get("ProfD", Ci.nsIFile);
-  srcFile.append("LICENSE");
 
   // it is part of the active downloads the moment addDownload is called
   gDownloadCount++;
 
   var dl = dm.addDownload(nsIDownloadManager.DOWNLOAD_TYPE_DOWNLOAD,
-                          createURI("http://localhost:4444/LICENSE"),
+                          createURI("http://localhost:4444/res/language.properties"),
                           createURI(destFile), null, null,
                           Math.round(Date.now() * 1000), null, persist);
 
