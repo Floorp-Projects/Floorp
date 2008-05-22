1d7de9c30b9d34ed3b64152805ee677809b94b08
Justin Dolske <dolske@mozilla.com>
2008-05-21 17:53:35 -0700

    Cleanup some trivial nits with cookie (whitespace and function name).

--- services/sync/modules/stores.js
+++ services/sync/modules/stores.js
@@ -796,7 +796,7 @@ CookieStore.prototype = {
     return this.__cookieManager
   },
 
-  _createCommand: function HistStore__createCommand(command) {
+  _createCommand: function CookieStore__createCommand(command) {
     /* we got a command to create a cookie in the local browser
        in order to sync with the server. */
 
