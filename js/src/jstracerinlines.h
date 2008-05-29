fa46fae408997496d23d7140057153a66e85e443
Brendan Eich <brendan@mozilla.org>
2008-05-28 19:07:25 -0700

    Split trace-supporting inlines, add ifdefs/macros for deriving js_TracingInterpret in jstracer.cpp.

--- js/src/jstracerinlines.h
+++ js/src/jstracerinlines.h
@@ -14,14 +14,13 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is Mozilla SpiderMonkey JavaScript 1.8 code, released
- * March 4, 2008.
+ * The Original Code is Mozilla SpiderMonkey JavaScript 1.9 code, released
+ * May 28, 2008.
  *
  * The Initial Developer of the Original Code is
- *   Igor Bukanov <igor@mir2.org>
+ *   Brendan Eich <brendan@mozilla.org
  *
  * Contributor(s):
- *   Brendan Eich <brendan@mozilla.org
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either of the GNU General Public License Version 2 or later (the "GPL"),
@@ -37,6 +36,9 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#define jsinvoke_cpp___
+#ifndef jstracer_h___
+#define jstracer_h___
 
-#include "jsinterp.cpp"
+#include "jsinterpinlines.h"	// XXXbe fork me
+
+#endif /* jstracer_h___ */
