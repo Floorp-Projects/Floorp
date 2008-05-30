fd8c97eb6052ae1be36121adbba4e8142ac802b1
Atul Varma <varmaa@toolness.com>
2008-05-30 16:32:06 -0700

    Added a trivial appender to faultTolerance.js.

--- services/sync/modules/faultTolerance.js
+++ services/sync/modules/faultTolerance.js
@@ -1,3 +1,6 @@
+const Cu = Components.utils;
+Cu.import("resource://weave/log4moz.js");
+
 const EXPORTED_SYMBOLS = ["FaultTolerance"];
 
 FaultTolerance = {
@@ -6,13 +9,35 @@ FaultTolerance = {
       this._Service = new FaultToleranceService();
     return this._Service;
   }
-}
+};
 
 function FaultToleranceService() {
 }
 
 FaultToleranceService.prototype = {
-  processMessage: function FTApp_doAppend(message) {
-    dump(message);
+  init: function FTS_init() {
+    var appender = new Appender();
+
+    Log4Moz.Service.rootLogger.addAppender(appender);
   }
 };
+
+function Formatter() {
+}
+Formatter.prototype = {
+  format: function FTF_format(message) {
+    return message;
+  }
+};
+Formatter.prototype.__proto__ = new Log4Moz.Formatter();
+
+function Appender() {
+  this._name = "FaultToleranceAppender";
+  this._formatter = new Formatter();
+}
+Appender.prototype = {
+  doAppend: function FTA_append(message) {
+    // TODO: Implement this.
+  }
+};
+Appender.prototype.__proto__ = new Log4Moz.Appender();
