f107a5fae3dab6b3a8510243dbee85812c7581a3
Atul Varma <varmaa@toolness.com>
2008-06-09 18:55:26 -0700

    Added modules/sharing.js, which provides access to the RESTful sharing API, and a unit test suite.  The unit test suite is pretty heinous right now and should get a bit of refactoring.

---
+++ services/sync/tests/unit/test_sharing.js
@@ -0,0 +1,62 @@
+const Cu = Components.utils;
+
+Cu.import("resource://weave/sharing.js");
+Cu.import("resource://weave/async.js");
+Cu.import("resource://weave/util.js");
+Cu.import("resource://weave/log4moz.js");
+
+function runTestGenerator() {
+  let self = yield;
+
+  let fakeDav = {
+    POST: function fakeDav_POST(url, data, callback) {
+      let result = {status: 200, responseText: "OK"};
+      let cb = function() { callback(result); };
+
+      let timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+      timer.initWithCallback(new Utils.EventListener(cb),
+                             0, timer.TYPE_ONE_SHOT);
+    }
+  };
+
+  let api = new Sharing.Api(fakeDav);
+  api.shareWithUsers("/fake/dir", ["johndoe"], self.cb);
+  let result = yield;
+
+  do_check_eq(result.wasSuccessful, true);
+  self.done();
+}
+
+function BasicFormatter() {
+  this.errors = 0;
+}
+BasicFormatter.prototype = {
+  format: function BF_format(message) {
+    let date = new Date(message.time);
+    if (message.level == Log4Moz.Level.Error)
+      this.errors += 1;
+    return message.loggerName + "\t" + message.levelDesc + "\t" +
+      message.message + "\n";
+  }
+};
+BasicFormatter.prototype.__proto__ = new Log4Moz.Formatter();
+
+function run_test() {
+  var log = Log4Moz.Service.rootLogger;
+  var formatter = new BasicFormatter();
+  var appender = new Log4Moz.DumpAppender(formatter);
+  log.level = Log4Moz.Level.Debug;
+  appender.level = Log4Moz.Level.Debug;
+  log.addAppender(appender);
+
+  var threadManager = Cc["@mozilla.org/thread-manager;1"].getService();
+  let thread = threadManager.currentThread;
+  let gen = Async.run({}, runTestGenerator);
+
+  while (gen.generator && !formatter.errors) {
+    thread.processNextEvent(true);
+  }
+
+  if (formatter.errors)
+    throw new Error("Errors occurred.");
+}
