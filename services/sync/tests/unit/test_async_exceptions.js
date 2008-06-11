1a2855c05b2e762d41a3ea34c75c6ead795076e7
Atul Varma <varmaa@toolness.com>
2008-06-11 16:38:22 -0700

    Added a unit test for async exceptions.

---
+++ services/sync/tests/unit/test_async_exceptions.js
@@ -0,0 +1,47 @@
+const Cu = Components.utils;
+
+Cu.import("resource://weave/util.js");
+Cu.import("resource://weave/async.js");
+
+Function.prototype.async = Async.sugar;
+
+function thirdGen() {
+  let self = yield;
+
+  Utils.makeTimerForCall(self.cb);
+  yield;
+
+  throw new Error("intentional failure");
+}
+
+function secondGen() {
+  let self = yield;
+
+  thirdGen.async({}, self.cb);
+  try {
+    let result = yield;
+  } catch (e) {
+    do_check_eq(e.message, "Error: intentional failure");
+    throw e;
+  }
+
+  self.done();
+}
+
+function runTestGenerator() {
+  let self = yield;
+
+  secondGen.async({}, self.cb);
+  let wasCaught = false;
+  try {
+    let result = yield;
+  } catch (e) {
+    do_check_eq(e.message, "Error: intentional failure");
+    wasCaught = true;
+  }
+
+  do_check_true(wasCaught);
+  self.done();
+}
+
+var run_test = makeAsyncTestRunner(runTestGenerator);
