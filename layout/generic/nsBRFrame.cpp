c34399e064cf58562930230338a804925749dc03
Robert O'Callahan <robert@ocallahan.org>
2008-06-17 10:39:30 +1200

    Backing out bug 421436 due to regression bug 439462

--- layout/generic/nsBRFrame.cpp
+++ layout/generic/nsBRFrame.cpp
@@ -159,6 +159,14 @@ BRFrame::Reflow(nsPresContext* aPresContext,
       else {
         aMetrics.ascent = aMetrics.height = 0;
       }
+
+      // XXX temporary until I figure out a better solution; see the
+      // code in nsLineLayout::VerticalAlignFrames that zaps minY/maxY
+      // if the width is zero.
+      // XXX This also fixes bug 10036!
+      // Warning: nsTextControlFrame::CalculateSizeStandard depends on
+      // the following line, see bug 228752.
+      aMetrics.width = 1;
     }
 
     // Return our reflow status
