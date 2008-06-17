feffbb7a14454556ecafd2de7bf6b4ec29d1aada
Andreas Gal <gal@uci.edu>
2008-06-17 13:30:59 -0700

    Fixes to the trace recorder.

--- js/src/recorder.js
+++ js/src/recorder.js
@@ -55,7 +55,9 @@
     },
     /* emit an IR instruction */
     emit: function(x, to) {
-        this.map[to] = this.code.push(x);
+        var x = this.code.push(x);
+        if (x)
+            this.map[to] = x;
     },
     /* register a change in the stack pointer */
     setSP: function(sp) {
@@ -63,22 +65,22 @@
     },
     /* create a constant and assign it to v */
     generate_constant: function(v, vv) {
-        this.emit({ op: "generate_constant", value: vv });
+        this.emit({ op: "generate_constant", value: vv }, v);
     },
     boolean_to_jsval: function(b, v, vv) {
-        this.emit({ op: "boolean_to_jsval", value: vv, operand: this.map[b] });
+        this.emit({ op: "boolean_to_jsval", value: vv, operand: this.map[b] }, v);
     },
     string_to_jsval: function(s, v, vv) {
-        this.emit({ op: "string_to_jsval", value: vv, operand: this.map[s] });
+        this.emit({ op: "string_to_jsval", value: vv, operand: this.map[s] }, v);
     },
     object_to_jsval: function(o, v, vv) {
-        this.emit({ op: "object_to_jsval", value: vv, operand: this.map[o] });
+        this.emit({ op: "object_to_jsval", value: vv, operand: this.map[o] }, v);
     },
     id_to_jsval: function(id, v, vv) {
-        this.emit({ op: "id_to_jsval", value: vv, operand: this.map[id] });
+        this.emit({ op: "id_to_jsval", value: vv, operand: this.map[id] }, v);
     },
     guard_jsdouble_is_int_and_int_fits_in_jsval: function(d, i, vv, g) {
         this.emit({ op: "guard_jsdouble_is_int_and_int_fits_in_jsval", value: vv, 
-                    operand: this.map[i], state: g });
+                    operand: this.map[d], state: g }, i);
     }
 });
