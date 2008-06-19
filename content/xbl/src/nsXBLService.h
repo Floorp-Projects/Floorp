8282a8faca260110dcf52d452d5e5b98d4c7e2ff
Neil Deakin <neil@mozilla.com>
2008-06-19 09:36:46 -0400

    Bug 437640, support removing <keyset> elements, and add support for a disabled attribute,r+sr=neil

--- content/xbl/src/nsXBLService.h
+++ content/xbl/src/nsXBLService.h
@@ -88,6 +88,7 @@ class nsXBLService : public nsIXBLService,
 
   // Used by XUL key bindings and for window XBL.
   NS_IMETHOD AttachGlobalKeyHandler(nsPIDOMEventTarget* aTarget);
+  NS_IMETHOD DetachGlobalKeyHandler(nsPIDOMEventTarget* aTarget);
 
   NS_DECL_NSIOBSERVER
 
