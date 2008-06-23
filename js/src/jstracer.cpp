b585428002955aa3f7cbe5c37b825620be8dab91
Andreas Gal <gal@uci.edu>
2008-06-23 16:51:28 -0700

    Merge.

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -101,6 +101,7 @@ Tracker::get(const void* v) const
     struct Tracker::Page* p = findPage(v);
     JS_ASSERT(p != 0); /* we must have a page for the slot we are looking for */
     LIns* i = p->map[(((long)v) & 0xfff) >> 2];
+    printf("get v=%p ins=%p\n", v, i);
     JS_ASSERT(i != 0);
     return i;
 }
@@ -108,11 +109,47 @@ Tracker::get(const void* v) const
 void 
 Tracker::set(const void* v, LIns* ins) 
 {
+    static int q = 0;
+    printf("q=%d set v=%p ins=%p\n", q, v, ins);
+    //if (q++ > 3)
+    //printf("%d: 0x80689c is %p\n", q, get((void*)0x80689c));
     struct Tracker::Page* p = findPage(v);
     if (!p) 
         p = addPage(v);
     p->map[(((long)v) & 0xfff) >> 2] = ins;
 }
+
+template <int N>
+class BitStream
+{
+    uint32_t* ptr;
+    unsigned n;
+public:
+    BitStream(uint32_t* p, int words) 
+    {
+        ptr = p;
+        n = 0;
+    }
+    
+    void write(int data) 
+    {
+        if (n + N > sizeof(uint32_t)) {
+            n = 0;
+            ++ptr;
+        }
+        *ptr |= ((data & 7) << n);
+        n += N;
+    }
+    
+    unsigned read(int data)
+    {
+        if (n + N > sizeof(uint32_t)) {
+            n = 0;
+            ++ptr;
+        }
+        return (*ptr >> n) & 7;
+    }
+};
 
 using namespace avmplus;
 using namespace nanojit;
@@ -207,4 +244,12 @@ js_EndRecording(JSContext* cx, JSFrameRegs& regs)
         compile(tm->fragmento->assm(), tm->fragment);
     }
     tm->status = IDLE;
+/*   
+    uint32_t x[2];
+    BitStream<3> w(x, 2);
+    for (int n = 0; n < 12; ++n)
+        w.write(n);
+    BitStream<3> r(x, 2);
+    for (int n = 0; n < 12; ++n)
+        printf("bs: %d\n", r.read(n));*/
 }
