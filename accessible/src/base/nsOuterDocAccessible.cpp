387995b3b87301063d4562f75637c897b0ab1556
Marco Zehe <marco.zehe@googlemail.com>
2008-06-24 15:03:06 +0200

    bug 440770 - DOCUMENT_FRAME has a parent INTERNAL_FRAME with an action, r=aaronlev

--- accessible/src/base/nsOuterDocAccessible.cpp
+++ accessible/src/base/nsOuterDocAccessible.cpp
@@ -148,3 +148,35 @@ nsOuterDocAccessible::GetAttributesInternal(nsIPersistentProperties *aAttributes
   }
   return nsAccessible::GetAttributesInternal(aAttributes);
 }
+
+// Internal frame, which is the doc's parent, should not have a click action
+NS_IMETHODIMP
+nsOuterDocAccessible::GetNumActions(PRUint8 *aNumActions)
+{
+  NS_ENSURE_ARG_POINTER(aNumActions);
+  *aNumActions = 0;
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsOuterDocAccessible::GetActionName(PRUint8 aIndex, nsAString& aName)
+{
+  aName.Truncate();
+
+  return NS_ERROR_INVALID_ARG;
+}
+
+NS_IMETHODIMP
+nsOuterDocAccessible::GetActionDescription(PRUint8 aIndex, nsAString& aDescription)
+{
+  aDescription.Truncate();
+
+  return NS_ERROR_INVALID_ARG;
+}
+
+NS_IMETHODIMP
+nsOuterDocAccessible::DoAction(PRUint8 aIndex)
+{
+  return NS_ERROR_INVALID_ARG;
+}
