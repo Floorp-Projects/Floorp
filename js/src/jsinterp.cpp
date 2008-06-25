f9264deae0da9a54ad431936d3077db3400c544e
Brendan Eich <brendan@mozilla.org>
2008-06-24 17:28:22 -0700

    Fix 433672, r=igor, a=shaver.

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -3830,10 +3830,10 @@ js_Interpret(JSContext *cx)
 
 #define BINARY_OP(OP)                                                         \
     JS_BEGIN_MACRO                                                            \
-        FETCH_NUMBER(cx, -1, d2);                                             \
         FETCH_NUMBER(cx, -2, d);                                              \
+        FETCH_NUMBER(cx, -1, d2);                                             \
         d = d OP d2;                                                          \
-        regs.sp--;                                                                 \
+        regs.sp--;                                                            \
         STORE_NUMBER(cx, -1, d);                                              \
     JS_END_MACRO
 
