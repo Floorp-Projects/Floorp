ae73e2fa311bea347b4bff90a90e91fd5a5ba4e3
Marco Zehe <marco.zehe@googlemail.com>
2008-06-25 09:27:45 +0200

    bug 438784 - URL of loaded page no longer exposed as the accValue for the Doc accessible, r=surkov

--- accessible/src/msaa/nsAccessibleWrap.cpp
+++ accessible/src/msaa/nsAccessibleWrap.cpp
@@ -333,6 +333,12 @@ __try {
     if (NS_FAILED(xpAccessible->GetValue(value)))
       return E_FAIL;
 
+    // see bug 438784: Need to expose URL on doc's value attribute.
+    // For this, reverting part of fix for bug 425693 to make this MSAA method 
+    // behave IAccessible2-style.
+    if (value.IsEmpty())
+      return S_FALSE;
+
     *pszValue = ::SysAllocStringLen(value.get(), value.Length());
     if (!*pszValue)
       return E_OUTOFMEMORY;
