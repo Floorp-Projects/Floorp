ed8a7a5add6fbe3c0850dd279ee5fecdf3883273
Atul Varma <varmaa@toolness.com>
2008-06-25 14:30:53 -0700

    Added a number of "real" bookmark sync tests; see test_bookmark_syncing.js for information.

--- services/sync/tests/unit/test_bookmark_syncing.js
+++ services/sync/tests/unit/test_bookmark_syncing.js
@@ -1,18 +1,65 @@
 Cu.import("resource://weave/engines/bookmarks.js");
 
+load("bookmark_setup.js");
+
 // ----------------------------------------
 // Test Logic
 // ----------------------------------------
+
+function FakeMicrosummaryService() {
+  return {hasMicrosummary: function() { return false; }};
+}
 
 function run_test() {
   var syncTesting = new SyncTestingInfrastructure();
 
   function freshEngineSync(cb) {
     let engine = new BookmarksEngine();
+    engine._store.__ms = new FakeMicrosummaryService();
     engine.sync(cb);
   };
 
-  syncTesting.runAsyncFunc("initial sync", freshEngineSync);
+  function resetProfile() {
+    // Simulate going to another computer by removing stuff from our
+    // objects.
+    syncTesting.fakeFilesystem.fakeContents = {};
+    bms.removeItem(boogleBm);
+    bms.removeItem(yoogleBm);
+  }
+
+  let bms = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
+    getService(Ci.nsINavBookmarksService);
+
+  cleanUp();
+
+  let boogleBm = bms.insertBookmark(bms.bookmarksMenuFolder,
+                                    uri("http://www.boogle.com"),
+                                    -1,
+                                    "Boogle");
+  bms.setItemGUID(boogleBm, "boogle-bookmark-guid");
+
+  syncTesting.runAsyncFunc("initial sync w/ one bookmark", freshEngineSync);
 
   syncTesting.runAsyncFunc("trivial re-sync", freshEngineSync);
+
+  let yoogleBm = bms.insertBookmark(bms.bookmarksMenuFolder,
+                                    uri("http://www.yoogle.com"),
+                                    -1,
+                                    "Yoogle");
+  bms.setItemGUID(yoogleBm, "yoogle-bookmark-guid");
+
+  syncTesting.runAsyncFunc("add bookmark and re-sync", freshEngineSync);
+
+  bms.moveItem(yoogleBm,
+               bms.bookmarksMenuFolder,
+               0);
+
+  syncTesting.runAsyncFunc("swap bookmark order and re-sync",
+                           freshEngineSync);
+
+  resetProfile();
+
+  syncTesting.runAsyncFunc("re-sync on second computer", freshEngineSync);
+
+  cleanUp();
 }
