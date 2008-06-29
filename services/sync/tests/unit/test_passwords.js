0780fb5c835b0673e1e434ca031f8bb04058da82
Justin Dolske <dolske@mozilla.com>
2008-06-29 16:46:59 -0700

    Partial bustage fix: test was broken, is now less broken but still fails.

--- services/sync/tests/unit/test_passwords.js
+++ services/sync/tests/unit/test_passwords.js
@@ -6,13 +6,15 @@ var loginMgr = new FakeLoginManager(fakeSampleLogins);
 var passwords = loadInSandbox("resource://weave/engines/passwords.js");
 
 function test_hashLoginInfo_works() {
-  var fakeUserHash = passwords._hashLoginInfo(fakeSampleLogins[0]);
+  var pwStore = new passwords.PasswordStore();
+  var fakeUserHash = pwStore._hashLoginInfo(fakeSampleLogins[0]);
   do_check_eq(typeof fakeUserHash, 'string');
   do_check_eq(fakeUserHash.length, 40);
 }
 
 function test_synccore_itemexists_works() {
-  var fakeUserHash = passwords._hashLoginInfo(fakeSampleLogins[0]);
+  var pwStore = new passwords.PasswordStore();
+  var fakeUserHash = pwStore._hashLoginInfo(fakeSampleLogins[0]);
   var psc = new passwords.PasswordSyncCore();
   do_check_false(psc._itemExists("invalid guid"));
   do_check_true(psc._itemExists(fakeUserHash));
