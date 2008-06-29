47bd6342d96a35a4407839fa5e142a6cd10a10aa
shaver@mozilla.org <shaver@mozilla.org>
2008-06-29 10:36:31 -0400

    guard on inlined branch in TRY_BRANCH_AFTER_COND

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -3802,7 +3802,7 @@ JS_INTERPRET(JSContext *cx, JSInterpreterState *state)
         diff_ = (uintN) regs.pc[1] - (uintN) JSOP_IFEQ;                       \
         if (diff_ <= 1) {                                                     \
             ADJUST_STACK(-spdec);                                             \
-            /* XXX this probably needs to be guarded properly */              \
+            guard_boolean_is_true(cx, regs, cond);                            \
             if (cond == (diff_ != 0)) {                                       \
                 ++regs.pc;                                                    \
                 len = GET_JUMP_OFFSET(regs.pc);                               \
