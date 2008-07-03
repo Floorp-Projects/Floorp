95e2024781e0f3fb03a8a5c82bc5db32073e1dcb
Dietrich Ayala <dietrich@mozilla.com>
2008-07-03 10:17:13 -0700

    Bug 437078 - GUIDs not included in bookmark backups (r=mano)

--- toolkit/components/places/src/utils.js
+++ toolkit/components/places/src/utils.js
@@ -1332,7 +1332,7 @@ var PlacesUtils = {
               // When copying a read-only node, remove the read-only annotation.
               return false;
             }
-            return anno.name != "placesInternal/GUID";
+            return true;
           });
         } catch(ex) {
           LOG(ex);
