b0d019628260d80bbfe00990ebd95d63f191348a
Andreas Gal <gal@mozilla.com>
2008-07-06 17:32:21 -0700

    Reduce redundant doubleToInt32(i2f(x)) chains to simply x.

--- js/src/trace.js
+++ js/src/trace.js
@@ -1,9 +1,11 @@
 f = function() {
 	var q = 1;
 	//for (var j = 0; j < 500; ++j)
-	for (var i = 0; i < 5000; ++i)
-		q += 2.5;
+	for (var i = 0; i < 5000; ++i) {
+		q += 2;
+		q += 2;
 		//++q;
+	}
 	print("q=" + q + " i=" + i);
 }
 
