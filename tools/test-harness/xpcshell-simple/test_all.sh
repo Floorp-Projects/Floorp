c4dbfc356aff93fc9b0478d12795009619519ba2
Gavin Sharp <gavin@mozilla.com>
2008-07-08 11:23:25 -0400

    Fix typo from bug 431745

--- tools/test-harness/xpcshell-simple/test_all.sh
+++ tools/test-harness/xpcshell-simple/test_all.sh
@@ -112,7 +112,6 @@ for t in $testdir/test_*.js
 do
     echo -n "$t: "
     NATIVE_TOPSRCDIR="$native_topsrcdir" TOPSRCDIR="$topsrcdir" $xpcshell -s $headfiles -f $t $tailfiles 2> $t.log 1>&2
-    fi
     rv="$?"
     if [ ! "$rv" = "0"  -o \
          `grep -c '\*\*\* PASS' $t.log` = 0 ]
