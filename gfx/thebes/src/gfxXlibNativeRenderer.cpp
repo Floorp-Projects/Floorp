f39cfdc3c225dfc47ae15606e0a9f8517d38c90f
Karl Tomlinson <karlt+@karlt.net>
2008-07-08 16:58:17 +1200

    remove excess spaces to force rebuild for system-headers changes 6c0971153949

--- gfx/thebes/src/gfxXlibNativeRenderer.cpp
+++ gfx/thebes/src/gfxXlibNativeRenderer.cpp
@@ -154,7 +154,7 @@ gfxXlibNativeRenderer::Draw(Display* dpy, gfxContext* ctx, int width, int height
     // Make sure result.surface is null to start with; we rely on it
     // being non-null meaning that a surface actually got allocated.
     result.surface = NULL;
-  
+
     if (output) {
         output->mSurface = NULL;
         output->mUniformAlpha = PR_FALSE;
