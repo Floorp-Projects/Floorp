896870fede79d17e86676bee5fcfa8113b4663fe
Andreas Gal <gal@mozilla.com>
2008-07-10 22:35:06 -0700

    Trying to make arm work. Flying blind here.

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -1015,9 +1015,9 @@ nanojit::Assembler::asm_bailout(LIns *guard, Register state)
     if (exit->ip_adj)
         ADDmi((int32_t)offsetof(InterpState, ip), state, exit->ip_adj);
 #elif defined(NANOJIT_ARM)
+    NanoAssert(offsetof(avmplus::InterpState,ip) == 0);
+    NanoAssert(offsetof(avmplus::InterpState,sp) == 4);
     //NanoAssert(offsetof(avmplus::InterpState,f) == 0);
-    NanoAssert(offsetof(avmplus::InterpState,ip) == 4);
-    NanoAssert(offsetof(avmplus::InterpState,sp) == 8);
     //NanoAssert(offsetof(avmplus::InterpState,rp) == 12);
     //NanoAssert(sizeof(avmplus::InterpState) == 16);
     RegisterMask ptrs = 0x1e; // { R1-R4 }
@@ -1025,9 +1025,9 @@ nanojit::Assembler::asm_bailout(LIns *guard, Register state)
     SUBi(state,16);
     STMIA(state,ptrs);
 
+    if (exit->sp_adj)       ADDi(R2, exit->sp_adj);
+    if (exit->ip_adj)       ADDi(R1, exit->ip_adj);
     //if (exit->rp_adj)       ADDi(R4, exit->rp_adj);
-    if (exit->sp_adj)       ADDi(R3, exit->sp_adj);
-    if (exit->ip_adj)       ADDi(R2, exit->ip_adj);
     //if (exit->f_adj)        ADDi(R1, exit->f_adj);
 
     SUBi(state,16);
