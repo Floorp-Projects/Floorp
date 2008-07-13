11e023b6ed0225836a01b2ff3a584c05bf0d9ceb
Olli Pettay <Olli.Pettay@helsinki.fi>
2008-07-13 22:28:22 +0300

    Bug 444030, Crash [@ nsGenericDOMDataNode::FirstLogicallyAdjacentTextNode], r+sr=jst

--- content/base/src/nsGenericDOMDataNode.h
+++ content/base/src/nsGenericDOMDataNode.h
@@ -314,12 +314,12 @@ protected:
 
   friend class nsText3Tearoff;
 
-  static PRUint32 FirstLogicallyAdjacentTextNode(nsIContent* aParent,
-                                                 PRUint32 aIndex);
+  static PRInt32 FirstLogicallyAdjacentTextNode(nsIContent* aParent,
+                                                PRInt32 aIndex);
 
-  static PRUint32 LastLogicallyAdjacentTextNode(nsIContent* aParent,
-                                                PRUint32 aIndex,
-                                                PRUint32 aCount);
+  static PRInt32 LastLogicallyAdjacentTextNode(nsIContent* aParent,
+                                               PRInt32 aIndex,
+                                               PRUint32 aCount);
 
   nsresult GetWholeText(nsAString& aWholeText);
 
