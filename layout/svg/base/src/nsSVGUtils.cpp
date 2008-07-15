3b486327eaa74e15b42f130f6358831b094a4380
Robert O'Callahan <robert@ocallahan.org>
2008-07-15 15:16:43 +1200

    Bug 445079. Make filters and patterns use an offscreen surface with the ability to create an nsIRenderingContext wrapper as needed, so foreignObject works in those contexts. r=longsonr,sr=mats

--- layout/svg/base/src/nsSVGUtils.cpp
+++ layout/svg/base/src/nsSVGUtils.cpp
@@ -1813,7 +1813,19 @@ nsSVGRenderState::nsSVGRenderState(nsIRenderingContext *aContext) :
   mGfxContext = aContext->ThebesContext();
 }
 
-nsSVGRenderState::nsSVGRenderState(gfxContext *aContext) :
-  mRenderMode(NORMAL), mRenderingContext(nsnull), mGfxContext(aContext)
+nsSVGRenderState::nsSVGRenderState(gfxASurface *aSurface) :
+  mRenderMode(NORMAL)
 {
+  mGfxContext = new gfxContext(aSurface);
+}
+
+nsIRenderingContext*
+nsSVGRenderState::GetRenderingContext(nsIFrame *aFrame)
+{
+  if (!mRenderingContext) {
+    nsIDeviceContext* devCtx = aFrame->PresContext()->DeviceContext();
+    devCtx->CreateRenderingContextInstance(*getter_AddRefs(mRenderingContext));
+    mRenderingContext->Init(devCtx, mGfxContext);
+  }
+  return mRenderingContext;
 }
