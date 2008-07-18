2812cac48f8a81d1cb16df901199b30e3d5f11d3
Dan Mills <thunder@mozilla.com>
2008-07-17 20:40:29 -0700

    time out requests after 30 seconds

--- services/sync/modules/dav.js
+++ services/sync/modules/dav.js
@@ -133,6 +133,12 @@ DAVCollection.prototype = {
     request.mozBackgroundRequest = true;
     request.open(op, this._baseURL + path, true);
 
+    // time out requests after 30 seconds
+    let cb = function() { request.abort(); };
+    let listener = new Utils.EventListener(cb);
+    let timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+    timer.initWithCallback(listener, 30000, timer.TYPE_ONE_SHOT);
+
     // Force cache validation
     let channel = request.channel;
     channel = channel.QueryInterface(Ci.nsIRequest);
@@ -156,7 +162,7 @@ DAVCollection.prototype = {
     if (ret.status == 423)
       this._log.warn("_makeRequest: got status " + ret.status + " (This is not necessarily bad. It could just mean that another Firefox was syncing at the same time.)");
     else
-    if (ret.status < 200 || ret.status >= 300) 
+    if (ret.status < 200 || ret.status >= 300)
       this._log.warn("_makeRequest: got status " + ret.status);
 
     self.done(ret);
