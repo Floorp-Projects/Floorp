fa5a4ce61a6249aa3fe139ba3c6cbf35ac7b25bd
Justin Dolske <dolske@mozilla.com>
2008-07-18 14:18:05 -0700

    Bug 327977 – Password manager overwrites page data for password-only forms. r=gavin

--- toolkit/components/passwordmgr/src/nsLoginManager.js
+++ toolkit/components/passwordmgr/src/nsLoginManager.js
@@ -1002,6 +1002,11 @@ LoginManager.prototype = {
         if (passwordField == null)
             return [false, foundLogins];
 
+        // If there's only a password field and it has a value, there's
+        // nothing for us to do. (Don't clobber the existing value)
+        if (!usernameField && passwordField.value)
+            return [false, foundLogins];
+
         // Need to get a list of logins if we weren't given them
         if (foundLogins == null) {
             var formOrigin = 
