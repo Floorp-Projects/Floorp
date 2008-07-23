85e446af523cc8f4c5e1825a5cc291e7514f79ef
John Fitzgerald <jjfitzgerald@gmail.com>
2008-07-23 12:46:39 -0400

    Calculate the time zone offset correctly. bug 445879, r=mrbkap

--- js/src/jsdate.cpp
+++ js/src/jsdate.cpp
@@ -1254,7 +1254,7 @@ date_getTimezoneOffset(JSContext *cx, uintN argc, jsval *vp)
      * appropriate for this time. This value would be a constant except for
      * daylight savings time.
      */
-    result = (utctime - localtime) / msPerMinute;
+    result = (localtime - utctime) / msPerMinute;
     return js_NewNumberInRootedValue(cx, result, vp);
 }
 
