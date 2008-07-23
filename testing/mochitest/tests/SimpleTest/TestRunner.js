5f2bd448519fabf6673cf2faf27cf4ed8017ebf1
Andrew Schultz <ajschult@verizon.net>
2008-07-23 13:32:30 -0400

    Bug 442125: check Date.now() to try to diagnose focus failures.  r=ted

--- testing/mochitest/tests/SimpleTest/TestRunner.js
+++ testing/mochitest/tests/SimpleTest/TestRunner.js
@@ -69,7 +69,7 @@ TestRunner._toggle = function(el) {
 /**
  * Creates the iframe that contains a test
 **/
-TestRunner._makeIframe = function (url, retry) {
+TestRunner._makeIframe = function (url, retry, startTime) {
     var iframe = $('testframe');
     window.scrollTo(0, $('indicator').offsetTop);
     if (url != "about:blank" && (!document.hasFocus() ||
@@ -79,13 +79,14 @@ TestRunner._makeIframe = function (url, retry) {
         window.focus();
         iframe.focus();
         if (retry < 3) {
-            window.setTimeout('TestRunner._makeIframe("'+url+'", '+(retry+1)+')', 1000);
+            window.setTimeout('TestRunner._makeIframe("'+url+'", '+(retry+1)+', '+startTime+')', 1000);
             return;
         }
 
         var frameWindow = $('testframe').contentWindow.wrappedJSObject ||
                           $('testframe').contentWindow;
-        frameWindow.SimpleTest.ok(false, "Unable to restore focus ("+
+        frameWindow.SimpleTest.ok(false, "Unable to restore focus after "+
+            ((Date.now()-startTime)/1000)+" seconds ("+
             (document.hasFocus() ? document.activeElement : "[none]")+
             "), expect failures and timeouts.");
     }
@@ -130,10 +131,10 @@ TestRunner.runNextTest = function() {
         if (TestRunner.logEnabled)
             TestRunner.logger.log("Running " + url + "...");
         
-        TestRunner._makeIframe(url, 0);
+        TestRunner._makeIframe(url, 0, Date.now());
     }  else {
         $("current-test").innerHTML = "<b>Finished</b>";
-        TestRunner._makeIframe("about:blank", 0);
+        TestRunner._makeIframe("about:blank", 0, Date.now());
         if (TestRunner.logEnabled) {
             TestRunner.logger.log("Passed: " + $("pass-count").innerHTML);
             TestRunner.logger.log("Failed: " + $("fail-count").innerHTML);
