23afbb36f1ad6b265adebeae3146b83e546adb39
Ted Mielczarek <ted.mielczarek@gmail.com>
2008-07-27 13:06:58 -0400

    bug 417516 - add top level Makefile targets to run test suites. add targets for mochitest. r=bsmedberg

--- browser/build.mk
+++ browser/build.mk
@@ -59,3 +59,14 @@ package:
 
 install::
 	@$(MAKE) -C browser/installer install
+
+ifdef ENABLE_TESTS
+# Implemented in testing/testsuite-targets.mk
+mochitest-browser-chrome:
+	$(RUN_MOCHITEST) --browser-chrome
+	$(CHECK_TEST_ERROR)
+
+mochitest:: mochitest-browser-chrome
+
+.PHONY: mochitest-browser-chrome
+endif
