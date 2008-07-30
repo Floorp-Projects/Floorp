14999cbbb13f450cfe02a6ee8337b34eebd180af
Xiph contributors (see /modules/libfishsound/AUTHORS), plus Chris Double <chris.double@double.co.nz>
2008-07-29 23:43:24 -0700

    Bug 422538. Import libfishsound

---
+++ modules/libfishsound/src/libfishsound/fs_compat.h
@@ -0,0 +1,57 @@
+/*
+   Copyright (C) 2003 Commonwealth Scientific and Industrial Research
+   Organisation (CSIRO) Australia
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+   - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+   - Neither the name of CSIRO Australia nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+   PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE ORGANISATION OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "config.h"
+
+/* WINDOWS */
+#ifdef _WIN32
+#define inline __inline
+#define alloca _alloca
+#define strncasecmp _strnicmp
+#define snprintf _snprintf
+#ifndef __SYMBIAN32__
+#define strcasecmp _stricmp
+#endif /* ! __SYMBIAN32__ */
+#endif
+
+/* malloc/realloc/free macros */
+#ifndef fs_malloc
+#define fs_malloc malloc
+#endif
+
+#ifndef fs_realloc
+#define fs_realloc realloc
+#endif
+
+#ifndef fs_free
+#define fs_free free
+#endif
