095c183293237cea4fdb52f0ec62485331123381
Ere Maijala <emaijala@kolumbus.fi>
2008-08-01 10:32:32 -0700

    Patch for bug 112294: calling an alert box from window.onblur triggers the event multiple times r=smaug sr=roc

--- widget/src/windows/nsWindow.cpp
+++ widget/src/windows/nsWindow.cpp
@@ -1608,9 +1608,14 @@ PRBool nsWindow::CanTakeFocus()
 
 NS_METHOD nsWindow::Show(PRBool bState)
 {
+  PRBool wasVisible = mIsVisible;
+  // Set the status now so that anyone asking during ShowWindow or
+  // SetWindowPos would get the correct answer.
+  mIsVisible = bState;
+
   if (mWnd) {
     if (bState) {
-      if (!mIsVisible && mWindowType == eWindowType_toplevel) {
+      if (!wasVisible && mWindowType == eWindowType_toplevel) {
         switch (mSizeMode) {
           case nsSizeMode_Maximized :
             ::ShowWindow(mWnd, SW_SHOWMAXIMIZED);
@@ -1638,7 +1643,7 @@ NS_METHOD nsWindow::Show(PRBool bState)
         }
       } else {
         DWORD flags = SWP_NOSIZE | SWP_NOMOVE | SWP_SHOWWINDOW;
-        if (mIsVisible)
+        if (wasVisible)
           flags |= SWP_NOZORDER;
 
         if (mWindowType == eWindowType_popup) {
@@ -1668,11 +1673,9 @@ NS_METHOD nsWindow::Show(PRBool bState)
   }
   
 #ifdef MOZ_XUL
-  if (!mIsVisible && bState)
+  if (!wasVisible && bState)
     Invalidate(PR_FALSE);
 #endif
-
-  mIsVisible = bState;
 
   return NS_OK;
 }
