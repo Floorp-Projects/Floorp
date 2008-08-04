894a8e40bd1738c87522b2ab4600b7ffe783cfa6
Shawn Wilsher <sdwilsh@shawnwilsher.com>
2008-08-04 13:14:17 -0400

    Bug 443433 - [refactor] move places table creation statements to one file This refactors how tables are created in places.  There is no change in logic. r=dietrich

--- toolkit/components/places/src/nsNavBookmarks.cpp
+++ toolkit/components/places/src/nsNavBookmarks.cpp
@@ -51,6 +51,7 @@
 #include "prprf.h"
 #include "nsILivemarkService.h"
 #include "nsPlacesTriggers.h"
+#include "nsPlacesTables.h"
 
 const PRInt32 nsNavBookmarks::kFindBookmarksIndex_ID = 0;
 const PRInt32 nsNavBookmarks::kFindBookmarksIndex_Type = 1;
@@ -327,19 +328,7 @@ nsNavBookmarks::InitTables(mozIStorageConnection* aDBConn)
   nsresult rv = aDBConn->TableExists(NS_LITERAL_CSTRING("moz_bookmarks"), &exists);
   NS_ENSURE_SUCCESS(rv, rv);
   if (! exists) {
-    // The fk column is for "foreign key". It contains ids from moz_places
-    // if the row is a bookmark.
-    rv = aDBConn->ExecuteSimpleSQL(NS_LITERAL_CSTRING("CREATE TABLE moz_bookmarks ("
-        "id INTEGER PRIMARY KEY,"
-        "type INTEGER, "
-        "fk INTEGER DEFAULT NULL, "
-        "parent INTEGER, "
-        "position INTEGER, "
-        "title LONGVARCHAR, "
-        "keyword_id INTEGER, "
-        "folder_type TEXT, "
-        "dateAdded INTEGER, " 
-        "lastModified INTEGER)"));
+    rv = aDBConn->ExecuteSimpleSQL(CREATE_MOZ_BOOKMARKS);
     NS_ENSURE_SUCCESS(rv, rv);
 
     // This index will make it faster to determine if a given item is
@@ -368,9 +357,7 @@ nsNavBookmarks::InitTables(mozIStorageConnection* aDBConn)
   rv = aDBConn->TableExists(NS_LITERAL_CSTRING("moz_bookmarks_roots"), &exists);
   NS_ENSURE_SUCCESS(rv, rv);
   if (!exists) {
-    rv = aDBConn->ExecuteSimpleSQL(NS_LITERAL_CSTRING("CREATE TABLE moz_bookmarks_roots ("
-        "root_name VARCHAR(16) UNIQUE, "
-        "folder_id INTEGER)"));
+    rv = aDBConn->ExecuteSimpleSQL(CREATE_MOZ_BOOKMARKS_ROOTS);
     NS_ENSURE_SUCCESS(rv, rv);
   }
 
@@ -378,10 +365,7 @@ nsNavBookmarks::InitTables(mozIStorageConnection* aDBConn)
   rv = aDBConn->TableExists(NS_LITERAL_CSTRING("moz_keywords"), &exists);
   NS_ENSURE_SUCCESS(rv, rv);
   if (! exists) {
-    rv = aDBConn->ExecuteSimpleSQL(NS_LITERAL_CSTRING(
-        "CREATE TABLE moz_keywords ("
-        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-        "keyword TEXT UNIQUE)"));
+    rv = aDBConn->ExecuteSimpleSQL(CREATE_MOZ_KEYWORDS);
     NS_ENSURE_SUCCESS(rv, rv);
 
     // Create trigger to update as well
