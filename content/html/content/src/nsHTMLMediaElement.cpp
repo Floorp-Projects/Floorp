41d4ed929486b5cedd8f08ce6ca8e21952e0753d
Chris Double <chris.double@double.co.nz>
2008-08-04 13:51:01 +1200

    Bug 448534. Make sure that the video stops when the element is removed from the document. r+sr=roc

--- content/html/content/src/nsHTMLMediaElement.cpp
+++ content/html/content/src/nsHTMLMediaElement.cpp
@@ -515,6 +515,9 @@ nsresult nsHTMLMediaElement::BindToTree(nsIDocument* aDocument, nsIContent* aPar
 void nsHTMLMediaElement::UnbindFromTree(PRBool aDeep,
                                         PRBool aNullParent)
 {
+  if (!mPaused && mNetworkState != nsIDOMHTMLMediaElement::EMPTY)
+    Pause();
+
   nsGenericHTMLElement::UnbindFromTree(aDeep, aNullParent);
 }
 
