cbe4d658d2ae4a60ec1fdf17a2ed82c3634ffd41
Neil Deakin <neil@mozilla.com>
2008-08-06 10:32:09 -0400

    Bug 437640, ability to disable and remove <keyset> elements, r+sr=neil

--- content/xul/document/src/nsXULDocument.cpp
+++ content/xul/document/src/nsXULDocument.cpp
@@ -1731,6 +1731,14 @@ nsXULDocument::RemoveSubtreeFromDocument(nsIContent* aElement)
     // document.
     nsresult rv;
 
+    if (aElement->NodeInfo()->Equals(nsGkAtoms::keyset, kNameSpaceID_XUL)) {
+        nsCOMPtr<nsIXBLService> xblService(do_GetService("@mozilla.org/xbl;1"));
+        if (xblService) {
+            nsCOMPtr<nsPIDOMEventTarget> piTarget(do_QueryInterface(aElement));
+            xblService->DetachGlobalKeyHandler(piTarget);
+        }
+    }
+
     // 1. Remove any children from the document.
     PRUint32 count = aElement->GetChildCount();
 
