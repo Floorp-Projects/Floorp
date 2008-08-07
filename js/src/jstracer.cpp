0305a8e41deddb49cc305898a91c21705f0860ab
Andreas Gal <gal@mozilla.com>
2008-08-07 16:27:26 -0700

    Fix typo (missing memcmp in statement).

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -555,7 +555,7 @@ bool
 TypeMap::matches(TypeMap& other)
 {
     JS_ASSERT(length() == other.length()); /* this should be called with compatible maps only */
-    return !(data(), other.data(), length());
+    return !memcmp(data(), other.data(), length());
 }
 
 TraceRecorder::TraceRecorder(JSContext* cx, GuardRecord* _anchor,
