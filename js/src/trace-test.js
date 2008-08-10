c9c5866973a99df38cf9149c49a84dc86d7a83a2
Brendan Eich <brendan@mozilla.org>
2008-08-10 15:21:14 -0700

    joinTest.

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -572,6 +572,23 @@ function typeofTest()
 typeofTest.expected = "string,string,string,object,number,number,boolean,undefined,object,function,object,object";
 test(typeofTest);
 
+function joinTest()
+{
+  var s = "";
+  var a = [];
+  for (var i = 0; i < 8; i++)
+    a[i] = [String.fromCharCode(97 + i)];
+  for (i = 0; i < 8; i++) {
+    for (var j = 0; j < 8; j++)
+      a[i][1 + j] = j;
+  }
+  for (i = 0; i < 8; i++)
+    s += a[i].join(",");
+  return s;
+}
+joinTest.expected = "a,0,1,2,3,4,5,6,7b,0,1,2,3,4,5,6,7c,0,1,2,3,4,5,6,7d,0,1,2,3,4,5,6,7e,0,1,2,3,4,5,6,7f,0,1,2,3,4,5,6,7g,0,1,2,3,4,5,6,7h,0,1,2,3,4,5,6,7";
+test(joinTest);
+
 /* Keep these at the end so that we can see the summary after the trace-debug spew. */
 print("\npassed:", passes.length && passes.join(","));
 print("\nFAILED:", fails.length && fails.join(","));
