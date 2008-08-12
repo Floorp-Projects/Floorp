6bf0af5166c0edcc7c0bee5bb1917bf3bf4f2505
David Anderson <danderson@mozilla.com>
2008-08-11 18:20:10 -0700

    Fixed some explicit deallocation bugs that cropped up running in the browser.

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -481,14 +481,14 @@ js_DestroyContext(JSContext *cx, JSDestroyContextMode mode)
         JS_free(cx, lrs);
     }
 
+#ifdef JS_TRACER
+    js_DestroyJIT(cx);
+#endif
+
 #ifdef JS_THREADSAFE
     js_ClearContextThread(cx);
 #endif
 
-#ifdef JS_TRACER
-    js_DestroyJIT(cx);
-#endif
-    
     /* Finally, free cx itself. */
     free(cx);
 }
