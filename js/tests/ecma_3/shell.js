e1ada444d98e32bc5e57ac344e3f336eb9cbce31
Bob Clary <bclary@bclary.com>
2008-08-13 05:17:23 -0700

    JavaScript Tests - clean up contributors on boilerplate shell.js files, no bug

--- js/tests/ecma_3/shell.js
+++ js/tests/ecma_3/shell.js
@@ -22,7 +22,6 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *   Rob Ginda rginda@netscape.com
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
