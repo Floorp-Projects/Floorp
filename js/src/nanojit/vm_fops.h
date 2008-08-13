2d0a7b3dffe0e3dccb1daf723488e779638410c5
Brendan Eich <brendan@mozilla.org>
2008-08-12 18:52:28 -0700

    - Add String match and three replace overloadings, and allow known native matching to continue in search of exact match (not best, and not abort on first mismatch). - Add CallGetter built-in and use it for regexp class-getter-implemented prototype properties. - Add BUILTIN5 support (this should be "it" ;-).

--- js/src/nanojit/vm_fops.h
+++ js/src/nanojit/vm_fops.h
@@ -39,6 +39,7 @@
 #define BUILTIN2(op, at0, at1, atr, tr, t0, t1, cse, fold)          F_##op,
 #define BUILTIN3(op, at0, at1, at2, atr, tr, t0, t1, t2, cse, fold) F_##op,
 #define BUILTIN4(op, at0, at1, at2, at3, atr, tr, t0, t1, t2, t3, cse, fold) F_##op,
+#define BUILTIN5(op, at0, at1, at2, at3, at4, atr, tr, t0, t1, t2, t3, t4, cse, fold) F_##op,
 
 INTERP_FOPCODE_LIST_BEGIN
 #include "builtins.tbl"
@@ -48,3 +49,4 @@ INTERP_FOPCODE_LIST_END
 #undef BUILTIN2
 #undef BUILTIN3
 #undef BUILTIN4
+#undef BUILTIN5
