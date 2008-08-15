61f98fcc5f3a521f85b174a1a568eee09cba63a5
Paul O'Shannessy <paul@oshannessy.com>
2008-08-15 15:41:22 -0700

    Bug 288040 – convert signons3.txt to mozstorage. r=dolske, r=sdwilsh
    --HG--
    rename : storage/test/unit/corruptDB.sqlite => toolkit/components/passwordmgr/test/unit/data/corruptDB.sqlite

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -2611,7 +2611,7 @@ pref("signon.rememberSignons",              true);
 pref("signon.expireMasterPassword",         false);
 pref("signon.SignonFileName",               "signons.txt"); // obsolete 
 pref("signon.SignonFileName2",              "signons2.txt"); // obsolete
-pref("signon.SignonFileName3",              "signons3.txt");
+pref("signon.SignonFileName3",              "signons3.txt"); // obsolete
 pref("signon.autofillForms",                true); 
 pref("signon.debug",                        false); // logs to Error Console
 
