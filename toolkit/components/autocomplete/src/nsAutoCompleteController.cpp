28c7a082520d2258a864bacdcae3c415a5f3dd0a
Mark Banner <bugzilla@standard8.plus.com>
2008-08-24 19:37:40 +0100

    Bug 438861 Autocomplete does not pass back previous results to a second search. r=enndeakin

--- toolkit/components/autocomplete/src/nsAutoCompleteController.cpp
+++ toolkit/components/autocomplete/src/nsAutoCompleteController.cpp
@@ -972,6 +972,13 @@ nsAutoCompleteController::StartSearch()
   mSearchStatus = nsIAutoCompleteController::STATUS_SEARCHING;
   mDefaultIndexCompleted = PR_FALSE;
 
+  // Cache the current results so that we can pass these through to all the
+  // searches without loosing them
+  nsresult rv;
+  nsCOMPtr<nsISupportsArray> resultCache;
+  rv = mResults->Clone(getter_AddRefs(resultCache));
+  NS_ENSURE_SUCCESS(rv, rv);
+
   PRUint32 count;
   mSearches->Count(&count);
   mSearchesOngoing = count;
@@ -985,7 +992,7 @@ nsAutoCompleteController::StartSearch()
     nsCOMPtr<nsIAutoCompleteSearch> search;
     mSearches->GetElementAt(i, getter_AddRefs(search));
     nsCOMPtr<nsIAutoCompleteResult> result;
-    mResults->GetElementAt(i, getter_AddRefs(result));
+    resultCache->GetElementAt(i, getter_AddRefs(result));
 
     if (result) {
       PRUint16 searchResult;
@@ -996,7 +1003,7 @@ nsAutoCompleteController::StartSearch()
     }
 
     nsAutoString searchParam;
-    nsresult rv = input->GetSearchParam(searchParam);
+    rv = input->GetSearchParam(searchParam);
     if (NS_FAILED(rv))
         return rv;
 
