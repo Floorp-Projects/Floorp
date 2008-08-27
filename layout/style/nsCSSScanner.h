176db77f5d33cecc53c0a887343b800df82f9c41
Benjamin Smedberg <benjamin@smedbergs.us>
2008-08-27 10:58:50 -0400

    Bug 445570 - Mark CSSToken::mIdent heap autostring as we-know-what-we're-doing, r=dbaron

--- layout/style/nsCSSScanner.h
+++ layout/style/nsCSSScanner.h
@@ -107,7 +107,7 @@ struct nsCSSToken {
   nsCSSTokenType  mType;
   PRPackedBool    mIntegerValid; // for number and dimension
   PRPackedBool    mHasSign; // for number, percentage, and dimension
-  nsAutoString    mIdent;
+  nsAutoString    mIdent NS_OKONHEAP;
   float           mNumber;
   PRInt32         mInteger;
   PRUnichar       mSymbol;
