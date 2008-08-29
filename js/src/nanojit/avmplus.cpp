d0655265aa7d72a5f03db48c0c140017eca9c000
Jesse Ruderman <jruderman@gmail.com>
2008-08-28 19:07:49 -0700

    Turn NANO_DIE macro into NanoAssertFail function so that it shows up in stack traces.  Having it appear in stack traces makes it clear the exit was due to an assertion failure rather than a real crash.  Followup to bug 452674.

--- js/src/nanojit/avmplus.cpp
+++ js/src/nanojit/avmplus.cpp
@@ -41,3 +41,17 @@ GCHeap GC::heap;
 String* AvmCore::k_str[] = { (String*)"" };
 bool AvmCore::sse2_available = false;
 
+#ifdef _DEBUG
+// NanoAssertFail matches JS_Assert in jsutil.cpp.
+void NanoAssertFail()
+{
+    #if defined(WIN32)
+        DebugBreak();
+        exit(3);
+    #elif defined(XP_OS2) || (defined(__GNUC__) && defined(__i386))
+        asm("int $3");
+    #endif
+
+    abort();
+}
+#endif
