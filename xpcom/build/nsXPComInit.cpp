913bd144bd5a2b71e6f0a70f6158ee4b234c155b
Doug Turner <dougt@meer.net>
2008-09-03 16:06:23 -0700

    Bug 452489 - clean up nsMemoryImpl for OOM issues. r=bsmedberg

--- xpcom/build/nsXPComInit.cpp
+++ xpcom/build/nsXPComInit.cpp
@@ -660,9 +660,6 @@ NS_InitXPCOM3(nsIServiceManager* *result,
     // to the directory service.
     nsDirectoryService::gService->RegisterCategoryProviders();
 
-    // Initialize memory flusher
-    nsMemoryImpl::InitFlusher();
-
     // Notify observers of xpcom autoregistration start
     NS_CreateServicesFromCategory(NS_XPCOM_STARTUP_CATEGORY, 
                                   nsnull,
