622235899f0f13f674163638e5217e6fca980968
Wan-Teh Chang <wtc@google.com>
2008-09-05 11:03:20 -0700

    Bug 453372 - Import NSPR_4_7_2_BETA3.  r=kaie.

--- nsprpub/lib/libc/src/strcase.c
+++ nsprpub/lib/libc/src/strcase.c
@@ -36,6 +36,7 @@
  * ***** END LICENSE BLOCK ***** */
 
 #include "plstr.h"
+#include <string.h>
 
 static const unsigned char uc[] =
 {
--- nsprpub/pr/include/md/_beos.h
+++ nsprpub/pr/include/md/_beos.h
@@ -56,6 +56,7 @@
 
 #ifdef BONE_VERSION
 #define _PR_HAVE_SOCKADDR_LEN
+#define HAVE_NETINET_TCP_H
 #endif
 
 #define PR_LINKER_ARCH	"beos"
