a4a9bd57317318a28f6d838a7acd92571ef43ed2
Wan-Teh Chang <wtc@google.com>
2008-09-05 11:03:20 -0700

    Bug 453372 - Import NSPR_4_7_2_BETA3.  r=kaie.

--- nsprpub/pr/src/io/prmapopt.c
+++ nsprpub/pr/src/io/prmapopt.c
@@ -70,8 +70,7 @@
 #include <netinet/in_systm.h>  /* n_short, n_long, n_time */
 #endif
 
-#if (defined(XP_UNIX) && !defined(SYMBIAN)) \
-    || defined(OS2) || (defined(XP_BEOS) && defined(BONE_VERSION))
+#ifdef HAVE_NETINET_TCP_H
 #include <netinet/tcp.h>  /* TCP_NODELAY, TCP_MAXSEG */
 #endif
 
