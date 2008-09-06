04f1838be42b85dc5a6c785ef80518f669e39cc1
Serge Gautherie <sgautherie.bz@free.fr>
2008-09-06 21:12:09 +0200

    Bug 452797 - Clean up unused variables in layout/; (debug) bustage fix; noticed by Jesse on irc.

--- layout/generic/nsContainerFrame.cpp
+++ layout/generic/nsContainerFrame.cpp
@@ -1099,7 +1099,11 @@ void
 nsContainerFrame::DeleteNextInFlowChild(nsPresContext* aPresContext,
                                         nsIFrame*      aNextInFlow)
 {
-  NS_PRECONDITION(aNextInFlow->GetPrevInFlow(), "bad prev-in-flow");
+#ifdef DEBUG
+  nsIFrame* prevInFlow = aNextInFlow->GetPrevInFlow();
+#endif
+
+  NS_PRECONDITION(prevInFlow, "bad prev-in-flow");
 
   // If the next-in-flow has a next-in-flow then delete it, too (and
   // delete it first).
