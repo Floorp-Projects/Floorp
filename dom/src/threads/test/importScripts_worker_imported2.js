8036cf5436c65fa21942e497570a486bf90bacc3
Ben Turner <bent.mozilla@gmail.com>
2008-09-08 00:08:24 -0700

    Bug 450449 - " Implement 'importScripts' for worker threads". r=jst+mrbkap, sr=jst.

---
+++ dom/src/threads/test/importScripts_worker_imported2.js
@@ -0,0 +1,7 @@
+// This caused security exceptions in the past, make sure it doesn't!
+var myConstructor2 = {}.constructor;
+
+// Try to call a function defined in the imported script.
+function importedScriptFunction2() {
+  dump("running importedScriptFunction2\n");
+}
