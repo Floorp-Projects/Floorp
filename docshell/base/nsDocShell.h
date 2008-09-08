1dcb882cbc86187cae5fc8d43a1cb8d62473a7ce
Boris Zbarsky <bzbarsky@mit.edu>
2008-09-08 09:19:49 -0400

    Bug 436990.  Make sure to remove already-fired timers from our refresh URI list.  r+sr=jst

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -109,6 +109,7 @@
 #include "nsIChannelClassifier.h"
 
 class nsIScrollableView;
+class nsDocShell;
 
 /* load commands were moved to nsIDocShell.h */
 /* load types were moved to nsDocShellLoadTypes.h */
@@ -133,7 +134,7 @@ public:
 
     PRInt32 GetDelay() { return mDelay ;}
 
-    nsCOMPtr<nsIDocShell> mDocShell;
+    nsRefPtr<nsDocShell>  mDocShell;
     nsCOMPtr<nsIURI>      mURI;
     PRInt32               mDelay;
     PRPackedBool          mRepeat;
@@ -234,6 +235,13 @@ public:
     // This method swaps out the content viewer and simulates loads for
     // subframes.  It then simulates the completion of the toplevel load.
     nsresult RestoreFromHistory();
+
+    // Perform a URI load from a refresh timer.  This is just like the
+    // ForceRefreshURI method on nsIRefreshURI, but makes sure to take
+    // the timer involved out of mRefreshURIList if it's there.
+    // aTimer must not be null.
+    nsresult ForceRefreshURIFromTimer(nsIURI * aURI, PRInt32 aDelay,
+                                      PRBool aMetaRefresh, nsITimer* aTimer);
 
 protected:
     // Object Management
