998fccbd652dd9239f02cf61e4a5116ac129ecac
Bobby Holley <bholley@mozilla.com>
2008-09-08 14:47:26 -0700

    Force cms to use sRGB as an output profile during reftests - bug 452125.r=vlad

--- layout/tools/reftest/reftest.js
+++ layout/tools/reftest/reftest.js
@@ -121,6 +121,11 @@ function OnRefTestLoad()
 
 function OnRefTestUnload()
 {
+    /* Clear the sRGB forcing pref to leave the profile as we found it. */
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"].
+                getService(Components.interfaces.nsIPrefBranch2);
+    prefs.clearUserPref("gfx.color_management.force_srgb");
+
     gBrowser.removeEventListener("load", OnDocumentLoad, true);
 }
 
