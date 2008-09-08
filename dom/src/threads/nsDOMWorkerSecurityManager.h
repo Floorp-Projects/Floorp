68517dddae7629ea25541713b8124b34d7566b2a
Ben Turner <bent.mozilla@gmail.com>
2008-09-08 10:48:14 -0700

    Bug 450449 - " Implement 'importScripts' for worker threads". r=jst+mrbkap, sr=jst.

--- dom/src/threads/nsDOMWorkerSecurityManager.h
+++ dom/src/threads/nsDOMWorkerSecurityManager.h
@@ -40,12 +40,17 @@
 #define __NSDOMWORKERSECURITYMANAGER_H__
 
 #include "nsIXPCSecurityManager.h"
+#include "jsapi.h"
 
 class nsDOMWorkerSecurityManager : public nsIXPCSecurityManager
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIXPCSECURITYMANAGER
+
+  static JSBool JSCheckAccess(JSContext *cx, JSObject *obj, jsval id,
+                              JSAccessMode mode, jsval *vp);
+
 };
 
 #endif /* __NSDOMWORKERSECURITYMANAGER_H__ */
