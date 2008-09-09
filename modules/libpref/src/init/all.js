4fb9e4449c8aaa87ab0a43b375cf8f193cf31ea4
Phil Ringnalda <philringnalda@gmail.com>
2008-09-08 21:42:16 -0700

    Bug 454297 - move external viewsource prefs to all.js so viewSource doesn't throw from the console in non-Fx apps, r=gavin

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -196,6 +196,11 @@ pref("toolkit.scrollbox.clickToScroll.scrollDelay", 150);
 // view source
 pref("view_source.syntax_highlight", true);
 pref("view_source.wrap_long_lines", false);
+pref("view_source.editor.external", false);
+pref("view_source.editor.path", "");
+// allows to add further arguments to the editor; use the %LINE% placeholder
+// for jumping to a specific line (e.g. "/line:%LINE%" or "--goto %LINE%")
+pref("view_source.editor.args", "");
 
 // dispatch left clicks only to content in browser (still allows clicks to chrome/xul)
 pref("nglayout.events.dispatchLeftClickOnly", true);
