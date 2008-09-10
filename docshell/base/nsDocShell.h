41bc46d3808df8f1ef4106a85f41f42cba1fad73
Andrew Schultz <ajschult@verizon.net>
2008-09-09 21:39:38 -0400

    Bug 396519.  Encapsulate content viewer eviction in session history.  r+sr=bzbarsky

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -648,11 +648,6 @@ protected:
     // hash of session storages, keyed by domain
     nsInterfaceHashtable<nsCStringHashKey, nsIDOMStorage> mStorages;
 
-    // Index into the SHTransaction list, indicating the previous and current
-    // transaction at the time that this DocShell begins to load
-    PRInt32                    mPreviousTransIndex;
-    PRInt32                    mLoadedTransIndex;
-
     // Editor data, if this document is designMode or contentEditable.
     nsAutoPtr<nsDocShellEditorData> mEditorData;
 
