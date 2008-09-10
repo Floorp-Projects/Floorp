ad493cbfd69b44dcb7ce994729de574d02a61e13
Jesse Ruderman <jruderman@gmail.com>
2008-09-10 16:23:00 -0700

    Fix a regression from my patch for bug 445999 in the self-assignment case.

--- xpcom/glue/nsVoidArray.cpp
+++ xpcom/glue/nsVoidArray.cpp
@@ -732,6 +732,11 @@ nsStringArray::~nsStringArray(void)
 nsStringArray& 
 nsStringArray::operator=(const nsStringArray& other)
 {
+  if (this == &other)
+  {
+    return *this;
+  }
+
   // Free our strings
   Clear();
   
@@ -985,6 +990,11 @@ nsCStringArray::~nsCStringArray(void)
 nsCStringArray& 
 nsCStringArray::operator=(const nsCStringArray& other)
 {
+  if (this == &other)
+  {
+    return *this;
+  }
+
   // Free our strings
   Clear();
   
