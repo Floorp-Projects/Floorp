eebdfea92a6a046b599a5ee52c7fa4281f131e15
Brad Lassey <blassey@mozilla.com>
2008-09-10 12:12:58 -0400

    Bug 406837 -  clicking on text input box or address bar doesn't raise virtual keyboard on n800 r=roc sr=roc

--- widget/src/gtk2/nsWindow.cpp
+++ widget/src/gtk2/nsWindow.cpp
@@ -38,6 +38,11 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+#ifdef MOZ_PLATFORM_HILDON
+#define MAEMO_CHANGES
+#include <gtk/gtkimcontext.h>
+#endif
+
 #include "prlink.h"
 
 #include "nsWindow.h"
@@ -6114,6 +6119,13 @@ nsWindow::SetIMEEnabled(PRUint32 aState)
         // Even when aState is not PR_TRUE, we need to set IME focus.
         // Because some IMs are updating the status bar of them in this time.
         focusedWin->IMESetFocus();
+#ifdef MOZ_PLATFORM_HILDON
+        if (mIMEData->mEnabled)
+            hildon_gtk_im_context_show (focusedIm);
+        else
+            hildon_gtk_im_context_hide (focusedIm);
+#endif
+        
     } else {
         if (IsIMEEditableState(mIMEData->mEnabled))
             ResetInputState();
