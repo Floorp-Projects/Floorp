373e9cbd8714ec98c4236f1001821a6e3a91062a
Boris Zbarsky <bzbarsky@mit.edu>
2008-09-10 23:22:20 -0400

    Bug 454317.  Improve GetClasses to not do virtual calls when not needed.  r=smaug, sr=jst

--- content/xtf/src/nsXTFElementWrapper.cpp
+++ content/xtf/src/nsXTFElementWrapper.cpp
@@ -73,6 +73,8 @@ nsXTFElementWrapper::nsXTFElementWrapper(nsINodeInfo* aNodeInfo,
       mTmpAttrName(nsGkAtoms::_asterix) // XXX this is a hack, but names
                                             // have to have a value
 {
+  // We never know when we might have a class
+  SetFlags(NODE_MAY_HAVE_CLASS);
 }
 
 nsXTFElementWrapper::~nsXTFElementWrapper()
@@ -931,7 +933,7 @@ nsXTFElementWrapper::GetClassAttributeName() const
 }
 
 const nsAttrValue*
-nsXTFElementWrapper::GetClasses() const
+nsXTFElementWrapper::DoGetClasses() const
 {
   const nsAttrValue* val = nsnull;
   nsIAtom* clazzAttr = GetClassAttributeName();
