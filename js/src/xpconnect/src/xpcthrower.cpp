934f7f310a6dc292f6c0a50e68a419a8c001ddf3
Ben Newman <bnewman@mozilla.com>
2008-09-16 13:47:47 +0200

    Bug 446584 - NodeIterator doesn't forward exception properly; r=(jonas + jst) sr=jst

--- js/src/xpconnect/src/xpcthrower.cpp
+++ js/src/xpconnect/src/xpcthrower.cpp
@@ -259,6 +259,16 @@ XPCThrower::BuildAndThrowException(JSContext* cx, nsresult rv, const char* sz)
         JS_ReportOutOfMemory(cx);
 }
 
+static PRBool
+IsCallerChrome()
+{
+    PRBool isChrome = PR_FALSE;
+    nsCOMPtr<nsIScriptSecurityManager> securityManager =
+        do_GetService(NS_SCRIPTSECURITYMANAGER_CONTRACTID);
+    nsresult rv = securityManager->SubjectPrincipalIsSystem(&isChrome);
+    return NS_SUCCEEDED(rv) && isChrome;
+}
+
 // static
 JSBool
 XPCThrower::ThrowExceptionObject(JSContext* cx, nsIException* e)
@@ -266,8 +276,21 @@ XPCThrower::ThrowExceptionObject(JSContext* cx, nsIException* e)
     JSBool success = JS_FALSE;
     if(e)
     {
-        nsXPConnect* xpc = nsXPConnect::GetXPConnect();
-        if(xpc)
+        nsCOMPtr<nsXPCException> xpcEx;
+        jsval thrown;
+        nsXPConnect* xpc;
+
+        // If we stored the original thrown JS value in the exception
+        // (see XPCConvert::ConstructException) and we are in a web
+        // context (i.e., not chrome), rethrow the original value.
+        if((xpcEx = do_QueryInterface(e)) &&
+           xpcEx->GetThrownJSVal(&thrown) &&
+           !IsCallerChrome())
+        {
+            JS_SetPendingException(cx, thrown);
+            success = JS_TRUE;
+        }
+        else if((xpc = nsXPConnect::GetXPConnect()))
         {
             JSObject* glob = JS_GetScopeChain(cx);
             if(!glob)
