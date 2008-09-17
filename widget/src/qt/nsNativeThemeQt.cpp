4b15e608551723f5e1cd19db1489d9e557362d40
Oleg Romashin <romaxa@gmail.com>
2008-09-17 17:40:06 +0300

    Bug 454269 -  Popup menus drawn without borders. r=vladimir.

--- widget/src/qt/nsNativeThemeQt.cpp
+++ widget/src/qt/nsNativeThemeQt.cpp
@@ -260,6 +260,12 @@ nsNativeThemeQt::DrawWidgetBackground(nsIRenderingContext* aContext,
         style->drawPrimitive(QStyle::PE_FrameLineEdit, &frameOpt, qPainter, NULL);
         break;
     }
+    case NS_THEME_MENUPOPUP: {
+        QStyleOptionMenuItem option;
+        InitPlainStyle(aWidgetType, aFrame, r, (QStyleOption&)option, extraFlags);
+        style->drawPrimitive(QStyle::PE_FrameMenu, &option, qPainter, NULL);
+        break;
+    }
     default:
         break;
     }
@@ -276,12 +282,17 @@ nsNativeThemeQt::GetWidgetBorder(nsIDeviceContext* aContext,
 {
     (*aResult).top = (*aResult).bottom = (*aResult).left = (*aResult).right = 0;
 
-//     switch(aWidgetType) {
+    QStyle* style = qApp->style();
+    switch(aWidgetType) {
 //     case NS_THEME_TEXTFIELD:
 //     case NS_THEME_LISTBOX:
-//         (*aResult).top = (*aResult).bottom = (*aResult).left = (*aResult).right =
-//                          frameWidth;
-//     }
+    case NS_THEME_MENUPOPUP: {
+       (*aResult).top = (*aResult).bottom = (*aResult).left = (*aResult).right = style->pixelMetric(QStyle::PM_MenuPanelWidth);
+       break;
+    }
+    default:
+        break;
+    }
 
     return NS_OK;
 }
@@ -515,6 +526,7 @@ nsNativeThemeQt::ThemeSupportsWidget(nsPresContext* aPresContext,
     case NS_THEME_TEXTFIELD:
     case NS_THEME_TEXTFIELD_MULTILINE:
     case NS_THEME_LISTBOX:
+    case NS_THEME_MENUPOPUP:
         return !IsWidgetStyled(aPresContext, aFrame, aWidgetType);
     default:
         break;
