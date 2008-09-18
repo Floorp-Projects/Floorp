11e2d475dda6abe59a86750a83781e386f48ce37
Andreas Gal <gal@mozilla.com>
2008-09-18 16:04:54 -0700

    Add support for JSOP_NOT on strings (451787, r=danderson).

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -3970,6 +3970,12 @@ TraceRecorder::record_JSOP_NOT()
         set(&v, lir->ins_eq0(get(&v)));
         return true;
     }
+    if (JSVAL_IS_STRING(v)) {
+        set(&v, lir->ins_eq0(lir->ins2(LIR_piand, 
+                lir->insLoad(LIR_ldp, get(&v), (int)offsetof(JSString, length)),
+                INS_CONSTPTR(JSSTRING_LENGTH_MASK))));
+        return true;
+    }
     return false;
 }
 
