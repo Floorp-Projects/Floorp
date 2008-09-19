fccdef9efc58706ae5c0e4433c5d288824889ef4
Zack Weinberg <zweinberg@mozilla.com>
2008-09-19 19:06:46 +0200

    Bug 455217 - Selection colors for plain text files is wrong (white background, blue text instead of blue background, white text); "code" part; r+sr=dbaron

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -1301,17 +1301,14 @@ nsCSSRendering::PaintBackground(nsPresContext* aPresContext,
     // If the window is intended to be opaque, ensure that we always
     // paint an opaque color for its root element, in case there's no
     // background at all or a partly transparent image.
-    //
-    // The default background color from the prescontext is under user
-    // control, so it might not be opaque either.
     nsIView* rView;
     vm->GetRootView(rView);
     if (!rView->GetParent() &&
         (!rView->HasWidget() ||
          rView->GetWidget()->GetTransparencyMode() == eTransparencyOpaque)) {
-      nscolor backColor =
-        NS_ComposeColors(NS_RGB(255,255,255),
-                         aPresContext->DefaultBackgroundColor());
+      nscolor backColor = aPresContext->DefaultBackgroundColor();
+      NS_ASSERTION(NS_GET_A(backColor) == 255,
+                   "default background color is not opaque");
 
       canvasColor.mBackgroundColor =
         NS_ComposeColors(backColor, canvasColor.mBackgroundColor);
