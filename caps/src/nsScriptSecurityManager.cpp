215880a8a64030378cbb2ec3ee97d96ed1f1db8a
David Bienvenu <bienvenu@nventure.com>
2008-09-21 15:21:07 -0700

    bug 453943, always disable js for mailnews for 3.0 b1, don't load pref, r=bz, sr=dmose

--- caps/src/nsScriptSecurityManager.cpp
+++ caps/src/nsScriptSecurityManager.cpp
@@ -1708,8 +1708,7 @@ nsScriptSecurityManager::CanExecuteScripts(JSContext* cx,
             if (NS_FAILED(rv)) return rv;
             if (appType == nsIDocShell::APP_TYPE_MAIL) 
             {
-                // we are temporarily disabling js in mail for TB 3.0 b1
-                *result = PR_FALSE; // mIsMailJavaScriptEnabled;
+                *result = mIsMailJavaScriptEnabled;
             }
         }
     }
@@ -3786,7 +3785,8 @@ nsScriptSecurityManager::ScriptSecurityPrefChanged()
 
     rv = mSecurityPref->SecurityGetBoolPref(sJSMailEnabledPrefName, &temp);
     // JavaScript in Mail defaults to disabled in failure cases.
-    mIsMailJavaScriptEnabled = NS_SUCCEEDED(rv) && temp;
+    // disable javascript in mailnews for TB 3.0 beta1
+    mIsMailJavaScriptEnabled = PR_FALSE; // NS_SUCCEEDED(rv) && temp;
 
     rv = mSecurityPref->SecurityGetBoolPref(sFileOriginPolicyPrefName, &temp);
     sStrictFileOriginPolicy = NS_SUCCEEDED(rv) && temp;
