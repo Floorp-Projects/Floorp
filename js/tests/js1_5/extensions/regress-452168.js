989e48ce8fca454233c9f290ec3b37d05d1831c5
Bob Clary <bclary@bclary.com>
2008-09-21 12:44:29 -0700

    JavaScript Tests - gczeal should be guarded, bug 456292

--- js/tests/js1_5/extensions/regress-452168.js
+++ js/tests/js1_5/extensions/regress-452168.js
@@ -52,14 +52,20 @@ function test()
   printBugNumber(BUGNUMBER);
   printStatus (summary);
 
-  jit(true);
-  gczeal(2);
+  if (typeof gczeal == 'undefined')
+  {
+      expect = actual = 'Test requires gczeal, skipped.';
+  }
+  else
+  {
+    jit(true);
+    gczeal(2);
 
-  var a, b; gczeal(2); (function() { for (var p in this) { } })();
+    var a, b; gczeal(2); (function() { for (var p in this) { } })();
 
-  gczeal(0);
-  jit(false);
-
+    gczeal(0);
+    jit(false);
+  }
   reportCompare(expect, actual, summary);
 
   exitFunc ('test');
