f495f5c947950e4f70a137812d20736bd4acef86
Neil Deakin <neil@mozilla.com>
2008-09-24 12:14:06 -0400

    Bug 456083, open new windows in tabs

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -236,3 +236,8 @@ pref("gfx.color_management.mode", 0);
 
 // don't allow JS to move and resize existing windows
 pref("dom.disable_window_move_resize", true);
+
+// open in tab preferences
+// 0=default window, 1=current window/tab, 2=new window, 3=new tab in most window
+pref("browser.link.open_external", 3);
+pref("browser.link.open_newwindow", 3);
