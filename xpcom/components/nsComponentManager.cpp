adeb2806b3872a511791e7b713b57fa6cbd53f12
Ben Turner <bent.mozilla@gmail.com>
2008-09-26 16:05:57 -0700

    Backed out changeset e154f70dc6ea

--- xpcom/components/nsComponentManager.cpp
+++ xpcom/components/nsComponentManager.cpp
@@ -578,17 +578,9 @@ static nsresult GetLocationFromDirectoryService(const char* prop,
     if (!directoryService)
         return NS_ERROR_FAILURE;
 
-    nsCOMPtr<nsIFile> directory;
-    nsresult rv = directoryService->Get(prop, NS_GET_IID(nsIFile),
-                                        getter_AddRefs(directory));
-    if (NS_FAILED(rv))
-        return rv;
-
-    rv = directory->Normalize();
-    NS_ENSURE_SUCCESS(rv, rv);
-
-    directory.forget(aDirectory);
-    return NS_OK;
+    return directoryService->Get(prop,
+                                 NS_GET_IID(nsIFile),
+                                 (void**)aDirectory);
 }
 
 
