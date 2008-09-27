071f7e847297c72b1cd719e672bbe93c3bcece3b
Andreas Gal <gal@mozilla.com>
2008-09-26 19:20:52 -0700

    Can't use JSVAL_IS_BOOLEAN on trace since boolean type contains undefined (457351, r=brendan).

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -1703,6 +1703,32 @@ function testINITELEM()
 testINITELEM.expected = 10;
 test(testINITELEM);
 
+function testUndefinedBooleanCmp() 
+{
+    var t = true, f = false, x = [];
+    for (var i = 0; i < 10; ++i) {
+	x[0] = t == undefined;
+	x[1] = t != undefined;
+	x[2] = t === undefined;
+	x[3] = t !== undefined;
+	x[4] = t < undefined;
+	x[5] = t > undefined;
+	x[6] = t <= undefined;
+	x[7] = t >= undefined;
+	x[8] = f == undefined;
+	x[9] = f != undefined;
+	x[10] = f === undefined;
+	x[11] = f !== undefined;
+	x[12] = f < undefined;
+	x[13] = f > undefined;
+	x[14] = f <= undefined;
+	x[15] = f >= undefined;
+    }
+    return x.join(",");
+}
+testUndefinedBooleanCmp.expected = "false,true,false,true,false,false,false,false,false,true,false,true,false,false,false,false";
+test(testUndefinedBooleanCmp);
+
 /* Keep these at the end so that we can see the summary after the trace-debug spew. */
 print("\npassed:", passes.length && passes.join(","));
 print("\nFAILED:", fails.length && fails.join(","));
