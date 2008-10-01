36279e509da349d26f0b361653f1006f9dd744a5
L. David Baron <dbaron@dbaron.org>
2008-10-01 10:36:54 -0700

    Back out bug 114169 (changeset 8110e3bae4bf) and change the test (changeset 8f53fcb8bde5) to todos, to see if it was repsonsible for the regression in bug 458065.

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -2581,9 +2581,7 @@ PresShell::ResizeReflow(nscoord aWidth, nscoord aHeight)
 void
 PresShell::CreateResizeEventTimer ()
 {
-  // if we already have a timer set, ignore this call
-  if (mResizeEventTimer)
-    return;
+  KillResizeEventTimer();
 
   if (mIsDocumentGone)
     return;
@@ -2618,9 +2616,6 @@ PresShell::FireResizeEvent()
 {
   if (mIsDocumentGone)
     return;
-
-  // allow a new timer to be set
-  mResizeEventTimer = nsnull;
 
   //Send resize event from here.
   nsEvent event(PR_TRUE, NS_RESIZE_EVENT);
