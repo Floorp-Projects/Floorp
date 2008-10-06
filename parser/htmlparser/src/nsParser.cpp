d572d83117523d8d2243fa5f3f5755c56fe3b4a4
Blake Kaplan <mrbkap@gmail.com>
2008-10-06 15:19:28 -0700

    Temporarily disable speculative parsing to see its effect on tinderbox crashiness. r+sr=jst

--- parser/htmlparser/src/nsParser.cpp
+++ parser/htmlparser/src/nsParser.cpp
@@ -1538,6 +1538,7 @@ nsParser::DidBuildModel(nsresult anErrorCode)
 void
 nsParser::SpeculativelyParse()
 {
+#if 0 // Disable temporarily to see if this is the cause of the bustage.
   if (mParserContext->mParserCommand == eViewNormal &&
       !mParserContext->mMimeType.EqualsLiteral("text/html")) {
     return;
@@ -1554,6 +1555,7 @@ nsParser::SpeculativelyParse()
   if (NS_FAILED(rv)) {
     mSpeculativeScriptThread = nsnull;
   }
+#endif
 }
 
 /**
