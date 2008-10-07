8d7e2e471e6710e85979ca9771de7e243f7873b2
Boris Zbarsky <bzbarsky@mit.edu>
2008-10-07 14:53:23 -0400

    Bug 458100.  Make sure that we'll also instantiate the plug-in even if the nsPluginDocument has no presshell yet at OnStartRequest time.  Thanks to Evgeny <evg.salmin@gmail.com> for pointing out the problem.  r+sr=jst

--- content/html/document/src/nsPluginDocument.cpp
+++ content/html/document/src/nsPluginDocument.cpp
@@ -71,12 +71,21 @@ public:
   const nsCString& GetType() const { return mMimeType; }
   nsIContent*      GetPluginContent() { return mPluginContent; }
 
+  void SkippedInstantiation() {
+    mWillHandleInstantiation = PR_FALSE;
+  }
+
 protected:
   nsresult CreateSyntheticPluginDocument();
 
   nsCOMPtr<nsIContent>                     mPluginContent;
   nsRefPtr<nsMediaDocumentStreamListener>  mStreamListener;
   nsCString                                mMimeType;
+
+  // Hack to handle the fact that plug-in loading lives in frames and that the
+  // frames may not be around when we need to instantiate.  Once plug-in
+  // loading moves to content, this can all go away.
+  PRBool                                   mWillHandleInstantiation;
 };
 
 class nsPluginStreamListener : public nsMediaDocumentStreamListener
@@ -104,6 +113,7 @@ nsPluginStreamListener::OnStartRequest(nsIRequest* request, nsISupports *ctxt)
   nsIPresShell* shell = mDocument->GetPrimaryShell();
   if (!shell) {
     // Can't instantiate w/o a shell
+    mPluginDoc->SkippedInstantiation();
     return NS_BINDING_ABORTED;
   }
 
@@ -114,12 +124,14 @@ nsPluginStreamListener::OnStartRequest(nsIRequest* request, nsISupports *ctxt)
 
   nsIFrame* frame = shell->GetPrimaryFrameFor(embed);
   if (!frame) {
+    mPluginDoc->SkippedInstantiation();
     return rv;
   }
 
   nsIObjectFrame* objFrame;
   CallQueryInterface(frame, &objFrame);
   if (!objFrame) {
+    mPluginDoc->SkippedInstantiation();
     return NS_ERROR_UNEXPECTED;
   }
 
@@ -138,6 +150,7 @@ nsPluginStreamListener::OnStartRequest(nsIRequest* request, nsISupports *ctxt)
   // bother initializing members to 0.
 
 nsPluginDocument::nsPluginDocument()
+  : mWillHandleInstantiation(PR_TRUE)
 {
 }
 
@@ -320,6 +333,13 @@ nsPluginDocument::Print()
     }
   }
 
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsPluginDocument::GetWillHandleInstantiation(PRBool* aWillHandle)
+{
+  *aWillHandle = mWillHandleInstantiation;
   return NS_OK;
 }
 
