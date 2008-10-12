f6277151d1f91179a9149ba57b8c728623e3bfef
Andreas Gal <gal@mozilla.com>
2008-10-12 15:58:34 -0700

    Use a single instance of the shell to run the quick benchmark.

--- js/src/bench.sh
+++ js/src/bench.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-T="0"
-for i in t/*.js; do 
-	T+="+"
-	T+=`Darwin_OPT.OBJ/js -j -e 'var d = Date.now(); load("'$i'"); print(Date.now() - d);'`
-	 
-done
-echo $T | bc
-
+X="var d = Date.now();";
+for i in t/*.js; do X="$X load(\"$i\");"; done
+X="$X print(Date.now() - d);"
+echo $X | (./Darwin_OPT.OBJ/js -j || ./Linux_All_OPT.OBJ/js -j)
