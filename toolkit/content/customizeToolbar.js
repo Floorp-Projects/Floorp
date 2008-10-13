9e3fc5bc06b782a8170b3a5ed5422a605d1f9675
Markus Stange <mstange@themasta.com>
2008-10-13 14:30:47 +0200

    Backed out changeset 151e51ec625e (bug 398928) because of unit test orange

--- toolkit/content/customizeToolbar.js
+++ toolkit/content/customizeToolbar.js
@@ -56,8 +56,7 @@ function InitWithToolbox(aToolbox)
 {
   gToolbox = aToolbox;
   gToolboxDocument = gToolbox.ownerDocument;
-  gToolbox.customizing = true;
-
+  
   gToolbox.addEventListener("draggesture", onToolbarDragGesture, false);
   gToolbox.addEventListener("dragover", onToolbarDragOver, false);
   gToolbox.addEventListener("dragexit", onToolbarDragExit, false);
@@ -68,7 +67,6 @@ function InitWithToolbox(aToolbox)
 
 function finishToolbarCustomization()
 {
-  gToolbox.customizing = false;
   removeToolboxListeners();
   unwrapToolbarItems();
   persistCurrentSets();
