c6d47a93d5d000d7a0d815206fc014c7325f7a85
Marco Bonardo <mak77@bonardo.net>
2008-10-13 17:58:36 +0200

    Bug 457681 - if ExcludeReadOnlyFolders is false, readonly dynamic containers are still excluded, r=dietrich

--- toolkit/components/places/src/nsNavBookmarks.cpp
+++ toolkit/components/places/src/nsNavBookmarks.cpp
@@ -1470,7 +1470,7 @@ nsNavBookmarks::RemoveFolder(PRInt64 aFolderId)
 
   // If this is a container bookmark, try to notify its service.
   if (folderType.Length() > 0) {
-    // There is a type associated with this folder; it's a livemark.
+    // There is a type associated with this folder.
     nsCOMPtr<nsIDynamicContainer> bmcServ = do_GetService(folderType.get());
     if (bmcServ) {
       rv = bmcServ->OnContainerRemoving(aFolderId);
@@ -2066,6 +2066,8 @@ nsNavBookmarks::QueryFolderChildren(PRInt64 aFolderId,
   PRBool results;
 
   nsCOMPtr<nsNavHistoryQueryOptions> options = do_QueryInterface(aOptions, &rv);
+  NS_ENSURE_SUCCESS(rv, rv);
+
   PRInt32 index = -1;
   while (NS_SUCCEEDED(mDBGetChildren->ExecuteStep(&results)) && results) {
 
@@ -2093,8 +2095,7 @@ nsNavBookmarks::QueryFolderChildren(PRInt64 aFolderId,
         continue;
       }
     } else if (itemType == TYPE_FOLDER || itemType == TYPE_DYNAMIC_CONTAINER) {
-      if (itemType == TYPE_DYNAMIC_CONTAINER ||
-          (itemType == TYPE_FOLDER && options->ExcludeReadOnlyFolders())) {
+      if (options->ExcludeReadOnlyFolders()) {
         // see if it's read only and skip it
         PRBool readOnly = PR_FALSE;
         GetFolderReadonly(id, &readOnly);
