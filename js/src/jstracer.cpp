b8e4cbc561f46bd6a82686fce4a4e3ed323f79ea
Jason Orendorff <jorendorff@mozilla.com>
2008-10-13 14:10:35 -0500

    TM: Remove unreachable "return false" in TraceRecorder::record_JSOP_NOT. No bug, r=gal.

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -4439,13 +4439,11 @@ TraceRecorder::record_JSOP_NOT()
         set(&v, lir->ins_eq0(get(&v)));
         return true;
     }
-    if (JSVAL_IS_STRING(v)) {
-        set(&v, lir->ins_eq0(lir->ins2(LIR_piand, 
-                lir->insLoad(LIR_ldp, get(&v), (int)offsetof(JSString, length)),
-                INS_CONSTPTR(JSSTRING_LENGTH_MASK))));
-        return true;
-    }
-    return false;
+    JS_ASSERT(JSVAL_IS_STRING(v));
+    set(&v, lir->ins_eq0(lir->ins2(LIR_piand, 
+                                   lir->insLoad(LIR_ldp, get(&v), (int)offsetof(JSString, length)),
+                                   INS_CONSTPTR(JSSTRING_LENGTH_MASK))));
+    return true;
 }
 
 bool
