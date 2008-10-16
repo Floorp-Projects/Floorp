8fd5e031699c0c95f44a137c7af200adf794ceaa
Shawn Wilsher <sdwilsh@shawnwilsher.com>
2008-10-16 15:59:33 -0400

    Bug 460315 - set journal_mode to TRUNCATE Sets the journal_mode to the places database to TRUNCATE r=dietrich

--- toolkit/components/places/src/nsNavHistory.cpp
+++ toolkit/components/places/src/nsNavHistory.cpp
@@ -800,6 +800,11 @@ nsNavHistory::InitDB(PRInt16 *aMadeChanges)
     NS_LITERAL_CSTRING("PRAGMA locking_mode = EXCLUSIVE"));
   NS_ENSURE_SUCCESS(rv, rv);
 
+  // We use the TRUNCATE journal mode to reduce the number of fsyncs
+  rv = mDBConn->ExecuteSimpleSQL(
+    NS_LITERAL_CSTRING("PRAGMA journal_mode = TRUNCATE"));
+  NS_ENSURE_SUCCESS(rv, rv);
+
   // moz_places
   if (!tableExists) {
     *aMadeChanges = DB_MIGRATION_CREATED;
