3b78a54441500fd8a80f6393362e389957699ea1
Jim Blandy <jimb@mozilla.org>
2008-10-22 14:52:14 -0700

    Bug 461192: Drop fd_ prefix from math functions. r=jorendorff
    The Mozilla tree used to optionally use its own math library, fdlibm.
    For each standard math function FOO, fdlibm defined its own version
    named fd_FOO.  The SpiderMonkey sources called the fd_FOO functions
    directly.  In configurations where fdlibm was not used, jslibmath.h
    #defined fd_FOO to expand to FOO, resulting in references to the
    standard math library's functions.
    
    Now that fdlibm is not used, even optionally, those fd_ prefixes are
    unnecessary.  However, some code is still needed to choose the right
    copysign function, so jslibmath.h still has a reason to exist.

--- js/src/jslibmath.h
+++ js/src/jslibmath.h
@@ -48,12 +48,6 @@
  * Use system provided math routines.
  */
 
-#define fd_acos acos
-#define fd_asin asin
-#define fd_atan atan
-#define fd_atan2 atan2
-#define fd_ceil ceil
-
 /* The right copysign function is not always named the same thing. */
 #if __GNUC__ >= 4 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
 #define fd_copysign __builtin_copysign
@@ -70,17 +64,6 @@ extern double js_copysign(double, double);
 #else
 #define fd_copysign copysign
 #endif
-
-#define fd_cos cos
-#define fd_exp exp
-#define fd_fabs fabs
-#define fd_floor floor
-#define fd_fmod fmod
-#define fd_log log
-#define fd_pow pow
-#define fd_sin sin
-#define fd_sqrt sqrt
-#define fd_tan tan
 
 #endif /* _LIBMATH_H */
 
