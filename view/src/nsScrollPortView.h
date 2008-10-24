6291661be164a8c0d0a02242601bdc750d13cf5e
Markus Stange <mstange@themasta.com>
2008-10-24 10:38:30 +0200

    Bug 457864 - Make wheel scrolling asynchronous, r+sr=roc

--- view/src/nsScrollPortView.h
+++ view/src/nsScrollPortView.h
@@ -44,7 +44,7 @@
 #include "nsITimer.h"
 
 class nsISupportsArray;
-class SmoothScroll;
+class AsyncScroll;
 
 //this is a class that acts as a container for other views and provides
 //automatic management of scrolling of the views it contains.
@@ -88,15 +88,15 @@ public:
   nsView*     GetScrolledView() const { return GetFirstChild(); }
 
 private:
-  NS_IMETHOD  ScrollToImpl(nscoord aX, nscoord aY, PRUint32 aUpdateFlags);
+  NS_IMETHOD  ScrollToImpl(nscoord aX, nscoord aY);
 
   // data members
-  SmoothScroll* mSmoothScroll;
+  AsyncScroll* mAsyncScroll;
 
   // methods
   void        IncrementalScroll();
   PRBool      IsSmoothScrollingEnabled();
-  static void SmoothScrollAnimationCallback(nsITimer *aTimer, void* aESM);
+  static void AsyncScrollCallback(nsITimer *aTimer, void* aSPV);
 
 protected:
   virtual ~nsScrollPortView();
@@ -106,6 +106,7 @@ protected:
   PRBool CannotBitBlt(nsView* aScrolledView);
 
   nscoord             mOffsetX, mOffsetY;
+  nscoord             mDestinationX, mDestinationY;
   PRUint32            mScrollProperties;
   nscoord             mLineHeight;
   nsISupportsArray   *mListeners;
