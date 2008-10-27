9c76022114b7108133ba2ffcbecd58f8cfc922b3
L. David Baron <dbaron@dbaron.org>
2008-10-27 10:55:51 -0700

    Add nsCSSProps::PropHasFlags to address bzbarsky's review comments.  (Bug 461731)  r+sr=bzbarsky

--- layout/style/nsCSSDeclaration.cpp
+++ layout/style/nsCSSDeclaration.cpp
@@ -237,8 +237,8 @@ PRBool nsCSSDeclaration::AppendValueToString(nsCSSProperty aProperty, nsAString&
           AppendCSSValueToString(aProperty, val->mValue, aResult);
           val = val->mNext;
           if (val) {
-            if (nsCSSProps::kFlagsTable[aProperty] &
-                CSS_PROPERTY_VALUE_LIST_USES_COMMAS)
+            if (nsCSSProps::PropHasFlags(aProperty,
+                                         CSS_PROPERTY_VALUE_LIST_USES_COMMAS))
               aResult.Append(PRUnichar(','));
             aResult.Append(PRUnichar(' '));
           }
@@ -547,7 +547,7 @@ nsCSSDeclaration::GetValue(nsCSSProperty aProperty,
            initialCount = 0, inheritCount = 0;
   CSSPROPS_FOR_SHORTHAND_SUBPROPERTIES(p, aProperty) {
     if (*p == eCSSProperty__x_system_font ||
-         nsCSSProps::kFlagsTable[*p] & CSS_PROPERTY_DIRECTIONAL_SOURCE) {
+         nsCSSProps::PropHasFlags(*p, CSS_PROPERTY_DIRECTIONAL_SOURCE)) {
       // The system-font subproperty and the *-source properties don't count.
       continue;
     }
