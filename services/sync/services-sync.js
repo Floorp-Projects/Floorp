9fbfbe43a02b5bc865ad4c8a77b5a839823e4405
Dan Mills <thunder@mozilla.com>
2008-11-03 14:54:21 -0800

    add max retries config setting that resources support now

--- services/sync/services-sync.js
+++ services/sync/services-sync.js
@@ -38,3 +38,5 @@ pref("extensions.weave.log.logger.service.main", "Trace");
 pref("extensions.weave.xmpp.enabled", false);
 pref("extensions.weave.xmpp.server.url", "https://sm-labs01.mozilla.org:81/xmpp");
 pref("extensions.weave.xmpp.server.realm", "sm-labs01.mozilla.org");
+
+pref("extensions.weave.network.numRetries", 4);
