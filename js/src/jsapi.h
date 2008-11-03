b62dc12989fb0d3cb965e3573e50a72f70c323b2
Blake Kaplan <mrbkap@gmail.com>
2008-11-03 14:28:36 -0800

    Bug 462910 - Add a JS_ValueToSource API. r=brendan

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -338,6 +338,9 @@ JS_ValueToConstructor(JSContext *cx, jsval v);
 extern JS_PUBLIC_API(JSString *)
 JS_ValueToString(JSContext *cx, jsval v);
 
+extern JS_PUBLIC_API(JSString *)
+JS_ValueToSource(JSContext *cx, jsval v);
+
 extern JS_PUBLIC_API(JSBool)
 JS_ValueToNumber(JSContext *cx, jsval v, jsdouble *dp);
 
