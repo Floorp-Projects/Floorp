3769265c5f395433ab92207efe17f940f2351182
Shawn Wilsher <sdwilsh@shawnwilsher.com>
2008-11-03 18:55:15 -0500

    backout of changeset b0dc837c20fa and 73350927f5f1 due to test failures

--- testing/mochitest/tests/SimpleTest/EventUtils.js
+++ testing/mochitest/tests/SimpleTest/EventUtils.js
@@ -211,18 +211,15 @@ function synthesizeMouse(aTarget, aOffsetX, aOffsetY, aEvent, aWindow)
     var modifiers = _parseModifiers(aEvent);
 
     var rect = aTarget.getBoundingClientRect();
-
-    // Need to round, since rect could have non-integer coordinates, and sadly
-    // our event code can't deal with fractional pixel click coords.
-    var left = Math.round(rect.left + aOffsetX);
-    var top = Math.round(rect.top + aOffsetY);
+    var left = rect.left;
+    var top = rect.top;
 
     if (aEvent.type) {
-      utils.sendMouseEvent(aEvent.type, left, top, button, clickCount, modifiers);
+      utils.sendMouseEvent(aEvent.type, left + aOffsetX, top + aOffsetY, button, clickCount, modifiers);
     }
     else {
-      utils.sendMouseEvent("mousedown", left, top, button, clickCount, modifiers);
-      utils.sendMouseEvent("mouseup", left, top, button, clickCount, modifiers);
+      utils.sendMouseEvent("mousedown", left + aOffsetX, top + aOffsetY, button, clickCount, modifiers);
+      utils.sendMouseEvent("mouseup", left + aOffsetX, top + aOffsetY, button, clickCount, modifiers);
     }
   }
 }
