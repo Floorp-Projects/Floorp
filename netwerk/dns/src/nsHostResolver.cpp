fac46d58c0020adb65800a9f14c9583ebf784f1c
Boris Zbarsky <bzbarsky@mit.edu>
2008-11-04 12:03:11 -0500

    Bug 453403.  Fix leak of nsHostResolver by making sure to wake up all the worker threads on shutdown

--- netwerk/dns/src/nsHostResolver.cpp
+++ netwerk/dns/src/nsHostResolver.cpp
@@ -430,7 +430,7 @@ nsHostResolver::Shutdown()
         mPendingCount = 0;
         
         if (mNumIdleThreads)
-            PR_NotifyCondVar(mIdleThreadCV);
+            PR_NotifyAllCondVar(mIdleThreadCV);
         
         // empty host database
         PL_DHashTableEnumerate(&mDB, HostDB_RemoveEntry, nsnull);
