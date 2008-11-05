0829df0d64021627f8d6af28f519553671aba608
Curtis Bartley <cbartley@mozilla.com>
2008-11-04 23:01:33 -0500

    Bug 17612: front end patch to enable view-source link browsing, r=gavin

--- toolkit/components/viewsource/content/viewSource.js
+++ toolkit/components/viewsource/content/viewSource.js
@@ -37,6 +37,8 @@
 #
 # ***** END LICENSE BLOCK *****
 
+const Cc = Components.classes;
+const Ci = Components.interfaces;
 const pageLoaderIface = Components.interfaces.nsIWebPageDescriptor;
 const nsISelectionPrivate = Components.interfaces.nsISelectionPrivate;
 const nsISelectionController = Components.interfaces.nsISelectionController;
@@ -64,10 +66,49 @@ var gSelectionListener = {
   }
 }
 
+var gViewSourceProgressListener = {
+
+  QueryInterface: function (aIID) {
+    if (aIID.equals(Ci.nsIWebProgressListener) ||
+        aIID.equals(Ci.nsISupportsWeakReference))
+      return this;
+    throw Cr.NS_NOINTERFACE;
+  },
+
+  onStateChange: function (aWebProgress, aRequest, aStateFlags, aStatus) {
+  },
+
+  onProgressChange: function (aWebProgress, aRequest,
+                              aCurSelfProgress, aMaxSelfProgress,
+                              aCurTotalProgress, aMaxTotalProgress) {
+  },
+
+  onLocationChange: function (aWebProgress, aRequest, aLocationURI) {
+    UpdateBackForwardCommands(getBrowser().webNavigation);
+  },
+
+  onStatusChange: function (aWebProgress, aRequest, aStatus, aMessage) {
+  },
+
+  onSecurityChange: function (aWebProgress, aRequest, aState) {
+  }
+
+}
+
 function onLoadViewSource() 
 {
   viewSource(window.arguments[0]);
   document.commandDispatcher.focusedWindow = content;
+  
+  // Attach the progress listener.
+  getBrowser().addProgressListener(gViewSourceProgressListener, 
+      Components.interfaces.nsIWebProgress.NOTIFY_ALL);
+}
+
+function onUnloadViewSource() 
+{
+  // Detach the progress listener.
+  getBrowser().removeProgressListener(gViewSourceProgressListener);
 }
 
 function getBrowser()
@@ -97,6 +138,8 @@ function viewSource(url)
 {
   if (!url)
     return false; // throw Components.results.NS_ERROR_FAILURE;
+    
+  var viewSrcUrl = "view-source:" + url;
 
   getBrowser().addEventListener("unload", onUnloadContent, true);
   getBrowser().addEventListener("load", onLoadContent, true);
@@ -170,6 +213,14 @@ function viewSource(url)
           // possible) rather than the network...
           //
           PageLoader.loadPage(arg, pageLoaderIface.DISPLAY_AS_SOURCE);
+
+          // Record the page load in the session history so <back> will work.
+          var shEntry = Cc["@mozilla.org/browser/session-history-entry;1"].createInstance(Ci.nsISHEntry);
+          shEntry.setURI(makeURI(viewSrcUrl, null, null));
+          shEntry.setTitle(viewSrcUrl);
+          shEntry.loadType = Ci.nsIDocShellLoadInfo.loadHistory;
+          getBrowser().webNavigation.sessionHistory.addEntry(shEntry, true);
+
           // The content was successfully loaded from the page cookie.
           loadFromURL = false;
         }
@@ -181,16 +232,11 @@ function viewSource(url)
   }
 
   if (loadFromURL) {
-    // We need to set up session history to give us a page descriptor.
-    //
-    var webNavigation = getBrowser().webNavigation;
-    webNavigation.sessionHistory = Components.classes["@mozilla.org/browser/shistory;1"].createInstance();
     //
     // Currently, an exception is thrown if the URL load fails...
     //
     var loadFlags = Components.interfaces.nsIWebNavigation.LOAD_FLAGS_NONE;
-    var viewSrcUrl = "view-source:" + url;
-    webNavigation.loadURI(viewSrcUrl, loadFlags, null, null, null);
+    getBrowser().webNavigation.loadURI(viewSrcUrl, loadFlags, null, null, null);
   }
 
   //check the view_source.wrap_long_lines pref and set the menuitem's checked attribute accordingly
@@ -378,7 +424,7 @@ function goToLine(line)
   // to position the focusNode and the caret at the beginning of the line.
 
   selection.QueryInterface(nsISelectionPrivate)
-    .interlinePosition = true;	
+    .interlinePosition = true;
 
   selection.addRange(result.range);
 
@@ -629,5 +675,43 @@ function BrowserSetForcedDetector(doReload)
   {
     var PageLoader = getBrowser().webNavigation.QueryInterface(pageLoaderIface);
     PageLoader.loadPage(PageLoader.currentDescriptor, pageLoaderIface.DISPLAY_NORMAL);
+  }
+}
+
+function BrowserForward(aEvent) {
+  try {
+    getBrowser().goForward();
+  }
+  catch(ex) {
+  }
+}
+
+function BrowserBack(aEvent) {
+  try {
+    getBrowser().goBack();
+  }
+  catch(ex) {
+  }
+}
+
+function UpdateBackForwardCommands(aWebNavigation) {
+  var backBroadcaster = document.getElementById("Browser:Back");
+  var forwardBroadcaster = document.getElementById("Browser:Forward");
+
+  var backDisabled = backBroadcaster.hasAttribute("disabled");
+  var forwardDisabled = forwardBroadcaster.hasAttribute("disabled");
+
+  if (backDisabled == aWebNavigation.canGoBack) {
+    if (backDisabled)
+      backBroadcaster.removeAttribute("disabled");
+    else
+      backBroadcaster.setAttribute("disabled", true);
+  }
+
+  if (forwardDisabled == aWebNavigation.canGoForward) {
+    if (forwardDisabled)
+      forwardBroadcaster.removeAttribute("disabled");
+    else
+      forwardBroadcaster.setAttribute("disabled", true);
   }
 }
