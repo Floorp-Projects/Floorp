9e2a943e886f7b5c9ec7ef4e6f728cd8dbfaea96
Edward Lee <edward.lee@engineering.uiuc.edu>
2008-11-10 10:04:36 -0600

    Bug 463661 - Create a pref to control the location bar search sources (history, bookmark, both, none). r=dietrich, a1.9.1b2=beltzner Bug 463535 - Continuing search immediately after changing prefs give wrong results

--- toolkit/components/places/src/nsNavHistory.h
+++ toolkit/components/places/src/nsNavHistory.h
@@ -702,10 +702,21 @@ protected:
     MATCH_BEGINNING
   };
 
+  /**
+   * Determine which sources (if any) of data to search for the autocomplete
+   */
+  enum SearchSource {
+    SEARCH_NONE,
+    SEARCH_HISTORY,
+    SEARCH_BOOKMARK,
+    SEARCH_BOTH
+  };
+
   nsresult InitAutoComplete();
   nsresult CreateAutoCompleteQueries();
   PRBool mAutoCompleteOnlyTyped;
   MatchType mAutoCompleteMatchBehavior;
+  SearchSource mAutoCompleteSearchSources;
   PRBool mAutoCompleteFilterJavascript;
   PRInt32 mAutoCompleteMaxResults;
   nsString mAutoCompleteRestrictHistory;
