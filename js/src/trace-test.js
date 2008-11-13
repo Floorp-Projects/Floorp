9e7d7262e53745713078821abb32fecb1834dbea
Brendan Eich <brendan@mozilla.org>
2008-11-13 00:30:20 -0800

    Bug 456511 - (imacros) TM: Make conversion work on arbitrary JSObjects (r=gal).

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -2323,6 +2323,54 @@ function testStringify() {
 testStringify.expected = "true,true,false,false,undefined,undefined,5,5,5.5,5.5,x,x";
 test(testStringify);
 
+function testObjectToString() {
+    var o = {toString: function()"foo"};
+    var s = "";
+    for (var i = 0; i < 10; i++)
+        s += o;
+    return s;
+}
+testObjectToString.expected = "foofoofoofoofoofoofoofoofoofoo";
+test(testObjectToString);
+
+function testObjectToNumber() {
+    var o = {valueOf: function()-3};
+    var x = 0;
+    for (var i = 0; i < 10; i++)
+        x -= o;
+    return x;
+}
+testObjectToNumber.expected = 30;
+test(testObjectToNumber);
+
+function my_iterator_next() {
+    if (this.i == 10) {
+        this.i = 0;
+        throw this.StopIteration;
+    }
+    return this.i++;
+}
+function testCustomIterator() {
+    var o = {
+        __iterator__: function () {
+            return {
+                i: 0,
+                next: my_iterator_next,
+                StopIteration: StopIteration
+            };
+        }
+    };
+    var a=[];
+    for (var k = 0; k < 100; k += 10) {
+        for(var j in o) {
+            a[k + (j >> 0)] = j*k;
+        }
+    }
+    return a.join();
+}
+testCustomIterator.expected = "0,0,0,0,0,0,0,0,0,0,0,10,20,30,40,50,60,70,80,90,0,20,40,60,80,100,120,140,160,180,0,30,60,90,120,150,180,210,240,270,0,40,80,120,160,200,240,280,320,360,0,50,100,150,200,250,300,350,400,450,0,60,120,180,240,300,360,420,480,540,0,70,140,210,280,350,420,490,560,630,0,80,160,240,320,400,480,560,640,720,0,90,180,270,360,450,540,630,720,810";
+test(testCustomIterator);
+
 /* NOTE: Keep this test last, since it screws up all for...in loops after it. */
 function testGlobalProtoAccess() {
     return "ok";
