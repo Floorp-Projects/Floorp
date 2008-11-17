1748da0666d84596d87e86164a2167c741319486
David Anderson <danderson@mozilla.com>
2008-11-16 17:55:01 -0800

    Fixed deep abort logic when onTrace is set without a recorder (bug 465192, r=gal).

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -2579,10 +2579,16 @@ js_Interpret(JSContext *cx)
         tr = TRACE_RECORDER(cx);
         SET_TRACE_RECORDER(cx, NULL);
         JS_TRACE_MONITOR(cx).onTrace = JS_FALSE;
-        if (tr->wasDeepAborted())
-            tr->removeFragmentoReferences();
-        else
-            tr->pushAbortStack();
+        /*
+         * ON_TRACE means either recording or coming from traced code.
+         * If there's no recorder (the latter case), don't care.
+         */
+        if (tr) {
+            if (tr->wasDeepAborted())
+                tr->removeFragmentoReferences();
+            else
+                tr->pushAbortStack();
+        }
     }
 #endif
 
