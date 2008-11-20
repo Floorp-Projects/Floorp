17e1be243f0b2f5d3fb7d05305303d0167f4cb56
Mike Connor <mconnor@mozilla.com>
2008-11-20 11:53:01 -0500

    Bug 465843.  Remove ctrl-tab preview switching and revert all tabs button to menu, for now.  r=dao, a=beltzner

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1275,7 +1275,9 @@ function delayedStartup(isLoadingBlank, mustLoadSidebar) {
   gBrowser.addEventListener("command", BrowserOnCommand, false);
 
   tabPreviews.init();
+#ifdef USE_TAB_PREVIEWS
   ctrlTab.init();
+#endif
 
   // Initialize the microsummary service by retrieving it, prompting its factory
   // to create its singleton, whose constructor initializes the service.
@@ -1322,8 +1324,9 @@ function delayedStartup(isLoadingBlank, mustLoadSidebar) {
 function BrowserShutdown()
 {
   tabPreviews.uninit();
+#ifdef USE_TAB_PREVIEWS
   ctrlTab.uninit();
-
+#endif
   gGestureSupport.init(false);
 
   try {
