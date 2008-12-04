8ed18b2a3fdf71e304c1d12d0f2f7ed9e4aa9d51
Paul O'Shannessy <paul@oshannessy.com>
2008-12-04 18:41:44 +0100

    Bug 452899 - Don't explicitly create the statement wrapper - storage does it for us; (retry, after bug 457743 was fixed); r=(dolske + sdwilsh)

--- toolkit/components/passwordmgr/src/storage-mozStorage.js
+++ toolkit/components/passwordmgr/src/storage-mozStorage.js
@@ -954,12 +954,7 @@ LoginManagerStorage_mozStorage.prototype = {
         // Memoize the statements
         if (!this._dbStmts[query]) {
             this.log("Creating new statement for query: " + query);
-            let stmt = this._dbConnection.createStatement(query);
-
-            let wrappedStmt = Cc["@mozilla.org/storage/statement-wrapper;1"].
-                              createInstance(Ci.mozIStorageStatementWrapper);
-            wrappedStmt.initialize(stmt);
-            this._dbStmts[query] = wrappedStmt;
+            this._dbStmts[query] = this._dbConnection.createStatement(query);
         }
         // Replace parameters, must be done 1 at a time
         if (params)
@@ -1082,7 +1077,7 @@ LoginManagerStorage_mozStorage.prototype = {
 
         // Finalize all statements to free memory, avoid errors later
         for (let i = 0; i < this._dbStmts.length; i++)
-            this._dbStmts[i].statement.finalize();
+            this._dbStmts[i].finalize();
         this._dbStmts = [];
 
         // Close the connection, ignore 'already closed' error
