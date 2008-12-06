474b29f7c494e49291b22d80457a81acc72af927
Justin Wood <Callek@gmail.com>
2008-12-05 23:04:01 -0500

    Bug 467050, add config.mk to the GLOBAL_DEPS list as well r=ted

--- config/rules.mk
+++ config/rules.mk
@@ -853,7 +853,7 @@ HOST_LIBS_DEPS = $(filter %.$(LIB_SUFFIX), $(HOST_LIBS))
 DSO_LDOPTS_DEPS = $(EXTRA_DSO_LIBS) $(filter %.$(LIB_SUFFIX), $(EXTRA_DSO_LDOPTS))
 
 # Dependancies which, if modified, should cause everything to rebuild
-GLOBAL_DEPS += Makefile Makefile.in $(DEPTH)/config/autoconf.mk
+GLOBAL_DEPS += Makefile Makefile.in $(DEPTH)/config/autoconf.mk $(DEPTH)/config/config.mk
 
 ##############################################
 ifdef PARALLEL_DIRS
