36a00cfb4134347bbd7a357600b453050f307cbc
Simon Bünzli <zeniko@gmail.com>
2008-12-07 14:31:13 +0100

    Bug 462863 - about:sessionrestore shouldn't display authentication requests for favicons. r=dietrich

--- browser/components/sessionstore/content/aboutSessionRestore.js
+++ browser/components/sessionstore/content/aboutSessionRestore.js
@@ -79,10 +79,14 @@ function initTreeView() {
     };
     winState.tabs = aWinData.tabs.map(function(aTabData) {
       var entry = aTabData.entries[aTabData.index - 1] || { url: "about:blank" };
+      var iconURL = aTabData.attributes && aTabData.attributes.image || null;
+      // don't initiate a connection just to fetch a favicon (see bug 462863)
+      if (/^https?:/.test(iconURL))
+        iconURL = "moz-anno:favicon:" + iconURL;
       return {
         label: entry.title || entry.url,
         checked: true,
-        src: aTabData.attributes.image || null,
+        src: iconURL,
         parent: winState
       };
     });
