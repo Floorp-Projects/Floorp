cff4389294ba13e775dcb7be2570f5c4beb00695
Oleg Romashin <romaxa@gmail.com>
2008-12-11 03:04:46 -0500

    Bug 428988 - Expose mouse event pressure. r=Olli.Pettay, sr=roc.

--- content/events/src/nsDOMMouseEvent.h
+++ content/events/src/nsDOMMouseEvent.h
@@ -41,13 +41,14 @@
 
 #include "nsIDOMMouseEvent.h"
 #include "nsDOMUIEvent.h"
+#include "nsIDOMNSMouseEvent.h"
 
 class nsIContent;
 class nsIScrollableView;
 class nsEvent;
 
-class nsDOMMouseEvent : public nsIDOMMouseEvent,
-                        public nsDOMUIEvent
+class nsDOMMouseEvent : public nsDOMUIEvent,
+                        public nsIDOMNSMouseEvent
 {
 public:
   nsDOMMouseEvent(nsPresContext* aPresContext, nsInputEvent* aEvent);
@@ -57,7 +58,10 @@ public:
 
   // nsIDOMMouseEvent Interface
   NS_DECL_NSIDOMMOUSEEVENT
-  
+
+  // nsIDOMNSMouseEvent Interface
+  NS_DECL_NSIDOMNSMOUSEEVENT
+
   // Forward to base class
   NS_FORWARD_TO_NSDOMUIEVENT
 
