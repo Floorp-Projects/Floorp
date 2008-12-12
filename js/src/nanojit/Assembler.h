68c896be8147660323e5c45b14764aa08f8119b6
Graydon Hoare <graydon@mozilla.com>
2008-12-11 17:09:45 -0800

    Bug 468484 - Back out further arm-wince breakage from recent merging activity.

--- js/src/nanojit/Assembler.h
+++ js/src/nanojit/Assembler.h
@@ -314,7 +314,7 @@ namespace nanojit
 			void		asm_call(LInsp);
             void        asm_arg(ArgSize, LInsp, Register);
 			Register	asm_binop_rhs_reg(LInsp ins);
-			NIns*		asm_branch(bool branchOnFalse, LInsp cond, NIns* targ, bool far);
+			NIns*		asm_branch(bool branchOnFalse, LInsp cond, NIns* targ, bool isfar);
             void        assignSavedRegs();
             void        reserveSavedRegs();
             void        assignParamRegs();
