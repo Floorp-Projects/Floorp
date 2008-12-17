88e70d14c6fa7254eec8095cdc36b5a08aaeb46a
Dan Mills <thunder@mozilla.com>
2008-12-17 02:30:11 -0800

    change itemExists to check for the cache, then call _itemExists, that way subclasses can override just that without having to care about the cache

--- services/sync/modules/stores.js
+++ services/sync/modules/stores.js
@@ -97,12 +97,19 @@ Store.prototype = {
     fn.async(this, onComplete, record);
   },
 
-  // override only if neccessary
-  itemExists: function Store_itemExists(GUID) {
-    if (GUID in this._lookup)
+  itemExists: function Store_itemExists(id) {
+    if (!this._itemCache)
+      return this._itemExists(id);
+
+    if (id in this._itemCache)
       return true;
     else
       return false;
+  },
+
+  // subclasses probably want to override this one
+  _itemExists: function Store__itemExists(id) {
+    return false;
   },
 
   cacheItemsHint: function Store_cacheItemsHint() {
