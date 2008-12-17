5f8aedcf1357a075940c96ce3e6f4dccc3474435
Martin Stránský <stransky@redhat.com>
2008-12-17 16:17:50 +1300

    b=403513 bad kerning in print output if hinting is set to medium or full r=karlt sr=vlad

--- gfx/src/thebes/nsThebesFontMetrics.cpp
+++ gfx/src/thebes/nsThebesFontMetrics.cpp
@@ -83,9 +83,11 @@ nsThebesFontMetrics::Init(const nsFont& aFont, nsIAtom* aLangGroup,
         langGroup.Assign(lg);
     }
 
+    PRBool printerFont = mDeviceContext->IsPrinterSurface();
     mFontStyle = new gfxFontStyle(aFont.style, aFont.weight, size, langGroup,
                                   aFont.sizeAdjust, aFont.systemFont,
-                                  aFont.familyNameQuirks);
+                                  aFont.familyNameQuirks,
+                                  printerFont);
 
     mFontGroup =
         gfxPlatform::GetPlatform()->CreateFontGroup(aFont.name, mFontStyle, 
