c1901dc2798649941e0421ec450900ce6555f02e
Chris Double <chris.double@double.co.nz>
2008-12-18 20:56:32 +1300

    Bug 464376. Fire progress events reliably and especially be sure to fire a final progress event when the resource finishes loading. r+sr=roc
    --HG--
    extra : rebase_source : 387866a3c9ad9f05e35e9876ebd1db1224237798

--- content/media/video/public/nsOggDecoder.h
+++ content/media/video/public/nsOggDecoder.h
@@ -512,6 +512,10 @@ private:
   // when writing to the state, or when reading from a non-main thread.
   // Any change to the state must call NotifyAll on the monitor.
   PlayState mNextState;	
+
+  // True when the media resource has completely loaded. Accessed on
+  // the main thread only.
+  PRPackedBool mResourceLoaded;
 };
 
 #endif
