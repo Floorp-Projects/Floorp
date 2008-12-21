cb0384401841dc7f9b028b627c5b592486a658af
Igor Bukanov <igor@mir2.org>
2008-12-21 12:31:38 +0100

    bug 469233 - using interrupt hook support in the interpreter for trace recording. r=brendan

--- js/src/jsxdrapi.h
+++ js/src/jsxdrapi.h
@@ -204,7 +204,7 @@ JS_XDRFindClassById(JSXDRState *xdr, uint32 id);
  * before deserialization of bytecode.  If the saved version does not match
  * the current version, abort deserialization and invalidate the file.
  */
-#define JSXDR_BYTECODE_VERSION      (0xb973c0de - 38)
+#define JSXDR_BYTECODE_VERSION      (0xb973c0de - 39)
 
 /*
  * Library-private functions.
