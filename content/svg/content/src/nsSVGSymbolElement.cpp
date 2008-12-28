f16a5513ad6c8f851d2816343b80adad8e8cd92e
Craig Topper <craig.topper@gmail.com>
2008-12-28 07:58:37 +0100

    Bug 471235 - IsAttributeMapped appears in many SVG class definitions with NS_IMETHODIMP_ instead of NS_IMETHOD_; r=longsonr sr=roc

--- content/svg/content/src/nsSVGSymbolElement.cpp
+++ content/svg/content/src/nsSVGSymbolElement.cpp
@@ -70,7 +70,7 @@ public:
   NS_FORWARD_NSIDOMSVGELEMENT(nsSVGElement::)
 
   // nsIContent interface
-  NS_IMETHODIMP_(PRBool) IsAttributeMapped(const nsIAtom* name) const;
+  NS_IMETHOD_(PRBool) IsAttributeMapped(const nsIAtom* name) const;
 
   virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
 
