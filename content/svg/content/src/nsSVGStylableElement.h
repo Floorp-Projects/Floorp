70985fa04f2e2b17148199d13a202b24698880a5
Craig Topper <craig.topper@gmail.com>
2009-01-05 14:13:56 +1300

    Bug 471165. Make SVG animated class storage an nsAutoPtr<nsString> and make the DOM 'className' object a tear-off. r=longsonr,sr=roc
    --HG--
    extra : rebase_source : 8ba937100e482ea0dab88a310ad378afd76c8953

--- content/svg/content/src/nsSVGStylableElement.h
+++ content/svg/content/src/nsSVGStylableElement.h
@@ -43,7 +43,6 @@
 #include "nsIDOMSVGStylable.h"
 #include "nsIDOMSVGAnimatedString.h"
 #include "nsAutoPtr.h"
-#include "nsSVGClassValue.h"
 
 typedef nsSVGElement nsSVGStylableElementBase;
 
@@ -52,7 +51,6 @@ class nsSVGStylableElement : public nsSVGStylableElementBase,
 {
 protected:
   nsSVGStylableElement(nsINodeInfo *aNodeInfo);
-  nsresult Init();
 
 public:
   // interfaces:
@@ -62,8 +60,43 @@ public:
   // nsIContent
   virtual const nsAttrValue* DoGetClasses() const;
 
+  // nsSVGElement
+  virtual nsresult UnsetAttr(PRInt32 aNamespaceID, nsIAtom* aAttribute,
+                             PRBool aNotify);
+
 protected:
-  nsRefPtr<nsSVGClassValue> mClassName;
+
+  // nsSVGElement
+  virtual PRBool ParseAttribute(PRInt32 aNamespaceID, nsIAtom* aName,
+                                const nsAString& aValue,
+                                nsAttrValue& aResult);
+
+private:
+  nsAutoPtr<nsAttrValue> mClassAnimAttr;
+
+  const nsAttrValue* GetClassAnimAttr() const;
+ 
+  void GetClassBaseValString(nsAString &aResult) const;
+  void SetClassBaseValString(const nsAString& aValue);
+  void GetClassAnimValString(nsAString& aResult) const;
+
+  struct DOMAnimatedClassString : public nsIDOMSVGAnimatedString
+  {
+    NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+    NS_DECL_CYCLE_COLLECTION_CLASS(DOMAnimatedClassString)
+
+    DOMAnimatedClassString(nsSVGStylableElement *aSVGElement)
+      : mSVGElement(aSVGElement) {}
+
+    nsRefPtr<nsSVGStylableElement> mSVGElement;
+
+    NS_IMETHOD GetBaseVal(nsAString& aResult)
+      { mSVGElement->GetClassBaseValString(aResult); return NS_OK; }
+    NS_IMETHOD SetBaseVal(const nsAString& aValue)
+      { mSVGElement->SetClassBaseValString(aValue); return NS_OK; }
+    NS_IMETHOD GetAnimVal(nsAString& aResult)
+      { mSVGElement->GetClassAnimValString(aResult); return NS_OK; }
+  };
 };
 
 
