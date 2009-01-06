328d8d82131ba36a65c1ab826e5daac818e9004c
Marco Zehe <marco.zehe@googlemail.com>
2009-01-06 15:04:28 +0100

    Bug 469818 - Refactor test_textboxes.* files, r=surkov

--- accessible/tests/mochitest/testTextboxes.js
+++ accessible/tests/mochitest/testTextboxes.js
@@ -1,20 +1,6 @@
-// Mapping needed state flags for easier handling.
-const state_focusable = nsIAccessibleStates.STATE_FOCUSABLE;
-const state_focused = nsIAccessibleStates.STATE_FOCUSED;
-const state_readonly = nsIAccessibleStates.STATE_READONLY;
-
-const ext_state_multi_line = nsIAccessibleStates.EXT_STATE_MULTI_LINE;
-const ext_state_editable = nsIAccessibleStates.EXT_STATE_EDITABLE;
-const ext_state_required = nsIAccessibleStates.STATE_REQUIRED;
-const ext_state_invalid = nsIAccessibleStates.STATE_INVALID;
-
-// Mapping needed final roles
-const role_entry = nsIAccessibleRole.ROLE_ENTRY;
-const role_password_text = nsIAccessibleRole.ROLE_PASSWORD_TEXT;
-
 function testValue(aID, aAcc, aValue, aRole)
 {
-  if (aRole == role_password_text) {
+  if (aRole == ROLE_PASSWORD_TEXT) {
     var value;
     try {
       value = aAcc.value;
@@ -25,22 +11,6 @@ function testValue(aID, aAcc, aValue, aRole)
     }
   } else
     is(aAcc.value, aValue, "Wrong value for " + aID + "!");
-}
-
-function testStates(aID, aAcc, aState, aExtraState, aAbsentState)
-{
-  var state = {}, extraState = {};
-  aAcc.getState(state, extraState);
-  is(state.value & aState, aState, "wrong state bits for " + aID + "!");
-  is(extraState.value & aExtraState, aExtraState,
-     "wrong extraState bits for " + aID + "!");
-  if (aAbsentState != 0)
-    is(state.value & aAbsentState, 0, "state bits should not be present in ID "
-       + aID + "!");
-  if (state.value & state_readonly)
-    // if state is readonly, ext state must not be ext_state_editable.
-    is(extraState.value & ext_state_editable, 0,
-       "Read-only " + aID + " cannot be editable!");
 }
 
 function testAction(aID, aAcc, aNumActions, aActionName, aActionDescription)
@@ -61,21 +31,16 @@ function testThis(aID, aName, aValue, aDescription, aRole, aState,
                   aExtraState, aAbsentState, aNumActions, aActionName,
                   aActionDescription)
 {
-  var elem = document.getElementById(aID);
-  var acc = null;
-  try {
-    acc = gAccRetrieval.getAccessibleFor(elem);
-  } catch(e) {}
-  ok(acc, "No accessible for " + aID + "!");
+  var acc = getAccessible(aID);
+  if (!acc)
+    return;
 
-  if (acc) {
-    is(acc.name, aName, "Wrong name for " + aID + "!");
-    testValue(aID, acc, aValue, aRole);
-    is(acc.description, aDescription, "Wrong description for " + aID + "!");
-    is(acc.role, aRole, "Wrong role for " + aID + "!");
+  is(acc.name, aName, "Wrong name for " + aID + "!");
+  testValue(aID, acc, aValue, aRole);
+  is(acc.description, aDescription, "Wrong description for " + aID + "!");
+  is(acc.role, aRole, "Wrong role for " + aID + "!");
 
-    testStates(aID, acc, aState, aExtraState, aAbsentState);
+  testStates(acc, aState, aExtraState, aAbsentState);
 
-    testAction(aID, acc, aNumActions, aActionName, aActionDescription);
-  }
+  testAction(aID, acc, aNumActions, aActionName, aActionDescription);
 }
