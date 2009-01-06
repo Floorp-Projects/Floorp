59af88ade981ef22b7fcccb13a2e414f67882e83
Dave Camp <dcamp@mozilla.com>
2009-01-05 20:12:30 -0800

    Workaround for the bustage caused by bug 472253.

--- toolkit/components/url-classifier/content/listmanager.js
+++ toolkit/components/url-classifier/content/listmanager.js
@@ -77,7 +77,7 @@ function PROT_ListManager() {
   this.tablesData = {};
 
   this.observerServiceObserver_ = new G_ObserverServiceObserver(
-                                          'xpcom-shutdown',
+                                          'quit-application',
                                           BindToObject(this.shutdown_, this),
                                           true /*only once*/);
 
@@ -119,6 +119,10 @@ function PROT_ListManager() {
  * Delete all of our data tables which seem to leak otherwise.
  */
 PROT_ListManager.prototype.shutdown_ = function() {
+  if (this.keyManager_) {
+    this.keyManager_.shutdown();
+  }
+
   for (var name in this.tablesData) {
     delete this.tablesData[name];
   }
