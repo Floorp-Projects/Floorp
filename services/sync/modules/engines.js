57cf66d8705a39dc8586161ed2f31ca5ec287647
Dan Mills <thunder@mozilla.com>
2009-01-07 13:05:05 -0800

    fail recordLike if either record is null (deleted)

--- services/sync/modules/engines.js
+++ services/sync/modules/engines.js
@@ -226,6 +226,9 @@ SyncEngine.prototype = {
     if (a.depth != b.depth)
       return false;
     // note: sortindex ignored
+    if (a.cleartext == null ||
+        b.cleartext == null)
+      return false;
     return Utils.deepEquals(a.cleartext, b.cleartext);
   },
 
