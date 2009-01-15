75ac3676a2d2f80497eea6482a0b2ef836aa4056
Jonas Sicking <jonas@sicking.cc>
2009-01-14 17:25:21 -0800

    Bug 461555: Don't clear out the parser until all deferred scripts have executed to ensure that a document.write in a deferred script doesn't clear the page. r/sr=mrbkap

--- content/html/document/src/nsHTMLContentSink.cpp
+++ content/html/document/src/nsHTMLContentSink.cpp
@@ -185,6 +185,7 @@ public:
   NS_IMETHOD WillParse(void);
   NS_IMETHOD WillBuildModel(void);
   NS_IMETHOD DidBuildModel(void);
+  virtual PRBool ReadyToCallDidBuildModel(void);
   NS_IMETHOD WillInterrupt(void);
   NS_IMETHOD WillResume(void);
   NS_IMETHOD SetParser(nsIParser* aParser);
@@ -1835,6 +1836,12 @@ HTMLContentSink::DidBuildModel(void)
   DropParserAndPerfHint();
 
   return NS_OK;
+}
+
+PRBool
+HTMLContentSink::ReadyToCallDidBuildModel()
+{
+  return ReadyToCallDidBuildModelImpl();
 }
 
 NS_IMETHODIMP
