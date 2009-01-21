c15f54734ff1564eb17d9e2ff15e3133e72a76f7
Simon Montagu <smontagu@smontagu.org>
2009-01-21 12:27:39 +0200

    Back out changeset d9c4a4d5a4e4 (bug 441167)

--- intl/locale/src/nsScriptableDateFormat.cpp
+++ intl/locale/src/nsScriptableDateFormat.cpp
@@ -46,7 +46,6 @@
 #include "nsIScriptableDateFormat.h"
 #include "nsCRT.h"
 #include "nsReadableUtils.h"
-#include "nsIPrefService.h"
 
 static NS_DEFINE_CID(kLocaleServiceCID, NS_LOCALESERVICE_CID);
 static NS_DEFINE_CID(kDateTimeFormatCID, NS_DATETIMEFORMAT_CID);
@@ -115,19 +114,14 @@ NS_IMETHODIMP nsScriptableDateFormat::FormatDateTime(
 
   nsCOMPtr<nsILocale> locale;
   // re-initialise locale pointer only if the locale was given explicitly
-  if (localeName.IsEmpty()) {
-    nsCOMPtr<nsIPrefBranch> prefs(do_GetService(NS_PREFSERVICE_CONTRACTID, &rv));
+  if (!localeName.IsEmpty()) {
+    // get locale service
+    nsCOMPtr<nsILocaleService> localeService(do_GetService(kLocaleServiceCID, &rv));
     NS_ENSURE_SUCCESS(rv, rv);
-    nsCString cLocaleName;
-    rv = prefs->GetCharPref("general.useragent.locale", getter_Copies(cLocaleName));
-    CopyUTF8toUTF16(cLocaleName, localeName);
+    // get locale
+    rv = localeService->NewLocale(localeName, getter_AddRefs(locale));
+    NS_ENSURE_SUCCESS(rv, rv);
   }
-  // get locale service
-  nsCOMPtr<nsILocaleService> localeService(do_GetService(kLocaleServiceCID, &rv));
-  NS_ENSURE_SUCCESS(rv, rv);
-  // get locale
-  rv = localeService->NewLocale(localeName, getter_AddRefs(locale));
-  NS_ENSURE_SUCCESS(rv, rv);
 
   nsCOMPtr<nsIDateTimeFormat> dateTimeFormat(do_CreateInstance(kDateTimeFormatCID, &rv));
   NS_ENSURE_SUCCESS(rv, rv);
