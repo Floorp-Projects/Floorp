4931df8a3adc5a6e998be797e7fe6ef49a330552
Dan Mills <thunder@mozilla.com>
2009-01-22 11:48:36 -0800

    move Observers.js and Preferences.js into an ext/ directory, to make it clear they are externally-mantained libraries

--- services/sync/modules/base_records/keys.js
+++ services/sync/modules/base_records/keys.js
@@ -42,8 +42,6 @@ const Ci = Components.interfaces;
 const Cr = Components.results;
 const Cu = Components.utils;
 
-Cu.import("resource://weave/Observers.js");
-Cu.import("resource://weave/Preferences.js");
 Cu.import("resource://weave/log4moz.js");
 Cu.import("resource://weave/constants.js");
 Cu.import("resource://weave/util.js");
