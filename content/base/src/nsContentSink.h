41679160f624e31a2d3e8bf246fab8978d0bb611
Jonas Sicking <jonas@sicking.cc>
2009-01-26 01:53:20 -0800

    Backed out changeset e8023bc83264 due to orange

--- content/base/src/nsContentSink.h
+++ content/base/src/nsContentSink.h
@@ -138,7 +138,6 @@ class nsContentSink : public nsICSSLoaderObserver,
   NS_HIDDEN_(nsresult) DidProcessATokenImpl(void);
   NS_HIDDEN_(void) WillBuildModelImpl(void);
   NS_HIDDEN_(void) DidBuildModelImpl(void);
-  NS_HIDDEN_(PRBool) ReadyToCallDidBuildModelImpl(void);
   NS_HIDDEN_(void) DropParserAndPerfHint(void);
 
   void NotifyAppend(nsIContent* aContent, PRUint32 aStartIndex);
@@ -351,8 +350,6 @@ protected:
   PRUint8 mDeferredLayoutStart : 1;
   // If true, we deferred notifications until sheets load
   PRUint8 mDeferredFlushTags : 1;
-  // If true, we did get a ReadyToCallDidBuildModel call
-  PRUint8 mDidGetReadyToCallDidBuildModelCall : 1;
   
   // -- Can interrupt parsing members --
   PRUint32 mDelayTimerStart;
