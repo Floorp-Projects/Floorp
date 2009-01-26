5e9ed03b2db01e8018c392163a1953322f973517
Peter Van der Beken <peterv@propagandism.org>
2009-01-26 09:09:22 +0100

    Backed out changeset 81428de4b5dc (Fix for bug 464676 (Cycle collector sometimes unlinks live cycles). r=bent, sr=jst.).

--- js/src/xpconnect/src/nsXPConnect.cpp
+++ js/src/xpconnect/src/nsXPConnect.cpp
@@ -706,35 +706,6 @@ nsXPConnect::Traverse(void *p, nsCycleCollectionTraversalCallback &cb)
     JSContext *cx = mCycleCollectionContext->GetJSContext();
 
     uint32 traceKind = js_GetGCThingTraceKind(p);
-    JSObject *obj;
-    JSClass *clazz;
-
-    // We do not want to add wrappers to the cycle collector if their native
-    // can't participate because they might be used off of the main thread, the
-    // cycle collector isn't able to deal with such objects. We do want to
-    // explicitly mark them for cycle collection if the wrapper has an
-    // external reference, because the wrapper would mark the JS object if
-    // we did add the wrapper to the cycle collector.
-    JSBool dontTraverse = PR_FALSE;
-    JSBool markJSObject = PR_FALSE;
-    if(traceKind == JSTRACE_OBJECT)
-    {
-        obj = static_cast<JSObject*>(p);
-        clazz = OBJ_GET_CLASS(cx, obj);
-
-        if(clazz == &XPC_WN_Tearoff_JSClass)
-        {
-            XPCWrappedNative *wrapper =
-                (XPCWrappedNative*)xpc_GetJSPrivate(STOBJ_GET_PARENT(obj));
-            dontTraverse = wrapper->DontCycleCollect();
-        }
-        else if(IS_WRAPPER_CLASS(clazz))
-        {
-            XPCWrappedNative *wrapper = (XPCWrappedNative*)xpc_GetJSPrivate(obj);
-            dontTraverse = wrapper->DontCycleCollect();
-            markJSObject = dontTraverse && wrapper->HasExternalReference();
-        }
-    }
 
     CCNodeType type;
 
@@ -755,14 +726,12 @@ nsXPConnect::Traverse(void *p, nsCycleCollectionTraversalCallback &cb)
         // ExplainLiveExpectedGarbage codepath
         PLDHashEntryHdr* entry =
             PL_DHashTableOperate(&mJSRoots, p, PL_DHASH_LOOKUP);
-        type = markJSObject || PL_DHASH_ENTRY_IS_BUSY(entry) ? GCMarked :
-                                                               GCUnmarked;
+        type = PL_DHASH_ENTRY_IS_BUSY(entry) ? GCMarked : GCUnmarked;
     }
     else
     {
         // Normal codepath (matches non-DEBUG_CC codepath).
-        type = !markJSObject && JS_IsAboutToBeFinalized(cx, p) ? GCUnmarked :
-                                                                 GCMarked;
+        type = JS_IsAboutToBeFinalized(cx, p) ? GCUnmarked : GCMarked;
     }
 
     char name[72];
@@ -886,8 +855,7 @@ nsXPConnect::Traverse(void *p, nsCycleCollectionTraversalCallback &cb)
 
     }
 #else
-    type = !markJSObject && JS_IsAboutToBeFinalized(cx, p) ? GCUnmarked :
-                                                             GCMarked;
+    type = JS_IsAboutToBeFinalized(cx, p) ? GCUnmarked : GCMarked;
     cb.DescribeNode(type, 0);
 #endif
 
@@ -908,9 +876,12 @@ nsXPConnect::Traverse(void *p, nsCycleCollectionTraversalCallback &cb)
     JS_TRACER_INIT(&trc, cx, NoteJSChild);
     JS_TraceChildren(&trc, p, traceKind);
 
-    if(traceKind != JSTRACE_OBJECT || dontTraverse)
+    if(traceKind != JSTRACE_OBJECT)
         return NS_OK;
     
+    JSObject *obj = static_cast<JSObject*>(p);
+    JSClass* clazz = OBJ_GET_CLASS(cx, obj);
+
     if(clazz == &XPC_WN_Tearoff_JSClass)
     {
         // A tearoff holds a strong reference to its native object
--- js/src/xpconnect/src/xpcprivate.h
+++ js/src/xpconnect/src/xpcprivate.h
@@ -2193,29 +2193,11 @@ public:
                                 GetProto()->GetLock() : nsnull;}
 
     XPCNativeSet*
-    GetSet() const {XPCAutoLock al(GetLock()); return GetSetNoLock();}
-
-#define XPC_SET_WORD(s)        ((jsword)(s))
-#define XPC_SET_MASK           ((jsword)0x1)
-#define XPC_NOT_CC_PARTICIPANT ((jsword)0x1)
-
-    inline JSBool
-    DontCycleCollect() const
-        {return XPC_SET_WORD(mSet) & XPC_NOT_CC_PARTICIPANT;}
+    GetSet() const {XPCAutoLock al(GetLock()); return mSet;}
 
 private:
-    inline void
-    SetSet(XPCNativeSet* set, PRBool aDontCycleCollect)
-        {mSet = aDontCycleCollect ?
-                (XPCNativeSet*)(XPC_SET_WORD(set) | XPC_NOT_CC_PARTICIPANT) :
-                set;}
-
-    inline void
-    SetSet(XPCNativeSet* set) {SetSet(set, DontCycleCollect());}
-
-    inline XPCNativeSet*
-    GetSetNoLock() const
-        {return (XPCNativeSet*)(XPC_SET_WORD(mSet) & ~XPC_SET_MASK);}
+    void
+    SetSet(XPCNativeSet* set) {XPCAutoLock al(GetLock()); mSet = set;}
 
     inline void
     ExpireWrapper()
@@ -2329,7 +2311,7 @@ public:
                                          nsresult* pError = nsnull);
     void Mark() const
     {
-        GetSetNoLock()->Mark();
+        mSet->Mark();
         if(mScriptableInfo) mScriptableInfo->Mark();
         if(HasProto()) GetProto()->Mark();
     }
