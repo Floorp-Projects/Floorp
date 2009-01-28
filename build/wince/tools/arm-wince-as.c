a47f5cc23c3b6d8db6f085566e6edf7631bba9f0
Brad Lassey <blassey@mozilla.com>
2009-01-28 13:13:56 -0500

    bug 474737 - Windows ce tools refactor, NPOTB r=dougt, ted

--- build/wince/tools/arm-wince-as.c
+++ build/wince/tools/arm-wince-as.c
@@ -1,16 +1,15 @@
 #include "toolspath.h"
 
-
 int 
 main(int argc, char **argv)
-{
+{  
   char* args[1000];
   int i = 0;
+  
+  args[i++] = ASM_PATH;
+  args[i++] = "-I\"" WCE_INC "\""; 
 
-  args[i++] = LIB_PATH;
-
-  argpath_conv(&argv[1], &args[i]);
+  i += argpath_conv(&argv[1], &args[i]);
 
   return run(args);
-
 }
