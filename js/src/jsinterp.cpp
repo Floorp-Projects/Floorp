b2fa096da8b1f8b6f771eb765f987596bfca0508
Jeff Walden <jwalden@mit.edu>
2009-01-30 20:00:52 -0800

    Back out bug 474501; it has a problem with one Mochitest.

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -6194,10 +6194,8 @@ js_Interpret(JSContext *cx)
                                            NULL, NULL)) {
                     goto error;
                 }
-                if (!js_DefineNativeProperty(cx, obj, id, rval, NULL, NULL,
-                                             JSPROP_ENUMERATE, 0, 0, NULL, &entry)) {
+                if (!js_SetPropertyHelper(cx, obj, id, &rval, &entry))
                     goto error;
-                }
 #ifdef JS_TRACER
                 if (entry)
                     TRACE_1(SetPropMiss, entry);
@@ -6231,7 +6229,7 @@ js_Interpret(JSContext *cx)
             }
 
             /*
-             * If rval is a hole, do not call OBJ_DEFINE_PROPERTY. In this case,
+             * If rval is a hole, do not call OBJ_SET_PROPERTY. In this case,
              * obj must be an array, so if the current op is the last element
              * initialiser, set the array length to one greater than id.
              */
@@ -6245,7 +6243,7 @@ js_Interpret(JSContext *cx)
                     goto error;
                 }
             } else {
-                if (!OBJ_DEFINE_PROPERTY(cx, obj, id, rval, NULL, NULL, JSPROP_ENUMERATE, NULL))
+                if (!OBJ_SET_PROPERTY(cx, obj, id, &rval))
                     goto error;
             }
             regs.sp -= 2;
@@ -6798,7 +6796,7 @@ js_Interpret(JSContext *cx)
                 goto error;
             }
             id = INT_TO_JSID(i);
-            if (!OBJ_DEFINE_PROPERTY(cx, obj, id, rval, NULL, NULL, JSPROP_ENUMERATE, NULL))
+            if (!OBJ_SET_PROPERTY(cx, obj, id, &rval))
                 goto error;
             regs.sp--;
           END_CASE(JSOP_ARRAYPUSH)
