cf32779e2f2d76cab890d1d8646e36a3d7294326
Boris Zbarsky <bzbarsky@mit.edu>
2009-02-03 09:29:47 -0500

    Bug 476063.  Only do special fieldsetty stuff with <legend>s that are in a fieldset.  r+sr=dbaron

--- layout/base/nsCSSFrameConstructor.h
+++ layout/base/nsCSSFrameConstructor.h
@@ -792,6 +792,7 @@ private:
   static const FrameConstructionData* FindHTMLData(nsIContent* aContent,
                                                    nsIAtom* aTag,
                                                    PRInt32 aNameSpaceID,
+                                                   nsIFrame* aParentFrame,
                                                    nsStyleContext* aStyleContext);
   // HTML data-finding helper functions
   static const FrameConstructionData*
