ba19b57ad25f6118eb7fb405a316d9f43adf730b
Marco Bonardo <mbonardo@mozilla.com>
2009-02-04 15:27:18 +0100

    Backed out changeset 1cf6c5b25fe5 due to crashes on linux unit test boxes

--- browser/components/places/tests/unit/tail_bookmarks.js
+++ browser/components/places/tests/unit/tail_bookmarks.js
@@ -43,14 +43,3 @@ var os = Cc['@mozilla.org/observer-service;1'].
          getService(Ci.nsIObserverService);
 os.notifyObservers(null, "quit-application-granted", null);
 os.notifyObservers(null, "quit-application", null);
-
-// try to close the connection so we can remove places.sqlite
-var pip = Cc["@mozilla.org/browser/nav-history-service;1"].
-          getService(Ci.nsINavHistoryService).
-          QueryInterface(Ci.nsPIPlacesDatabase);
-if (pip.DBConnection.connectionReady) {
-  pip.commitPendingChanges();
-  pip.finalizeInternalStatements();
-  pip.DBConnection.close();
-  do_check_false(pip.DBConnection.connectionReady);
-}
