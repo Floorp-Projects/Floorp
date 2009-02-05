bb4083d89797dac7d2c95dc62e7586730d38c2d4
Marco Bonardo <mbonardo@mozilla.com>
2009-02-05 14:11:54 +0100

    Bug 474081 - Sporadic failures in browser_bug321000.js - poll clipboard, r=gavin

--- browser/base/content/test/browser_bug321000.js
+++ browser/base/content/test/browser_bug321000.js
@@ -9,6 +9,8 @@ function testPaste(name, element, expected) {
   element.focus();
   listener.expected = expected;
   listener.name = name;
+  // Pasting is async because the Accel+V codepath ends up going through
+  // DocumentViewerImpl::FireClipboardEvent.
   EventUtils.synthesizeKey("v", { accelKey: true });
 }
 
@@ -41,9 +43,43 @@ function test() {
   Components.classes["@mozilla.org/widget/clipboardhelper;1"]
             .getService(Components.interfaces.nsIClipboardHelper)
             .copyString(kTestString);
-  testPaste('urlbar strips newlines and surrounding whitespace', 
-            kUrlBarElm,
-            kTestString.replace(/\s*\n\s*/g,''));
+
+  // Setting the clipboard value is an async OS operation, so we need to poll
+  // the clipboard for valid data before going on.
+  setTimeout(poll_clipboard, 100);
+}
+
+var runCount = 0;
+function poll_clipboard() {
+  // Poll for a maximum of 5s
+  if (++runCount > 50) {
+    // Log the failure
+    ok(false, "Timed out while polling clipboard for pasted data");
+    // Cleanup and interrupt the test
+    finish_test();
+    return;
+  }
+
+  var clip = Components.classes["@mozilla.org/widget/clipboard;1"].
+             getService(Components.interfaces.nsIClipboard);
+  var trans = Components.classes["@mozilla.org/widget/transferable;1"].
+              createInstance(Components.interfaces.nsITransferable);
+  trans.addDataFlavor("text/unicode");
+  var str = new Object();
+  try {
+    // This code could throw if the clipboard is not set
+    clip.getData(trans,clip.kGlobalClipboard);
+    trans.getTransferData("text/unicode",str,{});
+    str = str.value.QueryInterface(Components.interfaces.nsISupportsString);
+  } catch (ex) {}
+
+  if (kTestString == str) {
+    testPaste('urlbar strips newlines and surrounding whitespace',
+              kUrlBarElm,
+              kTestString.replace(/\s*\n\s*/g,''));
+  }
+  else
+    setTimeout(poll_clipboard, 100);
 }
 
 function continue_test() {
@@ -55,6 +91,11 @@ function continue_test() {
 function finish_test() {
   kUrlBarElm.removeEventListener("input", listener, true);
   kSearchBarElm.removeEventListener("input", listener, true);
+  // Clear the clipboard, emptyClipboard would not clear the native one, so
+  // setting it to an empty string.
+  Components.classes["@mozilla.org/widget/clipboardhelper;1"]
+            .getService(Components.interfaces.nsIClipboardHelper)
+            .copyString("");
   // Clear fields
   kUrlBarElm.value="";
   kSearchBarElm.value="";
