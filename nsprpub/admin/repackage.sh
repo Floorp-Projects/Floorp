042e2846aa26c46d61a9d7a843cadf7270c3d628
Wan-Teh Chang <wtc@google.com>
2009-02-04 20:49:21 -0800

    Bug 456449: updated NSPR to NSPR_HEAD_20090204.

--- nsprpub/admin/repackage.sh
+++ nsprpub/admin/repackage.sh
@@ -64,10 +64,10 @@
 # 
 # ------------------------------------------------------------------
 
-FROMTOP=/share/builds/components/nspr20/v4.7.4
-TOTOP=./v4.7.4
-NSPRDIR=nspr-4.7.4
-SOURCETAG=NSPR_4_7_4_RTM
+FROMTOP=/share/builds/components/nspr20/v4.8
+TOTOP=./v4.8
+NSPRDIR=nspr-4.8
+SOURCETAG=NSPR_4_8_RTM
 
 #
 # enumerate Unix object directories on /s/b/c
