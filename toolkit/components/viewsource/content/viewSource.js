7ae7d77cc078168a0618f960340dbb7a98f8ec47
Curtis Bartley <cbartley@mozilla.com>
2009-02-05 17:38:17 -0500

    Bug 475542 - history/cache problem with view page source issuing a GET on an already POSTed form. r=gavin

--- toolkit/components/viewsource/content/viewSource.js
+++ toolkit/components/viewsource/content/viewSource.js
@@ -232,10 +232,12 @@ function viewSource(url)
           loadFromURL = false;
 
           // Record the page load in the session history so <back> will work.
+          var shEntrySource = arg.QueryInterface(Ci.nsISHEntry);
           var shEntry = Cc["@mozilla.org/browser/session-history-entry;1"].createInstance(Ci.nsISHEntry);
           shEntry.setURI(makeURI(viewSrcUrl, null, null));
           shEntry.setTitle(viewSrcUrl);
           shEntry.loadType = Ci.nsIDocShellLoadInfo.loadHistory;
+          shEntry.cacheKey = shEntrySource.cacheKey;
           getBrowser().webNavigation.sessionHistory
                       .QueryInterface(Ci.nsISHistoryInternal)
                       .addEntry(shEntry, true);
