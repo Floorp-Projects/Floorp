6ba852af3469191313bccdf6d124b7c965558d6d
Jeff Walden <jwalden@mit.edu>
2009-02-06 13:53:27 -0800

    Bug 477158 - TM: "Assertion failure: v == JSVAL_TRUE || v == JSVAL_FALSE, at ../jsapi.h".  r=gal

--- js/src/jsbool.h
+++ js/src/jsbool.h
@@ -43,6 +43,8 @@
  * JS boolean interface.
  */
 
+#include "jsapi.h"
+
 JS_BEGIN_EXTERN_C
 
 /*
@@ -58,6 +60,13 @@ JS_BEGIN_EXTERN_C
  */
 #define JSVAL_HOLE      PSEUDO_BOOLEAN_TO_JSVAL(3)
 #define JSVAL_ARETURN   PSEUDO_BOOLEAN_TO_JSVAL(4)
+
+static JS_ALWAYS_INLINE JSBool
+JSVAL_TO_PUBLIC_PSEUDO_BOOLEAN(jsval v)
+{
+    JS_ASSERT(v == JSVAL_TRUE || v == JSVAL_FALSE || v == JSVAL_VOID);
+    return JSVAL_TO_PSEUDO_BOOLEAN(v);
+}
 
 extern JSClass js_BooleanClass;
 
