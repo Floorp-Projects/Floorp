5ee4168813f68a0d8e8298643b2be5935119de11
David Bolter <dbolter@mozilla.com>
2009-02-10 10:40:38 +0100

    bug 474408 - Remove role='description', r=MarcoZ

--- accessible/src/base/nsARIAMap.cpp
+++ accessible/src/base/nsARIAMap.cpp
@@ -135,14 +135,6 @@ nsRoleMapEntry nsARIAMap::gWAIRoleMap[] =
     kEndEntry
   },
   {
-    "description",
-    nsIAccessibleRole::ROLE_TEXT_CONTAINER,
-    eNoValue,
-    eNoAction,
-    kNoReqStates,
-    kEndEntry
-  },
-  {
     "dialog",
     nsIAccessibleRole::ROLE_DIALOG,
     eNoValue,
