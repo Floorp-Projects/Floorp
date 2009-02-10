ae06fa6dd54aa61949edb38b49f3316021e453a7
Andreas Gal <gal@mozilla.com>
2009-02-10 09:54:14 -0800

    Backout bug 477187.

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -54,7 +54,6 @@
 #include "jsatom.h"
 #include "jsbool.h"
 #include "jscntxt.h"
-#include "jsdate.h"
 #include "jsversion.h"
 #include "jsdbgapi.h"
 #include "jsfun.h"
@@ -2685,7 +2684,7 @@ js_Interpret(JSContext *cx)
      */
 #define CHECK_BRANCH()                                                        \
     JS_BEGIN_MACRO                                                            \
-        if (!JS_CHECK_OPERATION_LIMIT(cx))                                    \
+        if (!JS_CHECK_OPERATION_LIMIT(cx, JSOW_SCRIPT_JUMP))                  \
             goto error;                                                       \
     JS_END_MACRO
 
