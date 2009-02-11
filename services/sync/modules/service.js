8aad4428fd61c7f4908d4daf00b285e42a2e9279
Dan Mills <thunder@mozilla.com>
2009-02-11 13:47:51 -0800

    remove sm-weave-proxy01 hack

--- services/sync/modules/service.js
+++ services/sync/modules/service.js
@@ -196,9 +196,6 @@ WeaveSvc.prototype = {
     let url = Svc.Prefs.get("clusterURL");
     if (!url)
       return null;
-    // XXX tmp hack for sm-weave-proxy01
-    if (url == "https://sm-weave-proxy01.services.mozilla.com/")
-      return "https://sm-weave-proxy01.services.mozilla.com/weave/0.3/";
     if (url[url.length-1] != '/')
       url += '/';
     url += "0.3/user/";
