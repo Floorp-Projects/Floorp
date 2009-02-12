777fb0c88cd10f1be72e8a4c2b0346da1613fd73
Gavin Sharp <gavin@mozilla.com>
2009-02-12 16:34:54 -0500

    Fix typo from patch for bug 476897

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -154,7 +154,7 @@ pref("gfx.downloadable_fonts.enabled", true);
 pref("accessibility.browsewithcaret", false);
 pref("accessibility.warn_on_browsewithcaret", true);
 
-pref("accessibility.browserwithcaret_shortcut.enabled", true);
+pref("accessibility.browsewithcaret_shortcut.enabled", true);
 
 #ifndef XP_MACOSX
 // Tab focus model bit field:
