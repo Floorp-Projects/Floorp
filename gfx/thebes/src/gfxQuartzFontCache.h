24c233369fd538b60ac688cf75572e4e430aee14
John Daggett <jdaggett@mozilla.com>
2009-02-18 16:33:39 +0900

    Bug 476724 - clean out Windows-specific flags from gfxFontEntry. r=vlad

--- gfx/thebes/src/gfxQuartzFontCache.h
+++ gfx/thebes/src/gfxQuartzFontCache.h
@@ -77,9 +77,7 @@ public:
 
     // initialize with Apple-type weight [1..14]
     MacOSFontEntry(const nsAString& aPostscriptName, PRInt32 aAppleWeight, PRUint32 aTraits, 
-                   MacOSFamilyEntry *aFamily, PRBool aIsStandardFace = PR_FALSE);
-
-    const nsString& FamilyName();
+                   PRBool aIsStandardFace = PR_FALSE);
 
     PRUint32 Traits() { return mTraits; }
     
@@ -93,7 +91,6 @@ protected:
                    gfxUserFontData *aUserFontData);
 
     PRUint32 mTraits;
-    MacOSFamilyEntry *mFamily;
 
     ATSUFontID mATSUFontID;
     PRPackedBool mATSUIDInitialized;
