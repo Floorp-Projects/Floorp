e80c4663c86a0681bf31ab6e59badfd1f3d062a5
Jason Duell <jduell.mcbugs@gmail.com>
2009-02-19 15:25:18 -0500

    Bug 474536.  Expose the underlying channel's content-disposition on jar: channels.  r+sr=bzbarsky

--- netwerk/base/public/nsChannelProperties.h
+++ netwerk/base/public/nsChannelProperties.h
@@ -58,11 +58,21 @@
  */
 #define NS_CHANNEL_PROP_CONTENT_LENGTH_STR "content-length"
 
+/**
+ * MIME Content-Disposition header of channel.  
+ * Not available before onStartRequest. 
+ * Type: nsACString
+ */
+#define NS_CHANNEL_PROP_CONTENT_DISPOSITION_STR "content-disposition"
+
 #ifdef IMPL_NS_NET
 #define NS_CHANNEL_PROP_CONTENT_LENGTH gNetStrings->kContentLength
+#define NS_CHANNEL_PROP_CONTENT_DISPOSITION gNetStrings->kContentDisposition
 #else
 #define NS_CHANNEL_PROP_CONTENT_LENGTH \
   NS_LITERAL_STRING(NS_CHANNEL_PROP_CONTENT_LENGTH_STR)
+#define NS_CHANNEL_PROP_CONTENT_DISPOSITION \
+  NS_LITERAL_STRING(NS_CHANNEL_PROP_CONTENT_DISPOSITION_STR)
 #endif
 
 #endif
