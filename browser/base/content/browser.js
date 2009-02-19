39287d366208be6fbbb0e86f6b267830b8e21f6f
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2009-02-19 09:34:12 +0330

    Bug 464736 - Cannot import History and Cookies from Safari while in Private Browsing mode; r=gavin

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6868,6 +6868,8 @@ let gPrivateBrowsingUI = {
   onEnterPrivateBrowsing: function PBUI_onEnterPrivateBrowsing() {
     this._setPBMenuTitle("stop");
 
+    document.getElementById("menu_import").setAttribute("disabled", "true");
+
     this._privateBrowsingAutoStarted = this._privateBrowsingService.autoStarted;
 
     if (!this._privateBrowsingAutoStarted) {
@@ -6892,6 +6894,8 @@ let gPrivateBrowsingUI = {
   onExitPrivateBrowsing: function PBUI_onExitPrivateBrowsing() {
     if (BrowserSearch.searchBar)
       BrowserSearch.searchBar.textbox.reset();
+
+    document.getElementById("menu_import").removeAttribute("disabled");
 
     gFindBar.getElement("findbar-textbox").reset();
 
