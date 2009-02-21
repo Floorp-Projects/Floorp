079b2f89aa2082404eaf11c3b8994b3d839aabde
Bob Clary <bclary@bclary.com>
2009-02-20 16:57:02 -0800

    bug 451974 - in order to exclude randam passes on platforms that do not support jit, require jit time to be less than 1/2 non jit time in order to pass.

--- js/tests/js1_8_1/trace/regress-451974-01.js
+++ js/tests/js1_8_1/trace/regress-451974-01.js
@@ -82,6 +82,6 @@ jit(false);
 print('time: nonjit = ' + timenonjit + ', jit = ' + timejit);
 
 expect = true;
-actual = timejit < timenonjit;
+actual = timejit < timenonjit/2;
 
 reportCompare(expect, actual, summary);
