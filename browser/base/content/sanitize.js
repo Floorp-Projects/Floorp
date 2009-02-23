ef6ef3eca651f3b0783d0c96d9bdb87adf9d749a
Michal Novotny <michal@allpeers.com>
2009-02-23 19:28:17 +0100

    Bug 474434 - Active ftp sessions are not closed when clearing recent history; r=bzbarsky sr=cbiesinger

--- browser/base/content/sanitize.js
+++ browser/base/content/sanitize.js
@@ -327,10 +327,10 @@ Sanitizer.prototype = {
                             .getService(Components.interfaces.nsISecretDecoderRing);
         sdr.logoutAndTeardown();
 
-        // clear plain HTTP auth sessions
-        var authMgr = Components.classes['@mozilla.org/network/http-auth-manager;1']
-                                .getService(Components.interfaces.nsIHttpAuthManager);
-        authMgr.clearAll();
+        // clear FTP and plain HTTP auth sessions
+        var os = Components.classes["@mozilla.org/observer-service;1"]
+                           .getService(Components.interfaces.nsIObserverService);
+        os.notifyObservers(null, "net:clear-active-logins", null);
       },
       
       get canClear()
