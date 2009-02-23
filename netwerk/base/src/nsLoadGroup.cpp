53027c35ddbc9a484cd7961fd941f37904c6e6a6
Arpad Borsos <arpad.borsos@googlemail.com>
2009-02-23 02:05:28 +0100

    Bug 474369 - get rid of nsVoidArray; netwerk part, v2; r=cbiesinger

--- netwerk/base/src/nsLoadGroup.cpp
+++ netwerk/base/src/nsLoadGroup.cpp
@@ -49,7 +49,7 @@
 #include "nsXPIDLString.h"
 #include "nsReadableUtils.h"
 #include "nsString.h"
-#include "nsVoidArray.h"
+#include "nsTArray.h"
 
 #if defined(PR_LOGGING)
 //
@@ -243,18 +243,18 @@ nsLoadGroup::GetStatus(nsresult *status)
 }
 
 // PLDHashTable enumeration callback that appends strong references to
-// all nsIRequest to an nsVoidArray.
+// all nsIRequest to an nsTArray<nsIRequest*>.
 static PLDHashOperator
-AppendRequestsToVoidArray(PLDHashTable *table, PLDHashEntryHdr *hdr,
-                          PRUint32 number, void *arg)
+AppendRequestsToArray(PLDHashTable *table, PLDHashEntryHdr *hdr,
+                      PRUint32 number, void *arg)
 {
     RequestMapEntry *e = static_cast<RequestMapEntry *>(hdr);
-    nsVoidArray *array = static_cast<nsVoidArray *>(arg);
+    nsTArray<nsIRequest*> *array = static_cast<nsTArray<nsIRequest*> *>(arg);
 
     nsIRequest *request = e->mKey;
     NS_ASSERTION(request, "What? Null key in pldhash entry?");
 
-    PRBool ok = array->AppendElement(request);
+    PRBool ok = array->AppendElement(request) != nsnull;
 
     if (!ok) {
         return PL_DHASH_STOP;
@@ -265,18 +265,6 @@ AppendRequestsToVoidArray(PLDHashTable *table, PLDHashEntryHdr *hdr,
     return PL_DHASH_NEXT;
 }
 
-// nsVoidArray enumeration callback that releases all items in the
-// nsVoidArray
-static PRBool
-ReleaseVoidArrayItems(void *aElement, void *aData)
-{
-    nsISupports *s = static_cast<nsISupports *>(aElement);
-
-    NS_RELEASE(s);
-
-    return PR_TRUE;
-}
-
 NS_IMETHODIMP
 nsLoadGroup::Cancel(nsresult status)
 {
@@ -284,13 +272,15 @@ nsLoadGroup::Cancel(nsresult status)
     nsresult rv;
     PRUint32 count = mRequests.entryCount;
 
-    nsAutoVoidArray requests;
+    nsAutoTArray<nsIRequest*, 8> requests;
 
-    PL_DHashTableEnumerate(&mRequests, AppendRequestsToVoidArray,
-                           static_cast<nsVoidArray *>(&requests));
+    PL_DHashTableEnumerate(&mRequests, AppendRequestsToArray,
+                           static_cast<nsTArray<nsIRequest*> *>(&requests));
 
-    if (requests.Count() != (PRInt32)count) {
-        requests.EnumerateForwards(ReleaseVoidArrayItems, nsnull);
+    if (requests.Length() != count) {
+        for (PRUint32 i = 0, len = requests.Length(); i < len; ++i) {
+            NS_RELEASE(requests[i]);
+        }
 
         return NS_ERROR_OUT_OF_MEMORY;
     }
@@ -308,7 +298,7 @@ nsLoadGroup::Cancel(nsresult status)
     nsresult firstError = NS_OK;
 
     while (count > 0) {
-        nsIRequest* request = static_cast<nsIRequest*>(requests.ElementAt(--count));
+        nsIRequest* request = requests.ElementAt(--count);
 
         NS_ASSERTION(request, "NULL request found in list.");
 
@@ -368,13 +358,15 @@ nsLoadGroup::Suspend()
     nsresult rv, firstError;
     PRUint32 count = mRequests.entryCount;
 
-    nsAutoVoidArray requests;
+    nsAutoTArray<nsIRequest*, 8> requests;
 
-    PL_DHashTableEnumerate(&mRequests, AppendRequestsToVoidArray,
-                           static_cast<nsVoidArray *>(&requests));
+    PL_DHashTableEnumerate(&mRequests, AppendRequestsToArray,
+                           static_cast<nsTArray<nsIRequest*> *>(&requests));
 
-    if (requests.Count() != (PRInt32)count) {
-        requests.EnumerateForwards(ReleaseVoidArrayItems, nsnull);
+    if (requests.Length() != count) {
+        for (PRUint32 i = 0, len = requests.Length(); i < len; ++i) {
+            NS_RELEASE(requests[i]);
+        }
 
         return NS_ERROR_OUT_OF_MEMORY;
     }
@@ -385,8 +377,7 @@ nsLoadGroup::Suspend()
     // get removed from the list it won't affect our iteration
     //
     while (count > 0) {
-        nsIRequest* request =
-            static_cast<nsIRequest*>(requests.ElementAt(--count));
+        nsIRequest* request = requests.ElementAt(--count);
 
         NS_ASSERTION(request, "NULL request found in list.");
         if (!request)
@@ -419,13 +410,15 @@ nsLoadGroup::Resume()
     nsresult rv, firstError;
     PRUint32 count = mRequests.entryCount;
 
-    nsAutoVoidArray requests;
+    nsAutoTArray<nsIRequest*, 8> requests;
 
-    PL_DHashTableEnumerate(&mRequests, AppendRequestsToVoidArray,
-                           static_cast<nsVoidArray *>(&requests));
+    PL_DHashTableEnumerate(&mRequests, AppendRequestsToArray,
+                           static_cast<nsTArray<nsIRequest*> *>(&requests));
 
-    if (requests.Count() != (PRInt32)count) {
-        requests.EnumerateForwards(ReleaseVoidArrayItems, nsnull);
+    if (requests.Length() != count) {
+        for (PRUint32 i = 0, len = requests.Length(); i < len; ++i) {
+            NS_RELEASE(requests[i]);
+        }
 
         return NS_ERROR_OUT_OF_MEMORY;
     }
@@ -436,8 +429,7 @@ nsLoadGroup::Resume()
     // get removed from the list it won't affect our iteration
     //
     while (count > 0) {
-        nsIRequest* request =
-            static_cast<nsIRequest*>(requests.ElementAt(--count));
+        nsIRequest* request = requests.ElementAt(--count);
 
         NS_ASSERTION(request, "NULL request found in list.");
         if (!request)
