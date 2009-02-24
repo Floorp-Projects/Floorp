fe09ec4b0ea6f068c44959acb52beae2b65c8bb3
Jonathan Kew <jfkthame@gmail.com>
2009-02-24 21:32:58 +1300

    Bug 479276. Support a new kind of glyph bounding-box: TIGHT_HINTED_OUTLINE_EXTENTS, and use it to stop MathML spacing depending on exact ink extents. r+sr=roc

--- gfx/src/thebes/nsThebesFontMetrics.cpp
+++ gfx/src/thebes/nsThebesFontMetrics.cpp
@@ -438,7 +438,10 @@ GetTextRunBoundingMetrics(gfxTextRun *aTextRun, PRUint32 aStart, PRUint32 aLengt
 {
     StubPropertyProvider provider;
     gfxTextRun::Metrics theMetrics =
-        aTextRun->MeasureText(aStart, aLength, PR_TRUE, aContext->ThebesContext(), &provider);
+        aTextRun->MeasureText(aStart, aLength, gfxFont::TIGHT_HINTED_OUTLINE_EXTENTS,
+                              aContext->ThebesContext(), &provider);
+        // note that TIGHT_UNHINTED_OUTLINE_EXTENTS can be expensive (on Windows)
+        // but this is only used for MathML positioning so it's not critical
 
     aBoundingMetrics.leftBearing = NSToCoordFloor(theMetrics.mBoundingBox.X());
     aBoundingMetrics.rightBearing = NSToCoordCeil(theMetrics.mBoundingBox.XMost());
