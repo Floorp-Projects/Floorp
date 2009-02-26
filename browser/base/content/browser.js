10989695ba3c0670244d2bfd643347a867b2bc19
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2009-02-27 00:15:33 +0330

    Backed out changeset 876aa29eebe4 which was a patch in my mq which I pushed by mistake

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6913,8 +6913,6 @@ let gPrivateBrowsingUI = {
 
     document.getElementById("menu_import").setAttribute("disabled", "true");
 
-    document.getElementById("Tools:Sanitize").setAttribute("disabled", "true");
-
     this._privateBrowsingAutoStarted = this._privateBrowsingService.autoStarted;
 
     if (!this._privateBrowsingAutoStarted) {
@@ -6941,8 +6939,6 @@ let gPrivateBrowsingUI = {
       BrowserSearch.searchBar.textbox.reset();
 
     document.getElementById("menu_import").removeAttribute("disabled");
-
-    document.getElementById("Tools:Sanitize").removeAttribute("disabled");
 
     gFindBar.getElement("findbar-textbox").reset();
 
