76d58254c67a4abfa27e70388126a289f7531343
Andreas Gal <gal@mozilla.com>
2009-02-26 21:53:30 -0800

    Permit and guard on dense arrays when tracing a not-found property acccess (480479, r=jwalden).

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -4420,6 +4420,22 @@ function testFunctionToFunctionGlobalPropertyTransition()
 testFunctionToFunctionGlobalPropertyTransition.expected = "ggggh";
 test(testFunctionToFunctionGlobalPropertyTransition);
 
+function testUndefinedPropertyAccess() {
+    var x = [1,2,3];
+    var y = {};
+    var a = { foo: 1 };
+    y.__proto__ = x;
+    var z = [x, x, x, y, y, y, y, a, a, a];
+    var s = "";
+    for (var i = 0; i < z.length; ++i)
+        s += z[i].foo;
+    return s;
+}
+testUndefinedPropertyAccess.expected = "undefinedundefinedundefinedundefinedundefinedundefinedundefined111";
+testUndefinedPropertyAccess.jitstats = {
+    traceCompleted: 3
+};
+test(testUndefinedPropertyAccess);
 
 /*****************************************************************************
  *                                                                           *
