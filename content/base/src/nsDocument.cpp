b7dc00cff28fcaa6ff7df9d3c448db7d918d5f3a
Asaf Romano <mozilla.mano@sent.com>
2009-02-26 16:01:11 -0800

    comments and a hard-tab fix

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -7175,6 +7175,8 @@ nsDocument::OnPageShow(PRBool aPersisted, nsIDOMEventTarget* aDispatchStartTarge
 
   // See nsIDocument
   if (!aDispatchStartTarget) {
+    // Set mIsShowing before firing events, in case those event handlers
+    // move us around.
     mIsShowing = PR_TRUE;
   }
  
@@ -7217,6 +7219,8 @@ nsDocument::OnPageHide(PRBool aPersisted, nsIDOMEventTarget* aDispatchStartTarge
 
   // See nsIDocument
   if (!aDispatchStartTarget) {
+    // Set mIsShowing before firing events, in case those event handlers
+    // move us around.
     mIsShowing = PR_FALSE;
   }
 
