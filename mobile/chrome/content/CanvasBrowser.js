756ec79520ac1da15c3e0cdd93ad4c1c8fab4654
Taras Glek <tglek@mozilla.com>
2009-03-01 14:32:40 -0800

    Bug 480447 -  clear outstanding paint events when repainting the whole canvas r=gavin

--- mobile/chrome/content/CanvasBrowser.js
+++ mobile/chrome/content/CanvasBrowser.js
@@ -77,6 +77,15 @@ CanvasBrowser.prototype = {
     return this.canvasDimensions.map(this._screenToPage, this);
   },
 
+  get contentDOMWindowUtils() {
+    if (!this._contentDOMWindowUtils) {
+      this._contentDOMWindowUtils = this._browser.contentWindow
+        .QueryInterface(Ci.nsIInterfaceRequestor)
+        .getInterface(Ci.nsIDOMWindowUtils);
+    }
+    return this._contentDOMWindowUtils;
+  },
+  
   setCurrentBrowser: function(browser, skipZoom) {
     let currentBrowser = this._browser;
     if (currentBrowser) {
@@ -85,6 +94,7 @@ CanvasBrowser.prototype = {
       currentBrowser.setAttribute("type", "content");
       currentBrowser.docShell.isOffScreenBrowser = false;
     }
+    this._contentDOMWindowUtils = null;
 
     browser.setAttribute("type", "content-primary");
     if (!skipZoom)
@@ -253,9 +263,12 @@ CanvasBrowser.prototype = {
     // if the page is being panned, flush the queue, so things blit correctly
     // this avoids incorrect offsets due to a change in _pageBounds.x/y
     // should probably check that (visibleBounds|pageBounds).(x|y) actually changed
-    if (boundsSizeChanged)
+    if (boundsSizeChanged) {
       this.clearRegion();
-    else
+      // since we are going to repaint the whole browser
+      // any outstanding paint events will cause redundant draws
+      this.contentDOMWindowUtils.clearMozAfterPaintEvents();
+    } else
       this.flushRegion();
     
     this._visibleBounds = visibleBounds;
@@ -424,15 +437,10 @@ CanvasBrowser.prototype = {
    */
   elementFromPoint: function(aX, aY) {
     let [x, y] = this._clientToContentCoords(aX, aY);
-    let cwu = this._browser.contentWindow
-                  .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-                  .getInterface(Components.interfaces.nsIDOMWindowUtils);
-
-    let element = cwu.elementFromPoint(x, y,
-                                       true,   /* ignore root scroll frame*/
-                                       false); /* don't flush layout */
-
-    return element;
+    let cwu = this.contentDOMWindowUtils
+    return cwu.elementFromPoint(x, y,
+                                true,   /* ignore root scroll frame*/
+                                false); /* don't flush layout */
   },
 
   /**
