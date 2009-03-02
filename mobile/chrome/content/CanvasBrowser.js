9b817da3b78c2135e0770391390241da6e73457c
Gavin Sharp <gavin@mozilla.com>
2009-03-02 05:25:43 -0500

    Add missing ;

--- mobile/chrome/content/CanvasBrowser.js
+++ mobile/chrome/content/CanvasBrowser.js
@@ -437,7 +437,7 @@ CanvasBrowser.prototype = {
    */
   elementFromPoint: function(aX, aY) {
     let [x, y] = this._clientToContentCoords(aX, aY);
-    let cwu = this.contentDOMWindowUtils
+    let cwu = this.contentDOMWindowUtils;
     return cwu.elementFromPoint(x, y,
                                 true,   /* ignore root scroll frame*/
                                 false); /* don't flush layout */
