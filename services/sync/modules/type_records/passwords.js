33a1012482a421050aaae82d491b24d4aaf3b70c
Anant Narayanan <anant@kix.in>
2009-03-03 00:42:57 +0100

    Password sync for 0.3 (bug #468697)

---
+++ services/sync/modules/type_records/passwords.js
@@ -0,0 +1,100 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Weave.
+ *
+ * The Initial Developer of the Original Code is Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Anant Narayanan <anant@kix.in>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+const EXPORTED_SYMBOLS = ['LoginRec'];
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+Cu.import("resource://weave/log4moz.js");
+Cu.import("resource://weave/util.js");
+Cu.import("resource://weave/async.js");
+Cu.import("resource://weave/base_records/wbo.js");
+Cu.import("resource://weave/base_records/crypto.js");
+Cu.import("resource://weave/base_records/keys.js");
+
+Function.prototype.async = Async.sugar;
+
+function LoginRec(uri) {
+  this._LoginRec_init(uri);
+}
+LoginRec.prototype = {
+  __proto__: CryptoWrapper.prototype,
+  _logName: "Record.Login",
+
+  _LoginRec_init: function LoginItem_init(uri) {
+    this._CryptoWrap_init(uri);
+    this.cleartext = {
+    };
+  },
+
+  get hostname() this.cleartext.hostname,
+  set hostname(value) {
+      this.cleartext.hostname = value;
+  },
+
+  get formSubmitURL() this.cleartext.formSubmitURL,
+  set formSubmitURL(value) {
+    this.cleartext.formSubmitURL = value;
+  },
+
+  get httpRealm() this.cleartext.httpRealm,
+  set httpRealm(value) {
+    this.cleartext.httpRealm = value;
+  },
+  
+  get username() this.cleartext.username,
+  set username(value) {
+    this.cleartext.username = value;
+  },
+  
+  get password() this.cleartext.password,
+  set password(value) {
+    this.cleartext.password = value;
+  },
+  
+  get usernameField() this.cleartext.usernameField,
+  set usernameField(value) {
+    this.cleartext.usernameField = value;
+  },
+  
+  get passwordField() this.cleartext.passwordField,
+  set passwordField(value) {
+    this.cleartext.passwordField = value;
+  }
+};
