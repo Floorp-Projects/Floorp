05f33e08fbe43db90f91fcf9977c97c2f7cdd72b
Mike Connor <mconnor@mozilla.com>
2009-03-03 10:14:32 -0800

    bug 480854 - Start/Stop of Private Browsing mode from window-less state always opens 2 windows, r=gavin

--- browser/components/privatebrowsing/src/nsPrivateBrowsingService.js
+++ browser/components/privatebrowsing/src/nsPrivateBrowsingService.js
@@ -154,31 +154,32 @@ PrivateBrowsingService.prototype = {
           this._savedBrowserState = ss.getBrowserState();
       }
       if (!this._quitting && this._saveSession) {
-        // dummy session used to transition from/to pb mode, see bug 476463
-        let transitionState = {
-          "windows": [{
-            "tabs": [{
-              "entries": [{
-                "url": "about:blank"
-              }]
-            }],
-            "_closedTabs": []
-          }]
-        };
-        // load dummy session to get a distinct separation between private and
-        // non-private sessions
-        ss.setBrowserState(JSON.stringify(transitionState));
+        let browserWindow = this._getBrowserWindow();
 
-        let browser = Cc["@mozilla.org/appshell/window-mediator;1"].
-                      getService(Ci.nsIWindowMediator).
-                      getMostRecentWindow("navigator:browser");
+        // if there are open browser windows, load a dummy session to get a distinct 
+        // separation between private and non-private sessions
+        if (browserWindow) {
+          // dummy session used to transition from/to pb mode, see bug 476463
+          let transitionState = {
+            "windows": [{
+              "tabs": [{
+                "entries": [{
+                  "url": "about:blank"
+                }]
+              }],
+              "_closedTabs": []
+            }]
+          };
 
-        // if all browser windows are closed (i.e. on Mac) we won't have a window here
-        // this is only needed on Mac, but it's just good hygiene
-        if (browser) {
+          ss.setBrowserState(JSON.stringify(transitionState));
+
+          // just in case the only remaining window after setBrowserState is different.
+          // it probably shouldn't be with the current sessionstore impl, but we shouldn't
+          // rely on behaviour the API doesn't guarantee
+          let browser = this._getBrowserWindow().gBrowser;
+
           // this ensures a clean slate from which to transition into or out of
           // private browsing
-          browser = browser.gBrowser;
           browser.addTab();
           browser.removeTab(browser.tabContainer.firstChild);
         }
@@ -233,6 +234,12 @@ PrivateBrowsingService.prototype = {
     cancelLeave.data = false;
     this._obs.notifyObservers(cancelLeave, "private-browsing-cancel-vote", "exit");
     return !cancelLeave.data;
+  },
+
+  _getBrowserWindow: function PBS__getBrowserWindow() {
+    return Cc["@mozilla.org/appshell/window-mediator;1"].
+           getService(Ci.nsIWindowMediator).
+           getMostRecentWindow("navigator:browser");
   },
 
   // nsIObserver
