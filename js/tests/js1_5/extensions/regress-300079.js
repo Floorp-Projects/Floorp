0a52986767020b155b9b200ea532097e692daf36
Bob Clary <bclary@bclary.com>
2009-03-05 17:15:07 -0800

    bug 300079 - Test is for clone and not for js1.8 features. Modify to use functions and move to js1_5/extensions/.

--- js/tests/js1_5/extensions/regress-300079.js
+++ js/tests/js1_5/extensions/regress-300079.js
@@ -60,7 +60,7 @@ function test()
   else {
     expect = 'PASSED';
 
-    f = eval("(function (a) function () a * a)()");
+    f = eval("(function (a) {return (function () { return a * a;}); })()");
     g = clone(f, {a: 3});
     f = null;
     gc();
