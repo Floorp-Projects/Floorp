c9e9c3a5cd47c65773c1b4235502bb44adff1a08
Ted Mielczarek <ted.mielczarek@gmail.com>
2009-03-11 11:56:58 -0400

    bug 421611 - Need to be able to run tests on arbitrary build. add a package target for reftest. r=bsmedberg

--- layout/tools/reftest/reftest.js
+++ layout/tools/reftest/reftest.js
@@ -211,6 +211,8 @@ function ReadTopManifest(aFileURL)
     ReadManifest(url);
 }
 
+// Note: If you materially change the reftest manifest parsing,
+// please keep the parser in print-manifest-dirs.py in sync.
 function ReadManifest(aURL)
 {
     var listURL = aURL.QueryInterface(CI.nsIFileURL);
