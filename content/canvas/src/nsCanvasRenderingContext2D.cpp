e2cf71f1f91edd7a29e8036b7c84476715f4dff5
Doug Turner <dougt@meer.net>
2009-03-12 09:56:44 -0700

    Backing out bug 473443 - drawImage of canvas onto self is incorrect.  WINCE only.

--- content/canvas/src/nsCanvasRenderingContext2D.cpp
+++ content/canvas/src/nsCanvasRenderingContext2D.cpp
@@ -3154,14 +3154,9 @@ nsCanvasRenderingContext2D::ThebesSurfaceFromElement(nsIDOMElement *imgElt,
         if (!forceCopy && canvas->CountContexts() == 1) {
             nsICanvasRenderingContextInternal *srcCanvas = canvas->GetContextAtIndex(0);
             rv = srcCanvas->GetThebesSurface(getter_AddRefs(sourceSurface));
-#ifndef WINCE
             // force a copy if we couldn't get the surface, or if it's
             // the same as what we have
             if (sourceSurface == mSurface || NS_FAILED(rv))
-#else
-            // force a copy if we couldn't get the surface
-            if (NS_FAILED(rv))
-#endif
                 sourceSurface = nsnull;
         }
 
