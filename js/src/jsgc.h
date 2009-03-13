be1175b982c5e640a755296c195af1075300843f
Igor Bukanov <igor@mir2.org>
2009-03-13 12:10:34 +0100

    bug 437325 - JSThread is not shared between runtimes. r=brendan

--- js/src/jsgc.h
+++ js/src/jsgc.h
@@ -247,6 +247,9 @@ js_TraceRuntime(JSTracer *trc, JSBool allAtoms);
 extern JS_REQUIRES_STACK JS_FRIEND_API(void)
 js_TraceContext(JSTracer *trc, JSContext *acx);
 
+extern void
+js_PurgeTraceMonitor(JSContext *cx, JSTraceMonitor *tm);
+
 /*
  * Kinds of js_GC invocation.
  */
@@ -339,6 +342,9 @@ extern const JSGCFreeListSet js_GCEmptyFreeListSet;
 
 extern void
 js_RevokeGCLocalFreeLists(JSContext *cx);
+
+extern void
+js_DestroyScriptsToGC(JSContext *cx, JSThreadData *data);
 
 struct JSWeakRoots {
     /* Most recently created things by type, members of the GC's root set. */
