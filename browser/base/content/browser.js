04e7acb687a925cf6ddb078d33a7153867ba74a1
Mike Connor <mconnor@mozilla.com>
2009-03-13 00:25:22 -0400

    Backed out changeset e27fe0b767ea to see if it helps unit test failures

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6934,15 +6934,7 @@ let gPrivateBrowsingUI = {
 
     this._privateBrowsingAutoStarted = this._privateBrowsingService.autoStarted;
 
-    if (this._privateBrowsingAutoStarted) {
-      // Disable the menu item in auto-start mode
-      let pbMenuItem = document.getElementById("privateBrowsingItem");
-      if (pbMenuItem)
-        pbMenuItem.setAttribute("disabled", "true");
-      document.getElementById("Tools:PrivateBrowsing")
-              .setAttribute("disabled", "true");
-    }
-    else if (window.location.href == getBrowserURL()) {
+    if (!this._privateBrowsingAutoStarted) {
       // Adjust the window's title
       let docElement = document.documentElement;
       docElement.setAttribute("title",
@@ -6950,6 +6942,14 @@ let gPrivateBrowsingUI = {
       docElement.setAttribute("titlemodifier",
         docElement.getAttribute("titlemodifier_privatebrowsing"));
       docElement.setAttribute("browsingmode", "private");
+    }
+    else {
+      // Disable the menu item in auto-start mode
+      let pbMenuItem = document.getElementById("privateBrowsingItem");
+      if (pbMenuItem)
+        pbMenuItem.setAttribute("disabled", "true");
+      document.getElementById("Tools:PrivateBrowsing")
+              .setAttribute("disabled", "true");
     }
   },
 
@@ -6968,7 +6968,7 @@ let gPrivateBrowsingUI = {
 
     this._setPBMenuTitle("start");
 
-    if (window.location.href == getBrowserURL()) {
+    if (!this._privateBrowsingAutoStarted) {
       // Adjust the window's title
       let docElement = document.documentElement;
       docElement.setAttribute("title",
@@ -6977,8 +6977,8 @@ let gPrivateBrowsingUI = {
         docElement.getAttribute("titlemodifier_normal"));
       docElement.setAttribute("browsingmode", "normal");
     }
-
-    this._privateBrowsingAutoStarted = false;
+    else
+      this._privateBrowsingAutoStarted = false;
   },
 
   _setPBMenuTitle: function PBUI__setPBMenuTitle(aMode) {
