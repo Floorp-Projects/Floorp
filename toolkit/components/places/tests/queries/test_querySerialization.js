2a9c7ac66ce832cca46f5a46f69e21e68e65d028
Marco Bonardo <mbonardo@mozilla.com>
2009-03-17 22:00:50 +0100

    Bug 428690 - Two entries added to history menu when visiting a page that redirects, r=dietrich

--- toolkit/components/places/tests/queries/test_querySerialization.js
+++ toolkit/components/places/tests/queries/test_querySerialization.js
@@ -442,7 +442,24 @@ const queryOptionSwitches = [
         aQueryOptions.queryType = aQueryOptions.QUERY_TYPE_UNIFIED;
       }
     ]
-  }
+  },
+  // redirectsMode
+  {
+    property: "redirectsMode",
+    desc:     "nsINavHistoryQueryOptions.redirectsMode",
+    matches:  simplePropertyMatches,
+    runs:     [
+      function (aQuery, aQueryOptions) {
+        aQueryOptions.redirectsMode = aQueryOptions.REDIRECTS_MODE_ALL;
+      },
+      function (aQuery, aQueryOptions) {
+        aQueryOptions.redirectsMode = aQueryOptions.REDIRECTS_MODE_TARGET;
+      },
+      function (aQuery, aQueryOptions) {
+        aQueryOptions.redirectsMode = aQueryOptions.REDIRECTS_MODE_SOURCE;
+      }
+    ]
+  },
 ];
 
 ///////////////////////////////////////////////////////////////////////////////
