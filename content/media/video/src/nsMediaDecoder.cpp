d9341e98f4c9b10e1f3184586cf9bf55525a6712
Matthew Gregan <kinetik@flim.org>
2009-03-17 16:45:00 +1300

    Bug 474748 - Defer YUV conversion until frame paint, and allow SetRGBData to assume ownership of the RGB buffer rather than copying it.  r=chris.double, sr=roc
    --HG--
    extra : rebase_source : 7d122efaed1a506cebe35740f7e6d33bf6311ced

--- content/media/video/src/nsMediaDecoder.cpp
+++ content/media/video/src/nsMediaDecoder.cpp
@@ -200,25 +200,15 @@ nsresult nsMediaDecoder::StopProgress()
 
 void nsMediaDecoder::SetRGBData(PRInt32 aWidth, PRInt32 aHeight, float aFramerate, unsigned char* aRGBBuffer)
 {
+  nsAutoLock lock(mVideoUpdateLock);
+
   if (mRGBWidth != aWidth || mRGBHeight != aHeight) {
     mRGBWidth = aWidth;
     mRGBHeight = aHeight;
     mSizeChanged = PR_TRUE;
-    // Delete buffer so we'll reallocate it
-    mRGB = nsnull;
   }
   mFramerate = aFramerate;
-
-  if (aRGBBuffer) {
-    if (!mRGB) {
-      mRGB = new unsigned char[aWidth * aHeight * 4];
-    }
-    if (mRGB) {
-      memcpy(mRGB.get(), aRGBBuffer, aWidth*aHeight*4);
-    }
-  } else {
-    mRGB = nsnull;
-  }
+  mRGB = aRGBBuffer;
 }
 
 void nsMediaDecoder::Paint(gfxContext* aContext, const gfxRect& aRect)
