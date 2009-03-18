683ac127124941c40d7713a7bedf12b806ef4e89
Simon Montagu <smontagu@smontagu.org>
2009-03-18 01:39:50 -0700

    xpcshell tests for bug 22310, based on a test by Jesper Kristensen <bugzilla@jesperkristensen.dk>

---
+++ intl/locale/tests_multilocale/unit/test_bug22310.js
@@ -0,0 +1,47 @@
+String.prototype.has = function(s) this.indexOf(s) != -1;
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+const dts = Cc["@mozilla.org/intl/scriptabledateformat;1"]
+        .getService(Ci.nsIScriptableDateFormat);
+function dt(locale) dts.FormatDateTime(locale, dts.dateFormatLong,
+  dts.timeFormatSeconds, 2008, 6, 30, 13, 56, 34);
+var all_passed = true;
+const tests = 
+[
+ [dt("en-US").has("June"), "month name in en-US"],
+ [dt("en-US").has("2008"), "year in en-US"],
+ [dt("da").has("jun"), "month name in da"],
+ [dt("da-DK") == dt("da"), "da same as da-DK"],
+ [dt("en-GB").has("30") && dt("en-GB").has("June") &&
+   dt("en-GB").indexOf("30") < dt("en-GB").indexOf("June"),
+  "day before month in en-GB"],
+ [dt("en-US").has("30") && dt("en-US").has("June") &&
+   dt("en-US").indexOf("30") > dt("en-US").indexOf("June"),
+  "month before day in en-US"],
+ [dt("ja-JP").has("\u5E746\u670830\u65E5"), "year month and day in ja-JP"],
+ [dt("ja-JP") == dt("ja-JP-mac"), "ja-JP-mac same as ja-JP"],
+ [dt("nn-NO").has("juni"), "month name in nn-NO"],
+ [dt("nb-NO").has("juni"), "month name in nb-NO"],
+ [dt("no-NO").has("30. juni"), "month name in no-NO"],
+ [dt("sv-SE").has("30 jun"), "month name in sv-SE"],
+ [dt("kok").has("\u091C\u0942\u0928"), "month name in kok"],
+ [dt("gu-IN").has("\u0A9C\u0AC2\u0AA8"), "month name in gu-IN"],
+ [dt("ab-CD").length > 0, "fallback for ab-CD"]
+];
+
+function one_test(testcase, msg)
+{
+    if (!testcase) {
+        all_passed = false;
+        dump("Unexpected date format: " + msg + "\n");
+    }
+}
+
+function run_test()
+{
+    for (var i = 0; i < tests.length; ++i) {
+        one_test(tests[i][0], tests[i][1]);
+    }
+    do_check_true(all_passed);
+}
