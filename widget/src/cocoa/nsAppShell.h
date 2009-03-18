0309839a68ec24b286243f865555fb4b5bb96b44
Steven Michaud <smichaud@pobox.com>
2009-03-18 17:51:40 -0500

    File picker dialog sometimes doesn't close when Open/Save/Cancel is clicked. b=473467 r=josh sr=roc

--- widget/src/cocoa/nsAppShell.h
+++ widget/src/cocoa/nsAppShell.h
@@ -43,24 +43,39 @@
 #ifndef nsAppShell_h_
 #define nsAppShell_h_
 
+class nsCocoaWindow;
+
 #include "nsBaseAppShell.h"
+#include "nsTArray.h"
 
 typedef struct _nsCocoaAppModalWindowListItem {
-  _nsCocoaAppModalWindowListItem() : mPrev(NULL), mWindow(nil), mSession(nil) {}
-  struct _nsCocoaAppModalWindowListItem *mPrev;
+  _nsCocoaAppModalWindowListItem(NSWindow *aWindow, NSModalSession aSession) :
+    mWindow(aWindow), mSession(aSession), mWidget(nsnull) {}
+  _nsCocoaAppModalWindowListItem(NSWindow *aWindow, nsCocoaWindow *aWidget) :
+    mWindow(aWindow), mSession(nil), mWidget(aWidget) {}
   NSWindow *mWindow;       // Weak
   NSModalSession mSession; // Weak (not retainable)
+  nsCocoaWindow *mWidget;  // Weak
 } nsCocoaAppModalWindowListItem;
 
 class nsCocoaAppModalWindowList {
 public:
-  nsCocoaAppModalWindowList() : mList(NULL) {}
-  ~nsCocoaAppModalWindowList();
-  nsresult Push(NSWindow *aWindow, NSModalSession aSession);
-  nsresult Pop(NSWindow *aWindow, NSModalSession aSession);
+  nsCocoaAppModalWindowList() {}
+  ~nsCocoaAppModalWindowList() {}
+  // Push a Cocoa app-modal window onto the top of our list.
+  nsresult PushCocoa(NSWindow *aWindow, NSModalSession aSession);
+  // Pop the topmost Cocoa app-modal window off our list.
+  nsresult PopCocoa(NSWindow *aWindow, NSModalSession aSession);
+  // Push a Gecko-modal window onto the top of our list.
+  nsresult PushGecko(NSWindow *aWindow, nsCocoaWindow *aWidget);
+  // Pop the topmost Gecko-modal window off our list.
+  nsresult PopGecko(NSWindow *aWindow, nsCocoaWindow *aWidget);
+  // Return the "session" of the top-most visible Cocoa app-modal window.
   NSModalSession CurrentSession();
+  // Has a Gecko modal dialog popped up over a Cocoa app-modal dialog?
+  PRBool GeckoModalAboveCocoaModal();
 private:
-  nsCocoaAppModalWindowListItem *mList;
+  nsTArray<nsCocoaAppModalWindowListItem> mList;
 };
 
 
