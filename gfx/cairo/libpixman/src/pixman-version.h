771bcfe35febedb8a448681780c7e212ce42874f
Jeff Muizelaar <jmuizelaar@mozilla.com>
2009-03-19 13:34:17 -0400

    Bug 484076 - Update pixman and cairo to newer upstream versions. r=vlad
    Pixman revision is 072d848f592530973f1f0a0066a320ec5965625c
    Cairo revision is 3ec94f9b5917b8f71304c982bd153e5a810f3b12

--- gfx/cairo/libpixman/src/pixman-version.h
+++ gfx/cairo/libpixman/src/pixman-version.h
@@ -33,9 +33,9 @@
 
 #define PIXMAN_VERSION_MAJOR 0
 #define PIXMAN_VERSION_MINOR 13
-#define PIXMAN_VERSION_MICRO 1
+#define PIXMAN_VERSION_MICRO 3
 
-#define PIXMAN_VERSION_STRING "0.13.1"
+#define PIXMAN_VERSION_STRING "0.13.3"
 
 #define PIXMAN_VERSION_ENCODE(major, minor, micro) (	\
 	  ((major) * 10000)				\
