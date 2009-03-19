0458b9d8709235ac5c88325f6b34e53bec069ae5
Dietrich Ayala <dietrich@mozilla.com>
2009-03-18 19:13:10 -0700

    backing out due to unit test failures

--- toolkit/components/places/src/nsNavHistory.h
+++ toolkit/components/places/src/nsNavHistory.h
@@ -62,6 +62,7 @@
 #endif
 #include "nsIBrowserHistory.h"
 #include "nsICollation.h"
+#include "nsIDateTimeFormat.h"
 #include "nsIGlobalHistory.h"
 #include "nsIGlobalHistory3.h"
 #include "nsIDownloadHistory.h"
@@ -241,12 +242,10 @@ public:
    * objects for places components.
    */
   nsIStringBundle* GetBundle();
-  nsIStringBundle* GetDateFormatBundle();
   nsICollation* GetCollation();
   void GetStringFromName(const PRUnichar* aName, nsACString& aResult);
   void GetAgeInDaysString(PRInt32 aInt, const PRUnichar *aName,
                           nsACString& aResult);
-  void GetMonthName(PRInt32 aIndex, nsACString& aResult);
 
   // returns true if history has been disabled
   PRBool IsHistoryDisabled() { return mExpireDaysMax == 0 || InPrivateBrowsingMode(); }
@@ -633,7 +632,6 @@ protected:
 
   // localization
   nsCOMPtr<nsIStringBundle> mBundle;
-  nsCOMPtr<nsIStringBundle> mDateFormatBundle;
   nsCOMPtr<nsICollation> mCollation;
 
   // annotation service : MAY BE NULL!
