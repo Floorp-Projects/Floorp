df15f7aa0cd8128a4e2bc3f2015656a60ef051ca
Jeff Walden <jwalden@mit.edu>
2009-03-20 15:31:01 -0700

    Bug 483103 - Better fixes for getting the lengths of strings and String objects.  r=brendan

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -4559,6 +4559,9 @@ function testLengthInString()
   var res = "length" in s;
   for (var i = 0; i < 5; i++)
     res = res && ("length" in s);
+  res = res && s.hasOwnProperty("length");
+  for (var i = 0; i < 5; i++)
+    res = res && s.hasOwnProperty("length");
   return res;
 }
 testLengthInString.expected = true;
