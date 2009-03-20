8cf88bb4fd05a63119b863e24214ac751a1ad971
Igor Bukanov <igor@mir2.org>
2009-03-20 12:30:40 +0100

    436700 - fixing backref assert. r=dmandelin

--- js/src/jsregexp.cpp
+++ js/src/jsregexp.cpp
@@ -1266,7 +1266,14 @@ ParseTerm(CompilerState *state)
                 /* Treat this as an octal escape. */
                 goto doOctal;
             }
-            JS_ASSERT(1 <= num && num <= 0x10000);
+
+            /*
+             * When FindParenCount calls the regex parser recursively (to find
+             * the number of backrefs) num can be arbitrary and the maximum
+             * supported number of backrefs does not bound it.
+             */
+            JS_ASSERT_IF(!(state->flags & JSREG_FIND_PAREN_COUNT),
+                         1 <= num && num <= 0x10000);
             state->result = NewRENode(state, REOP_BACKREF);
             if (!state->result)
                 return JS_FALSE;
