d194c8272308f050a81aa1f1a1dae104e9dfd6b6
Marco Bonardo <mbonardo@mozilla.com>
2009-03-23 13:42:03 +0100

    Bug 390614 - Split up the "Older than 6 days" history folder, r=dietrich, pike ui-r=beltzner a=mconnor

--- toolkit/components/places/src/nsNavHistory.h
+++ toolkit/components/places/src/nsNavHistory.h
@@ -62,7 +62,6 @@
 #endif
 #include "nsIBrowserHistory.h"
 #include "nsICollation.h"
-#include "nsIDateTimeFormat.h"
 #include "nsIGlobalHistory.h"
 #include "nsIGlobalHistory3.h"
 #include "nsIDownloadHistory.h"
@@ -242,10 +241,12 @@ public:
    * objects for places components.
    */
   nsIStringBundle* GetBundle();
+  nsIStringBundle* GetDateFormatBundle();
   nsICollation* GetCollation();
   void GetStringFromName(const PRUnichar* aName, nsACString& aResult);
   void GetAgeInDaysString(PRInt32 aInt, const PRUnichar *aName,
                           nsACString& aResult);
+  void GetMonthName(PRInt32 aIndex, nsACString& aResult);
 
   // returns true if history has been disabled
   PRBool IsHistoryDisabled() { return mExpireDaysMax == 0 || InPrivateBrowsingMode(); }
@@ -632,6 +633,7 @@ protected:
 
   // localization
   nsCOMPtr<nsIStringBundle> mBundle;
+  nsCOMPtr<nsIStringBundle> mDateFormatBundle;
   nsCOMPtr<nsICollation> mCollation;
 
   // annotation service : MAY BE NULL!
