4791b7137f0a229f5f0de2fbca9372e79cc4ba08
Peter Van der Beken <peterv@propagandism.org>
2009-03-24 10:43:42 +0100

    Backing out 8f9ff81ef3fa to try to fix orange.

--- parser/htmlparser/src/nsHTMLTokenizer.h
+++ parser/htmlparser/src/nsHTMLTokenizer.h
@@ -70,7 +70,7 @@ public:
   nsHTMLTokenizer(PRInt32 aParseMode = eDTDMode_quirks,
                   eParserDocType aDocType = eHTML_Quirks,
                   eParserCommands aCommand = eViewNormal,
-                  nsIHTMLContentSink* aSink = 0);
+                  PRUint16 aFlags = 0);
   virtual ~nsHTMLTokenizer();
 
 protected:
