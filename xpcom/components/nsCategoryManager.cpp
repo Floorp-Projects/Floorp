a3cb4f1f32d3ab4f285e53d101fb84b67a3380f6
Neil Rashbrook <neil@parkwaycc.co.uk>
2009-03-25 13:19:08 +0000

    Fix allocator abuses: bug 331165 r+sr=bsmedberg; bug 484309 r+sr=bsmedberg; bug 484312 r+sr=biesi

--- xpcom/components/nsCategoryManager.cpp
+++ xpcom/components/nsCategoryManager.cpp
@@ -276,7 +276,7 @@ CategoryNode::GetLeaf(const char* aEntryName,
 
   // we only want the non-persistent value
   if (ent && ent->nonpValue) {
-    *_retval = nsCRT::strdup(ent->nonpValue);
+    *_retval = NS_strdup(ent->nonpValue);
     if (*_retval)
       rv = NS_OK;
   }
