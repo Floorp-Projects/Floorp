c66ba56f60e5472217a9352e9c1e29da5d818ed8
Blake Kaplan <mrbkap@gmail.com>
2009-03-25 13:07:44 -0700

    Bug 485217 - Pop the eval context before returning. r+sr=peterv

--- content/xslt/src/xslt/txKeyFunctionCall.cpp
+++ content/xslt/src/xslt/txKeyFunctionCall.cpp
@@ -395,9 +395,9 @@ nsresult txXSLKey::testNode(const txXPathNode& aNode,
             nsRefPtr<txAExprResult> exprResult;
             rv = mKeys[currKey].useExpr->evaluate(&evalContext,
                                                   getter_AddRefs(exprResult));
-            NS_ENSURE_SUCCESS(rv, rv);
 
             aEs.popEvalContext();
+            NS_ENSURE_SUCCESS(rv, rv);
 
             if (exprResult->getResultType() == txAExprResult::NODESET) {
                 txNodeSet* res = static_cast<txNodeSet*>
