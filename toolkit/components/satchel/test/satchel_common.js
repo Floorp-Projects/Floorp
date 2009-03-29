24024186fd522238d380c6558655e30d7d3257e7
Justin Dolske <dolske@mozilla.com>
2009-03-29 15:29:22 -0700

    Bug 463486 - Form history shouldn't remember the default values of unmodified fields. r=gavin

--- toolkit/components/satchel/test/satchel_common.js
+++ toolkit/components/satchel/test/satchel_common.js
@@ -35,6 +35,36 @@
  *
  * ***** END LICENSE BLOCK ***** */
  
+/*
+ * $_
+ *
+ * Returns the element with the specified |name| attribute.
+ */
+function $_(formNum, name) {
+  var form = document.getElementById("form" + formNum);
+  if (!form) {
+    ok(false, "$_ couldn't find requested form " + formNum);
+    return null;
+  }
+
+  var element = form.elements.namedItem(name);
+  if (!element) {
+    ok(false, "$_ couldn't find requested element " + name);
+    return null;
+  }
+
+  // Note that namedItem is a bit stupid, and will prefer an
+  // |id| attribute over a |name| attribute when looking for
+  // the element.
+
+  if (element.hasAttribute("name") && element.getAttribute("name") != name) {
+    ok(false, "$_ got confused.");
+    return null;
+  }
+
+  return element;
+}
+
 function cleanUpFormHist() {
   netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
   var formhist = Components.classes["@mozilla.org/satchel/form-history;1"].
