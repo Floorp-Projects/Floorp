f156a254b7112995a5f48661c4377f77e8e0f454
Jeff Walden <jwalden@mit.edu>
2009-03-30 17:15:42 -0700

    Bug 485889 - Incorrect null checking/assignment? (with xpcshell test case).  r=gal

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -4740,6 +4740,26 @@ testNewString.jitstats = {
 };
 test(testNewString);
 
+function testWhileObjectOrNull()
+{
+  try
+  {
+    for (var i = 0; i < 3; i++)
+    {
+      var o = { p: { p: null } };
+      while (o.p)
+        o = o.p;
+    }
+    return "pass";
+  }
+  catch (e)
+  {
+    return "threw exception: " + e;
+  }
+}
+testWhileObjectOrNull.expected = "pass";
+test(testWhileObjectOrNull);
+
 
 /*****************************************************************************
  *                                                                           *
