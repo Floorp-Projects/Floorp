5f99ce2bec653812979f71812129b07d364d9407
L. David Baron <dbaron@dbaron.org>
2009-04-02 07:55:50 -0700

    Use a valid value for storing in a bitfield as the style struct ID for backend-only properties.  (Bug 469244)  r+sr=bzbarsky

--- layout/style/nsCSSProps.cpp
+++ layout/style/nsCSSProps.cpp
@@ -1431,8 +1431,10 @@ const nsStyleStructID nsCSSProps::kSIDTable[eCSSProperty_COUNT_no_shorthands] = 
     #define CSS_PROP_SVG(name_, id_, method_, flags_, datastruct_, member_, type_, kwtable_) eStyleStruct_SVG,
     #define CSS_PROP_SVGRESET(name_, id_, method_, flags_, datastruct_, member_, type_, kwtable_) eStyleStruct_SVGReset,
     #define CSS_PROP_COLUMN(name_, id_, method_, flags_, datastruct_, member_, type_, kwtable_) eStyleStruct_Column,
-    // This shouldn't matter, but we need something to go here.
-    #define CSS_PROP_BACKENDONLY(name_, id_, method_, flags_, datastruct_, member_, type_, kwtable_) nsStyleStructID(-1),
+    // Use the special BackendOnly style struct ID (which does need to
+    // be valid for storing in the nsCSSCompressedDataBlock::mStyleBits
+    // bitfield).
+    #define CSS_PROP_BACKENDONLY(name_, id_, method_, flags_, datastruct_, member_, type_, kwtable_) eStyleStruct_BackendOnly,
 
     #include "nsCSSPropList.h"
 
