cb1de7652151137644b72e826ee1a72420b0f417
Matthew Gregan <kinetik@flim.org>
2009-04-04 19:21:15 +1300

    Bug 485291 - Make liboggplay's config.h pick up NSPR's endianness defines.  r=chris.double

--- media/liboggplay/update.sh
+++ media/liboggplay/update.sh
@@ -45,4 +45,5 @@ sed 's/#include <config.h>/#ifdef WIN32\
 rm ./src/liboggplay/oggplay_private.h1
 sed s/\#ifdef\ HAVE_INTTYPES_H/\#if\ HAVE_INTTYPES_H/g $1/src/liboggplay/oggplay_data.c >./src/liboggplay/oggplay_data.c
 patch -p3 < bug485291_yuv_align.patch
+patch -p3 < endian.patch
 
