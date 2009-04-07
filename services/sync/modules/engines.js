9426e0d7b8a0c2b796daff267b32a0c39ba98265
Edward Lee <edilee@mozilla.com>
2009-04-07 16:45:41 -0500

    Bug 487308 - Allow registering of an array of engines Import engines to the Weave global object and use them to register engines, which checks if the arg is an array. To support handling of errors (unused), the engine is returned on register failure.

--- services/sync/modules/engines.js
+++ services/sync/modules/engines.js
@@ -104,8 +104,35 @@ EngineManagerSvc.prototype = {
     }
     return ret;
   },
-  register: function EngMgr_register(engine) {
-    this._engines[engine.name] = engine;
+
+  /**
+   * Register an Engine to the service. Alternatively, give an array of engine
+   * objects to register.
+   *
+   * @param engineObject
+   *        Engine object used to get an instance of the engine
+   * @return The engine object if anything failed
+   */
+  register: function EngMgr_register(engineObject) {
+    if (Utils.isArray(engineObject))
+      return engineObject.map(this.register, this);
+
+    try {
+      let engine = new engineObject();
+      this._engines[engine.name] = engine;
+    }
+    catch(ex) {
+      let mesg = ex.message ? ex.message : ex;
+      let name = engineObject || "";
+      name = name.prototype || "";
+      name = name.name || "";
+
+      let out = "Could not initialize engine '" + name + "': " + mesg;
+      dump(out);
+      this._log.error(out);
+
+      return engineObject;
+    }
   },
   unregister: function EngMgr_unregister(val) {
     let name = val;
