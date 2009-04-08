7080a6b67e7315831adc528c1977aa36bb5cc51a
Olli Pettay <Olli.Pettay@helsinki.fi>
2009-04-08 18:28:52 +0300

    Bug 487345, Crash [@ nsObjectLoadingContent::Instantiate], r+sr=bz

--- content/base/src/nsObjectLoadingContent.cpp
+++ content/base/src/nsObjectLoadingContent.cpp
@@ -134,7 +134,14 @@ nsAsyncInstantiateEvent::Run()
   // Also make sure that we still refer to the same data.
   nsIObjectFrame* frame = mContent->
     GetExistingFrame(nsObjectLoadingContent::eFlushContent);
-  if (mFrame.IsAlive() &&
+#ifdef DEBUG
+  if (frame && mFrame.IsAlive()) {
+    nsIFrame* objectFrame = do_QueryFrame(frame);
+    NS_ASSERTION(objectFrame == mFrame.GetFrame(), "Wrong frame!");
+  }
+#endif
+  if (frame &&
+      mFrame.IsAlive() &&
       mContent->mURI == mURI &&
       mContent->mContentType.Equals(mContentType)) {
     if (LOG_ENABLED()) {
