29ae86be70468bf0bb689390892ea287c369aa40
Robert O'Callahan <robert@ocallahan.org>
2009-04-10 13:15:47 +1200

    Bug 485121. The screen coordinates for an element should be snapped to the nearest screen pixel, not rounded out, for consistency with the way we position native widgets and snap rectangle drawing. r=dbaron

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -3608,7 +3608,7 @@ nsIntRect nsIFrame::GetScreenRectExternal() const
 
 nsIntRect nsIFrame::GetScreenRect() const
 {
-  return nsRect::ToOutsidePixels(GetScreenRectInAppUnits(), PresContext()->AppUnitsPerDevPixel());
+  return nsRect::ToNearestPixels(GetScreenRectInAppUnits(), PresContext()->AppUnitsPerDevPixel());
 }
 
 // virtual
