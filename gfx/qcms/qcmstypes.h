3aa8c0a10cf40b675d4920362f31609ffdd6e3fb
Marco Perez <bugmail@millibyte.net>
2009-04-14 11:08:23 -0400

    Bug 487754 - Problems building qcms on FreeBSD. r=jrmuizel

--- gfx/qcms/qcmstypes.h
+++ gfx/qcms/qcmstypes.h
@@ -17,6 +17,8 @@ typedef PRUint64 uint64_t;
 #ifdef __OS2__
 /* OS/2's stdlib typdefs uintptr_t. So we'll just include that so we don't collide */
 #include <stdlib.h>
+#elif defined(__FreeBSD__)
+/* FreeBSD typedefs uintptr_t in /usr/include/sys/types.h */
 #else
 typedef PRUptrdiff uintptr_t;
 #endif
