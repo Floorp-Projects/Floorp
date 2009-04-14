bd52039335ad5f7cfc403d9b460397facd91ca3d
Doug Tuner <dougt@mozilla.com>
2009-04-13 22:49:06 -0400

    bug 486063 -  AutoDialer support on Windows Mobile doesn't work just quite right r=blassey sr=stuart

--- netwerk/base/src/nsAutodialWinCE.cpp
+++ netwerk/base/src/nsAutodialWinCE.cpp
@@ -48,7 +48,6 @@
 #include "nsIPrefService.h"
 #include "nsIServiceManager.h"
 
-
 // pulled from the header so that we do not get multiple define errors during link
 static const GUID ras_DestNetInternet =
         { 0x436ef144, 0xb4fb, 0x4863, { 0xa0, 0x41, 0x8f, 0x90, 0x5a, 0x62, 0xc5, 0x72 } };
@@ -85,23 +84,11 @@ nsresult nsRASAutodial::DialDefault(const PRUnichar* /* hostName */)
   
   DWORD status;
   HRESULT result = ConnMgrEstablishConnectionSync(&conn_info, 
-						  &connectionHandle, 
-						  60000,
-						  &status);
-  if (result != S_OK)
-    return NS_ERROR_FAILURE;
+                                                  &connectionHandle, 
+                                                  60000,
+                                                  &status);
 
-  PRInt32 defaultCacheTime = 1;    // 1 second according to msdn
-  nsCOMPtr<nsIPrefBranch> prefs = do_GetService(NS_PREFSERVICE_CONTRACTID);
-  if (prefs) {
-    PRInt32 t;
-    if (NS_SUCCEEDED(prefs->GetIntPref("network.autodial.cacheTime", &t)))
-	defaultCacheTime = t;
-  }
-
-  ConnMgrReleaseConnection(connectionHandle, defaultCacheTime);
-  
-  if (status != CONNMGR_STATUS_CONNECTED)
+  if (result != S_OK || status != CONNMGR_STATUS_CONNECTED)
     return NS_ERROR_FAILURE;
 
   return NS_OK;
