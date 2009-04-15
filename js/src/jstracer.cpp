bd4ff09b8015eb320a4c162d464e2f9caea3bd72
Andreas Gal <gal@mozilla.com>
2009-04-14 20:15:43 -0700

    Fix static analysis. It is ok for ComputeThis_tn to see a stale cx->fp since we just want to ensure the global object is wrapped.

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -6239,6 +6239,8 @@ TraceRecorder::unbox_jsval(jsval v, LIns*& v_ins, LIns* exit)
 static JSObject*
 ComputeThis_tn(JSContext* cx)
 {
+    VOUCH_DOES_NOT_REQUIRE_STACK();
+
     return js_ComputeThisForFrame(cx, cx->fp);
 }
 
