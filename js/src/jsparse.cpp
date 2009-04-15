0d2f34c122cc52c58c01402e2d6467706eb0a116
Brendan Eich <brendan@mozilla.org>
2009-04-14 17:20:12 -0700

    Back out bad patch for 488272.

--- js/src/jsparse.cpp
+++ js/src/jsparse.cpp
@@ -4109,12 +4109,11 @@ RebindLets(JSParseNode *pn, JSTreeContext *tc)
                 ForgetUse(pn);
 
                 JSAtomListElement *ale = tc->decls.lookup(pn->pn_atom);
-                while (ale) {
+                while ((ale = ALE_NEXT(ale)) != NULL) {
                     if (ALE_ATOM(ale) == pn->pn_atom) {
                         LinkUseToDef(pn, ALE_DEFN(ale), tc);
                         return true;
                     }
-                    ale = ALE_NEXT(ale);
                 }
 
                 ale = tc->upvars.lookup(pn->pn_atom);
