dede57f995178a4bda5d1e28ca0b0c74274b1b9e
Brad Lassey <blassey@mozilla.com>
2009-04-16 12:03:18 -0400

    Bug 427987 - Add support for Hildon (Maemo) tap-n-hold context menu, r=ted

--- content/events/src/nsEventStateManager.h
+++ content/events/src/nsEventStateManager.h
@@ -65,7 +65,7 @@ class nsDOMDataTransfer;
 // mac uses click-hold context menus, a holdover from 4.x
 // touch screens (like hildon) could use this also, 
 // perhaps we should move to NS_TOUCHSCREEN
-#if defined(XP_MACOSX) || defined(NS_HILDON)
+#if defined(XP_MACOSX) || defined(MOZ_PLATFORM_HILDON)
 #define CLICK_HOLD_CONTEXT_MENUS 1
 #endif
 
