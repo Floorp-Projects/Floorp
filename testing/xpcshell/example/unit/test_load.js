ceea2583e61b608414a113ecacc77c8f09a94cd8
Ted Mielczarek <ted.mielczarek@gmail.com>
2009-04-20 06:56:23 -0400

    bug 439110 - xpcshell's load() just silently fails for non-existent files - add automated test

--- testing/xpcshell/example/unit/test_load.js
+++ testing/xpcshell/example/unit/test_load.js
@@ -42,4 +42,13 @@ var subscriptLoaded = false;
 function run_test() {
   load("load_subscript.js");
   do_check_true(subscriptLoaded);
+  subscriptLoaded = false;
+  try {
+    load("file_that_does_not_exist.js");
+    subscriptLoaded = true;
+  }
+  catch (ex) {
+    do_check_eq(ex.message.substring(0,16), "cannot open file");
+  }
+  do_check_false(subscriptLoaded, "load() should throw an error");
 }
