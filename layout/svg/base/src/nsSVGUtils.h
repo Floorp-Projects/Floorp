8c7b34405538ffe12c7b95011d9aae1f503c8b30
Jonathan Watt <jwatt@jwatt.org>
2009-04-25 01:17:43 +0200

    Bug 463939. When an SVG element's geometry changes and it has an ancestor with a filter  effect, invalidate the nearest SVG viewport ancestor because we don't have a good way to compute precise invalidation bounds. r=roc

--- layout/svg/base/src/nsSVGUtils.h
+++ layout/svg/base/src/nsSVGUtils.h
@@ -79,6 +79,7 @@ struct nsStyleFont;
 class nsSVGEnum;
 class nsISVGChildFrame;
 class nsSVGGeometryFrame;
+class nsSVGDisplayContainerFrame;
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -255,6 +256,13 @@ public:
    */
   static nsresult GetNearestViewportElement(nsIContent *aContent,
                                             nsIDOMSVGElement * *aNearestViewportElement);
+
+  /**
+   * Gets the nearest nsSVGInnerSVGFrame or nsSVGOuterSVGFrame frame. aFrame
+   * must be an SVG frame. If aFrame is of type nsGkAtoms::svgOuterSVGFrame,
+   * returns nsnull.
+   */
+  static nsSVGDisplayContainerFrame* GetNearestSVGViewport(nsIFrame *aFrame);
 
   /*
    * Get the farthest viewport element
