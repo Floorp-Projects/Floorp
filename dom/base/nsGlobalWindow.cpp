88e212051f45f4160be900c65da4d63722489050
Olli Pettay <Olli.Pettay@helsinki.fi>
2009-04-25 02:53:25 +0300

    Bug 489812 -  Suspending timeouts doesn't clear mFiringDepth, r=bent, sr=jst

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -7656,7 +7656,7 @@ nsGlobalWindow::RunTimeout(nsTimeout *aTimeout)
   mTimeoutInsertionPoint = &dummy_timeout;
 
   for (timeout = FirstTimeout();
-       timeout != &dummy_timeout && !IsFrozen() && !mTimeoutsSuspendDepth;
+       timeout != &dummy_timeout && !IsFrozen();
        timeout = nextTimeout) {
     nextTimeout = timeout->Next();
 
@@ -7664,6 +7664,13 @@ nsGlobalWindow::RunTimeout(nsTimeout *aTimeout)
       // We skip the timeout since it's on the list to run at another
       // depth.
 
+      continue;
+    }
+
+    if (mTimeoutsSuspendDepth) {
+      // Some timer did suspend us. Make sure the
+      // rest of the timers get executed later.
+      timeout->mFiringDepth = 0;
       continue;
     }
 
