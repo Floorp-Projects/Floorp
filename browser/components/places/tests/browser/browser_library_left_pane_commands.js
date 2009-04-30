4f1fae16adfa06445269804ef8f563517c16a198
Marco Bonardo <mbonardo@mozilla.com>
2009-04-30 09:52:49 -0700

    Bug 490156 - Can't delete smart bookmark containers, r=adw

--- browser/components/places/tests/browser/browser_library_left_pane_commands.js
+++ browser/components/places/tests/browser/browser_library_left_pane_commands.js
@@ -102,10 +102,68 @@ gTests.push({
 
 //------------------------------------------------------------------------------
 
+gTests.push({
+  desc: "Bug 490156 - Can't delete smart bookmark containers",
+  run: function() {
+    // Select and open the left pane "Bookmarks Toolbar" folder.
+    var PO = gLibrary.PlacesOrganizer;
+    PO.selectLeftPaneQuery('BookmarksToolbar');
+    isnot(PO._places.selectedNode, null, "We have a valid selection");
+    is(PlacesUtils.getConcreteItemId(PO._places.selectedNode),
+       PlacesUtils.toolbarFolderId,
+       "We have correctly selected bookmarks toolbar node.");
+
+    // Check that both cut and delete commands are disabled.
+    ok(!PO._places.controller.isCommandEnabled("cmd_cut"),
+       "Cut command is disabled");
+    ok(!PO._places.controller.isCommandEnabled("cmd_delete"),
+       "Delete command is disabled");
+
+    var toolbarNode = PO._places.selectedNode
+                        .QueryInterface(Ci.nsINavHistoryContainerResultNode);
+    toolbarNode.containerOpen = true;
+
+    // Add an History query to the toolbar.
+    PlacesUtils.bookmarks.insertBookmark(PlacesUtils.toolbarFolderId,
+                                         PlacesUtils._uri("place:sort=4"),
+                                         0, // Insert at start.
+                                         "special_query");
+    // Get first child and check it is the "Most Visited" smart bookmark.
+    ok(toolbarNode.childCount > 0, "Toolbar node has children");
+    var queryNode = toolbarNode.getChild(0);
+    is(queryNode.title, "special_query", "Query node is correctly selected");
+
+    // Select query node.
+    PO._places.selectNode(queryNode);
+    is(PO._places.selectedNode, queryNode, "We correctly selected query node");
+
+    // Check that both cut and delete commands are enabled.
+    ok(PO._places.controller.isCommandEnabled("cmd_cut"),
+       "Cut command is enabled");
+    ok(PO._places.controller.isCommandEnabled("cmd_delete"),
+       "Delete command is enabled");
+
+    // Execute the delete command and check bookmark has been removed.
+    PO._places.controller.doCommand("cmd_delete");
+    try {
+      PlacesUtils.bookmarks.getFolderIdForItem(queryNode.itemId);  
+      ok(false, "Unable to remove query node bookmark");
+    } catch(ex) {
+      ok(true, "Query node bookmark has been correctly removed");
+    }
+
+    toolbarNode.containerOpen = false;
+    nextTest();
+  }
+});
+
+//------------------------------------------------------------------------------
+
 function nextTest() {
   if (gTests.length) {
     var test = gTests.shift();
     ok(true, "TEST: " + test.desc);
+    dump("TEST: " + test.desc + "\n");
     test.run();
   }
   else {
@@ -136,6 +194,7 @@ var windowObserver = {
 };
 
 function test() {
+  dump("Starting test browser_library_left_pane_commands.js\n");
   waitForExplicitFinish();
   // Sanity checks.
   ok(PlacesUtils, "PlacesUtils is running in chrome context");
