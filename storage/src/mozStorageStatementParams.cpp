65c9a0c76c432ca5867d5bf606a7fe55349879e2
Shawn Wilsher <me@shawnwilsher.com>
2009-05-08 19:57:37 -0700

    Backed out changeset b6f09258a505 (bug 490833).

--- storage/src/mozStorageStatementParams.cpp
+++ storage/src/mozStorageStatementParams.cpp
@@ -91,8 +91,9 @@ StatementParams::SetProperty(nsIXPConnectWrappedNative *aWrapper,
   }
   else if (JSVAL_IS_STRING(aId)) {
     JSString *str = JSVAL_TO_STRING(aId);
-    NS_ConvertUTF16toUTF8 name(::JS_GetStringChars(str),
-                               ::JS_GetStringLength(str));
+    nsCAutoString name(":");
+    name.Append(NS_ConvertUTF16toUTF8(::JS_GetStringChars(str),
+                                      ::JS_GetStringLength(str)));
 
     // check to see if there's a parameter with this name
     PRUint32 index;
@@ -205,9 +206,11 @@ StatementParams::NewResolve(nsIXPConnectWrappedNative *aWrapper,
     jschar *nameChars = ::JS_GetStringChars(str);
     size_t nameLength = ::JS_GetStringLength(str);
 
+    nsCAutoString name(":");
+    name.Append(NS_ConvertUTF16toUTF8(nameChars, nameLength));
+
     // Check to see if there's a parameter with this name, and if not, let
     // the rest of the prototype chain be checked.
-    NS_ConvertUTF16toUTF8 name(nameChars, nameLength);
     nsresult rv = mStatement->GetParameterIndex(name, &idx);
     if (NS_FAILED(rv))
       return NS_OK;
