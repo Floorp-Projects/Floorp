464dfe6ce6a55fa486361b5368424c3c9d67b5b2
Marco Bonardo <mbonardo@mozilla.com>
2009-05-11 11:05:56 +0200

    Bug 491954 - Views should select exact columns, r=sdwilsh

--- toolkit/components/places/src/nsFaviconService.cpp
+++ toolkit/components/places/src/nsFaviconService.cpp
@@ -68,6 +68,7 @@
 #include "mozIStoragePendingStatement.h"
 #include "mozIStorageStatementCallback.h"
 #include "mozIStorageError.h"
+#include "nsPlacesTables.h"
 
 // For favicon optimization
 #include "imgITools.h"
@@ -184,10 +185,10 @@ nsFaviconService::Init()
   rv = mDBConn->CreateStatement(NS_LITERAL_CSTRING(
       "SELECT f.id, f.url, length(f.data), f.expiration "
       "FROM ( "
-        "SELECT * FROM moz_places_temp "
+        "SELECT " MOZ_PLACES_COLUMNS " FROM moz_places_temp "
         "WHERE url = ?1 "
         "UNION ALL "
-        "SELECT * FROM moz_places "
+        "SELECT " MOZ_PLACES_COLUMNS " FROM moz_places "
         "WHERE url = ?1 "
       ") AS h JOIN moz_favicons f ON h.favicon_id = f.id "
       "LIMIT 1"),
