71d45d670dc047712b1ea1fa5ff7f292e00e596c
Serge Gautherie <sgautherie.bz@free.fr>
2009-05-11 17:11:50 +0200

    Bug 464707 - port upload build target to comm-central apps (Bv1-MC) 1 s/topsrcdir/MOZILLA_DIR/

--- toolkit/mozapps/installer/packager.mk
+++ toolkit/mozapps/installer/packager.mk
@@ -505,7 +505,7 @@ space = $(empty) $(empty)
 QUOTED_WILDCARD = $(if $(wildcard $(subst $(space),?,$(1))),"$(1)")
 
 upload:
-	$(PYTHON) $(topsrcdir)/build/upload.py --base-path $(DIST) \
+	$(PYTHON) $(MOZILLA_DIR)/build/upload.py --base-path $(DIST) \
 		$(call QUOTED_WILDCARD,$(DIST)/$(PACKAGE)) \
 		$(call QUOTED_WILDCARD,$(INSTALLER_PACKAGE)) \
 		$(call QUOTED_WILDCARD,$(DIST)/$(COMPLETE_MAR)) \
