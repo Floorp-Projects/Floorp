bd6823b10e9d7e30c7f49478d6f4c7d29e057fb6
Jeff Muizelaar <jmuizelaar@mozilla.com>
2009-05-12 15:25:05 -0400

    Bug 487305 - create video surface element with RGB24 colour format instead of ARGB32. r=joe

--- content/media/video/src/nsMediaDecoder.cpp
+++ content/media/video/src/nsMediaDecoder.cpp
@@ -227,7 +227,7 @@ void nsMediaDecoder::Paint(gfxContext* aContext,
     new gfxImageSurface(mRGB,
                         gfxIntSize(mRGBWidth, mRGBHeight), 
                         mRGBWidth * 4,
-                        gfxASurface::ImageFormatARGB32);    
+                        gfxASurface::ImageFormatRGB24);    
 
   if (!surface)
     return;
