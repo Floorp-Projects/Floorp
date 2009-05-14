97376f0f1fa9199dbff7a0f79599bda48597d800
Paul O’Shannessy <paul@oshannessy.com>
2009-05-14 15:52:51 -0700

    Bug 490040 - Reattaching a lone tab into another window causes an empty window to be added to Recently Closed Windows, r=zeniko

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -509,7 +509,9 @@ SessionStoreService.prototype = {
           this.onTabAdd(aEvent.currentTarget.ownerDocument.defaultView, tabpanel);
         }
         else {
-          this.onTabClose(aEvent.currentTarget.ownerDocument.defaultView, aEvent.originalTarget);
+          // aEvent.detail determines if the tab was closed by moving to a different window
+          if (!aEvent.detail)
+            this.onTabClose(aEvent.currentTarget.ownerDocument.defaultView, aEvent.originalTarget);
           this.onTabRemove(aEvent.currentTarget.ownerDocument.defaultView, tabpanel);
         }
         break;
@@ -638,9 +640,12 @@ SessionStoreService.prototype = {
         this._updateCookies([winData]);
       }
       
-      // store closed-window data for undo
-      this._closedWindows.unshift(winData);
-      this._capClosedWindows();
+      // save the window if it has multiple tabs or a single tab with entries
+      if (winData.tabs.length > 1 ||
+          (winData.tabs.length == 1 && winData.tabs[0].entries.length > 0)) {
+        this._closedWindows.unshift(winData);
+        this._capClosedWindows();
+      }
       
       // clear this window from the list
       delete this._windows[aWindow.__SSi];
