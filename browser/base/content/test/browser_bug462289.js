d96f99a627bc5ee4d798b846e3907a2d54761267
Dão Gottwald <dao@mozilla.com>
2009-05-14 20:35:40 +0200

    test and additional fix for bug 482801

--- browser/base/content/test/browser_bug462289.js
+++ browser/base/content/test/browser_bug462289.js
@@ -22,23 +22,32 @@ function step3()
 {
   isnot(document.activeElement, tab1, "mouse on tab again activeElement");
 
-  document.getElementById("searchbar").focus();
-  EventUtils.synthesizeKey("VK_TAB", { });
-  is(document.activeElement, tab1, "tab key to tab activeElement");
-
-  EventUtils.synthesizeMouse(tab1, 2, 2, {});
+  EventUtils.synthesizeMouse(tab1, 2, 2, {button: 1, type: "mousedown"});
+  EventUtils.synthesizeMouse(tab1, 0, 0, {button: 1, type: "mouseout"});
   setTimeout(step4, 0);
 }
 
 function step4()
 {
-  is(document.activeElement, tab1, "mouse on tab while focused still activeElement");
+  isnot(document.activeElement, tab1, "tab not focused via middle click");
 
-  EventUtils.synthesizeMouse(tab2, 2, 2, {});
+  document.getElementById("searchbar").focus();
+  EventUtils.synthesizeKey("VK_TAB", { });
+  is(document.activeElement, tab1, "tab key to tab activeElement");
+
+  EventUtils.synthesizeMouse(tab1, 2, 2, {});
   setTimeout(step5, 0);
 }
 
 function step5()
+{
+  is(document.activeElement, tab1, "mouse on tab while focused still activeElement");
+
+  EventUtils.synthesizeMouse(tab2, 2, 2, {});
+  setTimeout(step6, 0);
+}
+
+function step6()
 {
   // The tabbox selects a tab within a setTimeout in a bubbling mousedown event
   // listener, and focuses the current tab if another tab previously had focus
