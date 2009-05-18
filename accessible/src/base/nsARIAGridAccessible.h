29445a98cfb562bf02cd7d67864a75176524fc92
Alexander Surkov <surkov.alexander@gmail.com>
2009-05-18 16:35:21 +0800

    Bug 386813 - Support table interfaces for grid/treegrid when no HTML table undeneath, r=davdib, marcoz, sr=neil

---
+++ accessible/src/base/nsARIAGridAccessible.h
@@ -0,0 +1,82 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * Mozilla Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Alexander Surkov <surkov.alexander@gmail.com> (original author)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#ifndef nsARIAGridAccessible_h_
+#define nsARIAGridAccessible_h_
+
+#include "nsIAccessibleTable.h"
+
+#include "nsHyperTextAccessibleWrap.h"
+
+/**
+ * Accessible for ARIA grid and treegrid.
+ */
+class nsARIAGridAccessible : public nsAccessibleWrap,
+                             public nsIAccessibleTable
+{
+public:
+  nsARIAGridAccessible(nsIDOMNode* aDOMNode, nsIWeakReference* aShell);
+
+  // nsISupports
+  NS_DECL_ISUPPORTS_INHERITED
+
+  // nsIAccessibleTable
+  NS_DECL_NSIACCESSIBLETABLE
+
+protected:
+  PRBool IsValidRow(PRInt32 aRow);
+  PRBool IsValidColumn(PRInt32 aColumn);
+  PRBool IsValidRowNColumn(PRInt32 aRow, PRInt32 aColumn);
+};
+
+/**
+ * Accessible for ARIA gridcell and rowheader/columnheader.
+ */
+class nsARIAGridCellAccessible : public nsHyperTextAccessibleWrap
+{
+public:
+  nsARIAGridCellAccessible(nsIDOMNode* aDOMNode, nsIWeakReference* aShell);
+
+  // nsISupports
+  NS_DECL_ISUPPORTS_INHERITED
+
+  // nsAccessible
+  virtual nsresult GetAttributesInternal(nsIPersistentProperties *aAttributes);
+};
+
+#endif
