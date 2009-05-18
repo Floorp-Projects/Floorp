fc6c9fa7989ec03b2bd7454517aabc7cc5390da2
Robert O'Callahan <robert@ocallahan.org>
2009-05-19 11:07:19 +1200

    Bug 480058. Handle non-square Theora pixel-aspect-ratio property. r=doublec
    --HG--
    extra : rebase_source : 762e10566ae85d0f73d7080ccb090533d50bfe66

--- content/media/video/public/nsMediaDecoder.h
+++ content/media/video/public/nsMediaDecoder.h
@@ -244,12 +244,14 @@ protected:
   // Stop progress information timer.
   nsresult StopProgress();
 
-  // Set the RGB width, height and framerate. Ownership of the passed RGB
-  // buffer is transferred to the decoder.  This is the only nsMediaDecoder
-  // method that may be called from threads other than the main thread.
+  // Set the RGB width, height, pixel aspect ratio, and framerate.
+  // Ownership of the passed RGB buffer is transferred to the decoder.
+  // This is the only nsMediaDecoder method that may be called from
+  // threads other than the main thread.
   void SetRGBData(PRInt32 aWidth,
                   PRInt32 aHeight,
                   float aFramerate,
+                  float aAspectRatio,
                   unsigned char* aRGBBuffer);
 
 protected:
@@ -294,6 +296,9 @@ protected:
   // Framerate of video being displayed in the element
   // expressed in numbers of frames per second.
   float mFramerate;
+
+  // Pixel aspect ratio (ratio of the pixel width to pixel height)
+  float mAspectRatio;
 
   // Has our size changed since the last repaint?
   PRPackedBool mSizeChanged;
