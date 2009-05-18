2a3bc2dc98160a8d69386d2655b00140c13eaf4b
Robert O'Callahan <robert@ocallahan.org>
2009-05-19 11:07:19 +1200

    Bug 480058. Handle non-square Theora pixel-aspect-ratio property. r=doublec
    --HG--
    extra : rebase_source : 0a43b053b52a9b1e300f7f516517722c44040992

--- content/media/video/src/nsOggDecoder.cpp
+++ content/media/video/src/nsOggDecoder.cpp
@@ -473,6 +473,7 @@ private:
   // They are only accessed from the decoder thread.
   PRInt32 mVideoTrack;
   float   mFramerate;
+  float   mAspectRatio;
 
   // Audio data. These are initially set when the metadata is loaded.
   // They are only accessed from the decoder thread.
@@ -660,6 +661,7 @@ nsOggDecodeStateMachine::nsOggDecodeStateMachine(nsOggDecoder* aDecoder) :
   mCallbackPeriod(1.0),
   mVideoTrack(-1),
   mFramerate(0.0),
+  mAspectRatio(1.0),
   mAudioRate(0),
   mAudioChannels(0),
   mAudioTrack(-1),
@@ -933,7 +935,8 @@ void nsOggDecodeStateMachine::PlayVideo(FrameData* aFrame)
 
     oggplay_yuv2bgra(&yuv, &rgb);
 
-    mDecoder->SetRGBData(aFrame->mVideoWidth, aFrame->mVideoHeight, mFramerate, buffer.forget());
+    mDecoder->SetRGBData(aFrame->mVideoWidth, aFrame->mVideoHeight,
+                         mFramerate, mAspectRatio, buffer.forget());
   }
 }
 
@@ -1550,10 +1553,18 @@ void nsOggDecodeStateMachine::LoadOggHeaders(nsChannelReader* aReader)
         mCallbackPeriod = 1.0 / mFramerate;
         LOG(PR_LOG_DEBUG, ("Frame rate: %f", mFramerate));
 
+        int aspectd, aspectn;
+        // this can return E_OGGPLAY_UNINITIALIZED if the video has
+        // no aspect ratio data. We assume 1.0 in that case.
+        OggPlayErrorCode r =
+          oggplay_get_video_aspect_ratio(mPlayer, i, &aspectd, &aspectn);
+        mAspectRatio = r == E_OGGPLAY_OK && aspectd > 0 ?
+            float(aspectn)/float(aspectd) : 1.0;
+
         int y_width;
         int y_height;
         oggplay_get_video_y_size(mPlayer, i, &y_width, &y_height);
-        mDecoder->SetRGBData(y_width, y_height, mFramerate, nsnull);
+        mDecoder->SetRGBData(y_width, y_height, mFramerate, mAspectRatio, nsnull);
       }
       else if (mAudioTrack == -1 && oggplay_get_track_type(mPlayer, i) == OGGZ_CONTENT_VORBIS) {
         mAudioTrack = i;
