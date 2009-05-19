4cfc33e7adcdc2a5b596f434fa7bc22db9d75a09
Robert Strong <robert.bugzilla@gmail.com>
2009-05-19 13:24:26 -0700

    Bug 486275 - active-update.xml is not saved after a channel change which leaves behind cruft. r=dtownsend

---
+++ toolkit/mozapps/update/test/unit/test_0063_manager.js
@@ -0,0 +1,78 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the Application Update Service.
+ *
+ * The Initial Developer of the Original Code is
+ * Robert Strong <robert.bugzilla@gmail.com>.
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Mozilla Foundation <http://www.mozilla.org/>. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+
+/* General Update Manager Tests */
+
+function run_test() {
+  dump("Testing: removing an active update for a channel that is not valid" +
+       "due to switching channels - bug 486275\n");
+  removeUpdateDirsAndFiles();
+  var defaults = getPrefBranch().QueryInterface(AUS_Ci.nsIPrefService).
+                 getDefaultBranch(null);
+  defaults.setCharPref("app.update.channel", "original_channel");
+
+  var patches = getLocalPatchString(null, null, null, null, null, null,
+                                    STATE_FAILED);
+  var updates = getLocalUpdateString(patches, "Existing", null, "3.0", "3.0",
+                                     "3.0", null, null, null, null, null,
+                                     getString("patchApplyFailure"));
+  writeUpdatesToXMLFile(getLocalUpdatesXMLString(updates), false);
+
+  patches = getLocalPatchString(null, null, null, null, null, null,
+                                STATE_DOWNLOADING);
+  updates = getLocalUpdateString(patches, null, null, "1.0", null, "1.0", null,
+                                 null, null, URL_HOST + DIR_DATA + "/empty.mar");
+  writeUpdatesToXMLFile(getLocalUpdatesXMLString(updates), true);
+  writeStatusFile(STATE_DOWNLOADING);
+
+  startAUS();
+  startUpdateManager();
+
+  do_check_eq(gUpdateManager.updateCount, 1);
+  var update = gUpdateManager.getUpdateAt(0);
+  do_check_eq(update.name, "Existing");
+
+  do_check_eq(gUpdateManager.activeUpdate, null);
+  // Verify that the active-update.xml file has had the update from the old
+  // channel removed.
+  var file = getCurrentProcessDir();
+  file.append(FILE_UPDATE_ACTIVE);
+  dump("Testing: verifying contents of " + FILE_UPDATE_ACTIVE + "\n");
+  do_check_eq(readFile(file), getLocalUpdatesXMLString(""));
+  cleanUp();
+}
