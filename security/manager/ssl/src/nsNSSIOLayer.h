ae6d153f0d6baf7e358e8ad6e144ac41e8b6cca5
Kai Engert <kaie@kuix.de>
2009-05-21 00:21:51 +0200

    Bug 431819, IMAP/POP/SMTP/LDAP with SSL client auth, Thunderbird repeatedly prompts for client certificate (applies to firefox with SSL client auth, too) r=relyea for an earlier patch that was checked in to mozilla-1.8.x more than a year ago r=honzab on the diff on top of that earlier patch a=beltzner for landing on restricted trunk

--- security/manager/ssl/src/nsNSSIOLayer.h
+++ security/manager/ssl/src/nsNSSIOLayer.h
@@ -55,6 +55,7 @@
 #include "nsIAssociatedContentSecurity.h"
 #include "nsXPIDLString.h"
 #include "nsNSSShutDown.h"
+#include "nsIClientAuthDialogs.h"
 #include "nsAutoPtr.h"
 #include "nsNSSCertificate.h"
 #include "nsDataHashtable.h"
@@ -133,6 +134,7 @@ class nsNSSSocketInfo : public nsITransportSecurityInfo,
                         public nsIAssociatedContentSecurity,
                         public nsISerializable,
                         public nsIClassInfo,
+                        public nsIClientAuthUserDecision,
                         public nsNSSShutDownObject,
                         public nsOnPK11LogoutCancelObject
 {
@@ -149,6 +151,7 @@ public:
   NS_DECL_NSIASSOCIATEDCONTENTSECURITY
   NS_DECL_NSISERIALIZABLE
   NS_DECL_NSICLASSINFO
+  NS_DECL_NSICLIENTAUTHUSERDECISION
 
   nsresult SetSecurityState(PRUint32 aState);
   nsresult SetShortSecurityDescription(const PRUnichar *aText);
@@ -221,6 +224,7 @@ protected:
   PRPackedBool mHasCleartextPhase;
   PRPackedBool mHandshakeInProgress;
   PRPackedBool mAllowTLSIntoleranceTimeout;
+  PRPackedBool mRememberClientAuthCertificate;
   PRIntervalTime mHandshakeStartTime;
   PRInt32 mPort;
   nsXPIDLCString mHostName;
