65ea364284a6f1f26eb46b1df483dd49c91f6af5
Asaf Romano <aromano@mozilla.com>
2009-05-20 03:13:00 +0300

    Bug 487263 - dragging a tab up or over the tab bar then out of the window does not detach. r=dietrich,mconnor.

--- toolkit/content/nsDragAndDrop.js
+++ toolkit/content/nsDragAndDrop.js
@@ -295,6 +295,7 @@ var transferUtils = {
     switch (flavour) {
       case "text/unicode":
       case "text/plain":
+      case "text/x-moz-text-internal":
         return aData.replace(/^\s+|\s+$/g, "");
       case "text/x-moz-url":
         return ((aData instanceof Components.interfaces.nsISupportsString) ? aData.toString() : aData).split("\n")[0];
