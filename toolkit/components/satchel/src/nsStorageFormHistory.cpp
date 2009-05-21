4daead73c571b3df306ae16e380d45f079772f00
Matt Noorenberghe <mnoorenberghe@mozilla.com>
2009-05-20 20:22:38 -0700

    Bug 492701 - form history should cap the number of fields saved per form submission. r=dolske, a192=beltzner

--- toolkit/components/satchel/src/nsStorageFormHistory.cpp
+++ toolkit/components/satchel/src/nsStorageFormHistory.cpp
@@ -75,6 +75,8 @@
 // Limit the length of names and values stored in form history
 #define MAX_HISTORY_NAME_LEN    200
 #define MAX_HISTORY_VALUE_LEN   200
+// Limit the number of fields saved in a form
+#define MAX_FIELDS_SAVED        100
 
 // nsFormHistoryResult is a specialized autocomplete result class that knows
 // how to remove entries from the form history table.
@@ -480,6 +482,7 @@ nsFormHistory::Notify(nsIDOMHTMLFormElement* formElt, nsIDOMWindowInternal* aWin
   nsCOMPtr<nsIDOMHTMLCollection> elts;
   formElt->GetElements(getter_AddRefs(elts));
 
+  PRUint32 savedCount = 0;
   PRUint32 length;
   elts->GetLength(&length);
   if (length == 0)
@@ -524,6 +527,8 @@ nsFormHistory::Notify(nsIDOMHTMLFormElement* formElt, nsIDOMWindowInternal* aWin
           if (name.Length() > MAX_HISTORY_NAME_LEN ||
               value.Length() > MAX_HISTORY_VALUE_LEN)
             continue;
+          if (savedCount++ >= MAX_FIELDS_SAVED)
+            break;
           AddEntry(name, value);
         }
       }
