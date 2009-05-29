fbed1ee4785643479d311d9e96639feecf95e9e6
Frédéric Wang <fred.wang@free.fr>
2009-05-29 13:08:35 +0200

    b=297467 menclose r=karlt rs=roc

---
+++ layout/mathml/nsMathMLmencloseFrame.h
@@ -0,0 +1,158 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla MathML Project.
+ *
+ * The Initial Developer of the Original Code is
+ * The University Of Queensland.
+ * Portions created by the Initial Developer are Copyright (C) 1999
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Roger B. Sidje <rbs@maths.uq.edu.au>
+ *   David J. Fiddes <D.J.Fiddes@hw.ac.uk>
+ *   Vilya Harvey <vilya@nag.co.uk>
+ *   Shyjan Mahamud <mahamud@cs.cmu.edu>
+ *   Frederic Wang <fred.wang@free.fr> - extension of <msqrt/> to <menclose/>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+
+#ifndef nsMathMLmencloseFrame_h___
+#define nsMathMLmencloseFrame_h___
+
+#include "nsCOMPtr.h"
+#include "nsMathMLContainerFrame.h"
+
+//
+// <menclose> -- enclose content with a stretching symbol such
+// as a long division sign.
+//
+
+/*
+  The MathML REC describes:
+
+  The menclose element renders its content inside the enclosing notation
+  specified by its notation attribute. menclose accepts any number of arguments;
+  if this number is not 1, its contents are treated as a single "inferred mrow"
+  containing its arguments, as described in Section 3.1.3 Required Arguments. 
+*/
+
+enum nsMencloseNotation
+  {
+    NOTATION_LONGDIV = 0x1,
+    NOTATION_RADICAL = 0x2,
+    NOTATION_ROUNDEDBOX = 0x4,
+    NOTATION_CIRCLE = 0x8,
+    NOTATION_LEFT = 0x10,
+    NOTATION_RIGHT = 0x20,
+    NOTATION_TOP = 0x40,
+    NOTATION_BOTTOM = 0x80,
+    NOTATION_UPDIAGONALSTRIKE = 0x100,
+    NOTATION_DOWNDIAGONALSTRIKE = 0x200,
+    NOTATION_VERTICALSTRIKE = 0x400,
+    NOTATION_HORIZONTALSTRIKE = 0x800
+    // NOTATION_MADRUWB = 0x1000
+  };
+
+class nsMathMLmencloseFrame : public nsMathMLContainerFrame {
+public:
+  friend nsIFrame* NS_NewMathMLmencloseFrame(nsIPresShell*   aPresShell,
+                                             nsStyleContext* aContext);
+  
+  virtual nsresult
+  Place(nsIRenderingContext& aRenderingContext,
+        PRBool               aPlaceOrigin,
+        nsHTMLReflowMetrics& aDesiredSize);
+  
+  virtual nsresult
+  MeasureForWidth(nsIRenderingContext& aRenderingContext,
+                  nsHTMLReflowMetrics& aDesiredSize);
+  
+  NS_IMETHOD
+  AttributeChanged(PRInt32         aNameSpaceID,
+                   nsIAtom*        aAttribute,
+                   PRInt32         aModType);
+  
+  virtual void
+  SetAdditionalStyleContext(PRInt32          aIndex, 
+                            nsStyleContext*  aStyleContext);
+  virtual nsStyleContext*
+  GetAdditionalStyleContext(PRInt32 aIndex) const;
+
+  NS_IMETHOD
+  Init(nsIContent* aContent,
+       nsIFrame*   aParent,
+       nsIFrame*   aPrevInFlow);
+
+  NS_IMETHOD BuildDisplayList(nsDisplayListBuilder*   aBuilder,
+                              const nsRect&           aDirtyRect,
+                              const nsDisplayListSet& aLists);
+
+  NS_IMETHOD
+  InheritAutomaticData(nsIFrame* aParent);
+
+  NS_IMETHOD
+  TransmitAutomaticData();
+
+  virtual nscoord
+  FixInterFrameSpacing(nsHTMLReflowMetrics& aDesiredSize);
+
+protected:
+  nsMathMLmencloseFrame(nsStyleContext* aContext);
+  virtual ~nsMathMLmencloseFrame();
+
+  nsresult PlaceInternal(nsIRenderingContext& aRenderingContext,
+                         PRBool               aPlaceOrigin,
+                         nsHTMLReflowMetrics& aDesiredSize,
+                         PRBool               aWidthOnly);
+  
+  // functions to parse the "notation" attribute.
+  nsresult AddNotation(const nsAString& aNotation);
+  void InitNotations();
+
+  // Description of the notations to draw
+  PRUint32 mNotationsToDraw;
+  PRBool IsToDraw(nsMencloseNotation mask)
+  {
+    return mask & mNotationsToDraw;
+  }
+
+  nscoord mRuleThickness;
+  nsTArray<nsMathMLChar> mMathMLChar;
+  PRInt8 mLongDivCharIndex, mRadicalCharIndex;
+  nscoord mContentWidth;
+  nsresult AllocateMathMLChar(nsMencloseNotation mask);
+
+  // Display a frame of the specified type.
+  // @param aType Type of frame to display
+  nsresult DisplayNotation(nsDisplayListBuilder* aBuilder,
+                           nsIFrame* aFrame, const nsRect& aRect,
+                           const nsDisplayListSet& aLists,
+                           nscoord aThickness, nsMencloseNotation aType);
+};
+
+#endif /* nsMathMLmencloseFrame_h___ */
