250f8ee3f8b88d36681c7d20d1e27841f74ea3fb
Boris Zbarsky <bzbarsky@mit.edu>
2009-05-28 20:41:27 -0400

    Bug 495274.  Fix broken selector cloning.  r+sr=dbaron

--- layout/style/nsCSSStyleRule.cpp
+++ layout/style/nsCSSStyleRule.cpp
@@ -280,6 +280,7 @@ nsCSSSelector::Clone(PRBool aDeepNext, PRBool aDeepNegations) const
 
   result->mNameSpace = mNameSpace;
   result->mTag = mTag;
+  result->mOperator = mOperator;
   
   NS_IF_CLONE(mIDList);
   NS_IF_CLONE(mClassList);
