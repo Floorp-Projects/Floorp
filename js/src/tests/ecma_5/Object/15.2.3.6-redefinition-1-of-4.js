42d5d4d10d0e799cb3fd4805b023faf736637269
Jeff Walden <jwalden@mit.edu>
2009-06-05 12:56:45 -0700

    Bug 430133 - Implement ES3.1's Object.defineProperty and Object.defineProperties.  r=jorendorff

---
+++ js/src/tests/ecma_5/Object/15.2.3.6-redefinition-1-of-4.js
@@ -0,0 +1,39 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/licenses/publicdomain/
+
+var PART = 1, PARTS = 4;
+
+var gTestfile = '15.2.3.6-redefinition-1-of-4.js';
+//-----------------------------------------------------------------------------
+var BUGNUMBER = 430133;
+var summary =
+  'ES5 Object.defineProperty(O, P, Attributes): redefinition ' +
+  PART + ' of ' + PARTS;
+
+print(BUGNUMBER + ": " + summary);
+
+load("ecma_5/Object/defineProperty-setup.js");
+
+/**************
+ * BEGIN TEST *
+ **************/
+
+try
+{
+  new TestRunner().runPropertyPresentTestsFraction(PART, PARTS);
+}
+catch (e)
+{
+  throw "Error thrown during testing: " + e +
+          " at line " + e.lineNumber + "\n" +
+        (e.stack
+          ? "Stack: " + e.stack.split("\n").slice(2).join("\n") + "\n"
+          : "");
+}
+
+/******************************************************************************/
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
+
+print("Tests complete!");
