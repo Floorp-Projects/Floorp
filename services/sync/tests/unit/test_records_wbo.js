cadd0295cbae5c79c21297425eefd287dd3a8296
Dan Mills <thunder@mozilla.com>
2009-06-05 10:39:51 -0700

    partially revert d6cac1e80da9 - use id from the body; add additional tests to catch more errors

--- services/sync/tests/unit/test_records_wbo.js
+++ services/sync/tests/unit/test_records_wbo.js
@@ -6,6 +6,7 @@ try {
   Cu.import("resource://weave/identity.js");
   Cu.import("resource://weave/resource.js");
   Cu.import("resource://weave/base_records/wbo.js");
+  Cu.import("resource://weave/base_records/collection.js");
 } catch (e) { do_throw(e); }
 
 Function.prototype.async = Async.sugar;
@@ -24,6 +25,13 @@ function record_handler2(metadata, response) {
   return httpd_basic_auth_handler(JSON.stringify(obj), metadata, response);
 }
 
+function coll_handler(metadata, response) {
+  let obj = [{id: "record2",
+              modified: 2454725.98284,
+              payload: JSON.stringify({cheese: "gruyere"})}];
+  return httpd_basic_auth_handler(JSON.stringify(obj), metadata, response);
+}
+
 function async_test() {
   let self = yield;
   let server;
@@ -35,7 +43,8 @@ function async_test() {
     log.info("Setting up server and authenticator");
 
     server = httpd_setup({"/record": record_handler,
-                          "/record2": record_handler2});
+                          "/record2": record_handler2,
+                          "/coll": coll_handler});
 
     let auth = new BasicAuthenticator(new Identity("secret", "guest", "guest"));
     Auth.defaultAuthenticator = auth;
@@ -45,10 +54,13 @@ function async_test() {
     let res = new Resource("http://localhost:8080/record");
     yield res.get(self.cb);
 
-    let rec = new WBORecord(res.uri.spec);
+    let rec = new WBORecord();
     rec.deserialize(res.data);
+    do_check_eq(rec.id, "asdf-1234-asdf-1234"); // NOT "record"!
 
+    rec.uri = res.uri;
     do_check_eq(rec.id, "record"); // NOT "asdf-1234-asdf-1234"!
+
     do_check_eq(rec.modified, 2454725.98283);
     do_check_eq(typeof(rec.payload), "object");
     do_check_eq(rec.payload.cheese, "roquefort");
@@ -62,6 +74,18 @@ function async_test() {
     do_check_eq(typeof(rec2.payload), "object");
     do_check_eq(rec2.payload.cheese, "gruyere");
     do_check_eq(Records.lastResource.lastChannel.responseStatus, 200);
+
+    log.info("Using a collection to get a record");
+
+    let coll = new Collection("http://localhost:8080/coll", WBORecord);
+    yield coll.get(self.cb);
+    do_check_eq(coll.iter.count, 1);
+
+    let rec3 = yield coll.iter.next(self.cb);
+    do_check_eq(rec3.id, "record2");
+    do_check_eq(rec3.modified, 2454725.98284);
+    do_check_eq(typeof(rec3.payload), "object");
+    do_check_eq(rec3.payload.cheese, "gruyere");
 
     log.info("Done!");
     do_test_finished();
