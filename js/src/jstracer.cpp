de92f7238c0db878f79aa01e57efdf2789d4635b
David Anderson <danderson@mozilla.com>
2009-06-08 13:20:25 -0700

    Fixed sideexit coalescing code not checking global count (bug 496813, r=gal).

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -2437,6 +2437,7 @@ TraceRecorder::snapshot(ExitType exitType)
         for (unsigned n = 0; n < nexits; ++n) {
             VMSideExit* e = exits[n];
             if (e->pc == pc && e->imacpc == fp->imacpc &&
+                ngslots == e->numGlobalSlots &&
                 !memcmp(getFullTypeMap(exits[n]), typemap, typemap_size)) {
                 AUDIT(mergedLoopExits);
                 return e;
