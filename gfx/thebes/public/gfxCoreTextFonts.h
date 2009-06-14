ee135a894c302be57fe3af4e0dda760c95d72748
Jonathan Kew <jfkthame@gmail.com>
2009-06-15 10:00:12 +1200

    Bug 476927. Pad out the glyph extents of Quartz text to account for possible antialiased pixels outside the regular bounds. r=roc

--- gfx/thebes/public/gfxCoreTextFonts.h
+++ gfx/thebes/public/gfxCoreTextFonts.h
@@ -98,6 +98,12 @@ public:
         return mSpaceGlyph;
     }
 
+    virtual RunMetrics Measure(gfxTextRun *aTextRun,
+                               PRUint32 aStart, PRUint32 aEnd,
+                               BoundingBoxType aBoundingBoxType,
+                               gfxContext *aContextForTightBoundingBox,
+                               Spacing *aSpacing);
+
     PRBool TestCharacterMap(PRUint32 aCh);
 
     MacOSFontEntry* GetFontEntry();
