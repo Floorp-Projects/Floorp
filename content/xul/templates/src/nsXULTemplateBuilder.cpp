acbff4f66d5542e144a70054ee2b2f6684547807
Olli Pettay <Olli.Pettay@helsinki.fi>
2009-06-15 12:46:39 +0300

    Bug 495444, r=enn,sr=neil
    --HG--
    extra : rebase_source : a4eaa6a8c816630456381b78f436447159bd44a8

--- content/xul/templates/src/nsXULTemplateBuilder.cpp
+++ content/xul/templates/src/nsXULTemplateBuilder.cpp
@@ -1109,7 +1109,9 @@ nsXULTemplateBuilder::AttributeChanged(nsIDocument* aDocument,
         // case we may need to nuke and rebuild the entire content model
         // beneath the element.
         if (aAttribute == nsGkAtoms::ref)
-            Rebuild();
+            nsContentUtils::AddScriptRunner(
+                NS_NEW_RUNNABLE_METHOD(nsXULTemplateBuilder, this,
+                                       RunnableRebuild));
 
         // Check for a change to the 'datasources' attribute. If so, setup
         // mDB by parsing the new value and rebuild.
@@ -1119,7 +1121,9 @@ nsXULTemplateBuilder::AttributeChanged(nsIDocument* aDocument,
             PRBool shouldDelay;
             LoadDataSources(aDocument, &shouldDelay);
             if (!shouldDelay)
-                Rebuild();
+                nsContentUtils::AddScriptRunner(
+                    NS_NEW_RUNNABLE_METHOD(nsXULTemplateBuilder, this,
+                                           RunnableRebuild));
         }
     }
 }
