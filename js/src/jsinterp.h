407f7741419ae72f0468f12e9b626ed609ac45e5
Igor Bukanov <igor@mir2.org>
2009-06-18 15:00:55 +0200

    Backed out changeset 7ab1be136cfa - that patch for bug 498899 has a bug.

--- js/src/jsinterp.h
+++ js/src/jsinterp.h
@@ -380,20 +380,23 @@ js_FillPropertyCache(JSContext *cx, JSObject *obj,
         if (entry->kpc == pc && entry->kshape == kshape_) {                   \
             JSObject *tmp_;                                                   \
             pobj = obj;                                                       \
+            JS_LOCK_OBJ(cx, pobj);                                            \
             JS_ASSERT(PCVCAP_TAG(entry->vcap) <= 1);                          \
             if (PCVCAP_TAG(entry->vcap) == 1 &&                               \
-                (tmp_ = OBJ_GET_PROTO(cx, pobj)) != NULL &&                   \
+                (tmp_ = LOCKED_OBJ_GET_PROTO(pobj)) != NULL &&                \
                 OBJ_IS_NATIVE(tmp_)) {                                        \
+                JS_UNLOCK_OBJ(cx, pobj);                                      \
                 pobj = tmp_;                                                  \
+                JS_LOCK_OBJ(cx, pobj);                                        \
             }                                                                 \
-                                                                              \
-            if (JS_LOCK_OBJ_IF_SHAPE(cx, pobj, PCVCAP_SHAPE(entry->vcap))) {  \
+            if (PCVCAP_SHAPE(entry->vcap) == OBJ_SHAPE(pobj)) {               \
                 PCMETER(cache_->pchits++);                                    \
                 PCMETER(!PCVCAP_TAG(entry->vcap) || cache_->protopchits++);   \
                 pobj = OBJ_SCOPE(pobj)->object;                               \
                 atom = NULL;                                                  \
                 break;                                                        \
             }                                                                 \
+            JS_UNLOCK_OBJ(cx, pobj);                                          \
         }                                                                     \
         atom = js_FullTestPropertyCache(cx, pc, &obj, &pobj, &entry);         \
         if (atom)                                                             \
