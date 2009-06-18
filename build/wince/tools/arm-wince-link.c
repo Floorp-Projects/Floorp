96329b99676c633dffd8052f5be1f96742f6c8be
Brad Lassey <blassey@mozilla.com>
2009-06-18 07:28:57 -0400

    bug 491796 -  arm-wince-gcc should support link and entry options r=dougt

--- build/wince/tools/arm-wince-link.c
+++ build/wince/tools/arm-wince-link.c
@@ -1,4 +1,5 @@
 #include "toolspath.h"
+#include "linkargs.h"
 
 int
 main(int argc, char **argv)
@@ -9,56 +10,17 @@ main(int argc, char **argv)
   int j = 0;
   int k = 0;
   int s = 0;
-  args[i++] = LINK_PATH;
-
-  args[i++] = "/LIBPATH:\"" WCE_LIB "\"";
-  args[i++] = "/LIBPATH:\"" WCE_CRT "\"";
-  args[i++] = "/NODEFAULTLIB";
-
-#ifdef HAVE_SHUNT   // simple test to see if we're in configure or not
-  if(getenv("NO_SHUNT") == NULL) {
-    args[i++] = "/LIBPATH:\"" SHUNT_LIB "\"";
-    args[i++] = "mozce_shunt.lib";
-  }
-#endif
-
-  args[i++] = "corelibc.lib";
-  args[i++] = "coredll.lib";
-  args[i++] = "ceshell.lib";
-  args[i++] = "mmtimer.lib";
 
   // if -DLL is not passed, then change the entry to 'main'
   while(argv[j]) {
-
-      if (strncmp(argv[j], "-DLL", 4) == 0 ||
-	  strncmp(argv[j], "/DLL", 4) == 0) {
-	k = 1;
-      }
-      if (strncmp(argv[j], "-entry", 6) == 0 ||
-	  strncmp(argv[j], "/entry", 6) == 0 ||
-	  strncmp(argv[j], "-ENTRY", 6) == 0 ||
-	  strncmp(argv[j], "/ENTRY",6 ) == 0) {
-	k = 1;
-      }
-      if (strncmp(argv[j], "-subsystem:", 11) == 0 ||
-	  strncmp(argv[j], "/subsystem:", 11) == 0 ||
-	  strncmp(argv[j], "-SUBSYSTEM:", 11) == 0 ||
-	  strncmp(argv[j], "/SUBSYSTEM:", 11) == 0) {
-	s = 1;
-      }
-      j++;
+    checkLinkArgs(&k, &s, &i, &j, args, argv);
+    j++;
   }
-
-  if (k==0)
-    args[i++] = "/ENTRY:main";
-
-  if (s==0){
-    args[i++] = "/subsystem:\"WINDOWSCE,5.02\"";
-  }
+  args[i++] = LINK_PATH;
+  addLinkArgs(k, s, &i, &j, args, argv);
 
   argpath_conv(&argv[1], &args[i]);
 
-  //  dumpargs(args);
-
   return run(args);
 }
+
