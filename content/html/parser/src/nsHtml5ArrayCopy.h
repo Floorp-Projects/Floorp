43172f7c332051049d43ebc479b07a81a6a8256d
Henri Sivonen <hsivonen@iki.fi>
2009-06-21 15:46:29 +0300

    Move static inline void on one line in nsHtml5ArrayCopy.h

--- content/html/parser/src/nsHtml5ArrayCopy.h
+++ content/html/parser/src/nsHtml5ArrayCopy.h
@@ -49,49 +49,37 @@ class nsHtml5AttributeName;
 
 class nsHtml5ArrayCopy {
   public: 
-    static
-    inline
-    void
+    static inline void
     arraycopy(PRUnichar* source, PRInt32 sourceOffset, PRUnichar* target, PRInt32 targetOffset, PRInt32 length)
     {
       memcpy(&(target[targetOffset]), &(source[sourceOffset]), length * sizeof(PRUnichar));
     }
     
-    static
-    inline
-    void
+    static inline void
     arraycopy(PRUnichar* source, PRUnichar* target, PRInt32 length)
     {
       memcpy(target, source, length * sizeof(PRUnichar));
     }
     
-    static
-    inline
-    void
+    static inline void
     arraycopy(nsString** source, nsString** target, PRInt32 length)
     {
       memcpy(target, source, length * sizeof(nsString*));
     }
     
-    static
-    inline
-    void
+    static inline void
     arraycopy(nsHtml5AttributeName** source, nsHtml5AttributeName** target, PRInt32 length)
     {
       memcpy(target, source, length * sizeof(nsHtml5AttributeName*));
     }
     
-    static
-    inline
-    void
+    static inline void
     arraycopy(nsHtml5StackNode** source, nsHtml5StackNode** target, PRInt32 length)
     {
       memcpy(target, source, length * sizeof(nsHtml5StackNode*));
     }
     
-    static
-    inline
-    void
+    static inline void
     arraycopy(nsHtml5StackNode** arr, PRInt32 sourceOffset, PRInt32 targetOffset, PRInt32 length)
     {
       memmove(&(arr[targetOffset]), &(arr[sourceOffset]), length * sizeof(nsHtml5StackNode*));
