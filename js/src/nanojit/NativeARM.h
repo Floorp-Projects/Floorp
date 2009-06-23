af24928a8fe1e1a5bd44bafadef0f2214a37aecc
Jacob Bramley <Jacob.Bramley@arm.com>
2009-06-23 09:36:53 +0100

    Fix crash on ARM caused by spilling registers in asm_mmq. (495239, r=vladimir) Follow-up: Reveals (or creates) some failures documented in bug 499889.

--- js/src/nanojit/NativeARM.h
+++ js/src/nanojit/NativeARM.h
@@ -196,10 +196,10 @@ verbose_only( extern const char* shiftNames[]; )
     void asm_ldr_chk(Register d, Register b, int32_t off, bool chk);    \
     void asm_ld_imm(Register d, int32_t imm);                           \
     void asm_arg(ArgSize sz, LInsp arg, Register& r, int& stkd);        \
+    uint32_t CountLeadingZeroes(uint32_t data);                         \
     int* _nSlot;                                                        \
-    int* _startingSlot;                                                \
+    int* _startingSlot;                                                 \
     int* _nExitSlot;
-
 
 //nj_dprintf("jmp_l_n count=%d, nins=%X, %X = %X\n", (_c), nins, _nIns, ((intptr_t)(nins+(_c))-(intptr_t)_nIns - 4) );
 
