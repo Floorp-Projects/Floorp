cb5a73c4448dc38f9305f2786a93a553723eab89
Benjamin Smedberg <benjamin@smedbergs.us>
2009-06-30 15:38:59 -0400

    Import IPDL from cjones' working repo, revision 282b4211d881. New IPDL work will take place in electrolysis.

---
+++ ipc/ipdl/ipdl/cxx/cgen.py
@@ -0,0 +1,337 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is mozilla.org code.
+#
+# Contributor(s):
+#   Chris Jones <jones.chris.g@gmail.com>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either of the GNU General Public License Version 2 or later (the "GPL"),
+# or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+import sys
+
+from ipdl.cgen import CodePrinter
+from ipdl.cxx.ast import Visitor
+
+class CxxCodeGen(CodePrinter, Visitor):
+    def __init__(self, outf=sys.stdout, indentCols=4):
+        CodePrinter.__init__(self, outf, indentCols)
+
+    def cgen(self, cxxfile):
+        cxxfile.accept(self)
+
+    def visitWhitespace(self, ws):
+        self.write(ws.ws)
+
+    def visitCppDirective(self, cd):
+        self.println('#%s %s'% (cd.directive, cd.rest))
+
+    def visitNamespace(self, ns):
+        self.println('namespace '+ ns.name +' {')
+        self.visitBlock(ns)
+        self.println('} // namespace '+ ns.name)
+
+    def visitType(self, t):
+        ts = ''
+        if t.const:  ts += 'const '
+        ts += t.name
+        if t.ptr:  ts += '*'
+        if t.ref:  ts += '&'
+        self.write(ts)
+
+    def visitTypeEnum(self, te):
+        self.write('enum')
+        if te.name:
+            self.write(' '+ te.name)
+        self.println(' {')
+
+        self.indent()
+        nids = len(te.idnums)
+        for i, (id, num) in enumerate(te.idnums):
+            self.printdent(id)
+            if num:
+                self.write(' = '+ str(num))
+            if i != (nids-1):
+                self.write(',')
+            self.println()
+        self.dedent()
+        self.printdent('}')
+            
+
+    def visitTypedef(self, td):
+        self.printdent('typedef ')
+        td.fromtype.accept(self)
+        self.write(' ')
+        td.totype.accept(self)
+        self.println(';')
+
+    def visitDecl(self, d):
+        d.type.accept(self)
+        if d.name:
+            self.write(' '+ d.name)
+
+    def visitClass(self, c):
+        self.printdent('class')
+        if c.interface:
+            # FIXME/cjones: turn this "on" when we get the analysis
+            self.write(' /*NS_INTERFACE_CLASS*/')
+        if c.final:
+            self.write(' /*NS_FINAL_CLASS*/')
+        self.write(' '+ c.name)
+
+        ninh = len(c.inherits)
+        if 0 < ninh:
+            self.println(' :')
+            self.indent()
+            for i, inherit in enumerate(c.inherits):
+                self.printdent()
+                inherit.accept(self)
+                if i != (ninh - 1):
+                    self.println(',')
+            self.dedent()
+        self.println()
+
+        self.printdentln('{')
+        self.indent()
+
+        self.visitBlock(c)
+
+        self.dedent()
+        self.printdentln('};')
+
+    def visitInherit(self, inh):
+        self.write(inh.viz +' '+ inh.name)
+
+
+    def visitMethodDecl(self, md):
+        assert not (md.static and md.virtual)
+        if md.static:
+            self.write('static ')
+        if md.virtual:
+            self.write('virtual ')
+        if md.ret:
+            md.ret.accept(self)
+            self.write(' ')
+        self.write(md.name +'(')
+        self.writeDeclList(md.params)
+        self.write(')')
+        if md.const:
+            self.write(' const')
+        if md.pure:
+            self.write(' = 0')
+
+    def visitMethodDefn(self, md):
+        self.printdent()
+        md.decl.accept(self)
+        self.println()
+
+        self.printdentln('{')
+        self.indent()
+        self.visitBlock(md)
+        self.dedent()
+        self.printdentln('}')
+
+
+    def visitConstructorDecl(self, cd):
+        # FIXME/cjones: explicit when possible
+        self.visitMethodDecl(cd)
+
+    def visitConstructorDefn(self, cd):
+        self.printdent()
+        cd.decl.accept(self)
+        if len(cd.memberinits):
+            self.println(' :')
+            self.indent()
+            ninits = len(cd.memberinits)
+            for i, init in enumerate(cd.memberinits):
+                self.printdent()
+                init.accept(self)
+                if i != (ninits-1):
+                    self.println(',')
+            self.dedent()
+        self.println()
+
+        self.printdentln('{')
+        self.indent()
+
+        self.visitBlock(cd)
+
+        self.dedent()
+        self.printdentln('}')
+
+
+    def visitDestructorDecl(self, dd):
+        if dd.virtual:
+            self.write('virtual ')
+        self.write('~'+ dd.name +'()')
+
+    def visitDestructorDefn(self, dd):
+        self.printdent()
+        dd.decl.accept(self)
+        self.println()
+
+        self.printdentln('{')
+        self.indent()
+
+        self.visitBlock(dd)
+
+        self.dedent()
+        self.printdentln('}')
+
+
+    def visitExprVar(self, ev):
+        self.write(ev.name)
+
+    def visitExprPrefixUnop(self, e):
+        self.write(e.op)
+        self.write('(')
+        e.expr.accept(self)
+        self.write(')')
+
+    def visitExprSelect(self, es):
+        es.obj.accept(self)
+        self.write(es.op + es.field)
+
+    def visitExprAssn(self, ea):
+        ea.lhs.accept(self)
+        self.write(' = ')
+        ea.rhs.accept(self)
+
+    def visitExprCall(self, ec):
+        ec.func.accept(self)
+        self.write('(')
+        self.writeExprList(ec.args)
+        self.write(')')
+
+    def visitExprNew(self, en):
+        self.write('new ')
+        self.visitExprCall(en)
+
+    def visitExprDelete(self, ed):
+        self.write('delete ')
+        ed.accept(self)
+
+
+    def visitStmtBlock(self, b):
+        self.printdentln('{')
+        self.indent()
+        self.visitBlock(b)
+        self.dedent()
+        self.printdentln('}')
+
+    def visitLabel(self, label):
+        self.dedent()           # better not be at global scope ...
+        self.printdentln(label.name +':')
+        self.indent()
+
+    def visitCaseLabel(self, cl):
+        self.dedent()
+        self.printdentln('case '+ cl.name +':')
+        self.indent()
+
+    def visitDefaultLabel(self, dl):
+        self.dedent()
+        self.printdentln('default:')
+        self.indent()
+
+
+    def visitStmtIf(self, si):
+        self.printdent('if (')
+        si.cond.accept(self)
+        self.println(') {')
+        self.indent()
+        si.ifb.accept(self)
+        self.dedent()
+        self.printdentln('}')
+
+        if si.elseb is not None:
+            self.printdentln('else {')
+            self.indent()
+            si.elseb.accept(self)
+            self.dedent()
+            self.println('}')
+
+
+    def visitStmtSwitch(self, sw):
+        self.printdent('switch (')
+        sw.expr.accept(self)
+        self.println(') {')
+        self.indent()
+        self.visitBlock(sw)
+        self.dedent()
+        self.printdentln('}')
+
+
+    def visitStmtDecl(self, sd):
+        self.printdent()
+        sd.decl.accept(self)
+        self.println(';')
+
+
+    def visitStmtExpr(self, se):
+        self.printdent()
+        se.expr.accept(self)
+        self.println(';')
+
+
+    def visitStmtReturn(self, sr):
+        self.printdent('return')
+        if sr.expr:
+            self.write (' ')
+            sr.expr.accept(self)
+        self.println(';')
+
+
+    def writeDeclList(self, decls):
+        # FIXME/cjones: try to do nice formatting of these guys
+
+        ndecls = len(decls)
+        if 0 == ndecls:
+            return
+        elif 1 == ndecls:
+            decls[0].accept(self)
+            return
+
+        self.indent()
+        self.indent()
+        self.indent()
+        for i, decl in enumerate(decls):
+            self.println()
+            self.printdent()
+            decl.accept(self)
+            if i != (ndecls-1):
+                self.write(',')
+        self.dedent()
+        self.dedent()
+        self.dedent()
+
+    def writeExprList(self, exprs):
+        # FIXME/cjones: try to do nice formatting and share code with
+        # writeDeclList()
+        nexprs = len(exprs)
+        for i, expr in enumerate(exprs):
+            expr.accept(self)
+            if i != (nexprs-1):
+                self.write(', ')
