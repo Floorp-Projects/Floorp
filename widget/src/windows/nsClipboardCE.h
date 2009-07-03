cec2096119dc523b8bac01165a269f08f857987a
Justin Dolske <dolske@mozilla.com>
2009-07-03 16:11:05 -0700

    Bug 499803 - "Implement simple clipboard on Windows CE" [r=vladimir]

--- widget/src/windows/nsClipboardCE.h
+++ widget/src/windows/nsClipboardCE.h
@@ -53,10 +53,15 @@ class nsClipboard :
 public:
   nsClipboard();
   virtual ~nsClipboard();
+  NS_IMETHOD HasDataMatchingFlavors(const char** aFlavorList, PRUint32 aLength,
+                                    PRInt32 aWhichClipboard, PRBool *_retval);
 
 protected:
   NS_IMETHOD SetNativeClipboardData (PRInt32 aWhichClipboard);
   NS_IMETHOD GetNativeClipboardData (nsITransferable * aTransferable, PRInt32 aWhichClipboard);
+
+private:
+  static UINT     GetFormat(const char* aMimeStr);
 };
 
 #endif
