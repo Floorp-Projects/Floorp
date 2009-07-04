fe1d1dcf8c574749570dda4b154d8dfc18edfb03
Andreas Gal <gal@mozilla.com>
2009-07-03 17:37:41 -0700

    Fix warnings in jsxdrapi.cpp (502296, r=jwalden).

--- js/src/jsxdrapi.cpp
+++ js/src/jsxdrapi.cpp
@@ -493,8 +493,7 @@ XDRDoubleValue(JSXDRState *xdr, jsdouble *dp)
 {
     jsdpun u;
 
-    if (xdr->mode == JSXDR_ENCODE)
-        u.d = *dp;
+    u.d = (xdr->mode == JSXDR_ENCODE) ? *dp : 0.0;
     if (!JS_XDRUint32(xdr, &u.s.lo) || !JS_XDRUint32(xdr, &u.s.hi))
         return JS_FALSE;
     if (xdr->mode == JSXDR_DECODE)
@@ -505,10 +504,7 @@ XDRDoubleValue(JSXDRState *xdr, jsdouble *dp)
 JS_PUBLIC_API(JSBool)
 JS_XDRDouble(JSXDRState *xdr, jsdouble **dpp)
 {
-    jsdouble d;
-
-    if (xdr->mode == JSXDR_ENCODE)
-        d = **dpp;
+    jsdouble d = (xdr->mode == JSXDR_ENCODE) ? **dpp : 0.0;
     if (!XDRDoubleValue(xdr, &d))
         return JS_FALSE;
     if (xdr->mode == JSXDR_DECODE) {
@@ -544,9 +540,7 @@ XDRValueBody(JSXDRState *xdr, uint32 type, jsval *vp)
         break;
       }
       case JSVAL_DOUBLE: {
-        jsdouble *dp;
-        if (xdr->mode == JSXDR_ENCODE)
-            dp = JSVAL_TO_DOUBLE(*vp);
+        jsdouble *dp = (xdr->mode == JSXDR_ENCODE) ? JSVAL_TO_DOUBLE(*vp) : NULL;
         if (!JS_XDRDouble(xdr, &dp))
             return JS_FALSE;
         if (xdr->mode == JSXDR_DECODE)
