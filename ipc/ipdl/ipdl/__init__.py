0c8c8b8a9fd3ea353aab2787064c130513998f2f
Chris Jones <jones.chris.g@gmail.com>
2009-07-07 15:52:10 -0500

    bugs 502003, 502634: compile multiple IPDL specs in one invocation

--- ipc/ipdl/ipdl/__init__.py
+++ ipc/ipdl/ipdl/__init__.py
@@ -30,9 +30,10 @@
 #
 # ***** END LICENSE BLOCK *****
 
-__all__ = [ 'gencxx', 'genipdl', 'parse', 'typecheck' ]
+__all__ = [ 'gencxx', 'genipdl', 'parse', 'typecheck', 'writeifmodified' ]
 
 import os, sys
+from cStringIO import StringIO
 
 from ipdl.cgen import IPDLCodeGen
 from ipdl.lower import LowerToCxx
@@ -51,8 +52,26 @@ def typecheck(ast, errout=sys.stderr):
 
 def gencxx(ast, outdir):
     for hdr in LowerToCxx().lower(ast):
-        path = os.path.join(outdir, hdr.filename)
-        CxxCodeGen(outf=open(path, 'wb')).cgen(hdr)
+        file = os.path.join(outdir,
+                            *([ns.namespace for ns in ast.protocol.namespaces] + [hdr.filename]))
+
+        tempfile = StringIO()
+        CxxCodeGen(tempfile).cgen(hdr)
+        writeifmodified(tempfile.getvalue(), file)
 
 def genipdl(ast, outdir):
     return IPDLCodeGen().cgen(ast)
+
+def writeifmodified(contents, file):
+    dir = os.path.dirname(file)
+    os.path.exists(dir) or os.makedirs(dir)
+
+    oldcontents = None
+    if os.path.exists(file):
+        fd = open(file, 'rb')
+        oldcontents = fd.read()
+        fd.close()
+    if oldcontents != contents:
+        fd = open(file, 'wb')
+        fd.write(contents)
+        fd.close()
