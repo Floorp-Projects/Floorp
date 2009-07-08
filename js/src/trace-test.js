0343e7f3ce1749049e6fb1b4141fd992d0058359
Jason Orendorff <jorendorff@mozilla.com>
2009-07-08 15:38:55 -0500

    Last-minute change to the unit test in the previous changeset a08e3540da32, requested by bz in bug 502890 comment 6. Informal r=graydon.

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -4449,6 +4449,7 @@ function testRebranding2() {
     function h(){ x = "ok"; }
     var obj = {m: g};
     var arr = [g, g, g, g, h];
+    assertEq(arr.length > RUNLOOP, true);
     for (var i = 0; i < 5; i++) {
         obj.m = arr[i];
         obj.m();
