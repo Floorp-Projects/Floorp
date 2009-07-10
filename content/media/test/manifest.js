54606826b2ba97452de7383c8a22fa05895b950d
Robert O'Callahan <robert@ocallahan.org>
2009-07-10 14:03:01 +1200

    Bug 502211. Make test_media_selection.html and test_load.html backend-independent. r=doublec

---
+++ content/media/test/manifest.js
@@ -0,0 +1,6 @@
+// These are small test files, good for just seeing if something loads
+var gSmallTests = [
+  { name:"r11025_s16_c1.wav", type:"audio/x-wav", duration:1.0 },
+  { name:"320x240.ogv", type:"video/ogg", width:320, height:240 },
+  { name:"bogus.duh", type:"bogus/duh" }
+];
