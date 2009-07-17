305d028f254f1bfab322bacd51cb66d9d9bb51e7
Luke Wagner <lw@mozilla.com>
2009-07-16 17:17:35 -0700

    Bug 406271: add quantifier support for regexp->native compiler, r=dmandelin

--- js/src/nanojit/Assembler.cpp
+++ js/src/nanojit/Assembler.cpp
@@ -1590,6 +1590,12 @@ namespace nanojit
         for (int i=0, n=pending_lives.size(); i < n; i++) {
             findMemFor(pending_lives[i]);
         }
+        /*
+         * TODO: I'm not positive, but I think the following line needs to be
+         * added, otherwise the pending_lives will build up and never get
+         * cleared.
+         */
+        pending_lives.clear();
     }
 
     void Assembler::arFree(uint32_t idx)
