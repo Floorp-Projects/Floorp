b2926d7961c09a4af55f782131bb65eb6032f467
Jacob Bramley <Jacob.Bramley@arm.com>
2009-07-20 11:34:34 +0100

    Backing out a broken part of 99dfb2001094. (no bug)

--- js/src/nanojit/Assembler.cpp
+++ js/src/nanojit/Assembler.cpp
@@ -1544,7 +1544,8 @@ namespace nanojit
         underrunProtect(si->count * sizeof(NIns*) + 20);
         _nIns = reinterpret_cast<NIns*>(uintptr_t(_nIns) & ~(sizeof(NIns*) - 1));
         for (uint32_t i = 0; i < si->count; ++i) {
-            *(--_nIns) = (NIns)target;
+            _nIns = (NIns*) (((uint8*) _nIns) - sizeof(NIns*));
+            *(NIns**) _nIns = target;
         }
         si->table = (NIns**) _nIns;
     }
