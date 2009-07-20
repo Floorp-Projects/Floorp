7ec0037c62d049aa3dacaff3178eea2b83f0a538
Steven Michaud <smichaud@pobox.com>
2009-07-20 17:10:36 -0500

    [OSX] Embedded plugins don't print - NPP_Print not called. b=191046 r=josh,kinetic

--- modules/plugin/base/public/npapi.h
+++ modules/plugin/base/public/npapi.h
@@ -136,6 +136,12 @@ typedef char*         NPMIMEType;
 /*                       Structures and definitions                     */
 /*----------------------------------------------------------------------*/
 
+#if !defined(__LP64__)
+#if defined(XP_MAC) || defined(XP_MACOSX)
+#pragma options align=mac68k
+#endif
+#endif /* __LP64__ */
+
 /*
  *  NPP is a plug-in's opaque instance handle
  */
@@ -510,6 +516,11 @@ enum NPEventType {
 
 #define NP_MAXREADY (((unsigned)(~0)<<1)>>1)
 
+#if !defined(__LP64__)
+#if defined(XP_MAC) || defined(XP_MACOSX)
+#pragma options align=reset
+#endif
+#endif /* __LP64__ */
 
 /*----------------------------------------------------------------------*/
 /*       Error and Reason Code definitions                              */
