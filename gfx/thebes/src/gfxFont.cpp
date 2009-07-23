b03ad1722205cc76d202038a5c794947743ac057
John Daggett <jdaggett@mozilla.com>
2009-07-23 11:22:24 -1000

    Bug 499621. Fix synthetic bolding of downloadable fonts on Windows. r=jkew

--- gfx/thebes/src/gfxFont.cpp
+++ gfx/thebes/src/gfxFont.cpp
@@ -130,17 +130,26 @@ gfxFontEntry *gfxFontFamily::FindFontForStyle(const gfxFontStyle& aFontStyle, PR
     gfxFontEntry *matchFE;
     const PRInt8 absDistance = abs(weightDistance);
     direction = (weightDistance >= 0) ? 1 : -1;
-    PRInt8 i, k;
-    for (i = matchBaseWeight, k = 0; i < 10 && i > 0; i += direction) {
+    PRInt8 i, wghtSteps = 0;
+
+    // account for synthetic bold in lighter case
+    // if lighter is applied with an inherited bold weight,
+    // and no actual bold faces exist, synthetic bold is used
+    // so the matched weight above is actually one step down already
+    if (weightDistance < 0 && baseWeight > 5 && matchBaseWeight < 6) {
+        wghtSteps = 1; // if no faces [600, 900] then synthetic bold at 700
+    }
+
+    for (i = matchBaseWeight; i < 10 && i > 0; i += direction) {
         if (weightList[i]) {
             matchFE = weightList[i];
-            k++;
+            wghtSteps++;
         }
-        if (k > absDistance)
+        if (wghtSteps > absDistance)
             break;
     }
 
-    if (weightDistance > 0 && k <= absDistance) {
+    if (weightDistance > 0 && wghtSteps <= absDistance) {
         aNeedsBold = PR_TRUE;
     }
 
