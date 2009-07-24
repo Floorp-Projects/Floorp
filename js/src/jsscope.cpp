1302ef18a77f6b2563ae583a513dac1ef9415abc
Andreas Gal <gal@mozilla.com>
2009-07-23 19:38:09 -0700

    Fix merge conflict that lost part of bug 505612.

--- js/src/jsscope.cpp
+++ js/src/jsscope.cpp
@@ -251,14 +251,15 @@ JSScope::destroy(JSContext *cx, JSScope *scope)
 #ifdef JS_THREADSAFE
     js_FinishTitle(cx, &scope->title);
 #endif
+    JSRuntime* rt = cx->runtime;
     if (scope->table)
-        cx->runtime->asynchronousFree(scope->table);
+        rt->asynchronousFree(scope->table);
     if (scope->emptyScope)
         scope->emptyScope->drop(cx, NULL);
 
     LIVE_SCOPE_METER(cx, cx->runtime->liveScopeProps -= scope->entryCount);
     JS_RUNTIME_UNMETER(cx->runtime, liveScopes);
-    JS_free(cx, scope);
+    rt->asynchronousFree(scope);
 }
 
 #ifdef JS_DUMP_PROPTREE_STATS
