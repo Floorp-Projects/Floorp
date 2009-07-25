c0a65c6f9cec95b016ea105356df4b3fe88cde9b
Matthew Noorenberghe <mnoorenberghe@mozilla.com>
2009-07-25 22:05:03 +0200

    Bug 370117 - form autocomplete should sort by frequency of use. r=dolske

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -2729,9 +2729,12 @@ pref("signon.autofillForms",                true);
 pref("signon.debug",                        false); // logs to Error Console
 
 // Satchel (Form Manager) prefs
-pref("browser.formfill.enable",     true);
-pref("browser.formfill.debug",      false);
-
+pref("browser.formfill.debug",            false);
+pref("browser.formfill.enable",           true);
+pref("browser.formfill.agedWeight",       2);
+pref("browser.formfill.bucketSize",       5);
+pref("browser.formfill.maxTimeGroupings", 25);
+pref("browser.formfill.timeGroupingSize", 604800);
 
 // Zoom prefs
 pref("browser.zoom.full", false);
