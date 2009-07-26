1452015054e94e4e6987252f15d3fdde54b9d9c3
Robert O'Callahan <robert@ocallahan.org>
2009-07-27 09:29:01 +1200

    Bug 505743. Fix nsHTMLScrollFrame::InvalidateInternal to not take the 'invalidation in scrollbar' path when there is no scrollbar. r=dbaron

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -229,38 +229,40 @@ nsHTMLScrollFrame::InvalidateInternal(const nsRect& aDamageRect,
                                       nscoord aX, nscoord aY, nsIFrame* aForChild,
                                       PRUint32 aFlags)
 {
-  if (aForChild == mInner.mScrolledFrame) {
-    // restrict aDamageRect to the scrollable view's bounds
-    nsRect damage = aDamageRect + nsPoint(aX, aY);
-    nsRect r;
-    if (r.IntersectRect(damage, mInner.mScrollableView->View()->GetBounds())) {
-      nsHTMLContainerFrame::InvalidateInternal(r, 0, 0, aForChild, aFlags);
-    }
-    if (mInner.mIsRoot && r != damage) {
-      // Make sure we notify our prescontext about invalidations outside
-      // viewport clipping.
-      // This is important for things that are snapshotting the viewport,
-      // possibly outside the scrolled bounds.
-      // We don't need to propagate this any further up, though. Anyone who
-      // cares about scrolled-out-of-view invalidates had better be listening
-      // to our window directly.
-      PresContext()->NotifyInvalidation(damage,
-          (aFlags & INVALIDATE_CROSS_DOC) != 0);
-    }
-    return;
-  } else if (aForChild == mInner.mHScrollbarBox) {
-    if (!mInner.mHasHorizontalScrollbar) {
-      // Our scrollbars may send up invalidations even when they're collapsed,
-      // because we just size a collapsed scrollbar to empty and some
-      // descendants may be non-empty. Suppress that invalidation here.
+  if (aForChild) {
+    if (aForChild == mInner.mScrolledFrame) {
+      // restrict aDamageRect to the scrollable view's bounds
+      nsRect damage = aDamageRect + nsPoint(aX, aY);
+      nsRect r;
+      if (r.IntersectRect(damage, mInner.mScrollableView->View()->GetBounds())) {
+        nsHTMLContainerFrame::InvalidateInternal(r, 0, 0, aForChild, aFlags);
+      }
+      if (mInner.mIsRoot && r != damage) {
+        // Make sure we notify our prescontext about invalidations outside
+        // viewport clipping.
+        // This is important for things that are snapshotting the viewport,
+        // possibly outside the scrolled bounds.
+        // We don't need to propagate this any further up, though. Anyone who
+        // cares about scrolled-out-of-view invalidates had better be listening
+        // to our window directly.
+        PresContext()->NotifyInvalidation(damage,
+            (aFlags & INVALIDATE_CROSS_DOC) != 0);
+      }
       return;
-    }
-  } else if (aForChild == mInner.mVScrollbarBox) {
-    if (!mInner.mHasVerticalScrollbar) {
-      // Our scrollbars may send up invalidations even when they're collapsed,
-      // because we just size a collapsed scrollbar to empty and some
-      // descendants may be non-empty. Suppress that invalidation here.
-      return;
+    } else if (aForChild == mInner.mHScrollbarBox) {
+      if (!mInner.mHasHorizontalScrollbar) {
+        // Our scrollbars may send up invalidations even when they're collapsed,
+        // because we just size a collapsed scrollbar to empty and some
+        // descendants may be non-empty. Suppress that invalidation here.
+        return;
+      }
+    } else if (aForChild == mInner.mVScrollbarBox) {
+      if (!mInner.mHasVerticalScrollbar) {
+        // Our scrollbars may send up invalidations even when they're collapsed,
+        // because we just size a collapsed scrollbar to empty and some
+        // descendants may be non-empty. Suppress that invalidation here.
+        return;
+      }
     }
   }
  
