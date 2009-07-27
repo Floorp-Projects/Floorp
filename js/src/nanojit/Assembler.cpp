8b7a24306e599ffd2bf87c8b0db35f7f3bd24027
Andreas Gal <gal@uci.edu>
2009-07-26 20:28:46 -0400

    Bug 501275 -  TM: Crash [@ nanojit::Assembler::nPatchBranch]. r=dvander

--- js/src/nanojit/Assembler.cpp
+++ js/src/nanojit/Assembler.cpp
@@ -634,6 +634,8 @@ namespace nanojit
 
     void Assembler::patch(GuardRecord *lr)
     {
+        if (!lr->jmp) // the guard might have been eliminated as redundant
+            return;
         Fragment *frag = lr->exit->target;
         NanoAssert(frag->fragEntry != 0);
         NIns* was = nPatchBranch((NIns*)lr->jmp, frag->fragEntry);
