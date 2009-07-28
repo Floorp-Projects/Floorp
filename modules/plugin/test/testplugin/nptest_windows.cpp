d2a8a5b9c4ccd05e21c54bff899093f8e27327d0
Ben Turner <bent.mozilla@gmail.com>
2009-07-28 11:48:21 -0700

    Fix testplugin on windows to not crash until we support scriptable objects

--- modules/plugin/test/testplugin/nptest_windows.cpp
+++ modules/plugin/test/testplugin/nptest_windows.cpp
@@ -101,8 +101,10 @@ drawToDC(InstanceData* instanceData, HDC dc,
   BYTE alpha = 255;
   RECT fill = { 0, 0, width, height };
 
+#if 0 // OOP Plugins don't yet support scriptable objects
   switch (instanceData->scriptableObject->drawMode) {
     case DM_DEFAULT:
+#endif
     {
       HBRUSH brush = ::CreateSolidBrush(RGB(0, 0, 0));
       if (brush) {
@@ -135,6 +137,7 @@ drawToDC(InstanceData* instanceData, HDC dc,
         }
       }
     }
+#if 0 // OOP Plugins don't yet support scriptable objects
     break;
 
     case DM_SOLID_COLOR:
@@ -153,6 +156,7 @@ drawToDC(InstanceData* instanceData, HDC dc,
     }
     break;
   }
+#endif
 
   BLENDFUNCTION blendFunc;
   blendFunc.BlendOp = AC_SRC_OVER;
