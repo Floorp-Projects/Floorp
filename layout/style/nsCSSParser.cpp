389de5e6db1232e6799169740e0f701a93a3306b
L. David Baron <dbaron@dbaron.org>
2009-07-31 09:39:53 -0700

    Backed out changeset 358af1196dc2 (bug 499655) until we get bug 507487 sorted out.

--- layout/style/nsCSSParser.cpp
+++ layout/style/nsCSSParser.cpp
@@ -2452,7 +2452,7 @@ CSSParserImpl::ParseSelectorList(nsCSSSelectorList*& aListHead,
 static PRBool IsSinglePseudoClass(const nsCSSSelector& aSelector)
 {
   return PRBool((aSelector.mNameSpace == kNameSpaceID_Unknown) &&
-                (aSelector.mLowercaseTag == nsnull) &&
+                (aSelector.mTag == nsnull) &&
                 (aSelector.mIDList == nsnull) &&
                 (aSelector.mClassList == nsnull) &&
                 (aSelector.mAttrList == nsnull) &&
@@ -2527,7 +2527,7 @@ CSSParserImpl::ParseSelectorGroup(nsCSSSelectorList*& aList)
             list->AddSelector(empty); // leave a blank (universal) selector in the middle
             listSel = list->mSelectors; // use the new one for the pseudo
           }
-          listSel->mLowercaseTag = pseudoElement;
+          listSel->mTag = pseudoElement;
         }
         else {  // append new pseudo element selector
           nsAutoPtr<nsCSSSelector> pseudoTagSelector(new nsCSSSelector());
@@ -2535,9 +2535,9 @@ CSSParserImpl::ParseSelectorGroup(nsCSSSelectorList*& aList)
             mScanner.SetLowLevelError(NS_ERROR_OUT_OF_MEMORY);
             return PR_FALSE;
           }
-          pseudoTagSelector->mLowercaseTag = pseudoClassList->mAtom; // steal ref count
+          pseudoTagSelector->mTag = pseudoClassList->mAtom; // steal ref count
 #ifdef MOZ_XUL
-          if (IsTreePseudoElement(pseudoTagSelector->mLowercaseTag)) {
+          if (IsTreePseudoElement(pseudoTagSelector->mTag)) {
             // Take the remaining "pseudoclasses" that we parsed
             // inside the tree pseudoelement's ()-list, and
             // make our new selector have these pseudoclasses
@@ -2684,8 +2684,13 @@ CSSParserImpl::ParseTypeOrUniversalSelector(PRInt32&       aDataMask,
       }
       if (eCSSToken_Ident == mToken.mType) {  // element name
         aDataMask |= SEL_MASK_ELEM;
-
-        aSelector.SetTag(mToken.mIdent, mCaseSensitive);
+        if (mCaseSensitive) {
+          aSelector.SetTag(mToken.mIdent);
+        }
+        else {
+          ToLowerCase(mToken.mIdent, buffer);
+          aSelector.SetTag(buffer);
+        }
       }
       else if (mToken.IsSymbol('*')) {  // universal selector
         aDataMask |= SEL_MASK_ELEM;
@@ -2723,8 +2728,13 @@ CSSParserImpl::ParseTypeOrUniversalSelector(PRInt32&       aDataMask,
       }
       if (eCSSToken_Ident == mToken.mType) {  // element name
         aDataMask |= SEL_MASK_ELEM;
-       
-        aSelector.SetTag(mToken.mIdent, mCaseSensitive);
+        if (mCaseSensitive) {
+          aSelector.SetTag(mToken.mIdent);
+        }
+        else {
+          ToLowerCase(mToken.mIdent, buffer);
+          aSelector.SetTag(buffer);
+        }
       }
       else if (mToken.IsSymbol('*')) {  // universal selector
         aDataMask |= SEL_MASK_ELEM;
@@ -2738,8 +2748,13 @@ CSSParserImpl::ParseTypeOrUniversalSelector(PRInt32&       aDataMask,
     }
     else {  // was element name
       SetDefaultNamespaceOnSelector(aSelector);
-      aSelector.SetTag(buffer, mCaseSensitive);
-
+      if (mCaseSensitive) {
+        aSelector.SetTag(buffer);
+      }
+      else {
+        ToLowerCase(buffer);
+        aSelector.SetTag(buffer);
+      }
       aDataMask |= SEL_MASK_ELEM;
     }
     if (! GetToken(PR_FALSE)) {   // premature eof is ok (here!)
@@ -2757,7 +2772,13 @@ CSSParserImpl::ParseTypeOrUniversalSelector(PRInt32&       aDataMask,
     }
     if (eCSSToken_Ident == mToken.mType) {  // element name
       aDataMask |= SEL_MASK_ELEM;
-      aSelector.SetTag(mToken.mIdent, mCaseSensitive);
+      if (mCaseSensitive) {
+        aSelector.SetTag(mToken.mIdent);
+      }
+      else {
+        ToLowerCase(mToken.mIdent, buffer);
+        aSelector.SetTag(buffer);
+      }
     }
     else if (mToken.IsSymbol('*')) {  // universal selector
       aDataMask |= SEL_MASK_ELEM;
