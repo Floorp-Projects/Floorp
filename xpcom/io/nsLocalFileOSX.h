4673470d7d221f14a131e5446e98ec3cbdcf2a0c
Josh Aas <joshmoz@gmail.com>
2009-07-31 16:49:48 -0400

    Backed out changeset ad9a4a3a5409, bug 506812. CLOSED TREE

--- xpcom/io/nsLocalFileOSX.h
+++ xpcom/io/nsLocalFileOSX.h
@@ -43,6 +43,8 @@
 #include "nsString.h"
 #include "nsIHashable.h"
 
+class nsDirEnumerator;
+
 // Mac OS X 10.4 does not have stat64/lstat64
 #if defined(HAVE_STAT64) && defined(HAVE_LSTAT64) && (MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_4)
 #define STAT stat64
@@ -69,6 +71,8 @@
 class NS_COM nsLocalFile : public nsILocalFileMac,
                            public nsIHashable
 {
+  friend class nsDirEnumerator;
+    
 public:
   NS_DEFINE_STATIC_CID_ACCESSOR(NS_LOCAL_FILE_CID)
 
