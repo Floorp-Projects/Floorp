9264c0d7128c33c1fba8b53e04a686e374506e83
L. David Baron <dbaron@dbaron.org>
2009-07-31 19:37:14 -0700

    Backed out changeset 6a5f22ccbe0e (no bug) to see if it is the cause of the mozilla-browser-chrome orange (bug 507605 and bug 507698)

--- browser/components/sessionstore/test/browser/browser_483330.js
+++ browser/components/sessionstore/test/browser/browser_483330.js
@@ -10,7 +10,7 @@ function test() {
 
   let browser = tab.linkedBrowser;
   browser.addEventListener("load", function loadListener(e) {
-    browser.removeEventListener("load", arguments.callee, true);
+    browser.removeEventListener("load", arguments.callee, false);
 
     // Scroll the content document
     browser.contentWindow.scrollTo(1100, 1200);
@@ -21,7 +21,7 @@ function test() {
 
     let newTab = ss.undoCloseTab(window, 0);
     newTab.addEventListener("SSTabRestored", function tabRestored(e) {
-      newTab.removeEventListener("SSTabRestored", arguments.callee, true);
+      newTab.removeEventListener("SSTabRestored", arguments.callee, false);
 
       let newBrowser = newTab.linkedBrowser;
 
