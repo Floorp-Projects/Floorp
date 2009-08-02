9e3365bba28efa19ac3e2148145b29c3271f0274
MattN <mnoorenberghe@mozilla.com>
2009-08-01 17:30:27 -0700

    Bug 444728 - autocomplete disregards maxlength for input fields. r=dolske, sr=mconnor

--- toolkit/components/satchel/src/nsFormAutoComplete.js
+++ toolkit/components/satchel/src/nsFormAutoComplete.js
@@ -173,11 +173,12 @@ FormAutoComplete.prototype = {
      *
      * aInputName    -- |name| attribute from the form input being autocompleted.
      * aUntrimmedSearchString -- current value of the input
+     * aField -- nsIDOMHTMLInputElement being autocompleted (may be null if from chrome)
      * aPreviousResult -- previous search result, if any.
      *
      * Returns: an nsIAutoCompleteResult
      */
-    autoCompleteSearch : function (aInputName, aUntrimmedSearchString, aPreviousResult) {
+    autoCompleteSearch : function (aInputName, aUntrimmedSearchString, aField, aPreviousResult) {
         function sortBytotalScore (a, b) {
             let x = a.totalScore;
             let y = b.totalScore;
@@ -222,6 +223,11 @@ FormAutoComplete.prototype = {
             this.log("Creating new autocomplete search result.");
             let entries = this.getAutoCompleteValues(aInputName, searchString);
             result = new FormAutoCompleteResult(this._formHistory, entries, aInputName, aUntrimmedSearchString);
+            if (aField && aField.maxLength > -1) {
+                let original = result.wrappedJSObject.entries;
+                let filtered = original.filter(function (el) el.text.length <= this.maxLength, aField);
+                result.wrappedJSObject.entries = filtered;
+            }
         }
 
         return result;
