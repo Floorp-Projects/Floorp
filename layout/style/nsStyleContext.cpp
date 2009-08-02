6eeafcfd4e6c6c891cda8e703dcf9743377ddfb1
Daniel Holbert <dholbert@cs.stanford.edu>
2009-08-02 15:32:06 -0700

    Bug 507695: Remove unneeded #includes from nsStyleContext.cpp. r=dbaron

--- layout/style/nsStyleContext.cpp
+++ layout/style/nsStyleContext.cpp
@@ -43,18 +43,13 @@
 #include "nsString.h"
 #include "nsPresContext.h"
 #include "nsIStyleRule.h"
-#include "nsCRT.h"
 
 #include "nsCOMPtr.h"
 #include "nsStyleSet.h"
 #include "nsIPresShell.h"
-#include "prenv.h"
 
 #include "nsRuleNode.h"
 #include "nsStyleContext.h"
-#include "imgIRequest.h"
-
-#include "nsPrintfCString.h"
 
 #ifdef DEBUG
 // #define NOISY_DEBUG
