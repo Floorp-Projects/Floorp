c2440a4cf33ea9305b66bed139483b38dc346491
Blake Kaplan <mrbkap@gmail.com>
2009-08-06 20:25:13 -0700

    Bug 504877 - Check for UniversalXPConnect access further down on the stack chain. r=jst sr=bzbarsky

--- js/src/xpconnect/src/XPCNativeWrapper.cpp
+++ js/src/xpconnect/src/XPCNativeWrapper.cpp
@@ -205,17 +205,7 @@ EnsureLegalActivity(JSContext *cx, JSObject *obj,
   JSStackFrame *fp;
   nsIPrincipal *subjectPrincipal = ssm->GetCxSubjectPrincipalAndFrame(cx, &fp);
   if (!subjectPrincipal || !fp) {
-    // We must allow the access if there is no code running.
-    return JS_TRUE;
-  }
-
-  // This might be chrome code or content code with UniversalXPConnect.
-  void *annotation = JS_GetFrameAnnotation(cx, fp);
-  PRBool isPrivileged = PR_FALSE;
-  nsresult rv = subjectPrincipal->IsCapabilityEnabled("UniversalXPConnect",
-                                                      annotation,
-                                                      &isPrivileged);
-  if (NS_SUCCEEDED(rv) && isPrivileged) {
+    // We must allow access if there is no code running.
     return JS_TRUE;
   }
 
@@ -227,6 +217,13 @@ EnsureLegalActivity(JSContext *cx, JSObject *obj,
     PRBool subsumes;
     if (NS_FAILED(subjectPrincipal->Subsumes(objectPrincipal, &subsumes)) ||
         !subsumes) {
+      // This might be chrome code or content code with UniversalXPConnect.
+      PRBool isPrivileged = PR_FALSE;
+      nsresult rv =
+        ssm->IsCapabilityEnabled("UniversalXPConnect", &isPrivileged);
+      if (NS_SUCCEEDED(rv) && isPrivileged) {
+        return JS_TRUE;
+      }
 
       JSObject* flatObj;
       if (!JSVAL_IS_VOID(id) &&
@@ -288,26 +285,15 @@ XPCNativeWrapper::GetWrappedNative(JSContext *cx, JSObject *obj,
     return JS_TRUE;
   }
 
-  if (fp) {
-    void *annotation = JS_GetFrameAnnotation(cx, fp);
-
-    PRBool isPrivileged;
-    nsresult rv =
-      subjectPrincipal->IsCapabilityEnabled("UniversalXPConnect",
-                                            annotation,
-                                            &isPrivileged);
-    if (NS_SUCCEEDED(rv) && isPrivileged) {
-      return JS_TRUE;
-    }
-  }
-
   XPCWrappedNativeScope *scope = wn->GetScope();
   nsIPrincipal *objectPrincipal = scope->GetPrincipal();
 
   PRBool subsumes;
   nsresult rv = subjectPrincipal->Subsumes(objectPrincipal, &subsumes);
   if (NS_FAILED(rv) || !subsumes) {
-    return JS_FALSE;
+    PRBool isPrivileged;
+    rv = ssm->IsCapabilityEnabled("UniversalXPConnect", &isPrivileged);
+    return NS_SUCCEEDED(rv) && isPrivileged;
   }
 
   return JS_TRUE;
--- js/src/xpconnect/src/XPCSystemOnlyWrapper.cpp
+++ js/src/xpconnect/src/XPCSystemOnlyWrapper.cpp
@@ -164,6 +164,7 @@ GetWrappedObject(JSContext *cx, JSObject *wrapper)
 JSBool
 AllowedToAct(JSContext *cx, jsval idval)
 {
+  // TODO bug 508928: Refactor this with the XOW security checking code.
   nsIScriptSecurityManager *ssm = XPCWrapper::GetSecurityManager();
   if (!ssm) {
     return JS_TRUE;
@@ -187,13 +188,10 @@ AllowedToAct(JSContext *cx, jsval idval)
     fp = nsnull;
   }
 
-  void *annotation = fp ? JS_GetFrameAnnotation(cx, fp) : nsnull;
   PRBool privileged;
-  if (NS_SUCCEEDED(principal->IsCapabilityEnabled("UniversalXPConnect",
-                                                  annotation,
-                                                  &privileged)) &&
+  if (NS_SUCCEEDED(ssm->IsSystemPrincipal(principal, &privileged)) &&
       privileged) {
-    // UniversalXPConnect things are allowed to touch us.
+    // Chrome things are allowed to touch us.
     return JS_TRUE;
   }
 
@@ -216,6 +214,12 @@ AllowedToAct(JSContext *cx, jsval idval)
       JS_ReportError(cx, "Permission denied to access property '%hs' from a non-chrome context",
                      JS_GetStringChars(str));
     }
+  }
+
+  // Before we throw, check for UniversalXPConnect.
+  nsresult rv = ssm->IsCapabilityEnabled("UniversalXPConnect", &privileged);
+  if (NS_SUCCEEDED(rv) && privileged) {
+    return JS_TRUE;
   }
 
   return JS_FALSE;
