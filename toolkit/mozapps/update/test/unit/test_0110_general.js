dd34af6b41a61045f41b2d59704914d5e9160dbf
Robert Strong <robert.bugzilla@gmail.com>
2009-08-07 13:19:58 -0700

    Bug 507339 - Test more files in the complete / partial mar files and check file permissions. r=blassey, r=me

--- toolkit/mozapps/update/test/unit/test_0110_general.js
+++ toolkit/mozapps/update/test/unit/test_0110_general.js
@@ -38,15 +38,63 @@
 
 /* General Complete MAR File Patch Apply Test */
 
+var gTestFiles = [
+{
+  fileName         : "1_exe1.exe",
+  destinationDir   : "mar_test/1/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : "data/aus-0111_general_ref_image.png",
+  compareFile      : "data/aus-0110_general_ref_image.png",
+  originalPerms    : 0777,
+  comparePerms     : 0755
+}, {
+  fileName         : "1_1_image1.png",
+  destinationDir   : "mar_test/1/1_1/",
+  originalContents : null,
+  compareContents  : null,
+  originalFile     : null,
+  compareFile      : "data/aus-0110_general_ref_image.png",
+  originalPerms    : 0776,
+  comparePerms     : 0644
+}, {
+  fileName         : "1_1_text1",
+  destinationDir   : "mar_test/1/1_1/",
+  originalContents : "ToBeReplacedWithToBeModified\n",
+  compareContents  : "ToBeModified\n",
+  originalFile     : null,
+  compareFile      : null,
+  originalPerms    : 0775,
+  comparePerms     : 0644
+}, {
+  fileName         : "1_1_text2",
+  destinationDir   : "mar_test/1/1_1/",
+  originalContents : "ToBeReplacedWithToBeDeleted\n",
+  compareContents  : "ToBeDeleted\n",
+  originalFile     : null,
+  compareFile      : null,
+  originalPerms    : 0677,
+  comparePerms     : 0644
+}, {
+  fileName         : "2_1_text1",
+  destinationDir   : "mar_test/2/2_1/",
+  originalContents : "ToBeReplacedWithToBeDeleted\n",
+  compareContents  : "ToBeDeleted\n",
+  originalFile     : null,
+  compareFile      : null,
+  originalPerms    : 0767,
+  comparePerms     : 0644
+}];
+
 function run_test() {
+  var testFile;
   // The directory the updates will be applied to is the current working
   // directory and not dist/bin.
   var testDir = do_get_file("mar_test", true);
   // The mar files were created with all files in a subdirectory named
   // mar_test... clear it out of the way if it exists and then create it.
   try {
-    if (testDir.exists())
-      testDir.remove(true);
+    removeDirRecursive(testDir);
   }
   catch (e) {
     dump("Unable to remove directory\npath: " + testDir.path +
@@ -54,13 +102,36 @@ function run_test() {
   }
   dump("Testing: successful removal of the directory used to apply the mar file\n");
   do_check_false(testDir.exists());
-  testDir = do_get_file("mar_test/1/1_1/", true);
-  testDir.create(AUS_Ci.nsIFile.DIRECTORY_TYPE, PERMS_DIRECTORY);
 
-  // Create an empty test file to test the complete mar's ability to replace an
-  // existing file.
-  var testFile = do_get_file("mar_test/1/1_1/1_1_text1", true);
-  testFile.create(AUS_Ci.nsIFile.NORMAL_FILE_TYPE, PERMS_FILE);
+  // Create the files to test the complete mar's ability to replace files.
+  for (var i = 0; i < gTestFiles.length; i++) {
+    var f = gTestFiles[i];
+    if (f.originalFile || f.originalContents) {
+      testDir = do_get_file(f.destinationDir, true);
+      if (!testDir.exists())
+        testDir.create(AUS_Ci.nsIFile.DIRECTORY_TYPE, PERMS_DIRECTORY);
+
+      if (f.originalFile) {
+        testFile = do_get_file(f.originalFile);
+        testFile.copyTo(testDir, f.fileName);
+        testFile = do_get_file(f.destinationDir + f.fileName);
+      }
+      else {
+        testFile = do_get_file(f.destinationDir + f.fileName, true);
+        writeFile(testFile, f.originalContents);
+      }
+
+      // Skip these tests on Windows (includes WinCE) and OS/2 since their
+      // implementaions of chmod doesn't really set permissions.
+      if (!IS_WIN && !IS_OS2 && f.originalPerms) {
+        testFile.permissions = f.originalPerms;
+        // Store the actual permissions on the file for reference later after
+        // setting the permissions.
+        if (!f.comparePerms)
+          f.comparePerms = testFile.permissions;
+      }
+    }
+  }
 
   var binDir = getGREDir();
 
@@ -80,101 +151,68 @@ function run_test() {
   }
 
   // Use a directory outside of dist/bin to lessen the garbage in dist/bin
-  var updatesSubDir = do_get_file("0110_complete_mar", true);
+  var updatesDir = do_get_file("0110_complete_mar", true);
   try {
     // Mac OS X intermittently fails when removing the dir where the updater
     // binary was launched.
-    if (updatesSubDir.exists())
-      updatesSubDir.remove(true);
+    removeDirRecursive(updatesDir);
   }
   catch (e) {
-    dump("Unable to remove directory\npath: " + updatesSubDir.path +
+    dump("Unable to remove directory\npath: " + updatesDir.path +
          "\nException: " + e + "\n");
   }
 
   var mar = do_get_file("data/aus-0110_general.mar");
-  mar.copyTo(updatesSubDir, "update.mar");
+  mar.copyTo(updatesDir, "update.mar");
 
   // apply the complete mar and check the innards of the files
-  var exitValue = runUpdate(updatesSubDir, updater);
+  var exitValue = runUpdate(updatesDir, updater);
   dump("Testing: updater binary process exitValue for success when applying " +
        "a complete mar\n");
   do_check_eq(exitValue, 0);
 
   dump("Testing: update.status should be set to STATE_SUCCEEDED\n");
-  testFile = updatesSubDir.clone();
+  testFile = updatesDir.clone();
   testFile.append("update.status");
   do_check_eq(readFile(testFile).split("\n")[0], STATE_SUCCEEDED);
 
   dump("Testing: contents of files added by a complete mar\n");
-  do_check_eq(getFileBytes(do_get_file("mar_test/1/1_1/1_1_text1", true)),
-              "ToBeModified\n");
-  do_check_eq(getFileBytes(do_get_file("mar_test/1/1_1/1_1_text2", true)),
-              "ToBeDeleted\n");
+  for (i = 0; i < gTestFiles.length; i++) {
+    f = gTestFiles[i];
+    testFile = do_get_file(f.destinationDir + f.fileName, true);
+    dump("Testing: " + testFile.path + "\n");
+    if (f.compareFile || f.compareContents) {
+      do_check_true(testFile.exists());
 
-  var refImage = do_get_file("data/aus-0110_general_ref_image.png");
-  var srcImage = do_get_file("mar_test/1/1_1/1_1_image1.png", true);
-  do_check_eq(getFileBytes(srcImage), getFileBytes(refImage));
+      // Skip these tests on Windows (includes WinCE) and OS/2 since their
+      // implementaions of chmod doesn't really set permissions.
+      if (!IS_WIN && !IS_OS2 && f.comparePerms) {
+        // Check if the permssions as set in the complete mar file are correct.
+        if (f.originalPerms)
+          dump("original permissions: " + f.originalPerms.toString(8) + "\n");
+        dump("compare permissions : " + f.comparePerms.toString(8) + "\n");
+        dump("updated permissions : " + testFile.permissions.toString(8) + "\n");
+        do_check_eq(testFile.permissions & 0xfff, f.comparePerms & 0xfff);
+      }
 
-  do_check_eq(getFileBytes(do_get_file("mar_test/2/2_1/2_1_text1", true)),
-              "ToBeDeleted\n");
-
-  try {
-    // Mac OS X intermittently fails when removing the dir where the updater
-    // binary was launched.
-    if (updatesSubDir.exists())
-      updatesSubDir.remove(true);
-  }
-  catch (e) {
-    dump("Unable to remove directory\npath: " + updatesSubDir.path +
-         "\nException: " + e + "\n");
+      if (f.compareFile) {
+        do_check_eq(readFileBytes(testFile),
+                    readFileBytes(do_get_file(f.compareFile)));
+        if (f.originalFile) {
+          // Verify that readFileBytes returned the entire contents by checking
+          // the contents against the original file.
+          do_check_neq(readFileBytes(testFile),
+                       readFileBytes(do_get_file(f.originalFile)));
+        }
+      }
+      else {
+        do_check_eq(readFileBytes(testFile), f.compareContents);
+      }
+    }
+    else {
+      do_check_false(testFile.exists());
+    }
   }
 
   cleanUp();
-}
-
-// Launches the updater binary to apply a mar file
-function runUpdate(aUpdatesSubDir, aUpdater) {
-  // Copy the updater binary to the update directory so the updater.ini is not
-  // in the same directory as it is. This prevents ui from displaying and the
-  // PostUpdate executable which is defined in the updater.ini from launching.
-  aUpdater.copyTo(aUpdatesSubDir, aUpdater.leafName);
-  var updateBin = aUpdatesSubDir.clone();
-  updateBin.append(aUpdater.leafName);
-  if (updateBin.leafName == "updater.app") {
-    updateBin.append("Contents");
-    updateBin.append("MacOS");
-    updateBin.append("updater");
-    if (!updateBin.exists())
-      do_throw("Unable to find the updater executable!");
-  }
-
-  var updatesSubDirPath = aUpdatesSubDir.path;
-  if (/ /.test(updatesSubDirPath))
-    updatesSubDirPath = '"' + updatesSubDirPath + '"';
-
-  var cwdPath = do_get_file("/", true).path;
-  if (/ /.test(cwdPath))
-    cwdPath = '"' + cwdPath + '"';
-
-  var process = AUS_Cc["@mozilla.org/process/util;1"].
-                createInstance(AUS_Ci.nsIProcess);
-  process.init(updateBin);
-  var args = [updatesSubDirPath, 0, cwdPath];
-  process.run(true, args, args.length);
-  return process.exitValue;
-}
-
-// Returns the binary contents of a file
-function getFileBytes(aFile) {
-  var fis = AUS_Cc["@mozilla.org/network/file-input-stream;1"].
-            createInstance(AUS_Ci.nsIFileInputStream);
-  fis.init(aFile, -1, -1, false);
-  var bis = AUS_Cc["@mozilla.org/binaryinputstream;1"].
-            createInstance(AUS_Ci.nsIBinaryInputStream);
-  bis.setInputStream(fis);
-  var data = bis.readBytes(bis.available());
-  bis.close();
-  fis.close();
-  return data;
 }
