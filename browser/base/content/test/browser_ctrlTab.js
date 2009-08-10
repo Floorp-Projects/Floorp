14bc32191a9a44e8c3cda10ecd7e0d336ea87d10
Dão Gottwald <dao@mozilla.com>
2009-08-10 14:32:07 +0200

    fix another focus issue with browser_bug304198.js and potentially browser_ctrlTab.js on Linux

--- browser/base/content/test/browser_ctrlTab.js
+++ browser/base/content/test/browser_ctrlTab.js
@@ -3,10 +3,7 @@ function test() {
 
   if (Cc["@mozilla.org/focus-manager;1"].getService(Ci.nsIFocusManager).activeWindow !=
       window) {
-    window.addEventListener("focus", function () {
-      window.removeEventListener("focus", arguments.callee, false);
-      test();
-    }, false);
+    setTimeout(test, 0);
     window.focus();
     return;
   }
