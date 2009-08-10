8778526f9f0d1f244ab4cd8314dac835088d8279
Jason Orendorff <jorendorff@mozilla.com>
2009-08-10 14:00:46 -0500

    Bug 461269 - Remove TOK_RP nodes from the parse tree. r=brendan.

---
+++ js/tests/js1_8_1/extensions/strict-warning.js
@@ -0,0 +1,29 @@
+// Turn on strict mode and warnings-as-errors mode.
+if (options().split().indexOf('strict') == -1)
+    options('strict');
+if (options().split().indexOf('werror') == -1)
+    options('werror');
+
+function expectSyntaxError(stmt) {
+    print(stmt);
+    var result = 'no error';
+    try {
+        Function(stmt);
+    } catch (exc) {
+        result = exc.constructor.name;
+    }
+    assertEq(result, 'SyntaxError');
+}
+
+function test(expr) {
+    // Without extra parentheses, expect an error.
+    expectSyntaxError('if (' + expr + ') ;');
+
+    // Extra parentheses silence the warning/error.
+    Function('if ((' + expr + ')) ;');
+}
+
+// Overparenthesized assignment in a condition should not be a strict error.
+test('a = 0');
+test('a = (f(), g)');
+test('a = b || c > d');
