58fc1c73ed92b524e96eb7ad24ac2ee4ede94444
Maks Verver <maksverver@geocities.com>
2009-08-11 12:54:55 -0700

    Simpler parseInt logic (bug 507993 followup, r=brendan,dvander).

--- js/src/jsnum.cpp
+++ js/src/jsnum.cpp
@@ -208,16 +208,11 @@ ParseIntDouble(jsdouble d)
 {
     if (!JSDOUBLE_IS_FINITE(d))
         return js_NaN;
-
-    /* Don't preserve -0, because js_strtointeger doesn't. */
-    if (d == 0)
-        return 0;
     if (d > 0)
         return floor(d);
-    d = ceil(d);
-
-    /* ceil does not seem to return -0 if not given -0. */
-    return (d == 0) ? -0.0 : d;
+    if (d < 0)
+    	return -floor(-d);
+    return 0;
 }
 #endif
 
