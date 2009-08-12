a193677809960f8640bb00a532ad21dd4f0c7689
Jason Orendorff <jorendorff@mozilla.com>
2009-08-12 15:40:56 -0500

    Bug 508051 part 2 - Avoid imacros for JSOP_INSTANCEOF. r=gal.

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -5721,6 +5721,21 @@ function testInitelemWithSetter() {  // bug 509843
 }
 test(testInitelemWithSetter);
 
+function testDeepBailFromHasInstance() {
+    var arr = [StopIteration, StopIteration, StopIteration, StopIteration, {}];
+    var obj = {};
+    var x;
+    var result = 'no error';
+    try {
+        for (var i = 0; i < arr.length; i++)
+            x = (obj instanceof arr[i]);  // last iteration throws, triggering deep bail
+    } catch (exc) {
+        result = exc.constructor.name;
+    }
+    assertEq(result, 'TypeError');
+}
+test(testDeepBailFromHasInstance);
+
 /*****************************************************************************
  *                                                                           *
  *  _____ _   _  _____ ______ _____ _______                                  *
