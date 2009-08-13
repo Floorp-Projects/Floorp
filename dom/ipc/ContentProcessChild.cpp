e6735a6a9e349ff3bb45364a5f6661ecb487ab3e
Chris Jones <jones.chris.g@gmail.com>
2009-08-12 19:32:50 -0500

    add ContentProcessChild singleton and accessor

--- dom/ipc/ContentProcessChild.cpp
+++ dom/ipc/ContentProcessChild.cpp
@@ -1,10 +1,13 @@
-/* -*- Mode: C++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 8; -*- */
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: sw=4 ts=4 et : */
 
 #include "ContentProcessChild.h"
 #include "TabChild.h"
 
 namespace mozilla {
 namespace dom {
+
+ContentProcessChild* ContentProcessChild::sSingleton;
 
 ContentProcessChild::ContentProcessChild()
 {
@@ -17,21 +20,25 @@ ContentProcessChild::~ContentProcessChild()
 bool
 ContentProcessChild::Init(MessageLoop* aIOLoop, IPC::Channel* aChannel)
 {
-  Open(aChannel, aIOLoop);
-  return true;
+    NS_ASSERTION(!sSingleton, "only one ContentProcessChild per child");
+  
+    Open(aChannel, aIOLoop);
+    sSingleton = this;
+
+    return true;
 }
 
 IFrameEmbeddingProtocolChild*
 ContentProcessChild::IFrameEmbeddingConstructor(const MagicWindowHandle& hwnd)
 {
-  return new TabChild(hwnd);
+    return new TabChild(hwnd);
 }
 
 nsresult
 ContentProcessChild::IFrameEmbeddingDestructor(IFrameEmbeddingProtocolChild* iframe)
 {
-  delete iframe;
-  return NS_OK;
+    delete iframe;
+    return NS_OK;
 }
 
 } // namespace dom
