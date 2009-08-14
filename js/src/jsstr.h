97b1bbc7d1f46721fb9bcf1cba58f3ecdadfd01c
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2009-08-14 11:28:24 -0700

    Bug 501324 - Bug 497618 causes crashes while building with MSVC++ on Win x64.  r=jwalden

--- js/src/jsstr.h
+++ js/src/jsstr.h
@@ -135,7 +135,11 @@ private:
      *
      * ATOMIZED is used only with flat, immutable strings.
      */
-    enum {
+    enum
+#if defined(_MSC_VER) && defined(_WIN64)
+    : size_t /* VC++ 64-bit incorrectly defaults this enum's size to int. */
+#endif
+    {
         DEPENDENT =     JSSTRING_BIT(JS_BITS_PER_WORD - 1),
         PREFIX =        JSSTRING_BIT(JS_BITS_PER_WORD - 2),
         MUTABLE =       PREFIX,
@@ -145,7 +149,12 @@ private:
         LENGTH_BITS =   JS_BITS_PER_WORD - 4,
         LENGTH_MASK =   JSSTRING_BITMASK(LENGTH_BITS),
 
-        DEPENDENT_LENGTH_BITS = LENGTH_BITS / 2,
+        /*
+         * VC++ 64-bit incorrectly produces the compiler error "Conversion to
+         * enumeration type requires an explicit cast" unless we cast to size_t
+         * here.
+         */
+        DEPENDENT_LENGTH_BITS = size_t(LENGTH_BITS) / 2,
         DEPENDENT_LENGTH_MASK = JSSTRING_BITMASK(DEPENDENT_LENGTH_BITS),
         DEPENDENT_START_BITS =  LENGTH_BITS - DEPENDENT_LENGTH_BITS,
         DEPENDENT_START_SHIFT = DEPENDENT_LENGTH_BITS,
@@ -157,7 +166,11 @@ private:
     }
 
 public:
-    enum {
+    enum
+#if defined(_MSC_VER) && defined(_WIN64)
+    : size_t /* VC++ 64-bit incorrectly defaults this enum's size to int. */
+#endif
+    {
         MAX_LENGTH = LENGTH_MASK,
         MAX_DEPENDENT_START = DEPENDENT_START_MASK,
         MAX_DEPENDENT_LENGTH = DEPENDENT_LENGTH_MASK
