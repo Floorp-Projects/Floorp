0930d522d8ffeb6d523514623fbb51b3ac222892
Edward Lee <edilee@mozilla.com>
2009-08-15 01:07:40 -0700

    Reparent orphans when creating the parent folder.

--- services/sync/modules/engines/bookmarks.js
+++ services/sync/modules/engines/bookmarks.js
@@ -227,6 +227,18 @@ BookmarksStore.prototype = {
     }
   },
 
+  /**
+   * Find all ids of items that have a given value for an annotation
+   */
+  _findAnnoItems: function BStore__findAnnoItems(anno, val) {
+    // XXX Work around Bug 510628 by prepending parenT
+    if (anno == PARENT_ANNO)
+      val = "T" + val;
+
+    return Svc.Annos.getItemsWithAnnotation(anno, {}).filter(function(id)
+      Utils.anno(id, anno) == val);
+  },
+
   create: function BStore_create(record) {
     let newId;
     switch (record.type) {
@@ -285,6 +297,15 @@ BookmarksStore.prototype = {
 
     this._log.trace("Setting GUID of new item " + newId + " to " + record.id);
     this._setGUID(newId, record.id);
+
+    // Find orphans and reunite with this new folder parent
+    if (record.type == "folder") {
+      let orphans = this._findAnnoItems(PARENT_ANNO, record.id);
+      this._log.debug("Reparenting orphans " + orphans + " to " + record.title);
+      orphans.forEach(function(orphan) {
+        Svc.Bookmark.moveItem(orphan, newId, -1);
+      });
+    }
   },
 
   remove: function BStore_remove(record) {
