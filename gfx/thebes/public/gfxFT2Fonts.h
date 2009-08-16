b025f9e8de6bcd50fa305a398d5142bbed8a2a47
Jonathan Kew <jfkthame@gmail.com>
2009-08-16 18:12:12 +0100

    further winmo (gfxFT2Fonts) bustage fix from bug 493280

--- gfx/thebes/public/gfxFT2Fonts.h
+++ gfx/thebes/public/gfxFT2Fonts.h
@@ -63,7 +63,8 @@ public:
     FontEntry *FindFontEntry(const gfxFontStyle& aFontStyle);
 
 protected:
-    virtual PRBool FindWeightsForStyle(gfxFontEntry* aFontsForWeights[], const gfxFontStyle& aFontStyle);
+    virtual PRBool FindWeightsForStyle(gfxFontEntry* aFontsForWeights[],
+                                       PRBool anItalic, PRInt16 aStretch);
 
 public:
     nsString mName;
