9d1e36e1cce1cc47beb415c5ba3d71689839d1ac
Vladimir Vukicevic <vladimir@pobox.com>
2009-08-20 12:55:21 -0700

    b=506470, Mac alerts service should not initialize Growl on app startup; r=sdwilsh

--- toolkit/components/alerts/src/mac/nsAlertsServiceModule.cpp
+++ toolkit/components/alerts/src/mac/nsAlertsServiceModule.cpp
@@ -45,56 +45,12 @@
 
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsAlertsService, Init)
 
-static
-NS_METHOD
-nsAlertsServiceRegister(nsIComponentManager* aCompMgr,
-                        nsIFile* aPath,
-                        const char* registryLocation,
-                        const char* componentType,
-                        const nsModuleComponentInfo* info)
-{
-  nsresult rv;
-
-  nsCOMPtr<nsICategoryManager> catman =
-    do_GetService(NS_CATEGORYMANAGER_CONTRACTID, &rv);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  char* prev = nsnull;
-  rv = catman->AddCategoryEntry("app-startup", "nsAlertsService",
-                                "service," NS_ALERTSERVICE_CONTRACTID, PR_TRUE,
-                                PR_TRUE, &prev);
-  if (prev)
-    nsMemory::Free(prev);
-
-  return rv;
-}
-
-static
-NS_METHOD
-nsAlertsServiceUnregister(nsIComponentManager* aCompMgr,
-                          nsIFile* aPath,
-                          const char* registryLocation,
-                          const nsModuleComponentInfo* info)
-{
-  nsresult rv;
-
-  nsCOMPtr<nsICategoryManager> catman =
-    do_GetService(NS_CATEGORYMANAGER_CONTRACTID, &rv);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  rv = catman->DeleteCategoryEntry("app-startup", "nsAlertsService", PR_TRUE);
-
-  return rv;
-}
-
 static const nsModuleComponentInfo components[] =
 {
   { "Alerts Service",
     NS_ALERTSSERVICE_CID,
     NS_ALERTSERVICE_CONTRACTID,
-    nsAlertsServiceConstructor,
-    nsAlertsServiceRegister,
-    nsAlertsServiceUnregister },
+    nsAlertsServiceConstructor },
 };
 
 NS_IMPL_NSGETMODULE(nsAlertsServiceModule, components)
