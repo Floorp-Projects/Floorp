9af3ac5ed37d7ae0e60c615c5d44acb2416f9ccf
Graydon Hoare <graydon@mozilla.com>
2009-08-21 13:51:55 -0700

    Bug 511972 - More Fragment cleanups, r=dvander.

--- js/src/nanojit/Fragmento.cpp
+++ js/src/nanojit/Fragmento.cpp
@@ -53,22 +53,6 @@ namespace nanojit
     //
     Fragment::Fragment(const void* _ip)
         :
-#ifdef NJ_VERBOSE
-          _called(0),
-          _native(0),
-          _exitNative(0),
-          _lir(0),
-          _lirbytes(0),
-          _token(NULL),
-          traceTicks(0),
-          interpTicks(0),
-          eot_target(NULL),
-          sid(0),
-          compileNbr(0),
-#endif
-          treeBranches(NULL),
-          branches(NULL),
-          nextbranch(NULL),
           anchor(NULL),
           root(NULL),
           parent(NULL),
@@ -76,11 +60,8 @@ namespace nanojit
           peer(NULL),
           lirbuf(NULL),
           lastIns(NULL),
-          spawnedFrom(NULL),
           kind(LoopTrace),
           ip(_ip),
-          guardCount(0),
-          xjumpCount(0),
           recordAttempts(0),
           fragEntry(NULL),
           loopEntry(NULL),
