8b25c2ca128c0fcd424c30d9db5859bdb45a8683
Graydon Hoare <graydon@mozilla.com>
2009-08-20 17:22:47 -0700

    Bug 511309 - Delete class Fragmento and all uses of it, r=dvander.

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -98,8 +98,10 @@ namespace nanojit {
     class Assembler;
     class CodeAlloc;
     class Fragment;
-    class Fragmento;
     class LirBuffer;
+#ifdef DEBUG
+    class LabelMap;
+#endif
     extern "C++" { template<typename K, typename V, typename H> class HashMap; }
 }
 class TraceRecorder;
@@ -148,10 +150,13 @@ struct JSTraceMonitor {
     JSContext               *tracecx;
 
     CLS(nanojit::LirBuffer) lirbuf;
-    CLS(nanojit::Fragmento) fragmento;
     CLS(VMAllocator)        allocator;   // A chunk allocator for LIR.
     CLS(nanojit::CodeAlloc) codeAlloc;   // A general allocator for native code.
     CLS(nanojit::Assembler) assembler;
+#ifdef DEBUG
+    CLS(nanojit::LabelMap)  labels;
+#endif
+
     CLS(TraceRecorder)      recorder;
     jsval                   *reservedDoublePool;
     jsval                   *reservedDoublePoolPtr;
@@ -187,8 +192,10 @@ struct JSTraceMonitor {
     CLS(nanojit::CodeAlloc) reCodeAlloc;
     CLS(nanojit::Assembler) reAssembler;
     CLS(nanojit::LirBuffer) reLirBuf;
-    CLS(nanojit::Fragmento) reFragmento;
     CLS(REHashMap)          reFragments;
+#ifdef DEBUG
+    CLS(nanojit::LabelMap)  reLabels;
+#endif
 
     /* Keep a list of recorders we need to abort on cache flush. */
     CLS(TraceRecorder)      abortStack;
