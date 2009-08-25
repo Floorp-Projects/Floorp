614a0602cf6b24456720386edc3f0adf591b23d4
Andreas Gal <gal@mozilla.com>
2009-08-24 18:24:52 -0700

    Memory-pressure based GC scheduler (506125, r=igor,jwalden).

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -1799,6 +1799,7 @@ js_TriggerAllOperationCallbacks(JSRuntime *rt, JSBool gcLocked)
     if (!gcLocked)
         JS_LOCK_GC(rt);
 #endif
+
     iter = NULL;
     while ((acx = js_ContextIterator(rt, JS_FALSE, &iter)))
         JS_TriggerOperationCallback(acx);
