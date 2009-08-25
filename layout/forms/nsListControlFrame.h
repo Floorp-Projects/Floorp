fa97ceb453d7ed513d7ace70675a4b1dfb399697
Timothy Nikkel <tnikkel@gmail.com>
2009-08-25 12:07:57 -0700

    Bug 511323. If a dropdown combobox (select) is given a translucent background color make sure to draw an opaque color on the dropdown's opaque widget so we don't get garbage painted. r=roc

--- layout/forms/nsListControlFrame.h
+++ layout/forms/nsListControlFrame.h
@@ -443,11 +443,15 @@ protected:
   // about the extra member on listboxes?
   nscoord mLastDropdownComputedHeight;
 
+  // At the time of our last dropdown, the backstop color to draw in case we
+  // are translucent.
+  nscolor mLastDropdownBackstopColor;
+  
   nsRefPtr<nsListEventListener> mEventListener;
 
   static nsListControlFrame * mFocused;
   static nsString * sIncrementalString;
-  
+
 #ifdef DO_REFLOW_COUNTER
   PRInt32 mReflowId;
 #endif
