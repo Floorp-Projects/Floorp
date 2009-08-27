4a8a2a318be1ee77a814efa80405ea7967b069f2
Benjamin Smedberg <benjamin@smedbergs.us>
2009-08-27 12:38:33 -0700

    Bug 503451 - remove GeckoActiveXObject/COMObject, and similar functions which aren't usable by content except that content can still detect which COM objects are present on the system r=mrbkap sr=jst

--- js/src/xpconnect/src/xpcprivate.h
+++ js/src/xpconnect/src/xpcprivate.h
@@ -704,11 +704,6 @@ public:
         IDX_PROTO                   ,
         IDX_ITERATOR                ,
         IDX_PARENT                  ,
-#ifdef XPC_IDISPATCH_SUPPORT
-        IDX_ACTIVEX_OBJECT          ,
-        IDX_COM_OBJECT              ,
-        IDX_ACTIVEX_SUPPORTS        ,
-#endif
         IDX_TOTAL_COUNT // just a count of the above
     };
 
