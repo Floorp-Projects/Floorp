5aca2348ead00d2ad25e555ea20efceadcde74e9
Jonathan Watt <jwatt@jwatt.org>
2009-08-26 20:53:35 -0700

    Bug 508179. Implement 'pointer-events:none;' for all elements. r=roc, r=dbaron.

--- layout/style/nsCSSParser.cpp
+++ layout/style/nsCSSParser.cpp
@@ -5837,9 +5837,6 @@ CSSParserImpl::ParseSingleValueProperty(nsCSSValue& aValue,
     return ParseVariant(aValue, VARIANT_HUO, nsnull);
   case eCSSProperty_mask:
     return ParseVariant(aValue, VARIANT_HUO, nsnull);
-  case eCSSProperty_pointer_events:
-    return ParseVariant(aValue, VARIANT_HOK,
-                        nsCSSProps::kPointerEventsKTable);
   case eCSSProperty_shape_rendering:
     return ParseVariant(aValue, VARIANT_AHK,
                         nsCSSProps::kShapeRenderingKTable);
@@ -6022,6 +6019,9 @@ CSSParserImpl::ParseSingleValueProperty(nsCSSValue& aValue,
     return ParseVariant(aValue, VARIANT_HKF, nsCSSProps::kPitchKTable);
   case eCSSProperty_pitch_range:
     return ParseVariant(aValue, VARIANT_HN, nsnull);
+  case eCSSProperty_pointer_events:
+    return ParseVariant(aValue, VARIANT_HOK,
+                        nsCSSProps::kPointerEventsKTable);
   case eCSSProperty_position:
     return ParseVariant(aValue, VARIANT_HK, nsCSSProps::kPositionKTable);
   case eCSSProperty_richness:
