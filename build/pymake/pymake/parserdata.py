1b3327c26d2853f2e673a67990fb55db84235adc
Benjamin Smedberg <benjamin@smedbergs.us>
2009-08-27 08:53:13 -0700

    Additional followup to bug 398573 - remove REQUIRES from the tree even when they are in makefile conditional blocks

--- build/pymake/pymake/parserdata.py
+++ build/pymake/pymake/parserdata.py
@@ -493,4 +493,5 @@ def iterstatements(stmts):
     for s in stmts:
         yield s
         if isinstance(s, ConditionBlock):
-            for c, ss in iterstatements(s): yield ss
+            for c, sl in iterstatements(s):
+                for s2 in sl: yield s2
