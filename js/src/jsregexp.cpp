05ba27f00da06edc60eed9d48792b36f086e94b6
Ben Newman <bnewman@mozilla.com>
2009-08-27 18:07:44 -0700

    Bug 500644 - PAC: crash when using PAC-based manual proxy config and the attached testcase. r=mrbkap sr=brendan

--- js/src/jsregexp.cpp
+++ js/src/jsregexp.cpp
@@ -5135,17 +5135,25 @@ js_InitRegExpStatics(JSContext *cx)
 }
 
 JS_FRIEND_API(void)
-js_SaveRegExpStatics(JSContext *cx, JSRegExpStatics *statics,
-                     JSTempValueRooter *tvr)
+js_SaveAndClearRegExpStatics(JSContext *cx, JSRegExpStatics *statics,
+                             JSTempValueRooter *tvr)
 {
   *statics = cx->regExpStatics;
   JS_PUSH_TEMP_ROOT_STRING(cx, statics->input, tvr);
+  /*
+   * Prevent JS_ClearRegExpStatics from freeing moreParens, since we've only
+   * moved it elsewhere (into statics->moreParens).
+   */
+  cx->regExpStatics.moreParens = NULL;
+  JS_ClearRegExpStatics(cx);
 }
 
 JS_FRIEND_API(void)
 js_RestoreRegExpStatics(JSContext *cx, JSRegExpStatics *statics,
                         JSTempValueRooter *tvr)
 {
+  /* Clear/free any new JSRegExpStatics data before clobbering. */
+  JS_ClearRegExpStatics(cx);
   cx->regExpStatics = *statics;
   JS_POP_TEMP_ROOT(cx, tvr);
 }
@@ -5162,12 +5170,7 @@ js_TraceRegExpStatics(JSTracer *trc, JSContext *acx)
 void
 js_FreeRegExpStatics(JSContext *cx)
 {
-    JSRegExpStatics *res = &cx->regExpStatics;
-
-    if (res->moreParens) {
-        cx->free(res->moreParens);
-        res->moreParens = NULL;
-    }
+    JS_ClearRegExpStatics(cx);
     JS_FinishArenaPool(&cx->regexpPool);
 }
 
