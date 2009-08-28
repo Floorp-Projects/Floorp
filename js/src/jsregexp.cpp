5dee786b99b8671c5bb6c8b980f437e7c2d2f41f
Ben Newman <bnewman@mozilla.com>
2009-08-28 15:34:31 -0700

    Trivial indentation fixups that should have gone in with 9e16ec20e6fe. r=brendan

--- js/src/jsregexp.cpp
+++ js/src/jsregexp.cpp
@@ -5138,24 +5138,24 @@ JS_FRIEND_API(void)
 js_SaveAndClearRegExpStatics(JSContext *cx, JSRegExpStatics *statics,
                              JSTempValueRooter *tvr)
 {
-  *statics = cx->regExpStatics;
-  JS_PUSH_TEMP_ROOT_STRING(cx, statics->input, tvr);
-  /*
-   * Prevent JS_ClearRegExpStatics from freeing moreParens, since we've only
-   * moved it elsewhere (into statics->moreParens).
-   */
-  cx->regExpStatics.moreParens = NULL;
-  JS_ClearRegExpStatics(cx);
+    *statics = cx->regExpStatics;
+    JS_PUSH_TEMP_ROOT_STRING(cx, statics->input, tvr);
+    /*
+     * Prevent JS_ClearRegExpStatics from freeing moreParens, since we've only
+     * moved it elsewhere (into statics->moreParens).
+     */
+    cx->regExpStatics.moreParens = NULL;
+    JS_ClearRegExpStatics(cx);
 }
 
 JS_FRIEND_API(void)
 js_RestoreRegExpStatics(JSContext *cx, JSRegExpStatics *statics,
                         JSTempValueRooter *tvr)
 {
-  /* Clear/free any new JSRegExpStatics data before clobbering. */
-  JS_ClearRegExpStatics(cx);
-  cx->regExpStatics = *statics;
-  JS_POP_TEMP_ROOT(cx, tvr);
+    /* Clear/free any new JSRegExpStatics data before clobbering. */
+    JS_ClearRegExpStatics(cx);
+    cx->regExpStatics = *statics;
+    JS_POP_TEMP_ROOT(cx, tvr);
 }
 
 void
