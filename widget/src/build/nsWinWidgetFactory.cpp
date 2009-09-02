132d3531553d343bde789a65d847e1fb27372e96
Jesper Kristensen <bugzilla@jesperkristensen.dk>
2009-09-02 09:56:56 -0700

    Bug 513233 - Implement accelerometer support for Windows ThinkPad. r=dougt

--- widget/src/build/nsWinWidgetFactory.cpp
+++ widget/src/build/nsWinWidgetFactory.cpp
@@ -75,10 +75,8 @@
 #include "nsPrintSession.h"
 #endif
 
-#ifdef WINCE_WINDOWS_MOBILE
-#include "nsAccelerometerCE.h"
+#include "nsAccelerometerWin.h"
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsAccelerometerWin)
-#endif
 
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsWindow)
 NS_GENERIC_FACTORY_CONSTRUCTOR(ChildWindow)
@@ -176,12 +174,10 @@ static const nsModuleComponentInfo components[] =
     nsBidiKeyboardConstructor },
 #endif
 
-#ifdef WINCE_WINDOWS_MOBILE
   { "Accelerometer",
     NS_ACCELEROMETER_CID,
     NS_ACCELEROMETER_CONTRACTID,
     nsAccelerometerWinConstructor },
-#endif
 
 #ifdef NS_PRINTING
   { "nsPrintOptionsWin",
