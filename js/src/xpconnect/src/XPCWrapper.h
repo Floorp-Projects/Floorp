a73bd5ca0397eea30e9d77a91445b392b7f19bd1
Blake Kaplan <mrbkap@gmail.com>
2009-09-02 17:25:25 -0700

    Bug 505915 - Throw exceptions eagerly when we try to convert a cross-origin security wrapper to a C++ object. r=jst

--- js/src/xpconnect/src/XPCWrapper.h
+++ js/src/xpconnect/src/XPCWrapper.h
@@ -215,6 +215,15 @@ public:
     return JS_TRUE;
   }
 
+  static JSBool IsSecurityWrapper(JSObject *wrapper)
+  {
+    JSClass *clasp = STOBJ_GET_CLASS(wrapper);
+    return clasp == &sXPC_COW_JSClass.base ||
+           clasp == &sXPC_SJOW_JSClass.base ||
+           clasp == &sXPC_SOW_JSClass.base ||
+           clasp == &sXPC_XOW_JSClass.base;
+  }
+
   /**
    * Given an arbitrary object, Unwrap will return the wrapped object if the
    * passed-in object is a wrapper that Unwrap knows about *and* the
