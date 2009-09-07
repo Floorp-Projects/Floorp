d93f43845fa248aff4bcf6db6e3b1216882b97c7
Chris Pearce <chris@pearce.org.nz>
2009-09-08 10:23:11 +1200

    Bug 514573 - Free video data after paint, prevents repainting. r=doublec

--- content/media/ogg/nsOggDecoder.cpp
+++ content/media/ogg/nsOggDecoder.cpp
@@ -154,9 +154,13 @@ public:
     ~FrameData()
     {
       MOZ_COUNT_DTOR(FrameData);
+      ClearVideoHeader();
+    }
 
+    void ClearVideoHeader() {
       if (mVideoHeader) {
         oggplay_callback_info_unlock_item(mVideoHeader);
+        mVideoHeader = nsnull;
       }
     }
 
@@ -997,6 +1001,9 @@ void nsOggDecodeStateMachine::PlayVideo(FrameData* aFrame)
 
     mDecoder->SetRGBData(aFrame->mVideoWidth, aFrame->mVideoHeight,
                          mFramerate, mAspectRatio, buffer.forget());
+
+    // Don't play the frame's video data more than once.
+    aFrame->ClearVideoHeader();
   }
 }
 
