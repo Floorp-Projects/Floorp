175d63409f0f6b374858443b1663f0ecd2f854ef
Ben Turner <bent.mozilla@gmail.com>
2009-09-08 13:35:12 -0700

    Fix crashing iframes due to gtk_init, stop using windows in xpcshell tests

--- ipc/testshell/tests/test_ipcshell.js
+++ ipc/testshell/tests/test_ipcshell.js
@@ -1,15 +1,15 @@
-function checkStatus(result) {
-  if (result == "Waiting") {
-    sendCommand("observer.status;", checkStatus);
-    return;
-  }
-
-  do_check_eq(result, "Success");
+function callback(result) {
+  do_check_eq(result, Ci.nsIXULRuntime.PROCESS_TYPE_CONTENT);
   do_test_finished();
 }
 
 function run_test() {
   do_test_pending();
-  sendCommand("load('test_ipcshell_child.js'); start();");
-  sendCommand("observer.status;", checkStatus);
+
+  do_check_eq(runtime.processType, Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT);
+  
+  sendCommand("load('test_ipcshell_child.js');");
+  sendCommand("runtime.processType;", callback);
 }
+load('test_ipcshell_child.js');
+
