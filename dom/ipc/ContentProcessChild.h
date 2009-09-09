df0dc3228b8ed46b0eecabd9d6b4cbfab48a7290
Ben Turner <bent.mozilla@gmail.com>
2009-09-09 15:00:14 -0700

    Rename remaining protocols
    --HG--
    rename : dom/ipc/ContentProcess.ipdl => dom/ipc/PContentProcess.ipdl
    rename : dom/ipc/IFrameEmbedding.ipdl => dom/ipc/PIFrameEmbedding.ipdl
    rename : ipc/test-harness/Test.ipdl => ipc/test-harness/PTest.ipdl
    rename : ipc/testshell/TestShell.ipdl => ipc/testshell/PTestShell.ipdl
    rename : ipc/testshell/TestShellCommand.ipdl => ipc/testshell/PTestShellCommand.ipdl

--- dom/ipc/ContentProcessChild.h
+++ dom/ipc/ContentProcessChild.h
@@ -1,10 +1,45 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /* vim: sw=4 ts=4 et : */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Content App.
+ *
+ * The Initial Developer of the Original Code is
+ *   The Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 #ifndef mozilla_dom_ContentProcessChild_h
 #define mozilla_dom_ContentProcessChild_h
 
-#include "mozilla/dom/ContentProcessProtocolChild.h"
+#include "mozilla/dom/PContentProcessProtocolChild.h"
 
 #include "nsTArray.h"
 #include "nsAutoPtr.h"
@@ -13,7 +48,7 @@ namespace mozilla {
 namespace dom {
 
 class ContentProcessChild
-    : public ContentProcessProtocolChild
+    : public PContentProcessProtocolChild
 {
 public:
     ContentProcessChild();
@@ -26,11 +61,11 @@ public:
         return sSingleton;
     }
 
-    virtual IFrameEmbeddingProtocolChild* IFrameEmbeddingConstructor(const MagicWindowHandle& hwnd);
-    virtual nsresult IFrameEmbeddingDestructor(IFrameEmbeddingProtocolChild*);
+    virtual PIFrameEmbeddingProtocolChild* PIFrameEmbeddingConstructor(const MagicWindowHandle& hwnd);
+    virtual nsresult PIFrameEmbeddingDestructor(PIFrameEmbeddingProtocolChild*);
 
-    virtual TestShellProtocolChild* TestShellConstructor();
-    virtual nsresult TestShellDestructor(TestShellProtocolChild*);
+    virtual PTestShellProtocolChild* PTestShellConstructor();
+    virtual nsresult PTestShellDestructor(PTestShellProtocolChild*);
 
     void Quit();
     virtual nsresult RecvQuit();
@@ -38,8 +73,8 @@ public:
 private:
     static ContentProcessChild* sSingleton;
 
-    nsTArray<nsAutoPtr<IFrameEmbeddingProtocolChild> > mIFrames;
-    nsTArray<nsAutoPtr<TestShellProtocolChild> > mTestShells;
+    nsTArray<nsAutoPtr<PIFrameEmbeddingProtocolChild> > mIFrames;
+    nsTArray<nsAutoPtr<PTestShellProtocolChild> > mTestShells;
 
     PRBool mQuit;
 
