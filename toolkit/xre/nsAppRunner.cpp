c8d974adb06ee3c83eb4a48896b1c0d723cea436
Josh Aas <joshmoz@gmail.com>
2009-09-09 08:28:23 -0400

    Re-land: Get rid of unnecessary conversion from CFURLRef to FSRef. b=506812 r=vlad

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -1468,13 +1468,7 @@ XRE_GetBinaryPath(const char* argv0, nsILocalFile* *aResult)
   if (!bundleURL)
     return NS_ERROR_FAILURE;
 
-  FSRef fileRef;
-  if (!CFURLGetFSRef(bundleURL, &fileRef)) {
-    CFRelease(bundleURL);
-    return NS_ERROR_FAILURE;
-  }
-
-  rv = lfm->InitWithFSRef(&fileRef);
+  rv = lfm->InitWithCFURL(bundleURL);
   CFRelease(bundleURL);
 
   if (NS_FAILED(rv))
