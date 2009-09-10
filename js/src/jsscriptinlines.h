1782600815f1d9fc6c6bab30652815b0c26bccd6
Brendan Eich <brendan@mozilla.org>
2009-09-10 07:22:20 -0700

    Followup for 513119, r=jorendorff.

--- js/src/jsscriptinlines.h
+++ js/src/jsscriptinlines.h
@@ -41,7 +41,23 @@
 #ifndef jsscriptinlines_h___
 #define jsscriptinlines_h___
 
-inline JSObject *JSScript::getRegExp(size_t index) {
+#include "jsfun.h"
+#include "jsregexp.h"
+
+inline JSFunction *
+JSScript::getFunction(size_t index)
+{
+    JSObject *funobj = getObject(index);
+    JS_ASSERT(HAS_FUNCTION_CLASS(funobj));
+    JS_ASSERT(funobj == (JSObject *) funobj->getPrivate());
+    JSFunction *fun = (JSFunction *) funobj;
+    JS_ASSERT(FUN_INTERPRETED(fun));
+    return fun;
+}
+
+inline JSObject *
+JSScript::getRegExp(size_t index)
+{
     JSObjectArray *arr = regexps();
     JS_ASSERT((uint32) index < arr->length);
     JSObject *obj = arr->vector[index];
