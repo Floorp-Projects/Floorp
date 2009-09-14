8d3a1117d460cea9793dce4f4f977e26fbe2815c
Lukas Blakk <lsblakk@mozilla.com>
2009-09-14 22:37:11 +1200

    Bug 513969, Add output to symbolstore.py to decrease frequency of timeouts, r=ted.mielczarek

--- toolkit/crashreporter/tools/symbolstore.py
+++ toolkit/crashreporter/tools/symbolstore.py
@@ -485,6 +485,7 @@ class Dumper:
     def ProcessFile(self, file):
         """Dump symbols from this file into a symbol file, stored
         in the proper directory structure in  |symbol_path|."""
+        print >> sys.stderr, "Processing file: %s" % file
         result = False
         sourceFileStream = ''
         # tries to get the vcs root from the .mozconfig first - if it's not set
