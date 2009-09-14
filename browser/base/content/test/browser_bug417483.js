09024b918e08804127f01e7f0b6f0e51903350fc
Michael Schonfeld <dev@schonfeld.org>
2009-09-14 11:19:58 -0400

    Bug 417483 -  Double separator in context menu when selecting empty text & using iframes r=gavin

---
+++ browser/base/content/test/browser_bug417483.js
@@ -0,0 +1,26 @@
+function test() {
+  waitForExplicitFinish();
+  
+  var htmlContent = "data:text/html, <iframe src='data:text/html,text text'></iframe>";
+  gBrowser.addEventListener("pageshow", onPageShow, false);
+  gBrowser.loadURI(htmlContent);
+}
+
+function onPageShow() {
+    gBrowser.removeEventListener("pageshow", onPageShow, false);
+    var frame = gBrowser.contentWindow.frames[0];
+    var sel = frame.getSelection();
+    var range = frame.document.createRange();
+    var tn = frame.document.body.childNodes[0];
+    range.setStart(tn , 4);
+    range.setEnd(tn , 5);
+    sel.addRange(range);
+    frame.focus();
+    
+    document.popupNode = frame.document.body;
+    var contentAreaContextMenu = document.getElementById("contentAreaContextMenu");
+    var contextMenu = new nsContextMenu(contentAreaContextMenu, gBrowser);
+
+    ok(document.getElementById("frame-sep").hidden, "'frame-sep' should be hidden if the selection contains only spaces");
+    finish();
+}
