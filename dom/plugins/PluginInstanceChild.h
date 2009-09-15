b063012cae7796e8a3700096bbb2c363de288a2d
Ben Turner <bent.mozilla@gmail.com>
2009-09-14 19:26:17 -0700

    Further fixes for unions

--- dom/plugins/PluginInstanceChild.h
+++ dom/plugins/PluginInstanceChild.h
@@ -69,7 +69,7 @@ protected:
 
     // FIXME/bent: demo purposes only
     virtual nsresult
-    RecvTest(const Variant& v1, const Variant& v2)
+    AnswerTest(const Variant& v1, const Variant& v2, Variant* _retval)
     {
         printf("\n[PluginInstanceChild] v1: ");
 
@@ -116,6 +116,7 @@ protected:
         }
 
         puts("\n");
+        *_retval = v1;
         return NS_OK;
     }
 
