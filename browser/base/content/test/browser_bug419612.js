9fab32e992b1d0bb67d93878cbbc9290e0cd15d1
Dão Gottwald <dao@mozilla.com>
2009-09-16 18:30:20 +0200

    tests cleanup

--- browser/base/content/test/browser_bug419612.js
+++ browser/base/content/test/browser_bug419612.js
@@ -2,27 +2,28 @@ function test() {
   waitForExplicitFinish();
 
   let testPage = "http://example.org/browser/browser/base/content/test/dummy_page.html";
-  let prefService = Components.classes["@mozilla.org/preferences-service;1"]
-                              .getService(Components.interfaces.nsIPrefBranch);
   let tab1 = gBrowser.selectedTab = gBrowser.addTab();
   tab1.linkedBrowser.addEventListener("load", (function(event) {
     event.currentTarget.removeEventListener("load", arguments.callee, true);
+
     let tab2 = gBrowser.addTab();
     tab2.linkedBrowser.addEventListener("load", (function(event) {
       event.currentTarget.removeEventListener("load", arguments.callee, true);
-      let oldPref = prefService.getBoolPref("browser.zoom.updateBackgroundTabs");
+
       FullZoom.enlarge();
       let tab1Zoom = ZoomManager.getZoomForBrowser(tab1.linkedBrowser);
       gBrowser.selectedTab = tab2;
       let tab2Zoom = ZoomManager.getZoomForBrowser(tab2.linkedBrowser);
       is(tab2Zoom, tab1Zoom, "Zoom should affect background tabs");
-      prefService.setBoolPref("browser.zoom.updateBackgroundTabs", false);
+
+      gPrefService.setBoolPref("browser.zoom.updateBackgroundTabs", false);
       FullZoom.reset();
       gBrowser.selectedTab = tab1;
       tab1Zoom = ZoomManager.getZoomForBrowser(tab1.linkedBrowser);
       tab2Zoom = ZoomManager.getZoomForBrowser(tab2.linkedBrowser);
       isnot(tab1Zoom, tab2Zoom, "Zoom should not affect background tabs");
-      prefService.setBoolPref("browser.zoom.updateBackgroundTabs", oldPref);
+
+      gPrefService.clearUserPref("browser.zoom.updateBackgroundTabs");
       gBrowser.removeTab(tab1);
       gBrowser.removeTab(tab2);
       finish();
