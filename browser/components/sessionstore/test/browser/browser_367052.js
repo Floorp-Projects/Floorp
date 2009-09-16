df620e933e67ed2cf7bb82566712f68e5170dfcc
Dão Gottwald <dao@mozilla.com>
2009-09-16 12:21:19 +0200

    tests cleanup

--- browser/components/sessionstore/test/browser/browser_367052.js
+++ browser/components/sessionstore/test/browser/browser_367052.js
@@ -52,8 +52,7 @@ function test() {
   tab.linkedBrowser.addEventListener("load", function(aEvent) {
     this.removeEventListener("load", arguments.callee, true);
     
-    let browser = tabbrowser.getBrowserForTab(tab);
-    let history = browser.webNavigation.sessionHistory;
+    let history = tab.linkedBrowser.webNavigation.sessionHistory;
     ok(history.count >= 1, "the new tab does have at least one history entry");
     
     ss.setTabState(tab, "{ entries: [] }");
