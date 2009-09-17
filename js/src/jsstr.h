203c0fc4feac81999108f04fa2da212f0cd8f1b6
Luke Wagner <lw@mozilla.com>
2009-09-16 18:23:27 -0700

    Bug 511777 - avoid regexp engine for flat strings
    --HG--
    extra : rebase_source : 81afb9a486643f4d88dff9bba6ec2cccd07b07f0

--- js/src/jsstr.h
+++ js/src/jsstr.h
@@ -680,20 +680,18 @@ js_CompareStrings(JSString *str1, JSString *str2);
 
 /*
  * Boyer-Moore-Horspool superlinear search for pat:patlen in text:textlen.
- * The patlen argument must be positive and no greater than BMH_PATLEN_MAX.
- * The start argument tells where in text to begin the search.
+ * The patlen argument must be positive and no greater than sBMHPatLenMax.
  *
  * Return the index of pat in text, or -1 if not found.
  */
-#define BMH_CHARSET_SIZE 256    /* ISO-Latin-1 */
-#define BMH_PATLEN_MAX   255    /* skip table element is uint8 */
+static const jsuint sBMHCharSetSize = 256; /* ISO-Latin-1 */
+static const jsuint sBMHPatLenMax   = 255; /* skip table element is uint8 */
 
 #define BMH_BAD_PATTERN  (-2)   /* return value if pat is not ISO-Latin-1 */
 
 extern jsint
-js_BoyerMooreHorspool(const jschar *text, jsint textlen,
-                      const jschar *pat, jsint patlen,
-                      jsint start);
+js_BoyerMooreHorspool(const jschar *text, jsuint textlen,
+                      const jschar *pat, jsuint patlen);
 
 extern size_t
 js_strlen(const jschar *s);
