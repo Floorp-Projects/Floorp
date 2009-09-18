40d02465d7ce328ec7d0551a652953b8f9da09ce
David Bolter <dbolter@mozilla.com>
2009-09-18 12:20:45 -0400

    Bug 516047 - crash [@ nsXULTreeGridCellAccessible::GetAttributesInternal(nsIPersistentProperties*)]. r=MarcoZ

--- accessible/src/xul/nsXULTreeGridAccessible.cpp
+++ accessible/src/xul/nsXULTreeGridAccessible.cpp
@@ -1234,6 +1234,10 @@ nsXULTreeGridCellAccessible::GetAttributesInternal(nsIPersistentProperties *aAtt
   mParent->GetParent(getter_AddRefs(accessible));
   nsCOMPtr<nsIAccessibleTable> tableAccessible = do_QueryInterface(accessible);
 
+  // XXX - temp fix for crash bug 516047
+  if (!tableAccessible)
+    return NS_ERROR_FAILURE;
+    
   PRInt32 colIdx = GetColumnIndex();
 
   PRInt32 cellIdx = -1;
