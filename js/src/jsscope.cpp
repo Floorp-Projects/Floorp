be6095a2375019d6039734bb0894920b9dd69be1
Jeff Walden <jwalden@mit.edu>
2009-09-18 15:39:09 -0700

    Bug 517594 - Add assertions to verify that sprops being used don't have an id == JSVAL_NULL, to attempt to catch such bugs as early as possible.  r=jorendorff

--- js/src/jsscope.cpp
+++ js/src/jsscope.cpp
@@ -297,6 +297,7 @@ JSScope::searchTable(jsid id, bool adding)
     uint32 sizeMask;
 
     JS_ASSERT(table);
+    JS_ASSERT(!JSVAL_IS_NULL(id));
 
     /* Compute the primary hash address. */
     METER(hashes);
@@ -448,7 +449,8 @@ js_HashScopeProperty(JSDHashTable *table, const void *key)
 
 #define SPROP_MATCH_PARAMS(sprop, aid, agetter, asetter, aslot, aattrs,       \
                            aflags, ashortid)                                  \
-    ((sprop)->id == (aid) &&                                                  \
+    (JS_ASSERT(!JSVAL_IS_NULL((sprop)->id)), JS_ASSERT(!JSVAL_IS_NULL(aid)),  \
+     (sprop)->id == (aid) &&                                                  \
      SPROP_MATCH_PARAMS_AFTER_ID(sprop, agetter, asetter, aslot, aattrs,      \
                                  aflags, ashortid))
 
@@ -585,6 +587,7 @@ InsertPropertyTreeChild(JSRuntime *rt, JSScopeProperty *parent,
     uintN i;
 
     JS_ASSERT(!parent || child->parent != parent);
+    JS_ASSERT(!JSVAL_IS_NULL(child->id));
 
     if (!parent) {
         table = &rt->propertyTreeHash;
@@ -617,6 +620,7 @@ InsertPropertyTreeChild(JSRuntime *rt, JSScopeProperty *parent,
             JS_RUNTIME_METER(rt, duplicatePropTreeNodes);
         }
     } else {
+        JS_ASSERT(!JSVAL_IS_NULL(parent->id));
         childp = &parent->kids;
         kids = *childp;
         if (kids) {
@@ -729,6 +733,7 @@ RemovePropertyTreeChild(JSRuntime *rt, JSScopeProperty *child)
          */
         table = &rt->propertyTreeHash;
     } else {
+        JS_ASSERT(!JSVAL_IS_NULL(parent->id));
         kids = parent->kids;
         if (KIDS_IS_CHUNKY(kids)) {
             list = chunk = KIDS_TO_CHUNK(kids);
@@ -832,6 +837,8 @@ GetPropertyTreeChild(JSContext *cx, JSScopeProperty *parent,
     PropTreeKidsChunk *chunk;
     uintN i, n;
 
+    JS_ASSERT(!JSVAL_IS_NULL(child->id));
+
     rt = cx->runtime;
     if (!parent) {
         JS_LOCK_GC(rt);
@@ -846,6 +853,8 @@ GetPropertyTreeChild(JSContext *cx, JSScopeProperty *parent,
         if (sprop)
             goto out;
     } else {
+        JS_ASSERT(!JSVAL_IS_NULL(parent->id));
+
         /*
          * Because chunks are appended at the end and never deleted except by
          * the GC, we can search without taking the runtime's GC lock.  We may
@@ -1036,6 +1045,8 @@ JSScope::add(JSContext *cx, jsid id,
 
     JS_ASSERT_IF(attrs & JSPROP_GETTER, getter);
     JS_ASSERT_IF(attrs & JSPROP_SETTER, setter);
+
+    JS_ASSERT(!JSVAL_IS_NULL(id));
 
     /*
      * You can't add properties to a sealed scope.  But note well that you can
@@ -1391,6 +1402,8 @@ JSScope::change(JSContext *cx, JSScopeProperty *sprop,
 
     CHECK_ANCESTOR_LINE(this, true);
 
+    JS_ASSERT(!JSVAL_IS_NULL(sprop->id));
+
     /* Allow only shared (slot-less) => unshared (slot-full) transition. */
     attrs |= sprop->attrs & mask;
     JS_ASSERT(!((attrs ^ sprop->attrs) & JSPROP_SHARED) ||
@@ -1571,12 +1584,14 @@ JSScope::brandingShapeChange(JSContext *cx, uint32 slot, jsval v)
 void
 JSScope::deletingShapeChange(JSContext *cx, JSScopeProperty *sprop)
 {
+    JS_ASSERT(!JSVAL_IS_NULL(sprop->id));
     generateOwnShape(cx);
 }
 
 bool
 JSScope::methodShapeChange(JSContext *cx, JSScopeProperty *sprop, jsval toval)
 {
+    JS_ASSERT(!JSVAL_IS_NULL(sprop->id));
     if (sprop->isMethod()) {
 #ifdef DEBUG
         jsval prev = LOCKED_OBJ_GET_SLOT(object, sprop->slot);
@@ -1610,6 +1625,7 @@ JSScope::methodShapeChange(JSContext *cx, uint32 slot, jsval toval)
         generateOwnShape(cx);
     } else {
         for (JSScopeProperty *sprop = lastProp; sprop; sprop = sprop->parent) {
+            JS_ASSERT(!JSVAL_IS_NULL(sprop->id));
             if (sprop->slot == slot && (!hadMiddleDelete() || has(sprop)))
                 return methodShapeChange(cx, sprop, toval);
         }
@@ -1626,6 +1642,7 @@ JSScope::protoShapeChange(JSContext *cx)
 void
 JSScope::replacingShapeChange(JSContext *cx, JSScopeProperty *sprop, JSScopeProperty *newsprop)
 {
+    JS_ASSERT(!JSVAL_IS_NULL(sprop->id));
     if (shape == sprop->shape)
         shape = newsprop->shape;
     else
@@ -1641,6 +1658,7 @@ JSScope::sealingShapeChange(JSContext *cx)
 void
 JSScope::shadowingShapeChange(JSContext *cx, JSScopeProperty *sprop)
 {
+    JS_ASSERT(!JSVAL_IS_NULL(sprop->id));
     generateOwnShape(cx);
 }
 
@@ -1665,6 +1683,7 @@ PrintPropertyGetterOrSetter(JSTracer *trc, char *buf, size_t bufsize)
     JS_ASSERT(trc->debugPrinter == PrintPropertyGetterOrSetter);
     sprop = (JSScopeProperty *)trc->debugPrintArg;
     id = sprop->id;
+    JS_ASSERT(!JSVAL_IS_NULL(id));
     name = trc->debugPrintIndex ? js_setter_str : js_getter_str;
 
     if (JSID_IS_ATOM(id)) {
@@ -1689,6 +1708,7 @@ PrintPropertyMethod(JSTracer *trc, char *buf, size_t bufsize)
     JS_ASSERT(trc->debugPrinter == PrintPropertyMethod);
     sprop = (JSScopeProperty *)trc->debugPrintArg;
     id = sprop->id;
+    JS_ASSERT(!JSVAL_IS_NULL(id));
 
     JS_ASSERT(JSID_IS_ATOM(id));
     n = js_PutEscapedString(buf, bufsize - 1, ATOM_TO_STRING(JSID_TO_ATOM(id)), 0);
@@ -1785,6 +1805,7 @@ DumpSubtree(JSContext *cx, JSScopeProperty *sprop, int level, FILE *fp)
 
     fprintf(fp, "%*sid ", level, "");
     v = ID_TO_VALUE(sprop->id);
+    JS_ASSERT(!JSVAL_IS_NULL(v));
     if (JSID_IS_INT(sprop->id)) {
         fprintf(fp, "%d", JSVAL_TO_INT(v));
     } else {
