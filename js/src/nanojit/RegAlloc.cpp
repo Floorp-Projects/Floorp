f7166fde25c49e02b459b65197e8d5b79ca2197a
Nicholas Nethercote <nnethercote@mozilla.com>
2009-09-22 17:21:59 +1000

    Bug 518061 - NJ merge: add ExprFilter::insLoad().  r=graydon.

--- js/src/nanojit/RegAlloc.cpp
+++ js/src/nanojit/RegAlloc.cpp
@@ -56,6 +56,11 @@ namespace nanojit
                 continue;
             NanoAssertMsg(!isFree(r), "Coding error; register is both free and active! " );
 
+            if (ins->isop(LIR_param) && ins->paramKind()==1 && r == Assembler::savedRegs[ins->paramArg()]) {
+                // dont print callee-saved regs that arent used
+                continue;
+            }
+
             s += VMPI_strlen(s);
             const char* rname = ins->isQuad() ? fpn(r) : gpn(r);
             VMPI_sprintf(s, " %s(%s)", rname, names->formatRef(ins));
