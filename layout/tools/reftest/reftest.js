9f8d93426d28f74cbda8a69d2ec09e8cf1761798
Vladimir Vukicevic <vladimir@pobox.com>
2009-09-22 13:01:11 -0700

    b=514334; add option to disable canvas cache in reftests (intermittent crash when running reftests on low memory devices); r=roc

--- layout/tools/reftest/reftest.js
+++ layout/tools/reftest/reftest.js
@@ -121,6 +121,9 @@ const EXPECTED_DEATH = 3;  // test must be skipped to avoid e.g. crash/hang
 var HTTP_SERVER_PORT = 4444;
 const HTTP_SERVER_PORTS_TO_TRY = 50;
 
+// whether we should skip caching canvases
+var gNoCanvasCache = false;
+
 var gRecycledCanvases = new Array();
 
 function AllocateCanvas()
@@ -133,12 +136,15 @@ function AllocateCanvas()
     var canvas = document.createElementNS(XHTML_NS, "canvas");
     canvas.setAttribute("width", windowElem.getAttribute("width"));
     canvas.setAttribute("height", windowElem.getAttribute("height"));
+
     return canvas;
 }
 
 function ReleaseCanvas(canvas)
 {
-    gRecycledCanvases.push(canvas);
+    // store a maximum of 2 canvases, if we're not caching
+    if (!gNoCanvasCache || gRecycledCanvases.length < 2)
+        gRecycledCanvases.push(canvas);
 }
 
 function OnRefTestLoad()
@@ -207,7 +213,12 @@ function StartTests()
 {
     try {
         // Need to read the manifest once we have the final HTTP_SERVER_PORT.
-        ReadTopManifest(window.arguments[0]);
+        args = window.arguments[0].wrappedJSObject;
+
+        if ("nocache" in args && args["nocache"])
+            gNoCanvasCache = true;
+
+        ReadTopManifest(args.uri);
         BuildUseCounts();
         gTotalTests = gURLs.length;
 
@@ -790,7 +801,8 @@ function UpdateCanvasCache(url, canvas)
     var spec = url.spec;
 
     --gURIUseCounts[spec];
-    if (gURIUseCounts[spec] == 0) {
+
+    if (gNoCanvasCache || gURIUseCounts[spec] == 0) {
         ReleaseCanvas(canvas);
         delete gURICanvases[spec];
     } else if (gURIUseCounts[spec] > 0) {
