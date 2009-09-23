4479b6c7bfb82de2bb875fc0f35cab70ec1e5225
Luke Wagner <lw@mozilla.com>
2009-09-23 08:54:50 -0700

    Bug 511777 - avoid regexp engine for flat strings (r=waldo)

--- js/src/jsobj.h
+++ js/src/jsobj.h
@@ -257,8 +257,9 @@ struct JSObject {
     }
 
     JSBool defineProperty(JSContext *cx, jsid id, jsval value,
-                          JSPropertyOp getter, JSPropertyOp setter,
-                          uintN attrs) {
+                          JSPropertyOp getter = JS_PropertyStub,
+                          JSPropertyOp setter = JS_PropertyStub,
+                          uintN attrs = JSPROP_ENUMERATE) {
         return map->ops->defineProperty(cx, this, id, value, getter, setter, attrs);
     }
 
