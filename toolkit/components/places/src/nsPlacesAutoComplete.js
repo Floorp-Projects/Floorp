55b92d4de23db4520b0818b9fc3649b64795f678
Shawn Wilsher <sdwilsh@shawnwilsher.com>
2009-09-23 10:19:27 -0700

    Bug 516465 - Adaptive results aren't filtered. r=dietrich Run adaptive search results through AUTOCOMPLETE_MATCH so they properly obey the preferences of the user.  Also runs through adaptive results again if we do not have enough matches and the match behavior is MATCH_BOUNDARY_ANYWHERE.

--- toolkit/components/places/src/nsPlacesAutoComplete.js
+++ toolkit/components/places/src/nsPlacesAutoComplete.js
@@ -322,6 +322,10 @@ function nsPlacesAutoComplete()
       "LEFT JOIN moz_places_temp h_t ON h_t.id = i.place_id " +
       "LEFT JOIN moz_favicons f ON f.id = IFNULL(h_t.favicon_id, h.favicon_id) "+
       "WHERE IFNULL(h_t.url, h.url) NOTNULL " +
+      "AND AUTOCOMPLETE_MATCH(:searchString, 0 /* url */, " +
+                             "IFNULL(bookmark, 1 /* title */), tags, " +
+                             "6 /* visit_count */, 7 /* typed */, parent, " +
+                             ":matchBehavior, :searchBehavior) " +
       "ORDER BY rank DESC, IFNULL(h_t.frecency, h.frecency) DESC"
     );
   });
@@ -484,8 +488,11 @@ nsPlacesAutoComplete.prototype = {
     if (this._matchBehavior == MATCH_BOUNDARY_ANYWHERE &&
         this._result.matchCount < this._maxRichResults && !this._secondPass) {
       this._secondPass = true;
-      let query = this._getBoundSearchQuery(MATCH_ANYWHERE, this._searchTokens);
-      this._executeQueries([query]);
+      let queries = [
+        this._getBoundAdaptiveQuery(MATCH_ANYWHERE),
+        this._getBoundSearchQuery(MATCH_ANYWHERE, this._searchTokens),
+      ];
+      this._executeQueries(queries);
       return;
     }
 
@@ -827,13 +834,19 @@ nsPlacesAutoComplete.prototype = {
    *
    * @return the bound adaptive query.
    */
-  _getBoundAdaptiveQuery: function PAC_getBoundAdaptiveQuery()
+  _getBoundAdaptiveQuery: function PAC_getBoundAdaptiveQuery(aMatchBehavior)
   {
+    // If we were not given a match behavior, use the stored match behavior.
+    if (arguments.length == 0)
+      aMatchBehavior = this._matchBehavior;
+
     let query = this._adaptiveQuery;
     let (params = query.params) {
       params.parent = this._bs.tagsFolder;
       params.search_string = this._currentSearchString;
       params.query_type = kQueryTypeFiltered;
+      params.matchBehavior = aMatchBehavior;
+      params.searchBehavior = this._behavior;
     }
 
     return query;
@@ -898,7 +911,8 @@ nsPlacesAutoComplete.prototype = {
     // the result does not fall into any of those, it just gets the favicon.
     if (!style) {
       // It is possible that we already have a style set (from a keyword
-      // search), so only set it if we haven't already done so.
+      // search or because of the user's preferences), so only set it if we
+      // haven't already done so.
       if (showTags)
         style = "tag";
       else if (entryParentId)
