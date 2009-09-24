65b01f7a1976fa73a048190c10a451aa8f22f6df
Markus Stange <mstange@themasta.com>
2009-09-24 18:19:40 +1200

    Bug 518395 - White text on drop down buttons. r=josh, r=roc

--- widget/src/cocoa/nsLookAndFeel.mm
+++ widget/src/cocoa/nsLookAndFeel.mm
@@ -158,10 +158,8 @@ nsresult nsLookAndFeel::NativeGetColor(const nsColorID aID, nscolor &aColor)
     case eColor_captiontext:
     case eColor_menutext:
     case eColor_infotext:
-      aColor = GetColorFromNSColor([NSColor textColor]);
-      break;
     case eColor__moz_menubartext:
-      aColor = NS_RGB(0xFF,0xFF,0xFF);
+      aColor = GetColorFromNSColor([NSColor textColor]);
       break;
     case eColor_windowtext:
       aColor = GetColorFromNSColor([NSColor windowFrameTextColor]);
@@ -230,10 +228,10 @@ nsresult nsLookAndFeel::NativeGetColor(const nsColorID aID, nscolor &aColor)
     case eColor_window:
     case eColor__moz_field:
     case eColor__moz_combobox:
-    case eColor__moz_comboboxtext:
       aColor = NS_RGB(0xff,0xff,0xff);
       break;
     case eColor__moz_fieldtext:
+    case eColor__moz_comboboxtext:
       aColor = GetColorFromNSColor([NSColor controlTextColor]);
       break;
     case eColor__moz_dialog:
