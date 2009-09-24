0e09ab162dbb51e0b2f689ff2507f6d037d79bac
Chris Pearce <chris@pearce.org.nz>
2009-09-24 16:22:23 +1200

    Bug 512328 - Update liboggplay to rev 404316e595. r=doublec

--- media/liboggplay/src/liboggplay/oggplay_data.h
+++ media/liboggplay/src/liboggplay/oggplay_data.h
@@ -41,26 +41,26 @@
 void
 oggplay_data_initialise_list (OggPlayDecode *decode);
 
-void
+OggPlayErrorCode
 oggplay_data_handle_theora_frame (OggPlayTheoraDecode *decode, 
-                                  yuv_buffer *buffer);
+                                  const yuv_buffer *buffer);
 
-void
-oggplay_data_handle_audio_data (OggPlayDecode *decode, void *data, 
-                                int samples, int samplesize);
+OggPlayErrorCode
+oggplay_data_handle_audio_data (OggPlayDecode *decode, 
+                                void *data, long samples, size_t samplesize);
 
-void
-oggplay_data_handle_cmml_data(OggPlayDecode *decode, unsigned char *data,
-                                int size);
+OggPlayErrorCode
+oggplay_data_handle_cmml_data(OggPlayDecode *decode, 
+                              unsigned char *data, long size);
 
 #ifdef HAVE_KATE
-void
+OggPlayErrorCode
 oggplay_data_handle_kate_data(OggPlayKateDecode *decode,
                               const kate_event *ev);
 #endif
 
 #ifdef HAVE_TIGER
-void
+OggPlayErrorCode
 oggplay_data_update_tiger(OggPlayKateDecode *decode,
                           int active, ogg_int64_t presentation_time,
                           OggPlayCallbackInfo *info);
