58d78642be2592b1637977242eb571581e81d4a1
Dan Witte <dwitte@mozilla.com>
2009-09-25 09:44:00 -0700

    Add int8/int64 tests and simplify error propagation. b=513783, r=jorendorff

--- js/ctypes/tests/jsctypes-test.h
+++ js/ctypes/tests/jsctypes-test.h
@@ -39,62 +39,42 @@
  * ***** END LICENSE BLOCK ***** */
 
 #include "nscore.h"
+#include "prtypes.h"
 
 NS_EXTERN_C
 {
   NS_EXPORT void test_v();
 
-  NS_EXPORT short test_s();
-  NS_EXPORT short test_s_s(short);
-  NS_EXPORT short test_s_ss(short, short);
+  NS_EXPORT PRInt8 test_i8();
+  NS_EXPORT PRInt8 test_i8_i8(PRInt8);
+  NS_EXPORT PRInt8 test_i8_i8_sum(PRInt8, PRInt8);
 
-  NS_EXPORT int test_i();
-  NS_EXPORT int test_i_i(int);
-  NS_EXPORT int test_i_ii(int, int);
+  NS_EXPORT PRInt16 test_i16();
+  NS_EXPORT PRInt16 test_i16_i16(PRInt16);
+  NS_EXPORT PRInt16 test_i16_i16_sum(PRInt16, PRInt16);
+
+  NS_EXPORT PRInt32 test_i32();
+  NS_EXPORT PRInt32 test_i32_i32(PRInt32);
+  NS_EXPORT PRInt32 test_i32_i32_sum(PRInt32, PRInt32);
+
+  NS_EXPORT PRInt64 test_i64();
+  NS_EXPORT PRInt64 test_i64_i64(PRInt64);
+  NS_EXPORT PRInt64 test_i64_i64_sum(PRInt64, PRInt64);
 
   NS_EXPORT float test_f();
   NS_EXPORT float test_f_f(float);
-  NS_EXPORT float test_f_ff(float, float);
+  NS_EXPORT float test_f_f_sum(float, float);
 
   NS_EXPORT double test_d();
   NS_EXPORT double test_d_d(double);
-  NS_EXPORT double test_d_dd(double, double);
+  NS_EXPORT double test_d_d_sum(double, double);
 
-  NS_EXPORT int test_ansi_len(const char*);
-  NS_EXPORT int test_wide_len(const PRUnichar*);
+  NS_EXPORT PRInt32 test_ansi_len(const char*);
+  NS_EXPORT PRInt32 test_wide_len(const PRUnichar*);
   NS_EXPORT const char* test_ansi_ret();
   NS_EXPORT const PRUnichar* test_wide_ret();
   NS_EXPORT char* test_ansi_echo(const char*);
 
-  NS_EXPORT int test_i_if_floor(int, float);
-
-  struct POINT {
-    int x;
-    int y;
-  };
-
-  struct RECT {
-    int top;
-    int left;
-    int bottom;
-    int right;
-  };
-
-  struct INNER {
-    unsigned char i1;
-    long long int i2;
-    char i3;
-  };
-
-  struct NESTED {
-    int n1;
-    short n2;
-    INNER inner;
-    long long int n3;
-    int n4;
-  };
-
-  NS_EXPORT int test_pt_in_rect(RECT, POINT);
-  NS_EXPORT int test_nested_struct(NESTED);
-  NS_EXPORT POINT test_struct_return(RECT);
+  NS_EXPORT PRInt32 test_floor(PRInt32, float);
 }
+
