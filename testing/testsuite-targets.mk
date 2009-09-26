100f13d6b052e8d1db7402fa000fe0a67b541ac7
Bob Clary <bclary@bclary.com>
2009-09-26 15:54:12 -0700

    Bug 469718 - browser javascript reftests, r=ted.mielczarek, r=sayrer, r=dbaron.

--- testing/testsuite-targets.mk
+++ testing/testsuite-targets.mk
@@ -92,6 +92,10 @@ crashtest:
 	$(call RUN_REFTEST,$(topsrcdir)/testing/crashtest/crashtests.list)
 	$(CHECK_TEST_ERROR)
 
+jstestbrowser: EXTRA_TEST_ARGS += --extra-profile-file=$(topsrcdir)/js/src/tests/user.js
+jstestbrowser:
+	$(call RUN_REFTEST,$(topsrcdir)/js/src/tests/jstests.list)
+	$(CHECK_TEST_ERROR)
 
 # Execute all xpcshell tests in the directories listed in the manifest.
 # See also config/rules.mk 'xpcshell-tests' target for local execution.
@@ -112,7 +116,7 @@ include $(topsrcdir)/toolkit/mozapps/installer/package-name.mk
 
 PKG_STAGE = $(DIST)/test-package-stage
 
-package-tests: stage-mochitest stage-reftest stage-xpcshell
+package-tests: stage-mochitest stage-reftest stage-xpcshell stage-jstests
 	@(cd $(PKG_STAGE) && tar $(TAR_CREATE_FLAGS) - *) | bzip2 -f > $(DIST)/$(PKG_PATH)$(TEST_PACKAGE)
 
 make-stage-dir:
@@ -127,9 +131,12 @@ stage-reftest: make-stage-dir
 stage-xpcshell: make-stage-dir
 	$(MAKE) -C $(DEPTH)/testing/xpcshell stage-package
 
+stage-jstests: make-stage-dir
+	$(MAKE) -C $(DEPTH)/js/src/tests stage-package
 
 .PHONY: \
   mochitest mochitest-plain mochitest-chrome mochitest-a11y \
   reftest crashtest \
   xpcshell-tests \
-  package-tests make-stage-dir stage-mochitest stage-reftest stage-xpcshell
+  jstestbrowser \
+  package-tests make-stage-dir stage-mochitest stage-reftest stage-xpcshell stage-jstests
