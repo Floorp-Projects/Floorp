739fbf69a4c2daa770728e8056206193a183efda
Hanspeter Niederstrasser <niederstrasser@gmail.com>
2009-10-01 18:07:00 -0400

    Bug 520098 - Darwin build fails on new quota check from bug 72892 (nsLocalFileUnix.cpp). r=bsmedberg

--- xpcom/io/nsLocalFileUnix.cpp
+++ xpcom/io/nsLocalFileUnix.cpp
@@ -68,7 +68,9 @@
 #endif
 
 #if defined(HAVE_SYS_QUOTA_H)
+#if defined(HAVE_SYS_SYSMACROS_H)
 #include <sys/sysmacros.h>
+#endif
 #include <sys/quota.h>
 #endif
 
@@ -1231,7 +1233,7 @@ nsLocalFile::GetDiskSpaceAvailable(PRInt64 *aDiskSpaceAvailable)
      */
     *aDiskSpaceAvailable = (PRInt64)fs_buf.f_bsize * (fs_buf.f_bavail - 1);
 
-#if defined(HAVE_SYS_STAT_H) || defined(HAVE_SYS_QUOTA_H)
+#if defined(HAVE_SYS_STAT_H) || defined(HAVE_SYS_SYSMACROS_H)
 
     if(!FillStatCache()) {
         // Return available size from statfs
