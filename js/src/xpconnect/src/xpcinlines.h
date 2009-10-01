8168b8f791dfac988e9f8eee87fe8d82e3857f51
Blake Kaplan <mrbkap@gmail.com>
2009-09-30 19:38:57 -0700

    Bug 518991 - Implement access controls in COWs. For now, this is opt-in default unsafe. r=jst

--- js/src/xpconnect/src/xpcinlines.h
+++ js/src/xpconnect/src/xpcinlines.h
@@ -757,11 +757,17 @@ xpc_SameScope(XPCWrappedNativeScope *objectscope, XPCWrappedNativeScope *xpcscop
     return JS_FALSE;
 }
 
+inline jsid
+GetRTIdByIndex(JSContext *cx, uintN index)
+{
+  XPCJSRuntime *rt = nsXPConnect::GetRuntimeInstance();
+  return rt->GetStringID(index);
+}
+
 inline jsval
 GetRTStringByIndex(JSContext *cx, uintN index)
 {
-  XPCJSRuntime *rt = nsXPConnect::GetRuntimeInstance();
-  return ID_TO_VALUE(rt->GetStringID(index));
+  return ID_TO_VALUE(GetRTIdByIndex(cx, index));
 }
 
 inline
