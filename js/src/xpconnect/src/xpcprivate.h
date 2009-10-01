1f391f0b3ea20283190d9e956a43009d2b161e89
Daniel Holbert <dholbert@cs.stanford.edu>
2009-09-30 23:42:27 -0700

    Backed out changeset 58c5864cb9c6 (Bug 518991) due to linux orange (test failure in test_bug408328.html & 9240-byte leak)

--- js/src/xpconnect/src/xpcprivate.h
+++ js/src/xpconnect/src/xpcprivate.h
@@ -704,7 +704,6 @@ public:
         IDX_PROTO                   ,
         IDX_ITERATOR                ,
         IDX_PARENT                  ,
-        IDX_EXPOSEDPROPS            ,
         IDX_TOTAL_COUNT // just a count of the above
     };
 
@@ -4298,9 +4297,6 @@ xpc_EvalInSandbox(JSContext *cx, JSObject *sandbox, const nsAString& source,
 
 inline JSBool
 xpc_ForcePropertyResolve(JSContext* cx, JSObject* obj, jsval idval);
-
-inline jsid
-GetRTIdByIndex(JSContext *cx, uintN index);
 
 inline jsval
 GetRTStringByIndex(JSContext *cx, uintN index);
