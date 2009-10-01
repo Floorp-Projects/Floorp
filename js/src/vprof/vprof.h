e3eca80bebb1ca5ebc959e0440c8defda4b68d94
Graydon Hoare <graydon@mozilla.com>
2009-09-30 17:45:17 -0700

    Bug 519856 - NJ merge: move VMPI stuff from avmplus, r=jimb.

--- js/src/vprof/vprof.h
+++ js/src/vprof/vprof.h
@@ -85,18 +85,7 @@
 #define THREADED 0
 #define THREAD_SAFE 0
 
-#ifdef _MSC_VER
-typedef __int8             int8_t;
-typedef __int16            int16_t;
-typedef __int32            int32_t;
-typedef __int64            int64_t;
-typedef unsigned __int8    uint8_t;
-typedef unsigned __int16   uint16_t;
-typedef unsigned __int32   uint32_t;
-typedef unsigned __int64   uint64_t;
-#else
-#include <inttypes.h>
-#endif
+#include "VMPI.h"
 
 // portable align macro
 #if defined(_MSC_VER)
