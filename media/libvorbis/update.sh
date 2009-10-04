5ce0652f0d8b96052a90bf6fd171eacaebc1d651
Matthew Gregan <kinetik@flim.org>
2009-10-05 11:56:08 +1300

    Bug 518952 - Remove unnecessary local patch against libvorbis.  r=chris.double

--- media/libvorbis/update.sh
+++ media/libvorbis/update.sh
@@ -47,6 +47,5 @@ cp $1/COPYING ./COPYING
 cp $1/README ./README
 cp $1/AUTHORS ./AUTHORS
 patch -p3 < ./alloca.diff
-patch -p3 <./bug481601.patch
 patch -p3 <bug487519.patch
 patch -p3 <bug498855.patch
