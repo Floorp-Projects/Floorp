ea139765e717083e001d65eebaacadafc0f16311
Robert O'Callahan <robert@ocallahan.org>
2009-10-07 17:19:59 +1300

    Bug 520462. Let NS_PLUGIN_ACTIVATE events look up the view hierarchy to find a frame. r=karlt

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -6113,7 +6113,7 @@ PresShell::HandleEvent(nsIView         *aView,
   // view that has a frame.
   if (!frame &&
       (dispatchUsingCoordinates || NS_IS_KEY_EVENT(aEvent) ||
-       NS_IS_IME_EVENT(aEvent))) {
+       NS_IS_IME_EVENT(aEvent) || aEvent->message == NS_PLUGIN_ACTIVATE)) {
     nsIView* targetView = aView;
     while (targetView && !targetView->GetClientData()) {
       targetView = targetView->GetParent();
