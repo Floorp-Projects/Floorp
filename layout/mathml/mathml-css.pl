3200e50b7327c1a9f33881cd230101a6fdd0bd94
Taras Glek <tglek@mozilla.com>
2009-10-08 16:43:55 -0700

    Backed out changeset e22bef491d84

--- layout/mathml/mathml-css.pl
+++ layout/mathml/mathml-css.pl
@@ -12,7 +12,7 @@ if (!($css =~ m|\@import.*mathml\.css|))
 {
   # since bad end of lines cause troubles on some platforms
   # do a little perl magic for dos2{unix or mac} here
-  $css =~ s#(\@import[^\@]+\;)(\cM?)(\n\cM?\n)#$1$2\n\@import url\(resource://gre-resources/mathml\.css\);$2$3#;
+  $css =~ s#(\@import[^\@]+\;)(\cM?)(\n\cM?\n)#$1$2\n\@import url\(resource://gre/res/mathml\.css\);$2$3#;
   open(UA, ">$ua");
   print UA $css;
   close(UA);
