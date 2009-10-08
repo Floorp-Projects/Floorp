0a9aeebf93b983a33a53bab537a57a6c8b41b428
Simon Montagu <smontagu@smontagu.org>
2009-10-07 21:53:50 -0700

    Ensure that continuation frames after the end of a directional run are non-fluid. Bug 490559, r+sr=roc

--- layout/base/nsBidiPresUtils.cpp
+++ layout/base/nsBidiPresUtils.cpp
@@ -514,6 +514,16 @@ nsBidiPresUtils::Resolve(nsBlockFrame*   aBlockFrame,
             do {
             } while (mLogicalFrames[++newIndex]->GetType() == nsGkAtoms::directionalFrame);
             RemoveBidiContinuation(frame, frameIndex, newIndex, lineOffset);
+          } else if (runLength == fragmentLength) {
+            /*
+             * The directional run ends at the end of the frame. Make sure that
+             * the next frame is a non-fluid continuation
+             */
+            nsIFrame* next = frame->GetNextInFlow();
+            if (next) {
+              frame->SetNextContinuation(next);
+              next->SetPrevContinuation(frame);
+            }
           }
           frame->AdjustOffsetsForBidi(contentOffset, contentOffset + fragmentLength);
           if (lineNeedsUpdate) {
