06ca5b9190481b1facf61e11c46cdd2621f4856d
Karl Tomlinson <karlt+@karlt.net>
2009-10-14 15:40:44 +1300

    b=518915 remove incorrect comment about clipRect "Used by MAC only" r=josh

--- modules/plugin/base/public/npapi.h
+++ modules/plugin/base/public/npapi.h
@@ -413,7 +413,6 @@ typedef struct _NPWindow
   uint32_t width;  /* Maximum window size */
   uint32_t height;
   NPRect   clipRect; /* Clipping rectangle in port coordinates */
-                     /* Used by MAC only. */
 #if defined(XP_UNIX) && !defined(XP_MACOSX)
   void * ws_info; /* Platform-dependent additonal data */
 #endif /* XP_UNIX */
