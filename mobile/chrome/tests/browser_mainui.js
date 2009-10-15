ee526cd913350fdc36278f771d25af41e70960b1
Benjamin Stover <webapps@stechz.com>
2009-10-15 15:00:39 -0400

    Bug 522529: Diagonal panning is jerky (and a few other issues) [r=mark.finkle]

--- mobile/chrome/tests/browser_mainui.js
+++ mobile/chrome/tests/browser_mainui.js
@@ -6,12 +6,31 @@ const Ci = Components.interfaces;
 //------------------------------------------------------------------------------
 // Entry point (must be named "test")
 function test() {
-   is(window.location.href, "chrome://browser/content/browser.xul", "Main window should be browser.xul");
+ is(window.location.href, "chrome://browser/content/browser.xul", "Main window should be browser.xul");
 
-   window.focus();
+ window.focus();
 
-   let browser = Browser.selectedBrowser;
-   isnot(browser, null, "Should have a browser");
+ let browser = Browser.selectedBrowser;
+ isnot(browser, null, "Should have a browser");
    
-   is(browser.currentURI.spec, Browser.selectedTab.browser.currentURI.spec, "selectedBrowser == selectedTab.browser");
+ is(browser.currentURI.spec, Browser.selectedTab.browser.currentURI.spec, "selectedBrowser == selectedTab.browser");
+
+ testContentContainerSize();
+}
+
+function testContentContainerSize() {
+  let tiles = document.getElementById("tile-container");
+  let oldtilesstyle = tiles.getAttribute("style");
+
+  try {
+    tiles.style.width = (window.innerWidth + 100) + "px";
+    tiles.style.height = (window.innerHeight + 100) + "px";
+    let container = document.getElementById("tile-container-container");
+    let rect = container.getBoundingClientRect();
+    ok(rect.width == window.innerWidth, "Content container is same width as window");
+    ok(rect.height == window.innerHeight, "Content container is same height as window");
+  }
+  finally {
+    tiles.setAttribute("style", oldtilesstyle);
+  }
 }
