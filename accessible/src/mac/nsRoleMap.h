ffa66d32b35de0fe17eb3b684de551658bf39b64
Alexander Surkov <surkov.alexander@gmail.com>
2009-10-15 11:53:08 +0800

    Bug 485270 - embed and object HTML tags should be given an accessible role of embedded object, r=marcoz, davidb

--- accessible/src/mac/nsRoleMap.h
+++ accessible/src/mac/nsRoleMap.h
@@ -164,5 +164,6 @@ static const NSString* AXRoles [] = {
   NSAccessibilityListRole,                      // ROLE_LISTBOX
   NSAccessibilityUnknownRole,                   // ROLE_FLAT_EQUATION
   NSAccessibilityGroupRole,                     // ROLE_GRID_CELL
+  NSAccessibilityGroupRole                      // ROLE_EMBEDDED_OBJECT
   @"ROLE_LAST_ENTRY"                            // ROLE_LAST_ENTRY. bogus role that will never be shown (just marks the end of this array)!
 };
