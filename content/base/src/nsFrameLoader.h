acca60ec88dcf7b1aadabfb6595ddbf8d6a11424
Benjamin Smedberg <benjamin@smedbergs.us>
2009-10-16 15:42:27 -0400

    Bug 522351 - check for remote="true" on the element before trying to remote it, r=bz

--- content/base/src/nsFrameLoader.h
+++ content/base/src/nsFrameLoader.h
@@ -73,8 +73,8 @@ protected:
     mNeedsAsyncDestroy(PR_FALSE),
     mInSwap(PR_FALSE)
 #ifdef MOZ_IPC
+    , mRemoteFrame(false)
     , mChildProcess(nsnull)
-    , mTriedNewProcess(PR_FALSE)
 #endif
   {}
 
@@ -102,8 +102,16 @@ public:
                                nsRefPtr<nsFrameLoader>& aSecondToSwap);
 private:
 
-  NS_HIDDEN_(nsresult) EnsureDocShell();
-  NS_HIDDEN_(void) GetURL(nsString& aURL);
+#ifdef MOZ_IPC
+  bool ShouldUseRemoteProcess();
+#endif
+
+  /**
+   * If we are an IPC frame, set mRemoteFrame. Otherwise, create and
+   * initialize mDocShell.
+   */
+  nsresult MaybeCreateDocShell();
+  void GetURL(nsString& aURL);
 
   // Properly retrieves documentSize of any subdocument type.
   NS_HIDDEN_(nsIntSize) GetSubDocumentSize(const nsIFrame *aIFrame);
@@ -128,9 +136,9 @@ private:
   PRPackedBool mInSwap : 1;
 
 #ifdef MOZ_IPC
+  bool mRemoteFrame;
   // XXX leaking
   mozilla::dom::TabParent* mChildProcess;
-  PRBool mTriedNewProcess;
 #endif
 };
 
