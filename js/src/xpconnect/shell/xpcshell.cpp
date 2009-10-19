fbde8e9887a4d0f1950a4733fb123ba861fe82d2
Daniel Holbert <dholbert@cs.stanford.edu>
2009-10-19 08:03:00 -0700

    Bug 521677: Use fputs instead of printf/fprintf in a few cases, to fix "format not a string literal and no format arguments" build warnings. r=dbaron

--- js/src/xpconnect/shell/xpcshell.cpp
+++ js/src/xpconnect/shell/xpcshell.cpp
@@ -267,7 +267,7 @@ GetLine(JSContext *cx, char *bufp, FILE *file, const char *prompt) {
 #endif
     {
         char line[256];
-        fprintf(gOutFile, prompt);
+        fputs(prompt, gOutFile);
         fflush(gOutFile);
         if (!fgets(line, sizeof line, file))
             return JS_FALSE;
