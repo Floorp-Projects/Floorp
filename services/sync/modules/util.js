57cd173e1de4579bebac4fcf5a10f0570dea59d0
Edward Lee <edilee@mozilla.com>
2009-10-19 16:37:25 -0700

    Remove unused change password/passphrase/generic dialog.

--- services/sync/modules/util.js
+++ services/sync/modules/util.js
@@ -719,11 +719,6 @@ let Utils = {
       options || "centerscreen,chrome,dialog,modal,resizable=no", args);
   },
 
-  openGenericDialog: function Utils_openGenericDialog(type) {
-    this._genericDialogType = type;
-    this.openDialog("ChangeSomething", "generic-change.xul");
-  },
-
   openLog: function Utils_openLog() {
     Utils._openChromeWindow("Log", "log.xul");
   },
