11f01af3414e6e009329754527ea134a64bd9e1c
Edward Lee <edilee@mozilla.com>
2009-10-22 13:51:50 -0700

    Bug 523384 - Update Fennec UI for 0.8 release Provide an optionsURL to show settings on Fennec and update the options on various events like sync, login, options loaded.

--- services/sync/modules/util.js
+++ services/sync/modules/util.js
@@ -792,5 +792,5 @@ Svc.Prefs = new Preferences(PREFS_BRANCH);
 ].forEach(function(lazy) Utils.lazySvc(Svc, lazy[0], lazy[1], Ci[lazy[2]]));
 
 let Str = {};
-["about", "errors"]
+["about", "errors", "sync"]
   .forEach(function(lazy) Utils.lazy2(Str, lazy, Utils.lazyStrings(lazy)));
