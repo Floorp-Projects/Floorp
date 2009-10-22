58921427b12a6684d5aceb5d9864024bb4824221
Graydon Hoare <graydon@mozilla.com>
2009-10-21 19:58:21 -0700

    Bug 523505 - Followup build fixes, r=gal on IRC.

--- js/src/nanojit/NativeX64.h
+++ js/src/nanojit/NativeX64.h
@@ -384,7 +384,7 @@ namespace nanojit
     typedef uint8_t NIns;
 
     // Bytes of icache to flush after Assembler::patch
-    const size_t LARGEST_BRANCH_PATCH = 16 * sizeof(Nins);
+    const size_t LARGEST_BRANCH_PATCH = 16 * sizeof(NIns);
 
     inline Register nextreg(Register r) {
         return Register(r+1);
