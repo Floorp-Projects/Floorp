db9114e8fb4ed37e690d9be8c5636d6a83e180cc
David Anderson <danderson@mozilla.com>
2009-10-23 14:28:35 -0700

    Fixed decompiler assertion related to JSOP_TRACE (bug 522624, r=brendan).

--- js/src/jsopcode.cpp
+++ js/src/jsopcode.cpp
@@ -4981,7 +4981,8 @@ js_DecompileFunction(JSPrinter *jp)
 
 #if JS_HAS_DESTRUCTURING
         /* Skip trace hint if it appears here. */
-        if (js_GetOpcode(jp->sprinter.context, fun->u.i.script, pc) == JSOP_TRACE) {
+        JSOp op = js_GetOpcode(jp->sprinter.context, fun->u.i.script, pc);
+        if (op == JSOP_TRACE || op == JSOP_NOP) {
             JS_STATIC_ASSERT(JSOP_TRACE_LENGTH == JSOP_NOP_LENGTH);
             pc += JSOP_TRACE_LENGTH;
         }
