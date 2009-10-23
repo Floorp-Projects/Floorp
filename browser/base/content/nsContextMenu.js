4b57d6e20bc814e4bd731fe71ac927a1b85b7b3e
Tanner M. Young <mozilla.bugs@alyoung.com>
2009-10-22 19:32:49 -0500

    Bug 517902 - "Reimplement image properties, using the existing "Media" panel" [ui-r=faaborg r=db48x sr=mconnor]

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -257,6 +257,8 @@ nsContextMenu.prototype = {
     this.showItem("context-sep-viewbgimage", shouldShow && !this._hasMultipleBGImages);
     document.getElementById("context-viewbgimage")
             .disabled = !this.hasBGImage;
+
+    this.showItem("context-viewimageinfo", (this.onImage));
   },
 
   initMiscItems: function CM_initMiscItems() {
@@ -774,6 +776,10 @@ nsContextMenu.prototype = {
 
   viewInfo: function() {
     BrowserPageInfo(this.target.ownerDocument.defaultView.top.document);
+  },
+
+  viewImageInfo: function() {
+    BrowserPageInfo(this.target.ownerDocument.defaultView.top.document,"mediaTab",this.mediaURL);
   },
 
   viewFrameInfo: function() {
