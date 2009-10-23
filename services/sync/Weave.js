53c1af3392610d27aefa7d20ff75f6c7fab81e20
Edward Lee <edilee@mozilla.com>
2009-10-22 23:46:50 -0700

    Bug 524052 - Remove about:weave Remove files under content/about and content/ext (jQuery) and about:weave service.

--- services/sync/Weave.js
+++ services/sync/Weave.js
@@ -66,30 +66,7 @@ WeaveService.prototype = {
   }
 };
 
-function AboutWeaveService() {}
-AboutWeaveService.prototype = {
-  classDescription: "about:weave",
-  contractID: "@mozilla.org/network/protocol/about;1?what=weave",
-  classID: Components.ID("{ecb6987d-9d71-475d-a44d-a5ff2099b08c}"),
-
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAboutModule,
-                                         Ci.nsISupportsWeakReference]),
-
-  getURIFlags: function(aURI) {
-    return (Ci.nsIAboutModule.ALLOW_SCRIPT |
-            Ci.nsIAboutModule.URI_SAFE_FOR_UNTRUSTED_CONTENT);
-  },
-
-  newChannel: function(aURI) {
-    let ios = Cc["@mozilla.org/network/io-service;1"]
-      .getService(Ci.nsIIOService);
-    let ch = ios.newChannel("chrome://weave/content/about/index.html", null, null);
-    ch.originalURI = aURI;
-    return ch;
-  }
-};
-
 function NSGetModule(compMgr, fileSpec) {
-  return XPCOMUtils.generateModule([WeaveService, AboutWeaveService]);
+  return XPCOMUtils.generateModule([WeaveService]);
 }
 
