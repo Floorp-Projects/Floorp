881804a6663b831098cd4196ee2635827fc6c5d7
Olli Pettay <Olli.Pettay@helsinki.fi>
2009-10-24 18:39:28 +0300

    Bug 517851 -  nsJSEventListener's string/atom handling can be optimized a bit, r=jst

--- content/xbl/src/nsXBLPrototypeHandler.cpp
+++ content/xbl/src/nsXBLPrototypeHandler.cpp
@@ -334,11 +334,9 @@ nsXBLPrototypeHandler::ExecuteHandler(nsPIDOMEventTarget* aTarget,
   // Execute it.
   nsCOMPtr<nsIDOMEventListener> eventListener;
   NS_NewJSEventListener(boundContext, scope,
-                        scriptTarget, getter_AddRefs(eventListener));
+                        scriptTarget, onEventAtom,
+                        getter_AddRefs(eventListener));
 
-  nsCOMPtr<nsIJSEventListener> jsListener(do_QueryInterface(eventListener));
-  jsListener->SetEventName(onEventAtom);
-  
   // Handle the event.
   eventListener->HandleEvent(aEvent);
   return NS_OK;
