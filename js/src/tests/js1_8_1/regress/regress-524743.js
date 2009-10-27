0cc1aa6a756ff10691744db48e031843ac6f3273
Jason Orendorff <jorendorff@mozilla.com>
2009-10-27 16:15:50 -0500

    Tweak test js1_8_1/regress/regress-524743.js to not use Math.random. no_r=me.

--- js/src/tests/js1_8_1/regress/regress-524743.js
+++ js/src/tests/js1_8_1/regress/regress-524743.js
@@ -3,12 +3,10 @@
 if (typeof gczeal != 'undefined')
     gczeal(2);
 if (typeof gc != 'undefined') {
-    for (var i = 0; i < 10; i++) {
-        var obj = {};
-        for (var j = 0; j < 5; j++) {
-            obj[Math.floor(Math.random() * 5)] = 0;
-            gc();
-        }
+    var obj = {};
+    for (var i = 0; i < 50; i++) {
+        obj["_" + i] = 0;
+        gc();
     }
 }
 reportCompare("no assertion failure", "no assertion failure", "bug 524743");
