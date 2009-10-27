f948c688882986fb42ce7b3b96fea46e551f2f99
Graydon Hoare <graydon@mozilla.com>
2009-10-27 15:24:12 -0700

    Bug 524587 - remove softfloat guards, restore CodeAlloc ref-ptr, r=gal.

--- js/src/nanojit/CodeAlloc.cpp
+++ js/src/nanojit/CodeAlloc.cpp
@@ -244,7 +244,7 @@ namespace nanojit
 #if defined NANOJIT_ARM && defined UNDER_CE
     // Use a single flush for the whole CodeList, when we have no
     // finer-granularity flush support, as on WinCE.
-    void CodeAlloc::flushICache(CodeList*) {
+    void CodeAlloc::flushICache(CodeList* &/*blocks*/) {
         FlushInstructionCache(GetCurrentProcess(), NULL, NULL);
     }
 #else
