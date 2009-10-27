cc4f335fc9f60102b2e60368d721faab3e39609c
Doug Turner <dougt@meer.net>
2009-10-26 22:18:35 -0700

    Bug 521257 - Support NPImageExpose on Maemo/Hildon only. r=roc

--- modules/plugin/base/src/nsNPAPIPluginInstance.cpp
+++ modules/plugin/base/src/nsNPAPIPluginInstance.cpp
@@ -881,6 +881,7 @@ nsNPAPIPluginInstance::nsNPAPIPluginInstance(NPPluginFuncs* callbacks,
 #endif
 #endif
     mWindowless(PR_FALSE),
+    mWindowlessLocal(PR_FALSE),
     mTransparent(PR_FALSE),
     mStarted(PR_FALSE),
     mCached(PR_FALSE),
@@ -1382,6 +1383,14 @@ NS_IMETHODIMP nsNPAPIPluginInstance::HandleEvent(void* event, PRBool* handled)
 
 NS_IMETHODIMP nsNPAPIPluginInstance::GetValueFromPlugin(NPPVariable variable, void* value)
 {
+#ifdef MOZ_PLATFORM_HILDON
+  // The maemo flash plugin does not remember this.  It sets the
+  // value, but doesn't support the get value.
+  if (variable == NPPVpluginWindowlessLocalBool) {
+    *(NPBool*)value = mWindowlessLocal;
+    return NS_OK;
+  }
+#endif
   nsresult  res = NS_OK;
   if (mCallbacks->getvalue && mStarted) {
     PluginDestructionGuard guard(this);
@@ -1418,6 +1427,12 @@ nsresult nsNPAPIPluginInstance::GetCallbacks(const NPPluginFuncs ** aCallbacks)
 NPError nsNPAPIPluginInstance::SetWindowless(PRBool aWindowless)
 {
   mWindowless = aWindowless;
+  return NPERR_NO_ERROR;
+}
+
+NPError nsNPAPIPluginInstance::SetWindowlessLocal(PRBool aWindowlessLocal)
+{
+  mWindowlessLocal = aWindowlessLocal;
   return NPERR_NO_ERROR;
 }
 
