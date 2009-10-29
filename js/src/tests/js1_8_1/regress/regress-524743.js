b1126fe559a5e70175b420c2f36b7e076d2ee55e
Bob Clary <bclary@bclary.com>
2009-10-29 00:44:40 -0700

    bug 524743 - js1_8_1/regress/regress-524743.js must turn off gczeal(2) before exiting; skip in debug browser due to hang.

--- js/src/tests/js1_8_1/regress/regress-524743.js
+++ js/src/tests/js1_8_1/regress/regress-524743.js
@@ -1,12 +1,20 @@
 // Any copyright is dedicated to the Public Domain.
 // http://creativecommons.org/licenses/publicdomain/
-if (typeof gczeal != 'undefined')
-    gczeal(2);
-if (typeof gc != 'undefined') {
-    var obj = {};
-    for (var i = 0; i < 50; i++) {
-        obj["_" + i] = 0;
-        gc();
+gTestfile = 'regress-524743';
+
+if (typeof gczeal != 'undefined' && typeof gc != 'undefined') {
+    try
+    {
+        gczeal(2);
+        var obj = {};
+        for (var i = 0; i < 50; i++) {
+            obj["_" + i] = 0;
+            gc();
+        }
     }
+    catch(ex)
+    {
+    }
+    gczeal(0);
 }
 reportCompare("no assertion failure", "no assertion failure", "bug 524743");
