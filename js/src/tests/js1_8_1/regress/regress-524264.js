e70482a59f00b45da4e20da83db56db49a417e28
Jason Orendorff <jorendorff@mozilla.com>
2009-10-30 14:56:10 -0500

    Bug 524564 - "Assertion failure: 0, at ../jsopcode.cpp" with uneval, yield, do...while. r=dvander.

---
+++ js/src/tests/js1_8_1/regress/regress-524264.js
@@ -0,0 +1,8 @@
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/licenses/publicdomain/
+ * Contributor: Jason Orendorff
+ */
+gTestfile = 'regress-524264';
+uneval(function () { do yield; while (0); });
+reportCompare("no assertion failure", "no assertion failure", "bug 524264");
