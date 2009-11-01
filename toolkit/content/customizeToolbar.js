bd2e143dc7d655319ef009a8f8aabd4654fad9d9
Dão Gottwald <dao@mozilla.com>
2009-11-02 00:24:12 +0100

    Bug 525712 - Remove dead throbber-specific code from customizeToolbar.js, handle it in themes. r=gavin

--- toolkit/content/customizeToolbar.js
+++ toolkit/content/customizeToolbar.js
@@ -488,10 +488,6 @@ function cleanUpItemForPalette(aItem, aWrapper)
 
   if (aItem.localName == "toolbaritem" && aItem.firstChild) {
     aItem.firstChild.removeAttribute("observes");
-
-    // So the throbber doesn't throb in the dialog,
-    // cute as that may be...
-    aItem.firstChild.removeAttribute("busy");
   }
 }
 
