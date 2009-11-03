15d86cc6ff300ed071929d17208e69a1bc96be55
Robert O'Callahan <robert@ocallahan.org>
2009-11-04 07:39:42 +1300

    Bug 513082. Take advantage of our knowledge that nsDisplayClip, when used in a blitting scrolling frame, must snap to device pixels, to get more accurate blit regions. r=dbaron

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -1180,8 +1180,16 @@ AddItemsToRegion(nsDisplayListBuilder* aBuilder, nsDisplayList* aList,
         // If the clipping frame is moving, then it isn't clipping any
         // non-moving content (see ApplyAbsPosClipping), so we don't need
         // to do anything special, but we should not restrict aClipRect.
-        if (!aBuilder->IsMovingFrame(clipItem->GetClippingFrame())) {
-          clip.IntersectRect(clip, clipItem->GetClipRect());
+        nsIFrame* clipFrame = clipItem->GetClippingFrame();
+        if (!aBuilder->IsMovingFrame(clipFrame)) {
+          nscoord appUnitsPerDevPixel = clipFrame->PresContext()->AppUnitsPerDevPixel();
+          // We know the nsDisplayClip will snap because we're in a context
+          // where pixels can be blitted and we don't traverse down through
+          // an nsDisplayTransform.
+          nsRect snappedClip =
+            clipItem->GetClipRect().ToNearestPixels(appUnitsPerDevPixel).
+            ToAppUnits(appUnitsPerDevPixel);
+          clip.IntersectRect(clip, snappedClip);
 
           // Invalidate the translation of the source area that was clipped out
           nsRegion clippedOutSource;
