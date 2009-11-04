21273de915b95521e2e1099a95b819048ba885f9
Edward Lee <edilee@mozilla.com>
2009-11-04 15:27:08 -0800

    Bug 513438 - No easy way to enable / re-enable Weave Assume Weave is always enabled (don't store it in a pref) and only disable if checks fail on startup.

--- services/sync/services-sync.js
+++ services/sync/services-sync.js
@@ -10,7 +10,6 @@ pref("extensions.weave.lastversion", "firstrun");
 
 pref("extensions.weave.rememberpassword", true);
 pref("extensions.weave.autoconnect", true);
-pref("extensions.weave.enabled", true);
 
 pref("extensions.weave.syncOnQuit.enabled", true);
 
