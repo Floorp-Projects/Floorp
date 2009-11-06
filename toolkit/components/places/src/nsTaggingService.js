7a081bc973a12f5a2272283bfe208b6920766008
Marco Bonardo <mbonardo@mozilla.com>
2009-11-06 16:27:48 +0100

    Bug 526776 - TagAutoCompleteSearch wants to implement nsITimerCallback, r=dietrich

--- toolkit/components/places/src/nsTaggingService.js
+++ toolkit/components/places/src/nsTaggingService.js
@@ -660,8 +660,7 @@ TagAutoCompleteSearch.prototype = {
   },
 
   // nsISupports
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAutoCompleteSearch,
-                                         Ci.nsITimerCallback]), 
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAutoCompleteSearch]), 
 
   classDescription: "Places Tag AutoComplete",
   contractID: "@mozilla.org/autocomplete/search;1?name=places-tag-autocomplete",
