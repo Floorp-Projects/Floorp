d2bab04fe5ee326236ce7dbfb3c4fc68a2571d38
Markus Stange <mstange@themasta.com>
2009-11-06 11:21:41 +0100

    Bug 376071 - When starting a drag, pass the mouse down event that started the drag (instead of the last mouse move event) so that the drag image can be positioned correctly. r=josh
    --HG--
    extra : rebase_source : e62fc324934c2abe38dce6936d8cab09ee1e2715

--- widget/src/cocoa/nsDragService.mm
+++ widget/src/cocoa/nsDragService.mm
@@ -75,7 +75,7 @@ extern PRLogModuleInfo* sCocoaLog;
 
 extern NSPasteboard* globalDragPboard;
 extern NSView* gLastDragView;
-extern NSEvent* gLastDragEvent;
+extern NSEvent* gLastDragMouseDownEvent;
 extern PRBool gUserCancelledDrag;
 
 // This global makes the transferable array available to Cocoa's promised
@@ -166,7 +166,7 @@ nsDragService::ConstructDragImage(nsIDOMNode* aDOMNode,
 {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NIL;
 
-  NSPoint screenPoint = [[gLastDragView window] convertBaseToScreen:[gLastDragEvent locationInWindow]];
+  NSPoint screenPoint = [[gLastDragView window] convertBaseToScreen:[gLastDragMouseDownEvent locationInWindow]];
   // Y coordinates are bottom to top, so reverse this
   if ([[NSScreen screens] count] > 0)
     screenPoint.y = NSMaxY([[[NSScreen screens] objectAtIndex:0] frame]) - screenPoint.y;
@@ -303,12 +303,12 @@ nsDragService::InvokeDragSession(nsIDOMNode* aDOMNode, nsISupportsArray* aTransf
 
   // We need to retain the view and the event during the drag in case either gets destroyed.
   mNativeDragView = [gLastDragView retain];
-  mNativeDragEvent = [gLastDragEvent retain];
+  mNativeDragEvent = [gLastDragMouseDownEvent retain];
 
   gUserCancelledDrag = PR_FALSE;
   [mNativeDragView dragImage:image
                           at:localPoint
-                      offset:NSMakeSize(0,0)
+                      offset:NSZeroSize
                        event:mNativeDragEvent
                   pasteboard:[NSPasteboard pasteboardWithName:NSDragPboard]
                       source:mNativeDragView
