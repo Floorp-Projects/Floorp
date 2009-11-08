9e6ad606e72483675f0f5ea3173a33250bef9e9a
Dão Gottwald <dao@mozilla.com>
2009-11-08 10:22:11 +0100

    Bug 5265285 - getAllLogins, getAllDisabledHosts and getAllEncryptedLogins count out params should be optional. r=dolske

--- browser/base/content/sanitize.js
+++ browser/base/content/sanitize.js
@@ -356,19 +356,19 @@ Sanitizer.prototype = {
       {
         // Clear site-specific permissions like "Allow this site to open popups"
         var pm = Components.classes["@mozilla.org/permissionmanager;1"]
-                          .getService(Components.interfaces.nsIPermissionManager);
+                           .getService(Components.interfaces.nsIPermissionManager);
         pm.removeAll();
         
         // Clear site-specific settings like page-zoom level
         var cps = Components.classes["@mozilla.org/content-pref/service;1"]
-                           .getService(Components.interfaces.nsIContentPrefService);
+                            .getService(Components.interfaces.nsIContentPrefService);
         cps.removeGroupedPrefs();
         
         // Clear "Never remember passwords for this site", which is not handled by
         // the permission manager
         var pwmgr = Components.classes["@mozilla.org/login-manager;1"]
-                   .getService(Components.interfaces.nsILoginManager);
-        var hosts = pwmgr.getAllDisabledHosts({})
+                              .getService(Components.interfaces.nsILoginManager);
+        var hosts = pwmgr.getAllDisabledHosts();
         for each (var host in hosts) {
           pwmgr.setLoginSavingEnabled(host, true);
         }
--- browser/components/privatebrowsing/src/nsPrivateBrowsingService.js
+++ browser/components/privatebrowsing/src/nsPrivateBrowsingService.js
@@ -561,7 +561,7 @@ PrivateBrowsingService.prototype = {
               getService(Ci.nsILoginManager)) {
       // Clear all passwords for domain
       try {
-        let logins = lm.getAllLogins({});
+        let logins = lm.getAllLogins();
         for (let i = 0; i < logins.length; i++)
           if (logins[i].hostname.hasRootDomain(aDomain))
             lm.removeLogin(logins[i]);
@@ -571,7 +571,7 @@ PrivateBrowsingService.prototype = {
       catch (ex if ex.message.indexOf("User canceled Master Password entry") != -1) { }
 
       // Clear any "do not save for this site" for this domain
-      let disabledHosts = lm.getAllDisabledHosts({});
+      let disabledHosts = lm.getAllDisabledHosts();
       for (let i = 0; i < disabledHosts.length; i++)
         if (disabledHosts[i].hasRootDomain(aDomain))
           lm.setLoginSavingEnabled(disabledHosts, true);
