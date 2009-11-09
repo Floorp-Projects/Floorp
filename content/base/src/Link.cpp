a91c45ecd62278505b113de21d84ac78c402254d
Shawn Wilsher <sdwilsh@shawnwilsher.com>
2009-11-09 10:00:54 -0800

    Bug 461199 (Part 9) - Move href helpers from nsGenericHTMLElement to mozilla::dom::Link. Move the helpers so that they can easily use the cached URI when available. r=sicking sr=bz

--- content/base/src/Link.cpp
+++ content/base/src/Link.cpp
@@ -40,6 +40,11 @@
 #include "Link.h"
 
 #include "nsIEventStateManager.h"
+#include "nsIURL.h"
+
+#include "nsEscape.h"
+#include "nsGkAtoms.h"
+#include "nsString.h"
 
 namespace mozilla {
 namespace dom {
@@ -99,6 +104,288 @@ Link::GetURI() const
   return uri.forget();
 }
 
+nsresult
+Link::SetProtocol(const nsAString &aProtocol)
+{
+  nsCOMPtr<nsIURI> uri(GetURIToMutate());
+  if (!uri) {
+    // Ignore failures to be compatible with NS4.
+    return NS_OK;
+  }
+
+  nsAString::const_iterator start, end;
+  aProtocol.BeginReading(start);
+  aProtocol.EndReading(end);
+  nsAString::const_iterator iter(start);
+  (void)FindCharInReadable(':', iter, end);
+  (void)uri->SetScheme(NS_ConvertUTF16toUTF8(Substring(start, iter)));
+
+  SetHrefAttribute(uri);
+  return NS_OK;
+}
+
+nsresult
+Link::SetHost(const nsAString &aHost)
+{
+  nsCOMPtr<nsIURI> uri(GetURIToMutate());
+  if (!uri) {
+    // Ignore failures to be compatible with NS4.
+    return NS_OK;
+  }
+
+  // We cannot simply call nsIURI::SetHost because that would treat the name as
+  // an IPv6 address (like http:://[server:443]/).  We also cannot call
+  // nsIURI::SetHostPort because that isn't implemented.  Sadfaces.
+
+  // First set the hostname.
+  nsAString::const_iterator start, end;
+  aHost.BeginReading(start);
+  aHost.EndReading(end);
+  nsAString::const_iterator iter(start);
+  (void)FindCharInReadable(':', iter, end);
+  NS_ConvertUTF16toUTF8 host(Substring(start, iter));
+  (void)uri->SetHost(host);
+
+  // Also set the port if needed.
+  if (iter != end) {
+    iter++;
+    if (iter != end) {
+      nsAutoString portStr(Substring(iter, end));
+      nsresult rv;
+      PRInt32 port = portStr.ToInteger((PRInt32 *)&rv);
+      if (NS_SUCCEEDED(rv)) {
+        (void)uri->SetPort(port);
+      }
+    }
+  };
+
+  SetHrefAttribute(uri);
+  return NS_OK;
+}
+
+nsresult
+Link::SetHostname(const nsAString &aHostname)
+{
+  nsCOMPtr<nsIURI> uri(GetURIToMutate());
+  if (!uri) {
+    // Ignore failures to be compatible with NS4.
+    return NS_OK;
+  }
+
+  (void)uri->SetHost(NS_ConvertUTF16toUTF8(aHostname));
+  SetHrefAttribute(uri);
+  return NS_OK;
+}
+
+nsresult
+Link::SetPathname(const nsAString &aPathname)
+{
+  nsCOMPtr<nsIURI> uri(GetURIToMutate());
+  nsCOMPtr<nsIURL> url(do_QueryInterface(uri));
+  if (!url) {
+    // Ignore failures to be compatible with NS4.
+    return NS_OK;
+  }
+
+  (void)url->SetFilePath(NS_ConvertUTF16toUTF8(aPathname));
+  SetHrefAttribute(uri);
+  return NS_OK;
+}
+
+nsresult
+Link::SetSearch(const nsAString &aSearch)
+{
+  nsCOMPtr<nsIURI> uri(GetURIToMutate());
+  nsCOMPtr<nsIURL> url(do_QueryInterface(uri));
+  if (!url) {
+    // Ignore failures to be compatible with NS4.
+    return NS_OK;
+  }
+
+  (void)url->SetQuery(NS_ConvertUTF16toUTF8(aSearch));
+  SetHrefAttribute(uri);
+  return NS_OK;
+}
+
+nsresult
+Link::SetPort(const nsAString &aPort)
+{
+  nsCOMPtr<nsIURI> uri(GetURIToMutate());
+  if (!uri) {
+    // Ignore failures to be compatible with NS4.
+    return NS_OK;
+  }
+
+  nsresult rv;
+  nsAutoString portStr(aPort);
+  PRInt32 port = portStr.ToInteger((PRInt32 *)&rv);
+  if (NS_FAILED(rv)) {
+    return NS_OK;
+  }
+
+  (void)uri->SetPort(port);
+  SetHrefAttribute(uri);
+  return NS_OK;
+}
+
+nsresult
+Link::SetHash(const nsAString &aHash)
+{
+  nsCOMPtr<nsIURI> uri(GetURIToMutate());
+  nsCOMPtr<nsIURL> url(do_QueryInterface(uri));
+  if (!url) {
+    // Ignore failures to be compatible with NS4.
+    return NS_OK;
+  }
+
+  (void)url->SetRef(NS_ConvertUTF16toUTF8(aHash));
+  SetHrefAttribute(uri);
+  return NS_OK;
+}
+
+nsresult
+Link::GetProtocol(nsAString &_protocol)
+{
+  nsCOMPtr<nsIURI> uri(GetURI());
+  if (!uri) {
+    _protocol.AssignLiteral("http");
+  }
+  else {
+    nsCAutoString scheme;
+    (void)uri->GetScheme(scheme);
+    CopyASCIItoUTF16(scheme, _protocol);
+  }
+  _protocol.Append(PRUnichar(':'));
+  return NS_OK;
+}
+
+nsresult
+Link::GetHost(nsAString &_host)
+{
+  _host.Truncate();
+
+  nsCOMPtr<nsIURI> uri(GetURI());
+  if (!uri) {
+    // Do not throw!  Not having a valid URI should result in an empty string.
+    return NS_OK;
+  }
+
+  nsCAutoString hostport;
+  nsresult rv = uri->GetHostPort(hostport);
+  if (NS_SUCCEEDED(rv)) {
+    CopyUTF8toUTF16(hostport, _host);
+  }
+  return NS_OK;
+}
+
+nsresult
+Link::GetHostname(nsAString &_hostname)
+{
+  _hostname.Truncate();
+
+  nsCOMPtr<nsIURI> uri(GetURI());
+  if (!uri) {
+    // Do not throw!  Not having a valid URI should result in an empty string.
+    return NS_OK;
+  }
+
+  nsCAutoString host;
+  nsresult rv = uri->GetHost(host);
+  // Note that failure to get the host from the URI is not necessarily a bad
+  // thing.  Some URIs do not have a host.
+  if (NS_SUCCEEDED(rv)) {
+    CopyUTF8toUTF16(host, _hostname);
+  }
+  return NS_OK;
+}
+
+nsresult
+Link::GetPathname(nsAString &_pathname)
+{
+  _pathname.Truncate();
+
+  nsCOMPtr<nsIURI> uri(GetURI());
+  nsCOMPtr<nsIURL> url(do_QueryInterface(uri));
+  if (!url) {
+    // Do not throw!  Not having a valid URI or URL should result in an empty
+    // string.
+    return NS_OK;
+  }
+
+  nsCAutoString file;
+  nsresult rv = url->GetFilePath(file);
+  NS_ENSURE_SUCCESS(rv, rv);
+  CopyUTF8toUTF16(file, _pathname);
+  return NS_OK;
+}
+
+nsresult
+Link::GetSearch(nsAString &_search)
+{
+  _search.Truncate();
+
+  nsCOMPtr<nsIURI> uri(GetURI());
+  nsCOMPtr<nsIURL> url(do_QueryInterface(uri));
+  if (!url) {
+    // Do not throw!  Not having a valid URI or URL should result in an empty
+    // string.
+    return NS_OK;
+  }
+
+  nsCAutoString search;
+  nsresult rv = url->GetQuery(search);
+  if (NS_SUCCEEDED(rv) && !search.IsEmpty()) {
+    CopyUTF8toUTF16(NS_LITERAL_CSTRING("?") + search, _search);
+  }
+  return NS_OK;
+}
+
+nsresult
+Link::GetPort(nsAString &_port)
+{
+  _port.Truncate();
+
+  nsCOMPtr<nsIURI> uri(GetURI());
+  if (!uri) {
+    // Do not throw!  Not having a valid URI should result in an empty string.
+    return NS_OK;
+  }
+
+  PRInt32 port;
+  nsresult rv = uri->GetPort(&port);
+  // Note that failure to get the port from the URI is not necessarily a bad
+  // thing.  Some URIs do not have a port.
+  if (NS_SUCCEEDED(rv) && port != -1) {
+    nsAutoString portStr;
+    portStr.AppendInt(port, 10);
+    _port.Assign(portStr);
+  }
+  return NS_OK;
+}
+
+nsresult
+Link::GetHash(nsAString &_hash)
+{
+  _hash.Truncate();
+
+  nsCOMPtr<nsIURI> uri(GetURI());
+  nsCOMPtr<nsIURL> url(do_QueryInterface(uri));
+  if (!url) {
+    // Do not throw!  Not having a valid URI or URL should result in an empty
+    // string.
+    return NS_OK;
+  }
+
+  nsCAutoString ref;
+  nsresult rv = url->GetRef(ref);
+  if (NS_SUCCEEDED(rv) && !ref.IsEmpty()) {
+    NS_UnescapeURL(ref); // XXX may result in random non-ASCII bytes!
+    _hash.Assign(PRUnichar('#'));
+    AppendUTF8toUTF16(ref, _hash);
+  }
+  return NS_OK;
+}
+
 void
 Link::ResetLinkState()
 {
@@ -116,6 +403,31 @@ Link::ResetLinkState()
 
   // Get rid of our cached URI.
   mCachedURI = nsnull;
+}
+
+already_AddRefed<nsIURI>
+Link::GetURIToMutate()
+{
+  nsCOMPtr<nsIURI> uri(GetURI());
+  if (!uri) {
+    return nsnull;
+  }
+  nsCOMPtr<nsIURI> clone;
+  (void)uri->Clone(getter_AddRefs(clone));
+  return clone.forget();
+}
+
+void
+Link::SetHrefAttribute(nsIURI *aURI)
+{
+  NS_ASSERTION(aURI, "Null URI is illegal!");
+  nsCOMPtr<nsIContent> content(do_QueryInterface(this));
+  NS_ASSERTION(content, "Why isn't this an nsIContent node?!");
+
+  nsCAutoString href;
+  (void)aURI->GetSpec(href);
+  (void)content->SetAttr(kNameSpaceID_None, nsGkAtoms::href,
+                         NS_ConvertUTF8toUTF16(href), PR_TRUE);
 }
 
 } // namespace dom
