d5cb008c0f0ac41995cea8d8b5377df9cf16ab94
Shawn Wilsher <sdwilsh@shawnwilsher.com>
2009-11-09 10:00:53 -0800

    Bug 461199 (Part 3) - Create a cached getter for mozilla::IHistory on nsContentUtils. r=sicking

--- content/base/src/nsContentUtils.cpp
+++ content/base/src/nsContentUtils.cpp
@@ -98,6 +98,8 @@
 #include "imgIRequest.h"
 #include "imgIContainer.h"
 #include "imgILoader.h"
+#include "mozilla/IHistory.h"
+#include "nsDocShellCID.h"
 #include "nsIImageLoadingContent.h"
 #include "nsIInterfaceRequestor.h"
 #include "nsIInterfaceRequestorUtils.h"
@@ -201,6 +203,7 @@ nsIXTFService *nsContentUtils::sXTFService = nsnull;
 nsIPrefBranch2 *nsContentUtils::sPrefBranch = nsnull;
 imgILoader *nsContentUtils::sImgLoader;
 imgICache *nsContentUtils::sImgCache;
+mozilla::IHistory *nsContentUtils::sHistory;
 nsIConsoleService *nsContentUtils::sConsoleService;
 nsDataHashtable<nsISupportsHashKey, EventNameMapping>* nsContentUtils::sEventTable = nsnull;
 nsIStringBundleService *nsContentUtils::sStringBundleService;
@@ -373,6 +376,9 @@ nsContentUtils::Init()
     if (NS_FAILED(CallGetService("@mozilla.org/image/cache;1", &sImgCache )))
       sImgCache = nsnull;
   }
+
+  rv = CallGetService(NS_IHISTORY_CONTRACTID, &sHistory);
+  NS_ENSURE_SUCCESS(rv, rv);
 
   sPtrsToPtrsToRelease = new nsTArray<nsISupports**>();
   if (!sPtrsToPtrsToRelease) {
@@ -954,6 +960,7 @@ nsContentUtils::Shutdown()
 #endif
   NS_IF_RELEASE(sImgLoader);
   NS_IF_RELEASE(sImgCache);
+  NS_IF_RELEASE(sHistory);
   NS_IF_RELEASE(sPrefBranch);
 #ifdef IBMBIDI
   NS_IF_RELEASE(sBidiKeyboard);
