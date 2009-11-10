0c09795fa8260f0492f2fd9f232caa0c1da632b3
Phil Ringnalda <philringnalda@gmail.com>
2009-11-09 19:45:23 -0800

    Backed out changeset a696d331ebf7 (bug 526789) for test failures

--- extensions/cookie/test/unit/test_bug481775.js
+++ extensions/cookie/test/unit/test_bug481775.js
@@ -2,7 +2,9 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 
 function run_test() {
+  var cs = Cc["@mozilla.org/cookieService;1"].getService(Ci.nsICookieService);
   var cm = Cc["@mozilla.org/cookiemanager;1"].getService(Ci.nsICookieManager2);
+  var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
   var prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
   var pb = null;
   try {
@@ -11,7 +13,7 @@ function run_test() {
 
   // accept all cookies and clear the table
   prefs.setIntPref("network.cookie.lifetimePolicy", 0);
-  cm.removeAll();
+  cs.removeAll();
 
   // saturate the cookie table
   addCookies(0, 5000);
@@ -35,7 +37,7 @@ function run_test() {
     do_check_eq(getCookieCount(), count);
 
     // remove them all
-    cm.removeAll();
+    cs.removeAll();
     do_check_eq(getCookieCount(), 0);
 
     // leave private browsing mode
@@ -53,7 +55,7 @@ function run_test() {
   do_check_eq(getCookieCount(), count);
 
   // remove them all
-  cm.removeAll();
+  cs.removeAll();
   do_check_eq(getCookieCount(), 0);
 }
 
