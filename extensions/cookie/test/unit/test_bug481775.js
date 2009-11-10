e9f7ee50f894b636b0bf06ab6a9409497330ceea
Dan Witte <dwitte@mozilla.com>
2009-11-09 16:21:57 -0800

    Bug 526789 - UMR in nsCookieService::CountCookiesFromHostInternal, GetCookiesFromHost, and GetCookieInternal when given empty host string. r=biesi

--- extensions/cookie/test/unit/test_bug481775.js
+++ extensions/cookie/test/unit/test_bug481775.js
@@ -2,9 +2,7 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 
 function run_test() {
-  var cs = Cc["@mozilla.org/cookieService;1"].getService(Ci.nsICookieService);
   var cm = Cc["@mozilla.org/cookiemanager;1"].getService(Ci.nsICookieManager2);
-  var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
   var prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
   var pb = null;
   try {
@@ -13,7 +11,7 @@ function run_test() {
 
   // accept all cookies and clear the table
   prefs.setIntPref("network.cookie.lifetimePolicy", 0);
-  cs.removeAll();
+  cm.removeAll();
 
   // saturate the cookie table
   addCookies(0, 5000);
@@ -37,7 +35,7 @@ function run_test() {
     do_check_eq(getCookieCount(), count);
 
     // remove them all
-    cs.removeAll();
+    cm.removeAll();
     do_check_eq(getCookieCount(), 0);
 
     // leave private browsing mode
@@ -55,7 +53,7 @@ function run_test() {
   do_check_eq(getCookieCount(), count);
 
   // remove them all
-  cs.removeAll();
+  cm.removeAll();
   do_check_eq(getCookieCount(), 0);
 }
 
