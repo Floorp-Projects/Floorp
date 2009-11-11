ffed43b4ec9acac4572e251c70e4d05b7187f7fe
Reed Loden <reed@reedloden.com>
2009-11-10 23:39:13 -0600

    Another follow-up to bug 527784: just use resource:/// across the board because xpcshell doesn't implement resource://app/ like toolkit's nsXREDirProvider does.

--- browser/base/content/openLocation.js
+++ browser/base/content/openLocation.js
@@ -49,7 +49,7 @@ try {
   // not critical, remain silent
 }
 
-Components.utils.import("resource://app/modules/openLocationLastURL.jsm");
+Components.utils.import("resource:///modules/openLocationLastURL.jsm");
 
 function onLoad()
 {
