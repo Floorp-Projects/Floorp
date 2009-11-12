f490c3a4b6b6ca4756e685e0bf17b1324bfc74d4
Daniel Holbert <dholbert@cs.stanford.edu>
2009-11-12 09:23:31 -0800

    Bug 509828 followup: Reorder nsXULWindow constructor's initializer list, to fix build warning. r=bz

--- xpfe/appshell/src/nsXULWindow.cpp
+++ xpfe/appshell/src/nsXULWindow.cpp
@@ -143,15 +143,15 @@ nsXULWindow::nsXULWindow(PRUint32 aChromeFlags)
     mCenterAfterLoad(PR_FALSE),
     mIsHiddenWindow(PR_FALSE),
     mLockedUntilChromeLoad(PR_FALSE),
+    mIgnoreXULSize(PR_FALSE),
+    mIgnoreXULPosition(PR_FALSE),
     mContextFlags(0),
     mBlurSuppressionLevel(0),
     mPersistentAttributesDirty(0),
     mPersistentAttributesMask(0),
     mChromeFlags(aChromeFlags),
     // best guess till we have a widget
-    mAppPerDev(nsPresContext::AppUnitsPerCSSPixel()),
-    mIgnoreXULSize(PR_FALSE),
-    mIgnoreXULPosition(PR_FALSE)
+    mAppPerDev(nsPresContext::AppUnitsPerCSSPixel())
 {
 }
 
