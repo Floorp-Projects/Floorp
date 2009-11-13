a291a5e0f3ac041fe1ef844a38ff3aef68f4e782
Ehsan Akhgari <ehsan@mozilla.com>
2009-11-13 12:07:46 -0500

    Bug 524006 - toolkit/content/tests/widgets/test_videocontrols_audio_direction.html fails on Windows debug builds

--- toolkit/content/tests/widgets/videocontrols_direction_test.js
+++ toolkit/content/tests/widgets/videocontrols_direction_test.js
@@ -21,7 +21,8 @@ RemoteCanvas.prototype.load = function(callback) {
   iframe.addEventListener("load", function() {
     var m = iframe.contentDocument.getElementById("av");
     m.addEventListener("suspend", function(aEvent) {
-      if (aEvent.loaded == aEvent.total) {
+      if (m.readyState == m.HAVE_ENOUGH_DATA &&
+          aEvent.loaded == aEvent.total) {
         m.removeEventListener("suspend", arguments.callee, false);
         setTimeout(function() {
           me.remotePageLoaded(callback);
