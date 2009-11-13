afee15ba6118b2efe3f5ee78e68eb1226f6ddf7f
Doug Turner <dougt@meer.net>
2009-11-13 07:10:53 -0800

    Bug 528429 - MOZ_COMPOSITED_PLUGINS should be disabled when building for Qt. r=vlad

--- modules/plugin/base/public/nsPluginNativeWindow.h
+++ modules/plugin/base/public/nsPluginNativeWindow.h
@@ -103,7 +103,7 @@ public:
     SetPluginInstance(aPluginInstance);
     return NS_OK;
   }
-#ifdef MOZ_PLATFORM_HILDON
+#if defined(MOZ_PLATFORM_HILDON) && defined(MOZ_WIDGET_GTK2)
 #define MOZ_COMPOSITED_PLUGINS
 #endif
 #ifdef MOZ_COMPOSITED_PLUGINS
