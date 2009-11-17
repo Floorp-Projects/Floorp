e38ba20db82acd74993f63385b8f22825f730d11
Dão Gottwald <dao@mozilla.com>
2009-11-17 13:07:34 +0100

    Count open browser windows for bug 528776

--- browser/components/sessionstore/test/browser/browser_461634.js
+++ browser/components/sessionstore/test/browser/browser_461634.js
@@ -35,8 +35,21 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+function browserWindowsCount() {
+  let count = 0;
+  let e = Cc["@mozilla.org/appshell/window-mediator;1"]
+            .getService(Ci.nsIWindowMediator)
+            .getEnumerator("navigator:browser");
+  while (e.hasMoreElements()) {
+    if (!e.getNext().closed)
+      ++count;
+  }
+  return count;
+}
+
 function test() {
   /** Test for Bug 461634 **/
+  is(browserWindowsCount(), 1, "Only one browser window should be open initially");
   
   // test setup
   let ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
@@ -65,9 +78,8 @@ function test() {
   }
   
   // open a window and add the above closed tab list
-  let newWin = openDialog(location, "_blank", "chrome,all,dialog=no");
+  let newWin = open(location, "", "chrome,all");
   newWin.addEventListener("load", function(aEvent) {
-    this.removeEventListener("load", arguments.callee, false);
     gPrefService.setIntPref("browser.sessionstore.max_tabs_undo",
                             test_state.windows[0]._closedTabs.length);
     ss.setWindowState(newWin, JSON.stringify(test_state), true);
@@ -103,6 +115,7 @@ function test() {
 
     // clean up
     newWin.close();
+    is(browserWindowsCount(), 1, "Only one browser window should be open eventually");
     gPrefService.clearUserPref("browser.sessionstore.max_tabs_undo");
     finish();
   }, false);
