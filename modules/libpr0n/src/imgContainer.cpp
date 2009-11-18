fca3e63feec5ff4f23c0133d6f53977a1e70d41f
Joe Drew <joe@drew.ca>
2009-11-18 18:20:27 -0500

    Bug 523528 - Handle compositing frame initialization failure properly. r=jrmuizel

--- modules/libpr0n/src/imgContainer.cpp
+++ modules/libpr0n/src/imgContainer.cpp
@@ -1583,7 +1583,10 @@ nsresult imgContainer::DoComposite(imgFrame** aFrameToUse,
     }
     nsresult rv = mAnim->compositingFrame->Init(0, 0, mSize.width, mSize.height,
                                                 gfxASurface::ImageFormatARGB32);
-    NS_ENSURE_SUCCESS(rv, rv);
+    if (NS_FAILED(rv)) {
+      mAnim->compositingFrame = nsnull;
+      return rv;
+    }
     needToBlankComposite = PR_TRUE;
   } else if (aNextFrameIndex == 1) {
     // When we are looping the compositing frame needs to be cleared.
@@ -1687,7 +1690,10 @@ nsresult imgContainer::DoComposite(imgFrame** aFrameToUse,
       }
       nsresult rv = mAnim->compositingPrevFrame->Init(0, 0, mSize.width, mSize.height,
                                                       gfxASurface::ImageFormatARGB32);
-      NS_ENSURE_SUCCESS(rv, rv);
+      if (NS_FAILED(rv)) {
+        mAnim->compositingPrevFrame = nsnull;
+        return rv;
+      }
     }
 
     CopyFrameImage(mAnim->compositingFrame, mAnim->compositingPrevFrame);
