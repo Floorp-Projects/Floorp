769905fee85875e7e63c29abc4d8599e11a42f86
Gavin Sharp <gavin@gavinsharp.com>
2009-11-19 14:40:54 -0500

    Bug 525184: put mobile bookmarks in a virtual "Mobile" bookmarks root, r=mfinkle

--- mobile/chrome/content/browser-ui.js
+++ mobile/chrome/content/browser-ui.js
@@ -712,7 +712,10 @@ var BrowserUI = {
         let autoClose = false;
 
         if (PlacesUtils.getMostRecentBookmarkForURI(bookmarkURI) == -1) {
-          var bookmarkId = PlacesUtils.bookmarks.insertBookmark(PlacesUtils.bookmarks.unfiledBookmarksFolder, bookmarkURI, PlacesUtils.bookmarks.DEFAULT_INDEX, bookmarkTitle);
+          let bmsvc = PlacesUtils.bookmarks;
+          let bookmarkId = bmsvc.insertBookmark(BookmarkList.mobileRoot, bookmarkURI,
+                                                bmsvc.DEFAULT_INDEX,
+                                                bookmarkTitle);
           this.updateStar();
 
           // autoclose the bookmark popup
@@ -920,6 +923,15 @@ var BookmarkList = {
   _panel: null,
   _bookmarks: null,
   _manageButtton: null,
+
+  get mobileRoot() {
+    let items = PlacesUtils.annotations.getItemsWithAnnotation("mobile/bookmarksRoot", {});
+    if (!items.length)
+      throw "Couldn't find mobile bookmarks root!";
+
+    delete this.mobileRoot;
+    return this.mobileRoot = items[0];
+  },
 
   show: function() {
     this._panel = document.getElementById("bookmarklist-container");
