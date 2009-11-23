cbb04fbf2f7599a3dc1a5340581e65aed183d317
Dão Gottwald <dao@mozilla.com>
2009-11-23 09:11:07 +0100

    Fix browser_NetworkPrioritizer.js for bug 525635

--- browser/base/content/test/browser_NetworkPrioritizer.js
+++ browser/base/content/test/browser_NetworkPrioritizer.js
@@ -34,6 +34,24 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+
+function waitForActivation(cb, win) {
+  if (!win)
+    win = window;
+
+  let fm = Cc["@mozilla.org/focus-manager;1"].getService(Ci.nsIFocusManager);
+  if (fm.activeWindow == win) {
+    cb();
+    return;
+  }
+
+  win.addEventListener("activate", function () {
+    win.removeEventListener("activate", arguments.callee, false);
+    executeSoon(cb);
+  }, false);
+  win.focus();
+}
+
 function test() {
   /** Tests for NetworkPrioritizer.jsm (Bug 514490) **/
 
@@ -104,21 +122,17 @@ function test() {
       window_B.addEventListener("load", function(aEvent) {
         window_B.removeEventListener("load", arguments.callee, false);
         window_B.gBrowser.addEventListener("load", function(aEvent) {
-          // waitForFocus can attach to the wrong "window" with about:blank loading first
-          // So just ensure that we're getting the load event for the right URI
-          if (window_B.gBrowser.currentURI.spec == "about:blank")
-            return;
           window_B.gBrowser.removeEventListener("load", arguments.callee, true);
 
-          waitForFocus(function() {
+          waitForActivation(function () {
             isWindowState(window_A, [10, 0]);
             isWindowState(window_B, [-10]);
 
-            waitForFocus(function() {
+            waitForActivation(function () {
               isWindowState(window_A, [0, -10]);
               isWindowState(window_B, [0]);
 
-              waitForFocus(function() {
+              waitForActivation(function () {
                 isWindowState(window_A, [10, 0]);
                 isWindowState(window_B, [-10]);
 
@@ -177,7 +191,7 @@ function test() {
   function runNextTest() {
     if (tests.length) {
       // Linux has problems if window isn't focused. Should help prevent [orange].
-      waitForFocus(tests.shift());
+      waitForActivation(tests.shift());
     } else {
       finish();
     }
