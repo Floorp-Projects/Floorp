e08988507fe563e35ce90159d9e58ef46139527e
Gavin Sharp <gavin@gavinsharp.com>
2009-11-23 13:46:25 -0500

    Bug 529952: fix error console spew on gmail due to bad assumption about nsIRequests all being nsIChannels, r=dietrich

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4326,9 +4326,8 @@ var TabsProgressListener = {
 
   onStateChange: function (aBrowser, aWebProgress, aRequest, aStateFlags, aStatus) {
 #ifdef MOZ_CRASHREPORTER
-    if (!aRequest.URI)
-      aRequest.QueryInterface(Ci.nsIChannel);
-    if (aStateFlags & Ci.nsIWebProgressListener.STATE_START &&
+    if (aRequest instanceof Ci.nsIChannel &&
+        aStateFlags & Ci.nsIWebProgressListener.STATE_START &&
         aStateFlags & Ci.nsIWebProgressListener.STATE_IS_DOCUMENT) {
       gCrashReporter.annotateCrashReport("URL", aRequest.URI.spec);
     }
