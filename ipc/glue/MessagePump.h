0bce8ed97511f0d490a3ab68200e4d5b9b61ffed
Ben Turner <bent.mozilla@gmail.com>
2009-11-23 16:01:12 -0500

    Fix PostDelayedTask in the message loop

--- ipc/glue/MessagePump.h
+++ ipc/glue/MessagePump.h
@@ -37,28 +37,52 @@
 #ifndef __IPC_GLUE_MESSAGEPUMP_H__
 #define __IPC_GLUE_MESSAGEPUMP_H__
 
+#include "base/basictypes.h"
 #include "base/message_pump_default.h"
+#include "base/time.h"
 
-#include "prtypes.h"
+#include "nsAutoPtr.h"
 #include "nsCOMPtr.h"
 
-class nsIRunnable;
-class nsIThread;
+#include "nsIRunnable.h"
+#include "nsIThread.h"
+#include "nsITimer.h"
 
 namespace mozilla {
 namespace ipc {
 
-class MessagePump : public base::MessagePumpDefault
+class MessagePump;
+
+class DoWorkRunnable : public nsIRunnable,
+                       public nsITimerCallback
 {
 public:
+  DoWorkRunnable(MessagePump* aPump)
+  : mPump(aPump) { }
+
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIRUNNABLE
+  NS_DECL_NSITIMERCALLBACK
+
+private:
+  MessagePump* mPump;
+};
+
+class MessagePump : public base::MessagePumpDefault
+{
+
+public:
   MessagePump();
-  ~MessagePump();
 
   virtual void Run(base::MessagePump::Delegate* aDelegate);
   virtual void ScheduleWork();
+  virtual void ScheduleDelayedWork(const base::Time& delayed_work_time);
+
+  void DoDelayedWork(base::MessagePump::Delegate* aDelegate);
 
 private:
-  nsIRunnable* mDummyEvent;
+  nsRefPtr<DoWorkRunnable> mDoWorkEvent;
+  nsCOMPtr<nsITimer> mDelayedWorkTimer;
 
   // Weak!
   nsIThread* mThread;
