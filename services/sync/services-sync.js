d17c549b3ca78186c0c76a6729837265e2509af5
Edward Lee <edilee@mozilla.com>
2009-11-24 14:53:59 -0800

    Bug 515593 - audit set of prefs we're syncing Add in prefs that are linked to the Firefox Preferences UI.

--- services/sync/services-sync.js
+++ services/sync/services-sync.js
@@ -45,28 +45,80 @@ pref("extensions.weave.network.numRetries", 2);
 pref("extensions.weave.tabs.sortMode", "recency");
 
 // Preferences to be synced by default
+pref("extensions.weave.prefs.sync.accessibility.blockautorefresh", true);
+pref("extensions.weave.prefs.sync.accessibility.browsewithcaret", true);
+pref("extensions.weave.prefs.sync.accessibility.typeaheadfind", true);
+pref("extensions.weave.prefs.sync.accessibility.typeaheadfind.linksonly", true);
+pref("extensions.weave.prefs.sync.app.update.mode", true);
+pref("extensions.weave.prefs.sync.browser.download.manager.closeWhenDone", true);
+pref("extensions.weave.prefs.sync.browser.download.manager.retention", true);
 pref("extensions.weave.prefs.sync.browser.download.manager.scanWhenDone", true);
-
+pref("extensions.weave.prefs.sync.browser.download.manager.showWhenStarting", true);
+pref("extensions.weave.prefs.sync.browser.formfill.enable", true);
+pref("extensions.weave.prefs.sync.browser.history_expire_days", true);
+pref("extensions.weave.prefs.sync.browser.link.open_newwindow", true);
+pref("extensions.weave.prefs.sync.browser.offline-apps.notify", true);
+pref("extensions.weave.prefs.sync.browser.safebrowsing.enabled", true);
+pref("extensions.weave.prefs.sync.browser.safebrowsing.malware.enabled", true);
 pref("extensions.weave.prefs.sync.browser.search.selectedEngine", true);
-
+pref("extensions.weave.prefs.sync.browser.search.update", true);
 pref("extensions.weave.prefs.sync.browser.startup.homepage", true);
-
-pref("extensions.weave.prefs.sync.browser.tabs.tabMinWidth", true);
-pref("extensions.weave.prefs.sync.browser.tabs.tabMaxWidth", true);
-pref("extensions.weave.prefs.sync.browser.tabs.warnOnClose", true);
+pref("extensions.weave.prefs.sync.browser.startup.page", true);
+pref("extensions.weave.prefs.sync.browser.tabs.autoHide", true);
 pref("extensions.weave.prefs.sync.browser.tabs.closeButtons", true);
-
+pref("extensions.weave.prefs.sync.browser.tabs.loadInBackground", true);
+pref("extensions.weave.prefs.sync.browser.tabs.tabMaxWidth", true);
+pref("extensions.weave.prefs.sync.browser.tabs.tabMinWidth", true);
+pref("extensions.weave.prefs.sync.browser.tabs.warnOnClose", true);
+pref("extensions.weave.prefs.sync.browser.tabs.warnOnOpen", true);
+pref("extensions.weave.prefs.sync.browser.urlbar.autocomplete.enabled", true);
 pref("extensions.weave.prefs.sync.browser.urlbar.autoFill", true);
+pref("extensions.weave.prefs.sync.browser.urlbar.default.behavior", true);
 pref("extensions.weave.prefs.sync.browser.urlbar.maxRichResults", true);
-
+pref("extensions.weave.prefs.sync.dom.disable_open_during_load", true);
+pref("extensions.weave.prefs.sync.dom.disable_window_flip", true);
+pref("extensions.weave.prefs.sync.dom.disable_window_move_resize", true);
+pref("extensions.weave.prefs.sync.dom.disable_window_open_feature.status", true);
+pref("extensions.weave.prefs.sync.dom.disable_window_status_change", true);
+pref("extensions.weave.prefs.sync.dom.event.contextmenu.enabled", true);
 pref("extensions.weave.prefs.sync.extensions.personas.current", true);
-
+pref("extensions.weave.prefs.sync.extensions.update.enabled", true);
+pref("extensions.weave.prefs.sync.general.autoScroll", true);
+pref("extensions.weave.prefs.sync.general.smoothScroll", true);
+pref("extensions.weave.prefs.sync.javascript.enabled", true);
 pref("extensions.weave.prefs.sync.layout.spellcheckDefault", true);
-pref("extensions.weave.prefs.sync.signon.rememberSignons", true);
-pref("extensions.weave.prefs.sync.spellchecker.dictionary", true);
-
-// Persona preferences for FF 3.6
 pref("extensions.weave.prefs.sync.lightweightThemes.isThemeSelected", true);
 pref("extensions.weave.prefs.sync.lightweightThemes.persisted.footerURL", true);
 pref("extensions.weave.prefs.sync.lightweightThemes.persisted.headerURL", true);
 pref("extensions.weave.prefs.sync.lightweightThemes.usedThemes", true);
+pref("extensions.weave.prefs.sync.network.cookie.cookieBehavior", true);
+pref("extensions.weave.prefs.sync.network.cookie.lifetimePolicy", true);
+pref("extensions.weave.prefs.sync.permissions.default.image", true);
+pref("extensions.weave.prefs.sync.pref.advanced.images.disable_button.view_image", true);
+pref("extensions.weave.prefs.sync.pref.advanced.javascript.disable_button.advanced", true);
+pref("extensions.weave.prefs.sync.pref.downloads.disable_button.edit_actions", true);
+pref("extensions.weave.prefs.sync.pref.privacy.disable_button.cookie_exceptions", true);
+pref("extensions.weave.prefs.sync.privacy.clearOnShutdown.cache", true);
+pref("extensions.weave.prefs.sync.privacy.clearOnShutdown.cookies", true);
+pref("extensions.weave.prefs.sync.privacy.clearOnShutdown.downloads", true);
+pref("extensions.weave.prefs.sync.privacy.clearOnShutdown.formdata", true);
+pref("extensions.weave.prefs.sync.privacy.clearOnShutdown.history", true);
+pref("extensions.weave.prefs.sync.privacy.clearOnShutdown.offlineApps", true);
+pref("extensions.weave.prefs.sync.privacy.clearOnShutdown.passwords", true);
+pref("extensions.weave.prefs.sync.privacy.clearOnShutdown.sessions", true);
+pref("extensions.weave.prefs.sync.privacy.clearOnShutdown.siteSettings", true);
+pref("extensions.weave.prefs.sync.privacy.sanitize.sanitizeOnShutdown", true);
+pref("extensions.weave.prefs.sync.security.OCSP.disable_button.managecrl", true);
+pref("extensions.weave.prefs.sync.security.OCSP.enabled", true);
+pref("extensions.weave.prefs.sync.security.OCSP.require", true);
+pref("extensions.weave.prefs.sync.security.default_personal_cert", true);
+pref("extensions.weave.prefs.sync.security.enable_java", true);
+pref("extensions.weave.prefs.sync.security.enable_ssl3", true);
+pref("extensions.weave.prefs.sync.security.enable_tls", true);
+pref("extensions.weave.prefs.sync.security.warn_entering_secure", true);
+pref("extensions.weave.prefs.sync.security.warn_entering_weak", true);
+pref("extensions.weave.prefs.sync.security.warn_leaving_secure", true);
+pref("extensions.weave.prefs.sync.security.warn_submit_insecure", true);
+pref("extensions.weave.prefs.sync.security.warn_viewing_mixed", true);
+pref("extensions.weave.prefs.sync.signon.rememberSignons", true);
+pref("extensions.weave.prefs.sync.spellchecker.dictionary", true);
