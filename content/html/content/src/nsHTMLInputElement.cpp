ca0d92aff02d7def27de29821d098beefa32bf51
Geoff Lankow <geoff@darktrojan.net>
2009-11-24 18:12:00 -0800

    Bug 500885 - DOMActivate is fired twice for input[type=file] if user clicks on browse button, r=Olli.Pettay sr=jonas

--- content/html/content/src/nsHTMLInputElement.cpp
+++ content/html/content/src/nsHTMLInputElement.cpp
@@ -1748,6 +1748,22 @@ nsHTMLInputElement::PostHandleEvent(nsEventChainPostVisitor& aVisitor)
   if (!aVisitor.mPresContext) {
     return NS_OK;
   }
+
+  // ignore the activate event fired by the "Browse..." button
+  // (file input controls fire their own) (bug 500885)
+  if (mType == NS_FORM_INPUT_FILE) {
+    nsCOMPtr<nsIContent> maybeButton =
+      do_QueryInterface(aVisitor.mEvent->originalTarget);
+    if (maybeButton &&
+      maybeButton->IsRootOfNativeAnonymousSubtree() &&
+      maybeButton->AttrValueIs(kNameSpaceID_None,
+                               nsGkAtoms::type,
+                               nsGkAtoms::button,
+                               eCaseMatters)) {
+        return NS_OK;
+    }
+  }
+
   nsresult rv = NS_OK;
   PRBool outerActivateEvent = !!(aVisitor.mItemFlags & NS_OUTER_ACTIVATE_EVENT);
   PRBool originalCheckedValue =
