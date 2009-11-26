c060bbabf9190f481bc4cc83d677b7f36511b10f
Doug Turner <dougt@dougt.org>
2009-11-25 18:38:22 -0800

    Bug 530739 - Intermittent Shutdown crash after visiting a page with flash video. r=jst

--- layout/generic/nsObjectFrame.cpp
+++ layout/generic/nsObjectFrame.cpp
@@ -2108,6 +2108,12 @@ GetMIMEType(nsIPluginInstance *aPluginInstance)
 static PRBool
 DoDelayedStop(nsPluginInstanceOwner *aInstanceOwner, PRBool aDelayedStop)
 {
+#ifdef MOZ_PLATFORM_HILDON
+  // Don't delay stop on Maemo/Hildon (bug 530739).
+  if (aDelayedStop && aInstanceOwner->MatchPluginName("Shockwave Flash"))
+    return PR_FALSE;
+#endif
+
   // Don't delay stopping QuickTime (bug 425157), Flip4Mac (bug 426524),
   // XStandard (bug 430219), CMISS Zinc (bug 429604).
   if (aDelayedStop
