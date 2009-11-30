84f2e8f2a5b9d8a5f1ddd7f758efc1ab6f2ab57e
Robert Sayre <sayrer@gmail.com>
2009-11-30 11:15:10 -0500

    Add a missing credit to trigger a re-compile.

--- js/src/json.cpp
+++ js/src/json.cpp
@@ -23,6 +23,7 @@
  *
  * Contributor(s):
  *   Robert Sayre <sayrer@gmail.com>
+ *   Dave Camp <dcamp@mozilla.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either of the GNU General Public License Version 2 or later (the "GPL"),
