0a30c91bc0f5bffd6c360aef0497707c35e8fc59
Edward Lee <edilee@mozilla.com>
2009-12-02 14:44:52 -0800

    Bug 530717 - sync after wipe local should always replace local values with remote Always take the incoming item after a wipe and otherwise do the normal reconcile.

--- services/sync/modules/engines/prefs.js
+++ services/sync/modules/engines/prefs.js
@@ -61,6 +61,20 @@ PrefsEngine.prototype = {
   _storeObj: PrefStore,
   _trackerObj: PrefTracker,
   _recordObj: PrefRec,
+
+  _wipeClient: function _wipeClient() {
+    SyncEngine.prototype._wipeClient.call(this);
+    this.justWiped = true;
+  },
+
+  _reconcile: function _reconcile(item) {
+    // Apply the incoming item if we don't care about the local data
+    if (this.justWiped) {
+      this.justWiped = false;
+      return true;
+    }
+    return SyncEngine.prototype._reconcile.call(this, item);
+  }
 };
 
 
