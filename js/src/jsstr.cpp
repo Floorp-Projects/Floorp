8b65f5443b89a42d0342fda50faf1f115c3efca7
Boris Zbarsky <bzbarsky@mit.edu>
2009-12-03 14:39:55 -0500

    Bug 530240.  Faster NormalizeThis for String objects.  r=brendan

--- js/src/jsstr.cpp
+++ js/src/jsstr.cpp
@@ -649,9 +649,21 @@ NormalizeThis(JSContext *cx, jsval *vp)
 
     if (JSVAL_IS_NULL(vp[1]) && JSVAL_IS_NULL(JS_THIS(cx, vp)))
         return NULL;
-    str = js_ValueToString(cx, vp[1]);
-    if (!str)
-        return NULL;
+
+    /*
+     * js_GetPrimitiveThis seems to do a bunch of work (like calls to
+     * JS_THIS_OBJECT) which we don't need in the common case (where
+     * vp[1] is a String object) here.
+     */
+    JS_ASSERT(!JSVAL_IS_PRIMITIVE(vp[1]));
+    JSObject *obj = JSVAL_TO_OBJECT(vp[1]);
+    if (obj->getClass() == &js_StringClass) {
+        str = JSVAL_TO_STRING(obj->fslots[JSSLOT_PRIMITIVE_THIS]);
+    } else {
+        str = js_ValueToString(cx, vp[1]);
+        if (!str)
+            return NULL;
+    }
     vp[1] = STRING_TO_JSVAL(str);
     return str;
 }
@@ -928,9 +940,7 @@ str_charAt(JSContext *cx, uintN argc, jsval *vp)
         if ((size_t)i >= str->length())
             goto out_of_range;
     } else {
-        str = NormalizeThis(cx, vp);
-        if (!str)
-            return JS_FALSE;
+        NORMALIZE_THIS(cx, vp, str);
 
         if (argc == 0) {
             d = 0.0;
@@ -972,9 +982,7 @@ str_charCodeAt(JSContext *cx, uintN argc, jsval *vp)
         if ((size_t)i >= str->length())
             goto out_of_range;
     } else {
-        str = NormalizeThis(cx, vp);
-        if (!str)
-            return JS_FALSE;
+        NORMALIZE_THIS(cx, vp, str);
 
         if (argc == 0) {
             d = 0.0;
