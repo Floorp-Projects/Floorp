a64fc3e1be9aaf94c7cfd481786c1946cdc5dc04
Boris Zbarsky <bzbarsky@mit.edu>
2009-12-03 23:03:46 -0500

    Bug 530255.  Replace GetFromClosure/GetClosureVar/GetClosureArg with hand-crafted LIR in the common easycase of a Call object whose frame is no longer active at record time, and make GetFromClosure use a direct read from the object's dslots if we discover that the frame became inactive between record and trace.  r=dmandelin,brendan

--- js/src/jsfun.h
+++ js/src/jsfun.h
@@ -207,6 +207,7 @@ struct JSFunction : public JSObject {
 extern JSClass js_ArgumentsClass;
 extern JS_FRIEND_DATA(JSClass) js_CallClass;
 extern JSClass js_DeclEnvClass;
+extern const uint32 CALL_CLASS_FIXED_RESERVED_SLOTS;
 
 /* JS_FRIEND_DATA so that VALUE_IS_FUNCTION is callable from the shell. */
 extern JS_FRIEND_DATA(JSClass) js_FunctionClass;
