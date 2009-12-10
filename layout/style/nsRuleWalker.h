1b3c665031c1c748c50fd05bf168f5151c8a2d14
Boris Zbarsky <bzbarsky@mit.edu>
2009-12-10 14:36:02 -0800

    Bug 523148.  Speed up AddImportantRules.  r=dbaron

--- layout/style/nsRuleWalker.h
+++ layout/style/nsRuleWalker.h
@@ -51,6 +51,8 @@ public:
   void Forward(nsIStyleRule* aRule) { 
     if (mCurrent) { // check for OOM from previous step
       mCurrent = mCurrent->Transition(aRule, mLevel, mImportance);
+      mCheckForImportantRules =
+        mCheckForImportantRules && !aRule->GetImportantRule();
     }
   }
 
@@ -58,18 +60,27 @@ public:
 
   PRBool AtRoot() { return mCurrent == mRoot; }
 
-  void SetLevel(PRUint8 aLevel, PRBool aImportance) {
+  void SetLevel(PRUint8 aLevel, PRBool aImportance,
+                PRBool aCheckForImportantRules) {
+    NS_ASSERTION(!aCheckForImportantRules || !aImportance,
+                 "Shouldn't be checking for important rules while walking "
+                 "important rules");
     mLevel = aLevel;
     mImportance = aImportance;
+    mCheckForImportantRules = aCheckForImportantRules;
   }
   PRUint8 GetLevel() const { return mLevel; }
   PRBool GetImportance() const { return mImportance; }
+  PRBool GetCheckForImportantRules() const { return mCheckForImportantRules; }
 
 private:
   nsRuleNode* mCurrent; // Our current position.
   nsRuleNode* mRoot; // The root of the tree we're walking.
   PRUint8 mLevel; // an nsStyleSet::sheetType
   PRPackedBool mImportance;
+  PRPackedBool mCheckForImportantRules; // If true, check for important rules as
+                                        // we walk and set to false if we find
+                                        // one.
 
 public:
   nsRuleWalker(nsRuleNode* aRoot) :mCurrent(aRoot), mRoot(aRoot) { MOZ_COUNT_CTOR(nsRuleWalker); }
