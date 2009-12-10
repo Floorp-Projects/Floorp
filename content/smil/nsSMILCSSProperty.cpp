c0378b188c6a6d066dd6409718c9a839ec694e7f
Daniel Holbert <dholbert@cs.stanford.edu>
2009-12-10 09:26:27 -0800

    Bug 528656: Support "font-family" property in SVG/SMIL. r=roc

--- content/smil/nsSMILCSSProperty.cpp
+++ content/smil/nsSMILCSSProperty.cpp
@@ -241,7 +241,7 @@ nsSMILCSSProperty::IsPropertyAnimatable(nsCSSProperty aPropID)
     // case eCSSProperty_filter:
     case eCSSProperty_flood_color:
     case eCSSProperty_flood_opacity:
-    // case eCSSProperty_font_family:
+    case eCSSProperty_font_family:
     case eCSSProperty_font_size:
     case eCSSProperty_font_size_adjust:
     case eCSSProperty_font_stretch:
