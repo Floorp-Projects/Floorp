fbb842ae9317b3c7c31597ce413254f28d195a83
Boris Zbarsky <bzbarsky@mit.edu>
2009-12-10 14:36:06 -0800

    Bug 525608 part 4.  Change pseudo-element probing and resolution to not use EnumerateTagRules.  r=dbaron

--- layout/style/nsHTMLStyleSheet.h
+++ layout/style/nsHTMLStyleSheet.h
@@ -79,6 +79,7 @@ public:
 
   // nsIStyleRuleProcessor API
   NS_IMETHOD RulesMatching(ElementRuleProcessorData* aData);
+  NS_IMETHOD RulesMatching(PseudoElementRuleProcessorData* aData);
   NS_IMETHOD RulesMatching(PseudoRuleProcessorData* aData);
   NS_IMETHOD HasStateDependentStyle(StateRuleProcessorData* aData,
                                     nsReStyleHint* aResult);
