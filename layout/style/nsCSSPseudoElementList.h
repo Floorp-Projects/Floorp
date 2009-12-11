a6ba222af7057c3b3ac8a3ff85813e6a772c65e1
Boris Zbarsky <bzbarsky@mit.edu>
2009-12-11 02:37:40 -0500

    Bug 525608 part 1.  Introduce an enum for keeping track of pseudo-elements.  r=dbaron

--- layout/style/nsCSSPseudoElementList.h
+++ layout/style/nsCSSPseudoElementList.h
@@ -71,11 +71,13 @@ CSS_PSEUDO_ELEMENT(firstLine, ":first-line",
 CSS_PSEUDO_ELEMENT(mozSelection, ":-moz-selection",
                    CSS_PSEUDO_ELEMENT_CONTAINS_ELEMENTS)
 
+// XXXbz should we really allow random content to style these?  Maybe
+// use our flags to prevent that?
 CSS_PSEUDO_ELEMENT(mozFocusInner, ":-moz-focus-inner", 0)
 CSS_PSEUDO_ELEMENT(mozFocusOuter, ":-moz-focus-outer", 0)
 
+// XXXbz should we really allow random content to style these?  Maybe
+// use our flags to prevent that?
 CSS_PSEUDO_ELEMENT(mozListBullet, ":-moz-list-bullet", 0)
 CSS_PSEUDO_ELEMENT(mozListNumber, ":-moz-list-number", 0)
 
-CSS_PSEUDO_ELEMENT(horizontalFramesetBorder, ":-moz-hframeset-border", 0)
-CSS_PSEUDO_ELEMENT(verticalFramesetBorder, ":-moz-vframeset-border", 0)
