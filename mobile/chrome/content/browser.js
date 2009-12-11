de985bc3c38a7541a45c52862fcaef0020719649
Benjamin Stover <webapps@stechz.com>
2009-12-11 10:39:59 -0800

    Bug 531897 - plugins in iframes do not work. r=dougt

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -3039,7 +3039,7 @@ PluginObserver.prototype = {
 
   /** Observe listens for plugin change events and maintains an embed cache. */
   observe: function observe(subject, topic, data) {
-    let doc = subject.ownerDocument;
+    let doc = subject.ownerDocument.defaultView.top.document;
     if (data == "init") {
       if (doc.pluginCache === undefined)
         doc.pluginCache = [];
