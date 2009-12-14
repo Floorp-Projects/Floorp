aba2a6854ae7f97eee9b7f5a5b17714fd030d7aa
Ted Mielczarek <ted.mielczarek@gmail.com>
2009-12-14 06:55:28 -0500

    bug 514188 - sync to breakpad revision 437 to pick up Linux client rewrite. r=bsmedberg

--- toolkit/crashreporter/google-breakpad/src/client/windows/handler/exception_handler.cc
+++ toolkit/crashreporter/google-breakpad/src/client/windows/handler/exception_handler.cc
@@ -249,12 +249,12 @@ ExceptionHandler::~ExceptionHandler() {
       // TODO(mmentovai): use advapi32!ReportEvent to log the warning to the
       // system's application event log.
       fprintf(stderr, "warning: removing Breakpad handler out of order\n");
-      for (vector<ExceptionHandler*>::iterator iterator =
-               handler_stack_->begin();
-           iterator != handler_stack_->end();
-           ++iterator) {
+      vector<ExceptionHandler*>::iterator iterator = handler_stack_->begin();
+      while (iterator != handler_stack_->end()) {
         if (*iterator == this) {
-          handler_stack_->erase(iterator);
+          iterator = handler_stack_->erase(iterator);
+        } else {
+          ++iterator;
         }
       }
     }
