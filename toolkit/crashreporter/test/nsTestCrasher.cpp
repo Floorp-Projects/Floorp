1bc4764e563c96bae2ac46148664e778d27bf60c
Ted Mielczarek <ted.mielczarek@gmail.com>
2009-12-14 06:44:27 -0500

    bug 514188 - fix nsProfileLock to use SA_SIGINFO style signal handler, so it can chain to Breakpad's signal handler properly. r=bsmedberg

--- toolkit/crashreporter/test/nsTestCrasher.cpp
+++ toolkit/crashreporter/test/nsTestCrasher.cpp
@@ -2,6 +2,7 @@
 #include "nsIComponentManager.h"
 #include "nsIGenericFactory.h"
 #include "nsITestCrasher.h"
+#include "nsXULAppAPI.h"
 
 class nsTestCrasher : public nsITestCrasher
 {
@@ -25,6 +26,13 @@ NS_IMETHODIMP nsTestCrasher::Crash()
   *foo = 0;
   // not reached
   return NS_OK;
+}
+
+/* nsISupports LockDir (in nsILocalFile directory); */
+NS_IMETHODIMP nsTestCrasher::LockDir(nsILocalFile *directory,
+                                     nsISupports **_retval NS_OUTPARAM)
+{
+  return XRE_LockProfileDirectory(directory, _retval);
 }
 
 // 54afce51-38d7-4df0-9750-2f90f9ffbca2
