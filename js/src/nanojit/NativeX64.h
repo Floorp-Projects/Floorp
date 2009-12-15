68cbf8df1b0bd9c2ddf51a821c5029345a216fa2
Nicholas Nethercote <nnethercote@mozilla.com>
2009-12-15 13:16:54 +1100

    Backed out changeset 2bd8a736808d, caused ARM bustage.

--- js/src/nanojit/NativeX64.h
+++ js/src/nanojit/NativeX64.h
@@ -329,6 +329,9 @@ namespace nanojit
     static const int NumArgRegs = 6;
 #endif
 
+    static inline bool isValidDisplacement(LOpcode, int32_t) {
+        return true;
+    }
     static inline bool IsFpReg(Register r) {
         return ((1<<r) & FpRegs) != 0;
     }
