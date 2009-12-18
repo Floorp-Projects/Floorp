e8ba2c61e8508c15e99806303bf13c9fd3afbae9
Chris Jones <jones.chris.g@gmail.com>
2009-12-17 18:12:01 -0600

    bug 535298: IPDL unit test for use-after-free crashes after RPC errors. r=test-only

---
+++ ipc/ipdl/test/cxx/TestRPCErrorCleanup.h
@@ -0,0 +1,55 @@
+#ifndef mozilla__ipdltest_TestRPCErrorCleanup_h
+#define mozilla__ipdltest_TestRPCErrorCleanup_h 1
+
+#include "mozilla/_ipdltest/IPDLUnitTests.h"
+
+#include "mozilla/_ipdltest/PTestRPCErrorCleanupParent.h"
+#include "mozilla/_ipdltest/PTestRPCErrorCleanupChild.h"
+
+namespace mozilla {
+namespace _ipdltest {
+
+
+class TestRPCErrorCleanupParent :
+    public PTestRPCErrorCleanupParent
+{
+public:
+    TestRPCErrorCleanupParent();
+    virtual ~TestRPCErrorCleanupParent();
+
+    void Main();
+
+protected:    
+    NS_OVERRIDE
+    virtual void ActorDestroy(ActorDestroyReason why)
+    {
+        if (AbnormalShutdown != why)
+            fail("unexpected destruction!");  
+    }
+};
+
+
+class TestRPCErrorCleanupChild :
+    public PTestRPCErrorCleanupChild
+{
+public:
+    TestRPCErrorCleanupChild();
+    virtual ~TestRPCErrorCleanupChild();
+
+protected:
+    NS_OVERRIDE
+    virtual bool AnswerError();
+
+    NS_OVERRIDE
+    virtual void ActorDestroy(ActorDestroyReason why)
+    {
+        fail("should have 'crashed'!");
+    }
+};
+
+
+} // namespace _ipdltest
+} // namespace mozilla
+
+
+#endif // ifndef mozilla__ipdltest_TestRPCErrorCleanup_h
