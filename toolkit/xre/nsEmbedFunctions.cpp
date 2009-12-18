cdfa70320ac62399c49b2179778b47d9a5352089
Benjamin Smedberg <benjamin@smedbergs.us>
2009-12-18 16:16:38 -0500

    Bug 535802 - disable the Windows JIT crash dialog when running mochitests, r=ted

--- toolkit/xre/nsEmbedFunctions.cpp
+++ toolkit/xre/nsEmbedFunctions.cpp
@@ -251,6 +251,8 @@ XRE_InitChildProcess(int aArgc,
   NS_ENSURE_ARG_POINTER(aArgv);
   NS_ENSURE_ARG_POINTER(aArgv[0]);
 
+  SetupErrorHandling();
+
   sChildProcessType = aProcess;
   
 #if defined(MOZ_WIDGET_GTK2)
