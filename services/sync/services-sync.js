9d7f3f628167fd2b8b3c1146bca8927aa3659388
Edward Lee <edilee@mozilla.com>
2009-12-17 18:40:29 -0800

    Bug 534459 - Can't reset password Update the preference to point to the new password reset page on auth.smc, and for now, just open it per mconnor's suggestion.

--- services/sync/services-sync.js
+++ services/sync/services-sync.js
@@ -2,7 +2,7 @@ pref("extensions.weave.serverURL", "@server_url@");
 pref("extensions.weave.storageAPI", "1.0");
 pref("extensions.weave.userURL", "user/");
 pref("extensions.weave.miscURL", "misc/");
-pref("extensions.weave.pwChangeURL", "https://services.mozilla.com/pw/forgot.php");
+pref("extensions.weave.pwChangeURL", "https://auth.services.mozilla.com/weave-password-reset");
 pref("extensions.weave.termsURL", "https://mozillalabs.com/weave/tos/");
 
 pref("extensions.weave.client.name", "Firefox");
