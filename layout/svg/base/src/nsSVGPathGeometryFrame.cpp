1b491e44adc6d0193eb412e8c949bb58c870e2d7
Robert Longson <longsonr@gmail.com>
2009-12-19 19:28:48 +0000

    backout Bug 510177 (reftest failure)

--- layout/svg/base/src/nsSVGPathGeometryFrame.cpp
+++ layout/svg/base/src/nsSVGPathGeometryFrame.cpp
@@ -270,16 +270,6 @@ nsSVGPathGeometryFrame::UpdateCoveredRegion()
 
   if (HasStroke()) {
     SetupCairoStrokeGeometry(&context);
-    if (extent.IsEmpty()) {
-      // If 'extent' is empty, its position will not be set. Although
-      // GetUserStrokeExtent gets the extents wrong we can still use it
-      // to get the device space position of zero length stroked paths.
-      extent = context.GetUserStrokeExtent();
-      extent.pos.x += extent.size.width / 2;
-      extent.pos.y += extent.size.height / 2;
-      extent.size.width = 0;
-      extent.size.height = 0;
-    }
     extent = nsSVGUtils::PathExtentsToMaxStrokeExtents(extent, this);
   } else if (GetStyleSVG()->mFill.mType == eStyleSVGPaintType_None) {
     extent = gfxRect(0, 0, 0, 0);
