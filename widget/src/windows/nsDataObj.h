b21309a8ddde95b3192bf5fd7fb069f0fed4a562
Phil Lacy <philbaseless-firefox@yahoo.com>
2009-12-20 11:03:18 -0600

    Bug 484667 - drag attachment to 'other' app doesn't work. r=roc.

--- widget/src/windows/nsDataObj.h
+++ widget/src/windows/nsDataObj.h
@@ -233,6 +233,7 @@ class nsDataObj : public IDataObject,
         
         virtual HRESULT DropImage( FORMATETC& aFE, STGMEDIUM& aSTG );
         virtual HRESULT DropFile( FORMATETC& aFE, STGMEDIUM& aSTG );
+        virtual HRESULT DropTempFile( FORMATETC& aFE, STGMEDIUM& aSTG );
 
     virtual HRESULT GetUniformResourceLocator ( FORMATETC& aFE, STGMEDIUM& aSTG, PRBool aIsUnicode ) ;
     virtual HRESULT ExtractUniformResourceLocatorA ( FORMATETC& aFE, STGMEDIUM& aSTG ) ;
