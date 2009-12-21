81a238e55f30c4d56ab2b1ebeb2851f97512086c
Steven Johnson <stejohns@adobe.com>
2009-12-21 12:03:59 -0800

    Increase NJ_MAX_STACK_ENTRY on desktop systems, revise search algo in AR (r=nnethercote,r=edwsmith,bug=473769)
    --HG--
    extra : convert_revision : 0a12bb3f8436feac1814bfdd25dde1afcd050c0a

--- js/src/nanojit/NativeX64.h
+++ js/src/nanojit/NativeX64.h
@@ -58,7 +58,7 @@
 
 namespace nanojit
 {
-#define NJ_MAX_STACK_ENTRY              256
+#define NJ_MAX_STACK_ENTRY              16384
 #define NJ_ALIGN_STACK                  16
 #define NJ_JTBL_SUPPORTED               1
 #define NJ_EXPANDED_LOADSTORE_SUPPORTED 1
