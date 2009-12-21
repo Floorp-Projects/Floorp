b5f483062962be6d06747360c85de561396c895b
Robert Longson <longsonr@gmail.com>
2009-12-21 22:10:34 +0000

    Bug 510177 - path with a single point not rendered. r=jwatt

--- layout/svg/base/src/nsSVGUtils.cpp
+++ layout/svg/base/src/nsSVGUtils.cpp
@@ -1486,10 +1486,6 @@ nsSVGUtils::WritePPM(const char *fname, gfxImageSurface *aSurface)
 nsSVGUtils::PathExtentsToMaxStrokeExtents(const gfxRect& aPathExtents,
                                           nsSVGGeometryFrame* aFrame)
 {
-  if (aPathExtents.Width() == 0 && aPathExtents.Height() == 0) {
-    return gfxRect(0, 0, 0, 0);
-  }
-
   // The logic here comes from _cairo_stroke_style_max_distance_from_path
 
   double style_expansion = 0.5;
