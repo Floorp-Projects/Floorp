259a398dae6e674e302f7ca717ae77bdfefa5bc9
Metal Sonic <kidlinux96@gmail.com>
2009-12-21 21:50:30 +0000

    Bug 507387 - fix copyright holder; Mozilla Corporation -> Mozilla Foundation. r=gerv.

--- xpcom/glue/nsTPtrArray.h
+++ xpcom/glue/nsTPtrArray.h
@@ -15,7 +15,7 @@
  *
  * The Original Code is C++ pointer array template.
  *
- * The Initial Developer of the Original Code is Mozilla Corporation.
+ * The Initial Developer of the Original Code is Mozilla Foundation.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
  *
