d84794e3afef09e9fd6ef0c3846e73e589684edd
Benjamin Smedberg <benjamin@smedbergs.us>
2009-12-21 09:55:45 -0500

    Backed out changeset 94561cb0f0bd, bug 474500 because of static-analysis bustage.

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -2604,12 +2604,6 @@ JS_GetGCParameterForThread(JSContext *cx, JSGCParamKey key)
 #endif
 }
 
-JS_PUBLIC_API(void)
-JS_FlushCaches(JSContext *cx)
-{
-    js_FlushJITCache(cx);
-}
-
 JS_PUBLIC_API(intN)
 JS_AddExternalStringFinalizer(JSStringFinalizeOp finalizer)
 {
