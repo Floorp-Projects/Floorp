27ec8deb0fe1f9b2a31e3d39555b25f2d52e1f56
Blake Kaplan <mrbkap@gmail.com>
2009-12-23 14:02:02 -0800

    Bug 536562 - Don't crash on null. r=dbaron
    --HG--
    extra : rebase_source : a8fb13c8024deb4c09bd592b91632cbee9bbeeda

--- js/src/xpconnect/src/XPCCrossOriginWrapper.cpp
+++ js/src/xpconnect/src/XPCCrossOriginWrapper.cpp
@@ -558,7 +558,7 @@ XPC_XOW_AddProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
     return JS_FALSE;
   }
 
-  if (JSVAL_IS_OBJECT(*vp)) {
+  if (!JSVAL_IS_PRIMITIVE(*vp)) {
     JSObject *addedObj = JSVAL_TO_OBJECT(*vp);
     if (STOBJ_GET_CLASS(addedObj) == &XOWClass.base &&
         STOBJ_GET_PARENT(addedObj) != STOBJ_GET_PARENT(obj)) {
