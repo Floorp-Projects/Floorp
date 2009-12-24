d07ca499d7fcf4bb3eb2780be58f8578e1978d64
Jeff Walden <jwalden@mit.edu>
2009-12-24 16:10:18 -0500

    Bug 534377 - Object literal duplicate-property checking doesn't work for same property as number and as string.  r=jimb

--- js/src/tests/ecma_5/strict/11.1.5.js
+++ js/src/tests/ecma_5/strict/11.1.5.js
@@ -58,6 +58,26 @@ assertEq(testLenientAndStrict('({6.02214179e23:1, 3.1415926535:1})',
                               parsesSuccessfully),
          true);
 
+assertEq(testLenientAndStrict('({ 1: 1, "1": 2 })',
+                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError)),
+         true);
+
+assertEq(testLenientAndStrict('({ "1": 1, 1: 2 })',
+                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError)),
+         true);
+
+assertEq(testLenientAndStrict('({ 2.5: 1, "2.5": 2 })',
+                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError)),
+         true);
+
+assertEq(testLenientAndStrict('({ "2.5": 1, 2.5: 2 })',
+                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError)),
+         true);
+
 /* Many properties, to exercise JSAtomList's hash-table variant. */
 assertEq(testLenientAndStrict('({a:1, b:1, c:1, d:1, e:1, f:1, g:1, h:1, i:1, j:1, k:1, l:1, m:1, n:1, o:1, p:1, q:1, r:1, s:1, t:1, u:1, v:1, w:1, x:1, y:1, z:1})',
                               parsesSuccessfully,
