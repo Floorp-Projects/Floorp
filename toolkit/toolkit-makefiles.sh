02f1ea70dcfcd4751719783669941c87f8e14969
Daniel Holbert <dholbert@cs.stanford.edu>
2010-01-05 21:42:34 -0800

    Bug 538090: Fix incorrect path to google-breakpad's "dwarf/Makefile" in toolkit-makefiles.sh. blanket-r=ted

--- toolkit/toolkit-makefiles.sh
+++ toolkit/toolkit-makefiles.sh
@@ -677,7 +677,7 @@ MAKEFILES_xulapp="
   toolkit/crashreporter/google-breakpad/src/common/Makefile
   toolkit/crashreporter/google-breakpad/src/common/linux/Makefile
   toolkit/crashreporter/google-breakpad/src/common/mac/Makefile
-  toolkit/crashreporter/google-breakpad/src/common/mac/dwarf/Makefile
+  toolkit/crashreporter/google-breakpad/src/common/dwarf/Makefile
   toolkit/crashreporter/google-breakpad/src/common/solaris/Makefile
   toolkit/crashreporter/google-breakpad/src/common/windows/Makefile
   toolkit/crashreporter/google-breakpad/src/tools/linux/dump_syms/Makefile
