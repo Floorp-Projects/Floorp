db08f1beb968e9117cdb7be983b71118e3347b2c
Stuart Parmenter <stuart@mozilla.com>
2010-01-06 10:38:02 -0800

    Bug 538174 - N900: App Manager only picks up xulrunner as an update option going from 12/5 nightly to rc2 [r=blassey a=stuart]

--- mobile/branding/nightly/configure.sh
+++ mobile/branding/nightly/configure.sh
@@ -1,2 +1,11 @@
 MOZ_APP_DISPLAYNAME=Fennec
 MOZ_APP_UA_EXTRA=Namoroka
+
+case "$target" in
+*-wince*)
+    MOZ_APP_VERSION=1.0a4pre
+    ;;
+*)
+    MOZ_APP_VERSION=1.1a1pre
+    ;;
+esac
--- mobile/branding/official/configure.sh
+++ mobile/branding/official/configure.sh
@@ -1,2 +1,11 @@
 MOZ_APP_DISPLAYNAME=Firefox
 MOZ_APP_UA_EXTRA=Firefox
+
+case "$target" in
+*-wince*)
+    MOZ_APP_VERSION=1.0a4pre
+    ;;
+*)
+    MOZ_APP_VERSION=1.1a1
+    ;;
+esac
