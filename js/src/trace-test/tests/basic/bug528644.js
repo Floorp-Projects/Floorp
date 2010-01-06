43a35e57bc619c1625f8712862f326ebf6474150
David Mandelin <dmandelin@mozilla.com>
2010-01-06 14:33:52 -0800

    Bug 528644: do not generate JSOP_GETUPVAR in a generator called from eval, r=mrbkap

---
+++ js/src/trace-test/tests/basic/bug528644.js
@@ -0,0 +1,16 @@
+// Don't crash
+
+function g(foo) {
+  for (a in foo) {
+  }
+}
+
+var makegen = eval("\n\
+  function(b) {\n\
+      var h = \n\
+        eval(\"new function() { yield print(b) }\" ); \n\
+    return h\n\
+  }\n\
+");
+
+g(makegen());
