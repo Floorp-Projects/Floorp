2d2e5792266b9fd0ed7eb99038a287868b3d4faa
Gavin Sharp <gavin@gavinsharp.com>
2010-01-06 22:38:40 -0500

    Bug 538174 followup, don't have version depend on branding, r=mfinkle
    --HG--
    extra : transplant_source : %F7%89%F4%0C8%0F%BC%83%AD%B0Qf%17%3B%01%16%A2%98%BAT

--- mobile/confvars.sh
+++ mobile/confvars.sh
@@ -40,7 +40,15 @@ MOZ_BRANDING_DIRECTORY=mobile/branding/nightly
 MOZ_OFFICIAL_BRANDING_DIRECTORY=mobile/branding/official
 
 # MOZ_APP_DISPLAYNAME is set by branding/configure.sh
-# MOZ_APP_VERSION is set by branding/configure.sh
+
+case "$target" in
+*-wince*)
+    MOZ_APP_VERSION=1.0a4pre
+    ;;
+*)
+    MOZ_APP_VERSION=1.1a1pre
+    ;;
+esac
 
 MOZ_XUL_APP=1
 MOZ_UPDATER=0
