d283228231f373e22d05b690e890c33218d17c45
Robert O'Callahan <robert@ocallahan.org>
2010-01-12 10:45:20 +1300

    Bug 526394. Part 34: Create nsContentUtils::AddScriptBlockerAndPreventAddingRunners. r=sicking

--- content/base/public/nsContentUtils.h
+++ content/base/public/nsContentUtils.h
@@ -1367,6 +1367,13 @@ public:
   static void AddScriptBlocker();
 
   /**
+   * Increases the count of blockers preventing scripts from running.
+   * Also, while this script blocker is active, script runners must not be
+   * added --- we'll assert if one is, and ignore it.
+   */
+  static void AddScriptBlockerAndPreventAddingRunners();
+
+  /**
    * Decreases the count of blockers preventing scripts from running.
    * NOTE: You might want to use nsAutoScriptBlocker rather than calling
    * this directly
@@ -1590,6 +1597,7 @@ private:
   static PRUint32 sRemovableScriptBlockerCount;
   static nsCOMArray<nsIRunnable>* sBlockedScriptRunners;
   static PRUint32 sRunnersCountAtFirstBlocker;
+  static PRUint32 sScriptBlockerCountWhereRunnersPrevented;
 
   static nsIInterfaceRequestor* sSameOriginChecker;
 };
