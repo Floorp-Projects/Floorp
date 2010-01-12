c8f6f13d2111ce973d43e1c70ba131d5cf140f8e
Brian Birtles <birtles@gmail.com>
2010-01-12 12:00:49 -0800

    Bug 474743 Patch D: Implement syncbase logic. r=dholbert sr=roc

--- content/smil/nsSMILInterval.h
+++ content/smil/nsSMILInterval.h
@@ -38,23 +38,98 @@
 #ifndef NS_SMILINTERVAL_H_
 #define NS_SMILINTERVAL_H_
 
-#include "nsSMILTimeValue.h"
+#include "nsSMILInstanceTime.h"
 
 //----------------------------------------------------------------------
 // nsSMILInterval class
 //
-// This class is essentially a structure consisting of a begin and end time. It
-// is used for representing the current interval and also for storing past
-// intervals for the purpose of hyperlinking back in time.
+// A structure consisting of a begin and end time. The begin time must be
+// resolved (i.e. not indefinite or unresolved).
 //
 // For an overview of how this class is related to other SMIL time classes see
-// the documentstation in nsSMILTimeValue.h
+// the documentation in nsSMILTimeValue.h
 
 class nsSMILInterval
 {
 public:
-  nsSMILTimeValue mBegin;
-  nsSMILTimeValue mEnd;
+  void Set(nsSMILInstanceTime& aBegin, nsSMILInstanceTime& aEnd)
+  {
+    NS_ABORT_IF_FALSE(aBegin.Time().IsResolved(),
+        "Attempting to set unresolved begin time on an interval");
+    mBegin = &aBegin;
+    mEnd = &aEnd;
+  }
+
+  PRBool IsSet() const
+  {
+    NS_ABORT_IF_FALSE(!mBegin == !mEnd, "Bad interval: only one endpoint set");
+    return !!mBegin;
+  }
+
+  void Reset()
+  {
+    mBegin = nsnull;
+    mEnd = nsnull;
+  }
+
+  // Begin() and End() will be non-null so long as IsSet() is true. Otherwise,
+  // they probably shouldn't be called.
+
+  const nsSMILInstanceTime* Begin() const
+  {
+    NS_ABORT_IF_FALSE(mBegin, "Calling Begin() on un-set interval");
+    return mBegin;
+  }
+
+  nsSMILInstanceTime* Begin()
+  {
+    NS_ABORT_IF_FALSE(mBegin, "Calling Begin() on un-set interval");
+    return mBegin;
+  }
+
+  const nsSMILInstanceTime* End() const
+  {
+    NS_ABORT_IF_FALSE(mEnd, "Calling End() on un-set interval");
+    return mEnd;
+  }
+
+  nsSMILInstanceTime* End()
+  {
+    NS_ABORT_IF_FALSE(mEnd, "Calling End() on un-set interval");
+    return mEnd;
+  }
+
+  void SetBegin(nsSMILInstanceTime& aBegin)
+  {
+    NS_ABORT_IF_FALSE(mBegin, "Calling SetBegin() on un-set interval");
+    NS_ABORT_IF_FALSE(aBegin.Time().IsResolved(),
+        "Attempting to set unresolved begin time on interval");
+    mBegin = &aBegin;
+  }
+
+  void SetEnd(nsSMILInstanceTime& aEnd)
+  {
+    NS_ABORT_IF_FALSE(mEnd, "Calling SetEnd() on un-set interval");
+    mEnd = &aEnd;
+  }
+
+  void FreezeBegin()
+  {
+    NS_ABORT_IF_FALSE(mBegin, "Calling FreezeBegin() on un-set interval");
+    mBegin->MarkNoLongerUpdating();
+  }
+
+  void FreezeEnd()
+  {
+    NS_ABORT_IF_FALSE(mEnd, "Calling FreezeEnd() on un-set interval");
+    NS_ABORT_IF_FALSE(!mBegin->MayUpdate(),
+        "Freezing the end of an interval without a fixed begin");
+    mEnd->MarkNoLongerUpdating();
+  }
+
+private:
+  nsRefPtr<nsSMILInstanceTime> mBegin;
+  nsRefPtr<nsSMILInstanceTime> mEnd;
 };
 
 #endif // NS_SMILINTERVAL_H_
