65721e4fc888b1fc6fcc7c86045007a72191f249
Chris Jones <jones.chris.g@gmail.com>
2010-01-12 15:14:38 -0600

    Bug 516759: Use OOP crash reporting in Gecko. r=luser

--- ipc/glue/GeckoChildProcessHost.cpp
+++ ipc/glue/GeckoChildProcessHost.cpp
@@ -46,6 +46,11 @@
 
 #include "prprf.h"
 
+#if defined(OS_LINUX)
+#  define XP_LINUX 1
+#endif
+#include "nsExceptionHandler.h"
+
 #include "mozilla/ipc/GeckoThread.h"
 
 using mozilla::MonitorAutoEnter;
@@ -179,7 +184,7 @@ GeckoChildProcessHost::PerformAsyncLaunch(std::vector<std::string> aExtraOpts)
   int srcChannelFd, dstChannelFd;
   channel().GetClientFileDescriptorMapping(&srcChannelFd, &dstChannelFd);
   mFileMap.push_back(std::pair<int,int>(srcChannelFd, dstChannelFd));
-  
+
   // no need for kProcessChannelID, the child process inherits the
   // other end of the socketpair() from us
 
@@ -191,6 +196,22 @@ GeckoChildProcessHost::PerformAsyncLaunch(std::vector<std::string> aExtraOpts)
 
   childArgv.push_back(pidstring);
   childArgv.push_back(childProcessType);
+
+#if defined(MOZ_CRASHREPORTER)
+  int childCrashFd, childCrashRemapFd;
+  if (!CrashReporter::CreateNotificationPipeForChild(
+        &childCrashFd, &childCrashRemapFd))
+    return false;
+  if (0 <= childCrashFd) {
+    mFileMap.push_back(std::pair<int,int>(childCrashFd, childCrashRemapFd));
+    // "true" == crash reporting enabled
+    childArgv.push_back("true");
+  }
+  else {
+    // "false" == crash reporting disabled
+    childArgv.push_back("false");
+  }
+#endif
 
   base::LaunchApp(childArgv, mFileMap, false, &process);
 
@@ -214,6 +235,10 @@ GeckoChildProcessHost::PerformAsyncLaunch(std::vector<std::string> aExtraOpts)
 
   cmdLine.AppendLooseValue(UTF8ToWide(pidstring));
   cmdLine.AppendLooseValue(UTF8ToWide(childProcessType));
+#if defined(MOZ_CRASHREPORTER)
+  cmdLine.AppendLooseValue(
+    UTF8ToWide(CrashReporter::GetChildNotificationPipe().BeginReading()));
+#endif
 
   base::LaunchApp(cmdLine, false, false, &process);
 
