67cbd716f0ec0dbaf231b5a248117d0c74472e6a
Markus Stange <mstange@themasta.com>
2010-01-13 17:08:06 +0100

    Bug 461291 - Reset the titlebar gradient when the unified toolbar is hidden. r=josh

--- widget/src/cocoa/nsCocoaWindow.h
+++ widget/src/cocoa/nsCocoaWindow.h
@@ -172,12 +172,15 @@ struct UnifiedGradientInfo {
 {
   TitlebarAndBackgroundColor *mColor;
   float mUnifiedToolbarHeight;
+  BOOL mWaitingForUnifiedToolbarHeight;
   NSColor *mBackgroundColor;
 }
 // Pass nil here to get the default appearance.
 - (void)setTitlebarColor:(NSColor*)aColor forActiveWindow:(BOOL)aActive;
 - (void)setUnifiedToolbarHeight:(float)aToolbarHeight;
 - (float)unifiedToolbarHeight;
+- (void)beginMaybeResetUnifiedToolbar;
+- (void)endMaybeResetUnifiedToolbar;
 - (float)titlebarHeight;
 - (NSRect)titlebarRect;
 - (void)setTitlebarNeedsDisplayInRect:(NSRect)aRect sync:(BOOL)aSync;
