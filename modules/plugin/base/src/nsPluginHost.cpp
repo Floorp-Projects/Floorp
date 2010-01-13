9a44b72106c746854638896e5d9a05b7ce761a9b
Josh Aas <joshmoz@gmail.com>
2010-01-13 11:42:41 -0500

    Sent "PluginCrashed" notification when a plugin crashes. b=519541 r=jst r=bsmedberg

--- modules/plugin/base/src/nsPluginHost.cpp
+++ modules/plugin/base/src/nsPluginHost.cpp
@@ -88,6 +88,7 @@
 #include "nsIBlocklistService.h"
 #include "nsVersionComparator.h"
 #include "nsIPrivateBrowsingService.h"
+#include "nsIObjectLoadingContent.h"
 
 #include "nsEnumeratorUtils.h"
 #include "nsXPCOM.h"
@@ -5177,6 +5178,14 @@ nsPluginHost::PluginCrashed(nsNPAPIPlugin* aPlugin)
   while (*pinstancetag) {
     nsPluginInstanceTag* instancetag = *pinstancetag;
     if (instancetag->mPluginTag == plugin) {
+      // notify the content node (nsIObjectLoadingContent) that the plugin has crashed
+      nsCOMPtr<nsIDOMElement> domElement;
+      instancetag->mInstance->GetDOMElement(getter_AddRefs(domElement));
+      nsCOMPtr<nsIObjectLoadingContent> objectContent(do_QueryInterface(domElement));
+      if (objectContent) {
+        objectContent->PluginCrashed();
+      }
+
       *pinstancetag = (*pinstancetag)->mNext;
       delete instancetag;
     }
