8359646d642a086ea58b504c007e56adfbf5f94d
Chris Jones <jones.chris.g@gmail.com>
2010-01-14 17:38:00 -0500

    bug 539552 - keep track of child process minidumps, offer pid based lookup API, write out .extra data in child minidump callback. r=ted,bsmedberg

--- toolkit/crashreporter/content/oopcrashdialog.js
+++ toolkit/crashreporter/content/oopcrashdialog.js
@@ -3,22 +3,6 @@
 
 var id;
 
-function getExtraData() {
-  let appData = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime);
-  appData.QueryInterface(Ci.nsICrashReporter);
-  appData.QueryInterface(Ci.nsIXULAppInfo);
-
-  let r = "";
-  r += "ServerURL=" + appData.serverURL.spec + "\n";
-  r += "BuildID=" + appData.appBuildID + "\n";
-  r += "ProductName=" + appData.name + "\n";
-  r += "Vendor=" + appData.vendor + "\n";
-  r += "Version=" + appData.version + "\n";
-  r += "CrashTime=" + ((new Date()).getTime() / 1000).toFixed() + "\n";
-  r += "ProcessType=plugin\n";
-  return r;
-}
-
 function collectData() {
   // HACK: crashes.js uses document.body, so we just alias it
   document.body = document.getElementById('iframe-holder');
@@ -40,27 +24,11 @@ function collectData() {
       reportsDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0770);
 
   let dumpFile = window.arguments[0].QueryInterface(Ci.nsIFile);
+  let extraFile = dumpFile.clone();
+  id = dumpFile.leafName.replace(/.dmp$/, "");
+  extraFile.leafName = id + ".extra";
   dumpFile.moveTo(pendingDir, "");
-  let leafName = dumpFile.leafName;
-
-  id = /([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})\.dmp/(leafName)[1];
-
-  dumpFile = pendingDir.clone();
-  dumpFile.append(leafName);
-
-  let extraFile = pendingDir.clone();
-  extraFile.append(id + ".extra");
-
-  let fstream = Cc["@mozilla.org/network/file-output-stream;1"].
-    createInstance(Ci.nsIFileOutputStream);
-  fstream.init(extraFile, -1, -1, 0);
-
-  var os = Cc["@mozilla.org/intl/converter-output-stream;1"].
-    createInstance(Ci.nsIConverterOutputStream);
-  os.init(fstream, "UTF-8", 0, 0x0000);
-  os.writeString(getExtraData());
-  os.close();
-  fstream.close();
+  extraFile.moveTo(pendingDir, "");
 }
 
 function onSubmit()
