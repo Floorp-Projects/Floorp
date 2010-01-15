c5dcdd95e5204acbcd8d2d7256ae6a8899845833
Marco Bonardo <mbonardo@mozilla.com>
2010-01-15 17:40:47 +0100

    Bug 520165 - Part13: New expiration tests, r=mano

---
+++ toolkit/components/places/tests/expiration/test_annos_expire_never.js
@@ -0,0 +1,139 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: sw=2 ts=2 et lcs=trail\:.,tab\:>~ :
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Places Unit Tests.
+ *
+ * The Initial Developer of the Original Code is
+ * Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Marco Bonardo <mak77@bonardo.net> (Original Author)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+/**
+ * What this is aimed to test:
+ *
+ * EXPIRE_NEVER annotations should be expired when a page is removed from the
+ * database.
+ * If the annotation is a page annotation this will happen when the page is
+ * expired, namely when the page has no visits and is not bookmarked.
+ * Otherwise if it's an item annotation the annotation will be expired when
+ * the item is removed, thus expiration won't handle this case at all.
+ */
+
+const TOPIC_EXPIRATION_FINISHED = "places-expiration-finished";
+
+let os = Cc["@mozilla.org/observer-service;1"].
+         getService(Ci.nsIObserverService);
+let hs = Cc["@mozilla.org/browser/nav-history-service;1"].
+         getService(Ci.nsINavHistoryService);
+let bs = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
+         getService(Ci.nsINavBookmarksService);
+let as = Cc["@mozilla.org/browser/annotation-service;1"].
+         getService(Ci.nsIAnnotationService);
+
+function run_test() {
+  // Set interval to a large value so we don't expire on it.
+  setInterval(3600); // 1h
+
+  // Expire all expirable pages.
+  setMaxPages(0);
+
+  // Add some visited page and a couple expire never annotations for each.
+  let now = Date.now() * 1000;
+  for (let i = 0; i < 5; i++) {
+    let pageURI = uri("http://page_anno." + i + ".mozilla.org/");
+    hs.addVisit(pageURI, now++, null, hs.TRANSITION_TYPED, false, 0);
+    as.setPageAnnotation(pageURI, "page_expire1", "test", 0, as.EXPIRE_NEVER);
+    as.setPageAnnotation(pageURI, "page_expire2", "test", 0, as.EXPIRE_NEVER);
+  }
+
+  let pages = as.getPagesWithAnnotation("page_expire1");
+  do_check_eq(pages.length, 5);
+  pages = as.getPagesWithAnnotation("page_expire2");
+  do_check_eq(pages.length, 5);
+
+  // Add some bookmarked page and a couple expire never annotations for each.
+  for (let i = 0; i < 5; i++) {
+    let pageURI = uri("http://item_anno." + i + ".mozilla.org/");
+    // We also add a visit before bookmarking.
+    hs.addVisit(pageURI, now++, null, hs.TRANSITION_TYPED, false, 0);
+    let id = bs.insertBookmark(bs.unfiledBookmarksFolder, pageURI,
+                               bs.DEFAULT_INDEX, null);
+    as.setItemAnnotation(id, "item_persist1", "test", 0, as.EXPIRE_NEVER);
+    as.setItemAnnotation(id, "item_persist2", "test", 0, as.EXPIRE_NEVER);
+  }
+
+  let items = as.getItemsWithAnnotation("item_persist1");
+  do_check_eq(items.length, 5);
+  items = as.getItemsWithAnnotation("item_persist2");
+  do_check_eq(items.length, 5);
+
+  // Add other visited page and a couple expire never annotations for each.
+  // We won't expire these visits, so the annotations should survive.
+  for (let i = 0; i < 5; i++) {
+    let pageURI = uri("http://persist_page_anno." + i + ".mozilla.org/");
+    hs.addVisit(pageURI, now++, null, hs.TRANSITION_TYPED, false, 0);
+    as.setPageAnnotation(pageURI, "page_persist1", "test", 0, as.EXPIRE_NEVER);
+    as.setPageAnnotation(pageURI, "page_persist2", "test", 0, as.EXPIRE_NEVER);
+  }
+
+  pages = as.getPagesWithAnnotation("page_persist1");
+  do_check_eq(pages.length, 5);
+  pages = as.getPagesWithAnnotation("page_persist2");
+  do_check_eq(pages.length, 5);
+
+  // Observe expirations.
+  observer = {
+    observe: function(aSubject, aTopic, aData) {
+      os.removeObserver(observer, TOPIC_EXPIRATION_FINISHED);
+
+      let pages = as.getPagesWithAnnotation("page_expire1");
+      do_check_eq(pages.length, 0);
+      pages = as.getPagesWithAnnotation("page_expire2");
+      do_check_eq(pages.length, 0);
+      let items = as.getItemsWithAnnotation("item_persist1");
+      do_check_eq(items.length, 5);
+      items = as.getItemsWithAnnotation("item_persist2");
+      do_check_eq(items.length, 5);
+      pages = as.getPagesWithAnnotation("page_persist1");
+      do_check_eq(pages.length, 5);
+      pages = as.getPagesWithAnnotation("page_persist2");
+      do_check_eq(pages.length, 5);
+
+      do_test_finished();
+    }
+  };
+  os.addObserver(observer, TOPIC_EXPIRATION_FINISHED, false);
+
+  // Expire all visits for the first 5 pages and the bookmarks.
+  force_expiration_step(10);
+  do_test_pending();
+}
---
+++ toolkit/components/places/tests/expiration/test_notifications_onDeleteURI.js
@@ -0,0 +1,168 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: sw=2 ts=2 et lcs=trail\:.,tab\:>~ :
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Places Unit Tests.
+ *
+ * The Initial Developer of the Original Code is
+ * Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Marco Bonardo <mak77@bonardo.net> (Original Author)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+/**
+ * What this is aimed to test:
+ *
+ * Expiring a full page should fire an onDeleteURI notification.
+ */
+
+const TOPIC_EXPIRATION_FINISHED = "places-expiration-finished";
+
+let os = Cc["@mozilla.org/observer-service;1"].
+         getService(Ci.nsIObserverService);
+let hs = Cc["@mozilla.org/browser/nav-history-service;1"].
+         getService(Ci.nsINavHistoryService);
+let bs = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
+         getService(Ci.nsINavBookmarksService);
+
+let gTests = [
+
+  { desc: "Add 1 bookmarked page.",
+    addPages: 1,
+    addBookmarks: 1,
+    expectedNotifications: 0, // No expirable pages.
+  },
+
+  { desc: "Add 2 pages, 1 bookmarked.",
+    addPages: 2,
+    addBookmarks: 1,
+    expectedNotifications: 1, // Only one expirable page.
+  },
+
+  { desc: "Add 10 pages, none bookmarked.",
+    addPages: 10,
+    addBookmarks: 0,
+    expectedNotifications: 10, // Will expire everything.
+  },
+
+  { desc: "Add 10 pages, all bookmarked.",
+    addPages: 10,
+    addBookmarks: 10,
+    expectedNotifications: 0, // No expirable pages.
+  },
+
+];
+
+let gCurrentTest;
+let gTestIndex = 0;
+
+function run_test() {
+  // Set interval to a large value so we don't expire on it.
+  setInterval(3600); // 1h
+
+  // Expire anything that is expirable.
+  setMaxPages(0);
+
+  do_test_pending();
+  run_next_test();
+}
+
+function run_next_test() {
+  if (gTests.length) {
+    gCurrentTest = gTests.shift();
+    gTestIndex++;
+    print("\nTEST " + gTestIndex + ": " + gCurrentTest.desc);
+    gCurrentTest.receivedNotifications = 0;
+
+    // Setup visits.
+    let now = Date.now() * 1000;
+    for (let i = 0; i < gCurrentTest.addPages; i++) {
+      let page = "http://" + gTestIndex + "." + i + ".mozilla.org/";
+      hs.addVisit(uri(page), now++, null, hs.TRANSITION_TYPED, false, 0);
+    }
+
+    // Setup bookmarks.
+    gCurrentTest.bookmarks = [];
+    for (let i = 0; i < gCurrentTest.addBookmarks; i++) {
+      let page = "http://" + gTestIndex + "." + i + ".mozilla.org/";
+      bs.insertBookmark(bs.unfiledBookmarksFolder, uri(page),
+                        bs.DEFAULT_INDEX, null);
+      gCurrentTest.bookmarks.push(page);
+    }
+
+    // Observe history.
+    historyObserver = {
+      onBeginUpdateBatch: function PEX_onBeginUpdateBatch() {},
+      onEndUpdateBatch: function PEX_onEndUpdateBatch() {},
+      onClearHistory: function() {},
+      onVisit: function() {},
+      onTitleChanged: function() {},
+      onBeforeDeleteURI: function() {},
+      onDeleteURI: function(aURI) {
+        gCurrentTest.receivedNotifications++;
+        // Check this uri was not bookmarked.
+        do_check_eq(gCurrentTest.bookmarks.indexOf(aURI.spec), -1);
+      },
+      onPageChanged: function() {},
+      onDeleteVisits: function(aURI, aTime) { },
+    };
+    hs.addObserver(historyObserver, false);
+
+    // Observe expirations.
+    observer = {
+      observe: function(aSubject, aTopic, aData) {
+        os.removeObserver(observer, TOPIC_EXPIRATION_FINISHED);
+        hs.removeObserver(historyObserver, false);
+
+        // This test finished.
+        check_result();
+      }
+    };
+    os.addObserver(observer, TOPIC_EXPIRATION_FINISHED, false);
+
+    // Expire now, observers will check results.
+    force_expiration_step();
+  }
+  else {
+    clearMaxPages();
+    bs.removeFolderChildren(bs.unfiledBookmarksFolder);
+    waitForClearHistory(do_test_finished);
+  }
+}
+
+function check_result() {
+
+  do_check_eq(gCurrentTest.receivedNotifications,
+              gCurrentTest.expectedNotifications);
+
+  // Clean up.
+  bs.removeFolderChildren(bs.unfiledBookmarksFolder);
+  waitForClearHistory(run_next_test);
+}
