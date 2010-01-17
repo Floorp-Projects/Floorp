a9e424effdaa2ce2dc2c4cddb53d009bae1c1650
Nicholas Nethercote <nnethercote@mozilla.com>
2010-01-18 09:47:31 +1100

    Bug 538538 - lirasm: don't run the optimizers, except when using --random (TM-specific part).  r=graydon.

--- js/src/jsregexp.cpp
+++ js/src/jsregexp.cpp
@@ -3241,7 +3241,8 @@ class RegExpNativeCompiler {
          */
         JS_ASSERT(!lirbuf->sp && !lirbuf->rp);
 
-        ::compile(assm, fragment, tempAlloc verbose_only(, lirbuf->names->labels));
+        ::compile(assm, fragment, tempAlloc, /*optimize*/true
+                  verbose_only(, lirbuf->names->labels));
         if (assm->error() != nanojit::None)
             goto fail;
 
