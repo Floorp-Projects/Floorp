f299ed57ea6a9fcb73f42efe92be6368e3bd968b
Chris Jones <jones.chris.g@gmail.com>
2010-01-22 15:31:56 -0600

    Temporary spew part 2: convert fprintf(stderr into printf( so that child output shows up in the log.

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3598,7 +3598,7 @@ XRE_main(int argc, char* argv[], const nsXREAppData* aAppData)
 
 
 #ifdef OS_LINUX
-  fprintf(stderr, "TEST-UNEXPECTED-FAIL | browser process %d | exiting\n", getpid());
+  printf("TEST-UNEXPECTED-FAIL | browser process %d | exiting\n", getpid());
 #endif
 
 
