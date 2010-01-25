6af8f0e388bbf6265b99309d7553c38aa64c3e61
Alexander Surkov <surkov.alexander@gmail.com>
2010-01-25 23:09:25 +0800

    Bug 541108 -  nsAccessible::DoCommand shouldn't return value, r=davidb

--- accessible/src/html/nsHTMLLinkAccessible.cpp
+++ accessible/src/html/nsHTMLLinkAccessible.cpp
@@ -160,8 +160,8 @@ nsHTMLLinkAccessible::DoAction(PRUint8 aIndex)
   if (IsDefunct())
     return NS_ERROR_FAILURE;
 
-  nsCOMPtr<nsIContent> content(do_QueryInterface(mDOMNode));
-  return DoCommand(content);
+  DoCommand();
+  return NS_OK;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
