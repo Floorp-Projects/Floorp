5e1a6a8a96a1a500645c8c1ddd554ffef117e733
Honza Bambas <honzab.moz@firemni.cz>
2010-01-26 14:33:03 +0100

    Bug 501423 - StorageEvent implementation does not match the spec, r=jst

---
+++ dom/tests/mochitest/storageevent/interOriginTest2.js
@@ -0,0 +1,58 @@
+var frameLoadsPending = 2;
+
+var callMasterFrame = true;
+var testDone = false;
+
+var masterFrameOrigin = "";
+var slaveFrameOrigin = "";
+
+var failureRegExp = new RegExp("^FAILURE");
+var todoRegExp = new RegExp("^TODO");
+
+const framePath = "/tests/dom/tests/mochitest/storageevent/";
+
+window.addEventListener("message", onMessageReceived, false);
+
+function onMessageReceived(event)
+{
+  netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+  netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
+
+  switch (event.data)
+  {
+    // Indication of the frame onload event
+    case "frame loaded":
+      if (--frameLoadsPending)
+        break;
+
+      // Just fall through...
+
+    // Indication of successfully finished step of a test
+    case "perf":
+      if (callMasterFrame)
+        masterFrame.postMessage("step", masterFrameOrigin);
+      else if (slaveFrame)
+        slaveFrame.postMessage("step", slaveFrameOrigin);
+      else if (masterFrame.slaveFrame)
+        masterFrame.slaveFrame.postMessage("step", slaveFrameOrigin);
+      callMasterFrame = !callMasterFrame;
+      break;
+
+    // Indication of all test parts finish (from any of the frames)
+    case "done":
+      if (testDone)
+        break;
+
+      testDone = true;
+      SimpleTest.finish();
+      break;
+
+    // Any other message indicates error, succes or todo message of a test
+    default:
+      if (event.data.match(todoRegExp))
+        SimpleTest.todo(false, event.data);
+      else
+        SimpleTest.ok(!event.data.match(failureRegExp), event.data);
+      break;
+  }
+}
