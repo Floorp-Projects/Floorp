a0d47c0f05e0acaf6c5c71166e795b26a36490e3
Chris Jones <jones.chris.g@gmail.com>
2010-01-27 00:41:33 -0600

    Bug 540886, part 3: IPDL/C++ test for BlockChild()/UnblockChild(). r=bnewman

---
+++ ipc/ipdl/test/cxx/TestBlockChild.h
@@ -0,0 +1,96 @@
+#ifndef mozilla__ipdltest_TestBlockChild_h
+#define mozilla__ipdltest_TestBlockChild_h 1
+
+#include "mozilla/_ipdltest/IPDLUnitTests.h"
+
+#include "mozilla/_ipdltest/PTestBlockChildParent.h"
+#include "mozilla/_ipdltest/PTestBlockChildChild.h"
+
+namespace mozilla {
+namespace _ipdltest {
+
+
+class TestBlockChildParent :
+    public PTestBlockChildParent
+{
+public:
+    TestBlockChildParent() : mChildBlocked(false),
+                             mGotP1(false),
+                             mGotP2(false)
+    { }
+    virtual ~TestBlockChildParent() { }
+
+    void Main();
+
+protected:
+    NS_OVERRIDE
+    virtual bool AnswerStackFrame();
+
+    NS_OVERRIDE
+    virtual bool RecvP1();
+
+    NS_OVERRIDE
+    virtual bool RecvP2();
+
+    NS_OVERRIDE
+    virtual bool RecvDone();
+
+    NS_OVERRIDE
+    virtual void ActorDestroy(ActorDestroyReason why)
+    {
+        if (NormalShutdown != why)
+            fail("unexpected destruction!");  
+        passed("ok");
+        QuitParent();
+    }
+
+private:
+    void BlockChildInLowerFrame();
+
+    bool mChildBlocked;
+    bool mGotP1;
+    bool mGotP2;
+    bool mGotP3;
+};
+
+
+class TestBlockChildChild :
+    public PTestBlockChildChild
+{
+public:
+    TestBlockChildChild() { }
+    virtual ~TestBlockChildChild() { }
+
+protected:
+    NS_OVERRIDE
+    virtual bool RecvPoke1();
+
+    NS_OVERRIDE
+    virtual bool AnswerStackFrame();
+
+    NS_OVERRIDE
+    virtual bool RecvPoke2();
+
+    NS_OVERRIDE
+    virtual bool RecvLastPoke();
+
+    NS_OVERRIDE
+    virtual void ActorDestroy(ActorDestroyReason why)
+    {
+        if (NormalShutdown != why)
+            fail("unexpected destruction!");
+        QuitChild();
+    }
+
+private:
+    void OnPoke1();
+    void OnPoke2();
+    void OnLastPoke();
+};
+
+
+} // namespace _ipdltest
+} // namespace mozilla
+
+
+#endif // ifndef mozilla__ipdltest_TestBlockChild_h
