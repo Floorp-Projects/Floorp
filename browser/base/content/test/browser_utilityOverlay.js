9f5051a843f9b6c5a14edce9e6bd5cecb64963f7
Gavin Sharp <gavin@gavinsharp.com>
2010-01-28 13:31:45 -0500

    Bug 512784: add Services.jsm module to allow easy global access to common services, r=dao, r=Mossop

---
+++ browser/base/content/test/browser_utilityOverlay.js
@@ -0,0 +1,35 @@
+var gTestTab;
+
+function test() {
+  waitForExplicitFinish();
+
+  is(getTopWin(), window, "got top window");
+  is(getBoolPref("general.startup.browser", false), true, "getBoolPref");
+  is(getBoolPref("this.pref.doesnt.exist", true), true, "getBoolPref fallback");
+  is(getBoolPref("this.pref.doesnt.exist", false), false, "getBoolPref fallback #2");
+
+
+  gTestTab = openNewTabWith("http://example.com");
+  gBrowser.selectedTab = gTestTab;
+  gTestTab.linkedBrowser.addEventListener("load", function () {
+    gTestTab.linkedBrowser.removeEventListener("load", arguments.callee, true);
+
+    is(gTestTab.linkedBrowser.currentURI.spec, "http://example.com/", "example.com loaded");
+
+    test_openUILink();
+  }, true);
+}
+
+function test_openUILink() {
+  gTestTab.linkedBrowser.addEventListener("load", function () {
+    gTestTab.linkedBrowser.removeEventListener("load", arguments.callee, true);
+
+    is(gTestTab.linkedBrowser.currentURI.spec, "http://example.org/", "example.org loaded");
+
+    gBrowser.removeTab(gTestTab);
+    finish();
+  }, true);
+
+  //openUILink(url, e, ignoreButton, ignoreAlt, allowKeywordFixup, postData, referrerUrl);
+  openUILink("http://example.org"); // defaults to "current"
+}
