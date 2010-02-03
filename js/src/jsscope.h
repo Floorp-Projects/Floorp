9d7785e018afbdee2e636cf5b3481a7973d4523f
Igor Bukanov <igor@mir2.org>
2010-02-03 13:42:07 +0300

    bug 538463 - caching only single-threaded objects. r=jorendorff

--- js/src/jsscope.h
+++ js/src/jsscope.h
@@ -521,7 +521,7 @@ JS_IS_SCOPE_LOCKED(JSContext *cx, JSScope *scope)
 inline JSScope *
 OBJ_SCOPE(JSObject *obj)
 {
-    JS_ASSERT(OBJ_IS_NATIVE(obj));
+    JS_ASSERT(obj->isNative());
     return (JSScope *) obj->map;
 }
 
