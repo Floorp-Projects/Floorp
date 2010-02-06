c0ec1eed4e11e9cb88c0929cc9d4e8d4530a2b91
Dão Gottwald <dao@mozilla.com>
2010-02-06 10:03:25 +0100

    tests cleanup

--- browser/components/places/tests/browser/browser_library_left_pane_commands.js
+++ browser/components/places/tests/browser/browser_library_left_pane_commands.js
@@ -183,10 +183,7 @@ function windowObserver(aSubject, aTopic, aData) {
   gLibrary = aSubject.QueryInterface(Ci.nsIDOMWindow);
   gLibrary.addEventListener("load", function onLoad(event) {
     gLibrary.removeEventListener("load", onLoad, false);
-    executeSoon(function () {
-      // Execute tests.
-      nextTest();
-    });
+    executeSoon(nextTest);
   }, false);
 }
 
