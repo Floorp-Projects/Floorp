deec8f937af6a2960fd0b9d4c9b27504c773d2fa
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2010-02-08 11:49:18 +0900

    Bug 182954 - Content should use the newer nsIPrefService APIs instead of nsIPref. r=jst

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -79,6 +79,7 @@
 #include "nsDisplayList.h"
 #include "nsBidiUtils.h"
 #include "nsFrameManager.h"
+#include "nsIPrefService.h"
 
 //----------------------------------------------------------------------
 
