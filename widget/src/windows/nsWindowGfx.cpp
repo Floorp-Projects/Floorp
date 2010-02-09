69719d7efcec3084f9a38f30840c5c2a74750711
Jim Mathies <jmathies@mozilla.com>
2010-02-09 16:34:38 -0600

    Bug 538918 - [OOPP] Modal system dialogs cause UI freeze w/windowless controls. r=bent, cjones.

--- widget/src/windows/nsWindowGfx.cpp
+++ widget/src/windows/nsWindowGfx.cpp
@@ -355,6 +355,15 @@ PRBool nsWindow::OnPaint(HDC aDC)
   }
 #endif
 
+#ifdef MOZ_IPC
+  // We never have reentrant paint events, except when we're running our RPC
+  // windows event spin loop. If we don't trap for this, we'll try to paint,
+  // but view manager will refuse to paint the surface, resulting is black
+  // flashes on the plugin rendering surface.
+  if (mozilla::ipc::RPCChannel::IsSpinLoopActive() && mPainting)
+    return PR_FALSE;
+#endif
+
   nsPaintEvent willPaintEvent(PR_TRUE, NS_WILL_PAINT, this);
   DispatchWindowEvent(&willPaintEvent);
 
