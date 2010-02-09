2747021f6c1dec99e7c61a8bb802263463305c11
David Dahl <ddahl@mozilla.com>
2010-02-09 15:48:51 -0800

    Bug 534079: Sub chunks are incorrectly applied in some cases. r=Tony Chang

--- toolkit/components/url-classifier/src/nsUrlClassifierDBService.cpp
+++ toolkit/components/url-classifier/src/nsUrlClassifierDBService.cpp
@@ -23,6 +23,7 @@
  *   Tony Chang <tony@ponderer.org> (original author)
  *   Brett Wilson <brettw@gmail.com>
  *   Dave Camp <dcamp@mozilla.com>
+ *   David Dahl <ddahl@mozilla.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -131,7 +132,7 @@ static const PRLogModuleInfo *gUrlClassifierDbServiceLog = nsnull;
 // want to change schema, or to recover from updating bugs.  When an
 // implementation version change is detected, the database is scrapped
 // and we start over.
-#define IMPLEMENTATION_VERSION 6
+#define IMPLEMENTATION_VERSION 7
 
 #define MAX_HOST_COMPONENTS 5
 #define MAX_PATH_COMPONENTS 4
@@ -2639,10 +2640,8 @@ nsUrlClassifierDBServiceWorker::SubChunk(PRUint32 tableId,
 
   LOG(("Subbing %d entries in chunk %d in table %d", entries.Length(), chunkNum, tableId));
 
-  nsAutoTArray<nsUrlClassifierEntry, 5> existingEntries;
-  nsUrlClassifierDomainHash lastKey;
-
   for (PRUint32 i = 0; i < entries.Length(); i++) {
+    nsAutoTArray<nsUrlClassifierEntry, 5> existingEntries;
     nsUrlClassifierEntry& thisEntry = entries[i];
 
     HandlePendingLookups();
@@ -2651,15 +2650,10 @@ nsUrlClassifierDBServiceWorker::SubChunk(PRUint32 tableId,
     PRBool haveAdds = (mCachedAddChunks.BinaryIndexOf(thisEntry.mAddChunkId) !=
                        mCachedAddChunks.NoIndex);
 
-    if (i == 0 || lastKey != thisEntry.mKey) {
-      existingEntries.Clear();
-      lastKey = thisEntry.mKey;
-
-      if (haveAdds) {
-        rv = mMainStore.ReadAddEntries(thisEntry.mKey, thisEntry.mTableId,
-                                       thisEntry.mAddChunkId, existingEntries);
-        NS_ENSURE_SUCCESS(rv, rv);
-      }
+    if (haveAdds) {
+      rv = mMainStore.ReadAddEntries(thisEntry.mKey, thisEntry.mTableId,
+                                     thisEntry.mAddChunkId, existingEntries);
+      NS_ENSURE_SUCCESS(rv, rv);
     }
 
     for (PRUint32 j = 0; j < existingEntries.Length(); j++) {
