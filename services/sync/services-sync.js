04f2b696e359c136c469c701d9b63502eb7015c2
Edward Lee <edilee@mozilla.com>
2010-02-11 14:28:35 -0800

    Bug 533207 - wyciwyg:// URLs should not be synced as-is Filter out tabs that start with wyciwyg:.

--- services/sync/services-sync.js
+++ services/sync/services-sync.js
@@ -14,7 +14,7 @@ pref("extensions.weave.engine.history", true);
 pref("extensions.weave.engine.passwords", true);
 pref("extensions.weave.engine.prefs", true);
 pref("extensions.weave.engine.tabs", true);
-pref("extensions.weave.engine.tabs.filteredUrls", "^(about:blank|chrome://weave/.*)$");
+pref("extensions.weave.engine.tabs.filteredUrls", "^(about:blank|chrome://weave/.*|wyciwyg:.*)$");
 
 pref("extensions.weave.log.appender.console", "Warn");
 pref("extensions.weave.log.appender.dump", "Error");
