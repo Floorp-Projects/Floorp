15b77da9e41551e64dc14675eb396f3f84f40eab
Blake Kaplan <mrbkap@gmail.com>
2010-02-11 17:04:41 -0800

    Bug 386635 - Automatically wrap sandboxes in security wrappers to make them easier to use safely. r=jst

--- js/src/xpconnect/src/XPCWrapper.cpp
+++ js/src/xpconnect/src/XPCWrapper.cpp
@@ -429,7 +429,7 @@ AddProperty(JSContext *cx, JSObject *wrapperObj, JSBool wantGetterSetter,
   NS_ASSERTION(desc.obj == wrapperObj,
                "What weird wrapper are we using?");
 
-  return JS_DefinePropertyById(cx, innerObj, interned_id, desc.value,
+  return JS_DefinePropertyById(cx, innerObj, interned_id, *vp,
                                desc.getter, desc.setter, desc.attrs);
 }
 
