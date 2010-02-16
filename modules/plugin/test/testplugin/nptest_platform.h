982f5e950859605d781458ea63f2902badd2ff61
Chris Jones <jones.chris.g@gmail.com>
2010-02-16 12:44:26 -0600

    Bug 544945: Mochitest of clipboard interaction that forces the plugin subprocess into a nested event loop on gtk2 platforms. r=karlt

--- modules/plugin/test/testplugin/nptest_platform.h
+++ modules/plugin/test/testplugin/nptest_platform.h
@@ -120,4 +120,10 @@ int32_t pluginGetClipRegionRectEdge(InstanceData* instanceData,
  */
 void pluginDoInternalConsistencyCheck(InstanceData* instanceData, std::string& error);
 
+/**
+ * Get the current clipboard item as text.  If the clipboard item
+ * isn't text, the returned value is undefined.
+ */
+std::string pluginGetClipboardText(InstanceData* instanceData);
+
 #endif // nptest_platform_h_
