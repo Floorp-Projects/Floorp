10e6a7bddb5f41a6993a214a05c89f2626e1b367
Nicholas Nethercote <nnethercote@mozilla.com>
2010-02-18 10:41:48 +1100

    Temporarily comment out assertion that is failing bogusly to fix WinMo builds;  will be overwritten by the next NJ-to-TM merge, so we need to fix MSVC's compile-time ARM arch detection in the meantime.  r=me.

--- js/src/nanojit/njconfig.cpp
+++ js/src/nanojit/njconfig.cpp
@@ -94,7 +94,7 @@ namespace nanojit
 
 #if defined(NANOJIT_ARM)
 
-        NanoStaticAssert(NJ_COMPILER_ARM_ARCH >= 5 && NJ_COMPILER_ARM_ARCH <= 7);
+        //NanoStaticAssert(NJ_COMPILER_ARM_ARCH >= 5 && NJ_COMPILER_ARM_ARCH <= 7);
 
         arm_arch = NJ_COMPILER_ARM_ARCH;
         arm_vfp = (arm_arch >= 7);
