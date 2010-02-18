38e9ef32fc340a00e399ef5a514dc6806affe2c0
Shawn Wilsher <sdwilsh@shawnwilsher.com>
2010-02-18 09:42:20 -0800

    Backed out changeset 550a4379468f

--- content/svg/content/src/nsSVGAElement.cpp
+++ content/svg/content/src/nsSVGAElement.cpp
@@ -267,19 +267,12 @@ nsSVGAElement::SetAttr(PRInt32 aNameSpaceID, nsIAtom* aName,
                        nsIAtom* aPrefix, const nsAString& aValue,
                        PRBool aNotify)
 {
-  nsresult rv = nsSVGAElementBase::SetAttr(aNameSpaceID, aName, aPrefix,
-                                           aValue, aNotify);
-
-  // The ordering of the parent class's SetAttr call and Link::ResetLinkState
-  // is important here!  The attribute is not set until SetAttr returns, and
-  // we will need the updated attribute value because notifying the document
-  // that content states have changed will call IntrinsicState, which will try
-  // to get updated information about the visitedness from Link.
   if (aName == nsGkAtoms::href && aNameSpaceID == kNameSpaceID_XLink) {
     Link::ResetLinkState(!!aNotify);
   }
 
-  return rv;
+  return nsSVGAElementBase::SetAttr(aNameSpaceID, aName, aPrefix, aValue,
+                                    aNotify);
 }
 
 nsresult
