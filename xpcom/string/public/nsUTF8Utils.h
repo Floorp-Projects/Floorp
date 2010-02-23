b0c93a14434683a559a72409679417b3004c6d0e
Jonas Sicking <jonas@sicking.cc>
2010-02-23 09:38:10 -0800

    Bug 541245 part 4: Be consistent about prohibited characters such as 0xFFFE in UTF8<->UTF16 conversion code. r=smontagu

--- xpcom/string/public/nsUTF8Utils.h
+++ xpcom/string/public/nsUTF8Utils.h
@@ -134,10 +134,9 @@ public:
           ucs4 = UCS2_REPLACEMENT_CHAR;
         }
       else if ( ucs4 >= 0xD800 &&
-                (ucs4 <= 0xDFFF || ucs4 == 0xFFFE || ucs4 == 0xFFFF ||
-                 ucs4 >= UCS_END))
+                (ucs4 <= 0xDFFF || ucs4 >= UCS_END))
         {
-          // Surrogates and prohibited characters
+          // Surrogates and code points outside the Unicode range.
           ucs4 = UCS2_REPLACEMENT_CHAR;
         }
 
