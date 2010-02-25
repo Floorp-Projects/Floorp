40d81b44ce57de7e29f3595eeaa4aca5237a16e8
Jonas Sicking <jonas@sicking.cc>
2010-02-25 01:25:53 -0800

    Fix license in new files added for bug 546528

--- content/base/src/nsFormData.h
+++ content/base/src/nsFormData.h
@@ -14,7 +14,7 @@
  * The Original Code is mozilla.org code.
  *
  * The Initial Developer of the Original Code is
- * mozilla.org.
+ * Mozilla Foundation.
  * Portions created by the Initial Developer are Copyright (C) 2010
  * the Initial Developer. All Rights Reserved.
  *
