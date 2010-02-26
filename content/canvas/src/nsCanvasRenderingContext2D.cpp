a4d09f69e4bc337b9a110b8add96ef42f03ba908
Jonathan Kew <jfkthame@gmail.com>
2010-02-26 08:58:48 -0800

    Bug 548751 - make canvas respect the style's language when resolving fonts. r=dbaron

--- content/canvas/src/nsCanvasRenderingContext2D.cpp
+++ content/canvas/src/nsCanvasRenderingContext2D.cpp
@@ -1932,8 +1932,6 @@ nsCanvasRenderingContext2D::SetFont(const nsAString& font)
       return NS_ERROR_FAILURE;
     nsIDocument* document = presShell->GetDocument();
 
-    nsIAtom *language = presShell->GetPresContext()->GetLanguageFromCharset();
-
     nsCOMArray<nsIStyleRule> rules;
 
     nsCOMPtr<nsICSSStyleRule> rule;
@@ -1984,6 +1982,11 @@ nsCanvasRenderingContext2D::SetFont(const nsAString& font)
     const nsStyleFont* fontStyle = sc->GetStyleFont();
 
     NS_ASSERTION(fontStyle, "Could not obtain font style");
+
+    nsIAtom* language = sc->GetStyleVisibility()->mLanguage;
+    if (!language) {
+        language = presShell->GetPresContext()->GetLanguageFromCharset();
+    }
 
     // use CSS pixels instead of dev pixels to avoid being affected by page zoom
     const PRUint32 aupcp = nsPresContext::AppUnitsPerCSSPixel();
