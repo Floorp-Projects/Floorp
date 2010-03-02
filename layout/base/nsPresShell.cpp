827926975c1c6d9b85eac89db1cac4e832d8cbb3
Daniel Holbert <dholbert@cs.stanford.edu>
2010-03-02 07:36:33 -0800

    Bug 547333 followup: Move FlushAnimations() call from nsSVGElement::GetAnimatedLengthValues to FlushPendingNotifications. r=jwatt r=bz

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -158,6 +158,9 @@
 #ifdef MOZ_MEDIA
 #include "nsHTMLMediaElement.h"
 #endif
+#ifdef MOZ_SMIL
+#include "nsSMILAnimationController.h"
+#endif
 
 // Drag & Drop, Clipboard
 #include "nsWidgetsCID.h"
@@ -4755,6 +4758,13 @@ PresShell::FlushPendingNotifications(mozFlushType aType)
       // cause style changes (for updating ex/ch units, and to cause a
       // reflow).
       mPresContext->FlushUserFontSet();
+
+#ifdef MOZ_SMIL
+      // Flush any requested SMIL samples.
+      if (mDocument->HasAnimationController()) {
+        mDocument->GetAnimationController()->FlushResampleRequests();
+      }
+#endif // MOZ_SMIL
 
       nsAutoScriptBlocker scriptBlocker;
       mFrameConstructor->ProcessPendingRestyles();
