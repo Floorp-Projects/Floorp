dd6f85d103e7668aec0162ae562da36d7e7d9e8c
Brendan Eich <brendan@mozilla.org>
2010-03-02 11:03:04 -0800

    Bug 549531 - fix strict aliasing warnings about scopeChain (also clean trailing whitespace; r=dmandelin).

--- js/src/jsrecursion.cpp
+++ js/src/jsrecursion.cpp
@@ -526,7 +526,7 @@ TraceRecorder::slurpDownFrames(jsbytecode* return_pc)
               &info);
     /* scopeChain */
     slurpSlot(addName(lir->insLoad(LIR_ldp, fp_ins, offsetof(JSStackFrame, scopeChain)), "scopeChain"),
-              (jsval*) &fp->scopeChain,
+              &fp->scopeChainVal,
               &info);
     /* vars */
     LIns* slots_ins = addName(lir->insLoad(LIR_ldp, fp_ins, offsetof(JSStackFrame, slots)),
