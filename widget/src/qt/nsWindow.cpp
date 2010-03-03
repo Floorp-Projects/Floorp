ab0b7495b26271760e0854c296fe7b87d5509710
Oleg Romashin <romaxa@gmail.com>
2010-03-03 19:45:16 +0200

    Bug 549858 - [QT] build break in Qt widgets due to new layer infrastructure. r=roc

--- widget/src/qt/nsWindow.cpp
+++ widget/src/qt/nsWindow.cpp
@@ -1,4 +1,4 @@
-./* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /* vim:expandtab:shiftwidth=4:tabstop=4:
  */
 /* ***** BEGIN LICENSE BLOCK *****
