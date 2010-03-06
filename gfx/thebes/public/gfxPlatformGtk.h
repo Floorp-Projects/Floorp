61c625c90baf5d073e00e3bdaaa1b83081f19941
Oleg Romashin <romaxa@gmail.com>
2010-03-06 15:29:55 +0200

    Bug 550468 - Fix support for Pango font engine on Qt platform. r=karlt.

--- gfx/thebes/public/gfxPlatformGtk.h
+++ gfx/thebes/public/gfxPlatformGtk.h
@@ -54,13 +54,6 @@ class FontEntry;
 typedef struct FT_LibraryRec_ *FT_Library;
 #endif
 
-template <class T>
-class gfxGObjectRefTraits : public nsPointerRefTraits<T> {
-public:
-    static void Release(T *aPtr) { g_object_unref(aPtr); }
-    static void AddRef(T *aPtr) { g_object_ref(aPtr); }
-};
-
 class THEBES_API gfxPlatformGtk : public gfxPlatform {
 public:
     gfxPlatformGtk();
