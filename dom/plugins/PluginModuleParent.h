6fa2d57900f86a03fbd7fc25fa5b1cf54fdbcf9f
Chris Jones <jones.chris.g@gmail.com>
2010-03-08 09:16:02 +1300

    b=550026 Don't call PluginCrashed while plugin code is still on the stack r=bsmedberg

--- dom/plugins/PluginModuleParent.h
+++ dom/plugins/PluginModuleParent.h
@@ -120,6 +120,10 @@ public:
 
     bool EnsureValidNPIdentifier(NPIdentifier aIdentifier);
 
+    bool OkToCleanup() const {
+        return !IsOnCxxStack();
+    }
+
 protected:
     NS_OVERRIDE
     virtual bool ShouldContinueFromReplyTimeout();
@@ -228,6 +232,7 @@ private:
                              const char* value);
     void CleanupFromTimeout();
     static int TimeoutChanged(const char* aPref, void* aModule);
+    void NotifyPluginCrashed();
 
     nsCString mCrashNotes;
     PluginProcessParent* mSubprocess;
@@ -236,6 +241,8 @@ private:
     nsTHashtable<nsVoidPtrHashKey> mValidIdentifiers;
     nsNPAPIPlugin* mPlugin;
     time_t mProcessStartTime;
+    CancelableTask* mPluginCrashedTask;
+    nsString mDumpID;
 };
 
 } // namespace plugins
