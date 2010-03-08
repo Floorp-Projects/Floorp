feae80e467bc64d7c90c19d7361976eda1a85563
David Bolter <dbolter@mozilla.com>
2010-03-07 21:03:19 -0500

    Bug 548291 - Support ARIA on area (map) elements; r=marcoz,surkov

--- accessible/src/html/nsHTMLAreaAccessible.h
+++ accessible/src/html/nsHTMLAreaAccessible.h
@@ -58,6 +58,7 @@ public:
 
   // nsAccessible
   virtual nsresult GetNameInternal(nsAString& aName);
+  virtual nsresult GetStateInternal(PRUint32 *aState, PRUint32 *aExtraState);
   virtual nsresult GetChildAtPoint(PRInt32 aX, PRInt32 aY,
                                    PRBool aDeepestChild,
                                    nsIAccessible **aChild);
