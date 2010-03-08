a2c9bac20313bac7a1401fa329d40372aa45098a
Dave Townsend <dtownsend@oxymoronical.com>
2010-03-08 10:26:54 -0800

    Bug 550481: Exceptions from the do_execute_soon callback should be logged and fail the test. r=ted

---
+++ testing/xpcshell/example/unit/test_execute_soon.js
@@ -0,0 +1,20 @@
+/* vim:set ts=2 sw=2 sts=2 et: */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/licenses/publicdomain/
+ * ***** END LICENSE BLOCK ***** */
+
+var complete = false;
+
+function run_test() {
+  dump("Starting test\n");
+  do_register_cleanup(function() {
+    dump("Checking test completed\n");
+    do_check_true(complete);
+  });
+
+  do_execute_soon(function execute_soon_callback() {
+    dump("do_execute_soon callback\n");
+    complete = true;
+  });
+}
