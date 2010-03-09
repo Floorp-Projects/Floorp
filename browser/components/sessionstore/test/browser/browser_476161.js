fc70473586983c81323fb64ea67d50f9279abf80
Joel Maher <jmaher@mozilla.com>
2010-03-08 19:21:33 -0800

    Bug 544097 Change mochitests to use mochi.test instead of localhost for mobile refactoring p=jmaher r=ted,dwitte,sayrer

--- browser/components/sessionstore/test/browser/browser_476161.js
+++ browser/components/sessionstore/test/browser/browser_476161.js
@@ -39,7 +39,7 @@ function test() {
   
   waitForExplicitFinish();
   
-  let testURL = "http://localhost:8888/browser/" +
+  let testURL = "http://mochi.test:8888/browser/" +
     "browser/components/sessionstore/test/browser/browser_476161_sample.html";
   let tab = gBrowser.addTab(testURL);
   tab.linkedBrowser.addEventListener("load", function(aEvent) {
