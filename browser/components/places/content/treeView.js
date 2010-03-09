0cc367f06accfcea13bc7e7df7c74a6cfb2d0805
Asaf Romano <aromano@mozilla.com>
2010-03-09 14:43:09 +0100

    Bug 543444 - Replace single-view API with multiple observers. r=mak sr=rstrong

--- browser/components/places/content/treeView.js
+++ browser/components/places/content/treeView.js
@@ -63,7 +63,7 @@ PlacesTreeView.prototype = {
 
   QueryInterface: function PTV_QueryInterface(aIID) {
     if (aIID.equals(Ci.nsITreeView) ||
-        aIID.equals(Ci.nsINavHistoryResultViewer) ||
+        aIID.equals(Ci.nsINavHistoryResultObserver) ||
         aIID.equals(Ci.nsINavHistoryResultTreeViewer) ||
         aIID.equals(Ci.nsISupports))
       return this;
@@ -591,7 +591,7 @@ PlacesTreeView.prototype = {
     return [this.COLUMN_TYPE_UNKNOWN, false];
   },
 
-  // nsINavHistoryResultViewer
+  // nsINavHistoryResultObserver
   nodeInserted: function PTV_nodeInserted(aParentNode, aNode, aNewIndex) {
     NS_ASSERT(this._result, "Got a notification but have no result!");
     if (!this._tree || !this._result)
@@ -995,22 +995,16 @@ PlacesTreeView.prototype = {
 
   get result() this._result,
   set result(val) {
-    // Some methods (e.g. getURLsFromContainer) temporarily null out the
-    // viewer when they do temporary changes to the view, this does _not_
-    // call setResult(null), but then, we're called again with the result
-    // object which is already set for this viewer. At that point,
-    // we should do nothing.
-    if (this._result != val) {
-      if (this._result)
-        this._rootNode.containerOpen = false;
+    if (this._result)
+      this._rootNode.containerOpen = false;
 
-      this._result = val;
-      this._rootNode = val ? val.root : null;
+    this._result = val;
+    this._rootNode = val ? val.root : null;
 
-      // If the tree is not set yet, setTree will call finishInit.
-      if (this._tree && val)
-        this._finishInit();
-    }
+    // If the tree is not set yet, setTree will call finishInit.
+    if (this._tree && val)
+      this._finishInit();
+
     return val;
   },
 
@@ -1364,7 +1358,7 @@ PlacesTreeView.prototype = {
         // detach from result when we are detaching from the tree.
         // This breaks the reference cycle between us and the result.
         if (!aTree)
-          this._result.viewer = null;
+          this._result.removeObserver(this);
       }
       if (aTree)
         this._finishInit();
