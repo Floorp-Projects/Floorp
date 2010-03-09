2865c087a87397b9664c9368bab1f34be91cb17f
Boris Zbarsky <bzbarsky@mit.edu>
2010-03-08 19:47:37 -0500

    Bug 550842 followup.  Make this actually compile.

--- content/base/src/nsGenericElement.cpp
+++ content/base/src/nsGenericElement.cpp
@@ -5176,7 +5176,7 @@ TryMatchingElementsInSubtree(nsINode* aRoot,
    * parent data chain, since we have those Around anyway */
   union { char c[2 * sizeof(RuleProcessorData)]; void *p; } databuf;
   RuleProcessorData* prevSibling = nsnull;
-  RuleProcessorData* data = reinterpret_cast<RuleProcessorData*>(databuf);
+  RuleProcessorData* data = reinterpret_cast<RuleProcessorData*>(databuf.c);
 
   PRBool continueIteration = PR_TRUE;
   for (nsINode::ChildIterator iter(aRoot); !iter.IsDone(); iter.Next()) {
