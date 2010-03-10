58f3d556b9617a3dffcb855b3c692c1802908526
Martin Stransky <stransky@redhat.com>
2010-03-10 12:33:35 -0800

    Bug 546270: Use SetupSilentPrinting() to initialize mGTKPrinter for window.print() calls. r=roc

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -4531,6 +4531,13 @@ nsGlobalWindow::Print()
       if (printSettingsAreGlobal) {
         printSettingsService->GetGlobalPrintSettings(getter_AddRefs(printSettings));
 
+        if (printSettings) {
+          // Call any code that requires a run of the event loop.
+          EnterModalState();
+          printSettings->SetupSilentPrinting();
+          LeaveModalState();
+        }
+
         nsXPIDLString printerName;
         printSettingsService->GetDefaultPrinterName(getter_Copies(printerName));
         if (printerName)
