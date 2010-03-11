87abf060978dcf6cec4da7fdebc93ddab087f46e
David Anderson <danderson@mozilla.com>
2010-03-11 12:19:36 -0800

    Backed out changeset 04023ea0fb08.

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -3057,6 +3057,10 @@ js_GC(JSContext *cx, JSGCInvocationKind gckind)
   }
 #endif
 
+#ifdef JS_TRACER
+    PurgeJITOracle();
+#endif
+
     /*
      * Reset the property cache's type id generator so we can compress ids.
      * Same for the protoHazardShape proxy-shape standing in for all object
