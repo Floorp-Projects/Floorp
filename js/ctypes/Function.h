e148d83c5a9b2babac32841ce80a6b8fc7d00310
Dan Witte <dwitte@mozilla.com>
2010-03-11 17:17:36 -0800

    Revise js-facing API for js-ctypes, patch v1. b=513788, r=jorendorff

--- js/ctypes/Function.h
+++ js/ctypes/Function.h
@@ -40,48 +40,45 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
-#include "Module.h"
+#include "CTypes.h"
 #include "nsTArray.h"
 #include "prlink.h"
-#include "ffi.h"
 
 namespace mozilla {
 namespace ctypes {
 
-// for JS error reporting
-enum ErrorNum {
-#define MSG_DEF(name, number, count, exception, format) \
-  name = number,
-#include "ctypes.msg"
-#undef MSG_DEF
-  CTYPESERR_LIMIT
+enum FunctionSlot
+{
+  SLOT_FUNCTION = 0,
+  SLOT_LIBRARYOBJ = 1
+  // JSFunction objects always get exactly two slots.
 };
-
-const JSErrorFormatString*
-GetErrorMessage(void* userRef, const char* locale, const uintN errorNumber);
 
 struct Type
 {
   ffi_type mFFIType;
-  TypeCode mType;
+  JSObject* mType;
 };
 
-struct Value
+struct AutoValue
 {
+  AutoValue() : mData(NULL) { }
+
+  ~AutoValue()
+  {
+    delete static_cast<char*>(mData);
+  }
+
+  bool SizeToType(JSContext* cx, JSObject* type)
+  {
+    size_t size = CType::GetSize(cx, type);
+    mData = new char[size];
+    if (mData)
+      memset(mData, 0, size);
+    return mData != NULL;
+  }
+
   void* mData;
-  union {
-    PRInt8   mInt8;
-    PRInt16  mInt16;
-    PRInt32  mInt32;
-    PRInt64  mInt64;
-    PRUint8  mUint8;
-    PRUint16 mUint16;
-    PRUint32 mUint32;
-    PRUint64 mUint64;
-    float    mFloat;
-    double   mDouble;
-    void*    mPointer;
-  } mValue;
 };
 
 class Function
@@ -90,6 +87,7 @@ public:
   Function();
 
   Function*& Next() { return mNext; }
+  void Trace(JSTracer *trc);
 
   static JSObject* Create(JSContext* aContext, JSObject* aLibrary, PRFuncPtr aFunc, const char* aName, jsval aCallType, jsval aResultType, jsval* aArgTypes, uintN aArgLength);
   static JSBool Call(JSContext* cx, uintN argc, jsval* vp);
