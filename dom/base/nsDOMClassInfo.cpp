e0f914f5678702125d5afefa536ebf3710a382b3
Robert Sayre <sayrer@gmail.com>
2010-03-13 14:35:27 -0500

    Fix up a little merge fail so that infallible malloc macros don't get run on js/src files.

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -37,6 +37,13 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+// JavaScript includes
+#include "jsapi.h"
+#include "jsprvtd.h"    // we are using private JS typedefs...
+#include "jscntxt.h"
+#include "jsobj.h"
+#include "jsdbgapi.h"
+
 #include "nscore.h"
 #include "nsDOMClassInfo.h"
 #include "nsCRT.h"
@@ -61,13 +68,6 @@
 #include "nsCSSValue.h"
 #include "nsIRunnable.h"
 #include "nsThreadUtils.h"
-
-// JavaScript includes
-#include "jsapi.h"
-#include "jsprvtd.h"    // we are using private JS typedefs...
-#include "jscntxt.h"
-#include "jsobj.h"
-#include "jsdbgapi.h"
 
 // General helper includes
 #include "nsGlobalWindow.h"
