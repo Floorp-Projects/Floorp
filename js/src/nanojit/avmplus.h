54a12b45e6f524fe40ad25fbc2d2999ce6a69d38
Nicholas Nethercote <nnethercote@mozilla.com>
2010-03-15 12:30:32 +1100

    Bug 551876 - nanojit: fix avmplus.h warnings.  r=edwsmith.

--- js/src/nanojit/avmplus.h
+++ js/src/nanojit/avmplus.h
@@ -250,10 +250,6 @@ namespace avmplus {
      * on a set of items or conditions. Class BitSet provides functions
      * to manipulate individual bits in the vector.
      *
-     * Since most vectors are rather small an array of longs is used by
-     * default to house the value of the bits.  If more bits are needed
-     * then an array is allocated dynamically outside of this object.
-     *
      * This object is not optimized for a fixed sized bit vector
      * it instead allows for dynamically growing the bit vector.
      */
@@ -266,23 +262,19 @@ namespace avmplus {
             BitSet()
             {
                 capacity = kDefaultCapacity;
+                ar = (long*)calloc(capacity, sizeof(long));
                 reset();
             }
 
             ~BitSet()
             {
-                if (capacity > kDefaultCapacity)
-                    free(bits.ptr);
+                free(ar);
             }
 
             void reset()
             {
-                if (capacity > kDefaultCapacity)
-                    for(int i=0; i<capacity; i++)
-                        bits.ptr[i] = 0;
-                else
-                    for(int i=0; i<capacity; i++)
-                        bits.ar[i] = 0;
+                for (int i = 0; i < capacity; i++)
+                    ar[i] = 0;
             }
 
             void set(int bitNbr)
@@ -292,10 +284,7 @@ namespace avmplus {
                 if (index >= capacity)
                     grow(index+1);
 
-                if (capacity > kDefaultCapacity)
-                    bits.ptr[index] |= (1<<bit);
-                else
-                    bits.ar[index] |= (1<<bit);
+                ar[index] |= (1<<bit);
             }
 
             void clear(int bitNbr)
@@ -303,12 +292,7 @@ namespace avmplus {
                 int index = bitNbr / kUnit;
                 int bit = bitNbr % kUnit;
                 if (index < capacity)
-                {
-                    if (capacity > kDefaultCapacity)
-                        bits.ptr[index] &= ~(1<<bit);
-                    else
-                        bits.ar[index] &= ~(1<<bit);
-                }
+                    ar[index] &= ~(1<<bit);
             }
 
             bool get(int bitNbr) const
@@ -317,12 +301,7 @@ namespace avmplus {
                 int bit = bitNbr % kUnit;
                 bool value = false;
                 if (index < capacity)
-                {
-                    if (capacity > kDefaultCapacity)
-                        value = ( bits.ptr[index] & (1<<bit) ) ? true : false;
-                    else
-                        value = ( bits.ar[index] & (1<<bit) ) ? true : false;
-                }
+                    value = ( ar[index] & (1<<bit) ) ? true : false;
                 return value;
             }
 
@@ -333,35 +312,21 @@ namespace avmplus {
                 // create vector that is 2x bigger than requested
                 newCapacity *= 2;
                 //MEMTAG("BitVector::Grow - long[]");
-                long* newBits = (long*)calloc(1, newCapacity * sizeof(long));
-                //memset(newBits, 0, newCapacity * sizeof(long));
+                long* newAr = (long*)calloc(newCapacity, sizeof(long));
 
                 // copy the old one
-                if (capacity > kDefaultCapacity)
-                    for(int i=0; i<capacity; i++)
-                        newBits[i] = bits.ptr[i];
-                else
-                    for(int i=0; i<capacity; i++)
-                        newBits[i] = bits.ar[i];
+                for (int i = 0; i < capacity; i++)
+                    newAr[i] = ar[i];
 
                 // in with the new out with the old
-                if (capacity > kDefaultCapacity)
-                    free(bits.ptr);
+                free(ar);
 
-                bits.ptr = newBits;
+                ar = newAr;
                 capacity = newCapacity;
             }
 
-            // by default we use the array, but if the vector
-            // size grows beyond kDefaultCapacity we allocate
-            // space dynamically.
             int capacity;
-            union
-            {
-                long ar[kDefaultCapacity];
-                long*  ptr;
-            }
-            bits;
+            long* ar;
     };
 }
 
