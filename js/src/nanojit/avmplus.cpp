e9a883bbb8142c921bde4b4a929b886a9b395e9c
Nicholas Nethercote <nnethercote@mozilla.com>
2010-03-15 16:23:30 +1100

    Don't use DebugBreak() in NanoAssertFail(), it causes Tinderbox problems on Windows.  No bug, r=me.

--- js/src/nanojit/avmplus.cpp
+++ js/src/nanojit/avmplus.cpp
@@ -62,15 +62,7 @@ avmplus::AvmLog(char const *msg, ...) {
 #ifdef _DEBUG
 void NanoAssertFail()
 {
-    #if defined(WIN32)
-        DebugBreak();
-        exit(3);
-    #elif defined(XP_OS2) || (defined(__GNUC__) && defined(__i386))
-        asm("int $3");
-        abort();
-    #else
-        abort();
-    #endif
+    abort();
 }
 #endif
 
