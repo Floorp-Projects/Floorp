4b4233facdd6e93171586d41e670a55d440e2957
Chris Jones <jones.chris.g@gmail.com>
2010-03-18 17:52:36 -0500

    Bug 551392: Remove subprocess minidumps after they're grabbed to avoid spurious crash detection. r=ted

--- ipc/ipdl/ipdl/lower.py
+++ ipc/ipdl/ipdl/lower.py
@@ -2961,14 +2961,14 @@ class _GenerateProtocolActorCode(ipdl.ast.Visitor):
 
             dumpvar = ExprVar('aDump')
             getdump = MethodDefn(MethodDecl(
-                'GetMinidump',
+                'TakeMinidump',
                 params=[ Decl(Type('nsIFile', ptrptr=1), dumpvar.name) ],
                 ret=Type.BOOL,
                 const=1))
             getdump.addstmts([
                 CppDirective('ifdef', 'MOZ_CRASHREPORTER'),
                 StmtReturn(ExprCall(
-                    ExprVar('XRE_GetMinidumpForChild'),
+                    ExprVar('XRE_TakeMinidumpForChild'),
                     args=[ ExprCall(otherpidvar), dumpvar ])),
                 CppDirective('else'),
                 StmtReturn(ExprLiteral.FALSE),
