58a9207ab8ccdfd3e9170c6336d3abe3476b45ce
Ginn Chen <ginn.chen@sun.com>
2010-03-23 14:41:52 +0800

    Bug 529846 check-3d-raytrace.js failed on Solaris x86 r=jimb

--- js/src/jsstr.cpp
+++ js/src/jsstr.cpp
@@ -1102,7 +1102,7 @@ StringMatch(const jschar *text, jsuint textlen,
     if (textlen < patlen)
         return -1;
 
-#if __i386__
+#if defined(__i386__) || defined(__i386)
     /*
      * Given enough registers, the unrolled loop below is faster than the
      * following loop. 32-bit x86 does not have enough registers.
