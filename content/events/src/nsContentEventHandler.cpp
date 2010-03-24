734e31069bad67da8d455b097f7cbd9fc9618085
Theppitak Karoonboonyanan <thep@linux.thai.net>
2010-03-25 00:04:39 +0900

    Bug 353776 need the surrounding text support for some language input r=masayuki

--- content/events/src/nsContentEventHandler.cpp
+++ content/events/src/nsContentEventHandler.cpp
@@ -939,8 +939,8 @@ nsContentEventHandler::OnSelectionEvent(nsSelectionEvent* aEvent)
   // Get range from offset and length
   nsRefPtr<nsRange> range = new nsRange();
   NS_ENSURE_TRUE(range, NS_ERROR_OUT_OF_MEMORY);
-  rv = SetRangeFromFlatTextOffset(range, aEvent->mOffset,
-                                  aEvent->mLength, PR_TRUE);
+  rv = SetRangeFromFlatTextOffset(range, aEvent->mOffset, aEvent->mLength,
+                                  aEvent->mExpandToClusterBoundary);
   NS_ENSURE_SUCCESS(rv, rv);
 
   nsINode* startNode = range->GetStartParent();
