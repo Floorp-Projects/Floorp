550b3fd4fab8ad9ba528f7fde76143534b5dc034
Jason Orendorff <jorendorff@mozilla.com>
2010-03-29 11:24:42 -0500

    Bug 541255 - "Assertion failure: obj->isDenseArray(), at ../jsarray.cpp". r=brendan.

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -12213,7 +12213,7 @@ TraceRecorder::upvar(JSScript* script, JSUpvarArray* uva, uintN index, jsval& v)
     JSStackFrame* fp = cx->display[level];
     const CallInfo* ci;
     int32 slot;
-    if (!fp->fun) {
+    if (!fp->fun || (fp->flags & JSFRAME_EVAL)) {
         ci = &GetUpvarStackOnTrace_ci;
         slot = cookieSlot;
     } else if (cookieSlot < fp->fun->nargs) {
