b587390278684de8ba548028ab1978249185273a
Ginn Chen <ginn.chen@sun.com>
2010-03-29 19:43:35 +0800

    Bug 555673 FramePropertyTable.cpp failed to compile with Sun Studio on Solaris r=roc

--- layout/base/FramePropertyTable.cpp
+++ layout/base/FramePropertyTable.cpp
@@ -70,7 +70,7 @@ FramePropertyTable::Set(nsIFrame* aFrame, const FramePropertyDescriptor* aProper
     // We need to expand the single current entry to an array
     PropertyValue current = entry->mProp;
     entry->mProp.mProperty = nsnull;
-    PR_STATIC_ASSERT(sizeof(nsTArray<PropertyValue>) <= sizeof(entry->mProp.mValue));
+    PR_STATIC_ASSERT(sizeof(nsTArray<PropertyValue>) <= sizeof(void *));
     new (&entry->mProp.mValue) nsTArray<PropertyValue>(4);
     entry->mProp.ToArray()->AppendElement(current);
   }
