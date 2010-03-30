2254ed7047c2bae5b4d90f75937725c803306281
Brad Lassey <blassey@mozilla.com>
2010-03-30 15:23:44 -0400

    update nspr to NSPR_HEAD_20100330 to pick up support for android r=ted

--- nsprpub/pr/src/linking/prlink.c
+++ nsprpub/pr/src/linking/prlink.c
@@ -198,7 +198,7 @@ void _PR_InitLinker(void)
 
 #elif defined(XP_UNIX)
 #ifdef HAVE_DLL
-#ifdef USE_DLFCN
+#if defined(USE_DLFCN) && !defined(NO_DLOPEN_NULL)
     h = dlopen(0, RTLD_LAZY);
     if (!h) {
         char *error;
@@ -214,8 +214,8 @@ void _PR_InitLinker(void)
 #elif defined(USE_HPSHL)
     h = NULL;
     /* don't abort with this NULL */
-#elif defined(USE_MACH_DYLD)
-    h = NULL; /* XXXX  toshok */
+#elif defined(USE_MACH_DYLD) || defined(NO_DLOPEN_NULL)
+    h = NULL; /* XXXX  toshok */ /* XXXX  vlad */
 #else
 #error no dll strategy
 #endif /* USE_DLFCN */
@@ -1354,7 +1354,7 @@ PR_LoadStaticLibrary(const char *name, const PRStaticLinkTable *slt)
 PR_IMPLEMENT(char *)
 PR_GetLibraryFilePathname(const char *name, PRFuncPtr addr)
 {
-#if defined(USE_DLFCN) && (defined(SOLARIS) || defined(FREEBSD) \
+#if defined(USE_DLFCN) && !defined(ANDROID) && (defined(SOLARIS) || defined(FREEBSD) \
         || defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
         || defined(DARWIN))
     Dl_info dli;
