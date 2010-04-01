605900c19fefba0517a533826fd2d34c4ce19525
Daniel Holbert <dholbert@cs.stanford.edu>
2010-04-01 09:38:53 -0700

    Backed out changeset 29bc09de2f77 (Bug 551298) due to Linux debug mochitest-5 orange

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -42,8 +42,6 @@
 #include "nsRegion.h"
 #include "nsPoint.h"
 #include "nsRect.h"
-#include "nsISupportsImpl.h"
-#include "nsAutoPtr.h"
 #include "gfx3DMatrix.h"
 
 class gfxContext;
@@ -108,7 +106,7 @@ class ImageContainer;
  * root layer, and each container layer holds a reference to its children.
  */
 class THEBES_API LayerManager {
-  NS_INLINE_DECL_REFCOUNTING(LayerManager)
+  THEBES_INLINE_DECL_REFCOUNTING(LayerManager)  
 
 public:
   enum LayersBackend {
@@ -183,7 +181,7 @@ public:
  * surface.
  */
 class THEBES_API Layer {
-  NS_INLINE_DECL_REFCOUNTING(Layer)  
+  THEBES_INLINE_DECL_REFCOUNTING(Layer)  
 
 public:
   virtual ~Layer() {}
