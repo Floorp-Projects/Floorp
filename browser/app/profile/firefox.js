af303a543672fd3c62e4bfd402451d41b539f102
Ehsan Akhgari <ehsan@mozilla.com>
2010-04-01 16:13:07 -0400

    Bug 549094 - Change the value of editor.singleLine.pasteNewlines to match the Firefox value across all Gecko based applications; r=bzbarsky

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -844,9 +844,6 @@ pref("browser.zoom.siteSpecific", true);
 // Whether or not to update background tabs to the current zoom level.
 pref("browser.zoom.updateBackgroundTabs", true);
 
-// replace newlines with spaces when pasting into <input type="text"> fields
-pref("editor.singleLine.pasteNewlines", 2);
-
 // The breakpad report server to link to in about:crashes
 pref("breakpad.reportURL", "http://crash-stats.mozilla.com/report/index/");
 
