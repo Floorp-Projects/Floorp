b9f7a3d9ad7a1d6a240092ee186ed7dfee66894c
L. David Baron <dbaron@dbaron.org>
2010-04-06 12:42:41 -0700

    Don't start image loads for the if-visited style contexts.  (Bug 557287)  r=bzbarsky

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -684,7 +684,12 @@ static void SetStyleImage(nsStyleContext* aStyleContext,
     case eCSSUnit_None:
       break;
     default:
-      NS_NOTREACHED("unexpected unit; maybe nsCSSValue::Image::Image() failed?");
+      // We might have eCSSUnit_URL values for if-visited style
+      // contexts, which we can safely treat like 'none'.  Otherwise
+      // this is an unexpected unit.
+      NS_ASSERTION(aStyleContext->IsStyleIfVisited() &&
+                   aValue.GetUnit() == eCSSUnit_URL,
+                   "unexpected unit; maybe nsCSSValue::Image::Image() failed?");
       break;
   }
 }
