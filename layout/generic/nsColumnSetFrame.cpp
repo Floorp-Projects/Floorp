44fc00f5f01efc21e924fd63f24cf3505bfc29c9
L. David Baron <dbaron@dbaron.org>
2010-04-06 19:03:12 -0700

    Use the appropriate color based on visitedness for '-moz-column-rule-color'.  (Bug 557580)  r=roc

--- layout/generic/nsColumnSetFrame.cpp
+++ layout/generic/nsColumnSetFrame.cpp
@@ -245,11 +245,8 @@ nsColumnSetFrame::PaintColumnRule(nsIRenderingContext* aCtx,
   if (!ruleWidth)
     return;
 
-  nscolor ruleColor;
-  if (colStyle->mColumnRuleColorIsForeground)
-    ruleColor = GetStyleColor()->mColor;
-  else
-    ruleColor = colStyle->mColumnRuleColor;
+  nscolor ruleColor =
+    GetVisitedDependentColor(eCSSProperty__moz_column_rule_color);
 
   // In order to re-use a large amount of code, we treat the column rule as a border.
   // We create a new border style object and fill in all the details of the column rule as
