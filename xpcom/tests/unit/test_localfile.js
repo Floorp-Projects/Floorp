a5da2488a9441a92060346336964b7b3a7d0a0bf
Dave Townsend <dtownsend@oxymoronical.com>
2010-04-08 12:12:19 -0700

    Bug 557539: Trying to set nsILocalFile.lastModifiedTime for a directory on windows throws NS_ERROR_ACCESS_DENIED. r=jimm

--- xpcom/tests/unit/test_localfile.js
+++ xpcom/tests/unit/test_localfile.js
@@ -38,6 +38,10 @@
 
 const Cr = Components.results;
 const CC = Components.Constructor;
+const Ci = Components.interfaces;
+
+const MAX_TIME_DIFFERENCE = 2500;
+const MILLIS_PER_DAY      = 1000 * 60 * 60 * 24;
 
 var LocalFile = CC("@mozilla.org/file/local;1", "nsILocalFile", "initWithPath");
 
@@ -45,6 +49,8 @@ function run_test()
 {
   test_toplevel_parent_is_null();
   test_normalize_crash_if_media_missing();
+  test_file_modification_time();
+  test_directory_modification_time();
 }
 
 function test_toplevel_parent_is_null()
@@ -80,5 +86,73 @@ function test_normalize_crash_if_media_missing()
     {
     }
   }
+}
+
+// Tests that changing a file's modification time is possible   
+function test_file_modification_time()
+{
+  var file = do_get_profile();
+  file.append("testfile");
+
+  // Should never happen but get rid of it anyway
+  if (file.exists())
+    file.remove(true);
+
+  var now = Date.now();
+  file.create(Ci.nsIFile.NORMAL_FILE_TYPE, 0644);
+  do_check_true(file.exists());
+
+  // Modification time may be out by up to 2 seconds on FAT filesystems. Test
+  // with a bit of leeway, close enough probably means it is correct.
+  var diff = Math.abs(file.lastModifiedTime - now);
+  do_check_true(diff < MAX_TIME_DIFFERENCE);
+
+  var yesterday = now - MILLIS_PER_DAY;
+  file.lastModifiedTime = yesterday;
+
+  diff = Math.abs(file.lastModifiedTime - yesterday);
+  do_check_true(diff < MAX_TIME_DIFFERENCE);
+
+  var tomorrow = now - MILLIS_PER_DAY;
+  file.lastModifiedTime = tomorrow;
+
+  diff = Math.abs(file.lastModifiedTime - tomorrow);
+  do_check_true(diff < MAX_TIME_DIFFERENCE);
+
+  file.remove(true);
+}
+
+// Tests that changing a directory's modification time is possible   
+function test_directory_modification_time()
+{
+  var dir = do_get_profile();
+  dir.append("testdir");
+
+  // Should never happen but get rid of it anyway
+  if (dir.exists())
+    dir.remove(true);
+
+  var now = Date.now();
+  dir.create(Ci.nsIFile.DIRECTORY_TYPE, 0755);
+  do_check_true(dir.exists());
+
+  // Modification time may be out by up to 2 seconds on FAT filesystems. Test
+  // with a bit of leeway, close enough probably means it is correct.
+  var diff = Math.abs(dir.lastModifiedTime - now);
+  do_check_true(diff < MAX_TIME_DIFFERENCE);
+
+  var yesterday = now - MILLIS_PER_DAY;
+  dir.lastModifiedTime = yesterday;
+
+  diff = Math.abs(dir.lastModifiedTime - yesterday);
+  do_check_true(diff < MAX_TIME_DIFFERENCE);
+
+  var tomorrow = now - MILLIS_PER_DAY;
+  dir.lastModifiedTime = tomorrow;
+
+  diff = Math.abs(dir.lastModifiedTime - tomorrow);
+  do_check_true(diff < MAX_TIME_DIFFERENCE);
+
+  dir.remove(true);
 }
 
