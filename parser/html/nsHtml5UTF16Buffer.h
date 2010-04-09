cdc7245b3044936ab19fb8d2acf5cdb049c7dfac
Henri Sivonen <hsivonen@iki.fi>
2010-04-09 18:48:29 +0300

    Sync the Java sources of the HTML5 parser with the parser repo. rs=sicking. NPOTB.

--- parser/html/nsHtml5UTF16Buffer.h
+++ parser/html/nsHtml5UTF16Buffer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Mozilla Foundation
+ * Copyright (c) 2008-2010 Mozilla Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a 
  * copy of this software and associated documentation files (the "Software"), 
