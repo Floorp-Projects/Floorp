10b84d40ce95f1071c78a8e593454b9512aaa546
Jonathan Kew <jfkthame@gmail.com>
2010-04-13 21:37:07 +0100

    bug 559075 - add local autorelease pool to SetupMacApplicationDelegate to prevent startup leakage. r=josh

--- toolkit/xre/MacApplicationDelegate.mm
+++ toolkit/xre/MacApplicationDelegate.mm
@@ -92,6 +92,10 @@ SetupMacApplicationDelegate()
 {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK;
 
+  // this is called during startup, outside an event loop, and therefore
+  // needs an autorelease pool to avoid cocoa object leakage (bug 559075)
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
   // This call makes it so that application:openFile: doesn't get bogus calls
   // from Cocoa doing its own parsing of the argument string. And yes, we need
   // to use a string with a boolean value in it. That's just how it works.
@@ -101,6 +105,8 @@ SetupMacApplicationDelegate()
   // Create the delegate. This should be around for the lifetime of the app.
   MacApplicationDelegate *delegate = [[MacApplicationDelegate alloc] init];
   [[NSApplication sharedApplication] setDelegate:delegate];
+
+  [pool release];
 
   NS_OBJC_END_TRY_ABORT_BLOCK;
 }
