43c899492b9d89aab02bed328c454facfa944f5f
Ben Turner <bent.mozilla@gmail.com>
2010-04-14 17:21:06 -0700

    Bug 559442 - 'Fix ctypes closures to work on a non-GC thread'. r=dwitte.

--- js/src/ctypes/CTypes.cpp
+++ js/src/ctypes/CTypes.cpp
@@ -57,6 +57,19 @@ namespace js {
 namespace ctypes {
 
 /*******************************************************************************
+** Helper classes
+*******************************************************************************/
+
+class ScopedContextThread
+{
+public:
+  ScopedContextThread(JSContext* cx) : mCx(cx) { JS_SetContextThread(cx); }
+  ~ScopedContextThread() { JS_ClearContextThread(mCx); }
+private:
+  JSContext* mCx;
+};
+
+/*******************************************************************************
 ** JSAPI function prototypes
 *******************************************************************************/
 
@@ -2589,7 +2602,9 @@ CType::FinalizeProtoClass(JSContext* cx, JSObject* obj)
   if (!JS_GetReservedSlot(cx, obj, SLOT_CLOSURECX, &slot) || JSVAL_IS_VOID(slot))
     return;
 
-  JS_DestroyContextNoGC(static_cast<JSContext*>(JSVAL_TO_PRIVATE(slot)));
+  JSContext* closureCx = static_cast<JSContext*>(JSVAL_TO_PRIVATE(slot));
+  JS_SetContextThread(closureCx);
+  JS_DestroyContextNoGC(closureCx);
 }
 
 void
@@ -4868,7 +4883,14 @@ CClosure::Create(JSContext* cx,
       JS_DestroyContextNoGC(cinfo->cx);
       return NULL;
     }
+
+    JS_ClearContextThread(cinfo->cx);
   }
+
+#ifdef DEBUG
+  // We want *this* context's thread here so use cx instead of cinfo->cx.
+  cinfo->cxThread = JS_GetContextThread(cx);
+#endif
 
   cinfo->closureObj = result;
   cinfo->typeObj = typeObj;
@@ -4959,10 +4981,10 @@ CClosure::ClosureStub(ffi_cif* cif, void* result, void** args, void* userData)
   JSObject* thisObj = cinfo->thisObj;
   JSObject* jsfnObj = cinfo->jsfnObj;
 
-#ifdef JS_THREADSAFE
+  ScopedContextThread scopedThread(cx);
+
   // Assert that we're on the thread we were created from.
-  JS_ASSERT(CURRENT_THREAD_IS_ME(cx->thread));
-#endif
+  JS_ASSERT(cinfo->cxThread == JS_GetContextThread(cx));
 
   JSAutoRequest ar(cx);
 
