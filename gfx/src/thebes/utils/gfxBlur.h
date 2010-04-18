21516fafe0e036baa08114a685445546d5170f99
Michael Ventnor <mventnor@mozilla.com>
2010-04-18 13:13:10 +1000

    Bug 544099 - Highly optimise -moz-box-shadow for common cases by allowing layout to specify an area where blurring is not needed r=roc,joe

--- gfx/src/thebes/utils/gfxBlur.h
+++ gfx/src/thebes/utils/gfxBlur.h
@@ -40,8 +40,8 @@
 
 #include "gfxContext.h"
 #include "gfxImageSurface.h"
-#include "gfxRect.h"
 #include "gfxTypes.h"
+#include "gfxThebesUtils.h"
 
 /**
  * Implementation of a box blur approximation of a Gaussian blur.
@@ -69,10 +69,15 @@ public:
      *
      * @param aDirtyRect A pointer to a dirty rect, measured in device units, if available.
      *  This will be used for optimizing the blur operation. It is safe to pass NULL here.
+     *
+     * @param aSkipRect A pointer to a rect, measured in device units, that represents an area
+     *  where blurring is unnecessary and shouldn't be done for speed reasons. It is safe to
+     *  pass NULL here.
      */
     gfxContext* Init(const gfxRect& aRect,
                      const gfxIntSize& aBlurRadius,
-                     const gfxRect* aDirtyRect);
+                     const gfxRect* aDirtyRect,
+                     const gfxRect* aSkipRect);
 
     /**
      * Returns the context that should be drawn to supply the alpha mask to be
@@ -126,7 +131,13 @@ protected:
      * mHasDirtyRect is TRUE.
      */
     gfxRect mDirtyRect;
-    PRBool mHasDirtyRect;
+    /**
+     * A rect indicating the area where blurring is unnecessary, and the blur
+     * algorithm should skip over it.
+     */
+    nsIntRect mSkipRect;
+
+    PRPackedBool mHasDirtyRect;
 };
 
 #endif /* GFX_BLUR_H */
