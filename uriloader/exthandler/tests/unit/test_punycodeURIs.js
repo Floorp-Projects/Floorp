29b1f5944676b7c58a404380da0b5fa23804dab0
Serge Gautherie <sgautherie.bz@free.fr>
2010-04-19 16:54:20 +0200

    Bug 456606 - test_punycodeURIs.js failure on Mac OS X 10.4 (currently worked around); (Av2) Remove a useless createInstance(), Whitespace cleanup, Remove MacOSX 10.4 workaround (on m-c). r=cbiesinger.

--- uriloader/exthandler/tests/unit/test_punycodeURIs.js
+++ uriloader/exthandler/tests/unit/test_punycodeURIs.js
@@ -46,15 +46,13 @@ var gCheckExistsAttempts = 0;
 
 function checkFile() {
   // This is where we expect the output
-  var tempFile = Components.classes["@mozilla.org/file/local;1"].
-    createInstance(Components.interfaces.nsILocalFile);
-  tempFile = do_get_cwd();
+  var tempFile = do_get_cwd();
   tempFile.append(kOutputFile);
 
   if (!tempFile.exists()) {
     if (gCheckExistsAttempts >= kMaxCheckExistAttempts) {
       do_throw("Expected File " + tempFile.path + " does not exist after " +
-	       kMaxCheckExistAttempts + " seconds");
+                 kMaxCheckExistAttempts + " seconds");
     }
     else {
       // Wait a bit longer then try again
@@ -63,12 +61,14 @@ function checkFile() {
   }
 
   // Now read it
-  var fstream = Components.classes["@mozilla.org/network/file-input-stream;1"].
-    createInstance(Components.interfaces.nsIFileInputStream);
-  var sstream = Components.classes["@mozilla.org/scriptableinputstream;1"].
-    createInstance(Components.interfaces.nsIScriptableInputStream);
+  var fstream =
+    Components.classes["@mozilla.org/network/file-input-stream;1"]
+              .createInstance(Components.interfaces.nsIFileInputStream);
+  var sstream =
+    Components.classes["@mozilla.org/scriptableinputstream;1"]
+              .createInstance(Components.interfaces.nsIScriptableInputStream);
   fstream.init(tempFile, -1, 0, 0);
-  sstream.init(fstream); 
+  sstream.init(fstream);
 
   // Read the first line only as that's the one we expect WriteArguments
   // to be writing the argument to.
@@ -92,25 +92,15 @@ function checkFile() {
 }
 
 function run_test() {
-  // don't run the test on Mac OS 10.4 - see bug 447999
-  const httpHandler =
-    Components.classes["@mozilla.org/network/protocol;1?name=http"]
-              .getService(Components.interfaces.nsIHttpProtocolHandler);
-  const oscpu = httpHandler.oscpu;
-  if (oscpu.match(/Mac OS X 10.4/)) {
-    dump("This test is not run on Mac OS 10.4 because it fails for unknown " +
-         "reasons. See bug 447999.\n");
-    return;
-  }
-
   // set up the uri to test with
-  var ioService = Components.classes["@mozilla.org/network/io-service;1"].
-    getService(Components.interfaces.nsIIOService);
+  var ioService =
+    Components.classes["@mozilla.org/network/io-service;1"]
+              .getService(Components.interfaces.nsIIOService);
 
   // set up the local handler object
-  var localHandler = 
-    Components.classes["@mozilla.org/uriloader/local-handler-app;1"].
-    createInstance(Components.interfaces.nsILocalHandlerApp);
+  var localHandler =
+    Components.classes["@mozilla.org/uriloader/local-handler-app;1"]
+              .createInstance(Components.interfaces.nsILocalHandlerApp);
   localHandler.name = "Test Local Handler App";
 
   // WriteArgument will just dump its arguments to a file for us.
@@ -129,8 +119,9 @@ function run_test() {
   outFile.append(kOutputFile);
 
   // Set an environment variable for WriteArgument to pick up
-  var envSvc = Components.classes["@mozilla.org/process/environment;1"].
-    getService(Components.interfaces.nsIEnvironment);
+  var envSvc =
+    Components.classes["@mozilla.org/process/environment;1"]
+              .getService(Components.interfaces.nsIEnvironment);
 
   // The Write Argument file needs to know where its libraries are, so
   // just force the path variable
