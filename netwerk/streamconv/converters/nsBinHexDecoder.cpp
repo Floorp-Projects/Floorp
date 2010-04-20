a00d94a3d6e514047e559ac04c3f1b78b25353fb
Mike Hommey <mh+mozilla@glandium.org>
2010-04-20 10:44:59 +0200

    Bug 502301 - Fix binhex_decode array signedness which broke builds on architectures where char is unsigned [r=benjamin]
    This was detected by the C++0x narrowing conversion inside {} compilation errors.

--- netwerk/streamconv/converters/nsBinHexDecoder.cpp
+++ netwerk/streamconv/converters/nsBinHexDecoder.cpp
@@ -98,7 +98,7 @@ NS_INTERFACE_MAP_END
 
 // The binhex 4.0 decoder table....
 
-static char binhex_decode[256] =
+static signed char binhex_decode[256] =
 {
   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
