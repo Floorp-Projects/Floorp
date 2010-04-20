bfa6500e5062a7843c21f98b24f2f3f911851ecd
Daniel Holbert <dholbert@cs.stanford.edu>
2010-04-20 11:50:39 -0700

    Bug 560378, Patch C: Make int<-->float conversions explicit to avoid MSVC compile warnings, in nsCanvasRenderingContext2D.cpp. r=vlad

--- content/canvas/src/nsCanvasRenderingContext2D.cpp
+++ content/canvas/src/nsCanvasRenderingContext2D.cpp
@@ -2065,7 +2065,7 @@ nsCanvasRenderingContext2D::SetFont(const nsAString& font)
     gfxFontStyle style(fontStyle->mFont.style,
                        fontStyle->mFont.weight,
                        fontStyle->mFont.stretch,
-                       NSAppUnitsToFloatPixels(fontSize, aupcp),
+                       NSAppUnitsToFloatPixels(fontSize, float(aupcp)),
                        language,
                        fontStyle->mFont.sizeAdjust,
                        fontStyle->mFont.systemFont,
@@ -3328,7 +3328,8 @@ nsCanvasRenderingContext2D::DrawWindow(nsIDOMWindow* aWindow, float aX, float aY
 
     // protect against too-large surfaces that will cause allocation
     // or overflow issues
-    if (!gfxASurface::CheckSurfaceSize(gfxIntSize(aW, aH), 0xffff))
+    if (!gfxASurface::CheckSurfaceSize(gfxIntSize(PRInt32(aW), PRInt32(aH)),
+                                       0xffff))
         return NS_ERROR_FAILURE;
 
     // We can't allow web apps to call this until we fix at least the
