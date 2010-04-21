c0e98ed6aece89afbd504cb9f5b25eb5cdd1c0e9
Doug Turner <dougt@dougt.org>
2010-04-21 04:26:01 -0700

    Fix for build bustage in necko

--- netwerk/protocol/http/src/nsHttpChannel.cpp
+++ netwerk/protocol/http/src/nsHttpChannel.cpp
@@ -44,6 +44,10 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+#ifdef MOZ_IPC
+#include "base/basictypes.h"
+#endif 
+
 #include "nsHttpChannel.h"
 #include "nsHttpHandler.h"
 #include "nsIHttpAuthenticator.h"
@@ -66,6 +70,8 @@
 #include "nsAuthInformationHolder.h"
 #include "nsICacheService.h"
 #include "nsDNSPrefetch.h"
+
+#include "nsIOService.h"
 
 // True if the local cache should be bypassed when processing a request.
 #define BYPASS_LOCAL_CACHE(loadFlags) \
