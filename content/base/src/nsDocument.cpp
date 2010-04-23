b2ffd64a7c435471808613bffc0e113fcd91e830
Brandon Sterne <bsterne@mozilla.com>
2010-04-23 08:45:40 -0700

    Bug 556625 - fix CSP report-only header, r=dveditz, a=bustage-fix

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -6795,7 +6795,7 @@ nsDocument::RetrieveRelevantHeaders(nsIChannel *aChannel)
       "refresh",
       "x-dns-prefetch-control",
       "x-content-security-policy",
-      "x-content-security-policy-read-only",
+      "x-content-security-policy-report-only",
       // add more http headers if you need
       // XXXbz don't add content-location support without reading bug
       // 238654 and its dependencies/dups first.
