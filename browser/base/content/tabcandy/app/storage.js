b27e7afe65a24030c5926538cb2925ed2df6ac2c
Ian Gilman <iangilman@gmail.com>
2010-04-22 17:19:42 -0700

    + We're now storing data in a file named after your candy, in a folder named 'tabcandy' inside your profile folder (e.g. tabcandy/revision-a.json). Currently the only data that's round-tripped is whether you have hidden the tab bar, but we are saving group data out (just not reading it yet). + The nav bar wasn't successfully hiding when first starting the browser; fixed.

--- browser/base/content/tabcandy/app/storage.js
+++ browser/base/content/tabcandy/app/storage.js
@@ -1,12 +1,55 @@
 // ##########
 Storage = {
+  // ----------
   init: function() {
+  },
+  
+  // ----------
+  read: function() {
+    var data = {};
+    var file = this.getFile();
+    if(file.exists()) {
+      var fstream = Components.classes["@mozilla.org/network/file-input-stream;1"].  
+                              createInstance(Components.interfaces.nsIFileInputStream);  
+      var cstream = Components.classes["@mozilla.org/intl/converter-input-stream;1"].  
+                              createInstance(Components.interfaces.nsIConverterInputStream);  
+      fstream.init(file, -1, 0, 0);  
+      cstream.init(fstream, "UTF-8", 0, 0); // you can use another encoding here if you wish  
+      
+      let (str = {}) {  
+        cstream.readString(-1, str); // read the whole file and put it in str.value  
+        if(str.value)
+          data = JSON.parse(str.value);  
+      }  
+      cstream.close(); // this closes fstream  
+    }
+   
+    return data;
+  },
+  
+  // ----------
+  write: function(data) {
+    var file = this.getFile();
+    var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].  
+                             createInstance(Components.interfaces.nsIFileOutputStream);  
+    foStream.init(file, 0x02 | 0x08 | 0x20, 0666, 0);   
+    var str = JSON.stringify(data);
+    foStream.write(str, str.length);
+    foStream.close();
+  },
+  
+  // ----------  
+  getFile: function() {
     var file = Components.classes["@mozilla.org/file/directory_service;1"].  
       getService(Components.interfaces.nsIProperties).  
       get("ProfD", Components.interfaces.nsIFile);  
-
-/*     var dir = Utils.getInstallDirectory('tabcandy@aza.raskin');   */
-    Utils.log(file); 
+      
+    file.append('tabcandy');
+    if(!file.exists())
+      file.create(Components.interfaces.nsIFile.DIRECTORY_TYPE, 0777);        
+      
+    file.append(Switch.name + '.json');
+    return file;
   }
 };
 
--- browser/base/content/tabcandy/app/switch.js
+++ browser/base/content/tabcandy/app/switch.js
@@ -1,40 +1,48 @@
 (function(){
 
-  window.Switch = {
-    insert: function(selector, style) {
-      var chunks = window.location.toString().split('/');
-      var myName = chunks[chunks.length - 2];
-      var html = '<div style="font-size: 11px; margin: 5px 0px 0px 5px;'
-        + style
-        + '">Candy: <select id="visualization-select">';
-        
-      var names = Utils.getVisualizationNames();
-      var count = names.length;
-      var a;
-      for(a = 0; a < count; a++) {
-        var name = names[a];
-        html += '<option value="'
-          + name
-          + '"'
-          + (name == myName ? ' selected="true"' : '')
-          + '>'
-          + name
-          + '</option>';
-      }
+// ##########
+window.Switch = {
+  // ----------
+  get name() {
+    var chunks = window.location.toString().split('/');
+    var myName = chunks[chunks.length - 2];
+    return myName;
+  },
       
-      html += '<option disabled="disabled">----------</option>';
-      html += '<option value="">Home</option>';
-
-      html += '</select>';
-      $(selector).prepend(html);
-      $('#visualization-select').change(function () {
-        var name = $(this).val();
-        if(name)
-          location.href = '../' + name + '/index.html';
-        else
-          location.href = '../../index.html';
-      });
+  // ----------
+  insert: function(selector, style) {
+    var myName = this.name;
+    var html = '<div style="font-size: 11px; margin: 5px 0px 0px 5px;'
+      + style
+      + '">Candy: <select id="visualization-select">';
+      
+    var names = Utils.getVisualizationNames();
+    var count = names.length;
+    var a;
+    for(a = 0; a < count; a++) {
+      var name = names[a];
+      html += '<option value="'
+        + name
+        + '"'
+        + (name == myName ? ' selected="true"' : '')
+        + '>'
+        + name
+        + '</option>';
     }
-  };
+    
+    html += '<option disabled="disabled">----------</option>';
+    html += '<option value="">Home</option>';
+
+    html += '</select>';
+    $(selector).prepend(html);
+    $('#visualization-select').change(function () {
+      var name = $(this).val();
+      if(name)
+        location.href = '../' + name + '/index.html';
+      else
+        location.href = '../../index.html';
+    });
+  }
+};
 
 })();
