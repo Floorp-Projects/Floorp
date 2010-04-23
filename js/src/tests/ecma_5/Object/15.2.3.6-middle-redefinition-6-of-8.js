ef40b312bb3c1292c0682a8d2c420acae6268ac8
Jeff Walden <jwalden@mit.edu>
2010-04-22 18:53:54 -0700

    Bug 560566 - Add this particular failure mode to the Object.defineProperty megatests.  r=jorendorff

---
+++ js/src/tests/ecma_5/Object/15.2.3.6-middle-redefinition-6-of-8.js
@@ -0,0 +1,5 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/licenses/publicdomain/
+
+load("ecma_5/Object/defineProperty-setup.js");
+runNonTerminalPropertyPresentTestsFraction(6, 8);
