3e6bc2c32db4f2dbe3ca23a69014df1f4f064138
Chris Jones <jones.chris.g@gmail.com>
2010-04-26 20:11:40 -0500

    Bug 555275: Implement a DeallocShmem() interface. r=bent

--- ipc/ipdl/ipdl/cxx/ast.py
+++ ipc/ipdl/ipdl/cxx/ast.py
@@ -657,6 +657,7 @@ class StmtDecl(Node):
     def __init__(self, decl, init=None, initargs=None):
         assert not (init and initargs)
         assert not isinstance(init, str) # easy to confuse with Decl
+        assert not isinstance(init, list)
         assert not isinstance(decl, tuple)
         
         Node.__init__(self)
