9907d2d6ff2e4e1a10992745e428d63ff69be715
Daniel Holbert <dholbert@cs.stanford.edu>
2010-04-28 16:00:53 -0700

    Bug 436418, patch C3: SVG/SMIL animateMotion - add "TreatSingleValueAsStatic" helper method, to assist nsSMILAnimationFunction subclasses in customizing animation behavior. r=roc

--- content/smil/nsSMILAnimationFunction.cpp
+++ content/smil/nsSMILAnimationFunction.cpp
@@ -248,8 +248,7 @@ nsSMILAnimationFunction::ComposeResult(const nsISMILAttr& aSMILAttr,
   nsSMILValue result(aResult.mType);
 
   if (mSimpleDuration.IsIndefinite() ||
-      (HasAttr(nsGkAtoms::values) && values.Length() == 1)) {
-
+      (values.Length() == 1 && TreatSingleValueAsStatic())) {
     // Indefinite duration or only one value set: Always set the first value
     result = values[0];
 
