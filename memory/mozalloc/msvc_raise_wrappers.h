ec4f8aa648be2bc0f02f14bf2c3c0506bf5d0a03
Chris Jones <jones.chris.g@gmail.com>
2010-04-28 19:38:41 -0500

    Bug 560723: Interpose non-throwing mozilla variants of VC10 STL functions that throw exceptions. r=ehsan sr=bsmedberg

--- memory/mozalloc/msvc_raise_wrappers.h
+++ memory/mozalloc/msvc_raise_wrappers.h
@@ -38,15 +38,46 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#ifndef mozilla_throw_msvc_h
-#define mozilla_throw_msvc_h
+#ifndef mozilla_msvc_raise_wrappers_h
+#define mozilla_msvc_throw_wrappers_h
 
-#if defined(MOZ_MSVC_STL_WRAP__RAISE)
-#  include "msvc_raise_wrappers.h"
-#elif defined(MOZ_MSVC_STL_WRAP__Throw)
-#  include "msvc_throw_wrapper.h"
-#else
-#  error "Unknown STL wrapper tactic"
+#ifdef _XSTDDEF_
+#  error "Unable to wrap _RAISE(); CRT _RAISE() already defined"
+#endif
+#ifdef _XUTILITY_
+#  error "Unabled to wrap _X[exception]"(); CRT versions already declared"
 #endif
 
-#endif  // mozilla_throw_msvc_h
+#include "mozilla/mozalloc_abort.h"
+
+namespace std {
+
+// NB: user code is not supposed to touch the std:: namespace.  We're
+// doing this after careful review because we want to define our own
+// exception throwing semantics.  Don't try this at home!
+
+MOZALLOC_EXPORT __declspec(noreturn) void moz_Xinvalid_argument(const char*);
+MOZALLOC_EXPORT __declspec(noreturn) void moz_Xlength_error(const char*);
+MOZALLOC_EXPORT __declspec(noreturn) void moz_Xout_of_range(const char*);
+MOZALLOC_EXPORT __declspec(noreturn) void moz_Xoverflow_error(const char*);
+MOZALLOC_EXPORT __declspec(noreturn) void moz_Xruntime_error(const char*);
+
+} // namespace std
+
+#ifndef MOZALLOC_DONT_WRAP_RAISE_FUNCTIONS
+
+#  define _Xinvalid_argument  moz_Xinvalid_argument
+#  define _Xlength_error      moz_Xlength_error
+#  define _Xout_of_range      moz_Xout_of_range
+#  define _Xoverflow_error    moz_Xoverflow_error
+#  define _Xruntime_error     moz_Xruntime_error
+
+#  include <xstddef>
+#  include <xutility>
+
+#  undef _RAISE
+#  define _RAISE(x) mozalloc_abort((x).what())
+
+#endif  // ifndef MOZALLOC_DONT_WRAP_RAISE_FUNCTIONS
+
+#endif  // ifndef mozilla_msvc_raise_wrappers_h
