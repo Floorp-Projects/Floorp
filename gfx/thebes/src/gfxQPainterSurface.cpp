b150d375c89b3579593e444802f8443e86609063
Oleg Romashin <romaxa@gmail.com>
2010-04-29 06:25:02 -0400

    Bug 561891 - QPainter is is busted after last cairo merge. r=jmuizelaar.

--- gfx/thebes/src/gfxQPainterSurface.cpp
+++ gfx/thebes/src/gfxQPainterSurface.cpp
@@ -40,11 +40,11 @@
 #include "gfxQPainterSurface.h"
 #include "gfxImageSurface.h"
 
-#include "cairo-qpainter.h"
+#include "cairo-qt.h"
 
 gfxQPainterSurface::gfxQPainterSurface(QPainter *painter)
 {
-    cairo_surface_t *csurf = cairo_qpainter_surface_create (painter);
+    cairo_surface_t *csurf = cairo_qt_surface_create (painter);
 
     mPainter = painter;
 
@@ -53,27 +53,27 @@ gfxQPainterSurface::gfxQPainterSurface(QPainter *painter)
 
 gfxQPainterSurface::gfxQPainterSurface(const gfxIntSize& size, gfxImageFormat format)
 {
-    cairo_surface_t *csurf = cairo_qpainter_surface_create_with_qimage ((cairo_format_t) format,
+    cairo_surface_t *csurf = cairo_qt_surface_create_with_qimage ((cairo_format_t) format,
                                                                         size.width,
                                                                         size.height);
-    mPainter = cairo_qpainter_surface_get_qpainter (csurf);
+    mPainter = cairo_qt_surface_get_qpainter (csurf);
 
     Init (csurf);
 }
 
 gfxQPainterSurface::gfxQPainterSurface(const gfxIntSize& size, gfxContentType content)
 {
-    cairo_surface_t *csurf = cairo_qpainter_surface_create_with_qpixmap ((cairo_content_t) content,
+    cairo_surface_t *csurf = cairo_qt_surface_create_with_qpixmap ((cairo_content_t) content,
                                                                          size.width,
                                                                          size.height);
-    mPainter = cairo_qpainter_surface_get_qpainter (csurf);
+    mPainter = cairo_qt_surface_get_qpainter (csurf);
 
     Init (csurf);
 }
 
 gfxQPainterSurface::gfxQPainterSurface(cairo_surface_t *csurf)
 {
-    mPainter = cairo_qpainter_surface_get_qpainter (csurf);
+    mPainter = cairo_qt_surface_get_qpainter (csurf);
 
     Init(csurf, PR_TRUE);
 }
@@ -88,7 +88,7 @@ gfxQPainterSurface::GetQImage()
     if (!mSurfaceValid)
         return nsnull;
 
-    return cairo_qpainter_surface_get_qimage(CairoSurface());
+    return cairo_qt_surface_get_qimage(CairoSurface());
 }
 
 already_AddRefed<gfxImageSurface>
@@ -97,7 +97,7 @@ gfxQPainterSurface::GetImageSurface()
     if (!mSurfaceValid)
         return nsnull;
 
-    cairo_surface_t *isurf = cairo_qpainter_surface_get_image(CairoSurface());
+    cairo_surface_t *isurf = cairo_qt_surface_get_image(CairoSurface());
     if (!isurf)
         return nsnull;
 
