e36598229f389ee97db086cfe91a0368bfb48c41
Vivien Nicolas <21@vingtetun.org>
2010-04-30 01:00:27 -0400

    Bug 562366 - revise dialog styling to match new context and site menus [r=mfinkle]

--- mobile/chrome/content/prompt/prompt.js
+++ mobile/chrome/content/prompt/prompt.js
@@ -31,79 +31,65 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-var PromptHelper = {
-    
-    closeDialog : function(confirm, id) {
-      let dialog = document.getElementById(id);
-      if (typeof confirm == "boolean" && dialog.arguments && "defaultButton" in dialog.arguments)
-        // confirmEx always returns 1 when dismissed with "escape" (bug 345067).
-        dialog.arguments.result = confirm ? dialog.arguments.defaultButton : 1;
-      else
-        dialog.arguments.result = confirm;
-      dialog.close();
-    },
-    
-    //
-    // alert dialog
-    //
-    onCloseAlert: function(dialog) {
-        if (dialog.arguments) {
-            dialog.arguments.value = document.getElementById("prompt-alert-checkbox").checked;
-        }
-    },
-    
-    //
-    // confirm dialog
-    //
-    closeConfirm : function(confirm) {
-        this.closeDialog(confirm, "prompt-confirm-dialog");
-    },
-    
-    onCloseConfirm: function(dialog) {
-        if (dialog.arguments && ("checkbox" in dialog.arguments)) {
-            dialog.arguments.checkbox.value = document.getElementById("prompt-confirm-checkbox").checked;
-        }
-    },
-    
-    //
-    // prompt dialog
-    //
-    closePrompt : function(confirm) {
-        this.closeDialog(confirm, "prompt-prompt-dialog");
-    },
-    
-    onClosePrompt: function(dialog) {
-        if (dialog.arguments) {
-            dialog.arguments.checkbox.value = document.getElementById("prompt-prompt-checkbox").checked;
-            dialog.arguments.value.value = document.getElementById("prompt-prompt-textbox").value;
-        }
-    },
-    
-    //
-    // user / password dialog
-    //
-    closePassword : function(confirm) {
-        this.closeDialog(confirm, "prompt-password-dialog");
-    },
-    
-    onClosePassword: function(dialog) {
-        if (dialog.arguments) {
-            dialog.arguments.checkbox.value = document.getElementById("prompt-password-checkbox").checked;
-            dialog.arguments.user.value = document.getElementById("prompt-password-user").value;
-            dialog.arguments.password.value = document.getElementById("prompt-password-password").value;
-        }
-    },
-    
-    //
-    // select dialog
-    //
-    closeSelect: function(confirm) {
-        this.closeDialog(confirm, "prompt-select-dialog");
-    },
-    
-    onCloseSelect: function(dialog) {
-        if (dialog.arguments) {
-            dialog.arguments.selection.value = document.getElementById("prompt-select-list").selectedIndex;
-        }
+const PromptHelper = {
+  closeDialog: function(confirm, id) {
+    let dialog = document.getElementById(id);
+    if (typeof confirm == "boolean" && dialog.arguments && "defaultButton" in dialog.arguments)
+      // confirmEx always returns 1 when dismissed with "escape" (bug 345067).
+      dialog.arguments.result = confirm ? dialog.arguments.defaultButton : 1;
+    else
+      dialog.arguments.result = confirm;
+    dialog.close();
+  },
+
+  // Alert dialog
+  onCloseAlert: function(dialog) {
+    if (dialog.arguments)
+      dialog.arguments.value = document.getElementById("prompt-alert-checkbox").checked;
+  },
+
+  // Confirm dialog
+  closeConfirm: function(confirm) {
+    this.closeDialog(confirm, "prompt-confirm-dialog");
+  },
+
+  onCloseConfirm: function(dialog) {
+    if (dialog.arguments && ("checkbox" in dialog.arguments))
+      dialog.arguments.checkbox.value = document.getElementById("prompt-confirm-checkbox").checked;
+  },
+
+  // Prompt dialog
+  closePrompt: function(confirm) {
+    this.closeDialog(confirm, "prompt-prompt-dialog");
+  },
+
+  onClosePrompt: function(dialog) {
+    if (dialog.arguments) {
+      dialog.arguments.checkbox.value = document.getElementById("prompt-prompt-checkbox").checked;
+      dialog.arguments.value.value = document.getElementById("prompt-prompt-textbox").value;
     }
-}
+  },
+
+  // User / Password dialog
+  closePassword: function(confirm) {
+    this.closeDialog(confirm, "prompt-password-dialog");
+  },
+
+  onClosePassword: function(dialog) {
+    if (dialog.arguments) {
+      dialog.arguments.checkbox.value = document.getElementById("prompt-password-checkbox").checked;
+      dialog.arguments.user.value = document.getElementById("prompt-password-user").value;
+      dialog.arguments.password.value = document.getElementById("prompt-password-password").value;
+    }
+  },
+
+  // Select dialog
+  closeSelect: function(confirm) {
+    this.closeDialog(confirm, "prompt-select-dialog");
+  },
+
+  onCloseSelect: function(dialog) {
+    if (dialog.arguments)
+      dialog.arguments.selection.value = document.getElementById("prompt-select-list").selectedIndex;
+  }
+};
