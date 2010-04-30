7d68b79556cd3de969925e8972dd1bf44b147d32
Matt Brubeck <mbrubeck@mozilla.org>
2010-04-30 15:30:28 -0400

    Bug 562849 - Reverse meanings of N900 volume buttons in portrait mode [r=mfinkle]

--- mobile/chrome/content/browser-ui.js
+++ mobile/chrome/content/browser-ui.js
@@ -799,6 +799,8 @@ var BrowserUI = {
       case "cmd_sanitize":
       case "cmd_zoomin":
       case "cmd_zoomout":
+      case "cmd_volumeLeft":
+      case "cmd_volumeRight":
       case "cmd_lockscreen":
         isSupported = true;
         break;
@@ -905,6 +907,14 @@ var BrowserUI = {
         break;
       case "cmd_zoomout":
         Browser.zoom(1);
+        break;
+      case "cmd_volumeLeft":
+        // Zoom in (portrait) or out (landscape)
+        Browser.zoom(Util.isPortrait() ? -1 : 1);
+        break;
+      case "cmd_volumeRight":
+        // Zoom out (portrait) or in (landscape)
+        Browser.zoom(Util.isPortrait() ? 1 : -1);
         break;
       case "cmd_lockscreen":
       {
