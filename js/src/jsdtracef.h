fba7b65b4b9b78955443c0bfa0716cb03030036b
Chris Leary <cdleary@mozilla.com>
2010-04-30 17:04:31 -0700

    Bug 562144: Make DTrace hooks pretty. (r=sayrer)

--- js/src/jsdtracef.h
+++ js/src/jsdtracef.h
@@ -33,7 +33,9 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+#ifdef INCLUDE_MOZILLA_DTRACE
 #include "javascript-trace.h"
+#endif
 #include "jspubtd.h"
 #include "jsprvtd.h"
 
@@ -78,4 +80,91 @@ jsdtrace_execute_done(JSScript *script);
 
 JS_END_EXTERN_C
 
+namespace js {
+
+class DTrace {
+  public:
+    /*
+     * If |lval| is provided to the enter/exit methods, it is tested to see if
+     * it is a function as a predicate to the dtrace event emission.
+     */
+    static void enterJSFun(const JSContext *cx, const JSStackFrame *fp, const JSFunction *fun,
+                           const JSStackFrame *dfp, jsuint argc, const jsval *argv,
+                           const jsval *lval = NULL);
+    static void exitJSFun(const JSContext *cx, const JSStackFrame *fp, const JSFunction *fun,
+                          const jsval *rval, const jsval *lval = NULL);
+
+    static void finalizeObject(const JSObject *obj);
+
+    class ExecutionScope {
+        const JSScript *script;
+        void startExecution();
+        void endExecution();
+      public:
+        explicit ExecutionScope(const JSScript *script) : script(script) { startExecution(); }
+        ~ExecutionScope() { endExecution(); }
+    };
+
+};
+
+inline void
+DTrace::enterJSFun(const JSContext *cx, const JSStackFrame *fp, const JSFunction *fun,
+                   const JSStackFrame *dfp, jsuint argc, const jsval *argv,
+                   const jsval *lval)
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (!lval || VALUE_IS_FUNCTION(cx, lval)) {
+        if (JAVASCRIPT_FUNCTION_ENTRY_ENABLED())
+            jsdtrace_function_entry(cx, &frame, fun);
+        if (JAVASCRIPT_FUNCTION_INFO_ENABLED())
+            jsdtrace_function_info(cx, &frame, frame.down, fun);
+        if (JAVASCRIPT_FUNCTION_ARGS_ENABLED())
+            jsdtrace_function_args(cx, &frame, fun, frame.argc, frame.argv);
+    }
+#endif
+}
+
+inline void
+DTrace::exitJSFun(const JSContext *cx, const JSStackFrame *fp, const JSFunction *fun,
+                  const jsval *rval, const jsval *lval)
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (!lval || VALUE_IS_FUNCTION(cx, lval)) {
+        if (JAVASCRIPT_FUNCTION_RVAL_ENABLED())
+            jsdtrace_function_rval(cx, fp, fun, rval);
+        if (JAVASCRIPT_FUNCTION_RETURN_ENABLED())
+            jsdtrace_function_return(cx, fp, fun);
+    }
+#endif
+}
+
+inline void
+DTrace::finalizeObject(const JSObject *obj)
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (JAVASCRIPT_OBJECT_FINALIZE_ENABLED())
+        jsdtrace_object_finalize(obj);
+#endif
+}
+
+inline void
+DTrace::ExecutionScope::startExecution()
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (JAVASCRIPT_EXECUTE_START_ENABLED())
+        jsdtrace_execute_start(script);
+#endif
+}
+
+inline void
+DTrace::ExecutionScope::endExecution()
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (JAVASCRIPT_EXECUTE_END_ENABLED())
+        jsdtrace_execute_done(script);
+#endif
+}
+
+} /* namespace js */
+    
 #endif /* _JSDTRACE_H */
