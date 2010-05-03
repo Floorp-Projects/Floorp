7700eca9a327fe6118d74f0ab231c82b8c43c955
Vivien Nicolas <21@vingtetun.org>
2010-05-03 11:42:56 -0400

    Bug 562981 - Mouse click event in iframe has wrong coordinates [r=mfinkle]

--- mobile/chrome/tests/browser_click_content.js
+++ mobile/chrome/tests/browser_click_content.js
@@ -3,6 +3,7 @@ let testURL_click = "chrome://mochikit/content/browser/mobile/chrome/browser_cli
 let newTab;
 let element;
 let isClickFired = false;
+let clickPosition = { x: null, y: null};
 
 //------------------------------------------------------------------------------
 // Entry point (must be named "test")
@@ -15,14 +16,17 @@ function test() {
   ok(newTab, "Tab Opened");
 
   // Wait for tab load (need to check the tab "loading", not the document "loading")
-  waitFor(testClick, function() { return newTab.isLoading() == false; });
+  waitFor(testClickAndPosition, function() { return newTab.isLoading() == false; });
 }
 
-function clickFired() {
+function clickFired(aEvent) {
   isClickFired = true;
+  let [x, y] = Browser.browserViewToClient(aEvent.clientX, aEvent.clientY);
+  clickPosition.x = x;
+  clickPosition.y = y;
 }
 
-function testClick() {
+function testClickAndPosition() {
   // Do sanity tests
   let uri = newTab.browser.currentURI.spec;
   is(uri, testURL_click, "URL Matches newly created Tab");
@@ -30,21 +34,37 @@ function testClick() {
   // Check click
   element = newTab.browser.contentDocument.querySelector("iframe");
   element.addEventListener("click", clickFired, true);
+
   EventUtils.synthesizeMouseForContent(element, 1, 1, {}, window);
   waitFor(checkClick, function() { return isClickFired });
 }
 
 function checkClick() {
   ok(isClickFired, "Click handler fired");
+  element.removeEventListener("click", clickFired, true);
+
+  // Check position
+  isClickFired = false;
+  element = newTab.browser.contentDocument.documentElement;
+  element.addEventListener("click", clickFired, true);
+  let rect = Browser.getBoundingContentRect(element);
+  EventUtils.synthesizeMouse(element, 1, rect.height + 10, {}, window);
+  waitFor(checkPosition, function() { return isClickFired });
+}
+
+function checkPosition() {
+  element.removeEventListener("click", clickFired, true);
+
+  let rect = Browser.getBoundingContentRect(element);
+  is(clickPosition.x, 1, "X position is correct");
+  is(clickPosition.y, rect.height + 10, "Y position is correct");
+
   close();
 }
 
 function close() {
   // Close the tab
   Browser.closeTab(newTab);
-
-  // Remove the listener
-  element.removeEventListener("click", clickFired, true);
 
   // We must finialize the tests
   finish();
