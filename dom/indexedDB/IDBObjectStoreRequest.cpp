1ce0a632a2a8017cc06e688d696efd420d2b6499
Ben Turner <bent.mozilla@gmail.com>
2010-05-04 13:25:16 -0700

    Add Get/Put on IDBObjectStoreRequest, move database files into subfolders based on origin

--- dom/indexedDB/IDBObjectStoreRequest.cpp
+++ dom/indexedDB/IDBObjectStoreRequest.cpp
@@ -37,43 +37,98 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+// XXX remove once we can get jsvals out of nsIVariant
+#include "jscntxt.h"
+#include "jsapi.h"
+#include "nsContentUtils.h"
+#include "nsJSON.h"
+#include "IDBEvents.h"
+
 #include "IDBObjectStoreRequest.h"
-#include "AsyncConnectionHelper.h"
+
 #include "nsIIDBDatabaseError.h"
 
 #include "nsDOMClassInfo.h"
 #include "nsThreadUtils.h"
 #include "mozilla/Storage.h"
 
+#include "AsyncConnectionHelper.h"
+
 USING_INDEXEDDB_NAMESPACE
 
 namespace {
 
-class PutObjectStoreHelper : public AsyncConnectionHelper
+class PutHelper : public AsyncConnectionHelper
 {
 public:
-  PutObjectStoreHelper(IDBDatabaseRequest* aDatabase,
-                       nsIDOMEventTarget* aTarget,
-                       PRInt64 aObjectStoreID,
-                       nsIVariant* aValue,
-                       nsIVariant* aKey,
-                       bool aAutoIncrement,
-                       bool aNoOverwrite)
+  PutHelper(IDBDatabaseRequest* aDatabase,
+            nsIDOMEventTarget* aTarget,
+            PRInt64 aObjectStoreID,
+            const nsAString& aValue,
+            nsIVariant* aKey,
+            bool aAutoIncrement,
+            bool aNoOverwrite)
   : AsyncConnectionHelper(aDatabase, aTarget), mOSID(aObjectStoreID),
     mValue(aValue), mKey(aKey), mAutoIncrement(aAutoIncrement),
     mNoOverwrite(aNoOverwrite)
   { }
 
-  nsresult Init();
   PRUint16 DoDatabaseWork();
   void GetSuccessResult(nsIWritableVariant* aResult);
 
 private:
+  // In-params.
   const PRInt64 mOSID;
-  nsCOMPtr<nsIVariant> mValue;
+  nsString mValue;
   nsCOMPtr<nsIVariant> mKey;
   const bool mAutoIncrement;
   const bool mNoOverwrite;
+};
+
+class GetHelper : public AsyncConnectionHelper
+{
+public:
+  GetHelper(IDBDatabaseRequest* aDatabase,
+            nsIDOMEventTarget* aTarget,
+            PRInt64 aObjectStoreID,
+            nsIVariant* aKey,
+            bool aAutoIncrement)
+  : AsyncConnectionHelper(aDatabase, aTarget), mOSID(aObjectStoreID),
+    mKey(aKey), mAutoIncrement(aAutoIncrement)
+  { }
+
+  PRUint16 DoDatabaseWork();
+  PRUint16 OnSuccess(nsIDOMEventTarget* aTarget);
+
+  // Disabled until we can use nsIVariants with jsvals
+  //void GetSuccessResult(nsIWritableVariant* aResult);
+
+private:
+  // In-params.
+  const PRInt64 mOSID;
+  nsCOMPtr<nsIVariant> mKey;
+  const bool mAutoIncrement;
+
+  // Out-params.
+  nsString mValue;
+};
+
+// Remove once nsIVariant can handle jsvals
+class GetSuccessEvent : public IDBSuccessEvent
+{
+public:
+  GetSuccessEvent(const nsAString& aValue)
+  : mValue(aValue)
+  { }
+
+  NS_IMETHOD GetResult(nsIVariant** aResult);
+
+  void Init() {
+    IDBSuccessEvent::Init();
+  }
+
+private:
+  nsString mValue;
 };
 
 } // anonymous namespace
@@ -84,7 +139,8 @@ IDBObjectStoreRequest::Create(IDBDatabaseRequest* aDatabase,
                               const nsAString& aName,
                               const nsAString& aKeyPath,
                               PRBool aAutoIncrement,
-                              PRUint16 aMode)
+                              PRUint16 aMode,
+                              PRInt64 aId)
 {
   NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
 
@@ -100,24 +156,7 @@ IDBObjectStoreRequest::Create(IDBDatabaseRequest* aDatabase,
   }
   objectStore->mAutoIncrement = aAutoIncrement;
   objectStore->mMode = aMode;
-
-  return objectStore.forget();
-}
-
-// static
-already_AddRefed<IDBObjectStoreRequest>
-IDBObjectStoreRequest::Create(IDBDatabaseRequest* aDatabase,
-                              const nsAString& aName,
-                              PRUint16 aMode)
-{
-  NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
-
-  nsRefPtr<IDBObjectStoreRequest> objectStore = new IDBObjectStoreRequest();
-
-  objectStore->mDatabase = aDatabase;
-  objectStore->mName.Assign(aName);
-  objectStore->mKeyPath.SetIsVoid(PR_TRUE);
-  objectStore->mMode = aMode;
+  objectStore->mId = aId;
 
   return objectStore.forget();
 }
@@ -133,31 +172,6 @@ IDBObjectStoreRequest::IDBObjectStoreRequest()
 IDBObjectStoreRequest::~IDBObjectStoreRequest()
 {
   NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
-}
-
-void
-IDBObjectStoreRequest::SetId(PRInt64 aId)
-{
-  NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
-  NS_ASSERTION(mId == LL_MININT, "Overwriting mId!");
-  mId = aId;
-}
-
-void
-IDBObjectStoreRequest::SetKeyPath(const nsAString& aKeyPath)
-{
-  NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
-  NS_ASSERTION(mKeyPath.IsVoid(), "Overwriting mKeyPath!");
-  if (!aKeyPath.IsVoid()) {
-    mKeyPath.Assign(aKeyPath);
-  }
-}
-
-void
-IDBObjectStoreRequest::SetAutoIncrement(PRBool aAutoIncrement)
-{
-  NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
-  mAutoIncrement = aAutoIncrement;
 }
 
 NS_IMPL_ADDREF(IDBObjectStoreRequest)
@@ -220,13 +234,66 @@ IDBObjectStoreRequest::GetIndexNames(nsIDOMDOMStringList** aIndexNames)
 }
 
 NS_IMETHODIMP
-IDBObjectStoreRequest::Put(nsIVariant* aValue,
+IDBObjectStoreRequest::Put(nsIVariant* /* aValue */,
                            nsIVariant* aKey,
                            PRBool aNoOverwrite,
                            nsIIDBRequest** _retval)
 {
   NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
-  return NS_ERROR_NOT_IMPLEMENTED;
+
+  // This is the slow path, need to do this better once nsIVariants can have
+  // raw jsvals inside them.
+  NS_WARNING("Using a slow path for Put! Fix this now!");
+
+  nsIXPConnect* xpc = nsContentUtils::XPConnect();
+  NS_ENSURE_TRUE(xpc, NS_ERROR_UNEXPECTED);
+
+  nsAXPCNativeCallContext* cc;
+  nsresult rv = xpc->GetCurrentNativeCallContext(&cc);
+  NS_ENSURE_SUCCESS(rv, rv);
+  NS_ENSURE_TRUE(cc, NS_ERROR_UNEXPECTED);
+
+  PRUint32 argc;
+  rv = cc->GetArgc(&argc);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  if (argc < 2) {
+    return NS_ERROR_XPC_NOT_ENOUGH_ARGS;
+  }
+
+  jsval* argv;
+  rv = cc->GetArgvPtr(&argv);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  JSContext* cx;
+  rv = cc->GetJSContext(&cx);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  JSAutoRequest ar(cx);
+
+  js::AutoValueRooter clone(cx);
+  rv = nsContentUtils::CreateStructuredClone(cx, argv[0], clone.addr());
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+
+  nsCOMPtr<nsIJSON> json(new nsJSON());
+
+  nsString jsonString;
+  rv = json->EncodeFromJSVal(clone.addr(), cx, jsonString);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsRefPtr<IDBRequest> request = GenerateRequest();
+  NS_ENSURE_TRUE(request, NS_ERROR_FAILURE);
+
+  nsRefPtr<PutHelper> helper =
+    new PutHelper(mDatabase, request, mId, jsonString, aKey, mAutoIncrement,
+                  aNoOverwrite);
+  rv = helper->Dispatch(mDatabase->ConnectionThread());
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  request.forget(_retval);
+  return NS_OK;
 }
 
 NS_IMETHODIMP
@@ -242,7 +309,17 @@ IDBObjectStoreRequest::Get(nsIVariant* aKey,
                            nsIIDBRequest** _retval)
 {
   NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
-  return NS_ERROR_NOT_IMPLEMENTED;
+
+  nsRefPtr<IDBRequest> request = GenerateRequest();
+  NS_ENSURE_TRUE(request, NS_ERROR_FAILURE);
+
+  nsRefPtr<GetHelper> helper =
+    new GetHelper(mDatabase, request, mId, aKey, mAutoIncrement);
+  nsresult rv = helper->Dispatch(mDatabase->ConnectionThread());
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  request.forget(_retval);
+  return NS_OK;
 }
 
 NS_IMETHODIMP
@@ -254,22 +331,14 @@ IDBObjectStoreRequest::OpenCursor(nsIIDBKeyRange* aRange,
   return NS_ERROR_NOT_IMPLEMENTED;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-//// PutObjectStoreHelper
-
-nsresult
-PutObjectStoreHelper::Init()
-{
-  return NS_OK;
-}
-
 PRUint16
-PutObjectStoreHelper::DoDatabaseWork()
+PutHelper::DoDatabaseWork()
 {
   nsresult rv = mDatabase->EnsureConnection();
   NS_ENSURE_SUCCESS(rv, nsIIDBDatabaseError::UNKNOWN_ERR);
 
   nsCOMPtr<mozIStorageConnection> connection = mDatabase->Connection();
+  return OK;
 
   // Rollback on any errors.
   mozStorageTransaction transaction(connection, PR_FALSE);
@@ -313,7 +382,60 @@ PutObjectStoreHelper::DoDatabaseWork()
 }
 
 void
-PutObjectStoreHelper::GetSuccessResult(nsIWritableVariant* aResult)
+PutHelper::GetSuccessResult(nsIWritableVariant* aResult)
 {
-  // XXX implement me
+  aResult->SetFromVariant(mKey);
+}
+
+PRUint16
+GetHelper::DoDatabaseWork()
+{
+  // Put value into mValue.
+  mValue.AssignLiteral("\"foo\"");
+  return OK;
+}
+
+PRUint16
+GetHelper::OnSuccess(nsIDOMEventTarget* aTarget)
+{
+  nsRefPtr<GetSuccessEvent> event(new GetSuccessEvent(mValue));
+  event->Init();
+
+  PRBool dummy;
+  aTarget->DispatchEvent(static_cast<nsDOMEvent*>(event), &dummy);
+  return OK;
+}
+
+// Remove once nsIVariant supports jsvals!
+NS_IMETHODIMP
+GetSuccessEvent::GetResult(nsIVariant** aResult)
+{
+  // This is the slow path, need to do this better once nsIVariants can have
+  // raw jsvals inside them.
+  NS_WARNING("Using a slow path for Get! Fix this now!");
+
+  nsIXPConnect* xpc = nsContentUtils::XPConnect();
+  NS_ENSURE_TRUE(xpc, NS_ERROR_UNEXPECTED);
+
+  nsAXPCNativeCallContext* cc;
+  nsresult rv = xpc->GetCurrentNativeCallContext(&cc);
+  NS_ENSURE_SUCCESS(rv, rv);
+  NS_ENSURE_TRUE(cc, NS_ERROR_UNEXPECTED);
+
+  jsval* retval;
+  rv = cc->GetRetValPtr(&retval);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  JSContext* cx;
+  rv = cc->GetJSContext(&cx);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  JSAutoRequest ar(cx);
+
+  nsCOMPtr<nsIJSON> json(new nsJSON());
+  rv = json->DecodeToJSVal(mValue, cx, retval);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  cc->SetReturnValueWasSet(PR_TRUE);
+  return NS_OK;
 }
