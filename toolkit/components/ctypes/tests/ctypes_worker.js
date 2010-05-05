4cc867a178497641270b742cb7105ef4579dddf0
Jeff Walden <jwalden@mit.edu>
2010-05-05 14:35:12 -0700

    Bug 552560 - Reenable the ctypes test in chrome workers, and make all parent-checks only happen if we're running in xpcshell and have access to the parent(o) function.  r=dwitte

--- toolkit/components/ctypes/tests/ctypes_worker.js
+++ toolkit/components/ctypes/tests/ctypes_worker.js
@@ -40,9 +40,7 @@ importScripts("xpcshellTestHarnessAdaptor.js");
 
 onmessage = function(event) {
   _WORKINGDIR_ = event.data;
-if(false){
   importScripts("test_jsctypes.js");
   run_test();
-}
   postMessage("Done!");
 }
