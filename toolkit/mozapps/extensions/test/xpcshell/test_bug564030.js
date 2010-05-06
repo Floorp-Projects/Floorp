1a66111d58012d7a7fa2fbb3a08581b059d051ca
Dave Townsend <dtownsend@oxymoronical.com>
2010-05-06 12:45:56 -0700

    Bug 564030: Updating an add-on that is incompatible to a version that is compatible requires more restarts. r=robstrong

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_bug564030.js
@@ -0,0 +1,65 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+// Tests that upgrading an incompatible add-on to a compatible one forces an
+// EM restart
+
+const profileDir = gProfD.clone();
+profileDir.append("extensions");
+
+function run_test() {
+  do_test_pending();
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "2", "1.9.2");
+
+  dest = profileDir.clone();
+  dest.append("addon1@tests.mozilla.org");
+  writeInstallRDFToDir({
+    id: "addon1@tests.mozilla.org",
+    version: "1.0",
+    name: "Test",
+    targetApplications: [{
+      id: "xpcshell@tests.mozilla.org",
+      minVersion: "1",
+      maxVersion: "1"
+    }]
+  }, dest);
+  // Attempt to make this look like it was added some time in the past so
+  // the update makes the last modified time change.
+  dest.lastModifiedTime -= 5000;
+
+  startupManager(1);
+
+  AddonManager.getAddonByID("addon1@tests.mozilla.org", function(a) {
+    do_check_neq(a, null);
+    do_check_eq(a.version, "1.0");
+    do_check_false(a.userDisabled);
+    do_check_true(a.appDisabled);
+    do_check_false(a.isActive);
+    do_check_false(isExtensionInAddonsList(profileDir, a.id));
+
+    writeInstallRDFToDir({
+      id: "addon1@tests.mozilla.org",
+      version: "2.0",
+      name: "Test",
+      targetApplications: [{
+        id: "xpcshell@tests.mozilla.org",
+        minVersion: "1",
+        maxVersion: "2"
+      }]
+    }, dest);
+
+    restartManager(1);
+
+    AddonManager.getAddonByID("addon1@tests.mozilla.org", function(a) {
+      do_check_neq(a, null);
+      do_check_eq(a.version, "2.0");
+      do_check_false(a.userDisabled);
+      do_check_false(a.appDisabled);
+      do_check_true(a.isActive);
+      do_check_true(isExtensionInAddonsList(profileDir, a.id));
+
+      do_test_finished();
+    });
+  });
+}
