e0e949e522c96f3661b2c0acc9f7bceb3bf99f51
Jason Orendorff <jorendorff@mozilla.com>
2010-05-10 18:43:34 -0500

    Bug 564344 - Crash or "Assertion failure: slot < (this)->scope()->freeslot, at ../jsscope.h:563" @ JSObject::getSlot(unsigned int) when redefining property. r=brendan.
    --HG--
    extra : transplant_source : %9A%DA%EE%06%F2V%B6%C6%03x%26%985%BA%CF%D0%BE%A88Q

---
+++ js/src/jsapi-tests/testDefineProperty.cpp
@@ -0,0 +1,20 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=4 sw=4 et tw=99:
+ */
+
+#include "tests.h"
+
+BEGIN_TEST(testDefineProperty_bug564344)
+{
+    jsvalRoot x(cx);
+    EVAL("function f() {}\n"
+         "var x = {p: f};\n"
+         "x.p();  // brand x's scope\n"
+         "x;", x.addr());
+
+    JSObject *obj = JSVAL_TO_OBJECT(x.value());
+    for (int i = 0; i < 2; i++)
+        CHECK(JS_DefineProperty(cx, obj, "q", JSVAL_VOID, NULL, NULL, JSPROP_SHARED));
+    return true;
+}
+END_TEST(testDefineProperty_bug564344)
