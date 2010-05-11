37cd68aba5607b657c0f0b5f0401e3fcce19a991
Robert Sayre <sayrer@gmail.com>
2010-05-11 11:52:17 -0400

    Backed out changeset 919950c7f0f0

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -101,13 +101,12 @@ using namespace js;
 static JS_REQUIRES_STACK JSBool
 AllocateAfterSP(JSContext *cx, jsval *sp, uintN nslots)
 {
-    jsval *avail = (jsval *) cx->stackPool.getCurrent()->getAvail();
-    JS_ASSERT((jsval *) cx->stackPool.getCurrent()->getBase() <= sp);
-    JS_ASSERT(sp <= avail);
-
-    uintN surplus = avail - sp;
+    uintN surplus;
     jsval *sp2;
 
+    JS_ASSERT((jsval *) cx->stackPool.current->base <= sp);
+    JS_ASSERT(sp <= (jsval *) cx->stackPool.current->avail);
+    surplus = (jsval *) cx->stackPool.current->avail - sp;
     if (nslots <= surplus)
         return JS_TRUE;
 
@@ -115,10 +114,11 @@ AllocateAfterSP(JSContext *cx, jsval *sp, uintN nslots)
      * No room before current->avail, check if the arena has enough space to
      * fit the missing slots before the limit.
      */
-    if (nslots > (size_t) ((jsval *) cx->stackPool.getCurrent()->getLimit() - sp))
+    if (nslots > (size_t) ((jsval *) cx->stackPool.current->limit - sp))
         return JS_FALSE;
 
-    cx->stackPool.allocateCast<jsval *>(sp2, (nslots - surplus) * sizeof(jsval));
+    JS_ARENA_ALLOCATE_CAST(sp2, jsval *, &cx->stackPool,
+                           (nslots - surplus) * sizeof(jsval));
     JS_ASSERT(sp2 == sp + surplus);
     return JS_TRUE;
 }
@@ -131,10 +131,11 @@ js_AllocRawStack(JSContext *cx, uintN nslots, void **markp)
     JS_ASSERT(nslots != 0);
     JS_ASSERT_NOT_ON_TRACE(cx);
 
-    if (!cx->stackPool.getSecond()) {
+    if (!cx->stackPool.first.next) {
         int64 *timestamp;
 
-        cx->stackPool.allocateCast<int64 *>(timestamp, sizeof *timestamp);
+        JS_ARENA_ALLOCATE_CAST(timestamp, int64 *,
+                               &cx->stackPool, sizeof *timestamp);
         if (!timestamp) {
             js_ReportOutOfScriptQuota(cx);
             return NULL;
@@ -143,8 +144,8 @@ js_AllocRawStack(JSContext *cx, uintN nslots, void **markp)
     }
 
     if (markp)
-        *markp = cx->stackPool.getMark();
-    cx->stackPool.allocateCast<jsval *>(sp, nslots * sizeof(jsval));
+        *markp = JS_ARENA_MARK(&cx->stackPool);
+    JS_ARENA_ALLOCATE_CAST(sp, jsval *, &cx->stackPool, nslots * sizeof(jsval));
     if (!sp)
         js_ReportOutOfScriptQuota(cx);
     return sp;
@@ -153,19 +154,20 @@ js_AllocRawStack(JSContext *cx, uintN nslots, void **markp)
 JS_STATIC_INTERPRET JS_REQUIRES_STACK void
 js_FreeRawStack(JSContext *cx, void *mark)
 {
-    cx->stackPool.release(mark);
+    JS_ARENA_RELEASE(&cx->stackPool, mark);
 }
 
 JS_REQUIRES_STACK JS_FRIEND_API(jsval *)
 js_AllocStack(JSContext *cx, uintN nslots, void **markp)
 {
     jsval *sp;
+    JSArena *a;
     JSStackHeader *sh;
 
     /* Callers don't check for zero nslots: we do to avoid empty segments. */
     if (nslots == 0) {
         *markp = NULL;
-        return (jsval *) cx->stackPool.getMark();
+        return (jsval *) JS_ARENA_MARK(&cx->stackPool);
     }
 
     /* Allocate 2 extra slots for the stack segment header we'll likely need. */
@@ -174,12 +176,12 @@ js_AllocStack(JSContext *cx, uintN nslots, void **markp)
         return NULL;
 
     /* Try to avoid another header if we can piggyback on the last segment. */
-    JSArena *a = cx->stackPool.getCurrent();
+    a = cx->stackPool.current;
     sh = cx->stackHeaders;
     if (sh && JS_STACK_SEGMENT(sh) + sh->nslots == sp) {
         /* Extend the last stack segment, give back the 2 header slots. */
         sh->nslots += nslots;
-        a->setAvail(a->getAvail() - 2 * sizeof(jsval));
+        a->avail -= 2 * sizeof(jsval);
     } else {
         /*
          * Need a new stack segment, so allocate and push a stack segment
@@ -223,7 +225,7 @@ js_FreeStack(JSContext *cx, void *mark)
         cx->stackHeaders = sh->down;
 
     /* Release the stackPool space allocated since mark was set. */
-    cx->stackPool.release(mark);
+    JS_ARENA_RELEASE(&cx->stackPool, mark);
 }
 
 JSObject *
@@ -582,11 +584,11 @@ js_Invoke(JSContext *cx, uintN argc, jsval *vp, uintN flags)
     JS_ASSERT(argc <= JS_ARGS_LENGTH_MAX);
 
     /* [vp .. vp + 2 + argc) must belong to the last JS stack arena. */
-    JS_ASSERT((jsval *) cx->stackPool.getCurrent()->getBase() <= vp);
-    JS_ASSERT(vp + 2 + argc <= (jsval *) cx->stackPool.getCurrent()->getAvail());
+    JS_ASSERT((jsval *) cx->stackPool.current->base <= vp);
+    JS_ASSERT(vp + 2 + argc <= (jsval *) cx->stackPool.current->avail);
 
     /* Mark the top of stack and load frequently-used registers. */
-    mark = cx->stackPool.getMark();
+    mark = JS_ARENA_MARK(&cx->stackPool);
     MUST_FLOW_THROUGH("out2");
     v = *vp;
 
@@ -849,7 +851,7 @@ out:
 
 out2:
     /* Pop everything we may have allocated off the stack. */
-    cx->stackPool.release(mark);
+    JS_ARENA_RELEASE(&cx->stackPool, mark);
     if (!ok)
         *vp = JSVAL_NULL;
     return ok;
--- js/src/jsopcode.cpp
+++ js/src/jsopcode.cpp
@@ -539,9 +539,9 @@ SprintEnsureBuffer(Sprinter *sp, size_t len)
         return JS_TRUE;
     base = sp->base;
     if (!base) {
-        sp->pool->allocateCast<char *>(base, nb);
+        JS_ARENA_ALLOCATE_CAST(base, char *, sp->pool, nb);
     } else {
-        sp->pool->growCast<char *>(base, sp->size, nb);
+        JS_ARENA_GROW_CAST(base, char *, sp->pool, sp->size, nb);
     }
     if (!base) {
         js_ReportOutOfScriptQuota(sp->context);
@@ -717,11 +717,11 @@ js_QuoteString(JSContext *cx, JSString *str, jschar quote)
     char *bytes;
     JSString *escstr;
 
-    mark = cx->tempPool.getMark();
+    mark = JS_ARENA_MARK(&cx->tempPool);
     INIT_SPRINTER(cx, &sprinter, &cx->tempPool, 0);
     bytes = QuoteString(&sprinter, str, quote);
     escstr = bytes ? JS_NewStringCopyZ(cx, bytes) : NULL;
-    cx->tempPool.release(mark);
+    JS_ARENA_RELEASE(&cx->tempPool, mark);
     return escstr;
 }
 
@@ -751,7 +751,7 @@ js_NewPrinter(JSContext *cx, const char *name, JSFunction *fun,
     if (!jp)
         return NULL;
     INIT_SPRINTER(cx, &jp->sprinter, &jp->pool, 0);
-    jp->pool.init(name, 256, 1, &cx->scriptStackQuota);
+    JS_InitArenaPool(&jp->pool, name, 256, 1, &cx->scriptStackQuota);
     jp->indent = indent;
     jp->pretty = !!pretty;
     jp->grouped = !!grouped;
@@ -774,7 +774,7 @@ js_NewPrinter(JSContext *cx, const char *name, JSFunction *fun,
 void
 js_DestroyPrinter(JSPrinter *jp)
 {
-    jp->pool.finish();
+    JS_FinishArenaPool(&jp->pool);
     jp->sprinter.context->free(jp);
 }
 
@@ -790,7 +790,7 @@ js_GetPrinterOutput(JSPrinter *jp)
     str = JS_NewStringCopyZ(cx, jp->sprinter.base);
     if (!str)
         return NULL;
-    jp->pool.free();
+    JS_FreeArenaPool(&jp->pool);
     INIT_SPRINTER(cx, &jp->sprinter, &jp->pool, 0);
     return str;
 }
@@ -1760,7 +1760,7 @@ InitSprintStack(JSContext *cx, SprintStack *ss, JSPrinter *jp, uintN depth)
     /* Allocate the parallel (to avoid padding) offset and opcode stacks. */
     offsetsz = depth * sizeof(ptrdiff_t);
     opcodesz = depth * sizeof(jsbytecode);
-    cx->tempPool.allocate(space, offsetsz + opcodesz);
+    JS_ARENA_ALLOCATE(space, &cx->tempPool, offsetsz + opcodesz);
     if (!space) {
         js_ReportOutOfScriptQuota(cx);
         return JS_FALSE;
@@ -4050,7 +4050,7 @@ Decompile(SprintStack *ss, jsbytecode *pc, intN nb, JSOp nextop)
                      * Therefore after InitSprintStack succeeds, we must
                      * release to mark before returning.
                      */
-                    mark = cx->tempPool.getMark();
+                    mark = JS_ARENA_MARK(&cx->tempPool);
                     if (!fun->hasLocalNames()) {
                         innerLocalNames = NULL;
                     } else {
@@ -4060,7 +4060,7 @@ Decompile(SprintStack *ss, jsbytecode *pc, intN nb, JSOp nextop)
                     }
                     inner = fun->u.i.script;
                     if (!InitSprintStack(cx, &ss2, jp, StackDepth(inner))) {
-                        cx->tempPool.release(mark);
+                        JS_ARENA_RELEASE(&cx->tempPool, mark);
                         return NULL;
                     }
                     ss2.inGenExp = JS_TRUE;
@@ -4084,7 +4084,7 @@ Decompile(SprintStack *ss, jsbytecode *pc, intN nb, JSOp nextop)
                     jp->fun = outerfun;
                     jp->localNames = outerLocalNames;
                     if (!ok) {
-                        cx->tempPool.release(mark);
+                        JS_ARENA_RELEASE(&cx->tempPool, mark);
                         return NULL;
                     }
 
@@ -4150,7 +4150,7 @@ Decompile(SprintStack *ss, jsbytecode *pc, intN nb, JSOp nextop)
                      * from cx's tempPool.
                      */
                     rval = JS_strdup(cx, PopStr(&ss2, op));
-                    cx->tempPool.release(mark);
+                    JS_ARENA_RELEASE(&cx->tempPool, mark);
                     if (!rval)
                         return NULL;
                     todo = SprintCString(&ss->sprinter, rval);
@@ -4832,7 +4832,7 @@ DecompileCode(JSPrinter *jp, JSScript *script, jsbytecode *pc, uintN len,
 
     /* Initialize a sprinter for use with the offset stack. */
     cx = jp->sprinter.context;
-    mark = cx->tempPool.getMark();
+    mark = JS_ARENA_MARK(&cx->tempPool);
     ok = InitSprintStack(cx, &ss, jp, depth);
     if (!ok)
         goto out;
@@ -4886,7 +4886,7 @@ DecompileCode(JSPrinter *jp, JSScript *script, jsbytecode *pc, uintN len,
 
 out:
     /* Free all temporary stuff allocated under this call. */
-    cx->tempPool.release(mark);
+    JS_ARENA_RELEASE(&cx->tempPool, mark);
     return ok;
 }
 
@@ -5003,7 +5003,7 @@ js_DecompileFunction(JSPrinter *jp)
 #if JS_HAS_DESTRUCTURING
         ss.printer = NULL;
         jp->script = script;
-        mark = jp->sprinter.context->tempPool.getMark();
+        mark = JS_ARENA_MARK(&jp->sprinter.context->tempPool);
 #endif
 
         for (i = 0; i < fun->nargs; i++) {
@@ -5054,7 +5054,7 @@ js_DecompileFunction(JSPrinter *jp)
 
 #if JS_HAS_DESTRUCTURING
         jp->script = NULL;
-        jp->sprinter.context->tempPool.release(mark);
+        JS_ARENA_RELEASE(&jp->sprinter.context->tempPool, mark);
 #endif
         if (!ok)
             return JS_FALSE;
