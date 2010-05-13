91404dc4b672c35471c7b7432393de7546b6ca95
Robert O'Callahan <robert@ocallahan.org>
2010-05-13 14:52:39 +1200

    Back out part of fix for bug 538062 to fix accessibility tests
    --HG--
    extra : rebase_source : ffdabcf1caa16e68fa654e4f2830ffd235be91be

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -3911,8 +3911,9 @@ nsCSSFrameConstructor::CreateAnonymousFrames(nsFrameConstructorState& aState,
 
     nsIFrame* newFrame = creator->CreateFrameFor(content);
     if (newFrame) {
-      NS_ASSERTION(content->GetPrimaryFrame(),
-                   "Content must have a primary frame now");
+// See bug 565569
+//      NS_ASSERTION(content->GetPrimaryFrame(),
+//                   "Content must have a primary frame now");
       aChildItems.AddChild(newFrame);
     }
     else {
