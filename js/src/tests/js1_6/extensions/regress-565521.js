dd7e232a0fdd5c615743a92c1a125a40d7ea61b9
Blake Kaplan <mrbkap@gmail.com>
2010-05-12 18:18:54 -0700

    Bug 565521 - Add a test. r=sayrer

---
+++ js/src/tests/js1_6/extensions/regress-565521.js
@@ -0,0 +1,41 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/licenses/publicdomain/
+ * Contributor: Blake Kaplan
+ */
+
+var gTestfile = 'regress-565521.js';
+
+//-----------------------------------------------------------------------------
+var BUGNUMBER = 565521;
+var summary = 'for-each getter calling';
+var actual = '';
+var expect = 'PASS';
+
+
+//-----------------------------------------------------------------------------
+test();
+//-----------------------------------------------------------------------------
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  var proto = { get prop() { return (this === proto) ? "FAIL" : "PASS"; } };
+  var obj = Object.create(proto);
+
+  var itercount = 0;
+  for each (var i in obj) {
+      ++itercount;
+      actual = i;
+  }
+
+  reportCompare(itercount, 1, summary + ': right itercount')
+  reportCompare(expect, actual, summary + ': right value');
+
+  exitFunc ('test');
+}
+
