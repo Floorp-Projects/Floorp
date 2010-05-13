d38d1a7f69b888a7700d064df0f6e50ab3e07cdc
Chris Jones <jones.chris.g@gmail.com>
2010-05-13 17:24:30 -0500

    Backed out changeset eef17c173aaa

--- js/src/xpconnect/src/XPCNativeWrapper.cpp
+++ js/src/xpconnect/src/XPCNativeWrapper.cpp
@@ -1255,8 +1255,6 @@ JSObject *
 XPCNativeWrapper::GetNewOrUsed(JSContext *cx, XPCWrappedNative *wrapper,
                                JSObject *scope, nsIPrincipal *aObjectPrincipal)
 {
-  CheckWindow(wrapper);
-
   if (aObjectPrincipal) {
     nsIScriptSecurityManager *ssm = GetSecurityManager();
 
