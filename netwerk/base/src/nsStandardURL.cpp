10228ae1850d0dddad345471a166cf0c22957a74
Karl Tomlinson <karlt+@karlt.net>
2010-05-13 14:34:38 +1200

    b=559492 only dump leaked URLs on successful shutdown r=dbaron

--- netwerk/base/src/nsStandardURL.cpp
+++ netwerk/base/src/nsStandardURL.cpp
@@ -343,7 +343,6 @@ nsStandardURL::InitGlobalObjects()
 
 #ifdef DEBUG_DUMP_URLS_AT_SHUTDOWN
     PR_INIT_CLIST(&gAllURLs);
-    atexit(DumpLeakedURLs);
 #endif
 }
 
@@ -352,6 +351,11 @@ nsStandardURL::ShutdownGlobalObjects()
 {
     NS_IF_RELEASE(gIDN);
     NS_IF_RELEASE(gCharsetMgr);
+
+#ifdef DEBUG_DUMP_URLS_AT_SHUTDOWN
+    if (gInitialized)
+        atexit(DumpLeakedURLs);
+#endif
 }
 
 //----------------------------------------------------------------------------
