95f9bb24ac47d74e9d950f16303946787ec613dc
Robert O'Callahan <robert@ocallahan.org>
2010-05-13 14:21:53 +1200

    Backed out changeset 054698c2cb8f

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -346,9 +346,8 @@ nsDisplayList::ComputeVisibility(nsDisplayListBuilder* aBuilder,
     // Record bounds of moving visible items in movingContentAccumulatedBounds.
     // We do not need to add items that are uniform across the entire visible
     // area, since they have no visible movement.
-    nscolor color;
     if (isMoving &&
-        !(item->IsUniform(aBuilder, &color) &&
+        !(item->IsUniform(aBuilder) &&
           bounds.Contains(aVisibleRegion->GetBounds()) &&
           bounds.Contains(aVisibleRegionBeforeMove->GetBounds()))) {
       if (movingContentAccumulatedBounds.IsEmpty()) {
@@ -571,28 +570,6 @@ CreateEmptyThebesLayer(nsDisplayListBuilder* aBuilder,
   return itemGroup;
 }
 
-static PRBool
-IsAllUniform(nsDisplayListBuilder* aBuilder, nsDisplayList::ItemGroup* aGroup,
-             nscolor* aColor)
-{
-  nsRect visibleRect = aGroup->mStartItem->GetVisibleRect();
-  nscolor finalColor = NS_RGBA(0,0,0,0);
-  for (nsDisplayList::ItemGroup* group = aGroup; group;
-       group = group->mNextItemsForLayer) {
-    for (nsDisplayItem* item = group->mStartItem; item != group->mEndItem;
-         item = item->GetAbove()) {
-      nscolor color;
-      if (visibleRect != item->GetVisibleRect())
-        return PR_FALSE;
-      if (!item->IsUniform(aBuilder, &color))
-        return PR_FALSE;
-      finalColor = NS_ComposeColors(finalColor, color);
-    }
-  }
-  *aColor = finalColor;
-  return PR_TRUE;
-}
-
 /**
  * This is the heart of layout's integration with layers. We
  * use a ClippedItemIterator to iterate through descendant display
@@ -703,15 +680,6 @@ void nsDisplayList::BuildLayers(nsDisplayListBuilder* aBuilder,
 
   for (PRUint32 i = 0; i < aLayers->Length(); ++i) {
     LayerItems* layerItems = &aLayers->ElementAt(i);
-
-    nscolor color;
-    if (layerItems->mThebesLayer &&
-        IsAllUniform(aBuilder, layerItems->mItems, &color)) {
-      layerItems->mThebesLayer = nsnull;
-      nsRefPtr<ColorLayer> layer = aManager->CreateColorLayer();
-      layer->SetColor(gfxRGBA(color));
-      layerItems->mLayer = layer.forget();
-    }
 
     gfxMatrix transform;
     nsIntRect visibleRect = layerItems->mVisibleRect;
@@ -1131,7 +1099,7 @@ nsDisplayBackground::IsOpaque(nsDisplayListBuilder* aBuilder) {
 }
 
 PRBool
-nsDisplayBackground::IsUniform(nsDisplayListBuilder* aBuilder, nscolor* aColor) {
+nsDisplayBackground::IsUniform(nsDisplayListBuilder* aBuilder) {
   // theme background overrides any other background
   if (mIsThemed)
     return PR_FALSE;
@@ -1139,18 +1107,14 @@ nsDisplayBackground::IsUniform(nsDisplayListBuilder* aBuilder, nscolor* aColor) 
   nsStyleContext *bgSC;
   PRBool hasBG =
     nsCSSRendering::FindBackground(mFrame->PresContext(), mFrame, &bgSC);
-  if (!hasBG) {
-    aColor = NS_RGBA(0,0,0,0);
+  if (!hasBG)
     return PR_TRUE;
-  }
   const nsStyleBackground* bg = bgSC->GetStyleBackground();
   if (bg->BottomLayer().mImage.IsEmpty() &&
       bg->mImageCount == 1 &&
       !nsLayoutUtils::HasNonZeroCorner(mFrame->GetStyleBorder()->mBorderRadius) &&
-      bg->BottomLayer().mClip == NS_STYLE_BG_CLIP_BORDER) {
-    *aColor = bg->mBackgroundColor;
+      bg->BottomLayer().mClip == NS_STYLE_BG_CLIP_BORDER)
     return PR_TRUE;
-  }
   return PR_FALSE;
 }
 
@@ -1462,7 +1426,7 @@ nsDisplayWrapList::IsOpaque(nsDisplayListBuilder* aBuilder) {
   return PR_FALSE;
 }
 
-PRBool nsDisplayWrapList::IsUniform(nsDisplayListBuilder* aBuilder, nscolor* aColor) {
+PRBool nsDisplayWrapList::IsUniform(nsDisplayListBuilder* aBuilder) {
   // We could try to do something but let's conservatively just return PR_FALSE.
   return PR_FALSE;
 }
@@ -2032,12 +1996,12 @@ PRBool nsDisplayTransform::IsOpaque(nsDisplayListBuilder *aBuilder)
  * wrapped list is uniform.  See IsOpaque for discussion of why this
  * works.
  */
-PRBool nsDisplayTransform::IsUniform(nsDisplayListBuilder *aBuilder, nscolor* aColor)
+PRBool nsDisplayTransform::IsUniform(nsDisplayListBuilder *aBuilder)
 {
   const nsStyleDisplay* disp = mFrame->GetStyleDisplay();
   return disp->mTransform.GetMainMatrixEntry(1) == 0.0f &&
     disp->mTransform.GetMainMatrixEntry(2) == 0.0f &&
-    mStoredList.IsUniform(aBuilder, aColor);
+    mStoredList.IsUniform(aBuilder);
 }
 
 /* If UNIFIED_CONTINUATIONS is defined, we can merge two display lists that
