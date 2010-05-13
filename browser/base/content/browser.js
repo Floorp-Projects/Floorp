70ec02bf8067a8d083726086029c8eb89abd51d8
Gavin Sharp <gavin@gavinsharp.com>
2010-05-13 16:36:43 -0400

    Backed out changeset f551d1be254e (inspector-0.1, fixing commit message)

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -51,7 +51,6 @@
 #   Dietrich Ayala <dietrich@mozilla.com>
 #   Gavin Sharp <gavin@gavinsharp.com>
 #   Justin Dolske <dolske@mozilla.com>
-#   Rob Campbell <rcampbell@mozilla.com>
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -129,7 +128,6 @@ let gInitialPages = [
 ];
 
 #include browser-fullZoom.js
-#include inspector.js
 #include browser-places.js
 #include browser-tabPreviews.js
 
