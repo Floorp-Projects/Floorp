13ee8f43a4496b5b86873b6a4c9ebce899090256
Mark Banner <bugzilla@standard8.plus.com>
2010-05-14 09:07:36 +0100

    Bug 530079 Catch and handle nicely 'malformed URI sequence when migrating passwords from the legacy storage to the new. r=dolske

--- toolkit/components/passwordmgr/test/unit/test_storage_legacy_4.js
+++ toolkit/components/passwordmgr/test/unit/test_storage_legacy_4.js
@@ -89,7 +89,7 @@ Cm.nsIComponentRegistrar.registerFactory(NEWSBOXPH_CID,
 try {
 /* ========== 0 ========== */
 var testnum = 0;
-var testdesc = "Initial connection to storage module"
+var testdesc = "Initial connection to storage module";
 
 var storage = Cc["@mozilla.org/login-manager/storage/legacy;1"].
               createInstance(Ci.nsILoginManagerStorage);
@@ -142,6 +142,8 @@ testnum++;
 testdesc = "checking reading of mailnews-like old logins";
 storage = LoginTest.initStorage(INDIR, "signons-403790.txt",
                                OUTDIR, "output-403790.txt");
+// signons-403790.txt has one extra login that is invalid, and hence isn't
+// shown here.
 LoginTest.checkStorageData(storage, [], [dummyuser1, dummyuser2, dummyuser3,
                                          dummyuser5, dummyuser6]);
 
