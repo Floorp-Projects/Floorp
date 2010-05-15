14ac18f0309ee363e0c1732e9f920fadaa07a9c2
Aza Raskin <aza@mozilla.com>
2010-05-14 17:23:21 -0700

    + Fixed a bug whereby hitting return in the group name would zoom in on the selected tab.

--- browser/base/content/tabcandy/app/ui.js
+++ browser/base/content/tabcandy/app/ui.js
@@ -219,15 +219,15 @@ window.Page = {
           self.setActiveTab(nextTab);           
         }
         e.preventDefault();               
-      }      
-    });
-    
-    $(window).keyup(function(e){
-      // If you hit escape or return, zoom into the active tab,
-      // but only if a title or other element isn't focused.
+      }
+      
+      Utils.log('hizzzzz', $(":focus").length );
       if((e.which == 27 || e.which == 13) && $(":focus").length == 0 )
         if( self.getActiveTab() ) self.getActiveTab().zoom();
+      
+       
     });
+    
   },
     
   // ----------  
