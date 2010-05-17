12c3df0484ded4991f37d57abbdec194cea5bbe0
Benjamin Smedberg <benjamin@smedbergs.us>
2010-05-17 11:33:34 -0500

    Bug 558986 - Fix for crashes in TSF with Flash/Silverlight on tablets (OLE/COM nested event loops). r=cjones, jimm, ehsan.

--- ipc/chromium/src/base/message_pump_win.cc
+++ ipc/chromium/src/base/message_pump_win.cc
@@ -97,6 +97,10 @@ void MessagePumpForUI::ScheduleWork() {
 
   // Make sure the MessagePump does some work for us.
   PostMessage(message_hwnd_, kMsgHaveWork, reinterpret_cast<WPARAM>(this), 0);
+
+  // In order to wake up any cross-process COM calls which may currently be
+  // pending on the main thread, we also have to post a UI message.
+  PostMessage(message_hwnd_, WM_NULL, NULL, 0);
 }
 
 void MessagePumpForUI::ScheduleDelayedWork(const Time& delayed_work_time) {
