a28f3011b620613d81849e5163b65b31154880b8
Vladimir Vukicevic <vladimir@pobox.com>
2010-05-18 10:07:43 -0700

    followup fix for when webgl is not supported; files removed, forgot to remove this

--- content/canvas/src/WebGLContextNotSupported.cpp
+++ content/canvas/src/WebGLContextNotSupported.cpp
@@ -37,25 +37,10 @@
  * ***** END LICENSE BLOCK ***** */
 
 #include "nsICanvasRenderingContextWebGL.h"
-#include "WebGLArray.h"
 #include "nsDOMClassInfoID.h"
 
 #define DUMMY(func,rtype)  nsresult func (rtype ** aResult) { return NS_ERROR_FAILURE; }
 
 DUMMY(NS_NewCanvasRenderingContextWebGL, nsICanvasRenderingContextWebGL)
-DUMMY(NS_NewWebGLFloatArray, nsISupports)
-DUMMY(NS_NewWebGLByteArray, nsISupports)
-DUMMY(NS_NewWebGLUnsignedByteArray, nsISupports)
-DUMMY(NS_NewWebGLShortArray, nsISupports)
-DUMMY(NS_NewWebGLUnsignedShortArray, nsISupports)
-DUMMY(NS_NewWebGLIntArray, nsISupports)
-DUMMY(NS_NewWebGLUnsignedIntArray, nsISupports)
-DUMMY(NS_NewWebGLArrayBuffer, nsISupports)
 
 DOMCI_DATA(CanvasRenderingContextWebGL, void)
-DOMCI_DATA(WebGLBuffer, void)
-DOMCI_DATA(WebGLTexture, void)
-DOMCI_DATA(WebGLProgram, void)
-DOMCI_DATA(WebGLShader, void)
-DOMCI_DATA(WebGLFramebuffer, void)
-DOMCI_DATA(WebGLRenderbuffer, void)
