52adda535c43b32b0e10d066c919bee2f78fc763
Rich Dougherty <rich@rd.gen.nz>
2010-05-19 15:04:33 +1200

    Bug 557982. Use Image objects in the video frame queue so we can avoid making an extra copy as we put frames into the queue. r=kinetik

--- content/media/ogg/nsOggCodecState.cpp
+++ content/media/ogg/nsOggCodecState.cpp
@@ -61,9 +61,19 @@ static PRBool AddOverflow(PRInt64 a, PRInt64 b, PRInt64& aResult);
 // in an integer overflow.
 static PRBool MulOverflow(PRInt64 a, PRInt64 b, PRInt64& aResult);
 
-// Defined in nsOggReader.cpp.
-extern PRBool MulOverflow32(PRUint32 a, PRUint32 b, PRUint32& aResult);
-
+static PRBool MulOverflow32(PRUint32 a, PRUint32 b, PRUint32& aResult)
+{
+  // 32 bit integer multiplication with overflow checking. Returns PR_TRUE
+  // if the multiplication was successful, or PR_FALSE if the operation resulted
+  // in an integer overflow.
+  PRUint64 a64 = a;
+  PRUint64 b64 = b;
+  PRUint64 r64 = a64 * b64;
+  if (r64 > PR_UINT32_MAX)
+     return PR_FALSE;
+  aResult = static_cast<PRUint32>(r64);
+  return PR_TRUE;
+}
 
 nsOggCodecState*
 nsOggCodecState::Create(ogg_page* aPage)
@@ -179,6 +189,7 @@ PRBool nsTheoraState::Init() {
   mFrameDuration = static_cast<PRUint32>(f);
 
   n = mInfo.aspect_numerator;
+
   d = mInfo.aspect_denominator;
   mAspectRatio = (n == 0 || d == 0) ?
     1.0f : static_cast<float>(n) / static_cast<float>(d);
