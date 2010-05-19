7d9b1bce816ade4e1cac6fa7d694274978847440
Vladimir Vukicevic <vladimir@pobox.com>
2010-05-19 13:46:08 -0700

    b=561168, removed too much from the NotSupported webgl file, putting it back

--- content/canvas/src/WebGLContextNotSupported.cpp
+++ content/canvas/src/WebGLContextNotSupported.cpp
@@ -44,3 +44,9 @@
 DUMMY(NS_NewCanvasRenderingContextWebGL, nsICanvasRenderingContextWebGL)
 
 DOMCI_DATA(CanvasRenderingContextWebGL, void)
+DOMCI_DATA(WebGLBuffer, void)
+DOMCI_DATA(WebGLTexture, void)
+DOMCI_DATA(WebGLProgram, void)
+DOMCI_DATA(WebGLShader, void)
+DOMCI_DATA(WebGLFramebuffer, void)
+DOMCI_DATA(WebGLRenderbuffer, void)
