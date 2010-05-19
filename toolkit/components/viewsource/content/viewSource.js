b441a7ac767ee8d2f341d390a924216c01daf3b6
Ehsan Akhgari <ehsan@mozilla.com>
2010-05-19 10:48:30 -0400

    Back out changeset 6fc5d661ca55 because of a11y test failures

--- toolkit/components/viewsource/content/viewSource.js
+++ toolkit/components/viewsource/content/viewSource.js
@@ -394,10 +394,8 @@ var PrintPreviewListener = {
       browser = document.createElement("browser");
       browser.setAttribute("id", "ppBrowser");
       browser.setAttribute("flex", "1");
-
-      // XXX: Bug 566849.
-      let browserBottomBox = document.getElementById("browser-bottombox");
-      browserBottomBox.insertBefore(browser, browserBottomBox.firstChild);
+      document.getElementById("appcontent").
+        insertBefore(browser, document.getElementById("FindToolbar"));
     }
     return browser;
   },
