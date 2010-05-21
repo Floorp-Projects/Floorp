049e1a0d665f3c2600f47ffcabdc448f21f1dac2
Jason Duell <jduell.mcbugs@gmail.com>
2010-05-21 16:18:19 -0700

    Bug 567428: Turn e10s/necko on by default.  r=dwitte

--- netwerk/cookie/test/unit_ipc/head_ipc_setup.js
+++ netwerk/cookie/test/unit_ipc/head_ipc_setup.js
@@ -1,11 +1,3 @@
-
-/** 
- * Turn on e10s networking (god help us)
- */
-Components.classes["@mozilla.org/process/environment;1"]
-          .getService(Components.interfaces.nsIEnvironment)
-          .set("NECKO_E10S_HTTP", "1");
-
 // If using NSPR logging, create child log as "${NSPR_LOG_FILE}.child" 
 // - TODO: remove when bug 534764 is fixed
 var env = Components.classes["@mozilla.org/process/environment;1"]
--- netwerk/test/unit_ipc/head_ipc_setup.js
+++ netwerk/test/unit_ipc/head_ipc_setup.js
@@ -1,9 +1,7 @@
-
-/** 
- * Turn on e10s IPC-based HTTP for tests in this directory
- * - Someday this will not be needed, once IPC HTTP is the default.
- */
-Components.classes["@mozilla.org/process/environment;1"]
-          .getService(Components.interfaces.nsIEnvironment)
-          .set("NECKO_E10S_HTTP", "1");
-
+// If using NSPR logging, create child log as "${NSPR_LOG_FILE}.child" 
+// - TODO: remove when bug 534764 is fixed
+var env = Components.classes["@mozilla.org/process/environment;1"]
+          .getService(Components.interfaces.nsIEnvironment);
+var log = env.get("NSPR_LOG_FILE");
+if (log) 
+  env.set("NSPR_LOG_FILE", log + ".child");
