06256fe30ff4714af1bc6f66ec695f4171112d68
Saint Wesonga <wesongathedeveloper@yahoo.com>
2010-05-24 11:38:31 +0200

    Bug 524201 - Move browserconfig.properties to a jar. r=gavin/pike/kev

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4775,7 +4775,7 @@ var gHomeButton = {
     // use this if we can't find the pref
     if (!url) {
       var SBS = Cc["@mozilla.org/intl/stringbundle;1"].getService(Ci.nsIStringBundleService);
-      var configBundle = SBS.createBundle("resource:/browserconfig.properties");
+      var configBundle = SBS.createBundle("chrome://branding/locale/browserconfig.properties");
       url = configBundle.GetStringFromName(this.prefDomain);
     }
 
