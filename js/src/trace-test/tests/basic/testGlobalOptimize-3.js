c68185d98ab313caf2bc4c587610394b3a3617ac
David Anderson <danderson@mozilla.com>
2010-05-23 22:09:41 -0700

    Backed out changeset 0f0f444d57ad

--- js/src/trace-test/tests/basic/testGlobalOptimize-3.js
+++
@@ -1,9 +0,0 @@
-var test;
-{
-  with ({a: 2}) {
-    let (a = 5) {
-      test = (function () { return a; })();
-    }
-  }
-}
-assertEq(test, 5);
--- js/src/trace-test/tests/basic/testGlobalOptimize-4.js
+++
@@ -1,8 +0,0 @@
-var test;
-{
-  with ({a: 2}) {
-      test = (function () { return a; })();
-  }
-  let a = 5;
-}
-assertEq(test, 2);
