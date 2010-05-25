648c641d3c0e5b95219c9b3e495b14f681dedbdc
Mark Banner <bugzilla@standard8.plus.com>
2010-05-25 08:12:31 +0100

    Bug 562886 test_plugins.js fails (8 == 4) with Linux objdir builds - detect whether symlinks are in use or not. r=dtownsend, test-only change

--- toolkit/mozapps/extensions/test/xpcshell/test_plugins.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_plugins.js
@@ -16,6 +16,21 @@ function run_test() {
   run_test_1();
 }
 
+function get_unix_test_plugin() {
+  let plugins = Components.classes["@mozilla.org/file/directory_service;1"].
+                getService(Components.interfaces.nsIProperties).
+                get("CurProcD", Components.interfaces.nsILocalFile);
+  plugins.append("plugins");
+  do_check_true(plugins.exists());
+  let plugin = plugins.clone();
+  // *nix plugin
+  plugin.append("libnptest.so");
+  if (plugin.exists())
+    return plugin;
+
+  return null;
+}
+
 // Tests that the test plugin exists
 function run_test_1() {
   AddonManager.getAddonsByTypes("plugin", function(addons) {
@@ -29,7 +44,7 @@ function run_test_1() {
     do_check_neq(gID, null);
 
     AddonManager.getAddonByID(gID, function(p) {
-      do_check_neq(p, null)
+      do_check_neq(p, null);
       do_check_eq(p.name, "Test Plug-in");
       do_check_eq(p.description, "Plug-in for testing purposes.");
       do_check_eq(p.creator, "");
@@ -43,7 +58,15 @@ function run_test_1() {
       do_check_eq(p.blocklistState, 0);
       do_check_eq(p.permissions, AddonManager.PERM_CAN_DISABLE);
       do_check_eq(p.pendingOperations, 0);
-      do_check_eq(p.scope, AddonManager.SCOPE_APPLICATION);
+
+      // Work around the fact that on Linux source builds, if we're using
+      // symlinks (i.e. objdir), then Linux will see these as a different scope
+      // to non-symlinks.
+      let pluginLoc = get_unix_test_plugin();
+      let pluginScope = AddonManager.SCOPE_APPLICATION;
+      if (pluginLoc && pluginLoc.isSymlink())
+        pluginScope = AddonManager.SCOPE_SYSTEM;
+      do_check_eq(p.scope, pluginScope);
       do_check_true("isCompatibleWith" in p);
       do_check_true("findUpdates" in p);
 
@@ -70,7 +93,7 @@ function run_test_2(p) {
   do_check_false(p.isActive);
 
   AddonManager.getAddonByID(gID, function(p) {
-    do_check_neq(p, null)
+    do_check_neq(p, null);
     do_check_true(p.userDisabled);
     do_check_false(p.appDisabled);
     do_check_false(p.isActive);
@@ -98,7 +121,7 @@ function run_test_3(p) {
   do_check_true(p.isActive);
 
   AddonManager.getAddonByID(gID, function(p) {
-    do_check_neq(p, null)
+    do_check_neq(p, null);
     do_check_false(p.userDisabled);
     do_check_false(p.appDisabled);
     do_check_true(p.isActive);
