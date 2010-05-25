9760e90b3df352af7569e2a1cfb59e6df3fc8df2
timeless@mozdev.org <timeless@mozdev.org>
2010-05-25 17:34:13 +0300

    Bug 564717 warning: functions defined but not used in xpcmodule.cpp r=mrbkap
    --HG--
    extra : rebase_source : e8a15a17c5210b3ad17c6af79f1e2443f3eeb0b3

--- js/src/xpconnect/loader/mozJSLoaderConstructors.h
+++ js/src/xpconnect/loader/mozJSLoaderConstructors.h
@@ -45,6 +45,7 @@
 #define NO_SUBSCRIPT_LOADER
 #endif
 
+#if defined XPCONNECT_STANDALONE || !defined XPCONNECT_MODULE
 #include "mozilla/ModuleUtils.h"
 #include "nsICategoryManager.h"
 #include "mozJSComponentLoader.h"
@@ -58,4 +59,5 @@ NS_GENERIC_FACTORY_CONSTRUCTOR(mozJSComponentLoader)
 
 #ifndef NO_SUBSCRIPT_LOADER
 NS_GENERIC_FACTORY_CONSTRUCTOR(mozJSSubScriptLoader)
+#endif
 #endif
