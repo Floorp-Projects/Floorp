3655e13cb4cedad2768b3db125f40c22d884bd62
Dan Witte <dwitte@mozilla.com>
2010-05-27 14:38:49 -0700

    Add tests for cookie persistence and third party cookies.

--- extensions/cookie/test/unit/test_cookies_thirdparty.js
+++ extensions/cookie/test/unit/test_cookies_thirdparty.js
@@ -22,13 +22,17 @@ function run_test() {
 
   // test with cookies enabled
   prefs.setIntPref("network.cookie.cookieBehavior", 0);
-  run_cookie_test(cs, uri1, channel1, [1, 2, 3, 4]);
-  run_cookie_test(cs, uri1, channel2, [1, 2, 3, 4]);
+  do_set_cookies(uri1, channel1, true, [1, 2, 3, 4]);
+  cs.removeAll();
+  do_set_cookies(uri1, channel2, true, [1, 2, 3, 4]);
+  cs.removeAll();
 
   // test with third party cookies blocked
   prefs.setIntPref("network.cookie.cookieBehavior", 1);
-  run_cookie_test(cs, uri1, channel1, [0, 0, 0, 0]);
-  run_cookie_test(cs, uri1, channel2, [0, 0, 0, 0]);
+  do_set_cookies(uri1, channel1, true, [0, 0, 0, 0]);
+  cs.removeAll();
+  do_set_cookies(uri1, channel2, true, [0, 0, 0, 0]);
+  cs.removeAll();
 
   // Force the channel URI to be used when determining the originating URI of
   // the channel.
@@ -39,28 +43,16 @@ function run_test() {
 
   // test with cookies enabled
   prefs.setIntPref("network.cookie.cookieBehavior", 0);
-  run_cookie_test(cs, uri1, channel1, [1, 2, 3, 4]);
-  run_cookie_test(cs, uri1, channel2, [1, 2, 3, 4]);
+  do_set_cookies(uri1, channel1, true, [1, 2, 3, 4]);
+  cs.removeAll();
+  do_set_cookies(uri1, channel2, true, [1, 2, 3, 4]);
+  cs.removeAll();
 
   // test with third party cookies blocked
   prefs.setIntPref("network.cookie.cookieBehavior", 1);
-  run_cookie_test(cs, uri1, channel1, [0, 1, 1, 2]);
-  run_cookie_test(cs, uri1, channel2, [0, 0, 0, 0]);
-}
-
-function run_cookie_test(cs, uri, channel, expected) {
-  // without channel
-  cs.setCookieString(uri, null, "oh=hai", null);
-  do_check_eq(cs.countCookiesFromHost("foo.com"), expected[0]);
-  // with channel
-  cs.setCookieString(uri, null, "can=has", channel);
-  do_check_eq(cs.countCookiesFromHost("foo.com"), expected[1]);
-  // without channel, from http
-  cs.setCookieStringFromHttp(uri, null, null, "cheez=burger", null, null);
-  do_check_eq(cs.countCookiesFromHost("foo.com"), expected[2]);
-  // with channel, from http
-  cs.setCookieStringFromHttp(uri, null, null, "hot=dog", null, channel);
-  do_check_eq(cs.countCookiesFromHost("foo.com"), expected[3]);
+  do_set_cookies(uri1, channel1, true, [0, 1, 1, 2]);
+  cs.removeAll();
+  do_set_cookies(uri1, channel2, true, [0, 0, 0, 0]);
   cs.removeAll();
 }
 
