67c677f1de0ec7027e955d73ea350326556b5c91
Andreas Gal <gal@mozilla.com>
2010-05-29 19:04:01 -0700

    Improve error reporting for proxy handlers and __iterator__ (568966, r=brendan).

--- js/src/jsfun.h
+++ js/src/jsfun.h
@@ -340,7 +340,6 @@ js_DefineFunction(JSContext *cx, JSObject *obj, JSAtom *atom, JSNative native,
  * with #if/#error in jsfun.c.
  */
 #define JSV2F_CONSTRUCT         JSINVOKE_CONSTRUCT
-#define JSV2F_ITERATOR          JSINVOKE_ITERATOR
 #define JSV2F_SEARCH_STACK      0x10000
 
 extern JSFunction *
