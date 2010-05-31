943dddb7cdfba3d3647befccb0eade6a0b161bef
Ehsan Akhgari <ehsan@mozilla.com>
2010-05-30 21:50:11 -0400

    Bug 568373 - Use "Desktop" for Windows, because apparently we don't have a useful notion of "Home" on Windows

--- layout/forms/nsFileControlFrame.cpp
+++ layout/forms/nsFileControlFrame.cpp
@@ -421,7 +421,12 @@ nsFileControlFrame::MouseListener::MouseClick(nsIDOMEvent* aMouseEvent)
     if (!localFile) {
       // Default to "home" directory for each platform
       nsCOMPtr<nsIFile> homeDir;
-      NS_GetSpecialDirectory(NS_OS_HOME_DIR, getter_AddRefs(homeDir));
+#ifdef XP_WIN
+      const char *dir = NS_WIN_DESKTOP_DIRECTORY; // Use Desktop on Windows
+#else
+      const char *dir = NS_OS_HOME_DIR;
+#endif
+      NS_GetSpecialDirectory(dir, getter_AddRefs(homeDir));
       localFile = do_QueryInterface(homeDir);
     }
     filePicker->SetDisplayDirectory(localFile);
