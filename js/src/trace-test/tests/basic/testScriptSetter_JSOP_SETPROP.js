91d0e71a091e7fd4f538fa1f259c6f5ef1927770
Jason Orendorff <jorendorff@mozilla.com>
2010-06-02 15:58:17 -0700

    Back out changeset ca772ab995c9 (bug 559813, trace script setters).

--- js/src/trace-test/tests/basic/testScriptSetter_JSOP_SETPROP.js
+++
@@ -1,7 +0,0 @@
-var n = 0;
-var a = {set p(x) { n += x; }};
-for (var i = 0; i < 9; i++)
-    a.p = i;
-assertEq(n, 36);
-
-checkStats({recorderStarted: 1, recorderAborted: 0, traceCompleted: 1, traceTriggered: 1});
