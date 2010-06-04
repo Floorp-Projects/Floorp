96afbb9f3d63ed2ffe9cccc675eac83875a83431
Ehsan Akhgari <ehsan@mozilla.com>
2010-06-03 21:28:03 -0400

    Backed out changeset 634e1f2cf46c because it breaks the Esc key in XUL dialogs (possibly among others)

--- content/events/src/nsEventListenerService.cpp
+++ content/events/src/nsEventListenerService.cpp
@@ -36,7 +36,7 @@
  * ***** END LICENSE BLOCK ***** */
 #include "nsEventListenerService.h"
 #include "nsCOMArray.h"
-#include "nsEventListenerManager.h"
+#include "nsIEventListenerManager.h"
 #include "nsPIDOMEventTarget.h"
 #include "nsIVariant.h"
 #include "nsIServiceManager.h"
@@ -51,7 +51,6 @@
 #include "nsGUIEvent.h"
 #include "nsEventDispatcher.h"
 #include "nsIJSEventListener.h"
-#include "nsIDOMEventGroup.h"
 #ifdef MOZ_JSDEBUGGER
 #include "jsdIDebuggerService.h"
 #endif
@@ -245,16 +244,6 @@ nsEventListenerService::GetEventTargetChainFor(nsIDOMEventTarget* aEventTarget,
   }
   *aCount = count;
 
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsEventListenerService::GetSystemEventGroup(nsIDOMEventGroup** aSystemGroup)
-{
-  NS_ENSURE_ARG_POINTER(aSystemGroup);
-  *aSystemGroup = nsEventListenerManager::GetSystemEventGroup();
-  NS_ENSURE_TRUE(*aSystemGroup, NS_ERROR_OUT_OF_MEMORY);
-  NS_ADDREF(*aSystemGroup);
   return NS_OK;
 }
 
