d2066d2fe93023029e945e7f01f2a8acf52a208e
Ehsan Akhgari <ehsan@mozilla.com>
2010-06-03 21:31:07 -0400

    Bug 529922 - Make sure that delayedStartup has run, therefore the test is valid; r=gavin

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_newwindow_stopcmd.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_newwindow_stopcmd.js
@@ -47,17 +47,16 @@ function test() {
   pb.privateBrowsingEnabled = true;
 
   let win = OpenBrowserWindow();
-  win.addEventListener("load", function() {
-    executeSoon(function() {
-      executeSoon(function() {
-        let cmd = win.document.getElementById("Tools:PrivateBrowsing");
-        ok(!cmd.hasAttribute("disabled"),
-           "The Private Browsing command in a new window should be enabled");
+  Services.obs.addObserver(function(subject, topic, data) {
+    Services.obs.removeObserver(arguments.callee, "browser-delayed-startup-finished");
+    var win = subject.QueryInterface(Ci.nsIDOMWindow);
 
-        win.close();
-        pb.privateBrowsingEnabled = false;
-        finish();
-      });
-    });
-  }, false);
+    let cmd = win.document.getElementById("Tools:PrivateBrowsing");
+    ok(!cmd.hasAttribute("disabled"),
+       "The Private Browsing command in a new window should be enabled");
+
+    win.close();
+    pb.privateBrowsingEnabled = false;
+    finish();
+  }, "browser-delayed-startup-finished", false);
 }
