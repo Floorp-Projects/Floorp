52aab63c91e89c5d929a8b8cde4a821fb45251fe
Dan Witte <dwitte@mozilla.com>
2010-06-07 12:32:12 -0700

    Bug 565475 - Allow 3rd party cookies for session only. r=sdwilsh

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -422,8 +422,6 @@ pref("privacy.sanitize.migrateFx3Prefs",    false);
 
 pref("network.proxy.share_proxy_settings",  false); // use the same proxy settings for all protocols
 
-pref("network.cookie.cookieBehavior", 0); // 0-Accept, 1-dontAcceptForeign, 2-dontUse
-
 // l12n and i18n
 pref("intl.accept_languages", "chrome://global/locale/intl.properties");
 pref("intl.charsetmenu.browser.static", "chrome://global/locale/intl.properties");
