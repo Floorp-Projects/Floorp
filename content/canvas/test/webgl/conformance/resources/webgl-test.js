6897f88628ec1e083d6ce83f20db286d8d0461dc
Vladimir Vukicevic <vladimir@pobox.com>
2010-06-08 15:14:39 -0700

    [webgl] test suite additions; npotb

--- content/canvas/test/webgl/conformance/resources/webgl-test.js
+++ content/canvas/test/webgl/conformance/resources/webgl-test.js
@@ -27,6 +27,10 @@ function webglTestLog(msg) {
   if (window.console && window.console.log) {
     window.console.log(msg);
   }
+  if (document.getElementById("console")) {
+    var log = document.getElementById("console");
+    log.innerHTML += msg + "<br>";
+  }
 }
 
 //
@@ -94,6 +98,7 @@ function getGLErrorAsString(ctx, err) {
     return "NO_ERROR";
   }
   for (var name in ctx) {
+      if (name == "canvas") continue;
     if (ctx[name] === err) {
       return name;
     }
@@ -130,6 +135,66 @@ function glErrorShouldBe(ctx, glError) {
 }
 
 //
+// createProgram
+//
+// Create and return a program object, attaching each of the given shaders.
+//
+// If attribs are given, bind an attrib with that name at that index.
+//
+function createProgram(gl, vshaders, fshaders, attribs)
+{
+    if (typeof(vshaders) == "string")
+	vshaders = [vshaders];
+    if (typeof(fshaders) == "string")
+	fshaders = [fshaders];
+
+    var shaders = [];
+    var i;
+
+    for (i = 0; i < vshaders.length; ++i) {
+	var shader = loadShader(gl, vshaders[i], gl.VERTEX_SHADER);
+	if (!shader)
+	    return null;
+	shaders.push(shader);
+    }
+
+    for (i = 0; i < fshaders.length; ++i) {
+	var shader = loadShader(gl, fshaders[i], gl.FRAGMENT_SHADER);
+	if (!shader)
+	    return null;
+	shaders.push(shader);
+    }
+
+    var prog = gl.createProgram();
+    for (i = 0; i < shaders.length; ++i) {
+	gl.attachShader(prog, shaders[i]);
+    }
+
+    if (attribs) {
+        for (var i in attribs) {
+            gl.bindAttribLocation (prog, i, attribs[i]);
+	}
+    }
+
+    gl.linkProgram(prog);
+
+    // Check the link status
+    var linked = gl.getProgramParameter(prog, gl.LINK_STATUS);
+    if (!linked) {
+        // something went wrong with the link
+        var error = gl.getProgramInfoLog(prog);
+        webglTestLog("Error in program linking:" + error);
+
+        gl.deleteProgram(prog);
+	for (i = 0; i < shaders.length; ++i)
+	    gl.deleteShader(shaders[i]);
+        return null;
+    }
+
+    return prog;
+}
+
+//
 // initWebGL
 //
 // Initialize the Canvas element with the passed name as a WebGL object and return the
@@ -153,43 +218,10 @@ function initWebGL(canvasName, vshader, fshader, attribs, clearColor, clearDepth
         return null;
     }
 
-    // create our shaders
-    var vertexShader = loadShaderFromScript(gl, vshader);
-    var fragmentShader = loadShaderFromScript(gl, fshader);
-
-    if (!vertexShader || !fragmentShader)
-        return null;
-
     // Create the program object
-    gl.program = gl.createProgram();
-
+    gl.program = createProgram(gl, vshader, fshader, attribs);
     if (!gl.program)
         return null;
-
-    // Attach our two shaders to the program
-    gl.attachShader (gl.program, vertexShader);
-    gl.attachShader (gl.program, fragmentShader);
-
-    // Bind attributes
-    for (var i in attribs)
-        gl.bindAttribLocation (gl.program, i, attribs[i]);
-
-    // Link the program
-    gl.linkProgram(gl.program);
-
-    // Check the link status
-    var linked = gl.getProgramParameter(gl.program, gl.LINK_STATUS);
-    if (!linked) {
-        // something went wrong with the link
-        var error = gl.getProgramInfoLog (gl.program);
-        webglTestLog("Error in program linking:"+error);
-
-        gl.deleteProgram(gl.program);
-        gl.deleteProgram(fragmentShader);
-        gl.deleteProgram(vertexShader);
-
-        return null;
-    }
 
     gl.useProgram(gl.program);
 
