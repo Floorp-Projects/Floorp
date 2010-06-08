3e542e4bad1854d2cbd606d4cf6b5396cb620302
Blake Kaplan <mrbkap@gmail.com>
2010-06-08 09:12:52 -0700

    Bug 566651 - Actually store the result in vp. r=jorendorff

--- js/src/jsarray.cpp
+++ js/src/jsarray.cpp
@@ -225,6 +225,10 @@ ValueIsLength(JSContext *cx, jsval* vp)
     length = (jsuint) d;
     if (d != (jsdouble) length)
         goto error;
+    if (!js_NewNumberInRootedValue(cx, d, vp)) {
+        *vp = JSVAL_NULL;
+        return 0;
+    }
     return length;
 
   error:
