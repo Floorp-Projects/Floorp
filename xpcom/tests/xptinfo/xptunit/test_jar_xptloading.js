e194975263b6e6a5ee85f11861e9530675bebe68
Benjamin Smedberg <benjamin@smedbergs.us>
2010-06-10 12:51:43 -0400

    Bug 570488 part B - fix and test loading XPT files from JARs, needed for omnijar, r=mwu

---
+++ xpcom/tests/xptinfo/xptunit/test_jar_xptloading.js
@@ -0,0 +1,7 @@
+const xptiITestInterface_IID = "{13c9fc33-40e6-44dc-81e2-21e0dd41f232}";
+
+var id = Components.interfaces.xptiITestInterface;
+do_check_true(id, "xptiITestInterface not registered");
+
+var id2 = Components.interfacesById[xptiITestInterface_IID];
+do_check_true(id === id2, "xptiITestInterface info doesn't match");
