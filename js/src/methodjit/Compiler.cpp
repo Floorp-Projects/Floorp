a5d0552580ebf02a244b3ab1bb801d8734295417
David Anderson <danderson@mozilla.com>
2010-06-10 13:37:37 -0700

    [JAEGER] Added JSOP_TABLESWITCH.

--- js/src/methodjit/Compiler.cpp
+++ js/src/methodjit/Compiler.cpp
@@ -727,6 +727,15 @@ mjit::Compiler::generateMethod()
           }
           END_CASE(JSOP_AND)
 
+          BEGIN_CASE(JSOP_TABLESWITCH)
+            frame.forgetEverything();
+            masm.move(ImmPtr(PC), Registers::ArgReg1);
+            stubCall(stubs::TableSwitch, Uses(1), Defs(0));
+            masm.jump(Registers::ReturnReg);
+            PC += js_GetVariableBytecodeLength(PC);
+            break;
+          END_CASE(JSOP_TABLESWITCH)
+
           BEGIN_CASE(JSOP_LOOKUPSWITCH)
             frame.forgetEverything();
             masm.move(ImmPtr(PC), Registers::ArgReg1);
