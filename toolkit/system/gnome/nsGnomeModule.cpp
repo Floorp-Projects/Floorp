b4b10cbf8528d22954ddf96e08ec2f00caf52713
Benjamin Smedberg <benjamin@smedbergs.us>
2010-06-12 20:50:56 -0400

    More Linux-specific fixup from bug 568691

--- toolkit/system/gnome/nsGnomeModule.cpp
+++ toolkit/system/gnome/nsGnomeModule.cpp
@@ -37,7 +37,7 @@
  * ***** END LICENSE BLOCK ***** */
 
 #include "nsToolkitCompsCID.h"
-#include "nsIGenericFactory.h"
+#include "mozilla/ModuleUtils.h"
 
 #ifdef MOZ_ENABLE_GCONF
 #include "nsGConfService.h"
@@ -56,31 +56,56 @@ NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsGIOService, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsAlertsService, Init)
 #endif
 
-static const nsModuleComponentInfo components[] = {
 #ifdef MOZ_ENABLE_GCONF
-  { "GConf Service",
-    NS_GCONFSERVICE_CID,
-    NS_GCONFSERVICE_CONTRACTID,
-    nsGConfServiceConstructor },
+NS_DEFINE_NAMED_CID(NS_GCONFSERVICE_CID);
 #endif
 #ifdef MOZ_ENABLE_GNOMEVFS
-  { "GnomeVFS Service",
-    NS_GNOMEVFSSERVICE_CID,
-    NS_GNOMEVFSSERVICE_CONTRACTID,
-    nsGnomeVFSServiceConstructor },
+NS_DEFINE_NAMED_CID(NS_GNOMEVFSSERVICE_CID);
 #endif
 #ifdef MOZ_ENABLE_GIO
-  { "GIO Service",
-    NS_GIOSERVICE_CID,
-    NS_GIOSERVICE_CONTRACTID,
-    nsGIOServiceConstructor },
+NS_DEFINE_NAMED_CID(NS_GIOSERVICE_CID);
 #endif
 #ifdef MOZ_ENABLE_LIBNOTIFY
-  { "Gnome Alerts Service",
-    NS_SYSTEMALERTSSERVICE_CID,
-    NS_SYSTEMALERTSERVICE_CONTRACTID,
-    nsAlertsServiceConstructor },
+NS_DEFINE_NAMED_CID(NS_SYSTEMALERTSSERVICE_CID);
 #endif
+
+
+static const mozilla::Module::CIDEntry kGnomeCIDs[] = {
+#ifdef MOZ_ENABLE_GCONF
+  { &kNS_GCONFSERVICE_CID, false, NULL, nsGConfServiceConstructor },
+#endif
+#ifdef MOZ_ENABLE_GNOMEVFS
+  { &kNS_GNOMEVFSSERVICE_CID, false, NULL, nsGnomeVFSServiceConstructor },
+#endif
+#ifdef MOZ_ENABLE_GIO
+  { &kNS_GIOSERVICE_CID, false, NULL, nsGIOServiceConstructor },
+#endif
+#ifdef MOZ_ENABLE_LIBNOTIFY
+  { &kNS_SYSTEMALERTSSERVICE_CID, false, NULL, nsAlertsServiceConstructor },
+#endif
+  { NULL }
 };
 
-NS_IMPL_NSGETMODULE(mozgnome, components)
+static const mozilla::Module::ContractIDEntry kGnomeContracts[] = {
+#ifdef MOZ_ENABLE_GCONF
+  { NS_GCONFSERVICE_CONTRACTID, &kNS_GCONFSERVICE_CID },
+#endif
+#ifdef MOZ_ENABLE_GNOMEVFS
+  { NS_GNOMEVFSSERVICE_CONTRACTID, &kNS_GNOMEVFSSERVICE_CID },
+#endif
+#ifdef MOZ_ENABLE_GIO
+  { NS_GIOSERVICE_CONTRACTID, &kNS_GIOSERVICE_CID },
+#endif
+#ifdef MOZ_ENABLE_LIBNOTIFY
+  { NS_SYSTEMALERTSERVICE_CONTRACTID, &kNS_SYSTEMALERTSSERVICE_CID },
+#endif
+  { NULL }
+};
+
+static const mozilla::Module kGnomeModule = {
+  mozilla::Module::kVersion,
+  kGnomeCIDs,
+  kGnomeContracts
+};
+
+NSMODULE_DEFN(mozgnome) = &kGnomeModule;
