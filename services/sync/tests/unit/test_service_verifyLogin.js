c67af812e52667d561b7585bc612b7560c90507c
Philipp von Weitershausen <philipp@weitershausen.de>
2010-06-14 21:03:39 +0100

    Bug 570180 - Setup wizard sets passphrase='foo' [r=mconnor] Promote _verifyLogin to a public method so we have a way to query login status even with a non-existent or invalid passphrase.

--- services/sync/tests/unit/test_service_verifyLogin.js
+++ services/sync/tests/unit/test_service_verifyLogin.js
@@ -29,26 +29,21 @@ function run_test() {
   try {
     Weave.Service.serverURL = "http://localhost:8080/";
     Weave.Service.clusterURL = "http://localhost:8080/";
-    Svc.Prefs.set("autoconnect", false);
 
     _("Initial state is ok.");
     do_check_eq(Status.service, STATUS_OK);
 
-    _("Try logging in. It wont' work because we're not configured yet.");
-    Weave.Service.login();
+    _("Credentials won't check out because we're not configured yet.");
+    do_check_false(Weave.Service.verifyLogin());
     do_check_eq(Status.service, CLIENT_NOT_CONFIGURED);
     do_check_eq(Status.login, LOGIN_FAILED_NO_USERNAME);
-    do_check_false(Weave.Service.isLoggedIn);
-    do_check_false(Svc.Prefs.get("autoconnect"));
 
     _("Try again with username and password set.");
     Weave.Service.username = "johndoe";
     Weave.Service.password = "ilovejane";
-    Weave.Service.login();
+    do_check_false(Weave.Service.verifyLogin());
     do_check_eq(Status.service, CLIENT_NOT_CONFIGURED);
     do_check_eq(Status.login, LOGIN_FAILED_NO_PASSPHRASE);
-    do_check_false(Weave.Service.isLoggedIn);
-    do_check_false(Svc.Prefs.get("autoconnect"));
 
     _("Success if passphrase is set.");
     Weave.Service.passphrase = "foo";
@@ -56,7 +51,6 @@ function run_test() {
     do_check_eq(Status.service, STATUS_OK);
     do_check_eq(Status.login, LOGIN_SUCCEEDED);
     do_check_true(Weave.Service.isLoggedIn);
-    do_check_true(Svc.Prefs.get("autoconnect"));
 
   } finally {
     Svc.Prefs.resetBranch("");
