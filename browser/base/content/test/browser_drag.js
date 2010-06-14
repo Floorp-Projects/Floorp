4eaa949d991f2b3b951330f8ec01661d3e99cbaf
Dão Gottwald <dao@mozilla.com>
2010-06-14 18:50:28 +0200

    Disable tap opening animation in browser_drag.js to make synthesizeDrop work

--- browser/base/content/test/browser_drag.js
+++ browser/base/content/test/browser_drag.js
@@ -28,7 +28,7 @@ function test()
   EventUtils.synthesizeKey("VK_ESCAPE", {}, window);
 
   // now test dragging onto a tab
-  var tab = gBrowser.addTab();
+  var tab = gBrowser.addTab("about:blank", {skipAnimation: true});
   var browser = gBrowser.getBrowserForTab(tab);
 
   browser.addEventListener("load", function () {
