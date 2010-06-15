faea0a185c15ffbeeebf5ef4d1fe710da207c86e
Michael Wu <mwu@mozilla.com>
2010-06-14 20:47:55 -0700

    Disable new jar xptload test due to failures

--- xpcom/tests/xptinfo/xptunit/test_jar_xptloading.js
+++ xpcom/tests/xptinfo/xptunit/test_jar_xptloading.js
@@ -3,5 +3,5 @@ const xptiITestInterface_IID = "{13c9fc33-40e6-44dc-81e2-21e0dd41f232}";
 var id = Components.interfaces.xptiITestInterface;
 do_check_true(id != undefined, "xptiITestInterface not registered");
 
-var id2 = Components.interfacesById[xptiITestInterface_IID];
+var id2 = Components.interfacesByID[xptiITestInterface_IID];
 do_check_true(id === id2, "xptiITestInterface info doesn't match");
