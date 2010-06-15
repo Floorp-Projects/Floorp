50b1c02e21aa648699bc754a0bceebe8bded2aa9
David Mandelin <dmandelin@mozilla.com>
2010-06-15 15:43:41 -0700

    Bug 572042: get tracing+fat values to build in the browser. As part of this I disabled the quickstub traceable natives. According to Andreas, we don't necessarily need them for good perf now that we can call the standard quickstub from trace. This build works on simple JS, but there are still bugs (e.g., Google Maps asserts).

--- js/src/jspubtd.h
+++ js/src/jspubtd.h
@@ -162,10 +162,11 @@ typedef struct JSCompartment     JSCompartment;
  * TODO: explain boxing strategy
  */
 
+#if defined(__cplusplus) || !defined(_MSC_VER)
 typedef enum JSValueMask16 
-#if defined(_MSC_VER)
+# if defined(_MSC_VER)
                            : uint16
-#endif
+# endif
 {
     JSVAL_MASK16_NULL      = (uint16)0x0001,
     JSVAL_MASK16_UNDEFINED = (uint16)0x0002,
@@ -188,10 +189,30 @@ typedef enum JSValueMask16
      */
     JSVAL_NANBOX_PATTERN   = ((uint16)0xFFFF)
 }
-#if defined(__GNUC__)
+# if defined(__GNUC__)
 __attribute__((packed))
-#endif
+# endif
 JSValueMask16;
+#else  /* defined(__cplusplus) || !defined(_MSC_VER) */
+// We need this C API for MSVC, because MSVC doesn't allow us to
+// make a 16-bit enum in C.
+typedef uint16 JSValueMask16;
+
+#define JSVAL_MASK16_NULL      ((uint16)0x0001)
+#define JSVAL_MASK16_UNDEFINED ((uint16)0x0002)
+#define JSVAL_MASK16_INT32     ((uint16)0x0004)
+#define JSVAL_MASK16_STRING    ((uint16)0x0008)
+#define JSVAL_MASK16_NONFUNOBJ ((uint16)0x0010)
+#define JSVAL_MASK16_FUNOBJ    ((uint16)0x0020)
+#define JSVAL_MASK16_BOOLEAN   ((uint16)0x0040)
+#define JSVAL_MASK16_MAGIC     ((uint16)0x0080)
+#define JSVAL_MASK16_SINGLETON (JSVAL_MASK16_NULL | JSVAL_MASK16_UNDEFINED)
+#define JSVAL_MASK16_OBJECT    (JSVAL_MASK16_NONFUNOBJ | JSVAL_MASK16_FUNOBJ)
+#define JSVAL_MASK16_OBJORNULL (JSVAL_MASK16_OBJECT | JSVAL_MASK16_NULL)
+#define JSVAL_MASK16_GCTHING   (JSVAL_MASK16_OBJECT | JSVAL_MASK16_STRING)
+#define JSVAL_NANBOX_PATTERN   ((uint16)0xFFFF)
+
+#endif /* defined(__cplusplus) || !defined(_MSC_VER) */
 
 #define JSVAL_MASK32_CLEAR      ((uint32)0xFFFF0000)
 
@@ -1116,14 +1137,14 @@ static inline JSEqualityOp     Jsvalify(EqualityOp f);      /* Same type as HasI
 }  /* namespace js */
 
 typedef js::Class JSFunctionClassType;
+extern "C" JS_FRIEND_DATA(JSFunctionClassType) js_FunctionClass;
 
 #else  /* !defined(__cplusplus) */
 
 typedef JSClass JSFunctionClassType;
+extern JS_FRIEND_DATA(JSFunctionClassType) js_FunctionClass;
 
 #endif /* __cplusplus */
-
-extern JS_FRIEND_DATA(JSFunctionClassType) js_FunctionClass;
 
 typedef struct JSPretendObject
 {
