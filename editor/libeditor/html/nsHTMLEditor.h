b51e1f6c6acc852ed4d1080e2b1ffe3913ff9e06
Masayuki Nakano <masayuki@d-toybox.com>
2010-06-17 14:32:05 +0900

    Bug 545775 part.5 Text editor in ASUS Forum doesn't work r=ehsan

--- editor/libeditor/html/nsHTMLEditor.h
+++ editor/libeditor/html/nsHTMLEditor.h
@@ -448,7 +448,10 @@ protected:
   virtual nsresult InstallEventListeners();
   virtual void RemoveEventListeners();
 
+  PRBool ShouldReplaceRootElement();
+  void ResetRootElementAndEventTarget();
   nsresult GetBodyElement(nsIDOMHTMLElement** aBody);
+  already_AddRefed<nsINode> GetFocusedNode();
 
   // Return TRUE if aElement is a table-related elemet and caret was set
   PRBool SetCaretInTableCell(nsIDOMElement* aElement);
