25efd0d53e48d1eacf1a6e78fe0a752edca96334
Boris Zbarsky <bzbarsky@mit.edu>
2010-06-18 15:18:02 -0400

    Addressing some bug 479655 review comments I missed.

--- layout/base/RestyleTracker.cpp
+++ layout/base/RestyleTracker.cpp
@@ -139,7 +139,8 @@ RestyleTracker::ProcessOneRestyle(Element* aElement,
 {
   NS_PRECONDITION((aRestyleHint & eRestyle_LaterSiblings) == 0,
                   "Someone should have handled this before calling us");
-  NS_PRECONDITION(aElement->GetCurrentDoc() == mFrameConstructor->mDocument,
+  NS_PRECONDITION(Document(), "Must have a document");
+  NS_PRECONDITION(aElement->GetCurrentDoc() == Document(),
                   "Element has unexpected document");
 
   nsIFrame* primaryFrame = aElement->GetPrimaryFrame();
@@ -217,7 +218,7 @@ RestyleTracker::ProcessRestyles()
       // Do the document check before calling GetRestyleData, since we
       // don't want to do the sibling-processing GetRestyleData does if
       // the node is no longer relevant.
-      if (element->GetCurrentDoc() != mFrameConstructor->mDocument) {
+      if (element->GetCurrentDoc() != Document()) {
         // Content node has been removed from our document; nothing else
         // to do here
         continue;
@@ -276,7 +277,7 @@ RestyleTracker::ProcessRestyles()
 PRBool
 RestyleTracker::GetRestyleData(Element* aElement, RestyleData* aData)
 {
-  NS_PRECONDITION(aElement->GetCurrentDoc() == mFrameConstructor->mDocument,
+  NS_PRECONDITION(aElement->GetCurrentDoc() == Document(),
                   "Unexpected document; this will lead to incorrect behavior!");
 
   if (!aElement->HasFlag(RestyleBit())) {
