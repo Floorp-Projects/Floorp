867641d6ca6e2e0714f34019343840bfacaa4817
Edward Lee <edilee@mozilla.com>
2010-06-21 16:46:57 -0700

    Bug 573194 - AboutWeaveTabs still references chrome://weave/content/firefox/tabs.xul [r=Mardak] Split off about: chrome aliasing from other services aliases.

--- services/sync/Weave.js
+++ services/sync/Weave.js
@@ -92,28 +92,6 @@ WeaveService.prototype = {
   }
 };
 
-function AboutWeaveTabs() {}
-AboutWeaveTabs.prototype = {
-  classDescription: "about:sync-tabs",
-  contractID: "@mozilla.org/network/protocol/about;1?what=sync-tabs",
-  classID: Components.ID("{ecb6987d-9d71-475d-a44d-a5ff2099b08c}"),
-
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAboutModule,
-                                         Ci.nsISupportsWeakReference]),
-
-  getURIFlags: function(aURI) {
-    return (Ci.nsIAboutModule.ALLOW_SCRIPT);
-  },
-
-  newChannel: function(aURI) {
-    let ios = Cc["@mozilla.org/network/io-service;1"]
-      .getService(Ci.nsIIOService);
-    let ch = ios.newChannel("chrome://weave/content/firefox/tabs.xul", null, null);
-    ch.originalURI = aURI;
-    return ch;
-  }
-};
-
 function AboutWeaveLog() {}
 AboutWeaveLog.prototype = {
   classDescription: "about:sync-log",
@@ -173,7 +151,6 @@ AboutWeaveLog1.prototype = {
 function NSGetModule(compMgr, fileSpec) {
   return XPCOMUtils.generateModule([
     WeaveService,
-    AboutWeaveTabs,
     AboutWeaveLog,
     AboutWeaveLog1
   ]);
