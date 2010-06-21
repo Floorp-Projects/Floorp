5d8c2e8024fe5498b3dac8bd65fc9ad5d57a22c3
Benjamin Smedberg <benjamin@smedbergs.us>
2010-06-21 14:29:32 -0400

    Bug 568691 - Fix XPCOMUtils.jsm to generate NSGetFactory, and fix nsSample.js to use XPCOMUtils

--- xpcom/sample/nsSample.js
+++ xpcom/sample/nsSample.js
@@ -1,22 +1,34 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
-/*
- * No magic constructor behaviour, as is de rigeur for XPCOM.
- * If you must perform some initialization, and it could possibly fail (even
- * due to an out-of-memory condition), you should use an Init method, which
- * can convey failure appropriately (thrown exception in JS,
- * NS_FAILED(nsresult) return in C++).
- *
- * In JS, you can actually cheat, because a thrown exception will cause the
- * CreateInstance call to fail in turn, but not all languages are so lucky.
- * (Though ANSI C++ provides exceptions, they are verboten in Mozilla code
- * for portability reasons -- and even when you're building completely
- * platform-specific code, you can't throw across an XPCOM method boundary.)
- */
-function mySample() { /* big comment for no code, eh? */ }
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
-/* decorate prototype to provide ``class'' methods and property accessors */
+/**
+ * We set up a sample component. The constructor is empty, all the interesting
+ * stuff goes in the prototype.
+ */
+function mySample() { }
+
 mySample.prototype = {
+    /**
+     * .classID is required for generateNSGetFactory to work correctly.
+     * Make sure this CID matches the "component" in your .manifest file.
+     */
+    classID: Components.ID("{dea98e50-1dd1-11b2-9344-8902b4805a2e}"),
+
+    /**
+     * .classDescription and .contractID are only used for
+     * backwards compatibility with Gecko 1.9.2 and
+     * XPCOMUtils.generateNSGetModule.
+     */
+    classDescription: "nsSample: JS version", // any human-readable string
+    contractID: "@mozilla.org/jssample;1",
+
+    /**
+     * List all the interfaces your component supports.
+     * @note nsISupports is generated automatically; you don't need to list it.
+     */
+    QueryInterface: XPCOMUtils.generateQI([Components.interfaces.nsISample]),
+
     /*
      * get and set are new Magic in JS1.5, borrowing the intent -- if not
      * the exact syntax -- from the JS2 design.  They define accessors for
@@ -31,39 +43,14 @@ mySample.prototype = {
     },
     poke: function (aValue) { this.val = aValue; },
 
-    QueryInterface: function (iid) {
-        if (iid.equals(Components.interfaces.nsISample) ||
-            iid.equals(Components.interfaces.nsISupports))
-            return this;
-
-        throw Components.results.NS_ERROR_NO_INTERFACE;
-    },
-
     val: "<default value>"
 };
 
-const kMyCID = Components.ID("{dea98e50-1dd1-11b2-9344-8902b4805a2e}");
-
-const kMyFactory = {
-    /*
-     * Construct an instance of the interface specified by iid, possibly
-     * aggregating it with the provided outer.  (If you don't know what
-     * aggregation is all about, you don't need to.  It reduces even the
-     * mightiest of XPCOM warriors to snivelling cowards.)
-     */
- createInstance: function (outer, iid) {
-   debug("CI: " + iid + "\n");
-   if (outer != null)
-     throw Components.results.NS_ERROR_NO_AGGREGATION;
-
-   return (new mySample()).QueryInterface(iid);
-  }
-};
-
-function NSGetFactory(cid)
-{
-    if (cid.equals(kMyCID))
-        return kMyFactory;
-        
-    throw Components.results.NS_ERROR_FACTORY_NOT_REGISTERED;
-}
+/**
+ * XPCOMUtils.generateNSGetFactory was introduced in Mozilla 2 (Firefox 4).
+ * XPCOMUtils.generateNSGetModule is for Mozilla 1.9.2 (Firefox 3.6).
+ */
+if (XPCOMUtils.generateNSGetFactory)
+    NSGetFactory = XPCOMUtils.generateNSGetFactory([mySample]);
+else
+    NSGetModule = XPCOMUtils.generateNSGetModule([mySample]);
