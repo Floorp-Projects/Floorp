b18d839d9910fc5e96285526e86892b1748a646c
Edward Lee <edilee@mozilla.com>
2010-06-22 16:48:55 -0700

    Bug 573679 - Fix tests to pass on trunk Switch around which lazy services we use to test with but cover even more data types.

--- services/sync/tests/unit/test_utils_lazySvc.js
+++ services/sync/tests/unit/test_utils_lazySvc.js
@@ -10,12 +10,17 @@ function run_test() {
   do_check_eq(typeof obj.app.vendor, "string");
   do_check_eq(typeof obj.app.name, "string");
 
-  _("Check other types of properties on profiles");
-  Utils.lazySvc(obj, "prof", "@mozilla.org/toolkit/profile-service;1", "nsIToolkitProfileService");
-  do_check_eq(typeof obj.prof.QueryInterface, "function");
-  do_check_eq(typeof obj.prof.startOffline, "boolean");
-  do_check_eq(typeof obj.prof.profileCount, "number");
-  do_check_eq(typeof obj.prof.createProfile, "function");
+  _("Check other types of properties on other services");
+  Utils.lazySvc(obj, "io", "@mozilla.org/network/io-service;1", "nsIIOService");
+  do_check_eq(typeof obj.io.newURI, "function");
+  do_check_eq(typeof obj.io.offline, "boolean");
+
+  Utils.lazySvc(obj, "thread", "@mozilla.org/thread-manager;1", "nsIThreadManager");
+  do_check_true(obj.thread.currentThread instanceof Ci.nsIThread);
+
+  Utils.lazySvc(obj, "net", "@mozilla.org/network/util;1", "nsINetUtil");
+  do_check_eq(typeof obj.net.ESCAPE_ALL, "number");
+  do_check_eq(obj.net.ESCAPE_URL_SCHEME, 1);
 
   _("Make sure fake services get loaded correctly (private browsing doesnt exist on all platforms)");
   Utils.lazySvc(obj, "priv", "@mozilla.org/privatebrowsing;1", "nsIPrivateBrowsingService");
