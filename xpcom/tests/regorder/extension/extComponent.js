69474567d08a63c89842990020172ee8c20543fe
Benjamin Smedberg <benjamin@smedbergs.us>
2010-06-22 13:26:23 -0400

    Fix the TestRegistrationOrder test: we no longer support deferred registration.

--- xpcom/tests/regorder/extension/extComponent.js
+++ xpcom/tests/regorder/extension/extComponent.js
@@ -61,51 +61,11 @@ var gRegTestExtComponent =
   lockFactory: function (aDoLock) {}
 };
 
-var gRegTestExtComponentModule =
+const kClassID = Components.ID("{fe64efb7-c5ab-41a6-b639-e6c0f483181e}");
+function NSGetFactory(cid)
 {
-  kClassName: "RegTestExtComponent",
-  kClassID: Components.ID("{fe64efb7-c5ab-41a6-b639-e6c0f483181e}"),
-  kContractID: "@mozilla.org/RegTestServiceA;1",
-  kICompReg: Components.interfaces.nsIComponentRegistrar,
+  if (!cid.equals(kClassID))
+    throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
 
-  /* nsISupports implementation. */
-  QueryInterface: function (aIID)
-  {
-    if (aIID.equals(Components.interfaces.nsIModule) ||
-        aIID.equals(Components.interfaces.nsISupports))
-      return this;
-
-    throw Components.results.NS_ERROR_NO_INTERFACE;
-  },
-
-  /* nsIModule implementation. */
-  getClassObject: function (aCompMgr, aClassID, aIID)
-  {
-    if (!aClassID.equals(this.kClassID))
-      throw Components.results.NS_ERROR_NO_INTERFACE;
-    if (!aIID.equals(Components.interfaces.nsIFactory))
-      throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
-
-    return gRegTestExtComponent.QueryInterface(aIID);
-  },
-
-  registerSelf: function (aCompMgr, aFileSpec, aLocation, aType)
-  {
-    aCompMgr = aCompMgr.QueryInterface(this.kICompReg);
-    aCompMgr.registerFactoryLocation(this.kClassID, this.kClassName,
-                                 this.kContractID, aFileSpec, aLocation, aType);
-  },
-
-  unregisterSelf: function (aCompMgr, aFileSpec, aLocation)
-  {
-    aCompMgr = aCompMgr.QueryInterface(this.kICompReg);
-    aCompMgr.unregisterFactoryLocation(this.kClassID, aFileSpec);
-  },
-
-  canUnload: function (aCompMgr) { return true; }
-};
-
-function NSGetModule(aCompMgr, aFileSpec)
-{
-  return gRegTestExtComponentModule;
+  return gRegTestExtComponent;
 }
