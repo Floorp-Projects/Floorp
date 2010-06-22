5b1b5baded1ed0ee01f1131d5baa6c4ef69161d6
Dão Gottwald <dao@mozilla.com>
2010-06-22 22:19:22 +0200

    Bug 557877 - Don't persist the findbar in new windows. r=gavin, ui-r=faaborg

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -996,14 +996,6 @@ function BrowserStartup() {
   }
 
   if (window.opener && !window.opener.closed) {
-    let openerFindBar = window.opener.gFindBarInitialized ?
-                        window.opener.gFindBar : null;
-    if (openerFindBar &&
-        !openerFindBar.hidden &&
-        openerFindBar.findMode == openerFindBar.FIND_NORMAL) {
-      gFindBar.open();
-    }
-
     let openerSidebarBox = window.opener.document.getElementById("sidebar-box");
     // If the opener had a sidebar, open the same sidebar in our window.
     // The opener can be the hidden window too, if we're coming from the state
