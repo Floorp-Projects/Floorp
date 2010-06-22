3881a9b0a5f94b2149a879bbb62bfba283528aab
Benjamin Smedberg <benjamin@smedbergs.us>
2010-06-22 13:05:00 -0400

    Bug 568691 - Mac-specific changes to binary components.

--- toolkit/components/alerts/src/mac/nsAlertsServiceModule.cpp
+++ toolkit/components/alerts/src/mac/nsAlertsServiceModule.cpp
@@ -36,7 +36,7 @@
 
 #include "nsAlertsService.h"
 #include "nsToolkitCompsCID.h"
-#include "nsIGenericFactory.h"
+#include "mozilla/ModuleUtils.h"
 #include "nsCOMPtr.h"
 #include "nsIServiceManager.h"
 #include "nsServiceManagerUtils.h"
@@ -44,13 +44,22 @@
 #include "nsMemory.h"
 
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsAlertsService, Init)
+NS_DEFINE_NAMED_CID(NS_ALERTSSERVICE_CID);
 
-static const nsModuleComponentInfo components[] =
-{
-  { "Alerts Service",
-    NS_ALERTSSERVICE_CID,
-    NS_ALERTSERVICE_CONTRACTID,
-    nsAlertsServiceConstructor },
+static const mozilla::Module::CIDEntry kAlertsCIDs[] = {
+  { &kNS_ALERTSSERVICE_CID, false, NULL, nsAlertsServiceConstructor },
+  { NULL }
 };
 
-NS_IMPL_NSGETMODULE(nsAlertsServiceModule, components)
+static const mozilla::Module::ContractIDEntry kAlertsContracts[] = {
+  { NS_ALERTSERVICE_CONTRACTID, &kNS_ALERTSSERVICE_CID },
+  { NULL }
+};
+
+static const mozilla::Module kAlertsModule = {
+  mozilla::Module::kVersion,
+  kAlertsCIDs,
+  kAlertsContracts
+};
+
+NSMODULE_DEFN(nsAlertsServiceModule) = &kAlertsModule;
