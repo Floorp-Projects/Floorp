04afb9641ea69e0a2e602602e9d6f3e0d662b014
Matt Woodrow <matt.woodrow+bugzilla@gmail.com>
2010-06-23 18:37:00 -0500

    Bug 573929: Prevent GLX textures from being released after the window supplying the GLContext from which they were allocated dies. r=vlad

--- gfx/thebes/src/GLContextProviderGLX.cpp
+++ gfx/thebes/src/GLContextProviderGLX.cpp
@@ -213,6 +213,34 @@ public:
         return PR_TRUE;
     }
 
+    void WindowDestroyed()
+    {
+        for (unsigned int i=0; i<textures.Length(); i++) {
+            GLContext::DestroyTexture(textures.ElementAt(i));
+        }
+        textures.Clear();
+    }
+
+    // NB: we could set a flag upon WindowDestroyed() to dictate an
+    // early-return from CreateTexture(), but then we would need the
+    // same check before all GL calls, and that heads down a rabbit
+    // hole.
+    virtual GLuint CreateTexture()
+    {
+        GLuint tex = GLContext::CreateTexture();
+        NS_ASSERTION(!textures.Contains(tex), "");
+        textures.AppendElement(tex);
+        return tex;
+    }
+
+    virtual void DestroyTexture(GLuint texture)
+    {
+        if (textures.Contains(texture)) {
+            textures.RemoveElement(texture);
+            GLContext::DestroyTexture(texture);
+        }
+    }
+
 private:
     GLContextGLX(Display *aDisplay, GLXDrawable aWindow, GLXContext aContext, PRBool aPBuffer = PR_FALSE, PRBool aDoubleBuffered=PR_FALSE)
         : mContext(aContext), 
@@ -226,6 +254,7 @@ private:
     GLXDrawable mWindow;
     PRBool mPBuffer;
     PRBool mDoubleBuffered;
+    nsTArray<GLuint> textures;
 };
 
 static PRBool AreCompatibleVisuals(XVisualInfo *one, XVisualInfo *two)
