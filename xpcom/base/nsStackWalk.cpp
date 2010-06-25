8c1379861a3434b7d52b42d58562190da4716ea3
Jacek Caban <jacek@codeweavers.com>
2010-06-25 14:02:24 +0200

    Bug 569586  - XPCOM compilation failure on mingw-w64 due to pointer to int cast loosing precision. r=benjamin

--- xpcom/base/nsStackWalk.cpp
+++ xpcom/base/nsStackWalk.cpp
@@ -1031,7 +1031,7 @@ NS_DescribeCodeAddress(void *aPC, nsCodeAddressDetails *aDetails)
         // This just makes sure we get good info if available.
         //
 
-        DWORD addr = (DWORD)aPC;
+        DWORD_PTR addr = (DWORD_PTR)aPC;
         IMAGEHLP_MODULE modInfo;
         IMAGEHLP_LINE lineInfo;
         BOOL modInfoRes;
