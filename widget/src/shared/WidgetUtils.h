3bb697ac020ffd9574bf5d17e67a9ee31f43aa79
Timothy Nikkel <tnikkel@gmail.com>
2010-06-25 16:51:17 -0500

    Bug 567702. Factor out duplicated DOMWindowToWidget. r=roc

--- widget/src/shared/WidgetUtils.h
+++ widget/src/shared/WidgetUtils.h
@@ -38,46 +38,29 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#ifndef nsBaseFilePicker_h__
-#define nsBaseFilePicker_h__
+#ifndef __mozilla_widget_WidgetUtils_h__
+#define __mozilla_widget_WidgetUtils_h__
 
-#include "nsISupports.h"
-#include "nsIFilePicker.h"
-#include "nsISimpleEnumerator.h"
-#include "nsArrayEnumerator.h"
 #include "nsCOMPtr.h"
+#include "nsIWidget.h"
+#include "nsPIDOMWindow.h"
+#include "nsIDOMWindow.h"
 
-class nsIWidget;
+namespace mozilla {
+namespace widget {
 
-#define BASEFILEPICKER_HAS_DISPLAYDIRECTORY 1
-
-class nsBaseFilePicker : public nsIFilePicker
+class WidgetUtils
 {
 public:
-  nsBaseFilePicker(); 
-  virtual ~nsBaseFilePicker();
 
-  NS_IMETHOD Init(nsIDOMWindow *aParent,
-                  const nsAString& aTitle,
-                  PRInt16 aMode);
-
-  NS_IMETHOD AppendFilters(PRInt32 filterMask);
-  NS_IMETHOD GetFilterIndex(PRInt32 *aFilterIndex);
-  NS_IMETHOD SetFilterIndex(PRInt32 aFilterIndex);
-  NS_IMETHOD GetFiles(nsISimpleEnumerator **aFiles);
-#ifdef BASEFILEPICKER_HAS_DISPLAYDIRECTORY 
-  NS_IMETHOD GetDisplayDirectory(nsILocalFile * *aDisplayDirectory);
-  NS_IMETHOD SetDisplayDirectory(nsILocalFile * aDisplayDirectory);
-#endif
-
-protected:
-
-  virtual void InitNative(nsIWidget *aParent, const nsAString& aTitle,
-                          PRInt16 aMode) = 0;
-
-#ifdef BASEFILEPICKER_HAS_DISPLAYDIRECTORY 
-  nsCOMPtr<nsILocalFile> mDisplayDirectory;
-#endif
+  /**
+   * Starting at the docshell item for the passed in DOM window this looks up
+   * the docshell tree until it finds a docshell item that has a widget.
+   */
+  static already_AddRefed<nsIWidget> DOMWindowToWidget(nsIDOMWindow *aDOMWindow);
 };
 
-#endif // nsBaseFilePicker_h__
+} // namespace widget
+} // namespace mozilla
+
+#endif
