5fa55edd9043aa0fc2bc8d28bbb41f84d282c161
Jeff Muizelaar <jmuizelaar@mozilla.com>
2010-06-25 15:20:18 -0400

    Bug 570451 - Avoid adding unneeded lines to image; r=joe sr=vlad

--- modules/libpr0n/decoders/png/nsPNGDecoder.cpp
+++ modules/libpr0n/decoders/png/nsPNGDecoder.cpp
@@ -776,6 +776,9 @@ row_callback(png_structp png_ptr, png_bytep new_row,
   if (decoder->mFrameIsHidden)
     return;
 
+  if (row_num >= decoder->mFrameRect.height)
+    return;
+
   if (new_row) {
     PRInt32 width = decoder->mFrameRect.width;
     PRUint32 iwidth = decoder->mFrameRect.width;
