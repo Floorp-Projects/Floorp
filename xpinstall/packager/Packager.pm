aabf0560bdb5ecf9db52b7f75518ac852755fbc8
Benjamin Smedberg <benjamin@smedbergs.us>
2010-06-29 18:19:09 -0400

    Link all the component manifests together at package time.

--- xpinstall/packager/Packager.pm
+++ xpinstall/packager/Packager.pm
@@ -230,7 +230,15 @@ sub do_copyfile
 
   # set the destination path, if alternate destination given, use it.
   if ($flat) {
-    if ($srcsuffix eq ".xpt" && $srcpath =~ m|/components/$|) {
+    if ($srcsuffix eq ".manifest" && $srcpath =~ m|/components/$|) {
+      if ($component eq "") {
+        die ("Manifest file was not part of a component.");
+      }
+
+      $destpathcomp = "$srcdir/manifests/$component";
+      $altdest = "$srcname$srcsuffix";
+    }
+    elsif ($srcsuffix eq ".xpt" && $srcpath =~ m|/components/$|) {
       if ($component eq "") {
         die ("XPT file was not part of a component.");
       }
