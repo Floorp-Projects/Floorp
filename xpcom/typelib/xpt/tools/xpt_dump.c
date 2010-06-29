ff0cff1a6ba610b996ad5ff6fd1e3ad2e185239d
Ted Mielczarek <ted.mielczarek@gmail.com>
2010-06-29 10:03:47 -0400

    fix minor type name output in xpt_dump. r=timeless (no bug)

--- xpcom/typelib/xpt/tools/xpt_dump.c
+++ xpcom/typelib/xpt/tools/xpt_dump.c
@@ -60,7 +60,7 @@ static char *type_array[32] =
             {"int8",        "int16",       "int32",       "int64",
              "uint8",       "uint16",      "uint32",      "uint64",
              "float",       "double",      "boolean",     "char",
-             "wchar_t",     "void",        "reserved",    "reserved",
+             "wchar_t",     "void",        "nsIID",       "reserved",
              "reserved",    "reserved",    "reserved",    "reserved",
              "reserved",    "reserved",    "reserved",    "reserved",
              "reserved",    "reserved",    "jsval",       "reserved",
