cc1294623abaa3e7e65f16620bffeae95cc2b31b
Benjamin Smedberg <benjamin@smedbergs.us>
2010-06-29 16:56:13 -0400

    Bug 574457 - Back out the functional changes from revision 0327e126ea24, bug 569644, because of 100ms (20%) Ts regression on Linux/Mac. This leaves the nsIContentUtils helper function, but restores the plugin host category entry munging.

--- browser/components/preferences/applications.js
+++ browser/components/preferences/applications.js
@@ -438,6 +438,11 @@ HandlerInfoWrapper.prototype = {
 
     this._prefSvc.setCharPref(PREF_DISABLED_PLUGIN_TYPES,
                               disabledPluginTypes.join(","));
+
+    // Update the category manager so existing browser windows update.
+    this._categoryMgr.deleteCategoryEntry("Gecko-Content-Viewers",
+                                          this.type,
+                                          false);
   },
 
   enablePluginType: function() {
@@ -448,6 +453,14 @@ HandlerInfoWrapper.prototype = {
 
     this._prefSvc.setCharPref(PREF_DISABLED_PLUGIN_TYPES,
                               disabledPluginTypes.join(","));
+
+    // Update the category manager so existing browser windows update.
+    this._categoryMgr.
+      addCategoryEntry("Gecko-Content-Viewers",
+                       this.type,
+                       "@mozilla.org/content/plugin/document-loader-factory;1",
+                       false,
+                       true);
   },
 
 
