9b07aa174a1bb527b47acd63d2aaa22f944b9d24
Michael Wu <mwu@mozilla.com>
2010-06-28 23:42:28 -0700

    Bug 575421 - Improve 2D drawing path on Android, r=vlad

--- widget/src/android/AndroidJavaWrappers.cpp
+++ widget/src/android/AndroidJavaWrappers.cpp
@@ -83,6 +83,7 @@ jmethodID AndroidLocation::jGetTimeMethod = 0;
 jclass AndroidGeckoSurfaceView::jGeckoSurfaceViewClass = 0;
 jmethodID AndroidGeckoSurfaceView::jBeginDrawingMethod = 0;
 jmethodID AndroidGeckoSurfaceView::jEndDrawingMethod = 0;
+jmethodID AndroidGeckoSurfaceView::jDraw2DMethod = 0;
 jmethodID AndroidGeckoSurfaceView::jGetSoftwareDrawBufferMethod = 0;
 jmethodID AndroidGeckoSurfaceView::jGetHolderMethod = 0;
 
@@ -147,6 +148,7 @@ AndroidGeckoSurfaceView::InitGeckoSurfaceViewClass(JNIEnv *jEnv)
     jBeginDrawingMethod = getMethod("beginDrawing", "()I");
     jGetSoftwareDrawBufferMethod = getMethod("getSoftwareDrawBuffer", "()Ljava/nio/ByteBuffer;");
     jEndDrawingMethod = getMethod("endDrawing", "()V");
+    jDraw2DMethod = getMethod("draw2D", "(Ljava/nio/ByteBuffer;)V");
     jGetHolderMethod = getMethod("getHolder", "()Landroid/view/SurfaceHolder;");
 }
 
@@ -361,23 +363,16 @@ AndroidGeckoSurfaceView::EndDrawing()
     JNI()->CallVoidMethod(wrapped_obj, jEndDrawingMethod);
 }
 
-unsigned char *
-AndroidGeckoSurfaceView::GetSoftwareDrawBuffer(int *cap)
+void
+AndroidGeckoSurfaceView::Draw2D(jobject buffer)
 {
-    jobject buf = JNI()->CallObjectMethod(wrapped_obj, jGetSoftwareDrawBufferMethod);
-    if (!buf)
-        return nsnull;
+    JNI()->CallVoidMethod(wrapped_obj, jDraw2DMethod, buffer);
+}
 
-    void *bp = JNI()->GetDirectBufferAddress(buf);
-    jlong blen = JNI()->GetDirectBufferCapacity(buf);
-
-    if (!bp || blen == -1)
-        return nsnull;
-
-    if (cap)
-        *cap = blen;
-
-    return (unsigned char*) bp;
+jobject
+AndroidGeckoSurfaceView::GetSoftwareDrawBuffer()
+{
+    return JNI()->CallObjectMethod(wrapped_obj, jGetSoftwareDrawBufferMethod);
 }
 
 jobject
