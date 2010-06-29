025123e9c93b98bae60bedcc14debe116c440c65
Matt Brubeck <mbrubeck@mozilla.com>
2010-06-29 20:34:48 +0200

    Bug 575295 - Clicking links broken after scrolling via in-page (uri fragment) link [r=vingtetun]

--- mobile/chrome/content/content.js
+++ mobile/chrome/content/content.js
@@ -552,10 +552,6 @@ Content.prototype = {
   },
 
   _sendMouseEvent: function _sendMouseEvent(aName, aElement, aX, aY) {
-    let scrollOffset = Util.getScrollOffset(content);
-    aX -= scrollOffset.x;
-    aY -= scrollOffset.y;
-
     // the element can be out of the aX/aY point because of the touch radius
     if (!(aElement instanceof HTMLHtmlElement)) {
       let isTouchClick = true;
@@ -577,8 +573,9 @@ Content.prototype = {
       }
     }
 
+    let scrollOffset = Util.getScrollOffset(content);
     let windowUtils = Util.getWindowUtils(content);
-    windowUtils.sendMouseEvent(aName, aX, aY, 0, 1, 0, true);
+    windowUtils.sendMouseEvent(aName, aX - scrollOffset.x, aY - scrollOffset.y, 0, 1, 0, true);
   },
 
   startLoading: function startLoading() {
