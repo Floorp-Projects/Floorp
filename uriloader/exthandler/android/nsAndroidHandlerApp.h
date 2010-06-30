fa3f69d3eca91bd8eefae45e50f6a046361f9529
Brad Lassey <blassey@mozilla.com>
2010-06-29 22:22:08 -0400

    Bug 575750 - Implement support for OS protocol handlers on Android [r=mwu,bzbarsky]

--- uriloader/exthandler/android/nsAndroidHandlerApp.h
+++ uriloader/exthandler/android/nsAndroidHandlerApp.h
@@ -45,12 +45,16 @@ public:
     NS_DECL_ISUPPORTS
     NS_DECL_NSIHANDLERAPP
     public:
-    nsAndroidHandlerApp(nsAString& aName, nsAString& aDescription);
+    nsAndroidHandlerApp(const nsAString& aName, const nsAString& aDescription,
+                        const nsAString& aPackageName, const nsAString& aClassName, 
+                        const nsACString& aMimeType);
     virtual ~nsAndroidHandlerApp();
 
 private:
     nsString mName;
     nsString mDescription;
-
+    nsCString mMimeType;
+    nsString mClassName;
+    nsString mPackageName;
 };
 #endif
