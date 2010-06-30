aa1e74af5eea83920ccbbb8471d82b08071dd1e2
Vivien Nicolas <21@vingtetun.org>
2010-06-30 19:22:57 +0200

    Bug 575939 - View is pannable once context menu has been displayed [r=mfinkle]

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -476,10 +476,10 @@ var Browser = {
     notifications.addEventListener("AlertActive", notificationHandler, false);
     notifications.addEventListener("AlertClose", notificationHandler, false);
 
+    BrowserUI.init();
+
     // initialize input handling
     ih = new InputHandler(container);
-
-    BrowserUI.init();
 
     window.controllers.appendController(this);
     window.controllers.appendController(BrowserUI);
