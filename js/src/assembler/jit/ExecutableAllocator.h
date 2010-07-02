692d954ede962af2a6ca4bf7b5830e533d361d96
Jacob Bramley <Jacob.Bramley@arm.com>
2010-07-02 11:34:56 +0100

    [JAEGER] Fix a minor #if mistake so the ARM implementation of cacheFlush works again in GCC. [Bug 572633] [r=dvander]

--- js/src/assembler/jit/ExecutableAllocator.h
+++ js/src/assembler/jit/ExecutableAllocator.h
@@ -285,7 +285,7 @@ public:
     }
 #elif WTF_CPU_ARM_TRADITIONAL && WTF_PLATFORM_LINUX && WTF_COMPILER_RVCT
     static __asm void cacheFlush(void* code, size_t size);
-#elif WTF_CPU_ARM_TRADITIONAL && WTF_PLATFORM_LINUX && WTF_COMPILER_RVCT
+#elif WTF_CPU_ARM_TRADITIONAL && WTF_PLATFORM_LINUX && WTF_COMPILER_GCC
     static void cacheFlush(void* code, size_t size)
     {
         asm volatile (
