f222832b192bfedcbcf533e6571275ccc8f88512
Karl Tomlinson <karlt+@karlt.net>
2010-07-02 16:08:06 +1200

    b=444870 include overflow in the dirty rect passed to DrawWidgetBackground() r=roc

--- widget/src/gtk2/nsNativeThemeGTK.cpp
+++ widget/src/gtk2/nsNativeThemeGTK.cpp
@@ -748,22 +748,19 @@ nsNativeThemeGTK::DrawWidgetBackground(nsIRenderingContext* aContext,
   // GTK themes can only draw an integer number of pixels
   // (even when not snapped).
   nsIntRect widgetRect(0, 0, NS_lround(rect.Width()), NS_lround(rect.Height()));
+  nsIntRect overflowRect(widgetRect);
+  nsIntMargin extraSize;
+  if (GetExtraSizeForWidget(aWidgetType, state.isDefault, &extraSize)) {
+    overflowRect.Inflate(extraSize);
+  }
 
   // This is the rectangle that will actually be drawn, in gdk pixels
   nsIntRect drawingRect(PRInt32(dirtyRect.X()),
                         PRInt32(dirtyRect.Y()),
                         PRInt32(dirtyRect.Width()),
                         PRInt32(dirtyRect.Height()));
-  if (!drawingRect.IntersectRect(widgetRect, drawingRect))
+  if (!drawingRect.IntersectRect(overflowRect, drawingRect))
     return NS_OK;
-
-  nsIntMargin extraSize;
-  // The margin should be applied to the widget rect rather than the dirty
-  // rect but nsCSSRendering::PaintBackgroundWithSC has already intersected
-  // the dirty rect with the uninflated widget rect.
-  if (GetExtraSizeForWidget(aWidgetType, state.isDefault, &extraSize)) {
-    drawingRect.Inflate(extraSize);
-  }
 
   // gdk rectangles are wrt the drawing rect.
 
