de6611ed3eba2cd7e2a18862614072d5ce29b9c3
Doug Turner <dougt@dougt.org>
2010-07-02 08:50:24 -0700

    Bug 551181 - [e10s] Tell the chrome process when we visit a page. r=sdwilsh, r=cjones, sr=bz

--- toolkit/components/places/src/History.cpp
+++ toolkit/components/places/src/History.cpp
@@ -43,6 +43,11 @@
 #include "nsXULAppAPI.h"
 #endif
 
+#ifdef MOZ_IPC
+#include "mozilla/dom/ContentChild.h"
+#include "nsXULAppAPI.h"
+#endif
+
 #include "History.h"
 #include "nsNavHistory.h"
 #include "nsNavBookmarks.h"
@@ -1078,6 +1083,16 @@ History::VisitURI(nsIURI* aURI,
   if (mShuttingDown) {
     return NS_OK;
   }
+
+#ifdef MOZ_IPC
+  if (XRE_GetProcessType() == GeckoProcessType_Content) {
+    mozilla::dom::ContentChild * cpc = 
+      mozilla::dom::ContentChild::GetSingleton();
+    NS_ASSERTION(cpc, "Content Protocol is NULL!");
+    (void)cpc->SendVisitURI(IPC::URI(aURI), IPC::URI(aLastVisitedURI), aFlags);
+    return NS_OK;
+  } 
+#endif /* MOZ_IPC */
 
   nsNavHistory* history = nsNavHistory::GetHistoryService();
   NS_ENSURE_TRUE(history, NS_ERROR_OUT_OF_MEMORY);
