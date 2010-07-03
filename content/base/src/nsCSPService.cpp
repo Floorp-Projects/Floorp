5bcba4f7bacbfac4bd4f877c7956c8874f2ca414
L. David Baron <dbaron@dbaron.org>
2010-07-02 18:15:13 -0700

    Backed out changeset f0d3b958d38d (bug 552523) for causing 137-140 mochitest failures on the two mochitest runs (both Windows) that have completed so far.

--- content/base/src/nsCSPService.cpp
+++ content/base/src/nsCSPService.cpp
@@ -117,18 +117,10 @@ CSPService::ShouldLoad(PRUint32 aContentType,
         if (csp) {
 #ifdef PR_LOGGING
             nsAutoString policy;
-            csp->GetEnforcedPolicy(policy);
-            if (policy.Length() > 0) {
-              PR_LOG(gCspPRLog, PR_LOG_DEBUG,
-                      ("Document CSP: %s",
-                      NS_ConvertUTF16toUTF8(policy).get()));
-            }
-            csp->GetReportOnlyPolicy(policy);
-            if (policy.Length() > 0) {
-              PR_LOG(gCspPRLog, PR_LOG_DEBUG,
-                      ("Report-Only CSP: %s",
-                      NS_ConvertUTF16toUTF8(policy).get()));
-            }
+            csp->GetPolicy(policy);
+            PR_LOG(gCspPRLog, PR_LOG_DEBUG,
+                    ("Document has CSP: %s",
+                     NS_ConvertUTF16toUTF8(policy).get()));
 #endif
             // obtain the enforcement decision
             csp->ShouldLoad(aContentType,
@@ -183,18 +175,10 @@ CSPService::ShouldProcess(PRUint32         aContentType,
         if (csp) {
 #ifdef PR_LOGGING
             nsAutoString policy;
-            csp->GetEnforcedPolicy(policy);
-            if (policy.Length() > 0) {
-              PR_LOG(gCspPRLog, PR_LOG_DEBUG,
-                      ("shouldProcess - CSP: %s",
-                      NS_ConvertUTF16toUTF8(policy).get()));
-            }
-            csp->GetReportOnlyPolicy(policy);
-            if (policy.Length() > 0) {
-              PR_LOG(gCspPRLog, PR_LOG_DEBUG,
-                      ("shouldProcess - Report-Only CSP: %s",
-                      NS_ConvertUTF16toUTF8(policy).get()));
-            }
+            csp->GetPolicy(policy);
+            PR_LOG(gCspPRLog, PR_LOG_DEBUG,
+                  ("shouldProcess - document has policy: %s",
+                    NS_ConvertUTF16toUTF8(policy).get()));
 #endif
             // obtain the enforcement decision
             csp->ShouldProcess(aContentType,
