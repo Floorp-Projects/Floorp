da8037f7df6e8005e86ddc1308d258987294e604
Dave Townsend <dtownsend@oxymoronical.com>
2010-07-02 20:00:45 -0700

    Backed out changeset 07a9dcc28c5c from bug 566738 due to possible browser-chrome failures.
    --HG--
    extra : transplant_source : %0C%19%86%CE%D8%F3%95%7FV%BA%3DsM%06%D4%EF%C8%B7%8Dq

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -4704,15 +4704,10 @@ nsDocShell::SetTitle(const PRUnichar * aTitle)
             treeOwnerAsWin->SetTitle(aTitle);
     }
 
-    if (mCurrentURI && mLoadType != LOAD_ERROR_PAGE) {
-        nsCOMPtr<IHistory> history = services::GetHistoryService();
-        if (history) {
-            history->SetURITitle(mCurrentURI, mTitle);
-        }
-        else if (mGlobalHistory) {
-            mGlobalHistory->SetPageTitle(mCurrentURI, nsString(mTitle));
-        }
+    if (mGlobalHistory && mCurrentURI && mLoadType != LOAD_ERROR_PAGE) {
+        mGlobalHistory->SetPageTitle(mCurrentURI, nsString(mTitle));
     }
+
 
     // Update SessionHistory with the document's title.
     if (mOSHE && mLoadType != LOAD_BYPASS_HISTORY &&
