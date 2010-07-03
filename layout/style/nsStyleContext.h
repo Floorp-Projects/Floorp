d00ce9807e1e36b58def0d5a806f93c5b992afb2
L. David Baron <dbaron@dbaron.org>
2010-07-02 21:18:55 -0700

    Add nsStyleContext::Alloc for allocations scoped to the lifetime of the style context.  (Bug 363249)  r=bzbarsky

--- layout/style/nsStyleContext.h
+++ layout/style/nsStyleContext.h
@@ -301,6 +301,27 @@ public:
   static nscolor CombineVisitedColors(nscolor *aColors,
                                       PRBool aLinkIsVisited);
 
+  /**
+   * Allocate a chunk of memory that is scoped to the lifetime of this
+   * style context, i.e., memory that will automatically be freed when
+   * this style context is destroyed.  This is intended for allocations
+   * that are stored on this style context or its style structs.  (Use
+   * on style structs is fine since any style context to which this
+   * context's style structs are shared will be a descendant of this
+   * style context and thus keep it alive.)
+   *
+   * This currently allocates the memory out of the pres shell arena.
+   *
+   * It would be relatively straightforward to write a Free method
+   * for the underlying implementation, but we don't need it (or the
+   * overhead of making a doubly-linked list or other structure to
+   * support it).
+   *
+   * WARNING: Memory allocated using this method cannot be stored in the
+   * rule tree, since rule nodes may outlive the style context.
+   */
+  void* Alloc(size_t aSize);
+
 #ifdef DEBUG
   void List(FILE* out, PRInt32 aIndent);
 #endif
@@ -310,6 +331,8 @@ protected:
   void RemoveChild(nsStyleContext* aChild);
 
   void ApplyStyleFixups(nsPresContext* aPresContext);
+
+  void FreeAllocations(nsPresContext* aPresContext);
 
   // Helper function that GetStyleData and GetUniqueStyleData use.  Only
   // returns the structs we cache ourselves; never consults the ruletree.
@@ -367,6 +390,15 @@ protected:
   // root of the rule tree, and the most specific rule matched is the
   // |mRule| member of |mRuleNode|.
   nsRuleNode* const       mRuleNode;
+
+  // Private to nsStyleContext::Alloc and FreeAllocations.
+  struct AllocationHeader {
+    AllocationHeader* mNext;
+    size_t mSize;
+
+    void* mStorageStart; // ensure the storage is at least pointer-aligned
+  };
+  AllocationHeader*       mAllocations;
 
   // mCachedInheritedData and mCachedResetData point to both structs that
   // are owned by this style context and structs that are owned by one of
