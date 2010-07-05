cb18f471fd9686a6c826eaa7f0a62ad0570ff93f
Luke Wagner <lw@mozilla.com>
2010-07-05 04:22:37 -0700

    Change the type of jsid in release builds so that it is always different than jsval

--- js/src/jsarray.h
+++ js/src/jsarray.h
@@ -254,7 +254,7 @@ js_Array(JSContext* cx, JSObject* obj, uintN argc, js::Value* argv, js::Value* r
  * resulting array has length and count both equal to |capacity|.
  */
 JS_FRIEND_API(JSObject *)
-js_NewArrayObjectWithCapacity(JSContext *cx, uint32 capacity, jsval **vector);
+js_NewArrayObjectWithCapacity(JSContext *cx, uint32_t capacity, jsval **vector);
 
 /*
  * Makes a fast clone of a dense array as long as the array only contains
