5a7ed388da73ec81cfd64fad4a0535ad56df5d1b
Mark Finkle <mfinkle@mozilla.com>
2010-07-09 04:17:58 -0400

    Bug 575999  - Context menu pops up when clicking on links (missed a nit) [r=me]

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -59,7 +59,7 @@ pref("zoom.dpiScale", 150);
 pref("ui.use_native_popup_windows", true);
 
 /* use long press to display a context menu */
-pref("ui.click_hold_context_menus", true);
+pref("ui.click_hold_context_menus", false);
 
 /* cache prefs */
 pref("browser.cache.disk.enable", false);
