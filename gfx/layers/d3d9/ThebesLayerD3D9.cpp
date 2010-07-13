bc942e367ec489755deb5ac9ed7e4edbb15381d9
Bas Schouten <bschouten@mozilla.com>
2010-07-14 00:23:32 +0200

    Bug 577462: Addition - add comment requested in review.

--- gfx/layers/d3d9/ThebesLayerD3D9.cpp
+++ gfx/layers/d3d9/ThebesLayerD3D9.cpp
@@ -195,6 +195,8 @@ ThebesLayerD3D9::RenderLayer()
 
   nsIntRect visibleRect = mVisibleRegion.GetBounds();
 
+  // We differentiate between these formats since D3D9 will only allow us to
+  // call GetDC on an opaque surface.
   D3DFORMAT fmt = UseOpaqueSurface(this) ? D3DFMT_X8R8G8B8 : D3DFMT_A8R8G8B8;
   if (mTexture) {
     D3DSURFACE_DESC desc;
