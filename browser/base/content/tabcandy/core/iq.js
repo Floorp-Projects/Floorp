1118a027193f6c2b640795c878d1e7af867d8586
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2010-07-13 20:28:36 -0400

    cleanup: iQ.each: never used with args, so that code is removed

--- browser/base/content/tabcandy/core/iq.js
+++ browser/base/content/tabcandy/core/iq.js
@@ -932,38 +932,20 @@ iQ.extend({
   // ----------
   // Function: each
   // args is for internal usage only
-  each: function( object, callback, args ) {
+  each: function( object, callback ) {
     var name, i = 0,
       length = object.length,
       isObj = length === undefined || iQ.isFunction(object);
 
-    if ( args ) {
-      if ( isObj ) {
-        for ( name in object ) {
-          if ( callback.apply( object[ name ], args ) === false ) {
-            break;
-          }
-        }
-      } else {
-        for ( ; i < length; ) {
-          if ( callback.apply( object[ i++ ], args ) === false ) {
-            break;
-          }
+    if ( isObj ) {
+      for ( name in object ) {
+        if ( callback.call( object[ name ], name, object[ name ] ) === false ) {
+          break;
         }
       }
-
-    // A special, fast, case for the most common use of each
     } else {
-      if ( isObj ) {
-        for ( name in object ) {
-          if ( callback.call( object[ name ], name, object[ name ] ) === false ) {
-            break;
-          }
-        }
-      } else {
-        for ( var value = object[0];
-          i < length && callback.call( value, i, value ) !== false; value = object[++i] ) {}
-      }
+      for ( var value = object[0];
+        i < length && callback.call( value, i, value ) !== false; value = object[++i] ) {}
     }
 
     return object;
