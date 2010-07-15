ea2fefefb7c80ed4b26e7bdf5e1e3df8fb8fcde0
Andreas Gal <gal@mozilla.com>
2010-07-15 10:21:34 -0700

    Disentangle cycle collector and JS GC (549806, r=peterv, igor).

--- js/src/xpconnect/src/xpcwrappednativescope.cpp
+++ js/src/xpconnect/src/xpcwrappednativescope.cpp
@@ -408,15 +408,20 @@ WrappedNativeSuspecter(JSDHashTable *table, JSDHashEntryHdr *hdr,
 {
     SuspectClosure* closure = static_cast<SuspectClosure*>(arg);
     XPCWrappedNative* wrapper = ((Native2WrappedNativeMap::Entry*)hdr)->value;
-    if(wrapper->IsValid())
+
+    if(wrapper->IsValid() &&
+       wrapper->HasExternalReference() &&
+       !wrapper->IsWrapperExpired())
     {
         NS_ASSERTION(NS_IsMainThread(), 
                      "Suspecting wrapped natives from non-main thread");
+        NS_ASSERTION(!JS_IsAboutToBeFinalized(closure->cx, wrapper->GetFlatJSObject()),
+                     "WrappedNativeSuspecter attempting to touch dead object");
 
         // Only record objects that might be part of a cycle as roots, unless
         // the callback wants all traces (a debug feature).
-        if(!(closure->cb.WantAllTraces()) && 
-           !JS_IsAboutToBeFinalized(closure->cx, wrapper->GetFlatJSObject()))
+        if(!(closure->cb.WantAllTraces()) &&
+           !nsXPConnect::IsGray(wrapper->GetFlatJSObject()))
             return JS_DHASH_NEXT;
 
         closure->cb.NoteRoot(nsIProgrammingLanguage::JAVASCRIPT,
