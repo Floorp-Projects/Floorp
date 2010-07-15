6bb6f55b688c33e85ee27675b74890ce8605dbef
Robert O'Callahan <robert@ocallahan.org>
2010-07-16 09:08:09 +1200

    Bug 577631. Don't pass zero sizes to XCreatePixmap. r=jrmuizel

--- gfx/thebes/gfxXlibSurface.cpp
+++ gfx/thebes/gfxXlibSurface.cpp
@@ -44,6 +44,7 @@
 #include <X11/Xlibint.h>	/* For XESetCloseDisplay */
 
 #include "nsTArray.h"
+#include "nsAlgorithm.h"
 #include "nsServiceManagerUtils.h"
 #include "nsIPrefService.h"
 
@@ -117,8 +118,11 @@ CreatePixmap(Screen *screen, const gfxIntSize& size, unsigned int depth,
         relatedDrawable = RootWindowOfScreen(screen);
     }
     Display *dpy = DisplayOfScreen(screen);
+    // X gives us a fatal error if we try to create a pixmap of width
+    // or height 0
     return XCreatePixmap(dpy, relatedDrawable,
-                         size.width, size.height, depth);
+                         NS_MAX(1, size.width), NS_MAX(1, size.height),
+                         depth);
 }
 
 /* static */
