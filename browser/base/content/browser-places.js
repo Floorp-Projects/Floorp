9f6a69d39ca416dc9aebb9c7780c0724a3c39e4b
Marco Bonardo <mbonardo@mozilla.com>
2010-07-15 22:38:05 +0200

    Bug 575896 - Reset bookmarks toolbar view during customization. r=dao

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -1067,30 +1067,24 @@ var PlacesStarButton = {
 // after closing the toolbar customization dialog.
 let PlacesToolbarHelper = {
   _place: "place:folder=TOOLBAR",
-  _cachedElt: null,
 
-  onBrowserWindowClose: function PTH_onBrowserWindowClose() {
-    if (this._cachedElt)
-      this._cachedElt._placesView.uninit();
+  get _viewElt() {
+    return document.getElementById("PlacesToolbar");
   },
 
-  updateState: function PTH_updateState() {
-    let currentElt = document.getElementById("PlacesToolbar");
-
-    // Bail out if the state has not changed.
-    if (currentElt == this._cachedElt)
-      return;
-
-    if (!this._cachedElt) {
-      // The toolbar has been added.
+  init: function PTH_init() {
+    if (this._viewElt)
       new PlacesToolbar(this._place);
-      this._cachedElt = currentElt;
-    }
-    else {
-      // The toolbar has been removed.
-      this._cachedElt._placesView.uninit();
-      this._cachedElt = null;
-    }
+  },
+
+  customizeStart: function PTH_customizeStart() {
+    let viewElt = this._viewElt;
+    if (viewElt && viewElt._placesView)
+      viewElt._placesView.uninit();
+  },
+
+  customizeDone: function PTH_customizeDone() {
+    this.init();
   }
 };
 
