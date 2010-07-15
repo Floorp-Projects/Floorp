9d3483b9f4e24e315f12fec76b271a6e911b7dc5
Blake Kaplan <mrbkap@gmail.com>
2010-07-14 17:39:22 -0700

    Fix bustage from bug 578392 (r=sayrer on the bustage fix).

--- modules/libpref/src/nsPrefBranch.h
+++ modules/libpref/src/nsPrefBranch.h
@@ -67,8 +67,10 @@ public:
 
   PRInt32 GetRootLength() { return mPrefRootLength; }
 
+  nsresult RemoveObserverFromList(const char *aDomain, nsISupports *aObserver);
+
 protected:
-  nsPrefBranch()	/* disallow use of this constructer */
+  nsPrefBranch()    /* disallow use of this constructer */
     { }
 
   nsresult   GetDefaultFromPropertiesFile(const char *aPrefName, PRUnichar **return_buf);
