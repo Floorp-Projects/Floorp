1345979dc28196ee07c5432ade374ee7bcd3e32e
Julian Viereck <jviereck@mozilla.com>
2010-07-16 12:00:38 -0300

    bug 568652 - jsTerm history keybindings, r=dietrich

--- toolkit/components/console/hudservice/tests/browser/browser_HUDServiceTestsAll.js
+++ toolkit/components/console/hudservice/tests/browser/browser_HUDServiceTestsAll.js
@@ -316,6 +316,52 @@ function testRecordManyEntries() {
      "1001 entries in foo now");
 }
 
+function testConsoleHistory()
+{
+  let HUD = HUDService.hudWeakReferences[hudId].get();
+  let jsterm = HUD.jsterm;
+  let input = jsterm.inputNode;
+
+  let executeList = ["document", "window", "window.location"];
+
+  for each (var item in executeList) {
+    input.value = item;
+    jsterm.execute();
+  }
+
+  for (var i = executeList.length - 1; i != -1; i--) {
+    jsterm.historyPeruse(true);
+    is (input.value, executeList[i], "check history previous idx:" + i);
+  }
+
+  jsterm.historyPeruse(true);
+  is (input.value, executeList[0], "test that item is still index 0");
+
+  jsterm.historyPeruse(true);
+  is (input.value, executeList[0], "test that item is still still index 0");
+
+
+  for (var i = 1; i < executeList.length; i++) {
+    jsterm.historyPeruse(false);
+    is (input.value, executeList[i], "check history next idx:" + i);
+  }
+
+  jsterm.historyPeruse(false);
+  is (input.value, "", "check input is empty again");
+
+  // Simulate pressing Arrow_Down a few times and then if Arrow_Up shows
+  // the previous item from history again.
+  jsterm.historyPeruse(false);
+  jsterm.historyPeruse(false);
+  jsterm.historyPeruse(false);
+
+  is (input.value, "", "check input is still empty");
+
+  let idxLast = executeList.length - 1;
+  jsterm.historyPeruse(true);
+  is (input.value, executeList[idxLast], "check history next idx:" + idxLast);
+}
+
 function testIteration() {
   var id = "foo";
   var it = cs.displayStore(id);
@@ -394,6 +440,7 @@ function test() {
       testRecordEntry();
       testRecordManyEntries();
       testIteration();
+      testConsoleHistory();
 
       // testUnregister();
       executeSoon(function () {
