05862cfc39df34da56560c6b264d93bd6b0580b0
Blake Kaplan <mrbkap@gmail.com>
2010-07-19 13:50:20 -0700

    Bug 579616 - Fix leak when a cloned node has an XBL binding attached to it: clean up the cycle when the ownerdoc goes away and we will no longer be able to traverse the cycle. r=sicking

--- content/xbl/src/nsBindingManager.cpp
+++ content/xbl/src/nsBindingManager.cpp
@@ -1,4 +1,5 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 sw=2 et tw=79: */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
@@ -626,6 +627,9 @@ nsBindingManager::RemovedFromDocumentInternal(nsIContent* aContent,
                                               nsIDocument* aOldDocument)
 {
   NS_PRECONDITION(aOldDocument != nsnull, "no old document");
+
+  if (mDestroyed)
+    return;
 
   // Hold a ref to the binding so it won't die when we remove it from our
   // table.
@@ -1650,6 +1654,8 @@ nsBindingManager::ContentRemoved(nsIDocument* aDocument,
 void
 nsBindingManager::DropDocumentReference()
 {
+  mDestroyed = PR_TRUE;
+
   // Make sure to not run any more XBL constructors
   mProcessingAttachedStack = PR_TRUE;
   if (mProcessAttachedQueueEvent) {
@@ -1667,6 +1673,9 @@ nsBindingManager::DropDocumentReference()
   if (mInsertionParentTable.ops)
     PL_DHashTableFinish(&(mInsertionParentTable));
   mInsertionParentTable.ops = nsnull;
+
+  if (mBindingTable.IsInitialized())
+    mBindingTable.Clear();
 
   mDocument = nsnull;
 }
