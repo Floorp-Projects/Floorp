f3866c0a679ba2df39f8033ceff8dfa4ddcde5d1
Jeff Walden <jwalden@mit.edu>
2010-07-20 12:46:58 -0500

    Bug 580200 - Assertion failure, or duplicated enumeration, enumerating own properties of proxy returning duplicated property names.  r=jorendorff
    --HG--
    extra : rebase_source : 19d4bf112e0dfdb5ed619f9d6e1f72b72a3e0801

---
+++ js/src/tests/js1_8_5/extensions/proxy-enumerateOwn-duplicates.js
@@ -0,0 +1,32 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/licenses/publicdomain/
+
+var gTestfile = 'proxy-enumerateOwn-duplicates.js';
+//-----------------------------------------------------------------------------
+var BUGNUMBER = 580200;
+var summary =
+  'Assertion failure enumerating own properties of proxy returning ' +
+  'duplicated own property name';
+print(BUGNUMBER + ": " + summary);
+
+/**************
+ * BEGIN TEST *
+ **************/
+
+var x = Proxy.create({ enumerateOwn: function() { return ["0","0"]; } }, [1,2]);
+var ax = Object.getOwnPropertyNames(x);
+assertEq(ax.length, 1, "array: " + ax);
+assertEq(ax[0], "0");
+
+var p = Proxy.create({ enumerateOwn: function() { return ["1","1"]; } }, null);
+var ap = Object.getOwnPropertyNames(p);
+assertEq(ap.length, 1, "array: " + ap);
+assertEq(ap[0], "1");
+
+
+/******************************************************************************/
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
+
+print("All tests passed!");
