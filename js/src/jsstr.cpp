bb1f9884faf4d925159a3099e89c39967aea6e73
David Anderson <danderson@mozilla.com>
2010-07-19 21:14:44 -0700

    [JAEGER] Fixed bug in lambda replace w/ eager this.

--- js/src/jsstr.cpp
+++ js/src/jsstr.cpp
@@ -2018,7 +2018,7 @@ FindReplaceLength(JSContext *cx, ReplaceData &rdata, size_t *sizep)
         /* Push lambda and its 'this' parameter. */
         Value *sp = rdata.args.getvp();
         sp++->setObject(*lambda);
-        sp++->setObjectOrNull(lambda->getParent());
+        sp++->setNull();
 
         /* Push $&, $1, $2, ... */
         if (!PushRegExpSubstr(cx, cx->regExpStatics.lastMatch, sp))
