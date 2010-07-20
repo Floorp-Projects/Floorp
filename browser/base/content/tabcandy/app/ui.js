4b7c39f45bef4a5e468574d44729b41f1a031db5
Raymond Lee <raymond@raysquare.com>
2010-07-20 10:27:03 +0800

    Change a forEach to map in the showOnlyTheseTabs()

--- browser/base/content/tabcandy/app/ui.js
+++ browser/base/content/tabcandy/app/ui.js
@@ -113,11 +113,7 @@ var Tabbar = {
 
       var tabbrowser = Utils.getCurrentWindow().gBrowser;
       var tabBarTabs = this.getAllTabs();
-      var visibleTabs = [];
-
-      tabs.forEach(function(tab) {
-        visibleTabs.push(tab.tab.raw);
-      });
+      var visibleTabs = tabs.map(function(tab) tab.tab.raw);
 
       // Show all of the tabs in the group.
       tabBarTabs.forEach(function(tab){
