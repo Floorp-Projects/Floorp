cb3569dfca9f85fb6c3676168bf7af07573b7694
Alexander Surkov <surkov.alexander@gmail.com>
2010-07-21 08:12:08 +0400

    Bug 575693 - Download Manager and Page Info no longer have an accessible tree, r=davidb, marcoz, sr=roc

--- accessible/src/msaa/nsRootAccessibleWrap.h
+++ accessible/src/msaa/nsRootAccessibleWrap.h
@@ -43,15 +43,8 @@
 #ifndef _nsRootAccessibleWrap_H_
 #define _nsRootAccessibleWrap_H_
 
-#include "nsCOMPtr.h"
 #include "nsRootAccessible.h"
 
-class nsRootAccessibleWrap: public nsRootAccessible
-{
-public:
-  nsRootAccessibleWrap(nsIDocument *aDocument, nsIContent *aRootContent,
-                       nsIWeakReference *aShell);
-  virtual ~nsRootAccessibleWrap();
-};
+typedef class nsRootAccessible nsRootAccessibleWrap;
 
 #endif
