61d26ac5e18eada14f0ac7830f97adffce434f0b
Edward Lee <edilee@mozilla.com>
2010-07-21 00:35:09 -0700

    Resolve merge conflicts.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -7757,16 +7757,12 @@ var TabContextMenu = {
   updateContextMenu: function updateContextMenu(aPopupMenu) {
     this.contextTab = document.popupNode.localName == "tab" ?
                       document.popupNode : gBrowser.selectedTab;
-<<<<<<< local
     var disabled = gBrowser.visibleTabs.length == 1;
-=======
-    var disabled = gBrowser.tabs.length == 1;
 
     // Enable the "Close Tab" menuitem when the window doesn't close with the last tab.
     document.getElementById("context_closeTab").disabled =
       disabled && gBrowser.tabContainer._closeWindowWithLastTab;
 
->>>>>>> other
     var menuItems = aPopupMenu.getElementsByAttribute("tbattr", "tabbrowser-multiple");
     for (var i = 0; i < menuItems.length; i++)
       menuItems[i].disabled = disabled;
