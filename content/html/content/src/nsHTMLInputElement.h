66d7877e5f26726aa9a26601f9d8cdd691e6cd08
Mounir Lamouri <mounir.lamouri@gmail.com>
2010-07-22 17:28:03 +0200

    Bug 572897 - Do not always call SetValueChanged(PR_TRUE) when SetValueInternal is called; r=Olli.Pettay approval2.0=benjamin

--- content/html/content/src/nsHTMLInputElement.h
+++ content/html/content/src/nsHTMLInputElement.h
@@ -208,7 +208,8 @@ protected:
 
   // Helper method
   nsresult SetValueInternal(const nsAString& aValue,
-                            PRBool aUserInput);
+                            PRBool aUserInput,
+                            PRBool aSetValueChanged);
 
   void ClearFileNames() {
     nsTArray<nsString> fileNames;
