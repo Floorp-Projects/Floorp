28bb4235737ec223689d8bb25130934b39e06b59
Zack Weinberg <zweinberg@mozilla.com>
2010-07-23 11:00:27 -0700

    Bug 569719 part 6: Move Declaration::AppendCSSValueToString into nsCSSValue.  r=dbaron

--- layout/style/Declaration.h
+++ layout/style/Declaration.h
@@ -187,11 +187,6 @@ public:
   void List(FILE* out = stdout, PRInt32 aIndent = 0) const;
 #endif
 
-  // return whether there was a value in |aValue| (i.e., it had a non-null unit)
-  static PRBool AppendCSSValueToString(nsCSSProperty aProperty,
-                                       const nsCSSValue& aValue,
-                                       nsAString& aResult);
-
   // return whether there was a value in |aStorage| (i.e., it was non-null)
   static PRBool AppendStorageToString(nsCSSProperty aProperty,
                                       const void* aStorage,
