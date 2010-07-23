85cd8e693cac2e404a42ddac4dbb079507c24da4
Zack Weinberg <zweinberg@mozilla.com>
2010-07-23 11:00:19 -0700

    Bug 569719 part 3: collapse nsDOMCSSDeclaration::ParseDeclaration into its sole caller (nsDOMCSSDeclaration::SetCSSText); remove always-constant flag arguments to nsCSSParser::ParseAndAppendDeclaration; rename that method to ParseDeclarations.  r=dbaron

--- layout/style/nsCSSParser.h
+++ layout/style/nsCSSParser.h
@@ -130,14 +130,17 @@ public:
                                nsIPrincipal*     aNodePrincipal,
                                nsICSSStyleRule** aResult);
 
-  nsresult ParseAndAppendDeclaration(const nsAString&  aBuffer,
-                                     nsIURI*           aSheetURL,
-                                     nsIURI*           aBaseURL,
-                                     nsIPrincipal*     aSheetPrincipal,
-                                     mozilla::css::Declaration* aDeclaration,
-                                     PRBool            aParseOnlyOneDecl,
-                                     PRBool*           aChanged,
-                                     PRBool            aClearOldDecl);
+  // Parse the body of a declaration block.  Very similar to
+  // ParseStyleAttribute, but used under different circumstances.
+  // The contents of aDeclaration will be erased and replaced with the
+  // results of parsing; aChanged will be set true if the aDeclaration
+  // argument was modified.
+  nsresult ParseDeclarations(const nsAString&  aBuffer,
+                             nsIURI*           aSheetURL,
+                             nsIURI*           aBaseURL,
+                             nsIPrincipal*     aSheetPrincipal,
+                             mozilla::css::Declaration* aDeclaration,
+                             PRBool*           aChanged);
 
   nsresult ParseRule(const nsAString&        aRule,
                      nsIURI*                 aSheetURL,
