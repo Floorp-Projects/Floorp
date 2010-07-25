c4d47fb7d10fe0fce5b98a07a0e15ee42350d750
Luke Wagner <lw@mozilla.com>
2010-07-24 20:09:39 -0700

    [JAEGER] Move regs->sp dec from popInlineFrame to caller to get ready for upcoming changes

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -2776,11 +2776,13 @@ BEGIN_CASE(JSOP_STOP)
         }
 
         JSStackFrame *down = fp->down;
+        Value *newsp = fp->argv - 1;
 
         /* Pop the frame. */
         cx->stack().popInlineFrame(cx, fp, down);
 
         /* Propagate return value before fp is lost. */
+        regs.sp = newsp;
         regs.sp[-1] = fp->rval;
 
         /* Sync interpreter registers. */
