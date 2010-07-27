5e1317668f61cb54efdc0b853367c0145a45a97f
Masayuki Nakano <masayuki@d-toybox.com>
2010-07-27 21:48:59 +0900

    Bug 574340 Cleaning up nsKeyboardLayout which doesn't use our coding style. Part.2 Remove unnecessary white spaces and wrap long lines r=jmatheis, a2=joe

--- widget/src/windows/KeyboardLayout.h
+++ widget/src/windows/KeyboardLayout.h
@@ -83,7 +83,7 @@ class VirtualKey
   {
     struct
     {
-      PRUnichar Chars [4];
+      PRUnichar Chars[4];
     } Normal;
     struct
     {
@@ -92,10 +92,10 @@ class VirtualKey
     } DeadKey;
   };
 
-  KeyShiftState mShiftStates [16];
+  KeyShiftState mShiftStates[16];
   PRUint16 mIsDeadKey;
 
-  void SetDeadKey (PRUint8 aShiftState, PRBool aIsDeadKey)
+  void SetDeadKey(PRUint8 aShiftState, PRBool aIsDeadKey)
   {
     if (aIsDeadKey)
       mIsDeadKey |= 1 << aShiftState;
@@ -104,22 +104,28 @@ class VirtualKey
   }
 
 public:
-  PRBool IsDeadKey (PRUint8 aShiftState) const
+  PRBool IsDeadKey(PRUint8 aShiftState) const
   {
     return (mIsDeadKey & (1 << aShiftState)) != 0;
   }
 
-  void AttachDeadKeyTable (PRUint8 aShiftState, const DeadKeyTable* aDeadKeyTable)
+  void AttachDeadKeyTable(PRUint8 aShiftState,
+                          const DeadKeyTable* aDeadKeyTable)
   {
-    mShiftStates [aShiftState].DeadKey.Table = aDeadKeyTable;
+    mShiftStates[aShiftState].DeadKey.Table = aDeadKeyTable;
   }
 
-  void SetNormalChars (PRUint8 aShiftState, const PRUnichar* aChars, PRUint32 aNumOfChars);
-  void SetDeadChar (PRUint8 aShiftState, PRUnichar aDeadChar);
-  const DeadKeyTable* MatchingDeadKeyTable (const DeadKeyEntry* aDeadKeyArray, PRUint32 aEntries) const;
-  inline PRUnichar GetCompositeChar (PRUint8 aShiftState, PRUnichar aBaseChar) const;
-  PRUint32 GetNativeUniChars (PRUint8 aShiftState, PRUnichar* aUniChars = nsnull) const;
-  PRUint32 GetUniChars (PRUint8 aShiftState, PRUnichar* aUniChars, PRUint8* aFinalShiftState) const;
+  void SetNormalChars(PRUint8 aShiftState, const PRUnichar* aChars,
+                      PRUint32 aNumOfChars);
+  void SetDeadChar(PRUint8 aShiftState, PRUnichar aDeadChar);
+  const DeadKeyTable* MatchingDeadKeyTable(const DeadKeyEntry* aDeadKeyArray,
+                                           PRUint32 aEntries) const;
+  inline PRUnichar GetCompositeChar(PRUint8 aShiftState,
+                                    PRUnichar aBaseChar) const;
+  PRUint32 GetNativeUniChars(PRUint8 aShiftState,
+                             PRUnichar* aUniChars = nsnull) const;
+  PRUint32 GetUniChars(PRUint8 aShiftState, PRUnichar* aUniChars,
+                       PRUint8* aFinalShiftState) const;
 };
 
 
@@ -128,50 +134,59 @@ class KeyboardLayout
   struct DeadKeyTableListEntry
   {
     DeadKeyTableListEntry* next;
-    PRUint8 data [1];
+    PRUint8 data[1];
   };
 
   #define NUM_OF_KEYS   50
 
   HKL mKeyboardLayout;
 
-  VirtualKey mVirtualKeys [NUM_OF_KEYS];
+  VirtualKey mVirtualKeys[NUM_OF_KEYS];
   DeadKeyTableListEntry* mDeadKeyTableListHead;
   PRInt32 mActiveDeadKey;                 // -1 = no active dead-key
   PRUint8 mDeadKeyShiftState;
   PRInt32 mLastVirtualKeyIndex;
   PRUint8 mLastShiftState;
-  PRUnichar mChars [5];                   // Dead-key + up to 4 characters
-  PRUint8 mShiftStates [5];
+  PRUnichar mChars[5];                    // Dead-key + up to 4 characters
+  PRUint8 mShiftStates[5];
   PRUint8 mNumOfChars;
 
-  static PRUint8 GetShiftState (const PBYTE aKbdState);
-  static void SetShiftState (PBYTE aKbdState, PRUint8 aShiftState);
-  static inline PRInt32 GetKeyIndex (PRUint8 aVirtualKey);
-  static int CompareDeadKeyEntries (const void* aArg1, const void* aArg2, void* aData);
-  static PRBool AddDeadKeyEntry (PRUnichar aBaseChar, PRUnichar aCompositeChar, DeadKeyEntry* aDeadKeyArray, PRUint32 aEntries);
-  PRBool EnsureDeadKeyActive (PRBool aIsActive, PRUint8 aDeadKey, const PBYTE aDeadKeyKbdState);
-  PRUint32 GetDeadKeyCombinations (PRUint8 aDeadKey, const PBYTE aDeadKeyKbdState, PRUint16 aShiftStatesWithBaseChars,
-                                   DeadKeyEntry* aDeadKeyArray, PRUint32 aMaxEntries);
-  void DeactivateDeadKeyState ();
-  const DeadKeyTable* AddDeadKeyTable (const DeadKeyEntry* aDeadKeyArray, PRUint32 aEntries);
-  void ReleaseDeadKeyTables ();
+  static PRUint8 GetShiftState(const PBYTE aKbdState);
+  static void SetShiftState(PBYTE aKbdState, PRUint8 aShiftState);
+  static inline PRInt32 GetKeyIndex(PRUint8 aVirtualKey);
+  static int CompareDeadKeyEntries(const void* aArg1, const void* aArg2,
+                                   void* aData);
+  static PRBool AddDeadKeyEntry(PRUnichar aBaseChar, PRUnichar aCompositeChar,
+                                DeadKeyEntry* aDeadKeyArray, PRUint32 aEntries);
+  PRBool EnsureDeadKeyActive(PRBool aIsActive, PRUint8 aDeadKey,
+                             const PBYTE aDeadKeyKbdState);
+  PRUint32 GetDeadKeyCombinations(PRUint8 aDeadKey,
+                                  const PBYTE aDeadKeyKbdState,
+                                  PRUint16 aShiftStatesWithBaseChars,
+                                  DeadKeyEntry* aDeadKeyArray,
+                                  PRUint32 aMaxEntries);
+  void DeactivateDeadKeyState();
+  const DeadKeyTable* AddDeadKeyTable(const DeadKeyEntry* aDeadKeyArray,
+                                      PRUint32 aEntries);
+  void ReleaseDeadKeyTables();
 
 public:
-  KeyboardLayout ();
-  ~KeyboardLayout ();
+  KeyboardLayout();
+  ~KeyboardLayout();
 
-  static PRBool IsPrintableCharKey (PRUint8 aVirtualKey);
-  static PRBool IsNumpadKey (PRUint8 aVirtualKey);
+  static PRBool IsPrintableCharKey(PRUint8 aVirtualKey);
+  static PRBool IsNumpadKey(PRUint8 aVirtualKey);
 
-  PRBool IsDeadKey () const
+  PRBool IsDeadKey() const
   {
-    return (mLastVirtualKeyIndex >= 0) ? mVirtualKeys [mLastVirtualKeyIndex].IsDeadKey (mLastShiftState) : PR_FALSE;
+    return (mLastVirtualKeyIndex >= 0) ?
+      mVirtualKeys[mLastVirtualKeyIndex].IsDeadKey(mLastShiftState) : PR_FALSE;
   }
 
-  void LoadLayout (HKL aLayout);
-  void OnKeyDown (PRUint8 aVirtualKey);
-  PRUint32 GetUniChars (PRUnichar* aUniChars, PRUint8* aShiftStates, PRUint32 aMaxChars) const;
+  void LoadLayout(HKL aLayout);
+  void OnKeyDown(PRUint8 aVirtualKey);
+  PRUint32 GetUniChars(PRUnichar* aUniChars, PRUint8* aShiftStates,
+                       PRUint32 aMaxChars) const;
   PRUint32 GetUniCharsWithShiftState(PRUint8 aVirtualKey, PRUint8 aShiftStates,
                                      PRUnichar* aUniChars,
                                      PRUint32 aMaxChars) const;
