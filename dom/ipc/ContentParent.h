d34ac3ee4581b587d9dfbec884f28b1988b7d6fe
Kyle Huey <me@kylehuey.com>
2010-07-27 17:13:20 -0700

    Backed out changeset 998a00c4052b

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -47,7 +47,6 @@
 #include "nsIObserver.h"
 #include "nsIThreadInternal.h"
 #include "mozilla/Monitor.h"
-#include "nsNetUtil.h"
 #include "nsIPrefService.h"
 #include "nsIPermissionManager.h"
 
@@ -147,10 +146,6 @@ private:
     void EnsurePermissionService();
 
     virtual bool RecvStartVisitedQuery(const IPC::URI& uri);
-
-    virtual bool RecvVisitURI(const IPC::URI& uri,
-                              const IPC::URI& referrer,
-                              const PRUint32& flags);
 
     mozilla::Monitor mMonitor;
 
