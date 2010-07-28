78b092c5447540aa84bd8355da6f51a265a8341c
Vladimir Vukicevic <vladimir@pobox.com>
2010-07-28 14:24:09 -0700

    b=580345; fix up some GLESv2 vs. desktop GL differences; r=joe

--- gfx/thebes/GLContext.cpp
+++ gfx/thebes/GLContext.cpp
@@ -173,33 +173,17 @@ GLContext::InitWithPrefix(const char *prefix, PRBool trygl)
         { (PRFuncPtr*) &fBufferSubData, { "BufferSubData", NULL } },
         { (PRFuncPtr*) &fClear, { "Clear", NULL } },
         { (PRFuncPtr*) &fClearColor, { "ClearColor", NULL } },
-#ifdef USE_GLES2
-        { (PRFuncPtr*) &fClearDepthf, { "ClearDepthf", NULL } },
-#else
-        { (PRFuncPtr*) &fClearDepth, { "ClearDepth", NULL } },
-#endif
         { (PRFuncPtr*) &fClearStencil, { "ClearStencil", NULL } },
         { (PRFuncPtr*) &fColorMask, { "ColorMask", NULL } },
         { (PRFuncPtr*) &fCullFace, { "CullFace", NULL } },
         { (PRFuncPtr*) &fDetachShader, { "DetachShader", "DetachShaderARB", NULL } },
         { (PRFuncPtr*) &fDepthFunc, { "DepthFunc", NULL } },
         { (PRFuncPtr*) &fDepthMask, { "DepthMask", NULL } },
-#ifdef USE_GLES2
-        { (PRFuncPtr*) &fDepthRangef, { "DepthRangef", NULL } },
-#else
-        { (PRFuncPtr*) &fDepthRange, { "DepthRange", NULL } },
-#endif
         { (PRFuncPtr*) &fDisable, { "Disable", NULL } },
-#ifndef USE_GLES2
-        { (PRFuncPtr*) &fDisableClientState, { "DisableClientState", NULL } },
-#endif
         { (PRFuncPtr*) &fDisableVertexAttribArray, { "DisableVertexAttribArray", "DisableVertexAttribArrayARB", NULL } },
         { (PRFuncPtr*) &fDrawArrays, { "DrawArrays", NULL } },
         { (PRFuncPtr*) &fDrawElements, { "DrawElements", NULL } },
         { (PRFuncPtr*) &fEnable, { "Enable", NULL } },
-#ifndef USE_GLES2
-        { (PRFuncPtr*) &fEnableClientState, { "EnableClientState", NULL } },
-#endif
         { (PRFuncPtr*) &fEnableVertexAttribArray, { "EnableVertexAttribArray", "EnableVertexAttribArrayARB", NULL } },
         { (PRFuncPtr*) &fFinish, { "Finish", NULL } },
         { (PRFuncPtr*) &fFlush, { "Flush", NULL } },
@@ -215,9 +199,6 @@ GLContext::InitWithPrefix(const char *prefix, PRBool trygl)
         { (PRFuncPtr*) &fGetError, { "GetError", NULL } },
         { (PRFuncPtr*) &fGetProgramiv, { "GetProgramiv", "GetProgramivARB", NULL } },
         { (PRFuncPtr*) &fGetProgramInfoLog, { "GetProgramInfoLog", "GetProgramInfoLogARB", NULL } },
-#ifndef USE_GLES2
-        { (PRFuncPtr*) &fTexCoordPointer, { "TexCoordPointer", NULL } },
-#endif
         { (PRFuncPtr*) &fTexParameteri, { "TexParameteri", NULL } },
         { (PRFuncPtr*) &fTexParameterf, { "TexParameterf", NULL } },
         { (PRFuncPtr*) &fGetString, { "GetString", NULL } },
@@ -238,9 +219,6 @@ GLContext::InitWithPrefix(const char *prefix, PRBool trygl)
         { (PRFuncPtr*) &fLinkProgram, { "LinkProgram", "LinkProgramARB", NULL } },
         { (PRFuncPtr*) &fPixelStorei, { "PixelStorei", NULL } },
         { (PRFuncPtr*) &fPolygonOffset, { "PolygonOffset", NULL } },
-#ifndef USE_GLES2
-        { (PRFuncPtr*) &fReadBuffer,  { "ReadBuffer", NULL } },
-#endif
         { (PRFuncPtr*) &fReadPixels, { "ReadPixels", NULL } },
         { (PRFuncPtr*) &fSampleCoverage, { "SampleCoverage", NULL } },
         { (PRFuncPtr*) &fScissor, { "Scissor", NULL } },
@@ -250,9 +228,6 @@ GLContext::InitWithPrefix(const char *prefix, PRBool trygl)
         { (PRFuncPtr*) &fStencilMaskSeparate, { "StencilMaskSeparate", "StencilMaskSeparateEXT", NULL } },
         { (PRFuncPtr*) &fStencilOp, { "StencilOp", NULL } },
         { (PRFuncPtr*) &fStencilOpSeparate, { "StencilOpSeparate", "StencilOpSeparateEXT", NULL } },
-#ifndef USE_GLES2
-        { (PRFuncPtr*) &fTexEnvf, { "TexEnvf",  NULL } },
-#endif
         { (PRFuncPtr*) &fTexImage2D, { "TexImage2D", NULL } },
         { (PRFuncPtr*) &fTexSubImage2D, { "TexSubImage2D", NULL } },
         { (PRFuncPtr*) &fUniform1f, { "Uniform1f", NULL } },
@@ -285,9 +260,6 @@ GLContext::InitWithPrefix(const char *prefix, PRBool trygl)
         { (PRFuncPtr*) &fVertexAttrib2fv, { "VertexAttrib2fv", NULL } },
         { (PRFuncPtr*) &fVertexAttrib3fv, { "VertexAttrib3fv", NULL } },
         { (PRFuncPtr*) &fVertexAttrib4fv, { "VertexAttrib4fv", NULL } },
-#ifndef USE_GLES2
-        { (PRFuncPtr*) &fVertexPointer, { "VertexPointer", NULL } },
-#endif
         { (PRFuncPtr*) &fViewport, { "Viewport", NULL } },
         { (PRFuncPtr*) &fCompileShader, { "CompileShader", NULL } },
         { (PRFuncPtr*) &fCopyTexImage2D, { "CopyTexImage2D", NULL } },
@@ -322,6 +294,15 @@ GLContext::InitWithPrefix(const char *prefix, PRBool trygl)
         { (PRFuncPtr*) &priv_fDeleteShader, { "DeleteShader", "DeleteShaderARB", NULL } },
         { (PRFuncPtr*) &priv_fDeleteFramebuffers, { "DeleteFramebuffers", "DeleteFramebuffersEXT", NULL } },
         { (PRFuncPtr*) &priv_fDeleteRenderbuffers, { "DeleteRenderbuffers", "DeleteRenderbuffersEXT", NULL } },
+
+        { mIsGLES2 ? (PRFuncPtr*) &priv_fClearDepthf : (PRFuncPtr*) &priv_fClearDepth,
+          { mIsGLES2 ? "ClearDepthf" : "ClearDepth", NULL } },
+        { mIsGLES2 ? (PRFuncPtr*) &priv_fDepthRangef : (PRFuncPtr*) &priv_fDepthRange,
+          { mIsGLES2 ? "DepthRangef" : "DepthRange", NULL } },
+
+        // XXX FIXME -- we shouldn't be using glReadBuffer!
+        { mIsGLES2 ? (PRFuncPtr*) NULL : (PRFuncPtr*) &fReadBuffer,
+          { mIsGLES2 ? NULL : "ReadBuffer", NULL } },
 
         { NULL, { NULL } },
 
@@ -655,21 +636,13 @@ GLContext::ClearSafely()
 
     fClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     fClearStencil(0);
-#ifdef USE_GLES2
-    fClearDepthf(1.0f);
-#else
-    fClearDepth(1.0);
-#endif
+    fClearDepth(1.0f);
 
     fClear(LOCAL_GL_COLOR_BUFFER_BIT | LOCAL_GL_DEPTH_BUFFER_BIT | LOCAL_GL_STENCIL_BUFFER_BIT);
 
     fClearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3]);
     fClearStencil(clearStencil);
-#ifdef USE_GLES2
-    fClearDepthf(clearDepth);
-#else
     fClearDepth(clearDepth);
-#endif
 }
 
 void
