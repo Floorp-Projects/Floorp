c9964e0a70185048f3476a2e0809a4994d833d5a
Robert Sayre <sayrer@gmail.com>
2010-07-29 13:13:11 -0700

    Backed out changeset e393b9673207. ( Bug 573803 - add SSL WebSocket support to mochitest. r=ted )

--- content/base/test/file_websocket_hello_wsh.py
+++ content/base/test/file_websocket_hello_wsh.py
@@ -4,10 +4,7 @@ def web_socket_do_extra_handshake(request):
   pass
 
 def web_socket_transfer_data(request):
-  resp = "Fail"
-  request_text = msgutil.receive_message(request)
-  if request_text == "data":
+  resp = "Test"
+  if msgutil.receive_message(request) == "data":
     resp = "Hello world!"
-  elif request_text == "ssltest":
-    resp = "ssldata"
   msgutil.send_message(request, resp)
