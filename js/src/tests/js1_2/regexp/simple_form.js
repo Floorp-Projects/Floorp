8e2957011000873f920c802e76cd150ec06af9c0
David Mandelin <dmandelin@mozilla.com>
2010-07-28 18:17:17 -0700

    Backed out changeset 71d776c0ec08 -- this modified some tests to disable an old syntax, but we are keeping that syntax for now.

--- js/src/tests/js1_2/regexp/simple_form.js
+++ js/src/tests/js1_2/regexp/simple_form.js
@@ -55,16 +55,16 @@ writeHeaderToLog('Executing script: simple_form.js');
 writeHeaderToLog( SECTION + " "+ TITLE);
 
 new TestCase ( SECTION,
-	       "/[0-9]{3}/.exec('23 2 34 678 9 09')",
-	       String(["678"]), String(/[0-9]{3}/.exec('23 2 34 678 9 09')));
+	       "/[0-9]{3}/('23 2 34 678 9 09')",
+	       String(["678"]), String(/[0-9]{3}/('23 2 34 678 9 09')));
 
 new TestCase ( SECTION,
-	       "/3.{4}8/.exec('23 2 34 678 9 09')",
-	       String(["34 678"]), String(/3.{4}8/.exec('23 2 34 678 9 09')));
+	       "/3.{4}8/('23 2 34 678 9 09')",
+	       String(["34 678"]), String(/3.{4}8/('23 2 34 678 9 09')));
 
 new TestCase ( SECTION,
-	       "(/3.{4}8/.exec('23 2 34 678 9 09').length",
-	       1, (/3.{4}8/.exec('23 2 34 678 9 09')).length);
+	       "(/3.{4}8/('23 2 34 678 9 09').length",
+	       1, (/3.{4}8/('23 2 34 678 9 09')).length);
 
 var re = /[0-9]{3}/;
 new TestCase ( SECTION,
@@ -77,8 +77,8 @@ new TestCase ( SECTION,
 	       String(["34 678"]), String(re('23 2 34 678 9 09')));
 
 new TestCase ( SECTION,
-	       "/3.{4}8/.exec('23 2 34 678 9 09')",
-	       String(["34 678"]), String(/3.{4}8/.exec('23 2 34 678 9 09')));
+	       "/3.{4}8/('23 2 34 678 9 09')",
+	       String(["34 678"]), String(/3.{4}8/('23 2 34 678 9 09')));
 
 re =/3.{4}8/;
 new TestCase ( SECTION,
@@ -86,7 +86,7 @@ new TestCase ( SECTION,
 	       1, (re('23 2 34 678 9 09')).length);
 
 new TestCase ( SECTION,
-	       "(/3.{4}8/.exec('23 2 34 678 9 09').length",
-	       1, (/3.{4}8/.exec('23 2 34 678 9 09')).length);
+	       "(/3.{4}8/('23 2 34 678 9 09').length",
+	       1, (/3.{4}8/('23 2 34 678 9 09')).length);
 
 test();
