9f9fdac3a9a427616dfde3e110b690c548815892
Josh Aas <joshmoz@gmail.com>
2010-07-30 09:44:51 -0400

    Bug 531552: Force handling of at-launch open-url and open-file Apple Events to happen during command line construction so that we can put them on the command line and don't open unnecessary windows. r=benwa

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3565,6 +3565,10 @@ XRE_main(int argc, char* argv[], const nsXREAppData* aAppData)
 #endif
 
 #ifdef XP_MACOSX
+          // Set up ability to respond to system (Apple) events. This must be
+          // done before setting up the command line service.
+          SetupMacApplicationDelegate();
+
           // we re-initialize the command-line service and do appleevents munging
           // after we are sure that we're not restarting
           cmdLine = do_CreateInstance("@mozilla.org/toolkit/command-line;1");
@@ -3575,9 +3579,6 @@ XRE_main(int argc, char* argv[], const nsXREAppData* aAppData)
           rv = cmdLine->Init(gArgc, gArgv,
                              workingDir, nsICommandLine::STATE_INITIAL_LAUNCH);
           NS_ENSURE_SUCCESS(rv, 1);
-          
-          // Set up ability to respond to system (Apple) events.
-          SetupMacApplicationDelegate();
 #endif
 
           MOZ_SPLASHSCREEN_UPDATE(70);
