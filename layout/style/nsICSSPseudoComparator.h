be3e16336eb5f44836bfa9dec507315bb31c64eb
Craig Topper <craig.topper@gmail.com>
2010-07-30 16:48:57 -0700

    Remove QueryFrame support for nsICSSPseudoComparator.  (Bug 576794)  r=bzbarsky  a2.0=bsmedberg

--- layout/style/nsICSSPseudoComparator.h
+++ layout/style/nsICSSPseudoComparator.h
@@ -40,16 +40,12 @@
 #ifndef nsICSSPseudoComparator_h___
 #define nsICSSPseudoComparator_h___
 
-#include "nsQueryFrame.h"
-
 class nsIAtom;
 struct nsCSSSelector;
 
 class nsICSSPseudoComparator
 {
 public:
-  NS_DECL_QUERYFRAME_TARGET(nsICSSPseudoComparator)
-
   virtual PRBool PseudoMatches(nsIAtom* aTag, nsCSSSelector* aSelector)=0;
 };
 
