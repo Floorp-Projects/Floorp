44e12bf078153a8ee665632c44569a4ab7ff1de2
Olli Pettay <Olli.Pettay@helsinki.fi>
2010-07-31 21:22:29 +0300

    Bug 576075, stricter tree selection handling, r=enndeakin
    --HG--
    extra : rebase_source : ea68e33207720ee4193642aaf0749c9939cc12d2

--- layout/xul/base/src/tree/src/nsTreeSelection.h
+++ layout/xul/base/src/tree/src/nsTreeSelection.h
@@ -50,7 +50,7 @@
 class nsITreeBoxObject;
 struct nsTreeRange;
 
-class nsTreeSelection : public nsITreeSelection
+class nsTreeSelection : public nsINativeTreeSelection
 {
 public:
   nsTreeSelection(nsITreeBoxObject* aTree);
@@ -59,6 +59,9 @@ public:
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
   NS_DECL_CYCLE_COLLECTION_CLASS(nsTreeSelection)
   NS_DECL_NSITREESELECTION
+
+  // nsINativeTreeSelection: Untrusted code can use us
+  NS_IMETHOD EnsureNative() { return NS_OK; }
 
   friend struct nsTreeRange;
 
