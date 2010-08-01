0eea49cd7a9721c3f4d991b9aca3eadc75b5b30b
David Anderson <danderson@mozilla.com>
2010-07-31 18:58:53 -0700

    [JAEGER] Add diagnostic tool for trace JIT tuning.

--- js/src/metrics/jint/sunspider/bitops-bitwise-and.js
+++ js/src/metrics/jint/sunspider/bitops-bitwise-and.js
@@ -23,42 +23,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-var loops = 15
-var nx = 120
-var nz = 120
-
-function morph(a, f) {
-    var PI2nx = Math.PI * 8/nx
-    var sin = Math.sin
-    var f30 = -(50 * sin(f*Math.PI*2))
-    
+bitwiseAndValue = 4294967296;
 /* BEGIN LOOP */
-    for (var i = 0; i < nz; ++i) {
-/* BEGIN LOOP */
-        for (var j = 0; j < nx; ++j) {
-            a[3*(i*nx+j)+1]    = sin((j-1) * PI2nx ) * -f30
-        }
+for (var i = 0; i < 600000; i++)
+    bitwiseAndValue = bitwiseAndValue & i;
 /* END LOOP */
-    }
-/* END LOOP */
-}
-
-    
-var a = Array()
-/* BEGIN LOOP */
-for (var i=0; i < nx*nz*3; ++i) 
-    a[i] = 0
-/* END LOOP */
-
-/* BEGIN LOOP */
-for (var i = 0; i < loops; ++i) {
-    morph(a, i/loops)
-}
-/* END LOOP */
-
-testOutput = 0;
-/* BEGIN LOOP */
-for (var i = 0; i < nx; i++)
-    testOutput += a[3*(i*nx+i)+1];
-/* END LOOP */
-a = null;
