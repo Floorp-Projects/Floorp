2ccaefd0819d770c0517b733ac136a09a3c923f8
Felipe Gomes <felipc@gmail.com>
2010-08-02 10:06:04 -0500

    Bug 575328 - Fix for search suggestions box appears on wrong monitor in multiple monitor environment when window is maximized. r=enndeakin.

--- layout/xul/base/src/nsResizerFrame.cpp
+++ layout/xul/base/src/nsResizerFrame.cpp
@@ -227,11 +227,11 @@ nsResizerFrame::HandleEvent(nsPresContext* aPresContext,
         }
       }
       else if (menuPopupFrame) {
-        nsPoint framePoint = menuPopupFrame->GetScreenRectInAppUnits().TopLeft();
+        nsRect frameRect = menuPopupFrame->GetScreenRectInAppUnits();
         nsIFrame* rootFrame = aPresContext->PresShell()->FrameManager()->GetRootFrame();
         nsRect rootScreenRect = rootFrame->GetScreenRectInAppUnits();
 
-        nsRect screenRect = menuPopupFrame->GetConstraintRect(framePoint, rootScreenRect);
+        nsRect screenRect = menuPopupFrame->GetConstraintRect(frameRect, rootScreenRect);
         // round using ToInsidePixels as it's better to be a pixel too small
         // than be too large. If the popup is too large it could get flipped
         // to the opposite side of the anchor point while resizing.
