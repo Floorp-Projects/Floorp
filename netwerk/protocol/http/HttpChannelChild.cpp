e2c90d023702e1cb751bfc56f1d64261f3e55523
Chris Jones <jones.chris.g@gmail.com>
2010-08-05 08:43:02 -0700

    Bug 582477 - e10s: HttpChannelChild triggers abort on bing.com. r=jduell,jdm

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -146,8 +146,7 @@ HttpChannelChild::RecvOnStartRequest(const nsHttpResponseHead& responseHead,
     //  - Send Cancel msg to parent 
     //  - drop any in flight OnDataAvail msgs we receive
     //  - make sure we do call OnStopRequest eventually
-    //  - return true here, not false
-    return false;  
+    return true;  
   }
 
   if (mResponseHead)
@@ -226,7 +225,6 @@ HttpChannelChild::OnDataAvailable(const nsCString& data,
   stringStream->Close();
   if (NS_FAILED(rv)) {
     // TODO: Cancel request: see OnStartRequest. Bug 536317
-    return false; 
   }
   return true;
 }
