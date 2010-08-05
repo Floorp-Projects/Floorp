03059441d3f7b6cd20d2b4bf510390ba050f16b8
Chris Jones <jones.chris.g@gmail.com>
2010-08-05 17:11:23 -0500

    Bug 570620, part f: Add a "destroy" phase to PBrowser shutdown to make clean-up easier. r=smaug sr=bsmedberg

--- content/base/src/nsFrameLoader.cpp
+++ content/base/src/nsFrameLoader.cpp
@@ -1171,7 +1171,10 @@ nsFrameLoader::DestroyChild()
 #ifdef MOZ_IPC
   if (mRemoteBrowser) {
     mRemoteBrowser->SetOwnerElement(nsnull);
-    unused << PBrowserParent::Send__delete__(mRemoteBrowser);
+    // If this fails, it's most likely due to a content-process crash,
+    // and auto-cleanup will kick in.  Otherwise, the child side will
+    // destroy itself and send back __delete__().
+    unused << mRemoteBrowser->SendDestroy();
     mRemoteBrowser = nsnull;
   }
 #endif
