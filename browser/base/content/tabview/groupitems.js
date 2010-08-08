55e1194abad01719f92631b93e8abdd0e0f44242
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2010-08-08 18:47:06 -0400

    Bug 575851: kill the now defunct New Tab Group.

--- browser/base/content/tabview/groupitems.js
+++ browser/base/content/tabview/groupitems.js
@@ -1794,5 +1794,17 @@ window.GroupItems = {
       tab.tabItem.setZoomPrep(false);
       UI.showTabView();
     }
+  },
+  
+  // ----------
+  // Function: killNewTabGroup
+  // Removes the New Tab Group, which is now defunct. See bug 575851 and comments therein.
+  killNewTabGroup: function() {
+    this.groupItems.forEach(function(groupItem) {
+      if (groupItem.getTitle() == 'New Tabs') {
+        groupItem.removeAll();
+        groupItem.close();
+      }
+    });
   }
 };
