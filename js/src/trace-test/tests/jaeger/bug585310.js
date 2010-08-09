bf26924b41645965bb5c8021ef7a554eaf59bcfe
Sean Stangl <sstangl@mozilla.com>
2010-08-09 15:29:58 -0700

    [JAEGER] Add tests from bug 585310.

---
+++ js/src/trace-test/tests/jaeger/bug585310.js
@@ -0,0 +1,19 @@
+gczeal(2)
+try {
+    (function () {
+        for each(l in [0, 0, 0]) {
+            print(''.replace(function () {}))
+        }
+    })()
+} catch (e) {}
+
+x = gczeal(2)
+try {
+    (function () {
+        for each(d in [x, x, x]) {
+            'a'.replace(/a/, function () {})
+        }
+    })()
+} catch (e) {}
+
+/* Don't assert or segfault. Tests PIC resetting. */
