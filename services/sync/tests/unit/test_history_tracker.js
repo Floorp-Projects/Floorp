96c792a8d43bfcdc0f9daa7c42f2a4503c639d18
Philipp von Weitershausen <philipp@weitershausen.de>
2010-08-09 18:38:18 +0200

    Bug 583852 - Weave should not be querying on places views [r=mconnor]
    Tests for the history store.

--- services/sync/tests/unit/test_history_tracker.js
+++ services/sync/tests/unit/test_history_tracker.js
@@ -2,28 +2,6 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://services-sync/engines/history.js");
 Cu.import("resource://services-sync/util.js");
 
-
-// See toolkit/components/places/tests/head_common.js
-const NS_APP_HISTORY_50_FILE = "UHist";
-const dirsvc = Cc["@mozilla.org/file/directory_service;1"].
-               getService(Ci.nsIDirectoryService);
-
-// Add our own dirprovider for old history.dat.
-let provider = {
-  getFile: function(prop, persistent) {
-    persistent.value = true;
-    if (prop == NS_APP_HISTORY_50_FILE) {
-      let histFile = dirsvc.get("ProfD", Ci.nsIFile);
-      histFile.append("history.dat");
-      return histFile;
-    }
-    throw Cr.NS_ERROR_FAILURE;
-  },
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIDirectoryServiceProvider])
-};
-dirsvc.registerProvider(provider);
-
-
 function run_test() {
   _("Verify we've got an empty tracker to work with.");
   let tracker = new HistoryEngine()._tracker;
