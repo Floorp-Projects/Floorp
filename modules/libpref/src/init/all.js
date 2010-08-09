dce3acd6a9fd018b60cb9096ad5c0afdeabac702
Neil Deakin <neil@mozilla.com>
2010-08-09 12:15:47 -0400

    Bug 577316, add preference to allow form elements to be focused when clicked, r=smaug

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -222,6 +222,7 @@ pref("accessibility.tabfocus_applies_to_xul", true);
 pref("accessibility.usetexttospeech", "");
 pref("accessibility.usebrailledisplay", "");
 pref("accessibility.accesskeycausesactivation", true);
+pref("accessibility.mouse_focuses_formcontrol", false);
 
 // Type Ahead Find
 pref("accessibility.typeaheadfind", true);
