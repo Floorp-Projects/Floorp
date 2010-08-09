e1344b373ac1dcc69b4590e094b6c492ad9a1eea
David Mandelin <dmandelin@mozilla.com>
2010-08-09 16:21:49 -0700

    [JAEGER] Bug 585794 temporary fix followup: undisable getelem PICs

--- js/src/methodjit/PolyIC.cpp
+++ js/src/methodjit/PolyIC.cpp
@@ -1890,7 +1890,7 @@ ic::GetElem(VMFrame &f, uint32 picIndex)
     Value idval = f.regs.sp[-1];
     JS_ASSERT(idval.isString());
     JSString *id = idval.toString();
-    if (0 && pic.shouldGenerate()) {
+    if (pic.shouldGenerate()) {
         GetElemCompiler cc(f, script, obj, pic, id, stubs::GetElem);
         if (!cc.update()) {
             cc.disable("error");
