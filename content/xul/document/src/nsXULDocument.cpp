8dcfe196d42ef8bfb0f3bcd52f636e80d5f9387d
Ms2ger <ms2ger@gmail.com>
2010-08-10 14:25:45 +0200

    Bug 562698 - Part c: Pass Elements to AttributeWillChange; r=bzbarsky, a=dbaron

--- content/xul/document/src/nsXULDocument.cpp
+++ content/xul/document/src/nsXULDocument.cpp
@@ -976,19 +976,19 @@ nsXULDocument::ExecuteOnBroadcastHandlerFor(nsIContent* aBroadcaster,
 
 void
 nsXULDocument::AttributeWillChange(nsIDocument* aDocument,
-                                   nsIContent* aContent, PRInt32 aNameSpaceID,
+                                   Element* aElement, PRInt32 aNameSpaceID,
                                    nsIAtom* aAttribute, PRInt32 aModType)
 {
-    NS_ABORT_IF_FALSE(aContent, "Null content!");
+    NS_ABORT_IF_FALSE(aElement, "Null content!");
     NS_PRECONDITION(aAttribute, "Must have an attribute that's changing!");
 
     // XXXbz check aNameSpaceID, dammit!
     // See if we need to update our ref map.
     if (aAttribute == nsGkAtoms::ref ||
-        (aAttribute == nsGkAtoms::id && !aContent->GetIDAttributeName())) {
+        (aAttribute == nsGkAtoms::id && !aElement->GetIDAttributeName())) {
         // Might not need this, but be safe for now.
         nsCOMPtr<nsIMutationObserver> kungFuDeathGrip(this);
-        RemoveElementFromRefMap(aContent->AsElement());
+        RemoveElementFromRefMap(aElement);
     }
 }
 
