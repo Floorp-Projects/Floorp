36a755f42bfb507bc810132a1fb28e16f99401a8
Kyle Huey <me@kylehuey.com>
2010-08-09 17:55:33 -0700

    Backed out changeset f2c04d62dc9c

--- build/pymake/tests/pycmd.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def writetofile(args):
-  with open(args[0], 'w') as f:
-    f.write(' '.join(args[1:]))
-
-def writeenvtofile(args):
-  with open(args[0], 'w') as f:
-    f.write(os.environ[args[1]])
