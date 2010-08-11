2d2e0343208d7333a7ac80702b5032611ae85aac
Chris Leary <cdleary@mozilla.com>
2010-08-11 16:00:58 -0700

    ARM and PCRE build fixes. (r=red)

--- js/src/assembler/assembler/MacroAssemblerX86Common.cpp
+++ js/src/assembler/assembler/MacroAssemblerX86Common.cpp
@@ -1,7 +1,11 @@
+#include "assembler/wtf/Platform.h"
+
+#if WTF_CPU_X86 && !WTF_PLATFORM_MAC
+
 #include "MacroAssemblerX86Common.h"
 
 using namespace JSC;
 
-#if WTF_CPU_X86 && !WTF_PLATFORM_MAC
 MacroAssemblerX86Common::SSE2CheckState MacroAssemblerX86Common::s_sse2CheckState = NotCheckedSSE2;
+
 #endif
