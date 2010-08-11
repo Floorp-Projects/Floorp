614ec4a44c57fd54f9e76847e276606ec8f277fd
Dão Gottwald <dao@mozilla.com>
2010-08-11 14:02:49 +0200

    Bug 585361 - Handle requests for synchronously removing an already asynchronously closing tab. r=gavin

--- browser/base/content/test/browser_bug380960.js
+++ browser/base/content/test/browser_bug380960.js
@@ -5,6 +5,11 @@ function test() {
   gBrowser.removeTab(tab);
   is(tab.parentNode, null, "tab removed immediately");
 
+  tab = gBrowser.addTab("about:blank", { skipAnimation: true });
+  gBrowser.removeTab(tab, { animate: true });
+  gBrowser.removeTab(tab);
+  is(tab.parentNode, null, "tab removed immediately when calling removeTab again after the animation was kicked off");
+
   waitForExplicitFinish();
 
   Services.prefs.setBoolPref("browser.tabs.animate", true);
