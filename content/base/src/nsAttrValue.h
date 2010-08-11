edf2c5ceae840fb543ca5b6eaad960c4b8b34a01
Boris Zbarsky <bzbarsky@mit.edu>
2010-08-11 17:05:28 -0400

    Bug 586381.  Switch attribute atom array storage to nsTArray.  r=sicking

--- content/base/src/nsAttrValue.h
+++ content/base/src/nsAttrValue.h
@@ -50,6 +50,7 @@
 #include "nsColor.h"
 #include "nsCaseTreatment.h"
 #include "nsMargin.h"
+#include "nsCOMPtr.h"
 
 typedef PRUptrdiff PtrBits;
 class nsAString;
@@ -57,7 +58,7 @@ class nsIAtom;
 class nsICSSStyleRule;
 class nsISVGValue;
 class nsIDocument;
-template<class E> class nsCOMArray;
+template<class E> class nsTArray;
 template<class E> class nsTPtrArray;
 
 #define NS_ATTRVALUE_MAX_STRINGLENGTH_ATOM 12
@@ -92,6 +93,8 @@ public:
 
 class nsAttrValue {
 public:
+  typedef nsTArray< nsCOMPtr<nsIAtom> > AtomArray;
+
   nsAttrValue();
   nsAttrValue(const nsAttrValue& aOther);
   explicit nsAttrValue(const nsAString& aValue);
@@ -151,7 +154,7 @@ public:
   PRBool GetColorValue(nscolor& aColor) const;
   inline PRInt16 GetEnumValue() const;
   inline float GetPercentValue() const;
-  inline nsCOMArray<nsIAtom>* GetAtomArrayValue() const;
+  inline AtomArray* GetAtomArrayValue() const;
   inline nsICSSStyleRule* GetCSSStyleRuleValue() const;
 #ifdef MOZ_SVG
   inline nsISVGValue* GetSVGValue() const;
@@ -170,7 +173,7 @@ public:
   // Methods to get access to atoms we may have
   // Returns the number of atoms we have; 0 if we have none.  It's OK
   // to call this without checking the type first; it handles that.
-  PRInt32 GetAtomCount() const;
+  PRUint32 GetAtomCount() const;
   // Returns the atom at aIndex (0-based).  Do not call this with
   // aIndex >= GetAtomCount().
   nsIAtom* AtomAt(PRInt32 aIndex) const;
@@ -333,7 +336,7 @@ private:
       PRUint32 mEnumValue;
       PRInt32 mPercent;
       nsICSSStyleRule* mCSSStyleRule;
-      nsCOMArray<nsIAtom>* mAtomArray;
+      AtomArray* mAtomArray;
 #ifdef MOZ_SVG
       nsISVGValue* mSVGValue;
 #endif
@@ -426,7 +429,7 @@ nsAttrValue::GetPercentValue() const
             / 100.0f;
 }
 
-inline nsCOMArray<nsIAtom>*
+inline nsAttrValue::AtomArray*
 nsAttrValue::GetAtomArrayValue() const
 {
   NS_PRECONDITION(Type() == eAtomArray, "wrong type");
