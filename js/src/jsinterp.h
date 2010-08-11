b3967ba774ae4ce3ee6c0e06b57e4c47e42b6a06
Andrew Drake <drakedevel@gmail.com>
2010-08-10 17:21:11 -0700

    Bug 584882: Don't attempt to compute this for dummy frames. r=jorendorff

--- js/src/jsinterp.h
+++ js/src/jsinterp.h
@@ -68,6 +68,7 @@ enum JSFrameFlags {
     JSFRAME_YIELDING           =  0x40, /* js_Interpret dispatched JSOP_YIELD */
     JSFRAME_GENERATOR          =  0x80, /* frame belongs to generator-iterator */
     JSFRAME_OVERRIDE_ARGS      = 0x100, /* overridden arguments local variable */
+    JSFRAME_DUMMY              = 0x200, /* frame is a dummy frame */
 
     JSFRAME_SPECIAL            = JSFRAME_DEBUGGER | JSFRAME_EVAL
 };
@@ -222,7 +223,7 @@ struct JSStackFrame
         return !!(flags & JSFRAME_FLOATING_GENERATOR);
     }
 
-    bool isDummyFrame() const { return !script && !fun; }
+    bool isDummyFrame() const { return !!(flags & JSFRAME_DUMMY); }
 };
 
 namespace js {
@@ -510,6 +511,7 @@ js_MeterSlotOpcode(JSOp op, uint32 slot);
 inline JSObject *
 JSStackFrame::getThisObject(JSContext *cx)
 {
+    JS_ASSERT(!isDummyFrame());
     if (flags & JSFRAME_COMPUTED_THIS)
         return &thisv.toObject();
     if (!js::ComputeThisFromArgv(cx, argv))
