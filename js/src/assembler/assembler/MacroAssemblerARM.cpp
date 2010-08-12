b572a1b5b0fcb51e56b9963cf90644c07957b9d4
Robert Sayre <sayrer@gmail.com>
2010-08-11 21:01:30 -0700

    Try to fix Maemo 4 again

--- js/src/assembler/assembler/MacroAssemblerARM.cpp
+++ js/src/assembler/assembler/MacroAssemblerARM.cpp
@@ -37,8 +37,11 @@
 #include <unistd.h>
 #include <elf.h>
 
+// lame check for kernel version
+// see bug 586550
 #ifdef MOZ_PLATFORM_MAEMO
-#if (MOZ_PLATFORM_MAEMO == 4)
+#include <linux/version.h>
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,28) 
 #include <asm/procinfo.h>
 #else
 #include <asm/hwcap.h>
