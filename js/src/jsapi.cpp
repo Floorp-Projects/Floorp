488e9841aff656431109141a8a5556809cc3bd01
Jim Blandy <jimb@mozilla.org>
2010-08-12 12:38:37 -0700

    Bug 438633: Give new JSScript objects lifetimes like GCThings. r=brendan
    Attach script objects immediately in all JSAPI script-creating functions;
    have JS_NewScriptObject simply return the already-allocated object; and
    make JS_DestroyScript a no-op.
    
    Verify that all scripts given to JSAPI script-consuming functions have
    objects, or are the canonical empty script object.

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -4445,7 +4445,14 @@ JS_CompileUCScriptForPrincipals(JSContext *cx, JSObject *obj, JSPrincipals *prin
     uint32 tcflags = JS_OPTIONS_TO_TCFLAGS(cx) | TCF_NEED_MUTABLE_SCRIPT;
     JSScript *script = Compiler::compileScript(cx, obj, NULL, principals, tcflags,
                                                chars, length, NULL, filename, lineno);
-    LAST_FRAME_CHECKS(cx, script);
+    if (!script) {
+        LAST_FRAME_CHECKS(cx, script);
+        return NULL;
+    }
+    if (!js_NewScriptObject(cx, script)) {
+        js_DestroyScript(cx, script);
+        return NULL;
+    }
     return script;
 }
 
@@ -4545,7 +4552,14 @@ JS_CompileFile(JSContext *cx, JSObject *obj, const char *filename)
                                      NULL, 0, fp, filename, 1);
     if (fp != stdin)
         fclose(fp);
-    LAST_FRAME_CHECKS(cx, script);
+    if (!script) {
+        LAST_FRAME_CHECKS(cx, script);
+        return NULL;
+    }
+    if (!js_NewScriptObject(cx, script)) {
+        js_DestroyScript(cx, script);
+        return NULL;
+    }
     return script;
 }
 
@@ -4561,7 +4575,14 @@ JS_CompileFileHandleForPrincipals(JSContext *cx, JSObject *obj, const char *file
     tcflags = JS_OPTIONS_TO_TCFLAGS(cx) | TCF_NEED_MUTABLE_SCRIPT;
     script = Compiler::compileScript(cx, obj, NULL, principals, tcflags,
                                      NULL, 0, file, filename, 1);
-    LAST_FRAME_CHECKS(cx, script);
+    if (!script) {
+        LAST_FRAME_CHECKS(cx, script);
+        return NULL;
+    }
+    if (!js_NewScriptObject(cx, script)) {
+        js_DestroyScript(cx, script);
+        return NULL;
+    }
     return script;
 }
 
@@ -4574,34 +4595,29 @@ JS_CompileFileHandle(JSContext *cx, JSObject *obj, const char *filename, FILE *f
 JS_PUBLIC_API(JSObject *)
 JS_NewScriptObject(JSContext *cx, JSScript *script)
 {
-    JSObject *obj;
-
     CHECK_REQUEST(cx);
     assertSameCompartment(cx, script);
     if (!script)
         return NewNonFunction<WithProto::Class>(cx, &js_ScriptClass, NULL, NULL);
 
-    JS_ASSERT(!script->u.object);
-
-    {
-        AutoScriptRooter root(cx, script);
-
-        obj = NewNonFunction<WithProto::Class>(cx, &js_ScriptClass, NULL, NULL);
-        if (obj) {
-            obj->setPrivate(script);
-            script->u.object = obj;
-#ifdef CHECK_SCRIPT_OWNER
-            script->owner = NULL;
-#endif
-        }
-    }
-
-    return obj;
+    /*
+     * This function should only ever be applied to JSScripts that had
+     * script objects allocated for them when they were created, as
+     * described in the comment for JSScript::u.object.
+     */
+    JS_ASSERT(script->u.object);
+    return script->u.object;
 }
 
 JS_PUBLIC_API(JSObject *)
 JS_GetScriptObject(JSScript *script)
 {
+    /*
+     * This function should only ever be applied to JSScripts that had
+     * script objects allocated for them when they were created, as
+     * described in the comment for JSScript::u.object.
+     */
+    JS_ASSERT(script->u.object);
     return script->u.object;
 }
 
@@ -4609,8 +4625,17 @@ JS_PUBLIC_API(void)
 JS_DestroyScript(JSContext *cx, JSScript *script)
 {
     CHECK_REQUEST(cx);
-    assertSameCompartment(cx, script);
-    js_DestroyScript(cx, script);
+
+    /*
+     * Originally, JSScript lifetimes were managed explicitly, and this function
+     * was used to free a JSScript. Now, this function does nothing, and the
+     * garbage collector manages JSScripts; you must root the JSScript's script
+     * object (obtained via JS_GetScriptObject) to keep it alive.
+     *
+     * However, since the script objects have taken over this responsibility, it
+     * follows that every script passed here must have a script object.
+     */
+    JS_ASSERT(script->u.object);
 }
 
 JS_PUBLIC_API(JSFunction *)
@@ -4775,6 +4800,8 @@ JS_ExecuteScript(JSContext *cx, JSObject *obj, JSScript *script, jsval *rval)
 
     CHECK_REQUEST(cx);
     assertSameCompartment(cx, obj, script);
+    /* This should receive only scripts handed out via the JSAPI. */
+    JS_ASSERT(script == JSScript::emptyScript() || script->u.object);
     ok = Execute(cx, obj, script, NULL, 0, Valueify(rval));
     LAST_FRAME_CHECKS(cx, ok);
     return ok;
@@ -4802,7 +4829,7 @@ JS_EvaluateUCScriptForPrincipals(JSContext *cx, JSObject *obj,
     }
     ok = Execute(cx, obj, script, NULL, 0, Valueify(rval));
     LAST_FRAME_CHECKS(cx, ok);
-    JS_DestroyScript(cx, script);
+    js_DestroyScript(cx, script);
     return ok;
 }
 
