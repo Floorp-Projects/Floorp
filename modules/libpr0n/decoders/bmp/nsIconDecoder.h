c16264b42c860d29a07c724e0edd7e5788ca3f70
Bobby Holley <bobbyholley@gmail.com>
2010-08-12 00:31:37 -0400

    Bug 513681 - part 4 - Get rid of CIDs and friends for decoders.r=joe,a=blocker

--- modules/libpr0n/decoders/bmp/nsIconDecoder.h
+++ modules/libpr0n/decoders/bmp/nsIconDecoder.h
@@ -48,14 +48,6 @@
 #include "imgIContainer.h"
 #include "imgIDecoderObserver.h"
 
-#define NS_ICONDECODER_CID                           \
-{ /* FFC08380-256C-11d5-9905-001083010E9B */         \
-     0xffc08380,                                     \
-     0x256c,                                         \
-     0x11d5,                                         \
-    { 0x99, 0x5, 0x0, 0x10, 0x83, 0x1, 0xe, 0x9b }   \
-}
-
 namespace mozilla {
 namespace imagelib {
 class RasterImage;
