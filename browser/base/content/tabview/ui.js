df9910a872a96523f1a740bc72d7e9fbf66f71f5
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2010-08-12 18:32:18 -0500

    Bug 586685: All user-facing strings in JS are now localizable via stringbundle [r=dolske a=dolske] NOT. Everything but user facing strings being localized. Random whitespace fixes. Random styling changes. Also has Bug 587158 - Update intro-video URL to CDN'ed version And string changes. And Bug 586814 - Unable to close Tab groups
    --HG--
    extra : rebase_source : bc46adb3b3acc2e4daee067e9209d049fe1a8970

--- browser/base/content/tabview/ui.js
+++ browser/base/content/tabview/ui.js
@@ -184,11 +184,14 @@ var UIManager = {
         });
 
         // ___ make info item
+        let welcome = "How to organize your tabs";
+        let more = "";
+        let video = "http://videos-cdn.mozilla.net/firefox4beta/tabcandy_howto.webm";
         var html =
           "<div class='intro'>"
-            + "<h1>Welcome to Firefox Tab Sets</h1>" // TODO: This needs to be localized if it's kept in
-            + "<div>(more goes here)</div><br>"
-            + "<video src='http://people.mozilla.org/~araskin/movies/tabcandy_howto.webm' "
+            + "<h1>" + welcome + "</h1>"
+            + ( more && more.length ? "<div>" + more + "</div><br>" : "")
+            + "<video src='" + video + "' "
             + "width='100%' preload controls>"
           + "</div>";
 
