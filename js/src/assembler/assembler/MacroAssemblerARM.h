19fa117bc07b1c02afb22045a19d77a53c34df86
Jacob Bramley <Jacob.Bramley@arm.com>
2010-08-12 13:54:26 +0100

    [JAEGER] Fix broken logic from my previous patch (c5754250e813). [Bug 585918] [r=me]

--- js/src/assembler/assembler/MacroAssemblerARM.h
+++ js/src/assembler/assembler/MacroAssemblerARM.h
@@ -980,7 +980,7 @@ public:
         // integer, it saturates at INT_MAX or INT_MIN. Testing this is
         // probably quicker than testing FPSCR for exception.
         m_assembler.fmrs_r(dest, ARMRegisters::SD0);
-        m_assembler.cmn_r(dest, ARMAssembler::getOp2(1));
+        m_assembler.cmn_r(dest, ARMAssembler::getOp2(-0x7fffffff));
         m_assembler.cmp_r(dest, ARMAssembler::getOp2(0x80000000), ARMCondition(NonZero));
         return Jump(m_assembler.jmp(ARMCondition(Zero)));
     }
