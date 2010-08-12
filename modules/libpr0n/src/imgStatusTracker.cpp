9307bd1489c876bc3dca431a0b5221a1c50b2e5e
Joe Drew <joe@drew.ca>
2010-08-12 11:59:28 -0400

    Bug 583028 - Hold on to a runnable while it's outstanding, and add proxies for it to notify when it does get run. This makes us delay onload less. r=bholley a=blocking

--- modules/libpr0n/src/imgStatusTracker.cpp
+++ modules/libpr0n/src/imgStatusTracker.cpp
@@ -61,6 +61,16 @@ imgStatusTracker::imgStatusTracker(imgIContainer* aImage)
     mHadLastPart(PR_FALSE)
 {}
 
+imgStatusTracker::imgStatusTracker(const imgStatusTracker& aOther)
+  : mImage(aOther.mImage),
+    mState(aOther.mState),
+    mImageStatus(aOther.mImageStatus),
+    mHadLastPart(aOther.mHadLastPart)
+    // Note: we explicitly don't copy mRequestRunnable, because it won't be
+    // nulled out when the mRequestRunnable's Run function eventually gets
+    // called.
+{}
+
 PRBool
 imgStatusTracker::IsLoading() const
 {
@@ -81,20 +91,32 @@ class imgRequestNotifyRunnable : public nsRunnable
 {
   public:
     imgRequestNotifyRunnable(imgRequest* request, imgRequestProxy* requestproxy)
-      : mRequest(request), mProxy(requestproxy)
-    {}
+      : mRequest(request)
+    {
+      mProxies.AppendElement(requestproxy);
+    }
 
     NS_IMETHOD Run()
     {
-      mProxy->SetNotificationsDeferred(PR_FALSE);
+      for (PRUint32 i = 0; i < mProxies.Length(); ++i) {
+        mProxies[i]->SetNotificationsDeferred(PR_FALSE);
+        mRequest->mImage->GetStatusTracker().SyncNotify(mProxies[i]);
+      }
 
-      mRequest->mImage->GetStatusTracker().SyncNotify(mProxy);
+      mRequest->mImage->GetStatusTracker().mRequestRunnable = nsnull;
       return NS_OK;
     }
 
+    void AddProxy(imgRequestProxy* aRequestProxy)
+    {
+      mProxies.AppendElement(aRequestProxy);
+    }
+
   private:
+    friend class imgStatusTracker;
+
     nsRefPtr<imgRequest> mRequest;
-    nsRefPtr<imgRequestProxy> mProxy;
+    nsTArray<nsRefPtr<imgRequestProxy> > mProxies;
 };
 
 void
@@ -110,8 +132,19 @@ imgStatusTracker::Notify(imgRequest* request, imgRequestProxy* proxy)
 
   proxy->SetNotificationsDeferred(PR_TRUE);
 
-  nsCOMPtr<nsIRunnable> ev = new imgRequestNotifyRunnable(request, proxy);
-  NS_DispatchToCurrentThread(ev);
+  // If we have an existing runnable that we can use, we just append this proxy
+  // to its list of proxies to be notified. This ensures we don't unnecessarily
+  // delay onload.
+  imgRequestNotifyRunnable* runnable = static_cast<imgRequestNotifyRunnable*>(mRequestRunnable.get());
+  if (runnable && runnable->mRequest == request) {
+    runnable->AddProxy(proxy);
+  } else {
+    // It's okay to overwrite an existing mRequestRunnable, because adding a
+    // new proxy is strictly a performance optimization. The notification will
+    // always happen, regardless of whether we hold a reference to a runnable.
+    mRequestRunnable = new imgRequestNotifyRunnable(request, proxy);
+    NS_DispatchToCurrentThread(mRequestRunnable);
+  }
 }
 
 // A helper class to allow us to call SyncNotify asynchronously for a given,
@@ -119,7 +152,7 @@ imgStatusTracker::Notify(imgRequest* request, imgRequestProxy* proxy)
 class imgStatusNotifyRunnable : public nsRunnable
 {
   public:
-    imgStatusNotifyRunnable(imgStatusTracker status,
+    imgStatusNotifyRunnable(imgStatusTracker& status,
                             imgRequestProxy* requestproxy)
       : mStatus(status), mImage(status.mImage), mProxy(requestproxy)
     {}
@@ -153,6 +186,7 @@ imgStatusTracker::NotifyCurrentState(imgRequestProxy* proxy)
 
   proxy->SetNotificationsDeferred(PR_TRUE);
 
+  // We don't keep track of 
   nsCOMPtr<nsIRunnable> ev = new imgStatusNotifyRunnable(*this, proxy);
   NS_DispatchToCurrentThread(ev);
 }
