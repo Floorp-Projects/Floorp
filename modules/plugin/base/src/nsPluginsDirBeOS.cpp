465814b36d573d946bfef7b9ed6e9a192db6c73e
Josh Aas <joshmoz@gmail.com>
2010-08-13 04:23:13 -0400

    Backed out changeset 452db8c688ba, bug 578868.

--- modules/plugin/base/src/nsPluginsDirBeOS.cpp
+++ modules/plugin/base/src/nsPluginsDirBeOS.cpp
@@ -139,14 +139,14 @@ nsPluginFile::~nsPluginFile()
  * Loads the plugin into memory using NSPR's shared-library loading
  * mechanism. Handles platform differences in loading shared libraries.
  */
-nsresult nsPluginFile::LoadPlugin(PRLibrary **outLibrary)
+nsresult nsPluginFile::LoadPlugin(PRLibrary* &outLibrary)
 {
         nsCAutoString path;
         nsresult rv = mPlugin->GetNativePath(path);
         if (NS_OK != rv) {
             return rv;
         }
-        pLibrary = *outLibrary = PR_LoadLibrary(path.get());
+        pLibrary = outLibrary = PR_LoadLibrary(path.get());
 
 #ifdef NS_DEBUG
         printf("LoadPlugin() %s returned %lx\n",path,(unsigned long)pLibrary);
@@ -161,10 +161,8 @@ typedef char* (*BeOS_Plugin_GetMIMEDescription)();
 /**
  * Obtains all of the information currently available for this plugin.
  */
-nsresult nsPluginFile::GetPluginInfo(nsPluginInfo& info, PRLibrary **outLibrary)
+nsresult nsPluginFile::GetPluginInfo(nsPluginInfo& info)
 {
-    *outLibrary = nsnull;
-
     info.fVersion = nsnull;
 
     nsCAutoString fullPath;
