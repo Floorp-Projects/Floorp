98fc75216e408d07c5fa2376abeb0da01d459c4a
David Anderson <danderson@mozilla.com>
2010-08-13 02:22:39 -0700

    Backed out changeset c0061c6b65b4 (3% perf regression on v8).

--- js/src/methodjit/StubCalls.cpp
+++ js/src/methodjit/StubCalls.cpp
@@ -1099,6 +1099,8 @@ StubEqualityOp(VMFrame &f)
             } else {
                 cond = (l == r) == EQ;
             }
+        } else if (lval.isNullOrUndefined()) {
+            cond = EQ;
         } else {
             cond = (lval.payloadAsRawUint32() == rval.payloadAsRawUint32()) == EQ;
         }
