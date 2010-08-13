9f901f38b81c036b4b913d0e27e27b22b2f1dcb6
Markus Stange <mstange@themasta.com>
2010-08-13 15:32:27 +0200

    Bug 506826 - Add nsSVGIntegrationUtils::DrawPaintServer for use in -moz-element drawing. r=roc

--- layout/svg/base/src/nsSVGIntegrationUtils.h
+++ layout/svg/base/src/nsSVGIntegrationUtils.h
@@ -42,6 +42,7 @@
 #include "nsRect.h"
 #include "gfxRect.h"
 #include "gfxMatrix.h"
+#include "gfxPattern.h"
 
 class nsIFrame;
 class nsDisplayListBuilder;
@@ -59,6 +60,12 @@ public:
   static PRBool
   UsingEffectsForFrame(const nsIFrame* aFrame);
 
+  /**
+   * Get the union the frame border-box rects over all continuations,
+   * relative to aFirst. This defines "user space" for non-SVG frames.
+   */
+  static nsRect
+  GetNonSVGUserSpace(nsIFrame* aFirst);
   /**
    * Adjust overflow rect for effects.
    * XXX this is a problem. We really need to compute the effects rect for
@@ -114,6 +121,34 @@ public:
    */
   static gfxRect
   GetSVGBBoxForNonSVGFrame(nsIFrame* aNonSVGFrame);
+
+  /**
+   * @param aRenderingContext the target rendering context in which the paint
+   * server will be rendered
+   * @param aTarget the target frame onto which the paint server will be
+   * rendered
+   * @param aPaintServer a first-continuation frame to use as the source
+   * @param aFilter a filter to be applied when scaling
+   * @param aDest the area the paint server image should be mapped to
+   * @param aFill the area to be filled with copies of the paint server image
+   * @param aAnchor a point in aFill which we will ensure is pixel-aligned in
+   * the output
+   * @param aDirty pixels outside this area may be skipped
+   * @param aPaintServerSize the size that would be filled when using
+   * background-repeat:no-repeat and background-size:auto. For normal background
+   * images, this would be the intrinsic size of the image; for gradients and
+   * patterns this would be the whole target frame fill area.
+   */
+  static void
+  DrawPaintServer(nsIRenderingContext* aRenderingContext,
+                  nsIFrame*            aTarget,
+                  nsIFrame*            aPaintServer,
+                  gfxPattern::GraphicsFilter aFilter,
+                  const nsRect&        aDest,
+                  const nsRect&        aFill,
+                  const nsPoint&       aAnchor,
+                  const nsRect&        aDirty,
+                  const nsSize&        aPaintServerSize);
 };
 
 #endif /*NSSVGINTEGRATIONUTILS_H_*/
