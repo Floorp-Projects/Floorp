6aafc856fa81e6e42acf5e08cce9f3d826370b3d
Chris Leary <cdleary@mozilla.com>
2010-08-14 16:46:37 -0700

    [Bug 587183] TM merge broke android in Nitro. r=sayrer.

--- js/src/yarr/yarr/RegexJIT.cpp
+++ js/src/yarr/yarr/RegexJIT.cpp
@@ -25,13 +25,13 @@
 
 #include "RegexJIT.h"
 
+#if ENABLE_ASSEMBLER
+
 #include "assembler/assembler/LinkBuffer.h"
 #include "assembler/assembler/MacroAssembler.h"
 #include "RegexCompiler.h"
 
 #include "yarr/pcre/pcre.h" // temporary, remove when fallback is removed.
-
-#if ENABLE_YARR_JIT
 
 using namespace WTF;
 
