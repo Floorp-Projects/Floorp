e3c4409264ea42d1c48b0fa74a3c52a3eae60df5
Jim Chen <nchen@mozilla.com>
2010-08-16 14:48:32 -0700

    bug 582644 - IME event remoting, patch to widget r=dougt blocking-fennec=2.0a1+

--- dom/ipc/TabChild.h
+++ dom/ipc/TabChild.h
@@ -191,6 +191,10 @@ public:
                               const PRInt32&  aCharCode,
                               const PRInt32&  aModifiers,
                               const bool&     aPreventDefault);
+    virtual bool RecvCompositionEvent(const nsCompositionEvent& event);
+    virtual bool RecvTextEvent(const nsTextEvent& event);
+    virtual bool RecvQueryContentEvent(const nsQueryContentEvent& event);
+    virtual bool RecvSelectionEvent(const nsSelectionEvent& event);
     virtual bool RecvActivateFrameEvent(const nsString& aType, const bool& capture);
     virtual bool RecvLoadRemoteScript(const nsString& aURL);
     virtual bool RecvAsyncMessage(const nsString& aMessage,
@@ -284,6 +288,8 @@ public:
 protected:
     NS_OVERRIDE
     virtual bool RecvDestroy();
+
+    bool DispatchWidgetEvent(nsGUIEvent& event);
 
 private:
     void ActorDestroy(ActorDestroyReason why);
