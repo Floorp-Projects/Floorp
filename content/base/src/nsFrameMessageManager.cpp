e398188894f62b92a5aa327dce88b53d7b216a3d
Ehsan Akhgari <ehsan@mozilla.com>
2010-08-16 17:20:03 -0400

    Bustage fix for bug 587222 landed on a CLOSED TREE

--- content/base/src/nsFrameMessageManager.cpp
+++ content/base/src/nsFrameMessageManager.cpp
@@ -678,5 +678,5 @@ nsFrameScriptExecutor::LoadFrameScriptInternal(const nsAString& aURL)
   }
 }
 
-NS_IMPL_ISUPPORTS1(nsScriptCacheCleaner, nsIObserver);
+NS_IMPL_ISUPPORTS1(nsScriptCacheCleaner, nsIObserver)
 
