6d2d927eebced5b420016ab3481c69dce4e5cc93
David Anderson <danderson@mozilla.com>
2010-08-16 13:37:47 -0400

    Disable recursion for heavyweight functions (bug 583757, r=jorendorff).

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -10348,7 +10348,7 @@ IsTraceableRecursion(JSContext *cx)
         return false;
     if (*fp->getScript()->code != JSOP_TRACE)
         return false;
-    return true;
+    return !fp->getFunction()->isHeavyweight();
 }
 
 JS_REQUIRES_STACK AbortableRecordingStatus
