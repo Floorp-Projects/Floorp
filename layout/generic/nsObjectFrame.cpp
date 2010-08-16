2afbd46dc160c12fce7b3b700b8106cfd9b9bdbe
Benjamin Smedberg <benjamin@smedbergs.us>
2010-08-16 10:10:25 -0400

    Bug 583053 - Corruption issues when running rdio because windows double-pass rendering events don't win the RPC race, r=bent

--- layout/generic/nsObjectFrame.cpp
+++ layout/generic/nsObjectFrame.cpp
@@ -46,6 +46,10 @@
 
 /* rendering objects for replaced elements implemented by a plugin */
 
+#ifdef MOZ_IPC
+#include "mozilla/plugins/PluginMessageUtils.h"
+#endif
+
 #ifdef MOZ_WIDGET_QT
 #include <QWidget>
 #include <QKeyEvent>
@@ -207,9 +211,6 @@ using mozilla::DefaultXDisplay;
 #ifdef XP_WIN
 #include <wtypes.h>
 #include <winuser.h>
-#ifdef MOZ_IPC
-#define NS_OOPP_DOUBLEPASS_MSGID TEXT("MozDoublePassMsg")
-#endif
 #endif
 
 #ifdef XP_OS2
@@ -623,10 +624,6 @@ nsObjectFrame::Init(nsIContent*      aContent,
          ("Initializing nsObjectFrame %p for content %p\n", this, aContent));
 
   nsresult rv = nsObjectFrameSuper::Init(aContent, aParent, aPrevInFlow);
-
-#ifdef XP_WIN
-  mDoublePassEvent = 0;
-#endif
 
   return rv;
 }
@@ -1726,15 +1723,12 @@ nsObjectFrame::PaintPlugin(nsIRenderingContext& aRenderingContext,
         // OOP plugin specific: let the shim know before we paint if we are doing a
         // double pass render. If this plugin isn't oop, the register window message
         // will be ignored.
-        if (!mDoublePassEvent)
-          mDoublePassEvent = ::RegisterWindowMessage(NS_OOPP_DOUBLEPASS_MSGID);
-        if (mDoublePassEvent) {
-          NPEvent pluginEvent;
-          pluginEvent.event = mDoublePassEvent;
-          pluginEvent.wParam = 0;
-          pluginEvent.lParam = 0;
+        NPEvent pluginEvent;
+        pluginEvent.event = mozilla::plugins::DoublePassRenderingEvent();
+        pluginEvent.wParam = 0;
+        pluginEvent.lParam = 0;
+        if (pluginEvent.event)
           inst->HandleEvent(&pluginEvent, nsnull);
-        }
       }
 #endif
       do {
