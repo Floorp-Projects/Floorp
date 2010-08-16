36b65f9ee11e24ca735e4babd46a746913e6ec0b
Frank Yan <fyan@mozilla.com>
2010-08-16 16:33:32 -0400

    Bug 587267 - Display the Firefox button when in TabView; r=dao approval2.0=gavin.sharp

--- browser/base/content/tabview/ui.js
+++ browser/base/content/tabview/ui.js
@@ -308,7 +308,7 @@ var UIManager = {
 
 #ifdef XP_WIN
     // Restore the full height when showing TabView
-    gTabViewFrame.style.marginTop = "22px";
+    gTabViewFrame.style.marginTop = "";
 #endif
     gTabViewDeck.selectedIndex = 1;
     gTabViewFrame.contentWindow.focus();
