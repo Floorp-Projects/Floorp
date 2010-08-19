1adcc044ca738815852b646e71574289b79db601
Mounir Lamouri <mounir.lamouri@gmail.com>
2010-08-20 01:23:59 +0200

    Bug 566160 - Implement formaction for <input> and <button> which ovverid <form> action attribute. r=sicking sr=smaug a2.0=blocking

--- content/html/content/src/nsHTMLFormElement.h
+++ content/html/content/src/nsHTMLFormElement.h
@@ -241,7 +241,7 @@ public:
    * submission. In that case the form will defer the submission until the
    * script handler returns and the return value is known.
    */
-  void OnSubmitClickBegin();
+  void OnSubmitClickBegin(nsIContent* aOriginatingElement);
   void OnSubmitClickEnd();
 
   virtual nsXPCClassInfo* GetClassInfo();
@@ -326,8 +326,9 @@ protected:
    * Get the full URL to submit to.  Do not submit if the returned URL is null.
    *
    * @param aActionURL the full, unadulterated URL you'll be submitting to [OUT]
+   * @param aOriginatingElement the originating element of the form submission [IN]
    */
-  nsresult GetActionURL(nsIURI** aActionURL);
+  nsresult GetActionURL(nsIURI** aActionURL, nsIContent* aOriginatingElement);
 
 public:
   /**
