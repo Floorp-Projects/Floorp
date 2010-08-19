8f47aad420423bbda7532ddc9e73d8db1c1cb60b
fantasai <fantasai.cvs@inkedblade.net>
2010-08-19 09:38:00 -0400

    Do IsProperAncestorFrame check before destroying floats, but not for popups.  (Bug 588158)  r=dbaron  a2.0=blocking

--- layout/generic/nsPlaceholderFrame.cpp
+++ layout/generic/nsPlaceholderFrame.cpp
@@ -163,7 +163,7 @@ nsPlaceholderFrame::DestroyFrom(nsIFrame* aDestructRoot)
     // then call RemoveFrame on it here.
     // Also destroy it here if it's a popup frame. (Bug 96291)
     if (shell->FrameManager() &&
-        ((GetStateBits() & PLACEHOLDER_FOR_FLOAT) ||
+        ((GetStateBits() & PLACEHOLDER_FOR_POPUP) ||
          !nsLayoutUtils::IsProperAncestorFrame(aDestructRoot, oof))) {
       nsIAtom* listName = nsLayoutUtils::GetChildListNameFor(oof);
       shell->FrameManager()->RemoveFrame(listName, oof);
