1cb20180925cdd1d11bbb07141e81eeea781b5f0
David Mandelin <dmandelin@mozilla.com>
2010-08-19 16:25:54 -0700

    [JAEGER] Backed out changeset b88bab8e77c5: prerequisite to backing out 04bc789f7a43

--- js/src/methodjit/TrampolineCompiler.cpp
+++ js/src/methodjit/TrampolineCompiler.cpp
@@ -138,7 +138,11 @@ TrampolineCompiler::generateForceReturn(Assembler &masm)
     masm.loadPayload(rval, JSReturnReg_Data);
     masm.loadTypeTag(rval, JSReturnReg_Type);
 
-    masm.restoreReturnAddress();
+#ifndef JS_CPU_ARM
+    masm.push(Address(JSFrameReg, offsetof(JSStackFrame, ncode)));
+#else
+    masm.move(Address(JSFrameReg, offsetof(JSStackFrame, ncode)), JSC::ARMRegisters::lr);
+#endif
 
     masm.move(Registers::ReturnReg, JSFrameReg);
 #ifdef DEBUG
