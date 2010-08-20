7387dfa5b16fd4fce630e5aa3811b75666e5afe6
Chris Jones <jones.chris.g@gmail.com>
2010-08-20 14:29:01 -0500

    Bug 582057, part c: Initialize default widget init data earlier so that it's always available. r=roc

--- view/src/nsView.cpp
+++ view/src/nsView.cpp
@@ -688,26 +688,24 @@ nsresult nsView::CreateWidget(const nsIID &aWindowIID,
     NS_RELEASE(mWindow);
   }
 
-  nsIntRect trect = CalcWidgetBounds(aWidgetInitData
-                                     ? aWidgetInitData->mWindowType
-                                     : eWindowType_child);
+  PRBool initDataPassedIn = PR_TRUE;
+  nsWidgetInitData initData;
+  if (!aWidgetInitData) {
+    // No initData, we're a child window
+    initDataPassedIn = PR_FALSE;
+    initData.mWindowType = eWindowType_child;
+    initData.clipChildren = PR_TRUE;
+    initData.clipSiblings = PR_TRUE;
+    aWidgetInitData = &initData;
+  }
+  aWidgetInitData->mContentType = aContentType;
+
+  nsIntRect trect = CalcWidgetBounds(aWidgetInitData->mWindowType);
 
   if (NS_OK == LoadWidget(aWindowIID))
   {
     nsCOMPtr<nsIDeviceContext> dx;
     mViewManager->GetDeviceContext(*getter_AddRefs(dx));
-
-    PRBool initDataPassedIn = PR_TRUE;
-    nsWidgetInitData initData;
-    if (!aWidgetInitData) {
-      // No initData, we're a child window
-      // Create initData to pass in params
-      initDataPassedIn = PR_FALSE;
-      initData.clipChildren = PR_TRUE; // Clip child window's children
-      initData.clipSiblings = PR_TRUE; // Clip child window's siblings
-      aWidgetInitData = &initData;
-    }
-    aWidgetInitData->mContentType = aContentType;
 
     if (aNative && aWidgetInitData->mWindowType != eWindowType_popup)
       mWindow->Create(nsnull, aNative, trect, ::HandleEvent, dx, nsnull, nsnull, aWidgetInitData);
