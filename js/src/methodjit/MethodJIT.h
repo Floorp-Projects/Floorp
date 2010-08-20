7881bcd6de13883eada38aed442b6e2ef5a85e3f
David Anderson <dvander@alliedmods.net>
2010-08-20 13:20:38 -0700

    [JAEGER] Bug 587833 reland part 1: remove VMFrame::scriptedReturn for x86 and x64, r=dmandelin

--- js/src/methodjit/MethodJIT.h
+++ js/src/methodjit/MethodJIT.h
@@ -57,13 +57,11 @@ namespace js {
 
 struct VMFrame
 {
-    /* This must be the first entry on CPUs which push return addresses. */
-    void *scriptedReturn;
-
     union Arguments {
         struct {
             void *ptr;
             void *ptr2;
+            void *ptr3;
         } x;
     } u;
 
@@ -173,8 +171,11 @@ typedef void (JS_FASTCALL *VoidStubPC)(VMFrame &, jsbytecode *);
 
 namespace mjit {
 
-JSBool
-JaegerShot(JSContext *cx);
+/* Execute a method that has been JIT compiled. */
+JSBool JaegerShot(JSContext *cx);
+
+/* Drop into the middle of a method at an arbitrary point, and execute. */
+JSBool JaegerShotAtSafePoint(JSContext *cx, void *safePoint);
 
 enum CompileStatus
 {
@@ -224,7 +225,7 @@ extern "C" void *JaegerThrowpoline(js::VMFrame *vmFrame);
 #else
 extern "C" void JaegerThrowpoline();
 #endif
-extern "C" void JaegerFromTracer();
+extern "C" void InjectJaegerReturn();
 
 #endif /* jsjaeger_h__ */
 
