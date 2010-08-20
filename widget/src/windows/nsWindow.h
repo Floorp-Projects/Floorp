bac42502568e7fa379f20127680ae0b69d7dd55d
Bas Schouten <bschouten@mozilla.com>
2010-08-20 05:40:51 +0200

    Bug 588690 - Part 2: Clear cached resources when a windows becomes inactive. r=roc a=blocking-beta5+

--- widget/src/windows/nsWindow.h
+++ widget/src/windows/nsWindow.h
@@ -291,9 +291,7 @@ protected:
   static LRESULT CALLBACK MozSpecialWndProc(int code, WPARAM wParam, LPARAM lParam);
   static LRESULT CALLBACK MozSpecialMouseProc(int code, WPARAM wParam, LPARAM lParam);
   static VOID    CALLBACK HookTimerForPopups( HWND hwnd, UINT uMsg, UINT idEvent, DWORD dwTime );
-#ifdef CAIRO_HAS_D2D_SURFACE
-  static BOOL    CALLBACK ClearD2DSurfaceCallback(HWND aChild, LPARAM aParam);
-#endif
+  static BOOL    CALLBACK ClearResourcesCallback(HWND aChild, LPARAM aParam);
 
   /**
    * Window utilities
@@ -455,9 +453,7 @@ protected:
 #ifdef ACCESSIBILITY
   static STDMETHODIMP_(LRESULT) LresultFromObject(REFIID riid, WPARAM wParam, LPUNKNOWN pAcc);
 #endif // ACCESSIBILITY
-#ifdef CAIRO_HAS_D2D_SURFACE
-  void                    ClearD2DSurface();
-#endif
+  void                    ClearCachedResources();
 
 protected:
   nsCOMPtr<nsIWidget>   mParent;
