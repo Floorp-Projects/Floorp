5300943be29e1b52e5b2f13f2e5c845337360e78
Gavin Sharp <gavin@gavinsharp.com>
2010-08-23 15:44:07 -0700

    Bug 589498: clean up browser test timing code, r=dtownsend, a=tests

--- testing/mochitest/browser-test.js
+++ testing/mochitest/browser-test.js
@@ -161,6 +161,11 @@ Tester.prototype = {
         let func = testScope.__cleanupFunctions.shift();
         func.apply(testScope);
       };
+
+      // Note the test run time
+      let time = Date.now() - this.lastStartTime;
+      let msg = "Test took " + (time / 1000) + "s to complete\n";
+      this.currentTest.addResult(new testMessage(msg));
     }
 
     // Check the window state for the current test before moving to the next one.
@@ -170,12 +175,6 @@ Tester.prototype = {
   },
 
   realNextTest: function Test_realNextTest() {
-    if (this.lastStartTime) {
-      let time = Date.now() - this.lastStartTime;
-      this.dumper.dump("TEST-END | " + this.currentTest.path + " | Test took " +
-                       time + "ms to complete\n");
-    }
-
     if (this.done) {
       this.finish();
       return;
