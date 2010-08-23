bf05ea86782b716beed59327f8aa625997dc53bf
Axel Hecht <l10n@mozilla.com>
2010-08-23 10:08:50 +0200

    bug 589313, remove obsolete l10n tests, those are hosted elsewhere, details in the bug. r/a=sayrer, NPOTB

--- testing/tests/l10n/extensions/coverage/coverage-extension-prefs.js
+++
@@ -1,3 +0,0 @@
-pref('javascript.options.showInConsole', true);
-pref('testing.extensions.l10n.coverage', 'chrome://coverage/content/');
-pref('testing.extensions.l10n.neterror-coverage', 'chrome://coverage/content/neterror-coverage.xul');
