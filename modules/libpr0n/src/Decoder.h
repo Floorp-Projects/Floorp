c6ee2409ab014e3164cf792515cac6d89a90c050
Bobby Holley <bobbyholley@gmail.com>
2010-08-24 16:40:45 -0400

    Bug 590252 - part 1 - Add framework to Decoder superclass to handle invalidations.r=joe,a=blocker

--- modules/libpr0n/src/Decoder.h
+++ modules/libpr0n/src/Decoder.h
@@ -86,6 +86,11 @@ public:
    */
   nsresult Finish();
 
+  /**
+   * Tells the decoder to flush any pending invalidations.
+   */
+  void FlushInvalidations();
+
   // We're not COM-y, so we don't get refcounts by default
   NS_INLINE_DECL_REFCOUNTING(Decoder)
 
@@ -130,6 +135,9 @@ protected:
   void PostFrameStart();
   void PostFrameStop();
 
+  // Called by the decoders when they have a region to invalidate. We may not
+  // actually pass these invalidations on right away.
+  void PostInvalidation(nsIntRect& aRect);
 
   /*
    * Member variables.
@@ -140,6 +148,8 @@ protected:
   nsCOMPtr<imgIDecoderObserver> mObserver;
 
   PRUint32 mFrameCount; // Number of frames, including anything in-progress
+
+  nsIntRect mInvalidRect; // Tracks an invalidation region in the current frame.
 
   bool mInitialized;
   bool mSizeDecode;
