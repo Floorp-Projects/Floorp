cee73eabca851a83be009639de644e2405f07b19
David Mandelin <dmandelin@mozilla.com>
2010-08-23 17:24:34 -0700

    [JAEGER] Enable tracejit for content to start testing trace integration on tinderbox

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -573,7 +573,7 @@ pref("javascript.options.strict",           false);
 pref("javascript.options.strict.debug",     true);
 #endif
 pref("javascript.options.relimit",          true);
-pref("javascript.options.tracejit.content",  false);
+pref("javascript.options.tracejit.content",  true);
 pref("javascript.options.tracejit.chrome",   true);
 pref("javascript.options.methodjit.content", true);
 pref("javascript.options.methodjit.chrome",  false);
