1de8e440137b6340e68f3136ccbefa2fdf317bb2
Matt Woodrow <matt.woodrow+bugzilla@gmail.com>
2010-08-23 20:58:56 -0700

    Bug 589634 - Don't create a sub-widget for decks. r=roc, a=roc

--- layout/generic/nsFrameFrame.cpp
+++ layout/generic/nsFrameFrame.cpp
@@ -292,11 +292,6 @@ nsSubDocumentFrame::Init(nsIContent*     aContent,
   }
   nsIView* view = GetView();
 
-  if (aParent->GetStyleDisplay()->mDisplay == NS_STYLE_DISPLAY_DECK
-      && !view->HasWidget()) {
-    view->CreateWidget();
-  }
-
   // Set the primary frame now so that
   // DocumentViewerImpl::FindContainerView called by ShowViewer below
   // can find it if necessary.
