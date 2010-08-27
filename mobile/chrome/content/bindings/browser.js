e4273eb658d84268333737f55f76c77e4cf0fa2c
Benjamin Stover <bstover@mozilla.com>
2010-08-27 12:12:48 -0700

    support for caching outside of visible region (currently buggy in platform)

--- mobile/chrome/content/bindings/browser.js
+++ mobile/chrome/content/bindings/browser.js
@@ -366,6 +366,8 @@ let ContentScroll =  {
   init: function() {
     addMessageListener("Content:ScrollTo", this);
     addMessageListener("Content:ScrollBy", this);
+    addMessageListener("Content:ZoomLevel", this);
+    addMessageListener("Content:FastScrollTo", this);
   },
 
   receiveMessage: function(aMessage) {
@@ -376,6 +378,20 @@ let ContentScroll =  {
         break;
       case "Content:ScrollBy":
         content.scrollBy(json.dx, json.dy);
+        break;
+      case "Content:ZoomLevel":
+        // XXX not working yet
+        break;
+      case "Content:FastScrollTo":
+        // XXX not working yet
+        try {
+          let cwu = Util.getWindowUtils(content);
+          cwu.setDisplayport(json.x - 200, json.y - 400,
+                             content.innerWidth + 400, content.innerHeight + 800);
+        } catch(e) {
+          Util.dumpLn(e);
+        }
+        sendAsyncMessage("Content:FastScrollTo:Return");
         break;
     }
   }
