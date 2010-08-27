203e5bb58c613d566fc03eaa95edb19877af8e1c
Timothy Nikkel <tnikkel@gmail.com>
2010-08-27 18:15:07 -0500

    Bug 577579. Add a nsIDOMWindowUtils method to send a mouse event to the window directly instead of via the toplevel window. r=roc

--- dom/base/nsDOMWindowUtils.cpp
+++ dom/base/nsDOMWindowUtils.cpp
@@ -66,6 +66,8 @@
 #include "gfxImageSurface.h"
 #include "nsLayoutUtils.h"
 #include "nsComputedDOMStyle.h"
+#include "nsIViewObserver.h"
+#include "nsIPresShell.h"
 
 #if defined(MOZ_X11) && defined(MOZ_WIDGET_GTK2)
 #include <gdk/gdk.h>
@@ -226,6 +228,33 @@ nsDOMWindowUtils::SendMouseEvent(const nsAString& aType,
                                  PRInt32 aModifiers,
                                  PRBool aIgnoreRootScrollFrame)
 {
+  return SendMouseEventCommon(aType, aX, aY, aButton, aClickCount, aModifiers,
+                              aIgnoreRootScrollFrame, PR_FALSE);
+}
+
+NS_IMETHODIMP
+nsDOMWindowUtils::SendMouseEventToWindow(const nsAString& aType,
+                                         float aX,
+                                         float aY,
+                                         PRInt32 aButton,
+                                         PRInt32 aClickCount,
+                                         PRInt32 aModifiers,
+                                         PRBool aIgnoreRootScrollFrame)
+{
+  return SendMouseEventCommon(aType, aX, aY, aButton, aClickCount, aModifiers,
+                              aIgnoreRootScrollFrame, PR_TRUE);
+}
+
+NS_IMETHODIMP
+nsDOMWindowUtils::SendMouseEventCommon(const nsAString& aType,
+                                       float aX,
+                                       float aY,
+                                       PRInt32 aButton,
+                                       PRInt32 aClickCount,
+                                       PRInt32 aModifiers,
+                                       PRBool aIgnoreRootScrollFrame,
+                                       PRBool aToWindow)
+{
   if (!IsUniversalXPConnectCapable()) {
     return NS_ERROR_DOM_SECURITY_ERR;
   }
@@ -281,8 +310,29 @@ nsDOMWindowUtils::SendMouseEvent(const nsAString& aType,
                           appPerDev);
   event.ignoreRootScrollFrame = aIgnoreRootScrollFrame;
 
+  nsresult rv;
   nsEventStatus status;
-  return widget->DispatchEvent(&event, status);
+  if (aToWindow) {
+    nsIPresShell* presShell = presContext->PresShell();
+    if (!presShell)
+      return NS_ERROR_FAILURE;
+    nsCOMPtr<nsIViewObserver> vo = do_QueryInterface(presShell);
+    if (!vo)
+      return NS_ERROR_FAILURE;
+    nsIViewManager* viewManager = presShell->GetViewManager();
+    if (!viewManager)
+      return NS_ERROR_FAILURE;
+    nsIView* view = nsnull;
+    rv = viewManager->GetRootView(view);
+    if (NS_FAILED(rv) || !view)
+      return NS_ERROR_FAILURE;
+
+    status = nsEventStatus_eIgnore;
+    rv = vo->HandleEvent(view, &event, &status);
+  } else {
+    rv = widget->DispatchEvent(&event, status);
+  }
+  return rv;
 }
 
 NS_IMETHODIMP
