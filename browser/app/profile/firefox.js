d7ea82d23498b930c637a880ae0b94c426286017
Blair McBride <bmcbride@mozilla.com>
2010-08-30 17:35:21 +1200

    Backed out changeset 6fe388a0fb5e (Bug 581076) due to test failures. a=bustage
    --HG--
    extra : rebase_source : f726613cbce0938d25fd58a9547a5a25d15c1433

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -56,12 +56,13 @@ pref("browser.hiddenWindowChromeURL", "chrome://browser/content/hiddenWindow.xul
 // Enables some extra Extension System Logging (can reduce performance)
 pref("extensions.logging.enabled", false);
 
-// Preferences for AMO integration
+// Preferences for the Addon Repository
 pref("extensions.getAddons.cache.enabled", true);
 pref("extensions.getAddons.maxResults", 15);
 pref("extensions.getAddons.get.url", "https://services.addons.mozilla.org/%LOCALE%/%APP%/api/%API_VERSION%/search/guid:%IDS%");
-pref("extensions.getAddons.search.browseURL", "https://addons.mozilla.org/%LOCALE%/%APP%/search?q=%TERMS%");
 pref("extensions.getAddons.search.url", "https://services.addons.mozilla.org/%LOCALE%/%APP%/api/%API_VERSION%/search/%TERMS%/all/%MAX_RESULTS%/%OS%/%VERSION%");
+
+// Preferences for AMO integration
 pref("extensions.webservice.discoverURL", "https://services.addons.mozilla.org/%LOCALE%/%APP%/discovery/%VERSION%/%OS%");
 
 // Blocklist preferences
