0fe2dd0835db31fb7bf5843bb897285a5a36345c
Matt Woodrow <mwoodrow@mozilla.com>
2010-09-03 15:51:04 +1200

    Bug 590367 - Render RGB d2d layers with a RGBA surface and shader r=Bas, a=blocking2.0

--- gfx/layers/d3d9/CanvasLayerD3D9.cpp
+++ gfx/layers/d3d9/CanvasLayerD3D9.cpp
@@ -239,7 +239,7 @@ CanvasLayerD3D9::RenderLayer()
   opacity[0] = GetOpacity();
   device()->SetPixelShaderConstantF(0, opacity, 1);
 
-  mD3DManager->SetShaderMode(DeviceManagerD3D9::RGBLAYER);
+  mD3DManager->SetShaderMode(DeviceManagerD3D9::RGBALAYER);
 
   if (!mDataIsPremultiplied) {
     device()->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
