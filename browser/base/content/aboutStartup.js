dda07207ffc914302a32693e0fe4ec0af4cbd3bf
Daniel Brooks <db48x@db48x.net>
2010-09-05 16:09:55 -0400

    about:startup - record results in database after startup has finished, and show the records in the about:startup page as a chart

--- browser/base/content/aboutStartup.js
+++ browser/base/content/aboutStartup.js
@@ -19,10 +19,63 @@ displayDuration("restored", restored - startup);
 
 function displayTimestamp(id, µs)
 {
-  document.getElementById(id).textContent = new Date(µs/1000) +" ("+ µs +" µs)";
+  document.getElementById(id).textContent = formatstamp(µs);
 }
 
 function displayDuration(id, µs)
 {
-  document.getElementById(id).nextSibling.textContent = µs +" µs";
+  document.getElementById(id).nextSibling.textContent = formatµs(µs);
+}
+
+function formatstamp(µs)
+{
+  return new Date(µs/1000) +" ("+ formatµs(µs) +")";
+}
+
+function formatµs(µs)
+{
+  return µs + " µs";
+}
+
+var table = document.getElementsByTagName("table")[1];
+
+var file = Components.classes["@mozilla.org/file/directory_service;1"]
+                     .getService(Components.interfaces.nsIProperties)
+                     .get("ProfD", Components.interfaces.nsIFile);
+file.append("startup.sqlite");
+
+var svc = Components.classes["@mozilla.org/storage/service;1"]
+                    .getService(Components.interfaces.mozIStorageService);
+var db = svc.openDatabase(file);
+var query = db.createStatement("SELECT timestamp, launch, startup FROM duration");
+query.executeAsync({
+  handleResult: function(results)
+  {
+    for (let row = results.getNextRow(); row; row = results.getNextRow())
+    {
+        table.appendChild(tr(td(formatstamp(row.getResultByName("timestamp"))),
+                             td(formatµs(l = row.getResultByName("launch"))),
+                             td(formatµs(s = row.getResultByName("startup"))),
+                             td(formatµs(l + s))));
+    }
+  },
+  handleError: function(error)
+  {
+      table.appendChild(tr(td("Error: "+ error.message +" ("+ error.result +")")));
+  },
+  handleCompletion: function() { },
+});
+
+function td(str)
+{
+  let cell = document.createElement("td");
+  cell.innerHTML = str;
+  return cell;
+}
+
+function tr()
+{
+  let row = document.createElement("tr");
+  Array.forEach(arguments, function(cell) { row.appendChild(cell); });
+  return row;
 }
