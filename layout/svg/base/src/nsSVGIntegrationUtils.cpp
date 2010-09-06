d788a8fef81a2291d514eb901598bb80913d8d2d
Markus Stange <mstange@themasta.com>
2010-09-06 15:12:46 +1200

    Bug 591141. Protect against null pattern. r=roc

--- layout/svg/base/src/nsSVGIntegrationUtils.cpp
+++ layout/svg/base/src/nsSVGIntegrationUtils.cpp
@@ -504,6 +504,9 @@ DrawableFromPaintServer(nsIFrame*         aFrame,
     nsRefPtr<gfxPattern> pattern =
       server->GetPaintServerPattern(aTarget, 1.0, &overrideBounds);
 
+    if (!pattern)
+      return nsnull;
+
     // pattern is now set up to fill aPaintServerSize. But we want it to
     // fill aRenderSize, so we need to add a scaling transform.
     // We couldn't just have set overrideBounds to aRenderSize - it would have
@@ -547,6 +550,8 @@ nsSVGIntegrationUtils::DrawPaintServer(nsIRenderingContext* aRenderingContext,
   nsRefPtr<gfxDrawable> drawable =
     DrawableFromPaintServer(aPaintServer, aTarget, aPaintServerSize, imageSize);
 
-  nsLayoutUtils::DrawPixelSnapped(aRenderingContext, drawable, aFilter,
-                                  aDest, aFill, aAnchor, aDirty);
+  if (drawable) {
+    nsLayoutUtils::DrawPixelSnapped(aRenderingContext, drawable, aFilter,
+                                    aDest, aFill, aAnchor, aDirty);
+  }
 }
