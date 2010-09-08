3b745d1980f6a846e45972f4266f331090c542c5
Rick Reitmaier <rreitmai@adobe.com>
2010-09-08 08:50:53 -0700

    Bug 568486 - Synchronize the SH4 backend with Bug 587916 (r=rreitmai) [Cedric Vincent]

--- js/src/nanojit/NativeSH4.cpp
+++ js/src/nanojit/NativeSH4.cpp
@@ -2661,7 +2661,7 @@ namespace nanojit
         findSpecificRegFor(value, reg);
     }
 
-    void Assembler::asm_spill(Register reg, int offset, bool /* pop */, bool quad) {
+    void Assembler::asm_spill(Register reg, int offset, bool quad) {
         (void)quad;
 
         if (offset == 0)
@@ -3161,7 +3161,6 @@ namespace nanojit
     void Assembler::nRegisterResetAll(RegAlloc& regs) {
         regs.clear();
         regs.free = GpRegs | FpRegs;
-        debug_only( regs.managed = regs.free; )
     }
 
     // Keep in sync' with Assembler::asm_param().
