9517b04f9daff72605cccc161ac4dbbfaaac8a88
Marco Bonardo <mbonardo@mozilla.com>
2010-09-08 16:06:42 +0200

    Bug 594183 - Remove useless title tooltips from about:home. r+a=gavin

--- browser/base/content/aboutHome.js
+++ browser/base/content/aboutHome.js
@@ -95,8 +95,11 @@ function setupSearchEngine() {
     gSearchEngine.searchUrl += "&" + gSearchEngine.params;
 
   // Add search engine logo.
-  if (gSearchEngine.image)
-    document.getElementById("searchEngineLogo").src = gSearchEngine.image;
+  if (gSearchEngine.image) {
+    let logoElt = document.getElementById("searchEngineLogo");
+    logoElt.src = gSearchEngine.image;
+    logoElt.alt = gSearchEngine.name;
+  }
 
   if (gSearchEngine.links) {
     // Add search engine links.
