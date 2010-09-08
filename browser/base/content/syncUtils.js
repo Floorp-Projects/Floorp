0bbd68642c2fa11be9fa773a0d1d57bf79f4432a
Philipp von Weitershausen <philipp@weitershausen.de>
2010-09-08 03:37:46 +0200

    Bug 590805 - Sync UI: Revamp setup wizard (part 1), r=mconnor a=blocking2.0
    Generate Sync Key: Implement key generation and backup (email/print/save)

--- browser/base/content/syncUtils.js
+++ browser/base/content/syncUtils.js
@@ -22,6 +22,7 @@
  *   Edward Lee <edilee@mozilla.com>
  *   Mike Connor <mconnor@mozilla.com>
  *   Paul O’Shannessy <paul@oshannessy.com>
+ *   Philipp von Weitershausen <philipp@weitershausen.de>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -39,6 +40,11 @@
 
 // Weave should always exist before before this file gets included.
 let gSyncUtils = {
+  get bundle() {
+    delete this.bundle;
+    return this.bundle = Services.strings.createBundle("chrome://browser/locale/syncSetup.properties");
+  },
+
   // opens in a new window if we're in a modal prefwindow world, in a new tab otherwise
   _openLink: function (url) {
     let thisDocEl = document.documentElement,
@@ -108,6 +114,136 @@ let gSyncUtils = {
     let url = this._baseURL + "secondrun.html";
     this._openLink(url);
   },
+
+  /**
+   * Generate 20 random characters a-z
+   */
+  generatePassphrase: function() {
+    let rng = Cc["@mozilla.org/security/random-generator;1"]
+                .createInstance(Ci.nsIRandomGenerator);
+    let bytes = rng.generateRandomBytes(20);
+    return [String.fromCharCode(97 + Math.floor(byte * 26 / 256))
+            for each (byte in bytes)].join("");
+  },
+
+  /**
+   * Hyphenate a 20 character passphrase in 4 groups of 5.
+   */
+  hyphenatePassphrase: function(passphrase) {
+    return passphrase.slice(0, 5) + '-'
+         + passphrase.slice(5, 10) + '-'
+         + passphrase.slice(10, 15) + '-'
+         + passphrase.slice(15, 20);
+  },
+
+  /**
+   * Remove hyphens as inserted by hyphenatePassphrase().
+   */
+  normalizePassphrase: function(pp) {
+    if (pp.length == 23 && pp[5] == '-' && pp[11] == '-' && pp[17] == '-')
+      return pp.slice(0, 5) + pp.slice(6, 11)
+           + pp.slice(12, 17) + pp.slice(18, 23);
+    return pp;
+  },
+
+  /**
+   * Trigger the mailto protocol handler to send a passphrase backup email.
+   * 
+   * @param elid : ID of the form element containing the passphrase.
+   */
+  passphraseEmail: function(elid) {
+    let pp = document.getElementById(elid).value;
+    let subject = this.bundle.GetStringFromName("email.synckey.subject");
+    let body = this.bundle.formatStringFromName("email.synckey.body", [pp], 1);
+    let uri = Weave.Utils.makeURI("mailto:?subject=" + subject + "&body=" + body);
+    let protoSvc = Cc["@mozilla.org/uriloader/external-protocol-service;1"]
+                     .getService(Ci.nsIExternalProtocolService);
+    protoSvc.loadURI(uri);
+  },
+
+  /**
+   * Prepare an invisible iframe with the passphrase backup document.
+   * Used by both the print and saving methods.
+   *
+   * @param elid : ID of the form element containing the passphrase.
+   * @param callback : Function called once the iframe has loaded.
+   */
+  _preparePPiframe: function(elid, callback) {
+    let pp = document.getElementById(elid).value;
+
+    // Create an invisible iframe whose contents we can print.
+    let iframe = document.createElement("iframe");
+    iframe.setAttribute("src", "chrome://browser/content/syncKey.xhtml");
+    iframe.collapsed = true;
+    document.documentElement.appendChild(iframe);
+    iframe.contentWindow.addEventListener("load", function() {
+      iframe.contentWindow.removeEventListener("load", arguments.callee, false);
+
+      // Insert the Sync Key into the page.
+      let el = iframe.contentDocument.getElementById("synckey");
+      el.firstChild.nodeValue = pp;
+
+      callback(iframe);
+    }, false);
+  },
+
+  /**
+   * Print passphrase backup document.
+   * 
+   * @param elid : ID of the form element containing the passphrase.
+   */
+  passphrasePrint: function(elid) {
+    this._preparePPiframe(elid, function(iframe) {
+      let webBrowserPrint = iframe.contentWindow
+                                  .QueryInterface(Ci.nsIInterfaceRequestor)
+                                  .getInterface(Ci.nsIWebBrowserPrint);
+      let printSettings = PrintUtils.getPrintSettings();
+
+      // Display no header/footer decoration except for the date.
+      printSettings.headerStrLeft
+        = printSettings.headerStrCenter
+        = printSettings.headerStrRight
+        = printSettings.footerStrLeft
+        = printSettings.footerStrCenter = "";
+      printSettings.footerStrRight = "&D";
+
+      try {
+        webBrowserPrint.print(printSettings, null);
+      } catch (ex) {
+        // print()'s return codes are expressed as exceptions. Ignore.
+      }
+    });
+  },
+
+  /**
+   * Save passphrase backup document to disk as HTML file.
+   * 
+   * @param elid : ID of the form element containing the passphrase.
+   */
+  passphraseSave: function(elid) {
+    let dialogTitle = this.bundle.GetStringFromName("save.synckey.title");
+    this._preparePPiframe(elid, function(iframe) {
+      let filepicker = Cc["@mozilla.org/filepicker;1"]
+                         .createInstance(Ci.nsIFilePicker);
+      filepicker.init(window, dialogTitle, Ci.nsIFilePicker.modeSave);
+      filepicker.appendFilters(Ci.nsIFilePicker.filterHTML);
+      filepicker.defaultString = "Firefox Sync Key.html";
+      let rv = filepicker.show();
+      if (rv == Ci.nsIFilePicker.returnOK
+          || rv == Ci.nsIFilePicker.returnReplace) {
+        let stream = Cc["@mozilla.org/network/file-output-stream;1"]
+                       .createInstance(Ci.nsIFileOutputStream);
+        stream.init(filepicker.file, -1, -1, 0);
+
+        let serializer = new XMLSerializer();
+        let output = serializer.serializeToString(iframe.contentDocument);
+        output = Weave.Utils.encodeUTF8(output);
+        stream.write(output, output.length);
+      }
+      return false;
+    });
+  },
+
 
   /**
    * validatePassword / validatePassphrase
