58eb6a43d396c3ce6d0ba18e61033a71e12d8b96
L. David Baron <dbaron@dbaron.org>
2010-09-09 08:21:46 -0700

    Clip contents of elements with overflow != visible to the border radius.  (Bug 459144, patch 14)  r=roc  a2.0=blocking2.0:beta6

--- layout/forms/nsFileControlFrame.cpp
+++ layout/forms/nsFileControlFrame.cpp
@@ -672,7 +672,8 @@ nsFileControlFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
   // Clip height only
   nsRect clipRect(aBuilder->ToReferenceFrame(this), GetSize());
   clipRect.width = GetOverflowRect().XMost();
-  rv = OverflowClip(aBuilder, tempList, aLists, clipRect);
+  nscoord radii[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  rv = OverflowClip(aBuilder, tempList, aLists, clipRect, radii);
   NS_ENSURE_SUCCESS(rv, rv);
 
   // Disabled file controls don't pass mouse events to their children, so we
