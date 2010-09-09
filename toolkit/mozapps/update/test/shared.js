8ca0f8324132f7996eaa3b5fc9a999c9dccf5501
Robert Strong <robert.bugzilla@gmail.com>
2010-09-09 12:22:27 -0700

    Bug 546593 - If both a partial and complete update have a verification failure this._update is null, the UI breaks, and the user is not notified of the update failure. r=dtownsend, a=approval2.0 dtownsend

--- toolkit/mozapps/update/test/shared.js
+++ toolkit/mozapps/update/test/shared.js
@@ -54,6 +54,7 @@ const PREF_APP_UPDATE_ENABLED             = "app.update.enabled";
 const PREF_APP_UPDATE_IDLETIME            = "app.update.idletime";
 const PREF_APP_UPDATE_LOG                 = "app.update.log";
 const PREF_APP_UPDATE_NEVER_BRANCH        = "app.update.never.";
+const PREF_APP_UPDATE_PROMPTWAITTIME      = "app.update.promptWaitTime";
 const PREF_APP_UPDATE_SHOW_INSTALLED_UI   = "app.update.showInstalledUI";
 const PREF_APP_UPDATE_URL                 = "app.update.url";
 const PREF_APP_UPDATE_URL_DETAILS         = "app.update.url.details";
