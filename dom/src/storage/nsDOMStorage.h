50b7cf28b3925184b16501f546eac2111e5ebb04
Marco Bonardo <mbonardo@mozilla.com>
2010-09-09 12:20:05 +0200

    Bug 592990 - about:home DOM storage is cleared with cookies and private browsing. r=mayhemer a=blocking

--- dom/src/storage/nsDOMStorage.h
+++ dom/src/storage/nsDOMStorage.h
@@ -175,6 +175,10 @@ public:
     return mSessionOnly;
   }
 
+  // Some privileged internal pages can use a persistent storage even in
+  // session-only or private-browsing modes.
+  bool CanUseChromePersist();
+
   // Check whether storage may be used by the caller, and whether it
   // is session only.  Returns true if storage may be used.
   static PRBool
@@ -265,6 +269,8 @@ protected:
   friend class nsIDOMStorage2;
   nsPIDOMStorage* mSecurityChecker;
   nsPIDOMStorage* mEventBroadcaster;
+
+  bool mCanUseChromePersist;
 
 public:
   // e.g. "moc.rab.oof.:" or "moc.rab.oof.:http:80" depending
