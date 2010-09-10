594f4a01a5c2828f591ee44c66510339b0196e9b
Oleg Romashin <romaxa@gmail.com>
2010-09-10 11:28:52 -0700

    Bug 556487 - Plugin child/parent/PluginInstance API part. r=roc a=blocking2.0

--- modules/plugin/base/src/PluginPRLibrary.h
+++ modules/plugin/base/src/PluginPRLibrary.h
@@ -133,6 +133,10 @@ public:
                              char* argv[], NPSavedData* saved,
                              NPError* error);
 
+    virtual nsresult AsyncSetWindow(NPP instance, NPWindow* window);
+    virtual nsresult NotifyPainted(NPP instance);
+    virtual nsresult GetSurface(NPP instance, gfxASurface** aSurface);
+
 private:
     NP_InitializeFunc mNP_Initialize;
     NP_ShutdownFunc mNP_Shutdown;
