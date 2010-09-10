348eb47cda9d64b03da42d7d2bdbf9e75b4d1204
Paul O’Shannessy <paul@oshannessy.com>
2010-09-10 10:57:28 -0700

    Bug 588482 - Restore Session on-demand at startup [r=dao, r=dietrich, a=blocking-b6+]

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -780,6 +780,8 @@ pref("browser.sessionstore.postdata", 0);
 // on which sites to save text data, POSTDATA and cookies
 // 0 = everywhere, 1 = unencrypted sites, 2 = nowhere
 pref("browser.sessionstore.privacy_level", 1);
+// the same as browser.sessionstore.privacy_level, but for saving deferred session data
+pref("browser.sessionstore.privacy_level_deferred", 2);
 // how many tabs can be reopened (per window)
 pref("browser.sessionstore.max_tabs_undo", 10);
 // how many windows can be reopened (per session) - on non-OS X platforms this
