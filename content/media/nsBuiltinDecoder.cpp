2d53bb36909f728072b42a6be6df4da7ddf36298
Chris Double <chris.double@double.co.nz>
2010-09-10 17:49:26 +1200

    Bug 571822 - Fire timeupdate event less frequently than once per frame - r=kinetik a=blocking2.0

--- content/media/nsBuiltinDecoder.cpp
+++ content/media/nsBuiltinDecoder.cpp
@@ -145,6 +145,8 @@ void nsBuiltinDecoder::Shutdown()
 
   mShuttingDown = PR_TRUE;
 
+  StopTimeUpdate();
+
   // This changes the decoder state to SHUTDOWN and does other things
   // necessary to unblock the state machine thread if it's blocked, so
   // the asynchronous shutdown in nsDestroyStateMachine won't deadlock.
@@ -387,6 +389,8 @@ void nsBuiltinDecoder::MetadataLoaded(PRUint32 aChannels,
   if (resourceIsLoaded) {
     ResourceLoaded();
   }
+
+  StartTimeUpdate();
 }
 
 void nsBuiltinDecoder::ResourceLoaded()
@@ -773,7 +777,7 @@ void nsBuiltinDecoder::PlaybackPositionChanged()
   Invalidate();
 
   if (mElement && lastTime != mCurrentTime) {
-    mElement->DispatchEvent(NS_LITERAL_STRING("timeupdate"));
+    FireTimeUpdate();
   }
 }
 
