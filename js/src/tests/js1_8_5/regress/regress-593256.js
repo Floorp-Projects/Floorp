4fe2a107564bf6a7689280f43547cd7d9cb75205
Brendan Eich <brendan@mozilla.org>
2010-09-11 15:24:51 -0700

    Parenthesize lambda statement expressions to work in browser (where JSOPTION_ANONFUNFIX is set; followup for 593256).

--- js/src/tests/js1_8_5/regress/regress-593256.js
+++ js/src/tests/js1_8_5/regress/regress-593256.js
@@ -5,16 +5,16 @@
  */
 
 eval("\
-  function(){for(d in[0,Number]) {\
+  (function(){for(d in[0,Number]) {\
     __defineGetter__(\"\",function(){}),\
     [(__defineGetter__(\"x\",Math.pow))]\
-  }}\
+  }})\
 ")()
 delete gc
 eval("\
-  function() {\
+  (function() {\
     for(e in __defineSetter__(\"x\",function(){})){}\
-  }\
+  })\
 ")()
 delete gc
 
