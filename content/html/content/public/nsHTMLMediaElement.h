317e38a5c72dd8dd5e8fdb974135f45100792953
Marco Bonardo <mbonardo@mozilla.com>
2010-09-11 11:17:11 +0200

    Backed out changeset 96b74fec2915

--- content/html/content/public/nsHTMLMediaElement.h
+++ content/html/content/public/nsHTMLMediaElement.h
@@ -185,8 +185,10 @@ public:
   gfxASurface* GetPrintSurface() { return mPrintSurface; }
 
   // Dispatch events
-  nsresult DispatchEvent(const nsAString& aName);
-  nsresult DispatchAsyncEvent(const nsAString& aName);
+  nsresult DispatchSimpleEvent(const nsAString& aName);
+  nsresult DispatchProgressEvent(const nsAString& aName);
+  nsresult DispatchAsyncSimpleEvent(const nsAString& aName);
+  nsresult DispatchAsyncProgressEvent(const nsAString& aName);
   nsresult DispatchAudioAvailableEvent(float* aFrameBuffer,
                                        PRUint32 aFrameBufferLength,
                                        PRUint64 aTime);
