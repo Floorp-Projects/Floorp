b1bd94e39a52e40d9f73b7124f82017a177c269d
Brian Crowder <crowder@fiverocks.com>
2010-09-13 11:17:00 -0700

    Bug 589025 - Support content-disposition attribute in e10s external app handling, r=dwitte, blocking-fennec2.0b1=blassey
    Bug 589025 - Support content-disposition attribute in e10s external app handling, r=dwitte, blocking-fennec2.0b1=blassey

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -1241,6 +1241,7 @@ TabChildGlobal::GetPrincipal()
 PExternalHelperAppChild*
 TabChild::AllocPExternalHelperApp(const IPC::URI& uri,
                                   const nsCString& aMimeContentType,
+                                  const nsCString& aContentDisposition,
                                   const bool& aForceSave,
                                   const PRInt64& aContentLength)
 {
