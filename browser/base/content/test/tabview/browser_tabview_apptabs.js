be7c57f02d8b95ad20c133fe62d7fcdef7dc8765
Ian Gilman <ian@iangilman.com>
2010-09-13 11:45:02 +0800

    Bug 593871 - Panorama should do the right thing when tabs become pinned/unpinned [r=dietrich, a=blocking betaN]

--- browser/base/content/test/tabview/browser_tabview_apptabs.js
+++ browser/base/content/test/tabview/browser_tabview_apptabs.js
@@ -52,38 +52,77 @@ function onTabViewWindowLoaded() {
   // establish initial state
   is(contentWindow.GroupItems.groupItems.length, 1, "we start with one group (the default)"); 
   is(gBrowser.tabs.length, 1, "we start with one tab");
+  let originalTab = gBrowser.tabs[0];
   
-  // create an app tab
-  let appXulTab = gBrowser.loadOneTab("about:blank");
-  gBrowser.pinTab(appXulTab);
-  is(gBrowser.tabs.length, 2, "we now have two tabs");
-  
-  // Create a group 
+  // create a group 
   let box = new contentWindow.Rect(20, 20, 180, 180);
-  let groupItem = new contentWindow.GroupItem([], { bounds: box });
+  let groupItemOne = new contentWindow.GroupItem([], { bounds: box, title: "test1" });
   is(contentWindow.GroupItems.groupItems.length, 2, "we now have two groups");
+  contentWindow.GroupItems.setActiveGroupItem(groupItemOne);
   
+  // create a tab
+  let xulTab = gBrowser.loadOneTab("about:blank");
+  is(gBrowser.tabs.length, 2, "we now have two tabs");
+  is(groupItemOne._children.length, 1, "the new tab was added to the group");
+  
+  // make sure the group has no app tabs
+  let appTabIcons = groupItemOne.container.getElementsByClassName("appTabIcon");
+  is(appTabIcons.length, 0, "there are no app tab icons");
+  
+  // pin the tab, make sure the TabItem goes away and the icon comes on
+  gBrowser.pinTab(xulTab);
+
+  is(groupItemOne._children.length, 0, "the app tab's TabItem was removed from the group");
+
+  appTabIcons = groupItemOne.container.getElementsByClassName("appTabIcon");
+  is(appTabIcons.length, 1, "there's now one app tab icon");
+
+  // create a second group and make sure it gets the icon too
+  box.offset(box.width + 20, 0);
+  let groupItemTwo = new contentWindow.GroupItem([], { bounds: box, title: "test2" });
+  is(contentWindow.GroupItems.groupItems.length, 3, "we now have three groups");
+  appTabIcons = groupItemTwo.container.getElementsByClassName("appTabIcon");
+  is(appTabIcons.length, 1, "there's an app tab icon in the second group");
+  
+  // unpin the tab, make sure the icon goes away and the TabItem comes on
+  gBrowser.unpinTab(xulTab);
+
+  is(groupItemOne._children.length, 1, "the app tab's TabItem is back");
+
+  appTabIcons = groupItemOne.container.getElementsByClassName("appTabIcon");
+  is(appTabIcons.length, 0, "the icon is gone from group one");
+
+  appTabIcons = groupItemTwo.container.getElementsByClassName("appTabIcon");
+  is(appTabIcons.length, 0, "the icon is gone from group 2");
+  
+  // pin the tab again
+  gBrowser.pinTab(xulTab);
+
+  // close the second group
+  groupItemTwo.close();
+
   // find app tab in group and hit it
   let onTabViewHidden = function() {
     window.removeEventListener("tabviewhidden", onTabViewHidden, false);
     ok(!TabView.isVisible(), "Tab View is hidden because we clicked on the app tab");
     
     // clean up
-    gBrowser.unpinTab(appXulTab);
-    gBrowser.removeTab(appXulTab);
+    gBrowser.selectedTab = originalTab;
+    
+    gBrowser.unpinTab(xulTab);
+    gBrowser.removeTab(xulTab);
     is(gBrowser.tabs.length, 1, "we finish with one tab");
   
-    groupItem.close();
+    groupItemOne.close();
     is(contentWindow.GroupItems.groupItems.length, 1, "we finish with one group");
     
-    ok(!TabView.isVisible(), "Tab View is not visible");
+    ok(!TabView.isVisible(), "we finish with Tab View not visible");
     
     finish();
   };
 
   window.addEventListener("tabviewhidden", onTabViewHidden, false);
 
-  let appTabButtons = groupItem.$appTabTray[0].getElementsByTagName("img");
-  ok(appTabButtons.length == 1, "there is one app tab button");
-  EventUtils.sendMouseEvent({ type: "click" }, appTabButtons[0], contentWindow);
+  appTabIcons = groupItemOne.container.getElementsByClassName("appTabIcon");
+  EventUtils.sendMouseEvent({ type: "click" }, appTabIcons[0], contentWindow);
 }
