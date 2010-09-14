072f4a88f117c9fd9e06c672252c17e804eab0cd
David Anderson <danderson@mozilla.com>
2010-09-14 12:48:45 -0700

    Backed out changeset 3d11e994ad09

--- js/src/methodjit/FrameState.h
+++ js/src/methodjit/FrameState.h
@@ -659,9 +659,10 @@ class FrameState
     void shift(int32 n);
 
     /*
-     * Notifies the frame of a slot that can escape.
+     * Notifies the frame of a slot that can escape. Returns whether or not
+     * the slot was added.
      */
-    inline void setClosedVar(uint32 slot);
+    inline bool addEscaping(uint32 local);
 
     inline void setInTryBlock(bool inTryBlock) {
         this->inTryBlock = inTryBlock;
@@ -716,8 +717,6 @@ class FrameState
         return uint32(fe - entries);
     }
 
-    inline bool isClosedVar(uint32 slot);
-
   private:
     JSContext *cx;
     JSScript *script;
@@ -756,7 +755,7 @@ class FrameState
 
     mutable ImmutableSync reifier;
 
-    bool *closedVars;
+    uint32 *escaping;
     bool eval;
     bool inTryBlock;
 };
