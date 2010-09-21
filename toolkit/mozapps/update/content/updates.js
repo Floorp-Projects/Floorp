82141f62cb23329f9454c57be1a259c238138559
Robert Strong <robert.bugzilla@gmail.com>
2010-09-20 21:42:26 -0700

    Bug 597664 - Add finalURI.spec for the mar file download to the local update xml and log the finalURI.spec along with the original URI.spe. r=dtownsend, a=approval2.0

--- toolkit/mozapps/update/content/updates.js
+++ toolkit/mozapps/update/content/updates.js
@@ -1451,8 +1451,6 @@ var gDownloadingPage = {
    *          Additional data
    */
   onStartRequest: function(request, context) {
-    if (request instanceof CoI.nsIIncrementalDownload)
-      LOG("gDownloadingPage", "onStartRequest - spec: " + request.URI.spec);
     // This !paused test is necessary because onStartRequest may fire after
     // the download was paused (for those speedy clickers...)
     if (this._paused)
@@ -1475,8 +1473,6 @@ var gDownloadingPage = {
    *          The total number of bytes that must be transferred
    */
   onProgress: function(request, context, progress, maxProgress) {
-    LOG("gDownloadingPage", "onProgress - progress: " + progress + "/" +
-        maxProgress);
     let status = this._updateDownloadStatus(progress, maxProgress);
     var currentProgress = Math.round(100 * (progress / maxProgress));
 
@@ -1522,8 +1518,6 @@ var gDownloadingPage = {
    *          Human readable version of |status|
    */
   onStatus: function(request, context, status, statusText) {
-    LOG("gDownloadingPage", "onStatus - status: " + status + ", text: " +
-        statusText);
     this._setStatus(statusText);
   },
 
@@ -1537,10 +1531,6 @@ var gDownloadingPage = {
    *          Status code containing the reason for the cessation.
    */
   onStopRequest: function(request, context, status) {
-    if (request instanceof CoI.nsIIncrementalDownload)
-      LOG("gDownloadingPage", "onStopRequest - spec: " + request.URI.spec +
-          ", status: " + status);
-
     if (this._downloadProgress.mode != "normal")
       this._downloadProgress.mode = "normal";
 
