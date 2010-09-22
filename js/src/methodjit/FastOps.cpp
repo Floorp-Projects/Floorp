4d67cca75bb07626ee20acbb774de452dde5c752
David Anderson <danderson@mozilla.com>
2010-09-21 18:34:42 -0700

    Optimize FrameState for large linear scripts (bug 591836, r=dmandelin). * * * Remove FrameState::base (bug 591836 part 1, r=dmandelin). * * * New register pinning invariants (bug 591836 part 2, r=dmandelin). * * * Remove FrameState::tosFe() (bug 591836 part 3, r=dmandelin). * * * New copy order invariant (bug 591836 part 4, r=dmandelin). * * * Optimize immutable frame syncing (bug 591836 part 5, r=dmandelin). * * * Optimize frame merging (bug 581936 part 6, r=dmandelin). * * * Optimize copying frame entries (bug 591836 part 7, r=dmandelin). * * * Optimize mutable frame syncing (bug 591836 part 8, r=dmandelin). * * * Optimize syncing in try blocks (bug 591836 part 9, r=dmandelin).

--- js/src/methodjit/FastOps.cpp
+++ js/src/methodjit/FastOps.cpp
@@ -660,7 +660,7 @@ mjit::Compiler::jsop_equality(JSOp op, BoolStub stub, jsbytecode *target, JSOp f
          */
 
         if (target) {
-            frame.forgetEverything();
+            frame.syncAndForgetEverything();
 
             if ((op == JSOP_EQ && fused == JSOP_IFNE) ||
                 (op == JSOP_NE && fused == JSOP_IFEQ)) {
@@ -907,8 +907,7 @@ mjit::Compiler::booleanJumpScript(JSOp op, jsbytecode *target)
         type.setReg(frame.copyTypeIntoReg(fe));
     data.setReg(frame.copyDataIntoReg(fe));
 
-    /* :FIXME: Can something more lightweight be used? */
-    frame.forgetEverything();
+    frame.syncAndForgetEverything();
 
     Assembler::Condition cond = (op == JSOP_IFNE || op == JSOP_OR)
                                 ? Assembler::NonZero
@@ -995,7 +994,7 @@ mjit::Compiler::jsop_ifneq(JSOp op, jsbytecode *target)
         if (op == JSOP_IFEQ)
             b = !b;
         if (b) {
-            frame.forgetEverything();
+            frame.syncAndForgetEverything();
             jumpAndTrace(masm.jump(), target);
         }
         return;
@@ -1015,7 +1014,7 @@ mjit::Compiler::jsop_andor(JSOp op, jsbytecode *target)
         /* Short-circuit. */
         if ((op == JSOP_OR && b == JS_TRUE) ||
             (op == JSOP_AND && b == JS_FALSE)) {
-            frame.forgetEverything();
+            frame.syncAndForgetEverything();
             jumpAndTrace(masm.jump(), target);
         }
 
