d103effb3f6b43bf68b5154150f196693d2e5019
John Daggett <jdaggett@mozilla.com>
2010-09-22 10:15:57 +0100

    bug 593296 - avoid potential read beyond end of kern-pair list. r=jfkthame a=blocking-beta7+

--- gfx/thebes/gfxHarfBuzzShaper.cpp
+++ gfx/thebes/gfxHarfBuzzShaper.cpp
@@ -284,6 +284,7 @@ GetKernValueFmt0(const void* aSubtable,
 
     const KernPair *lo = reinterpret_cast<const KernPair*>(hdr + 1);
     const KernPair *hi = lo + PRUint16(hdr->nPairs);
+    const KernPair *limit = hi;
 
     if (reinterpret_cast<const char*>(aSubtable) + aSubtableLen <
         reinterpret_cast<const char*>(hi)) {
@@ -304,7 +305,7 @@ GetKernValueFmt0(const void* aSubtable,
         }
     }
 
-    if (KERN_PAIR_KEY(lo->left, lo->right) == key) {
+    if (lo < limit && KERN_PAIR_KEY(lo->left, lo->right) == key) {
         if (aIsOverride) {
             aValue = PRInt16(lo->value);
         } else if (aIsMinimum) {
