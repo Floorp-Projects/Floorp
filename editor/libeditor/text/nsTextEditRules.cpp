f4b7bdf0a1fa04e042cd571961dec101dcd5889c
Ehsan Akhgari <ehsan@mozilla.com>
2010-09-24 15:10:53 -0400

    Bug 597519 - Attempting to add characters to a textarea filled to its maxlength with a trailing newline results in the caret to jump to the previous line; r=roc a=blocking-final+

--- editor/libeditor/text/nsTextEditRules.cpp
+++ editor/libeditor/text/nsTextEditRules.cpp
@@ -658,8 +658,14 @@ nsTextEditRules::WillInsertText(PRInt32          aAction,
 
   // handle docs with a max length
   // NOTE, this function copies inString into outString for us.
-  nsresult res = TruncateInsertionIfNeeded(aSelection, inString, outString, aMaxLength, nsnull);
+  PRBool truncated = PR_FALSE;
+  nsresult res = TruncateInsertionIfNeeded(aSelection, inString, outString,
+                                           aMaxLength, &truncated);
   NS_ENSURE_SUCCESS(res, res);
+  if (truncated) {
+    *aCancel = PR_TRUE;
+    return NS_OK;
+  }
   
   PRUint32 start = 0;
   PRUint32 end = 0;  
