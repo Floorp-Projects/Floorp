0ff954f4f99ccc744e5cd67bc2f4932dbbf56464
Doug Turner <dougt@dougt.org>
2010-09-27 14:23:35 -0700

    Bug 589905 - Cache remote-preferences to improve performance. r=dwitte. a=blocking-fennec

--- modules/libpref/src/prefapi_private_data.h
+++ modules/libpref/src/prefapi_private_data.h
@@ -40,7 +40,7 @@
 extern PLDHashTable			gHashTable;
 extern PRBool               gDirty;
 
-enum pref_SaveTypes { SAVE_NONSHARED, SAVE_SHARED, SAVE_ALL };
+enum pref_SaveTypes { SAVE_NONSHARED, SAVE_SHARED, SAVE_ALL, SAVE_ALL_AND_DEFAULTS };
 
 // Passed as the arg to pref_savePref
 struct pref_saveArgs {
