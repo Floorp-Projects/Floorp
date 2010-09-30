73ca7fbe874a6c265df1d2f4089660412ddc8c6e
Andreas Gal <gal@mozilla.com>
2010-09-29 20:38:03 -0700

    Back out bug 584789.

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -4516,7 +4516,6 @@ JS_NewScriptObject(JSContext *cx, JSScript *script)
      * described in the comment for JSScript::u.object.
      */
     JS_ASSERT(script->u.object);
-    JS_ASSERT(script != JSScript::emptyScript());
     return script->u.object;
 }
 
