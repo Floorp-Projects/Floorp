b5184b44e2b280dbf0ef22917295eab454044648
Phil Ringnalda <philringnalda@gmail.com>
2010-10-01 19:36:58 -0700

    Bug 385845 - Remove bogus assertion that mDocElement is in mDocument during XML parsing, since it was never a requirement and we've long since dropped doing what caused us to care, r=bz, a=orange

--- content/xml/document/src/nsXMLContentSink.cpp
+++ content/xml/document/src/nsXMLContentSink.cpp
@@ -332,15 +332,6 @@ nsXMLContentSink::DidBuildModel(PRBool aTerminated)
     // Kick off layout for non-XSLT transformed documents.
     mDocument->ScriptLoader()->RemoveObserver(this);
 
-    if (mDocElement) {
-      // Notify document observers that all the content has been stuck
-      // into the document.
-      // XXX do we need to notify for things like PIs?  Or just the
-      // documentElement?
-      NS_ASSERTION(mDocument->IndexOf(mDocElement) != -1,
-                   "mDocElement not in doc?");
-    }
-
     // Check if we want to prettyprint
     MaybePrettyPrint();
 
