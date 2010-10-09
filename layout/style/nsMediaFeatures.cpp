55b9f24b32b790b3ef09f456e241b25342889f01
Jim Mathies <jmathies@mozilla.com>
2010-10-09 15:53:45 -0500

    Bug 543910 - Layout support for -moz-windows-theme. r=dbaron, a=final.

--- layout/style/nsMediaFeatures.cpp
+++ layout/style/nsMediaFeatures.cpp
@@ -46,6 +46,7 @@
 #include "nsCSSValue.h"
 #include "nsIDocShell.h"
 #include "nsLayoutUtils.h"
+#include "nsILookAndFeel.h"
 #include "nsCSSRuleProcessor.h"
 
 static const PRInt32 kOrientationKeywords[] = {
@@ -59,6 +60,24 @@ static const PRInt32 kScanKeywords[] = {
   eCSSKeyword_interlace,                NS_STYLE_SCAN_INTERLACE,
   eCSSKeyword_UNKNOWN,                  -1
 };
+
+#ifdef XP_WIN
+struct WindowsThemeName {
+    nsILookAndFeel::WindowsThemeIdentifier id;
+    const wchar_t* name;
+};
+
+// Windows theme identities used in the -moz-windows-theme media query.
+const WindowsThemeName themeStrings[] = {
+    { nsILookAndFeel::eWindowsTheme_Aero,       L"aero" },
+    { nsILookAndFeel::eWindowsTheme_LunaBlue,   L"luna-blue" },
+    { nsILookAndFeel::eWindowsTheme_LunaOlive,  L"luna-olive" },
+    { nsILookAndFeel::eWindowsTheme_LunaSilver, L"luna-silver" },
+    { nsILookAndFeel::eWindowsTheme_Royale,     L"royale" },
+    { nsILookAndFeel::eWindowsTheme_Zune,       L"zune" },
+    { nsILookAndFeel::eWindowsTheme_Generic,    L"generic" }
+};
+#endif
 
 // A helper for four features below
 static nsSize
@@ -280,6 +299,31 @@ GetSystemMetric(nsPresContext* aPresContext, const nsMediaFeature* aFeature,
     return NS_OK;
 }
 
+static nsresult
+GetWindowsTheme(nsPresContext* aPresContext, const nsMediaFeature* aFeature,
+                nsCSSValue& aResult)
+{
+    aResult.Reset();
+#ifdef XP_WIN
+    PRUint8 windowsThemeId =
+        nsCSSRuleProcessor::GetWindowsThemeIdentifier();
+
+    // Classic mode should fail to match.
+    if (windowsThemeId == nsILookAndFeel::eWindowsTheme_Classic)
+        return NS_OK;
+
+    // Look up the appropriate theme string
+    for (size_t i = 0; i < NS_ARRAY_LENGTH(themeStrings); ++i) {
+        if (windowsThemeId == themeStrings[i].id) {
+            aResult.SetStringValue(nsDependentString(themeStrings[i].name),
+                                   eCSSUnit_Ident);
+            break;
+        }
+    }
+#endif
+    return NS_OK;
+}
+
 /*
  * Adding new media features requires (1) adding the new feature to this
  * array, with appropriate entries (and potentially any new code needed
@@ -489,7 +533,13 @@ nsMediaFeatures::features[] = {
         { &nsGkAtoms::menubar_drag },
         GetSystemMetric
     },
-
+    {
+        &nsGkAtoms::_moz_windows_theme,
+        nsMediaFeature::eMinMaxNotAllowed,
+        nsMediaFeature::eIdent,
+        { nsnull },
+        GetWindowsTheme
+    },
     // Null-mName terminator:
     {
         nsnull,
