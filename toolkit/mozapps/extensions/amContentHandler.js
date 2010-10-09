9e867d723026e82758cca5f9d3efb31e07d53b99
Wes Johnston <wjohnston@mozilla.com>
2010-10-09 09:27:00 -0400

    Bug 593025 - Use InstallTrigger for xpi links. r=mossop a=blocking-fennec

--- toolkit/mozapps/extensions/amContentHandler.js
+++ toolkit/mozapps/extensions/amContentHandler.js
@@ -68,12 +68,6 @@ amContentHandler.prototype = {
 
     let uri = aRequest.URI;
 
-    let referer = null;
-    if (aRequest instanceof Ci.nsIPropertyBag2) {
-      referer = aRequest.getPropertyAsInterface("docshell.internalReferrer",
-                                                Ci.nsIURI);
-    }
-
     let window = null;
     let callbacks = aRequest.notificationCallbacks ?
                     aRequest.notificationCallbacks :
@@ -83,14 +77,44 @@ amContentHandler.prototype = {
 
     aRequest.cancel(Cr.NS_BINDING_ABORTED);
 
-    let manager = Cc["@mozilla.org/addons/integration;1"].
-                  getService(Ci.amIWebInstaller);
-    manager.installAddonsFromWebpage(aMimetype, window, referer, [uri.spec],
-                                     [null], [null], [null], null, 1);
+    let appinfo = Cc["@mozilla.org/xre/app-info;1"].
+                  getService(Ci.nsIXULRuntime);
+    if (appinfo.processType == Ci.nsIXULRuntime.PROCESS_TYPE_CONTENT) {
+      try {
+        if (!window.InstallTrigger) 
+          window = window.wrappedJSObject;
+  
+        if (window.InstallTrigger)
+          window.InstallTrigger.startSoftwareUpdate(uri.spec);
+        else
+          this.log("Window does not have an InstallTrigger");
+      } catch(ex) {
+        this.log(ex);
+      }
+    }
+    else {
+      let referer = null;
+      if (aRequest instanceof Ci.nsIPropertyBag2) {
+        referer = aRequest.getPropertyAsInterface("docshell.internalReferrer",
+                                                Ci.nsIURI);
+      }
+
+      let manager = Cc["@mozilla.org/addons/integration;1"].
+                    getService(Ci.amIWebInstaller);
+      manager.installAddonsFromWebpage(aMimetype, window, referer, [uri.spec],
+                                       [null], [null], [null], null, 1);
+    }
   },
 
   classID: Components.ID("{7beb3ba8-6ec3-41b4-b67c-da89b8518922}"),
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentHandler])
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentHandler]),
+
+  log : function(aMsg) {
+    let msg = "amContentHandler.js: " + (aMsg.join ? aMsg.join("") : aMsg);
+    Cc["@mozilla.org/consoleservice;1"].getService(Ci.nsIConsoleService).
+      logStringMessage(msg);
+    dump(msg + "\n");
+  }
 };
 
 var NSGetFactory = XPCOMUtils.generateNSGetFactory([amContentHandler]);
