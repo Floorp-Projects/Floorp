e3db97114a96c8f8be25ecfbb0ef392c7d8c85ac
Mihai Sucan <msucan@mozilla.com>
2010-10-11 14:37:34 -0700

    Bug 594477 - Console clickable items pick up events that they shouldn't r=sdwilsh a=final+

---
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_bug_594477_clickable_output.js
@@ -0,0 +1,162 @@
+/* vim:set ts=2 sw=2 sts=2 et: */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * Contributor(s):
+ *  Mihai Șucan <mihai.sucan@gmail.com>
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+const TEST_URI = "http://example.com/browser/toolkit/components/console/hudservice/tests/browser/test-console.html";
+
+let outputItem;
+
+function tabLoad1(aEvent) {
+  browser.removeEventListener(aEvent.type, arguments.callee, true);
+
+  openConsole();
+
+  let hudId = HUDService.getHudIdByWindow(content);
+  HUD = HUDService.hudWeakReferences[hudId].get();
+
+  let display = HUDService.getOutputNodeById(hudId);
+  outputNode = display.querySelector(".hud-output-node");
+
+  browser.addEventListener("load", tabLoad2, true);
+
+  // Reload so we get some output in the console.
+  content.location.reload();
+}
+
+function tabLoad2(aEvent) {
+  browser.removeEventListener(aEvent.type, arguments.callee, true);
+
+  outputItem = outputNode.querySelector(".hud-network");
+  ok(outputItem, "found a network message");
+
+  document.addEventListener("popupshown", networkPanelShown, false);
+
+  // Send the mousedown and click events such that the network panel opens.
+  EventUtils.sendMouseEvent({type: "mousedown"}, outputItem);
+  EventUtils.sendMouseEvent({type: "click"}, outputItem);
+}
+
+function networkPanelShown(aEvent) {
+  document.removeEventListener(aEvent.type, arguments.callee, false);
+
+  document.addEventListener("popupshown", networkPanelShowFailure, false);
+
+  // The network panel should not open for the second time.
+  EventUtils.sendMouseEvent({type: "mousedown"}, outputItem);
+  EventUtils.sendMouseEvent({type: "click"}, outputItem);
+
+  executeSoon(function() {
+    aEvent.target.addEventListener("popuphidden", networkPanelHidden, false);
+    aEvent.target.hidePopup();
+  });
+}
+
+function networkPanelShowFailure(aEvent) {
+  document.removeEventListener(aEvent.type, arguments.callee, false);
+
+  ok(false, "the network panel should not show");
+}
+
+function networkPanelHidden(aEvent) {
+  this.removeEventListener(aEvent.type, arguments.callee, false);
+
+  // The network panel should not show because this is a mouse event that starts
+  // in a position and ends in another.
+  EventUtils.sendMouseEvent({type: "mousedown", clientX: 3, clientY: 4},
+    outputItem);
+  EventUtils.sendMouseEvent({type: "click", clientX: 5, clientY: 6},
+    outputItem);
+
+  // The network panel should not show because this is a middle-click.
+  EventUtils.sendMouseEvent({type: "mousedown", button: 1},
+    outputItem);
+  EventUtils.sendMouseEvent({type: "click", button: 1},
+    outputItem);
+
+  // The network panel should not show because this is a right-click.
+  EventUtils.sendMouseEvent({type: "mousedown", button: 2},
+    outputItem);
+  EventUtils.sendMouseEvent({type: "click", button: 2},
+    outputItem);
+
+  executeSoon(function() {
+    document.removeEventListener("popupshown", networkPanelShowFailure, false);
+
+    // Done with the network output. Now test the jsterm output and the property
+    // panel.
+    HUD.jsterm.setInputValue("document");
+    HUD.jsterm.execute();
+
+    outputItem = outputNode.querySelector(".jsterm-output-line");
+    ok(outputItem, "found a jsterm output message");
+
+    document.addEventListener("popupshown", properyPanelShown, false);
+
+    // Send the mousedown and click events such that the property panel opens.
+    EventUtils.sendMouseEvent({type: "mousedown"}, outputItem);
+    EventUtils.sendMouseEvent({type: "click"}, outputItem);
+  });
+}
+
+function properyPanelShown(aEvent) {
+  document.removeEventListener(aEvent.type, arguments.callee, false);
+
+  document.addEventListener("popupshown", propertyPanelShowFailure, false);
+
+  // The property panel should not open for the second time.
+  EventUtils.sendMouseEvent({type: "mousedown"}, outputItem);
+  EventUtils.sendMouseEvent({type: "click"}, outputItem);
+
+  executeSoon(function() {
+    aEvent.target.addEventListener("popuphidden", propertyPanelHidden, false);
+    let closeButton = aEvent.target.querySelector(".jsPropertyPanelCloseButton");
+    closeButton.doCommand();
+  });
+}
+
+function propertyPanelShowFailure(aEvent) {
+  document.removeEventListener(aEvent.type, arguments.callee, false);
+
+  ok(false, "the property panel should not show");
+}
+
+function propertyPanelHidden(aEvent) {
+  this.removeEventListener(aEvent.type, arguments.callee, false);
+
+  // The property panel should not show because this is a mouse event that
+  // starts in a position and ends in another.
+  EventUtils.sendMouseEvent({type: "mousedown", clientX: 3, clientY: 4},
+    outputItem);
+  EventUtils.sendMouseEvent({type: "click", clientX: 5, clientY: 6},
+    outputItem);
+
+  // The property panel should not show because this is a middle-click.
+  EventUtils.sendMouseEvent({type: "mousedown", button: 1},
+    outputItem);
+  EventUtils.sendMouseEvent({type: "click", button: 1},
+    outputItem);
+
+  // The property panel should not show because this is a right-click.
+  EventUtils.sendMouseEvent({type: "mousedown", button: 2},
+    outputItem);
+  EventUtils.sendMouseEvent({type: "click", button: 2},
+    outputItem);
+
+  executeSoon(function() {
+    document.removeEventListener("popupshown", propertyPanelShowFailure, false);
+    outputItem = null;
+    finishTest();
+  });
+}
+
+function test() {
+  addTab(TEST_URI);
+  browser.addEventListener("load", tabLoad1, true);
+}
+
