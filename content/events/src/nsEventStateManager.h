a15e412c374c1dbda6e81b8021483b86818d5203
Olli Pettay <Olli.Pettay@helsinki.fi>
2010-10-12 12:55:31 +0300

    Backout Bug 508518,  a=regression?

--- content/events/src/nsEventStateManager.h
+++ content/events/src/nsEventStateManager.h
@@ -396,9 +396,6 @@ protected:
 
   PRPackedBool m_haveShutdown;
 
-
-public:
-  static nsresult UpdateUserActivityTimer(void);
   // Array for accesskey support
   nsCOMArray<nsIContent> mAccessKeys;
 
