0ce588ae814a9119b2e70e6752bb877674004309
Olli Pettay <Olli.Pettay@helsinki.fi>
2010-10-12 11:09:06 +0300

    Bug 508518, Implement nsUITimerCallback with one-shot timer, p=romaxa+me, r=jst, a=blocking-fennec

--- content/events/src/nsEventStateManager.h
+++ content/events/src/nsEventStateManager.h
@@ -396,6 +396,9 @@ protected:
 
   PRPackedBool m_haveShutdown;
 
+
+public:
+  static nsresult UpdateUserActivityTimer(void);
   // Array for accesskey support
   nsCOMArray<nsIContent> mAccessKeys;
 
