219151e86ea821d840a074b75445916dfa130e5f
Chris Jones <jones.chris.g@gmail.com>
2010-10-13 18:36:44 -0500

    Fix --disable-libxul bustage from bug 602431 and bug 602428. a=bustage

--- gfx/layers/Layers.cpp
+++ gfx/layers/Layers.cpp
@@ -477,7 +477,10 @@ PrintInfo(nsACString& aTo, ShadowLayer* aShadowLayer)
   return aTo;
 }
 # else
-static nsACString& PrintInfo(nsACString& aTo, ShadowLayer* aShadowLayer) {}
+static nsACString& PrintInfo(nsACString& aTo, ShadowLayer* aShadowLayer)
+{
+  return aTo;
+}
 # endif  // MOZ_IPC
 
 #else  // !MOZ_LAYERS_HAVE_LOG
