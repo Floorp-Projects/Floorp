afc66df206c7cb5b479a74ec6e5e21d4a9f0f46e
Dietrich Ayala <dietrich@mozilla.com>
2010-10-14 19:52:05 +0700

    Backout bug 567306 (r=test bustage, a=blocking)

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -115,6 +115,7 @@ var gFindBarInitialized = false;
 XPCOMUtils.defineLazyGetter(window, "gFindBar", function() {
   let XULNS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
   let findbar = document.createElementNS(XULNS, "findbar");
+  findbar.setAttribute("browserid", "content");
   findbar.id = "FindToolbar";
 
   let browserBottomBox = document.getElementById("browser-bottombox");
@@ -122,7 +123,6 @@ XPCOMUtils.defineLazyGetter(window, "gFindBar", function() {
 
   // Force a style flush to ensure that our binding is attached.
   findbar.clientTop;
-  findbar.browser = gBrowser;
   window.gFindBarInitialized = true;
   return findbar;
 });
