878792189b9d88677096465a7fe0622282d16ac9
Jeff Walden <jwalden@mit.edu>
2010-10-14 14:20:59 -0700

    Revert 782a44082ed2, which was a somewhat-too-optimistic removal of code that's not dead yet (and is not getting better).  r=orange

--- js/src/jspropertycacheinlines.h
+++ js/src/jspropertycacheinlines.h
@@ -115,12 +115,16 @@ PropertyCache::testForSet(JSContext *cx, jsbytecode *pc, JSObject *obj,
     if (entry->kpc == pc && entry->kshape == shape && CX_OWNS_OBJECT_TITLE(cx, obj))
         return true;
 
+#ifdef DEBUG
+    JSObject *orig = obj;
+#endif
     JSAtom *atom = fullTest(cx, pc, &obj, obj2p, entry);
-    JS_ASSERT(atom);
-
-    PCMETER(misses++);
-    PCMETER(setmisses++);
-
+    if (atom) {
+        PCMETER(misses++);
+        PCMETER(setmisses++);
+    } else {
+        JS_ASSERT(obj == orig);
+    }
     *atomp = atom;
     return false;
 }
