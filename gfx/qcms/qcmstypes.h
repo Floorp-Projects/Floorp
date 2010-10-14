97a46decff383a7a7141c30ef1b86129407c432f
Jacek Caban <jacek@codeweavers.com>
2010-10-14 21:00:22 +0000

    Bug 594769 - qcms fails to compile on mingw r=jmuizelaar a=joe

--- gfx/qcms/qcmstypes.h
+++ gfx/qcms/qcmstypes.h
@@ -25,9 +25,7 @@ typedef PRUint64 uint64_t;
 #ifdef __OS2__
 /* OS/2's stdlib typdefs uintptr_t. So we'll just include that so we don't collide */
 #include <stdlib.h>
-#elif defined(__FreeBSD__)
-/* FreeBSD typedefs uintptr_t in /usr/include/sys/types.h */
-#else
+#elif !defined(__intptr_t_defined) && !defined(_UINTPTR_T_DEFINED)
 typedef PRUptrdiff uintptr_t;
 #endif
 #endif
