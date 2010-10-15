2223192523a5cd717b3f3e8851abd6a01badf8df
Ms2ger <ms2ger@gmail.com>
2010-10-15 17:23:06 +0200

    Bug 598316 - Fix a comparison between signed and unsigned integers in nsHTMLFieldSetElement::InsertChildAt; r=volkmar a=jst

--- content/html/content/src/nsHTMLFieldSetElement.cpp
+++ content/html/content/src/nsHTMLFieldSetElement.cpp
@@ -193,7 +193,7 @@ nsHTMLFieldSetElement::InsertChildAt(nsIContent* aChild, PRUint32 aIndex,
     } else {
       // If mFirstLegend is before aIndex, we do not change it.
       // Otherwise, mFirstLegend is now aChild.
-      if (aIndex <= IndexOf(mFirstLegend)) {
+      if (PRInt32(aIndex) <= IndexOf(mFirstLegend)) {
         mFirstLegend = aChild;
         firstLegendHasChanged = true;
       }
