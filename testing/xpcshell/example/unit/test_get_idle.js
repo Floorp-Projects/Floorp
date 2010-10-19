5792a53b377498ccf9f3f4172a38c3b761e4ceca
Marco Bonardo <mbonardo@mozilla.com>
2010-10-19 11:18:40 +0200

    Bug 602871 - XPCShell test harness should ignore system idle time while running tests, r=ted a=tests-only

---
+++ testing/xpcshell/example/unit/test_get_idle.js
@@ -0,0 +1,23 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+function run_test() {
+  print("Init the fake idle service and check its identity.");
+  let fakeIdleService = Components.classes["@mozilla.org/widget/idleservice;1"].
+                        getService(Components.interfaces.nsIIdleService);
+  try {
+    fakeIdleService.QueryInterface(Components.interfaces.nsIFactory);
+  } catch (ex) {
+    do_throw("The fake idle service implements nsIFactory.");
+  }
+  // We need at least one PASS, thus sanity check the idle time.
+  do_check_eq(fakeIdleService.idleTime, 0);
+
+  print("Init the real idle service and check its identity.");
+  let realIdleService = do_get_idle();
+  try {
+    realIdleService.QueryInterface(Components.interfaces.nsIFactory);
+    do_throw("The real idle service does not implement nsIFactory.");
+  } catch (ex) {}
+}
