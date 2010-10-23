309819dad71cd9daeaec189069334d330dbe9c4c
Steve Fink <sfink@mozilla.com>
2010-10-22 17:18:47 -0700

    Bug 606629 - Record op execution counts for mjitted code (r=dmandelin) I hope to obsolete this soon with bug 637393.

--- js/src/methodjit/Logging.cpp
+++ js/src/methodjit/Logging.cpp
@@ -79,6 +79,7 @@ js::JMCheckLogging()
             "  scripts       ???\n"
             "  profile       ???\n"
 #ifdef DEBUG
+            "  pcprofile     Runtime hit counts of every JS opcode executed\n"
             "  jsops         JS opcodes\n"
 #endif
             "  insns         JS opcodes and generated insns\n"
@@ -99,6 +100,8 @@ js::JMCheckLogging()
     if (strstr(env, "profile"))
         LoggingBits |= (1 << uint32(JSpew_Prof));
 #ifdef DEBUG
+    if (strstr(env, "pcprofile"))
+        LoggingBits |= (1 << uint32(JSpew_PCProf));
     if (strstr(env, "jsops"))
         LoggingBits |= (1 << uint32(JSpew_JSOps));
 #endif
