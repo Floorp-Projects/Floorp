ed8adad800dd1ebccf3759bb63db05ed80c66ea9
Dão Gottwald <dao@mozilla.com>
2010-10-25 09:59:42 +0200

    Bug 606567 - Intermittent failure in browser/base/content/test/browser_bug585785.js | tab successfully removed. r=gavin a=...

--- browser/base/content/test/browser_bug585785.js
+++ browser/base/content/test/browser_bug585785.js
@@ -1,7 +1,16 @@
 function test() {
   waitForExplicitFinish();
+
   var tab = gBrowser.addTab();
-  executeSoon(function () {
+  isnot(tab.getAttribute("fadein"), "true", "newly opened tab is yet to fade in");
+
+  // Remove the tab right before the opening animation's first frame
+  window.mozRequestAnimationFrame(function () {
+    if (tab.getAttribute("fadein") != "true") {
+      window.mozRequestAnimationFrame(arguments.callee);
+      return;
+    }
+
     info(window.getComputedStyle(tab).maxWidth);
     gBrowser.removeTab(tab, {animate:true});
     ok(!tab.parentNode, "tab successfully removed");
