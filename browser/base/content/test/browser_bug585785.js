9460288afa6a55ada491e567c5b629869e3baa77
Dão Gottwald <dao@mozilla.com>
2010-10-25 12:06:45 +0200

    Bug 606567 - go back to using executeSoon in addition to mozRequestAnimationFrame. a=...

--- browser/base/content/test/browser_bug585785.js
+++ browser/base/content/test/browser_bug585785.js
@@ -1,19 +1,31 @@
+var tab;
+var performedTest = false;
+
 function test() {
   waitForExplicitFinish();
 
-  var tab = gBrowser.addTab();
+  tab = gBrowser.addTab();
   isnot(tab.getAttribute("fadein"), "true", "newly opened tab is yet to fade in");
 
   // Remove the tab right before the opening animation's first frame
-  window.mozRequestAnimationFrame(function () {
-    if (tab.getAttribute("fadein") != "true") {
-      window.mozRequestAnimationFrame(arguments.callee);
-      return;
-    }
+  window.mozRequestAnimationFrame(checkAnimationState);
+  executeSoon(checkAnimationState);
+}
 
-    info(window.getComputedStyle(tab).maxWidth);
-    gBrowser.removeTab(tab, {animate:true});
-    ok(!tab.parentNode, "tab successfully removed");
-    finish();
-  });
+function checkAnimationState() {
+  if (performedTest)
+    return;
+
+  if (tab.getAttribute("fadein") != "true") {
+    window.mozRequestAnimationFrame(checkAnimationState);
+    executeSoon(checkAnimationState);
+    return;
+  }
+
+  performedTest = true;
+
+  info(window.getComputedStyle(tab).maxWidth);
+  gBrowser.removeTab(tab, {animate:true});
+  ok(!tab.parentNode, "tab successfully removed");
+  finish();
 }
