aa24fb8734def72d3fe016dc73332e43fa614684
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2010-10-27 08:15:08 +0300

    Bug 601914 - XPCOM does not work with ARM hardfp ABI. r=Jacob.Bramley a=blocking-fennec

--- js/src/jit-test/tests/jaeger/bug563000/trap-force-return-1.js
+++
@@ -1,7 +0,0 @@
-setDebug(true);
-function main() {
-  return "failure";
-}
-/* JSOP_RETURN in main. */
-trap(main, 3, "'success'");
-assertEq(main(), "success");
--- js/src/jit-test/tests/jaeger/bug585341.js
+++
@@ -1,6 +0,0 @@
-__defineGetter__("x", Float64Array)
-Function("\
-  with(this) {\
-    eval(\"x\")\
-  }\
-")()
