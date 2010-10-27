be04eed28c20a35caaa143904295d95058f191df
David Mandelin <dmandelin@mozilla.com>
2010-10-26 18:14:25 -0700

    Backed out changeset 8dd996c91f40 -- it doesn't fix the problem, but does make it harder to analyze.

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -915,8 +915,6 @@ JS_StringToVersion(const char *string);
                                                    embedding. */
 
 #define JSOPTION_METHODJIT      JS_BIT(14)      /* Whole-method JIT. */
-#define JSOPTION_ROPES          JS_BIT(16)      /* Enable rope optimization for
-                                                 * string concat. */
 
 extern JS_PUBLIC_API(uint32)
 JS_GetOptions(JSContext *cx);
