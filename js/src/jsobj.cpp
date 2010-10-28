c3ca3d4883365cb4b5639bfc36fc58c234275236
Brian Hackett <bhackett1024@gmail.com>
2010-10-28 14:33:32 -0700

    Special case object lookup lambda in String.replace, bug 605317. r=jorendorff

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -4459,14 +4459,6 @@ error: // TRACE_2 jumps here on error.
     return false;
 }
 
-JS_FRIEND_API(JSBool)
-js_LookupProperty(JSContext *cx, JSObject *obj, jsid id, JSObject **objp,
-                  JSProperty **propp)
-{
-    return js_LookupPropertyWithFlags(cx, obj, id, cx->resolveFlags,
-                                      objp, propp) >= 0;
-}
-
 #define SCOPE_DEPTH_ACCUM(bs,val)                                             \
     JS_SCOPE_DEPTH_METERING(JS_BASIC_STATS_ACCUM(bs, val))
 
@@ -4587,8 +4579,8 @@ static JS_ALWAYS_INLINE int
 js_LookupPropertyWithFlagsInline(JSContext *cx, JSObject *obj, jsid id, uintN flags,
                                  JSObject **objp, JSProperty **propp)
 {
-    /* Convert string indices to integers if appropriate. */
-    id = js_CheckForStringIndex(id);
+    /* We should not get string indices which aren't already integers here. */
+    JS_ASSERT(id == js_CheckForStringIndex(id));
 
     /* Search scopes starting with obj and following the prototype link. */
     JSObject *start = obj;
@@ -4636,10 +4628,23 @@ js_LookupPropertyWithFlagsInline(JSContext *cx, JSObject *obj, jsid id, uintN fl
     return protoIndex;
 }
 
+JS_FRIEND_API(JSBool)
+js_LookupProperty(JSContext *cx, JSObject *obj, jsid id, JSObject **objp,
+                  JSProperty **propp)
+{
+    /* Convert string indices to integers if appropriate. */
+    id = js_CheckForStringIndex(id);
+
+    return js_LookupPropertyWithFlagsInline(cx, obj, id, cx->resolveFlags, objp, propp) >= 0;
+}
+
 int
 js_LookupPropertyWithFlags(JSContext *cx, JSObject *obj, jsid id, uintN flags,
                            JSObject **objp, JSProperty **propp)
 {
+    /* Convert string indices to integers if appropriate. */
+    id = js_CheckForStringIndex(id);
+
     return js_LookupPropertyWithFlagsInline(cx, obj, id, flags, objp, propp);
 }
 
