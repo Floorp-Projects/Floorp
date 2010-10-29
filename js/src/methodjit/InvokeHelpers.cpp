992a462636741a2b0bb0d21f73f9057f7480c564
David Mandelin <dmandelin@mozilla.com>
2010-10-29 16:08:47 -0700

    Backed out changeset 394e0bd325b1: tinderbox bustage

--- js/src/methodjit/InvokeHelpers.cpp
+++ js/src/methodjit/InvokeHelpers.cpp
@@ -515,7 +515,7 @@ js_InternalThrow(VMFrame &f)
             cx->throwing = JS_FALSE;
             cx->fp()->setReturnValue(rval);
             return JS_FUNC_TO_DATA_PTR(void *,
-                   cx->jaegerCompartment()->forceReturnTrampoline());
+                   JS_METHODJIT_DATA(cx).trampolines.forceReturn);
 
           case JSTRAP_THROW:
             cx->exception = rval;
