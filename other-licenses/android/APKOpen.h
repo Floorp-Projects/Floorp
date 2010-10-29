f0731a5a375d155e8be77b7a05fc637b6a55e903
Mark Finkle <mfinkle@mozilla.com>
2010-10-29 10:07:54 -0400

    Bug 608239: Backout changeset 99233ad2ff70 a=fennec-should-load-pages

--- other-licenses/android/APKOpen.h
+++ other-licenses/android/APKOpen.h
@@ -45,16 +45,6 @@ struct mapping_info {
   size_t offset;
 };
 
-const struct mapping_info * getLibraryMapping();
-
-#define MAX_LIB_CACHE_ENTRIES 32
-#define MAX_LIB_CACHE_NAME_LEN 32
-
-struct lib_cache_info {
-  char name[MAX_LIB_CACHE_NAME_LEN];
-  int fd;
-};
-
-const struct lib_cache_info * getLibraryCache();
+extern struct mapping_info * lib_mapping;
 
 #endif /* APKOpen_h */
--- toolkit/xre/nsAndroidStartup.cpp
+++ toolkit/xre/nsAndroidStartup.cpp
@@ -75,7 +75,7 @@ static void*
 GeckoStart(void *data)
 {
 #ifdef MOZ_CRASHREPORTER
-    const struct mapping_info *info = getLibraryMapping();
+    struct mapping_info *info = lib_mapping;
     while (info->name) {
       CrashReporter::AddLibraryMapping(info->name, info->file_id, info->base,
                                        info->len, info->offset);
