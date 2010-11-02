21966b22dd1c0a2c8b0b43bbd96303610741ce9f
David Anderson <danderson@mozilla.com>
2010-11-02 10:56:17 -0700

    Backed out changeset 4a6259d13882 (bug 608810, a=blocking).

--- js/src/methodjit/Compiler.h
+++ js/src/methodjit/Compiler.h
@@ -351,7 +351,6 @@ class Compiler : public BaseCompiler
     void jsop_bindgname();
     void jsop_setelem_slow();
     void jsop_getelem_slow();
-    void jsop_callelem_slow();
     void jsop_unbrand();
     bool jsop_getprop(JSAtom *atom, bool typeCheck = true, bool usePropCache = true);
     bool jsop_length();
@@ -417,8 +416,7 @@ class Compiler : public BaseCompiler
     void jsop_arginc(JSOp op, uint32 slot, bool popped);
     void jsop_localinc(JSOp op, uint32 slot, bool popped);
     void jsop_setelem();
-    bool jsop_getelem(bool isCall);
-    bool isCacheableBaseAndIndex(FrameEntry *obj, FrameEntry *id);
+    bool jsop_getelem();
     void jsop_stricteq(JSOp op);
     void jsop_equality(JSOp op, BoolStub stub, jsbytecode *target, JSOp fused);
     void jsop_equality_int_string(JSOp op, BoolStub stub, jsbytecode *target, JSOp fused);
