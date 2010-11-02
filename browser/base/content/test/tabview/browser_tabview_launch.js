5345a8155991c2ef41bdec8bb54dff004cc61e9b
Ian Gilman <ian@iangilman.com>
2010-11-02 13:34:09 -0700

    Backed out changeset bcad241e5c22

--- browser/base/content/test/tabview/browser_tabview_launch.js
+++ browser/base/content/test/tabview/browser_tabview_launch.js
@@ -55,28 +55,13 @@ function test() {
 function onTabViewLoadedAndShown() {
   window.removeEventListener("tabviewshown", onTabViewLoadedAndShown, false);
   
-  // Evidently sometimes isVisible (which is based on the selectedIndex of the
-  // tabview deck) isn't updated immediately when called from button.doCommand,
-  // so we add a little timeout here to get outside of the doCommand call.
-  // If the initial timeout isn't enough, we keep waiting in case it's taking
-  // longer than expected.
-  // See bug 594909.
-  let deck = document.getElementById("tab-view-deck");
-  function waitForSwitch() {
-    if (deck.selectedIndex == 1) {
-      ok(TabView.isVisible(), "Tab View is visible. Count: " + tabViewShownCount);
-      tabViewShownCount++;
-      
-      // kick off the series
-      window.addEventListener("tabviewshown", onTabViewShown, false);
-      window.addEventListener("tabviewhidden", onTabViewHidden, false);
-      TabView.toggle();
-    } else {
-      setTimeout(waitForSwitch, 10);
-    }
-  }
+  ok(TabView.isVisible(), "Tab View is visible. Count: " + tabViewShownCount);
+  tabViewShownCount++;
   
-  setTimeout(waitForSwitch, 1);
+  // kick off the series
+  window.addEventListener("tabviewshown", onTabViewShown, false);
+  window.addEventListener("tabviewhidden", onTabViewHidden, false);
+  TabView.toggle();
 }
 
 // ----------
