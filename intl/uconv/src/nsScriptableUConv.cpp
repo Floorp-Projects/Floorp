7149e8039284ca56b7a427b370b97c276e712ada
Simon Montagu <smontagu@smontagu.org>
2010-11-07 14:26:52 +0200

    Backout changeset 97aca07b7f32 due to test failures, a=backout

--- intl/uconv/src/nsScriptableUConv.cpp
+++ intl/uconv/src/nsScriptableUConv.cpp
@@ -56,7 +56,6 @@ static PRInt32          gInstanceCount = 0;
 NS_IMPL_ISUPPORTS1(nsScriptableUnicodeConverter, nsIScriptableUnicodeConverter)
 
 nsScriptableUnicodeConverter::nsScriptableUnicodeConverter()
-: mIsInternal(PR_FALSE)
 {
   PR_AtomicIncrement(&gInstanceCount);
 }
@@ -272,20 +271,6 @@ nsScriptableUnicodeConverter::SetCharset(const char * aCharset)
   return InitConverter();
 }
 
-NS_IMETHODIMP
-nsScriptableUnicodeConverter::GetIsInternal(PRBool *aIsInternal)
-{
-  *aIsInternal = mIsInternal;
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsScriptableUnicodeConverter::SetIsInternal(const PRBool aIsInternal)
-{
-  mIsInternal = aIsInternal;
-  return NS_OK;
-}
-
 nsresult
 nsScriptableUnicodeConverter::InitConverter()
 {
@@ -302,11 +287,7 @@ nsScriptableUnicodeConverter::InitConverter()
     if(NS_SUCCEEDED(rv)) {
       rv = mEncoder->SetOutputErrorBehavior(nsIUnicodeEncoder::kOnError_Replace, nsnull, (PRUnichar)'?');
       if(NS_SUCCEEDED(rv)) {
-        rv = mIsInternal ?
-          ccm->GetUnicodeDecoderInternal(mCharset.get(),
-                                         getter_AddRefs(mDecoder)) :
-          ccm->GetUnicodeDecoder(mCharset.get(),
-                                 getter_AddRefs(mDecoder));
+        rv = ccm->GetUnicodeDecoder(mCharset.get(), getter_AddRefs(mDecoder));
       }
     }
   }
