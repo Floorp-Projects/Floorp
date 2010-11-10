f1d14db0284df8bf98576abe8de9f34984a52fc5
Brendan Eich <brendan@mozilla.org>
2010-11-10 10:59:02 -0800

    Lots of MISMATCH_EXITS exits due to protohazardshape on voxel rendering demo (550391, r=mrbkap).

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -9402,7 +9402,7 @@ TraceRecorder::guardPropertyCacheHit(LIns* obj_ins,
 
         guard(true,
               w.name(w.eqiN(vshape_ins, vshape), "guard_protoHazardShape"),
-              MISMATCH_EXIT);
+              BRANCH_EXIT);
     }
 
     // For any hit that goes up the scope and/or proto chains, we will need to
