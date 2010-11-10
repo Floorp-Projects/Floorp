2c264b1c99078d436593d96a392719236d839834
Brian Hackett <bhackett1024@gmail.com>
2010-11-09 16:46:48 -0800

    [INFER] Fix ARM build break.

--- js/src/methodjit/MethodJIT.cpp
+++ js/src/methodjit/MethodJIT.cpp
@@ -400,7 +400,7 @@ JS_STATIC_ASSERT(offsetof(VMFrame, entryFp) ==          (4*10));
 JS_STATIC_ASSERT(offsetof(VMFrame, stackLimit) ==       (4*9));
 JS_STATIC_ASSERT(offsetof(VMFrame, cx) ==               (4*8));
 JS_STATIC_ASSERT(offsetof(VMFrame, regs.fp) ==          (4*7));
-JS_STATIC_ASSERT(offsetof(VMFrame, unused) ==           (4*4));
+JS_STATIC_ASSERT(offsetof(VMFrame, scratch) ==          (4*4));
 JS_STATIC_ASSERT(offsetof(VMFrame, previous) ==         (4*3));
 
 JS_STATIC_ASSERT(JSFrameReg == JSC::ARMRegisters::r11);
@@ -457,7 +457,7 @@ SYMBOL_STRING(JaegerTrampoline) ":"         "\n"
      *  [ regs.fp   ]
      *  [ regs.pc   ]
      *  [ regs.sp   ]
-     *  [ unused    ]
+     *  [ scratch   ]
      *  [ previous  ]
      *  [ args.ptr3 ]
      *  [ args.ptr2 ]
