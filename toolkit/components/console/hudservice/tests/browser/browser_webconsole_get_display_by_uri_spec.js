fea1c182d70639b06c87236a833fded9ca177af2
Rob Campbell <rcampbell@mozilla.com>
2010-11-12 10:20:38 -0400

    bug 597136 - Remove the HUDService onerror event handler and use the window ID, backed out rev 563c20646a43, a=leaks CLOSED TREE

--- toolkit/components/console/hudservice/tests/browser/browser_webconsole_get_display_by_uri_spec.js
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_get_display_by_uri_spec.js
@@ -38,58 +38,23 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-// Tests that exceptions thrown by content don't show up twice in the Web
-// Console.
+// Tests the HUDService.getDisplayByURISpec() method.
 
-const TEST_DUPLICATE_ERROR_URI = "http://example.com/browser/toolkit/components/console/hudservice/tests/browser/test-duplicate-error.html";
+const TEST_URI = "http://example.com/browser/toolkit/components/console/hudservice/tests/browser/test-console.html";
 
 function test() {
-  addTab(TEST_DUPLICATE_ERROR_URI);
-  browser.addEventListener("DOMContentLoaded", testDuplicateErrors, false);
+  addTab(TEST_URI);
+  browser.addEventListener("DOMContentLoaded", testGetDisplayByURISpec,
+                           false);
 }
 
-function testDuplicateErrors() {
-  browser.removeEventListener("DOMContentLoaded", testDuplicateErrors,
+function testGetDisplayByURISpec() {
+  browser.removeEventListener("DOMContentLoaded", testGetDisplayByURISpec,
                               false);
   openConsole();
-
-  let hudId = HUDService.displaysIndex()[0];
-  HUDService.clearDisplay(hudId);
-
-  Services.console.registerListener(consoleObserver);
-
-  content.location.reload();
+  outputNode = HUDService.getDisplayByURISpec(TEST_URI);
+  hudId = outputNode.getAttribute("id");
+  ok(hudId == HUDService.displaysIndex()[0], "outputNode fetched by URIspec");
+  finishTest();
 }
 
-var consoleObserver = {
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver]),
-
-  observe: function (aMessage)
-  {
-    // we ignore errors we don't care about
-    if (!(aMessage instanceof Ci.nsIScriptError) ||
-      aMessage.category != "content javascript") {
-      return;
-    }
-
-    Services.console.unregisterListener(this);
-
-    var display = HUDService.getDisplayByURISpec(content.location.href);
-    var outputNode = display.querySelectorAll(".hud-output-node")[0];
-
-    executeSoon(function () {
-      var text = outputNode.textContent;
-      var error1pos = text.indexOf("fooDuplicateError1");
-      ok(error1pos > -1, "found fooDuplicateError1");
-      if (error1pos > -1) {
-        ok(text.indexOf("fooDuplicateError1", error1pos + 1) == -1,
-          "no duplicate for fooDuplicateError1");
-      }
-
-      ok(text.indexOf("test-duplicate-error.html") > -1,
-        "found test-duplicate-error.html");
-
-      finishTest();
-    });
-  }
-};
