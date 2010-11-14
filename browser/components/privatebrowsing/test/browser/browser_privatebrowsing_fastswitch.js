3baac87b0cdc8008fa8bbf9503275d1ad099deb1
Ehsan Akhgari <ehsan@mozilla.com>
2010-11-14 03:22:21 -0500

    Bug 607482 - Part 2: Adjust the test so that the code doesn't need to unset the attribute asynchronously; r=dao a=gavin

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_fastswitch.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_fastswitch.js
@@ -48,13 +48,6 @@ function test() {
   let pass = 1;
   function observer(aSubject, aTopic, aData) {
     switch (aTopic) {
-      case "private-browsing":
-        setTimeout(function () {
-          ok(document.getElementById("Tools:PrivateBrowsing").hasAttribute("disabled"),
-             "The private browsing command should be disabled immediately after the mode switch");
-        }, 0);
-        break;
-
       case "private-browsing-transition-complete":
         if (pass++ == 1) {
           setTimeout(function () {
@@ -69,16 +62,31 @@ function test() {
             ok(!pbCmd.hasAttribute("disabled"),
                "The private browsing command should be re-enabled after exiting the private browsing mode");
 
-            Services.obs.removeObserver(observer, "private-browsing");
-            Services.obs.removeObserver(observer, "private-browsing-transition-complete");
             finish();
           }, 100);
         }
         break;
     }
+    Services.obs.removeObserver(observer, "private-browsing-transition-complete");
   }
-  Services.obs.addObserver(observer, "private-browsing", false);
-  Services.obs.addObserver(observer, "private-browsing-transition-complete", false);
+  let originalOnEnter = gPrivateBrowsingUI.onEnterPrivateBrowsing;
+  gPrivateBrowsingUI.onEnterPrivateBrowsing = function() {
+    originalOnEnter.apply(gPrivateBrowsingUI, arguments);
+    ok(pbCmd.hasAttribute("disabled"),
+       "The private browsing command should be disabled right after entering the private browsing mode");
+    Services.obs.addObserver(observer, "private-browsing-transition-complete", false);
+  };
+  let originalOnExit = gPrivateBrowsingUI.onExitPrivateBrowsing;
+  gPrivateBrowsingUI.onExitPrivateBrowsing = function() {
+    originalOnExit.apply(gPrivateBrowsingUI, arguments);
+    ok(pbCmd.hasAttribute("disabled"),
+       "The private browsing command should be disabled right after exiting the private browsing mode");
+    Services.obs.addObserver(observer, "private-browsing-transition-complete", false);
+  };
+  registerCleanupFunction(function() {
+    gPrivateBrowsingUI.onEnterPrivateBrowsing = originalOnEnter;
+    gPrivateBrowsingUI.onExitPrivateBrowsing = originalOnExit;
+  });
 
   pb.privateBrowsingEnabled = true;
 }
