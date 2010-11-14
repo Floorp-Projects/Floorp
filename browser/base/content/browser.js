06bbd4ead4ed0272c6295c262663ed10493aa27f
Ehsan Akhgari <ehsan@mozilla.com>
2010-11-14 03:22:21 -0500

    Bug 607482 - Part 2: Adjust the test so that the code doesn't need to unset the attribute asynchronously; r=dao a=gavin

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -7484,11 +7484,8 @@ let gPrivateBrowsingUI = {
     }
     else if (aTopic == "private-browsing-transition-complete") {
       if (this._disableUIOnToggle) {
-        // use setTimeout here in order to make the code testable
-        setTimeout(function() {
-          document.getElementById("Tools:PrivateBrowsing")
-                  .removeAttribute("disabled");
-        }, 0);
+        document.getElementById("Tools:PrivateBrowsing")
+                .removeAttribute("disabled");
       }
     }
   },
