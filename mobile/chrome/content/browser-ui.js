f9c1c01019928cab2846d13944c9a62ed1af0cf2
Vivien Nicolas <21@vingtetun.org>
2010-11-17 22:37:04 +0100

    Bug 611893 - The form helper up arrow is incorrectly enabled on Google.com [r=mfinkle]

--- mobile/chrome/content/browser-ui.js
+++ mobile/chrome/content/browser-ui.js
@@ -1859,11 +1859,10 @@ var FormHelperUI = {
   },
 
   show: function formHelperShow(aElement, aHasPrevious, aHasNext) {
-    this._open = true;
-
     // Update the next/previous commands
     this._cmdPrevious.setAttribute("disabled", !aHasPrevious);
     this._cmdNext.setAttribute("disabled", !aHasNext);
+    this._open = true;
 
     let lastElement = this._currentElement || null;
     this._currentElement = {
