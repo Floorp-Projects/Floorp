f7387309897fab02b05651b1f2058d68baa7fd99
Doug Turner <dougt@dougt.org>
2010-11-16 20:14:19 -0800

    Bug 599089 - Remote Audio to parent process. r=kinetik a=blocking-fennec

--- content/media/wave/nsWaveDecoder.cpp
+++ content/media/wave/nsWaveDecoder.cpp
@@ -295,7 +295,7 @@ private:
   // Our audio stream.  Created on demand when entering playback state.  It
   // is destroyed when seeking begins and will not be reinitialized until
   // playback resumes, so it is possible for this to be null.
-  nsAutoPtr<nsAudioStream> mAudioStream;
+  nsRefPtr<nsAudioStream> mAudioStream;
 
   // Maximum time to spend waiting for data during buffering.
   TimeDuration mBufferingWait;
@@ -906,7 +906,7 @@ nsWaveStateMachine::ChangeState(State aState)
 void
 nsWaveStateMachine::OpenAudioStream()
 {
-  mAudioStream = new nsAudioStream();
+  mAudioStream = nsAudioStream::AllocateStream();
   if (!mAudioStream) {
     LOG(PR_LOG_ERROR, ("Could not create audio stream"));
   } else {
