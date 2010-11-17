0fabe74c6de8f6ffb2a1d022c2ee6183e1e7b2cd
Henri Sivonen <hsivonen@iki.fi>
2010-11-17 11:06:12 +0200

    Bug 612530 - Sync html5lib tree builder tests into m-c. r=jgriffin, a=test-only.

--- parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
+++ parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
@@ -8,4 +8,13 @@ var html5Exceptions = {
   "<!doctype html><keygen><frameset>" : true, // Bug 101019
   "<select><keygen>" : true, // Bug 101019
   "<math><mi><div><object><div><span></span></div></object></div></mi><mi>" : true, // Bug 606925
+  "<plaintext>\u0000filler\u0000text\u0000" : true, // Bug 612527
+  "<body><svg><foreignObject>\u0000filler\u0000text" : true, // Bug 612527
+  "<svg>\u0000</svg><frameset>" : true, // Bug 612527
+  "<svg>\u0000 </svg><frameset>" : true, // Bug 612527
+  "<option><span><option>" : true, // Bug 612528
+  "<math><annotation-xml encoding=\"application/xhtml+xml\"><div>" : true, // Bug 612529
+  "<math><annotation-xml encoding=\"aPPlication/xhtmL+xMl\"><div>" : true, // Bug 612529
+  "<math><annotation-xml encoding=\"text/html\"><div>" : true, // Bug 612529
+  "<math><annotation-xml encoding=\"Text/htmL\"><div>" : true, // Bug 612529
 }
