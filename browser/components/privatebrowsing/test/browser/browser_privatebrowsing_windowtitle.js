ec1c019f4861ee6d21297989e784dbf4b30589bd
Ehsan Akhgari <ehsan@mozilla.com>
2010-11-18 18:35:04 -0500

    Backed out changeset 364409371144 because of the orange

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_windowtitle.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_windowtitle.js
@@ -93,14 +93,19 @@ function test() {
         win.addEventListener("load", function() {
           win.removeEventListener("load", arguments.callee, false);
 
-          executeSoon(function() {
+          // ensure that the test is run after delayedStartup
+          let _delayedStartup = win.delayedStartup;
+          win.delayedStartup = function() {
+            _delayedStartup.apply(win, arguments);
+            win.delayedStartup = _delayedStartup;
+
             is(win.document.title, expected_title, "The window title for " + url +
                " detached tab is correct (" + (insidePB ? "inside" : "outside") +
                " private browsing mode)");
             win.close();
 
             setTimeout(funcNext, 0);
-          });
+          };
         }, false);
       });
     }, false);
