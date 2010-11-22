10a98cc35b8895f780fec8d7eb9aa7e5f9ad599a
Dão Gottwald <dao@mozilla.com>
2010-11-22 11:21:43 +0100

    Bug 612588 - Toolbars outside of the toolbox should be visible while customizing, just like those inside of the toolbox. r=enn a=gavin

--- browser/base/content/test/browser_bug599325.js
+++ browser/base/content/test/browser_bug599325.js
@@ -1,39 +1,17 @@
 function test() {
   waitForExplicitFinish();
-  
-  // test the main (normal) browser window
-  testCustomize(window, testChromeless);
-}
 
-function testChromeless() {
-  // test a chromeless window
-  var newWin = openDialog("chrome://browser/content/", "_blank",
-                      "chrome,dialog=no,toolbar=no", "about:blank");
-  ok(newWin, "got new window");
-
-  function runWindowTest() {
-    function finalize() {
-      newWin.removeEventListener("load", runWindowTest, false);
-      newWin.close();
-      finish();
-    }
-    testCustomize(newWin, finalize);
-  }
-
-  newWin.addEventListener("load", runWindowTest, false);
+  testCustomize(window, finish);
 }
 
 function testCustomize(aWindow, aCallback) {
   var addonBar = aWindow.document.getElementById("addon-bar");
   ok(addonBar, "got addon bar");
-  is(addonBar.collapsed, true, "addon bar initially disabled");
+  ok(!isElementVisible(addonBar), "addon bar initially hidden");
 
   // Launch toolbar customization
   // ctEl is either iframe that contains the customize sheet, or the dialog
   var ctEl = aWindow.BrowserCustomizeToolbar();
-
-  is(addonBar.collapsed, false,
-     "file menu is not collapsed during toolbar customization");
 
   aWindow.gNavToolbox.addEventListener("beforecustomization", function () {
     aWindow.gNavToolbox.removeEventListener("beforecustomization", arguments.callee, false);
@@ -41,11 +19,14 @@ function testCustomize(aWindow, aCallback) {
   }, false);
 
   function ctInit() {
+    ok(isElementVisible(addonBar),
+       "add-on bar is visible during toolbar customization");
+
     // Close toolbar customization
     closeToolbarCustomization(aWindow, ctEl);
 
-    is(addonBar.getAttribute("collapsed"), "true",
-       "addon bar is collapsed after toolbar customization");
+    ok(!isElementVisible(addonBar),
+       "addon bar is hidden after toolbar customization");
 
     if (aCallback)
       aCallback();
