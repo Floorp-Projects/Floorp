d1a71502a56ca8969bef56f63f2495456890fa5e
Wes Johnston <wjohnston@mozilla.com>
2010-11-22 19:36:45 -0800

    Bug 593815 - Remove all files left behind when a download is canceled. [r=sdwilsh, sr=bz, a=blocker]

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -2352,7 +2352,7 @@ nsresult nsExternalAppHandler::OpenWithApplication()
     if (deleteTempFileOnExit || gExtProtSvc->InPrivateBrowsing())
       mFinalFileDestination->SetPermissions(0400);
 
-    rv = mMimeInfo->LaunchWithFile(mFinalFileDestination);        
+    rv = mMimeInfo->LaunchWithFile(mFinalFileDestination);
     if (NS_FAILED(rv))
     {
       // Send error notification.
@@ -2481,6 +2481,13 @@ NS_IMETHODIMP nsExternalAppHandler::Cancel(nsresult aReason)
   {
     mTempFile->Remove(PR_FALSE);
     mTempFile = nsnull;
+  }
+
+  // If we have already created a final destination file, we remove it as well
+  if (mFinalFileDestination)
+  {
+    mFinalFileDestination->Remove(PR_FALSE);
+    mFinalFileDestination = nsnull;
   }
 
   // Release the listener, to break the reference cycle with it (we are the
