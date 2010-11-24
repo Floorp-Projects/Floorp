e0e26989680db6542dd3c51113c3b8f1a894ebb2
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2010-11-24 11:11:45 +0100

    Bug 606990 - Remove CAIRO_HAS_DDRAW_SURFACE/CAIRO_DDRAW_USE_GL dead code. r=vlad,jmuizelaar a=bsmedberg

--- gfx/cairo/cairo/src/cairo-ddraw.h
+++ gfx/cairo/cairo/src/cairo-ddraw.h
@@ -38,37 +38,4 @@
 
 #include "cairo.h"
 
-#if CAIRO_HAS_DDRAW_SURFACE
-
-#include <windows.h>
-#include <ddraw.h>
-
-CAIRO_BEGIN_DECLS
-
-cairo_public cairo_surface_t *
-cairo_ddraw_surface_create (LPDIRECTDRAW lpdd,
-			    cairo_format_t format,
-			    int width,
-			    int height);
-
-cairo_public cairo_surface_t *
-cairo_ddraw_surface_create_alias (cairo_surface_t *surface,
-				  int x,
-				  int y,
-				  int width,
-				  int height);
-
-cairo_public LPDIRECTDRAWSURFACE
-cairo_ddraw_surface_get_ddraw_surface (cairo_surface_t *surface);
-
-cairo_public cairo_surface_t *
-cairo_ddraw_surface_get_image (cairo_surface_t *surface);
-
-
-CAIRO_END_DECLS
-
-#else  /* CAIRO_HAS_DDRAW_SURFACE */
-# error Cairo was not compiled with support for the direct draw backend
-#endif /* CAIRO_HAS_DDRAW_SURFACE */
-
 #endif /* _CAIRO_DDRAW_H_ */
--- gfx/thebes/gfxWindowsPlatform.h
+++ gfx/thebes/gfxWindowsPlatform.h
@@ -45,7 +45,7 @@
 
 
 /**
- * XXX to get CAIRO_HAS_DDRAW_SURFACE, CAIRO_HAS_D2D_SURFACE and
+ * XXX to get CAIRO_HAS_D2D_SURFACE and
  * CAIRO_HAS_DWRITE_FONT
  */
 #include "cairo.h"
