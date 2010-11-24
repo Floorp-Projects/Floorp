e284b18b93cb3df8b1c93319a7baa5734bdd0f96
Kyle Huey <khuey@kylehuey.com>
2010-11-24 01:09:48 +0100

    Bug 613027: Make nsHTMLFieldSetElement participate in cycle collection properly. r=peterv a=blocking-final

--- content/html/content/src/nsHTMLFieldSetElement.h
+++ content/html/content/src/nsHTMLFieldSetElement.h
@@ -55,7 +55,7 @@ public:
   virtual ~nsHTMLFieldSetElement();
 
   // nsISupports
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_ISUPPORTS_INHERITED
 
   // nsIDOMNode
   NS_FORWARD_NSIDOMNODE(nsGenericHTMLFormElement::)
