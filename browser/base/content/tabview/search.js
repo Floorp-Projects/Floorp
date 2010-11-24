a49f3412d889d41847fa391b4b3f073c49991f37
Kyle Huey <khuey@kylehuey.com>
2010-11-24 00:59:48 -0500

    Mass backout to fix OS X 64 Leak.

--- browser/base/content/tabview/search.js
+++ browser/base/content/tabview/search.js
@@ -515,6 +515,7 @@ function ensureSearchShown(event){
   var $searchbox = iQ("#searchbox");
   iQ("#searchbutton").css({ opacity: 1 });
 
+
   if (!isSearchEnabled()) {
     $search.show();
     var mainWindow = gWindow.document.getElementById("main-window");
