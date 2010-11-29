e65845ab6884ea7e1ef08e42c48f1a2ad1328765
Jason Orendorff <jorendorff@mozilla.com>
2010-11-29 12:50:07 -0600

    JS_DeepFreezeObject does not actually do anything. Bug 609024, r=Waldo.

--- js/src/jsapi-tests/testDeepFreeze.cpp
+++ js/src/jsapi-tests/testDeepFreeze.cpp
@@ -6,9 +6,49 @@
 
 BEGIN_TEST(testDeepFreeze_bug535703)
 {
-    JSObject *obj = JS_NewObject(cx, NULL, NULL, NULL);
-    CHECK(obj);
-    JS_DeepFreezeObject(cx, obj);  // don't crash
+    jsval v;
+    EVAL("var x = {}; x;", &v);
+    CHECK(JS_DeepFreezeObject(cx, JSVAL_TO_OBJECT(v)));  // don't crash
+    EVAL("Object.isFrozen(x)", &v);
+    CHECK_SAME(v, JSVAL_TRUE);
     return true;
 }
 END_TEST(testDeepFreeze_bug535703)
+
+BEGIN_TEST(testDeepFreeze_deep)
+{
+    jsval a, o;
+    EXEC("var a = {}, o = a;\n"
+         "for (var i = 0; i < 10000; i++)\n"
+         "    a = {x: a, y: a};\n");
+    EVAL("a", &a);
+    EVAL("o", &o);
+
+    CHECK(JS_DeepFreezeObject(cx, JSVAL_TO_OBJECT(a)));
+
+    jsval b;
+    EVAL("Object.isFrozen(a)", &b);
+    CHECK_SAME(b, JSVAL_TRUE);
+    EVAL("Object.isFrozen(o)", &b);
+    CHECK_SAME(b, JSVAL_TRUE);
+    return true;
+}
+END_TEST(testDeepFreeze_deep)
+
+BEGIN_TEST(testDeepFreeze_loop)
+{
+    jsval x, y;
+    EXEC("var x = [], y = {x: x}; y.y = y; x.push(x, y);");
+    EVAL("x", &x);
+    EVAL("y", &y);
+
+    CHECK(JS_DeepFreezeObject(cx, JSVAL_TO_OBJECT(x)));
+
+    jsval b;
+    EVAL("Object.isFrozen(x)", &b);
+    CHECK_SAME(b, JSVAL_TRUE);
+    EVAL("Object.isFrozen(y)", &b);
+    CHECK_SAME(b, JSVAL_TRUE);
+    return true;
+}
+END_TEST(testDeepFreeze_loop)
