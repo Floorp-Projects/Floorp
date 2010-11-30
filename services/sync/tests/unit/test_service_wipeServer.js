f35aba613e330ea339b1823fa0c64fb5325a4e00
Richard Newman <rnewman@mozilla.com>
2010-11-29 16:41:33 -0800

    Bug 614737: detecting upgrade, being nice to old clients. r=philikon

--- services/sync/tests/unit/test_service_wipeServer.js
+++ services/sync/tests/unit/test_service_wipeServer.js
@@ -34,7 +34,7 @@ function setUpTestFixtures() {
 
   Service.clusterURL = "http://localhost:8080/";
   Service.username = "johndoe";
-  Service.passphrase = "secret";
+  Service.passphrase = "aabcdeabcdeabcdeabcdeabcde";
 }
 
 function test_withCollectionList_fail() {
@@ -77,6 +77,68 @@ function test_withCollectionList_fail() {
   }
 }
 
+function test_wipeServer_leaves_collections() {
+  _("Service.wipeServer() deletes everything but keys.");
+  
+  let steam_coll = new FakeCollection();
+  let diesel_coll = new FakeCollection();
+  let keys_coll = new FakeCollection();
+
+  function info_collections(request, response) {
+    let collections = {};
+    let timestamp = Date.now() / 1000;
+    if (!steam_coll.deleted)
+      collections.steam = timestamp
+    if (!diesel_coll.deleted)
+      collections.diesel = timestamp;
+    if (!keys_coll.deleted)
+      collections.keys = timestamp;
+    let body = JSON.stringify(collections);
+    response.setStatusLine(request.httpVersion, 200, "OK");
+    response.bodyOutputStream.write(body, body.length);
+  }
+
+  let server = httpd_setup({
+    "/1.0/johndoe/storage/steam": steam_coll.handler(),
+    "/1.0/johndoe/storage/diesel": diesel_coll.handler(),
+    "/1.0/johndoe/storage/keys": keys_coll.handler(),
+    "/1.0/johndoe/info/collections": info_collections
+  });
+  do_test_pending();
+
+  try {
+    setUpTestFixtures();
+    _("Info URL: " + Service.infoURL);
+
+    _("Confirm initial environment.");
+    do_check_false(steam_coll.deleted);
+    do_check_false(diesel_coll.deleted);
+    do_check_false(keys_coll.deleted);
+    
+    _("Collections: " + new Resource(Service.infoURL).get());
+    _("Try deletion.");
+    Service.wipeServer();
+    _("Collections: " + new Resource(Service.infoURL).get());
+    
+    _("Make sure keys is still present.");
+    do_check_true(steam_coll.deleted);
+    do_check_true(diesel_coll.deleted);
+    do_check_false(keys_coll.deleted);
+    
+    _("Delete everything.");
+    Service.wipeServer(null, true);
+    do_check_true(steam_coll.deleted);
+    do_check_true(diesel_coll.deleted);
+    do_check_true(keys_coll.deleted);
+    
+  } finally {
+    server.stop(do_test_finished);
+    Svc.Prefs.resetBranch("");
+  }
+}
+
 function run_test() {
+  initTestLogging("Trace");
   test_withCollectionList_fail();
+  test_wipeServer_leaves_collections();
 }
