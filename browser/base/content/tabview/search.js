9661a1f8ec05357b5cdd84cdf38de5641971b79b
Raymond Lee <raymond@raysquare.com>
2010-11-30 10:02:22 +0800

    Bug 608158 - Exiting Panorama when nothing but hidden groups and no app tabs, should create new tab [r=ian, a=beltzner]

--- browser/base/content/tabview/search.js
+++ browser/base/content/tabview/search.js
@@ -365,6 +365,7 @@ SearchEventHandlerClass.prototype = {
   // Function: inSearchKeyHandler
   // Handles all keypresses while search mode.
   inSearchKeyHandler: function (event) {
+    let term = iQ("#searchbox").val();
     if ((event.keyCode == event.DOM_VK_ESCAPE) || 
         (event.keyCode == event.DOM_VK_BACK_SPACE && term.length <= 1)) {
       hideSearch(event);
