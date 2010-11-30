d6c6690f78eee7d6fe82ea0d0cadcc7e2375f753
Brian Hackett <bhackett1024@gmail.com>
2010-11-30 06:52:08 -0800

    [INFER] Fix broken jsop_this() in strict-mode code, bug 608750.

--- js/src/methodjit/Compiler.cpp
+++ js/src/methodjit/Compiler.cpp
@@ -3850,21 +3850,23 @@ mjit::Compiler::jsop_this()
      * In direct-call eval code, we wrapped 'this' before entering the eval.
      * In global code, 'this' is always an object.
      */
-    JSValueType type = knownThisType(); 
-    FrameEntry *thisFe = frame.peek(-1);
-    if (!thisFe->isTypeKnown()) {
-        if (fun && !script->strictModeCode && type != JSVAL_TYPE_OBJECT) {
-            Jump notObj = frame.testObject(Assembler::NotEqual, thisFe);
-            stubcc.linkExit(notObj, Uses(1));
-            stubcc.leave();
-            OOL_STUBCALL(stubs::This);
-            stubcc.rejoin(Changes(1));
-        }
+    if (fun && !script->strictModeCode) {
+        FrameEntry *thisFe = frame.peek(-1);
+        if (!thisFe->isTypeKnown()) {
+            JSValueType type = knownThisType();
+            if (type != JSVAL_TYPE_OBJECT) {
+                Jump notObj = frame.testObject(Assembler::NotEqual, thisFe);
+                stubcc.linkExit(notObj, Uses(1));
+                stubcc.leave();
+                OOL_STUBCALL(stubs::This);
+                stubcc.rejoin(Changes(1));
+            }
 
-        // Now we know that |this| is an object.
-        frame.pop();
-        frame.learnThisIsObject();
-        frame.pushThis();
+            // Now we know that |this| is an object.
+            frame.pop();
+            frame.learnThisIsObject();
+            frame.pushThis();
+        }
 
         JS_ASSERT(thisFe->isType(JSVAL_TYPE_OBJECT));
     }
