fba3b5726778fa0679f578d8971ec1b6b57d0bc7
Vivien Nicolas <21@vingtetun.org>
2010-11-30 15:02:23 +0100

    Bug 597296 - Chrome registry selected locale should not changed immediatly [r=bsmedberg,glandium, a=blocking-fennec]

--- chrome/test/unit/test_bug519468.js
+++ chrome/test/unit/test_bug519468.js
@@ -40,6 +40,7 @@ var MANIFESTS = [
   do_get_file("data/test_bug519468.manifest")
 ];
 
+do_test_pending()
 registerManifests(MANIFESTS);
 
 var chromeReg = Cc["@mozilla.org/chrome/chrome-registry;1"]
@@ -53,29 +54,42 @@ var localeService = Cc["@mozilla.org/intl/nslocaleservice;1"]
 var prefService = Cc["@mozilla.org/preferences-service;1"]
                   .getService(Ci.nsIPrefService)
                   .QueryInterface(Ci.nsIPrefBranch);
+var os = Cc["@mozilla.org/observer-service;1"]
+         .getService(Ci.nsIObserverService);
+
+var systemLocale = localeService.getLocaleComponentForUserAgent();
+var count = 0;
+var testsLocale = [
+  {matchOS: false, selected: null, locale: "en-US"},
+  {matchOS: true, selected: null, locale: systemLocale},
+  {matchOS: true, selected: "fr-FR", locale: systemLocale},
+  {matchOS: false, selected: "fr-FR", locale: "fr-FR"},
+  {matchOS: false, selected: "de-DE", locale: "de-DE"},
+  {matchOS: true, selected: null, locale: systemLocale}
+];
 
 function test_locale(aTest) {
   prefService.setBoolPref("intl.locale.matchOS", aTest.matchOS);
   prefService.setCharPref("general.useragent.locale", aTest.selected || "en-US");
 
-  var selectedLocale = chromeReg.getSelectedLocale("testmatchos");
-  do_check_eq(selectedLocale, aTest.locale);
+  chromeReg.reloadChrome();
 }
 
-function run_test()
-{
-  var systemLocale = localeService.getLocaleComponentForUserAgent();
+function checkValidity() {
+  var selectedLocale = chromeReg.getSelectedLocale("testmatchos");
+  do_check_eq(selectedLocale, testsLocale[count].locale);
 
-  var tests = [
-    {matchOS: false, selected: null, locale: "en-US"},
-    {matchOS: true, selected: null, locale: systemLocale},
-    {matchOS: true, selected: "fr-FR", locale: systemLocale},
-    {matchOS: false, selected: "fr-FR", locale: "fr-FR"},
-    {matchOS: true, selected: null, locale: systemLocale}
-  ];
-
-  for (var i = 0; i < tests.length; ++ i) {
-    var test = tests[i];
-    test_locale(test);
+  count++;
+  if (count >= testsLocale.length) {
+    os.removeObserver(checkValidity, "selected-locale-has-changed");
+    do_test_finished();
   }
+  else {
+    test_locale(testsLocale[count]);
+  }
+}
+
+function run_test() {
+  os.addObserver(checkValidity, "selected-locale-has-changed", false);
+  test_locale(testsLocale[count]);
 }
