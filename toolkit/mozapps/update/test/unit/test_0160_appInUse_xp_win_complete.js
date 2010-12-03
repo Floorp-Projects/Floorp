1cf2a753695379b1586b3fb556f4842a31dcf567
Robert Strong <robert.bugzilla@gmail.com>
2010-12-02 19:57:20 -0800

    Bug 615467 - intermittent failure in toolkit\mozapps\update\test\unit\test_0170_fileLocked_xp_win_complete.js | test failed (with xpcshell return code: 0), see following log: succeeded == failed. r=dtownsend, a=test fix

--- toolkit/mozapps/update/test/unit/test_0160_appInUse_xp_win_complete.js
+++ toolkit/mozapps/update/test/unit/test_0160_appInUse_xp_win_complete.js
@@ -5,6 +5,8 @@
 /* Application in use complete MAR file patch apply failure test */
 
 const TEST_ID = "0160";
+// Time to wait for the test helper process to start before continuing the test
+const TEST_HELPER_TIMEOUT = 1000;
 
 // The files are in the same order as they are applied from the mar
 const TEST_FILES = [
@@ -70,7 +72,7 @@ function run_test() {
 
   // Give the lock file process time to lock the file before updating otherwise
   // this test can fail intermittently on Windows debug builds.
-  do_timeout(100, testUpdate);
+  do_timeout(TEST_HELPER_TIMEOUT, testUpdate);
 }
 
 function end_test() {
