a96f0e4b008e7c1bdeff9491352f900e0698f89e
Ehsan Akhgari <ehsan@mozilla.com>
2010-12-03 13:57:14 -0500

    Backed out changeset b7e28f448533 (bug 609685) because of test failures

--- browser/base/content/tabview/ui.js
+++ browser/base/content/tabview/ui.js
@@ -398,11 +398,6 @@ let UI = {
     if (this.isTabViewVisible())
       return;
 
-    // Update the tab we're looking at, so that the correct image is used 
-    // to zoom out to the TabView.
-    if (!gBrowser.selectedTab.pinned)
-      TabItems._update(gBrowser.selectedTab);
-
     // initialize the direction of the page
     this._initPageDirection();
 
@@ -458,9 +453,6 @@ let UI = {
       self.setActiveTab(null);
       dispatchEvent(event);
     }
-
-    // Flush pending updates
-    GroupItems.flushAppTabUpdates();
 
     TabItems.resumePainting();
   },
