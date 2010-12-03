38c8f81e7baaa87e25e1c1ed1416a958853e7e94
Paul O’Shannessy <paul@oshannessy.com>
2010-12-03 14:52:57 -0800

    Backout revision f15120d5a96e (bug 607016). a=orange

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -1459,15 +1459,6 @@ SessionStoreService.prototype = {
         tabData.pinned = true;
       else
         delete tabData.pinned;
-      tabData.hidden = aTab.hidden;
-
-      // If __SS_extdata is set then we'll use that since it might be newer.
-      if (aTab.__SS_extdata)
-        tabData.extData = aTab.__SS_extdata;
-      // If it exists but is empty then a key was likely deleted. In that case just
-      // delete extData.
-      if (tabData.extData && !Object.keys(tabData.extData).length)
-        delete tabData.extData;
       return tabData;
     }
     
