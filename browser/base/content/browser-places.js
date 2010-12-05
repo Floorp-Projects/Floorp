19b434fb8802727536ccd11ca55f6cf5087ead78
Neil Deakin <neil@mozilla.com>
2010-12-05 17:10:45 -0500

    Bug 606343, part 3, use centered arrow panel for bookmarks and identity panel, r=dao, a=blocking

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -347,7 +347,8 @@ var PlacesCommandHook = {
       if (starIcon && isElementVisible(starIcon)) {
         // Make sure the bookmark properties dialog hangs toward the middle of
         // the location bar in RTL builds
-        var position = (getComputedStyle(gNavToolbox, "").direction == "rtl") ? 'after_start' : 'after_end';
+        var position = (getComputedStyle(gNavToolbox, "").direction == "rtl") ?
+          'bottomcenter topleft' : 'bottomcenter topright';
         if (aShowEditUI)
           StarUI.showEditBookmarkPopup(itemId, starIcon, position);
         return;
