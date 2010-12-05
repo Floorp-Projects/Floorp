5d7a393be34881b05f94d232d289fad6b6e3f43b
Robert Longson <longsonr@gmail.com>
2010-12-05 20:37:39 +0000

    Bug 489871 - CSS comments should not be allowed in presentation attribute values. r=dbaron a=roc
    --HG--
    extra : rebase_source : 7a41fca1422a024934839ebe0c50cc479223915f

--- layout/style/nsCSSScanner.h
+++ layout/style/nsCSSScanner.h
@@ -148,7 +148,6 @@ class nsCSSScanner {
   static PRBool InitGlobals();
   static void ReleaseGlobals();
 
-#ifdef  MOZ_SVG
   // Set whether or not we are processing SVG
   void SetSVGMode(PRBool aSVGMode) {
     NS_ASSERTION(aSVGMode == PR_TRUE || aSVGMode == PR_FALSE,
@@ -159,7 +158,6 @@ class nsCSSScanner {
     return mSVGMode;
   }
 
-#endif
 #ifdef CSS_REPORT_PARSE_ERRORS
   void AddToError(const nsSubstring& aErrorText);
   void OutputError();
@@ -238,10 +236,8 @@ protected:
   nsresult mLowLevelError;
 
   PRUint32 mLineNumber;
-#ifdef MOZ_SVG
   // True if we are in SVG mode; false in "normal" CSS
   PRPackedBool mSVGMode;
-#endif
 #ifdef CSS_REPORT_PARSE_ERRORS
   nsXPIDLCString mFileName;
   nsCOMPtr<nsIURI> mURI;  // Cached so we know to not refetch mFileName
