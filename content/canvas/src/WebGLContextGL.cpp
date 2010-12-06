62e8b57bf57d2ce384a19619360fc2c291fc9cfb
Benoit Jacob <bjacob@mozilla.com>
2010-12-06 06:34:35 -0500

    Bug 613306 - fix bufferSubData quickStub - r=vlad

--- content/canvas/src/WebGLContextGL.cpp
+++ content/canvas/src/WebGLContextGL.cpp
@@ -353,6 +353,14 @@ WebGLContext::BufferData(PRInt32 dummy)
 }
 
 NS_IMETHODIMP
+WebGLContext::BufferData_null()
+{
+    // see http://www.khronos.org/bugzilla/show_bug.cgi?id=386
+    return ErrorInvalidValue("bufferData: null object passed");
+}
+
+
+NS_IMETHODIMP
 WebGLContext::BufferData_size(WebGLenum target, WebGLsizei size, WebGLenum usage)
 {
     WebGLBuffer *boundBuffer = NULL;
@@ -446,6 +454,12 @@ NS_IMETHODIMP
 WebGLContext::BufferSubData(PRInt32 dummy)
 {
     return NS_ERROR_FAILURE;
+}
+
+NS_IMETHODIMP
+WebGLContext::BufferSubData_null()
+{
+    return NS_OK; // see http://www.khronos.org/bugzilla/show_bug.cgi?id=386
 }
 
 NS_IMETHODIMP
