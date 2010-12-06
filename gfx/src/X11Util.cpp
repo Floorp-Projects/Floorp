dac80986dca8aa2d5ee5d9590ce6b91defa71fd5
Benoit Jacob <bjacob@mozilla.com>
2010-12-06 09:10:42 -0500

    Bug 613079 - fix non-libxul build and style fixes - r=dholbert, a=fix-build-nonlibxul

--- gfx/src/X11Util.cpp
+++ gfx/src/X11Util.cpp
@@ -41,6 +41,41 @@
 
 namespace mozilla {
 
-ScopedXErrorHandler::ErrorEvent* ScopedXErrorHandler::s_xerrorptr;
+ScopedXErrorHandler::ErrorEvent* ScopedXErrorHandler::sXErrorPtr;
+
+int
+ScopedXErrorHandler::ErrorHandler(Display *, XErrorEvent *ev)
+{
+    sXErrorPtr->mError = *ev;
+    return 0;
+}
+
+
+ScopedXErrorHandler::ScopedXErrorHandler()
+{
+    // let sXErrorPtr point to this object's mXError object, but don't reset this mXError object!
+    // think of the case of nested ScopedXErrorHandler's.
+    mOldXErrorPtr = sXErrorPtr;
+    sXErrorPtr = &mXError;
+    mOldErrorHandler = XSetErrorHandler(ErrorHandler);
+}
+
+ScopedXErrorHandler::~ScopedXErrorHandler()
+{
+    sXErrorPtr = mOldXErrorPtr;
+    XSetErrorHandler(mOldErrorHandler);
+}
+
+bool
+ScopedXErrorHandler::SyncAndGetError(Display *dpy, XErrorEvent *ev)
+{
+    XSync(dpy, False);
+    bool retval = mXError.mError.error_code != 0;
+    if (ev)
+        *ev = mXError.mError;
+    mXError = ErrorEvent(); // reset
+    return retval;
+}
+
 
 } // namespace mozilla
