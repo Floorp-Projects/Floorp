3cadda98a41953df66c91903fa5b347b6e39804b
Brian Hackett <bhackett1024@gmail.com>
2010-12-07 09:13:53 -0800

    [INFER] Another liveness analysis bugfix, bug 609899.

--- js/src/jit-test/tests/jaeger/regalloc-live.js
+++ js/src/jit-test/tests/jaeger/regalloc-live.js
@@ -15,3 +15,27 @@ function foo(a, b, c) {
   return x + y + z;
 }
 assertEq(foo(1, 2, 3), 6);
+
+// restore liveness correctly before switch statements
+
+function foo(a, b, c) {
+  var x = 0, y = 0, z = 0;
+  if (a < b) {
+    x = a + 0;
+    y = b + 0;
+    z = c + 0;
+  } else {
+    switch (c) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5: return 0;
+    }
+    x = 0;
+    y = 0;
+    z = 0;
+  }
+  return x + y + z;
+}
+assertEq(foo(1, 2, 3), 6);
