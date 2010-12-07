eaa8ea7162886b070c26a09e40a9c4234f3e71d3
Daniel Brooks <db48x@db48x.net>
2010-12-07 02:28:12 -0600

    about:startup - final review hurdle, add _MOZILLA_2_0 interfaces to contain the additions to nsIXULRuntime and nsIXULAppInfo

--- toolkit/components/startup/src/nsAppStartup.h
+++ toolkit/components/startup/src/nsAppStartup.h
@@ -55,7 +55,7 @@ struct PLEvent;
 { 0x7dd4d320, 0xc84b, 0x4624, { 0x8d, 0x45, 0x7b, 0xb9, 0xb2, 0x35, 0x69, 0x77 } }
 
 
-class nsAppStartup : public nsIAppStartup2,
+class nsAppStartup : public nsIAppStartup_MOZILLA_2_0,
                      public nsIWindowCreator2,
                      public nsIObserver,
                      public nsSupportsWeakReference
@@ -64,6 +64,7 @@ public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIAPPSTARTUP
   NS_DECL_NSIAPPSTARTUP2
+  NS_DECL_NSIAPPSTARTUP_MOZILLA_2_0
   NS_DECL_NSIWINDOWCREATOR
   NS_DECL_NSIWINDOWCREATOR2
   NS_DECL_NSIOBSERVER
