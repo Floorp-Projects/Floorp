702b90e2aad4199675c3d0d951e00b77cde895ed
Mats Palmgren <matspal@gmail.com>
2010-12-08 03:20:52 +0100

    Bug 610601 - re-use the frame only if there is image data.  r+a=joe

--- modules/libpr0n/src/RasterImage.cpp
+++ modules/libpr0n/src/RasterImage.cpp
@@ -918,21 +918,21 @@ RasterImage::EnsureCleanFrame(PRUint32 aFrameNum, PRInt32 aX, PRInt32 aY,
 
   // See if we can re-use the frame that already exists.
   nsIntRect rect = frame->GetRect();
-  if (rect.x != aX || rect.y != aY || rect.width != aWidth || rect.height != aHeight ||
-      frame->GetFormat() != aFormat) {
-    DeleteImgFrame(aFrameNum);
-    return InternalAddFrame(aFrameNum, aX, aY, aWidth, aHeight, aFormat, 
-                            /* aPaletteDepth = */ 0, imageData, imageLength,
-                            /* aPaletteData = */ nsnull, 
-                            /* aPaletteLength = */ nsnull);
+  if (rect.x == aX && rect.y == aY && rect.width == aWidth &&
+      rect.height == aHeight && frame->GetFormat() == aFormat) {
+    // We can re-use the frame if it has image data.
+    frame->GetImageData(imageData, imageLength);
+    if (*imageData) {
+      return NS_OK;
+    }
   }
 
-  // We can re-use the frame.
-  frame->GetImageData(imageData, imageLength);
-
-  return NS_OK;
+  DeleteImgFrame(aFrameNum);
+  return InternalAddFrame(aFrameNum, aX, aY, aWidth, aHeight, aFormat, 
+                          /* aPaletteDepth = */ 0, imageData, imageLength,
+                          /* aPaletteData = */ nsnull, 
+                          /* aPaletteLength = */ nsnull);
 }
-
 
 void
 RasterImage::FrameUpdated(PRUint32 aFrameNum, nsIntRect &aUpdatedRect)
