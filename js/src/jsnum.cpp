afd7d0db7e3d8b36cc3ce1993f7763e26a8d2a2e
Robert Sayre <sayrer@gmail.com>
2010-12-08 19:42:58 -0500

    Bug 617215. JS_NewString leaks. r=bz, a=blocking b8

--- js/src/jsnum.cpp
+++ js/src/jsnum.cpp
@@ -812,11 +812,10 @@ num_toLocaleString(JSContext *cx, uintN argc, Value *vp)
     if (cx->localeCallbacks && cx->localeCallbacks->localeToUnicode)
         return cx->localeCallbacks->localeToUnicode(cx, buf, Jsvalify(vp));
 
-    str = JS_NewString(cx, buf, size);
-    if (!str) {
-        cx->free(buf);
+    str = js_NewStringCopyN(cx, buf, size);
+    cx->free(buf);
+    if (!str)
         return JS_FALSE;
-    }
 
     vp->setString(str);
     return JS_TRUE;
