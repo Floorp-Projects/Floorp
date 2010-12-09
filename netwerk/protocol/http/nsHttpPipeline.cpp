37984326e6e5d02bb74d973be0b28439574bf4c9
Honza Bambas <honzab.moz@firemni.cz>
2010-12-08 20:57:14 -0500

    Bug 616591.  Fix crash due to http transactions trying to get connection info from the http pipeline while it's being closed.  r=bzbarsky, a=blocker

--- netwerk/protocol/http/nsHttpPipeline.cpp
+++ netwerk/protocol/http/nsHttpPipeline.cpp
@@ -502,9 +502,6 @@ nsHttpPipeline::Close(nsresult reason)
     mStatus = reason;
     mClosed = PR_TRUE;
 
-    // we must no longer reference the connection!
-    NS_IF_RELEASE(mConnection);
-
     PRUint32 i, count;
     nsAHttpTransaction *trans;
 
@@ -536,6 +533,9 @@ nsHttpPipeline::Close(nsresult reason)
         }
         mResponseQ.Clear();
     }
+
+    // we must no longer reference the connection!
+    NS_IF_RELEASE(mConnection);
 }
 
 nsresult
