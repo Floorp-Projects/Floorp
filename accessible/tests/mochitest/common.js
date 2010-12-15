2fd3f1a06c609d95ba3f718b4e34d04ba1a1099f
Alexander Surkov <surkov.alexander@gmail.com>
2010-12-15 13:23:19 -0800

    Bug 617273 - make editable text tests async, r=marcoz, fer, a=test

--- accessible/tests/mochitest/common.js
+++ accessible/tests/mochitest/common.js
@@ -540,6 +540,36 @@ function relationTypeToString(aRelationType)
 }
 
 /**
+ * Return text from clipboard.
+ */
+function getTextFromClipboard()
+{
+  var clip = Components.classes["@mozilla.org/widget/clipboard;1"].
+    getService(Components.interfaces.nsIClipboard);
+  if (!clip)
+    return;
+
+  var trans = Components.classes["@mozilla.org/widget/transferable;1"].
+    createInstance(Components.interfaces.nsITransferable);
+  if (!trans)
+    return;
+
+  trans.addDataFlavor("text/unicode");
+  clip.getData(trans, clip.kGlobalClipboard);
+
+  var str = new Object();
+  var strLength = new Object();
+  trans.getTransferData("text/unicode", str, strLength);
+
+  if (str)
+    str = str.value.QueryInterface(Components.interfaces.nsISupportsString);
+  if (str)
+    return str.data.substring(0, strLength.value / 2);
+
+  return "";
+}
+
+/**
  * Return pretty name for identifier, it may be ID, DOM node or accessible.
  */
 function prettyName(aIdentifier)
