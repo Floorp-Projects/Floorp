95aec3e589bc8d10ca7ace271a6b3157e27e2552
Jan de Mooij <jandemooij@gmail.com>
2010-12-16 14:05:59 -0800

    [INFER] Always infer int/int as producing an int, bug 619737. r=bhackett

--- js/src/jsinfer.cpp
+++ js/src/jsinfer.cpp
@@ -3010,11 +3010,6 @@ Script::analyzeTypes(JSContext *cx, Bytecode *code, AnalyzeState &state)
       case JSOP_DIV:
         code->popped(0)->addArith(cx, pool, code, code->pushed(0));
         code->popped(1)->addArith(cx, pool, code, code->pushed(0));
-        if (op == JSOP_DIV) {
-            /* Guess that divisions other than x/n produce doubles. */
-            if (!state.popped(0).isConstant)
-                code->setFixed(cx, 0, TYPE_DOUBLE);
-        }
         break;
 
       case JSOP_NEG:
