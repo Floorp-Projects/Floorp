440a2e24b0cddf430822a4d12904db42d258813c
Jonathan Kew <jfkthame@gmail.com>
2010-12-16 14:17:41 -0800

    bug 332636 - fix editor's handling of surrogate-pair combining marks with backspace. r=ehsan a=roc

--- layout/generic/nsFrame.h
+++ layout/generic/nsFrame.h
@@ -238,7 +238,8 @@ public:
   NS_IMETHOD  GetSelectionController(nsPresContext *aPresContext, nsISelectionController **aSelCon);
 
   virtual PRBool PeekOffsetNoAmount(PRBool aForward, PRInt32* aOffset);
-  virtual PRBool PeekOffsetCharacter(PRBool aForward, PRInt32* aOffset);
+  virtual PRBool PeekOffsetCharacter(PRBool aForward, PRInt32* aOffset,
+                                     PRBool aRespectClusters = PR_TRUE);
   virtual PRBool PeekOffsetWord(PRBool aForward, PRBool aWordSelectEatSpace, PRBool aIsKeyboardSelect,
                                 PRInt32* aOffset, PeekWordState *aState);
   /**
