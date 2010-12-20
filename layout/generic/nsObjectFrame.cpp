95b762fe4be585d1c099ee39a6cd6dc7a0ea7bac
Robert O'Callahan <robert@ocallahan.org>
2010-12-20 14:37:43 +1300

    Bug 617152. Part 9: Use desired bounds in nsDisplayList::GetBounds, not actual bounds, when computing plugin geometry. r=tnikkel

--- layout/generic/nsObjectFrame.cpp
+++ layout/generic/nsObjectFrame.cpp
@@ -1251,6 +1251,12 @@ nsDisplayPlugin::GetBounds(nsDisplayListBuilder* aBuilder)
 {
   nsRect r = mFrame->GetContentRect() - mFrame->GetPosition() +
     ToReferenceFrame();
+  if (aBuilder->IsForPluginGeometry()) {
+    // Return the geometry we want, not the geometry we have (which is based
+    // on the surface the plugin last gave us)
+    return r;
+  }
+
   nsObjectFrame* f = static_cast<nsObjectFrame*>(mFrame);
   if (mozilla::LAYER_ACTIVE == f->GetLayerState(aBuilder, nsnull)) {
     ImageContainer* c = f->GetImageContainer();
