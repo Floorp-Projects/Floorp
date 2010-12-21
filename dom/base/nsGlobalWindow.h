0ce6811105913cf4c9593677e5cb3f1490bbf52c
Ben Turner <bent.mozilla@gmail.com>
2010-12-21 14:28:05 -0500

    Bug 615153 - 'nsIWorkerFactory.newChromeWorker() throws NS_ERROR_UNEXPECTED'. r=sicking, a=blocking.

--- dom/base/nsGlobalWindow.h
+++ dom/base/nsGlobalWindow.h
@@ -1058,6 +1058,11 @@ protected:
   nsIDocShell* mDocShell; // weak reference
 };
 
+nsresult NS_GetNavigatorUserAgent(nsAString& aUserAgent);
+nsresult NS_GetNavigatorPlatform(nsAString& aPlatform);
+nsresult NS_GetNavigatorAppVersion(nsAString& aAppVersion);
+nsresult NS_GetNavigatorAppName(nsAString& aAppName);
+
 class nsIURI;
 
 //*****************************************************************************
