ec96f1b239259241e55c431f74364c8988ecbee3
Mark Finkle <mfinkle@mozilla.com>
2010-12-21 12:42:04 -0500

    Bug 531825 - Confirm before clearing private data [r=mbrubeck]

--- mobile/chrome/content/browser-ui.js
+++ mobile/chrome/content/browser-ui.js
@@ -1098,12 +1098,18 @@ var BrowserUI = {
         break;
       case "cmd_sanitize":
       {
-        // disable the button temporarily to indicate something happened
-        let button = document.getElementById("prefs-clear-data");
-        button.disabled = true;
-        setTimeout(function() { button.disabled = false; }, 5000);
+        let strings = Elements.browserBundle;
+        let title = strings.getString("clearPrivateData.title");
+        let message = strings.getString("clearPrivateData.message");
+        let clear = Services.prompt.confirm(window, title, message);
+        if (clear) {
+          // disable the button temporarily to indicate something happened
+          let button = document.getElementById("prefs-clear-data");
+          button.disabled = true;
+          setTimeout(function() { button.disabled = false; }, 5000);
 
-        Sanitizer.sanitize();
+          Sanitizer.sanitize();
+        }
         break;
       }
       case "cmd_panel":
