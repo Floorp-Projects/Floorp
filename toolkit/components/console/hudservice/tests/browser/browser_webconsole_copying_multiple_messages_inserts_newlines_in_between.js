0e36444a641991e3b86eebf8538bb9c9a6e9cae4
Patrick Walton <pwalton@mozilla.com>
2010-12-23 11:59:19 -0400

    Bug 605621 - Web Console output box should be reskinned - part 1; f=mihai.sucan r=sdwilsh a=blocking2.0

--- toolkit/components/console/hudservice/tests/browser/browser_webconsole_copying_multiple_messages_inserts_newlines_in_between.js
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_copying_multiple_messages_inserts_newlines_in_between.js
@@ -8,8 +8,7 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-// Tests that newlines are present in the output of the console, so that
-// copying works properly.
+// Tests that copying multiple messages inserts newlines in between.
 
 const TEST_URI = "http://example.com/";
 
@@ -44,15 +43,18 @@ function testNewlines() {
   let outputNode = hudNode.querySelector(".hud-output-node");
   ok(outputNode != null, "we have the output node");
 
-  let labels = outputNode.querySelectorAll("label");
-  is(labels.length, 20, "we found 20 labels in the output node");
+  outputNode.selectAll();
+  outputNode.focus();
 
-  for (let i = 0; i < labels.length; i++) {
-    let value = labels[i].textContent;
-    is(value[value.length - 1], "\n", "the value of label " + i + " ends " +
-       "with a newline");
+  let clipboardTexts = [];
+  for (let i = 0; i < outputNode.itemCount; i++) {
+    let item = outputNode.getItemAtIndex(i);
+    clipboardTexts.push("[" + ConsoleUtils.timestampString(item.timestamp) +
+                        "] " + item.clipboardText);
   }
 
-  finishTest();
+  waitForClipboard(clipboardTexts.join("\n"),
+                   function() { goDoCommand("cmd_copy"); },
+                   finishTest, finishTest);
 }
 
