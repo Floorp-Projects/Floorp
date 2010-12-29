62f9a2c75c10cc69764d8923b083b18caafaa7c3
Bas Schouten <bschouten@mozilla.com>
2010-12-29 01:07:09 +0000

    Bug 620665 - Part 4: Also recreate container when types don't match. r=roc a=blocking-beta9

--- layout/generic/nsVideoFrame.cpp
+++ layout/generic/nsVideoFrame.cpp
@@ -193,11 +193,15 @@ nsVideoFrame::BuildLayer(nsDisplayListBuilder* aBuilder,
     container->SetLayerManager(aManager);
   }
 
-  // If we have a container with the right layer manager already, we don't
-  // need to do anything here. Otherwise we need to set up a temporary
+  // If we have a container of the correct type already, we don't need
+  // to do anything here. Otherwise we need to set up a temporary
   // ImageContainer, capture the video data and store it in the temp
-  // container.
-  if (!container || container->Manager() != aManager) {
+  // container. For now we also check if the manager is equal since not all
+  // image containers are manager independent yet.
+  if (!container || 
+      (container->Manager() && container->Manager() != aManager) ||
+      container->GetBackendType() != aManager->GetBackendType())
+  {
     nsRefPtr<ImageContainer> tmpContainer = aManager->CreateImageContainer();
     if (!tmpContainer)
       return nsnull;
