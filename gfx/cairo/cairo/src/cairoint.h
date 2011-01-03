75aa7440610d1f2cc8a52d545cb8be75318b2463
Robert O'Callahan <robert@ocallahan.org>
2011-01-03 14:48:08 +1300

    Bug 363861. Part 2: Introduce cairo_surface_get/set_subpixel_antialiasing. r=jrmuizel,sr=vlad,a=blocking

--- gfx/cairo/cairo/src/cairoint.h
+++ gfx/cairo/cairo/src/cairoint.h
@@ -2755,6 +2755,8 @@ slim_hidden_proto (cairo_surface_get_font_options);
 slim_hidden_proto (cairo_surface_get_mime_data);
 slim_hidden_proto (cairo_surface_get_type);
 slim_hidden_proto (cairo_surface_has_show_text_glyphs);
+slim_hidden_proto (cairo_surface_set_subpixel_antialiasing);
+slim_hidden_proto (cairo_surface_get_subpixel_antialiasing);
 slim_hidden_proto (cairo_surface_mark_dirty_rectangle);
 slim_hidden_proto_no_warn (cairo_surface_reference);
 slim_hidden_proto (cairo_surface_set_device_offset);
