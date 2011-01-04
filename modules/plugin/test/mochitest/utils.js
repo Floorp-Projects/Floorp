928b4c12a714532f2ab83e3ccc3d3e364849479c
Robert O'Callahan <robert@ocallahan.org>
2011-01-04 16:56:09 +1300

    Bug 601064. Fix tests (and test this bug). a=tests

---
+++ modules/plugin/test/mochitest/utils.js
@@ -0,0 +1,19 @@
+function paintCountIs(plugin, expected, msg) {
+  var count = plugin.getPaintCount();
+  var realExpected = expected;
+  var isAsync = SimpleTest.testPluginIsOOP() &&
+    navigator.platform.indexOf("Mac") < 0;
+  if (isAsync) {
+    ++realExpected; // extra paint at startup for all async-rendering plugins
+  } else {
+    netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    try {
+      if (Components.classes["@mozilla.org/gfx/info;1"].getService(Components.interfaces.nsIGfxInfo).D2DEnabled) {
+        realExpected *= 2;
+      }
+    } catch (e) {}
+  }
+  ok(realExpected == count, msg + " (expected " + expected +
+     " independent paints, expected " + realExpected + " logged paints, got " +
+     count + " actual paints)");
+}
