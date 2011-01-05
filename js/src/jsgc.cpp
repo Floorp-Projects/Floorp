e43d254d94049e5653bdfaeb17cb375039eef9b3
Jeff Walden <jwalden@mit.edu>
2011-01-05 16:50:30 -0600

    Bug 623301 - Properly root arguments names during a call to Function().  r=brendan

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -1601,6 +1601,11 @@ AutoGCRooter::trace(JSTracer *trc)
         MarkIdRange(trc, vector.length(), vector.begin(), "js::AutoIdVector.vector");
         return;
       }
+
+      case BINDINGS: {
+        static_cast<js::AutoBindingsRooter *>(this)->bindings.trace(trc);
+        return;
+      }
     }
 
     JS_ASSERT(tag >= 0);
