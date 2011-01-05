d3d92c83a3a5dcd4e65a2604a6208777b184561e
Vladimir Vukicevic <vladimir@pobox.com>
2011-01-05 13:08:53 -0800

    b=622956; [webgl] add stub extension mechanism, fixes extension tests; r=bjacob

--- content/canvas/src/WebGLContextGL.cpp
+++ content/canvas/src/WebGLContextGL.cpp
@@ -4201,3 +4201,10 @@ int mozilla::GetWebGLTexelFormat(GLenum format, GLenum type)
         }
     }
 }
+
+NS_IMETHODIMP
+WebGLContext::GetExtension(const nsAString& aName, nsISupports **retval)
+{
+    *retval = nsnull;
+    return NS_OK;
+}
