cd2543115f1e639bbf3b20e3fe84bff55af9b412
Oleg Romashin <romaxa@gmail.com>
2011-01-05 15:48:16 +0200

    Bug 622063 - Make Fennec-Gtk compiling on Maemo6 r=dougt a=approval2.0

--- widget/src/gtk2/nsFilePicker.cpp
+++ widget/src/gtk2/nsFilePicker.cpp
@@ -56,7 +56,7 @@
 #include "nsFilePicker.h"
 #include "nsAccessibilityHelper.h"
 
-#ifdef MOZ_PLATFORM_MAEMO
+#if (MOZ_PLATFORM_MAEMO == 5)
 #include <hildon-fm-2/hildon/hildon-file-chooser-dialog.h>
 #endif
 
@@ -434,7 +434,7 @@ nsFilePicker::Show(PRInt16 *aReturn)
   GtkFileChooserAction action = GetGtkFileChooserAction(mMode);
   const gchar *accept_button = (action == GTK_FILE_CHOOSER_ACTION_SAVE)
                                ? GTK_STOCK_SAVE : GTK_STOCK_OPEN;
-#ifdef MOZ_PLATFORM_MAEMO
+#if (MOZ_PLATFORM_MAEMO == 5)
   GtkWidget *file_chooser =
     hildon_file_chooser_dialog_new_with_properties(parent_widget,
                                                    "action", action,
