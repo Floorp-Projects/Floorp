fff8711fb7ab5c21c44a2f1dacfd73aad438651d
Matt Brubeck <mbrubeck@mozilla.com>
2011-01-06 12:20:23 -0800

    Bug 623675 - Don't snap zoom to 100% as aggressively [r=mfinkle]

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -379,7 +379,7 @@ pref("browser.ui.kinetic.speedSensitivity", 80);
 pref("browser.ui.kinetic.swipeLength", 160);
 
 // zooming
-pref("browser.ui.zoom.pageFitGranularity", 5); // don't zoom to fit by less than 1/5 (20%)
+pref("browser.ui.zoom.pageFitGranularity", 9); // don't zoom to fit by less than 1/9 (11%)
 pref("browser.ui.zoom.animationDuration", 200); // ms duration of double-tap zoom animation
 pref("browser.ui.zoom.reflow", true); // Change text wrapping on double-tap
 
