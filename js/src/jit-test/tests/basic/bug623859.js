326619a3c1aa8bdbe3b660f9ae513a2cbbe20b5b
Paul Biggar <pbiggar@mozilla.com>
2011-01-07 18:55:35 -0800

    Bug 623859 - Add missing OOM checks during array allocation (r=lw)

---
+++ js/src/jit-test/tests/basic/bug623859.js
@@ -0,0 +1,16 @@
+// |jit-test| allow-oom
+
+(function() {
+   Iterator((function() {
+           switch ((7)) {
+               default:
+                     return (Float32Array).call([], 4300018)
+                         case Proxy.create((function() {
+                                     return {
+                                               e:
+                                                         function() {}
+                                                                 }
+                                                                       })):
+                             }
+                               })())
+   })()
