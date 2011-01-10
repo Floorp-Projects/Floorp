b1b767f953eb3bc2d29f03adc5b72e313860cffd
Wes Johnston <wjohnston@mozilla.com>
2011-01-10 09:48:41 -0800

    Bug 618124 - Don't use the play event to start fullscreen video [r=mfinkle]

--- mobile/chrome/content/fullscreen-video.js
+++ mobile/chrome/content/fullscreen-video.js
@@ -39,12 +39,20 @@ function closeFullScreen() {
   sendAsyncMessage("Browser:FullScreenVideo:Close");
 }
 
+function startPlayback(aEvent) {
+  let target = aEvent.originalTarget;
+  contentObject._sendMouseEvent("mousedown", target, 0, 0);
+  contentObject._sendMouseEvent("mousemove", target, 0, 0);
+  contentObject._sendMouseEvent("mouseup", target, 0, 0);
+}
+
 addEventListener("click", function(aEvent) {
   if (aEvent.target.id == "close")
     closeFullScreen();
 }, false);
 
 addEventListener("CloseVideo", closeFullScreen, false);
+addEventListener("StartVideo", startPlayback, false);
 
 addEventListener("PlayVideo", function() {
   sendAsyncMessage("Browser:FullScreenVideo:Play");
