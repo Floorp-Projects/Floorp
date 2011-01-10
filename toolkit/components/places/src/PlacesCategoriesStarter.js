ab775a22e0ffa2ed4e91118132147e196dd4ed6c
Marco Bonardo <mbonardo@mozilla.com>
2011-01-10 19:54:49 +0100

    Bug 609286 (Part 1) - Detect corrupt places.sqlite and replace it on next startup. r=dietrich a=blocker

--- toolkit/components/places/src/PlacesCategoriesStarter.js
+++ toolkit/components/places/src/PlacesCategoriesStarter.js
@@ -43,6 +43,9 @@
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 
+// Seconds between maintenance runs.
+const MAINTENANCE_INTERVAL_SECONDS = 7 * 86400;
+
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
 /**
@@ -61,9 +64,16 @@ PlacesCategoriesStarter.prototype = {
   {
     switch (aTopic) {
       case "idle-daily":
-        // Run places.sqlite maintenance tasks.
-        Components.utils.import("resource://gre/modules/PlacesDBUtils.jsm");
-        PlacesDBUtils.maintenanceOnIdle();
+        // Once a week run places.sqlite maintenance tasks.
+        let lastMaintenance = 0;
+        try {
+          lastMaintenance = Services.prefs.getIntPref("places.database.lastMaintenance");
+        } catch (ex) {}
+        let nowSeconds = parseInt(Date.now() / 1000);
+        if (lastMaintenance < nowSeconds - MAINTENANCE_INTERVAL_SECONDS) {
+          Components.utils.import("resource://gre/modules/PlacesDBUtils.jsm");
+          PlacesDBUtils.maintenanceOnIdle();
+        }
         break;
       default:
         throw new Error("Trying to handle an unknown category.");
