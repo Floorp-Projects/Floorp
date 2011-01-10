ce2456c7573a6de6d9e05b6e2ca6923f3504dd17
Marco Bonardo <mbonardo@mozilla.com>
2011-01-10 19:54:49 +0100

    Bug 609286 (Part 1) - Detect corrupt places.sqlite and replace it on next startup. r=dietrich a=blocker

--- toolkit/components/places/tests/unit/test_preventive_maintenance_checkAndFixDatabase.js
+++ toolkit/components/places/tests/unit/test_preventive_maintenance_checkAndFixDatabase.js
@@ -46,23 +46,36 @@ Components.utils.import("resource://gre/modules/PlacesDBUtils.jsm");
 function run_test() {
   do_test_pending();
   PlacesDBUtils.checkAndFixDatabase(function(aLog) {
-    // Integrity should be good.
-    do_check_eq(aLog[1], "ok");
+    let sections = [];
+    let positives = [];
+    let negatives = [];
+    let infos = [];
 
-    // Check we are not wrongly executing bad integrity tasks.
-    const goodMsgs = ["INTEGRITY", "EXPIRE", "VACUUM", "CLEANUP", "STATS"];
-    const badMsgs = ["BACKUP", "REINDEX"];
-
-    aLog.forEach(function (aLogMsg) {
-      print (aLogMsg);
-      do_check_eq(badMsgs.indexOf(aLogMsg), -1);
-      let index = goodMsgs.indexOf(aLogMsg);
-      if (index != -1)
-        goodMsgs.splice(index, 1);
+    aLog.forEach(function (aMsg) {
+      print (aMsg);
+      switch (aMsg.substr(0, 1)) {
+        case "+":
+          positives.push(aMsg);
+          break;
+        case "-":
+          negatives.push(aMsg);
+          break;
+        case ">":
+          sections.push(aMsg);
+          break;
+        default:
+          infos.push(aMsg);
+      }
     });
 
-    // Check we have run all tasks.
-    do_check_eq(goodMsgs.length, 0);
+    print("Check that we have run all sections.");
+    do_check_eq(sections.length, 5);
+    print("Check that we have no negatives.");
+    do_check_false(!!negatives.length);
+    print("Check that we have positives.");
+    do_check_true(!!positives.length);
+    print("Check that we have info.");
+    do_check_true(!!infos.length);
 
     do_test_finished();
   });
