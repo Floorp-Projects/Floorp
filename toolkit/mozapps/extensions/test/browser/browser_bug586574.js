79c435a959ae553c8b3331ae7800fc0ba5a9aeff
Dave Townsend <dtownsend@oxymoronical.com>
2011-01-11 15:42:08 -0800

    Bug 596174: Fix test failures in browser_bug586574.js. r=Unfocused, a=tests try: -b do -p linux,linux64,macosx,macosx64,win32 -u xpcshell,mochitest-o -t none

--- toolkit/mozapps/extensions/test/browser/browser_bug586574.js
+++ toolkit/mozapps/extensions/test/browser/browser_bug586574.js
@@ -17,7 +17,7 @@ var gResetToManual;
 
 function test() {
   waitForExplicitFinish();
-  
+
   gProvider = new MockProvider();
 
   gProvider.createAddons([{
@@ -29,6 +29,10 @@ function test() {
 
   open_manager("addons://list/extension", function(aWindow) {
     gManagerWindow = aWindow;
+
+    gUtilsBtn = gManagerWindow.document.getElementById("header-utils-btn");
+    gUtilsMenu = gManagerWindow.document.getElementById("utils-menu");
+
     run_next_test();
   });
 }
@@ -39,26 +43,48 @@ function end_test() {
 }
 
 
+function wait_for_popup(aCallback) {
+  if (gUtilsMenu.state == "open") {
+    aCallback();
+    return;
+  }
+
+  gUtilsMenu.addEventListener("popupshown", function() {
+    gUtilsMenu.removeEventListener("popupshown", arguments.callee, false);
+    info("Utilities menu shown");
+    aCallback();
+  }, false);
+}
+
+function wait_for_hide(aCallback) {
+  if (gUtilsMenu.state == "closed") {
+    aCallback();
+    return;
+  }
+
+  gUtilsMenu.addEventListener("popuphidden", function() {
+    gUtilsMenu.removeEventListener("popuphidden", arguments.callee, false);
+    info("Utilities menu hidden");
+    aCallback();
+  }, false);
+}
+
 add_test(function() {
-  gUtilsBtn = gManagerWindow.document.getElementById("header-utils-btn");
-  gUtilsMenu = gManagerWindow.document.getElementById("utils-menu");
   gSetDefault = gManagerWindow.document.getElementById("utils-autoUpdateDefault");
   gResetToAutomatic = gManagerWindow.document.getElementById("utils-resetAddonUpdatesToAutomatic");
   gResetToManual = gManagerWindow.document.getElementById("utils-resetAddonUpdatesToManual");
-  
+
   info("Ensuring default is set to true");
   Services.prefs.setBoolPref(PREF_AUTOUPDATE_DEFAULT, true);
-  
-  gUtilsBtn.addEventListener("popupshown", function() {
-    gUtilsBtn.removeEventListener("popupshown", arguments.callee, false);
-    
+
+  wait_for_popup(function() {
     is(gSetDefault.getAttribute("checked"), "true",
        "Set Default menuitem should be checked");
     is_element_visible(gResetToAutomatic,
                        "Reset to Automatic menuitem should be visible");
     is_element_hidden(gResetToManual,
                       "Reset to Manual menuitem should be hidden");
-    
+
     var listener = {
       onPropertyChanged: function(aAddon, aProperties) {
         AddonManager.removeAddonListener(listener);
@@ -68,55 +94,53 @@ add_test(function() {
            "Should have gotten applyBackgroundUpdates in properties array");
         is(aAddon.applyBackgroundUpdates, AddonManager.AUTOUPDATE_DEFAULT,
            "Addon.applyBackgroundUpdates should have been reset to default");
-        
+
         info("Setting Addon.applyBackgroundUpdates back to disabled");
         aAddon.applyBackgroundUpdates = AddonManager.AUTOUPDATE_DISABLE;
-        executeSoon(run_next_test);
+
+        wait_for_hide(run_next_test);
       }
     };
     AddonManager.addAddonListener(listener);
-    
+
     info("Clicking Reset to Automatic menuitem");
-    EventUtils.synthesizeMouse(gResetToAutomatic, 2, 2, { }, gManagerWindow);
+    EventUtils.synthesizeMouseAtCenter(gResetToAutomatic, { }, gManagerWindow);
+  });
 
-  }, false);
   info("Opening utilities menu");
-  EventUtils.synthesizeMouse(gUtilsBtn, 2, 2, { }, gManagerWindow);
+  EventUtils.synthesizeMouseAtCenter(gUtilsBtn, { }, gManagerWindow);
 });
 
 
 add_test(function() {
-  gUtilsBtn.addEventListener("popupshown", function() {
-    gUtilsBtn.removeEventListener("popupshown", arguments.callee, false);
-    
+  wait_for_popup(function() {
     is(gSetDefault.getAttribute("checked"), "true",
        "Set Default menuitem should be checked");
     is_element_visible(gResetToAutomatic,
                        "Reset to Automatic menuitem should be visible");
     is_element_hidden(gResetToManual,
                       "Reset to Manual menuitem should be hidden");
-    
-    info("Clicking Set Default menuitem");
-    EventUtils.synthesizeMouse(gSetDefault, 2, 2, { }, gManagerWindow);
 
-    executeSoon(run_next_test);
-  }, false);
+    wait_for_hide(run_next_test);
+
+    info("Clicking Set Default menuitem");
+    EventUtils.synthesizeMouseAtCenter(gSetDefault, { }, gManagerWindow);
+  });
+
   info("Opening utilities menu");
-  EventUtils.synthesizeMouse(gUtilsBtn, 2, 2, { }, gManagerWindow);
+  EventUtils.synthesizeMouseAtCenter(gUtilsBtn, { }, gManagerWindow);
 });
 
 
 add_test(function() {
-  gUtilsBtn.addEventListener("popupshown", function() {
-    gUtilsBtn.removeEventListener("popupshown", arguments.callee, false);
-    
+  wait_for_popup(function() {
     isnot(gSetDefault.getAttribute("checked"), "true",
           "Set Default menuitem should not be checked");
     is_element_hidden(gResetToAutomatic,
                       "Reset to automatic menuitem should be hidden");
     is_element_visible(gResetToManual,
                        "Reset to manual menuitem should be visible");
-    
+
     var listener = {
       onPropertyChanged: function(aAddon, aProperties) {
         AddonManager.removeAddonListener(listener);
@@ -126,60 +150,60 @@ add_test(function() {
            "Should have gotten applyBackgroundUpdates in properties array");
         is(aAddon.applyBackgroundUpdates, AddonManager.AUTOUPDATE_DEFAULT,
            "Addon.applyBackgroundUpdates should have been reset to default");
-        
+
         info("Setting Addon.applyBackgroundUpdates back to disabled");
         aAddon.applyBackgroundUpdates = AddonManager.AUTOUPDATE_DISABLE;
-        executeSoon(run_next_test);
+
+        wait_for_hide(run_next_test);
       }
     };
     AddonManager.addAddonListener(listener);
-    
-    info("Clicking Reset to Manual menuitem");
-    EventUtils.synthesizeMouse(gResetToManual, 2, 2, { }, gManagerWindow);
 
-  }, false);
+    info("Clicking Reset to Manual menuitem");
+    EventUtils.synthesizeMouseAtCenter(gResetToManual, { }, gManagerWindow);
+
+  });
+
   info("Opening utilities menu");
-  EventUtils.synthesizeMouse(gUtilsBtn, 2, 2, { }, gManagerWindow);
+  EventUtils.synthesizeMouseAtCenter(gUtilsBtn, { }, gManagerWindow);
 });
 
 
-
 add_test(function() {
-  gUtilsBtn.addEventListener("popupshown", function() {
-    gUtilsBtn.removeEventListener("popupshown", arguments.callee, false);
-    
+  wait_for_popup(function() {
     isnot(gSetDefault.getAttribute("checked"), "true",
           "Set Default menuitem should not be checked");
     is_element_hidden(gResetToAutomatic,
                       "Reset to automatic menuitem should be hidden");
     is_element_visible(gResetToManual,
                        "Reset to manual menuitem should be visible");
-    
+
+    wait_for_hide(run_next_test);
+
     info("Clicking Set Default menuitem");
-    EventUtils.synthesizeMouse(gSetDefault, 2, 2, { }, gManagerWindow);
-    
-    executeSoon(run_next_test);
-  }, false);
+    EventUtils.synthesizeMouseAtCenter(gSetDefault, { }, gManagerWindow);
+  });
+
   info("Opening utilities menu");
-  EventUtils.synthesizeMouse(gUtilsBtn, 2, 2, { }, gManagerWindow);
+  EventUtils.synthesizeMouseAtCenter(gUtilsBtn, { }, gManagerWindow);
 });
 
 
 add_test(function() {
-  gUtilsBtn.addEventListener("popupshown", function() {
-    gUtilsBtn.removeEventListener("popupshown", arguments.callee, false);
-    
+  wait_for_popup(function() {
     is(gSetDefault.getAttribute("checked"), "true",
        "Set Default menuitem should be checked");
     is_element_visible(gResetToAutomatic,
                        "Reset to Automatic menuitem should be visible");
     is_element_hidden(gResetToManual,
                       "Reset to Manual menuitem should be hidden");
-    
+
+    wait_for_hide(run_next_test);
+
     gUtilsMenu.hidePopup();
-    run_next_test();
-  }, false);
+  });
+
   info("Opening utilities menu");
-  EventUtils.synthesizeMouse(gUtilsBtn, 2, 2, { }, gManagerWindow);
+  EventUtils.synthesizeMouseAtCenter(gUtilsBtn, { }, gManagerWindow);
 });
 
