3b29b3a244b8d4467a12838e6109a90bf912bfab
Dave Townsend <dtownsend@oxymoronical.com>
2011-01-11 09:42:50 -0800

    Bug 611242: InstallTrigger should not use a prototype shared across all windows. r=robstrong, a=blocks-betaN

---
+++ toolkit/mozapps/extensions/test/xpinstall/browser_bug611242.js
@@ -0,0 +1,32 @@
+// ----------------------------------------------------------------------------
+// Test whether setting a new property in InstallTrigger then persists to other
+// page loads
+function loadURI(aUri, aCallback) {
+  gBrowser.selectedBrowser.addEventListener("load", function() {
+    if (gBrowser.selectedBrowser.currentURI.spec != aUri)
+      return;
+
+    aCallback();
+  }, true);
+
+  gBrowser.loadURI(aUri);
+}
+
+function test() {
+  waitForExplicitFinish();
+
+  gBrowser.selectedTab = gBrowser.addTab();
+
+  loadURI(TESTROOT + "enabled.html", function() {
+    window.content.wrappedJSObject.InstallTrigger.enabled.k = function() { };
+
+    loadURI(TESTROOT2 + "enabled.html", function() {
+      is(window.content.wrappedJSObject.InstallTrigger.enabled.k, undefined, "Property should not be defined");
+
+      gBrowser.removeTab(gBrowser.selectedTab);
+
+      finish();
+    });
+  });
+}
+// ----------------------------------------------------------------------------
