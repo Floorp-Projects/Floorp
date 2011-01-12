9b89df0451337669214769c995ce2da15f3ac415
Dhiren Bhatia <dbub87@hotmail.com>
2011-01-12 17:51:27 +1300

    Bug 617220 - Add 3DVision stereoscopic rendering support to D3D10 layers.  r=bas.schouten r=vlad a=roc a=vlad

--- gfx/layers/d3d9/ImageLayerD3D9.cpp
+++ gfx/layers/d3d9/ImageLayerD3D9.cpp
@@ -283,7 +283,10 @@ ImageLayerD3D9::RenderLayer()
 
     mD3DManager->SetShaderMode(DeviceManagerD3D9::YCBCRLAYER);
 
-    if (yuvImage->mData.mStereoMode != STEREO_MODE_MONO) {
+    /*
+     * Send 3d control data and metadata
+     */
+    if (mD3DManager->GetNv3DVUtils()) {
       Nv_Stereo_Mode mode;
       switch (yuvImage->mData.mStereoMode) {
       case STEREO_MODE_LEFT_RIGHT:
@@ -298,12 +301,15 @@ ImageLayerD3D9::RenderLayer()
       case STEREO_MODE_TOP_BOTTOM:
         mode = NV_STEREO_MODE_TOP_BOTTOM;
         break;
+      case STEREO_MODE_MONO:
+        mode = NV_STEREO_MODE_MONO;
+        break;
       }
 
-      /*
-       * Send 3d control data and metadata
-       */
-      if (mD3DManager->GetNv3DVUtils()) {
+      // Send control data even in mono case so driver knows to leave stereo mode.
+      mD3DManager->GetNv3DVUtils()->SendNv3DVControl(mode, true, FIREFOX_3DV_APP_HANDLE);
+
+      if (yuvImage->mData.mStereoMode != STEREO_MODE_MONO) {
         mD3DManager->GetNv3DVUtils()->SendNv3DVControl(mode, true, FIREFOX_3DV_APP_HANDLE);
 
         nsRefPtr<IDirect3DSurface9> renderTarget;
