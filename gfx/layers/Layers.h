5950a3639615863bcb2f333ea2ecef3d6c74f619
Benjamin Stover <bstover@mozilla.com>
2011-01-13 09:45:14 -0800

    Bug 605618 Part 1: Tag layers with scrollable information r=cjones sr=roc a=blocking-fennec

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -87,18 +87,28 @@ class SpecificLayerAttributes;
  * atomically with new pixels.
  */
 struct FrameMetrics {
+public:
+  // We use IDs to identify frames across processes.
+  typedef PRUint64 ViewID;
+  static const ViewID NULL_SCROLL_ID;   // This container layer does not scroll.
+  static const ViewID ROOT_SCROLL_ID;   // This is the root scroll frame.
+  static const ViewID START_SCROLL_ID;  // This is the ID that scrolling subframes
+                                        // will begin at.
+
   FrameMetrics()
-    : mViewportSize(0, 0)
+    : mViewport(0, 0, 0, 0)
     , mViewportScrollOffset(0, 0)
+    , mScrollId(NULL_SCROLL_ID)
   {}
 
   // Default copy ctor and operator= are fine
 
   PRBool operator==(const FrameMetrics& aOther) const
   {
-    return (mViewportSize == aOther.mViewportSize &&
+    return (mViewport == aOther.mViewport &&
             mViewportScrollOffset == aOther.mViewportScrollOffset &&
-            mDisplayPort == aOther.mDisplayPort);
+            mDisplayPort == aOther.mDisplayPort &&
+            mScrollId == aOther.mScrollId);
   }
 
   PRBool IsDefault() const
@@ -106,9 +116,20 @@ struct FrameMetrics {
     return (FrameMetrics() == *this);
   }
 
-  nsIntSize mViewportSize;
+  PRBool IsRootScrollable() const
+  {
+    return mScrollId == ROOT_SCROLL_ID;
+  }
+
+  PRBool IsScrollable() const
+  {
+    return mScrollId != NULL_SCROLL_ID;
+  }
+
+  nsIntRect mViewport;
   nsIntPoint mViewportScrollOffset;
   nsIntRect mDisplayPort;
+  ViewID mScrollId;
 };
 
 #define MOZ_LAYER_DECL_NAME(n, e)                           \
