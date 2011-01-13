95441cb8b91e88f598f7d73240f3bb7d25cb9992
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2011-01-13 13:22:47 -0500

    Bug 610242 - Don't show tab icon if there isn't one? [r=ian, a=beltzner]

--- browser/base/content/test/tabview/head.js
+++ browser/base/content/test/tabview/head.js
@@ -20,6 +20,8 @@
  *
  * Contributor(s):
  * Raymond Lee <raymond@appcoast.com>
+ * Michael Yoshitaka Erlewine <mitcho@mitcho.com>
+ * Tim Taubert <tim.taubert@gmx.de>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -51,19 +53,49 @@ function createEmptyGroupItem(contentWindow, width, height, padding, noAnimation
 }
 
 // ----------
-function afterAllTabsLoaded(callback) {
+function afterAllTabItemsUpdated(callback, win) {
+  win = win || window;
+  let tabItems = win.document.getElementById("tab-view").contentWindow.TabItems;
+
+  for (let a = 0; a < win.gBrowser.tabs.length; a++) {
+    let tabItem = win.gBrowser.tabs[a]._tabViewTabItem;
+    if (tabItem)
+      tabItems._update(win.gBrowser.tabs[a]);
+  }
+  callback();
+}
+
+// ---------
+function newWindowWithTabView(callback) {
+  let win = window.openDialog(getBrowserURL(), "_blank", 
+                              "chrome,all,dialog=no,height=800,width=800");
+  let onLoad = function() {
+    win.removeEventListener("load", onLoad, false);
+    let onShown = function() {
+      win.removeEventListener("tabviewshown", onShown, false);
+      callback(win);
+    };
+    win.addEventListener("tabviewshown", onShown, false);
+    win.TabView.toggle();
+  }
+  win.addEventListener("load", onLoad, false);
+}
+
+// ----------
+function afterAllTabsLoaded(callback, win) {
+  win = win || window;
+
   let stillToLoad = 0;
 
   function onLoad() {
     this.removeEventListener("load", onLoad, true);
-    
     stillToLoad--;
     if (!stillToLoad)
       callback();
   }
 
-  for (let a = 0; a < gBrowser.tabs.length; a++) {
-    let browser = gBrowser.tabs[a].linkedBrowser;
+  for (let a = 0; a < win.gBrowser.tabs.length; a++) {
+    let browser = win.gBrowser.tabs[a].linkedBrowser;
     if (browser.contentDocument.readyState != "complete") {
       stillToLoad++;
       browser.addEventListener("load", onLoad, true);
