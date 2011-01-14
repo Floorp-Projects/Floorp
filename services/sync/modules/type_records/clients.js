f46d33c149098007d66577be8d11b5471c84481c
Philipp von Weitershausen <philipp@weitershausen.de>
2011-01-14 13:22:20 -0800

    Bug 591103 - use TTL for history, form history, clients, tabs. r=mconnor

--- services/sync/modules/type_records/clients.js
+++ services/sync/modules/type_records/clients.js
@@ -44,12 +44,15 @@ const Cu = Components.utils;
 Cu.import("resource://services-sync/base_records/crypto.js");
 Cu.import("resource://services-sync/util.js");
 
+const CLIENTS_TTL = 1814400; // 21 days
+
 function ClientsRec(collection, id) {
   CryptoWrapper.call(this, collection, id);
 }
 ClientsRec.prototype = {
   __proto__: CryptoWrapper.prototype,
   _logName: "Record.Clients",
+  ttl: CLIENTS_TTL
 };
 
 Utils.deferGetSet(ClientsRec, "cleartext", ["name", "type", "commands"]);
