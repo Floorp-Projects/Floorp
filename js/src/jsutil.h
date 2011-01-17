ba53bef661709cad9b61e01167c03a3a48673740
Paul Biggar <pbiggar>
2011-01-17 15:15:00 -0800

    Bug 642327 - Add OOM regression checking to the shell via |make check|. (r=nnethercote)
    In a known test file, there are 129 OOM errors. This adds a script which checks
    the number of unhandled OOMs, and fails if it is not exactly 129. If we
    accidentally increase the number of unhandled OOMs, this should catch it. If we
    reduce the number of unhandled OOMs (which is the plan), then adjust this
    number in Makefile.in.

--- js/src/jsutil.h
+++ js/src/jsutil.h
@@ -209,15 +209,38 @@ JS_DumpBacktrace(JSCallsite *trace);
 
 #else
 
+#ifdef DEBUG
+/*
+ * In order to test OOM conditions, when the shell command-line option
+ * |-A NUM| is passed, we fail continuously after the NUM'th allocation.
+ */
+extern JS_PUBLIC_DATA(JSUint32) OOM_maxAllocations; /* set from shell/js.cpp */
+extern JS_PUBLIC_DATA(JSUint32) OOM_counter; /* data race, who cares. */
+#define JS_OOM_POSSIBLY_FAIL() \
+    do \
+    { \
+        if (OOM_counter++ >= OOM_maxAllocations) { \
+            return NULL; \
+        } \
+    } while (0)
+
+#else
+#define JS_OOM_POSSIBLY_FAIL() do {} while(0)
+#endif
+
+
 static JS_INLINE void* js_malloc(size_t bytes) {
+    JS_OOM_POSSIBLY_FAIL();
     return malloc(bytes);
 }
 
 static JS_INLINE void* js_calloc(size_t bytes) {
+    JS_OOM_POSSIBLY_FAIL();
     return calloc(bytes, 1);
 }
 
 static JS_INLINE void* js_realloc(void* p, size_t bytes) {
+    JS_OOM_POSSIBLY_FAIL();
     return realloc(p, bytes);
 }
 
