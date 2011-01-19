54156dc22bf5db5c1149aac5da7450cc00cddc76
Mihai Sucan <mihai.sucan@gmail.com>
2011-01-19 10:23:07 -0400

    Bug 618078 - Exception in asynchronous callback not visible in web or error console; f=rcampbell r=sdwilsh a=blocking2.0

--- toolkit/components/console/hudservice/tests/browser/browser_webconsole_netlogging.js
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_netlogging.js
@@ -20,6 +20,7 @@ const TEST_DATA_JSON_CONTENT =
   '{ id: "test JSON data", myArray: [ "foo", "bar", "baz", "biff" ] }';
 
 let lastRequest = null;
+let requestCallback = null;
 
 function test()
 {
@@ -36,6 +37,9 @@ function test()
 
     HUDService.lastFinishedRequestCallback = function(aRequest) {
       lastRequest = aRequest;
+      if (requestCallback) {
+        requestCallback();
+      }
     };
 
     executeSoon(testPageLoad);
@@ -83,7 +87,7 @@ function testPageLoadBody()
 
 function testXhrGet()
 {
-  let callback = function() {
+  requestCallback = function() {
     ok(lastRequest, "testXhrGet() was logged");
     is(lastRequest.method, "GET", "Method is correct");
     is(lastRequest.request.body, null, "No request body was sent");
@@ -91,21 +95,17 @@ function testXhrGet()
       "Response is correct");
 
     lastRequest = null;
+    requestCallback = null;
     executeSoon(testXhrPost);
   };
 
   // Start the XMLHttpRequest() GET test.
-  content.wrappedJSObject.testXhrGet(function() {
-    // Use executeSoon here as the xhr callback is invoked before the network
-    // observer detected that the request is completly done and the
-    // HUDService.lastFinishedRequest is set. executeSoon solves that problem.
-    executeSoon(callback);
-  });
+  content.wrappedJSObject.testXhrGet();
 }
 
 function testXhrPost()
 {
-  let callback = function() {
+  requestCallback = function() {
     ok(lastRequest, "testXhrPost() was logged");
     is(lastRequest.method, "POST", "Method is correct");
     is(lastRequest.request.body, "Hello world!",
@@ -114,13 +114,12 @@ function testXhrPost()
       "Response is correct");
 
     lastRequest = null;
+    requestCallback = null;
     executeSoon(testFormSubmission);
   };
 
   // Start the XMLHttpRequest() POST test.
-  content.wrappedJSObject.testXhrPost(function() {
-    executeSoon(callback);
-  });
+  content.wrappedJSObject.testXhrPost();
 }
 
 function testFormSubmission()
