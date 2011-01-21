d16e93b5c84b1620a2b0cca22378feb1ec726956
Mike Conley <mconley@mozillamessaging.com>
2011-01-21 15:03:10 -0500

    Bug 624808 - Add-ons should be grouped in list view according to their status (enabled, disabled, etc). r=Mossop a=b

--- toolkit/mozapps/extensions/test/browser/browser_sorting.js
+++ toolkit/mozapps/extensions/test/browser/browser_sorting.js
@@ -13,6 +13,7 @@ function test() {
 
   gProvider = new MockProvider();
   gProvider.createAddons([{
+    // Enabled extensions
     id: "test1@tests.mozilla.org",
     name: "Test add-on",
     description: "foo",
@@ -41,6 +42,121 @@ function test() {
     description: "foo",
     updateDate: new Date(2012, 12, 12, 00, 00, 00),
     size: 5
+  }, {
+    // Incompatible, disabled extensions
+    id: "test6@tests.mozilla.org",
+    name: "orange Add-on",
+    description: "foo",
+    updateDate: new Date(2010, 04, 02, 00, 00, 00),
+    size: 142,
+    isCompatible: false,
+    isActive: false,
+  }, {
+    id: "test7@tests.mozilla.org",
+    name: "Blue Add-on",
+    description: "foo",
+    updateDate: new Date(2010, 04, 01, 23, 59, 59),
+    size: 65,
+    isCompatible: false,
+    isActive: false,
+  }, {
+    id: "test8@tests.mozilla.org",
+    name: "Green Add-on",
+    description: "foo",
+    updateDate: new Date(2010, 04, 03, 00, 00, 01),
+    size: 125,
+    isCompatible: false,
+    isActive: false,
+  }, {
+    id: "test9@tests.mozilla.org",
+    name: "red Add-on",
+    updateDate: new Date(2011, 04, 01, 00, 00, 00),
+    description: "foo",
+    isCompatible: false,
+    isActive: false,
+  }, {
+    id: "test10@tests.mozilla.org",
+    name: "Purple Add-on",
+    description: "foo",
+    updateDate: new Date(2012, 12, 12, 00, 00, 00),
+    size: 56,
+    isCompatible: false,
+    isActive: false,
+  }, {
+    // Disabled, compatible extensions
+    id: "test11@tests.mozilla.org",
+    name: "amber Add-on",
+    description: "foo",
+    updateDate: new Date(1978, 04, 02, 00, 00, 00),
+    size: 142,
+    isActive: false,
+  }, {
+    id: "test12@tests.mozilla.org",
+    name: "Salmon Add-on",
+    description: "foo",
+    updateDate: new Date(2054, 04, 01, 23, 59, 59),
+    size: 65,
+    isActive: false,
+  }, {
+    id: "test13@tests.mozilla.org",
+    name: "rose Add-on",
+    description: "foo",
+    updateDate: new Date(2010, 04, 02, 00, 00, 01),
+    size: 125,
+    isActive: false,
+  }, {
+    id: "test14@tests.mozilla.org",
+    name: "Violet Add-on",
+    updateDate: new Date(2010, 05, 01, 00, 00, 00),
+    description: "foo",
+    isActive: false,
+  }, {
+    id: "test15@tests.mozilla.org",
+    name: "white Add-on",
+    description: "foo",
+    updateDate: new Date(2010, 04, 12, 00, 00, 00),
+    size: 56,
+    isActive: false,
+  }, {
+    // Blocked extensions
+    id: "test16@tests.mozilla.org",
+    name: "grimsby Add-on",
+    description: "foo",
+    updateDate: new Date(2010, 04, 01, 00, 00, 00),
+    size: 142,
+    isActive: false,
+    blocklistState: Ci.nsIBlocklistService.STATE_SOFTBLOCKED,
+  }, {
+    id: "test17@tests.mozilla.org",
+    name: "beamsville Add-on",
+    description: "foo",
+    updateDate: new Date(2010, 04, 8, 23, 59, 59),
+    size: 65,
+    isActive: false,
+    blocklistState: Ci.nsIBlocklistService.STATE_SOFTBLOCKED,
+  }, {
+    id: "test18@tests.mozilla.org",
+    name: "smithville Add-on",
+    description: "foo",
+    updateDate: new Date(2010, 04, 03, 00, 00, 01),
+    size: 125,
+    isActive: false,
+    blocklistState: Ci.nsIBlocklistService.STATE_OUTDATED,
+  }, {
+    id: "test19@tests.mozilla.org",
+    name: "dunnville Add-on",
+    updateDate: new Date(2010, 04, 02, 00, 00, 00),
+    description: "foo",
+    isActive: false,
+    blocklistState: Ci.nsIBlocklistService.STATE_SOFTBLOCKED,
+  }, {
+    id: "test20@tests.mozilla.org",
+    name: "silverdale Add-on",
+    description: "foo",
+    updateDate: new Date(2010, 04, 12, 00, 00, 00),
+    size: 56,
+    isActive: false,
+    blocklistState: Ci.nsIBlocklistService.STATE_BLOCKED,
   }]);
 
   open_manager("addons://list/extension", function(aWindow) {
@@ -63,7 +179,7 @@ function set_order(aSortBy, aAscending) {
     elements.push(node);
     node = node.nextSibling;
   }
-  gManagerWindow.sortElements(elements, aSortBy, aAscending);
+  gManagerWindow.sortElements(elements, ["uiState", aSortBy], aAscending);
   elements.forEach(function(aElement) {
     list.appendChild(aElement);
   });
@@ -85,12 +201,28 @@ function check_order(aExpectedOrder) {
 
 // Tests that ascending name ordering was the default
 add_test(function() {
+
   check_order([
     "test2@tests.mozilla.org",
     "test4@tests.mozilla.org",
     "test3@tests.mozilla.org",
     "test5@tests.mozilla.org",
-    "test1@tests.mozilla.org"
+    "test1@tests.mozilla.org",
+    "test7@tests.mozilla.org",
+    "test8@tests.mozilla.org",
+    "test6@tests.mozilla.org",
+    "test10@tests.mozilla.org",
+    "test9@tests.mozilla.org",
+    "test11@tests.mozilla.org",
+    "test13@tests.mozilla.org",
+    "test12@tests.mozilla.org",
+    "test14@tests.mozilla.org",
+    "test15@tests.mozilla.org",
+    "test17@tests.mozilla.org",
+    "test19@tests.mozilla.org",
+    "test16@tests.mozilla.org",
+    "test20@tests.mozilla.org",
+    "test18@tests.mozilla.org",
   ]);
 
   run_next_test();
@@ -100,12 +232,30 @@ add_test(function() {
 add_test(function() {
   set_order("updateDate", false);
 
+  // When we're ascending with updateDate, it's from newest
+  // to oldest.
+
   check_order([
     "test5@tests.mozilla.org",
     "test3@tests.mozilla.org",
     "test1@tests.mozilla.org",
     "test2@tests.mozilla.org",
-    "test4@tests.mozilla.org"
+    "test4@tests.mozilla.org",
+    "test10@tests.mozilla.org",
+    "test9@tests.mozilla.org",
+    "test8@tests.mozilla.org",
+    "test6@tests.mozilla.org",
+    "test7@tests.mozilla.org",
+    "test12@tests.mozilla.org",
+    "test14@tests.mozilla.org",
+    "test15@tests.mozilla.org",
+    "test13@tests.mozilla.org",
+    "test11@tests.mozilla.org",
+    "test20@tests.mozilla.org",
+    "test17@tests.mozilla.org",
+    "test18@tests.mozilla.org",
+    "test19@tests.mozilla.org",
+    "test16@tests.mozilla.org",
   ]);
 
   set_order("updateDate", true);
@@ -115,7 +265,22 @@ add_test(function() {
     "test2@tests.mozilla.org",
     "test1@tests.mozilla.org",
     "test3@tests.mozilla.org",
-    "test5@tests.mozilla.org"
+    "test5@tests.mozilla.org",
+    "test7@tests.mozilla.org",
+    "test6@tests.mozilla.org",
+    "test8@tests.mozilla.org",
+    "test9@tests.mozilla.org",
+    "test10@tests.mozilla.org",
+    "test11@tests.mozilla.org",
+    "test13@tests.mozilla.org",
+    "test15@tests.mozilla.org",
+    "test14@tests.mozilla.org",
+    "test12@tests.mozilla.org",
+    "test16@tests.mozilla.org",
+    "test19@tests.mozilla.org",
+    "test18@tests.mozilla.org",
+    "test17@tests.mozilla.org",
+    "test20@tests.mozilla.org",
   ]);
 
   run_next_test();
@@ -130,7 +295,22 @@ add_test(function() {
     "test4@tests.mozilla.org",
     "test3@tests.mozilla.org",
     "test5@tests.mozilla.org",
-    "test1@tests.mozilla.org"
+    "test1@tests.mozilla.org",
+    "test7@tests.mozilla.org",
+    "test8@tests.mozilla.org",
+    "test6@tests.mozilla.org",
+    "test10@tests.mozilla.org",
+    "test9@tests.mozilla.org",
+    "test11@tests.mozilla.org",
+    "test13@tests.mozilla.org",
+    "test12@tests.mozilla.org",
+    "test14@tests.mozilla.org",
+    "test15@tests.mozilla.org",
+    "test17@tests.mozilla.org",
+    "test19@tests.mozilla.org",
+    "test16@tests.mozilla.org",
+    "test20@tests.mozilla.org",
+    "test18@tests.mozilla.org",
   ]);
 
   set_order("name", false);
@@ -140,7 +320,22 @@ add_test(function() {
     "test5@tests.mozilla.org",
     "test3@tests.mozilla.org",
     "test4@tests.mozilla.org",
-    "test2@tests.mozilla.org"
+    "test2@tests.mozilla.org",
+    "test9@tests.mozilla.org",
+    "test10@tests.mozilla.org",
+    "test6@tests.mozilla.org",
+    "test8@tests.mozilla.org",
+    "test7@tests.mozilla.org",
+    "test15@tests.mozilla.org",
+    "test14@tests.mozilla.org",
+    "test12@tests.mozilla.org",
+    "test13@tests.mozilla.org",
+    "test11@tests.mozilla.org",
+    "test18@tests.mozilla.org",
+    "test20@tests.mozilla.org",
+    "test16@tests.mozilla.org",
+    "test19@tests.mozilla.org",
+    "test17@tests.mozilla.org",
   ]);
 
   run_next_test();
