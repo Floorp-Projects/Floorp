9481cb9d445db603fe4070fa98590c608614eb45
Tim Taubert <tim.taubert@gmx.de>
2011-01-25 22:13:21 +0100

    Bug 626368 - Empty space left between tabs when closing multiple pages in Panorama view [r=ian, a=beltzner]

--- browser/base/content/tabview/tabitems.js
+++ browser/base/content/tabview/tabitems.js
@@ -188,7 +188,7 @@ function TabItem(tab, options) {
       return;
 
     // press close button or middle mouse click
-    if (iQ(e.target).hasClass("close") || e.button == 1) {
+    if (iQ(e.target).hasClass("close") || Utils.isMiddleClick(e)) {
       self.close();
     } else {
       if (!Items.item(this).isDragging)
