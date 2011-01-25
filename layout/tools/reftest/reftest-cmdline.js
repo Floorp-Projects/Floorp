59f4f09fc122b50c7fc31efd146fe6b7a16f10f5
L. David Baron <dbaron@dbaron.org>
2011-01-24 16:23:08 -0800

    By default, disable MozAfterPaint for content.  (Bug 608030)  r=roc  a=blocking2.0:betaN+

--- layout/tools/reftest/reftest-cmdline.js
+++ layout/tools/reftest/reftest-cmdline.js
@@ -107,6 +107,7 @@ RefTestCmdLineHandler.prototype =
     branch.setBoolPref("gfx.color_management.force_srgb", true);
     branch.setBoolPref("browser.dom.window.dump.enabled", true);
     branch.setIntPref("ui.caretBlinkTime", -1);
+    branch.setBoolPref("dom.send_after_paint_to_content", true);
     // no slow script dialogs
     branch.setIntPref("dom.max_script_run_time", 0);
     branch.setIntPref("dom.max_chrome_script_run_time", 0);
