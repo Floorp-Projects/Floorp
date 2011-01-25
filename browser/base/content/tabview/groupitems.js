c65c7f0e030abd8db3b57c7b62ea844c3af4a47c
Tim Taubert <tim.taubert@gmx.de>
2011-01-25 22:13:21 +0100

    Bug 626368 - Empty space left between tabs when closing multiple pages in Panorama view [r=ian, a=beltzner]

--- browser/base/content/tabview/groupitems.js
+++ browser/base/content/tabview/groupitems.js
@@ -206,7 +206,7 @@ function GroupItem(listOfEls, options) {
   else {
     this.$titleShield
       .mousedown(function(e) {
-        self.lastMouseDownTarget = (Utils.isRightClick(e) ? null : e.target);
+        self.lastMouseDownTarget = (Utils.isLeftClick(e) ? e.target : null);
       })
       .mouseup(function(e) {
         var same = (e.target == self.lastMouseDownTarget);
@@ -1011,7 +1011,7 @@ GroupItem.prototype = Utils.extend(new Item(), new Subscribable(), {
       .data("xulTab", xulTab)
       .appendTo(this.$appTabTray)
       .click(function(event) {
-        if (Utils.isRightClick(event))
+        if (!Utils.isLeftClick(event))
           return;
 
         GroupItems.setActiveGroupItem(self);
