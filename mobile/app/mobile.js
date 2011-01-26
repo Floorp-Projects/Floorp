a66f6965bd4c54f1986b96c51dff2581099b1f1d
Gervase Markham <gerv@mozilla.org>
2011-01-26 12:08:00 -0500

    Bug 624345 - "Fennec's About box should link to about:credits" [r=mark.finkle a-2.0=blassey.bugs]

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -444,7 +444,7 @@ pref("app.sync.tutorialURL", "https://support.mozilla.com/kb/sync-firefox-betwee
 pref("app.support.baseURL", "http://support.mozilla.com/mobile");
 pref("app.feedbackURL", "http://input.mozilla.com/feedback/");
 pref("app.privacyURL", "http://www.mozilla.com/%LOCALE%/m/privacy.html");
-pref("app.creditsURL", "http://www.mozilla.com/%LOCALE%/mobile/credits/");
+pref("app.creditsURL", "http://www.mozilla.org/credits/");
 #if MOZ_UPDATE_CHANNEL == beta
 pref("app.featuresURL", "http://www.mozilla.com/%LOCALE%/mobile/beta/features/");
 pref("app.faqURL", "http://www.mozilla.com/%LOCALE%/mobile/beta/faq/");
