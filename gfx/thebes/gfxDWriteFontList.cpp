418c7555ca2cbae98c15d7f5e446dbf84a3b0a48
John Daggett <jdaggett@mozilla.com>
2011-01-28 09:40:06 +0900

    Bug 628152. Fix crash in BuildFontList and revert lazy metrics initialization in gfxDWriteFont. r=jkew, a=blocker

--- gfx/thebes/gfxDWriteFontList.cpp
+++ gfx/thebes/gfxDWriteFontList.cpp
@@ -645,6 +645,8 @@ gfxDWriteFontList::InitFontList()
 {
     LOGREGISTRY(L"InitFontList start");
 
+    mInitialized = PR_FALSE;
+
 #ifdef PR_LOGGING
     LARGE_INTEGER frequency;        // ticks per second
     LARGE_INTEGER t1, t2, t3;           // ticks
