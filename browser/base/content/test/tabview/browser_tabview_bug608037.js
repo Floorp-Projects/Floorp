3ed20c245bfe688c91dd65d774e5d61f475cca25
Tim Taubert <tim.taubert@gmx.de>
2011-01-28 21:23:05 +0100

    Bug 629233 - Intermittent 'TEST-UNEXPECTED-FAIL | chrome://mochitests/content/browser/browser/base/content/test/tabview/browser_tabview_bug608037.js | Test timed out' during OSX 10.6 opt Moth [r=ian, a=test]

--- browser/base/content/test/tabview/browser_tabview_bug608037.js
+++ browser/base/content/test/tabview/browser_tabview_bug608037.js
@@ -1,39 +1,5 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is a test for bug 608037.
- *
- * The Initial Developer of the Original Code is
- * Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2010
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- * Raymond Lee <raymond@appcoast.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
 
 let tabOne;
 let tabTwo;
@@ -44,36 +10,16 @@ function test() {
   tabOne = gBrowser.addTab("http://mochi.test:8888/");
   tabTwo = gBrowser.addTab("http://mochi.test:8888/browser/browser/base/content/test/tabview/dummy_page.html");
 
-  // make sure our tabs are loaded so their titles are right
-  let stillToLoad = 0;
-  let onLoad = function(event) {
-    event.target.removeEventListener("load", onLoad, true);
-
-    stillToLoad--;
-    if (!stillToLoad) {
-      // show the tab view
-      window.addEventListener("tabviewshown", onTabViewWindowLoaded, false);
-      ok(!TabView.isVisible(), "Tab View is hidden");
-
-      // make sure the tab one is selected because undoCloseTab() would remove
-      // the selected tab if it's a blank tab.
-      gBrowser.selectedTab = tabOne;
-
-      TabView.toggle();
-    }
-  }
-
-  let newTabs = [ tabOne, tabTwo ];
-  newTabs.forEach(function(tab) {
-    stillToLoad++; 
-    tab.linkedBrowser.addEventListener("load", onLoad, true);
+  afterAllTabsLoaded(function () {
+    // make sure the tab one is selected because undoCloseTab() would remove
+    // the selected tab if it's a blank tab.
+    gBrowser.selectedTab = tabOne;
+    showTabView(onTabViewWindowLoaded);
   });
 }
 
 function onTabViewWindowLoaded() {
-  window.removeEventListener("tabviewshown", onTabViewWindowLoaded, false);
-
-  let contentWindow = document.getElementById("tab-view").contentWindow;
+  let contentWindow = TabView.getContentWindow();
   let groupItems = contentWindow.GroupItems.groupItems;
   is(groupItems.length, 1, "There is only one group");
   is(groupItems[0].getChildren().length, 3, "The group has three tab items");
@@ -83,21 +29,30 @@ function onTabViewWindowLoaded() {
   is(groupItems[0].getChildren().length, 2, "The group has two tab items");
 
   tabTwo = undoCloseTab(0);
-  tabTwo._tabViewTabItem.addSubscriber(tabTwo, "reconnected", function() {
-    tabTwo._tabViewTabItem.removeSubscriber(tabTwo, "reconnected");
-
+  whenTabIsReconnected(tabTwo, function() {
     ok(TabView.isVisible(), "Tab View is still visible after restoring a tab");
     is(groupItems[0].getChildren().length, 3, "The group still has three tab items");
-  
+
     // clean up and finish
-    let endGame = function() {
-      window.removeEventListener("tabviewhidden", endGame, false);
-  
+    hideTabView(function () {
       gBrowser.removeTab(tabOne);
       gBrowser.removeTab(tabTwo);
       finish();
-    }
-    window.addEventListener("tabviewhidden", endGame, false);
-    TabView.toggle();
+    });
+  });
+}
+
+// ----------
+function whenTabIsReconnected(tab, callback) {
+  let tabItem = tab._tabViewTabItem;
+
+  if (tabItem._reconnected) {
+    callback();
+    return;
+  }
+
+  tabItem.addSubscriber(tabItem, "reconnected", function () {
+    tabItem.removeSubscriber(tabItem, "reconnected");
+    callback();
   });
 }
