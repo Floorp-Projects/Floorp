9b446d33f3d416d6f4bad629cbc47243fe02d632
Jason Orendorff <jorendorff@mozilla.com>
2011-02-01 15:48:46 -0600

    Add comments explaining how we cope with clobbering deep bails, and fix a small bug in one of the cases. Bug 627692, r=dvander.
    --HG--
    extra : rebase_source : af24852232d272776e828c740c041c217f6cecc8

---
+++ js/src/jit-test/tests/basic/bug627692-1.js
@@ -0,0 +1,11 @@
+N = typeof w == "";
+t = e = false;
+(function() {
+    __defineGetter__("",
+                     function(h) {
+                         for (var y in this) {}
+                     });
+    do {
+        for each(var e in this) {}
+    } while ( function () {})
+})();
