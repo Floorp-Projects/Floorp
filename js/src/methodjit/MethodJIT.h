85fe5f038c2bcba8cbf8b6990c104a25cafebc67
David Anderson <danderson@mozilla.com>
2011-02-01 12:04:07 -0800

    Fix incoherent PC in FixupArity failure case (bug 629650, r=luke).

--- js/src/methodjit/MethodJIT.h
+++ js/src/methodjit/MethodJIT.h
@@ -361,6 +361,8 @@ struct JITScript {
     size_t scriptDataSize();
 
     size_t mainCodeSize() { return code.m_size; } /* doesn't account for fragmentation */
+
+    jsbytecode *nativeToPC(void *returnAddress) const;
 };
 
 /*
