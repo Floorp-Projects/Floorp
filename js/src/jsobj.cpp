3f455cce324050d74281b7e6d766b5461c8b83ee
Bill McCloskey <wmccloskey@mozilla.com>
2011-02-02 14:20:06 -0800

    Backed out bug 609104 (csets ac3ba38f20c0 and 7717cabaf20c) (orange)

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -3254,7 +3254,7 @@ js_NewWithObject(JSContext *cx, JSObject *proto, JSObject *parent, jsint depth)
     JSStackFrame *priv = js_FloatingFrameIfGenerator(cx, cx->fp());
 
     obj->init(cx, &js_WithClass, proto, parent, priv, false);
-    obj->setMap(cx->compartment->emptyWithShape);
+    obj->setMap(cx->runtime->emptyWithShape);
     OBJ_SET_BLOCK_DEPTH(cx, obj, depth);
 
     AutoObjectRooter tvr(cx, obj);
@@ -3280,7 +3280,7 @@ js_NewBlockObject(JSContext *cx)
         return NULL;
 
     blockObj->init(cx, &js_BlockClass, NULL, NULL, NULL, false);
-    blockObj->setMap(cx->compartment->emptyBlockShape);
+    blockObj->setMap(cx->runtime->emptyBlockShape);
     return blockObj;
 }
 
@@ -4729,7 +4729,7 @@ js_DefineNativeProperty(JSContext *cx, JSObject *obj, jsid id, const Value &valu
      * member declaration.
      */
     if (obj->isDelegate() && (attrs & (JSPROP_READONLY | JSPROP_SETTER)))
-        cx->runtime->protoHazardShape = js_GenerateShape(cx);
+        cx->runtime->protoHazardShape = js_GenerateShape(cx, false);
 
     /* Use the object's class getter and setter by default. */
     Class *clasp = obj->getClass();
@@ -6538,7 +6538,7 @@ js_TraceObject(JSTracer *trc, JSObject *obj)
     }
     if (clasp->flags & JSCLASS_IS_GLOBAL) {
         JSCompartment *compartment = obj->getCompartment();
-        compartment->mark(trc);
+        compartment->marked = true;
     }
 
     /*
