24c4accfb36eb7266cdc9df204a3b64cbb284c5f
Daniel Holbert <dholbert@cs.stanford.edu>
2011-02-04 11:19:27 -0800

    Bug 631300: Synchronize the "do not track" pref w/ Firefox Sync. r=philikon a=jst

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1026,6 +1026,7 @@ pref("services.sync.prefs.sync.privacy.clearOnShutdown.offlineApps", true);
 pref("services.sync.prefs.sync.privacy.clearOnShutdown.passwords", true);
 pref("services.sync.prefs.sync.privacy.clearOnShutdown.sessions", true);
 pref("services.sync.prefs.sync.privacy.clearOnShutdown.siteSettings", true);
+pref("services.sync.prefs.sync.privacy.donottrackheader.enabled", true);
 pref("services.sync.prefs.sync.privacy.sanitize.sanitizeOnShutdown", true);
 pref("services.sync.prefs.sync.security.OCSP.disable_button.managecrl", true);
 pref("services.sync.prefs.sync.security.OCSP.enabled", true);
