2a016cd17de44cf78c665638b8deeb4fe560437f
Reed Loden <reed@reedloden.com>
2011-02-04 15:51:36 -0800

    Bug 631497 - "Restore http to link target overlay" [r=dao ui-r=beltzner a=blocking2.0:betaN+]

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4152,8 +4152,7 @@ var XULBrowserWindow = {
     // Encode bidirectional formatting characters.
     // (RFC 3987 sections 3.2 and 4.1 paragraph 6)
     this.overLink = url.replace(/[\u200e\u200f\u202a\u202b\u202c\u202d\u202e]/g,
-                                encodeURIComponent)
-                       .replace(/^http:\/\//, "");
+                                encodeURIComponent);
     LinkTargetDisplay.update();
   },
 
