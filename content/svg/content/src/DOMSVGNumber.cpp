7b15b63e36ec46eb553629970963cab1c76a2605
Jonathan Watt <jwatt@jwatt.org>
2011-02-08 13:43:34 +0000

    Bug 631946 - SVG DOM lists should limit the number of items to the number they can index. r=roc, a=roc.

--- content/svg/content/src/DOMSVGNumber.cpp
+++ content/svg/content/src/DOMSVGNumber.cpp
@@ -87,7 +87,7 @@ DOMSVGNumber::DOMSVGNumber(DOMSVGNumberList *aList,
   // These shifts are in sync with the members in the header.
   NS_ABORT_IF_FALSE(aList &&
                     aAttrEnum < (1 << 4) &&
-                    aListIndex < (1 << 27) &&
+                    aListIndex <= MaxListIndex() &&
                     aIsAnimValItem < (1 << 1), "bad arg");
 
   NS_ABORT_IF_FALSE(IndexIsValid(), "Bad index for DOMSVGNumber!");
