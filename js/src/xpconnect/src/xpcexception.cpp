787ac1bb427a0c08922c975ef4cb2d81f7a58d27
Dan Witte <dwitte@mozilla.com>
2011-02-08 14:16:07 -0800

    Bug 508167 - NPAPI additions for clearing recent history (e.g. for "flash cookies"). r=josh, a=beltzner

--- js/src/xpconnect/src/xpcexception.cpp
+++ js/src/xpconnect/src/xpcexception.cpp
@@ -44,6 +44,7 @@
 #include "xpcprivate.h"
 #include "nsNetError.h"
 #include "mozStorage.h"
+#include "nsPluginError.h"
 
 /***************************************************************************/
 /* Quick and dirty mapping of well known result codes to strings. We only
