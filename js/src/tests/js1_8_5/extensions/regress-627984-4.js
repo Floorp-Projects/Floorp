0b708f23e344444103fe8f5b708f6182268952e3
Jason Orendorff <jorendorff@mozilla.com>
2011-02-08 16:09:33 -0600

    Bug 627984 - Tighten up assertions in JSObject::methodReadBarrier. r=brendan.

---
+++ js/src/tests/js1_8_5/extensions/regress-627984-4.js
@@ -0,0 +1,16 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/licenses/publicdomain/
+
+// See bug 627984, comment 17, item 3.
+var obj = {};
+obj.watch("m", function (id, oldval, newval) {
+        delete obj.m;
+        obj.m = function () {};
+        dumpObject(obj);
+        return newval;
+    });
+delete obj.m;
+obj.m = 1;
+assertEq(obj.m, 1);
+
+reportCompare(0, 0, 'ok');
