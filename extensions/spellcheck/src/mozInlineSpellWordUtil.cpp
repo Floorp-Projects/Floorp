b2561b180af94e4c9c3b432a822283bb1c387937
Ehsan Akhgari <ehsan@mozilla.com>
2011-02-08 21:50:51 -0500

    Bug 355178 - Don't break words on hyphens before passing them to hunspell, as it can handle word breaking on hyphens on its own; r=smaug f=Pike a=beltzner

--- extensions/spellcheck/src/mozInlineSpellWordUtil.cpp
+++ extensions/spellcheck/src/mozInlineSpellWordUtil.cpp
@@ -899,12 +899,26 @@ WordSplitState::ClassifyCharacter(PRInt32 aIndex, PRBool aRecurse) const
     return CHAR_CLASS_WORD;
   }
 
+  // The dot character, if appearing at the end of a word, should
+  // be considered part of that word.  Example: "etc.", or
+  // abbreviations
+  if (aIndex > 0 &&
+      mDOMWordText[aIndex] == '.' &&
+      mDOMWordText[aIndex - 1] != '.' &&
+      ClassifyCharacter(aIndex - 1, false) != CHAR_CLASS_WORD) {
+    return CHAR_CLASS_WORD;
+  }
+
   // all other punctuation
   if (charCategory == nsIUGenCategory::kSeparator ||
       charCategory == nsIUGenCategory::kOther ||
       charCategory == nsIUGenCategory::kPunctuation ||
-      charCategory == nsIUGenCategory::kSymbol)
-    return CHAR_CLASS_SEPARATOR;
+      charCategory == nsIUGenCategory::kSymbol) {
+    // Don't break on hyphens, as hunspell handles them on its own.
+    if (mDOMWordText[aIndex] != '-') {
+      return CHAR_CLASS_SEPARATOR;
+    }
+  }
 
   // any other character counts as a word
   return CHAR_CLASS_WORD;
