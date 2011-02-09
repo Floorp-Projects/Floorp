bf20c145265baf22c4a5d82f9b57094af5942b11
Chris Jones <jones.chris.g@gmail.com>
2011-02-08 18:44:14 -0600

    Bug 626602, part 9: When possible, copy from a background to an opaque surface and have transparent plugins draw directly on the copied background, instead of doing alpha recovery or hoping plugins give us alpha values. r=bsmedberg,karlt sr=roc

--- dom/plugins/PluginModuleChild.h
+++ dom/plugins/PluginModuleChild.h
@@ -246,9 +246,6 @@ public:
         // results so mouse input works when flash is displaying it's settings
         // window.
         QUIRK_FLASH_HOOK_GETWINDOWINFO                  = 1 << 5,
-        // Win: Flash trashes the alpha channel in our buffers when cleartype
-        // is enabled. Mask this setting so they don't know it's enabled.
-        QUIRK_FLASH_MASK_CLEARTYPE_SETTINGS             = 1 << 6,
     };
 
     int GetQuirks() { return mQuirks; }
