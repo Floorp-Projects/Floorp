a2afd142f5ce8c8fa706a19a7f7268659d36d216
Brian Birtles <birtles@gmail.com>
2011-02-11 11:15:18 +0900

    Backing out bug 628888, changeset e3a645b56973 and changeset 4327761b6350 due to failing test, a=bustage

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -883,17 +883,6 @@ TransferZoomLevels(nsIDocument* aFromDoc,
   toCtxt->SetTextZoom(fromCtxt->TextZoom());
 }
 
-void
-TransferShowingState(nsIDocument* aFromDoc, nsIDocument* aToDoc)
-{
-  NS_ABORT_IF_FALSE(aFromDoc && aToDoc,
-                    "transferring showing state from/to null doc");
-
-  if (aFromDoc->IsShowing()) {
-    aToDoc->OnPageShow(PR_TRUE, nsnull);
-  }
-}
-
 nsresult
 nsExternalResourceMap::AddExternalResource(nsIURI* aURI,
                                            nsIDocumentViewer* aViewer,
@@ -953,7 +942,6 @@ nsExternalResourceMap::AddExternalResource(nsIURI* aURI,
     newResource->mLoadGroup = aLoadGroup;
     if (doc) {
       TransferZoomLevels(aDisplayDocument, doc);
-      TransferShowingState(aDisplayDocument, doc);
     }
   }
 
