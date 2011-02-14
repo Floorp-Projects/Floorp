2bd64f1f8abb98b5b1d6bc111aa34daf6035c912
Bas Schouten <bschouten@mozilla.com>
2011-02-15 00:47:30 +0100

    Bug 629866 - Part 9: Improve ImageLayerD3D9 uploading code. r+a=roc

--- gfx/layers/d3d9/ImageLayerD3D9.cpp
+++ gfx/layers/d3d9/ImageLayerD3D9.cpp
@@ -53,13 +53,18 @@ SurfaceToTexture(IDirect3DDevice9 *aDevice,
                  gfxASurface *aSurface,
                  const gfxIntSize &aSize)
 {
-  nsRefPtr<gfxImageSurface> imageSurface =
-    new gfxImageSurface(aSize, gfxASurface::ImageFormatARGB32);
 
-  nsRefPtr<gfxContext> context = new gfxContext(imageSurface);
+  nsRefPtr<gfxImageSurface> imageSurface = aSurface->GetAsImageSurface();
 
-  context->SetSource(aSurface);
-  context->Paint();
+  if (!imageSurface) {
+    imageSurface = new gfxImageSurface(aSize,
+                                       gfxASurface::ImageFormatARGB32);
+    
+    nsRefPtr<gfxContext> context = new gfxContext(imageSurface);
+    context->SetSource(aSurface);
+    context->SetOperator(gfxContext::OPERATOR_SOURCE);
+    context->Paint();
+  }
 
   nsRefPtr<IDirect3DTexture9> texture;
   nsRefPtr<IDirect3DDevice9Ex> deviceEx;
