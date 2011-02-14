31e9aaba7fabbbf0f8a623a443c56db707db09a2
Ginn Chen <ginn.chen@oracle.com>
2011-02-14 14:44:55 +0800

    Bug 596338 Undefined symbol js::JMCheckLogging on PPC and SPARC debug build with no methodjit r=dvander

--- js/src/methodjit/Logging.h
+++ js/src/methodjit/Logging.h
@@ -38,7 +38,7 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#if !defined jsjaeger_logging_h__
+#if !defined jsjaeger_logging_h__ && defined JS_METHODJIT
 #define jsjaeger_logging_h__
 
 #include "prmjtime.h"
