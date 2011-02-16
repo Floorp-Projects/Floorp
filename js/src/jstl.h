a9887a5e059061a320dcf0493ce0b3c518ed0ee5
Chris Leary <cdleary@mozilla.com>
2011-02-16 13:33:08 -0800

    Backed out changeset 62a979cc89a2 due to risk (bug 614155).

--- js/src/jstl.h
+++ js/src/jstl.h
@@ -311,9 +311,6 @@ class LazilyConstructed
 
     T &asT() { return *storage.addr(); }
 
-    LazilyConstructed operator=(const LazilyConstructed &other);
-    explicit LazilyConstructed(const LazilyConstructed &other);
-
   public:
     LazilyConstructed() { constructed = false; }
     ~LazilyConstructed() { if (constructed) asT().~T(); }
@@ -367,11 +364,6 @@ class LazilyConstructed
     void destroy() {
         ref().~T();
         constructed = false;
-    }
-
-    void destroyIfConstructed() {
-        if (!empty())
-            destroy();
     }
 };
 
