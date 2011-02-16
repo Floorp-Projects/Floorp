580149d472251a3689c5b9f6181619741d6bb275
Daniel Holbert <dholbert@cs.stanford.edu>
2011-02-15 23:54:37 -0800

    Bug 632143 patch 3: Update animVal after modifying baseVal length, for DOMSVGPathSegList. r=roc a=blocking-final+

--- content/svg/content/src/DOMSVGPathSegList.h
+++ content/svg/content/src/DOMSVGPathSegList.h
@@ -190,6 +190,12 @@ private:
   // aIndex, if it doesn't already exist.
   void EnsureItemAt(PRUint32 aIndex);
 
+  void MaybeInsertNullInAnimValListAt(PRUint32 aIndex,
+                                      PRUint32 aInternalIndex,
+                                      PRUint32 aArgCountForItem);
+  void MaybeRemoveItemFromAnimValListAt(PRUint32 aIndex,
+                                        PRUint32 aArgCountForItem);
+
   // Calls UpdateListIndex on all elements in |mItems| that satisfy ItemAt(),
   // from |aStartingIndex| to the end of |mItems|.  Also adjusts
   // |mItems.mInternalDataIndex| by the requested amount.
