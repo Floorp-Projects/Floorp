59514e938829fa41c84b54218143e3318242c735
Chris Jones <jones.chris.g@gmail.com>
2011-02-16 16:43:31 -0600

    Bug 626602, part 8.1: Close the XDisplay to free the gfxXlibSurface DisplayTable, for the sake of leak checkers. r=karlt

--- dom/plugins/PluginModuleChild.h
+++ dom/plugins/PluginModuleChild.h
@@ -264,6 +264,7 @@ public:
 private:
     void InitQuirksModes(const nsCString& aMimeType);
     bool InitGraphics();
+    void DeinitGraphics();
 #if defined(MOZ_WIDGET_GTK2)
     static gboolean DetectNestedEventLoop(gpointer data);
     static gboolean ProcessBrowserEvents(gpointer data);
