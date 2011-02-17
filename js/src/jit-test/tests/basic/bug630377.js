7032e92ee5b50961fb9fcc6eae47ff34ab2678f5
Jason Orendorff <jorendorff@mozilla.com>
2011-02-17 16:10:10 -0600

    Remove bogus assertions in the tracer and in ArrayCompPushImpl helper function. Bug 630377, r=brendan.
    --HG--
    extra : rebase_source : 1ed9f873b630f7078f275846d95fbd68b4daeabe

---
+++ js/src/jit-test/tests/basic/bug630377.js
@@ -0,0 +1,7 @@
+var a = [];
+for (var i = 0; i < RUNLOOP; i++)
+    a[i] = 0;
+var b = #1=[x === "0" && (#1#.slow = 1) for (x in a)];
+assertEq(b[0], 1);
+for (var i = 1; i < RUNLOOP; i++)
+    assertEq(b[i], false);
