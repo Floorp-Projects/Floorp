72b24c5e258f3f08fac4be593d7bb814cffdbc59
Jan de Mooij <jandemooij@gmail.com>
2011-02-17 14:17:24 -0800

    Bug 633741 - Check result of defineProperty in js_InitFunctionAndObjectClasses.  r=jwalden, a=dmandelin
    --HG--
    extra : rebase_source : 25e7e6dce22a5f5eb57a7954bcc39415050b5595

---
+++ js/src/tests/js1_8_5/regress/regress-633741.js
@@ -0,0 +1,17 @@
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/licenses/publicdomain/
+ * Contributors: Jan de Mooij
+ */
+
+Object.preventExtensions(this);
+delete Function;
+
+try {
+    /* Don't assert. */
+    Object.getOwnPropertyNames(this);
+} catch(e) {
+    reportCompare(true, false, "this shouldn't have thrown");
+}
+reportCompare(0, 0, "ok");
+
