44fc552b8c53b7090ca7aa42f56ee578c8c9fc2c
Mark Finkle <mfinkle@mozilla.com>
2011-02-16 23:34:21 -0500

    Bug 633769 - Add final parts for the Firefox Spark promotion (part 1) [r=vingtetun]

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -446,7 +446,7 @@ pref("app.faqURL", "http://www.mozilla.com/%LOCALE%/mobile/beta/faq/");
 pref("app.featuresURL", "http://www.mozilla.com/%LOCALE%/mobile/features/");
 pref("app.faqURL", "http://www.mozilla.com/%LOCALE%/mobile/faq/");
 #endif
-pref("app.promo.spark.endDate", "2011-01-01");
+pref("app.promo.spark.endDate", "2011-03-01");
 
 // Name of alternate about: page for certificate errors (when undefined, defaults to about:neterror)
 pref("security.alternate_certificate_error_page", "certerror");
