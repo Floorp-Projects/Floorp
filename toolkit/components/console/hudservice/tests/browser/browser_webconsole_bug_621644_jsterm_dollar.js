87a5f3aa3110ff2a698eb73a2c011a4147ae7880
Mihai Sucan <mihai.sucan@gmail.com>
2011-02-18 09:09:11 -0800

    Bug 621644 - $ is shadowed in web console; f=rcampbell r=dolske approval2.0=dolske

---
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_bug_621644_jsterm_dollar.js
@@ -0,0 +1,48 @@
+/* vim:set ts=2 sw=2 sts=2 et: */
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * Contributor(s):
+ *   Mihai Sucan <mihai.sucan@gmail.com>
+ */
+
+const TEST_URI = "http://example.com/browser/toolkit/components/console/hudservice/tests/browser/test-bug-621644-jsterm-dollar.html";
+
+function tabLoad(aEvent) {
+  browser.removeEventListener(aEvent.type, arguments.callee, true);
+
+  waitForFocus(function () {
+    openConsole();
+
+    let hudId = HUDService.getHudIdByWindow(content);
+    let HUD = HUDService.hudReferences[hudId];
+
+    HUD.jsterm.clearOutput();
+
+    HUD.jsterm.setInputValue("$(document.body)");
+    HUD.jsterm.execute();
+
+    let outputItem = HUD.outputNode.
+                     querySelector(".webconsole-msg-output:last-child");
+    ok(outputItem.textContent.indexOf("<p>") > -1,
+       "jsterm output is correct for $()");
+
+    HUD.jsterm.clearOutput();
+
+    HUD.jsterm.setInputValue("$$(document)");
+    HUD.jsterm.execute();
+
+    outputItem = HUD.outputNode.
+                     querySelector(".webconsole-msg-output:last-child");
+    ok(outputItem.textContent.indexOf("621644") > -1,
+       "jsterm output is correct for $$()");
+
+    executeSoon(finishTest);
+  }, content);
+}
+
+function test() {
+  addTab(TEST_URI);
+  browser.addEventListener("load", tabLoad, true);
+}
