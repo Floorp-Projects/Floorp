70ed6d9fcd8d55c3bb0b1826691c7715c260cabb
Jeff Walden <jwalden@mit.edu>
2011-02-20 21:40:06 -0800

    Back out af1e10bea0f4 from a CLOSED TREE for being orangetastic on one platform.

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -2178,30 +2178,6 @@ ScriptPrologue(JSContext *cx, JSStackFrame *fp)
     return true;
 }
 
-static inline bool
-SlowThis(JSContext *cx, JSObject *obj, const Value &funval, Value *vp)
-{
-    if (!funval.isObject() ||
-        ((obj->isGlobal() || IsCacheableNonGlobalScope(obj)) &&
-         IsSafeForLazyThisCoercion(cx, &funval.toObject()))) {
-        /*
-         * We can avoid computing 'this' eagerly and push the implicit 'this'
-         * value (undefined), as long the scope is cachable and we are not
-         * crossing into another scope (in which case lazy calculation of 'this'
-         * would pick up the new and incorrect scope). 'strict' functions are an
-         * exception. We don't want to eagerly calculate 'this' for them even if
-         * the callee is in a different scope.
-         */
-        *vp = UndefinedValue();
-        return true;
-    }
-
-    if (!(obj = obj->thisObject(cx)))
-        return false;
-    *vp = ObjectValue(*obj);
-    return true;
-}
-
 namespace js {
 
 JS_REQUIRES_STACK JS_NEVER_INLINE bool
@@ -2227,8 +2203,8 @@ Interpret(JSContext *cx, JSStackFrame *entryFrame, uintN inlineCallCount, JSInte
      * expect from looking at the code.  (We do omit POPs after SETs;
      * unfortunate, but not worth fixing.)
      */
-#  define LOG_OPCODE(OP)    JS_BEGIN_MACRO                                    \
-                                if (JS_UNLIKELY(cx->logfp != NULL) &&         \
+#  define LOG_OPCODE(OP)    JS_BEGIN_MACRO                                      \
+                                if (JS_UNLIKELY(cx->logfp != NULL) &&       \
                                     (OP) == *regs.pc)                         \
                                     js_LogOpcode(cx);                         \
                             JS_END_MACRO
@@ -4816,13 +4792,26 @@ BEGIN_CASE(JSOP_SETCALL)
 }
 END_CASE(JSOP_SETCALL)
 
-#define SLOW_PUSH_THISV(cx, obj, funval)                                      \
-    JS_BEGIN_MACRO                                                            \
-        Value v;                                                              \
-        if (!SlowThis(cx, obj, funval, &v))                                   \
-            goto error;                                                       \
-        PUSH_COPY(v);                                                         \
-    JS_END_MACRO                                                              \
+#define SLOW_PUSH_THISV(cx, obj)                                            \
+    JS_BEGIN_MACRO                                                          \
+        Class *clasp;                                                       \
+        JSObject *thisp = obj;                                              \
+        if (!thisp->getParent() ||                                          \
+            (clasp = thisp->getClass()) == &js_CallClass ||                 \
+            clasp == &js_BlockClass ||                                      \
+            clasp == &js_DeclEnvClass) {                                    \
+            /* Push the ImplicitThisValue for the Environment Record */     \
+            /* associated with obj. See ES5 sections 10.2.1.1.6 and  */     \
+            /* 10.2.1.2.6 (ImplicitThisValue) and section 11.2.3     */     \
+            /* (Function Calls). */                                         \
+            PUSH_UNDEFINED();                                               \
+        } else {                                                            \
+            thisp = thisp->thisObject(cx);                                  \
+            if (!thisp)                                                     \
+                goto error;                                                 \
+            PUSH_OBJECT(*thisp);                                            \
+        }                                                                   \
+    JS_END_MACRO
 
 BEGIN_CASE(JSOP_GETGNAME)
 BEGIN_CASE(JSOP_CALLGNAME)
@@ -4852,17 +4841,20 @@ BEGIN_CASE(JSOP_CALLNAME)
             PUSH_COPY(rval);
         }
 
-        JS_ASSERT(obj->isGlobal() || IsCacheableNonGlobalScope(obj));
-        if (op == JSOP_CALLNAME || op == JSOP_CALLGNAME) {
-            if (regs.sp[-1].isObject() &&
-                !IsSafeForLazyThisCoercion(cx, &regs.sp[-1].toObject())) {
-                if (!(obj = obj->thisObject(cx)))
-                    return false;
-                PUSH_OBJECT(*obj);
-            } else {
-                PUSH_UNDEFINED();
-            }
-        }
+        /*
+         * Push results, the same as below, but with a prop$ hit there
+         * is no need to test for the unusual and uncacheable case where
+         * the caller determines |this|.
+         */
+#if DEBUG
+        Class *clasp;
+        JS_ASSERT(!obj->getParent() ||
+                  (clasp = obj->getClass()) == &js_CallClass ||
+                  clasp == &js_BlockClass ||
+                  clasp == &js_DeclEnvClass);
+#endif
+        if (op == JSOP_CALLNAME || op == JSOP_CALLGNAME)
+            PUSH_UNDEFINED();
         len = JSOP_NAME_LENGTH;
         DO_NEXT_OP(len);
     }
@@ -4900,7 +4892,7 @@ BEGIN_CASE(JSOP_CALLNAME)
 
     /* obj must be on the scope chain, thus not a function. */
     if (op == JSOP_CALLNAME || op == JSOP_CALLGNAME)
-        SLOW_PUSH_THISV(cx, obj, rval);
+        SLOW_PUSH_THISV(cx, obj);
 }
 END_CASE(JSOP_NAME)
 
@@ -6403,7 +6395,7 @@ BEGIN_CASE(JSOP_XMLNAME)
         goto error;
     regs.sp[-1] = rval;
     if (op == JSOP_CALLXMLNAME)
-        SLOW_PUSH_THISV(cx, obj, rval);
+        SLOW_PUSH_THISV(cx, obj);
 }
 END_CASE(JSOP_XMLNAME)
 
