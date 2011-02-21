097210b32e9a633b13dc7dfb7cb03b2b5dec784a
Jeff Walden <jwalden@mit.edu>
2011-02-20 21:38:29 -0800

    Back out a8574322a3ba from a CLOSED TREE, for its predecessor is orangetastic on one platform.

--- js/src/jit-test/tests/basic/testBug634590.js
+++ js/src/jit-test/tests/basic/testBug634590.js
@@ -3,10 +3,5 @@ var sb = evalcx('');
 sb.name = "inner";
 sb.parent = this;
 function f() { return this.name; }
-assertEq(evalcx('this.f = parent.f;\n' +
-                'var s = "";\n' +
-                'for (i = 0; i < 10; ++i)\n' +
-                '  s += f();\n' +
-                's',
-                sb),
+assertEq(evalcx('this.f = parent.f; var s = ""; for (i = 0; i < 10; ++i) s += f(); s', sb),
 	 "innerinnerinnerinnerinnerinnerinnerinnerinnerinner");
