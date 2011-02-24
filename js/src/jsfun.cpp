eabb9f738ab359ab5fa549eabe1aae3c7d757c5f
Brendan Eich <brendan@mozilla.org>
2011-02-23 19:38:27 -0800

    Followup fixes and tests in wake of global |this| binding change (636364, r=luke/gal/dvander, thanks to Waldo for the newGlobal shell function).

--- js/src/jsfun.cpp
+++ js/src/jsfun.cpp
@@ -2668,7 +2668,7 @@ LookupInterpretedFunctionPrototype(JSContext *cx, JSObject *funobj)
     return shape;
 }
 
-}
+} /* namespace js */
 
 static JSBool
 ThrowTypeError(JSContext *cx, uintN argc, Value *vp)
@@ -3050,35 +3050,4 @@ js_ReportIsNotFunction(JSContext *cx, const Value *vp, uintN flags)
         spindex = ((flags & JSV2F_SEARCH_STACK) ? JSDVG_SEARCH_STACK : JSDVG_IGNORE_STACK);
 
     js_ReportValueError3(cx, error, spindex, *vp, NULL, name, source);
-}
-
-namespace js {
-
-bool
-IsSafeForLazyThisCoercion(JSContext *cx, JSObject *callee)
-{
-    /*
-     * Look past any function wrappers. If the callee is a wrapped strict-mode
-     * function, lazy 'this' coercion is vacuously safe because strict-mode
-     * functions don't coerce 'this' at all. Otherwise, the callee is safe to
-     * transform into the lazy 'this' cookie (the undefined value) only if it
-     * is in the current scope.
-     *
-     * Without this restriction, lazy 'this' coercion would pick up the "wrong"
-     * global at the end of the callee function object's scope, rather than the
-     * "right" (backward compatible since 1995) global that was the "Reference
-     * base object" in the callee expression.
-     */
-    if (callee->isProxy()) {
-        callee = callee->unwrap();
-        if (!callee->isFunction())
-            return true; // treat any non-wrapped-function proxy as strict
-
-        JSFunction *fun = callee->getFunctionPrivate();
-        if (fun->isInterpreted() && fun->inStrictMode())
-            return true;
-    }
-    return callee->getGlobal() == cx->fp()->scopeChain().getGlobal();
-}
-
 }
