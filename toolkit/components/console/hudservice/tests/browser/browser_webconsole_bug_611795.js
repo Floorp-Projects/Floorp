5091dfaefeec4756b1caab05fbd5c32b12f6d4bf
Rob Campbell <rcampbell@mozilla.com>
2011-02-25 18:12:42 -0400

    Bug 611795 - Repeated messages in the Web Console should be collapsed into one; f=mihai.sucan r=sdwilsh,dao a=beltzner

---
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_bug_611795.js
@@ -0,0 +1,50 @@
+/* vim:set ts=2 sw=2 sts=2 et: */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+const TEST_URI = 'data:text/html,<div style="-moz-opacity:0;">test repeated' +
+                 ' css warnings</div><p style="-moz-opacity:0">hi</p>';
+
+function onContentLoaded()
+{
+  browser.removeEventListener("load", arguments.callee, true);
+
+  hudId = HUDService.displaysIndex()[0];
+  HUD = HUDService.hudReferences[hudId].HUDBox;
+  jsterm = HUDService.hudReferences[hudId].jsterm;
+  let filterBox = HUD.querySelector(".hud-filter-box");
+  let outputNode = HUD.querySelector(".hud-output-node");
+
+  let msg = "The unknown CSS property warning is displayed only once";
+  let node = outputNode.firstChild;
+
+  is (node.childNodes[2].textContent, "Unknown property '-moz-opacity'.  Declaration dropped.", "correct node")
+  is(node.childNodes[3].getAttribute("value"), 2, msg);
+
+  jsterm.clearOutput();
+
+  jsterm.setInputValue("for (let i = 0; i < 10; ++i) console.log('hi');");
+  jsterm.execute();
+
+  msg = "The console output is repeated 10 times";
+  let node = outputNode.querySelector(".webconsole-msg-console");
+  is(node.childNodes[3].getAttribute("value"), 10, msg);
+
+  finishTest();
+}
+
+/**
+ * Unit test for bug 611795:
+ * Repeated CSS messages get collapsed into one.
+ */
+function test()
+{
+  addTab(TEST_URI);
+  browser.addEventListener("load", function() {
+    browser.removeEventListener("load", arguments.callee, true);
+
+    openConsole();
+    browser.addEventListener("load", onContentLoaded, true);
+    content.location.reload();
+  }, true);
+}
