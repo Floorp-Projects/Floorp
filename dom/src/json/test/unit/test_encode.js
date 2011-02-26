df4416e6d260310477015c286b7ef51246b0fddb
Jeff Walden <jwalden@mit.edu>
2011-02-25 19:01:54 -0800

    Bug 636175 - Move dom/src/json/test/unit tests into JS suite tests, because that's where they belong.  r=pbiggar

--- dom/src/json/test/unit/test_encode.js
+++ dom/src/json/test/unit/test_encode.js
@@ -1,111 +1,36 @@
+var Ci = Components.interfaces;
+var Cc = Components.classes;
 
-// returns a list of [string, object] pairs to test encoding
-function getTestPairs() {
-  var testPairs = [
-    ["{}", {}],
-    ["[]", []],
-    ['{"foo":"bar"}', {"foo":"bar"}],
-    ['{"null":null}', {"null":null}],
-    ['{"five":5}', {"five":5}],
-    ['{"five":5,"six":6}', {"five":5, "six":6}],
-    ['{"x":{"y":"z"}}', {"x":{"y":"z"}}],
-    ['{"w":{"x":{"y":"z"}}}', {"w":{"x":{"y":"z"}}}],
-    ['[1,2,3]', [1,2,3]],
-    ['{"w":{"x":{"y":[1,2,3]}}}', {"w":{"x":{"y":[1,2,3]}}}],
-    ['{"false":false}', {"false":false}],
-    ['{"true":true}', {"true":true}],
-    ['{"child has two members":{"this":"one","2":"and this one"}}',
-     {"child has two members": {"this":"one", 2:"and this one"}}],
-    ['{"x":{"a":"b","c":{"y":"z"},"f":"g"}}',
-     {"x":{"a":"b","c":{"y":"z"},"f":"g"}}],
-    ['{"x":[1,{"y":"z"},3]}', {"x":[1,{"y":"z"},3]}],
-    ['["hmm"]', [new String("hmm")]],
-    ['[true]', [new Boolean(true)]],
-    ['[42]', [new Number(42)]],
-    ['["1978-09-13T12:24:34.023Z"]', [new Date(Date.UTC(1978, 8, 13, 12, 24, 34, 23))]],
-    ['[1,null,3]',[1,,3]],
-    [null, function test(){}],
-    [null, dump],
-    ['{"mm\\\"mm":"hmm"}',{"mm\"mm":"hmm"}],
-    ['{"mm\\\"mm\\\"mm":"hmm"}',{"mm\"mm\"mm":"hmm"}],
-    ['{"\\\"":"hmm"}',{'"':"hmm"}],
-    ['{"\\\\":"hmm"}',{'\\':"hmm"}],
-    ['{"mmm\\\\mmm":"hmm"}',{'mmm\\mmm':"hmm"}],
-    ['{"mmm\\\\mmm\\\\mmm":"hmm"}',{'mmm\\mmm\\mmm':"hmm"}],
-    ['{"mm\\u000bmm":"hmm"}',{"mm\u000bmm":"hmm"}],
-    ['{"mm\\u0000mm":"hmm"}',{"mm\u0000mm":"hmm"}]
-  ];
+var nativeJSON = Cc["@mozilla.org/dom/json;1"].createInstance(Ci.nsIJSON);
 
-  var x = {"free":"variable"}
-  testPairs.push(['{"free":"variable"}', x]);
-  testPairs.push(['{"y":{"free":"variable"}}', {"y":x}]);
+var dirSvc = Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties);
+var workingDir = dirSvc.get("TmpD", Ci.nsIFile);
 
-  // array prop
-  var x = {
-    a: [1,2,3]
-  }
-  testPairs.push(['{"a":[1,2,3]}', x])
+var outputName = "json-test-output";
+var outputDir = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+outputDir.initWithFile(workingDir);
+outputDir.append(outputName);
 
-  var y = {
-    foo: function(hmm) { return hmm; }
-  }
-  testPairs.push(['{"y":{}}',{"y":y}]);
-
-  // test toJSON
-  var hmm = {
-    toJSON: function() { return {"foo":"bar"}}
-  }
-  testPairs.push(['{"hmm":{"foo":"bar"}}', {"hmm":hmm}]);
-  testPairs.push(['{"foo":"bar"}', hmm]); // on the root
-
-  // toJSON on prototype
-  var Y = function() {
-    this.d = "e";
-  }
-  Y.prototype = {
-    not:"there?",
-    toJSON: function() { return {"foo":"bar"}}
-  };
-  var y = new Y();
-  testPairs.push(['{"foo":"bar"}', y.toJSON()]);
-  testPairs.push(['{"foo":"bar"}', y]);
-
-  // return undefined from toJSON
-  var hmm = {
-    toJSON: function() { return; }
-  }
-  testPairs.push(['{}', {"hmm":hmm}]);
-
-  // array with named prop
-  var x= new Array();
-  x[0] = 1;
-  x.foo = "bar";
-  //testPairs.push(['[1]', x]);
-
-  // prototype
-  var X = function() { this.foo = "b" }
-  X.prototype = {c:"d"}
-  var y = new X();
-  testPairs.push(['{"foo":"b"}', y]);
-
-  // useless roots will be dropped
-  testPairs.push([null, null]);
-  testPairs.push([null, ""]);
-  testPairs.push([null, undefined]);
-  testPairs.push([null, 5]);
-
-  return testPairs;
+if (!outputDir.exists()) {
+  outputDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0777);
+} else if (!outputDir.isDirectory()) {
+  do_throw(outputName + " is not a directory?")
 }
 
-function testStringEncode() {
-  // test empty arg
-  do_check_eq(null, nativeJSON.encode());
+function testStringEncode()
+{
+  do_check_eq(nativeJSON.encode(), null);
 
-  var pairs = getTestPairs();
-  for each(pair in pairs) {
-    var nativeResult = nativeJSON.encode(pair[1]);
-    do_check_eq(pair[0], nativeResult);
-  }
+  // useless roots are dropped
+  do_check_eq(nativeJSON.encode(null), null);
+  do_check_eq(nativeJSON.encode(""), null);
+  do_check_eq(nativeJSON.encode(undefined), null);
+  do_check_eq(nativeJSON.encode(5), null);
+  do_check_eq(nativeJSON.encode(function(){}), null);
+  do_check_eq(nativeJSON.encode(dump), null);
+
+  // All other testing should occur in js/src/tests/ecma_5/JSON/ using
+  // the otherwise-exactly-identical JSON.stringify.
 }
 
 function testOutputStreams() {
@@ -124,8 +49,21 @@ function testOutputStreams() {
     return jsonFile;
   }
 
-  var pairs = getTestPairs();
-  for each(pair in pairs) {
+  var pairs = [
+    ["{}", {}],
+    ['{"foo":"bar"}', {"foo":"bar"}],
+    ['{"null":null}', {"null":null}],
+    ['{"five":5}', {"five":5}],
+    ['{"true":true}', {"true":true}],
+    ['{"x":{"y":"z"}}', {"x":{"y":"z"}}],
+    ['{"w":{"x":{"y":"z"}}}', {"w":{"x":{"y":"z"}}}],
+    ["[]", []],
+    ['[1,2,3]', [1,2,3]],
+    ['[1,null,3]',[1,,3]],
+  ];
+  for (var i = 0; i < pairs.length; i++)
+  {
+    var pair = pairs[i];
     if (pair[1] && (typeof pair[1] == "object")) {
       var utf8File = writeToFile(pair[1], "UTF-8", false);
       var utf16LEFile = writeToFile(pair[1], "UTF-16LE", false);
@@ -156,10 +94,14 @@ function throwingToJSON() {
   }
   try {
     var y = nativeJSON.encode(a);
-  } catch (ex) {}
+    throw "didn't throw";
+  } catch (ex) {
+    do_check_eq(ex, "uh oh");
+  }
 }
 
-function run_test() {
+function run_test()
+{
   testStringEncode();
   throwingToJSON();
   
