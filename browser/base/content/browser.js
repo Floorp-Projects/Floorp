d8b365034d07cdd38c7dfb99c8f8b33be1e8f08d
Gavin Sharp <gavin@gavinsharp.com>
2011-02-28 11:56:56 -0800

    Back out bug 628179 (make navigation and tab switching always close the Find bar) to reduce risk. r=dao a=backout

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4439,7 +4439,10 @@ var XULBrowserWindow = {
       });
 
       if (gFindBarInitialized) {
-        gFindBar.close();
+        if (gFindBar.findMode != gFindBar.FIND_NORMAL) {
+          // Close the Find toolbar if we're in old-style TAF mode
+          gFindBar.close();
+        }
 
         // fix bug 253793 - turn off highlight when page changes
         gFindBar.getElement("highlight").checked = false;
