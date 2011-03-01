a9deee6a37b0df8237994b94530722051d5d50bf
Joe Drew <joe@drew.ca>
2011-03-01 11:58:59 -0500

    Bug 637413 - Always use a blank blocklist.xml file in the addon manager tests so we aren't broken by blocklist file updates. r=mossop a=blocker

--- toolkit/mozapps/extensions/test/xpcshell/test_bug406118.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_bug406118.js
@@ -40,6 +40,10 @@ function run_test() {
   createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9");
 
   // We cannot force the blocklist to update so just copy our test list to the profile
+  var blocklistFile = gProfD.clone();
+  blocklistFile.append("blocklist.xml");
+  if (blocklistFile.exists())
+    blocklistFile.remove(false);
   var source = do_get_file("data/test_bug393285.xml");
   source.copyTo(gProfD, "blocklist.xml");
 
