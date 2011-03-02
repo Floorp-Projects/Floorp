2e975afe462e101fc63ea54529ec5f105237ecd4
Matt Woodrow <mwoodrow@mozilla.com>
2011-03-02 21:53:27 +1300

    Bug 637901 - Don't trash our cached ImageContainer unless the retained layer manager has changed. r=tnikkel a=blocking2.0

--- content/base/public/nsContentUtils.h
+++ content/base/public/nsContentUtils.h
@@ -1699,12 +1699,12 @@ public:
    * a presentation with an associated widget, and use that widget's
    * layer manager.
    *
-   * If one can't be found, a BasicLayerManager is created and returned.
-   *
    * @param aDoc the document for which to return a layer manager.
+   * @param aAllowRetaining an outparam that states whether the returned
+   * layer manager should be used for retained layers
    */
   static already_AddRefed<mozilla::layers::LayerManager>
-  LayerManagerForDocument(nsIDocument *aDoc);
+  LayerManagerForDocument(nsIDocument *aDoc, bool *aAllowRetaining = nsnull);
 
   /**
    * Returns a layer manager to use for the given document. Basically we
@@ -1716,12 +1716,12 @@ public:
    * forseeable future. This function should be used carefully as it may change
    * the document's layer manager.
    *
-   * If one can't be found, a BasicLayerManager is created and returned.
-   *
    * @param aDoc the document for which to return a layer manager.
+   * @param aAllowRetaining an outparam that states whether the returned
+   * layer manager should be used for retained layers
    */
   static already_AddRefed<mozilla::layers::LayerManager>
-  PersistentLayerManagerForDocument(nsIDocument *aDoc);
+  PersistentLayerManagerForDocument(nsIDocument *aDoc, bool *aAllowRetaining = nsnull);
 
   /**
    * Determine whether a content node is focused or not,
