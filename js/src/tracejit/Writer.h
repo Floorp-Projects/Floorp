c19daccc4ec719a0e596c2faba7f8ddc2036d97e
Nicholas Nethercote <nnethercote@mozilla.com>
2011-03-02 20:00:45 -0800

    Bug 617244 - nanojit: remove AvmCore (TM-specific part).  r=gal.

--- js/src/tracejit/Writer.h
+++ js/src/tracejit/Writer.h
@@ -339,12 +339,13 @@ class Writer
     nj::CseFilter *const cse;   // created in this class
 
     nj::LogControl *logc;       // passed in from outside
+    nj::Config     *njConfig;   // passed in from outside
 
   public:
     Writer(nj::Allocator *alloc, nj::LirBuffer *lirbuf)
-      : alloc(alloc), lirbuf(lirbuf), lir(NULL), cse(NULL), logc(NULL) {}
+      : alloc(alloc), lirbuf(lirbuf), lir(NULL), cse(NULL), logc(NULL), njConfig(NULL) {}
 
-    void init(nj::LogControl *logc); 
+    void init(nj::LogControl *logc, nj::Config *njConfig); 
 
     nj::LIns *name(nj::LIns *ins, const char *name) const {
 #ifdef JS_JIT_SPEW
@@ -1071,13 +1072,13 @@ class Writer
     nj::LIns *cmovi(nj::LIns *cond, nj::LIns *t, nj::LIns *f) const {
         /* We can only use cmovi if the configuration says we can. */
         NanoAssert(t->isI() && f->isI());
-        return lir->insChoose(cond, t, f, avmplus::AvmCore::use_cmov());
+        return lir->insChoose(cond, t, f, njConfig->use_cmov());
     }
 
     nj::LIns *cmovp(nj::LIns *cond, nj::LIns *t, nj::LIns *f) const {
         /* We can only use cmovp if the configuration says we can. */
         NanoAssert(t->isP() && f->isP());
-        return lir->insChoose(cond, t, f, avmplus::AvmCore::use_cmov());
+        return lir->insChoose(cond, t, f, njConfig->use_cmov());
     }
 
     nj::LIns *cmovd(nj::LIns *cond, nj::LIns *t, nj::LIns *f) const {
