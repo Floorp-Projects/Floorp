5d1ccfea7667afefa40c21410eed9fa83eb966e8
Matt Brubeck <mbrubeck@mozilla.com>
2011-03-02 17:30:59 -0800

    Bug 637936 (followup) - Fix variable name collision [r=mfinkle]

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -1191,10 +1191,10 @@ Browser.MainDragger.prototype = {
 
     // Any leftover panning in doffset would bring controls into view. Add to sidebar
     // away panning for the total scroll offset.
-    let dx = Math.round(doffset.x);
-    if ((this._stopAtSidebar > 0 && dx > 0) ||
-        (this._stopAtSidebar < 0 && dx < 0)) {
-      if (dx != Math.round(panOffset.x))
+    let offsetX = Math.round(doffset.x);
+    if ((this._stopAtSidebar > 0 && offsetX > 0) ||
+        (this._stopAtSidebar < 0 && offsetX < 0)) {
+      if (offsetX != Math.round(panOffset.x))
         this._hitSidebar = true;
       doffset.x = panOffset.x;
     } else {
