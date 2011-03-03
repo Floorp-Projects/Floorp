2a4cabe958b4257a6a2ba84b0f619ceffcd04df3
Jason Orendorff <jorendorff@mozilla.com>
2011-03-03 14:50:40 -0600

    Bug 636818 - Crash [@ js::CallJSPropertyOpSetter] or [@ js_SetProperty] when assigning to a property inherited from a wrapper. r=gal.

---
+++ js/src/tests/js1_8_5/extensions/regress-636818.js
@@ -0,0 +1,8 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/licenses/publicdomain/
+
+a = evalcx('');
+a.__proto__ = ''.__proto__;
+a.length = 3;  // don't assert
+
+reportCompare(0, 0, 'ok');
