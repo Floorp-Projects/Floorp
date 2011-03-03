664310c23a8cf9b8cde19b6867a70a57f9a81035
Matt Brubeck <mbrubeck@mozilla.com>
2011-03-02 16:35:54 -0800

    Back out 62e1753d84e4 (bug 633279) on suspicion of N900 Tzoom regression.

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -1191,10 +1191,10 @@ Browser.MainDragger.prototype = {
 
     // Any leftover panning in doffset would bring controls into view. Add to sidebar
     // away panning for the total scroll offset.
-    let dx = doffset.x;
+    let dx = Math.round(doffset.x);
     if ((this._stopAtSidebar > 0 && dx > 0) ||
         (this._stopAtSidebar < 0 && dx < 0)) {
-      if (dx != panOffset.x)
+      if (dx != Math.round(panOffset.x))
         this._hitSidebar = true;
       doffset.x = panOffset.x;
     } else {
