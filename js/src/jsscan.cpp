5fe67247645e5d0a54dccd28be0338e873a3d1e2
Nicholas Nethercote <nnethercote@mozilla.com>
2011-03-09 15:26:38 -0800

    Bug 640076 - TOK_XMLATTR tokens can span multiple lines.  r=brendan.

--- js/src/jsscan.cpp
+++ js/src/jsscan.cpp
@@ -806,6 +806,7 @@ IsTokenSane(Token *tp)
         /* Only certain token kinds can be multi-line. */
         switch (tp->type) {
           case TOK_STRING:
+          case TOK_XMLATTR:
           case TOK_XMLSPACE:
           case TOK_XMLTEXT:
           case TOK_XMLCOMMENT:
