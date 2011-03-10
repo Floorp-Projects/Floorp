a3883d33bff4d916c4834819186a5e8ca3dd2e72
Nicholas Nethercote <nnethercote@mozilla.com>
2011-03-09 19:12:26 -0800

    Bug 639743 - JM: clean up Executable{Pool,Allocator} some more, 4/6.  r=dvander.

--- js/src/assembler/jit/ExecutableAllocatorOS2.cpp
+++ js/src/assembler/jit/ExecutableAllocatorOS2.cpp
@@ -33,9 +33,9 @@
 
 namespace JSC {
 
-void ExecutableAllocator::intializePageSize()
+size_t ExecutableAllocator::determinePageSize()
 {
-    ExecutableAllocator::pageSize = 4096u;
+    return 4096u;
 }
 
 ExecutablePool::Allocation ExecutablePool::systemAlloc(size_t n)
