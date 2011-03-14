8715ed8f3893aa7a16a1f20b155512b05fc0f126
Jeff Walden <jwalden@mit.edu>
2011-03-13 20:38:34 -0700

    Bug 640503 - Convert new String().length to use a regular data property and not a shared-permanent property-op getter/setter.  r=jorendorff
    --HG--
    extra : rebase_source : 06bb9b53c0498d20b3afc6e558096c2398559602

--- js/src/jscompartment.cpp
+++ js/src/jscompartment.cpp
@@ -81,6 +81,7 @@ JSCompartment::JSCompartment(JSRuntime *rt)
     emptyEnumeratorShape(NULL),
     emptyWithShape(NULL),
     initialRegExpShape(NULL),
+    initialStringShape(NULL),
     debugMode(rt->debugMode),
 #if ENABLE_YARR_JIT
     regExpAllocator(NULL),
@@ -488,6 +489,8 @@ JSCompartment::sweep(JSContext *cx, uint32 releaseInterval)
 
     if (initialRegExpShape && IsAboutToBeFinalized(cx, initialRegExpShape))
         initialRegExpShape = NULL;
+    if (initialStringShape && IsAboutToBeFinalized(cx, initialStringShape))
+        initialStringShape = NULL;
 
 #ifdef JS_TRACER
     traceMonitor.sweep(cx);
