401e3dc9b69c94f4a9390de46de376a315bc2d09
L. David Baron <dbaron@dbaron.org>
2011-03-17 20:14:31 -0700

    Add poisoning for nsRuleData::mValueOffsets.  (Bug 636039, patch 19)  r=bzbarsky
    I tested manually that after:
     (a) removing the |ruleData.mValueOffsets[aSID] = 0;| in
         nsRuleNode::WalkRuleTree
     (b) removing the NS_ABORT_IF_FALSE(aRuleData->mValueOffsets[aSID] == 0,
         ...) from nsRuleNode::CheckSpecifiedProperties and
         UnsetPropertiesWithoutFlags
    that we crash dereferencing the poison address in a SetCoord call inside
    nsRuleNode::ComputeTextResetData

--- layout/style/nsRuleData.cpp
+++ layout/style/nsRuleData.cpp
@@ -38,3 +38,46 @@
 
 #include "nsRuleData.h"
 #include "nsCSSProps.h"
+#include "nsPresArena.h"
+
+inline size_t
+nsRuleData::GetPoisonOffset()
+{
+  // Fill in mValueOffsets such that mValueStorage + mValueOffsets[i]
+  // will yield the frame poison value for all uninitialized value
+  // offsets.
+  PR_STATIC_ASSERT(sizeof(PRUword) == sizeof(size_t));
+  PR_STATIC_ASSERT(PRUword(-1) > PRUword(0));
+  PR_STATIC_ASSERT(size_t(-1) > size_t(0));
+  PRUword framePoisonValue = nsPresArena::GetPoisonValue();
+  return size_t(framePoisonValue - PRUword(mValueStorage)) /
+         sizeof(nsCSSValue);
+}
+
+nsRuleData::nsRuleData(PRUint32 aSIDs, nsCSSValue* aValueStorage,
+                       nsPresContext* aContext, nsStyleContext* aStyleContext)
+  : mSIDs(aSIDs),
+    mCanStoreInRuleTree(PR_TRUE),
+    mPresContext(aContext),
+    mStyleContext(aStyleContext),
+    mPostResolveCallback(nsnull),
+    mValueStorage(aValueStorage)
+{
+  size_t framePoisonOffset = GetPoisonOffset();
+  for (size_t i = 0; i < nsStyleStructID_Length; ++i) {
+    mValueOffsets[i] = framePoisonOffset;
+  }
+}
+
+#ifdef DEBUG
+nsRuleData::~nsRuleData()
+{
+  // assert nothing in mSIDs has poison value
+  size_t framePoisonOffset = GetPoisonOffset();
+  for (size_t i = 0; i < nsStyleStructID_Length; ++i) {
+    NS_ABORT_IF_FALSE(!(mSIDs & (1 << i)) ||
+                      mValueOffsets[i] != framePoisonOffset,
+                      "value in SIDs was left with poison offset");
+  }
+}
+#endif
