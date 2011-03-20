0cee7aff463a8c6ec3f2e9bc1d85b98733840fef
Brian Hackett <bhackett1024@gmail.com>
2011-03-20 10:43:38 -0700

    [INFER] Fix type handler for Object.valueOf, add testcase, bug 643244.

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -2846,7 +2846,7 @@ static JSFunctionSpec object_methods[] = {
 #endif
     JS_FN_TYPE(js_toString_str,             obj_toString,                0,0, JS_TypeHandlerString),
     JS_FN_TYPE(js_toLocaleString_str,       obj_toLocaleString,          0,0, JS_TypeHandlerString),
-    JS_FN_TYPE(js_valueOf_str,              obj_valueOf,                 0,0, JS_TypeHandlerThis),
+    JS_FN_TYPE(js_valueOf_str,              obj_valueOf,                 0,0, JS_TypeHandlerDynamic),
 #if JS_HAS_OBJ_WATCHPOINT
     JS_FN_TYPE(js_watch_str,                obj_watch,                   2,0, JS_TypeHandlerVoid),
     JS_FN_TYPE(js_unwatch_str,              obj_unwatch,                 1,0, JS_TypeHandlerVoid),
