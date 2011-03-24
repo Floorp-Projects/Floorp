e07c13af6c88d9b8090e00db1fd85afda8aaedef
Nicholas Nethercote <nnethercote@mozilla.com>
2011-03-23 18:00:04 -0700

    Bug 637549 - Speed up expression parsing, part 1: avoid ungetting/regetting many tokens.  r=brendan,jimb.

--- js/src/jsparse.h
+++ js/src/jsparse.h
@@ -1137,6 +1137,11 @@ private:
      * object, pointed to by this->tc.
      *
      * Each returns a parse node tree or null on error.
+     *
+     * Parsers with a name that ends in '1' leave the TokenStream state
+     * pointing to the token one past the end of the parsed fragment.  For a
+     * number of the parsers this is convenient and avoids a lot of
+     * unnecessary ungetting and regetting of tokens.
      */
     JSParseNode *functionStmt();
     JSParseNode *functionExpr();
@@ -1153,17 +1158,17 @@ private:
     JSParseNode *variables(bool inLetHead);
     JSParseNode *expr();
     JSParseNode *assignExpr();
-    JSParseNode *condExpr();
-    JSParseNode *orExpr();
-    JSParseNode *andExpr();
-    JSParseNode *bitOrExpr();
-    JSParseNode *bitXorExpr();
-    JSParseNode *bitAndExpr();
-    JSParseNode *eqExpr();
-    JSParseNode *relExpr();
-    JSParseNode *shiftExpr();
-    JSParseNode *addExpr();
-    JSParseNode *mulExpr();
+    JSParseNode *condExpr1();
+    JSParseNode *orExpr1();
+    JSParseNode *andExpr1();
+    JSParseNode *bitOrExpr1();
+    JSParseNode *bitXorExpr1();
+    JSParseNode *bitAndExpr1();
+    JSParseNode *eqExpr1();
+    JSParseNode *relExpr1();
+    JSParseNode *shiftExpr1();
+    JSParseNode *addExpr1();
+    JSParseNode *mulExpr1();
     JSParseNode *unaryExpr();
     JSParseNode *memberExpr(JSBool allowCallSyntax);
     JSParseNode *primaryExpr(js::TokenKind tt, JSBool afterDot);
