ac0eb6cddb9858018f60aa2141c1903dfc99049c
Simon Montagu <smontagu@smontagu.org>
2011-03-24 11:28:44 +0200

    Debugging code for Bidi resolution. NPOTDB

--- layout/base/nsBidiPresUtils.cpp
+++ layout/base/nsBidiPresUtils.cpp
@@ -58,6 +58,9 @@
 #include "gfxUnicodeProperties.h"
 #include "nsIThebesFontMetrics.h"
 
+#undef NOISY_BIDI
+#undef REALLY_NOISY_BIDI
+
 using namespace mozilla;
 
 static const PRUnichar kSpace            = 0x0020;
@@ -424,6 +427,16 @@ nsBidiPresUtils::Resolve(nsBlockFrame* aBlockFrame)
     }
   }
   
+#ifdef NOISY_BIDI
+  if (mBuffer[0] != kObjectSubstitute) {
+    printf("Before Resolve(), aBlockFrame=0x%p, mBuffer='%s', frameCount=%d\n",
+           (void*)aBlockFrame, NS_ConvertUTF16toUTF8(mBuffer).get(), frameCount);
+#ifdef REALLY_NOISY_BIDI
+    printf(" frameTree=:\n");
+    nsFrame::DumpFrameTree(aBlockFrame);
+#endif
+  }
+#endif
   for (; ;) {
     if (fragmentLength <= 0) {
       // Get the next frame from mLogicalFrames
@@ -607,6 +620,13 @@ nsBidiPresUtils::Resolve(nsBlockFrame* aBlockFrame)
       }
     }
   } // for
+#ifdef REALLY_NOISY_BIDI
+  if (mBuffer[0] != kObjectSubstitute) {
+    printf("---\nAfter Resolve(), frameTree =:\n");
+    nsFrame::DumpFrameTree(aBlockFrame);
+    printf("===\n");
+  }
+#endif
   return mSuccess;
 }
 
