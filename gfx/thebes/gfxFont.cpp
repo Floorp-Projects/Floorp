340316d75aa191b5de64fc065f35096d6053b2ae
Jonathan Kew <jfkthame@gmail.com>
2011-03-23 23:01:50 -0400

    Bug 623711 - erratic font selection with multiple @font-face rules and bad font resources; r=jdaggett

--- gfx/thebes/gfxFont.cpp
+++ gfx/thebes/gfxFont.cpp
@@ -2040,21 +2040,26 @@ gfxFontGroup::FindPlatformFont(const nsAString& aName,
     PRBool needsBold;
     gfxFontEntry *fe = nsnull;
 
-    // first, look up in the user font set
+    // First, look up in the user font set...
+    // If the fontSet matches the family, we must not look for a platform
+    // font of the same name, even if we fail to actually get a fontEntry
+    // here; we'll fall back to the next name in the CSS font-family list.
+    PRBool foundFamily = PR_FALSE;
     gfxUserFontSet *fs = fontGroup->GetUserFontSet();
     if (fs) {
-        // if the fontSet matches the family, but the font has not yet finished
+        // If the fontSet matches the family, but the font has not yet finished
         // loading (nor has its load timeout fired), the fontGroup should wait
-        // for the download, and not actually draw its text yet
+        // for the download, and not actually draw its text yet.
         PRBool waitForUserFont = PR_FALSE;
-        fe = fs->FindFontEntry(aName, *fontStyle, needsBold, waitForUserFont);
+        fe = fs->FindFontEntry(aName, *fontStyle, foundFamily,
+                               needsBold, waitForUserFont);
         if (!fe && waitForUserFont) {
             fontGroup->mSkipDrawing = PR_TRUE;
         }
     }
 
-    // nothing in the user font set ==> check system fonts
-    if (!fe) {
+    // Not known in the user font set ==> check system fonts
+    if (!foundFamily) {
         fe = gfxPlatformFontList::PlatformFontList()->
             FindFontForFamily(aName, fontStyle, needsBold);
     }
@@ -2247,22 +2252,24 @@ gfxFontGroup::ForEachFontInternal(const nsAString& aFamilies,
             if (aResolveFontName) {
                 ResolveData data(fc, gf, closure);
                 PRBool aborted = PR_FALSE, needsBold;
-                nsresult rv;
+                nsresult rv = NS_OK;
+                PRBool foundFamily = PR_FALSE;
                 PRBool waitForUserFont = PR_FALSE;
                 if (mUserFontSet &&
-                    mUserFontSet->FindFontEntry(family, mStyle, needsBold,
-                                                waitForUserFont))
+                    mUserFontSet->FindFontEntry(family, mStyle, foundFamily,
+                                                needsBold, waitForUserFont))
                 {
                     gfxFontGroup::FontResolverProc(family, &data);
-                    rv = NS_OK;
                 } else {
                     if (waitForUserFont) {
                         mSkipDrawing = PR_TRUE;
                     }
-                    gfxPlatform *pf = gfxPlatform::GetPlatform();
-                    rv = pf->ResolveFontName(family,
-                                             gfxFontGroup::FontResolverProc,
-                                             &data, aborted);
+                    if (!foundFamily) {
+                        gfxPlatform *pf = gfxPlatform::GetPlatform();
+                        rv = pf->ResolveFontName(family,
+                                                 gfxFontGroup::FontResolverProc,
+                                                 &data, aborted);
+                    }
                 }
                 if (NS_FAILED(rv) || aborted)
                     return PR_FALSE;
