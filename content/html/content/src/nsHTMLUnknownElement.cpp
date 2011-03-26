36159e3d03c4f7ccbce2cb788bfdd648da946cb6
Ms2ger <ms2ger@gmail.com>
2011-03-26 11:58:55 -0400

    Bug 585933 - Only implement HTMLUnknownElement on unknown elements; r=jonas,smaug

--- content/html/content/src/nsHTMLUnknownElement.cpp
+++ content/html/content/src/nsHTMLUnknownElement.cpp
@@ -20,6 +20,7 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
+ *   Ms2ger <ms2ger@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either of the GNU General Public License Version 2 or later (the "GPL"),
@@ -34,20 +35,16 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
-#include "nsIDOMHTMLElement.h"
-#include "nsIDOMEventTarget.h"
-#include "nsGenericHTMLElement.h"
-#include "nsGkAtoms.h"
-#include "nsStyleConsts.h"
-#include "nsIAtom.h"
-#include "nsRuleData.h"
 
-class nsHTMLSpanElement : public nsGenericHTMLElement,
-                          public nsIDOMHTMLElement
+#include "nsGenericHTMLElement.h"
+#include "nsIDOMHTMLUnknownElement.h"
+
+class nsHTMLUnknownElement : public nsGenericHTMLElement
+                           , public nsIDOMHTMLUnknownElement
 {
 public:
-  nsHTMLSpanElement(already_AddRefed<nsINodeInfo> aNodeInfo);
-  virtual ~nsHTMLSpanElement();
+  nsHTMLUnknownElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  virtual ~nsHTMLUnknownElement();
 
   // nsISupports
   NS_DECL_ISUPPORTS_INHERITED
@@ -67,31 +64,32 @@ public:
 };
 
 
-NS_IMPL_NS_NEW_HTML_ELEMENT(Span)
+NS_IMPL_NS_NEW_HTML_ELEMENT(Unknown)
 
 
-nsHTMLSpanElement::nsHTMLSpanElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+nsHTMLUnknownElement::nsHTMLUnknownElement(already_AddRefed<nsINodeInfo> aNodeInfo)
   : nsGenericHTMLElement(aNodeInfo)
 {
 }
 
-nsHTMLSpanElement::~nsHTMLSpanElement()
+nsHTMLUnknownElement::~nsHTMLUnknownElement()
 {
 }
 
 
-NS_IMPL_ADDREF_INHERITED(nsHTMLSpanElement, nsGenericElement)
-NS_IMPL_RELEASE_INHERITED(nsHTMLSpanElement, nsGenericElement)
+NS_IMPL_ADDREF_INHERITED(nsHTMLUnknownElement, nsGenericElement)
+NS_IMPL_RELEASE_INHERITED(nsHTMLUnknownElement, nsGenericElement)
 
 
-DOMCI_NODE_DATA(HTMLSpanElement, nsHTMLSpanElement)
+DOMCI_NODE_DATA(HTMLUnknownElement, nsHTMLUnknownElement)
 
-// QueryInterface implementation for nsHTMLSpanElement
-NS_INTERFACE_TABLE_HEAD(nsHTMLSpanElement)
-  NS_HTML_CONTENT_INTERFACE_TABLE0(nsHTMLSpanElement)
-  NS_HTML_CONTENT_INTERFACE_TABLE_TO_MAP_SEGUE(nsHTMLSpanElement,
+// QueryInterface implementation for nsHTMLUnknownElement
+NS_INTERFACE_TABLE_HEAD(nsHTMLUnknownElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE1(nsHTMLUnknownElement,
+                                   nsIDOMHTMLUnknownElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE_TO_MAP_SEGUE(nsHTMLUnknownElement,
                                                nsGenericHTMLElement)
-NS_HTML_CONTENT_INTERFACE_TABLE_TAIL_CLASSINFO(HTMLSpanElement)
+NS_HTML_CONTENT_INTERFACE_TABLE_TAIL_CLASSINFO(HTMLUnknownElement)
 
 
-NS_IMPL_ELEMENT_CLONE(nsHTMLSpanElement)
+NS_IMPL_ELEMENT_CLONE(nsHTMLUnknownElement)
