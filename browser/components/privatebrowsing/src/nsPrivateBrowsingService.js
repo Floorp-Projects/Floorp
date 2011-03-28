a5d820eed534532fa49f870fbfd05696d4cbe371
Ehsan Akhgari <ehsan@mozilla.com>
2011-03-28 16:29:22 -0400

    Bug 631973 - Opening Firefox in permanent private browsing mode opens an additional window with the home page; r=gavin

--- browser/components/privatebrowsing/src/nsPrivateBrowsingService.js
+++ browser/components/privatebrowsing/src/nsPrivateBrowsingService.js
@@ -467,7 +467,7 @@ PrivateBrowsingService.prototype = {
 
   handle: function PBS_handle(aCmdLine) {
     if (aCmdLine.handleFlag("private", false))
-      ; // It has already been handled
+      aCmdLine.preventDefault = true; // It has already been handled
     else if (aCmdLine.handleFlag("private-toggle", false)) {
       if (this._autoStarted) {
         throw Cr.NS_ERROR_ABORT;
