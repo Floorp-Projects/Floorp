cec8fe19c4ea6f440dc9def50960329b6b35e996
Jonathan Kew <jfkthame@gmail.com>
2011-03-31 09:47:01 +0100

    bug 646561 - pass correct length to PropertyProvider in bidi case. r=roc

--- layout/generic/nsTextFrameThebes.cpp
+++ layout/generic/nsTextFrameThebes.cpp
@@ -6073,7 +6073,7 @@ nsTextFrame::AddInlineMinWidthForFlow(nsIRenderingContext *aRenderingContext,
   PRBool hyphenating = frag->GetLength() > 0 &&
     (mTextRun->GetFlags() & gfxTextRunFactory::TEXT_ENABLE_HYPHEN_BREAKS) != 0;
   if (hyphenating) {
-    len = frag->GetLength() - iter.GetOriginalOffset();
+    len = GetInFlowContentLength() - iter.GetOriginalOffset();
 #ifdef DEBUG
     // check that the length we're going to pass to PropertyProvider matches
     // the expected range of text in the run
