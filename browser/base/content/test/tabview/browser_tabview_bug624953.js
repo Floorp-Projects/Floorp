404f03614fd0260981918b7b87b661598d98cee7
Ehsan Akhgari <ehsan@mozilla.com>
2011-03-31 12:00:08 -0400

    Backout changeset 7fbc721aa455 (bug 634672) because it breaks the Mac mochitest-browser-chrome tests

--- browser/base/content/test/tabview/browser_tabview_bug624953.js
+++ browser/base/content/test/tabview/browser_tabview_bug624953.js
@@ -4,20 +4,36 @@
 function test() {
   waitForExplicitFinish();
 
+  let finishTest = function (groupItem) {
+    groupItem.addSubscriber(groupItem, 'groupHidden', function () {
+      groupItem.removeSubscriber(groupItem, 'groupHidden');
+      groupItem.closeHidden();
+      hideTabView(finish);
+    });
+
+    groupItem.closeAll();
+  }
+
   showTabView(function () {
     let cw = TabView.getContentWindow();
 
-    let groupItem = createGroupItemWithBlankTabs(window, 200, 240, 20, 4)
-    ok(!groupItem.isStacked(), 'groupItem is not stacked');
+    let bounds = new cw.Rect(20, 20, 150, 200);
+    let groupItem = new cw.GroupItem([], {bounds: bounds, immediately: true});
+    cw.GroupItems.setActiveGroupItem(groupItem);
+
+    for (let i=0; i<4; i++)
+      gBrowser.loadOneTab('about:blank', {inBackground: true});
+
+    ok(!groupItem._isStacked, 'groupItem is not stacked');
     cw.GroupItems.pauseArrange();
 
-    groupItem.setSize(100, 100, true);
+    groupItem.setSize(150, 150);
     groupItem.setUserSize();
-    ok(!groupItem.isStacked(), 'groupItem is still not stacked');
+    ok(!groupItem._isStacked, 'groupItem is still not stacked');
 
     cw.GroupItems.resumeArrange();
-    ok(groupItem.isStacked(), 'groupItem is now stacked');
+    ok(groupItem._isStacked, 'groupItem is now stacked');
 
-    closeGroupItem(groupItem, finish);
+    finishTest(groupItem);
   });
 }
