10b6ab3fda6035d0c52fdf29c8562c52f2beb564
Matt Woodrow <mwoodrow@mozilla.com>
2011-04-07 16:58:08 -0700

    Bug 640082 - Part 3 - Add BindTexture functions to TextureImage. r=joe

--- gfx/thebes/GLContext.h
+++ gfx/thebes/GLContext.h
@@ -205,6 +205,31 @@ public:
 
     virtual bool DirectUpdate(gfxASurface *aSurf, const nsIntRegion& aRegion) =0;
 
+    virtual void BindTexture(GLenum aTextureUnit) = 0;
+    virtual void ReleaseTexture() {};
+
+    class ScopedBindTexture
+    {
+    public:
+        ScopedBindTexture(TextureImage *aTexture, GLenum aTextureUnit) :
+          mTexture(aTexture)
+        {
+            if (mTexture) {
+                mTexture->BindTexture(aTextureUnit);
+            }
+        }
+
+        ~ScopedBindTexture()
+        {
+            if (mTexture) {
+                mTexture->ReleaseTexture();
+            }       
+        }
+
+    private:
+        TextureImage *mTexture;
+    };
+
     /**
      * Return this TextureImage's texture ID for use with GL APIs.
      * Callers are responsible for properly binding the texture etc.
@@ -302,6 +327,8 @@ public:
       Initialized,  // Texture memory exists, but contents are invalid.
       Valid  // Texture fully ready to use.
     };
+    
+    virtual void BindTexture(GLenum aTextureUnit);
 
     virtual gfxASurface* BeginUpdate(nsIntRegion& aRegion);
     virtual void EndUpdate();
