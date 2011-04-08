9da02e52493091111289da4be30ee2db62af3256
Raymond Lee <raymond@raysquare.com>
2011-04-08 13:19:48 +0800

    Bug 596833 - Refinement for bug 595943: No need to iterate over all tabs, just the ones at the beginning r=ian

--- browser/base/content/tabview/ui.js
+++ browser/base/content/tabview/ui.js
@@ -714,9 +714,9 @@ let UI = {
         // if not closing the last tab
         if (gBrowser.tabs.length > 1) {
           // Don't return to TabView if there are any app tabs
-          for (let a = 0; a < gBrowser.tabs.length; a++) {
+          for (let a = 0; a < gBrowser._numPinnedTabs; a++) {
             let theTab = gBrowser.tabs[a]; 
-            if (theTab.pinned && gBrowser._removingTabs.indexOf(theTab) == -1) 
+            if (gBrowser._removingTabs.indexOf(theTab) == -1) 
               return;
           }
 
