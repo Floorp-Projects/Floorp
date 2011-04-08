d3022c4117791c42749f67a171273f4b319234a0
Josh Aas <joshmoz@gmail.com>
2011-04-08 12:58:04 -0700

    Bug 636872: Allow CSS cursor property to be used for plugins. r=bz

--- modules/plugin/base/public/npapi.h
+++ modules/plugin/base/public/npapi.h
@@ -366,7 +366,9 @@ typedef enum {
   /* Checks to see if the plug-in would like the browser to load the "src" attribute. */
   NPPVpluginCancelSrcStream = 20,
 
-  NPPVSupportsAdvancedKeyHandling = 21
+  NPPVSupportsAdvancedKeyHandling = 21,
+
+  NPPVpluginUsesDOMForCursorBool = 22
 
 #if defined(XP_MACOSX)
   /* Used for negotiating drawing models */
