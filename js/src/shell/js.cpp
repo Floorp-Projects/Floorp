b10ac1acf2f4c159c8f50000cfeee99696ab0d18
Ed Morley <bmo@edmorley.co.uk>
2011-04-09 20:17:53 +0100

    Bug 647389 - Remove WINCE stuff from Spidermonkey (r=jorendorff)

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -393,10 +393,6 @@ SetContextOptions(JSContext *cx)
     JS_SetOperationCallback(cx, ShellOperationCallback);
 }
 
-#ifdef WINCE
-int errno;
-#endif
-
 static void
 Process(JSContext *cx, JSObject *obj, char *filename, JSBool forceTTY)
 {
@@ -425,10 +421,7 @@ Process(JSContext *cx, JSObject *obj, char *filename, JSBool forceTTY)
 
     SetContextOptions(cx);
 
-#ifndef WINCE
-    /* windows mobile (and possibly other os's) does not have a TTY */
     if (!forceTTY && !isatty(fileno(file)))
-#endif
     {
         /*
          * It's not interactive - just execute it.
