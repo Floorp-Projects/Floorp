332c3bd85a4c27768e25017df43a9b3457d000a7
Boris Zbarsky <bzbarsky@mit.edu>
2011-04-12 01:12:12 -0700

    Fix 32-bit mac opt bustage, bug 435442, r=dbaron

--- layout/style/nsComputedDOMStyle.cpp
+++ layout/style/nsComputedDOMStyle.cpp
@@ -4079,7 +4079,15 @@ nsComputedDOMStyle::DoGetAnimationIterationCount()
     valueList->AppendCSSValue(iterationCount);
 
     float f = animation->GetIterationCount();
-    if (f == NS_IEEEPositiveInfinity()) {
+    /* Need a nasty hack here to work around an optimizer bug in gcc
+       4.2 on Mac, which somehow gets confused when directly comparing
+       a float to the return value of NS_IEEEPositiveInfinity when
+       building 32-bit builds. */
+#ifdef XP_MACOSX
+    volatile
+#endif
+      float inf = NS_IEEEPositiveInfinity();
+    if (f == inf) {
       iterationCount->SetIdent(eCSSKeyword_infinite);
     } else {
       iterationCount->SetNumber(f);
