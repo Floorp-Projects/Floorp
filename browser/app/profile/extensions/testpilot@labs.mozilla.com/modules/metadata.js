ccdce1e91d888717febdae2c6bb954a4e9a2c02e
Jono X <jdicarlo@mozilla.com>
2011-04-11 22:30:44 -0400

    Bug 577694 - Add detection of accessibility instantiation [r=mark.finkle]

--- browser/app/profile/extensions/testpilot@labs.mozilla.com/modules/metadata.js
+++ browser/app/profile/extensions/testpilot@labs.mozilla.com/modules/metadata.js
@@ -117,6 +117,19 @@ let MetadataCollector = {
       accessibilities.push({ name: prefName, value: prefValue });
     }
 
+    /* Detect accessibility instantiation
+     * (David Bolter's code from bug 577694) */
+    let enabled;
+    try {
+      enabled = Components.manager.QueryInterface(Ci.nsIServiceManager)
+                  .isServiceInstantiatedByContractID(
+                    "@mozilla.org/accessibilityService;1",
+                    Ci.nsISupports);
+    } catch (ex) {
+      enabled = false;
+    }
+    accessibilities.push({name: "isInstantiated", value: enabled});
+
     return accessibilities;
   },
 
