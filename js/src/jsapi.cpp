0e5c53d8fc990a51784983e5932f15154d712e68
Blake Kaplan <mrbkap@gmail.com>
2011-04-12 15:56:45 -0700

    Bug 618176 - Skip multiple wrappers when deciding whether this object is an array. r=gal

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -4163,7 +4163,7 @@ JS_IsArrayObject(JSContext *cx, JSObject *obj)
 {
     assertSameCompartment(cx, obj);
     return obj->isArray() ||
-           (obj->isWrapper() && JSWrapper::wrappedObject(obj)->isArray());
+           (obj->isWrapper() && obj->unwrap()->isArray());
 }
 
 JS_PUBLIC_API(JSBool)
