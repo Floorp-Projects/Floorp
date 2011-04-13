9bab24b2fd04f96ceed39f7211b7a0e3dc40b896
Brian Hackett <bhackett1024@gmail.com>
2011-04-13 15:18:15 -0700

    [INFER] Don't hoist accesses that could underflow the array, bug 649775.

---
+++ js/src/jit-test/tests/jaeger/bug649775.js
@@ -0,0 +1,17 @@
+var x = [, ];
+var n = [, ];
+var np = 18229;
+sa = Array;
+function copy_(x, y) {
+    var i;
+    var k = x < y ? x.length : y.length;
+    for (i = 0; i < k; i--) {
+        x[i];
+        if (i == -100)
+          return;
+    }
+}
+function mont_(x, y, n, np) {
+    copy_(x, sa);
+}
+mont_(x, x, n, np);
