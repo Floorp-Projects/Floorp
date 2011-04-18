78c69d3a4f660c0d02100b01fdad53919f402ab2
Bob Moss <bmoss@mozilla.com>
2011-04-18 11:55:24 -0700

    Bug 650110 - Update SUTAgent, whitespace refactor for agent code r=ctalbert

--- build/mobile/sutagent/android/DoAlert.java
+++ build/mobile/sutagent/android/DoAlert.java
@@ -20,7 +20,7 @@
  *
  * Contributor(s):
  *  Bob Moss <bmoss@mozilla.com>
- * 
+ *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
  * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
@@ -46,36 +46,36 @@ import android.media.RingtoneManager;
 import android.widget.Toast;
 
 class DoAlert extends TimerTask
-	{
-	int	lcv = 0;
-	Toast toast = null;
-	Ringtone rt = null;
+    {
+    int    lcv = 0;
+    Toast toast = null;
+    Ringtone rt = null;
 
-	DoAlert(ContextWrapper contextWrapper)
-		{
-		Context	ctx = contextWrapper.getApplicationContext();
-		this.toast = Toast.makeText(ctx, "Help me!", Toast.LENGTH_LONG);
-		rt = RingtoneManager.getRingtone(ctx, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
-		}
+    DoAlert(ContextWrapper contextWrapper)
+        {
+        Context    ctx = contextWrapper.getApplicationContext();
+        this.toast = Toast.makeText(ctx, "Help me!", Toast.LENGTH_LONG);
+        rt = RingtoneManager.getRingtone(ctx, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
+        }
 
-	public void term()
-		{
-		if (rt != null)
-			{
-			if (rt.isPlaying())
-				rt.stop();
-			}
+    public void term()
+        {
+        if (rt != null)
+            {
+            if (rt.isPlaying())
+                rt.stop();
+            }
 
-		if (toast != null)
-			toast.cancel();
-		}
+        if (toast != null)
+            toast.cancel();
+        }
 
-	public void run ()
-		{
-		String sText =(((lcv++ % 2) == 0)  ? "Help me!" : "I've fallen down!" );
-		toast.setText(sText);
-		toast.show();
-		if (rt != null)
-			rt.play();
-		}
-	}
+    public void run ()
+        {
+        String sText =(((lcv++ % 2) == 0)  ? "Help me!" : "I've fallen down!" );
+        toast.setText(sText);
+        toast.show();
+        if (rt != null)
+            rt.play();
+        }
+    }
