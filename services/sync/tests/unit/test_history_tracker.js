22c72214b72da9bb5f1fcc0f3df069819bac755c
Philipp von Weitershausen <philipp@weitershausen.de>
2011-04-18 14:18:36 -0700

    Bug 648371 - Convert async tests to use run_next_test helper. r=rnewman

--- services/sync/tests/unit/test_history_tracker.js
+++ services/sync/tests/unit/test_history_tracker.js
@@ -14,91 +14,94 @@ function onScoreUpdated(callback) {
   });
 }
 
-function run_test() {
-  Engines.register(HistoryEngine);
-  let engine = Engines.get("history");
-  let tracker = engine._tracker;
+Engines.register(HistoryEngine);
+let engine = Engines.get("history");
+let tracker = engine._tracker;
 
+let _counter = 0;
+function addVisit() {
+  Svc.History.addVisit(Utils.makeURI("http://getfirefox.com/" + _counter),
+                       Date.now() * 1000, null, 1, false, 0);
+  _counter += 1;
+}
+
+
+function run_test() {
+  run_next_test();
+}
+
+add_test(function test_empty() {
   _("Verify we've got an empty tracker to work with.");
   do_check_eq([id for (id in tracker.changedIDs)].length, 0);
+  run_next_test();
+});
 
-  let _counter = 0;
-  function addVisit() {
-    Svc.History.addVisit(Utils.makeURI("http://getfirefox.com/" + _counter),
-                         Date.now() * 1000, null, 1, false, 0);
-    _counter += 1;
-  }
+add_test(function test_not_tracking(next) {
+  _("Create history item. Won't show because we haven't started tracking yet");
+  addVisit();
+  Utils.delay(function() {
+    do_check_eq([id for (id in tracker.changedIDs)].length, 0);
+    run_next_test();
+  }, 0);
+});
 
-  do_test_pending();
-  asyncChainTests(function (next) {
+add_test(function test_start_tracking() {
+  _("Tell the tracker to start tracking changes.");
+  onScoreUpdated(function() {
+    do_check_eq([id for (id in tracker.changedIDs)].length, 1);
+    run_next_test();
+  });
+  Svc.Obs.notify("weave:engine:start-tracking");
+  addVisit();
+});
 
-    _("Create history item. Won't show because we haven't started tracking yet");
-    addVisit();
-    Utils.delay(function() {
-      do_check_eq([id for (id in tracker.changedIDs)].length, 0);
-      next();
-    }, 0);
+add_test(function test_start_tracking_twice() {
+  _("Notifying twice won't do any harm.");
+  onScoreUpdated(function() {
+    do_check_eq([id for (id in tracker.changedIDs)].length, 2);
+    run_next_test();
+  });
+  Svc.Obs.notify("weave:engine:start-tracking");
+  addVisit();
+});
 
-  }, function (next) {
+add_test(function test_track_delete() {
+  _("Deletions are tracked.");
+  let uri = Utils.makeURI("http://getfirefox.com/0");
+  let guid = engine._store.GUIDForUri(uri);
+  do_check_false(guid in tracker.changedIDs);
 
-    _("Tell the tracker to start tracking changes.");
-    onScoreUpdated(function() {
-      do_check_eq([id for (id in tracker.changedIDs)].length, 1);
-      next();
-    });
-    Svc.Obs.notify("weave:engine:start-tracking");
-    addVisit();
+  onScoreUpdated(function() {
+    do_check_true(guid in tracker.changedIDs);
+    do_check_eq([id for (id in tracker.changedIDs)].length, 3);
+    run_next_test();
+  });
+  Svc.History.removePage(uri);
+});
 
-  }, function (next) {
+add_test(function test_stop_tracking() {
+  _("Let's stop tracking again.");
+  tracker.clearChangedIDs();
+  Svc.Obs.notify("weave:engine:stop-tracking");
+  addVisit();
+  Utils.delay(function() {
+    do_check_eq([id for (id in tracker.changedIDs)].length, 0);
+    run_next_test();
+  }, 0);
+});
 
-    _("Notifying twice won't do any harm.");
-    onScoreUpdated(function() {
-      do_check_eq([id for (id in tracker.changedIDs)].length, 2);
-      next();
-    });
-    Svc.Obs.notify("weave:engine:start-tracking");
-    addVisit();
+add_test(function test_stop_tracking_twice() {
+  _("Notifying twice won't do any harm.");
+  Svc.Obs.notify("weave:engine:stop-tracking");
+  addVisit();
+  Utils.delay(function() {
+    do_check_eq([id for (id in tracker.changedIDs)].length, 0);
+    run_next_test();
+  }, 0);
+});
 
-  }, function (next) {
-
-    _("Deletions are tracked.");
-    let uri = Utils.makeURI("http://getfirefox.com/0");
-    let guid = engine._store.GUIDForUri(uri);
-    do_check_false(guid in tracker.changedIDs);
-
-    onScoreUpdated(function() {
-      do_check_true(guid in tracker.changedIDs);
-      do_check_eq([id for (id in tracker.changedIDs)].length, 3);
-      next();
-    });
-    Svc.History.removePage(uri);
-
-  }, function (next) {
-
-    _("Let's stop tracking again.");
-    tracker.clearChangedIDs();
-    Svc.Obs.notify("weave:engine:stop-tracking");
-    addVisit();
-    Utils.delay(function() {
-      do_check_eq([id for (id in tracker.changedIDs)].length, 0);
-      next();
-    }, 0);
-
-  }, function (next) {
-
-    _("Notifying twice won't do any harm.");
-    Svc.Obs.notify("weave:engine:stop-tracking");
-    addVisit();
-    Utils.delay(function() {
-      do_check_eq([id for (id in tracker.changedIDs)].length, 0);
-      next();
-    }, 0);
-
-  }, function (next) {
-
-    _("Clean up.");
-    Svc.History.removeAllPages();
-    do_test_finished();
-
-  })();
-}
+add_test(function cleanup() {
+   _("Clean up.");
+  Svc.History.removeAllPages();
+  run_next_test();
+});
