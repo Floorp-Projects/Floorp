2c6cf5ed6bf3103e2db0589baa82f61d5894ae89
Bob Moss <bmoss@mozilla.com>
2011-04-18 11:55:24 -0700

    Bug 650110 Update SUTAgent in m-c to 1.01, main agent update r=ctalbert

--- build/mobile/sutagent/android/ASMozStub.java
+++ build/mobile/sutagent/android/ASMozStub.java
@@ -37,15 +37,21 @@
 
 package com.mozilla.SUTAgentAndroid.service;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.util.Timer;
 
+import com.mozilla.SUTAgentAndroid.R;
+
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-// import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
+import android.view.Gravity;
 import android.widget.Toast;
 
 public class ASMozStub extends android.app.Service {
@@ -58,7 +64,20 @@ public class ASMozStub extends android.app.Service {
 	Thread monitor = null;
 	Timer timer = null;
 	
-	@Override
+	@SuppressWarnings("unchecked")
+	private static final Class[] mStartForegroundSignature = new Class[] {
+        int.class, Notification.class};
+	@SuppressWarnings("unchecked")
+	private static final Class[] mStopForegroundSignature = new Class[] {
+        boolean.class};
+	
+    private NotificationManager mNM;
+    private Method mStartForeground;
+    private Method mStopForeground;
+    private Object[] mStartForegroundArgs = new Object[2];
+    private Object[] mStopForegroundArgs = new Object[1];
+
+    @Override
 	public IBinder onBind(Intent intent)
 		{
 		return null;
@@ -67,7 +86,18 @@ public class ASMozStub extends android.app.Service {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		Toast.makeText(this, "Listener Service created...", Toast.LENGTH_LONG).show();
+		
+        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        try {
+            mStartForeground = getClass().getMethod("startForeground", mStartForegroundSignature);
+            mStopForeground = getClass().getMethod("stopForeground", mStopForegroundSignature);
+        	}
+        catch (NoSuchMethodException e) {
+            // Running on an older platform.
+            mStartForeground = mStopForeground = null;
+        	}
+		
+        doToast("Listener Service created...");
 		}
 
 	public void onStart(Intent intent, int startId) {
@@ -77,14 +107,18 @@ public class ASMozStub extends android.app.Service {
 			cmdChnl = new ServerSocket(20701);
 			runCmdThrd = new RunCmdThread(cmdChnl, this, handler);
 			runCmdThrd.start();
-			Toast.makeText(this, "Command channel port 20701 ...", Toast.LENGTH_LONG).show();
+			doToast("Command channel port 20701 ...");
 			
 			dataChnl = new ServerSocket(20700);
 			runDataThrd = new RunDataThread(dataChnl, this);
 			runDataThrd.start();
-			Toast.makeText(this, "Data channel port 20700 ...", Toast.LENGTH_LONG).show();
+			doToast("Data channel port 20700 ...");
+			
+            Notification notification = new Notification();
+            startForegroundCompat(R.string.foreground_service_started, notification);
 			} 
 		catch (Exception e) {
+			doToast(e.toString());
 //			Toast.makeText(getApplication().getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
 			}
 		
@@ -94,6 +128,7 @@ public class ASMozStub extends android.app.Service {
 	public void onDestroy()
 		{
 		super.onDestroy();
+		
 		if (runCmdThrd.isAlive())
 			{
 			runCmdThrd.StopListening();
@@ -107,7 +142,9 @@ public class ASMozStub extends android.app.Service {
 		NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
 		notificationManager.cancel(1959);
 		
-		Toast.makeText(this, "Listener Service destroyed...", Toast.LENGTH_LONG).show();
+		stopForegroundCompat(R.string.foreground_service_started);
+		
+		doToast("Listener Service destroyed...");
 		
 		System.exit(0);
 		}
@@ -115,8 +152,64 @@ public class ASMozStub extends android.app.Service {
 	public void SendToDataChannel(String strToSend)
 		{
 		if (runDataThrd.isAlive())
-			{
 			runDataThrd.SendToDataChannel(strToSend);
-			}
 		}
+	
+	public void doToast(String sMsg) {
+		Toast toast = Toast.makeText(this, sMsg, Toast.LENGTH_LONG);
+		toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 100);
+		toast.show();
+	}
+	
+    /**
+     * This is a wrapper around the new startForeground method, using the older
+     * APIs if it is not available.
+     */
+    void startForegroundCompat(int id, Notification notification) {
+        // If we have the new startForeground API, then use it.
+        if (mStartForeground != null) {
+            mStartForegroundArgs[0] = Integer.valueOf(id);
+            mStartForegroundArgs[1] = notification;
+            try {
+                mStartForeground.invoke(this, mStartForegroundArgs);
+            } catch (InvocationTargetException e) {
+                // Should not happen.
+                Log.w("ScreenOnWidget", "Unable to invoke startForeground", e);
+            } catch (IllegalAccessException e) {
+                // Should not happen.
+                Log.w("ScreenOnWidget", "Unable to invoke startForeground", e);
+            }
+            return;
+        }
+
+        // Fall back on the old API.
+        setForeground(true);
+        mNM.notify(id, notification);
+    }
+
+    /**
+     * This is a wrapper around the new stopForeground method, using the older
+     * APIs if it is not available.
+     */
+    void stopForegroundCompat(int id) {
+        // If we have the new stopForeground API, then use it.
+        if (mStopForeground != null) {
+            mStopForegroundArgs[0] = Boolean.TRUE;
+            try {
+                mStopForeground.invoke(this, mStopForegroundArgs);
+            } catch (InvocationTargetException e) {
+                // Should not happen.
+                Log.w("ScreenOnWidget", "Unable to invoke stopForeground", e);
+            } catch (IllegalAccessException e) {
+                // Should not happen.
+                Log.w("ScreenOnWidget", "Unable to invoke stopForeground", e);
+            }
+            return;
+        }
+
+        // Fall back on the old API.  Note to cancel BEFORE changing the
+        // foreground state, since we could be killed at that point.
+        mNM.cancel(id);
+        setForeground(false);
+    }
 }
