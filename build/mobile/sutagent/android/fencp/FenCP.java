80e8072f9231bd4eeb5487f668d5a5ce540c61c6
Bob Moss <bmoss@mozilla.com>
2011-04-18 11:55:24 -0700

    Bug 650110 Update SUTAgent - adding in fencp, ffxcp, watcher code r=ctalbert

---
+++ build/mobile/sutagent/android/fencp/FenCP.java
@@ -0,0 +1,13 @@
+package org.mozilla.fencp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FenCP extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ build/mobile/sutagent/android/ffxcp/ffxcp.java
@@ -0,0 +1,13 @@
+package org.mozilla.ffxcp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ffxcp extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
