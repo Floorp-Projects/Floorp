342ac3bde6c951732c4ead5fdc50cd6267292953
Jason Orendorff <jorendorff@mozilla.com>
2011-04-18 23:52:17 -0500

    Add Debug.prototype.enabled. This implementation is not ideal, since we want clearing .enabled to remove the debugger from every place where it might cause overhead. For now it just sets a flag.

---
+++ js/src/tests/js1_8_5/extensions/debug-object-17.js
@@ -0,0 +1,28 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/licenses/publicdomain/
+
+// Disabling a Debug object causes events to stop being delivered to it
+// immediately, even if we're in the middle of dispatching.
+
+var g = newGlobal('new-compartment');
+var log;
+
+var arr = [];
+for (var i = 0; i < 4; i++) {
+    arr[i] = new Debug(g);
+    arr[i].hooks = {
+        num: i,
+        debuggerHandler: function () {
+            log += this.num;
+            // Disable them all.
+            for (var j = 0; j < arr.length; j++)
+                arr[j].enabled = false;
+        }
+    };
+}
+
+log = '';
+g.eval("debugger; debugger;");
+assertEq(log, '0');
+
+reportCompare(0, 0, 'ok');
