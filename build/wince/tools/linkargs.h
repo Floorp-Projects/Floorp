6c5524ea80d4a2f0018d2c141a6847aad605858d
Ed Morley <bmo@edmorley.co.uk>
2011-04-19 14:11:40 +1000

    Bug 648866 - Remove WinCE code from main build system + that of js/src/. r=ted

--- build/wince/tools/linkargs.h
+++
@@ -1,7 +0,0 @@
-#ifndef LINKARGS_H
-#define LINKARGS_H
-
-void checkLinkArgs(int* k, int* s, int* i, int* j, char** args, char** argv);
-void addLinkArgs(int k, int s, int *i, int *j, char** args, char** argv);
-
-#endif
