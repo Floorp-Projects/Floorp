75919522852159c98a03846e3010b4cd10cfd437
Justin Dolske <dolske@mozilla.com>
2011-04-19 14:18:17 -0700

    Bug 638352 - tab-modal dialogs can get into bad focus state and key events can bleed through tabs. r=gavin

--- toolkit/components/prompts/src/nsPrompter.js
+++ toolkit/components/prompts/src/nsPrompter.js
@@ -438,12 +438,6 @@ function openTabPrompt(domWin, tabPrompt, args) {
         winUtils.leaveModalStateWithWindow(callerWin);
 
         PromptUtils.fireDialogEvent(domWin, "DOMModalDialogClosed");
-
-        // Restore focus to the previously focused element within tab.
-        let fm = Cc["@mozilla.org/focus-manager;1"].
-                 getService(Ci.nsIFocusManager);
-        let e = fm.getFocusedElementForWindow(domWin.top, false, {});
-        fm.setFocus(e, fm.FLAG_NOSCROLL);
     }
 
     let newPrompt;
