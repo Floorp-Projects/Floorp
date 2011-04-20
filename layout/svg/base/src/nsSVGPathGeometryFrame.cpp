5b752721f45a21c19c5ef421a16f798c1b1f5558
Jonathan Watt <jwatt@jwatt.org>
2011-04-20 10:16:01 +0100

    Bug 488314 part 2 - Kill vestiges of SetMatrixPropagation. r=longsonr.
    --HG--
    extra : rebase_source : ab532ae0ee733e6c759856640d755869bc231f73

--- layout/svg/base/src/nsSVGPathGeometryFrame.cpp
+++ layout/svg/base/src/nsSVGPathGeometryFrame.cpp
@@ -335,23 +335,6 @@ nsSVGPathGeometryFrame::NotifyRedrawUnsuspended()
   return NS_OK;
 }
 
-NS_IMETHODIMP
-nsSVGPathGeometryFrame::SetMatrixPropagation(PRBool aPropagate)
-{
-  if (aPropagate) {
-    AddStateBits(NS_STATE_SVG_PROPAGATE_TRANSFORM);
-  } else {
-    RemoveStateBits(NS_STATE_SVG_PROPAGATE_TRANSFORM);
-  }
-  return NS_OK;
-}
-
-PRBool
-nsSVGPathGeometryFrame::GetMatrixPropagation()
-{
-  return (GetStateBits() & NS_STATE_SVG_PROPAGATE_TRANSFORM) != 0;
-}
-
 gfxRect
 nsSVGPathGeometryFrame::GetBBoxContribution(const gfxMatrix &aToBBoxUserspace)
 {
