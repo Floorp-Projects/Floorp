814127d4b5010bf29326ea40c438fba5532c97c1
Zack Weinberg <zackw@panix.com>
2011-04-21 12:36:46 -0700

    Bug 649429 part 2: move nsSVGUtils::ConvertToSurfaceSize back to nsSVGUtils.cpp. r=dholbert

--- layout/svg/base/src/nsSVGUtils.cpp
+++ layout/svg/base/src/nsSVGUtils.cpp
@@ -1140,6 +1140,26 @@ nsSVGUtils::ToAppPixelRect(nsPresContext *aPresContext, const gfxRect& rect)
                 aPresContext->DevPixelsToAppUnits(NSToIntCeil(rect.YMost()) - NSToIntFloor(rect.Y())));
 }
 
+gfxIntSize
+nsSVGUtils::ConvertToSurfaceSize(const gfxSize& aSize,
+                                 PRBool *aResultOverflows)
+{
+  gfxIntSize surfaceSize(ClampToInt(aSize.width), ClampToInt(aSize.height));
+
+  *aResultOverflows = surfaceSize.width != NS_round(aSize.width) ||
+    surfaceSize.height != NS_round(aSize.height);
+
+  if (!gfxASurface::CheckSurfaceSize(surfaceSize)) {
+    surfaceSize.width = NS_MIN(NS_SVG_OFFSCREEN_MAX_DIMENSION,
+                               surfaceSize.width);
+    surfaceSize.height = NS_MIN(NS_SVG_OFFSCREEN_MAX_DIMENSION,
+                                surfaceSize.height);
+    *aResultOverflows = PR_TRUE;
+  }
+
+  return surfaceSize;
+}
+
 gfxMatrix
 nsSVGUtils::ConvertSVGMatrixToThebes(nsIDOMSVGMatrix *aMatrix)
 {
