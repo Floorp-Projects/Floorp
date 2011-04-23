604caf65ce42ee926077c73be556cc8a57d0e31c
Marco Bonardo <mbonardo@mozilla.com>
2011-04-23 02:09:13 +0200

    Bug 641531 - Close Places containers after use (mechanical changes) rs=me

--- toolkit/components/places/tests/unit/test_history_import.js
+++ toolkit/components/places/tests/unit/test_history_import.js
@@ -56,7 +56,9 @@ function uri_in_db(aURI) {
   var result = hs.executeQuery(query, options);
   var root = result.root;
   root.containerOpen = true;
-  return (root.childCount == 1);
+  var cc = root.childCount;
+  root.containerOpen = false;
+  return (cc == 1);
 }
 
 // main
