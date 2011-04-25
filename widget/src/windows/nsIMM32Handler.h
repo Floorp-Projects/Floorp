1c4d6bdef0ae098753b71cb909558733e8149f04
Ed Morley <bmo@edmorley.co.uk>
2011-04-24 20:10:14 -0700

    Bug 473687 - Remove widget WinCE support: Part C (Remove ENABLE_IME_MOUSE_HANDLING); r=dougt

--- widget/src/windows/nsIMM32Handler.h
+++ widget/src/windows/nsIMM32Handler.h
@@ -57,8 +57,6 @@ class nsIWidget;
 class nsWindow;
 struct nsIntRect;
 
-#define ENABLE_IME_MOUSE_HANDLING 1
-
 #define NS_WM_IMEFIRST WM_IME_SETCONTEXT
 #define NS_WM_IMELAST  WM_IME_KEYUP
 
@@ -172,9 +170,7 @@ protected:
 
   // The result of following On*Event methods means "The message was processed,
   // don't process the message in the caller (nsWindow)".
-#ifdef ENABLE_IME_MOUSE_HANDLING
   PRBool OnMouseEvent(nsWindow* aWindow, LPARAM lParam, int aAction);
-#endif // ENABLE_IME_MOUSE_HANDLING
   static PRBool OnKeyDownEvent(nsWindow* aWindow, WPARAM wParam, LPARAM lParam,
                                PRBool &aEatMessage);
 
