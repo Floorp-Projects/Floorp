cdc68fa8055e9305bf23724bfe5885facd9a1f5e
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2011-04-26 16:29:36 +0100

    bug 643460 pt 1 - import OTS r.62 from http://code.google.com/p/ots/. r=jfkthame

--- gfx/ots/src/gdef.h
+++ gfx/ots/src/gdef.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 Mozilla Foundation. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,10 +10,27 @@
 namespace ots {
 
 struct OpenTypeGDEF {
+  OpenTypeGDEF()
+      : version_2(false),
+        has_glyph_class_def(false),
+        has_mark_attachment_class_def(false),
+        has_mark_glyph_sets_def(false),
+        num_mark_glyph_sets(0),
+        data(NULL),
+        length(0) {
+  }
+
+  bool version_2;
+  bool has_glyph_class_def;
+  bool has_mark_attachment_class_def;
+  bool has_mark_glyph_sets_def;
+  uint16_t num_mark_glyph_sets;
+
   const uint8_t *data;
-  uint32_t length;
+  size_t length;
 };
 
 }  // namespace ots
 
-#endif  // OTS_GDEF_H_
+#endif
+
