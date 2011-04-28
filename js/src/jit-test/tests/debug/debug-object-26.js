0c1ca6bf4fa046cb92eccd946729501bd5edfac3
Jason Orendorff <jorendorff@mozilla.com>
2011-04-28 18:44:28 -0500

    Add Debug.Frame.prototype.live.

---
+++ js/src/jit-test/tests/debug/debug-object-26.js
@@ -0,0 +1,35 @@
+// |jit-test| debug
+// Debug.Frame.prototype.live is false for frames that have thrown or been thrown through
+
+load(libdir + "asserts.js");
+
+var g = newGlobal('new-compartment');
+g.debuggeeGlobal = this;
+g.eval("var finalCheck;");
+g.eval("(" + function () {
+        var a = [];
+        var dbg = Debug(debuggeeGlobal);
+        dbg.hooks = {
+            debuggerHandler: function (frame) {
+                a.push(frame);
+                for (var i = 0; i < a.length; i++)
+                    assertEq(a[i].live, true);
+            }
+        };
+        finalCheck = function (n) {
+            assertEq(a.length, n);
+            for (var i = 0; i < n; i++)
+                assertEq(a[i].live, false);
+        };
+    } + ")()");
+
+function f(n) {
+    debugger;
+    if (--n > 0)
+        f(n);
+    else
+        throw "fit";
+}
+
+assertThrowsValue(function () { f(10); }, "fit");
+g.finalCheck(10);
