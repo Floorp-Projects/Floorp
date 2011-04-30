347e03e754cddb64eab0ae8df6d9196b7b9df4c5
Matt Brubeck <mbrubeck@mozilla.com>
2011-04-29 17:19:45 -0700

    Bug 653009 (part 1/2) - Make preventDefault on touch events compatible with WebKit [r=wesj] DONTBUILD (mobile only)
    --HG--
    extra : rebase_source : bb77614e151f8491d2ebe0daaed2b1f809780b92

--- mobile/chrome/content/content.js
+++ mobile/chrome/content/content.js
@@ -1205,6 +1205,8 @@ ConsoleAPIObserver.init();
 
 var TouchEventHandler = {
   element: null,
+  isCancellable: true,
+
   init: function() {
     addMessageListener("Browser:MouseUp", this);
     addMessageListener("Browser:MouseDown", this);
@@ -1222,12 +1224,13 @@ var TouchEventHandler = {
 
     switch (aMessage.name) {
       case "Browser:MouseDown":
-        let cwu = Util.getWindowUtils(content);
+        this.isCancellable = true;
         this.element = elementFromPoint(json.x, json.y);
         cancelled = !this.sendEvent("touchstart", json, this.element);
         break;
 
       case "Browser:MouseUp":
+        this.isCancellable = false;
         if (this.element)
           this.sendEvent("touchend", json, this.element);
         this.element = null;
@@ -1239,9 +1242,15 @@ var TouchEventHandler = {
         break;
     }
 
-    if (aMessage.name != "Browser:MouseUp")
+    if (this.isCancellable) {
       sendAsyncMessage("Browser:CaptureEvents", { messageId: json.messageId,
                                                   panning: cancelled });
+      // Panning can be cancelled only during the "touchstart" event and the
+      // first "touchmove" event.  After it's cancelled, it stays cancelled
+      // until the next touchstart event.
+      if (cancelled || aMessage.name == "Browser:MouseMove")
+        this.isCancellable = false;
+    }
   },
 
   sendEvent: function(aName, aData, aElement) {
