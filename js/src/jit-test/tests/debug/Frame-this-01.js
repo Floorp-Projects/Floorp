87de7f9c035d847abd3961f580609b4afc7a56c3
Jason Orendorff <jorendorff@mozilla.com>
2011-05-03 11:40:54 -0500

    Add Debug.Object, Frame.prototype.this, Frame.prototype.arguments.

---
+++ js/src/jit-test/tests/debug/Frame-this-01.js
@@ -0,0 +1,26 @@
+// |jit-test| debug
+// Frame.prototype.this in strict-mode functions, with primitive values
+
+var g = newGlobal('new-compartment');
+var dbg = new Debug(g);
+var hits = 0;
+var v;
+dbg.hooks = {
+    debuggerHandler: function (frame) {
+        hits++;
+        assertEq(frame.this, g.v);
+    }
+};
+
+g.eval("function f() { 'use strict'; debugger; }");
+
+g.eval("Boolean.prototype.f = f; v = true; v.f();");
+g.eval("f.call(v);");
+g.eval("Number.prototype.f = f; v = 3.14; v.f();");
+g.eval("f.call(v);");
+g.eval("String.prototype.f = f; v = 'hello'; v.f();");
+g.eval("f.call(v);");
+g.eval("v = undefined; f.call(v);");
+g.eval("v = null; f.call(v);");
+
+assertEq(hits, 8);
