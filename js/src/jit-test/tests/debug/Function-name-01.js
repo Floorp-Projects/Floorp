102ab1acc2b4edf78b0fa4c12f62ada31f97153f
Jason Orendorff <jorendorff@mozilla.com>
2011-05-03 16:01:23 -0500

    Implement Debug.Function.prototype.name.

---
+++ js/src/jit-test/tests/debug/Function-name-01.js
@@ -0,0 +1,14 @@
+// |jit-test| debug
+// Debug.Function.prototype.name
+
+var g = newGlobal('new-compartment');
+var dbg = Debug(g);
+var name, hits;
+dbg.hooks = {debuggerHandler: function (frame) { hits++; assertEq(frame.callee.name, name); }};
+
+hits = 0;
+name = "f";
+g.eval("(function f() { debugger; })();");
+name = null;
+g.eval("(function () { debugger; })();");
+assertEq(hits, 2);
