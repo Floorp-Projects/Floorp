5b1589813db83d2e8ca7fec10ae16260764250f7
Jason Orendorff <jorendorff@mozilla.com>
2011-05-04 11:15:42 -0500

    Bug 654734 - TrampolineCompiler::generateForceReturn should emit a call to ScriptDebugEpilogue.

---
+++ js/src/jit-test/tests/debug/Frame-identity-01.js
@@ -0,0 +1,22 @@
+// |jit-test| debug
+// Check that {return:} resumption kills the current stack frame.
+
+var g = newGlobal('new-compartment');
+g.debuggeeGlobal = this;
+g.eval("(" + function () {
+        var dbg = new Debug(debuggeeGlobal);
+        var prev = null;
+        dbg.hooks = {
+            debuggerHandler: function (frame) {
+                assertEq(frame === prev, false);
+                if (prev)
+                    assertEq(prev.live, false);
+                prev = frame;
+                return {return: frame.arguments[0]};
+            }
+        };
+    } + ")();");
+
+function f(i) { debugger; }
+for (var i = 0; i < HOTLOOP + 2; i++)
+    assertEq(f(i), i);
