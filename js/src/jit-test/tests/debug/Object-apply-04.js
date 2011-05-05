437f777fab3cb713863364c186704568a56b26b4
Jason Orendorff <jorendorff@mozilla.com>
2011-05-05 16:26:43 -0500

    Passing test: .apply works on function proxies.

---
+++ js/src/jit-test/tests/debug/Object-apply-04.js
@@ -0,0 +1,17 @@
+// |jit-test| debug
+// Debug.Object.prototype.apply works with function proxies
+
+var g = newGlobal('new-compartment');
+g.eval("function f() { debugger; }");
+var dbg = Debug(g);
+var hits = 0;
+dbg.hooks = {
+    debuggerHandler: function (frame) {
+        var proxy = frame.arguments[0];
+        assertEq(proxy.name, null);
+        assertEq(proxy.apply(null, [33]).return, 34);
+        hits++;
+    }
+};
+g.eval("f(Proxy.createFunction({}, function (arg) { return arg + 1; }));");
+assertEq(hits, 1);
