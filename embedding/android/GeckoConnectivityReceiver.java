efc8a34d0214498470192f967241402ecd57d844
Matt Brubeck <mbrubeck@mozilla.com>
2011-05-09 12:58:55 -0700

    Bug 643032 - Don't start network status listener until Fennec starts [r=blassey]

--- embedding/android/GeckoConnectivityReceiver.java
+++ embedding/android/GeckoConnectivityReceiver.java
@@ -58,7 +58,5 @@ public class GeckoConnectivityReceiver
 
         if (GeckoApp.checkLaunchState(GeckoApp.LaunchState.GeckoRunning))
             GeckoAppShell.onChangeNetworkLinkStatus(status);
-        else
-            GeckoApp.mStartedEarly = true;
     }
 }
