aa9f7ee8fef3cc2e7fe46b5c51c472d191033c72
Tim Taubert <tim.taubert@gmx.de>
2011-05-11 16:13:58 +0200

    Bug 654941 - First tab in group has black thumbnail after 'Undo close group' is used; f=raymond, r=ehsan

---
+++ browser/base/content/test/tabview/browser_tabview_bug624692.js
@@ -0,0 +1,25 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+function test() {
+  waitForExplicitFinish();
+
+  newWindowWithTabView(function (win) {
+    registerCleanupFunction(function () win.close());
+
+    let cw = win.TabView.getContentWindow();
+    let groupItem = cw.GroupItems.groupItems[0];
+    let tabItem = groupItem.getChild(0);
+
+    hideGroupItem(groupItem, function () {
+      unhideGroupItem(groupItem, function () {
+        let bounds = tabItem.getBounds();
+        groupItem.arrange({immediately: true});
+        ok(bounds.equals(tabItem.getBounds()),
+           "tabItem bounds were correct after unhiding the groupItem");
+
+        finish();
+      });
+    });
+  });
+}
---
+++ browser/base/content/test/tabview/browser_tabview_bug654941.js
@@ -0,0 +1,33 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+function test() {
+  waitForExplicitFinish();
+
+  let compareCanvasSize = function (prefix, width, height, tabItem) {
+    let canvas = tabItem.$canvas[0];
+    is(canvas.width, width, prefix + ": canvas widths are equal (" + width + "px)");
+    is(canvas.height, height, prefix + ": canvas heights are equal (" + height + "px)");
+  };
+
+  newWindowWithTabView(function (win) {
+    registerCleanupFunction(function () win.close());
+
+    let cw = win.TabView.getContentWindow();
+    let groupItem = cw.GroupItems.groupItems[0];
+    let tabItem = groupItem.getChild(0);
+
+    afterAllTabItemsUpdated(function () {
+      let {width, height} = tabItem.$canvas[0];
+
+      hideGroupItem(groupItem, function () {
+        compareCanvasSize("hidden", width, height, tabItem);
+
+        unhideGroupItem(groupItem, function () {
+          compareCanvasSize("unhidden", width, height, tabItem);
+          finish();
+        });
+      });
+    }, win);
+  });
+}
