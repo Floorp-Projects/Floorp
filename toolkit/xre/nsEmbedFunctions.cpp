899d3b6b5d77030496e75dd6733354dc6e726057
Michael Wu <mwu@mozilla.com>
2011-05-10 18:27:51 -0700

    Back out parts 3-7 of bug 620931 which caused bug 656172, r=killer

--- toolkit/xre/nsEmbedFunctions.cpp
+++ toolkit/xre/nsEmbedFunctions.cpp
@@ -512,7 +512,9 @@ XRE_InitChildProcess(int aArgc,
       // Allow ProcessChild to clean up after itself before going out of
       // scope and being deleted
       process->CleanUp();
-      mozilla::Omnijar::CleanUp();
+#ifdef MOZ_OMNIJAR
+      mozilla::SetOmnijar(nsnull);
+#endif
     }
   }
 
