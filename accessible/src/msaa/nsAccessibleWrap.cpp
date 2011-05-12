03481e3f41e949d5d9f11dd223b5f006429a8484
Alexander Surkov <surkov.alexander@gmail.com>
2011-05-12 20:37:18 +0900

    Bug 648223 - stop support of accessibility.disableenumvariant pref, r=davidb

--- accessible/src/msaa/nsAccessibleWrap.cpp
+++ accessible/src/msaa/nsAccessibleWrap.cpp
@@ -119,7 +119,7 @@ __try {
 
   if (IID_IUnknown == iid || IID_IDispatch == iid || IID_IAccessible == iid)
     *ppv = static_cast<IAccessible*>(this);
-  else if (IID_IEnumVARIANT == iid && !gIsEnumVariantSupportDisabled) {
+  else if (IID_IEnumVARIANT == iid) {
     long numChildren;
     get_accChildCount(&numChildren);
     if (numChildren > 0)  // Don't support this interface for leaf elements
