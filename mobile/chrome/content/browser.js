f15e72b456ab7080856e34ca28131809b35ded53
Matt Brubeck <mbrubeck@mozilla.com>
2011-05-14 14:11:44 -0700

    Bug 656923 - Tab opened on command-line is in the background behind restored tabs [r=mfinkle]

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -339,24 +339,26 @@ var Browser = {
     // Should we restore the previous session (crash or some other event)
     let ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
     if (ss.shouldRestore()) {
-      // Initial window resizes call functions that assume a tab is in the tab list
-      // and restored tabs are added too late. We add a dummy to to satisfy the resize
-      // code and then remove the dummy after the session has been restored.
-      let dummy = this.addTab("about:blank");
-      let dummyCleanup = {
-        observe: function() {
-          Services.obs.removeObserver(dummyCleanup, "sessionstore-windows-restored");
-          dummy.chromeTab.ignoreUndo = true;
-          Browser.closeTab(dummy, { forceClose: true });
-        }
-      };
-      Services.obs.addObserver(dummyCleanup, "sessionstore-windows-restored", false);
-
-      ss.restoreLastSession();
-
-      // Also open any commandline URLs, except the homepage
-      if (commandURL && commandURL != this.getHomePage())
+      let bringFront = false;
+      // First open any commandline URLs, except the homepage
+      if (commandURL && commandURL != this.getHomePage()) {
         this.addTab(commandURL, true);
+      } else {
+        bringFront = true;
+        // Initial window resizes call functions that assume a tab is in the tab list
+        // and restored tabs are added too late. We add a dummy to to satisfy the resize
+        // code and then remove the dummy after the session has been restored.
+        let dummy = this.addTab("about:blank");
+        let dummyCleanup = {
+          observe: function() {
+            Services.obs.removeObserver(dummyCleanup, "sessionstore-windows-restored");
+            dummy.chromeTab.ignoreUndo = true;
+            Browser.closeTab(dummy, { forceClose: true });
+          }
+        };
+        Services.obs.addObserver(dummyCleanup, "sessionstore-windows-restored", false);
+      }
+      ss.restoreLastSession(bringFront);
     } else {
       this.addTab(commandURL || this.getHomePage(), true);
     }
