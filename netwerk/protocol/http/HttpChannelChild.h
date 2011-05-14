99b659685b716826898960da5dd7af1e22fb679d
Phil Ringnalda <philringnalda@gmail.com>
2011-05-13 20:48:38 -0700

    Back out 93604789da99 (bug 637339) on suspicion of causing timeouts in Linux debug test_httpsuspend_wrap.js CLOSED TREE

--- netwerk/protocol/http/HttpChannelChild.h
+++ netwerk/protocol/http/HttpChannelChild.h
@@ -69,7 +69,6 @@ namespace net {
 
 class HttpChannelChild : public PHttpChannelChild
                        , public HttpBaseChannel
-                       , public HttpAsyncAborter<HttpChannelChild>
                        , public nsICacheInfoChannel
                        , public nsIProxiedChannel
                        , public nsITraceableChannel
@@ -147,7 +146,7 @@ protected:
   bool RecvOnStopRequest(const nsresult& statusCode);
   bool RecvOnProgress(const PRUint64& progress, const PRUint64& progressMax);
   bool RecvOnStatus(const nsresult& status);
-  bool RecvFailedAsyncOpen(const nsresult& status);
+  bool RecvCancelEarly(const nsresult& status);
   bool RecvRedirect1Begin(const PRUint32& newChannel,
                           const URI& newURI,
                           const PRUint32& redirectFlags,
@@ -158,7 +157,6 @@ protected:
   bool RecvDeleteSelf();
 
   bool GetAssociatedContentSecurity(nsIAssociatedContentSecurity** res = nsnull);
-  virtual void DoNotifyListenerCleanup();
 
 private:
   RequestHeaderTuples mRequestHeaders;
@@ -173,6 +171,8 @@ private:
 
   // If ResumeAt is called before AsyncOpen, we need to send extra data upstream
   bool mSendResumeAt;
+  // Current suspension depth for this channel object
+  PRUint32 mSuspendCount;
 
   bool mIPCOpen;
   bool mKeptAlive;
@@ -196,8 +196,7 @@ private:
   void OnStopRequest(const nsresult& statusCode);
   void OnProgress(const PRUint64& progress, const PRUint64& progressMax);
   void OnStatus(const nsresult& status);
-  void FailedAsyncOpen(const nsresult& status);
-  void HandleAsyncAbort();
+  void OnCancel(const nsresult& status);
   void Redirect1Begin(const PRUint32& newChannelId,
                       const URI& newUri,
                       const PRUint32& redirectFlags,
@@ -205,19 +204,15 @@ private:
   void Redirect3Complete();
   void DeleteSelf();
 
-  // Called asynchronously from Resume: continues any pending calls into client.
-  void CompleteResume();
-
   friend class StartRequestEvent;
   friend class StopRequestEvent;
   friend class TransportAndDataEvent;
   friend class ProgressEvent;
   friend class StatusEvent;
-  friend class FailedAsyncOpenEvent;
+  friend class CancelEvent;
   friend class Redirect1Event;
   friend class Redirect3Event;
   friend class DeleteSelfEvent;
-  friend class HttpAsyncAborter<HttpChannelChild>;
 };
 
 //-----------------------------------------------------------------------------
