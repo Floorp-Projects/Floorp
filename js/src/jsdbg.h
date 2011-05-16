b61ee12a6cf895f11f4127f99256c446bb17ba8a
Jason Orendorff <jorendorff@mozilla.com>
2011-05-16 17:56:22 -0500

    Fix Debug.Frame.prototype.older to skip non-debuggee stack frames.

--- js/src/jsdbg.h
+++ js/src/jsdbg.h
@@ -103,9 +103,6 @@ class Debug {
                                      DebugObservesMethod observesEvent,
                                      DebugHandleMethod handleEvent);
 
-    inline bool observesScope(JSObject *obj) const;
-    inline bool observesFrame(StackFrame *fp) const;
-
     bool observesDebuggerStatement() const;
     JSTrapStatus handleDebuggerStatement(JSContext *cx, Value *vp);
 
@@ -148,6 +145,9 @@ class Debug {
     static inline JSTrapStatus onThrow(JSContext *cx, js::Value *vp);
 
     /**************************************** Functions for use by jsdbg.cpp. */
+
+    inline bool observesScope(JSObject *obj) const;
+    inline bool observesFrame(StackFrame *fp) const;
 
     // Precondition: *vp is a value from a debuggee compartment and cx is in
     // the debugger's compartment.
