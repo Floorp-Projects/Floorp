6ef45c128d4973a6649ab47a36f6b843354806b9
Jason Orendorff <jorendorff@mozilla.com>
2011-05-16 17:20:04 -0500

    Make the throw hook tests pass in the methodjit. Add a simpler version of one of the tests.
    --HG--
    rename : js/src/jit-test/tests/debug/hooks-throw-02.js => js/src/jit-test/tests/debug/hooks-throw-03.js
    rename : js/src/jit-test/tests/debug/hooks-throw-03.js => js/src/jit-test/tests/debug/hooks-throw-04.js

--- js/src/jit-test/tests/debug/hooks-throw-03.js
+++ js/src/jit-test/tests/debug/hooks-throw-03.js
@@ -1,21 +1,60 @@
 // |jit-test| debug
-// hooks.throw is not called for exceptions thrown and handled in the debugger.
+// The throw hook is called multiple times as the stack unwinds.
 
 var g = newGlobal('new-compartment');
-var dbg = Debug(g);
-g.log = '';
-dbg.hooks = {
-    debuggerHandler: function (frame) {
-        try {
-            throw new Error("oops");
-        } catch (exc) {
-            g.log += exc.message;
-        }
-    },
-    throw: function (frame) {
-        g.log += 'BAD';
-    }
-};
+g.debuggeeGlobal = this;
+g.dbg = null;
+g.eval("(" + function () {
+        dbg = new Debug(debuggeeGlobal);
+        dbg.hooks = {
+            throw: function (frame, exc) {
+                assertEq(frame instanceof Debug.Frame, true);
+                assertEq(exc instanceof Debug.Object, true);
+                var s = '!';
+                for (var f = frame; f; f = f.older)
+                    if (f.type === "call")
+                        s += f.callee.name;
+                s += ', ';
+                debuggeeGlobal.log += s;
+            }
+        };
+    } + ")();");
 
-g.eval("debugger; log += ' ok';");
-assertEq(g.log, 'oops ok');
+var log;
+
+function k() {
+    try {
+        throw new Error("oops");  // hook call 1
+    } finally {
+        log += 'k-finally, ';
+    } // hook call 2
+}
+
+function j() {
+    k();  // hook call 3
+    log += 'j-unreached, ';
+}
+
+function h() {
+    try {
+        j();  // hook call 4
+        log += 'h-unreached, ';
+    } catch (exc) {
+        log += 'h-catch, ';
+        throw exc; // hook call 5
+    }
+}
+
+function f() {
+    try {
+        h(); // hook call 6
+    } catch (exc) {
+        log += 'f-catch, ';
+    }
+    log += 'f-after, ';
+}
+
+log = '';
+f();
+g.dbg.enabled = false;
+assertEq(log, '!kjhf, k-finally, !kjhf, !jhf, !hf, h-catch, !hf, !f, f-catch, f-after, ');
