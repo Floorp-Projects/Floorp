2f8d93ea26a390147713eccd494f8aeaddbc2f1d
Tim Taubert <tim.taubert@gmx.de>
2011-05-19 23:44:51 +0200

    Bug 650573 - Panorama hangs when restoring a session; f=raymond, r=sdwilsh

--- browser/base/content/test/tabview/browser_tabview_bug624727.js
+++ browser/base/content/test/tabview/browser_tabview_bug624727.js
@@ -62,7 +62,10 @@ function test() {
   let testStateAfterEnteringPB = function () {
     let prefix = 'enter';
     ok(!pb.privateBrowsingEnabled, prefix + ': private browsing is disabled');
-    registerCleanupFunction(function () pb.privateBrowsingEnabled = false);
+    registerCleanupFunction(function () {
+      if (pb.privateBrowsingEnabled)
+        pb.privateBrowsingEnabled = false
+    });
 
     togglePrivateBrowsing(function () {
       assertTabViewIsHidden(prefix);
