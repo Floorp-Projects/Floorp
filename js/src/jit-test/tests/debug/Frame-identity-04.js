468721308f08c8eb1a67db6603e30bfc8cb71b4f
Jason Orendorff <jorendorff@mozilla.com>
2011-05-20 16:12:26 -0500

    Fix up treatment of Debug.Frame objects during GC. Frames were not being marked properly during Debug::trace and were being swept from the table during Debug::sweepCompartment.

---
+++ js/src/jit-test/tests/debug/Frame-identity-04.js
@@ -0,0 +1,23 @@
+// |jit-test| debug
+// Test that on-stack Debug.Frames are not GC'd even if they are only reachable
+// from the js::Debug::frames table.
+
+var g = newGlobal('new-compartment');
+g.eval("function f(n) { if (n) f(n - 1); debugger; }");
+var dbg = new Debug(g);
+var hits = 0;
+dbg.hooks = {
+    debuggerHandler: function (frame) {
+        if (hits === 0) {
+            for (; frame; frame = frame.older)
+                frame.seen = true;
+        } else {
+            for (; frame; frame = frame.older)
+                assertEq(frame.seen, true);
+        }
+        gc();
+        hits++;
+    }
+};
+g.f(20);
+assertEq(hits, 21);
