08ad523bc4b180a58b8f76d7c42cf8f8b3f8b2f1
Jan de Mooij <jandemooij@gmail.com>
2011-05-20 22:02:54 +0200

    [INFER] Release GC lock after printing inference results to prevent deadlocks, bug 658623. r=bhackett

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -621,12 +621,14 @@ js_DestroyContext(JSContext *cx, JSDestroyContextMode mode)
              * Dump remaining type inference results first. This printing
              * depends on atoms still existing.
              */
-            AutoLockGC lock(rt);
-            JSCompartment **compartment = rt->compartments.begin();
-            JSCompartment **end = rt->compartments.end();
-            while (compartment < end) {
-                (*compartment)->types.print(cx, *compartment);
-                compartment++;
+            {
+                AutoLockGC lock(rt);
+                JSCompartment **compartment = rt->compartments.begin();
+                JSCompartment **end = rt->compartments.end();
+                while (compartment < end) {
+                    (*compartment)->types.print(cx, *compartment);
+                    compartment++;
+                }
             }
 
             js_FinishRuntimeNumberState(cx);
