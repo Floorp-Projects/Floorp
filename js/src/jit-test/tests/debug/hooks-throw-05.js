60cc539771d082e5c3c5a3cf17bc559bbf1d046f
Jason Orendorff <jorendorff@mozilla.com>
2011-05-20 22:59:06 -0500

    Fix a few bugs in exception handling and the throw hook. Make Debug::parseResumptionValue and Debug::handleUncaughtException reliably call ac.leave().

---
+++ js/src/jit-test/tests/debug/hooks-throw-05.js
@@ -0,0 +1,13 @@
+// |jit-test| debug
+// hooks.throw returning undefined does not affect the thrown exception.
+
+var g = newGlobal('new-compartment');
+g.parent = this;
+g.eval("new Debug(parent).hooks = {throw: function () {}};");
+
+var obj = new Error("oops");
+try {
+    throw obj;
+} catch (exc) {
+    assertEq(exc, obj);
+}
