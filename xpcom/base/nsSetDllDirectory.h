6ee4fe0a99de4bd852cd2895b3d940a2f2bb2031
Mike Hommey <mh+mozilla@glandium.org>
2011-05-22 08:22:27 +0200

    Bug 552864 part 1 - Move DLL blocklist in XRE, and inline NS_SetDllDirectory and environment sanitization, which now needs to be called manually. r=ehsan,r=bsmedberg

--- xpcom/base/nsSetDllDirectory.h
+++ xpcom/base/nsSetDllDirectory.h
@@ -43,13 +43,48 @@
 #error This file only makes sense on Windows.
 #endif
 
+#include <windows.h>
 #include <nscore.h>
+#include <stdlib.h>
 
 namespace mozilla {
 
+static void SanitizeEnvironmentVariables()
+{
+  DWORD bufferSize = GetEnvironmentVariableW(L"PATH", NULL, 0);
+  if (bufferSize) {
+    wchar_t* originalPath = new wchar_t[bufferSize];
+    if (bufferSize - 1 == GetEnvironmentVariableW(L"PATH", originalPath, bufferSize)) {
+      bufferSize = ExpandEnvironmentStringsW(originalPath, NULL, 0);
+      if (bufferSize) {
+        wchar_t* newPath = new wchar_t[bufferSize];
+        if (ExpandEnvironmentStringsW(originalPath,
+                                      newPath,
+                                      bufferSize)) {
+          SetEnvironmentVariableW(L"PATH", newPath);
+        }
+        delete[] newPath;
+      }
+    }
+    delete[] originalPath;
+  }
+}
+
 // Sets the directory from which DLLs can be loaded if the SetDllDirectory OS
 // API is available.
-XPCOM_API(void) NS_SetDllDirectory(const WCHAR *aDllDirectory);
+// You must call SanitizeEnvironmentVariables before this function when calling
+// it the first time.
+static inline void NS_SetDllDirectory(const WCHAR *aDllDirectory)
+{
+  typedef BOOL
+  (WINAPI *pfnSetDllDirectory) (LPCWSTR);
+  pfnSetDllDirectory setDllDirectory = nsnull;
+  setDllDirectory = reinterpret_cast<pfnSetDllDirectory>
+      (GetProcAddress(GetModuleHandleW(L"kernel32.dll"), "SetDllDirectoryW"));
+  if (setDllDirectory) {
+    setDllDirectory(aDllDirectory);
+  }
+}
 
 }
 
