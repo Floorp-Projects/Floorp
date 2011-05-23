26cc57671217e38d94682c468731b5c11a1ad60a
Jason Orendorff <jorendorff@mozilla.com>
2011-05-23 17:07:26 -0500

    Fix Debug.Object.prototype.name to return undefined if the object is not a function or is a nameless function.

---
+++ js/src/jit-test/tests/debug/Object-name-02.js
@@ -0,0 +1,18 @@
+// |jit-test| debug
+// The .name of a non-function object is undefined.
+
+var g = newGlobal('new-compartment');
+var hits = 0;
+var dbg = new Debug(g);
+dbg.hooks = {
+    debuggerHandler: function (frame) {
+        assertEq(frame.arguments[0].name, undefined);
+        hits++;
+    }
+};
+g.eval("function f(nonfunction) { debugger; }");
+g.eval("f({});");
+g.eval("f(/a*/);");
+g.eval("f({name: 'bad'});");
+g.eval("f(Proxy.createFunction({name: {value: 'bad'}}, function () {}));");
+assertEq(hits, 4);
