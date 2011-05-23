ba4d8c0d0cd42fb004108d09913f4d5b502afaa3
Kyle Huey <khuey@kylehuey.com>
2011-05-23 13:04:45 -0700

    Bug 657964: FileReader should throw when abort() is called and readystate is not LOADING. r=sicking

--- content/base/src/nsDOMFileReader.cpp
+++ content/base/src/nsDOMFileReader.cpp
@@ -271,7 +271,7 @@ NS_IMETHODIMP
 nsDOMFileReader::Abort()
 {
   if (mReadyState != nsIDOMFileReader::LOADING)
-    return NS_OK;
+    return NS_ERROR_DOM_FILE_ABORT_ERR;
 
   //Clear progress and file data
   mProgressEventWasDelayed = PR_FALSE;
