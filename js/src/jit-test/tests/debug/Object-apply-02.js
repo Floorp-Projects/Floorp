b86352266317866db629c2e44785735914996518
Jason Orendorff <jorendorff@mozilla.com>
2011-05-24 17:12:43 -0500

    Add Debug.Object.prototype.call.

--- js/src/jit-test/tests/debug/Object-apply-02.js
+++ js/src/jit-test/tests/debug/Object-apply-02.js
@@ -1,5 +1,5 @@
 // |jit-test| debug
-// tests calling native functions via Debug.Object.prototype.apply
+// tests calling native functions via Debug.Object.prototype.apply/call
 
 load(libdir + "asserts.js");
 
@@ -8,41 +8,47 @@ g.eval("function f() { debugger; }");
 var dbg = new Debug(g);
 dbg.hooks = {debuggerHandler: function () {}};
 
-dbg.hooks.debuggerHandler = function (frame) {
-    var max = frame.arguments[0];
-    var cv = max.apply(null, [9, 16]);
-    assertEq(cv.return, 16);
+function test(usingApply) {
+    dbg.hooks.debuggerHandler = function (frame) {
+	var max = frame.arguments[0];
+	var cv = usingApply ? max.apply(null, [9, 16]) : max.call(null, 9, 16);
+	assertEq(cv.return, 16);
 
-    cv = max.apply();
-    assertEq(cv.return, -1/0);
+	cv = usingApply ? max.apply() : max.call();
+	assertEq(cv.return, -1/0);
 
-    cv = max.apply(null, [2, 5, 3, 8, 1, 9, 4, 6, 7]);
-    assertEq(cv.return, 9);
+	cv = usingApply ? max.apply(null, [2, 5, 3, 8, 1, 9, 4, 6, 7])
+                        : max.call(null, 2, 5, 3, 8, 1, 9, 4, 6, 7);
+	assertEq(cv.return, 9);
 
-    // second argument to apply must be an array
-    assertThrowsInstanceOf(function () { max.apply(null, 12); }, TypeError);
-};
-g.eval("f(Math.max);");
+	// second argument to apply must be an array
+	assertThrowsInstanceOf(function () { max.apply(null, 12); }, TypeError);
+    };
+    g.eval("f(Math.max);");
 
-dbg.hooks.debuggerHandler = function (frame) {
-    var push = frame.arguments[0];
-    var arr = frame.arguments[1];
-    var cv;
+    dbg.hooks.debuggerHandler = function (frame) {
+	var push = frame.arguments[0];
+	var arr = frame.arguments[1];
+	var cv;
 
-    cv = push.apply(arr, [0, 1, 2]);
-    assertEq(cv.return, 3);
+	cv = usingApply ? push.apply(arr, [0, 1, 2]) : push.call(arr, 0, 1, 2);
+	assertEq(cv.return, 3);
 
-    cv = push.apply(arr, [arr]);
-    assertEq(cv.return, 4);
+	cv = usingApply ? push.apply(arr, [arr]) : push.call(arr, arr);
+	assertEq(cv.return, 4);
 
-    cv = push.apply(arr);
-    assertEq(cv.return, 4);
+	cv = usingApply ? push.apply(arr) : push.call(arr);
+	assertEq(cv.return, 4);
 
-    // you can apply Array.prototype.push to a string; it does ToObject on it.
-    cv = push.apply("hello", ["world"]);
-    assertEq(cv.return, 6);
-};
-g.eval("var a = []; f(Array.prototype.push, a);");
-assertEq(g.a.length, 4);
-assertEq(g.a.slice(0, 3).join(","), "0,1,2");
-assertEq(g.a[3], g.a);
+	// you can apply Array.prototype.push to a string; it does ToObject on it.
+	cv = usingApply ? push.apply("hello", ["world"]) : push.call("hello", "world");
+	assertEq(cv.return, 6);
+    };
+    g.eval("var a = []; f(Array.prototype.push, a);");
+    assertEq(g.a.length, 4);
+    assertEq(g.a.slice(0, 3).join(","), "0,1,2");
+    assertEq(g.a[3], g.a);
+}
+
+test(true);
+test(false);
