6a2355a4891cee3b4dea5dddb1fb29cba267223a
Daniel Holbert <dholbert@cs.stanford.edu>
2011-05-24 03:36:31 -0700

    Bug 658949 patch 0: Fix existing tests that use "#" in data URIs to use %23 instead. rs=bz

--- intl/uconv/tests/unit/test_bug90411.js
+++ intl/uconv/tests/unit/test_bug90411.js
@@ -7,7 +7,7 @@
  * 我听见 我忘记; 我看见 我记住; 我做 我了解。
  */
 
-const sample = "~{NJK{K-JGI59OAK~}5~{7VVS!#5+JG#,K{K-2;R*GsHTH;JGR;8vI59OS@T6!#~} ~{NRL}<{~} ~{NRM|<G~}; ~{NR?4<{~} ~{NR<GW!~}; ~{NRWv~} ~{NRAK=b!#~}";
+const sample = "~{NJK{K-JGI59OAK~}5~{7VVS!%235+JG%23,K{K-2;R*GsHTH;JGR;8vI59OS@T6!%23~} ~{NRL}<{~} ~{NRM|<G~}; ~{NR?4<{~} ~{NR<GW!~}; ~{NRWv~} ~{NRAK=b!%23~}";
 
 const expected = "\u95EE\u4ED6\u8C01\u662F\u50BB\u74DC\u4E865\u5206\u949F\u3002\u4F46\u662F\uFF0C\u4ED6\u8C01\u4E0D\u8981\u6C42\u4ECD\u7136\u662F\u4E00\u4E2A\u50BB\u74DC\u6C38\u8FDC\u3002 \u6211\u542C\u89C1 \u6211\u5FD8\u8BB0; \u6211\u770B\u89C1 \u6211\u8BB0\u4F4F; \u6211\u505A \u6211\u4E86\u89E3\u3002"; 
 
