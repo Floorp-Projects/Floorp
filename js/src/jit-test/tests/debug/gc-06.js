f408e45e7272e2d62c8c6c3e913a249989633079
Jason Orendorff <jorendorff@mozilla.com>
2011-05-24 22:56:09 -0500

    Two passing tests: Debug objects do not keep debuggee globals alive; and addDebuggee does not assert if the target compartment is not in debug mode.

---
+++ js/src/jit-test/tests/debug/gc-06.js
@@ -0,0 +1,8 @@
+// |jit-test| debug
+// Debug objects do not keep debuggee globals live.
+
+var dbg = new Debug;
+for (var i = 0; i < 4; i++)
+    dbg.addDebuggee(newGlobal('new-compartment'));
+gc();
+assertEq(dbg.getDebuggees().length <= 1, true);
