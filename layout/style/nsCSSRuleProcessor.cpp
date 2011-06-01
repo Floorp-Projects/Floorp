a164a04d18604493253e3f59a8a21153529c56c0
Boris Zbarsky <bzbarsky@mit.edu>
2011-05-31 21:46:56 -0400

    Bug 598833 part 6.  Get rid of nsEventStateManager::GetContentState.  r=smaug

--- layout/style/nsCSSRuleProcessor.cpp
+++ layout/style/nsCSSRuleProcessor.cpp
@@ -1149,14 +1149,7 @@ static void GetLang(nsIContent* aContent, nsString& aLang)
 nsEventStates
 nsCSSRuleProcessor::GetContentState(Element* aElement)
 {
-  nsIPresShell* shell = aElement->GetOwnerDoc()->GetShell();
-  nsPresContext* presContext;
-  nsEventStates state;
-  if (shell && (presContext = shell->GetPresContext())) {
-    state = presContext->EventStateManager()->GetContentState(aElement);
-  } else {
-    state = aElement->IntrinsicState();
-  }
+  nsEventStates state = aElement->State();
 
   // If we are not supposed to mark visited links as such, be sure to
   // flip the bits appropriately.  We want to do this here, rather
