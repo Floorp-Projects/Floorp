24bfe843c45a7a96cc2b59f8528aad80b907e322
Mike Hommey <mh+mozilla@glandium.org>
2011-06-02 10:00:13 +0200

    Bug 652657 - Allow to opt-in for telemetry reporting. r=gavin,ui-r=limi

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -376,8 +376,13 @@ BrowserGlue.prototype = {
   // Browser startup complete. All initial windows have opened.
   _onBrowserStartup: function BG__onBrowserStartup() {
     // Show about:rights notification, if needed.
-    if (this._shouldShowRights())
+    if (this._shouldShowRights()) {
       this._showRightsNotification();
+    } else {
+      // Only show telemetry notification when about:rights notification is not shown.
+      this._showTelemetryNotification();
+    }
+
 
     // Show update notification, if needed.
     if (Services.prefs.prefHasUserValue("app.update.postupdate"))
@@ -733,6 +738,72 @@ BrowserGlue.prototype = {
     }
     catch (e) {
     }
+  },
+
+  _showTelemetryNotification: function BG__showTelemetryNotification() {
+    const PREF_TELEMETRY_PROMPTED = "toolkit.telemetry.prompted";
+    const PREF_TELEMETRY_ENABLED  = "toolkit.telemetry.enabled";
+    const PREF_TELEMETRY_INFOURL  = "toolkit.telemetry.infoURL";
+    const PREF_TELEMETRY_SERVER_OWNER = "toolkit.telemetry.server_owner";
+
+    try {
+      // If the user hasn't already been prompted, ask if they want to
+      // send telemetry data.
+      if (Services.prefs.getBoolPref(PREF_TELEMETRY_ENABLED) ||
+          Services.prefs.getBoolPref(PREF_TELEMETRY_PROMPTED))
+         return;
+    } catch(e) {}
+
+    // Stick the notification onto the selected tab of the active browser window.
+    var win = this.getMostRecentBrowserWindow();
+    var browser = win.gBrowser; // for closure in notification bar callback
+    var notifyBox = browser.getNotificationBox();
+
+    var browserBundle   = Services.strings.createBundle("chrome://browser/locale/browser.properties");
+    var brandBundle     = Services.strings.createBundle("chrome://branding/locale/brand.properties");
+
+    var productName        = brandBundle.GetStringFromName("brandFullName");
+    var serverOwner        = Services.prefs.getCharPref(PREF_TELEMETRY_SERVER_OWNER);
+    var telemetryText      = browserBundle.formatStringFromName("telemetryText", [productName, serverOwner], 2);
+
+    var buttons = [
+                    {
+                      label:     browserBundle.GetStringFromName("telemetryYesButtonLabel"),
+                      accessKey: browserBundle.GetStringFromName("telemetryYesButtonAccessKey"),
+                      popup:     null,
+                      callback:  function(aNotificationBar, aButton) {
+                        Services.prefs.setBoolPref(PREF_TELEMETRY_ENABLED, true);
+                      }
+                    },
+                    {
+                      label:     browserBundle.GetStringFromName("telemetryNoButtonLabel"),
+                      accessKey: browserBundle.GetStringFromName("telemetryNoButtonAccessKey"),
+                      popup:     null,
+                      callback:  function(aNotificationBar, aButton) {}
+                    }
+                  ];
+
+    // Set pref to indicate we've shown the notification.
+    Services.prefs.setBoolPref(PREF_TELEMETRY_PROMPTED, true);
+
+    var notification = notifyBox.appendNotification(telemetryText, "telemetry", null, notifyBox.PRIORITY_INFO_LOW, buttons);
+    notification.persistence = 3; // arbitrary number, just so bar sticks around for a bit
+
+    let XULNS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+    let link = notification.ownerDocument.createElementNS(XULNS, "label");
+    link.className = "text-link telemetry-text-link";
+    link.setAttribute("value", browserBundle.GetStringFromName("telemetryLinkLabel"));
+    link.addEventListener('click', function() {
+      // Open the learn more url in a new tab
+      browser.selectedTab = browser.addTab(Services.prefs.getCharPref(PREF_TELEMETRY_INFOURL));
+      // Remove the notification on which the user clicked
+      notification.parentNode.removeNotification(notification, true);
+      // Add a new notification to that tab, with no "Learn more" link
+      var notifyBox = browser.getNotificationBox();
+      notifyBox.appendNotification(telemetryText, "telemetry", null, notifyBox.PRIORITY_INFO_LOW, buttons);
+    }, false);
+    let description = notification.ownerDocument.getAnonymousElementByAttribute(notification, "anonid", "messageText");
+    description.appendChild(link);
   },
 
   _showPluginUpdatePage: function BG__showPluginUpdatePage() {
