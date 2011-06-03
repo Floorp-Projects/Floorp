8367589869e487c0b679ffef79090d92bd3bd058
Luke Wagner <luke@mozilla.com>
2011-06-02 18:39:48 -0700

    Dial down recursion in new test added by abd2dcd555f4 so as to not chew up so much C stack when the mjit is disabled (r=orange)

--- js/src/jit-test/tests/basic/test-apply-many-args.js
+++ js/src/jit-test/tests/basic/test-apply-many-args.js
@@ -5,7 +5,10 @@ function f(x) {
     f.apply(null, arguments);
 }
 
-a = [100];
+// When the apply-optimization isn't on, each recursive call chews up the C
+// stack, so don't push it.
+a = [20];
+
 for (var i = 0; i < 2000; ++i)
   a.push(i);
 f.apply(null, a);
