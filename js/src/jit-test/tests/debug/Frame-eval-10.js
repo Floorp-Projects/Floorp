86616ff0df0e999efd49046e3ba58af96712f6f4
Jason Orendorff <jorendorff@mozilla.com>
2011-06-02 20:41:16 -0500

    Add passing test.

---
+++ js/src/jit-test/tests/debug/Frame-eval-10.js
@@ -0,0 +1,14 @@
+// frame.eval returns null if the eval code fails with an uncatchable error.
+var g = newGlobal('new-compartment');
+var dbg = Debug(g);
+var hits = 0;
+dbg.hooks = {
+    debuggerHandler: function (frame) {
+	if (hits++ === 0)
+	    assertEq(frame.eval("debugger;"), null);
+	else
+	    return null;
+    }
+};
+assertEq(g.eval("debugger; 'ok';"), "ok");
+assertEq(hits, 2);
