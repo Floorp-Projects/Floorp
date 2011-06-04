338feb13848afafd3e5480907a53542256b26695
Ms2ger <ms2ger@gmail.com>
2011-06-04 08:57:23 +0200

    Bug 660669 - Remove nsHTMLDocument::OpenCommon; r=bz

--- content/html/document/src/nsHTMLDocument.h
+++ content/html/document/src/nsHTMLDocument.h
@@ -233,8 +233,6 @@ protected:
 
   nsresult WriteCommon(JSContext *cx, const nsAString& aText,
                        PRBool aNewlineTerminate);
-  nsresult OpenCommon(JSContext *cx, const nsAString& aContentType,
-                      PRBool aReplace);
 
   nsresult CreateAndAddWyciwygChannel(void);
   nsresult RemoveWyciwygChannel(void);
