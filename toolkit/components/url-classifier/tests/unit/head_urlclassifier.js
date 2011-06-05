a915bbd5b3ee097c23c39051e4c47793a6389efa
Mehdi Mulani <mmmulani@uwaterloo.ca>
2011-06-04 23:00:22 -0400

    Bug 652294 - Fix intermittent failure in test_hashcompleter.js by checking for duplicates and adding a RNG. r=dcamp

--- toolkit/components/url-classifier/tests/unit/head_urlclassifier.js
+++ toolkit/components/url-classifier/tests/unit/head_urlclassifier.js
@@ -304,4 +304,33 @@ notify: function(timer) {
   }
 }
 
+// LFSRgenerator is a 32-bit linear feedback shift register random number
+// generator. It is highly predictable and is not intended to be used for
+// cryptography but rather to allow easier debugging than a test that uses
+// Math.random().
+function LFSRgenerator(seed) {
+  // Force |seed| to be a number.
+  seed = +seed;
+  // LFSR generators do not work with a value of 0.
+  if (seed == 0)
+    seed = 1;
+
+  this._value = seed;
+}
+LFSRgenerator.prototype = {
+  // nextNum returns a random unsigned integer of in the range [0,2^|bits|].
+  nextNum: function(bits) {
+    if (!bits)
+      bits = 32;
+
+    let val = this._value;
+    // Taps are 32, 22, 2 and 1.
+    let bit = ((val >>> 0) ^ (val >>> 10) ^ (val >>> 30) ^ (val >>> 31)) & 1;
+    val = (val >>> 1) | (bit << 31);
+    this._value = val;
+
+    return (val >>> (32 - bits));
+  },
+};
+
 cleanUp();
