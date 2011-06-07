0695a567acbba074f8c3020a07e360ce05bbf116
Brian Hackett <bhackett1024@gmail.com>
2011-06-07 16:33:25 -0700

    [INFER] Fix no-op propagation of deoptimization flags for array comprehensions, bug 660538.

---
+++ js/src/jit-test/tests/basic/bug660538.js
@@ -0,0 +1,11 @@
+
+Error.prototype.__proto__.p = 5;
+f = Function("return( \"\" <arguments for(w in[]))");
+for (i in f()) {}
+
+(function() {
+    for (a in (
+        arguments for (l in [0])
+    ))
+    {}
+})()
