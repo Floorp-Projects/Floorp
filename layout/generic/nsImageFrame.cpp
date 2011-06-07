f1f4bb45b52d178d32640515a725a3fc55ccc033
Masayuki Nakano <masayuki@d-toybox.com>
2011-06-07 16:44:56 +0900

    Bug 661101 backout a part of 9014863a7e2a which was included to the mq file unexpectedly

--- layout/generic/nsImageFrame.cpp
+++ layout/generic/nsImageFrame.cpp
@@ -1887,7 +1887,6 @@ static const char* kIconLoadPrefs[] = {
 
 nsImageFrame::IconLoad::IconLoad()
 {
-  mHasShutdown = PR_FALSE;
   // register observers
   Preferences::AddStrongObservers(this, kIconLoadPrefs);
   GetPrefs();
@@ -1896,10 +1895,6 @@ nsImageFrame::IconLoad::IconLoad()
 void
 nsImageFrame::IconLoad::Shutdown()
 {
-  if (mHasShutdown) {
-    return;
-  }
-  mHasShutdown = PR_TRUE;
   Preferences::RemoveObservers(this, kIconLoadPrefs);
   // in case the pref service releases us later
   if (mLoadingImage) {
