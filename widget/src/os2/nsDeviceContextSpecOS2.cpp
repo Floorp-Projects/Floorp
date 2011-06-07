d6fe1821fdcc76a6386220359b2a7f1471189a34
Masayuki Nakano <masayuki@d-toybox.com>
2011-06-07 18:42:54 +0900

    Bug 661101 fix a nit of 9014863a7e2a in widget/src/os2/nsDeviceContextSpecOS2.cpp r=roc DONTBUILD

--- widget/src/os2/nsDeviceContextSpecOS2.cpp
+++ widget/src/os2/nsDeviceContextSpecOS2.cpp
@@ -660,7 +660,7 @@ nsresult GlobalPrinters::InitializeGlobalPrinters ()
      return NS_ERROR_OUT_OF_MEMORY;
 
   // don't return on failure, optional feature
-  BOOL prefFailed = (Preferences::GetRootBranch() != nsnull);
+  BOOL prefFailed = (Preferences::GetRootBranch() == nsnull);
 
   for (ULONG i = 0; i < mGlobalNumPrinters; i++) {
     nsXPIDLCString printer;
