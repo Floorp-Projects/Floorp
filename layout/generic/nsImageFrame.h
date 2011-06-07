ff37b945be472bf4e2b1b9919fbf986d84304d4d
Masayuki Nakano <masayuki@d-toybox.com>
2011-06-07 16:44:56 +0900

    Bug 661101 backout a part of 9014863a7e2a which was included to the mq file unexpectedly

--- layout/generic/nsImageFrame.h
+++ layout/generic/nsImageFrame.h
@@ -327,10 +327,6 @@ private:
     // broken image and loading image icons
   public:
     IconLoad();
-    ~IconLoad()
-    {
-      Shutdown();
-    }
 
     void Shutdown();
 
@@ -363,7 +359,6 @@ private:
     nsCOMPtr<imgIRequest> mBrokenImage;
     PRPackedBool     mPrefForceInlineAltText;
     PRPackedBool     mPrefShowPlaceholders;
-    PRPackedBool     mHasShutdown;
   };
   
 public:
