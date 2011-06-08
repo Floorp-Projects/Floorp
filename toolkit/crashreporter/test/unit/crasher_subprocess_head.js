60f6fc84576c33d17bf5aaa77daf3101e6bbf340
Josh Matthews <josh@joshmatthews.net>
2011-06-08 16:21:20 -0400

    Bug 581341 - Part 5: Test. r=ted

--- toolkit/crashreporter/test/unit/crasher_subprocess_head.js
+++ toolkit/crashreporter/test/unit/crasher_subprocess_head.js
@@ -2,11 +2,19 @@
 let cwd = Components.classes["@mozilla.org/file/directory_service;1"]
       .getService(Components.interfaces.nsIProperties)
       .get("CurWorkD", Components.interfaces.nsILocalFile);
+
 let crashReporter =
   Components.classes["@mozilla.org/toolkit/crash-reporter;1"]
     .getService(Components.interfaces.nsICrashReporter);
-crashReporter.enabled = true;
-crashReporter.minidumpPath = cwd;
+
+// the crash reporter is already enabled in content processes,
+// and setting the minidump path is not allowed
+let processType = Components.classes["@mozilla.org/xre/runtime;1"].
+      getService(Components.interfaces.nsIXULRuntime).processType;
+if (processType == Components.interfaces.nsIXULRuntime.PROCESS_TYPE_DEFAULT) {
+  crashReporter.enabled = true;
+  crashReporter.minidumpPath = cwd;
+}
 
 let ios = Components.classes["@mozilla.org/network/io-service;1"]
             .getService(Components.interfaces.nsIIOService);
