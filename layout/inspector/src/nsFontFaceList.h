6e0f6efdfdf7735115f021bb7e459e8ebbcd1aa7
Jonathan Kew <jfkthame@gmail.com>
2011-06-15 20:17:50 +0100

    bug 467669 - pt 1 - get list of fonts actually used to render a document range. r=roc sr=bzbarsky
    --HG--
    extra : rebase_source : 3d7699c4a201bafdfa1d97ba97117a497303c1b5

--- layout/inspector/src/nsFontFaceList.h
+++ layout/inspector/src/nsFontFaceList.h
@@ -13,13 +13,12 @@
  *
  * The Original Code is mozilla.org code.
  *
- * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 2001
+ * The Initial Developer of the Original Code is Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2011
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *   Joe Hewitt <hewitt@netscape.com> (original author)
+ *   Jonathan Kew <jfkthame@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -35,37 +34,32 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#ifndef __inDOMUtils_h__
-#define __inDOMUtils_h__
+#ifndef __nsFontFaceList_h__
+#define __nsFontFaceList_h__
 
-#include "inIDOMUtils.h"
+#include "nsIDOMFontFaceList.h"
+#include "nsIDOMFontFace.h"
+#include "nsCOMPtr.h"
+#include "nsInterfaceHashtable.h"
+#include "nsHashKeys.h"
+#include "gfxFont.h"
 
-#include "nsISupportsArray.h"
+class gfxTextRun;
 
-class nsRuleNode;
-class nsStyleContext;
-class nsIAtom;
-class nsIContent;
-
-class inDOMUtils : public inIDOMUtils
+class nsFontFaceList : public nsIDOMFontFaceList
 {
 public:
   NS_DECL_ISUPPORTS
-  NS_DECL_INIDOMUTILS
+  NS_DECL_NSIDOMFONTFACELIST
 
-  inDOMUtils();
-  virtual ~inDOMUtils();
+  nsFontFaceList();
+  virtual ~nsFontFaceList();
 
-private:
-  // aStyleContext must be released by the caller once he's done with aRuleNode.
-  static nsresult GetRuleNodeForContent(nsIContent* aContent,
-                                        nsIAtom* aPseudo,
-                                        nsStyleContext** aStyleContext,
-                                        nsRuleNode** aRuleNode);
+  nsresult AddFontsFromTextRun(gfxTextRun* aTextRun,
+                               PRUint32 aOffset, PRUint32 aLength);
+
+protected:
+  nsInterfaceHashtable<nsPtrHashKey<gfxFontEntry>,nsIDOMFontFace> mFontFaces;
 };
 
-// {0a499822-a287-4089-ad3f-9ffcd4f40263}
-#define IN_DOMUTILS_CID \
-  {0x0a499822, 0xa287, 0x4089, {0xad, 0x3f, 0x9f, 0xfc, 0xd4, 0xf4, 0x02, 0x63}}
-
-#endif // __inDOMUtils_h__
+#endif // __nsFontFaceList_h__
