ebb4d3b611df956543d56b3d711be7c40147e53b
Simon Montagu <smontagu@smontagu.org>
2011-06-15 10:51:36 +0300

    Don't overshoot the last sibling when iterating frames. Bug 663295 (whitespace change with correct bug number)

--- layout/base/nsBidiPresUtils.cpp
+++ layout/base/nsBidiPresUtils.cpp
@@ -805,7 +805,7 @@ nsBidiPresUtils::TraverseFrames(nsBlockFrame*              aBlockFrame,
                * If the frame ends before the new line, save the text and move
                * into the next continuation
                */
-              while (end < endLine && nextSibling) {
+              while (end < endLine && nextSibling) { 
                 mBuffer.Append(Substring(text, start, end - start));
 
                 AdvanceAndAppendFrame(&frame, aLineIter, &nextSibling);
