67f7adb80a5e2621f01d2440aba7a78ec4d3d43e
Jonathan Kew <jfkthame@gmail.com>
2011-06-15 20:17:51 +0100

    bug 467669 - pt 3 - track which stage of font matching led to selection of a particular face. r=roc

--- layout/inspector/src/nsFontFace.h
+++ layout/inspector/src/nsFontFace.h
@@ -49,15 +49,21 @@ public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIDOMFONTFACE
 
-  nsFontFace(gfxFontEntry* aFontEntry,
+  nsFontFace(gfxFontEntry*      aFontEntry,
+             PRUint8            aMatchInfo,
              nsCSSFontFaceRule* aRule);
   virtual ~nsFontFace();
 
   gfxFontEntry* GetFontEntry() const { return mFontEntry.get(); }
 
+  void AddMatchType(PRUint8 aMatchType) {
+    mMatchType |= aMatchType;
+  }
+
 protected:
   nsRefPtr<gfxFontEntry> mFontEntry;
   nsRefPtr<nsCSSFontFaceRule> mRule;
+  PRUint8 mMatchType;
 };
 
 #endif // __nsFontFace_h__
