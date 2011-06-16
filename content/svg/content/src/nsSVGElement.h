5402b359947275665b93eb9fe22aad28a3fc3e3b
Jonathan Watt <jwatt@jwatt.org>
2011-06-16 11:53:13 +0100

    Bug 664130 - const-ify some SVG element class methods. r=dholbert.

--- content/svg/content/src/nsSVGElement.h
+++ content/svg/content/src/nsSVGElement.h
@@ -152,13 +152,13 @@ public:
   // Gets the element that establishes the rectangular viewport against which
   // we should resolve percentage lengths (our "coordinate context"). Returns
   // nsnull for outer <svg> or SVG without an <svg> parent (invalid SVG).
-  nsSVGSVGElement* GetCtx();
+  nsSVGSVGElement* GetCtx() const;
 
   /**
    * Returns aMatrix post-multiplied by the transform from the userspace
    * established by this element to the userspace established by its parent.
    */
-  virtual gfxMatrix PrependLocalTransformTo(const gfxMatrix &aMatrix);
+  virtual gfxMatrix PrependLocalTransformTo(const gfxMatrix &aMatrix) const;
 
   // Setter for to set the current <animateMotion> transformation
   // Only visible for nsSVGGraphicElement, so it's a no-op here, and that
