d58153e873a6bb5fe449b7a68401dc564d7f3708
Jezreel Ng <jezreel@gmail.com>
2011-06-17 12:37:39 -0700

    Bug 588506 - nsSessionStartup is keeping restored session in memory [r=zpao]

--- browser/components/sessionstore/src/nsSessionStartup.js
+++ browser/components/sessionstore/src/nsSessionStartup.js
@@ -157,18 +157,16 @@ SessionStartup.prototype = {
     else
       this._iniString = null; // reset the state string
 
-    if (this.doRestore()) {
-      // wait for the first browser window to open
+    // wait for the first browser window to open
+    // Don't reset the initial window's default args (i.e. the home page(s))
+    // if all stored tabs are pinned.
+    if (this.doRestore() &&
+        (!initialState.windows ||
+        !initialState.windows.every(function (win)
+           win.tabs.every(function (tab) tab.pinned))))
+      Services.obs.addObserver(this, "domwindowopened", true);
 
-      // Don't reset the initial window's default args (i.e. the home page(s))
-      // if all stored tabs are pinned.
-      if (!initialState.windows ||
-          !initialState.windows.every(function (win)
-             win.tabs.every(function (tab) tab.pinned)))
-        Services.obs.addObserver(this, "domwindowopened", true);
-
-      Services.obs.addObserver(this, "sessionstore-windows-restored", true);
-    }
+    Services.obs.addObserver(this, "sessionstore-windows-restored", true);
   },
 
   /**
@@ -200,18 +198,9 @@ SessionStartup.prototype = {
       break;
     case "sessionstore-windows-restored":
       Services.obs.removeObserver(this, "sessionstore-windows-restored");
-      // We only want to start listening for the purge notification after we've
-      // sessionstore has finished its initial startup. That way we won't observe
-      // the purge notification & clear the old session before sessionstore loads
-      // it (in the case of a crash).
-      Services.obs.addObserver(this, "browser:purge-session-history", true);
-      break;
-    case "browser:purge-session-history":
-      // reset all state on sanitization
+      // free _iniString after nsSessionStore is done with it
       this._iniString = null;
       this._sessionType = Ci.nsISessionStartup.NO_SESSION;
-      // no need in repeating this, since startup state won't change
-      Services.obs.removeObserver(this, "browser:purge-session-history");
       break;
     }
   },
