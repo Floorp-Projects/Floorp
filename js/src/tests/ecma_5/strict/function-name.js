17b65a0cad4154951817a7f8de00ce08c511f195
Tom Schuster <evilpies@gmail.com>
2011-06-18 00:55:55 +0200

    Bug 640593 - Remove Function.arity and other cleanup around function properties r=jorendorff

--- js/src/tests/ecma_5/strict/function-name.js
+++ js/src/tests/ecma_5/strict/function-name.js
@@ -16,11 +16,4 @@ assertEq(testLenientAndStrict('var f = fn(); delete f.name',
                               returns(false), raisesException(TypeError)),
          true);
 
-assertEq(testLenientAndStrict('var f = fn(); f.arity = 4; f.arity',
-                              returns(3), raisesException(TypeError)),
-         true);
-assertEq(testLenientAndStrict('var f = fn(); delete f.arity',
-                              returns(false), raisesException(TypeError)),
-         true);
-
 reportCompare(true, true);
