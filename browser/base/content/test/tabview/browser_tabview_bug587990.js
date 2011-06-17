6a27222e87eee2a80069973644e9588b886ee428
Tim Taubert <tim.taubert@gmx.de>
2011-06-17 14:07:59 +0200

    Bug 663795 - clean up browser_tabview_bug587990.js; f=raymond, r=dietrich

--- browser/base/content/test/tabview/browser_tabview_bug587990.js
+++ browser/base/content/test/tabview/browser_tabview_bug587990.js
@@ -1,69 +1,31 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-let newTab;
-let win;
-
 function test() {
-  waitForExplicitFinish();
+  let newTab;
 
-  win = window.openDialog(getBrowserURL(), "_blank", "all,dialog=no", "about:blank");
-
-  let onLoad = function() {
-    win.removeEventListener("load", onLoad, false);
+  let onLoad = function (win) {
+    registerCleanupFunction(function () win.close());
 
     newTab = win.gBrowser.addTab();
 
-    let onTabViewFrameInitialized = function() {
-      win.removeEventListener(
-        "tabviewframeinitialized", onTabViewFrameInitialized, false);
+    let popup = win.document.getElementById("context_tabViewMenuPopup");
+    win.TabView.updateContextMenu(newTab, popup);
+  };
 
-      win.addEventListener("tabviewshown", onTabViewWindowLoaded, false);
-      win.TabView.toggle();
-    }
-    win.addEventListener(
-      "tabviewframeinitialized", onTabViewFrameInitialized, false);
-    win.TabView.updateContextMenu(
-      newTab, win.document.getElementById("context_tabViewMenuPopup"));
-  }
-  win.addEventListener("load", onLoad, false);
-}
+  let onShow = function (win) {
+    let cw = win.TabView.getContentWindow();
+    is(cw.GroupItems.groupItems.length, 1, "Has only one group");
 
-function onTabViewWindowLoaded() {
-  win.removeEventListener("tabviewshown", onTabViewWindowLoaded, false);
+    let groupItem = cw.GroupItems.groupItems[0];
+    let tabItems = groupItem.getChildren();
 
-  ok(win.TabView.isVisible(), "Tab View is visible");
+    let tab = tabItems[tabItems.length - 1].tab;
+    is(tab, newTab, "The new tab exists in the group");
 
-  let contentWindow = win.document.getElementById("tab-view").contentWindow;
-
-  is(contentWindow.GroupItems.groupItems.length, 1, "Has only one group");
-
-  let groupItem = contentWindow.GroupItems.groupItems[0];
-  let tabItems = groupItem.getChildren();
-
-  is(tabItems[tabItems.length - 1].tab, newTab, "The new tab exists in the group");
-
-  win.gBrowser.removeTab(newTab);
-  whenWindowObservesOnce(win, "domwindowclosed", function() {
     finish();
-  });
-  win.close();
-}
+  };
 
-function whenWindowObservesOnce(win, topic, func) {
-    let windowWatcher = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
-      .getService(Components.interfaces.nsIWindowWatcher);
-    let origWin = win;
-    let origTopic = topic;
-    let origFunc = func;        
-    function windowObserver(aSubject, aTopic, aData) {
-      let theWin = aSubject.QueryInterface(Ci.nsIDOMWindow);
-      if (origWin && theWin != origWin)
-        return;
-      if(aTopic == origTopic) {
-          windowWatcher.unregisterNotification(windowObserver);
-          origFunc.apply(this, []);
-      }
-    }
-    windowWatcher.registerNotification(windowObserver);
+  waitForExplicitFinish();
+  newWindowWithTabView(onShow, onLoad);
 }
