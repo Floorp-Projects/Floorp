5e7764d4abb1b8ade655cd07e08aa5c8e8146ba3
Ed Morley <bmo@edmorley.co.uk>
2011-06-18 11:04:30 +0200

    Bug 664311 - Remove WinCE code from gfx/; r=jmuizelaar

--- gfx/src/nsSystemFontsWin.cpp
+++ gfx/src/nsSystemFontsWin.cpp
@@ -112,15 +112,6 @@ nsresult nsSystemFontsWin::GetSysFontInfo(HDC aHDC, nsSystemFontID anID,
   LOGFONTW logFont;
   LOGFONTW* ptrLogFont = NULL;
 
-#ifdef WINCE
-  hGDI = ::GetStockObject(SYSTEM_FONT);
-  if (hGDI == NULL)
-    return NS_ERROR_UNEXPECTED;
-  
-  if (::GetObjectW(hGDI, sizeof(logFont), &logFont) > 0)
-    ptrLogFont = &logFont;
-#else
-
   NONCLIENTMETRICSW ncm;
 
   BOOL status;
@@ -196,8 +187,6 @@ nsresult nsSystemFontsWin::GetSysFontInfo(HDC aHDC, nsSystemFontID anID,
       }
       break;
   } // switch 
-
-#endif // WINCE
 
   if (nsnull == ptrLogFont)
   {
