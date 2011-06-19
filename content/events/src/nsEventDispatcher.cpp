b714e8c7d070810c5a8cd1cf9625b0c638d868b9
Jan Varga <jan.varga@gmail.com>
2011-06-19 11:23:12 +0200

    Bug 617528 Part 1 - Dispatch shift right click (contextmenu event) only to chrome. r=smaug

--- content/events/src/nsEventDispatcher.cpp
+++ content/events/src/nsEventDispatcher.cpp
@@ -489,6 +489,21 @@ nsEventDispatcher::Dispatch(nsISupports* aTarget,
 
   nsCOMPtr<nsPIDOMEventTarget> target = do_QueryInterface(aTarget);
 
+  PRBool retargeted = PR_FALSE;
+
+  if (aEvent->flags & NS_EVENT_RETARGET_TO_NON_NATIVE_ANONYMOUS) {
+    nsCOMPtr<nsIContent> content = do_QueryInterface(target);
+    if (content && content->IsInNativeAnonymousSubtree()) {
+      nsCOMPtr<nsPIDOMEventTarget> newTarget =
+        do_QueryInterface(content->FindFirstNonNativeAnonymous());
+      NS_ENSURE_STATE(newTarget);
+
+      aEvent->originalTarget = target;
+      target = newTarget;
+      retargeted = PR_TRUE;
+    }
+  }
+
   if (aEvent->flags & NS_EVENT_FLAG_ONLY_CHROME_DISPATCH) {
     nsCOMPtr<nsINode> node = do_QueryInterface(aTarget);
     if (!node) {
@@ -579,7 +594,16 @@ nsEventDispatcher::Dispatch(nsISupports* aTarget,
     aEvent->target = aEvent->target->GetTargetForEventTargetChain();
     NS_ENSURE_STATE(aEvent->target);
   }
-  aEvent->originalTarget = aEvent->target;
+
+  if (retargeted) {
+    aEvent->originalTarget =
+      aEvent->originalTarget->GetTargetForEventTargetChain();
+    NS_ENSURE_STATE(aEvent->originalTarget);
+  }
+  else {
+    aEvent->originalTarget = aEvent->target;
+  }
+
   nsCOMPtr<nsIContent> content = do_QueryInterface(aEvent->originalTarget);
   PRBool isInAnon = (content && content->IsInAnonymousSubtree());
 
