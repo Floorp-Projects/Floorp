4097f2fe30fc0d08c30bec8a102ae24acbc84f44
Kyle Huey <khuey@kylehuey.com>
2011-06-20 11:18:19 -0700

    Back out Bug 441197 which was going to try ...

--- dom/plugins/ipc/PluginModuleChild.h
+++ dom/plugins/ipc/PluginModuleChild.h
@@ -156,11 +156,6 @@ protected:
     virtual bool
     AnswerNPP_GetSitesWithData(InfallibleTArray<nsCString>* aResult);
 
-    virtual bool
-    RecvSetAudioSessionData(const nsID& aId,
-                            const nsString& aDisplayName,
-                            const nsString& aIconPath);
-
     virtual void
     ActorDestroy(ActorDestroyReason why);
 
