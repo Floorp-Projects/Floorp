b785679c11bb0b27602a6c4ac66587ce37f48e13
Matheus Kerschbaum <matjk7@gmail.com>
2011-06-22 11:56:47 -0400

    Bug 662000 part 2: Remove XPC_IDISPATCH_SUPPORT from the build-system and XPConnect. r=mrbkap

--- js/src/xpconnect/src/xpcinlines.h
+++ js/src/xpconnect/src/xpcinlines.h
@@ -271,11 +271,7 @@ XPCCallContext::GetMember() const
 inline JSBool
 XPCCallContext::HasInterfaceAndMember() const
 {
-    return mState >= HAVE_NAME && mInterface && (mMember
-#ifdef XPC_IDISPATCH_SUPPORT
-        || mIDispatchMember
-#endif
-        );
+    return mState >= HAVE_NAME && mInterface && mMember;
 }
 
 inline jsid
@@ -612,65 +608,19 @@ inline void XPCNativeSet::ASSERT_NotMarked()
 inline
 JSObject* XPCWrappedNativeTearOff::GetJSObject() const
 {
-#ifdef XPC_IDISPATCH_SUPPORT
-    if(IsIDispatch())
-    {
-        XPCDispInterface * iface = GetIDispatchInfo();
-        return iface ? iface->GetJSObject() : nsnull;
-    }
-#endif
     return mJSObject;
 }
 
 inline
 void XPCWrappedNativeTearOff::SetJSObject(JSObject*  JSObj)
 {
-#ifdef XPC_IDISPATCH_SUPPORT
-    if(IsIDispatch())
-    {
-        XPCDispInterface* iface = GetIDispatchInfo();
-        if(iface)
-            iface->SetJSObject(JSObj);
-    }
-    else
-#endif
         mJSObject = JSObj;
 }
-
-#ifdef XPC_IDISPATCH_SUPPORT
-inline void
-XPCWrappedNativeTearOff::SetIDispatch(JSContext* cx)
-{
-    mJSObject = (JSObject*)(((jsword)
-        ::XPCDispInterface::NewInstance(cx,
-                                          mNative)) | 2);
-}
-
-inline XPCDispInterface* 
-XPCWrappedNativeTearOff::GetIDispatchInfo() const
-{
-    NS_ASSERTION((jsword)mJSObject & 2, "XPCWrappedNativeTearOff::GetIDispatchInfo "
-                                "called on a non IDispatch interface");
-    return reinterpret_cast<XPCDispInterface*>
-                           ((((jsword)mJSObject) & ~JSOBJECT_MASK));
-}
-
-inline JSBool
-XPCWrappedNativeTearOff::IsIDispatch() const
-{
-    return (JSBool)(((jsword)mJSObject) & IDISPATCH_BIT);
-}
-
-#endif
 
 inline
 XPCWrappedNativeTearOff::~XPCWrappedNativeTearOff()
 {
     NS_ASSERTION(!(GetInterface()||GetNative()||GetJSObject()), "tearoff not empty in dtor");
-#ifdef XPC_IDISPATCH_SUPPORT
-    if(IsIDispatch())
-        delete GetIDispatchInfo();
-#endif
 }
 
 /***************************************************************************/
