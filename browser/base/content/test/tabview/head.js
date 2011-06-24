ada14f729697fc2c0fe808fac2537c621aca8241
Marco Bonardo <mbonardo@mozilla.com>
2011-06-24 14:49:42 +0200

    Backout changesets bdd79a4d1dc9, d697b0b26227, 5abf332b2c45, 8e1bf0b98a30 because of increase in Panorama random oranges.

--- browser/base/content/test/tabview/head.js
+++ browser/base/content/test/tabview/head.js
@@ -81,15 +81,19 @@ function newWindowWithTabView(shownCallback, loadCallback, width, height) {
   let win = window.openDialog(getBrowserURL(), "_blank",
                               "chrome,all,dialog=no,height=" + winHeight +
                               ",width=" + winWidth);
-
-  whenWindowLoaded(win, function () {
-    if (loadCallback)
+  let onLoad = function() {
+    win.removeEventListener("load", onLoad, false);
+    if (typeof loadCallback == "function")
       loadCallback(win);
 
-    whenDelayedStartupFinished(win, function () {
-      showTabView(function () shownCallback(win), win);
-    });
-  });
+    let onShown = function() {
+      win.removeEventListener("tabviewshown", onShown, false);
+      shownCallback(win);
+    };
+    win.addEventListener("tabviewshown", onShown, false);
+    win.TabView.toggle();
+  }
+  win.addEventListener("load", onLoad, false);
 }
 
 // ----------
