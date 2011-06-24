2f57ac1b4408885dbe9b981249940ed828e210a5
Kai Liu <kliu@code.kliu.org>
2011-06-24 14:00:50 -0700

    Bug 659993: Part 3: Consolidate the internal padding calculations for regular menuitems into GetWidgetPadding. r=roc

--- widget/src/gtk2/nsNativeThemeGTK.cpp
+++ widget/src/gtk2/nsNativeThemeGTK.cpp
@@ -931,14 +931,23 @@ nsNativeThemeGTK::GetWidgetBorder(nsDeviceContext* aContext, nsIFrame* aFrame,
     else
         aResult->bottom = 0;
     break;
+  case NS_THEME_MENUITEM:
+  case NS_THEME_CHECKMENUITEM:
+  case NS_THEME_RADIOMENUITEM:
+    // For regular menuitems, we will be using GetWidgetPadding instead of
+    // GetWidgetBorder to pad up the widget's internals; other menuitems
+    // will need to fall through and use the default case as before.
+    if (IsRegularMenuItem(aFrame))
+      break;
   default:
     {
       GtkThemeWidgetType gtkWidgetType;
       if (GetGtkWidgetAndState(aWidgetType, aFrame, gtkWidgetType, nsnull,
-                               nsnull))
+                               nsnull)) {
         moz_gtk_get_widget_border(gtkWidgetType, &aResult->left, &aResult->top,
                                   &aResult->right, &aResult->bottom, direction,
                                   IsFrameContentNodeInNamespace(aFrame, kNameSpaceID_XHTML));
+      }
     }
   }
   return NS_OK;
@@ -976,13 +985,25 @@ nsNativeThemeGTK::GetWidgetPadding(nsDeviceContext* aContext,
         if (!IsRegularMenuItem(aFrame))
           return PR_FALSE;
 
+        aResult->SizeTo(0, 0, 0, 0);
+        GtkThemeWidgetType gtkWidgetType;
+        if (GetGtkWidgetAndState(aWidgetType, aFrame, gtkWidgetType, nsnull,
+                                 nsnull)) {
+          moz_gtk_get_widget_border(gtkWidgetType, &aResult->left, &aResult->top,
+                                    &aResult->right, &aResult->bottom, GetTextDirection(aFrame),
+                                    IsFrameContentNodeInNamespace(aFrame, kNameSpaceID_XHTML));
+        }
+
         gint horizontal_padding;
 
         if (aWidgetType == NS_THEME_MENUITEM)
           moz_gtk_menuitem_get_horizontal_padding(&horizontal_padding);
         else
           moz_gtk_checkmenuitem_get_horizontal_padding(&horizontal_padding);
-        aResult->SizeTo(horizontal_padding, 0, horizontal_padding, 0);
+
+        aResult->left += horizontal_padding;
+        aResult->right += horizontal_padding;
+
         return PR_TRUE;
       }
   }
