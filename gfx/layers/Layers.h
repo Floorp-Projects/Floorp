cf2d3c9f14856f7efa5ba9fa10e8b7e3fec9134a
Bas Schouten <bschouten@mozilla.com>
2011-06-24 19:41:18 +0200

    Bug 651858 - Part 3: Add layers API for creating an optimal DrawTarget. r=roc

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -49,6 +49,8 @@
 #include "gfxColor.h"
 #include "gfxPattern.h"
 
+#include "mozilla/gfx/2D.h"
+
 #if defined(DEBUG) || defined(PR_LOGGING)
 #  include <stdio.h>            // FILE
 #  include "prlog.h"
@@ -423,6 +425,15 @@ public:
   virtual already_AddRefed<gfxASurface>
     CreateOptimalSurface(const gfxIntSize &aSize,
                          gfxASurface::gfxImageFormat imageFormat);
+
+  /**
+   * Creates a DrawTarget which is optimized for inter-operating with this
+   * layermanager.
+   */
+  virtual TemporaryRef<mozilla::gfx::DrawTarget>
+    CreateDrawTarget(const mozilla::gfx::IntSize &aSize,
+                     mozilla::gfx::SurfaceFormat aFormat);
+
 
   /**
    * Return the name of the layer manager's backend.
