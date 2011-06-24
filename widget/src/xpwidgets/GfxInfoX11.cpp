c5df4c25da232f97d8a20d8f1fc2084f2ca177aa
Bas Schouten <bschouten@mozilla.com>
2011-06-24 19:41:18 +0200

    Bug 651858 - Part 8: Expose azure on/off state from gfxInfo. r=joedrew

--- widget/src/xpwidgets/GfxInfoX11.cpp
+++ widget/src/xpwidgets/GfxInfoX11.cpp
@@ -299,6 +299,12 @@ GfxInfo::GetDWriteEnabled(PRBool *aEnabled)
   return NS_ERROR_FAILURE;
 }
 
+NS_IMETHODIMP
+GfxInfo::GetAzureEnabled(PRBool *aEnabled)
+{
+  return NS_ERROR_FAILURE;
+}
+
 /* readonly attribute DOMString DWriteVersion; */
 NS_IMETHODIMP
 GfxInfo::GetDWriteVersion(nsAString & aDwriteVersion)
