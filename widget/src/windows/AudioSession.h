d076c0d5234462c2496583dbbe935b6fbdea99fe
Kyle Huey <khuey@kylehuey.com>
2011-06-24 07:20:03 -0700

    Bug 441197: Maintain an audio session across processes. r=jimm

--- widget/src/windows/AudioSession.h
+++ widget/src/windows/AudioSession.h
@@ -41,7 +41,21 @@
 namespace mozilla {
 namespace widget {
 
+// Start the audio session in the current process
 nsresult StartAudioSession();
+
+// Pass the information necessary to start an audio session in another process
+nsresult GetAudioSessionData(nsID& aID,
+                             nsString& aSessionName,
+                             nsString& aIconPath);
+
+// Receive the information necessary to start an audio session in a non-chrome
+// process
+nsresult RecvAudioSessionData(const nsID& aID,
+                              const nsString& aSessionName,
+                              const nsString& aIconPath);
+
+// Stop the audio session in the current process
 nsresult StopAudioSession();
 
 } // namespace widget
