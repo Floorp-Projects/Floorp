0601993702d77840e4639dd91dfd85135cd34ee2
Phil Ringnalda <philringnalda@gmail.com>
2011-06-25 11:46:05 -0700

    No bug, arguments.calle typo

--- browser/components/sessionstore/test/browser/browser_528776.js
+++ browser/components/sessionstore/test/browser/browser_528776.js
@@ -20,7 +20,7 @@ function test() {
 
   var win = openDialog(location, "", "chrome,all,dialog=no");
   win.addEventListener("load", function () {
-    win.removeEventListener("load", arguments.calle, false);
+    win.removeEventListener("load", arguments.callee, false);
     browserWindowsCount(2);
     win.close();
     browserWindowsCount(1);
