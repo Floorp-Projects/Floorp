9cb6ce116ccbe97c4beef196776146ad42cc6805
Kyle Huey <khuey@kylehuey.com>
2011-06-27 16:59:20 -0700

    Bug 515492: Android build bustage fix.

--- memory/jemalloc/jemalloc.h
+++ memory/jemalloc/jemalloc.h
@@ -60,8 +60,10 @@ void *je_memalign(size_t alignment, size_t size);
 int	je_posix_memalign(void **memptr, size_t alignment, size_t size);
 char    *je_strndup(const char *src, size_t len);
 char    *je_strdup(const char *src);
-#if defined(WIN32_NEW_STYLE_JEMALLOC)
-size_t  je_malloc_usable_size(void *ptr)
+#if defined(MOZ_MEMORY_ANDROID)
+size_t  je_malloc_usable_size(void *ptr);
+#else
+size_t	je_malloc_usable_size(const void *ptr);
 #endif
 #endif
 
