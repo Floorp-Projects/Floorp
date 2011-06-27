b8fe8dda58161fd678ca2b6e8351d3f6ba2971e8
Kyle Huey <khuey@kylehuey.com>
2011-06-27 16:11:45 -0700

    Followup fixes for Bug 515492.

--- memory/jemalloc/jemalloc.h
+++ memory/jemalloc/jemalloc.h
@@ -60,7 +60,11 @@ void *je_memalign(size_t alignment, size_t size);
 int	je_posix_memalign(void **memptr, size_t alignment, size_t size);
 char    *je_strndup(const char *src, size_t len);
 char    *je_strdup(const char *src);
+#if defined(MOZ_MEMORY_ANDROID)
+size_t  je_malloc_usable_size(void *ptr)
+#else
 size_t	je_malloc_usable_size(const void *ptr);
+#endif
 #endif
 
 /* Linux has memalign and malloc_usable_size */
