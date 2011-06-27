b613bdcfdab05457c66e87dc58cb242dc1e07f7f
Mounir Lamouri <mounir.lamouri@gmail.com>
2011-06-27 15:33:16 +0200

    Bug 665866 - Use reflect.js for input.type and button.type reflection tests. r=Ms2ger

--- content/html/content/test/reflect.js
+++ content/html/content/test/reflect.js
@@ -97,42 +97,99 @@ function reflectUnsignedInt(aElement, aAttr, aNonNull, aDefault)
 /**
  * @param aElement            Element     node to test on
  * @param aAttr               String      name of the attribute
- * @param aSupportedValues    Array       values we supported
- * @param aUnsupportedValues  Array       values we don't support
+ * @param aValidValues        Array       valid values we support
+ * @param aInvalidValues      Array       invalid values
+ * @param aDefaultValue       String      default value when no valid value is set [optional]
+ * @param aUnsupportedValues  Array       valid values we do not support [optional]
  */
-function reflectLimitedEnumerated(aElement, aAttr, aSupportedValues,
-                                  aUnsupportedValues)
+function reflectLimitedEnumerated(aElement, aAttr, aValidValues, aInvalidValues,
+                                  aDefaultValue, aUnsupportedValues)
 {
-  aSupportedValues.forEach(function (v) {
+  var defaultValue = aDefaultValue !== undefined ? aDefaultValue : "";
+  var unsupportedValues = aUnsupportedValues !== undefined ? aUnsupportedValues
+                                                           : [];
+  // Explicitly check the default value.
+  aElement.removeAttribute(aAttr);
+  is(aElement[aAttr], defaultValue,
+     "When no attribute is set, the value should be the default value.");
+
+  // Check valid values.
+  aValidValues.forEach(function (v) {
     aElement.setAttribute(aAttr, v);
-    is(aElement[aAttr], v);
-    is(aElement.getAttribute(aAttr), v);
+    is(aElement[aAttr], v,
+       v + " should be accepted as a valid value for " + aAttr);
+    is(aElement.getAttribute(aAttr), v,
+       "Content attribute should return the value it has been set to.");
     aElement.removeAttribute(aAttr);
 
     aElement.setAttribute(aAttr, v.toUpperCase());
-    is(aElement[aAttr], v);
-    is(aElement.getAttribute(aAttr), v.toUpperCase());
+    is(aElement[aAttr], v,
+       "Enumerated attributes should be case-insensitive.");
+    is(aElement.getAttribute(aAttr), v.toUpperCase(),
+       "Content attribute should be upper-cased.");
     aElement.removeAttribute(aAttr);
 
     aElement[aAttr] = v;
-    is(aElement[aAttr], v);
-    is(aElement.getAttribute(aAttr), v);
+    is(aElement[aAttr], v,
+       v + " should be accepted as a valid value for " + aAttr);
+    is(aElement.getAttribute(aAttr), v,
+       "Content attribute should return the value it has been set to.");
     aElement.removeAttribute(aAttr);
 
     aElement[aAttr] = v.toUpperCase();
-    is(aElement[aAttr], v);
-    is(aElement.getAttribute(aAttr), v.toUpperCase());
+    is(aElement[aAttr], v,
+       "Enumerated attributes should be case-insensitive.");
+    is(aElement.getAttribute(aAttr), v.toUpperCase(),
+       "Content attribute should be upper-cased.");
     aElement.removeAttribute(aAttr);
   });
-  ["cheesecake"].concat(aUnsupportedValues).forEach(function (v) {
+
+  // Check invalid values.
+  aInvalidValues.forEach(function (v) {
     aElement.setAttribute(aAttr, v);
-    is(aElement[aAttr], "");
-    is(aElement.getAttribute(aAttr), v);
+    is(aElement[aAttr], defaultValue,
+       "When the content attribute is set to an invalid value, the default value should be returned.");
+    is(aElement.getAttribute(aAttr), v,
+       "Content attribute should not have been changed.");
     aElement.removeAttribute(aAttr);
 
     aElement[aAttr] = v;
-    is(aElement[aAttr], "");
-    is(aElement.getAttribute(aAttr), v);
+    is(aElement[aAttr], defaultValue,
+       "When the value is set to an invalid value, the default value should be returned.");
+    is(aElement.getAttribute(aAttr), v,
+       "Content attribute should not have been changed.");
+    aElement.removeAttribute(aAttr);
+  });
+
+  // Check valid values we currently do not support.
+  // Basically, it's like the checks for the valid values but with some todo's.
+  unsupportedValues.forEach(function (v) {
+    aElement.setAttribute(aAttr, v);
+    todo_is(aElement[aAttr], v,
+            v + " should be accepted as a valid value for " + aAttr);
+    is(aElement.getAttribute(aAttr), v,
+       "Content attribute should return the value it has been set to.");
+    aElement.removeAttribute(aAttr);
+
+    aElement.setAttribute(aAttr, v.toUpperCase());
+    todo_is(aElement[aAttr], v,
+            "Enumerated attributes should be case-insensitive.");
+    is(aElement.getAttribute(aAttr), v.toUpperCase(),
+       "Content attribute should be upper-cased.");
+    aElement.removeAttribute(aAttr);
+
+    aElement[aAttr] = v;
+    todo_is(aElement[aAttr], v,
+            v + " should be accepted as a valid value for " + aAttr);
+    is(aElement.getAttribute(aAttr), v,
+       "Content attribute should return the value it has been set to.");
+    aElement.removeAttribute(aAttr);
+
+    aElement[aAttr] = v.toUpperCase();
+    todo_is(aElement[aAttr], v,
+            "Enumerated attributes should be case-insensitive.");
+    is(aElement.getAttribute(aAttr), v.toUpperCase(),
+       "Content attribute should be upper-cased.");
     aElement.removeAttribute(aAttr);
   });
 }
