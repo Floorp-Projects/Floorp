0e6323805517da04ccf7e5859227c64dd1e7d0a5
Jason Orendorff <jorendorff@mozilla.com>
2011-06-28 12:28:02 -0500

    Quick follow-up for thinko in bug 667925: lists can be empty. r=orange.

--- js/src/jsemit.cpp
+++ js/src/jsemit.cpp
@@ -1456,7 +1456,7 @@ EmitTraceOp(JSContext *cx, JSCodeGenerator *cg, JSParseNode *nextpn)
          * instruction. nextpn is often a block, in which case the next
          * instruction typically comes from the first statement inside.
          */
-        if (nextpn->pn_type == TOK_LC && nextpn->pn_arity == PN_LIST)
+        if (nextpn->pn_type == TOK_LC && nextpn->pn_arity == PN_LIST && nextpn->pn_head)
             nextpn = nextpn->pn_head;
         if (!UpdateLineNumberNotes(cx, cg, nextpn->pn_pos.begin.lineno))
             return -1;
