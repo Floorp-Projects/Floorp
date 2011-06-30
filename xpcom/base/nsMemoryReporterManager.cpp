2f3a8271499221e79af3ba0bea8d001765a4bfd7
Phil Ringnalda <philringnalda@gmail.com>
2011-06-29 21:48:57 -0700

    Back out da1b9a3c0a98 (bug 668137) for failing to build on OS X

--- xpcom/base/nsMemoryReporterManager.cpp
+++ xpcom/base/nsMemoryReporterManager.cpp
@@ -132,18 +132,6 @@ static PRInt64 GetResident()
 #include <windows.h>
 #include <psapi.h>
 
-static PRInt64 GetVsize()
-{
-  MEMORYSTATUSEX s;
-  s.dwLength = sizeof(s);
-
-  bool success = GlobalMemoryStatusEx(&s);
-  if (!success)
-    return -1;
-
-  return s.ullTotalPhys - s.ullAvailPhys;
-}
-
 #if MOZ_WINSDK_TARGETVER >= MOZ_NTDDI_LONGHORN
 static PRInt64 GetPrivate()
 {
@@ -187,7 +175,7 @@ static PRInt64 GetResident()
 
 #endif
 
-#if defined(XP_LINUX) || defined(XP_MACOS) || defined(XP_WIN)
+#if defined(XP_LINUX) || defined(XP_MACOSX)
 NS_MEMORY_REPORTER_IMPLEMENT(Vsize,
     "vsize",
     KIND_OTHER,
@@ -201,9 +189,7 @@ NS_MEMORY_REPORTER_IMPLEMENT(Vsize,
     "This is the vsize figure as reported by 'top' or 'ps'; on Mac the amount "
     "of memory shared with other processes is very high and so this figure is "
     "of limited use.")
-#endif
 
-#if defined(XP_LINUX) || defined(XP_MACOSX)
 NS_MEMORY_REPORTER_IMPLEMENT(SoftPageFaults,
     "soft-page-faults",
     KIND_OTHER,
@@ -414,16 +400,11 @@ nsMemoryReporterManager::Init()
     REGISTER(HeapUnused);
     REGISTER(Resident);
 
-#if defined(XP_LINUX) || defined(XP_MACOSX) || defined(XP_WIN)
-    REGISTER(Vsize);
-#endif
-
 #if defined(XP_LINUX) || defined(XP_MACOSX)
+    REGISTER(Vsize);
     REGISTER(SoftPageFaults);
     REGISTER(HardPageFaults);
-#endif
-
-#if defined(XP_WIN) && MOZ_WINSDK_TARGETVER >= MOZ_NTDDI_LONGHORN
+#elif defined(XP_WIN) && MOZ_WINSDK_TARGETVER >= MOZ_NTDDI_LONGHORN
     REGISTER(Private);
 #endif
 
