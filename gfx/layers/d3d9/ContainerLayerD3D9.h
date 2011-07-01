a564797d6e6c77f61c561a40bcae5f28e77bc361
Benoit Girard <b56girard@gmail.com>
2011-07-01 16:57:15 -0400

    Bug 648480 - Add shadow-layer support to d3d9 backend. r=cjones

--- gfx/layers/d3d9/ContainerLayerD3D9.h
+++ gfx/layers/d3d9/ContainerLayerD3D9.h
@@ -43,10 +43,24 @@
 
 namespace mozilla {
 namespace layers {
+  
+template<class Container>
+static void ContainerInsertAfter(Container* aContainer, Layer* aChild, Layer* aAfter);
+template<class Container>
+static void ContainerRemoveChild(Container* aContainer, Layer* aChild);
+template<class Container>
+static void ContainerRender(Container* aContainer, LayerManagerD3D9* aManager);
 
 class ContainerLayerD3D9 : public ContainerLayer,
                            public LayerD3D9
 {
+  template<class Container>
+  friend void ContainerInsertAfter(Container* aContainer, Layer* aChild, Layer* aAfter);
+  template<class Container>
+  friend void ContainerRemoveChild(Container* aContainer, Layer* aChild);
+  template<class Container>
+  friend void ContainerRender(Container* aContainer, LayerManagerD3D9* aManager);
+
 public:
   ContainerLayerD3D9(LayerManagerD3D9 *aManager);
   ~ContainerLayerD3D9();
@@ -68,6 +82,39 @@ public:
   void RenderLayer();
 
   virtual void LayerManagerDestroyed();
+
+  virtual void ComputeEffectiveTransforms(const gfx3DMatrix& aTransformToSurface)
+  {
+    DefaultComputeEffectiveTransforms(aTransformToSurface);
+  }
+};
+
+class ShadowContainerLayerD3D9 : public ShadowContainerLayer,
+                                public LayerD3D9
+{
+  template<class Container>
+  friend void ContainerInsertAfter(Container* aContainer, Layer* aChild, Layer* aAfter);
+  template<class Container>
+  friend void ContainerRemoveChild(Container* aContainer, Layer* aChild);
+  template<class Container>
+  friend void ContainerRender(Container* aContainer, LayerManagerD3D9* aManager);
+
+public:
+  ShadowContainerLayerD3D9(LayerManagerD3D9 *aManager);
+  ~ShadowContainerLayerD3D9();
+
+  void InsertAfter(Layer* aChild, Layer* aAfter);
+
+  void RemoveChild(Layer* aChild);
+
+  // LayerD3D9 Implementation
+  virtual Layer* GetLayer() { return this; }
+
+  virtual void Destroy();
+
+  LayerD3D9* GetFirstChildD3D9();
+
+  virtual void RenderLayer();
 
   virtual void ComputeEffectiveTransforms(const gfx3DMatrix& aTransformToSurface)
   {
