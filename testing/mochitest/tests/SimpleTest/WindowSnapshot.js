b55d19aa5d68f026caa6050c5ea4860001b2f005
Joe Drew <joe@drew.ca>
2011-07-01 13:03:43 -0400

    Bug 552605 - Write tests to make sure we reload images that are redirected with a Cache-Control: no-cache header. r=bz

--- testing/mochitest/tests/SimpleTest/WindowSnapshot.js
+++ testing/mochitest/tests/SimpleTest/WindowSnapshot.js
@@ -3,7 +3,7 @@ netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
 var gWindowUtils;
 
 try {
-  gWindowUtils = window.QueryInterface(CI.nsIInterfaceRequestor).getInterface(CI.nsIDOMWindowUtils);
+  gWindowUtils = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor).getInterface(Components.interfaces.nsIDOMWindowUtils);
   if (gWindowUtils && !gWindowUtils.compareCanvases)
     gWindowUtils = null;
 } catch (e) {
