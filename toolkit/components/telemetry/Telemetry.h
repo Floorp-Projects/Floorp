5753cc530cfdf68b799f3b0eda2ea4037af8bf5a
Taras Glek <tglek@mozilla.com>
2011-07-01 12:06:09 -0700

    Bug 668355: RAII Telemetry timer r=glandium

--- toolkit/components/telemetry/Telemetry.h
+++ toolkit/components/telemetry/Telemetry.h
@@ -39,6 +39,8 @@
 #ifndef Telemetry_h__
 #define Telemetry_h__
 
+#include "mozilla/TimeStamp.h"
+
 namespace mozilla {
 namespace Telemetry {
 
@@ -59,6 +61,21 @@ HistogramCount
  */
 void Accumulate(ID id, PRUint32 sample);
 
+template<ID id>
+class Timer {
+public:
+  Timer():
+    start(TimeStamp::Now())
+  {
+  }
+
+  ~Timer() {
+    Accumulate(id, (TimeStamp::Now() - start).ToMilliseconds());
+  }
+
+private:
+  const TimeStamp start;
+};
 } // namespace Telemetry
 } // namespace mozilla
 #endif // Telemetry_h__
