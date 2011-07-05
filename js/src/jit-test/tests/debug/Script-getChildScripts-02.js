e4996eb358a7c72d445459d4c15ab58721c53f9e
Jason Orendorff <jorendorff@mozilla.com>
2011-07-05 06:57:06 -0500

    Add Debug.Script.getChildScripts.

---
+++ js/src/jit-test/tests/debug/Script-getChildScripts-02.js
@@ -0,0 +1,22 @@
+// getChildScripts returns scripts in source order.
+
+var g = newGlobal('new-compartment');
+var dbg = new Debug(g);
+var scripts = [];
+var cs;
+dbg.hooks = {
+    debuggerHandler: function (frame) {
+	scripts.push(frame.script);
+	if (scripts.length === 1)
+	    cs = frame.script.getChildScripts();
+    }
+};
+
+g.eval("function f() { debugger; }\n" +
+       "var g = function () { debugger; }\n" +
+       "debugger; f(); g();");
+
+assertEq(scripts.length, 3);
+assertEq(cs.length, 2);
+assertEq(cs[0], scripts[1]);
+assertEq(cs[1], scripts[2]);
