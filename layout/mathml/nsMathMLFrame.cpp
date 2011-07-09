187cfbc3446a81b7f8c95b588f1df143518b308a
Jonathan Hage <hage.jonathan@gmail.com>
2011-07-09 15:52:19 +0200

    Bug 669719 - support the displaystyle attribute on the <math/> element. r=roc

--- layout/mathml/nsMathMLFrame.cpp
+++ layout/mathml/nsMathMLFrame.cpp
@@ -219,6 +219,7 @@ nsMathMLFrame::GetPresentationDataFrom(nsIFrame*           aFrame,
       if (display->mDisplay == NS_STYLE_DISPLAY_BLOCK) {
         aPresentationData.flags |= NS_MATHML_DISPLAYSTYLE;
       }
+      FindAttrDisplaystyle(content, aPresentationData);
       aPresentationData.mstyle = frame->GetFirstContinuation();
       break;
     }
