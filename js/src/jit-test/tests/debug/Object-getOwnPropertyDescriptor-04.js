f9e4eb51a822ec118c829ade0ce16e4f351c9125
Jason Orendorff <jorendorff@mozilla.com>
2011-07-11 20:22:50 -0500

    Add an initial cut at Debugger.Object.prototype.getOwnPropertyDescriptor. This implementation does not ensure that the debuggee does not run.

---
+++ js/src/jit-test/tests/debug/Object-getOwnPropertyDescriptor-04.js
@@ -0,0 +1,18 @@
+// obj.getOwnPropertyDescriptor works on accessor properties.
+
+var g = newGlobal('new-compartment');
+var dbg = new Debugger;
+var gdo = dbg.addDebuggee(g);
+
+g.called = false;
+g.eval("var a = {get x() { called = true; }};");
+
+var desc = gdo.getOwnPropertyDescriptor("a").value.getOwnPropertyDescriptor("x");
+assertEq(g.called, false);
+assertEq(desc.enumerable, true);
+assertEq(desc.configurable, true);
+assertEq("value" in desc, false);
+assertEq("writable" in desc, false);
+assertEq(desc.get instanceof Debugger.Object, true);
+assertEq(desc.get.class, "Function");
+assertEq(desc.set, undefined);
