01f326c814484d68afd667e9a32cc8606f533f37
Daniel Holbert <dholbert@cs.stanford.edu>
2011-07-11 18:24:03 -0700

    backout 36b64e9837f5 (Bug 668764) due to reftest orange on Win7

--- gfx/cairo/cairo/src/cairo-d2d-surface.cpp
+++ gfx/cairo/cairo/src/cairo-d2d-surface.cpp
@@ -1186,7 +1186,7 @@ _cairo_d2d_create_strokestyle_for_stroke_style(const cairo_stroke_style_t *style
 							       line_join, 
 							       (FLOAT)style->miter_limit,
 							       dashStyle,
-							       FLOAT(style->dash_offset) / style->line_width),
+							       (FLOAT)style->dash_offset),
 							       dashes,
 							       style->num_dashes,
 							       &strokeStyle);
