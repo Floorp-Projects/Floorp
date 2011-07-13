dae0d106ae2b996cc466bee8adce3006cb21c79b
David Mandelin <dmandelin@mozilla.com>
2011-07-13 16:47:10 -0700

    Bug 605290: check for null globalObject before innerizing, r=luke

--- js/src/jscntxtinlines.h
+++ js/src/jscntxtinlines.h
@@ -69,11 +69,8 @@ GetGlobalForScopeChain(JSContext *cx)
         return cx->fp()->scopeChain().getGlobal();
 
     JSObject *scope = cx->globalObject;
-    if (!scope) {
-        JS_ReportErrorNumber(cx, js_GetErrorMessage, NULL, JSMSG_INACTIVE);
+    if (!NULLABLE_OBJ_TO_INNER_OBJECT(cx, scope))
         return NULL;
-    }
-    OBJ_TO_INNER_OBJECT(cx, scope);
     return scope->asGlobal();
 }
 
