4ee7d6e56201dd34d39b65d101becd9c88da5139
Jason Orendorff <jorendorff@mozilla.com>
2011-07-13 06:46:23 -0500

    Add Debugger.Object.prototype.defineProperties and a few defineProperty tests.

--- js/src/jswrapper.h
+++ js/src/jswrapper.h
@@ -200,6 +200,22 @@ class AutoCompartment
     AutoCompartment & operator=(const AutoCompartment &);
 };
 
+/*
+ * Use this to change the behavior of an AutoCompartment slightly on error. If
+ * the exception happens to be an Error object, copy it to the origin compartment
+ * instead of wrapping it.
+ */
+class ErrorCopier {
+    AutoCompartment &ac;
+    JSObject *scope;
+
+  public:
+    ErrorCopier(AutoCompartment &ac, JSObject *scope) : ac(ac), scope(scope) {
+        JS_ASSERT(scope->compartment() == ac.origin);
+    }
+    ~ErrorCopier();
+};
+
 extern JSObject *
 TransparentObjectWrapper(JSContext *cx, JSObject *obj, JSObject *wrappedProto, JSObject *parent,
                          uintN flags);
