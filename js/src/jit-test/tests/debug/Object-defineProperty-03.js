80415acc632b422ea14828c81181908e2ac681f9
Jason Orendorff <jorendorff@mozilla.com>
2011-07-12 20:19:13 -0500

    Add Debugger.Object.prototype.defineProperty.

---
+++ js/src/jit-test/tests/debug/Object-defineProperty-03.js
@@ -0,0 +1,21 @@
+// defineProperty can set array elements
+
+var g = newGlobal('new-compartment');
+g.a = g.Array(0, 1, 2);
+var dbg = new Debugger;
+var gw = dbg.addDebuggee(g);
+var aw = gw.getOwnPropertyDescriptor("a").value;
+
+aw.defineProperty(0, {value: 'ok0'});  // by number
+assertEq(g.a[0], 'ok0');
+var desc = g.Object.getOwnPropertyDescriptor(g.a, "0");
+assertEq(desc.configurable, true);
+assertEq(desc.enumerable, true);
+assertEq(desc.writable, true);
+
+aw.defineProperty("1", {value: 'ok1'});  // by string
+assertEq(g.a[1], 'ok1');
+desc = g.Object.getOwnPropertyDescriptor(g.a, "1");
+assertEq(desc.configurable, true);
+assertEq(desc.enumerable, true);
+assertEq(desc.writable, true);
