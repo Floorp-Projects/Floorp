82ac1796d4b18bd073dff932c77bcf7ffbfc601f
David Mandelin <dmandelin@mozilla.com>
2011-07-13 16:47:10 -0700

    Bug 605290: Check for null cx->globalObject, r=luke

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -131,6 +131,17 @@ js_ObjectToOuterObject(JSContext *cx, JSObject *obj)
     return obj;
 }
 
+inline bool
+NULLABLE_OBJ_TO_INNER_OBJECT(JSContext *cx, JSObject *&obj)
+{
+    if (!obj) {
+        JS_ReportErrorNumber(cx, js_GetErrorMessage, NULL, JSMSG_INACTIVE);
+        return false;
+    }
+    OBJ_TO_INNER_OBJECT(cx, obj);
+    return !!obj;
+}
+
 #if JS_HAS_OBJ_PROTO_PROP
 
 static JSBool
