32c92c0d98c75533b6be9aef1109eda176ee2a8b
Nicholas Nethercote <nnethercote@mozilla.com>
2011-07-15 09:31:52 +1000

    Bug 671482 - Rename some identifiers to improve clarity.  r=anygregor.

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -666,7 +666,7 @@ JSRuntime::init(uint32 maxbytes)
         return false;
     }
 
-    atomsCompartment->systemGCChunks = true;
+    atomsCompartment->isSystemCompartment = true;
     atomsCompartment->setGCLastBytes(8192, GC_NORMAL);
 
     if (!js_InitAtomState(this))
