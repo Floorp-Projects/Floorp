3892707c1b7eac3fa9a5c6a685700221616f1e9c
Paul A. Adeyenuwo <nanodevwiz@yahoo.com>
2011-07-17 12:07:42 +0200

    Bug 660158 - Remove all references to head_ipc.js; r=jduell

--- netwerk/test/unit_ipc/head_ipc_setup.js
+++
@@ -1,7 +0,0 @@
-// If using NSPR logging, create child log as "${NSPR_LOG_FILE}.child" 
-// - TODO: remove when bug 534764 is fixed
-var env = Components.classes["@mozilla.org/process/environment;1"]
-          .getService(Components.interfaces.nsIEnvironment);
-var log = env.get("NSPR_LOG_FILE");
-if (log) 
-  env.set("NSPR_LOG_FILE", log + ".child");
