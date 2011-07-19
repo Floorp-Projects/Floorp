8c877f27a1ede5ddfd3ce7ffb4b3d8de62bf60dd
Mounir Lamouri <mounir.lamouri@gmail.com>
2011-07-19 09:42:14 -0700

    Bug 667887 - Implement nsMappedAttributes::SizeOf(). r=jst,bz

--- content/base/src/nsMappedAttributes.h
+++ content/base/src/nsMappedAttributes.h
@@ -108,6 +108,12 @@ public:
   virtual void List(FILE* out = stdout, PRInt32 aIndent = 0) const;
 #endif
 
+  PRInt64 SizeOf() const {
+    NS_ASSERTION(mAttrCount == mBufferSize,
+                 "mBufferSize and mAttrCount are expected to be the same.");
+    return sizeof(*this) - sizeof(void*) + mAttrCount * sizeof(InternalAttr);
+  }
+
 private:
   nsMappedAttributes(const nsMappedAttributes& aCopy);
   ~nsMappedAttributes();
