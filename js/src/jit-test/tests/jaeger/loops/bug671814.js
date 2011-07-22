e4be3ff42ec22c23b2248c778cb9cee896a0b04c
Brian Hackett <bhackett1024@gmail.com>
2011-07-21 20:27:34 -0700

    [INFER] Check for constant integer objects when hoisting array and property accesses, bug 671814.

---
+++ js/src/jit-test/tests/jaeger/loops/bug671814.js
@@ -0,0 +1,12 @@
+var ta = Int8Array([]);
+function Int8Array(summary) {
+    summary.length;
+}
+function test() {
+    ctors = [ Int8Array ]
+    for (var i = 0; i < 10; i++) {
+        ctor = ctors[0]
+        b = ctor(0)
+    }
+}
+test();
