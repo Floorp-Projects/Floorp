c9c57295d38078d654fdefb37d5ad40d80711363
Matt Woodrow <mwoodrow@mozilla.com>
2011-07-25 12:50:00 +1200

    Bug 670573 - Add glTex(Sub)Image2D wrappers that correctly handle stride and alignment. r=derf

--- gfx/thebes/GLContext.h
+++ gfx/thebes/GLContext.h
@@ -862,6 +862,19 @@ public:
                                              const nsIntPoint& aSrcPoint = nsIntPoint(0, 0),
                                              bool aPixelBuffer = PR_FALSE);
 
+    
+    void TexImage2D(GLenum target, GLint level, GLint internalformat, 
+                    GLsizei width, GLsizei height, GLsizei stride,
+                    GLint pixelsize, GLint border, GLenum format, 
+                    GLenum type, const GLvoid *pixels);
+
+
+    void TexSubImage2D(GLenum target, GLint level, 
+                       GLint xoffset, GLint yoffset, 
+                       GLsizei width, GLsizei height, GLsizei stride,
+                       GLint pixelsize, GLenum format, 
+                       GLenum type, const GLvoid* pixels);
+
     /** Helper for DecomposeIntoNoRepeatTriangles
      */
     struct RectTriangles {
