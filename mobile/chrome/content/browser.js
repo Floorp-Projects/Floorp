03f674a21608430f033317b34f2e9be108338a96
Matt Brubeck <mbrubeck@mozilla.com>
2011-08-01 15:40:04 -0700

    Bug 675750 - Back out 10cb6aeef385 (bug 672411) on suspicion of causing talos tp4m failures

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -2731,8 +2731,8 @@ Tab.prototype = {
 
     // Make sure the viewport height is not shorter than the window when
     // the page is zoomed out to show its full width.
-    let minScale = this.clampZoomLevel(this.getPageZoomLevel());
-    viewportH = Math.max(viewportH, screenH / minScale);
+    if (viewportH * this.clampZoomLevel(this.getPageZoomLevel()) < screenH)
+      viewportH = Math.max(viewportH, screenH * (browser.contentDocumentWidth / screenW));
 
     if (browser.contentWindowWidth != viewportW || browser.contentWindowHeight != viewportH)
       browser.setWindowSize(viewportW, viewportH);
@@ -2865,13 +2865,10 @@ Tab.prototype = {
   },
 
   clampZoomLevel: function clampZoomLevel(aScale) {
-    let md = this.metadata;
-    if (!this.allowZoom)
-      return (md && md.defaultZoom) ? md.defaultZoom : this.getPageZoomLevel();
-
     let browser = this._browser;
     let bounded = Util.clamp(aScale, ZoomManager.MIN, ZoomManager.MAX);
 
+    let md = this.metadata;
     if (md && md.minZoom)
       bounded = Math.max(bounded, md.minZoom);
     if (md && md.maxZoom)
