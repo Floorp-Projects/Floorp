cf4ab51fd0bb792631147f67d60be24bae287d8c
Lucas Rocha <lucasr@mozilla.com>
2011-08-01 11:03:00 -0400

    Bug 662520 - NewTab popup indicator 'arrow' is missing or is pointed to the right instead of left

--- mobile/chrome/content/common-ui.js
+++ mobile/chrome/content/common-ui.js
@@ -437,7 +437,17 @@ var NewTabPopup = {
     setTimeout((function() {
       let boxRect = this.box.getBoundingClientRect();
       this.box.top = tabRect.top + (tabRect.height / 2) - (boxRect.height / 2);
-      this.box.anchorTo(aTab);
+
+      let tabs = document.getElementById("tabs");
+
+      // We don't use anchorTo() here because the tab
+      // being anchored to might be overflowing the tabs
+      // scrollbox which confuses the dynamic arrow direction
+      // calculation (see bug 662520).
+      if (tabs.getBoundingClientRect().left < 0)
+        this.box.pointLeftAt(aTab);
+      else
+        this.box.pointRightAt(aTab);
     }).bind(this), 0);
 
     if (this._timeout)
