df587a00087c3e66afdc0e21d96326b63085c1d6
Marco Bonardo <mbonardo@mozilla.com>
2011-08-02 23:36:14 +0200

    Backout changesets e0aab5011b70, 0040f406ea6d (Bug 462959) due to intermittent crashes in its own test.

--- content/html/content/src/nsTimeRanges.h
+++ content/html/content/src/nsTimeRanges.h
@@ -55,9 +55,6 @@ public:
 
   void Add(double aStart, double aEnd);
 
-  // See <http://www.whatwg.org/html/#normalized-timeranges-object>.
-  void Normalize();
-
 private:
 
   struct TimeRange {
@@ -66,21 +63,6 @@ private:
         mEnd(aEnd) {}
     double mStart;
     double mEnd;
-  };
-
-  struct CompareTimeRanges
-  {
-    PRBool Equals(const TimeRange& tr1, const TimeRange& tr2) const
-    {
-      return tr1.mStart == tr2.mStart && tr1.mEnd == tr2.mEnd;
-    }
-
-    // Here, we aim at time range normalization. That why we order only by start
-    // time, since the ranges can overlap.
-    PRBool LessThan(const TimeRange& tr1, const TimeRange& tr2) const
-    {
-      return tr1.mStart < tr2.mStart;
-    }
   };
 
   nsAutoTArray<TimeRange,4> mRanges;
