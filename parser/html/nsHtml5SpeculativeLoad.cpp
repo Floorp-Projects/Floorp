15802c3801f02d37ec97a4b5a36d516f5253f364
Henri Sivonen <hsivonen@iki.fi>
2011-08-02 20:45:38 +0300

    Bug 675499 - Transfer initially sniffed document charset through the speculative load queue to avoid race conditions. r=bzbarsky.

--- parser/html/nsHtml5SpeculativeLoad.cpp
+++ parser/html/nsHtml5SpeculativeLoad.cpp
@@ -58,19 +58,29 @@ nsHtml5SpeculativeLoad::Perform(nsHtml5TreeOpExecutor* aExecutor)
 {
   switch (mOpCode) {
     case eSpeculativeLoadBase:
-        aExecutor->SetSpeculationBase(mUrl);
+      aExecutor->SetSpeculationBase(mUrl);
       break;
     case eSpeculativeLoadImage:
-        aExecutor->PreloadImage(mUrl, mCharsetOrCrossOrigin);
+      aExecutor->PreloadImage(mUrl, mCharsetOrCrossOrigin);
       break;
     case eSpeculativeLoadScript:
-        aExecutor->PreloadScript(mUrl, mCharsetOrCrossOrigin, mType);
+      aExecutor->PreloadScript(mUrl, mCharsetOrCrossOrigin, mTypeOrCharsetSource);
       break;
     case eSpeculativeLoadStyle:
-        aExecutor->PreloadStyle(mUrl, mCharsetOrCrossOrigin);
+      aExecutor->PreloadStyle(mUrl, mCharsetOrCrossOrigin);
       break;
     case eSpeculativeLoadManifest:  
-        aExecutor->ProcessOfflineManifest(mUrl);
+      aExecutor->ProcessOfflineManifest(mUrl);
+      break;
+    case eSpeculativeLoadSetDocumentCharset: {
+        nsCAutoString narrowName;
+        CopyUTF16toUTF8(mCharsetOrCrossOrigin, narrowName);
+        NS_ASSERTION(mTypeOrCharsetSource.Length() == 1,
+            "Unexpected charset source string");
+        PRInt32 intSource = (PRInt32)mTypeOrCharsetSource.First();
+        aExecutor->SetDocumentCharsetAndSource(narrowName,
+                                               intSource);
+      }
       break;
     default:
       NS_NOTREACHED("Bogus speculative load.");
