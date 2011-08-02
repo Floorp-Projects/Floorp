b8f605479f228453aa15215422771b75d35b7b40
Benoit Jacob <bjacob@mozilla.com>
2011-08-02 18:06:25 -0400

    Bug 675625 - limit GLSL identifiers to 250 characters to work around a NVIDIA bug - r=jrmuizel

--- gfx/angle/src/compiler/preprocessor/scanner.h
+++ gfx/angle/src/compiler/preprocessor/scanner.h
@@ -49,7 +49,9 @@ NVIDIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define __SCANNER_H 1
 
 // These lengths do not include the NULL terminator.
-#define MAX_SYMBOL_NAME_LEN 256
+// see bug 675625: NVIDIA driver crash with lengths >= 253
+// this is only an interim fix, the real fix is name mapping, see ANGLE bug 144 / r619
+#define MAX_SYMBOL_NAME_LEN 250
 #define MAX_STRING_LEN 511
 
 #include "compiler/preprocessor/parser.h"
