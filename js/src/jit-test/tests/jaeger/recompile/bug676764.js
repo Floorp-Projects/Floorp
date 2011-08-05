b55e8813e02308743d32aae021ea25e4becea98f
Brian Hackett <bhackett1024@gmail.com>
2011-08-05 07:41:11 -0700

    [INFER] Handle recompilation hazard in NAME ICs, recompilation triggered by BINDNAME, bug 676764.

---
+++ js/src/jit-test/tests/jaeger/recompile/bug676764.js
@@ -0,0 +1,14 @@
+
+try { with( <x/> ) {
+    (function () {
+        for (;;) {
+            t
+        }
+    })()
+} } catch (e) {}
+
+with( <x/> ) {
+  (function () {
+    for (b = 0; b < 18; ++b) {}
+  })();
+}
