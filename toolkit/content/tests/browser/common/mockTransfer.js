7fc52fd3c94d699c2568a47c6c38588a34186a4b
Geoff Lankow <geoff@darktrojan.net>
2011-08-05 23:55:49 +1200

    Bug 668710 - Move mockObjects.js in with the rest of SimpleTest; r=dao
    --HG--
    rename : toolkit/content/tests/browser/common/mockObjects.js => testing/mochitest/tests/SimpleTest/MockObjects.js

--- toolkit/content/tests/browser/common/mockTransfer.js
+++ toolkit/content/tests/browser/common/mockTransfer.js
@@ -36,8 +36,7 @@
 
 Cc["@mozilla.org/moz/jssubscript-loader;1"]
   .getService(Ci.mozIJSSubScriptLoader)
-  .loadSubScript("chrome://mochitests/content/browser/toolkit/content/tests/browser/common/mockObjects.js",
-                 this);
+  .loadSubScript("chrome://mochikit/content/tests/SimpleTest/MockObjects.js", this);
 
 var mockTransferCallback;
 
