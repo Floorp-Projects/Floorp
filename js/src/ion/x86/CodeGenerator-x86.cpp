a5c805c537c247ba446c9f93d1d39cadb4e57048
David Anderson <danderson@mozilla.com>
2011-08-05 22:54:37 -0700

    Make frame size classes optional (bug 670827 part 2, r=adrake).

--- js/src/ion/x86/CodeGenerator-x86.cpp
+++ js/src/ion/x86/CodeGenerator-x86.cpp
@@ -52,6 +52,37 @@ CodeGeneratorX86::CodeGeneratorX86(MIRGenerator *gen, LIRGraph &graph)
 {
 }
 
+// The first two size classes are 128 and 256 bytes respectively. After that we
+// increment by 512.
+static const uint32 LAST_FRAME_SIZE = 512;
+static const uint32 LAST_FRAME_INCREMENT = 512;
+static const uint32 FrameSizes[] = { 128, 256, LAST_FRAME_SIZE };
+
+FrameSizeClass
+FrameSizeClass::FromDepth(uint32 frameDepth)
+{
+    for (uint32 i = 0; i < JS_ARRAY_LENGTH(FrameSizes); i++) {
+        if (frameDepth < FrameSizes[i])
+            return FrameSizeClass(i);
+    }
+
+    uint32 newFrameSize = frameDepth - LAST_FRAME_SIZE;
+    uint32 sizeClass = (newFrameSize / LAST_FRAME_INCREMENT) + 1;
+
+    return FrameSizeClass(JS_ARRAY_LENGTH(FrameSizes) + sizeClass);
+}
+uint32
+FrameSizeClass::frameSize() const
+{
+    JS_ASSERT(class_ != NO_FRAME_SIZE_CLASS_ID);
+
+    if (class_ < JS_ARRAY_LENGTH(FrameSizes))
+        return FrameSizes[class_];
+
+    uint32 step = class_ - JS_ARRAY_LENGTH(FrameSizes);
+    return LAST_FRAME_SIZE + step * LAST_FRAME_INCREMENT;
+}
+
 bool
 CodeGeneratorX86::visitValue(LValue *value)
 {
