a1491b2f0f02e33f3f84b5e07d318d7a05aec5f2
Karl Tomlinson <karlt+@karlt.net>
2011-08-08 17:28:06 +1200

    b=672103 revert removal of early return in bug 624329, to avoid unwanted rollup on spurious ConfigureNotify events from Cygwin/X r=roc
    --HG--
    extra : transplant_source : Vp%114%06%B5%12%16%06ZkP%27%11%40%17%5D%F0%E7%1C

--- widget/src/gtk2/nsWindow.cpp
+++ widget/src/gtk2/nsWindow.cpp
@@ -2379,6 +2379,15 @@ nsWindow::OnConfigureEvent(GtkWidget *aWidget, GdkEventConfigure *aEvent)
     LOG(("configure event [%p] %d %d %d %d\n", (void *)this,
          aEvent->x, aEvent->y, aEvent->width, aEvent->height));
 
+    // mBounds.x/y are set to the window manager frame top-left when Move() or
+    // Resize()d from within Gecko, so comparing with the client window
+    // top-left is weird.  However, mBounds.x/y are set to client window
+    // position below, so this check avoids unwanted rollup on spurious
+    // configure events from Cygwin/X (bug 672103).
+    if (mBounds.x == aEvent->x &&
+        mBounds.y == aEvent->y)
+        return FALSE;
+
     if (mWindowType == eWindowType_toplevel || mWindowType == eWindowType_dialog) {
         check_for_rollup(aEvent->window, 0, 0, PR_FALSE, PR_TRUE);
     }
