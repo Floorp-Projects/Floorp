4372e795bbba8347304245abaa574bc4de1bb1c0
Brian Hackett <bhackett1024@gmail.com>
2011-08-10 09:18:05 -0700

    [INFER] Maybe fix ARM build.

--- js/src/assembler/assembler/ARMAssembler.h
+++ js/src/assembler/assembler/ARMAssembler.h
@@ -1221,7 +1221,6 @@ namespace JSC {
             return AL | B | (offset & BRANCH_MASK);
         }
 
-    private:
         // pretty-printing functions
         static char const * nameGpReg(int reg)
         {
