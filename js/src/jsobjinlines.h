2b5c7a7b2e81e8eae2fa0fed4415712cd08ba056
Jeff Walden <jwalden@mit.edu>
2011-08-10 14:54:52 -0700

    Bug 686582 - Begin to specialize ObjectOps::setElementAttributes to not just delegate to ObjectOps::setAttributes.  r=dvander

--- js/src/jsobjinlines.h
+++ js/src/jsobjinlines.h
@@ -1138,15 +1138,6 @@ JSObject::setElement(JSContext *cx, uint32 index, js::Value *vp, JSBool strict)
 }
 
 inline JSBool
-JSObject::setElementAttributes(JSContext *cx, uint32 index, uintN *attrsp)
-{
-    jsid id;
-    if (!js::IndexToId(cx, index, &id))
-        return false;
-    return setAttributes(cx, id, attrsp);
-}
-
-inline JSBool
 JSObject::deleteElement(JSContext *cx, uint32 index, js::Value *rval, JSBool strict)
 {
     jsid id;
