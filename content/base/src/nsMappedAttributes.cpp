40a245a800a7c923932ee68849128b9749864bea
Mounir Lamouri <mounir.lamouri@gmail.com>
2011-08-11 00:54:19 +0200

    Bug 669904 - Add nsAttrValue size to the DOM Memory Reporter. f=bz r=jst

--- content/base/src/nsMappedAttributes.cpp
+++ content/base/src/nsMappedAttributes.cpp
@@ -277,3 +277,19 @@ nsMappedAttributes::IndexOfAttr(nsIAtom* aLocalName, PRInt32 aNamespaceID) const
 
   return -1;
 }
+
+PRInt64
+nsMappedAttributes::SizeOf() const
+{
+  NS_ASSERTION(mAttrCount == mBufferSize,
+               "mBufferSize and mAttrCount are expected to be the same.");
+
+  PRInt64 size = sizeof(*this) - sizeof(void*) + mAttrCount * sizeof(InternalAttr);
+
+  for (PRUint16 i = 0; i < mAttrCount; ++i) {
+    size += Attrs()[i].mValue.SizeOf() - sizeof(Attrs()[i].mValue);
+  }
+
+  return size;
+}
+
