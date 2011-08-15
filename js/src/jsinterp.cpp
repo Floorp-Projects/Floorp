1b9bc451f4b02701b4f73a371a19b41835d282c9
Jeff Walden <jwalden@mit.edu>
2011-08-15 00:12:48 -0400

    Bug 678998 - Rename IsFunctionQName to GetLocalNameFromFunctionQName and make it infallible, now that QName's localName slot stores an atom and conversion to jsid can be infallible.  r=luke

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -538,9 +538,7 @@ js_OnUnknownMethod(JSContext *cx, Value *vp)
         /* Extract the function name from function::name qname. */
         if (vp[0].isObject()) {
             obj = &vp[0].toObject();
-            if (!js_IsFunctionQName(cx, obj, &id))
-                return false;
-            if (!JSID_IS_VOID(id))
+            if (js_GetLocalNameFromFunctionQName(obj, &id, cx))
                 vp[0] = IdToValue(id);
         }
 #endif
