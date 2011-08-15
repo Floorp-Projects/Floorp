5c4a98a4dce9cbe5c4f4de30f7601590680ca9b2
Sean Stangl <sstangl@mozilla.com>
2011-08-15 14:15:27 -0700

    Implement FixedList. (Bug 670484, r=dvander)

--- js/src/ion/FixedArityList.h
+++ js/src/ion/FixedArityList.h
@@ -77,6 +77,45 @@ class FixedArityList<T, 0>
     }
 };
 
+// List of a fixed length, but the length is unknown until runtime.
+template <typename T>
+class FixedList
+{
+    size_t length_;
+    T *list_;
+
+  private:
+    FixedList(const FixedList&); // no copy definition.
+    void operator= (const FixedList*); // no assignment definition.
+
+  public:
+    FixedList()
+      : length_(0)
+    { }
+
+    // Dynamic memory allocation requires the ability to report failure.
+    bool init(size_t length) {
+        length_ = length;
+        if (length == 0)
+            return true;
+
+        list_ = (T *)GetIonContext()->temp->allocate(length * sizeof(T *));
+        return list_ != NULL;
+    }
+
+    size_t length() const {
+        return length_;
+    }
+
+    T &operator[](size_t index) {
+        JS_ASSERT(index < length_);
+        return list_[index];
+    }
+    const T &operator [](size_t index) const {
+        JS_ASSERT(index < length_);
+        return list_[index];
+    };
+};
 
 } // namespace ion
 } // namespace js
