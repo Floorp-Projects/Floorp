34ad032a6344ae2b8fea02cc6ff9b84213893070
Paul O’Shannessy <paul@oshannessy.com>
2011-08-15 18:20:15 -0700

    Review comments for bug 645428

--- browser/components/sessionstore/test/browser/browser_645428.js
+++ browser/components/sessionstore/test/browser/browser_645428.js
@@ -1,30 +1,21 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-let ss = Cc["@mozilla.org/browser/sessionstore;1"]
-         .getService(Ci.nsISessionStore);
-
-let obs = Cc["@mozilla.org/observer-service;1"]
-          .getService(Ci.nsIObserverService);
-
 const NOTIFICATION = "sessionstore-browser-state-restored";
 
 function test() {
   waitForExplicitFinish();
 
-  let observer = {
-    observe: function (subject, topic, data) {
-      if (NOTIFICATION == topic)
-        finish();
-    },
+  function observe(subject, topic, data) {
+    if (NOTIFICATION == topic) {
+      finish();
+      ok(true, "TOPIC received");
+    }
+  }
 
-    QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver,
-                                           Ci.nsISupportsWeakReference]),
-  };
-
-  obs.addObserver(observer, NOTIFICATION, true);
+  Services.obs.addObserver(observe, NOTIFICATION, false);
   registerCleanupFunction(function () {
-    obs.removeObserver(observer, NOTIFICATION, true);
+    Services.obs.removeObserver(observe, NOTIFICATION, false);
   });
 
   ss.setBrowserState(JSON.stringify({ windows: [] }));
