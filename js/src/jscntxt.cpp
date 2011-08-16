1b6b1a79deb707ac13d62ca1b0c05d6e88b3ecaf
Luke Wagner <luke@mozilla.com>
2011-08-15 17:21:23 -0700

    Backed out changeset 0cf822d12c64 (and dependent be91fb29d950 and 53dd7a930bae) due to intermittent crashes

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -316,8 +316,6 @@ static const size_t TEMP_POOL_CHUNK_SIZE = 4096 - ARENA_HEADER_SIZE_HACK;
 JSContext *
 js_NewContext(JSRuntime *rt, size_t stackChunkSize)
 {
-    JS_AbortIfWrongThread(rt);
-
     JSContext *cx;
     JSBool first;
     JSContextCallback cxCallback;
@@ -429,14 +427,13 @@ js_NewContext(JSRuntime *rt, size_t stackChunkSize)
 void
 js_DestroyContext(JSContext *cx, JSDestroyContextMode mode)
 {
-    JSRuntime *rt = cx->runtime;
-    JS_AbortIfWrongThread(rt);
-
+    JSRuntime *rt;
     JSContextCallback cxCallback;
     JSBool last;
 
     JS_ASSERT(!cx->enumerators);
 
+    rt = cx->runtime;
 #ifdef JS_THREADSAFE
     /*
      * For API compatibility we allow to destroy contexts without a thread in
