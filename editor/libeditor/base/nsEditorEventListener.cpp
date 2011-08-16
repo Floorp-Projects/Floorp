60e74032e8517c1d579107df5816cc2a5849d1be
Ehsan Akhgari <ehsan@mozilla.com>
2011-08-15 22:30:23 -0400

    Backout changeset 83b6f03bb285 (bug 674770) because mochitest-3 orange on Windows

--- editor/libeditor/base/nsEditorEventListener.cpp
+++ editor/libeditor/base/nsEditorEventListener.cpp
@@ -389,16 +389,6 @@ nsEditorEventListener::MouseClick(nsIDOMEvent* aMouseEvent)
     return NS_OK;
   }
 
-  nsCOMPtr<nsIDOMEventTarget> target;
-  aMouseEvent->GetTarget(getter_AddRefs(target));
-  nsCOMPtr<nsIDOMNode> node = do_QueryInterface(target);
-  NS_ENSURE_TRUE(node, NS_ERROR_FAILURE);
-
-  if (!mEditor->IsModifiableNode(node)) {
-    // We shouldn't handle the event for non-editable content.
-    return NS_OK;
-  }
-
   nsresult rv;
   nsCOMPtr<nsIDOMNSUIEvent> nsuiEvent = do_QueryInterface(aMouseEvent);
   NS_ENSURE_TRUE(nsuiEvent, NS_ERROR_NULL_POINTER);
