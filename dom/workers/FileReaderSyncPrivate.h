9698231df57b38a4b954ba143b8b2022cb91e6c2
William Chen <wchen@mozilla.com>
2011-08-15 20:40:38 -0700

    Bug 664783 - Implement FileReaderSync for Workers. r=bent+sicking.

--- dom/workers/FileReaderSyncPrivate.h
+++ dom/workers/FileReaderSyncPrivate.h
@@ -1,4 +1,4 @@
-/* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set ts=2 et sw=2 tw=80: */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
@@ -13,15 +13,15 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is Web Workers.
+ * The Original Code is mozilla.org code.
  *
  * The Initial Developer of the Original Code is
- *   The Mozilla Foundation.
+ * the Mozilla Foundation.
  * Portions created by the Initial Developer are Copyright (C) 2011
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *   Ben Turner <bent.mozilla@gmail.com> (Original Author)
+ *   William Chen <wchen@mozilla.com> (Original Author)
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -37,62 +37,44 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#ifndef mozilla_dom_workers_exceptions_h__
-#define mozilla_dom_workers_exceptions_h__
+#ifndef nsDOMFileReaderSyncPrivate_h
+#define nsDOMFileReaderSyncPrivate_h
 
 #include "Workers.h"
 
-#include "jspubtd.h"
+#include "nsICharsetDetectionObserver.h"
+#include "nsStringGlue.h"
 
-// DOMException Codes.
-#define INDEX_SIZE_ERR 1
-#define DOMSTRING_SIZE_ERR 2
-#define HIERARCHY_REQUEST_ERR 3
-#define WRONG_DOCUMENT_ERR 4
-#define INVALID_CHARACTER_ERR 5
-#define NO_DATA_ALLOWED_ERR 6
-#define NO_MODIFICATION_ALLOWED_ERR 7
-#define NOT_FOUND_ERR 8
-#define NOT_SUPPORTED_ERR 9
-#define INUSE_ATTRIBUTE_ERR 10
-#define INVALID_STATE_ERR 11
-#define SYNTAX_ERR 12
-#define INVALID_MODIFICATION_ERR 13
-#define NAMESPACE_ERR 14
-#define INVALID_ACCESS_ERR 15
-#define VALIDATION_ERR 16
-#define TYPE_MISMATCH_ERR 17
-#define SECURITY_ERR 18
-#define NETWORK_ERR 19
-#define ABORT_ERR 20
-#define URL_MISMATCH_ERR 21
-#define QUOTA_EXCEEDED_ERR 22
-#define TIMEOUT_ERR 23
-#define INVALID_NODE_TYPE_ERR 24
-#define DATA_CLONE_ERR 25
-
-// FileException Codes
-#define FILE_NOT_FOUND_ERR 1
-#define FILE_SECURITY_ERR 2
-#define FILE_ABORT_ERR 3
-#define FILE_NOT_READABLE_ERR 4
-#define FILE_ENCODING_ERR 5
+class nsIInputStream;
+class nsIDOMBlob;
 
 BEGIN_WORKERS_NAMESPACE
 
-namespace exceptions {
+class FileReaderSyncPrivate : public PrivatizableBase,
+                              public nsICharsetDetectionObserver
+{
+  nsCString mCharset;
+  nsresult ConvertStream(nsIInputStream *aStream, const char *aCharset,
+                         nsAString &aResult);
+  nsresult GuessCharset(nsIInputStream *aStream, nsACString &aCharset);
 
-bool
-InitClasses(JSContext* aCx, JSObject* aGlobal);
+public:
+  NS_DECL_ISUPPORTS
 
-void
-ThrowDOMExceptionForCode(JSContext* aCx, intN aCode);
+  FileReaderSyncPrivate();
+  ~FileReaderSyncPrivate();
 
-void
-ThrowFileExceptionForCode(JSContext* aCx, intN aCode);
+  nsresult ReadAsArrayBuffer(nsIDOMBlob* aBlob, PRUint32 aLength,
+                             uint8* aBuffer);
+  nsresult ReadAsBinaryString(nsIDOMBlob* aBlob, nsAString& aResult);
+  nsresult ReadAsText(nsIDOMBlob* aBlob, const nsAString& aEncoding,
+                      nsAString& aResult);
+  nsresult ReadAsDataURL(nsIDOMBlob* aBlob, nsAString& aResult);
 
-} // namespace exceptions
+  // From nsICharsetDetectionObserver
+  NS_IMETHOD Notify(const char *aCharset, nsDetectionConfident aConf);
+};
 
 END_WORKERS_NAMESPACE
 
-#endif // mozilla_dom_workers_exceptions_h__
+#endif
