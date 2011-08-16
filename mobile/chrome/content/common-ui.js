5edc5ea331c5ab760742a663b8433367475ac43a
Daniel Holbert <dholbert@cs.stanford.edu>
2011-08-15 20:59:13 -0700

    Backing out f085bbca2ee9 (bug 609043) since it triggered perma-orange in Android b-c tests

--- mobile/chrome/content/common-ui.js
+++ mobile/chrome/content/common-ui.js
@@ -1677,102 +1677,13 @@ var CharsetMenu = {
 
 var WebappsUI = {
   _dialog: null,
+  _manifest: null,
   _perms: [],
-  _openwebapps: null,
-  _application: null,
-
-  init: function() {
-    Cu.import("resource:///modules/openWebapps.jsm");
-    messageManager.addMessageListener("OpenWebapps:Install", this);
-    messageManager.addMessageListener("OpenWebapps:GetInstalledBy", this);
-    messageManager.addMessageListener("OpenWebapps:AmInstalled", this);
-    messageManager.addMessageListener("OpenWebapps:MgmtLaunch", this);
-    messageManager.addMessageListener("OpenWebapps:MgmtList", this);
-    messageManager.addMessageListener("OpenWebapps:MgmtUninstall", this);
-  },
-
-  // converts a manifest to an application as expected by openwebapps.install()
-  convertManifest: function(aData) {
-    let app = {
-      manifest : JSON.parse(aData.manifest),
-      installData : aData.installData,
-      storeURI : aData.storeURI,
-      manifestURI : aData.manifestURIs,
-      capabilities : [],
-      callbackID : aData.callbackID
-    }
-
-    let chrome = Cc["@mozilla.org/chrome/chrome-registry;1"].getService(Ci.nsIXULChromeRegistry).QueryInterface(Ci.nsIToolkitChromeRegistry);
-    let locale = chrome.getSelectedLocale("browser");
-
-    let localeRoot;
-    if (app.manifest.locales)
-      localeRoot = app.manifest.locales[locale];
-
-    if (!localeRoot)
-      localeRoot = app.manifest;
-
-    let baseURI = Services.io.newURI(aData.manifestURI, null, null);
-    app.title = localeRoot.name || app.manifest.name;
-
-    // choose the larger icon
-    let max = 0;
-    let icon;
-    for (let size in app.manifest.icons) {
-      let iSize = parseInt(size);
-      if (iSize > max) {
-        icon = baseURI.resolve(app.manifest.icons[size]);
-        max = iSize;
-      }
-    }
-    if (icon)
-      app.iconURI = icon;
-
-    let root = baseURI.resolve("/").toString();
-    app.appURI = app.manifest.launch_path ? baseURI.resolve(app.manifest.launch_path) : root.substring(0, root.length - 1);
-
-    return app;
-  },
-
-  receiveMessage: function(aMessage) {
-    let browser = aMessage.target;
-    switch(aMessage.name) {
-      case "OpenWebapps:Install":
-        WebappsUI._openwebapps = browser;
-        WebappsUI.show(WebappsUI.convertManifest(aMessage.json));
-        break;
-      case "OpenWebapps:GetInstalledBy":
-        let apps = OpenWebapps.getInstalledBy(aMessage.json.storeURI);
-        browser.messageManager.sendAsyncMessage("OpenWebapps:GetInstalledBy:Return",
-            { apps: apps, callbackID: aMessage.json.callbackID });
-        break;
-      case "OpenWebapps:AmInstalled":
-        let app = OpenWebapps.amInstalled(aMessage.json.appURI);
-        browser.messageManager.sendAsyncMessage("OpenWebapps:AmInstalled:Return",
-            { installed: app != null, app: app, callbackID: aMessage.json.callbackID });
-        break;
-      case "OpenWebapps:MgmtList":
-        let list = OpenWebapps.mgmtList();
-        browser.messageManager.sendAsyncMessage("OpenWebapps:MgmtList:Return",
-            { ok: true, apps: list, callbackID: aMessage.json.callbackID });
-        break;
-      case "OpenWebapps:MgmtLaunch":
-        let res = OpenWebapps.mgmtLaunch(aMessage.json.origin);
-        browser.messageManager.sendAsyncMessage("OpenWebapps:MgmtLaunch:Return",
-            { ok: res, callbackID: aMessage.json.callbackID });
-        break;
-      case "OpenWebapps:MgmtUninstall":
-        let uninstalled = OpenWebapps.mgmtUninstall(aMessage.json.origin);
-        browser.messageManager.sendAsyncMessage("OpenWebapps:MgmtUninstall:Return",
-            { ok: uninstalled, callbackID: aMessage.json.callbackID });
-        break;
-    }
-  },
-
+  
   checkBox: function(aEvent) {
     let elem = aEvent.originalTarget;
     let perm = elem.getAttribute("perm");
-    if (this._application.capabilities && this._application.capabilities.indexOf(perm) != -1) {
+    if (this._manifest.capabilities && this._manifest.capabilities.indexOf(perm) != -1) {
       if (elem.checked) {
         elem.classList.remove("webapps-noperm");
         elem.classList.add("webapps-perm");
@@ -1783,10 +1694,8 @@ var WebappsUI = {
     }
   },
 
-  show: function show(aApplication) {
-    if (!aApplication) {
-      this._openwebapps = null;
-
+  show: function show(aManifest) {
+    if (!aManifest) {
       // Try every way to get an icon
       let browser = Browser.selectedBrowser;
       let icon = browser.appIcon.href;
@@ -1795,32 +1704,24 @@ var WebappsUI = {
       if (!icon) 
         icon = gFaviconService.getFaviconImageForPage(browser.currentURI).spec;
 
-      // Create a simple application object
-      aApplication = {
-        appURI: browser.currentURI.spec,
-        storeURI: browser.currentURI.spec,
-        title: browser.contentTitle,
-        iconURI: icon,
+      // Create a simple manifest
+      aManifest = {
+        uri: browser.currentURI.spec,
+        name: browser.contentTitle,
+        icon: icon,
         capabilities: [],
-        manifest: {
-          name: browser.contentTitle,
-          launch_path: browser.currentURI.spec,
-          icons: {
-            "64": icon
-          }
-        }
-      }
+      };
     }
 
-    this._application = aApplication;
+    this._manifest = aManifest;
     this._dialog = importDialog(window, "chrome://browser/content/webapps.xul", null);
 
-    if (aApplication.title)
-      document.getElementById("webapps-title").value = aApplication.title;
-    if (aApplication.iconURI)
-      document.getElementById("webapps-icon").src = aApplication.iconURI;  
+    if (aManifest.name)
+      document.getElementById("webapps-title").value = aManifest.name;
+    if (aManifest.icon)
+      document.getElementById("webapps-icon").src = aManifest.icon;  
 
-    let uri = Services.io.newURI(aApplication.appURI, null, null);
+    let uri = Services.io.newURI(aManifest.uri, null, null);
 
     let perms = [["offline", "offline-app"], ["geoloc", "geo"], ["notifications", "desktop-notification"]];
     let self = this;
@@ -1828,7 +1729,7 @@ var WebappsUI = {
       let elem = document.getElementById("webapps-" + tuple[0] + "-checkbox");
       let currentPerm = Services.perms.testExactPermission(uri, tuple[1]);
       self._perms[tuple[1]] = (currentPerm == Ci.nsIPermissionManager.ALLOW_ACTION);
-      if ((aApplication.capabilities && (aApplication.capabilities.indexOf(tuple[1]) != -1)) || (currentPerm == Ci.nsIPermissionManager.ALLOW_ACTION))
+      if ((aManifest.capabilities && (aManifest.capabilities.indexOf(tuple[1]) != -1)) || (currentPerm == Ci.nsIPermissionManager.ALLOW_ACTION))
         elem.checked = true;
       else
         elem.checked = (currentPerm == Ci.nsIPermissionManager.ALLOW_ACTION);
@@ -1846,18 +1747,11 @@ var WebappsUI = {
     this._dialog.close();
     this._dialog = null;
     BrowserUI.popPopup(this);
-
-    if (this._openwebapps) {
-      let browser = this._openwebapps;
-      browser.messageManager.sendAsyncMessage("OpenWebapps:InstallAborted", { callbackID: this._application.callbackID });
-    }
-
-    this._openwebapps = null;
   },
 
   _updatePermission: function updatePermission(aId, aPerm) {
     try {
-      let uri = Services.io.newURI(this._application.appURI, null, null);
+      let uri = Services.io.newURI(this._manifest.uri, null, null);
       let currentState = document.getElementById(aId).checked;
       if (currentState != this._perms[aPerm])
         Services.perms.add(uri, aPerm, currentState ? Ci.nsIPermissionManager.ALLOW_ACTION : Ci.nsIPermissionManager.DENY_ACTION);
@@ -1871,19 +1765,12 @@ var WebappsUI = {
     if (!title)
       return;
 
-    this._application.title = title;
-
     this._updatePermission("webapps-offline-checkbox", "offline-app");
     this._updatePermission("webapps-geoloc-checkbox", "geo");
     this._updatePermission("webapps-notifications-checkbox", "desktop-notification");
 
-    let browser = this._openwebapps;
-    this._openwebapps = null;
     this.hide();
-    this.install();
-
-    if (browser != null)
-      browser.messageManager.sendAsyncMessage("OpenWebapps:InstallDone", { callbackID: this._application.callbackID });
+    this.install(this._manifest.uri, title, this._manifest.icon);
   },
   
   updateWebappsInstall: function updateWebappsInstall(aNode) {
@@ -1892,10 +1779,11 @@ var WebappsUI = {
 
     let browser = Browser.selectedBrowser;
 
-    return !(OpenWebapps.amInstalled(browser.currentURI.spec));
+    let webapp = Cc["@mozilla.org/webapps/support;1"].getService(Ci.nsIWebappsSupport);
+    return !(webapp && webapp.isApplicationInstalled(browser.currentURI.spec));
   },
   
-  install: function(aIconURI) {
+  install: function(aURI, aTitle, aIcon) {
     const kIconSize = 64;
     
     let canvas = document.createElementNS("http://www.w3.org/1999/xhtml", "canvas");
@@ -1907,20 +1795,19 @@ var WebappsUI = {
       canvas.width = canvas.height = kIconSize; // clears the canvas
       let ctx = canvas.getContext("2d");
       ctx.drawImage(image, 0, 0, kIconSize, kIconSize);
-      self._application.iconData = canvas.toDataURL("image/png", "");
+      let data = canvas.toDataURL("image/png", "");
       canvas = null;
       try {
-        OpenWebapps.install(self._application);
+        let webapp = Cc["@mozilla.org/webapps/support;1"].getService(Ci.nsIWebappsSupport);
+        webapp.installApplication(aTitle, aURI, aIcon, data);
       } catch(e) {
         Cu.reportError(e);
       }
     }
-
     image.onerror = function() {
       // can't load the icon (bad URI) : fallback to the default one from chrome
-      self.install("chrome://browser/skin/images/favicon-default-30.png");
+      self.install(aURI, aTitle, "chrome://browser/skin/images/favicon-default-30.png");
     }
-
-    image.src = aIconURI || this._application.iconURI;
+    image.src = aIcon;
   }
 };
