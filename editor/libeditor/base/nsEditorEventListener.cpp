284368edffe71c57757577294b425f4a29bbc9d7
Ehsan Akhgari <ehsan@mozilla.com>
2011-08-18 16:25:11 -0400

    Backout changeset 46dc0fb0b1cb (bug 674770) because of yet another mochitest-3 Windows failure

--- editor/libeditor/base/nsEditorEventListener.cpp
+++ editor/libeditor/base/nsEditorEventListener.cpp
@@ -389,16 +389,6 @@ nsEditorEventListener::MouseClick(nsIDOMEvent* aMouseEvent)
   // IME to commit before we change the cursor position
   mEditor->ForceCompositionEnd();
 
-  nsCOMPtr<nsIDOMEventTarget> target;
-  aMouseEvent->GetTarget(getter_AddRefs(target));
-  nsCOMPtr<nsIDOMNode> node = do_QueryInterface(target);
-  NS_ENSURE_TRUE(node, NS_ERROR_FAILURE);
-
-  if (!mEditor->IsModifiableNode(node)) {
-    // We shouldn't handle the event for non-editable content.
-    return NS_OK;
-  }
-
   PRUint16 button = (PRUint16)-1;
   mouseEvent->GetButton(&button);
   // middle-mouse click (paste);
