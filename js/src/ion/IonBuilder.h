db8f63ebe57ab29d6172f9574455e4b372f059e7
Hannes Verschore <hverschore@mozilla.com>
2011-08-19 12:39:23 -0700

    Remove unused argument from jsop_bitnot, no bug. r=dvander

--- js/src/ion/IonBuilder.h
+++ js/src/ion/IonBuilder.h
@@ -230,7 +230,7 @@ class IonBuilder : public MIRGenerator
     bool snapshotAfter(MInstruction *ins);
 
     bool pushConstant(const Value &v);
-    bool jsop_bitnot(JSOp op);
+    bool jsop_bitnot();
     bool jsop_bitop(JSOp op);
     bool jsop_binary(JSOp op);
     bool jsop_neg();
