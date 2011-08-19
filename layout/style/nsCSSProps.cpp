ae0c15b0c7fe0034539dcfc01ba4b8055b6cbf94
Matt Woodrow <mwoodrow@mozilla.com>
2011-08-19 12:43:12 +1200

    Bug 677173 - Remove 3D transform IDL properties until it is enabled by default. r=dbaron

--- layout/style/nsCSSProps.cpp
+++ layout/style/nsCSSProps.cpp
@@ -351,6 +351,11 @@ nsCSSProps::LookupProperty(const nsACString& aProperty)
       }
     }
   }
+  
+  if (res == eCSSProperty_perspective || res == eCSSProperty_perspective_origin || res == eCSSProperty_backface_visibility) {
+    return eCSSProperty_UNKNOWN;
+  }
+
   return res;
 }
 
@@ -372,6 +377,11 @@ nsCSSProps::LookupProperty(const nsAString& aProperty)
       }
     }
   }
+  
+  if (res == eCSSProperty_perspective || res == eCSSProperty_perspective_origin || res == eCSSProperty_backface_visibility) {
+    return eCSSProperty_UNKNOWN;
+  }
+
   return res;
 }
 
--- layout/style/nsDOMCSSDeclaration.h
+++ layout/style/nsDOMCSSDeclaration.h
@@ -67,6 +67,13 @@ public:
 
   NS_DECL_NSICSSDECLARATION
 
+  NS_IMETHOD GetMozPerspective(nsAString_internal&);
+  NS_IMETHOD SetMozPerspective(const nsAString_internal&);
+  NS_IMETHOD GetMozPerspectiveOrigin(nsAString_internal&);
+  NS_IMETHOD SetMozPerspectiveOrigin(const nsAString_internal&);
+  NS_IMETHOD GetMozBackfaceVisibility(nsAString_internal&);
+  NS_IMETHOD SetMozBackfaceVisibility(const nsAString_internal&);
+
   // Require subclasses to implement |GetParentRule|.
   //NS_DECL_NSIDOMCSSSTYLEDECLARATION
   NS_IMETHOD GetCssText(nsAString & aCssText);
