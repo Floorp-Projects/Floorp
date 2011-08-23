4696e81d5dfed725d904d4d3e979c3d324cf60ac
Jason Orendorff <jorendorff@mozilla.com>
2011-08-23 15:34:31 -0500

    Back out 331146c93ebe (bug 673125 part 3).

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -1482,8 +1482,7 @@ class AutoGCRooter {
         STRING =      -14, /* js::AutoStringRooter */
         IDVECTOR =    -15, /* js::AutoIdVector */
         BINDINGS =    -16, /* js::Bindings */
-        SHAPEVECTOR = -17, /* js::AutoShapeVector */
-        OBJVECTOR =   -18  /* js::AutoObjectVector */
+        SHAPEVECTOR = -17  /* js::AutoShapeVector */
     };
 
     private:
@@ -2484,19 +2483,6 @@ class AutoValueVector : public AutoVectorRooter<Value>
 
     const jsval *jsval_end() const { return Jsvalify(end()); }
     jsval *jsval_end() { return Jsvalify(end()); }
-
-    JS_DECL_USE_GUARD_OBJECT_NOTIFIER
-};
-
-class AutoObjectVector : public AutoVectorRooter<JSObject *>
-{
-  public:
-    explicit AutoObjectVector(JSContext *cx
-                              JS_GUARD_OBJECT_NOTIFIER_PARAM)
-        : AutoVectorRooter<JSObject *>(cx, OBJVECTOR)
-    {
-        JS_GUARD_OBJECT_NOTIFIER_INIT;
-    }
 
     JS_DECL_USE_GUARD_OBJECT_NOTIFIER
 };
