cab535ee1ce70594fe26048c6d0cc4e500df0488
Matheus Kerschbaum <matjk7@gmail.com>
2011-08-26 00:34:45 +0100

    Bug 680792 - Remove support for Irix from our build system. r=ted

--- config/config.mk
+++ config/config.mk
@@ -129,7 +129,7 @@ endif
 # but save the version to allow multiple versions of the same base
 # platform to be built in the same tree.
 #
-ifneq (,$(filter FreeBSD HP-UX IRIX Linux NetBSD OpenBSD OSF1 SunOS,$(OS_ARCH)))
+ifneq (,$(filter FreeBSD HP-UX Linux NetBSD OpenBSD OSF1 SunOS,$(OS_ARCH)))
 OS_RELEASE	:= $(basename $(OS_RELEASE))
 
 # Allow the user to ignore the OS_VERSION, which is usually irrelevant.
