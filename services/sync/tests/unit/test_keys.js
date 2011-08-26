26fed849c5b2192f64fdfe386fe91cd66cb1e548
Philipp von Weitershausen <philipp@weitershausen.de>
2011-08-26 10:27:29 -0700

    Bug 681864 - Get rid of unused Utils.sha* helpers. r=rnewman

--- services/sync/tests/unit/test_keys.js
+++ services/sync/tests/unit/test_keys.js
@@ -4,6 +4,11 @@ Cu.import("resource://services-sync/record.js");
 Cu.import("resource://services-sync/constants.js");
 btoa = Cu.import("resource://services-sync/util.js").btoa;
 
+function sha256HMAC(message, key) {
+  let h = Utils.makeHMACHasher(Ci.nsICryptoHMAC.SHA256, key);
+  return Utils.digestBytes(message, h);
+}
+
 function test_time_keyFromString(iterations) {
   let k;
   let o;
@@ -11,10 +16,10 @@ function test_time_keyFromString(iterations) {
   let d = Utils.decodeKeyBase32("ababcdefabcdefabcdefabcdef");
   b.generateRandom();
   
-  _("Running " + iterations + " iterations of hmacKeyObject + sha256HMACBytes.");
+  _("Running " + iterations + " iterations of hmacKeyObject + sha256HMAC.");
   for (let i = 0; i < iterations; ++i) {
     let k = b.hmacKeyObject;
-    o = Utils.sha256HMACBytes(d, k);
+    o = sha256HMAC(d, k);
   }
   do_check_true(!!o);
   _("Done.");
@@ -23,8 +28,8 @@ function test_time_keyFromString(iterations) {
 function test_repeated_hmac() {
   let testKey = "ababcdefabcdefabcdefabcdef";
   let k = Utils.makeHMACKey("foo");
-  let one = Utils.sha256HMACBytes(Utils.decodeKeyBase32(testKey), k);
-  let two = Utils.sha256HMACBytes(Utils.decodeKeyBase32(testKey), k);
+  let one = sha256HMAC(Utils.decodeKeyBase32(testKey), k);
+  let two = sha256HMAC(Utils.decodeKeyBase32(testKey), k);
   do_check_eq(one, two);
 }
 
@@ -45,10 +50,10 @@ function test_keymanager() {
   
   let sha256inputE = "" + HMAC_INPUT + username + "\x01";
   let key = Utils.makeHMACKey(Utils.decodeKeyBase32(testKey));
-  let encryptKey = Utils.sha256HMACBytes(sha256inputE, key);
+  let encryptKey = sha256HMAC(sha256inputE, key);
   
   let sha256inputH = encryptKey + HMAC_INPUT + username + "\x02";
-  let hmacKey = Utils.sha256HMACBytes(sha256inputH, key);
+  let hmacKey = sha256HMAC(sha256inputH, key);
   
   // Encryption key is stored in base64 for WeaveCrypto convenience.
   do_check_eq(btoa(encryptKey), new SyncKeyBundle(null, username, testKey).encryptionKey);
