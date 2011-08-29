5211efa149bf9b9f96ed7df366fd6b32be2771ed
Oleg Romashin <romaxa@gmail.com>
2011-08-29 17:26:23 +0100

    Bug 682017 - Remove CustomKeySender and use content KeyEvents forwarding; r=mfinkle

--- mobile/chrome/content/common-ui.js
+++ mobile/chrome/content/common-ui.js
@@ -593,7 +593,6 @@ var FormHelperUI = {
         if (focusedElement && focusedElement.localName == "browser")
           return;
 
-        Browser.keySender.handleEvent(aEvent);
         break;
 
       case "SizeChanged":
