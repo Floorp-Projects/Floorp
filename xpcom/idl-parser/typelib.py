d6ae1202d58fb376a0af49344719b96702f28937
Kyle Huey <khuey@kylehuey.com>
2011-09-02 17:36:48 -0400

    Bug 683096: Make arrays of nsQIResults work. r=ted

--- xpcom/idl-parser/typelib.py
+++ xpcom/idl-parser/typelib.py
@@ -94,7 +94,9 @@ def build_interface(iface, ifaces):
                                         reference=False)
 
         if isinstance(type, xpidl.Array):
-            return xpt.ArrayType(get_type(type.type, calltype), size_is,
+            # NB: For an Array<T> we pass down the iid_is to get the type of T.
+            #     This allows Arrays of InterfaceIs types to work.
+            return xpt.ArrayType(get_type(type.type, calltype, iid_is), size_is,
                                  #XXXkhuey length_is duplicates size_is (bug 677788),
                                  size_is)
 
