b28b781bde111087f6721e172febfee07a7cc612
Ehsan Akhgari <ehsan@mozilla.com>
2011-09-02 09:04:24 -0400

    Keep changeset 6a70000360a0 backed out on the reverted tree

--- toolkit/mozapps/installer/packager.mk
+++ toolkit/mozapps/installer/packager.mk
@@ -397,18 +397,8 @@ endif
 
 ifdef MOZ_OMNIJAR
 
-# Set MOZ_CAN_RUN_PROGRAMS for trivial cases.
-ifndef MOZ_CAN_RUN_PROGRAMS
-ifdef UNIVERSAL_BINARY
-MOZ_CAN_RUN_PROGRAMS=1
-endif
-ifndef CROSS_COMPILE
-MOZ_CAN_RUN_PROGRAMS=1
-endif
-endif # MOZ_CAN_RUN_PROGRAMS
-
 ifdef GENERATE_CACHE
-ifdef MOZ_CAN_RUN_PROGRAMS
+ifneq (1_,$(if $(CROSS_COMPILE),1,0)_$(UNIVERSAL_BINARY))
 ifdef RUN_TEST_PROGRAM
 _ABS_RUN_TEST_PROGRAM = $(call core_abspath,$(RUN_TEST_PROGRAM))
 endif
@@ -487,7 +477,7 @@ endif
 
 # dummy macro if we don't have PSM built
 SIGN_NSS		=
-ifdef MOZ_CAN_RUN_PROGRAMS
+ifneq (1_,$(if $(CROSS_COMPILE),1,0)_$(UNIVERSAL_BINARY))
 ifdef MOZ_PSM
 SIGN_NSS		= @echo signing nss libraries;
 
