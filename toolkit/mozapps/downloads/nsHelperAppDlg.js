758ce2b3084668e4407ce5d471ca86aeba17cf2e
Kailas <patilkr24@gmail.com>
2011-09-06 15:32:52 -0400

    Bug 683258 - Size reported as -1 bytes when attempting download with no Content-Length header; r=dolske

--- toolkit/mozapps/downloads/nsHelperAppDlg.js
+++ toolkit/mozapps/downloads/nsHelperAppDlg.js
@@ -611,7 +611,8 @@ nsUnknownContentTypeDialog.prototype = {
       else
         typeString = mimeInfo.MIMEType;
     }
-    if (this.mLauncher.contentLength) {
+    // When the length is unknown, contentLength would be -1
+    if (this.mLauncher.contentLength >= 0) {
       let [size, unit] = DownloadUtils.
                          convertByteUnits(this.mLauncher.contentLength);
       type.value = this.dialogElement("strings")
