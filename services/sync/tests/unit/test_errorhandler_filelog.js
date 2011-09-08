8b6d37b6db47b627b81ae02732182af26dc94ea9
Chenxia Liu <liuche@mozilla.com>
2011-09-08 16:52:07 -0700

    Bug 663181 - Automatic cleanup for Sync error logs. r=philikon

--- services/sync/tests/unit/test_errorhandler_filelog.js
+++ services/sync/tests/unit/test_errorhandler_filelog.js
@@ -5,9 +5,11 @@ Cu.import("resource://services-sync/service.js");
 Cu.import("resource://services-sync/util.js");
 Cu.import("resource://services-sync/log4moz.js");
 
-const logsdir = FileUtils.getDir("ProfD", ["weave", "logs"], true);
+const logsdir            = FileUtils.getDir("ProfD", ["weave", "logs"], true);
 const LOG_PREFIX_SUCCESS = "success-";
 const LOG_PREFIX_ERROR   = "error-";
+const CLEANUP_DELAY      = 1000; // delay to age files for cleanup (ms)
+const DELAY_BUFFER       = 50; // buffer for timers on different OS platforms
 
 const PROLONGED_ERROR_DURATION =
   (Svc.Prefs.get('errorhandler.networkFailureReportTimeout') * 2) * 1000;
@@ -230,4 +232,54 @@ add_test(function test_login_error_logOnError_true() {
   // Fake an unsuccessful login due to prolonged failure.
   setLastSync(PROLONGED_ERROR_DURATION);
   Svc.Obs.notify("weave:service:login:error");
+});
+
+// Check that error log files are deleted above an age threshold.
+add_test(function test_logErrorCleanup_age() {
+  let maxAge = CLEANUP_DELAY/1000;
+  let firstlog_name;
+  let oldLogs = [];
+  let numLogs = 10;
+  let errString = "some error log\n";
+
+  Svc.Prefs.set("log.appender.file.logOnError", true);
+  Svc.Prefs.set("log.appender.file.maxErrorAge", maxAge);
+
+  // Make some files.
+  for (let i = 0; i < numLogs; i++) {
+    let filename = LOG_PREFIX_ERROR + Date.now() + i + ".txt";
+    let newLog = FileUtils.getFile("ProfD", ["weave", "logs", filename]);
+    let foStream = FileUtils.openFileOutputStream(newLog);
+    foStream.write(errString, errString.length);
+    foStream.close();
+    oldLogs.push(newLog.leafName);
+  }
+
+  Svc.Obs.add("weave:service:cleanup-logs", function onCleanupLogs() {
+    Svc.Obs.remove("weave:service:cleanup-logs", onCleanupLogs);
+
+    // Only the newest created log file remains.
+    let entries = logsdir.directoryEntries;
+    do_check_true(entries.hasMoreElements());
+    let logfile = entries.getNext().QueryInterface(Ci.nsILocalFile);
+    do_check_true(oldLogs.every(function (e) {
+      return e != logfile.leafName;
+    }));
+    do_check_false(entries.hasMoreElements());
+
+    // Clean up.
+    try {
+      logfile.remove(false);
+    } catch(ex) {
+      dump("Couldn't delete file: " + ex + "\n");
+      // Stupid Windows box.
+    }
+
+    Svc.Prefs.resetBranch("");
+    run_next_test();
+  });
+
+  Utils.namedTimer(function () Svc.Obs.notify("weave:service:sync:error"),
+                   CLEANUP_DELAY + DELAY_BUFFER, this, "cleanup-timer");
+
 });
