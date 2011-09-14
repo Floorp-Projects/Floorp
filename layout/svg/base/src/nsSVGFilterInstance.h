c1803beadff059a389b5c618c42c54634d0dc305
Robert O'Callahan <roc@ocallahan.org>
2011-09-14 15:48:32 +0100

    Bug 619992 - Fix location calculations for light sources. r=longsonr

--- layout/svg/base/src/nsSVGFilterInstance.h
+++ layout/svg/base/src/nsSVGFilterInstance.h
@@ -63,8 +63,6 @@ struct gfxRect;
 class NS_STACK_CLASS nsSVGFilterInstance
 {
 public:
-  void ConvertLocation(float aValues[3]) const;
-
   nsSVGFilterInstance(nsIFrame *aTargetFrame,
                       nsSVGFilterPaintCallback *aPaintCallback,
                       nsSVGFilterElement *aFilterElement,
@@ -115,10 +113,18 @@ public:
   {
     return GetPrimitiveNumber(aCtxType, aNumberPair->GetAnimValue(aIndex));
   }
+  /**
+   * Converts a point and a length in filter primitive units into filter space.
+   * For object-bounding-box units, the object bounding box offset is applied
+   * to the point.
+   */
+  void ConvertLocation(float aValues[3]) const;
+
   gfxMatrix GetUserSpaceToFilterSpaceTransform() const;
   gfxMatrix GetFilterSpaceToDeviceSpaceTransform() const {
     return mFilterSpaceToDeviceSpaceTransform;
   }
+  gfxPoint FilterSpaceToUserSpace(const gfxPoint& aPt) const;
 
 private:
   typedef nsSVGFE::Image Image;
