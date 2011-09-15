e626e8ff0c11fd8023c01f72bc4304389243c8ef
Neil Rashbrook <neil@parkwaycc.co.uk>
2011-09-15 08:40:17 +0100

    Bug 611503 Constructing an nsTDependentString from an nsTAString makes no sense r=dbaron,dwitte

--- xpcom/string/src/nsTDependentString.cpp
+++ xpcom/string/src/nsTDependentString.cpp
@@ -48,3 +48,20 @@ nsTDependentString_CharT::Rebind( const char_type* data, size_type length )
     SetDataFlags(F_TERMINATED);
     AssertValid();
   }
+
+void
+nsTDependentString_CharT::Rebind( const string_type& str, PRUint32 startPos )
+  {
+    // If we currently own a buffer, release it.
+    Finalize();
+
+    size_type strLength = str.Length();
+
+    if (startPos > strLength)
+      startPos = strLength;
+
+    mData = const_cast<char_type*>(str.Data()) + startPos;
+    mLength = strLength - startPos;
+
+    SetDataFlags(F_TERMINATED);
+  }
