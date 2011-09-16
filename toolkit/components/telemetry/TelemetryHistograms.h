1e44a0ac60daefef5caf668b9d1d38dcc6298465
Trevor Saunders <trev.saunders@gmail.com>
2011-09-15 23:39:15 -0400

    bug 682247 - telemetry for deprocated IAccessibleTable interface r=surkov

--- toolkit/components/telemetry/TelemetryHistograms.h
+++ toolkit/components/telemetry/TelemetryHistograms.h
@@ -54,6 +54,7 @@
  */
 HISTOGRAM(A11Y_INSTANTIATED, 0, 1, 2, BOOLEAN, "has accessibility support been instantiated")
 HISTOGRAM(ISIMPLE_DOM_USAGE, 0, 1, 2, BOOLEAN, "have the ISimpleDOM* accessibility interfaces been used")
+HISTOGRAM(IACCESSIBLE_TABLE_USAGE, 0, 1, 2, BOOLEAN, "has the IAccessibleTable accessibility interface been used")
 
 HISTOGRAM(CYCLE_COLLECTOR, 1, 10000, 50, EXPONENTIAL, "Time spent on one cycle collection (ms)")
 HISTOGRAM(CYCLE_COLLECTOR_VISITED_REF_COUNTED, 1, 300000, 50, EXPONENTIAL, "Number of ref counted objects visited by the cycle collector")
