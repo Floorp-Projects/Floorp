153490ba94e2bc0fdf7d9ab7ae0a6a967683bd9d
James Willcox <jwillcox@mozilla.com>
2011-09-21 12:46:00 -0400

    Bug 686992 - Draw to Android window/surface directly r=blassey

--- widget/src/android/AndroidBridge.h
+++ widget/src/android/AndroidBridge.h
@@ -267,6 +267,22 @@ public:
 
     void ExecuteNextRunnable();
 
+    /* Copied from Android's native_window.h in newer (platform 9) NDK */
+    enum {
+        WINDOW_FORMAT_RGBA_8888          = 1,
+        WINDOW_FORMAT_RGBX_8888          = 2,
+        WINDOW_FORMAT_RGB_565            = 4,
+    };
+
+    bool HasNativeWindowAccess();
+
+    void *AcquireNativeWindow(jobject surface);
+    void ReleaseNativeWindow(void *window);
+    bool SetNativeWindowFormat(void *window, int format);
+
+    bool LockWindow(void *window, unsigned char **bits, int *width, int *height, int *format, int *stride);
+    bool UnlockWindow(void *window);
+
 protected:
     static AndroidBridge *sBridge;
 
@@ -288,8 +304,11 @@ protected:
 
     void EnsureJNIThread();
 
-    bool mOpenedBitmapLibrary;
+    bool mOpenedGraphicsLibraries;
+    void OpenGraphicsLibraries();
+
     bool mHasNativeBitmapAccess;
+    bool mHasNativeWindowAccess;
 
     nsCOMArray<nsIRunnable> mRunnableQueue;
 
@@ -346,6 +365,13 @@ protected:
     int (* AndroidBitmap_getInfo)(JNIEnv *env, jobject bitmap, void *info);
     int (* AndroidBitmap_lockPixels)(JNIEnv *env, jobject bitmap, void **buffer);
     int (* AndroidBitmap_unlockPixels)(JNIEnv *env, jobject bitmap);
+
+    void* (*ANativeWindow_fromSurface)(JNIEnv *env, jobject surface);
+    void (*ANativeWindow_release)(void *window);
+    int (*ANativeWindow_setBuffersGeometry)(void *window, int width, int height, int format);
+
+    int (* ANativeWindow_lock)(void *window, void *outBuffer, void *inOutDirtyBounds);
+    int (* ANativeWindow_unlockAndPost)(void *window);
 };
 
 }
