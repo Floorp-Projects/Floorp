63a7fe6a7a72ff189093bf5e2685845abfcda945
Bobby Holley <bobbyholley@gmail.com>
2011-09-23 14:50:27 -0700

    Bug 684327 - Start building a small subset of the xpconnect test component again, kill the rest. r=khuey
    Thanks to Mook for the initial patch!

--- js/src/xpconnect/tests/components/xpctest_module.cpp
+++ js/src/xpconnect/tests/components/xpctest_module.cpp
@@ -47,75 +47,18 @@
 #include "nsCRT.h"
 #include "nsIClassInfoImpl.h"
 
-NS_DEFINE_NAMED_CID(NS_ECHO_CID);
-NS_DEFINE_NAMED_CID(NS_CHILD_CID);
-NS_DEFINE_NAMED_CID(NS_NOISY_CID);
-NS_DEFINE_NAMED_CID(NS_STRING_TEST_CID);
-NS_DEFINE_NAMED_CID(NS_OVERLOADED_CID);
 NS_DEFINE_NAMED_CID(NS_XPCTESTOBJECTREADONLY_CID);
 NS_DEFINE_NAMED_CID(NS_XPCTESTOBJECTREADWRITE_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTIN_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTOUT_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTINOUT_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTCONST_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTCALLJS_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTPARENTONE_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTPARENTTWO_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTCHILD2_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTCHILD3_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTCHILD4_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTCHILD5_CID);
-NS_DEFINE_NAMED_CID(NS_ARRAY_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTDOMSTRING_CID);
-NS_DEFINE_NAMED_CID(NS_XPCTESTVARIANT_CID);
 
 static const mozilla::Module::CIDEntry kXPCTestCIDs[] = {
-    { &kNS_ECHO_CID, false, NULL, xpctest::ConstructEcho },
-    { &kNS_CHILD_CID, false, NULL, xpctest::ConstructChild },
-    { &kNS_NOISY_CID, false, NULL, xpctest::ConstructNoisy },
-    { &kNS_STRING_TEST_CID, false, NULL, xpctest::ConstructStringTest },
-    { &kNS_OVERLOADED_CID, false, NULL, xpctest::ConstructOverloaded },
     { &kNS_XPCTESTOBJECTREADONLY_CID, false, NULL, xpctest::ConstructXPCTestObjectReadOnly },
     { &kNS_XPCTESTOBJECTREADWRITE_CID, false, NULL, xpctest::ConstructXPCTestObjectReadWrite },
-    { &kNS_XPCTESTIN_CID, false, NULL, xpctest::ConstructXPCTestIn },
-    { &kNS_XPCTESTOUT_CID, false, NULL, xpctest::ConstructXPCTestOut },
-    { &kNS_XPCTESTINOUT_CID, false, NULL, xpctest::ConstructXPCTestInOut },
-    { &kNS_XPCTESTCONST_CID, false, NULL, xpctest::ConstructXPCTestConst },
-    { &kNS_XPCTESTCALLJS_CID, false, NULL, xpctest::ConstructXPCTestCallJS },
-    { &kNS_XPCTESTPARENTONE_CID, false, NULL, xpctest::ConstructXPCTestParentOne },
-    { &kNS_XPCTESTPARENTTWO_CID, false, NULL, xpctest::ConstructXPCTestParentTwo },
-    { &kNS_XPCTESTCHILD2_CID, false, NULL, xpctest::ConstructXPCTestChild2 },
-    { &kNS_XPCTESTCHILD3_CID, false, NULL, xpctest::ConstructXPCTestChild3 },
-    { &kNS_XPCTESTCHILD4_CID, false, NULL, xpctest::ConstructXPCTestChild4 },
-    { &kNS_XPCTESTCHILD5_CID, false, NULL, xpctest::ConstructXPCTestChild5 },
-    { &kNS_ARRAY_CID, false, NULL, xpctest::ConstructArrayTest },
-    { &kNS_XPCTESTDOMSTRING_CID, false, NULL, xpctest::ConstructXPCTestDOMString },
-    { &kNS_XPCTESTVARIANT_CID, false, NULL, xpctest::ConstructXPCTestVariant },
     { NULL }
 };
 
 static const mozilla::Module::ContractIDEntry kXPCTestContracts[] = {
-    { "@mozilla.org/js/xpc/test/Echo;1", &kNS_ECHO_CID },
-    { "@mozilla.org/js/xpc/test/Child;1", &kNS_CHILD_CID },
-    { "@mozilla.org/js/xpc/test/Noisy;1", &kNS_NOISY_CID },
-    { "@mozilla.org/js/xpc/test/StringTest;1", &kNS_STRING_TEST_CID },
-    { "@mozilla.org/js/xpc/test/Overloaded;1", &kNS_OVERLOADED_CID },
     { "@mozilla.org/js/xpc/test/ObjectReadOnly;1", &kNS_XPCTESTOBJECTREADONLY_CID },
     { "@mozilla.org/js/xpc/test/ObjectReadWrite;1", &kNS_XPCTESTOBJECTREADWRITE_CID },
-    { "@mozilla.org/js/xpc/test/In;1", &kNS_XPCTESTIN_CID },
-    { "@mozilla.org/js/xpc/test/Out;1", &kNS_XPCTESTOUT_CID },
-    { "@mozilla.org/js/xpc/test/InOut;1", &kNS_XPCTESTINOUT_CID },
-    { "@mozilla.org/js/xpc/test/Const;1", &kNS_XPCTESTCONST_CID },
-    { "@mozilla.org/js/xpc/test/CallJS;1", &kNS_XPCTESTCALLJS_CID },
-    { "@mozilla.org/js/xpc/test/ParentOne;1", &kNS_XPCTESTPARENTONE_CID },
-    { "@mozilla.org/js/xpc/test/ParentTwo;1", &kNS_XPCTESTPARENTTWO_CID },
-    { "@mozilla.org/js/xpc/test/Child2;1", &kNS_XPCTESTCHILD2_CID },
-    { "@mozilla.org/js/xpc/test/Child3;1", &kNS_XPCTESTCHILD3_CID },
-    { "@mozilla.org/js/xpc/test/Child4;1", &kNS_XPCTESTCHILD4_CID },
-    { "@mozilla.org/js/xpc/test/Child5;1", &kNS_XPCTESTCHILD5_CID },
-    { "@mozilla.org/js/xpc/test/ArrayTest;1", &kNS_ARRAY_CID },
-    { "@mozilla.org/js/xpc/test/DOMString;1", &kNS_XPCTESTDOMSTRING_CID },
-    { "@mozilla.org/js/xpc/test/TestVariant;1", &kNS_XPCTESTVARIANT_CID },
     { NULL }
 };
 
