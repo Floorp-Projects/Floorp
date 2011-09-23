95741426206c9c29a0bc7f1b403c598b839e7fbd
Ed Morley <bmo@edmorley.co.uk>
2011-09-23 10:34:42 +0100

    Backout changeset f4b1fa7a0f31 (bug 684889) for Android bustage

--- gfx/thebes/gfxFT2FontList.h
+++ gfx/thebes/gfxFT2FontList.h
@@ -55,69 +55,6 @@ namespace mozilla {
 using mozilla::dom::FontListEntry;
 
 class FontNameCache;
-typedef struct FT_FaceRec_* FT_Face;
-
-class FT2FontEntry : public gfxFontEntry
-{
-public:
-    FT2FontEntry(const nsAString& aFaceName) :
-        gfxFontEntry(aFaceName)
-    {
-        mFTFace = nsnull;
-        mFontFace = nsnull;
-        mFTFontIndex = 0;
-    }
-
-    ~FT2FontEntry();
-
-    const nsString& GetName() const {
-        return Name();
-    }
-
-    // create a font entry for a downloaded font
-    static FT2FontEntry* 
-    CreateFontEntry(const gfxProxyFontEntry &aProxyEntry,
-                    const PRUint8 *aFontData, PRUint32 aLength);
-
-    // create a font entry representing an installed font, identified by
-    // a FontListEntry; the freetype and cairo faces will not be instantiated
-    // until actually needed
-    static FT2FontEntry*
-    CreateFontEntry(const FontListEntry& aFLE);
-
-    // create a font entry for a given freetype face; if it is an installed font,
-    // also record the filename and index
-    static FT2FontEntry* 
-    CreateFontEntry(FT_Face aFace, const char *aFilename, PRUint8 aIndex,
-                    const PRUint8 *aFontData = nsnull);
-        // aFontData is NS_Malloc'ed data that aFace depends on, to be freed
-        // after the face is destroyed; null if there is no such buffer
-
-    virtual gfxFont *CreateFontInstance(const gfxFontStyle *aFontStyle,
-                                        PRBool aNeedsBold);
-
-    cairo_font_face_t *CairoFontFace();
-    cairo_scaled_font_t *CreateScaledFont(const gfxFontStyle *aStyle);
-
-    nsresult ReadCMAP();
-    nsresult GetFontTable(PRUint32 aTableTag, FallibleTArray<PRUint8>& aBuffer);
-
-    FT_Face mFTFace;
-    cairo_font_face_t *mFontFace;
-
-    nsCString mFilename;
-    PRUint8 mFTFontIndex;
-};
-
-class FT2FontFamily : public gfxFontFamily
-{
-public:
-    FT2FontFamily(const nsAString& aName) :
-        gfxFontFamily(aName) { }
-
-    // Append this family's faces to the IPC fontlist
-    void AddFacesToFontList(InfallibleTArray<FontListEntry>* aFontList);
-};
 
 class gfxFT2FontList : public gfxPlatformFontList
 {
