bdc7e4ee3e204f55a084c7b554a35d4f7dde6f73
Brian Birtles <birtles@gmail.com>
2011-09-25 22:04:31 +0100

    Bug 602759 part 7 - Refactor nsSVGAnimatedTransformList into SVGAnimatedTransformList and DOMSVGAnimatedTransformList and incorporate nsSVGTransformSMILAttr; r=jwatt
    --HG--
    rename : content/svg/content/src/nsSVGAnimatedTransformList.cpp => content/svg/content/src/DOMSVGAnimatedTransformList.cpp
    rename : content/svg/content/src/nsSVGAnimatedTransformList.h => content/svg/content/src/DOMSVGAnimatedTransformList.h

--- content/svg/content/src/nsSVGAnimateTransformElement.cpp
+++ content/svg/content/src/nsSVGAnimateTransformElement.cpp
@@ -42,7 +42,6 @@
 #include "nsSVGEnum.h"
 #include "nsIDOMSVGTransform.h"
 #include "nsIDOMSVGTransformable.h"
-#include "nsSVGAnimatedTransformList.h"
 #include "nsSVGTransformSMILAttr.h"
 #include "nsSMILAnimationFunction.h"
 
