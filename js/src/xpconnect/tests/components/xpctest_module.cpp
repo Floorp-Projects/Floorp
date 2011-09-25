cb36277b9470526d104b572fb586b63865f99efe
Bobby Holley <bobbyholley@gmail.com>
2011-09-25 15:37:59 +0100

    Bug 684327 - Misc fixes to xpctest_attributes. r=khuey
    This patch is best reviewed while listening to http://www.youtube.com/watch?v=MK6TXMsvgQg
    
    NB - The code here would make a great "find the bugs in this code" interview screen question.

--- js/src/xpconnect/tests/components/xpctest_module.cpp
+++ js/src/xpconnect/tests/components/xpctest_module.cpp
@@ -40,19 +40,29 @@
 
 /* module registration and factory code. */
 
-#include "xpctest_private.h"
 #include "nsCOMPtr.h"
 #include "nsIModule.h"
 #include "mozilla/ModuleUtils.h"
 #include "nsCRT.h"
 #include "nsIClassInfoImpl.h"
+#include "xpctest_private.h"
 
+#define NS_XPCTESTOBJECTREADONLY_CID \
+{ 0x492609a7, 0x2582, 0x436b, \
+   { 0xb0, 0xef, 0x92, 0xe2, 0x9b, 0xb9, 0xe1, 0x43 } }
+
+#define NS_XPCTESTOBJECTREADWRITE_CID \
+{ 0x8f37f760, 0x3686, 0x4dbb, \
+   { 0xb1, 0x21, 0x96, 0x93, 0xba, 0x81, 0x3f, 0x8f } }
+
+NS_GENERIC_FACTORY_CONSTRUCTOR(xpcTestObjectReadOnly)
+NS_GENERIC_FACTORY_CONSTRUCTOR(xpcTestObjectReadWrite)
 NS_DEFINE_NAMED_CID(NS_XPCTESTOBJECTREADONLY_CID);
 NS_DEFINE_NAMED_CID(NS_XPCTESTOBJECTREADWRITE_CID);
 
 static const mozilla::Module::CIDEntry kXPCTestCIDs[] = {
-    { &kNS_XPCTESTOBJECTREADONLY_CID, false, NULL, xpctest::ConstructXPCTestObjectReadOnly },
-    { &kNS_XPCTESTOBJECTREADWRITE_CID, false, NULL, xpctest::ConstructXPCTestObjectReadWrite },
+    { &kNS_XPCTESTOBJECTREADONLY_CID, false, NULL, xpcTestObjectReadOnlyConstructor },
+    { &kNS_XPCTESTOBJECTREADWRITE_CID, false, NULL, xpcTestObjectReadWriteConstructor },
     { NULL }
 };
 
