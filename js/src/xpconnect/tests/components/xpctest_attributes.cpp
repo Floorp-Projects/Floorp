904f7af5eb17bbff9159dadf612a8fa4cb77262f
Bobby Holley <bobbyholley@gmail.com>
2011-09-25 15:37:59 +0100

    Bug 684327 - Misc fixes to xpctest_attributes. r=khuey
    This patch is best reviewed while listening to http://www.youtube.com/watch?v=MK6TXMsvgQg
    
    NB - The code here would make a great "find the bugs in this code" interview screen question.

--- js/src/xpconnect/tests/components/xpctest_attributes.cpp
+++ js/src/xpconnect/tests/components/xpctest_attributes.cpp
@@ -38,47 +38,16 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include "xpctest_attributes.h"
-#include "nsISupports.h"
 #include "xpctest_private.h"
-
-#define NS_IXPCTESTOBJECTREADONLY_IID \
-  {0x1364941e, 0x4462, 0x11d3, \
-    { 0x82, 0xee, 0x00, 0x60, 0xb0, 0xeb, 0x59, 0x6f }}
-
-class xpcTestObjectReadOnly : public nsIXPCTestObjectReadOnly {
- public: 
-  NS_DECL_ISUPPORTS
-  NS_DECL_NSIXPCTESTOBJECTREADONLY
-  xpcTestObjectReadOnly();
-   
- private:
-    PRBool  boolProperty;
-    PRInt16 shortProperty;
-    PRInt32 longProperty;
-    float   floatProperty;
-    char    charProperty;
-    char    *stringID;
-};
 
 NS_IMPL_ISUPPORTS1(xpcTestObjectReadOnly, nsIXPCTestObjectReadOnly)
 
 xpcTestObjectReadOnly :: xpcTestObjectReadOnly() {
-    NS_ADDREF_THIS();
-
     boolProperty = PR_TRUE;
     shortProperty = 32767;
     longProperty =  2147483647;
+    floatProperty = 5.5f;
     charProperty = 'X';
-
-    const char _id[] = "a68cc6a6-6552-11d3-82ef-0060b0eb596f";
-    stringID = (char*) nsMemory::Clone(_id, sizeof(char)*(strlen(_id)+1));
-}
-
-NS_IMETHODIMP xpcTestObjectReadOnly :: GetID(char **_retval) {
-    *_retval= (char*) nsMemory::Clone(stringID, 
-                                         sizeof(char)*(strlen(stringID)+1));
-    return *_retval? NS_OK : NS_ERROR_OUT_OF_MEMORY;
 }
 
 NS_IMETHODIMP xpcTestObjectReadOnly :: GetStrReadOnly(char * *aStrReadOnly){
@@ -86,8 +55,8 @@ NS_IMETHODIMP xpcTestObjectReadOnly :: GetStrReadOnly(char * *aStrReadOnly){
 
     if(!aStrReadOnly)
         return NS_ERROR_NULL_POINTER;
-    *aStrReadOnly = (char*) nsMemory::Clone(aStrReadOnly, 
-                                               sizeof(char)*(strlen(aString)+1));
+    *aStrReadOnly = (char*) nsMemory::Clone(aString,
+                                            sizeof(char)*(strlen(aString)+1));
     return *aStrReadOnly ? NS_OK : NS_ERROR_OUT_OF_MEMORY;
 }
 
@@ -111,68 +80,22 @@ NS_IMETHODIMP xpcTestObjectReadOnly :: GetCharReadOnly(char *aCharReadOnly){
     *aCharReadOnly = charProperty;
     return NS_OK;
 }
-nsresult
-xpctest::ConstructXPCTestObjectReadOnly(nsISupports *aOuter, REFNSIID aIID, void **aResult)
-{
-    nsresult rv;
-    NS_ASSERTION(aOuter == nsnull, "no aggregation");
-    xpcTestObjectReadOnly *obj = new xpcTestObjectReadOnly();
-
-    if(obj)
-    {
-        rv = obj->QueryInterface(aIID, aResult);
-        NS_ASSERTION(NS_SUCCEEDED(rv), "unable to find correct interface");
-        NS_RELEASE(obj);
-    }
-    else
-    {
-        *aResult = nsnull;
-        rv = NS_ERROR_OUT_OF_MEMORY;
-    }
-
-    return rv;
-}
-/****************************************************************************/
-/* starting interface:    nsIXPCTestObjectReadWrite */
-/* {3b9b1d38-491a-11d3-82ef-0060b0eb596f} */
-/*
-
-#define NS_IXPCTESTOBJECTREADWRITE_IID_STR "3b9b1d38-491a-11d3-82ef-0060b0eb596f"
-*/
-#define NS_IXPCTESTOBJECTREADWRITE_IID \
-  {0x3b9b1d38, 0x491a, 0x11d3, \
-    { 0x82, 0xef, 0x00, 0x60, 0xb0, 0xeb, 0x59, 0x6f }}
-
-class xpcTestObjectReadWrite : public nsIXPCTestObjectReadWrite {
-  public: 
-  NS_DECL_ISUPPORTS
-  NS_DECL_NSIXPCTESTOBJECTREADWRITE
-  
-  xpcTestObjectReadWrite();
-
- private:
-     PRBool boolProperty;
-     PRInt16 shortProperty;
-     PRInt32 longProperty;
-     float floatProperty;
-     char charProperty;
-     const char *stringProperty;
-};
 
 NS_IMPL_ISUPPORTS1(xpcTestObjectReadWrite, nsIXPCTestObjectReadWrite)
 
 xpcTestObjectReadWrite :: xpcTestObjectReadWrite() {
-    NS_ADDREF_THIS();
-
-
+    const char s[] = "XPConnect Read-Writable String";
+    stringProperty = (char*) nsMemory::Clone(s, sizeof(char)*(strlen(s)+1));
     boolProperty = PR_TRUE;
     shortProperty = 32767;
     longProperty =  2147483647;
+    floatProperty = 5.5f;
     charProperty = 'X';
+}
 
-    const char s[] = "XPConnect Read-Writable String";
-    stringProperty = (char*) nsMemory::Clone(s, 
-                                                sizeof(char)*(strlen(s)+1));
+xpcTestObjectReadWrite :: ~xpcTestObjectReadWrite()
+{
+    nsMemory::Free(stringProperty);
 }
 
 NS_IMETHODIMP xpcTestObjectReadWrite :: GetStringProperty(char * *aStringProperty) {
@@ -184,7 +107,9 @@ NS_IMETHODIMP xpcTestObjectReadWrite :: GetStringProperty(char * *aStringPropert
 
 }
 NS_IMETHODIMP xpcTestObjectReadWrite :: SetStringProperty(const char * aStringProperty) {
-    stringProperty = aStringProperty;
+    nsMemory::Free(stringProperty);
+    stringProperty = (char*) nsMemory::Clone(aStringProperty,
+                                             sizeof(char)*(strlen(aStringProperty)+1));
     return NS_OK;
 }
 
@@ -193,6 +118,8 @@ NS_IMETHODIMP xpcTestObjectReadWrite :: GetBooleanProperty(PRBool *aBooleanPrope
     return NS_OK;
 }
 NS_IMETHODIMP xpcTestObjectReadWrite :: SetBooleanProperty(PRBool aBooleanProperty) {
+    NS_ENSURE_TRUE(aBooleanProperty == PR_TRUE || aBooleanProperty == PR_FALSE,
+                   NS_ERROR_INVALID_ARG);
     boolProperty = aBooleanProperty;
     return NS_OK;
 }
@@ -228,78 +155,3 @@ NS_IMETHODIMP xpcTestObjectReadWrite :: SetCharProperty(char aCharProperty) {
     charProperty = aCharProperty;
     return NS_OK;
 }
-nsresult
-xpctest::ConstructXPCTestObjectReadWrite(nsISupports *aOuter, REFNSIID aIID, void **aResult)
-{
-    nsresult rv;
-    NS_ASSERTION(aOuter == nsnull, "no aggregation");
-    xpcTestObjectReadWrite *obj = new xpcTestObjectReadWrite();
-
-    if(obj)
-    {
-        rv = obj->QueryInterface(aIID, aResult);
-        NS_ASSERTION(NS_SUCCEEDED(rv), "unable to find correct interface");
-        NS_RELEASE(obj);
-    }
-    else
-    {
-        *aResult = nsnull;
-        rv = NS_ERROR_OUT_OF_MEMORY;
-    }
-    return rv;
-}
-
-
-/****************************************************************************/
-/*
-class xpcTestAttributes : public nsIXPCTestObjectReadWrite, 
-    public nsIXPCTestObjectReadOnly
-{
-public: 
-  NS_DECL_ISUPPORTS
-  NS_DECL_NSIXPCTESTOBJECTREADONLY
-  NS_DECL_NSIXPCTESTOBJECTREADWRITE
-
-  NS_IMETHOD GetName(char * *aString);
-  NS_IMETHOD SetName(char * aString );
-
-private:
-    char *name;
-};
-  
-NS_IMPL_ISUPPORTS2(xpcTestAttributes, nsIXPCTestObjectReadWrite, nsIXPCTestObjectReadOnly)
-
-NS_IMETHODIMP xpcTestAttributes ::GetName(char * *aString) {
-    if(!aString)
-        return NS_ERROR_NULL_POINTER;
-    *aString = (char*) nsMemory::Clone(name, 
-                sizeof(char)*(strlen(name)+1));
-    return *aString ? NS_OK : NS_ERROR_OUT_OF_MEMORY;
-
-}
-NS_IMETHODIMP xpcTestAttributes ::SetName(char * aString) {
-    name = aString;
-    return NS_OK;
-}
-
-nsresult
-xpctest::ConstructXPCTestAttributes(nsISupports *aOuter, REFNSIID aIID, void **aResult)
-{
-    nsresult rv;
-    NS_ASSERTION(aOuter == nsnull, "no aggregation");
-    xpcTestAttributes *obj = new xpcTestAttributes();
-
-    if(obj)
-    {
-        rv = obj->QueryInterface(aIID, aResult);
-        NS_ASSERTION(NS_SUCCEEDED(rv), "unable to find correct interface");
-        NS_RELEASE(obj);
-    }
-    else
-    {
-        *aResult = nsnull;
-        rv = NS_ERROR_OUT_OF_MEMORY;
-    }
-    return rv;
-}
-*/
