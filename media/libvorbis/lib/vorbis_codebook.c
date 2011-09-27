94e83a3e1d5924f86eda1354bef5b244edb11bbb
Ralph Giles <giles@mozilla.com>
2011-09-27 19:59:34 +0100

    Bug 683823 - Update libvorbis to upstream r18077. r=kinetik This removes warning fixes which have been merged upstream since the 1.3.2 release. The only other upstream changes have been to documentation, which we don't include in our repository. I also corrected the usage comment at the top of update.sh.

--- media/libvorbis/lib/vorbis_codebook.c
+++ media/libvorbis/lib/vorbis_codebook.c
@@ -11,7 +11,7 @@
  ********************************************************************
 
  function: basic codebook pack/unpack/code/decode operations
- last mod: $Id: codebook.c 17553 2010-10-21 17:54:26Z tterribe $
+ last mod: $Id: codebook.c 18076 2011-09-02 02:44:49Z giles $
 
  ********************************************************************/
 
