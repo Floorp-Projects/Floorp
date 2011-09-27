78880b7b595fdd586caad70f9e545cd16f9a7a2f
Rafael Ávila de Espíndola <respindola@mozilla.com>
2011-09-27 10:29:42 -0400

    Revert 687e0bbfe996 while I try to find std::move is on the android SDK. r=revert.

--- js/src/jsutil.h
+++ js/src/jsutil.h
@@ -50,7 +50,6 @@
 #include <string.h>
 
 #ifdef __cplusplus
-#include <utility>
 
 /* The public JS engine namespace. */
 namespace JS {}
@@ -834,18 +833,7 @@ class MoveRef {
     explicit MoveRef(T &t) : pointer(&t) { }
     T &operator*()  const { return *pointer; }
     T *operator->() const { return  pointer; }
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-    /* If MoveRef is used in a rvalue position (which is expected), we can
-       end up in a situation where, without this ifdef, we would try to pass
-       a T& to a move constructor, which fails. It is not clear if the compiler
-       should instead use the copy constructor, but for now this lets us build
-       with clang. See bug 689066 and llvm.org/pr11003 for the details. */
-    /* Note: We can probably remove MoveRef completely once we are comfortable
-       using c++11. */
-    operator T&& ()  const { return std::move(*pointer); }
-#else
-    operator T& ()   const { return *pointer; }
-#endif
+    operator T &()  const { return *pointer; }
   private:
     T *pointer;
 };
