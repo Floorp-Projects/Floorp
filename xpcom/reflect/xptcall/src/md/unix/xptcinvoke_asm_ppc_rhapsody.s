1921ffc98dd60a15464b403cf20193b9dc46c477
Felix Fung <ffung@mozilla.com>
2011-09-28 11:32:39 -0700

    Bug 674012 - Remove checks for HAVE_GCC3_ABI. r=glandium

--- xpcom/reflect/xptcall/src/md/unix/xptcinvoke_asm_ppc_rhapsody.s
+++ xpcom/reflect/xptcall/src/md/unix/xptcinvoke_asm_ppc_rhapsody.s
@@ -115,9 +115,6 @@ __NS_InvokeByIndex_P:
 	lwz	r4,0(r3)                ; get vTable from 'that'
 	lwz	r5,172(r31)             ; methodIndex
 	slwi	r5,r5,2                 ; methodIndex * 4
-#ifndef HAVE_GCC3_ABI
-	addi	r5,r5,8                 ; (methodIndex * 4) + 8
-#endif
 	lwzx	r12,r5,r4               ; get function pointer
 
 	lwz	r4,28(r1)
