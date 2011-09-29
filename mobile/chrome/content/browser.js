dcd031117289dd041c1d7fad9d37ac6707114350
Matt Brubeck <mbrubeck@mozilla.com>
2011-09-28 18:29:22 -0700

    Bug 681995 - Zoom to viewport width by default instead of document width [r=mfinkle]

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -3110,15 +3110,16 @@ Tab.prototype = {
     if (md && md.defaultZoom)
       return this.clampZoomLevel(md.defaultZoom);
 
-    let pageZoom = this.getPageZoomLevel();
+    let browserWidth = this._browser.getBoundingClientRect().width;
+    let defaultZoom = browserWidth / this._browser.contentWindowWidth;
 
-    // If pageZoom is "almost" 100%, zoom in to exactly 100% (bug 454456).
+    // If defaultZoom is "almost" 100%, zoom in to exactly 100% (bug 454456).
     let granularity = Services.prefs.getIntPref("browser.ui.zoom.pageFitGranularity");
     let threshold = 1 - 1 / granularity;
-    if (threshold < pageZoom && pageZoom < 1)
-      pageZoom = 1;
+    if (threshold < defaultZoom && defaultZoom < 1)
+      defaultZoom = 1;
 
-    return this.clampZoomLevel(pageZoom);
+    return this.clampZoomLevel(defaultZoom);
   },
 
   /**
