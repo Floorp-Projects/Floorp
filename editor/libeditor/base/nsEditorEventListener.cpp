8c08170e70b7a012f604f11e9b84b5116415eee8
Ehsan Akhgari <ehsan@mozilla.com>
2011-09-29 15:24:13 -0400

    Bug 674770 - contenteditable breaks middle-click to open links when middlemouse.paste=true; r=roc

--- editor/libeditor/base/nsEditorEventListener.cpp
+++ editor/libeditor/base/nsEditorEventListener.cpp
@@ -522,6 +522,16 @@ nsEditorEventListener::MouseClick(nsIDOMEvent* aMouseEvent)
     return NS_OK;
   }
 
+  nsCOMPtr<nsIDOMEventTarget> target;
+  aMouseEvent->GetTarget(getter_AddRefs(target));
+  nsCOMPtr<nsIDOMNode> node = do_QueryInterface(target);
+  NS_ENSURE_TRUE(node, NS_ERROR_FAILURE);
+
+  if (!mEditor->IsModifiableNode(node)) {
+    // We shouldn't handle the event for non-editable content.
+    return NS_OK;
+  }
+
   bool preventDefault;
   nsresult rv = nsevent->GetPreventDefault(&preventDefault);
   if (NS_FAILED(rv) || preventDefault) {
