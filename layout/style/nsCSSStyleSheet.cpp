abcaf71c2a24f033203b27a456c4aeada1cb9497
Matt Brubeck <mbrubeck@mozilla.com>
2011-09-30 14:11:24 -0700

    Back out 6d1c0e678b61 (Bug 689319) because of test failures

--- layout/style/nsCSSStyleSheet.cpp
+++ layout/style/nsCSSStyleSheet.cpp
@@ -542,7 +542,8 @@ NS_IMPL_RELEASE(nsMediaList)
 
 
 nsMediaList::nsMediaList()
-  : mStyleSheet(nsnull)
+  : mIsEmpty(PR_TRUE)
+  , mStyleSheet(nsnull)
 {
 }
 
@@ -554,6 +555,10 @@ nsresult
 nsMediaList::GetText(nsAString& aMediaText)
 {
   aMediaText.Truncate();
+
+  if (mArray.Length() == 0 && !mIsEmpty) {
+    aMediaText.AppendLiteral("not all");
+  }
 
   for (PRInt32 i = 0, i_end = mArray.Length(); i < i_end; ++i) {
     nsMediaQuery* query = mArray[i];
@@ -596,7 +601,7 @@ nsMediaList::Matches(nsPresContext* aPresContext,
       return PR_TRUE;
     }
   }
-  return mArray.IsEmpty();
+  return mIsEmpty;
 }
 
 nsresult
@@ -619,6 +624,7 @@ nsMediaList::Clone(nsMediaList** aResult)
       return NS_ERROR_OUT_OF_MEMORY;
     }
   }
+  result->mIsEmpty = mIsEmpty;
   NS_ADDREF(*aResult = result);
   return NS_OK;
 }
