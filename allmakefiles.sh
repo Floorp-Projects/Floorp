94b12e47bedf5ed214e998fe9991dd4171ace828
Matt Brubeck <mbrubeck@mozilla.com>
2011-09-30 20:43:13 -0700

    Back out cb715f8a1363 (bug 680246) because of Win opt build failures on a CLOSED TREE

--- allmakefiles.sh
+++ allmakefiles.sh
@@ -62,7 +62,6 @@ build/pgo/Makefile
 build/pgo/blueprint/Makefile
 build/pgo/js-input/Makefile
 build/unix/Makefile
-build/unix/test/Makefile
 build/win32/Makefile
 build/win32/crashinjectdll/Makefile
 config/Makefile
