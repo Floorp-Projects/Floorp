71b089a1b1cd1401f129597aaffb66135307c499
Philipp von Weitershausen <philipp@weitershausen.de>
2011-10-02 01:16:01 -0700

    Bug 690616 - Part 2: Implement (for now empty) onPairingStart methods in UI. r=rnewman

--- browser/base/content/syncSetup.js
+++ browser/base/content/syncSetup.js
@@ -611,6 +611,8 @@ var gSyncSetup = {
         document.getElementById("easySetupPIN3").value = pin.slice(8);
       },
 
+      onPairingStart: function onPairingStart() {},
+
       onComplete: function onComplete(credentials) {
         Weave.Service.account = credentials.account;
         Weave.Service.password = credentials.password;
