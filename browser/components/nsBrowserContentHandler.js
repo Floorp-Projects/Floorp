b79ecdff07f51821421619a6e58c07d4fc9cd7bf
Margaret Leibovic <margaret.leibovic@gmail.com>
2011-10-03 16:28:13 -0700

    Bug 691579 - Get rid of broken multiple homepage code in startPage() r=gavin

--- browser/components/nsBrowserContentHandler.js
+++ browser/components/nsBrowserContentHandler.js
@@ -648,36 +648,13 @@ nsBrowserContentHandler.prototype = {
   },
 
   get startPage() {
-    var prefb = Components.classes["@mozilla.org/preferences-service;1"]
-                          .getService(nsIPrefBranch);
-
-    var uri = prefb.getComplexValue("browser.startup.homepage",
-                                    nsIPrefLocalizedString).data;
-
+    var uri = Services.prefs.getComplexValue("browser.startup.homepage",
+                                             nsIPrefLocalizedString).data;
     if (!uri) {
-      prefb.clearUserPref("browser.startup.homepage");
-      uri = prefb.getComplexValue("browser.startup.homepage",
-                                  nsIPrefLocalizedString).data;
+      Services.prefs.clearUserPref("browser.startup.homepage");
+      uri = Services.prefs.getComplexValue("browser.startup.homepage",
+                                           nsIPrefLocalizedString).data;
     }
-                                
-    var count;
-    try {
-      count = prefb.getIntPref("browser.startup.homepage.count");
-    }
-    catch (e) {
-      return uri;
-    }
-
-    for (var i = 1; i < count; ++i) {
-      try {
-        var page = prefb.getComplexValue("browser.startup.homepage." + i,
-                                         nsIPrefLocalizedString).data;
-        uri += "\n" + page;
-      }
-      catch (e) {
-      }
-    }
-
     return uri;
   },
 
