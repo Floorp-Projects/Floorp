9970f234fc78d3084bf1fce921f700f90b11150b
Masayuki Nakano <masayuki@d-toybox.com>
2011-10-03 16:56:21 +0900

    Bug 690700 Remove PR_TRUE/PR_FALSE from widget/src/gtk2 r=karlt

--- widget/src/gtk2/nsPaperPS.cpp
+++ widget/src/gtk2/nsPaperPS.cpp
@@ -47,13 +47,13 @@ const nsPaperSizePS_ nsPaperSizePS::mList[] =
 {
 #define SIZE_MM(x)      (x)
 #define SIZE_INCH(x)    ((x) * MM_PER_INCH_FLOAT)
-    { "A5",             SIZE_MM(148),   SIZE_MM(210),   PR_TRUE },
-    { "A4",             SIZE_MM(210),   SIZE_MM(297),   PR_TRUE },
-    { "A3",             SIZE_MM(297),   SIZE_MM(420),   PR_TRUE },
-    { "Letter",         SIZE_INCH(8.5), SIZE_INCH(11),  PR_FALSE },
-    { "Legal",          SIZE_INCH(8.5), SIZE_INCH(14),  PR_FALSE },
-    { "Tabloid",        SIZE_INCH(11),  SIZE_INCH(17),  PR_FALSE },
-    { "Executive",      SIZE_INCH(7.5), SIZE_INCH(10),  PR_FALSE },
+    { "A5",             SIZE_MM(148),   SIZE_MM(210),   true },
+    { "A4",             SIZE_MM(210),   SIZE_MM(297),   true },
+    { "A3",             SIZE_MM(297),   SIZE_MM(420),   true },
+    { "Letter",         SIZE_INCH(8.5), SIZE_INCH(11),  false },
+    { "Legal",          SIZE_INCH(8.5), SIZE_INCH(14),  false },
+    { "Tabloid",        SIZE_INCH(11),  SIZE_INCH(17),  false },
+    { "Executive",      SIZE_INCH(7.5), SIZE_INCH(10),  false },
 #undef SIZE_INCH
 #undef SIZE_MM
 };
@@ -66,8 +66,8 @@ nsPaperSizePS::Find(const char *aName)
     for (int i = mCount; i--; ) {
         if (!PL_strcasecmp(aName, mList[i].name)) {
             mCurrent = i;
-            return PR_TRUE;
+            return true;
         }
     }
-    return PR_FALSE;
+    return false;
 }
