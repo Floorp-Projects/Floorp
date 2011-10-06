7e0de657fba8598f14de132d01a674170a269bee
Brian Hackett <bhackett1024@gmail.com>
2011-10-06 11:53:11 -0700

    Don't bypass watchpoint handlers when optimizing property accesses on singletons, bug 690292.

--- js/src/jsinfer.cpp
+++ js/src/jsinfer.cpp
@@ -2724,6 +2724,14 @@ TypeObject::addProperty(JSContext *cx, jsid id, Property **pprop)
             if (shape)
                 UpdatePropertyType(cx, &base->types, singleton, shape, false);
         }
+
+        if (singleton->watched()) {
+            /*
+             * Mark the property as configured, to inhibit optimizations on it
+             * and avoid bypassing the watchpoint handler.
+             */
+            base->types.setOwnProperty(cx, true);
+        }
     }
 
     *pprop = base;
