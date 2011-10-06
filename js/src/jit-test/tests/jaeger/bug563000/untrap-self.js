d19716569c9fa42421deeb02c132efe5cb18b916
David Anderson <danderson@mozilla.com>
2011-10-05 23:40:00 -0700

    Fix bogus offset in test.

--- js/src/jit-test/tests/jaeger/bug563000/untrap-self.js
+++ js/src/jit-test/tests/jaeger/bug563000/untrap-self.js
@@ -3,12 +3,13 @@ setDebug(true);
 x = "notset";
 function main() {
   /* JSOP_STOP in main. */
-  untrap(main, 22);
+  untrap(main, 24);
   x = "success";
 }
+dis(main);
 function failure() { x = "failure"; }
 
 /* JSOP_STOP in main. */
-trap(main, 22, "failure()");
+trap(main, 24, "failure()");
 main();
 assertEq(x, "success");
