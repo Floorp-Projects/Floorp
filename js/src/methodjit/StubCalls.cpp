8af2b8a1f3bdd2e58998fd1a523315e6963f2587
Jim Blandy <jimb@mozilla.com>
2011-10-07 11:46:17 -0700

    (no bug): Fix Emacs/vi mode line at top of js/src/methodjit/StubCalls.cpp. r=only comments changed

--- js/src/methodjit/StubCalls.cpp
+++ js/src/methodjit/StubCalls.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C++ tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  * vim: set ts=4 sw=4 et tw=99:
  *
  * ***** BEGIN LICENSE BLOCK *****
