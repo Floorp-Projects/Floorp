b565dbfea6ad18e0a78a8d6c46defebe3b75c21d
Tim Taubert <tim.taubert@gmx.de>
2011-10-08 17:19:32 +0200

    Bug 657472 - 'Tune' the time to wait before displaying the update been downloaded / restart notification and provide ability to override in the update xml; r=rstrong

--- browser/branding/nightly/pref/firefox-branding.js
+++ browser/branding/nightly/pref/firefox-branding.js
@@ -5,6 +5,8 @@ pref("app.update.interval", 7200); // 2 hours
 // The time interval between the downloading of mar file chunks in the
 // background (in seconds)
 pref("app.update.download.backgroundInterval", 60);
+// Give the user x seconds to react before showing the big UI. default=12 hours
+pref("app.update.promptWaitTime", 43200);
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
 pref("app.update.url.manual", "http://nightly.mozilla.org/");
