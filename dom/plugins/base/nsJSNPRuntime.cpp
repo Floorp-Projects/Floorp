55c600efc0af500f32b6fbaf5943e9dfe518be66
Brian Hackett <bhackett1024@gmail.com>
2011-10-12 09:48:46 -0700

    Fix use of getPrivate() on function object, bug 693754.

--- dom/plugins/base/nsJSNPRuntime.cpp
+++ dom/plugins/base/nsJSNPRuntime.cpp
@@ -1483,7 +1483,7 @@ CallNPMethodInternal(JSContext *cx, JSObject *obj, uintN argc, jsval *argv,
     // the function object.
 
     if (npobj->_class->invoke) {
-      JSFunction *fun = (JSFunction *)::JS_GetPrivate(cx, funobj);
+      JSFunction *fun = ::JS_GetObjectFunction(funobj);
       JSString *name = ::JS_InternJSString(cx, ::JS_GetFunctionId(fun));
       NPIdentifier id = StringToNPIdentifier(cx, name);
 
