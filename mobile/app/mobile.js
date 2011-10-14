593f6f6db107736e23c7a94c3626f7fed22a7a5e
Doug Turner <dougt@dougt.org>
2011-10-13 21:13:01 -0700

    Typo

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -389,7 +389,7 @@ pref("geo.enabled", true);
 // see https://bugzilla.mozilla.org/show_bug.cgi?id=481566#c9
 //pref("content.sink.enable_perf_mode",  2); // 0 - switch, 1 - interactive, 2 - perf
 //pref("content.sink.pending_event_mode", 0);
-/pref("content.sink.perf_deflect_count", 1000000);
+//pref("content.sink.perf_deflect_count", 1000000);
 //pref("content.sink.perf_parse_time", 50000000);
 
 // Disable methodjit in chrome to save memory
