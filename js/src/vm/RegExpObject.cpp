463cc1ab63cd9699fb758f9b2e9cdc835cd36ae7
Chris Leary <cdleary@mozilla.com>
2011-10-14 10:44:13 -0700

    Bug 694527: Force RegExpPrivate creation for clone source. (r=bhackett)

--- js/src/vm/RegExpObject.cpp
+++ js/src/vm/RegExpObject.cpp
@@ -407,16 +407,14 @@ RegExpObject::clone(JSContext *cx, RegExpObject *reobj, RegExpObject *proto)
         return reclone->reset(cx, reobj->getSource(), newFlags) ? reclone : NULL;
     }
 
-    RegExpPrivate *toShare = reobj->getPrivate();
-    if (toShare) {
-        toShare->incref(cx);
-        if (!reclone->reset(cx, AlreadyIncRefed<RegExpPrivate>(toShare))) {
-            toShare->decref(cx);
-            return NULL;
-        }
-    } else {
-        if (!reclone->reset(cx, reobj))
-            return NULL;
+    RegExpPrivate *toShare = reobj->getOrCreatePrivate(cx);
+    if (!toShare)
+        return NULL;
+
+    toShare->incref(cx);
+    if (!reclone->reset(cx, AlreadyIncRefed<RegExpPrivate>(toShare))) {
+        toShare->decref(cx);
+        return NULL;
     }
 
     return reclone;
