2661c1a9c69ecb5106f078ac986c970404ea2232
Jeff Walden <jwalden@mit.edu>
2011-10-17 14:33:32 -0700

    Bug 693100 - Fix "warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]" with gcc 4.6.1 in SpiderMonkey.  r=dvander

--- js/src/methodjit/FastBuiltins.cpp
+++ js/src/methodjit/FastBuiltins.cpp
@@ -607,7 +607,8 @@ mjit::Compiler::compileArrayWithArgs(uint32 argc)
         frame.storeTo(arg, Address(result, JSObject::getFixedSlotOffset(i)), /* popped = */ true);
     }
 
-    masm.storePtr(ImmPtr((void *) argc), Address(result, offsetof(JSObject, initializedLength)));
+    masm.storePtr(ImmIntPtr(intptr_t(argc)),
+                  Address(result, offsetof(JSObject, initializedLength)));
 
     stubcc.leave();
 
