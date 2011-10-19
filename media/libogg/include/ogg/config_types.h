ae0d7c5e40da9865581ace76bab2c8056008d17c
Matthew Gregan <kinetik@flim.org>
2011-10-20 11:18:51 +1300

    Bug 695240 - ogg_int64_t should be 64-bit everywhere.  r=derf r=rillian

--- media/libogg/include/ogg/config_types.h
+++ media/libogg/include/ogg/config_types.h
@@ -20,6 +20,6 @@ typedef short ogg_int16_t;
 typedef unsigned short ogg_uint16_t;
 typedef int ogg_int32_t;
 typedef unsigned int ogg_uint32_t;
-typedef long ogg_int64_t;
+typedef long long ogg_int64_t;
 
 #endif
