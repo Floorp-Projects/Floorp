ed115ced0bd2722abb887d11157e418fab90982a
Jeff Muizelaar <jmuizelaar@mozilla.com>
2011-10-19 18:06:14 -0400

    Bug 695859. Don't progressively display images during decode if we have all of the data. r=bholley
    This should reduce the amount of image repainting that happens during download.

--- image/src/RasterImage.cpp
+++ image/src/RasterImage.cpp
@@ -2839,10 +2839,10 @@ imgDecodeWorker::Run()
   mDecodeTime += decodeLatency;
 
   // Flush invalidations _after_ we've written everything we're going to.
-  // Furthermore, if this is a redecode, we don't want to do progressive
+  // Furthermore, if we have all of the data, we don't want to do progressive
   // display at all. In that case, let Decoder::PostFrameStop() do the
   // flush once the whole frame is ready.
-  if (!image->mHasBeenDecoded) {
+  if (!image->mHasSourceData) {
     image->mInDecoder = true;
     image->mDecoder->FlushInvalidations();
     image->mInDecoder = false;
