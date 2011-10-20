49f36cb7a50516d30555c932dbb3966ed8873ee5
Andrew McCreight <amccreight@mozilla.com>
2011-10-20 09:25:31 -0700

    Bug 680482, part 1 - add new js::DumpHeapComplete to dump the entire heap. r=billm

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -151,6 +151,16 @@ JS_END_EXTERN_C
 
 namespace js {
 
+#ifdef DEBUG
+ /*
+  * DEBUG-only method to dump the complete object graph of heap-allocated things.
+  * fp is the file for the dump output.
+  */
+extern JS_FRIEND_API(void)
+DumpHeapComplete(JSContext *cx, FILE *fp);
+
+#endif
+
 class JS_FRIEND_API(AutoPreserveCompartment) {
   private:
     JSContext *cx;
