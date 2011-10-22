8b25aa7ee0d3850a8d55dc46f29d62f14bdf129b
Marco Bonardo <mbonardo@mozilla.com>
2011-10-22 09:32:54 +0200

    Backout 73512caf4647 (bug 692260) for Ts regression.

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -839,17 +839,9 @@ nsHttpHandler::PrefsChanged(nsIPrefBranch *prefs, const char *pref)
         if (NS_SUCCEEDED(rv)) {
             PR_CallOnce(&nsSocketTransportService::gMaxCountInitOnce,
                         nsSocketTransportService::DiscoverMaxCount);
-            // Don't use the full max count because sockets can be held in
-            // the persistent connection pool for a long time and that could
-            // starve other users.
-
-            PRUint32 maxCount = nsSocketTransportService::gMaxCount;
-            if (maxCount <= 8)
-                maxCount = 1;
-            else
-                maxCount -= 8;
-            mMaxConnections = (PRUint16) NS_CLAMP((PRUint32)val, 1, maxCount);
-
+            mMaxConnections =
+                (PRUint16) NS_CLAMP((PRUint32)val, 1,
+                                    nsSocketTransportService::gMaxCount);
             if (mConnMgr)
                 mConnMgr->UpdateParam(nsHttpConnectionMgr::MAX_CONNECTIONS,
                                       mMaxConnections);
