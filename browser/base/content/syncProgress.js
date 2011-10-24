bcfc0980269947f07cde2ac99790828a60b747a7
Allison Naaktgeboren <ally@mozilla.com>
2011-10-24 12:24:00 -0700

    Bug 696823 - follow-up: adjust progress bar max to account for client engine sync notification. r=philikon

--- browser/base/content/syncProgress.js
+++ browser/base/content/syncProgress.js
@@ -44,12 +44,14 @@ let gProgressBar;
 let gCounter = 0;
 
 function onLoad(event) {
-  Services.obs.addObserver(increaseProgressBar, "weave:engine:sync:finish", false);
-  Services.obs.addObserver(increaseProgressBar, "weave:engine:sync:error", false);
+  Services.obs.addObserver(onEngineSync, "weave:engine:sync:finish", false);
+  Services.obs.addObserver(onEngineSync, "weave:engine:sync:error", false);
+  Services.obs.addObserver(onServiceSync, "weave:service:sync:finish", false);
+  Services.obs.addObserver(onServiceSync, "weave:service:sync:error", false);
+
   gProgressBar = document.getElementById('uploadProgressBar');
 
   if (Services.prefs.getPrefType("services.sync.firstSync") != Ci.nsIPrefBranch.PREF_INVALID) {
-    gProgressBar.max = Weave.Engines.getEnabled().length;
     gProgressBar.style.display = "inline";
   }
   else {
@@ -58,13 +60,44 @@ function onLoad(event) {
 }
 
 function onUnload(event) {
-  Services.obs.removeObserver(increaseProgressBar, "weave:engine:sync:finish");
-  Services.obs.removeObserver(increaseProgressBar, "weave:engine:sync:error");
+  cleanUpObservers();
 }
 
-function increaseProgressBar(){
+function cleanUpObservers() {
+  try {
+    Services.obs.removeObserver(onEngineSync, "weave:engine:sync:finish", false);
+    Services.obs.removeObserver(onEngineSync, "weave:engine:sync:error", false);
+    Services.obs.removeObserver(onServiceSync, "weave:service:sync:finish", false);
+    Services.obs.removeObserver(onServiceSync, "weave:service:sync:error", false);
+  }
+  catch (e) {
+    // may be double called by unload & exit. Ignore.
+  }
+}
+
+function onEngineSync(subject, topic, data) {
+  // The Clients engine syncs first. At this point we don't necessarily know
+  // yet how many engines will be enabled, so we'll ignore the Clients engine
+  // and evaluate how many engines are enabled when the first "real" engine
+  // syncs.
+  if (data == "clients") {
+    return;
+  }
+
+  if (!gCounter &&
+      Services.prefs.getPrefType("services.sync.firstSync") != Ci.nsIPrefBranch.PREF_INVALID) {
+    gProgressBar.max = Weave.Engines.getEnabled().length;
+  }
+
   gCounter += 1;
   gProgressBar.setAttribute("value", gCounter);
+}
+
+function onServiceSync(subject, topic, data) {
+  // To address the case where 0 engines are synced, we will fill the
+  // progress bar so the user knows that the sync has finished.
+  gProgressBar.setAttribute("value", gProgressBar.max);
+  cleanUpObservers();
 }
 
 function closeTab() {
