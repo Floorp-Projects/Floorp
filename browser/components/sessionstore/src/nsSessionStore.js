13e42802963915cf865736b37cf9b7b5db5aa905
Paul O’Shannessy <paul@oshannessy.com>
2011-10-25 10:19:29 -0700

    Bug 694378 - session restore fails when selectedWindow > number of windows [r=dietrich]

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -2577,7 +2577,7 @@ SessionStoreService.prototype = {
       this._closedWindows = root._closedWindows;
 
     var winData;
-    if (!root.selectedWindow) {
+    if (!root.selectedWindow || root.selectedWindow > root.windows.length) {
       root.selectedWindow = 0;
     } else {
       // put the selected window at the beginning of the array to ensure that
