11ea4e251646cdfe7e9d510ba5cee37d844e931a
Landry Breuil <landry@openbsd.org>
2011-10-25 08:48:39 -0400

    Bug 687320 - On OpenBSD, libGL.so.1 doesn't exist - r=bjacob
    Opening libGL.so will work regardless of the version, which changes over time.

--- toolkit/xre/glxtest.cpp
+++ toolkit/xre/glxtest.cpp
@@ -111,7 +111,11 @@ x_error_handler(Display *, XErrorEvent *ev)
 static void glxtest()
 {
   ///// Open libGL and load needed symbols /////
+#ifdef __OpenBSD__
+  void *libgl = dlopen("libGL.so", RTLD_LAZY);
+#else
   void *libgl = dlopen("libGL.so.1", RTLD_LAZY);
+#endif
   if (!libgl)
     fatal_error("Unable to load libGL.so.1");
   
