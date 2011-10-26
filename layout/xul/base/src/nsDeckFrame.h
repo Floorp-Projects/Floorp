18d865bedc3b14f24ffa639677ed7ecf357e7d63
Timothy Nikkel <tnikkel@gmail.com>
2011-10-26 18:57:55 -0500

    Bug 658005. Part 1. Allowing query frame to work on deck frames. r=roc

--- layout/xul/base/src/nsDeckFrame.h
+++ layout/xul/base/src/nsDeckFrame.h
@@ -51,6 +51,8 @@
 class nsDeckFrame : public nsBoxFrame
 {
 public:
+  NS_DECL_QUERYFRAME_TARGET(nsDeckFrame)
+  NS_DECL_QUERYFRAME
   NS_DECL_FRAMEARENA_HELPERS
 
   friend nsIFrame* NS_NewDeckFrame(nsIPresShell* aPresShell,
