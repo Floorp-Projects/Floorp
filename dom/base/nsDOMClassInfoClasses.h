942467690c712db3bc35860508c53dfdb3f36c12
Doug Sherk <dsherk@mozilla.com>
2011-10-26 16:00:44 -0400

    Bug 656824: Implemented ARB_robustness to detect driver resets in WebGL, only on GLX for now - r=bjacob
    This patch implements ARB_robustness on GLX and allows scripts to handle driver resets
    through events. The changes in this patch are very sweeping; they hit almost
    every NS_IMETHODIMP function within WebGLContextGL.cpp and WebGLContext.cpp.
    More work must be done on this to support EGL_CONTEXT_LOST.

--- dom/base/nsDOMClassInfoClasses.h
+++ dom/base/nsDOMClassInfoClasses.h
@@ -472,6 +472,7 @@ DOMCI_CLASS(WebGLUniformLocation)
 DOMCI_CLASS(WebGLActiveInfo)
 DOMCI_CLASS(WebGLExtension)
 DOMCI_CLASS(WebGLExtensionStandardDerivatives)
+DOMCI_CLASS(WebGLExtensionLoseContext)
 
 DOMCI_CLASS(PaintRequest)
 DOMCI_CLASS(PaintRequestList)
