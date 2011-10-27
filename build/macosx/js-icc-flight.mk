d50971a20fb40ea62496476ae53e37693c9f3fe8
Gavin Sharp <gavin@gavinsharp.com>
2011-10-27 10:27:56 -0700

    Bug 696436: consolidate Mac bundle name selection, and allow adding a prefix to it at build time, r=dolske, r=ted relanding with a clobber on a CLOSED TREE

--- build/macosx/js-icc-flight.mk
+++ build/macosx/js-icc-flight.mk
@@ -4,13 +4,7 @@ OBJDIR_X86 = $(MOZ_OBJDIR)/i386
 include $(OBJDIR_X86)/config/autoconf.mk
 include $(TOPSRCDIR)/config/config.mk
 
-ifdef MOZ_DEBUG
-DBGTAG = Debug
-else
-DBGTAG =
-endif
-
 # Just copy the icc-produced libmozjs.dylib over
 postflight_all:
 	echo "Postflight: copying libmozjs.dylib..."
-	$(INSTALL) $(OBJDIR_ICC)/dist/bin/libmozjs.dylib $(OBJDIR_X86)/dist/$(MOZ_APP_DISPLAYNAME)$(DBGTAG).app/Contents/MacOS/
+	$(INSTALL) $(OBJDIR_ICC)/dist/bin/libmozjs.dylib $(OBJDIR_X86)/dist/$(MOZ_MACBUNDLE_NAME)/Contents/MacOS/
