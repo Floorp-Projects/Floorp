aee2f61f14a2f8bfaa829246af55f4be7e486e8c
Mike Hommey <mh+mozilla@glandium.org>
2011-10-27 14:53:20 +0200

    Bug 686464 - Leave it to Android to unpack the content process executable. r=ted

--- ipc/app/defs.mk
+++ ipc/app/defs.mk
@@ -34,5 +34,13 @@
 #
 # ***** END LICENSE BLOCK *****
 
+ifneq ($(OS_TARGET),Android)
 MOZ_CHILD_PROCESS_NAME := plugin-container$(BIN_SUFFIX)
+else
+# We want to let Android unpack the file at install time, but it only does
+# so if the file is named libsomething.so. The lib/ path is also required
+# because the unpacked file will be under the lib/ subdirectory and will
+# need to be executed from that path.
+MOZ_CHILD_PROCESS_NAME := lib/libplugin-container.so
+endif
 MOZ_CHILD_PROCESS_BUNDLE := plugin-container.app/Contents/MacOS/
