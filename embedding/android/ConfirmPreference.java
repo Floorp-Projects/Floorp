af4810535b8a32a572e1921f8eee5f1b4ec279b3
Mark Finkle <mfinkle@mozilla.com>
2011-10-28 00:55:15 -0400

    Bug 697720 - Add 'Clear private data' preference [r=blassey]

--- embedding/android/ConfirmPreference.java
+++ embedding/android/ConfirmPreference.java
@@ -36,33 +36,42 @@
  * ***** END LICENSE BLOCK ***** */
 
 package org.mozilla.gecko;
-import android.preference.*;
-import android.content.*;
-import android.util.*;
+
+import android.content.Context;
+import android.preference.DialogPreference;
 import android.provider.Browser;
+import android.util.AttributeSet;
+import android.util.Log;
 
 class ConfirmPreference extends DialogPreference {
-    String mAction = null;
-    Context mContext = null;
+    private String mAction = null;
+    private Context mContext = null;
     public ConfirmPreference(Context context, AttributeSet attrs) {
-	super(context, attrs);
-	mAction = attrs.getAttributeValue(null, "action");
-	mContext = context;
+        super(context, attrs);
+        mAction = attrs.getAttributeValue(null, "action");
+        mContext = context;
     }
     public ConfirmPreference(Context context, AttributeSet attrs, int defStyle) {
-	super(context, attrs, defStyle);
-	mAction = attrs.getAttributeValue(null, "action");
-	mContext = context;
+        super(context, attrs, defStyle);
+        mAction = attrs.getAttributeValue(null, "action");
+        mContext = context;
     }
     protected void onDialogClosed(boolean positiveResult) {
-	if (!positiveResult)
-	    return;
-	if ("clear_history".equalsIgnoreCase(mAction)) {
-	    GeckoAppShell.getHandler().post(new Runnable(){
-		    public void run() {
-			Browser.clearHistory(mContext.getContentResolver());
-		    }});
-	}
-	Log.i("GeckoPref", "action: " + mAction);
+        if (!positiveResult)
+            return;
+        if ("clear_history".equalsIgnoreCase(mAction)) {
+            GeckoAppShell.getHandler().post(new Runnable(){
+                public void run() {
+                    Browser.clearHistory(mContext.getContentResolver());
+                }
+            });
+        } else if ("clear_private_data".equalsIgnoreCase(mAction)) {
+            GeckoAppShell.getHandler().post(new Runnable(){
+                public void run() {
+                    GeckoAppShell.sendEventToGecko(new GeckoEvent("Sanitize:ClearAll", null));
+                }
+            });
+        }
+        Log.i("GeckoPref", "action: " + mAction);
     }
 }
