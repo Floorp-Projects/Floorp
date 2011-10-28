2c73da4bcff33b71858d81b05dabaf5be8e873ac
Richard Newman <rnewman@mozilla.com>
2011-10-27 22:25:00 -0700

    Bug 684798 - Part 3: Catch server errors when wiping/disabling engines. r=rnewman (reland)

--- services/sync/modules/engines.js
+++ services/sync/modules/engines.js
@@ -724,7 +724,7 @@ SyncEngine.prototype = {
     // Delete any existing data and reupload on bad version or missing meta.
     // No crypto component here...? We could regenerate per-collection keys...
     if (needsWipe) {
-      this.wipeServer(true);
+      this.wipeServer();
     }
 
     // Save objects that need to be uploaded in this._modified. We also save
@@ -1248,7 +1248,10 @@ SyncEngine.prototype = {
   },
 
   wipeServer: function wipeServer() {
-    new Resource(this.engineURL).delete();
+    let response = new Resource(this.engineURL).delete();
+    if (response.status != 200 && response.status != 404) {
+      throw response;
+    }
     this._resetClient();
   },
 
