e8c4b3139e84dbd1c8bb418e7d39e4fb9cc52b4a
Ms2ger <ms2ger@gmail.com>
2011-10-29 22:14:13 +0200

    Bug 694867 - Part d: Store a JSScript in nsXULPrototypeCache; r=volkmar

--- content/xul/document/src/nsXULPrototypeCache.h
+++ content/xul/document/src/nsXULPrototypeCache.h
@@ -53,6 +53,9 @@
 #include "nsXULPrototypeDocument.h"
 #include "nsIInputStream.h"
 #include "nsIStorageStream.h"
+
+#include "jspubtd.h"
+
 #include "mozilla/scache/StartupCache.h"
 
 using namespace mozilla::scache;
@@ -62,7 +65,7 @@ class nsCSSStyleSheet;
 struct CacheScriptEntry
 {
     PRUint32    mScriptTypeID; // the script language ID.
-    void*       mScriptObject; // the script object.
+    JSScript*   mScriptObject; // the script object.
 };
 
 /**
@@ -105,7 +108,7 @@ public:
     nsresult PutPrototype(nsXULPrototypeDocument* aDocument);
 
     JSScript* GetScript(nsIURI* aURI, PRUint32* langID);
-    nsresult PutScript(nsIURI* aURI, PRUint32 langID, void* aScriptObject);
+    nsresult PutScript(nsIURI* aURI, PRUint32 langID, JSScript* aScriptObject);
 
     nsXBLDocumentInfo* GetXBLDocumentInfo(nsIURI* aURL) {
         return mXBLDocTable.GetWeak(aURL);
