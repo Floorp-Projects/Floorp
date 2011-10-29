47c0598145546ab26b839cc6a564d6e02dee2483
Brad Lassey <blassey@mozilla.com>
2011-10-28 23:00:57 -0400

    bug 697986 - Ads show up in history r=dougt

--- toolkit/components/places/nsAndroidHistory.cpp
+++ toolkit/components/places/nsAndroidHistory.cpp
@@ -118,6 +118,9 @@ nsAndroidHistory::VisitURI(nsIURI *aURI, nsIURI *aLastVisitedURI, PRUint32 aFlag
   if (!aURI)
     return NS_OK;
 
+  if (!(aFlags & VisitFlags::TOP_LEVEL))
+    return NS_OK;
+
   AndroidBridge *bridge = AndroidBridge::Bridge();
   if (bridge) {
     nsCAutoString uri;
