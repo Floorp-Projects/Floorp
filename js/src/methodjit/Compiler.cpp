1dc93867a2cfe1d21b6310cd4f9c5df6a2014f83
Brian Hackett <bhackett1024@gmail.com>
2011-10-29 17:30:08 -0700

    Remove defined variables analysis, bug 697861. r=dvander

--- js/src/methodjit/Compiler.cpp
+++ js/src/methodjit/Compiler.cpp
@@ -746,16 +746,7 @@ mjit::Compiler::generatePrologue()
             stubcc.crossJump(stubcc.masm.jump(), masm.label());
         }
 
-        /*
-         * Set locals to undefined, as in initCallFrameLatePrologue.
-         * Skip locals which aren't closed and are known to be defined before used,
-         */
-        for (uint32 i = 0; i < script->nfixed; i++) {
-            if (analysis->localHasUseBeforeDef(i)) {
-                Address local(JSFrameReg, sizeof(StackFrame) + i * sizeof(Value));
-                masm.storeValue(UndefinedValue(), local);
-            }
-        }
+        markUndefinedLocals();
 
         types::TypeScriptNesting *nesting = script->nesting();
 
@@ -875,6 +866,28 @@ mjit::Compiler::ensureDoubleArguments()
         uint32 slot = ArgSlot(i);
         if (a->varTypes[slot].type == JSVAL_TYPE_DOUBLE && analysis->trackSlot(slot))
             frame.ensureDouble(frame.getArg(i));
+    }
+}
+
+void
+mjit::Compiler::markUndefinedLocals()
+{
+    uint32 depth = ssa.getFrame(a->inlineIndex).depth;
+
+    /*
+     * Set locals to undefined, as in initCallFrameLatePrologue.
+     * Skip locals which aren't closed and are known to be defined before used,
+     */
+    for (uint32 i = 0; i < script->nfixed; i++) {
+        uint32 slot = LocalSlot(script, i);
+        Address local(JSFrameReg, sizeof(StackFrame) + (depth + i) * sizeof(Value));
+        if (!cx->typeInferenceEnabled() || !analysis->trackSlot(slot)) {
+            masm.storeValue(UndefinedValue(), local);
+        } else {
+            Lifetime *lifetime = analysis->liveness(slot).live(0);
+            if (lifetime)
+                masm.storeValue(UndefinedValue(), local);
+        }
     }
 }
 
@@ -3971,6 +3984,8 @@ mjit::Compiler::inlineScriptedFunction(uint32 argc, bool callingNew)
          * argument inferred as double but we are passing an int.
          */
         ensureDoubleArguments();
+
+        markUndefinedLocals();
 
         status = generateMethod();
         if (status != Compile_Okay) {
