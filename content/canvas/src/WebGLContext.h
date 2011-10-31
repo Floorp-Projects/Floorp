34ff2cc95c86abfb8ba9fca38675542e23b58142
Jeff Gilbert <jgilbert@mozilla.com>
2011-10-31 16:55:01 -0700

    Bug 697560 - Generate WebGL errors in readPixels according to spec - r=bjacob * * * Bug 697560 - Disallow conformance failures on read-pixels-test - r=bjacob

--- content/canvas/src/WebGLContext.h
+++ content/canvas/src/WebGLContext.h
@@ -580,7 +580,7 @@ protected:
                                 int jsArrayType,
                                 int srcFormat, bool srcPremultiplied);
     nsresult ReadPixels_base(WebGLint x, WebGLint y, WebGLsizei width, WebGLsizei height,
-                             WebGLenum format, WebGLenum type, void *data, PRUint32 byteLength);
+                             WebGLenum format, WebGLenum type, JSObject* pixels);
     nsresult TexParameter_base(WebGLenum target, WebGLenum pname,
                                WebGLint *intParamPtr, WebGLfloat *floatParamPtr);
 
