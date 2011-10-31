7a684ca1bec525cffbed4eb0b36597fa83f5a306
Benoit Jacob <bjacob@mozilla.com>
2011-10-31 17:14:12 -0400

    Bug 677543 - Not clearing WebGL framebuffer in teapot demo - r=jgilbert
    We are supposed to clear the WebGL framebuffer after it's been presented to the compositor and before the next draw operation, and were using the wrong way to know when we got composited. The right way is DidTransactionCallback.

--- content/canvas/src/WebGLContext.cpp
+++ content/canvas/src/WebGLContext.cpp
@@ -811,18 +811,31 @@ WebGLContext::GetThebesSurface(gfxASurface **surface)
 
 static PRUint8 gWebGLLayerUserData;
 
+namespace mozilla {
+
 class WebGLContextUserData : public LayerUserData {
 public:
     WebGLContextUserData(nsHTMLCanvasElement *aContent)
     : mContent(aContent) {}
+
+  /** DidTransactionCallback gets called by the Layers code everytime the WebGL canvas gets composite,
+    * so it really is the right place to put actions that have to be performed upon compositing
+    */
   static void DidTransactionCallback(void* aData)
   {
-    static_cast<WebGLContextUserData*>(aData)->mContent->MarkContextClean();
+    WebGLContextUserData *userdata = static_cast<WebGLContextUserData*>(aData);
+    nsHTMLCanvasElement *canvas = userdata->mContent;
+    WebGLContext *context = static_cast<WebGLContext*>(canvas->GetContextAtIndex(0));
+
+    context->mBackbufferClearingStatus = BackbufferClearingStatus::NotClearedSinceLastPresented;
+    canvas->MarkContextClean();
   }
 
 private:
   nsRefPtr<nsHTMLCanvasElement> mContent;
 };
+
+} // end namespace mozilla
 
 already_AddRefed<layers::CanvasLayer>
 WebGLContext::GetCanvasLayer(nsDisplayListBuilder* aBuilder,
@@ -886,8 +899,6 @@ WebGLContext::GetCanvasLayer(nsDisplayListBuilder* aBuilder,
     canvasLayer->Updated();
 
     mResetLayer = false;
-
-    mBackbufferClearingStatus = BackbufferClearingStatus::NotClearedSinceLastPresented;
 
     return canvasLayer.forget().get();
 }
