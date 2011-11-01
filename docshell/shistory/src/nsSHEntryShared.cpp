5144bf31998b416c984494ea1a5fa7b99ce1fc9d
Justin Lebar <justin.lebar@gmail.com>
2011-10-31 22:33:24 -0400

    Bug 698656 - mParent pointer should be on nsSHEntry, not nsSHEntryShared. r=bz

--- docshell/shistory/src/nsSHEntryShared.cpp
+++ docshell/shistory/src/nsSHEntryShared.cpp
@@ -93,7 +93,6 @@ nsSHEntryShared::Shutdown()
 
 nsSHEntryShared::nsSHEntryShared()
   : mDocShellID(0)
-  , mParent(nsnull)
   , mIsFrameNavigation(false)
   , mSaveLayoutState(true)
   , mSticky(true)
@@ -136,7 +135,6 @@ nsSHEntryShared::Duplicate(nsSHEntryShared *aEntry)
   newEntry->mDocShellID = aEntry->mDocShellID;
   newEntry->mChildShells.AppendObjects(aEntry->mChildShells);
   newEntry->mOwner = aEntry->mOwner;
-  newEntry->mParent = aEntry->mParent;
   newEntry->mContentType.Assign(aEntry->mContentType);
   newEntry->mIsFrameNavigation = aEntry->mIsFrameNavigation;
   newEntry->mSaveLayoutState = aEntry->mSaveLayoutState;
