505e9f3919a13a32e0915f1925770623ecffa690
Mounir Lamouri <mounir.lamouri@gmail.com>
2011-11-02 16:35:11 +0100

    Bug 696038 - (2/3) Boilerplate code to communicate between hal and Android Java code. r=cjones

--- embedding/android/GeckoBatteryManager.java
+++ embedding/android/GeckoBatteryManager.java
@@ -1,5 +1,5 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* ***** BEGIN LICENSE BLOCK *****
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
  * The contents of this file are subject to the Mozilla Public License Version
@@ -12,9 +12,9 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is mozilla.org code.
+ * The Original Code is Mozilla Android code.
  *
- * The Initial Developer of the Original Code is Mozilla Foundation
+ * The Initial Developer of the Original Code is Mozilla Foundation.
  * Portions created by the Initial Developer are Copyright (C) 2011
  * the Initial Developer. All Rights Reserved.
  *
@@ -22,8 +22,8 @@
  *   Mounir Lamouri <mounir.lamouri@mozilla.com> (Original Author)
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
@@ -35,49 +35,17 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include "Hal.h"
-#include "AndroidBridge.h"
+package org.mozilla.gecko;
 
-namespace mozilla {
-namespace hal_impl {
-
-void
-Vibrate(const nsTArray<uint32>& pattern)
-{}
-
-void
-EnableBatteryNotifications()
+public class GeckoBatteryManager
 {
-  AndroidBridge* bridge = AndroidBridge::Bridge();
-  if (!bridge) {
-    return;
+  public static void enableNotifications() {
   }
 
-  bridge->EnableBatteryNotifications();
-}
-
-void
-DisableBatteryNotifications()
-{
-  AndroidBridge* bridge = AndroidBridge::Bridge();
-  if (!bridge) {
-    return;
+  public static void disableNotifications() {
   }
 
-  bridge->DisableBatteryNotifications();
-}
-
-void
-GetCurrentBatteryInformation(hal::BatteryInformation* aBatteryInfo)
-{
-  AndroidBridge* bridge = AndroidBridge::Bridge();
-  if (!bridge) {
-    return;
+  public static float[] getCurrentInformation() {
+    return new float[] { 1.0f, 1.0f };
   }
-
-  bridge->GetCurrentBatteryInformation(aBatteryInfo);
 }
-
-} // hal_impl
-} // mozilla
-
