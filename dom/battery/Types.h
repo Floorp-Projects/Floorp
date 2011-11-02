67f913dca30644fd7ff8843a767fe9c59692b127
Mounir Lamouri <mounir.lamouri@gmail.com>
2011-11-02 16:14:01 +0100

    Bug 678694 - (6/7) hal code for the Battery API. r=cjones

--- dom/battery/Types.h
+++ dom/battery/Types.h
@@ -1,11 +1,10 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set sw=2 ts=8 et ft=cpp : */
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
  * The contents of this file are subject to the Mozilla Public License Version
  * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at:
+ * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
  *
  * Software distributed under the License is distributed on an "AS IS" basis,
@@ -13,19 +12,18 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is Mozilla Code.
+ * The Original Code is mozilla.org code.
  *
- * The Initial Developer of the Original Code is
- *   The Mozilla Foundation
+ * The Initial Developer of the Original Code is Mozilla Foundation
  * Portions created by the Initial Developer are Copyright (C) 2011
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *   Chris Jones <jones.chris.g@gmail.com>
+ *   Mounir Lamouri <mounir.lamouri@mozilla.com> (Original Author)
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
@@ -37,30 +35,20 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include "Hal.h"
-#include "mozilla/dom/battery/Constants.h"
+#ifndef mozilla_dom_battery_Types_h
+#define mozilla_dom_battery_Types_h
 
 namespace mozilla {
-namespace hal_impl {
+namespace hal {
+class BatteryInformation;
+} // namespace hal
 
-void
-Vibrate(const nsTArray<uint32>& pattern)
-{}
+template <class T>
+class Observer;
 
-void
-EnableBatteryNotifications()
-{}
+typedef Observer<hal::BatteryInformation> BatteryObserver;
 
-void
-DisableBatteryNotifications()
-{}
-
-void
-GetCurrentBatteryInformation(hal::BatteryInformation* aBatteryInfo)
-{
-  aBatteryInfo->level() = dom::battery::kDefaultLevel;
-  aBatteryInfo->charging() = dom::battery::kDefaultCharging;
-}
-
-} // hal_impl
 } // namespace mozilla
+
+#endif // mozilla_dom_battery_Types_h
+
