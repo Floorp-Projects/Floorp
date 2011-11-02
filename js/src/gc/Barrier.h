827946f2236c6aeaae6398477a4ff6ac13080d6d
Nicholas Nethercote <nnethercote@mozilla.com>
2011-11-01 19:16:48 -0700

    Bug 631138 - Update the big comment describing Shapes.  r=bhackett.
    --HG--
    extra : rebase_source : f20c7fb10703a6d5cced7ac53715e240b4367f38

--- js/src/gc/Barrier.h
+++ js/src/gc/Barrier.h
@@ -52,7 +52,7 @@
  * traced through by the GC to change. This includes:
  *   - writes to object properties
  *   - writes to array slots
- *   - writes to fields like JSObject::lastProp that we trace through
+ *   - writes to fields like JSObject::shape_ that we trace through
  *   - writes to fields in private data, like JSGenerator::obj
  *   - writes to non-markable fields like JSObject::private that point to
  *     markable data
