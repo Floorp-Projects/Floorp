aacc1d19e2db0bf2f5276f3cc91ca45c217ef23d
ABE Hiroki <h.rayflood@gmail.com>
2011-11-04 21:26:53 +0000

    Bug 675593 - Fix nspr4!1.pgc path mistake on NSPR PGO build (m-c part); r=ted

--- config/rules.mk
+++ config/rules.mk
@@ -805,11 +805,11 @@ ifneq (,$(SHARED_LIBRARY)$(PROGRAM))
 export::
 ifdef PROGRAM
 	$(PYTHON) $(topsrcdir)/build/win32/pgomerge.py \
-	  $(PROGRAM:$(BIN_SUFFIX)=) $(DIST)/$(MOZ_APP_NAME)
+	  $(PROGRAM:$(BIN_SUFFIX)=) $(DIST)/bin
 endif
 ifdef SHARED_LIBRARY
 	$(PYTHON) $(topsrcdir)/build/win32/pgomerge.py \
-	  $(SHARED_LIBRARY_NAME) $(DIST)/$(MOZ_APP_NAME)
+	  $(SHARED_LIBRARY_NAME) $(DIST)/bin
 endif
 endif # SHARED_LIBRARY || PROGRAM
 endif # WINNT_
