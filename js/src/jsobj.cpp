d5b73381ddad227e18c814d17a53f98a622339d5
Brian Hackett <bhackett1024@gmail.com>
2011-11-09 22:47:26 -0800

    Always set the scope chain of XDR'ed static block objects, no bug.

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -4235,10 +4235,11 @@ js_XDRBlockObject(JSXDRState *xdr, JSObject **objp)
          * object array. We know that we XDR block object in outer-to-inner
          * order, which means that getting the parent now will work.
          */
-        if (parentId != NO_PARENT_INDEX) {
+        if (parentId == NO_PARENT_INDEX)
+            parent = NULL;
+        else
             parent = xdr->script->getObject(parentId);
-            obj->setStaticBlockScopeChain(parent);
-        }
+        obj->setStaticBlockScopeChain(parent);
     }
 
     AutoObjectRooter tvr(cx, obj);
