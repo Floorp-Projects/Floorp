32b9c236ce94b4b60d0b72682773041e68ff2a11
Bill McCloskey <wmccloskey@mozilla.com>
2011-11-10 13:38:05 -0800

    Bug 641027 - Fix warnings-as-errors bustage

--- js/src/jsscopeinlines.h
+++ js/src/jsscopeinlines.h
@@ -80,7 +80,7 @@ js::types::TypeObject::getEmptyShape(JSContext *cx, js::Class *aclasp,
      */
     JS_ASSERT(this == proto->newType);
 
-    JS_ASSERT(kind >= js::gc::FINALIZE_OBJECT0 && kind < js::gc::FINALIZE_OBJECT_LIMIT);
+    JS_ASSERT(kind >= js::gc::FINALIZE_OBJECT0 && kind <= js::gc::FINALIZE_OBJECT_LAST);
     int i = kind - js::gc::FINALIZE_OBJECT0;
 
     if (!emptyShapes) {
