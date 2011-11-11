fa72cab35156208275cc6b1a1fd29ab5ff2204d4
Doug Turner <dougt@dougt.org>
2011-11-10 21:05:02 -0500

    bug 700847 - crash stack found in logcat while running pageloader tests on native fennec r=blassey

--- embedding/android/GeckoAppShell.java
+++ embedding/android/GeckoAppShell.java
@@ -626,14 +626,9 @@ public class GeckoAppShell
             Log.i("GeckoAppJava", "we're done, good bye");
             GeckoApp.mAppContext.finish();
         }
-        getHandler().postDelayed(new Runnable() {
-            // This is just a watch dog to make sure we die
-            // we should never hit this
-            public void run() {
-                Log.w("GeckoAppShell", "Killing via System.exit()");
-                System.exit(0);
-            }
-        }, 5000);
+
+        Log.w("GeckoAppShell", "Killing via System.exit()");
+        System.exit(0);
     }
     static void scheduleRestart() {
         Log.i("GeckoAppJava", "scheduling restart");
