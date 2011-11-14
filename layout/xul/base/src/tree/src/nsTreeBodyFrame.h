2cbf891226a4e1f85c6b164f7957be0133ba0ddb
Scott Johnson <sjohnson@mozilla.com>
2011-11-15 10:59:19 +1300

    Bug 682077 - Remove nsITreeImageListener interface to make inheritance hierarchy more clear. r=neil, sr=bz

--- layout/xul/base/src/tree/src/nsTreeBodyFrame.h
+++ layout/xul/base/src/tree/src/nsTreeBodyFrame.h
@@ -61,7 +61,6 @@
 #include "nsScrollbarFrame.h"
 #include "nsThreadUtils.h"
 #include "mozilla/LookAndFeel.h"
-#include "nsITreeImageListener.h"
 
 class nsOverflowChecker;
 class nsTreeImageListener;
