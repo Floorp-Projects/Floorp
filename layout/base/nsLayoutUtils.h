c346e00fe20baeb70ebd67f36a8ba0c2a260531d
L. David Baron <dbaron@dbaron.org>
2011-11-15 17:02:00 +1300

    Implement computation of font size inflation for improved readibility of text on mobile devices.  (Bug 627842, patch 4)  r=roc
    This implements computation of the font size inflation factor for a
    given frame.  Since Fennec does layout using a fake viewport whose width
    represents a typical viewport width on the desktop and then allows users
    to pan and zoom, fonts are not always readable even when zoomed.  The
    goal of this font size inflation is to ensure that when a block of text
    is zoomed to fill the width of the device, the fonts are large enough to
    read.  We do this by increasing the font sizes in the page.  Since this
    increase is a function of the width of the text's container, the
    inflation must be performed (in later patches in this series) after
    style data computation and after intrinsic width computation.
    
    The font size inflation factor does not vary *within* a block.
    
    Since sync uses a whitelist (the services.sync.prefs.sync.* prefs) for
    preferences (i.e., preferences are not synced by default), this patch
    does not make any changes relating to sync, since we do not want the
    inflation preferences synced across devices (since preferred settings
    are likely to be device-specific).

--- layout/base/nsLayoutUtils.h
+++ layout/base/nsLayoutUtils.h
@@ -1452,6 +1452,59 @@ public:
    */
   static bool Are3DTransformsEnabled();
 
+  /**
+   * Return whether this is a frame whose width is used when computing
+   * the font size inflation of its descendants.
+   */
+  static bool IsContainerForFontSizeInflation(const nsIFrame *aFrame);
+
+  /**
+   * Return the font size inflation *ratio* for a given frame.  This is
+   * the factor by which font sizes should be inflated; it is never
+   * smaller than 1.
+   *
+   * There are three variants: pass a reflow state if the frame or any
+   * of its ancestors are currently being reflowed and a frame
+   * otherwise, or, if you know the width of the inflation container (a
+   * somewhat sketchy assumption), its width.
+   */
+  static float FontSizeInflationFor(const nsHTMLReflowState &aReflowState);
+  static float FontSizeInflationFor(const nsIFrame *aFrame);
+  static float FontSizeInflationFor(const nsIFrame *aFrame,
+                                    nscoord aInflationContainerWidth);
+
+  /**
+   * Perform the first half of the computation of FontSizeInflationFor
+   * (see above).
+   * This includes determining whether inflation should be performed
+   * within this container and returning 0 if it should not be.
+   *
+   * The result is guaranteed not to vary between line participants
+   * (inlines, text frames) within a line.
+   *
+   * The result should not be used directly since font sizes slightly
+   * above the minimum should always be adjusted as done by
+   * FontSizeInflationInner.
+   */
+  static nscoord InflationMinFontSizeFor(const nsHTMLReflowState
+                                                 &aReflowState);
+  static nscoord InflationMinFontSizeFor(const nsIFrame *aFrame);
+  static nscoord InflationMinFontSizeFor(const nsIFrame *aFrame,
+                                         nscoord aInflationContainerWidth);
+
+  /**
+   * Perform the second half of the computation done by
+   * FontSizeInflationFor (see above).
+   *
+   * aMinFontSize must be the result of one of the
+   *   InflationMinFontSizeFor methods above.
+   */
+  static float FontSizeInflationInner(const nsIFrame *aFrame,
+                                      nscoord aMinFontSize);
+
+  static bool FontSizeInflationEnabled(nsPresContext *aPresContext);
+
+  static void Initialize();
   static void Shutdown();
 
   /**
