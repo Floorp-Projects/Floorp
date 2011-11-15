0e71f1847d705d8751624d9eefdd99c2b1aab13b
Cedric Vivier <cedricv@neonux.com>
2011-11-15 16:41:37 +0800

    Bug 583041 - Style Editor integration; part 1; r=rcampbell

--- browser/devtools/scratchpad/test/browser_scratchpad_restore.js
+++ browser/devtools/scratchpad/test/browser_scratchpad_restore.js
@@ -49,9 +49,10 @@ function testRestore()
   asyncMap(states, function(state, done) {
     // Open some scratchpad windows
     let win = ScratchpadManager.openScratchpad(state);
-    win.addEventListener("load", function() {
+    win.addEventListener("load", function onScratchpadLoad() {
+      removeEventListener("load", onScratchpadLoad, false);
       done(win);
-    })
+    }, false)
   }, function(wins) {
     // Then save the windows to session store
     ScratchpadManager.saveOpenWindows();
@@ -73,11 +74,12 @@ function testRestore()
     is(restoredWins.length, 3, "Three scratchad windows restored");
 
     asyncMap(restoredWins, function(restoredWin, done) {
-      restoredWin.addEventListener("load", function() {
+      restoredWin.addEventListener("load", function onScratchpadLoad() {
+        restoredWin.removeEventListener("load", onScratchpadLoad, false);
         let state = restoredWin.Scratchpad.getState();
         restoredWin.close();
         done(state);
-      });
+      }, false);
     }, function(restoredStates) {
       // Then make sure they were restored with the right states
       ok(statesMatch(restoredStates, states),
