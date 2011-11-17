aed51607077262dbb43acb26df996f4324aa95fa
Matt Woodrow <mwoodrow@mozilla.com>
2011-11-17 16:45:51 +1300

    Bug 703109 - Add CreateOffscreenDrawTarget to gfxAndroidPlatform. r=Bas

--- gfx/thebes/gfxAndroidPlatform.cpp
+++ gfx/thebes/gfxAndroidPlatform.cpp
@@ -79,6 +79,12 @@ gfxAndroidPlatform::CreateOffscreenSurface(const gfxIntSize& size,
     return newSurface.forget();
 }
 
+RefPtr<DrawTarget>
+gfxAndroidPlatform::CreateOffscreenDrawTarget(const IntSize& aSize, SurfaceFormat aFormat)
+{
+  return Factory::CreateDrawTarget(BACKEND_SKIA, aSize, aFormat);
+}
+
 nsresult
 gfxAndroidPlatform::GetFontList(nsIAtom *aLangGroup,
                                 const nsACString& aGenericFamily,
