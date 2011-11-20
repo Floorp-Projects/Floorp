14895e915e4065cc27a627bcdad557da5425ab05
Geoff Lankow <geoff@darktrojan.net>
2011-11-21 00:34:33 +1300

    Bug 702448 - Create some view source UI mochitests; r=jwein

---
+++ toolkit/components/viewsource/test/browser/browser_bug699356.js
@@ -0,0 +1,16 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+function test() {
+  let source = "about:blank";
+
+  waitForExplicitFinish();
+  openViewSourceWindow(source, function(aWindow) {
+    let gBrowser = aWindow.gBrowser;
+
+    todo(gBrowser.contentDocument.title == source, "Correct document title");
+    todo(aWindow.document.documentElement.getAttribute("title") == "Source of: " + source, "Correct window title");
+    closeViewSourceWindow(aWindow, finish);
+  });
+}
