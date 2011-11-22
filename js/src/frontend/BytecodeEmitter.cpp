e80787258a77db8c5c996e1f8234d701308d309d
Chris Leary <cdleary@mozilla.com>
2011-11-22 11:46:18 -0800

    Bug 704369: Factor conditional expr emit. (r=Waldo)

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -6651,18 +6651,18 @@ EmitConditionalExpression(JSContext *cx, BytecodeEmitter *bce, ParseNode *pn)
 {
     /* Emit the condition, then branch if false to the else part. */
     if (!EmitTree(cx, bce, pn->pn_kid1))
-        return JS_FALSE;
+        return false;
     ptrdiff_t noteIndex = NewSrcNote(cx, bce, SRC_COND);
     if (noteIndex < 0)
-        return JS_FALSE;
+        return false;
     ptrdiff_t beq = EmitJump(cx, bce, JSOP_IFEQ, 0);
     if (beq < 0 || !EmitTree(cx, bce, pn->pn_kid2))
-        return JS_FALSE;
+        return false;
 
     /* Jump around else, fixup the branch, emit else, fixup jump. */
     ptrdiff_t jmp = EmitJump(cx, bce, JSOP_GOTO, 0);
     if (jmp < 0)
-        return JS_FALSE;
+        return false;
     CHECK_AND_SET_JUMP_OFFSET_AT(cx, bce, beq);
 
     /*
@@ -6671,20 +6671,16 @@ EmitConditionalExpression(JSContext *cx, BytecodeEmitter *bce, ParseNode *pn)
      * ignore the value pushed by the first branch.  Execution will follow
      * only one path, so we must decrement bce->stackDepth.
      *
-     * Failing to do this will foil code, such as the try/catch/finally
-     * exception handling code generator, that samples bce->stackDepth for
-     * use at runtime (JSOP_SETSP), or in let expression and block code
-     * generation, which must use the stack depth to compute local stack
-     * indexes correctly.
+     * Failing to do this will foil code, such as let expression and block
+     * code generation, which must use the stack depth to compute local
+     * stack indexes correctly.
      */
     JS_ASSERT(bce->stackDepth > 0);
     bce->stackDepth--;
     if (!EmitTree(cx, bce, pn->pn_kid3))
-        return JS_FALSE;
+        return false;
     CHECK_AND_SET_JUMP_OFFSET_AT(cx, bce, jmp);
-    if (!SetSrcNoteOffset(cx, bce, noteIndex, 0, jmp - beq))
-        return JS_FALSE;
-    return true;
+    return SetSrcNoteOffset(cx, bce, noteIndex, 0, jmp - beq);
 }
 
 JSBool
