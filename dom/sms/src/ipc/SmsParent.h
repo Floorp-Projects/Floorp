894c05344bf15286f944bb25acc8cc4b1dde6caa
Mounir Lamouri <mounir.lamouri@gmail.com>
2011-11-24 12:45:55 +0100

    Bug 674725 - Part N - Receiving SMS: IPC handling. r=cjones

--- dom/sms/src/ipc/SmsParent.h
+++ dom/sms/src/ipc/SmsParent.h
@@ -39,16 +39,27 @@
 #define mozilla_dom_sms_SmsParent_h
 
 #include "mozilla/dom/sms/PSmsParent.h"
+#include "nsIObserver.h"
 
 namespace mozilla {
 namespace dom {
 namespace sms {
 
 class SmsParent : public PSmsParent
+                , public nsIObserver
 {
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIOBSERVER
+
+  SmsParent();
+
   NS_OVERRIDE virtual bool RecvHasSupport(bool* aHasSupport);
   NS_OVERRIDE virtual bool RecvGetNumberOfMessagesForText(const nsString& aText, PRUint16* aResult);
   NS_OVERRIDE virtual bool RecvSendMessage(const nsString& aNumber, const nsString& aMessage);
+
+protected:
+  virtual void ActorDestroy(ActorDestroyReason why);
 };
 
 } // namespace sms
