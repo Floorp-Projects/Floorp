e81afcfb00932903db6e8a92948a49397322d164
Mounir Lamouri <mounir.lamouri@gmail.com>
2011-11-24 12:45:55 +0100

    Bug 674725 - Part N - Receiving SMS: IPC handling. r=cjones

--- dom/sms/src/Types.h
+++ dom/sms/src/Types.h
@@ -35,91 +35,37 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include "SmsMessage.h"
-#include "nsIDOMClassInfo.h"
-#include "jsapi.h" // For OBJECT_TO_JSVAL and JS_NewDateObjectMsec
-#include "Constants.h"
-
-DOMCI_DATA(MozSmsMessage, mozilla::dom::sms::SmsMessage)
+#ifndef mozilla_dom_sms_Types_h
+#define mozilla_dom_sms_Types_h
 
 namespace mozilla {
 namespace dom {
 namespace sms {
 
-NS_INTERFACE_MAP_BEGIN(SmsMessage)
-  NS_INTERFACE_MAP_ENTRY(nsIDOMMozSmsMessage)
-  NS_INTERFACE_MAP_ENTRY(nsISupports)
-  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(MozSmsMessage)
-NS_INTERFACE_MAP_END
-
-NS_IMPL_ADDREF(SmsMessage)
-NS_IMPL_RELEASE(SmsMessage)
-
-SmsMessage::SmsMessage(const SmsMessageData& aData)
-  : mData(aData)
-{
-}
-
-const SmsMessageData&
-SmsMessage::GetData() const
-{
-  return mData;
-}
-
-NS_IMETHODIMP
-SmsMessage::GetId(PRInt32* aId)
-{
-  *aId = mData.id();
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-SmsMessage::GetDelivery(nsAString& aDelivery)
-{
-  switch (mData.delivery()) {
-    case eDeliveryState_Received:
-      aDelivery.AssignLiteral("received");
-      break;
-    case eDeliveryState_Sent:
-      aDelivery.AssignLiteral("sent");
-      break;
-    case eDeliveryState_Unknown:
-    default:
-      NS_ASSERTION(true, "We shouldn't get an unknown delivery state!");
-      return NS_ERROR_UNEXPECTED;
-  }
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-SmsMessage::GetSender(nsAString& aSender)
-{
-  aSender = mData.sender();
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-SmsMessage::GetReceiver(nsAString& aReceiver)
-{
-  aReceiver = mData.receiver();
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-SmsMessage::GetBody(nsAString& aBody)
-{
-  aBody = mData.body();
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-SmsMessage::GetTimestamp(JSContext* cx, jsval* aDate)
-{
-  *aDate = OBJECT_TO_JSVAL(JS_NewDateObjectMsec(cx, mData.timestamp()));
-  return NS_OK;
-}
+// For SmsMessageDate.delivery.
+enum DeliveryState {
+  eDeliveryState_Sent,
+  eDeliveryState_Received,
+  // This state should stay at the end.
+  eDeliveryState_Unknown
+};
 
 } // namespace sms
 } // namespace dom
 } // namespace mozilla
+
+namespace IPC {
+
+/**
+ * Delivery state serializer.
+ */
+template <>
+struct ParamTraits<mozilla::dom::sms::DeliveryState>
+  : public EnumSerializer<mozilla::dom::sms::DeliveryState,
+                          mozilla::dom::sms::eDeliveryState_Sent,
+                          mozilla::dom::sms::eDeliveryState_Unknown>
+{};
+
+} // namespace IPC
+
+#endif // mozilla_dom_sms_Types_h
