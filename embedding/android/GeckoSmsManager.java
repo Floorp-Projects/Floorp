25739c722c76fd0c2313a8926b4e8c0d842a7f59
Mounir Lamouri <mounir.lamouri@gmail.com>
2011-11-25 10:48:51 +0100

    Bug 674725 - Part I - Implement mozSms.getNumberOfMessagesForText(). r=smaug,cjones

--- embedding/android/GeckoSmsManager.java
+++ embedding/android/GeckoSmsManager.java
@@ -1,5 +1,5 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* ***** BEGIN LICENSE BLOCK *****
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
  * The contents of this file are subject to the Mozilla Public License Version
@@ -12,9 +12,9 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is mozilla.org code.
+ * The Original Code is Mozilla Android code.
  *
- * The Initial Developer of the Original Code is Mozilla Foundation
+ * The Initial Developer of the Original Code is Mozilla Foundation.
  * Portions created by the Initial Developer are Copyright (C) 2011
  * the Initial Developer. All Rights Reserved.
  *
@@ -22,8 +22,8 @@
  *   Mounir Lamouri <mounir.lamouri@mozilla.com> (Original Author)
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
@@ -35,37 +35,17 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include "SmsParent.h"
-#include "nsISmsService.h"
+package org.mozilla.gecko;
 
-namespace mozilla {
-namespace dom {
-namespace sms {
+import java.util.ArrayList;
 
-bool
-SmsParent::RecvHasSupport(bool* aHasSupport)
+import android.util.Log;
+
+import android.telephony.SmsManager;
+
+public class GeckoSmsManager
 {
-  *aHasSupport = false;
-
-  nsCOMPtr<nsISmsService> smsService = do_GetService(SMSSERVICE_CONTRACTID);
-  NS_ENSURE_TRUE(smsService, true);
-
-  smsService->HasSupport(aHasSupport);
-  return true;
+  public static int getNumberOfMessagesForText(String aText) {
+    return SmsManager.getDefault().divideMessage(aText).size();
+  }
 }
-
-bool
-SmsParent::RecvGetNumberOfMessagesForText(const nsString& aText, PRUint16* aResult)
-{
-  *aResult = 0;
-
-  nsCOMPtr<nsISmsService> smsService = do_GetService(SMSSERVICE_CONTRACTID);
-  NS_ENSURE_TRUE(smsService, true);
-
-  smsService->GetNumberOfMessagesForText(aText, aResult);
-  return true;
-}
-
-} // namespace sms
-} // namespace dom
-} // namespace mozilla
