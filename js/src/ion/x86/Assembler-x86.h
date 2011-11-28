beb6822b5de803d772851dd087658015d73a20e0
Nicolas Pierron <nioclas.b.pierron@mozilla.com>
2011-11-29 00:44:09 +0100

    Use target->raw() instead IonCode* for calls. (no bug, r=dvander)

--- js/src/ion/x86/Assembler-x86.h
+++ js/src/ion/x86/Assembler-x86.h
@@ -339,7 +339,7 @@ class Assembler : public AssemblerX86Shared
     }
     void call(IonCode *target) {
         JmpSrc src = masm.call();
-        addPendingJump(src, target, Relocation::CODE);
+        addPendingJump(src, target->raw(), Relocation::CODE);
     }
     void call(ImmWord target) {
         JmpSrc src = masm.call();
