0de1200eac272566fd44816af72d4ae897177192
Nicolas Pierron <nioclas.b.pierron@mozilla.com>
2011-11-29 00:38:54 +0100

    Add comments on floats.

--- js/src/ion/x64/Architecture-x64.h
+++ js/src/ion/x64/Architecture-x64.h
@@ -182,7 +182,10 @@ class FloatRegisters {
 
     static const uint32 AllocatableMask = AllMask & ~NonAllocatableMask;
 
+    // Registers that may be clobbered during a JS -> JS call.
     static const uint32 JSCallClobberMask = AllocatableMask;
+
+    // Registers returned from a JS -> C call.
     static const uint32 JSCCallMask = 0;
 };
 
