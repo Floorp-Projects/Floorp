478e8c4f49402a79ade803c649d3a74b6919c4df
Nicolas Pierron <nioclas.b.pierron@mozilla.com>
2011-11-29 00:20:58 +0100

    Add safepoint based on the properties of the MIR (Bug 695887, r=dvander)

--- js/src/ion/x64/Lowering-x64.h
+++ js/src/ion/x64/Lowering-x64.h
@@ -58,6 +58,8 @@ class LIRGeneratorX64 : public LIRGeneratorX86Shared
     LSnapshot *buildSnapshot(LInstruction *ins, MResumePoint *rp, BailoutKind kind);
     bool assignSnapshot(LInstruction *ins, BailoutKind kind = Bailout_Normal);
     bool assignPostSnapshot(MInstruction *mir, LInstruction *ins);
+    bool assignSafepoint(MInstruction *mir, LInstruction *ins);
+
     void lowerUntypedPhiInput(MPhi *phi, uint32 inputPosition, LBlock *block, size_t lirIndex);
     bool defineUntypedPhi(MPhi *phi, size_t lirIndex);
 
