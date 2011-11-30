5aae1f453dd10ddbe1a9053ed62695413071ab5e
Henri Sivonen <hsivonen@iki.fi>
2011-11-30 19:44:31 +0200

    Bug 703965 - Use View Source styling when viewing plain text source. r=smaug.

--- parser/html/nsHtml5Parser.cpp
+++ parser/html/nsHtml5Parser.cpp
@@ -714,6 +714,8 @@ nsHtml5Parser::MarkAsNotScriptCreated(const char* aCommand)
     mode = VIEW_SOURCE_HTML;
   } else if (!nsCRT::strcmp(aCommand, "view-source-xml")) {
     mode = VIEW_SOURCE_XML;
+  } else if (!nsCRT::strcmp(aCommand, "view-source-plain")) {
+    mode = VIEW_SOURCE_PLAIN;
   } else if (!nsCRT::strcmp(aCommand, "plain-text")) {
     mode = PLAIN_TEXT;
   } else if (!nsCRT::strcmp(aCommand, kLoadAsData)) {
