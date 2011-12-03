aab1f54a1545b8017763e5af7376d919f16dc955
Kyle Huey <khuey@kylehuey.com>
2011-12-03 12:18:38 -0500

    Bug 704464: Part 2 - Make Database creation hook into the quota system. r=bent

--- dom/indexedDB/test/browser_quotaPromptAllow.js
+++ dom/indexedDB/test/browser_quotaPromptAllow.js
@@ -4,7 +4,7 @@
  */
 
 // Make sure this is a unique origin or the tests will randomly fail!
-const testPageURL = "http://test1.example.org/browser/" +
+const testPageURL = "http://bug704464-1.example.com/browser/" +
   "dom/indexedDB/test/browser_quotaPrompt.html";
 const notificationID = "indexedDB-quota-prompt";
 
