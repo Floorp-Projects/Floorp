59b41e200e5ec14b5c9b18fde55c7bad64e79c5c
Rafael Ávila de Espíndola <respindola@mozilla.com>
2011-12-02 19:26:04 -0500

    Bug 696376 - Change how we find critical ranges so that it works on 10.6 too. r=dbaron. Currently we use dlsym on pthread_cond_wait$UNIX2003 to find a function that indicates that new_sem_from_pool is on the stack. This works on 10.5, but on 10.6 I could not find a single reliable indicator that would work with dlsym.
    The good news is that dladdr works with any symbol, not just exported
    ones. To find the address of new_sem_from_pool, we set up a malloc logger
    and force a call to new_sem_from_pool. From the logger callback we walk
    the stack trying dladdr on every address.
    
    To force a call to new_sem_from_pool, the initialization code has to be the
    first to use semaphores, so it is now run from NS_LogInit.
    
    This works on 10.6 and 10.5 (but we have to look for
    "pthread_cond_wait$UNIX2003"). In 10.7 the call to malloc is gone, so we don't
    have to worry about critical addresses on it anymore.

--- xpcom/base/nsTraceRefcntImpl.cpp
+++ xpcom/base/nsTraceRefcntImpl.cpp
@@ -50,6 +50,7 @@
 #include "nsCOMPtr.h"
 #include "nsCRT.h"
 #include <math.h>
+#include "nsStackWalkPrivate.h"
 #include "nsStackWalk.h"
 #include "nsString.h"
 
@@ -923,6 +924,8 @@ nsTraceRefcntImpl::DemangleSymbol(const char * aSymbol,
 EXPORT_XPCOM_API(void)
 NS_LogInit()
 {
+  // FIXME: This is called multiple times, we should probably not allow that.
+  StackWalkInitCriticalAddress();
 #ifdef NS_IMPL_REFCNT_LOGGING
   if (++gInitCount)
     nsTraceRefcntImpl::SetActivityIsLegal(true);
