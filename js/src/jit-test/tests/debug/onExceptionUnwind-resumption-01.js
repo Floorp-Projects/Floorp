062e8d4c10e32cb117006ea2b4be957d63d19bff
Jim Blandy <jimb@mozilla.com>
2011-12-03 18:51:30 -0800

    Bug 700070: Have stubs::AnyFrameEpilogue call the full ScriptEpilogue before StackFrame::functionEpilogue, not just ScriptDebugEpilogue after functionEpilogue. r=jorendorff
    The debugger tests don't try returning all four kinds of resumption values
    from an onExceptionUnwind handler, and under JaegerMonkey, forcing a
    constructor to return a primitive can cause an assertion:
    
    Assertion failure: !args.rval().isPrimitive(), at /home/jimb/moz/dbg/js/src/jsinterp.cpp:651
    
    This patch changes AnyFrameEpilogue to match what the interpreter and
    mjit::Compiler::emitReturn does, and adds test cases for all the resumption
    values.

---
+++ js/src/jit-test/tests/debug/onExceptionUnwind-resumption-01.js
@@ -0,0 +1,9 @@
+// Check that an onExceptionUnwind hook can force a frame to return a value early.
+
+var g = newGlobal('new-compartment');
+var dbg = Debugger(g);
+dbg.onExceptionUnwind = function (frame, exc) {
+    return { return:"sproon" };
+};
+g.eval("function f() { throw 'ksnife'; }");
+assertEq(g.f(), "sproon");
