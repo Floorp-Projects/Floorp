a9518b8d5c6b8a1302d79b704ab15c7dd368ae2a
Nicolas Pierron <nioclas.b.pierron@mozilla.com>
2011-12-06 20:59:52 +0100

    Fix: Add call masks to ARM. (Bug 685838, r=red)

--- js/src/ion/arm/Architecture-arm.h
+++ js/src/ion/arm/Architecture-arm.h
@@ -157,11 +157,15 @@ class Registers
     // Registers that can be allocated without being saved, generally.
     static const uint32 TempMask = VolatileMask & ~NonAllocatableMask;
 
-    static const uint32 JSCallClobberMask =
+    // Registers that hold arguments and results of calls.
+    static const uint32 JSCallMask =
               (1 << Registers::r0) |
               (1 << Registers::r1) |
               (1 << Registers::r2) |
               (1 << Registers::r3);
+
+    static const uint32 JSCCallMask = JSCallMask;
+
     static const uint32 AllocatableMask = AllMask & ~NonAllocatableMask;
 };
 
@@ -231,8 +235,6 @@ class FloatRegisters
     static const uint32 TempMask = VolatileMask & ~NonAllocatableMask;
 
     static const uint32 AllocatableMask = AllMask & ~NonAllocatableMask;
-    static const uint32 JSCallClobberMask = AllocatableMask;
-
 };
 
 bool hasMOVWT();
