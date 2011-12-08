0ee1724fd8f56a5df5b313dae938818fb0ae7592
Brandon Sterne <bsterne@mozilla.com>
2011-12-08 14:59:17 -0800

    remove unused variable introduced by bug 702439, r=me

--- content/base/src/nsCSPService.cpp
+++ content/base/src/nsCSPService.cpp
@@ -297,7 +297,6 @@ CSPService::AsyncOnChannelRedirect(nsIChannel *oldChannel,
 
   // The redirecting channel isn't a writable property bag, we won't be able
   // to enforce the load policy if it redirects again, so we stop it now.
-  nsXPIDLString message;
   nsCAutoString newUriSpec;
   newUri->GetSpec(newUriSpec);
   const PRUnichar *formatParams[] = { NS_ConvertUTF8toUTF16(newUriSpec).get() };
