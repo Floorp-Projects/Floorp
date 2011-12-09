17c4a3538ee51a71d070dc0cbd15ca381314870f
Daniel Holbert <dholbert@cs.stanford.edu>
2011-12-08 17:39:23 -0800

    Bug 708443 patch 2: disable debug output from animationPolling.js, and add emacs modeline. r=jwir3

--- image/test/mochitest/animationPolling.js
+++ image/test/mochitest/animationPolling.js
@@ -1,5 +1,7 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 var currentTest;
 var gIsRefImageLoaded = false;
+const gShouldOutputDebugInfo = false;
 
 function pollForSuccess()
 {
@@ -119,6 +121,9 @@ AnimationTest.prototype.preloadImage = function()
 
 AnimationTest.prototype.outputDebugInfo = function(message, id, dataUri)
 {
+  if (!gShouldOutputDebugInfo) {
+    return;
+  }
   var debugElement = document.getElementById(this.debugElementId);
   var newDataUriElement = document.createElement("a");
   newDataUriElement.setAttribute("id", id);
