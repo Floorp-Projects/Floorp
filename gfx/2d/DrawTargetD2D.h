f9928a2b38ac0cd169ba43602e370391027b97aa
Robert O'Callahan <robert@ocallahan.org>
2011-12-09 22:53:14 +1300

    Bug 705559. Part 3: Use mDirtyTransform to avoid unnecessary SetTransform calls. r=bas

--- gfx/2d/DrawTargetD2D.h
+++ gfx/2d/DrawTargetD2D.h
@@ -157,6 +157,12 @@ private:
   // This function will mark the surface as changing, and make sure any
   // copy-on-write snapshots are notified.
   void MarkChanged();
+  void FlushTransformToRT() {
+    if (mTransformDirty) {
+      mRT->SetTransform(D2DMatrix(mTransform));
+      mTransformDirty = false;
+    }
+  }
   void AddDependencyOnSource(SourceSurfaceD2DTarget* aSource);
 
   ID3D10BlendState *GetBlendStateForOperator(CompositionOp aOperator);
