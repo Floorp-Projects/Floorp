223c11cff5d5990b14ee8eeb2e2b04e48131d870
Ms2ger <ms2ger@gmail.com>
2011-12-11 21:00:28 +0100

    Bug 709523 - Part d: Remove nsHTMLEditor::IsRootTag; r=ehsan a=edmorley

--- editor/libeditor/html/nsHTMLEditor.h
+++ editor/libeditor/html/nsHTMLEditor.h
@@ -513,8 +513,6 @@ protected:
 
 // End of Table Editing utilities
   
-  NS_IMETHOD IsRootTag(nsString &aTag, bool &aIsTag);
-
   virtual bool IsBlockNode(nsIDOMNode *aNode);
   virtual bool IsBlockNode(nsINode *aNode);
   
