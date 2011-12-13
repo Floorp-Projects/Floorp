ff78998302d4fa06be7dcee198358a447f756daf
Brijesh Patel <brijesh3105@gmail.com>
2011-12-13 09:47:31 -0400

    Bug 704295 - Autocomplete with a variable name that is equal to a prefix of a global variable makes it impossible to use the variable; r=msucan

--- browser/devtools/webconsole/test/browser/browser_webconsole_bug_704295.js
+++ browser/devtools/webconsole/test/browser/browser_webconsole_bug_704295.js
@@ -19,11 +19,9 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *  David Dahl <ddahl@mozilla.com>
- *  Patrick Walton <pcwalton@mozilla.com>
- *  Julian Viereck <jviereck@mozilla.com>
  *  Mihai Sucan <mihai.sucan@gmail.com>
- *
+ *  Brijesh Patel <brijesh3105@gmail.com>
+ * 
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
  * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
@@ -38,7 +36,7 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-// Tests that code completion works properly.
+// Tests for bug 704295
 
 const TEST_URI = "http://example.com/browser/browser/devtools/webconsole/test//browser/test-console.html";
 
@@ -55,53 +53,23 @@ function testCompletion() {
   var jsterm = HUDService.getHudByWindow(content).jsterm;
   var input = jsterm.inputNode;
 
-  // Test typing 'docu'.
-  input.value = "docu";
-  input.setSelectionRange(4, 4);
-  jsterm.complete(jsterm.COMPLETE_HINT_ONLY);
-  is(input.value, "docu", "'docu' completion");
-  is(jsterm.completeNode.value, "    ment", "'docu' completion");
-
-  // Test typing 'docu' and press tab.
-  input.value = "docu";
-  input.setSelectionRange(4, 4);
-  jsterm.complete(jsterm.COMPLETE_FORWARD);
-  is(input.value, "document", "'docu' tab completion");
-  is(input.selectionStart, 8, "start selection is alright");
-  is(input.selectionEnd, 8, "end selection is alright");
-  is(jsterm.completeNode.value.replace(/ /g, ""), "", "'docu' completed");
-
-  // Test typing 'document.getElem'.
-  input.value = "document.getElem";
-  input.setSelectionRange(16, 16);
-  jsterm.complete(jsterm.COMPLETE_FORWARD);
-  is(input.value, "document.getElem", "'document.getElem' completion");
-  is(jsterm.completeNode.value, "                entById", "'document.getElem' completion");
-
-  // Test pressing tab another time.
-  jsterm.complete(jsterm.COMPLETE_FORWARD);
-  is(input.value, "document.getElem", "'document.getElem' completion");
-  is(jsterm.completeNode.value, "                entsByClassName", "'document.getElem' another tab completion");
-
-  // Test pressing shift_tab.
-  jsterm.complete(jsterm.COMPLETE_BACKWARD);
-  is(input.value, "document.getElem", "'document.getElem' untab completion");
-  is(jsterm.completeNode.value, "                entById", "'document.getElem' completion");
-
-  jsterm.clearOutput();
-  jsterm.history.splice(0, jsterm.history.length);   // workaround for bug 592552
-
-  input.value = "docu";
-  jsterm.complete(jsterm.COMPLETE_HINT_ONLY);
-  is(jsterm.completeNode.value, "    ment", "'docu' completion");
-  jsterm.execute();
+  // Test typing 'var d = 5;' and press RETURN
+  jsterm.setInputValue("var d = ");
+  EventUtils.synthesizeKey("5", {});
+  EventUtils.synthesizeKey(";", {});
+  is(input.value, "var d = 5;", "var d = 5;");
+  is(jsterm.completeNode.value, "", "no completion");
+  EventUtils.synthesizeKey("VK_ENTER", {});
   is(jsterm.completeNode.value, "", "clear completion on execute()");
-
-  // Test multi-line completion works
-  input.value =                 "console.log('one');\nconsol";
-  jsterm.complete(jsterm.COMPLETE_HINT_ONLY);
-  is(jsterm.completeNode.value, "                   \n      e", "multi-line completion");
-
+  
+  // Test typing 'var a = d' and press RETURN
+  jsterm.setInputValue("var a = ");
+  EventUtils.synthesizeKey("d", {});
+  is(input.value, "var a = d", "var a = d");
+  is(jsterm.completeNode.value, "", "no completion");
+  EventUtils.synthesizeKey("VK_ENTER", {});
+  is(jsterm.completeNode.value, "", "clear completion on execute()");
+  
   jsterm = input = null;
   finishTest();
 }
