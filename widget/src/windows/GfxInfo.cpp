0d1edb8efed40c9e01bf9d7d2c690d0fd8053319
Doug Sherk <dsherk@mozilla.com>
2011-12-14 21:03:08 -0800

    Bug 706739: don't evaluate static blocklist or special cases when evaluating downloaded blocklist r=joe
    A bug was introduced in 668004 which caused features to be preffed off while
    evaluating the downloaded blocklist, even if they weren't in the list. This
    shouldn't actually have any impact on the end user experience, but is definitely
    not optimal.

--- widget/src/windows/GfxInfo.cpp
+++ widget/src/windows/GfxInfo.cpp
@@ -940,89 +940,89 @@ GfxInfo::GetFeatureStatusImpl(PRInt32 aFeature,
                               const nsTArray<GfxDriverInfo>& aDriverInfo,
                               OperatingSystem* aOS /* = nsnull */)
 {
+  NS_ENSURE_ARG_POINTER(aStatus);
   aSuggestedDriverVersion.SetIsVoid(true);
-
-  PRInt32 status = nsIGfxInfo::FEATURE_STATUS_UNKNOWN;
-
-  nsAutoString adapterVendorID;
-  nsAutoString adapterDeviceID;
-  nsAutoString adapterDriverVersionString;
-  if (NS_FAILED(GetAdapterVendorID(adapterVendorID)) ||
-      NS_FAILED(GetAdapterDeviceID(adapterDeviceID)) ||
-      NS_FAILED(GetAdapterDriverVersion(adapterDriverVersionString)))
-  {
-    return NS_ERROR_FAILURE;
-  }
-
-  if (adapterVendorID != GfxDriverInfo::GetDeviceVendor(VendorIntel) &&
-      adapterVendorID != GfxDriverInfo::GetDeviceVendor(VendorNVIDIA) &&
-      adapterVendorID != GfxDriverInfo::GetDeviceVendor(VendorAMD) &&
-      adapterVendorID != GfxDriverInfo::GetDeviceVendor(VendorATI) &&
-      // FIXME - these special hex values are currently used in xpcshell tests introduced by
-      // bug 625160 patch 8/8. Maybe these tests need to be adjusted now that we're only whitelisting
-      // intel/ati/nvidia.
-      !adapterVendorID.LowerCaseEqualsLiteral("0xabcd") &&
-      !adapterVendorID.LowerCaseEqualsLiteral("0xdcba") &&
-      !adapterVendorID.LowerCaseEqualsLiteral("0xabab") &&
-      !adapterVendorID.LowerCaseEqualsLiteral("0xdcdc"))
-  {
-    *aStatus = FEATURE_BLOCKED_DEVICE;
-    return NS_OK;
-  }
-
-  PRUint64 driverVersion;
-  if (!ParseDriverVersion(adapterDriverVersionString, &driverVersion)) {
-    return NS_ERROR_FAILURE;
-  }
-
-  // special-case the WinXP test slaves: they have out-of-date drivers, but we still want to
-  // whitelist them, actually we do know that this combination of device and driver version
-  // works well.
-  if (mWindowsVersion == gfxWindowsPlatform::kWindowsXP &&
-      adapterVendorID == GfxDriverInfo::GetDeviceVendor(VendorNVIDIA) &&
-      adapterDeviceID.LowerCaseEqualsLiteral("0x0861") && // GeForce 9400
-      driverVersion == V(6,14,11,7756))
-  {
-    *aStatus = FEATURE_NO_INFO;
-    return NS_OK;
-  }
-
-  if (aFeature == FEATURE_DIRECT3D_9_LAYERS &&
-      mWindowsVersion < gfxWindowsPlatform::kWindowsXP)
-  {
-    *aStatus = FEATURE_BLOCKED_OS_VERSION;
-    return NS_OK;
-  }
-
-  // ANGLE currently uses D3D10 <-> D3D9 interop, which crashes on Optimus
-  // machines.
-  if (aFeature == FEATURE_WEBGL_ANGLE &&
-      gfxWindowsPlatform::IsOptimus())
-  {
-    *aStatus = FEATURE_BLOCKED_DEVICE;
-    return NS_OK;
-  }
-
   OperatingSystem os = WindowsVersionToOperatingSystem(mWindowsVersion);
-
-  // Windows Server 2003 should be just like Windows XP for present purpose, but still has a different version number.
-  // OTOH Windows Server 2008 R1 and R2 already have the same version numbers as Vista and Seven respectively
-  if (os == DRIVER_OS_WINDOWS_SERVER_2003)
-    os = DRIVER_OS_WINDOWS_XP;
-
-  if (mHasDriverVersionMismatch) {
-    if (aFeature == nsIGfxInfo::FEATURE_DIRECT3D_10_LAYERS ||
-        aFeature == nsIGfxInfo::FEATURE_DIRECT3D_10_1_LAYERS ||
-        aFeature == nsIGfxInfo::FEATURE_DIRECT2D)
-    {
-      *aStatus = nsIGfxInfo::FEATURE_BLOCKED_DRIVER_VERSION;
-      return NS_OK;
-    }
-  }
-
-  *aStatus = status;
+  *aStatus = nsIGfxInfo::FEATURE_STATUS_UNKNOWN;
   if (aOS)
     *aOS = os;
+
+  // Don't evaluate special cases if we're checking the downloaded blocklist.
+  if (!aDriverInfo.Length()) {
+    nsAutoString adapterVendorID;
+    nsAutoString adapterDeviceID;
+    nsAutoString adapterDriverVersionString;
+    if (NS_FAILED(GetAdapterVendorID(adapterVendorID)) ||
+        NS_FAILED(GetAdapterDeviceID(adapterDeviceID)) ||
+        NS_FAILED(GetAdapterDriverVersion(adapterDriverVersionString)))
+    {
+      return NS_ERROR_FAILURE;
+    }
+
+    if (adapterVendorID != GfxDriverInfo::GetDeviceVendor(VendorIntel) &&
+        adapterVendorID != GfxDriverInfo::GetDeviceVendor(VendorNVIDIA) &&
+        adapterVendorID != GfxDriverInfo::GetDeviceVendor(VendorAMD) &&
+        adapterVendorID != GfxDriverInfo::GetDeviceVendor(VendorATI) &&
+        // FIXME - these special hex values are currently used in xpcshell tests introduced by
+        // bug 625160 patch 8/8. Maybe these tests need to be adjusted now that we're only whitelisting
+        // intel/ati/nvidia.
+        !adapterVendorID.LowerCaseEqualsLiteral("0xabcd") &&
+        !adapterVendorID.LowerCaseEqualsLiteral("0xdcba") &&
+        !adapterVendorID.LowerCaseEqualsLiteral("0xabab") &&
+        !adapterVendorID.LowerCaseEqualsLiteral("0xdcdc"))
+    {
+      *aStatus = FEATURE_BLOCKED_DEVICE;
+      return NS_OK;
+    }
+
+    PRUint64 driverVersion;
+    if (!ParseDriverVersion(adapterDriverVersionString, &driverVersion)) {
+      return NS_ERROR_FAILURE;
+    }
+
+    // special-case the WinXP test slaves: they have out-of-date drivers, but we still want to
+    // whitelist them, actually we do know that this combination of device and driver version
+    // works well.
+    if (mWindowsVersion == gfxWindowsPlatform::kWindowsXP &&
+        adapterVendorID == GfxDriverInfo::GetDeviceVendor(VendorNVIDIA) &&
+        adapterDeviceID.LowerCaseEqualsLiteral("0x0861") && // GeForce 9400
+        driverVersion == V(6,14,11,7756))
+    {
+      *aStatus = FEATURE_NO_INFO;
+      return NS_OK;
+    }
+
+    if (aFeature == FEATURE_DIRECT3D_9_LAYERS &&
+        mWindowsVersion < gfxWindowsPlatform::kWindowsXP)
+    {
+      *aStatus = FEATURE_BLOCKED_OS_VERSION;
+      return NS_OK;
+    }
+
+    // ANGLE currently uses D3D10 <-> D3D9 interop, which crashes on Optimus
+    // machines.
+    if (aFeature == FEATURE_WEBGL_ANGLE &&
+        gfxWindowsPlatform::IsOptimus())
+    {
+      *aStatus = FEATURE_BLOCKED_DEVICE;
+      return NS_OK;
+    }
+
+    // Windows Server 2003 should be just like Windows XP for present purpose, but still has a different version number.
+    // OTOH Windows Server 2008 R1 and R2 already have the same version numbers as Vista and Seven respectively
+    if (os == DRIVER_OS_WINDOWS_SERVER_2003)
+      os = DRIVER_OS_WINDOWS_XP;
+
+    if (mHasDriverVersionMismatch) {
+      if (aFeature == nsIGfxInfo::FEATURE_DIRECT3D_10_LAYERS ||
+          aFeature == nsIGfxInfo::FEATURE_DIRECT3D_10_1_LAYERS ||
+          aFeature == nsIGfxInfo::FEATURE_DIRECT2D)
+      {
+        *aStatus = nsIGfxInfo::FEATURE_BLOCKED_DRIVER_VERSION;
+        return NS_OK;
+      }
+    }
+  }
 
   return GfxInfoBase::GetFeatureStatusImpl(aFeature, aStatus, aSuggestedDriverVersion, aDriverInfo, &os);
 }
