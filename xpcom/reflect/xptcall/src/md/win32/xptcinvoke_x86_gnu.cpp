8070c82b921555d1133860d252993954f2e8ba0b
Jacek Caban <jacek@codeweavers.com>
2011-12-15 17:14:34 +0100

    Bug 707469 - Don't use UNIX variant xptcinvoke implementation on mingw r=benjamin

--- xpcom/reflect/xptcall/src/md/win32/xptcinvoke_x86_gnu.cpp
+++ xpcom/reflect/xptcall/src/md/win32/xptcinvoke_x86_gnu.cpp
@@ -39,10 +39,9 @@
 /* Platform specific code to invoke XPCOM methods on native objects */
 
 #include "xptcprivate.h"
-#include "xptc_gcc_x86_unix.h"
 
 extern "C" {
-static void ATTRIBUTE_USED __attribute__ ((regparm(3)))
+void __attribute__ ((__used__)) __attribute__ ((regparm(3)))
 invoke_copy_to_stack(PRUint32 paramCount, nsXPTCVariant* s, PRUint32* d)
 {
     for(PRUint32 i = paramCount; i >0; i--, d++, s++)
@@ -55,10 +54,23 @@ invoke_copy_to_stack(PRUint32 paramCount, nsXPTCVariant* s, PRUint32* d)
 
         switch(s->type)
         {
+        case nsXPTType::T_I8     : *((PRInt8*)  d) = s->val.i8;          break;
+        case nsXPTType::T_I16    : *((PRInt16*) d) = s->val.i16;         break;
+        case nsXPTType::T_I32    : *((PRInt32*) d) = s->val.i32;         break;
         case nsXPTType::T_I64    : *((PRInt64*) d) = s->val.i64; d++;    break;
+        case nsXPTType::T_U8     : *((PRUint8*) d) = s->val.u8;          break;
+        case nsXPTType::T_U16    : *((PRUint16*)d) = s->val.u16;         break;
+        case nsXPTType::T_U32    : *((PRUint32*)d) = s->val.u32;         break;
         case nsXPTType::T_U64    : *((PRUint64*)d) = s->val.u64; d++;    break;
+        case nsXPTType::T_FLOAT  : *((float*)   d) = s->val.f;           break;
         case nsXPTType::T_DOUBLE : *((double*)  d) = s->val.d;   d++;    break;
-        default                  : *((void**)d)    = s->val.p;           break;
+        case nsXPTType::T_BOOL   : *((bool*)    d) = s->val.b;           break;
+        case nsXPTType::T_CHAR   : *((char*)    d) = s->val.c;           break;
+        case nsXPTType::T_WCHAR  : *((wchar_t*) d) = s->val.wc;          break;
+        default:
+            // all the others are plain pointer types
+            *((void**)d) = s->val.p;
+            break;
         }
     }
 }
@@ -85,11 +97,8 @@ __asm__ (
 /* alignment here seems unimportant here; this was 16, now it's 2 which
    is what xptcstubs uses. */
 	".align 2\n\t"
-	".globl " SYMBOL_UNDERSCORE "NS_InvokeByIndex_P\n\t"
-#ifndef XP_MACOSX
-	".type  " SYMBOL_UNDERSCORE "NS_InvokeByIndex_P,@function\n"
-#endif
-	SYMBOL_UNDERSCORE "NS_InvokeByIndex_P:\n\t"
+	".globl _NS_InvokeByIndex_P\n\t"
+	"_NS_InvokeByIndex_P:\n\t"
 	"pushl %ebp\n\t"
 	"movl  %esp, %ebp\n\t"
 	"movl  0x10(%ebp), %eax\n\t"
@@ -115,7 +124,7 @@ __asm__ (
 	"pushl %edx\n\t"                /* push this. esp % 16 == 0 */
 
 	"movl  0x14(%ebp), %edx\n\t"
-	"call  " SYMBOL_UNDERSCORE "invoke_copy_to_stack\n\t"
+	"call  _invoke_copy_to_stack\n\t"
 	"movl  0x08(%ebp), %ecx\n\t"	/* 'that' */
 	"movl  (%ecx), %edx\n\t"
 	"movl  0x0c(%ebp), %eax\n\t"    /* function index */
@@ -124,7 +133,7 @@ __asm__ (
 	"movl  %ebp, %esp\n\t"
 	"popl  %ebp\n\t"
 	"ret\n"
-#ifndef XP_MACOSX
-	".size " SYMBOL_UNDERSCORE "NS_InvokeByIndex_P, . -" SYMBOL_UNDERSCORE "NS_InvokeByIndex_P\n\t"
-#endif
+	".section .drectve\n\t"
+	".ascii \" -export:NS_InvokeByIndex_P\"\n\t"
+	".text\n\t"
 );
