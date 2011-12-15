a067c3405ef7d3d7a9418e18cc55ef1be7a6f611
Jeff Muizelaar <jmuizelaar@mozilla.com>
2011-12-15 07:31:41 -0500

    Bug 711491. Refactor MapInfo. r=bgirard
    Moves MapInfo out of platform and renames it to SharedLibrary. There will
    eventually be an implementation for all major platforms.
    
    --HG--
    extra : rebase_source : c7eae4bc0f0e27f2801c4e639d7dc82b47465f0b

---
+++ tools/profiler/sps/shared-libraries.h
@@ -0,0 +1,108 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim:set ts=4 sw=4 sts=4 et cindent: */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla code.
+ *
+ * The Initial Developer of the Original Code is the Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Jeff Muizelaar <jmuizelaar@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#include <vector>
+#include <string.h>
+#include <stdlib.h>
+#include <mozilla/StdInt.h>
+
+class SharedLibrary {
+public:
+  SharedLibrary(unsigned long aStart, unsigned long aEnd, unsigned long aOffset, char *aName)
+    : mStart(aStart)
+    , mEnd(aEnd)
+    , mOffset(aOffset)
+    , mName(strdup(aName))
+  {}
+
+  SharedLibrary(const SharedLibrary& aEntry)
+    : mStart(aEntry.mStart)
+    , mEnd(aEntry.mEnd)
+    , mOffset(aEntry.mOffset)
+    , mName(strdup(aEntry.mName))
+  {}
+
+  SharedLibrary& operator=(const SharedLibrary& aEntry)
+  {
+    mStart = aEntry.mStart;
+    mEnd = aEntry.mEnd;
+    mOffset = aEntry.mOffset;
+    mName = strdup(aEntry.mName);
+    return *this;
+  }
+
+  ~SharedLibrary()
+  {
+    free(mName);
+  }
+
+  uintptr_t GetStart() { return mStart; }
+  uintptr_t GetEnd() { return mEnd; }
+  char* GetName() { return mName; }
+
+private:
+  explicit SharedLibrary() {}
+
+  uintptr_t mStart;
+  uintptr_t mEnd;
+  uintptr_t mOffset;
+  char *mName;
+};
+
+class SharedLibraryInfo {
+public:
+  static SharedLibraryInfo GetInfoForSelf();
+  SharedLibraryInfo() {}
+
+  void AddSharedLibrary(SharedLibrary entry)
+  {
+    mEntries.push_back(entry);
+  }
+
+  SharedLibrary& GetEntry(size_t i)
+  {
+    return mEntries[i];
+  }
+
+  size_t GetSize()
+  {
+    return mEntries.size();
+  }
+private:
+  std::vector<SharedLibrary> mEntries;
+};
