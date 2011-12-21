5b4d10ba67c9339044509af6318f7c2c187925a9
Gregory Szorc <gps@mozilla.com>
2011-12-21 11:28:24 -0800

    Bug 710961 - Carry over syncGUID generated when install is staged; r=Mossop

--- toolkit/mozapps/extensions/test/xpcshell/test_install.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_install.js
@@ -105,7 +105,9 @@ function run_test_1() {
           }, [
             "onInstallStarted",
             "onInstallEnded",
-          ], check_test_1);
+          ], function() {
+            check_test_1(install.addon.syncGUID);
+          });
           install.install();
         });
       });
@@ -113,7 +115,7 @@ function run_test_1() {
   });
 }
 
-function check_test_1() {
+function check_test_1(installSyncGUID) {
   ensure_test_completed();
   AddonManager.getAddonByID("addon1@tests.mozilla.org", function(olda1) {
     do_check_eq(olda1, null);
@@ -160,6 +162,7 @@ function check_test_1() {
           do_check_neq(a1, null);
           do_check_neq(a1.syncGUID, null);
           do_check_true(a1.syncGUID.length >= 9);
+          do_check_eq(a1.syncGUID, installSyncGUID);
           do_check_eq(a1.type, "extension");
           do_check_eq(a1.version, "1.0");
           do_check_eq(a1.name, "Test 1");
