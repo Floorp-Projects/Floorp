4db16a76a90bf500b3f23edcd326cac157314510
James Willcox <jwillcox@mozilla.com>
2011-12-21 14:03:26 -0500

    Bug 670930 - Add Android direct texture implementation

---
+++ widget/src/android/AndroidGraphicBuffer.h
@@ -0,0 +1,99 @@
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Corporation code.
+ *
+ * The Initial Developer of the Original Code is Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   James Willcox <jwillcox@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#ifndef AndroidGraphicBuffer_h_
+#define AndroidGraphicBuffer_h_
+
+#include "gfxASurface.h"
+#include "nsRect.h"
+
+namespace mozilla {
+
+/**
+ * This class allows access to Android's direct texturing mechanism. Locking
+ * the buffer gives you a pointer you can read/write to directly. It is fully
+ * threadsafe, but you probably really want to use the AndroidDirectTexture
+ * class which will handle double buffering.
+ *
+ * In order to use the buffer in OpenGL, just call Bind() and it will attach
+ * to whatever texture is bound to GL_TEXTURE_2D.
+ */
+class AndroidGraphicBuffer
+{
+public:
+  enum {
+    UsageSoftwareRead = 1,
+    UsageSoftwareWrite = 1 << 1,
+    UsageTexture = 1 << 2,
+    UsageTarget = 1 << 3,
+    Usage2D = 1 << 4
+  };
+
+  AndroidGraphicBuffer(PRUint32 width, PRUint32 height, PRUint32 usage, gfxASurface::gfxImageFormat format);
+  virtual ~AndroidGraphicBuffer();
+
+  bool Lock(PRUint32 usage, unsigned char **bits);
+  bool Lock(PRUint32 usage, const nsIntRect& rect, unsigned char **bits);
+  bool Unlock();
+  bool Reallocate(PRUint32 aWidth, PRUint32 aHeight, gfxASurface::gfxImageFormat aFormat);
+
+  PRUint32 Width() { return mWidth; }
+  PRUint32 Height() { return mHeight; }
+
+  bool Bind();
+
+private:
+  PRUint32 mWidth;
+  PRUint32 mHeight;
+  PRUint32 mUsage;
+  gfxASurface::gfxImageFormat mFormat;
+
+  bool EnsureInitialized();
+  bool EnsureEGLImage();
+
+  void DestroyBuffer();
+  bool EnsureBufferCreated(PRUint32 aWidth, PRUint32 aHeight, PRUint32 aUsage, gfxASurface::gfxImageFormat aFormat);
+
+  PRUint32 GetAndroidUsage(PRUint32 aUsage);
+  PRUint32 GetAndroidFormat(gfxASurface::gfxImageFormat aFormat);
+
+  void *mHandle;
+  void *mEGLImage;
+};
+
+} /* mozilla */
+#endif /* AndroidGraphicBuffer_h_ */
