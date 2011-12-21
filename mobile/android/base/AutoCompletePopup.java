4dd2e9a1b8e4e0fc07b326a90b9adfaf50be1914
Margaret Leibovic <margaret.leibovic@gmail.com>
2011-12-21 12:30:53 -0800

    backout 243520b7d8c3 because it broke the autocomplete popup text

--- mobile/android/base/AutoCompletePopup.java
+++ mobile/android/base/AutoCompletePopup.java
@@ -68,8 +68,6 @@ public class AutoCompletePopup extends ListView {
 
     private static final String LOGTAG = "AutoCompletePopup";
 
-    private static int sMinWidth = 0;
-
     public AutoCompletePopup(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
@@ -134,18 +132,7 @@ public class AutoCompletePopup extends ListView {
         // If the textbox is smaller than the screen-width,
         // shrink the list's width
         if ((left + width) < viewport.width) 
-            listWidth = left < 0 ? left + width : width;
-
-        // Late initializing variable to allow DisplayMetrics not to be null and avoid NPE
-        if (sMinWidth == 0)
-            sMinWidth = 100 * GeckoAppShell.getDpi();
-
-        if (listWidth < sMinWidth) {
-            listWidth = sMinWidth;
-
-            if ((listLeft + listWidth) > viewport.width)
-                listLeft = (int)viewport.width - listWidth;
-        }
+            listWidth = left + width;
 
         // If the list is extending outside of the viewport
         // try moving above
