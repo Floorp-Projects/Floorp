baac67658a1165938f1fe48ec0be0557efc0d8d7
Terrence Cole <terrence@mozilla.com>
2011-12-21 18:20:17 -0800

    Bug 712519 - Update test file encodings; r=rwalden
    Since we switched to using JS_CompileUTF8File in the shell, all of our tests
    that include comments with non-UTF-8 encoded characters fail.  This patch
    replaces these characters with their UTF-8 equivalents.
    
    --HG--
    extra : rebase_source : 4090e824320a329e989cffb61b3eb3c421a1bbd7

--- js/src/tests/ecma_3/Unicode/uc-005.js
+++ js/src/tests/ecma_3/Unicode/uc-005.js
@@ -47,7 +47,7 @@
  *    js> function f\u02B1 () {}
  *
  *    js> f\u02B1.toSource();
- *    function f¦() {}
+ *    function fÂ¦() {}
  *
  *    js> f\u02B1.toSource().toSource();
  *    (new String("function f\xB1() {}"))
@@ -58,7 +58,7 @@
  *
  *    js> f\u02B1.toString();
  *
- *    function f¦() {
+ *    function fÂ¦() {
  *    }
  *
  *    js> f\u02B1.toString().toSource();
