40070013015832bbcc4619f0d2bdad699456f7a0
Markus Stange <mstange@themasta.com>
2011-12-23 22:52:22 -0500

    Bug 598482 part 11 - Set up a connection between the view manager and the refresh driver.  r=roc

--- view/src/nsViewManager.h
+++ view/src/nsViewManager.h
@@ -144,13 +144,15 @@ public:
   /* Update the cached RootViewManager pointer on this view manager. */
   void InvalidateHierarchy();
 
+  virtual void ProcessPendingUpdates();
+
 protected:
   virtual ~nsViewManager();
 
 private:
 
   void FlushPendingInvalidates();
-  void ProcessPendingUpdates(nsView *aView, bool aDoInvalidate);
+  void ProcessPendingUpdatesForView(nsView *aView, bool aDoInvalidate);
   void FlushDirtyRegionToWidget(nsView* aView);
   /**
    * Call WillPaint() on all view observers under this vm root.
