9f1345a07ac7b9669c53047216a7db39782996a2
Vivien Nicolas <21@vingtetun.org>
2011-12-26 15:07:41 +0100

    Bug 711358 - [b2g] Add indexedDB-unlimited permission to pre-installed apps r=cjones

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -76,7 +76,9 @@ function startupHttpd(baseDir, port) {
 // until we have a proper security model, add some rights to
 // the pre-installed web applications 
 function addPermissions(urls) {
-  let permissions = ['indexedDB', 'webapps-manage', 'offline-app'];
+  let permissions = [
+    'indexedDB', 'indexedDB-unlimited', 'webapps-manage', 'offline-app'
+  ];
   urls.forEach(function(url) {
     let uri = Services.io.newURI(url, null, null);
     let allow = Ci.nsIPermissionManager.ALLOW_ACTION;
