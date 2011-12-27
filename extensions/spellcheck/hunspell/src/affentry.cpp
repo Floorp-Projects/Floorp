b5892eb4a8b3624acfc309b6984783b0a095b47e
Ehsan Akhgari <ehsan@mozilla.com>
2011-12-27 13:53:57 -0800

    Bug 694002 - Properly initialize things to fix crash in AffixMgr::~AffixMgr. r=smaug

--- extensions/spellcheck/hunspell/src/affentry.cpp
+++ extensions/spellcheck/hunspell/src/affentry.cpp
@@ -467,7 +467,9 @@ SfxEntry::SfxEntry(AffixMgr * pmgr, affentry* dp)
     memcpy(c.l.conds1, dp->c.l.conds1, MAXCONDLEN_1);
     c.l.conds2 = dp->c.l.conds2;
   } else memcpy(c.conds, dp->c.conds, MAXCONDLEN);
-
+  next = NULL;
+  nextne = NULL;
+  nexteq = NULL;
   rappnd = myrevstrdup(appnd);
   morphcode = dp->morphcode;
   contclass = dp->contclass;
