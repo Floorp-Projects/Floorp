4440067bf4283ffc34aec98c8b589884cb7a2b2d
Patrick Walton <pwalton@mozilla.com>
2011-12-29 15:10:30 -0800

    Bug 709492 - Part 4: Hide the page when navigating, and expose it when the new page appears. r=kats

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -200,6 +200,7 @@ var BrowserApp = {
     Services.obs.addObserver(this, "Viewport:Change", false);
     Services.obs.addObserver(this, "AgentMode:Change", false);
     Services.obs.addObserver(this, "SearchEngines:Get", false);
+    Services.obs.addObserver(this, "document-shown", false);
 
     function showFullScreenWarning() {
       NativeWindow.toast.show(Strings.browser.GetStringFromName("alertFullScreenToast"), "short");
@@ -715,6 +716,20 @@ var BrowserApp = {
       ViewportHandler.onResize();
     } else if (aTopic == "SearchEngines:Get") {
       this.getSearchEngines();
+    } else if (aTopic == "document-shown") {
+      let tab = this.selectedTab;
+      if (tab.browser.contentDocument != aSubject) {
+        return;
+      }
+
+      ViewportHandler.updateMetadata(tab);
+
+      // Unsuppress drawing unless the page was being thawed from the bfcache (which is an atomic
+      // operation, so there is no drawing to suppress).
+      if (tab.suppressDrawing) {
+        tab.sendExposeEvent();
+        tab.suppressDrawing = false;
+      }
     }
   },
 
@@ -722,7 +737,7 @@ var BrowserApp = {
     delete this.defaultBrowserWidth;
     let width = Services.prefs.getIntPref("browser.viewport.desktopWidth");
     return this.defaultBrowserWidth = width;
-   }
+  }
 };
 
 var NativeWindow = {
@@ -1194,6 +1209,12 @@ Tab.prototype = {
     this.browser.addEventListener("PluginClickToPlay", this, true);
     this.browser.addEventListener("pagehide", this, true);
 
+    let chromeEventHandler = this.browser.contentWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+                                                       .getInterface(Ci.nsIWebNavigation)
+                                                       .QueryInterface(Ci.nsIDocShell)
+                                                       .chromeEventHandler;
+    chromeEventHandler.addEventListener("DOMWindowCreated", this, false);
+
     Services.obs.addObserver(this, "http-on-modify-request", false);
 
     if (!aParams.delayLoad) {
@@ -1403,8 +1424,7 @@ Tab.prototype = {
       return;
     sendMessageToJava({
       gecko: {
-        type: "Viewport:Update",
-        viewport: JSON.stringify(this.viewport)
+        type: "Viewport:UpdateAndDraw"
       }
     });
   },
@@ -1545,6 +1565,18 @@ Tab.prototype = {
           this._pluginsToPlay = [];
           this._pluginOverlayShowing = false;
         }
+        break;
+      }
+
+      case "DOMWindowCreated": {
+        // Conveniently, this call to getBrowserForDocument() will return null if the document is
+        // not the top-level content document of the browser.
+        let browser = BrowserApp.getBrowserForDocument(aEvent.originalTarget);
+        if (!browser)
+          break;
+
+        let tab = BrowserApp.getTabForBrowser(browser);
+        tab.suppressDrawing = true;
         break;
       }
     }
@@ -1804,6 +1836,16 @@ Tab.prototype = {
       if (this.agentMode == UA_MODE_DESKTOP)
         channel.setRequestHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:7.0.1) Gecko/20100101 Firefox/7.0.1", false);
     }
+  },
+
+  sendExposeEvent: function() {
+    // Now that the document is actually on the screen, send an expose event.
+    this._timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+    sendMessageToJava({
+      gecko: {
+        type: "Viewport:Expose"
+      }
+    });
   },
 
   QueryInterface: XPCOMUtils.generateQI([
