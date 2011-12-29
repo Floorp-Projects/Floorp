6e871b27408eeaefc2b70ddb5dfdb08a7767c6c2
Igor Bukanov <igor@mir2.org>
2011-12-29 22:22:21 +0100

    bug 714066 - Missed FreeChunkList call in JSRuntime::onOutOfMemory. r=wmccloskey

--- js/src/jsgc.h
+++ js/src/jsgc.h
@@ -808,6 +808,9 @@ class ChunkPool {
      */
     Chunk *expire(JSRuntime *rt, bool releaseAll);
 
+    /* Must be called with the GC lock taken. */
+    void expireAndFree(JSRuntime *rt, bool releaseAll);
+
     /* Must be called either during the GC or with the GC lock taken. */
     JS_FRIEND_API(int64_t) countCleanDecommittedArenas(JSRuntime *rt);
 };
