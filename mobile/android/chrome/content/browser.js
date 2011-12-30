adff481cc235b329a46500f036764b7f618de20f
Mark Finkle <mfinkle@mozilla.com>
2011-12-29 23:35:53 -0500

    Bug 713887 - Javascript error when using the menu quit to quit fennec r=mbrubeck

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -438,7 +438,6 @@ var BrowserApp = {
   },
 
   quit: function quit() {
-      Cu.reportError("got quit quit message");
       window.QueryInterface(Ci.nsIDOMChromeWindow).minimize();
       window.close();
   },
