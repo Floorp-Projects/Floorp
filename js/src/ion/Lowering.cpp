0cfd10683dc4ce938bf191e0ab1cb667607f5219
Sean Stangl <sstangl@mozilla.com>
2011-12-30 17:13:55 -0800

    Ensure distinct objreg and ArgumentsRectifierReg in visitCallGeneric(). (No bug, r=dvander)

--- js/src/ion/Lowering.cpp
+++ js/src/ion/Lowering.cpp
@@ -153,7 +153,7 @@ LIRGenerator::visitCall(MCall *call)
     // stored in an argument vector. Therefore visitCall() may be generic.
     LCallGeneric *ins = new LCallGeneric(useRegister(call->getFunction()),
                                          argslot,
-                                         temp(LDefinition::GENERAL),
+                                         tempFixed(ArgumentsRectifierReg),
                                          temp(LDefinition::GENERAL));
     if (!defineReturn(ins, call))
         return false;
