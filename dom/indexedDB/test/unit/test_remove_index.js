ed1b218fda8bb7dad4332a06378793a19eb8b718
Gabor Krizsanits <gkrizsanits@mozilla.com>
2012-01-03 10:31:12 -0500

    Bug 587797 - IndexedDB: Tests for making it possible to access IndexedDB APIs from chrome. r=khuey

---
+++ dom/indexedDB/test/unit/test_remove_index.js
@@ -0,0 +1,57 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+var testGenerator = testSteps();
+
+function testSteps()
+{
+  const nsIIDBObjectStore = Components.interfaces.nsIIDBObjectStore;
+
+  const name = this.window ? window.location.pathname : "Splendid Test";
+  const description = "My Test Database";
+  const indexName = "My Test Index";
+
+  let request = mozIndexedDB.open(name, 1, description);
+  request.onerror = errorHandler;
+  request.onupgradeneeded = grabEventAndContinueHandler;
+  let event = yield;
+
+  let db = event.target.result;
+  is(db.objectStoreNames.length, 0, "Correct objectStoreNames list");
+
+  let objectStore = db.createObjectStore("test store", { keyPath: "foo" });
+  is(db.objectStoreNames.length, 1, "Correct objectStoreNames list");
+  is(db.objectStoreNames.item(0), objectStore.name, "Correct name");
+
+  is(objectStore.indexNames.length, 0, "Correct indexNames list");
+
+  let index = objectStore.createIndex(indexName, "foo");
+
+  is(objectStore.indexNames.length, 1, "Correct indexNames list");
+  is(objectStore.indexNames.item(0), indexName, "Correct name");
+  is(objectStore.index(indexName), index, "Correct instance");
+
+  objectStore.deleteIndex(indexName);
+
+  is(objectStore.indexNames.length, 0, "Correct indexNames list");
+  try {
+    objectStore.index(indexName);
+    ok(false, "should have thrown");
+  }
+  catch(ex) {
+    ok(ex instanceof IDBDatabaseException, "Got a IDBDatabaseException");
+    is(ex.code, IDBDatabaseException.NOT_FOUND_ERR, "expect a NOT_FOUND_ERR");
+  }
+
+  let index2 = objectStore.createIndex(indexName, "foo");
+  isnot(index, index2, "New instance should be created");
+
+  is(objectStore.indexNames.length, 1, "Correct recreacted indexNames list");
+  is(objectStore.indexNames.item(0), indexName, "Correct recreacted name");
+  is(objectStore.index(indexName), index2, "Correct instance");
+
+  finishTest();
+  yield;
+}
---
+++ dom/indexedDB/test/unit/test_writer_starvation.js
@@ -0,0 +1,95 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+var testGenerator = testSteps();
+
+function testSteps()
+{
+  const READ_ONLY = Components.interfaces.nsIIDBTransaction.READ_ONLY;
+  const READ_WRITE = Components.interfaces.nsIIDBTransaction.READ_WRITE;
+  const VERSION_CHANGE =
+    Components.interfaces.nsIIDBTransaction.VERSION_CHANGE;
+
+  const name = this.window ? window.location.pathname : "Splendid Test";
+  const description = "My Test Database";
+
+  let request = mozIndexedDB.open(name, 1, description);
+  request.onerror = errorHandler;
+  request.onupgradeneeded = grabEventAndContinueHandler;
+  request.onsuccess = grabEventAndContinueHandler;
+  let event = yield;
+
+  let db = event.target.result;
+
+  is(event.target.transaction.mode, VERSION_CHANGE, "Correct mode");
+
+  let objectStore = db.createObjectStore("foo", { autoIncrement: true });
+
+  request = objectStore.add({});
+  request.onerror = errorHandler;
+  request.onsuccess = grabEventAndContinueHandler;
+  event = yield;
+
+  let key = event.target.result;
+  ok(key, "Got a key");
+
+  yield;
+
+  let continueReading = true;
+  let readerCount = 0;
+  let callbackCount = 0;
+  let finalCallbackCount = 0;
+
+  // Generate a bunch of reads right away without returning to the event
+  // loop.
+  for (let i = 0; i < 20; i++) {
+    readerCount++;
+    request = db.transaction("foo").objectStore("foo").get(key);
+    request.onerror = errorHandler;
+    request.onsuccess = function(event) {
+      callbackCount++;
+    };
+  }
+
+  while (continueReading) {
+    readerCount++;
+    request = db.transaction("foo").objectStore("foo").get(key);
+    request.onerror = errorHandler;
+    request.onsuccess = function(event) {
+      is(event.target.transaction.mode, READ_ONLY, "Correct mode");
+      callbackCount++;
+      if (callbackCount == 100) {
+        request = db.transaction("foo", READ_WRITE)
+                    .objectStore("foo")
+                    .add({}, readerCount);
+        request.onerror = errorHandler;
+        request.onsuccess = function(event) {
+          continueReading = false;
+          finalCallbackCount = callbackCount;
+          is(event.target.result, callbackCount,
+             "write callback came before later reads");
+        }
+      }
+    };
+
+    executeSoon(function() { testGenerator.next(); });
+    yield;
+  }
+
+  while (callbackCount < readerCount) {
+    executeSoon(function() { testGenerator.next(); });
+    yield;
+  }
+
+  is(callbackCount, readerCount, "All requests accounted for");
+  ok(callbackCount > finalCallbackCount, "More readers after writer");
+
+  finishTest();
+  yield;
+}
+
+if (this.window)
+  SimpleTest.requestLongerTimeout(5); // see bug 580875
+
