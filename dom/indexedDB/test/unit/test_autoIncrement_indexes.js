91b6eefb6b16b9ab1878ad36c38247342994c686
Gabor Krizsanits <gkrizsanits@mozilla.com>
2012-01-04 10:43:33 -0500

    Bug 587797 - Tests. r=khuey

---
+++ dom/indexedDB/test/unit/test_autoIncrement_indexes.js
@@ -0,0 +1,56 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+var testGenerator = testSteps();
+
+function testSteps()
+{
+  let request = mozIndexedDB.open(this.window ? window.location.pathname : "Splendid Test", 1);
+  request.onerror = errorHandler;
+  request.onupgradeneeded = grabEventAndContinueHandler;
+  let event = yield;
+
+  let db = request.result;
+  db.onerror = errorHandler;
+
+  let objectStore = db.createObjectStore("foo", { keyPath: "id",
+                                                  autoIncrement: true });
+  objectStore.createIndex("first","first");
+  objectStore.createIndex("second","second");
+  objectStore.createIndex("third","third");
+
+  let data = { first: "foo", second: "foo", third: "foo" };
+
+  objectStore.add(data).onsuccess = grabEventAndContinueHandler;
+  event = yield;
+
+  is(event.target.result, 1, "Added entry");
+  request.onsuccess = grabEventAndContinueHandler;
+
+  event = yield;
+
+  let objectStore = db.transaction("foo").objectStore("foo");
+  let first = objectStore.index("first");
+  let second = objectStore.index("second");
+  let third = objectStore.index("third");
+
+  first.get("foo").onsuccess = grabEventAndContinueHandler;
+  event = yield;
+
+  is (event.target.result.id, 1, "Entry in first");
+
+  second.get("foo").onsuccess = grabEventAndContinueHandler;
+  event = yield;
+
+  is (event.target.result.id, 1, "Entry in second");
+
+  third.get("foo").onsuccess = grabEventAndContinueHandler;
+  event = yield;
+
+  is (event.target.result.id, 1, "Entry in third");
+
+  finishTest();
+  yield;
+}
---
+++ dom/indexedDB/test/unit/test_odd_result_order.js
@@ -0,0 +1,76 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+var testGenerator = testSteps();
+
+function testSteps()
+{
+  const data = { key: 5, index: 10 };
+
+  let request = mozIndexedDB.open(this.window ? window.location.pathname : "Splendid Test", 1);
+  request.onerror = errorHandler;
+  request.onupgradeneeded = grabEventAndContinueHandler;
+  let event = yield;
+
+  let db = event.target.result;
+
+  ok(db instanceof IDBDatabase, "Got a real database");
+
+  db.onerror = errorHandler;
+
+  let objectStore = db.createObjectStore("foo", { keyPath: "key",
+                                                  autoIncrement: true });
+  let index = objectStore.createIndex("foo", "index");
+
+  event.target.onsuccess = continueToNextStep;
+  yield;
+
+  objectStore = db.transaction("foo", IDBTransaction.READ_WRITE)
+                  .objectStore("foo");
+  request = objectStore.add(data);
+  request.onsuccess = grabEventAndContinueHandler;
+  event = yield;
+
+  let key;
+  executeSoon(function() {
+    key = request.result;
+    continueToNextStep();
+  });
+  yield;
+
+  is(key, data.key, "Got the right key");
+
+  objectStore = db.transaction("foo").objectStore("foo");
+  objectStore.get(data.key).onsuccess = grabEventAndContinueHandler;
+  event = yield;
+
+  let obj;
+  executeSoon(function() {
+    obj = event.target.result;
+    continueToNextStep();
+  });
+  yield;
+
+  is(obj.key, data.key, "Got the right key");
+  is(obj.index, data.index, "Got the right property value");
+
+  objectStore = db.transaction("foo", IDBTransaction.READ_WRITE)
+                  .objectStore("foo");
+  request = objectStore.delete(data.key);
+  request.onsuccess = grabEventAndContinueHandler;
+  event = yield;
+
+  key = undefined;
+  executeSoon(function() {
+    key = request.result;
+    continueToNextStep();
+  }, 0);
+  yield;
+
+  ok(key === undefined, "Got the right value");
+
+  finishTest();
+  yield;
+}
