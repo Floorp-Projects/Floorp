c9ddb36599a4b244c73fbd9d8ec748f533c2f95d
Patrick Walton <pwalton@mozilla.com>
2012-01-06 16:42:47 -0800

    Bug 708746 - Part 4: Remove viewport metadata updates that occur after the document is shown. r=mbrubeck

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -2869,18 +2869,12 @@ var ViewportHandler = {
   _nextDocumentId: 0,
 
   init: function init() {
-    addEventListener("DOMWindowCreated", this, false);
     addEventListener("DOMMetaAdded", this, false);
-    addEventListener("DOMContentLoaded", this, false);
-    addEventListener("pageshow", this, false);
     addEventListener("resize", this, false);
   },
 
   uninit: function uninit() {
-    removeEventListener("DOMWindowCreated", this, false);
     removeEventListener("DOMMetaAdded", this, false);
-    removeEventListener("DOMContentLoaded", this, false);
-    removeEventListener("pageshow", this, false);
     removeEventListener("resize", this, false);
   },
 
@@ -2893,18 +2887,9 @@ var ViewportHandler = {
       return;
 
     switch (aEvent.type) {
-      case "DOMWindowCreated":
-        this.resetMetadata(tab);
-        break;
-
       case "DOMMetaAdded":
         if (target.name == "viewport")
           this.updateMetadata(tab);
-        break;
-
-      case "DOMContentLoaded":
-      case "pageshow":
-        this.updateMetadata(tab);
         break;
 
       case "resize":
