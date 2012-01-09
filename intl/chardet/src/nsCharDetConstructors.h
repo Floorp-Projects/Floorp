394977e69a3dc7e45924954b6a93ac2a22cecdc7
Henri Sivonen <hsivonen@iki.fi>
2012-01-09 14:19:51 +0200

    Bug 655255 - Remove nsICharsetDetectionAdaptor from the tree. r=smontagu.

--- intl/chardet/src/nsCharDetConstructors.h
+++ intl/chardet/src/nsCharDetConstructors.h
@@ -49,13 +49,11 @@
 #include "nsICharsetDetector.h"
 #include "nsICharsetAlias.h"
 #include "nsDocumentCharsetInfo.h"
-#include "nsICharsetDetectionAdaptor.h"
 #include "nsICharsetDetectionObserver.h"
 #include "nsIStringCharsetDetector.h"
 #include "nsCyrillicDetector.h"
 #include "nsDocumentCharsetInfoCID.h"
 #include "nsXMLEncodingCID.h"
-#include "nsCharsetDetectionAdaptorCID.h"
 
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsDocumentCharsetInfo)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsRUProbDetector)
