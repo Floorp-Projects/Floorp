572910107def17d3994a38d1c26f393c9fddc1b5
Joe Drew <joe@drew.ca>
2012-01-09 16:50:01 -0500

    Bug 715652 - Implement significantly more of the 2D API for the Cairo backend. r=jrmuizel

--- gfx/2d/DrawTargetCairo.h
+++ gfx/2d/DrawTargetCairo.h
@@ -40,8 +40,35 @@
 #include "2D.h"
 #include "cairo.h"
 
+#include <vector>
+
 namespace mozilla {
 namespace gfx {
+
+class SourceSurfaceCairo;
+
+class GradientStopsCairo : public GradientStops
+{
+  public:
+    GradientStopsCairo(GradientStop* aStops, uint32_t aNumStops)
+    {
+      for (uint32_t i = 0; i < aNumStops; ++i) {
+        mStops.push_back(aStops[i]);
+      }
+    }
+
+    virtual ~GradientStopsCairo() {}
+
+    const std::vector<GradientStop>& GetStops() const
+    {
+      return mStops;
+    }
+
+    virtual BackendType GetBackendType() const { return BACKEND_CAIRO; }
+
+  private:
+    std::vector<GradientStop> mStops;
+};
 
 class DrawTargetCairo : public DrawTarget
 {
@@ -51,7 +78,7 @@ public:
 
   virtual BackendType GetType() const { return BACKEND_CAIRO; }
   virtual TemporaryRef<SourceSurface> Snapshot();
-  virtual IntSize GetSize() { return IntSize(); }
+  virtual IntSize GetSize();
 
   virtual void Flush();
   virtual void DrawSurface(SourceSurface *aSurface,
@@ -64,16 +91,13 @@ public:
                                      const Color &aColor,
                                      const Point &aOffset,
                                      Float aSigma,
-                                     CompositionOp aOperator)
-  { }
+                                     CompositionOp aOperator);
 
-  virtual void ClearRect(const Rect &aRect)
-  { }
+  virtual void ClearRect(const Rect &aRect);
 
   virtual void CopySurface(SourceSurface *aSurface,
                            const IntRect &aSourceRect,
-                           const IntPoint &aDestination)
-  { }
+                           const IntPoint &aDestination);
 
   virtual void FillRect(const Rect &aRect,
                         const Pattern &aPattern,
@@ -81,41 +105,37 @@ public:
   virtual void StrokeRect(const Rect &aRect,
                           const Pattern &aPattern,
                           const StrokeOptions &aStrokeOptions = StrokeOptions(),
-                          const DrawOptions &aOptions = DrawOptions())
-  { return; }
+                          const DrawOptions &aOptions = DrawOptions());
   virtual void StrokeLine(const Point &aStart,
                           const Point &aEnd,
                           const Pattern &aPattern,
                           const StrokeOptions &aStrokeOptions = StrokeOptions(),
-                          const DrawOptions &aOptions = DrawOptions())
-  { return; }
+                          const DrawOptions &aOptions = DrawOptions());
 
   virtual void Stroke(const Path *aPath,
                       const Pattern &aPattern,
                       const StrokeOptions &aStrokeOptions = StrokeOptions(),
-                      const DrawOptions &aOptions = DrawOptions())
-  { return; }
+                      const DrawOptions &aOptions = DrawOptions());
 
   virtual void Fill(const Path *aPath,
                     const Pattern &aPattern,
-                    const DrawOptions &aOptions = DrawOptions())
-  { return; }
+                    const DrawOptions &aOptions = DrawOptions());
 
   virtual void FillGlyphs(ScaledFont *aFont,
                           const GlyphBuffer &aBuffer,
                           const Pattern &aPattern,
-                          const DrawOptions &aOptions)
-  { return; }
+                          const DrawOptions &aOptions);
   virtual void Mask(const Pattern &aSource,
                     const Pattern &aMask,
                     const DrawOptions &aOptions = DrawOptions())
-  { return; }
+  { }
 
-  virtual void PushClip(const Path *aPath) { }
-  virtual void PushClipRect(const Rect &aRect) { }
-  virtual void PopClip() { }
+  virtual void PushClip(const Path *aPath);
+  virtual void PushClipRect(const Rect &aRect)
+  { }
+  virtual void PopClip();
 
-  virtual TemporaryRef<PathBuilder> CreatePathBuilder(FillRule aFillRule = FILL_WINDING) const { return NULL; }
+  virtual TemporaryRef<PathBuilder> CreatePathBuilder(FillRule aFillRule = FILL_WINDING) const;
 
   virtual TemporaryRef<SourceSurface> CreateSourceSurfaceFromData(unsigned char *aData,
                                                             const IntSize &aSize,
@@ -125,22 +145,43 @@ public:
   virtual TemporaryRef<SourceSurface>
     CreateSourceSurfaceFromNativeSurface(const NativeSurface &aSurface) const;
   virtual TemporaryRef<DrawTarget>
-    CreateSimilarDrawTarget(const IntSize &aSize, SurfaceFormat aFormat) const
-  { return NULL; }
+    CreateSimilarDrawTarget(const IntSize &aSize, SurfaceFormat aFormat) const;
 
-  virtual TemporaryRef<GradientStops> CreateGradientStops(GradientStop *aStops, uint32_t aNumStops, ExtendMode aExtendMode = EXTEND_CLAMP) const
-  { return NULL; }
+  virtual TemporaryRef<GradientStops>
+    CreateGradientStops(GradientStop *aStops,
+                        uint32_t aNumStops,
+                        ExtendMode aExtendMode = EXTEND_CLAMP) const;
 
-  virtual void *GetNativeSurface(NativeSurfaceType aType)
-  { return NULL; }
-
-  virtual void SetTransform(const Matrix& aTransform);
+  virtual void *GetNativeSurface(NativeSurfaceType aType);
 
   bool Init(cairo_surface_t* aSurface);
 
-private:
+private: // methods
+  void PrepareForDrawing(cairo_t* aContext);
 
+  enum DrawPatternType { DRAW_FILL, DRAW_STROKE };
+  void DrawPattern(const Rect& aRect,
+                   const Pattern& aPattern,
+                   const StrokeOptions& aStrokeOptions,
+                   const DrawOptions& aOptions,
+                   DrawPatternType aDrawType);
+
+  // Copy-on-write support for snapshot surfaces.
+  friend class SourceSurfaceCairo;
+  void AppendSnapshot(SourceSurfaceCairo* aSnapshot);
+  void RemoveSnapshot(SourceSurfaceCairo* aSnapshot);
+
+  // Call before you make any changes to the backing surface with which this
+  // context is associated.
+  void MarkChanged();
+
+  // Call if there is any reason to disassociate all snapshots from this draw
+  // target; for example, because we're going to be destroyed.
+  void MarkSnapshotsIndependent();
+
+private: // data
   cairo_t* mContext;
+  std::vector<SourceSurfaceCairo*> mSnapshots;
 };
 
 }
--- gfx/2d/SourceSurfaceCairo.cpp
+++ gfx/2d/SourceSurfaceCairo.cpp
@@ -36,18 +36,76 @@
  * ***** END LICENSE BLOCK ***** */
 
 #include "SourceSurfaceCairo.h"
+#include "DrawTargetCairo.h"
 
 #include "cairo.h"
 
 namespace mozilla {
 namespace gfx {
 
-SourceSurfaceCairo::SourceSurfaceCairo()
+static cairo_format_t
+GfxFormatToCairoFormat(SurfaceFormat format)
 {
+  switch (format)
+  {
+    case FORMAT_B8G8R8A8:
+      return CAIRO_FORMAT_ARGB32;
+    case FORMAT_B8G8R8X8:
+      return CAIRO_FORMAT_RGB24;
+    case FORMAT_A8:
+      return CAIRO_FORMAT_A8;
+  }
+
+  return CAIRO_FORMAT_ARGB32;
+}
+
+static SurfaceFormat
+CairoFormatToSurfaceFormat(cairo_format_t format)
+{
+  switch (format)
+  {
+    case CAIRO_FORMAT_ARGB32:
+      return FORMAT_B8G8R8A8;
+    case CAIRO_FORMAT_RGB24:
+      return FORMAT_B8G8R8X8;
+    case CAIRO_FORMAT_A8:
+      return FORMAT_A8;
+  }
+
+  return FORMAT_B8G8R8A8;
+}
+
+static cairo_content_t
+GfxFormatToCairoContent(SurfaceFormat format)
+{
+  switch(format)
+  {
+    case FORMAT_B8G8R8A8:
+      return CAIRO_CONTENT_COLOR_ALPHA;
+    case FORMAT_B8G8R8X8:
+      return CAIRO_CONTENT_COLOR;
+    case FORMAT_A8:
+      return CAIRO_CONTENT_ALPHA;
+  }
+
+  return CAIRO_CONTENT_COLOR_ALPHA;
+}
+
+SourceSurfaceCairo::SourceSurfaceCairo(cairo_surface_t* aSurface,
+                                       const IntSize& aSize,
+                                       const SurfaceFormat& aFormat,
+                                       DrawTargetCairo* aDrawTarget /* = NULL */)
+ : mSize(aSize)
+ , mFormat(aFormat)
+ , mSurface(aSurface)
+ , mDrawTarget(aDrawTarget)
+{
+  cairo_surface_reference(mSurface);
 }
 
 SourceSurfaceCairo::~SourceSurfaceCairo()
 {
+  MarkIndependent();
   cairo_surface_destroy(mSurface);
 }
 
@@ -66,26 +124,108 @@ SourceSurfaceCairo::GetFormat() const
 TemporaryRef<DataSourceSurface>
 SourceSurfaceCairo::GetDataSurface()
 {
-  return NULL;
+  RefPtr<DataSourceSurfaceCairo> dataSurf;
+
+  if (cairo_surface_get_type(mSurface) == CAIRO_SURFACE_TYPE_IMAGE) {
+    dataSurf = new DataSourceSurfaceCairo(mSurface);
+  } else {
+    cairo_surface_t* imageSurf = cairo_image_surface_create(GfxFormatToCairoFormat(mFormat),
+                                                            mSize.width, mSize.height);
+
+    // Fill the new image surface with the contents of our surface.
+    cairo_t* ctx = cairo_create(imageSurf);
+    cairo_pattern_t* pat = cairo_pattern_create_for_surface(mSurface);
+    cairo_set_source(ctx, pat);
+    cairo_paint(ctx);
+    cairo_destroy(ctx);
+
+    dataSurf = new DataSourceSurfaceCairo(imageSurf);
+    cairo_surface_destroy(imageSurf);
+  }
+
+  return dataSurf;
 }
 
 cairo_surface_t*
-SourceSurfaceCairo::GetSurface()
+SourceSurfaceCairo::GetSurface() const
 {
   return mSurface;
 }
 
-bool
-SourceSurfaceCairo::InitFromSurface(cairo_surface_t* aSurface,
-                                    const IntSize& aSize,
-                                    const SurfaceFormat& aFormat)
+void
+SourceSurfaceCairo::DrawTargetWillChange()
 {
-  mSurface = aSurface;
-  cairo_surface_reference(mSurface);
-  mSize = aSize;
-  mFormat = aFormat;
+  if (mDrawTarget) {
+    mDrawTarget = NULL;
 
-  return true;
+    // We're about to lose our version of the surface, so make a copy of it.
+    cairo_surface_t* surface = cairo_surface_create_similar(mSurface,
+                                                            GfxFormatToCairoContent(mFormat),
+                                                            mSize.width, mSize.height);
+    cairo_t* ctx = cairo_create(surface);
+    cairo_pattern_t* pat = cairo_pattern_create_for_surface(mSurface);
+    cairo_set_source(ctx, pat);
+    cairo_paint(ctx);
+    cairo_destroy(ctx);
+
+    // Swap in this new surface.
+    cairo_surface_destroy(mSurface);
+    mSurface = surface;
+  }
+}
+
+void
+SourceSurfaceCairo::MarkIndependent()
+{
+  if (mDrawTarget) {
+    mDrawTarget->RemoveSnapshot(this);
+    mDrawTarget = NULL;
+  }
+}
+
+DataSourceSurfaceCairo::DataSourceSurfaceCairo(cairo_surface_t* imageSurf)
+ : mImageSurface(imageSurf)
+{
+  cairo_surface_reference(mImageSurface);
+}
+
+DataSourceSurfaceCairo::~DataSourceSurfaceCairo()
+{
+  cairo_surface_destroy(mImageSurface);
+}
+
+unsigned char *
+DataSourceSurfaceCairo::GetData()
+{
+  return cairo_image_surface_get_data(mImageSurface);
+}
+
+int32_t
+DataSourceSurfaceCairo::Stride()
+{
+  return cairo_image_surface_get_stride(mImageSurface);
+}
+
+IntSize
+DataSourceSurfaceCairo::GetSize() const
+{
+  IntSize size;
+  size.width = cairo_image_surface_get_width(mImageSurface);
+  size.height = cairo_image_surface_get_height(mImageSurface);
+
+  return size;
+}
+
+SurfaceFormat
+DataSourceSurfaceCairo::GetFormat() const
+{
+  return CairoFormatToSurfaceFormat(cairo_image_surface_get_format(mImageSurface));
+}
+
+cairo_surface_t*
+DataSourceSurfaceCairo::GetSurface() const
+{
+  return mImageSurface;
 }
 
 }
