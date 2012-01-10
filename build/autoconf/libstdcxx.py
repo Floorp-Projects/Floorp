fba1d057c9c4f9c75f21f64544893b3de2978881
Rafael Ávila de Espíndola <respindola@mozilla.com>
2012-01-10 15:01:03 -0500

    Bug 715930 - Use stdc++compat.cpp on OS X too. r=:glandium.

--- build/autoconf/libstdcxx.py
+++ build/autoconf/libstdcxx.py
@@ -12,6 +12,7 @@
 import os
 import subprocess
 import re
+import sys
 
 re_for_ld = re.compile('.*\((.*)\).*')
 
@@ -67,7 +68,19 @@ def find_version(e):
     return encode_ver(last_version)
 
 if __name__ == '__main__':
-    cxx_env = os.environ['CXX']
-    print 'MOZ_LIBSTDCXX_TARGET_VERSION=%s' % find_version(cxx_env)
-    host_cxx_env = os.environ.get('HOST_CXX', cxx_env)
-    print 'MOZ_LIBSTDCXX_HOST_VERSION=%s' % find_version(host_cxx_env)
+    if os.uname()[0] == 'Darwin':
+        sdk_dir = os.environ['MACOS_SDK_DIR']
+        if 'MacOSX10.5.sdk' in sdk_dir:
+            target_ver = 0
+            host_ver = 0
+        else:
+            target_ver = encode_ver('3.4.9')
+            host_ver = encode_ver('3.4.9')
+    else:
+        cxx_env = os.environ['CXX']
+        target_ver = find_version(cxx_env)
+        host_cxx_env = os.environ.get('HOST_CXX', cxx_env)
+        host_ver = find_version(host_cxx_env)
+
+    print 'MOZ_LIBSTDCXX_TARGET_VERSION=%s' % target_ver
+    print 'MOZ_LIBSTDCXX_HOST_VERSION=%s' % host_ver
