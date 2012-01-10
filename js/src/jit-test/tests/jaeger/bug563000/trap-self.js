5332089db1d6d0a7deb63e3e298a40233f764abb
David Anderson <danderson@mozilla.com>
2012-01-10 15:01:29 -0800

    Complete merge from mozilla-central.

--- js/src/jit-test/tests/jaeger/bug563000/trap-self.js
+++ js/src/jit-test/tests/jaeger/bug563000/trap-self.js
@@ -3,7 +3,7 @@ setDebug(true);
 x = "notset";
 function main() {
   /* The JSOP_STOP in main. */
-  trap(main, 28, "success()");
+  trap(main, 29, "success()");
   x = "failure";
 }
 function success() { x = "success"; }
