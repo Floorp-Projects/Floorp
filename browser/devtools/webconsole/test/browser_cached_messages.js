92c24a547c4725c61db8ba40798c1706a49bdf0b
Mihai Sucan <mihai.sucan@gmail.com>
2012-01-11 12:51:49 +0200

    Bug 611032 - Errors from before the Web Console is opened do not show. r=gavin.sharp,rcampbell f=rcampbell

--- browser/devtools/webconsole/test/browser_cached_messages.js
+++ browser/devtools/webconsole/test/browser_cached_messages.js
@@ -36,29 +36,26 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-const TEST_URI = "data:text/html,<p>Web Console test for bug 609890";
+const TEST_URI = "http://example.com/browser/browser/devtools/webconsole/test/test-webconsole-error-observer.html";
 
 function test()
 {
   waitForExplicitFinish();
 
+  expectUncaughtException();
+
   gBrowser.selectedTab = gBrowser.addTab(TEST_URI);
 
-  gBrowser.selectedBrowser.addEventListener("load", testOpenUI, true);
+  gBrowser.selectedBrowser.addEventListener("load", function onLoad() {
+    gBrowser.selectedBrowser.removeEventListener("load", onLoad, true);
+    testOpenUI(true);
+  }, true);
 }
 
-function testOpenUI()
+function testOpenUI(aTestReopen)
 {
-  gBrowser.selectedBrowser.removeEventListener("load", testOpenUI, true);
-
   // test to see if the messages are
   // displayed when the console UI is opened
-
-  let console = content.wrappedJSObject.console;
-  console.log("log Bazzle");
-  console.info("info Bazzle");
-  console.warn("warn Bazzle");
-  console.error("error Bazzle");
 
   HUDService.activateHUDForContext(gBrowser.selectedTab);
   let hudId = HUDService.getHudIdByWindow(content);
@@ -68,7 +65,19 @@ function testOpenUI()
                "Find a console log entry from before console UI is opened",
                false, null);
 
+  testLogEntry(hud.outputNode, "error Bazzle",
+               "Find a console error entry from before console UI is opened",
+               false, null);
+
+  testLogEntry(hud.outputNode, "bazBug611032", "Found the JavaScript error");
+  testLogEntry(hud.outputNode, "cssColorBug611032", "Found the CSS error");
+
   HUDService.deactivateHUDForContext(gBrowser.selectedTab);
 
-  executeSoon(finish);
+  if (aTestReopen) {
+    HUDService.deactivateHUDForContext(gBrowser.selectedTab);
+    executeSoon(testOpenUI);
+  } else {
+    executeSoon(finish);
+  }
 }
