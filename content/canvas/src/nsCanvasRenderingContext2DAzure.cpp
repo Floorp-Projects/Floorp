8cea012b85f7fc5367f09d8907ef805569f961dc
Ed Morley <bmo@edmorley.co.uk>
2012-01-11 01:22:15 +0000

    Backout b9e154713763 (bug 702158) for pgo linux failures

--- content/canvas/src/nsCanvasRenderingContext2DAzure.cpp
+++ content/canvas/src/nsCanvasRenderingContext2DAzure.cpp
@@ -995,7 +995,7 @@ NS_NewCanvasRenderingContext2DAzure(nsIDOMCanvasRenderingContext2D** aResult)
       !Preferences::GetBool("gfx.canvas.azure.prefer-skia", false)) {
     return NS_ERROR_NOT_AVAILABLE;
   }
-#elif !defined(XP_MACOSX) && !defined(ANDROID) && !defined(XP_LINUX)
+#elif !defined(XP_MACOSX) && !defined(ANDROID)
   return NS_ERROR_NOT_AVAILABLE;
 #endif
 
