fefb174447687b578129e55a3d0c6c27508cbba0
David Anderson <danderson@mozilla.com>
2012-01-11 16:01:20 -0800

    Make sure phi inputs are held live and have unique stack slots (bug 716149, r=sstangl).

--- js/src/ion/GreedyAllocator.cpp
+++ js/src/ion/GreedyAllocator.cpp
@@ -741,6 +741,20 @@ GreedyAllocator::allocateInstruction(LBlock *block, LInstruction *ins)
     return true;
 }
 
+void
+GreedyAllocator::findLoopCarriedUses(LInstruction *ins, uint32 lowerBound, uint32 upperBound)
+{
+    for (size_t i = 0; i < ins->numOperands(); i++) {
+        LAllocation *a = ins->getOperand(i);
+        if (!a->isUse())
+            continue;
+        LUse *use = a->toUse();
+        VirtualRegister *vr = getVirtualRegister(use);
+        if (vr->def->virtualRegister() < lowerBound || vr->def->virtualRegister() > upperBound)
+            allocateStack(vr);
+    }
+}
+
 // Scan all instructions inside the loop. If any instruction has a use of a
 // definition that is defined outside its containing loop, then stack space
 // for that definition must be reserved ahead of time. Otherwise, we could
@@ -758,20 +772,10 @@ GreedyAllocator::findLoopCarriedUses(LBlock *backedge)
     for (size_t i = 0; i < mheader->numContainedInLoop(); i++) {
         LBlock *block = mheader->getContainedInLoop(i)->lir();
 
-        for (LInstructionIterator i = block->begin(); i != block->end(); i++) {
-            LInstruction *ins = *i;
-            for (size_t i = 0; i < ins->numOperands(); i++) {
-                LAllocation *a = ins->getOperand(i);
-                if (!a->isUse())
-                    continue;
-                LUse *use = a->toUse();
-                VirtualRegister *vr = getVirtualRegister(use);
-                if (vr->def->virtualRegister() < lowerBound ||
-                    vr->def->virtualRegister() > upperBound) {
-                    allocateStack(vr);
-                }
-            }
-        }
+        for (LInstructionIterator i = block->begin(); i != block->end(); i++)
+            findLoopCarriedUses(*i, lowerBound, upperBound);
+        for (size_t i = 0; i < block->numPhis(); i++)
+            findLoopCarriedUses(block->getPhi(i), lowerBound, upperBound);
     }
 
     IonSpew(IonSpew_RegAlloc, "  Done finding loop-carried uses.");
@@ -927,6 +931,18 @@ GreedyAllocator::allocateRegisters()
 
             killReg(vreg);
             vreg->unsetRegister();
+        }
+
+        // Before killing phis, ensure that each phi input has its own stack
+        // allocation. This ensures we won't allocate the same slot for any phi
+        // as its input, which technically may be legal (since the phi becomes
+        // the last use of the slot), but we avoid for sanity.
+        for (size_t i = 0; i < block->numPhis(); i++) {
+            LPhi *phi = block->getPhi(i);
+            for (size_t j = 0; j < phi->numOperands(); j++) {
+                VirtualRegister *in = getVirtualRegister(phi->getOperand(j)->toUse());
+                allocateStack(in);
+            }
         }
 
         // Kill phis.
