36391ff6b0830dd3b6434dffd694e718c46b9bd3
Asaf Romano <mano@mozilla.com>
2012-01-12 18:24:16 +0200

    Bug 713519 - getCharsetAlias should throw for unknown character sets. r=smontagu.

---
+++ intl/uconv/tests/unit/test_bug713519.js
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+function run_test() {
+  let svc =  Components.classes["@mozilla.org/charset-converter-manager;1"]
+                       .getService(Components.interfaces.nsICharsetConverterManager);
+
+  // Ensure normal calls to getCharsetAlias do work.
+  do_check_eq(svc.getCharsetAlias("Windows-1255"), "windows-1255");
+
+  try {
+    svc.getCharsetAlias("no such thing");
+    do_throw("Calling getCharsetAlias with invalid value should throw.");
+  }
+  catch (ex) {
+    // The exception is expected.
+  }
+
+  do_check_eq(svc.getCharsetTitle("windows-1255"), "Hebrew (Windows-1255)");
+  try {
+    svc.getCharsetTitle("no such thing");
+    do_throw("Calling getCharsetTitle with invalid value should throw.");
+  }
+  catch (ex) {
+    // The exception is expected.
+  }
+}
