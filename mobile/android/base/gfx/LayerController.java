c8916399fb57dfe204e03bc34a75a25afc76ad13
Kartikaya Gupta <kgupta@mozilla.com>
2012-01-13 07:03:22 -0500

    Bug 715836 - (Cleanup) Remove unused functions. r=pcwalton

--- mobile/android/base/gfx/LayerController.java
+++ mobile/android/base/gfx/LayerController.java
@@ -178,15 +178,6 @@ public class LayerController {
         mView.requestRender();
     }
 
-    /** Scrolls the viewport to the given point. You must hold the monitor while calling this. */
-    public void scrollTo(PointF point) {
-        mViewportMetrics.setOrigin(point);
-        Log.d(LOGTAG, "scrollTo: " + mViewportMetrics);
-        notifyLayerClientOfGeometryChange();
-        GeckoApp.mAppContext.repositionPluginViews(false);
-        mView.requestRender();
-    }
-
     /** Scrolls the viewport by the given offset. You must hold the monitor while calling this. */
     public void scrollBy(PointF point) {
         PointF origin = mViewportMetrics.getOrigin();
@@ -194,15 +185,6 @@ public class LayerController {
         mViewportMetrics.setOrigin(origin);
         Log.d(LOGTAG, "scrollBy: " + mViewportMetrics);
 
-        notifyLayerClientOfGeometryChange();
-        GeckoApp.mAppContext.repositionPluginViews(false);
-        mView.requestRender();
-    }
-
-    /** Sets the current viewport. You must hold the monitor while calling this. */
-    public void setViewport(RectF viewport) {
-        mViewportMetrics.setViewport(viewport);
-        Log.d(LOGTAG, "setViewport: " + mViewportMetrics);
         notifyLayerClientOfGeometryChange();
         GeckoApp.mAppContext.repositionPluginViews(false);
         mView.requestRender();
@@ -234,11 +216,6 @@ public class LayerController {
         mView.requestRender();
     }
 
-    /** Scales the viewport. You must hold the monitor while calling this. */
-    public void scaleTo(float zoomFactor) {
-        scaleWithFocus(zoomFactor, new PointF(0,0));
-    }
-
     /**
      * Scales the viewport, keeping the given focus point in the same place before and after the
      * scale operation. You must hold the monitor while calling this.
@@ -252,16 +229,6 @@ public class LayerController {
         notifyLayerClientOfGeometryChange();
         GeckoApp.mAppContext.repositionPluginViews(false);
         mView.requestRender();
-    }
-
-    /**
-     * Sets the viewport origin and scales in one operation. You must hold the monitor while
-     * calling this.
-     */
-    public void scaleWithOrigin(float zoomFactor, PointF origin) {
-        mViewportMetrics.setOrigin(origin);
-        Log.d(LOGTAG, "scaleWithOrigin: " + mViewportMetrics + "; zf=" + zoomFactor);
-        scaleTo(zoomFactor);
     }
 
     public boolean post(Runnable action) { return mView.post(action); }
