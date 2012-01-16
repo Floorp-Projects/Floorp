71215ea93ac6a5c104be0d27fb59f180d741cc3f
Mounir Lamouri <mounir.lamouri@gmail.com>
2012-01-16 13:25:13 +0100

    Bug 677166 - Part 5 - Use a pref for connection.metered. r=sicking

--- dom/network/src/Connection.cpp
+++ dom/network/src/Connection.cpp
@@ -37,12 +37,16 @@
 
 #include "Connection.h"
 #include "nsIDOMClassInfo.h"
+#include "mozilla/Preferences.h"
 
 DOMCI_DATA(MozConnection, mozilla::dom::network::Connection)
 
 namespace mozilla {
 namespace dom {
 namespace network {
+
+const char* Connection::sMeteredPrefName     = "dom.network.metered";
+const bool  Connection::sMeteredDefaultValue = false;
 
 NS_INTERFACE_MAP_BEGIN(Connection)
   NS_INTERFACE_MAP_ENTRY(nsIDOMMozConnection)
@@ -52,6 +56,14 @@ NS_INTERFACE_MAP_END
 
 NS_IMPL_ADDREF(Connection)
 NS_IMPL_RELEASE(Connection)
+
+NS_IMETHODIMP
+Connection::GetMetered(bool* aMetered)
+{
+  *aMetered = Preferences::GetBool(sMeteredPrefName,
+                                   sMeteredDefaultValue);
+  return NS_OK;
+}
 
 } // namespace network
 } // namespace dom
