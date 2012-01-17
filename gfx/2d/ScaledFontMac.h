b4f4c7b868ccc5d0e0efe1ed538e2f30105f44d1
Ms2ger <ms2ger@gmail.com>
2012-01-17 10:48:19 +0100

    Backout bug 580786 / bug 716639 / bug 692879 / bug 717921 / bug 692879 (2dc5909e63b0:ced751d32df6); a=philor

--- gfx/2d/ScaledFontMac.h
+++ gfx/2d/ScaledFontMac.h
@@ -38,28 +38,25 @@
 #ifndef MOZILLA_GFX_SCALEDFONTMAC_H_
 #define MOZILLA_GFX_SCALEDFONTMAC_H_
 
+#include "ScaledFontSkia.h"
 #import <ApplicationServices/ApplicationServices.h>
-#include "2D.h"
 
-#include "ScaledFontBase.h"
 
 namespace mozilla {
 namespace gfx {
 
-class ScaledFontMac : public ScaledFontBase
+class ScaledFontMac : public ScaledFontSkia
 {
 public:
   ScaledFontMac(CGFontRef aFont, Float aSize);
   virtual ~ScaledFontMac();
 
   virtual FontType GetType() const { return FONT_MAC; }
-#ifdef USE_SKIA
-  virtual SkTypeface* GetSkTypeface();
-#endif
-  virtual TemporaryRef<Path> GetPathForGlyphs(const GlyphBuffer &aBuffer, const DrawTarget *aTarget);
+
 private:
-  friend class DrawTargetCG;
-  CGFontRef mFont;
+  friend class DrawTargetSkia;
+
+  CTFontRef mFontFace;
 };
 
 }
