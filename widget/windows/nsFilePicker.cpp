dd02f0e70948d1e3b3c09e6d4f1f853e2b1110a3
Jim Mathies <jmathies@mozilla.com>
2012-01-18 07:31:09 -0600

    Bug 718752 - Only apply read only checks in save mode for Vista+ file pickers. r=bbondy

--- widget/windows/nsFilePicker.cpp
+++ widget/windows/nsFilePicker.cpp
@@ -893,7 +893,7 @@ nsFilePicker::ShowFilePicker(const nsString& aInitialDir)
 
   FILEOPENDIALOGOPTIONS fos = 0;
   fos |= FOS_SHAREAWARE | FOS_OVERWRITEPROMPT |
-         FOS_NOREADONLYRETURN | FOS_FORCEFILESYSTEM;
+         FOS_FORCEFILESYSTEM;
 
   // Handle add to recent docs settings
   if (IsPrivacyModeEnabled() || !mAddToRecentDocs) {
@@ -915,6 +915,7 @@ nsFilePicker::ShowFilePicker(const nsString& aInitialDir)
       break;
 
     case modeSave:
+      fos |= FOS_NOREADONLYRETURN;
       // Don't follow shortcuts when saving a shortcut, this can be used
       // to trick users (bug 271732)
       if (IsDefaultPathLink())
