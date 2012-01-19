82961d726740153d6e5c05e1efb2a2ec18fa79a9
David Anderson <danderson@mozilla.com>
2012-01-18 18:07:46 -0800

    Follow-up fix for ARMv7 regression after soft-float support on ARMv6 (bug 696291, r=mrosenberg).
    --HG--
    extra : rebase_source : f7a86715ad9a478381c7af37cba946a0c3566f69

--- js/src/assembler/assembler/MacroAssemblerARM.cpp
+++ js/src/assembler/assembler/MacroAssemblerARM.cpp
@@ -56,7 +56,7 @@ namespace JSC {
 
 static bool isVFPPresent()
 {
-#if WTF_PLATFORM_LINUX
+#if WTF_OS_LINUX || WTF_OS_ANDROID
     int fd = open("/proc/self/auxv", O_RDONLY);
     if (fd > 0) {
         Elf32_auxv_t aux;
