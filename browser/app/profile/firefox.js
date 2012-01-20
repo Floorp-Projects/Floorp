1aafc6e2112668e24a2049f905a3057d207d4e57
Dave Camp <dcamp@mozilla.com>
2012-01-20 12:29:04 -0800

    Bug 719934: Disable GCLI by default. r=robcee

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1049,7 +1049,7 @@ pref("devtools.styleeditor.transitions", true);
 pref("devtools.chrome.enabled", false);
 
 // Disable the GCLI enhanced command line.
-pref("devtools.gcli.enable", true);
+pref("devtools.gcli.enable", false);
 
 // The last Web Console height. This is initially 0 which means that the Web
 // Console will use the default height next time it shows.
