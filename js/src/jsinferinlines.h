6a0b6f53cf791d3a3302de665223a5440772e716
Geoff Lankow <geoff@darktrojan.net>
2012-01-21 11:26:37 +1300

    Backed out changeset febc999231e4 (bug 719841)

--- js/src/jsinferinlines.h
+++ js/src/jsinferinlines.h
@@ -1150,9 +1150,6 @@ inline TypeObject::TypeObject(JSObject *proto, bool function, bool unknown)
 {
     PodZero(this);
 
-    /* Inner objects may not appear on prototype chains. */
-    JS_ASSERT(!proto->getClass()->ext.outerObject);
-
     this->proto = proto;
 
     if (function)
