80112887601f4cdc9a936aa5152493419e19c38d
Chris Lord <chrislord.net@gmail.com>
2012-01-22 09:40:37 +0000

    Bug 717283 - Use tiles on-demand. r=pcwalton, snorp
    Instead of tying the tile-buffer in MultiTileLayer directly to the back-buffer
    of the page, make sure rendering is always aligned to the tile grid and use
    tiles on-demand. This makes better use of tiles when panning/zooming, and opens
    up the route for further optimisations.

--- mobile/android/base/gfx/SingleTileLayer.java
+++ mobile/android/base/gfx/SingleTileLayer.java
@@ -43,6 +43,7 @@ import org.mozilla.gecko.gfx.IntSize;
 import org.mozilla.gecko.gfx.LayerController;
 import org.mozilla.gecko.gfx.TileLayer;
 import android.graphics.PointF;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.opengl.GLES11;
 import android.opengl.GLES11Ext;
@@ -58,6 +59,8 @@ import javax.microedition.khronos.opengles.GL10;
  * TODO: Repeating textures really should be their own type of layer.
  */
 public class SingleTileLayer extends TileLayer {
+    private static final String LOGTAG = "GeckoSingleTileLayer";
+
     public SingleTileLayer(CairoImage image) { this(false, image); }
 
     public SingleTileLayer(boolean repeat, CairoImage image) {
@@ -71,6 +74,11 @@ public class SingleTileLayer extends TileLayer {
         if (!initialized())
             return;
 
+        // If the texture contents is entirely invalid, we have nothing to draw.
+        Rect validTexture = getValidTextureArea();
+        if (validTexture.isEmpty())
+            return;
+
         GLES11.glBindTexture(GL10.GL_TEXTURE_2D, getTextureID());
 
         RectF bounds;
@@ -79,13 +87,27 @@ public class SingleTileLayer extends TileLayer {
         RectF viewport = context.viewport;
 
         if (repeats()) {
+            if (!validTexture.equals(new Rect(0, 0, size.width, size.height))) {
+                Log.e(LOGTAG, "Drawing partial repeating textures is unsupported!");
+            }
+
             bounds = new RectF(0.0f, 0.0f, viewport.width(), viewport.height());
             int width = Math.round(viewport.width());
             int height = Math.round(-viewport.height());
             cropRect = new int[] { 0, size.height, width, height };
         } else {
             bounds = getBounds(context, new FloatSize(size));
-            cropRect = new int[] { 0, size.height, size.width, -size.height };
+
+            float scaleFactor = bounds.width() / (float)size.width;
+            bounds.left += validTexture.left * scaleFactor;
+            bounds.top += validTexture.top * scaleFactor;
+            bounds.right -= (size.width - validTexture.right) * scaleFactor;
+            bounds.bottom -= (size.height - validTexture.bottom) * scaleFactor;
+
+            cropRect = new int[] { validTexture.left,
+                                   validTexture.bottom,
+                                   validTexture.width(),
+                                   -validTexture.height() };
         }
 
         GLES11.glTexParameteriv(GL10.GL_TEXTURE_2D, GLES11Ext.GL_TEXTURE_CROP_RECT_OES, cropRect,
