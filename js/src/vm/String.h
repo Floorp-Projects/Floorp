197bda73120c50d2cce0445c5fa0dc9b1613813e
Nicholas Nethercote <nnethercote@mozilla.com>
2012-01-23 15:38:59 -0800

    Bug 720219 (part 2) - Clean up JS string measurement.  r=bhackett.
    --HG--
    extra : rebase_source : bc43388760741370265654aa4661ee422d6b6a33

--- js/src/vm/String.h
+++ js/src/vm/String.h
@@ -407,7 +407,7 @@ class JSString : public js::gc::Cell
 
     /* Gets the number of bytes that the chars take on the heap. */
 
-    JS_FRIEND_API(size_t) charsHeapSize(JSMallocSizeOfFun mallocSizeOf);
+    size_t sizeOfExcludingThis(JSMallocSizeOfFun mallocSizeOf);
 
     /* Offsets for direct field from jit code. */
 
