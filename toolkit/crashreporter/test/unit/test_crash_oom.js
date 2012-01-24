42b67a523b9903baed8dbff7130e30a4097197c1
Benjamin Smedberg <benjamin@smedbergs.us>
2012-01-24 11:08:51 -0500

    Bug 716638 - Annotate the size of a failed allocation due to OOM and submit it with the crash report, r=ted

---
+++ toolkit/crashreporter/test/unit/test_crash_oom.js
@@ -0,0 +1,19 @@
+function run_test()
+{
+  if (!("@mozilla.org/toolkit/crash-reporter;1" in Components.classes)) {
+    dump("INFO | test_crash_purevirtual.js | Can't test crashreporter in a non-libxul build.\n");
+    return;
+  }
+
+  do_crash(
+    function() {
+      crashType = CrashTestUtils.CRASH_OOM;
+      crashReporter.annotateCrashReport("TestingOOMCrash", "Yes");
+    },
+    function(mdump, extra) {
+      do_check_eq(extra.TestingOOMCrash, "Yes");
+      do_check_true("OOMAllocationSize" in extra);
+      do_check_true(Number(extra.OOMAllocationSize) > 0);
+    },
+    true);
+}
