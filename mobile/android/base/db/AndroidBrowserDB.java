835417830209b76c454a3ba6b82cb772ef995deb
Gian-Carlo Pascutto <gpascutto@mozilla.com>
2012-01-25 22:10:01 +0100

    Bug 713283 - Limit imported history entries. Sort by approximate frecency. Update visits count. r=lucasr

--- mobile/android/base/db/AndroidBrowserDB.java
+++ mobile/android/base/db/AndroidBrowserDB.java
@@ -118,9 +118,31 @@ public class AndroidBrowserDB implements BrowserDB.BrowserDBIface {
                   new String[] { uri });
     }
 
-    public void updateHistoryDate(ContentResolver cr, String uri, long date) {
+    public void updateHistoryEntry(ContentResolver cr, String uri, String title,
+                                   long date, int visits) {
+        int oldVisits = 0;
+        Cursor cursor = null;
+        try {
+            cursor = cr.query(Browser.BOOKMARKS_URI,
+                              new String[] { Browser.BookmarkColumns.VISITS },
+                              Browser.BookmarkColumns.URL + " = ?",
+                              new String[] { uri },
+                              null);
+
+            if (cursor.moveToFirst()) {
+                oldVisits = cursor.getInt(0);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+
         ContentValues values = new ContentValues();
         values.put(Browser.BookmarkColumns.DATE, date);
+        values.put(Browser.BookmarkColumns.VISITS, oldVisits + visits);
+        if (title != null) {
+            values.put(Browser.BookmarkColumns.TITLE, title);
+        }
 
         cr.update(Browser.BOOKMARKS_URI,
                   values,
@@ -145,7 +167,8 @@ public class AndroidBrowserDB implements BrowserDB.BrowserDBIface {
                                            BookmarkColumns.URL,
                                            BookmarkColumns.TITLE,
                                            BookmarkColumns.FAVICON,
-                                           BookmarkColumns.DATE },
+                                           BookmarkColumns.DATE,
+                                           BookmarkColumns.VISITS },
                             // Bookmarks that have not been visited have a date value
                             // of 0, so don't pick them up in the history view.
                             Browser.BookmarkColumns.DATE + " > 0",
@@ -153,6 +176,11 @@ public class AndroidBrowserDB implements BrowserDB.BrowserDBIface {
                             Browser.BookmarkColumns.DATE + " DESC LIMIT " + limit);
 
         return new AndroidDBCursor(c);
+    }
+
+    public int getMaxHistoryCount() {
+        // Valid for Android versions up to 4.0.
+        return 250;
     }
 
     public void clearHistory(ContentResolver cr) {
@@ -357,6 +385,8 @@ public class AndroidBrowserDB implements BrowserDB.BrowserDBIface {
                 columnName = URL_COLUMN_THUMBNAIL;
             } else if (columnName.equals(BrowserDB.URLColumns.DATE_LAST_VISITED)) {
                 columnName = Browser.BookmarkColumns.DATE;
+            } else if (columnName.equals(BrowserDB.URLColumns.VISITS)) {
+                columnName = Browser.BookmarkColumns.VISITS;
             }
 
             return columnName;
