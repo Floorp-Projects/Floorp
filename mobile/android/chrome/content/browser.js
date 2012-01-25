acea520545b8caabdf24c38199b2b43250f0fcf4
Matt Brubeck <mbrubeck@mozilla.com>
2012-01-24 18:39:22 -0800

    Bug 718296 - Use third-party fixup on location bar searches [r=wesj]

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -750,9 +750,9 @@ var BrowserApp = {
     let uri;
     if (aParams.engine) {
       let engine;
-      if (aParams.engine == "__default__")
-        engine = Services.search.currentEngine || Services.search.defaultEngine;
-      else
+      // If the default engine was requested, we just pass the URL through
+      // and let the third-party fixup send it to the default search.
+      if (aParams.engine != "__default__")
         engine = Services.search.getEngineByName(aParams.engine);
 
       if (engine)
@@ -872,6 +872,7 @@ var BrowserApp = {
         selected: true,
         parentId: ("parentId" in data) ? data.parentId : -1,
         flags: Ci.nsIWebNavigation.LOAD_FLAGS_DISALLOW_INHERIT_OWNER
+             | Ci.nsIWebNavigation.LOAD_FLAGS_ALLOW_THIRD_PARTY_FIXUP
       };
 
       let url = this.getSearchOrFixupURI(data);
