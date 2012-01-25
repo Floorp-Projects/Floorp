6bc1759abb259397f8979512945b9e73c359f470
L. David Baron <dbaron@dbaron.org>
2012-01-24 17:21:28 -0800

    Make floating :first-letter frames not be containers for font size inflation, to avoid triggering assertions.  (Bug 706609, patch 3.5)  r=roc

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -461,6 +461,8 @@ IsFontSizeInflationContainer(nsIFrame* aFrame,
    * always containers.
    */
   bool isInline = (aStyleDisplay->mDisplay == NS_STYLE_DISPLAY_INLINE ||
+                   (aStyleDisplay->IsFloating() &&
+                    aFrame->GetType() == nsGkAtoms::letterFrame) ||
                    (aFrame->GetContent() &&
                     aFrame->GetContent()->IsInNativeAnonymousSubtree())) &&
                   !(aFrame->IsBoxFrame() && aFrame->GetParent() &&
