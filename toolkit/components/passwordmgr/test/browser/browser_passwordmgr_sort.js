aea1f129a9e1c262fd97ba634fba8f9efd21784d
Karun Dambiec <karun.84@gmx.de>
2012-01-26 14:13:00 -0800

    Bug 721495 - Passwordmgr tests: Use EventUtils sendChar() and sendKey(), instead of calling synthesizeKey() directly, r=zpao

--- toolkit/components/passwordmgr/test/browser/browser_passwordmgr_sort.js
+++ toolkit/components/passwordmgr/test/browser/browser_passwordmgr_sort.js
@@ -114,7 +114,7 @@ function test() {
                     else if (aTopic == "domwindowopened") {
                         let win = aSubject.QueryInterface(Ci.nsIDOMEventTarget);
                         SimpleTest.waitForFocus(function() {
-                            EventUtils.synthesizeKey("VK_RETURN", {}, win)
+                            EventUtils.sendKey("RETURN", win);
                         }, win);
                     }
                 });
