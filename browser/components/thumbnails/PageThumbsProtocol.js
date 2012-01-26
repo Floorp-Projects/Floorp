bebde5349676c53aa28cdbbea695c3ccf1cc5910
Tim Taubert <tim.taubert@gmx.de>
2012-01-26 18:28:41 +0100

    Bug 721398 - moz-page-thumb protocol should not access from a web page; r=mak

--- browser/components/thumbnails/PageThumbsProtocol.js
+++ browser/components/thumbnails/PageThumbsProtocol.js
@@ -52,7 +52,7 @@ Protocol.prototype = {
    * The flags specific to this protocol implementation.
    */
   get protocolFlags() {
-    return Ci.nsIProtocolHandler.URI_IS_UI_RESOURCE |
+    return Ci.nsIProtocolHandler.URI_DANGEROUS_TO_LOAD |
            Ci.nsIProtocolHandler.URI_NORELATIVE |
            Ci.nsIProtocolHandler.URI_NOAUTH;
   },
