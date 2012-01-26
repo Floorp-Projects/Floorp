bb8e96f2905e9b5528df3de7c67890fa39bb0a46
Margaret Leibovic <margaret.leibovic@gmail.com>
2012-01-26 15:07:38 -0800

    Bug 719868 - Progress throbber still shows when we load about:home on startup. r=mfinkle

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -307,7 +307,7 @@ var BrowserApp = {
           observe: function(aSubject, aTopic, aData) {
             Services.obs.removeObserver(restoreCleanup, "sessionstore-windows-restored");
             if (aData == "fail")
-              BrowserApp.addTab("about:home");
+              BrowserApp.addTab("about:home", { showProgress: false });
           }
         };
         Services.obs.addObserver(restoreCleanup, "sessionstore-windows-restored", false);
@@ -316,7 +316,7 @@ var BrowserApp = {
       // Start the restore
       ss.restoreLastSession(restoreToFront);
     } else {
-      this.addTab(url);
+      this.addTab(url, { showProgress: url != "about:home" });
 
       // show telemetry door hanger if we aren't restoring a session
       this._showTelemetryPrompt();
