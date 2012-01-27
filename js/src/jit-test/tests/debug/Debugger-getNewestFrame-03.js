b8aa7263d8f3d0a55acfc3fe76135b32f34a1ee2
Jason Orendorff <jorendorff@mozilla.com>
2012-01-27 14:16:22 -0600

    Make the JS debugger consistently ignore dummy frames, fixing an assertion failure. Bug 678086, r=jimb.

---
+++ js/src/jit-test/tests/debug/Debugger-getNewestFrame-03.js
@@ -0,0 +1,9 @@
+// Debugger.prototype.getNewestFrame() ignores dummy frames.
+// See bug 678086.
+
+var g = newGlobal('new-compartment');
+g.f = function () { return dbg.getNewestFrame(); };
+var dbg = new Debugger;
+var gw = dbg.addDebuggee(g);
+var fw = gw.getOwnPropertyDescriptor("f").value;
+assertEq(fw.call().return, null);
