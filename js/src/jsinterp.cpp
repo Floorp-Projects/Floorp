b7af83e107738fa1f4ec3426977a92bec955569f
Bobby Holley <bobbyholley@gmail.com>
2012-01-27 14:16:27 -0600

    Do not innerize an object assigned to __proto__ or used as the target of a with statement. Bug 719841, r=mrbkap.

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -994,10 +994,6 @@ EnterWith(JSContext *cx, jsint stackIndex)
     if (!parent)
         return JS_FALSE;
 
-    OBJ_TO_INNER_OBJECT(cx, obj);
-    if (!obj)
-        return JS_FALSE;
-
     JSObject *withobj = WithObject::create(cx, fp, *obj, *parent,
                                            sp + stackIndex - fp->base());
     if (!withobj)
