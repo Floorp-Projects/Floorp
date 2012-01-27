05013372cd225446849fa90be34cf6d22cd08b90
Rafael Ávila de Espíndola <respindola@mozilla.com>
2012-01-27 10:31:44 -0500

    Bug 683975 - Need infra for developer contributed compilers. r=rail.
    --HG--
    extra : rebase_source : b5b616042e629be4c9cd1c8bc7217bc15f4d4a1c

--- build/unix/build-toolchain/build-gcc.py
+++ build/unix/build-toolchain/build-gcc.py
@@ -64,6 +64,7 @@ def build_glibc_aux(stage_dir, inst_dir):
                    "--enable-add-ons=nptl",
                    "--without-selinux",
                    "--enable-kernel=2.6.18",
+                   "--libdir=%s/lib64" % inst_dir,
                    "--prefix=%s" % inst_dir])
 
 def build_one_stage(env, stage_dir, is_stage_one):
@@ -104,10 +105,23 @@ def build_one_stage_aux(stage_dir, is_stage_one):
                           "--disable-bootstrap"]
     if is_stage_one:
         gcc_configure_args.append("--enable-languages=c")
+        gcc_configure_args.append("--disable-multilib")
+        # We build the stage1 gcc without shared libraries. Otherwise its
+        # libgcc.so would depend on the system libc.so, which causes problems
+        # when it tries to use that libgcc.so and the libc we are about to
+        # build.
+        gcc_configure_args.append("--disable-shared")
     else:
         gcc_configure_args.append("--enable-languages=c,c++")
 
     build_package(gcc_source_dir, gcc_build_dir, gcc_configure_args)
+
+    if is_stage_one:
+        # The glibc build system uses -lgcc_eh, but at least in this setup
+        # libgcc.a has all it needs.
+        d = tool_inst_dir + "/lib/gcc/x86_64-unknown-linux-gnu/4.5.2/"
+        os.symlink(d + "libgcc.a", d + "libgcc_eh.a")
+
     build_glibc({"CC"  : tool_inst_dir + "/bin/gcc",
                  "CXX" : tool_inst_dir + "/bin/g++"},
                 stage_dir, tool_inst_dir)
