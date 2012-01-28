5d7394e94d38abdef855545d22129dd608c98707
Mats Palmgren <matspal@gmail.com>
2012-01-28 04:35:58 +0100

    Bug 720987 - Don't use the existing overflow areas when calculating the new overflow areas after a style change.  r=roc

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -8021,21 +8021,7 @@ nsCSSFrameConstructor::ProcessRestyledFrames(nsStyleChangeList& aChangeList)
       }
       if (hint & nsChangeHint_UpdateOverflow) {
         while (frame) {
-          nsOverflowAreas overflowAreas;
-          nsOverflowAreas* pre = static_cast<nsOverflowAreas*>
-            (frame->Properties().Get(frame->PreTransformOverflowAreasProperty()));
-          if (pre) {
-            // FinishAndStoreOverflow will change the overflow areas passed in,
-            // so make a copy.
-            overflowAreas = *pre;
-          } else {
-            // There is no transform yet on this frame, so we can just use its
-            // current overflow areas.
-            overflowAreas = frame->GetOverflowAreas();
-          }
-
-          frame->FinishAndStoreOverflow(overflowAreas, frame->GetSize());
-
+          frame->UpdateOverflow();
           nsIFrame* next =
             nsLayoutUtils::GetNextContinuationOrSpecialSibling(frame);
           // Update the ancestors' overflow after we have updated the overflow
