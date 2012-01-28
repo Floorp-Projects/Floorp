4ac8c65daf3447f427aad58a75b60f38e9b91398
Mats Palmgren <matspal@gmail.com>
2012-01-28 04:35:59 +0100

    Bug 719177 - Introduce NS_STYLE_HINT_UPDATE_OVERFLOW as a short-hand for nsChangeHint(nsChangeHint_RepaintFrame | nsChangeHint_UpdateOverflow).  r=roc

--- layout/style/nsStyleContext.cpp
+++ layout/style/nsStyleContext.cpp
@@ -438,7 +438,7 @@ nsStyleContext::CalcStyleDifference(nsStyleContext* aOther)
   // Visibility, Outline, TableBorder, Table, Text, UIReset, Quotes
   nsChangeHint maxHint = nsChangeHint(NS_STYLE_HINT_FRAMECHANGE |
       nsChangeHint_UpdateTransformLayer | nsChangeHint_UpdateOpacityLayer |
-      nsChangeHint_UpdateOverflow);
+      NS_STYLE_HINT_UPDATE_OVERFLOW);
   DO_STRUCT_DIFFERENCE(Display);
 
   // Changes to 'visibility' cause SyncFrameView.
@@ -447,7 +447,7 @@ nsStyleContext::CalcStyleDifference(nsStyleContext* aOther)
   DO_STRUCT_DIFFERENCE(Visibility);
 
   maxHint = nsChangeHint(NS_STYLE_HINT_FRAMECHANGE |
-      nsChangeHint_UpdateOverflow | nsChangeHint_UpdateCursor);
+      NS_STYLE_HINT_UPDATE_OVERFLOW | nsChangeHint_UpdateCursor);
   DO_STRUCT_DIFFERENCE(XUL);
   DO_STRUCT_DIFFERENCE(Column);
   DO_STRUCT_DIFFERENCE(Content);
@@ -465,7 +465,7 @@ nsStyleContext::CalcStyleDifference(nsStyleContext* aOther)
   DO_STRUCT_DIFFERENCE(SVGReset);
   DO_STRUCT_DIFFERENCE(SVG);
 
-  maxHint = nsChangeHint(NS_STYLE_HINT_REFLOW | nsChangeHint_UpdateOverflow);
+  maxHint = nsChangeHint(NS_STYLE_HINT_REFLOW | NS_STYLE_HINT_UPDATE_OVERFLOW);
   DO_STRUCT_DIFFERENCE(Border);
       
   // Changes to 'z-index' cause SyncFrameView.
@@ -485,7 +485,7 @@ nsStyleContext::CalcStyleDifference(nsStyleContext* aOther)
   DO_STRUCT_DIFFERENCE(TextReset);
 
   // Outline needs to update the overflow and repaint.
-  maxHint = nsChangeHint(nsChangeHint_RepaintFrame | nsChangeHint_UpdateOverflow);
+  maxHint = NS_STYLE_HINT_UPDATE_OVERFLOW;
   DO_STRUCT_DIFFERENCE(Outline);
 
   // Most backgrounds only require a repaint, but backgrounds using -moz-element
