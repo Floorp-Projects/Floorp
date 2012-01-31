eb9974e490692538da6c3bc4836a18711a467e71
Benoit Girard <b56girard@gmail.com>
2012-01-31 14:28:37 -0500

    Android build fixes

--- mobile/android/base/gfx/LayerRenderer.java
+++ mobile/android/base/gfx/LayerRenderer.java
@@ -263,7 +263,7 @@ public class LayerRenderer implements GLSurfaceView.Renderer {
 
     /** Used by robocop for testing purposes. Not for production use! */
     IntBuffer getPixels() {
-        IntBuffer pixelBuffer = IntBuffer.allocate(mView.getWidth() * mView.getHeight());
+        IntBuffer pixelBuffer = IntBuffer.allocate(mView.getAndroidView().getWidth() * mView.getAndroidView().getHeight());
         synchronized (pixelBuffer) {
             mPixelBuffer = pixelBuffer;
             mView.requestRender();
