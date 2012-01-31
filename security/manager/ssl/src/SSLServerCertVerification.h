782c8feb7ee14f5c29dd5a4aa95f94b26723ea74
Brian Smith <bsmith@mozilla.com>
2012-01-31 08:04:57 -0800

    Bug 712363, Part 2: Move CertErrorRunnable, SSLServerCertVerificationResult, and HandleBadCertificate to SSLServerCertVerification.cpp, r=honzab

--- security/manager/ssl/src/SSLServerCertVerification.h
+++ security/manager/ssl/src/SSLServerCertVerification.h
@@ -39,11 +39,7 @@
 #define _SSLSERVERCERTVERIFICATION_H
 
 #include "seccomon.h"
-#include "nsAutoPtr.h"
-#include "nsThreadUtils.h"
-#include "nsIRunnable.h"
-#include "prerror.h"
-#include "nsNSSIOLayer.h"
+#include "prio.h"
 
 typedef struct PRFileDesc PRFileDesc;
 typedef struct CERTCertificateStr CERTCertificate;
@@ -54,35 +50,6 @@ namespace mozilla { namespace psm {
 
 SECStatus AuthCertificateHook(void *arg, PRFileDesc *fd, 
                               PRBool checkSig, PRBool isServer);
-
-SECStatus HandleBadCertificate(PRErrorCode defaultErrorCodeToReport,
-                               nsNSSSocketInfo * socketInfo,
-                               CERTCertificate & cert,
-                               const void * fdForLogging,
-                               const nsNSSShutDownPreventionLock &);
-
-// Dispatched from a cert verification thread to the STS thread to notify the
-// socketInfo of the verification result.
-//
-// This will cause the PR_Poll in the STS thread to return, so things work
-// correctly even if the STS thread is blocked polling (only) on the file
-// descriptor that is waiting for this result.
-class SSLServerCertVerificationResult : public nsRunnable
-{
-public:
-  NS_DECL_NSIRUNNABLE
-
-  SSLServerCertVerificationResult(nsNSSSocketInfo & socketInfo,
-                                  PRErrorCode errorCode,
-                                  SSLErrorMessageType errorMessageType = 
-                                      PlainErrorMessage);
-
-  void Dispatch();
-private:
-  const nsRefPtr<nsNSSSocketInfo> mSocketInfo;
-  const PRErrorCode mErrorCode;
-  const SSLErrorMessageType mErrorMessageType;
-};
 
 } } // namespace mozilla::psm
 
