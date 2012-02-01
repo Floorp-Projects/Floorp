ca5328a5d637410203e4bb628c781efc5d36be9a
Matt Brubeck <mbrubeck@mozilla.com>
2012-01-31 17:34:26 -0800

    Back out 08f4fc94e27b f3ceed05f6b7 f87d25461d6c 317e842f6c65 57adfe804b56 (bug 720316, bug 722260)

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -1410,7 +1410,6 @@ js::Interpret(JSContext *cx, StackFrame *entryFrame, InterpMode interpMode)
 # define END_CASE_LEN3      len = 3; goto advance_pc;
 # define END_CASE_LEN4      len = 4; goto advance_pc;
 # define END_CASE_LEN5      len = 5; goto advance_pc;
-# define END_CASE_LEN7      len = 7; goto advance_pc;
 # define END_VARLEN_CASE    goto advance_pc;
 # define ADD_EMPTY_CASE(OP) BEGIN_CASE(OP)
 # define END_EMPTY_CASES    goto advance_pc_by_one;
@@ -1435,6 +1434,12 @@ js::Interpret(JSContext *cx, StackFrame *entryFrame, InterpMode interpMode)
 
 #define GET_FULL_INDEX(PCOFF)                                                 \
     (atoms - script->atoms + GET_INDEX(regs.pc + (PCOFF)))
+
+#define LOAD_OBJECT(PCOFF, obj)                                               \
+    (obj = script->getObject(GET_FULL_INDEX(PCOFF)))
+
+#define LOAD_FUNCTION(PCOFF)                                                  \
+    (fun = script->getFunction(GET_FULL_INDEX(PCOFF)))
 
 #define LOAD_DOUBLE(PCOFF, dbl)                                               \
     (dbl = script->getConst(GET_FULL_INDEX(PCOFF)).toDouble())
@@ -2942,7 +2947,9 @@ END_CASE(JSOP_STRING)
 
 BEGIN_CASE(JSOP_OBJECT)
 {
-    PUSH_OBJECT(*script->getObject(GET_UINT32_INDEX(regs.pc)));
+    JSObject *obj;
+    LOAD_OBJECT(0, obj);
+    PUSH_OBJECT(*obj);
 }
 END_CASE(JSOP_OBJECT)
 
@@ -3221,7 +3228,8 @@ BEGIN_CASE(JSOP_DEFFUN)
      * a compound statement (not at the top statement level of global code, or
      * at the top level of a function body).
      */
-    JSFunction *fun = script->getFunction(GET_UINT32_INDEX(regs.pc));
+    JSFunction *fun;
+    LOAD_FUNCTION(0);
     JSObject *obj = fun;
 
     JSObject *obj2;
@@ -3330,7 +3338,8 @@ END_CASE(JSOP_DEFFUN)
 
 BEGIN_CASE(JSOP_DEFFUN_FC)
 {
-    JSFunction *fun = script->getFunction(GET_UINT32_INDEX(regs.pc));
+    JSFunction *fun;
+    LOAD_FUNCTION(0);
 
     JSObject *obj = js_NewFlatClosure(cx, fun);
     if (!obj)
@@ -3366,7 +3375,8 @@ BEGIN_CASE(JSOP_DEFLOCALFUN)
      * JSOP_DEFFUN that avoids requiring a call object for the outer function's
      * activation.
      */
-    JSFunction *fun = script->getFunction(GET_UINT32_INDEX(regs.pc + SLOTNO_LEN));
+    JSFunction *fun;
+    LOAD_FUNCTION(SLOTNO_LEN);
     JS_ASSERT(fun->isInterpreted());
     JS_ASSERT(!fun->isFlatClosure());
 
@@ -3391,7 +3401,8 @@ END_CASE(JSOP_DEFLOCALFUN)
 
 BEGIN_CASE(JSOP_DEFLOCALFUN_FC)
 {
-    JSFunction *fun = script->getFunction(GET_UINT32_INDEX(regs.pc + SLOTNO_LEN));
+    JSFunction *fun;
+    LOAD_FUNCTION(SLOTNO_LEN);
 
     JSObject *obj = js_NewFlatClosure(cx, fun);
     if (!obj)
@@ -3405,7 +3416,8 @@ END_CASE(JSOP_DEFLOCALFUN_FC)
 BEGIN_CASE(JSOP_LAMBDA)
 {
     /* Load the specified function object literal. */
-    JSFunction *fun = script->getFunction(GET_UINT32_INDEX(regs.pc));
+    JSFunction *fun;
+    LOAD_FUNCTION(0);
     JSObject *obj = fun;
 
     /* do-while(0) so we can break instead of using a goto. */
@@ -3498,7 +3510,8 @@ END_CASE(JSOP_LAMBDA)
 
 BEGIN_CASE(JSOP_LAMBDA_FC)
 {
-    JSFunction *fun = script->getFunction(GET_UINT32_INDEX(regs.pc));
+    JSFunction *fun;
+    LOAD_FUNCTION(0);
 
     JSObject *obj = js_NewFlatClosure(cx, fun);
     if (!obj)
@@ -3675,7 +3688,8 @@ END_CASE(JSOP_NEWARRAY)
 
 BEGIN_CASE(JSOP_NEWOBJECT)
 {
-    JSObject *baseobj = script->getObject(GET_UINT32_INDEX(regs.pc));
+    JSObject *baseobj;
+    LOAD_OBJECT(0, baseobj);
 
     TypeObject *type = TypeScript::InitObject(cx, script, regs.pc, JSProto_Object);
     if (!type)
@@ -4221,7 +4235,9 @@ BEGIN_CASE(JSOP_ENTERBLOCK)
 BEGIN_CASE(JSOP_ENTERLET0)
 BEGIN_CASE(JSOP_ENTERLET1)
 {
-    StaticBlockObject &blockObj = script->getObject(GET_UINT32_INDEX(regs.pc))->asStaticBlock();
+    JSObject *obj;
+    LOAD_OBJECT(0, obj);
+    StaticBlockObject &blockObj = obj->asStaticBlock();
     JS_ASSERT(regs.fp()->maybeBlockChain() == blockObj.enclosingBlock());
 
     if (op == JSOP_ENTERBLOCK) {
