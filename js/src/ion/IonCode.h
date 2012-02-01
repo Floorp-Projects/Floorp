d2d6abead93baaff19ecd7fcf3062434268ed426
Chris Leary <cdleary@mozilla.com>
2012-02-01 15:04:56 -0800

    No bug: Fix invalidation on already-invalidated frames. (r=dvander)

--- js/src/ion/IonCode.h
+++ js/src/ion/IonCode.h
@@ -332,6 +332,7 @@ struct IonScript
     }
     const SafepointIndex *getSafepointIndex(uint32 disp) const;
     const SafepointIndex *getSafepointIndex(uint8 *retAddr) const {
+        JS_ASSERT(retAddr > method()->raw());
         return getSafepointIndex(retAddr - method()->raw());
     }
     const OsiIndex *getOsiIndex(uint32 disp) const;
