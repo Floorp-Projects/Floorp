8d7817acb938a3d0fd1f76e8115dd97407398764
Chris Jones <jones.chris.g@gmail.com>
2012-02-02 00:33:06 -0800

    Bug 718254: Get the IPDL unit test harness compiling again. r=bent

--- ipc/ipdl/test/cxx/TestSysVShmem.h
+++ ipc/ipdl/test/cxx/TestSysVShmem.h
@@ -17,6 +17,9 @@ public:
     TestSysVShmemParent() { }
     virtual ~TestSysVShmemParent() { }
 
+    static bool RunTestInProcesses() { return true; }
+    static bool RunTestInThreads() { return true; }
+
     void Main();
 
 protected:
