c71622c072edd4ad2cf8d7e6194c3665bfbc13de
Jeff Muizelaar <jmuizelaar@mozilla.com>
2012-02-03 15:13:24 -0500

    Bug 724079. Add an internal stackwalk API. r=ehsan
    This will be used by the profiler to be able to unwind arbitrary threads.

--- xpcom/base/nsStackWalk.cpp
+++ xpcom/base/nsStackWalk.cpp
@@ -41,10 +41,13 @@
 /* API for getting a stack trace of the C/C++ stack on the current thread */
 
 #include "mozilla/Util.h"
+#include "mozilla/StackWalk.h"
 #include "nsDebug.h"
 #include "nsStackWalkPrivate.h"
 
 #include "nsStackWalk.h"
+
+using namespace mozilla;
 
 // The presence of this address is the stack must stop the stack walk. If
 // there is no such address, the structure will be {NULL, true}.
@@ -214,8 +217,6 @@ StackWalkInitCriticalAddress()
 #define USING_WXP_VERSION 1
 #endif
 #endif
-
-using namespace mozilla;
 
 // Define these as static pointers so that we can load the DLL on the
 // fly (and not introduce a link-time dependency on it). Tip o' the
@@ -1572,9 +1573,6 @@ NS_FormatCodeAddressDetails(void *aPC, const nsCodeAddressDetails *aDetails,
 
 #else // not __sun-specific
 
-#define X86_OR_PPC (defined(__i386) || defined(PPC) || defined(__ppc__))
-#if X86_OR_PPC && (NSSTACKWALK_SUPPORTS_MACOSX || NSSTACKWALK_SUPPORTS_LINUX) // i386 or PPC Linux or Mac stackwalking code
-
 #if __GLIBC__ > 2 || __GLIBC_MINOR > 1
 #define HAVE___LIBC_STACK_END 1
 #else
@@ -1584,25 +1582,12 @@ NS_FormatCodeAddressDetails(void *aPC, const nsCodeAddressDetails *aDetails,
 #if HAVE___LIBC_STACK_END
 extern void *__libc_stack_end; // from ld-linux.so
 #endif
-
-EXPORT_XPCOM_API(nsresult)
-NS_StackWalk(NS_WalkStackCallback aCallback, PRUint32 aSkipFrames,
-             void *aClosure, uintptr_t aThread)
+namespace mozilla {
+nsresult
+FramePointerStackWalk(NS_WalkStackCallback aCallback, PRUint32 aSkipFrames,
+                      void *aClosure, void **bp)
 {
-  MOZ_ASSERT(gCriticalAddress.mInit);
-  MOZ_ASSERT(!aThread);
   // Stack walking code courtesy Kipp's "leaky".
-
-  // Get the frame pointer
-  void **bp;
-#if defined(__i386) 
-  __asm__( "movl %%ebp, %0" : "=g"(bp));
-#else
-  // It would be nice if this worked uniformly, but at least on i386 and
-  // x86_64, it stopped working with gcc 4.1, because it points to the
-  // end of the saved registers instead of the start.
-  bp = (void**) __builtin_frame_address(0);
-#endif
 
   int skip = aSkipFrames;
   while (1) {
@@ -1634,6 +1619,33 @@ NS_StackWalk(NS_WalkStackCallback aCallback, PRUint32 aSkipFrames,
     bp = next;
   }
   return NS_OK;
+}
+
+}
+
+#define X86_OR_PPC (defined(__i386) || defined(PPC) || defined(__ppc__))
+#if X86_OR_PPC && (NSSTACKWALK_SUPPORTS_MACOSX || NSSTACKWALK_SUPPORTS_LINUX) // i386 or PPC Linux or Mac stackwalking code
+
+EXPORT_XPCOM_API(nsresult)
+NS_StackWalk(NS_WalkStackCallback aCallback, PRUint32 aSkipFrames,
+             void *aClosure, uintptr_t aThread)
+{
+  MOZ_ASSERT(gCriticalAddress.mInit);
+  MOZ_ASSERT(!aThread);
+
+  // Get the frame pointer
+  void **bp;
+#if defined(__i386) 
+  __asm__( "movl %%ebp, %0" : "=g"(bp));
+#else
+  // It would be nice if this worked uniformly, but at least on i386 and
+  // x86_64, it stopped working with gcc 4.1, because it points to the
+  // end of the saved registers instead of the start.
+  bp = (void**) __builtin_frame_address(0);
+#endif
+  return FramePointerStackWalk(aCallback, aSkipFrames,
+                               aClosure, bp);
+
 }
 
 #elif defined(HAVE__UNWIND_BACKTRACE)
