c75dc186d010fbeb8736cc35bfb3ab0528bc41d5
Richard Newman <rnewman@mozilla.com>
2012-02-03 13:09:29 -0800

    Bug 723235, Bug 720304, Bug 723240 - Request bookmarks and history items sorted by sortindex/frecency, with limits.
    • Bug 720304 - Limit number of processed history records, with a hard limit of 500 history items per request.
    • Bug 723235 - Request history items sorted by sortindex/frecency.
    • Bug 723240 - Request bookmark items sorted by sortindex/frecency.
    
    And imposes a sanity limit of 5,000 bookmarks per fetch.
    
    (Note that batching is outside the scope of these bugs.)

---
+++ mobile/android/base/sync/repositories/ConstrainedServer11Repository.java
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.sync.repositories;
+
+import java.net.URISyntaxException;
+
+import org.mozilla.gecko.sync.CredentialsSource;
+
+/**
+ * A kind of Server11Repository that supports explicit setting of limit and sort on operations.
+ *
+ * @author rnewman
+ *
+ */
+public class ConstrainedServer11Repository extends Server11Repository {
+
+  private String sort = null;
+  private long limit  = -1;
+
+  public ConstrainedServer11Repository(String serverURI, String username, String collection, CredentialsSource credentialsSource, long limit, String sort) throws URISyntaxException {
+    super(serverURI, username, collection, credentialsSource);
+
+    this.limit = limit;
+    this.sort  = sort;
+  }
+
+  @Override
+  protected String getDefaultSort() {
+    return sort;
+  }
+
+  @Override
+  protected long getDefaultFetchLimit() {
+    return limit;
+  }
+}
--- mobile/android/base/sync/stage/AndroidBrowserHistoryServerSyncStage.java
+++ mobile/android/base/sync/stage/AndroidBrowserHistoryServerSyncStage.java
@@ -37,12 +37,21 @@
 
 package org.mozilla.gecko.sync.stage;
 
+import java.net.URISyntaxException;
+
+import org.mozilla.gecko.sync.repositories.ConstrainedServer11Repository;
 import org.mozilla.gecko.sync.repositories.RecordFactory;
 import org.mozilla.gecko.sync.repositories.Repository;
 import org.mozilla.gecko.sync.repositories.android.AndroidBrowserHistoryRepository;
 import org.mozilla.gecko.sync.repositories.domain.HistoryRecordFactory;
 
 public class AndroidBrowserHistoryServerSyncStage extends ServerSyncStage {
+
+  // Eventually this kind of sync stage will be data-driven,
+  // and all this hard-coding can go away.
+  private static final String HISTORY_SORT          = "index";
+  private static final long   HISTORY_REQUEST_LIMIT = 500;
+
   @Override
   public void execute(org.mozilla.gecko.sync.GlobalSession session) throws NoSuchStageException {
     super.execute(session);
@@ -60,6 +69,16 @@ public class AndroidBrowserHistoryServerSyncStage extends ServerSyncStage {
   @Override
   protected Repository getLocalRepository() {
     return new AndroidBrowserHistoryRepository();
+  }
+
+  @Override
+  protected Repository getRemoteRepository() throws URISyntaxException {
+    return new ConstrainedServer11Repository(session.config.getClusterURLString(),
+                                             session.config.username,
+                                             getCollection(),
+                                             session,
+                                             HISTORY_REQUEST_LIMIT,
+                                             HISTORY_SORT);
   }
 
   @Override
