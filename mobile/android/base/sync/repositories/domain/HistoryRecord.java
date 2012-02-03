7b5fb1882ce50a1a7f7105393e5f7f8f2b1256c6
Richard Newman <rnewman@mozilla.com>
2012-02-03 13:09:28 -0800

    Bug 720934 - Part 1: Collected Android Sync 0.4 code drop (Bug 721305, Bug 709348, Bug 719693, Bug 709660, Bug 718703). a=mobile
    Includes:
    3ad57f4 Bug 718703 - Pair a Device crashes. r=rnewman
    3c530d1 Bug 709660 - Fixed blurry icons used in SyncAdapter. r=rnewman
    bde287a Bug 719693 - Compatibility for JDK < 6.
    91c5e23 Bug 719693 - Effect review comments on 6ffb7d72f.
    6ffb7d7 Bug 719693 - Handle date-format HTTP Retry-After headers.
    6df289e Bug 709348 - Trace logging and fixed logging of visit equality.
    71580df Bug 709348 - Include Fennec visits and counts in HistoryRecord androidID equivalence.
    8f6a106 Bug 709348 - Equality work.
    f40bf4a Bug 709348 - Part 5: Rework handling of certain cases of incoming deletion, including history visits.
    0dd004d Bug 709348 - Part 4: Reworking equality tests between records.
    2210717 Bug 709348 - Part 3: Better commenting for ignored bookmark records.
    c05236b Bug 709348 - Part 2: Introduce trace log method.
    a34f4aa Bug 709348 - Part 1: Introduce copyWithIDs to clone records.
    796b15f Bug 721305 - sync.link.advancedsetup.label should use …, not ....
    adf7586 Bump AndroidSync User-Agent version.

--- mobile/android/base/sync/repositories/domain/HistoryRecord.java
+++ mobile/android/base/sync/repositories/domain/HistoryRecord.java
@@ -83,6 +83,32 @@ public class HistoryRecord extends Record {
   public long      fennecDateVisited;
   public long      fennecVisitCount;
 
+  @SuppressWarnings("unchecked")
+  private JSONArray copyVisits() {
+    if (this.visits == null) {
+      return null;
+    }
+    JSONArray out = new JSONArray();
+    out.addAll(this.visits);
+    return out;
+  }
+
+  @Override
+  public Record copyWithIDs(String guid, long androidID) {
+    HistoryRecord out = new HistoryRecord(guid, this.collection, this.lastModified, this.deleted);
+    out.androidID = androidID;
+    out.sortIndex = this.sortIndex;
+
+    // Copy HistoryRecord fields.
+    out.title             = this.title;
+    out.histURI           = this.histURI;
+    out.fennecDateVisited = this.fennecDateVisited;
+    out.fennecVisitCount  = this.fennecVisitCount;
+    out.visits            = this.copyVisits();
+
+    return out;
+  }
+
   @Override
   public void initFromPayload(CryptoRecord payload) {
     ExtendedJSONObject p = payload.payload;
@@ -115,32 +141,62 @@ public class HistoryRecord extends Record {
     return rec;
   }
 
-  public boolean equalsExceptVisits(Object o) {
-    if (!(o instanceof HistoryRecord)) {
+
+  /**
+   * We consider two history records to be congruent if they represent the
+   * same history record regardless of visits.
+   */
+  @Override
+  public boolean congruentWith(Object o) {
+    if (o == null || !(o instanceof HistoryRecord)) {
       return false;
     }
     HistoryRecord other = (HistoryRecord) o;
-    return super.equals(other) &&
-           RepoUtils.stringsEqual(this.title, other.title) &&
+    if (!super.congruentWith(other)) {
+      return false;
+    }
+    return RepoUtils.stringsEqual(this.title, other.title) &&
            RepoUtils.stringsEqual(this.histURI, other.histURI);
   }
 
-  public boolean equalsIncludingVisits(Object o) {
+  @Override
+  public boolean equalPayloads(Object o) {
+    if (o == null || !(o instanceof HistoryRecord)) {
+      Log.d(LOG_TAG, "Not a HistoryRecord: " + o);
+      return false;
+    }
     HistoryRecord other = (HistoryRecord) o;
-    return equalsExceptVisits(other) && this.checkVisitsEquals(other);
+    if (!super.equalPayloads(other)) {
+      Log.d(LOG_TAG, "super.equalPayloads returned false.");
+      return false;
+    }
+    return RepoUtils.stringsEqual(this.title, other.title) &&
+           RepoUtils.stringsEqual(this.histURI, other.histURI) &&
+           checkVisitsEquals(other);
   }
 
   @Override
-  /**
-   * We consider two history records to be equal if they represent the
-   * same history record regardless of visits.
-   */
-  public boolean equals(Object o) {
-    return equalsExceptVisits(o);
+  public boolean equalAndroidIDs(Record other) {
+    return super.equalAndroidIDs(other) &&
+           this.equalFennecVisits(other);
+  }
+
+  private boolean equalFennecVisits(Record other) {
+    if (!(other instanceof HistoryRecord)) {
+      return false;
+    }
+    HistoryRecord h = (HistoryRecord) other;
+    return this.fennecDateVisited == h.fennecDateVisited &&
+           this.fennecVisitCount  == h.fennecVisitCount;
   }
 
   private boolean checkVisitsEquals(HistoryRecord other) {
-    
+    Log.d(LOG_TAG, "Checking visits.");
+    if (Utils.ENABLE_TRACE_LOGGING) {
+      Log.d(LOG_TAG, ">> Mine:   " + ((this.visits == null) ? "null" : this.visits.toJSONString()));
+      Log.d(LOG_TAG, ">> Theirs: " + ((other.visits == null) ? "null" : other.visits.toJSONString()));
+    }
+
     // Handle nulls.
     if (this.visits == other.visits) {
       return true;
