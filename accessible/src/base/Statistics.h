98064cf7d951ee0339aac8b741eca6543f3e46c9
David Bolter <dbolter@mozilla.com>
2012-02-04 13:52:52 -0500

    Bug 721772 - Call ISimpleDOM telemetry accumulation only once. r=tbsaunde

--- accessible/src/base/Statistics.h
+++ accessible/src/base/Statistics.h
@@ -56,7 +56,13 @@ namespace statistics {
    * Report that ISimpleDOM* has been used.
    */
   inline void ISimpleDOMUsed()
-    { Telemetry::Accumulate(Telemetry::ISIMPLE_DOM_USAGE, 1); }
+  {
+    static bool firstTime = true;
+    if (firstTime) {
+      Telemetry::Accumulate(Telemetry::ISIMPLE_DOM_USAGE, 1);
+      firstTime = false;
+    }
+  }
 
   /**
    * Report that IAccessibleTable has been used.
