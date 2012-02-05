eb8e25f5060db0bc79a5ef9c367b2dafbb52bf03
Sinker Li <thinker@codemud.net>
2012-02-05 19:51:05 +0000

    Bug 697641, part 1: Export Android Sensor Manager API to Gecko. r=cjones

--- widget/android/AndroidBridge.h
+++ widget/android/AndroidBridge.h
@@ -162,6 +162,10 @@ public:
 
     void EnableLocation(bool aEnable);
 
+    void EnableSensor(int aSensorType);
+
+    void DisableSensor(int aSensorType);
+
     void ReturnIMEQueryResult(const PRUnichar *aResult, PRUint32 aLen, int aSelStart, int aSelLen);
 
     void NotifyXreExit();
@@ -419,6 +423,8 @@ protected:
     jmethodID jAcknowledgeEventSync;
     jmethodID jEnableDeviceMotion;
     jmethodID jEnableLocation;
+    jmethodID jEnableSensor;
+    jmethodID jDisableSensor;
     jmethodID jReturnIMEQueryResult;
     jmethodID jNotifyAppShellReady;
     jmethodID jNotifyXreExit;
