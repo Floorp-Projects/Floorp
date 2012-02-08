7fba27a12de3b55ad9b67605f13376f232bcd58b
Josh Matthews <josh@joshmatthews.net>
2012-02-08 13:37:07 -0500

    Bug 722850 - Part 2: Query the private browsing status of channels used to manipulate cookies. r=mconnor

--- extensions/cookie/test/unit/test_bug468700.js
+++ extensions/cookie/test/unit/test_bug468700.js
@@ -1,6 +1,18 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+function make_channel(url) {
+  var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+  var chan = ios.newChannel(url, null, null).QueryInterface(Ci.nsIHttpChannel);
+  return chan;
+}
+
+function make_uri(url) {
+  var ios = Cc["@mozilla.org/network/io-service;1"].
+            getService(Ci.nsIIOService);
+  return ios.newURI(url, null, null);
+}
+
 function run_test() {
   do_load_manifest("cookieprompt.manifest");
 
@@ -8,10 +20,6 @@ function run_test() {
   var cm = Cc["@mozilla.org/cookiemanager;1"].getService(Ci.nsICookieManager2);
   var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
   var prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
-  var pb = null;
-  try {
-    pb = Cc["@mozilla.org/privatebrowsing;1"].getService(Ci.nsIPrivateBrowsingService);
-  } catch (e) {}
 
   var spec = "http://foo.bar/baz";
   var uri = ios.newURI(spec, null, null);
@@ -28,25 +36,22 @@ function run_test() {
   do_check_eq(cs.countCookiesFromHost("foo.bar"), 1);
   cs.removeAll();
 
-  // if private browsing is available
-  if (pb) {
-    prefs.setBoolPref("browser.privatebrowsing.keep_current_session", true);
 
-    // enter private browsing mode
-    pb.privateBrowsingEnabled = true;
+  // Simulate private browsing with a private channel for context
+  var chan = make_channel(uri.spec);
+  chan.QueryInterface(Ci.nsIPrivateBrowsingChannel);
+  chan.setPrivate(true);
+  
+  // accept all cookies
+  prefs.setIntPref("network.cookie.lifetimePolicy", 0);
 
-    // accept all cookies
-    prefs.setIntPref("network.cookie.lifetimePolicy", 0);
-    // add a test cookie
-    cs.setCookieString(uri, null, "foobar=bar", null);
-    do_check_eq(cs.countCookiesFromHost("foo.bar"), 1);
-    // ask all cookies (will result in rejection because the prompt is not available)
-    prefs.setIntPref("network.cookie.lifetimePolicy", 1);
-    // add a test cookie
-    cs.setCookieString(uri, null, "foobaz=bar", null);
-    do_check_eq(cs.countCookiesFromHost("foo.bar"), 2);
-
-    prefs.clearUserPref("browser.privatebrowsing.keep_current_session");
-  }
+  // add a test cookie
+  cs.setCookieString(uri, null, "foobar=bar", chan);
+  do_check_eq(cs.getCookieString(uri, chan), "foobar=bar");
+  // ask all cookies (will result in rejection because the prompt is not available)
+  prefs.setIntPref("network.cookie.lifetimePolicy", 1);
+  // add a test cookie
+  cs.setCookieString(uri, null, "foobaz=bar", chan);
+  do_check_eq(cs.getCookieString(uri, chan), "foobar=bar; foobaz=bar");
 }
 
