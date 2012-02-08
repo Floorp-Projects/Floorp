cdcc1f5d177b6b0218e18936b2cbda86ca009ecd
Patrick Walton <pwalton@mozilla.com>
2012-02-07 23:10:58 -0800

    Lower the minimum viewport change delay to 25 for now

--- mobile/android/base/gfx/GeckoLayerClient.java
+++ mobile/android/base/gfx/GeckoLayerClient.java
@@ -74,7 +74,7 @@ public abstract class GeckoLayerClient extends LayerClient implements GeckoEvent
     /* The viewport that Gecko will display when drawing is finished */
     protected ViewportMetrics mNewGeckoViewport;
 
-    private static final long MIN_VIEWPORT_CHANGE_DELAY = 200L;
+    private static final long MIN_VIEWPORT_CHANGE_DELAY = 25L;
     private long mLastViewportChangeTime;
     private boolean mPendingViewportAdjust;
     private boolean mViewportSizeChanged;
