d0d1ae6c09b333bc0119e30784f9b6ea5f5878ca
Chris Pearce <cpearce@mozilla.com>
2012-02-08 17:10:11 +1300

    Bug 718939 - Backout bea7ecf9084e. r=roc

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -5195,11 +5195,7 @@ bool nsWindow::ProcessMessage(UINT msg, WPARAM &wParam, LPARAM &lParam,
       break;
 
     case WM_KILLFOCUS:
-      if (sJustGotDeactivate || !wParam) {
-        // Note: wParam is FALSE when the window has lost focus. Sometimes
-        // We can receive WM_KILLFOCUS with !wParam while changing to
-        // full-screen mode and we won't receive an WM_ACTIVATE/WA_INACTIVE
-        // message, so inform the focus manager that we've lost focus now.
+      if (sJustGotDeactivate) {
         result = DispatchFocusToTopLevelWindow(NS_DEACTIVATE);
       }
       break;
