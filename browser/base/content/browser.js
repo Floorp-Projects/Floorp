418cff759b7580843224a0d0ad24a47214fdda00
Tim Taubert <tim.taubert@gmx.de>
2012-02-09 20:20:30 +0100

    Backed out changeset b3a4b572a634 (bug 715402)

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -86,8 +86,7 @@ var gLastValidURLStr = "";
 var gInPrintPreviewMode = false;
 var gDownloadMgr = null;
 var gContextMenu = null; // nsContextMenu instance
-var gDelayedStartupTimeoutId; // used for non-browser-windows
-var gFirstPaintListener = null;
+var gDelayedStartupTimeoutId;
 var gStartupRan = false;
 
 #ifndef XP_MACOSX
@@ -1384,18 +1383,7 @@ function BrowserStartup() {
 
   retrieveToolbarIconsizesFromTheme();
 
-  // Listen for the first paint event for this window, and only do non-critical
-  // work then, so that things like session restore don't block the window from
-  // being visible.
-  gFirstPaintListener = function(e) {
-    if (e.target == window) {
-      window.removeEventListener("MozAfterPaint", gFirstPaintListener, false);
-      gFirstPaintListener = null;
-      delayedStartup(isLoadingBlank, mustLoadSidebar);
-    }
-  };
-  window.addEventListener("MozAfterPaint", gFirstPaintListener, false);
-
+  gDelayedStartupTimeoutId = setTimeout(delayedStartup, 0, isLoadingBlank, mustLoadSidebar);
   gStartupRan = true;
 }
 
@@ -1526,6 +1514,7 @@ function delayedStartup(isLoadingBlank, mustLoadSidebar) {
   Cu.import("resource:///modules/TelemetryTimestamps.jsm", tmp);
   let TelemetryTimestamps = tmp.TelemetryTimestamps;
   TelemetryTimestamps.add("delayedStartupStarted");
+  gDelayedStartupTimeoutId = null;
 
   Services.obs.addObserver(gSessionHistoryObserver, "browser:purge-session-history", false);
   Services.obs.addObserver(gXPInstallObserver, "addon-install-disabled", false);
@@ -1878,9 +1867,8 @@ function BrowserShutdown() {
 
   // Now either cancel delayedStartup, or clean up the services initialized from
   // it.
-  if (gFirstPaintListener) {
-    window.removeEventListener("MozAfterPaint", gFirstPaintListener, false);
-    gFirstPaintListener = null;
+  if (gDelayedStartupTimeoutId) {
+    clearTimeout(gDelayedStartupTimeoutId);
   } else {
     if (Win7Features)
       Win7Features.onCloseWindow();
