301647ba08da54f46f06b3d1426017d73ae68ff0
Mounir Lamouri <mounir.lamouri@gmail.com>
2012-02-09 16:55:54 +0100

    Bug 720794 - Part 2 (DOM) - Implement reading and event parts of Screen Orientation API. r=jst sr=sicking

--- dom/base/nsScreen.h
+++ dom/base/nsScreen.h
@@ -37,26 +37,37 @@
 #ifndef nsScreen_h___
 #define nsScreen_h___
 
+#include "mozilla/Hal.h"
 #include "nsIDOMScreen.h"
 #include "nsISupports.h"
 #include "nsIScriptContext.h"
 #include "nsCOMPtr.h"
+#include "mozilla/dom/ScreenOrientation.h"
+#include "nsDOMEventTargetHelper.h"
+#include "mozilla/Observer.h"
 
 class nsIDocShell;
 class nsDeviceContext;
 struct nsRect;
 
 // Script "screen" object
-class nsScreen : public nsIDOMScreen
+class nsScreen : public nsDOMEventTargetHelper
+               , public nsIDOMScreen
+               , public mozilla::hal::ScreenOrientationObserver
 {
 public:
-  nsScreen(nsIDocShell* aDocShell);
-  virtual ~nsScreen();
+  static already_AddRefed<nsScreen> Create(nsPIDOMWindow* aWindow);
 
-  NS_IMETHOD SetDocShell(nsIDocShell* aDocShell);
+  void Invalidate();
 
   NS_DECL_ISUPPORTS
   NS_DECL_NSIDOMSCREEN
+  NS_FORWARD_NSIDOMEVENTTARGET(nsDOMEventTargetHelper::)
+
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(nsScreen,
+                                           nsDOMEventTargetHelper)
+
+  void Notify(const mozilla::dom::ScreenOrientationWrapper& aOrientation);
 
 protected:
   nsDeviceContext* GetDeviceContext();
@@ -65,12 +76,19 @@ protected:
 
   nsIDocShell* mDocShell; // Weak Reference
 
+  mozilla::dom::ScreenOrientation mOrientation;
+
 private:
+  nsScreen();
+  virtual ~nsScreen();
+
   static bool sInitialized;
   static bool sAllowScreenEnabledProperty;
   static bool sAllowScreenBrightnessProperty;
 
   static void Initialize();
+
+  NS_DECL_EVENT_HANDLER(mozorientationchange)
 };
 
 #endif /* nsScreen_h___ */
