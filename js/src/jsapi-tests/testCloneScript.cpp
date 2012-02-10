92341a2a13df7ce08573019ee8455c4b925c74a0
Igor Bukanov <igor@mir2.org>
2012-02-10 13:40:34 +0100

    bug 725576 - serialize principals only once per top-level script. r=luke

--- js/src/jsapi-tests/testCloneScript.cpp
+++ js/src/jsapi-tests/testCloneScript.cpp
@@ -6,6 +6,8 @@
 
 #include "tests.h"
 #include "jsapi.h"
+#include "jsdbgapi.h"
+#include "jsxdrapi.h"
 
 BEGIN_TEST(test_cloneScript)
 {
@@ -48,3 +50,122 @@ BEGIN_TEST(test_cloneScript)
     return true;
 }
 END_TEST(test_cloneScript)
+
+void
+DestroyPrincipals(JSContext *cx, JSPrincipals *principals)
+{
+    delete principals;
+}
+
+struct Principals : public JSPrincipals
+{
+  public:
+    Principals(const char *name)
+    {
+        refcount = 0;
+        codebase = const_cast<char *>(name);
+        destroy = DestroyPrincipals;
+        subsume = NULL;
+    }
+};
+
+class AutoDropPrincipals
+{
+    JSContext *cx;
+    JSPrincipals *principals;
+
+  public:
+    AutoDropPrincipals(JSContext *cx, JSPrincipals *principals)
+      : cx(cx), principals(principals)
+    {
+        JSPRINCIPALS_HOLD(cx, principals);
+    }
+
+    ~AutoDropPrincipals()
+    {
+        JSPRINCIPALS_DROP(cx, principals);
+    }
+};
+
+JSBool
+TranscodePrincipals(JSXDRState *xdr, JSPrincipals **principalsp)
+{
+    return JS_XDRBytes(xdr, reinterpret_cast<char *>(principalsp), sizeof(*principalsp));
+}
+
+BEGIN_TEST(test_cloneScriptWithPrincipals)
+{
+    JSSecurityCallbacks cbs = {
+        NULL,
+        TranscodePrincipals,
+        NULL,
+        NULL
+    };
+
+    JS_SetRuntimeSecurityCallbacks(rt, &cbs);
+
+    JSPrincipals *principalsA = new Principals("A");
+    AutoDropPrincipals dropA(cx, principalsA);
+    JSPrincipals *principalsB = new Principals("B");
+    AutoDropPrincipals dropB(cx, principalsB);
+
+    JSObject *A, *B;
+
+    CHECK(A = createGlobal(principalsA));
+    CHECK(B = createGlobal(principalsB));
+
+    const char *argnames[] = { "arg" };
+    const char *source = "return function() { return arg; }";
+
+    JSObject *obj;
+
+    // Compile in A
+    {
+        JSAutoEnterCompartment a;
+        if (!a.enter(cx, A))
+            return false;
+
+        JSFunction *fun;
+        CHECK(fun = JS_CompileFunctionForPrincipals(cx, A, principalsA, "f",
+                                                    mozilla::ArrayLength(argnames), argnames,
+                                                    source, strlen(source), __FILE__, 1));
+
+        JSScript *script;
+        CHECK(script = JS_GetFunctionScript(cx, fun));
+
+        CHECK(JS_GetScriptPrincipals(cx, script) == principalsA);
+        CHECK(obj = JS_GetFunctionObject(fun));
+    }
+
+    // Clone into B
+    {
+        JSAutoEnterCompartment b;
+        if (!b.enter(cx, B))
+            return false;
+
+        JSObject *cloned;
+        CHECK(cloned = JS_CloneFunctionObject(cx, obj, B));
+
+        JSFunction *fun;
+        CHECK(fun = JS_ValueToFunction(cx, JS::ObjectValue(*cloned)));
+
+        JSScript *script;
+        CHECK(script = JS_GetFunctionScript(cx, fun));
+
+        CHECK(JS_GetScriptPrincipals(cx, script) == principalsB);
+
+        JS::Value v;
+        JS::Value args[] = { JS::Int32Value(1) };
+        CHECK(JS_CallFunctionValue(cx, B, JS::ObjectValue(*cloned), 1, args, &v));
+        CHECK(v.isObject());
+
+        JSObject *funobj = &v.toObject();
+        CHECK(JS_ObjectIsFunction(cx, funobj));
+        CHECK(fun = JS_ValueToFunction(cx, v));
+        CHECK(script = JS_GetFunctionScript(cx, fun));
+        CHECK(JS_GetScriptPrincipals(cx, script) == principalsB);
+    }
+
+    return true;
+}
+END_TEST(test_cloneScriptWithPrincipals)
