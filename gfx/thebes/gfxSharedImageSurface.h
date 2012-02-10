8d9de61a32c6d5da4f82bef40a48f507cdd75ba0
Benoit Girard <b56girard@gmail.com>
2012-02-10 15:13:02 -0500

    Bug 721489 - Make gfxSharedImageSurface divisible by 64 on mobile

--- gfx/thebes/gfxSharedImageSurface.h
+++ gfx/thebes/gfxSharedImageSurface.h
@@ -39,6 +39,14 @@
 #ifndef GFX_SHARED_IMAGESURFACE_H
 #define GFX_SHARED_IMAGESURFACE_H
 
+#if ANDROID
+ // Using an 64 align prevents us from crashing in driver
+ // code when doing RGB565 uploads. See bug 721489
+ #define MOZ_IMG_MEM_ALIGN 64
+#else
+ #define MOZ_IMG_MEM_ALIGN 1
+#endif
+
 #include "mozilla/ipc/Shmem.h"
 #include "mozilla/ipc/SharedMemory.h"
 
@@ -114,6 +122,14 @@ private:
         Shmem shmem;
         long stride = ComputeStride(aSize, aFormat);
         size_t size = GetAlignedSize(aSize, stride);
+
+        int w = aSize.width;
+        int h = aSize.height;
+        if ((aSize.width%MOZ_IMG_MEM_ALIGN) != 0)
+          w = aSize.width + MOZ_IMG_MEM_ALIGN - (aSize.width%MOZ_IMG_MEM_ALIGN);
+        stride = ComputeStride(gfxIntSize(w, h), aFormat);
+        size = GetAlignedSize(aSize, stride);
+
         if (!Unsafe) {
             if (!aAllocator->AllocShmem(size, aShmType, &shmem))
                 return nsnull;
