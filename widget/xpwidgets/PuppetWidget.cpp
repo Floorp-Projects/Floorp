1593a9824bc75a5cdd30e0b760cc9f1e6cd46207
Oleg Romashin <romaxa@gmail.com>
2012-02-10 11:22:21 -0800

    Bug 725925 - ShadowableThebesLayer does useless extra composite into 1x1 fake surface. r=cjones

--- widget/xpwidgets/PuppetWidget.cpp
+++ widget/xpwidgets/PuppetWidget.cpp
@@ -530,6 +530,8 @@ PuppetWidget::DispatchPaintEvent()
       DispatchEvent(&event, status);
     } else {
       nsRefPtr<gfxContext> ctx = new gfxContext(mSurface);
+      ctx->Rectangle(gfxRect(0,0,0,0));
+      ctx->Clip();
       AutoLayerManagerSetup setupLayerManager(this, ctx,
                                               BasicLayerManager::BUFFER_NONE);
       DispatchEvent(&event, status);  
