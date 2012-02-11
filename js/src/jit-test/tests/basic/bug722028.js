ba034b66b7b638a776b833d02f4e24ea1f706556
Bill McCloskey <wmccloskey@mozilla.com>
2012-02-10 18:32:18 -0800

    Bug 722028 - Need write barrier for SuppressDeletedPropertyHelper (r=bhackett)

---
+++ js/src/jit-test/tests/basic/bug722028.js
@@ -0,0 +1,13 @@
+
+gczeal(4);
+var BUGNUMBER = 668024;
+var summary =
+print(BUGNUMBER + ": " + summary);
+var arr = [0, 1, 2, 3, 4, 5, , 7];
+var seen = [];
+for (var p in arr) {
+    if (seen.indexOf(unescape) >= 0) {}
+    arr.splice(2, 3);
+  seen.push(p);
+}
+
