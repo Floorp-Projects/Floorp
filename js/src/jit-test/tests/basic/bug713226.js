80cadd13906f4e5772ce6f640a593195155f68fb
Bill McCloskey <wmccloskey@mozilla.com>
2012-02-10 18:32:31 -0800

    Bug 713226 - Fix SETPROP write barrier in methodjit (r=bhackett)

---
+++ js/src/jit-test/tests/basic/bug713226.js
@@ -0,0 +1,21 @@
+// |jit-test| mjitalways;
+gczeal(4);
+var optionNames = options().split(',');
+  for (var i = 0; i < optionNames.length; i++)
+    var optionName = optionNames[i];
+      options(optionName);
+evaluate("\
+function addDebug(g, id) {\
+    var debuggerGlobal = newGlobal('new-compartment');\
+    debuggerGlobal.debuggee = g;\
+    debuggerGlobal.id = id;\
+    debuggerGlobal.print = function (s) { (g) += s; };\
+    debuggerGlobal.eval('var dbg = new Debugger(debuggee);dbg.onDebuggerStatement = function () { print(id); debugger; };');\
+    return debuggerGlobal;\
+}\
+var base = newGlobal('new-compartment');\
+var top = base;\
+for (var i = 0; i < 8; i++ )\
+    top = addDebug(top, i);\
+base.eval('debugger;');\
+");
