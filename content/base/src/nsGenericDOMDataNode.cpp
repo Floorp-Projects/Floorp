ef38438ad0cdd623be014ea7f4d05794516d8553
Phil Ringnalda <philringnalda@gmail.com>
2012-02-12 14:01:58 -0800

    Back out 1cd8c9b40035 (bug 725376), df43cc1f72e1 (bug 725535), and 8a820e4ee7f6 (bug 726364) for reftest orange

--- content/base/src/nsGenericDOMDataNode.cpp
+++ content/base/src/nsGenericDOMDataNode.cpp
@@ -752,9 +752,6 @@ nsGenericDOMDataNode::SplitData(PRUint32 aOffset, nsIContent** aReturn,
     return rv;
   }
 
-  nsIDocument* document = GetCurrentDoc();
-  mozAutoDocUpdate updateBatch(document, UPDATE_CONTENT_MODEL, true);
-
   // Use Clone for creating the new node so that the new node is of same class
   // as this node!
   nsCOMPtr<nsIContent> newContent = CloneDataNode(mNodeInfo, false);
