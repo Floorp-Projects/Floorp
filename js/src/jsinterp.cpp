fd8b55ef22adc9c396e7957c0e97a03360e9c26e
Jan de Mooij <jdemooij@mozilla.com>
2012-02-14 23:36:36 +0100

    Bug 727087 - Move JSOP_TOID implementation to jsinterpinlines. r=dvander
    --HG--
    extra : rebase_source : 2bcc7b7a30f613489a93cb1465a2ae6d64815f1c

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -2453,18 +2453,10 @@ BEGIN_CASE(JSOP_TOID)
      * There must be an object value below the id, which will not be popped
      * but is necessary in interning the id for XML.
      */
- 
-    Value &idval = regs.sp[-1];
-    if (!idval.isInt32()) {
-        JSObject *obj;
-        FETCH_OBJECT(cx, -2, obj);
- 
-        jsid dummy;
-        if (!js_InternNonIntElementId(cx, obj, idval, &dummy, &idval))
-            goto error;
- 
-        TypeScript::MonitorUnknown(cx, script, regs.pc);
-    } 
+    Value objval = regs.sp[-2];
+    Value idval = regs.sp[-1];
+    if (!ToIdOperation(cx, objval, idval, &regs.sp[-1]))
+        goto error;
 }
 END_CASE(JSOP_TOID)
 
