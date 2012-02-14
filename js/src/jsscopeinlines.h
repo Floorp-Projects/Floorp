3396b5b18b167f86497466540a66ba02d3857e96
Terrence Cole <terrence@mozilla.com>
2012-02-14 09:10:23 -0800

    Bug 721463 - Clean up and minimize marking interfaces; r=billm
    Currently, the marking interfaces are a giant pile of duplicated and,
    frequently, unused code.  This patch reworks the interface to clean
    up jsgcmark.h.

--- js/src/jsscopeinlines.h
+++ js/src/jsscopeinlines.h
@@ -395,7 +395,7 @@ Shape::writeBarrierPre(const js::Shape *shape)
 
     JSCompartment *comp = shape->compartment();
     if (comp->needsBarrier())
-        MarkShapeUnbarriered(comp->barrierTracer(), shape, "write barrier");
+        MarkShapeUnbarriered(comp->barrierTracer(), const_cast<Shape *>(shape), "write barrier");
 #endif
 }
 
@@ -410,7 +410,7 @@ Shape::readBarrier(const Shape *shape)
 #ifdef JSGC_INCREMENTAL
     JSCompartment *comp = shape->compartment();
     if (comp->needsBarrier())
-        MarkShapeUnbarriered(comp->barrierTracer(), shape, "read barrier");
+        MarkShapeUnbarriered(comp->barrierTracer(), const_cast<Shape *>(shape), "read barrier");
 #endif
 }
 
