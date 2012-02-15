cd049292f5384e5e6df90b1db7974135e6b498fc
Rafael Ávila de Espíndola <respindola@mozilla.com>
2012-02-15 10:42:34 -0500

    Bug 727222 - mark some classes in content/html/content/src/ final. r=smaug.

--- content/html/content/src/nsMediaError.h
+++ content/html/content/src/nsMediaError.h
@@ -37,8 +37,9 @@
  * ***** END LICENSE BLOCK ***** */
 #include "nsIDOMMediaError.h"
 #include "nsISupports.h"
+#include "mozilla/Attributes.h"
 
-class nsMediaError : public nsIDOMMediaError
+class nsMediaError MOZ_FINAL : public nsIDOMMediaError
 {
 public:
   nsMediaError(PRUint16 aCode);
