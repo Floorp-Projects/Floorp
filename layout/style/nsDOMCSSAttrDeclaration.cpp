e9eaeb9674691abe0ecb586f247f7b915d0e0278
Andrew McCreight <amccreight@mozilla.com>
2012-02-16 08:45:19 -0800

    Bug 727625 - remove childless nsDOMCSSAttributeDeclaration from the purple buffer. r=smaug

--- layout/style/nsDOMCSSAttrDeclaration.cpp
+++ layout/style/nsDOMCSSAttrDeclaration.cpp
@@ -49,6 +49,7 @@
 #include "nsIPrincipal.h"
 #include "nsIURI.h"
 #include "nsNodeUtils.h"
+#include "nsGenericElement.h"
 
 namespace css = mozilla::css;
 namespace dom = mozilla::dom;
@@ -68,7 +69,25 @@ nsDOMCSSAttributeDeclaration::~nsDOMCSSAttributeDeclaration()
   MOZ_COUNT_DTOR(nsDOMCSSAttributeDeclaration);
 }
 
+// If nsDOMCSSAttributeDeclaration is changed so that any additional
+// fields are traversed by the cycle collector (for instance, if
+// wrapper cache handling is changed) then CAN_SKIP must be updated.
 NS_IMPL_CYCLE_COLLECTION_1(nsDOMCSSAttributeDeclaration, mElement)
+
+// nsDOMCSSAttributeDeclaration has only one cycle collected field, so
+// if mElement is going to be skipped, the attribute declaration can't
+// be part of a garbage cycle.
+NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_BEGIN(nsDOMCSSAttributeDeclaration)
+  return !tmp->mElement || nsGenericElement::CanSkip(tmp->mElement, true);
+NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_END
+
+NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_IN_CC_BEGIN(nsDOMCSSAttributeDeclaration)
+  return !tmp->mElement || nsGenericElement::CanSkipInCC(tmp->mElement);
+NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_IN_CC_END
+
+// CanSkipThis returns false to avoid problems with incomplete unlinking.
+NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_THIS_BEGIN(nsDOMCSSAttributeDeclaration)
+NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_THIS_END
 
 NS_INTERFACE_MAP_BEGIN(nsDOMCSSAttributeDeclaration)
   NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
