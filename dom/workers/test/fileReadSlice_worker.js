5515f7ea94078ae2077ab24556345018b1060b7b
Matthew Schranz <schranz.m@gmail.com>
2012-02-16 11:22:25 +0100

    Bug 725289 - Removed moz prefix in Blob.mozSlice. r=sicking

--- dom/workers/test/fileReadSlice_worker.js
+++ dom/workers/test/fileReadSlice_worker.js
@@ -7,7 +7,7 @@ onmessage = function(event) {
   var start = event.data.start;
   var end = event.data.end;
 
-  var slicedBlob = blob.mozSlice(start, end);
+  var slicedBlob = blob.slice(start, end);
 
   var fileReader = new FileReaderSync();
   var text = fileReader.readAsText(slicedBlob);
