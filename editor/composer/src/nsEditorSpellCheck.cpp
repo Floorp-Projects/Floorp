5b0c52860f77721bda9ea50a3541626841e17d7f
Rafael Ávila de Espíndola <respindola@mozilla.com>
2012-02-16 09:39:57 -0500

    Bug 727221 - mark LastDictionary and nsElementDeletionObserver final. r=ehsan.

--- editor/composer/src/nsEditorSpellCheck.cpp
+++ editor/composer/src/nsEditorSpellCheck.cpp
@@ -83,7 +83,7 @@ class UpdateDictionnaryHolder {
 
 #define CPS_PREF_NAME NS_LITERAL_STRING("spellcheck.lang")
 
-class LastDictionary {
+class LastDictionary MOZ_FINAL {
 public:
   /**
    * Store current dictionary for editor document url. Use content pref
