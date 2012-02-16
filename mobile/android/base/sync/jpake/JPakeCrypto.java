390b9bd7efebd50cbeb74ca1ecf5e93b3184d4e8
Richard Newman <rnewman@mozilla.com>
2012-02-15 22:05:51 -0800

    Bug 722524. r=rnewman

--- mobile/android/base/sync/jpake/JPakeCrypto.java
+++ mobile/android/base/sync/jpake/JPakeCrypto.java
@@ -131,16 +131,17 @@ public class JPakeCrypto {
    * @param gx4
    * @param zkp3
    * @param zkp4
-   * @throws Gx4IsOneException
    * @throws IncorrectZkpException
    */
   public static void round2(String secret, JPakeParty jp,
-      JPakeNumGenerator gen) throws Gx4IsOneException, IncorrectZkpException {
+      JPakeNumGenerator gen) throws IncorrectZkpException, Gx3OrGx4IsZeroOrOneException {
 
     Log.d(LOG_TAG, "round2 started.");
 
-    if (BigInteger.ONE.compareTo(jp.gx4) == 0) {
-      throw new Gx4IsOneException();
+    // checkZkp does some additional checks, but we can throw a more informative exception here.
+    if (BigInteger.ZERO.compareTo(jp.gx3) == 0 || BigInteger.ONE.compareTo(jp.gx3) == 0 ||
+        BigInteger.ZERO.compareTo(jp.gx4) == 0 || BigInteger.ONE.compareTo(jp.gx4) == 0) {
+      throw new Gx3OrGx4IsZeroOrOneException();
     }
 
     // Check ZKP.
@@ -243,7 +244,7 @@ public class JPakeCrypto {
 
     // Check parameters of zkp, and compare to computed hash. These shouldn't
     // fail.
-    if (gx.compareTo(BigInteger.ZERO) < 1) {// g^x > 1
+    if (gx.compareTo(BigInteger.ONE) < 1) { // g^x > 1.
       Log.e(LOG_TAG, "g^x > 1 fails.");
       throw new IncorrectZkpException();
     }
