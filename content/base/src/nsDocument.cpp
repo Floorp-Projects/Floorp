75e3ace8546ed499b8691ece14d2b76754b74e33
Chris Pearce <cpearce@mozilla.com>
2012-02-20 10:02:08 +1300

    Bug 727303 - Add nsPIDOMWindow::SetFullScreenInteral. r=roc

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -8532,7 +8532,7 @@ public:
   NS_IMETHOD Run()
   {
     if (mDoc->GetWindow()) {
-      mDoc->GetWindow()->SetFullScreen(mValue);
+      mDoc->GetWindow()->SetFullScreenInternal(mValue, false);
     }
     return NS_OK;
   }
