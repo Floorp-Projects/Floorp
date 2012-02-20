01299aae8ac774b263d0d1c41c33baed6e5f3c58
Nicholas Nethercote <nnethercote@mozilla.com>
2012-02-19 19:51:48 -0800

    Bug 711901 - Use mallocSizeOf in the source image memory reporters.  r=joedrew.
    --HG--
    extra : rebase_source : 9c39bbb44cb40fa900a8245842a50d894c02f4e0

--- image/src/VectorImage.cpp
+++ image/src/VectorImage.cpp
@@ -248,31 +248,31 @@ VectorImage::GetCurrentFrameRect(nsIntRect& aRect)
   aRect = nsIntRect::GetMaxSizedIntRect();
 }
 
-PRUint32
-VectorImage::GetDecodedHeapSize()
-{
-  // XXXdholbert TODO: return num bytes used by helper SVG doc. (bug 590790)
-  return sizeof(*this);
-}
-
-PRUint32
-VectorImage::GetDecodedNonheapSize()
-{
-  return 0;
-}
-
-PRUint32
-VectorImage::GetDecodedOutOfProcessSize()
-{
-  return 0;
-}
-
-PRUint32
-VectorImage::GetSourceHeapSize()
+size_t
+VectorImage::HeapSizeOfSourceWithComputedFallback(nsMallocSizeOfFun aMallocSizeOf) const
 {
   // We're not storing the source data -- we just feed that directly to
   // our helper SVG document as we receive it, for it to parse.
   // So 0 is an appropriate return value here.
+  return 0;
+}
+
+size_t
+VectorImage::HeapSizeOfDecodedWithComputedFallback(nsMallocSizeOfFun aMallocSizeOf) const
+{
+  // XXXdholbert TODO: return num bytes used by helper SVG doc. (bug 590790)
+  return 0;
+}
+
+size_t
+VectorImage::NonHeapSizeOfDecoded() const
+{
+  return 0;
+}
+
+size_t
+VectorImage::OutOfProcessSizeOfDecoded() const
+{
   return 0;
 }
 
