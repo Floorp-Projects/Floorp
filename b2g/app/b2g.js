978dc8924b63c6e872c5d7744aeb6d824dfa63e9
Philipp von Weitershausen <philipp@weitershausen.de>
2012-02-20 17:44:24 +0100

    Bug 728890 follow-up: path served by HTTP server changed, too. DONTBUILD because NPOTB

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -46,7 +46,7 @@ pref("browser.homescreenURL", "file:///data/local/homescreen.html,file:///system
 #endif
 
 // URL for the dialer application.
-pref("dom.telephony.app.phone.url", "http://localhost:7777/apps/dialer/dialer.html");
+pref("dom.telephony.app.phone.url", "http://localhost:7777/data/local/apps/dialer/dialer.html");
 
 // Device pixel to CSS px ratio, in percent. Set to -1 to calculate based on display density.
 pref("browser.viewport.scaleRatio", -1);
