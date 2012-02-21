a62e2a10af4f8c8840371666bb4d7b2b3f6156f8
David Anderson <danderson@mozilla.com>
2012-02-21 15:16:23 -0800

    Fix some merge fallout.

--- js/src/jsgcmark.cpp
+++ js/src/jsgcmark.cpp
@@ -1,4 +1,5 @@
 /* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=4 sw=4 tw=79 et:
  */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
@@ -932,6 +933,10 @@ PushArena(GCMarker *gcmarker, ArenaHeader *aheader)
 
       case JSTRACE_TYPE_OBJECT:
         PushArenaTyped<js::types::TypeObject>(gcmarker, aheader);
+        break;
+
+      case JSTRACE_IONCODE:
+        PushArenaTyped<js::ion::IonCode>(gcmarker, aheader);
         break;
 
 #if JS_HAS_XML_SUPPORT
