20f810ab32aff9992ab6b78caadba61251e4663f
Alexander Surkov <surkov.alexander@gmail.com>
2012-02-22 20:27:28 +0900

    Bug 650241 - improve accessible boundaries testing, r=marcoz

--- accessible/tests/mochitest/layout.js
+++ accessible/tests/mochitest/layout.js
@@ -34,17 +34,14 @@ function testChildAtPoint(aIdentifier, aX, aY, aFindDeepestChild,
  */
 function getChildAtPoint(aIdentifier, aX, aY, aFindDeepestChild)
 {
-  var nodeObj = { value: null };
-  var acc = getAccessible(aIdentifier, null, nodeObj);
-  var node = nodeObj.value;
-
-  if (!acc || !node)
+  var acc = getAccessible(aIdentifier);
+  if (!acc)
     return;
 
-  var [deltaX, deltaY] = getScreenCoords(node);
+  var [screenX, screenY] = getBoundsForDOMElm(acc.DOMNode);
 
-  var x = deltaX + aX;
-  var y = deltaY + aY;
+  var x = screenX + aX;
+  var y = screenY + aY;
 
   try {
     if (aFindDeepestChild)
@@ -58,14 +55,16 @@ function getChildAtPoint(aIdentifier, aX, aY, aFindDeepestChild)
 /**
  * Test the accessible boundaries.
  */
-function testBounds(aID, aX, aY, aWidth, aHeight)
+function testBounds(aID)
 {
+  var [expectedX, expectedY, expectedWidth, expectedHeight] =
+    getBoundsForDOMElm(aID);
+
   var [x, y, width, height] = getBounds(aID);
-  is(x, aX, "Wrong x coordinate of " + prettyName(aID));
-  is(y, aY, "Wrong y coordinate of " + prettyName(aID));
-  // XXX: width varies depending on platform
-  //is(width, aWidth, "Wrong width of " + prettyName(aID));
-  is(height, aHeight, "Wrong height of " + prettyName(aID));
+  is(x, expectedX, "Wrong x coordinate of " + prettyName(aID));
+  is(y, expectedY, "Wrong y coordinate of " + prettyName(aID));
+  is(width, expectedWidth, "Wrong width of " + prettyName(aID));
+  is(height, expectedHeight, "Wrong height of " + prettyName(aID));
 }
 
 /**
@@ -80,24 +79,21 @@ function getBounds(aID)
 }
 
 /**
- * Return DOM node coordinates relative screen as pair (x, y).
+ * Return DOM node coordinates relative the screen and its size in device
+ * pixels.
  */
-function getScreenCoords(aNode)
+function getBoundsForDOMElm(aID)
 {
-  if (aNode instanceof nsIDOMXULElement)
-    return [node.boxObject.screenX, node.boxObject.screenY];
+  var elm = getNode(aID);
+  var elmWindow = elm.ownerDocument.defaultView;
+  var winUtil = elmWindow.
+    QueryInterface(Components.interfaces.nsIInterfaceRequestor).
+    getInterface(Components.interfaces.nsIDOMWindowUtils);
 
-  // Ugly hack until bug 486200 is fixed.
-  const XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
-  var descr = document.createElementNS(XUL_NS, "description");
-  descr.setAttribute("value", "helper description");
-  aNode.parentNode.appendChild(descr);
-  var descrBoxObject = descr.boxObject;
-  var descrRect = descr.getBoundingClientRect();
-  var deltaX = descrBoxObject.screenX - descrRect.left;
-  var deltaY = descrBoxObject.screenY - descrRect.top;
-  aNode.parentNode.removeChild(descr);
-
-  var rect = aNode.getBoundingClientRect();
-  return [rect.left + deltaX, rect.top + deltaY];
+  var ratio = winUtil.screenPixelsPerCSSPixel;
+  var rect = elm.getBoundingClientRect();
+  return [ (rect.left + elmWindow.mozInnerScreenX) * ratio,
+           (rect.top + elmWindow.mozInnerScreenY) * ratio,
+           rect.width * ratio,
+           rect.height * ratio ];
 }
