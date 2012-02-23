cb302a5c1b1dd3ebf8695bd49b2d436c7914f635
Kartikaya Gupta <kgupta@mozilla.com>
2012-02-23 14:55:15 -0500

    Bug 730033 - Rip out WidgetTileLayer and associated gunk. r=snorp

--- mobile/android/base/gfx/GeckoLayerClient.java
+++ mobile/android/base/gfx/GeckoLayerClient.java
@@ -127,9 +127,8 @@ public class GeckoLayerClient implements GeckoEventListener,
 
     /** This function is invoked by Gecko via JNI; be careful when modifying signature. */
     public Rect beginDrawing(int width, int height, int tileWidth, int tileHeight,
-                             String metadata, boolean hasDirectTexture) {
-        Log.e(LOGTAG, "### beginDrawing " + width + " " + height + " " + tileWidth + " " +
-              tileHeight + " " + hasDirectTexture);
+                             String metadata) {
+        Log.e(LOGTAG, "### beginDrawing " + width + " " + height + " " + tileWidth + " " + tileHeight);
 
         // If the viewport has changed but we still don't have the latest viewport
         // from Gecko, ignore the viewport passed to us from Gecko since that is going
@@ -140,8 +139,8 @@ public class GeckoLayerClient implements GeckoEventListener,
         mFirstPaint = false;
 
         // If we've changed surface types, cancel this draw
-        if (handleDirectTextureChange(hasDirectTexture)) {
-            Log.e(LOGTAG, "### Cancelling draw due to direct texture change");
+        if (initializeVirtualLayer()) {
+            Log.e(LOGTAG, "### Cancelling draw due to virtual layer initialization");
             return null;
         }
 
@@ -318,7 +317,7 @@ public class GeckoLayerClient implements GeckoEventListener,
         return Color.rgb(r, g, b);
     }
 
-    private boolean handleDirectTextureChange(boolean hasDirectTexture) {
+    private boolean initializeVirtualLayer() {
         if (mTileLayer != null) {
             return false;
         }
