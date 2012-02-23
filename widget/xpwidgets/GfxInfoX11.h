758e803cd9a7f9c9e639610c6272c5994a7b7014
Benoit Jacob <bjacob@mozilla.com>
2012-02-23 17:23:29 -0500

    Bug 729817 - Block the Nouveau 3D driver as it's installed by default on OpenSuse 12.1 and crashes - r=mattwoodrow

--- widget/xpwidgets/GfxInfoX11.h
+++ widget/xpwidgets/GfxInfoX11.h
@@ -98,7 +98,7 @@ private:
   nsCString mRenderer;
   nsCString mVersion;
   nsCString mAdapterDescription;
-  bool mIsMesa, mIsNVIDIA, mIsFGLRX;
+  bool mIsMesa, mIsNVIDIA, mIsFGLRX, mIsNouveau;
   bool mHasTextureFromPixmap;
   int mGLMajorVersion, mMajorVersion, mMinorVersion, mRevisionVersion;
 
