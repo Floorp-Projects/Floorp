84243047e92a91f5dc32223d97d62a0e444204fe
Cedric Vivier <cedricv@neonux.com>
2012-02-23 04:15:00 +0100

    Bug 719552 - Scheme-less URLs references in the style sheet are lost when editing. r=rcampbell

--- browser/devtools/styleeditor/test/browser_styleeditor_new.js
+++ browser/devtools/styleeditor/test/browser_styleeditor_new.js
@@ -35,6 +35,10 @@ let gNewEditor;       // to make sure only one new stylesheet got created
 let gUpdateCount = 0; // to make sure only one Update event is triggered
 let gCommitCount = 0; // to make sure only one Commit event is triggered
 let gTransitionEndCount = 0;
+let gOriginalStyleSheet;
+let gOriginalOwnerNode;
+let gOriginalHref;
+
 
 function finishOnTransitionEndAndCommit() {
   if (gCommitCount && gTransitionEndCount) {
@@ -42,7 +46,17 @@ function finishOnTransitionEndAndCommit() {
     is(gCommitCount, 1, "received one Commit event");
     is(gTransitionEndCount, 1, "received one transitionend event");
 
-    finish();
+    if (gNewEditor) {
+      is(gNewEditor.styleSheet, gOriginalStyleSheet,
+         "style sheet object did not change");
+      is(gNewEditor.styleSheet.ownerNode, gOriginalOwnerNode,
+         "style sheet owner node did not change");
+      is(gNewEditor.styleSheet.href, gOriginalHref,
+         "style sheet href did not change");
+
+      gNewEditor = null;
+      finish();
+    }
   }
 }
 
@@ -68,6 +82,10 @@ function testEditorAdded(aChrome, aEditor)
   let listener = {
     onAttach: function (aEditor) {
       waitForFocus(function () {
+        gOriginalStyleSheet = aEditor.styleSheet;
+        gOriginalOwnerNode = aEditor.styleSheet.ownerNode;
+        gOriginalHref = aEditor.styleSheet.href;
+
         ok(aEditor.isLoaded,
            "new editor is loaded when attached");
         ok(aEditor.hasFlag("new"),
@@ -142,7 +160,6 @@ function testEditorAdded(aChrome, aEditor)
          "StyleEditor's transition class has been removed from content");
 
       aEditor.removeActionListener(listener);
-      gNewEditor = null;
 
       executeSoon(finishOnTransitionEndAndCommit);
     }
