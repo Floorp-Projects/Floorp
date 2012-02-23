8c2a09d3b4b6c1aba7985fb30fc82071c0ed9fc6
Kartikaya Gupta <kgupta@mozilla.com>
2012-02-23 10:09:38 -0500

    Re-land a fixed c8451607557b (Take out the empty GeckoLayerClient.dimensionsChanged method and the unneeded support architecture for it)

--- mobile/android/base/gfx/VirtualLayer.java
+++ mobile/android/base/gfx/VirtualLayer.java
@@ -38,15 +38,8 @@
 
 package org.mozilla.gecko.gfx;
 
-import android.graphics.Point;
-
 public class VirtualLayer extends Layer {
-    private Listener mListener;
     private IntSize mSize;
-
-    public void setListener(Listener listener) {
-        mListener = listener;
-    }
 
     @Override
     public void draw(RenderContext context) {
@@ -60,21 +53,6 @@ public class VirtualLayer extends Layer {
 
     public void setSize(IntSize size) {
         mSize = size;
-    }
-
-    @Override
-    protected boolean performUpdates(RenderContext context) {
-        boolean dimensionsChanged = dimensionChangesPending();
-        boolean result = super.performUpdates(context);
-        if (dimensionsChanged && mListener != null) {
-            mListener.dimensionsChanged(getOrigin(), getResolution());
-        }
-
-        return result;
-    }
-
-    public interface Listener {
-        void dimensionsChanged(Point newOrigin, float newResolution);
     }
 }
 
