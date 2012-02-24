05c71c7232851afaad484bc8e4cf5aeb3aa9ac59
Jeff Gilbert <jgilbert@mozilla.com>
2012-02-24 15:10:18 -0800

    Bug 727178 - Revert ANGLE path to using glFinish to resolve - r=bjacob

--- content/canvas/src/WebGLContext.cpp
+++ content/canvas/src/WebGLContext.cpp
@@ -489,15 +489,9 @@ WebGLContext::SetDimensions(PRInt32 width, PRInt32 height)
     // if we want EGL, try it now
     if (!gl && (preferEGL || useANGLE) && !preferOpenGL) {
         gl = gl::GLContextProviderEGL::CreateOffscreen(gfxIntSize(width, height), format);
-        if (gl) {
-            if (InitAndValidateGL()) {
-                if (useANGLE) {
-                    gl->SetFlushGuaranteesResolve(true);
-                }
-            } else {
-                LogMessage("Error during ANGLE OpenGL ES initialization");
-                return NS_ERROR_FAILURE;
-            }
+        if (gl && !InitAndValidateGL()) {
+            LogMessage("Error during ANGLE OpenGL ES initialization");
+            return NS_ERROR_FAILURE;
         }
     }
 #endif
