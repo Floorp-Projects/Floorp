c5a42c79373faa2bd16377688409dc855bfe54d6
Benoit Jacob <bjacob@mozilla.com>
2012-02-27 16:33:19 -0500

    Bug 711656 - report force-enabled features in crash reports - r=jrmuizel
    Force-enabled features will be reported with a '!' instead of the usual '?' in AppNotes in crash reports.

--- widget/xpwidgets/nsBaseWidget.cpp
+++ widget/xpwidgets/nsBaseWidget.cpp
@@ -115,6 +115,7 @@ nsBaseWidget::nsBaseWidget()
 , mBorderStyle(eBorderStyle_none)
 , mOnDestroyCalled(false)
 , mUseAcceleratedRendering(false)
+, mForceLayersAcceleration(false)
 , mTemporarilyUseBasicLayerManager(false)
 , mBounds(0,0,0,0)
 , mOriginalBounds(nsnull)
@@ -795,7 +796,7 @@ nsBaseWidget::GetShouldAccelerate()
   // we should use AddBoolPrefVarCache
   bool disableAcceleration =
     Preferences::GetBool("layers.acceleration.disabled", false);
-  bool forceAcceleration =
+  mForceLayersAcceleration =
     Preferences::GetBool("layers.acceleration.force-enabled", false);
 
   const char *acceleratedEnv = PR_GetEnv("MOZ_ACCELERATED");
@@ -828,7 +829,7 @@ nsBaseWidget::GetShouldAccelerate()
   if (disableAcceleration || safeMode)
     return false;
 
-  if (forceAcceleration)
+  if (mForceLayersAcceleration)
     return true;
   
   if (!whitelisted) {
@@ -906,7 +907,8 @@ LayerManager* nsBaseWidget::GetLayerManager(PLayersChild* aShadowManager,
          * platforms on LayerManagerOGL should ensure their widget is able to
          * deal with it though!
          */
-        if (layerManager->Initialize()) {
+
+        if (layerManager->Initialize(mForceLayersAcceleration)) {
           mLayerManager = layerManager;
         }
       }
