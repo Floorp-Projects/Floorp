f02bce8ce10cc9abee1edda04ac5a56403db63b3
Kartikaya Gupta <kgupta@mozilla.com>
2012-02-27 10:13:36 -0500

    Bug 730710 - Add a GeckoEventResponder interface to allow event listeners to respond to events as well. r=blassey

---
+++ mobile/android/base/GeckoEventResponder.java
@@ -0,0 +1,14 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * ***** BEGIN LICENSE BLOCK *****
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.mozilla.gecko;
+
+public interface GeckoEventResponder extends GeckoEventListener {
+    public String getResponse();
+}
