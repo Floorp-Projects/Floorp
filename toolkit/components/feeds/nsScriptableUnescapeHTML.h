3ec758cd7cd9a7abe1b3359bb666d41cae73bd96
Henri Sivonen <hsivonen@iki.fi>
2012-02-27 13:57:48 +0200

    Bug 650784 part 1 - Introduce a new API for converting HTML to plain text. r=smaug.

--- toolkit/components/feeds/nsScriptableUnescapeHTML.h
+++ toolkit/components/feeds/nsScriptableUnescapeHTML.h
@@ -38,12 +38,15 @@
 #define nsScriptableHTMLUnescape_h__
 
 #include "nsIScriptableUnescapeHTML.h"
+#include "nsIParserUtils.h"
 
-class nsScriptableUnescapeHTML : public nsIScriptableUnescapeHTML
+class nsScriptableUnescapeHTML : public nsIScriptableUnescapeHTML,
+                                 public nsIParserUtils
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSISCRIPTABLEUNESCAPEHTML
+  NS_DECL_NSIPARSERUTILS
 };
 
 #endif // nsScriptableHTMLUnescape_h__
