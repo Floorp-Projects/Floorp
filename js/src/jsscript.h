12cf66dff4f81fee9de5740768363c0f138ccbc5
Steve Fink <sfink@mozilla.com>
2012-02-27 13:18:30 -0800

    Bug 723712 - Allow entering a compartment with a cached eval script. r=jorendorff

--- js/src/jsscript.h
+++ js/src/jsscript.h
@@ -660,7 +660,7 @@ struct JSScript : public js::gc::Cell
 
     /* Return creation time global or null. */
     js::GlobalObject *getGlobalObjectOrNull() const {
-        return isCachedEval ? NULL : globalObject.get();
+        return (isCachedEval || isActiveEval) ? NULL : globalObject.get();
     }
 
   private:
