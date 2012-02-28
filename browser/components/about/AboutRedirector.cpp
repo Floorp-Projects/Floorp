7c60a74eeff29ef8c7bdaa3c504f65f696f4f0ad
Dão Gottwald <dao@mozilla.com>
2012-02-28 17:03:35 +0100

    Bug 730271 - Create sync folder for Sync-related content files. r=mak

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -97,9 +97,9 @@ static RedirEntry kRedirMap[] = {
   { "sessionrestore", "chrome://browser/content/aboutSessionRestore.xhtml",
     nsIAboutModule::ALLOW_SCRIPT },
 #ifdef MOZ_SERVICES_SYNC
-  { "sync-progress", "chrome://browser/content/syncProgress.xhtml",
+  { "sync-progress", "chrome://browser/content/sync/progress.xhtml",
     nsIAboutModule::ALLOW_SCRIPT },
-  { "sync-tabs", "chrome://browser/content/aboutSyncTabs.xul",
+  { "sync-tabs", "chrome://browser/content/sync/aboutSyncTabs.xul",
     nsIAboutModule::ALLOW_SCRIPT },
 #endif
   { "home", "chrome://browser/content/aboutHome.xhtml",
