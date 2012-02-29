a56c056d48802b4a3ac8c8ccd287ef5a5ac03a17
Josh Matthews <josh@joshmatthews.net>
2012-02-29 12:11:03 -0500

    Bug 677194 - Change assertion to an early-return bailout. r=jst, a=akeybl

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -6100,7 +6100,10 @@ nsDOMConstructor::HasInstance(nsIXPConnectWrappedNative *wrapper,
       return NS_ERROR_UNEXPECTED;
     }
 
-    JS_ASSERT(!JSVAL_IS_PRIMITIVE(val));
+    if (JSVAL_IS_PRIMITIVE(val)) {
+      return NS_OK;
+    }
+
     JSObject *dot_prototype = JSVAL_TO_OBJECT(val);
 
     JSObject *proto = JS_GetPrototype(dom_obj);
