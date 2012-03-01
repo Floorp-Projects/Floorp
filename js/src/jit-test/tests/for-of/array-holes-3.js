328839e30c1bbe70559530b91dc55666771bfaa8
Jason Orendorff <jorendorff@mozilla.com>
2012-03-01 09:01:45 -0600

    Element iterators should use [[Get]] and not peculiarly ignore the prototype chain. Bug 725168, r=Waldo.

--- js/src/jit-test/tests/for-of/array-holes-3.js
+++ js/src/jit-test/tests/for-of/array-holes-3.js
@@ -1,8 +1,8 @@
-// for-of does not consult Array.prototype when it encounters a hole.
+// for-of consults Array.prototype when it encounters a hole.
 
-Array.prototype[1] = 'FAIL';
+Array.prototype[1] = 'peek';
 var log = [];
 for (var x of [0, , 2, 3])
     log.push(x);
-assertEq(log[1], undefined);
-assertEq(log.join(), "0,,2,3");
+assertEq(log[1], 'peek');
+assertEq(log.join(), "0,peek,2,3");
