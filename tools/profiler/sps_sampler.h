fbbac8e7ccb27644df6b1d56e3fc8152e39fe387
Ehsan Akhgari <ehsan@mozilla.com>
2012-03-02 14:11:47 -0500

    Bug 734302 - Part 1: Enable the Gecko Profiler on native Fennec; r=BenWa,khuey

--- tools/profiler/sps_sampler.h
+++ tools/profiler/sps_sampler.h
@@ -89,6 +89,20 @@ extern bool stack_key_initialized;
 #warning Please add support for your architecture in chromium_types.h
 #endif
 
+#define PROFILE_DEFAULT_ENTRY 100000
+#ifdef ANDROID
+// We use a lower frequency on Android, in order to make things work
+// more smoothly on phones.  This value can be adjusted later with
+// some libunwind optimizations.
+// In one sample measurement on Galaxy Nexus, out of about 700 backtraces,
+// 60 of them took more than 25ms, and the average and standard deviation
+// were 6.17ms and 9.71ms respectively.
+#define PROFILE_DEFAULT_INTERVAL 25
+#else
+#define PROFILE_DEFAULT_INTERVAL 10
+#endif
+#define PROFILE_DEFAULT_FEATURES NULL
+#define PROFILE_DEFAULT_FEATURE_COUNT 0
 
 // STORE_SEQUENCER: Because signals can interrupt our profile modification
 //                  we need to make stores are not re-ordered by the compiler
