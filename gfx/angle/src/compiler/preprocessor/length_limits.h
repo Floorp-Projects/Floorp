4a75ad60244f2fc26a58851ee4c68f513d302781
Benoit Jacob <bjacob@mozilla.com>
2012-03-02 15:42:49 -0500

    Bug 676071 - drop ANGLE patch limiting identifiers to 250 chars - r=jgilbert

--- gfx/angle/src/compiler/preprocessor/length_limits.h
+++ gfx/angle/src/compiler/preprocessor/length_limits.h
@@ -15,9 +15,7 @@
 // make it easier to reference them from the compiler sources.
 
 // These lengths do not include the NULL terminator.
-// see bug 675625: NVIDIA driver crash with lengths >= 253
-// this is only an interim fix, the real fix is name mapping, see ANGLE bug 144 / r619
-#define MAX_SYMBOL_NAME_LEN 250
+#define MAX_SYMBOL_NAME_LEN 256
 #define MAX_STRING_LEN 511
 
 #endif // !(defined(__LENGTH_LIMITS_H)
