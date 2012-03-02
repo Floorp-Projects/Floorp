d31e74051ffe575be5e5acd8c5b34d7319ab168d
Marty Rosenberg <mrosenberg@mozilla.com>
2012-03-02 09:34:46 -0800

    Fix GVN by implementing congruence classes as a list (bug 723536, r=dvander)

--- js/src/ion/MIR.h
+++ js/src/ion/MIR.h
@@ -61,7 +61,7 @@
 
 namespace js {
 namespace ion {
-
+class ValueNumberData;
 static const inline
 MIRType MIRTypeFromValue(const js::Value &vp)
 {
@@ -255,7 +255,7 @@ class MDefinition : public MNode
     InlineForwardList<MUse> uses_; // Use chain.
     uint32 id_;                    // Instruction ID, which after block re-ordering
                                    // is sorted within a basic block.
-    uint32 valueNumber_;           // The instruction's value number (see GVN for details in use)
+    ValueNumberData *valueNumber_; // The instruction's value number (see GVN for details in use)
     MIRType resultType_;           // Representation of result type.
     uint32 flags_;                 // Bit flags.
     MDefinition *dependency_;      // Implicit dependency (store, call, etc.) of this instruction.
@@ -301,7 +301,7 @@ class MDefinition : public MNode
   public:
     MDefinition()
       : id_(0),
-        valueNumber_(0),
+        valueNumber_(NULL),
         resultType_(MIRType_None),
         flags_(0),
         dependency_(NULL)
@@ -334,12 +334,13 @@ class MDefinition : public MNode
         id_ = id;
     }
 
-    uint32 valueNumber() const {
-        JS_ASSERT(block_);
+    uint32 valueNumber() const;
+    void setValueNumber(uint32 vn);
+    ValueNumberData *valueNumberData() {
         return valueNumber_;
     }
-
-    void setValueNumber(uint32 vn) {
+    void setValueNumberData(ValueNumberData *vn) {
+        JS_ASSERT(valueNumber_ == NULL);
         valueNumber_ = vn;
     }
 #define FLAG_ACCESSOR(flag) \
