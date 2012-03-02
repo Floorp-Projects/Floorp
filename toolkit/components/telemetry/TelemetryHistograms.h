7233fee884b3d10005e9c6cc0e3c9983214e2f51
Nathan Froyd <froydnj@mozilla.com>
2012-03-02 09:59:38 -0500

    Bug 723846 - Part 2: add FlagHistograms to nsITelemetry. r=taras

--- toolkit/components/telemetry/TelemetryHistograms.h
+++ toolkit/components/telemetry/TelemetryHistograms.h
@@ -51,6 +51,8 @@
 
 /* Convenience macro for BOOLEAN histograms. */
 #define HISTOGRAM_BOOLEAN(id, message) HISTOGRAM(id, 0, 1, 2, BOOLEAN, message)
+/* Likewise for FLAG histograms. */
+#define HISTOGRAM_FLAG(id, message) HISTOGRAM(id, 0, 1, 2, FLAG, message)
 
 /**
  * a11y telemetry
@@ -414,3 +416,4 @@ HISTOGRAM(TOTAL_COUNT_HIGH_ERRORS, 1, 3000, 10, EXPONENTIAL, "Number of histogra
 HISTOGRAM(TOTAL_COUNT_LOW_ERRORS, 1, 3000, 10, EXPONENTIAL, "Number of histograms with total count low errors")
 
 #undef HISTOGRAM_BOOLEAN
+#undef HISTOGRAM_FLAG
