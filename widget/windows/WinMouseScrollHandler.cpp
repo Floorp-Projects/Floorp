b284f3709f1b038900e0cbd57090ec9499bddef0
Masayuki Nakano <masayuki@d-toybox.com>
2012-03-06 12:20:28 +0900

    Bug 672175 part.2 MouseScrollHandler should manage system settings r=jimm

--- widget/windows/WinMouseScrollHandler.cpp
+++ widget/windows/WinMouseScrollHandler.cpp
@@ -11,6 +11,7 @@
 
 #include "WinMouseScrollHandler.h"
 #include "nsWindow.h"
+#include "WinUtils.h"
 
 namespace mozilla {
 namespace widget {
@@ -77,9 +78,86 @@ MouseScrollHandler::ProcessMessage(nsWindow* aWindow, UINT msg,
                                    WPARAM wParam, LPARAM lParam,
                                    LRESULT *aRetValue, bool &aEatMessage)
 {
-  return false;
+  switch (msg) {
+    case WM_SETTINGCHANGE:
+      if (!sInstance) {
+        return false;
+      }
+      if (wParam == SPI_SETWHEELSCROLLLINES ||
+          wParam == SPI_SETWHEELSCROLLCHARS) {
+        sInstance->mSystemSettings.MarkDirty();
+      }
+      return false;
+    default:
+      return false;
+  }
 }
 
+/******************************************************************************
+ *
+ * SystemSettings
+ *
+ ******************************************************************************/
+
+void
+MouseScrollHandler::SystemSettings::Init()
+{
+  if (mInitialized) {
+    return;
+  }
+
+  mInitialized = true;
+
+  if (!::SystemParametersInfo(SPI_GETWHEELSCROLLLINES, 0, &mScrollLines, 0)) {
+    PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+      ("MouseScroll::SystemSettings::Init(): ::SystemParametersInfo("
+         "SPI_GETWHEELSCROLLLINES) failed"));
+    mScrollLines = 3;
+  } else if (mScrollLines > WHEEL_DELTA) {
+    PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+      ("MouseScroll::SystemSettings::Init(): the result of "
+         "::SystemParametersInfo(SPI_GETWHEELSCROLLLINES) is too large: %d",
+       mScrollLines));
+    // sScrollLines usually equals 3 or 0 (for no scrolling)
+    // However, if sScrollLines > WHEEL_DELTA, we assume that
+    // the mouse driver wants a page scroll.  The docs state that
+    // sScrollLines should explicitly equal WHEEL_PAGESCROLL, but
+    // since some mouse drivers use an arbitrary large number instead,
+    // we have to handle that as well.
+    mScrollLines = WHEEL_PAGESCROLL;
+  }
+
+  if (!::SystemParametersInfo(SPI_GETWHEELSCROLLCHARS, 0, &mScrollChars, 0)) {
+    PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+      ("MouseScroll::SystemSettings::Init(): ::SystemParametersInfo("
+         "SPI_GETWHEELSCROLLCHARS) failed, %s",
+       WinUtils::GetWindowsVersion() >= WinUtils::VISTA_VERSION ?
+         "this is unexpected on Vista or later" :
+         "but on XP or earlier, this is not a problem"));
+    mScrollChars = 1;
+  } else if (mScrollChars > WHEEL_DELTA) {
+    PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+      ("MouseScroll::SystemSettings::Init(): the result of "
+         "::SystemParametersInfo(SPI_GETWHEELSCROLLCHARS) is too large: %d",
+       mScrollChars));
+    // See the comments for the case mScrollLines > WHEEL_DELTA.
+    mScrollChars = WHEEL_PAGESCROLL;
+  }
+
+  PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+    ("MouseScroll::SystemSettings::Init(): initialized, "
+       "mScrollLines=%d, mScrollChars=%d",
+     mScrollLines, mScrollChars));
+}
+
+void
+MouseScrollHandler::SystemSettings::MarkDirty()
+{
+  PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+    ("MouseScrollHandler::SystemSettings::MarkDirty(): "
+       "Marking SystemSettings dirty"));
+  mInitialized = false;
+}
 
 } // namespace widget
 } // namespace mozilla
