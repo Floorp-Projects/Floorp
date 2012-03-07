fa303f6190eacf1d117443749d300644349f1382
Philipp von Weitershausen <philipp@weitershausen.de>
2012-03-07 14:14:37 -0800

    Bug 733674 - B2G SMS: Fix off-by-one errors in timestamp computation. r=qDot

--- dom/system/b2g/ril_worker.js
+++ dom/system/b2g/ril_worker.js
@@ -2850,10 +2850,10 @@ let GsmPDUHelper = {
       // - TP-Service-Center-Time-Stamp -
       let year   = this.readSwappedNibbleBCD(1) + PDU_TIMESTAMP_YEAR_OFFSET;
       let month  = this.readSwappedNibbleBCD(1) - 1;
-      let day    = this.readSwappedNibbleBCD(1) - 1;
-      let hour   = this.readSwappedNibbleBCD(1) - 1;
-      let minute = this.readSwappedNibbleBCD(1) - 1;
-      let second = this.readSwappedNibbleBCD(1) - 1;
+      let day    = this.readSwappedNibbleBCD(1);
+      let hour   = this.readSwappedNibbleBCD(1);
+      let minute = this.readSwappedNibbleBCD(1);
+      let second = this.readSwappedNibbleBCD(1);
       msg.timestamp = Date.UTC(year, month, day, hour, minute, second);
 
       // If the most significant bit of the least significant nibble is 1,
