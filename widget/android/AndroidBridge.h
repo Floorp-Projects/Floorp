b40c86ca32aeecd69a99bc66d84158afd3486a80
Eitan Isaacson <eitan@monotonous.org>
2012-03-07 19:24:38 -0500

    Bug 729237 - Fixed AndroidBridge emit a11y event function, added java string class as class member. r=blassey

--- widget/android/AndroidBridge.h
+++ widget/android/AndroidBridge.h
@@ -357,7 +357,7 @@ public:
 
     nsCOMPtr<nsIAndroidDrawMetadataProvider> GetDrawMetadataProvider();
 
-    void EmitGeckoAccessibilityEvent (PRInt32 eventType, const nsAString& role, const nsAString& text, const nsAString& description, bool enabled, bool checked, bool password);
+    void EmitGeckoAccessibilityEvent (PRInt32 eventType, const nsTArray<nsString>& text, const nsAString& description, bool enabled, bool checked, bool password);
 
     void CheckURIVisited(const nsAString& uri);
     void MarkURIVisited(const nsAString& uri);
@@ -497,6 +497,9 @@ protected:
     jclass jEGLDisplayImplClass;
     jclass jEGLContextClass;
     jclass jEGL10Class;
+
+    // some convinient types to have around
+    jclass jStringClass;
 
     // calls we've dlopened from libjnigraphics.so
     int (* AndroidBitmap_getInfo)(JNIEnv *env, jobject bitmap, void *info);
