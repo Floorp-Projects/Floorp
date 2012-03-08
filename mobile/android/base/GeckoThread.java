500986fc9c5ac6a29a546ff0439ce268c253f8a2
Wes Johnston <wjohnston@mozilla.com>
2012-03-08 10:25:44 -0800

    Bug 718760 - Crypto for the java passwords provider. r=blassey,bsmith

--- mobile/android/base/GeckoThread.java
+++ mobile/android/base/GeckoThread.java
@@ -90,6 +90,7 @@ public class GeckoThread extends Thread {
         String resourcePath = app.getApplication().getPackageResourcePath();
         GeckoAppShell.setupGeckoEnvironment(app);
         GeckoAppShell.loadSQLiteLibs(app, resourcePath);
+        GeckoAppShell.loadNSSLibs(app, resourcePath);
         GeckoAppShell.loadGeckoLibs(resourcePath);
 
         Locale.setDefault(locale);
@@ -111,5 +112,6 @@ public class GeckoThread extends Thread {
         } catch (Exception e) {
             GeckoAppShell.reportJavaCrash(e);
         }
+
     }
 }
