d2e74c91c93932165fc3eecfe1fb4693e0c47c9b
Chris Peterson <cpeterson@mozilla.com>
2012-03-08 10:08:15 -0800

    Bug 734156 - Part 1: Backout ineffective workaround d923ae85be05 (Reset IME selection when Fennec resumes). r=blassey

--- mobile/android/base/GeckoAppShell.java
+++ mobile/android/base/GeckoAppShell.java
@@ -621,12 +621,6 @@ public class GeckoAppShell
         mInputConnection.returnIMEQueryResult(result, selectionStart, selectionLength);
     }
 
-    public static void resetIMESelection() {
-        if (mInputConnection != null) {
-            mInputConnection.resetSelection();
-        }
-    }
-
     static void onXreExit() {
         // mLaunchState can only be Launched or GeckoRunning at this point
         GeckoApp.setLaunchState(GeckoApp.LaunchState.GeckoExiting);
