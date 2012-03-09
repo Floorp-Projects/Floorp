63a7f34a84e86fd46a5a9a1da462ee95ca033ea7
Doug Turner <dougt@dougt.org>
2012-03-09 10:40:43 -0800

    Bug 734391 - coalesce the device motion events. r=jdm

--- dom/system/nsDeviceMotion.h
+++ dom/system/nsDeviceMotion.h
@@ -43,6 +43,10 @@
 #include "nsTArray.h"
 #include "nsCOMPtr.h"
 #include "nsITimer.h"
+#include "nsIDOMDeviceOrientationEvent.h"
+#include "nsIDOMDeviceMotionEvent.h"
+#include "nsDOMDeviceMotionEvent.h"
+#include "mozilla/TimeStamp.h"
 
 #define NS_DEVICE_MOTION_CID \
 { 0xecba5203, 0x77da, 0x465a, \
@@ -91,10 +95,16 @@ private:
                           double x,
                           double y,
                           double z);
-  bool mEnabled;
 
   virtual void Startup()  = 0;
   virtual void Shutdown() = 0;
+
+  bool mEnabled;
+  mozilla::TimeStamp mLastDOMMotionEventTime;
+  nsRefPtr<nsDOMDeviceAcceleration> mLastAcceleration;
+  nsRefPtr<nsDOMDeviceAcceleration> mLastAccelerationIncluduingGravity;
+  nsRefPtr<nsDOMDeviceRotationRate> mLastRotationRate;
+
 };
 
 #endif
