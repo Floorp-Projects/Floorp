948d9d8bd42cb9d61a5d78f73f9338a2474522c6
Richard Newman <rnewman@mozilla.com>
2012-03-09 15:53:23 -0800

    Bug 709403 - Parts 3-6: tabs repository and stage. r=nalexander

---
+++ mobile/android/base/sync/repositories/NoContentProviderException.java
@@ -0,0 +1,25 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.sync.repositories;
+
+import org.mozilla.gecko.sync.SyncException;
+
+import android.net.Uri;
+
+/**
+ * Raised when a Content Provider cannot be retrieved.
+ *
+ * @author rnewman
+ *
+ */
+public class NoContentProviderException extends SyncException {
+  private static final long serialVersionUID = 1L;
+
+  public final Uri requestedProvider;
+  public NoContentProviderException(Uri requested) {
+    super();
+    this.requestedProvider = requested;
+  }
+}
