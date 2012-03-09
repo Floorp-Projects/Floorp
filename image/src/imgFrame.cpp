74cb6a16da5efc63246ceb0e306c357222dcdbd0
Justin Lebar <justin.lebar@gmail.com>
2012-03-09 17:21:01 -0500

    Bug 732820 - Part 2: Cap the amount of discardable image data we'll willingly keep around. r=joe

--- image/src/imgFrame.cpp
+++ image/src/imgFrame.cpp
@@ -37,6 +37,7 @@
  * ***** END LICENSE BLOCK ***** */
 
 #include "imgFrame.h"
+#include "DiscardTracker.h"
 
 #include <limits.h>
 
@@ -66,6 +67,8 @@ static PRUint32 gTotalDDBSize = 0;
 #define kMaxSingleDDBSize (4*1024*1024)
 
 #endif
+
+using namespace mozilla::image;
 
 // Returns true if an image of aWidth x aHeight is allowed and legal.
 static bool AllowedImageSize(PRInt32 aWidth, PRInt32 aHeight)
@@ -147,6 +150,7 @@ imgFrame::imgFrame() :
   , mIsDDBSurface(false)
 #endif
   , mLocked(false)
+  , mInformedDiscardTracker(false)
 {
   static bool hasCheckedOptimize = false;
   if (!hasCheckedOptimize) {
@@ -166,6 +170,10 @@ imgFrame::~imgFrame()
       gTotalDDBSize -= mSize.width * mSize.height * 4;
   }
 #endif
+
+  if (mInformedDiscardTracker) {
+    DiscardTracker::InformAllocation(-4 * mSize.height * mSize.width);
+  }
 }
 
 nsresult imgFrame::Init(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight, 
@@ -227,6 +235,14 @@ nsresult imgFrame::Init(PRInt32 aX, PRInt32 aY, PRInt32 aWidth, PRInt32 aHeight,
 #endif
   }
 
+  // Inform the discard tracker that we've allocated some memory, but only if
+  // we're not a paletted image (paletted images are not usually large and are
+  // used only for animated frames, which we don't discard).
+  if (!mPalettedImageData) {
+    DiscardTracker::InformAllocation(4 * mSize.width * mSize.height);
+    mInformedDiscardTracker = true;
+  }
+
   return NS_OK;
 }
 
@@ -271,6 +287,14 @@ nsresult imgFrame::Optimize()
 #ifdef XP_MACOSX
         mQuartzSurface = nsnull;
 #endif
+
+        // We just dumped most of our allocated memory, so tell the discard
+        // tracker that we're not using any at all.
+        if (mInformedDiscardTracker) {
+          DiscardTracker::InformAllocation(-4 * mSize.width * mSize.height);
+          mInformedDiscardTracker = false;
+        }
+
         return NS_OK;
       }
     }
