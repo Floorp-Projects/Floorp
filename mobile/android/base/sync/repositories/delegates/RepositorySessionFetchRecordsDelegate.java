26bb7e529918c85d3c3a6b7f67e62835113fa52f
Richard Newman <rnewman@mozilla.com>
2012-03-08 22:43:16 -0800

    Bug 734316 - Part 4: incorrect JavaDoc parameter, commenting, MPL in RepositorySessionFetchRecordsDelegate.java. r=trivial

--- mobile/android/base/sync/repositories/delegates/RepositorySessionFetchRecordsDelegate.java
+++ mobile/android/base/sync/repositories/delegates/RepositorySessionFetchRecordsDelegate.java
@@ -1,40 +1,6 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Android Sync Client.
- *
- * The Initial Developer of the Original Code is
- * the Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- * Jason Voll <jvoll@mozilla.com>
- * Richard Newman <rnewman@mozilla.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package org.mozilla.gecko.sync.repositories.delegates;
 
@@ -49,7 +15,7 @@ public interface RepositorySessionFetchRecordsDelegate {
   /**
    * Called when all records in this fetch have been returned.
    *
-   * @param end
+   * @param fetchEnd
    *        A millisecond-resolution timestamp indicating the *remote* timestamp
    *        at the end of the range of records. Usually this is the timestamp at
    *        which the request was received.
@@ -57,8 +23,19 @@ public interface RepositorySessionFetchRecordsDelegate {
    */
   public void onFetchCompleted(final long fetchEnd);
 
-  // Shorthand for calling onFetchedRecord for each record in turn, then
-  // calling onFetchCompleted.
+  /**
+   * Shorthand for calling onFetchedRecord for each record in turn, then
+   * calling onFetchCompleted.
+   *
+   * @param records
+   *        An array of fetched records. Can be empty.
+   *
+   * @param fetchEnd
+   *        A millisecond-resolution timestamp indicating the *remote* timestamp
+   *        at the end of the range of records. Usually this is the timestamp at
+   *        which the request was received.
+   *        E.g., the (normalized) value of the X-Weave-Timestamp header.
+   */
   public void onFetchSucceeded(Record[] records, final long fetchEnd);
 
   public RepositorySessionFetchRecordsDelegate deferredFetchDelegate(ExecutorService executor);
