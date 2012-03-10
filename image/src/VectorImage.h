c8dec52f59981ff89ae6e354c7bf5adc123a5464
Justin Lebar <justin.lebar@gmail.com>
2012-03-10 01:29:28 -0500

    Bug 731419 - Part 1: Discard image data immediately on tab close, imagelib changes. r=joe

--- image/src/VectorImage.h
+++ image/src/VectorImage.h
@@ -81,6 +81,7 @@ public:
   NS_SCRIPTABLE NS_IMETHOD RequestDecode(void);
   NS_SCRIPTABLE NS_IMETHOD LockImage(void);
   NS_SCRIPTABLE NS_IMETHOD UnlockImage(void);
+  NS_SCRIPTABLE NS_IMETHOD RequestDiscard(void);
   NS_SCRIPTABLE NS_IMETHOD ResetAnimation(void);
   NS_IMETHOD_(void) RequestRefresh(const mozilla::TimeStamp& aTime);
   // END NS_DECL_IMGICONTAINER
