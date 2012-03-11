26783a78d2aaedbfeb8c5fe625c9b71bd4cf76f7
Daniel Holbert <dholbert@cs.stanford.edu>
2012-03-11 16:43:12 -0700

    Backout 4b46f6eff8f4 (Bug 732988) for linux Cipc orange

--- gfx/layers/opengl/ImageLayerOGL.cpp
+++ gfx/layers/opengl/ImageLayerOGL.cpp
@@ -230,8 +230,6 @@ ImageLayerOGL::RenderLayer(int,
 
   NS_ASSERTION(image->GetFormat() != Image::REMOTE_IMAGE_BITMAP,
     "Remote images aren't handled yet in OGL layers!");
-  NS_ASSERTION(mScaleMode == SCALE_NONE || image->GetSize() == mScaleToSize,
-    "Scale modes other than none not handled yet in OGL layers!");
 
   if (image->GetFormat() == Image::PLANAR_YCBCR) {
     PlanarYCbCrImage *yuvImage =
