9a23d3054bd6d3984cce6ec50c515c0b2b6c1fa9
Jonathan Watt <jwatt@jwatt.org>
2012-03-11 23:04:37 +0000

    Bug 734079 - Give nsSVGSwitchElement::MaybeInvalidate a better name and comment. r=longsonr.

--- content/svg/content/src/nsSVGSwitchElement.h
+++ content/svg/content/src/nsSVGSwitchElement.h
@@ -58,8 +58,8 @@ protected:
 public:
   nsIContent * GetActiveChild() const
   { return mActiveChild; }
-  void MaybeInvalidate();
-    
+  void InvalidateIfActiveChildChanged();
+
   // interfaces:
 
   NS_DECL_ISUPPORTS_INHERITED
