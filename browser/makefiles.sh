d580ac7e7551f75df9267e3455757f96a4d5b45c
Ed Morley <bmo@edmorley.co.uk>
2012-03-11 23:38:53 +0000

    Backout ba7da6256b23 (bug 726444) for talos regressions; a=mak

--- browser/makefiles.sh
+++ browser/makefiles.sh
@@ -48,8 +48,6 @@ browser/components/about/Makefile
 browser/components/build/Makefile
 browser/components/certerror/Makefile
 browser/components/dirprovider/Makefile
-browser/components/downloads/Makefile
-browser/components/downloads/src/Makefile
 browser/components/feeds/Makefile
 browser/components/feeds/public/Makefile
 browser/components/feeds/src/Makefile
@@ -135,8 +133,6 @@ if [ "$ENABLE_TESTS" ]; then
     browser/base/content/test/newtab/Makefile
     browser/components/certerror/test/Makefile
     browser/components/dirprovider/tests/Makefile
-    browser/components/downloads/test/Makefile
-    browser/components/downloads/test/browser/Makefile
     browser/components/preferences/tests/Makefile
     browser/components/search/test/Makefile
     browser/components/sessionstore/test/Makefile
