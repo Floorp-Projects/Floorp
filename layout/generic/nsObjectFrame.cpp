25f6085512156d20451826da4afd36d2afeb8a13
Daniel Holbert <dholbert@cs.stanford.edu>
2012-03-11 16:41:39 -0700

    Backout f667f6c22bed (Bug 732988) for linux Cipc orange

--- layout/generic/nsObjectFrame.cpp
+++ layout/generic/nsObjectFrame.cpp
@@ -1549,13 +1549,7 @@ nsObjectFrame::BuildLayer(nsDisplayListBuilder* aBuilder,
     return nsnull;
   }
 
-  gfxIntSize size;
-  
-  if (mInstanceOwner->UseAsyncRendering()) {
-    size = container->GetCurrentSize();
-  } else {
-    size = gfxIntSize(window->width, window->height);
-  }
+  gfxIntSize size = container->GetCurrentSize();
 
   nsRect area = GetContentRectRelativeToSelf() + aItem->ToReferenceFrame();
   gfxRect r = nsLayoutUtils::RectToGfxRect(area, PresContext()->AppUnitsPerDevPixel());
@@ -1578,9 +1572,6 @@ nsObjectFrame::BuildLayer(nsDisplayListBuilder* aBuilder,
     ImageLayer* imglayer = static_cast<ImageLayer*>(layer.get());
     UpdateImageLayer(r);
 
-    if (!mInstanceOwner->UseAsyncRendering()) {
-      imglayer->SetScaleToSize(size, ImageLayer::SCALE_STRETCH);
-    }
     imglayer->SetContainer(container);
     gfxPattern::GraphicsFilter filter =
       nsLayoutUtils::GetGraphicsFilterForFrame(this);
