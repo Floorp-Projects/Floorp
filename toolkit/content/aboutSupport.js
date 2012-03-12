690efe5d3a104e3dec4bcdd88ff5633ea8a4385d
Kai Engert <kaie@kuix.de>
2012-03-13 00:04:37 +0100

    Bug 673381, patch v4 to add NSPR/NSS library version numbers to about:support, r=dtownsend

--- toolkit/content/aboutSupport.js
+++ toolkit/content/aboutSupport.js
@@ -119,6 +119,7 @@ window.onload = function () {
   populateExtensionsSection();
   populateGraphicsSection();
   populateJavaScriptSection();
+  populateLibVersionsSection();
 }
 
 function populateExtensionsSection() {
@@ -170,6 +171,41 @@ function populatePreferencesSection() {
   });
 
   appendChildren(document.getElementById("prefs-tbody"), trPrefs);
+}
+
+function populateLibVersionsSection() {
+  function pushInfoRow(table, name, value, value2)
+  {
+    table.push(createParentElement("tr", [
+      createElement("td", name),
+      createElement("td", value),
+      createElement("td", value2),
+    ]));
+  }
+    
+  var v = null;
+  try { // just to be safe
+    v = Cc["@mozilla.org/security/nssversion;1"].getService(Ci.nsINSSVersion);
+  } catch(e) {}
+  if (!v)
+    return;
+    
+  let bundle = Services.strings.createBundle("chrome://global/locale/aboutSupport.properties");
+  let libversions_tbody = document.getElementById("libversions-tbody");
+
+  let trLibs = [];
+  trLibs.push(createParentElement("tr", [
+    createElement("th", ""),
+    createElement("th", bundle.GetStringFromName("minLibVersions")),
+    createElement("th", bundle.GetStringFromName("loadedLibVersions")),
+  ]));
+  pushInfoRow(trLibs, "NSPR", v.NSPR_MinVersion, v.NSPR_Version);
+  pushInfoRow(trLibs, "NSS", v.NSS_MinVersion, v.NSS_Version);
+  pushInfoRow(trLibs, "NSS Util", v.NSSUTIL_MinVersion, v.NSSUTIL_Version);
+  pushInfoRow(trLibs, "NSS SSL", v.NSSSSL_MinVersion, v.NSSSSL_Version);
+  pushInfoRow(trLibs, "NSS S/MIME", v.NSSSMIME_MinVersion, v.NSSSMIME_Version);
+
+  appendChildren(libversions_tbody, trLibs);
 }
 
 function populateGraphicsSection() {
