68c641d2fd3b00147f341e00d6a14757aaa51661
Pallani Kumaran <pallanikumaran@gmail.com>
2012-03-13 02:12:47 +0800

    Bug 701967 - Misc code cleanup in Sync's util.js; r=gps

--- services/sync/tests/unit/test_resource.js
+++ services/sync/tests/unit/test_resource.js
@@ -495,8 +495,10 @@ function run_test() {
 
   let query = "?" + args.join("&");
 
-  let uri1 = Utils.makeURL("http://foo/" + query);
-  let uri2 = Utils.makeURL("http://foo/");
+  let uri1 = Utils.makeURI("http://foo/" + query)
+                  .QueryInterface(Ci.nsIURL);
+  let uri2 = Utils.makeURI("http://foo/")
+                  .QueryInterface(Ci.nsIURL);
   uri2.query = query;
   do_check_eq(uri1.query, uri2.query);
   server.stop(do_test_finished);
