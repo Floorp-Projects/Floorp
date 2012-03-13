45d469274633674976c06afdcdddbe243b10af9a
Nick Alexander <nalexander@mozilla.com>
2012-03-12 19:17:56 -0700

    Bug 709329 - 401 node reassignment. r=rnewman

--- mobile/android/base/sync/GlobalSession.java
+++ mobile/android/base/sync/GlobalSession.java
@@ -1,40 +1,6 @@
-// vim: ts=2:sw=2:expandtab:
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Android Sync Client.
- *
- * The Initial Developer of the Original Code is
- * the Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Richard Newman <rnewman@mozilla.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package org.mozilla.gecko.sync;
 
@@ -93,7 +59,7 @@ public class GlobalSession implements CredentialsSource, PrefsSource {
   protected Map<Stage, GlobalSyncStage> stages;
   public Stage currentState = Stage.idle;
 
-  private GlobalSessionCallback callback;
+  public final GlobalSessionCallback callback;
   private Context context;
   private ClientsDataDelegate clientsDelegate;
 
@@ -343,8 +309,16 @@ public class GlobalSession implements CredentialsSource, PrefsSource {
     if (backoff > 0) {
       callback.requestBackoff(backoff);
     }
-  }
 
+    if (response.getStatusLine() != null && response.getStatusLine().getStatusCode() == 401) {
+      /*
+       * Alert our callback we have a 401 on a cluster URL. This GlobalSession
+       * will fail, but the next one will fetch a new cluster URL and will
+       * distinguish between "node reassignment" and "user password changed".
+       */
+      callback.informUnauthorizedResponse(this, config.getClusterURL());
+    }
+  }
 
   public void fetchMetaGlobal(MetaGlobalDelegate callback) throws URISyntaxException {
     if (this.config.metaGlobal == null) {
--- mobile/android/base/sync/net/SyncResourceDelegate.java
+++ mobile/android/base/sync/net/SyncResourceDelegate.java
@@ -1,39 +1,6 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Android Sync Client.
- *
- * The Initial Developer of the Original Code is
- * the Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Richard Newman <rnewman@mozilla.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package org.mozilla.gecko.sync.net;
 
@@ -41,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 import ch.boye.httpclientandroidlib.HttpEntity;
+import ch.boye.httpclientandroidlib.HttpResponse;
 import ch.boye.httpclientandroidlib.client.methods.HttpRequestBase;
 import ch.boye.httpclientandroidlib.impl.client.DefaultHttpClient;
 import ch.boye.httpclientandroidlib.util.EntityUtils;
@@ -78,7 +46,12 @@ public abstract class SyncResourceDelegate implements ResourceDelegate {
   }
 
   /**
-   * Best-effort attempt to ensure that the entity has been fully consumed.
+   * Best-effort attempt to ensure that the entity has been fully consumed and
+   * that the underlying stream has been closed.
+   *
+   * This releases the connection back to the connection pool.
+   *
+   * @param entity The HttpEntity to be consumed.
    */
   public static void consumeEntity(HttpEntity entity) {
     try {
@@ -88,6 +61,24 @@ public abstract class SyncResourceDelegate implements ResourceDelegate {
     }
   }
 
+  public static void consumeEntity(HttpResponse response) {
+    consumeEntity(response.getEntity());
+  }
+
+  public static void consumeEntity(SyncStorageResponse response) {
+    if (response.httpResponse() != null) {
+      consumeEntity(response.httpResponse());
+    }
+  }
+
+  /**
+   * Best-effort attempt to ensure that the reader has been fully consumed, so
+   * that the underlying stream will be closed.
+   *
+   * This should allow the connection to be released back to the connection pool.
+   *
+   * @param reader The BufferedReader to be consumed.
+   */
   public static void consumeReader(BufferedReader reader) {
     try {
       while ((reader.readLine()) != null) {
