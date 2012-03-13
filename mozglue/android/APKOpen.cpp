c0c28f2faed05dde2a77d20dd38f886d91bd973b
Mike Hommey <mh+mozilla@glandium.org>
2012-03-13 09:49:18 +0100

    Bug 734832 - Forcibly export a __dso_handle symbol from libmozglue.so on android. r=blassey

--- mozglue/android/APKOpen.cpp
+++ mozglue/android/APKOpen.cpp
@@ -77,6 +77,22 @@
 #define RUSAGE_THREAD 1
 #endif
 
+extern "C" {
+/*
+ * To work around http://code.google.com/p/android/issues/detail?id=23203
+ * we don't link with the crt objects. In some configurations, this means
+ * a lack of the __dso_handle symbol because it is defined there, and
+ * depending on the android platform and ndk versions used, it may or may
+ * not be defined in libc.so. In the latter case, we fail to link. Defining
+ * it here as weak makes us provide the symbol when it's not provided by
+ * the crt objects, making the change transparent for future NDKs that
+ * would fix the original problem. On older NDKs, it is not a problem
+ * either because the way __dso_handle was used was already broken (and
+ * the custom linker works around it).
+ */
+  NS_EXPORT __attribute__((weak)) void *__dso_handle;
+}
+
 typedef int mozglueresult;
 
 enum StartupEvent {
