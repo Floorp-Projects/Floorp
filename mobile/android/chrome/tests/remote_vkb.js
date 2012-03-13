37b8248b476e09db1452c6b82522fe375bab7d7a
Richard Newman <rnewman@mozilla.com>
2012-03-13 15:48:18 -0700

    Bug 735490 - Delete mobile/android/chrome/tests/. r=mbrubeck

--- mobile/android/chrome/tests/remote_vkb.js
+++
@@ -1,6 +0,0 @@
-dump("====================== Content Script Loaded =======================\n");
-
-AsyncTests.add("Test:FocusRoot", function(aMessage, aJson) {
-  content.document.getElementById("root").focus();
-  return true;
-});
