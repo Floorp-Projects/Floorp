52a9d1407014d6202f07d90fccfcb774fcc1610e
David Rajchenbach-Teller <dteller@mozilla.com>
2012-03-13 23:32:53 +0100

    Bug 699856 - Refactor nsSearchService.js to not use a database engine. r=gavin

--- toolkit/components/search/tests/xpcshell/head_search.js
+++ toolkit/components/search/tests/xpcshell/head_search.js
@@ -1,37 +1,10 @@
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim:set ts=2 sw=2 sts=2 et: */
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Initial Developer of the Original Code is POTI Inc.
- * Portions created by the Initial Developer are Copyright (C) 2007
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://gre/modules/NetUtil.jsm");
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
 
 const XULAPPINFO_CONTRACTID = "@mozilla.org/xre/app-info;1";
 const XULAPPINFO_CID = Components.ID("{c763b610-9d49-455a-bbd2-ede71682a1ac}");
@@ -90,3 +63,49 @@ function createAppInfo(id, name, version, platformVersion)
 // Need to create and register a profile folder.
 var gProfD = do_get_profile();
 
+function dumpn(text)
+{
+  dump(text+"\n");
+}
+
+/**
+ * Clean the profile of any metadata files left from a previous run.
+ */
+function removeMetadata()
+{
+  let file = gProfD.clone();
+  file.append("search-metadata.json");
+  if (file.exists()) {
+    file.remove(false);
+  }
+
+  file = gProfD.clone();
+  file.append("search.sqlite");
+  if (file.exists()) {
+    file.remove(false);
+  }
+}
+
+/**
+ * Run some callback once metadata has been committed to disk.
+ */
+function afterCommit(callback)
+{
+  let obs = function(result, topic, verb) {
+    if (verb == "write-metadata-to-disk-complete") {
+      callback(result);
+    } else {
+      dump("TOPIC: " + topic+ "\n");
+    }
+  }
+  Services.obs.addObserver(obs, "browser-search-service", false);
+}
+
+function  parseJsonFromStream(aInputStream) {
+  const json = Cc["@mozilla.org/dom/json;1"].createInstance(Components.interfaces.nsIJSON);
+  const data = json.decodeFromStream(aInputStream, aInputStream.available());
+  return data;
+}
+
+Services.prefs.setBoolPref("browser.search.log", true);
+//Otherwise, error logs contain no useful data
