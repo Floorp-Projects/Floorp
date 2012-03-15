822e9ed34b1bceae23d0fc5fff0f2bcccc79eac8
Tim Taubert <tim.taubert@gmx.de>
2012-03-15 23:28:22 +0100

    Bug 735166 - Intermittent timeout in browser_newtab_bug734043.js; r=dietrich

--- browser/base/content/test/newtab/browser_newtab_bug734043.js
+++ browser/base/content/test/newtab/browser_newtab_bug734043.js
@@ -2,13 +2,11 @@
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
 function runTests() {
-  // TODO Bug 735166 - Intermittent timeout in browser_newtab_bug734043.js
-  return;
-
   setLinks("0,1,2,3,4,5,6,7,8");
   setPinnedLinks("");
 
   yield addNewTabPageTab();
+  checkGrid("0,1,2,3,4,5,6,7,8");
 
   let receivedError = false;
   let block = cw.document.querySelector(".newtab-control-block");
@@ -19,10 +17,8 @@ function runTests() {
 
   cw.addEventListener("error", onError);
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++)
     EventUtils.synthesizeMouseAtCenter(block, {}, cw);
-    yield executeSoon(TestRunner.next);
-  }
 
   yield whenPagesUpdated();
   ok(!receivedError, "we got here without any errors");
