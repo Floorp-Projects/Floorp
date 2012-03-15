ea1fb78e92ba430334c1c81adf8aadb5bbb569d0
Charles Chan <charles.wh.chan@gmail.com>
2012-03-14 21:56:58 -0700

    Bug 729049 - Inline IsXMLLetter, IsXMLNCNameChar, DecodeEntity from nsIParserService. r=hsivonen

--- content/xslt/src/xml/txXMLUtils.h
+++ content/xslt/src/xml/txXMLUtils.h
@@ -48,10 +48,12 @@
 #include "nsDependentSubstring.h"
 #include "nsIAtom.h"
 #include "txXPathNode.h"
-#include "nsIParserService.h"
 #include "nsContentUtils.h"
 
 #define kExpatSeparatorChar 0xFFFF
+
+extern "C" int MOZ_XMLIsLetter(const char* ptr);
+extern "C" int MOZ_XMLIsNCNameChar(const char* ptr);
 
 class nsIAtom;
 class txNamespaceMap;
@@ -156,17 +158,15 @@ public:
      */
     static bool isLetter(PRUnichar aChar)
     {
-        nsIParserService* ps = nsContentUtils::GetParserService();
-        return ps && ps->IsXMLLetter(aChar);
-    }
+        return !!MOZ_XMLIsLetter(reinterpret_cast<const char*>(&aChar));
+    }   
 
     /**
      * Returns true if the given character is an allowable NCName character
      */
     static bool isNCNameChar(PRUnichar aChar)
     {
-        nsIParserService* ps = nsContentUtils::GetParserService();
-        return ps && ps->IsXMLNCNameChar(aChar);
+        return !!MOZ_XMLIsNCNameChar(reinterpret_cast<const char*>(&aChar));
     }
 
     /*
