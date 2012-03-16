9ae6627189e8b72f2ec9562cd622ad6e01baa271
Fabrice Desré <fabrice@mozilla.com>
2012-03-16 15:23:28 -0700

    Bug 735947 - support the desktop notifications API in b2g [r=vingtetun]

---
+++ b2g/components/ContentPermissionPrompt.js
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+const Cc = Components.classes;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+function ContentPermissionPrompt() {}
+
+ContentPermissionPrompt.prototype = {
+
+  handleExistingPermission: function handleExistingPermission(request) {
+    let result = Services.perms.testExactPermission(request.uri, request.type);
+    if (result == Ci.nsIPermissionManager.ALLOW_ACTION) {
+      request.allow();
+      return true;
+    }
+    if (result == Ci.nsIPermissionManager.DENY_ACTION) {
+      request.cancel();
+      return true;
+    }
+    return false;
+  },
+
+  prompt: function(request) {
+    // returns true if the request was handled
+    if (this.handleExistingPermission(request))
+       return;
+
+    // TODO : show UI to grant or deny permission
+  },
+
+  classID: Components.ID("{8c719f03-afe0-4aac-91ff-6c215895d467}"),
+
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPermissionPrompt])
+};
+
+
+//module initialization
+const NSGetFactory = XPCOMUtils.generateNSGetFactory([ContentPermissionPrompt]);
