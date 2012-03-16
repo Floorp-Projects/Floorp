80c51739253734ce3cbe0b2800f0e8715840ad0e
Ms2ger <ms2ger@gmail.com>
2012-03-16 10:41:53 +0100

    Bug 550309 - Part a: Tighten up support for fake ImageData objects; r=bz

--- content/canvas/src/CustomQS_Canvas2D.h
+++ content/canvas/src/CustomQS_Canvas2D.h
@@ -42,6 +42,7 @@
 #include "nsIDOMCanvasRenderingContext2D.h"
 #include "CheckedInt.h"
 #include "nsMathUtils.h"
+#include "CustomQS_Canvas.h"
 
 #include "jsapi.h"
 
@@ -219,32 +220,6 @@ CreateImageData(JSContext* cx,
     return true;
 }
 
-static bool
-GetImageDataDimensions(JSContext *cx, JSObject *dataObject, uint32_t *width, uint32_t *height)
-{
-    jsval temp;
-    int32_t wi, hi;
-    
-    // Need to check that dataObject is ImageData object. That's hard for the moment 
-    // because they're just vanilla objects in our implementation.
-    // Let's guess, if the object has valid width and height then it's suitable
-    // for this operation.
-    if (!JS_GetProperty(cx, dataObject, "width", &temp) ||
-        !JS_ValueToECMAInt32(cx, temp, &wi))
-        return false;
-
-    if (!JS_GetProperty(cx, dataObject, "height", &temp) ||
-        !JS_ValueToECMAInt32(cx, temp, &hi))
-        return false;
-
-    if (wi <= 0 || hi <= 0)
-        return xpc_qsThrow(cx, NS_ERROR_DOM_INDEX_SIZE_ERR);
-
-    *width = uint32_t(wi);
-    *height = uint32_t(hi);
-    return true;
-}
-
 static JSBool
 nsIDOMCanvasRenderingContext2D_CreateImageData(JSContext *cx, unsigned argc, jsval *vp)
 {
@@ -258,16 +233,11 @@ nsIDOMCanvasRenderingContext2D_CreateImageData(JSContext *cx, unsigned argc, jsv
     jsval *argv = JS_ARGV(cx, vp);
 
     if (argc == 1) {
-        // The specification asks to throw NOT_SUPPORTED if first argument is NULL,
-        // An object is expected, so throw an exception for all primitives.
-        if (JSVAL_IS_PRIMITIVE(argv[0]))
-            return xpc_qsThrow(cx, NS_ERROR_DOM_NOT_SUPPORTED_ERR);
-
-        JSObject *dataObject = JSVAL_TO_OBJECT(argv[0]);
-
         uint32_t data_width, data_height;
-        if (!GetImageDataDimensions(cx, dataObject, &data_width, &data_height))
+        JS::Anchor<JSObject*> darray;
+        if (!GetImageData(cx, argv[0], &data_width, &data_height, &darray)) {
             return false;
+        }
 
         return CreateImageData(cx, data_width, data_height, NULL, 0, 0, vp);
     }
@@ -370,10 +340,11 @@ nsIDOMCanvasRenderingContext2D_PutImageData(JSContext *cx, unsigned argc, jsval 
 
     jsval *argv = JS_ARGV(cx, vp);
 
-    if (JSVAL_IS_PRIMITIVE(argv[0]))
-        return xpc_qsThrow(cx, NS_ERROR_DOM_TYPE_MISMATCH_ERR);
-
-    JSObject *dataObject = JSVAL_TO_OBJECT(argv[0]);
+    uint32_t w, h;
+    JS::Anchor<JSObject*> darray;
+    if (!GetImageData(cx, argv[0], &w, &h, &darray)) {
+        return false;
+    }
 
     double xd, yd;
     if (!JS_ValueToNumber(cx, argv[1], &xd) ||
@@ -387,13 +358,6 @@ nsIDOMCanvasRenderingContext2D_PutImageData(JSContext *cx, unsigned argc, jsval 
 
     int32_t x = JS_DoubleToInt32(xd);
     int32_t y = JS_DoubleToInt32(yd);
-
-    // Grab width, height, and the dense array from the dataObject.
-    JS::AutoValueRooter tv(cx);
-
-    uint32_t w, h;
-    if (!GetImageDataDimensions(cx, dataObject, &w, &h))
-        return JS_FALSE;
 
     // the optional dirty rect
     bool hasDirtyRect = false;
@@ -424,24 +388,16 @@ nsIDOMCanvasRenderingContext2D_PutImageData(JSContext *cx, unsigned argc, jsval 
         hasDirtyRect = true;
     }
 
-    if (!JS_GetProperty(cx, dataObject, "data", tv.jsval_addr()))
-        return JS_FALSE;
-
-    if (JSVAL_IS_PRIMITIVE(tv.jsval_value()))
-        return xpc_qsThrow(cx, NS_ERROR_DOM_TYPE_MISMATCH_ERR);
-
-    JSObject *darray = JSVAL_TO_OBJECT(tv.jsval_value());
-
     JS::AutoValueRooter tsrc_tvr(cx);
 
     JSObject *tsrc = NULL;
-    if (js::GetObjectClass(darray) == &js::TypedArray::fastClasses[js::TypedArray::TYPE_UINT8] ||
-        js::GetObjectClass(darray) == &js::TypedArray::fastClasses[js::TypedArray::TYPE_UINT8_CLAMPED])
+    if (js::GetObjectClass(darray.get()) == &js::TypedArray::fastClasses[js::TypedArray::TYPE_UINT8] ||
+        js::GetObjectClass(darray.get()) == &js::TypedArray::fastClasses[js::TypedArray::TYPE_UINT8_CLAMPED])
     {
-        tsrc = js::TypedArray::getTypedArray(darray);
-    } else if (JS_IsArrayObject(cx, darray) || js_IsTypedArray(darray)) {
+        tsrc = darray.get();
+    } else if (JS_IsArrayObject(cx, darray.get()) || js_IsTypedArray(darray.get())) {
         // ugh, this isn't a uint8 typed array, someone made their own object; convert it to a typed array
-        JSObject *nobj = js_CreateTypedArrayWithArray(cx, js::TypedArray::TYPE_UINT8, darray);
+        JSObject *nobj = js_CreateTypedArrayWithArray(cx, js::TypedArray::TYPE_UINT8, darray.get());
         if (!nobj)
             return JS_FALSE;
 
