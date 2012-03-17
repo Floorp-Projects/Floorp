8d8c64d96373106c4e93c77d447209c873bc7346
aceman <acelists@atlas.sk>
2012-03-17 20:38:35 +0100

    Bug 736660 - fix compilation: 'source' is not a member of 'nsGkAtoms', if MOZ_MEDIA is not set. r=bz

--- content/base/src/nsContentUtils.cpp
+++ content/base/src/nsContentUtils.cpp
@@ -1249,7 +1249,9 @@ nsContentUtils::IsHTMLVoid(nsIAtom* aLocalName)
     (aLocalName == nsGkAtoms::link) ||
     (aLocalName == nsGkAtoms::meta) ||
     (aLocalName == nsGkAtoms::param) ||
+#ifdef MOZ_MEDIA
     (aLocalName == nsGkAtoms::source) ||
+#endif
     (aLocalName == nsGkAtoms::track) ||
     (aLocalName == nsGkAtoms::wbr);
 }
@@ -3658,9 +3660,9 @@ nsContentUtils::MaybeFireNodeRemoved(nsINode* aChild, nsINode* aParent,
   // that is a know case when we'd normally fire a mutation event, but can't
   // make that safe and so we suppress it at this time. Ideally this should
   // go away eventually.
-  NS_ASSERTION(aChild->IsNodeOfType(nsINode::eCONTENT) &&
+  NS_ASSERTION((aChild->IsNodeOfType(nsINode::eCONTENT) &&
                static_cast<nsIContent*>(aChild)->
-                 IsInNativeAnonymousSubtree() ||
+                 IsInNativeAnonymousSubtree()) ||
                IsSafeToRunScript() ||
                sDOMNodeRemovedSuppressCount,
                "Want to fire DOMNodeRemoved event, but it's not safe");
