65f7b5fca288459908f5b7e4447ac58a8b8d5802
William Lachance <wlachance@mozilla.com>
2012-03-18 13:25:13 -0400

    Bug 734003 - Using white as the base checkerboard color doesn't work. r=pwalton

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -1699,7 +1699,7 @@ Tab.prototype = {
         // event fires; it's not clear that doing so is worth the effort.
         var backgroundColor = null;
         try {
-          let browser = this.selectedBrowser;
+          let browser = BrowserApp.selectedBrowser;
           if (browser) {
             let { contentDocument, contentWindow } = browser;
             let computedStyle = contentWindow.getComputedStyle(contentDocument.body);
