74622d3afc14fc7781796afb68889c3baac3ca6e
Nicholas Cameron <ncameron@mozilla.com>
2012-03-19 09:08:53 +1300

    Bug 716439; using mask layers in OGL. r=BenWa

--- gfx/layers/opengl/ColorLayerOGL.cpp
+++ gfx/layers/opengl/ColorLayerOGL.cpp
@@ -63,12 +63,14 @@ RenderColorLayer(ColorLayer* aLayer, LayerManagerOGL *aManager,
   color.b *= opacity;
   color.a = opacity;
 
-  ShaderProgramOGL *program = aManager->GetProgram(gl::ColorLayerProgramType);
+  ShaderProgramOGL *program = aManager->GetProgram(gl::ColorLayerProgramType,
+                                                   aLayer->GetMaskLayer());
   program->Activate();
   program->SetLayerQuadRect(visibleRect);
   program->SetLayerTransform(aLayer->GetEffectiveTransform());
   program->SetRenderOffset(aOffset);
   program->SetRenderColor(color);
+  program->LoadMask(aLayer->GetMaskLayer());
 
   aManager->BindAndDrawQuad(program);
 }
