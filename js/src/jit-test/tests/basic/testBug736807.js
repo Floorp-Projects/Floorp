afabd11923d0da441201951167b9681ff8748b10
Luke Wagner <luke@mozilla.com>
2012-03-19 10:49:12 -0700

    Bug 736807 - InitExnPrivate needs to root values since they can now be wrapped (r=bholley)
    --HG--
    extra : rebase_source : 4aeb6c6b271588c57c4b6e166c630a72c8c7d0d5

---
+++ js/src/jit-test/tests/basic/testBug736807.js
@@ -0,0 +1,12 @@
+function f() {
+    newGlobal('new-compartment').eval('\
+        try {\
+          gczeal(2,1); \
+          throw new Error();\
+        } catch (e) { \
+          gc(); \
+          assertEq("" + e, "Error"); \
+        } \
+    ');
+}
+f({}, [1,2,4,5,6,7,8,1], new RegExp(), function() {});
