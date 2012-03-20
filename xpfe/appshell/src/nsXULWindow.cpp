e1d9321eb508c073d5ab6ca95aaa10d05274b29d
Paul O’Shannessy <paul@oshannessy.com>
2012-03-20 16:21:14 -0700

    Bug 639705 (Lion Full Screen) Part 1: Initial support for fullscreenbutton in XUL [r=enn]

--- xpfe/appshell/src/nsXULWindow.cpp
+++ xpfe/appshell/src/nsXULWindow.cpp
@@ -1413,6 +1413,12 @@ void nsXULWindow::SyncAttributesToWidget()
     mWindow->SetShowsToolbarButton(attr.LowerCaseEqualsLiteral("true"));
   }
 
+  // "fullscreenbutton" attribute
+  rv = windowElement->GetAttribute(NS_LITERAL_STRING("fullscreenbutton"), attr);
+  if (NS_SUCCEEDED(rv)) {
+    mWindow->SetShowsFullScreenButton(attr.LowerCaseEqualsLiteral("true"));
+  }
+
   // "macanimationtype" attribute
   rv = windowElement->GetAttribute(NS_LITERAL_STRING("macanimationtype"), attr);
   if (NS_SUCCEEDED(rv) && attr.EqualsLiteral("document")) {
