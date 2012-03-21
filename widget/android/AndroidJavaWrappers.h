2e03b73e479a9bd5ca042b5820010b736304ec4f
Doug Turner <dougt@dougt.org>
2012-03-20 23:36:17 -0700

    Bug 735330 - move device motion from dom/system to hal. r=jdm * * * Bug 735330 - move device motion from dom/system to hal. cocoa bits. r=jdm * * * Bug 735330 - move device motion from dom/system to hal. window/qt/linux bits. r=jdm * * * Bug 735330 - move device motion from dom/system to hal. move accuracy into sensor data.  support compassneedscalibration. r=jdm

--- widget/android/AndroidJavaWrappers.h
+++ widget/android/AndroidJavaWrappers.h
@@ -589,7 +589,7 @@ public:
         PROXIMITY_EVENT = 23,
         ACTIVITY_RESUMING = 24,
         SCREENSHOT = 25,
-        SENSOR_ACCURACY = 26,
+        UNUSED2_EVENT = 26,
         SCREENORIENTATION_CHANGED = 27,
         dummy_java_enum_list_end
     };
