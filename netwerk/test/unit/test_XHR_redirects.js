cb943f718a5d4d101fe4efe9a69416a416774758
julian.reschke@gmx.de <julian.reschke@gmx.de>
2012-03-21 16:13:42 -0400

    Bug 714302: add suport for HTTP status code 308 r=mcmanus
    --HG--
    extra : rebase_source : 2e4a46ac5ebe89a36fb4b12b1985d07d674db5e8

--- netwerk/test/unit/test_XHR_redirects.js
+++ netwerk/test/unit/test_XHR_redirects.js
@@ -47,12 +47,14 @@ function run_test() {
   sSame.registerPathHandler("/bug" + BUGID + "-redirect302", bug676059redirect302);
   sSame.registerPathHandler("/bug" + BUGID + "-redirect303", bug676059redirect303);
   sSame.registerPathHandler("/bug" + BUGID + "-redirect307", bug676059redirect307);
+  sSame.registerPathHandler("/bug" + BUGID + "-redirect308", bug676059redirect308);
 
   // cross-origin redirects
   sSame.registerPathHandler("/bug" + OTHERBUGID + "-redirect301", bug696849redirect301);
   sSame.registerPathHandler("/bug" + OTHERBUGID + "-redirect302", bug696849redirect302);
   sSame.registerPathHandler("/bug" + OTHERBUGID + "-redirect303", bug696849redirect303);
   sSame.registerPathHandler("/bug" + OTHERBUGID + "-redirect307", bug696849redirect307);
+  sSame.registerPathHandler("/bug" + OTHERBUGID + "-redirect308", bug696849redirect308);
   
   // same-origin target
   sSame.registerPathHandler("/bug" + BUGID + "-target", echoMethod);
@@ -102,6 +104,13 @@ function run_test() {
     [307, "POST", null, 307],
     [307, "PUT", null, 307],
     [307, "PROPFIND", "PROPFIND", 200],
+    // 308: never rewrite
+    [308, "DELETE", null, 308],
+    [308, "GET", "GET", 200],
+    [308, "HEAD", "HEAD", 200],
+    [308, "POST", null, 308],
+    [308, "PUT", null, 308],
+    [308, "PROPFIND", "PROPFIND", 200],
   ];
 
   // cross-origin variant
@@ -143,6 +152,9 @@ function redirect(metadata, response, status, port, bugid) {
   else if (status == 307) {
     reason = "Temporary Redirect";
   }
+  else if (status == 308) {
+    reason = "Permanent Redirect";
+  }
   
   response.setStatusLine(metadata.httpVersion, status, reason);
   response.setHeader("Location", "http://localhost:" + port + "/bug" + bugid + "-target");
@@ -183,9 +195,19 @@ function bug676059redirect307(metadata, response) {
   redirect(metadata, response, 307, pSame, BUGID);
 }
 
+// PATH HANDLER FOR /bug676059-redirect308
+function bug676059redirect308(metadata, response) {
+  redirect(metadata, response, 308, pSame, BUGID);
+}
+
 // PATH HANDLER FOR /bug696849-redirect307
 function bug696849redirect307(metadata, response) {
   redirect(metadata, response, 307, pOther, OTHERBUGID);
+}
+
+// PATH HANDLER FOR /bug696849-redirect308
+function bug696849redirect308(metadata, response) {
+  redirect(metadata, response, 308, pOther, OTHERBUGID);
 }
 
 // Echo the request method in "X-Received-Method" header field
