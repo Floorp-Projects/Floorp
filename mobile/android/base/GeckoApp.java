234b5860ae00b964b38012057732ff019105d877
Brian Nicholson <bnicholson@mozilla.com>
2012-03-21 17:29:43 -0700

    Backout f8b7d116a096 for Tp regression

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -1677,10 +1677,9 @@ abstract public class GeckoApp
         if (uri != null && uri.length() > 0)
             passedUri = mLastTitle = uri;
 
-        mRestoreSession |= getProfile().shouldRestoreSession();
         if (passedUri == null || passedUri.equals("about:home")) {
             // show about:home if we aren't restoring previous session
-            if (!mRestoreSession) {
+            if (! getProfile().hasSession()) {
                 mBrowserToolbar.updateTabCount(1);
                 showAboutHome();
             }
