291c1ee048e75052446b596ea17d961ffdd011ce
Boris Zbarsky <bzbarsky@mit.edu>
2012-03-22 00:10:51 -0400

    Bug 700981 part 5.  Reduce the amount of time spent calling GetBindingParent().  r=smaug

--- content/base/public/nsIContent.h
+++ content/base/public/nsIContent.h
@@ -114,8 +114,9 @@ public:
    * @param aParent The new parent for the content node.  May be null if the
    *                node is being bound as a direct child of the document.
    * @param aBindingParent The new binding parent for the content node.
-   *                       This is allowed to be null.  In that case, the
-   *                       binding parent of aParent, if any, will be used.
+   *                       This is must either be non-null if a particular
+   *                       binding parent is desired or match aParent's binding
+   *                       parent.
    * @param aCompileEventHandlers whether to initialize the event handlers in
    *        the document (used by nsXULElement)
    * @note either aDocument or aParent must be non-null.  If both are null,
