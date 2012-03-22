aa6b949a9f3f809ba7353a3681dbfa0adba8e19f
Markus Stange <mstange@themasta.com>
2012-03-22 23:30:37 +0100

    Bug 731272 - Don't skip the topmost stack frame. r=jrmuizel

--- tools/profiler/TableTicker.cpp
+++ tools/profiler/TableTicker.cpp
@@ -528,7 +528,7 @@ void TableTicker::doBacktrace(ThreadProfile &aProfile, TickSample* aSample)
   void *stackEnd = reinterpret_cast<void*>(-1);
   if (pt)
     stackEnd = static_cast<char*>(pthread_get_stackaddr_np(pt));
-  nsresult rv = FramePointerStackWalk(StackWalkCallback, 1, &array, reinterpret_cast<void**>(aSample->fp), stackEnd);
+  nsresult rv = FramePointerStackWalk(StackWalkCallback, 0, &array, reinterpret_cast<void**>(aSample->fp), stackEnd);
 #else
   nsresult rv = NS_StackWalk(StackWalkCallback, 0, &array, thread);
 #endif
