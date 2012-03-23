1ca56a7c5e47d5a48c8216a123ebc9ea99c6f42e
Nicolas Pierron <nicolas.b.pierron@mozilla.com>
2012-03-23 14:58:42 -0700

    Remove debug only shell function from ion/bug732862.js (no bug, r=me)

--- js/src/jit-test/tests/ion/bug732862.js
+++ js/src/jit-test/tests/ion/bug732862.js
@@ -2,6 +2,5 @@ function testReallyDeepNestedExit() {
     for (var i = 0; i < 5*4; i++) {}
     for (var o = schedule = i = 9 ; i < 5; i++) {}
 }
-dis(testReallyDeepNestedExit);
 assertEq(testReallyDeepNestedExit(), undefined);
 
