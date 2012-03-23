b00e7c1eb3fa8caef475b2f89dbbb9f1a36f25a2
Bobby Holley <bobbyholley@gmail.com>
2012-03-23 15:58:18 -0700

    Bug 733984 - Explicitly disallow shadowing on location wrappers. r=mrbkap This was taken care of in other ways before, but we need to be more explicit about it now that we're doing more Xray stuff with Location wrappers.

--- js/xpconnect/wrappers/WrapperFactory.h
+++ js/xpconnect/wrappers/WrapperFactory.h
@@ -51,7 +51,13 @@ class WrapperFactory {
            IS_XRAY_WRAPPER_FLAG    = WAIVE_XRAY_WRAPPER_FLAG << 1,
            SCRIPT_ACCESS_ONLY_FLAG = IS_XRAY_WRAPPER_FLAG << 1,
            PARTIALLY_TRANSPARENT   = SCRIPT_ACCESS_ONLY_FLAG << 1,
-           SOW_FLAG                = PARTIALLY_TRANSPARENT << 1 };
+           SOW_FLAG                = PARTIALLY_TRANSPARENT << 1,
+
+           // Prevent scripts from shadowing native properties.
+           // NB: Applies only to Xray wrappers.
+           // NB: This will prevent scriptable helpers from defining special
+           //     handlers for properties defined in IDL. Use with caution.
+           SHADOWING_FORBIDDEN     = SOW_FLAG << 1 };
 
     // Return true if any of any of the nested wrappers have the flag set.
     static bool HasWrapperFlag(JSObject *wrapper, unsigned flag) {
@@ -70,6 +76,10 @@ class WrapperFactory {
 
     static bool HasWaiveXrayFlag(JSObject *wrapper) {
         return HasWrapperFlag(wrapper, WAIVE_XRAY_WRAPPER_FLAG);
+    }
+
+    static bool IsShadowingForbidden(JSObject *wrapper) {
+        return HasWrapperFlag(wrapper, SHADOWING_FORBIDDEN);
     }
 
     static JSObject *WaiveXray(JSContext *cx, JSObject *obj);
