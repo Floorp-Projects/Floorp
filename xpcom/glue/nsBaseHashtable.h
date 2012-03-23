adee1614788439981986c404f808dd7fe86ae969
Nicholas Nethercote <n.nethercote@gmail.com>
2012-03-23 12:13:44 +0000

    bug 737412 - handle null sizeOfEntryExcludingThis pointer within ns{T,Base}Hashtable SizeOfExcludingThis functions. r=jfkthame

--- xpcom/glue/nsBaseHashtable.h
+++ xpcom/glue/nsBaseHashtable.h
@@ -274,14 +274,15 @@ public:
   size_t SizeOfExcludingThis(SizeOfEntryExcludingThisFun sizeOfEntryExcludingThis,
                              nsMallocSizeOfFun mallocSizeOf, void *userArg = nsnull)
   {
-    if (IsInitialized()) {
-      s_SizeOfArgs args = { sizeOfEntryExcludingThis, userArg };
-      return PL_DHashTableSizeOfExcludingThis(&this->mTable,
-                                              s_SizeOfStub,
-                                              mallocSizeOf,
-                                              &args);
+    if (!IsInitialized()) {
+      return 0;
     }
-    return 0;
+    if (sizeOfEntryExcludingThis) {
+      s_SizeOfArgs args = { sizeOfEntryExcludingThis, userArg };
+      return PL_DHashTableSizeOfExcludingThis(&this->mTable, s_SizeOfStub,
+                                              mallocSizeOf, &args);
+    }
+    return PL_DHashTableSizeOfExcludingThis(&this->mTable, NULL, mallocSizeOf);
   }
 
 protected:
