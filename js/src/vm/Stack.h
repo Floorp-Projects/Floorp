6d855624b031d0cd2479dbef384e7cc6c37b6c07
Nicolas Pierron <nicolas.b.pierron@mozilla.com>
2012-03-23 20:30:57 -0700

    Rewrite fun_getProperty to handle Ion Frames (Bug 732853 part 2, r=dvander)

--- js/src/vm/Stack.h
+++ js/src/vm/Stack.h
@@ -1901,6 +1901,7 @@ class StackIter
     Value      *sp() const { JS_ASSERT(!done() && isScript()); return sp_; }
     jsbytecode *pc() const { JS_ASSERT(!done() && isScripted()); return pc_; }
     JSScript   *script() const { JS_ASSERT(!done() && isScripted()); return script_; }
+    JSObject   &callee() const;
     Value       calleev() const;
 
     bool isNativeCall() const { JS_ASSERT(!done()); return state_ != SCRIPTED; }
