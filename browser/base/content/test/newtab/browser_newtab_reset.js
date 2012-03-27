b70ec95d9b00ea854ddf68bb5890e9fab228c088
Tim Taubert <tim.taubert@gmx.de>
2012-03-27 12:02:55 +0200

    Bug 734280 - [New Tab Page] clean up newtab test suite; r=dietrich

--- browser/base/content/test/newtab/browser_newtab_reset.js
+++ browser/base/content/test/newtab/browser_newtab_reset.js
@@ -13,16 +13,16 @@ function runTests() {
   setPinnedLinks("");
 
   yield addNewTabPageTab();
-  let resetButton = cw.document.getElementById("toolbar-button-reset");
+  let resetButton = getContentDocument().getElementById("toolbar-button-reset");
 
   checkGrid("0,1,2,3,4,5,6,7,8");
   ok(!resetButton.hasAttribute("modified"), "page is not modified");
 
-  yield blockCell(cells[4]);
+  yield blockCell(4);
   checkGrid("0,1,2,3,5,6,7,8,");
   ok(resetButton.hasAttribute("modified"), "page is modified");
 
-  yield cw.gToolbar.reset(TestRunner.next);
+  yield getContentWindow().gToolbar.reset(TestRunner.next);
   checkGrid("0,1,2,3,4,5,6,7,8");
   ok(!resetButton.hasAttribute("modified"), "page is not modified");
 }
