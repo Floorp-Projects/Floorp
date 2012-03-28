0b081f92253dc4678a3da3d229b8e0ddd54e6359
Luke Wagner <luke@mozilla.com>
2012-03-28 16:15:38 -0700

    Bug 733793 - Check for null return from JS_ObjectToOuterObject (r=bholley)

--- js/xpconnect/wrappers/WrapperFactory.cpp
+++ js/xpconnect/wrappers/WrapperFactory.cpp
@@ -79,6 +79,9 @@ static JSObject *
 GetCurrentOuter(JSContext *cx, JSObject *obj)
 {
     obj = JS_ObjectToOuterObject(cx, obj);
+    if (!obj)
+        return nsnull;
+
     if (IsWrapper(obj) && !js::GetObjectClass(obj)->ext.innerObject) {
         obj = UnwrapObject(obj);
         NS_ASSERTION(js::GetObjectClass(obj)->ext.innerObject,
@@ -174,6 +177,9 @@ WrapperFactory::PrepareForWrapping(JSContext *cx, JSObject *scope, JSObject *obj
 
     // We only hand out outer objects to script.
     obj = GetCurrentOuter(cx, obj);
+    if (!obj)
+        return nsnull;
+
     if (js::GetObjectClass(obj)->ext.innerObject)
         return DoubleWrap(cx, obj, flags);
 
