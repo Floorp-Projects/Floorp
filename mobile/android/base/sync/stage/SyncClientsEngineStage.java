a1df88279fa4c6b862ca19732fafe66517cca1b6
Richard Newman <rnewman@mozilla.com>
2012-03-29 16:48:55 -0700

    Bug 740637 - Better handling of unexpected failures during info/collections fetch. r=liuche,nalexander

--- mobile/android/base/sync/stage/SyncClientsEngineStage.java
+++ mobile/android/base/sync/stage/SyncClientsEngineStage.java
@@ -18,7 +18,6 @@ import org.mozilla.gecko.sync.GlobalSession;
 import org.mozilla.gecko.sync.HTTPFailureException;
 import org.mozilla.gecko.sync.Logger;
 import org.mozilla.gecko.sync.NoCollectionKeysSetException;
-import org.mozilla.gecko.sync.Utils;
 import org.mozilla.gecko.sync.crypto.CryptoException;
 import org.mozilla.gecko.sync.crypto.KeyBundle;
 import org.mozilla.gecko.sync.delegates.ClientsDataDelegate;
