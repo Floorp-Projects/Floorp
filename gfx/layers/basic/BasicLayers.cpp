fd1a7125dd9de8809a220bc95c75807ff93129fa
Ali Juma <ajuma@mozilla.com>
2012-03-30 09:25:20 -0400

    Bug 694964 - Don't generate a Thebes Paint edit in an incomplete transaction. r=bgirard

--- gfx/layers/basic/BasicLayers.cpp
+++ gfx/layers/basic/BasicLayers.cpp
@@ -2412,7 +2412,7 @@ BasicShadowableThebesLayer::PaintBuffer(gfxContext* aContext,
                     aRegionToDraw, aExtendedRegionToDraw, aRegionToInvalidate,
                     aDidSelfCopy,
                     aCallback, aCallbackData);
-  if (!HasShadow()) {
+  if (!HasShadow() || BasicManager()->IsTransactionIncomplete()) {
     return;
   }
 
