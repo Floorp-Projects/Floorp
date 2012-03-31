e1c9589023f07469f0d2860af2b8f19c831a9713
Henri Sivonen <hsivonen@iki.fi>
2012-03-31 17:10:34 +0300

    Bug 732343 - Defend HTML document loading against extensions causing bogus use of the old HTML parser. r=smaug.

--- parser/html/nsHtml5Module.cpp
+++ parser/html/nsHtml5Module.cpp
@@ -53,7 +53,6 @@
 using namespace mozilla;
 
 // static
-bool nsHtml5Module::sEnabled = true;
 bool nsHtml5Module::sOffMainThread = true;
 nsIThread* nsHtml5Module::sStreamParserThread = nsnull;
 nsIThread* nsHtml5Module::sMainThread = nsnull;
