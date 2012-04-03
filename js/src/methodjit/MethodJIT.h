c619465ded958660c6176daa8df3c2530816dca7
Nicholas Nethercote <nnethercote@mozilla.com>
2012-04-02 18:02:54 -0700

    Backout 2f9b6bdf63f7 (bug 739512) due to a bad V8 regression.  r=me.

--- js/src/methodjit/MethodJIT.h
+++ js/src/methodjit/MethodJIT.h
@@ -806,6 +806,7 @@ struct JITScript
 
     void            *invokeEntry;       /* invoke address */
     void            *fastEntry;         /* cached entry, fastest */
+    void            *arityCheckEntry;   /* arity check address */
     void            *argsCheckEntry;    /* arguments check address */
 
     /* List of inline caches jumping to the fastEntry. */
