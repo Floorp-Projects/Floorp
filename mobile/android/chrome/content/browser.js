2dd8ee359c948e8e034fa85c0ff3ea875a094cb9
Brian Nicholson <bnicholson@mozilla.com>
2012-04-04 13:33:09 -0700

    Bug 741712 - Fixup URIs stop throbber prematurely. r=mfinkle

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -1965,8 +1965,14 @@ Tab.prototype = {
     if (contentWin != contentWin.top)
         return;
 
+    // Filter optimization: Only really send NETWORK state changes to Java listener
     if (aStateFlags & Ci.nsIWebProgressListener.STATE_IS_NETWORK) {
-      // Filter optimization: Only really send NETWORK state changes to Java listener
+      if ((aStateFlags & Ci.nsIWebProgressListener.STATE_STOP) && aWebProgress.isLoadingDocument) {
+        // We may receive a document stop event while a document is still loading
+        // (such as when doing URI fixup). Don't notify Java UI in these cases.
+        return;
+      }
+
       let browser = BrowserApp.getBrowserForWindow(aWebProgress.DOMWindow);
       let uri = "";
       if (browser)
