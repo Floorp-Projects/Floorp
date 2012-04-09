203f34b56ae927de8de7ab2486f44e5ce4d7bec3
Gregory Szorc <gps@mozilla.com>
2012-04-09 15:40:06 -0700

    Bug 727210 - Implement client for Services' token server; r=rnewman

--- services/common/services-common.js
+++ services/common/services-common.js
@@ -3,3 +3,5 @@
 
 pref("services.common.log.logger.rest.request", "Debug");
 pref("services.common.log.logger.rest.response", "Debug");
+
+pref("services.common.tokenserverclient.logger.level", "Info");
