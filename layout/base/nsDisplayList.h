6a6630c551e18860220b245c8fab8b50152c8fd6
Robert O'Callahan <robert@ocallahan.org>
2012-04-11 16:48:47 +1200

    Bug 735898. Part 1: Cache bounds (union of all child item bounds) in nsDisplayWrapList. r=mattwoodrow

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -1701,8 +1701,8 @@ public:
                                                 nsIFrame* aFrame);
   virtual void Paint(nsDisplayListBuilder* aBuilder, nsRenderingContext* aCtx);
   virtual bool ComputeVisibility(nsDisplayListBuilder* aBuilder,
-                                   nsRegion* aVisibleRegion,
-                                   const nsRect& aAllowVisibleRegionExpansion);
+                                 nsRegion* aVisibleRegion,
+                                 const nsRect& aAllowVisibleRegionExpansion);
   virtual bool TryMerge(nsDisplayListBuilder* aBuilder, nsDisplayItem* aItem) {
     NS_WARNING("This list should already have been flattened!!!");
     return false;
@@ -1737,8 +1737,15 @@ public:
 
 protected:
   nsDisplayWrapList() {}
-  
+
+  void MergeFrom(nsDisplayWrapList* aOther)
+  {
+    mList.AppendToBottom(&aOther->mList);
+    mBounds.UnionRect(mBounds, aOther->mBounds);
+  }
+
   nsDisplayList mList;
+  nsRect mBounds;
 };
 
 /**
@@ -2065,7 +2072,7 @@ public:
                        HitTestState* aState, nsTArray<nsIFrame*> *aOutFrames);
   virtual nsRect GetBounds(nsDisplayListBuilder* aBuilder, bool* aSnap) {
     *aSnap = false;
-    return mBounds + aBuilder->ToReferenceFrame(mEffectsFrame);
+    return mEffectsBounds + aBuilder->ToReferenceFrame(mEffectsFrame);
   }
   virtual void Paint(nsDisplayListBuilder* aBuilder, nsRenderingContext* aCtx);
   virtual bool ComputeVisibility(nsDisplayListBuilder* aBuilder,
@@ -2083,7 +2090,7 @@ public:
 private:
   nsIFrame* mEffectsFrame;
   // relative to mEffectsFrame
-  nsRect    mBounds;
+  nsRect    mEffectsBounds;
 };
 
 /* A display item that applies a transformation to all of its descendant
