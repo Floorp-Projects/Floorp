cf4dd68f307044eef1e709957533a820b9fc613c
Bas Schouten <bschouten@mozilla.com>
2012-04-11 16:55:31 +0200

    Bug 743593: Properly cast gfxFont based on type. r=jfkthame

--- gfx/thebes/gfxOS2Fonts.h
+++ gfx/thebes/gfxOS2Fonts.h
@@ -79,6 +79,8 @@ public:
 protected:
     virtual bool SetupCairoFont(gfxContext *aContext);
 
+    virtual FontType GetType() const { return FONT_TYPE_OS2; }
+
 private:
     cairo_font_face_t *mFontFace;
     Metrics *mMetrics;
