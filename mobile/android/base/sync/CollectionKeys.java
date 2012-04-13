37ff3c92d491def2b23606120ece5832ba0a5b3e
Nick Alexander <nalexander@mozilla.com>
2012-04-12 20:15:53 -0700

    Bug 709324, Bug 730643: persist crypto/keys and meta/global. r=rnewman

--- mobile/android/base/sync/CollectionKeys.java
+++ mobile/android/base/sync/CollectionKeys.java
@@ -1,39 +1,6 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Android Sync Client.
- *
- * The Initial Developer of the Original Code is
- * the Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Richard Newman <rnewman@mozilla.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package org.mozilla.gecko.sync;
 
@@ -48,8 +15,6 @@ import org.mozilla.apache.commons.codec.binary.Base64;
 import org.mozilla.gecko.sync.crypto.CryptoException;
 import org.mozilla.gecko.sync.crypto.KeyBundle;
 
-import android.util.Log;
-
 public class CollectionKeys {
   private static final String LOG_TAG = "CollectionKeys";
   private KeyBundle                  defaultKeyBundle     = null;
@@ -61,7 +26,7 @@ public class CollectionKeys {
       return ck.asCryptoRecord();
     } catch (NoCollectionKeysSetException e) {
       // Cannot occur.
-      Log.e(LOG_TAG, "generateCollectionKeys returned a value with no default key. Unpossible.", e);
+      Logger.error(LOG_TAG, "generateCollectionKeys returned a value with no default key.", e);
       throw new IllegalStateException("CollectionKeys should not have null default key.");
     }
   }
@@ -138,38 +103,22 @@ public class CollectionKeys {
     return record;
   }
 
-  public static CollectionKeys fromCryptoRecord(CryptoRecord keys, KeyBundle syncKeyBundle) throws CryptoException, IOException, ParseException, NonObjectJSONException {
+  /**
+   * Set my key bundle and collection keys with the given key bundle and data
+   * (possibly decrypted) from the given record.
+   *
+   * @param keys
+   *          A "crypto/keys" <code>CryptoRecord</code>, encrypted with
+   *          <code>syncKeyBundle</code> if <code>syncKeyBundle</code> is non-null.
+   * @param syncKeyBundle
+   *          If non-null, the sync key bundle to decrypt <code>keys</code> with.
+   */
+  public void setKeyPairsFromWBO(CryptoRecord keys, KeyBundle syncKeyBundle)
+      throws CryptoException, IOException, ParseException, NonObjectJSONException {
     if (syncKeyBundle != null) {
       keys.keyBundle = syncKeyBundle;
       keys.decrypt();
     }
-    ExtendedJSONObject cleartext = keys.payload;
-    KeyBundle defaultKey = arrayToKeyBundle((JSONArray) cleartext.get("default"));
-
-    ExtendedJSONObject collections = cleartext.getObject("collections");
-    HashMap<String, KeyBundle> collectionKeys = new HashMap<String, KeyBundle>();
-    for (Entry<String, Object> pair : collections.entryIterable()) {
-      KeyBundle bundle = arrayToKeyBundle((JSONArray) pair.getValue());
-      collectionKeys.put(pair.getKey(), bundle);
-    }
-
-    CollectionKeys ck = new CollectionKeys();
-    ck.collectionKeyBundles = collectionKeys;
-    ck.defaultKeyBundle     = defaultKey;
-    return ck;
-  }
-
-  /**
-   * Take a downloaded record, and the Sync Key, decrypting the record and
-   * setting our own keys accordingly.
-   */
-  public void setKeyPairsFromWBO(CryptoRecord keys, KeyBundle syncKeyBundle)
-                                                                            throws CryptoException,
-                                                                            IOException,
-                                                                            ParseException,
-                                                                            NonObjectJSONException {
-    keys.keyBundle = syncKeyBundle;
-    keys.decrypt();
     ExtendedJSONObject cleartext = keys.payload;
     KeyBundle defaultKey = arrayToKeyBundle((JSONArray) cleartext.get("default"));
 
---
+++ mobile/android/base/sync/crypto/PersistedCrypto5Keys.java
@@ -0,0 +1,99 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.sync.crypto;
+
+import org.mozilla.gecko.sync.CollectionKeys;
+import org.mozilla.gecko.sync.CryptoRecord;
+import org.mozilla.gecko.sync.Logger;
+
+import android.content.SharedPreferences;
+
+public class PersistedCrypto5Keys {
+  public static final String LOG_TAG = "PersistedC5Keys";
+
+  public static final String CRYPTO5_KEYS_SERVER_RESPONSE_BODY = "crypto5KeysServerResponseBody";
+  public static final String CRYPTO5_KEYS_LAST_MODIFIED        = "crypto5KeysLastModified";
+
+  protected SharedPreferences prefs;
+  protected KeyBundle syncKeyBundle;
+
+  public PersistedCrypto5Keys(SharedPreferences prefs, KeyBundle syncKeyBundle) {
+    if (syncKeyBundle == null) {
+      throw new IllegalArgumentException("Null syncKeyBundle passed in to PersistedCrypto5Keys constructor.");
+    }
+    this.prefs = prefs;
+    this.syncKeyBundle = syncKeyBundle;
+  }
+
+  /**
+   * Get persisted crypto/keys.
+   * <p>
+   * crypto/keys is fetched from an encrypted JSON-encoded <code>CryptoRecord</code>.
+   *
+   * @return A <code>CollectionKeys</code> instance or <code>null</code> if none
+   *         is currently persisted.
+   */
+  public CollectionKeys keys() {
+    String keysJSON = prefs.getString(CRYPTO5_KEYS_SERVER_RESPONSE_BODY, null);
+    if (keysJSON == null) {
+      return null;
+    }
+    try {
+      CryptoRecord cryptoRecord = CryptoRecord.fromJSONRecord(keysJSON);
+      CollectionKeys keys = new CollectionKeys();
+      keys.setKeyPairsFromWBO(cryptoRecord, syncKeyBundle);
+      return keys;
+    } catch (Exception e) {
+      Logger.warn(LOG_TAG, "Got exception decrypting persisted crypto/keys.", e);
+      return null;
+    }
+  }
+
+  /**
+   * Persist crypto/keys.
+   * <p>
+   * crypto/keys is stored as an encrypted JSON-encoded <code>CryptoRecord</code>.
+   *
+   * @param keys
+   *          The <code>CollectionKeys</code> object to persist, which should
+   *          have the same default key bundle as the sync key bundle.
+   */
+  public void persistKeys(CollectionKeys keys) {
+    if (keys == null) {
+      Logger.debug(LOG_TAG, "Clearing persisted crypto/keys.");
+      prefs.edit().remove(CRYPTO5_KEYS_SERVER_RESPONSE_BODY).commit();
+      return;
+    }
+    try {
+      CryptoRecord cryptoRecord = keys.asCryptoRecord();
+      cryptoRecord.keyBundle = syncKeyBundle;
+      cryptoRecord.encrypt();
+      String keysJSON = cryptoRecord.toJSONString();
+      Logger.debug(LOG_TAG, "Persisting crypto/keys.");
+      prefs.edit().putString(CRYPTO5_KEYS_SERVER_RESPONSE_BODY, keysJSON).commit();
+    } catch (Exception e) {
+      Logger.warn(LOG_TAG, "Got exception encrypting while persisting crypto/keys.", e);
+    }
+  }
+
+  public long lastModified() {
+    return prefs.getLong(CRYPTO5_KEYS_LAST_MODIFIED, -1);
+  }
+
+  public void persistLastModified(long lastModified) {
+    if (lastModified <= 0) {
+      Logger.debug(LOG_TAG, "Clearing persisted crypto/keys last modified timestamp.");
+      prefs.edit().remove(CRYPTO5_KEYS_LAST_MODIFIED).commit();
+      return;
+    }
+    Logger.debug(LOG_TAG, "Persisting crypto/keys last modified timestamp " + lastModified + ".");
+    prefs.edit().putLong(CRYPTO5_KEYS_LAST_MODIFIED, lastModified).commit();
+  }
+
+  public void purge() {
+    persistLastModified(-1);
+    persistKeys(null);
+  }
+}
