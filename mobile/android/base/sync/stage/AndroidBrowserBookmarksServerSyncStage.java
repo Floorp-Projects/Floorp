0b4b762b31e597db92970d7d3ce57bcfb18edc4e
Nick Alexander <nalexander@mozilla.com>
2012-04-12 20:15:53 -0700

    Bug 743098 - disable bookmarks and history stages if Fennec profile migration has not completed. r=rnewman

--- mobile/android/base/sync/stage/AndroidBrowserBookmarksServerSyncStage.java
+++ mobile/android/base/sync/stage/AndroidBrowserBookmarksServerSyncStage.java
@@ -39,13 +39,17 @@ package org.mozilla.gecko.sync.stage;
 
 import java.net.URISyntaxException;
 
+import org.mozilla.gecko.sync.Logger;
+import org.mozilla.gecko.sync.MetaGlobalException;
 import org.mozilla.gecko.sync.repositories.ConstrainedServer11Repository;
 import org.mozilla.gecko.sync.repositories.RecordFactory;
 import org.mozilla.gecko.sync.repositories.Repository;
 import org.mozilla.gecko.sync.repositories.android.AndroidBrowserBookmarksRepository;
+import org.mozilla.gecko.sync.repositories.android.FennecControlHelper;
 import org.mozilla.gecko.sync.repositories.domain.BookmarkRecordFactory;
 
 public class AndroidBrowserBookmarksServerSyncStage extends ServerSyncStage {
+  protected static final String LOG_TAG = "BookmarksStage";
 
   // Eventually this kind of sync stage will be data-driven,
   // and all this hard-coding can go away.
@@ -79,5 +83,17 @@ public class AndroidBrowserBookmarksServerSyncStage extends ServerSyncStage {
   @Override
   protected RecordFactory getRecordFactory() {
     return new BookmarkRecordFactory();
+  }
+
+  @Override
+  protected boolean isEnabled() throws MetaGlobalException {
+    if (session == null || session.getContext() == null) {
+      return false;
+    }
+    boolean migrated = FennecControlHelper.areBookmarksMigrated(session.getContext());
+    if (!migrated) {
+      Logger.warn(LOG_TAG, "Not enabling bookmarks engine since Fennec bookmarks are not migrated.");
+    }
+    return super.isEnabled() && migrated;
   }
 }
