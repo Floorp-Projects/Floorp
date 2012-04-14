fddbc72044ff9bb2301c3024ccbb144db2cba33f
Ms2ger <ms2ger@gmail.com>
2012-04-14 14:57:00 +0200

    Bug 745287 - Fix recent a11y build warnings; r=davidb,capella

--- accessible/src/generic/FormControlAccessible.cpp
+++ accessible/src/generic/FormControlAccessible.cpp
@@ -119,13 +119,13 @@ ProgressMeterAccessible<Max>::Value(nsString& aValue)
 
   double maxValue = 0;
   nsresult rv = GetMaximumValue(&maxValue);
-  NS_ENSURE_SUCCESS(rv, );
-  if (maxValue == 0)
+  if (NS_FAILED(rv) || maxValue == 0)
     return;
 
   double curValue = 0;
   GetCurrentValue(&curValue);
-  NS_ENSURE_SUCCESS(rv, );
+  if (NS_FAILED(rv))
+    return;
 
   // Treat the current value bigger than maximum as 100%.
   double percentValue = (curValue < maxValue) ?
