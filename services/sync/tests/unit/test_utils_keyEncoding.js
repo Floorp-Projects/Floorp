913961907540b5346f92c2949f861801725169a6
Gregory Szorc <gps@mozilla.com>
2012-04-16 12:19:01 -0700

    Bug 745396 - Refactor Sync utility functions into services-{common,crypto}; r=rnewman

---
+++ services/sync/tests/unit/test_utils_keyEncoding.js
@@ -0,0 +1,15 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+Cu.import("resource://services-sync/util.js");
+
+function run_test() {
+  do_check_eq(Utils.encodeKeyBase32("foobarbafoobarba"), "mzxw6ytb9jrgcztpn5rgc4tcme");
+  do_check_eq(Utils.decodeKeyBase32("mzxw6ytb9jrgcztpn5rgc4tcme"), "foobarbafoobarba");
+  do_check_eq(
+      Utils.encodeKeyBase32("\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01"),
+      "aeaqcaibaeaqcaibaeaqcaibae");
+  do_check_eq(
+      Utils.decodeKeyBase32("aeaqcaibaeaqcaibaeaqcaibae"),
+      "\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01");
+}
