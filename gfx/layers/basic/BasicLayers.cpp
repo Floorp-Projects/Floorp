7a09ae104e4ef0bcaddeb621f480b80a6406f6c0
Benoit Girard <b56girard@gmail.com>
2012-04-16 19:02:45 -0400

    Bug 739679 - Part 3: BasicTiledThebesLayer. r=mwoodrow,roc

--- gfx/layers/basic/BasicLayers.cpp
+++ gfx/layers/basic/BasicLayers.cpp
@@ -49,6 +49,7 @@
 
 #include "BasicLayers.h"
 #include "BasicImplData.h"
+#include "BasicTiledThebesLayer.h"
 #include "ImageLayers.h"
 #include "RenderTrace.h"
 
@@ -3126,10 +3127,23 @@ already_AddRefed<ThebesLayer>
 BasicShadowLayerManager::CreateThebesLayer()
 {
   NS_ASSERTION(InConstruction(), "Only allowed in construction phase");
-  nsRefPtr<BasicShadowableThebesLayer> layer =
-    new BasicShadowableThebesLayer(this);
-  MAYBE_CREATE_SHADOW(Thebes);
-  return layer.forget();
+#ifdef FORCE_BASICTILEDTHEBESLAYER
+  if (HasShadowManager()) {
+    // BasicTiledThebesLayer doesn't support main
+    // thread compositing so only return this layer
+    // type if we have a shadow manager.
+    nsRefPtr<BasicTiledThebesLayer> layer =
+      new BasicTiledThebesLayer(this);
+    MAYBE_CREATE_SHADOW(Thebes);
+    return layer.forget();
+  } else
+#endif
+  {
+    nsRefPtr<BasicShadowableThebesLayer> layer =
+      new BasicShadowableThebesLayer(this);
+    MAYBE_CREATE_SHADOW(Thebes);
+    return layer.forget();
+  }
 }
 
 already_AddRefed<ContainerLayer>
