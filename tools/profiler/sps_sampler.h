00409b37a6cfa4fa8842c2775410edd52dea50d2
Jeff Muizelaar <jmuizelaar@mozilla.com>
2012-04-17 16:21:23 -0400

    Bug 745864 - make SAMPLE_LABELS per line instead of per independent scope. r=bgirard a=blassey
    --HG--
    extra : rebase_source : 1d09f95da1677e9d13932017894f443d02aa40af

--- tools/profiler/sps_sampler.h
+++ tools/profiler/sps_sampler.h
@@ -74,7 +74,12 @@ extern bool stack_key_initialized;
 #define SAMPLER_GET_FEATURES() mozilla_sampler_get_features()
 // we want the class and function name but can't easily get that using preprocessor macros
 // __func__ doesn't have the class name and __PRETTY_FUNCTION__ has the parameters
-#define SAMPLE_LABEL(name_space, info) mozilla::SamplerStackFrameRAII only_one_sampleraii_per_scope(name_space "::" info)
+
+#define SAMPLER_APPEND_LINE_NUMBER_PASTE(id, line) id ## line
+#define SAMPLER_APPEND_LINE_NUMBER_EXPAND(id, line) SAMPLER_APPEND_LINE_NUMBER_PASTE(id, line)
+#define SAMPLER_APPEND_LINE_NUMBER(id) SAMPLER_APPEND_LINE_NUMBER_EXPAND(id, __LINE__)
+
+#define SAMPLE_LABEL(name_space, info) mozilla::SamplerStackFrameRAII SAMPLER_APPEND_LINE_NUMBER(sampler_raii)(name_space "::" info)
 #define SAMPLE_MARKER(info) mozilla_sampler_add_marker(info)
 
 /* we duplicate this code here to avoid header dependencies
