a49ff60ce7c97ce4a02409ada5b86ee06094311d
Simon Montagu <smontagu@smontagu.org>
2012-04-17 07:03:10 +0300

    Add support for :dir(ltr/rtl) CSS selector, content part, Bug 562169, r=bz

--- content/base/src/nsGenericElement.cpp
+++ content/base/src/nsGenericElement.cpp
@@ -51,6 +51,7 @@
 #include "nsIDOMMutationEvent.h"
 #include "nsMutationEvent.h"
 #include "nsNodeUtils.h"
+#include "mozilla/dom/DirectionalityUtils.h"
 #include "nsDocument.h"
 #include "nsAttrValueOrString.h"
 #ifdef MOZ_XUL
@@ -128,6 +129,7 @@
 
 using namespace mozilla;
 using namespace mozilla::dom;
+using namespace mozilla::directionality;
 
 nsEventStates
 Element::IntrinsicState() const
@@ -1358,6 +1360,13 @@ nsGenericElement::BindToTree(nsIDocument* aDocument, nsIContent* aParent,
     SetSubtreeRootPointer(aParent->SubtreeRoot());
   }
 
+  // This has to be here, rather than in nsGenericHTMLElement::BindToTree, 
+  //  because it has to happen after updating the parent pointer, but before
+  //  recursively binding the kids.
+  if (IsHTML()) {
+    RecomputeDirectionality(this, false);
+  }
+
   // If NODE_FORCE_XBL_BINDINGS was set we might have anonymous children
   // that also need to be told that they are moving.
   nsresult rv;
@@ -1541,6 +1550,13 @@ nsGenericElement::UnbindFromTree(bool aDeep, bool aNullParent)
     if (slots) {
       slots->mBindingParent = nullptr;
     }
+  }
+
+  // This has to be here, rather than in nsGenericHTMLElement::UnbindFromTree, 
+  //  because it has to happen after unsetting the parent pointer, but before
+  //  recursively unbinding the kids.
+  if (IsHTML()) {
+    RecomputeDirectionality(this, false);
   }
 
   if (aDeep) {
