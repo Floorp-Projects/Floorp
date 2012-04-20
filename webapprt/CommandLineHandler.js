39402fa9c5ef9f31e90bad705422bc8d21cc1b08
Edward Lee <edilee@mozilla.com>
2012-04-20 15:56:06 -0700

    Bug 746213 - Support mozApps.getSelf/getInstalled in desktop runtime to allow apps to access receipts [r=felipe, a=desktop-only] Import Webapps.jsm to initialize DOMApplicationRegistry, which handles Webapps:GetSelf, etc. messages.

--- webapprt/CommandLineHandler.js
+++ webapprt/CommandLineHandler.js
@@ -9,6 +9,9 @@ const Cu = Components.utils;
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 
+// Initialize DOMApplicationRegistry so it can receive and respond to messages
+Cu.import("resource://gre/modules/Webapps.jsm");
+
 function CommandLineHandler() {}
 
 CommandLineHandler.prototype = {
