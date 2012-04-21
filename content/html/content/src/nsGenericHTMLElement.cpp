c7ce95932f8ea416feca75190eb5d5e3b9ef98ee
Matthew Schranz <schranz.m@gmail.com>
2012-04-21 17:35:42 -0400

    Bug 686913 - HTMLProgressElement and HTMLMeterElement should not be form controls. r=mounir, a=mfinkle

--- content/html/content/src/nsGenericHTMLElement.cpp
+++ content/html/content/src/nsGenericHTMLElement.cpp
@@ -2918,8 +2918,7 @@ nsGenericHTMLFormElement::CanBeDisabled() const
   return
     type != NS_FORM_LABEL &&
     type != NS_FORM_OBJECT &&
-    type != NS_FORM_OUTPUT &&
-    type != NS_FORM_PROGRESS;
+    type != NS_FORM_OUTPUT;
 }
 
 bool
