6dd2beeefb14f5a659a64c56afa8bf917c496214
Benoit Jacob <bjacob@mozilla.com>
2012-04-21 16:48:22 -0400

    Bug 736481 - 7/7 - ForceLoseContext should not do anything on already lost context awaiting event - no review, a=blocking-fennec
    Sorry for this little no-review patch. I meant to roll this into another of the patches for review and apparently forgot. It's 3 lines and quite trivial.

--- content/canvas/src/WebGLContext.cpp
+++ content/canvas/src/WebGLContext.cpp
@@ -1170,6 +1170,9 @@ WebGLContext::MaybeRestoreContext()
 void
 WebGLContext::ForceLoseContext()
 {
+    if (mContextStatus == ContextLostAwaitingEvent)
+        return;
+
     mContextStatus = ContextLostAwaitingEvent;
     // Queue up a task to restore the event.
     SetupContextLossTimer();
