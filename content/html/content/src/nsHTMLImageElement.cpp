c15603f2b452502b5e3258bba63f4594a94e2c2e
Ms2ger <ms2ger@gmail.com>
2012-04-24 21:04:37 +0200

    Bug 731832 - Backout bug 587021 for breaking a specific web page; r=bz

--- content/html/content/src/nsHTMLImageElement.cpp
+++ content/html/content/src/nsHTMLImageElement.cpp
@@ -171,6 +171,7 @@ public:
   void MaybeLoadImage();
   virtual nsXPCClassInfo* GetClassInfo();
 protected:
+  nsIntPoint GetXY();
   nsSize GetWidthHeight();
 };
 
@@ -273,6 +274,42 @@ nsHTMLImageElement::GetComplete(bool* aComplete)
   *aComplete =
     (status &
      (imgIRequest::STATUS_LOAD_COMPLETE | imgIRequest::STATUS_ERROR)) != 0;
+
+  return NS_OK;
+}
+
+nsIntPoint
+nsHTMLImageElement::GetXY()
+{
+  nsIntPoint point(0, 0);
+
+  nsIFrame* frame = GetPrimaryFrame(Flush_Layout);
+
+  if (!frame) {
+    return point;
+  }
+
+  nsIFrame* layer = nsLayoutUtils::GetClosestLayer(frame->GetParent());
+  nsPoint origin(frame->GetOffsetTo(layer));
+  // Convert to pixels using that scale
+  point.x = nsPresContext::AppUnitsToIntCSSPixels(origin.x);
+  point.y = nsPresContext::AppUnitsToIntCSSPixels(origin.y);
+
+  return point;
+}
+
+NS_IMETHODIMP
+nsHTMLImageElement::GetX(PRInt32* aX)
+{
+  *aX = GetXY().x;
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsHTMLImageElement::GetY(PRInt32* aY)
+{
+  *aY = GetXY().y;
 
   return NS_OK;
 }
