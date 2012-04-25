75d4768e4c90cf0fe3c196cfe40c25d9fb1d19e9
Paul Rouget <paul@mozilla.com>
2012-04-25 16:53:21 +0200

    Bug 747919 - browser/devtools/layoutview/test/browser_layoutview.js leaks chrome and content windows; r=dao

--- browser/devtools/layoutview/test/browser_layoutview.js
+++ browser/devtools/layoutview/test/browser_layoutview.js
@@ -82,6 +82,8 @@ function test() {
   function viewReady(e) {
     if (e.data != "layoutview-ready") return;
 
+    window.removeEventListener("message", viewReady, true);
+
     info("Layout view ready");
 
     view = InspectorUI._sidebar._layoutview;
