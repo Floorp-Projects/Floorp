8fa462d93e565896b3e8dd0bde685da1efd5944c
Ed Morley <emorley@mozilla.com>
2012-04-25 16:51:31 +0100

    Backout 949cb1977ef9 (bug 714861) for build bustage

--- dom/tests/mochitest/browser-frame/browserFrameHelpers.js
+++ dom/tests/mochitest/browser-frame/browserFrameHelpers.js
@@ -21,15 +21,6 @@ const browserFrameHelpers = {
     }
   },
 
-  'getOOPDisabledPref': function() {
-    try {
-      return SpecialPowers.getBoolPref('dom.ipc.tabs.disabled');
-    }
-    catch(e) {
-      return undefined;
-    }
-  },
-
   'setEnabledPref': function(enabled) {
     if (enabled !== undefined) {
       SpecialPowers.setBoolPref('dom.mozBrowserFramesEnabled', enabled);
@@ -48,15 +39,6 @@ const browserFrameHelpers = {
     }
   },
 
-  'setOOPDisabledPref': function(value) {
-    if (value !== undefined) {
-      SpecialPowers.setBoolPref('dom.ipc.tabs.disabled', value);
-    }
-    else {
-      SpecialPowers.clearUserPref('dom.ipc.tabs.disabled');
-    }
-  },
-
   'addToWhitelist': function() {
     var whitelist = browserFrameHelpers.getWhitelistPref();
     whitelist += ',  http://' + window.location.host + ',  ';
@@ -66,12 +48,10 @@ const browserFrameHelpers = {
   'restoreOriginalPrefs': function() {
     browserFrameHelpers.setEnabledPref(browserFrameHelpers.origEnabledPref);
     browserFrameHelpers.setWhitelistPref(browserFrameHelpers.origWhitelistPref);
-    browserFrameHelpers.setOOPDisabledPref(browserFrameHelpers.origOOPDisabledPref);
   },
 
   'origEnabledPref': null,
   'origWhitelistPref': null,
-  'origOOPDisabledPref': null,
 
   // Two basically-empty pages from two different domains you can load.
   'emptyPage1': 'http://example.com' +
@@ -84,11 +64,6 @@ const browserFrameHelpers = {
 
 browserFrameHelpers.origEnabledPref = browserFrameHelpers.getEnabledPref();
 browserFrameHelpers.origWhitelistPref = browserFrameHelpers.getWhitelistPref();
-browserFrameHelpers.origOOPDisabledPref = browserFrameHelpers.getOOPDisabledPref();
-
-// Set OOPDisabledPref to true, because none of these tests pass with OOP
-// browser frames, at the moment.
-browserFrameHelpers.setOOPDisabledPref(true);
 
 addEventListener('unload', function() {
   browserFrameHelpers.restoreOriginalPrefs();
