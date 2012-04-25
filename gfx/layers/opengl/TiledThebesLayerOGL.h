ecaa17693912958373ad9ba180225d253d6a42c7
Matt Brubeck <mbrubeck@mozilla.com>
2012-04-24 20:18:50 -0700

    Back out 4114c654f5d0 (bug 745177) because of Mac build errors

--- gfx/layers/opengl/TiledThebesLayerOGL.h
+++ gfx/layers/opengl/TiledThebesLayerOGL.h
@@ -20,8 +20,6 @@ class GLContext;
 
 namespace layers {
 
-class ReusableTileStoreOGL;
-
 class TiledTexture {
 public:
   // Constructs a placeholder TiledTexture. See the comments above
@@ -75,12 +73,9 @@ public:
 
   void Upload(const BasicTiledLayerBuffer* aMainMemoryTiledBuffer,
               const nsIntRegion& aNewValidRegion,
-              const nsIntRegion& aInvalidateRegion,
-              const gfxSize& aResolution);
+              const nsIntRegion& aInvalidateRegion);
 
   TiledTexture GetPlaceholderTile() const { return TiledTexture(); }
-
-  const gfxSize& GetResolution() { return mResolution; }
 
 protected:
   TiledTexture ValidateTile(TiledTexture aTile,
@@ -96,7 +91,6 @@ protected:
 private:
   nsRefPtr<gl::GLContext> mContext;
   const BasicTiledLayerBuffer* mMainMemoryTiledBuffer;
-  gfxSize mResolution;
 
   void GetFormatAndTileForImageFormat(gfxASurface::gfxImageFormat aFormat,
                                       GLenum& aOutFormat,
@@ -109,7 +103,10 @@ class TiledThebesLayerOGL : public ShadowThebesLayer,
 {
 public:
   TiledThebesLayerOGL(LayerManagerOGL *aManager);
-  virtual ~TiledThebesLayerOGL();
+  virtual ~TiledThebesLayerOGL()
+  {
+    mMainMemoryTiledBuffer.ReadUnlock();
+  }
 
   // LayerOGL impl
   void Destroy() {}
@@ -129,22 +126,10 @@ public:
   }
   void PaintedTiledLayerBuffer(const BasicTiledLayerBuffer* mTiledBuffer);
   void ProcessUploadQueue();
-
-  // Renders a single given tile.
-  // XXX This currently takes an nsIntRect, but should actually take an
-  //     nsIntRegion and iterate over each rectangle in the region.
-  void RenderTile(TiledTexture aTile,
-                  const gfx3DMatrix& aTransform,
-                  const nsIntPoint& aOffset,
-                  nsIntRect aScreenRect,
-                  nsIntRect aTextureRect,
-                  nsIntSize aTextureBounds);
-
 private:
   nsIntRegion                  mRegionToUpload;
   BasicTiledLayerBuffer        mMainMemoryTiledBuffer;
   TiledLayerBufferOGL          mVideoMemoryTiledBuffer;
-  ReusableTileStoreOGL*        mReusableTileStore;
 };
 
 } // layers
