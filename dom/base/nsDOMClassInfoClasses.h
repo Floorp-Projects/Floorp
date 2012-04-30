e18635a0ab7313fb35f55ece6ab5f7b0b782326d
Robert O'Callahan <robert@ocallahan.org>
2012-04-30 15:11:34 +1200

    Bug 664918. Part 4: Create nsDOMMediaStream, a DOM object wrapping an underlying MediaStream. r=smaug,jesup

--- dom/base/nsDOMClassInfoClasses.h
+++ dom/base/nsDOMClassInfoClasses.h
@@ -449,6 +449,9 @@ DOMCI_CLASS(HTMLSourceElement)
 DOMCI_CLASS(MediaError)
 DOMCI_CLASS(HTMLAudioElement)
 DOMCI_CLASS(TimeRanges)
+
+// Media streams
+DOMCI_CLASS(MediaStream)
 #endif
 
 DOMCI_CLASS(ProgressEvent)
