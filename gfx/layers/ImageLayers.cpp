5fe2a7d6950b548253aabcd8f0357d6d1be588a6
Bas Schouten <bschouten@mozilla.com>
2012-05-01 23:46:53 +0200

    Bug 749482: Fix assertion in SetRemoteImageData. r=roc

--- gfx/layers/ImageLayers.cpp
+++ gfx/layers/ImageLayers.cpp
@@ -266,8 +266,9 @@ void
 ImageContainer::SetRemoteImageData(RemoteImageData *aData, CrossProcessMutex *aMutex)
 {
   ReentrantMonitorAutoEnter mon(mReentrantMonitor);
-  NS_ASSERTION(!mActiveImage, "No active image expected when SetRemoteImageData is called.");
-  NS_ASSERTION(!mRemoteData, "No remote data expected when SetRemoteImageData is called.");
+
+  NS_ASSERTION(!mActiveImage || !aData, "No active image expected when SetRemoteImageData is called with non-NULL aData.");
+  NS_ASSERTION(!mRemoteData || !aData, "No remote data expected when SetRemoteImageData is called with non-NULL aData.");
 
   mRemoteData = aData;
 
