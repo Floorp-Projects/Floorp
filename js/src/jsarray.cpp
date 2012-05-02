9827e8040de70b348f1916900d953339e022ed70
Tom Schuster <evilpies@gmail.com>
2012-05-02 16:11:00 +0200

    Bug 749620 - Add missing power of 10 for edge case. r=luke

--- js/src/jsarray.cpp
+++ js/src/jsarray.cpp
@@ -2015,8 +2015,8 @@ CompareStringValues(JSContext *cx, const Value &a, const Value &b, bool *lessOrE
     return true;
 }
 
-static uint32_t const powersOf10[] = {
-    1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000
+static uint64_t const powersOf10[] = {
+    1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 1000000000000ULL
 };
 
 static inline unsigned
@@ -2083,12 +2083,15 @@ CompareLexicographicInt32(JSContext *cx, const Value &a, const Value &b, bool *l
          */
         unsigned digitsa = NumDigitsBase10(auint);
         unsigned digitsb = NumDigitsBase10(buint);
-        if (digitsa == digitsb)
+        if (digitsa == digitsb) {
             *lessOrEqualp = (auint <= buint);
-        else if (digitsa > digitsb)
+        } else if (digitsa > digitsb) {
+            JS_ASSERT((digitsa - digitsb) < ArrayLength(powersOf10));
             *lessOrEqualp = (uint64_t(auint) < uint64_t(buint) * powersOf10[digitsa - digitsb]);
-        else /* if (digitsb > digitsa) */
+        } else { /* if (digitsb > digitsa) */
+            JS_ASSERT((digitsb - digitsa) < ArrayLength(powersOf10));
             *lessOrEqualp = (uint64_t(auint) * powersOf10[digitsb - digitsa] <= uint64_t(buint));
+        }
     }
 
     return true;
