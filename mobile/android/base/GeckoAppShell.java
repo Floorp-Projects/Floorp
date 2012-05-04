d7ebe76317401205f423c38559954d4817fd98ae
Chris Lord <chrislord.net@gmail.com>
2012-05-04 17:04:15 +1200

    Bug 607417 - Reconcile async scrolling for fixed position layers. r=ajuma
    Untranslate fixed position layers when doing async scrolling so that they don't
    jump about as content re-renders them in the correct place.

--- mobile/android/base/GeckoAppShell.java
+++ mobile/android/base/GeckoAppShell.java
@@ -411,6 +411,9 @@ public class GeckoAppShell
         f = Environment.getDownloadCacheDirectory();
         GeckoAppShell.putenv("EXTERNAL_STORAGE=" + f.getPath());
 
+        // Enable fixed position layers
+        GeckoAppShell.putenv("MOZ_ENABLE_FIXED_POSITION_LAYERS=1");
+
         putLocaleEnv();
     }
 
