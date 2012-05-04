3426224446201c82651316e79518dc6db52c719d
Blake Kaplan <mrbkap@gmail.com>
2012-05-04 14:22:55 +0200

    Bug 751858 - Actually throw when we deny access. r=bholley

--- js/xpconnect/wrappers/AccessCheck.h
+++ js/xpconnect/wrappers/AccessCheck.h
@@ -120,14 +120,13 @@ struct LocationPolicy : public Policy {
         perm = DenyAccess;
 
         // Location object security is complicated enough. Don't allow punctures.
-        if (act == js::Wrapper::PUNCTURE)
-            return false;
-
-        if (AccessCheck::isCrossOriginAccessPermitted(cx, wrapper, id, act) ||
-            AccessCheck::isLocationObjectSameOrigin(cx, wrapper)) {
+        if (act != js::Wrapper::PUNCTURE &&
+            (AccessCheck::isCrossOriginAccessPermitted(cx, wrapper, id, act) ||
+             AccessCheck::isLocationObjectSameOrigin(cx, wrapper))) {
             perm = PermitPropertyAccess;
             return true;
         }
+
         JSAutoEnterCompartment ac;
         if (!ac.enter(cx, wrapper))
             return false;
