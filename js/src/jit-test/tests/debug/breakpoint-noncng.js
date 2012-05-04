cd1b87e2979b675e0129f0936621958c459a4535
Jason Orendorff <jorendorff@mozilla.com>
2012-05-04 10:49:13 -0500

    Bug 738479 - Debugger.Script objects whose referents are non-compile-and-go are crashy. Also fixes bug 746973. r=jimb.
    --HG--
    extra : rebase_source : 406065559a1e26485503b6d1e4d7eeea9070321f

---
+++ js/src/jit-test/tests/debug/breakpoint-noncng.js
@@ -0,0 +1,20 @@
+// Breakpoints work in non-compile-and-go code. Bug 738479.
+
+var g = newGlobal('new-compartment');
+g.s = '';
+var dbg = new Debugger;
+var gw = dbg.addDebuggee(g);
+g.evaluateNonCompileAndGo(
+    "function f() {\n" +  // fscript.startLine
+    "    s += 'a';\n" +   // fscript.startLine + 1
+    "    s += 'b';\n" +   // fscript.startLine + 2
+    "}\n");
+
+var fscript = gw.makeDebuggeeValue(g.f).script;
+var handler = {hit: function (frame) { g.s += '1'; }};
+for (var pc of fscript.getLineOffsets(fscript.startLine + 2))
+    fscript.setBreakpoint(pc, handler);
+
+g.f();
+
+assertEq(g.s, "a1b");
