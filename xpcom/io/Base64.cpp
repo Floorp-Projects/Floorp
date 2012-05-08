3250008f94d9d636daed3078eecd4a37d2246d9d
Benjamin Smedberg <benjamin@smedbergs.us>
2012-05-08 12:42:27 -0400

    Bug 737164 part A - make the nsTSubstring string API infallible by default. Note that this patch for reviewing sanity does not modify the subclass APIs, that will be a separate revision, r=jlebar
    --HG--
    extra : rebase_source : 48db8595e35e95ea6ddc3a35a553d5641b8d1a42

--- xpcom/io/Base64.cpp
+++ xpcom/io/Base64.cpp
@@ -277,7 +277,7 @@ Base64Encode(const nsACString &aBinaryData, nsACString &aString)
   char *buffer;
 
   // Add one byte for null termination.
-  if (aString.SetCapacity(stringLen + 1) &&
+  if (aString.SetCapacity(stringLen + 1, fallible_t()) &&
     (buffer = aString.BeginWriting()) &&
     PL_Base64Encode(aBinaryData.BeginReading(), aBinaryData.Length(), buffer)) {
     // PL_Base64Encode doesn't null terminate the buffer for us when we pass
@@ -321,7 +321,7 @@ Base64Decode(const nsACString &aString, nsACString &aBinaryData)
   char *buffer;
 
   // Add one byte for null termination.
-  if (aBinaryData.SetCapacity(binaryDataLen + 1) &&
+  if (aBinaryData.SetCapacity(binaryDataLen + 1, fallible_t()) &&
     (buffer = aBinaryData.BeginWriting()) &&
     PL_Base64Decode(aString.BeginReading(), aString.Length(), buffer)) {
     // PL_Base64Decode doesn't null terminate the buffer for us when we pass
