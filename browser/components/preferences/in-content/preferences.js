c9dbc7cbb75074fd1511423db857a29151780998
Jon Rietveld <jon.rietveld@gmail.com>
2012-05-08 19:10:25 -0700

    Bug 733473 - Implement initial prerequisites for in-content preferences, and landing page. r=bmcbride

---
+++ browser/components/preferences/in-content/preferences.js
@@ -0,0 +1,58 @@
+/* - This Source Code Form is subject to the terms of the Mozilla Public
+   - License, v. 2.0. If a copy of the MPL was not distributed with this file,
+   - You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+ 
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+const Cr = Components.results;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+function init_all() {
+  document.documentElement.instantApply = true;
+  window.history.replaceState("landing", document.title);
+  window.addEventListener("popstate", onStatePopped, true);
+  updateCommands();
+  var initFinished = document.createEvent("Event");
+  initFinished.initEvent("Initialized", true, true);
+  document.dispatchEvent(initFinished);
+}
+
+function gotoPref(page) {
+  window.history.pushState(page, document.title);
+  updateCommands();
+}
+ 
+function cmd_back() {
+  window.history.back();
+}
+ 
+function cmd_forward() {
+  window.history.forward();
+}
+
+function onStatePopped(aEvent) {
+  updateCommands();
+  search(aEvent.state, "data-category");
+}
+
+function updateCommands() {
+  document.getElementById("back-btn").disabled = !canGoBack();
+  document.getElementById("forward-btn").disabled = !canGoForward();
+}
+
+function canGoBack() {
+  return window.QueryInterface(Ci.nsIInterfaceRequestor)
+               .getInterface(Ci.nsIWebNavigation)
+               .canGoBack;
+}
+
+function canGoForward() {
+  return window.QueryInterface(Ci.nsIInterfaceRequestor)
+               .getInterface(Ci.nsIWebNavigation)
+               .canGoForward;
+}
