f44d419839f426dc87073381f38cc8b0acd390ef
Vladimir Vukicevic <vladimir@pobox.com>
2012-05-09 22:32:54 -0400

    b=752368; [android] random black screen when opening links from other apps; r=bgirard,ajuma

--- gfx/layers/ipc/CompositorParent.h
+++ gfx/layers/ipc/CompositorParent.h
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set sw=4 ts=8 et tw=80 : */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
@@ -52,6 +52,7 @@
 #include "mozilla/layers/PCompositorParent.h"
 #include "mozilla/layers/PLayersParent.h"
 #include "base/thread.h"
+#include "mozilla/Monitor.h"
 #include "ShadowLayersManager.h"
 
 class nsIWidget;
@@ -169,6 +170,8 @@ private:
   PlatformThreadId mThreadID;
   bool mRenderToEGLSurface;
   nsIntSize mEGLSurfaceSize;
+
+  mozilla::Monitor mPauseCompositionMonitor;
 
   DISALLOW_EVIL_CONSTRUCTORS(CompositorParent);
 };
