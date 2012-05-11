ae9248f803c33bfb9a18c068f061fcd738ac6b1e
Mark Capella <markcapella@twcny.rr.com>
2012-05-10 23:50:03 -0400

    Bug 751497 - replace nsHTMLSelectOptionAccessible::GetSelectState by nice inline, r=surkov, f=hub

--- accessible/src/html/nsHTMLSelectAccessible.cpp
+++ accessible/src/html/nsHTMLSelectAccessible.cpp
@@ -265,9 +265,12 @@ nsHTMLSelectOptionAccessible::NativeState()
   // because we don't want EDITABLE or SELECTABLE_TEXT
   PRUint64 state = nsAccessible::NativeState();
 
-  PRUint64 selectState = 0;
-  nsIContent* selectContent = GetSelectState(&selectState);
-  if (!selectContent || selectState & states::INVISIBLE)
+  nsAccessible* select = GetSelect();
+  if (!select)
+    return state;
+
+  PRUint64 selectState = select->State();
+  if (selectState & states::INVISIBLE)
     return state;
 
   // Focusable and selectable
@@ -390,30 +393,6 @@ nsHTMLSelectOptionAccessible::ContainerWidget() const
 {
   return mParent && mParent->IsListControl() ? mParent : nsnull;
 }
-
-////////////////////////////////////////////////////////////////////////////////
-// nsHTMLSelectOptionAccessible: private methods
-
-nsIContent*
-nsHTMLSelectOptionAccessible::GetSelectState(PRUint64* aState)
-{
-  *aState = 0;
-
-  nsIContent* selectNode = mContent;
-  while (selectNode && selectNode->Tag() != nsGkAtoms::select) {
-    selectNode = selectNode->GetParent();
-  }
-
-  if (selectNode) {
-    nsAccessible* select = mDoc->GetAccessible(selectNode);
-    if (select) {
-      *aState = select->State();
-      return selectNode;
-    }
-  }
-  return nsnull; 
-}
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // nsHTMLSelectOptGroupAccessible
