0acea152200949949ab47e027142e39ef2a898f9
Paul Rouget <paul@mozilla.com>
2012-05-11 16:54:47 +0200

    Bug 724509 - Add an Option menu in the Inspector Toolbar. r=dao r=dcamp

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1051,6 +1051,8 @@ pref("devtools.inspector.htmlHeight", 112);
 pref("devtools.inspector.htmlPanelOpen", false);
 pref("devtools.inspector.sidebarOpen", false);
 pref("devtools.inspector.activeSidebar", "ruleview");
+pref("devtools.inspector.highlighterShowVeil", true);
+pref("devtools.inspector.highlighterShowInfobar", true);
 
 // Enable the Layout View
 pref("devtools.layoutview.enabled", false);
