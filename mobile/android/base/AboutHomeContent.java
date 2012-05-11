5d7549316f4f29c04203aeffe21f0a04ec171d27
Gian-Carlo Pascutto <gpascutto@mozilla.com>
2012-05-11 18:10:57 +0200

    Bug 752492 - Make sure Cursor objects are closed correctly. r=blassey

--- mobile/android/base/AboutHomeContent.java
+++ mobile/android/base/AboutHomeContent.java
@@ -334,8 +334,10 @@ public class AboutHomeContent extends ScrollView
         // UI thread as it touches disk to access a sqlite DB.
         final boolean syncIsSetup = isSyncSetup();
 
-        ContentResolver resolver = GeckoApp.mAppContext.getContentResolver();
-        mCursor = BrowserDB.getTopSites(resolver, NUMBER_OF_TOP_SITES_PORTRAIT);
+        final ContentResolver resolver = GeckoApp.mAppContext.getContentResolver();
+        final Cursor oldCursor = mCursor;
+        // Swap in the new cursor.
+        mCursor = BrowserDB.getTopSites(resolver, NUMBER_OF_TOP_SITES_PORTRAIT);;
 
         GeckoApp.mAppContext.mMainHandler.post(new Runnable() {
             public void run() {
@@ -354,6 +356,10 @@ public class AboutHomeContent extends ScrollView
                 }
 
                 updateLayout(startupMode, syncIsSetup);
+
+                // Free the old Cursor in the right thread now.
+                if (oldCursor != null && !oldCursor.isClosed())
+                    oldCursor.close();
             }
         });
     }
