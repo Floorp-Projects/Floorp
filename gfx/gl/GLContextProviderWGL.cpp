1a4e3926bf1621dc02eba3df6fcb989bb0429af0
Benoit Jacob <bjacob@mozilla.com>
2012-05-12 19:23:56 -0400

    Back out patches from bug 749678, except for the include guards - no review, fix regression (Bug 753350)
    Unfortunately, in-process plugins using OpenGL break the assumption made by these patches, that the current GL context is only changed by GLContext::MakeCurrent. Another issue, regardless of in-process, is that our host-side code in nsCoreAnimationSupport.mm uses direct CGL calls, bypassing GLContext.

--- gfx/gl/GLContextProviderWGL.cpp
+++ gfx/gl/GLContextProviderWGL.cpp
@@ -333,10 +333,18 @@ public:
         return true;
     }
 
-    bool MakeCurrentImpl()
+    bool MakeCurrentImpl(bool aForce = false)
     {
-        bool succeeded = sWGLLibrary.fMakeCurrent(mDC, mContext);
-        NS_ASSERTION(succeeded, "Failed to make GL context current!");
+        BOOL succeeded = true;
+
+        // wglGetCurrentContext seems to just pull the HGLRC out
+        // of its TLS slot, so no need to do our own tls slot.
+        // You would think that wglMakeCurrent would avoid doing
+        // work if mContext was already current, but not so much..
+        if (aForce || sWGLLibrary.fGetCurrentContext() != mContext) {
+            succeeded = sWGLLibrary.fMakeCurrent(mDC, mContext);
+            NS_ASSERTION(succeeded, "Failed to make GL context current!");
+        }
 
         return succeeded;
     }
