cd350779e82577248a574f60493c3165ac20e82d
Bas Schouten <bschouten@mozilla.com>
2012-05-15 16:57:51 +0200

    Bug 717393 - Part 1: Add helper for creating DWriteGlyphRuns. r=jrmuizel

--- gfx/2d/DrawTargetD2D.cpp
+++ gfx/2d/DrawTargetD2D.cpp
@@ -910,34 +910,11 @@ DrawTargetD2D::FillGlyphs(ScaledFont *aFont,
 
   RefPtr<ID2D1Brush> brush = CreateBrushForPattern(aPattern, aOptions.mAlpha);
 
-  DWRITE_GLYPH_RUN glyphRun;
-
-  glyphRun.bidiLevel = 0;
-  glyphRun.fontEmSize = font->mSize;
-  glyphRun.isSideways = FALSE;
-  glyphRun.fontFace = font->mFontFace;
-  glyphRun.glyphCount = aBuffer.mNumGlyphs;
-  
-  std::vector<UINT16> indices;
-  std::vector<FLOAT> advances;
-  std::vector<DWRITE_GLYPH_OFFSET> offsets;
-  indices.resize(aBuffer.mNumGlyphs);
-  advances.resize(aBuffer.mNumGlyphs);
-  offsets.resize(aBuffer.mNumGlyphs);
-
-  memset(&advances.front(), 0, sizeof(FLOAT) * aBuffer.mNumGlyphs);
-  for (unsigned int i = 0; i < aBuffer.mNumGlyphs; i++) {
-    indices[i] = aBuffer.mGlyphs[i].mIndex;
-    offsets[i].advanceOffset = aBuffer.mGlyphs[i].mPosition.x;
-    offsets[i].ascenderOffset = -aBuffer.mGlyphs[i].mPosition.y;
-  }
-
-  glyphRun.glyphAdvances = &advances.front();
-  glyphRun.glyphIndices = &indices.front();
-  glyphRun.glyphOffsets = &offsets.front();
+  AutoDWriteGlyphRun autoRun;
+  DWriteGlyphRunFromGlyphs(aBuffer, font, &autoRun);
 
   if (brush) {
-    rt->DrawGlyphRun(D2D1::Point2F(), &glyphRun, brush);
+    rt->DrawGlyphRun(D2D1::Point2F(), &autoRun, brush);
   }
 
   FinalizeRTForOperation(aOptions.mCompositionOp, aPattern, Rect(0, 0, (Float)mSize.width, (Float)mSize.height));
