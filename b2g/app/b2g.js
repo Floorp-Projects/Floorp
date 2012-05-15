6fd4153fd760d30fff41c99d9a5c03822f49b0ab
Ed Morley <emorley@mozilla.com>
2012-05-15 18:38:50 +0100

    Backout f177646e2aa2 (bug 755320), 1bae327de007 (bug 755001) for test_browserFrame7.html M3 failures

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -425,12 +425,10 @@ pref("browser.link.open_newwindow", 3);
 // 2: don't divert window.open with features
 pref("browser.link.open_newwindow.restriction", 0);
 
-// Enable browser frames (including OOP), but make in-process browser frames
-// the default.
+// Enable browser frames, but not OOP.
 pref("dom.mozBrowserFramesEnabled", true);
 pref("dom.mozBrowserFramesWhitelist", "http://homescreen.gaiamobile.org,http://browser.gaiamobile.org");
-pref("dom.ipc.tabs.disabled", false);
-pref("dom.ipc.browser_frames.oop_by_default", false);
+pref("dom.ipc.tabs.disabled", true);
 
 // Temporary permission hack for WebSMS
 pref("dom.sms.enabled", true);
