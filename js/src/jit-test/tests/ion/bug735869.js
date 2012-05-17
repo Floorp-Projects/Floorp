0e4890b6265de4e4adf51476f17327559eeb1c4b
David Anderson <danderson@mozilla.com>
2012-05-17 11:01:22 -0700

    Only unlink cx->enumerators for JSITER_ENUMERATE iterators (bug 735869, r=jandem).

---
+++ js/src/jit-test/tests/ion/bug735869.js
@@ -0,0 +1,11 @@
+o = {}
+for (let i = 0; i < 70; i++) {
+    try {
+        p
+    } catch (e) {}
+    (function() {
+        for (x in Iterator.prototype) {}
+    })()
+    o.__proto__ = null
+    delete o.__proto__
+}
