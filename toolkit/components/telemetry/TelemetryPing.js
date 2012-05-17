66e9d62896f8bad474c6d7069d5d193fab4eb3fc
Nathan Froyd <froydnj@mozilla.com>
2012-05-17 14:58:48 -0400

    Bug 756152 - disable persistent telemetry sending; r=taras

--- toolkit/components/telemetry/TelemetryPing.js
+++ toolkit/components/telemetry/TelemetryPing.js
@@ -173,6 +173,8 @@ TelemetryPing.prototype = {
   _startupHistogramRegex: /SQLITE|HTTP|SPDY|CACHE|DNS/,
   _slowSQLStartup: {},
   _prevSession: null,
+  // Bug 756152
+  _disablePersistentTelemetrySending: true,
 
   /**
    * When reflecting a histogram into JS, Telemetry hands us an object
@@ -643,6 +645,10 @@ TelemetryPing.prototype = {
   },
 
   loadHistograms: function loadHistograms(file, sync) {
+    if (this._disablePersistentTelemetrySending) {
+      return;
+    }
+
     let self = this;
     let loadCallback = function(data) {
       self._prevSession = data;
@@ -729,6 +735,9 @@ TelemetryPing.prototype = {
       break;
     case "test-load-histograms":
       this.loadHistograms(aSubject.QueryInterface(Ci.nsILocalFile), true);
+      break;
+    case "test-enable-persistent-telemetry-send":
+      this._disablePersistentTelemetrySending = false;
       break;
     case "test-ping":
       server = aData;
