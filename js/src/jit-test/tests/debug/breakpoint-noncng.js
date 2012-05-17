8dd1defcac11bcbe4b700fc3f3f36e07f757ff1f
Jason Orendorff <jorendorff@mozilla.com>
2012-05-17 18:54:30 -0500

    Bug 755808 - Add more options to JS shell evaluate() function. r=jimb.

--- js/src/jit-test/tests/debug/breakpoint-noncng.js
+++ js/src/jit-test/tests/debug/breakpoint-noncng.js
@@ -4,11 +4,12 @@ var g = newGlobal('new-compartment');
 g.s = '';
 var dbg = new Debugger;
 var gw = dbg.addDebuggee(g);
-g.evaluateNonCompileAndGo(
+g.evaluate(
     "function f() {\n" +  // fscript.startLine
     "    s += 'a';\n" +   // fscript.startLine + 1
     "    s += 'b';\n" +   // fscript.startLine + 2
-    "}\n");
+    "}\n",
+    {compileAndGo: false});
 
 var fscript = gw.makeDebuggeeValue(g.f).script;
 var handler = {hit: function (frame) { g.s += '1'; }};
