9fc93b9b41f6f77e4da8a2acbd5a24e8db1907a6
Andrew McCreight <amccreight@mozilla.com>
2012-05-18 15:00:19 -0700

    Bug 749385 - use deferred release in nsHTMLDocumentSH::ReleaseDocument. r=peterv

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -698,7 +698,7 @@ public:
 
     JSBool OnJSContextNew(JSContext* cx);
 
-    JSBool DeferredRelease(nsISupports* obj);
+    bool DeferredRelease(nsISupports* obj);
 
     JSBool GetDoingFinalization() const {return mDoingFinalization;}
 
