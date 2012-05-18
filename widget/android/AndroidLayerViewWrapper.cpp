632fd8a93f1d06aedf25618dc9214c9dbe8b6bd3
Vladimir Vukicevic <vladimir@pobox.com>
2012-05-18 08:59:01 -0400

    b=750272; protect against raised Java-side exception when getting EGL Surface; r=blassey

--- widget/android/AndroidLayerViewWrapper.cpp
+++ widget/android/AndroidLayerViewWrapper.cpp
@@ -90,6 +90,9 @@ AndroidGLController::ProvideEGLSurface()
     ASSERT_THREAD();
     AutoLocalJNIFrame jniFrame(mJEnv);
     jobject jObj = mJEnv->CallObjectMethod(mJObj, jProvideEGLSurfaceMethod);
+    if (jniFrame.CheckForException())
+        return NULL;
+
     return reinterpret_cast<EGLSurface>(mJEnv->GetIntField(jObj, jEGLSurfacePointerField));
 }
 
