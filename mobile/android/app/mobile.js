c7eaef7d7f77c1a82d4f2401bcecb5fc0d2098d0
Eitan Isaacson <eitan@monotonous.org>
2012-05-18 11:56:38 -0700

    Bug 756529 - Move accessfu setting to accessibility.accessfu.activate. r=davidb

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -760,7 +760,7 @@ pref("ui.scrolling.min_scrollable_distance", -1);
 pref("ui.zooming.animation_frames", "");
 
 // Enable accessibility mode if platform accessibility is enabled.
-pref("accessibility.accessfu", 2);
+pref("accessibility.accessfu.activate", 2);
 
 // Mobile manages state by autodetection
 pref("network.manage-offline-status", true);
