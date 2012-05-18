4c90d2e85229b8b411dd4778cd43c241690247c4
Philipp von Weitershausen <philipp@weitershausen.de>
2012-05-18 13:30:13 -0700

    Bug 754178 - Expose SpecialPowers in Marionette content JS. r=jgriffin

--- testing/marionette/client/marionette/tests/unit/test_specialpowers.py
+++ testing/marionette/client/marionette/tests/unit/test_specialpowers.py
@@ -5,12 +5,31 @@
 from marionette_test import MarionetteTestCase
 from errors import JavascriptException, MarionetteException
 
-class TestSpecialPowers(MarionetteTestCase):
+class TestSpecialPowersContent(MarionetteTestCase):
+
+    testpref = "testing.marionette.contentcharpref"
+    testvalue = "blabla"
 
     def test_prefs(self):
-        self.marionette.set_context("chrome")
         result = self.marionette.execute_script("""
-        SpecialPowers.setCharPref("testing.marionette.charpref", "blabla");
-        return SpecialPowers.getCharPref("testing.marionette.charpref")
-        """);
-        self.assertEqual(result, "blabla")
+        SpecialPowers.setCharPref("%(pref)s", "%(value)s");
+        return SpecialPowers.getCharPref("%(pref)s")
+        """ % {'pref': self.testpref, 'value': self.testvalue});
+        self.assertEqual(result, self.testvalue)
+
+    def test_prefs_after_navigate(self):
+        test_html = self.marionette.absolute_url("test.html")
+        self.marionette.navigate(test_html)
+        self.test_prefs()
+
+class TestSpecialPowersChrome(TestSpecialPowersContent):
+
+    testpref = "testing.marionette.chromecharpref"
+    testvalue = "blabla"
+
+    def setUp(self):
+        super(TestSpecialPowersChrome, self).setUp()
+        self.marionette.set_context("chrome")
+
+    def test_prefs_after_navigate(self):
+        pass
