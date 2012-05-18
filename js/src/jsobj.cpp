5c789c363cf32b9821150686308171db88035c91
Ms2ger <ms2ger@gmail.com>
2012-05-18 10:29:40 +0200

    Bug 744935 - Remove JS_PROPERTY_TREE; r=Waldo

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -87,6 +87,7 @@
 
 #include "jsarrayinlines.h"
 #include "jsatominlines.h"
+#include "jscntxtinlines.h"
 #include "jsinterpinlines.h"
 #include "jsobjinlines.h"
 #include "jsscopeinlines.h"
@@ -2556,7 +2557,7 @@ JSObject::sealOrFreeze(JSContext *cx, ImmutabilityType it)
             if (!JSID_IS_EMPTY(child.propid))
                 MarkTypePropertyConfigured(cx, self, child.propid);
 
-            last = JS_PROPERTY_TREE(cx).getChild(cx, last, self->numFixedSlots(), child);
+            last = cx->propertyTree().getChild(cx, last, self->numFixedSlots(), child);
             if (!last)
                 return NULL;
         }
