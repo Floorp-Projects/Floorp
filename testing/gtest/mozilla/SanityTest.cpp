f49834c59d4a1041af30d77dd1a6643508b9852f
Benoit Girard <bgirard@mozilla.com>
2012-05-22 13:45:24 -0700

    Bug 767231 - Part 1: Add GTest. r=ted

---
+++ testing/gtest/mozilla/SanityTest.cpp
@@ -0,0 +1,12 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * * This Source Code Form is subject to the terms of the Mozilla Public
+ * * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "gtest/gtest.h"
+
+// Sanity test to make sure that GTest is hooked into
+// the mozilla build system correctly
+TEST(MozillaGTestSanity, Runs) {
+  EXPECT_EQ(1, 1);
+}
