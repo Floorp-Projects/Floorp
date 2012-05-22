7aff32f12aa76a076ff9b3ee408245b506c4acd6
Ryan Pearl <rpearl@endofunctor.org>
2012-05-22 18:18:28 -0400

    Select an appropriate new representative when breakClass() is called on the old one (Bug 756094, r=mjrosenb)

--- js/src/ion/ValueNumbering.h
+++ js/src/ion/ValueNumbering.h
@@ -107,7 +107,7 @@ class ValueNumberer
     void setClass(MDefinition *toSet, MDefinition *representative);
 
   public:
-    static bool needsSplit(MDefinition *);
+    static MDefinition *findSplit(MDefinition *);
     void breakClass(MDefinition*);
 
   protected:
@@ -124,7 +124,7 @@ class ValueNumberer
 class ValueNumberData : public TempObject {
 
     friend void ValueNumberer::breakClass(MDefinition*);
-    friend bool ValueNumberer::needsSplit(MDefinition*);
+    friend MDefinition *ValueNumberer::findSplit(MDefinition*);
     uint32 number;
     MDefinition *classNext;
     MDefinition *classPrev;
