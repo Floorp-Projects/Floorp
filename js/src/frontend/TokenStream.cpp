336a795c758c42dbeecf3d412eb0348a86ae261f
Jan de Mooij <jdemooij@mozilla.com>
2012-05-23 13:37:43 +0200

    Bug 756600 - Initialize TokenStream::sourceMap in constructor instead of init. r=njn

--- js/src/frontend/TokenStream.cpp
+++ js/src/frontend/TokenStream.cpp
@@ -123,7 +123,7 @@ TokenStream::TokenStream(JSContext *cx, JSPrincipals *prin, JSPrincipals *origin
   : tokens(), tokensRoot(cx, &tokens),
     cursor(), lookahead(), flags(),
     linebaseRoot(cx, &linebase), prevLinebaseRoot(cx, &prevLinebase), userbufRoot(cx, &userbuf),
-    listenerTSData(), tokenbuf(cx),
+    sourceMap(), listenerTSData(), tokenbuf(cx),
     cx(cx), originPrincipals(JSScript::normalizeOriginPrincipals(prin, originPrin))
 {
     if (originPrincipals)
@@ -145,7 +145,6 @@ TokenStream::init(const jschar *base, size_t length, const char *fn, unsigned ln
     userbuf.init(base, length);
     linebase = base;
     prevLinebase = NULL;
-    sourceMap = NULL;
 
     JSSourceHandler listener = cx->runtime->debugHooks.sourceHandler;
     void *listenerData = cx->runtime->debugHooks.sourceHandlerData;
