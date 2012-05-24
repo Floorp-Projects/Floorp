5934a623477b025040de18220961d349325e6fc9
Bas Schouten <bschouten@mozilla.com>
2012-05-24 17:43:55 +0200

    Bug 758120: Block attempts to do manual subpixel-AA on bitmap fonts. r=jfkthame

--- gfx/thebes/gfxFont.h
+++ gfx/thebes/gfxFont.h
@@ -1457,6 +1457,8 @@ public:
     // This is called by the default Draw() implementation above.
     virtual bool SetupCairoFont(gfxContext *aContext) = 0;
 
+    virtual bool AllowSubpixelAA() { return true; }
+
     bool IsSyntheticBold() { return mApplySyntheticBold; }
 
     // Amount by which synthetic bold "fattens" the glyphs: 1/16 of the em-size
