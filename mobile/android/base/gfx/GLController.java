34309ca0659f5a3f95352f5f28d109a142c47553
Ed Morley <emorley@mozilla.com>
2012-05-24 17:51:46 +0100

    Backout 5f82df6b980e (bug 757944) on suspicion of reftest failures

--- mobile/android/base/gfx/GLController.java
+++ mobile/android/base/gfx/GLController.java
@@ -137,6 +137,11 @@ public class GLController {
         }
 
         mGL = mEGLContext.getGL();
+
+        if (mView.getRenderer() != null) {
+            mView.getRenderer().onSurfaceCreated((GL10)mGL, mEGLConfig);
+            mView.getRenderer().onSurfaceChanged((GL10)mGL, mWidth, mHeight);
+        }
     }
 
     private EGLConfig chooseConfig() {
