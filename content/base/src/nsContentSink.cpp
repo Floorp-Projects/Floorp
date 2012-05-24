dcd6c8a7101de4902d0d1ae3972844daa783cd0f
Josh Matthews <josh@joshmatthews.net>
2012-05-24 16:31:54 +0100

    Bug 722845 - Part 5: Disable offline cache entries for private channels. r=mayhemer

--- content/base/src/nsContentSink.cpp
+++ content/base/src/nsContentSink.cpp
@@ -18,6 +18,7 @@
 #include "nsStyleLinkElement.h"
 #include "nsINodeInfo.h"
 #include "nsIDocShell.h"
+#include "nsILoadContext.h"
 #include "nsIDocShellTreeItem.h"
 #include "nsCPrefetchService.h"
 #include "nsIURI.h"
@@ -1029,6 +1030,13 @@ nsContentSink::ProcessOfflineManifest(const nsAString& aManifestSpec)
   // Don't bother processing offline manifest for documents
   // without a docshell
   if (!mDocShell) {
+    return;
+  }
+
+  // If the docshell's in private browsing mode, we don't want to do any
+  // manifest processing.
+  nsCOMPtr<nsILoadContext> loadContext = do_QueryInterface(mDocShell);
+  if (loadContext->UsePrivateBrowsing()) {
     return;
   }
 
