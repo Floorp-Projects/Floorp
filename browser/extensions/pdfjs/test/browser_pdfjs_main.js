54513dba58271762b1002aa9b082826e45964d38
Artur Adib <aadib@mozilla.com>
2012-05-25 13:21:59 -0400

    Bug 748924 - PDF viewer: New interface + Mochitest controls (tests) r=ctalbert

--- browser/extensions/pdfjs/test/browser_pdfjs_main.js
+++ browser/extensions/pdfjs/test/browser_pdfjs_main.js
@@ -2,27 +2,70 @@
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
 
-const RELATIVE_DIR = "browser/features/pdfjs/test/";
+const RELATIVE_DIR = "browser/extensions/pdfjs/test/";
 const TESTROOT = "http://example.com/browser/" + RELATIVE_DIR;
 
 function test() {
+  var tab;
+
   waitForExplicitFinish();
-
-  var tab = gBrowser.addTab(TESTROOT + "file_pdfjs_test.pdf");
-  var newTabBrowser = gBrowser.getBrowserForTab(tab);
-  newTabBrowser.addEventListener("load", function onLoad() {
-    newTabBrowser.removeEventListener("load", onLoad, true);
-
-    var hasViewer = newTabBrowser.contentDocument.querySelector('div#viewer');
-    var hasPDFJS = 'PDFJS' in newTabBrowser.contentWindow.wrappedJSObject;
-
-    ok(hasViewer, "document content has viewer UI");
-    ok(hasPDFJS, "window content has PDFJS object");
-
-    finish();
-  }, true, true);
-
   registerCleanupFunction(function() {
     gBrowser.removeTab(tab);
   });
+
+  tab = gBrowser.addTab(TESTROOT + "file_pdfjs_test.pdf");
+  var newTabBrowser = gBrowser.getBrowserForTab(tab);
+  newTabBrowser.addEventListener("load", function eventHandler() {
+    newTabBrowser.removeEventListener("load", eventHandler, true);
+
+    var document = newTabBrowser.contentDocument,
+        window = newTabBrowser.contentWindow;
+
+    // Runs tests after all 'load' event handlers have fired off
+    setTimeout(function() {
+      runTests(document, window);
+    }, 0);
+  }, true);
+}
+
+
+function runTests(document, window) {
+  //
+  // Overall sanity tests
+  //
+  ok(document.querySelector('div#viewer'), "document content has viewer UI");
+  ok('PDFJS' in window.wrappedJSObject, "window content has PDFJS object");
+
+  //
+  // Sidebar: open
+  //
+  var sidebar = document.querySelector('button#sidebarToggle'),
+      outerContainer = document.querySelector('div#outerContainer');
+
+  sidebar.click();
+  ok(outerContainer.classList.contains('sidebarOpen'), 'sidebar opens on click');
+
+  //
+  // Sidebar: close
+  //
+  sidebar.click();
+  ok(!outerContainer.classList.contains('sidebarOpen'), 'sidebar closes on click');
+
+  //
+  // Page change from prev/next buttons
+  //
+  var prevPage = document.querySelector('button#previous'),
+      nextPage = document.querySelector('button#next');
+
+  var pageNumber = document.querySelector('input#pageNumber');
+  is(parseInt(pageNumber.value), 1, 'initial page is 1');
+
+  //
+  // Bookmark button
+  //
+  var viewBookmark = document.querySelector('a#viewBookmark');
+  viewBookmark.click();
+  ok(viewBookmark.href.length > 0, 'viewBookmark button has href');
+
+  finish();
 }
