7238dcb39601f0972514a669efea6ad9e78d2093
Jeff Walden <jwalden@mit.edu>
2012-05-24 23:54:05 -0700

    Bug 758499 - Remove JSRESOLVE_WITH.  r=dmandelin

--- js/src/vm/ScopeObject.cpp
+++ js/src/vm/ScopeObject.cpp
@@ -332,12 +332,6 @@ WithObject::create(JSContext *cx, HandleObject proto, HandleObject enclosing, ui
 static JSBool
 with_LookupGeneric(JSContext *cx, HandleObject obj, HandleId id, JSObject **objp, JSProperty **propp)
 {
-    /* Fixes bug 463997 */
-    unsigned flags = cx->resolveFlags;
-    if (flags == RESOLVE_INFER)
-        flags = js_InferFlags(cx, flags);
-    flags |= JSRESOLVE_WITH;
-    JSAutoResolveFlags rf(cx, flags);
     return obj->asWith().object().lookupGeneric(cx, id, objp, propp);
 }
 
