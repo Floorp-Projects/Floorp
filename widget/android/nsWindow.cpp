11b3ab6fce5639d5c9c5fc6ba9010622752d65ef
Kartikaya Gupta <kgupta@mozilla.com>
2012-05-26 11:06:55 -0400

    Bug 753665 - Add some missing kungFuDeathGrips while dispatching events. r=blassey

--- widget/android/nsWindow.cpp
+++ widget/android/nsWindow.cpp
@@ -1310,6 +1310,8 @@ nsWindow::OnMotionEvent(AndroidGeckoEvent *ae)
             return;
     }
 
+    nsRefPtr<nsWindow> kungFuDeathGrip(this);
+
 send_again:
 
     nsMouseEvent event(true,
@@ -1343,6 +1345,8 @@ getDistance(const nsIntPoint &p1, const nsIntPoint &p2)
 
 bool nsWindow::OnMultitouchEvent(AndroidGeckoEvent *ae)
 {
+    nsRefPtr<nsWindow> kungFuDeathGrip(this);
+
     // This is set to true once we have called SetPreventPanning() exactly
     // once for a given sequence of touch events. It is reset on the start
     // of the next sequence.
