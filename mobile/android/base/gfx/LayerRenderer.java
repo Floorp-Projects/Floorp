dffc958152ff26b0d7a3c1c31f52e23d8bf542f1
Kartikaya Gupta <kgupta@mozilla.com>
2012-05-26 10:59:11 -0400

    Bug 758635 - Remove redundant code to set the viewport size. r=Cwiiis

--- mobile/android/base/gfx/LayerRenderer.java
+++ mobile/android/base/gfx/LayerRenderer.java
@@ -331,14 +331,6 @@ public class LayerRenderer implements GLSurfaceView.Renderer {
             moveFrameRateLayer(width, height);
         }
 
-        // updating the state in the view/controller/client should be
-        // done on the main UI thread, not the GL renderer thread
-        mView.post(new Runnable() {
-            public void run() {
-                mView.setViewportSize(new IntSize(width, height));
-            }
-        });
-
         /* TODO: Throw away tile images? */
     }
 
