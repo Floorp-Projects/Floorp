97d4f96edee8ac59d08633448c2b73c7ff3b63ae
Jason Orendorff <jorendorff@mozilla.com>
2012-05-30 15:05:59 -0500

    Bug 738468 - DebuggerScript_getUrl crashes if script->filename is null. r=jimb.

---
+++ js/src/jit-test/tests/debug/Script-url.js
@@ -0,0 +1,10 @@
+// Script.prototype.url can be a string or null.
+
+var g = newGlobal('new-compartment');
+var dbg = new Debugger;
+var gw = dbg.addDebuggee(g);
+for (var fileName of ['file:///var/foo.js', null]) {
+    g.evaluate("function f(x) { return 2*x; }", {fileName: fileName});
+    var fw = gw.getOwnPropertyDescriptor('f').value;
+    assertEq(fw.script.url, fileName);
+}
