f2ec60ed52f7fb1a2ce12d5d14bfa49aff84f7ed
Phil Ringnalda <philringnalda@gmail.com>
2012-05-29 23:27:40 -0700

    Bug 758990 - Don't create feed URIs that inherit security context, r=gavin

---
+++ browser/components/feeds/test/unit/test_758990.js
@@ -0,0 +1,42 @@
+function run_test() {
+  var success = false;
+  try {
+    var newURI = ios.newURI("feed:javascript:alert('hi');", null, null);
+  }
+  catch (e) {
+    success = e.result == Cr.NS_ERROR_MALFORMED_URI;
+  }
+  if (!success)
+    do_throw("We didn't throw NS_ERROR_MALFORMED_URI creating a feed:javascript: URI");
+
+  success = false;
+  try {
+    newURI = ios.newURI("feed:data:text/html,hi", null, null);
+  }
+  catch (e) {
+    success = e.result == Cr.NS_ERROR_MALFORMED_URI;
+  }
+  if (!success)
+    do_throw("We didn't throw NS_ERROR_MALFORMED_URI creating a feed:data: URI");
+
+  success = false;
+  try {
+    newURI = ios.newURI("pcast:javascript:alert('hi');", null, null);
+  }
+  catch (e) {
+    success = e.result == Cr.NS_ERROR_MALFORMED_URI;
+  }
+  if (!success)
+    do_throw("We didn't throw NS_ERROR_MALFORMED_URI creating a pcast:javascript: URI");
+
+  success = false;
+  try {
+    newURI = ios.newURI("pcast:data:text/html,hi", null, null);
+  }
+  catch (e) {
+    success = e.result == Cr.NS_ERROR_MALFORMED_URI;
+  }
+  if (!success)
+    do_throw("We didn't throw NS_ERROR_MALFORMED_URI creating a pcast:data: URI");
+
+}
