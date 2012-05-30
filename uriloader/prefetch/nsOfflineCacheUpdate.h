ac8629b453455498303dd99da2ceffcc6cd61a40
Phil Ringnalda <philringnalda@gmail.com>
2012-05-29 21:08:18 -0700

    Back out e4823da049f8 (bug 744719) for test failures and crashes

--- uriloader/prefetch/nsOfflineCacheUpdate.h
+++ uriloader/prefetch/nsOfflineCacheUpdate.h
@@ -71,10 +71,6 @@ public:
     nsresult Cancel();
     nsresult GetRequestSucceeded(bool * succeeded);
 
-    bool IsInProgress();
-    bool IsScheduled();
-    bool IsCompleted();
-
 private:
     nsOfflineCacheUpdate*          mUpdate;
     nsCOMPtr<nsIChannel>           mChannel;
@@ -183,14 +179,12 @@ public:
 
 class nsOfflineCacheUpdate : public nsIOfflineCacheUpdate
                            , public nsIOfflineCacheUpdateObserver
-                           , public nsIRunnable
                            , public nsOfflineCacheUpdateOwner
 {
 public:
     NS_DECL_ISUPPORTS
     NS_DECL_NSIOFFLINECACHEUPDATE
     NS_DECL_NSIOFFLINECACHEUPDATEOBSERVER
-    NS_DECL_NSIRUNNABLE
 
     nsOfflineCacheUpdate();
     ~nsOfflineCacheUpdate();
@@ -202,7 +196,7 @@ public:
     nsresult Begin();
     nsresult Cancel();
 
-    void LoadCompleted(nsOfflineCacheUpdateItem *aItem);
+    void LoadCompleted();
     void ManifestCheckCompleted(nsresult aStatus,
                                 const nsCString &aManifestHash);
     void StickDocument(nsIURI *aDocumentURI);
@@ -266,7 +260,7 @@ private:
     nsRefPtr<nsOfflineManifestItem> mManifestItem;
 
     /* Items being updated */
-    PRUint32 mItemsInProgress;
+    PRInt32 mCurrentItem;
     nsTArray<nsRefPtr<nsOfflineCacheUpdateItem> > mItems;
 
     /* Clients watching this update for changes */
