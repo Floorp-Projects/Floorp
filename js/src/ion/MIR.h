c1b961a022977843214f43b0a8c72cf5271afad1
David Anderson <danderson@mozilla.com>
2012-05-30 16:15:31 -0400

    Ensure that all phis connected to iterators are kept live (bug 759213, r=jandem).

--- js/src/ion/MIR.h
+++ js/src/ion/MIR.h
@@ -2341,11 +2341,13 @@ class MPhi : public MDefinition, public InlineForwardListNode<MPhi>
     uint32 slot_;
     bool triedToSpecialize_;
     bool hasBytecodeUses_;
+    bool isIterator_;
 
     MPhi(uint32 slot)
       : slot_(slot),
         triedToSpecialize_(false),
-        hasBytecodeUses_(false)
+        hasBytecodeUses_(false),
+        isIterator_(false)
     {
         setResultType(MIRType_Value);
     }
@@ -2386,6 +2388,12 @@ class MPhi : public MDefinition, public InlineForwardListNode<MPhi>
     }
     void setHasBytecodeUses() {
         hasBytecodeUses_ = true;
+    }
+    bool isIterator() const {
+        return isIterator_;
+    }
+    void setIterator() {
+        isIterator_ = true;
     }
 
     AliasSet getAliasSet() const {
