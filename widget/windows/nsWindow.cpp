60c519213722ac2616f5bfc87792a02df5bbc553
Masayuki Nakano <masayuki@d-toybox.com>
2012-05-30 09:47:03 +0900

    Bug 759346 Append alternative keycodes from virtual keycode if the virtual keycode is an OEM keycode which indicates a character but the character cannot be inputted by the key r=jimm

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -6523,9 +6523,33 @@ LRESULT nsWindow::OnKeyDown(const MSG &aMsg,
         nsAlternativeCharCode chars(unshiftedChar, shiftedChar);
         altArray.AppendElement(chars);
       }
-      if (cnt == num - 1 && (unshiftedLatinChar || shiftedLatinChar)) {
-        nsAlternativeCharCode chars(unshiftedLatinChar, shiftedLatinChar);
-        altArray.AppendElement(chars);
+      if (cnt == num - 1) {
+        if (unshiftedLatinChar || shiftedLatinChar) {
+          nsAlternativeCharCode chars(unshiftedLatinChar, shiftedLatinChar);
+          altArray.AppendElement(chars);
+        }
+
+        // Typically, following virtual keycodes are used for a key which can
+        // input the character.  However, these keycodes are also used for
+        // other keys on some keyboard layout.  E.g., in spite of Shift+'1'
+        // inputs '+' on Thai keyboard layout, a key which is at '=/+'
+        // key on ANSI keyboard layout is VK_OEM_PLUS.  Native applications
+        // handle it as '+' key if Ctrl key is pressed.
+        PRUnichar charForOEMKeyCode = 0;
+        switch (virtualKeyCode) {
+          case VK_OEM_PLUS:   charForOEMKeyCode = '+'; break;
+          case VK_OEM_COMMA:  charForOEMKeyCode = ','; break;
+          case VK_OEM_MINUS:  charForOEMKeyCode = '-'; break;
+          case VK_OEM_PERIOD: charForOEMKeyCode = '.'; break;
+        }
+        if (charForOEMKeyCode &&
+            charForOEMKeyCode != unshiftedChars[0] &&
+            charForOEMKeyCode != shiftedChars[0] &&
+            charForOEMKeyCode != unshiftedLatinChar &&
+            charForOEMKeyCode != shiftedLatinChar) {
+          nsAlternativeCharCode OEMChars(charForOEMKeyCode, charForOEMKeyCode);
+          altArray.AppendElement(OEMChars);
+        }
       }
 
       nsKeyEvent keypressEvent(true, NS_KEY_PRESS, this);
