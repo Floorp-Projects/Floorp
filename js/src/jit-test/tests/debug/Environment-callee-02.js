8811738492b356849e441b00f3a4fe5b0cfd0a7a
Jim Blandy <jimb@mozilla.com>
2012-05-31 13:13:53 -0700

    Bug 747514: Implement Debugger.Environment.prototype.callee accessor. r=jorendorff

---
+++ js/src/jit-test/tests/debug/Environment-callee-02.js
@@ -0,0 +1,25 @@
+// Debugger.Environment.prototype.callee gets the right closure.
+
+var g = newGlobal('new-compartment');
+var dbg = new Debugger;
+var gw = dbg.addDebuggee(g);
+
+g.eval('function f(x) { return function (y) { debugger; return x + y; } }');
+g.eval('var g = f(2);');
+g.eval('var h = f(3);');
+
+function check(fun, label) {
+  print("check(" + label + ")");
+  var hits;
+  dbg.onDebuggerStatement = function (frame) {
+    hits++;
+    var env = frame.environment;
+    assertEq(env.callee, gw.makeDebuggeeValue(fun));
+  };
+  hits = 0;
+  fun();
+  assertEq(hits, 1);
+}
+
+check(g.g, 'g.g');
+check(g.h, 'g.h');
