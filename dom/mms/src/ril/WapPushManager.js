3144699fbfff8678123e5022c8b878b1d13d5b26
Vicamo Yang <vyang@mozilla.com>
2012-06-01 16:48:47 +0800

    Bug 749856 - Part 3: handle WAP Push notification, r=philikon

--- dom/mms/src/ril/WapPushManager.js
+++ dom/mms/src/ril/WapPushManager.js
@@ -6,6 +6,9 @@
 
 const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
 Cu.import("resource://gre/modules/WspPduHelper.jsm");
 
 const DEBUG = false; // set to true to see debug messages
@@ -38,9 +41,14 @@ let WapPushManager = {
       return;
     }
 
-    // FIXME: hand over to others
-
-    debug("No WAP Push application registered for " + appid);
+    if (appid == "x-wap-application:mms.ua") {
+      let mmsService = Cc["@mozilla.org/mms/rilmmsservice;1"]
+                       .getService(Ci.nsIMmsService);
+      mmsService.QueryInterface(Ci.nsIWapPushApplication)
+                .receiveWapPush(data.array, data.array.length, data.offset, options);
+    } else {
+      debug("No WAP Push application registered for " + appid);
+    }
   },
 
   /**
