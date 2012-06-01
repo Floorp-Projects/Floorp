18937d831f816e69699a1be43148f626e7b0b63b
Oleg Romashin <romaxa@gmail.com>
2012-05-31 21:30:08 -0400

    Bug 728524 - Render to shared EGLImage/Texture implementation. r=bgirard,vlad,jgilbert

--- gfx/gl/GLDefs.h
+++ gfx/gl/GLDefs.h
@@ -3253,6 +3253,9 @@ typedef void* GLeglImage;
 #define LOCAL_EGL_DRAW                        0x3059
 #define LOCAL_EGL_CONTEXT_LOST                0x300E
 
+// EGL_KHR_gl_texture_2D_image
+#define LOCAL_EGL_GL_TEXTURE_2D               0x30B1
+
 // EGL_KHR_fence_sync
 #define LOCAL_EGL_SYNC_FENCE                  0x30F9
 #define LOCAL_EGL_SYNC_TYPE                   0x30F7
