61fb0f12a8fe82f6f74ba722da2f35d05270c9f2
Kyle Machulis <kyle@nonpolynomial.com>
2012-06-02 11:23:16 -0700

    Bug 744349 - Create message distribution mechanism for DBus Bluetooth Signals; r=cjones

--- dom/bluetooth/BluetoothCommon.h
+++ dom/bluetooth/BluetoothCommon.h
@@ -7,6 +7,10 @@
 #ifndef mozilla_dom_bluetooth_bluetoothcommon_h__
 #define mozilla_dom_bluetooth_bluetoothcommon_h__
 
+#include "nsString.h"
+#include "nsTArray.h"
+#include "mozilla/Observer.h"
+
 #define BEGIN_BLUETOOTH_NAMESPACE \
   namespace mozilla { namespace dom { namespace bluetooth {
 #define END_BLUETOOTH_NAMESPACE \
@@ -14,6 +18,48 @@
 #define USING_BLUETOOTH_NAMESPACE \
   using namespace mozilla::dom::bluetooth;
 
-class nsIDOMBluetooth;
+class nsCString;
+
+BEGIN_BLUETOOTH_NAMESPACE
+
+/**
+ * BluetoothEvents usually hand back one of 3 types:
+ *
+ * - 32-bit Int
+ * - String
+ * - Bool
+ *
+ * BluetoothVariant encases the types into a single structure.
+ */
+struct BluetoothVariant
+{
+  uint32_t mUint32;
+  nsCString mString;  
+};
+
+/**
+ * BluetoothNamedVariant is a variant with a name value, for passing around
+ * things like properties with variant values.
+ */
+struct BluetoothNamedVariant
+{
+  nsCString mName;
+  BluetoothVariant mValue;
+};
+
+/**
+ * BluetoothEvent holds a variant value and the name of an event, such as
+ * PropertyChanged or DeviceFound.
+ */
+struct BluetoothEvent
+{
+  nsCString mEventName;
+  nsTArray<BluetoothNamedVariant> mValues;
+};
+
+typedef mozilla::Observer<BluetoothEvent> BluetoothEventObserver;
+typedef mozilla::ObserverList<BluetoothEvent> BluetoothEventObserverList;
+
+END_BLUETOOTH_NAMESPACE
 
 #endif // mozilla_dom_bluetooth_bluetoothcommon_h__
