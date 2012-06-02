7901a3257f4f36752a83c4f713a3a9bb5ffd80e8
Mark Capella <markcapella@twcny.rr.com>
2012-06-02 07:30:29 -0400

    Bug 759310 - de-ns-ify nsHTMLTextAccessible file classes, r=surkov

---
+++ accessible/src/html/HTMLElementAccessibles.h
@@ -0,0 +1,83 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_a11y_HTMLElementAccessibles_h__
+#define mozilla_a11y_HTMLElementAccessibles_h__
+
+#include "nsAutoPtr.h"
+#include "nsBaseWidgetAccessible.h"
+
+namespace mozilla {
+namespace a11y {
+
+/**
+ * Used for HTML hr element.
+ */
+class HTMLHRAccessible : public nsLeafAccessible
+{
+public:
+
+  HTMLHRAccessible(nsIContent* aContent, DocAccessible* aDoc) :
+    nsLeafAccessible(aContent, aDoc) {};
+
+  // Accessible
+  virtual a11y::role NativeRole();
+};
+
+/**
+ * Used for HTML br element.
+ */
+class HTMLBRAccessible : public nsLeafAccessible
+{
+public:
+
+  HTMLBRAccessible(nsIContent* aContent, DocAccessible* aDoc) :
+    nsLeafAccessible(aContent, aDoc) {};
+
+  // Accessible
+  virtual nsresult GetNameInternal(nsAString& aName);
+  virtual a11y::role NativeRole();
+  virtual PRUint64 NativeState();
+};
+
+/**
+ * Used for HTML label element.
+ */
+class HTMLLabelAccessible : public HyperTextAccessibleWrap
+{
+public:
+
+  HTMLLabelAccessible(nsIContent* aContent, DocAccessible* aDoc) :
+    HyperTextAccessibleWrap(aContent, aDoc) {};
+
+  NS_DECL_ISUPPORTS_INHERITED
+
+  // Accessible
+  virtual nsresult GetNameInternal(nsAString& aName);
+  virtual a11y::role NativeRole();
+};
+
+/**
+ * Used for HTML output element.
+ */
+class HTMLOutputAccessible : public HyperTextAccessibleWrap
+{
+public:
+
+  HTMLOutputAccessible(nsIContent* aContent, DocAccessible* aDoc) :
+    HyperTextAccessibleWrap(aContent, aDoc) {};
+
+  NS_DECL_ISUPPORTS_INHERITED
+
+  // Accessible
+  virtual a11y::role NativeRole();
+  virtual nsresult GetAttributesInternal(nsIPersistentProperties* aAttributes);
+  virtual Relation RelationByType(PRUint32 aType);
+};
+
+} // namespace a11y
+} // namespace mozilla
+
+#endif
