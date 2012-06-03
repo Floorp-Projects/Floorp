19204af86d33eaf771527c5601b1d16ca4314ed9
Anant Narayanan <anant@kix.in>
2012-06-02 22:08:54 -0700

    Bug 745345: BrowserID support for Apps in the Cloud; r=khuey, r=gps

--- services/aitc/services-aitc.js
+++ services/aitc/services-aitc.js
@@ -1,2 +1,9 @@
+pref("services.aitc.browserid.url", "https://browserid.org/sign_in");
+pref("services.aitc.browserid.log.level", "Debug");
+
 pref("services.aitc.client.log.level", "Debug");
-pref("services.aitc.storage.log.level", "Debug");pref("services.aitc.client.timeout", 120);
+pref("services.aitc.storage.log.level", "Debug");
+
+pref("services.aitc.client.timeout", 120);
+
+pref("services.aitc.storage.log.level", "Debug");
