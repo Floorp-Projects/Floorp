c667e6d3b4ef80394f4325cda8e0f41a6a65fdbd
Vicamo Yang <vyang@mozilla.com>
2012-06-04 13:04:28 +0800

    Bug 744360 - Part 2: add WapPushManager, r=philikon

--- dom/mms/src/ril/wap_consts.js
+++ dom/mms/src/ril/wap_consts.js
@@ -8,6 +8,14 @@
 // @see WAP-230-WSP-20010705-a Appendix A. Assigned Numbers.
 const WSP_PDU_TYPE_PUSH = 0x06;
 
+// Registered WDP Port Numbers
+// @see WAP-259-WDP-20010614-a Appendix B. Port Number Definitions.
+const WDP_PORT_PUSH = 2948;
+
+// Bearer Type Assignments
+// @see WAP-259-WDP-20010614-a Appendix C. Network Bearer Table.
+const WDP_BEARER_GSM_SMS_GSM_MSISDN = 0x03;
+
 const ALL_CONST_SYMBOLS = Object.keys(this);
 
 const EXPORTED_SYMBOLS = ALL_CONST_SYMBOLS;
