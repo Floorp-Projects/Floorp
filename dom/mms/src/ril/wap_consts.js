f760be60b3a13a3bdcbbfc21af60ed6f83cd8f5d
Vicamo Yang <vyang@mozilla.com>
2012-06-04 13:04:24 +0800

    Bug 744360 - Part 1: add WspPduHelper, r=philikon

---
+++ dom/mms/src/ril/wap_consts.js
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// WSP PDU Type Assignments
+// @see WAP-230-WSP-20010705-a Appendix A. Assigned Numbers.
+const WSP_PDU_TYPE_PUSH = 0x06;
+
+const ALL_CONST_SYMBOLS = Object.keys(this);
+
+const EXPORTED_SYMBOLS = ALL_CONST_SYMBOLS;
+
