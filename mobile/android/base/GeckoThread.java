4f84d3cc37b95bf679f7134071f95eb1f52d8f45
Mike Hommey <mh+mozilla@glandium.org>
2012-06-04 16:58:54 +0200

    Bug 760152 - Start library decompression earlier. r=blassey

--- mobile/android/base/GeckoThread.java
+++ mobile/android/base/GeckoThread.java
@@ -18,6 +18,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.CountDownLatch;
 
 public class GeckoThread extends Thread {
     private static final String LOGTAG = "GeckoThread";
@@ -25,13 +26,23 @@ public class GeckoThread extends Thread {
     Intent mIntent;
     String mUri;
     int mRestoreMode;
+    CountDownLatch mStartSignal;
 
-    GeckoThread(Intent intent, String uri, int restoreMode) {
+    GeckoThread() {
+        mStartSignal = new CountDownLatch(1);
+        setName("Gecko");
+    }
+
+    public void init(Intent intent, String uri, int restoreMode) {
         mIntent = intent;
         mUri = uri;
         mRestoreMode = restoreMode;
+    }
 
-        setName("Gecko");
+    public void reallyStart() {
+        mStartSignal.countDown();
+        if (getState() == Thread.State.NEW)
+            start();
     }
 
     public void run() {
@@ -48,6 +59,11 @@ public class GeckoThread extends Thread {
         GeckoAppShell.loadGeckoLibs(resourcePath);
 
         Locale.setDefault(locale);
+
+        try {
+            mStartSignal.await();
+        } catch (Exception e) { }
+
         Resources res = app.getBaseContext().getResources();
         Configuration config = res.getConfiguration();
         config.locale = locale;
