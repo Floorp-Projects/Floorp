b341a19547ce0707aad71bbb1d4ee2cd4175597a
Margaret Leibovic <margaret.leibovic@gmail.com>
2012-06-05 10:43:22 -0700

    Bug 759120 - History list doesn't update after you remove the last history item using context menu. r=lucasr

--- mobile/android/base/AwesomeBarTabs.java
+++ mobile/android/base/AwesomeBarTabs.java
@@ -418,7 +418,6 @@ public class AwesomeBarTabs extends TabHost {
         private static final long MS_PER_WEEK = MS_PER_DAY * 7;
 
         protected Pair<GroupList,List<ChildrenList>> doInBackground(Void... arg0) {
-            Pair<GroupList, List<ChildrenList>> result = null;
             Cursor cursor = BrowserDB.getRecentHistory(mContentResolver, MAX_RESULTS);
 
             Date now = new Date();
@@ -430,9 +429,9 @@ public class AwesomeBarTabs extends TabHost {
 
             // Split the list of urls into separate date range groups
             // and show it in an expandable list view.
-            List<ChildrenList> childrenLists = null;
+            List<ChildrenList> childrenLists = new LinkedList<ChildrenList>();
             ChildrenList children = null;
-            GroupList groups = null;
+            GroupList groups = new GroupList();
             HistorySection section = null;
 
             // Move cursor before the first row in preparation
@@ -446,12 +445,6 @@ public class AwesomeBarTabs extends TabHost {
             while (cursor.moveToNext()) {
                 long time = cursor.getLong(cursor.getColumnIndexOrThrow(URLColumns.DATE_LAST_VISITED));
                 HistorySection itemSection = getSectionForTime(time, today);
-
-                if (groups == null)
-                    groups = new GroupList();
-
-                if (childrenLists == null)
-                    childrenLists = new LinkedList<ChildrenList>();
 
                 if (section != itemSection) {
                     if (section != null) {
@@ -476,11 +469,8 @@ public class AwesomeBarTabs extends TabHost {
             // Close the query cursor as we won't use it anymore
             cursor.close();
 
-            if (groups != null && childrenLists != null) {
-                result = Pair.<GroupList,List<ChildrenList>>create(groups, childrenLists);
-            }
-
-            return result;
+            // groups and childrenLists will be empty lists if there's no history
+            return Pair.<GroupList,List<ChildrenList>>create(groups, childrenLists);
         }
 
         public Map<String,Object> createHistoryItem(Cursor cursor) {
@@ -561,10 +551,6 @@ public class AwesomeBarTabs extends TabHost {
         }
 
         protected void onPostExecute(Pair<GroupList,List<ChildrenList>> result) {
-            // FIXME: display some sort of message when there's no history
-            if (result == null)
-                return;
-
             mHistoryAdapter = new HistoryListAdapter(
                 mContext,
                 result.first,
