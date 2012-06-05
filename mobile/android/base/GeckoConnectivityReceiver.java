54ca35ce5568c8a2b0fdc70826b67377a0137cb9
Mark Finkle <mfinkle@mozilla.com>
2012-06-05 13:44:35 -0400

    Bug 744850 - java.lang.IllegalArgumentException: Receiver not registered (ignore the crash) r=kats

--- mobile/android/base/GeckoConnectivityReceiver.java
+++ mobile/android/base/GeckoConnectivityReceiver.java
@@ -5,8 +5,6 @@
 
 package org.mozilla.gecko;
 
-import android.util.Log;
-
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -14,6 +12,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
 public class GeckoConnectivityReceiver extends BroadcastReceiver {
     /*
@@ -24,7 +23,7 @@ public class GeckoConnectivityReceiver extends BroadcastReceiver {
     private static final String LINK_DATA_DOWN = "down";
     private static final String LINK_DATA_UNKNOWN = "unknown";
 
-    private static final String LOGTAG = "GeckoConnectivityReciever";
+    private static final String LOGTAG = "GeckoConnectivityReceiver";
 
     private IntentFilter mFilter;
 
@@ -54,7 +53,7 @@ public class GeckoConnectivityReceiver extends BroadcastReceiver {
 
     public void registerFor(Activity activity) {
         if (!isRegistered) {
-            // registerReciever will return null if registering throws a RemoteException
+            // registerReciever will return null if registering fails
             isRegistered = activity.registerReceiver(this, mFilter) != null;
             if (!isRegistered)
                 Log.e(LOGTAG, "Registering receiver failed");
@@ -63,7 +62,11 @@ public class GeckoConnectivityReceiver extends BroadcastReceiver {
 
     public void unregisterFor(Activity activity) {
         if (isRegistered) {
-            activity.unregisterReceiver(this);
+            try {
+                activity.unregisterReceiver(this);
+            } catch (IllegalArgumentException iae) {
+                Log.e(LOGTAG, "Unregistering receiver failed", iae);
+            }
             isRegistered = false;
         }
     }
