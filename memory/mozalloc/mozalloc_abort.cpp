f8f041cbc9e5c3c3d19e143b4e637ea7cb805ba4
Jeff Walden <jwalden@mit.edu>
2012-06-05 16:49:30 -0700

    Bug 761859 - Make mozalloc_abort use MOZ_CRASH to crash.  r=ted

--- memory/mozalloc/mozalloc_abort.cpp
+++ memory/mozalloc/mozalloc_abort.cpp
@@ -5,76 +5,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/Assertions.h"
+
 #include <stdio.h>
-#include <stdlib.h>             // for abort()
-
-#if defined(_MSC_VER)           // MSVC
-#  include <intrin.h>           // for __debugbreak()
-#elif defined(XP_WIN)           // mingw
-#  include <windows.h>          // for DebugBreak
-#elif defined(XP_UNIX)
-#  include <unistd.h>           // for _exit
-#  include <signal.h>
-#endif
-
-#if defined(XP_WIN) || defined(XP_OS2)
-#  define MOZALLOC_EXPORT __declspec(dllexport)
-#endif
 
 #include "mozilla/mozalloc_abort.h"
-
-static int gDummyCounter;
-
-// Not inlining this function avoids the compiler making optimizations
-// that end up corrupting stack traces.
-MOZ_NEVER_INLINE static void
-TouchBadMemory()
-{
-    // XXX this should use the frame poisoning code
-    volatile int *p = 0;
-    gDummyCounter += *p;   // TODO annotation saying we know 
-                           // this is crazy
-}
 
 void
 mozalloc_abort(const char* const msg)
 {
     fputs(msg, stderr);
     fputs("\n", stderr);
-
-#if defined(_MSC_VER)
-    __debugbreak();
-#elif defined(XP_WIN)
-    DebugBreak();
-#endif
-
-    // On *NIX platforms the prefered way to abort is by touching bad memory,
-    // since this generates a stack trace inside our own code (avoiding
-    // problems with starting the trace inside libc, where we might not have
-    // symbols and can get lost).
-
-    TouchBadMemory();
-
-    // If we haven't aborted yet, we can try to raise SIGABRT which might work
-    // on some *NIXs, but not OS X (it doesn't trigger breakpad there).
-    // Note that we don't call abort(), since raise is likelier to give us
-    // useful stack data, and also since abort() is redirected to call this
-    // function (see below).
-#if defined(XP_UNIX) && !defined(XP_MACOSX)
-    raise(SIGABRT);
-#endif
-
-    // Still haven't aborted?  Try _exit().
-    _exit(127);
+    MOZ_CRASH();
 }
 
 #if defined(XP_UNIX)
 // Define abort() here, so that it is used instead of the system abort(). This
 // lets us control the behavior when aborting, in order to get better results
-// on *NIX platfrorms. See mozalloc_abort for details.
+// on *NIX platforms. See mozalloc_abort for details.
 void abort(void)
 {
-  mozalloc_abort("Redirecting call to abort() to mozalloc_abort\n");
+    mozalloc_abort("Redirecting call to abort() to mozalloc_abort\n");
 }
 #endif
 
