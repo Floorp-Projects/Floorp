6e0d7b61eebed8f18e7f544855c31c966e93631e
Jon Coppeard <jcoppeard@mozilla.com>
2012-06-05 11:47:42 -0700

    Bug 650161 - Update pointers to relocated objects r=terrence

--- js/src/jsinfer.cpp
+++ js/src/jsinfer.cpp
@@ -3440,12 +3440,13 @@ types::TypeMonitorCallSlow(JSContext *cx, JSObject *callee, const CallArgs &args
 }
 
 static inline bool
-IsAboutToBeFinalized(TypeObjectKey *key)
+IsAboutToBeFinalized(TypeObjectKey **keyp)
 {
     /* Mask out the low bit indicating whether this is a type or JS object. */
-    gc::Cell *tmp = reinterpret_cast<gc::Cell *>(uintptr_t(key) & ~1);
+    uintptr_t flagBit = uintptr_t(*keyp) & 1;
+    gc::Cell *tmp = reinterpret_cast<gc::Cell *>(uintptr_t(*keyp) & ~1);
     bool isAboutToBeFinalized = IsCellAboutToBeFinalized(&tmp);
-    JS_ASSERT(tmp == reinterpret_cast<gc::Cell *>(uintptr_t(key) & ~1));
+    *keyp = reinterpret_cast<TypeObjectKey *>(uintptr_t(tmp) | flagBit);
     return isAboutToBeFinalized;
 }
 
@@ -3943,7 +3944,7 @@ ConstraintTypeSet::sweep(Zone *zone, bool *oom)
         objectCount = 0;
         for (unsigned i = 0; i < oldCapacity; i++) {
             TypeObjectKey *object = oldArray[i];
-            if (object && !IsAboutToBeFinalized(object)) {
+            if (object && !IsAboutToBeFinalized(&object)) {
                 TypeObjectKey **pentry =
                     HashSetInsert<TypeObjectKey *,TypeObjectKey,TypeObjectKey>
                         (zone->types.typeLifoAlloc, objectSet, objectCount, object);
@@ -3961,9 +3962,11 @@ ConstraintTypeSet::sweep(Zone *zone, bool *oom)
         setBaseObjectCount(objectCount);
     } else if (objectCount == 1) {
         TypeObjectKey *object = (TypeObjectKey *) objectSet;
-        if (IsAboutToBeFinalized(object)) {
+        if (IsAboutToBeFinalized(&object)) {
             objectSet = nullptr;
             setBaseObjectCount(0);
+        } else {
+            objectSet = reinterpret_cast<TypeObjectKey **>(object);
         }
     }
 
@@ -4177,26 +4180,59 @@ TypeCompartment::sweep(FreeOp *fop)
 void
 JSCompartment::sweepNewTypeObjectTable(TypeObjectWithNewScriptSet &table)
 {
-    gcstats::AutoPhase ap(runtimeFromMainThread()->gc.stats,
-                          gcstats::PHASE_SWEEP_TABLES_TYPE_OBJECT);
-
-    JS_ASSERT(zone()->isGCSweeping());
+    JS_ASSERT(zone()->isCollecting());
     if (table.initialized()) {
         for (TypeObjectWithNewScriptSet::Enum e(table); !e.empty(); e.popFront()) {
             TypeObjectWithNewScriptEntry entry = e.front();
-            if (IsTypeObjectAboutToBeFinalized(entry.object.unsafeGet())) {
+            if (IsTypeObjectAboutToBeFinalized(entry.object.unsafeGet()) ||
+                (entry.newFunction && IsObjectAboutToBeFinalized(&entry.newFunction)))
+            {
                 e.removeFront();
-            } else if (entry.newFunction && IsObjectAboutToBeFinalized(&entry.newFunction)) {
-                e.removeFront();
-            } else if (entry.object.unbarrieredGet() != e.front().object.unbarrieredGet()) {
+            } else {
+                /* Any rekeying necessary is handled by fixupNewTypeObjectTable() below. */
+                JS_ASSERT(entry.object == e.front().object);
+                JS_ASSERT(entry.newFunction == e.front().newFunction);
+            }
+        }
+    }
+}
+
+#ifdef JSGC_COMPACTING
+void
+JSCompartment::fixupNewTypeObjectTable(TypeObjectWithNewScriptSet &table)
+{
+    /*
+     * Each entry's hash depends on the object's prototype and we can't tell
+     * whether that has been moved or not in sweepNewTypeObjectTable().
+     */
+    JS_ASSERT(zone()->isCollecting());
+    if (table.initialized()) {
+        for (TypeObjectWithNewScriptSet::Enum e(table); !e.empty(); e.popFront()) {
+            TypeObjectWithNewScriptEntry entry = e.front();
+            bool needRekey = false;
+            if (IsForwarded(entry.object.get())) {
+                entry.object.set(Forwarded(entry.object.get()));
+                needRekey = true;
+            }
+            TaggedProto proto = entry.object->proto();
+            if (proto.isObject() && IsForwarded(proto.toObject())) {
+                proto = TaggedProto(Forwarded(proto.toObject()));
+                needRekey = true;
+            }
+            if (entry.newFunction && IsForwarded(entry.newFunction)) {
+                entry.newFunction = Forwarded(entry.newFunction);
+                needRekey = true;
+            }
+            if (needRekey) {
                 TypeObjectWithNewScriptSet::Lookup lookup(entry.object->clasp(),
-                                                          entry.object->proto(),
+                                                          proto,
                                                           entry.newFunction);
                 e.rekeyFront(lookup, entry);
             }
         }
     }
 }
+#endif
 
 #ifdef JSGC_HASH_TABLE_CHECKS
 
@@ -4245,7 +4281,7 @@ TypeCompartment::~TypeCompartment()
 TypeScript::Sweep(FreeOp *fop, JSScript *script, bool *oom)
 {
     JSCompartment *compartment = script->compartment();
-    JS_ASSERT(compartment->zone()->isGCSweeping());
+    JS_ASSERT(compartment->zone()->isGCSweepingOrCompacting());
 
     unsigned num = NumTypeSets(script);
     StackTypeSet *typeArray = script->types->typeArray();
@@ -4324,7 +4360,7 @@ TypeZone::~TypeZone()
 void
 TypeZone::sweep(FreeOp *fop, bool releaseTypes, bool *oom)
 {
-    JS_ASSERT(zone()->isGCSweeping());
+    JS_ASSERT(zone()->isGCSweepingOrCompacting());
 
     JSRuntime *rt = fop->runtime();
 
