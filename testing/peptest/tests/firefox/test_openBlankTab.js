2bf0885bd20580a774ea5fb4a6ef57e52fbb05ea
Mark Cote <mcote@mozilla.com>
2012-06-06 11:31:12 -0400

    Bug 759000 Use accelKey instead of ctrlKey in peptest tests. r=ctalbert

--- testing/peptest/tests/firefox/test_openBlankTab.js
+++ testing/peptest/tests/firefox/test_openBlankTab.js
@@ -10,9 +10,9 @@ c.waitForPageLoad();
 
 // Only put things you want to test for responsiveness inside a perfom action call
 pep.performAction('open_blank_tab', function() {
-  c.rootElement.keypress('t', {'ctrlKey': true});
+  c.rootElement.keypress('t', {'accelKey': true});
 });
 
 pep.performAction('close_blank_tab', function() {
-  c.rootElement.keypress('w', {'ctrlKey': true});
+  c.rootElement.keypress('w', {'accelKey': true});
 });
