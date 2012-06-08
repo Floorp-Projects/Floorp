32d7daf83a8b08d9099890ae7ebb135febe95d8b
Hsinyi Tsai <htsai@mozilla.com>
2012-06-08 18:51:38 +0800

    Bug 759521 - WebTelephony: investigate test failures for outgoing calls. r=philikon

--- dom/telephony/test/marionette/test_outgoing_reject.js
+++ dom/telephony/test/marionette/test_outgoing_reject.js
@@ -34,10 +34,10 @@ function dial() {
   is(outgoing.number, number);
   is(outgoing.state, "dialing");
 
-  //is(outgoing, telephony.active); // bug 757587
-  //ok(telephony.calls === calls); // bug 757587
-  //is(calls.length, 1); // bug 757587
-  //is(calls[0], outgoing); // bug 757587
+  is(outgoing, telephony.active);
+  //ok(telephony.calls === calls); // bug 717414
+  is(telephony.calls.length, 1);
+  is(telephony.calls[0], outgoing);
 
   runEmulatorCmd("gsm list", function(result) {
     log("Call list is now: " + result);
@@ -57,7 +57,7 @@ function reject() {
     is(outgoing, event.call);
     is(outgoing.state, "disconnected");
 
-    //is(telephony.active, null);  // bug 757587
+    is(telephony.active, null);
     is(telephony.calls.length, 0);
 
     runEmulatorCmd("gsm list", function(result) {
@@ -66,8 +66,21 @@ function reject() {
       cleanUp();
     });
   };
-  runEmulatorCmd("gsm cancel " + number);
+  runEmulatorCmd("gsm list", cmdCallback);
 };
+
+function cmdCallback(result) {
+  let state = "outbound to  " + number + " : unknown";
+  log("Call list is now: " + result);
+
+  // The outgoing call cannot be canceled when call state is unknown.
+  // Wait until the call connection is established.
+  if (result[0] == state) {
+    runEmulatorCmd("gsm list", cmdCallback);
+  } else {
+    runEmulatorCmd("gsm cancel " + number);
+  }
+}
 
 function cleanUp() {
   SpecialPowers.clearUserPref(WHITELIST_PREF);
