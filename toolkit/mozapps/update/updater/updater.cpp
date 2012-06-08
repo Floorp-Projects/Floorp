4ef5db6e16bca8aaeb9cc9b1af93ab97b8e820bc
Ehsan Akhgari <ehsan@mozilla.com>
2012-06-08 15:06:53 -0400

    Bug 760577 - Part 4: Add full round-trip tests for keeping the mar file around and falling back to apply it at startup if replacing the application fails; r=rstrong
    --HG--
    rename : toolkit/mozapps/update/test/unit/test_0201_app_launch_apply_update.js => toolkit/mozapps/update/test/unit/test_0203_app_launch_apply_update.js
    rename : toolkit/mozapps/update/test_svc/unit/test_0201_app_launch_apply_update_svc.js => toolkit/mozapps/update/test_svc/unit/test_0203_app_launch_apply_update_svc.js

--- toolkit/mozapps/update/updater/updater.cpp
+++ toolkit/mozapps/update/updater/updater.cpp
@@ -2067,6 +2067,7 @@ UpdateThreadFunc(void *param)
 
       ensure_remove_recursive(stageDir);
       WriteStatusText(sUsingService ? "pending-service" : "pending");
+      putenv("MOZ_PROCESS_UPDATES="); // We need to use -process-updates again in the tests
       reportRealResults = false; // pretend success
     }
   }
