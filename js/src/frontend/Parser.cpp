17d6def4ee7a6aa7704c47f3a13e090b04a9e972
Nicholas Nethercote <nnethercote@mozilla.com>
2012-06-07 18:48:37 -0700

    Bug 761914 (part 3) - Do level check in UpvarCookie::set().  r=jorendorff.
    --HG--
    extra : rebase_source : 5df9e41cb94ae78f13d589bb9406ca101130515d

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -562,7 +562,8 @@ BindLocalVariable(JSContext *cx, SharedContext *sc, ParseNode *pn, BindingKind k
     if (!sc->bindings.add(cx, RootedAtom(cx, pn->pn_atom), kind))
         return false;
 
-    pn->pn_cookie.set(sc->staticLevel, index);
+    if (!pn->pn_cookie.set(cx, sc->staticLevel, index))
+        return false;
     pn->pn_dflags |= PND_BOUND;
     return true;
 }
@@ -930,7 +931,8 @@ js::DefineArg(ParseNode *pn, JSAtom *atom, unsigned i, Parser *parser)
     argsbody->append(argpn);
 
     argpn->setOp(JSOP_GETARG);
-    argpn->pn_cookie.set(parser->tc->sc->staticLevel, i);
+    if (!argpn->pn_cookie.set(parser->context, parser->tc->sc->staticLevel, i))
+        return false;
     argpn->pn_dflags |= PND_BOUND;
     return true;
 }
@@ -1083,8 +1085,7 @@ EnterFunction(SharedContext *outersc, SharedContext *funsc)
     funsc->blockidGen = outersc->blockidGen;
     if (!GenerateBlockId(funsc, funsc->bodyid))
         return false;
-    if (!SetStaticLevel(funsc, outersc->staticLevel + 1))
-        return false;
+    funsc->staticLevel = outersc->staticLevel + 1;
 
     return true;
 }
@@ -1144,7 +1145,9 @@ LeaveFunction(ParseNode *fn, Parser *parser, PropertyName *funName = NULL,
 
             if (atom == funName && kind == Expression) {
                 dn->setOp(JSOP_CALLEE);
-                dn->pn_cookie.set(funtc->sc->staticLevel, UpvarCookie::CALLEE_SLOT);
+                if (!dn->pn_cookie.set(parser->context, funtc->sc->staticLevel,
+                                       UpvarCookie::CALLEE_SLOT))
+                    return false;
                 dn->pn_dflags |= PND_BOUND;
                 foundCallee = 1;
                 continue;
@@ -1337,7 +1340,8 @@ Parser::functionArguments(ParseNode **listp, bool &hasRest)
                 if (!rhs)
                     return false;
                 rhs->setOp(JSOP_GETARG);
-                rhs->pn_cookie.set(tc->sc->staticLevel, slot);
+                if (!rhs->pn_cookie.set(context, tc->sc->staticLevel, slot))
+                    return false;
                 rhs->pn_dflags |= PND_BOUND;
                 rhs->setDefn(true);
 
@@ -1552,7 +1556,8 @@ Parser::functionDef(HandlePropertyName funName, FunctionType type, FunctionSynta
                 /* FALL THROUGH */
 
               case VARIABLE:
-                pn->pn_cookie.set(tc->sc->staticLevel, index);
+                if (!pn->pn_cookie.set(context, tc->sc->staticLevel, index))
+                    return NULL;
                 pn->pn_dflags |= PND_BOUND;
                 break;
 
@@ -2039,7 +2044,8 @@ BindLet(JSContext *cx, BindData *data, JSAtom *atom, Parser *parser)
      * again to include script->nfixed.
      */
     pn->setOp(JSOP_GETLOCAL);
-    pn->pn_cookie.set(tc->sc->staticLevel, uint16_t(blockCount));
+    if (!pn->pn_cookie.set(parser->context, tc->sc->staticLevel, uint16_t(blockCount)))
+        return false;
     pn->pn_dflags |= PND_LET | PND_BOUND;
 
     /*
@@ -5019,19 +5025,12 @@ GenexpGuard::maybeNoteGenerator(ParseNode *pn)
 static bool
 BumpStaticLevel(ParseNode *pn, TreeContext *tc)
 {
-    if (!pn->pn_cookie.isFree()) {
-        unsigned level = pn->pn_cookie.level() + 1;
+    if (pn->pn_cookie.isFree())
+        return true;
 
-        JS_ASSERT(level >= tc->sc->staticLevel);
-        if (level >= UpvarCookie::FREE_LEVEL) {
-            JS_ReportErrorNumber(tc->sc->context, js_GetErrorMessage, NULL,
-                                 JSMSG_TOO_DEEP, js_function_str);
-            return false;
-        }
-
-        pn->pn_cookie.set(level, pn->pn_cookie.slot());
-    }
-    return true;
+    unsigned level = unsigned(pn->pn_cookie.level()) + 1;
+    JS_ASSERT(level >= tc->sc->staticLevel);
+    return pn->pn_cookie.set(tc->sc->context, level, pn->pn_cookie.slot());
 }
 
 static void
