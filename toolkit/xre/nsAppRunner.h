fcb2602dd7b778cdf2b5dcd123c9beda9072760b
Benjamin Smedberg <benjamin@smedbergs.us>
2012-06-08 08:41:30 -0400

    Bug 755724 Part C - Move ScopedAppData into the XPCOM glue, r=glandium

--- toolkit/xre/nsAppRunner.h
+++ toolkit/xre/nsAppRunner.h
@@ -108,46 +108,6 @@ WriteStatusApplied(LPCWSTR updateDirPath);
 
 #define NS_NATIVEAPPSUPPORT_CONTRACTID "@mozilla.org/toolkit/native-app-support;1"
 
-// Like nsXREAppData, but releases all strong refs/allocated memory
-// in the destructor.
-class ScopedAppData : public nsXREAppData
-{
-public:
-  ScopedAppData() { Zero(); this->size = sizeof(*this); }
-
-  ScopedAppData(const nsXREAppData* aAppData);
-
-  void Zero() { memset(this, 0, sizeof(*this)); }
-
-  ~ScopedAppData();
-};
-
-/**
- * Given "str" is holding a string allocated with NS_Alloc, or null:
- * replace the value in "str" with a new value.
- *
- * @param newvalue Null is permitted. The string is cloned with
- *                 NS_strdup
- */
-void SetAllocatedString(const char *&str, const char *newvalue);
-
-/**
- * Given "str" is holding a string allocated with NS_Alloc, or null:
- * replace the value in "str" with a new value.
- *
- * @param newvalue If "newvalue" is the empty string, "str" will be set
- *                 to null.
- */
-void SetAllocatedString(const char *&str, const nsACString &newvalue);
-
-template<class T>
-void SetStrongPtr(T *&ptr, T* newvalue)
-{
-  NS_IF_RELEASE(ptr);
-  ptr = newvalue;
-  NS_IF_ADDREF(ptr);
-}
-
 namespace mozilla {
 namespace startup {
 extern GeckoProcessType sChildProcessType;
