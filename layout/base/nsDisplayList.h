dff7715e01223e4c93c7f93d659f2ee911b24c47
Timothy Nikkel <tnikkel@gmail.com>
2012-06-09 19:26:57 -0500

    Bug 747493. Give frameset documents a basic scroll layer so we can communicate the document's metrics to the compositor and frontend. r=roc,mats

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -2016,6 +2016,39 @@ public:
 };
 
 /**
+ * A display item that has no purpose but to ensure its contents get
+ * their own layer and that FrameMetrics are recorded, if the frame's
+ * document's root element has a displayport.
+ */
+class nsDisplaySimpleScrollLayer : public nsDisplayWrapList {
+public:
+  nsDisplaySimpleScrollLayer(nsDisplayListBuilder* aBuilder, nsIFrame* aFrame,
+                             nsDisplayList* aList);
+#ifdef NS_BUILD_REFCNT_LOGGING
+  virtual ~nsDisplaySimpleScrollLayer();
+#endif
+
+  virtual bool ComputeVisibility(nsDisplayListBuilder* aBuilder,
+                                 nsRegion* aVisibleRegion,
+                                 const nsRect& aAllowVisibleRegionExpansion);
+
+  virtual already_AddRefed<Layer> BuildLayer(nsDisplayListBuilder* aBuilder,
+                                             LayerManager* aManager,
+                                             const ContainerParameters& aContainerParameters);
+  virtual LayerState GetLayerState(nsDisplayListBuilder* aBuilder,
+                                   LayerManager* aManager,
+                                   const ContainerParameters& aParameters)
+  {
+    return mozilla::LAYER_ACTIVE;
+  }
+  virtual bool TryMerge(nsDisplayListBuilder* aBuilder, nsDisplayItem* aItem)
+  {
+    return false;
+  }
+  NS_DISPLAY_DECL_NAME("SimpleScrollLayer", TYPE_SIMPLE_SCROLL_LAYER)
+};
+
+/**
  * nsDisplayClip can clip a list of items, but we take a single item
  * initially and then later merge other items into it when we merge
  * adjacent matching nsDisplayClips
