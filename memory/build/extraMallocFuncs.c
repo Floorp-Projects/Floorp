e4c4bbeca2ec507bc9d7489cf0e888df27589757
Mike Hommey <mh+mozilla@glandium.org>
2012-06-11 08:12:40 +0200

    Bug 762446 - Hook jemalloc's abort. r=jlebar

--- memory/build/extraMallocFuncs.c
+++ memory/build/extraMallocFuncs.c
@@ -107,4 +107,10 @@ _je_malloc_message(void *cbopaque, const char *s)
 void (*je_malloc_message)(void *, const char *s) = _je_malloc_message;
 #endif
 
+#include <mozilla/Assertions.h>
+
+void moz_abort() {
+  MOZ_CRASH();
+}
+
 #endif /* MOZ_JEMALLOC */
