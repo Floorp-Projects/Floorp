275a432416683cd1fd841cded3bfb23a5726ad88
Matt Woodrow <mwoodrow@mozilla.com>
2012-06-11 16:45:39 +1200

    Bug 539356 - Part 19 - Only repaint retained layers after the previous repainted has been drawn to the window. r=roc

--- view/src/nsViewManager.cpp
+++ view/src/nsViewManager.cpp
@@ -303,6 +303,11 @@ void nsViewManager::Refresh(nsView *aView, const nsIntRegion& aRegion,
                             bool aWillSendDidPaint)
 {
   NS_ASSERTION(aView->GetViewManager() == this, "wrong view manager");
+  aView->SetPendingRefresh(false);
+  if (aView->NeedsToTriggerPaintAfterRefresh()) {
+    RootViewManager()->GetPresShell()->ScheduleViewManagerFlush(true);
+    aView->SetNeedsToTriggerPaintAfterRefresh(false);
+  }
 
   // damageRegion is the damaged area, in twips, relative to the view origin
   nsRegion damageRegion = aRegion.ToAppUnits(AppUnitsPerDevPixel());
@@ -354,14 +359,14 @@ void nsViewManager::Refresh(nsView *aView, const nsIntRegion& aRegion,
   }
 }
 
-void nsViewManager::ProcessPendingUpdatesForView(nsView* aView,
+bool nsViewManager::ProcessPendingUpdatesForView(nsView* aView,
                                                  bool aFlushDirtyRegion)
 {
   NS_ASSERTION(IsRootVM(), "Updates will be missed");
 
   // Protect against a null-view.
   if (!aView) {
-    return;
+    return false;
   }
 
   if (aView->HasWidget()) {
@@ -369,9 +374,10 @@ void nsViewManager::ProcessPendingUpdatesForView(nsView* aView,
   }
 
   // process pending updates in child view.
+  bool skipped = false;
   for (nsView* childView = aView->GetFirstChild(); childView;
        childView = childView->GetNextSibling()) {
-    ProcessPendingUpdatesForView(childView, aFlushDirtyRegion);
+    skipped |= ProcessPendingUpdatesForView(childView, aFlushDirtyRegion);
   }
 
   // Push out updates after we've processed the children; ensures that
@@ -379,17 +385,27 @@ void nsViewManager::ProcessPendingUpdatesForView(nsView* aView,
   if (aFlushDirtyRegion) {
     nsIWidget *widget = aView->GetWidget();
     if (widget && widget->NeedsPaint()) {
+      if (aView->PendingRefresh() && !aView->NeedsToTriggerPaintAfterRefresh()) {
+        aView->SetNeedsToTriggerPaintAfterRefresh(true);
+        skipped = true;
+      } else {
+        aView->SetNeedsToTriggerPaintAfterRefresh(false);
+        aView->SetPendingRefresh(false);
+        SetPainting(true);
 #ifdef DEBUG_INVALIDATIONS
-      printf("---- PAINT START ----PresShell(%p), nsView(%p), nsIWidget(%p)\n", mPresShell, aView, widget);
+        printf("---- PAINT START ----PresShell(%p), nsView(%p), nsIWidget(%p)\n", mPresShell, aView, widget);
 #endif
-      nsAutoScriptBlocker scriptBlocker;
-      mPresShell->Paint(aView, nsRegion(), nsIPresShell::PaintType_NoComposite, false);
+        nsAutoScriptBlocker scriptBlocker;
+        mPresShell->Paint(aView, nsRegion(), nsIPresShell::PaintType_NoComposite, false);
 #ifdef DEBUG_INVALIDATIONS
-      printf("---- PAINT END ----\n");
+        printf("---- PAINT END ----\n");
 #endif
+        SetPainting(false);
+      }
     }
     FlushDirtyRegionToWidget(aView);
   }
+  return skipped;
 }
 
 void nsViewManager::FlushDirtyRegionToWidget(nsView* aView)
@@ -432,7 +448,7 @@ nsViewManager::PostPendingUpdate()
 {
   nsViewManager* rootVM = RootViewManager();
   rootVM->mHasPendingWidgetGeometryChanges = true;
-  if (rootVM->mPresShell) {
+  if (rootVM->mPresShell && !rootVM->IsPainting()) {
     rootVM->mPresShell->ScheduleViewManagerFlush(nsIFrame::PAINT_COMPOSITE_ONLY);
   }
 }
@@ -515,6 +531,7 @@ nsViewManager::InvalidateWidgetArea(nsView *aWidgetView,
   leftOver.Sub(aDamagedRegion, children);
 
   if (!leftOver.IsEmpty()) {
+    aWidgetView->SetPendingRefresh(true);
     const nsRect* r;
     for (nsRegionRectIterator iter(leftOver); (r = iter.Next());) {
       nsIntRect bounds = ViewToWidget(aWidgetView, *r);
@@ -1301,16 +1318,15 @@ nsViewManager::IsPainting(bool& aIsPainting)
   return NS_OK;
 }
 
-void
+bool
 nsViewManager::ProcessPendingUpdates()
 {
   if (!IsRootVM()) {
-    RootViewManager()->ProcessPendingUpdates();
-    return;
+    return RootViewManager()->ProcessPendingUpdates();
   }
 
   mPresShell->GetPresContext()->RefreshDriver()->RevokeViewManagerFlush();
-  ProcessPendingUpdatesForView(mRootView, true);
+  return ProcessPendingUpdatesForView(mRootView, true);
 }
 
 void
