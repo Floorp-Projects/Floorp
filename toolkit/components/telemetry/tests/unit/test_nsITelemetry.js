d01f4b6895553a6c19c19509e07b382d88a99651
Nathan Froyd <froydnj@mozilla.com>
2012-06-11 14:35:35 -0400

    Bug 763525 - delete C++ persistent telemetry implementation; r=taras

--- toolkit/components/telemetry/tests/unit/test_nsITelemetry.js
+++ toolkit/components/telemetry/tests/unit/test_nsITelemetry.js
@@ -286,39 +286,6 @@ function generateUUID() {
   return str.substring(1, str.length - 1);
 }
 
-// Check that we do sane things when saving to disk.
-function test_loadSave()
-{
-  let dirService = Cc["@mozilla.org/file/directory_service;1"]
-                    .getService(Ci.nsIProperties);
-  let tmpDir = dirService.get("TmpD", Ci.nsILocalFile);
-  let tmpFile = tmpDir.clone();
-  tmpFile.append("saved-histograms.dat");
-  if (tmpFile.exists()) {
-    tmpFile.remove(true);
-  }
-
-  let saveFinished = false;
-  let loadFinished = false;
-  let uuid = generateUUID();
-  let loadCallback = function(data) {
-    do_check_true(data != null);
-    do_check_eq(uuid, data.uuid);
-    loadFinished = true;
-    do_test_finished();
-  };
-  let saveCallback = function(success) {
-    do_check_true(success);
-    Telemetry.loadHistograms(tmpFile, loadCallback, false);
-    saveFinished = true;
-  };
-  do_test_pending();
-  Telemetry.saveHistograms(tmpFile, uuid, saveCallback, false);
-  do_register_cleanup(function () do_check_true(saveFinished));
-  do_register_cleanup(function () do_check_true(loadFinished));
-  do_register_cleanup(function () tmpFile.remove(true));
-}
-
 function run_test()
 {
   let kinds = [Telemetry.HISTOGRAM_EXPONENTIAL, Telemetry.HISTOGRAM_LINEAR]
@@ -342,5 +309,4 @@ function run_test()
   test_getSlowSQL();
   test_privateMode();
   test_addons();
-  test_loadSave();
 }
