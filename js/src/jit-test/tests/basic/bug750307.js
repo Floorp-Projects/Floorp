530c20d3630ec7e76035f6bc4801ad68ea8350f9
Phil Ringnalda <philringnalda@gmail.com>
2012-06-11 17:02:21 -0700

    Back out 7166a68a7994 (bug 750307) for browser_bug645699.js bustage in a CLOSED TREE

--- js/src/jit-test/tests/basic/bug750307.js
+++
@@ -1,6 +0,0 @@
-load(libdir + "asserts.js");
-var g = newGlobal('new-compartment');
-var a = g.RegExp("x");
-assertThrowsInstanceOf(function () { Object.defineProperty(a, "ignoreCase", {value: undefined}); },
-                       g.TypeError);
-a.toString();
