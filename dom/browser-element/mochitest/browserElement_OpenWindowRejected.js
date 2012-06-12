df71560906fc3e6cc61a4de82a3f3a70be2be916
Justin Lebar <justin.lebar@gmail.com>
2012-06-12 18:01:25 -0400

    Bug 742944 - Part 5: Tests for window.open in <iframe mozbrowser>. r=bz

---
+++ dom/browser-element/mochitest/browserElement_OpenWindowRejected.js
@@ -0,0 +1,44 @@
+/* Any copyright is dedicated to the public domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Bug 742944 - Do window.open from inside <iframe mozbrowser>.  But then
+// reject the call.  This shouldn't cause problems (crashes, leaks).
+
+"use strict";
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addToWhitelist();
+
+  var iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+
+  iframe.addEventListener('mozbrowseropenwindow', function(e) {
+    ok(e.detail.url.indexOf('does_not_exist.html') != -1,
+       'Opened URL; got ' + e.detail.url);
+    is(e.detail.name, '');
+    is(e.detail.features, '');
+
+    // Don't add e.detail.frameElement to the DOM, so the window.open is
+    // effectively blocked.
+  });
+
+  iframe.addEventListener('mozbrowsershowmodalprompt', function(e) {
+    var msg = e.detail.message;
+    if (msg.indexOf('success:') == 0) {
+      ok(true, msg);
+    }
+    else if (msg == 'finish') {
+      SimpleTest.finish();
+    }
+    else {
+      ok(false, msg);
+    }
+  });
+
+  iframe.src = 'file_browserElement_OpenWindowRejected.html';
+  document.body.appendChild(iframe);
+}
+
+runTest();
