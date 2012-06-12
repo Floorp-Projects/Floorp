f574fbb6a6ce759c94bfb6e067ba7317d28c7593
Marshall Culpepper <marshall@mozilla.com>
2012-06-12 17:05:50 -0400

    Bug 761482 - Rename MobileOperatorInfo to MobileNetworkInfo for consistency and update MobileConnectionInfo.network to use the new MobileNetworkInfo type. r=philikon sr=sicking

--- dom/network/tests/marionette/test_mobile_networks.js
+++ dom/network/tests/marionette/test_mobile_networks.js
@@ -9,44 +9,70 @@ const WHITELIST_PREF = "dom.mobileconnection.whitelist";
 let uriPrePath = window.location.protocol + "//" + window.location.host;
 SpecialPowers.setCharPref(WHITELIST_PREF, uriPrePath);
 
-ok(navigator.mozMobileConnection instanceof MozMobileConnection,
-   "mozMobileConnection is instanceof " + navigator.mozMobileConnection.constructor);
+let connection = navigator.mozMobileConnection;
+ok(connection instanceof MozMobileConnection,
+   "connection is instanceof " + connection.constructor);
 
-let request = navigator.mozMobileConnection.getNetworks();
-ok(request instanceof DOMRequest,
-   "request is instanceof " + request.constructor);
+function isAndroidNetwork(network) {
+  is(network.longName, "Android");
+  is(network.shortName, "Android");
+  is(network.mcc, 310);
+  is(network.mnc, 260);
+}
 
-request.onerror = function() {
-  ok(false, request.error);
-  cleanUp();
-};
+function testConnectionInfo() {
+  let voice = connection.voice;
+  is(voice.connected, true);
+  is(voice.emergencyCallsOnly, false);
+  is(voice.roaming, false);
+  isAndroidNetwork(voice.network);
 
-request.onsuccess = function() {
-  ok('result' in request, "Request did not contain a result");
-  let networks = request.result;
+  let data = connection.data;
+  // TODO Bug 762959: enable these checks when data state updates have been implemented
+  // is(data.connected, true);
+  // is(data.emergencyCallsOnly, false);
+  // is(data.roaming, false);
+  isAndroidNetwork(data.network);
 
-  // The emulator RIL server should always return 2 networks:
-  // {"longName":"Android","shortName":"Android","mcc":310,"mnc":260,"state":"available"}
-  // {"longName":"TelKila","shortName":"TelKila","mcc":310,"mnc":295,"state":"available"}
-  is(networks.length, 2);
+  testGetNetworks();
+}
 
-  let network1 = networks[0];
-  is(network1.longName, "Android");
-  is(network1.shortName, "Android");
-  is(network1.mcc, 310);
-  is(network1.mnc, 260);
-  is(network1.state, "available");
+function testGetNetworks() {
+  let request = connection.getNetworks();
+  ok(request instanceof DOMRequest,
+      "request is instanceof " + request.constructor);
 
-  let network2 = networks[1];
-  is(network2.longName, "TelKila");
-  is(network2.shortName, "TelKila");
-  is(network2.mcc, 310);
-  is(network2.mnc, 295);
-  is(network2.state, "available");
-  cleanUp();
-};
+  request.onerror = function() {
+    ok(false, request.error);
+    cleanUp();
+  };
+
+  request.onsuccess = function() {
+    ok('result' in request, "Request did not contain a result");
+    let networks = request.result;
+
+    // The emulator RIL server should always return 2 networks:
+    // {"longName":"Android","shortName":"Android","mcc":310,"mnc":260,"state":"available"}
+    // {"longName":"TelKila","shortName":"TelKila","mcc":310,"mnc":295,"state":"available"}
+    is(networks.length, 2);
+
+    let network1 = networks[0];
+    isAndroidNetwork(network1);
+    is(network1.state, "available");
+
+    let network2 = networks[1];
+    is(network2.longName, "TelKila");
+    is(network2.shortName, "TelKila");
+    is(network2.mcc, 310);
+    is(network2.mnc, 295);
+    is(network2.state, "available");
+    cleanUp();
+  };
+}
 
 function cleanUp() {
   SpecialPowers.clearUserPref(WHITELIST_PREF);
   finish();
 }
+
+testConnectionInfo();
