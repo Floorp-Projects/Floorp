7fde138bc2ac6f26f6ebe3020278473d71913441
Justin Lebar <justin.lebar@gmail.com>
2012-06-12 18:01:25 -0400

    Bug 742944 - Part 5: Tests for window.open in <iframe mozbrowser>. r=bz
    --HG--
    extra : rebase_source : 8419648607fbf9f7acd672cb0f94ce7a05a32d88

---
+++ dom/browser-element/mochitest/browserElement_OpenWindowRejected2.js
@@ -0,0 +1,48 @@
+/* Any copyright is dedicated to the public domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Bug 742944 - Do window.open from inside <iframe mozbrowser>.  But then
+// reject the call.  This shouldn't cause problems (crashes, leaks).
+//
+// This is the same as OpenWindowRejected, except we "reject" the popup by not
+// adding the iframe element to our DOM, instead of by not calling
+// preventDefault() on the event.
+
+"use strict";
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addToWhitelist();
+
+  var iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+
+  iframe.addEventListener('mozbrowseropenwindow', function(e) {
+    ok(e.detail.url.indexOf('does_not_exist.html') != -1,
+       'Opened URL; got ' + e.detail.url);
+    is(e.detail.name, '');
+    is(e.detail.features, '');
+
+    // Call preventDefault, but don't add the iframe to the DOM.  This still
+    // amounts to rejecting the popup.
+    e.preventDefault();
+  });
+
+  iframe.addEventListener('mozbrowsershowmodalprompt', function(e) {
+    var msg = e.detail.message;
+    if (msg.indexOf('success:') == 0) {
+      ok(true, msg);
+    }
+    else if (msg == 'finish') {
+      SimpleTest.finish();
+    }
+    else {
+      ok(false, msg);
+    }
+  });
+
+  iframe.src = 'file_browserElement_OpenWindowRejected.html';
+}
+
+runTest();
