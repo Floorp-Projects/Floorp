add06953b909be0424cdc341e5eb68465ef5bb09
Ehsan Akhgari <ehsan@mozilla.com>
2012-06-12 23:35:10 -0400

    Bug 758992 - Make the classes which use the XPCOM nsISupports implementation macros final, to avoid the warning about deleting using a pointer to a base class with virtual functions and no virtual dtor (storage parts); r=mak

--- storage/src/mozStorageStatementRow.h
+++ storage/src/mozStorageStatementRow.h
@@ -9,14 +9,15 @@
 
 #include "mozIStorageStatementRow.h"
 #include "nsIXPCScriptable.h"
+#include "mozilla/Attributes.h"
 
 namespace mozilla {
 namespace storage {
 
 class Statement;
 
-class StatementRow : public mozIStorageStatementRow
-                   , public nsIXPCScriptable
+class StatementRow MOZ_FINAL : public mozIStorageStatementRow
+                             , public nsIXPCScriptable
 {
 public:
   NS_DECL_ISUPPORTS
