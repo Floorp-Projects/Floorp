ba10340a7c3ca38d1245cf642ccd94cd9e8e9b79
Ehsan Akhgari <ehsan@mozilla.com>
2012-06-12 23:13:31 -0400

    Bug 758992 - Make the classes which use the XPCOM nsISupports implementation macros final, to avoid the warning about deleting using a pointer to a base class with virtual functions and no virtual dtor (extensions/auth/ parts); r=bsmedberg
    --HG--
    extra : rebase_source : bf2a9c8de523d78eac780da7d7d755be47b97114

--- extensions/auth/nsHttpNegotiateAuth.h
+++ extensions/auth/nsHttpNegotiateAuth.h
@@ -9,11 +9,12 @@
 #include "nsIHttpAuthenticator.h"
 #include "nsIURI.h"
 #include "nsSubstring.h"
+#include "mozilla/Attributes.h"
 
 // The nsHttpNegotiateAuth class provides responses for the GSS-API Negotiate method
 // as specified by Microsoft in draft-brezak-spnego-http-04.txt
 
-class nsHttpNegotiateAuth : public nsIHttpAuthenticator
+class nsHttpNegotiateAuth MOZ_FINAL : public nsIHttpAuthenticator
 {
 public:
     NS_DECL_ISUPPORTS
