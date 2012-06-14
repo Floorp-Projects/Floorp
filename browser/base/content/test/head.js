6be9decac53219029bb10fc61552e723c03f18d9
David Keeler <dkeeler@mozilla.com>
2012-06-14 16:52:49 -0700

    Bug 764421 - poll with setInterval instead of using setTimeout in browser_bug743421.js; r=jaws

--- browser/base/content/test/head.js
+++ browser/base/content/test/head.js
@@ -72,3 +72,18 @@ function closeToolbarCustomizationUI(aCallback, aBrowserWin) {
   button.focus();
   button.doCommand();
 }
+
+function waitForCondition(condition, nextTest, errorMsg) {
+  var tries = 0;
+  var interval = setInterval(function() {
+    if (tries >= 30) {
+      ok(false, errorMsg);
+      moveOn();
+    }
+    if (condition()) {
+      moveOn();
+    }
+    tries++;
+  }, 100);
+  var moveOn = function() { clearInterval(interval); nextTest(); };
+}
