ddb3e40159e921819816974450f892af24d4b757
Henri Sivonen <hsivonen@iki.fi>
2012-06-14 09:14:47 +0300

    Bug 750096. r=bzbarsky.

--- parser/html/nsParserUtils.cpp
+++ parser/html/nsParserUtils.cpp
@@ -149,6 +149,8 @@ nsParserUtils::ParseFragment(const nsAString& aFragment,
   document = do_QueryInterface(domDocument);
   NS_ENSURE_TRUE(document, NS_ERROR_NOT_AVAILABLE);
 
+  nsAutoScriptBlockerSuppressNodeRemoved autoBlocker;
+
   // stop scripts
   nsRefPtr<nsScriptLoader> loader;
   bool scripts_enabled = false;
