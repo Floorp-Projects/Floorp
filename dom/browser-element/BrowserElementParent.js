384401194dbb53b790fcb71d33f681bbda188ec2
Justin Lebar <justin.lebar@gmail.com>
2012-06-15 11:01:44 -0400

    Bug 764248 - Create one BrowserElementParent object for each <iframe mozbrowser>. r=vingtetun

--- dom/browser-element/BrowserElementParent.js
+++ dom/browser-element/BrowserElementParent.js
@@ -18,25 +18,24 @@ function debug(msg) {
   //dump("BrowserElementParent - " + msg + "\n");
 }
 
-function sendAsyncMsg(frameElement, msg, data) {
-  let mm = frameElement.QueryInterface(Ci.nsIFrameLoaderOwner)
-                       .frameLoader
-                       .messageManager;
-
-  mm.sendAsyncMessage('browser-element-api:' + msg, data);
-}
-
 /**
- * The BrowserElementParent implements one half of <iframe mozbrowser>.
- * (The other half is, unsurprisingly, BrowserElementChild.)
+ * BrowserElementParent implements one half of <iframe mozbrowser>.  (The other
+ * half is, unsurprisingly, BrowserElementChild.)
  *
- * We detect windows and docshells contained inside <iframe mozbrowser>s and
- * inject script to listen for certain events in the child.  We then listen to
- * messages from the child script and take appropriate action here.
+ * BrowserElementParentFactory detects when we create a windows or docshell
+ * contained inside a <iframe mozbrowser> and creates a BrowserElementParent
+ * object for that window.
+ *
+ * BrowserElementParent injects script to listen for certain events in the
+ * child.  We then listen to messages from the child script and take
+ * appropriate action here in the parent.
  */
 
-function BrowserElementParent() {}
-BrowserElementParent.prototype = {
+function BrowserElementParentFactory() {
+  this._initialized = false;
+}
+
+BrowserElementParentFactory.prototype = {
   classID: Components.ID("{ddeafdac-cb39-47c4-9cb8-c9027ee36d26}"),
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver,
                                          Ci.nsISupportsWeakReference]),
@@ -61,8 +60,10 @@ BrowserElementParent.prototype = {
     debug("_init");
     this._initialized = true;
 
-    this._screenshotListeners = {};
-    this._screenshotReqCounter = 0;
+    // Maps frame elements to BrowserElementParent objects.  We never look up
+    // anything in this map; the purpose is to keep the BrowserElementParent
+    // alive for as long as its frame element lives.
+    this._bepMap = new WeakMap();
 
     var os = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
     os.addObserver(this, 'remote-browser-frame-shown', /* ownsWeak = */ true);
@@ -81,175 +82,17 @@ BrowserElementParent.prototype = {
 
   _observeInProcessBrowserFrameShown: function(frameLoader) {
     debug("In-process browser frame shown " + frameLoader);
-    this._setUpMessageManagerListeners(frameLoader);
+    this._createBrowserElementParent(frameLoader);
   },
 
   _observeRemoteBrowserFrameShown: function(frameLoader) {
     debug("Remote browser frame shown " + frameLoader);
-    this._setUpMessageManagerListeners(frameLoader);
+    this._createBrowserElementParent(frameLoader);
   },
 
-  _setUpMessageManagerListeners: function(frameLoader) {
+  _createBrowserElementParent: function(frameLoader) {
     let frameElement = frameLoader.QueryInterface(Ci.nsIFrameLoader).ownerElement;
-    if (!frameElement) {
-      debug("No frame element?");
-      return;
-    }
-
-    let mm = frameLoader.messageManager;
-
-    // Messages we receive are handled by functions with parameters
-    // (frameElement, data), where |data| is the message manager's data object.
-
-    let self = this;
-    function addMessageListener(msg, handler) {
-      mm.addMessageListener('browser-element-api:' + msg, handler.bind(self, frameElement));
-    }
-
-    addMessageListener("hello", this._recvHello);
-    addMessageListener("locationchange", this._fireEventFromMsg);
-    addMessageListener("loadstart", this._fireEventFromMsg);
-    addMessageListener("loadend", this._fireEventFromMsg);
-    addMessageListener("titlechange", this._fireEventFromMsg);
-    addMessageListener("iconchange", this._fireEventFromMsg);
-    addMessageListener("close", this._fireEventFromMsg);
-    addMessageListener("get-mozapp-manifest-url", this._sendMozAppManifestURL);
-    addMessageListener("keyevent", this._fireKeyEvent);
-    addMessageListener("showmodalprompt", this._handleShowModalPrompt);
-    mm.addMessageListener('browser-element-api:got-screenshot',
-                          this._recvGotScreenshot.bind(this));
-
-    XPCNativeWrapper.unwrap(frameElement).getScreenshot =
-      this._getScreenshot.bind(this, mm, frameElement);
-
-    XPCNativeWrapper.unwrap(frameElement).setVisible =
-      this._setVisible.bind(this, mm, frameElement);
-
-    mm.loadFrameScript("chrome://global/content/BrowserElementChild.js",
-                       /* allowDelayedLoad = */ true);
-  },
-
-  _recvHello: function(frameElement, data) {
-    debug("recvHello " + frameElement);
-  },
-
-  /**
-   * Fire either a vanilla or a custom event, depending on the contents of
-   * |data|.
-   */
-  _fireEventFromMsg: function(frameElement, data) {
-    let name = data.name.substring('browser-element-api:'.length);
-    let detail = data.json;
-
-    debug('fireEventFromMsg: ' + name + ', ' + detail);
-    let evt = this._createEvent(frameElement, name, detail,
-                                /* cancelable = */ false);
-    frameElement.dispatchEvent(evt);
-  },
-
-  _handleShowModalPrompt: function(frameElement, data) {
-    // Fire a showmodalprmopt event on the iframe.  When this method is called,
-    // the child is spinning in a nested event loop waiting for an
-    // unblock-modal-prompt message.
-    //
-    // If the embedder calls preventDefault() on the showmodalprompt event,
-    // we'll block the child until event.detail.unblock() is called.
-    //
-    // Otherwise, if preventDefault() is not called, we'll send the
-    // unblock-modal-prompt message to the child as soon as the event is done
-    // dispatching.
-
-    let detail = data.json;
-    debug('handleShowPrompt ' + JSON.stringify(detail));
-
-    // Strip off the windowID property from the object we send along in the
-    // event.
-    let windowID = detail.windowID;
-    delete detail.windowID;
-    debug("Event will have detail: " + JSON.stringify(detail));
-    let evt = this._createEvent(frameElement, 'showmodalprompt', detail,
-                                /* cancelable = */ true);
-
-    let unblockMsgSent = false;
-    function sendUnblockMsg() {
-      if (unblockMsgSent) {
-        return;
-      }
-      unblockMsgSent = true;
-
-      // We don't need to sanitize evt.detail.returnValue (e.g. converting the
-      // return value of confirm() to a boolean); Gecko does that for us.
-
-      let data = { windowID: windowID,
-                   returnValue: evt.detail.returnValue };
-      sendAsyncMsg(frameElement, 'unblock-modal-prompt', data);
-    }
-
-    XPCNativeWrapper.unwrap(evt.detail).unblock = function() {
-      sendUnblockMsg();
-    };
-
-    frameElement.dispatchEvent(evt);
-
-    if (!evt.defaultPrevented) {
-      // Unblock the inner frame immediately.  Otherwise we'll unblock upon
-      // evt.detail.unblock().
-      sendUnblockMsg();
-    }
-  },
-
-  _createEvent: function(frameElement, evtName, detail, cancelable) {
-    let win = frameElement.ownerDocument.defaultView;
-    let evt;
-
-    // This will have to change if we ever want to send a CustomEvent with null
-    // detail.  For now, it's OK.
-    if (detail !== undefined && detail !== null) {
-      evt = new win.CustomEvent('mozbrowser' + evtName,
-                                {bubbles: true, cancelable: cancelable,
-                                 detail: detail});
-    }
-    else {
-      evt = new win.Event('mozbrowser' + evtName,
-                          {bubbles: true, cancelable: cancelable});
-    }
-
-    return evt;
-  },
-
-  _sendMozAppManifestURL: function(frameElement, data) {
-    return frameElement.getAttribute('mozapp');
-  },
-
-  _recvGotScreenshot: function(data) {
-    var req = this._screenshotListeners[data.json.id];
-    delete this._screenshotListeners[data.json.id];
-    Services.DOMRequest.fireSuccess(req, data.json.screenshot);
-  },
-
-  _getScreenshot: function(mm, frameElement) {
-    let id = 'req_' + this._screenshotReqCounter++;
-    let req = Services.DOMRequest
-      .createRequest(frameElement.ownerDocument.defaultView);
-    this._screenshotListeners[id] = req;
-    mm.sendAsyncMessage('browser-element-api:get-screenshot', {id: id});
-    return req;
-  },
-
-   _setVisible: function(mm, frameElement, visible) {
-     mm.sendAsyncMessage('browser-element-api:set-visible', {visible: visible});
-   },
-
-  _fireKeyEvent: function(frameElement, data) {
-    let win = frameElement.ownerDocument.defaultView;
-    let evt = frameElement.ownerDocument.createEvent("KeyboardEvent");
-
-    evt.initKeyEvent(data.json.type, true, true, win,
-                     false, false, false, false, // modifiers
-                     data.json.keyCode,
-                     data.json.charCode);
-
-    frameElement.dispatchEvent(evt);
+    this._bepMap.set(frameElement, new BrowserElementParent(frameLoader));
   },
 
   observe: function(subject, topic, data) {
@@ -275,4 +118,180 @@ BrowserElementParent.prototype = {
   },
 };
 
-var NSGetFactory = XPCOMUtils.generateNSGetFactory([BrowserElementParent]);
+function BrowserElementParent(frameLoader) {
+  debug("Creating new BrowserElementParent object for " + frameLoader);
+  this._screenshotListeners = {};
+  this._screenshotReqCounter = 0;
+
+  this._frameElement = frameLoader.QueryInterface(Ci.nsIFrameLoader).ownerElement;
+  if (!this._frameElement) {
+    debug("No frame element?");
+    return;
+  }
+
+  this._mm = frameLoader.messageManager;
+
+  // Messages we receive are handed to functions which take a (data) argument,
+  // where |data| is the message manager's data object.
+
+  let self = this;
+  function addMessageListener(msg, handler) {
+    self._mm.addMessageListener('browser-element-api:' + msg, handler.bind(self));
+  }
+
+  addMessageListener("hello", this._recvHello);
+  addMessageListener("locationchange", this._fireEventFromMsg);
+  addMessageListener("loadstart", this._fireEventFromMsg);
+  addMessageListener("loadend", this._fireEventFromMsg);
+  addMessageListener("titlechange", this._fireEventFromMsg);
+  addMessageListener("iconchange", this._fireEventFromMsg);
+  addMessageListener("close", this._fireEventFromMsg);
+  addMessageListener("get-mozapp-manifest-url", this._sendMozAppManifestURL);
+  addMessageListener("keyevent", this._fireKeyEvent);
+  addMessageListener("showmodalprompt", this._handleShowModalPrompt);
+  addMessageListener('got-screenshot', this._recvGotScreenshot);
+
+  function defineMethod(name, fn) {
+    XPCNativeWrapper.unwrap(self._frameElement)[name] = fn.bind(self);
+  }
+
+  // Define methods on the frame element.
+  defineMethod('getScreenshot', this._getScreenshot);
+  defineMethod('setVisible', this._setVisible);
+
+  self._mm.loadFrameScript("chrome://global/content/BrowserElementChild.js",
+                           /* allowDelayedLoad = */ true);
+}
+
+BrowserElementParent.prototype = {
+  get _window() {
+    return this._frameElement.ownerDocument.defaultView;
+  },
+
+  _sendAsyncMsg: function(msg, data) {
+    this._frameElement.QueryInterface(Ci.nsIFrameLoaderOwner)
+                      .frameLoader
+                      .messageManager
+                      .sendAsyncMessage('browser-element-api:' + msg, data);
+  },
+
+  _recvHello: function(data) {
+    debug("recvHello");
+  },
+
+  /**
+   * Fire either a vanilla or a custom event, depending on the contents of
+   * |data|.
+   */
+  _fireEventFromMsg: function(data) {
+    let name = data.name.substring('browser-element-api:'.length);
+    let detail = data.json;
+
+    debug('fireEventFromMsg: ' + name + ', ' + detail);
+    let evt = this._createEvent(name, detail,
+                                /* cancelable = */ false);
+    this._frameElement.dispatchEvent(evt);
+  },
+
+  _handleShowModalPrompt: function(data) {
+    // Fire a showmodalprmopt event on the iframe.  When this method is called,
+    // the child is spinning in a nested event loop waiting for an
+    // unblock-modal-prompt message.
+    //
+    // If the embedder calls preventDefault() on the showmodalprompt event,
+    // we'll block the child until event.detail.unblock() is called.
+    //
+    // Otherwise, if preventDefault() is not called, we'll send the
+    // unblock-modal-prompt message to the child as soon as the event is done
+    // dispatching.
+
+    let detail = data.json;
+    debug('handleShowPrompt ' + JSON.stringify(detail));
+
+    // Strip off the windowID property from the object we send along in the
+    // event.
+    let windowID = detail.windowID;
+    delete detail.windowID;
+    debug("Event will have detail: " + JSON.stringify(detail));
+    let evt = this._createEvent('showmodalprompt', detail,
+                                /* cancelable = */ true);
+
+    let self = this;
+    let unblockMsgSent = false;
+    function sendUnblockMsg() {
+      if (unblockMsgSent) {
+        return;
+      }
+      unblockMsgSent = true;
+
+      // We don't need to sanitize evt.detail.returnValue (e.g. converting the
+      // return value of confirm() to a boolean); Gecko does that for us.
+
+      let data = { windowID: windowID,
+                   returnValue: evt.detail.returnValue };
+      self._sendAsyncMsg('unblock-modal-prompt', data);
+    }
+
+    XPCNativeWrapper.unwrap(evt.detail).unblock = function() {
+      sendUnblockMsg();
+    };
+
+    this._frameElement.dispatchEvent(evt);
+
+    if (!evt.defaultPrevented) {
+      // Unblock the inner frame immediately.  Otherwise we'll unblock upon
+      // evt.detail.unblock().
+      sendUnblockMsg();
+    }
+  },
+
+  _createEvent: function(evtName, detail, cancelable) {
+    // This will have to change if we ever want to send a CustomEvent with null
+    // detail.  For now, it's OK.
+    if (detail !== undefined && detail !== null) {
+      return new this._window.CustomEvent('mozbrowser' + evtName,
+                                          { bubbles: true,
+                                            cancelable: cancelable,
+                                            detail: detail });
+    }
+
+    return new this._window.Event('mozbrowser' + evtName,
+                                  { bubbles: true,
+                                    cancelable: cancelable });
+  },
+
+  _sendMozAppManifestURL: function(data) {
+    return this._frameElement.getAttribute('mozapp');
+  },
+
+
+  _getScreenshot: function() {
+    let id = 'req_' + this._screenshotReqCounter++;
+    let req = Services.DOMRequest.createRequest(this._window);
+    this._screenshotListeners[id] = req;
+    this._sendAsyncMsg('get-screenshot', {id: id});
+    return req;
+  },
+
+  _recvGotScreenshot: function(data) {
+    var req = this._screenshotListeners[data.json.id];
+    delete this._screenshotListeners[data.json.id];
+    Services.DOMRequest.fireSuccess(req, data.json.screenshot);
+  },
+
+  _setVisible: function(visible) {
+    this._sendAsyncMsg('set-visible', {visible: visible});
+  },
+
+  _fireKeyEvent: function(data) {
+    let evt = this._window.document.createEvent("KeyboardEvent");
+    evt.initKeyEvent(data.json.type, true, true, this._window,
+                     false, false, false, false, // modifiers
+                     data.json.keyCode,
+                     data.json.charCode);
+
+    this._frameElement.dispatchEvent(evt);
+  },
+};
+
+var NSGetFactory = XPCOMUtils.generateNSGetFactory([BrowserElementParentFactory]);
