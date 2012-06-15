080714d344d80e2ee11c6cad1d5dea629d7913f9
David Anderson <danderson@mozilla.com>
2012-06-15 15:11:38 -0700

    Backed out changeset 2235fddebe47

--- js/src/ion/CodeGenerator.h
+++ js/src/ion/CodeGenerator.h
@@ -135,7 +135,6 @@ class CodeGenerator : public CodeGeneratorSpecific
     bool visitAbsI(LAbsI *lir);
     bool visitMathFunctionD(LMathFunctionD *ins);
     bool visitBinaryV(LBinaryV *lir);
-    bool visitCompareS(LCompareS *lir);
     bool visitCompareV(LCompareV *lir);
     bool visitIsNullOrUndefined(LIsNullOrUndefined *lir);
     bool visitIsNullOrUndefinedAndBranch(LIsNullOrUndefinedAndBranch *lir);
