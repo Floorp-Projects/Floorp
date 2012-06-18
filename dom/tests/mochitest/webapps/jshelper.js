e1b97959e31ab0f53f522e0cb71a5c66f460a5ed
Ian Bicking <ianb@mozilla.com>
2012-06-18 12:49:35 -0700

    bug 734294 - when installing an app the InstallTime is returning undefined; r=fabrice

--- dom/tests/mochitest/webapps/jshelper.js
+++ dom/tests/mochitest/webapps/jshelper.js
@@ -269,6 +269,7 @@ function install(appURL, check, next) {
       [{
         status: "== \"success\"",
         installOrigin: "== " + installOrigin.quote(),
+        installTime: "!== undefined",
         origin: "== " + origin.quote(),
         manifestURL: "== " +  appURL.quote(),
         // |manifest| is not accessible to content, so js_traverse needs to
@@ -334,4 +335,3 @@ function check_event_listener_fired (next) {
   triggered = false;
   next();
 }
-
