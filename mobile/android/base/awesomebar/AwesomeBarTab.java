9f28399db833ec04b6e341405666a52f0f081248
Wes Johnston <wjohnston@mozilla.com>
2012-06-18 12:39:13 -0700

    Bug 759041 - Move all pages tab to interface. r=lucasr

--- mobile/android/base/awesomebar/AwesomeBarTab.java
+++ mobile/android/base/awesomebar/AwesomeBarTab.java
@@ -5,11 +5,21 @@
 
 package org.mozilla.gecko;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ImageView;
 import android.widget.TabHost.TabContentFactory;
+
+import org.mozilla.gecko.db.BrowserContract.Combined;
+import org.mozilla.gecko.db.BrowserDB.URLColumns;
 
 abstract public class AwesomeBarTab {
     abstract public String getTag();
@@ -17,6 +27,10 @@ abstract public class AwesomeBarTab {
     abstract public void destroy();
     abstract public TabContentFactory getFactory();
 
+    private AwesomeBarTabs.OnUrlOpenListener mListener;
+    private LayoutInflater mInflater = null;
+    private ContentResolver mContentResolver = null;
+    private Resources mResources;
     // FIXME: This value should probably come from a prefs key
     public static final int MAX_RESULTS = 100;
     protected Context mContext = null;
@@ -30,5 +44,71 @@ abstract public class AwesomeBarTab {
         public TextView urlView;
         public ImageView faviconView;
         public ImageView bookmarkIconView;
+    }
+
+    protected LayoutInflater getInflater() {
+        if (mInflater == null) {
+            mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        return mInflater;
+    }
+
+    protected AwesomeBarTabs.OnUrlOpenListener getUrlListener() {
+        return mListener;
+    }
+
+    protected void setUrlListener(AwesomeBarTabs.OnUrlOpenListener listener) {
+        mListener = listener;
+    }
+
+    protected ContentResolver getContentResolver() {
+        if (mContentResolver == null) {
+            mContentResolver = mContext.getContentResolver();
+        }
+        return mContentResolver;
+    }
+
+    protected Resources getResources() {
+        if (mResources == null) {
+            mResources = mContext.getResources();
+        }
+        return mResources;
+    }
+
+    protected String getReaderForUrl(String url) {
+        // FIXME: still need to define the final way to open items from
+        // reading list. For now, we're using an about:reader page.
+        return "about:reader?url=" + url;
+    }
+
+    protected void updateFavicon(ImageView faviconView, Cursor cursor) {
+        byte[] b = cursor.getBlob(cursor.getColumnIndexOrThrow(URLColumns.FAVICON));
+        if (b == null) {
+            faviconView.setImageDrawable(null);
+        } else {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
+            faviconView.setImageBitmap(bitmap);
+        }
+    }
+
+    protected void updateTitle(TextView titleView, Cursor cursor) {
+        int titleIndex = cursor.getColumnIndexOrThrow(URLColumns.TITLE);
+        String title = cursor.getString(titleIndex);
+
+        // Use the URL instead of an empty title for consistency with the normal URL
+        // bar view - this is the equivalent of getDisplayTitle() in Tab.java
+        if (TextUtils.isEmpty(title)) {
+            int urlIndex = cursor.getColumnIndexOrThrow(URLColumns.URL);
+            title = cursor.getString(urlIndex);
+        }
+
+        titleView.setText(title);
+    }
+
+    protected void updateUrl(TextView urlView, Cursor cursor) {
+        int urlIndex = cursor.getColumnIndexOrThrow(URLColumns.URL);
+        String url = cursor.getString(urlIndex);
+
+        urlView.setText(url);
     }
 }
