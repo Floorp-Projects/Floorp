f97889d9a75e6d3595818ae0dffccd87fd52df4c
Jonathan Griffin <jgriffin@mozilla.com>
2012-06-19 15:29:17 -0700

    Bug 766206 - Add missing parameter to catch clause, DONTBUILD because NPOTB

--- testing/marionette/components/marionettecomponent.js
+++ testing/marionette/components/marionettecomponent.js
@@ -77,7 +77,7 @@ MarionetteComponent.prototype = {
         try {
           original = Services.prefs.getBoolPref(DEBUGGER_ENABLED_PREF);
         }
-        catch() { }
+        catch(e) { }
         Services.prefs.setBoolPref(DEBUGGER_ENABLED_PREF, true);
         // Always allow remote connections.
         DebuggerServer.initTransport(function () { return true; });
