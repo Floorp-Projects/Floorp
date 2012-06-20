8f4d91a1a1cfcd2c29ca9de1e892eeffe1bbab12
Ehsan Akhgari <ehsan@mozilla.com>
2012-06-20 00:01:02 -0400

    Bug 758992 - Make the classes which use the XPCOM nsISupports implementation macros final, to avoid the warning about deleting using a pointer to a base class with virtual functions and no virtual dtor (url-classifier parts); r=dcamp
    Pushed on a CLOSED TREE

--- toolkit/components/url-classifier/nsUrlClassifierDBService.cpp
+++ toolkit/components/url-classifier/nsUrlClassifierDBService.cpp
@@ -41,6 +41,7 @@
 #include "prprf.h"
 #include "prnetdb.h"
 #include "zlib.h"
+#include "mozilla/Attributes.h"
 
 // Needed to interpert mozIStorageConnection::GetLastError
 #include <sqlite3.h>
@@ -3825,8 +3826,8 @@ nsUrlClassifierDBServiceWorker::MaybeCreateTables(mozIStorageConnection* connect
 // and handles any necessary partial hash expansions before calling
 // the client callback.
 
-class nsUrlClassifierLookupCallback : public nsIUrlClassifierLookupCallback
-                                    , public nsIUrlClassifierHashCompleterCallback
+class nsUrlClassifierLookupCallback MOZ_FINAL : public nsIUrlClassifierLookupCallback
+                                              , public nsIUrlClassifierHashCompleterCallback
 {
 public:
   NS_DECL_ISUPPORTS
@@ -4034,7 +4035,7 @@ nsUrlClassifierLookupCallback::HandleResults()
 // Helper class for nsIURIClassifier implementation, translates table names
 // to nsIURIClassifier enums.
 
-class nsUrlClassifierClassifyCallback : public nsIUrlClassifierCallback
+class nsUrlClassifierClassifyCallback MOZ_FINAL : public nsIUrlClassifierCallback
 {
 public:
   NS_DECL_ISUPPORTS
