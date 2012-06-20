1cd465dc551d2cdfac02a00a5b67b3e475da5505
Lucas Rocha <lucasr@mozilla.com>
2012-06-20 18:12:13 +0100

    Bug 750686 - Implement style toolbar for Reader Mode (r=mfinkle)

--- mobile/android/chrome/content/aboutReader.js
+++ mobile/android/chrome/content/aboutReader.js
@@ -23,12 +23,51 @@ function dump(s) {
 let gStrings = Services.strings.createBundle("chrome://browser/locale/aboutReader.properties");
 
 let AboutReader = {
+  _STEP_INCREMENT: 0,
+  _STEP_DECREMENT: 1,
+
   init: function Reader_init() {
     dump("Init()");
 
-    dump("Feching header and content notes from about:reader");
+    this._article = null;
+
+    dump("Feching toolbar, header and content notes from about:reader");
     this._titleElement = document.getElementById("reader-header");
     this._contentElement = document.getElementById("reader-content");
+    this._toolbarElement = document.getElementById("reader-toolbar");
+
+    this._scrollOffset = window.pageYOffset;
+
+    let body = document.body;
+    body.addEventListener("touchstart", this, false);
+    body.addEventListener("click", this, false);
+    window.addEventListener("scroll", this, false);
+
+    this._setupAllDropdowns();
+    this._setupButton("share-button", this._onShare.bind(this));
+
+    let colorSchemeOptions = [
+      { name: gStrings.GetStringFromName("aboutReader.colorSchemeLight"),
+        value: "light"},
+      { name: gStrings.GetStringFromName("aboutReader.colorSchemeDark"),
+        value: "dark"},
+      { name: gStrings.GetStringFromName("aboutReader.colorSchemeSepia"),
+        value: "sepia"}
+    ];
+
+    let colorScheme = Services.prefs.getCharPref("reader.color_scheme");
+    this._setupSegmentedButton("color-scheme-buttons", colorSchemeOptions, colorScheme, this._setColorScheme.bind(this));
+    this._setColorScheme(colorScheme);
+
+    let fontTitle = gStrings.GetStringFromName("aboutReader.fontTitle");
+    this._setupStepControl("font-size-control", fontTitle, this._onFontSizeChange.bind(this));
+    this._fontSize = 0;
+    this._setFontSize(Services.prefs.getIntPref("reader.font_size"));
+
+    let marginTitle = gStrings.GetStringFromName("aboutReader.marginTitle");
+    this._setupStepControl("margin-size-control", marginTitle, this._onMarginSizeChange.bind(this));
+    this._marginSize = 0;
+    this._setMarginSize(Services.prefs.getIntPref("reader.margin_size"));
 
     dump("Decoding query arguments");
     let queryArgs = this._decodeQueryString(window.location.href);
@@ -46,11 +85,123 @@ let AboutReader = {
     }
   },
 
-  uninit: function Reader_uninit() {
-    this._hideContent();
+  handleEvent: function Reader_handleEvent(aEvent) {
+    switch (aEvent.type) {
+      case "touchstart":
+        this._scrolled = false;
+        break;
+      case "click":
+        if (!this._scrolled)
+          this._toggleToolbarVisibility();
+        break;
+      case "scroll":
+        if (!this._scrolled) {
+          this._scrolled = true;
+          this._setToolbarVisibility(false);
+        }
+        break;
+    }
+  },
 
-    delete this._titleElement;
-    delete this._contentElement;
+  uninit: function Reader_uninit() {
+    dump("Uninit()");
+
+    let body = document.body;
+    body.removeEventListener("touchstart", this, false);
+    body.removeEventListener("click", this, false);
+    window.removeEventListener("scroll", this, false);
+
+    this._hideContent();
+  },
+
+  _onShare: function Reader_onShare() {
+    if (!this._article)
+      return;
+
+    gChromeWin.sendMessageToJava({
+      gecko: {
+        type: "Reader:Share",
+        url: this._article.url,
+        title: this._article.title
+      }
+    });
+  },
+
+  _onMarginSizeChange: function Reader_onMarginSizeChange(operation) {
+    if (operation == this._STEP_INCREMENT)
+      this._setMarginSize(this._marginSize + 5);
+    else
+      this._setMarginSize(this._marginSize - 5);
+  },
+
+  _setMarginSize: function Reader_setMarginSize(newMarginSize) {
+    if (this._marginSize === newMarginSize)
+      return;
+
+    this._marginSize = Math.max(5, Math.min(25, newMarginSize));
+    document.body.style.marginLeft = this._marginSize + "%";
+    document.body.style.marginRight = this._marginSize + "%";
+
+    Services.prefs.setIntPref("reader.margin_size", this._marginSize);
+  },
+
+  _onFontSizeChange: function Reader_onFontSizeChange(operation) {
+    if (operation == this._STEP_INCREMENT)
+      this._setFontSize(this._fontSize + 1);
+    else
+      this._setFontSize(this._fontSize - 1);
+  },
+
+  _setFontSize: function Reader_setFontSize(newFontSize) {
+    if (this._fontSize === newFontSize)
+      return;
+
+    let bodyClasses = document.body.classList;
+
+    if (this._fontSize > 0)
+      bodyClasses.remove("font-size" + this._fontSize);
+
+    this._fontSize = Math.max(1, Math.min(7, newFontSize));
+    bodyClasses.add("font-size" + this._fontSize);
+
+    Services.prefs.setIntPref("reader.font_size", this._fontSize);
+  },
+
+  _setColorScheme: function Reader_setColorScheme(newColorScheme) {
+    if (this._colorScheme === newColorScheme)
+      return;
+
+    let bodyClasses = document.body.classList;
+
+    if (this._colorScheme)
+      bodyClasses.remove(this._colorScheme);
+
+    this._colorScheme = newColorScheme;
+    bodyClasses.add(this._colorScheme);
+
+    Services.prefs.setCharPref("reader.color_scheme", this._colorScheme);
+  },
+
+  _getToolbarVisibility: function Reader_getToolbarVisibility() {
+    return !this._toolbarElement.classList.contains("toolbar-hidden");
+  },
+
+  _setToolbarVisibility: function Reader_setToolbarVisibility(visible) {
+    this._closeAllDropdowns();
+
+    if (this._getToolbarVisibility() === visible)
+      return;
+
+    let toolbarElement = this._toolbarElement;
+
+    if (visible)
+      toolbarElement.display = "block";
+
+    toolbarElement.classList.toggle("toolbar-hidden");
+  },
+
+  _toggleToolbarVisibility: function Reader_toggleToolbarVisibility(visible) {
+    this._setToolbarVisibility(!this._getToolbarVisibility());
   },
 
   _loadFromURL: function Reader_loadFromURL(url) {
@@ -84,6 +235,8 @@ let AboutReader = {
   },
 
   _showContent: function Reader_showContent(article) {
+    this._article = article;
+
     this._titleElement.innerHTML = article.title;
     this._titleElement.style.display = "block";
 
@@ -116,5 +269,126 @@ let AboutReader = {
     }
 
     return result;
+  },
+
+  _setupStepControl: function Reader_setupStepControl(id, name, callback) {
+    let stepControl = document.getElementById(id);
+
+    let title = document.createElement("h1");
+    title.innerHTML = name;
+    stepControl.appendChild(title);
+
+    let plusButton = document.createElement("div");
+    plusButton.className = "button plus-button";
+    stepControl.appendChild(plusButton);
+
+    let minusButton = document.createElement("div");
+    minusButton.className = "button minus-button";
+    stepControl.appendChild(minusButton);
+
+    plusButton.addEventListener("click", function(aEvent) {
+      aEvent.stopPropagation();
+      callback(this._STEP_INCREMENT);
+    }.bind(this), true);
+
+    minusButton.addEventListener("click", function(aEvent) {
+      aEvent.stopPropagation();
+      callback(this._STEP_DECREMENT);
+    }.bind(this), true);
+  },
+
+  _setupSegmentedButton: function Reader_setupSegmentedButton(id, options, initialValue, callback) {
+    let segmentedButton = document.getElementById(id);
+
+    for (let i = 0; i < options.length; i++) {
+      let option = options[i];
+
+      let item = document.createElement("li");
+      let link = document.createElement("a");
+      link.innerHTML = option.name;
+      item.appendChild(link);
+
+      segmentedButton.appendChild(item);
+
+      link.addEventListener("click", function(aEvent) {
+        aEvent.stopPropagation();
+
+        let items = segmentedButton.children;
+        for (let j = items.length - 1; j >= 0; j--) {
+          items[j].classList.remove("selected");
+        }
+
+        item.classList.add("selected");
+        callback(option.value);
+      }.bind(this), true);
+
+      if (option.value === initialValue)
+        item.classList.add("selected");
+    }
+  },
+
+  _setupButton: function Reader_setupButton(id, callback) {
+    let button = document.getElementById(id);
+
+    button.addEventListener("click", function(aEvent) {
+      callback();
+    }, true);
+  },
+
+  _setupAllDropdowns: function Reader_setupAllDropdowns() {
+    let dropdowns = document.getElementsByClassName("dropdown");
+
+    for (let i = dropdowns.length - 1; i >= 0; i--) {
+      let dropdown = dropdowns[i];
+
+      let dropdownToggle = dropdown.getElementsByClassName("dropdown-toggle")[0];
+      let dropdownPopup = dropdown.getElementsByClassName("dropdown-popup")[0];
+
+      if (!dropdownToggle || !dropdownPopup)
+        continue;
+
+      let dropdownArrow = document.createElement("div");
+      dropdownArrow.className = "dropdown-arrow";
+      dropdownPopup.appendChild(dropdownArrow);
+
+      let updatePopupPosition = function() {
+          let popupWidth = dropdownPopup.offsetWidth + 30;
+          let arrowWidth = dropdownArrow.offsetWidth;
+          let toggleWidth = dropdownToggle.offsetWidth;
+          let toggleLeft = dropdownToggle.offsetLeft;
+
+          let popupShift = (toggleWidth - popupWidth) / 2;
+          let popupLeft = Math.max(0, Math.min(window.innerWidth - popupWidth, toggleLeft + popupShift));
+          dropdownPopup.style.left = popupLeft + "px";
+
+          let arrowShift = (toggleWidth - arrowWidth) / 2;
+          let arrowLeft = toggleLeft - popupLeft + arrowShift;
+          dropdownArrow.style.left = arrowLeft + "px";
+      }
+
+      window.addEventListener("resize", function(aEvent) {
+        updatePopupPosition();
+      }, true);
+
+      dropdownToggle.addEventListener("click", function(aEvent) {
+        aEvent.stopPropagation();
+
+        let dropdownClasses = dropdown.classList;
+
+        if (!dropdownClasses.contains("open")) {
+          updatePopupPosition();
+          this._closeAllDropdowns();
+        }
+
+        dropdownClasses.toggle("open");
+      }.bind(this), true);
+    }
+  },
+
+  _closeAllDropdowns : function Reader_closeAllDropdowns() {
+    let dropdowns = document.getElementsByClassName("dropdown");
+    for (let i = dropdowns.length - 1; i >= 0; i--) {
+      dropdowns[i].classList.remove("open");
+    }
   }
 }
