7079a510032648be1eb9b229e330b3fd29194889
Justin Lebar <justin.lebar@gmail.com>
2012-06-21 14:23:48 -0400

    Bug 741755 - Part 4: Tests for goBack / goForward on <iframe mozbrowser>. r=smaug
    --HG--
    rename : dom/browser-element/mochitest/browserElement_CanGoBack.js => dom/browser-element/mochitest/browserElement_BackForward.js
    rename : dom/browser-element/mochitest/test_browserElement_oop_CanGoBack.html => dom/browser-element/mochitest/test_browserElement_oop_BackForward.html

--- dom/browser-element/mochitest/browserElement_BackForward.js
+++ dom/browser-element/mochitest/browserElement_BackForward.js
@@ -1,12 +1,22 @@
 /* Any copyright is dedicated to the public domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-// Bug 741755 - Test that canGo{Back,Forward} works with <iframe mozbrowser>.
+// Bug 741755 - Test that canGo{Back,Forward} and go{Forward,Back} work with
+// <iframe mozbrowser>.
 
 "use strict";
 SimpleTest.waitForExplicitFinish();
 
 var iframe;
+function addOneShotIframeEventListener(event, fn) {
+  function wrapper(e) {
+    iframe.removeEventListener(event, wrapper);
+    fn(e);
+  };
+
+  iframe.addEventListener(event, wrapper);
+}
+
 function runTest() {
   // At the moment, this isn't going to work unless we're actually out of
   // process.
@@ -26,8 +36,7 @@ function runTest() {
   iframe = document.createElement('iframe');
   iframe.mozbrowser = true;
 
-  iframe.addEventListener('mozbrowserloadend', function loadend() {
-    iframe.removeEventListener('mozbrowserloadend', loadend);
+  addOneShotIframeEventListener('mozbrowserloadend', function() {
     SimpleTest.executeSoon(test2);
   });
 
@@ -60,14 +69,44 @@ function checkCanGoBackAndForward(canGoBack, canGoForward, nextTest) {
 }
 
 function test2() {
-  checkCanGoBackAndForward(/* canGoBack = */ false, /* canGoForward = */ false, test3);
+  checkCanGoBackAndForward(false, false, test3);
 }
 
 function test3() {
-  iframe.addEventListener('mozbrowserloadend', function loadend() {
-    checkCanGoBackAndForward(/* canGoBack = */ true, /* canGoForward = */ false, SimpleTest.finish);
+  addOneShotIframeEventListener('mozbrowserloadend', function() {
+    checkCanGoBackAndForward(true, false, test4);
   });
-  iframe.src = browserElementTestHelpers.emptyPage2;
+
+  SimpleTest.executeSoon(function() {
+    iframe.src = browserElementTestHelpers.emptyPage2;
+  });
+}
+
+function test4() {
+  addOneShotIframeEventListener('mozbrowserlocationchange', function(e) {
+    is(e.detail, browserElementTestHelpers.emptyPage3);
+    checkCanGoBackAndForward(true, false, test5);
+  });
+
+  SimpleTest.executeSoon(function() {
+    iframe.src = browserElementTestHelpers.emptyPage3;
+  });
+}
+
+function test5() {
+  addOneShotIframeEventListener('mozbrowserlocationchange', function(e) {
+    is(e.detail, browserElementTestHelpers.emptyPage2);
+    checkCanGoBackAndForward(true, true, test6);
+  });
+  iframe.goBack();
+}
+
+function test6() {
+  addOneShotIframeEventListener('mozbrowserlocationchange', function(e) {
+    is(e.detail, browserElementTestHelpers.emptyPage1);
+    checkCanGoBackAndForward(false, true, SimpleTest.finish);
+  });
+  iframe.goBack();
 }
 
 runTest();
