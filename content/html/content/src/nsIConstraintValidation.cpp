04b2e147fae3f5d45b7be0a6d8f20cba5a18c54c
Mounir Lamouri <mounir.lamouri@gmail.com>
2012-06-22 11:36:24 +0200

    Bug 635499 (2/2) - <input type='number'> can suffer from a range overflow when @max is set. r=sicking

--- content/html/content/src/nsIConstraintValidation.cpp
+++ content/html/content/src/nsIConstraintValidation.cpp
@@ -72,6 +72,8 @@ nsIConstraintValidation::GetValidationMessage(nsAString& aValidationMessage)
       GetValidationMessage(aValidationMessage, VALIDITY_STATE_TYPE_MISMATCH);
     } else if (GetValidityState(VALIDITY_STATE_PATTERN_MISMATCH)) {
       GetValidationMessage(aValidationMessage, VALIDITY_STATE_PATTERN_MISMATCH);
+    } else if (GetValidityState(VALIDITY_STATE_RANGE_OVERFLOW)) {
+      GetValidationMessage(aValidationMessage, VALIDITY_STATE_RANGE_OVERFLOW);
     } else {
       // TODO: The other messages have not been written
       // because related constraint validation are not implemented yet.
