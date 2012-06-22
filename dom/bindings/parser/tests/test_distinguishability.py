49e2a9624f4f943b44ce12d9e7ebdce5e68f2c83
Boris Zbarsky <bzbarsky@mit.edu>
2012-06-22 16:18:50 -0400

    Bug 742213.  isDistinguishableFrom should correctly check whether two interfaces can be implemented on the same object.  r=jlebar

--- dom/bindings/parser/tests/test_distinguishability.py
+++ dom/bindings/parser/tests/test_distinguishability.py
@@ -1,3 +1,6 @@
+def firstArgType(method):
+    return method.signatures()[0][1][0].type
+
 def WebIDLTest(parser, harness):
     parser.parse("""
       dictionary Dict {
@@ -17,9 +20,6 @@ def WebIDLTest(parser, harness):
     dictMethod = iface.members[0]
     ifaceMethod = iface.members[1]
 
-    def firstArgType(method):
-        return method.signatures()[0][1][0].type
-
     dictType = firstArgType(dictMethod)
     ifaceType = firstArgType(ifaceMethod)
 
@@ -31,3 +31,48 @@ def WebIDLTest(parser, harness):
                "Dictionary not distinguishable from callback interface")
     harness.ok(not ifaceType.isDistinguishableFrom(dictType),
                "Callback interface not distinguishable from dictionary")
+
+    parser = parser.reset()
+    parser.parse("""
+      interface TestIface {
+        void passKid(Kid arg);
+        void passParent(Parent arg);
+        void passGrandparent(Grandparent arg);
+        void passImplemented(Implemented arg);
+        void passImplementedParent(ImplementedParent arg);
+        void passUnrelated1(Unrelated1 arg);
+        void passUnrelated2(Unrelated2 arg);
+        void passArrayBuffer(ArrayBuffer arg);
+        void passArrayBuffer(ArrayBufferView arg);
+      };
+
+      interface Kid : Parent {};
+      interface Parent : Grandparent {};
+      interface Grandparent {};
+      interface Implemented : ImplementedParent {};
+      Parent implements Implemented;
+      interface ImplementedParent {};
+      interface Unrelated1 {};
+      interface Unrelated2 {};
+    """)
+    results = parser.finish()
+
+    iface = results[0]
+    harness.ok(iface.isInterface(), "Should have interface")
+    argTypes = [firstArgType(method) for method in iface.members]
+    unrelatedTypes = [firstArgType(method) for method in iface.members[-3:]]
+
+    for type1 in argTypes:
+        for type2 in argTypes:
+            distinguishable = (type1 is not type2 and
+                               (type1 in unrelatedTypes or
+                                type2 in unrelatedTypes))
+
+            harness.check(type1.isDistinguishableFrom(type2),
+                          distinguishable,
+                          "Type %s should %sbe distinguishable from type %s" %
+                          (type1, "" if distinguishable else "not ", type2))
+            harness.check(type2.isDistinguishableFrom(type1),
+                          distinguishable,
+                          "Type %s should %sbe distinguishable from type %s" %
+                          (type2, "" if distinguishable else "not ", type1))
