a5069ca074f798941127543554c6802611d94211
David Zbarsky <dzbarsky@gmail.com>
2012-06-23 08:57:57 +0200

    Bug 661746 - Part of part 2: Introduce a const AsElement(). r=bzbarsky

--- content/base/public/nsINode.h
+++ content/base/public/nsINode.h
@@ -373,6 +373,7 @@ public:
    * for which IsElement() is true.  This is defined inline in Element.h.
    */
   mozilla::dom::Element* AsElement();
+  const mozilla::dom::Element* AsElement() const;
 
   /**
    * Return this node as nsIContent.  Should only be used for nodes for which
