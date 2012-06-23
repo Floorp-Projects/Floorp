4b7dc6ef0de6c8354ed3c7cf72f0c478d0b84583
Mats Palmgren <matspal@gmail.com>
2012-06-23 03:13:56 +0200

    Bug 575294. part=3/5 r=roc

--- layout/forms/nsListControlFrame.cpp
+++ layout/forms/nsListControlFrame.cpp
@@ -584,10 +584,22 @@ nsListControlFrame::ReflowAsDropdown(nsPresContext*           aPresContext,
     } else {
       nscoord bp = aReflowState.mComputedBorderPadding.TopBottom();
       nscoord availableHeight = NS_MAX(above, below) - bp;
-      nscoord height = NS_MIN(visibleHeight, availableHeight);
-      PRInt32 rows = height / heightOfARow;
-      mNumDisplayRows = clamped(rows, 1, kMaxDropDownRows);
-      nscoord newHeight = mNumDisplayRows * heightOfARow;
+      nscoord newHeight;
+      PRInt32 rows;
+      if (visibleHeight <= availableHeight) {
+        // The dropdown fits in the available height.
+        rows = GetNumberOfOptions();
+        mNumDisplayRows = clamped(rows, 1, kMaxDropDownRows);
+        if (mNumDisplayRows == rows) {
+          newHeight = visibleHeight;  // use the exact height
+        } else {
+          newHeight = mNumDisplayRows * heightOfARow; // approximate
+        }
+      } else {
+        rows = availableHeight / heightOfARow;
+        mNumDisplayRows = clamped(rows, 1, kMaxDropDownRows);
+        newHeight = mNumDisplayRows * heightOfARow; // approximate
+      }
       state.SetComputedHeight(newHeight);
       mDropdownCanGrow = visibleHeight - newHeight >= heightOfARow &&
                          mNumDisplayRows != kMaxDropDownRows;
