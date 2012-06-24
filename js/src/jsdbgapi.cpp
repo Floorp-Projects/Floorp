ee8456ebc38903f65bfe6c76d9bdbc6e01216163
Luke Wagner <luke@mozilla.com>
2012-06-23 20:46:14 -0700

    Bug 756864 - Avoid quadratic cost of JS_GetFramePC when the stack is deep (r=bhackett)
    --HG--
    extra : rebase_source : 68dfd8c07bde53ff371067003d434b9b43464196

--- js/src/jsdbgapi.cpp
+++ js/src/jsdbgapi.cpp
@@ -497,7 +497,14 @@ JS_GetFrameScript(JSContext *cx, JSStackFrame *fp)
 JS_PUBLIC_API(jsbytecode *)
 JS_GetFramePC(JSContext *cx, JSStackFrame *fp)
 {
-    return Valueify(fp)->pcQuadratic(cx->stack);
+    /*
+     * This API is used to compute the line number for jsd and XPConnect
+     * exception handling backtraces. Once the stack gets really deep, the
+     * overall cost can become quadratic. This can hang the browser (eventually
+     * terminated by a slow-script dialog) when content causes infinite
+     * recursion and a backtrace.
+     */
+    return Valueify(fp)->pcQuadratic(cx->stack, 100);
 }
 
 JS_PUBLIC_API(void *)
