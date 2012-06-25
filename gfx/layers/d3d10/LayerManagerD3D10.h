b3677a7494079885f8822b64a68b75cc1aa94758
Nicholas Cameron <ncameron@mozilla.com>
2012-06-26 09:44:41 +1200

    Bug 757346; mask image format stuff - use A8 textures (except DX10) and some small, misc fixes. r=roc

--- gfx/layers/d3d10/LayerManagerD3D10.h
+++ gfx/layers/d3d10/LayerManagerD3D10.h
@@ -138,6 +138,9 @@ public:
     CreateOptimalSurface(const gfxIntSize &aSize,
                          gfxASurface::gfxImageFormat imageFormat);
 
+  virtual already_AddRefed<gfxASurface>
+    CreateOptimalMaskSurface(const gfxIntSize &aSize);
+
   virtual TemporaryRef<mozilla::gfx::DrawTarget>
     CreateDrawTarget(const mozilla::gfx::IntSize &aSize,
                      mozilla::gfx::SurfaceFormat aFormat);
