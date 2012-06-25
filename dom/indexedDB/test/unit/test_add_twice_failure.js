67ae46877171c3be46670974ddb2ad221901ef88
Kyle Huey <khuey@kylehuey.com>
2012-06-25 12:15:17 -0700

    Bug 748630: Support IDBTransaction.error. r=bent

--- dom/indexedDB/test/unit/test_add_twice_failure.js
+++ dom/indexedDB/test/unit/test_add_twice_failure.js
@@ -30,7 +30,7 @@ function testSteps()
   is(request.result, key, "Correct key");
 
   request = objectStore.add({}, key);
-  request.onerror = new ExpectError("ConstraintError");
+  request.onerror = new ExpectError("ConstraintError", true);
   request.onsuccess = unexpectedSuccessHandler;
   yield;
 
