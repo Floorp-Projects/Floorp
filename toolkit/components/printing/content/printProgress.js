22b5178982424d4aee86135af2c07147bd2e8077
Neil Rashbrook <neil@parkwaycc.co.uk>
2012-06-26 00:11:16 +0100

    Bug 758354 Stop unnecessarily preprocessing chrome r=dolske

--- toolkit/components/printing/content/printProgress.js
+++ toolkit/components/printing/content/printProgress.js
@@ -1,7 +1,8 @@
-# -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // dialog is just an array we'll use to store various properties from the dialog document...
 var dialog;
