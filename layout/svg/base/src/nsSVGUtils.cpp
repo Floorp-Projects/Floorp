b6413ca97987e09e82d4844eb875e4f6adc5b96a
Jonathan Watt <jwatt@jwatt.org>
2012-06-25 16:17:42 +0100

    Bug 767996 - Add null check to nsSVGUtils::InvalidateBounds. r=me.

--- layout/svg/base/src/nsSVGUtils.cpp
+++ layout/svg/base/src/nsSVGUtils.cpp
@@ -701,6 +701,12 @@ nsSVGUtils::InvalidateBounds(nsIFrame *aFrame, bool aDuringUpdate,
     aFrame = aFrame->GetParent();
   }
 
+  if (!aFrame) {
+    // We seem to be able to get here, even though SVG frames are never created
+    // without an ancestor nsSVGOuterSVGFrame. See bug 767996.
+    return;
+  }
+
   NS_ASSERTION(aFrame->GetStateBits() & NS_STATE_IS_OUTER_SVG,
                "SVG frames must always have an nsSVGOuterSVGFrame ancestor!");
   invalidArea.MoveBy(aFrame->GetContentRect().TopLeft() - aFrame->GetPosition());
