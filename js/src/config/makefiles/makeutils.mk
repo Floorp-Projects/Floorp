0998f7c5448491e317be760692976e23cc34f0e8
Phil Ringnalda <philringnalda@gmail.com>
2012-06-27 08:31:38 -0700

    Back out 6a386d638a1a (bug 746277) for packaged build bustage

--- js/src/config/makefiles/makeutils.mk
+++ js/src/config/makefiles/makeutils.mk
@@ -106,15 +106,9 @@ endif #}
 
 ###########################################################################
 ## Common makefile library loader
-##   USE_AUTOTARGETS_MK - mkdir_deps directory creation
-##   USE_RCS_MK         - revision control functions
 ###########################################################################
 topORerr =$(if $(topsrcdir),$(topsrcdir),$(error topsrcdir is not defined))
 
 ifdef USE_AUTOTARGETS_MK # mkdir_deps
   include $(topORerr)/config/makefiles/autotargets.mk
-endif
-
-ifdef USE_RCS_MK
-  include $(topORerr)/config/makefiles/rcs.mk
 endif
