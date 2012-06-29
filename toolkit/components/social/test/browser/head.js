4f9d14de943da16ae99a12f67e14a9d1e6af6973
Gavin Sharp <gavin@gavinsharp.com>
2012-06-28 21:12:01 -0400

    Bug 762569: add "FrameWorker" module, implementing a SharedWorker-like API for use by social providers. Original code by Mark Hammond <mhammond@skippinet.com.au>, Shane Caraveo <mixedpuppy@gmail.com>, and Michael Hanson <mhanson@mozilla.com>, r=me/shane/markh
    --HG--
    extra : transplant_source : %92%D8Q%B7gP4%2A%D3%B7%B3%1E%E6%18T%A7%08%F6%D9%9A

---
+++ toolkit/components/social/test/browser/head.js
@@ -0,0 +1,51 @@
+Cu.import("resource://gre/modules/Services.jsm");
+
+// A helper to run a suite of tests.
+// The "test object" should be an object with function names as keys and a
+// function as the value.  The functions will be called with a "cbnext" param
+// which should be called when the test is complete.
+// eg:
+// test = {
+//   foo: function(cbnext) {... cbnext();}
+// }
+function runTests(tests, cbPreTest, cbPostTest) {
+  waitForExplicitFinish();
+  let testIter = Iterator(tests);
+
+  if (cbPreTest === undefined) {
+    cbPreTest = function(cb) {cb()};
+  }
+  if (cbPostTest === undefined) {
+    cbPostTest = function(cb) {cb()};
+  }
+
+  let runNextTest = function() {
+    let name, func;
+    try {
+      [name, func] = testIter.next();
+    } catch (err if err instanceof StopIteration) {
+      // out of items:
+      finish();
+      return;
+    }
+    // We run on a timeout as the frameworker also makes use of timeouts, so
+    // this helps keep the debug messages sane.
+    window.setTimeout(function() {
+      function cleanupAndRunNextTest() {
+        info("sub-test " + name + " complete");
+        cbPostTest(runNextTest);
+      }
+      cbPreTest(function() {
+        info("sub-test " + name + " starting");
+        try {
+          func.call(tests, cleanupAndRunNextTest);
+        } catch (ex) {
+          ok(false, "sub-test " + name + " failed: " + ex.toString() +"\n"+ex.stack);
+          cleanupAndRunNextTest();
+        }
+      })
+    }, 0)
+  }
+  runNextTest();
+}
+
