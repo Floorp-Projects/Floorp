aed4366ee5c1801a13b519c4bee7634b088fb257
Nicholas Cameron <ncameron@mozilla.com>
2012-06-29 18:01:34 +1200

    Bug 768723; Reorganise BasicLayers.cpp into multiple files; r=roc

---
+++ gfx/layers/basic/BasicThebesLayer.h
@@ -0,0 +1,193 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef GFX_BASICTHEBESLAYER_H
+#define GFX_BASICTHEBESLAYER_H
+
+#include "mozilla/layers/PLayersParent.h"
+#include "BasicBuffers.h"
+
+namespace mozilla {
+namespace layers {
+
+class BasicThebesLayer : public ThebesLayer, public BasicImplData {
+public:
+  typedef BasicThebesLayerBuffer Buffer;
+
+  BasicThebesLayer(BasicLayerManager* aLayerManager) :
+    ThebesLayer(aLayerManager, static_cast<BasicImplData*>(this)),
+    mBuffer(this)
+  {
+    MOZ_COUNT_CTOR(BasicThebesLayer);
+  }
+  virtual ~BasicThebesLayer()
+  {
+    MOZ_COUNT_DTOR(BasicThebesLayer);
+  }
+
+  virtual void SetVisibleRegion(const nsIntRegion& aRegion)
+  {
+    NS_ASSERTION(BasicManager()->InConstruction(),
+                 "Can only set properties in construction phase");
+    ThebesLayer::SetVisibleRegion(aRegion);
+  }
+  virtual void InvalidateRegion(const nsIntRegion& aRegion)
+  {
+    NS_ASSERTION(BasicManager()->InConstruction(),
+                 "Can only set properties in construction phase");
+    mValidRegion.Sub(mValidRegion, aRegion);
+  }
+
+  virtual void PaintThebes(gfxContext* aContext,
+                           Layer* aMaskLayer,
+                           LayerManager::DrawThebesLayerCallback aCallback,
+                           void* aCallbackData,
+                           ReadbackProcessor* aReadback);
+
+  virtual void ClearCachedResources() { mBuffer.Clear(); mValidRegion.SetEmpty(); }
+  
+  virtual already_AddRefed<gfxASurface>
+  CreateBuffer(Buffer::ContentType aType, const nsIntSize& aSize);
+
+  virtual void ComputeEffectiveTransforms(const gfx3DMatrix& aTransformToSurface)
+  {
+    if (!BasicManager()->IsRetained()) {
+      // Don't do any snapping of our transform, since we're just going to
+      // draw straight through without intermediate buffers.
+      mEffectiveTransform = GetLocalTransform()*aTransformToSurface;
+      if (gfxPoint(0,0) != mResidualTranslation) {
+        mResidualTranslation = gfxPoint(0,0);
+        mValidRegion.SetEmpty();
+      }
+      ComputeEffectiveTransformForMaskLayer(aTransformToSurface);
+      return;
+    }
+    ThebesLayer::ComputeEffectiveTransforms(aTransformToSurface);
+  }
+
+  // Sync front/back buffers content
+  virtual void SyncFrontBufferToBackBuffer() {}
+
+protected:
+  BasicLayerManager* BasicManager()
+  {
+    return static_cast<BasicLayerManager*>(mManager);
+  }
+
+  virtual void
+  PaintBuffer(gfxContext* aContext,
+              const nsIntRegion& aRegionToDraw,
+              const nsIntRegion& aExtendedRegionToDraw,
+              const nsIntRegion& aRegionToInvalidate,
+              bool aDidSelfCopy,
+              LayerManager::DrawThebesLayerCallback aCallback,
+              void* aCallbackData)
+  {
+    if (!aCallback) {
+      BasicManager()->SetTransactionIncomplete();
+      return;
+    }
+    aCallback(this, aContext, aExtendedRegionToDraw, aRegionToInvalidate,
+              aCallbackData);
+    // Everything that's visible has been validated. Do this instead of just
+    // OR-ing with aRegionToDraw, since that can lead to a very complex region
+    // here (OR doesn't automatically simplify to the simplest possible
+    // representation of a region.)
+    nsIntRegion tmp;
+    tmp.Or(mVisibleRegion, aExtendedRegionToDraw);
+    mValidRegion.Or(mValidRegion, tmp);
+  }
+
+  Buffer mBuffer;
+};
+
+class BasicShadowableThebesLayer : public BasicThebesLayer,
+                                   public BasicShadowableLayer
+{
+  typedef BasicThebesLayer Base;
+
+public:
+  BasicShadowableThebesLayer(BasicShadowLayerManager* aManager)
+    : BasicThebesLayer(aManager)
+    , mIsNewBuffer(false)
+    , mFrontAndBackBufferDiffer(false)
+  {
+    MOZ_COUNT_CTOR(BasicShadowableThebesLayer);
+  }
+  virtual ~BasicShadowableThebesLayer()
+  {
+    DestroyBackBuffer();
+    MOZ_COUNT_DTOR(BasicShadowableThebesLayer);
+  }
+
+  virtual void PaintThebes(gfxContext* aContext,
+                           Layer* aMaskLayer,
+                           LayerManager::DrawThebesLayerCallback aCallback,
+                           void* aCallbackData,
+                           ReadbackProcessor* aReadback);
+
+  virtual void FillSpecificAttributes(SpecificLayerAttributes& aAttrs)
+  {
+    aAttrs = ThebesLayerAttributes(GetValidRegion());
+  }
+
+  virtual Layer* AsLayer() { return this; }
+  virtual ShadowableLayer* AsShadowableLayer() { return this; }
+  virtual bool MustRetainContent() { return HasShadow(); }
+
+  void SetBackBufferAndAttrs(const OptionalThebesBuffer& aBuffer,
+                             const nsIntRegion& aValidRegion,
+                             const OptionalThebesBuffer& aReadOnlyFrontBuffer,
+                             const nsIntRegion& aFrontUpdatedRegion);
+
+  virtual void Disconnect();
+
+  virtual BasicShadowableThebesLayer* AsThebes() { return this; }
+
+  virtual void SyncFrontBufferToBackBuffer();
+
+private:
+  BasicShadowLayerManager* BasicManager()
+  {
+    return static_cast<BasicShadowLayerManager*>(mManager);
+  }
+
+  virtual void
+  PaintBuffer(gfxContext* aContext,
+              const nsIntRegion& aRegionToDraw,
+              const nsIntRegion& aExtendedRegionToDraw,
+              const nsIntRegion& aRegionToInvalidate,
+              bool aDidSelfCopy,
+              LayerManager::DrawThebesLayerCallback aCallback,
+              void* aCallbackData) MOZ_OVERRIDE;
+
+  virtual already_AddRefed<gfxASurface>
+  CreateBuffer(Buffer::ContentType aType, const nsIntSize& aSize) MOZ_OVERRIDE;
+
+  void DestroyBackBuffer()
+  {
+    if (IsSurfaceDescriptorValid(mBackBuffer)) {
+      BasicManager()->ShadowLayerForwarder::DestroySharedSurface(&mBackBuffer);
+    }
+  }
+
+  // This describes the gfxASurface we hand to mBuffer.  We keep a
+  // copy of the descriptor here so that we can call
+  // DestroySharedSurface() on the descriptor.
+  SurfaceDescriptor mBackBuffer;
+  nsIntRect mBackBufferRect;
+  nsIntPoint mBackBufferRectRotation;
+
+  bool mIsNewBuffer;
+  OptionalThebesBuffer mROFrontBuffer;
+  nsIntRegion mFrontUpdatedRegion;
+  nsIntRegion mFrontValidRegion;
+  PRPackedBool mFrontAndBackBufferDiffer;
+};
+
+}
+}
+
+#endif
