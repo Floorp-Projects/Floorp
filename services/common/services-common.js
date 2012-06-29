6b9044cd4927f8553a754856b026f156e1248065
Gregory Szorc <gps@mozilla.com>
2012-06-29 13:30:05 -0700

    Bug 732152 - Implement standalone client for Storage Service 2.0; r=rnewman

--- services/common/services-common.js
+++ services/common/services-common.js
@@ -8,4 +8,5 @@
 pref("services.common.log.logger.rest.request", "Debug");
 pref("services.common.log.logger.rest.response", "Debug");
 
+pref("services.common.storageservice.sendVersionInfo", true);
 pref("services.common.tokenserverclient.logger.level", "Info");
