0895d990b9faf2a40e11c48774b4a5e73d93a1ce
Gregory Szorc <gps@mozilla.com>
2012-06-29 13:27:06 -0700

    Bug 755339 - Package testing-only JS modules; r=ted

--- testing/testsuite-targets.mk
+++ testing/testsuite-targets.mk
@@ -293,7 +293,16 @@ package-tests: stage-android
 endif
 
 make-stage-dir:
-	rm -rf $(PKG_STAGE) && $(NSINSTALL) -D $(PKG_STAGE) && $(NSINSTALL) -D $(PKG_STAGE)/bin && $(NSINSTALL) -D $(PKG_STAGE)/bin/components && $(NSINSTALL) -D $(PKG_STAGE)/certs && $(NSINSTALL) -D $(PKG_STAGE)/jetpack && $(NSINSTALL) -D $(PKG_STAGE)/firebug && $(NSINSTALL) -D $(PKG_STAGE)/peptest && $(NSINSTALL) -D $(PKG_STAGE)/mozbase && $(NSINSTALL) -D $(PKG_STAGE)/modules
+	rm -rf $(PKG_STAGE)
+	$(NSINSTALL) -D $(PKG_STAGE)
+	$(NSINSTALL) -D $(PKG_STAGE)/bin
+	$(NSINSTALL) -D $(PKG_STAGE)/bin/components
+	$(NSINSTALL) -D $(PKG_STAGE)/certs
+	$(NSINSTALL) -D $(PKG_STAGE)/jetpack
+	$(NSINSTALL) -D $(PKG_STAGE)/firebug
+	$(NSINSTALL) -D $(PKG_STAGE)/peptest
+	$(NSINSTALL) -D $(PKG_STAGE)/mozbase
+	$(NSINSTALL) -D $(PKG_STAGE)/modules
 
 robotium-id-map:
 ifeq ($(MOZ_BUILD_APP),mobile/android)
@@ -337,9 +346,9 @@ stage-tps: make-stage-dir
 	@(cd $(topsrcdir)/services/sync/tps && tar $(TAR_CREATE_FLAGS) - *) | (cd $(PKG_STAGE)/tps && tar -xf -)
 	(cd $(topsrcdir)/services/sync/tests/tps && tar $(TAR_CREATE_FLAGS_QUIET) - *) | (cd $(PKG_STAGE)/tps/tests && tar -xf -)
 
-# This will get replaced by actual logic in a subsequent patch.
 stage-modules: make-stage-dir
-	$(TOUCH) $(PKG_STAGE)/modules/.dummy
+	$(NSINSTALL) -D $(PKG_STAGE)/modules
+	cp -RL $(DEPTH)/_tests/modules $(PKG_STAGE)
 
 stage-mozbase: make-stage-dir
 	$(MAKE) -C $(DEPTH)/testing/mozbase stage-package
