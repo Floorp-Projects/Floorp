2ed11f64dbbdc1021e5ab0e86e0dd51dab0a2156
Brian Smith <bsmith@mozilla.com>
2012-06-28 19:39:48 -0700

    Backout change to netwerk/test/unit/test_bug651100.js from the patch for Bug 737615, a=bustage
    --HG--
    extra : rebase_source : 50d718395cafa292025452bb01a94fec9f4ae216

--- netwerk/test/unit/test_bug651100.js
+++ netwerk/test/unit/test_bug651100.js
@@ -2,6 +2,29 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cr = Components.results;
 
+var _CSvc;
+function get_cache_service() {
+  if (_CSvc)
+    return _CSvc;
+
+  return _CSvc = Cc["@mozilla.org/network/cache-service;1"].
+                 getService(Ci.nsICacheService);
+}
+
+function get_ostream_for_entry(key, asFile, entryRef)
+{
+  var cache = get_cache_service();
+  var session = cache.createSession(
+                  "HTTP",
+                  asFile ? Ci.nsICache.STORE_ON_DISK_AS_FILE
+                         : Ci.nsICache.STORE_ON_DISK,
+                  Ci.nsICache.STREAM_BASED);
+  var cacheEntry = session.openCacheEntry(key, Ci.nsICache.ACCESS_WRITE, true);
+  var oStream = cacheEntry.openOutputStream(0);
+  entryRef.value = cacheEntry;
+  return oStream;
+}
+
 function gen_1MiB()
 {
   var i;
@@ -21,39 +44,37 @@ function write_and_check(str, data, len)
   }
 }
 
-function write_big_datafile(status, entry)
+function write_big_datafile()
 {
-  do_check_eq(status, Cr.NS_OK);
-  var os = entry.openOutputStream(0);
+  var entry = {};
+  var oStr = get_ostream_for_entry("bigdata", true, entry);
   var data = gen_1MiB();
 
   // >64MiB
   var i;
   for (i=0 ; i<65 ; i++)
-    write_and_check(os, data, data.length);
+    write_and_check(oStr, data, data.length);
 
-  os.close();
-  entry.close();
-  do_execute_soon(run_test_2);
+  oStr.close();
+  entry.value.close();
 }
 
-function write_and_doom_big_metafile(status, entry)
+function write_and_doom_big_metafile()
 {
-  do_check_eq(status, Cr.NS_OK);
-  var os = entry.openOutputStream(0);
+  var entry = {};
+  var oStr = get_ostream_for_entry("bigmetadata", true, entry);
   var data = gen_1MiB();
 
   // >64MiB
   var i;
   for (i=0 ; i<65 ; i++)
-    entry.setMetaDataElement("metadata_"+i, data);
+    entry.value.setMetaDataElement("metadata_"+i, data);
 
-  write_and_check(os, data, data.length);
+  write_and_check(oStr, data, data.length);
 
-  os.close();
-  entry.doom();
-  entry.close();
-  do_execute_soon(run_test_3);
+  oStr.close();
+  entry.value.doom();
+  entry.value.close();
 }
 
 function check_cache_size() {
@@ -77,6 +98,34 @@ function check_cache_size() {
   do_check_true(diskDeviceVisited);
 }
 
+var _continue_with = null;
+function sync_with_cache_IO_thread(aFunc)
+{
+  do_check_eq(sync_with_cache_IO_thread_cb.listener, null);
+  sync_with_cache_IO_thread_cb.listener = aFunc;
+  var cache = get_cache_service();
+  var session = cache.createSession(
+                  "HTTP",
+                  Ci.nsICache.STORE_ON_DISK,
+                  Ci.nsICache.STREAM_BASED);
+  var cacheEntry = session.asyncOpenCacheEntry(
+                     "nonexistententry",
+                     Ci.nsICache.ACCESS_READ,
+                     sync_with_cache_IO_thread_cb);
+}
+
+var sync_with_cache_IO_thread_cb = {
+  listener: null,
+
+  onCacheEntryAvailable: function oCEA(descriptor, accessGranted, status) {
+    do_check_eq(status, Cr.NS_ERROR_CACHE_KEY_NOT_FOUND);
+    cb = this.listener;
+    this.listener = null;
+    do_timeout(0, cb);
+  }
+};
+
+
 function run_test() {
   var prefBranch = Cc["@mozilla.org/preferences-service;1"].
                      getService(Ci.nsIPrefBranch);
@@ -85,14 +134,16 @@ function run_test() {
   do_get_profile();
 
   // clear the cache
-  evict_cache_entries();
+  get_cache_service().evictEntries(Ci.nsICache.STORE_ANYWHERE);
 
   // write an entry with data > 64MiB
-  asyncOpenCacheEntry("bigdata",
-                      "HTTP",
-                      Ci.nsICache.STORE_ON_DISK_AS_FILE,
-                      Ci.nsICache.ACCESS_WRITE,
-                      write_big_datafile);
+  write_big_datafile();
+
+  // DoomEntry() is called when the cache is full, but the data is really
+  // deleted (and the cache size updated) on the background thread when the
+  // entry is deactivated. We need to sync with the cache IO thread before we
+  // continue with the test.
+  sync_with_cache_IO_thread(run_test_2);
 
   do_test_pending();
 }
@@ -102,11 +153,9 @@ function run_test_2()
   check_cache_size();
 
   // write an entry with metadata > 64MiB
-  asyncOpenCacheEntry("bigmetadata",
-                      "HTTP",
-                      Ci.nsICache.STORE_ON_DISK_AS_FILE,
-                      Ci.nsICache.ACCESS_WRITE,
-                      write_and_doom_big_metafile);
+  write_and_doom_big_metafile();
+
+  sync_with_cache_IO_thread(run_test_3);
 }
 
 function run_test_3()
