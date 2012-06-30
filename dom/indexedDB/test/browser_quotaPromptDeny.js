a221e9eebb52de00b473a06b754cfe35dfe54b25
Ben Turner <bent.mozilla@gmail.com>
2012-06-30 19:57:15 +0200

    Bug 758357: Only show prompt after 50mb of usage for now. Plan is to soon introduce a new service which allows us to remove "old" indexedDB databases and keep a global quota, but for now we simply remove the prompt. r=sicking

--- dom/indexedDB/test/browser_quotaPromptDeny.js
+++ dom/indexedDB/test/browser_quotaPromptDeny.js
@@ -12,7 +12,6 @@ function test()
 {
   waitForExplicitFinish();
   requestLongerTimeout(10);
-  setPermission(testPageURL, "indexedDB");
   removePermission(testPageURL, "indexedDB-unlimited");
   Services.prefs.setIntPref("dom.indexedDB.warningQuota", 2);
   executeSoon(test1);
