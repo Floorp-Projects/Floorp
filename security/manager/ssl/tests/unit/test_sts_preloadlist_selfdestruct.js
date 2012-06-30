eb8910018d3e03da0f18b261af1d7f46dcd5c6a1
Josh Matthews <josh@joshmatthews.net>
2012-06-30 07:34:17 -0700

    Bug 722979 - Add privacy status argument to relevant nsIStrictTransportSecurityService methods. r=bsmith sr=biesi

--- security/manager/ssl/tests/unit/test_sts_preloadlist_selfdestruct.js
+++ security/manager/ssl/tests/unit/test_sts_preloadlist_selfdestruct.js
@@ -9,16 +9,16 @@ function run_test() {
                      .getService(Ci.nsIStrictTransportSecurityService);
 
   // check that a host on the preload list is identified as an sts host
-  do_check_true(STSService.isStsHost("alpha.irccloud.com"));
+  do_check_true(STSService.isStsHost("alpha.irccloud.com", 0));
 
   // now simulate that it's 19 weeks later than it actually is
   let offsetSeconds = 19 * 7 * 24 * 60 * 60;
   Services.prefs.setIntPref("test.currentTimeOffsetSeconds", offsetSeconds);
 
   // check that the preloaded host is no longer considered sts
-  do_check_false(STSService.isStsHost("alpha.irccloud.com"));
+  do_check_false(STSService.isStsHost("alpha.irccloud.com", 0));
 
   // just make sure we can get everything back to normal
   Services.prefs.clearUserPref("test.currentTimeOffsetSeconds");
-  do_check_true(STSService.isStsHost("alpha.irccloud.com"));
+  do_check_true(STSService.isStsHost("alpha.irccloud.com", 0));
 }
