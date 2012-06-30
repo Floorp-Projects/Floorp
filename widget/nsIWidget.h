af7876e875435c5580a3f70a3e30f6264c7915df
Matt Woodrow <mwoodrow@mozilla.com>
2012-06-30 15:06:13 +1200

    Bug 539356 - Part 24 - Don't paint android widgets that aren't being composited, or at the front. r=roc

--- widget/nsIWidget.h
+++ widget/nsIWidget.h
@@ -1572,7 +1572,7 @@ class nsIWidget : public nsISupports {
     virtual bool WidgetPaintsBackground() { return false; }
 
     void SetNeedsPaint(bool aNeedsPaint) { mNeedsPaint = aNeedsPaint; }
-    bool NeedsPaint() { 
+    virtual bool NeedsPaint() { 
       if (!mNeedsPaint) {
         return false;
       }
