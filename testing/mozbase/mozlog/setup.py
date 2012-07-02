933bdbeae06ffb8098c44a7fab215ec40e12a3f1
Siddharth Agarwal <sid.bugzilla@gmail.com>
2012-07-03 01:52:54 +0530

    Bug 769808 - Update mozbase on m-c to include fix for bug 769789. r=jhammel

--- testing/mozbase/mozlog/setup.py
+++ testing/mozbase/mozlog/setup.py
@@ -1,13 +1,13 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
 import sys
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = "mozlog"
-PACKAGE_VERSION = "1.0"
+PACKAGE_VERSION = "1.1"
 
 desc = """Robust log handling specialized for logging in the Mozilla universe"""
 # take description from README
--- testing/mozbase/mozrunner/mozrunner/runner.py
+++ testing/mozbase/mozrunner/mozrunner/runner.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 # This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 
 __all__ = ['Runner', 'ThunderbirdRunner', 'FirefoxRunner', 'runners', 'CLI', 'cli', 'package_metadata']
 
@@ -8,15 +10,55 @@ import mozinfo
 import optparse
 import os
 import platform
+import subprocess
 import sys
 import ConfigParser
 
+from threading import Thread
 from utils import get_metadata_from_egg
 from utils import findInPath
 from mozprofile import *
 from mozprocess.processhandler import ProcessHandler
 
+if mozinfo.isMac:
+    from plistlib import readPlist
+
 package_metadata = get_metadata_from_egg('mozrunner')
+
+# Map of debugging programs to information about them
+# from http://mxr.mozilla.org/mozilla-central/source/build/automationutils.py#59
+debuggers = {'gdb': {'interactive': True,
+                     'args': ['-q', '--args'],},
+             'valgrind': {'interactive': False,
+                          'args': ['--leak-check=full']}
+             }
+
+def debugger_arguments(debugger, arguments=None, interactive=None):
+    """
+    finds debugger arguments from debugger given and defaults
+    * debugger : debugger name or path to debugger
+    * arguments : arguments to the debugger, or None to use defaults
+    * interactive : whether the debugger should be run in interactive mode, or None to use default
+    """
+
+    # find debugger executable if not a file
+    executable = debugger
+    if not os.path.exists(executable):
+        executable = findInPath(debugger)
+    if executable is None:
+        raise Exception("Path to '%s' not found" % debugger)
+
+    # if debugger not in dictionary of knowns return defaults
+    dirname, debugger = os.path.split(debugger)
+    if debugger not in debuggers:
+        return ([executable] + (arguments or []), bool(interactive))
+
+    # otherwise use the dictionary values for arguments unless specified
+    if arguments is None:
+        arguments = debuggers[debugger].get('args', [])
+    if interactive is None:
+        interactive = debuggers[debugger].get('interactive', False)
+    return ([executable] + arguments, interactive)
 
 class Runner(object):
     """Handles all running operations. Finds bins, runs and kills the process."""
@@ -24,8 +66,8 @@ class Runner(object):
     profile_class = Profile # profile class to use by default
 
     @classmethod
-    def create(cls, binary, cmdargs=None, env=None, kp_kwargs=None, profile_args=None,
-                                               clean_profile=True, process_class=ProcessHandler):
+    def create(cls, binary=None, cmdargs=None, env=None, kp_kwargs=None, profile_args=None,
+               clean_profile=True, process_class=ProcessHandler):
         profile = cls.profile_class(**(profile_args or {}))
         return cls(profile, binary=binary, cmdargs=cmdargs, env=env, kp_kwargs=kp_kwargs,
                                            clean_profile=clean_profile, process_class=process_class)
@@ -43,6 +85,13 @@ class Runner(object):
             raise Exception("Binary not specified")
         if not os.path.exists(self.binary):
             raise OSError("Binary path does not exist: %s" % self.binary)
+
+        # allow Mac binaries to be specified as an app bundle
+        plist = '%s/Contents/Info.plist' % self.binary
+        if mozinfo.isMac and os.path.exists(plist):
+            info = readPlist(plist)
+            self.binary = os.path.join(self.binary, "Contents/MacOS/",
+                                       info['CFBundleExecutable'])
 
         self.cmdargs = cmdargs or []
         _cmdargs = [i for i in self.cmdargs
@@ -102,8 +151,11 @@ class Runner(object):
     def is_running(self):
         return self.process_handler is not None
 
-    def start(self):
-        """Run self.command in the proper environment."""
+    def start(self, debug_args=None, interactive=False):
+        """
+        Run self.command in the proper environment.
+        - debug_args: arguments for the debugger
+        """
 
         # ensure you are stopped
         self.stop()
@@ -111,17 +163,35 @@ class Runner(object):
         # ensure the profile exists
         if not self.profile.exists():
             self.profile.reset()
-        
+            assert self.profile.exists(), "%s : failure to reset profile" % self.__class__.__name__
+
         cmd = self._wrap_command(self.command+self.cmdargs)
-        # this run uses the managed processhandler
-        self.process_handler = self.process_class(cmd, env=self.env, **self.kp_kwargs)
-        self.process_handler.run()
+
+        # attach a debugger, if specified
+        if debug_args:
+            cmd = list(debug_args) + cmd
+
+        #
+        if interactive:
+            self.process_handler = subprocess.Popen(cmd, env=self.env)
+            # TODO: other arguments
+        else:
+            # this run uses the managed processhandler
+            self.process_handler = self.process_class(cmd, env=self.env, **self.kp_kwargs)
+            self.process_handler.run()
+
+            # Spin a thread to handle reading the output
+            self.outThread = OutputThread(self.process_handler)
+            self.outThread.start()
 
     def wait(self, timeout=None, outputTimeout=None):
         """Wait for the app to exit."""
         if self.process_handler is None:
             return
-        self.process_handler.waitForFinish(timeout=timeout, outputTimeout=outputTimeout)
+        if isinstance(self.process_handler, subprocess.Popen):
+            self.process_handler.wait()
+        else:
+            self.process_handler.waitForFinish(timeout=timeout, outputTimeout=outputTimeout)
         self.process_handler = None
 
     def stop(self):
@@ -152,7 +222,7 @@ class Runner(object):
         if mozinfo.isMac and hasattr(platform, 'mac_ver') and \
                                platform.mac_ver()[0][:4] < '10.6':
             return ["arch", "-arch", "i386"] + cmd
-        return cmd 
+        return cmd
 
     __del__ = cleanup
 
@@ -170,27 +240,19 @@ class FirefoxRunner(Runner):
 
         Runner.__init__(self, profile, binary, **kwargs)
 
-        # Find application version number
-        appdir = os.path.dirname(os.path.realpath(self.binary))
-        appini = ConfigParser.RawConfigParser()
-        appini.read(os.path.join(appdir, 'application.ini'))
-        # Version needs to be of the form 3.6 or 4.0b and not the whole string
-        version = appini.get('App', 'Version').rstrip('0123456789pre').rstrip('.')
-
-        # Disable compatibility check. See:
-        # - http://kb.mozillazine.org/Extensions.checkCompatibility
-        # - https://bugzilla.mozilla.org/show_bug.cgi?id=659048
-        preference = {'extensions.checkCompatibility.' + version: False,
-                      'extensions.checkCompatibility.nightly': False}
-        self.profile.set_preferences(preference)
-
-
 class ThunderbirdRunner(Runner):
     """Specialized Runner subclass for running Thunderbird"""
     profile_class = ThunderbirdProfile
 
 runners = {'firefox': FirefoxRunner,
            'thunderbird': ThunderbirdRunner}
+
+class OutputThread(Thread):
+    def __init__(self, prochandler):
+        Thread.__init__(self)
+        self.ph = prochandler
+    def run(self):
+        self.ph.waitForFinish()
 
 class CLI(MozProfileCLI):
     """Command line interface."""
@@ -239,6 +301,14 @@ class CLI(MozProfileCLI):
         parser.add_option('--app-arg', dest='appArgs',
                           default=[], action='append',
                           help="provides an argument to the test application")
+        parser.add_option('--debugger', dest='debugger',
+                          help="run under a debugger, e.g. gdb or valgrind")
+        parser.add_option('--debugger-args', dest='debugger_args',
+                          action='append', default=None,
+                          help="arguments to the debugger")
+        parser.add_option('--interactive', dest='interactive',
+                          action='store_true',
+                          help="run the program interactively")
         if self.metadata:
             parser.add_option("--info", dest="info", default=False,
                               action="store_true",
@@ -284,10 +354,24 @@ class CLI(MozProfileCLI):
         self.start(runner)
         runner.cleanup()
 
+    def debugger_arguments(self):
+        """
+        returns a 2-tuple of debugger arguments:
+        (debugger_arguments, interactive)
+        """
+        debug_args = self.options.debugger_args
+        interactive = self.options.interactive
+        if self.options.debugger:
+            debug_args, interactive = debugger_arguments(self.options.debugger)
+        return debug_args, interactive
+
     def start(self, runner):
         """Starts the runner and waits for Firefox to exit or Keyboard Interrupt.
         Shoule be overwritten to provide custom running of the runner instance."""
-        runner.start()
+
+        # attach a debugger if specified
+        debug_args, interactive = self.debugger_arguments()
+        runner.start(debug_args=debug_args, interactive=interactive)
         print 'Starting:', ' '.join(runner.command)
         try:
             runner.wait()
