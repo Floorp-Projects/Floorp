ab79ac2196e958e76d8f5c6fc41db444d7db96a2
Rafael Ávila de Espíndola <respindola@mozilla.com>
2012-07-03 09:15:34 -0400

    Bug 732173 - poison write during shutdown in a debug build. r=ted. With this patch we now patch the write functions during shutdown in a debug build and abort if a non white listed write is found.

---
+++ xpcom/build/mozPoisonWriteStub.cpp
@@ -0,0 +1,18 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim:set ts=4 sw=4 sts=4 ci et: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+namespace mozilla {
+void PoisonWrite() {
+}
+void DisableWritePoisoning() {
+}
+}
+extern "C" {
+    void MozillaRegisterDebugFD(int fd) {
+    }
+    void MozillaUnRegisterDebugFD(int fd) {
+    }
+}
