90a1f3db568b6aec01712b557e96425937b8be4a
Fabrice Desré <fabrice@mozilla.com>
2012-07-02 17:16:55 -0700

    Bug 755245 - Implement System Message Handler : Part 3, DOM implementation [r=vingtetun,mounir]

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -522,6 +522,8 @@ using mozilla::dom::indexedDB::IDBWrapperCache;
 #include "BluetoothAdapter.h"
 #endif
 
+#include "nsIDOMNavigatorSystemMessages.h"
+
 #include "DOMError.h"
 #include "DOMRequest.h"
 #include "nsIOpenWindowEventDetail.h"
@@ -2483,6 +2485,7 @@ nsDOMClassInfo::Init()
 #ifdef MOZ_B2G_BT
     DOM_CLASSINFO_MAP_ENTRY(nsIDOMNavigatorBluetooth)
 #endif
+    DOM_CLASSINFO_MAP_ENTRY(nsIDOMNavigatorSystemMessages)
   DOM_CLASSINFO_MAP_END
 
   DOM_CLASSINFO_MAP_BEGIN(Plugin, nsIDOMPlugin)
