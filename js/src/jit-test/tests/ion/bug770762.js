b601aacfc31dcfc26d18cda995418f376f36b386
Kannan Vijayan <kvijayan@mozilla.com>
2012-07-04 16:27:58 -0400

    Bug 770762 - added test case to ion-tests. (r=gkw)

---
+++ js/src/jit-test/tests/ion/bug770762.js
@@ -0,0 +1,18 @@
+function first(a) {
+  return a[0];
+}
+function g() {
+  first([function() {}])();
+}
+first([function() {}]);
+first([function() {}]);
+first([function() {}]);
+first([function() {}]);
+first([function() {}]);
+first([function() {}]);
+first([function() {}]);
+first([function() {}]);
+first([function() {}]);
+first([function() {}]);
+g();
+g();
