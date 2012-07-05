b8a9e50736c791314832e42f67519912ce25017a
Margaret Leibovic <margaret.leibovic@gmail.com>
2012-07-05 16:12:17 -0700

    Bug 718705 - Only persist the password doorhanger across location changes if it is showing. r=mfinkle

--- mobile/android/base/DoorHanger.java
+++ mobile/android/base/DoorHanger.java
@@ -38,6 +38,7 @@ public class DoorHanger extends LinearLayout implements Button.OnClickListener {
     static private LayoutInflater mInflater;
 
     private int mPersistence = 0;
+    private boolean mPersistWhileVisible = false;
     private long mTimeout = 0;
 
     public DoorHanger(Context aContext, String aValue) {
@@ -127,6 +128,10 @@ public class DoorHanger extends LinearLayout implements Button.OnClickListener {
         } catch (JSONException e) { }
 
         try {
+            mPersistWhileVisible = options.getBoolean("persistWhileVisible");
+        } catch (JSONException e) { }
+
+        try {
             mTimeout = options.getLong("timeout");
         } catch (JSONException e) { }
 
@@ -163,6 +168,13 @@ public class DoorHanger extends LinearLayout implements Button.OnClickListener {
     // This method checks with persistence and timeout options to see if
     // it's okay to remove a doorhanger.
     public boolean shouldRemove() {
+        if (mPersistWhileVisible && GeckoApp.mAppContext.mDoorHangerPopup.isShowing()) {
+            // We still want to decrement mPersistence, even if the popup is showing
+            if (mPersistence != 0)
+                mPersistence--;
+            return false;
+        }
+
         // If persistence is set to -1, the doorhanger will never be
         // automatically removed.
         if (mPersistence != 0) {
