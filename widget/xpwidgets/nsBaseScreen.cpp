e3783dda32a33ef1d4b705fb035f2e5d2f1797b6
Benoit Jacob <bjacob@mozilla.com>
2012-07-05 10:32:10 -0400

    Bug 754572 - add option to use fatal assertions in NS_CheckThreadSafe, use it in nsBaseScreen and nsScreenManagerAndroid - r=bsmedberg

--- widget/xpwidgets/nsBaseScreen.cpp
+++ widget/xpwidgets/nsBaseScreen.cpp
@@ -5,6 +5,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#define MOZ_FATAL_ASSERTIONS_FOR_THREAD_SAFETY
+
 #include "nsBaseScreen.h"
 
 NS_IMPL_ISUPPORTS1(nsBaseScreen, nsIScreen)
