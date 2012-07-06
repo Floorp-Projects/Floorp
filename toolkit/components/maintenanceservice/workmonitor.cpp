f08be271cca87294eddf39510cb893f5cb68fcaf
Brian R. Bondy <netzen@gmail.com>
2012-07-06 07:41:41 -0400

    Bug 770883 - Disable functionality for clearing prefetch. r=taras

--- toolkit/components/maintenanceservice/workmonitor.cpp
+++ toolkit/components/maintenanceservice/workmonitor.cpp
@@ -504,9 +504,14 @@ ExecuteServiceCommand(int argc, LPWSTR *argv)
     // because the service self updates itself and the service
     // installer will stop the service.
     LOG(("Service command %ls complete.\n", argv[2]));
-  } else if (!lstrcmpi(argv[2], L"clear-prefetch")) {
+  }
+  // See Bug 770883
+#if 0
+  else if (!lstrcmpi(argv[2], L"clear-prefetch")) {
     result = ClearKnownPrefetch();
-  } else {
+  } 
+#endif
+  else {
     LOG(("Service command not recognized: %ls.\n", argv[2]));
     // result is already set to FALSE
   }
