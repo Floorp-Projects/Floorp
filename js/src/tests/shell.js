052445d477a1b41c8fd22d63fa213c1b9cde32d3
Benjamin Peterson <benjamin@python.org>
2012-07-05 21:08:02 -0400

    Bug 771281 - Remove js shell workers. r=jorendorff

--- js/src/tests/shell.js
+++ js/src/tests/shell.js
@@ -797,18 +797,6 @@ function getFailedCases() {
   }
 }
 
-var JSTest = {
-  waitForExplicitFinish: function () {
-    gDelayTestDriverEnd = true;
-  },
-
-  testFinished: function () {
-    gDelayTestDriverEnd = false;
-    jsTestDriverEnd();
-    quit();
-  }
-};
-
 function jsTestDriverEnd()
 {
   // gDelayTestDriverEnd is used to
