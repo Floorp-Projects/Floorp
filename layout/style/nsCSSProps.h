eb2dd03221e9840911f85e3b325cac8dd3b83d62
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2012-07-07 10:27:08 -0400

    Bug 752187 - Part 9: Implement unprefixed radial-gradient parsing. r=dbaron

--- layout/style/nsCSSProps.h
+++ layout/style/nsCSSProps.h
@@ -382,6 +382,7 @@ public:
   static const PRInt32 kPositionKTable[];
   static const PRInt32 kRadialGradientShapeKTable[];
   static const PRInt32 kRadialGradientSizeKTable[];
+  static const PRInt32 kRadialGradientLegacySizeKTable[];
   static const PRInt32 kResizeKTable[];
   static const PRInt32 kSpeakKTable[];
   static const PRInt32 kSpeakHeaderKTable[];
