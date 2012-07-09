2547c51f5a901c311c70b33cba911ac21dc59bef
Olli Pettay <Olli.Pettay@helsinki.fi>
2012-07-09 21:24:58 +0300

    Bug 770684, make sure chrome DOMParser doesn't load external resources, r=bz

--- content/base/src/nsDOMParser.cpp
+++ content/base/src/nsDOMParser.cpp
@@ -69,9 +69,6 @@ nsDOMParser::ParseFromString(const PRUnichar *str,
     rv = SetUpDocument(DocumentFlavorHTML, getter_AddRefs(domDocument));
     NS_ENSURE_SUCCESS(rv, rv);
     nsCOMPtr<nsIDocument> document = do_QueryInterface(domDocument);
-    nsDependentString sourceBuffer(str);
-    rv = nsContentUtils::ParseDocumentHTML(sourceBuffer, document, false);
-    NS_ENSURE_SUCCESS(rv, rv);
 
     // Keep the XULXBL state, base URL and principal setting in sync with the
     // XML case
@@ -84,6 +81,10 @@ nsDOMParser::ParseFromString(const PRUnichar *str,
     document->SetBaseURI(mBaseURI);
     // And the right principal
     document->SetPrincipal(mPrincipal);
+
+    nsDependentString sourceBuffer(str);
+    rv = nsContentUtils::ParseDocumentHTML(sourceBuffer, document, false);
+    NS_ENSURE_SUCCESS(rv, rv);
 
     domDocument.forget(aResult);
     return rv;
