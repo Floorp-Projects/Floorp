65f062c239871a7c28df6edfcb0f963acfedbaaa
Margaret Leibovic <margaret.leibovic@gmail.com>
2012-07-09 15:05:53 -0700

    Bug 768073 - Fix doorhanger arrow alignment on tablets. r=sriram

--- mobile/android/base/DoorHangerPopup.java
+++ mobile/android/base/DoorHangerPopup.java
@@ -8,6 +8,7 @@ package org.mozilla.gecko;
 import java.util.HashMap;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.Log;
 import android.view.Gravity;
@@ -15,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
 import android.widget.PopupWindow;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -29,11 +31,15 @@ public class DoorHangerPopup extends PopupWindow {
     private LinearLayout mContent;
 
     private boolean mInflated; 
+    private ImageView mArrow;
+    private int mArrowWidth;
 
     public DoorHangerPopup(Context aContext) {
         super(aContext);
         mContext = aContext;
+
         mInflated = false;
+        mArrowWidth = aContext.getResources().getDimensionPixelSize(R.dimen.doorhanger_arrow_width);
    }
 
     private void init() {
@@ -44,6 +50,7 @@ public class DoorHangerPopup extends PopupWindow {
 
         LayoutInflater inflater = LayoutInflater.from(mContext);
         RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.doorhangerpopup, null);
+        mArrow = (ImageView) layout.findViewById(R.id.doorhanger_arrow);
         mContent = (LinearLayout) layout.findViewById(R.id.doorhanger_container);
         
         setContentView(layout);
@@ -137,10 +144,18 @@ public class DoorHangerPopup extends PopupWindow {
     public void showPopup(View v) {
         fixBackgroundForFirst();
 
-        if (isShowing())
+        if (isShowing()) {
             update();
-        else
-            showAsDropDown(v);
+            return;
+        }
+
+        // On tablets, we need to position the popup so that the center of the arrow points to the
+        // center of the anchor view. On phones the popup stretches across the entire screen, so the
+        // arrow position is determined by its left margin.
+        int offset = GeckoApp.mAppContext.isTablet() ? v.getWidth()/2 - mArrowWidth/2 -
+                     ((RelativeLayout.LayoutParams) mArrow.getLayoutParams()).leftMargin : 0;
+
+        showAsDropDown(v, offset, 0);
     }
 
     private void fixBackgroundForFirst() {
