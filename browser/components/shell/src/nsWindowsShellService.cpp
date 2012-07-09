0a13fff84d048016ee39f82e4c8b3717a52e562d
Brian R. Bondy <netzen@gmail.com>
2012-07-09 19:00:03 -0400

    Bug 770899 - Prefetch clearing should be repurposed to delete 0 sized read only .pf files only. r=taras

--- browser/components/shell/src/nsWindowsShellService.cpp
+++ browser/components/shell/src/nsWindowsShellService.cpp
@@ -194,8 +194,6 @@ static SETTING gDDESettings[] = {
   { MAKE_KEY_NAME1("Software\\Classes\\HTTPS", SOD) }
 };
 
-// See Bug 770883
-#if 0
 #if defined(MOZ_MAINTENANCE_SERVICE)
 
 #define ONLY_SERVICE_LAUNCHING
@@ -205,7 +203,6 @@ static SETTING gDDESettings[] = {
 static const char *kPrefetchClearedPref =
   "app.update.service.lastVersionPrefetchCleared";
 static nsCOMPtr<nsIThread> sThread;
-#endif
 #endif
 
 nsresult
@@ -1004,8 +1001,6 @@ nsWindowsShellService::SetDesktopBackgroundColor(PRUint32 aColor)
 nsWindowsShellService::nsWindowsShellService() : 
   mCheckedThisSession(false) 
 {
-// See Bug 770883
-#if 0
 #if defined(MOZ_MAINTENANCE_SERVICE)
 
   // Check to make sure the service is installed
@@ -1050,13 +1045,10 @@ nsWindowsShellService::nsWindowsShellService() :
       nsnull, CLEAR_PREFETCH_TIMEOUT_MS, nsITimer::TYPE_ONE_SHOT);
   }
 #endif
-#endif
 }
 
 nsWindowsShellService::~nsWindowsShellService()
 {
-// See Bug 770883
-#if 0
 #if defined(MOZ_MAINTENANCE_SERVICE)
  if (mTimer) {
     mTimer->Cancel();
@@ -1067,11 +1059,8 @@ nsWindowsShellService::~nsWindowsShellService()
     sThread = nsnull;
   }
 #endif
-#endif
 }
 
-// See Bug 770883
-#if 0
 #if defined(MOZ_MAINTENANCE_SERVICE)
 
 class ClearPrefetchEvent : public nsRunnable {
@@ -1094,7 +1083,6 @@ public:
   }
 };
 #endif
-#endif
 
 /**
  * For faster startup we attempt to clear the prefetch if the maintenance
@@ -1105,8 +1093,6 @@ public:
  * This is done on every update but also there is a one time operation done
  * from within the program for first time installs.
  */ 
-// See Bug 770883
-#if 0
 #if defined(MOZ_MAINTENANCE_SERVICE)
 void
 nsWindowsShellService::LaunchPrefetchClearCommand(nsITimer *aTimer, void*)
@@ -1130,7 +1116,6 @@ nsWindowsShellService::LaunchPrefetchClearCommand(nsITimer *aTimer, void*)
     sThread->Dispatch(prefetchEvent, NS_DISPATCH_NORMAL);
   }
 }
-#endif
 #endif
 
 NS_IMETHODIMP
