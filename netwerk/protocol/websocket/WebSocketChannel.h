3161d97ddf57ea6a94cc7b0a6c9b1497a27345f1
Jason Duell <jduell.mcbugs@gmail.com>
2012-07-09 18:20:25 -0700

    Bug 771318 - Fix early websocket Init fail. r=mcmanus, r=smaug

--- netwerk/protocol/websocket/WebSocketChannel.h
+++ netwerk/protocol/websocket/WebSocketChannel.h
@@ -198,7 +198,8 @@ private:
   PRUint32                        mAutoFollowRedirects       : 1;
   PRUint32                        mReleaseOnTransmit         : 1;
   PRUint32                        mTCPClosed                 : 1;
-  PRUint32                        mChannelWasOpened          : 1;
+  PRUint32                        mWasOpened                 : 1;
+  PRUint32                        mOpenedHttpChannel         : 1;
   PRUint32                        mDataStarted               : 1;
   PRUint32                        mIncrementedSessionCount   : 1;
   PRUint32                        mDecrementedSessionCount   : 1;
