6ffc4cf10b81ce897e5221a8a5ede25f023dc000
Ed Morley <emorley@mozilla.com>
2012-07-10 12:41:50 +0100

    Backout 751aa72f61a2 (bug 729495) for robocop testAllPagesTab failures

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -189,7 +189,7 @@ abstract public class BrowserApp extends GeckoApp
 
     @Override
     protected void loadRequest(String url, AwesomeBar.Target target, String searchEngine, boolean userEntered) {
-        mBrowserToolbar.setTitle(StringUtils.prettyURL(url));
+        mBrowserToolbar.setTitle(url);
         super.loadRequest(url, target, searchEngine, userEntered);
     }
 
@@ -250,7 +250,7 @@ abstract public class BrowserApp extends GeckoApp
         }
 
         if (uri != null && uri.length() > 0) {
-            mBrowserToolbar.setTitle(StringUtils.prettyURL(uri));
+            mBrowserToolbar.setTitle(uri);
         }
 
         if (!isExternalURL) {
