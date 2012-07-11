46e45a7481ca3f757c1949b43fa2f299b6389ed5
Brian Smith <bsmith@mozilla.com>
2012-07-10 19:42:50 -0700

    Bug 758314 - Allow end-user to override error when MD5 cert is encountered, r=kaie

--- security/manager/pki/resources/content/viewCertDetails.js
+++ security/manager/pki/resources/content/viewCertDetails.js
@@ -216,6 +216,8 @@ function DisplayVerificationData(cert, result)
     verifystr = bundle.GetStringFromName('certNotVerified_IssuerUnknown');
   } else if (verifystate == cert.INVALID_CA) {
     verifystr = bundle.GetStringFromName('certNotVerified_CAInvalid');
+  } else if (verifystate == cert.SIGNATURE_ALGORITHM_DISABLED) {
+    verifystr = bundle.GetStringFromName('certNotVerified_AlgorithmDisabled');
   } else { /* if (verifystate == cert.NOT_VERIFIED_UNKNOWN || == USAGE_NOT_ALLOWED) */
     verifystr = bundle.GetStringFromName('certNotVerified_Unknown');
   }
