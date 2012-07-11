d5cdea24052192d5810f2e1accb5da29e27ff9a5
Terrence Cole <terrence@mozilla.com>
2012-07-11 13:49:51 -0700

    Bug 772722 - Remove superfluous usage of Atomics in SpiderMonkey; r=luke
    None of the current usages of ATOMIC_INC/DEC are useful now that we do not allow
    multi-threaded runtimes. This also removes a pre-mature optimization that
    happened to be using the atomics. I measured it as saving strictly < 3us on old
    hardware.

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -4563,14 +4563,14 @@ JS_CheckAccess(JSContext *cx, JSObject *obj, jsid id_, JSAccessMode mode,
 JS_PUBLIC_API(void)
 JS_HoldPrincipals(JSPrincipals *principals)
 {
-    JS_ATOMIC_INCREMENT(&principals->refcount);
+    ++principals->refcount;
 }
 
 JS_PUBLIC_API(void)
 JS_DropPrincipals(JSRuntime *rt, JSPrincipals *principals)
 {
-    int rc = JS_ATOMIC_DECREMENT(&principals->refcount);
-    if (rc == 0)
+    JS_AbortIfWrongThread(rt);
+    if (--principals->refcount == 0)
         rt->destroyPrincipals(principals);
 }
 
