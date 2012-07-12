7c181001da9fd93baf8117bb18b5f916a35edb42
Mike Hommey <mh+mozilla@glandium.org>
2012-07-12 08:30:58 +0200

    Bug 762864 - Ship webapprt locale files in webapprt chrome. r=bsmedberg

--- config/JarMaker.py
+++ config/JarMaker.py
@@ -278,7 +278,7 @@ class JarMaker(object):
     # chromebasepath is used for chrome registration manifests
     # %s is getting replaced with chrome/ for chrome.manifest, and with
     # an empty string for jarfile.manifest
-    chromebasepath = '%s' + jarfile
+    chromebasepath = '%s' + os.path.basename(jarfile)
     if self.outputFormat == 'jar':
       chromebasepath = 'jar:' + chromebasepath + '.jar!'
     chromebasepath += '/'
