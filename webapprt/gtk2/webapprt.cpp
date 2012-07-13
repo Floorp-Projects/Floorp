f3b128e710c3942f5dd4143362888508d6a146a1
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2012-07-13 11:27:49 -0700

    Bug 768768 - Launch app from shell, close, then launch it from terminal results in no icon showing up in task bar in ubuntu; r=karlt

--- webapprt/gtk2/webapprt.cpp
+++ webapprt/gtk2/webapprt.cpp
@@ -139,7 +139,7 @@ bool GRELoadAndLaunch(const char* firefoxDir, const char* profile)
   // DE can match our window to the correct launcher
   char programClass[MAXPATHLEN];
   snprintf(programClass, MAXPATHLEN, "owa-%s", profile);
-  gdk_set_program_class(programClass);
+  g_set_prgname(programClass);
 
   // NOTE: The GRE has successfully loaded, so we can use XPCOM now
   { // Scope for any XPCOM stuff we create
