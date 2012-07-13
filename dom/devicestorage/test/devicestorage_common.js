b9e3a33a23109bc69cdb706ef9a0a0c998b6db01
Doug Turner <dougt@dougt.org>
2012-07-13 12:35:33 -0700

    Bug 773713 - Stop using profile directory for Device Storage testings. r=jmaher

--- dom/devicestorage/test/devicestorage_common.js
+++ dom/devicestorage/test/devicestorage_common.js
@@ -13,6 +13,17 @@ Array.prototype.remove = function(from, to) {
 };
 
 function devicestorage_setup() {
+
+  // ensure that the directory we are writing into is empty
+  try {
+    const Cc = SpecialPowers.wrap(Components).classes;
+    const Ci = Components.interfaces;
+    var directoryService = Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties);
+    var f = directoryService.get("TmpD", Ci.nsIFile);
+    f.appendRelativePath("device-storage-testing");
+    f.remove(true);
+  } catch(e) {}
+
   SimpleTest.waitForExplicitFinish();
   if (SpecialPowers.isMainProcess()) {
     try {
