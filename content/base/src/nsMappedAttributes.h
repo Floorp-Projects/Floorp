5e15276248d0a7693afb8cd1796ab26d78bffda2
L. David Baron <dbaron@dbaron.org>
2012-07-13 18:01:25 -0700

    Back out changeset d49beb57db23 (bug 558516) for causing all Mac builds to crash in nsXULElement::GetAttrValue and for causing a single mochitest-1 failure.

--- content/base/src/nsMappedAttributes.h
+++ content/base/src/nsMappedAttributes.h
@@ -34,7 +34,6 @@ public:
 
   nsresult SetAndTakeAttr(nsIAtom* aAttrName, nsAttrValue& aValue);
   const nsAttrValue* GetAttr(nsIAtom* aAttrName) const;
-  const nsAttrValue* GetAttr(const nsAString& aAttrName) const;
 
   PRUint32 Count() const
   {
@@ -68,7 +67,7 @@ public:
   // aValue; any value that was already in aValue is destroyed.
   void RemoveAttrAt(PRUint32 aPos, nsAttrValue& aValue);
   const nsAttrName* GetExistingAttrNameFromQName(const nsAString& aName) const;
-  PRInt32 IndexOfAttr(nsIAtom* aLocalName) const;
+  PRInt32 IndexOfAttr(nsIAtom* aLocalName, PRInt32 aNamespaceID) const;
   
 
   // nsIStyleRule 
