2fe6de024a6f82b5f1c9197e9043a12bb0914a55
Randell Jesup <rjesup@jesup.org>
2012-07-14 00:01:09 -0400

    Bug 773151: provide back-compatibility for external API code using nsCAutoString CLOSED TREE r=bsmedberg

--- xpcom/glue/nsStringAPI.h
+++ xpcom/glue/nsStringAPI.h
@@ -1435,4 +1435,7 @@ typedef nsString nsAutoString;
 NS_HIDDEN_(bool) ParseString(const nsACString& aAstring, char aDelimiter, 
                                nsTArray<nsCString>& aArray);
 
+/* for back compatibility for pre-name-change code */
+typedef nsAutoCString nsCAutoString;
+
 #endif // nsStringAPI_h__
