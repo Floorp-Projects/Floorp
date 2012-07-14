8f239cbc46758e3ffe58666223742082cb5658e9
Chris Lord <chrislord.net@gmail.com>
2012-07-14 08:49:05 +0100

    Bug 772079 - Fix ThebesLayerInvalidRegion being destroyed too soon. r=roc
    A comment in ApplyThebesLayerInvalidation says that it preserves the content
    of ThebesLayerInvalidRegion, in case there are multiple container layers for
    the same frame. SetHasContainerLayer, however, immediately clears said property.
    
    This was causing invalidations to be lost since Bug 758620 on fixed-position
    elements, as they were being separated out onto their own layers but were still
    merged in the root scroll layer. This is tracked in Bug 769541.
    
    This fixes the problem by storing the new invalid region in DisplayItemDataEntry
    and clearing/setting the ThebesLayerInvalidRegion property in the
    UpdateDisplayItemData callback from FrameLayerBuilder::WillEndTransaction.

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -1925,7 +1925,7 @@ public:
   virtual already_AddRefed<Layer> BuildLayer(nsDisplayListBuilder* aBuilder,
                                              LayerManager* aManager,
                                              const ContainerParameters& aContainerParameters);
-  NS_DISPLAY_DECL_NAME("FixedPosition", TYPE_OWN_LAYER)
+  NS_DISPLAY_DECL_NAME("FixedPosition", TYPE_FIXED_POSITION)
 };
 
 /**
