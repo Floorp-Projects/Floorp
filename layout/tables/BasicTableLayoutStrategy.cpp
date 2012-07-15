6502be8178a0b8d9b2968be416552dc3275e7945
Rafael Ávila de Espíndola <respindola@mozilla.com>
2012-07-15 00:20:24 -0400

    Bug 774040 - remove unused field. r=roc.

--- layout/tables/BasicTableLayoutStrategy.cpp
+++ layout/tables/BasicTableLayoutStrategy.cpp
@@ -257,7 +257,7 @@ BasicTableLayoutStrategy::ComputeColumnIntrinsicWidths(nsRenderingContext* aRend
     nsTableCellMap *cellMap = tableFrame->GetCellMap();
 
     mozilla::AutoStackArena arena;
-    SpanningCellSorter spanningCells(tableFrame->PresContext()->PresShell());
+    SpanningCellSorter spanningCells;
 
     // Loop over the columns to consider the columns and cells *without*
     // a colspan.
