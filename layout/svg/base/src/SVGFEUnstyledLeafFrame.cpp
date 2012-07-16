3762a2e29318a2f4f30febef774ae8c9af367711
Jonathan Watt <jwatt@jwatt.org>
2012-07-15 20:42:50 -0400

    Bug 774087 - Mark SVG filter primitive frame classes as NS_STATE_SVG_NONDISPLAY_CHILD. r=dholbert.

--- layout/svg/base/src/SVGFEUnstyledLeafFrame.cpp
+++ layout/svg/base/src/SVGFEUnstyledLeafFrame.cpp
@@ -16,7 +16,11 @@ class SVGFEUnstyledLeafFrame : public SVGFEUnstyledLeafFrameBase
   friend nsIFrame*
   NS_NewSVGFEUnstyledLeafFrame(nsIPresShell* aPresShell, nsStyleContext* aContext);
 protected:
-  SVGFEUnstyledLeafFrame(nsStyleContext* aContext) : SVGFEUnstyledLeafFrameBase(aContext) {}
+  SVGFEUnstyledLeafFrame(nsStyleContext* aContext)
+    : SVGFEUnstyledLeafFrameBase(aContext)
+  {
+    AddStateBits(NS_STATE_SVG_NONDISPLAY_CHILD);
+  }
 
 public:
   NS_DECL_FRAMEARENA_HELPERS
