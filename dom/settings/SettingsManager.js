ba1ad0f277018bd4f034d0447cd655ce3f11f6ef
Mounir Lamouri <mounir.lamouri@gmail.com>
2012-07-16 08:44:52 -0700

    Bug 769621 - Checking Contacts, Settings, Wifi and WebApps permission should use principal instead of URI. r=sicking

--- dom/settings/SettingsManager.js
+++ dom/settings/SettingsManager.js
@@ -321,8 +321,12 @@ SettingsManager.prototype = {
 
     let principal = aWindow.document.nodePrincipal;
     let secMan = Cc["@mozilla.org/scriptsecuritymanager;1"].getService(Ci.nsIScriptSecurityManager);
-    let readPerm = principal == secMan.getSystemPrincipal() ? Ci.nsIPermissionManager.ALLOW_ACTION : Services.perms.testExactPermission(principal.URI, "websettings-read");
-    let readwritePerm = principal == secMan.getSystemPrincipal() ? Ci.nsIPermissionManager.ALLOW_ACTION : Services.perms.testExactPermission(principal.URI, "websettings-readwrite");
+    let readPerm = principal == secMan.getSystemPrincipal()
+                     ? Ci.nsIPermissionManager.ALLOW_ACTION
+                     : Services.perms.testExactPermissionFromPrincipal(principal, "websettings-read");
+    let readwritePerm = principal == secMan.getSystemPrincipal()
+                          ? Ci.nsIPermissionManager.ALLOW_ACTION
+                          : Services.perms.testExactPermissionFromPrincipal(principal, "websettings-readwrite");
     this.hasReadPrivileges = readPerm == Ci.nsIPermissionManager.ALLOW_ACTION;
     this.hasReadWritePrivileges = readwritePerm == Ci.nsIPermissionManager.ALLOW_ACTION;
     debug("has read privileges :" + this.hasReadPrivileges + ", has read-write privileges: " + this.hasReadWritePrivileges);
