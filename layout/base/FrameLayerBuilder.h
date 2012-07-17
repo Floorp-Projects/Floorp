2a7d54c02e393e65d9bbec92d87bb2cc88193967
Matt Woodrow <mwoodrow@mozilla.com>
2012-07-17 13:03:51 -0400

    Bug 539356 - Part 7 - Store FrameLayerBuilder objects on the LayerManager instead of nsDisplayListBuilder. r=roc

--- layout/base/FrameLayerBuilder.h
+++ layout/base/FrameLayerBuilder.h
@@ -21,6 +21,8 @@ class nsRootPresContext;
 
 namespace mozilla {
 
+class FrameLayerBuilder;
+
 enum LayerState {
   LAYER_NONE,
   LAYER_INACTIVE,
@@ -33,6 +35,35 @@ enum LayerState {
   // Inactive style layer for rendering SVG effects.
   LAYER_SVG_EFFECTS
 };
+
+class LayerManagerLayerBuilder : public layers::LayerUserData {
+public:
+  LayerManagerLayerBuilder(FrameLayerBuilder* aBuilder, bool aDelete = true)
+    : mLayerBuilder(aBuilder)
+    , mDelete(aDelete)
+  {
+    MOZ_COUNT_CTOR(LayerManagerLayerBuilder);
+  }
+  ~LayerManagerLayerBuilder();
+
+  FrameLayerBuilder* mLayerBuilder;
+  bool mDelete;
+};
+
+extern PRUint8 gLayerManagerLayerBuilder;
+
+class ContainerLayerPresContext : public layers::LayerUserData {
+public:
+  nsPresContext* mPresContext;
+};
+
+extern PRUint8 gContainerLayerPresContext;
+
+static inline FrameLayerBuilder *GetLayerBuilderForManager(layers::LayerManager* aManager)
+{
+  LayerManagerLayerBuilder *data = static_cast<LayerManagerLayerBuilder*>(aManager->GetUserData(&gLayerManagerLayerBuilder));
+  return data ? data->mLayerBuilder : nsnull;
+}
 
 class RefCountedRegion : public RefCounted<RefCountedRegion> {
 public:
@@ -90,8 +121,13 @@ public:
     mDetectedDOMModification(false),
     mInvalidateAllLayers(false)
   {
+    MOZ_COUNT_CTOR(FrameLayerBuilder);
     mNewDisplayItemData.Init();
     mThebesLayerItems.Init();
+  }
+  ~FrameLayerBuilder()
+  {
+    MOZ_COUNT_DTOR(FrameLayerBuilder);
   }
 
   static void Shutdown();
@@ -239,7 +275,7 @@ public:
    * Dumps this FrameLayerBuilder's retained layer manager's retained
    * layer tree to stderr.
    */
-  void DumpRetainedLayerTree(FILE* aFile = stdout);
+  static void DumpRetainedLayerTree(LayerManager* aManager, FILE* aFile = stdout);
 #endif
 
   /******* PRIVATE METHODS to FrameLayerBuilder.cpp ********/
@@ -276,6 +312,7 @@ public:
    */
   Layer* GetOldLayerFor(nsIFrame* aFrame, PRUint32 aDisplayItemKey);
 
+  static Layer* GetDebugOldLayerFor(nsIFrame* aFrame, PRUint32 aDisplayItemKey);
   /**
    * Try to determine whether the ThebesLayer aLayer paints an opaque
    * single color everywhere it's visible in aRect.
