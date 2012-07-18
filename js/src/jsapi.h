fe184c1b2e5ffecb21bd0b1957b2f75ac6af67f9
Ms2ger <ms2ger@gmail.com>
2012-07-18 12:36:08 +0200

    Bug 765464 - Part a: Introduce JS_ReportErrorNumberVA; r=Waldo

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -5650,6 +5650,12 @@ extern JS_PUBLIC_API(void)
 JS_ReportErrorNumber(JSContext *cx, JSErrorCallback errorCallback,
                      void *userRef, const unsigned errorNumber, ...);
 
+#ifdef va_start
+extern JS_PUBLIC_API(void)
+JS_ReportErrorNumberVA(JSContext *cx, JSErrorCallback errorCallback,
+                       void *userRef, const unsigned errorNumber, va_list ap);
+#endif
+
 /*
  * Use an errorNumber to retrieve the format string, args are jschar *
  */
