b9d3334c84ba7d61289c6c58ff4df835226be293
Ehsan Akhgari <ehsan@mozilla.com>
2012-07-18 12:19:20 -0400

    Backout changeset f0be368bb172 (bug 773100) for broken builds across the board

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -2305,7 +2305,7 @@ InternalInvalidateThebesLayersInSubtree(nsIFrame* aFrame)
   if (aFrame->GetStateBits() & NS_FRAME_HAS_CONTAINER_LAYER) {
     // Delete the invalid region to indicate that all Thebes contents
     // need to be invalidated
-    InvalidateThebesLayerContents(aFrame, aFrame->GetVisualOverflowRectRelativeToSelf());
+    aFrame->Properties().Delete(ThebesLayerInvalidRegionProperty());
     foundContainerLayer = true;
   }
 
