c0be64c53c53907380aa4997fb957b5bfd16b339
Jeff Gilbert <jgilbert@mozilla.com>
2012-07-18 15:23:47 -0700

    Backed out changeset d4a04cc38326

--- gfx/gl/GLContext.h
+++ gfx/gl/GLContext.h
@@ -1534,7 +1534,6 @@ public:
         ARB_sync,
         OES_EGL_image,
         OES_EGL_sync,
-        OES_EGL_image_external,
         Extensions_Max
     };
 
@@ -3083,10 +3082,10 @@ public:
      }
 
      // OES_EGL_image (GLES)
-     void fEGLImageTargetTexture2D(GLenum target, GLeglImage image)
+     void fImageTargetTexture2D(GLenum target, GLeglImage image)
      {
          BEFORE_GL_CALL;
-         mSymbols.fEGLImageTargetTexture2D(target, image);
+         mSymbols.fImageTargetTexture2D(target, image);
          AFTER_GL_CALL;
      }
 
