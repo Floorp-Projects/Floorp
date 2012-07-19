767aeb6bc1965c44b8724e643894538dcc260ae6
Nicholas Nethercote <nnethercote@mozilla.com>
2012-07-18 17:38:10 -0700

    Bug 647367 - Sequester jshash.{h,cpp} in js/jsd/.  r=luke.

--- js/src/jsscope.cpp
+++ js/src/jsscope.cpp
@@ -24,6 +24,7 @@
 #include "jsscope.h"
 #include "jsstr.h"
 
+#include "js/HashTable.h"
 #include "js/MemoryMetrics.h"
 
 #include "jsatominlines.h"
@@ -145,7 +146,7 @@ Shape::hashify(JSContext *cx)
 Shape **
 ShapeTable::search(jsid id, bool adding)
 {
-    JSHashNumber hash0, hash1, hash2;
+    js::HashNumber hash0, hash1, hash2;
     int sizeLog2;
     Shape *stored, *shape, **spp, **firstRemoved;
     uint32_t sizeMask;
