7dcbbb5a6c5003cb3452e2acca4ee4edc8a65469
Philipp von Weitershausen <philipp@weitershausen.de>
2012-07-19 16:56:57 -0700

    Bug 775359 - B2G 3G: Keep track of data call state. r=marshall_law

--- dom/system/gonk/RadioInterfaceLayer.js
+++ dom/system/gonk/RadioInterfaceLayer.js
@@ -346,7 +346,7 @@ RadioInterfaceLayer.prototype = {
         this.handleSmsSendFailed(message);
         return;
       case "datacallstatechange":
-        this.handleDataCallState(message.datacall);
+        this.handleDataCallState(message);
         break;
       case "datacalllist":
         this.handleDataCallList(message);
@@ -470,7 +470,7 @@ RadioInterfaceLayer.prototype = {
       voiceInfo.signalStrength = null;
       voiceInfo.relSignalStrength = null;
       ppmm.sendAsyncMessage("RIL:VoiceInfoChanged", voiceInfo);
-      return;
+      return false;
     }
 
     let isRoaming = regState == RIL.NETWORK_CREG_STATE_REGISTERED_ROAMING;
@@ -525,7 +525,7 @@ RadioInterfaceLayer.prototype = {
       data.signalStrength = null;
       data.relSignalStrength = null;
       ppmm.sendAsyncMessage("RIL:DataInfoChanged", data);
-      return;
+      return false;
     }
     data.roaming =
       (state.regState == RIL.NETWORK_CREG_STATE_REGISTERED_ROAMING);
