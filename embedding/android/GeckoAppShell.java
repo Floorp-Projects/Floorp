07d068b18cdda2441266e086001147dd644eac02
James Willcox <jwillcox@mozilla.com>
2012-07-19 17:20:19 -0400

    Bug 687267 - Fix XUL bustage r=blassey

--- embedding/android/GeckoAppShell.java
+++ embedding/android/GeckoAppShell.java
@@ -102,6 +102,7 @@ public class GeckoAppShell
     public static native void notifyListCreated(int aListId, int aMessageId, String aReceiver, String aSender, String aBody, long aTimestamp, int aRequestId, long aProcessId);
     public static native void notifyGotNextMessage(int aMessageId, String aReceiver, String aSender, String aBody, long aTimestamp, int aRequestId, long aProcessId);
     public static native void notifyReadingMessageListFailed(int aError, int aRequestId, long aProcessId);
+    public static native void onSurfaceTextureFrameAvailable(SurfaceTexture surfaceTexture, int id);
 
     // A looper thread, accessed by GeckoAppShell.getHandler
     private static class LooperThread extends Thread {
