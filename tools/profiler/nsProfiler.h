fc6b30fa6d14aa1e14bb7b6ba24f6a5e90d6eb76
Ehsan Akhgari <ehsan@mozilla.com>
2012-07-19 00:42:48 -0400

    Bug 758992 - Make the classes which use the XPCOM nsISupports implementation macros final, to avoid the warning about deleting using a pointer to a base class with virtual functions and no virtual dtor (tools/profiler parts); blanket-r=bzbarsky

--- tools/profiler/nsProfiler.h
+++ tools/profiler/nsProfiler.h
@@ -7,8 +7,9 @@
 #define _NSPROFILER_H_
 
 #include "nsIProfiler.h"
+#include "mozilla/Attributes.h"
 
-class nsProfiler : public nsIProfiler
+class nsProfiler MOZ_FINAL : public nsIProfiler
 {
 public:
     nsProfiler();
