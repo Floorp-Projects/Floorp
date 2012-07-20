5bb9ba116b4c68a208f14b4c85a1fd3ba3bdee32
Michael Comella <michael.l.comella@gmail.com>
2012-07-19 21:08:40 -0400

    Bug 715179 - (4 of 8) Created the full FontSizePreference dialog UI. This includes the font preview box and the increase/decrease font size buttons. These buttons do not function yet. r=bnicholson

--- mobile/android/base/FontSizePreference.java
+++ mobile/android/base/FontSizePreference.java
@@ -5,12 +5,42 @@
 
 package org.mozilla.gecko;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.res.Resources;
 import android.preference.DialogPreference;
+import android.text.method.ScrollingMovementMethod;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 class FontSizePreference extends DialogPreference {
+    private static final String LOGTAG = "FontSizePreference";
+
+    private final Context mContext;
+    private TextView mPreviewFontView;
+    private Button mIncreaseFontButton;
+    private Button mDecreaseFontButton;
+
     public FontSizePreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
+        final LayoutInflater inflater =
+            (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View dialogView = inflater.inflate(R.layout.font_size_preference, null);
+        mPreviewFontView = (TextView) dialogView.findViewById(R.id.preview);
+        mPreviewFontView.setMovementMethod(new ScrollingMovementMethod());
+
+        mDecreaseFontButton = (Button) dialogView.findViewById(R.id.decrease_preview_font_button);
+        mIncreaseFontButton = (Button) dialogView.findViewById(R.id.increase_preview_font_button);
+
+        builder.setView(dialogView);
     }
 }
