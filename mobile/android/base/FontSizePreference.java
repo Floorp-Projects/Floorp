7499fff5b7d4d7eaa95454f284b2357c5674e9fb
Michael Comella <michael.l.comella@gmail.com>
2012-07-19 21:08:40 -0400

    Bug 715179 - (5 of 8) The buttons on the FontSizePreference dialog are interactive - they can be used to increase and decrease preview font size. r=bnicholson

--- mobile/android/base/FontSizePreference.java
+++ mobile/android/base/FontSizePreference.java
@@ -12,6 +12,7 @@ import android.preference.DialogPreference;
 import android.text.method.ScrollingMovementMethod;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -19,15 +20,26 @@ import android.widget.TextView;
 
 class FontSizePreference extends DialogPreference {
     private static final String LOGTAG = "FontSizePreference";
+    private static final int TWIP_TO_PT_RATIO = 20; // 20 twip = 1 point.
+    private static final int PREVIEW_FONT_SIZE_UNIT = TypedValue.COMPLEX_UNIT_PT;
+    private static final int DEFAULT_FONT_INDEX = 2;
 
     private final Context mContext;
     private TextView mPreviewFontView;
     private Button mIncreaseFontButton;
     private Button mDecreaseFontButton;
 
+    private final String[] mFontTwipValues;
+    /** Index into the above arrays for the saved preference value (from Gecko). */
+    private int mSavedFontIndex = DEFAULT_FONT_INDEX;
+    /** Index into the above arrays for the currently displayed font size (the preview). */
+    private int mPreviewFontIndex = mSavedFontIndex;
     public FontSizePreference(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
+
+        final Resources res = mContext.getResources();
+        mFontTwipValues = res.getStringArray(R.array.pref_font_size_values);
     }
 
     @Override
@@ -40,7 +52,41 @@ class FontSizePreference extends DialogPreference {
 
         mDecreaseFontButton = (Button) dialogView.findViewById(R.id.decrease_preview_font_button);
         mIncreaseFontButton = (Button) dialogView.findViewById(R.id.increase_preview_font_button);
+        mDecreaseFontButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                updatePreviewFontSize(mFontTwipValues[--mPreviewFontIndex]);
+                mIncreaseFontButton.setEnabled(true);
+                // If we reached the minimum index, disable the button.
+                if (mPreviewFontIndex == 0) {
+                    mDecreaseFontButton.setEnabled(false);
+                }
+            }
+        });
+        mIncreaseFontButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                updatePreviewFontSize(mFontTwipValues[++mPreviewFontIndex]);
+        
+                mDecreaseFontButton.setEnabled(true);
+                // If we reached the maximum index, disable the button.
+                if (mPreviewFontIndex == mFontTwipValues.length - 1) {
+                    mIncreaseFontButton.setEnabled(false);
+                }
+            }
+        });
 
         builder.setView(dialogView);
+    }
+
+    /**
+     * Updates the mPreviewFontView to the given text size and forces redraw. Does not update the
+     * font indicies.
+     */
+    private void updatePreviewFontSize(String twip) {
+        mPreviewFontView.setTextSize(PREVIEW_FONT_SIZE_UNIT, convertTwipStrToPT(twip));
+        mPreviewFontView.invalidate();
+    }
+
+    private float convertTwipStrToPT(String twip) {
+        return Float.parseFloat(twip) / TWIP_TO_PT_RATIO; 
     }
 }
