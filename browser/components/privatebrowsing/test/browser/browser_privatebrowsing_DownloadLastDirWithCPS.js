ccc9eae1fc7fd2aeafa930fe2986386a040fb72e
Saurabh Anand <saurabhanandiit@gmail.com>
2012-07-21 11:24:41 +0530

    Bug 722995 - Part 5: Make the tests compatible to the code changes, r=ehsan

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_DownloadLastDirWithCPS.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_DownloadLastDirWithCPS.js
@@ -3,10 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-Cu.import("resource://gre/modules/DownloadLastDir.jsm");
+let downloadModule = {};
+Cu.import("resource://gre/modules/DownloadLastDir.jsm", downloadModule);
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/FileUtils.jsm");
 
+let gDownloadLastDir = new downloadModule.DownloadLastDir(window);
 let pb = Cc["@mozilla.org/privatebrowsing;1"].
          getService(Ci.nsIPrivateBrowsingService);
 
@@ -216,6 +218,5 @@ function test() {
       Services.prefs.clearUserPref("browser.download.lastDir");
       gDownloadLastDir.cleanupPrivateFile();
       delete FileUtils;
-      delete gDownloadLastDir;
   }
 }
