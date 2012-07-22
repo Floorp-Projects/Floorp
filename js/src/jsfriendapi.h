99aa02e2150171bf03c0b02553a9240230e8101e
Benjamin Peterson <benjamin@python.org>
2012-07-22 09:00:45 -0700

    Bug 776283 - Decode sources properly before handing them to the JS engine. r=bz

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -175,7 +175,7 @@ JS_END_EXTERN_C
 
 #ifdef __cplusplus
 
-typedef bool (* JS_SourceHook)(JSContext *cx, JSScript *script, char **src, uint32_t *length);
+typedef bool (* JS_SourceHook)(JSContext *cx, JSScript *script, jschar **src, uint32_t *length);
 
 extern JS_FRIEND_API(void)
 JS_SetSourceHook(JSRuntime *rt, JS_SourceHook hook);
