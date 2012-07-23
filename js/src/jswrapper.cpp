c5e4a24c7c5899143e0f79214a646ed175e4c80f
Bobby Holley <bobbyholley@gmail.com>
2012-07-23 17:57:39 +0200

    Bug 773962 - Fix windows linkage bustage. r=me

--- js/src/jswrapper.cpp
+++ js/src/jswrapper.cpp
@@ -1141,7 +1141,7 @@ js::RemapWrapper(JSContext *cx, JSObject *wobj, JSObject *newTarget)
 
 // Remap all cross-compartment wrappers pointing to |oldTarget| to point to
 // |newTarget|. All wrappers are recomputed.
-bool
+JS_FRIEND_API(bool)
 js::RemapAllWrappersForObject(JSContext *cx, JSObject *oldTarget,
                               JSObject *newTarget)
 {
