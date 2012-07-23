f1aa34ee511161bb9be0b88870c172dcc6df1481
Ehsan Akhgari <ehsan@mozilla.com>
2012-07-23 17:24:03 -0400

    Bug 758992 - Make the classes which use the XPCOM nsISupports implementation macros final, to avoid the warning about deleting using a pointer to a base class with virtual functions and no virtual dtor (gnome parts); blanket-r=bzbarsky

--- toolkit/system/gnome/nsGnomeVFSService.h
+++ toolkit/system/gnome/nsGnomeVFSService.h
@@ -7,11 +7,12 @@
 #define nsGnomeVFSService_h_
 
 #include "nsIGnomeVFSService.h"
+#include "mozilla/Attributes.h"
 
 #define NS_GNOMEVFSSERVICE_CID \
 {0x5f43022c, 0x6194, 0x4b37, {0xb2, 0x6d, 0xe4, 0x10, 0x24, 0x62, 0x52, 0x64}}
 
-class nsGnomeVFSService : public nsIGnomeVFSService
+class nsGnomeVFSService MOZ_FINAL : public nsIGnomeVFSService
 {
 public:
   NS_DECL_ISUPPORTS
