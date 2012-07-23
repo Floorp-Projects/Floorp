5cf1d86d348facda56b763fbad558aecc9f89916
Gary Kwong <gary@rumblingedge.com>
2012-07-23 14:00:36 -0700

    Backed out changeset 4ee96b33d289, r=wrongAuthor

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -1132,8 +1132,11 @@ LeaveFunction(ParseNode *fn, Parser *parser, PropertyName *funName = NULL,
              * by eval and function statements (which both flag the function as
              * having an extensible scope) or any enclosing 'with'.
              */
-            if (funtc->sc->funHasExtensibleScope() || tc->innermostWith)
+            if (funtc->sc->funHasExtensibleScope() ||
+                (outer_dn && tc->innermostWith &&
+                 outer_dn->pn_pos < tc->innermostWith->pn_pos)) {
                 DeoptimizeUsesWithin(dn, fn->pn_pos);
+            }
 
             if (!outer_dn) {
                 AtomDefnAddPtr p = tc->lexdeps->lookupForAdd(atom);
