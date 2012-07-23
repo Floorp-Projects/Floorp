280c8102d6de1274814c54a1dbe4d7d463d328da
David Mandelin <dmandelin@mozilla.com>
2012-07-23 14:33:14 -0700

    Backed out changeset d6531ef05a6f

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -180,12 +180,6 @@ struct EvalCacheLookup
     unsigned staticLevel;
     JSVersion version;
     JSCompartment *compartment;
-
-    EvalCacheLookup(JSLinearString *str, JSFunction *caller,
-                    unsigned staticLevel, JSVersion version, JSCompartment *compartment)
-        : str(str), caller(caller),
-          staticLevel(staticLevel), version(version), compartment(compartment)
-    {}
 };
 
 struct EvalCacheHashPolicy
