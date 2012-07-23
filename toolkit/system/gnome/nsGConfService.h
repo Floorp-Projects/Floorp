53639e8b26c993dfac53136b7ced775ecc693c1e
Ehsan Akhgari <ehsan@mozilla.com>
2012-07-23 17:24:03 -0400

    Bug 758992 - Make the classes which use the XPCOM nsISupports implementation macros final, to avoid the warning about deleting using a pointer to a base class with virtual functions and no virtual dtor (gnome parts); blanket-r=bzbarsky

--- toolkit/system/gnome/nsGConfService.h
+++ toolkit/system/gnome/nsGConfService.h
@@ -8,11 +8,12 @@
 
 #include "nsIGConfService.h"
 #include "gconf/gconf-client.h"
+#include "mozilla/Attributes.h"
 
 #define NS_GCONFSERVICE_CID \
 {0xd96d5985, 0xa13a, 0x4bdc, {0x93, 0x86, 0xef, 0x34, 0x8d, 0x7a, 0x97, 0xa1}}
 
-class nsGConfService : public nsIGConfService
+class nsGConfService MOZ_FINAL : public nsIGConfService
 {
 public:
   NS_DECL_ISUPPORTS
