2d3730553f0c59d9680d15b1267b0809a9ef6196
Justin Lebar <justin.lebar@gmail.com>
2012-07-23 10:44:13 -0400

    Bug 769254 - Part 1: Fix crashes in BrowserElementParent.cpp, TabChild.cpp on null URI to window-opening code. r=bz

--- dom/browser-element/BrowserElementParent.h
+++ dom/browser-element/BrowserElementParent.h
@@ -53,6 +53,8 @@ public:
    *    set aPopupTabParent's frame element to event.detail.frameElement.
    *    Otherwise, we return false.
    *
+   * @param aURL the absolute URL the new window should load.  The empty string
+   *        is allowed indicates we shouldn't load anything.
    * @param aOpenerTabParent the TabParent whose TabChild called window.open.
    * @param aPopupTabParent the TabParent inside which the opened window will
    *                        live.
@@ -75,6 +77,9 @@ public:
    *
    * (These parameter types are silly, but they match what our caller has in
    * hand.  Feel free to add an override, if they are inconvenient to you.)
+   *
+   * @param aURI the URI the new window should load.  May be null, which
+   *        indicates that we shouldn't load anything.
    */
   static bool
   OpenWindowInProcess(nsIDOMWindow* aOpenerWindow,
