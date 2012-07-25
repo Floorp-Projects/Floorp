e099952b0c7bedaefbf1c5d78e35c32fcc4bf56c
Georg Fritzsche <georg.fritzsche@googlemail.com>
2012-07-25 14:38:43 +0200

    Bug 764660 - Collect and report on CPU utilization of a hung plugin process. r=bbondy

--- dom/plugins/ipc/PluginModuleParent.h
+++ dom/plugins/ipc/PluginModuleParent.h
@@ -301,6 +301,9 @@ private:
     nsString mPluginDumpID;
     nsString mBrowserDumpID;
     nsString mHangID;
+#ifdef XP_WIN
+    float mPluginCpuUsageOnHang;
+#endif
 
 #ifdef MOZ_X11
     // Dup of plugin's X socket, used to scope its resources to this
