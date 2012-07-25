098e4596c25b46e58aa4c5f384dcd62ee63b5672
Marshall Culpepper <marshall@mozilla.com>
2012-07-25 13:49:05 -0500

    Bug 776724: Voicemail tests now import ril_consts with Cu.import, fixing breakage when packaged with Marionette. r=philikon
    --HG--
    extra : rebase_source : fe3effc50ea11ee9a4292d127150b7bf92fb7634

--- dom/telephony/test/marionette/test_voicemail_statuschanged.py
+++ dom/telephony/test/marionette/test_voicemail_statuschanged.py
@@ -5,12 +5,6 @@ class TestVoicemailStatusChanged(MarionetteTestCase):
 
     def testStatusChanged(self):
         this_dir = os.path.abspath(os.path.dirname(__file__))
-        system_gonk_dir = os.path.abspath(os.path.join(this_dir,
-            os.path.pardir, os.path.pardir, os.path.pardir, "system", "gonk"))
-
-        ril_consts_path = os.path.join(system_gonk_dir, "ril_consts.js")
-        self.marionette.import_script(ril_consts_path)
-
         pdu_builder_path = os.path.join(this_dir, "pdu_builder.js")
         self.marionette.import_script(pdu_builder_path)
 
