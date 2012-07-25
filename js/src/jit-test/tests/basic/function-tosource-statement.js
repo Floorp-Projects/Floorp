8bc653a84f57797348ba090b5b1b4e854e97ba76
Benjamin Peterson <benjamin@python.org>
2012-07-25 10:32:01 -0700

    Bug 776484: Only strip whitespace on expression bodies. r=jorendorff

--- js/src/jit-test/tests/basic/function-tosource-statement.js
+++ js/src/jit-test/tests/basic/function-tosource-statement.js
@@ -8,3 +8,6 @@ function f2(a, /* ))))pernicious comment */ b,
             d) {}
 assertEq(f2.toString(), "function f2(a, /* ))))pernicious comment */ b,\n            c, // another comment((\n            d) {}");
 assertEq(decompileBody(f2), "");
+function f3() { }
+assertEq(f3.toString(), "function f3() { }");
+assertEq(decompileBody(f3), " ");
