7a526cbcfcfbef266f3a59504a4b64d97546b909
Vicamo Yang <vyang@mozilla.com>
2012-07-26 12:11:40 +0800

    Bug 761057 - Part 2: Add required encoders for M-Send.req/M-Send.conf PDU, r=philikon

--- dom/mms/tests/test_mms_pdu_helper.js
+++ dom/mms/tests/test_mms_pdu_helper.js
@@ -81,6 +81,42 @@ add_test(function test_Address_decode() {
   run_next_test();
 });
 
+//// Address.encode ////
+
+add_test(function test_Address_encode() {
+  // Test for PLMN address
+  wsp_encode_test(MMS.Address, {address: "+123.456-789", type: "PLMN"},
+                  strToCharCodeArray("+123.456-789/TYPE=PLMN"));
+  wsp_encode_test(MMS.Address, {address: "123456789", type: "PLMN"},
+                  strToCharCodeArray("123456789/TYPE=PLMN"));
+  // Test for IPv4
+  wsp_encode_test(MMS.Address, {address: "1.2.3.4", type: "IPv4"},
+                  strToCharCodeArray("1.2.3.4/TYPE=IPv4"));
+  // Test for IPv6
+  wsp_encode_test(MMS.Address,
+    {address: "1111:AAAA:bbbb:CdEf:1ABC:2cde:3Def:0000", type: "IPv6"},
+    strToCharCodeArray("1111:AAAA:bbbb:CdEf:1ABC:2cde:3Def:0000/TYPE=IPv6")
+  );
+  // Test for other device-address
+  wsp_encode_test(MMS.Address, {address: "+H-e.l%l_o", type: "W0r1d_"},
+                  strToCharCodeArray("+H-e.l%l_o/TYPE=W0r1d_"));
+  // Test for num-shortcode
+  wsp_encode_test(MMS.Address, {address: "+123", type: "num"},
+                  strToCharCodeArray("+123"));
+  wsp_encode_test(MMS.Address, {address: "*123", type: "num"},
+                  strToCharCodeArray("*123"));
+  wsp_encode_test(MMS.Address, {address: "#123", type: "num"},
+                  strToCharCodeArray("#123"));
+  // Test for alphanum-shortcode
+  wsp_encode_test(MMS.Address, {address: "H0wD0Y0uTurnTh1s0n", type: "alphanum"},
+                  strToCharCodeArray("H0wD0Y0uTurnTh1s0n"));
+  // Test for email address
+  wsp_encode_test(MMS.Address, {address: "Joe User <joe@user.org>", type: "email"},
+                  strToCharCodeArray("Joe User <joe@user.org>"));
+
+  run_next_test();
+});
+
 //
 // Test target: HeaderField
 //
@@ -153,6 +189,20 @@ add_test(function test_ContentClassValue_decode() {
       wsp_decode_test(MMS.ContentClassValue, [i], i);
     } else {
       wsp_decode_test(MMS.ContentClassValue, [i], null, "CodeError");
+    }
+  }
+
+  run_next_test();
+});
+
+//// ContentClassValue.encode ////
+
+add_test(function test_ContentClassValue_encode() {
+  for (let i = 0; i < 256; i++) {
+    if ((i >= 128) && (i <= 135)) {
+      wsp_encode_test(MMS.ContentClassValue, i, [i]);
+    } else {
+      wsp_encode_test(MMS.ContentClassValue, i, null, "CodeError");
     }
   }
 
@@ -266,6 +316,24 @@ add_test(function test_Parameter_decodeMultiple() {
   run_next_test();
 });
 
+//// Parameter.encode ////
+
+add_test(function test_Parameter_encode() {
+  // Test for invalid parameter value
+  wsp_encode_test(MMS.Parameter, null, null, "CodeError");
+  wsp_encode_test(MMS.Parameter, undefined, null, "CodeError");
+  wsp_encode_test(MMS.Parameter, {}, null, "CodeError");
+  // Test for case-insensitive parameter name
+  wsp_encode_test(MMS.Parameter, {name: "TYPE", value: 0}, [130, 128]);
+  wsp_encode_test(MMS.Parameter, {name: "type", value: 0}, [130, 128]);
+  // Test for non-well-known parameter name
+  wsp_encode_test(MMS.Parameter, {name: "name", value: 0}, [110, 97, 109, 101, 0, 128]);
+  // Test for constrained encoding value
+  wsp_encode_test(MMS.Parameter, {name: "type", value: "0"}, [130, 48, 0]);
+
+  run_next_test();
+});
+
 //
 // Test target: EncodedStringValue
 //
@@ -294,6 +362,28 @@ add_test(function test_EncodedStringValue_decode() {
   run_next_test();
 });
 
+//// EncodedStringValue.encode ////
+
+add_test(function test_EncodedStringValue_encode() {
+  // Test for normal TextString
+  wsp_encode_test(MMS.EncodedStringValue, "Hello", strToCharCodeArray("Hello"));
+  // Test for utf-8
+  let (entry = MMS.WSP.WSP_WELL_KNOWN_CHARSETS["utf-8"]) {
+    // "Mozilla" in full width.
+    let str = "\uff2d\uff4f\uff5a\uff49\uff4c\uff4c\uff41";
+
+    let conv = Cc["@mozilla.org/intl/scriptableunicodeconverter"]
+               .createInstance(Ci.nsIScriptableUnicodeConverter);
+    conv.charset = entry.converter;
+
+    let raw = conv.convertToByteArray(str).concat([0]);
+    wsp_encode_test(MMS.EncodedStringValue, str,
+                    [raw.length + 2, 0x80 | entry.number, 127].concat(raw));
+  }
+
+  run_next_test();
+});
+
 //
 // Test target: ExpiryValue
 //
@@ -305,6 +395,17 @@ add_test(function test_ExpiryValue_decode() {
   wsp_decode_test(MMS.ExpiryValue, [3, 128, 1, 0x80], new Date(0x80 * 1000));
   // Test for Relative-token Delta-seconds-value
   wsp_decode_test(MMS.ExpiryValue, [2, 129, 0x80], 0);
+
+  run_next_test();
+});
+
+//// ExpiryValue.encode ////
+
+add_test(function test_ExpiryValue_encode() {
+  // Test for Absolute-token Date-value
+  wsp_encode_test(MMS.ExpiryValue, new Date(0x80 * 1000), [3, 128, 1, 0x80]);
+  // Test for Relative-token Delta-seconds-value
+  wsp_encode_test(MMS.ExpiryValue, 0, [2, 129, 0x80]);
 
   run_next_test();
 });
@@ -354,6 +455,20 @@ add_test(function test_FromValue_decode() {
   run_next_test();
 });
 
+//// FromValue.encode ////
+
+add_test(function test_FromValue_encode() {
+  // Test for Insert-address-token:
+  wsp_encode_test(MMS.FromValue, null, [1, 129]);
+  // Test for Address-present-token:
+  let (addr = strToCharCodeArray("+123/TYPE=PLMN")) {
+    wsp_encode_test(MMS.FromValue, {address: "+123", type: "PLMN"},
+                    [addr.length + 1, 128].concat(addr));
+  }
+
+  run_next_test();
+});
+
 //
 // Test target: MessageClassValue
 //
@@ -385,6 +500,18 @@ add_test(function test_MessageClassValue_decodeClassIdentifier() {
 add_test(function test_MessageClassValue_decode() {
   wsp_decode_test(MMS.MessageClassValue, [65, 0], "A");
   wsp_decode_test(MMS.MessageClassValue, [128], "personal");
+
+  run_next_test();
+});
+
+//// MessageClassValue.encode ////
+
+add_test(function test_MessageClassValue_encode() {
+  wsp_encode_test(MMS.MessageClassValue, "personal", [128]);
+  wsp_encode_test(MMS.MessageClassValue, "advertisement", [129]);
+  wsp_encode_test(MMS.MessageClassValue, "informational", [130]);
+  wsp_encode_test(MMS.MessageClassValue, "auto", [131]);
+  wsp_encode_test(MMS.MessageClassValue, "A", [65, 0]);
 
   run_next_test();
 });
@@ -439,6 +566,20 @@ add_test(function test_MmFlagsValue_decode() {
   run_next_test();
 });
 
+//// MmFlagsValue.encode ////
+
+add_test(function test_MmFlagsValue_encode() {
+  for (let i = 0; i < 256; i++) {
+    if ((i >= 128) && (i <= 130)) {
+      wsp_encode_test(MMS.MmFlagsValue, {type: i, text: "A"}, [3, i, 65, 0]);
+    } else {
+      wsp_encode_test(MMS.MmFlagsValue, {type: i, text: "A"}, null, "CodeError");
+    }
+  }
+
+  run_next_test();
+});
+
 //
 // Test target: MmStateValue
 //
@@ -457,6 +598,20 @@ add_test(function test_MmStateValue_decode() {
   run_next_test();
 });
 
+//// MmStateValue.encode ////
+
+add_test(function test_MmStateValue_encode() {
+  for (let i = 0; i < 256; i++) {
+    if ((i >= 128) && (i <= 132)) {
+      wsp_encode_test(MMS.MmStateValue, i, [i]);
+    } else {
+      wsp_encode_test(MMS.MmStateValue, i, null, "CodeError");
+    }
+  }
+
+  run_next_test();
+});
+
 //
 // Test target: PriorityValue
 //
@@ -469,6 +624,20 @@ add_test(function test_PriorityValue_decode() {
       wsp_decode_test(MMS.PriorityValue, [i], i);
     } else {
       wsp_decode_test(MMS.PriorityValue, [i], null, "CodeError");
+    }
+  }
+
+  run_next_test();
+});
+
+//// PriorityValue.encode ////
+
+add_test(function test_PriorityValue_encode() {
+  for (let i = 0; i < 256; i++) {
+    if ((i >= 128) && (i <= 130)) {
+      wsp_encode_test(MMS.PriorityValue, i, [i]);
+    } else {
+      wsp_encode_test(MMS.PriorityValue, i, null, "CodeError");
     }
   }
 
@@ -507,6 +676,58 @@ add_test(function test_ReplyChargingValue_decode() {
       wsp_decode_test(MMS.ReplyChargingValue, [i], null, "CodeError");
     }
   }
+
+  run_next_test();
+});
+
+//// ReplyChargingValue.encode ////
+
+add_test(function test_ReplyChargingValue_encode() {
+  for (let i = 0; i < 256; i++) {
+    if ((i >= 128) && (i <= 131)) {
+      wsp_encode_test(MMS.ReplyChargingValue, i, [i]);
+    } else {
+      wsp_encode_test(MMS.ReplyChargingValue, i, null, "CodeError");
+    }
+  }
+
+  run_next_test();
+});
+
+//
+// Test target: ResponseText
+//
+
+//// ResponseText.decode ////
+
+add_test(function test_ResponseText_decode() {
+  // Test for MMS_PDU_TYPE_MBOX_DELETE_CONF & MMS_PDU_TYPE_DELETE_CONF
+  wsp_decode_test_ex(function (data) {
+      data.array[0] = data.array.length - 1;
+
+      let options = {};
+      options["x-mms-message-type"] = MMS_PDU_TYPE_MBOX_DELETE_CONF;
+      return MMS.ResponseText.decode(data, options);
+    }, [0, 0x80 | 0x00].concat(strToCharCodeArray("http://no.such.com/path")),
+    {statusCount: 0, text: "http://no.such.com/path"}
+  );
+  wsp_decode_test_ex(function (data) {
+      data.array[0] = data.array.length - 1;
+
+      let options = {};
+      options["x-mms-message-type"] = MMS_PDU_TYPE_DELETE_CONF;
+      return MMS.ResponseText.decode(data, options);
+    }, [0, 0x80 | 0x00].concat(strToCharCodeArray("http://no.such.com/path")),
+    {statusCount: 0, text: "http://no.such.com/path"}
+  );
+  // Test for other situations
+  wsp_decode_test_ex(function (data) {
+      let options = {};
+      options["x-mms-message-type"] = MMS_PDU_TYPE_SEND_REQ;
+      return MMS.ResponseText.decode(data, options);
+    }, strToCharCodeArray("http://no.such.com/path"),
+    {text: "http://no.such.com/path"}
+  );
 
   run_next_test();
 });
@@ -585,7 +806,7 @@ add_test(function test_PduHelper_parseHeaders() {
   };
   parse([0x80 | 0x0D, 0x80 | MMS_VERSION,   // X-Mms-Mms-Version: 1.3
          0x80 | 0x04, 0x80 | 0x33,          // Content-Type: application/vnd.wap.multipart.related
-	 0x80 | 0x0C, MMS_PDU_TYPE_SEND_REQ // X-Mms-Message-Type: M-Send.req
+         0x80 | 0x0C, MMS_PDU_TYPE_SEND_REQ // X-Mms-Message-Type: M-Send.req
         ], expect);
 
   // Parse header fields with multiple entries
@@ -606,3 +827,83 @@ add_test(function test_PduHelper_parseHeaders() {
 
   run_next_test();
 });
+
+//// PduHelper.encodeHeader ////
+
+add_test(function test_PduHelper_encodeHeader() {
+  function func(name, data, headers) {
+    MMS.PduHelper.encodeHeader(data, headers, name);
+
+    // Remove extra space consumed during encoding.
+    while (data.array.length > data.offset) {
+      data.array.pop();
+    }
+
+    return data.array;
+  }
+
+  // Encode header fields with multiple entries
+  let headers = {
+    to: [
+      { address: "+123", type: "PLMN" },
+      { address: "+456", type: "num" },
+    ],
+  };
+  wsp_encode_test_ex(func.bind(null, "to"), headers,
+                     Array.concat([0x80 | 0x17]).concat(strToCharCodeArray("+123/TYPE=PLMN"))
+                          .concat([0x80 | 0x17]).concat(strToCharCodeArray("+456")));
+
+  run_next_test();
+});
+
+//// PduHelper.encodeHeaderIfExists ////
+
+add_test(function test_PduHelper_encodeHeaderIfExists() {
+  function func(name, data, headers) {
+    MMS.PduHelper.encodeHeaderIfExists(data, headers, name);
+
+    // Remove extra space consumed during encoding.
+    while (data.array.length > data.offset) {
+      data.array.pop();
+    }
+
+    return data.array;
+  }
+
+  wsp_encode_test_ex(func.bind(null, "to"), {}, []);
+
+  run_next_test();
+});
+
+//// PduHelper.encodeHeaders ////
+
+add_test(function test_PduHelper_encodeHeaders() {
+  function func(data, headers) {
+    MMS.PduHelper.encodeHeaders(data, headers);
+
+    // Remove extra space consumed during encoding.
+    while (data.array.length > data.offset) {
+      data.array.pop();
+    }
+
+    return data.array;
+  }
+
+  let headers = {};
+  headers["x-mms-message-type"] = MMS_PDU_TYPE_SEND_REQ;
+  headers["x-mms-mms-version"] = MMS_VERSION;
+  headers["x-mms-transaction-id"] = "asdf";
+  headers["to"] = { address: "+123", type: "PLMN" };
+  headers["content-type"] = {
+    media: "application/vnd.wap.multipart.related",
+  };
+  wsp_encode_test_ex(func, headers,
+                     Array.concat([0x80 | 0x0C, MMS_PDU_TYPE_SEND_REQ])
+                          .concat([0x80 | 0x18]).concat(strToCharCodeArray(headers["x-mms-transaction-id"]))
+                          .concat([0x80 | 0x0D, 0x80 | MMS_VERSION])
+                          .concat([0x80 | 0x17]).concat(strToCharCodeArray("+123/TYPE=PLMN"))
+                          .concat([0x80 | 0x04, 0x80 | 0x33]));
+
+  run_next_test();
+});
+
