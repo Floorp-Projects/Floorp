5af6ddcfe6cc03ce59914840bd2fb76d8a29f355
Dave Townsend <dtownsend@oxymoronical.com>
2012-07-27 10:53:09 -0700

    Bug 753900: Encode extension ID and version in breakpad annotations. r=Unfocused

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_bug753900.js
@@ -0,0 +1,86 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+// This verifies that strange characters in an add-on version don't break the
+// crash annotation.
+
+var addon1 = {
+  id: "addon1@tests.mozilla.org",
+  version: "1,0",
+  name: "Test 1",
+  targetApplications: [{
+    id: "xpcshell@tests.mozilla.org",
+    minVersion: "1",
+    maxVersion: "1"
+  }]
+};
+
+var addon2 = {
+  id: "addon2@tests.mozilla.org",
+  version: "1:0",
+  name: "Test 2",
+  targetApplications: [{
+    id: "xpcshell@tests.mozilla.org",
+    minVersion: "1",
+    maxVersion: "1"
+  }]
+};
+
+var addon3 = {
+  id: "addon3@tests.mozilla.org",
+  version: "1,0",
+  name: "Test 3",
+  bootstrap: true,
+  targetApplications: [{
+    id: "xpcshell@tests.mozilla.org",
+    minVersion: "1",
+    maxVersion: "1"
+  }]
+};
+
+var addon4 = {
+  id: "addon4@tests.mozilla.org",
+  version: "1:0",
+  name: "Test 4",
+  bootstrap: true,
+  targetApplications: [{
+    id: "xpcshell@tests.mozilla.org",
+    minVersion: "1",
+    maxVersion: "1"
+  }]
+};
+
+createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+
+const profileDir = gProfD.clone();
+profileDir.append("extensions");
+
+function run_test() {
+  do_test_pending();
+
+  writeInstallRDFForExtension(addon1, profileDir);
+  writeInstallRDFForExtension(addon2, profileDir);
+  writeInstallRDFForExtension(addon3, profileDir);
+  writeInstallRDFForExtension(addon4, profileDir);
+
+  startupManager();
+
+  AddonManager.getAddonsByIDs(["addon1@tests.mozilla.org",
+                               "addon2@tests.mozilla.org",
+                               "addon3@tests.mozilla.org",
+                               "addon4@tests.mozilla.org"],
+                               function([a1, a2, a3, a4]) {
+
+    do_check_neq(a1, null);
+    do_check_in_crash_annotation(addon1.id, addon1.version);
+    do_check_neq(a2, null);
+    do_check_in_crash_annotation(addon2.id, addon2.version);
+    do_check_neq(a3, null);
+    do_check_in_crash_annotation(addon3.id, addon3.version);
+    do_check_neq(a4, null);
+    do_check_in_crash_annotation(addon4.id, addon4.version);
+
+    do_test_finished();
+  });
+}
