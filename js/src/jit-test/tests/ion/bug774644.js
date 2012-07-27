0bf8c45c52e9c547b2be8a136a52e691b7ad34c0
David Anderson <danderson@mozilla.com>
2012-07-27 16:13:02 -0700

    Include loop entry types when determining OSR types (bug 774644, r=jandem).

---
+++ js/src/jit-test/tests/ion/bug774644.js
@@ -0,0 +1,10 @@
+function YearFromTime() {}
+addTestCase();
+function addTestCase() {
+  var start = -62126352000000;
+  YearFromTime();
+  var stop = -62094729600000;
+  for (var d = start; d < stop; d >>= 86400000)
+    new TestCase();
+}
+dis(addTestCase);
