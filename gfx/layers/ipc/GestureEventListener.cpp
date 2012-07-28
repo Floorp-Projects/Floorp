226bca51d3453d08184d61b28cb46922cbf42a3d
Doug Sherk <dsherk2@mozilla.com>
2012-07-27 19:24:24 -0700

    Bug 775463: Recognize double tap gestures while still supporting single taps r=cjones

--- gfx/layers/ipc/GestureEventListener.cpp
+++ gfx/layers/ipc/GestureEventListener.cpp
@@ -4,15 +4,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "base/basictypes.h"
+#include "base/thread.h"
+
 #include "GestureEventListener.h"
 #include "AsyncPanZoomController.h"
 
 namespace mozilla {
 namespace layers {
 
+/**
+ * Maximum time for a touch on the screen and corresponding lift of the finger
+ * to be considered a tap. This also applies to double taps, except that it is
+ * used twice.
+ */
+static const int MAX_TAP_TIME = 300;
+
 GestureEventListener::GestureEventListener(AsyncPanZoomController* aAsyncPanZoomController)
   : mAsyncPanZoomController(aAsyncPanZoomController),
-    mState(NoGesture)
+    mState(GESTURE_NONE),
+    mLastTouchInput(MultiTouchInput::MULTITOUCH_START, 0)
 {
 }
 
@@ -49,7 +60,13 @@ nsEventStatus GestureEventListener::HandleInputEvent(const InputData& aEvent)
       }
     }
 
-    if (mTouches.Length() == 2) {
+    size_t length = mTouches.Length();
+    if (length == 1) {
+      mTapStartTime = event.mTime;
+      if (mState == GESTURE_NONE) {
+        mState = GESTURE_WAITING_SINGLE_TAP;
+      }
+    } else if (length == 2) {
       // Another finger has been added; it can't be a tap anymore.
       HandleTapCancel(event);
     }
@@ -89,18 +106,37 @@ nsEventStatus GestureEventListener::HandleInputEvent(const InputData& aEvent)
 
     NS_WARN_IF_FALSE(foundAlreadyExistingTouch, "Touch ended, but not in list");
 
-    if (event.mTime - mTouchStartTime <= MAX_TAP_TIME) {
-      // XXX: Incorrect use of the tap event. In the future, we want to send this
-      // on NS_TOUCH_END, then have a short timer afterwards which sends
-      // SingleTapConfirmed. Since we don't have double taps yet, this is fine for
-      // now.
-      if (HandleSingleTapUpEvent(event) == nsEventStatus_eConsumeNoDefault) {
-        return nsEventStatus_eConsumeNoDefault;
-      }
+    if (event.mTime - mTapStartTime <= MAX_TAP_TIME) {
+      if (mState == GESTURE_WAITING_DOUBLE_TAP) {
+        mDoubleTapTimeoutTask->Cancel();
 
-      if (HandleSingleTapConfirmedEvent(event) == nsEventStatus_eConsumeNoDefault) {
-        return nsEventStatus_eConsumeNoDefault;
+        // We were waiting for a double tap and it has arrived.
+        HandleDoubleTap(event);
+        mState = GESTURE_NONE;
+      } else if (mState == GESTURE_WAITING_SINGLE_TAP) {
+        HandleSingleTapUpEvent(event);
+
+        // We were not waiting for anything but a single tap has happened that
+        // may turn into a double tap. Wait a while and if it doesn't turn into
+        // a double tap, send a single tap instead.
+        mState = GESTURE_WAITING_DOUBLE_TAP;
+
+        // Cache the current event since it may become the single tap that we
+        // send.
+        mLastTouchInput = event;
+
+        mDoubleTapTimeoutTask =
+          NewRunnableMethod(this, &GestureEventListener::TimeoutDoubleTap);
+
+        MessageLoop::current()->PostDelayedTask(
+          FROM_HERE,
+          mDoubleTapTimeoutTask,
+          MAX_TAP_TIME);
       }
+    }
+
+    if (mState == GESTURE_WAITING_SINGLE_TAP) {
+      mState = GESTURE_NONE;
     }
 
     break;
@@ -130,7 +166,7 @@ nsEventStatus GestureEventListener::HandlePinchGestureEvent(const MultiTouchInpu
       float(NS_hypot(firstTouch.x - secondTouch.x,
                      firstTouch.y - secondTouch.y));
 
-    if (mState == NoGesture) {
+    if (mState == GESTURE_NONE) {
       PinchGestureInput pinchEvent(PinchGestureInput::PINCHGESTURE_START,
                                    aEvent.mTime,
                                    focusPoint,
@@ -139,7 +175,7 @@ nsEventStatus GestureEventListener::HandlePinchGestureEvent(const MultiTouchInpu
 
       mAsyncPanZoomController->HandleInputEvent(pinchEvent);
 
-      mState = InPinchGesture;
+      mState = GESTURE_PINCH;
     } else {
       PinchGestureInput pinchEvent(PinchGestureInput::PINCHGESTURE_SCALE,
                                    aEvent.mTime,
@@ -153,16 +189,16 @@ nsEventStatus GestureEventListener::HandlePinchGestureEvent(const MultiTouchInpu
     mPreviousSpan = currentSpan;
 
     rv = nsEventStatus_eConsumeNoDefault;
-  } else if (mState == InPinchGesture) {
+  } else if (mState == GESTURE_PINCH) {
     PinchGestureInput pinchEvent(PinchGestureInput::PINCHGESTURE_END,
                                  aEvent.mTime,
                                  mTouches[0].mScreenPoint,
                                  1.0f,
                                  1.0f);
- 
+
     mAsyncPanZoomController->HandleInputEvent(pinchEvent);
 
-    mState = NoGesture;
+    mState = GESTURE_NONE;
 
     rv = nsEventStatus_eConsumeNoDefault;
   }
@@ -177,27 +213,47 @@ nsEventStatus GestureEventListener::HandlePinchGestureEvent(const MultiTouchInpu
 nsEventStatus GestureEventListener::HandleSingleTapUpEvent(const MultiTouchInput& aEvent)
 {
   TapGestureInput tapEvent(TapGestureInput::TAPGESTURE_UP, aEvent.mTime, aEvent.mTouches[0].mScreenPoint);
-  mAsyncPanZoomController->HandleInputEvent(tapEvent);
-
-  return nsEventStatus_eConsumeDoDefault;
+  return mAsyncPanZoomController->HandleInputEvent(tapEvent);
 }
 
 nsEventStatus GestureEventListener::HandleSingleTapConfirmedEvent(const MultiTouchInput& aEvent)
 {
   TapGestureInput tapEvent(TapGestureInput::TAPGESTURE_CONFIRMED, aEvent.mTime, aEvent.mTouches[0].mScreenPoint);
-  mAsyncPanZoomController->HandleInputEvent(tapEvent);
-
-  return nsEventStatus_eConsumeDoDefault;
+  return mAsyncPanZoomController->HandleInputEvent(tapEvent);
 }
 
 nsEventStatus GestureEventListener::HandleTapCancel(const MultiTouchInput& aEvent)
 {
-  // XXX: In the future we will have to actually send a cancel notification to
-  // Gecko, but for now since we're doing both the "SingleUp" and
-  // "SingleConfirmed" notifications together, there's no need to cancel either
-  // one.
-  mTouchStartTime = 0;
+  mTapStartTime = 0;
+
+  switch (mState)
+  {
+  case GESTURE_WAITING_SINGLE_TAP:
+  case GESTURE_WAITING_DOUBLE_TAP:
+    mState = GESTURE_NONE;
+    break;
+  default:
+    break;
+  }
+
   return nsEventStatus_eConsumeDoDefault;
+}
+
+nsEventStatus GestureEventListener::HandleDoubleTap(const MultiTouchInput& aEvent)
+{
+  TapGestureInput tapEvent(TapGestureInput::TAPGESTURE_DOUBLE, aEvent.mTime, aEvent.mTouches[0].mScreenPoint);
+  return mAsyncPanZoomController->HandleInputEvent(tapEvent);
+}
+
+void GestureEventListener::TimeoutDoubleTap()
+{
+  // If we haven't gotten another tap by now, reset the state and treat it as a
+  // single tap. It couldn't have been a double tap.
+  if (mState == GESTURE_WAITING_DOUBLE_TAP) {
+    mState = GESTURE_NONE;
+
+    HandleSingleTapConfirmedEvent(mLastTouchInput);
+  }
 }
 
 AsyncPanZoomController* GestureEventListener::GetAsyncPanZoomController() {
