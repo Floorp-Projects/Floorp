e752e1a84f2c1e02c6aaa687cf58a3891b941424
Kartikaya Gupta <kgupta@mozilla.com>
2012-07-28 10:33:51 -0400

    Bug 770047 - Add the LayerView directly to the XML layout rather than creating it later, so that we don't get a black flash on startup on JellyBean devices. r=sriram

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -1683,12 +1683,7 @@ abstract public class GeckoApp
              * run experience, perhaps?
              */
             mLayerController = new LayerController(this);
-            View v = mLayerController.getView();
-
-            // Instead of flickering the checkerboard, show a white screen until Gecko paints
-            v.setBackgroundColor(Color.WHITE);
-
-            mGeckoLayout.addView(v, 0);
+            mLayerController.setView((LayerView)findViewById(R.id.layer_view));
         }
 
         mPluginContainer = (AbsoluteLayout) findViewById(R.id.plugin_container);
