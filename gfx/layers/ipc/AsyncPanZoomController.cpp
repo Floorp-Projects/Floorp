834a95defcbcf2ff4450c26405eb1ac8bf17bdbb
Doug Sherk <dsherk2@mozilla.com>
2012-07-31 16:19:25 -0700

    Bug 779344: Properly transform B2G panning/zooming after layers changes r=dzbarsky

--- gfx/layers/ipc/AsyncPanZoomController.cpp
+++ gfx/layers/ipc/AsyncPanZoomController.cpp
@@ -13,6 +13,7 @@
 #include "GestureEventListener.h"
 #include "nsIThreadManager.h"
 #include "nsThreadUtils.h"
+#include "Layers.h"
 
 namespace mozilla {
 namespace layers {
@@ -697,7 +698,7 @@ void AsyncPanZoomController::RequestContentRepaint() {
 
 bool AsyncPanZoomController::SampleContentTransformForFrame(const TimeStamp& aSampleTime,
                                                             const FrameMetrics& aFrame,
-                                                            const gfx3DMatrix& aCurrentTransform,
+                                                            Layer* aLayer,
                                                             gfx3DMatrix* aNewTransform) {
   // The eventual return value of this function. The compositor needs to know
   // whether or not to advance by a frame as soon as it can. For example, if a
@@ -706,9 +707,11 @@ bool AsyncPanZoomController::SampleContentTransformForFrame(const TimeStamp& aSa
   // responsibility to schedule a composite.
   bool requestAnimationFrame = false;
 
+  const gfx3DMatrix& currentTransform = aLayer->GetTransform();
+
   // Scales on the root layer, on what's currently painted.
-  float rootScaleX = aCurrentTransform.GetXScale(),
-        rootScaleY = aCurrentTransform.GetYScale();
+  float rootScaleX = currentTransform.GetXScale(),
+        rootScaleY = currentTransform.GetYScale();
 
   nsIntPoint metricsScrollOffset(0, 0);
   nsIntPoint scrollOffset;
@@ -740,7 +743,14 @@ bool AsyncPanZoomController::SampleContentTransformForFrame(const TimeStamp& aSa
     (scrollOffset.y / rootScaleY - metricsScrollOffset.y) * localScaleY);
 
   ViewTransform treeTransform(-scrollCompensation, localScaleX, localScaleY);
-  *aNewTransform = gfx3DMatrix(treeTransform) * aCurrentTransform;
+  *aNewTransform = gfx3DMatrix(treeTransform) * currentTransform;
+
+  // The transform already takes the resolution scale into account.  Since we
+  // will apply the resolution scale again when computing the effective
+  // transform, we must apply the inverse resolution scale here.
+  aNewTransform->Scale(1.0f/aLayer->GetXScale(),
+                       1.0f/aLayer->GetYScale(),
+                       1);
 
   mLastSampleTime = aSampleTime;
 
