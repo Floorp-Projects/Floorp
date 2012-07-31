86a06f002272b29c6e665030ffe1fd3732a38af7
Robert O'Callahan <robert@ocallahan.org>
2012-08-01 00:17:22 +1200

    Bug 779715. Part 6: Rework capturing MediaStreams from media elements to use TrackUnionStreams. r=cpearce,jesup Moves to a new setup where a decoder manages a single SourceMediaStream internally. Each stream returned from mozCaptureStream(UntilEnded) is a TrackUnionStream which is fed by the decoder's SourceMediaStream. We want the captured streams to be blocked while the media element is not playing. We do that by blocking any captured stream that has no SourceMediaStream feeding into it, and blocking any SourceMediaStream while its decoder is not playing. We arrange for the decoders's PlaybackEnded to be delayed until its SourceMediaStream has finished according to the media stream graph. This ensures the state of captured media streams corresponds more closely to the media element state.
    --HG--
    extra : rebase_source : 3324ff0e9bdce9c71a23c0f5f2032815e9046081

--- content/media/nsDOMMediaStream.h
+++ content/media/nsDOMMediaStream.h
@@ -55,6 +55,11 @@ public:
    */
   static already_AddRefed<nsDOMMediaStream> CreateInputStream();
 
+  /**
+   * Create an nsDOMMediaStream whose underlying stream is a TrackUnionStream.
+   */
+  static already_AddRefed<nsDOMMediaStream> CreateTrackUnionStream();
+
 protected:
   // MediaStream is owned by the graph, but we tell it when to die, and it won't
   // die until we let it.
