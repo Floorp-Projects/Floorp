63993f05d96503d9628d344033e55f3c0cc6a2b2
Scott Johnson <sjohnson@mozilla.com>
2012-07-31 11:21:19 -0500

    Bug 764567: Implement column-fill part of CSS3 multicol spec, now with regression fixes [r=roc].

--- layout/style/nsComputedDOMStyle.h
+++ layout/style/nsComputedDOMStyle.h
@@ -333,6 +333,7 @@ private:
 
   /* Column properties */
   nsIDOMCSSValue* DoGetColumnCount();
+  nsIDOMCSSValue* DoGetColumnFill();
   nsIDOMCSSValue* DoGetColumnWidth();
   nsIDOMCSSValue* DoGetColumnGap();
   nsIDOMCSSValue* DoGetColumnRuleWidth();
