d9e580d2527c6daa3bf5ed91f0299dfb353db8c5
Kartikaya Gupta <kgupta@mozilla.com>
2012-08-01 10:48:19 -0400

    Bug 777351 - Some minor cleanup in GeckoLayerClient.java. r=sriram

--- mobile/android/base/gfx/GeckoLayerClient.java
+++ mobile/android/base/gfx/GeckoLayerClient.java
@@ -39,7 +39,7 @@ public class GeckoLayerClient implements GeckoEventResponder,
     private DisplayPortMetrics mReturnDisplayPort;
 
     private boolean mRecordDrawTimes;
-    private DrawTimingQueue mDrawTimingQueue;
+    private final DrawTimingQueue mDrawTimingQueue;
 
     private VirtualLayer mRootLayer;
 
@@ -52,13 +52,11 @@ public class GeckoLayerClient implements GeckoEventResponder,
      */
     private ImmutableViewportMetrics mFrameMetrics;
 
-    private String mLastCheckerboardColor;
-
     /* Used by robocop for testing purposes */
     private DrawListener mDrawListener;
 
     /* Used as a temporary ViewTransform by syncViewportInfo */
-    private ViewTransform mCurrentViewTransform;
+    private final ViewTransform mCurrentViewTransform;
 
     /* This is written by the compositor thread and read by the UI thread. */
     private volatile boolean mCompositorCreated;
