45562a3fe1269aeeeb706f37e8541dc09f9b0849
Kartikaya Gupta <kgupta@mozilla.com>
2012-08-01 10:38:30 -0400

    Bug 777075 - Eliminate the getViewport and getZoomFactor methods in LayerController to further reduce exposed surface area. r=mbrubeck

--- mobile/android/base/gfx/LayerRenderer.java
+++ mobile/android/base/gfx/LayerRenderer.java
@@ -539,7 +539,7 @@ public class LayerRenderer {
             /* Draw the drop shadow, if we need to. */
             RectF untransformedPageRect = new RectF(0.0f, 0.0f, mPageRect.width(),
                                                     mPageRect.height());
-            if (!untransformedPageRect.contains(mView.getController().getViewport()))
+            if (!untransformedPageRect.contains(mFrameMetrics.getViewport()))
                 mShadowLayer.draw(mPageContext);
 
             /* Draw the 'checkerboard'. We use gfx.show_checkerboard_pattern to
