4c5c9af95458daa8e99d7d9f4a815c7f72a0e076
Aryeh Gregor <ayg@aryeh.name>
2012-08-01 11:17:51 +0300

    Bug 778428 - Refcount arrays of nodes in editor/; r=ehsan

--- editor/libeditor/html/nsHTMLEditRules.cpp
+++ editor/libeditor/html/nsHTMLEditRules.cpp
@@ -7625,7 +7625,7 @@ nsHTMLEditRules::RemoveEmptyNodes()
   NS_ENSURE_SUCCESS(res, res);
   
   nsCOMArray<nsINode> arrayOfEmptyNodes, arrayOfEmptyCites;
-  nsTArray<nsINode*> skipList;
+  nsTArray<nsCOMPtr<nsINode> > skipList;
 
   // check for empty nodes
   while (!iter->IsDone()) {
