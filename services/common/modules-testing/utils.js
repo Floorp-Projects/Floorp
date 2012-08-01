375fc869e04abbecc9fcfdd9e432dfdec53c1aa5
Gregory Szorc <gps@mozilla.com>
2012-08-01 12:05:29 -0700

    Bug 779391 - Move deepCopy into CommonUtils; r=rnewman

---
+++ services/common/modules-testing/utils.js
@@ -0,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const EXPORTED_SYMBOLS = [
+  "TestingUtils",
+];
+
+let TestingUtils = {
+  /**
+   * Perform a deep copy of an Array or Object.
+   */
+  deepCopy: function deepCopy(thing, noSort) {
+    if (typeof(thing) != "object" || thing == null) {
+      return thing;
+    }
+
+    if (Array.isArray(thing)) {
+      let ret = [];
+      for (let element of thing) {
+        ret.push(this.deepCopy(element, noSort));
+      }
+
+      return ret;
+    }
+
+    let ret = {};
+    let props = [p for (p in thing)];
+
+    if (!noSort) {
+      props = props.sort();
+    }
+
+    for (let prop of props) {
+      ret[prop] = this.deepCopy(thing[prop], noSort);
+    }
+
+    return ret;
+  },
+};
