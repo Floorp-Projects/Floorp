6a4117a0fa6d6bd6ebfef16fb49e54b67c25abf4
Benjamin Peterson <benjamin@python.org>
2012-08-03 10:27:57 -0700

    Bug 778531 - Update Array.prototype.join/toLocaleString to follow ES5. r=jorendorff

--- js/src/tests/ecma_5/Array/toLocaleString-01.js
+++ js/src/tests/ecma_5/Array/toLocaleString-01.js
@@ -23,6 +23,12 @@ assertEq(Array.prototype.toLocaleString.call(o), "7,baz");
 o = {};
 assertEq(Array.prototype.toLocaleString.call(o), "");
 
+var log = '';
+arr = {length: {valueOf: function () { log += "L"; return 2; }},
+      0: "x", 1: "z"};
+assertEq(Array.prototype.toLocaleString.call(arr), "x,z");
+assertEq(log, "L");
+
 /******************************************************************************/
 
 reportCompare(true, true);
