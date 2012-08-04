eb8fc3e4fa19ce2126cdf3eba3572d7b50bcf732
Ms2ger <ms2ger@gmail.com>
2012-08-04 09:43:59 +0200

    Bug 768310 - Don't throw from Event.initEvent if it's called during event dispatch; r=smaug

--- content/events/src/nsDOMEvent.cpp
+++ content/events/src/nsDOMEvent.cpp
@@ -536,7 +536,7 @@ NS_IMETHODIMP
 nsDOMEvent::InitEvent(const nsAString& aEventTypeArg, bool aCanBubbleArg, bool aCancelableArg)
 {
   // Make sure this event isn't already being dispatched.
-  NS_ENSURE_TRUE(!NS_IS_EVENT_IN_DISPATCH(mEvent), NS_ERROR_INVALID_ARG);
+  NS_ENSURE_TRUE(!NS_IS_EVENT_IN_DISPATCH(mEvent), NS_OK);
 
   if (NS_IS_TRUSTED_EVENT(mEvent)) {
     // Ensure the caller is permitted to dispatch trusted DOM events.
