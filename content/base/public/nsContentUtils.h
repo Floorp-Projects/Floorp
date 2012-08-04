4a2c4cbe3940a73c942cce53f22f6971f9ad0d68
Ms2ger <ms2ger@gmail.com>
2012-08-04 09:43:59 +0200

    Bug 672814 - Part e: Expand the set of JS mime types we support; r=bz

--- content/base/public/nsContentUtils.h
+++ content/base/public/nsContentUtils.h
@@ -2034,6 +2034,17 @@ public:
       "application/javascript",
       "application/ecmascript",
       "application/x-javascript",
+      "application/x-ecmascript",
+      "text/javascript1.0",
+      "text/javascript1.1",
+      "text/javascript1.2",
+      "text/javascript1.3",
+      "text/javascript1.4",
+      "text/javascript1.5",
+      "text/jscript",
+      "text/livescript",
+      "text/x-ecmascript",
+      "text/x-javascript",
       nullptr
     };
 
