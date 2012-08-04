bacea6a53d25c463a4a7a00abd64d1ed0fa74700
David Zbarsky <dzbarsky@gmail.com>
2012-08-04 07:03:47 -0700

    Bug 780340 - Allow compositor-driven animations of visibility r=mattwoodrow

--- layout/style/AnimationCommon.cpp
+++ layout/style/AnimationCommon.cpp
@@ -230,6 +230,9 @@ CommonElementAnimationData::CanAnimatePropertyOnCompositor(const dom::Element *a
   }
 
   nsIFrame* frame = aElement->GetPrimaryFrame();
+  if (aProperty == eCSSProperty_visibility) {
+    return true;
+  }
   if (aProperty == eCSSProperty_opacity) {
     bool enabled = nsLayoutUtils::AreOpacityAnimationsEnabled();
     if (!enabled && sShouldLog) {
