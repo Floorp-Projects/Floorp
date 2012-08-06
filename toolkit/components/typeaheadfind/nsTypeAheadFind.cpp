1db41053b406f6efcdb1b8bd4ea514f0d4050b15
David Zbarsky <dzbarsky@gmail.com>
2012-08-05 23:00:55 -0400

    Bug 779684: Create a stack-based nsFrameIterator class out of nsIFrameTraversal and nsIFrameEnumerator r=roc

--- toolkit/components/typeaheadfind/nsTypeAheadFind.cpp
+++ toolkit/components/typeaheadfind/nsTypeAheadFind.cpp
@@ -25,7 +25,7 @@
 #include "nsIDOMNode.h"
 #include "mozilla/dom/Element.h"
 #include "nsIFrame.h"
-#include "nsFrameTraversal.h"
+#include "nsFrameIterator.h"
 #include "nsIImageDocument.h"
 #include "nsIDOMHTMLDocument.h"
 #include "nsIDOMHTMLElement.h"
@@ -62,8 +62,6 @@ NS_INTERFACE_MAP_END
 
 NS_IMPL_ADDREF(nsTypeAheadFind)
 NS_IMPL_RELEASE(nsTypeAheadFind)
-
-static NS_DEFINE_CID(kFrameTraversalCID, NS_FRAMETRAVERSAL_CID);
 
 #define NS_FIND_CONTRACTID "@mozilla.org/embedcomp/rangefind;1"
 
@@ -1116,23 +1114,12 @@ nsTypeAheadFind::IsRangeVisible(nsIPresShell *aPresShell,
   // We know that the target range isn't usable because it's not in the
   // view port. Move range forward to first visible point,
   // this speeds us up a lot in long documents
-  nsCOMPtr<nsIFrameEnumerator> frameTraversal;
-  nsCOMPtr<nsIFrameTraversal> trav(do_CreateInstance(kFrameTraversalCID));
-  if (trav)
-    trav->NewFrameTraversal(getter_AddRefs(frameTraversal),
-                            aPresContext, frame,
-                            eLeaf,
-                            false, // aVisual
-                            false, // aLockInScrollView
-                            false     // aFollowOOFs
-                            );
-
-  if (!frameTraversal)
-    return false;
+  nsFrameIterator frameTraversal(aPresContext, frame,
+                                 eLeaf, FrameIteratorFlags::FLAG_NONE);
 
   while (rectVisibility == nsRectVisibility_kAboveViewport) {
-    frameTraversal->Next();
-    frame = frameTraversal->CurrentItem();
+    frameTraversal.Next();
+    frame = frameTraversal.CurrentItem();
     if (!frame)
       return false;
 
