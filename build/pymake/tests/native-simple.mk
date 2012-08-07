c8966962fadda6ceea2233badacef6804676e78d
Siddharth Agarwal <sid.bugzilla@gmail.com>
2012-08-08 00:49:02 +0530

    Bug 780612 - Add a list of characters which probably indicate shell scripts but native commands won't reject. r=khuey
    --HG--
    extra : rebase_source : 38ffd729c2972e142673fbca50584c241b4eade0

--- build/pymake/tests/native-simple.mk
+++ build/pymake/tests/native-simple.mk
@@ -2,10 +2,11 @@ ifndef TOUCH
 TOUCH = touch
 endif
 
-all: testfile {testfile2}
+all: testfile {testfile2} (testfile3)
 	test -f testfile
 	test -f {testfile2}
+	test -f "(testfile3)"
 	@echo TEST-PASS
 
-testfile {testfile2}:
-	$(TOUCH) $@
+testfile {testfile2} (testfile3):
+	$(TOUCH) "$@"
