83aa36dcabd70c4afcdc99a8c0760baf7ca7e110
Gregor Wagner <anygregor@gmail.com>
2012-08-07 16:22:24 -0700

    Bug 779331 - Settings API: new permissions. r=mrbkap

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -54,7 +54,6 @@ function getContentWindow() {
 function addPermissions(urls) {
   let permissions = [
     'indexedDB-unlimited', 'webapps-manage', 'offline-app', 'pin-app',
-    'websettings-read', 'websettings-readwrite',
     'content-camera', 'wifi-manage', 'desktop-notification',
     'geolocation', 'device-storage', 'alarms'
   ];
