0835560f7452cacf8c79af67e395eb35da00ee89
Mike Hommey <mh+mozilla@glandium.org>
2012-08-08 09:33:16 +0200

    Bug 777379 - Ensure that default is always the default target. r=ted

--- config/rules.mk
+++ config/rules.mk
@@ -1774,6 +1774,7 @@ libs export::
 default all::
 	if test -d $(DIST)/bin ; then touch $(DIST)/bin/.purgecaches ; fi
 
+.DEFAULT_GOAL ?= default
 
 #############################################################################
 # Derived targets and dependencies
