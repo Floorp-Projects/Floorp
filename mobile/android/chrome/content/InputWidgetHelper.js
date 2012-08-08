f5c7c86242d97a7bcc65cd61845884a8a752d2c5
Raphael Catolino <rcatolino@mozilla.com>
2012-08-08 09:42:00 +0200

    Bug 730330 - Use new datetime picker for input date/datetime/month ui r=cpeterson

---
+++ mobile/android/chrome/content/InputWidgetHelper.js
@@ -0,0 +1,87 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+var InputWidgetHelper = {
+  _uiBusy: false,
+
+  handleEvent: function(aEvent) {
+    this.handleClick(aEvent.target);
+  },
+
+  handleClick: function(aTarget) {
+    // if we're busy looking at a InputWidget we want to eat any clicks that
+    // come to us, but not to process them
+    if (this._uiBusy || !this._isValidInput(aTarget))
+      return;
+
+    this._uiBusy = true;
+    this.show(aTarget);
+    this._uiBusy = false;
+  },
+
+  show: function(aElement) {
+    let msg = {
+      type: "Prompt:Show",
+      title: Strings.browser.GetStringFromName("inputWidgetHelper." + aElement.getAttribute('type')),
+      buttons: [
+        { label: Strings.browser.GetStringFromName("inputWidgetHelper.set") },
+        { label: Strings.browser.GetStringFromName("inputWidgetHelper.clear") },
+        { label: Strings.browser.GetStringFromName("inputWidgetHelper.cancel") }
+      ],
+      inputs: [
+        { type: aElement.getAttribute('type'), value: aElement.value }
+      ]
+    };
+
+    let data = JSON.parse(sendMessageToJava({ gecko: msg }));
+
+    let changed = false;
+    if (data.button == -1) {
+      // This type is not supported with this android version.
+      return;
+    }
+    if (data.button == 1) {
+      // The user cleared the value.
+      if (aElement.value != "") {
+        aElement.value = "";
+        changed = true;
+      }
+    } else if (data.button == 0) {
+      // Commit the new value.
+      if (aElement.value != data[aElement.getAttribute('type')]) {
+        aElement.value = data[aElement.getAttribute('type')];
+        changed = true;
+      }
+    }
+    // Else the user canceled the input.
+
+    if (changed)
+      this.fireOnChange(aElement);
+
+  },
+
+  _isValidInput: function(aElement) {
+    if (!aElement instanceof HTMLInputElement)
+      return false;
+
+    let type = aElement.getAttribute('type');
+    if (type == "date" || type == "datetime" || type == "datetime-local" ||
+        type == "week" || type == "month" || type == "time") {
+      return true;
+    }
+
+    return false;
+  },
+
+  fireOnChange: function(aElement) {
+    let evt = aElement.ownerDocument.createEvent("Events");
+    evt.initEvent("change", true, true, aElement.defaultView, 0,
+                  false, false,
+                  false, false, null);
+    setTimeout(function() {
+      aElement.dispatchEvent(evt);
+    }, 0);
+  }
+};
