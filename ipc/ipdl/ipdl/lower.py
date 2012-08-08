08e1e16569fc1e2e5ca532e9354dd3dbe9261c4e
Chris Dearman <chris@mips.com>
2012-08-08 15:10:03 -0400

    Bug 779013 - Align the mValue union in IPDL-generated code. r=cjones

--- ipc/ipdl/ipdl/lower.py
+++ ipc/ipdl/ipdl/lower.py
@@ -2098,7 +2098,9 @@ def _generateCxxUnion(ud):
     # the C++ union the discunion use for storage
     valueunion = TypeUnion(valuetype.name)
     for c in ud.components:
-        valueunion.addComponent(c.unionType(), c.name)
+        valueunion.addComponent(c);
+    valueunion.addAlignment();
+
     cls.addstmts([ StmtDecl(Decl(valueunion,'')),
                        Whitespace.NL ])
 
