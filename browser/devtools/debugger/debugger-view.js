1dfb1d04d6a7dec175a90244304e4e4f0623b837
Victor Porof <vporof@mozilla.com>
2012-08-09 00:37:23 +0300

    Bug 780631 - Don't change the selected script while filtering if the file part from the search expression hasn't changed, r=rcampbell

--- browser/devtools/debugger/debugger-view.js
+++ browser/devtools/debugger/debugger-view.js
@@ -480,7 +480,7 @@ ScriptsView.prototype = {
       for (let i = 0, l = scripts.itemCount; i < l; i++) {
         scripts.getItemAtIndex(i).hidden = false;
       }
-    } else {
+    } else if (this._prevSearchedFile !== file) {
       let found = false;
 
       for (let i = 0, l = scripts.itemCount; i < l; i++) {
@@ -508,15 +508,18 @@ ScriptsView.prototype = {
         scripts.removeAttribute("tooltiptext");
       }
     }
-    if (line > -1) {
+    if (this._prevSearchedLine !== line && line > -1) {
       editor.setCaretPosition(line - 1);
     }
-    if (token.length) {
+    if (this._prevSearchedToken !== token && token.length > 0) {
       let offset = editor.find(token, { ignoreCase: true });
       if (offset > -1) {
         editor.setSelection(offset, offset + token.length)
       }
     }
+    this._prevSearchedFile = file;
+    this._prevSearchedLine = line;
+    this._prevSearchedToken = token;
   },
 
   /**
