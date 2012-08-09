e296bd4554c4a947193b185ba99e7242e7aedbdf
Mike Hommey <mh+mozilla@glandium.org>
2012-08-09 16:34:26 +0200

    Bug 776325 - Set gtk program name to the one defined in application.ini. r=karlt

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3222,6 +3222,13 @@ XREMain::XRE_mainStartup(bool* aExitFlag)
     gdk_rgb_set_install(TRUE);
 #endif
 
+  // Set program name to the one defined in application.ini.
+  {
+    nsCAutoString program(gAppData->name);
+    ToLowerCase(program);
+    g_set_prgname(program.get());
+  }
+
   // Initialize GTK here for splash.
 
   // Open the display ourselves instead of using gtk_init, so that we can
