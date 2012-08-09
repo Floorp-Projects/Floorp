003cb43a0be36c364568257c74d27762a69d8b17
Gavin Sharp <gavin@gavinsharp.com>
2012-08-08 18:09:37 -0700

    Bug 781386: add pref to avoid loading built-in providers during test runs, r=markh/mixedpuppy

--- browser/base/content/test/browser_social_toolbar.js
+++ browser/base/content/test/browser_social_toolbar.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-let SocialService = Cu.import("resource://gre/modules/SocialService.jsm", {}).SocialService;
-
 function test() {
   waitForExplicitFinish();
 
@@ -13,10 +11,8 @@ function test() {
     workerURL: "https://example1.com/worker.js",
     iconURL: "chrome://branding/content/icon48.png"
   };
-  runSocialTestWithProvider(manifest, function () {
-    runSocialTests(tests, undefined, undefined, function () {
-      SocialService.removeProvider(Social.provider.origin, finish);
-    });
+  runSocialTestWithProvider(manifest, function (finishcb) {
+    runSocialTests(tests, undefined, undefined, finishcb);
   });
 }
 
