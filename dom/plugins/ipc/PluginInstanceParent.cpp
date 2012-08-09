c2b7b015375fbaf249d95c68a6137f988c9dedaa
Benoit Jacob <bjacob@mozilla.com>
2012-08-09 11:58:40 -0400

    Backed out changeset c69b5af20a85 because Ms2ger already fixed it on m-c and this would only give a merge conflict

--- dom/plugins/ipc/PluginInstanceParent.cpp
+++ dom/plugins/ipc/PluginInstanceParent.cpp
@@ -450,7 +450,7 @@ PluginInstanceParent::AnswerNPN_SetValue_NPPVpluginDrawingModel(
 
         mDrawingModel = drawingModel;
         *result = mNPNIface->setvalue(mNPP, NPPVpluginDrawingModel,
-                                      (void*)(intptr_t)drawingModel);
+                                      (void*)drawingModel);
 
         if (mRemoteImageDataShmem.IsWritable()) {
             container->SetRemoteImageData(nullptr, nullptr);
