d729e2028a7cec01ad6a1b3bc104dd74f9f24c12
Gavin Sharp <gavin@gavinsharp.com>
2012-08-08 18:09:37 -0700

    Bug 781386: add pref to avoid loading built-in providers during test runs, r=markh/mixedpuppy

--- browser/base/content/test/browser_social_sidebar.js
+++ browser/base/content/test/browser_social_sidebar.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-let SocialService = Cu.import("resource://gre/modules/SocialService.jsm", {}).SocialService;
-
 function test() {
   waitForExplicitFinish();
 
@@ -17,7 +15,7 @@ function test() {
   runSocialTestWithProvider(manifest, doTest);
 }
 
-function doTest() {
+function doTest(finishcb) {
   ok(SocialSidebar.canShow, "social sidebar should be able to be shown");
   ok(SocialSidebar.enabled, "social sidebar should be on by default");
 
@@ -50,8 +48,8 @@ function doTest() {
 
       checkShown(true);
 
-      // Remove the test provider
-      SocialService.removeProvider(Social.provider.origin, finish);
+      // Finish the test
+      finishcb();
     });
 
     // Toggle it back on
