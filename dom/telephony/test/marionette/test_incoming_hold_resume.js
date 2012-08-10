e736c55a99c70cdbabf9ddc24628d2e17bc86f77
Rob Wood <rwood@mozilla.com>
2012-08-10 13:45:14 -0400

    Bug 781548 - Add WebAPI tess for resuming held calls, r=jgriffin, DONTBUILD because NPOTB

---
+++ dom/telephony/test/marionette/test_incoming_hold_resume.js
@@ -0,0 +1,179 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+MARIONETTE_TIMEOUT = 12000;
+
+SpecialPowers.addPermission("telephony", true, document);
+
+let telephony = window.navigator.mozTelephony;
+let number = "5555551234";
+let connectedCalls;
+let incomingCall;
+
+function verifyInitialState() {
+  log("Verifying initial state.");
+  ok(telephony);
+  is(telephony.active, null);
+  ok(telephony.calls);
+  is(telephony.calls.length, 0);
+
+  runEmulatorCmd("gsm list", function(result) {
+    log("Initial call list: " + result);
+    is(result[0], "OK");
+    simulateIncoming();
+  });
+}
+
+function simulateIncoming() {
+  log("Simulating an incoming call.");
+
+  telephony.onincoming = function onincoming(event) {
+    log("Received 'incoming' call event.");
+    incomingCall = event.call;
+    ok(incomingCall);
+    is(incomingCall.number, number);
+    is(incomingCall.state, "incoming");
+
+    is(telephony.calls.length, 1);
+    is(telephony.calls[0], incomingCall);
+
+    runEmulatorCmd("gsm list", function(result) {
+      log("Call list is now: " + result);
+      is(result[0], "inbound from " + number + " : incoming");
+      is(result[1], "OK");
+      answer();
+    });
+  };
+  runEmulatorCmd("gsm call " + number);
+}
+
+function answer() {
+  log("Answering the incoming call.");
+
+  let gotConnecting = false;
+  incomingCall.onconnecting = function onconnecting(event) { 
+    log("Received 'connecting' call event.");
+    is(incomingCall, event.call);
+    is(incomingCall.state, "connecting");
+    gotConnecting = true;
+  };
+
+  incomingCall.onconnected = function onconnected(event) {
+    log("Received 'connected' call event.");
+    is(incomingCall, event.call);
+    is(incomingCall.state, "connected");
+    ok(gotConnecting);
+
+    is(incomingCall, telephony.active);
+
+    runEmulatorCmd("gsm list", function(result) {
+      log("Call list is now: " + result);
+      is(result[0], "inbound from " + number + " : active");
+      is(result[1], "OK");
+      hold();
+    });
+  };
+  incomingCall.answer();
+}
+
+function hold() {
+  log("Putting the call on hold.");
+
+  let gotHolding = false;
+  incomingCall.onholding = function onholding(event) {
+    log("Received 'holding' call event");
+    is(incomingCall, event.call);
+    is(incomingCall.state, "holding");
+    gotHolding = true;
+  };
+
+  incomingCall.onheld = function onheld(event) {
+    log("Received 'held' call event");
+    is(incomingCall, event.call);
+    is(incomingCall.state, "held");
+    ok(gotHolding);
+
+    is(telephony.active, null);
+    is(telephony.calls.length, 1);
+    is(telephony.calls[0], incomingCall);
+
+    runEmulatorCmd("gsm list", function(result) {
+      log("Call list is now: " + result);
+      is(result[0], "inbound from " + number + " : held");
+      is(result[1], "OK");   
+      // Wait on hold for a couple of seconds
+      log("Pausing 2 seconds while on hold");
+      setTimeout(resume, 2000);
+    });
+  };
+  incomingCall.hold();
+}
+
+function resume() {
+  log("Resuming the held call.");
+
+  let gotResuming = false;
+  incomingCall.onresuming = function onresuming(event) {
+    log("Received 'resuming' call event");
+    is(incomingCall, event.call);
+    is(incomingCall.state, "resuming");
+    gotResuming = true;
+  };
+
+  incomingCall.onconnected = function onconnected(event) {
+    log("Received 'connected' call event");
+    is(incomingCall, event.call);
+    is(incomingCall.state, "connected");
+    ok(gotResuming);
+
+    is(incomingCall, telephony.active);
+    is(telephony.calls.length, 1);
+    is(telephony.calls[0], incomingCall);
+
+    runEmulatorCmd("gsm list", function(result) {
+      log("Call list is now: " + result);
+      is(result[0], "inbound from " + number + " : active");
+      is(result[1], "OK");
+      hangUp();
+    });
+  };
+  incomingCall.resume();
+}
+
+function hangUp() {
+  log("Hanging up the incoming call.");
+
+  let gotDisconnecting = false;
+  incomingCall.ondisconnecting = function ondisconnecting(event) {
+    log("Received 'disconnecting' call event.");
+    is(incomingCall, event.call);
+    is(incomingCall.state, "disconnecting");
+    gotDisconnecting = true;
+  };
+
+  incomingCall.ondisconnected = function ondisconnected(event) {
+    log("Received 'disconnected' call event.");
+    is(incomingCall, event.call);
+    is(incomingCall.state, "disconnected");
+    ok(gotDisconnecting);
+
+    is(telephony.active, null);
+    is(telephony.calls.length, 0);
+
+    runEmulatorCmd("gsm list", function(result) {
+      log("Call list is now: " + result);
+      is(result[0], "OK");
+      cleanUp();
+    });
+  };
+  incomingCall.hangUp();
+}
+
+function cleanUp() {
+  telephony.onincoming = null;
+  SpecialPowers.removePermission("telephony", document);
+  finish();
+}
+
+// Start the test
+verifyInitialState();
