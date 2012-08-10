2252b6602653a8290a37afac5dd5626e552493a1
Eric Chou <echou@mozilla.com>
2012-08-10 11:17:06 +0800

    Bug 758504 - Final: patch 2: Implement function to remove reserved services. r=qdot

--- dom/bluetooth/BluetoothService.h
+++ dom/bluetooth/BluetoothService.h
@@ -196,6 +196,10 @@ public:
   AddReservedServicesInternal(const nsAString& aAdapterPath,
                               const nsTArray<PRUint32>& aServices) = 0;
 
+  virtual bool
+  RemoveReservedServicesInternal(const nsAString& aAdapterPath,
+                                 const nsTArray<PRUint32>& aServiceHandles) = 0;
+
   /**
    * Due to the fact that some operations require multiple calls, a
    * CommandThread is created that can run blocking, platform-specific calls
