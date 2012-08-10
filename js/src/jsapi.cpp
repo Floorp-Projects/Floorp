07f7e2103e649843f660ba8188a3c8b46a3a15d1
David Anderson <danderson@mozilla.com>
2012-08-10 15:49:13 -0700

    Remove IonMonkey-specific ParallelArray from the IonMonkey branch, in anticipation of a newer mozilla-central version.

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -54,7 +54,6 @@
 
 #include "builtin/Eval.h"
 #include "builtin/MapObject.h"
-#include "builtin/ParallelArray.h"
 #include "builtin/RegExp.h"
 #include "ds/LifoAlloc.h"
 #include "frontend/BytecodeCompiler.h"
@@ -1878,7 +1877,6 @@ static JSStdName standard_class_atoms[] = {
     {js_InitWeakMapClass,               EAGER_CLASS_ATOM(WeakMap), &js::WeakMapClass},
     {js_InitMapClass,                   EAGER_CLASS_ATOM(Map), &js::MapObject::class_},
     {js_InitSetClass,                   EAGER_CLASS_ATOM(Set), &js::SetObject::class_},
-    {js_InitParallelArrayClass,         EAGER_CLASS_ATOM(ParallelArray), &js::ParallelArrayClass},
     {NULL,                              0, NULL}
 };
 
