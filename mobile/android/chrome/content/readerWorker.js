2ec18563e6fe27f2e4b6b451b9a3e2f8b0abcba8
Brian Nicholson <bnicholson@mozilla.com>
2012-08-09 23:30:46 -0700

    Bug 779796 - Part 6: Remove article generator function in Readability.js. r=lucasr

--- mobile/android/chrome/content/readerWorker.js
+++ mobile/android/chrome/content/readerWorker.js
@@ -7,7 +7,6 @@ importScripts("JSDOMParser.js", "Readability.js");
 self.onmessage = function (msg) {
   let uri = msg.data.uri;
   let doc = new JSDOMParser().parse(msg.data.doc);
-  new Readability(uri, doc).parse(function (result) {
-    postMessage(result);
-  });
+  let article = new Readability(uri, doc).parse();
+  postMessage(article);
 };
