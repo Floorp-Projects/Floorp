90f873c09db731d80c708f5ab4aa91fb091a141d
Wan-Teh Chang <wtc@google.com>
2012-08-10 18:36:28 -0700

    Bug 780009: Update NSS to NSS_3_13_6_RTM. Note that this overwrites the changes to the three Makefile.in files in dbm/ in the changeset https://hg.mozilla.org/mozilla-central/rev/162130598df0 for bug 774032.

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -36,7 +36,7 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
-/* $Id: nss.h,v 1.92.2.5 2012/06/01 19:40:00 kaie%kuix.de Exp $ */
+/* $Id: nss.h,v 1.92.2.6 2012/08/11 01:10:37 wtc%google.com Exp $ */
 
 #ifndef __nss_h_
 #define __nss_h_
@@ -66,12 +66,12 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define NSS_VERSION  "3.13.6.0" _NSS_ECC_STRING _NSS_CUSTOMIZED " Beta"
+#define NSS_VERSION  "3.13.6.0" _NSS_ECC_STRING _NSS_CUSTOMIZED
 #define NSS_VMAJOR   3
 #define NSS_VMINOR   13
 #define NSS_VPATCH   6
 #define NSS_VBUILD   0
-#define NSS_BETA     PR_TRUE
+#define NSS_BETA     PR_FALSE
 
 #ifndef RC_INVOKED
 
