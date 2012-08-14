5b01f29a2099c0ae64f873f0b621e2b556b99e83
Aryeh Gregor <ayg@aryeh.name>
2012-08-14 12:10:42 +0300

    Bug 782605 - Make nsNPAPIPluginInstance::SetCached infallible; r=roc

--- dom/plugins/base/nsNPAPIPlugin.cpp
+++ dom/plugins/base/nsNPAPIPlugin.cpp
@@ -2443,7 +2443,8 @@ _setvalue(NPP npp, NPPVariable variable, void *result)
 
     case NPPVpluginKeepLibraryInMemory: {
       NPBool bCached = (result != nullptr);
-      return inst->SetCached(bCached);
+      inst->SetCached(bCached);
+      return NPERR_NO_ERROR;
     }
 
     case NPPVpluginUsesDOMForCursorBool: {
