581fed4f7b73b79eb582981eba554db87472f034
Joe Drew <joe@drew.ca>
2012-08-14 14:06:12 -0400

    Bug 782416 - Switch from NULL to nullptr in gfx::2d. r=Bas

--- gfx/2d/PathD2D.cpp
+++ gfx/2d/PathD2D.cpp
@@ -236,7 +236,7 @@ PathBuilderD2D::Finish()
   HRESULT hr = mSink->Close();
   if (FAILED(hr)) {
     gfxDebug() << "Failed to close PathSink. Code: " << hr;
-    return NULL;
+    return nullptr;
   }
 
   return new PathD2D(mGeometry, mFigureActive, mCurrentPoint, mFillRule);
@@ -256,14 +256,14 @@ PathD2D::TransformedCopyToBuilder(const Matrix &aTransform, FillRule aFillRule) 
 
   if (FAILED(hr)) {
     gfxWarning() << "Failed to create PathGeometry. Code: " << hr;
-    return NULL;
+    return nullptr;
   }
 
   RefPtr<ID2D1GeometrySink> sink;
   hr = path->Open(byRef(sink));
   if (FAILED(hr)) {
     gfxWarning() << "Failed to open Geometry for writing. Code: " << hr;
-    return NULL;
+    return nullptr;
   }
 
   if (aFillRule == FILL_WINDING) {
