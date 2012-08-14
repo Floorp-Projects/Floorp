3f0afe00d08c52c90b63bc0304bcf55d6d87fdc9
Joe Drew <joe@drew.ca>
2012-08-14 14:06:12 -0400

    Bug 782416 - Switch from NULL to nullptr in gfx::2d. r=Bas

--- gfx/2d/unittest/TestDrawTargetD2D.cpp
+++ gfx/2d/unittest/TestDrawTargetD2D.cpp
@@ -8,9 +8,9 @@
 using namespace mozilla::gfx;
 TestDrawTargetD2D::TestDrawTargetD2D()
 {
-  ::D3D10CreateDevice1(NULL,
+  ::D3D10CreateDevice1(nullptr,
                        D3D10_DRIVER_TYPE_HARDWARE,
-                       NULL,
+                       nullptr,
                        D3D10_CREATE_DEVICE_BGRA_SUPPORT |
                        D3D10_CREATE_DEVICE_PREVENT_INTERNAL_THREADING_OPTIMIZATIONS,
                        D3D10_FEATURE_LEVEL_10_0,
