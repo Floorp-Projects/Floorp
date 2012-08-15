9f25b56af3bbe6016fc0a10cc941ff5a5455efc4
Bill McCloskey <wmccloskey@mozilla.com>
2012-08-15 10:39:48 -0700

    Bug 781390 - Make barrier verifier testing work better with the methodjit (r=bhackett)

--- js/src/vm/Stack.cpp
+++ js/src/vm/Stack.cpp
@@ -185,7 +185,7 @@ StackFrame::prevpcSlow(InlinedSite **pinlined)
     JS_ASSERT(!(flags_ & HAS_PREVPC));
 #if defined(JS_METHODJIT) && defined(JS_MONOIC)
     StackFrame *p = prev();
-    mjit::JITScript *jit = p->script()->getJIT(p->isConstructing(), p->compartment()->needsBarrier());
+    mjit::JITScript *jit = p->script()->getJIT(p->isConstructing(), p->compartment()->compileBarriers());
     prevpc_ = jit->nativeToPC(ncode_, &prevInline_);
     flags_ |= HAS_PREVPC;
     if (pinlined)
