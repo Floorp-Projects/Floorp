a8d01917fd2ab44ff3e54aec8f9edc53b223d694
Bill McCloskey <wmccloskey@mozilla.com>
2012-08-16 14:03:18 -0700

    Bug 782337 - Stack clobbering (r=bhackett)

--- js/src/vm/Stack.h
+++ js/src/vm/Stack.h
@@ -1363,6 +1363,8 @@ class StackSpace
         return (Value *)fp >= base_ && (Value *)fp <= trustedEnd_;
     }
 
+    void markAndClobberFrame(JSTracer *trc, StackFrame *fp, Value *slotsEnd, jsbytecode *pc);
+
   public:
     StackSpace();
     bool init();
@@ -1414,8 +1416,7 @@ class StackSpace
     bool tryBumpLimit(JSContext *cx, Value *from, unsigned nvals, Value **limit);
 
     /* Called during GC: mark segments, frames, and slots under firstUnused. */
-    void mark(JSTracer *trc);
-    void markFrameValues(JSTracer *trc, StackFrame *fp, Value *slotsEnd, jsbytecode *pc);
+    void markAndClobber(JSTracer *trc);
 
     /* Called during GC: sets active flag on compartments with active frames. */
     void markActiveCompartments();
