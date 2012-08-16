57bf4ce23bfac9af6cb80374ab847db76105e8b3
Chris Jones <jones.chris.g@gmail.com>
2012-08-16 12:34:53 -0700

    Bug 776835: Check process capabilities in hal. r=blassey,jlebar

--- dom/ipc/AppProcessPermissions.h
+++ dom/ipc/AppProcessPermissions.h
@@ -15,6 +15,10 @@ class PBrowserParent;
 class PContentParent;
 }
 
+namespace hal_sandbox {
+class PHalParent;
+}
+
 /**
  * Return true iff the specified browser has the specified capability.
  */
@@ -28,6 +32,10 @@ AppProcessHasPermissions(mozilla::dom::PBrowserParent* aActor,
  */
 bool
 AppProcessHasPermission(mozilla::dom::PContentParent* aActor,
+                        const char* aPermission);
+
+bool
+AppProcessHasPermission(mozilla::hal_sandbox::PHalParent* aActor,
                         const char* aPermission);
 
 // NB: when adding capability checks for other IPDL actors, please add
