892314f60383b77fe15c2b90178c2fef20703563
Chris Jones <jones.chris.g@gmail.com>
2012-08-15 17:54:18 -0700

    Bug 783134: Work around crashing on startup with fullscreen widgets. r=roc

--- xpfe/appshell/src/nsWebShellWindow.cpp
+++ xpfe/appshell/src/nsWebShellWindow.cpp
@@ -252,7 +252,9 @@ nsWebShellWindow::WindowResized(nsIWidget* aWidget, PRInt32 aWidth, PRInt32 aHei
   }
 
   nsCOMPtr<nsIBaseWindow> shellAsWin(do_QueryInterface(mDocShell));
-  shellAsWin->SetPositionAndSize(0, 0, aWidth, aHeight, false);
+  if (shellAsWin) {
+    shellAsWin->SetPositionAndSize(0, 0, aWidth, aHeight, false);
+  }
   // Persist size, but not immediately, in case this OS is firing
   // repeated size events as the user drags the sizing handle
   if (!IsLocked())
