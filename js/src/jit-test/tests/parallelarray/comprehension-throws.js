4c3ee44470c3d9782ee9e6a99103ed7dfd129ae2
Ryan VanderMeulen <ryanvm@gmail.com>
2012-08-16 17:44:18 -0400

    Backout 06dae3ea0f89 (bug 778559) due to Windows build failures.

--- js/src/jit-test/tests/parallelarray/comprehension-throws.js
+++
@@ -1,7 +0,0 @@
-// |jit-test| error: TypeError;
-function buildComprehension() {
-  // Throws if elemental fun not callable
-  var p = new ParallelArray([2,2], undefined);
-}
-
-buildComprehension();
--- js/src/jit-test/tests/parallelarray/map-throws.js
+++
@@ -1,9 +0,0 @@
-// |jit-test| error: TypeError;
-function testMap() {
-  // Test map throws
-  var p = new ParallelArray([1,2,3,4])
-  var m = p.map(42);
-}
-
-testMap();
-
