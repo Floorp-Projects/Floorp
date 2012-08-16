f1cd9d5c9d3a747665d5250dc0aead446bd91a55
Ryan VanderMeulen <ryanvm@gmail.com>
2012-08-16 17:44:18 -0400

    Backout 06dae3ea0f89 (bug 778559) due to Windows build failures.

--- js/src/jit-test/tests/parallelarray/comprehension-1.js
+++
@@ -1,9 +0,0 @@
-
-function buildComprehension() {
-  // 1D comprehension 
-  var p = new ParallelArray(10, function (idx) { return idx; });
-  var a = [0,1,2,3,4,5,6,7,8,9];
-  assertEq(p.toString(), "<" + a.join(",") + ">");
-}
-
-buildComprehension();
