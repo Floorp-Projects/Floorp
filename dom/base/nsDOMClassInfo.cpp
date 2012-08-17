c70f83ae1420be61c6a44b0f8ae2792fc0dca592
Eric Chou <echou@mozilla.com>
2012-08-17 18:35:59 +0800

    Bug 730992 - Patch 1: The DOM boilerplate for BluetoothPairingEvent, r=qdot, sr=mrbkap
    ---
     dom/base/nsDOMClassInfo.cpp                   |    9 +++
     dom/base/nsDOMClassInfoClasses.h              |    1 +
     dom/bluetooth/BluetoothPairingEvent.cpp       |   78 +++++++++++++++++++++++++
     dom/bluetooth/BluetoothPairingEvent.h         |   70 ++++++++++++++++++++++
     dom/bluetooth/Makefile.in                     |    2 +
     dom/bluetooth/nsIDOMBluetoothPairingEvent.idl |   15 +++++
     6 files changed, 175 insertions(+), 0 deletions(-)
     create mode 100644 dom/bluetooth/BluetoothPairingEvent.cpp
     create mode 100644 dom/bluetooth/BluetoothPairingEvent.h
     create mode 100644 dom/bluetooth/nsIDOMBluetoothPairingEvent.idl

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -520,6 +520,7 @@ using mozilla::dom::indexedDB::IDBWrapperCache;
 #include "BluetoothDevice.h"
 #include "BluetoothDeviceEvent.h"
 #include "BluetoothPropertyEvent.h"
+#include "BluetoothPairingEvent.h"
 #endif
 
 #include "nsIDOMNavigatorSystemMessages.h"
@@ -1683,6 +1684,8 @@ static nsDOMClassInfoData sClassInfoData[] = {
   NS_DEFINE_CLASSINFO_DATA(BluetoothDeviceEvent, nsDOMGenericSH,
                            DOM_DEFAULT_SCRIPTABLE_FLAGS)
   NS_DEFINE_CLASSINFO_DATA(BluetoothPropertyEvent, nsDOMGenericSH,
+                           DOM_DEFAULT_SCRIPTABLE_FLAGS)
+  NS_DEFINE_CLASSINFO_DATA(BluetoothPairingEvent, nsDOMGenericSH,
                            DOM_DEFAULT_SCRIPTABLE_FLAGS)
 #endif
 
@@ -4488,6 +4491,12 @@ nsDOMClassInfo::Init()
     DOM_CLASSINFO_MAP_ENTRY(nsIDOMBluetoothPropertyEvent)
     DOM_CLASSINFO_MAP_ENTRY(nsIDOMEvent)
   DOM_CLASSINFO_MAP_END
+
+  DOM_CLASSINFO_MAP_BEGIN(BluetoothPairingEvent, nsIDOMBluetoothPairingEvent)
+    DOM_CLASSINFO_MAP_ENTRY(nsIDOMBluetoothPairingEvent)
+    DOM_CLASSINFO_MAP_ENTRY(nsIDOMEvent)
+  DOM_CLASSINFO_MAP_END
+
 #endif
 
   DOM_CLASSINFO_MAP_BEGIN(CameraManager, nsIDOMCameraManager)
