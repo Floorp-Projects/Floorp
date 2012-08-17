c64b3cb73b9a4207decbcb770607bc5b333f7da5
Brian R. Bondy <netzen@gmail.com>
2012-08-17 11:35:01 -0400

    Bug 731307 - Use async filepicker from /content. r=mounir

--- content/html/content/src/nsHTMLInputElement.h
+++ content/html/content/src/nsHTMLInputElement.h
@@ -18,6 +18,7 @@
 #include "nsDOMFile.h"
 #include "nsHTMLFormElement.h" // for ShouldShowInvalidUI()
 #include "nsIFile.h"
+#include "nsIFilePicker.h"
 
 class nsDOMFileList;
 class nsIFilePicker;
@@ -728,6 +729,38 @@ private:
     // mapping, which can be different accross OS, user's personal configuration, ...)
     // For now, only mask filters are considered to be "trusted".
     bool mIsTrusted; 
+  };
+
+  class AsyncClickHandler
+    : public nsRunnable
+  {
+  public:
+    AsyncClickHandler(nsHTMLInputElement* aInput);
+    NS_IMETHOD Run();
+
+  protected:
+    nsRefPtr<nsHTMLInputElement> mInput;
+    PopupControlState mPopupControlState;
+  };
+
+  class nsFilePickerShownCallback
+    : public nsIFilePickerShownCallback
+  {
+  public:
+    nsFilePickerShownCallback(nsHTMLInputElement* aInput,
+                              nsIFilePicker* aFilePicker,
+                              bool aMulti);
+    virtual ~nsFilePickerShownCallback()
+    { }
+
+    NS_DECL_ISUPPORTS
+
+    NS_IMETHOD Done(PRInt16 aResult);
+
+  private:
+    nsCOMPtr<nsIFilePicker> mFilePicker;
+    nsRefPtr<nsHTMLInputElement> mInput;
+    bool mMulti;
   };
 };
 
