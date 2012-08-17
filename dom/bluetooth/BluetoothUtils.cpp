b6812c2a11d4ed3a676f3867435183f522e7cb4a
Gina Yeh <gyeh@mozilla.com>
2012-08-17 10:53:45 +0800

    Bug 777671 - Final version: add function getPairedDevices() in nsIDOMBluetoothAdapter, r=qdot, sr=mrbkap
    ---
     dom/bluetooth/BluetoothAdapter.cpp           |   97 +++++++++++++++++++++++++-
     dom/bluetooth/BluetoothAdapter.h             |    2 +-
     dom/bluetooth/BluetoothPropertyContainer.cpp |   32 +--------
     dom/bluetooth/BluetoothPropertyContainer.h   |   26 +------
     dom/bluetooth/BluetoothService.h             |   11 ++-
     dom/bluetooth/BluetoothUtils.cpp             |   48 ++++++++++++-
     dom/bluetooth/BluetoothUtils.h               |    9 ++-
     dom/bluetooth/linux/BluetoothDBusService.cpp |   93 +++++++++++++++++++++++-
     dom/bluetooth/linux/BluetoothDBusService.h   |    4 +-
     dom/bluetooth/nsIDOMBluetoothAdapter.idl     |    3 +-
     10 files changed, 261 insertions(+), 64 deletions(-)

--- dom/bluetooth/BluetoothUtils.cpp
+++ dom/bluetooth/BluetoothUtils.cpp
@@ -1,10 +1,12 @@
 /* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
-/* vim: set ts=2 et sw=2 tw=40: */
+/* vim: set ts=2 et sw=2 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "BluetoothUtils.h"
+#include "nsContentUtils.h"
+#include "BluetoothDevice.h"
 #include "jsapi.h"
 #include "nsTArray.h"
 #include "nsString.h"
@@ -59,3 +61,47 @@ mozilla::dom::bluetooth::StringArrayToJSArray(JSContext* aCx, JSObject* aGlobal,
   return NS_OK;
 }
 
+nsresult
+mozilla::dom::bluetooth::BluetoothDeviceArrayToJSArray(JSContext* aCx, JSObject* aGlobal,
+                                                       const nsTArray<nsRefPtr<BluetoothDevice> >& aSourceArray,
+                                                       JSObject** aResultArray)
+{
+  NS_ASSERTION(aCx, "Null context!");
+  NS_ASSERTION(aGlobal, "Null global!");
+
+  JSAutoRequest ar(aCx);
+  JSAutoEnterCompartment ac;
+  if (!ac.enter(aCx, aGlobal)) {
+    NS_WARNING("Failed to enter compartment!");
+    return NS_ERROR_FAILURE;
+  }
+
+  JSObject* arrayObj;
+
+  if (aSourceArray.IsEmpty()) {
+    arrayObj = JS_NewArrayObject(aCx, 0, nullptr);
+  } else {
+    uint32_t valLength = aSourceArray.Length();
+    mozilla::ScopedDeleteArray<jsval> valArray(new jsval[valLength]);
+    JS::AutoArrayRooter tvr(aCx, valLength, valArray);
+    for (PRUint32 index = 0; index < valLength; index++) {
+      nsISupports* obj = aSourceArray[index]->ToISupports();
+      nsresult rv =
+        nsContentUtils::WrapNative(aCx, aGlobal, obj, &valArray[index]);
+      NS_ENSURE_SUCCESS(rv, rv);
+    }
+
+    arrayObj = JS_NewArrayObject(aCx, valLength, valArray);
+  }
+
+  if (!arrayObj) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+
+  if (!JS_FreezeObject(aCx, arrayObj)) {
+    return NS_ERROR_FAILURE;
+  }
+
+  *aResultArray = arrayObj;
+  return NS_OK;
+}
