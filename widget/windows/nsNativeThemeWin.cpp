a865bb103c08e4ad84cb394cae89d1d38b691352
Tim Abraldes <tabraldes@mozilla.com>
2012-08-20 12:11:31 -0700

    bug 758280. Refactor the way we handle `WM_NCCALCSIZE` messages and how we keep track of our frame offsets. r=jimm

--- widget/windows/nsNativeThemeWin.cpp
+++ widget/windows/nsNativeThemeWin.cpp
@@ -1768,7 +1768,8 @@ nsNativeThemeWin::GetWidgetPadding(nsDeviceContext* aContext,
     // the border padding. This should be addressed in nsWindow,
     // but currently can't be, see UpdateNonClientMargins.
     if (aWidgetType == NS_THEME_WINDOW_TITLEBAR_MAXIMIZED)
-      aResult->top = GetSystemMetrics(SM_CXFRAME);
+      aResult->top = GetSystemMetrics(SM_CXFRAME)
+                   + GetSystemMetrics(SM_CXPADDEDBORDER);
     return true;
   }
 
