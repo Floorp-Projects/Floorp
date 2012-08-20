192f94dbc666e6898e0c779a6d40f28f0d7bb3e7
Kartikaya Gupta <kgupta@mozilla.com>
2012-08-20 15:43:53 -0400

    Bug 781220 - Hide other GeckoLayerClient methods and expose them via the LayerView API. r=sriram

--- mobile/android/base/TextSelectionHandle.java
+++ mobile/android/base/TextSelectionHandle.java
@@ -4,8 +4,8 @@
 
 package org.mozilla.gecko;
 
-import org.mozilla.gecko.gfx.GeckoLayerClient;
 import org.mozilla.gecko.gfx.ImmutableViewportMetrics;
+import org.mozilla.gecko.gfx.LayerView;
 
 import org.json.JSONObject;
 
@@ -83,15 +83,15 @@ class TextSelectionHandle extends ImageView implements View.OnTouchListener {
         mLeft = mLeft + newX - mTouchStartX;
         mTop = mTop + newY - mTouchStartY;
 
-        GeckoLayerClient layerClient = GeckoApp.mAppContext.getLayerClient();
-        if (layerClient == null) {
-            Log.e(LOGTAG, "Can't move selection because layerClient is null");
+        LayerView layerView = GeckoApp.mAppContext.getLayerView();
+        if (layerView == null) {
+            Log.e(LOGTAG, "Can't move selection because layerView is null");
             return;
         }
         // Send x coordinate on the right side of the start handle, left side of the end handle.
         float left = (float) mLeft + (mHandleType.equals(HandleType.START) ? mWidth - mShadow : mShadow);
         PointF geckoPoint = new PointF(left, (float) mTop);
-        geckoPoint = layerClient.convertViewPointToLayerPoint(geckoPoint);
+        geckoPoint = layerView.convertViewPointToLayerPoint(geckoPoint);
 
         JSONObject args = new JSONObject();
         try {
@@ -107,14 +107,14 @@ class TextSelectionHandle extends ImageView implements View.OnTouchListener {
     }
 
     void positionFromGecko(int left, int top) {
-        GeckoLayerClient layerClient = GeckoApp.mAppContext.getLayerClient();
-        if (layerClient == null) {
-            Log.e(LOGTAG, "Can't position handle because layerClient is null");
+        LayerView layerView = GeckoApp.mAppContext.getLayerView();
+        if (layerView == null) {
+            Log.e(LOGTAG, "Can't position handle because layerView is null");
             return;
         }
 
         mGeckoPoint = new PointF((float) left, (float) top);
-        ImmutableViewportMetrics metrics = layerClient.getViewportMetrics();
+        ImmutableViewportMetrics metrics = layerView.getViewportMetrics();
         repositionWithViewport(metrics.viewportRectLeft, metrics.viewportRectTop, metrics.zoomFactor);
     }
 
