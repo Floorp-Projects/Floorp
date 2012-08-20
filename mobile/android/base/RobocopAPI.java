344f0be60b78e6471deae935c5176ed447625595
Kartikaya Gupta <kgupta@mozilla.com>
2012-08-20 15:43:53 -0400

    Bug 781220 - Remove the mLayerClient field from GeckoApp. r=sriram

--- mobile/android/base/RobocopAPI.java
+++ mobile/android/base/RobocopAPI.java
@@ -35,7 +35,7 @@ public class RobocopAPI {
     }
 
     public void setDrawListener(GeckoLayerClient.DrawListener listener) {
-        mGeckoApp.getLayerClient().setDrawListener(listener);
+        mGeckoApp.getLayerView().getLayerClient().setDrawListener(listener);
     }
 
     public Cursor querySql(String dbPath, String query) {
