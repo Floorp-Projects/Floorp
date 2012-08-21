eb7fa952b189188236580d15f8a11ff92bab992f
Wes Johnston <wjohnston@mozilla.com>
2012-08-21 13:39:39 -0700

    backout c5961842578e CLOSED TREE

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -171,7 +171,7 @@ abstract public class GeckoApp
     private HashMap<String, PowerManager.WakeLock> mWakeLocks = new HashMap<String, PowerManager.WakeLock>();
 
     protected int mRestoreMode = GeckoAppShell.RESTORE_NONE;
-    protected boolean mInitialized = false;
+    private boolean mInitialized = false;
 
     public enum LaunchState {Launching, WaitForDebugger,
                              Launched, GeckoRunning, GeckoExiting};
@@ -1921,7 +1921,7 @@ abstract public class GeckoApp
      * Handles getting a uri from and intent in a way that is backwards
      * compatable with our previous implementations
      */
-    protected String getURIFromIntent(Intent intent) {
+    private String getURIFromIntent(Intent intent) {
         String uri = intent.getDataString();
         if (uri != null)
             return uri;
