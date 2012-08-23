8298af775b48d467d79ba77aaf5fd502623f03e5
Kyle Machulis <kyle@nonpolynomial.com>
2012-08-22 17:58:05 -0700

    Bug 783426: Patch 2 - Tests for Async DOMRequest Firing

--- testing/mochitest/tests/SimpleTest/specialpowersAPI.js
+++ testing/mochitest/tests/SimpleTest/specialpowersAPI.js
@@ -924,7 +924,8 @@ SpecialPowersAPI.prototype = {
     var serv = Cc["@mozilla.org/dom/dom-request-service;1"].
       getService(Ci.nsIDOMRequestService);
     var res = { __exposedProps__: {} };
-    var props = ["createRequest", "fireError", "fireSuccess"];
+    var props = ["createRequest", "fireError", "fireSuccess", 
+								 "fireErrorAsync", "fireSuccessAsync"];
     for (i in props) {
       let prop = props[i];
       res[prop] = function() { return serv[prop].apply(serv, arguments) };
