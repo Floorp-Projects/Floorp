a3a9964e2911bc7e90169db5859f8febd097ac3e
Mounir Lamouri <mounir.lamouri@gmail.com>
2012-08-23 11:37:31 -0700

    Bug 777072 - 4/7 - Update nsPermission to use appId/isInBrowserElement. r=sicking

--- extensions/cookie/nsPermission.h
+++ extensions/cookie/nsPermission.h
@@ -19,19 +19,23 @@ public:
   NS_DECL_NSIPERMISSION
 
   nsPermission(const nsACString &aHost,
-               const nsACString &aType, 
+               uint32_t aAppId,
+               bool aIsInBrowserElement,
+               const nsACString &aType,
                uint32_t aCapability,
                uint32_t aExpireType,
                int64_t aExpireTime);
 
-  virtual ~nsPermission();
-  
+  virtual ~nsPermission() {};
+
 protected:
   nsCString mHost;
   nsCString mType;
   uint32_t  mCapability;
   uint32_t  mExpireType;
   int64_t   mExpireTime;
+  uint32_t  mAppId;
+  bool      mIsInBrowserElement;
 };
 
 #endif // nsPermission_h__
