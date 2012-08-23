16ca2e4d859e706fcb53aa6006d1a2b58c0564bf
Bobby Holley <bobbyholley@gmail.com>
2012-08-23 16:44:52 -0700

    Bug 774633 - Remove dead mOpenerScriptPrincipal. r=jst This value is initialized to null and never set, so we can get rid of it.

--- dom/base/nsGlobalWindow.h
+++ dom/base/nsGlobalWindow.h
@@ -344,7 +344,6 @@ public:
 
   virtual NS_HIDDEN_(void) SetOpenerScriptPrincipal(nsIPrincipal* aPrincipal);
   virtual NS_HIDDEN_(void) SetInitialPrincipalToSubject();
-  virtual NS_HIDDEN_(nsIPrincipal*) GetOpenerScriptPrincipal();
 
   virtual NS_HIDDEN_(PopupControlState) PushPopupControlState(PopupControlState state, bool aForce) const;
   virtual NS_HIDDEN_(void) PopPopupControlState(PopupControlState state) const;
@@ -1015,8 +1014,6 @@ protected:
   nsCOMPtr<nsIDOMStorage>      mSessionStorage;
 
   nsCOMPtr<nsIXPConnectJSObjectHolder> mInnerWindowHolder;
-  nsCOMPtr<nsIPrincipal> mOpenerScriptPrincipal; // strong; used to determine
-                                                 // whether to clear scope
 
   // These member variable are used only on inner windows.
   nsRefPtr<nsEventListenerManager> mListenerManager;
