a0190a2b7face8b3604bc90c7b9757de6c01a375
Mark Capella <markcapella@twcny.rr.com>
2012-08-23 08:16:44 -0400

    Bug 772272 - Remove do_load_httpd_js from xpcshell tests, Final, r=ted              Remove last three references, final cleanup

--- netwerk/test/httpserver/test/head_utils.js
+++ netwerk/test/httpserver/test/head_utils.js
@@ -4,7 +4,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-do_load_httpd_js();
+/**
+ * Loads _HTTPD_JS_PATH file, which is dynamically defined by
+ * <runxpcshelltests.py>.
+ */
+load(_HTTPD_JS_PATH);
 
 // if these tests fail, we'll want the debug output
 DEBUG = true;
