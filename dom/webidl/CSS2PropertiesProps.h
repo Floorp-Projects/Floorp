61a59802bd8d8f0c8380348db153d6616063ec09
Boris Zbarsky <bzbarsky@mit.edu>
2012-08-23 21:08:09 -0700

    Bug 753517 part 4.  Set up auto-generation of CSS2Properties.webidl from nsCSSPropList.h and enable Paris bindings for CSSStyleDeclaration and CSS2Properties.  r=khuey,peterv,dbaron

---
+++ dom/webidl/CSS2PropertiesProps.h
@@ -0,0 +1,34 @@
+/* A file meant as input to the preprocessor only */
+
+/* DO_PROP serves as an extra level of indirection to allow expansion
+   of CSS_PROP_DOMPROP_PREFIXED */
+
+[
+
+#define DO_PROP(method, pref) \
+  [ #method, pref ],
+#define CSS_PROP(name, id, method, flags, pref, parsevariant, kwtable, \
+		 stylestruct, stylestructofset, animtype) \
+  DO_PROP(method, pref)
+#define CSS_PROP_SHORTHAND(name, id, method, flags, pref) \
+  DO_PROP(method, pref)
+#define CSS_PROP_DOMPROP_PREFIXED(val) Moz##val
+#define CSS_PROP_LIST_EXCLUDE_INTERNAL
+
+#include "nsCSSPropList.h"
+
+#undef CSS_PROP_LIST_EXCLUDE_INTERNAL
+#undef CSS_PROP_DOMPROP_PREFIXED
+#undef CSS_PROP_SHORTHAND
+#undef CSS_PROP
+
+#define CSS_PROP_ALIAS(name, id, method, pref) \
+  DO_PROP(method, pref)
+
+#include "nsCSSPropAliasList.h"
+
+#undef CSS_PROP_ALIAS
+
+#undef DO_PROP
+
+]
