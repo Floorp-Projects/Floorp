2d6968cf36b98ea9e27f66032a9c58c7cb0233c5
Paul Rouget <paul@mozilla.com>
2012-08-25 14:51:36 +0200

    Bug 770818 - [inspector] highlighter v3. r=jwalker

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1007,8 +1007,6 @@ pref("devtools.inspector.htmlHeight", 112);
 pref("devtools.inspector.htmlPanelOpen", false);
 pref("devtools.inspector.sidebarOpen", false);
 pref("devtools.inspector.activeSidebar", "ruleview");
-pref("devtools.inspector.highlighterShowVeil", true);
-pref("devtools.inspector.highlighterShowInfobar", true);
 
 // Enable the Layout View
 pref("devtools.layoutview.enabled", true);
