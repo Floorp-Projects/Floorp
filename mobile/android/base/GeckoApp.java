6fd656a6868113f93a6e66a4c722f9a8f328f203
Kartikaya Gupta <kgupta@mozilla.com>
2012-08-27 16:44:27 -0400

    Bug 781883 - Ensure the GeckoApp unregisters itself as a tab listener on teardown, or it ends up getting registered multiple times. r=sriram

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -2167,6 +2167,8 @@ abstract public class GeckoApp
         if (mBatteryReceiver != null)
             mBatteryReceiver.unregisterFor(mAppContext);
 
+        Tabs.unregisterOnTabsChangedListener(this);
+
         ((GeckoApplication) getApplication()).removeApplicationLifecycleCallbacks(this);
     }
 
