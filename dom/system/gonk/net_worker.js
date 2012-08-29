3e2271b85f6c5bbe41e94fdff614037b97dfa22d
Vincent Chang <vchang@mozilla.com>
2012-08-30 03:27:08 +0800

    Bug 735547 - Support Wifi/USB Tethering, Part 3: NetworkManager. r=philikon

--- dom/system/gonk/net_worker.js
+++ dom/system/gonk/net_worker.js
@@ -15,9 +15,85 @@
 
 "use strict";
 
-const DEBUG = true;
+const DEBUG = false;
+
+const USB_FUNCTION_PATH  = "sys.usb.config";
+const USB_FUNCTION_STATE = "sys.usb.state";
+const USB_FUNCTION_RNDIS = "rndis,adb";
+const USB_FUNCTION_ADB   = "adb";
+// Retry 20 times (2 seconds) for usb state transition.
+const USB_FUNCTION_RETRY_TIMES = 20;
+// Check "sys.usb.state" every 100ms.
+const USB_FUNCTION_RETRY_INTERVAL = 100;
+
+// 2xx - Requested action has been successfully completed
+const NETD_COMMAND_OKAY  = 200;
+const NETD_COMMAND_ERROR = 300;
 
 importScripts("systemlibs.js");
+
+let gWifiFailChain = [stopSoftAP,
+                      setIpForwardingEnabled,
+                      stopTethering];
+
+function wifiTetheringFail(params) {
+  let unload = false;
+
+  // Unload wifi driver when failing to enable tethering.
+  if (params.enable) {
+    unload = true;
+  }
+  params.unload = unload;
+  // Notify the main thread.
+  postMessage(params);
+
+  // If one of the stages fails, we try roll back to ensure
+  // we don't leave the network systems in limbo.
+
+  // This parameter is used to disable ipforwarding.
+  params.enable = false;
+  chain(params, gWifiFailChain, null);
+}
+
+function wifiTetheringSuccess(params) {
+  let unload = false;
+
+  // Unload wifi driver after switch wifi to station mode.
+  if (!params.enable) {
+    unload = true;
+  }
+
+  params.unload = unload;
+  // Notify the main thread.
+  postMessage(params);
+  return true;
+}
+
+let gUSBFailChain = [stopSoftAP,
+                     setIpForwardingEnabled,
+                     stopTethering];
+
+function usbTetheringFail(params) {
+  // Notify the main thread.
+  postMessage(params);
+  // Try to roll back to ensure
+  // we don't leave the network systems in limbo.
+  let functionChain = [setIpForwardingEnabled,
+                       stopTethering];
+
+  // This parameter is used to disable ipforwarding.
+  params.enable = false;
+  chain(params, gUSBFailChain, null);
+
+  // Disable usb rndis function.
+  setUSBFunction({usbfunc: USB_FUNCTION_ADB, report: false});
+}
+
+function usbTetheringSuccess(params) {
+  // Notify the main thread.
+  postMessage(params);
+  return true;
+}
 
 /**
  * Get network interface properties from the system property table.
@@ -46,8 +122,12 @@ function getIFProperties(ifname) {
 self.onmessage = function onmessage(event) {
   let message = event.data;
   if (DEBUG) debug("received message: " + JSON.stringify(message));
+  // We have to keep the id in message. It will be used when post the result
+  // to NetworkManager later.
   let ret = self[message.cmd](message);
-  postMessage({id: message.id, ret: ret});
+  if (!message.isAsync) {
+    postMessage({id: message.id, ret: ret});
+  }
 };
 
 /**
@@ -86,8 +166,342 @@ function runDHCPAndSetDefaultRouteAndDNS(options) {
   setDefaultRouteAndDNS(dhcp);
 }
 
-if (!this.debug) {
-  this.debug = function debug(message) {
-    dump("Network Worker: " + message + "\n");
-  };
+let gCommandQueue = [];
+let gCurrentCommand = null;
+let gCurrentCallback = null;
+let gPending = false;
+
+/**
+ * Handle received data from netd.
+ */
+function onNetdMessage(data) {
+  let result = "";
+  let reason = "";
+
+  // The return result is separated from the reason by a space character.
+  let i = 0;
+  while (i < data.length) {
+    let octet = data[i];
+    i += 1;
+    if (octet == 32) {
+      break;
+    }
+    result += String.fromCharCode(octet);
+  }
+
+  let code = parseInt(result);
+
+  for (; i < data.length; i++) {
+    let octet = data[i];
+    reason += String.fromCharCode(octet);
+  }
+
+  // Set pending to false before we handle next command.
+  gPending = false;
+  debug("Receiving '" + gCurrentCommand + "' command response from netd.");
+  debug("          ==> Code: " + code + "  Reason: " + reason);
+  if (gCurrentCallback) {
+    let error;
+    (code >= NETD_COMMAND_OKAY && code < NETD_COMMAND_ERROR) ? (error = false) : (error = true);
+    gCurrentCallback(error, {code: code, reason: reason});
+  }
+
+  // Handling pending commands if any.
+  nextNetdCommand();
 }
+
+/**
+ * Send command to netd.
+ */
+function doCommand(command, callback) {
+  debug("Preparing to send '" + command + "' command...");
+  gCommandQueue.push([command, callback]);
+  return nextNetdCommand();
+}
+
+function nextNetdCommand() {
+  if (!gCommandQueue.length || gPending) {
+    return true;
+  }
+  [gCurrentCommand, gCurrentCallback] = gCommandQueue.shift();
+  debug("Sending '" + gCurrentCommand + "' command to netd.");
+  gPending = true;
+  return postNetdCommand(gCurrentCommand);
+}
+
+function setInterfaceUp(params, callback) {
+  let command = "interface setcfg " + params.ifname + " " + params.ip + " " +
+                params.prefix + " " + "[" + params.link + "]";
+  return doCommand(command, callback);
+}
+
+function setInterfaceDown(params, callback) {
+  let command = "interface setcfg " + params.ifname + " " + params.ip + " " +
+                params.prefix + " " + "[" + params.link + "]";
+  return doCommand(command, callback);
+}
+
+function setIpForwardingEnabled(params, callback) {
+  let command;
+
+  if (params.enable) {
+    command = "ipfwd enable";
+  } else {
+    command = "ipfwd disable";
+  }
+  return doCommand(command, callback);
+}
+
+function startTethering(params, callback) {
+  let command = "tether start " + params.startIp + " " + params.endIp;
+  return doCommand(command, callback);
+}
+
+function stopTethering(params, callback) {
+  let command = "tether stop";
+  return doCommand(command, callback);
+}
+
+function tetherInterface(params, callback) {
+  let command = "tether interface add " + params.ifname;
+  return doCommand(command, callback);
+}
+
+function untetherInterface(params, callback) {
+  let command = "tether interface remove " + params.ifname;
+  return doCommand(command, callback);
+}
+
+function setDnsForwarders(params, callback) {
+  let command = "tether dns set " + params.dns1 + " " + params.dns2;
+  return doCommand(command, callback);
+}
+
+function enableNat(params, callback) {
+  let command = "nat enable " + params.internalIfname + " " +
+                params.externalIfname + " " + "0";
+  return doCommand(command, callback);
+}
+
+function disableNat(params, callback) {
+  let command = "nat disable " + params.internalIfname + " " +
+                params.externalIfname + " " + "0";
+  return doCommand(command, callback);
+}
+
+function wifiFirmwareReload(params, callback) {
+  let command = "softap fwreload " + params.ifname + " " + params.mode;
+  return doCommand(command, callback);
+}
+
+function startAccessPointDriver(params, callback) {
+  let command = "softap start " + params.ifname;
+  return doCommand(command, callback);
+}
+
+function stopAccessPointDriver(params, callback) {
+  let command = "softap stop " + params.ifname;
+  return doCommand(command, callback);
+}
+
+function startSoftAP(params, callback) {
+  let command = "softap startap";
+  return doCommand(command, callback);
+}
+
+function stopSoftAP(params, callback) {
+  let command = "softap stopap";
+  return doCommand(command, callback);
+}
+
+// The command format is "softap set wlan0 wl0.1 hotspot456 open null 6 0 8".
+function setAccessPoint(params, callback) {
+  let command = "softap set " + params.ifname +
+                " " + params.wifictrlinterfacename +
+                " " + params.ssid +
+                " " + params.security +
+                " " + params.key +
+                " " + "6 0 8";
+  return doCommand(command, callback);
+}
+
+/**
+ * Modify usb function's property to turn on USB RNDIS function
+ */
+function setUSBFunction(params) {
+  let report = params.report;
+  let retry = 0;
+  let i = 0;
+
+  libcutils.property_set(USB_FUNCTION_PATH, params.usbfunc);
+  // Trigger the timer to check usb state and report the result to NetworkManager.
+  if (report) {
+    setTimeout(checkUSBFunction, USB_FUNCTION_RETRY_INTERVAL, params);
+  }
+
+  function checkUSBFunction(params) {
+    let result = libcutils.property_get(USB_FUNCTION_STATE);
+    if (result == params.usbfunc) {
+      params.result = true;
+      postMessage(params);
+      retry = 0;
+      return;
+    }
+    if (retry < USB_FUNCTION_RETRY_TIMES) {
+      retry++;
+      setTimeout(checkUSBFunction, USB_FUNCTION_RETRY_INTERVAL, params);
+      return;
+    }
+    params.result = false;
+    postMessage(params);
+  };
+
+  return true;
+}
+
+function dumpParams(params, type) {
+  if (!DEBUG) {
+    return;
+  }
+
+  debug("Dump params:");
+  debug("     ifname: " + params.ifname);
+  debug("     ip: " + params.ip);
+  debug("     link: " + params.link);
+  debug("     prefix: " + params.prefix);
+  debug("     startIp: " + params.startIp);
+  debug("     endIp: " + params.endIp);
+  debug("     dnsserver1: " + params.dns1);
+  debug("     dnsserver2: " + params.dns2);
+  debug("     internalIfname: " + params.internalIfname);
+  debug("     externalIfname: " + params.externalIfname);
+  if (type == "WIFI") {
+    debug("     wifictrlinterfacename: " + params.wifictrlinterfacename);
+    debug("     ssid: " + params.ssid);
+    debug("     security: " + params.security);
+    debug("     key: " + params.key);
+  }
+}
+
+function chain(params, funcs, onError) {
+  let i = -1;
+  let f = funcs[i];
+  function next(error, result) {
+    params.resultCode = result.code;
+    params.resultReason = result.reason;
+    if (error) {
+      if (onError) {
+        params.error = error;
+        params.state = f.name;
+        onError(params);
+      }
+      return;
+    }
+    i += 1;
+    f = funcs[i];
+    if (f) {
+      let ret = f(params, next);
+      if (!ret && onError) {
+        params.error = true;
+        params.state = f.name;
+        onError(params);
+      }
+    }
+  };
+  next(null, {code: NETD_COMMAND_ERROR, reason: ""});
+}
+
+let gWifiEnableChain = [wifiFirmwareReload,
+                        startAccessPointDriver,
+                        setAccessPoint,
+                        startSoftAP,
+                        setInterfaceUp,
+                        tetherInterface,
+                        setIpForwardingEnabled,
+                        startTethering,
+                        setDnsForwarders,
+                        enableNat,
+                        wifiTetheringSuccess];
+
+let gWifiDisableChain = [stopSoftAP,
+                         stopAccessPointDriver,
+                         wifiFirmwareReload,
+                         disableNat,
+                         untetherInterface,
+                         setIpForwardingEnabled,
+                         stopTethering,
+                         wifiTetheringSuccess];
+
+/**
+ * handling main thread's enable/disable WiFi Tethering request
+ */
+function setWifiTethering(params) {
+  let enable = params.enable;
+  let interfaceProperties = getIFProperties(params.externalIfname);
+
+  params.dns1 = interfaceProperties.dns1_str;
+  params.dns2 = interfaceProperties.dns2_str;
+  dumpParams(params, "WIFI");
+
+  if (enable) {
+    // Enable Wifi tethering.
+    debug("Starting Wifi Tethering on " +
+           params.internalIfname + "<->" + params.externalIfname);
+    chain(params, gWifiEnableChain, wifiTetheringFail);
+  } else {
+    // Disable Wifi tethering.
+    debug("Stopping Wifi Tethering on " +
+           params.internalIfname + "<->" + params.externalIfname);
+    chain(params, gWifiDisableChain, wifiTetheringFail);
+  }
+  return true;
+}
+
+let gUSBEnableChain = [setInterfaceUp,
+                       enableNat,
+                       setIpForwardingEnabled,
+                       tetherInterface,
+                       startTethering,
+                       setDnsForwarders,
+                       usbTetheringSuccess];
+
+let gUSBDisableChain = [disableNat,
+                        setIpForwardingEnabled,
+                        stopTethering,
+                        untetherInterface,
+                        usbTetheringSuccess];
+
+/**
+ * handling main thread's enable/disable USB Tethering request
+ */
+function setUSBTethering(params) {
+  let enable = params.enable;
+  let interfaceProperties = getIFProperties(params.externalIfname);
+
+  params.dns1 = interfaceProperties.dns1_str;
+  params.dns2 = interfaceProperties.dns2_str;
+  dumpParams(params, "USB");
+
+  if (enable) {
+    // Enable USB tethering.
+    debug("Starting USB Tethering on " +
+           params.internalIfname + "<->" + params.externalIfname);
+    chain(params, gUSBEnableChain, usbTetheringFail);
+  } else {
+    // Disable USB tetehring.
+    debug("Stopping USB Tethering on " +
+           params.internalIfname + "<->" + params.externalIfname);
+    chain(params, gUSBDisableChain, usbTetheringFail);
+  }
+  return true;
+}
+
+let debug;
+if (DEBUG) {
+  debug = function (s) {
+    dump("Network Worker: " + s + "\n");
+  };
+} else {
+  debug = function (s) {};
+}
+
