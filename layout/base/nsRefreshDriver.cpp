db77ca702cd5d618a7f6c9842d957a2bcb84aa80
Matt Woodrow <mwoodrow@mozilla.com>
2012-08-29 17:48:44 +1200

    Bug 539356 - Part 26 - Send invalidations for hidden documents. r=roc

--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -100,6 +100,11 @@ nsRefreshDriver::~nsRefreshDriver()
   NS_ABORT_IF_FALSE(ObserverCount() == 0,
                     "observers should have unregistered");
   NS_ABORT_IF_FALSE(!mTimer, "timer should be gone");
+  
+  for (uint32_t i = 0; i < mPresShellsToInvalidateIfHidden.Length(); i++) {
+    mPresShellsToInvalidateIfHidden[i]->InvalidatePresShellIfHidden();
+  }
+  mPresShellsToInvalidateIfHidden.Clear();
 }
 
 // Method for testing.  See nsIDOMWindowUtils.advanceTimeAndRefresh
@@ -412,6 +417,11 @@ nsRefreshDriver::Notify(nsITimer *aTimer)
     mRequests.EnumerateEntries(nsRefreshDriver::ImageRequestEnumerator, &parms);
     EnsureTimerStarted(false);
   }
+    
+  for (uint32_t i = 0; i < mPresShellsToInvalidateIfHidden.Length(); i++) {
+    mPresShellsToInvalidateIfHidden[i]->InvalidatePresShellIfHidden();
+  }
+  mPresShellsToInvalidateIfHidden.Clear();
 
   if (mViewManagerFlushIsPending) {
 #ifdef DEBUG_INVALIDATIONS
