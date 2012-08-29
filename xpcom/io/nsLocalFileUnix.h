339d6377647ba077c4a07106151d1044d636658b
Jan Beich <jbeich@tormail.org>
2012-08-29 17:00:09 -0400

    Bug 784124 - FreeBSD supports statvfs(2) since 5.0. r=bsmedberg

--- xpcom/io/nsLocalFileUnix.h
+++ xpcom/io/nsLocalFileUnix.h
@@ -52,14 +52,6 @@
     #endif
 #endif
 
-// so we can statfs on freebsd
-#if defined(__FreeBSD__)
-    #define HAVE_SYS_STATFS_H
-    #define STATFS statfs
-    #include <sys/param.h>
-    #include <sys/mount.h>
-#endif
-
 #if defined(HAVE_STAT64) && defined(HAVE_LSTAT64)
     #if defined (AIX)
         #if defined STAT
