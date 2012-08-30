eec49033244a849bad7918bba4521c5564a21f37
Nicholas Cameron <ncameron@mozilla.com>
2012-08-30 13:48:08 +1200

    Bug 773460; backout the Linux pref. r=roc

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -231,19 +231,22 @@ pref("gfx.font_rendering.directwrite.enabled", false);
 pref("gfx.font_rendering.directwrite.use_gdi_table_loading", true);
 #endif
 
-pref("gfx.canvas.azure.enabled", true);
 #ifdef XP_WIN
+pref("gfx.canvas.azure.enabled", true);
 // comma separated list of backends to use in order of preference
 // e.g., pref("gfx.canvas.azure.backends", "direct2d,skia,cairo");
 pref("gfx.canvas.azure.backends", "direct2d,cairo");
 pref("gfx.content.azure.enabled", true);
 #else
 #ifdef XP_MACOSX
+pref("gfx.canvas.azure.enabled", true);
 pref("gfx.canvas.azure.backends", "cg");
 #else
 #ifdef ANDROID
+pref("gfx.canvas.azure.enabled", true);
 pref("gfx.canvas.azure.backends", "cairo");
 #else
+pref("gfx.canvas.azure.enabled", false);
 pref("gfx.canvas.azure.backends", "cairo");
 #endif
 #endif
