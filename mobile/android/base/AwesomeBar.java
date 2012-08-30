c65a0c5c56f7d59326cfd145346ae96d7c374fe6
Chris Peterson <cpeterson@mozilla.com>
2012-08-29 17:19:32 -0700

    Bug 782096 - Catch unexpected NullPointerException from InputMethodManagerService. r=snorp

--- mobile/android/base/AwesomeBar.java
+++ mobile/android/base/AwesomeBar.java
@@ -6,7 +6,6 @@
 package org.mozilla.gecko;
 
 import org.mozilla.gecko.db.BrowserDB;
-import org.mozilla.gecko.db.BrowserContract.Bookmarks;
 import org.mozilla.gecko.util.GeckoAsyncTask;
 
 import android.app.Activity;
@@ -203,9 +202,16 @@ public class AwesomeBar extends GeckoActivity {
 
         mText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (v == null || hasFocus) {
+                    return;
+                }
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                try {
                     imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                } catch (NullPointerException e) {
+                    Log.e(LOGTAG, "InputMethodManagerService, why are you throwing"
+                                  + " a NullPointerException? See bug 782096", e);
                 }
             }
         });
