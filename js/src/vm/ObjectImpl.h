9252220319cf30f966e9af3a7dced4841d39795f
Steve Fink <sfink@mozilla.com>
2012-08-31 09:05:50 -0700

    Backed out changeset 804d74e217e3 for that burning sensation

--- js/src/vm/ObjectImpl.h
+++ js/src/vm/ObjectImpl.h
@@ -359,10 +359,6 @@ class ElementsHeader
             friend class SparseElementsHeader;
             Shape * shape;
         } sparse;
-        class {
-            friend class ArrayBufferElementsHeader;
-            JSObject * views;
-        } buffer;
     };
 
     void staticAsserts() {
@@ -754,8 +750,6 @@ class ArrayBufferElementsHeader : public ElementsHeader
 
     bool setElement(JSContext *cx, Handle<ObjectImpl*> obj, Handle<ObjectImpl*> receiver,
                     uint32_t index, const Value &v, unsigned resolveFlags, bool *succeeded);
-
-    JSObject **viewList() { return &buffer.views; }
 
   private:
     inline bool isArrayBufferElements() const MOZ_DELETE;
