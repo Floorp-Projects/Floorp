6a27dff75c005d803a4f6ca94db4f1006e0a2a7d
Chris Pearce <cpearce@mozilla.com>
2012-09-03 10:56:29 +1200

    Bug 786924 - Estimate Ogg buffered ranges on B2G to avoid main thread I/O. r=kinetik

--- content/media/VideoUtils.cpp
+++ content/media/VideoUtils.cpp
@@ -3,6 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "VideoUtils.h"
+#include "MediaResource.h"
+#include "nsTimeRanges.h"
 #include "nsMathUtils.h"
 #include "prtypes.h"
 
@@ -38,3 +40,52 @@ void ScaleDisplayByAspectRatio(nsIntSize& aDisplay, float aAspectRatio)
     aDisplay.height = ConditionDimension(aDisplay.height / aAspectRatio);
   }
 }
+
+static int64_t BytesToTime(int64_t offset, int64_t length, int64_t durationUs) {
+  NS_ASSERTION(length > 0, "Must have positive length");
+  double r = double(offset) / double(length);
+  if (r > 1.0)
+    r = 1.0;
+  return int64_t(double(durationUs) * r);
+}
+
+void GetEstimatedBufferedTimeRanges(mozilla::MediaResource* aStream,
+                                    int64_t aDurationUsecs,
+                                    nsTimeRanges* aOutBuffered)
+{
+  // Nothing to cache if the media takes 0us to play.
+  if (aDurationUsecs <= 0 || !aStream || !aOutBuffered)
+    return;
+
+  // Special case completely cached files.  This also handles local files.
+  if (aStream->IsDataCachedToEndOfResource(0)) {
+    aOutBuffered->Add(0, double(aDurationUsecs) / USECS_PER_S);
+    return;
+  }
+
+  int64_t totalBytes = aStream->GetLength();
+
+  // If we can't determine the total size, pretend that we have nothing
+  // buffered. This will put us in a state of eternally-low-on-undecoded-data
+  // which is not great, but about the best we can do.
+  if (totalBytes <= 0)
+    return;
+
+  int64_t startOffset = aStream->GetNextCachedData(0);
+  while (startOffset >= 0) {
+    int64_t endOffset = aStream->GetCachedDataEnd(startOffset);
+    // Bytes [startOffset..endOffset] are cached.
+    NS_ASSERTION(startOffset >= 0, "Integer underflow in GetBuffered");
+    NS_ASSERTION(endOffset >= 0, "Integer underflow in GetBuffered");
+
+    int64_t startUs = BytesToTime(startOffset, totalBytes, aDurationUsecs);
+    int64_t endUs = BytesToTime(endOffset, totalBytes, aDurationUsecs);
+    if (startUs != endUs) {
+      aOutBuffered->Add(double(startUs) / USECS_PER_S,
+                        double(endUs) / USECS_PER_S);
+    }
+    startOffset = aStream->GetNextCachedData(endOffset);
+  }
+  return;
+}
+
