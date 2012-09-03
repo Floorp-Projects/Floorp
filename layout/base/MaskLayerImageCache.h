c1030965859b4b24a79cab751fe51679a3edbbfa
Ed Morley <emorley@mozilla.com>
2012-09-03 09:20:05 +0100

    Backout 03cb77f8d75d (bug 769021), 04fd792509d6 (bug 786817) for burning the tree

--- layout/base/MaskLayerImageCache.h
+++ layout/base/MaskLayerImageCache.h
@@ -50,23 +50,9 @@ public:
               aPresContext->AppUnitsToGfxUnits(aRRect.mRect.width),
               aPresContext->AppUnitsToGfxUnits(aRRect.mRect.height))
     {
-      MOZ_COUNT_CTOR(PixelRoundedRect);
       NS_FOR_CSS_HALF_CORNERS(corner) {
         mRadii[corner] = aPresContext->AppUnitsToGfxUnits(aRRect.mRadii[corner]);
       }
-    }
-    PixelRoundedRect(const PixelRoundedRect& aPRR)
-      : mRect(aPRR.mRect)
-    {
-      MOZ_COUNT_CTOR(PixelRoundedRect);
-      NS_FOR_CSS_HALF_CORNERS(corner) {
-        mRadii[corner] = aPRR.mRadii[corner];
-      }
-    }
-
-    ~PixelRoundedRect()
-    {
-      MOZ_COUNT_DTOR(PixelRoundedRect);
     }
 
     // Applies the scale and translate components of aTransform.
@@ -113,9 +99,6 @@ public:
     gfxRect mRect;
     // Indices into mRadii are the NS_CORNER_* constants in nsStyleConsts.h
     gfxFloat mRadii[8];
-
-  private:
-    PixelRoundedRect() MOZ_DELETE;
   };
 
   /**
@@ -128,27 +111,14 @@ public:
    * pointers to a key object (the +1 being from the hashtable entry), but this
    * invariant may be temporarily broken.
    */
-  struct MaskLayerImageKey
+  class MaskLayerImageKey
   {
-    MaskLayerImageKey(layers::LayersBackend aBackend)
+  public:
+    MaskLayerImageKey(const nsTArray<PixelRoundedRect>& aRoundedClipRects, layers::LayersBackend aBackend)
       : mBackend(aBackend)
       , mLayerCount(0)
-      , mRoundedClipRects()
-    {
-      MOZ_COUNT_CTOR(MaskLayerImageKey);
-    }
-    MaskLayerImageKey(const MaskLayerImageKey& aKey)
-      : mBackend(aKey.mBackend)
-      , mLayerCount(aKey.mLayerCount)
-      , mRoundedClipRects(aKey.mRoundedClipRects)
-    {
-      MOZ_COUNT_CTOR(MaskLayerImageKey);
-    }
-
-    ~MaskLayerImageKey()
-    {
-      MOZ_COUNT_DTOR(MaskLayerImageKey);
-    }
+      , mRoundedClipRects(aRoundedClipRects)
+    {}
 
     void AddRef() const { ++mLayerCount; }
     void Release() const
@@ -202,14 +172,11 @@ protected:
     typedef const MaskLayerImageKey& KeyType;
     typedef const MaskLayerImageKey* KeyTypePointer;
 
-    MaskLayerImageEntry(KeyTypePointer aKey) : mKey(aKey)
+    MaskLayerImageEntry(KeyTypePointer aKey) : mKey(aKey) {}
+    MaskLayerImageEntry(const MaskLayerImageEntry& aOther)
+      : mKey(aOther.mKey.get())
     {
-      MOZ_COUNT_CTOR(MaskLayerImageEntry);
-    }
-    MaskLayerImageEntry(const MaskLayerImageEntry& aOther) MOZ_DELETE;
-    ~MaskLayerImageEntry()
-    {
-      MOZ_COUNT_DTOR(MaskLayerImageEntry);
+      NS_ERROR("ALLOW_MEMMOVE == true, should never be called");
     }
 
     // KeyEquals(): does this entry match this key?
