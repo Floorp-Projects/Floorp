69f43d56e68c88489fae467339f8d58719f96309
Kartikaya Gupta <kgupta@mozilla.com>
2012-09-04 18:06:22 -0400

    Bug 787060 - Update android widget RedrawAll to invalidate the entire view to work with DLBI. r=mattwoodrow

--- widget/android/nsWindow.cpp
+++ widget/android/nsWindow.cpp
@@ -23,6 +23,7 @@ using mozilla::unused;
 #include "nsIObserverService.h"
 #include "nsFocusManager.h"
 #include "nsIWidgetListener.h"
+#include "nsIViewManager.h"
 
 #include "nsRenderingContext.h"
 #include "nsIDOMSimpleGestureEvent.h"
@@ -275,9 +276,12 @@ nsWindow::ConfigureChildren(const nsTArray<nsIWidget::Configuration>& config)
 void
 nsWindow::RedrawAll()
 {
-    nsIntRect entireRect(0, 0, gAndroidBounds.width, gAndroidBounds.height);
-    AndroidGeckoEvent *event = new AndroidGeckoEvent(AndroidGeckoEvent::DRAW, entireRect);
-    nsAppShell::gAppShell->PostEvent(event);
+    if (mFocus && mFocus->mWidgetListener) {
+        nsIView* view = mFocus->mWidgetListener->GetView();
+        if (view && view->GetViewManager()) {
+            view->GetViewManager()->InvalidateView(view);
+        }
+    }
 }
 
 NS_IMETHODIMP
