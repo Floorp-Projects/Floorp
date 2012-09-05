17c0b94b27171cc9d404e17384b135612baf604c
Chris Jones <jones.chris.g@gmail.com>
2012-09-04 17:36:16 -0700

    Bug 785237: Give prelaunch processes background priority, and move the background oom_adj up (down?) a class. r=jlebar

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -507,7 +507,7 @@ pref("dom.ipc.processPriorityManager.enabled", true);
 pref("dom.ipc.processPriorityManager.gracePeriodMS", 1000);
 pref("hal.processPriorityManager.gonk.masterOomAdjust", 0);
 pref("hal.processPriorityManager.gonk.foregroundOomAdjust", 1);
-pref("hal.processPriorityManager.gonk.backgroundOomAdjust", 2);
+pref("hal.processPriorityManager.gonk.backgroundOomAdjust", 6);
 pref("hal.processPriorityManager.gonk.masterNice", -1);
 pref("hal.processPriorityManager.gonk.foregroundNice", 0);
 pref("hal.processPriorityManager.gonk.backgroundNice", 10);
