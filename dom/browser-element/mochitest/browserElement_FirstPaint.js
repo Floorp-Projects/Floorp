847762d4c4388b59c5130f1ef1f8c7ebba02bed2
Justin Lebar <justin.lebar@gmail.com>
2012-09-05 10:34:06 -0400

    Bug 787378 - Part 2: Tests for mozbrowserfirstpaint event.

---
+++ dom/browser-element/mochitest/browserElement_FirstPaint.js
@@ -0,0 +1,44 @@
+/* Any copyright is dedicated to the public domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Bug 787378 - Add mozbrowserfirstpaint event.
+"use strict";
+
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addPermission();
+
+  var iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+
+  var gotFirstPaint = false;
+  var gotFirstLocationChange = false;
+  iframe.addEventListener('mozbrowserfirstpaint', function(e) {
+    ok(!gotFirstPaint, "Got only one first paint.");
+    gotFirstPaint = true;
+
+    if (gotFirstLocationChange) {
+      iframe.src = browserElementTestHelpers.emptyPage1 + '?2';
+    }
+  });
+
+  iframe.addEventListener('mozbrowserlocationchange', function(e) {
+    if (e.detail == browserElementTestHelpers.emptyPage1) {
+      gotFirstLocationChange = true;
+      if (gotFirstPaint) {
+        iframe.src = browserElementTestHelpers.emptyPage1 + '?2';
+      }
+    }
+    else if (e.detail.endsWith('?2')) {
+      SimpleTest.finish();
+    }
+  });
+
+  document.body.appendChild(iframe);
+
+  iframe.src = browserElementTestHelpers.emptyPage1;
+}
+
+runTest();
