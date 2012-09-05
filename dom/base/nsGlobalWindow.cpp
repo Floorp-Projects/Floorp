f6e4f704ddb2e32705bba032459369d4c57bc9f5
Bobby Holley <bobbyholley@gmail.com>
2012-09-05 11:32:06 -0700

    Bug 774633 - Remove "is chrome window" condition for inner window reuse. r=jst
    WouldReuseInnerWindow also returns true if the new window is same-origin with
    the old one about:blank document.
    
    This condition exists in order to handle some sloppiness with respect to the
    principals on initial about:blank documents. Chrome callers sometimes parent
    chrome windows (with XUL document) to content windows. But this parenting causes
    us to push the cx of the content window during window creation, meaning that
    the subsequent load of chrome://foo.xul blows away the old inner window and any
    expandos on it. We can handle this case more precisely by skipping the cx push
    for type="chrome" windows.
    
    Furthermore, this was also necessary to prevent the inner window from being
    blown away in the call to SetOpenerScriptPrincipal once nsWindowWatcher gets
    the window back from the window creator (and after it's already told consumers
    about the window via "domwindowcreated"). But we fixed this nastiness in the
    previous patches.
    
    So we can remove this case. By doing so, we can prevent inner windows from ever
    changing origins, which is very important for compartment security invariants.

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -1462,8 +1462,6 @@ nsGlobalWindow::WouldReuseInnerWindow(nsIDocument *aNewDocument)
   // We reuse the inner window when:
   // a. We are currently at our original document.
   // b. At least one of the following conditions are true:
-  // -- We are not currently a content window (i.e., we're currently a chrome
-  //    window).
   // -- The new document is the same as the old document. This means that we're
   //    getting called from document.open().
   // -- The new document has the same origin as what we have loaded right now.
@@ -1478,9 +1476,10 @@ nsGlobalWindow::WouldReuseInnerWindow(nsIDocument *aNewDocument)
   
   NS_ASSERTION(NS_IsAboutBlank(mDoc->GetDocumentURI()),
                "How'd this happen?");
-  
+
   // Great, we're the original document, check for one of the other
   // conditions.
+
   if (mDoc == aNewDocument) {
     return true;
   }
@@ -1493,17 +1492,6 @@ nsGlobalWindow::WouldReuseInnerWindow(nsIDocument *aNewDocument)
     return true;
   }
 
-  nsCOMPtr<nsIDocShellTreeItem> treeItem(do_QueryInterface(mDocShell));
-
-  if (treeItem) {
-    int32_t itemType = nsIDocShellTreeItem::typeContent;
-    treeItem->GetItemType(&itemType);
-
-    // If we're a chrome window, then we want to reuse the inner window.
-    return itemType == nsIDocShellTreeItem::typeChrome;
-  }
-
-  // No treeItem: don't reuse the current inner window.
   return false;
 }
 
