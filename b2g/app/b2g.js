bae4cd67b6a6e4c47d839f194818bd2f870ac7e5
Patrick McManus <mcmanus@ducksong.com>
2012-09-05 08:27:31 -0400

    bug 648603 - update connection limits; especially for proxies r=jduell

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -44,9 +44,9 @@ pref("network.http.pipelining.ssl", true);
 pref("network.http.proxy.pipelining", true);
 pref("network.http.pipelining.maxrequests" , 6);
 pref("network.http.keep-alive.timeout", 600);
-pref("network.http.max-connections", 6);
-pref("network.http.max-persistent-connections-per-server", 4);
-pref("network.http.max-persistent-connections-per-proxy", 4);
+pref("network.http.max-connections", 20);
+pref("network.http.max-persistent-connections-per-server", 6);
+pref("network.http.max-persistent-connections-per-proxy", 20);
 
 // See bug 545869 for details on why these are set the way they are
 pref("network.buffer.cache.count", 24);
