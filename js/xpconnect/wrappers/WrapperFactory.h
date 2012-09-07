cd21ce7e8c209b1750181c1104b5dc6d9ae83518
Bobby Holley <bobbyholley@gmail.com>
2012-09-07 11:28:56 -0700

    Bug 788914 - Kill the XOW flag. r=mrbkap
    There are really two questions to be asked: is the caller chrome, and does the
    caller subsume the callee. We have other, more precise ways of asking both of
    these questions.

--- js/xpconnect/wrappers/WrapperFactory.h
+++ js/xpconnect/wrappers/WrapperFactory.h
@@ -18,8 +18,7 @@ class WrapperFactory {
     enum { WAIVE_XRAY_WRAPPER_FLAG = js::Wrapper::LAST_USED_FLAG << 1,
            IS_XRAY_WRAPPER_FLAG    = WAIVE_XRAY_WRAPPER_FLAG << 1,
            SCRIPT_ACCESS_ONLY_FLAG = IS_XRAY_WRAPPER_FLAG << 1,
-           XOW_FLAG                = SCRIPT_ACCESS_ONLY_FLAG << 1,
-           SOW_FLAG                = XOW_FLAG << 1,
+           SOW_FLAG                = SCRIPT_ACCESS_ONLY_FLAG << 1,
 
            // Prevent scripts from shadowing native properties.
            // NB: Applies only to Xray wrappers.
@@ -36,10 +35,6 @@ class WrapperFactory {
 
     static bool IsXrayWrapper(JSObject *wrapper) {
         return HasWrapperFlag(wrapper, IS_XRAY_WRAPPER_FLAG);
-    }
-
-    static bool IsXOW(JSObject *wrapper) {
-        return HasWrapperFlag(wrapper, XOW_FLAG);
     }
 
     static bool HasWaiveXrayFlag(JSObject *wrapper) {
