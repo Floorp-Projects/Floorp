feca58af5ef95d8a66aa1cf40c98c5c475a633b4
William Lachance <wlachance@mozilla.com>
2012-09-07 16:00:31 -0400

    Bug 723107 - Re-add mozdevice to testing/mozbase, remove duplicate files in build/mobile;r=jmaher

---
+++ testing/mozbase/mozdevice/sut_tests/runtests.py
@@ -0,0 +1,85 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from optparse import OptionParser
+import os
+import re
+import unittest
+import sys
+
+import dmunit
+import genfiles
+
+
+def main(ip, port, scripts, directory, isTestDevice):
+    dmunit.ip = ip
+    dmunit.port = port
+
+    suite = unittest.TestSuite()
+
+    genfiles.gen_test_files()
+
+    if scripts:
+        # Ensure the user didn't include the .py on the name of the test file
+        # (and get rid of it if they did)
+        scripts = map(lambda x: x.split('.')[0], scripts)
+    else:
+        # Go through the directory and pick up everything
+        # named test_*.py and run it
+        testfile = re.compile('^test_.*\.py$')
+        files = os.listdir(directory)
+
+        for f in files:
+            if testfile.match(f):
+                scripts.append(f.split('.')[0])
+
+    testLoader = dmunit.DeviceManagerTestLoader(isTestDevice)
+    for s in scripts:
+        suite.addTest(testLoader.loadTestsFromModuleName(s))
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+    genfiles.clean_test_files()
+
+
+if  __name__ == "__main__":
+
+    default_ip = '127.0.0.1'
+    default_port = 20701
+
+    env_ip, _, env_port = os.getenv('TEST_DEVICE', '').partition(':')
+    if env_port:
+        try:
+            env_port = int(env_port)
+        except ValueError:
+            print >> sys.stderr, "Port in TEST_DEVICE should be an integer."
+            sys.exit(1)
+
+    # Deal with the options
+    parser = OptionParser()
+    parser.add_option("--ip", action="store", type="string", dest="ip",
+                      help="IP address for device running SUTAgent, defaults "
+                      "to what's provided in $TEST_DEVICE or 127.0.0.1",
+                      default=(env_ip or default_ip))
+
+    parser.add_option("--port", action="store", type="int", dest="port",
+                      help="Port of SUTAgent on device, defaults to "
+                      "what's provided in $TEST_DEVICE or 20701",
+                      default=(env_port or default_port))
+
+    parser.add_option("--script", action="append", type="string",
+                      dest="scripts", help="Name of test script to run, "
+                      "can be specified multiple times", default=[])
+
+    parser.add_option("--directory", action="store", type="string", dest="dir",
+                      help="Directory to look for tests in, defaults to "
+                      "current directory", default=os.getcwd())
+
+    parser.add_option("--testDevice", action="store_true", dest="isTestDevice",
+                      help="Specifies that the device is a local test agent",
+                      default=False)
+
+    (options, args) = parser.parse_args()
+
+    main(options.ip, options.port, options.scripts,
+         options.dir, options.isTestDevice)
---
+++ testing/mozbase/mozdevice/sut_tests/test_getdir.py
@@ -0,0 +1,54 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+import posixpath
+import shutil
+import tempfile
+
+from dmunit import DeviceManagerTestCase
+
+
+class GetDirectoryTestCase(DeviceManagerTestCase):
+
+    def _setUp(self):
+        self.localsrcdir = tempfile.mkdtemp()
+        os.makedirs(os.path.join(self.localsrcdir, 'push1', 'sub.1', 'sub.2'))
+        path = os.path.join(self.localsrcdir,
+                            'push1', 'sub.1', 'sub.2', 'testfile')
+        file(path, 'w').close()
+        os.makedirs(os.path.join(self.localsrcdir, 'push1', 'emptysub'))
+        self.localdestdir = tempfile.mkdtemp()
+        self.expected_filelist = ['emptysub', 'sub.1']
+
+    def tearDown(self):
+        shutil.rmtree(self.localsrcdir)
+        shutil.rmtree(self.localdestdir)
+
+    def runTest(self):
+        """This tests the getDirectory() function.
+        """
+        testroot = posixpath.join(self.dm.getDeviceRoot(), 'infratest')
+        self.dm.removeDir(testroot)
+        self.dm.mkDir(testroot)
+        self.dm.pushDir(
+            os.path.join(self.localsrcdir, 'push1'),
+            posixpath.join(testroot, 'push1'))
+        # pushDir doesn't copy over empty directories, but we want to make sure
+        # that they are retrieved correctly.
+        self.dm.mkDir(posixpath.join(testroot, 'push1', 'emptysub'))
+        filelist = self.dm.getDirectory(
+            posixpath.join(testroot, 'push1'),
+            os.path.join(self.localdestdir, 'push1'))
+        filelist.sort()
+        self.assertEqual(filelist, self.expected_filelist)
+        self.assertTrue(os.path.exists(
+            os.path.join(self.localdestdir,
+                         'push1', 'sub.1', 'sub.2', 'testfile')))
+        self.assertTrue(os.path.exists(
+            os.path.join(self.localdestdir, 'push1', 'emptysub')))
+        filelist = self.dm.getDirectory('/dummy',
+            os.path.join(self.localdestdir, '/none'))
+        self.assertEqual(filelist, None)
+        self.assertFalse(os.path.exists(self.localdestdir + '/none'))
