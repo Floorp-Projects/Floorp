a3c088eb236735bfd1652a0aa068194909b1aa75
Ryan VanderMeulen <ryanvm@gmail.com>
2012-09-07 16:51:41 -0400

    Backout 1566e8499cb8 and fcc61274ec43 (bug 744700) due to mochitest orange.

--- dom/base/nsDOMClassInfoClasses.h
+++ dom/base/nsDOMClassInfoClasses.h
@@ -414,8 +414,6 @@ DOMCI_CLASS(MozMobileConnection)
 
 DOMCI_CLASS(USSDReceivedEvent)
 
-DOMCI_CLASS(DataErrorEvent)
-
 // @font-face in CSS
 DOMCI_CLASS(CSSFontFaceRule)
 DOMCI_CLASS(CSSFontFaceStyleDecl)
