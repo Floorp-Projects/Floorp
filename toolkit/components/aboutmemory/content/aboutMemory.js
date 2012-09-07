7184500fb58b870a85547bfae5cee94d52a41daf
Nicholas Nethercote <nnethercote@mozilla.com>
2012-09-06 23:42:12 -0700

    Backout 52abdd1fbcc8 (bug 780442) due to lack of appropriate review.

--- toolkit/components/aboutmemory/content/aboutMemory.js
+++ toolkit/components/aboutmemory/content/aboutMemory.js
@@ -422,13 +422,11 @@ function updateAboutMemory()
   }
 
   // The "Update" button has an id so it can be clicked in a test.
-  // (Also for mobile styling of it and the other buttons.)
   appendButton(UpDesc, updateAboutMemory, "Update", "updateButton");
-  appendButton(GCDesc, doGlobalGC,        "GC",     "doGCButton");
-  appendButton(CCDesc, doCC,              "CC",     "doCCButton");
+  appendButton(GCDesc, doGlobalGC,        "GC");
+  appendButton(CCDesc, doCC,              "CC");
   appendButton(MPDesc, function() { minimizeMemoryUsage3x(updateAboutMemory); },
-                                          "Minimize memory usage",
-                                                    "minMemUseButton");
+                                          "Minimize memory usage");
 
   let div1 = appendElement(body, "div");
   if (gVerbose) {
