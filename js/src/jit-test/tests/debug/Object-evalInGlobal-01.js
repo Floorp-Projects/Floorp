d11896c852883e8da1cbb2f60f1339b915ce2c82
Jim Blandy <jimb@mozilla.com>
2012-09-07 20:08:14 -0700

    Bug 785174: Implement Debugger.Object.prototype.evalInGlobal, .evalInGlobalWithBindings, .evalInGlobalWithBindingsAndABrandySoakedMaraschinoCherry r=jorendorff

---
+++ js/src/jit-test/tests/debug/Object-evalInGlobal-01.js
@@ -0,0 +1,13 @@
+// Debugger.Object.prototype.evalInGlobal basics
+
+var g = newGlobal('new-compartment');
+var h = newGlobal('new-compartment');
+var dbg = new Debugger;
+var gw = dbg.addDebuggee(g);
+var hw = dbg.addDebuggee(h);
+
+g.y = "Bitte Orca";
+h.y = "Visiter";
+var y = "W H O K I L L";
+assertEq(gw.evalInGlobal('y').return, "Bitte Orca");
+assertEq(hw.evalInGlobal('y').return, "Visiter");
