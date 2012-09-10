d1fc1a442bad9e1a941983e34c5cfdeec6e291a1
Mark Cote <mcote@mozilla.com>
2012-09-10 11:33:11 -0400

    Bug 789496 - DeviceManagerSUT: always consume prompt after receiving agent warning. r=wlach

--- testing/mozbase/mozdevice/tests/sut.py
+++ testing/mozbase/mozdevice/tests/sut.py
@@ -11,15 +11,13 @@ import unittest
 class BasicTest(unittest.TestCase):
 
     def _serve_thread(self):
-        need_connection = True
+        conn, addr = self._sock.accept()
+        conn.send("$>\x00")
         while self.commands:
             (command, response) = self.commands.pop(0)
-            if need_connection:
-                conn, addr = self._sock.accept()
-                need_connection = False
-            conn.send("$>\x00")
             data = conn.recv(1024).strip()
             self.assertEqual(data, command)
+            # send response and prompt separately to test for bug 789496
             conn.send("%s\n" % response)
             conn.send("$>\x00")
 
@@ -30,16 +28,36 @@ class BasicTest(unittest.TestCase):
         return thread
 
     def test_init(self):
+        """Tests DeviceManager initialization."""
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.bind(("127.0.0.1", 0))
         self._sock.listen(1)
 
         thread = self._serve([("testroot", "/mnt/sdcard"),
                               ("cd /mnt/sdcard/tests", ""),
-                              ("cwd", "/mnt/sdcard/tests")])
+                              ("cwd", "/mnt/sdcard/tests"),
+                              ("ver", "SUTAgentAndroid Version XX")])
+        
+        port = self._sock.getsockname()[1]
+        mozdevice.DroidSUT.debug = 4
+        d = mozdevice.DroidSUT("127.0.0.1", port=port)
+        thread.join()
+
+    def test_err(self):
+        """Tests error handling during initialization."""
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.bind(("127.0.0.1", 0))
+        self._sock.listen(1)
+
+        thread = self._serve([("testroot", "/mnt/sdcard"),
+                              ("cd /mnt/sdcard/tests", "##AGENT-WARNING## no such file or directory"),
+                              ("cd /mnt/sdcard/tests", "##AGENT-WARNING## no such file or directory"),
+                              ("mkdr /mnt/sdcard/tests", "/mnt/sdcard/tests successfully created"),
+                              ("ver", "SUTAgentAndroid Version XX")])
 
         port = self._sock.getsockname()[1]
-        d = mozdevice.DroidSUT("127.0.0.1", port=port)
+        mozdevice.DroidSUT.debug = 4
+        dm = mozdevice.DroidSUT("127.0.0.1", port=port)
         thread.join()
 
 if __name__ == '__main__':
