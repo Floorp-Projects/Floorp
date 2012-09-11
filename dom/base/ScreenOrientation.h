37e88bfeb1e16214026fcdf8f8026ea8b910e20b
Mounir Lamouri <mounir.lamouri@gmail.com>
2012-09-11 11:55:08 +0100

    Bug 784549 - lockOrientation() should accept an Array of DOMString's in addition of a DOMString. r=jlebar,khuey

--- dom/base/ScreenOrientation.h
+++ dom/base/ScreenOrientation.h
@@ -13,34 +13,17 @@ namespace dom {
 // Make sure that any change here is also made in
 // * mobile/android/base/GeckoScreenOrientationListener.java
 // * embedding/android/GeckoScreenOrientationListener.java
-enum ScreenOrientation {
-  eScreenOrientation_None               = 0,
-  eScreenOrientation_PortraitPrimary    = 1,  // 00000001
-  eScreenOrientation_PortraitSecondary  = 2,  // 00000010
-  eScreenOrientation_Portrait           = 3,  // 00000011
-  eScreenOrientation_LandscapePrimary   = 4,  // 00000100
-  eScreenOrientation_LandscapeSecondary = 8,  // 00001000
-  eScreenOrientation_Landscape          = 12, // 00001100
-  eScreenOrientation_EndGuard
-};
+typedef uint32_t ScreenOrientation;
+
+static const ScreenOrientation eScreenOrientation_None               = 0;
+static const ScreenOrientation eScreenOrientation_PortraitPrimary    = 1;  // 00000001
+static const ScreenOrientation eScreenOrientation_PortraitSecondary  = 2;  // 00000010
+static const ScreenOrientation eScreenOrientation_Portrait           = 3;  // 00000011
+static const ScreenOrientation eScreenOrientation_LandscapePrimary   = 4;  // 00000100
+static const ScreenOrientation eScreenOrientation_LandscapeSecondary = 8;  // 00001000
+static const ScreenOrientation eScreenOrientation_Landscape          = 12; // 00001100
 
 } // namespace dom
 } // namespace mozilla
-
-namespace IPC {
-
-/**
- * Screen orientation serializer.
- * Note that technically, 5, 6, 7, 9, 10 and 11 are illegal values but will
- * not make the serializer to fail. We might want to write our own serializer.
- */
-template <>
-struct ParamTraits<mozilla::dom::ScreenOrientation>
-  : public EnumSerializer<mozilla::dom::ScreenOrientation,
-                          mozilla::dom::eScreenOrientation_None,
-                          mozilla::dom::eScreenOrientation_EndGuard>
-{};
-
-} // namespace IPC
 
 #endif // mozilla_dom_ScreenOrientation_h
