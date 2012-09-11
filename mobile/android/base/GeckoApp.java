f2c9321b2ad25b49bf590457337a50c8f85a2fdf
Lucas Rocha <lucasr@mozilla.com>
2012-09-11 18:51:44 +0100

    Bug 778489 - Don't show progress when entering about:reader (r=mfinkle)

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -1086,7 +1086,7 @@ abstract public class GeckoApp
         if (tab == null)
             return;
 
-        tab.setState("about:home".equals(uri) ? Tab.STATE_SUCCESS : Tab.STATE_LOADING);
+        tab.setState(shouldShowProgress(uri) ? Tab.STATE_SUCCESS : Tab.STATE_LOADING);
         tab.updateIdentityData(null);
         tab.setReaderEnabled(false);
         if (Tabs.getInstance().isSelectedTab(tab))
@@ -2670,6 +2670,10 @@ abstract public class GeckoApp
             }
         }
         return false;
+    }
+
+    public static boolean shouldShowProgress(String url) {
+        return "about:home".equals(url) || ReaderModeUtils.isAboutReader(url);
     }
 
     public static void assertOnUiThread() {
