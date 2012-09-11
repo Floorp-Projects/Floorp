55eb7f36f5034622b81c30cb7452e33aa83790c6
Lucas Rocha <lucasr@mozilla.com>
2012-09-11 18:51:44 +0100

    Bug 778489 - Keep title and favicon when entering reader mode (r=mfinkle)

--- mobile/android/base/ReaderModeUtils.java
+++ mobile/android/base/ReaderModeUtils.java
@@ -17,6 +17,51 @@ public class ReaderModeUtils {
         return url.startsWith("about:reader");
     }
 
+    public static String getUrlFromAboutReader(String aboutReaderUrl) {
+        if (aboutReaderUrl == null)
+            return null;
+
+        String[] urlParts = aboutReaderUrl.split("\\?");
+        if (urlParts.length < 2)
+            return null;
+
+        String query = urlParts[1];
+        for (String param : query.split("&")) {
+            String pair[] = param.split("=");
+            String key = Uri.decode(pair[0]);
+
+            // Key is empty or not "url", discard
+            if (TextUtils.isEmpty(key) || !key.equals("url"))
+                continue;
+
+            // No value associated with key, discard
+            if (pair.length < 2)
+                continue;
+
+            String url = Uri.decode(pair[1]);
+            if (TextUtils.isEmpty(url))
+                return null;
+
+            return url;
+        }
+
+        return null;
+    }
+
+    public static boolean isEnteringReaderMode(String currentUrl, String newUrl) {
+        if (currentUrl == null || newUrl == null)
+            return false;
+
+        if (!isAboutReader(newUrl))
+            return false;
+
+        String urlFromAboutReader = getUrlFromAboutReader(newUrl);
+        if (urlFromAboutReader == null)
+            return false;
+
+        return urlFromAboutReader.equals(currentUrl);
+    }
+
     public static String getAboutReaderForUrl(String url, boolean inReadingList) {
         return getAboutReaderForUrl(url, -1, inReadingList);
     }
