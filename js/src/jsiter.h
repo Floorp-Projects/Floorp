bb664aca508f3061f3343d934c3a94f86914d58b
Jason Orendorff <jorendorff@mozilla.com>
2012-09-12 11:00:08 -0500

    Bug 778557 - "Assertion failure: !args.rval().isPrimitive() && callee != &args.rval().toObject()". r=jimb.
    --HG--
    extra : rebase_source : fa51b866178d26c0a2e17f0de13d4398b6ce8897

--- js/src/jsiter.h
+++ js/src/jsiter.h
@@ -153,6 +153,9 @@ UnwindIteratorForException(JSContext *cx, JSObject *obj);
 void
 UnwindIteratorForUncatchableException(JSContext *cx, JSObject *obj);
 
+JSBool
+IteratorConstructor(JSContext *cx, unsigned argc, Value *vp);
+
 }
 
 extern bool
