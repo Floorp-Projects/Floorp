a4c91157e3f4cbde68739ec25efae91d9304a7d2
Marco Bonardo <mbonardo@mozilla.com>
2012-09-12 12:02:03 +0200

    Bug 749477 - Stop abusing about:home localStorage from browser code. r=fryn

--- browser/base/content/abouthome/aboutHome.js
+++ browser/base/content/abouthome/aboutHome.js
@@ -112,28 +112,38 @@ const DEFAULT_SNIPPETS_URLS = [
 
 const SNIPPETS_UPDATE_INTERVAL_MS = 86400000; // 1 Day.
 
-let gSearchEngine;
-
-document.addEventListener("DOMContentLoaded", function init() {
-  setupSearchEngine();
-  loadSnippets();
+let gObserver = new MutationObserver(function (mutations) {
+  for (let mutation of mutations) {
+    if (mutation.attributeName == "searchEngineURL") {
+      gObserver.disconnect();
+      setupSearchEngine();
+      loadSnippets();
+      return;
+    }
+  }
 });
-window.addEventListener("load", fitToWidth);
+
+window.addEventListener("load", function () {
+  // Delay search engine setup, cause browser.js::BrowserOnAboutPageLoad runs
+  // later and may use asynchronous getters.
+  window.gObserver.observe(document.documentElement, { attributes: true });
+  fitToWidth();
+});
 window.addEventListener("resize", fitToWidth);
 
 
 function onSearchSubmit(aEvent)
 {
   let searchTerms = document.getElementById("searchText").value;
-  if (gSearchEngine && searchTerms.length > 0) {
+  let searchURL = document.documentElement.getAttribute("searchEngineURL");
+  if (searchURL && searchTerms.length > 0) {
     const SEARCH_TOKENS = {
       "_searchTerms_": encodeURIComponent(searchTerms)
     }
-    let url = gSearchEngine.searchUrl;
     for (let key in SEARCH_TOKENS) {
-      url = url.replace(key, SEARCH_TOKENS[key]);
+      searchURL = searchURL.replace(key, SEARCH_TOKENS[key]);
     }
-    window.location.href = url;
+    window.location.href = searchURL;
   }
 
   aEvent.preventDefault();
@@ -142,27 +152,24 @@ function onSearchSubmit(aEvent)
 
 function setupSearchEngine()
 {
-  gSearchEngine = JSON.parse(localStorage["search-engine"]);
-
-  if (!gSearchEngine)
+  let searchEngineName = document.documentElement.getAttribute("searchEngineName");
+  let searchEngineInfo = SEARCH_ENGINES[searchEngineName];
+  if (!searchEngineInfo) {
     return;
-
-  // Look for extended information, like logo and links.
-  let searchEngineInfo = SEARCH_ENGINES[gSearchEngine.name];
-  if (searchEngineInfo) {
-    for (let prop in searchEngineInfo)
-      gSearchEngine[prop] = searchEngineInfo[prop];
   }
 
   // Enqueue additional params if required by the engine definition.
-  if (gSearchEngine.params)
-    gSearchEngine.searchUrl += "&" + gSearchEngine.params;
+  if (searchEngineInfo.params) {
+    let searchEngineURL = document.documentElement.getAttribute("searchEngineURL");
+    searchEngineURL += "&" + searchEngineInfo.params;
+    document.documentElement.setAttribute("searchEngineURL", searchEngineURL);
+  }
 
   // Add search engine logo.
-  if (gSearchEngine.image) {
+  if (searchEngineInfo.image) {
     let logoElt = document.getElementById("searchEngineLogo");
-    logoElt.src = gSearchEngine.image;
-    logoElt.alt = gSearchEngine.name;
+    logoElt.src = searchEngineInfo.image;
+    logoElt.alt = searchEngineInfo.name;
   }
 
   // The "autofocus" attribute doesn't focus the form element
@@ -180,7 +187,7 @@ function loadSnippets()
 {
   // Check last snippets update.
   let lastUpdate = localStorage["snippets-last-update"];
-  let updateURL = localStorage["snippets-update-url"];
+  let updateURL = document.documentElement.getAttribute("snippetsURL");
   if (updateURL && (!lastUpdate ||
                     Date.now() - lastUpdate > SNIPPETS_UPDATE_INTERVAL_MS)) {
     // Try to update from network.
---
+++ browser/modules/AboutHomeUtils.jsm
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const EXPORTED_SYMBOLS = [ "AboutHomeUtils" ];
+
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("resource://gre/modules/Services.jsm");
+
+// Url to fetch snippets, in the urlFormatter service format.
+const SNIPPETS_URL_PREF = "browser.aboutHomeSnippets.updateUrl";
+
+// Should be bumped up if the snippets content format changes.
+const STARTPAGE_VERSION = 3;
+
+let AboutHomeUtils = new Object();
+
+/**
+ * Returns an object containing the name and searchURL of the original default
+ * search engine.
+ */
+XPCOMUtils.defineLazyGetter(AboutHomeUtils, "defaultSearchEngine", function() {
+  let defaultEngine = Services.search.originalDefaultEngine;
+  let submission = defaultEngine.getSubmission("_searchTerms_");
+  if (submission.postData) {
+    throw new Error("Home page does not support POST search engines.");
+  }
+
+  return Object.freeze({
+    name: defaultEngine.name,
+    searchURL: submission.uri.spec
+  });
+});
+
+/**
+ * Returns the URL to fetch snippets from, in the urlFormatter service format.
+ */
+XPCOMUtils.defineLazyGetter(AboutHomeUtils, "snippetsURL", function() {
+  let updateURL = Services.prefs
+                          .getCharPref(SNIPPETS_URL_PREF)
+                          .replace("%STARTPAGE_VERSION%", STARTPAGE_VERSION);
+  return Services.urlFormatter.formatURL(updateURL);
+});
