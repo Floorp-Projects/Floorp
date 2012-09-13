6b2ae3179b8a1625f7ee90d0b9c2b7cbedb199aa
Nathan Froyd <froydnj@mozilla.com>
2012-09-13 07:33:35 -0400

    Bug 781213 - remove chmod on telemetry saved-pings directory; r=taras

--- toolkit/components/telemetry/TelemetryPing.js
+++ toolkit/components/telemetry/TelemetryPing.js
@@ -868,18 +868,11 @@ TelemetryPing.prototype = {
     let profileDirectory = Services.dirsvc.get("ProfD", Ci.nsILocalFile);
     let directory = profileDirectory.clone();
     directory.append("saved-telemetry-pings");
-    if (directory.exists()) {
-      if (directory.isDirectory()) {
-        // We used to wrongly create the directory with mode 0600.
-        // Fix that.
-        directory.permissions = RWX_OWNER;
-        return directory;
-      } else {
-        directory.remove(true);
-      }
+    try {
+      directory.create(Ci.nsIFile.DIRECTORY_TYPE, RWX_OWNER);
+    } catch (e) {
+      // Already exists, just ignore this.
     }
-
-    directory.create(Ci.nsIFile.DIRECTORY_TYPE, RWX_OWNER);
     return directory;
   },
 
