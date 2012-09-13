4c105fe47a0fa99ba7ef2fc0e5264de311f43ac2
Gregory Szorc <gps@mozilla.com>
2012-09-12 21:29:20 -0700

    Bug 774109 - Bootstrap support for OS X; r=rnewman DONTBUILD
    This implements the OS X backend for the bootstrapper. It ensures that
    the system is in the proper state to build Firefox by installing
    required packages.
    
    This was tested with a fresh install of OS X 10.8. Basic support for OS
    X 10.6 and 10.7 is implemented, but not thorougly tested.

--- python/mozboot/mozboot/base.py
+++ python/mozboot/mozboot/base.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
+
 class BaseBootstrapper(object):
     """Base class for system bootstrappers."""
     def __init__(self):
@@ -10,3 +12,15 @@ class BaseBootstrapper(object):
     def install_system_packages(self):
         raise NotImplemented('%s must implement install_system_packages()' %
             __name__)
+
+    def which(self, name):
+        """Python implementation of which.
+
+        It returns the path of an executable or None if it couldn't be found.
+        """
+        for path in os.environ['PATH'].split(os.pathsep):
+            test = os.path.join(path, name)
+            if os.path.exists(test) and os.access(test, os.X_OK):
+                return test
+
+        return None
