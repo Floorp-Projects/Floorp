127870c935e8c0a4d7fbc56a896f8ac0efd3f6a1
Brian Nicholson <bnicholson@mozilla.com>
2012-09-13 14:17:30 -0700

    Bug 789340 - Only move sessionstore.js after a crash. r=mfinkle
    --HG--
    extra : rebase_source : fcfa8a8ec0a4e75e45f54542e6f25e0aa050d684

--- mobile/android/components/SessionStore.js
+++ mobile/android/components/SessionStore.js
@@ -67,9 +67,12 @@ SessionStore.prototype = {
         this._lastSessionTime = this._sessionFile.lastModifiedTime;
         let delta = Date.now() - this._lastSessionTime;
         let timeout = Services.prefs.getIntPref("browser.sessionstore.resume_from_crash_timeout");
+
         // Disable crash recovery if we have exceeded the timeout
         this._shouldRestore = (delta <= (timeout * 60000));
-        this._sessionFile.clone().moveTo(null, this._sessionFileBackup.leafName);
+        if (!this._shouldRestore) {
+          this._sessionFile.clone().moveTo(null, this._sessionFileBackup.leafName);
+        }
       }
 
       if (!this._sessionCache.exists() || !this._sessionCache.isDirectory())
@@ -889,7 +892,22 @@ SessionStore.prototype = {
       Services.obs.notifyObservers(null, "sessionstore-windows-restored", aMessage || "");
     }
 
+    let sessionFile = this._sessionFile;
+
+    // aForceRestore will be true when we are recovering from Android OOM kills
     if (!aForceRestore) {
+      // If we are not recovering from an OOM kill (i.e., we actually crashed),
+      // move sessionstore.js -> sessionstore.bak. sessionstore.bak is used in
+      // about:home to read the "tabs from last time", so since we've started a
+      // new session after the crash, we need to make sure sessionstore.bak is
+      // current. We do not move sessionstore.js -> sessionstore.bak if we had
+      // an OOM kill since restoring from an OOM kill should look like the same
+      // session as before (so the "tabs from last time" should stay the same).
+      if (sessionFile.exists()) {
+        sessionFile.clone().moveTo(null, this._sessionFileBackup.leafName);
+        sessionFile = this._sessionFileBackup;
+      }
+
       let maxCrashes = Services.prefs.getIntPref("browser.sessionstore.max_resumed_crashes");
       let recentCrashes = Services.prefs.getIntPref("browser.sessionstore.recent_crashes") + 1;
       Services.prefs.setIntPref("browser.sessionstore.recent_crashes", recentCrashes);
@@ -901,14 +919,13 @@ SessionStore.prototype = {
       }
     }
 
-    // The previous session data has already been renamed to the backup file
-    if (!this._sessionFileBackup.exists()) {
+    if (!sessionFile.exists()) {
       notifyObservers("fail");
       return;
     }
 
     try {
-      let channel = NetUtil.newChannel(this._sessionFileBackup);
+      let channel = NetUtil.newChannel(sessionFile);
       channel.contentType = "application/json";
       NetUtil.asyncFetch(channel, function(aStream, aResult) {
         if (!Components.isSuccessCode(aResult)) {
