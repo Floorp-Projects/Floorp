bee81ea88aea0c159ffb57a268ee1ff1bcf5059b
Patrick McManus <mcmanus@ducksong.com>
2012-09-13 15:22:56 -0400

    bug 347307 - make pac myIPAddress() more accurate r=biesi

--- netwerk/base/src/ProxyAutoConfig.h
+++ netwerk/base/src/ProxyAutoConfig.h
@@ -8,6 +8,10 @@
 #define ProxyAutoConfig_h__
 
 #include "nsString.h"
+#include "jsapi.h"
+#include "prio.h"
+#include "nsITimer.h"
+#include "nsAutoPtr.h"
 
 namespace mozilla { namespace net {
 
@@ -21,7 +25,6 @@ class ProxyAutoConfig  {
 public:
   ProxyAutoConfig()
     : mJSRuntime(nullptr)
-    , mRunning(false)
     , mJSNeedsSetup(false)
     , mShutdown(false)
   {
@@ -33,6 +36,9 @@ public:
                 const nsCString &aPACScript);
   void     Shutdown();
   void     GC();
+  bool     MyIPAddress(jsval *vp);
+  bool     ResolveAddress(const nsCString &aHostName,
+                          PRNetAddr *aNetAddr, unsigned int aTimeout);
 
   /**
    * Get the proxy string for the specified URI.  The proxy string is
@@ -73,15 +79,22 @@ public:
                           nsACString &result);
 
 private:
+  const static unsigned int kTimeout = 1000; // ms to allow for myipaddress dns queries
+
   // used to compile the PAC file and setup the execution context
   nsresult SetupJS();
 
+  bool SrcAddress(const PRNetAddr *remoteAddress, nsCString &localAddress);
+  bool MyIPAddressTryHost(const nsCString &hostName, unsigned int timeout,
+                          jsval *vp);
+
   JSRuntimeWrapper *mJSRuntime;
-  bool              mRunning;
   bool              mJSNeedsSetup;
   bool              mShutdown;
   nsCString         mPACScript;
   nsCString         mPACURI;
+  nsCString         mRunningHost;
+  nsCOMPtr<nsITimer> mTimer;
 };
 
 }} // namespace mozilla::net
