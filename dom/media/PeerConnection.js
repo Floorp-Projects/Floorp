fc3e1c00570b5b6284f574d1322b1de860a982cc
Peter Van der Beken <peterv@propagandism.org>
2012-09-13 18:04:31 +0200

    Fix for bug 711628 (Implement PeerConnection.localStreams/remoteStreams). r=bz.

--- dom/media/PeerConnection.js
+++ dom/media/PeerConnection.js
@@ -404,6 +404,13 @@ PeerConnection.prototype = {
     });
   },
 
+  get localStreams() {
+    return this._pc.localStreams;
+  },
+  get remoteStreams() {
+    return this._pc.remoteStreams;
+  },
+
   createDataChannel: function(label, dict) {
     if (dict &&
         dict.maxRetransmitTime != undefined &&
