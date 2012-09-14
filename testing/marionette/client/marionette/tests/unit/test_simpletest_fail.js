63bafb318ddd5bcf895d86a4b391127c486b1b20
Jonathan Griffin <jgriffin@mozilla.com>
2012-09-14 14:34:38 -0700

    Bug 791033 - Allow tests to provide custom pass/fail strings for is/isnot/ok assertions, r=mdas

--- testing/marionette/client/marionette/tests/unit/test_simpletest_fail.js
+++ testing/marionette/client/marionette/tests/unit/test_simpletest_fail.js
@@ -7,10 +7,10 @@ MARIONETTE_TIMEOUT = 1000;
 /* this test will fail */
 
 setTimeout(function() { 
-    is(1, 2); 
+    is(1, 2, "is(1,2) should fail", TEST_UNEXPECTED_FAIL, TEST_PASS); 
     finish();
 }, 100);
-isnot(1, 1);
-ok(1 == 2);
+isnot(1, 1, "isnot(1,1) should fail", TEST_UNEXPECTED_FAIL, TEST_PASS);
+ok(1 == 2, "ok(1==2) should fail", TEST_UNEXPECTED_FAIL, TEST_PASS);
 
 
