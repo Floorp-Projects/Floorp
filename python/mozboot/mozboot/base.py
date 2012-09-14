642cfffdbb445e663002a100ac693a18cd855ff2
Gregory Szorc <gps@mozilla.com>
2012-09-13 17:21:49 -0700

    Bug 790995 - Change how root commands are executed; r=jhammel
    os.system has been replaced with subprocess.check_call. We also check
    the effective UID to determine whether to run via sudo.

--- python/mozboot/mozboot/base.py
+++ python/mozboot/mozboot/base.py
@@ -2,12 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from __future__ import print_function, unicode_literals
+
 import os
+import subprocess
 
 class BaseBootstrapper(object):
     """Base class for system bootstrappers."""
-    def __init__(self):
-        pass
 
     def install_system_packages(self):
         raise NotImplemented('%s must implement install_system_packages()' %
@@ -24,3 +25,29 @@ class BaseBootstrapper(object):
                 return test
 
         return None
+
+    def run_as_root(self, command):
+        if os.geteuid() != 0:
+            command.insert(0, 'sudo')
+
+        print('Executing as root:', subprocess.list2cmdline(command))
+
+        subprocess.check_call(command)
+
+    def yum_install(self, *packages):
+        command = ['yum', 'install']
+        command.extend(packages)
+
+        self.run_as_root(command)
+
+    def yum_groupinstall(self, *packages):
+        command = ['yum', 'groupinstall']
+        command.extend(packages)
+
+        self.run_as_root(command)
+
+    def apt_install(self, *packages):
+        command = ['apt-get', 'install']
+        command.extend(packages)
+
+        self.run_as_root(command)
