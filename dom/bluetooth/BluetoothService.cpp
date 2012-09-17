6c78e09184c6e0905717d5ce1671877bf3ca3539
Kyle Machulis <kyle@nonpolynomial.com>
2012-09-17 15:54:12 -0700

    Bug 791823: Creating system message for pairing requires js request; r=mrbkap

--- dom/bluetooth/BluetoothService.cpp
+++ dom/bluetooth/BluetoothService.cpp
@@ -734,7 +734,8 @@ BluetoothService::Notify(const BluetoothSignal& aData)
   JSContext* cx = nsContentUtils::GetSafeJSContext();
   NS_ASSERTION(!::JS_IsExceptionPending(cx),
                "Shouldn't get here when an exception is pending!");
-
+  
+  JSAutoRequest jsar(cx);  
   JSObject* obj = JS_NewObject(cx, NULL, NULL, NULL);
   if (!obj) {
     NS_WARNING("Failed to new JSObject for system message!");
