bb39ff9510929ff4d0335d3679b10878a0ad134d
Jan Beich <jbeich@tormail.org>
2012-09-17 18:42:36 -0400

    Bug 791366 - Define HAVE_PAGE_FAULT_REPORTERS on every Unix. r=njn

--- xpcom/base/nsMemoryReporterManager.cpp
+++ xpcom/base/nsMemoryReporterManager.cpp
@@ -33,7 +33,7 @@ using namespace mozilla;
 #  include "jemalloc.h"
 #endif  // MOZ_MEMORY
 
-#if defined(XP_LINUX) || defined(XP_MACOSX) || defined(SOLARIS)
+#ifdef XP_UNIX
 
 #include <sys/time.h>
 #include <sys/resource.h>
