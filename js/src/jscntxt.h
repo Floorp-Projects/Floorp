0a7cea387f721c5f3b82abd831e2cb907f5f9e85
Jeff Walden <jwalden@mit.edu>
2012-09-17 15:06:16 -0700

    Backed out changeset cf239b55242a, possible memory leak.  r=graphserver

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -383,25 +383,19 @@ struct RuntimeSizes;
 struct JSAtomState
 {
 #define PROPERTYNAME_FIELD(idpart, id, text) \
-    union { \
-        js::FixedHeapPtr<js::PropertyName> idpart##Atom; \
-        js::FixedHeapPtr<js::PropertyName> id; \
-    };
+    union { js::PropertyName *idpart##Atom; js::PropertyName *id; };
     FOR_EACH_COMMON_PROPERTYNAME(PROPERTYNAME_FIELD)
 #undef PROPERTYNAME_FIELD
 #define PROPERTYNAME_FIELD(name, code, init) \
-    union { \
-        js::FixedHeapPtr<js::PropertyName> name##Atom; \
-        js::FixedHeapPtr<js::PropertyName> name; \
-    };
+    union { js::PropertyName *name##Atom; js::PropertyName *name; };
     JS_FOR_EACH_PROTOTYPE(PROPERTYNAME_FIELD)
 #undef PROPERTYNAME_FIELD
 };
 
-#define ATOM(name) (cx->names().name)
+#define ATOM(name) js::HandlePropertyName::fromMarkedLocation(&cx->names().name)
 
 #define NAME_OFFSET(name)       offsetof(JSAtomState, name##Atom)
-#define OFFSET_TO_NAME(rt,off)  (*(js::FixedHeapPtr<js::PropertyName>*)((char*)&(rt)->atomState + (off)))
+#define OFFSET_TO_NAME(rt,off)  (*(js::PropertyName **)((char*)&(rt)->atomState + (off)))
 
 struct JSRuntime : js::RuntimeFriendFields
 {
@@ -791,7 +785,7 @@ struct JSRuntime : js::RuntimeFriendFields
     js::Value           negativeInfinityValue;
     js::Value           positiveInfinityValue;
 
-    js::PropertyName    *emptyString;
+    JSAtom              *emptyString;
 
     /* List of active contexts sharing this runtime. */
     JSCList             contextList;
@@ -919,7 +913,7 @@ struct JSRuntime : js::RuntimeFriendFields
          */
         JSAtomState atomState;
 
-        js::FixedHeapPtr<js::PropertyName> firstCachedName;
+        js::PropertyName *firstCachedName;
     };
 
     /* Tables of strings that are pre-allocated in the atomsCompartment. */
