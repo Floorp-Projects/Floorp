3fafd4349c3e71e662ae35726e882496c126bf0e
Ed Morley <emorley@mozilla.com>
2012-09-17 17:35:46 +0100

    Backout 62ef2365014e (bug 583890), 8c3df98a9f06 (bug 583890) for intermittent failures in browser_bug583890.js

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -394,7 +394,6 @@ pref("browser.tabs.tabClipWidth", 140);
 pref("browser.tabs.animate", true);
 pref("browser.tabs.onTop", true);
 pref("browser.tabs.drawInTitlebar", true);
-pref("browser.tabs.cropTitleRedundancy", true);
 
 // Where to show tab close buttons:
 // 0  on active tab only
