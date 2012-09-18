8cb90241b83390d9946404d989029131f66f7dac
Andrew Quartey <andrew.quartey@gmail.com>
2012-09-18 16:45:51 -0400

    Bug 788741 - Truncate file extension if too long  r=bz

--- embedding/components/webbrowserpersist/src/nsWebBrowserPersist.cpp
+++ embedding/components/webbrowserpersist/src/nsWebBrowserPersist.cpp
@@ -2213,7 +2213,13 @@ nsWebBrowserPersist::CalculateAndAppendFileExt(nsIURI *aURI, nsIChannel *aChanne
                     uint32_t newLength = newFileName.Length() + fileExt.Length() + 1;
                     if (newLength > kDefaultMaxFilenameLength)
                     {
-                        newFileName.Truncate(newFileName.Length() - (newLength - kDefaultMaxFilenameLength));
+                        if (fileExt.Length() > kDefaultMaxFilenameLength/2)
+                            fileExt.Truncate(kDefaultMaxFilenameLength/2);
+
+                        uint32_t diff = kDefaultMaxFilenameLength - 1 -
+                                        fileExt.Length();
+                        if (newFileName.Length() > diff)
+                            newFileName.Truncate(diff);
                     }
                     newFileName.Append(".");
                     newFileName.Append(fileExt);
