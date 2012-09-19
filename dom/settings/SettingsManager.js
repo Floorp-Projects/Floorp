d20c190c302fb8bff65498e4c2d8a358ceff7467
Gregor Wagner <anygregor@gmail.com>
2012-09-19 10:21:30 -0700

    Bug 791799 - Settings: Remove mozSettings.getLock. r=fabrice

--- dom/settings/SettingsManager.js
+++ dom/settings/SettingsManager.js
@@ -209,7 +209,7 @@ SettingsLock.prototype = {
 };
 
 const SETTINGSMANAGER_CONTRACTID = "@mozilla.org/settingsManager;1";
-const SETTINGSMANAGER_CID        = Components.ID("{0694f5e0-f777-11e1-a21f-0800200c9a66}");
+const SETTINGSMANAGER_CID        = Components.ID("{c40b1c70-00fb-11e2-a21f-0800200c9a66}");
 const nsIDOMSettingsManager      = Ci.nsIDOMSettingsManager;
 
 let myGlobal = this;
@@ -257,11 +257,6 @@ SettingsManager.prototype = {
       myGlobal );
     this.nextTick(function() { this._open = false; }, lock);
     return lock;
-  },
-
-  getLock: function() {
-    dump("WARNING: Use createLock instead of getLock!");
-    return this.createLock();
   },
 
   receiveMessage: function(aMessage) {
