2e895ae996fba454a2ab20eab4714bbe34a3c0ee
Kyle Huey <khuey@kylehuey.com>
2012-09-20 08:50:16 -0700

    Bug 770844: Add a window.mozIndexedDB to work around this bug for now. r=sicking a=akeybl

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -8503,6 +8503,12 @@ nsGlobalWindow::GetIndexedDB(nsIIDBFactory** _retval)
   return NS_OK;
 }
 
+NS_IMETHODIMP
+nsGlobalWindow::GetMozIndexedDB(nsIIDBFactory** _retval)
+{
+  return GetIndexedDB(_retval);
+}
+
 //*****************************************************************************
 // nsGlobalWindow::nsIInterfaceRequestor
 //*****************************************************************************
