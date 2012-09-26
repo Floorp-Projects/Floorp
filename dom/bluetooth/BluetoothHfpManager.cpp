c2f88e81e10938311b9491a8e2f1c7972f15ee28
Eric Chou <echou@mozilla.com>
2012-09-26 21:34:55 +0800

    Bug 791197 - [b2g-bluetooth] BluetoothHfpManager prototype, r=qdot

---
+++ dom/bluetooth/BluetoothHfpManager.cpp
@@ -0,0 +1,80 @@
+/* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "BluetoothHfpManager.h"
+
+#include "BluetoothReplyRunnable.h"
+#include "BluetoothService.h"
+#include "BluetoothServiceUuid.h"
+
+USING_BLUETOOTH_NAMESPACE
+using namespace mozilla::ipc;
+
+static nsRefPtr<BluetoothHfpManager> sInstance = nullptr;
+
+BluetoothHfpManager::BluetoothHfpManager()
+{
+}
+
+BluetoothHfpManager::~BluetoothHfpManager()
+{
+}
+
+//static
+BluetoothHfpManager*
+BluetoothHfpManager::Get()
+{
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (sInstance == nullptr) {
+    sInstance = new BluetoothHfpManager();
+  }
+
+  return sInstance;
+}
+
+// Virtual function of class SocketConsumer
+void
+BluetoothHfpManager::ReceiveSocketData(UnixSocketRawData* aMessage)
+{
+  MOZ_ASSERT(NS_IsMainThread());
+}
+
+bool
+BluetoothHfpManager::Connect(const nsAString& aDeviceObjectPath,
+                             BluetoothReplyRunnable* aRunnable)
+{
+  MOZ_ASSERT(NS_IsMainThread());
+
+  BluetoothService* bs = BluetoothService::Get();
+  if (!bs) {
+    NS_WARNING("BluetoothService not available!");
+    return false;
+  }
+
+  nsString serviceUuidStr =
+    NS_ConvertUTF8toUTF16(mozilla::dom::bluetooth::BluetoothServiceUuidStr::Handsfree);
+
+  nsRefPtr<BluetoothReplyRunnable> runnable = aRunnable;
+
+  nsresult rv = bs->GetSocketViaService(aDeviceObjectPath,
+                                        serviceUuidStr,
+                                        BluetoothSocketType::RFCOMM,
+                                        true,
+                                        false,
+                                        this,
+                                        runnable);
+
+  runnable.forget();
+  return NS_FAILED(rv) ? false : true;
+}
+
+void
+BluetoothHfpManager::Disconnect()
+{
+  CloseSocket();
+}
+
