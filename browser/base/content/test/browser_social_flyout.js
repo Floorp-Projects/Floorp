09e6da528011142cdea0b45a6283ee0f987e4ece
Mark Hammond <mhammond@skippinet.com.au>
2012-09-26 16:22:38 +1000

    Bug 792677 - dynamically adjust social panels as content changes.  r=jaws

--- browser/base/content/test/browser_social_flyout.js
+++ browser/base/content/test/browser_social_flyout.js
@@ -44,6 +44,35 @@ var tests = {
       }
     }
     port.postMessage({topic: "test-init"});
+  },
+
+  testResizeFlyout: function(next) {
+    let panel = document.getElementById("social-flyout-panel");
+    let port = Social.provider.getWorkerPort();
+    ok(port, "provider has a port");
+    port.onmessage = function (e) {
+      let topic = e.data.topic;
+      switch (topic) {
+        case "test-init-done":
+          port.postMessage({topic: "test-flyout-open"});
+          break;
+        case "got-flyout-visibility":
+          // The width of the flyout should be 250px
+          let iframe = panel.firstChild;
+          let cs = iframe.contentWindow.getComputedStyle(iframe.contentDocument.body);
+          is(cs.width, "250px", "should be 250px wide");
+          iframe.contentDocument.addEventListener("SocialTest-DoneMakeWider", function _doneHandler() {
+            iframe.contentDocument.removeEventListener("SocialTest-DoneMakeWider", _doneHandler, false);
+            cs = iframe.contentWindow.getComputedStyle(iframe.contentDocument.body);
+            is(cs.width, "500px", "should now be 500px wide");
+            panel.hidePopup();
+            next();
+          }, false);
+          SocialFlyout.dispatchPanelEvent("socialTest-MakeWider");
+          break;
+      }
+    }
+    port.postMessage({topic: "test-init"});
   }
 }
 
