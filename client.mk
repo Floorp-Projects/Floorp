e58eccb4b287b29430e28aee271e39497a7d4eb1
Ehsan Akhgari <ehsan@mozilla.com>
2012-09-26 15:15:41 -0400

    Bug 793953 follow-up: use the ?= syntax to get rid of the explicit condition; irc-r=glandium
    DONTBUILD this either!

--- client.mk
+++ client.mk
@@ -61,9 +61,7 @@ endif
 AUTOCONF ?= $(shell which autoconf-2.13 autoconf2.13 autoconf213 2>/dev/null | grep -v '^no autoconf' | head -1)
 
 # See if the autoconf package was installed through fink
-ifeq (,$(strip $(AUTOCONF)))
-AUTOCONF = $(shell which fink >/dev/null 2>&1 && echo `which fink`/../../lib/autoconf2.13/bin/autoconf)
-endif
+AUTOCONF ?= $(shell which fink >/dev/null 2>&1 && echo `which fink`/../../lib/autoconf2.13/bin/autoconf)
 
 ifeq (,$(strip $(AUTOCONF)))
 AUTOCONF=$(error Could not find autoconf 2.13)
