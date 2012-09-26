9ee32dea4b0a67b9fcbcb2a9cf68eb4b2709cfe8
Chris Jones <jones.chris.g@gmail.com>
2012-09-25 17:04:01 -0700

    Bug 794297: Block the android build system from looking in mozilla-central. r=mwu

--- Android.mk
+++ Android.mk
@@ -2,4 +2,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# empty file to block B2G/Gonk from trying to build anything inside media/webrtc
+# empty file to block B2G/Gonk from trying to build anything inside mozilla-central
