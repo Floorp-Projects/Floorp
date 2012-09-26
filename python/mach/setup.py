0f8e4c10e371883b55c99bb82a69fad301ce6e1f
Gregory Szorc <gps@mozilla.com>
2012-09-26 09:43:54 -0700

    Bug 751795 - Part 1: mach, the new frontend to mozilla-central; r=jhammel

---
+++ python/mach/setup.py
@@ -0,0 +1,16 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from setuptools import setup
+
+VERSION = '0.1'
+
+setup(
+    name='mach',
+    description='CLI frontend to mozilla-central.',
+    license='MPL 2.0',
+    packages=['mach'],
+    version=VERSION
+)
+
