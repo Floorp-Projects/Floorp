9e9c1eccbe82e096a331ea0a0b95c8b908dab69d
Hubert Figuière <hfiguiere@mozilla.com>
2012-09-26 15:49:46 -0700

    Bug 789466 - Report crashes with prerelease.dogfood.id if available. r=fabrice

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -114,6 +114,19 @@ var shell = {
    },
 
   start: function shell_start() {
+
+    // Dogfood id. We might want to remove it in the future.
+    // see bug 789466
+    try {
+      let dogfoodId = Services.prefs.getCharPref('prerelease.dogfood.id');
+      if (dogfoodId != "") {
+        let cr = Cc["@mozilla.org/xre/app-info;1"]
+                   .getService(Ci.nsICrashReporter);
+        cr.annotateCrashReport("Email", dogfoodId);
+      }
+    }
+    catch (e) { }
+
     let homeURL = this.homeURL;
     if (!homeURL) {
       let msg = 'Fatal error during startup: No homescreen found: try setting B2G_HOMESCREEN';
