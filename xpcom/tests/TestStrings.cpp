e0ccbfc6c7182d33343af1b87322f33e19020be8
Isaac Aggrey <isaac.aggrey@gmail.com>
2012-09-28 17:08:26 -0500

    Bug 795351: Replace LL_MaxInt(), LL_MinInt(), LL_MaxUint() with stdint macros; r=ehsan

--- xpcom/tests/TestStrings.cpp
+++ xpcom/tests/TestStrings.cpp
@@ -686,9 +686,9 @@ bool test_appendint64()
   {
     nsCString str;
 
-    int64_t max = LL_MaxInt();
+    int64_t max = INT64_MAX;
     static const char max_expected[] = "9223372036854775807";
-    int64_t min = LL_MinInt();
+    int64_t min = INT64_MIN;
     static const char min_expected[] = "-9223372036854775808";
     static const char min_expected_oct[] = "1000000000000000000000";
     int64_t maxint_plus1 = LL_INIT(1, 0);
@@ -698,20 +698,20 @@ bool test_appendint64()
     str.AppendInt(max);
 
     if (!str.Equals(max_expected)) {
-      fprintf(stderr, "Error appending LL_MaxInt(): Got %s\n", str.get());
+      fprintf(stderr, "Error appending INT64_MAX: Got %s\n", str.get());
       return false;
     }
 
     str.Truncate();
     str.AppendInt(min);
     if (!str.Equals(min_expected)) {
-      fprintf(stderr, "Error appending LL_MinInt(): Got %s\n", str.get());
+      fprintf(stderr, "Error appending INT64_MIN: Got %s\n", str.get());
       return false;
     }
     str.Truncate();
     str.AppendInt(min, 8);
     if (!str.Equals(min_expected_oct)) {
-      fprintf(stderr, "Error appending LL_MinInt() (oct): Got %s\n", str.get());
+      fprintf(stderr, "Error appending INT64_MIN (oct): Got %s\n", str.get());
       return false;
     }
 
