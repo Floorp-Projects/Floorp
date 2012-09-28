4b31ecf24c1288eae1258d543b350578109e45d3
Gregory Szorc <gps@mozilla.com>
2012-09-28 10:29:32 -0700

    Bug 794713 - Convert process output to Unicode; handle Unicode in logger properly; r=glandium
    DONTBUILD (NPOTB)

--- python/mozbuild/mozbuild/base.py
+++ python/mozbuild/mozbuild/base.py
@@ -9,6 +9,7 @@ import multiprocessing
 import os
 import pymake.parser
 import shlex
+import sys
 import subprocess
 import which
 
@@ -304,6 +305,10 @@ class MozbuildObject(object):
         self.log(logging.INFO, 'process', {'args': args}, ' '.join(args))
 
         def handleLine(line):
+            # Converts str to unicode on Python 2 and bytes to str on Python 3.
+            if isinstance(line, bytes):
+                line = line.decode(sys.stdout.encoding)
+
             if line_handler:
                 line_handler(line)
 
