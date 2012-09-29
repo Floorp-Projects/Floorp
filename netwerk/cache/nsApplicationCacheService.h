da9299600b43b589888fdfdcebd02abbbfa720dc
Ed Morley <emorley@mozilla.com>
2012-09-29 01:44:30 +0100

    Backout bec7e68cad9a & a6228bc28958 (bug 786299) for Windows compilation errors on a CLOSED TREE

--- netwerk/cache/nsApplicationCacheService.h
+++ netwerk/cache/nsApplicationCacheService.h
@@ -5,10 +5,7 @@
 #ifndef _nsApplicationCacheService_h_
 #define _nsApplicationCacheService_h_
 
-#include "nsIApplicationCacheService.h"
 #include "mozilla/Attributes.h"
-
-class nsCacheService;
 
 class nsApplicationCacheService MOZ_FINAL : public nsIApplicationCacheService
 {
@@ -17,9 +14,6 @@ public:
 
     NS_DECL_ISUPPORTS
     NS_DECL_NSIAPPLICATIONCACHESERVICE
-
-    static void AppClearDataObserverInit();
-
 private:
     nsresult GetJARIdentifier(nsIURI *aURI,
                               nsILoadContext *aLoadContext,
