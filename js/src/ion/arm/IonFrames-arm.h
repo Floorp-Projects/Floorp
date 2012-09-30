6a06f6988b726d8f7bf84e926dd88ed615888814
Kannan Vijayan <kvijayan@mozilla.com>
2012-09-29 21:25:26 -0400

    Backing out 4858ec60ad5f - bad bug number in commit message.

--- js/src/ion/arm/IonFrames-arm.h
+++ js/src/ion/arm/IonFrames-arm.h
@@ -157,7 +157,6 @@ class IonOsrFrameLayout : public IonJSFrameLayout
 
 class IonNativeExitFrameLayout;
 class IonOOLNativeGetterExitFrameLayout;
-class IonOOLPropertyOpExitFrameLayout;
 class IonDOMExitFrameLayout;
 
 // this is the frame layout when we are exiting ion code, and about to enter EABI code
@@ -197,9 +196,6 @@ class IonExitFrameLayout : public IonCommonFrameLayout
     inline bool isOOLNativeGetterExit() {
         return footer()->ionCode() == ION_FRAME_OOL_NATIVE_GETTER;
     }
-    inline bool isOOLPropertyOpExit() {
-        return footer()->ionCode() == ION_FRAME_OOL_PROPERTY_OP;
-    }
     inline bool isDomExit() {
         IonCode *code = footer()->ionCode();
         return
@@ -216,10 +212,6 @@ class IonExitFrameLayout : public IonCommonFrameLayout
     inline IonOOLNativeGetterExitFrameLayout *oolNativeGetterExit() {
         JS_ASSERT(isOOLNativeGetterExit());
         return reinterpret_cast<IonOOLNativeGetterExitFrameLayout *>(footer());
-    }
-    inline IonOOLPropertyOpExitFrameLayout *oolPropertyOpExit() {
-        JS_ASSERT(isOOLPropertyOpExit());
-        return reinterpret_cast<IonOOLPropertyOpExitFrameLayout *>(footer());
     }
     inline IonDOMExitFrameLayout *DOMExit() {
         JS_ASSERT(isDomExit());
@@ -283,41 +275,6 @@ class IonOOLNativeGetterExitFrameLayout
     }
     inline uintptr_t argc() const {
         return 0;
-    }
-};
-
-class IonOOLPropertyOpExitFrameLayout
-{
-    IonExitFooterFrame footer_;
-    IonExitFrameLayout exit_;
-
-    // Object for JSHandleObject
-    JSObject *obj_;
-
-    // id for JSHandleId
-    jsid id_;
-
-    // space for JSMutableHandleValue result
-    // use two uint32_t so compiler doesn't align.
-    uint32_t vp0_;
-    uint32_t vp1_;
-
-  public:
-    static inline size_t Size() {
-        return sizeof(IonOOLPropertyOpExitFrameLayout);
-    }
-
-    static size_t offsetOfResult() {
-        return offsetof(IonOOLPropertyOpExitFrameLayout, vp0_);
-    }
-    inline Value *vp() {
-        return reinterpret_cast<Value*>(&vp0_);
-    }
-    inline jsid *id() {
-        return &id_;
-    }
-    inline JSObject **obj() {
-        return &obj_;
     }
 };
 
--- js/src/ion/shared/IonFrames-shared.h
+++ js/src/ion/shared/IonFrames-shared.h
@@ -45,6 +45,5 @@
 #define ION_FRAME_DOMSETTER ((IonCode *)0x2)
 #define ION_FRAME_DOMMETHOD ((IonCode *)0x3)
 #define ION_FRAME_OOL_NATIVE_GETTER ((IonCode *)0x4)
-#define ION_FRAME_OOL_PROPERTY_OP   ((IonCode *)0x5)
 
 #endif
