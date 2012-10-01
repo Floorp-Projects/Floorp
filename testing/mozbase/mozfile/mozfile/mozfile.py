1a43e8a3771ecd3f585690df4b487066472c566b
Jeff Hammel <jhammel@mozilla.com>
2012-10-01 14:00:55 -0700

    Bug 795943 - Mirror mozbase -> m-c for week of Oct 1 @ https://github.com/mozilla/mozbase/commit/22aa0aee78cb3c45ff8c4f1b1844f13216a9c220;r=wlach
    --HG--
    extra : rebase_source : 389b6c3a2923c40167698b477e4d57b230b1a063

---
+++ testing/mozbase/mozfile/mozfile/mozfile.py
@@ -0,0 +1,133 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+mozfile.py:
+Cointains file functions for mozbase:
+https://bugzilla.mozilla.org/show_bug.cgi?id=774916
+"""
+
+import os
+import tarfile
+import zipfile
+
+__all__ = ['extract_tarball', 'extract_zip', 'extract', 'rmtree']
+
+
+### utilities for extracting archives
+
+def extract_tarball(src, dest):
+    """extract a .tar file"""
+
+    bundle = tarfile.open(src)
+    namelist = bundle.getnames()
+
+    for name in namelist:
+        bundle.extract(name, path=dest)
+    bundle.close()
+    return namelist
+
+
+def extract_zip(src, dest):
+    """extract a zip file"""
+
+    bundle = zipfile.ZipFile(src)
+    namelist = bundle.namelist()
+
+    for name in namelist:
+        filename = os.path.realpath(os.path.join(dest, name))
+        if name.endswith('/'):
+            os.makedirs(filename)
+        else:
+            path = os.path.dirname(filename)
+            if not os.path.isdir(path):
+                os.makedirs(path)
+            _dest = open(filename, 'wb')
+            _dest.write(bundle.read(name))
+            _dest.close()
+    bundle.close()
+    return namelist
+
+
+def extract(src, dest=None):
+    """
+    Takes in a tar or zip file and extracts it to dest
+
+    If dest is not specified, extracts to os.path.dirname(src)
+
+    Returns the list of top level files that were extracted
+    """
+
+    assert os.path.exists(src), "'%s' does not exist" % src
+    assert not os.path.isfile(dest), "dest cannot be a file"
+
+    if dest is None:
+        dest = os.path.dirname(src)
+    elif not os.path.isdir(dest):
+        os.makedirs(dest)
+
+    if zipfile.is_zipfile(src):
+        namelist = extract_zip(src, dest)
+    elif tarfile.is_tarfile(src):
+        namelist = extract_tarball(src, dest)
+    else:
+        raise Exception("mozfile.extract: no archive format found for '%s'" %
+                        src)
+
+    # namelist returns paths with forward slashes even in windows
+    top_level_files = [os.path.join(dest, name) for name in namelist
+                       if len(name.rstrip('/').split('/')) == 1]
+
+    # namelist doesn't include folders, append these to the list
+    for name in namelist:
+        root = os.path.join(dest, name[:name.find('/')])
+        if root not in top_level_files:
+            top_level_files.append(root)
+
+    return top_level_files
+
+
+def rmtree(dir):
+    """This is a replacement for shutil.rmtree that works better under
+    windows. Thanks to Bear at the OSAF for the code."""
+
+    if not os.path.exists(dir):
+        return
+    if os.path.islink(dir):
+        os.remove(dir)
+        return
+
+    # Verify the directory is read/write/execute for the current user
+    os.chmod(dir, 0700)
+
+    # os.listdir below only returns a list of unicode filenames
+    # if the parameter is unicode.
+    # If a non-unicode-named dir contains a unicode filename,
+    # that filename will get garbled.
+    # So force dir to be unicode.
+    try:
+        dir = unicode(dir, "utf-8")
+    except:
+        print("rmtree: decoding from UTF-8 failed")
+
+    for name in os.listdir(dir):
+        full_name = os.path.join(dir, name)
+        # on Windows, if we don't have write permission we can't remove
+        # the file/directory either, so turn that on
+        if os.name == 'nt':
+            if not os.access(full_name, os.W_OK):
+                # I think this is now redundant, but I don't have an NT
+                # machine to test on, so I'm going to leave it in place
+                # -warner
+                os.chmod(full_name, 0600)
+
+        if os.path.islink(full_name):
+            os.remove(full_name)
+        elif os.path.isdir(full_name):
+            rmtree(full_name)
+        else:
+            if os.path.isfile(full_name):
+                os.chmod(full_name, 0700)
+            os.remove(full_name)
+    os.rmdir(dir)
--- testing/mozbase/mozfile/setup.py
+++ testing/mozbase/mozfile/setup.py
@@ -2,37 +2,31 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+
 import os
 from setuptools import setup
 
-PACKAGE_VERSION = '0.9'
+PACKAGE_VERSION = '0.0'
 
-# take description from README
-here = os.path.dirname(os.path.abspath(__file__))
+# get documentation from the README
 try:
+    here = os.path.dirname(os.path.abspath(__file__))
     description = file(os.path.join(here, 'README.md')).read()
 except (OSError, IOError):
     description = ''
 
-deps = ['mozprocess == 0.7']
-
-setup(name='mozdevice',
+setup(name='mozfile',
       version=PACKAGE_VERSION,
-      description="Mozilla-authored device management",
+      description="common file utilities for Mozilla python usage",
       long_description=description,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Mozilla Automation and Testing Team',
+      keywords='mozilla',
+      author='Mozilla Automation and Tools team',
       author_email='tools@lists.mozilla.org',
       url='https://wiki.mozilla.org/Auto-tools/Projects/MozBase',
       license='MPL',
-      packages=['mozdevice'],
+      packages=['mozfile'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=deps,
-      entry_points="""
-      # -*- Entry points: -*-
-      [console_scripts]
-      sut = mozdevice.sutcli:cli
-      """,
+      install_requires=[]
       )
