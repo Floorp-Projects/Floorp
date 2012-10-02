f5c9eee65915168ae2573720ca2bb3f3748b9640
Ryan VanderMeulen <ryanvm@gmail.com>
2012-10-02 16:38:51 -0400

    Bug 795662 - Update libpng to version 1.5.13. r=joe

--- media/libpng/png.c
+++ media/libpng/png.c
@@ -14,7 +14,7 @@
 #include "pngpriv.h"
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_5_11 Your_png_h_is_not_version_1_5_11;
+typedef png_libpng_version_1_5_13 Your_png_h_is_not_version_1_5_13;
 
 /* Tells libpng that we have already handled the first "num_bytes" bytes
  * of the PNG file signature.  If the PNG data is embedded into another
@@ -655,13 +655,13 @@ png_get_copyright(png_const_structp png_ptr)
 #else
 #  ifdef __STDC__
    return PNG_STRING_NEWLINE \
-     "libpng version 1.5.11 - June 14, 2012" PNG_STRING_NEWLINE \
+     "libpng version 1.5.13 - September 27, 2012" PNG_STRING_NEWLINE \
      "Copyright (c) 1998-2012 Glenn Randers-Pehrson" PNG_STRING_NEWLINE \
      "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
      "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
      PNG_STRING_NEWLINE;
 #  else
-      return "libpng version 1.5.11 - June 14, 2012\
+      return "libpng version 1.5.13 - September 27, 2012\
       Copyright (c) 1998-2012 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.";
