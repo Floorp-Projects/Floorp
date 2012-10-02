f647fe112ef66b422dd6484c06a5e46b7f5167fe
Nathan Froyd <froydnj@mozilla.com>
2012-10-02 15:14:50 -0400

    Bug 797106 - don't use PR_BYTES_PER_WORD in nsTextFragment*.cpp; r=ehsan

--- content/base/src/nsTextFragmentSSE2.cpp
+++ content/base/src/nsTextFragmentSSE2.cpp
@@ -8,6 +8,7 @@
 #include <emmintrin.h>
 #include "nscore.h"
 #include "nsAlgorithm.h"
+#include "nsTextFragmentImpl.h"
 
 namespace mozilla {
 namespace SSE2 {
@@ -23,17 +24,9 @@ int32_t
 FirstNon8Bit(const PRUnichar *str, const PRUnichar *end)
 {
   const uint32_t numUnicharsPerVector = 8;
-
-#if PR_BYTES_PER_WORD == 4
-  const size_t mask = 0xff00ff00;
-  const uint32_t numUnicharsPerWord = 2;
-#elif PR_BYTES_PER_WORD == 8
-  const size_t mask = 0xff00ff00ff00ff00;
-  const uint32_t numUnicharsPerWord = 4;
-#else
-#error Unknown platform!
-#endif
-
+  typedef Non8BitParameters<sizeof(size_t)> p;
+  const size_t mask = p::mask();
+  const uint32_t numUnicharsPerWord = p::numUnicharsPerWord();
   const int32_t len = end - str;
   int32_t i = 0;
 
