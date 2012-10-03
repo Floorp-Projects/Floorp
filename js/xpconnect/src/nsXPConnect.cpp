7d4b7c3cb6e7efe157ab893b66b96fb8e54c2f14
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2012-10-02 20:14:38 -0400

    Bug 795544 - Make StringEncoding API available from xpcshell/jsm. r=bz

--- js/xpconnect/src/nsXPConnect.cpp
+++ js/xpconnect/src/nsXPConnect.cpp
@@ -34,6 +34,8 @@
 #include "dombindings.h"
 
 #include "mozilla/dom/BindingUtils.h"
+#include "mozilla/dom/TextDecoderBinding.h"
+#include "mozilla/dom/TextEncoderBinding.h"
 
 #include "nsWrapperCacheInlines.h"
 #include "nsDOMMutationObserver.h"
@@ -1185,6 +1187,12 @@ nsXPConnect::InitClassesWithNewWrappedGlobal(JSContext * aJSContext,
     // XXX Someone who knows why we can assert this should re-check
     //     (after bug 720580).
     MOZ_ASSERT(js::GetObjectClass(global)->flags & JSCLASS_DOM_GLOBAL);
+
+    // Init WebIDL binding constructors wanted on all XPConnect globals.
+    if (!TextDecoderBinding::GetProtoObject(aJSContext, global, global) ||
+        !TextEncoderBinding::GetProtoObject(aJSContext, global, global)) {
+        return UnexpectedFailure(NS_ERROR_FAILURE);
+    }
 
     wrappedGlobal.forget(_retval);
     return NS_OK;
