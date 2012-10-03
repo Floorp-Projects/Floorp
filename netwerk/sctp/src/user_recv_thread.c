c7a187c763d8f8ec9e38dee37e867b3540bcd3a0
Randell Jesup <rjesup@jesup.org>
2012-10-03 03:22:45 -0400

    Bug 729511: patch to libsctp_8279 - add __USE_GNU for netinet/in.h r=biesi

--- netwerk/sctp/src/user_recv_thread.c
+++ netwerk/sctp/src/user_recv_thread.c
@@ -32,7 +32,10 @@
 #include <sys/types.h>
 #if !defined(__Userspace_os_Windows)
 #include <sys/socket.h>
+/*  to make sure some OSs define in6_pktinfo */
+#define __USE_GNU
 #include <netinet/in.h>
+#undef __USE_GNU
 #include <unistd.h>
 #include <pthread.h>
 #if !defined(__Userspace_os_FreeBSD)
