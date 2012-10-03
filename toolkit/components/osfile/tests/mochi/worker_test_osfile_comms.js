e848fc620667bc62841d8c757aefaaa7ce7137ba
David Rajchenbach-Teller <dteller@mozilla.com>
2012-10-02 20:14:39 -0400

    Bug 795687 - Move all of OS.File to typed arrays instead of array buffers. r=froydnj

--- toolkit/components/osfile/tests/mochi/worker_test_osfile_comms.js
+++ toolkit/components/osfile/tests/mochi/worker_test_osfile_comms.js
@@ -48,14 +48,13 @@ self.onmessage = function(msg) {
         is(candidate, "This is a test", prefix);
       }},
     { typename: "OS.Shared.Type.char.in_ptr",
-      valuedescr: "ArrayBuffer",
+      valuedescr: "Typed array",
       value: (function() {
-                let buf = new ArrayBuffer(15);
-                let view = new Uint8Array(buf);
+                let view = new Uint8Array(15);
                 for (let i = 0; i < 15; ++i) {
                   view[i] = i;
                 }
-                return buf;
+                return view;
               })(),
       type: OS.Shared.Type.char.in_ptr,
       check: function check_ArrayBuffer(candidate, prefix) {
