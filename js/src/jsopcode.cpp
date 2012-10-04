2648bbfdda291928201fc03514c7dc33ac1a08ff
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2012-10-04 12:35:03 -0700

    Revert "Bug 797496 - Ignore hidden PC even with try notes. r=luke"
    This reverts changeset 3b7a134bc3aa.

--- js/src/jsopcode.cpp
+++ js/src/jsopcode.cpp
@@ -6436,27 +6436,31 @@ ReconstructPCStack(JSContext *cx, JSScript *script, jsbytecode *target, jsbyteco
                 continue;
             }
 
+            if (!script->hasTrynotes()) {
+                /* Use the normal pc count if continue after the goto */
+                hpcdepth = unsigned(-1);
+                continue;
+            }
+
             /*
              * If we do not follow a goto we look for another mean to continue
              * at the next PC.
              */
-            if (script->hasTrynotes()) {
-                JSTryNote *tn = script->trynotes()->vector;
-                JSTryNote *tnEnd = tn + script->trynotes()->length;
-                for (; tn != tnEnd; tn++) {
-                    jsbytecode *start = script->main() + tn->start;
-                    jsbytecode *end = start + tn->length;
-                    if (start < pc && pc <= end && end <= target)
-                        break;
-                }
+            JSTryNote *tn = script->trynotes()->vector;
+            JSTryNote *tnEnd = tn + script->trynotes()->length;
+            for (; tn != tnEnd; tn++) {
+                jsbytecode *start = script->main() + tn->start;
+                jsbytecode *end = start + tn->length;
+                if (start < pc && pc <= end && end <= target)
+                    break;
+            }
 
-                if (tn != tnEnd) {
-                    pcdepth = tn->stackDepth;
-                    hpcdepth = unsigned(-1);
-                    oplen = 0;
-                    pc = script->main() + tn->start + tn->length;
-                    continue;
-                }
+            if (tn != tnEnd) {
+                pcdepth = tn->stackDepth;
+                hpcdepth = unsigned(-1);
+                oplen = 0;
+                pc = script->main() + tn->start + tn->length;
+                continue;
             }
 
             /*
@@ -6465,10 +6469,6 @@ ReconstructPCStack(JSContext *cx, JSScript *script, jsbytecode *target, jsbyteco
              */
             if (JSOp(*(pc + oplen)) == JSOP_THROWING)
                 hpcdepth = pcdepth + 2;
-            else
-                /* Use the normal pc count if continue after the goto */
-                hpcdepth = unsigned(-1);
-
             continue;
         }
 
