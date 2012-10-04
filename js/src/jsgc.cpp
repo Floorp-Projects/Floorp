c61a0f1cc82d345b72f738440a7335cc28ce5522
Terrence Cole <terrence@mozilla.com>
2012-10-04 14:22:51 -0700

    Bug 794667 - Add an AutoAssertCanGC to help with exact rooting; r=billm
    This will allow us to annotate all methods with either an AutoAssertNoGC or
    AssertCanGC. These will serve the purpose currently being served by
    MaybeCheckStackRoots, but because they are lighter can be used throughout the
    engine.

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -4670,12 +4670,14 @@ namespace js {
 void
 GC(JSRuntime *rt, JSGCInvocationKind gckind, gcreason::Reason reason)
 {
+    AssertCanGC();
     Collect(rt, false, SliceBudget::Unlimited, gckind, reason);
 }
 
 void
 GCSlice(JSRuntime *rt, JSGCInvocationKind gckind, gcreason::Reason reason, int64_t millis)
 {
+    AssertCanGC();
     int64_t sliceBudget;
     if (millis)
         sliceBudget = SliceBudget::TimeBudget(millis);
@@ -4690,12 +4692,14 @@ GCSlice(JSRuntime *rt, JSGCInvocationKind gckind, gcreason::Reason reason, int64
 void
 GCFinalSlice(JSRuntime *rt, JSGCInvocationKind gckind, gcreason::Reason reason)
 {
+    AssertCanGC();
     Collect(rt, true, SliceBudget::Unlimited, gckind, reason);
 }
 
 void
 GCDebugSlice(JSRuntime *rt, bool limit, int64_t objCount)
 {
+    AssertCanGC();
     int64_t budget = limit ? SliceBudget::WorkBudget(objCount) : SliceBudget::Unlimited;
     PrepareForDebugGC(rt);
     Collect(rt, true, budget, GC_NORMAL, gcreason::API);
