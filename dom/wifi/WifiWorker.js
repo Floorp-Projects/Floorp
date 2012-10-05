1d658451f2b7e6cd97d2569af8d6525ac2cdfd7e
Masashi Honma <masashi.honma@gmail.com>
2012-10-05 16:13:54 -0400

    Bug 790231 - Add a way of telling if a scan result supports WPS. r=mrbkap

--- dom/wifi/WifiWorker.js
+++ dom/wifi/WifiWorker.js
@@ -1237,6 +1237,16 @@ function getKeyManagement(flags) {
   return types;
 }
 
+function getCapabilities(flags) {
+  var types = [];
+  if (!flags)
+    return types;
+
+  if (/\[WPS/.test(flags))
+    types.push("WPS");
+  return types;
+}
+
 // These constants shamelessly ripped from WifiManager.java
 // strength is the value returned by scan_results. It is nominally in dB. We
 // transform it into a percentage for clients looking to simply show a
@@ -1259,12 +1269,14 @@ function calculateSignal(strength) {
   return Math.floor(((strength - MIN_RSSI) / (MAX_RSSI - MIN_RSSI)) * 100);
 }
 
-function Network(ssid, capabilities, password) {
+function Network(ssid, security, password, capabilities) {
   this.ssid = ssid;
-  this.capabilities = capabilities;
+  this.security = security;
 
   if (typeof password !== "undefined")
     this.password = password;
+  if (typeof capabilities !== "undefined")
+    this.capabilities = capabilities;
   // TODO connected here as well?
 
   this.__exposedProps__ = Network.api;
@@ -1272,6 +1284,7 @@ function Network(ssid, capabilities, password) {
 
 Network.api = {
   ssid: "r",
+  security: "r",
   capabilities: "r",
   known: "r",
 
@@ -1286,7 +1299,8 @@ Network.api = {
 // Note: We never use ScanResult.prototype, so the fact that it's unrelated to
 // Network.prototype is OK.
 function ScanResult(ssid, bssid, flags, signal) {
-  Network.call(this, ssid, getKeyManagement(flags));
+  Network.call(this, ssid, getKeyManagement(flags), undefined,
+               getCapabilities(flags));
   this.bssid = bssid;
   this.signalStrength = signal;
   this.relSignalStrength = calculateSignal(Number(signal));
@@ -1423,11 +1437,9 @@ function WifiWorker() {
   // self.configuredNetworks and prepares it for the DOM.
   netToDOM = function(net) {
     var ssid = dequote(net.ssid);
-    var capabilities = (net.key_mgmt === "NONE" && net.wep_key0)
-                       ? ["WEP"]
-                       : (net.key_mgmt && net.key_mgmt !== "NONE")
-                       ? [net.key_mgmt]
-                       : [];
+    var security = (net.key_mgmt === "NONE" && net.wep_key0) ? ["WEP"]
+                   : (net.key_mgmt && net.key_mgmt !== "NONE") ? [net.key_mgmt]
+                   : [];
     var password;
     if (("psk" in net && net.psk) ||
         ("password" in net && net.password) ||
@@ -1435,7 +1447,7 @@ function WifiWorker() {
       password = "*";
     }
 
-    var pub = new Network(ssid, capabilities, password);
+    var pub = new Network(ssid, security, password);
     if (net.identity)
       pub.identity = dequote(net.identity);
     if (net.netId)
@@ -1456,6 +1468,7 @@ function WifiWorker() {
     delete net.bssid;
     delete net.signalStrength;
     delete net.relSignalStrength;
+    delete net.security;
     delete net.capabilities;
 
     if (!configured)
