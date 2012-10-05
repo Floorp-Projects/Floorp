e062c22ddeefaa9522b66fec98dffd6f8b6ff773
Andrew McCreight <amccreight@mozilla.com>
2012-10-05 12:15:04 -0700

    Bug 797649 - Truncate URLs logged in --DOMWINDOW. r=bz

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -834,6 +834,12 @@ nsGlobalWindow::~nsGlobalWindow()
     nsAutoCString url;
     if (mLastOpenedURI) {
       mLastOpenedURI->GetSpec(url);
+
+      // Data URLs can be very long, so truncate to avoid flooding the log.
+      const uint32_t maxURLLength = 1000;
+      if (url.Length() > maxURLLength) {
+        url.Truncate(maxURLLength);
+      }
     }
 
     printf("--DOMWINDOW == %d (%p) [serial = %d] [outer = %p] [url = %s]\n",
