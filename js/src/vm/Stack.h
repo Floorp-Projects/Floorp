8afcb0df69789c13295b6a0c6b59a6b32c685415
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2012-10-05 11:54:02 -0700

    Bug 797977 - Embed !isIon() assertion in StackIter::fp(). r=luke

--- js/src/vm/Stack.h
+++ js/src/vm/Stack.h
@@ -1770,8 +1770,7 @@ class StackIter
     bool isNonEvalFunctionFrame() const;
     bool isConstructing() const;
 
-    // :TODO: Add && !isIon() in JS_ASSERT of fp() and sp().
-    StackFrame *fp() const { JS_ASSERT(isScript()); return fp_; }
+    StackFrame *fp() const { JS_ASSERT(isScript() && !isIon()); return fp_; }
     jsbytecode *pc() const { JS_ASSERT(isScript()); return pc_; }
     JSScript   *script() const { JS_ASSERT(isScript()); return script_; }
     JSFunction *callee() const;
