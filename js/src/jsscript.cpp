685bbd8098a02c262a2951ddfa001f4f48df88ab
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2012-10-04 23:13:37 -0700

    Bug 792398 - Recover arguments from bailouts. r=luke

--- js/src/jsscript.cpp
+++ js/src/jsscript.cpp
@@ -2573,7 +2573,7 @@ JSScript::argumentsOptimizationFailed(JSContext *cx, HandleScript script)
      */
     for (AllFramesIter i(cx->stack.space()); !i.done(); ++i) {
         StackFrame *fp = i.fp();
-        if (fp->isFunctionFrame() && fp->script() == script) {
+        if (fp->isFunctionFrame() && !fp->runningInIon() && fp->script() == script) {
             ArgumentsObject *argsobj = ArgumentsObject::createExpected(cx, fp);
             if (!argsobj) {
                 /*
