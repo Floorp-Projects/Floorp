d6211fbaeefc341842b69fdc9f95fac0f41b63e5
Robert O'Callahan <robert@ocallahan.org>
2012-10-05 15:29:10 +1300

    Bug 603880. Implement nsWindow::GetDefaultScale on Windows based on LOGPIXELSY, set by font DPI settings. r=jmathies

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -90,6 +90,7 @@ public:
   NS_IMETHOD              SetParent(nsIWidget *aNewParent);
   virtual nsIWidget*      GetParent(void);
   virtual float           GetDPI();
+  virtual double          GetDefaultScale();
   NS_IMETHOD              Show(bool bState);
   virtual bool            IsVisible() const;
   NS_IMETHOD              ConstrainPosition(bool aAllowSlop, int32_t *aX, int32_t *aY);
