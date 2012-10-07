94fa1a954d60df45195edb26d2c6bf2a1e1ecc7b
Patrick McManus <mcmanus@ducksong.com>
2012-10-06 20:48:20 -0400

    bug 790184 spdy: fix tcp send buffers to bound ping rtt r=honzab sr=biesi

--- netwerk/protocol/http/ASpdySession.h
+++ netwerk/protocol/http/ASpdySession.h
@@ -41,6 +41,7 @@ public:
   virtual void PrintDiagnostics (nsCString &log) = 0;
 
   const static uint32_t kSendingChunkSize = 4096;
+  const static uint32_t kTCPSendBufferSize = 131072;
 };
 
 // this is essentially a single instantiation as a member of nsHttpHandler.
