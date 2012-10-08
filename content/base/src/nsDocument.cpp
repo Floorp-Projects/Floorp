ea25893c4260b72e163510d3c167bc734bafaa93
Boris Zbarsky <bzbarsky@mit.edu>
2012-10-07 22:39:09 -0400

    Bug 795221 part 4.  Hook up <link> elements to cycle collect their stylesheet.  r=smaug
    --HG--
    rename : content/html/content/crashtests/795221-1.html => content/html/content/crashtests/795221-3.html

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -1897,6 +1897,10 @@ NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INTERNAL(nsDocument)
   if (tmp->mSubDocuments && tmp->mSubDocuments->ops) {
     PL_DHashTableEnumerate(tmp->mSubDocuments, SubDocTraverser, &cb);
   }
+
+  if (tmp->mCSSLoader) {
+    tmp->mCSSLoader->TraverseCachedSheets(cb);
+  }
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
 
 
@@ -1969,6 +1973,10 @@ NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(nsDocument)
   }
 
   tmp->mPendingTitleChangeEvent.Revoke();
+
+  if (tmp->mCSSLoader) {
+    tmp->mCSSLoader->UnlinkCachedSheets();
+  }
 
   tmp->mInUnlinkOrDeletion = false;
 NS_IMPL_CYCLE_COLLECTION_UNLINK_END
