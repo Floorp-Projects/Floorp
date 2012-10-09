566476704c7903479f14a418d7f102c1b5d180dd
Ehsan Akhgari <ehsan@mozilla.com>
2012-10-09 16:01:24 -0400

    Bug 799432 - Backout changeset 30ab4e1d4874 and changeset dbcc29e9fcf6 from bug 777292 since they're no longer necessary with bug 795238

--- js/xpconnect/src/XPCComponents.cpp
+++ js/xpconnect/src/XPCComponents.cpp
@@ -1404,10 +1404,7 @@ nsXPCComponents_Results::NewResolve(nsIXPConnectWrappedNative *wrapper,
         nsresult rv;
         while (nsXPCException::IterateNSResults(&rv, &rv_name, nullptr, &iter)) {
             if (!strcmp(name.ptr(), rv_name)) {
-                // The two casts below is required since nsresult is an enum,
-                // and it can be interpreted as a signed integer if we directly
-                // cast to a double.
-                jsval val = JS_NumberValue((double)(uint32_t)rv);
+                jsval val = JS_NumberValue((double)rv);
 
                 *objp = obj;
                 if (!JS_DefinePropertyById(cx, obj, id, val,
@@ -4734,10 +4731,7 @@ nsXPCComponents::GetProperty(nsIXPConnectWrappedNative *wrapper,
 
     nsresult rv = NS_OK;
     if (doResult) {
-        // The two casts below is required since nsresult is an enum,
-        // and it can be interpreted as a signed integer if we directly
-        // cast to a double.
-        *vp = JS_NumberValue((double)(uint32_t) res);
+        *vp = JS_NumberValue((double) res);
         rv = NS_SUCCESS_I_DID_SOMETHING;
     }
 
