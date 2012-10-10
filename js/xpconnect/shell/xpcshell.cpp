e1511ebef3ddeb9784e784b4a6838360dd1572ca
Bobby Holley <bobbyholley@gmail.com>
2012-10-10 11:01:26 +0200

    Bug 797204 - Remove API. r=mrbkap
    \o/

--- js/xpconnect/shell/xpcshell.cpp
+++ js/xpconnect/shell/xpcshell.cpp
@@ -1508,13 +1508,6 @@ FullTrustSecMan::GetCxSubjectPrincipal(JSContext *cx)
     return mSystemPrincipal;
 }
 
-NS_IMETHODIMP_(nsIPrincipal *)
-FullTrustSecMan::GetCxSubjectPrincipalAndFrame(JSContext *cx, JSStackFrame **fp)
-{
-    *fp = nullptr;
-    return mSystemPrincipal;
-}
-
 NS_IMETHODIMP
 FullTrustSecMan::GetExtendedOrigin(nsIURI* aURI, uint32_t aAppId,
                                    bool aInMozBrowser,
