3a537ff9cab85aa8464e35204d480e6cff53e60f
Ed Morley <emorley@mozilla.com>
2012-10-10 12:15:30 +0100

    Backout 2231b8e778a3 (bug 715658) for errors linking on Windows

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -503,9 +503,11 @@ void SourceBufferDestroy(void *srcSurfUD)
   delete static_cast<SourceSurfaceUserData*>(srcSurfUD);
 }
 
-void SourceSnapshotDetached(void *nullSurf)
+void SourceSnapshotDetached(cairo_surface_t *nullSurf)
 {
-  gfxImageSurface *origSurf = static_cast<gfxImageSurface*>(nullSurf);
+  gfxImageSurface* origSurf =
+    static_cast<gfxImageSurface*>(cairo_surface_get_user_data(nullSurf, &kSourceSurface));
+
   origSurf->SetData(&kSourceSurface, NULL, NULL);
 }
 
@@ -620,9 +622,14 @@ gfxPlatform::GetSourceSurfaceForSurface(DrawTarget *aTarget, gfxASurface *aSurfa
 
     }
 
-    cairo_surface_set_mime_data(imgSurface->CairoSurface(), "mozilla/magic",
-                                (const unsigned char *) "data", 4,
-                                SourceSnapshotDetached, imgSurface.get());
+    cairo_surface_t *nullSurf =
+	cairo_null_surface_create(CAIRO_CONTENT_COLOR_ALPHA);
+    cairo_surface_set_user_data(nullSurf,
+                                &kSourceSurface,
+                                imgSurface,
+                                NULL);
+    cairo_surface_attach_snapshot(imgSurface->CairoSurface(), nullSurf, SourceSnapshotDetached);
+    cairo_surface_destroy(nullSurf);
   }
 
   SourceSurfaceUserData *srcSurfUD = new SourceSurfaceUserData;
