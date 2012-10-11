b8c0d1c541b029671485113ed7a1abf1deeddd55
Isaac Aggrey <isaac.aggrey@gmail.com>
2012-10-11 01:00:54 -0500

    Bug 794510: Part 1 - Build with NO_NSPR_10_SUPPORT by default; r=ehsan,glandium,wtc

--- toolkit/xre/nsConsoleWriter.cpp
+++ toolkit/xre/nsConsoleWriter.cpp
@@ -2,10 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifdef NO_NSPR_10_SUPPORT
-#undef NO_NSPR_10_SUPPORT
-#endif
-
 #include "NSPRFormatTime.h" // must include before any headers that include prtime.h
 #include "nsAppRunner.h"
 
