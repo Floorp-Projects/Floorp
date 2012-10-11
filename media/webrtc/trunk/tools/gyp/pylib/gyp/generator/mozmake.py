08c7895492ca9a4a420ef173ad0ac0b6e976d304
Ed Morley <emorley@mozilla.com>
2012-10-11 14:37:44 +0100

    Backout d6e84a64d3d4 (bug 798969) for burning

--- media/webrtc/trunk/tools/gyp/pylib/gyp/generator/mozmake.py
+++ media/webrtc/trunk/tools/gyp/pylib/gyp/generator/mozmake.py
@@ -440,7 +440,7 @@ def GenerateOutput(target_list, target_dicts, data, params):
                 swapslashes(top),
                 swapslashes(src),
                 swapslashes(relative_srcdir))
-  scriptname = "$(topsrcdir)/media/webrtc/trunk/tools/gyp/pylib/gyp/generator/mozmake.py"
+  scriptname = topsrcdir_path(__file__)
   # Reassemble a commandline from parts so that all the paths are correct
   commandline = [topsrcdir_path(sys.argv[0]),
                  "--format=mozmake",
