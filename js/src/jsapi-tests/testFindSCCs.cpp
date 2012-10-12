11592816224f0afaf20919583fafdad5a79ed99e
Jon Coppeard <jcoppeard@mozilla.com>
2012-10-12 10:45:29 +0100

    Bug 790338 - Implement Tarjan's algorithm to find the stongly connected components of the compartment graph r=billm
    --HG--
    rename : accessible/src/windows/sdn/sdnAccessible.cpp => accessible/src/msaa/nsAccessNodeWrap.cpp
    rename : accessible/src/windows/sdn/sdnAccessible.h => accessible/src/msaa/nsAccessNodeWrap.h
    extra : rebase_source : f529480202322726c55c23e40529c81092c5b6c3

---
+++ js/src/jsapi-tests/testFindSCCs.cpp
@@ -0,0 +1,263 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=8 sw=4 et tw=99:
+ */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+#include "tests.h"
+#include <string.h>
+#include <stdarg.h>
+
+#include "../gc/FindSCCs.h"
+#include "jscntxt.h"
+#include "jsgc.h"
+
+static const unsigned MaxVertices = 10;
+
+using js::gc::GraphNodeBase;
+using js::gc::ComponentFinder;
+
+struct TestNode : public GraphNodeBase
+{
+    unsigned   index;
+    bool       hasEdge[MaxVertices];
+
+    void findOutgoingEdges(ComponentFinder& finder);
+};
+
+static TestNode Vertex[MaxVertices];
+
+void
+TestNode::findOutgoingEdges(ComponentFinder& finder)
+{
+    for (unsigned i = 0; i < MaxVertices; ++i) {
+        if (hasEdge[i])
+            finder.addEdgeTo(&Vertex[i]);
+    }
+}
+
+BEGIN_TEST(testFindSCCs)
+{
+    // no vertices
+
+    setup(0);
+    run();
+    CHECK(end());
+
+    // no edges
+
+    setup(1);
+    run();
+    CHECK(group(0, -1));
+    CHECK(end());
+
+    setup(3);
+    run();
+    CHECK(group(2, -1));
+    CHECK(group(1, -1));
+    CHECK(group(0, -1));
+    CHECK(end());
+
+    // linear
+
+    setup(3);
+    edge(0, 1);
+    edge(1, 2);
+    run();
+    CHECK(group(0, -1));
+    CHECK(group(1, -1));
+    CHECK(group(2, -1));
+    CHECK(end());
+
+    // tree
+
+    setup(3);
+    edge(0, 1);
+    edge(0, 2);
+    run();
+    CHECK(group(0, -1));
+    CHECK(group(2, -1));
+    CHECK(group(1, -1));
+    CHECK(end());
+
+    // cycles
+
+    setup(3);
+    edge(0, 1);
+    edge(1, 2);
+    edge(2, 0);
+    run();
+    CHECK(group(0, 1, 2, -1));
+    CHECK(end());
+
+    setup(4);
+    edge(0, 1);
+    edge(1, 2);
+    edge(2, 1);
+    edge(2, 3);
+    run();
+    CHECK(group(0, -1));
+    CHECK(group(1, 2, -1));
+    CHECK(group(3, -1));
+    CHECK(end());
+
+    // remaining
+
+    setup(2);
+    edge(0, 1);
+    run();
+    CHECK(remaining(0, 1, -1));
+    CHECK(end());
+
+    setup(2);
+    edge(0, 1);
+    run();
+    CHECK(group(0, -1));
+    CHECK(remaining(1, -1));
+    CHECK(end());
+
+    setup(2);
+    edge(0, 1);
+    run();
+    CHECK(group(0, -1));
+    CHECK(group(1, -1));
+    CHECK(remaining(-1));
+    CHECK(end());
+
+    return true;
+}
+
+unsigned vertex_count;
+ComponentFinder *finder;
+GraphNodeBase *resultsList;
+
+void setup(unsigned count)
+{
+    vertex_count = count;
+    for (unsigned i = 0; i < MaxVertices; ++i) {
+        TestNode &v = Vertex[i];
+        v.gcNextGraphNode = NULL;
+        v.index = i;
+        memset(&v.hasEdge, 0, sizeof(v.hasEdge));
+    }
+}
+
+void edge(unsigned src_index, unsigned dest_index)
+{
+    Vertex[src_index].hasEdge[dest_index] = true;
+}
+
+void run()
+{
+    finder = new ComponentFinder(rt->nativeStackLimit);
+    for (unsigned i = 0; i < vertex_count; ++i)
+        finder->addNode(&Vertex[i]);
+    resultsList = finder->getResultsList();
+}
+
+bool group(int vertex, ...)
+{
+    TestNode *v = (TestNode *)ComponentFinder::getNextGroup(resultsList);
+
+    va_list ap;
+    va_start(ap, vertex);
+    while (vertex != -1) {
+        CHECK(v != NULL);
+        CHECK(v->index == vertex);
+        v = (TestNode *)v->gcNextGraphNode;
+        vertex = va_arg(ap, int);
+    }
+    va_end(ap);
+
+    CHECK(v == NULL);
+    return true;
+}
+
+bool remaining(int vertex, ...)
+{
+    TestNode *v = (TestNode *)ComponentFinder::getAllRemaining(resultsList);
+
+    va_list ap;
+    va_start(ap, vertex);
+    while (vertex != -1) {
+        CHECK(v != NULL);
+        CHECK(v->index == vertex);
+        v = (TestNode *)v->gcNextGraphNode;
+        vertex = va_arg(ap, int);
+    }
+    va_end(ap);
+
+    CHECK(v == NULL);
+    return true;
+}
+
+bool end()
+{
+    CHECK(resultsList == NULL);
+
+    delete finder;
+    finder = NULL;
+    return true;
+}
+END_TEST(testFindSCCs)
+
+struct TestNode2 : public GraphNodeBase
+{
+    TestNode2 *edge;
+
+    TestNode2() :
+        edge(NULL)
+    {
+    }
+
+    void
+    findOutgoingEdges(ComponentFinder& finder)
+    {
+        if (edge)
+            finder.addEdgeTo(edge);
+    }
+};
+
+BEGIN_TEST(testFindSCCsStackLimit)
+{
+    /*
+     * Test what happens if recusion causes the stack to become full while
+     * traversing the graph.
+     *
+     * The test case is a large number of vertices, almost all of which are
+     * arranged in a linear chain.  The last few are left unlinked to exercise
+     * adding vertices after the stack full condition has already been detected.
+     *
+     * Such an arrangement with no cycles would normally result in one group for
+     * each vertex, but since the stack is exhasted in processing a single group
+     * is returned containing all the vertices.
+     */
+    const unsigned max = 1000000;
+
+    TestNode2 *vertices = new TestNode2[max]();
+    for (unsigned i = 0; i < (max - 10); ++i)
+        vertices[i].edge = &vertices[i + 1];
+
+    ComponentFinder finder(rt->nativeStackLimit);
+    for (unsigned i = 0; i < max; ++i)
+        finder.addNode(&vertices[i]);
+
+    GraphNodeBase *r = finder.getResultsList();
+    CHECK(r);
+    GraphNodeBase *v = finder.getNextGroup(r);
+    CHECK(v);
+
+    unsigned count = 0;
+    while (v) {
+        ++count;
+        v = v->gcNextGraphNode;
+    }
+    CHECK(count == max);
+    CHECK(finder.getNextGroup(r) == NULL);
+
+    delete [] vertices;
+    return true;
+}
+END_TEST(testFindSCCsStackLimit)
