2cdba3cfd5e93f7fc6c808df306eae5cc06ce7ba
Olli Pettay <Olli.Pettay@helsinki.fi>
2012-10-12 13:17:56 +0300

    Bug 799592  nsDOMWindowUtils::AllowScriptsToClose to bypass dom.allow_scripts_to_close_windows, r=jst

--- dom/base/nsDOMWindowUtils.cpp
+++ dom/base/nsDOMWindowUtils.cpp
@@ -2969,3 +2969,15 @@ nsDOMWindowUtils::GetIsHandlingUserInput(bool* aHandlingUserInput)
 
   return NS_OK;
 }
+
+NS_IMETHODIMP
+nsDOMWindowUtils::AllowScriptsToClose()
+{
+  if (!IsUniversalXPConnectCapable()) {
+    return NS_ERROR_DOM_SECURITY_ERR;
+  }
+  nsCOMPtr<nsPIDOMWindow> window = do_QueryReferent(mWindow);
+  NS_ENSURE_STATE(window);
+  static_cast<nsGlobalWindow*>(window.get())->AllowScriptsToClose();
+  return NS_OK;
+}
