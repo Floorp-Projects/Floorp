f7d95b72012951d4de18bb4b469ed6d904affcb5
Lucas Rocha <lucasr@mozilla.com>
2012-10-12 13:47:53 +0100

    Backout 110075:81be299f5004 - Big regression on tabs tray

--- mobile/android/base/GeckoViewsFactory.java
+++ mobile/android/base/GeckoViewsFactory.java
@@ -52,8 +52,6 @@ public final class GeckoViewsFactory implements LayoutInflater.Factory {
                 return new BrowserToolbarBackground(context, attrs);
             else if (TextUtils.equals(viewName, "FormAssistPopup"))
                 return new FormAssistPopup(context, attrs);
-            else if (TextUtils.equals(viewName, "GeckoApp$MainLayout"))
-                return new GeckoApp.MainLayout(context, attrs);
             else if (TextUtils.equals(viewName, "LinkTextView"))
                 return new LinkTextView(context, attrs);
             else if (TextUtils.equals(viewName, "FindInPageBar"))
