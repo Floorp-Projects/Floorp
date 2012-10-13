86123853396f736985ec047213f5975987e952ab
Jacek Szpot <maligree@gmail.com>
2012-10-13 17:52:10 +0200

    Bug 800106: Replace more NS_ALWAYS_INLINEs with MOZ_ALWAYS_INLINE; r=ehsan

--- media/libjpeg/config.h
+++ media/libjpeg/config.h
@@ -3,4 +3,5 @@
 #define PACKAGE_NAME "libjpeg-turbo"
 
 /* Need to use Mozilla-specific function inlining. */
-#define INLINE NS_ALWAYS_INLINE
+#include "mozilla/Attributes.h"
+#define INLINE MOZ_ALWAYS_INLINE
