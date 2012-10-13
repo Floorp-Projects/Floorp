f767de9ba4842f900c69429bf986650d5cc324f5
Jim Blandy <jimb@mozilla.com>
2012-10-13 16:04:41 -0700

    Bug 796073: Use Debugger.Object.prototype.makeDebuggeeValue where appropriate, not Debugger.prototype.addDebuggee. r=luke
    Many tests use Debugger.prototype.addDebuggee to produce Debugger.Object
    instances referring to random objects. Now we have
    Debugger.Object.prototype.makeDebuggeeValue to do that, which is more
    careful about wrappers. This patch changes all the existing tests that
    abuse addDebuggee in this way (well, they didn't have any choice when they
    were written) to use makeDebuggeeValue instead, in preparation for
    tightening up addDebuggee to only accept global objects.

--- js/src/jit-test/tests/debug/Debugger-findScripts-11.js
+++ js/src/jit-test/tests/debug/Debugger-findScripts-11.js
@@ -1,13 +1,14 @@
 // Debugger.prototype.findScripts can filter scripts by line number.
-var g = newGlobal('new-compartment');
-var dbg = new Debugger(g);
+var g = newGlobal();
+var dbg = new Debugger();
+var gw = dbg.addDebuggee(g);
 
 var scriptname = scriptdir + 'Debugger-findScripts-11-script2';
 g.load(scriptname);
 
-var gfw = dbg.addDebuggee(g.f);
-var ggw = dbg.addDebuggee(g.f());
-var ghw = dbg.addDebuggee(g.h);
+var gfw = gw.makeDebuggeeValue(g.f);
+var ggw = gw.makeDebuggeeValue(g.f());
+var ghw = gw.makeDebuggeeValue(g.h);
 
 // Specifying a line outside of all functions screens out all function scripts.
 assertEq(dbg.findScripts({url:scriptname, line:3}).indexOf(gfw.script) != -1, false);
