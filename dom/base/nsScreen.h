80c519028b36d1419e34d59db1c73ec501c0cacf
Ms2ger <ms2ger@gmail.com>
2012-10-14 09:39:34 +0200

    Bug 793244 - Part a: Rewrite mozLockOrientation to match WebIDL requirements; f=mounir r=bz

--- dom/base/nsScreen.h
+++ dom/base/nsScreen.h
@@ -5,14 +5,16 @@
 #ifndef nsScreen_h___
 #define nsScreen_h___
 
+#include "mozilla/Attributes.h"
+#include "mozilla/dom/BindingUtils.h"
 #include "mozilla/dom/ScreenOrientation.h"
+#include "mozilla/ErrorResult.h"
 #include "mozilla/Hal.h"
 #include "nsIDOMScreen.h"
 #include "nsISupports.h"
 #include "nsIScriptContext.h"
 #include "nsCOMPtr.h"
 #include "nsDOMEventTargetHelper.h"
-#include "mozilla/Attributes.h"
 
 class nsIDocShell;
 class nsDeviceContext;
@@ -23,6 +25,7 @@ class nsScreen : public nsDOMEventTargetHelper
                , public nsIDOMScreen
                , public mozilla::hal::ScreenConfigurationObserver
 {
+  typedef mozilla::ErrorResult ErrorResult;
 public:
   static already_AddRefed<nsScreen> Create(nsPIDOMWindow* aWindow);
 
@@ -31,6 +34,9 @@ public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIDOMSCREEN
   NS_FORWARD_NSIDOMEVENTTARGET(nsDOMEventTargetHelper::)
+
+  bool MozLockOrientation(const nsAString& aOrientation, ErrorResult& aRv);
+  bool MozLockOrientation(const mozilla::dom::Sequence<nsString>& aOrientations, ErrorResult& aRv);
 
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(nsScreen,
                                            nsDOMEventTargetHelper)
