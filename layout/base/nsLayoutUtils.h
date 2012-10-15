d7b18a660065ef793d5737745b1e24f400c03b64
Scott Johnson <sjohnson@mozilla.com>
2012-10-15 14:03:37 -0500

    Bug 784375: Add a preference to control maximum font size inflation ratio. [r=dbaron]

--- layout/base/nsLayoutUtils.h
+++ layout/base/nsLayoutUtils.h
@@ -1612,6 +1612,14 @@ public:
   static bool FontSizeInflationEnabled(nsPresContext *aPresContext);
 
   /**
+   * See comment above "font.size.inflation.maxRatio" in
+   * modules/libpref/src/init/all.js .
+   */
+  static uint32_t FontSizeInflationMaxRatio() {
+    return sFontSizeInflationMaxRatio;
+  }
+
+  /**
    * See comment above "font.size.inflation.emPerLine" in
    * modules/libpref/src/init/all.js .
    */
@@ -1751,7 +1759,8 @@ private:
   static uint32_t sFontSizeInflationEmPerLine;
   static uint32_t sFontSizeInflationMinTwips;
   static uint32_t sFontSizeInflationLineThreshold;
-  static int32_t sFontSizeInflationMappingIntercept;
+  static int32_t  sFontSizeInflationMappingIntercept;
+  static uint32_t sFontSizeInflationMaxRatio;
 };
 
 template<typename PointType, typename RectType, typename CoordType>
