2409f6ad32853508a62faa682d7c2813f20aee47
Nicholas Nethercote <nnethercote@mozilla.com>
2012-10-17 15:54:25 -0700

    Bug 744686 - Comment out unused |alpha_bits| variable to fix a compile warning.  r=jmuizelaar.
    --HG--
    extra : rebase_source : 48be31fe3eab889e6f7930f44b96140ba076c389

--- image/decoders/nsPNGDecoder.cpp
+++ image/decoders/nsPNGDecoder.cpp
@@ -583,6 +583,8 @@ nsPNGDecoder::info_callback(png_structp png_ptr, png_infop info_ptr)
   /* copy PNG info into imagelib structs (formerly png_set_dims()) */
   /*---------------------------------------------------------------*/
 
+  // This code is currently unused, but it will be needed for bug 517713.
+#if 0
   int32_t alpha_bits = 1;
 
   if (channels == 2 || channels == 4) {
@@ -601,6 +603,7 @@ nsPNGDecoder::info_callback(png_structp png_ptr, png_infop info_ptr)
       alpha_bits = 8;
     }
   }
+#endif
 
   if (channels == 1 || channels == 3)
     decoder->format = gfxASurface::ImageFormatRGB24;
