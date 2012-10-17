eba2e6d7ce6e9ba67330ebaea56c31dc45c3135a
Jed Parsons <jparsons@mozilla.com>
2012-10-16 21:34:02 -0400

    Bug 794680 - Connect gecko to b2g identity ui. r=benadida

--- dom/identity/nsDOMIdentity.js
+++ dom/identity/nsDOMIdentity.js
@@ -17,6 +17,10 @@ const MAX_RP_CALLS = 100;
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
+XPCOMUtils.defineLazyServiceGetter(this, "cpmm",
+                                   "@mozilla.org/childprocessmessagemanager;1",
+                                   "nsIMessageSender");
+
 // This is the child process corresponding to nsIDOMIdentity.
 
 
@@ -483,10 +487,12 @@ nsDOMIdentityInternal.prototype = {
 
     this._log("init was called from " + aWindow.document.location);
 
-    this._mm = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+    /*this._mm = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
                       .getInterface(Ci.nsIWebNavigation)
                       .QueryInterface(Ci.nsIInterfaceRequestor)
                       .getInterface(Ci.nsIContentFrameMessageManager);
+    */
+    this._mm = cpmm;
 
     // Setup listeners for messages from parent process.
     this._messages = [
