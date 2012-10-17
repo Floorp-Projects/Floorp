fdbdad837f04032700f192b43befc5c12b0bd61a
Mike Hommey <mh+mozilla@glandium.org>
2012-10-17 16:29:28 +0200

    Bug 799090 - Update jemalloc3 to commit d0ffd8e. r=jlebar

--- memory/jemalloc/src/src/chunk.c
+++ memory/jemalloc/src/src/chunk.c
@@ -43,6 +43,7 @@ chunk_recycle(size_t size, size_t alignment, bool base, bool *zero)
 	extent_node_t *node;
 	extent_node_t key;
 	size_t alloc_size, leadsize, trailsize;
+	bool zeroed;
 
 	if (base) {
 		/*
@@ -107,17 +108,18 @@ chunk_recycle(size_t size, size_t alignment, bool base, bool *zero)
 	}
 	malloc_mutex_unlock(&chunks_mtx);
 
-	if (node != NULL)
+	zeroed = false;
+	if (node != NULL) {
+		if (node->zeroed) {
+			zeroed = true;
+			*zero = true;
+		}
 		base_node_dealloc(node);
-#ifdef JEMALLOC_PURGE_MADVISE_DONTNEED
-	/* Pages are zeroed as a side effect of pages_purge(). */
-	*zero = true;
-#else
-	if (*zero) {
+	}
+	if (zeroed == false && *zero) {
 		VALGRIND_MAKE_MEM_UNDEFINED(ret, size);
 		memset(ret, 0, size);
 	}
-#endif
 	return (ret);
 }
 
@@ -191,9 +193,10 @@ label_return:
 static void
 chunk_record(void *chunk, size_t size)
 {
+	bool unzeroed;
 	extent_node_t *xnode, *node, *prev, key;
 
-	pages_purge(chunk, size);
+	unzeroed = pages_purge(chunk, size);
 
 	/*
 	 * Allocate a node before acquiring chunks_mtx even though it might not
@@ -216,6 +219,7 @@ chunk_record(void *chunk, size_t size)
 		extent_tree_szad_remove(&chunks_szad, node);
 		node->addr = chunk;
 		node->size += size;
+		node->zeroed = (node->zeroed && (unzeroed == false));
 		extent_tree_szad_insert(&chunks_szad, node);
 		if (xnode != NULL)
 			base_node_dealloc(xnode);
@@ -234,6 +238,7 @@ chunk_record(void *chunk, size_t size)
 		node = xnode;
 		node->addr = chunk;
 		node->size = size;
+		node->zeroed = (unzeroed == false);
 		extent_tree_ad_insert(&chunks_ad, node);
 		extent_tree_szad_insert(&chunks_szad, node);
 	}
@@ -253,6 +258,7 @@ chunk_record(void *chunk, size_t size)
 		extent_tree_szad_remove(&chunks_szad, node);
 		node->addr = prev->addr;
 		node->size += prev->size;
+		node->zeroed = (node->zeroed && prev->zeroed);
 		extent_tree_szad_insert(&chunks_szad, node);
 
 		base_node_dealloc(prev);
@@ -311,4 +317,34 @@ chunk_boot(void)
 	}
 
 	return (false);
+}
+
+void
+chunk_prefork(void)
+{
+
+	malloc_mutex_lock(&chunks_mtx);
+	if (config_ivsalloc)
+		rtree_prefork(chunks_rtree);
+	chunk_dss_prefork();
+}
+
+void
+chunk_postfork_parent(void)
+{
+
+	chunk_dss_postfork_parent();
+	if (config_ivsalloc)
+		rtree_postfork_parent(chunks_rtree);
+	malloc_mutex_postfork_parent(&chunks_mtx);
+}
+
+void
+chunk_postfork_child(void)
+{
+
+	chunk_dss_postfork_child();
+	if (config_ivsalloc)
+		rtree_postfork_child(chunks_rtree);
+	malloc_mutex_postfork_child(&chunks_mtx);
 }
