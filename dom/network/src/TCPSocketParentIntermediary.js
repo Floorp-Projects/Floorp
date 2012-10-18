7247280a55948e60a90454dc47956a43e664329b
Donovan Preston <dpreston@mozilla.com>
2012-10-17 22:29:58 -0400

    Bug 784893 - Rename the event's .socket attribute to .target; remove "on" prefix from event names. r=jonas

--- dom/network/src/TCPSocketParentIntermediary.js
+++ dom/network/src/TCPSocketParentIntermediary.js
@@ -26,9 +26,9 @@ TCPSocketParentIntermediary.prototype = {
 
     // Create handlers for every possible callback that attempt to trigger
     // corresponding callbacks on the child object.
-    ["onopen", "ondrain", "ondata", "onerror", "onclose"].forEach(
+    ["open", "drain", "data", "error", "close"].forEach(
       function(p) {
-        socket[p] = function(data) {
+        socket["on" + p] = function(data) {
           aParentSide.sendCallback(p, data.data, socket.readyState,
                                    socket.bufferedAmount);
         };
