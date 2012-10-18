4585798f8e8fb5db9b50f6d97c384aad3c03f3a9
Anton Kovalyov <anton@mozilla.com>
2012-10-18 11:00:33 -0700

    Bug 801982 - Scratchpad doesn't show confirmation dialog after opening a file; r=harth

--- browser/devtools/scratchpad/scratchpad.js
+++ browser/devtools/scratchpad/scratchpad.js
@@ -676,8 +676,6 @@ var Scratchpad = {
         }
 
         if (shouldOpen) {
-          this._skipClosePrompt = true;
-
           let file;
           if (aFile) {
             file = aFile;
@@ -1313,25 +1311,14 @@ var Scratchpad = {
    * there are unsaved changes.
    *
    * @param nsIDOMEvent aEvent
+   * @param function aCallback
+   *        Optional function you want to call when file is saved/closed.
+   *        Used mainly for tests.
    */
-  onClose: function SP_onClose(aEvent)
+  onClose: function SP_onClose(aEvent, aCallback)
   {
-    if (this._skipClosePrompt) {
-      return;
-    }
-
-    this.promptSave(function(aShouldClose, aSaved, aStatus) {
-      let shouldClose = aShouldClose;
-      if (aSaved && !Components.isSuccessCode(aStatus)) {
-        shouldClose = false;
-      }
-
-      if (shouldClose) {
-        this._skipClosePrompt = true;
-        window.close();
-      }
-    }.bind(this));
     aEvent.preventDefault();
+    this.close(aCallback);
   },
 
   /**
@@ -1350,7 +1337,6 @@ var Scratchpad = {
       }
 
       if (shouldClose) {
-        this._skipClosePrompt = true;
         window.close();
       }
       if (aCallback) {
