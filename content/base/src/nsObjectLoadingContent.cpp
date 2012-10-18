5013eab7b6b6df6a111734b32f6ca506f504c15d
John Schoenick <jschoenick@mozilla.com>
2012-10-18 11:49:45 -0700

    Bug 802355 - Handle failures in OnStartRequest when spawning plugins. r=josh

--- content/base/src/nsObjectLoadingContent.cpp
+++ content/base/src/nsObjectLoadingContent.cpp
@@ -1869,8 +1869,20 @@ nsObjectLoadingContent::LoadObject(bool aNotify,
     // (this is otherwise unset by the stack class)
     mIsLoading = false;
     mFinalListener = finalListener;
-    finalListener->OnStartRequest(mChannel, nullptr);
+    rv = finalListener->OnStartRequest(mChannel, nullptr);
     mSrcStreamLoading = false;
+    if (NS_FAILED(rv)) {
+      // Failed to load new content, but since we've already notified of our
+      // transition, we can just Unload and call LoadFallback (which will notify
+      // again)
+      mType = eType_Null;
+      // This could *also* technically re-enter if OnStartRequest fails after
+      // spawning a plugin.
+      mIsLoading = true;
+      UnloadObject(false);
+      NS_ENSURE_TRUE(mIsLoading, NS_OK);
+      LoadFallback(fallbackType, true);
+    }
   }
 
   return NS_OK;
