806ad6ccb067ac6d15b677c5da15d29266dfab75
Robert Strong <robert.bugzilla@gmail.com>
2012-10-19 12:42:28 -0700

    Bug 803181 - Change update background download interval from 10 minutes to 1 minute and update check interval from 24 hours to 12 hours. r=jaws

--- browser/branding/aurora/pref/firefox-branding.js
+++ browser/branding/aurora/pref/firefox-branding.js
@@ -6,8 +6,7 @@
 pref("startup.homepage_override_url","");
 pref("startup.homepage_welcome_url","");
 // The time interval between checks for a new version (in seconds)
-// nightly=8 hours, official=24 hours
-pref("app.update.interval", 28800);
+pref("app.update.interval", 28800); // 8 hours
 // The time interval between the downloading of mar file chunks in the
 // background (in seconds)
 pref("app.update.download.backgroundInterval", 60);
