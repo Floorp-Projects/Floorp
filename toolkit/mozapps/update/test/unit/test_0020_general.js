58b1fede0ed6c1a07e83d36a29a088643e0323c6
Robert Strong <robert.bugzilla@gmail.com>
2012-10-19 12:43:15 -0700

    Test changes for Bug 774618 - Partner builds don't download complete update if partial fails, or show workaround information if complete fails. r=bbondy

--- toolkit/mozapps/update/test/unit/test_0020_general.js
+++ toolkit/mozapps/update/test/unit/test_0020_general.js
@@ -13,7 +13,7 @@ function run_test() {
   do_register_cleanup(end_test);
   removeUpdateDirsAndFiles();
   setUpdateURLOverride();
-  setUpdateChannel();
+  setUpdateChannel("test_channel");
   // The mock XMLHttpRequest is MUCH faster
   overrideXHR(callHandleEvent);
   standardInit();
