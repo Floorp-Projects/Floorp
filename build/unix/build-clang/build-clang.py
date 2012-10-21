a06ac3b6c5403ee3191360aac84994865a854672
Rafael Ávila de Espíndola <respindola@mozilla.com>
2012-10-20 20:50:10 -0400

    Bug 799572 - clang update broke NSS. r=rail.

--- build/unix/build-clang/build-clang.py
+++ build/unix/build-clang/build-clang.py
@@ -3,7 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-llvm_revision = "164411"
+llvm_revision = "166095"
 moz_version = "moz0"
 
 ##############################################
