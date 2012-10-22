b911750d2ed0ffcb65d6007ec618cd5871c067a1
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2012-10-22 12:42:53 +0100

    Bug 409624 - Sanitizer should clear findBar data. r=adw

--- browser/base/content/test/browser_bug567306.js
+++ browser/base/content/test/browser_bug567306.js
@@ -3,38 +3,45 @@
  */
 
 let Ci = Components.interfaces;
+let testWindow = null;
 
 function test() {
   waitForExplicitFinish();
-  let tab = gBrowser.addTab();
-  gBrowser.selectedTab = tab;
-  tab.linkedBrowser.addEventListener("load", function(aEvent) {
-    tab.linkedBrowser.removeEventListener("load", arguments.callee, true);
 
+  testWindow = OpenBrowserWindow();
+  testWindow.addEventListener("load", function(aEvent) {
+    testWindow.removeEventListener("load", arguments.callee, false);
     ok(true, "Load listener called");
-    waitForFocus(onFocus, content);
-  }, true);
 
-  content.location = "data:text/html,<h1 id='h1'>Select Me</h1>";
+    executeSoon(function() {
+      let selectedBrowser = testWindow.gBrowser.selectedBrowser;
+      selectedBrowser.addEventListener("pageshow", function() {
+        selectedBrowser.removeEventListener("pageshow", arguments.callee, true);
+        ok(true, "pageshow listener called");
+        waitForFocus(onFocus, testWindow.contentWindow);
+      }, true);
+      testWindow.content.location = "data:text/html,<h1 id='h1'>Select Me</h1>";
+    });
+  }, false);
 }
 
 function selectText() {
-  let elt = content.document.getElementById("h1");
-  let selection = content.getSelection();
-  let range = content.document.createRange();
+  let elt = testWindow.content.document.getElementById("h1");
+  let selection = testWindow.content.getSelection();
+  let range = testWindow.content.document.createRange();
   range.setStart(elt, 0);
   range.setEnd(elt, 1);
   selection.removeAllRanges();
   selection.addRange(range);
 }
 
-
 function onFocus() {
-  ok(!gFindBarInitialized, "find bar is not yet initialized");
+  ok(!testWindow.gFindBarInitialized, "find bar is not yet initialized");
+  let findBar = testWindow.gFindBar;
   selectText();
-  gFindBar.onFindCommand();
-  ok(gFindBar._findField.value == "Select Me", "Findbar is initialized with selection");
-  gFindBar.close();
-  gBrowser.removeCurrentTab();
+  findBar.onFindCommand();
+  is(findBar._findField.value, "Select Me", "Findbar is initialized with selection");
+  findBar.close();
+  testWindow.close();
   finish();
 }
