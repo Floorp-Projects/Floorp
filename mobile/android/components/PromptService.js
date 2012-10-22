21dc6b90ef8c04d6dd50f1694ca689a2b4e8850e
Wes Johnston <wjohnston@mozilla.com>
2012-10-22 16:20:01 -0700

    Bug 789888 - Catch errors thrown using document objects that have been destroyed. r=mfinkle

--- mobile/android/components/PromptService.js
+++ mobile/android/components/PromptService.js
@@ -755,9 +755,15 @@ let PromptUtils = {
   },
 
   fireDialogEvent: function(aDomWin, aEventName) {
-    let event = aDomWin.document.createEvent("Events");
-    event.initEvent(aEventName, true, true);
-    aDomWin.dispatchEvent(event);
+    // accessing the document object can throw if this window no longer exists. See bug 789888.
+    try {
+      if (!aDomWin.document)
+        return;
+      let event = aDomWin.document.createEvent("Events");
+      event.initEvent(aEventName, true, true);
+      aDomWin.dispatchEvent(event);
+    } catch(ex) {
+    }
   }
 };
 
