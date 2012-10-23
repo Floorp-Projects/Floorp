b6965303947678c3560dc2217557785af325d60f
Magnus Melin <mkmelin+mozilla@iki.fi>
2012-10-23 20:53:15 +0300

    Bug 804664 - remove typedef nsAutoCString nsCAutoString. r=benjamin

--- xpcom/glue/nsStringAPI.h
+++ xpcom/glue/nsStringAPI.h
@@ -1451,7 +1451,4 @@ typedef nsString nsAutoString;
 NS_HIDDEN_(bool) ParseString(const nsACString& aAstring, char aDelimiter, 
                                nsTArray<nsCString>& aArray);
 
-/* for back compatibility for pre-name-change code */
-typedef nsAutoCString nsCAutoString;
-
 #endif // nsStringAPI_h__
