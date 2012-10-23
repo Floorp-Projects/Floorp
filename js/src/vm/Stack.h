1b2a480e6186c93daebe82ddd19b8291c8db4d97
Brian Hackett <bhackett1024@gmail.com>
2012-10-23 09:20:56 -0700

    Keep the interpreter stack synced for GC scanning, bug 781657. r=billm

--- js/src/vm/Stack.h
+++ js/src/vm/Stack.h
@@ -1382,7 +1382,7 @@ class StackSpace
         return (Value *)fp >= base_ && (Value *)fp <= trustedEnd_;
     }
 
-    void markAndClobberFrame(JSTracer *trc, StackFrame *fp, Value *slotsEnd, jsbytecode *pc);
+    void markFrame(JSTracer *trc, StackFrame *fp, Value *slotsEnd);
 
   public:
     StackSpace();
@@ -1435,7 +1435,7 @@ class StackSpace
     bool tryBumpLimit(JSContext *cx, Value *from, unsigned nvals, Value **limit);
 
     /* Called during GC: mark segments, frames, and slots under firstUnused. */
-    void markAndClobber(JSTracer *trc);
+    void mark(JSTracer *trc);
 
     /* Called during GC: sets active flag on compartments with active frames. */
     void markActiveCompartments();
