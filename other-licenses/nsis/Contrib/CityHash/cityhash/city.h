5ed3bb3dbe0fc0f7eaa56402f18629f3ac8a9f1b
Brian R. Bondy <netzen@gmail.com>
2012-10-24 11:45:15 -0400

    Bug 734628 - Fix for city hash NSIS pluign src for not returning padded spaces before a hash. r=jimm

--- other-licenses/nsis/Contrib/CityHash/cityhash/city.h
+++ other-licenses/nsis/Contrib/CityHash/cityhash/city.h
@@ -46,12 +46,11 @@
 #include "../CityHash.h" // added by moz, specific to nsis project
 
 #include <stdlib.h>  // for size_t.
-#include "stdint.h"
 #include <utility>
 
-typedef uint8_t uint8;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
+typedef unsigned __int8 uint8;
+typedef unsigned __int32 uint32;
+typedef unsigned __int64 uint64;
 typedef std::pair<uint64, uint64> uint128;
 
 inline uint64 Uint128Low64(const uint128& x) { return x.first; }
@@ -80,7 +79,7 @@ uint128 CityHash128WithSeed(const char *s, size_t len, uint128 seed);
 // This is intended to be a reasonably good hash function.
 inline uint64 Hash128to64(const uint128& x) {
   // Murmur-inspired hashing.
-  const uint64 kMul = 0x9ddfea08eb382d69ULL;
+  const uint64 kMul = 0x9ddfea08eb382d69;
   uint64 a = (Uint128Low64(x) ^ Uint128High64(x)) * kMul;
   a ^= (a >> 47);
   uint64 b = (Uint128High64(x) ^ a) * kMul;
