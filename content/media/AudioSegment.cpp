680d072ebbd14e4fada9146aac94c63b2024a5b8
Robert O'Callahan <robert@ocallahan.org>
2012-10-25 23:09:40 +1300

    Bug 805254. Part 8: Consolidate audio sample processing code using templates over the format types. r=kinetik
    Replace nsAudioStream::Format with an AUDIO_OUTPUT_FORMAT enum value so we
    can use it as a template parameter.
    
    Introduce AudioSampleTraits<AudioSampleFormat> to give us access to the C++ type
    corresponding to an enum value.
    
    Move SampleToFloat/FloatToSample to AudioSampleFormat.h.
    
    Introduce ConvertAudioSamples and ConvertAudioSamplesWithScale functions
    and use them from various places.
    
    Moves AudioDataValue to AudioSampleFormat.h. The name isn't great, but it'll do.

--- content/media/AudioSegment.cpp
+++ content/media/AudioSegment.cpp
@@ -9,36 +9,6 @@
 
 namespace mozilla {
 
-/*
- * Use "2^N" conversion since it's simple, fast, "bit transparent", used by
- * many other libraries and apparently behaves reasonably.
- * http://blog.bjornroche.com/2009/12/int-float-int-its-jungle-out-there.html
- * http://blog.bjornroche.com/2009/12/linearity-and-dynamic-range-in-int.html
- */
-static float
-SampleToFloat(float aValue)
-{
-  return aValue;
-}
-static float
-SampleToFloat(int16_t aValue)
-{
-  return aValue/32768.0f;
-}
-
-static void
-FloatToSample(float aValue, float* aOut)
-{
-  *aOut = aValue;
-}
-static void
-FloatToSample(float aValue, int16_t* aOut)
-{
-  float v = aValue*32768.0f;
-  float clamped = NS_MAX(-32768.0f, NS_MIN(32767.0f, v));
-  *aOut = int16_t(clamped);
-}
-
 template <class SrcT, class DestT>
 static void
 InterleaveAndConvertBuffer(const SrcT* aSource, int32_t aSourceLength,
@@ -50,8 +20,8 @@ InterleaveAndConvertBuffer(const SrcT* aSource, int32_t aSourceLength,
   DestT* output = aOutput;
   for (int32_t i = 0; i < aLength; ++i) {
     for (int32_t channel = 0; channel < aChannels; ++channel) {
-      float v = SampleToFloat(aSource[channel*aSourceLength + i])*aVolume;
-      FloatToSample(v, output);
+      float v = AudioSampleToFloat(aSource[channel*aSourceLength + i])*aVolume;
+      *output = FloatToAudioSample<DestT>(v);
       ++output;
     }
   }
@@ -137,7 +107,7 @@ AudioSegment::WriteTo(nsAudioStream* aOutput)
 {
   NS_ASSERTION(mChannels == aOutput->GetChannels(), "Wrong number of channels");
   nsAutoTArray<uint8_t,STATIC_AUDIO_BUFFER_BYTES> buf;
-  uint32_t frameSize = GetSampleSize(nsAudioStream::Format())*mChannels;
+  uint32_t frameSize = GetSampleSize(AUDIO_OUTPUT_FORMAT)*mChannels;
   for (ChunkIterator ci(*this); !ci.IsEnded(); ci.Next()) {
     AudioChunk& c = *ci;
     if (frameSize*c.mDuration > UINT32_MAX) {
@@ -150,7 +120,7 @@ AudioSegment::WriteTo(nsAudioStream* aOutput)
                                  c.mOffset, int32_t(c.mDuration),
                                  c.mVolume,
                                  aOutput->GetChannels(),
-                                 buf.Elements(), nsAudioStream::Format());
+                                 buf.Elements(), AUDIO_OUTPUT_FORMAT);
     } else {
       // Assumes that a bit pattern of zeroes == 0.0f
       memset(buf.Elements(), 0, buf.Length());
