c9e3f28a2eb243419854185dd10d2bfb06bc0564
Jared Wein <jwein@mozilla.com>
2012-10-24 20:48:01 -0700

    Bug 805206 - Keyboard accessible menuitem has a blank label. r=felipe

--- browser/base/content/browser-social.js
+++ browser/base/content/browser-social.js
@@ -98,6 +98,11 @@ let SocialUI = {
     toggleCommand.setAttribute("label", label);
     toggleCommand.setAttribute("accesskey", accesskey);
 
+    let kbMenuitem = document.getElementById("menu_socialAmbientMenu");
+    kbMenuitem.hidden = !Social.enabled;
+    kbMenuitem.setAttribute("label", label);
+    kbMenuitem.setAttribute("accesskey", accesskey);
+
     SocialToolbar.init();
     SocialShareButton.init();
     SocialSidebar.init();
@@ -608,6 +613,7 @@ var SocialMenu = {
     let ambientMenuItems = submenu.getElementsByClassName("ambient-menuitem");
     for (let ambientMenuItem of ambientMenuItems)
       submenu.removeChild(ambientMenuItem);
+    separator.hidden = true;
     let provider = Social.provider;
     if (Social.active && provider) {
       let iconNames = Object.keys(provider.ambientNotificationIcons);
@@ -616,6 +622,7 @@ var SocialMenu = {
         let icon = provider.ambientNotificationIcons[name];
         if (!icon.label || !icon.menuURL)
           continue;
+        separator.hidden = false;
         let menuitem = document.createElement("menuitem");
         menuitem.setAttribute("label", icon.label);
         menuitem.classList.add("ambient-menuitem");
@@ -624,9 +631,7 @@ var SocialMenu = {
         }, false);
         submenu.insertBefore(menuitem, separator);
       }
-      separator.hidden = !iconNames.length;
     }
-    document.getElementById("menu_socialAmbientMenu").hidden = !Social.enabled;
   }
 };
 
