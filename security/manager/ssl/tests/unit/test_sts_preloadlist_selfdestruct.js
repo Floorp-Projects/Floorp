e00053f7e2adf0a33bc089b49521d1609d1f1991
David Keeler <dkeeler@mozilla.com>
2012-10-26 09:50:23 -0700

    bug 800444 - disable HSTS preload list if firefox has not updated in 18 weeks r=bsmith, mayhemer

---
+++ security/manager/ssl/tests/unit/test_sts_preloadlist_selfdestruct.js
@@ -0,0 +1,24 @@
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+var Cu = Components.utils;
+
+Cu.import("resource://gre/modules/Services.jsm");
+
+function run_test() {
+  let STSService = Cc["@mozilla.org/stsservice;1"]
+                     .getService(Ci.nsIStrictTransportSecurityService);
+
+  // check that a host on the preload list is identified as an sts host
+  do_check_true(STSService.isStsHost("alpha.irccloud.com"));
+
+  // now simulate that it's 19 weeks later than it actually is
+  let offsetSeconds = 19 * 7 * 24 * 60 * 60;
+  Services.prefs.setIntPref("test.currentTimeOffsetSeconds", offsetSeconds);
+
+  // check that the preloaded host is no longer considered sts
+  do_check_false(STSService.isStsHost("alpha.irccloud.com"));
+
+  // just make sure we can get everything back to normal
+  Services.prefs.clearUserPref("test.currentTimeOffsetSeconds");
+  do_check_true(STSService.isStsHost("alpha.irccloud.com"));
+}
