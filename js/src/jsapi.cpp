9d53f808b3cdae71dd3bf648daa44e59ef965322
Bill McCloskey <wmccloskey@mozilla.com>
2012-10-26 11:53:20 -0700

    Bug 803907 - Remove some GC debugging functions (r=terrence)

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -2452,18 +2452,6 @@ JS_AnchorPtr(void *p)
 {
 }
 
-#ifdef DEBUG
-
-JS_PUBLIC_API(void)
-JS_DumpNamedRoots(JSRuntime *rt,
-                  void (*dump)(const char *name, void *rp, JSGCRootType type, void *data),
-                  void *data)
-{
-    js_DumpNamedRoots(rt, dump, data);
-}
-
-#endif /* DEBUG */
-
 JS_PUBLIC_API(JSBool)
 JS_LockGCThing(JSContext *cx, void *thing)
 {
