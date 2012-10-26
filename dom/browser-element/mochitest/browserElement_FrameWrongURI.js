66d6229c8f68ba112c5812e9d41bdf747f3c4e4f
Andrea Marchesini <amarchesini@mozilla.com>
2012-10-26 10:46:27 +0200

    Bug 804446 - Disable the javascript: protocol when it is entered in the location bar of browser in Firefox OS, r=bz

---
+++ dom/browser-element/mochitest/browserElement_FrameWrongURI.js
@@ -0,0 +1,53 @@
+/* Any copyright is dedicated to the public domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Bug 804446 - Test that window.open(javascript:..) works with <iframe mozbrowser>.
+
+"use strict";
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addPermission();
+
+  var iframeJS = document.createElement('iframe');
+  iframeJS.mozbrowser = true;
+
+  iframeJS.addEventListener('mozbrowserloadstart', function(e) {
+    ok(false, "This should not happen!");
+  });
+
+  iframeJS.addEventListener('mozbrowserloadend', function(e) {
+    ok(false, "This should not happen!");
+  });
+
+  iframeJS.src = 'javascript:alert("Foo");';
+  document.body.appendChild(iframeJS);
+
+  var iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+
+  var gotPopup = false;
+  iframe.addEventListener('mozbrowseropenwindow', function(e) {
+    is(gotPopup, false, 'Should get just one popup.');
+    gotPopup = true;
+
+    document.body.appendChild(e.detail.frameElement);
+  });
+
+  iframe.addEventListener('mozbrowsershowmodalprompt', function(e) {
+    ok(gotPopup, 'Got mozbrowseropenwindow event before showmodalprompt event.');
+    if (e.detail.message.indexOf("success") == 0) {
+      ok(true, e.detail.message);
+      SimpleTest.finish();
+    }
+    else {
+      ok(false, "Got invalid message: " + e.detail.message);
+    }
+  });
+
+  iframe.src = 'file_browserElement_FrameWrongURI.html';
+  document.body.appendChild(iframe);
+}
+
+runTest();
