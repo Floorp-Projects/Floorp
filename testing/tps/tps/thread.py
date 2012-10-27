8974eafb698562e8e3abfbc1ed522f8d4f624a5f
Sam Garrett <samdgarrett@gmail.com>
2012-10-27 17:42:25 -0400

    Bug 771554 - Removing autolog and having tps write-out results to JSON so that coversheet can handle them, r=jgriffin, DONTBUILD(NPOTB)

--- testing/tps/tps/thread.py
+++ testing/tps/tps/thread.py
@@ -8,32 +8,27 @@ from testrunner import TPSTestRunner
 
 class TPSTestThread(Thread):
 
-  def __init__(self, extensionDir, builddata=None, emailresults=False,
-               testfile=None, logfile=None, rlock=None, config=None,
-               autolog=False):
+  def __init__(self, extensionDir, builddata=None,
+               testfile=None, logfile=None, rlock=None, config=None):
     assert(builddata)
     assert(config)
     self.extensionDir = extensionDir
     self.builddata = builddata
-    self.emailresults = emailresults
     self.testfile = testfile
     self.logfile = logfile
     self.rlock = rlock
     self.config = config
-    self.autolog = autolog
     Thread.__init__(self)
 
   def run(self):
     # run the tests in normal mode ...
     TPS = TPSTestRunner(self.extensionDir,
-                        emailresults=self.emailresults,
                         testfile=self.testfile,
                         logfile=self.logfile,
                         binary=self.builddata['buildurl'],
                         config=self.config,
                         rlock=self.rlock,
-                        mobile=False,
-                        autolog=self.autolog)
+                        mobile=False)
     TPS.run_tests()
 
     # Get the binary used by this TPS instance, and use it in subsequent
@@ -42,14 +37,12 @@ class TPSTestThread(Thread):
 
     # ... and then again in mobile mode
     TPS_mobile = TPSTestRunner(self.extensionDir,
-                               emailresults=self.emailresults,
                                testfile=self.testfile,
                                logfile=self.logfile,
                                binary=binary,
                                config=self.config,
                                rlock=self.rlock,
-                               mobile=True,
-                               autolog=self.autolog)
+                               mobile=True)
     TPS_mobile.run_tests()
 
     # ... and again via the staging server, if credentials are present
@@ -62,12 +55,10 @@ class TPSTestThread(Thread):
         stageconfig = self.config.copy()
         stageconfig['account'] = stageaccount.copy()
         TPS_stage = TPSTestRunner(self.extensionDir,
-                                  emailresults=self.emailresults,
                                   testfile=self.testfile,
                                   logfile=self.logfile,
                                   binary=binary,
                                   config=stageconfig,
                                   rlock=self.rlock,
-                                  mobile=False,
-                                  autolog=self.autolog)
+                                  mobile=False)#, autolog=self.autolog)
         TPS_stage.run_tests()
