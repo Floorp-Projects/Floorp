69cbdc869efb2cb6efdc5785d07da513773dc382
Jan de Mooij <jdemooij@mozilla.com>
2012-10-29 19:00:53 +0100

    Bug 800179 - Initialize CallArgsList::active_ in the constructor. r=luke

---
+++ js/src/jit-test/tests/ion/bug800179.js
@@ -0,0 +1,23 @@
+// |jit-test| error: is not iterable
+try {
+    x = []
+    y = function() {}
+    t = Uint8ClampedArray
+    Object.defineProperty(x, 1, {
+        get: (function() {
+            for (v of t) {}
+        })
+    })
+    Object.defineProperty(x, 8, {
+        configurable: t
+    }).reverse()
+} catch (e) {}
+Object.defineProperty([], 1, {
+    configurable: true,
+    get: (function() {
+        for (j = 0; j < 50; ++j) {
+            y()
+        }
+    })
+}).pop()
+x.map(y)
