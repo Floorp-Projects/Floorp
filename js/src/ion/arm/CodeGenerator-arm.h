5e6c57110185c1703ec93694e94cc98fb60d7312
Tony Young <tony@rfw.name>
2012-10-30 14:59:06 -0700

    Bug 806643 - Negate doubles by flipping signed bit. r=sstangl

--- js/src/ion/arm/CodeGenerator-arm.h
+++ js/src/ion/arm/CodeGenerator-arm.h
@@ -69,6 +69,7 @@ class CodeGeneratorARM : public CodeGeneratorShared
   public:
     // Instruction visitors.
     virtual bool visitMinMaxD(LMinMaxD *ins);
+    virtual bool visitNegD(LNegD *ins);
     virtual bool visitAbsD(LAbsD *ins);
     virtual bool visitSqrtD(LSqrtD *ins);
     virtual bool visitAddI(LAddI *ins);
