a1f264f275d9532288b324b166a4df508ad4713f
Kannan Vijayan <kvijayan@mozilla.com>
2012-10-30 11:53:12 -0400

    Bug 806568 - Move Baseline IC code into its own file. (r=jandem)

--- js/src/ion/BaselineJIT.h
+++ js/src/ion/BaselineJIT.h
@@ -16,127 +16,7 @@
 namespace js {
 namespace ion {
 
-// Since BaseCache has virtual methods, it can't be stored in a Vector.
-// As a workaround, use a POD base class to hold all the data.
-struct CacheData
-{
-    jsbytecode *pc;
-    uint32_t data;
-    CodeLocationCall call;
-
-    void updateBaseAddress(IonCode *code, MacroAssembler &masm) {
-        call.repoint(code, &masm);
-    }
-};
-
-class BaselineScript;
-
-class BaseCache
-{
-  public:
-    CacheData &data;
-
-    explicit BaseCache(CacheData &data)
-      : data(data)
-    { }
-
-    explicit BaseCache(CacheData &data, jsbytecode *pc)
-      : data(data)
-    {
-        data.pc = pc;
-    }
-
-  protected:
-    virtual uint32_t getKey() const = 0;
-    virtual IonCode *generate(JSContext *cx) = 0;
-
-  public:
-    IonCode *getCode(JSContext *cx) {
-        uint32_t key = getKey();
-        (void)key; //XXX
-
-        // XXX if lookup failure..
-        return generate(cx);
-    }
-};
-
-class BinaryOpCache : public BaseCache
-{
-    enum State {
-        Uninitialized = 0,
-        Int32
-    };
-
-  public:
-    explicit BinaryOpCache(CacheData &data)
-      : BaseCache(data)
-    { }
-
-    explicit BinaryOpCache(CacheData &data, jsbytecode *pc)
-      : BaseCache(data, pc)
-    {
-        data.pc = pc;
-        setState(Uninitialized);
-    }
-
-    uint32_t getKey() const {
-        return uint32_t(JSOp(*data.pc) << 16) | data.data;
-    }
-
-    bool update(JSContext *cx, HandleValue lhs, HandleValue rhs, HandleValue res);
-
-    IonCode *generate(JSContext *cx);
-    bool generateUpdate(JSContext *cx, MacroAssembler &masm);
-    bool generateInt32(JSContext *cx, MacroAssembler &masm);
-
-    State getTargetState(HandleValue lhs, HandleValue rhs, HandleValue res);
-
-    State getState() const {
-        return (State)data.data;
-    }
-    void setState(State state) {
-        data.data = (uint32_t)state;
-    }
-};
-
-class CompareCache : public BaseCache
-{
-    enum State {
-        Uninitialized = 0,
-        Int32
-    };
-
-  public:
-    explicit CompareCache(CacheData &data)
-      : BaseCache(data)
-    { }
-
-    explicit CompareCache(CacheData &data, jsbytecode *pc)
-      : BaseCache(data, pc)
-    {
-        data.pc = pc;
-        setState(Uninitialized);
-    }
-
-    uint32_t getKey() const {
-        return uint32_t(JSOp(*data.pc) << 16) | data.data;
-    }
-
-    bool update(JSContext *cx, HandleValue lhs, HandleValue rhs);
-
-    IonCode *generate(JSContext *cx);
-    bool generateUpdate(JSContext *cx, MacroAssembler &masm);
-    bool generateInt32(JSContext *cx, MacroAssembler &masm);
-
-    State getTargetState(HandleValue lhs, HandleValue rhs);
-
-    State getState() const {
-        return (State)data.data;
-    }
-    void setState(State state) {
-        data.data = (uint32_t)state;
-    }
-};
+struct CacheData;
 
 struct BaselineScript
 {
@@ -174,11 +54,7 @@ struct BaselineScript
         method_ = code;
     }
 
-    CacheData &getCache(size_t index) {
-        JS_ASSERT(index < numCaches());
-        return cacheList()[index];
-    }
-
+    CacheData &getCache(size_t index);
     CacheData &cacheDataFromReturnAddr(uint8_t *addr);
 
     size_t numCaches() const {
