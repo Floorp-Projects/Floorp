a55072e64618cde188e0fe8d37e6782fd62c0aa0
Ehsan Akhgari <ehsan@mozilla.com>
2012-10-30 17:39:38 -0400

    Use #include guards in the web audio code, no bug

--- content/media/webaudio/AudioParam.h
+++ content/media/webaudio/AudioParam.h
@@ -4,7 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#pragma once
+#ifndef AudioParam_h_
+#define AudioParam_h_
 
 #include "AudioEventTimeline.h"
 #include "nsWrapperCache.h"
@@ -113,4 +114,6 @@ private:
 
 }
 }
+
+#endif
 
