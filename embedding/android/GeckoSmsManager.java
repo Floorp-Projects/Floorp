e9136f2a7b03c19105a726ec0d2217979b42739c
Vicamo Yang <vyang@mozilla.com>
2012-10-30 18:55:44 +0800

    Bug 797277 - Part 4/5: Android implementation, r=mounir,blassey

--- embedding/android/GeckoSmsManager.java
+++ embedding/android/GeckoSmsManager.java
@@ -32,6 +32,8 @@ import android.os.Looper;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 
+import static android.telephony.SmsMessage.MessageClass;
+
 /**
  * This class is returning unique ids for PendingIntent requestCode attribute.
  * There are only |Integer.MAX_VALUE - Integer.MIN_VALUE| unique IDs available,
@@ -340,6 +342,16 @@ public class GeckoSmsManager
   private final static int kInternalDeliveryStatusPending  = 32;
   private final static int kInternalDeliveryStatusFailed   = 64;
 
+  /*
+   * Keep the following values in sync with |MessageClass| in:
+   * dom/sms/src/Types.h
+   */
+  private final static int kMessageClassNormal  = 0;
+  private final static int kMessageClassClass0  = 1;
+  private final static int kMessageClassClass1  = 2;
+  private final static int kMessageClassClass2  = 3;
+  private final static int kMessageClassClass3  = 4;
+
   private final static String[] kRequiredMessageRows = new String[] { "_id", "address", "body", "date", "type", "status" };
 
   public GeckoSmsManager() {
@@ -377,6 +389,7 @@ public class GeckoSmsManager
 
         GeckoAppShell.notifySmsReceived(msg.getDisplayOriginatingAddress(),
                                         msg.getDisplayMessageBody(),
+                                        getGeckoMessageClass(msg.getMessageClass()),
                                         System.currentTimeMillis());
       }
 
@@ -937,6 +950,21 @@ public class GeckoSmsManager
       return kDeliveryStatusPending;
     }
     return kDeliveryStatusSuccess;
+  }
+
+  private int getGeckoMessageClass(MessageClass aMessageClass) {
+    switch (aMessageClass) {
+      case UNKNOWN:
+        return kMessageClassNormal;
+      case CLASS_0:
+        return kMessageClassClass0;
+      case CLASS_1:
+        return kMessageClassClass1;
+      case CLASS_2:
+        return kMessageClassClass2;
+      case CLASS_3:
+        return kMessageClassClass3;
+    }
   }
 
   class IdTooHighException extends Exception {
