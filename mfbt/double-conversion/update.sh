329530abbe7fa603b801b60b5c4ea4a8e6929fb7
Rafael Ávila de Espíndola <respindola@mozilla.com>
2012-10-30 11:48:56 -0400

    Bug 806538 - Update double-conversion and document the version we are at. r=Waldo.

--- mfbt/double-conversion/update.sh
+++ mfbt/double-conversion/update.sh
@@ -3,6 +3,10 @@
 # Copies the needed files from a directory containing the original
 # double-conversion source that we need.
 
+# This was last updated with git rev e5b34421b763f7bf7e4f9081403db417d5a55a36.
+
+set -e
+
 cp $1/LICENSE ./
 cp $1/README ./
 
@@ -14,4 +18,3 @@ cp $1/src/*.cc ./
 
 patch -p3 < add-mfbt-api-markers.patch
 patch -p3 < use-StandardInteger.patch
-patch -p3 < more-architectures.patch
