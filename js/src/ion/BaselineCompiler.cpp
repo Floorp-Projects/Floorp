0b5af66d2ae1d24deb9174593f96861cbed285d9
Jan de Mooij <jdemooij@mozilla.com>
2012-10-30 14:19:58 +0100

    Bug 806415 part 1 - Rename FrameState to FrameInfo, move to its own file. r=djvj

--- js/src/ion/BaselineCompiler.cpp
+++ js/src/ion/BaselineCompiler.cpp
@@ -16,15 +16,6 @@
 using namespace js;
 using namespace js::ion;
 
-bool
-FrameState::init() {
-    size_t nstack = script->nslots - script->nfixed;
-    if (!stack.init(nstack))
-        return false;
-
-    return true;
-}
-
 BaselineCompiler::BaselineCompiler(JSContext *cx, JSScript *script)
   : cx(cx),
     script(script),
@@ -527,100 +518,6 @@ BaselineCompiler::loadValue(const StackValue *source, const ValueOperand &dest)
       default:
         JS_NOT_REACHED("Foo");
     }
-}
-
-void
-FrameState::sync(StackValue *val)
-{
-    switch (val->kind()) {
-      case StackValue::Stack:
-        break;
-      case StackValue::LocalSlot:
-        masm.pushValue(addressOfLocal(val->localSlot()));
-        break;
-      case StackValue::Register:
-        masm.pushValue(val->reg());
-        break;
-      case StackValue::Constant:
-        masm.pushValue(val->constant());
-        break;
-      default:
-        JS_NOT_REACHED("Invalid kind");
-        break;
-    }
-
-    val->setStack();
-}
-
-void
-FrameState::syncStack(uint32_t uses)
-{
-    JS_ASSERT(uses <= stackDepth());
-
-    uint32_t depth = stackDepth() - uses;
-
-    for (uint32_t i = 0; i < depth; i++) {
-        StackValue *current = &stack[i];
-        sync(current);
-    }
-}
-
-void
-FrameState::ensureInRegister(StackValue *val, ValueOperand dest, ValueOperand scratch)
-{
-    switch (val->kind()) {
-      case StackValue::Constant:
-        masm.moveValue(val->constant(), dest);
-        break;
-      case StackValue::LocalSlot:
-        masm.loadValue(addressOfLocal(val->localSlot()), dest);
-        break;
-      case StackValue::Stack:
-        masm.popValue(dest);
-        break;
-      case StackValue::Register: {
-        ValueOperand reg = val->reg();
-        if (reg.payloadReg() != dest.payloadReg())
-            masm.mov(reg.payloadReg(), dest.payloadReg());
-        if (reg.typeReg() != dest.typeReg())
-            masm.mov(reg.typeReg(), dest.typeReg());
-        break;
-      }
-      default:
-        JS_NOT_REACHED("foo");
-    }
-}
-
-void
-FrameState::popRegsAndSync(uint32_t uses)
-{
-    // x86 has only 3 Value registers. Only support 2 regs here for now,
-    // so that there's always a scratch Value register for reg -> reg
-    // moves.
-    JS_ASSERT(uses > 0);
-    JS_ASSERT(uses <= 2);
-    JS_ASSERT(uses <= stackDepth());
-
-    syncStack(uses);
-
-    switch (uses) {
-      case 1:
-        ensureInRegister(peek(-1), R0, R2);
-        break;
-      case 2:
-        if (peek(-1)->kind() == StackValue::Register) {
-            ensureInRegister(peek(-1), R1, R2);
-            ensureInRegister(peek(-2), R0, R2);
-        } else {
-            ensureInRegister(peek(-2), R0, R2);
-            ensureInRegister(peek(-1), R1, R2);
-        }
-        break;
-      default:
-        JS_NOT_REACHED("Unexpected use count");
-    }
-
-    spIndex -= uses;
 }
 
 bool
