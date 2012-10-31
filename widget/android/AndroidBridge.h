f7834c604125eb3a08f793fcfb1a269113712652
Jim Chen <nchen@mozilla.com>
2012-10-31 17:35:31 -0400

    Bug 805162 - d. Redefine and implement Gecko to Java IME notifications in widget; r=blassey

--- widget/android/AndroidBridge.h
+++ widget/android/AndroidBridge.h
@@ -100,7 +100,7 @@ class AndroidBridge
 public:
     enum {
         NOTIFY_IME_RESETINPUTSTATE = 0,
-        NOTIFY_IME_SETOPENSTATE = 1,
+        NOTIFY_IME_REPLY_EVENT = 1,
         NOTIFY_IME_CANCELCOMPOSITION = 2,
         NOTIFY_IME_FOCUSCHANGE = 3
     };
@@ -170,8 +170,6 @@ public:
     void EnableSensor(int aSensorType);
 
     void DisableSensor(int aSensorType);
-
-    void ReturnIMEQueryResult(const PRUnichar *aResult, uint32_t aLen, int aSelStart, int aSelLen);
 
     void NotifyXreExit();
 
@@ -408,7 +406,6 @@ protected:
     jmethodID jEnableLocationHighAccuracy;
     jmethodID jEnableSensor;
     jmethodID jDisableSensor;
-    jmethodID jReturnIMEQueryResult;
     jmethodID jNotifyAppShellReady;
     jmethodID jNotifyXreExit;
     jmethodID jScheduleRestart;
