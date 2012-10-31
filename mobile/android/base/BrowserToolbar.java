299352e2d25e308ba1752a39db2526f6d7d7639b
Sriram Ramasubramanian <sriram@mozilla.com>
2012-10-31 10:59:46 -0700

    Bug 783092: Make the animation ready for Lightweight themes. [r=lucasr, f=mfinkle]

--- mobile/android/base/BrowserToolbar.java
+++ mobile/android/base/BrowserToolbar.java
@@ -515,6 +515,9 @@ public class BrowserToolbar implements ViewSwitcher.ViewFactory,
                         PropertyAnimator.Property.TRANSLATION_X,
                         width);
 
+        // Uses the old mTabsPaneWidth.
+        adjustTabsAnimation(false);
+
         mTabsPaneWidth = width;
 
         // Only update title padding immediatelly when shrinking the browser
@@ -522,6 +525,22 @@ public class BrowserToolbar implements ViewSwitcher.ViewFactory,
         // expanding (see finishTabsAnimation()).
         if (mTabsPaneWidth > 0)
             setPageActionVisibility(mStop.getVisibility() == View.VISIBLE);
+    }
+
+    public void adjustTabsAnimation(boolean reset) {
+        int width = reset ? 0 : mTabsPaneWidth;
+        mAwesomeBarRightEdge.setTranslationX(-width);
+        mAwesomeBar.setTranslationX(width);
+        mAddressBarBg.setTranslationX(width);
+        mTabs.setTranslationX(width);
+        mTabsCount.setTranslationX(width);
+        mBack.setTranslationX(width);
+        mForward.setTranslationX(width);
+        mTitle.setTranslationX(width);
+        mFavicon.setTranslationX(width);
+        mSiteSecurity.setTranslationX(width);
+
+        ((ViewGroup.MarginLayoutParams) mLayout.getLayoutParams()).leftMargin = reset ? mTabsPaneWidth : 0;
     }
 
     public void finishTabsAnimation() {
@@ -550,6 +569,10 @@ public class BrowserToolbar implements ViewSwitcher.ViewFactory,
                 mMenu.getBackground().setLevel(TABS_CONTRACTED);
             }
         }
+
+        // A level change will not trigger onMeasure() for the tabs, where the path is created.
+        // Manually requesting a layout to re-calculate the path.
+        mTabs.requestLayout();
     }
 
     public void setProgressVisibility(boolean visible) {
@@ -582,7 +605,7 @@ public class BrowserToolbar implements ViewSwitcher.ViewFactory,
         // We want title to fill the whole space available for it when there are icons
         // being shown on the right side of the toolbar as the icons already have some
         // padding in them. This is just to avoid wasting space when icons are shown.
-        mTitle.setPadding(0, 0, (!mShowReader && !isLoading ? mTitlePadding : 0) + mTabsPaneWidth, 0);
+        mTitle.setPadding(0, 0, (!mShowReader && !isLoading ? mTitlePadding : 0), 0);
 
         updateFocusOrder();
     }
