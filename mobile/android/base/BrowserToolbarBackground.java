dfe004f31555c3a041f633ccb3bc04abb8922b55
Sriram Ramasubramanian <sriram@mozilla.com>
2012-10-31 11:05:39 -0700

    Bug 783092: LightweightTheme for BrowserApp. [r=mfinkle]
    --HG--
    extra : rebase_source : 4c3223032eaa62d96586ed1abccd902d5cb31485

--- mobile/android/base/BrowserToolbarBackground.java
+++ mobile/android/base/BrowserToolbarBackground.java
@@ -5,23 +5,54 @@
 package org.mozilla.gecko;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.graphics.PorterDuff.Mode;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
 public class BrowserToolbarBackground extends LinearLayout
-                                      implements CanvasDelegate.DrawManager { 
-    Path mPath;
-    CanvasDelegate mCanvasDelegate;
+                                      implements CanvasDelegate.DrawManager,
+                                                 LightweightTheme.OnChangeListener { 
+    private GeckoActivity mActivity;
+    private Path mPath;
+    private CurveTowards mSide;
+    private CanvasDelegate mCanvasDelegate;
+
+    private enum CurveTowards { NONE, LEFT, RIGHT };
 
     public BrowserToolbarBackground(Context context, AttributeSet attrs) {
         super(context, attrs);
 
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BrowserToolbarCurve);
+        int curveTowards = a.getInt(R.styleable.BrowserToolbarCurve_curveTowards, 0x02);
+        a.recycle();
+
+        if (curveTowards == 0x00)
+            mSide = CurveTowards.NONE;
+        else if (curveTowards == 0x01)
+            mSide = CurveTowards.LEFT;
+        else
+            mSide = CurveTowards.RIGHT;
+
         // Path is clipped.
         mPath = new Path();
         mCanvasDelegate = new CanvasDelegate(this, Mode.DST_OUT);
+        mActivity = (GeckoActivity) context;
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mActivity.getLightweightTheme().addListener(this);
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mActivity.getLightweightTheme().removeListener(this);
     }
 
     @Override
@@ -33,12 +64,22 @@ public class BrowserToolbarBackground extends LinearLayout
         float curve = height * 1.125f;
 
         mPath.reset();
-        mPath.moveTo(width, height);
-        mPath.cubicTo((width - (curve * 0.75f)), height,
-                      (width - (curve * 0.25f)), 0,
-                      (width - curve), 0);
-        mPath.lineTo(width, 0);
-        mPath.lineTo(width, height);
+
+        if (mSide == CurveTowards.LEFT) {
+            mPath.moveTo(0, height);
+            mPath.cubicTo(curve * 0.75f, height,
+                          curve * 0.25f, 0,
+                          curve, 0);
+            mPath.lineTo(0, 0);
+            mPath.lineTo(0, height);
+        } else if (mSide == CurveTowards.RIGHT) {
+            mPath.moveTo(width, height);
+            mPath.cubicTo((width - (curve * 0.75f)), height,
+                          (width - (curve * 0.25f)), 0,
+                          (width - curve), 0);
+            mPath.lineTo(width, 0);
+            mPath.lineTo(width, height);
+        }
     }
 
     @Override
@@ -49,5 +90,37 @@ public class BrowserToolbarBackground extends LinearLayout
     @Override
     public void defaultDraw(Canvas canvas) {
         super.draw(canvas);
+    }
+
+    @Override
+    public void onLightweightThemeChanged() {
+        Drawable drawable = mActivity.getLightweightTheme().getDrawable(this);
+        if (drawable == null)
+            return;
+
+        int[] padding =  new int[] { getPaddingLeft(),
+                                     getPaddingTop(),
+                                     getPaddingRight(),
+                                     getPaddingBottom()
+                                   };
+        setBackgroundDrawable(drawable);
+        setPadding(padding[0], padding[1], padding[2], padding[3]);
+    }
+
+    @Override
+    public void onLightweightThemeReset() {
+        int[] padding =  new int[] { getPaddingLeft(),
+                                     getPaddingTop(),
+                                     getPaddingRight(),
+                                     getPaddingBottom()
+                                   };
+        setBackgroundResource(R.drawable.address_bar_bg);
+        setPadding(padding[0], padding[1], padding[2], padding[3]);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        onLightweightThemeChanged();
     }
 }
