b505fdd76d5dd853869c7843fc61ec5e04a69a2a
George Wright <gw@gwright.org.uk>
2012-11-01 12:11:42 -0400

    [PATCH 5/9] Bug 807500 - Add a bunch of required GL wrapper methods to GLContext for use with Skia-GL r=mattwoodrow
    From 280a6de21b11d92c287ec8d35daffd34179b6e94 Mon Sep 17 00:00:00 2001
    ---
     gfx/gl/GLContext.cpp      | 26 +++++++++++++++++
     gfx/gl/GLContext.h        | 73 +++++++++++++++++++++++++++++++++++++++++++++++
     gfx/gl/GLContextSymbols.h | 20 +++++++++++++
     3 files changed, 119 insertions(+)

--- gfx/gl/GLContext.h
+++ gfx/gl/GLContext.h
@@ -1626,6 +1626,12 @@ public:
         AFTER_GL_CALL;
     }
 
+    void fBeginQuery(GLenum target, GLuint id) {
+        BEFORE_GL_CALL;
+        mSymbols.fBeginQuery(target, id);
+        AFTER_GL_CALL;
+    }
+
     void fBindAttribLocation(GLuint program, GLuint index, const GLchar* name) {
         BEFORE_GL_CALL;
         mSymbols.fBindAttribLocation(program, index, name);
@@ -1775,6 +1781,18 @@ public:
         AFTER_GL_CALL;
     }
 
+    void fDrawBuffer(GLenum mode) {
+        BEFORE_GL_CALL;
+        mSymbols.fDrawBuffer(mode);
+        AFTER_GL_CALL;
+    }
+
+    void fDrawBuffers(GLsizei n, GLenum* bufs) {
+        BEFORE_GL_CALL;
+        mSymbols.fDrawBuffers(n, bufs);
+        AFTER_GL_CALL;
+    }
+
 private:
     void raw_fDrawArrays(GLenum mode, GLint first, GLsizei count) {
         BEFORE_GL_CALL;
@@ -1798,6 +1816,12 @@ public:
     void fEnableVertexAttribArray(GLuint index) {
         BEFORE_GL_CALL;
         mSymbols.fEnableVertexAttribArray(index);
+        AFTER_GL_CALL;
+    }
+
+    void fEndQuery(GLenum target) {
+        BEFORE_GL_CALL;
+        mSymbols.fEndQuery(target);
         AFTER_GL_CALL;
     }
 
@@ -1842,6 +1866,24 @@ public:
         GLint retval = mSymbols.fGetAttribLocation(program, name);
         AFTER_GL_CALL;
         return retval;
+    }
+
+    void fGetQueryiv(GLenum target, GLenum pname, GLint* params) {
+        BEFORE_GL_CALL;
+        mSymbols.fGetQueryiv(target, pname, params);
+        AFTER_GL_CALL;
+    }
+
+    void fGetQueryObjectiv(GLuint id, GLenum pname, GLint* params) {
+        BEFORE_GL_CALL;
+        mSymbols.fGetQueryObjectiv(id, pname, params);
+        AFTER_GL_CALL;
+    }
+
+    void fGetQueryObjectuiv(GLuint id, GLenum pname, GLuint* params) {
+        BEFORE_GL_CALL;
+        mSymbols.fGetQueryObjectuiv(id, pname, params);
+        AFTER_GL_CALL;
     }
 
 private:
@@ -1895,6 +1937,12 @@ public:
     void fTexParameteri(GLenum target, GLenum pname, GLint param) {
         BEFORE_GL_CALL;
         mSymbols.fTexParameteri(target, pname, param);
+        AFTER_GL_CALL;
+    }
+
+    void fTexParameteriv(GLenum target, GLenum pname, GLint* params) {
+        BEFORE_GL_CALL;
+        mSymbols.fTexParameteriv(target, pname, params);
         AFTER_GL_CALL;
     }
 
@@ -2573,6 +2621,12 @@ private:
         AFTER_GL_CALL;
     }
 
+    void GLAPIENTRY raw_fGenQueries(GLsizei n, GLuint* names) {
+        BEFORE_GL_CALL;
+        mSymbols.fGenQueries(n, names);
+        AFTER_GL_CALL;
+    }
+
     void GLAPIENTRY raw_fGenRenderbuffers(GLsizei n, GLuint* names) {
         BEFORE_GL_CALL;
         mSymbols.fGenRenderbuffers(n, names);
@@ -2606,6 +2660,11 @@ public:
     void fGenFramebuffers(GLsizei n, GLuint* names) {
         raw_fGenFramebuffers(n, names);
         TRACKING_CONTEXT(CreatedFramebuffers(this, n, names));
+    }
+
+    void fGenQueries(GLsizei n, GLuint* names) {
+        raw_fGenQueries(n, names);
+        TRACKING_CONTEXT(CreatedQueries(this, n, names));
     }
 
     void fGenRenderbuffers(GLsizei n, GLuint* names) {
@@ -2655,7 +2714,18 @@ private:
         AFTER_GL_CALL;
     }
 
+    void GLAPIENTRY raw_fDeleteQueries(GLsizei n, GLuint* names) {
+        BEFORE_GL_CALL;
+        mSymbols.fDeleteQueries(n, names);
+        AFTER_GL_CALL;
+    }
+
 public:
+    void GLAPIENTRY fDeleteQueries(GLsizei n, GLuint* names) {
+        raw_fDeleteQueries(n, names);
+        TRACKING_CONTEXT(DeletedQueries(this, n, names));
+    }
+
     void fDeleteProgram(GLuint program) {
         raw_fDeleteProgram(program);
         TRACKING_CONTEXT(DeletedProgram(this, program));
@@ -2783,12 +2853,14 @@ public:
     void THEBES_API CreatedProgram(GLContext *aOrigin, GLuint aName);
     void THEBES_API CreatedShader(GLContext *aOrigin, GLuint aName);
     void THEBES_API CreatedBuffers(GLContext *aOrigin, GLsizei aCount, GLuint *aNames);
+    void THEBES_API CreatedQueries(GLContext *aOrigin, GLsizei aCount, GLuint *aNames);
     void THEBES_API CreatedTextures(GLContext *aOrigin, GLsizei aCount, GLuint *aNames);
     void THEBES_API CreatedFramebuffers(GLContext *aOrigin, GLsizei aCount, GLuint *aNames);
     void THEBES_API CreatedRenderbuffers(GLContext *aOrigin, GLsizei aCount, GLuint *aNames);
     void THEBES_API DeletedProgram(GLContext *aOrigin, GLuint aName);
     void THEBES_API DeletedShader(GLContext *aOrigin, GLuint aName);
     void THEBES_API DeletedBuffers(GLContext *aOrigin, GLsizei aCount, GLuint *aNames);
+    void THEBES_API DeletedQueries(GLContext *aOrigin, GLsizei aCount, GLuint *aNames);
     void THEBES_API DeletedTextures(GLContext *aOrigin, GLsizei aCount, GLuint *aNames);
     void THEBES_API DeletedFramebuffers(GLContext *aOrigin, GLsizei aCount, GLuint *aNames);
     void THEBES_API DeletedRenderbuffers(GLContext *aOrigin, GLsizei aCount, GLuint *aNames);
@@ -2830,6 +2902,7 @@ public:
     nsTArray<NamedResource> mTrackedFramebuffers;
     nsTArray<NamedResource> mTrackedRenderbuffers;
     nsTArray<NamedResource> mTrackedBuffers;
+    nsTArray<NamedResource> mTrackedQueries;
 #endif
 
 public:
