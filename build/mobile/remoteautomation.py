29ee6e6f9e143e326e43e781957d80d40000c688
Ed Morley <emorley@mozilla.com>
2012-11-02 18:57:50 +0000

    Bug 757838 - Misc remoteautomation.py cleanup; r=jmaher

--- build/mobile/remoteautomation.py
+++ build/mobile/remoteautomation.py
@@ -61,11 +61,12 @@ class RemoteAutomation(Automation):
 
         return env
 
-    def waitForFinish(self, proc, utilityPath, timeout, maxTime, startTime, debuggerInfo, symbolsDir):
+    def waitForFinish(self, proc, utilityPath, timeout, maxTime, startTime, debuggerInfo, symbolsPath):
+        """ Wait for tests to finish (as evidenced by the process exiting),
+            or for maxTime elapse, in which case kill the process regardless.
+        """
         # maxTime is used to override the default timeout, we should honor that
         status = proc.wait(timeout = maxTime)
-
-        print proc.stdout
 
         if (status == 1 and self._devicemanager.processExist(proc.procName)):
             # Then we timed out, make sure Fennec is dead
@@ -185,9 +186,8 @@ class RemoteAutomation(Automation):
                     # function in dmSUT, so an error here is not necessarily
                     # the end of the world
                     return ''
-                tlen = len(t)
                 retVal = t[self.stdoutlen:]
-                self.stdoutlen = tlen
+                self.stdoutlen = len(t)
                 return retVal.strip('\n').strip()
             else:
                 return ''
@@ -206,6 +206,9 @@ class RemoteAutomation(Automation):
                 timer += interval
                 if (timer > timeout):
                     break
+
+            # Flush anything added to stdout during the sleep
+            print self.stdout
 
             if (timer >= timeout):
                 return 1
