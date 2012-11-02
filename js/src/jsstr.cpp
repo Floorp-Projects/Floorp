455e58bddb6786c9bdc04bb7ce338a4d36cb1350
Terrence Cole <terrence@mozilla.com>
2012-11-02 15:48:08 -0700

    Bug 808181: Do not allow implicit conversion of Return<T>; r=jorendorff
    This will allow the compiler to catch more usage errors.

--- js/src/jsstr.cpp
+++ js/src/jsstr.cpp
@@ -2369,7 +2369,7 @@ LambdaIsGetElem(JSObject &lambda)
     if (!fun->isInterpreted())
         return NULL;
 
-    RawScript script = fun->script();
+    RawScript script = fun->script().get(nogc);
     jsbytecode *pc = script->code;
 
     /*
