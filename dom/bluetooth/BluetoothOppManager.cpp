52d74bd835904271c3fdd34a5a73231564237f8d
Eric Chou <echou@mozilla.com>
2012-11-03 09:36:39 +0800

    Bug 805724 - patch 3: let user be able to terminate receiving process by calling StopSendingFile(), r=qdot

--- dom/bluetooth/BluetoothOppManager.cpp
+++ dom/bluetooth/BluetoothOppManager.cpp
@@ -277,10 +277,6 @@ BluetoothOppManager::SendFile(BlobParent* aActor,
 bool
 BluetoothOppManager::StopSendingFile(BluetoothReplyRunnable* aRunnable)
 {
-  if (!mBlob) {
-    return false;
-  }
-
   mAbortFlag = true;
 
   return true;
@@ -605,12 +601,16 @@ BluetoothOppManager::ReceiveSocketData(UnixSocketRawData* aMessage)
           ReceivingFileConfirmation(mConnectedDeviceAddress, sFileName,
                                     sFileLength, sContentType);
         } else {
-          ReplyToPut(mPutFinal, true);
+          ReplyToPut(mPutFinal, mAbortFlag ? false : true);
 
-          if (mPutFinal) {
+          if (mAbortFlag) {
             mReceiving = false;
-            FileTransferComplete(mConnectedDeviceAddress, true, true, sFileName,
-                                 sSentFileLength, sContentType);
+            FileTransferComplete(mConnectedDeviceAddress, false, true,
+                                 sFileName, sSentFileLength, sContentType);
+          } else if (mPutFinal) {
+            mReceiving = false;
+            FileTransferComplete(mConnectedDeviceAddress, true, true,
+                                 sFileName, sSentFileLength, sContentType);
           }
         }
       }
