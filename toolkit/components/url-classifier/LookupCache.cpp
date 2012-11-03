33944f0b33fd9a91c790c7c7caa4799f84f7929d
Gian-Carlo Pascutto <gpascutto@mozilla.com>
2012-11-03 01:53:11 +0100

    Bug 807822 - No noise added to completion request in some circumstances. r=dcamp

--- toolkit/components/url-classifier/LookupCache.cpp
+++ toolkit/components/url-classifier/LookupCache.cpp
@@ -199,14 +199,6 @@ LookupCache::Has(const Completion& aCompletion,
 {
   *aHas = *aComplete = false;
 
-  // check completion store first
-  if (mCompletions.BinaryIndexOf(aCompletion) != nsTArray<Completion>::NoIndex) {
-    LOG(("Complete in %s", mTableName.get()));
-    *aComplete = true;
-    *aHas = true;
-    return NS_OK;
-  }
-
   uint32_t prefix = aCompletion.ToUint32();
   uint32_t hostkey = aHostkey.ToUint32();
   uint32_t codedkey;
@@ -224,6 +216,12 @@ LookupCache::Has(const Completion& aCompletion,
   LOG(("Probe in %s: %X, found %d", mTableName.get(), prefix, found));
 
   if (found) {
+    *aHas = true;
+  }
+
+  if (mCompletions.BinaryIndexOf(aCompletion) != nsTArray<Completion>::NoIndex) {
+    LOG(("Complete in %s", mTableName.get()));
+    *aComplete = true;
     *aHas = true;
   }
 
