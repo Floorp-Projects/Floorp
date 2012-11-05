60766a1277e6bb45fb8c282ffd493ddb141673f0
Boris Zbarsky <bzbarsky@mit.edu>
2012-11-05 11:58:04 -0500

    Bug 804649.  Create a CommentNode header.  r=peterv
    --HG--
    rename : content/base/src/nsCommentNode.cpp => content/base/src/Comment.cpp
    rename : content/base/src/nsCommentNode.cpp => content/base/src/Comment.h

---
+++ content/base/src/Comment.cpp
@@ -0,0 +1,103 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/*
+ * Implementations of DOM Core's nsIDOMComment node.
+ */
+
+#include "nsCOMPtr.h"
+#include "nsGenericElement.h" // DOMCI_NODE_DATA
+#include "Comment.h"
+
+using namespace mozilla;
+using namespace dom;
+
+// DOMCI_NODE_DATA needs to be outside our namespaces
+DOMCI_NODE_DATA(Comment, Comment)
+
+nsresult
+NS_NewCommentNode(nsIContent** aInstancePtrResult,
+                  nsNodeInfoManager *aNodeInfoManager)
+{
+  NS_PRECONDITION(aNodeInfoManager, "Missing nodeinfo manager");
+
+  *aInstancePtrResult = nullptr;
+
+  nsCOMPtr<nsINodeInfo> ni = aNodeInfoManager->GetCommentNodeInfo();
+  NS_ENSURE_TRUE(ni, NS_ERROR_OUT_OF_MEMORY);
+
+  Comment *instance = new Comment(ni.forget());
+  if (!instance) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+
+  NS_ADDREF(*aInstancePtrResult = instance);
+
+  return NS_OK;
+}
+
+namespace mozilla {
+namespace dom {
+
+Comment::Comment(already_AddRefed<nsINodeInfo> aNodeInfo)
+  : nsGenericDOMDataNode(aNodeInfo)
+{
+  NS_ABORT_IF_FALSE(mNodeInfo->NodeType() == nsIDOMNode::COMMENT_NODE,
+                    "Bad NodeType in aNodeInfo");
+}
+
+Comment::~Comment()
+{
+}
+
+// QueryInterface implementation for Comment
+NS_INTERFACE_TABLE_HEAD(Comment)
+  NS_NODE_INTERFACE_TABLE3(Comment, nsIDOMNode, nsIDOMCharacterData,
+                           nsIDOMComment)
+  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(Comment)
+NS_INTERFACE_MAP_END_INHERITING(nsGenericDOMDataNode)
+
+
+NS_IMPL_ADDREF_INHERITED(Comment, nsGenericDOMDataNode)
+NS_IMPL_RELEASE_INHERITED(Comment, nsGenericDOMDataNode)
+
+
+bool
+Comment::IsNodeOfType(uint32_t aFlags) const
+{
+  return !(aFlags & ~(eCONTENT | eCOMMENT | eDATA_NODE));
+}
+
+nsGenericDOMDataNode*
+Comment::CloneDataNode(nsINodeInfo *aNodeInfo, bool aCloneText) const
+{
+  nsCOMPtr<nsINodeInfo> ni = aNodeInfo;
+  Comment *it = new Comment(ni.forget());
+  if (it && aCloneText) {
+    it->mText = mText;
+  }
+
+  return it;
+}
+
+#ifdef DEBUG
+void
+Comment::List(FILE* out, int32_t aIndent) const
+{
+  int32_t indx;
+  for (indx = aIndent; --indx >= 0; ) fputs("  ", out);
+
+  fprintf(out, "Comment@%p refcount=%d<!--", (void*)this, mRefCnt.get());
+
+  nsAutoString tmp;
+  ToCString(tmp, 0, mText.GetLength());
+  fputs(NS_LossyConvertUTF16toASCII(tmp).get(), out);
+
+  fputs("-->\n", out);
+}
+#endif
+
+} // namespace dom
+} // namespace mozilla
