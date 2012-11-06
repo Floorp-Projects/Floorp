5b4019065859404250cd4b74e7a1894f368ff634
Wes Johnston <wjohnston@mozilla.com>
2012-11-05 17:14:22 -0800

    Bug 786061 - Disable session restore in webapps. r=mfinkle

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -1591,7 +1591,7 @@ abstract public class GeckoApp
             passedUri = uri;
         }
 
-        if (mRestoreMode == RESTORE_NONE && getProfile().shouldRestoreSession()) {
+        if (mRestoreMode == GeckoAppShell.RESTORE_NONE && shouldRestoreSession()) {
             mRestoreMode = RESTORE_CRASH;
         }
 
@@ -1827,6 +1827,10 @@ abstract public class GeckoApp
             mProfile = GeckoProfile.get(this);
         }
         return mProfile;
+    }
+
+    protected boolean shouldRestoreSession() {
+        return getProfile().shouldRestoreSession();
     }
 
     /**
