e0408a8c63f764440fcd5b553245316c86dd8aa9
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2012-11-06 18:23:15 -0500

    Bug 801487 - Fixup imported tests. r=sicking

--- dom/encoding/test/unit/head.js
+++ dom/encoding/test/unit/head.js
@@ -5,34 +5,48 @@
 
 const { 'classes': Cc, 'interfaces': Ci } = Components;
 
-function equal(a, b, msg) {
-  dump("equal(" + a + ", " + b + ", \"" + msg + "\")");
+// Instantiate nsIDOMScriptObjectFactory so that DOMException is usable in xpcshell
+Components.classesByID["{9eb760f0-4380-11d2-b328-00805f8a3859}"].getService(Ci.nsISupports);
+
+function assert_equals(a, b, msg) {
+  dump("assert_equals(" + a + ", " + b + ", \"" + msg + "\")");
   do_check_eq(a, b, Components.stack.caller);
 }
 
-function ok(cond, msg) {
-  dump("ok(" + cond + ", \"" + msg + "\")");
+function assert_not_equals(a, b, msg) {
+  dump("assert_not_equals(" + a + ", " + b + ", \"" + msg + "\")");
+  do_check_neq(a, b, Components.stack.caller);
+}
+
+function assert_array_equals(a, b, msg) {
+  dump("assert_array_equals(\"" + msg + "\")");
+  do_check_eq(a.length, b.length, Components.stack.caller);
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) {
+      do_check_eq(a[i], b[i], Components.stack.caller);
+    }
+  }
+}
+
+function assert_true(cond, msg) {
+  dump("assert_true(" + cond + ", \"" + msg + "\")");
   do_check_true(!!cond, Components.stack.caller); 
 }
 
-function raises(func) {
-  dump("raises(" + func + ")");
+function assert_throws(ex, func) {
+  dump("assert_throws(\"" + ex + "\", " + func + ")");
   try {
     func();
     do_check_true(false, Components.stack.caller); 
   } catch (e) {
-    do_check_true(true, Components.stack.caller); 
+    do_check_eq(e.name, ex.name, Components.stack.caller); 
   }
 }
 
 var tests = [];
 
-function test(msg, no, func) {
-  tests.push({msg: msg, func: func || no});
-}
-
-function expect(count) {
-  dump("expect(", count, ")");
+function test(func, msg) {
+  tests.push({msg: msg, func: func});
 }
 
 function run_test() {
