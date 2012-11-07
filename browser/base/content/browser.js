c6031b2b42febabd3d27db4c14e3b8ef6ab8ec91
Jared Wein <jwein@mozilla.com>
2012-11-07 14:27:50 -0800

    Bug 808176 - devtools.chrome.enabled=true should also show the Error Console menuitem. r=dcamp

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1471,7 +1471,8 @@ var gBrowserInit = {
     }
 
     // Enable Error Console?
-    let consoleEnabled = gPrefService.getBoolPref("devtools.errorconsole.enabled");
+    let consoleEnabled = gPrefService.getBoolPref("devtools.errorconsole.enabled") ||
+                         gPrefService.getBoolPref("devtools.chrome.enabled");
     if (consoleEnabled) {
       let cmd = document.getElementById("Tools:ErrorConsole");
       cmd.removeAttribute("hidden");
