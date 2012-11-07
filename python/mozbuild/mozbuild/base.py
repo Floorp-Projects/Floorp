136ba6a6646579ddb4d0e741c4f1b1b6a9a9c7c2
Gregory Szorc <gps@mozilla.com>
2012-11-06 16:58:13 -0800

    Bug 808336 - Part 2: Discover mach settings providers via decorators; r=jhammel
    With this patch, mach is now decoupled from the build system and is
    truly a generic command dispatching framework.

--- python/mozbuild/mozbuild/base.py
+++ python/mozbuild/mozbuild/base.py
@@ -5,7 +5,6 @@
 from __future__ import unicode_literals
 
 import logging
-import multiprocessing
 import os
 import pymake.parser
 import subprocess
@@ -14,13 +13,10 @@ import which
 
 from pymake.data import Makefile
 
-from mach.config import (
-    ConfigProvider,
-    PositiveIntegerType,
-)
-
 from mach.mixin.logging import LoggingMixin
 from mach.mixin.process import ProcessExecutionMixin
+
+from .config import BuildConfig
 
 
 class MozbuildObject(ProcessExecutionMixin):
@@ -280,22 +276,6 @@ class MozbuildObject(ProcessExecutionMixin):
 
         return cls(self.topsrcdir, self.settings, self.log_manager,
             topobjdir=self.topobjdir)
-
-
-class BuildConfig(ConfigProvider):
-    """The configuration for mozbuild."""
-
-    def __init__(self, settings):
-        self.settings = settings
-
-    @classmethod
-    def _register_settings(cls):
-        def register(section, option, type_cls, **kwargs):
-            cls.register_setting(section, option, type_cls, domain='mozbuild',
-                **kwargs)
-
-        register('build', 'threads', PositiveIntegerType,
-            default=multiprocessing.cpu_count())
 
 
 class MachCommandBase(MozbuildObject):
