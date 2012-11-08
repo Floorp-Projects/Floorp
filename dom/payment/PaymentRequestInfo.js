671cd8cf4efbdf29429a4bc8826d7d63b7bc47fa
Fernando Jiménez <ferjmoreno@gmail.com>
2012-11-07 16:02:10 -0800

    Bug 800511 - Remove redundant validation from navigator.mozPay(); r=fabrice

--- dom/payment/PaymentRequestInfo.js
+++ dom/payment/PaymentRequestInfo.js
@@ -10,42 +10,11 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
 const PAYMENTREQUESTINFO_CID =
   Components.ID("{0a58c67d-f003-48da-81d1-bd8f605f4b1c}");
-const PAYMENTPRODUCTPRICE_CID =
-  Components.ID("{3d7dcabf-b77c-4bb3-b225-46011898ec32}");
-const PAYMENTREQUESTPAYMENTINFO_CID =
-  Components.ID("{7f2e3274-3956-42e1-b7ce-59b8cd23d177}");
-const PAYMENTREQUESTREFUNDINFO_CID =
-  Components.ID("{e75566c6-dfb1-4f6b-b21d-078536c883b0}");
-
-// nsIDOMPaymentProductPrice
-
-function PaymentProductPrice() {
-  this.wrappedJSObject = this;
-};
-
-PaymentProductPrice.prototype = {
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIDOMPaymentProductPrice]),
-  classID: PAYMENTPRODUCTPRICE_CID,
-  classInfo: XPCOMUtils.generateCI({
-    classID: PAYMENTPRODUCTPRICE_CID,
-    contractID: "@mozilla.org/payment/product-price;1",
-    classDescription: "Payment product price",
-    flags: Ci.nsIClassInfo.DOM_OBJECT,
-    interfaces: [Ci.nsIDOMPaymentProductPrice]
-  }),
-  currency: null,
-  amount: null,
-
-  init: function init(aCurrency, aAmount) {
-    this.currency = aCurrency;
-    this.amount = aAmount;
-  }
-};
-
 
 // nsIDOMPaymentRequestInfo
 
 function PaymentRequestInfo() {
+  this.wrappedJSObject = this;
 };
 
 PaymentRequestInfo.prototype = {
@@ -62,73 +31,11 @@ PaymentRequestInfo.prototype = {
   type: null,
   providerName: null,
 
-  initRequest: function initRequest(aJwt, aType, aProviderName) {
+  init: function init(aJwt, aType, aProviderName) {
     this.jwt = aJwt;
     this.type = aType;
     this.providerName = aProviderName;
   }
 };
 
-// nsIDOMPaymentRequestPaymentInfo
-
-function PaymentRequestPaymentInfo() {
-  this.wrappedJSObject = this;
-};
-
-PaymentRequestPaymentInfo.prototype = {
-  __proto__: PaymentRequestInfo.prototype,
-
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIDOMPaymentRequestPaymentInfo]),
-  classID: PAYMENTREQUESTPAYMENTINFO_CID,
-  classInfo: XPCOMUtils.generateCI({
-    classID: PAYMENTREQUESTPAYMENTINFO_CID,
-    contractID: "@mozilla.org/payment/request-payment-info;1",
-    classDescription: "Payment request payment information",
-    flags: Ci.nsIClassInfo.DOM_OBJECT,
-    interfaces: [Ci.nsIDOMPaymentRequestPaymentInfo]
-  }),
-  productName: null,
-  productDescription: null,
-  productPrice: null,
-
-  init: function init(aJwt, aType, aProviderName,
-                      aProductName, aProductDescription, aProductPrice) {
-    this.__proto__.initRequest.call(this, aJwt, aType, aProviderName);
-    this.productName = aProductName;
-    this.productDescription = aProductDescription;
-    this.productPrice = aProductPrice;
-  }
-};
-
-// nsIDOMPaymentRequestRefundInfo
-
-function PaymentRequestRefundInfo() {
-  this.wrappedJSObject = this;
-};
-
-PaymentRequestRefundInfo.prototype = {
-  __proto__: PaymentRequestInfo.prototype,
-
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIDOMPaymentRequestRefundInfo]),
-  classID: PAYMENTREQUESTREFUNDINFO_CID,
-  classInfo: XPCOMUtils.generateCI({
-    classID: PAYMENTREQUESTREFUNDINFO_CID,
-    contractID: "@mozilla.org/payment/request-refund-info;1",
-    classDescription: "Payment request refund information",
-    flags: Ci.nsIClassInfo.DOM_OBJECT,
-    interfaces: [Ci.nsIDOMPaymentRequestRefundInfo]
-  }),
-  reason: null,
-
-  init: function init(aJwt, aType, aProviderName, aReason) {
-    this.__proto__.initRequest.call(this, aJwt, aType, aProviderName);
-    this.reason = aReason;
-  }
-};
-
-this.NSGetFactory = XPCOMUtils.generateNSGetFactory([
-  PaymentProductPrice,
-  PaymentRequestInfo,
-  PaymentRequestPaymentInfo,
-  PaymentRequestRefundInfo
-]);
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([PaymentRequestInfo]);
