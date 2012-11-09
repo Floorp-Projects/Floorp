f4f4ec813d279f718dd770bc0f2d0cc0f38a1923
Robert O'Callahan <robert@ocallahan.org>
2012-11-09 18:14:59 -0500

    Bug 810470. Part 6: Rename nsDisplayBackground to nsDisplayBackgroundImage. r=mattwoodrow
    --HG--
    extra : rebase_source : c532ae41706db6c9d5488bc2b7901c3bf0f0fc25

--- layout/generic/nsFrame.h
+++ layout/generic/nsFrame.h
@@ -99,7 +99,7 @@
 //----------------------------------------------------------------------
 
 struct nsBoxLayoutMetrics;
-class nsDisplayBackground;
+class nsDisplayBackgroundImage;
 
 /**
  * Implementation of a simple frame that's not splittable and has no
@@ -510,7 +510,7 @@ public:
   nsresult DisplayBackgroundUnconditional(nsDisplayListBuilder*   aBuilder,
                                           const nsDisplayListSet& aLists,
                                           bool aForceBackground,
-                                          nsDisplayBackground** aBackground);
+                                          nsDisplayBackgroundImage** aBackground);
   /**
    * Adds display items for standard CSS borders, background and outline for
    * for this frame, as necessary. Checks IsVisibleForPainting and won't
