7752f218bb379a48e44fd612e6f8422bde1bb4ca
Boris Zbarsky <bzbarsky@mit.edu>
2012-11-09 07:43:57 -0800

    Bug 779048 part 4.  Implement basic codegen for callbacks, without handling of arguments or return values yet.  r=peterv

--- dom/bindings/BindingUtils.h
+++ dom/bindings/BindingUtils.h
@@ -914,6 +914,25 @@ WrapNativeParent(JSContext* cx, JSObject* scope, const T& p)
   return WrapNativeParent(cx, scope, GetParentPointer(p), GetWrapperCache(p));
 }
 
+template<typename T>
+static inline JSObject*
+WrapCallThisObject(JSContext* cx, JSObject* scope, const T& p)
+{
+  // WrapNativeParent is a bit of a Swiss army knife that will
+  // wrap anything for us.
+  JSObject* obj = WrapNativeParent(cx, scope, p);
+  if (!obj) {
+    return nullptr;
+  }
+
+  // But it won't necessarily put things in the compartment of cx.
+  if (!JS_WrapObject(cx, &obj)) {
+    return nullptr;
+  }
+
+  return obj;
+}
+
 static inline bool
 InternJSString(JSContext* cx, jsid& id, const char* chars)
 {
