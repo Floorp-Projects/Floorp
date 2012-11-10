d91b7132e11965e0a07e1eb657e7d7c8d791cc78
Justin Lebar <justin.lebar@gmail.com>
2012-11-10 10:32:35 -0800

    Bug 802366 - Prelude, part 2: Rename GetParentIgnoreBrowserFrame to GetSameTypeParentIgnoreBrowserAndAppBoundaries. r=bz
    It's a long name, but at least it's clear.

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -2830,7 +2830,7 @@ nsDocShell::GetSameTypeParent(nsIDocShellTreeItem ** aParent)
 }
 
 NS_IMETHODIMP
-nsDocShell::GetParentIgnoreBrowserFrame(nsIDocShell** aParent)
+nsDocShell::GetSameTypeParentIgnoreBrowserAndAppBoundaries(nsIDocShell** aParent)
 {
     NS_ENSURE_ARG_POINTER(aParent);
     *aParent = nullptr;
@@ -12442,7 +12442,7 @@ nsDocShell::GetAppId(uint32_t* aAppId)
     MOZ_ASSERT(GetFrameType() != eFrameTypeApp);
 
     nsCOMPtr<nsIDocShell> parent;
-    GetParentIgnoreBrowserFrame(getter_AddRefs(parent));
+    GetSameTypeParentIgnoreBrowserAndAppBoundaries(getter_AddRefs(parent));
 
     if (!parent) {
         *aAppId = nsIScriptSecurityManager::NO_APP_ID;
