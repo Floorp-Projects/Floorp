a43727d045a20a512b7a020144babe3b8092090a
Nicholas D. Matsakis <nmatsakis@mozilla.com>
2012-11-11 07:07:07 -0500

    Bug 800064 - Fix the units of computation in the shell watchdog. r=terrence

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -2795,24 +2795,24 @@ WatchdogMain(void *arg)
     while (gWatchdogThread) {
          int64_t now = PRMJ_Now();
          if (gWatchdogHasTimeout && !IsBefore(now, gWatchdogTimeout)) {
-            /*
-             * The timeout has just expired. Trigger the operation callback
-             * outside the lock.
-             */
-            gWatchdogHasTimeout = false;
-            PR_Unlock(gWatchdogLock);
-            CancelExecution(rt);
-            PR_Lock(gWatchdogLock);
+             /*
+              * The timeout has just expired. Trigger the operation callback
+              * outside the lock.
+              */
+             gWatchdogHasTimeout = false;
+             PR_Unlock(gWatchdogLock);
+             CancelExecution(rt);
+             PR_Lock(gWatchdogLock);
 
-            /* Wake up any threads doing sleep. */
-            PR_NotifyAllCondVar(gSleepWakeup);
+             /* Wake up any threads doing sleep. */
+             PR_NotifyAllCondVar(gSleepWakeup);
         } else {
-            int64_t sleepDuration = gWatchdogHasTimeout
-                                    ? gWatchdogTimeout - now
-                                    : PR_INTERVAL_NO_TIMEOUT;
-            mozilla::DebugOnly<PRStatus> status =
-                PR_WaitCondVar(gWatchdogWakeup, sleepDuration);
-            JS_ASSERT(status == PR_SUCCESS);
+             uint64_t sleepDuration = PR_INTERVAL_NO_TIMEOUT;
+             if (gWatchdogHasTimeout)
+                 sleepDuration = (gWatchdogTimeout - now) / PRMJ_USEC_PER_SEC * PR_TicksPerSecond();
+             mozilla::DebugOnly<PRStatus> status =
+               PR_WaitCondVar(gWatchdogWakeup, sleepDuration);
+             JS_ASSERT(status == PR_SUCCESS);
         }
     }
     PR_Unlock(gWatchdogLock);
