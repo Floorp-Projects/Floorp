10e8a73eea3d3c541c461a6f763020c98661ccba
David Bolter <dbolter@mozilla.com>
2012-11-13 14:07:21 -0500

    Bug 811307 - [AccessFu] Add mochitest for enabling. r=eeejay

--- accessible/tests/mochitest/common.js
+++ accessible/tests/mochitest/common.js
@@ -648,6 +648,23 @@ function shortenString(aString, aMaxLength)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// General Utils
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * Return main chrome window (crosses chrome boundary)
+ */
+ function getTopChromeWindow(aWindow)
+ {
+   return aWindow.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+                 .getInterface(Components.interfaces.nsIWebNavigation)
+                 .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
+                 .rootTreeItem
+                 .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+                 .getInterface(Components.interfaces.nsIDOMWindow);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
 // Private
 ////////////////////////////////////////////////////////////////////////////////
 
