7961445b00c103ea5157ce5d3c671b59a911ada2
Jim Mathies <jmathies@mozilla.com>
2012-11-14 10:24:05 -0600

    Bug 810704 - Fixup misc. toolkit resource path issues. r=mak77

--- toolkit/devtools/debugger/tests/unit/test_dbgsocket.js
+++ toolkit/devtools/debugger/tests/unit/test_dbgsocket.js
@@ -1,8 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-Cu.import("resource:///modules/devtools/dbg-server.jsm");
-Cu.import("resource:///modules/devtools/dbg-client.jsm");
+Cu.import("resource://gre/modules/devtools/dbg-server.jsm");
+Cu.import("resource://gre/modules/devtools/dbg-client.jsm");
 
 function run_test()
 {
