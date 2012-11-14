1c716ff4345fb9d44146b8f9ca084cdb2502736c
Cameron McCormack <cam@mcc.id.au>
2012-11-15 06:09:01 +1100

    Bug 811581 - Add a base class for all *RuleProcessorData classes. r=bz

--- content/xbl/src/nsBindingManager.h
+++ content/xbl/src/nsBindingManager.h
@@ -16,6 +16,7 @@
 #include "nsTArray.h"
 #include "nsThreadUtils.h"
 
+class ElementDependentRuleProcessorData;
 class nsIContent;
 class nsIXPConnectWrappedJS;
 class nsIAtom;
@@ -181,11 +182,11 @@ public:
 
   // Style rule methods
   nsresult WalkRules(nsIStyleRuleProcessor::EnumFunc aFunc,
-                     RuleProcessorData* aData,
+                     ElementDependentRuleProcessorData* aData,
                      bool* aCutOffInheritance);
 
   void WalkAllRules(nsIStyleRuleProcessor::EnumFunc aFunc,
-                    RuleProcessorData* aData);
+                    ElementDependentRuleProcessorData* aData);
   /**
    * Do any processing that needs to happen as a result of a change in
    * the characteristics of the medium, and return whether this rule
