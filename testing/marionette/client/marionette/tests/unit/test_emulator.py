fa422d5bf91854f0ba1646b1a6cb75b7429898d1
Ed Morley <emorley@mozilla.com>
2012-11-14 10:05:14 +0000

    Backout 992f2748681f (bug 811185) for breaking emulator tests

--- testing/marionette/client/marionette/tests/unit/test_emulator.py
+++ testing/marionette/client/marionette/tests/unit/test_emulator.py
@@ -1,7 +1,3 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 from marionette_test import MarionetteTestCase
 from errors import JavascriptException, MarionetteException
 
