8d7bf212ba1d9c5d5c31897fdf2f7ad319d05a8f
Boris Zbarsky <bzbarsky@mit.edu>
2012-11-14 10:05:41 -0800

    Bug 811663 part 2.  Create a DocumentFragment.h header.  r=smaug
    All the code is just being moved without any changes.

--- content/base/src/DocumentFragment.cpp
+++ content/base/src/DocumentFragment.cpp
@@ -7,8 +7,7 @@
  * Implementation of DOM Core's nsIDOMDocumentFragment.
  */
 
-#include "nsIDOMDocumentFragment.h"
-#include "mozilla/dom/FragmentOrElement.h"
+#include "mozilla/dom/DocumentFragment.h"
 #include "nsGenericElement.h" // for DOMCI_NODE_DATA
 #include "nsINameSpaceManager.h"
 #include "nsINodeInfo.h"
@@ -17,110 +16,6 @@
 #include "nsGkAtoms.h"
 #include "nsDOMString.h"
 #include "nsContentUtils.h"
-
-namespace mozilla {
-namespace dom {
-
-class DocumentFragment : public FragmentOrElement,
-                         public nsIDOMDocumentFragment
-{
-public:
-  using FragmentOrElement::GetFirstChild;
-
-  // nsISupports
-  NS_DECL_ISUPPORTS_INHERITED
-
-  // interface nsIDOMNode
-  NS_FORWARD_NSIDOMNODE_TO_NSINODE
-
-  // interface nsIDOMDocumentFragment
-  // NS_DECL_NSIDOCUMENTFRAGMENT  Empty
-
-  DocumentFragment(already_AddRefed<nsINodeInfo> aNodeInfo);
-  virtual ~DocumentFragment()
-  {
-  }
-
-  // nsIContent
-  virtual already_AddRefed<nsINodeInfo>
-    GetExistingAttrNameFromQName(const nsAString& aStr) const
-  {
-    return nullptr;
-  }
-
-  nsresult SetAttr(int32_t aNameSpaceID, nsIAtom* aName,
-                   const nsAString& aValue, bool aNotify)
-  {
-    return SetAttr(aNameSpaceID, aName, nullptr, aValue, aNotify);
-  }
-  virtual nsresult SetAttr(int32_t aNameSpaceID, nsIAtom* aName,
-                           nsIAtom* aPrefix, const nsAString& aValue,
-                           bool aNotify)
-  {
-    return NS_OK;
-  }
-  virtual bool GetAttr(int32_t aNameSpaceID, nsIAtom* aName, 
-                         nsAString& aResult) const
-  {
-    return false;
-  }
-  virtual bool HasAttr(int32_t aNameSpaceID, nsIAtom* aName) const
-  {
-    return false;
-  }
-  virtual nsresult UnsetAttr(int32_t aNameSpaceID, nsIAtom* aAttribute, 
-                             bool aNotify)
-  {
-    return NS_OK;
-  }
-  virtual const nsAttrName* GetAttrNameAt(uint32_t aIndex) const
-  {
-    return nullptr;
-  }
-  virtual uint32_t GetAttrCount() const
-  {
-    return 0;
-  }
-
-  virtual bool IsNodeOfType(uint32_t aFlags) const;
-
-  virtual nsXPCClassInfo* GetClassInfo();
-
-  virtual nsIDOMNode* AsDOMNode() { return this; }
-
-  virtual nsIAtom* DoGetID() const;
-  virtual nsIAtom *GetIDAttributeName() const;
-
-  virtual nsresult BindToTree(nsIDocument* aDocument, nsIContent* aParent,
-                              nsIContent* aBindingParent,
-                              bool aCompileEventHandlers)
-  {
-    NS_ASSERTION(false, "Trying to bind a fragment to a tree");
-    return NS_ERROR_NOT_IMPLEMENTED;
-  }
-
-  virtual void UnbindFromTree(bool aDeep, bool aNullParent)
-  {
-    NS_ASSERTION(false, "Trying to unbind a fragment from a tree");
-    return;
-  }
-
-  virtual Element* GetNameSpaceElement()
-  {
-    return nullptr;
-  }
-
-#ifdef DEBUG
-  virtual void List(FILE* out, int32_t aIndent) const;
-  virtual void DumpContent(FILE* out, int32_t aIndent, bool aDumpAll) const;
-#endif
-
-protected:
-  nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
-};
-
-} // namespace dom
-} // namespace mozilla
 
 nsresult
 NS_NewDocumentFragment(nsIDOMDocumentFragment** aInstancePtrResult,
