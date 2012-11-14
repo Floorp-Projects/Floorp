6d4a6c33ae822f8afffd4b91f1e1a83f357e3c94
Jim Mathies <jmathies@mozilla.com>
2012-11-14 10:24:05 -0600

    Bug 810704 - Fixup misc. toolkit resource path issues. r=mak77

--- toolkit/devtools/debugger/tests/unit/head_dbg.js
+++ toolkit/devtools/debugger/tests/unit/head_dbg.js
@@ -15,8 +15,8 @@ Services.prefs.setBoolPref("devtools.debugger.log", true);
 // Enable remote debugging for the relevant tests.
 Services.prefs.setBoolPref("devtools.debugger.remote-enabled", true);
 
-Cu.import("resource:///modules/devtools/dbg-server.jsm");
-Cu.import("resource:///modules/devtools/dbg-client.jsm");
+Cu.import("resource://gre/modules/devtools/dbg-server.jsm");
+Cu.import("resource://gre/modules/devtools/dbg-client.jsm");
 
 // Convert an nsIScriptError 'aFlags' value into an appropriate string.
 function scriptErrorFlagsToKind(aFlags) {
