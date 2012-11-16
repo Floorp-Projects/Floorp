a3975964b999e8077dda4b8ccd89001adffd3f70
Jed Parsons <jparsons@mozilla.com>
2012-11-16 18:34:32 -0500

    Bug 804932 - Pass arbitrary options from RPs to BrowserID internal api methods. r=benadida

--- b2g/components/test/unit/head_identity.js
+++ b2g/components/test/unit/head_identity.js
@@ -47,11 +47,20 @@ function uuid() {
 
 // create a mock "doc" object, which the Identity Service
 // uses as a pointer back into the doc object
-function mockDoc(aIdentity, aOrigin, aDoFunc) {
+function mockDoc(aParams, aDoFunc) {
   let mockedDoc = {};
   mockedDoc.id = uuid();
-  mockedDoc.loggedInUser = aIdentity;
-  mockedDoc.origin = aOrigin;
+
+  // Properties of aParams may include loggedInUser
+  Object.keys(aParams).forEach(function(param) {
+    mockedDoc[param] = aParams[param];
+  });
+
+  // the origin is set inside nsDOMIdentity by looking at the
+  // document.nodePrincipal.origin.  Here we, we must satisfy
+  // ourselves with pretending.
+  mockedDoc.origin = "https://jedp.gov";
+
   mockedDoc['do'] = aDoFunc;
   mockedDoc.doReady = partial(aDoFunc, 'ready');
   mockedDoc.doLogin = partial(aDoFunc, 'login');
@@ -67,7 +76,12 @@ function mockDoc(aIdentity, aOrigin, aDoFunc) {
 // messages up to gaia (either the trusty ui or the hidden iframe),
 // and convey messages back down from gaia to the controller through
 // the message callback.
-function mockPipe() {
+
+// The mock receiving pipe simulates gaia which, after receiving messages
+// through the pipe, will call back with instructions to invoke
+// certain methods.  It mocks what comes back from the other end of
+// the pipe.
+function mockReceivingPipe() {
   let MockedPipe = {
     communicate: function(aRpOptions, aGaiaOptions, aMessageCallback) {
       switch (aGaiaOptions.message) {
@@ -84,6 +98,17 @@ function mockPipe() {
           throw("what the what?? " + aGaiaOptions.message);
           break;
       }
+    }
+  };
+  return MockedPipe;
+}
+
+// The mock sending pipe lets us test what's actually getting put in the
+// pipe.
+function mockSendingPipe(aMessageCallback) {
+  let MockedPipe = {
+    communicate: function(aRpOptions, aGaiaOptions, aDummyCallback) {
+      aMessageCallback(aRpOptions, aGaiaOptions);
     }
   };
   return MockedPipe;
