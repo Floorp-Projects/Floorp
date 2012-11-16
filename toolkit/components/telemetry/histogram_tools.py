9c4ff63d2d41f597a9e22ca337dada225e0e2ed5
Ryan VanderMeulen <ryanvm@gmail.com>
2012-11-15 21:07:38 -0500

    Backed out changeset cc2469a8b41a (bug 800557) for Windows build bustage.

--- toolkit/components/telemetry/histogram_tools.py
+++ toolkit/components/telemetry/histogram_tools.py
@@ -5,11 +5,7 @@
 from __future__ import with_statement
 
 import math
-import json
-try:
-    from collections import OrderedDict
-except ImportError:
-    from simplejson import OrderedDict
+import simplejson as json
 
 def table_dispatch(kind, table, body):
     """Call body with table[kind] if it exists.  Raise an error otherwise."""
@@ -202,6 +198,6 @@ def from_file(filename):
 the histograms defined in filename.
     """
     with open(filename, 'r') as f:
-        histograms = json.load(f, object_pairs_hook=OrderedDict)
+        histograms = json.load(f, object_pairs_hook=json.OrderedDict)
         for (name, definition) in histograms.iteritems():
             yield Histogram(name, definition)
