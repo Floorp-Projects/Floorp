91bff06d271c58cc6686cfbe1813457db3823441
Jan de Mooij <jdemooij@mozilla.com>
2012-11-16 12:05:55 +0100

    Bug 812280 - Remove move32/movePtr overloads that take an Address. r=dvander

--- js/src/ion/x86/MacroAssembler-x86.h
+++ js/src/ion/x86/MacroAssembler-x86.h
@@ -195,12 +195,6 @@ class MacroAssemblerX86 : public MacroAssemblerX86Shared
     void movePtr(const Register &src, const Register &dest) {
         movl(src, dest);
     }
-    void movePtr(Operand op, const Register &dest) {
-        movl(op, dest);
-    }
-    void movePtr(const Address &src, const Register &dest) {
-        movl(Operand(src), dest);
-    }
 
     // Returns the register containing the type tag.
     Register splitTagForTest(const ValueOperand &value) {
