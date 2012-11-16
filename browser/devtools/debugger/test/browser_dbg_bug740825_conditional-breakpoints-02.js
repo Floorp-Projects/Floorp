0f9759963268925c174698b4660e767612e113f9
Victor Porof <vporof@mozilla.com>
2012-11-16 13:16:23 +0200

    No bug - Fix 'browser_dbg_bug740825_conditional-breakpoints-02.js' caused by 43303dfa12a5, r=past

--- browser/devtools/debugger/test/browser_dbg_bug740825_conditional-breakpoints-02.js
+++ browser/devtools/debugger/test/browser_dbg_bug740825_conditional-breakpoints-02.js
@@ -36,6 +36,8 @@ function test()
     gDebugger = gPane.contentWindow;
     gBreakpoints = gDebugger.DebuggerController.Breakpoints;
     gBreakpointsPane = gDebugger.DebuggerView.Breakpoints;
+
+    gDebugger.DebuggerView.togglePanes({ visible: true, animated: false });
     resumed = true;
 
     gDebugger.DebuggerController.activeThread.addOneTimeListener("framesadded", function() {
@@ -87,7 +89,6 @@ function test()
 
     is(gEditor.getBreakpoints().length, 0, "no breakpoints in the editor");
 
-    gDebugger.DebuggerView.togglePanes({ visible: true, animated: false });
     executeSoon(addBreakpoint1);
   }
 
