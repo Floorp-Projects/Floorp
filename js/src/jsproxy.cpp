5fe3a31c5addfce6c40eb173b20ad4b3e0f43fa9
Eddy Bruel <ejpbruel@mozilla.com>
2012-11-17 00:36:02 +0100

    Bug 811343 - proxy parent to cx->global() by default; r=bholley

--- js/src/jsproxy.cpp
+++ js/src/jsproxy.cpp
@@ -3178,12 +3178,9 @@ proxy(JSContext *cx, unsigned argc, jsval *vp)
     RootedObject proto(cx);
     if (!JSObject::getProto(cx, target, &proto))
         return false;
-    JSObject *parent = NULL;
-    if (proto)
-        parent = proto->getParent();
     RootedObject fun(cx, target->isCallable() ? target : (JSObject *) NULL);
     JSObject *proxy = NewProxyObject(cx, &ScriptedDirectProxyHandler::singleton,
-                                     ObjectValue(*target), proto, parent,
+                                     ObjectValue(*target), proto, cx->global(),
                                      fun, fun);
     if (!proxy)
         return false;
