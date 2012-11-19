0389f581552e13ac8d50427b4574bccfd2159fdb
Josh Aas <joshmoz@gmail.com>
2012-11-19 18:02:21 -0500

    Back out fix for bug 405407 due to regressions.

--- netwerk/cache/nsDiskCacheStreams.h
+++ netwerk/cache/nsDiskCacheStreams.h
@@ -14,24 +14,29 @@
 
 #include "nsIInputStream.h"
 #include "nsIOutputStream.h"
-#include "nsIDiskCacheStreamInternal.h"
 
 #include "pratom.h"
 
 class nsDiskCacheInputStream;
+class nsDiskCacheOutputStream;
 class nsDiskCacheDevice;
 
-class nsDiskCacheStreamIO : public nsIOutputStream, nsIDiskCacheStreamInternal {
+class nsDiskCacheStreamIO : public nsISupports {
 public:
              nsDiskCacheStreamIO(nsDiskCacheBinding *   binding);
     virtual ~nsDiskCacheStreamIO();
     
     NS_DECL_ISUPPORTS
-    NS_DECL_NSIOUTPUTSTREAM
-    NS_DECL_NSIDISKCACHESTREAMINTERNAL
 
     nsresult    GetInputStream(uint32_t offset, nsIInputStream ** inputStream);
     nsresult    GetOutputStream(uint32_t offset, nsIOutputStream ** outputStream);
+
+    nsresult    CloseOutputStream(nsDiskCacheOutputStream * outputStream);
+    nsresult    CloseOutputStreamInternal(nsDiskCacheOutputStream * outputStream);
+        
+    nsresult    Write( const char * buffer,
+                       uint32_t     count,
+                       uint32_t *   bytesWritten);
 
     nsresult    Seek(int32_t whence, int32_t offset);
     nsresult    Tell(uint32_t * position);    
@@ -52,14 +57,19 @@ public:
     // and OS/2 requires that it not be private
     nsDiskCacheStreamIO() { NS_NOTREACHED("oops"); }
 private:
+
+
+    void        Close();
     nsresult    OpenCacheFile(int flags, PRFileDesc ** fd);
     nsresult    ReadCacheBlocks();
     nsresult    FlushBufferToFile();
     void        UpdateFileSize();
     void        DeleteBuffer();
+    nsresult    Flush();
 
     nsDiskCacheBinding *        mBinding;       // not an owning reference
     nsDiskCacheDevice *         mDevice;
+    nsDiskCacheOutputStream *   mOutStream;     // not an owning reference
     int32_t                     mInStreamCount;
     nsCOMPtr<nsIFile>           mLocalFile;
     PRFileDesc *                mFD;
@@ -69,8 +79,7 @@ private:
     uint32_t                    mBufPos;        // current mark in buffer
     uint32_t                    mBufEnd;        // current end of data in buffer
     uint32_t                    mBufSize;       // current end of buffer
-    bool                        mBufDirty;      // Where there is unflushed data in the buffer
-    bool                        mOutputStreamIsOpen; // Whether the output stream is open (for writing...)
+    bool                        mBufDirty;
     char *                      mBuffer;
     
 };
