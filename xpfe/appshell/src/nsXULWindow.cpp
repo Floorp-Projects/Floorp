5b1c87f10aa9332d5dd86a00b0397872eac6be58
Mounir Lamouri <mounir.lamouri@gmail.com>
2012-11-19 23:18:21 +0000

    Bug 764240 - Clamp window.sizeToContent() the same way we clamp other resizing methods. r=bz

--- xpfe/appshell/src/nsXULWindow.cpp
+++ xpfe/appshell/src/nsXULWindow.cpp
@@ -975,9 +975,17 @@ void nsXULWindow::OnChromeLoaded()
       // (if LoadSizeFromXUL set the size, mIntrinsicallySized will be false)
       nsCOMPtr<nsIContentViewer> cv;
       mDocShell->GetContentViewer(getter_AddRefs(cv));
-      nsCOMPtr<nsIMarkupDocumentViewer> markupViewer(do_QueryInterface(cv));
-      if (markupViewer)
-        markupViewer->SizeToContent();
+      nsCOMPtr<nsIMarkupDocumentViewer> markupViewer = do_QueryInterface(cv);
+      if (markupViewer) {
+        nsCOMPtr<nsIDocShellTreeItem> docShellAsItem = do_QueryInterface(mDocShell);
+        nsCOMPtr<nsIDocShellTreeOwner> treeOwner;
+        docShellAsItem->GetTreeOwner(getter_AddRefs(treeOwner));
+        if (treeOwner) {
+          int32_t width, height;
+          markupViewer->GetContentSize(&width, &height);
+          treeOwner->SizeShellTo(docShellAsItem, width, height);
+        }
+      }
     }
 
     bool positionSet = !mIgnoreXULPosition;
