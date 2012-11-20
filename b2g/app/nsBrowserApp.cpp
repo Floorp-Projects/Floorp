fe1ca13ca38917215e63c8fff3ed75d65879bf28
Michael Wu <mwu@mozilla.com>
2012-11-19 19:54:33 -0600

    Backed out changeset d12d63253125 (bug 809665) for red

--- b2g/app/nsBrowserApp.cpp
+++ b2g/app/nsBrowserApp.cpp
@@ -28,11 +28,6 @@
 #define snprintf _snprintf
 #define strcasecmp _stricmp
 #endif
-
-#ifdef MOZ_WIDGET_GONK
-#include "BootAnimation.h"
-#endif
-
 #include "BinaryPath.h"
 
 #include "nsXPCOMPrivate.h" // for MAXPATHLEN and XPCOM_DLL
@@ -143,11 +138,6 @@ static int do_main(int argc, char* argv[])
     argv += 2;
     argc -= 2;
   }
-
-#ifdef MOZ_WIDGET_GONK
-  /* Called to start the boot animation */
-  (void) NativeWindow();
-#endif
 
   if (appini) {
     nsXREAppData *appData;
