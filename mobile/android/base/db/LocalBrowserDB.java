46f31d0ac6bb84b0e7a6946c8496c28d24b49cbd
Brian Nicholson <bnicholson@mozilla.com>
2012-11-21 10:23:18 -0800

    Bug 812805 - Pull favicon ID from history when adding bookmarks. r=lucasr

--- mobile/android/base/db/LocalBrowserDB.java
+++ mobile/android/base/db/LocalBrowserDB.java
@@ -542,6 +542,19 @@ public class LocalBrowserDB implements BrowserDB.BrowserDBIface {
         values.put(Bookmarks.PARENT, folderId);
         values.put(Bookmarks.DATE_MODIFIED, now);
 
+        // Get the page's favicon ID from the history table
+        Cursor c = cr.query(mHistoryUriWithProfile,
+                            new String[] { History.FAVICON_ID },
+                            History.URL + " = ?",
+                            new String[] { uri },
+                            null);
+        if (c.moveToFirst()) {
+            int columnIndex = c.getColumnIndexOrThrow(History.FAVICON_ID);
+            if (!c.isNull(columnIndex))
+                values.put(Bookmarks.FAVICON_ID, c.getLong(columnIndex));
+        }
+        c.close();
+
         // Restore deleted record if possible
         values.put(Bookmarks.IS_DELETED, 0);
 
