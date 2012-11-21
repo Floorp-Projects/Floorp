bb0f6d22e64baf800a04ece06389c00edb322f68
Boris Zbarsky <bzbarsky@mit.edu>
2012-11-21 11:19:03 -0500

    Bug 812744 part 2.  Use the document scope object, not the script global, to find the JSObject to use as a scope when creating event handlers.  r=smaug
    Anoother option would be to just explicitly wrap the node and use that, for what it's worth.  That wouldn't give us an nsIScriptContext, though.

--- content/events/src/nsEventListenerManager.cpp
+++ content/events/src/nsEventListenerManager.cpp
@@ -604,7 +604,11 @@ nsEventListenerManager::SetEventHandler(nsIAtom *aName,
     // XXX sXBL/XBL2 issue -- do we really want the owner here?  What
     // if that's the XBL document?
     doc = node->OwnerDoc();
-    global = doc->GetScriptGlobalObject();
+    MOZ_ASSERT(!doc->IsLoadedAsData(), "Should not get in here at all");
+
+    // We want to allow compiling an event handler even in an unloaded
+    // document, so use GetScopeObject here, not GetScriptHandlingObject.
+    global = doc->GetScopeObject();
   } else {
     nsCOMPtr<nsPIDOMWindow> win = GetTargetAsInnerWindow();
     if (win) {
@@ -622,6 +626,13 @@ nsEventListenerManager::SetEventHandler(nsIAtom *aName,
     // loaded as data.
     return NS_OK;
   }
+
+#ifdef DEBUG
+  nsCOMPtr<nsPIDOMWindow> win = do_QueryInterface(global);
+  if (win) {
+    MOZ_ASSERT(win->IsInnerWindow(), "We should not have an outer window here!");
+  }
+#endif
 
   nsresult rv = NS_OK;
   // return early preventing the event listener from being added
