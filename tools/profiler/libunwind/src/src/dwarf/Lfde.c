9c42e80971d8d03b579ffa56a0bfbee5b3f43edb
Benoit Girard <b56girard@gmail.com>
2012-11-22 14:04:56 -0500

    Bug 809571 - Remove the libunwind stack walking code for ARM/Android. r=ehsan

--- tools/profiler/libunwind/src/src/dwarf/Lfde.c
+++
@@ -1,9 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-#define UNW_LOCAL_ONLY
-#include <libunwind.h>
-#if defined(UNW_LOCAL_ONLY) && !defined(UNW_REMOTE_ONLY)
-#include "Gfde.c"
-#endif
