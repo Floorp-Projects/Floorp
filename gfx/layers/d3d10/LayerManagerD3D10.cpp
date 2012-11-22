c2c2ab8c9f9dd8fb9f80e62aeb49faf5513c4a19
Chiajung Hung <chung@mozilla.com>
2012-11-22 10:40:57 +0800

    Bug 805939: Wait for shadow trees to update orientation before recompositing. r=cjones

--- gfx/layers/d3d10/LayerManagerD3D10.cpp
+++ gfx/layers/d3d10/LayerManagerD3D10.cpp
@@ -30,6 +30,7 @@
 #endif
 
 using namespace std;
+using namespace mozilla::dom;
 using namespace mozilla::gfx;
 
 namespace mozilla {
@@ -755,7 +756,9 @@ LayerManagerD3D10::Render(EndTransactionFlags aFlags)
     PaintToTarget();
   } else if (mBackBuffer) {
     ShadowLayerForwarder::BeginTransaction(mWidget->GetNaturalBounds(),
-                                           ROTATION_0);
+                                           ROTATION_0,
+                                           mWidget->GetNaturalBounds(),
+                                           eScreenOrientation_LandscapePrimary);
     
     nsIntRect contentRect = nsIntRect(0, 0, rect.width, rect.height);
     if (!mRootForShadowTree) {
