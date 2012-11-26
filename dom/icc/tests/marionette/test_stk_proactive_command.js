e71f7ea603aca36559134dc28422b78d7b70f0d5
Edgar Chen <echen@mozilla.com>
2012-11-26 16:37:35 +0800

    Bug 804671 - Part 4: marrionette tests for PROVIDE_LOCAL_INFORMATION. r=allstars.chh

--- dom/icc/tests/marionette/test_stk_proactive_command.js
+++ dom/icc/tests/marionette/test_stk_proactive_command.js
@@ -17,9 +17,57 @@ function testDisplayTextGsm7BitEncoding(cmd) {
   runNextTest();
 }
 
+function testLocalInfoLocation(cmd) {
+  log("STK CMD " + JSON.stringify(cmd));
+  is(cmd.typeOfCommand, icc.STK_CMD_PROVIDE_LOCAL_INFO);
+  is(cmd.commandNumber, 0x01);
+  is(cmd.commandQualifier, icc.STK_LOCAL_INFO_LOCATION_INFO);
+  is(cmd.options.localInfoType, icc.STK_LOCAL_INFO_LOCATION_INFO);
+
+  runNextTest();
+}
+
+function testLocalInfoImei(cmd) {
+  log("STK CMD " + JSON.stringify(cmd));
+  is(cmd.typeOfCommand, icc.STK_CMD_PROVIDE_LOCAL_INFO);
+  is(cmd.commandNumber, 0x01);
+  is(cmd.commandQualifier, icc.STK_LOCAL_INFO_IMEI);
+  is(cmd.options.localInfoType, icc.STK_LOCAL_INFO_IMEI);
+
+  runNextTest();
+}
+
+function testLocalInfoDate(cmd) {
+  log("STK CMD " + JSON.stringify(cmd));
+  is(cmd.typeOfCommand, icc.STK_CMD_PROVIDE_LOCAL_INFO);
+  is(cmd.commandNumber, 0x01);
+  is(cmd.commandQualifier, icc.STK_LOCAL_INFO_DATE_TIME_ZONE);
+  is(cmd.options.localInfoType, icc.STK_LOCAL_INFO_DATE_TIME_ZONE);
+
+  runNextTest();
+}
+
+function testLocalInfoLanguage(cmd) {
+  log("STK CMD " + JSON.stringify(cmd));
+  is(cmd.typeOfCommand, icc.STK_CMD_PROVIDE_LOCAL_INFO);
+  is(cmd.commandNumber, 0x01);
+  is(cmd.commandQualifier, icc.STK_LOCAL_INFO_LANGUAGE);
+  is(cmd.options.localInfoType, icc.STK_LOCAL_INFO_LANGUAGE);
+
+  runNextTest();
+}
+
 let tests = [
   {command: "d0288103012180820281020d1d00d3309bfc06c95c301aa8e80259c3ec34b9ac07c9602f58ed159bb940",
    func: testDisplayTextGsm7BitEncoding},
+  {command: "d009810301260082028182",
+   func: testLocalInfoLocation},
+  {command: "d009810301260182028182",
+   func: testLocalInfoImei},
+  {command: "d009810301260382028182",
+   func: testLocalInfoDate},
+  {command: "d009810301260482028182",
+   func: testLocalInfoLanguage},
 ];
 
 let pendingEmulatorCmdCount = 0;
