22979fc6000dbbac10f64a0da5692f03e1d36fdd
Trevor Saunders <trev.saunders@gmail.com>
2012-11-27 22:16:12 +0900

    Bug 767756 - try implementing ISimpleDOMNode with a tear off, r=tbsaunde, f=marcoz
    --HG--
    rename : accessible/src/msaa/nsAccessNodeWrap.cpp => accessible/src/windows/sdn/sdnAccessible.cpp
    rename : accessible/src/msaa/nsAccessNodeWrap.h => accessible/src/windows/sdn/sdnAccessible.h

---
+++ accessible/src/windows/sdn/sdnAccessible-inl.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_a11y_sdnAccessible_inl_h_
+#define mozilla_a11y_sdnAccessible_inl_h_
+
+#include "sdnAccessible.h"
+
+#include "DocAccessible.h"
+#include "nsAccessibilityService.h"
+
+namespace mozilla {
+namespace a11y {
+
+inline DocAccessible*
+sdnAccessible::GetDocument() const
+{
+  DocManager* docMgr = GetAccService();
+  return docMgr ?
+    docMgr->GetDocAccessibleFromCache(mNode->OwnerDoc()) : nullptr;
+}
+
+inline Accessible*
+sdnAccessible::GetAccessible() const
+{
+  DocAccessible* document = GetDocument();
+  return document ? document->GetAccessible(mNode) : nullptr;
+}
+
+} // namespace a11y
+} // namespace mozilla
+
+#endif // mozilla_a11y_sdnAccessible_inl_h_
