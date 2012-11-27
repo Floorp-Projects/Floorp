c68bce3ac0cdab56c7a64d655dc3f4d7d89731e9
Eric Chou <echou@mozilla.com>
2012-11-27 14:12:19 +0800

    Bug 811946 - remove annoying compiler warnings and define a more structured interface for Bluetooth uuid, r=qdot

--- dom/bluetooth/BluetoothDevice.cpp
+++ dom/bluetooth/BluetoothDevice.cpp
@@ -10,7 +10,6 @@
 #include "BluetoothReplyRunnable.h"
 #include "BluetoothService.h"
 #include "BluetoothUtils.h"
-#include "BluetoothServiceUuid.h"
 
 #include "nsIDOMDOMRequest.h"
 #include "nsDOMClassInfo.h"
