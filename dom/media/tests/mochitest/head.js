d562690d266c9c205abdcb7c9cd24c2ade6e2d3c
Jason Smith <jsmith@mozilla.com>
2012-11-26 18:27:46 -0800

    Bug 781534 - Create basic automated test coverage for the mozGetUserMedia for desktop. r=roc

--- dom/media/tests/mochitest/head.js
+++ dom/media/tests/mochitest/head.js
@@ -1,4 +1,4 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
@@ -9,14 +9,33 @@ var Cr = SpecialPowers.Cr;
 /**
  * Setup any Mochitest for WebRTC by enabling the preference for
  * peer connections. As by bug 797979 it will also enable mozGetUserMedia().
- * Additionally, we disable the permissions prompt for these mochitests.
  *
  * @param {Function} aCallback Test method to execute after initialization
+ * @param {Boolean} desktopSupportedOnly specifies if the test currently
+ *                                       is known to work on desktop only
  */
-function runTest(aCallback) {
+function runTest(aCallback, desktopSupportedOnly) {
   SimpleTest.waitForExplicitFinish();
 
-  SpecialPowers.pushPrefEnv({'set': [['media.peerconnection.enabled', true],
-                            ['media.navigator.permission.disabled', true]]},
-                            aCallback);
+  // If this is a desktop supported test and we're on android or b2g,
+  // indicate that the test is not supported and skip the test
+  if(desktopSupportedOnly && (navigator.platform === 'Android' ||
+     navigator.platform === '')) {
+    ok(true, navigator.platform + ' currently not supported');
+    SimpleTest.finish();
+  } else {
+    SpecialPowers.pushPrefEnv({'set': [['media.peerconnection.enabled', true]]},
+      aCallback);
+  }
+}
+
+/**
+ * A callback function fired only under unexpected circumstances while
+ * running the tests. Kills off the test as well gracefully.
+ *
+ * @param {String} obj the object fired back from the callback
+ */
+function unexpectedCallbackAndFinish(obj) {
+  ok(false, "Unexpected error callback with " + obj);
+  SimpleTest.finish();
 }
