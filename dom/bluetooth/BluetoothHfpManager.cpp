1a82cbc056166ed962e2897a53107a47598ce342
Gina Yeh <gyeh@mozilla.com>
2012-11-28 15:59:20 +0800

    Bug 813817 - Patch 1: Cancelling file transferring doesn't clear the notifications, r=echou

--- dom/bluetooth/BluetoothHfpManager.cpp
+++ dom/bluetooth/BluetoothHfpManager.cpp
@@ -651,6 +651,11 @@ BluetoothHfpManager::Listen()
     return false;
   }
 
+  if (GetConnectionStatus() == SocketConnectionStatus::SOCKET_LISTENING) {
+    NS_WARNING("BluetoothHfpManager has been already listening");
+    return true;
+  }
+
   CloseSocket();
 
   BluetoothService* bs = BluetoothService::Get();
@@ -977,6 +982,8 @@ BluetoothHfpManager::OnDisconnect()
   if (mSocketStatus == SocketConnectionStatus::SOCKET_CONNECTED) {
     Listen();
     NotifySettings();
+  } else if (mSocketStatus == SocketConnectionStatus::SOCKET_CONNECTING) {
+    NS_WARNING("BluetoothHfpManager got unexpected socket status!");
   }
 
   sStopSendingRingFlag = true;
