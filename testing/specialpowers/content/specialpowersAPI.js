345efe131e2dacaa3e9f9f52daea9966f2f30047
David Chan <dchan@mozilla.com>
2012-11-30 10:52:40 -0800

    Bug 816847 - Use AppService instead of Webapps.jsm for manifest lookup. r=jgriffin

--- testing/specialpowers/content/specialpowersAPI.js
+++ testing/specialpowers/content/specialpowersAPI.js
@@ -1240,15 +1240,15 @@ SpecialPowersAPI.prototype = {
               .spec;
     } else if (arg.manifestURL) {
       // It's a thing representing an app.
-      let tmp = {};
-      Cu.import("resource://gre/modules/Webapps.jsm", tmp);
+      let appsSvc = Cc["@mozilla.org/AppsService;1"]
+                      .getService(Ci.nsIAppsService)
+      let app = appsSvc.getAppByManifestURL(arg.manifestURL); 
 
-      let app = tmp.DOMApplicationRegistry.getAppByManifestURL(arg.manifestURL);
       if (!app) {
         throw "No app for this manifest!";
       }
 
-      appId = app.localId;
+      appId = appsSvc.getAppLocalIdByManifestURL(arg.manifestURL);
       url = app.origin;
       isInBrowserElement = arg.isInBrowserElement || false;
     } else if (arg.nodePrincipal) {
