d7155248ab02095fb2d4d73d17939b2813601c28
Axel Hecht <axel@pike.org>
2012-11-30 09:22:48 -0700

    bug 796051, add chrome-% target to package b2g localized files, r=fabrice

--- b2g/locales/filter.py
+++ b2g/locales/filter.py
@@ -5,31 +5,10 @@
 
 def test(mod, path, entity = None):
   import re
-  # ignore anything but b2g, which is our local repo checkout name
+  # ignore anything but b2g and specific overloads from dom and toolkit
   if mod not in ("netwerk", "dom", "toolkit", "security/manager",
-                 "services/sync", "embedding/android",
+                 "mobile",
                  "b2g"):
-    return False
+    return "ignore"
 
-  # Ignore Lorentz strings, at least temporarily
-  if mod == "toolkit" and path == "chrome/mozapps/plugins/plugins.dtd":
-    if entity.startswith('reloadPlugin.'): return False
-    if entity.startswith('report.'): return False
-
-  if mod != "b2g":
-    # we only have exceptions for b2g
-    return True
-  if not entity:
-    return not (re.match(r"b2g-l10n.js", path) or
-                re.match(r"defines.inc", path))
-  if path == "defines.inc":
-    return entity != "MOZ_LANGPACK_CONTRIBUTORS"
-
-  if path != "chrome/region.properties":
-    # only region.properties exceptions remain, compare all others
-    return True
-  
-  return not (re.match(r"browser\.search\.order\.[1-9]", entity) or
-              re.match(r"browser\.contentHandlers\.types\.[0-5]", entity) or
-              re.match(r"gecko\.handlerService\.schemes\.", entity) or
-              re.match(r"gecko\.handlerService\.defaultHandlersVersion", entity))
+  return "error"
