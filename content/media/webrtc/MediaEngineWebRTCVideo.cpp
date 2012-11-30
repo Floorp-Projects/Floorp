e38e1cd666fb13173bc32f43658a1cee45dd5f3a
Randell Jesup <rjesup@jesup.org>
2012-11-30 03:08:17 -0500

    Bug 816664: wallpaper patch for negative delta times for video frames r=roc,derf

--- content/media/webrtc/MediaEngineWebRTCVideo.cpp
+++ content/media/webrtc/MediaEngineWebRTCVideo.cpp
@@ -114,12 +114,16 @@ MediaEngineWebRTCVideoSource::NotifyPull(MediaStreamGraph* aGraph,
   nsRefPtr<layers::Image> image = mImage;
   TrackTicks target = TimeToTicksRoundUp(USECS_PER_S, aDesiredTime);
   TrackTicks delta = target - aLastEndTime;
-  LOGFRAME(("NotifyPull, target = %lu, delta = %lu %s", (uint64_t) target, (uint64_t) delta,
-            image ? "" : "<null>"));
-  // NULL images are allowed
-  segment.AppendFrame(image ? image.forget() : nullptr, delta, gfxIntSize(mWidth, mHeight));
-  aSource->AppendToTrack(aID, &(segment));
-  aLastEndTime = target;
+  LOGFRAME(("NotifyPull, desired = %ld, target = %ld, delta = %ld %s", (int64_t) aDesiredTime, 
+            (int64_t) target, (int64_t) delta, image ? "" : "<null>"));
+  // Don't append if we've already provided a frame that supposedly goes past the current aDesiredTime
+  // Doing so means a negative delta and thus messes up handling of the graph
+  if (delta > 0) {
+    // NULL images are allowed
+    segment.AppendFrame(image ? image.forget() : nullptr, delta, gfxIntSize(mWidth, mHeight));
+    aSource->AppendToTrack(aID, &(segment));
+    aLastEndTime = target;
+  }
 }
 
 void
