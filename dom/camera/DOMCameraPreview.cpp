5e8a85dc9e61d8fa16f9edd550fc43f025113ab4
Mike Habicher <mhabicher@mozilla.com>
2012-11-30 18:26:58 -0800

    Bug 809259 - fix for buffer exhaustion when cycling camera modes. r=cjones a=blocking-basecamp

--- dom/camera/DOMCameraPreview.cpp
+++ dom/camera/DOMCameraPreview.cpp
@@ -251,8 +251,6 @@ DOMCameraPreview::StopPreview()
   DOM_CAMERA_LOGI("Stopping preview stream\n");
   mState = STOPPING;
   mCameraControl->StopPreview();
-  mInput->EndTrack(TRACK_VIDEO);
-  mInput->Finish();
 }
 
 void
@@ -260,6 +258,8 @@ DOMCameraPreview::SetStateStopped()
 {
   NS_ASSERTION(NS_IsMainThread(), "SetStateStopped() not called from main thread");
 
+  mInput->EndTrack(TRACK_VIDEO);
+  mInput->Finish();
   mState = STOPPED;
   DOM_CAMERA_LOGI("Preview stream stopped\n");
 
