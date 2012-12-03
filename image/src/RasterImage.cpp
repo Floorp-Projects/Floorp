844dd0599aa10d051f785ea2ef8f406ab11cea6f
Joe Drew <joe@drew.ca>
2012-12-03 18:02:11 -0500

    Bug 804559 - Use correct rounding code instead of rolling our own. r=jrmuizel
    Drew's First Law of Programming: If you write your own rounding code, you will
    be wrong 100% of the time.

--- image/src/RasterImage.cpp
+++ image/src/RasterImage.cpp
@@ -160,8 +160,10 @@ struct ScaleRequest
 
     weakImage = aImage->asWeakPtr();
     srcRect = aSrcFrame->GetRect();
-    dstSize.width = NSToIntRoundUp(srcRect.width * scale.width);
-    dstSize.height = NSToIntRoundUp(srcRect.height * scale.height);
+
+    nsIntRect dstRect = srcRect;
+    dstRect.ScaleRoundOut(scale.width, scale.height);
+    dstSize = dstRect.Size();
   }
 
   // This can only be called on the main thread.
