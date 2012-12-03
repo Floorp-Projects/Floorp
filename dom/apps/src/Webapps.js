1847b2a1b76d71bda4e79aee461f0463ffde8144
Fabrice Desré <fabrice@mozilla.com>
2012-12-03 11:38:09 -0800

    Bug 816149 - When updating hosted apps with appcache, the update is automatically done [r=ferjm]

--- dom/apps/src/Webapps.js
+++ dom/apps/src/Webapps.js
@@ -465,10 +465,20 @@ WebappsApplication.prototype = {
 
   checkForUpdate: function() {
     let request = this.createRequest();
-    cpmm.sendAsyncMessage("Webapps:CheckForUpdate",
-                          { manifestURL: this.manifestURL,
-                            oid: this._id,
-                            requestID: this.getRequestId(request) });
+
+    // We can't update apps that are not removable.
+    if (!this.removable) {
+      Services.tm.currentThread.dispatch({
+        run: function checkUpdateFail() {
+          Services.DOMRequest.fireError(request, "NOT_UPDATABLE");
+        }
+      }, Ci.nsIEventTarget.DISPATCH_NORMAL)
+    } else {
+      cpmm.sendAsyncMessage("Webapps:CheckForUpdate",
+                            { manifestURL: this.manifestURL,
+                              oid: this._id,
+                              requestID: this.getRequestId(request) });
+    }
     return request;
   },
 
