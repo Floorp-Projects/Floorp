950f86b0d6edbc42e95a7511c4316b01c15bc196
Jason Orendorff <jorendorff@mozilla.com>
2012-12-03 17:15:16 -0600

    Bug 744730 - jsdbg2: Fix assertion when processing onPop hook for a frame which the Debugger has forced to return. r=jimb.

---
+++ js/src/jit-test/tests/debug/Frame-onPop-after-debugger-return.js
@@ -0,0 +1,11 @@
+// Bug 744730.
+
+var g = newGlobal('new-compartment');
+var dbg = Debugger(g);
+dbg.onDebuggerStatement = function (f) { return {return: 1234}; };
+var hit = false;
+dbg.onEnterFrame = function (f) {
+    f.onPop = function () { hit = true};
+};
+g.eval("debugger;");
+assertEq(hit, true);
