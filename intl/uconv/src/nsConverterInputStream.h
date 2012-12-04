d1f37a8e6cb9b36be923bad682bd5c66fb22dabf
Trevor Saunders <trev.saunders@gmail.com>
2012-12-04 18:04:39 -0500

    bug 801466 - remove some usage of prmem in intl/ and network/ r=jduell

--- intl/uconv/src/nsConverterInputStream.h
+++ intl/uconv/src/nsConverterInputStream.h
@@ -7,12 +7,13 @@
 #include "nsIConverterInputStream.h"
 #include "nsIUnicharLineInputStream.h"
 #include "nsString.h"
-#include "nsReadLine.h"
 
+#include "nsAutoPtr.h"
 #include "nsCOMPtr.h"
 #include "nsIUnicodeDecoder.h"
 #include "nsIByteBuffer.h"
 #include "nsIUnicharBuffer.h"
+#include "nsReadLine.h"
 
 #define NS_CONVERTERINPUTSTREAM_CONTRACTID "@mozilla.org/intl/converter-input-stream;1"
 
@@ -58,5 +59,5 @@ class nsConverterInputStream : public nsIConverterInputStream,
     uint32_t  mUnicharDataLength;
     PRUnichar mReplacementChar;
 
-    nsLineBuffer<PRUnichar>* mLineBuffer;    
+    nsAutoPtr<nsLineBuffer<PRUnichar> > mLineBuffer;
 };
