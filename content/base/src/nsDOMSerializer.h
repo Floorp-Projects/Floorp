a7b4409ffcada22530a43c43145eea6728c4b1b1
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2012-12-03 20:26:16 -0500

    Bug 816410 - Part 1: Convert XMLSerializer and DOMParser to WebIDL bindings. r=bz

--- content/base/src/nsDOMSerializer.h
+++ content/base/src/nsDOMSerializer.h
@@ -3,21 +3,66 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef nsDOMSerializer_h__
-#define nsDOMSerializer_h__
+#ifndef nsDOMSerializer_h_
+#define nsDOMSerializer_h_
 
 #include "nsIDOMSerializer.h"
+#include "nsWrapperCache.h"
+#include "mozilla/ErrorResult.h"
+#include "mozilla/dom/XMLSerializerBinding.h"
 
-class nsDOMSerializer : public nsIDOMSerializer
+class nsINode;
+class nsIOutputStream;
+
+class nsDOMSerializer MOZ_FINAL : public nsIDOMSerializer,
+                                  public nsWrapperCache
 {
 public:
   nsDOMSerializer();
   virtual ~nsDOMSerializer();
 
-  NS_DECL_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(nsDOMSerializer)
 
   // nsIDOMSerializer
   NS_DECL_NSIDOMSERIALIZER
+
+  // WebIDL API
+  static already_AddRefed<nsDOMSerializer>
+  Constructor(nsISupports* aOwner, mozilla::ErrorResult& rv)
+  {
+    nsRefPtr<nsDOMSerializer> domSerializer = new nsDOMSerializer(aOwner);
+    return domSerializer.forget();
+  }
+
+  void
+  SerializeToString(nsINode& aRoot, nsAString& aStr,
+                    mozilla::ErrorResult& rv);
+
+  void
+  SerializeToStream(nsINode& aRoot, nsIOutputStream* aStream,
+                    const nsAString& aCharset, mozilla::ErrorResult& rv);
+
+  nsISupports* GetParentObject() const
+  {
+    return mOwner;
+  }
+
+  virtual JSObject* WrapObject(JSContext* aCx, JSObject* aScope,
+                               bool* aTriedToWrap) MOZ_OVERRIDE
+  {
+    return mozilla::dom::XMLSerializerBinding::Wrap(aCx, aScope, this,
+                                                    aTriedToWrap);
+  }
+
+private:
+  nsDOMSerializer(nsISupports* aOwner) : mOwner(aOwner)
+  {
+    MOZ_ASSERT(aOwner);
+    SetIsDOMBinding();
+  }
+
+  nsCOMPtr<nsISupports> mOwner;
 };
 
 
--- dom/webidl/WebIDL.mk
+++ dom/webidl/WebIDL.mk
@@ -24,9 +24,10 @@ webidl_files = \
   CSSStyleDeclaration.webidl \
   DelayNode.webidl \
   DOMImplementation.webidl \
-  DOMTokenList.webidl \
+  DOMParser.webidl \
   DOMSettableTokenList.webidl \
   DOMStringMap.webidl \
+  DOMTokenList.webidl \
   DynamicsCompressorNode.webidl \
   Element.webidl \
   EventHandler.webidl \
@@ -61,6 +62,7 @@ webidl_files = \
   XMLHttpRequest.webidl \
   XMLHttpRequestEventTarget.webidl \
   XMLHttpRequestUpload.webidl \
+  XMLSerializer.webidl \
   $(NULL)
 
 ifdef MOZ_WEBGL
