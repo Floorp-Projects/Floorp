ce2b1cea0a6b30c491d758dbd16c53182fdfd1c4
Mike Habicher <mikeh@mozilla.com>
2012-12-04 21:00:39 -0500

    Bug 817141 - Call EndTrack()/Finish() from StopPreview() (as well) to prevent deadlock. r=kchen

--- dom/camera/DOMCameraPreview.cpp
+++ dom/camera/DOMCameraPreview.cpp
@@ -251,6 +251,8 @@ DOMCameraPreview::StopPreview()
   DOM_CAMERA_LOGI("Stopping preview stream\n");
   mState = STOPPING;
   mCameraControl->StopPreview();
+  mInput->EndTrack(TRACK_VIDEO);
+  mInput->Finish();
 }
 
 void
@@ -258,8 +260,11 @@ DOMCameraPreview::SetStateStopped()
 {
   NS_ASSERTION(NS_IsMainThread(), "SetStateStopped() not called from main thread");
 
-  mInput->EndTrack(TRACK_VIDEO);
-  mInput->Finish();
+  // see bug 809259 and bug 817367.
+  if (mState != STOPPING) {
+    mInput->EndTrack(TRACK_VIDEO);
+    mInput->Finish();
+  }
   mState = STOPPED;
   DOM_CAMERA_LOGI("Preview stream stopped\n");
 
