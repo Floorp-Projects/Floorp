975f004b7b1ef4a0696579ce65b9dbdcf6f2ceba
Terrence Cole <terrence@mozilla.com>
2012-12-05 14:21:44 -0800

    Bug 819118 - Use accessor rather than direct script access; r=billm
    This abstracts the requirement to manually cast from HeapPtr to Unrooted.

--- js/src/jsscript.h
+++ js/src/jsscript.h
@@ -10,14 +10,15 @@
 /*
  * JS script descriptor.
  */
-#include "jsprvtd.h"
 #include "jsdbgapi.h"
-#include "jsclist.h"
 #include "jsinfer.h"
 #include "jsopcode.h"
 #include "jsscope.h"
 
 #include "gc/Barrier.h"
+#include "gc/Root.h"
+
+ForwardDeclareJS(Script);
 
 namespace js {
 
