2ff6dbab7ebf90f188b74cc953bc043123c4efc0
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2012-12-05 16:26:33 -0800

    Bug 811111 - Ensure that no GC happen during bailouts. r=terrence

--- js/src/vm/Stack-inl.h
+++ js/src/vm/Stack-inl.h
@@ -384,7 +384,12 @@ STATIC_POSTCONDITION(!return || ubound(from) >= nvals)
 JS_ALWAYS_INLINE bool
 StackSpace::ensureSpace(JSContext *cx, MaybeReportError report, Value *from, ptrdiff_t nvals) const
 {
-    AssertCanGC();
+    mozilla::Maybe<AutoAssertNoGC> maybeNoGC;
+    if (report)
+        AssertCanGC();
+    else
+        maybeNoGC.construct();
+
     assertInvariants();
     JS_ASSERT(from >= firstUnused());
 #ifdef XP_WIN
@@ -412,7 +417,12 @@ JS_ALWAYS_INLINE StackFrame *
 ContextStack::getCallFrame(JSContext *cx, MaybeReportError report, const CallArgs &args,
                            JSFunction *fun, JSScript *script, StackFrame::Flags *flags) const
 {
-    AssertCanGC();
+    mozilla::Maybe<AutoAssertNoGC> maybeNoGC;
+    if (report)
+        AssertCanGC();
+    else
+        maybeNoGC.construct();
+
     JS_ASSERT(fun->nonLazyScript() == script);
     unsigned nformal = fun->nargs;
 
@@ -454,7 +464,12 @@ ContextStack::pushInlineFrame(JSContext *cx, FrameRegs &regs, const CallArgs &ar
                               JSFunction &callee, JSScript *script,
                               InitialFrameFlags initial, MaybeReportError report)
 {
-    AssertCanGC();
+    mozilla::Maybe<AutoAssertNoGC> maybeNoGC;
+    if (report)
+        AssertCanGC();
+    else
+        maybeNoGC.construct();
+
     JS_ASSERT(onTop());
     JS_ASSERT(regs.sp == args.end());
     /* Cannot assert callee == args.callee() since this is called from LeaveTree. */
