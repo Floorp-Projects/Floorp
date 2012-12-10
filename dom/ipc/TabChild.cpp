faff7cd2bf6b02817f38c59621ddf616fd9e5f4c
Anthony Jones <ajones@mozilla.com>
2012-12-10 08:50:24 -0500

    Bug 804808 - Fixed missing reset of zoom. r=cjones

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -284,6 +284,9 @@ TabChild::OnLocationChange(nsIWebProgress* aWebProgress,
     return NS_OK;
   }
 
+  nsCOMPtr<nsIDOMWindowUtils> utils(do_GetInterface(window));
+  utils->SetIsFirstPaint(true);
+
   nsCOMPtr<nsIDOMDocument> progressDoc;
   window->GetDocument(getter_AddRefs(progressDoc));
   if (!progressDoc) {
