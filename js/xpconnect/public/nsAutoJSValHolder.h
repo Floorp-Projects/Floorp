df148e4482aa15e7fb59bb1babf221257aa626c3
Blake Kaplan <mrbkap@gmail.com>
2012-12-10 16:12:49 -0800

    Bug 820185 - Fix the copy constructor to not use uninitialized members. r=bent

--- js/xpconnect/public/nsAutoJSValHolder.h
+++ js/xpconnect/public/nsAutoJSValHolder.h
@@ -18,7 +18,8 @@
 class nsAutoJSValHolder
 {
 public:
-  nsAutoJSValHolder() : mVal(JSVAL_NULL), mRt(nullptr)
+  nsAutoJSValHolder()
+    : mVal(JSVAL_NULL), mRt(nullptr)
   {
     // nothing to do
   }
@@ -30,7 +31,9 @@ public:
     Release();
   }
 
-  nsAutoJSValHolder(const nsAutoJSValHolder& aOther) {
+  nsAutoJSValHolder(const nsAutoJSValHolder& aOther)
+    : mVal(JSVAL_NULL), mRt(nullptr)
+  {
     *this = aOther;
   }
 
