bee7733beb422abfb65c12fb18172d2036b6e434
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2012-12-12 16:00:23 +0900

    Bug 820255 - U+205F (MEDIUM MATHEMATICAL SPACE) and U+1680 (OGHAM SPACE MARK) should break line. r=masayuki

--- intl/lwbrk/tools/anzx4501.pl
+++ intl/lwbrk/tools/anzx4501.pl
@@ -59,6 +59,10 @@ open ( HEADER , "> ../src/jisx4501class.h")
 #
 ######################################################################
 $hthmlheader = <<END_OF_HTML;
+<!-- This Source Code Form is subject to the terms of the Mozilla Public
+   - License, v. 2.0. If a copy of the MPL was not distributed with this
+   - file, You can obtain one at http://mozilla.org/MPL/2.0/. -->
+
 <HTML>
 <HEAD>
 <TITLE>
