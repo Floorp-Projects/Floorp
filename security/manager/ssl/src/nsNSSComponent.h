b2bfb7686748e0d795cab8b7232ea0fb820123ea
Benjamin Smedberg <benjamin@smedbergs.us>
2012-12-13 12:20:00 -0500

    Bug 807757 part PSM - Remove nsIProfileChangeStatus and related veto notifications from PSM r=bsmith

--- security/manager/ssl/src/nsNSSComponent.h
+++ security/manager/ssl/src/nsNSSComponent.h
@@ -267,7 +267,7 @@ public:
 private:
 
   nsresult InitializeNSS(bool showWarningBox);
-  nsresult ShutdownNSS();
+  void ShutdownNSS();
 
 #ifdef XP_MACOSX
   void TryCFM2MachOMigration(nsIFile *cfmPath, nsIFile *machoPath);
@@ -291,7 +291,6 @@ private:
 
   // Methods that we use to handle the profile change notifications (and to
   // synthesize a full profile change when we're just doing a profile startup):
-  void DoProfileApproveChange(nsISupports* aSubject);
   void DoProfileChangeNetTeardown();
   void DoProfileChangeTeardown(nsISupports* aSubject);
   void DoProfileBeforeChange(nsISupports* aSubject);
