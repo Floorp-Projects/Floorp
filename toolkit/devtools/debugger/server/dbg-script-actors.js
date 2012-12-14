ff511f6cf11b08a569e3d542bd0bf6d0065f2598
Jim Blandy <jimb@mozilla.com>
2012-12-14 10:26:47 -0800

    Bug 819049: JS debugging protocol: drop 'calleeName' and 'functionName' properties from frame and environment forms. r=past
    Object grips for functions now include the function's name directly, so the
    'callee' and 'function' properties of those forms already provide this
    information. A prior patch in the series changed the users to consult the
    grip, not these additional properties.

--- toolkit/devtools/debugger/server/dbg-script-actors.js
+++ toolkit/devtools/debugger/server/dbg-script-actors.js
@@ -1850,7 +1850,6 @@ FrameActor.prototype = {
                  type: this.frame.type };
     if (this.frame.type === "call") {
       form.callee = this.threadActor.createValueGrip(this.frame.callee);
-      form.calleeName = getFunctionName(this.frame.callee);
     }
 
     let envActor = this.threadActor
@@ -2042,7 +2041,6 @@ EnvironmentActor.prototype = {
       if (aObject.callee) {
         form.type = "function";
         form.function = this.threadActor.createValueGrip(aObject.callee);
-        form.functionName = getFunctionName(aObject.callee);
       } else {
         form.type = "block";
       }
@@ -2194,30 +2192,6 @@ EnvironmentActor.prototype.requestTypes = {
   "assign": EnvironmentActor.prototype.onAssign,
   "bindings": EnvironmentActor.prototype.onBindings
 };
-
-/**
- * Helper function to deduce the name of the provided function.
- *
- * @param Debugger.Object aFunction
- *        The function whose name will be returned.
- */
-function getFunctionName(aFunction) {
-  let name;
-  if (aFunction.name) {
-    name = aFunction.name;
-  } else {
-    // Check if the developer has added a de-facto standard displayName
-    // property for us to use.
-    let desc = aFunction.getOwnPropertyDescriptor("displayName");
-    if (desc && desc.value && typeof desc.value == "string") {
-      name = desc.value;
-    } else {
-      // Otherwise use SpiderMonkey's inferred name.
-      name = aFunction.displayName;
-    }
-  }
-  return name;
-}
 
 /**
  * Override the toString method in order to get more meaningful script output
