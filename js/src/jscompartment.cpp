362887a5b50b75a5b639b5adb6e9a2467973ecc4
Jason Orendorff <jorendorff@mozilla.com>
2012-12-14 14:27:22 -0600

    Bug 820180 - Isolate JS pseudorandom number generator state per compartment. r=luke.

--- js/src/jscompartment.cpp
+++ js/src/jscompartment.cpp
@@ -78,6 +78,7 @@ JSCompartment::JSCompartment(JSRuntime *rt)
     gcGrayRoots(),
     gcMallocBytes(0),
     debugModeBits(rt->debugMode ? DebugFromC : 0),
+    rngState(0),
     watchpointMap(NULL),
     scriptCountsMap(NULL),
     debugScriptMap(NULL),
@@ -125,6 +126,9 @@ JSCompartment::init(JSContext *cx)
 
     if (!regExps.init(cx))
         return false;
+
+    if (cx)
+        InitRandom(cx->runtime, &rngState);
 
 #ifdef JSGC_GENERATIONAL
     /*
