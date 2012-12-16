41d61d9db0704ee7ddf9a4eb37e1f328e0261791
Mark Finkle <mfinkle@mozilla.com>
2012-12-15 23:55:03 -0500

    Bug 820658 - Private Browsing leaks history in SQLite's Write-Ahead Log for tabs.db r=bnicholson

--- mobile/android/base/TabsAccessor.java
+++ mobile/android/base/TabsAccessor.java
@@ -190,9 +190,9 @@ public final class TabsAccessor {
 
         int position = 0;
         for (Tab tab : tabs) {
-            // Skip this tab if it has a null URL.
+            // Skip this tab if it has a null URL or is in private browsing mode
             String url = tab.getURL();
-            if (url == null)
+            if (url == null || tab.isPrivate())
                 continue;
 
             ContentValues values = new ContentValues();
