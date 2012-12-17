d627a970a4bb459e57effee771545595f317577b
Neil Deakin <enndeakin@gmail.com>
2012-12-17 14:55:37 +0100

    Bug 815691 - The panel's context menu doesn't change after clicking on a different item r=jimm

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -8024,7 +8024,7 @@ nsWindow::DealWithPopups(HWND inWnd, UINT inMsg, WPARAM inWParam, LPARAM inLPara
         } // foreach parent menu widget
       }
 
-      if (inMsg == WM_MOUSEACTIVATE && popupsToRollup == UINT32_MAX) {
+      if (inMsg == WM_MOUSEACTIVATE) {
         // Prevent the click inside the popup from causing a change in window
         // activation. Since the popup is shown non-activated, we need to eat
         // any requests to activate the window while it is displayed. Windows
