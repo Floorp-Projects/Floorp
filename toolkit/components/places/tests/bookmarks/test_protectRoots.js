81057443373a2e8357855a01b3bb4f3dc2b92586
Marco Bonardo <mbonardo@mozilla.com>
2012-12-17 15:03:49 +0100

    Bug 791562 - crash in PlacesFolderConversion::AppendFolder. r=mano

---
+++ toolkit/components/places/tests/bookmarks/test_protectRoots.js
@@ -0,0 +1,36 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+function run_test()
+{
+  const ROOTS = [
+    PlacesUtils.bookmarksMenuFolderId,
+    PlacesUtils.toolbarFolderId,
+    PlacesUtils.unfiledBookmarksFolderId,
+    PlacesUtils.tagsFolderId,
+    PlacesUtils.placesRootId
+  ];
+
+  for (let root of ROOTS) {
+    do_check_true(PlacesUtils.isRootItem(root));
+
+    try {
+      PlacesUtils.bookmarks.removeItem(root);
+      do_throw("Trying to remove a root should throw");
+    } catch (ex) {}
+
+    try {
+      PlacesUtils.bookmarks.moveItem(root, PlacesUtils.placesRootId, 0);
+      do_throw("Trying to move a root should throw");
+    } catch (ex) {}
+
+    try {
+      PlacesUtils.bookmarks.removeFolderChildren(root);
+      if (root == PlacesUtils.placesRootId)
+        do_throw("Trying to remove children of the main root should throw");
+    } catch (ex) {
+      if (root != PlacesUtils.placesRootId)
+        do_throw("Trying to remove children of other roots should not throw");
+    }
+  }
+}
