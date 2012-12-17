e0cc5028fb3a624af9c5b224faf3c774c83e231a
Kartikaya Gupta <kgupta@mozilla.com>
2012-12-16 19:16:26 -0500

    Bug 818060 - Add a memory reporter for graphics textures. r=BenWa,njn

--- gfx/gl/GLContext.h
+++ gfx/gl/GLContext.h
@@ -2914,6 +2914,20 @@ public:
     nsTArray<NamedResource> mTrackedBuffers;
 #endif
 
+public:
+    enum MemoryUse {
+        // when memory being allocated is reported to a memory reporter
+        MemoryAllocated,
+        // when memory being freed is reported to a memory reporter
+        MemoryFreed
+    };
+
+    // When memory is used/freed for tile textures, call this method
+    // to update the value reported by the memory reporter.
+    static void UpdateTextureMemoryUsage(MemoryUse action,
+                                         GLenum format,
+                                         GLenum type,
+                                         uint16_t tileSize);
 };
 
 inline bool
@@ -3107,6 +3121,8 @@ protected:
         mGL->BindUserFBO(mOldState);
     }
 };
+
+uint32_t GetBitsPerTexel(GLenum format, GLenum type);
 
 } /* namespace gl */
 } /* namespace mozilla */
