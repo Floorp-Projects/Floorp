ba6066a0fe13d952c5e90be86c22c0bd39ec3c3b
Rafael Ávila de Espíndola <respindola@mozilla.com>
2012-12-18 16:22:38 -0500

    Bug 822464 - Upgrade clang to 170377 in the 3.2 branch. r=rail. Update the build script.

--- build/unix/build-clang/build-clang.py
+++ build/unix/build-clang/build-clang.py
@@ -3,7 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-llvm_revision = "169730"
+llvm_revision = "170377"
 moz_version = "moz0"
 
 ##############################################
