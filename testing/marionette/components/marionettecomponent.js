405e9c3c0c91fd306afb9767149eb9f3039da76f
Ed Morley <emorley@mozilla.com>
2012-12-19 01:23:00 +0000

    Backout 22f9cf3cc575 (bug 820463) for breaking marionette on a CLOSED TREE

--- testing/marionette/components/marionettecomponent.js
+++ testing/marionette/components/marionettecomponent.js
@@ -8,6 +8,7 @@ const MARIONETTE_CONTRACTID = "@mozilla.org/marionette;1";
 const MARIONETTE_CID = Components.ID("{786a1369-dca5-4adc-8486-33d23c88010a}");
 const DEBUGGER_ENABLED_PREF = 'devtools.debugger.remote-enabled';
 const MARIONETTE_ENABLED_PREF = 'marionette.defaultPrefs.enabled';
+const MARIONETTE_LOADEARLY_PREF = 'marionette.loadearly';
 const DEBUGGER_FORCELOCAL_PREF = 'devtools.debugger.force-local';
 const MARIONETTE_FORCELOCAL_PREF = 'marionette.force-local';
 
@@ -56,23 +57,30 @@ MarionetteComponent.prototype = {
       case "profile-after-change":
         let appName = Services.appinfo.name;
         let enabled = false;
+        let loadearly = appName == 'B2G' ? false : true;
         try {
           enabled = Services.prefs.getBoolPref(MARIONETTE_ENABLED_PREF);
+          loadearly = Services.prefs.getBoolPref(MARIONETTE_LOADEARLY_PREF);
         } catch(e) {}
         if (enabled) {
-          this.logger.info("marionette enabled");
+          this.logger.info("marionette enabled, loadearly: " + loadearly);
 
           //add observers
-          observerService.addObserver(this, "final-ui-startup", false);
+          if (loadearly) {
+            observerService.addObserver(this, "final-ui-startup", false);
+          }
+          else {
+            observerService.addObserver(this, "system-message-listener-ready", false);
+          }
           observerService.addObserver(this, "xpcom-shutdown", false);
         }
         else {
           this.logger.info("marionette not enabled");
         }
         break;
-      case "final-ui-startup":
-        this.logger.info("marionette initializing at " + aTopic);
-        observerService.removeObserver(this, aTopic);
+      case "system-message-listener-ready":
+        this.logger.info("marionette initializing at system-message-listener-ready");
+        observerService.removeObserver(this, "system-message-listener-ready");
 
         try {
           this.original_forcelocal = Services.prefs.getBoolPref(DEBUGGER_FORCELOCAL_PREF);
