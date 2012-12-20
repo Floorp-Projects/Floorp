1f7c60632473d4121b8b687a02d26a3b8d009a68
Ehsan Akhgari <ehsan@mozilla.com>
2012-12-20 17:43:13 -0500

    Bug 823725 - Remove the Svc.Private getter since it is no longer used; r=rnewman

--- services/sync/modules/util.js
+++ services/sync/modules/util.js
@@ -551,16 +551,6 @@ let _sessionCID = Services.appinfo.ID == SEAMONKEY_ID ?
   XPCOMUtils.defineLazyServiceGetter(Svc, name, contract, iface);
 });
 
-// nsIPrivateBrowsingService is not implemented in mobile Firefox.
-// Svc.Private should just return undefined in this case instead of throwing.
-XPCOMUtils.defineLazyGetter(Svc, "Private", function() {
-  try {
-    return Cc["@mozilla.org/privatebrowsing;1"].getService(Ci["nsIPrivateBrowsingService"]);
-  } catch (e) {
-    return undefined;
-  }
-});
-
 Svc.__defineGetter__("Crypto", function() {
   let cryptoSvc;
   let ns = {};
