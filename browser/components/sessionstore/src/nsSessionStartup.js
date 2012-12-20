26c9b3ed49689c743c9e887bfe9896f54c99d1ba
Ehsan Akhgari <ehsan@mozilla.com>
2012-12-20 17:50:35 -0500

    Bug 823732 - Remove the usage of the global PB service from nsSessionStartup.js in per-window PB builds; r=jdm

--- browser/components/sessionstore/src/nsSessionStartup.js
+++ browser/components/sessionstore/src/nsSessionStartup.js
@@ -73,11 +73,15 @@ SessionStartup.prototype = {
   init: function sss_init() {
     debug("init starting");
     // do not need to initialize anything in auto-started private browsing sessions
+    if (PrivateBrowsingUtils.permanentPrivateBrowsing)
+      return;
+
+#ifndef MOZ_PER_WINDOW_PRIVATE_BROWSING
     let pbs = Cc["@mozilla.org/privatebrowsing;1"].
               getService(Ci.nsIPrivateBrowsingService);
-    if (PrivateBrowsingUtils.permanentPrivateBrowsing ||
-        pbs.lastChangedByCommandLine)
+    if (pbs.lastChangedByCommandLine)
       return;
+#endif
     // Session state is unknown until we read the file.
     this._sessionType = null;
     _SessionFile.read().then(
