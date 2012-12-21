f681aaffe96323b98c522f768106453b9af681b8
Bobby Holley <bobbyholley@gmail.com>
2012-12-20 22:33:26 -0800

    Bug 809652 - Deny nativeCall for SecurityWrapper except under specific circumstances. r=jorendorff

--- js/src/jswrapper.cpp
+++ js/src/jswrapper.cpp
@@ -795,11 +795,8 @@ SecurityWrapper<Base>::enter(JSContext *cx, JSObject *wrapper, jsid id,
 SecurityWrapper<Base>::nativeCall(JSContext *cx, IsAcceptableThis test, NativeImpl impl,
                                   CallArgs args)
 {
-    /*
-     * Let this through until compartment-per-global lets us have stronger
-     * invariants wrt document.domain (bug 714547).
-     */
-    return Base::nativeCall(cx, test, impl, args);
+    JS_ReportErrorNumber(cx, js_GetErrorMessage, NULL, JSMSG_UNWRAP_DENIED);
+    return false;
 }
 
 template <class Base>
