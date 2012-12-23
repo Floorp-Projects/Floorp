2604bb971b016d7d81fe8b6d20e3fcdb1efe297c
David Zbarsky <dzbarsky@gmail.com>
2012-12-23 03:22:22 -0500

    Bug 823394 Part 1:Merge nsSVGStylableElement/nsSVGElement and nsIDOMSVGStylable/nsIDOMSVGElement r=bz

--- content/svg/content/src/nsSVGElement.h
+++ content/svg/content/src/nsSVGElement.h
@@ -20,6 +20,7 @@
 #include "mozilla/dom/Element.h"
 #include "nsISupportsImpl.h"
 #include "nsStyledElement.h"
+#include "nsSVGClass.h"
 
 class nsIDOMSVGElement;
 class nsIDOMSVGSVGElement;
@@ -74,6 +75,9 @@ public:
   // nsISupports
   NS_DECL_ISUPPORTS_INHERITED
 
+  virtual const nsAttrValue* DoGetClasses() const;
+  void DidAnimateClass();
+
   // nsIContent interface methods
 
   virtual nsresult BindToTree(nsIDocument* aDocument, nsIContent* aParent,
@@ -114,6 +118,9 @@ public:
   NS_IMETHOD SetId(const nsAString & aId);
   NS_IMETHOD GetOwnerSVGElement(nsIDOMSVGSVGElement** aOwnerSVGElement);
   NS_IMETHOD GetViewportElement(nsIDOMSVGElement** aViewportElement);
+  NS_IMETHOD GetClassName(nsIDOMSVGAnimatedString** aClassName);
+  NS_IMETHOD GetStyle(nsIDOMCSSStyleDeclaration** aStyle);
+  NS_IMETHOD GetPresentationAttribute(const nsAString& aName, nsIDOMCSSValue** aReturn);
 
   // Gets the element that establishes the rectangular viewport against which
   // we should resolve percentage lengths (our "coordinate context"). Returns
@@ -597,6 +604,8 @@ private:
   void UnsetAttrInternal(int32_t aNameSpaceID, nsIAtom* aAttribute,
                          bool aNotify);
 
+  nsSVGClass mClassAttribute;
+  nsAutoPtr<nsAttrValue> mClassAnimAttr;
   nsRefPtr<mozilla::css::StyleRule> mContentStyleRule;
 };
 
