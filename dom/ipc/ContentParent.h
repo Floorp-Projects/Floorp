0cf409d7fe91797a0bd172722f778e0aaa748461
Josh Matthews <josh@joshmatthews.net>
2012-12-24 15:14:34 -0500

    Bug 823789 - Pass enableHighAccuracy geolocation changes to the chrome process. r=dougt

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -288,6 +288,7 @@ private:
 
     virtual bool RecvAddGeolocationListener();
     virtual bool RecvRemoveGeolocationListener();
+    virtual bool RecvSetGeolocationHigherAccuracy(const bool& aEnable);
 
     virtual bool RecvConsoleMessage(const nsString& aMessage);
     virtual bool RecvScriptError(const nsString& aMessage,
