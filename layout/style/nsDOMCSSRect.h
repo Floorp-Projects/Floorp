d8b11604209f998b33038b98305d147337c6adec
Trevor Saunders <trev.saunders@gmail.com>
2012-12-25 22:12:15 -0500

    bug 824970 - webidl api for Rect r=bz

--- layout/style/nsDOMCSSRect.h
+++ layout/style/nsDOMCSSRect.h
@@ -31,6 +31,11 @@ public:
 
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(nsDOMCSSRect)
 
+  nsROCSSPrimitiveValue* Top() const { return mTop; }
+  nsROCSSPrimitiveValue* Right() const { return mRight; }
+  nsROCSSPrimitiveValue* Bottom() const { return mBottom; }
+  nsROCSSPrimitiveValue* Left() const { return mLeft; }
+
 private:
   nsRefPtr<nsROCSSPrimitiveValue> mTop;
   nsRefPtr<nsROCSSPrimitiveValue> mRight;
