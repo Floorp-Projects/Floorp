8e2d296f3602bb2176af993c2d5cbfd0ccb5635f
Mark Hammond <mhammond@skippinet.com.au>
2012-12-27 18:28:49 +1100

    Bug 806037 - use nsIPrincipal for origin checks.  r=gavin

--- toolkit/components/social/test/browser/browser_notifications.js
+++ toolkit/components/social/test/browser/browser_notifications.js
@@ -6,70 +6,6 @@ const TEST_PROVIDER_ORIGIN = 'http://example.com';
 
 Cu.import("resource://gre/modules/Services.jsm");
 
-// A mock notifications server.  Based on:
-// dom/tests/mochitest/notification/notification_common.js
-const FAKE_CID = Cc["@mozilla.org/uuid-generator;1"].
-    getService(Ci.nsIUUIDGenerator).generateUUID();
-
-const ALERTS_SERVICE_CONTRACT_ID = "@mozilla.org/alerts-service;1";
-const ALERTS_SERVICE_CID = Components.ID(Cc[ALERTS_SERVICE_CONTRACT_ID].number);
-
-function MockAlertsService() {}
-
-MockAlertsService.prototype = {
-
-    showAlertNotification: function(imageUrl, title, text, textClickable,
-                                    cookie, alertListener, name) {
-        let obData = JSON.stringify({
-          imageUrl: imageUrl,
-          title: title,
-          text:text,
-          textClickable: textClickable,
-          cookie: cookie,
-          name: name
-        });
-        Services.obs.notifyObservers(null, "social-test:notification-alert", obData);
-
-        if (textClickable) {
-          // probably should do this async....
-          alertListener.observe(null, "alertclickcallback", cookie);
-        }
-
-        alertListener.observe(null, "alertfinished", cookie);
-    },
-
-    QueryInterface: function(aIID) {
-        if (aIID.equals(Ci.nsISupports) ||
-            aIID.equals(Ci.nsIAlertsService))
-            return this;
-        throw Cr.NS_ERROR_NO_INTERFACE;
-    }
-};
-
-var factory = {
-    createInstance: function(aOuter, aIID) {
-        if (aOuter != null)
-            throw Cr.NS_ERROR_NO_AGGREGATION;
-        return new MockAlertsService().QueryInterface(aIID);
-    }
-};
-
-function replacePromptService() {
-  Components.manager.QueryInterface(Ci.nsIComponentRegistrar)
-            .registerFactory(FAKE_CID, "",
-                             ALERTS_SERVICE_CONTRACT_ID,
-                             factory)
-}
-
-function restorePromptService() {
-  Components.manager.QueryInterface(Ci.nsIComponentRegistrar)
-            .registerFactory(ALERTS_SERVICE_CID, "",
-                             ALERTS_SERVICE_CONTRACT_ID,
-                             null);
-}
-// end of alerts service mock.
-
-
 function ensureProvider(workerFunction, cb) {
   let manifest = {
     origin: TEST_PROVIDER_ORIGIN,
@@ -90,8 +26,8 @@ function test() {
   let cbPostTest = function(cb) {
     SocialService.removeProvider(TEST_PROVIDER_ORIGIN, function() {cb()});
   };
-  replacePromptService();
-  registerCleanupFunction(restorePromptService);
+  replaceAlertsService();
+  registerCleanupFunction(restoreAlertsService);
   runTests(tests, undefined, cbPostTest);
 }
 
--- toolkit/components/social/test/browser/worker_social.js
+++ toolkit/components/social/test/browser/worker_social.js
@@ -42,6 +42,11 @@ onconnect = function(e) {
         // start
         apiPort.postMessage({topic: 'social.reload-worker'});
         break;
+      case "test-notification-create":
+        apiPort.postMessage({topic: 'social.notification-create',
+                             data: data});
+        testerPort.postMessage({topic: 'did-notification-create'});
+        break;
     }
   }
   // used for "test-reload-worker"
