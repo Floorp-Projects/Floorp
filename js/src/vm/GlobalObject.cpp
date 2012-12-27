d112a662b6cc0240a1c3e9aac4a26758e0b7849c
Jeff Walden <jwalden@mit.edu>
2012-12-27 11:20:22 -0600

    Add a JSObject::addDataProperty overload that takes Handle<PropertyName*>, to preserve the name/element distinction further when possible, anticipating future property-splitting work.  No bug, r=sparky
    --HG--
    extra : rebase_source : 2fe4e9819bf8ab23b163889411b5d1fa74d59597

--- js/src/vm/GlobalObject.cpp
+++ js/src/vm/GlobalObject.cpp
@@ -354,9 +354,9 @@ GlobalObject::initFunctionAndObjectClasses(JSContext *cx)
     }
 
     /* Add the global Function and Object properties now. */
-    if (!self->addDataProperty(cx, NameToId(cx->names().Object), JSProto_Object + JSProto_LIMIT * 2, 0))
+    if (!self->addDataProperty(cx, cx->names().Object, JSProto_Object + JSProto_LIMIT * 2, 0))
         return NULL;
-    if (!self->addDataProperty(cx, NameToId(cx->names().Function), JSProto_Function + JSProto_LIMIT * 2, 0))
+    if (!self->addDataProperty(cx, cx->names().Function, JSProto_Function + JSProto_LIMIT * 2, 0))
         return NULL;
 
     /* Heavy lifting done, but lingering tasks remain. */
