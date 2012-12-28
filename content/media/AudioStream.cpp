e4c13a1319a996e741a9593c44e7ed7091ea980f
Paul Adenot <paul@paul.cx>
2012-12-28 23:01:38 +0100

    Bug 825329 - Properly implement AudioStream::GetPlaybackRate. r=kinetik

--- content/media/AudioStream.cpp
+++ content/media/AudioStream.cpp
@@ -1112,7 +1112,6 @@ AudioClock::AudioClock(AudioStream* aStream)
   mOutRate(0),
   mInRate(0),
   mPreservesPitch(true),
-  mPlaybackRate(1.0),
   mCompensatingLatency(false)
 {}
 
@@ -1120,7 +1119,6 @@ void AudioClock::Init()
 {
   mOutRate = mAudioStream->GetRate();
   mInRate = mAudioStream->GetRate();
-  mPlaybackRate = 1.0;
   mOldOutRate = mOutRate;
 }
 
@@ -1190,7 +1188,7 @@ void AudioClock::SetPlaybackRate(double aPlaybackRate)
 
 double AudioClock::GetPlaybackRate()
 {
-  return mPlaybackRate;
+  return static_cast<double>(mOutRate) / mInRate;
 }
 
 void AudioClock::SetPreservesPitch(bool aPreservesPitch)
