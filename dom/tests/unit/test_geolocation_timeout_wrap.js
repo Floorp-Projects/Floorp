37291317df10c4c295c499d10681f050d5462434
Josh Matthews <josh@joshmatthews.net>
2012-12-28 02:21:41 -0500

    Backed out changeset 3114517c141a (bug 823789)

--- dom/tests/unit/test_geolocation_timeout_wrap.js
+++
@@ -1,8 +0,0 @@
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-
-function run_test() {
-  var prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
-  prefs.setCharPref("geo.wifi.uri", "http://localhost:4444/geo");
-  run_test_in_child("./test_geolocation_timeout.js");
-}
