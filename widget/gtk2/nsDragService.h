4f6d999bf32d3255b60a5279a8206e84f225d059
Nicholas Nethercote <nnethercote@mozilla.com>
2013-01-03 14:36:53 -0800

    Bug 826158 - Fix two -Wself-assign warnings in widget/gtk2/.  r=karlt.
    --HG--
    extra : rebase_source : cfbd0c5807e7b067a881583c6ac9f48b2848c021

--- widget/gtk2/nsDragService.h
+++ widget/gtk2/nsDragService.h
@@ -1,6 +1,5 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/* vim:expandtab:shiftwidth=4:tabstop=4:
- */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=4 et sw=4 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
