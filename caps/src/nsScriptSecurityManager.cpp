4030d50d1b6e306daa46257c94f092a596e9db7b
Nicholas Nethercote <nnethercote@mozilla.com>
2013-01-02 21:47:50 -0800

    Bug 826154 - Remove unused field nsScriptSecurityManager::mIsWritingPrefs.  r=bz.
    --HG--
    extra : rebase_source : f7235787393fd6b4212aff0bb13e507bb0fd6d94

--- caps/src/nsScriptSecurityManager.cpp
+++ caps/src/nsScriptSecurityManager.cpp
@@ -1,4 +1,5 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=4 et sw=4 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -2440,7 +2441,6 @@ nsScriptSecurityManager::nsScriptSecurityManager(void)
       mCapabilities(nullptr),
       mPrefInitialized(false),
       mIsJavaScriptEnabled(false),
-      mIsWritingPrefs(false),
       mPolicyPrefsChanged(true)
 {
     MOZ_STATIC_ASSERT(sizeof(intptr_t) == sizeof(void*),
