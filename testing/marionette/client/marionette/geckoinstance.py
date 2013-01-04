144c469df0295a7efd15b2c2d961d615a0e04e23
Jonathan Griffin <jgriffin@mozilla.com>
2013-01-04 10:41:34 -0800

    Bug 826111 - Support mochitests on b2g desktop build, r=ahal

--- testing/marionette/client/marionette/geckoinstance.py
+++ testing/marionette/client/marionette/geckoinstance.py
@@ -2,9 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import datetime
-import socket
-import time
 from mozrunner import Runner
 
 
@@ -31,23 +28,3 @@ class GeckoInstance(object):
     def close(self):
         self.runner.stop()
         self.runner.cleanup()
-
-    def wait_for_port(self, timeout=3000):
-        assert(self.marionette_port)
-        starttime = datetime.datetime.now()
-        while datetime.datetime.now() - starttime < datetime.timedelta(seconds=timeout):
-            try:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.connect((self.marionette_host, self.marionette_port))
-                data = sock.recv(16)
-                print "closing socket"
-                sock.close()
-                if '"from"' in data:
-                    print "got data"
-                    time.sleep(5)
-                    return True
-            except:
-                import traceback
-                print traceback.format_exc()
-            time.sleep(1)
-        return False
