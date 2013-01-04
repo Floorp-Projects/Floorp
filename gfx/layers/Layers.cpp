b64cf566754a910434eb4c1d206a290dcbff9b83
Avi Halachmi <avihpit@yahoo.com>
2013-01-03 21:53:15 -0500

    Bug 820167 - Tab open/close animation prints performance data to error console when enabled. r=jmuizelaar,dao

--- gfx/layers/Layers.cpp
+++ gfx/layers/Layers.cpp
@@ -913,6 +913,15 @@ void
 LayerManager::StartFrameTimeRecording()
 {
   mLastFrameTime = TimeStamp::Now();
+  mPaintStartTime = mLastFrameTime;
+}
+
+void
+LayerManager::SetPaintStartTime(TimeStamp& aTime)
+{
+  if (!mLastFrameTime.IsNull()) {
+    mPaintStartTime = aTime;
+  }
 }
 
 void
@@ -920,7 +929,8 @@ LayerManager::PostPresent()
 {
   if (!mLastFrameTime.IsNull()) {
     TimeStamp now = TimeStamp::Now();
-    mFrameTimes.AppendElement((now - mLastFrameTime).ToMilliseconds());
+    mFrameIntervals.AppendElement((now - mLastFrameTime).ToMilliseconds());
+    mPaintTimes.AppendElement((now - mPaintStartTime).ToMilliseconds());
     mLastFrameTime = now;
   }
   if (!mTabSwitchStart.IsNull()) {
@@ -931,11 +941,13 @@ LayerManager::PostPresent()
 }
 
 void
-LayerManager::StopFrameTimeRecording(nsTArray<float>& aTimes)
+LayerManager::StopFrameTimeRecording(nsTArray<float>& aFrameIntervals, nsTArray<float>& aPaintTimes)
 {
   mLastFrameTime = TimeStamp();
-  aTimes.SwapElements(mFrameTimes);
-  mFrameTimes.Clear();
+  aFrameIntervals.SwapElements(mFrameIntervals);
+  aPaintTimes.SwapElements(mPaintTimes);
+  mFrameIntervals.Clear();
+  mPaintTimes.Clear();
 }
 
 void
