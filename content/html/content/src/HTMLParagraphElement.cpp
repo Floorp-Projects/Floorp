e84fc78ac44e882a810283209389e0194c3fa9b1
Boris Zbarsky <bzbarsky@mit.edu>
2013-01-04 12:02:14 -0500

    Bug 824823 part 10.  Rename nsHTMLParagraphElement to mozilla::dom::HTMLParagraphElement and move the class declaration into a header.  r=peterv

---
+++ content/html/content/src/HTMLParagraphElement.cpp
@@ -0,0 +1,78 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/HTMLParagraphElement.h"
+
+#include "nsStyleConsts.h"
+#include "nsMappedAttributes.h"
+#include "nsRuleData.h"
+
+
+NS_IMPL_NS_NEW_HTML_ELEMENT(Paragraph)
+DOMCI_NODE_DATA(HTMLParagraphElement, mozilla::dom::HTMLParagraphElement)
+
+namespace mozilla {
+namespace dom {
+
+HTMLParagraphElement::~HTMLParagraphElement()
+{
+}
+
+NS_IMPL_ADDREF_INHERITED(HTMLParagraphElement, Element)
+NS_IMPL_RELEASE_INHERITED(HTMLParagraphElement, Element)
+
+// QueryInterface implementation for nsHTMLParagraphElement
+NS_INTERFACE_TABLE_HEAD(HTMLParagraphElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE1(HTMLParagraphElement,
+                                   nsIDOMHTMLParagraphElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE_TO_MAP_SEGUE(HTMLParagraphElement,
+                                               nsGenericHTMLElement)
+NS_HTML_CONTENT_INTERFACE_TABLE_TAIL_CLASSINFO(HTMLParagraphElement)
+
+NS_IMPL_ELEMENT_CLONE(HTMLParagraphElement)
+
+NS_IMPL_STRING_ATTR(HTMLParagraphElement, Align, align)
+
+bool
+HTMLParagraphElement::ParseAttribute(int32_t aNamespaceID,
+                                     nsIAtom* aAttribute,
+                                     const nsAString& aValue,
+                                     nsAttrValue& aResult)
+{
+  if (aAttribute == nsGkAtoms::align && aNamespaceID == kNameSpaceID_None) {
+    return ParseDivAlignValue(aValue, aResult);
+  }
+
+  return nsGenericHTMLElement::ParseAttribute(aNamespaceID, aAttribute, aValue,
+                                              aResult);
+}
+
+static void
+MapAttributesIntoRule(const nsMappedAttributes* aAttributes, nsRuleData* aData)
+{
+  nsGenericHTMLElement::MapDivAlignAttributeInto(aAttributes, aData);
+  nsGenericHTMLElement::MapCommonAttributesInto(aAttributes, aData);
+}
+
+NS_IMETHODIMP_(bool)
+HTMLParagraphElement::IsAttributeMapped(const nsIAtom* aAttribute) const
+{
+  static const MappedAttributeEntry* const map[] = {
+    sDivAlignAttributeMap,
+    sCommonAttributeMap,
+  };
+
+  return FindAttributeDependence(aAttribute, map);
+}
+
+
+nsMapRuleToAttributesFunc
+HTMLParagraphElement::GetAttributeMappingFunction() const
+{
+  return &MapAttributesIntoRule;
+}
+
+} // namespace dom
+} // namespace mozilla
