4e1b03c87f43a067dddc9202ad43b1562154febe
Paolo Amadini <paolo.mozmail@amadzone.org>
2013-01-05 11:44:34 +0100

    Bug 812813 - The Downloads Panel needs to draw your attention when a download finishes.

--- browser/components/downloads/content/indicator.js
+++ browser/components/downloads/content/indicator.js
@@ -349,8 +349,11 @@ const DownloadsIndicatorView = {
   /**
    * If the status indicator is visible in its assigned position, shows for a
    * brief time a visual notification of a relevant event, like a new download.
+   *
+   * @param aType
+   *        Set to "start" for new downloads, "finish" for completed downloads.
    */
-  showEventNotification: function DIV_showEventNotification()
+  showEventNotification: function DIV_showEventNotification(aType)
   {
     if (!this._initialized) {
       return;
@@ -366,7 +369,7 @@ const DownloadsIndicatorView = {
       DownloadsButton.updatePosition();
 
       let indicator = this.indicator;
-      indicator.setAttribute("notification", "true");
+      indicator.setAttribute("notification", aType);
       this._notificationTimeout = setTimeout(
         function () indicator.removeAttribute("notification"), 1000);
     }
@@ -486,7 +489,7 @@ const DownloadsIndicatorView = {
     if (this._attention != aValue) {
       this._attention = aValue;
       if (aValue) {
-        this.indicator.setAttribute("attention", "true")
+        this.indicator.setAttribute("attention", "true");
       } else {
         this.indicator.removeAttribute("attention");
       }
