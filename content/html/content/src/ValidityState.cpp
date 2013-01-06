473c4f1b00d9dadd1c5d39ef142b1750c554eb5a
Ehsan Akhgari <ehsan@mozilla.com>
2013-01-06 14:20:02 -0500

    Bug 827158 - Part 2: Rename nsDOMValidityState to mozilla::dom::ValidityState; r=bzbarsky
    --HG--
    rename : content/html/content/src/nsDOMValidityState.cpp => content/html/content/src/ValidityState.cpp
    rename : content/html/content/src/nsDOMValidityState.h => content/html/content/src/ValidityState.h
    extra : rebase_source : 47640d8fd5e847af646949335f0fe19722101f47

--- content/html/content/src/ValidityState.cpp
+++ content/html/content/src/ValidityState.cpp
@@ -3,87 +3,93 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "nsDOMValidityState.h"
+#include "mozilla/dom/ValidityState.h"
 
 #include "nsDOMClassInfoID.h"
 
 
-DOMCI_DATA(ValidityState, nsDOMValidityState)
+DOMCI_DATA(ValidityState, mozilla::dom::ValidityState)
 
-NS_IMPL_ADDREF(nsDOMValidityState)
-NS_IMPL_RELEASE(nsDOMValidityState)
+namespace mozilla {
+namespace dom {
 
-NS_INTERFACE_MAP_BEGIN(nsDOMValidityState)
+NS_IMPL_ADDREF(ValidityState)
+NS_IMPL_RELEASE(ValidityState)
+
+NS_INTERFACE_MAP_BEGIN(ValidityState)
   NS_INTERFACE_MAP_ENTRY(nsIDOMValidityState)
   NS_INTERFACE_MAP_ENTRY_AMBIGUOUS(nsISupports, nsIDOMValidityState)
   NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(ValidityState)
 NS_INTERFACE_MAP_END
 
-nsDOMValidityState::nsDOMValidityState(nsIConstraintValidation* aConstraintValidation)
+ValidityState::ValidityState(nsIConstraintValidation* aConstraintValidation)
   : mConstraintValidation(aConstraintValidation)
 {
 }
 
 NS_IMETHODIMP
-nsDOMValidityState::GetValueMissing(bool* aValueMissing)
+ValidityState::GetValueMissing(bool* aValueMissing)
 {
   *aValueMissing = GetValidityState(nsIConstraintValidation::VALIDITY_STATE_VALUE_MISSING);
   return NS_OK;
 }
 
 NS_IMETHODIMP
-nsDOMValidityState::GetTypeMismatch(bool* aTypeMismatch)
+ValidityState::GetTypeMismatch(bool* aTypeMismatch)
 {
   *aTypeMismatch = GetValidityState(nsIConstraintValidation::VALIDITY_STATE_TYPE_MISMATCH);
   return NS_OK;
 }
 
 NS_IMETHODIMP
-nsDOMValidityState::GetPatternMismatch(bool* aPatternMismatch)
+ValidityState::GetPatternMismatch(bool* aPatternMismatch)
 {
   *aPatternMismatch = GetValidityState(nsIConstraintValidation::VALIDITY_STATE_PATTERN_MISMATCH);
   return NS_OK;
 }
 
 NS_IMETHODIMP
-nsDOMValidityState::GetTooLong(bool* aTooLong)
+ValidityState::GetTooLong(bool* aTooLong)
 {
   *aTooLong = GetValidityState(nsIConstraintValidation::VALIDITY_STATE_TOO_LONG);
   return NS_OK;
 }
 
 NS_IMETHODIMP
-nsDOMValidityState::GetRangeUnderflow(bool* aRangeUnderflow)
+ValidityState::GetRangeUnderflow(bool* aRangeUnderflow)
 {
   *aRangeUnderflow = GetValidityState(nsIConstraintValidation::VALIDITY_STATE_RANGE_UNDERFLOW);
   return NS_OK;
 }
 
 NS_IMETHODIMP
-nsDOMValidityState::GetRangeOverflow(bool* aRangeOverflow)
+ValidityState::GetRangeOverflow(bool* aRangeOverflow)
 {
   *aRangeOverflow = GetValidityState(nsIConstraintValidation::VALIDITY_STATE_RANGE_OVERFLOW);
   return NS_OK;
 }
 
 NS_IMETHODIMP
-nsDOMValidityState::GetStepMismatch(bool* aStepMismatch)
+ValidityState::GetStepMismatch(bool* aStepMismatch)
 {
   *aStepMismatch = GetValidityState(nsIConstraintValidation::VALIDITY_STATE_STEP_MISMATCH);
   return NS_OK;
 }
 
 NS_IMETHODIMP
-nsDOMValidityState::GetCustomError(bool* aCustomError)
+ValidityState::GetCustomError(bool* aCustomError)
 {
   *aCustomError = GetValidityState(nsIConstraintValidation::VALIDITY_STATE_CUSTOM_ERROR);
   return NS_OK;
 }
 
 NS_IMETHODIMP
-nsDOMValidityState::GetValid(bool* aValid)
+ValidityState::GetValid(bool* aValid)
 {
   *aValid = !mConstraintValidation || mConstraintValidation->IsValid();
   return NS_OK;
 }
+
+} // namespace dom
+} // namespace mozilla
 
