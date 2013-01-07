b1d8b1f7b4ac6c520fa951d226396d0f906b2b0c
Ben Turner <bent.mozilla@gmail.com>
2013-01-07 11:25:35 +0100

    Bug 824224 - IndexedDB blobs crash with 'ABORT: NULL actor value passed to non-nullable param.' r=cjones.

--- dom/indexedDB/IDBObjectStore.cpp
+++ dom/indexedDB/IDBObjectStore.cpp
@@ -1501,7 +1501,10 @@ IDBObjectStore::ConvertBlobsToActors(
 
       BlobParent* actor =
         aContentParent->GetOrCreateActorForBlob(blob);
-      NS_ASSERTION(actor, "This should never fail without aborting!");
+      if (!actor) {
+        // This can only fail if the child has crashed.
+        return NS_ERROR_DOM_INDEXEDDB_UNKNOWN_ERR;
+      }
 
       aActors.AppendElement(actor);
     }
