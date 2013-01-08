b34c426f379b82c68c61413338abf10744064b3d
Cameron McCormack <cam@mcc.id.au>
2013-01-09 10:25:47 +1100

    Bug 508725 - Part 2: Record on nsCSSStyleSheets whether they are for a <style scoped>. r=dbaron

--- content/base/src/nsDocument.h
+++ content/base/src/nsDocument.h
@@ -1080,6 +1080,7 @@ protected:
                          nsCompatibility aCompatMode,
                          nsIPresShell** aInstancePtrResult);
 
+  void RemoveDocStyleSheetsFromStyleSets();
   void RemoveStyleSheetsFromStyleSets(nsCOMArray<nsIStyleSheet>& aSheets, 
                                       nsStyleSet::sheetType aType);
   nsresult ResetStylesheetsToURI(nsIURI* aURI);
