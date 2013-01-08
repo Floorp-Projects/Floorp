c7a0942a522ce0a45b2ba313e4f07ffe527ac3e7
Andrew McCreight <amccreight@mozilla.com>
2013-01-08 10:36:51 -0800

    Bug 690970, part 3: fix black-gray edges in weak containers at start of CC. r=billm

--- xpcom/base/nsCycleCollector.h
+++ xpcom/base/nsCycleCollector.h
@@ -68,6 +68,11 @@ struct nsCycleCollectionJSRuntime
     virtual void NotifyEnterMainThread() = 0;
 
     /**
+     * Unmark gray any weak map values, as needed.
+     */
+    virtual void FixWeakMappingGrayBits() = 0;
+
+    /**
      * Should we force a JavaScript GC before a CC?
      */
     virtual bool NeedCollect() = 0;
