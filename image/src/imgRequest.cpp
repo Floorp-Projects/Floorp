6a490fa58d4b204ee5a3dceb61e5d3d3c805ef80
Seth Fowler <seth@mozilla.com>
2013-01-08 13:40:47 -0800

    Bug 822846 - Use nsMimeTypes.h instead of working with literal strings in imagelib. r=joe

--- image/src/imgRequest.cpp
+++ image/src/imgRequest.cpp
@@ -31,6 +31,7 @@
 #include "nsIHttpChannel.h"
 #include "nsIApplicationCache.h"
 #include "nsIApplicationCacheChannel.h"
+#include "nsMimeTypes.h"
 
 #include "nsIComponentManager.h"
 #include "nsIInterfaceRequestorUtils.h"
@@ -726,7 +727,7 @@ imgRequest::OnDataAvailable(nsIRequest *aRequest, nsISupports *ctxt,
     // type and decoder.
     // We always reinitialize for SVGs, because they have no way of
     // reinitializing themselves.
-    if (mContentType != newType || newType.Equals(SVG_MIMETYPE)) {
+    if (mContentType != newType || newType.EqualsLiteral(IMAGE_SVG_XML)) {
       mContentType = newType;
 
       // If we've resniffed our MIME type and it changed, we need to create a
