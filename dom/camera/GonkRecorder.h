521925ef88d66c98a5abde5fb6a2428f9521d200
Mike Habicher <mikeh@mozilla.com>
2013-01-08 10:11:32 +0100

    Bug 825986 - propagate startRecording() return code instead of asserting; clean up GonkRecorder lifecycle management, r=kanru

--- dom/camera/GonkRecorder.h
+++ dom/camera/GonkRecorder.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2013 Mozilla Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +16,9 @@
  */
 
 #ifndef GONK_RECORDER_H_
-
 #define GONK_RECORDER_H_
+
+#include "nsISupportsImpl.h"
 
 #include <media/mediarecorder.h>
 #include <camera/CameraParameters.h>
@@ -34,8 +36,9 @@ struct AudioSource;
 class MediaProfiles;
 
 struct GonkRecorder {
+    NS_INLINE_DECL_THREADSAFE_REFCOUNTING(GonkRecorder)
+
     GonkRecorder();
-    virtual ~GonkRecorder();
 
     virtual status_t init();
     virtual status_t setAudioSource(audio_source_t as);
@@ -59,6 +62,9 @@ struct GonkRecorder {
     virtual status_t getMaxAmplitude(int *max);
     virtual status_t dump(int fd, const Vector<String16>& args) const;
     // Querying a SurfaceMediaSourcer
+
+protected:
+    virtual ~GonkRecorder();
 
 private:
     sp<IMediaRecorderClient> mListener;
