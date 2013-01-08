64ccc57d29687da981dd8a5c0cc15b5bf340965b
Justin Lebar <justin.lebar@gmail.com>
2013-01-08 11:04:19 +0100

    Bug 827234 - Move Swap to js/public/Utility from js/src/jsutil.h. r=terrence

--- js/public/Utility.h
+++ js/public/Utility.h
@@ -709,6 +709,15 @@ class ReentrancyGuard
     }
 };
 
+template <class T>
+JS_ALWAYS_INLINE static void
+Swap(T &t, T &u)
+{
+    T tmp(Move(t));
+    t = Move(u);
+    u = Move(tmp);
+}
+
 /*
  * Round x up to the nearest power of 2.  This function assumes that the most
  * significant bit of x is not set, which would lead to overflow.
