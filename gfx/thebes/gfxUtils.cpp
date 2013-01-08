057be11ab4b3915a4d67676f729482dfb8a10e60
Ehsan Akhgari <ehsan@mozilla.com>
2013-01-08 14:09:32 -0500

    Bug 827934 - Only build gfxUtils.cpp when needed; r=jrmuizel,ted

--- gfx/thebes/gfxUtils.cpp
+++ gfx/thebes/gfxUtils.cpp
@@ -20,13 +20,7 @@ using namespace mozilla;
 using namespace mozilla::layers;
 using namespace mozilla::gfx;
 
-const uint8_t gfxUtils::sPremultiplyTable[256*256] = {
-#include "sPremultiplyTable.h"
-};
-
-const uint8_t gfxUtils::sUnpremultiplyTable[256*256] = {
-#include "sUnpremultiplyTable.h"
-};
+#include "PremultiplyTables.h"
 
 static const uint8_t PremultiplyValue(uint8_t a, uint8_t v) {
     return gfxUtils::sPremultiplyTable[a*256+v];
