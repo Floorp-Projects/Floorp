59e843bcab88624bdb89a906e36fc352c2d0bd9f
Jason Orendorff <jorendorff@mozilla.com>
2013-01-08 10:18:29 -0600

    Fix a dozen JS tests that were broken when using the command-line test harness (see bug 804834 comment 24 and 25). No bug#, r=evilpie on IRC.

---
+++ js/src/tests/js1_6/shell.js
@@ -0,0 +1,13 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+// explicitly turn on js16
+if (typeof version != 'undefined')
+{
+  version(160);
+}
+
