f1cd485faefd1a1dfecbdf0f5123b2835d954721
David Zbarsky <dzbarsky@gmail.com>
2013-01-09 18:05:08 -0500

    Bug 828169 - Don't use nsIDOMNode in editor transactions r=ehsan

--- editor/libeditor/base/InsertElementTxn.cpp
+++ editor/libeditor/base/InsertElementTxn.cpp
@@ -8,14 +8,16 @@
 #include "InsertElementTxn.h"
 #include "nsAString.h"
 #include "nsDebug.h"                    // for NS_ENSURE_TRUE, etc
+#include "nsEditor.h"                   // for nsEditor
 #include "nsError.h"                    // for NS_ERROR_NULL_POINTER, etc
 #include "nsIContent.h"                 // for nsIContent
-#include "nsIEditor.h"                  // for nsIEditor
 #include "nsINode.h"                    // for nsINode
 #include "nsISelection.h"               // for nsISelection
 #include "nsMemory.h"                   // for nsMemory
 #include "nsReadableUtils.h"            // for ToNewCString
 #include "nsString.h"                   // for nsString
+
+using namespace mozilla;
 
 #ifdef DEBUG
 static bool gNoisy = false;
@@ -44,10 +46,10 @@ NS_IMPL_RELEASE_INHERITED(InsertElementTxn, EditTxn)
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(InsertElementTxn)
 NS_INTERFACE_MAP_END_INHERITING(EditTxn)
 
-NS_IMETHODIMP InsertElementTxn::Init(nsIDOMNode *aNode,
-                                     nsIDOMNode *aParent,
+NS_IMETHODIMP InsertElementTxn::Init(nsINode *aNode,
+                                     nsINode *aParent,
                                      int32_t     aOffset,
-                                     nsIEditor  *aEditor)
+                                     nsEditor  *aEditor)
 {
   NS_ASSERTION(aNode && aParent && aEditor, "bad arg");
   NS_ENSURE_TRUE(aNode && aParent && aEditor, NS_ERROR_NULL_POINTER);
@@ -92,16 +94,14 @@ NS_IMETHODIMP InsertElementTxn::DoTransaction(void)
     mOffset = count;
   }
 
+  // note, it's ok for refContent to be null.  that means append
   nsIContent* refContent = parent->GetChildAt(mOffset);
-  // note, it's ok for refNode to be null.  that means append
-  nsCOMPtr<nsIDOMNode> refNode = refContent ? refContent->AsDOMNode() : nullptr;
 
   mEditor->MarkNodeDirty(mNode);
 
-  nsCOMPtr<nsIDOMNode> resultNode;
-  nsresult result = mParent->InsertBefore(mNode, refNode, getter_AddRefs(resultNode));
-  NS_ENSURE_SUCCESS(result, result);
-  NS_ENSURE_TRUE(resultNode, NS_ERROR_NULL_POINTER);
+  ErrorResult rv;
+  mParent->InsertBefore(*mNode, refContent, rv);
+  NS_ENSURE_SUCCESS(rv.ErrorCode(), rv.ErrorCode());
 
   // only set selection to insertion point if editor gives permission
   bool bAdjustSelection;
@@ -109,17 +109,17 @@ NS_IMETHODIMP InsertElementTxn::DoTransaction(void)
   if (bAdjustSelection)
   {
     nsCOMPtr<nsISelection> selection;
-    result = mEditor->GetSelection(getter_AddRefs(selection));
-    NS_ENSURE_SUCCESS(result, result);
+    rv = mEditor->GetSelection(getter_AddRefs(selection));
+    NS_ENSURE_SUCCESS(rv.ErrorCode(), rv.ErrorCode());
     NS_ENSURE_TRUE(selection, NS_ERROR_NULL_POINTER);
     // place the selection just after the inserted element
-    selection->Collapse(mParent, mOffset+1);
+    selection->Collapse(mParent->AsDOMNode(), mOffset+1);
   }
   else
   {
     // do nothing - dom range gravity will adjust selection
   }
-  return result;
+  return NS_OK;
 }
 
 NS_IMETHODIMP InsertElementTxn::UndoTransaction(void)
@@ -137,8 +137,9 @@ NS_IMETHODIMP InsertElementTxn::UndoTransaction(void)
 
   NS_ENSURE_TRUE(mNode && mParent, NS_ERROR_NOT_INITIALIZED);
 
-  nsCOMPtr<nsIDOMNode> resultNode;
-  return mParent->RemoveChild(mNode, getter_AddRefs(resultNode));
+  ErrorResult rv;
+  mParent->RemoveChild(*mNode, rv);
+  return rv.ErrorCode();
 }
 
 NS_IMETHODIMP InsertElementTxn::GetTxnDescription(nsAString& aString)
