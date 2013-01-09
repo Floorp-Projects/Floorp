078a401ea51c639073cbbff35135d7fc563a95bd
Patrick McManus <mcmanus@ducksong.com>
2013-01-09 10:17:32 -0500

    bug 796475 - proxy auth cancelation rendering fix r=jduell
    --HG--
    extra : rebase_source : 83d6ad3d15bae973c8a09bca02a8d86578467345

--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -331,6 +331,7 @@ private:
     uint32_t                          mCachedContentIsPartial   : 1;
     uint32_t                          mTransactionReplaced      : 1;
     uint32_t                          mAuthRetryPending         : 1;
+    uint32_t                          mProxyAuthPending         : 1;
     uint32_t                          mResuming                 : 1;
     uint32_t                          mInitedCacheEntry         : 1;
     // True if we are loading a fallback cache entry from the
