4b6abee5f4d7708fa99eea4e5c6a67cf467a995c
Trevor Saunders <trev.saunders@gmail.com>
2013-01-10 02:04:09 -0500

    bug 829288 - fix layout/ warnings on mingw r=dholbert

--- layout/base/nsPresContext.cpp
+++ layout/base/nsPresContext.cpp
@@ -2338,9 +2338,11 @@ enum InterruptMode {
 // "random" (except on Windows) or "counter".  If neither is used, the mode is
 // ModeEvent.
 static InterruptMode sInterruptMode = ModeEvent;
+#ifndef XP_WIN
 // Used for the "random" mode.  Controlled by the GECKO_REFLOW_INTERRUPT_SEED
 // env var.
 static uint32_t sInterruptSeed = 1;
+#endif
 // Used for the "counter" mode.  This is the number of unskipped interrupt
 // checks that have to happen before we interrupt.  Controlled by the
 // GECKO_REFLOW_INTERRUPT_FREQUENCY env var.
