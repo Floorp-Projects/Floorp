e5deefe9c9060f086049a594b645f0736f15a85c
Chris Jones <jones.chris.g@gmail.com>
2013-01-10 14:22:14 +0100

    Bug 828114: Set a timer to ensure content processes are killed if their tabs take a long time to shut down. r=jlebar a=blocking-basecamp

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -1762,6 +1762,10 @@ pref("dom.ipc.plugins.hangUITimeoutSecs", 5);
 // Minimum time that the plugin hang UI will be displayed
 pref("dom.ipc.plugins.hangUIMinDisplaySecs", 10);
 #endif
+// How long a content process can take before closing its IPC channel
+// after shutdown is initiated.  If the process exceeds the timeout,
+// we fear the worst and kill it.
+pref("dom.ipc.tabs.shutdownTimeoutSecs", 5);
 #else
 // No timeout in DEBUG builds
 pref("dom.ipc.plugins.timeoutSecs", 0);
@@ -1771,6 +1775,7 @@ pref("dom.ipc.plugins.parentTimeoutSecs", 0);
 pref("dom.ipc.plugins.hangUITimeoutSecs", 0);
 pref("dom.ipc.plugins.hangUIMinDisplaySecs", 0);
 #endif
+pref("dom.ipc.tabs.shutdownTimeoutSecs", 0);
 #endif
 
 #ifdef XP_WIN
