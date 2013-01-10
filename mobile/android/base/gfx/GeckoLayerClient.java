fd9dd3979f0678a6b83cf6c864eb6ae0f165ad64
Chris Lord <chrislord.net@gmail.com>
2013-01-10 17:32:43 +0000

    Bug 827844 - Fix flickering when layout changes on Firefox/android. r=kats,bgirard
    Fix flickering that can occur when the surface size changes due to a layout
    change (such as the virtual keyboard appearing/disappearing) on Android.

--- mobile/android/base/gfx/GeckoLayerClient.java
+++ mobile/android/base/gfx/GeckoLayerClient.java
@@ -613,6 +613,14 @@ public class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
     }
 
     /** Implementation of LayerView.Listener */
+    public void sizeChanged(int width, int height) {
+        // We need to make sure a draw happens synchronously at this point,
+        // but resizing the surface before the SurfaceView has resized will
+        // cause a visible jump.
+        compositionResumeRequested(mWindowSize.width, mWindowSize.height);
+    }
+
+    /** Implementation of LayerView.Listener */
     public void surfaceChanged(int width, int height) {
         setViewportSize(width, height);
 
@@ -620,7 +628,6 @@ public class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
         // paused (e.g. during an orientation change), to make the compositor
         // aware of the changed surface.
         compositionResumeRequested(width, height);
-        renderRequested();
     }
 
     /** Implementation of LayerView.Listener */
