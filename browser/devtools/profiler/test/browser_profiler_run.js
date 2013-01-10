e0b697327cfb06fbcd6c4cedf2ea47af9a818f27
Ed Morley <emorley@mozilla.com>
2013-01-10 14:59:27 +0000

    Bug 822287 - Mark part of browser_profiler_run.js todo() on Windows

--- browser/devtools/profiler/test/browser_profiler_run.js
+++ browser/devtools/profiler/test/browser_profiler_run.js
@@ -82,7 +82,11 @@ function onParsed() {
     }
 
     ok(sample.length > 0, "We have some items displayed");
-    is(sample[0].innerHTML, "100.0%", "First percentage is 100%");
+    if (navigator.platform.contains("Win")) {
+      todo(false, "First percentage is 100%: Disabled on Windows for intermittent failures, see bug 822287.");
+    } else {
+      is(sample[0].innerHTML, "100.0%", "First percentage is 100%");
+    }
     attemptTearDown();
   }
 
