2349f30f16f412eddb1dd491ba3a637cabc262e0
Ehsan Akhgari <ehsan@mozilla.com>
2013-01-11 11:44:09 -0500

    Correct the type names in speex_config_types.h; no bug, irc-r=padenot
    Turns out that the compiler believes you when you say unsigned once.
    No need to repeat yourself.
    
    --HG--
    extra : rebase_source : 979560fd47f7591d9cec0533a29462d3a25e3528

--- media/libspeex_resampler/src/speex_config_types.h
+++ media/libspeex_resampler/src/speex_config_types.h
@@ -2,10 +2,10 @@
 #define __SPEEX_TYPES_H__
 
 /* these are filled in by configure */
-typedef uint16_t spx_int16_t;
-typedef unsigned uint16_t spx_uint16_t;
-typedef uint32_t spx_int32_t;
-typedef unsigned uint32_t spx_uint32_t;
+typedef int16_t spx_int16_t;
+typedef uint16_t spx_uint16_t;
+typedef int32_t spx_int32_t;
+typedef uint32_t spx_uint32_t;
 
 #endif
 
--- media/libspeex_resampler/update.sh
+++ media/libspeex_resampler/update.sh
@@ -12,6 +12,6 @@ cp $1/libspeex/stack_alloc.h src
 cp $1/libspeex/fixed_generic.h src
 cp $1/include/speex/speex_resampler.h src
 cp $1/include/speex/speex_types.h src
-sed -e 's/@SIZE16@/uint16_t/g' -e 's/@SIZE32@/uint32_t/g' < $1/include/speex/speex_config_types.h.in > src/speex_config_types.h
+sed -e 's/unsigned @SIZE16@/uint16_t/g' -e 's/unsigned @SIZE32@/uint32_t/g' -e 's/@SIZE16@/int16_t/g' -e 's/@SIZE32@/int32_t/g' < $1/include/speex/speex_config_types.h.in > src/speex_config_types.h
 cp $1/AUTHORS .
 cp $1/COPYING .
