0637521fa3dda838abdd30d9a00b77eb5c0bd004
Mike Habicher <mikeh@mozilla.com>
2013-01-11 14:28:28 +0100

    backed out changeset d99397ff6bb8 (see bug 808146 for proper solution), a=not-needed

--- xpcom/glue/nsProxyRelease.h
+++ xpcom/glue/nsProxyRelease.h
@@ -114,7 +114,7 @@ public:
   }
 
   // We can be released on any thread.
-  virtual ~nsMainThreadPtrHolder() {
+  ~nsMainThreadPtrHolder() {
     if (NS_IsMainThread()) {
       NS_IF_RELEASE(mRawPtr);
     } else if (mRawPtr) {
