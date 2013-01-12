5cc5a758e78e0343737eed661f49e0c45a37a865
Anthony Jones <ajones@mozilla.com>
2013-01-12 15:48:55 -0500

    Bug 825808 - Fix animations by marking transforms as not fixed. r=roc

--- gfx/layers/ipc/CompositorParent.cpp
+++ gfx/layers/ipc/CompositorParent.cpp
@@ -822,6 +822,7 @@ SampleAnimations(Layer* aLayer, TimeStamp aPoint)
                          c->GetInheritedYScale(),
                          1);
       }
+      NS_ASSERTION(!aLayer->GetIsFixedPosition(), "Can't animate transforms on fixed-position layers");
       shadow->SetShadowTransform(matrix);
       break;
     }
