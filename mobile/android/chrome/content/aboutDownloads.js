48d35e7bbb941e0f5deafcfcac21d8836aef5583
Brian Nicholson <bnicholson@mozilla.com>
2013-01-14 13:47:24 -0800

    Bug 820491 - Part 3: Whitespace cleanup in aboutDownloads.js. r=wesj

--- mobile/android/chrome/content/aboutDownloads.js
+++ mobile/android/chrome/content/aboutDownloads.js
@@ -212,7 +212,7 @@ let Downloads = {
       this.logError("_moveDownloadAfterActive() " + ex);
     }
   },
-  
+
   _inProgress: function dl_inProgress(aState) {
     return [
       this._dlmgr.DOWNLOAD_NOTSTARTED,
@@ -246,7 +246,7 @@ let Downloads = {
     else
       return gStrings.GetStringFromName("downloadState.unknownSize");
   },
-  
+
   // Not all states are displayed as-is on mobile, some are translated to a generic state
   _getState: function dl_getState(aState) {
     let str;
@@ -256,7 +256,7 @@ let Downloads = {
       case this._dlmgr.DOWNLOAD_SCANNING:
         str = this._dlmgr.DOWNLOAD_DOWNLOADING;
         break;
-        
+
       // Failed, Dirty and Blocked states show up as "Failed"
       case this._dlmgr.DOWNLOAD_FAILED:
       case this._dlmgr.DOWNLOAD_DIRTY:
@@ -264,17 +264,17 @@ let Downloads = {
       case this._dlmgr.DOWNLOAD_BLOCKED_PARENTAL:
         str = this._dlmgr.DOWNLOAD_FAILED;
         break;
-        
-      /* QUEUED and NOTSTARTED are not translated as they 
+
+      /* QUEUED and NOTSTARTED are not translated as they
          dont fall under a common state but we still need
          to display a common "status" on the UI */
-         
+
       default:
         str = aState;
     }
     return str;
   },
-  
+
   // Note: This doesn't cover all states as some of the states are translated in _getState()
   _getStateString: function dl_getStateString(aState) {
     let str;
@@ -291,13 +291,13 @@ let Downloads = {
       case this._dlmgr.DOWNLOAD_PAUSED:
         str = "downloadState.paused";
         break;
-        
+
       // Queued and Notstarted show up as "Starting..."
       case this._dlmgr.DOWNLOAD_QUEUED:
       case this._dlmgr.DOWNLOAD_NOTSTARTED:
         str = "downloadState.starting";
         break;
-        
+
       default:
         return "";
     }
@@ -427,10 +427,10 @@ let Downloads = {
     // Make sure we have an item to remove
     if (!aItem)
       return;
-  
+
     aItem.parentNode.removeChild(aItem);
   },
-  
+
   openDownload: function dl_openDownload(aItem) {
     this._getDownloadForElement(aItem, function(aDownload) {
       try {
