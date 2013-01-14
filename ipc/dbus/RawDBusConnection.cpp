9b2d925217b73f8350fb09a6d0c28c976c3e854d
Thomas Zimmermann <tdz@users.sourceforge.net>
2013-01-14 11:50:18 +0100

    Bug 830290: Cleanup DBus EventLoop function r=bent,qdot
    We intent to run the DBus thread's EventLoop function as separate
    nsRunnable, but the function also contains code for initializing
    sockets that are used for internal communication.
    
    With this patch the socket initialization has been moved to the
    setup function. This reduces the EventLoop function to plain event
    handling.

--- ipc/dbus/RawDBusConnection.cpp
+++ ipc/dbus/RawDBusConnection.cpp
@@ -9,13 +9,21 @@
 
 using namespace mozilla::ipc;
 
+bool RawDBusConnection::sDBusIsInit(false);
+
 RawDBusConnection::RawDBusConnection() {
 }
 
 RawDBusConnection::~RawDBusConnection() {
 }
 
-nsresult RawDBusConnection::EstablishDBusConnection() {
+nsresult RawDBusConnection::EstablishDBusConnection()
+{
+  if (!sDBusIsInit) {
+    dbus_bool_t success = dbus_threads_init_default();
+    NS_ENSURE_TRUE(success == TRUE, NS_ERROR_FAILURE);
+    sDBusIsInit = true;
+  }
   DBusError err;
   dbus_error_init(&err);
   mConnection = dbus_bus_get(DBUS_BUS_SYSTEM, &err);
@@ -29,5 +37,7 @@ nsresult RawDBusConnection::EstablishDBusConnection() {
 
 void RawDBusConnection::ScopedDBusConnectionPtrTraits::release(DBusConnection* ptr)
 {
-  if (ptr) dbus_connection_unref(ptr);
+  if (ptr) {
+    dbus_connection_unref(ptr);
+  }
 }
