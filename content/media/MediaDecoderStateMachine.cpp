2e221c1027337a3df2189926b0ee043e2c706db0
Paul Adenot <paul@paul.cx>
2013-01-15 19:57:03 +0100

    Bug 819377 - Don't offset the clock by the start time of the video in AdvanceFrame, and set the media as playing earlier. r=kinetik

--- content/media/MediaDecoderStateMachine.cpp
+++ content/media/MediaDecoderStateMachine.cpp
@@ -2073,6 +2073,13 @@ nsresult MediaDecoderStateMachine::RunStateMachine()
         StopPlayback();
       }
 
+      if (mDecoder->GetState() == MediaDecoder::PLAY_STATE_PLAYING &&
+          !IsPlaying()) {
+        // We are playing, but the state machine does not know it yet. Tell it
+        // that it is, so that the clock can be properly queried.
+        StartPlayback();
+      }
+
       if (IsPausedAndDecoderWaiting()) {
         // The decode buffers are full, and playback is paused. Shutdown the
         // decode thread.
@@ -2347,9 +2354,9 @@ void MediaDecoderStateMachine::AdvanceFrame()
     // Current frame has already been presented, wait until it's time to
     // present the next frame.
     if (frame && !currentFrame) {
-      int64_t now = IsPlaying() ? clock_time : mPlayDuration;
+      int64_t now = IsPlaying() ? clock_time : mPlayDuration + mStartTime;
 
-      remainingTime = frame->mTime - mStartTime - now;
+      remainingTime = frame->mTime - now;
     }
   }
 
@@ -2396,7 +2403,7 @@ void MediaDecoderStateMachine::AdvanceFrame()
       return;
     }
     mDecoder->GetFrameStatistics().NotifyPresentedFrame();
-    remainingTime = currentFrame->mEndTime - mStartTime - clock_time;
+    remainingTime = currentFrame->mEndTime - clock_time;
     currentFrame = nullptr;
   }
 
