bc1fe36cd777d7ace639cffdf2246bc6fd164476
Andrew McCreight <amccreight@mozilla.com>
2013-01-15 10:14:12 -0800

    Bug 829798 - Add dummy PreserveWrapper to JS shell. r=billm

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -5219,6 +5219,13 @@ JSSecurityCallbacks securityCallbacks = {
     NULL
 };
 
+/* Pretend we can always preserve wrappers for dummy DOM objects. */
+static bool
+DummyPreserveWrapperCallback(JSContext *cx, JSObject *obj)
+{
+    return true;
+}
+
 int
 main(int argc, char **argv, char **envp)
 {
@@ -5381,6 +5388,8 @@ main(int argc, char **argv, char **envp)
 
     JS_SetGCParameter(rt, JSGC_MODE, JSGC_MODE_INCREMENTAL);
     JS_SetGCParameterForThread(cx, JSGC_MAX_CODE_CACHE_BYTES, 16 * 1024 * 1024);
+
+    js::SetPreserveWrapperCallback(rt, DummyPreserveWrapperCallback);
 
     /* Must be done before creating the global object */
     if (op.getBoolOption('D'))
