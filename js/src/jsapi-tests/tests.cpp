5853e1a99c7d36f5c8df0dc96c99ef43e285708e
Jon Coppeard <jcoppeard@mozilla.com>
2013-01-15 17:57:30 +0000

    Bug 830332 - jittest gc/incremental-state.js fails if rooting analysis enabled r=billm
    --HG--
    extra : rebase_source : f4abcc5eb03229f9725181887b3d239acccd2392

--- js/src/jsapi-tests/tests.cpp
+++ js/src/jsapi-tests/tests.cpp
@@ -20,9 +20,6 @@ bool JSAPITest::init()
     cx = createContext();
     if (!cx)
         return false;
-#ifdef JS_GC_ZEAL
-    JS_SetGCZeal(cx, 0, 0);
-#endif
     JS_BeginRequest(cx);
     js::RootedObject global(cx, createGlobal());
     if (!global)
