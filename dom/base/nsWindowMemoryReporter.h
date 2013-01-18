ef63d8fa251f19ec08f40faecc442b5ae5844dd2
Nicholas Nethercote <nnethercote@mozilla.com>
2013-01-17 21:21:43 -0800

    Bug 826521 (part 2) - Report memory used by event targets, especially XHRs.  r=bz.

--- dom/base/nsWindowMemoryReporter.h
+++ dom/base/nsWindowMemoryReporter.h
@@ -1,4 +1,5 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -34,6 +35,7 @@ public:
   size_t mDOMTextNodes;
   size_t mDOMCDATANodes;
   size_t mDOMCommentNodes;
+  size_t mDOMEventTargets;
   size_t mDOMOther;
   size_t mStyleSheets;
   size_t mLayoutPresShell;
