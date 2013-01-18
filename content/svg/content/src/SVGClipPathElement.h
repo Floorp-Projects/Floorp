fd22278f029df206b49c7aa0027844dbe6bed469
David Zbarsky <dzbarsky@gmail.com>
2013-01-18 14:52:40 -0500

    Bug 831669: Move SVGClipPathElement to mozilla::dom r=bz

--- content/svg/content/src/SVGClipPathElement.h
+++ content/svg/content/src/SVGClipPathElement.h
@@ -3,37 +3,45 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef __NS_SVGCLIPPATHELEMENT_H__
-#define __NS_SVGCLIPPATHELEMENT_H__
+#ifndef mozilla_dom_SVGClipPathElement_h
+#define mozilla_dom_SVGClipPathElement_h
 
 #include "nsIDOMSVGClipPathElement.h"
 #include "nsIDOMSVGUnitTypes.h"
 #include "nsSVGEnum.h"
 #include "mozilla/dom/SVGTransformableElement.h"
 
-typedef mozilla::dom::SVGTransformableElement nsSVGClipPathElementBase;
+class nsSVGClipPathFrame;
 
-class nsSVGClipPathElement : public nsSVGClipPathElementBase,
-                             public nsIDOMSVGClipPathElement,
-                             public nsIDOMSVGUnitTypes
+nsresult NS_NewSVGClipPathElement(nsIContent **aResult,
+                                  already_AddRefed<nsINodeInfo> aNodeInfo);
+
+namespace mozilla {
+namespace dom {
+
+typedef SVGTransformableElement SVGClipPathElementBase;
+
+class SVGClipPathElement MOZ_FINAL : public SVGClipPathElementBase,
+                                     public nsIDOMSVGClipPathElement,
+                                     public nsIDOMSVGUnitTypes
 {
-  friend class nsSVGClipPathFrame;
+  friend class ::nsSVGClipPathFrame;
 
 protected:
-  friend nsresult NS_NewSVGClipPathElement(nsIContent **aResult,
-                                           already_AddRefed<nsINodeInfo> aNodeInfo);
-  nsSVGClipPathElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  friend nsresult (::NS_NewSVGClipPathElement(nsIContent **aResult,
+                                              already_AddRefed<nsINodeInfo> aNodeInfo));
+  SVGClipPathElement(already_AddRefed<nsINodeInfo> aNodeInfo);
 
 public:
   // interfaces:
-  
+
   NS_DECL_ISUPPORTS_INHERITED
   NS_DECL_NSIDOMSVGCLIPPATHELEMENT
 
   // xxx I wish we could use virtual inheritance
   NS_FORWARD_NSIDOMNODE_TO_NSINODE
   NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
-  NS_FORWARD_NSIDOMSVGELEMENT(nsSVGClipPathElementBase::)
+  NS_FORWARD_NSIDOMSVGELEMENT(SVGClipPathElementBase::)
 
   virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
 
@@ -50,4 +58,7 @@ protected:
   virtual EnumAttributesInfo GetEnumInfo();
 };
 
-#endif
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_SVGClipPathElement_h
