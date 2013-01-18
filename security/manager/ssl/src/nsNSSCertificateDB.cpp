08a84fb073e482076b7af51e4fb6f0ed4746f4b7
Brian Smith <bsmith@mozilla.com>
2013-01-18 10:28:58 -0800

    Bug 714477: Do not check the OCSP enabled pref during extended validation, r=rrelyea

--- security/manager/ssl/src/nsNSSCertificateDB.cpp
+++ security/manager/ssl/src/nsNSSCertificateDB.cpp
@@ -1255,20 +1255,6 @@ finish:
   *_certNames = tmpArray;
 }
 
-/* somewhat follows logic of cert_list_include_cert from PSM 1.x */
-
-
-NS_IMETHODIMP 
-nsNSSCertificateDB::GetIsOcspOn(bool *aOcspOn)
-{
-  nsCOMPtr<nsIPrefBranch> pref = do_GetService(NS_PREFSERVICE_CONTRACTID);
-
-  int32_t ocspEnabled;
-  pref->GetIntPref("security.OCSP.enabled", &ocspEnabled);
-  *aOcspOn = ( ocspEnabled == 0 ) ? false : true; 
-  return NS_OK;
-}
-
 /* nsIX509Cert getDefaultEmailEncryptionCert (); */
 NS_IMETHODIMP
 nsNSSCertificateDB::FindEmailEncryptionCert(const nsAString &aNickname, nsIX509Cert **_retval)
