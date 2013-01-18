9f3b77fe34be2ed3299fbc333d54371b0f7a81cb
Henri Sivonen <hsivonen@iki.fi>
2013-01-18 16:27:03 +0200

    Bug 234628 part 4 - Test cases for character encoding override code. r=bzbarsky.
    --HG--
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-1.js
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-10.js
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-11.js
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-2.js
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-3.js
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-4.js
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-5.js
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-6.js
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-7.js
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-8.js
    rename : docshell/test/browser/browser_bug134911.js => docshell/test/browser/browser_bug234628-9.js

---
+++ docshell/test/browser/browser_bug234628-3.js
@@ -0,0 +1,39 @@
+function test() {
+  waitForExplicitFinish();
+
+  var rootDir = "http://mochi.test:8888/browser/docshell/test/browser/";
+  gBrowser.selectedTab = gBrowser.addTab(rootDir + "file_bug234628-3.html");
+  gBrowser.selectedBrowser.addEventListener("load", afterOpen, true);
+}
+
+function afterOpen(event) {
+  if (event.target != gBrowser.contentDocument) {
+    return;
+  }
+
+  gBrowser.selectedBrowser.removeEventListener("load", afterOpen, true);
+  gBrowser.selectedBrowser.addEventListener("load", afterChangeCharset, true);
+
+  is(gBrowser.contentDocument.documentElement.textContent.indexOf('\u20AC'), 118, "Parent doc should be windows-1252 initially");
+
+  is(gBrowser.contentDocument.getElementsByTagName("iframe")[0].contentDocument.documentElement.textContent.indexOf('\u20AC'), 73, "Child doc should be utf-8 initially");
+
+  BrowserSetForcedCharacterSet("windows-1251");
+}
+  
+function afterChangeCharset(event) {
+  if (event.target != gBrowser.contentDocument) {
+    return;
+  }
+
+  gBrowser.selectedBrowser.removeEventListener("load", afterChangeCharset, true);
+
+  is(gBrowser.contentDocument.documentElement.textContent.indexOf('\u0402'), 118, "Parent doc should decode as windows-1251 subsequently");
+  is(gBrowser.contentDocument.getElementsByTagName("iframe")[0].contentDocument.documentElement.textContent.indexOf('\u0432\u201A\u00AC'), 73, "Child doc should decode as windows-1251 subsequently");  
+
+  is(gBrowser.contentDocument.characterSet, "windows-1251", "Parent doc should report windows-1251 subsequently");
+  is(gBrowser.contentDocument.getElementsByTagName("iframe")[0].contentDocument.characterSet, "windows-1251", "Child doc should report windows-1251 subsequently");
+
+  gBrowser.removeCurrentTab();
+  finish();
+}
