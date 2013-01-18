31da143375628feaf325d849170e4b1c9d0f1dff
Sriram Ramasubramanian <sriram@mozilla.com>
2013-01-18 11:41:44 -0800

    Bug 832433: Use Android spinners on tabs UI for phones. [r=mfinkle]
    --HG--
    extra : rebase_source : 614269ba99d278771a293c45252ace306bdaa8d9

--- mobile/android/base/MenuPopup.java
+++ mobile/android/base/MenuPopup.java
@@ -30,6 +30,7 @@ public class MenuPopup extends PopupWindow {
     private int mYOffset;
     private int mArrowMargin;
     private int mPopupWidth;
+    private boolean mShowArrow;
 
     public MenuPopup(Context context) {
         super(context);
@@ -53,6 +54,7 @@ public class MenuPopup extends PopupWindow {
         mArrowTop = (ImageView) layout.findViewById(R.id.menu_arrow_top);
         mArrowBottom = (ImageView) layout.findViewById(R.id.menu_arrow_bottom);
         mPanel = (RelativeLayout) layout.findViewById(R.id.menu_panel);
+        mShowArrow = true;
     }
 
     /**
@@ -67,10 +69,26 @@ public class MenuPopup extends PopupWindow {
     }
 
     /**
+     * Show/hide the arrow pointing to the anchor.
+     *
+     * @param show Show/hide the arrow.
+     */
+    public void showArrowToAnchor(boolean show) {
+        mShowArrow = show;
+    }
+
+    /**
      * A small little offset for the arrow to overlap the anchor.
      */
     @Override
     public void showAsDropDown(View anchor) {
+        if (!mShowArrow) {
+            mArrowTop.setVisibility(View.GONE);
+            mArrowBottom.setVisibility(View.GONE);
+            showAsDropDown(anchor, 0, -mYOffset);
+            return;
+        }
+
         int[] anchorLocation = new int[2];
         anchor.getLocationOnScreen(anchorLocation);
 
