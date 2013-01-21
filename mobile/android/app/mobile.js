b3e046b51bd8d360a47d0b6bac70bff175293920
Mark Finkle <mfinkle@mozilla.com>
2013-01-21 14:43:06 -0500

    Bug 831123 - Disable CSS error reporting by default on mobile r=blassey, r=jmaher a=android-only

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -122,6 +122,7 @@ pref("mozilla.widget.force-24bpp", true);
 pref("mozilla.widget.use-buffer-pixmap", true);
 pref("mozilla.widget.disable-native-theme", true);
 pref("layout.reflow.synthMouseMove", false);
+pref("layout.css.report_errors", false);
 
 /* download manager (don't show the window or alert) */
 pref("browser.download.useDownloadDir", true);
