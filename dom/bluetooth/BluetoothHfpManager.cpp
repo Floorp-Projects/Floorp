a728ebf70e5530d89bb7d9eca24db41c7564e794
Gina Yeh <gyeh@mozilla.com>
2013-01-22 13:53:28 +0800

    Bug 823346 - [Bluetooth] [Hfp] Updating status change of Battery level, r=echou, a=nonlibxul

--- dom/bluetooth/BluetoothHfpManager.cpp
+++ dom/bluetooth/BluetoothHfpManager.cpp
@@ -16,6 +16,7 @@
 
 #include "MobileConnection.h"
 #include "mozilla/dom/bluetooth/BluetoothTypes.h"
+#include "mozilla/Hal.h"
 #include "mozilla/Services.h"
 #include "mozilla/StaticPtr.h"
 #include "nsContentUtils.h"
@@ -128,6 +129,7 @@ class mozilla::dom::bluetooth::Call {
 };
 
 class mozilla::dom::bluetooth::BluetoothHfpManagerObserver : public nsIObserver
+                                                           , public BatteryObserver
 {
 public:
   NS_DECL_ISUPPORTS
@@ -161,6 +163,8 @@ public:
       return false;
     }
 
+    hal::RegisterBatteryObserver(this);
+
     return true;
   }
 
@@ -175,12 +179,26 @@ public:
       NS_WARNING("Can't unregister observers, or already unregistered!");
       return false;
     }
+
+    hal::UnregisterBatteryObserver(this);
+
     return true;
   }
 
   ~BluetoothHfpManagerObserver()
   {
     Shutdown();
+  }
+
+  void Notify(const hal::BatteryInformation& aBatteryInfo)
+  {
+    // Range of battery level: [0, 1], double
+    // Range of CIND::BATTCHG: [0, 5], int
+    int level = ceil(aBatteryInfo.level() * 5.0);
+    if (level != sCINDItems[CINDType::BATTCHG].value) {
+      sCINDItems[CINDType::BATTCHG].value = level;
+      gBluetoothHfpManager->SendCommand("+CIEV: ", CINDType::BATTCHG);
+    }
   }
 };
 
