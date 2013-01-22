4d911dc3ee7a79ba417d79a8df56a8b3a96c1c70
Malini Das <mdas@mozilla.com>
2013-01-22 16:11:23 -0500

    Bug 833079 - python touch layer should check if element is displayed before acting on it, r=jgriffin

--- testing/marionette/client/marionette/errors.py
+++ testing/marionette/client/marionette/errors.py
@@ -2,10 +2,37 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+class ErrorCodes(object):
+    
+    SUCCESS = 0
+    NO_SUCH_ELEMENT = 7
+    NO_SUCH_FRAME = 8
+    UNKNOWN_COMMAND = 9
+    STALE_ELEMENT_REFERENCE = 10
+    ELEMENT_NOT_VISIBLE = 11
+    INVALID_ELEMENT_STATE = 12
+    UNKNOWN_ERROR = 13
+    ELEMENT_IS_NOT_SELECTABLE = 15
+    JAVASCRIPT_ERROR = 17
+    XPATH_LOOKUP_ERROR = 19
+    TIMEOUT = 21
+    NO_SUCH_WINDOW = 23
+    INVALID_COOKIE_DOMAIN = 24
+    UNABLE_TO_SET_COOKIE = 25
+    UNEXPECTED_ALERT_OPEN = 26
+    NO_ALERT_OPEN = 27
+    SCRIPT_TIMEOUT = 28
+    INVALID_ELEMENT_COORDINATES = 29
+    INVALID_SELECTOR = 32
+    MOVE_TARGET_OUT_OF_BOUNDS = 34
+    INVALID_XPATH_SELECTOR = 51
+    INVALID_XPATH_SELECTOR_RETURN_TYPER = 52
+    INVALID_RESPONSE = 53
+    MARIONETTE_ERROR = 500
 
 class MarionetteException(Exception):
 
-    def __init__(self, message=None, status=500, stacktrace=None):
+    def __init__(self, message=None, status=ErrorCodes.MARIONETTE_ERROR, stacktrace=None):
         self.msg = message
         self.status = status
         self.stacktrace = stacktrace
@@ -48,7 +75,9 @@ class ScriptTimeoutException(MarionetteException):
     pass
 
 class ElementNotVisibleException(MarionetteException):
-    pass
+    def __init__(self, message="Element is not currently visible and may not be manipulated",
+                status=ErrorCodes.ELEMENT_NOT_VISIBLE, stacktrace=None):
+        MarionetteException.__init__(self, message, status, stacktrace)
 
 class NoSuchFrameException(MarionetteException):
     pass
@@ -71,29 +100,3 @@ class InvalidSelectorException(MarionetteException):
 class MoveTargetOutOfBoundsException(MarionetteException):
     pass
 
-class ErrorCodes(object):
-    
-    SUCCESS = 0
-    NO_SUCH_ELEMENT = 7
-    NO_SUCH_FRAME = 8
-    UNKNOWN_COMMAND = 9
-    STALE_ELEMENT_REFERENCE = 10
-    ELEMENT_NOT_VISIBLE = 11
-    INVALID_ELEMENT_STATE = 12
-    UNKNOWN_ERROR = 13
-    ELEMENT_IS_NOT_SELECTABLE = 15
-    JAVASCRIPT_ERROR = 17
-    XPATH_LOOKUP_ERROR = 19
-    TIMEOUT = 21
-    NO_SUCH_WINDOW = 23
-    INVALID_COOKIE_DOMAIN = 24
-    UNABLE_TO_SET_COOKIE = 25
-    UNEXPECTED_ALERT_OPEN = 26
-    NO_ALERT_OPEN = 27
-    SCRIPT_TIMEOUT = 28
-    INVALID_ELEMENT_COORDINATES = 29
-    INVALID_SELECTOR = 32
-    MOVE_TARGET_OUT_OF_BOUNDS = 34
-    INVALID_XPATH_SELECTOR = 51
-    INVALID_XPATH_SELECTOR_RETURN_TYPER = 52
-    INVALID_RESPONSE = 53
--- testing/marionette/client/marionette/marionette_touch.py
+++ testing/marionette/client/marionette/marionette_touch.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
+from errors import ElementNotVisibleException
 
 """
 Adds touch support in Marionette
@@ -17,20 +18,28 @@ class MarionetteTouchMixin(object):
         self.library_name = library_name or "SyntheticGestures"
         self.import_script(self.library)
 
+    def check_element(self, element):
+        if not element.is_displayed():
+            raise ElementNotVisibleException
+
     def tap(self, element):
+        self.check_element(element)
         # we pass in touch/mouse/click events if mouse_event_shim.js isn't included in the gaia app
         # otherwise, we just send touch events. See Bug 829566
         send_all = self.execute_script("return typeof window.wrappedJSObject.MouseEventShim === 'undefined';") 
         self.execute_script("%s.tap(arguments[0], null, null, null, null, arguments[1]);" % self.library_name, [element, send_all])
 
     def double_tap(self, element):
+        self.check_element(element)
         self.execute_script("%s.dbltap(arguments[0]);" % self.library_name, [element])
 
     def long_press(self, element, holdtime=2000):
+        self.check_element(element)
         #I'm adding holdtime, since we have support for this, and we don't have chaining yet
         self.execute_script("%s.hold.apply(this, arguments);" % self.library_name, [element, holdtime, 0, 0, 0, 0, 0])
         
     def flick(self, element, x1, y1, x2, y2, duration=200):
+        self.check_element(element)
         # there's 'flick' which is pixels per second, but I'd rather have the library support it than piece it together here.
         self.execute_script("%s.swipe.apply(this, arguments);" % self.library_name, [element, x1, y1, x2, y2, duration])
 
