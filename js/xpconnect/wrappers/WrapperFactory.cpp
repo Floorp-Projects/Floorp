26a118b43a5a989e237b777a6e58a25a9c2e394b
Bobby Holley <bobbyholley@gmail.com>
2013-01-23 06:04:38 +0100

    Bug 823348 - Make NNXOWs use an explicitly opaque Policy. r=mrbkap
    There's no reason to be doing a dynamic check here, given that the JSClasses
    will never match. Lets be explicit and safe.

--- js/xpconnect/wrappers/WrapperFactory.cpp
+++ js/xpconnect/wrappers/WrapperFactory.cpp
@@ -445,8 +445,7 @@ WrapperFactory::Rewrap(JSContext *cx, JSObject *existing, JSObject *obj,
         // a predefined set of properties.
         XrayType type = GetXrayType(obj);
         if (type == NotXray) {
-            wrapper = &FilteringWrapper<CrossCompartmentSecurityWrapper,
-                                        CrossOriginAccessiblePropertiesOnly>::singleton;
+            wrapper = &FilteringWrapper<CrossCompartmentSecurityWrapper, Opaque>::singleton;
         } else if (type == XrayForDOMObject) {
             wrapper = &FilteringWrapper<SecurityXrayDOM,
                                         CrossOriginAccessiblePropertiesOnly>::singleton;
