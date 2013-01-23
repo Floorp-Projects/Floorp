fe64dbb11200fcc5bf73aba504a88950559c469f
Oleg Romashin <romaxa@gmail.com>
2013-01-23 18:08:16 -0500

    Bug 824642 - PlanarYCbCrImage::GetAsSurface does not create surface optimized for platform. r=joe, r=nsilva

--- gfx/layers/ImageContainer.cpp
+++ gfx/layers/ImageContainer.cpp
@@ -17,6 +17,7 @@
 #include "gfxSharedImageSurface.h"
 #include "yuv_convert.h"
 #include "gfxUtils.h"
+#include "gfxPlatform.h"
 
 #ifdef XP_MACOSX
 #include "mozilla/gfx/QuartzSupport.h"
@@ -477,6 +478,14 @@ PlanarYCbCrImage::SetData(const Data &aData)
   CopyData(aData);
 }
 
+gfxASurface::gfxImageFormat
+PlanarYCbCrImage::GetOffscreenFormat()
+{
+  return mOffscreenFormat != gfxASurface::ImageFormatUnknown ?
+    gfxPlatform::GetPlatform()->GetOffscreenFormat() :
+    mOffscreenFormat;
+}
+
 already_AddRefed<gfxASurface>
 PlanarYCbCrImage::GetAsSurface()
 {
@@ -486,7 +495,6 @@ PlanarYCbCrImage::GetAsSurface()
   }
 
   gfxASurface::gfxImageFormat format = GetOffscreenFormat();
-
   gfxIntSize size(mSize);
   gfxUtils::GetYCbCrToRGBDestFormatAndSize(mData, format, size);
   if (size.width > PlanarYCbCrImage::MAX_DIMENSION ||
