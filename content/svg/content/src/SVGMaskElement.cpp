1c79db0a2ec24c8c3df31172e731ba4160c98451
David Zbarsky <dzbarsky@gmail.com>
2013-01-23 15:59:06 -0500

    Bug 832153: Move SVGMaskElement to mozilla::dom r=bz

--- content/svg/content/src/SVGMaskElement.cpp
+++ content/svg/content/src/SVGMaskElement.cpp
@@ -7,13 +7,18 @@
 
 #include "nsCOMPtr.h"
 #include "nsGkAtoms.h"
-#include "nsSVGMaskElement.h"
+#include "mozilla/dom/SVGMaskElement.h"
 
-using namespace mozilla;
+DOMCI_NODE_DATA(SVGMaskElement, mozilla::dom::SVGMaskElement)
+
+NS_IMPL_NS_NEW_NAMESPACED_SVG_ELEMENT(Mask)
+
+namespace mozilla {
+namespace dom {
 
 //--------------------- Masks ------------------------
 
-nsSVGElement::LengthInfo nsSVGMaskElement::sLengthInfo[4] =
+nsSVGElement::LengthInfo SVGMaskElement::sLengthInfo[4] =
 {
   { &nsGkAtoms::x, -10, nsIDOMSVGLength::SVG_LENGTHTYPE_PERCENTAGE, SVGContentUtils::X },
   { &nsGkAtoms::y, -10, nsIDOMSVGLength::SVG_LENGTHTYPE_PERCENTAGE, SVGContentUtils::Y },
@@ -21,7 +26,7 @@ nsSVGElement::LengthInfo nsSVGMaskElement::sLengthInfo[4] =
   { &nsGkAtoms::height, 120, nsIDOMSVGLength::SVG_LENGTHTYPE_PERCENTAGE, SVGContentUtils::Y },
 };
 
-nsSVGElement::EnumInfo nsSVGMaskElement::sEnumInfo[2] =
+nsSVGElement::EnumInfo SVGMaskElement::sEnumInfo[2] =
 {
   { &nsGkAtoms::maskUnits,
     sSVGUnitTypesMap,
@@ -33,71 +38,67 @@ nsSVGElement::EnumInfo nsSVGMaskElement::sEnumInfo[2] =
   }
 };
 
-NS_IMPL_NS_NEW_SVG_ELEMENT(Mask)
-
 //----------------------------------------------------------------------
 // nsISupports methods
 
-NS_IMPL_ADDREF_INHERITED(nsSVGMaskElement,nsSVGMaskElementBase)
-NS_IMPL_RELEASE_INHERITED(nsSVGMaskElement,nsSVGMaskElementBase)
+NS_IMPL_ADDREF_INHERITED(SVGMaskElement,SVGMaskElementBase)
+NS_IMPL_RELEASE_INHERITED(SVGMaskElement,SVGMaskElementBase)
 
-DOMCI_NODE_DATA(SVGMaskElement, nsSVGMaskElement)
-
-NS_INTERFACE_TABLE_HEAD(nsSVGMaskElement)
-  NS_NODE_INTERFACE_TABLE5(nsSVGMaskElement, nsIDOMNode, nsIDOMElement,
+NS_INTERFACE_TABLE_HEAD(SVGMaskElement)
+  NS_NODE_INTERFACE_TABLE5(SVGMaskElement, nsIDOMNode, nsIDOMElement,
                            nsIDOMSVGElement,
                            nsIDOMSVGMaskElement, nsIDOMSVGUnitTypes)
   NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(SVGMaskElement)
-NS_INTERFACE_MAP_END_INHERITING(nsSVGMaskElementBase)
+NS_INTERFACE_MAP_END_INHERITING(SVGMaskElementBase)
 
 //----------------------------------------------------------------------
 // Implementation
 
-nsSVGMaskElement::nsSVGMaskElement(already_AddRefed<nsINodeInfo> aNodeInfo)
-  : nsSVGMaskElementBase(aNodeInfo)
+SVGMaskElement::SVGMaskElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+  : SVGMaskElementBase(aNodeInfo)
 {
 }
 
 //----------------------------------------------------------------------
 // nsIDOMNode method
 
-NS_IMPL_ELEMENT_CLONE_WITH_INIT(nsSVGMaskElement)
+NS_IMPL_ELEMENT_CLONE_WITH_INIT(SVGMaskElement)
 
 //----------------------------------------------------------------------
 // nsIDOMSVGMaskElement methods
 
 /* readonly attribute nsIDOMSVGAnimatedEnumeration maskUnits; */
-NS_IMETHODIMP nsSVGMaskElement::GetMaskUnits(nsIDOMSVGAnimatedEnumeration * *aMaskUnits)
+NS_IMETHODIMP SVGMaskElement::GetMaskUnits(nsIDOMSVGAnimatedEnumeration * *aMaskUnits)
 {
   return mEnumAttributes[MASKUNITS].ToDOMAnimatedEnum(aMaskUnits, this);
 }
 
 /* readonly attribute nsIDOMSVGAnimatedEnumeration maskContentUnits; */
-NS_IMETHODIMP nsSVGMaskElement::GetMaskContentUnits(nsIDOMSVGAnimatedEnumeration * *aMaskUnits)
+NS_IMETHODIMP SVGMaskElement::GetMaskContentUnits(nsIDOMSVGAnimatedEnumeration * *aMaskUnits)
 {
   return mEnumAttributes[MASKCONTENTUNITS].ToDOMAnimatedEnum(aMaskUnits, this);
 }
 
 /* readonly attribute nsIDOMSVGAnimatedLength x; */
-NS_IMETHODIMP nsSVGMaskElement::GetX(nsIDOMSVGAnimatedLength * *aX)
+NS_IMETHODIMP SVGMaskElement::GetX(nsIDOMSVGAnimatedLength * *aX)
 {
   return mLengthAttributes[X].ToDOMAnimatedLength(aX, this);
 }
 
 /* readonly attribute nsIDOMSVGAnimatedLength y; */
-NS_IMETHODIMP nsSVGMaskElement::GetY(nsIDOMSVGAnimatedLength * *aY)
+NS_IMETHODIMP SVGMaskElement::GetY(nsIDOMSVGAnimatedLength * *aY)
 {
   return mLengthAttributes[Y].ToDOMAnimatedLength(aY, this);
 }
 
 /* readonly attribute nsIDOMSVGAnimatedLength width; */
-NS_IMETHODIMP nsSVGMaskElement::GetWidth(nsIDOMSVGAnimatedLength * *aWidth)
+NS_IMETHODIMP SVGMaskElement::GetWidth(nsIDOMSVGAnimatedLength * *aWidth)
 {
   return mLengthAttributes[WIDTH].ToDOMAnimatedLength(aWidth, this);
 }
 
 /* readonly attribute nsIDOMSVGAnimatedLength height; */
-NS_IMETHODIMP nsSVGMaskElement::GetHeight(nsIDOMSVGAnimatedLength * *aHeight)
+NS_IMETHODIMP SVGMaskElement::GetHeight(nsIDOMSVGAnimatedLength * *aHeight)
 {
   return mLengthAttributes[HEIGHT].ToDOMAnimatedLength(aHeight, this);
 }
@@ -106,7 +107,7 @@ NS_IMETHODIMP nsSVGMaskElement::GetHeight(nsIDOMSVGAnimatedLength * *aHeight)
 // nsSVGElement methods
 
 /* virtual */ bool
-nsSVGMaskElement::HasValidDimensions() const
+SVGMaskElement::HasValidDimensions() const
 {
   return (!mLengthAttributes[WIDTH].IsExplicitlySet() ||
            mLengthAttributes[WIDTH].GetAnimValInSpecifiedUnits() > 0) &&
@@ -115,14 +116,14 @@ nsSVGMaskElement::HasValidDimensions() const
 }
 
 nsSVGElement::LengthAttributesInfo
-nsSVGMaskElement::GetLengthInfo()
+SVGMaskElement::GetLengthInfo()
 {
   return LengthAttributesInfo(mLengthAttributes, sLengthInfo,
                               ArrayLength(sLengthInfo));
 }
 
 nsSVGElement::EnumAttributesInfo
-nsSVGMaskElement::GetEnumInfo()
+SVGMaskElement::GetEnumInfo()
 {
   return EnumAttributesInfo(mEnumAttributes, sEnumInfo,
                             ArrayLength(sEnumInfo));
@@ -132,7 +133,7 @@ nsSVGMaskElement::GetEnumInfo()
 // nsIContent methods
 
 NS_IMETHODIMP_(bool)
-nsSVGMaskElement::IsAttributeMapped(const nsIAtom* name) const
+SVGMaskElement::IsAttributeMapped(const nsIAtom* name) const
 {
   static const MappedAttributeEntry* const map[] = {
     sFEFloodMap,
@@ -147,5 +148,8 @@ nsSVGMaskElement::IsAttributeMapped(const nsIAtom* name) const
   };
 
   return FindAttributeDependence(name, map) ||
-    nsSVGMaskElementBase::IsAttributeMapped(name);
+    SVGMaskElementBase::IsAttributeMapped(name);
 }
+
+} // namespace dom
+} // namespace mozilla
