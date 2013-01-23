880788e17fba8630a54df808e9b8cd860e904e02
Masayuki Nakano <masayuki@d-toybox.com>
2013-01-23 23:36:48 +0900

    Bug 832120 cursor: auto; should be cursor: text; on editable element r=dbaron

--- layout/generic/nsTextFrameThebes.cpp
+++ layout/generic/nsTextFrameThebes.cpp
@@ -4270,6 +4270,10 @@ nsTextFrame::GetCursor(const nsPoint& aPoint,
   FillCursorInformationFromStyle(GetStyleUserInterface(), aCursor);  
   if (NS_STYLE_CURSOR_AUTO == aCursor.mCursor) {
     aCursor.mCursor = NS_STYLE_CURSOR_TEXT;
+    // If this is editable, we should ignore tabindex value.
+    if (mContent->IsEditable()) {
+      return NS_OK;
+    }
 
     // If tabindex >= 0, use default cursor to indicate it's not selectable
     nsIFrame *ancestorFrame = this;
