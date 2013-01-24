1e7fd09cbb1800affa691feb1f3b24b9468a344d
Tim Taubert <ttaubert@mozilla.com>
2013-01-24 14:59:16 +0100

    Bug 797263 - Intermittent browser_586068-reload.js | Test timed out r=yoric

--- browser/components/sessionstore/test/browser_586068-browser_state_interrupted.js
+++ browser/components/sessionstore/test/browser_586068-browser_state_interrupted.js
@@ -4,11 +4,11 @@
 
 const PREF_RESTORE_ON_DEMAND = "browser.sessionstore.restore_on_demand";
 
-let stateBackup = ss.getBrowserState();
-
 function test() {
-  waitForExplicitFinish();
+  TestRunner.run();
+}
 
+function runTests() {
   Services.prefs.setBoolPref(PREF_RESTORE_ON_DEMAND, false);
   registerCleanupFunction(function () {
     Services.prefs.clearUserPref(PREF_RESTORE_ON_DEMAND);
@@ -89,10 +89,7 @@ function test() {
     // Remove the progress listener from this window, it will be removed from
     // theWin when that window is closed (in setBrowserState).
     gProgressListener.unsetCallback();
-    executeSoon(function () {
-      closeAllButPrimaryWindow();
-      waitForBrowserState(JSON.parse(stateBackup), finish);
-    });
+    executeSoon(next);
   });
 
   // We also want to catch the extra windows (there should be 2), so we need to observe domwindowopened
@@ -107,5 +104,5 @@ function test() {
     }
   });
 
-  ss.setBrowserState(JSON.stringify(state1));
+  yield ss.setBrowserState(JSON.stringify(state1));
 }
