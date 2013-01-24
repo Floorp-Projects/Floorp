7a21175d92426665314c6f2dce669e709341e0ff
Mike Hommey <mh+mozilla@glandium.org>
2013-01-25 00:40:13 +0100

    Bug 834228 - Force a stage-package to run before buildsymbols on universal builds. r=ted,gps
    Also, always strip and elfhack executables, so that running make package after
    a PKG_SKIP_STRIP=1 stage-package does strip ; but disable both for l10n-repacks
    and unpack.

--- toolkit/mozapps/installer/unpack.py
+++ toolkit/mozapps/installer/unpack.py
@@ -5,6 +5,7 @@
 import sys
 import os
 from mozpack.packager.unpack import unpack
+import buildconfig
 
 def main():
     if len(sys.argv) != 2:
@@ -12,6 +13,8 @@ def main():
                             os.path.basename(sys.argv[0])
         sys.exit(1)
 
+    buildconfig.substs['USE_ELF_HACK'] = False
+    buildconfig.substs['PKG_SKIP_STRIP'] = True
     unpack(sys.argv[1])
 
 if __name__ == "__main__":
