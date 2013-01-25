eccfc8c6157e31dda11833b0437f950d8e30b141
Dave Hylands <dhylands@mozilla.com>
2013-01-25 10:27:49 -0800

    Bug 831701 - Finish active updates unless the user swtiches to a newer one. Remove stale updates. r=rstrong
    Once the user starts to download an update, stick to that update until the user cancels the download and does a "Check Now".
    
    If a newer update is started and a stale update is detected, remove the stale update.
    
    If a download fails due an update being in the applied state, prompt the user to apply it.
    
    If an update was fully downloaded, but interrupted during verification, then don't redownload.

--- toolkit/mozapps/update/test/unit/test_0062_manager.js
+++ toolkit/mozapps/update/test/unit/test_0062_manager.js
@@ -26,14 +26,13 @@ function run_test() {
   standardInit();
 
   if (IS_TOOLKIT_GONK) {
-      // GONK doesn't resume downloads at boot time, so the updateCount will
-      // always be zero.
-      do_check_eq(gUpdateManager.updateCount, 0);
-      do_check_eq(gUpdateManager.activeUpdate, null);
+    // GONK doesn't resume downloads at boot time, so the updateCount will
+    // always be zero.
+    do_check_eq(gUpdateManager.updateCount, 0);
   } else {
-      do_check_eq(gUpdateManager.updateCount, 1);
-      do_check_eq(gUpdateManager.activeUpdate.state, STATE_DOWNLOADING);
+    do_check_eq(gUpdateManager.updateCount, 1);
   }
+  do_check_eq(gUpdateManager.activeUpdate.state, STATE_DOWNLOADING);
 
   do_test_finished();
 }
