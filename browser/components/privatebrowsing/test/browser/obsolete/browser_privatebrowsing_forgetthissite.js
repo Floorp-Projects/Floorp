043726fed49755ba23359bc13e7ed8bf1b63234c
Raymond Lee <raymond@raysquare.com>
2013-01-25 12:55:22 +0800

    Bug 820764 - Stop using addvisit() in browser tests r=mak77

--- browser/components/privatebrowsing/test/browser/obsolete/browser_privatebrowsing_forgetthissite.js
+++ browser/components/privatebrowsing/test/browser/obsolete/browser_privatebrowsing_forgetthissite.js
@@ -32,10 +32,34 @@ function test() {
   // Add a history entry.
   const TEST_URI = "http://www.mozilla.org/privatebrowsing";
   ok(PlacesUtils, "checking PlacesUtils, running in chrome context?");
-  let history = PlacesUtils.history;
-  let visitId = history.addVisit(PlacesUtils._uri(TEST_URI), Date.now() * 1000,
-                                 null, PlacesUtils.history.TRANSITION_TYPED, false, 0);
-  ok(visitId > 0, TEST_URI + " successfully marked visited");
+
+  let place = {
+    uri: PlacesUtils._uri(TEST_URI),
+    visits: [{
+      visitDate: Date.now() * 1000,
+      transitionType: PlacesUtils.history.TRANSITION_TYPED
+    }]
+  }
+  PlacesUtils.asyncHistory.updatePlaces(place, {
+    handleError: function () ok(false, "couldn't add visit"),
+    handleResult: function () {},
+    handleCompletion: function () {
+      ok(true, TEST_URI + " successfully marked visited");
+
+      testForgetThisSiteVisibility(true, function() {
+        // Enter private browsing mode
+        pb.privateBrowsingEnabled = true;
+        testForgetThisSiteVisibility(false, function() {
+          // Leave private browsing mode
+          pb.privateBrowsingEnabled = false;
+          testForgetThisSiteVisibility(true, function() {
+            // Cleanup
+            waitForClearHistory(finish);
+          });
+        });
+      });
+    }
+  });
 
   function testForgetThisSiteVisibility(expected, funcNext) {
     function observer(aSubject, aTopic, aData) {
@@ -102,17 +126,4 @@ function test() {
                            "chrome,toolbar=yes,dialog=no,resizable",
                            null);
   }
-
-  testForgetThisSiteVisibility(true, function() {
-    // Enter private browsing mode
-    pb.privateBrowsingEnabled = true;
-    testForgetThisSiteVisibility(false, function() {
-      // Leave private browsing mode
-      pb.privateBrowsingEnabled = false;
-      testForgetThisSiteVisibility(true, function() {
-        // Cleanup
-        waitForClearHistory(finish);
-      });
-    });
-  });
 }
