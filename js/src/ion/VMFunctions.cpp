c3d4a28c01787f2b3de3e294906d969b477d2e12
Hannes Verschore <hv1989@gmail.com>
2013-01-27 22:51:49 +0100

    Bug 834500: IonMonkey: Don't forbid ion compilation when JM is disabled and bouncing between IM and the interpreter, r=nbp

--- js/src/ion/VMFunctions.cpp
+++ js/src/ion/VMFunctions.cpp
@@ -52,13 +52,11 @@ InvokeFunction(JSContext *cx, HandleFunction fun0, uint32_t argc, Value *argv, V
     AssertCanGC();
 
     RootedFunction fun(cx, fun0);
-
-    // In order to prevent massive bouncing between Ion and JM, see if we keep
-    // hitting functions that are uncompilable.
     if (fun->isInterpreted()) {
         if (fun->isInterpretedLazy() && !fun->getOrCreateScript(cx))
             return false;
 
+        // Clone function at call site if needed.
         if (fun->isCloneAtCallsite()) {
             RootedScript script(cx);
             jsbytecode *pc;
@@ -68,7 +66,9 @@ InvokeFunction(JSContext *cx, HandleFunction fun0, uint32_t argc, Value *argv, V
                 return false;
         }
 
-        if (!fun->nonLazyScript()->canIonCompile()) {
+        // In order to prevent massive bouncing between Ion and JM, see if we keep
+        // hitting functions that are uncompilable.
+        if (cx->methodJitEnabled && !fun->nonLazyScript()->canIonCompile()) {
             UnrootedScript script = GetTopIonJSScript(cx);
             if (script->hasIonScript() &&
                 ++script->ion->slowCallCount >= js_IonOptions.slowCallLimit)
