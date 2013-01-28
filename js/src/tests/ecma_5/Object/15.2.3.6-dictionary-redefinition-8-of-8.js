7a5644181de662e5976cb50b1f7994e485209e9c
Steve Fink <sfink@mozilla.com>
2013-01-28 13:22:02 -0800

    Bug 835552 - Make load() be script-relative and read()/snarf() be cwd-relative. r=njn

--- js/src/tests/ecma_5/Object/15.2.3.6-dictionary-redefinition-8-of-8.js
+++ js/src/tests/ecma_5/Object/15.2.3.6-dictionary-redefinition-8-of-8.js
@@ -2,5 +2,5 @@
 // Any copyright is dedicated to the Public Domain.
 // http://creativecommons.org/licenses/publicdomain/
 
-load("ecma_5/Object/defineProperty-setup.js");
+load("defineProperty-setup.js");
 runDictionaryPropertyPresentTestsFraction(8, 8);
