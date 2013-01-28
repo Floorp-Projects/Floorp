46036aa594e9c150be490df7ee95b2e377d014fe
Ed Morley <emorley@mozilla.com>
2013-01-28 10:44:15 +0000

    Backout aad75c7c49c5:2addf5bd1730 (bug 830164) for B2G marionette failures on a CLOSED TREE

--- dom/system/gonk/RadioInterfaceLayer.js
+++ dom/system/gonk/RadioInterfaceLayer.js
@@ -215,8 +215,7 @@ function RadioInterfaceLayer() {
   this.rilContext = {
     radioState:     RIL.GECKO_RADIOSTATE_UNAVAILABLE,
     cardState:      RIL.GECKO_CARDSTATE_UNAVAILABLE,
-    imsi:           null,
-    iccInfo:        null,
+    icc:            null,
 
     // These objects implement the nsIDOMMozMobileConnectionInfo interface,
     // although the actual implementation lives in the content process. So are
@@ -608,9 +607,6 @@ RadioInterfaceLayer.prototype = {
       case "iccinfochange":
         this.handleICCInfoChange(message);
         break;
-      case "iccimsi":
-        this.rilContext.imsi = message.imsi;
-        break;
       case "iccGetCardLock":
       case "iccSetCardLock":
       case "iccUnlockCardLock":
@@ -827,19 +823,19 @@ RadioInterfaceLayer.prototype = {
     *                      roaming state will be changed (maybe, if needed).
     */
   checkRoamingBetweenOperators: function checkRoamingBetweenOperators(registration) {
-    let iccInfo = this.rilContext.iccInfo;
-    if (!iccInfo || !registration.connected) {
+    let icc = this.rilContext.icc;
+    if (!icc || !registration.connected) {
       return;
     }
 
-    let spn = iccInfo.spn && iccInfo.spn.toLowerCase();
+    let spn = icc.spn && icc.spn.toLowerCase();
     let operator = registration.network;
     let longName = operator.longName && operator.longName.toLowerCase();
     let shortName = operator.shortName && operator.shortName.toLowerCase();
 
     let equalsLongName = longName && (spn == longName);
     let equalsShortName = shortName && (spn == shortName);
-    let equalsMcc = iccInfo.mcc == operator.mcc;
+    let equalsMcc = icc.mcc == operator.mcc;
 
     registration.roaming = registration.roaming &&
                            !(equalsMcc && (equalsLongName || equalsShortName));
@@ -1387,7 +1383,7 @@ RadioInterfaceLayer.prototype = {
       bearer: WAP.WDP_BEARER_GSM_SMS_GSM_MSISDN,
       sourceAddress: message.sender,
       sourcePort: message.header.originatorPort,
-      destinationAddress: this.rilContext.iccInfo.msisdn,
+      destinationAddress: this.rilContext.icc.msisdn,
       destinationPort: message.header.destinationPort,
     };
     WAP.WapPushManager.receiveWdpPDU(message.fullData, message.fullData.length,
