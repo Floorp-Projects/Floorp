87fc6e7ab2ff1015eb14b0622a3f16b043b70844
Cameron McCormack <cam@mcc.id.au>
2013-01-29 15:59:55 +1100

    Bug 830278 - Allow disabling of selecting printing with mozdisallowselectionprint on the document element. r=roc

--- layout/printing/nsPrintEngine.cpp
+++ layout/printing/nsPrintEngine.cpp
@@ -236,7 +236,8 @@ nsPrintEngine::nsPrintEngine() :
   mDebugFile(nullptr),
   mLoadCounter(0),
   mDidLoadDataForPrinting(false),
-  mIsDestroying(false)
+  mIsDestroying(false),
+  mDisallowSelectionPrint(false)
 {
 }
 
@@ -559,7 +560,8 @@ nsPrintEngine::DoCommonPrint(bool                    aIsPrintPreview,
     mPrt->mPrintSettings->SetHowToEnableFrameUI(nsIPrintSettings::kFrameEnableNone);
   }
   // Now determine how to set up the Frame print UI
-  mPrt->mPrintSettings->SetPrintOptions(nsIPrintSettings::kEnableSelectionRB, isSelection || mPrt->mIsIFrameSelected);
+  mPrt->mPrintSettings->SetPrintOptions(nsIPrintSettings::kEnableSelectionRB,
+                                        !mDisallowSelectionPrint && (isSelection || mPrt->mIsIFrameSelected));
 
   nsCOMPtr<nsIDeviceContextSpec> devspec
     (do_CreateInstance("@mozilla.org/gfx/devicecontextspec;1", &rv));
