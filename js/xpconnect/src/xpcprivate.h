33ab8e324e7df280fc13240b4f533bf2ddcde1b9
Boris Zbarsky <bzbarsky@mit.edu>
2013-01-29 09:42:14 -0500

    Bug 834877 part 3.  Add faster DOMString-to-JS conversion code.  r=peterv

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -3423,25 +3423,6 @@ private:
 };
 
 /***************************************************************************/
-
-// readable string conversions, static methods only
-class XPCStringConvert
-{
-public:
-
-    // If the string shares the readable's buffer, that buffer will
-    // get assigned to *sharedBuffer.  Otherwise null will be
-    // assigned.
-    static jsval ReadableToJSVal(JSContext *cx, const nsAString &readable,
-                                 nsStringBuffer** sharedBuffer);
-
-    static void ClearCache();
-
-private:
-    XPCStringConvert();         // not implemented
-};
-
-/***************************************************************************/
 // code for throwing exceptions into JS
 
 class XPCThrower
