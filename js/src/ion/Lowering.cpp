a4359707363c43a80844968e0b5f35c1f2f18e3d
Luke Wagner <luke@mozilla.com>
2013-01-29 08:29:51 -0800

    Bug 835578 - IonMonkey: fix visitCompare/visitTest to not useRegister on constant operands (r=dvander)

--- js/src/ion/Lowering.cpp
+++ js/src/ion/Lowering.cpp
@@ -500,9 +500,11 @@ LIRGenerator::visitTest(MTest *test)
         {
             JSOp op = ReorderComparison(comp->jsop(), &left, &right);
             LAllocation lhs = useRegister(left);
-            LAllocation rhs = useRegister(right);
+            LAllocation rhs;
             if (comp->compareType() == MCompare::Compare_Int32)
                 rhs = useAnyOrConstant(right);
+            else
+                rhs = useRegister(right);
             LCompareAndBranch *lir = new LCompareAndBranch(op, lhs, rhs, ifTrue, ifFalse);
             return add(lir, comp);
         }
@@ -644,9 +646,11 @@ LIRGenerator::visitCompare(MCompare *comp)
     {
         JSOp op = ReorderComparison(comp->jsop(), &left, &right);
         LAllocation lhs = useRegister(left);
-        LAllocation rhs = useRegister(right);
+        LAllocation rhs;
         if (comp->compareType() == MCompare::Compare_Int32)
             rhs = useAnyOrConstant(right);
+        else
+            rhs = useRegister(right);
         return define(new LCompare(op, lhs, rhs), comp);
     }
 
