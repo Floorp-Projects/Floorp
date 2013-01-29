29d6fed9e1ea4534ddda0e9d1b32586c06e0f30b
Eric Chou <echou@mozilla.com>
2013-01-29 18:37:51 +0800

    Bug 834551 - Insert BT_LOG at several important check points, r=gyeh

--- dom/bluetooth/BluetoothAdapter.cpp
+++ dom/bluetooth/BluetoothAdapter.cpp
@@ -273,6 +273,8 @@ BluetoothAdapter::Notify(const BluetoothSignal& aData)
 {
   InfallibleTArray<BluetoothNamedValue> arr;
 
+  BT_LOG("[A] %s: %s", __FUNCTION__, NS_ConvertUTF16toUTF8(aData.name()).get());
+
   BluetoothValue v = aData.value();
   if (aData.name().EqualsLiteral("DeviceFound")) {
     nsRefPtr<BluetoothDevice> device = BluetoothDevice::Create(GetOwner(), mPath, aData.value());
