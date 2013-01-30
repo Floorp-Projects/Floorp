ce2c0911f24009b3c9ca8ec77225af25bdf1c06b
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2013-01-30 07:41:01 -0800

    Bug 796114 - Inline with type-checked arguments. r=h4writer

--- js/src/ion/IonBuilder.cpp
+++ js/src/ion/IonBuilder.cpp
@@ -2908,7 +2908,7 @@ IonBuilder::inlineScriptedCall(HandleFunction target, CallInfo &callInfo)
 {
     AssertCanGC();
     JS_ASSERT(target->isInterpreted());
-    JS_ASSERT(callInfo.hasTypeInfo());
+    JS_ASSERT(callInfo.hasCallType());
 
     // Remove the MPassArg if still present.
     if (callInfo.isWrapped())
@@ -2944,9 +2944,21 @@ IonBuilder::inlineScriptedCall(HandleFunction target, CallInfo &callInfo)
     callInfo.popFormals(current);
     current->push(callInfo.fun());
 
+    RootedScript calleeScript(cx, target->nonLazyScript());
+    TypeInferenceOracle oracle;
+    if (!oracle.init(cx, calleeScript))
+        return false;
+
+    // Add exclude type barriers.
+    if (callInfo.argsBarrier()) {
+        addTypeBarrier(0, callInfo, oracle.thisTypeSet(calleeScript));
+        int32_t max = (callInfo.argc() < target->nargs) ? callInfo.argc() : target->nargs;
+        for (int32_t i = 1; i <= max; i++)
+            addTypeBarrier(i, callInfo, oracle.parameterTypeSet(calleeScript, i - 1));
+    }
+
     // Start inlining
     LifoAlloc *alloc = GetIonContext()->temp->lifoAlloc();
-    RootedScript calleeScript(cx, target->nonLazyScript());
     CompileInfo *info = alloc->new_<CompileInfo>(calleeScript.get(), target,
                                                  (jsbytecode *)NULL, callInfo.constructing(),
                                                  SequentialExecution);
@@ -2955,10 +2967,6 @@ IonBuilder::inlineScriptedCall(HandleFunction target, CallInfo &callInfo)
 
     MIRGraphExits saveExits;
     AutoAccumulateExits aae(graph(), saveExits);
-
-    TypeInferenceOracle oracle;
-    if (!oracle.init(cx, calleeScript))
-        return false;
 
     IonBuilder inlineBuilder(cx, &temp(), &graph(), &oracle,
                              info, inliningDepth + 1, loopDepth_);
@@ -3005,6 +3013,68 @@ IonBuilder::inlineScriptedCall(HandleFunction target, CallInfo &callInfo)
     current = bottom;
 
     return true;
+}
+
+void
+IonBuilder::addTypeBarrier(uint32_t i, CallInfo &callinfo, types::StackTypeSet *calleeObs)
+{
+    MDefinition *ins = NULL;
+    types::StackTypeSet *callerObs = NULL;
+    types::TypeBarrier *excluded = callinfo.argsBarrier();
+
+    if (i == 0) {
+        ins = callinfo.thisArg();
+        callerObs = callinfo.thisType();
+    } else {
+        ins = callinfo.getArg(i - 1);
+        callerObs = callinfo.getArgType(i - 1);
+    }
+
+    while (excluded) {
+        if (excluded->target == calleeObs) {
+            JS_ASSERT(callerObs->hasType(excluded->type));
+
+            if (excluded->type == types::Type::DoubleType() &&
+                calleeObs->hasType(types::Type::Int32Type())) {
+                // The double type also implies int32, so this implies that
+                // double should be coerced into int if possible, and other
+                // types should remain.
+
+                JSValueType callerType = callerObs->getKnownTypeTag();
+                if (callerType == JSVAL_TYPE_DOUBLE) {
+                    MInstruction *bailType = MToInt32::New(ins);
+                    current->add(bailType);
+                    ins = bailType;
+                    break;
+                } else {
+                    // We expect either an Int or a Value, this variant is not
+                    // optimized and favor the int variant by filtering out all
+                    // other inputs.
+                    JS_ASSERT(callerType == JSVAL_TYPE_UNKNOWN);
+                    // Bail if the input is not a number.
+                    MInstruction *toDouble = MUnbox::New(ins, MIRType_Double, MUnbox::Fallible);
+                    // Bail if the double does not fit in an int.
+                    MInstruction *toInt = MToInt32::New(ins);
+                    current->add(toDouble);
+                    current->add(toInt);
+                    ins = toInt;
+                }
+            } else {
+                JS_ASSERT(!calleeObs->hasType(excluded->type));
+
+                // Filter out unexpected type which are not yet added to the set
+                // observed type but which are infered by type inference.
+                MInstruction *bailType = MExcludeType::New(ins, excluded->type);
+                current->add(bailType);
+            }
+        }
+        excluded = excluded->next;
+    }
+
+    if (i == 0)
+        callinfo.setThis(ins);
+    else
+        callinfo.setArg(i - 1, ins);
 }
 
 MDefinition *
@@ -3065,6 +3135,7 @@ IonBuilder::jsop_call_inline(HandleFunction callee, CallInfo &callInfo, MBasicBl
                              Vector<MDefinition *, 8, IonAllocPolicy> &retvalDefns)
 {
     AssertCanGC();
+    JS_ASSERT(callInfo.hasCallType());
 
     // Push formals to capture them in the inline resume point.
     int calleePos = -((int) callInfo.argc() + 2);
@@ -3086,8 +3157,20 @@ IonBuilder::jsop_call_inline(HandleFunction callee, CallInfo &callInfo, MBasicBl
     // Inlining JSOP_FUNCALL uses inlineScriptedCall
     JS_ASSERT(callInfo.argc() == GET_ARGC(inlineResumePoint->pc()));
 
-    LifoAlloc *alloc = GetIonContext()->temp->lifoAlloc();
     RootedScript calleeScript(cx, callee->nonLazyScript());
+    TypeInferenceOracle oracle;
+    if (!oracle.init(cx, calleeScript))
+        return false;
+
+    // Add exclude type barriers.
+    if (callInfo.argsBarrier()) {
+        addTypeBarrier(0, callInfo, oracle.thisTypeSet(calleeScript));
+        int32_t max = (callInfo.argc() < callee->nargs) ? callInfo.argc() : callee->nargs;
+        for (int32_t i = 1; i <= max; i++)
+            addTypeBarrier(i, callInfo, oracle.parameterTypeSet(calleeScript, i - 1));
+    }
+
+    LifoAlloc *alloc = GetIonContext()->temp->lifoAlloc();
     CompileInfo *info = alloc->new_<CompileInfo>(calleeScript.get(), callee,
                                                  (jsbytecode *)NULL, callInfo.constructing(),
                                                  SequentialExecution);
@@ -3096,10 +3179,6 @@ IonBuilder::jsop_call_inline(HandleFunction callee, CallInfo &callInfo, MBasicBl
 
     MIRGraphExits saveExits;
     AutoAccumulateExits aae(graph(), saveExits);
-
-    TypeInferenceOracle oracle;
-    if (!oracle.init(cx, calleeScript))
-        return false;
 
     IonBuilder inlineBuilder(cx, &temp(), &graph(), &oracle,
                              info, inliningDepth + 1, loopDepth_);
@@ -3508,14 +3587,19 @@ IonBuilder::inlineScriptedCalls(AutoObjectVector &targets, AutoObjectVector &ori
         // guaranteed to fail.
         JSFunction *func = originals[i]->toFunction();
         MConstant *constFun = MConstant::New(ObjectValue(*func));
+        top->add(constFun);
 
         // Create new entry block for the inlined callee graph.
         MBasicBlock *entryBlock = newBlock(current, pc);
         if (!entryBlock)
             return false;
 
+        // Modify the resume point to account for the callee selection.
+        int funIndex = entryBlock->entryResumePoint()->numOperands();
+        funIndex -= ((int) callInfo.argc() + 2);
+        entryBlock->entryResumePoint()->replaceOperand(funIndex, constFun);
+
         // Add case to PolyInlineDispatch
-        entryBlock->add(constFun);
         disp->addCallee(constFun, entryBlock);
     }
     top->end(disp);
@@ -4035,6 +4119,9 @@ IonBuilder::jsop_funapplyarguments(uint32_t argc)
     if (!args.append(inlinedArguments_.begin(), inlinedArguments_.end()))
         return false;
     callInfo.setArgs(&args);
+    RootedScript scriptRoot(cx, script());
+    if (!callInfo.initFunApplyArguments(oracle, scriptRoot, pc, info().nargs()))
+        return false;
 
     // This
     MPassArg *passThis = current->pop()->toPassArg();
@@ -4119,6 +4206,8 @@ IonBuilder::jsop_call(uint32_t argc, bool constructing)
     }
 
     // Inline scriped call(s).
+    if (!callInfo.initCallType(oracle, scriptRoot, pc))
+        return false;
     if (inliningEnabled() && targets.length() > 0 && makeInliningDecision(targets))
         return inlineScriptedCalls(targets, originals, callInfo);
 
