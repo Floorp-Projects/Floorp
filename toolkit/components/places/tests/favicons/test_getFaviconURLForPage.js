cbfc8d37c88cd00cef7a75e7c9114aad194f9199
Raymond Lee <raymond@raysquare.com>
2013-01-31 15:10:46 +0800

    Bug 816903 - Remove calls to addVisits from Places tests. r=mak

--- toolkit/components/places/tests/favicons/test_getFaviconURLForPage.js
+++ toolkit/components/places/tests/favicons/test_getFaviconURLForPage.js
@@ -17,7 +17,7 @@ add_test(function test_normal()
 {
   let pageURI = NetUtil.newURI("http://example.com/normal");
 
-  addVisits(pageURI, function () {
+  promiseAddVisits(pageURI).then(function () {
     PlacesUtils.favicons.setAndFetchFaviconForPage(
       pageURI, SMALLPNG_DATA_URI, true,
         PlacesUtils.favicons.FAVICON_LOAD_NON_PRIVATE,
