be352aedf905876670e2203ba6a8b0cd38e9c456
Chris Jones <jones.chris.g@gmail.com>
2013-01-30 22:40:43 -0800

    Bug 836593: Add profiler labels to organize samples under FrameLoader::StartLoading(), IFrameElement::BindToTree(), and TabParent creation code. r=jlebar,smaug

--- content/base/src/nsFrameLoader.cpp
+++ content/base/src/nsFrameLoader.cpp
@@ -411,6 +411,8 @@ nsFrameLoader::ReallyStartLoadingInternal()
 {
   NS_ENSURE_STATE(mURIToLoad && mOwnerContent && mOwnerContent->IsInDoc());
 
+  SAMPLE_LABEL("nsFrameLoader", "ReallyStartLoading");
+
   nsresult rv = MaybeCreateDocShell();
   if (NS_FAILED(rv)) {
     return rv;
@@ -2029,6 +2031,8 @@ nsFrameLoader::TryRemoteBrowser()
   if (NS_FAILED(window->GetChromeFlags(&chromeFlags))) {
     return false;
   }
+
+  SAMPLE_LABEL("nsFrameLoader", "CreateRemoteBrowser");
 
   MutableTabContext context;
   nsCOMPtr<mozIApplication> ownApp = GetOwnApp();
