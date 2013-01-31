80cbc1dfe46da09908372c4cead58b7c84c8d832
Kai Engert <kaie@kuix.de>
2013-02-01 00:31:20 +0100

    Bug 834741, landing final NSPR 4.9.5 and final NSS 3.14.2, r=wtc. Keeping the locally applied patch, but bumping its version tag, to indicate it hasn't been part of an official NSS release yet.

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -4,7 +4,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-/* $Id: nss.h,v 1.101 2012/12/10 23:39:39 wtc%google.com Exp $ */
+/* $Id: nss.h,v 1.102 2013/01/31 22:47:08 kaie%kuix.de Exp $ */
 
 #ifndef __nss_h_
 #define __nss_h_
@@ -34,12 +34,12 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define NSS_VERSION  "3.14.2.0" _NSS_ECC_STRING _NSS_CUSTOMIZED " Beta"
+#define NSS_VERSION  "3.14.2.0" _NSS_ECC_STRING _NSS_CUSTOMIZED
 #define NSS_VMAJOR   3
 #define NSS_VMINOR   14
 #define NSS_VPATCH   2
 #define NSS_VBUILD   0
-#define NSS_BETA     PR_TRUE
+#define NSS_BETA     PR_FALSE
 
 #ifndef RC_INVOKED
 
