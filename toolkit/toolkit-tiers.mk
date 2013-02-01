716d40560be783d954e02a70f20e821d44f50d51
Gregory Szorc <gps@mozilla.com>
2013-02-01 12:26:07 -0800

    Bug 809920 - Create testing/modules for shared testing-only JS modules; r=ted
    Includes AppInfo.jsm with code for manipulating XULAppInfo.
    
    --HG--
    rename : services/healthreport/modules-testing/utils.jsm => testing/modules/AppInfo.jsm

--- toolkit/toolkit-tiers.mk
+++ toolkit/toolkit-tiers.mk
@@ -324,6 +324,7 @@ tier_platform_dirs += testing/xpcshell
 tier_platform_dirs += testing/tools/screenshot
 tier_platform_dirs += testing/peptest
 tier_platform_dirs += testing/mozbase
+tier_platform_dirs += testing/modules
 ifdef MOZ_WEBRTC
 tier_platform_dirs += media/webrtc/signaling/test
 tier_platform_dirs += media/mtransport/test
