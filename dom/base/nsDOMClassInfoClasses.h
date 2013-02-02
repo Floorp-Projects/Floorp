c76b5ebc61e221e5adfa9033664a3a5d24366277
David Zbarsky <dzbarsky@gmail.com>
2013-02-02 15:22:37 -0500

    Bug 833446: Remove nsIDOMSVGSetElement r=peterv

--- dom/base/nsDOMClassInfoClasses.h
+++ dom/base/nsDOMClassInfoClasses.h
@@ -193,7 +193,6 @@ DOMCI_CLASS(SVGDocument)
 // SVG element classes
 DOMCI_CLASS(SVGAElement)
 DOMCI_CLASS(SVGMpathElement)
-DOMCI_CLASS(SVGSetElement)
 DOMCI_CLASS(TimeEvent)
 DOMCI_CLASS(SVGCircleElement)
 DOMCI_CLASS(SVGClipPathElement)
