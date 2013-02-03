c86b399d5898a62bd89d1b58b199d478c3aea27d
Ehsan Akhgari <ehsan@mozilla.com>
2013-02-03 15:18:19 -0500

    Backed out 3 changesets (bug 837416) because of test failures
    Backed out changeset 1d7232646d12 (bug 837416)
    Backed out changeset 476af8f75f28 (bug 837416)
    Backed out changeset 428b29658d07 (bug 837416)

--- dom/webidl/WebIDL.mk
+++ dom/webidl/WebIDL.mk
@@ -76,7 +76,6 @@ webidl_files = \
   HTMLPropertiesCollection.webidl \
   HTMLScriptElement.webidl \
   HTMLSpanElement.webidl \
-  HTMLStyleElement.webidl \
   HTMLTableCaptionElement.webidl \
   HTMLTableCellElement.webidl \
   HTMLTableColElement.webidl \
@@ -86,7 +85,6 @@ webidl_files = \
   HTMLTitleElement.webidl \
   HTMLUListElement.webidl \
   ImageData.webidl \
-  LinkStyle.webidl \
   Location.webidl \
   MutationObserver.webidl \
   Node.webidl \
