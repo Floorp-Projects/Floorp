fd3a81ecc606a2ec0642cf5f3d421029a5cc5596
Robert O'Callahan <robert@ocallahan.org>
2013-02-05 10:55:50 +1300

    Bug 804387. Part 7.2: Let BiquadFilterNode use the actual context sample rate. r=ehsan

--- content/media/webaudio/BiquadFilterNode.cpp
+++ content/media/webaudio/BiquadFilterNode.cpp
@@ -22,9 +22,7 @@ NS_IMPL_RELEASE_INHERITED(BiquadFilterNode, AudioNode)
 static float
 Nyquist(AudioContext* aContext)
 {
-  // TODO: Replace the hardcoded 44100 here with AudioContext::SampleRate()
-  // when we implement that.
-  return 0.5f * 44100;
+  return 0.5f * aContext->SampleRate();
 }
 
 BiquadFilterNode::BiquadFilterNode(AudioContext* aContext)
