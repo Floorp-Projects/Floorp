2565119691f538f0a2deccc27e10dd907f16e9ed
Bobby Holley <bobbyholley@gmail.com>
2013-02-04 15:13:14 +0000

    Bug 831076 - Enter a compartment in jsd. r=mrbkap

--- js/jsd/jsd_val.cpp
+++ js/jsd/jsd_val.cpp
@@ -323,6 +323,7 @@ jsd_GetValueWrappedJSVal(JSDContext* jsdc, JSDValue* jsdval)
     jsval val = jsdval->val;
     if (!JSVAL_IS_PRIMITIVE(val)) {
         cx = JSD_GetDefaultJSContext(jsdc);
+        JSAutoCompartment ac(cx, JSVAL_TO_OBJECT(val));
         obj = JS_ObjectToOuterObject(cx, JSVAL_TO_OBJECT(val));
         if (!obj)
         {
