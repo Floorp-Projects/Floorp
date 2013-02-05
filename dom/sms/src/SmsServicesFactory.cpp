0c33c793c12f1f56aafb2585cb7741e1ce484534
Gene Lian <clian@mozilla.com>
2013-02-05 19:10:39 +0800

    Bug 831683 - B2G SMS & B2G MMS: make SMS database more generic for MMS (part 1, s/SmsDatabaseService/MobileMessageDatabaseService). r=vicamo

--- dom/sms/src/SmsServicesFactory.cpp
+++ dom/sms/src/SmsServicesFactory.cpp
@@ -12,7 +12,7 @@
 #endif
 #include "nsServiceManagerUtils.h"
 
-#define RIL_SMS_DATABASE_SERVICE_CONTRACTID "@mozilla.org/sms/rilsmsdatabaseservice;1"
+#define RIL_MOBILE_MESSAGE_DATABASE_SERVICE_CONTRACTID "@mozilla.org/mobilemessage/rilmobilemessagedatabaseservice;1"
 
 namespace mozilla {
 namespace dom {
@@ -32,21 +32,21 @@ SmsServicesFactory::CreateSmsService()
   return smsService.forget();
 }
 
-/* static */ already_AddRefed<nsISmsDatabaseService>
-SmsServicesFactory::CreateSmsDatabaseService()
+/* static */ already_AddRefed<nsIMobileMessageDatabaseService>
+SmsServicesFactory::CreateMobileMessageDatabaseService()
 {
-  nsCOMPtr<nsISmsDatabaseService> smsDBService;
+  nsCOMPtr<nsIMobileMessageDatabaseService> mobileMessageDBService;
   if (XRE_GetProcessType() == GeckoProcessType_Content) {
-    smsDBService = new SmsIPCService();
+    mobileMessageDBService = new SmsIPCService();
   } else {
 #ifdef MOZ_B2G_RIL
-    smsDBService = do_GetService(RIL_SMS_DATABASE_SERVICE_CONTRACTID);
+    mobileMessageDBService = do_GetService(RIL_MOBILE_MESSAGE_DATABASE_SERVICE_CONTRACTID);
 #else
-    smsDBService = new SmsDatabaseService();
+    mobileMessageDBService = new MobileMessageDatabaseService();
 #endif
   }
 
-  return smsDBService.forget();
+  return mobileMessageDBService.forget();
 }
 
 } // namespace sms
--- dom/sms/src/android/SmsDatabaseService.h
+++ dom/sms/src/android/SmsDatabaseService.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_sms_SmsDatabaseService_h
-#define mozilla_dom_sms_SmsDatabaseService_h
+#ifndef mozilla_dom_sms_MobileMessageDatabaseService_h
+#define mozilla_dom_sms_MobileMessageDatabaseService_h
 
 #include "nsISmsDatabaseService.h"
 #include "mozilla/Attributes.h"
@@ -13,15 +13,15 @@ namespace mozilla {
 namespace dom {
 namespace sms {
 
-class SmsDatabaseService MOZ_FINAL : public nsISmsDatabaseService
+class MobileMessageDatabaseService MOZ_FINAL : public nsIMobileMessageDatabaseService
 {
 public:
   NS_DECL_ISUPPORTS
-  NS_DECL_NSISMSDATABASESERVICE
+  NS_DECL_NSIMOBILEMESSAGEDATABASESERVICE
 };
 
 } // namespace sms
 } // namespace dom
 } // namespace mozilla
 
-#endif // mozilla_dom_sms_SmsDatabaseService_h
+#endif // mozilla_dom_sms_MobileMessageDatabaseService_h
