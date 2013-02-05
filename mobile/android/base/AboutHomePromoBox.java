faa285b7b4b6dfbfe97b134aeac0dd620007761c
Mark Finkle <mfinkle@mozilla.com>
2013-02-05 08:52:57 -0500

    Bug 837142 - Regression: about:home promo banner swaps on device rotation r=wesj

--- mobile/android/base/AboutHomePromoBox.java
+++ mobile/android/base/AboutHomePromoBox.java
@@ -83,6 +83,7 @@ public class AboutHomePromoBox extends TextView implements View.OnClickListener 
         }
     }
 
+    private static int sTypeIndex = -1;
     private ArrayList<Type> mTypes;
     private Type mType;
 
@@ -144,8 +145,13 @@ public class AboutHomePromoBox extends TextView implements View.OnClickListener 
                     hide();
                     return;
                 }
-                int idx = new Random().nextInt(types.size());
-                mType = types.get(idx);
+
+                // Try to maintain a promo type for the lifetime of the application
+                if (AboutHomePromoBox.sTypeIndex == -1) {
+                    AboutHomePromoBox.sTypeIndex = new Random().nextInt(types.size());
+                }
+                mType = types.get(AboutHomePromoBox.sTypeIndex);
+
                 updateViewResources();
                 setVisibility(View.VISIBLE);
             }
