29b00423b293b294752f565b875e9104d18b36b8
Brian Hackett <bhackett1024@gmail.com>
2013-02-05 09:48:55 -0700

    Bug 836705 - Fix bogus assert when loading from definitely-double arrays, r=jandem.

---
+++ js/src/jit-test/tests/ion/bug836705.js
@@ -0,0 +1,12 @@
+
+Object.prototype[3] = 3;
+var sjcl = {
+    cipher: {},
+};
+sjcl.cipher.aes = function (a) {
+    d = a.slice(0);
+    for (a=0; a < 60; a++) {
+        c = d[a - 1];
+    }
+};
+new sjcl.cipher.aes([0xffffffff, 0xffffffff]);
