0daf84bc8f2b86575b062a1677a1b8fd17757d43
Ted Mielczarek <ted.mielczarek@gmail.com>
2013-02-06 18:58:26 -0500

    bug 838882 - Add ability to apply local patches to update-breakpad.sh NPOTB
    --HG--
    extra : rebase_source : eca6cf7c6a11f57c5318fe66aa1cb0ab06d4d676

--- toolkit/crashreporter/update-breakpad.sh
+++ toolkit/crashreporter/update-breakpad.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -23,5 +24,21 @@ hg -R ${repo} st -n | grep "objs\.mk$" | xargs hg revert --no-backup
 
 # Record `svn info`
 svn info $1 > ${crashreporter_dir}/google-breakpad/SVN-INFO
+
+# Apply any local patches
+shopt -s nullglob
+for p in ${crashreporter_dir}/breakpad-patches/*.patch; do
+    if grep -q -e "--git" $p; then
+        patch_opts="-p1"
+    else
+        patch_opts="-p0"
+    fi
+    echo "Applying $p"
+    if ! filterdiff -x '*/Makefile*' $p | \
+        patch -d ${crashreporter_dir}/google-breakpad ${patch_opts}; then
+      echo "Failed to apply $p"
+      exit 1
+    fi
+done
 
 hg addremove ${crashreporter_dir}/google-breakpad/
