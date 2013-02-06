7f5ce14642ce39f779b7c42a7b02d15ba8be8831
Chris Jones <jones.chris.g@gmail.com>
2013-02-05 18:27:52 -0800

    Bug 835584: Precompile forms.js and preload UserAgentOverrides.jsm. r=fabrice

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -133,8 +133,13 @@ TabChild::PreloadSlowThings()
         !tab->InitTabChildGlobal(DONT_LOAD_SCRIPTS)) {
         return;
     }
+    // Just load and compile these scripts, but don't run them.
     tab->TryCacheLoadAndCompileScript(BROWSER_ELEMENT_CHILD_SCRIPT);
-    tab->RecvLoadRemoteScript(NS_LITERAL_STRING("chrome://global/content/preload.js"));
+    tab->TryCacheLoadAndCompileScript(
+        NS_LITERAL_STRING("chrome://browser/content/forms.js"));
+    // Load, compile, and run these scripts.
+    tab->RecvLoadRemoteScript(
+        NS_LITERAL_STRING("chrome://global/content/preload.js"));
 
     nsCOMPtr<nsIDocShell> docShell = do_GetInterface(tab->mWebNav);
     nsCOMPtr<nsIPresShell> presShell;
