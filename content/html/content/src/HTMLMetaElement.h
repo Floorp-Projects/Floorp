bcdb782135eb7c59cbbd172bbf6b5d86cb174230
Ms2ger <ms2ger@gmail.com>
2013-02-07 09:08:58 +0100

    Bug 838160 - Part a: Rename nsHTMLMetaElement to mozilla::dom::HTMLMetaElement; r=bz
    --HG--
    rename : content/html/content/src/nsHTMLMetaElement.cpp => content/html/content/src/HTMLMetaElement.cpp
    rename : content/html/content/src/nsHTMLMetaElement.cpp => content/html/content/src/HTMLMetaElement.h

---
+++ content/html/content/src/HTMLMetaElement.h
@@ -0,0 +1,58 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_HTMLMetaElement_h
+#define mozilla_dom_HTMLMetaElement_h
+
+#include "nsGenericHTMLElement.h"
+#include "nsIDOMHTMLMetaElement.h"
+
+namespace mozilla {
+namespace dom {
+
+class HTMLMetaElement : public nsGenericHTMLElement,
+                        public nsIDOMHTMLMetaElement
+{
+public:
+  HTMLMetaElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  virtual ~HTMLMetaElement();
+
+  // nsISupports
+  NS_DECL_ISUPPORTS_INHERITED
+
+  // nsIDOMNode
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+
+  // nsIDOMElement
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+
+  // nsIDOMHTMLElement
+  NS_FORWARD_NSIDOMHTMLELEMENT_TO_GENERIC
+
+  // nsIDOMHTMLMetaElement
+  NS_DECL_NSIDOMHTMLMETAELEMENT
+
+  virtual nsresult BindToTree(nsIDocument* aDocument, nsIContent* aParent,
+                              nsIContent* aBindingParent,
+                              bool aCompileEventHandlers);
+  virtual void UnbindFromTree(bool aDeep = true,
+                              bool aNullParent = true);
+  void CreateAndDispatchEvent(nsIDocument* aDoc, const nsAString& aEventName);
+
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
+
+  virtual nsXPCClassInfo* GetClassInfo();
+
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+
+protected:
+  virtual void GetItemValueText(nsAString& text);
+  virtual void SetItemValueText(const nsAString& text);
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_HTMLMetaElement_h
