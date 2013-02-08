d0fe7fc7ff736f3d9bb2a33fa4dd56de1502e188
Ms2ger <ms2ger@gmail.com>
2013-02-08 13:50:30 +0100

    Bug 839179 - Part b: Move HTMLButtonElement to Paris bindings; r=bz

--- content/html/content/src/HTMLButtonElement.h
+++ content/html/content/src/HTMLButtonElement.h
@@ -60,6 +60,8 @@ public:
   virtual nsresult Clone(nsINodeInfo* aNodeInfo, nsINode** aResult) const;
   virtual nsXPCClassInfo* GetClassInfo();
   virtual nsIDOMNode* AsDOMNode() { return this; }
+  virtual JSObject*
+  WrapNode(JSContext* aCx, JSObject* aScope, bool* aTriedToWrap) MOZ_OVERRIDE;
 
   // nsIContent
   virtual nsresult BindToTree(nsIDocument* aDocument, nsIContent* aParent,
@@ -91,6 +93,76 @@ public:
   virtual bool IsHTMLFocusable(bool aWithMouse,
                                bool* aIsFocusable,
                                int32_t* aTabIndex);
+
+  // WebIDL
+  bool Autofocus() const
+  {
+    return GetBoolAttr(nsGkAtoms::autofocus);
+  }
+  void SetAutofocus(bool aAutofocus, ErrorResult& aError)
+  {
+    SetHTMLBoolAttr(nsGkAtoms::autofocus, aAutofocus, aError);
+  }
+  bool Disabled() const
+  {
+    return GetBoolAttr(nsGkAtoms::disabled);
+  }
+  void SetDisabled(bool aDisabled, ErrorResult& aError)
+  {
+    SetHTMLBoolAttr(nsGkAtoms::disabled, aDisabled, aError);
+  }
+  // nsGenericHTMLFormElement::GetForm is fine.
+  using nsGenericHTMLFormElement::GetForm;
+  // XPCOM GetFormAction is fine.
+  void SetFormAction(const nsAString& aFormAction, ErrorResult& aRv)
+  {
+    SetHTMLAttr(nsGkAtoms::formaction, aFormAction, aRv);
+  }
+  // XPCOM GetFormEnctype is fine.
+  void SetFormEnctype(const nsAString& aFormEnctype, ErrorResult& aRv)
+  {
+    SetHTMLAttr(nsGkAtoms::formenctype, aFormEnctype, aRv);
+  }
+  // XPCOM GetFormMethod is fine.
+  void SetFormMethod(const nsAString& aFormMethod, ErrorResult& aRv)
+  {
+    SetHTMLAttr(nsGkAtoms::formmethod, aFormMethod, aRv);
+  }
+  bool FormNoValidate() const
+  {
+    return GetBoolAttr(nsGkAtoms::formnovalidate);
+  }
+  void SetFormNoValidate(bool aFormNoValidate, ErrorResult& aError)
+  {
+    SetHTMLBoolAttr(nsGkAtoms::formnovalidate, aFormNoValidate, aError);
+  }
+  // XPCOM GetFormTarget is fine.
+  void SetFormTarget(const nsAString& aFormTarget, ErrorResult& aRv)
+  {
+    SetHTMLAttr(nsGkAtoms::formtarget, aFormTarget, aRv);
+  }
+  // XPCOM GetName is fine.
+  void SetName(const nsAString& aName, ErrorResult& aRv)
+  {
+    SetHTMLAttr(nsGkAtoms::name, aName, aRv);
+  }
+  // XPCOM GetType is fine.
+  void SetType(const nsAString& aType, ErrorResult& aRv)
+  {
+    SetHTMLAttr(nsGkAtoms::type, aType, aRv);
+  }
+  // XPCOM GetValue is fine.
+  void SetValue(const nsAString& aValue, ErrorResult& aRv)
+  {
+    SetHTMLAttr(nsGkAtoms::value, aValue, aRv);
+  }
+
+  // nsIConstraintValidation::WillValidate is fine.
+  // nsIConstraintValidation::Validity() is fine.
+  // nsIConstraintValidation::GetValidationMessage() is fine.
+  // nsIConstraintValidation::CheckValidity() is fine.
+  using nsIConstraintValidation::CheckValidity;
+  // nsIConstraintValidation::SetCustomValidity() is fine.
 
 protected:
   uint8_t mType;
