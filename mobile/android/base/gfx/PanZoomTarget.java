63862704476d7e5b0eb4152b357e30a148c30c57
Kartikaya Gupta <kgupta@mozilla.com>
2013-02-08 09:11:43 -0500

    Bug 777468 - Move mobile/android/base/ui/* to mobile/android/base/gfx/. r=Cwiiis

--- mobile/android/base/gfx/PanZoomTarget.java
+++ mobile/android/base/gfx/PanZoomTarget.java
@@ -3,10 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.gecko.ui;
+package org.mozilla.gecko.gfx;
 
 import org.mozilla.gecko.ZoomConstraints;
-import org.mozilla.gecko.gfx.ImmutableViewportMetrics;
 
 import android.graphics.PointF;
 
