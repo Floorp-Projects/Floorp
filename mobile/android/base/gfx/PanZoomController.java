29c672d20d8f55e9aa7d5c8cc50bc8210eaead55
Kartikaya Gupta <kgupta@mozilla.com>
2013-02-08 09:11:43 -0500

    Bug 777468 - Move mobile/android/base/ui/* to mobile/android/base/gfx/. r=Cwiiis

--- mobile/android/base/gfx/PanZoomController.java
+++ mobile/android/base/gfx/PanZoomController.java
@@ -3,14 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.gecko.ui;
+package org.mozilla.gecko.gfx;
 
 import org.mozilla.gecko.GeckoApp;
 import org.mozilla.gecko.GeckoAppShell;
 import org.mozilla.gecko.GeckoEvent;
 import org.mozilla.gecko.ZoomConstraints;
-import org.mozilla.gecko.gfx.ImmutableViewportMetrics;
-import org.mozilla.gecko.gfx.PointUtils;
 import org.mozilla.gecko.util.EventDispatcher;
 import org.mozilla.gecko.util.FloatUtils;
 import org.mozilla.gecko.util.GeckoEventListener;
--- mobile/android/base/gfx/SimpleScaleGestureDetector.java
+++ mobile/android/base/gfx/SimpleScaleGestureDetector.java
@@ -3,9 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.gecko.ui;
-
-import org.mozilla.gecko.gfx.PointUtils;
+package org.mozilla.gecko.gfx;
 
 import org.json.JSONException;
 
