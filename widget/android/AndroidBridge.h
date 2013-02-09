ed9a35ece6f0d1e95587e530cd883a4270f81019
Dan Mosedale <dmose@mozilla.org>
2013-02-09 13:38:08 -0800

    Bug 830935 - Add a method for getting Fennec's Activity Context from AndroidBridge. r=blassey

--- widget/android/AndroidBridge.h
+++ widget/android/AndroidBridge.h
@@ -268,6 +268,11 @@ public:
 
     void *LockBitmap(jobject bitmap);
 
+    // Returns a global reference to the Context for Fennec's Activity. The
+    // caller is responsible for ensuring this doesn't leak by calling
+    // DeleteGlobalRef() when the context is no longer needed.
+    jobject GetGlobalContextRef(void);
+
     void UnlockBitmap(jobject bitmap);
 
     bool UnlockProfile();
@@ -485,6 +490,8 @@ protected:
 
     jclass jThumbnailHelperClass;
     jmethodID jNotifyThumbnail;
+
+    jmethodID jGetContext;
 
     // for GfxInfo (gfx feature detection and blacklisting)
     jmethodID jGetGfxInfoData;
