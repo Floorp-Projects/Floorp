bbc574349bd31f9959df3afed5159f867cbdbb08
Robert Strong <robert.bugzilla@gmail.com>
2013-02-08 16:52:12 -0800

    Back out Bug 678392

--- widget/windows/nsLookAndFeel.cpp
+++ widget/windows/nsLookAndFeel.cpp
@@ -454,9 +454,6 @@ nsLookAndFeel::GetIntImpl(IntID aID, int32_t &aResult)
     case eIntID_ScrollbarButtonAutoRepeatBehavior:
         aResult = 0;
         break;
-    case eIntID_SwipeAnimationEnabled:
-        aResult = 0;
-        break;
     default:
         aResult = 0;
         res = NS_ERROR_FAILURE;
