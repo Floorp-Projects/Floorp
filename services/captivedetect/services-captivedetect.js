a2f05b75a8b9b135225e007c91b6655388d42318
Ryan VanderMeulen <ryanvm@gmail.com>
2013-02-11 14:33:27 -0500

    Backout 35d48d9d80d5 (bug 752982) for landing without proper review.

--- services/captivedetect/services-captivedetect.js
+++
@@ -1,9 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-pref("services.captivedetect.canonicalURL", "http://localhost/test.html");
-pref("services.captivedetect.canonicalContent", "true");
-pref("services.captivedetect.maxWaitingTime", 5000);
-pref("services.captivedetect.pollingTime", 3000);
-pref("services.captivedetect.maxRetryCount", 5);
