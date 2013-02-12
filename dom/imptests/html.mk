d291f8365c9fbdd5e9c5c7a61c5ead252d1fe7cc
Ms2ger <ms2ger@gmail.com>
2013-02-12 10:54:30 +0100

    Bug 838088 - Update html tests; rs=mounir
    --HG--
    rename : dom/imptests/failures/html/tests/submission/Opera/microdata/Makefile.in => dom/imptests/failures/html/old-tests/submission/Opera/microdata/Makefile.in
    rename : dom/imptests/failures/html/tests/submission/Opera/microdata/test_001.html.json => dom/imptests/failures/html/old-tests/submission/Opera/microdata/test_001.html.json
    rename : dom/imptests/html/tests/submission/Mozilla/test_pageload-image.html => dom/imptests/html/html/browsers/browsing-the-web/read-media/test_pageload-image.html
    rename : dom/imptests/html/tests/submission/Mozilla/test_pageload-video.html => dom/imptests/html/html/browsers/browsing-the-web/read-media/test_pageload-video.html
    rename : dom/imptests/html/tests/submission/Mozilla/test_script-for-onload.html => dom/imptests/html/html/semantics/scripting-1/the-script-element/test_script-for-onload.html
    rename : dom/imptests/html/tests/submission/Mozilla/test_body-onload.html => dom/imptests/html/html/webappapis/scripting/events/test_body-onload.html
    rename : dom/imptests/html/tests/submission/Mozilla/test_window-onerror-parse-error.html => dom/imptests/html/html/webappapis/scripting/processing-model-2/test_window-onerror-parse-error.html
    rename : dom/imptests/html/tests/submission/Mozilla/test_window-onerror-runtime-error-throw.html => dom/imptests/html/html/webappapis/scripting/processing-model-2/test_window-onerror-runtime-error-throw.html
    rename : dom/imptests/html/tests/submission/Mozilla/test_window-onerror-runtime-error.html => dom/imptests/html/html/webappapis/scripting/processing-model-2/test_window-onerror-runtime-error.html
    rename : dom/imptests/html/tests/submission/Opera/microdata/Makefile.in => dom/imptests/html/old-tests/submission/Opera/microdata/Makefile.in
    rename : dom/imptests/html/tests/submission/Opera/microdata/test_001.html => dom/imptests/html/old-tests/submission/Opera/microdata/test_001.html

--- dom/imptests/html.mk
+++ dom/imptests/html.mk
@@ -1,4 +1,7 @@
 DIRS += \
-  html/tests/submission/Mozilla \
-  html/tests/submission/Opera/microdata \
+  html/html/webappapis/scripting/events \
+  html/html/browsers/browsing-the-web/read-media \
+  html/html/semantics/scripting-1/the-script-element \
+  html/html/webappapis/scripting/processing-model-2 \
+  html/old-tests/submission/Opera/microdata \
   $(NULL)
