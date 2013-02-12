5996959f7ea17065d97755e6d40ce828ceabd0f7
Daniel Holbert <dholbert@cs.stanford.edu>
2013-02-11 17:29:48 -0800

    backout 69277e48526c (Bug 839865) due to reftest orange on a CLOSED TREE

--- layout/svg/nsSVGPathGeometryFrame.cpp
+++ layout/svg/nsSVGPathGeometryFrame.cpp
@@ -108,12 +108,10 @@ nsSVGPathGeometryFrame::AttributeChanged(int32_t         aNameSpaceID,
 {
   if (aNameSpaceID == kNameSpaceID_None &&
       (static_cast<nsSVGPathGeometryElement*>
-                  (mContent)->AttributeDefinesGeometry(aAttribute))) {
+                  (mContent)->AttributeDefinesGeometry(aAttribute) ||
+       aAttribute == nsGkAtoms::transform)) {
     nsSVGUtils::InvalidateBounds(this, false);
     nsSVGUtils::ScheduleReflowSVG(this);
-  } else if (aAttribute == nsGkAtoms::transform) {
-    // Don't invalidate or update bounds (the layers code does that).
-    SchedulePaint();
   }
   return NS_OK;
 }
