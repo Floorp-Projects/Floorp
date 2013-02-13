33a0752d1b93ca00781432064f340cb3ef760bef
Trevor Saunders <trev.saunders@gmail.com>
2013-02-13 16:39:23 -0500

    bug 840906 - densISupportsify nsDOMRGBCSSColor r=mccr8

--- layout/style/nsDOMCSSRGBColor.cpp
+++ layout/style/nsDOMCSSRGBColor.cpp
@@ -1,5 +1,4 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */ /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
@@ -8,8 +7,6 @@
 #include "nsDOMCSSRGBColor.h"
 
 #include "mozilla/dom/RGBColorBinding.h"
-#include "nsISupports.h"
-#include "nsCOMPtr.h"
 #include "nsIDOMCSSPrimitiveValue.h"
 #include "nsContentUtils.h"
 #include "nsROCSSPrimitiveValue.h"
@@ -31,14 +28,10 @@ nsDOMCSSRGBColor::~nsDOMCSSRGBColor(void)
 {
 }
 
-NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(nsDOMCSSRGBColor)
-  NS_INTERFACE_MAP_ENTRY(nsISupports)
-  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
-NS_INTERFACE_MAP_END
-
 NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE_4(nsDOMCSSRGBColor, mAlpha,  mBlue, mGreen, mRed)
-NS_IMPL_CYCLE_COLLECTING_ADDREF(nsDOMCSSRGBColor)
-NS_IMPL_CYCLE_COLLECTING_RELEASE(nsDOMCSSRGBColor)
+
+NS_IMPL_CYCLE_COLLECTION_ROOT_NATIVE(nsDOMCSSRGBColor, AddRef)
+NS_IMPL_CYCLE_COLLECTION_UNROOT_NATIVE(nsDOMCSSRGBColor, Release)
 
 JSObject*
 nsDOMCSSRGBColor::WrapObject(JSContext *aCx, JSObject *aScope, bool *aTried)
