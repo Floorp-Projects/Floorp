d4f314bfa39efab280846656aaf2860d86716dfd
Scott Johnson <sjohnson@mozilla.com>
2013-02-13 14:39:40 -0600

    Bug 810726: Rework logic in nsColumnSetFrame for handling overflow to prevent infinite loop problems. [r=roc]

--- layout/generic/nsColumnSetFrame.cpp
+++ layout/generic/nsColumnSetFrame.cpp
@@ -735,7 +735,10 @@ nsColumnSetFrame::ReflowChildren(nsHTMLReflowMetrics&     aDesiredSize,
       }
 
       if (columnCount >= aConfig.mBalanceColCount) {
-        if (contentBottom >= aReflowState.availableHeight) {
+        if (contentBottom > aReflowState.mComputedMaxHeight ||
+            contentBottom > aReflowState.ComputedHeight()) {
+            aColData.mShouldRevertToAuto = true;
+        } else {
           // No more columns allowed here. Stop.
           aStatus |= NS_FRAME_REFLOW_NEXTINFLOW;
           kidNextInFlow->AddStateBits(NS_FRAME_IS_DIRTY);
@@ -747,12 +750,6 @@ nsColumnSetFrame::ReflowChildren(nsHTMLReflowMetrics&     aDesiredSize,
           }
           child = nullptr;
           break;
-        } else if (contentBottom > aReflowState.mComputedMaxHeight ||
-                   contentBottom > aReflowState.ComputedHeight()) {
-          aColData.mShouldRevertToAuto = true;
-        } else {
-          // The number of columns required is too high.
-          allFit = false;
         }
       }
     }
