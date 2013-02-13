62cd9e3d8fff0296c6447fd854e40793d8a22496
Rafael Ávila de Espíndola <respindola@mozilla.org>
2013-02-13 08:54:32 -0500

    Revert fa1f52704c9e. r=bustage.

--- xpcom/build/mozPoisonWriteMac.cpp
+++ xpcom/build/mozPoisonWriteMac.cpp
@@ -247,6 +247,8 @@ void PoisonWrite() {
     if (!PoisonWriteEnabled())
         return;
 
+    PoisonWriteBase();
+
     for (int i = 0; i < NumFunctions; ++i) {
         FuncData *d = Functions[i];
         if (!d->Function)
