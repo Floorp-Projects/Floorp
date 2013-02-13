bef23894b655d47402fd56f730754b194eb743f2
Brian Nicholson <bnicholson@mozilla.com>
2013-02-12 23:44:40 -0800

    Bug 840601 - Clear saved reader mode page in tab destroy. r=kats

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -3003,6 +3003,7 @@ Tab.prototype = {
     BrowserApp.deck.selectedPanel = selectedPanel;
 
     this.browser = null;
+    this.savedArticle = null;
   },
 
   // This should be called to update the browser when the tab gets selected/unselected
