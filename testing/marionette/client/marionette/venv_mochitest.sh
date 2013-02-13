af0e70b70aab0a10e1fbdfddb288e8373a805257
Luqman Aden <laden@mozilla.com>
2013-02-12 16:35:55 -0800

    Bug 840636 - Update Mochitest script. r=jgriffin

--- testing/marionette/client/marionette/venv_mochitest.sh
+++ testing/marionette/client/marionette/venv_mochitest.sh
@@ -3,7 +3,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+VIRTUAL_ENV_VERSION="1.8.2"
+
 PYTHON=$1
+
+# Store the current working directory so we can change back into it after
+# preparing the Marionette virtualenv
+CWD=$(pwd)
 
 if [ -z "${PYTHON}" ]
 then
@@ -29,22 +35,17 @@ fi
 if [ -d $VENV_DIR ]
 then
   echo "Using virtual environment in $VENV_DIR"
-  cd $VENV_DIR
-  . bin/activate
 else
   echo "Creating a virtual environment in $VENV_DIR"
-  curl https://raw.github.com/pypa/virtualenv/develop/virtualenv.py | ${PYTHON} - $VENV_DIR
-  cd $VENV_DIR
-  . bin/activate
-  # set up mozbase
-  git clone git://github.com/mozilla/mozbase.git
-  cd mozbase
-  python setup_development.py
+  curl https://raw.github.com/pypa/virtualenv/${VIRTUAL_ENV_VERSION}/virtualenv.py | ${PYTHON} - $VENV_DIR
 fi
+. $VENV_DIR/bin/activate
 
-# update the marionette_client
+# Updating the marionette_client needs us to change into its package folder.
+# Otherwise the call to setup.py will hang
 cd $MARIONETTE_HOME
 python setup.py develop
+cd $CWD
 
 # pop off the python parameter
 shift
