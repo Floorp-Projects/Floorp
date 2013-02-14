5eecacbd17140d6107e370795b4fd5d417c2a090
Ed Morley <emorley@mozilla.com>
2013-02-14 10:02:49 +0000

    Backed out changeset 77b0d7480b4c (bug 836654)

--- hal/sandbox/SandboxHal.cpp
+++ hal/sandbox/SandboxHal.cpp
@@ -735,6 +735,9 @@ public:
   virtual bool
   RecvGetWakeLockInfo(const nsString &aTopic, WakeLockInformation *aWakeLockInfo) MOZ_OVERRIDE
   {
+    if (!AssertAppProcessPermission(this, "power")) {
+      return false;
+    }
     hal::GetWakeLockInfo(aTopic, aWakeLockInfo);
     return true;
   }
