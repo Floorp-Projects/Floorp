6755c6fcbc2debf5395ae85f53788f87238b2837
Andrea Marchesini <amarchesini@mozilla.com>
2013-02-14 10:59:21 -0500

    Bug 840559 - Convert MediaError to WebIDL. r=smaug

--- content/html/content/src/MediaError.cpp
+++ content/html/content/src/MediaError.cpp
@@ -6,32 +6,44 @@
 
 #include "mozilla/dom/MediaError.h"
 #include "nsDOMClassInfoID.h"
+#include "mozilla/dom/MediaErrorBinding.h"
 
 DOMCI_DATA(MediaError, mozilla::dom::MediaError)
 
 namespace mozilla {
 namespace dom {
 
-NS_IMPL_ADDREF(MediaError)
-NS_IMPL_RELEASE(MediaError)
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE_1(MediaError, mParent)
+NS_IMPL_CYCLE_COLLECTING_ADDREF(MediaError)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(MediaError)
 
-NS_INTERFACE_MAP_BEGIN(MediaError)
-  NS_INTERFACE_MAP_ENTRY(nsISupports)
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(MediaError)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
   NS_INTERFACE_MAP_ENTRY(nsIDOMMediaError)
+  NS_INTERFACE_MAP_ENTRY_AMBIGUOUS(nsISupports, nsIDOMMediaError)
   NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(MediaError)
 NS_INTERFACE_MAP_END
 
-MediaError::MediaError(uint16_t aCode)
-: mCode(aCode)
+MediaError::MediaError(nsHTMLMediaElement* aParent, uint16_t aCode)
+  : mParent(aParent)
+  , mCode(aCode)
 {
+  SetIsDOMBinding();
 }
 
 NS_IMETHODIMP MediaError::GetCode(uint16_t* aCode)
 {
   if (aCode)
-    *aCode = mCode;
+    *aCode = Code();
 
   return NS_OK;
+}
+
+JSObject*
+MediaError::WrapObject(JSContext* aCx, JSObject* aScope,
+                       bool* aTriedToWrap)
+{
+  return MediaErrorBinding::Wrap(aCx, aScope, this, aTriedToWrap);
 }
 
 } // namespace dom
