2a5cef0a4bffcea865c42956d8ba7c52f165c28d
Trevor Saunders <trev.saunders@gmail.com>
2013-02-14 07:06:16 -0500

    bug 833164 - consolodate the various makeDepend things in the tree r=ted

---
+++ python/codegen/makeutils.py
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+dependencies = []
+targets = []
+
+def makeQuote(filename):
+    return filename.replace(' ', '\\ ')  # enjoy!
+
+def writeMakeDependOutput(filename):
+    print "Creating makedepend file", filename
+    with open(filename, 'w') as f:
+        if len(targets) > 0:
+            f.write("%s:" % makeQuote(targets[0]))
+            for filename in dependencies:
+                f.write(' \\\n\t\t%s' % makeQuote(filename))
+            f.write('\n\n')
+            for filename in targets[1:]:
+                f.write('%s: %s\n' % (makeQuote(filename), makeQuote(targets[0])))
+
