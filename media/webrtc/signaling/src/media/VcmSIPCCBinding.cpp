498d0db3f084fb4031d82458d6bd27b860728f8f
Ethan Hugg <ethanhugg@gmail.com>
2013-02-15 14:35:50 -0800

    Bug 841641 - Set FORCE_PR_LOG in CSFLog r=jesup

--- media/webrtc/signaling/src/media/VcmSIPCCBinding.cpp
+++ media/webrtc/signaling/src/media/VcmSIPCCBinding.cpp
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "CC_Common.h"
-
 #include "CSFLogStream.h"
+
+#include "CC_Common.h"
 
 #include "CSFMediaProvider.h"
 #include "CSFAudioTermination.h"
