10910680e15f03e2fb0e6dd5553c78b9f0992030
Cykesiopka <cykesiopka@hotmail.com>
2013-02-17 07:07:50 -0500

    Bug 583566 - Remove old Java plugin scanning pref and related code. r=bsmedberg

--- dom/plugins/base/nsPluginHost.cpp
+++ dom/plugins/base/nsPluginHost.cpp
@@ -2281,8 +2281,7 @@ nsresult nsPluginHost::FindPlugins(bool aCreatePluginList, bool * aPluginsChange
   // Scan the installation paths of our popular plugins if the prefs are enabled
 
   // This table controls the order of scanning
-  const char* const prefs[] = {NS_WIN_JRE_SCAN_KEY,
-                               NS_WIN_ACROBAT_SCAN_KEY,
+  const char* const prefs[] = {NS_WIN_ACROBAT_SCAN_KEY,
                                NS_WIN_QUICKTIME_SCAN_KEY,
                                NS_WIN_WMP_SCAN_KEY};
 
