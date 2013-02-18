038556d38685c8d1d2004a02d1f060e6fb10a922
Ryan VanderMeulen <ryanvm@gmail.com>
2013-02-18 11:12:16 -0500

    Backed out changeset 645cb4fbf390 (bug 842132) for build bustage.

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -2930,9 +2930,9 @@ nsFrameSelection::DeleteFromDocument()
     return NS_OK;
   }
 
-  nsRefPtr<mozilla::Selection> selection = mDomSelections[index];
-  for (uint32_t rangeIdx = 0; rangeIdx < selection->GetRangeCount(); ++rangeIdx) {
-    nsRefPtr<nsRange> range = selection->GetRangeAt(rangeIdx);
+  uint32_t rangeCount = mDomSelections[index]->GetRangeCount();
+  for (uint32_t rangeIdx = 0; rangeIdx < rangeCount; ++rangeIdx) {
+    nsRefPtr<nsRange> range = mDomSelections[index]->GetRangeAt(rangeIdx);
     res = range->DeleteContents();
     if (NS_FAILED(res))
       return res;
