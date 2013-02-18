cab1c2dfbba84cff0162639af90e4176446c36ed
Gregory Szorc <gps@mozilla.com>
2013-02-18 13:05:07 -0800

    Bug 842377 - Rename "constant only" to "pull only"; r=rnewman
    The new name better reflects the lazy-init behavior of providers.

--- services/healthreport/tests/xpcshell/test_healthreporter.js
+++ services/healthreport/tests/xpcshell/test_healthreporter.js
@@ -171,9 +171,8 @@ add_task(function test_register_providers_from_category_manager() {
   reporter._shutdown();
 });
 
-// Constant only providers are only initialized at constant collect
-// time.
-add_task(function test_constant_only_providers() {
+// Pull-only providers are only initialized at collect time.
+add_task(function test_pull_only_providers() {
   const category = "healthreporter-constant-only";
 
   let cm = Cc["@mozilla.org/categorymanager;1"]
@@ -194,9 +193,9 @@ add_task(function test_constant_only_providers() {
   do_check_neq(reporter.getProvider("DummyProvider"), null);
   do_check_null(reporter.getProvider("DummyConstantProvider"));
 
-  yield reporter.ensureConstantOnlyProvidersRegistered();
+  yield reporter.ensurePullOnlyProvidersRegistered();
   yield reporter.collectMeasurements();
-  yield reporter.ensureConstantOnlyProvidersUnregistered();
+  yield reporter.ensurePullOnlyProvidersUnregistered();
 
   do_check_eq(reporter._collector._providers.size, 1);
   do_check_true(reporter._storage.hasProvider("DummyConstantProvider"));
