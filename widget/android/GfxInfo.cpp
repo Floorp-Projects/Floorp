a2145d7dade5e6965057563c1f32e55dc9f46e72
Chris Double <chris.double@double.co.nz>
2013-02-18 13:36:05 +1300

    Bug 823253 - Part 2 - Whitelist LG Froyo devices for stagefright decoding - r=bjacob

--- widget/android/GfxInfo.cpp
+++ widget/android/GfxInfo.cpp
@@ -355,7 +355,20 @@ GfxInfo::GetFeatureStatusImpl(int32_t aFeature,
     if (aFeature == FEATURE_STAGEFRIGHT) {
       NS_LossyConvertUTF16toASCII cManufacturer(mManufacturer);
       NS_LossyConvertUTF16toASCII cModel(mModel);
-      if (CompareVersions(mOSVersion.get(), "2.3.0") >= 0 &&
+      if (CompareVersions(mOSVersion.get(), "2.2.0") >= 0 &&
+          CompareVersions(mOSVersion.get(), "2.3.0") < 0)
+      {
+        // Froyo LG devices are whitelisted.
+        // All other Froyo
+        bool isWhitelisted =
+          cManufacturer.Equals("lge", nsCaseInsensitiveCStringComparator());
+
+        if (!isWhitelisted) {
+          *aStatus = nsIGfxInfo::FEATURE_BLOCKED_DEVICE;
+          return NS_OK;
+        }
+      }
+      else if (CompareVersions(mOSVersion.get(), "2.3.0") >= 0 &&
           CompareVersions(mOSVersion.get(), "2.4.0") < 0)
       {
         // Gingerbread HTC devices are whitelisted.
