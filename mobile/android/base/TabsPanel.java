4f7063b90a58b33969b253d76b0dd16a047d8697
Sriram Ramasubramanian <sriram@mozilla.com>
2013-02-21 14:21:57 -0800

    Bug 843619: Remove tabs tray menu. [r=mfinkle] [needs-clobber]
    --HG--
    extra : rebase_source : 7f590f1f0b21e15ced7efd59f6cf0cf9394e4671

--- mobile/android/base/TabsPanel.java
+++ mobile/android/base/TabsPanel.java
@@ -27,8 +27,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class TabsPanel extends LinearLayout
-                       implements GeckoPopupMenu.OnMenuItemClickListener,
-                                  LightweightTheme.OnChangeListener,
+                       implements LightweightTheme.OnChangeListener,
                                   IconTabWidget.OnTabChangedListener {
     private static final String LOGTAG = "GeckoTabsPanel";
 
@@ -61,15 +60,11 @@ public class TabsPanel extends LinearLayout
     private TabsLayoutChangeListener mLayoutChangeListener;
 
     private IconTabWidget mTabWidget;
-    private static ImageButton mMenuButton;
     private static ImageButton mAddTab;
 
     private Panel mCurrentPanel;
     private boolean mIsSideBar;
     private boolean mVisible;
-
-    private GeckoPopupMenu mPopupMenu;
-    private Menu mMenu;
 
     public TabsPanel(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -84,11 +79,6 @@ public class TabsPanel extends LinearLayout
         mVisible = false;
 
         mIsSideBar = false;
-
-        mPopupMenu = new GeckoPopupMenu(context);
-        mPopupMenu.inflate(R.menu.tabs_menu);
-        mPopupMenu.setOnMenuItemClickListener(this);
-        mMenu = mPopupMenu.getMenu();
 
         LayoutInflater.from(context).inflate(R.layout.tabs_panel, this);
         initialize();
@@ -120,15 +110,6 @@ public class TabsPanel extends LinearLayout
         mTabWidget.addTab(R.drawable.tabs_private);
         mTabWidget.addTab(R.drawable.tabs_synced);
         mTabWidget.setTabSelectionListener(this);
-
-        mMenuButton = (ImageButton) findViewById(R.id.menu);
-        mMenuButton.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View view) {
-                TabsPanel.this.openTabsMenu();
-            }
-        });
-
-        mPopupMenu.setAnchor(mMenuButton);
     }
 
     public void addTab() {
@@ -140,15 +121,6 @@ public class TabsPanel extends LinearLayout
         mActivity.autoHideTabs();
     }
 
-    public void openTabsMenu() {
-        if (mCurrentPanel == Panel.REMOTE_TABS)
-            mMenu.findItem(R.id.close_all_tabs).setEnabled(false);
-        else
-            mMenu.findItem(R.id.close_all_tabs).setEnabled(true); 
-
-        mPopupMenu.show();
-    }
-
     @Override
     public void onTabChanged(int index) {
         if (index == 0)
@@ -157,25 +129,6 @@ public class TabsPanel extends LinearLayout
             show(Panel.PRIVATE_TABS, false);
         else
             show(Panel.REMOTE_TABS, false);
-    }
-
-    @Override
-    public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.close_all_tabs:
-                for (Tab tab : Tabs.getInstance().getTabsInOrder()) {
-                    Tabs.getInstance().closeTab(tab);
-                }
-                autoHidePanel();
-                return true;
-
-            case R.id.new_tab:
-            case R.id.new_private_tab:
-                hide();
-            // fall through
-            default:
-                return mActivity.onOptionsItemSelected(item);
-        }
     }
 
     private static int getTabContainerHeight(TabsListContainer listContainer) {
@@ -357,14 +310,12 @@ public class TabsPanel extends LinearLayout
                 mFooter.setVisibility(View.GONE);
 
             mAddTab.setVisibility(View.INVISIBLE);
-            mMenuButton.setVisibility(View.INVISIBLE);
         } else {
             if (mFooter != null)
                 mFooter.setVisibility(View.VISIBLE);
 
             mAddTab.setVisibility(View.VISIBLE);
             mAddTab.setImageLevel(index);
-            mMenuButton.setVisibility(View.VISIBLE);
         }
 
         if (shouldResize) {
@@ -382,7 +333,6 @@ public class TabsPanel extends LinearLayout
     public void hide() {
         if (mVisible) {
             mVisible = false;
-            mPopupMenu.dismiss();
             dispatchLayoutChange(0, 0);
 
             if (mPanel != null) {
