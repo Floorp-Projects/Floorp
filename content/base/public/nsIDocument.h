516de3340fe31815624a1898a39aea568b01f89d
Boris Zbarsky <bzbarsky@mit.edu>
2013-02-22 09:56:29 -0500

    Bug 842726.  Kill off GetterShim and the classinfo support for chrome-only stuff on Document now that we can do that via WebIDL.  r=peterv

--- content/base/public/nsIDocument.h
+++ content/base/public/nsIDocument.h
@@ -1939,6 +1939,10 @@ public:
   void ReleaseCapture() const;
   virtual void MozSetImageElement(const nsAString& aImageElementId,
                                   Element* aElement) = 0;
+  nsIURI* GetDocumentURIObject()
+  {
+    return GetDocumentURI();
+  }
   // Not const because all the full-screen goop is not const
   virtual bool MozFullScreenEnabled() = 0;
   virtual Element* GetMozFullScreenElement(mozilla::ErrorResult& rv) = 0;
