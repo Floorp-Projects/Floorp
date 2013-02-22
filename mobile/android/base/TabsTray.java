3c15e144788532d1a4d583beea8100879eb10143
Lucas Rocha <lucasr@mozilla.com>
2013-02-22 07:22:36 +0000

    Bug 817721/817735/817732 - Vertical swipe support in TabsTray (r=mfinkle)

--- mobile/android/base/TabsTray.java
+++ mobile/android/base/TabsTray.java
@@ -9,6 +9,7 @@ import org.mozilla.gecko.PropertyAnimator.Property;
 import org.mozilla.gecko.widget.TwoWayView;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
@@ -141,7 +142,8 @@ public class TabsTray extends TwoWayView
             mOnCloseClickListener = new Button.OnClickListener() {
                 public void onClick(View v) {
                     TabRow tab = (TabRow) v.getTag();
-                    animateClose(tab.info, tab.info.getWidth());
+                    final int pos = (isVertical() ? tab.info.getWidth() : tab.info.getHeight());
+                    animateClose(tab.info, pos);
                 }
             };
         }
@@ -265,10 +267,18 @@ public class TabsTray extends TwoWayView
         }
     }
 
-    private void animateClose(final View view, int x) {
+    private boolean isVertical() {
+        return (getOrientation().compareTo(TwoWayView.Orientation.VERTICAL) == 0);
+    }
+
+    private void animateClose(final View view, int pos) {
         PropertyAnimator animator = new PropertyAnimator(ANIMATION_DURATION);
         animator.attach(view, Property.ALPHA, 0);
-        animator.attach(view, Property.TRANSLATION_X, x);
+
+        if (isVertical())
+            animator.attach(view, Property.TRANSLATION_X, pos);
+        else
+            animator.attach(view, Property.TRANSLATION_Y, pos);
 
         mCloseAnimationCount++;
         mPendingClosedTabs.add(view);
@@ -296,11 +306,16 @@ public class TabsTray extends TwoWayView
 
     private void animateFinishClose(final View view) {
         PropertyAnimator animator = new PropertyAnimator(ANIMATION_DURATION);
-        animator.attach(view, Property.HEIGHT, 1);
+
+        final boolean isVertical = isVertical();
+        if (isVertical)
+            animator.attach(view, Property.HEIGHT, 1);
+        else
+            animator.attach(view, Property.WIDTH, 1);
 
         TabRow tab = (TabRow)view.getTag();
         final int tabId = tab.id;
-        final int originalHeight = view.getHeight();
+        final int originalSize = (isVertical ? view.getHeight() : view.getWidth());
 
         animator.setPropertyAnimationListener(new PropertyAnimator.PropertyAnimationListener() {
             public void onPropertyAnimationStart() { }
@@ -309,8 +324,14 @@ public class TabsTray extends TwoWayView
                 // list view by the adapter.
                 AnimatorProxy proxy = AnimatorProxy.create(view);
                 proxy.setAlpha(1);
-                proxy.setTranslationX(0);
-                proxy.setHeight(originalHeight);
+
+                if (isVertical) {
+                    proxy.setHeight(originalSize);
+                    proxy.setTranslationX(0);
+                } else {
+                    proxy.setWidth(originalSize);
+                    proxy.setTranslationY(0);
+                }
 
                 Tabs tabs = Tabs.getInstance();
                 Tab tab = tabs.getTab(tabId);
@@ -324,7 +345,12 @@ public class TabsTray extends TwoWayView
     private void animateCancel(final View view) {
         PropertyAnimator animator = new PropertyAnimator(ANIMATION_DURATION);
         animator.attach(view, Property.ALPHA, 1);
-        animator.attach(view, Property.TRANSLATION_X, 0);
+
+        if (isVertical())
+            animator.attach(view, Property.TRANSLATION_X, 0);
+        else
+            animator.attach(view, Property.TRANSLATION_Y, 0);
+
 
         animator.setPropertyAnimationListener(new PropertyAnimator.PropertyAnimationListener() {
             public void onPropertyAnimationStart() { }
@@ -349,6 +375,7 @@ public class TabsTray extends TwoWayView
         private VelocityTracker mVelocityTracker;
 
         private int mListWidth = 1;
+        private int mListHeight = 1;
 
         private View mSwipeView;
         private AnimatorProxy mSwipeProxy;
@@ -395,8 +422,10 @@ public class TabsTray extends TwoWayView
             if (!mEnabled)
                 return false;
 
-            if (mListWidth < 2)
+            if (mListWidth < 2 || mListHeight < 2) {
                 mListWidth = TabsTray.this.getWidth();
+                mListHeight = TabsTray.this.getHeight();
+            }
 
             switch (e.getActionMasked()) {
                 case MotionEvent.ACTION_DOWN: {
@@ -404,8 +433,11 @@ public class TabsTray extends TwoWayView
                     // touched view after a standard delay.
                     triggerCheckForTap();
 
+                    final float x = e.getRawX();
+                    final float y = e.getRawY();
+
                     // Find out which view is being touched
-                    mSwipeView = findViewAt(e.getRawX(), e.getRawY());
+                    mSwipeView = findViewAt(x, y);
 
                     if (mSwipeView != null) {
                         mSwipeStartX = e.getRawX();
@@ -434,8 +466,6 @@ public class TabsTray extends TwoWayView
                         break;
                     }
 
-                    float deltaX = mSwipeProxy.getTranslationX();
-
                     mVelocityTracker.addMovement(e);
                     mVelocityTracker.computeCurrentVelocity(1000, mMaxFlingVelocity);
 
@@ -443,19 +473,39 @@ public class TabsTray extends TwoWayView
                     float velocityY = Math.abs(mVelocityTracker.getYVelocity());
 
                     boolean dismiss = false;
-                    boolean dismissRight = false;
+                    boolean dismissDirection = false;
+                    int dismissTranslation = 0;
 
-                    if (Math.abs(deltaX) > mListWidth / 2) {
-                        dismiss = true;
-                        dismissRight = (deltaX > 0);
-                    } else if (mMinFlingVelocity <= velocityX && velocityX <= mMaxFlingVelocity
-                            && velocityY < velocityX) {
-                        dismiss = mSwiping && (deltaX * mVelocityTracker.getXVelocity() > 0);
-                        dismissRight = (mVelocityTracker.getXVelocity() > 0);
-                    }
+                    if (isVertical()) {
+                        float deltaX = mSwipeProxy.getTranslationX();
+
+                        if (Math.abs(deltaX) > mListWidth / 2) {
+                            dismiss = true;
+                            dismissDirection = (deltaX > 0);
+                        } else if (mMinFlingVelocity <= velocityX && velocityX <= mMaxFlingVelocity
+                                && velocityY < velocityX) {
+                            dismiss = mSwiping && (deltaX * mVelocityTracker.getXVelocity() > 0);
+                            dismissDirection = (mVelocityTracker.getXVelocity() > 0);
+                        }
+
+                        dismissTranslation = (dismissDirection ? mListWidth : -mListWidth);
+                    } else {
+                        float deltaY = mSwipeProxy.getTranslationY();
+
+                        if (Math.abs(deltaY) > mListHeight / 2) {
+                            dismiss = true;
+                            dismissDirection = (deltaY > 0);
+                        } else if (mMinFlingVelocity <= velocityY && velocityY <= mMaxFlingVelocity
+                                && velocityX < velocityY) {
+                            dismiss = mSwiping && (deltaY * mVelocityTracker.getYVelocity() > 0);
+                            dismissDirection = (mVelocityTracker.getYVelocity() > 0);
+                        }
+
+                        dismissTranslation = (dismissDirection ? mListHeight : -mListHeight);
+                     }
 
                     if (dismiss)
-                        animateClose(mSwipeView, (dismissRight ? mListWidth : -mListWidth));
+                        animateClose(mSwipeView, dismissTranslation);
                     else
                         animateCancel(mSwipeView);
 
@@ -477,21 +527,22 @@ public class TabsTray extends TwoWayView
 
                     mVelocityTracker.addMovement(e);
 
+                    final boolean isVertical = isVertical();
+
                     float deltaX = e.getRawX() - mSwipeStartX;
                     float deltaY = e.getRawY() - mSwipeStartY;
+                    float delta = (isVertical ? deltaX : deltaY);
+
                     boolean isScrollingX = Math.abs(deltaX) > mSwipeThreshold;
                     boolean isScrollingY = Math.abs(deltaY) > mSwipeThreshold;
+                    boolean isSwipingToClose = (isVertical ? isScrollingX : isScrollingY);
 
                     // If we're actually swiping, make sure we don't
                     // set pressed state on the swiped view.
                     if (isScrollingX || isScrollingY)
                         cancelCheckForTap();
 
-                    if (isScrollingX) {
-                        // If we're actually swiping, make sure we don't
-                        // set pressed state on the swiped view.
-                        cancelCheckForTap();
-
+                    if (isSwipingToClose) {
                         mSwiping = true;
                         TabsTray.this.requestDisallowInterceptTouchEvent(true);
 
@@ -509,9 +560,13 @@ public class TabsTray extends TwoWayView
                     }
 
                     if (mSwiping) {
-                        mSwipeProxy.setTranslationX(deltaX);
+                        if (isVertical)
+                            mSwipeProxy.setTranslationX(delta);
+                        else
+                            mSwipeProxy.setTranslationY(delta);
+
                         mSwipeProxy.setAlpha(Math.max(0.1f, Math.min(1f,
-                                1f - 2f * Math.abs(deltaX) / mListWidth)));
+                                1f - 2f * Math.abs(delta) / (isVertical ? mListWidth : mListHeight))));
 
                         return true;
                     }
