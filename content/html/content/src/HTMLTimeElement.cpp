1e7d3d46e5e123481e90b1237e603d0c674c636b
David Humphrey (:humph) <david.humphrey@senecacollege.ca>
2013-02-22 10:07:42 -0500

    Bug 629801 Implement HTML5 <time> element. r=smaug, peterv, hsivonen

---
+++ content/html/content/src/HTMLTimeElement.cpp
@@ -0,0 +1,63 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "HTMLTimeElement.h"
+#include "mozilla/dom/HTMLTimeElementBinding.h"
+#include "nsContentUtils.h"
+#include "nsGenericHTMLElement.h"
+#include "nsVariant.h"
+#include "nsGkAtoms.h"
+
+NS_IMPL_NS_NEW_HTML_ELEMENT(Time)
+
+namespace mozilla {
+namespace dom {
+
+HTMLTimeElement::HTMLTimeElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+  : nsGenericHTMLElement(aNodeInfo)
+{
+  SetIsDOMBinding();
+}
+
+HTMLTimeElement::~HTMLTimeElement()
+{
+}
+
+NS_IMPL_ADDREF_INHERITED(HTMLTimeElement, Element)
+NS_IMPL_RELEASE_INHERITED(HTMLTimeElement, Element)
+
+NS_INTERFACE_TABLE_HEAD(HTMLTimeElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE0(HTMLTimeElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE_TO_MAP_SEGUE(HTMLTimeElement,
+                                               nsGenericHTMLElement)
+NS_HTML_CONTENT_INTERFACE_MAP_END
+
+NS_IMPL_ELEMENT_CLONE(HTMLTimeElement)
+
+JSObject*
+HTMLTimeElement::WrapNode(JSContext* cx, JSObject* scope, bool* triedToWrap)
+{
+  return HTMLTimeElementBinding::Wrap(cx, scope, this, triedToWrap);
+}
+
+void
+HTMLTimeElement::GetItemValueText(nsAString& text)
+{
+  if (HasAttr(kNameSpaceID_None, nsGkAtoms::datetime)) {
+    GetDateTime(text);
+  } else {
+    GetTextContentInternal(text);
+  }
+}
+
+void
+HTMLTimeElement::SetItemValueText(const nsAString& text)
+{
+  ErrorResult rv;
+  SetDateTime(text, rv);
+}
+
+} // namespace dom
+} // namespace mozilla
---
+++ content/html/content/src/HTMLTimeElement.h
@@ -0,0 +1,59 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_HTMLTimeElement_h
+#define mozilla_dom_HTMLTimeElement_h
+
+#include "nsIDOMHTMLElement.h"
+#include "nsGenericHTMLElement.h"
+#include "nsGkAtoms.h"
+
+namespace mozilla {
+namespace dom {
+
+class HTMLTimeElement MOZ_FINAL : public nsGenericHTMLElement,
+                                  public nsIDOMHTMLElement
+{
+public:
+  HTMLTimeElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  virtual ~HTMLTimeElement();
+
+  // nsISupports
+  NS_DECL_ISUPPORTS_INHERITED
+
+  // nsIDOMNode
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+
+  // nsIDOMElement
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+
+  // nsIDOMHTMLElement
+  NS_FORWARD_NSIDOMHTMLELEMENT_TO_GENERIC
+
+  // HTMLTimeElement WebIDL
+  void GetDateTime(nsAString& aDateTime)
+  {
+    GetHTMLAttr(nsGkAtoms::datetime, aDateTime);
+  }
+
+  void SetDateTime(const nsAString& aDateTime, ErrorResult& aError)
+  {
+    SetHTMLAttr(nsGkAtoms::datetime, aDateTime, aError);
+  }
+
+  virtual void GetItemValueText(nsAString& text);
+  virtual void SetItemValueText(const nsAString& text);
+  virtual nsresult Clone(nsINodeInfo* aNodeInfo, nsINode** aResult) const;
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+
+protected:
+  virtual JSObject* WrapNode(JSContext* aCx, JSObject* aScope,
+                             bool* aTriedToWrap) MOZ_OVERRIDE;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_HTMLTimeElement_h
