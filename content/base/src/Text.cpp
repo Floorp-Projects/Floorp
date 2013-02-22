81c78b8c603061f144aebfa8bc51557c7a76a199
Boris Zbarsky <bzbarsky@mit.edu>
2013-02-22 09:56:29 -0500

    Bug 842561.  Assert that our inheritance chain is correct for WebIDL objects.  r=peterv
    We could drop the descriptor for Text if we changed nsIDocument::CreateTextNode
    to return an already_AddRefed<Text>, but then we'd need more casting in
    nsDocument.cpp for the XPCOM CreateTextNode.  Not sure which way is better,
    really.

---
+++ content/base/src/Text.cpp
@@ -0,0 +1,23 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/Text.h"
+
+namespace mozilla {
+namespace dom {
+
+already_AddRefed<Text>
+Text::SplitText(uint32_t aOffset, ErrorResult& rv)
+{
+  nsCOMPtr<nsIContent> newChild;
+  rv = SplitData(aOffset, getter_AddRefs(newChild));
+  if (rv.Failed()) {
+    return nullptr;
+  }
+  return static_cast<Text*>(newChild.forget().get());
+}
+
+} // namespace dom
+} // namespace mozilla
