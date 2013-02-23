7d2c968594194b9779b390c43de50e7fd4c7888e
Benoit Girard <b56girard@gmail.com>
2013-02-22 19:28:08 -0500

    Bug 839740 - Add Moz2D Unit Tests to gtest. r=bas

---
+++ gfx/2d/unittest/GTestMain.cpp
@@ -0,0 +1,27 @@
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "gtest/gtest.h"
+#include "TestBase.h"
+#include "TestPoint.h"
+#include "TestScaling.h"
+
+TEST(Moz2D, Point) {
+  TestBase* test = new TestPoint();
+  int failures = 0;
+  int totalTests = test->RunTests(&failures);
+  delete test;
+
+  ASSERT_EQ(failures, 0);
+}
+
+TEST(Moz2D, Scaling) {
+  TestBase* test = new TestScaling();
+  int failures = 0;
+  int totalTests = test->RunTests(&failures);
+  delete test;
+
+  ASSERT_EQ(failures, 0);
+}
