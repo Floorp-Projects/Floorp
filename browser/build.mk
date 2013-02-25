1b27561fa47c8d7e28a1d1c7a23fb132703a9ad4
Gregory Szorc <gps@mozilla.com>
2013-02-25 12:47:18 -0800

    Bug 784841 - Part 18d: Convert /browser; r=gavin f=Ms2ger

--- browser/build.mk
+++ browser/build.mk
@@ -2,26 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ifndef LIBXUL_SDK
-include $(topsrcdir)/toolkit/toolkit-tiers.mk
-endif
-
-TIERS += app
-
-ifdef MOZ_EXTENSIONS
-tier_app_dirs += extensions
-endif
-
-tier_app_dirs += $(MOZ_BRANDING_DIRECTORY)
-
-ifdef MOZ_WEBAPP_RUNTIME
-tier_app_dirs += webapprt
-endif
-
-tier_app_dirs += browser
-# Never add other tier_app_dirs after browser. They won't get packaged
-# properly on mac.
-
 ################################################
 # Parallel build on Windows with GNU make check
 
