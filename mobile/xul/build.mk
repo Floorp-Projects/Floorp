3253f187f5cb119e8c017b3f01386aa1b0a609ee
Gregory Szorc <gps@mozilla.com>
2013-02-25 12:47:23 -0800

    Bug 784841 - Part 18x: Convert /mobile/xul; r=ted

--- mobile/xul/build.mk
+++ mobile/xul/build.mk
@@ -2,32 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ifndef LIBXUL_SDK
-# Needed for building our components as part of libxul
-APP_LIBXUL_DIRS += mobile/xul/components/build
-
-include $(topsrcdir)/toolkit/toolkit-tiers.mk
-else
-ifdef ENABLE_TESTS
-tier_testharness_dirs += \
-  testing/mochitest \
-  $(NULL)
-endif
-endif
-
-TIERS += app
-
-ifdef MOZ_EXTENSIONS
-tier_app_dirs += extensions
-endif
-
-tier_app_dirs += \
-  $(MOZ_BRANDING_DIRECTORY) \
-  mobile/xul \
-  $(NULL)
-
-
-installer: 
+installer:
 	@$(MAKE) -C mobile/xul/installer installer
 
 package:
