a75d2bc49fc5ba9b7cd3c4fe7f59102b0feca41d
Masayuki Nakano <masayuki@d-toybox.com>
2013-02-25 13:00:06 +0900

    Bug 840409  part.11 Implement nsIMEContext::AssociateDefaultContext() and nsIMEContext::Disassociate() r=jimm

--- widget/windows/WinIMEHandler.h
+++ widget/windows/WinIMEHandler.h
@@ -74,6 +74,11 @@ public:
   static bool GetOpenState(nsWindow* aWindow);
 
   /**
+   * Called when the window is destroying.
+   */
+  static void OnDestroyWindow(nsWindow* aWindow);
+
+  /**
    * "Kakutei-Undo" of ATOK or WXG (both of them are Japanese IME) causes
    * strange WM_KEYDOWN/WM_KEYUP/WM_CHAR message pattern.  So, when this
    * returns true, the caller needs to be careful for processing the messages.
