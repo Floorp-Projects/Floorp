f06e52935e5e40024d8ef7b66f89dffe1e47cbc1
Kannan Vijayan <kvijayan@mozilla.com>
2013-02-26 15:11:51 -0500

    Bug 844515 - Add BaselineInspector source files. r=bhackett

---
+++ js/src/ion/BaselineInspector.cpp
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=4 sw=4 et tw=99:
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "BaselineIC.h"
+#include "BaselineInspector.h"
+
+using namespace js;
+using namespace js::ion;
+
+RawShape
+BaselineInspector::maybeMonomorphicShapeForPropertyOp(jsbytecode *pc)
+{
+    if (!hasBaselineScript())
+        return NULL;
+
+    JS_ASSERT(isValidPC(pc));
+    const ICEntry &entry = icEntryFromPC(pc);
+
+    ICStub *stub = entry.firstStub();
+    ICStub *next = stub->next();
+
+    if (!next || !next->isFallback())
+        return NULL;
+
+    if (stub->isGetProp_Native()) {
+        JS_ASSERT(next->isGetProp_Fallback());
+        if (next->toGetProp_Fallback()->hadUnoptimizableAccess())
+            return NULL;
+        return stub->toGetProp_Native()->shape();
+    }
+
+    if (stub->isSetProp_Native()) {
+        JS_ASSERT(next->isSetProp_Fallback());
+        if (next->toSetProp_Fallback()->hadUnoptimizableAccess())
+            return NULL;
+        return stub->toSetProp_Native()->shape();
+    }
+
+    return NULL;
+}
---
+++ js/src/ion/BaselineInspector.h
@@ -0,0 +1,62 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=4 sw=4 et tw=99:
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#if !defined(jsion_baseline_inspector_h__) && defined(JS_ION)
+#define jsion_baseline_inspector_h__
+
+#include "jscntxt.h"
+#include "jscompartment.h"
+
+#include "BaselineJIT.h"
+
+namespace js {
+namespace ion {
+
+class BaselineInspector
+{
+  private:
+    JSContext *cx;
+    RootedScript script;
+
+  public:
+    BaselineInspector(JSContext *cx, RawScript rawScript)
+      : cx(cx), script(cx, rawScript)
+    {
+        JS_ASSERT(script);
+    }
+
+    bool hasBaselineScript() const {
+        return script->hasBaselineScript();
+    }
+
+    BaselineScript *baselineScript() const {
+        JS_ASSERT(hasBaselineScript());
+        return script->baseline;
+    }
+
+  private:
+#ifdef DEBUG
+    bool isValidPC(jsbytecode *pc) {
+        return (pc >= script->code) && (pc < script->code + script->length);
+    }
+#endif
+
+    ICEntry &icEntryFromPC(jsbytecode *pc) {
+        JS_ASSERT(hasBaselineScript());
+        JS_ASSERT(isValidPC(pc));
+        return baselineScript()->icEntryFromPCOffset(pc - script->code);
+    }
+
+  public:
+    RawShape maybeMonomorphicShapeForPropertyOp(jsbytecode *pc);
+};
+
+} // namespace ion
+} // namespace js
+
+#endif
+
