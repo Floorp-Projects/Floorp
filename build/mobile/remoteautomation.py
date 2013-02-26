77ad15b281da5a9d232e5928fe1afe5e4a94ec7e
Geoff Brown <gbrown@mozilla.com>
2013-02-26 07:19:58 -0700

    Bug 844797 - Avoid UnboundLocalError on dumpDir in checkForCrashes; r=edmorley

--- build/mobile/remoteautomation.py
+++ build/mobile/remoteautomation.py
@@ -123,6 +123,7 @@ class RemoteAutomation(Automation):
         if javaException:
             return True
         try:
+            dumpDir = tempfile.mkdtemp()
             remoteCrashDir = self._remoteProfile + '/minidumps/'
             if not self._devicemanager.dirExists(remoteCrashDir):
                 # As of this writing, the minidumps directory is automatically
@@ -131,7 +132,6 @@ class RemoteAutomation(Automation):
                 print "Automation Error: No crash directory (%s) found on remote device" % remoteCrashDir
                 # Whilst no crash was found, the run should still display as a failure
                 return True
-            dumpDir = tempfile.mkdtemp()
             self._devicemanager.getDirectory(remoteCrashDir, dumpDir)
             crashed = automationutils.checkForCrashes(dumpDir, symbolsPath,
                                             self.lastTestSeen)
