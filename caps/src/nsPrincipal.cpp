ec4cd42529c55d47c06f83cbadcc873a7c38da79
Bobby Holley <bobbyholley@gmail.com>
2013-02-26 11:04:12 -0800

    Bug 834732 - Push a cx in nsPrincipal::SetDomain. r=mrbkap

--- caps/src/nsPrincipal.cpp
+++ caps/src/nsPrincipal.cpp
@@ -492,8 +492,7 @@ nsPrincipal::SetDomain(nsIURI* aDomain)
 
   // Recompute all wrappers between compartments using this principal and other
   // non-chrome compartments.
-  JSContext *cx = nsContentUtils::GetSafeJSContext();
-  NS_ENSURE_TRUE(cx, NS_ERROR_FAILURE);
+  SafeAutoJSContext cx;
   JSPrincipals *principals = nsJSPrincipals::get(static_cast<nsIPrincipal*>(this));
   bool success = js::RecomputeWrappers(cx, js::ContentCompartmentsOnly(),
                                        js::CompartmentsWithPrincipals(principals));
