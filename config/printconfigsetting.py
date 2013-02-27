c340414b48a1a0b15ecebf991295c68e8ee345db
Yati Sagade <yati.sagade@gmail.com>
2013-02-27 22:30:56 +0530

    Bug 812179 - Removed hacks for Python < 2.6 from config/ [r=ted]

--- config/printconfigsetting.py
+++ config/printconfigsetting.py
@@ -1,13 +1,14 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from __future__ import print_function
 
 import configobj, sys
 
 try:
     (file, section, key) = sys.argv[1:]
 except ValueError:
-    print "Usage: printconfigsetting.py <file> <section> <setting>"
+    print("Usage: printconfigsetting.py <file> <section> <setting>")
     sys.exit(1)
 
 c = configobj.ConfigObj(file)
@@ -15,11 +16,11 @@ c = configobj.ConfigObj(file)
 try:
     s = c[section]
 except KeyError:
-    print >>sys.stderr, "Section [%s] not found." % section
+    print("Section [{0}] not found.".format(section), file=sys.stderr)
     sys.exit(1)
 
 try:
-    print s[key]
+    print(s[key])
 except KeyError:
-    print >>sys.stderr, "Key %s not found." % key
+    print("Key {0} not found.".format(key), file=sys.stderr)
     sys.exit(1)
