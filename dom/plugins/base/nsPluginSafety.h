8bb1a482fb6de77a7da5c4222be7f52dbde59e72
Ed Morley <emorley@mozilla.com>
2013-02-27 12:50:30 +0000

    Backed out changeset f73b7b8d1a2d (bug 829557) for failing to compile on Windows on a CLOSED TREE

--- dom/plugins/base/nsPluginSafety.h
+++ dom/plugins/base/nsPluginSafety.h
@@ -18,23 +18,23 @@
   #define MAIN_THREAD_JNI_REF_GUARD
 #endif
 
-PRIntervalTime NS_NotifyBeginPluginCall(NSPluginCallReentry aReentryState);
-void NS_NotifyPluginCall(PRIntervalTime aTime, NSPluginCallReentry aReentryState);
+PRIntervalTime NS_NotifyBeginPluginCall();
+void NS_NotifyPluginCall(PRIntervalTime);
 
-#define NS_TRY_SAFE_CALL_RETURN(ret, fun, pluginInst, pluginCallReentry) \
+#define NS_TRY_SAFE_CALL_RETURN(ret, fun, pluginInst) \
 PR_BEGIN_MACRO                                     \
   MAIN_THREAD_JNI_REF_GUARD;                       \
-  PRIntervalTime startTime = NS_NotifyBeginPluginCall(pluginCallReentry); \
+  PRIntervalTime startTime = NS_NotifyBeginPluginCall(); \
   ret = fun;                                       \
-  NS_NotifyPluginCall(startTime, pluginCallReentry); \
+  NS_NotifyPluginCall(startTime);		               \
 PR_END_MACRO
 
-#define NS_TRY_SAFE_CALL_VOID(fun, pluginInst, pluginCallReentry) \
+#define NS_TRY_SAFE_CALL_VOID(fun, pluginInst)     \
 PR_BEGIN_MACRO                                     \
   MAIN_THREAD_JNI_REF_GUARD;                       \
-  PRIntervalTime startTime = NS_NotifyBeginPluginCall(pluginCallReentry); \
+  PRIntervalTime startTime = NS_NotifyBeginPluginCall(); \
   fun;                                             \
-  NS_NotifyPluginCall(startTime, pluginCallReentry); \
+  NS_NotifyPluginCall(startTime);		               \
 PR_END_MACRO
 
 #endif //nsPluginSafety_h_
--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -820,7 +820,7 @@ nsRefreshDriver::Tick(int64_t aNowEpoch, TimeStamp aNowTime)
   NS_PRECONDITION(!nsContentUtils::GetCurrentJSContext(),
                   "Shouldn't have a JSContext on the stack");
 
-  if (nsNPAPIPluginInstance::InPluginCallUnsafeForReentry()) {
+  if (nsNPAPIPluginInstance::InPluginCall()) {
     NS_ERROR("Refresh driver should not run during plugin call!");
     // Try to survive this by just ignoring the refresh tick.
     return;
