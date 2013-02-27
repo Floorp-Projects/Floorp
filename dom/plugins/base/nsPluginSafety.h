9b4faa088ff14f54570f363a716ce3e2f34b2db9
Robert O'Callahan <robert@ocallahan.org>
2013-02-28 00:50:27 +1300

    Bug 829557. Part 1: When calling into plugin code, identify situations where it is safe (or unsafe) to reenter Gecko from plugin code. r=bsmedberg When Gecko is reentered from unsafe call sites, we allow the reentry, but we suppress execution of the refresh driver to minimize the danger. In this patch, we treat all sites as unsafe.

--- dom/plugins/base/nsPluginSafety.h
+++ dom/plugins/base/nsPluginSafety.h
@@ -18,23 +18,23 @@
   #define MAIN_THREAD_JNI_REF_GUARD
 #endif
 
-PRIntervalTime NS_NotifyBeginPluginCall();
-void NS_NotifyPluginCall(PRIntervalTime);
+PRIntervalTime NS_NotifyBeginPluginCall(NSPluginCallReentry aReentryState);
+void NS_NotifyPluginCall(PRIntervalTime aTime, NSPluginCallReentry aReentryState);
 
-#define NS_TRY_SAFE_CALL_RETURN(ret, fun, pluginInst) \
+#define NS_TRY_SAFE_CALL_RETURN(ret, fun, pluginInst, pluginCallReentry) \
 PR_BEGIN_MACRO                                     \
   MAIN_THREAD_JNI_REF_GUARD;                       \
-  PRIntervalTime startTime = NS_NotifyBeginPluginCall(); \
+  PRIntervalTime startTime = NS_NotifyBeginPluginCall(pluginCallReentry); \
   ret = fun;                                       \
-  NS_NotifyPluginCall(startTime);		               \
+  NS_NotifyPluginCall(startTime, pluginCallReentry); \
 PR_END_MACRO
 
-#define NS_TRY_SAFE_CALL_VOID(fun, pluginInst)     \
+#define NS_TRY_SAFE_CALL_VOID(fun, pluginInst, pluginCallReentry) \
 PR_BEGIN_MACRO                                     \
   MAIN_THREAD_JNI_REF_GUARD;                       \
-  PRIntervalTime startTime = NS_NotifyBeginPluginCall(); \
+  PRIntervalTime startTime = NS_NotifyBeginPluginCall(pluginCallReentry); \
   fun;                                             \
-  NS_NotifyPluginCall(startTime);		               \
+  NS_NotifyPluginCall(startTime, pluginCallReentry); \
 PR_END_MACRO
 
 #endif //nsPluginSafety_h_
