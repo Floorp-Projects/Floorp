371f5d9835d01a73e3fe3d2e34e3d8a3db250a97
Rafael Ávila de Espíndola <respindola@mozilla.org>
2013-02-28 16:35:18 -0500

    Bug 846298 - Misc cleanups to the write poisoning base/OS split. r=BenWa.

--- xpcom/build/mozPoisonWriteBase.h
+++ xpcom/build/mozPoisonWriteBase.h
@@ -17,53 +17,9 @@
 #include "mozilla/Scoped.h"
 
 namespace mozilla {
-
-
-struct DebugFDAutoLockTraits {
-  typedef PRLock *type;
-  const static type empty() {
-    return nullptr;
-  }
-  const static void release(type aL) {
-    PR_Unlock(aL);
-  }
-};
-
-class DebugFDAutoLock : public Scoped<DebugFDAutoLockTraits> {
-  static PRLock *Lock;
-public:
-  static void Clear();
-  static PRLock *getDebugFDsLock() {
-    // On windows this static is not thread safe, but we know that the first
-    // call is from
-    // * An early registration of a debug FD or
-    // * The call to InitWritePoisoning.
-    // Since the early debug FDs are logs created early in the main thread
-    // and no writes are trapped before InitWritePoisoning, we are safe.
-    static bool Initialized = false;
-    if (!Initialized) {
-      Lock = PR_NewLock();
-      Initialized = true;
-    }
-
-    // We have to use something lower level than a mutex. If we don't, we
-    // can get recursive in here when called from logging a call to free.
-    return Lock;
-  }
-
-  DebugFDAutoLock() : Scoped<DebugFDAutoLockTraits>(getDebugFDsLock()) {
-    PR_Lock(get());
-  }
-};
-
 bool PoisonWriteEnabled();
 bool ValidWriteAssert(bool ok);
-void BaseCleanup();
-// This method should always be called with the debugFDs lock.
-// Note: MSVC Local static is NOT thread safe
-// http://stackoverflow.com/questions/10585928/is-static-init-thread-safe-with-vc2010
-std::vector<int>& getDebugFDs();
-
+bool IsDebugFD(int fd);
 }
 
 #endif
