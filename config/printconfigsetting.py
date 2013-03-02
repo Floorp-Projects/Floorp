e21aafd494c4f7a6e8e01d75c6ba19f166dea709
Justin Wood <Callek@gmail.com>
2013-03-02 13:31:19 -0500

    Backout part of Bug 812179 for SeaMonkey bustage. rs+=gps

--- config/printconfigsetting.py
+++ config/printconfigsetting.py
@@ -1,14 +1,13 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from __future__ import print_function
 
 import configobj, sys
 
 try:
     (file, section, key) = sys.argv[1:]
 except ValueError:
-    print("Usage: printconfigsetting.py <file> <section> <setting>")
+    print "Usage: printconfigsetting.py <file> <section> <setting>"
     sys.exit(1)
 
 c = configobj.ConfigObj(file)
@@ -16,11 +15,11 @@ c = configobj.ConfigObj(file)
 try:
     s = c[section]
 except KeyError:
-    print("Section [{0}] not found.".format(section), file=sys.stderr)
+    print >>sys.stderr, "Section [%s] not found." % section
     sys.exit(1)
 
 try:
-    print(s[key])
+    print s[key]
 except KeyError:
-    print("Key {0} not found.".format(key), file=sys.stderr)
+    print >>sys.stderr, "Key %s not found." % key
     sys.exit(1)
