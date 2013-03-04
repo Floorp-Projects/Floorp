ad63958320374baf4f53f1436b28e7331253301d
Mounir Lamouri <mounir.lamouri@gmail.com>
2013-03-04 14:06:02 +0100

    Bug 832923 - re enable input file, r=baku

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -653,10 +653,6 @@ pref("webgl.can-lose-context-in-foreground", false);
 pref("memory_info_dumper.watch_fifo.enabled", true);
 pref("memory_info_dumper.watch_fifo.directory", "/data/local");
 
-// <input type='file'> implementation is not complete. We have to disable the
-// type to web content to help them do feature detection.
-pref("dom.disable_input_file", true);
-
 pref("general.useragent.enable_overrides", true);
 
 pref("b2g.version", @MOZ_B2G_VERSION@);
