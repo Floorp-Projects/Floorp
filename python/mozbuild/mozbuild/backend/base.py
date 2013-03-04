356f5aa7e2dd1442a2baddc1b038a9f7640ae300
Gregory Szorc <gps@mozilla.com>
2013-03-04 10:32:57 -0800

    Bug 847073 - Record and print detailed summary of moz.build execution; r=glandium

--- python/mozbuild/mozbuild/backend/base.py
+++ python/mozbuild/mozbuild/backend/base.py
@@ -11,11 +11,78 @@ from abc import (
 
 import os
 import sys
+import time
 
 from mach.mixin.logging import LoggingMixin
 
-from ..frontend.data import SandboxDerived
+from ..frontend.data import (
+    ReaderSummary,
+    SandboxDerived,
+)
 from .configenvironment import ConfigEnvironment
+
+
+class BackendConsumeSummary(object):
+    """Holds state about what a backend did.
+
+    This is used primarily to print a summary of what the backend did
+    so people know what's going on.
+    """
+    def __init__(self):
+        # How many moz.build files were read. This includes included files.
+        self.mozbuild_count = 0
+
+        # The number of derived objects from the read moz.build files.
+        self.object_count = 0
+
+        # The total wall time this backend spent consuming objects. If
+        # the iterable passed into consume() is a generator, this includes the
+        # time spent to read moz.build files.
+        self.wall_time = 0.0
+
+        # CPU time spent by during the interval captured by wall_time.
+        self.cpu_time = 0.0
+
+        # The total wall time spent executing moz.build files. This is just
+        # the read and execute time. It does not cover consume time.
+        self.mozbuild_execution_time = 0.0
+
+        # The total wall time spent in the backend. This counts the time the
+        # backend writes out files, etc.
+        self.backend_execution_time = 0.0
+
+        # How much wall time the system spent doing other things. This is
+        # wall_time - mozbuild_execution_time - backend_execution_time.
+        self.other_time = 0.0
+
+    @property
+    def reader_summary(self):
+        return 'Finished reading {:d} moz.build files into {:d} descriptors in {:.2f}s'.format(
+            self.mozbuild_count, self.object_count,
+            self.mozbuild_execution_time)
+
+    @property
+    def backend_summary(self):
+        return 'Backend executed in {:.2f}s'.format(self.backend_execution_time)
+
+    def backend_detailed_summary(self):
+        """Backend summary to be supplied by BuildBackend implementations."""
+        return None
+
+    @property
+    def total_summary(self):
+        return 'Total wall time: {:.2f}s; CPU time: {:.2f}s; Efficiency: {:.0%}'.format(
+            self.wall_time, self.cpu_time, self.cpu_time / self.wall_time)
+
+    def summaries(self):
+        yield self.reader_summary
+        yield self.backend_summary
+
+        detailed = self.backend_detailed_summary()
+        if detailed:
+            yield detailed
+
+        yield self.total_summary
 
 
 class BuildBackend(LoggingMixin):
@@ -34,6 +101,7 @@ class BuildBackend(LoggingMixin):
         self.populate_logger()
 
         self.environment = environment
+        self.summary = BackendConsumeSummary()
 
         self._environments = {}
         self._environments[environment.topobjdir] = environment
@@ -74,9 +142,19 @@ class BuildBackend(LoggingMixin):
         base class consumes objects and calls methods (possibly) implemented by
         child classes.
         """
+        cpu_start = time.clock()
+        time_start = time.time()
+        backend_time = 0.0
 
         for obj in objs:
+            self.summary.object_count += 1
+            obj_start = time.time()
             self.consume_object(obj)
+            backend_time += time.time() - obj_start
+
+            if isinstance(obj, ReaderSummary):
+                self.summary.mozbuild_count = obj.total_file_count
+                self.summary.mozbuild_execution_time = obj.total_execution_time
 
         # Write out a file indicating when this backend was last generated.
         age_file = os.path.join(self.environment.topobjdir,
@@ -84,7 +162,18 @@ class BuildBackend(LoggingMixin):
         with open(age_file, 'a'):
             os.utime(age_file, None)
 
+        finished_start = time.time()
         self.consume_finished()
+        backend_time += time.time() - finished_start
+
+        self.summary.cpu_time = time.clock() - cpu_start
+        self.summary.wall_time = time.time() - time_start
+        self.summary.backend_execution_time = backend_time
+        self.summary.other_time = self.summary.wall_time - \
+            self.summary.mozbuild_execution_time - \
+            self.summary.backend_execution_time
+
+        return self.summary
 
     @abstractmethod
     def consume_object(self, obj):
