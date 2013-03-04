79803b71d7a26a780aa2f8aa8f3a28790cb6d7f2
Kannan Vijayan <kvijayan@mozilla.com>
2013-03-04 11:54:06 -0500

    Bug 846531 - Add optimized stub for Compare(Boolean x Int32). r=bhackett

--- js/src/ion/x86/MacroAssembler-x86.h
+++ js/src/ion/x86/MacroAssembler-x86.h
@@ -685,6 +685,9 @@ class MacroAssemblerX86 : public MacroAssemblerX86Shared
     Register extractInt32(const ValueOperand &value, Register scratch) {
         return value.payloadReg();
     }
+    Register extractBoolean(const ValueOperand &value, Register scratch) {
+        return value.payloadReg();
+    }
     Register extractTag(const Address &address, Register scratch) {
         movl(tagOf(address), scratch);
         return scratch;
