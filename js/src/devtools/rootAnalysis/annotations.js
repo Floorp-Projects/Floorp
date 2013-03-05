4226f18fe6202fe677d2b77fa792ab851d829bbc
Steve Fink <sfink@mozilla.com>
2013-03-05 14:48:13 -0800

    Bug 838029 - Import bhackett static rooting analysis into tree. r=bhackett
    --HG--
    extra : rebase_source : 1f9104c6ea23868dfc93c8805b554356f22e233f

---
+++ js/src/devtools/rootAnalysis/annotations.js
@@ -0,0 +1,98 @@
+/* -*- Mode: Javascript; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+"use strict";
+
+function indirectCallCannotGC(caller, name)
+{
+    if (name == "mallocSizeOf")
+        return true;
+
+    // hook called during script finalization which cannot GC.
+    if (/CallDestroyScriptHook/.test(caller))
+        return true;
+
+    return false;
+}
+
+// classes to ignore indirect calls on.
+var ignoreClasses = [
+    "JSTracer",
+    "JSStringFinalizer",
+    "SprintfStateStr",
+    "JSLocaleCallbacks",
+    "JSC::ExecutableAllocator"
+];
+
+function fieldCallCannotGC(csu, field)
+{
+    for (var i = 0; i < ignoreClasses.length; i++) {
+        if (csu == ignoreClasses[i])
+            return true;
+    }
+    if (csu == "js::Class" && (field == "trace" || field == "finalize"))
+        return true;
+    if (csu == "JSRuntime" && field == "destroyPrincipals")
+        return true;
+    return false;
+}
+
+function shouldSuppressGC(name)
+{
+    // Various dead code that should only be called inside AutoEnterAnalysis.
+    // Functions with no known caller are by default treated as not suppressing GC.
+    return /TypeScript::Purge/.test(name)
+        || /StackTypeSet::addPropagateThis/.test(name)
+        || /ScriptAnalysis::addPushedType/.test(name)
+        || /IonBuilder/.test(name);
+}
+
+function ignoreEdgeUse(edge, variable)
+{
+    // Functions which should not be treated as using variable.
+    if (edge.Kind == "Call") {
+        var callee = edge.Exp[0];
+        if (callee.Kind == "Var") {
+            var name = callee.Variable.Name[0];
+            if (/~Anchor/.test(name))
+                return true;
+            if (/::Unrooted\(\)/.test(name))
+                return true;
+            if (/::~Unrooted\(\)/.test(name))
+                return true;
+            if (/~DebugOnly/.test(name))
+                return true;
+        }
+    }
+
+    return false;
+}
+
+function ignoreGCFunction(fun)
+{
+    // XXX modify refillFreeList<NoGC> to not need data flow analysis to understand it cannot GC.
+    if (/refillFreeList/.test(fun) && /\(js::AllowGC\)0u/.test(fun))
+        return true;
+    return false;
+}
+
+function isRootedTypeName(name)
+{
+    if (name.startsWith('struct '))
+        name = name.substr(7);
+    if (name.startsWith('class '))
+        name = name.substr(6);
+    if (name.startsWith('const '))
+        name = name.substr(6);
+    if (name.startsWith('js::'))
+        name = name.substr(4);
+    if (name.startsWith('JS::'))
+        name = name.substr(4);
+
+    return name.startsWith('Rooted');
+}
+
+function isSuppressConstructor(name)
+{
+    return /::AutoSuppressGC/.test(name)
+        || /::AutoEnterAnalysis/.test(name);
+}
