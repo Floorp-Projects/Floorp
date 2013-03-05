d5095cae67086065cd791a15b3950fcc06968d18
Ted Mielczarek <ted@mielczarek.org>
2013-03-05 09:41:26 -0500

    bug 779291 - Breakpad Makefile changes. r=glandium

--- build/unix/stdc++compat/stdc++compat.cpp
+++ build/unix/stdc++compat/stdc++compat.cpp
@@ -34,6 +34,7 @@ namespace std {
     template istream& istream::_M_extract(unsigned int&);
     template istream& istream::_M_extract(unsigned long&);
     template istream& istream::_M_extract(unsigned short&);
+    template istream& istream::_M_extract(unsigned long long&);
 #endif
 #if MOZ_LIBSTDCXX_VERSION >= GLIBCXX_VERSION(3, 4, 14)
     /* Instantiate these templates to avoid GLIBCXX_3.4.14 symbol versions
