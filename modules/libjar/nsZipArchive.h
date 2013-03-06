8d7fa0bbcd5a1a77ef26164dde44f3a5427a8428
Aaron Klotz <aklotz@mozilla.com>
2013-03-06 11:58:29 -0700

    Bug 810151: readahead for ordered jar files r=taras

--- modules/libjar/nsZipArchive.h
+++ modules/libjar/nsZipArchive.h
@@ -392,6 +392,9 @@ private:
   nsZipHandle();
   ~nsZipHandle();
 
+#if defined(XP_WIN)
+  PRFileDesc *                      mFd;     /* nspr file descriptor */
+#endif
   PRFileMap *                       mMap;    /* nspr datastructure for mmap */
   nsAutoPtr<nsZipItemPtr<uint8_t> > mBuf;
   nsrefcnt                          mRefCnt; /* ref count */
