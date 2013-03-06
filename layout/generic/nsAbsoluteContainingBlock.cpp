076a4bfaee885032c9fb71be5a2533ea7cc59920
Ryan VanderMeulen <ryanvm@gmail.com>
2013-03-06 16:11:05 -0500

    Backed out 9 changesets (bug 716403) for Android 4.0 robocop bustage on a CLOSED TREE.

--- layout/generic/nsAbsoluteContainingBlock.cpp
+++ layout/generic/nsAbsoluteContainingBlock.cpp
@@ -366,17 +366,7 @@ nsAbsoluteContainingBlock::ReflowAbsoluteFrame(nsIFrame*                aDelegat
 
   nsresult  rv;
   // Get the border values
-  nsMargin border = aReflowState.mStyleBorder->GetComputedBorder();
-
-  // Respect fixed position margins.
-  if (aDelegatingFrame->GetAbsoluteListID() == nsIFrame::kFixedList) {
-    const nsMargin& fixedMargins = aPresContext->PresShell()->
-      GetContentDocumentFixedPositionMargins();
-
-    border += fixedMargins;
-    aContainingBlockWidth -= fixedMargins.left + fixedMargins.right;
-    aContainingBlockHeight -= fixedMargins.top + fixedMargins.bottom;
-  }
+  const nsMargin& border = aReflowState.mStyleBorder->GetComputedBorder();
 
   nscoord availWidth = aContainingBlockWidth;
   if (availWidth == -1) {
