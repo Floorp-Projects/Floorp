377a3edafd8cbdcb69d4c6ce49a559a98585c77e
Masayuki Nakano <masayuki@d-toybox.com>
2013-03-06 15:14:34 +0900

    Bug 558976 part.4 Implement nsIWidget::NotifyIME() on GTK r=karlt

--- widget/gtk2/nsGtkIMModule.h
+++ widget/gtk2/nsGtkIMModule.h
@@ -84,7 +84,7 @@ public:
                       bool aKeyDownEventWasSent = false);
 
     // IME related nsIWidget methods.
-    nsresult ResetInputState(nsWindow* aCaller);
+    nsresult CommitIMEComposition(nsWindow* aCaller);
     void SetInputContext(nsWindow* aCaller,
                          const InputContext* aContext,
                          const InputContextAction* aAction);
