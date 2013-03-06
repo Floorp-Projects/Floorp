81bd73e76bbf11c5719d1093a8c35acb435db1b0
Jon Coppeard <jcoppeard@mozilla.com>
2013-03-06 16:41:42 +0000

    Bug 848395 - GC: Move Rooted to JS namespace - rename js::Rooted to JS::Rooted inside SpiderMonkey r=terrence

--- js/src/jsapi-tests/testScriptObject.cpp
+++ js/src/jsapi-tests/testScriptObject.cpp
@@ -21,7 +21,7 @@ struct ScriptObjectFixture : public JSAPITest {
 
     bool tryScript(JS::HandleObject global, JSScript *scriptArg)
     {
-        js::RootedScript script(cx, scriptArg);
+        JS::RootedScript script(cx, scriptArg);
         CHECK(script);
 
         JS_GC(rt);
