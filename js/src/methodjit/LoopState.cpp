8608d4bbec9838bdd0c023acc98ba461a1cdba57
Jeff Walden <jwalden@mit.edu>
2013-03-06 15:29:26 -0800

    Bug 847480 - Remove all DeprecatedAbs uses from range analysis code in SpiderMonkey, fixing a couple bugs and adding tests for those bugs.  r=nbp

--- js/src/methodjit/LoopState.cpp
+++ js/src/methodjit/LoopState.cpp
@@ -19,7 +19,7 @@ using namespace js::mjit;
 using namespace js::analyze;
 using namespace js::types;
 
-using mozilla::DeprecatedAbs;
+using mozilla::Abs;
 
 LoopState::LoopState(JSContext *cx, analyze::CrossScriptSSA *ssa,
                      mjit::Compiler *cc, FrameState *frame)
@@ -2029,9 +2029,9 @@ LoopState::computeInterval(const CrossSSAValue &cv, int32_t *pmin, int32_t *pmax
         if (!computeInterval(rhsv, &rhsmin, &rhsmax) || rhsmin != rhsmax)
             return false;
 
-        int32_t rhs = DeprecatedAbs(rhsmax);
-        *pmin = -(rhs - 1);
-        *pmax = rhs - 1;
+        uint32_t absRHS = Abs(rhsmax);
+        *pmin = -int32_t(absRHS - 1);
+        *pmax = int32_t(absRHS - 1);
         return true;
       }
 
@@ -2063,8 +2063,8 @@ LoopState::computeInterval(const CrossSSAValue &cv, int32_t *pmin, int32_t *pmax
         if (lhsmin == INT32_MIN || rhsmin == INT32_MIN)
             return false;
 
-        int32_t nlhs = Max(DeprecatedAbs(lhsmin), DeprecatedAbs(lhsmax));
-        int32_t nrhs = Max(DeprecatedAbs(rhsmin), DeprecatedAbs(rhsmax));
+        int32_t nlhs = int32_t(Max(Abs(lhsmin), Abs(lhsmax)));
+        int32_t nrhs = int32_t(Max(Abs(rhsmin), Abs(rhsmax)));
 
         if (!SafeMul(nlhs, nrhs, pmax))
             return false;
