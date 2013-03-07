9d5077b507fc5772d3aa87bf5724c596d8faced0
Gavin Sharp <gavin@gavinsharp.com>
2013-03-06 23:43:44 -0800

    Bug 831236: remove mobile/xul, r=axel,mfinkle,gps,glandium

--- mobile/xul/app/profile/extensions/feedback@mobile.mozilla.org/defaults/preferences/preferences.js
+++
@@ -1,6 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-pref("extensions.feedback.url.happy", "http://input.mozilla.com/happy");
-pref("extensions.feedback.url.sad", "http://input.mozilla.com/sad");
