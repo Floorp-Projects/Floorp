6edba067998488e255cad1931a602dbed1b475a9
Terrence Cole <terrence@mozilla.com>
2013-03-07 10:55:34 -0800

    Bug 849453 - Use the AllocKind to get the size instead of sizeOfThis; r=billm

--- js/src/jswrapper.cpp
+++ js/src/jswrapper.cpp
@@ -215,7 +215,9 @@ bool CrossCompartmentWrapper::finalizeInBackground(Value priv)
      * Make the 'background-finalized-ness' of the wrapper the same as the
      * wrapped object, to allow transplanting between them.
      */
-    return IsBackgroundFinalized(priv.toObject().getAllocKind());
+    if (IsInsideNursery(priv.toObject().runtime(), &priv.toObject()))
+        return false;
+    return IsBackgroundFinalized(priv.toObject().tenuredGetAllocKind());
 }
 
 #define PIERCE(cx, wrapper, pre, op, post)                      \
