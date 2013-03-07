cc661bf97597eb7e72ab79ab083bc71a5b4cbb21
Gregory Szorc <gps@mozilla.com>
2013-03-07 05:05:03 -0800

    Bug 818246 - Part 2: Support moz.build -> Makefile variable passthru; r=glandium

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -7,8 +7,9 @@ from __future__ import unicode_literals
 import os
 
 from .data import (
-    DirectoryTraversal,
     ConfigFileSubstitution,
+    DirectoryTraversal,
+    VariablePassthru,
     ReaderSummary,
 )
 
@@ -68,6 +69,14 @@ class TreeMetadataEmitter(object):
             sub.output_path = os.path.join(sandbox['OBJDIR'], path)
             sub.relpath = path
             yield sub
+
+        # Proxy some variables as-is until we have richer classes to represent
+        # them. We should aim to keep this set small because it violates the
+        # desired abstraction of the build definition away from makefiles.
+        passthru = VariablePassthru(sandbox)
+
+        if passthru.variables:
+            yield passthru
 
     def _emit_directory_traversal_from_sandbox(self, sandbox):
         o = DirectoryTraversal(sandbox)
