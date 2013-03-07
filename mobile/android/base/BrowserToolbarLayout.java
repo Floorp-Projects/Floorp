45cd2be77ffcd81524d46915c0fd79174041bd49
Chris Lord <chrislord.net@gmail.com>
2013-03-07 10:17:33 +0000

    Bug 716403 - Offset fixed layers so the toolbar doesn't obscure them. r=nrc,kats
    Offset fixed layers in the compositor so that the toolbar in Firefox for Android
    doesn't obscure them. This does not affect layout, so input on the elements in
    said layers will appear broken.

--- mobile/android/base/BrowserToolbarLayout.java
+++ mobile/android/base/BrowserToolbarLayout.java
@@ -30,5 +30,30 @@ public class BrowserToolbarLayout extends LinearLayout {
 
         return super.onTouchEvent(event);
     }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+
+        if (t != oldt) {
+            refreshMargins();
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        if (h != oldh) {
+            refreshMargins();
+        }
+    }
+
+    public void refreshMargins() {
+        LayerView view = GeckoApp.mAppContext.getLayerView();
+        if (view != null) {
+            view.getLayerClient().setFixedLayerMargins(0, getHeight() - getScrollY(), 0, 0);
+        }
+    }
 }
 
