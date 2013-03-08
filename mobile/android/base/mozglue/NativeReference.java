39dd3c8a6a33103e29a09e40768e97d74efaece9
Mike Hommey <mh+mozilla@glandium.org>
2013-03-08 09:27:52 +0100

    Bug 842681 - Use the linker Zip code through JNI for GeckoJarReader. r=kats

---
+++ mobile/android/base/mozglue/NativeReference.java
@@ -0,0 +1,13 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.mozglue;
+
+public interface NativeReference
+{
+    public void release();
+
+    public boolean isReleased();
+}
