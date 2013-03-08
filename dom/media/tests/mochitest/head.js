a3321dcb16a3d449d4e2b08d15c0f00a5c61acfc
Ryan VanderMeulen <ryanvm@gmail.com>
2013-03-08 16:08:45 -0500

    Backed out changeset 4740975a1063 (bug 837458) for Android mochitest failures.

--- dom/media/tests/mochitest/head.js
+++ dom/media/tests/mochitest/head.js
@@ -9,110 +9,13 @@ var Cr = SpecialPowers.Cr;
 // Specifies whether we are using fake streams to run this automation
 var FAKE_ENABLED = true;
 
-
-/**
- * Create the necessary HTML elements for head and body as used by Mochitests
- *
- * @param {object} meta
- *        Meta information of the test
- * @param {string} meta.title
- *        Description of the test
- * @param {string} [meta.bug]
- *        Bug the test was created for
- * @param {boolean} [meta.visible=false]
- *        Visibility of the media elements
- */
-function createHTML(meta) {
-  var test = document.getElementById('test');
-
-  // Create the head content
-  var elem = document.createElement('meta');
-  elem.setAttribute('charset', 'utf-8');
-  document.head.appendChild(elem);
-
-  var title = document.createElement('title');
-  title.textContent = meta.title;
-  document.head.appendChild(title);
-
-  // Create the body content
-  var anchor = document.createElement('a');
-  anchor.setAttribute('target', '_blank');
-
-  if (meta.bug) {
-    anchor.setAttribute('href', 'https://bugzilla.mozilla.org/show_bug.cgi?id=' + meta.bug);
-  }
-
-  anchor.textContent = meta.title;
-  document.body.insertBefore(anchor, test);
-
-  var display = document.createElement('p');
-  display.setAttribute('id', 'display');
-  document.body.insertBefore(display, test);
-
-  var content = document.createElement('div');
-  content.setAttribute('id', 'content');
-  content.style.display = meta.visible ? 'block' : "none";
-  document.body.appendChild(content);
-}
-
-
-/**
- * Create the HTML element if it doesn't exist yet and attach
- * it to the content node.
- *
- * @param {string} type
- *        Type of media element to create ('audio' or 'video')
- * @param {string} label
- *        Description to use for the element
- * @return {HTMLMediaElement} The created HTML media element
- */
-function createMediaElement(type, label) {
-  var id = label + '_' + type;
-  var element = document.getElementById(id);
-
-  // Sanity check that we haven't created the element already
-  if (element)
-    return element;
-
-  element = document.createElement(type === 'audio' ? 'audio' : 'video');
-  element.setAttribute('id', id);
-  element.setAttribute('height', 100);
-  element.setAttribute('width', 150);
-  element.setAttribute('controls', 'controls');
-  document.getElementById('content').appendChild(element);
-
-  return element;
-}
-
-
-/**
- * Wrapper function for mozGetUserMedia to allow a singular area of control
- * for determining whether we run this with fake devices or not.
- *
- * @param {Dictionary} constraints
- *        The constraints for this mozGetUserMedia callback
- * @param {Function} onSuccess
- *        The success callback if the stream is successfully retrieved
- * @param {Function} onError
- *        The error callback if the stream fails to be retrieved
- */
-function getUserMedia(constraints, onSuccess, onError) {
-  constraints["fake"] = FAKE_ENABLED;
-
-  info("Call getUserMedia for " + JSON.stringify(constraints));
-  navigator.mozGetUserMedia(constraints, onSuccess, onError);
-}
-
-
 /**
  * Setup any Mochitest for WebRTC by enabling the preference for
- * peer connections. As by bug 797979 it will also enable mozGetUserMedia()
- * and disable the mozGetUserMedia() permission checking.
+ * peer connections. As by bug 797979 it will also enable mozGetUserMedia().
  *
- * @param {Function} aCallback
- *        Test method to execute after initialization
- * @param {Boolean} desktopSupportedOnly
- *        Specifies if the test currently is known to work on desktop only
+ * @param {Function} aCallback Test method to execute after initialization
+ * @param {Boolean} desktopSupportedOnly specifies if the test currently
+ *                                       is known to work on desktop only
  */
 function runTest(aCallback, desktopSupportedOnly) {
   SimpleTest.waitForExplicitFinish();
@@ -125,9 +28,8 @@ function runTest(aCallback, desktopSupportedOnly) {
     SimpleTest.finish();
   } else {
     SpecialPowers.pushPrefEnv({'set': [
-        ['media.peerconnection.enabled', true],
-        ['media.navigator.permission.denied', true]]
-      }, function () {
+      ['media.peerconnection.enabled', true],
+      ['media.navigator.permission.denied', true]]}, function () {
       try {
         aCallback();
       }
@@ -138,13 +40,27 @@ function runTest(aCallback, desktopSupportedOnly) {
   }
 }
 
+/**
+ * Wrapper function for mozGetUserMedia to allow a singular area of control
+ * for determining whether we run this with fake devices or not.
+ *
+ * @param {Dictionary} constraints the constraints for this mozGetUserMedia
+ *                                 callback
+ * @param {Function} onSuccess the success callback if the stream is
+ *                             successfully retrieved
+ * @param {Function} onError the error callback if the stream fails to be
+ *                           retrieved
+ */
+function getUserMedia(constraints, onSuccess, onError) {
+  constraints["fake"] = FAKE_ENABLED;
+  navigator.mozGetUserMedia(constraints, onSuccess, onError);
+}
 
 /**
  * A callback function fired only under unexpected circumstances while
  * running the tests. Kills off the test as well gracefully.
  *
- * @param {object} aObj
- *        The object fired back from the callback
+ * @param {object} aObj The object fired back from the callback
  */
 function unexpectedCallbackAndFinish(aObj) {
   ok(false, "Unexpected error callback with " + aObj);
