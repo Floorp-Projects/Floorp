4c391005cbc586c0dbfcca3b0bd06c35e695ff52
Bill McCloskey <wmccloskey@mozilla.com>
2013-03-07 18:01:25 -0800

    Bug 848602 - Add some volatile annotations (r=terrence)

--- js/src/gc/Heap.h
+++ js/src/gc/Heap.h
@@ -664,7 +664,7 @@ const size_t ArenasPerChunk = ChunkBytesAvailable / BytesPerArenaWithHeader;
 /* A chunk bitmap contains enough mark bits for all the cells in a chunk. */
 struct ChunkBitmap
 {
-    uintptr_t bitmap[ArenaBitmapWords * ArenasPerChunk];
+    volatile uintptr_t bitmap[ArenaBitmapWords * ArenasPerChunk];
 
     MOZ_ALWAYS_INLINE void getMarkWordAndMask(const Cell *cell, uint32_t color,
                                               uintptr_t **wordp, uintptr_t *maskp)
