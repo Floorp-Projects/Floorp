54099eae9bb7996581364fc1f8009abc80a9a604
Shih-Chiang Chien <schien@mozilla.com>
2013-03-08 11:17:29 +0800

    Bug 847310 - WBMP decoder implementation. r=joe

--- image/src/RasterImage.cpp
+++ image/src/RasterImage.cpp
@@ -25,6 +25,7 @@
 #include "nsBMPDecoder.h"
 #include "nsICODecoder.h"
 #include "nsIconDecoder.h"
+#include "nsWBMPDecoder.h"
 
 #include "gfxContext.h"
 
@@ -2607,6 +2608,9 @@ RasterImage::InitDecoder(bool aDoSizeDecode)
       break;
     case eDecoderType_icon:
       mDecoder = new nsIconDecoder(*this, observer);
+      break;
+    case eDecoderType_wbmp:
+      mDecoder = new nsWBMPDecoder(*this, observer);
       break;
     default:
       NS_ABORT_IF_FALSE(0, "Shouldn't get here!");
