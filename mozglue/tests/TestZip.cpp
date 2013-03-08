a6da2dc544b6424f69d52a282d2b84ae6a31af8d
Mike Hommey <mh+mozilla@glandium.org>
2013-03-08 09:24:46 +0100

    Bug 842681 - Refactor the linker Zip code and allow to use an existing memory buffer as a Zip file. r=mwu

--- mozglue/tests/TestZip.cpp
+++ mozglue/tests/TestZip.cpp
@@ -5,6 +5,7 @@
 #include <cstdio>
 #include <unistd.h>
 #include "Zip.h"
+#include "mozilla/RefPtr.h"
 
 extern "C" void report_mapping() { }
 
@@ -44,19 +45,19 @@ int main(int argc, char *argv[])
   }
   chdir(argv[1]);
   Zip::Stream s;
-  Zip z("test.zip");
+  mozilla::RefPtr<Zip> z = ZipCollection::GetZip("test.zip");
   for (size_t i = 0; i < sizeof(test_entries) / sizeof(*test_entries); i++) {
-    if (!z.GetStream(test_entries[i], &s)) {
+    if (!z->GetStream(test_entries[i], &s)) {
       fprintf(stderr, "TEST-UNEXPECTED-FAIL | TestZip | test.zip: Couldn't get entry \"%s\"\n", test_entries[i]);
       return 1;
     }
   }
   fprintf(stderr, "TEST-PASS | TestZip | test.zip could be accessed fully\n");
 
-  Zip z2("no_central_dir.zip");
+  z = ZipCollection::GetZip("no_central_dir.zip");
   for (size_t i = 0; i < sizeof(no_central_dir_entries)
                          / sizeof(*no_central_dir_entries); i++) {
-    if (!z2.GetStream(no_central_dir_entries[i], &s)) {
+    if (!z->GetStream(no_central_dir_entries[i], &s)) {
       fprintf(stderr, "TEST-UNEXPECTED-FAIL | TestZip | no_central_dir.zip: Couldn't get entry \"%s\"\n", no_central_dir_entries[i]);
       return 1;
     }
