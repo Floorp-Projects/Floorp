9f63f73498a0325b3e44ae00ddf2212656ed7c49
Jacek Caban <jacek@codeweavers.com>
2013-03-09 17:26:07 +0100

    Bug 849174 - inline function declared as dllimport warning js jsfriendapi.h r=bholley

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -1418,7 +1418,7 @@ class JS_FRIEND_API(AutoCTypesActivityCallback) {
 extern JS_FRIEND_API(void)
 assertEnteredPolicy(JSContext *cx, JSObject *obj, jsid id);
 #else
-inline JS_FRIEND_API(void) assertEnteredPolicy(JSContext *cx, JSObject *obj, jsid id) {};
+inline void assertEnteredPolicy(JSContext *cx, JSObject *obj, jsid id) {};
 #endif
 
 } /* namespace js */
