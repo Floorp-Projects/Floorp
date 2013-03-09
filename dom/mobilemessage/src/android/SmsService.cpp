8ec9ae536123819b219e481be87dd719903928fb
Gene Lian <clian@mozilla.com>
2013-03-09 15:22:25 +0800

    Bug 844431 - B2G MMS: provide nsIDOMMobileMessageManager interface (with sendMMS() first) (part 4-2, nsIMmsService.send()). r=vicamo a=leo+

--- dom/mobilemessage/src/android/SmsService.cpp
+++ dom/mobilemessage/src/android/SmsService.cpp
@@ -51,37 +51,6 @@ SmsService::Send(const nsAString& aNumber, const nsAString& aMessage,
   return NS_OK;
 }
 
-NS_IMETHODIMP
-SmsService::CreateSmsMessage(int32_t aId,
-                             const nsAString& aDelivery,
-                             const nsAString& aDeliveryStatus,
-                             const nsAString& aSender,
-                             const nsAString& aReceiver,
-                             const nsAString& aBody,
-                             const nsAString& aMessageClass,
-                             const jsval& aTimestamp,
-                             const bool aRead,
-                             JSContext* aCx,
-                             nsIDOMMozSmsMessage** aMessage)
-{
-  return SmsMessage::Create(aId, aDelivery, aDeliveryStatus,
-                            aSender, aReceiver,
-                            aBody, aMessageClass, aTimestamp, aRead,
-                            aCx, aMessage);
-}
-
-NS_IMETHODIMP
-SmsService::CreateSmsSegmentInfo(int32_t aSegments,
-                                 int32_t aCharsPerSegment,
-                                 int32_t aCharsAvailableInLastSegment,
-                                 nsIDOMMozSmsSegmentInfo** aSegmentInfo)
-{
-  nsCOMPtr<nsIDOMMozSmsSegmentInfo> info =
-      new SmsSegmentInfo(aSegments, aCharsPerSegment, aCharsAvailableInLastSegment);
-  info.forget(aSegmentInfo);
-  return NS_OK;
-}
-
 } // namespace mobilemessage
 } // namespace dom
 } // namespace mozilla
--- dom/mobilemessage/src/ipc/SmsIPCService.cpp
+++ dom/mobilemessage/src/ipc/SmsIPCService.cpp
@@ -79,37 +79,6 @@ SmsIPCService::Send(const nsAString& aNumber,
   return NS_OK;
 }
 
-NS_IMETHODIMP
-SmsIPCService::CreateSmsMessage(int32_t aId,
-                                const nsAString& aDelivery,
-                                const nsAString& aDeliveryStatus,
-                                const nsAString& aSender,
-                                const nsAString& aReceiver,
-                                const nsAString& aBody,
-                                const nsAString& aMessageClass,
-                                const jsval& aTimestamp,
-                                const bool aRead,
-                                JSContext* aCx,
-                                nsIDOMMozSmsMessage** aMessage)
-{
-  return SmsMessage::Create(aId, aDelivery, aDeliveryStatus,
-                            aSender, aReceiver,
-                            aBody, aMessageClass, aTimestamp, aRead,
-                            aCx, aMessage);
-}
-
-NS_IMETHODIMP
-SmsIPCService::CreateSmsSegmentInfo(int32_t aSegments,
-                                    int32_t aCharsPerSegment,
-                                    int32_t aCharsAvailableInLastSegment,
-                                    nsIDOMMozSmsSegmentInfo** aSegmentInfo)
-{
-  nsCOMPtr<nsIDOMMozSmsSegmentInfo> info =
-      new SmsSegmentInfo(aSegments, aCharsPerSegment, aCharsAvailableInLastSegment);
-  info.forget(aSegmentInfo);
-  return NS_OK;
-}
-
 /*
  * Implementation of nsIMobileMessageDatabaseService.
  */
