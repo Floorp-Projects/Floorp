629aa8262d5f003f3e05e57fef4749f33d3f2598
Phil Ringnalda <philringnalda@gmail.com>
2013-03-09 08:15:42 -0800

    Back out c9f5d3ebba39 (bug 835552) for Windows build bustage CLOSED TREE

--- js/src/tests/ecma_5/Object/15.2.3.6-new-definition.js
+++ js/src/tests/ecma_5/Object/15.2.3.6-new-definition.js
@@ -8,7 +8,7 @@ var summary = 'ES5 Object.defineProperty(O, P, Attributes): new definition';
 
 print(BUGNUMBER + ": " + summary);
 
-load("defineProperty-setup.js");
+load("ecma_5/Object/defineProperty-setup.js");
 
 /**************
  * BEGIN TEST *
