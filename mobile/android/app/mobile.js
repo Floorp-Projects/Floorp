ddaa376ecc72ec8f3333c7d58c2d23e1f21ebc35
Raymond Lee <raymond@raysquare.com>
2013-03-12 00:10:45 +0800

    Bug 847814 - Remove browser.geolocation.warning.infoURL preference for mobile/andriod if it's not used. r=margaret

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -384,9 +384,6 @@ pref("privacy.item.geolocation", true);
 pref("privacy.item.siteSettings", true);
 pref("privacy.item.syncAccount", true);
 
-// URL to the Learn More link XXX this is the firefox one.  Bug 495578 fixes this.
-pref("browser.geolocation.warning.infoURL", "http://www.mozilla.com/%LOCALE%/firefox/geolocation/");
-
 // enable geo
 pref("geo.enabled", true);
 
