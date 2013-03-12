b3839debd3b9febdca38db7ef4fd221916565ff8
Ryan VanderMeulen <ryanvm@gmail.com>
2013-03-12 16:16:30 -0400

    Backed out changesets 4ee69256f8f2, 2becc603eeb8, and afad1c17015e (bug 849916) for bustage on a CLOSED TREE.

--- content/media/webaudio/PannerNode.h
+++ content/media/webaudio/PannerNode.h
@@ -23,14 +23,8 @@ class PannerNode : public AudioNode
 {
 public:
   explicit PannerNode(AudioContext* aContext);
-  virtual ~PannerNode();
 
   virtual JSObject* WrapObject(JSContext* aCx, JSObject* aScope);
-
-  virtual bool SupportsMediaStreams() const MOZ_OVERRIDE
-  {
-    return true;
-  }
 
   PanningModelType PanningModel() const
   {
@@ -39,7 +33,6 @@ public:
   void SetPanningModel(PanningModelType aPanningModel)
   {
     mPanningModel = aPanningModel;
-    SendInt32ParameterToStream(PANNING_MODEL, int32_t(mPanningModel));
   }
 
   DistanceModelType DistanceModel() const
@@ -49,7 +42,6 @@ public:
   void SetDistanceModel(DistanceModelType aDistanceModel)
   {
     mDistanceModel = aDistanceModel;
-    SendInt32ParameterToStream(DISTANCE_MODEL, int32_t(mDistanceModel));
   }
 
   void SetPosition(double aX, double aY, double aZ)
@@ -57,7 +49,6 @@ public:
     mPosition.x = aX;
     mPosition.y = aY;
     mPosition.z = aZ;
-    SendThreeDPointParameterToStream(POSITION, mPosition);
   }
 
   void SetOrientation(double aX, double aY, double aZ)
@@ -65,7 +56,6 @@ public:
     mOrientation.x = aX;
     mOrientation.y = aY;
     mOrientation.z = aZ;
-    SendThreeDPointParameterToStream(ORIENTATION, mOrientation);
   }
 
   void SetVelocity(double aX, double aY, double aZ)
@@ -73,7 +63,6 @@ public:
     mVelocity.x = aX;
     mVelocity.y = aY;
     mVelocity.z = aZ;
-    SendThreeDPointParameterToStream(VELOCITY, mVelocity);
   }
 
   double RefDistance() const
@@ -83,7 +72,6 @@ public:
   void SetRefDistance(double aRefDistance)
   {
     mRefDistance = aRefDistance;
-    SendDoubleParameterToStream(REF_DISTANCE, mRefDistance);
   }
 
   double MaxDistance() const
@@ -93,7 +81,6 @@ public:
   void SetMaxDistance(double aMaxDistance)
   {
     mMaxDistance = aMaxDistance;
-    SendDoubleParameterToStream(MAX_DISTANCE, mMaxDistance);
   }
 
   double RolloffFactor() const
@@ -103,7 +90,6 @@ public:
   void SetRolloffFactor(double aRolloffFactor)
   {
     mRolloffFactor = aRolloffFactor;
-    SendDoubleParameterToStream(ROLLOFF_FACTOR, mRolloffFactor);
   }
 
   double ConeInnerAngle() const
@@ -113,7 +99,6 @@ public:
   void SetConeInnerAngle(double aConeInnerAngle)
   {
     mConeInnerAngle = aConeInnerAngle;
-    SendDoubleParameterToStream(CONE_INNER_ANGLE, mConeInnerAngle);
   }
 
   double ConeOuterAngle() const
@@ -123,7 +108,6 @@ public:
   void SetConeOuterAngle(double aConeOuterAngle)
   {
     mConeOuterAngle = aConeOuterAngle;
-    SendDoubleParameterToStream(CONE_OUTER_ANGLE, mConeOuterAngle);
   }
 
   double ConeOuterGain() const
@@ -133,31 +117,7 @@ public:
   void SetConeOuterGain(double aConeOuterGain)
   {
     mConeOuterGain = aConeOuterGain;
-    SendDoubleParameterToStream(CONE_OUTER_GAIN, mConeOuterGain);
   }
-
-private:
-  friend class AudioListener;
-  friend class PannerNodeEngine;
-  enum EngineParameters {
-    LISTENER_POSITION,
-    LISTENER_ORIENTATION,
-    LISTENER_UPVECTOR,
-    LISTENER_VELOCITY,
-    LISTENER_DOPPLER_FACTOR,
-    LISTENER_SPEED_OF_SOUND,
-    PANNING_MODEL,
-    DISTANCE_MODEL,
-    POSITION,
-    ORIENTATION,
-    VELOCITY,
-    REF_DISTANCE,
-    MAX_DISTANCE,
-    ROLLOFF_FACTOR,
-    CONE_INNER_ANGLE,
-    CONE_OUTER_ANGLE,
-    CONE_OUTER_GAIN
-  };
 
 private:
   PanningModelType mPanningModel;
