d1cd1674500d8869410d69d321218704f773f747
Ryan VanderMeulen <ryanvm@gmail.com>
2013-03-12 16:16:30 -0400

    Backed out changesets 4ee69256f8f2, 2becc603eeb8, and afad1c17015e (bug 849916) for bustage on a CLOSED TREE.

--- content/media/AudioNodeStream.h
+++ content/media/AudioNodeStream.h
@@ -19,10 +19,6 @@
 
 namespace mozilla {
 
-namespace dom {
-struct ThreeDPoint;
-}
-
 class ThreadSharedFloatArrayBufferList;
 
 /**
@@ -58,7 +54,6 @@ public:
   void SetDoubleParameter(uint32_t aIndex, double aValue);
   void SetInt32Parameter(uint32_t aIndex, int32_t aValue);
   void SetTimelineParameter(uint32_t aIndex, const dom::AudioParamTimeline& aValue);
-  void SetThreeDPointParameter(uint32_t aIndex, const dom::ThreeDPoint& aValue);
   void SetBuffer(already_AddRefed<ThreadSharedFloatArrayBufferList> aBuffer);
 
   virtual AudioNodeStream* AsAudioNodeStream() { return this; }
