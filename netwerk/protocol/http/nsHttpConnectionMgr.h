7f2bd0e8e9170af12b801d0f95fbfa6b0425b5d0
Patrick McManus <mcmanus@ducksong.com>
2013-03-12 16:00:58 -0400

    bug 829120 - spdy could call connection::Activate re-entrantly r=honzab

--- netwerk/protocol/http/nsHttpConnectionMgr.h
+++ netwerk/protocol/http/nsHttpConnectionMgr.h
@@ -538,7 +538,6 @@ private:
                                              nsHttpTransaction *trans);
 
     void               ProcessSpdyPendingQ(nsConnectionEntry *ent);
-    void               ProcessAllSpdyPendingQ();
     static PLDHashOperator ProcessSpdyPendingQCB(
         const nsACString &key, nsAutoPtr<nsConnectionEntry> &ent,
         void *closure);
@@ -604,6 +603,7 @@ private:
     void OnMsgUpdateParam          (int32_t, void *);
     void OnMsgClosePersistentConnections (int32_t, void *);
     void OnMsgProcessFeedback      (int32_t, void *);
+    void OnMsgProcessAllSpdyPendingQ (int32_t, void *);
 
     // Total number of active connections in all of the ConnectionEntry objects
     // that are accessed from mCT connection table.
