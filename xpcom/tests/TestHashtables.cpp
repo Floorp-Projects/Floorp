d681b53e23969938933025923c711bd74a51e5a7
Kyle Huey <khuey@kylehuey.com>
2013-03-12 13:33:40 -0700

    Bug 849654: Kill nsClassHashtableMT. r=bsmedberg

--- xpcom/tests/TestHashtables.cpp
+++ xpcom/tests/TestHashtables.cpp
@@ -596,67 +596,6 @@ main(void) {
   printf("OK\n");
 
   //
-  // now check a thread-safe class-hashtable
-  //
-
-  nsClassHashtableMT<nsCStringHashKey,TestUniChar> EntToUniClassL;
-
-  printf("Initializing nsClassHashtableMT...");
-  EntToUniClassL.Init(ENTITY_COUNT);
-  printf("OK\n");
-
-  printf("Filling hash with %u entries.\n", ENTITY_COUNT);
-
-  for (i = 0; i < ENTITY_COUNT; ++i) {
-    printf("  Putting entry %u...", gEntities[i].mUnicode);
-    TestUniChar* temp = new TestUniChar(gEntities[i].mUnicode);
-
-    EntToUniClassL.Put(nsDependentCString(gEntities[i].mStr), temp);
-    printf("OK...\n");
-  }
-
-  printf("Testing Get:\n");
-
-  for (i = 0; i < ENTITY_COUNT; ++i) {
-    printf("  Getting entry %s...", gEntities[i].mStr);
-    if (!EntToUniClassL.Get(nsDependentCString(gEntities[i].mStr), &myChar)) {
-      printf("FAILED\n");
-      exit (18);
-    }
-
-    printf("Found %c\n", myChar->GetChar());
-  }
-
-  printf("Testing nonexistent entries...");
-  if (EntToUniClassL.Get(NS_LITERAL_CSTRING("xxxx"), &myChar)) {
-    printf("FOUND! BAD!\n");
-    exit (19);
-  }
-      
-  printf("not found; good.\n");
-      
-  printf("Enumerating:\n");
-  
-  count = EntToUniClassL.EnumerateRead(nsCEnumRead, nullptr);
-  if (count != ENTITY_COUNT) {
-    printf("  Bad count!\n");
-    exit (20);
-  }
-  
-  printf("Clearing...\n");
-  EntToUniClassL.Clear();
-  printf("  Clearing OK\n");
-
-  printf("Checking count...");
-  count = EntToUniClassL.Enumerate(nsCEnum, nullptr);
-  if (count) {
-    printf("  Clear did not remove all entries.\n");
-    exit (21);
-  }
-
-  printf("OK\n");
-
-  //
   // now check a data-hashtable with an interface key
   //
 
