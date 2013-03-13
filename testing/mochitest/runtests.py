012064caa4fbbf970d2f460785929512b8d5b345
Steve Fink <sfink@mozilla.com>
2013-03-13 13:12:07 -0700

    Bug 850903 - runtests.py --debugger should suppress the timeout. r=ted

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -745,7 +745,7 @@ class Mochitest(object):
     # then again to actually run mochitest
     if options.timeout:
       timeout = options.timeout + 30
-    elif not options.autorun:
+    elif options.debugger or not options.autorun:
       timeout = None
     else:
       timeout = 330.0 # default JS harness timeout is 300 seconds
