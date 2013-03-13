db18757691100baa4131d25dc690555613b4665c
vliu <vliu@mozilla.com>
2013-03-13 14:44:43 +0800

    Bug 849330 - Camera recording needs AID_SDCARD_RW capability. r=dhylands

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -410,10 +410,7 @@ PrivilegesForApp(mozIApplication* aApp)
     const SpecialPermission specialPermissions[] = {
         // FIXME/bug 785592: implement a CameraBridge so we don't have
         // to hack around with OS permissions
-        { "camera", base::PRIVILEGES_CAMERA },
-        // FIXME/bug 793034: change our video architecture so that we
-        // can stream video from remote processes
-        { "deprecated-hwvideo", base::PRIVILEGES_VIDEO }
+        { "camera", base::PRIVILEGES_CAMERA }
     };
     for (size_t i = 0; i < ArrayLength(specialPermissions); ++i) {
         const char* const permission = specialPermissions[i].perm;
