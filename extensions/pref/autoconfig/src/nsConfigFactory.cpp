11946d42cbab70f309607edb594a10a0be21b990
Joshua Cranmer <Pidgeot18@gmail.com>
2013-03-13 16:11:04 -0500

    Bug 846540 - Remove dead code from mozilla-central to let comm-central build. r=Standard8, NPOTFFB.
    Note that the actual implementation of code that would be used can be found in
    comm-central's ldap/xpcom part of the library.

--- extensions/pref/autoconfig/src/nsConfigFactory.cpp
+++ extensions/pref/autoconfig/src/nsConfigFactory.cpp
@@ -7,37 +7,22 @@
 #include "nsAutoConfig.h"
 #include "nsReadConfig.h"
 #include "nsIAppStartupNotifier.h"
-#if defined(MOZ_LDAP_XPCOM)
-#include "nsLDAPSyncQuery.h"
-#endif
 
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsAutoConfig, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsReadConfig, Init)
-#if defined(MOZ_LDAP_XPCOM)
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsLDAPSyncQuery)
-#endif
 
 NS_DEFINE_NAMED_CID(NS_AUTOCONFIG_CID);
 NS_DEFINE_NAMED_CID(NS_READCONFIG_CID);
-#if defined(MOZ_LDAP_XPCOM)
-NS_DEFINE_NAMED_CID(NS_LDAPSYNCQUERY_CID);
-#endif
 
 static const mozilla::Module::CIDEntry kAutoConfigCIDs[] = {
   { &kNS_AUTOCONFIG_CID, false, NULL, nsAutoConfigConstructor },
   { &kNS_READCONFIG_CID, false, NULL, nsReadConfigConstructor },
-#if defined(MOZ_LDAP_XPCOM)
-  { &kNS_LDAPSYNCQUERY_CID, false, NULL, nsLDAPSyncQueryConstructor },
-#endif
   { NULL }
 };
 
 static const mozilla::Module::ContractIDEntry kAutoConfigContracts[] = {
   { NS_AUTOCONFIG_CONTRACTID, &kNS_AUTOCONFIG_CID },
   { NS_READCONFIG_CONTRACTID, &kNS_READCONFIG_CID },
-#if defined(MOZ_LDAP_XPCOM)
-  { "@mozilla.org/ldapsyncquery;1", &kNS_LDAPSYNCQUERY_CID },
-#endif
   { NULL }
 };
 
