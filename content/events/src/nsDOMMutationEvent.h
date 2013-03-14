69238cb695c04bb01dc4d7aeea23df04dd391f48
Olli Pettay <Olli.Pettay@helsinki.fi>
2013-03-14 23:18:20 +0200

    Bug 847586 - Paris binding for MutationEvent, r=Ms2ger

--- content/events/src/nsDOMMutationEvent.h
+++ content/events/src/nsDOMMutationEvent.h
@@ -8,6 +8,8 @@
 
 #include "nsIDOMMutationEvent.h"
 #include "nsDOMEvent.h"
+#include "nsMutationEvent.h"
+#include "mozilla/dom/MutationEventBinding.h"
 
 class nsDOMMutationEvent : public nsDOMEvent,
                            public nsIDOMMutationEvent
@@ -17,13 +19,48 @@ public:
                      nsPresContext* aPresContext, nsMutationEvent* aEvent);
 
   virtual ~nsDOMMutationEvent();
-                     
+
   NS_DECL_ISUPPORTS_INHERITED
 
   NS_DECL_NSIDOMMUTATIONEVENT
 
   // Forward to base class
   NS_FORWARD_TO_NSDOMEVENT
+
+  virtual JSObject* WrapObject(JSContext* aCx, JSObject* aScope)
+  {
+    return mozilla::dom::MutationEventBinding::Wrap(aCx, aScope, this);
+  }
+
+  // xpidl implementation
+  // GetPrevValue(nsAString& aPrevValue);
+  // GetNewValue(nsAString& aNewValue);
+  // GetAttrName(nsAString& aAttrName);
+
+  already_AddRefed<nsINode> GetRelatedNode()
+  {
+    nsCOMPtr<nsINode> n =
+      do_QueryInterface(static_cast<nsMutationEvent*>(mEvent)->mRelatedNode);
+    return n.forget();
+  }
+
+  uint16_t AttrChange()
+  {
+    return static_cast<nsMutationEvent*>(mEvent)->mAttrChange;
+  }
+
+  void InitMutationEvent(const nsAString& aType,
+                         bool& aCanBubble, bool& aCancelable,
+                         nsINode* aRelatedNode,
+                         const nsAString& aPrevValue,
+                         const nsAString& aNewValue,
+                         const nsAString& aAttrName,
+                         uint16_t& aAttrChange, mozilla::ErrorResult& aRv)
+  {
+    aRv = InitMutationEvent(aType, aCanBubble, aCancelable,
+                            aRelatedNode ? aRelatedNode->AsDOMNode() : nullptr,
+                            aPrevValue, aNewValue, aAttrName, aAttrChange);
+  }
 };
 
 #endif // nsDOMMutationEvent_h__
