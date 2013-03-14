9a64baf98feae5e0a598a29b1a081867b4dffd2b
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2013-03-13 19:23:03 -0700

    Bug 850349 - Cleanup browser search tests while debugging intermittent failure in browser_contextmenu.js. r=gavin
    * browser_426329.js - Fix exception in popupshowing event listener causing search history dropdown to remain open on subsequent tests.
    * browser_contextmenu.js - Add some debug messages.
                             - Move |doOnloadOnce(checkSearchURL);| just before the search tab opens so there is less interference with later tests.
                             - Add nsISelectionListener to know when selection has finished.
    * head.js - Consolidate duplicated doOnloadOnce function.
    * test.html - Fix character set warning.

--- browser/components/search/test/browser_contextmenu.js
+++ browser/components/search/test/browser_contextmenu.js
@@ -30,6 +30,7 @@ function test() {
     }
   }
 
+  registerCleanupFunction(finalize);
   Services.obs.addObserver(observer, "browser-search-engine-modified", false);
   ss.addEngine("http://mochi.test:8888/browser/browser/components/search/test/testEngine_mozsearch.xml",
                Ci.nsISearchEngine.DATA_XML, "data:image/x-icon,%00",
@@ -46,6 +47,7 @@ function test() {
 
   function testContextMenu() {
     function rightClickOnDocument() {
+      info("rightClickOnDocument: " + content.window.location);
       waitForBrowserContextMenu(checkContextMenu);
       var clickTarget = content.document.body;
       var eventDetails = { type: "contextmenu", button: 2 };
@@ -54,30 +56,39 @@ function test() {
 
     // check the search menu item and then perform a search
     function checkContextMenu() {
+      info("checkContextMenu");
       var searchItem = contextMenu.getElementsByAttribute("id", "context-searchselect")[0];
       ok(searchItem, "Got search context menu item");
       is(searchItem.label, 'Search ' + ENGINE_NAME + ' for "test search"', "Check context menu label");
       is(searchItem.disabled, false, "Check that search context menu item is enabled");
+      doOnloadOnce(checkSearchURL);
       searchItem.click();
       contextMenu.hidePopup();
     }
 
-    function checkSearchURL(event){
+    function checkSearchURL(event) {
       is(event.originalTarget.URL,
          "http://mochi.test:8888/browser/browser/components/search/test/?test=test+search&ie=utf-8&client=app&channel=contextsearch",
          "Checking context menu search URL");
       finalize();
     }
 
-    doOnloadOnce(checkSearchURL);
+    var selectionListener = {
+      notifySelectionChanged: function(doc, sel, reason) {
+        if (reason != Ci.nsISelectionListener.SELECTALL_REASON || sel.toString() != "test search")
+          return;
+        info("notifySelectionChanged: Text selected");
+        content.window.getSelection().QueryInterface(Ci.nsISelectionPrivate).
+                                      removeSelectionListener(selectionListener);
+        SimpleTest.executeSoon(rightClickOnDocument);
+      }
+    };
 
+    // add a listener to know when the selection takes effect
+    content.window.getSelection().QueryInterface(Ci.nsISelectionPrivate).
+                                  addSelectionListener(selectionListener);
     // select the text on the page
-    var selectAllItem = contextMenu.getElementsByAttribute("id", "context-selectall")[0];
-    ok(selectAllItem, "Got select all context menu item");
-    selectAllItem.click();
-
-    // wait for the selection to take effect
-    SimpleTest.executeSoon(rightClickOnDocument);
+    goDoCommand('cmd_selectAll');
   }
 
   function finalize() {
@@ -86,13 +97,7 @@ function test() {
     }
     content.location.href = "about:blank";
     var engine = ss.getEngineByName(ENGINE_NAME);
-    ss.removeEngine(engine);
-  }
-
-  function doOnloadOnce(callback) {
-    gBrowser.addEventListener("DOMContentLoaded", function handleLoad(event) {
-      gBrowser.removeEventListener("DOMContentLoaded", handleLoad, true);
-      callback(event);
-    }, true);
+    if (engine)
+      ss.removeEngine(engine);
   }
 }
