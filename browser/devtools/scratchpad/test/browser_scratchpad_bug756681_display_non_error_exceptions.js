df67539788f3dd8e0b3640e615290cd27634215d
Brandon Benvie <bbenvie@mozilla.com>
2013-03-14 18:08:19 -0700

    Bug 851381 - make the scratchpad evaluate code asynchronously; r=harth

--- browser/devtools/scratchpad/test/browser_scratchpad_bug756681_display_non_error_exceptions.js
+++ browser/devtools/scratchpad/test/browser_scratchpad_bug756681_display_non_error_exceptions.js
@@ -27,80 +27,81 @@ function runTests()
   var error2 = "throw \"A thrown string\"";
   var error3 = "throw {}";
   var error4 = "document.body.appendChild(document.body)";
-  let messageArray = {};
-  let count = {};
 
-  // Display message
-  scratchpad.setText(message);
-  scratchpad.display();
-  is(scratchpad.getText(),
-      message + openComment + "Hello World!" + closeComment,
-      "message display output");
+  let tests = [{
+    // Display message
+    method: "display",
+    code: message,
+    result: message + openComment + "Hello World!" + closeComment,
+    label: "message display output"
+  },
+  {
+    // Display error1, throw new Error("Ouch")
+    method: "display",
+    code: error1,
+    result: error1 + openComment +
+            "Exception: Ouch!\n@" + scratchpad.uniqueName + ":1" + closeComment,
+    label: "error display output"
+  },
+  {
+    // Display error2, throw "A thrown string"
+    method: "display",
+    code: error2,
+    result: error2 + openComment + "Exception: A thrown string" + closeComment,
+    label: "thrown string display output"
+  },
+  {
+    // Display error3, throw {}
+    method: "display",
+    code: error3,
+    result: error3 + openComment + "Exception: [object Object]" + closeComment,
+    label: "thrown object display output"
+  },
+  {
+    // Display error4, document.body.appendChild(document.body)
+    method: "display",
+    code: error4,
+    result: error4 + openComment + "Exception: Node cannot be inserted " +
+            "at the specified point in the hierarchy\n@1" + closeComment,
+    label: "Alternative format error display output"
+  },
+  {
+    // Run message
+    method: "run",
+    code: message,
+    result: message,
+    label: "message run output"
+  },
+  {
+    // Run error1, throw new Error("Ouch")
+    method: "run",
+    code: error1,
+    result: error1 + openComment +
+            "Exception: Ouch!\n@" + scratchpad.uniqueName + ":1" + closeComment,
+    label: "error run output"
+  },
+  {
+    // Run error2, throw "A thrown string"
+    method: "run",
+    code: error2,
+    result: error2 + openComment + "Exception: A thrown string" + closeComment,
+    label: "thrown string run output"
+  },
+  {
+    // Run error3, throw {}
+    method: "run",
+    code: error3,
+    result: error3 + openComment + "Exception: [object Object]" + closeComment,
+    label: "thrown object run output"
+  },
+  {
+    // Run error4, document.body.appendChild(document.body)
+    method: "run",
+    code: error4,
+    result: error4 + openComment + "Exception: Node cannot be inserted " +
+            "at the specified point in the hierarchy\n@1" + closeComment,
+    label: "Alternative format error run output"
+  }];
 
-  // Display error1, throw new Error("Ouch")
-  scratchpad.setText(error1);
-  scratchpad.display();
-  is(scratchpad.getText(),
-      error1 + openComment +
-      "Exception: Ouch!\n@" + scratchpad.uniqueName + ":1" + closeComment,
-      "error display output");
-
-  // Display error2, throw "A thrown string"
-  scratchpad.setText(error2);
-  scratchpad.display();
-  is(scratchpad.getText(),
-      error2 + openComment + "Exception: A thrown string" + closeComment,
-      "thrown string display output");
-
-  // Display error3, throw {}
-  scratchpad.setText(error3);
-  scratchpad.display();
-  is(scratchpad.getText(),
-      error3 + openComment + "Exception: [object Object]" + closeComment,
-      "thrown object display output");
-
-  // Display error4, document.body.appendChild(document.body)
-  scratchpad.setText(error4);
-  scratchpad.display();
-  is(scratchpad.getText(),
-      error4 + openComment + "Exception: Node cannot be inserted " +
-      "at the specified point in the hierarchy\n@1" + closeComment,
-      "Alternative format error display output");
-
-  // Run message
-  scratchpad.setText(message);
-  scratchpad.run();
-  is(scratchpad.getText(), message, "message run output");
-
-  // Run error1, throw new Error("Ouch")
-  scratchpad.setText(error1);
-  scratchpad.run();
-  is(scratchpad.getText(),
-      error1 + openComment +
-      "Exception: Ouch!\n@" + scratchpad.uniqueName + ":1" + closeComment,
-      "error run output");
-
-  // Run error2, throw "A thrown string"
-  scratchpad.setText(error2);
-  scratchpad.run();
-  is(scratchpad.getText(),
-      error2 + openComment + "Exception: A thrown string" + closeComment,
-      "thrown string run output");
-
-  // Run error3, throw {}
-  scratchpad.setText(error3);
-  scratchpad.run();
-  is(scratchpad.getText(),
-      error3 + openComment + "Exception: [object Object]" + closeComment,
-      "thrown object run output");
-
-  // Run error4, document.body.appendChild(document.body)
-  scratchpad.setText(error4);
-  scratchpad.run();
-  is(scratchpad.getText(),
-      error4 + openComment + "Exception: Node cannot be inserted " +
-      "at the specified point in the hierarchy\n@1" + closeComment,
-      "Alternative format error run output");
-
-  finish();
+  runAsyncTests(scratchpad, tests).then(finish);
 }
