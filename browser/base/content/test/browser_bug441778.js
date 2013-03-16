369e6ca55205491c8e4f36dbb86fb385d968c921
Drew Willcoxon <adw@mozilla.com>
2013-03-16 15:12:31 -0700

    Bug 829456 - Update browser-fullZoom.js to use nsIContentPrefService2. r=mak

--- browser/base/content/test/browser_bug441778.js
+++ browser/base/content/test/browser_bug441778.js
@@ -13,19 +13,22 @@ function test() {
   const TEST_PAGE_URL = 'data:text/html,<body><iframe src=""></iframe></body>';
   const TEST_IFRAME_URL = "http://test2.example.org/";
 
-  // Prepare the test tab
-  gBrowser.selectedTab = gBrowser.addTab();
-  let testBrowser = gBrowser.selectedBrowser;
+  Task.spawn(function () {
+    // Prepare the test tab
+    let tab = gBrowser.addTab();
+    yield FullZoomHelper.selectTabAndWaitForLocationChange(tab);
 
-  testBrowser.addEventListener("load", function () {
-    testBrowser.removeEventListener("load", arguments.callee, true);
+    let testBrowser = tab.linkedBrowser;
+
+    yield FullZoomHelper.load(tab, TEST_PAGE_URL);
 
     // Change the zoom level and then save it so we can compare it to the level
     // after loading the sub-document.
-    FullZoom.enlarge();
+    yield FullZoomHelper.enlarge();
     var zoomLevel = ZoomManager.zoom;
 
     // Start the sub-document load.
+    let deferred = Promise.defer();
     executeSoon(function () {
       testBrowser.addEventListener("load", function (e) {
         testBrowser.removeEventListener("load", arguments.callee, true);
@@ -34,11 +37,10 @@ function test() {
         is(ZoomManager.zoom, zoomLevel, "zoom is retained after sub-document load");
 
         gBrowser.removeCurrentTab();
-        finish();
+        deferred.resolve();
       }, true);
       content.document.querySelector("iframe").src = TEST_IFRAME_URL;
     });
-  }, true);
-
-  content.location = TEST_PAGE_URL;
+    yield deferred.promise;
+  }).then(finish, FullZoomHelper.failAndContinue(finish));
 }
--- browser/base/content/test/browser_bug575830.js
+++ browser/base/content/test/browser_bug575830.js
@@ -13,25 +13,22 @@ function test() {
     gBrowser.removeTab(tab2);
   });
 
-  tab1 = gBrowser.addTab(TEST_IMAGE);
-  tab2 = gBrowser.addTab();
-  gBrowser.selectedTab = tab1;
+  Task.spawn(function () {
+    tab1 = gBrowser.addTab();
+    tab2 = gBrowser.addTab();
+    yield FullZoomHelper.selectTabAndWaitForLocationChange(tab1);
+    yield FullZoomHelper.load(tab1, TEST_IMAGE);
 
-  tab1.linkedBrowser.addEventListener("load", function onload() {
-    tab1.linkedBrowser.removeEventListener("load", onload, true);
     is(ZoomManager.zoom, 1, "initial zoom level for first should be 1");
 
-    FullZoom.enlarge();
+    yield FullZoomHelper.enlarge();
     let zoom = ZoomManager.zoom;
     isnot(zoom, 1, "zoom level should have changed");
 
-    gBrowser.selectedTab = tab2;
+    yield FullZoomHelper.selectTabAndWaitForLocationChange(tab2);
     is(ZoomManager.zoom, 1, "initial zoom level for second tab should be 1");
 
-    gBrowser.selectedTab = tab1;
+    yield FullZoomHelper.selectTabAndWaitForLocationChange(tab1);
     is(ZoomManager.zoom, zoom, "zoom level for first tab should not have changed");
-
-    finish();
-  }, true);
+  }).then(finish, FullZoomHelper.failAndContinue(finish));
 }
-
