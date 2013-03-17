4f5fca99bc0613173f0b4334dbeb58ff46d65275
Ms2ger <ms2ger@gmail.com>
2013-03-17 08:55:15 +0100

    Bug 845374 - Part l: Stop including nsIDocument.h in nsContentUtils.h and fix two nits; r=khuey

--- dom/quota/CheckQuotaHelper.cpp
+++ dom/quota/CheckQuotaHelper.cpp
@@ -12,6 +12,7 @@
 #include "nsIPrincipal.h"
 #include "nsIScriptObjectPrincipal.h"
 #include "nsIURI.h"
+#include "nsPIDOMWindow.h"
 
 #include "mozilla/dom/indexedDB/IndexedDatabaseManager.h"
 #include "mozilla/Services.h"
