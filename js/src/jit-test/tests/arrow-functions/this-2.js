83ae4ff56b323095d9783e8b218ae2cf7f1276f8
Jason Orendorff <jorendorff@mozilla.com>
2013-03-18 15:32:29 -0700

    Bug 848062 - Make arrow functions inherit the value of 'this' from the enclosing scope. r=bhackett.

---
+++ js/src/jit-test/tests/arrow-functions/this-2.js
@@ -0,0 +1,14 @@
+// 'this' is lexically scoped in direct eval code in arrow functions
+
+var obj = {
+    f: function (s) {
+        dis();
+        return a => eval(s);
+    }
+};
+
+var g = obj.f("this");
+assertEq(g(), obj);
+
+var obj2 = {g: g, fail: true};
+assertEq(obj2.g(), obj);
