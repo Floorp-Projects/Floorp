5125ca9fad2086969033c8170a00b6d081ab197e
William Chen <wchen@mozilla.com>
2013-03-18 06:24:53 -0700

    Bug 782211 - Part 4: Add function to remote test for permission in nsIPermissionManager. r=cjones

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -2273,6 +2273,23 @@ ContentParent::RecvCloseAlert(const nsString& aName)
 }
 
 bool
+ContentParent::RecvTestPermissionFromPrincipal(const IPC::Principal& aPrincipal,
+                                               const nsCString& aType,
+                                               uint32_t* permission)
+{
+    nsCOMPtr<nsIPermissionManager> permissionManager =
+        do_GetService(NS_PERMISSIONMANAGER_CONTRACTID);
+    NS_ENSURE_TRUE(permissionManager, false);
+
+    nsresult rv = permissionManager->TestPermissionFromPrincipal(aPrincipal,
+                                                                 aType.get(),
+                                                                 permission);
+    NS_ENSURE_SUCCESS(rv, false);
+
+    return true;
+}
+
+bool
 ContentParent::RecvSyncMessage(const nsString& aMsg,
                                const ClonedMessageData& aData,
                                InfallibleTArray<nsString>* aRetvals)
