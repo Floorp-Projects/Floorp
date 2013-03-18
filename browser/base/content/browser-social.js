07432195971a68d4ea1388c608f4063568693ad5
Shane Caraveo <scaraveo@mozilla.com>
2013-03-18 12:43:51 -0700

    bug 850077 make tooltips work in social content, r=felipc, a=gavin

--- browser/base/content/browser-social.js
+++ browser/base/content/browser-social.js
@@ -457,6 +457,7 @@ let SocialFlyout = {
     iframe.setAttribute("type", "content");
     iframe.setAttribute("class", "social-panel-frame");
     iframe.setAttribute("flex", "1");
+    iframe.setAttribute("tooltip", "aHTMLTooltip");
     iframe.setAttribute("origin", Social.provider.origin);
     panel.appendChild(iframe);
   },
@@ -897,6 +898,7 @@ var SocialToolbar = {
             "mozbrowser": "true",
             "class": "social-panel-frame",
             "id": notificationFrameId,
+            "tooltip": "aHTMLTooltip",
 
             // work around bug 793057 - by making the panel roughly the final size
             // we are more likely to have the anchor in the correct position.
