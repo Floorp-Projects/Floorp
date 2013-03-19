6c4203604c150f641aea9b46de4e81211e5e9ae8
Robert O'Callahan <robert@ocallahan.org>
2013-03-19 09:08:28 -0400

    Bug 846901 - Skip nsDisplayWrapList construction if there's only one item to wrap and it already has the right frame. r=mattwoodrow

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -2056,6 +2056,18 @@ IsRootScrollFrameActive(nsIPresShell* aPresShell)
   return sf && sf->IsScrollingActive();
 }
 
+static nsDisplayItem*
+WrapInWrapList(nsDisplayListBuilder* aBuilder,
+               nsIFrame* aFrame, nsDisplayList* aList)
+{
+  nsDisplayItem* item = aList->GetBottom();
+  if (!item || item->GetAbove() || item->GetUnderlyingFrame() != aFrame) {
+    return new (aBuilder) nsDisplayWrapList(aBuilder, aFrame, aList);
+  }
+  aList->RemoveBottom();
+  return item;
+}
+
 void
 nsIFrame::BuildDisplayListForChild(nsDisplayListBuilder*   aBuilder,
                                    nsIFrame*               aChild,
@@ -2290,7 +2302,7 @@ nsIFrame::BuildDisplayListForChild(nsDisplayListBuilder*   aBuilder,
       if (buildFixedPositionItem) {
         item = new (aBuilder) nsDisplayFixedPosition(aBuilder, child, child, &list);
       } else {
-        item = new (aBuilder) nsDisplayWrapList(aBuilder, child, &list);
+        item = WrapInWrapList(aBuilder, child, &list);
       }
       if (isSVG) {
         aLists.Content()->AppendNewToTop(item);
@@ -2313,8 +2325,7 @@ nsIFrame::BuildDisplayListForChild(nsDisplayListBuilder*   aBuilder,
     }
   } else if (!isSVG && disp->IsFloating(child)) {
     if (!list.IsEmpty()) {
-      aLists.Floats()->AppendNewToTop(new (aBuilder)
-          nsDisplayWrapList(aBuilder, child, &list));
+      aLists.Floats()->AppendNewToTop(WrapInWrapList(aBuilder, child, &list));
     }
   } else {
     aLists.Content()->AppendToTop(&list);
