dd41b4580f5c93c10e3f8bec5cd8ba529a8b7896
Trevor Saunders <trev.saunders@gmail.com>
2013-03-19 18:34:23 -0400

    bug 853682 -  remove most of the chromium gfx stuff since it isn't used we need to keep a few headers because they're included, but we don't even need to define the functions they declare. r=bsmedberg

--- ipc/chromium/src/base/gfx/platform_canvas.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// TODO(brettw) this file should be removed and the includes changed to this
-// new location.
-#include "skia/ext/platform_canvas.h"
