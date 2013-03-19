8575ddfb20768fd24fca8403aa8cc599213aa071
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2013-03-19 14:41:49 -0700

    Bug 851067 - Box non-matching argument types. r=h4writer

--- js/src/ion/IonBuilder.cpp
+++ js/src/ion/IonBuilder.cpp
@@ -3060,6 +3060,16 @@ IonBuilder::addTypeBarrier(uint32_t i, CallInfo &callinfo, types::StackTypeSet *
     if (needsBarrier) {
         MTypeBarrier *barrier = MTypeBarrier::New(ins, cloneTypeSet(calleeObs), Bailout_Normal);
         current->add(barrier);
+
+        // Non-matching types are boxed such as the MIRType does not conflict
+        // with the inferred type.
+        if (callerObs->getKnownTypeTag() != calleeObs->getKnownTypeTag() &&
+            ins->type() != MIRType_Value)
+        {
+            MBox *box = MBox::New(ins);
+            current->add(box);
+            ins = box;
+        }
     }
 
     if (i == 0)
