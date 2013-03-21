f98c8fa854b825c7249dd80100fd32780fda99b2
Tom Schuster <evilpies@gmail.com>
2013-03-21 23:23:48 +0100

    Bug 736324 - Test. r=sicking

---
+++ browser/base/content/test/browser_blob-channelname.js
@@ -0,0 +1,11 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+Cu.import("resource://gre/modules/NetUtil.jsm");
+
+function test() {
+    var file = new File(new Blob(['test'], {type: 'text/plain'}), {name: 'test-name'});
+    var url = URL.createObjectURL(file);
+    var channel = NetUtil.newChannel(url);
+
+    is(channel.contentDispositionFilename, 'test-name', "filename matches");
+}
