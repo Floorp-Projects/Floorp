54e5b34c44d981ae4c30bec0e7fe63897de5f820
David Zbarsky <dzbarsky@gmail.com>
2013-03-21 01:33:56 -0400

    [Bug 852843] SVGZoomAndPan interface object not being instantiated r=bz

--- dom/bindings/Configuration.py
+++ dom/bindings/Configuration.py
@@ -458,7 +458,7 @@ class Descriptor(DescriptorProvider):
         """
         return (self.interface.isExternal() or self.concrete or
             self.interface.getExtendedAttribute("PrefControlled") or
-            not all(m.isConst() for m in self.interface.members))
+            self.interface.hasInterfacePrototypeObject())
 
 # Some utility methods
 def getTypesFromDescriptor(descriptor):
