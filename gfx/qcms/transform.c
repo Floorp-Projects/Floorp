b48577984b45029dd84db1238cb150282d31759e
Phil Ringnalda <philringnalda@gmail.com>
2013-03-20 21:33:03 -0700

    Back out d77773a8abf9 and eca6b3ea3e8d (bug 853169) for Linux xpcshell crashes CLOSED TREE

--- gfx/qcms/transform.c
+++ gfx/qcms/transform.c
@@ -886,17 +886,9 @@ static void qcms_transform_data_rgb_out_linear(qcms_transform *transform, unsign
 }
 #endif
 
-/*
- * If users create and destroy objects on different threads, even if the same
- * objects aren't used on different threads at the same time, we can still run
- * in to trouble with refcounts if they aren't atomic.
- *
- * This can lead to us prematurely deleting the precache if threads get unlucky
- * and write the wrong value to the ref count.
- */
 static struct precache_output *precache_reference(struct precache_output *p)
 {
-	qcms_atomic_increment(p->ref_count);
+	p->ref_count++;
 	return p;
 }
 
@@ -910,7 +902,7 @@ static struct precache_output *precache_create()
 
 void precache_release(struct precache_output *p)
 {
-	if (qcms_atomic_decrement(p->ref_count) == 0) {
+	if (--p->ref_count == 0) {
 		free(p);
 	}
 }
