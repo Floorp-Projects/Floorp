6180010ee8a9b65a991a9a6d1d9e77cf27f61d57
Paul Adenot <paul@paul.cx>
2013-03-21 17:45:53 +0100

    Bug 849918 - Initial support for PannerNode's 3D positional audio (equalpower panning model). r=ehsan,roc

---
+++ content/media/webaudio/ThreeDPoint.cpp
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Other similar methods can be added if needed.
+ */
+
+#include "ThreeDPoint.h"
+
+namespace mozilla {
+
+namespace dom {
+
+ThreeDPoint operator-(const ThreeDPoint& lhs, const ThreeDPoint& rhs)
+{
+  return ThreeDPoint(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+}
+
+ThreeDPoint operator*(const ThreeDPoint& lhs, const ThreeDPoint& rhs)
+{
+  return ThreeDPoint(lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z);
+}
+
+ThreeDPoint operator*(const ThreeDPoint& lhs, const double rhs)
+{
+  return ThreeDPoint(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs);
+}
+
+bool operator==(const ThreeDPoint& lhs, const ThreeDPoint& rhs)
+{
+  return lhs.x == rhs.x &&
+         lhs.y == rhs.y &&
+         lhs.z == rhs.z;
+}
+
+}
+}
