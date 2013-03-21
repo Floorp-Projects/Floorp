e8206f0a2b19e54280a24d38744a42dbc4ee7c61
Yiming Yang <yiyang@mozilla.com>
2013-03-21 12:31:27 -0700

    Bug 848489 - send 'contextmenu' event if we are long pressing, r=mdas

--- testing/marionette/marionette-listener.js
+++ testing/marionette/marionette-listener.js
@@ -965,6 +965,14 @@ function actions(finger, touchId, command_id, i){
       touch = createATouch(el, corx, cory, touchId);
       lastTouch = touch;
       emitTouchEvent('touchstart', touch);
+      // check if it's a long press
+      // standard waiting time to fire contextmenu
+      let standard = Services.prefs.getIntPref("ui.click_hold_context_menus.delay");
+      // long press only happens when wait follows press
+      if (finger[i][0] == 'wait' && finger[i][1] != null && finger[i][1]*1000 >= standard) {
+        finger[i][1] = finger[i][1] - standard/1000;
+        finger.splice(i, 0, ['wait', standard/1000], ['longPress']);
+      }
       actions(finger,touchId, command_id, i);
       break;
     case 'release':
@@ -1025,6 +1033,14 @@ function actions(finger, touchId, command_id, i){
       touch = lastTouch;
       emitTouchEvent('touchcancel', touch);
       lastTouch = null;
+      actions(finger, touchId, command_id, i);
+      break;
+    case 'longPress':
+      let event = curWindow.document.createEvent('HTMLEvents');
+      event.initEvent('contextmenu',
+                      true,
+                      true);
+      lastTouch.target.dispatchEvent(event);
       actions(finger, touchId, command_id, i);
       break;
   }
