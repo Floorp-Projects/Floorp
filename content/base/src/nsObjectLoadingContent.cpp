b906b014e8e177b3e53b43df7914b8b994162832
John Schoenick <jschoenick@mozilla.com>
2013-03-21 18:02:23 -0700

    Bug 738396 - Only pass canonicalized codebase to Java. r=bsmedberg, sr=jst, a=lsblakk

--- content/base/src/nsObjectLoadingContent.cpp
+++ content/base/src/nsObjectLoadingContent.cpp
@@ -1067,6 +1067,13 @@ nsObjectLoadingContent::GetContentTypeForMIMEType(const nsACString& aMIMEType,
   return NS_OK;
 }
 
+NS_IMETHODIMP
+nsObjectLoadingContent::GetBaseURI(nsIURI **aResult)
+{
+  NS_IF_ADDREF(*aResult = mBaseURI);
+  return NS_OK;
+}
+
 // nsIInterfaceRequestor
 // We use a shim class to implement this so that JS consumers still
 // see an interface requestor even though WebIDL bindings don't expose
