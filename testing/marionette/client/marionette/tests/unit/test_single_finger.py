5ee25702fa32744710e1f1b3f0d07ea98ef5a1f3
Yiming Yang <yiyang@mozilla.com>
2013-03-22 11:29:02 -0700

    Bug 851523 - Allow users to execute action chain in segments, r=mdas

--- testing/marionette/client/marionette/tests/unit/test_single_finger.py
+++ testing/marionette/client/marionette/tests/unit/test_single_finger.py
@@ -5,6 +5,7 @@
 import time
 from marionette_test import MarionetteTestCase
 from marionette import Actions
+from errors import NoSuchElementException
 
 class testSingleFinger(MarionetteTestCase):
     def test_wait(self):
@@ -39,3 +40,22 @@ class testSingleFinger(MarionetteTestCase):
         time.sleep(15)
         self.assertEqual("Move", self.marionette.execute_script("return document.getElementById('mozLink').innerHTML;"))
         self.assertEqual("End", self.marionette.execute_script("return document.getElementById('mozLinkPos').innerHTML;"))
+
+    def test_chain(self):
+        testTouch = self.marionette.absolute_url("testAction.html")
+        self.marionette.navigate(testTouch)
+        action = Actions(self.marionette)
+        button1 = self.marionette.find_element("id", "mozLinkCopy2")
+        action.press(button1).perform()
+        button2 = self.marionette.find_element("id", "delayed")
+        time.sleep(5)
+        action.move(button2).release().perform()
+        time.sleep(15)
+        self.assertEqual("End", self.marionette.execute_script("return document.getElementById('delayed').innerHTML;"))
+
+    def test_no_press(self):
+        testTouch = self.marionette.absolute_url("testAction.html")
+        self.marionette.navigate(testTouch)
+        action = Actions(self.marionette)
+        action.release()
+        self.assertRaises(NoSuchElementException, action.perform)
