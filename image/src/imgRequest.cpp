98c0a6743c7982aac2e0b3732926fead4a87f689
Mina Almasry <almasry.mina@gmail.com>
2013-03-25 17:53:54 -0400

    Bug 582783 - Move windows.h include and undef LoadImage to gfxWindowsSurface.h. r=joe

--- image/src/imgRequest.cpp
+++ image/src/imgRequest.cpp
@@ -7,14 +7,6 @@
 #include "imgRequest.h"
 #include "ImageLogging.h"
 
-/* We end up pulling in windows.h because we eventually hit gfxWindowsSurface;
- * windows.h defines LoadImage, so we have to #undef it or imgLoader::LoadImage
- * gets changed.
- * This #undef needs to be in multiple places because we don't always pull
- * headers in in the same order.
- */
-#undef LoadImage
-
 #include "imgLoader.h"
 #include "imgRequestProxy.h"
 #include "imgStatusTracker.h"
