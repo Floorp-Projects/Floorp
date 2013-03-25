04902d0d9bc147895f3f4a1187af67c5d3822a03
Rob Wood <rwood@mozilla.com>
2013-03-25 16:38:18 -0400

    Bug 825722 - Fix intermittent failure by removing SMS timestamp comparison; r=jgriffin

--- dom/mobilemessage/tests/marionette/test_incoming.js
+++ dom/mobilemessage/tests/marionette/test_incoming.js
@@ -11,12 +11,11 @@ const RECEIVER = "15555215554"; // the emulator's number
 
 let sms = window.navigator.mozSms;
 let body = "Hello SMS world!";
-let now = Date.now();
 
 let completed = false;
 runEmulatorCmd("sms send " + SENDER + " " + body, function(result) {
   log("Sent fake SMS: " + result);
-  is(result[0], "OK");
+  is(result[0], "OK", "Emulator command result");
   completed = true;
 });
 
@@ -24,17 +23,15 @@ sms.onreceived = function onreceived(event) {
   log("Received an SMS!");
 
   let message = event.message;
-  ok(message instanceof MozSmsMessage);
+  ok(message instanceof MozSmsMessage, "Message is instanceof MozSmsMessage");
 
-  is(message.delivery, "received");
-  is(message.deliveryStatus, "success");
-  is(message.sender, SENDER);
-  is(message.receiver, RECEIVER);
-  is(message.body, body);
-  is(message.messageClass, "normal");
-  ok(message.timestamp instanceof Date);
-  // SMSC timestamp is in seconds.
-  ok(Math.floor(message.timestamp.getTime() / 1000) >= Math.floor(now / 1000));
+  is(message.delivery, "received", "Message delivery");
+  is(message.deliveryStatus, "success", "Delivery status");
+  is(message.sender, SENDER, "Message sender");
+  is(message.receiver, RECEIVER, "Message receiver");
+  is(message.body, body, "Message body");
+  is(message.messageClass, "normal", "Message class");
+  ok(message.timestamp instanceof Date, "Message timestamp is a date");
 
   cleanUp();
 };
