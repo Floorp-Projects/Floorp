d7a8cbf3177059dcb0173c749ef71fd1cdf3134e
Luke Wagner <luke@mozilla.com>
2013-03-25 15:12:44 -0700

    Bug 854448 - Fix debug-only null-deref in JSFunction::name (r=njn)

--- js/src/jsfun.h
+++ js/src/jsfun.h
@@ -165,7 +165,7 @@ class JSFunction : public JSObject
     }
 
     JSAtom *atom() const { return hasGuessedAtom() ? NULL : atom_.get(); }
-    js::PropertyName *name() const { return hasGuessedAtom() ? NULL : atom_->asPropertyName(); }
+    js::PropertyName *name() const { return hasGuessedAtom() || !atom_ ? NULL : atom_->asPropertyName(); }
     inline void initAtom(JSAtom *atom);
     JSAtom *displayAtom() const { return atom_; }
 
