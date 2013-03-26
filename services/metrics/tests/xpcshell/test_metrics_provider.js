c7f533212300f580b1b1a2ef31c367fa32e3bb9d
Richard Newman <rnewman@mozilla.com>
2013-03-25 18:46:22 -0700

    Bug 841554 - Part 2: alter search counts provider to record all of Mozilla's partner engines in each locale. r=gps

--- services/metrics/tests/xpcshell/test_metrics_provider.js
+++ services/metrics/tests/xpcshell/test_metrics_provider.js
@@ -273,5 +273,21 @@ add_task(function test_serialize_json_default() {
   do_check_eq(formatted["daily-last-numeric"], 5);
   do_check_eq(formatted["daily-last-text"], "orange");
 
+  // Now let's turn off a field so that it's present in the DB
+  // but not present in the output.
+  let called = false;
+  let excluded = "daily-last-numeric";
+  Object.defineProperty(m, "shouldIncludeField", {
+    value: function fakeShouldIncludeField(field) {
+      called = true;
+      return field != excluded;
+    },
+  });
+
+  let limited = serializer.daily(data.days.getDay(yesterday));
+  do_check_true(called);
+  do_check_false(excluded in limited);
+  do_check_eq(formatted["daily-last-text"], "orange");
+
   yield provider.storage.close();
 });
