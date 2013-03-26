61e9072a9e02205712383544fc2f8050bd9b9676
Benoit Girard <b56girard@gmail.com>
2013-03-26 12:34:27 -0400

    Bug 820048 - Add microsecond profiling. r=ehsan

--- tools/profiler/nsProfiler.cpp
+++ tools/profiler/nsProfiler.cpp
@@ -70,7 +70,7 @@ nsProfiler::Observe(nsISupports *aSubject,
 }
 
 NS_IMETHODIMP
-nsProfiler::StartProfiler(uint32_t aEntries, uint32_t aInterval,
+nsProfiler::StartProfiler(uint32_t aEntries, double aInterval,
                           const char** aFeatures, uint32_t aFeatureCount,
                           const char** aThreadNameFilters, uint32_t aFilterCount)
 {
