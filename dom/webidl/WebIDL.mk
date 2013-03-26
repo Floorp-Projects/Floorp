cba06257d90ec23ece49f00d1678a5cb7bb82213
David Zbarsky <dzbarsky@gmail.com>
2013-03-26 19:43:33 -0400

    Bug 847120: Convert SVGFEMorphologyElement to WebIDL r=Ms2ger

--- dom/webidl/WebIDL.mk
+++ dom/webidl/WebIDL.mk
@@ -206,6 +206,7 @@ webidl_files = \
   SVGFEImageElement.webidl \
   SVGFEMergeElement.webidl \
   SVGFEMergeNodeElement.webidl \
+  SVGFEMorphologyElement.webidl \
   SVGFEOffsetElement.webidl \
   SVGFEPointLightElement.webidl \
   SVGFESpecularLightingElement.webidl \
