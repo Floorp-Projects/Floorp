055e6e77c37d5b2eb4b75d2cbaaee461ecc387e3
Robert O'Callahan <robert@ocallahan.org>
2013-03-27 14:32:51 +1300

    Bug 834835. Part 1: Add initial AudioStreamTrack/VideoStreamTrack/MediaStreamTrack interfaces and implementations. r=jesup

---
+++ content/media/VideoStreamTrack.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-*/
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef VIDEOSTREAMTRACK_H_
+#define VIDEOSTREAMTRACK_H_
+
+#include "MediaStreamTrack.h"
+
+namespace mozilla {
+namespace dom {
+
+class VideoStreamTrack : public MediaStreamTrack {
+public:
+  VideoStreamTrack(DOMMediaStream* aStream, TrackID aTrackID)
+    : MediaStreamTrack(aStream, aTrackID) {}
+
+  virtual JSObject* WrapObject(JSContext* aCx, JSObject* aScope);
+
+  virtual VideoStreamTrack* AsVideoStreamTrack() { return this; }
+
+  // WebIDL
+  virtual void GetKind(nsAString& aKind) { aKind.AssignLiteral("video"); }
+};
+
+}
+}
+
+#endif /* VIDEOSTREAMTRACK_H_ */
