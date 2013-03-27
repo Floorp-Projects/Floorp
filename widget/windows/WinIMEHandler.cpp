d0683002217933a8fa28c4285dac0a0185d7cb31
Masayuki Nakano <masayuki@d-toybox.com>
2013-03-27 09:04:02 +0900

    Bug 848672 Redesign focus handling of nsTextStore r=jimm

--- widget/windows/WinIMEHandler.cpp
+++ widget/windows/WinIMEHandler.cpp
@@ -253,7 +253,9 @@ IMEHandler::OnDestroyWindow(nsWindow* aWindow)
 
 // static
 void
-IMEHandler::SetInputContext(nsWindow* aWindow, InputContext& aInputContext)
+IMEHandler::SetInputContext(nsWindow* aWindow,
+                            InputContext& aInputContext,
+                            const InputContextAction& aAction)
 {
   // FYI: If there is no composition, this call will do nothing.
   NotifyIME(aWindow, REQUEST_TO_COMMIT_COMPOSITION);
@@ -272,12 +274,14 @@ IMEHandler::SetInputContext(nsWindow* aWindow, InputContext& aInputContext)
 #ifdef NS_ENABLE_TSF
   // Note that even while a plugin has focus, we need to notify TSF of that.
   if (sIsInTSFMode) {
-    nsTextStore::SetInputContext(aInputContext);
+    nsTextStore::SetInputContext(aWindow, aInputContext, aAction);
     if (IsTSFAvailable()) {
       aInputContext.mNativeIMEContext = nsTextStore::GetTextStore();
+      if (adjustOpenState) {
+        nsTextStore::SetIMEOpenState(open);
+      }
+      return;
     }
-    // Currently, nsTextStore doesn't set focus to keyboard disabled document.
-    // Therefore, we still need to perform the following legacy code.
   }
 #endif // #ifdef NS_ENABLE_TSF
 
@@ -298,12 +302,6 @@ IMEHandler::SetInputContext(nsWindow* aWindow, InputContext& aInputContext)
   }
 
   if (adjustOpenState) {
-#ifdef NS_ENABLE_TSF
-    if (IsTSFAvailable()) {
-      nsTextStore::SetIMEOpenState(open);
-      return;
-    }
-#endif // #ifdef NS_ENABLE_TSF
     IMEContext.SetOpenState(open);
   }
 }
@@ -317,7 +315,9 @@ IMEHandler::InitInputContext(nsWindow* aWindow, InputContext& aInputContext)
 
 #ifdef NS_ENABLE_TSF
   if (sIsInTSFMode) {
-    nsTextStore::SetInputContext(aInputContext);
+    nsTextStore::SetInputContext(aWindow, aInputContext,
+      InputContextAction(InputContextAction::CAUSE_UNKNOWN,
+                         InputContextAction::GOT_FOCUS));
     aInputContext.mNativeIMEContext = nsTextStore::GetTextStore();
     MOZ_ASSERT(aInputContext.mNativeIMEContext);
     return;
