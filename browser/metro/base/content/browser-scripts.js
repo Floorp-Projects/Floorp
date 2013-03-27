c53aeea616ac879ff1cd9cf57d077660d7247284
Matt Brubeck <mbrubeck@mozilla.com>
2013-03-27 11:09:09 -0700

    Bug 808770 - Revert a change that accidentally added the same lines twice

--- browser/metro/base/content/browser-scripts.js
+++ browser/metro/base/content/browser-scripts.js
@@ -24,11 +24,6 @@ XPCOMUtils.defineLazyModuleGetter(this, "PdfJs",
 
 XPCOMUtils.defineLazyModuleGetter(this, "DownloadUtils",
                                   "resource://gre/modules/DownloadUtils.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "Promise",
-                                  "resource://gre/modules/commonjs/sdk/core/promise.js");
-
-XPCOMUtils.defineLazyModuleGetter(this, "Task",
-                                  "resource://gre/modules/Task.jsm");
 
 XPCOMUtils.defineLazyModuleGetter(this, "NewTabUtils",
                                   "resource://gre/modules/NewTabUtils.jsm");
