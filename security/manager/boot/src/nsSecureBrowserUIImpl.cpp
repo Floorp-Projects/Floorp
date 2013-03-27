0983dd84b615cad7f366ada9fd9be6fde6276fd6
Phil Ringnalda <philringnalda@gmail.com>
2013-03-26 20:18:30 -0700

    Back out ee27d1d8b302 (bug 836951) for depending on a patch that's being backed out CLOSED TREE

--- security/manager/boot/src/nsSecureBrowserUIImpl.cpp
+++ security/manager/boot/src/nsSecureBrowserUIImpl.cpp
@@ -19,7 +19,6 @@
 #include "nsIObserverService.h"
 #include "nsCURILoader.h"
 #include "nsIDocShell.h"
-#include "nsIDocShellTreeItem.h"
 #include "nsIDocument.h"
 #include "nsIPrincipal.h"
 #include "nsIDOMElement.h"
@@ -287,21 +286,12 @@ nsSecureBrowserUIImpl::MapInternalToExternalState(uint32_t* aState, lockIconStat
   if (ev && (*aState & STATE_IS_SECURE))
     *aState |= nsIWebProgressListener::STATE_IDENTITY_EV_TOPLEVEL;
 
-  nsCOMPtr<nsIDocShell> docShell = do_QueryReferent(mDocShell);
-  if (!docShell)
+  nsCOMPtr<nsPIDOMWindow> piwin = do_QueryReferent(mWindow);
+  if (!piwin)
     return NS_OK;
 
-  int32_t docShellType;
-  // For content docShell's, the mixed content security state is set on the root docShell.
-  if (NS_SUCCEEDED(docShell->GetItemType(&docShellType)) && docShellType == nsIDocShellTreeItem::typeContent) {
-    nsCOMPtr<nsIDocShellTreeItem> docShellTreeItem(do_QueryInterface(docShell));
-    nsCOMPtr<nsIDocShellTreeItem> sameTypeRoot;
-    docShellTreeItem->GetSameTypeRootTreeItem(getter_AddRefs(sameTypeRoot));
-    NS_ASSERTION(sameTypeRoot, "No document shell root tree item from document shell tree item!");
-    docShell = do_QueryInterface(sameTypeRoot);
-    if (!docShell)
-      return NS_OK;
-  }
+  nsIDocShell* docShell = piwin->GetDocShell();
+  MOZ_ASSERT(docShell);
 
   // Has a Mixed Content Load initiated in nsMixedContentBlocker?
   // If so, the state should be broken; overriding the previous state
@@ -327,14 +317,6 @@ nsSecureBrowserUIImpl::MapInternalToExternalState(uint32_t* aState, lockIconStat
     *aState |= nsIWebProgressListener::STATE_BLOCKED_MIXED_DISPLAY_CONTENT;
 
   return NS_OK;
-}
-
-NS_IMETHODIMP
-nsSecureBrowserUIImpl::SetDocShell(nsIDocShell *aDocShell)
-{
-  nsresult rv;
-  mDocShell = do_GetWeakReference(aDocShell, &rv);
-  return rv;
 }
 
 NS_IMETHODIMP
