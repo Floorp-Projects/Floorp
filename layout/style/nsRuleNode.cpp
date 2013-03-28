f350367b6b6e977c304ea8037ca96383a25e6b37
Daniel Holbert <dholbert@cs.stanford.edu>
2013-03-27 23:33:19 -0700

    Bug 848539 part 5: Back out 6bb37077d615, bug 763689 part 2 (style-system handling for min-width:auto). r=dbaron

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -6493,7 +6493,11 @@ nsRuleNode::ComputePositionData(void* aStartStruct,
            SETCOORD_LPOH | SETCOORD_INITIAL_NONE | SETCOORD_STORE_CALC,
            aContext, mPresContext, canStoreInRuleTree);
 
-  // Make 'auto' values for min-height compute to 0
+
+  // Handle 'auto' values for min-width / min-height
+  if (pos->mMinWidth.GetUnit() == eStyleUnit_Auto) {
+    pos->mMinWidth.SetCoordValue(0);
+  }
   if (pos->mMinHeight.GetUnit() == eStyleUnit_Auto) {
     pos->mMinHeight.SetCoordValue(0);
   }
