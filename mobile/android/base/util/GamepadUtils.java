bb07b5d4dce40aa93501bdb7706720540b256bbe
Kartikaya Gupta <kgupta@mozilla.com>
2013-03-28 11:35:04 -0400

    Bug 855016 - Allow gamepad action button to trigger the row items on about:home. r=sriram

--- mobile/android/base/util/GamepadUtils.java
+++ mobile/android/base/util/GamepadUtils.java
@@ -8,8 +8,11 @@ package org.mozilla.gecko.util;
 import android.os.Build;
 import android.view.InputDevice;
 import android.view.KeyEvent;
+import android.view.View;
 
 public final class GamepadUtils {
+    private static View.OnKeyListener sClickDispatcher;
+
     private GamepadUtils() {
     }
 
@@ -26,5 +29,20 @@ public final class GamepadUtils {
 
     public static boolean isBackKey(KeyEvent event) {
         return (isGamepadKey(event) && (event.getKeyCode() == KeyEvent.KEYCODE_BUTTON_B));
+    }
+
+    public static View.OnKeyListener getClickDispatcher() {
+        if (sClickDispatcher == null) {
+            sClickDispatcher = new View.OnKeyListener() {
+                @Override
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
+                    if (event.getAction() == KeyEvent.ACTION_DOWN && isActionKey(event)) {
+                        return v.performClick();
+                    }
+                    return false;
+                }
+            };
+        }
+        return sClickDispatcher;
     }
 }
