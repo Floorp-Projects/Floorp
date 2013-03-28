4c3a35bb4fa932058af24aa28119fb3e4ce335bf
Ryan VanderMeulen <ryanvm@gmail.com>
2013-03-28 16:44:37 -0400

    Backed out changesets 1f5713b14a2a, c30a353ecf23, and f4ef406c1256 (bug 836486) for Android and B2G bustage on a CLOSED TREE.

--- js/src/ion/arm/Architecture-arm.h
+++ js/src/ion/arm/Architecture-arm.h
@@ -16,7 +16,7 @@
 namespace js {
 namespace ion {
 
-static const uint32_t STACK_SLOT_SIZE       = 4;
+static const ptrdiff_t STACK_SLOT_SIZE       = 4;
 static const uint32_t DOUBLE_STACK_ALIGNMENT = 2;
 
 // In bytes: slots needed for potential memory->memory move spills.
@@ -209,7 +209,6 @@ class FloatRegisters
 
 bool hasMOVWT();
 bool hasVFPv3();
-bool hasVFP();
 bool has16DP();
 
 } // namespace ion
