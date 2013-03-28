0f0fef30424acdfa65d363a281f50ed486bfdc9f
Julian Seward <jseward@acm.org>
2013-03-28 11:30:34 +0100

    Bug 855012 - local_debug_info_symbolizer.cc,h and shim_mac_dump_syms.mm,h have no license information (& probably need MPL boilerplate), r=dholbert

--- tools/profiler/shim_mac_dump_syms.h
+++ tools/profiler/shim_mac_dump_syms.h
@@ -1,3 +1,7 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // Read debug info from |obj_file| and park it in a Module, returned
 // via |module|.  Caller owns the Module and is responsible for
