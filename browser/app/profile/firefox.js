1114475e6258d79d6948bed71f6cfc6d75de57d6
Masayuki Nakano <masayuki@d-toybox.com>
2013-03-31 13:17:38 +0900

    Bug 855916 Add a debug pref which allows moz gesture events through content r=jimm

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1232,3 +1232,6 @@ pref("plain_text.wrap_long_lines", true);
 pref("media.webaudio.enabled", true);
 #endif
 
+// If this turns true, Moz*Gesture events are not called stopPropagation()
+// before content.
+pref("dom.debug.propagate_gesture_events_through_content", false);
