ddba52e8747cae9ab1f89417c56f12c04f870f70
EKR <ekr@rtfm.com>
2013-04-01 12:34:36 -0700

    Bug 856848 - Fix thread crash r=ehugg Bug 856848 - Fix thread crash r=ehugg,ekr

--- media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.cpp
+++ media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.cpp
@@ -277,7 +277,7 @@ PeerConnectionImpl::PeerConnectionImpl()
   , mWindow(NULL)
   , mIdentity(NULL)
   , mSTSThread(NULL)
-  , mMedia(new PeerConnectionMedia(this))
+  , mMedia(NULL)
   , mNumAudioStreams(0)
   , mNumVideoStreams(0)
   , mHaveDataStream(false) {
@@ -525,6 +525,8 @@ PeerConnectionImpl::Initialize(IPeerConnectionObserver* aObserver,
     CSFLogError(logTag, "%s: Couldn't Create Call Object", __FUNCTION__);
     return NS_ERROR_FAILURE;
   }
+
+  mMedia = new PeerConnectionMedia(this);
 
   // Connect ICE slots.
   mMedia->SignalIceGatheringCompleted.connect(this, &PeerConnectionImpl::IceGatheringCompleted);
