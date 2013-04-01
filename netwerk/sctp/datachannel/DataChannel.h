17568f8e943cf206390210523a41b6799e464519
Randell Jesup <rjesup@jesup.org>
2013-03-31 21:09:26 -0400

    Bug 855623: Update (interdiffs) in response to review r=tuexen,ehugg

--- netwerk/sctp/datachannel/DataChannel.h
+++ netwerk/sctp/datachannel/DataChannel.h
@@ -387,7 +387,7 @@ public:
 
   uint16_t GetType() { return mPrPolicy; }
 
-  bool GetOrdered() { return !(mFlags & DATA_CHANNEL_FLAG_OUT_OF_ORDER_ALLOWED); }
+  bool GetOrdered() { return !(mFlags & DATA_CHANNEL_FLAGS_OUT_OF_ORDER_ALLOWED); }
 
   // Amount of data buffered to send
   uint32_t GetBufferedAmount();
