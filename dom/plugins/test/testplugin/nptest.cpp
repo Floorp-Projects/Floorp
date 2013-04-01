b5976bafabcc6bffb6cbcf72989844af594b008f
John Schoenick <jschoenick@mozilla.com>
2013-04-01 13:04:48 -0700

    Bug 971279 - Add nptest java plugin. r=bsmedberg

--- dom/plugins/test/testplugin/nptest.cpp
+++ dom/plugins/test/testplugin/nptest.cpp
@@ -52,6 +52,7 @@
 #include <float.h>
 #include <windows.h>
 #define getpid _getpid
+#define strcasecmp _stricmp
 #else
 #include <unistd.h>
 #include <pthread.h>
@@ -139,6 +140,7 @@ static bool setPluginWantsAllStreams(NPObject* npobj, const NPVariant* args, uin
 static bool crashPlugin(NPObject* npobj, const NPVariant* args, uint32_t argCount, NPVariant* result);
 static bool crashOnDestroy(NPObject* npobj, const NPVariant* args, uint32_t argCount, NPVariant* result);
 static bool getObjectValue(NPObject* npobj, const NPVariant* args, uint32_t argCount, NPVariant* result);
+static bool getJavaCodebase(NPObject* npobj, const NPVariant* args, uint32_t argCount, NPVariant* result);
 static bool checkObjectValue(NPObject* npobj, const NPVariant* args, uint32_t argCount, NPVariant* result);
 static bool enableFPExceptions(NPObject* npobj, const NPVariant* args, uint32_t argCount, NPVariant* result);
 static bool setCookie(NPObject* npobj, const NPVariant* args, uint32_t argCount, NPVariant* result);
@@ -202,6 +204,7 @@ static const NPUTF8* sPluginMethodIdentifierNames[] = {
   "crash",
   "crashOnDestroy",
   "getObjectValue",
+  "getJavaCodebase",
   "checkObjectValue",
   "enableFPExceptions",
   "setCookie",
@@ -266,6 +269,7 @@ static const ScriptableFunction sPluginMethodFunctions[] = {
   crashPlugin,
   crashOnDestroy,
   getObjectValue,
+  getJavaCodebase,
   checkObjectValue,
   enableFPExceptions,
   setCookie,
@@ -958,6 +962,11 @@ NPP_New(NPMIMEType pluginType, NPP instance, uint16_t mode, int16_t argc, char* 
     }
     if (strcmp(argn[i], "bugmode") == 0) {
       instanceData->bugMode = atoi(argv[i]);
+    }
+    // Try to emulate java's codebase handling: Use the last seen codebase
+    // value, regardless of whether it is in attributes or params.
+    if (strcasecmp(argn[i], "codebase") == 0) {
+      instanceData->javaCodebase = argv[i];
     }
   }
 
@@ -2888,6 +2897,18 @@ static const NPClass kTestSharedNPClass = {
   NP_CLASS_STRUCT_VERSION,
   // Everything else is nullptr
 };
+
+static bool getJavaCodebase(NPObject* npobj, const NPVariant* args, uint32_t argCount, NPVariant* result)
+{
+  if (argCount != 0) {
+    return false;
+  }
+
+  NPP npp = static_cast<TestNPObject*>(npobj)->npp;
+  InstanceData* id = static_cast<InstanceData*>(npp->pdata);
+  STRINGZ_TO_NPVARIANT(NPN_StrDup(id->javaCodebase.c_str()), *result);
+  return true;
+}
 
 static bool getObjectValue(NPObject* npobj, const NPVariant* args, uint32_t argCount, NPVariant* result)
 {
