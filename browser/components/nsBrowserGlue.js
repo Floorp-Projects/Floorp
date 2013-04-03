bf98708e78963fe1169c2427bb34ccda7c3e34e9
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2013-04-03 10:22:05 +0200

    Bug 854583 - Use 'pointer' instead of 'cursor' for mouse lock, r=dolske

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -1817,7 +1817,7 @@ ContentPermissionPrompt.prototype = {
 
     let originString = requestingURI.schemeIs("file") ? requestingURI.path : requestingURI.host;
     let message = browserBundle.formatStringFromName(autoAllow ?
-                                  "pointerLock.autoLock.title" : "pointerLock.title",
+                                  "pointerLock.autoLock.title2" : "pointerLock.title2",
                                   [originString], 1);
     // If this is an autoAllow info prompt, offer no actions.
     // _showPrompt() will allow the request when it's dismissed.
@@ -1825,7 +1825,7 @@ ContentPermissionPrompt.prototype = {
     if (!autoAllow) {
       actions = [
         {
-          stringId: "pointerLock.allow",
+          stringId: "pointerLock.allow2",
           action: null,
           expireType: null,
           callback: function() {},
