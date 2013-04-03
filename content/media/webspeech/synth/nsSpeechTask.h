0ef6c563a4f8cea374e3a972aa14b7844a192392
Eitan Isaacson <eitan@monotonous.org>
2013-04-03 15:13:17 -0700

    Bug 525444 - (Part 2/3) Added speech service API. r=smaug

---
+++ content/media/webspeech/synth/nsSpeechTask.h
@@ -0,0 +1,88 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "MediaStreamGraph.h"
+#include "SpeechSynthesisUtterance.h"
+#include "nsISpeechService.h"
+
+namespace mozilla {
+namespace dom {
+
+class SpeechSynthesisUtterance;
+class SpeechSynthesis;
+class SynthStreamListener;
+
+class nsSpeechTask : public nsISpeechTask
+{
+  friend class SynthStreamListener;
+
+public:
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_CLASS_AMBIGUOUS(nsSpeechTask, nsISpeechTask)
+
+  NS_DECL_NSISPEECHTASK
+
+  nsSpeechTask(SpeechSynthesisUtterance* aUtterance);
+  nsSpeechTask(float aVolume, const nsAString& aText);
+
+  virtual ~nsSpeechTask();
+
+  virtual void Pause();
+
+  virtual void Resume();
+
+  virtual void Cancel();
+
+  float GetCurrentTime();
+
+  uint32_t GetCurrentCharOffset();
+
+  void SetSpeechSynthesis(SpeechSynthesis* aSpeechSynthesis);
+
+  void SetIndirectAudio(bool aIndirectAudio) { mIndirectAudio = aIndirectAudio; }
+
+protected:
+  virtual nsresult DispatchStartImpl();
+
+  virtual nsresult DispatchEndImpl(float aElapsedTime, uint32_t aCharIndex);
+
+  virtual nsresult DispatchPauseImpl(float aElapsedTime, uint32_t aCharIndex);
+
+  virtual nsresult DispatchResumeImpl(float aElapsedTime, uint32_t aCharIndex);
+
+  virtual nsresult DispatchErrorImpl(float aElapsedTime, uint32_t aCharIndex);
+
+  virtual nsresult DispatchBoundaryImpl(const nsAString& aName,
+                                        float aElapsedTime,
+                                        uint32_t aCharIndex);
+
+  virtual nsresult DispatchMarkImpl(const nsAString& aName,
+                                    float aElapsedTime, uint32_t aCharIndex);
+
+  nsRefPtr<SpeechSynthesisUtterance> mUtterance;
+
+  float mVolume;
+
+  nsString mText;
+
+private:
+  void End();
+
+  nsRefPtr<SourceMediaStream> mStream;
+
+  nsCOMPtr<nsISpeechTaskCallback> mCallback;
+
+  uint32_t mChannels;
+
+  nsRefPtr<SpeechSynthesis> mSpeechSynthesis;
+
+  bool mIndirectAudio;
+};
+
+} // namespace dom
+} // namespace mozilla
