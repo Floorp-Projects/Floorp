ba85fa63ae914639a9d120512d7b03df044e7add
Kyle Huey <khuey@kylehuey.com>
2013-04-03 09:49:17 -0700

    Bug 855804: Add hashtable helpers for cycle collection. r=bz

--- xpcom/glue/nsRefPtrHashtable.h
+++ xpcom/glue/nsRefPtrHashtable.h
@@ -43,6 +43,26 @@ public:
   RefPtr* GetWeak(KeyType aKey, bool* aFound = nullptr) const;
 };
 
+template <typename K, typename T>
+inline void
+ImplCycleCollectionUnlink(nsRefPtrHashtable<K, T>& aField)
+{
+  aField.Clear();
+}
+
+template <typename K, typename T>
+inline void
+ImplCycleCollectionTraverse(nsCycleCollectionTraversalCallback& aCallback,
+                            nsRefPtrHashtable<K, T>& aField,
+                            const char* aName,
+                            uint32_t aFlags = 0)
+{
+  nsBaseHashtableCCTraversalData userData(aCallback, aName, aFlags);
+
+  aField.EnumerateRead(ImplCycleCollectionTraverse_EnumFunc<typename K::KeyType,T*>,
+                       &userData);
+}
+
 /**
  * Thread-safe version of nsRefPtrHashtable
  * @param KeyClass a wrapper-class for the hashtable key, see nsHashKeys.h
