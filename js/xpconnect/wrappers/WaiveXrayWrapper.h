a332f0164aaa779b36971312a14aa81130094b55
Bobby Holley <bobbyholley@gmail.com>
2013-04-03 11:41:23 -0700

    Bug 843829 - Apply transitive waivers for nativeCall. r=mrbkap

--- js/xpconnect/wrappers/WaiveXrayWrapper.h
+++ js/xpconnect/wrappers/WaiveXrayWrapper.h
@@ -36,6 +36,9 @@ class WaiveXrayWrapper : public js::CrossCompartmentWrapper {
                            unsigned argc, js::Value *argv,
                            JS::MutableHandle<JS::Value> rval) MOZ_OVERRIDE;
 
+    virtual bool nativeCall(JSContext *cx, JS::IsAcceptableThis test,
+                            JS::NativeImpl impl, JS::CallArgs args) MOZ_OVERRIDE;
+
     static WaiveXrayWrapper singleton;
 };
 
