18b7d42ec06924d2e21e902e0f968235cb7c2724
Eric Chou <echou@mozilla.com>
2013-04-04 17:55:34 -0700

    Bug 851046: Patch 7 - Remove function ListenSocketViaService(); r=mrbkap

--- dom/bluetooth/linux/BluetoothDBusService.cpp
+++ dom/bluetooth/linux/BluetoothDBusService.cpp
@@ -2793,22 +2793,3 @@ BluetoothDBusService::ConfirmReceivingFile(const nsAString& aDeviceAddress,
   DispatchBluetoothReply(aRunnable, v, errorStr);
 }
 
-nsresult
-BluetoothDBusService::ListenSocketViaService(
-                                    int aChannel,
-                                    BluetoothSocketType aType,
-                                    bool aAuth,
-                                    bool aEncrypt,
-                                    mozilla::ipc::UnixSocketConsumer* aConsumer)
-{
-  NS_ASSERTION(NS_IsMainThread(), "Must be called from main thread!");
-
-  BluetoothUnixSocketConnector* c =
-    new BluetoothUnixSocketConnector(aType, aChannel, aAuth, aEncrypt);
-  if (!aConsumer->ListenSocket(c)) {
-    NS_WARNING("Can't listen on socket!");
-    return NS_ERROR_FAILURE;
-  }
-
-  return NS_OK;
-}
