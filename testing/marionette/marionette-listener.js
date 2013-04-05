51a45a719887e482ec3d543e30873d19330a3688
Malini Das <mdas@mozilla.com>
2013-04-05 13:15:19 -0400

    Bug 858288 - set sandbox to null in switchToFrame, r=jgriffin

--- testing/marionette/marionette-listener.js
+++ testing/marionette/marionette-listener.js
@@ -1603,6 +1603,7 @@ function switchToFrame(msg) {
     if(msg.json.focus == true) {
       curWindow.focus();
     }
+    sandbox = null;
     checkTimer.initWithCallback(checkLoad, 100, Ci.nsITimer.TYPE_ONE_SHOT);
     return;
   }
