8ac16b0290481daf5ce32db55585a9b9febcc89d
Tom Schuster <evilpies@gmail.com>
2013-04-05 15:21:02 +0200

    Bug 855411 - InstallMember. r=bz,terrence

--- content/xbl/src/nsXBLProtoImplMember.h
+++ content/xbl/src/nsXBLProtoImplMember.h
@@ -74,7 +74,7 @@ public:
   const PRUnichar* GetName() { return mName; }
 
   virtual nsresult InstallMember(JSContext* aCx,
-                                 JSObject* aTargetClassObject) = 0;
+                                 JS::Handle<JSObject*> aTargetClassObject) = 0;
   virtual nsresult CompileMember(nsIScriptContext* aContext,
                                  const nsCString& aClassStr,
                                  JSObject* aClassObject) = 0;
