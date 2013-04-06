206e93e61e541ae28e003d398136352a168194d2
David Creswick <dcrewi@gyrae.net>
2013-04-06 12:38:56 -0500

    Bug 856317 - Expose the column number of style rules via inIDOMUtils. r=dbaron

--- layout/style/StyleRule.h
+++ layout/style/StyleRule.h
@@ -317,7 +317,10 @@ public:
   nsCSSSelectorList* Selector() { return mSelector; }
 
   uint32_t GetLineNumber() const { return mLineNumber; }
-  void SetLineNumber(uint32_t aLineNumber) { mLineNumber = aLineNumber; }
+  uint32_t GetColumnNumber() const { return mColumnNumber; }
+  void SetLineNumberAndColumnNumber(uint32_t aLineNumber,
+                                    uint32_t aColumnNumber)
+  { mLineNumber = aLineNumber; mColumnNumber = aColumnNumber; }
 
   Declaration* GetDeclaration() const { return mDeclaration; }
 
@@ -373,7 +376,8 @@ private:
   ImportantRule*          mImportantRule; // initialized by RuleMatched
   DOMCSSStyleRule*        mDOMRule;
   // Keep the same type so that MSVC packs them.
-  uint32_t                mLineNumber : 31;
+  uint32_t                mLineNumber;
+  uint32_t                mColumnNumber : 31;
   uint32_t                mWasMatched : 1;
 
 private:
