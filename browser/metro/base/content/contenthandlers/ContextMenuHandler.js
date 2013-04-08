35d4db0cbe743b1553d4a7db7638330edf4233a2
Brian R. Bondy <netzen@gmail.com>
2013-04-08 14:42:54 -0400

    Bug 858359 - Remove Selectable context menu options on input and textareas when there is text and no selection. r=jimm

--- browser/metro/base/content/contenthandlers/ContextMenuHandler.js
+++ browser/metro/base/content/contenthandlers/ContextMenuHandler.js
@@ -291,12 +291,13 @@ var ContextMenuHandler = {
 
           // Don't include "copy" for password fields.
           if (!(elem instanceof Ci.nsIDOMHTMLInputElement) || elem.mozIsTextField(true)) {
+            // If there is a selection add cut and copy
             if (selectionStart != selectionEnd) {
               state.types.push("cut");
               state.types.push("copy");
               state.string = elem.value.slice(selectionStart, selectionEnd);
-            }
-            if (elem.value && (selectionStart > 0 || selectionEnd < elem.textLength)) {
+            } else if (elem.value && elem.textLength) {
+              // There is text and it is not selected so add selectable items
               state.types.push("selectable");
               state.string = elem.value;
             }
