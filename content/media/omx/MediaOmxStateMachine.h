b815f4f9a0247d6246f2f555aa829f3c09bee755
Sotaro Ikeda <sikeda@mozilla.com>
2013-04-08 14:35:30 -0400

    Bug 837051 - Store less buffers for video playback at MediaOmxReader. r=doublec

--- content/media/omx/MediaOmxStateMachine.h
+++ content/media/omx/MediaOmxStateMachine.h
@@ -21,7 +21,7 @@ protected:
   // OMXCodec will crash.
   // So we need few enough buffers in the queue that all buffers will be
   // returned before OMXCodec begins shutdown.
-  uint32_t GetAmpleVideoFrames() { return 2; }
+  uint32_t GetAmpleVideoFrames() { return 1; }
 };
 
 } // namespace mozilla
