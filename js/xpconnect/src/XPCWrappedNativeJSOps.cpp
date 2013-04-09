a691603d5081c18d559cd83a5a96cf2642f9b095
Jon Coppeard <jcoppeard@mozilla.com>
2013-04-09 16:42:32 +0100

    Bug 861912 - GC: Remove use of AUTO_MARK_JSVAL in XPConnect r=bholley

--- js/xpconnect/src/XPCWrappedNativeJSOps.cpp
+++ js/xpconnect/src/XPCWrappedNativeJSOps.cpp
@@ -424,9 +424,6 @@ DefinePropertyIfFound(XPCCallContext& ccx,
     if (!member->NewFunctionObject(ccx, iface, obj, funval.address()))
         return false;
 
-    // protect funobj until it is actually attached
-    AUTO_MARK_JSVAL(ccx, funval);
-
 #ifdef off_DEBUG_jband
     {
         static int cloneCount = 0;
