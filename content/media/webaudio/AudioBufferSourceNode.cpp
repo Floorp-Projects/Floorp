8a68a1f49f86d17a4e517c512ca828de31246ddf
Ehsan Akhgari <ehsan@mozilla.com>
2013-04-09 11:28:42 -0400

    Backed out changeset 92c2f24cb6ff (bug 854336) because of mochitest-1 crashes

--- content/media/webaudio/AudioBufferSourceNode.cpp
+++ content/media/webaudio/AudioBufferSourceNode.cpp
@@ -9,7 +9,6 @@
 #include "nsMathUtils.h"
 #include "AudioNodeEngine.h"
 #include "AudioNodeStream.h"
-#include "AudioDestinationNode.h"
 #include "speex/speex_resampler.h"
 
 namespace mozilla {
@@ -26,14 +25,12 @@ NS_IMPL_RELEASE_INHERITED(AudioBufferSourceNode, AudioNode)
 class AudioBufferSourceNodeEngine : public AudioNodeEngine
 {
 public:
-  explicit AudioBufferSourceNodeEngine(AudioDestinationNode* aDestination) :
+  AudioBufferSourceNodeEngine() :
     mStart(0), mStop(TRACK_TICKS_MAX),
     mResampler(nullptr),
     mOffset(0), mDuration(0),
     mLoopStart(0), mLoopEnd(0),
-    mSampleRate(0), mPosition(0), mChannels(0), mPlaybackRate(1.0f),
-    mDestination(static_cast<AudioNodeStream*>(aDestination->Stream())),
-    mPlaybackRateTimeline(1.0f), mLoop(false)
+    mSampleRate(0), mPosition(0), mChannels(0), mLoop(false)
   {}
 
   ~AudioBufferSourceNodeEngine()
@@ -54,29 +51,8 @@ public:
     DURATION,
     LOOP,
     LOOPSTART,
-    LOOPEND,
-    PLAYBACKRATE
+    LOOPEND
   };
-  virtual void SetTimelineParameter(uint32_t aIndex, const dom::AudioParamTimeline& aValue)
-  {
-    switch (aIndex) {
-    case PLAYBACKRATE:
-      mPlaybackRateTimeline = aValue;
-      // If we have a simple value that is 1.0 (i.e. intrinsic speed), and our
-      // input buffer is already at the ideal audio rate, and we have a
-      // resampler, we can release it.
-      if (mResampler && mPlaybackRateTimeline.HasSimpleValue() &&
-          mPlaybackRateTimeline.GetValue() == 1.0 &&
-          mSampleRate == IdealAudioRate()) {
-        speex_resampler_destroy(mResampler);
-        mResampler = nullptr;
-      }
-      WebAudioUtils::ConvertAudioParamToTicks(mPlaybackRateTimeline, nullptr, mDestination);
-      break;
-    default:
-      NS_ERROR("Bad GainNodeEngine TimelineParameter");
-    }
-  }
   virtual void SetStreamTimeParameter(uint32_t aIndex, TrackTicks aParam)
   {
     switch (aIndex) {
@@ -152,8 +128,7 @@ public:
     }
   }
 
-  // Resamples input data to an output buffer, according to |mSampleRate| and
-  // the playbackRate.
+  // Resamples input data to an output buffer, according to |mSampleRate|
   // The number of frames consumed/produced depends on the amount of space
   // remaining in both the input and output buffer, and the playback rate (that
   // is, the ratio between the output samplerate and the input samplerate).
@@ -165,7 +140,7 @@ public:
                                          uint32_t& aFramesRead,
                                          uint32_t& aFramesWritten) {
     // Compute the sample rate we want to resample to.
-    double finalSampleRate = mSampleRate / mPlaybackRate;
+    double finalSampleRate = mSampleRate;
     double finalPlaybackRate = finalSampleRate / IdealAudioRate();
     uint32_t availableInOuputBuffer = WEBAUDIO_BLOCK_SIZE - aBufferOffset;
     uint32_t inputSamples, outputSamples;
@@ -247,8 +222,7 @@ public:
                                            aBufferMax - aBufferOffset),
                                   uint32_t(mStop - *aCurrentPosition));
     if (numFrames == WEBAUDIO_BLOCK_SIZE &&
-        mSampleRate == IdealAudioRate() &&
-        mPlaybackRate == 1.0f) {
+        mSampleRate == IdealAudioRate()) {
       BorrowFromInputBuffer(aOutput, aChannels, aBufferOffset);
       *aOffsetWithinBlock += numFrames;
       *aCurrentPosition += numFrames;
@@ -258,7 +232,7 @@ public:
         MOZ_ASSERT(*aOffsetWithinBlock == 0);
         AllocateAudioBlock(aChannels, aOutput);
       }
-      if (mSampleRate == IdealAudioRate() && mPlaybackRate == 1.0f) {
+      if (mSampleRate == IdealAudioRate()) {
         CopyFromInputBuffer(aOutput, aChannels, aBufferOffset, *aOffsetWithinBlock, numFrames);
         *aOffsetWithinBlock += numFrames;
         *aCurrentPosition += numFrames;
@@ -296,20 +270,6 @@ public:
     if (!channels) {
       aOutput->SetNull(WEBAUDIO_BLOCK_SIZE);
       return;
-    }
-
-    // WebKit treats the playbackRate as a k-rate parameter in their code,
-    // despite the spec saying that it should be an a-rate parameter. We treat
-    // it as k-rate. Spec bug: https://www.w3.org/Bugs/Public/show_bug.cgi?id=21592
-    float newPlaybackRate;
-    if (mPlaybackRateTimeline.HasSimpleValue()) {
-      newPlaybackRate = mPlaybackRateTimeline.GetValue();
-    } else {
-      newPlaybackRate = mPlaybackRateTimeline.GetValueAtTime<TrackTicks>(aStream->GetCurrentPosition());
-    }
-    if (newPlaybackRate != mPlaybackRate) {
-      mPlaybackRate = newPlaybackRate;
-      speex_resampler_set_rate(Resampler(mChannels), mSampleRate, mSampleRate / mPlaybackRate);
     }
 
     uint32_t written = 0;
@@ -360,9 +320,6 @@ public:
   int32_t mSampleRate;
   uint32_t mPosition;
   uint32_t mChannels;
-  float mPlaybackRate;
-  AudioNodeStream* mDestination;
-  AudioParamTimeline mPlaybackRateTimeline;
   bool mLoop;
 };
 
@@ -372,11 +329,10 @@ AudioBufferSourceNode::AudioBufferSourceNode(AudioContext* aContext)
   , mLoopEnd(0.0)
   , mLoop(false)
   , mStartCalled(false)
-  , mPlaybackRate(new AudioParam(this, SendPlaybackRateToStream, 1.0f))
 {
   SetProduceOwnOutput(true);
   mStream = aContext->Graph()->CreateAudioNodeStream(
-      new AudioBufferSourceNodeEngine(aContext->Destination()),
+      new AudioBufferSourceNodeEngine(),
       MediaStreamGraph::INTERNAL_STREAM);
   mStream->AddMainThreadListener(this);
 }
@@ -482,13 +438,6 @@ AudioBufferSourceNode::NotifyMainThreadStateChanged()
   if (mStream->IsFinished()) {
     SetProduceOwnOutput(false);
   }
-}
-
-void
-AudioBufferSourceNode::SendPlaybackRateToStream(AudioNode* aNode)
-{
-  AudioBufferSourceNode* This = static_cast<AudioBufferSourceNode*>(aNode);
-  SendTimelineParameterToStream(This, AudioBufferSourceNodeEngine::PLAYBACKRATE, *This->mPlaybackRate);
 }
 
 }
--- content/media/webaudio/AudioBufferSourceNode.h
+++ content/media/webaudio/AudioBufferSourceNode.h
@@ -64,10 +64,7 @@ public:
   {
     mBuffer = aBuffer;
   }
-  AudioParam* PlaybackRate() const
-  {
-    return mPlaybackRate;
-  }
+
   bool Loop() const
   {
     return mLoop;
@@ -96,13 +93,11 @@ public:
   virtual void NotifyMainThreadStateChanged() MOZ_OVERRIDE;
 
 private:
-  static void SendPlaybackRateToStream(AudioNode* aNode);
   nsRefPtr<AudioBuffer> mBuffer;
   double mLoopStart;
   double mLoopEnd;
   bool mLoop;
   bool mStartCalled;
-  nsRefPtr<AudioParam> mPlaybackRate;
 };
 
 }
