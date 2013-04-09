ef5fbc29f79f9053eba16becad5ff44e1086e9ae
Richard Newman <rnewman@mozilla.com>
2013-04-09 12:26:11 -0700

    Bug 859920 - Some TPS tests failed due to removed autoHide pref. r=trivial, a=test-only

--- services/sync/tests/tps/test_client_wipe.js
+++ services/sync/tests/tps/test_client_wipe.js
@@ -108,7 +108,7 @@ var prefs1 = [
   { name: "browser.urlbar.maxRichResults",
     value: 20
   },
-  { name: "browser.tabs.autoHide",
+  { name: "browser.link.open_newwindow",
     value: true
   }
 ];
@@ -120,7 +120,7 @@ var prefs2 = [
   { name: "browser.urlbar.maxRichResults",
     value: 18
   },
-  { name: "browser.tabs.autoHide",
+  { name: "browser.link.open_newwindow",
     value: false
   }
 ];
