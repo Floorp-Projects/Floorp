7e87955bb5595cff722cdcaf284cc07a7ca2610a
Panos Astithas <past@mozilla.com>
2013-04-09 14:17:03 +0300

    Bug 818134 - Allow multiple debuggers in toolboxes to debug separate tabs; r=rcampbell,dcamp

--- toolkit/devtools/debugger/tests/unit/test_nsjsinspector.js
+++ toolkit/devtools/debugger/tests/unit/test_nsjsinspector.js
@@ -1,6 +1,18 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+// Test the basic functionality of the nsIJSInspector component.
+var gCount = 0;
+const MAX = 10;
+var inspector = Cc["@mozilla.org/jsinspector;1"].getService(Ci.nsIJSInspector);
+var tm = Cc["@mozilla.org/thread-manager;1"].getService(Ci.nsIThreadManager);
+
+// Emulate 10 simultaneously-debugged windows from 3 separate client connections.
+var requestor = (count) => ({
+  url:"http://foo/bar/" + count,
+  connection: "conn" + (count % 3)
+});
+
 function run_test()
 {
   test_nesting();
@@ -8,15 +20,40 @@ function run_test()
 
 function test_nesting()
 {
-  let inspector = Cc["@mozilla.org/jsinspector;1"].getService(Ci.nsIJSInspector);
   do_check_eq(inspector.eventLoopNestLevel, 0);
 
-  let tm = Cc["@mozilla.org/thread-manager;1"].getService(Ci.nsIThreadManager);
-  tm.currentThread.dispatch({ run: function() {
-    do_check_eq(inspector.eventLoopNestLevel, 1);
-    do_check_eq(inspector.exitNestedEventLoop(), 0);
-  }}, 0);
+  tm.currentThread.dispatch({ run: enterEventLoop}, 0);
 
-  do_check_eq(inspector.enterNestedEventLoop(), 0);
+  do_check_eq(inspector.enterNestedEventLoop(requestor(gCount)), 0);
   do_check_eq(inspector.eventLoopNestLevel, 0);
+  do_check_eq(inspector.lastNestRequestor, null);
+}
+
+function enterEventLoop() {
+  if (gCount++ < MAX) {
+    tm.currentThread.dispatch({ run: enterEventLoop}, 0);
+
+    let r = Object.create(requestor(gCount));
+
+    do_check_eq(inspector.eventLoopNestLevel, gCount);
+    do_check_eq(inspector.lastNestRequestor.url, requestor(gCount - 1).url);
+    do_check_eq(inspector.lastNestRequestor.connection, requestor(gCount - 1).connection);
+    do_check_eq(inspector.enterNestedEventLoop(requestor(gCount)), gCount);
+  } else {
+    do_check_eq(gCount, MAX + 1);
+    tm.currentThread.dispatch({ run: exitEventLoop}, 0);
+  }
+}
+
+function exitEventLoop() {
+  if (inspector.lastNestRequestor != null) {
+    do_check_eq(inspector.lastNestRequestor.url, requestor(gCount - 1).url);
+    do_check_eq(inspector.lastNestRequestor.connection, requestor(gCount - 1).connection);
+    if (gCount-- > 1) {
+      tm.currentThread.dispatch({ run: exitEventLoop}, 0);
+    }
+
+    do_check_eq(inspector.exitNestedEventLoop(), gCount);
+    do_check_eq(inspector.eventLoopNestLevel, gCount);
+  }
 }
