8cc0a60704aa92c5d811f192fcc70c11d3d19729
Tom Schuster <evilpies@gmail.com>
2013-04-09 13:33:32 +0200

    Bug 836949 - Remove InternNonIntElementId/FetchElementId. r=jwalden

--- js/src/ion/VMFunctions.cpp
+++ js/src/ion/VMFunctions.cpp
@@ -495,9 +495,8 @@ SPSExit(JSContext *cx, HandleScript script)
 bool
 OperatorIn(JSContext *cx, HandleValue key, HandleObject obj, JSBool *out)
 {
-    RootedValue dummy(cx); // Disregards atomization changes: no way to propagate.
     RootedId id(cx);
-    if (!FetchElementId(cx, obj, key, &id, &dummy))
+    if (!ValueToId<CanGC>(cx, key, &id))
         return false;
 
     RootedObject obj2(cx);
