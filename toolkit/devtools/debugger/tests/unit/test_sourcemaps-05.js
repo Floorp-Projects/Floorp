13fe61dc4ee64a8ab48317cce92b1f22a7c727c9
Nick Fitzgerald <fitzgen@gmail.com>
2013-04-09 11:40:00 +0300

    Bug 849069 - Relative source map URLs should be resolved according to the spec's rules; r=past

---
+++ toolkit/devtools/debugger/tests/unit/test_sourcemaps-05.js
@@ -0,0 +1,48 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Check that relative source map urls work.
+ */
+
+var gDebuggee;
+var gClient;
+var gThreadClient;
+
+Components.utils.import('resource:///modules/devtools/SourceMap.jsm');
+
+function run_test()
+{
+  initTestDebuggerServer();
+  gDebuggee = addTestGlobal("test-source-map");
+  gClient = new DebuggerClient(DebuggerServer.connectPipe());
+  gClient.connect(function() {
+    attachTestGlobalClientAndResume(gClient, "test-source-map", function(aResponse, aThreadClient) {
+      gThreadClient = aThreadClient;
+      test_relative_source_map();
+    });
+  });
+  do_test_pending();
+}
+
+function test_relative_source_map()
+{
+  gClient.addOneTimeListener("newSource", function _onNewSource(aEvent, aPacket) {
+    do_check_eq(aEvent, "newSource");
+    do_check_eq(aPacket.type, "newSource");
+    do_check_true(!!aPacket.source);
+
+    do_check_true(aPacket.source.url.indexOf("sourcemapped.coffee") !== -1,
+                  "The new source should be a coffee file.");
+    do_check_eq(aPacket.source.url.indexOf("sourcemapped.js"), -1,
+                "The new source should not be a js file.");
+
+    finishClient(gClient);
+  });
+
+  code = readFile("sourcemapped.js")
+    + "\n//@ sourceMappingURL=source-map-data/sourcemapped.map";
+
+  Components.utils.evalInSandbox(code, gDebuggee, "1.8",
+                                 getFileUrl("sourcemapped.js"), 1);
+}
