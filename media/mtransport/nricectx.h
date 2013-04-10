4ac74593f735bdc8aac679c47a24b32347dddb0c
Adam Roach [:abr] <adam@nostrum.com>
2013-04-10 13:37:50 -0500

    Bug 860143: Check correct variable when setting host r=ekr

--- media/mtransport/nricectx.h
+++ media/mtransport/nricectx.h
@@ -112,7 +112,7 @@ class NrIceStunServer {
       has_addr_ = true;
       return NS_OK;
     }
-    else if (host_.size() < 256) {
+    else if (addr.size() < 256) {
       // Apparently this is a hostname.
       host_ = addr;
       port_ = port;
