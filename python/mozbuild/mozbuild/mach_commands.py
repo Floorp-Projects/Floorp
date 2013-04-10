86ad48dfa75278625c7df93c34b704f6c164f10c
Ted Mielczarek <ted@mielczarek.org>
2013-04-10 11:39:46 -0400

    bug 857383 - Don't try to print the app path for a partial build. r=gps

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -128,15 +128,14 @@ class Build(MachCommandBase):
         else:
             print('Your build was successful!')
 
-        # Fennec doesn't have useful output from just building. We should
-        # arguably make the build action useful for Fennec. Another day...
-        if self.substs['MOZ_BUILD_APP'] != 'mobile/android':
-            app_path = self.get_binary_path('app')
-            print('To take your build for a test drive, run: %s' % app_path)
-
         # Only for full builds because incremental builders likely don't
         # need to be burdened with this.
         if not what:
+            # Fennec doesn't have useful output from just building. We should
+            # arguably make the build action useful for Fennec. Another day...
+            if self.substs['MOZ_BUILD_APP'] != 'mobile/android':
+                app_path = self.get_binary_path('app')
+                print('To take your build for a test drive, run: %s' % app_path)
             app = self.substs['MOZ_BUILD_APP']
             if app in ('browser', 'mobile/android'):
                 print('For more information on what to do now, see '
