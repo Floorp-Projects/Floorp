15df3fd90f22d58cb5e9ff984dcd2cca6a1816a9
Neil Deakin <neil@mozilla.com>
2013-04-11 04:01:47 -0400

    Bug 566746 - Changes to shared form history tests files to support asynchronous form history, p=enndeakin,felix, r=mak

--- toolkit/components/satchel/test/unit/head_satchel.js
+++ toolkit/components/satchel/test/unit/head_satchel.js
@@ -2,8 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://gre/modules/FormHistory.jsm");
+
 const Ci = Components.interfaces;
 const Cc = Components.classes;
+const Cu = Components.utils;
 
 const CURRENT_SCHEMA = 4;
 const PR_HOURS = 60 * 60 * 1000000;
@@ -12,6 +17,12 @@ do_get_profile();
 
 var dirSvc = Cc["@mozilla.org/file/directory_service;1"].
              getService(Ci.nsIProperties);
+
+// Send the profile-after-change notification to the form history component to ensure
+// that it has been initialized.
+var formHistoryStartup = Cc["@mozilla.org/satchel/form-history-startup;1"].
+                         getService(Ci.nsIObserver);
+formHistoryStartup.observe(null, "profile-after-change", null);
 
 function getDBVersion(dbfile) {
     var ss = Cc["@mozilla.org/storage/service;1"].
@@ -25,10 +36,63 @@ function getDBVersion(dbfile) {
 
 const isGUID = /[A-Za-z0-9\+\/]{16}/;
 
-function getGUIDforID(conn, id) {
-    var stmt = conn.createStatement("SELECT guid from moz_formhistory WHERE id = " + id);
-    stmt.executeStep();
-    var guid = stmt.getString(0);
-    stmt.finalize();
-    return guid;
+// Find form history entries.
+function searchEntries(terms, params, iter) {
+  FormHistory.search(terms, params, { onSuccess: function (results) { iter.send(results); },
+                                      onFailure: function (error) {
+                                        do_throw("Error occurred searching form history: " + error);
+                                      }
+                                    });
+}
+
+// Count the number of entries with the given name and value, and call then(number)
+// when done. If name or value is null, then the value of that field does not matter.
+function countEntries(name, value, then) {
+  var obj = {};
+  if (name !== null)
+    obj.fieldname = name;
+  if (value !== null)
+    obj.value = value;
+
+  FormHistory.count(obj, { onSuccess: function (num) then(num),
+                           onFailure: function (error) {
+                             do_throw("Error occurred searching form history: " + error);
+                           }
+                         });
+}
+
+// Perform a single form history update and call then() when done.
+function updateEntry(op, name, value, then) {
+  var obj = { op: op };
+  if (name !== null)
+    obj.fieldname = name;
+  if (value !== null)
+    obj.value = value;
+  updateFormHistory(obj, then);
+}
+
+// Add a single form history entry with the current time and call then() when done.
+function addEntry(name, value, then) {
+  let now = Date.now() * 1000;
+  updateFormHistory({ op: "add", fieldname: name, value: value, timesUsed: 1,
+                      firstUsed: now, lastUsed: now }, then);
+}
+
+// Wrapper around FormHistory.update which handles errors. Calls then() when done.
+function updateFormHistory(changes, then) {
+  FormHistory.update(changes, { onSuccess: then,
+                                onFailure: function (error) {
+                                  do_throw("Error occurred updating form history: " + error);
+                                }
+                              });
+}
+
+/**
+ * Logs info to the console in the standard way (includes the filename).
+ *
+ * @param aMessage
+ *        The message to log to the console.
+ */
+function do_log_info(aMessage) {
+  print("TEST-INFO | " + _TEST_FILE + " | " + aMessage);
 }
