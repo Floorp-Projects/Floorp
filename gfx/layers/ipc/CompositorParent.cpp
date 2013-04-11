bae6f65df1fb021977b30f4bf2e77e986d00901b
Nicolas Silva <nical.bugzilla@gmail.com>
2013-04-11 06:14:29 -0400

    Bug 860463 - Ensure that we propagate Destroy() in the shadow layer tree on CompositorParent::ActorDestroy. r=nrc

--- gfx/layers/ipc/CompositorParent.cpp
+++ gfx/layers/ipc/CompositorParent.cpp
@@ -233,7 +233,9 @@ CompositorParent::RecvWillStop()
   RemoveCompositor(mCompositorID);
 
   // Ensure that the layer manager is destroyed before CompositorChild.
-  mLayerManager->Destroy();
+  if (mLayerManager) {
+    mLayerManager->Destroy();
+  }
 
   return true;
 }
@@ -278,6 +280,18 @@ CompositorParent::RecvMakeSnapshot(const SurfaceDescriptor& aInSnapshot,
   *aOutSnapshot = aInSnapshot;
   return true;
 }
+
+void
+CompositorParent::ActorDestroy(ActorDestroyReason why)
+{
+  mPaused = true;
+  RemoveCompositor(mCompositorID);
+
+  if (mLayerManager) {
+    mLayerManager->Destroy();
+  }
+}
+
 
 void
 CompositorParent::ScheduleRenderOnCompositorThread()
