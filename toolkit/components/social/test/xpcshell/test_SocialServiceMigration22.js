d18240e93f927ede7119379251e4691467b3cfb2
Shane Caraveo <scaraveo@mozilla.com>
2013-04-11 15:43:40 -0700

    bug 859715 protect against migration failures of social providers, r=markh

--- toolkit/components/social/test/xpcshell/test_SocialServiceMigration22.js
+++ toolkit/components/social/test/xpcshell/test_SocialServiceMigration22.js
@@ -26,6 +26,8 @@ function run_test() {
              createInstance(Ci.nsISupportsString);
   let active = {};
   active[manifest.origin] = 1;
+  // bad.origin tests that a missing manifest does not break migration, bug 859715
+  active["bad.origin"] = 1;
   activeVal.data = JSON.stringify(active);
   Services.prefs.setComplexValue("social.activeProviders",
                                  Ci.nsISupportsString, activeVal);
@@ -57,4 +59,7 @@ function testMigration(manifest, next) {
   do_check_true(activeProviders[manifest.origin]);
   do_check_true(MANIFEST_PREFS.prefHasUserValue(manifest.origin));
   do_check_true(JSON.parse(DEFAULT_PREFS.getCharPref(manifest.origin)).builtin);
+
+  // bug 859715, this should have been removed during migration
+  do_check_false(!!activeProviders["bad.origin"]);
 }
