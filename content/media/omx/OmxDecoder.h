7f283fdbd8bd8410b218690fccad006bb1777e52
Diego Wilson <dwilson@codeaurora.org>
2013-04-10 17:58:25 -0700

    Bug 860760 - Pause OMX media sources when playback is paused. r=sotaro, r=cdouble
    OMXCodec needs to now when the stream is paused. Otherwise it will
    prevent the device from lowering its power state when idle.

--- content/media/omx/OmxDecoder.h
+++ content/media/omx/OmxDecoder.h
@@ -132,6 +132,10 @@ class OmxDecoder : public RefBase {
   bool ToVideoFrame(VideoFrame *aFrame, int64_t aTimeUs, void *aData, size_t aSize, bool aKeyFrame);
   bool ToAudioFrame(AudioFrame *aFrame, int64_t aTimeUs, void *aData, size_t aDataOffset, size_t aSize,
                     int32_t aAudioChannels, int32_t aAudioSampleRate);
+
+  //True if decoder is in a paused state
+  bool mPaused;
+
 public:
   OmxDecoder(MediaResource *aResource, AbstractMediaDecoder *aDecoder);
   ~OmxDecoder();
@@ -172,6 +176,12 @@ public:
   }
 
   bool ReleaseVideoBuffer(MediaBuffer *aBuffer);
+
+  //Change decoder into a playing state
+  nsresult Play();
+
+  //Change decoder into a paused state
+  void Pause();
 };
 
 }
