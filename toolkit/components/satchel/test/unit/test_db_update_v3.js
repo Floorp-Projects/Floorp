0021e7aaee7381184481a8e9ace38a9844af6d94
Ryan VanderMeulen <ryanvm@gmail.com>
2013-04-11 11:53:13 -0400

    Backed out 17 changesets (bug 566746, bug 697377) for frequent OSX debug mochitest-5 crashes.

--- toolkit/components/satchel/test/unit/test_db_update_v3.js
+++ toolkit/components/satchel/test/unit/test_db_update_v3.js
@@ -1,6 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var testnum = 0;
 var fh;
@@ -10,7 +10,7 @@ function run_test()
   try {
 
   // ===== test init =====
-  var testfile = do_get_file("formhistory_v3v4.sqlite");
+  var testfile = do_get_file("formhistory_v2.sqlite");
   var profileDir = dirSvc.get("ProfD", Ci.nsIFile);
 
   // Cleanup from any previous tests or failures.
@@ -20,7 +20,7 @@ function run_test()
     destFile.remove(false);
 
   testfile.copyTo(profileDir, "formhistory.sqlite");
-  do_check_eq(3, getDBVersion(testfile));
+  do_check_eq(2, getDBVersion(testfile));
 
   fh = Cc["@mozilla.org/satchel/form-history;1"].
        getService(Ci.nsIFormHistory2);
@@ -30,11 +30,21 @@ function run_test()
   testnum++;
 
   // Check that the index was added
-  do_check_true(fh.DBConnection.tableExists("moz_deleted_formhistory"));
+  do_check_true(fh.DBConnection.indexExists("moz_formhistory_guid_index"));
   // check for upgraded schema.
   do_check_eq(CURRENT_SCHEMA, fh.DBConnection.schemaVersion);
-  // check that an entry still exists
+
   do_check_true(fh.entryExists("name-A", "value-A"));
+  var guid = getGUIDforID(fh.DBConnection, 1);
+  do_check_true(isGUID.test(guid));
+
+  // Add a new entry and check that it gets a GUID
+  do_check_false(fh.entryExists("name-B", "value-B"));
+  fh.addEntry("name-B", "value-B");
+  do_check_true(fh.entryExists("name-B", "value-B"));
+
+  guid = getGUIDforID(fh.DBConnection, 2);
+  do_check_true(isGUID.test(guid));
 
   } catch (e) {
     throw "FAILED in test #" + testnum + " -- " + e;
--- toolkit/components/satchel/test/unit/test_db_update_v999a.js
+++ toolkit/components/satchel/test/unit/test_db_update_v999a.js
@@ -11,20 +11,7 @@
  * Part B tests this when the columns do *not* match, so the DB is reset.
  */
 
-let iter = tests();
-
 function run_test()
-{
-  do_test_pending();
-  iter.next();
-}
-
-function next_test()
-{
-  iter.next();
-}
-
-function tests()
 {
   try {
   var testnum = 0;
@@ -42,34 +29,32 @@ function tests()
   testfile.copyTo(profileDir, "formhistory.sqlite");
   do_check_eq(999, getDBVersion(testfile));
 
-  let checkZero = function(num) { do_check_eq(num, 0); next_test(); }
-  let checkOne = function(num) { do_check_eq(num, 1); next_test(); }
+  var fh = Cc["@mozilla.org/satchel/form-history;1"].
+           getService(Ci.nsIFormHistory2);
+
 
   // ===== 1 =====
   testnum++;
   // Check for expected contents.
-  yield countEntries(null, null, function(num) { do_check_true(num > 0); next_test(); });
-  yield countEntries("name-A", "value-A", checkOne);
-  yield countEntries("name-B", "value-B", checkOne);
-  yield countEntries("name-C", "value-C1", checkOne);
-  yield countEntries("name-C", "value-C2", checkOne);
-  yield countEntries("name-E", "value-E", checkOne);
-
+  do_check_true(fh.hasEntries);
+  do_check_true(fh.entryExists("name-A", "value-A"));
+  do_check_true(fh.entryExists("name-B", "value-B"));
+  do_check_true(fh.entryExists("name-C", "value-C1"));
+  do_check_true(fh.entryExists("name-C", "value-C2"));
+  do_check_true(fh.entryExists("name-E", "value-E"));
   // check for downgraded schema.
-  do_check_eq(CURRENT_SCHEMA, FormHistory.schemaVersion);
+  do_check_eq(CURRENT_SCHEMA, fh.DBConnection.schemaVersion);
 
   // ===== 2 =====
   testnum++;
   // Exercise adding and removing a name/value pair
-  yield countEntries("name-D", "value-D", checkZero);
-  yield updateEntry("add", "name-D", "value-D", next_test);
-  yield countEntries("name-D", "value-D", checkOne);
-  yield updateEntry("remove", "name-D", "value-D", next_test);
-  yield countEntries("name-D", "value-D", checkZero);
+  do_check_false(fh.entryExists("name-D", "value-D"));
+  fh.addEntry("name-D", "value-D");
+  do_check_true(fh.entryExists("name-D", "value-D"));
+  fh.removeEntry("name-D", "value-D");
+  do_check_false(fh.entryExists("name-D", "value-D"));
 
   } catch (e) {
     throw "FAILED in test #" + testnum + " -- " + e;
   }
-
-  do_test_finished();
 }
