a46834c455e7a97edc7a733abef75362073f83f9
Fabrice Desré <fabrice@mozilla.com>
2013-04-11 07:45:02 -0700

    Bug 853716 - Let the SMS protocol handler deal with sms: url without numbers r=baku

--- b2g/components/SmsProtocolHandler.js
+++ b2g/components/SmsProtocolHandler.js
@@ -42,11 +42,24 @@ SmsProtocolHandler.prototype = {
 
   newChannel: function Proto_newChannel(aURI) {
     let number = TelURIParser.parseURI('sms', aURI.spec);
+    let body = "";
+    let query = aURI.spec.split("?")[1];
 
-    if (number) {
+    if (query) {
+      let params = query.split("&");
+      params.forEach(function(aParam) {
+        let [name, value] = aParam.split("=");
+        if (name === "body") {
+          body = decodeURIComponent(value);
+        }
+      })
+    }
+
+    if (number || body) {
       cpmm.sendAsyncMessage("sms-handler", {
-        number: number,
-        type: "websms/sms" });
+        number: number || "",
+        type: "websms/sms",
+        body: body });
     }
 
     throw Components.results.NS_ERROR_ILLEGAL_VALUE;
