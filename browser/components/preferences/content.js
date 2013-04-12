d0e91d2273469e23eb93ecb015965010fb9e11ed
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2013-04-11 19:41:21 -0700

    Bug 860033 - un-generic-ify _showExceptions, r=gavin

--- browser/components/preferences/content.js
+++ browser/components/preferences/content.js
@@ -32,28 +32,6 @@ var gContentPane = {
     return undefined;
   },
 
-  /**
-   * The exceptions types which may be passed to this._showExceptions().
-   */
-  _exceptionsParams: {
-    popup:   { blockVisible: false, sessionVisible: false, allowVisible: true, prefilledHost: "", permissionType: "popup"   }
-  },
-
-  /**
-   * Displays the exceptions dialog of the given type, where types map onto the
-   * the fields in this._exceptionsParams.
-   */  
-  _showExceptions: function (aPermissionType)
-  {
-    var bundlePreferences = document.getElementById("bundlePreferences");
-    var params = this._exceptionsParams[aPermissionType];
-    params.windowTitle = bundlePreferences.getString(aPermissionType + "permissionstitle");
-    params.introText = bundlePreferences.getString(aPermissionType + "permissionstext");
-    document.documentElement.openWindow("Browser:Permissions",
-                                        "chrome://browser/content/preferences/permissions.xul",
-                                        "", params);
-  },
-
   // BEGIN UI CODE
 
   /*
@@ -71,7 +49,13 @@ var gContentPane = {
    */
   showPopupExceptions: function ()
   {
-    this._showExceptions("popup");
+    var bundlePreferences = document.getElementById("bundlePreferences");
+    var params = { blockVisible: false, sessionVisible: false, allowVisible: true, prefilledHost: "", permissionType: "popup" };
+    params.windowTitle = bundlePreferences.getString("popuppermissionstitle");
+    params.introText = bundlePreferences.getString("popuppermissionstext");
+    document.documentElement.openWindow("Browser:Permissions",
+                                        "chrome://browser/content/preferences/permissions.xul",
+                                        "", params);
   },
 
 
