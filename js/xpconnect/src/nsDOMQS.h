1baa79df9fa98a125167a9e6bd253e17bb7e82be
Boris Zbarsky <bzbarsky@mit.edu>
2013-04-12 12:50:06 -0400

    Back out bug 860591 (rev 8488f69f8f91) because it causes orange, resulting in a CLOSED TREE

--- js/xpconnect/src/nsDOMQS.h
+++ js/xpconnect/src/nsDOMQS.h
@@ -17,8 +17,6 @@
 #include "nsICSSDeclaration.h"
 #include "nsSVGElement.h"
 #include "nsDOMEvent.h"
-#include "nsDOMMouseEvent.h"
-#include "nsDOMUIEvent.h"
 #include "mozilla/dom/EventTargetBinding.h"
 #include "mozilla/dom/NodeBinding.h"
 #include "mozilla/dom/ElementBinding.h"
@@ -54,8 +52,6 @@ NEW_BINDING(nsIDocument, Document);
 NEW_BINDING(nsDocument, Document);
 NEW_BINDING(nsSVGElement, SVGElement);
 NEW_BINDING(nsDOMEvent, Event);
-NEW_BINDING(nsDOMMouseEvent, MouseEvent);
-NEW_BINDING(nsDOMUIEvent, UIEvent);
 
 #define DEFINE_UNWRAP_CAST(_interface, _base, _bit)                           \
 template <>                                                                   \
