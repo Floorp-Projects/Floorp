1f8653bbbc5e81c7ebe123f58dc2c166efe09e87
Julian Seward <jseward@acm.org>
2013-04-15 16:46:43 +0200

    Bug 861141 - Connect Breakpad on Android to faulty.lib's mmap interface.  r=glandium,ted

--- toolkit/crashreporter/google-breakpad/src/common/linux/dump_symbols.h
+++ toolkit/crashreporter/google-breakpad/src/common/linux/dump_symbols.h
@@ -41,6 +41,7 @@
 
 #include "common/symbol_data.h"
 #include "common/using_std_string.h"
+#include "google_breakpad/common/breakpad_types.h"
 
 namespace google_breakpad {
 
@@ -64,6 +65,14 @@ bool ReadSymbolData(const string& obj_file,
                     const std::vector<string>& debug_dirs,
                     SymbolData symbol_data,
                     Module** module);
+
+// Same as ReadSymbolData, except don't try to open the file; instead
+// just use the in-memory data (mapped image of it) located at OBJ_FILE.
+bool ReadSymbolDataInternal(const uint8_t* obj_file,
+                            const string& obj_filename,
+                            const std::vector<string>& debug_dirs,
+                            SymbolData symbol_data,
+                            Module** module);
 
 }  // namespace google_breakpad
 
