57e4976d0f7d2a0c67ed612057c4b691548ebb0e
Girish Sharma <scrapmachines@gmail.com>
2013-04-15 18:04:48 +0530

    Bug 851546 - Options panel for DevTools Toolbox, r=jwalker,vporof,past

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1038,6 +1038,7 @@ pref("devtools.toolbox.host", "bottom");
 pref("devtools.toolbox.selectedTool", "webconsole");
 pref("devtools.toolbox.toolbarSpec", '["paintflashing toggle","tilt toggle","scratchpad","resize toggle"]');
 pref("devtools.toolbox.sideEnabled", true);
+pref("devtools.toolbox.disabledTools", "[]");
 
 // Enable the Inspector
 pref("devtools.inspector.enabled", true);
