dc8230838c8c5a66ec639181333fe318266b4b85
Sean Stangl <sstangl@mozilla.com>
2013-04-16 13:47:10 -0700

    Bug 860029 - Standardize Modelines in the JS Module. r=njn

--- js/src/devtools/jint/treesearch.py
+++ js/src/devtools/jint/treesearch.py
@@ -1,8 +1,7 @@
+# vim: set ts=8 sts=4 et sw=4 tw=99:
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-# vim: set ts=4 sw=4 tw=99 et: 
 
 import os, re
 import tempfile
