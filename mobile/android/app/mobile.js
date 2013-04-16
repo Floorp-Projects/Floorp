83ef688f07862d12716c17c94ededec9e716e0c5
Margaret Leibovic <margaret.leibovic@gmail.com>
2013-04-16 10:39:16 -0700

    Bug 857989 - Add Serif/Sans Serif font toggle to Reader Mode. r=lucasr

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -653,6 +653,9 @@ pref("reader.margin_size", 5);
 // The default color scheme in reader (light, dark, sepia)
 pref("reader.color_scheme", "light");
 
+// The font type in reader (sans-serif, serif)
+pref("reader.font_type", "sans-serif");
+
 // Used to show a first-launch tip in reader
 pref("reader.has_used_toolbar", false);
 
