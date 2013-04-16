57d20d5b9ab19ab583aeb5c17be316205ec592ca
Michal Novotny <michal.novotny@gmail.com>
2013-04-16 17:40:08 +0200

    Bug 829419 - Don't allow processing pending events while shutting down cache IO thread, r=jduell

---
+++ netwerk/cache/nsCacheUtils.h
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set sw=2 ts=8 et tw=80 : */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef _nsCacheUtils_h_
+#define _nsCacheUtils_h_
+
+#include "nsThreadUtils.h"
+#include "nsCOMPtr.h"
+#include "mozilla/Mutex.h"
+#include "mozilla/CondVar.h"
+
+class nsIThread;
+
+/**
+ * A class with utility methods for shutting down nsIThreads easily.
+  */
+class nsShutdownThread : public nsRunnable {
+public:
+  nsShutdownThread(nsIThread *aThread);
+  ~nsShutdownThread();
+
+  NS_IMETHOD Run();
+
+/**
+ * Shutdown ensures that aThread->Shutdown() is called on a main thread
+ */
+  static nsresult Shutdown(nsIThread *aThread);
+
+/**
+ * BlockingShutdown ensures that by the time it returns, aThread->Shutdown() has
+ * been called and no pending events have been processed on the current thread.
+ */
+  static nsresult BlockingShutdown(nsIThread *aThread);
+
+private:
+  mozilla::Mutex      mLock;
+  mozilla::CondVar    mCondVar;
+  nsCOMPtr<nsIThread> mThread;
+};
+
+#endif
