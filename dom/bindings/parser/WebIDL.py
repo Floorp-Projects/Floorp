baa392919d402f273d39252f48b3d7451249d787
Olli Pettay <Olli.Pettay@helsinki.fi>
2013-04-17 23:29:09 +0300

    Bug 862983 - HeaderFile annotation for WebIDL interfaces, r=bz
    --HG--
    extra : rebase_source : f096c032342b71aeb42999f80241697c79037b7c

--- dom/bindings/parser/WebIDL.py
+++ dom/bindings/parser/WebIDL.py
@@ -897,7 +897,8 @@ class IDLInterface(IDLObjectWithScope):
             elif (identifier == "PrefControlled" or
                   identifier == "Pref" or
                   identifier == "NeedNewResolve" or
-                  identifier == "JSImplementation"):
+                  identifier == "JSImplementation" or
+                  identifier == "HeaderFile"):
                 # Known attributes that we don't need to do anything with here
                 pass
             else:
