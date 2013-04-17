e91200595cbbd5f1a047aa98b6dbc65a2bd71bc9
Matt Woodrow <mwoodrow@mozilla.com>
2013-04-17 14:21:06 +1200

    Bug 860615 - Add CreateSharedHandle to GLContextProvider so we can create one without a GLContext. r=jgilbert

--- gfx/gl/GLContextProviderEGL.cpp
+++ gfx/gl/GLContextProviderEGL.cpp
@@ -665,7 +665,6 @@ public:
         return sEGLLibrary.HasKHRLockSurface();
     }
 
-    virtual SharedTextureHandle CreateSharedHandle(SharedTextureShareType shareType);
     virtual SharedTextureHandle CreateSharedHandle(SharedTextureShareType shareType,
                                                    void* buffer,
                                                    SharedTextureBufferType bufferType);
@@ -907,31 +906,6 @@ GLContextEGL::UpdateSharedHandle(SharedTextureShareType shareType,
     // in different thread GLContext.  If we have KHR_fence_sync, then
     // we insert a sync object, otherwise we have to do a GuaranteeResolve.
     wrap->MakeSync(this);
-}
-
-SharedTextureHandle
-GLContextEGL::CreateSharedHandle(SharedTextureShareType shareType)
-{
-    if (shareType != SameProcess)
-        return 0;
-
-    if (!mShareWithEGLImage)
-        return 0;
-
-    MakeCurrent();
-    mTemporaryEGLImageTexture = CreateTextureForOffscreen(GetGLFormats(), OffscreenSize());
-
-    EGLTextureWrapper* tex = new EGLTextureWrapper();
-    bool ok = tex->CreateEGLImage(this, mTemporaryEGLImageTexture);
-
-    if (!ok) {
-        NS_ERROR("EGLImage creation for EGLTextureWrapper failed");
-        ReleaseSharedHandle(shareType, (SharedTextureHandle)tex);
-        return 0;
-    }
-
-    // Raw pointer shared across threads
-    return (SharedTextureHandle)tex;
 }
 
 SharedTextureHandle
@@ -2362,6 +2336,14 @@ GLContextProviderEGL::CreateOffscreen(const gfxIntSize& size,
         return nullptr;
 
     return glContext.forget();
+}
+
+SharedTextureHandle
+GLContextProviderEGL::CreateSharedHandle(GLContext::SharedTextureShareType shareType,
+                                         void* buffer,
+                                         GLContext::SharedTextureBufferType bufferType)
+{
+  return 0;
 }
 
 // Don't want a global context on Android as 1) share groups across 2 threads fail on many Tegra drivers (bug 759225)
