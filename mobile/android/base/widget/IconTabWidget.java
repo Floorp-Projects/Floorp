c4b64e2b24de36d4748c429e200809cb9e7ec669
Sriram Ramasubramanian <sriram@mozilla.com>
2013-04-17 16:09:02 -0700

    Bug 862996: Put text headings back in the tab menu. [r=mfinkle]

--- mobile/android/base/widget/IconTabWidget.java
+++ mobile/android/base/widget/IconTabWidget.java
@@ -7,15 +7,16 @@ package org.mozilla.gecko.widget;
 import org.mozilla.gecko.R;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TabWidget;
 
 public class IconTabWidget extends TabWidget {
-    private Context mContext;
     private OnTabChangedListener mListener;
+    private boolean mIsIcon;
 
     public static interface OnTabChangedListener {
         public void onTabChanged(int tabIndex);
@@ -23,12 +24,22 @@ public class IconTabWidget extends TabWidget {
 
     public IconTabWidget(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mContext = context;
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.IconTabWidget);
+        int type = a.getInt(R.styleable.IconTabWidget_display, 0x00);
+        a.recycle();
+
+        mIsIcon = (type == 0x01);
     }
 
-    public ImageButton addTab(int resId) {
-        ImageButton button = (ImageButton) LayoutInflater.from(mContext).inflate(R.layout.tabs_panel_indicator, null);
-        button.setImageResource(resId);
+    public Button addTab(int imageResId, int stringResId) {
+        Button button = (Button) LayoutInflater.from(getContext()).inflate(R.layout.tabs_panel_indicator, null);
+        if (mIsIcon) {
+            button.setCompoundDrawablesWithIntrinsicBounds(imageResId, 0, 0, 0);
+            button.setContentDescription(getContext().getString(stringResId));
+        } else {
+            button.setText(getContext().getString(stringResId).toUpperCase());
+        }
 
         addView(button);
         button.setOnClickListener(new TabClickListener(getTabCount() - 1));
