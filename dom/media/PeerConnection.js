4a61c86411d4fb56dcc4765e3884caab301c5edc
Jan-Ivar Bruaroey <jib@mozilla.com>
2013-04-17 19:03:39 -0400

    Bug 857765 - Add try/catch to content-script window.onerror call. r=abr

--- dom/media/PeerConnection.js
+++ dom/media/PeerConnection.js
@@ -807,18 +807,32 @@ PeerConnectionObserver.prototype = {
         let file = stack[1];
         let line = parseInt(stack[3].split(" ")[1]);
 
-        let scriptError = Cc["@mozilla.org/scripterror;1"].
-            createInstance(Ci.nsIScriptError);
+        let scriptErrorClass = Cc["@mozilla.org/scripterror;1"];
+        let scriptError = scriptErrorClass.createInstance(Ci.nsIScriptError);
         scriptError.initWithWindowID(msg, file, null, line, 0,
                                      Ci.nsIScriptError.exceptionFlag,
                                      "content javascript",
                                      this._dompc._winID);
-        Cc["@mozilla.org/consoleservice;1"].
-            getService(Ci.nsIConsoleService).logMessage(scriptError);
+        let console = Cc["@mozilla.org/consoleservice;1"].
+            getService(Ci.nsIConsoleService);
+        console.logMessage(scriptError);
 
-        // Call onerror directly if present (necessary for testing)
-        if (typeof this._dompc._win.onerror === "function") {
-          this._dompc._win.onerror(msg, file, line);
+        // Safely call onerror directly if present (necessary for testing)
+        try {
+          if (typeof this._dompc._win.onerror === "function") {
+            this._dompc._win.onerror(msg, file, line);
+          }
+        } catch(e) {
+          // If onerror itself throws, service it.
+          try {
+            let scriptError = scriptErrorClass.createInstance(Ci.nsIScriptError);
+            scriptError.initWithWindowID(e.message, e.fileName, null,
+                                         e.lineNumber, 0,
+                                         Ci.nsIScriptError.exceptionFlag,
+                                         "content javascript",
+                                         this._dompc._winID);
+            console.logMessage(scriptError);
+          } catch(e) {}
         }
       }
     }
