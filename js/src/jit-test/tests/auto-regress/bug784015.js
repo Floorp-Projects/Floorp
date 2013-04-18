bd9b505cc9de9e85b64d0faf51b4d82526586986
Nicholas D. Matsakis <nmatsakis@mozilla.com>
2013-04-18 05:53:53 -0400

    Bug 861705 - Check for ParallelArray being disabled in various tests r=sstangl

--- js/src/jit-test/tests/auto-regress/bug784015.js
+++ js/src/jit-test/tests/auto-regress/bug784015.js
@@ -4,5 +4,9 @@
 // Flags:
 //
 
-var p = new ParallelArray([1,25e8 ,3,4]);
-var pp = p.partition(.34 );
+if (getBuildConfiguration().parallelJS) {
+  var p = new ParallelArray([1,25e8 ,3,4]);
+  var pp = p.partition(.34 );
+} else {
+  throw new Error();
+}
