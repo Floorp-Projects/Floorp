1ad0eae27dd40b318e9c5c632f04d025fdc864d9
Ehsan Akhgari <ehsan@mozilla.com>
2013-04-19 14:37:41 -0400

    Bug 863559 - Do not leak the mach port in PlatformThread::CurrentId; r=jlebar

--- ipc/chromium/src/base/platform_thread_posix.cc
+++ ipc/chromium/src/base/platform_thread_posix.cc
@@ -45,7 +45,9 @@ PlatformThreadId PlatformThread::CurrentId() {
   // Pthreads doesn't have the concept of a thread ID, so we have to reach down
   // into the kernel.
 #if defined(OS_MACOSX)
-  return mach_thread_self();
+  mach_port_t port = mach_thread_self();
+  mach_port_deallocate(mach_task_self(), port);
+  return port;
 #elif defined(OS_NETBSD)
   return _lwp_self();
 #elif defined(OS_DRAGONFLY)
