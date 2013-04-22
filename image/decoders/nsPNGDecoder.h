e3965097bad080ec7e7efe9da015b6a8c0d19c72
Joe Drew <joe@drew.ca>
2013-04-22 10:57:17 -0400

    Bug 863123 - Set APNG attributes on the right frame, rather than one frame behind. r=seth

--- image/decoders/nsPNGDecoder.h
+++ image/decoders/nsPNGDecoder.h
@@ -90,6 +90,20 @@ public:
   uint32_t mCMSMode;
   bool mDisablePremultipliedAlpha;
 
+  struct AnimFrameInfo
+  {
+    AnimFrameInfo();
+#ifdef PNG_APNG_SUPPORTED
+    AnimFrameInfo(png_structp aPNG, png_infop aInfo);
+#endif
+
+    RasterImage::FrameDisposalMethod mDispose;
+    RasterImage::FrameBlendMethod mBlend;
+    int32_t mTimeout;
+  };
+
+  AnimFrameInfo mAnimInfo;
+
   // The number of frames we've finished.
   uint32_t mNumFrames;
   
