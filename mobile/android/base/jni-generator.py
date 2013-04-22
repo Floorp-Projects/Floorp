4b1e05088795cc0a7c324a2aa3039dbef368dbb3
Ed Morley <emorley@mozilla.com>
2013-04-22 17:43:10 +0100

    Backed out changeset 26ab26b299a5 (bug 788022) on a CLOSED TREE

--- mobile/android/base/jni-generator.py
+++ mobile/android/base/jni-generator.py
@@ -69,7 +69,7 @@ class Generator:
                     returnValue = ''
                 elif returnType == 'jobject':
                     returnValue = 'NULL'
-                elif returnType in ('jint', 'jfloat', 'jdouble'):
+                elif returnType in ('jint', 'jfloat'):
                     returnValue = '0'
                 else:
                     raise Exception(('Unsupported JNI return type %s found; '
