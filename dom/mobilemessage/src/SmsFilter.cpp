c78bd008f8dc2549aa877c6e7e506e24389ff1e2
Jeff Walden <jwalden@mit.edu>
2013-04-22 16:29:25 -0700

    Bug 864558 - Fix some minor issues with SMS sending and filtering.  r=mounir, r=mrbkap taking into consideration a comment by bz
    --HG--
    extra : rebase_source : 6a63d12a77e7aec9d8719e338b138dcab7d1e3ac

--- dom/mobilemessage/src/SmsFilter.cpp
+++ dom/mobilemessage/src/SmsFilter.cpp
@@ -131,16 +131,27 @@ SmsFilter::GetNumbers(JSContext* aCx, JS::Value* aNumbers)
     return NS_OK;
   }
 
-  JS::Value* numbers = new JS::Value[length];
-
-  for (uint32_t i=0; i<length; ++i) {
-    numbers[i].setString(JS_NewUCStringCopyN(aCx, mData.numbers()[i].get(),
-                                             mData.numbers()[i].Length()));
+  JS::AutoValueVector numbers(aCx);
+  if (!numbers.resize(length)) {
+    return NS_ERROR_FAILURE;
   }
 
-  aNumbers->setObjectOrNull(JS_NewArrayObject(aCx, length, numbers));
-  NS_ENSURE_TRUE(aNumbers->isObject(), NS_ERROR_FAILURE);
+  for (uint32_t i = 0; i < length; ++i) {
+    JSString* str = JS_NewUCStringCopyN(aCx, mData.numbers()[i].get(),
+                                        mData.numbers()[i].Length());
+    if (!str) {
+      return NS_ERROR_FAILURE;
+    }
 
+    numbers[i].setString(str);
+  }
+
+  JSObject* obj = JS_NewArrayObject(aCx, numbers.length(), numbers.begin());
+  if (!obj) {
+    return NS_ERROR_FAILURE;
+  }
+
+  aNumbers->setObject(*obj);
   return NS_OK;
 }
 
