97ecd6c25a1a65644697baa95bfbcd98a2c8494f
Jeff Walden <jwalden@mit.edu>
2013-04-22 16:29:25 -0700

    Bug 864558 - Fix some minor issues with SMS sending and filtering.  r=mounir, r=mrbkap taking into consideration a comment by bz

--- dom/mobilemessage/src/SmsManager.cpp
+++ dom/mobilemessage/src/SmsManager.cpp
@@ -192,27 +192,44 @@ SmsManager::Send(const JS::Value& aNumber, const nsAString& aMessage, JS::Value*
     return Send(cx, global, aNumber.toString(), aMessage, aReturn);
   }
 
-  // Must be an array then.
+  // Must be an object then.
+  if (!aNumber.isObject()) {
+    return NS_ERROR_FAILURE;
+  }
+
   JSObject& numbers = aNumber.toObject();
-
   uint32_t size;
-  JS_ALWAYS_TRUE(JS_GetArrayLength(cx, &numbers, &size));
+  if (!JS_GetArrayLength(cx, &numbers, &size)) {
+    return NS_ERROR_FAILURE;
+  }
 
-  JS::Value* requests = new JS::Value[size];
+  JS::AutoValueVector requests(cx);
+  if (!requests.resize(size)) {
+    return NS_ERROR_FAILURE;
+  }
 
-  for (uint32_t i=0; i<size; ++i) {
+  JS::RootedString str(cx);
+  for (uint32_t i = 0; i < size; ++i) {
     JS::Value number;
     if (!JS_GetElement(cx, &numbers, i, &number)) {
       return NS_ERROR_INVALID_ARG;
     }
 
-    nsresult rv = Send(cx, global, number.toString(), aMessage, &requests[i]);
+    str = JS_ValueToString(cx, number);
+    if (!str) {
+      return NS_ERROR_FAILURE;
+    }
+
+    nsresult rv = Send(cx, global, str, aMessage, &requests[i]);
     NS_ENSURE_SUCCESS(rv, rv);
   }
 
-  aReturn->setObjectOrNull(JS_NewArrayObject(cx, size, requests));
-  NS_ENSURE_TRUE(aReturn->isObject(), NS_ERROR_FAILURE);
+  JSObject* obj = JS_NewArrayObject(cx, requests.length(), requests.begin());
+  if (!obj) {
+    return NS_ERROR_FAILURE;
+  }
 
+  aReturn->setObject(*obj);
   return NS_OK;
 }
 
