96d13b50c4ceeab789c55dd7566cd5c8b4d57e75
Wes Johnston <wjohnston@mozilla.com>
2013-04-23 10:30:47 -0700

    Bug 835456 - Cleanup whitespace. r=mfinkle

--- mobile/android/chrome/content/WebAppRT.js
+++ mobile/android/chrome/content/WebAppRT.js
@@ -134,7 +134,7 @@ let WebAppRT = {
     let target = event.target;
   
     // walk up the tree to find the nearest link tag
-    while(target && !(target instanceof HTMLAnchorElement)) {
+    while (target && !(target instanceof HTMLAnchorElement)) {
       target = target.parentNode;
     }
 
@@ -142,9 +142,7 @@ let WebAppRT = {
       return;
     }
   
-    let uri = Services.io.newURI(target.href,
-                                 target.ownerDocument.characterSet,
-                                 null);
+    let uri = Services.io.newURI(target.href, target.ownerDocument.characterSet, null);
   
     // Direct the URL to the browser.
     Cc["@mozilla.org/uriloader/external-protocol-service;1"].
