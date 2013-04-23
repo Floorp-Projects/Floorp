5fdd7f3441c82171e51e7c2509dc8dfee2150d22
Bill McCloskey <wmccloskey@mozilla.com>
2013-04-22 19:47:52 -0700

    Bug 862078 - Use an about:config preference to control multiprocess browsing, part 2 (r=felipe)

--- browser/base/content/browser-thumbnails.js
+++ browser/base/content/browser-thumbnails.js
@@ -31,6 +31,10 @@ let gBrowserThumbnails = {
   _tabEvents: ["TabClose", "TabSelect"],
 
   init: function Thumbnails_init() {
+    // Bug 863512 - Make page thumbnails work in electrolysis
+    if (gMultiProcessBrowser)
+      return;
+
     try {
       if (Services.prefs.getBoolPref("browser.pagethumbnails.capturing_disabled"))
         return;
@@ -51,6 +55,10 @@ let gBrowserThumbnails = {
   },
 
   uninit: function Thumbnails_uninit() {
+    // Bug 863512 - Make page thumbnails work in electrolysis
+    if (gMultiProcessBrowser)
+      return;
+
     PageThumbs.removeExpirationFilter(this);
     gBrowser.removeTabsProgressListener(this);
     Services.prefs.removeObserver(this.PREF_DISK_CACHE_SSL, this);
