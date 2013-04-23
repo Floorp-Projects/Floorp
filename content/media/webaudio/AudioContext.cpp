4393d3e222fb1173bd69c902ee9607fd8b1ab68f
Ehsan Akhgari <ehsan@mozilla.com>
2013-04-22 21:23:54 -0400

    Bug 864351 - Hold on to a SelfReference for ScriptProcessorNodes to make sure that they won't go away prematurely if they're used as source nodes; r=padenot

--- content/media/webaudio/AudioContext.cpp
+++ content/media/webaudio/AudioContext.cpp
@@ -141,6 +141,7 @@ AudioContext::CreateScriptProcessor(uint32_t aBufferSize,
   nsRefPtr<ScriptProcessorNode> scriptProcessor =
     new ScriptProcessorNode(this, aBufferSize, aNumberOfInputChannels,
                             aNumberOfOutputChannels);
+  mScriptProcessorNodes.AppendElement(scriptProcessor);
   return scriptProcessor.forget();
 }
 
@@ -246,6 +247,12 @@ AudioContext::UnregisterPannerNode(PannerNode* aNode)
 }
 
 void
+AudioContext::UnregisterScriptProcessorNode(ScriptProcessorNode* aNode)
+{
+  mScriptProcessorNodes.RemoveElement(aNode);
+}
+
+void
 AudioContext::UpdatePannerSource()
 {
   for (unsigned i = 0; i < mAudioBufferSourceNodes.Length(); i++) {
@@ -285,6 +292,11 @@ AudioContext::Shutdown()
   for (uint32_t i = 0; i < mAudioBufferSourceNodes.Length(); ++i) {
     ErrorResult rv;
     mAudioBufferSourceNodes[i]->Stop(0.0, rv);
+  }
+  // Stop all script processor nodes, to make sure that they release
+  // their self-references.
+  for (uint32_t i = 0; i < mScriptProcessorNodes.Length(); ++i) {
+    mScriptProcessorNodes[i]->Stop();
   }
 }
 
