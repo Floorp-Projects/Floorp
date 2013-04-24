56c1b648b3fa4141e5a8d43569e80e998e9d79d8
Ehsan Akhgari <ehsan@mozilla.com>
2013-04-24 12:13:45 -0400

    Bug 864709 - Part 2: Protect accesses to AudioNodeEngine::mNode using a lock; r=padenot

--- content/media/webaudio/DynamicsCompressorNode.cpp
+++ content/media/webaudio/DynamicsCompressorNode.cpp
@@ -140,7 +140,15 @@ private:
 
       NS_IMETHODIMP Run()
       {
-        nsRefPtr<DynamicsCompressorNode> node = static_cast<DynamicsCompressorNode*>(mStream->Engine()->Node());
+        nsRefPtr<DynamicsCompressorNode> node;
+        {
+          // No need to keep holding the lock for the whole duration of this
+          // function, since we're holding a strong reference to it, so if
+          // we can obtain the reference, we will hold the node alive in
+          // this function.
+          MutexAutoLock lock(mStream->Engine()->NodeMutex());
+          node = static_cast<DynamicsCompressorNode*>(mStream->Engine()->Node());
+        }
         if (node) {
           AudioParam* reduction = node->Reduction();
           reduction->CancelAllEvents();
