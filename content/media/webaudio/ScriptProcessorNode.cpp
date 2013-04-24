ad6c0922c29f141599e964c97696d2d87e717a38
Ehsan Akhgari <ehsan@mozilla.com>
2013-04-24 12:13:45 -0400

    Bug 864709 - Part 2: Protect accesses to AudioNodeEngine::mNode using a lock; r=padenot

--- content/media/webaudio/ScriptProcessorNode.cpp
+++ content/media/webaudio/ScriptProcessorNode.cpp
@@ -188,8 +188,10 @@ public:
                                  AudioChunk* aOutput,
                                  bool* aFinished) MOZ_OVERRIDE
   {
+    MutexAutoLock lock(NodeMutex());
+
     // If our node is dead, just output silence
-    if (!mNode) {
+    if (!Node()) {
       aOutput->SetNull(WEBAUDIO_BLOCK_SIZE);
       return;
     }
@@ -275,7 +277,15 @@ private:
           return NS_OK;
         }
 
-        nsRefPtr<ScriptProcessorNode> node = static_cast<ScriptProcessorNode*>(mStream->Engine()->Node());
+        nsRefPtr<ScriptProcessorNode> node;
+        {
+          // No need to keep holding the lock for the whole duration of this
+          // function, since we're holding a strong reference to it, so if
+          // we can obtain the reference, we will hold the node alive in
+          // this function.
+          MutexAutoLock lock(mStream->Engine()->NodeMutex());
+          node = static_cast<ScriptProcessorNode*>(mStream->Engine()->Node());
+        }
         if (!node) {
           return NS_OK;
         }
