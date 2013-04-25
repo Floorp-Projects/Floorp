fa1f714ff5e0aa96d14ca808d122684b3463699a
Dave Hunt <dhunt@mozilla.com>
2013-04-25 09:04:21 +0100

    Bug 842633 - Allow subclasses of MarionetteTestRunner to add their own command line options. r=jgriffin

--- testing/marionette/client/marionette/__init__.py
+++ testing/marionette/client/marionette/__init__.py
@@ -7,4 +7,4 @@ from marionette_test import MarionetteTestCase, CommonTestCase
 from marionette_touch import MarionetteTouchMixin
 from emulator import Emulator
 from runtests import MarionetteTestRunner
-
+from runtests import MarionetteTestOptions
