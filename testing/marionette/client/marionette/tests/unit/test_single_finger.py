2a0439520f6de319abe81704541847bd414aa944
Yiming Yang <yiyang@mozilla.com>
2013-04-26 11:14:50 -0700

    Bug 845925 - Add scroll/pinch actions using touch action chains to the python touch layer, r=mdas

--- testing/marionette/client/marionette/tests/unit/test_single_finger.py
+++ testing/marionette/client/marionette/tests/unit/test_single_finger.py
@@ -6,6 +6,7 @@ import time
 from marionette_test import MarionetteTestCase
 from marionette import Actions
 from errors import NoSuchElementException, MarionetteException
+from unittest import skip
 
 class testSingleFinger(MarionetteTestCase):
     def test_wait(self):
@@ -131,6 +132,7 @@ class testSingleFinger(MarionetteTestCase):
         time.sleep(10)
         self.assertEqual("ContextEnd", self.marionette.execute_script("return document.getElementById('mozLinkCopy').innerHTML;"))
 
+    @skip("Skipping due to Bug 865334")
     def test_long_press_fail(self):
         testTouch = self.marionette.absolute_url("testAction.html")
         self.marionette.navigate(testTouch)
@@ -143,4 +145,14 @@ class testSingleFinger(MarionetteTestCase):
         testTouch = self.marionette.absolute_url("testAction.html")
         self.marionette.navigate(testTouch)
         self.assertRaises(MarionetteException, self.marionette.send_mouse_event, "boolean")
+
+    def test_chain_flick(self):
+        testTouch = self.marionette.absolute_url("testAction.html")
+        self.marionette.navigate(testTouch)
+        button = self.marionette.find_element("id", "mozLinkScrollStart")
+        action = Actions(self.marionette)
+        action.flick(button, 0, 0, 0, -250).perform()
+        time.sleep(15)
+        self.assertEqual("End", self.marionette.execute_script("return document.getElementById('mozLinkScroll').innerHTML;"))
+        self.assertEqual("Start", self.marionette.execute_script("return document.getElementById('mozLinkScrollStart').innerHTML;"))
 
