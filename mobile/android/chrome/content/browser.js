72fd9c801393613fabcd5c7fba30475d9c4e31b6
Tetsuharu OHZEKI <saneyuki.s.snyk@gmail.com>
2013-04-26 10:10:59 -0400

    Bug 863242 - Add a preference setting "accept tracking me" to Fennec. r=mleibovic

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -138,6 +138,12 @@ const kDefaultCSSViewportHeight = 480;
 
 const kViewportRemeasureThrottle = 500;
 
+const kDoNotTrackPrefState = Object.freeze({
+  NO_PREF: "0",
+  DISALLOW_TRACKING: "1",
+  ALLOW_TRACKING: "2",
+});
+
 function dump(a) {
   Cc["@mozilla.org/consoleservice;1"].getService(Ci.nsIConsoleService).logStringMessage(a);
 }
@@ -972,6 +978,28 @@ var BrowserApp = {
             pref.value = MasterPassword.enabled;
             prefs.push(pref);
             continue;
+          /*
+           * Handle Do-not-track preference
+           *
+           * "privacy.donottrackheader" is not "real" pref name.
+           * This pref name is used in the setting menu, and
+           * this is passed when initializing the setting menu.
+           */
+          case "privacy.donottrackheader": {
+            pref.type = "string";
+
+            let enableDNT = Services.prefs.getBoolPref("privacy.donottrackheader.enabled");
+            if (!enableDNT) {
+              pref.value = kDoNotTrackPrefState.NO_PREF;
+            } else {
+              let dntState = Services.prefs.getIntPref("privacy.donottrackheader.value");
+              pref.value = (dntState === 0) ? kDoNotTrackPrefState.ALLOW_TRACKING :
+                                              kDoNotTrackPrefState.DISALLOW_TRACKING;
+            }
+
+            prefs.push(pref);
+            continue;
+          }
         }
 
         try {
@@ -1065,6 +1093,26 @@ var BrowserApp = {
         MasterPassword.removePassword(json.value);
       else
         MasterPassword.setPassword(json.value);
+      return;
+    } else if (json.name === "privacy.donottrackheader") {
+      // "privacy.donottrackheader" is not "real" pref name, it's used in the setting menu.
+      switch (json.value) {
+        case kDoNotTrackPrefState.NO_PREF:
+          // Don't tell anything about tracking me
+          Services.prefs.setBoolPref("privacy.donottrackheader.enabled", false);
+          Services.prefs.clearUserPref("privacy.donottrackheader.value");
+          break;
+        case kDoNotTrackPrefState.ALLOW_TRACKING:
+          // Accept tracking me
+          Services.prefs.setBoolPref("privacy.donottrackheader.enabled", true);
+          Services.prefs.setIntPref("privacy.donottrackheader.value", 0);
+          break;
+        case kDoNotTrackPrefState.DISALLOW_TRACKING:
+          // Not accept tracking me
+          Services.prefs.setBoolPref("privacy.donottrackheader.enabled", true);
+          Services.prefs.setIntPref("privacy.donottrackheader.value", 1);
+          break;
+      }
       return;
     } else if (json.name == SearchEngines.PREF_SUGGEST_ENABLED) {
       // Enabling or disabling suggestions will prevent future prompts
