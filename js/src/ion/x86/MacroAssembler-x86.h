671ed2fd5d370d124d630b1eaadd10e256f34e77
Jan de Mooij <jdemooij@mozilla.com>
2013-04-26 14:08:54 +0200

    Bug 863018 part 2 - Add JSShortString path back to ConcatStrings and LConcat. r=luke

--- js/src/ion/x86/MacroAssembler-x86.h
+++ js/src/ion/x86/MacroAssembler-x86.h
@@ -544,6 +544,10 @@ class MacroAssemblerX86 : public MacroAssemblerX86Shared
         testl(lhs, imm);
         j(cond, label);
     }
+    void branchTestPtr(Condition cond, const Address &lhs, Imm32 imm, Label *label) {
+        testl(Operand(lhs), imm);
+        j(cond, label);
+    }
     void decBranchPtr(Condition cond, const Register &lhs, Imm32 imm, Label *label) {
         subPtr(imm, lhs);
         j(cond, label);
