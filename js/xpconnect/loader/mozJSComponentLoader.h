a726d158470808e8c83caa2ee45f77fe8312052c
Jon Coppeard <jcoppeard@mozilla.com>
2013-04-26 18:50:18 +0100

    Bug 865308: GC: More rooting work in XPConnect r=bholley

--- js/xpconnect/loader/mozJSComponentLoader.h
+++ js/xpconnect/loader/mozJSComponentLoader.h
@@ -55,7 +55,7 @@ class mozJSComponentLoader : public mozilla::ModuleLoader,
 
     static mozJSComponentLoader* Get() { return sSelf; }
 
-    void NoteSubScript(JSScript* aScript, JSObject* aThisObject);
+    void NoteSubScript(JS::HandleScript aScript, JS::HandleObject aThisObject);
 
  protected:
     static mozJSComponentLoader* sSelf;
