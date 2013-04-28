7dced5c200eae9991c7e5527cd48ea0b01a67812
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2013-04-28 19:02:35 +0900

    Bug 859018 - Remove the MSVC exemption for FAIL_ON_WARNINGS in layout. r=dbaron

--- layout/tables/nsTableFrame.h
+++ layout/tables/nsTableFrame.h
@@ -108,7 +108,6 @@ private:
 class nsTableFrame : public nsContainerFrame
 {
 public:
-  NS_DECL_QUERYFRAME
   NS_DECL_FRAMEARENA_HELPERS
 
   /** nsTableOuterFrame has intimate knowledge of the inner table frame */
