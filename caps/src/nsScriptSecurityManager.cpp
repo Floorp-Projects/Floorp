91b18cb30d150d351cec0f4d8ca0c2ceb322223c
Bobby Holley <bobbyholley@gmail.com>
2013-04-29 11:16:24 -0700

    Bug 865729 - Remove nsIJSContextStack. r=gabor
    \o/

--- caps/src/nsScriptSecurityManager.cpp
+++ caps/src/nsScriptSecurityManager.cpp
@@ -262,10 +262,7 @@ JSContext *
 nsScriptSecurityManager::GetCurrentJSContext()
 {
     // Get JSContext from stack.
-    JSContext *cx;
-    if (NS_FAILED(sXPConnect->Peek(&cx)))
-        return nullptr;
-    return cx;
+    return sXPConnect->GetCurrentJSContext();
 }
 
 JSContext *
