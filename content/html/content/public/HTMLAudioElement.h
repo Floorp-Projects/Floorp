3b90907359afae1d03418b9f41326ca610d31008
Marco Chen <mchen@mozilla.com>
2013-04-29 14:45:13 +0800

    Bug 860150 - [Audio] Audio_Data API should join audio channel. r=kinetik

--- content/html/content/public/HTMLAudioElement.h
+++ content/html/content/public/HTMLAudioElement.h
@@ -6,6 +6,7 @@
 #ifndef mozilla_dom_HTMLAudioElement_h
 #define mozilla_dom_HTMLAudioElement_h
 
+#include "nsITimer.h"
 #include "nsIDOMHTMLAudioElement.h"
 #include "mozilla/dom/HTMLMediaElement.h"
 #include "mozilla/dom/TypedArray.h"
@@ -17,6 +18,7 @@ namespace mozilla {
 namespace dom {
 
 class HTMLAudioElement : public HTMLMediaElement,
+                         public nsITimerCallback,
                          public nsIDOMHTMLAudioElement
 {
 public:
@@ -38,6 +40,12 @@ public:
   // nsIDOMHTMLMediaElement
   using HTMLMediaElement::GetPaused;
   NS_FORWARD_NSIDOMHTMLMEDIAELEMENT(HTMLMediaElement::)
+
+  // nsIAudioChannelAgentCallback
+  NS_DECL_NSIAUDIOCHANNELAGENTCALLBACK
+
+  // NS_DECL_NSITIMERCALLBACK
+  NS_DECL_NSITIMERCALLBACK
 
   virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
   virtual nsresult SetAcceptHeader(nsIHttpChannel* aChannel);
@@ -68,6 +76,15 @@ public:
 protected:
   virtual JSObject* WrapNode(JSContext* aCx,
                              JS::Handle<JSObject*> aScope) MOZ_OVERRIDE;
+
+  // Update the audio channel playing state
+  virtual void UpdateAudioChannelPlayingState() MOZ_OVERRIDE;
+
+  // Due to that audio data API doesn't indicate the timing of pause or end,
+  // the timer is used to defer the timing of pause/stop after writing data.
+  nsCOMPtr<nsITimer> mDeferStopPlayTimer;
+  // To indicate mDeferStopPlayTimer is on fire or not.
+  bool mTimerActivated;
 };
 
 } // namespace dom
