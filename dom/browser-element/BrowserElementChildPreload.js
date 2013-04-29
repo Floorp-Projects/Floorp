71a46f183a5f7a6393213dc044b969b56a575d82
Justin Lebar <justin.lebar@gmail.com>
2013-04-29 18:12:11 -0400

    Bug 852847 - Part 5: Fix assertions caused by part 4. r=me

--- dom/browser-element/BrowserElementChildPreload.js
+++ dom/browser-element/BrowserElementChildPreload.js
@@ -326,7 +326,9 @@ BrowserElementChild.prototype = {
     let returnValue = win.modalReturnValue;
     delete win.modalReturnValue;
 
-    utils.leaveModalStateWithWindow(modalStateWin);
+    if (!this._shuttingDown) {
+      utils.leaveModalStateWithWindow(modalStateWin);
+    }
 
     debug("Leaving modal state (outerID=" + outerWindowID + ", " +
                                "innerID=" + innerWindowID + ")");
