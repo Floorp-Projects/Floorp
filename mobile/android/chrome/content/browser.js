f70654c652f4a91a8a71ebe5e1c3623c474cf107
Tetsuharu OHZEKI <saneyuki.s.snyk@gmail.com>
2013-04-30 22:10:01 -0400

    Bug 865250 - Part 4: Merge duplicated paths in switch statements. r=mleibovic

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -1033,9 +1033,6 @@ var BrowserApp = {
       // preferences to be actual booleans.
       switch (prefName) {
         case "network.cookie.cookieBehavior":
-          pref.type = "string";
-          pref.value = pref.value.toString();
-          break;
         case "font.size.inflation.minTwips":
           pref.type = "string";
           pref.value = pref.value.toString();
@@ -1120,9 +1117,6 @@ var BrowserApp = {
       // integers and strings to represent booleans. Here, we convert them back
       // to their actual types so we can store them.
       case "network.cookie.cookieBehavior":
-        json.type = "int";
-        json.value = parseInt(json.value);
-        break;
       case "font.size.inflation.minTwips":
         json.type = "int";
         json.value = parseInt(json.value);
