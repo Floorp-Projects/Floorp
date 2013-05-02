e6e0e0be60c4120d51e89f3064a287d3ceba3bda
Dimi Lee <dlee@mozilla.com>
2013-05-02 18:02:33 +0800

    Bug 834573 - B2G Wifi: IP and default route still exist after forget network. r=vchang

--- dom/system/gonk/NetworkManager.js
+++ dom/system/gonk/NetworkManager.js
@@ -259,6 +259,10 @@ NetworkManager.prototype = {
                 network.type == Ci.nsINetworkInterface.NETWORK_TYPE_MOBILE_SUPL) {
               this.removeHostRoute(network);
             }
+            // Remove routing table in /proc/net/route
+            if (network.type == Ci.nsINetworkInterface.NETWORK_TYPE_WIFI) {
+              this.resetRoutingTable(this._activeInfo);
+            }
             // Abort ongoing captive portal detection on the wifi interface
             CaptivePortalDetectionHelper.notify(CaptivePortalDetectionHelper.EVENT_DISCONNECT, network);
             this.setAndConfigureActive();
@@ -354,6 +358,9 @@ NetworkManager.prototype = {
   active: null,
   _overriddenActive: null,
 
+  // Clone network info so we can still get information when network is disconnected
+  _activeInfo: null,
+
   overrideActive: function overrideActive(network) {
     this._overriddenActive = network;
     this.setAndConfigureActive();
@@ -442,6 +449,7 @@ NetworkManager.prototype = {
 
     // Find a suitable network interface to activate.
     this.active = null;
+    this._activeInfo = Object.create(null);
     for each (let network in this.networkInterfaces) {
       if (network.state != Ci.nsINetworkInterface.NETWORK_STATE_CONNECTED) {
         continue;
@@ -450,6 +458,7 @@ NetworkManager.prototype = {
         defaultDataNetwork = network;
       }
       this.active = network;
+      this._activeInfo = {name:network.name, ip:network.ip, netmask:network.netmask};
       if (network.type == this.preferredNetworkType) {
         debug("Found our preferred type of network: " + network.name);
         break;
@@ -474,6 +483,16 @@ NetworkManager.prototype = {
     if (this._manageOfflineStatus) {
       Services.io.offline = !this.active;
     }
+  },
+
+  resetRoutingTable: function resetRoutingTable(network) {
+    let options = {
+      cmd: "removeNetworkRoute",
+      ifname: network.name,
+      ip : network.ip,
+      netmask: network.netmask,
+    };
+    this.worker.postMessage(options);
   },
 
   setDefaultRouteAndDNS: function setDefaultRouteAndDNS(oldInterface) {
