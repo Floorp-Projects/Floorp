a852984d89f3fcbcbae3be8bb0366968a0c8fc90
Bobby Holley <bobbyholley@gmail.com>
2013-05-03 14:47:10 -0700

    Bug 829872 - Fix up tests that depend on contentDocument being non-null. r=imelven
    These almost universally depend on some sort of special privileges, so I don't
    think they're representative of any use-cases we might find on the web.

--- dom/plugins/test/mochitest/pluginstream.js
+++ dom/plugins/test/mochitest/pluginstream.js
@@ -5,7 +5,10 @@
     var embed = document.getElementsByTagName('embed')[0];
     if (undefined === embed)
       embed = document.getElementsByTagName('object')[0];
-    try {
+
+    // In the file:// URI case, this ends up being cross-origin.
+    // Skip these checks in that case.
+    if (testframe.contentDocument) {
       var content = testframe.contentDocument.body.innerHTML;
       if (!content.length)
         return;
@@ -14,7 +17,7 @@
           embed.getAttribute("geturl") ||
           embed.getAttribute("geturlnotify") ||
           embed.getAttribute("data");
-      
+
       var req = new XMLHttpRequest();
       req.open('GET', filename, false);
       req.overrideMimeType('text/plain; charset=x-user-defined');
@@ -23,18 +26,7 @@
       is(content, req.responseText.replace(/\r\n/g, "\n"),
          "content doesn't match");
     }
-    catch (e) {
-      // For file:// url's, such as we get during the NPN_NewStream test,
-      // attempting to access the frame content throws an exception.
-      // For this case, we just verify the onload event is called.
 
-      // XXXbent Need to fix the underlying change to this message here! Should
-      //         the message include 'file://'? Message needs to be localized
-      //         and include both origins in the error console too!
-      ok(e.message.indexOf("Permission denied") > -1 &&
-         e.message.indexOf("access property 'body'") > -1,
-         "Unexpected exception thrown: " + e.message);
-    }
     is(embed.getError(), "pass", "plugin reported error");
     SimpleTest.finish();
   }
