713f890c2bc7db4b6b016c2ccc0fd22d7def88e1
Jon Coppeard <jcoppeard@mozilla.com>
2013-05-03 13:32:04 +0100

    Bug 868037 - GC: Fix more rooting analysis false positives r=sfink DONTBUILD

--- js/src/devtools/rootAnalysis/computeGCTypes.js
+++ js/src/devtools/rootAnalysis/computeGCTypes.js
@@ -19,7 +19,7 @@ function processCSU(csu, body)
         if (type.Kind == "CSU") {
             // Ignore nesting in classes which are AutoGCRooters. We only consider
             // types with fields that may not be properly rooted.
-            if (type.Name == "JS::AutoGCRooter")
+            if (type.Name == "JS::AutoGCRooter" || type.Name == "JS::CustomAutoRooter")
                 return;
             addNestedStructure(csu, type.Name);
         }
