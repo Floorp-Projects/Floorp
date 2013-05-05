f936e5198e9ec9aeac3068cf5521cc4259d5f729
Mike Hommey <mh+mozilla@glandium.org>
2013-05-05 10:16:25 +0200

    Bug 777379 - Set .DEFAULT_GOAL unconditionally, override with OVERRIDE_DEFAULT_GOAL, and fix pymake to be on par with GNU make when handling .DEFAULT_GOAL. r=gps

--- build/pymake/tests/default-goal.mk
+++ build/pymake/tests/default-goal.mk
@@ -1,7 +1,8 @@
 not-default:
-	@echo TEST-FAIL
+	@echo TEST-FAIL did not run default rule
 
 default:
-	@echo TEST-PASS
+	@echo $(if $(filter not-default,$(INTERMEDIATE_DEFAULT_GOAL)),TEST-PASS,TEST-FAIL .DEFAULT_GOAL not set by $(MAKE))
 
+INTERMEDIATE_DEFAULT_GOAL := $(.DEFAULT_GOAL)
 .DEFAULT_GOAL := default
