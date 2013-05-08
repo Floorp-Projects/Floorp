28aea6e717db7c2300496211f747aa4acbfdfaa9
Terrence Cole <terrence@mozilla.com>
2013-05-08 11:43:32 -0700

    Bug 869222 - Allow GC things embedded in IonScript to move. r=dvander
    IonScripts normally live as long as their owner JSScript; however, they can
    occasionally get destroyed outside the context of a GC by FinishInvalidationOf.
    Because of this case, we cannot use the normal store buffer to guard them.
    Instead we use the generic buffer to mark the owner script, which will mark the
    IonScript's fields, if it is still alive.

--- js/src/ion/IonCode.h
+++ js/src/ion/IonCode.h
@@ -149,10 +149,10 @@ struct IonScript
 {
   private:
     // Code pointer containing the actual method.
-    HeapPtr<IonCode> method_;
+    EncapsulatedPtr<IonCode> method_;
 
     // Deoptimization table used by this method.
-    HeapPtr<IonCode> deoptTable_;
+    EncapsulatedPtr<IonCode> deoptTable_;
 
     // Entrypoint for OSR, or NULL.
     jsbytecode *osrPc_;
@@ -255,8 +255,8 @@ struct IonScript
     SnapshotOffset *bailoutTable() {
         return (SnapshotOffset *) &bottomBuffer()[bailoutTable_];
     }
-    HeapValue *constants() {
-        return (HeapValue *) &bottomBuffer()[constantTable_];
+    EncapsulatedValue *constants() {
+        return (EncapsulatedValue *) &bottomBuffer()[constantTable_];
     }
     const SafepointIndex *safepointIndices() const {
         return const_cast<IonScript *>(this)->safepointIndices();
@@ -407,7 +407,7 @@ struct IonScript
     size_t sizeOfIncludingThis(JSMallocSizeOfFun mallocSizeOf) const {
         return mallocSizeOf(this);
     }
-    HeapValue &getConstant(size_t index) {
+    EncapsulatedValue &getConstant(size_t index) {
         JS_ASSERT(index < numConstants());
         return constants()[index];
     }
