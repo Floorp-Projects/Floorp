1269786058b603bd1effffde3e17f61168abc98e
Ryan VanderMeulen <ryanvm@gmail.com>
2013-05-07 22:05:30 -0400

    Backed out 4 changesets (bug 869178, bug 848490, bug 736276) for reftest crashes.
    Backed out changeset f2d0107e85ec (bug 848490)
    Backed out changeset f9a17848125a (bug 869178)
    Backed out changeset c6904473984e (bug 736276)
    Backed out changeset 84bdd96375ba (bug 736276)

---
+++ gfx/2d/ScaledFontFreetype.h
@@ -0,0 +1,24 @@
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef MOZILLA_GFX_SCALEDFONTFREETYPE_H_
+#define MOZILLA_GFX_SCALEDFONTFREETYPE_H_
+
+#include "ScaledFontBase.h"
+
+namespace mozilla {
+namespace gfx {
+
+class ScaledFontFreetype : public ScaledFontBase
+{
+public:
+
+  ScaledFontFreetype(FontOptions* aFont, Float aSize);
+};
+
+}
+}
+
+#endif /* MOZILLA_GFX_SCALEDFONTFREETYPE_H_ */
