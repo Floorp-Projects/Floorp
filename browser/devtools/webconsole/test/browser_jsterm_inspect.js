2442432a7e7056cc10d16d4a9c4942caaf8af909
Mihai Sucan <mihai.sucan@gmail.com>
2013-05-09 13:36:56 +0300

    Bug 869981 - Bring back inspect() jsterm helper function; r=me a=robcee

---
+++ browser/devtools/webconsole/test/browser_jsterm_inspect.js
@@ -0,0 +1,35 @@
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+// Check that the inspect() jsterm helper function works.
+
+function test()
+{
+  const TEST_URI = "data:text/html;charset=utf8,<p>hello bug 869981";
+
+  addTab(TEST_URI);
+  browser.addEventListener("load", function onLoad() {
+    browser.removeEventListener("load", onLoad, true);
+    openConsole(null, consoleOpened);
+  }, true);
+
+  function consoleOpened(hud)
+  {
+    content.wrappedJSObject.testProp = "testValue";
+
+    hud.jsterm.once("variablesview-fetched", onObjFetch);
+    hud.jsterm.execute("inspect(window)");
+  }
+
+  function onObjFetch(aEvent, aVar)
+  {
+    ok(aVar._variablesView, "variables view object");
+
+    findVariableViewProperties(aVar, [
+      { name: "testProp", value: "testValue" },
+      { name: "document", value: "[object HTMLDocument]" },
+    ], { webconsole: hud }).then(finishTest);
+  }
+}
