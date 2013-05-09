cd3d8f0642c1720d23f95a81b4c52e5314d21a8f
Mike Conley <mconley@mozilla.com>
2013-05-09 12:00:33 -0400

    Bug 867585 - Create panelmultiview binding for panels with subviews. r=Unfocused.

--- browser/components/customizableui/content/panelUI.js
+++ browser/components/customizableui/content/panelUI.js
@@ -17,25 +17,12 @@ const PanelUI = {
    */
   get kElements() {
     return {
-      clickCapturer: "PanelUI-clickCapturer",
-      container: "PanelUI-container",
       contents: "PanelUI-contents",
       mainView: "PanelUI-mainView",
-      mainViewSpring: "PanelUI-mainView-spring",
+      multiView: "PanelUI-multiView",
       menuButton: "PanelUI-menu-button",
       panel: "PanelUI-popup",
-      subViews: "PanelUI-subViews",
-      viewStack: "PanelUI-viewStack"
     };
-  },
-
-  /**
-   * Returns whether we're in subview mode. This can return true even if
-   * the transition to subview mode is not yet complete.
-   **/
-  get _showingSubView() {
-    return (this.viewStack.hasAttribute("view") &&
-            this.viewStack.getAttribute("view") == "subview");
   },
 
   init: function() {
@@ -52,31 +39,12 @@ const PanelUI = {
     for (let event of this.kEvents) {
       this.panel.addEventListener(event, this);
     }
-
-    this.clickCapturer.addEventListener("click", this._onCapturerClick,
-                                        true);
-
-    var self = this;
-    this.subViews.addEventListener("overflow", function() {
-      // Resize the subview on the next tick.
-      Services.tm.currentThread.dispatch(self._syncContainerWithSubView.bind(self),
-        Ci.nsIThread.DISPATCH_NORMAL);
-    });
-
-    // Get a MutationObserver ready to react to subview size changes. We
-    // only attach this MutationObserver when a subview is being displayed.
-    this._subViewObserver = new MutationObserver(function(aMutations) {
-      this._syncContainerWithSubView();
-    }.bind(this));
   },
 
   uninit: function() {
     for (let event of this.kEvents) {
       this.panel.removeEventListener(event, this);
     }
-
-    this.clickCapturer.removeEventListener("click", this._onCapturerClick,
-                                           true);
   },
 
   /**
@@ -88,8 +56,7 @@ const PanelUI = {
    *        The mainView node to put back into place.
    */
   replaceMainView: function(aMainView) {
-    this.viewStack.insertBefore(aMainView, this.viewStack.firstChild);
-    this._syncContainerWithMainView();
+    this.multiView.insertBefore(aMainView, this.multiView.firstChild);
   },
 
   /**
@@ -104,6 +71,8 @@ const PanelUI = {
     if (this.panel.state == "open") {
       this.hide();
     } else if (this.panel.state == "closed") {
+      this.ensureRegistered();
+
       let anchor = aEvent.target;
       let iconAnchor =
         document.getAnonymousElementByAttribute(anchor, "class",
@@ -122,17 +91,12 @@ const PanelUI = {
   handleEvent: function(aEvent) {
     switch (aEvent.type) {
       case "popupshowing":
-        this.ensureRegistered();
-        let cstyle = window.getComputedStyle(this.viewStack, null);
-        this.container.style.height = cstyle.getPropertyValue("height");
-        this.container.style.width = cstyle.getPropertyValue("width");
         // Fall through
       case "popupshown":
         // Fall through
       case "popuphiding":
         // Fall through
       case "popuphidden": {
-        this.showMainView();
         this._updatePanelButton(aEvent.target);
         break;
       }
@@ -154,22 +118,7 @@ const PanelUI = {
    * in that view.
    */
   showMainView: function() {
-    // Are we showing a subview? If so, fire the ViewHiding event on it.
-    if (this._showingSubView) {
-      let viewNode = this._currentSubView;
-      let evt = document.createEvent("CustomEvent");
-      evt.initCustomEvent("ViewHiding", true, true, viewNode);
-      viewNode.dispatchEvent(evt);
-
-      viewNode.removeAttribute("current");
-      this._currentSubView = null;
-      this._subViewObserver.disconnect();
-    }
-
-    this.viewStack.setAttribute("view", "main");
-    this._syncContainerWithMainView();
-
-    this._shiftMainView();
+    this.multiView.showMainView();
   },
 
   /**
@@ -192,46 +141,7 @@ const PanelUI = {
     }
 
     if (aPlacementArea == CustomizableUI.AREA_PANEL) {
-      // The subview can get moved if the widget is not in the
-      // panel. For robustness, check that the subview is in the
-      // right place and move it if necessary.
-      if (viewNode.parentElement != this.subViews) {
-        this.subViews.appendChild(viewNode);
-      }
-
-      let oldHeight = this.mainView.clientHeight;
-      viewNode.setAttribute("current", true);
-      this._currentSubView = viewNode;
-
-      // Emit the ViewShowing event so that the widget definition has a chance
-      // to lazily populate the subview with things.
-      let evt = document.createEvent("CustomEvent");
-      evt.initCustomEvent("ViewShowing", true, true, viewNode);
-      viewNode.dispatchEvent(evt);
-
-      this.viewStack.setAttribute("view", "subview");
-      this.mainViewSpring.style.height = this.subViews.scrollHeight - oldHeight + "px";
-      this.container.style.height = this.subViews.scrollHeight + "px";
-
-      // Now we have to transition to transition the panel. There are a few parts
-      // to this:
-      //
-      // 1) The main view content gets shifted so that the center of the anchor
-      //    node is at the left-most edge of the panel.
-      // 2) The subview deck slides in so that it takes up almost all of the
-      //    panel.
-      // 3) If the subview is taller then the main panel contents, then the panel
-      //    must grow to meet that new height. Otherwise, it must shrink.
-      //
-      // All three of these actions make use of CSS transformations, so they
-      // should all occur simultaneously.
-      this._shiftMainView(aAnchor);
-      this._subViewObserver.observe(viewNode, {
-        attributes: true,
-        characterData: true,
-        childList: true,
-        subtree: true
-      });
+      this.multiView.showSubView(aViewId, aAnchor);
     } else {
       // Emit the ViewShowing event so that the widget definition has a chance
       // to lazily populate the subview with things.
@@ -239,41 +149,19 @@ const PanelUI = {
       evt.initCustomEvent("ViewShowing", true, true, viewNode);
       viewNode.dispatchEvent(evt);
 
-      let panel = document.createElement("panel");
-      panel.appendChild(viewNode);
-      panel.setAttribute("type", "arrow");
-      panel.setAttribute("id", "customizationui-widget-panel");
-      document.getElementById(CustomizableUI.AREA_NAVBAR).appendChild(panel);
-      panel.addEventListener("popuphidden", function() {
-        this.subViews.appendChild(viewNode);
-        panel.parentElement.removeChild(panel);
-      });
+      let tempPanel = document.createElement("panel");
+      tempPanel.appendChild(viewNode);
+      tempPanel.setAttribute("type", "arrow");
+      tempPanel.setAttribute("id", "customizationui-widget-panel");
+      document.getElementById(CustomizableUI.AREA_NAVBAR).appendChild(tempPanel);
+      tempPanel.addEventListener("popuphidden", function panelRemover() {
+        tempPanel.removeEventListener("popuphidden", panelRemover);
+        this.multiView.appendChild(viewNode);
+        tempPanel.parentElement.removeChild(tempPanel);
+      }.bind(this));
 
-      panel.openPopup(aAnchor, "bottomcenter topright");
+      tempPanel.openPopup(aAnchor, "bottomcenter topright");
     }
-  },
-
-  /**
-   * Ensures that the container has the same height as the main view, minus
-   * any spring height. This is usually done after customization completes, or
-   * when switching back from a subview.
-   */
-  _syncContainerWithMainView: function() {
-    let springHeight = this.mainViewSpring.getBoundingClientRect().height;
-    this.container.style.height = (this.mainView.scrollHeight - springHeight) + "px";
-    this.mainViewSpring.style.height = "";
-  },
-
-  /**
-   * Ensures that the container has the same height as the subview holder.
-   * This is usually done when switching to a subview, or if a subview changes
-   * in size.
-   */
-  _syncContainerWithSubView: function() {
-    let springHeight = this.mainViewSpring.getBoundingClientRect().height;
-    let mainViewHeight = this.mainView.clientHeight - springHeight;
-    this.container.style.height = this.subViews.scrollHeight + "px";
-    this.mainViewSpring.style.height = (this.subViews.scrollHeight - mainViewHeight) + "px";
   },
 
   /**
@@ -283,44 +171,5 @@ const PanelUI = {
   _updatePanelButton: function() {
     this.menuButton.open = this.panel.state == "open" ||
                            this.panel.state == "showing";
-  },
-
-  /**
-   * Used to keep a reference to the toolbarbutton that the subview spawned from.
-   */
-  anchorElement: null,
-
-  /**
-   * If aAnchor is not null, this shifts the main view content so that it is
-   * partially clipped by the panel boundaries, placing the center of aAnchor
-   * at the clipping edge. If aAnchor is undefined or null, the main view
-   * content is shifted back to its original position.
-   */
-  _shiftMainView: function(aAnchor) {
-    if (aAnchor) {
-      // We need to find the left edge of the anchor, relative to the main
-      // panel. Then we need to add half the width of the anchor. This is the
-      // target that we need to transition to.
-      let anchorRect = aAnchor.getBoundingClientRect();
-      let mainViewRect = this.mainView.getBoundingClientRect();
-      let leftEdge = anchorRect.left - mainViewRect.left;
-      let center = aAnchor.clientWidth / 2;
-      let target = leftEdge + center;
-      this.mainView.style.transform = "translateX(-" + target + "px)";
-      aAnchor.classList.add("panelui-mainview-anchor");
-    } else {
-      this.mainView.style.transform = "";
-      if (this.anchorElement)
-        this.anchorElement.classList.remove("panelui-mainview-anchor");
-    }
-    this.anchorElement = aAnchor;
-  },
-
-  /**
-   * When viewing a subview, any click on the main view gets intercepted by our
-   * capturer, and then we switch to the main view.
-   */
-  _onCapturerClick: function(aEvent) {
-    PanelUI.showMainView();
-  },
+  }
 };
