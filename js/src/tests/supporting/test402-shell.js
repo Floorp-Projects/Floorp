dfa20a663321f37597e540168f8dd85a771725e6
Jeff Walden <jwalden@mit.edu>
2013-05-10 15:30:17 -0700

    Bug 496923 - Implement a script which clones the test262 repository and copies selected subsets of the tests into the jstests suite so that we can run them on tinderbox.  r=terrence

---
+++ js/src/tests/supporting/test402-shell.js
@@ -0,0 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/*
+ * Test402 tests all pass unless they throw, and there are no @negative tests.
+ * Once Test262 includes @negative support, and this call in test262-shell.js is
+ * removed, this'll need to be uncommented.
+ */
+//testPassesUnlessItThrows();
