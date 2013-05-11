74f00f4aa351569687e66483d45b3311768c026a
L. David Baron <dbaron@dbaron.org>
2013-05-10 17:35:08 -0700

    Bug 828312 patch 4.5:  Pass the same aMinChange to the top level of a ReResolveStyleContext that we would for the recursive calls, rather than including previous continutaions in aMinChange.  r=bzbarsky

--- layout/base/nsFrameManager.cpp
+++ layout/base/nsFrameManager.cpp
@@ -1645,8 +1645,6 @@ nsFrameManager::ComputeStyleChangeFor(nsIFrame          *aFrame,
     aChangeList->AppendChange(aFrame, content, aMinChange);
   }
 
-  nsChangeHint topLevelChange = aMinChange;
-
   nsIFrame* frame = aFrame;
   nsIFrame* frame2 = aFrame;
 
@@ -1672,16 +1670,15 @@ nsFrameManager::ComputeStyleChangeFor(nsIFrame          *aFrame,
       // Inner loop over next-in-flows of the current frame
       nsChangeHint frameChange =
         ReResolveStyleContext(GetPresContext(), frame, nullptr,
-                              aChangeList, topLevelChange, nsChangeHint(0),
+                              aChangeList, aMinChange, nsChangeHint(0),
                               aRestyleDescendants ?
                                 eRestyle_Subtree : eRestyle_Self,
                               aRestyleTracker,
                               eSendAllNotifications,
                               visibleKidsOfHiddenElement,
                               treeMatchContext);
-      NS_UpdateHint(topLevelChange, frameChange);
 
-      if (topLevelChange & nsChangeHint_ReconstructFrame) {
+      if (frameChange & nsChangeHint_ReconstructFrame) {
         // If it's going to cause a framechange, then don't bother
         // with the continuations or special siblings since they'll be
         // clobbered by the frame reconstruct anyway.
