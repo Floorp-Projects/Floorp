16d720e724407ca4393aa866ffc06ba2fbfc2efc
Randell Jesup <rjesup@jesup.org>
2013-05-12 22:16:40 -0400

    Bug 864255: Move more of PeerConnectionMedia shutdown to occur synchronously r=ekr

--- media/webrtc/signaling/src/media-conduit/MediaConduitInterface.h
+++ media/webrtc/signaling/src/media-conduit/MediaConduitInterface.h
@@ -167,6 +167,7 @@ public:
    * and attaches the new to the Conduit.
    */
   virtual MediaConduitErrorCode AttachRenderer(RefPtr<VideoRenderer> aRenderer) = 0;
+  virtual void DetachRenderer() = 0;
 
   /**
    * Function to deliver a capture video frame for encoding and transport
