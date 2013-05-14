145c9d0ad6f9d8ee36ed4470c1e90d4e042ff13d
Bobby Holley <bobbyholley@gmail.com>
2013-05-14 10:16:52 -0700

    Bug 871301 - Remove JS_GetGlobalObject from nsCrypto and nsDirectoryViewer. r=bz

--- security/manager/ssl/src/nsCrypto.cpp
+++ security/manager/ssl/src/nsCrypto.cpp
@@ -1916,7 +1916,7 @@ nsCrypto::GenerateCRMFRequest(nsIDOMCRMFObject** aReturn)
   JSAutoByteString jsCallback(cx, jsString);
   NS_ENSURE_TRUE(!!jsCallback, NS_ERROR_OUT_OF_MEMORY);
 
-  nrv = xpc->WrapNative(cx, ::JS_GetGlobalObject(cx),
+  nrv = xpc->WrapNative(cx, JS_GetGlobalForScopeChain(cx),
                         static_cast<nsIDOMCrypto *>(this),
                         NS_GET_IID(nsIDOMCrypto), getter_AddRefs(holder));
   NS_ENSURE_SUCCESS(nrv, nrv);
@@ -2176,14 +2176,9 @@ NS_IMETHODIMP
 nsCryptoRunnable::Run()
 {
   nsNSSShutDownPreventionLock locker;
-  JSContext *cx = m_args->m_cx;
-
+  AutoPushJSContext cx(m_args->m_cx);
   JSAutoRequest ar(cx);
   JSAutoCompartment ac(cx, m_args->m_scope);
-
-  // make sure the right context is on the stack. must not return w/out popping
-  nsCxPusher pusher;
-  pusher.Push(cx);
 
   JSBool ok =
     JS_EvaluateScriptForPrincipals(cx, m_args->m_scope,
