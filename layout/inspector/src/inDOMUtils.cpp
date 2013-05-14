5aae7587dd4421d673caacca6c51e14c75ba07a8
Boris Zbarsky <bzbarsky@mit.edu>
2013-05-14 14:20:58 -0400

    Bug 870845.  Correctly check for parse-inaccessible properties in inDOMUtils::GetCSSPropertyNames.  r=heycam

--- layout/inspector/src/inDOMUtils.cpp
+++ layout/inspector/src/inDOMUtils.cpp
@@ -389,8 +389,8 @@ inDOMUtils::GetCSSPropertyNames(uint32_t aFlags, uint32_t* aCount,
   // we've put into props so far.
   uint32_t prop = 0, propCount = 0;
   for ( ; prop < eCSSProperty_COUNT_no_shorthands; ++prop) {
-    if (!nsCSSProps::PropHasFlags(nsCSSProperty(prop),
-                                  CSS_PROPERTY_PARSE_INACCESSIBLE)) {
+    if (nsCSSProps::PropertyParseType(nsCSSProperty(prop)) !=
+        CSS_PROPERTY_PARSE_INACCESSIBLE) {
       DO_PROP(prop);
     }
   }
