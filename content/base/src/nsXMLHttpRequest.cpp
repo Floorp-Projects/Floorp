1229379a5d290d5acaa2f5902702d5f2235ff27c
Olli Pettay <Olli.Pettay@helsinki.fi>
2013-05-15 14:48:55 +0300

    Bug 848535, simplify js object holding in XHR and FileReader, r=mccr8

--- content/base/src/nsXMLHttpRequest.cpp
+++ content/base/src/nsXMLHttpRequest.cpp
@@ -324,15 +324,17 @@ nsXMLHttpRequest::~nsXMLHttpRequest()
   NS_ABORT_IF_FALSE(!(mState & XML_HTTP_REQUEST_SYNCLOOPING), "we rather crash than hang");
   mState &= ~XML_HTTP_REQUEST_SYNCLOOPING;
 
+  mResultJSON = JSVAL_VOID;
+  mResultArrayBuffer = nullptr;
+  NS_DROP_JS_OBJECTS(this, nsXMLHttpRequest);
+
   nsLayoutStatics::Release();
 }
 
 void
 nsXMLHttpRequest::RootJSResultObjects()
 {
-  nsContentUtils::PreserveWrapper(
-    static_cast<EventTarget*>(
-      static_cast<nsDOMEventTargetHelper*>(this)), this);
+  NS_HOLD_JS_OBJECTS(this, nsXMLHttpRequest);
 }
 
 /**
