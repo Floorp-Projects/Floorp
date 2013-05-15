91cdfe81aa0e6a2692ea4aa697308505c8569a2a
Olli Pettay <Olli.Pettay@helsinki.fi>
2013-05-15 14:35:41 +0300

    Bug 857883, don't assert about readyState when not needed, r=bz
    --HG--
    extra : rebase_source : 1dc9dc6bae5907cac3f2b8b1c31d79e7bcbb68f2

--- content/base/src/nsContentSink.cpp
+++ content/base/src/nsContentSink.cpp
@@ -1440,7 +1440,8 @@ void
 nsContentSink::DidBuildModelImpl(bool aTerminated)
 {
   if (mDocument) {
-    MOZ_ASSERT(mDocument->GetReadyStateEnum() ==
+    MOZ_ASSERT(aTerminated ||
+               mDocument->GetReadyStateEnum() ==
                nsIDocument::READYSTATE_LOADING, "Bad readyState");
     mDocument->SetReadyStateInternal(nsIDocument::READYSTATE_INTERACTIVE);
   }
