4cc988f54e2ccefad9b0216123aeb7591260d6e3
Masayuki Nakano <masayuki@d-toybox.com>
2013-05-16 13:42:20 +0900

    Bug 865976 Fix bustage of SeaMonkey on official build environment r=karlt

--- widget/gtk2/compat/gdk/gdkkeysyms.h
+++ widget/gtk2/compat/gdk/gdkkeysyms.h
@@ -47,6 +47,198 @@
 #define GDK_C_H                 0xFEA5
 #endif
 
+#ifndef GDK_MonBrightnessUp
+#define GDK_MonBrightnessUp     0x1008FF02
+#endif
+
+#ifndef GDK_MonBrightnessDown
+#define GDK_MonBrightnessDown   0x1008FF03
+#endif
+
+#ifndef GDK_AudioLowerVolume
+#define GDK_AudioLowerVolume    0x1008FF11
+#endif
+
+#ifndef GDK_AudioMute
+#define GDK_AudioMute           0x1008FF12
+#endif
+
+#ifndef GDK_AudioRaiseVolume
+#define GDK_AudioRaiseVolume    0x1008FF13
+#endif
+
+#ifndef GDK_AudioPlay
+#define GDK_AudioPlay           0x1008FF14
+#endif
+
+#ifndef GDK_AudioStop
+#define GDK_AudioStop           0x1008FF15
+#endif
+
+#ifndef GDK_AudioPrev
+#define GDK_AudioPrev           0x1008FF16
+#endif
+
+#ifndef GDK_AudioNext
+#define GDK_AudioNext           0x1008FF17
+#endif
+
+#ifndef GDK_HomePage
+#define GDK_HomePage            0x1008FF18
+#endif
+
+#ifndef GDK_Mail
+#define GDK_Mail                0x1008FF19
+#endif
+
+#ifndef GDK_Search
+#define GDK_Search              0x1008FF1B
+#endif
+
+#ifndef GDK_AudioRecord
+#define GDK_AudioRecord         0x1008FF1C
+#endif
+
+#ifndef GDK_Back
+#define GDK_Back                0x1008FF26
+#endif
+
+#ifndef GDK_Forward
+#define GDK_Forward             0x1008FF27
+#endif
+
+#ifndef GDK_Stop
+#define GDK_Stop                0x1008FF28
+#endif
+
+#ifndef GDK_Refresh
+#define GDK_Refresh             0x1008FF29
+#endif
+
+#ifndef GDK_PowerOff
+#define GDK_PowerOff            0x1008FF2A
+#endif
+
+#ifndef GDK_Eject
+#define GDK_Eject               0x1008FF2C
+#endif
+
+#ifndef GDK_AudioPause
+#define GDK_AudioPause          0x1008FF31
+#endif
+
+#ifndef GDK_BrightnessAdjust
+#define GDK_BrightnessAdjust    0x1008FF3B
+#endif
+
+#ifndef GDK_AudioRewind
+#define GDK_AudioRewind         0x1008FF3E
+#endif
+
+#ifndef GDK_Launch0
+#define GDK_Launch0             0x1008FF40
+#endif
+
+#ifndef GDK_Launch1
+#define GDK_Launch1             0x1008FF41
+#endif
+
+#ifndef GDK_Launch2
+#define GDK_Launch2             0x1008FF42
+#endif
+
+#ifndef GDK_Launch3
+#define GDK_Launch3             0x1008FF43
+#endif
+
+#ifndef GDK_Launch4
+#define GDK_Launch4             0x1008FF44
+#endif
+
+#ifndef GDK_Launch5
+#define GDK_Launch5             0x1008FF45
+#endif
+
+#ifndef GDK_Launch6
+#define GDK_Launch6             0x1008FF46
+#endif
+
+#ifndef GDK_Launch7
+#define GDK_Launch7             0x1008FF47
+#endif
+
+#ifndef GDK_Launch8
+#define GDK_Launch8             0x1008FF48
+#endif
+
+#ifndef GDK_Launch9
+#define GDK_Launch9             0x1008FF49
+#endif
+
+#ifndef GDK_LaunchA
+#define GDK_LaunchA             0x1008FF4A
+#endif
+
+#ifndef GDK_LaunchB
+#define GDK_LaunchB             0x1008FF4B
+#endif
+
+#ifndef GDK_LaunchC
+#define GDK_LaunchC             0x1008FF4C
+#endif
+
+#ifndef GDK_LaunchD
+#define GDK_LaunchD             0x1008FF4D
+#endif
+
+#ifndef GDK_LaunchE
+#define GDK_LaunchE             0x1008FF4E
+#endif
+
+#ifndef GDK_LaunchF
+#define GDK_LaunchF             0x1008FF4F
+#endif
+
+#ifndef GDK_Copy
+#define GDK_Copy                0x1008FF57
+#endif
+
+#ifndef GDK_Cut
+#define GDK_Cut                 0x1008FF58
+#endif
+
+#ifndef GDK_Paste
+#define GDK_Paste               0x1008FF6D
+#endif
+
+#ifndef GDK_Reload
+#define GDK_Reload              0x1008FF73
+#endif
+
+#ifndef GDK_AudioRandomPlay
+#define GDK_AudioRandomPlay     0x1008FF99
+#endif
+
+#ifndef GDK_Subtitle
+#define GDK_Subtitle            0x1008FF9A
+#endif
+
+#ifndef GDK_Red
+#define GDK_Red                 0x1008FFA3
+#endif
+
+#ifndef GDK_Green
+#define GDK_Green               0x1008FFA4
+#endif
+
+#ifndef GDK_Yellow
+#define GDK_Yellow              0x1008FFA5
+#endif
+
+#ifndef GDK_Blue
+#define GDK_Blue                0x1008FFA6
+#endif
+
 #ifndef GDK_TouchpadToggle
 #define GDK_TouchpadToggle      0x1008FFA9
 #endif
