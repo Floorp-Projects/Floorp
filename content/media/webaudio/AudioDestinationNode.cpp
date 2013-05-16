1bdc126e68a9375c16b12a80bd792957f6ebe8f4
Ehsan Akhgari <ehsan@mozilla.com>
2013-05-16 19:30:42 -0400

    Bug 836599 - Part 10: Use the non-realtime MediaStreamGraph API and a custom destination node engine for OfflineAudioContext; r=roc
    We offload most of the logic for OfflineAudioContext to the destination node,
    since that's where the sample recording needs to happen, so doing this will
    make the code simpler.

--- content/media/webaudio/AudioDestinationNode.cpp
+++ content/media/webaudio/AudioDestinationNode.cpp
@@ -13,22 +13,66 @@
 namespace mozilla {
 namespace dom {
 
+class OfflineDestinationNodeEngine : public AudioNodeEngine
+{
+public:
+  OfflineDestinationNodeEngine(AudioDestinationNode* aNode,
+                               uint32_t aNumberOfChannels,
+                               uint32_t aLength,
+                               float aSampleRate)
+    : AudioNodeEngine(aNode)
+    , mNumberOfChannels(aNumberOfChannels)
+    , mLength(aLength)
+    , mSampleRate(aSampleRate)
+  {
+  }
+
+  virtual void ProduceAudioBlock(AudioNodeStream* aStream,
+                                 const AudioChunk& aInput,
+                                 AudioChunk* aOutput,
+                                 bool* aFinished) MOZ_OVERRIDE
+  {
+    *aOutput = aInput;
+  }
+
+  uint32_t mNumberOfChannels;
+  uint32_t mLength;
+  float mSampleRate;
+};
+
 NS_IMPL_ISUPPORTS_INHERITED0(AudioDestinationNode, AudioNode)
 
-AudioDestinationNode::AudioDestinationNode(AudioContext* aContext, MediaStreamGraph* aGraph)
+AudioDestinationNode::AudioDestinationNode(AudioContext* aContext,
+                                           bool aIsOffline,
+                                           uint32_t aNumberOfChannels,
+                                           uint32_t aLength,
+                                           float aSampleRate)
   : AudioNode(aContext,
-              2,
+              aIsOffline ? aNumberOfChannels : 2,
               ChannelCountMode::Explicit,
               ChannelInterpretation::Speakers)
+  , mFramesToProduce(aLength)
 {
-  mStream = aGraph->CreateAudioNodeStream(new AudioNodeEngine(this),
-                                          MediaStreamGraph::EXTERNAL_STREAM);
+  MediaStreamGraph* graph = aIsOffline ?
+                            MediaStreamGraph::CreateNonRealtimeInstance() :
+                            MediaStreamGraph::GetInstance();
+  AudioNodeEngine* engine = aIsOffline ?
+                            new OfflineDestinationNodeEngine(this, aNumberOfChannels,
+                                                             aLength, aSampleRate) :
+                            new AudioNodeEngine(this);
+  mStream = graph->CreateAudioNodeStream(engine, MediaStreamGraph::EXTERNAL_STREAM);
 }
 
 JSObject*
 AudioDestinationNode::WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope)
 {
   return AudioDestinationNodeBinding::Wrap(aCx, aScope, this);
+}
+
+void
+AudioDestinationNode::StartRendering()
+{
+  mStream->Graph()->StartNonRealtimeProcessing(mFramesToProduce);
 }
 
 }
