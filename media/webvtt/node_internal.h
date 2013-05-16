a2d1c316467a7fd68852c2cdb0725387c40fb26a
Caitlin Potter <snowball@defpixel.com>
2013-05-16 00:10:51 -0400

    Bug 872822 - Update WebVTT library to v0.5. r=rillian Bug 872822 - Update WebVTT library to v0.5. r=rillian

--- media/webvtt/node_internal.h
+++ media/webvtt/node_internal.h
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
 #ifndef __WEBVTT_NODE_INTERNAL_H__
 # define __WEBVTT_NODE_INTERNAL_H__
 # include <webvtt/node.h>
@@ -32,19 +32,40 @@
 /**
  * Routines for creating nodes.
  */
-WEBVTT_INTERN webvtt_status webvtt_create_node( webvtt_node **node, webvtt_node_kind kind, webvtt_node *parent );
-WEBVTT_INTERN webvtt_status webvtt_create_internal_node( webvtt_node **node, webvtt_node *parent, webvtt_node_kind kind, webvtt_stringlist *css_classes, webvtt_string *annotation );
+WEBVTT_INTERN webvtt_status
+webvtt_create_node( webvtt_node **node, webvtt_node_kind kind,
+                    webvtt_node *parent );
+
+WEBVTT_INTERN webvtt_status
+webvtt_create_internal_node( webvtt_node **node, webvtt_node *parent,
+                             webvtt_node_kind kind,
+                             webvtt_stringlist *css_classes,
+                             webvtt_string *annotation );
+
+WEBVTT_INTERN webvtt_status
+webvtt_create_lang_node( webvtt_node **node, webvtt_node *parent,
+                         webvtt_stringlist *css_classes,
+                         webvtt_string *lang );
+
 /**
  * We probably shouldn't have a 'head node' type.
  * We should just return a list of node trees...
  */
-WEBVTT_INTERN webvtt_status webvtt_create_head_node( webvtt_node **node );
-WEBVTT_INTERN webvtt_status webvtt_create_timestamp_node( webvtt_node **node, webvtt_node *parent, webvtt_timestamp time_stamp );
-WEBVTT_INTERN webvtt_status webvtt_create_text_node( webvtt_node **node, webvtt_node *parent, webvtt_string *text );
+WEBVTT_INTERN webvtt_status
+webvtt_create_head_node( webvtt_node **node );
+
+WEBVTT_INTERN webvtt_status
+webvtt_create_timestamp_node( webvtt_node **node, webvtt_node *parent,
+                              webvtt_timestamp time_stamp );
+
+WEBVTT_INTERN webvtt_status
+webvtt_create_text_node( webvtt_node **node, webvtt_node *parent,
+                         webvtt_string *text );
 
 /**
  * Attaches a node to the internal node list of another node.
  */
-WEBVTT_INTERN webvtt_status webvtt_attach_node( webvtt_node *parent, webvtt_node *to_attach );
+WEBVTT_INTERN webvtt_status
+webvtt_attach_node( webvtt_node *parent, webvtt_node *to_attach );
 
 #endif
