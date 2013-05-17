5a9d07681e4b939c4c745c3cf4375faabc7a7c6d
Ed Morley <emorley@mozilla.com>
2013-05-17 09:08:38 +0100

    Backed out changeset 95006bb32743 (bug 860941)

--- dom/base/nsGlobalWindow.h
+++ dom/base/nsGlobalWindow.h
@@ -252,9 +252,6 @@ CreateVoidVariant()
 // However, we need to track both the caller-passed value as well as the
 // caller's, so that we can do an origin check (even for primitives) when the
 // value is accessed. This class encapsulates that magic.
-//
-// We also use the same machinery for |returnValue|, which needs similar origin
-// checks.
 class DialogValueHolder : public nsISupports
 {
 public:
@@ -1337,8 +1334,7 @@ public:
                                               bool aForceReuseInnerWindow);
 
 protected:
-  // For use by outer windows only.
-  nsRefPtr<DialogValueHolder> mReturnValue;
+  nsCOMPtr<nsIVariant> mReturnValue;
 };
 
 /* factory function */
