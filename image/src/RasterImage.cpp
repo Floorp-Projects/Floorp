13b1e489f70af316c0ba3a969084d7d2f047b8fe
Joe Drew <joe@drew.ca>
2013-05-17 13:41:35 -0400

    Bug 873505 - Set the animation frame's start time based on when we *wanted* to start, not when we actually did. r=seth

--- image/src/RasterImage.cpp
+++ image/src/RasterImage.cpp
@@ -617,7 +617,8 @@ RasterImage::AdvanceFrame(TimeStamp aTime, nsIntRect* aDirtyRect)
       NS_WARNING("RasterImage::AdvanceFrame(): Compositing of frame failed");
       nextFrame->SetCompositingFailed(true);
       mAnim->currentAnimationFrameIndex = nextFrameIndex;
-      mAnim->currentAnimationFrameTime = aTime;
+      mAnim->currentAnimationFrameTime = mAnim->currentAnimationFrameTime +
+                                         TimeDuration::FromMilliseconds(timeout);
       return false;
     }
 
@@ -626,7 +627,8 @@ RasterImage::AdvanceFrame(TimeStamp aTime, nsIntRect* aDirtyRect)
 
   // Set currentAnimationFrameIndex at the last possible moment
   mAnim->currentAnimationFrameIndex = nextFrameIndex;
-  mAnim->currentAnimationFrameTime = aTime;
+  mAnim->currentAnimationFrameTime = mAnim->currentAnimationFrameTime +
+                                     TimeDuration::FromMilliseconds(timeout);
 
   return true;
 }
