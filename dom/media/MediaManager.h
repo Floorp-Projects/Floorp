7f72bd9f9e8578ac308541f054add1c23cef3e08
Mike Hommey <mh+mozilla@glandium.org>
2013-05-18 11:10:45 +0200

    Backout changeset be729bc526a3 (part of bug 864035)

--- dom/media/MediaManager.h
+++ dom/media/MediaManager.h
@@ -2,9 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_MediaManager_h
-#define mozilla_dom_MediaManager_h
-
 #include "MediaEngine.h"
 #include "mozilla/dom/ContentChild.h"
 #include "mozilla/Services.h"
@@ -496,5 +493,3 @@ private:
 };
 
 } // namespace mozilla
-
-#endif /* mozilla_dom_MediaManager_h */
