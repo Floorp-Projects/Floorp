17eeebf3df26cdc3f99651d4b57382dc0c80ec07
Wan-Teh Chang <wtc@google.com>
2013-05-20 17:24:28 -0700

    Bug 858231: Update NSS to NSS 3.15 Beta 5, with the local patch revert-bug-808217.patch. r=wtc. Inclues fixes for bug 869262, bug 863947, bug 866362, and bug 863871.

--- security/nss/lib/ssl/sslimpl.h
+++ security/nss/lib/ssl/sslimpl.h
@@ -1175,7 +1175,8 @@ const unsigned char *  preferredCipher;
     /* Configuration state for server sockets */
     /* server cert and key for each KEA type */
     sslServerCerts        serverCerts[kt_kea_size];
-    SECItemArray *        certStatusArray;
+    /* each cert needs its own status */
+    SECItemArray *        certStatusArray[kt_kea_size];
 
     ssl3CipherSuiteCfg cipherSuites[ssl_V3_SUITES_IMPLEMENTED];
     ssl3KeyPair *         ephemeralECDHKeyPair; /* for ECDHE-* handshake */
