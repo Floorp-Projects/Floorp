bad8e4caac8efc4b1c57f492d8afb693db3a4f45
Brian R. Bondy <netzen@gmail.com>
2013-05-22 10:17:05 -0400

    Bug 861322 - Make Metro read Desktop app.update.mode setting. r=jimm

--- browser/metro/base/content/browser-ui.js
+++ browser/metro/base/content/browser-ui.js
@@ -154,6 +154,8 @@ var BrowserUI = {
         Util.dumpLn("Exception in delay load module:", ex.message);
       }
 
+      BrowserUI._pullDesktopControlledPrefs();
+
       // check for left over crash reports and submit them if found.
       if (BrowserUI.startupCrashCheck()) {
         Browser.selectedTab = BrowserUI.newOrSelectTab("about:crash");
@@ -242,6 +244,37 @@ var BrowserUI = {
     } catch (ex) {}
 
     return uri.spec;
+  },
+
+  /**
+    * Some prefs that have consequences in both Metro and Desktop such as
+    * app-update prefs, are automatically pulled from Desktop here.
+    */
+  _pullDesktopControlledPrefs: function() {
+    function pullDesktopControlledPrefType(prefType, prefFunc) {
+      try {
+        registry.create(Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
+                      "Software\\Mozilla\\Firefox\\Metro\\Prefs\\" + prefType,
+                      Ci.nsIWindowsRegKey.ACCESS_ALL);
+        for (let i = 0; i < registry.valueCount; i++) {
+          let prefName = registry.getValueName(i);
+          let prefValue = registry.readStringValue(prefName);
+          if (prefType == Ci.nsIPrefBranch.PREF_BOOL) {
+            prefValue = prefValue == "true";
+          }
+          Services.prefs[prefFunc](prefName, prefValue);
+        }
+      } catch (ex) {
+        Util.dumpLn("Could not pull for prefType " + prefType + ": " + ex);
+      } finally {
+        registry.close();
+      }
+    }
+    let registry = Cc["@mozilla.org/windows-registry-key;1"].
+                   createInstance(Ci.nsIWindowsRegKey);
+    pullDesktopControlledPrefType(Ci.nsIPrefBranch.PREF_INT, "setIntPref");
+    pullDesktopControlledPrefType(Ci.nsIPrefBranch.PREF_BOOL, "setBoolPref");
+    pullDesktopControlledPrefType(Ci.nsIPrefBranch.PREF_STRING, "setCharPref");
   },
 
   /* Set the location to the current content */
