c71ae889d92eb03f4ab25bbf03c87bf361a0d117
Bobby Holley <bobbyholley@gmail.com>
2013-05-22 10:05:24 -0600

    Bug 868130 - Make sure to push a JSContext in TabParent::ReceiveMessage. r=gabor

--- dom/ipc/TabParent.cpp
+++ dom/ipc/TabParent.cpp
@@ -1097,7 +1097,7 @@ TabParent::ReceiveMessage(const nsString& aMessage,
   if (frameLoader && frameLoader->GetFrameMessageManager()) {
     nsRefPtr<nsFrameMessageManager> manager =
       frameLoader->GetFrameMessageManager();
-    JSContext* ctx = manager->GetJSContext();
+    AutoPushJSContext ctx(manager->GetJSContext());
     JSAutoRequest ar(ctx);
     uint32_t len = 0; //TODO: obtain a real value in bug 572685
     // Because we want JS messages to have always the same properties,
