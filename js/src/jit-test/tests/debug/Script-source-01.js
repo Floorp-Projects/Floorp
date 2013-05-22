ce30351c0f487838513e091323b6e99c6bb150e3
Eddy Bruel <ejpbruel@mozilla.com>
2013-05-22 16:42:52 -0700

    Bug 637572 - Implement Debugger.Script.prototype.source; r=jimb

---
+++ js/src/jit-test/tests/debug/Script-source-01.js
@@ -0,0 +1,26 @@
+/*
+ * Script.prototype.source should be an object. Moreover, it should be the
+ * same object for each child script within the same debugger.
+ */
+let g = newGlobal('new-compartment');
+let dbg = new Debugger(g);
+
+let count = 0;
+dbg.onNewScript = function (script) {
+    assertEq(typeof script.source, "object");
+    function traverse(script) {
+        ++count;
+        script.getChildScripts().forEach(function (child) {
+            assertEq(child.source, script.source);
+            traverse(child);
+        });
+    }
+    traverse(script);
+}
+
+g.eval("2 * 3");
+g.eval("function f() {}");
+g.eval("function f() { function g() {} }");
+g.eval("eval('2 * 3')");
+g.eval("new Function('2 * 3')");
+assertEq(count, 10);
--- js/src/jscompartment.h
+++ js/src/jscompartment.h
@@ -65,6 +65,7 @@ struct CrossCompartmentKey
         ObjectWrapper,
         StringWrapper,
         DebuggerScript,
+        DebuggerSource,
         DebuggerObject,
         DebuggerEnvironment
     };
