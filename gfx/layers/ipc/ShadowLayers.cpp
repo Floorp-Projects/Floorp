cd76e0928e72cb5fd6f80c6325a0e52eb5844af7
Matt Woodrow <mwoodrow@mozilla.com>
2013-05-22 15:04:12 +0800

    Bug 874369 - Use normal memory instead of Shmem when only sharing between processes. r=Bas

--- gfx/layers/ipc/ShadowLayers.cpp
+++ gfx/layers/ipc/ShadowLayers.cpp
@@ -530,6 +530,16 @@ ShadowLayerForwarder::OpenDescriptor(OpenMode aMode,
                                rgbFormat);
     return surf.forget();
   }
+  case SurfaceDescriptor::TMemoryImage: {
+    const MemoryImage& image = aSurface.get_MemoryImage();
+    gfxASurface::gfxImageFormat format
+      = static_cast<gfxASurface::gfxImageFormat>(image.format());
+    surf = new gfxImageSurface((unsigned char *)image.data(),
+                               image.size(),
+                               image.stride(),
+                               format);
+    return surf.forget();
+  }
   default:
     NS_ERROR("unexpected SurfaceDescriptor type!");
     return nullptr;
