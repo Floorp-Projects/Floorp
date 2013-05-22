332d5c23b891097989f9b63fd4a0532e45af4c15
Richard Newman <rnewman@mozilla.com>
2013-05-22 10:23:53 -0700

    Bug 858742, Bug 868449 - Fennec integration for Firefox Health Report on Android. r=mfinkle, nalexander * * * Hacking on profile times.

--- mobile/android/base/GeckoProfile.java
+++ mobile/android/base/GeckoProfile.java
@@ -1,11 +1,7 @@
 /* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
- * ***** BEGIN LICENSE BLOCK *****
- *
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * ***** END LICENSE BLOCK ***** */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package org.mozilla.gecko;
 
