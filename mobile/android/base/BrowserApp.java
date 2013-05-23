cef299ed0023f37dac97f69133c1a9c9cb9ac8f2
Brian Nicholson <bnicholson@mozilla.com>
2013-05-23 11:29:43 -0700

    Bug 869411 - Part 2: Replace getPadding() with getTopPadding() in AboutHome. r=lucasr

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -466,7 +466,7 @@ abstract public class BrowserApp extends GeckoApp
             if (mLayerView != null) {
                 mLayerView.getLayerClient().setOnMetricsChangedListener(null);
             }
-            mAboutHome.setPadding(0, 0, 0, 0);
+            mAboutHome.setTopPadding(0);
             if (mBrowserToolbar != null) {
                 mBrowserToolbar.getLayout().scrollTo(0, 0);
             }
@@ -742,7 +742,7 @@ abstract public class BrowserApp extends GeckoApp
                 // When the dynamic toolbar is enabled, set the padding on the
                 // about:home widget directly - this is to avoid resizing the
                 // LayerView, which can cause visible artifacts.
-                mAboutHome.setPadding(0, height, 0, 0);
+                mAboutHome.setTopPadding(height);
             } else {
                 setToolbarMargin(height);
                 height = 0;
