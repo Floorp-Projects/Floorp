06cc4b9b3976050c9942c5216200850e1e453d89
Olli Pettay <Olli.Pettay@helsinki.fi>
2013-05-23 14:41:32 +0300

    Bug 875050, make WebSocket to use EventListenerAdded/Removed, r=bz
    --HG--
    extra : rebase_source : fa0995983c4a6c738bae2d88152db65e59cc1d72

--- content/base/src/WebSocket.h
+++ content/base/src/WebSocket.h
@@ -71,24 +71,9 @@ public:
   NS_DECL_NSIOBSERVER
   NS_DECL_NSIREQUEST
 
-  // nsIDOMEventTarget
-  NS_IMETHOD AddEventListener(const nsAString& aType,
-                              nsIDOMEventListener *aListener,
-                              bool aUseCapture,
-                              bool aWantsUntrusted,
-                              uint8_t optional_argc);
-  virtual void AddEventListener(const nsAString& aType,
-                                nsIDOMEventListener* aListener,
-                                bool aCapture,
-                                const Nullable<bool>& aWantsUntrusted,
-                                ErrorResult& aRv) MOZ_OVERRIDE;
-  NS_IMETHOD RemoveEventListener(const nsAString& aType,
-                                 nsIDOMEventListener* aListener,
-                                 bool aUseCapture);
-  virtual void RemoveEventListener(const nsAString& aType,
-                                   nsIDOMEventListener* aListener,
-                                   bool aUseCapture,
-                                   ErrorResult& aRv) MOZ_OVERRIDE;
+  // EventTarget
+  virtual void EventListenerAdded(nsIAtom* aType) MOZ_OVERRIDE;
+  virtual void EventListenerRemoved(nsIAtom* aType) MOZ_OVERRIDE;
 
   virtual void DisconnectFromOwner();
 
