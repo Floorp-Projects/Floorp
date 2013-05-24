f9f818c2260afd90a0b31df031c6470849f9929d
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2013-05-25 07:55:18 +0900

    Bug 875155 - Make bug 809674 test self-contained. r=bholley

---
+++ js/xpconnect/tests/components/js/xpctest_bug809674.js
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+Components.utils.import("resource:///modules/XPCOMUtils.jsm");
+
+function TestBug809674() {}
+TestBug809674.prototype = {
+
+  /* Boilerplate */
+  QueryInterface: XPCOMUtils.generateQI([Components.interfaces["nsIXPCTestBug809674"]]),
+  contractID: "@mozilla.org/js/xpc/test/js/Bug809674;1",
+  classID: Components.ID("{2df46559-da21-49bf-b863-0d7b7bbcbc73}"),
+
+  /* nsIXPCTestBug809674 */
+  jsvalProperty: {},
+};
+
+
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([TestBug809674]);
