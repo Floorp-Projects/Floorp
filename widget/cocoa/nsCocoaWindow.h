2d321ecce917bbe0b49a04e9fdc15aa624848342
Masayuki Nakano <masayuki@d-toybox.com>
2013-05-25 01:27:52 +0900

    Bug 807893 part.2 Cocoa widget should manage secure input mode with input context at changing the context and changing focus r=smichaud

--- widget/cocoa/nsCocoaWindow.h
+++ widget/cocoa/nsCocoaWindow.h
@@ -297,11 +297,10 @@ public:
     void SetMenuBar(nsMenuBarX* aMenuBar);
     nsMenuBarX *GetMenuBar();
 
-    NS_IMETHOD_(void) SetInputContext(const InputContext& aContext,
-                                      const InputContextAction& aAction)
-    {
-      mInputContext = aContext;
-    }
+    NS_IMETHOD NotifyIME(NotificationToIME aNotification) MOZ_OVERRIDE;
+    NS_IMETHOD_(void) SetInputContext(
+                        const InputContext& aContext,
+                        const InputContextAction& aAction) MOZ_OVERRIDE;
     NS_IMETHOD_(InputContext) GetInputContext()
     {
       NSView* view = mWindow ? [mWindow contentView] : nil;
