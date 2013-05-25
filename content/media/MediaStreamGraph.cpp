6f1c9639a23f48bc52e0d5732a180d65b2f53cf9
Ehsan Akhgari <ehsan@mozilla.com>
2013-05-25 10:01:08 -0400

    Bug 875402 - Delete MediaInputPort objects using normal refcounting semantics; r=roc

--- content/media/MediaStreamGraph.cpp
+++ content/media/MediaStreamGraph.cpp
@@ -1906,9 +1906,6 @@ MediaInputPort::Destroy()
     {
       Run();
     }
-    // This does not need to be strongly referenced; the graph is holding
-    // a strong reference to the port, which we will remove. This will be the
-    // last message for the port.
     MediaInputPort* mPort;
   };
   GraphImpl()->AppendMessage(new Message(this));
@@ -1949,6 +1946,10 @@ ProcessedMediaStream::AllocateInputPort(MediaStream* aStream, uint32_t aFlags,
       mPort->Init();
       // The graph holds its reference implicitly
       mPort.forget();
+    }
+    virtual void RunDuringShutdown()
+    {
+      Run();
     }
     nsRefPtr<MediaInputPort> mPort;
   };
