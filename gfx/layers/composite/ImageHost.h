0413024bf485a1c24424cb1cc2b0edf5fd651d68
Nicolas Silva <nical.bugzilla@gmail.com>
2013-05-26 04:44:24 +0200

    Bug 867656 - Part 2. Implement Dump for CompositableHost classes. r=mattwoodrow

--- gfx/layers/composite/ImageHost.h
+++ gfx/layers/composite/ImageHost.h
@@ -83,6 +83,10 @@ public:
 
   virtual void SetCompositor(Compositor* aCompositor) MOZ_OVERRIDE;
 
+  virtual void Dump(FILE* aFile=NULL,
+                    const char* aPrefix="",
+                    bool aDumpHtml=false) MOZ_OVERRIDE;
+
 #ifdef MOZ_LAYERS_HAVE_LOG
   virtual void PrintInfo(nsACString& aTo, const char* aPrefix);
 #endif
