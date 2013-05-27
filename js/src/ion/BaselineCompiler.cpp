7a623c39e51dfb7f19567baedc4f38586933dd0c
Hannes Verschore <hv1989@gmail.com>
2013-05-27 19:01:27 +0200

    Backout backout 2bb75f6d6877 (Bug 875276).

--- js/src/ion/BaselineCompiler.cpp
+++ js/src/ion/BaselineCompiler.cpp
@@ -67,6 +67,9 @@ BaselineCompiler::compile()
     IonSpew(IonSpew_BaselineScripts, "Baseline compiling script %s:%d (%p)",
             script->filename(), script->lineno, script.get());
 
+    if (cx->typeInferenceEnabled() && !script->ensureHasBytecodeTypeMap(cx))
+        return Method_Error;
+
     // Only need to analyze scripts which are marked |argumensHasVarBinding|, to
     // compute |needsArgsObj| flag.
     if (script->argumentsHasVarBinding()) {
