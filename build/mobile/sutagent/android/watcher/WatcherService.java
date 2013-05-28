ce2d7d36be315c2eda46d9c300d82088bec07e73
Geoff Brown <gbrown@mozilla.com>
2013-05-28 08:43:26 -0600

    Bug 867360 - Add onLowMemory handler to watcher; r=jmaher

--- build/mobile/sutagent/android/watcher/WatcherService.java
+++ build/mobile/sutagent/android/watcher/WatcherService.java
@@ -286,6 +286,18 @@ public class WatcherService extends Service
         stopForegroundCompat(R.string.foreground_service_started);
     }
 
+    @Override
+    public void onLowMemory() {
+        Log.e(LOGTAG, "onLowMemory");
+        System.gc();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        Log.e(LOGTAG, "onTrimMemory: "+level);
+        System.gc();
+    }
+
     protected void getKeyGuardAndWakeLock()
         {
         // Fire off a thread to do some work that we shouldn't do directly in the UI thread
