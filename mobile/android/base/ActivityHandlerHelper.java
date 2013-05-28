1e743599303167b68b97276d56984f499c5103be
Wes Johnston <wjohnston@mozilla.com>
2013-05-28 16:28:38 -0700

    Bug 874689 - Use message passing for file picker. r=blassey

--- mobile/android/base/ActivityHandlerHelper.java
+++ mobile/android/base/ActivityHandlerHelper.java
@@ -7,6 +7,7 @@ package org.mozilla.gecko;
 import org.mozilla.gecko.util.ActivityResultHandler;
 import org.mozilla.gecko.util.ActivityResultHandlerMap;
 import org.mozilla.gecko.util.ThreadUtils;
+import org.mozilla.gecko.util.GeckoEventListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,7 +29,7 @@ import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 
-public class ActivityHandlerHelper {
+public class ActivityHandlerHelper implements GeckoEventListener {
     private static final String LOGTAG = "GeckoActivityHandlerHelper";
 
     private final ConcurrentLinkedQueue<String> mFilePickerResult;
@@ -38,6 +39,10 @@ public class ActivityHandlerHelper {
     private final AwesomebarResultHandler mAwesomebarResultHandler;
     private final CameraImageResultHandler mCameraImageResultHandler;
     private final CameraVideoResultHandler mCameraVideoResultHandler;
+
+    public interface FileResultHandler {
+        public void gotFile(String filename);
+    }
 
     @SuppressWarnings("serial")
     public ActivityHandlerHelper() {
@@ -56,6 +61,34 @@ public class ActivityHandlerHelper {
         mAwesomebarResultHandler = new AwesomebarResultHandler();
         mCameraImageResultHandler = new CameraImageResultHandler(mFilePickerResult);
         mCameraVideoResultHandler = new CameraVideoResultHandler(mFilePickerResult);
+        GeckoAppShell.getEventDispatcher().registerEventListener("FilePicker:Show", this);
+    }
+
+    @Override
+    public void handleMessage(String event, final JSONObject message) {
+        if (event.equals("FilePicker:Show")) {
+            String mimeType = "*/*";
+            String mode = message.optString("mode");
+
+            if ("mimeType".equals(mode))
+                mimeType = message.optString("mimeType");
+            else if ("extension".equals(mode))
+                mimeType = GeckoAppShell.getMimeTypeFromExtensions(message.optString("extensions"));
+
+            Log.i(LOGTAG, "Mime: " + mimeType);
+
+            showFilePickerAsync(GeckoApp.mAppContext, mimeType, new FileResultHandler() {
+                public void gotFile(String filename) {
+                    try {
+                        message.put("file", filename);
+                    } catch (JSONException ex) {
+                        Log.i(LOGTAG, "Can't add filename to message " + filename);
+                    }
+                    GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent(
+                        "FilePicker:Result", message.toString()));
+                }
+            });
+        }
     }
 
     public int makeRequestCodeForAwesomebar() {
@@ -149,6 +182,57 @@ public class ActivityHandlerHelper {
         }
     }
 
+    private interface IntentHandler {
+        public void gotIntent(Intent intent);
+    }
+
+    private void getFilePickerIntentAsync(final Context context, String aMimeType, final IntentHandler handler) {
+        final ArrayList<Intent> intents = new ArrayList<Intent>();
+        final Prompt.PromptListItem[] items =
+            getItemsAndIntentsForFilePicker(context, aMimeType, intents);
+
+        if (intents.size() == 0) {
+            Log.i(LOGTAG, "no activities for the file picker!");
+            handler.gotIntent(null);
+            return;
+        }
+
+        if (intents.size() == 1) {
+            handler.gotIntent(intents.get(0));
+            return;
+        }
+
+        final Prompt prompt = new Prompt(context, null, new Prompt.PromptCallback() {
+            public void onPromptFinished(String promptServiceResult) {
+                int itemId = -1;
+                try {
+                    itemId = new JSONObject(promptServiceResult).getInt("button");
+
+                    if (itemId == -1) {
+                        handler.gotIntent(null);
+                        return;
+                    }
+                } catch (JSONException e) {
+                    Log.e(LOGTAG, "result from promptservice was invalid: ", e);
+                    handler.gotIntent(null);
+                    return;
+                }
+
+                if (handler != null)
+                    handler.gotIntent(intents.get(itemId));
+            }
+        });
+
+        final String title = getFilePickerTitle(context, aMimeType);
+        // Runnable has to be called to show an intent-like
+        // context menu UI using the PromptService.
+        ThreadUtils.postToUiThread(new Runnable() {
+            @Override public void run() {
+                prompt.show(title, "", items, false);
+            }
+        });
+    }
+
     private Intent getFilePickerIntent(Context context, String aMimeType) {
         ArrayList<Intent> intents = new ArrayList<Intent>();
         final Prompt.PromptListItem[] items =
@@ -223,6 +307,31 @@ public class ActivityHandlerHelper {
             GeckoAppShell.processNextNativeEvent(true);
         }
         return filePickerResult;
+    }
+
+    public void showFilePickerAsync(final Activity parentActivity, String aMimeType, final FileResultHandler handler) {
+        getFilePickerIntentAsync(parentActivity, aMimeType, new IntentHandler() {
+            public void gotIntent(Intent intent) {
+                if (intent == null) {
+                    handler.gotFile("");
+                }
+        
+                if (intent.getAction().equals(MediaStore.ACTION_IMAGE_CAPTURE)) {
+                    CameraImageResultHandler cam = new CameraImageResultHandler(handler);
+                    parentActivity.startActivityForResult(intent, mActivityResultHandlerMap.put(cam));
+                } else if (intent.getAction().equals(MediaStore.ACTION_VIDEO_CAPTURE)) {
+                    CameraVideoResultHandler vid = new CameraVideoResultHandler(handler);
+                    parentActivity.startActivityForResult(intent, mActivityResultHandlerMap.put(vid));
+                } else if (intent.getAction().equals(Intent.ACTION_GET_CONTENT)) {
+                    FilePickerResultHandlerSync file = new FilePickerResultHandlerSync(handler);
+                    parentActivity.startActivityForResult(intent, mActivityResultHandlerMap.put(file));
+                } else {
+                    Log.e(LOGTAG, "We should not get an intent with another action!");
+                    handler.gotFile("");
+                    return;
+                }
+            }
+        });
     }
 
     boolean handleActivityResult(int requestCode, int resultCode, Intent data) {
