44e231d80869c31dcb9280f12d3e126b68528249
Sean Stangl <sstangl@mozilla.com>
2013-05-28 14:21:21 -0700

    Bug 876976 - Speed up array creation for global regexp matches. r=h4writer

--- js/src/jsstr.cpp
+++ js/src/jsstr.cpp
@@ -1693,77 +1693,87 @@ class StringRegExpGuard
 };
 
 static bool
-DoMatchLocal(JSContext *cx, RegExpStatics *res, Handle<JSLinearString*> linearStr,
-             RegExpShared &re, MutableHandleValue rval)
+DoMatchLocal(JSContext *cx, CallArgs args, RegExpStatics *res, Handle<JSLinearString*> input,
+             RegExpShared &re)
 {
-    size_t charsLen = linearStr->length();
+    size_t charsLen = input->length();
+    const jschar *chars = input->chars();
+
     size_t i = 0;
     ScopedMatchPairs matches(&cx->tempLifoAlloc());
-    RegExpRunStatus status = re.execute(cx, linearStr->getChars(cx), charsLen, &i, matches);
+    RegExpRunStatus status = re.execute(cx, chars, charsLen, &i, matches);
     if (status == RegExpRunStatus_Error)
         return false;
 
-    /* Emulate ExecuteRegExpLegacy() behavior. */
     if (status == RegExpRunStatus_Success_NotFound) {
-        rval.setNull();
+        args.rval().setNull();
         return true;
     }
 
-    res->updateFromMatchPairs(cx, linearStr, matches);
-    return CreateRegExpMatchResult(cx, linearStr, matches, rval);
+    res->updateFromMatchPairs(cx, input, matches);
+
+    RootedValue rval(cx);
+    if (!CreateRegExpMatchResult(cx, input, chars, charsLen, matches, &rval))
+        return false;
+
+    args.rval().set(rval);
+    return true;
 }
 
 static bool
-BuildGlobalMatchArray(JSContext *cx, HandleString matchesInput, const MatchPair &pair, size_t count,
-                      MutableHandleObject array)
+DoMatchGlobal(JSContext *cx, CallArgs args, RegExpStatics *res, Handle<JSLinearString*> input,
+              RegExpShared &re)
 {
-    JS_ASSERT(count <= JSID_INT_MAX);  /* by max string length */
-    JS_ASSERT(pair.check());
+    size_t charsLen = input->length();
+    const jschar *chars = input->chars();
 
-    if (!array)
-        array.set(NewDenseEmptyArray(cx));
-    if (!array)
-        return false;
+    AutoValueVector elements(cx);
 
-    JSString *str = js_NewDependentString(cx, matchesInput, pair.start, pair.length());
-    if (!str)
-        return false;
+    MatchPair match;
+    size_t i = 0; /* Index used for iterating through the string. */
+    size_t lastMatch = 0; /* Index of last successful match. */
 
-    RootedValue v(cx);
-    v.setString(str);
-    return JSObject::defineElement(cx, array, count, v);
-}
-
-static bool
-DoMatchGlobal(JSContext *cx, RegExpStatics *res, Handle<JSLinearString*> linearPtr, RegExpShared &re,
-              MutableHandleObject array)
-{
-    size_t charsLen = linearPtr->length();
-
-    size_t prevMatch = 0;
-    bool isMatchFound = false;
-    for (size_t count = 0, i = 0, curMatch = 0; i <= charsLen; ++count) {
+    /* Accumulate results for each match. */
+    while (i <= charsLen) {
         if (!JS_CHECK_OPERATION_LIMIT(cx))
             return false;
 
-        MatchPair match;
-        RegExpRunStatus status = re.executeMatchOnly(cx, linearPtr->chars(), charsLen, &i, match);
+        size_t i_orig = i;
+
+        RegExpRunStatus status = re.executeMatchOnly(cx, chars, charsLen, &i, match);
         if (status == RegExpRunStatus_Error)
             return false;
-
         if (status == RegExpRunStatus_Success_NotFound)
             break;
 
-        isMatchFound = true;
-        prevMatch = curMatch;
-        curMatch = i;
-        if (!BuildGlobalMatchArray(cx, linearPtr, match, count, array))
+        lastMatch = i_orig;
+
+        /* Extract the matched substring, root it, and remember it for later. */
+        JSLinearString *str = js_NewDependentString(cx, input, match.start, match.length());
+        if (!str)
             return false;
+        if (!elements.append(StringValue(str)))
+            return false;
+
         if (match.isEmpty())
             ++i;
     }
-    if (isMatchFound)
-        res->updateLazily(cx, linearPtr, &re, prevMatch);
+
+    /* If unmatched, return null. */
+    if (elements.empty()) {
+        args.rval().setNull();
+        return true;
+    }
+
+    /* The last successful match updates the RegExpStatics. */
+    res->updateLazily(cx, input, &re, lastMatch);
+
+    /* Copy the rooted vector into the array object. */
+    JSObject *array = NewDenseCopiedArray(cx, elements.length(), elements.begin());
+    if (!array)
+        return false;
+
+    args.rval().setObject(*array);
     return true;
 }
 
@@ -1822,18 +1832,9 @@ js::str_match(JSContext *cx, unsigned argc, Value *vp)
     if (!linearStr)
         return false;
 
-    if (g.regExp().global()) {
-        RootedObject array(cx);
-        if (!DoMatchGlobal(cx, res, linearStr, g.regExp(), &array))
-            return false;
-        args.rval().setObjectOrNull(array);
-    } else {
-        RootedValue rval(cx);
-        if (!DoMatchLocal(cx, res, linearStr, g.regExp(), &rval))
-            return false;
-        args.rval().set(rval);
-    }
-    return true;
+    if (g.regExp().global())
+        return DoMatchGlobal(cx, args, res, linearStr, g.regExp());
+    return DoMatchLocal(cx, args, res, linearStr, g.regExp());
 }
 
 JSBool
