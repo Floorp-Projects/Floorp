c6d0c9ac5db98d1b5dd05744dde67633b2155d46
Arnaud Sourioux <six.dsn@gmail.com>
2013-05-29 12:37:49 -0700

    Bug 875367: Annotate ~280 more methods with MOZ_OVERRIDE in /layout. r=dholbert

--- layout/svg/nsSVGPathGeometryFrame.h
+++ layout/svg/nsSVGPathGeometryFrame.h
@@ -54,20 +54,20 @@ public:
   // nsIFrame interface:
   NS_IMETHOD  AttributeChanged(int32_t         aNameSpaceID,
                                nsIAtom*        aAttribute,
-                               int32_t         aModType);
+                               int32_t         aModType) MOZ_OVERRIDE;
 
   /**
    * Get the "type" of the frame
    *
    * @see nsGkAtoms::svgPathGeometryFrame
    */
-  virtual nsIAtom* GetType() const;
+  virtual nsIAtom* GetType() const MOZ_OVERRIDE;
 
   virtual bool IsSVGTransformed(gfxMatrix *aOwnTransforms = nullptr,
-                                gfxMatrix *aFromParentTransforms = nullptr) const;
+                                gfxMatrix *aFromParentTransforms = nullptr) const MOZ_OVERRIDE;
 
 #ifdef DEBUG
-  NS_IMETHOD GetFrameName(nsAString& aResult) const
+  NS_IMETHOD GetFrameName(nsAString& aResult) const MOZ_OVERRIDE
   {
     return MakeFrameName(NS_LITERAL_STRING("SVGPathGeometry"), aResult);
   }
@@ -78,7 +78,7 @@ public:
                                 const nsDisplayListSet& aLists) MOZ_OVERRIDE;
 
   // nsSVGGeometryFrame methods
-  gfxMatrix GetCanvasTM(uint32_t aFor);
+  gfxMatrix GetCanvasTM(uint32_t aFor) MOZ_OVERRIDE;
 
 protected:
   // nsISVGChildFrame interface:
