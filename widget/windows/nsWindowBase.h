866273d2f23c3c2af54041799253d712719d7375
Masayuki Nakano <masayuki@d-toybox.com>
2013-05-29 15:34:48 +0900

    Bug 855975 part.10 Move nsWindow::DispatchPluginEvent() to nsWindowBase r=jimm

--- widget/windows/nsWindowBase.h
+++ widget/windows/nsWindowBase.h
@@ -7,6 +7,8 @@
 #define nsWindowBase_h_
 
 #include "nsBaseWidget.h"
+#include "nsGUIEvent.h"
+#include "npapi.h"
 #include <windows.h>
 
 /*
@@ -33,6 +35,37 @@ public:
    * Dispatch a gecko event for this widget.
    */
   virtual bool DispatchWindowEvent(nsGUIEvent* aEvent) = 0;
+
+  /*
+   * Dispatch a plugin event with the message.
+   */
+  virtual bool DispatchPluginEvent(const MSG &aMsg) MOZ_FINAL
+  {
+    if (!PluginHasFocus()) {
+      return false;
+    }
+    nsPluginEvent pluginEvent(true, NS_PLUGIN_INPUT_EVENT, this);
+    nsIntPoint point(0, 0);
+    InitEvent(pluginEvent, &point);
+    NPEvent npEvent;
+    npEvent.event = aMsg.message;
+    npEvent.wParam = aMsg.wParam;
+    npEvent.lParam = aMsg.lParam;
+    pluginEvent.pluginEvent = (void *)&npEvent;
+    pluginEvent.retargetToFocusedDocument = true;
+    return DispatchWindowEvent(&pluginEvent);
+  }
+
+  /*
+   * Returns true if a plugin has focus on this widget.  Otherwise, false.
+   */
+  virtual bool PluginHasFocus() const MOZ_FINAL
+  {
+    return (mInputContext.mIMEState.mEnabled == IMEState::PLUGIN);
+  }
+
+protected:
+  InputContext mInputContext;
 };
 
 #endif // nsWindowBase_h_
