e90650d006bb008b0be4f321f8e76f49ba12f5bb
Brian Hackett <bhackett1024@gmail.com>
2013-05-29 13:31:35 -0600

    Bug 876458 - Fix MUnbox::congruentTo, r=jandem.

--- js/src/ion/MIR.h
+++ js/src/ion/MIR.h
@@ -1999,6 +1999,8 @@ class MUnbox : public MUnaryInstruction, public BoxInputsPolicy
         return mode() != Infallible;
     }
     bool congruentTo(MDefinition *const &ins) const {
+        if (!ins->isUnbox() || ins->toUnbox()->mode() != mode())
+            return false;
         return congruentIfOperandsEqual(ins);
     }
     AliasSet getAliasSet() const {
