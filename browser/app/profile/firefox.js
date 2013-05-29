0cc6185d74a4bb88ca4989329a9c16e35b1af3ad
Adri Hilviu <no52fear@gmail.com>
2013-05-29 11:00:18 -0400

    Bug 828284 - Force shortcut desktop icons off on Windows XP. r=bbondy

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -227,6 +227,7 @@ pref("general.useragent.complexOverride.moodle", false); // bug 797703
 
 // At startup, check if we're the default browser and prompt user if not.
 pref("browser.shell.checkDefaultBrowser", true);
+pref("browser.shell.shortcutFavicons",true);
 
 // 0 = blank, 1 = home (browser.startup.homepage), 2 = last visited page, 3 = resume previous browser session
 // The behavior of option 3 is detailed at: http://wiki.mozilla.org/Session_Restore
