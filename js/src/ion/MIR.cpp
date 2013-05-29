afdd23dc14502d82e4c46d5b35e37e4a200e39c7
Brian Hackett <bhackett1024@gmail.com>
2013-05-29 08:57:02 -0600

    Bug 875476 - Avoid passing operands of the wrong type to EvaluateConstantOperands, r=jandem.

--- js/src/ion/MIR.cpp
+++ js/src/ion/MIR.cpp
@@ -1283,7 +1283,7 @@ MBinaryArithInstruction::infer(BaselineInspector *inspector,
 
     // If the operation will always overflow on its constant operands, use a
     // double specialization so that it can be constant folded later.
-    if (isMul() || isDiv()) {
+    if ((isMul() || isDiv()) && lhs == MIRType_Int32 && rhs == MIRType_Int32) {
         bool typeChange = false;
         EvaluateConstantOperands(this, &typeChange);
         if (typeChange)
