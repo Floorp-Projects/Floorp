c8b367bbab4f301175b82dc9fe8cfde17311b39f
Steve Fink <sfink@mozilla.com>
2013-05-30 13:13:09 -0700

    Backed out 4 changesets (bug 876776) from cypress, since it's only fair that if I did the first-ever inbound2 push, I should get the first ever inbound2 backout (on a CLOSED TREE, no less!)
    Backed out changeset 31e27a44b7c4 (bug 876776)
    Backed out changeset f692efb201b0 (bug 876776)
    Backed out changeset 497545d56f6c (bug 876776)
    Backed out changeset 8ea5828317b2 (bug 876776)

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -5348,7 +5348,6 @@ main(int argc, char **argv, char **envp)
     }
 
     op.setArgTerminatesOptions("script", true);
-    op.setArgCapturesRest("scriptArgs");
 
     switch (op.parseArgs(argc, argv)) {
       case OptionParser::ParseHelp:
