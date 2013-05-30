c9a03cdf3987e09b680b08dbf957d958b3f76b11
Joe Drew <joe@drew.ca>
2013-05-30 10:32:16 -0400

    Bug 876355 - We must report telemetry *before* shutting down the decoder!  r=jrmuizel
    This simply restores the control flow from pre-bug 716140.

--- image/src/RasterImage.cpp
+++ image/src/RasterImage.cpp
@@ -3503,15 +3503,8 @@ RasterImage::FinishedSomeDecoding(eShutdownIntent aIntent /* = eShutdownIntent_D
 
       wasSize = decoder->IsSizeDecode();
 
-      // We need to shut down the decoder first, in order to ensure all
-      // decoding routines have been finished.
-      rv = image->ShutdownDecoder(aIntent);
-      if (NS_FAILED(rv)) {
-        image->DoError();
-      }
-
       // Do some telemetry if this isn't a size decode.
-      if (request && !decoder->IsSizeDecode()) {
+      if (request && !wasSize) {
         Telemetry::Accumulate(Telemetry::IMAGE_DECODE_TIME,
                               int32_t(request->mDecodeTime.ToMicroseconds()));
 
@@ -3523,6 +3516,13 @@ RasterImage::FinishedSomeDecoding(eShutdownIntent aIntent /* = eShutdownIntent_D
                                  (1024 * request->mDecodeTime.ToSeconds()));
           Telemetry::Accumulate(id, KBps);
         }
+      }
+
+      // We need to shut down the decoder first, in order to ensure all
+      // decoding routines have been finished.
+      rv = image->ShutdownDecoder(aIntent);
+      if (NS_FAILED(rv)) {
+        image->DoError();
       }
     }
   }
