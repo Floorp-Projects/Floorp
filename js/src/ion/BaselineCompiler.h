c9b5c50c6789a317293b8be2f4cbdf56960af79d
Ryan VanderMeulen <ryanvm@gmail.com>
2013-05-29 20:36:03 -0400

    Backed out changesets 4cd1bae06bb7 and 2d34ab368ffa (bug 831507) for dromaeo crashes on a CLOSED TREE.

--- js/src/ion/BaselineCompiler.h
+++ js/src/ion/BaselineCompiler.h
@@ -171,9 +171,6 @@ class BaselineCompiler : public BaselineCompilerSpecific
 {
     FixedList<Label>            labels_;
     HeapLabel *                 return_;
-#ifdef JSGC_GENERATIONAL
-    HeapLabel *                 postBarrierSlot_;
-#endif
 
     // Native code offset right before the scope chain is initialized.
     CodeOffsetLabel prologueOffset_;
@@ -193,9 +190,6 @@ class BaselineCompiler : public BaselineCompilerSpecific
 
     bool emitPrologue();
     bool emitEpilogue();
-#ifdef JSGC_GENERATIONAL
-    bool emitOutOfLinePostBarrierSlot();
-#endif
     bool emitIC(ICStub *stub, bool isForOp);
     bool emitOpIC(ICStub *stub) {
         return emitIC(stub, true);
@@ -244,8 +238,6 @@ class BaselineCompiler : public BaselineCompilerSpecific
 
     bool addPCMappingEntry(bool addIndexEntry);
 
-    void getScopeCoordinateObject(Register reg);
-    Address getScopeCoordinateAddressFromObject(Register objReg, Register reg);
     Address getScopeCoordinateAddress(Register reg);
 };
 
