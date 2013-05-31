da89a43933c1ba8fc79625b93df2936c66ce664b
Bobby Holley <bobbyholley@gmail.com>
2013-05-31 10:36:01 -0700

    Bug 858101 - Run the DefaultValue algorithm directly on the wrapper for Xrays. r=mrbkap

--- js/xpconnect/wrappers/XrayWrapper.cpp
+++ js/xpconnect/wrappers/XrayWrapper.cpp
@@ -1885,6 +1885,20 @@ XrayWrapper<Base, Traits>::construct(JSContext *cx, HandleObject wrapper, const 
     return Traits::construct(cx, wrapper, args, Base::singleton);
 }
 
+template <typename Base, typename Traits>
+bool
+XrayWrapper<Base, Traits>::defaultValue(JSContext *cx, HandleObject wrapper,
+                                        JSType hint, MutableHandleValue vp)
+{
+    // Even if this isn't a security wrapper, Xray semantics dictate that we
+    // run the DefaultValue algorithm directly on the Xray wrapper.
+    //
+    // NB: We don't have to worry about things with special [[DefaultValue]]
+    // behavior like Date because we'll never have an XrayWrapper to them.
+    return js::DefaultValue(cx, wrapper, hint, vp);
+}
+
+
 /*
  * The Permissive / Security variants should be used depending on whether the
  * compartment of the wrapper is guranteed to subsume the compartment of the
