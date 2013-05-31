99d20e13138edda7d1f0e80c9e0df0a3b35f466e
Mark Hammond <mhammond@skippinet.com.au>
2013-05-31 11:17:41 +1000

    Bug 875157 (part 2) - prevent background thumbnails from displaying alerts/dialogs.  r=adw

--- toolkit/components/thumbnails/test/browser_thumbnails_background.js
+++ toolkit/components/thumbnails/test/browser_thumbnails_background.js
@@ -227,9 +227,42 @@ let tests = [
       isnot([r, g, b].toString(), [0, 255, 0].toString(),
             "The captured page should not be green.");
       gBrowser.removeTab(tab);
+      file.remove(false);
       deferred.resolve();
     });
     yield deferred.promise;
+  },
+
+  // the following tests attempt to display modal dialogs.  The test just
+  // relies on the fact that if the dialog was displayed the test will hang
+  // and timeout.  IOW - the tests would pass if the dialogs appear and are
+  // manually closed by the user - so don't do that :)  (obviously there is
+  // noone available to do that when run via tbpl etc, so this should be safe,
+  // and it's tricky to use the window-watcher to check a window *does not*
+  // appear - how long should the watcher be active before assuming it's not
+  // going to appear?)
+  function noAuthPrompt() {
+    let url = "http://mochi.test:8888/browser/browser/base/content/test/authenticate.sjs?user=anyone";
+    let file = fileForURL(url);
+    ok(!file.exists(), "Thumbnail file should not already exist.");
+
+    let capturedURL = yield capture(url);
+    is(capturedURL, url, "Captured URL should be URL passed to capture.");
+    ok(file.exists(),
+       "Thumbnail file should exist even though it requires auth.");
+    file.remove(false);
+  },
+
+  function noAlert() {
+    let url = "data:text/html,<script>alert('yo!');</script>";
+    let file = fileForURL(url);
+    ok(!file.exists(), "Thumbnail file should not already exist.");
+
+    let capturedURL = yield capture(url);
+    is(capturedURL, url, "Captured URL should be URL passed to capture.");
+    ok(file.exists(),
+       "Thumbnail file should exist even though it alerted.");
+    file.remove(false);
   },
 ];
 
