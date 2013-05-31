5862fad936f17e2434e2637accbd0c68ad939955
Nicholas Cameron <ncameron@mozilla.com>
2013-05-31 16:36:19 +1200

    Bug 876661. Fix function hidden warnings. r=mattwoodrow
    --HG--
    extra : rebase_source : d008e636467c573723bc7de9cd0e13262d4ee9c1

--- gfx/layers/composite/CompositableHost.h
+++ gfx/layers/composite/CompositableHost.h
@@ -150,9 +150,9 @@ public:
    * don't have a single surface for the texture contents, and we
    * need to allocate our own one to be updated later.
    */
-  virtual void EnsureTextureHost(ISurfaceAllocator* aAllocator,
-                                 const TextureInfo& aTextureInfo,
-                                 const nsIntRect& aBufferRect)
+  virtual void EnsureTextureHostIncremental(ISurfaceAllocator* aAllocator,
+                                            const TextureInfo& aTextureInfo,
+                                            const nsIntRect& aBufferRect)
   {
     MOZ_ASSERT(false, "should be implemented or not used");
   }
