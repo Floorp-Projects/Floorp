10d4ed1a07afb952037844866f7a491144062827
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2013-06-03 12:05:58 +0200

    Bug 509719 - Fix crash in XULDocument::RemoveElement, r=bz

--- content/xul/document/src/XULDocument.cpp
+++ content/xul/document/src/XULDocument.cpp
@@ -4045,7 +4045,8 @@ XULDocument::OverlayForwardReference::Merge(nsIContent* aTargetNode,
         if (attr == nsGkAtoms::removeelement &&
             value.EqualsLiteral("true")) {
 
-            nsCOMPtr<nsIContent> parent = aTargetNode->GetParent();
+            nsCOMPtr<nsINode> parent = aTargetNode->GetParentNode();
+            if (!parent) return NS_ERROR_FAILURE;
             rv = RemoveElement(parent, aTargetNode);
             if (NS_FAILED(rv)) return rv;
 
@@ -4427,7 +4428,7 @@ XULDocument::CheckBroadcasterHookup(Element* aElement,
 }
 
 nsresult
-XULDocument::InsertElement(nsIContent* aParent, nsIContent* aChild,
+XULDocument::InsertElement(nsINode* aParent, nsIContent* aChild,
                            bool aNotify)
 {
     // Insert aChild appropriately into aParent, accounting for a
@@ -4508,7 +4509,7 @@ XULDocument::InsertElement(nsIContent* aParent, nsIContent* aChild,
 }
 
 nsresult
-XULDocument::RemoveElement(nsIContent* aParent, nsIContent* aChild)
+XULDocument::RemoveElement(nsINode* aParent, nsINode* aChild)
 {
     int32_t nodeOffset = aParent->IndexOf(aChild);
 
