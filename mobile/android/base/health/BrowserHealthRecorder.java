8649cc9838e0562a4663a703ebae3cdc4862bb7d
Richard Newman <rnewman@mozilla.com>
2013-06-03 14:12:17 -0700

    Bug 878303 - Part 2: use COUNTED_DISCRETE type for searches. r=nalexander

--- mobile/android/base/health/BrowserHealthRecorder.java
+++ mobile/android/base/health/BrowserHealthRecorder.java
@@ -479,7 +479,7 @@ public class BrowserHealthRecorder implements GeckoEventListener {
      */
 
     public static final String MEASUREMENT_NAME_SEARCH_COUNTS = "org.mozilla.searches.counts";
-    public static final int MEASUREMENT_VERSION_SEARCH_COUNTS = 3;
+    public static final int MEASUREMENT_VERSION_SEARCH_COUNTS = 4;
 
     public static final String[] SEARCH_LOCATIONS = {
         "barkeyword",
@@ -577,8 +577,9 @@ public class BrowserHealthRecorder implements GeckoEventListener {
                         // We're not using a counter, because the set of engine
                         // identifiers is potentially unbounded, and thus our
                         // measurement version would have to keep growing as
-                        // fields changed.
-                        out.add(new FieldSpec(location, Field.TYPE_STRING_DISCRETE));
+                        // fields changed. Instead we store discrete values, and
+                        // accumulate them into a counting map during processing.
+                        out.add(new FieldSpec(location, Field.TYPE_COUNTED_STRING_DISCRETE));
                     }
                     return out;
                 }
