418ccc1e933c5b8c560532892e9dcd0649379324
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2013-06-03 22:13:34 +0200

    Bug 878452 - dragging an item to an empty area should work, r=jaws

--- browser/components/customizableui/test/head.js
+++ browser/components/customizableui/test/head.js
@@ -5,9 +5,16 @@
 
 // Avoid leaks by using tmp for imports...
 let tmp = {};
+Cu.import("resource://gre/modules/commonjs/sdk/core/promise.js", tmp);
 Cu.import("resource://gre/modules/Task.jsm", tmp);
 Cu.import("resource:///modules/CustomizableUI.jsm", tmp);
-let {Task, CustomizableUI} = tmp;
+let {Promise, Task, CustomizableUI} = tmp;
+
+let ChromeUtils = {};
+let scriptLoader = Cc["@mozilla.org/moz/jssubscript-loader;1"].getService(Ci.mozIJSSubScriptLoader);
+scriptLoader.loadSubScript("chrome://mochikit/content/tests/SimpleTest/ChromeUtils.js", ChromeUtils);
+
+let {synthesizeDragStart, synthesizeDrop} = ChromeUtils;
 
 const kNSXUL = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
 
@@ -41,12 +48,15 @@ function removeCustomToolbars() {
 
 function resetCustomization() {
   CustomizableUI.reset();
+  if (document.documentElement.hasAttribute("customizing")) {
+    window.gCustomizeMode.exit();
+  }
 }
 
 function assertAreaPlacements(areaId, expectedPlacements) {
   let actualPlacements = getAreaWidgetIds(areaId);
   is(actualPlacements.length, expectedPlacements.length,
-     "Area " + areaId + " should have the right number of items.");
+     "Area " + areaId + " should have " + expectedPlacements.length + " items.");
   let minItems = Math.min(expectedPlacements.length, actualPlacements.length);
   for (let i = 0; i < minItems; i++) {
     if (typeof expectedPlacements[i] == "string") {
@@ -66,6 +76,35 @@ function assertAreaPlacements(areaId, expectedPlacements) {
 function getAreaWidgetIds(areaId) {
   let widgetAry = CustomizableUI.getWidgetsInArea(areaId);
   return widgetAry.map(x => x.id);
+}
+
+function simulateItemDrag(toDrag, target) {
+  let docId = toDrag.ownerDocument.documentElement.id;
+  let dragData = [[{type: 'text/toolbarwrapper-id/' + docId,
+                    data: {id: toDrag.id, width: toDrag.getBoundingClientRect().width + 'px'}}]];
+  synthesizeDragStart(toDrag.parentNode, dragData);
+  synthesizeDrop(target, target, dragData);
+}
+
+function endCustomizing() {
+  let deferred = Promise.defer();
+  function onCustomizationEnds(ev) {
+    window.gNavToolbox.removeEventListener("aftercustomization", onCustomizationEnds);
+    deferred.resolve(ev);
+  }
+  window.gNavToolbox.addEventListener("aftercustomization", onCustomizationEnds);
+  window.gCustomizeMode.exit();
+  return deferred.promise;
+}
+function startCustomizing() {
+  let deferred = Promise.defer();
+  function onCustomizing() {
+    window.gNavToolbox.removeEventListener("customizationready", onCustomizing);
+    deferred.resolve();
+  }
+  window.gNavToolbox.addEventListener("customizationready", onCustomizing);
+  window.gCustomizeMode.enter();
+  return deferred.promise;
 }
 
 function testRunner(testAry) {
