925af646317dfcb21e15b840731d9e29e4d7474c
Alexander Surkov <surkov.alexander@gmail.com>
2013-06-04 15:52:39 +0900

    Bug 878590 - HTML5 datalist is not conveyed by haspopup property, r=tbsaunde

--- accessible/src/html/HTMLFormControlAccessible.cpp
+++ accessible/src/html/HTMLFormControlAccessible.cpp
@@ -404,7 +404,7 @@ HTMLTextFieldAccessible::NativeState()
 
   // Expose autocomplete state if it has associated autocomplete list.
   if (mContent->HasAttr(kNameSpaceID_None, nsGkAtoms::list))
-    return state | states::SUPPORTS_AUTOCOMPLETION;
+    return state | states::SUPPORTS_AUTOCOMPLETION | states::HASPOPUP;
 
   // No parent can mean a fake widget created for XUL textbox. If accessible
   // is unattached from tree then we don't care.
