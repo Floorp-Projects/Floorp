4e810ae71d54535a6673e86d409d5e9e8a69477d
Glenn Randers-Pehrson <glennrp+bmo@gmail.com>
2013-06-05 16:50:53 -0400

    Bug 873001 - Update libpng to version 1.5.16. r=joe

--- media/libpng/pngrutil.c
+++ media/libpng/pngrutil.c
@@ -1,7 +1,7 @@
 
 /* pngrutil.c - utilities to read a PNG file
  *
- * Last changed in libpng 1.5.15 [March 28, 2013]
+ * Last changed in libpng 1.5.16 [May 23, 2013]
  * Copyright (c) 1998-2013 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -17,8 +17,6 @@
 #include "pngpriv.h"
 
 #ifdef PNG_READ_SUPPORTED
-
-#define png_strtod(p,a,b) strtod(a,b)
 
 png_uint_32 PNGAPI
 png_get_uint_31(png_structp png_ptr, png_const_bytep buf)
