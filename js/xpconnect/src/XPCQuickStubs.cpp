1422adb4f35b816ab0d843f4baa95e9d21fb3be3
Bobby Holley <bobbyholley@gmail.com>
2013-06-04 21:11:20 -0700

    Bug 877261 - Remove XPCLazyCallContext from Quickstubs. r=Ms2ger
    There are a number of places where quickstubs does a scary-looking call to
    lccx->SetWrapper. However, the lccx never gets morphed into a ccx, nor does
    it escape in any other way. And unlike ccxes, declaring an lccx on the stack
    doesn't have any observable side-effects. So this should actually be safe.

--- js/xpconnect/src/XPCQuickStubs.cpp
+++ js/xpconnect/src/XPCQuickStubs.cpp
@@ -617,16 +617,12 @@ castNative(JSContext *cx,
            const nsIID &iid,
            void **ppThis,
            nsISupports **pThisRef,
-           jsval *vp,
-           XPCLazyCallContext *lccx)
+           jsval *vp)
 {
     RootedObject cur(cx, curArg);
     if (wrapper) {
         nsresult rv = getNativeFromWrapper(cx,wrapper, iid, ppThis, pThisRef,
                                            vp);
-
-        if (lccx && NS_SUCCEEDED(rv))
-            lccx->SetWrapper(wrapper, tearoff);
 
         if (rv != NS_ERROR_NO_INTERFACE)
             return rv;
@@ -644,14 +640,6 @@ castNative(JSContext *cx,
         }
 
         if (NS_SUCCEEDED(getNative(native, entries, cur, iid, ppThis, pThisRef, vp))) {
-            if (lccx) {
-                // This only matters for unwrapping of this objects, so we
-                // shouldn't end up here for the new DOM bindings.
-                NS_ABORT_IF_FALSE(IS_SLIM_WRAPPER(cur),
-                                  "what kind of wrapper is this?");
-                lccx->SetWrapper(cur);
-            }
-
             return NS_OK;
         }
     }
@@ -668,7 +656,6 @@ castNativeFromWrapper(JSContext *cx,
                       int32_t protoDepth,
                       nsISupports **pRef,
                       jsval *pVal,
-                      XPCLazyCallContext *lccx,
                       nsresult *rv)
 {
     XPCWrappedNative *wrapper;
@@ -693,15 +680,11 @@ castNativeFromWrapper(JSContext *cx,
         cur = wrapper->GetFlatJSObject();
         if (!native || !HasBitInInterfacesBitmap(cur, interfaceBit)) {
             native = nullptr;
-        } else if (lccx) {
-            lccx->SetWrapper(wrapper, tearoff);
         }
     } else if (cur && IS_SLIM_WRAPPER(cur)) {
         native = static_cast<nsISupports*>(xpc_GetJSPrivate(cur));
         if (!native || !HasBitInInterfacesBitmap(cur, interfaceBit)) {
             native = nullptr;
-        } else if (lccx) {
-            lccx->SetWrapper(cur);
         }
     } else if (cur && protoDepth >= 0) {
         const mozilla::dom::DOMClass* domClass =
@@ -769,7 +752,7 @@ xpc_qsUnwrapArgImpl(JSContext *cx,
 
     if (wrapper || obj2) {
         if (NS_FAILED(castNative(cx, wrapper, obj2, tearoff, iid, ppArg,
-                                 ppArgRef, vp, nullptr)))
+                                 ppArgRef, vp)))
             return NS_ERROR_XPC_BAD_CONVERT_JS;
         return NS_OK;
     }
