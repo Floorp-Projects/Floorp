e3de7fac03ea09cf5d66c7f7270bf0663eaea1f0
Chris Peterson <cpeterson@mozilla.com>
2013-06-06 16:02:49 -0700

    Bug 878327 - Disable the Marketplace payment provider prefs in Beta and Release channels. r=wesj

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -730,11 +730,13 @@ pref("browser.contentHandlers.types.3.type", "application/vnd.mozilla.maybe.feed
 pref("media.webaudio.enabled", true);
 #endif
 
+#ifndef RELEASE_BUILD
 pref("dom.payment.provider.0.name", "Firefox Marketplace");
 pref("dom.payment.provider.0.description", "marketplace.firefox.com");
 pref("dom.payment.provider.0.uri", "https://marketplace.firefox.com/mozpay/?req=");
 pref("dom.payment.provider.0.type", "mozilla/payments/pay/v1");
 pref("dom.payment.provider.0.requestMethod", "GET");
+#endif
 
 // This needs more tests and stability fixes first, as well as UI.
 pref("media.navigator.enabled", false);
