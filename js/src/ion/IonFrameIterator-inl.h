3b9d2d0d0ee46b65af02ad3bfe8a4703d70ad6c7
Jason Orendorff <jorendorff@mozilla.com>
2013-06-06 22:47:23 -0500

    Bug 879831, part 0 - Add defined(JS_ION) guard to more ion headers, to prevent the need for #ifdef JS_ION around #includes in other files. r=njn.

--- js/src/ion/IonFrameIterator-inl.h
+++ js/src/ion/IonFrameIterator-inl.h
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef jsion_frame_iterator_inl_h__
+#if !defined(jsion_frame_iterator_inl_h__) && defined(JS_ION)
 #define jsion_frame_iterator_inl_h__
 
 #include "ion/BaselineFrame.h"
