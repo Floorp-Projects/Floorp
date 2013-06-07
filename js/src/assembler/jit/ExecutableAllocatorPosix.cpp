d05daf2bbcbce0a7c50e50c435cdeca80d71e351
Nicholas Nethercote <nnethercote@mozilla.com>
2013-06-06 21:35:53 -0700

    Bug 634839 (part 2) - Remove more unnecessary #includes from SpiderMonkey.  r=jorendorff.

--- js/src/assembler/jit/ExecutableAllocatorPosix.cpp
+++ js/src/assembler/jit/ExecutableAllocatorPosix.cpp
@@ -23,13 +23,15 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#include "ExecutableAllocator.h"
+#include "assembler/jit/ExecutableAllocator.h"
 
 #if ENABLE_ASSEMBLER && WTF_OS_UNIX && !WTF_OS_SYMBIAN
 
 #include <sys/mman.h>
 #include <unistd.h>
-#include <wtf/VMTags.h>
+
+#include "assembler/wtf/Assertions.h"
+#include "assembler/wtf/VMTags.h"
 
 namespace JSC {
 
