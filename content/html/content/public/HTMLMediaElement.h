3c738f2ef12f1ec585ae8670ee15a6c6b94d29e7
Robert O'Callahan <robert@ocallahan.org>
2013-06-07 16:26:55 +1200

    Bug 865537. Part 2: The Range used to track the source element pointer for a media element should not gravitate outside the media element. r=cpearce

--- content/html/content/public/HTMLMediaElement.h
+++ content/html/content/public/HTMLMediaElement.h
@@ -44,6 +44,7 @@ class MediaDecoder;
 }
 
 class nsITimer;
+class nsRange;
 
 namespace mozilla {
 namespace dom {
@@ -882,7 +883,7 @@ protected:
 
   // Points to the child source elements, used to iterate through the children
   // when selecting a resource to load.
-  nsCOMPtr<nsIDOMRange> mSourcePointer;
+  nsRefPtr<nsRange> mSourcePointer;
 
   // Points to the document whose load we're blocking. This is the document
   // we're bound to when loading starts.
