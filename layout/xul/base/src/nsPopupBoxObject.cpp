d6267ec436029c0b33443dc8c8432f2b95414945
Joe Drew <joe@drew.ca>
2013-06-10 17:00:00 -0400

    Bug 881459 - Don't include nsView.h from nsLayoutUtils.h r=jrmuizel

--- layout/xul/base/src/nsPopupBoxObject.cpp
+++ layout/xul/base/src/nsPopupBoxObject.cpp
@@ -14,6 +14,7 @@
 #include "nsGkAtoms.h"
 #include "nsMenuPopupFrame.h"
 #include "nsClientRect.h"
+#include "nsView.h"
 
 class nsPopupBoxObject : public nsBoxObject,
                          public nsIPopupBoxObject
