064a5b31e1b9846988fa0d9919ad344d4215bd4f
Dan Gohman <sunfish@google.com>
2013-06-10 14:13:57 -0700

    Bug 881380 - Pull the LDouble class into shared code. r=luke

--- js/src/ion/shared/LIR-x86-shared.h
+++ js/src/ion/shared/LIR-x86-shared.h
@@ -277,23 +277,6 @@ class LMulI : public LBinaryMath<0, 1>
     }
 };
 
-// Constant double.
-class LDouble : public LInstructionHelper<1, 0, 0>
-{
-    double d_;
-
-  public:
-    LIR_HEADER(Double)
-
-    LDouble(double d)
-      : d_(d)
-    { }
-
-    double getDouble() const {
-        return d_;
-    }
-};
-
 } // namespace ion
 } // namespace js
 
