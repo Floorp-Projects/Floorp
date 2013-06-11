bd9bf0afc27c4a0dec6f2f19802f5d033eb2cb30
Paul Adenot <paul@paul.cx>
2013-06-11 02:01:54 +0200

    Bug 877662 - Add an SSE2 implementation of AudioNodeEngine.cpp functions. r=ehsan
    MozReview-Commit-ID: GhxFwFlmqPr

---
+++ dom/media/webaudio/AudioNodeEngineSSE2.h
@@ -0,0 +1,39 @@
+/* -*- mode: c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* this source code form is subject to the terms of the mozilla public
+ * license, v. 2.0. if a copy of the mpl was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "AudioNodeEngine.h"
+
+namespace mozilla {
+void
+AudioBlockAddChannelWithScale_SSE(const float aInput[WEBAUDIO_BLOCK_SIZE],
+                                  float aScale,
+                                  float aOutput[WEBAUDIO_BLOCK_SIZE]);
+
+void
+AudioBlockAddChannel_SSE(const float aInput[WEBAUDIO_BLOCK_SIZE],
+                         float aOutput[WEBAUDIO_BLOCK_SIZE]);
+
+void
+AudioBlockCopyChannelWithScale_SSE(const float* aInput,
+                                   float aScale,
+                                   float* aOutput);
+
+void
+AudioBlockCopyChannelWithScale_SSE(const float aInput[WEBAUDIO_BLOCK_SIZE],
+                                   const float aScale[WEBAUDIO_BLOCK_SIZE],
+                                   float aOutput[WEBAUDIO_BLOCK_SIZE]);
+
+void
+AudioBlockInPlaceScale_SSE(float aBlock[WEBAUDIO_BLOCK_SIZE],
+                           uint32_t aChannelCount,
+                           float aScale);
+
+void
+AudioBlockPanStereoToStereo_SSE(const float aInputL[WEBAUDIO_BLOCK_SIZE],
+                                const float aInputR[WEBAUDIO_BLOCK_SIZE],
+                                float aGainL, float aGainR, bool aIsOnTheLeft,
+                                float aOutputL[WEBAUDIO_BLOCK_SIZE],
+                                float aOutputR[WEBAUDIO_BLOCK_SIZE]);
+}
