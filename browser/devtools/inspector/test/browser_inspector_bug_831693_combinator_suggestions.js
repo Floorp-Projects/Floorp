13b00812b7b92d7851b10ac558137e3bace93241
Dave Camp <dcamp@mozilla.com>
2013-06-10 21:18:46 -0700

    Bug 879523 - Remote the SelectorSearch. r=paul

--- browser/devtools/inspector/test/browser_inspector_bug_831693_combinator_suggestions.js
+++ browser/devtools/inspector/test/browser_inspector_bug_831693_combinator_suggestions.js
@@ -60,6 +60,7 @@ function test()
   function startTest(aInspector)
   {
     inspector = aInspector;
+
     searchBox =
       inspector.panelWin.document.getElementById("inspector-searchbox");
     popup = inspector.searchSuggestions.searchPopup;
@@ -85,7 +86,7 @@ function test()
   }
 
   function checkState(event) {
-    executeSoon(function() {
+    inspector.searchSuggestions._lastQuery.then(() => {
       let [key, suggestions] = keyStates[state];
       let actualSuggestions = popup.getItems();
       is(popup._panel.state == "open" || popup._panel.state == "showing"
