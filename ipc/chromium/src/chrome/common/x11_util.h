b689917b788db28bd023a016e6748fedc6e084c4
Martin Stransky <stransky@redhat.com>
2013-06-11 15:09:29 -0400

    Bug 877626 - Port GTK2 to GTK3 - build config, rest of the tree. r=karlt

--- ipc/chromium/src/chrome/common/x11_util.h
+++ ipc/chromium/src/chrome/common/x11_util.h
@@ -11,7 +11,11 @@
 // we use a void* for Visual*). The Xlib headers are highly polluting so we try
 // hard to limit their spread into the rest of the code.
 
+#if (MOZ_WIDGET_GTK == 2)
 typedef struct _GdkDrawable GdkWindow;
+#else
+typedef struct _GdkWindow GdkWindow;
+#endif
 typedef struct _GtkWidget GtkWidget;
 typedef unsigned long XID;
 typedef struct _XDisplay Display;
