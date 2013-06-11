8390b18e4676d4984f28c2cc0c41763f1db8e5be
Edgar Chen <echen@mozilla.com>
2013-06-11 16:36:29 +0800

    Bug 880369 - Part 2: Xpcshell tests for mcc table. r=allstars.chh

--- dom/system/gonk/tests/test_ril_worker_icc.js
+++ dom/system/gonk/tests/test_ril_worker_icc.js
@@ -2604,3 +2604,52 @@ add_test(function test_unlock_card_lock_corporateLocked() {
   run_next_test();
 });
 
+/**
+ * Verify MCC and MNC parsing
+ */
+add_test(function test_mcc_mnc_parsing()) {
+  let worker = newUint8Worker();
+  let record = worker.ICCRecordHelper;
+  let helper = worker.GsmPDUHelper;
+  let ril    = worker.RIL;
+  let buf    = worker.Buf;
+  let io     = worker.ICCIOHelper;
+
+  function do_test(mncLengthInEf, imsi, expectedMcc, expectedMnc) {
+    ril.iccInfoPrivate.imsi = imsi;
+
+    io.loadTransparentEF = function fakeLoadTransparentEF(options) {
+      let ad = [0x00, 0x00, 0x00];
+      if (mncLengthInEf) {
+        ad.push(mncLengthInEf);
+      }
+
+      // Write data size
+      buf.writeUint32(ad.length * 2);
+
+      // Write data
+      for (let i = 0; i < ad.length; i++) {
+        helper.writeHexOctet(ad[i]);
+      }
+
+      // Write string delimiter
+      buf.writeStringDelimiter(ad.length * 2);
+
+      if (options.callback) {
+        options.callback(options);
+      }
+    };
+
+    record.readAD();
+
+    do_check_eq(ril.iccInfo.mcc, expectedMcc);
+    do_check_eq(ril.iccInfo.mnc, expectedMnc);
+  }
+
+  do_test(undefined, "466923202422409", "466", "92" );
+  do_test(0x03,      "466923202422409", "466", "923");
+  do_test(undefined, "310260542718417", "310", "260");
+  do_test(0x02,      "310260542718417", "310", "26" );
+
+  run_next_test();
+});
