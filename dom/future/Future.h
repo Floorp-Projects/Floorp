2135899af6c900fce8e3609d05db924e7b6af66f
Andrea Marchesini <amarchesini@mozilla.com>
2013-06-11 21:41:21 -0400

    Bug 856410 - Implement futures - Part 1: Constructor, webidl, Done(). r=mounir, r=bz

---
+++ dom/future/Future.h
@@ -0,0 +1,101 @@
+/* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_Future_h
+#define mozilla_dom_Future_h
+
+#include "mozilla/Attributes.h"
+#include "mozilla/ErrorResult.h"
+#include "mozilla/dom/BindingDeclarations.h"
+#include "nsCycleCollectionParticipant.h"
+#include "mozilla/dom/FutureBinding.h"
+#include "nsWrapperCache.h"
+#include "nsAutoPtr.h"
+
+struct JSContext;
+class nsPIDOMWindow;
+
+namespace mozilla {
+namespace dom {
+
+class FutureInit;
+class AnyCallback;
+class FutureResolver;
+
+class Future MOZ_FINAL : public nsISupports,
+                         public nsWrapperCache
+{
+  friend class FutureTask;
+  friend class FutureResolver;
+  friend class FutureResolverTask;
+
+public:
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(Future)
+
+  Future(nsPIDOMWindow* aWindow);
+  ~Future();
+
+  // WebIDL
+
+  nsPIDOMWindow* GetParentObject() const
+  {
+    return mWindow;
+  }
+
+  virtual JSObject*
+  WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope) MOZ_OVERRIDE;
+
+  static already_AddRefed<Future>
+  Constructor(const GlobalObject& aGlobal, JSContext* aCx, FutureInit& aInit,
+              ErrorResult& aRv);
+
+  void Done(AnyCallback* aResolveCallback, AnyCallback* aRejectCallback);
+
+private:
+  enum FutureState {
+    Pending,
+    Resolved,
+    Rejected
+  };
+
+  void SetState(FutureState aState)
+  {
+    MOZ_ASSERT(mState == Pending);
+    MOZ_ASSERT(aState != Pending);
+    mState = aState;
+  }
+
+  void SetResult(JS::Handle<JS::Value> aValue)
+  {
+    mResult = aValue;
+  }
+
+  // This method processes future's resolve/reject callbacks with future's
+  // result. It's executed when the resolver.resolve() or resolver.reject() is
+  // called or when the future already has a result and new callbacks are
+  // appended by then(), catch() or done().
+  void RunTask();
+
+  void AppendCallbacks(AnyCallback* aResolveCallback,
+                       AnyCallback* aRejectCallback);
+
+  nsRefPtr<nsPIDOMWindow> mWindow;
+
+  nsRefPtr<FutureResolver> mResolver;
+
+  nsTArray<nsRefPtr<AnyCallback> > mResolveCallbacks;
+  nsTArray<nsRefPtr<AnyCallback> > mRejectCallbacks;
+
+  JS::Value mResult;
+  FutureState mState;
+  bool mTaskPending;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_Future_h
---
+++ dom/future/FutureResolver.cpp
@@ -0,0 +1,142 @@
+/* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/FutureResolver.h"
+#include "mozilla/dom/FutureBinding.h"
+#include "mozilla/dom/Future.h"
+
+namespace mozilla {
+namespace dom {
+
+// FutureResolverTask
+
+// This class processes the future's callbacks with future's result.
+class FutureResolverTask MOZ_FINAL : public nsRunnable
+{
+public:
+  FutureResolverTask(FutureResolver* aResolver,
+                     const JS::Handle<JS::Value> aValue,
+                     Future::FutureState aState)
+    : mResolver(aResolver)
+    , mValue(aValue)
+    , mState(aState)
+  {
+    MOZ_ASSERT(aResolver);
+    MOZ_ASSERT(mState != Future::Pending);
+    MOZ_COUNT_CTOR(FutureResolverTask);
+
+    JSContext* cx = nsContentUtils::GetSafeJSContext();
+    JS_AddNamedValueRootRT(JS_GetRuntime(cx), &mValue,
+                           "FutureResolverTask.mValue");
+  }
+
+  ~FutureResolverTask()
+  {
+    MOZ_COUNT_DTOR(FutureResolverTask);
+
+    JSContext* cx = nsContentUtils::GetSafeJSContext();
+    JS_RemoveValueRootRT(JS_GetRuntime(cx), &mValue);
+  }
+
+  NS_IMETHOD Run()
+  {
+    mResolver->RunTask(JS::Handle<JS::Value>::fromMarkedLocation(&mValue),
+                       mState, FutureResolver::SyncTask);
+    return NS_OK;
+  }
+
+private:
+  nsRefPtr<FutureResolver> mResolver;
+  JS::Value mValue;
+  Future::FutureState mState;
+};
+
+// FutureResolver
+
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE_1(FutureResolver, mFuture)
+
+NS_IMPL_CYCLE_COLLECTING_ADDREF(FutureResolver)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(FutureResolver)
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(FutureResolver)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+NS_INTERFACE_MAP_END
+
+FutureResolver::FutureResolver(Future* aFuture)
+  : mFuture(aFuture)
+  , mResolvePending(false)
+{
+  SetIsDOMBinding();
+}
+
+JSObject*
+FutureResolver::WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope)
+{
+  return FutureResolverBinding::Wrap(aCx, aScope, this);
+}
+
+void
+FutureResolver::Resolve(JSContext* aCx,
+                        const Optional<JS::Handle<JS::Value> >& aValue,
+                        FutureTaskSync aAsynchronous)
+{
+  if (mResolvePending) {
+    return;
+  }
+
+  // TODO: if the arg is a future?
+
+  mResolvePending = true;
+
+  // If the synchronous flag is set, process future's resolve callbacks with
+  // value. Otherwise, the synchronous flag is unset, queue a task to process
+  // future's resolve callbacks with value. Otherwise, the synchronous flag is
+  // unset, queue a task to process future's resolve callbacks with value.
+  RunTask(aValue.WasPassed() ? aValue.Value() : JS::UndefinedHandleValue,
+          Future::Resolved, aAsynchronous);
+}
+
+void
+FutureResolver::Reject(JSContext* aCx,
+                       const Optional<JS::Handle<JS::Value> >& aValue,
+                       FutureTaskSync aAsynchronous)
+{
+  if (mResolvePending) {
+    return;
+  }
+
+  mResolvePending = true;
+
+  // If the synchronous flag is set, process future's reject callbacks with
+  // value. Otherwise, the synchronous flag is unset, queue a task to process
+  // future's reject callbacks with value.
+  RunTask(aValue.WasPassed() ? aValue.Value() : JS::UndefinedHandleValue,
+          Future::Rejected, aAsynchronous);
+}
+
+void
+FutureResolver::RunTask(JS::Handle<JS::Value> aValue,
+                        Future::FutureState aState,
+                        FutureTaskSync aAsynchronous)
+{
+  // If the synchronous flag is unset, queue a task to process future's
+  // accept callbacks with value.
+  if (aAsynchronous == AsyncTask) {
+    nsRefPtr<FutureResolverTask> task =
+      new FutureResolverTask(this, aValue, aState);
+    NS_DispatchToCurrentThread(task);
+    return;
+  }
+
+  mFuture->SetResult(aValue);
+  mFuture->SetState(aState);
+  mFuture->RunTask();
+  mFuture = nullptr;
+}
+
+} // namespace dom
+} // namespace mozilla
