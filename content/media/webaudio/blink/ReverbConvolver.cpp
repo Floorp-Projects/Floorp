de88d1fec1e1088819fd78b5e765885792a71a8c
Ehsan Akhgari <ehsan@mozilla.com>
2013-06-12 20:57:17 -0400

    Bug 881558 - Re-introduce try-locking in the reverb implementation; r=roc

--- content/media/webaudio/blink/ReverbConvolver.cpp
+++ content/media/webaudio/blink/ReverbConvolver.cpp
@@ -60,7 +60,7 @@ ReverbConvolver::ReverbConvolver(const float* impulseResponseData, size_t impuls
     , m_minFFTSize(MinFFTSize) // First stage will have this size - successive stages will double in size each time
     , m_maxFFTSize(maxFFTSize) // until we hit m_maxFFTSize
     , m_backgroundThread("ConvolverWorker")
-    , m_backgroundThreadMonitor("ConvolverMonitor")
+    , m_backgroundThreadCondition(&m_backgroundThreadLock)
     , m_useBackgroundThreads(useBackgroundThreads)
     , m_wantsToExit(false)
     , m_moreInputBuffered(false)
@@ -138,9 +138,9 @@ ReverbConvolver::~ReverbConvolver()
 
         // Wake up thread so it can return
         {
-            MonitorAutoLock locker(m_backgroundThreadMonitor);
+            AutoLock locker(m_backgroundThreadLock);
             m_moreInputBuffered = true;
-            locker.Notify();
+            m_backgroundThreadCondition.Signal();
         }
 
         m_backgroundThread.Stop();
@@ -153,9 +153,9 @@ void ReverbConvolver::backgroundThreadEntry()
         // Wait for realtime thread to give us more input
         m_moreInputBuffered = false;
         {
-            MonitorAutoLock locker(m_backgroundThreadMonitor);
+            AutoLock locker(m_backgroundThreadLock);
             while (!m_moreInputBuffered && !m_wantsToExit)
-                locker.Wait();
+                m_backgroundThreadCondition.Wait();
         }
 
         // Process all of the stages until their read indices reach the input buffer's write index
@@ -209,9 +209,11 @@ void ReverbConvolver::process(const float* sourceChannelData, size_t sourceChann
     // signal from time to time, since we'll get to it the next time we're called.  We're called repeatedly
     // and frequently (around every 3ms).  The background thread is processing well into the future and has a considerable amount of 
     // leeway here...
-    MonitorAutoLock locker(m_backgroundThreadMonitor);
-    m_moreInputBuffered = true;
-    locker.Notify();
+    if (m_backgroundThreadLock.Try()) {
+        m_moreInputBuffered = true;
+        m_backgroundThreadCondition.Signal();
+        m_backgroundThreadLock.Release();
+    }
 }
 
 void ReverbConvolver::reset()
