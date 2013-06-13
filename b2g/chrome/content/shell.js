0e0c548fd6024f232ce2f99104bb90e9cda30156
Nikhil Marathe <nsm.nikhil@gmail.com>
2013-06-12 17:26:44 -0700

    Bug 863599 - Load PushService on Desktop. r=dougt sr=gavin

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -12,7 +12,6 @@ Cu.import('resource://gre/modules/DOMFMRadioParent.jsm');
 Cu.import('resource://gre/modules/AlarmService.jsm');
 Cu.import('resource://gre/modules/ActivitiesService.jsm');
 Cu.import('resource://gre/modules/PermissionPromptHelper.jsm');
-Cu.import('resource://gre/modules/PushService.jsm');
 Cu.import('resource://gre/modules/ObjectWrapper.jsm');
 Cu.import('resource://gre/modules/accessibility/AccessFu.jsm');
 Cu.import('resource://gre/modules/Payment.jsm');
