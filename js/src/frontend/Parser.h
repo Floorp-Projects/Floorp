eb6be0d8c3a8923e1ecebf3bb36c39e32bb28bad
Terrence Cole <terrence@mozilla.com>
2013-06-14 13:48:39 -0700

    Bug 848592 - Fix some dynamic rooting analysis failures; r=sfink

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -36,7 +36,6 @@ typedef HashSet<JSAtom *> FuncStmtSet;
 class SharedContext;
 
 typedef Vector<Definition *, 16> DeclVector;
-typedef Vector<JSFunction *, 4> FunctionVector;
 
 struct GenericParseContext
 {
@@ -201,7 +200,7 @@ struct ParseContext : public GenericParseContext
                                        the same name. */
 
     // All inner functions in this context. Only filled in when parsing syntax.
-    FunctionVector innerFunctions;
+    AutoFunctionVector innerFunctions;
 
     // Set when parsing a declaration-like destructuring pattern.  This flag
     // causes PrimaryExpr to create PN_NAME parse nodes for variable references
