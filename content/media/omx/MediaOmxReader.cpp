4111447dffdd3d895f1e9ad1d4cf8992814da43d
Robert O'Callahan <robert@ocallahan.org>
2013-06-15 23:51:56 +1200

    Bug 880601. Emulate a form of keyframe skipping by allowing MediaOmxReader::DecodeVideoFrame to decode more than one video frame if video decoding is falling behind. r=cpearce If we don't support this in any way, then we get into trouble when there is plenty of compressed data buffered but for some reason video decoding falls behind (maybe there was a short period where the video data wasn't available). Audio playback continues normally, but MediaOmxReader keeps making one call to ReadAudio and one to ReadVideo, and if each call to ReadAudio decodes at least as much as the calls to ReadVideo, video decoding can never catch up to the current audio playback position. So video never plays. And when the video decoding point gets far enough away from the audio decoding point, horrible things start to happen, such as video and audio reading completely different parts of the media cache and interfering with the caching mechanism (which assumes audio and video reads are close together).
    This patch approximates keyframe skipping, and is basically just a way for
    MediaOmxReader::DecodeVideoFrame to decode more than one frame at a time
    if we need video decoding to catch up.
    
    To prevent pathological situations, we cap both the maximum number of frames
    decoded per DecodeVideoFrame and the amount of time we spend in the method
    (excluding the time for the last frame).

--- content/media/omx/MediaOmxReader.cpp
+++ content/media/omx/MediaOmxReader.cpp
@@ -17,6 +17,8 @@
 #include "MPAPI.h"
 
 #define MAX_DROPPED_FRAMES 25
+// Try not to spend more than this much time in a single call to DecodeVideoFrame.
+#define MAX_VIDEO_DECODE_SECONDS 3.0
 
 using namespace android;
 
@@ -156,8 +158,10 @@ bool MediaOmxReader::DecodeVideoFrame(bool &aKeyframeSkip,
     aTimeThreshold = mVideoSeekTimeUs;
   }
 
-  // Read next frame
-  while (true) {
+  TimeStamp start = TimeStamp::Now();
+
+  // Read next frame. Don't let this loop run for too long.
+  while ((TimeStamp::Now() - start) < TimeDuration::FromSeconds(MAX_VIDEO_DECODE_SECONDS)) {
     MPAPI::VideoFrame frame;
     frame.mGraphicBuffer = nullptr;
     frame.mShouldSkip = false;
@@ -165,22 +169,23 @@ bool MediaOmxReader::DecodeVideoFrame(bool &aKeyframeSkip,
       mVideoQueue.Finish();
       return false;
     }
+    doSeek = false;
 
     // Ignore empty buffer which stagefright media read will sporadically return
     if (frame.mSize == 0 && !frame.mGraphicBuffer) {
-      return true;
+      continue;
     }
 
     parsed++;
     if (frame.mShouldSkip && mSkipCount < MAX_DROPPED_FRAMES) {
       mSkipCount++;
-      return true;
+      continue;
     }
 
     mSkipCount = 0;
 
     mVideoSeekTimeUs = -1;
-    doSeek = aKeyframeSkip = false;
+    aKeyframeSkip = false;
 
     nsIntRect picture = mPicture;
     if (frame.Y.mWidth != mInitialFrame.width ||
