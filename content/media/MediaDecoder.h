369dce9cd23e88df4d4a9a5d97cced7c8cae3c35
Robert O'Callahan <robert@ocallahan.org>
2013-06-16 01:28:34 +1200

    Backing out bug 882027

--- content/media/MediaDecoder.h
+++ content/media/MediaDecoder.h
@@ -710,6 +710,12 @@ public:
   // This must be called on the main thread only.
   void PlaybackPositionChanged();
 
+  // Calls mElement->UpdateReadyStateForData, telling it which state we have
+  // entered.  Main thread only.
+  void NextFrameUnavailableBuffering();
+  void NextFrameAvailable();
+  void NextFrameUnavailable();
+
   // Calls mElement->UpdateReadyStateForData, telling it whether we have
   // data for the next frame and if we're buffering. Main thread only.
   void UpdateReadyStateForData();
