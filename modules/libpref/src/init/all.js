97cd9f871d477a2fba42cfcc6aa9d0e865249e4e
Georg Fritzsche <georg.fritzsche@googlemail.com>
2013-06-17 12:35:29 +0200

    Bug 883671 - Get rid of plugin.expose_full_path. r=bsmedberg

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -1804,7 +1804,6 @@ pref("capability.policy.default.SOAPCall.invokeVerifySourceHeader", "allAccess")
 
 // if true, allow plug-ins to override internal imglib decoder mime types in full-page mode
 pref("plugin.override_internal_types", false);
-pref("plugin.expose_full_path", false); // if true navigator.plugins reveals full path
 
 // See bug 136985.  Gives embedders a pref to hook into to show
 // a popup blocker if they choose.
