844d09e5ca22d1d31e5b505758f787f669775737
Robert O'Callahan <robert@ocallahan.org>
2013-06-17 17:20:46 +1200

    Bug 876092. Copy the DisplayItemClip into OutOfFlowDisplayData instead of storing a pointer. r=mats
    --HG--
    extra : rebase_source : d941179d66854ab98b9f0aba40d33b534e69378e

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -2107,7 +2107,7 @@ nsIFrame::BuildDisplayListForChild(nsDisplayListBuilder*   aBuilder,
 
   if (savedOutOfFlowData) {
     clipState.SetClipForContainingBlockDescendants(
-      savedOutOfFlowData->mContainingBlockClip);
+      &savedOutOfFlowData->mContainingBlockClip);
   }
 
   // Setup clipping for the parent's overflow:-moz-hidden-unscrollable,
