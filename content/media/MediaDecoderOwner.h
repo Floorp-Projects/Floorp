a76e7dbf3fb0780b44d9dfbd3ed26022ae81cb50
Robert O'Callahan <robert@ocallahan.org>
2013-06-19 00:17:40 +1200

    Bug 883731. Part 2: Remove ResourceLoaded notifications since they don't make much sense with a media cache. r=cpearce
    --HG--
    extra : rebase_source : 8c1657977339c8f97b109c4ec775b7818ed4d57a

--- content/media/MediaDecoderOwner.h
+++ content/media/MediaDecoderOwner.h
@@ -65,11 +65,7 @@ public:
   // when it has read the first frame of the video
   // aResourceFullyLoaded should be true if the resource has been
   // fully loaded and the caller will call ResourceLoaded next.
-  virtual void FirstFrameLoaded(bool aResourceFullyLoaded) = 0;
-
-  // Called by the video decoder object, on the main thread,
-  // when the resource has completed downloading.
-  virtual void ResourceLoaded() = 0;
+  virtual void FirstFrameLoaded() = 0;
 
   // Called by the video decoder object, on the main thread,
   // when the resource has a network error during loading.
