83acb04b5835dc241dac068e7cffb07bd1c97f75
Asaf Romano <mano@mozilla.com>
2013-06-19 10:18:38 +0300

    Relanding Bug 834539 - Replace getPageTitle with an async API. r=mak. sr=gavin.

--- toolkit/components/places/Helpers.cpp
+++ toolkit/components/places/Helpers.cpp
@@ -300,7 +300,7 @@ GenerateGUID(nsCString& _guid)
 }
 
 bool
-IsValidGUID(const nsCString& aGUID)
+IsValidGUID(const nsACString& aGUID)
 {
   nsCString::size_type len = aGUID.Length();
   if (len != GUID_LENGTH) {
