f9fd520c0c46cd220a7f248022c3d9a16aa88283
Benjamin Smedberg <benjamin@smedbergs.us>
2013-06-19 13:29:05 -0400

    Bug 882858 - Add a "showing" notification to PopupNotifications, r=jaws sr=gavin

--- browser/base/content/test/browser_popupNotification.js
+++ browser/base/content/test/browser_popupNotification.js
@@ -137,6 +137,9 @@ function basicNotification() {
         case "dismissed":
           self.dismissalCallbackTriggered = true;
           break;
+        case "showing":
+          self.showingCallbackTriggered = true;
+          break;
         case "shown":
           self.shownCallbackTriggered = true;
           break;
@@ -858,6 +861,27 @@ var tests = [
       this.notification1.remove();
       this.notification2.remove();
     }
+  },
+  { // Test #30 - Showing should be able to modify the popup data
+    run: function() {
+      this.notifyObj = new basicNotification();
+      var normalCallback = this.notifyObj.options.eventCallback;
+      this.notifyObj.options.eventCallback = function (eventName) {
+        if (eventName == "showing") {
+          this.mainAction.label = "Alternate Label";
+        }
+        normalCallback.call(this, eventName);
+      };
+      showNotification(this.notifyObj);
+    },
+    onShown: function(popup) {
+      // checkPopup checks for the matching label. Note that this assumes that
+      // this.notifyObj.mainAction is the same as notification.mainAction,
+      // which could be a problem if we ever decided to deep-copy.
+      checkPopup(popup, this.notifyObj);
+      triggerMainCommand(popup);
+    },
+    onHidden: function() { }
   }
 ];
 
@@ -874,6 +898,7 @@ function showNotification(notifyObj) {
 function checkPopup(popup, notificationObj) {
   info("[Test #" + gTestIndex + "] checking popup");
 
+  ok(notificationObj.showingCallbackTriggered, "showing callback was triggered");
   ok(notificationObj.shownCallbackTriggered, "shown callback was triggered");
 
   let notifications = popup.childNodes;
