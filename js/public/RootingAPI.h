e0a70e1fe38a941d6b7ea51c97962e48547f132b
Emanuel Hoogeveen <emanuel.hoogeveen@gmail.com>
2013-06-19 17:59:09 -0700

    Bug 883697 (part 1) - Make include guards consistent in js/ductwork/, js/ipc/ and js/public/.  r=njn.
    --HG--
    extra : rebase_source : 34f5559ba6e64831905686e12356b70109d873a8

--- js/public/RootingAPI.h
+++ js/public/RootingAPI.h
@@ -4,8 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef jsgc_root_h__
-#define jsgc_root_h__
+#ifndef js_RootingAPI_h
+#define js_RootingAPI_h
 
 #include "mozilla/GuardObjects.h"
 #include "mozilla/TypeTraits.h"
@@ -931,4 +931,4 @@ class CompilerRootNode
 
 }  /* namespace js */
 
-#endif  /* jsgc_root_h___ */
+#endif  /* js_RootingAPI_h */
