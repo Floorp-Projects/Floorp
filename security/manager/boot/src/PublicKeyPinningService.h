18eaeebe0bd4a4eb59cc4d233ebae1c904ce4e34
Camilo Viecco <cviecco@mozilla.com>
2013-06-20 10:35:43 -0700

    Bug 744204 - Allow Key pining part 1 - Built-in Pinning Service. r=keeler

---
+++ security/manager/boot/src/PublicKeyPinningService.h
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef PublicKeyPinningService_h
+#define PublicKeyPinningService_h
+
+#include "cert.h"
+
+namespace mozilla {
+namespace psm {
+
+class PublicKeyPinningService
+{
+public:
+  /**
+   *  Check that there is an intersection of the keys in the given certificate
+   *  chain and the pin set specified by the hostname.If the hostname is null or
+   *  empty evaluate
+   *  against all the possible names for the EE cert (Common Name (CN) plus
+   *  all DNS Name: subject Alt Name entries).
+   *  The certList's head is the EE cert and the tail is the trust anchor.
+   *  Note:  if an alt name is a wildcard, it won't necessarily find a pinset
+   *  that would otherwise be valid for it
+   */
+  static bool ChainHasValidPins(const CERTCertList* certList,
+                                const char* hostname,
+                                const PRTime time);
+};
+
+}} // namespace mozilla::psm
+
+#endif // PublicKeyPinningServiceService_h
