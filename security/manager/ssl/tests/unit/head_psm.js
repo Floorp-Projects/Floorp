929293591071bb006d5f1dd8e1be58c4ba455f08
Ryan VanderMeulen <ryanvm@gmail.com>
2013-06-20 19:50:13 -0400

    Backed out changesets 448ba56d9ba4 and 8cea4b4646ef (bug 700693) for intermittent failures.

--- security/manager/ssl/tests/unit/head_psm.js
+++ security/manager/ssl/tests/unit/head_psm.js
@@ -7,10 +7,9 @@
 const { 'classes': Cc, 'interfaces': Ci, 'utils': Cu, 'results': Cr } = Components;
 
 let { NetUtil } = Cu.import("resource://gre/modules/NetUtil.jsm", {});
-let { FileUtils } = Cu.import("resource://gre/modules/FileUtils.jsm", {});
-let { Services } = Cu.import("resource://gre/modules/Services.jsm", {});
 
-let gIsWindows = ("@mozilla.org/windows-registry-key;1" in Cc);
+Cu.import("resource://gre/modules/FileUtils.jsm"); // XXX: tempScope?
+Cu.import("resource://gre/modules/Services.jsm");  // XXX: tempScope?
 
 function readFile(file) {
   let fstream = Cc["@mozilla.org/network/file-input-stream;1"]
@@ -25,11 +24,4 @@ function addCertFromFile(certdb, filename, trustString) {
   let certFile = do_get_file(filename, false);
   let der = readFile(certFile);
   certdb.addCert(der, trustString, null);
-}
-
-function getXPCOMStatusFromNSS(offset) {
-  let nssErrorsService = Cc["@mozilla.org/nss_errors_service;1"]
-                           .getService(Ci.nsINSSErrorsService);
-  let statusNSS = Ci.nsINSSErrorsService.NSS_SEC_ERROR_BASE + offset;
-  return nssErrorsService.getXPCOMFromNSSError(statusNSS);
 }
