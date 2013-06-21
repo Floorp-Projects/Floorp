538c92808de0e766cf97c041ce712b676d13cdb6
Jeff Gilbert <jgilbert@mozilla.com>
2013-06-21 16:42:32 -0700

    Bug 880734 - Assert bound to correct FB in FB funcs, and bind the correct FB on Mesa. - r=bjacob

--- content/canvas/src/WebGLFramebuffer.cpp
+++ content/canvas/src/WebGLFramebuffer.cpp
@@ -163,6 +163,7 @@ WebGLFramebuffer::FramebufferRenderbuffer(WebGLenum target,
                              WebGLenum rbtarget,
                              WebGLRenderbuffer *wrb)
 {
+    MOZ_ASSERT(mContext->mBoundFramebuffer == this);
     if (!mContext->ValidateObjectAllowNull("framebufferRenderbuffer: renderbuffer", wrb))
     {
         return;
@@ -223,6 +224,7 @@ WebGLFramebuffer::FramebufferTexture2D(WebGLenum target,
                           WebGLTexture *wtex,
                           WebGLint level)
 {
+    MOZ_ASSERT(mContext->mBoundFramebuffer == this);
     if (!mContext->ValidateObjectAllowNull("framebufferTexture2D: texture",
                                            wtex))
     {
@@ -383,6 +385,7 @@ WebGLFramebuffer::DetachRenderbuffer(const WebGLRenderbuffer *rb) {
 bool
 WebGLFramebuffer::CheckAndInitializeRenderbuffers()
 {
+    MOZ_ASSERT(mContext->mBoundFramebuffer == this);
     // enforce WebGL section 6.5 which is WebGL-specific, hence OpenGL itself would not
     // generate the INVALID_FRAMEBUFFER_OPERATION that we need here
     if (HasDepthStencilConflict())
