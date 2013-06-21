2a2504e4922f950ab5cf8c340c6860ce61b18b20
Jeff Gilbert <jgilbert@mozilla.com>
2013-06-21 16:33:57 -0700

    Backout 01aec9db40d9 for bustage on a CLOSED TREE

--- content/canvas/src/WebGLFramebuffer.cpp
+++ content/canvas/src/WebGLFramebuffer.cpp
@@ -155,7 +155,6 @@ WebGLFramebuffer::FramebufferRenderbuffer(WebGLenum target,
                              WebGLenum rbtarget,
                              WebGLRenderbuffer *wrb)
 {
-    MOZ_ASSERT(mContext->mBoundFramebuffer == this);
     if (!mContext->ValidateObjectAllowNull("framebufferRenderbuffer: renderbuffer", wrb))
     {
         return;
@@ -213,7 +212,6 @@ WebGLFramebuffer::FramebufferTexture2D(WebGLenum target,
                           WebGLTexture *wtex,
                           WebGLint level)
 {
-    MOZ_ASSERT(mContext->mBoundFramebuffer == this);
     if (!mContext->ValidateObjectAllowNull("framebufferTexture2D: texture",
                                            wtex))
     {
@@ -312,7 +310,6 @@ WebGLFramebuffer::DetachRenderbuffer(const WebGLRenderbuffer *rb) {
 bool
 WebGLFramebuffer::CheckAndInitializeRenderbuffers()
 {
-    MOZ_ASSERT(mContext->mBoundFramebuffer == this);
     // enforce WebGL section 6.5 which is WebGL-specific, hence OpenGL itself would not
     // generate the INVALID_FRAMEBUFFER_OPERATION that we need here
     if (HasDepthStencilConflict())
