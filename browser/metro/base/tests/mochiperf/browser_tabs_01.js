04d742a8c9021e6fb6b369e0438cf9c475ba90b8
Jim Mathies <jmathies@mozilla.com>
2013-06-21 15:50:31 -0500

    Bug 885536 - Do a better job of cleaning up tabs in browser_tabs test. r=sfoster

--- browser/metro/base/tests/mochiperf/browser_tabs_01.js
+++ browser/metro/base/tests/mochiperf/browser_tabs_01.js
@@ -15,7 +15,7 @@ function timeTab(aUrl) {
     let tab = Browser.addTab(aUrl, true);
     yield tab.pageShowPromise;
     stopwatch.stop();
-    Browser.closeTab(tab)
+    Browser.closeTab(tab, { forceClose: true })
     yield waitForMs(500);
     throw new Task.Result(stopwatch.time());
   });
@@ -33,6 +33,10 @@ gTests.push({
       let time = yield timeTab("about:blank");
       openDataSet.push(time);
     }
+
+    // Give the browser a little time to clean up its tabs. 2 because
+    // we open a tab above as a base to get rid of the start screen.
+    yield waitForCondition(() => Browser.tabs.length == 2);
     
     PerfTest.declareTest("FBD7A532-D63A-44B5-9744-5CB07CFD131A",
                          "tab open", "browser", "ux",
