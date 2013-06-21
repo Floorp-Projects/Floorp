bb1f9b3c0922a09a22b6b2d7feb31d091e563ee2
David Rajchenbach-Teller <dteller@mozilla.com>
2013-06-21 17:48:31 +0200

    Bug 837326 - Informing observers upon accept/reject of a third-party cookie;r=ehsan

--- netwerk/cookie/nsCookieService.h
+++ netwerk/cookie/nsCookieService.h
@@ -287,6 +287,7 @@ class nsCookieService : public nsICookieService
     bool                          FindCookie(const nsCookieKey& aKey, const nsAFlatCString &aHost, const nsAFlatCString &aName, const nsAFlatCString &aPath, nsListIter &aIter);
     static void                   FindStaleCookie(nsCookieEntry *aEntry, int64_t aCurrentTime, nsListIter &aIter);
     void                          NotifyRejected(nsIURI *aHostURI);
+    void                          NotifyThirdParty(nsIURI *aHostURI, bool aAccepted, nsIChannel *aChannel);
     void                          NotifyChanged(nsISupports *aSubject, const PRUnichar *aData);
     void                          NotifyPurged(nsICookie2* aCookie);
     already_AddRefed<nsIArray>    CreatePurgeList(nsICookie2* aCookie);
