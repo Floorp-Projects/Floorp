be5aae013e22b28dbae812b75fc9f33cf9d0dc0e
Margaret Leibovic <margaret.leibovic@gmail.com>
2013-06-21 20:36:35 -0700

    Bug 884069 - Refactor DoorHanger to create a better Java API that doesn't depend on JSON. r=sriram,wesj

--- mobile/android/base/DoorHanger.java
+++ mobile/android/base/DoorHanger.java
@@ -15,6 +15,7 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.os.Build;
 import android.text.SpannableString;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.URLSpan;
@@ -32,39 +33,48 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DoorHanger extends LinearLayout implements Button.OnClickListener {
+public class DoorHanger extends LinearLayout {
     private static final String LOGTAG = "GeckoDoorHanger";
-
-    // The popup that holds this doorhanger
-    private DoorHangerPopup mPopup;
-    private LinearLayout mChoicesLayout;
-    private TextView mTextView;
-    private List<PromptInput> mInputs;
 
     private static int sInputPadding = -1;
     private static int sSpinnerTextColor = -1;
     private static int sSpinnerTextSize = -1;
 
-    // LayoutParams used for adding button layouts
-    static private LayoutParams mLayoutParams;
-    private final int mTabId;
-    // Value used to identify the notification
-    private final String mValue;
+    private static LayoutParams sButtonParams;
+    static {
+        sButtonParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, 1.0f);
+    }
 
-    // Optional checkbox added underneath message text
-    private CheckBox mCheckBox;
+    private final TextView mTextView;
+    private final LinearLayout mChoicesLayout;
 
     // Divider between doorhangers.
-    private View mDivider;
+    private final View mDivider;
+
+    // The tab associated with this notification.
+    private final int mTabId;
+
+    // Value used to identify the notification.
+    private final String mValue;
+
+    private List<PromptInput> mInputs;
+    private CheckBox mCheckBox;
 
     private int mPersistence = 0;
     private boolean mPersistWhileVisible = false;
     private long mTimeout = 0;
 
-    DoorHanger(Context context, DoorHangerPopup popup, int tabId, String value) {
+    public interface OnButtonClickListener {
+        public void onButtonClick(DoorHanger dh, String tag);
+    }
+
+    DoorHanger(Context context) {
+        this(context, 0, null);
+    }
+
+    DoorHanger(Context context, int tabId, String value) {
         super(context);
 
-        mPopup = popup;
         mTabId = tabId;
         mValue = value;
 
@@ -77,8 +87,15 @@ public class DoorHanger extends LinearLayout implements Button.OnClickListener {
         if (sSpinnerTextSize == -1) {
             sSpinnerTextSize = getResources().getDimensionPixelSize(R.dimen.doorhanger_spinner_textsize);
         }
+
+        setOrientation(VERTICAL);
+
+        LayoutInflater.from(context).inflate(R.layout.doorhanger, this);
+        mTextView = (TextView) findViewById(R.id.doorhanger_title);
+        mChoicesLayout = (LinearLayout) findViewById(R.id.doorhanger_choices);
+        mDivider = findViewById(R.id.divider_doorhanger);
     }
- 
+
     int getTabId() {
         return mTabId;
     }
@@ -87,134 +104,93 @@ public class DoorHanger extends LinearLayout implements Button.OnClickListener {
         return mValue;
     }
 
-    public void showDivider() {
+    List<PromptInput> getInputs() {
+        return mInputs;
+    }
+
+    CheckBox getCheckBox() {
+        return mCheckBox;
+    }
+
+    void showDivider() {
         mDivider.setVisibility(View.VISIBLE);
     }
 
-    public void hideDivider() {
+    void hideDivider() {
         mDivider.setVisibility(View.GONE);
     }
 
-    // Postpone stuff that needs to be done on the main thread
-    void init(String message, JSONArray buttons, JSONObject options) {
-        setOrientation(VERTICAL);
-
-        LayoutInflater.from(getContext()).inflate(R.layout.doorhanger, this);
-        setVisibility(View.GONE);
-
-        mTextView = (TextView) findViewById(R.id.doorhanger_title);
+    void setMessage(String message) {
         mTextView.setText(message);
-
-        mChoicesLayout = (LinearLayout) findViewById(R.id.doorhanger_choices);
-
-        mDivider = findViewById(R.id.divider_doorhanger);
-
-        // Set the doorhanger text and buttons
-        for (int i = 0; i < buttons.length(); i++) {
-            try {
-                JSONObject buttonObject = buttons.getJSONObject(i);
-                String label = buttonObject.getString("label");
-                int callBackId = buttonObject.getInt("callback");
-                addButton(label, callBackId);
-            } catch (JSONException e) {
-                Log.e(LOGTAG, "Error creating doorhanger button", e);
-            }
-         }
-
-        // Enable the button layout if we have buttons.
-        if (buttons.length() > 0) {
-            findViewById(R.id.divider_choices).setVisibility(View.VISIBLE);
-            mChoicesLayout.setVisibility(View.VISIBLE);
-        }
-
-        setOptions(options);
     }
 
-    private void addButton(String aText, int aCallback) {
-        if (mLayoutParams == null)
-            mLayoutParams = new LayoutParams(LayoutParams.FILL_PARENT,
-                                             LayoutParams.FILL_PARENT,
-                                             1.0f);
+    void addLink(String label, String url, String delimiter) {
+        String title = mTextView.getText().toString();
+        SpannableString titleWithLink = new SpannableString(title + delimiter + label);
+        URLSpan linkSpan = new URLSpan(url) {
+            @Override
+            public void onClick(View view) {
+                Tabs.getInstance().loadUrlInTab(getURL());
+            }
+        };
 
-        Button button = (Button) LayoutInflater.from(getContext()).inflate(R.layout.doorhanger_button, null);
-        button.setText(aText);
-        button.setTag(Integer.toString(aCallback));
-        button.setOnClickListener(this);
+        // Prevent text outside the link from flashing when clicked.
+        ForegroundColorSpan colorSpan = new ForegroundColorSpan(mTextView.getCurrentTextColor());
+        titleWithLink.setSpan(colorSpan, 0, title.length(), 0);
 
-        if (mChoicesLayout.getChildCount() > 0) {
+        titleWithLink.setSpan(linkSpan, title.length() + 1, titleWithLink.length(), 0);
+        mTextView.setText(titleWithLink);
+        mTextView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    void addButton(final String text, final String tag, final OnButtonClickListener listener) {
+        final Button button = (Button) LayoutInflater.from(getContext()).inflate(R.layout.doorhanger_button, null);
+        button.setText(text);
+        button.setTag(tag);
+
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onButtonClick(DoorHanger.this, tag);
+            }
+        });
+
+        if (mChoicesLayout.getChildCount() == 0) {
+            // If this is the first button we're adding, make the choices layout visible.
+            mChoicesLayout.setVisibility(View.VISIBLE);
+            findViewById(R.id.divider_choices).setVisibility(View.VISIBLE);
+        } else {
+            // Add a divider for additional buttons.
             Divider divider = new Divider(getContext(), null);
             divider.setOrientation(Divider.Orientation.VERTICAL);
             divider.setBackgroundColor(0xFFD1D5DA);
             mChoicesLayout.addView(divider);
         }
 
-        mChoicesLayout.addView(button, mLayoutParams);
+        mChoicesLayout.addView(button, sButtonParams);
     }
 
-    @Override
-    public void onClick(View v) {
-        JSONObject response = new JSONObject();
-        try {
-            response.put("callback", v.getTag().toString());
-
-            // If the checkbox is being used, pass its value
-            if (mCheckBox != null)
-                response.put("checked", mCheckBox.isChecked());
-
-            if (mInputs != null) {
-                JSONObject inputs = new JSONObject();
-                for (PromptInput input : mInputs) {
-                    inputs.put(input.getId(), input.getValue());
-                }
-                response.put("inputs", inputs);
-            }
-        } catch (JSONException e) {
-            Log.e(LOGTAG, "Error creating onClick response", e);
+    void setOptions(final JSONObject options) {
+        final int persistence = options.optInt("persistence");
+        if (persistence > 0) {
+            mPersistence = persistence;
         }
 
-        GeckoEvent e = GeckoEvent.createBroadcastEvent("Doorhanger:Reply", response.toString());
-        GeckoAppShell.sendEventToGecko(e);
-        mPopup.removeDoorHanger(this);
+        mPersistWhileVisible = options.optBoolean("persistWhileVisible");
 
-        // This will hide the doorhanger (and hide the popup if there are no
-        // more doorhangers to show)
-        mPopup.updatePopup();
-    }
+        final long timeout = options.optLong("timeout");
+        if (timeout > 0) {
+            mTimeout = timeout;
+        }
 
-    private void setOptions(final JSONObject options) {
-        try {
-            mPersistence = options.getInt("persistence");
-        } catch (JSONException e) { }
-
-        try {
-            mPersistWhileVisible = options.getBoolean("persistWhileVisible");
-        } catch (JSONException e) { }
-
-        try {
-            mTimeout = options.getLong("timeout");
-        } catch (JSONException e) { }
-
-        try {
-            JSONObject link = options.getJSONObject("link");
-            String title = mTextView.getText().toString();
-            String linkLabel = link.getString("label");
-            String linkUrl = link.getString("url");
-            SpannableString titleWithLink = new SpannableString(title + " " + linkLabel);
-            URLSpan linkSpan = new URLSpan(linkUrl) {
-                @Override
-                public void onClick(View view) {
-                    Tabs.getInstance().loadUrlInTab(this.getURL());
-                }
-            };
-
-            // prevent text outside the link from flashing when clicked
-            ForegroundColorSpan colorSpan = new ForegroundColorSpan(mTextView.getCurrentTextColor());
-            titleWithLink.setSpan(colorSpan, 0, title.length(), 0);
-
-            titleWithLink.setSpan(linkSpan, title.length() + 1, titleWithLink.length(), 0);
-            mTextView.setText(titleWithLink);
-            mTextView.setMovementMethod(LinkMovementMethod.getInstance());
-        } catch (JSONException e) { }
+        final JSONObject link = options.optJSONObject("link");
+        if (link != null) {
+            try {
+                final String linkLabel = link.getString("label");
+                final String linkUrl = link.getString("url");
+                addLink(linkLabel, linkUrl, " ");
+            } catch (JSONException e) { }
+        }
 
         final JSONArray inputs = options.optJSONArray("inputs");
         if (inputs != null) {
@@ -235,12 +211,12 @@ public class DoorHanger extends LinearLayout implements Button.OnClickListener {
             }
         }
 
-        try {
-            String checkBoxText = options.getString("checkbox");
+        final String checkBoxText = options.optString("checkbox");
+        if (!TextUtils.isEmpty(checkBoxText)) {
             mCheckBox = (CheckBox) findViewById(R.id.doorhanger_checkbox);
             mCheckBox.setText(checkBoxText);
             mCheckBox.setVisibility(VISIBLE);
-        } catch (JSONException e) { }
+        }
     }
 
     private void styleInput(PromptInput input, View view) {
@@ -295,10 +271,15 @@ public class DoorHanger extends LinearLayout implements Button.OnClickListener {
         }
     }
 
-    // This method checks with persistence and timeout options to see if
-    // it's okay to remove a doorhanger.
-    boolean shouldRemove() {
-        if (mPersistWhileVisible && mPopup.isShowing()) {
+
+    /*
+     * Checks with persistence and timeout options to see if it's okay to remove a doorhanger.
+     *
+     * @param isShowing Whether or not this doorhanger is currently visible to the user.
+     *                 (e.g. the DoorHanger view might be VISIBLE, but its parent could be hidden)
+     */
+    boolean shouldRemove(boolean isShowing) {
+        if (mPersistWhileVisible && isShowing) {
             // We still want to decrement mPersistence, even if the popup is showing
             if (mPersistence != 0)
                 mPersistence--;
