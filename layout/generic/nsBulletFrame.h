046205da93a171896a12b76bd21ce58230787cb6
Seth Fowler <seth@mozilla.com>
2013-06-22 11:30:57 -0500

    Bug 695763. Part 2. Invalidate list style images if they are not decoded and we are asked to do a sync decode. r=roc

--- layout/generic/nsBulletFrame.h
+++ layout/generic/nsBulletFrame.h
@@ -15,6 +15,7 @@
 #include "imgIRequest.h"
 #include "imgINotificationObserver.h"
 
+class imgIContainer;
 class imgRequestProxy;
 
 #define BULLET_FRAME_IMAGE_LOADING NS_FRAME_STATE_BIT(63)
@@ -100,6 +101,8 @@ public:
   void SetFontSizeInflation(float aInflation);
 
   int32_t GetOrdinal() { return mOrdinal; }
+
+  already_AddRefed<imgIContainer> GetImage() const;
 
 protected:
   nsresult OnStartContainer(imgIRequest *aRequest, imgIContainer *aImage);
