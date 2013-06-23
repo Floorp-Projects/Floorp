7bb320d169c3d5e35f870a7f388be072ca88eccc
Ms2ger <ms2ger@gmail.com>
2013-06-23 09:14:15 +0200

    Bug 878950 - Add a test for HTMLImageElement and fix h/vspace reflection; r=mounir

--- content/html/content/src/HTMLImageElement.cpp
+++ content/html/content/src/HTMLImageElement.cpp
@@ -106,6 +106,7 @@ HTMLImageElement::SetItemValueText(const nsAString& aValue)
 
 // crossorigin is not "limited to only known values" per spec, so it's
 // just a string attr purposes of the DOM crossOrigin property.
+// TODO: It is now (bug 880997).
 NS_IMPL_STRING_ATTR(HTMLImageElement, CrossOrigin, crossorigin)
 
 bool
