0b602c6c635091b89ba653ad9237f6df0f7de27e
Frank Yan <fyan@mozilla.com>
2013-06-24 00:32:44 -0700

    Bug 880672:  Add support for unprefixed cursor:grab and cursor:grabbing.  r=bzbarsky  sr=dbaron

--- layout/style/nsCSSProps.cpp
+++ layout/style/nsCSSProps.cpp
@@ -911,6 +911,8 @@ const int32_t nsCSSProps::kCursorKTable[] = {
   eCSSKeyword_ns_resize, NS_STYLE_CURSOR_NS_RESIZE,
   eCSSKeyword_ew_resize, NS_STYLE_CURSOR_EW_RESIZE,
   eCSSKeyword_none, NS_STYLE_CURSOR_NONE,
+  eCSSKeyword_grab, NS_STYLE_CURSOR_GRAB,
+  eCSSKeyword_grabbing, NS_STYLE_CURSOR_GRABBING,
   eCSSKeyword_zoom_in, NS_STYLE_CURSOR_ZOOM_IN,
   eCSSKeyword_zoom_out, NS_STYLE_CURSOR_ZOOM_OUT,
   // -moz- prefixed vendor specific
