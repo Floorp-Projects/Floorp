b760c589d9a611d1d65c25d7a42f3def88d51dcd
Ehsan Akhgari <ehsan@mozilla.com>
2013-06-24 10:40:58 -0700

    Bug 865253 - Part 1: Implement the DOM bindings for OscillatorNode; r=roc, a=webaudio

--- dom/webidl/WebIDL.mk
+++ dom/webidl/WebIDL.mk
@@ -213,6 +213,7 @@ webidl_files = \
   OfflineAudioCompletionEvent.webidl \
   OfflineAudioContext.webidl \
   OfflineResourceList.webidl \
+  OscillatorNode.webidl \
   PaintRequest.webidl \
   PaintRequestList.webidl \
   PannerNode.webidl \
