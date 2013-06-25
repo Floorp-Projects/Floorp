0ad62b74f9d7ceb9165ae2cc3ab7d4486fc37ab0
Jonathan Kew <jkew@mozilla.com>
2013-06-25 09:14:03 +0100

    bug 878674 - pt 1 - load Fennec bundled fonts directly from omnijar without copying them out to the filesystem. r=roc

--- gfx/thebes/gfxFT2FontList.h
+++ gfx/thebes/gfxFT2FontList.h
@@ -23,16 +23,17 @@ using mozilla::dom::FontListEntry;
 
 class FontNameCache;
 typedef struct FT_FaceRec_* FT_Face;
+class nsZipArchive;
 
 class FT2FontEntry : public gfxFontEntry
 {
 public:
     FT2FontEntry(const nsAString& aFaceName) :
-        gfxFontEntry(aFaceName)
+        gfxFontEntry(aFaceName),
+        mFTFace(nullptr),
+        mFontFace(nullptr),
+        mFTFontIndex(0)
     {
-        mFTFace = nullptr;
-        mFontFace = nullptr;
-        mFTFontIndex = 0;
     }
 
     ~FT2FontEntry();
@@ -53,21 +54,29 @@ public:
     CreateFontEntry(const FontListEntry& aFLE);
 
     // Create a font entry for a given freetype face; if it is an installed font,
-    // also record the filename and index
+    // also record the filename and index.
     // aFontData (if non-NULL) is NS_Malloc'ed data that aFace depends on,
     // to be freed after the face is destroyed
     static FT2FontEntry* 
-    CreateFontEntry(FT_Face aFace, const char *aFilename, uint8_t aIndex,
+    CreateFontEntry(FT_Face aFace,
+                    const char *aFilename, uint8_t aIndex,
                     const nsAString& aName,
                     const uint8_t *aFontData = nullptr);
 
     virtual gfxFont *CreateFontInstance(const gfxFontStyle *aFontStyle,
                                         bool aNeedsBold);
 
+    // Create (if necessary) and return the cairo_font_face for this font.
+    // This may fail and return null, so caller must be prepared to handle this.
     cairo_font_face_t *CairoFontFace();
+
+    // Create a cairo_scaled_font for this face, with the given style.
+    // This may fail and return null, so caller must be prepared to handle this.
     cairo_scaled_font_t *CreateScaledFont(const gfxFontStyle *aStyle);
 
     nsresult ReadCMAP();
+
+    virtual hb_blob_t* GetFontTable(uint32_t aTableTag) MOZ_OVERRIDE;
 
     virtual nsresult CopyFontTable(uint32_t aTableTag,
                                    FallibleTArray<uint8_t>& aBuffer) MOZ_OVERRIDE;
@@ -85,7 +94,7 @@ public:
     cairo_font_face_t *mFontFace;
 
     nsCString mFilename;
-    uint8_t mFTFontIndex;
+    uint8_t   mFTFontIndex;
 };
 
 class FT2FontFamily : public gfxFontFamily
@@ -124,15 +133,25 @@ protected:
     void AppendFaceFromFontListEntry(const FontListEntry& aFLE,
                                      bool isStdFile);
 
-    void AppendFacesFromFontFile(nsCString& aFileName,
+    void AppendFacesFromFontFile(const nsCString& aFileName,
                                  bool isStdFile = false,
                                  FontNameCache *aCache = nullptr);
 
-    void AppendFacesFromCachedFaceList(nsCString& aFileName,
+    void AppendFacesFromOmnijarEntry(nsZipArchive *aReader,
+                                     const nsCString& aEntryName,
+                                     FontNameCache *aCache,
+                                     bool aJarChanged);
+
+    void AppendFacesFromCachedFaceList(const nsCString& aFileName,
                                        bool isStdFile,
-                                       nsCString& aFaceList);
+                                       const nsCString& aFaceList);
+
+    void AddFaceToList(const nsCString& aEntryName, uint32_t aIndex,
+                       bool aStdFile, FT_Face aFace, nsCString& aFaceList);
 
     void FindFonts();
+
+    void FindFontsInOmnijar(FontNameCache *aCache);
 
 #ifdef ANDROID
     void FindFontsInDir(const nsCString& aDir, FontNameCache* aFNC);
