7aaeed23267ebf5e90901e39e4c0bff7ebe25d2b
Jonathan Wilde <jwilde@jwilde.me>
2013-06-25 12:20:17 -0700

    Bug 885541 - Work - Move devtools icons in app bar into hamburger menu. r=fryn
    --HG--
    extra : rebase_source : bd850050552aecbd390f15f2c393927c9e09e750

--- browser/metro/base/content/ContextCommands.js
+++ browser/metro/base/content/ContextCommands.js
@@ -230,6 +230,17 @@ var ContextCommands = {
 
   // App bar
 
+  errorConsole: function cc_errorConsole() {
+    PanelUI.show("console-container");
+  },
+
+  jsShell: function cc_jsShell() {
+    // XXX for debugging, this only works when running on the desktop.
+    if (!MetroUtils.immersive)
+      window.openDialog("chrome://browser/content/shell.xul", "_blank",
+                        "all=no,scrollbars=yes,resizable=yes,dialog=no");
+  },
+
   findInPage: function cc_findInPage() {
     FindHelperUI.show();
   },
