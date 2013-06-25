2ad6c33545b0b3fe92786733402c25ac5ab9d08b
Lucas Rocha <lucasr@mozilla.com>
2013-06-25 18:00:09 +0100

    Bug 884896 - Move all search-related classes to home package (r=sriram)

--- mobile/android/base/home/SuggestClient.java
+++ mobile/android/base/home/SuggestClient.java
@@ -2,7 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.gecko;
+package org.mozilla.gecko.home;
+
+import org.mozilla.gecko.GeckoAppShell;
 
 import org.json.JSONArray;
 
