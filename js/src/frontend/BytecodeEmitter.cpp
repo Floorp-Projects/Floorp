59c61cce3df1c83c94d877731eecae08e851cb6d
Jason Orendorff <jorendorff@mozilla.com>
2013-06-25 17:40:00 -0500

    Bug 844805, part 2 - Don't even set the pn_op field of PNK_DOT/ELEM nodes. r=Waldo.

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -1894,7 +1894,6 @@ EmitPropLHS(JSContext *cx, ParseNode *pn, JSOp op, BytecodeEmitter *bce)
 
         do {
             /* Walk back up the list, emitting annotated name ops. */
-            JS_ASSERT(pndot->getOp() == JSOP_GETPROP);
             if (!EmitAtomOp(cx, pndot, JSOP_GETPROP, bce))
                 return false;
 
@@ -3436,7 +3435,6 @@ EmitAssignment(JSContext *cx, BytecodeEmitter *bce, ParseNode *lhs, JSOp op, Par
         }
         break;
       case PNK_DOT:
-        JS_ASSERT(lhs->getOp() == JSOP_SETPROP);
         if (!EmitIndexOp(cx, JSOP_SETPROP, atomIndex, bce))
             return false;
         break;
@@ -5032,12 +5030,10 @@ EmitCallOrNew(JSContext *cx, BytecodeEmitter *bce, ParseNode *pn)
             return false;
         break;
       case PNK_DOT:
-        JS_ASSERT(pn2->getOp() == JSOP_GETPROP);
         if (!EmitPropOp(cx, pn2, callop ? JSOP_CALLPROP : JSOP_GETPROP, bce))
             return false;
         break;
       case PNK_ELEM:
-        JS_ASSERT(pn2->isOp(JSOP_GETELEM));
         if (!EmitElemOp(cx, pn2, callop ? JSOP_CALLELEM : JSOP_GETELEM, bce))
             return false;
         break;
@@ -5923,23 +5919,10 @@ frontend::EmitTree(JSContext *cx, BytecodeEmitter *bce, ParseNode *pn)
         break;
 
       case PNK_DOT:
-        /*
-         * Pop a stack operand, convert it to object, get a property named by
-         * this bytecode's immediate-indexed atom operand, and push its value
-         * (not a reference to it).
-         */
-        JS_ASSERT(pn->getOp() == JSOP_GETPROP);
         ok = EmitPropOp(cx, pn, JSOP_GETPROP, bce);
         break;
 
       case PNK_ELEM:
-        /*
-         * Pop two operands, convert the left one to object and the right one
-         * to property name (atom or tagged int), get the named property, and
-         * push its value.  Set the "obj" register to the result of ToObject
-         * on the left operand.
-         */
-        JS_ASSERT(pn->getOp() == JSOP_GETELEM);
         ok = EmitElemOp(cx, pn, JSOP_GETELEM, bce);
         break;
 
