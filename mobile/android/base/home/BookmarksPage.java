57719bf06bee9bddf6e9445db19aa0f527604972
Lucas Rocha <lucasr@mozilla.com>
2013-06-25 11:58:53 +0100

    Bug 884901 - Remove unnecessary super.onCreateView() call from BookmarksPage (r=sriram)

--- mobile/android/base/home/BookmarksPage.java
+++ mobile/android/base/home/BookmarksPage.java
@@ -26,7 +26,6 @@ public class BookmarksPage extends HomeFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.home_bookmarks_page, null);
     }
 
