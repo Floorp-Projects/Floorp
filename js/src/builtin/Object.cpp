1ec14cdadd07627d55e2808deb16cb5797d9e2f5
Till Schneidereit <tschneidereit@gmail.com>
2013-06-26 17:00:24 +0200

    Bug 885729 - Inline array and proxy specializations of js::DefineProperty into js::DefineProperties. r=jwalden

--- js/src/builtin/Object.cpp
+++ js/src/builtin/Object.cpp
@@ -23,25 +23,6 @@ using js::frontend::IsIdentifier;
 using mozilla::ArrayLength;
 
 
-// Duplicated in jsobj.cpp
-static bool
-DefineProperties(JSContext *cx, HandleObject obj, HandleObject props)
-{
-    AutoIdVector ids(cx);
-    AutoPropDescArrayRooter descs(cx);
-    if (!ReadPropertyDescriptors(cx, props, true, &ids, &descs))
-        return false;
-
-    bool dummy;
-    for (size_t i = 0, len = ids.length(); i < len; i++) {
-        if (!DefineProperty(cx, obj, ids.handleAt(i), descs[i], true, &dummy))
-            return false;
-    }
-
-    return true;
-}
-
-
 JSBool
 js::obj_construct(JSContext *cx, unsigned argc, Value *vp)
 {
