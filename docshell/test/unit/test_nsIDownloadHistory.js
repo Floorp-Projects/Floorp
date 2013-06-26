9cebfa52d5b332fefb1cdf7367317bf543c4234b
Ed Morley <emorley@mozilla.com>
2013-06-26 11:20:46 +0100

    Backed out changeset b1dba904d5c8 (bug 886263) for xpcshell orange

--- docshell/test/unit/test_nsIDownloadHistory.js
+++ docshell/test/unit/test_nsIDownloadHistory.js
@@ -61,6 +61,8 @@ function run_test()
   // Needed to properly setup and shutdown the profile.
   do_get_profile();
 
+  Services.prefs.setBoolPref("places.history.enabled", true);
+
   for (var i = 0; i < tests.length; i++)
     tests[i]();
 
