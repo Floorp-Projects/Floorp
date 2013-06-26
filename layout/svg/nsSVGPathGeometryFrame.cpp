e07aa31e86c0993e2108dd88e393e9e3a87753e6
Robert Longson <longsonr@gmail.com>
2013-06-26 13:31:06 +0100

    Bug 829085 - Fix hit testing when vector-effect=non-scaling-stroke is used. r=dholbert

--- layout/svg/nsSVGPathGeometryFrame.cpp
+++ layout/svg/nsSVGPathGeometryFrame.cpp
@@ -244,6 +244,11 @@ nsSVGPathGeometryFrame::GetFrameForPoint(const nsPoint &aPoint)
     isHit = tmpCtx->PointInFill(userSpacePoint);
   if (!isHit && (hitTestFlags & SVG_HIT_TEST_STROKE)) {
     nsSVGUtils::SetupCairoStrokeGeometry(this, tmpCtx);
+    // tmpCtx's matrix may have transformed by SetupCairoStrokeGeometry
+    // if there is a non-scaling stroke. We need to transform userSpacePoint
+    // so that everything is using the same co-ordinate system.
+    userSpacePoint =
+      nsSVGUtils::GetStrokeTransform(this).Invert().Transform(userSpacePoint);
     isHit = tmpCtx->PointInStroke(userSpacePoint);
   }
 
