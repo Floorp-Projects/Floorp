e2c5bfa5e3258bbbc38a07db5d2f50cb42efdca9
Jim Mathies <jmathies@mozilla.com>
2013-06-27 14:47:25 -0500

    Bug 885005 - update the about firefox privacy link. r=jwilde

--- browser/metro/profile/metro.js
+++ browser/metro/profile/metro.js
@@ -385,7 +385,7 @@ pref("breakpad.reportURL", "https://crash-stats.mozilla.com/report/index/");
 // TODO: This is not the correct article for metro!!!
 pref("app.sync.tutorialURL", "https://support.mozilla.org/kb/sync-firefox-between-desktop-and-mobile");
 pref("app.support.baseURL", "https://support.mozilla.org/1/firefox/%VERSION%/%OS%/%LOCALE%/");
-pref("app.privacyURL", "https://www.mozilla.org/legal/privacy/");
+pref("app.privacyURL", "http://www.mozilla.org/%LOCALE%/legal/privacy/firefox.html");
 pref("app.creditsURL", "http://www.mozilla.org/credits/");
 pref("app.channelURL", "http://www.mozilla.org/%LOCALE%/firefox/channel/");
 
