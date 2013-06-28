f25d2441da2a2431cf2c986aa8e95ca7596d67c0
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2013-06-28 17:45:05 -0400

    Bug 833943, part 3: implement migrator changes, r=MattN, sr=gavin

--- browser/components/migration/src/FirefoxProfileMigrator.js
+++ browser/components/migration/src/FirefoxProfileMigrator.js
@@ -15,12 +15,25 @@
 
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 Components.utils.import("resource:///modules/MigrationUtils.jsm");
+Components.utils.import("resource://gre/modules/Services.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "PlacesBackups",
                                   "resource://gre/modules/PlacesBackups.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "SessionMigration",
+                                  "resource:///modules/sessionstore/SessionMigration.jsm");
 
 function FirefoxProfileMigrator() { }
 
 FirefoxProfileMigrator.prototype = Object.create(MigratorPrototype);
+
+FirefoxProfileMigrator.prototype._getFileObject = function(dir, fileName) {
+  let file = dir.clone();
+  file.append(fileName);
+
+  // File resources are monolithic.  We don't make partial copies since
+  // they are not expected to work alone. Return null to avoid trying to
+  // copy non-existing files.
+  return file.exists() ? file : null;
+}
 
 FirefoxProfileMigrator.prototype.getResources = function() {
   // Only allow migrating from the default (selected) profile since this will
@@ -48,14 +61,9 @@ FirefoxProfileMigrator.prototype.getResources = function() {
   let getFileResource = function(aMigrationType, aFileNames) {
     let files = [];
     for (let fileName of aFileNames) {
-      let file = sourceProfileDir.clone();
-      file.append(fileName);
-
-      // File resources are monolithic.  We don't make partial copies since
-      // they are not expected to work alone.
-      if (!file.exists())
+      let file = this._getFileObject(sourceProfileDir, fileName);
+      if (!file)
         return null;
-
       files.push(file);
     }
     return {
@@ -67,7 +75,7 @@ FirefoxProfileMigrator.prototype.getResources = function() {
         aCallback(true);
       }
     };
-  };
+  }.bind(this);
 
   let types = MigrationUtils.resourceTypes;
   let places = getFileResource(types.HISTORY, ["places.sqlite"]);
@@ -79,8 +87,39 @@ FirefoxProfileMigrator.prototype.getResources = function() {
     [PlacesBackups.profileRelativeFolderPath]);
   let dictionary = getFileResource(types.OTHERDATA, ["persdict.dat"]);
 
+  let sessionFile = this._getFileObject(sourceProfileDir, "sessionstore.js");
+  let session;
+  if (sessionFile) {
+    session = {
+      type: types.SESSION,
+      migrate: function(aCallback) {
+        let newSessionFile = currentProfileDir.clone();
+        newSessionFile.append("sessionstore.js");
+        let migrationPromise = SessionMigration.migrate(sessionFile.path, newSessionFile.path);
+        migrationPromise.then(function() {
+          let buildID = Services.appinfo.platformBuildID;
+          let mstone = Services.appinfo.platformVersion;
+          // Force the browser to one-off resume the session that we give it:
+          Services.prefs.setBoolPref("browser.sessionstore.resume_session_once", true);
+          // Reset the homepage_override prefs so that the browser doesn't override our
+          // session with the "what's new" page:
+          Services.prefs.setCharPref("browser.startup.homepage_override.mstone", mstone);
+          Services.prefs.setCharPref("browser.startup.homepage_override.buildID", buildID);
+          // It's too early in startup for the pref service to have a profile directory,
+          // so we have to manually tell it where to save the prefs file.
+          let newPrefsFile = currentProfileDir.clone();
+          newPrefsFile.append("prefs.js");
+          Services.prefs.savePrefFile(newPrefsFile);
+          aCallback(true);
+        }, function() {
+          aCallback(false);
+        });
+      }
+    }
+  }
+
   return [r for each (r in [places, cookies, passwords, formData,
-                            dictionary, bookmarksBackups]) if (r)];
+                            dictionary, bookmarksBackups, session]) if (r)];
 }
 
 Object.defineProperty(FirefoxProfileMigrator.prototype, "startupOnlyMigrator", {
