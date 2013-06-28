8e7858fd5f6af8ade3359413687414ad255eeea8
Jeff Walden <jwalden@mit.edu>
2013-06-28 14:01:09 -0700

    Bug 888106 - Add too-much-recursion detection to isExtensible tests, and make the isExtensible hook capable of failing.  r=bholley, r=ejpbruel

--- dom/bindings/DOMJSProxyHandler.cpp
+++ dom/bindings/DOMJSProxyHandler.cpp
@@ -144,9 +144,11 @@ DOMProxyHandler::EnsureExpandoObject(JSContext* cx, JS::Handle<JSObject*> obj)
 }
 
 bool
-DOMProxyHandler::isExtensible(JSObject *proxy)
+DOMProxyHandler::isExtensible(JSContext *cx, JS::Handle<JSObject*> proxy, bool *extensible)
 {
-  return true; // always extensible per WebIDL
+  // always extensible per WebIDL
+  *extensible = true;
+  return true;
 }
 
 bool
