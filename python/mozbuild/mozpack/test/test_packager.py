f87e3603bda9b51eba2678c5c91bf3e67317f87c
Nick Alexander <nalexander@mozilla.com>
2013-06-28 09:10:57 -0700

    Bug 887121 - Make packager install and szip .so libraries in assets/ directly. r=glandium, a=lsblakk
    This adds a Component type to the mozbuild.mozpack package manifest
    parser, and teaches the packager to accept components of the form
    [name destdir="dir"].  Then we update the Android package manifest and
    simplify the packager code.
    
    I would have liked to make the packager put mozglue.so and
    MOZ_CHILD_PROCESS_NAME in lib/$(ABI_DIR) directly, but this turned out
    to be awkward.  Since MOZ_CHILD_PROCESS_NAME needs to have lib/ in its
    name to load successfully on Android, we would have to add notation in
    package manifests to install bin/lib/*plugin-container* to
    lib/$(ABI_DIR)/*plugin-container*.

--- python/mozbuild/mozpack/test/test_packager.py
+++ python/mozbuild/mozpack/test/test_packager.py
@@ -7,9 +7,10 @@ import mozunit
 import os
 from mozpack.packager import (
     preprocess_manifest,
+    CallDeque,
+    Component,
     SimplePackager,
     SimpleManifestSink,
-    CallDeque,
 )
 from mozpack.files import GeneratedFile
 from mozpack.chrome.manifest import (
@@ -30,6 +31,8 @@ bar/*
 foo/*
 -foo/bar
 chrome.manifest
+[zot destdir="destdir"]
+foo/zot
 ; comment
 #ifdef baz
 [baz]
@@ -45,7 +48,8 @@ class TestPreprocessManifest(unittest.TestCase):
         ((MANIFEST_PATH, 2), 'add', '', 'bar/*'),
         ((MANIFEST_PATH, 4), 'add', 'foo', 'foo/*'),
         ((MANIFEST_PATH, 5), 'remove', 'foo', 'foo/bar'),
-        ((MANIFEST_PATH, 6), 'add', 'foo', 'chrome.manifest')
+        ((MANIFEST_PATH, 6), 'add', 'foo', 'chrome.manifest'),
+        ((MANIFEST_PATH, 8), 'add', 'zot destdir="destdir"', 'foo/zot'),
     ]
 
     def setUp(self):
@@ -53,11 +57,11 @@ class TestPreprocessManifest(unittest.TestCase):
             def __init__(self):
                 self.log = []
 
-            def add(self, section, path):
-                self._log(errors.get_context(), 'add', section, path)
+            def add(self, component, path):
+                self._log(errors.get_context(), 'add', repr(component), path)
 
-            def remove(self, section, path):
-                self._log(errors.get_context(), 'remove', section, path)
+            def remove(self, component, path):
+                self._log(errors.get_context(), 'remove', repr(component), path)
 
             def _log(self, *args):
                 self.log.append(args)
@@ -84,7 +88,7 @@ class TestPreprocessManifest(unittest.TestCase):
             preprocess_manifest(self.sink, 'manifest',
                                 {'baz': 1, 'SUFFIX': '.exe'})
         self.assertEqual(self.sink.log, self.EXPECTED_LOG +
-                         [((self.MANIFEST_PATH, 10), 'add', 'baz', 'baz.exe')])
+                         [((self.MANIFEST_PATH, 12), 'add', 'baz', 'baz.exe')])
 
 
 class MockFinder(object):
@@ -199,19 +203,25 @@ class TestSimpleManifestSink(unittest.TestCase):
         foobar = GeneratedFile('foobar')
         foobaz = GeneratedFile('foobaz')
         fooqux = GeneratedFile('fooqux')
+        foozot = GeneratedFile('foozot')
         finder = MockFinder({
             'bin/foo/bar': foobar,
             'bin/foo/baz': foobaz,
             'bin/foo/qux': fooqux,
+            'bin/foo/zot': foozot,
             'bin/foo/chrome.manifest': GeneratedFile('resource foo foo/'),
             'bin/chrome.manifest':
             GeneratedFile('manifest foo/chrome.manifest'),
         })
         parser = SimpleManifestSink(finder, formatter)
-        parser.add('section0', 'bin/foo/b*')
-        parser.add('section1', 'bin/foo/qux')
-        parser.add('section1', 'bin/foo/chrome.manifest')
-        self.assertRaises(ErrorMessage, parser.add, 'section1', 'bin/bar')
+        component0 = Component('component0')
+        component1 = Component('component1')
+        component2 = Component('component2', destdir='destdir')
+        parser.add(component0, 'bin/foo/b*')
+        parser.add(component1, 'bin/foo/qux')
+        parser.add(component1, 'bin/foo/chrome.manifest')
+        parser.add(component2, 'bin/foo/zot')
+        self.assertRaises(ErrorMessage, parser.add, 'component1', 'bin/bar')
 
         self.assertEqual(formatter.log, [])
         parser.close()
@@ -221,12 +231,14 @@ class TestSimpleManifestSink(unittest.TestCase):
             (None, 'add', 'foo/bar', foobar),
             (None, 'add', 'foo/baz', foobaz),
             (None, 'add', 'foo/qux', fooqux),
+            (None, 'add', 'destdir/foo/zot', foozot),
         ])
 
         self.assertEqual(finder.log, [
             'bin/foo/b*',
             'bin/foo/qux',
             'bin/foo/chrome.manifest',
+            'bin/foo/zot',
             'bin/bar',
             'bin/chrome.manifest'
         ])
@@ -257,6 +269,60 @@ class TestCallDeque(unittest.TestCase):
         self.assertEqual(logger._log, [])
         d.execute()
         self.assertEqual(logger._log, ['foo', 'bar', 'baz', 'qux'])
+
+
+class TestComponent(unittest.TestCase):
+    def do_split(self, string, name, options):
+        n, o = Component._split_component_and_options(string)
+        self.assertEqual(name, n)
+        self.assertEqual(options, o)
+
+    def test_component_split_component_and_options(self):
+        self.do_split('component', 'component', {})
+        self.do_split('trailingspace ', 'trailingspace', {})
+        self.do_split(' leadingspace', 'leadingspace', {})
+        self.do_split(' trim ', 'trim', {})
+        self.do_split(' trim key="value"', 'trim', {'key':'value'})
+        self.do_split(' trim empty=""', 'trim', {'empty':''})
+        self.do_split(' trim space=" "', 'trim', {'space':' '})
+        self.do_split('component key="value"  key2="second" ',
+                      'component', {'key':'value', 'key2':'second'})
+        self.do_split( 'trim key="  value with spaces   "  key2="spaces again"',
+                       'trim', {'key':'  value with spaces   ', 'key2': 'spaces again'})
+
+    def do_split_error(self, string):
+        self.assertRaises(ValueError, Component._split_component_and_options, string)
+
+    def test_component_split_component_and_options_errors(self):
+        self.do_split_error('"component')
+        self.do_split_error('comp"onent')
+        self.do_split_error('component"')
+        self.do_split_error('"component"')
+        self.do_split_error('=component')
+        self.do_split_error('comp=onent')
+        self.do_split_error('component=')
+        self.do_split_error('key="val"')
+        self.do_split_error('component key=')
+        self.do_split_error('component key="val')
+        self.do_split_error('component key=val"')
+        self.do_split_error('component key="val" x')
+        self.do_split_error('component x key="val"')
+        self.do_split_error('component key1="val" x key2="val"')
+
+    def do_from_string(self, string, name, destdir=''):
+        component = Component.from_string(string)
+        self.assertEqual(name, component.name)
+        self.assertEqual(destdir, component.destdir)
+
+    def test_component_from_string(self):
+        self.do_from_string('component', 'component')
+        self.do_from_string('component-with-hyphen', 'component-with-hyphen')
+        self.do_from_string('component destdir="foo/bar"', 'component', 'foo/bar')
+        self.do_from_string('component destdir="bar spc"', 'component', 'bar spc')
+        self.assertRaises(ErrorMessage, Component.from_string, '')
+        self.assertRaises(ErrorMessage, Component.from_string, 'component novalue=')
+        self.assertRaises(ErrorMessage, Component.from_string, 'component badoption=badvalue')
+
 
 if __name__ == '__main__':
     mozunit.main()
