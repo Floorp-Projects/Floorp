4a27519edff9cabfbd8bb4286d2738996ab033d1
Camilo Viecco <cviecco@mozilla.com>
2013-06-28 16:16:56 -0700

    Bug 916632 (insanity::pkix::ScopedPtr), Part 3: Work around old GCC's lack of nullptr, r=briansmith
    --HG--
    extra : rebase_source : 4b212aad2968101717be1e2af9f17d54939bba46
    extra : source : 39a083dfd00dd95fafd6444e9ea69ff1257e5eca

--- security/insanity/include/insanity/ScopedPtr.h
+++ security/insanity/include/insanity/ScopedPtr.h
@@ -18,6 +18,13 @@
 #ifndef insanity_pkix__ScopedPtr_h
 #define insanity_pkix__ScopedPtr_h
 
+// GCC does not understand nullptr until 4.6
+#ifdef __GNUC__
+#if __GNUC__ * 100 + __GNUC_MINOR__ < 406
+  #define nullptr __null
+#endif
+#endif
+
 namespace insanity { namespace pkix {
 
 // Similar to boost::scoped_ptr and std::unique_ptr. Does not support copying
