539909bb77d12b680814e59ad276b1e063f601e3
Max Li <maxli@maxli.ca>
2013-06-29 17:18:38 -0400

    Bug 886077 - Can't access address bar using Spiel screen reader. r=lucasr Relanded because bug 887020 accidentally reverted this patch.

--- mobile/android/base/BrowserToolbar.java
+++ mobile/android/base/BrowserToolbar.java
@@ -202,8 +202,6 @@ public class BrowserToolbar extends GeckoRelativeLayout
 
         mTitle = (GeckoTextView) findViewById(R.id.awesome_bar_title);
         mTitlePadding = mTitle.getPaddingRight();
-        if (Build.VERSION.SDK_INT >= 16)
-            mTitle.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_NO);
 
         mTabs = (ShapedButton) findViewById(R.id.tabs);
         mTabsCounter = (TabCounter) findViewById(R.id.tabs_counter);
