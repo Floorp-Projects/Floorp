8f3a2a0c6926fb68bcc3c92bdcf1b93e349508de
Catalin Iacob <iacobcatalin@gmail.com>
2013-06-30 18:26:39 +0200

    Bug 798914 (part 6) - Followup: move MemoryReporting.h include after the cpp's own header include.  r=njn.

--- caps/src/nsNullPrincipalURI.cpp
+++ caps/src/nsNullPrincipalURI.cpp
@@ -4,8 +4,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "mozilla/MemoryReporting.h"
 #include "nsNullPrincipalURI.h"
+
+#include "mozilla/MemoryReporting.h"
+
 #include "nsNetUtil.h"
 #include "nsEscape.h"
 #include "nsCRT.h"
