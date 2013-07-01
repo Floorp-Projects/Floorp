dfff78a666205fda3a8f0e011d1e275c5233274d
David Keeler <dkeeler@mozilla.com>
2013-07-01 10:49:40 -0700

    bug 887052 - refactor nsISiteSecurityService for multiple headers r=cviecco r=mcmanus r=jst sr=jst

--- security/manager/ssl/tests/unit/test_sts_preloadlist_selfdestruct.js
+++ security/manager/ssl/tests/unit/test_sts_preloadlist_selfdestruct.js
@@ -5,17 +5,18 @@ function run_test() {
   let SSService = Cc["@mozilla.org/ssservice;1"]
                     .getService(Ci.nsISiteSecurityService);
 
+  let uri = Services.io.newURI("https://bugzilla.mozilla.org", null, null);
   // check that a host on the preload list is identified as an sts host
-  do_check_true(SSService.isStsHost("bugzilla.mozilla.org", 0));
+  do_check_true(SSService.isSecureURI(Ci.nsISiteSecurityService.HEADER_HSTS, uri, 0));
 
   // now simulate that it's 19 weeks later than it actually is
   let offsetSeconds = 19 * 7 * 24 * 60 * 60;
   Services.prefs.setIntPref("test.currentTimeOffsetSeconds", offsetSeconds);
 
   // check that the preloaded host is no longer considered sts
-  do_check_false(SSService.isStsHost("bugzilla.mozilla.org", 0));
+  do_check_false(SSService.isSecureURI(Ci.nsISiteSecurityService.HEADER_HSTS, uri, 0));
 
   // just make sure we can get everything back to normal
   Services.prefs.clearUserPref("test.currentTimeOffsetSeconds");
-  do_check_true(SSService.isStsHost("bugzilla.mozilla.org", 0));
+  do_check_true(SSService.isSecureURI(Ci.nsISiteSecurityService.HEADER_HSTS, uri, 0));
 }
