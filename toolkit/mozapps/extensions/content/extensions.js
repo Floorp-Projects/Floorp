20ba210de97b4f20242f4d5e56e0dd0f085e4dd6
Geoff Lankow <geoff@darktrojan.net>
2013-07-03 00:20:51 +1200

    Bug 888778 - Custom binding not applied to inline settings; r=Unfocused

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -2973,7 +2973,8 @@ var gDetailView = {
         var settings = xml.querySelectorAll(":root > setting");
 
         var firstSetting = null;
-        for (let setting of settings) {
+        for (var xmlSetting of settings) {
+          var setting = document.importNode(xmlSetting, true);
 
           var desc = stripTextNodes(setting).trim();
           if (!setting.hasAttribute("desc"))
@@ -2982,6 +2983,12 @@ var gDetailView = {
           var type = setting.getAttribute("type");
           if (type == "file" || type == "directory")
             setting.setAttribute("fullpath", "true");
+
+          var style = setting.getAttribute("style");
+          if (style) {
+            setting.removeAttribute("style");
+            setting.setAttribute("style", style);
+          }
 
           rows.appendChild(setting);
           var visible = window.getComputedStyle(setting, null).getPropertyValue("display") != "none";
