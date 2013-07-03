0470cde9bdb9c7d2a84a2d59412b1824dac3145b
Hannes Verschore <hv1989@gmail.com>
2013-07-03 10:41:17 +0200

    Bug 886255 - Let activatation of inactive JitActivation behave more closely to the construction of active JitAction. r=jandem, a=bajaj

--- js/src/vm/Stack.h
+++ js/src/vm/Stack.h
@@ -1150,7 +1150,6 @@ class Activation
     JSContext *cx_;
     JSCompartment *compartment_;
     Activation *prev_;
-    bool active_;
 
     // Counter incremented by JS_SaveFrameChain on the top-most activation and
     // decremented by JS_RestoreFrameChain. If > 0, ScriptFrameIter should stop
@@ -1161,7 +1160,7 @@ class Activation
     enum Kind { Interpreter, Jit };
     Kind kind_;
 
-    inline Activation(JSContext *cx, Kind kind_, bool active = true);
+    inline Activation(JSContext *cx, Kind kind_);
     inline ~Activation();
 
   public:
@@ -1174,10 +1173,6 @@ class Activation
     Activation *prev() const {
         return prev_;
     }
-    bool isActive() const {
-        return active_;
-    }
-    void setActive(bool active = true);
 
     bool isInterpreter() const {
         return kind_ == Interpreter;
@@ -1278,10 +1273,16 @@ class JitActivation : public Activation
     uint8_t *prevIonTop_;
     JSContext *prevIonJSContext_;
     bool firstFrameIsConstructing_;
+    bool active_;
 
   public:
     JitActivation(JSContext *cx, bool firstFrameIsConstructing, bool active = true);
     ~JitActivation();
+
+    bool isActive() const {
+        return active_;
+    }
+    void setActive(JSContext *cx, bool active = true);
 
     uint8_t *prevIonTop() const {
         return prevIonTop_;
