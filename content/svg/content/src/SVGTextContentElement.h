7145c3dd0845411ab03c5e7b08cc2314a1407991
Cameron McCormack <cam@mcc.id.au>
2013-07-07 17:27:51 +1000

    Bug 569722 - Implement textLength and lengthAdjust on SVG text elements. r=longsonr

--- content/svg/content/src/SVGTextContentElement.h
+++ content/svg/content/src/SVGTextContentElement.h
@@ -7,6 +7,13 @@
 #define mozilla_dom_SVGTextContentElement_h
 
 #include "mozilla/dom/SVGGraphicsElement.h"
+#include "mozilla/dom/SVGAnimatedEnumeration.h"
+#include "nsSVGEnum.h"
+#include "nsSVGLength2.h"
+
+static const unsigned short SVG_LENGTHADJUST_UNKNOWN          = 0;
+static const unsigned short SVG_LENGTHADJUST_SPACING          = 1;
+static const unsigned short SVG_LENGTHADJUST_SPACINGANDGLYPHS = 2;
 
 class nsSVGTextContainerFrame;
 class nsSVGTextFrame2;
@@ -26,6 +33,8 @@ public:
   using FragmentOrElement::TextLength;
 
   // WebIDL
+  already_AddRefed<SVGAnimatedLength> TextLength();
+  already_AddRefed<SVGAnimatedEnumeration> LengthAdjust();
   int32_t GetNumberOfChars();
   float GetComputedTextLength();
   void SelectSubString(uint32_t charnum, uint32_t nchars, ErrorResult& rv);
@@ -45,6 +54,19 @@ protected:
   nsSVGTextContainerFrame* GetTextContainerFrame();
   nsSVGTextFrame2* GetSVGTextFrame();
   bool FrameIsSVGText();
+
+  virtual EnumAttributesInfo GetEnumInfo() MOZ_OVERRIDE;
+  virtual LengthAttributesInfo GetLengthInfo() MOZ_OVERRIDE;
+
+  enum { LENGTHADJUST };
+  nsSVGEnum mEnumAttributes[1];
+  static nsSVGEnumMapping sLengthAdjustMap[];
+  static EnumInfo sEnumInfo[1];
+
+  enum { TEXTLENGTH };
+  nsSVGLength2 mLengthAttributes[1];
+
+  static LengthInfo sLengthInfo[1];
 };
 
 } // namespace dom
