41eb003349d8f274bc216e1b2419c7f8a953172d
Robert Bindar <robertbindar@gmail.com>
2013-07-08 16:35:08 -0400

    Bug 888583 - Expose protocol version in about:networking. r=valentin.gosu

--- netwerk/protocol/http/nsHttpConnection.h
+++ netwerk/protocol/http/nsHttpConnection.h
@@ -119,6 +119,7 @@ public:
     nsresult ResumeSend();
     nsresult ResumeRecv();
     int64_t  MaxBytesRead() {return mMaxBytesRead;}
+    uint8_t GetLastHttpResponseVersion() { return mLastHttpResponseVersion; }
 
     friend class nsHttpConnectionForceRecv;
     nsresult ForceRecv();
@@ -134,6 +135,7 @@ public:
     void EndIdleMonitoring();
 
     bool UsingSpdy() { return !!mUsingSpdyVersion; }
+    uint8_t GetSpdyVersion() { return mUsingSpdyVersion; }
     bool EverUsedSpdy() { return mEverUsedSpdy; }
     PRIntervalTime Rtt() { return mRtt; }
 
@@ -252,6 +254,9 @@ private:
 
     // mUsingSpdyVersion is cleared when mSpdySession is freed, this is permanent
     bool                            mEverUsedSpdy;
+
+    // mLastHttpResponseVersion stores the last response's http version seen.
+    uint8_t                         mLastHttpResponseVersion;
 
     // The capabailities associated with the most recent transaction
     uint32_t                        mTransactionCaps;
