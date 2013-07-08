327503f9c3af2ae44bc014ee43f21ae6548d9a3b
Shu-yu Guo <shu@rfrn.org>
2013-07-08 03:24:54 -0700

    Bug 881574 - Part 1: Support Array.length in GetPropertyPure. (r=bhackett)

--- js/src/jsobj.h
+++ js/src/jsobj.h
@@ -1420,12 +1420,12 @@ bool
 LookupPropertyPure(JSObject *obj, jsid id, JSObject **objp, Shape **propp);
 
 bool
-GetPropertyPure(JSObject *obj, jsid id, Value *vp);
+GetPropertyPure(ThreadSafeContext *tcx, JSObject *obj, jsid id, Value *vp);
 
 inline bool
-GetPropertyPure(JSObject *obj, PropertyName *name, Value *vp)
+GetPropertyPure(ThreadSafeContext *tcx, JSObject *obj, PropertyName *name, Value *vp)
 {
-    return GetPropertyPure(obj, NameToId(name), vp);
+    return GetPropertyPure(tcx, obj, NameToId(name), vp);
 }
 
 bool
