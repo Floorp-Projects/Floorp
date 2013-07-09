13de593e8346bce6ecb9562117bfde7107b9b871
Dan Gohman <sunfish@google.com>
2013-07-08 19:51:56 -0700

    Bug 891070 - IonMonkey: Stronger range analysis for or, xor, and not. r=nbp

--- js/src/ion/RangeAnalysis.cpp
+++ js/src/ion/RangeAnalysis.cpp
@@ -465,11 +465,22 @@ Range::or_(const Range *lhs, const Range *rhs)
     int64_t lower = INT32_MIN;
     int64_t upper = INT32_MAX;
 
-    // If the sign bits are the same, the result has the same sign.
-    if (lhs->lower_ >= 0 && rhs->lower_ >= 0)
-        lower = 0;
-    else if (lhs->upper_ < 0 || rhs->upper_ < 0)
-        upper = -1;
+    if (lhs->lower_ >= 0 && rhs->lower_ >= 0) {
+        // Both operands are non-negative, so the result won't be greater than either.
+        lower = Max(lhs->lower_, rhs->lower_);
+        // The result will have leading zeros where both operands have leading zeros.
+        upper = UINT32_MAX >> Min(js_bitscan_clz32(lhs->upper_),
+                                  js_bitscan_clz32(rhs->upper_));
+    } else {
+        // The result will have leading ones where either operand has leading ones.
+        if (lhs->upper_ < 0)
+            lower = Max(lower, (int64_t)(int32_t)~(UINT32_MAX >> js_bitscan_clz32(~lhs->lower_)));
+        if (rhs->upper_ < 0)
+            lower = Max(lower, (int64_t)(int32_t)~(UINT32_MAX >> js_bitscan_clz32(~rhs->lower_)));
+        // If either operand is negative, the result is negative.
+        if (lhs->upper_ < 0 && rhs->upper_ < 0)
+            upper = -1;
+    }
 
     return new Range(lower, upper);
 }
@@ -480,11 +491,31 @@ Range::xor_(const Range *lhs, const Range *rhs)
     int64_t lower = INT32_MIN;
     int64_t upper = INT32_MAX;
 
-    // If the sign bits are identical, the result is non-negative.
-    if (lhs->lower_ >= 0 && rhs->lower_ >= 0)
+    if (lhs->lower_ >= 0 && rhs->lower_ >= 0) {
+        // Both operands are non-negative. The result will be non-negative and
+        // not greater than either.
         lower = 0;
-    else if (lhs->upper_ < 0 && rhs->upper_ < 0)
+        upper = UINT32_MAX >> Min(js_bitscan_clz32(lhs->upper_),
+                                  js_bitscan_clz32(rhs->upper_));
+    } else if (lhs->upper_ < 0 && rhs->upper_ < 0) {
+        // Both operands are negative. The result will be non-negative and
+        // will have leading zeros where both operands have leading ones.
         lower = 0;
+        upper = UINT32_MAX >> Min(js_bitscan_clz32(~lhs->lower_),
+                                  js_bitscan_clz32(~rhs->lower_));
+    } else if (lhs->upper_ < 0 && rhs->lower_ >= 0) {
+        // One operand is negative and the other is non-negative. The result
+        // will have leading ones where the negative operand has leading ones
+        // and the non-negative operand has leading zeros.
+        upper = -1;
+        lower = (int32_t)~(UINT32_MAX >> Min(js_bitscan_clz32(~lhs->lower_),
+                                             js_bitscan_clz32(rhs->upper_)));
+    } else if (lhs->lower_ >= 0 && rhs->upper_ < 0) {
+        // One operand is negative and the other is non-negative. As above.
+        upper = -1;
+        lower = (int32_t)~(UINT32_MAX >> Min(js_bitscan_clz32(lhs->upper_),
+                                             js_bitscan_clz32(~rhs->lower_)));
+    }
 
     return new Range(lower, upper);
 }
@@ -492,16 +523,7 @@ Range::xor_(const Range *lhs, const Range *rhs)
 Range *
 Range::not_(const Range *op)
 {
-    int64_t lower = INT32_MIN;
-    int64_t upper = INT32_MAX;
-
-    // Not inverts all bits, including the sign bit.
-    if (op->lower_ >= 0)
-        upper = -1;
-    else if (op->upper_ < 0)
-        lower = 0;
-
-    return new Range(lower, upper);
+    return new Range(~op->upper_, ~op->lower_);
 }
 
 Range *
