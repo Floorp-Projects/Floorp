f97d73335eee47f8ecccf00932a5b4c5d36b9795
Terrence Cole <terrence@mozilla.com>
2013-07-09 14:37:08 -0700

    Bug 891436 - Skip heap walk for profilingScripts during minor GC; r=billm

--- js/src/gc/RootMarking.cpp
+++ js/src/gc/RootMarking.cpp
@@ -713,7 +713,7 @@ js::gc::MarkRuntime(JSTracer *trc, bool useSavedRoots)
         }
 
         /* Do not discard scripts with counts while profiling. */
-        if (rt->profilingScripts) {
+        if (rt->profilingScripts && !rt->isHeapMinorCollecting()) {
             for (CellIterUnderGC i(zone, FINALIZE_SCRIPT); !i.done(); i.next()) {
                 JSScript *script = i.get<JSScript>();
                 if (script->hasScriptCounts) {
