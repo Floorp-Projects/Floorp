70bc2363f8206d6f05d428ac712de446d0569386
Masayuki Nakano <masayuki@d-toybox.com>
2013-07-10 00:54:41 +0900

    Bug 891292 r=jimm

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -3748,7 +3748,7 @@ bool nsWindow::DispatchPluginEvent(UINT aMessage,
 {
   bool ret = nsWindowBase::DispatchPluginEvent(
                WinUtils::InitMSG(aMessage, aWParam, aLParam, mWnd));
-  if (aDispatchPendingEvents) {
+  if (aDispatchPendingEvents && !Destroyed()) {
     DispatchPendingEvents();
   }
   return ret;
