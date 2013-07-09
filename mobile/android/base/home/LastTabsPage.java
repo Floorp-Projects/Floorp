90514342f13ff886bc245141771407d401d96fc4
Lucas Rocha <lucasr@mozilla.com>
2013-07-09 23:26:05 +0100

    Bug 891485 - Use SimpleCursorAdapter from Android's support library (r=margaret)

--- mobile/android/base/home/LastTabsPage.java
+++ mobile/android/base/home/LastTabsPage.java
@@ -26,13 +26,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
