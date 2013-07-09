3426f1fd5835333c0d977c5abe751dc52032f540
Nicholas D. Matsakis <nmatsakis@mozilla.com>
2013-07-09 15:29:39 -0400

    Bug 829896 - Make typed array element accesses return undefined if the element is out of bounds, rather than accessing the prototype chain r=jandem

--- js/src/jit-test/tests/ion/typed-arrays-2.js
+++ js/src/jit-test/tests/ion/typed-arrays-2.js
@@ -11,8 +11,6 @@ function test1() {
             assertEq(res, 0xffffee00);
         else if (i == 84)
             assertEq(res, 444);
-        else if (i == 105)
-            assertEq(res, true);
         else if (i >= 100)
             assertEq(res, undefined);
     }
---
+++ js/src/jit-test/tests/parallelarray/bug890465.js
@@ -0,0 +1,4 @@
+x = Uint8ClampedArray()
+ParallelArray([320], function() {
+    return x[8]
+})
