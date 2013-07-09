311f14b5eda725cd82519dc50f7aa7c5d2e022f2
Nicholas Nethercote <nnethercote@mozilla.com>
2013-07-08 23:03:03 -0700

    Bug 891215 (part 13) - Slim down ArgumentsObject-inl.h.  r=terrence.

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -1615,12 +1615,6 @@ class MOZ_STACK_CLASS AutoKeepAtoms
     ~AutoKeepAtoms() { JS_UNKEEP_ATOMS(rt); }
 };
 
-// Maximum supported value of arguments.length. This bounds the maximum
-// number of arguments that can be supplied to Function.prototype.apply.
-// This value also bounds the number of elements parsed in an array
-// initialiser.
-static const unsigned ARGS_LENGTH_MAX = 500 * 1000;
-
 inline void
 PerThreadData::setIonStackLimit(uintptr_t limit)
 {
