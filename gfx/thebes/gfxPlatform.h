cb012d22919146278c77c78add363e7439d96c5b
Jeff Muizelaar <jmuizelaar@mozilla.com>
2013-07-09 21:02:41 -0400

    Bug 891660. Add support for Azure on Mac with OMTC. r=mattwoodrow
    This is not the best approach but it works for now.  We basically pull the cg
    context from gfxASurface and wrap a DrawTarget around it. In the end we'll need
    to plumb the DrawTarget through more.

--- gfx/thebes/gfxPlatform.h
+++ gfx/thebes/gfxPlatform.h
@@ -180,6 +180,9 @@ public:
     virtual mozilla::RefPtr<mozilla::gfx::DrawTarget>
       CreateDrawTargetForSurface(gfxASurface *aSurface, const mozilla::gfx::IntSize& aSize);
 
+    virtual mozilla::RefPtr<mozilla::gfx::DrawTarget>
+      CreateDrawTargetForUpdateSurface(gfxASurface *aSurface, const mozilla::gfx::IntSize& aSize);
+
     /*
      * Creates a SourceSurface for a gfxASurface. This function does no caching,
      * so the caller should cache the gfxASurface if it will be used frequently.
