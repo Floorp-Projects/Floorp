fe63355cbbb8309bfddf4d17e68f202d8e5c39ee
Jason Orendorff <jorendorff@mozilla.com>
2013-07-10 08:14:01 -0500

    Bug 888002 - Constant folding affects meaning of some weird delete operands. r=Waldo.

--- js/src/frontend/FoldConstants.h
+++ js/src/frontend/FoldConstants.h
@@ -9,6 +9,8 @@
 
 #include "jsprvtd.h"
 
+#include "frontend/SyntaxParseHandler.h"
+
 namespace js {
 namespace frontend {
 
@@ -25,11 +27,14 @@ namespace frontend {
 //        return false;
 //    if (!FoldConstants(cx, &pn, parser))
 //        return false;
-template <typename ParseHandler>
 bool
-FoldConstants(JSContext *cx, typename ParseHandler::Node *pnp,
-              Parser<ParseHandler> *parser,
-              bool inGenexpLambda = false, bool inCond = false);
+FoldConstants(JSContext *cx, ParseNode **pnp, Parser<FullParseHandler> *parser);
+
+inline bool
+FoldConstants(JSContext *cx, SyntaxParseHandler::Node *pnp, Parser<SyntaxParseHandler> *parser)
+{
+    return true;
+}
 
 } /* namespace frontend */
 } /* namespace js */
