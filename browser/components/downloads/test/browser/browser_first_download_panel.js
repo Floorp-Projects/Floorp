1503cb0b8a070f99936d163468b79ffd6a0fa6ac
Tim Taubert <ttaubert@mozilla.com>
2013-07-10 23:52:04 +0200

    Bug 891806 - part 1 - replace 'yield;' with 'yield undefined;' in browser/; r=jaws

--- browser/components/downloads/test/browser/browser_first_download_panel.js
+++ browser/components/downloads/test/browser/browser_first_download_panel.js
@@ -12,7 +12,7 @@ function gen_test()
 {
   try {
     // Ensure that state is reset in case previous tests didn't finish.
-    for (let yy in gen_resetState(DownloadsCommon.getData(window))) yield;
+    for (let yy in gen_resetState(DownloadsCommon.getData(window))) yield undefined;
 
     // With this set to false, we should automatically open the panel
     // the first time a download is started.
@@ -20,7 +20,7 @@ function gen_test()
 
     prepareForPanelOpen();
     DownloadsCommon.getData(window)._notifyDownloadEvent("start");
-    yield;
+    yield undefined;
 
     // If we got here, that means the panel opened.
     DownloadsPanel.hidePanel();
@@ -37,7 +37,7 @@ function gen_test()
     ok(false, e);
   } finally {
     // Clean up when the test finishes.
-    for (let yy in gen_resetState(DownloadsCommon.getData(window))) yield;
+    for (let yy in gen_resetState(DownloadsCommon.getData(window))) yield undefined;
   }
 }
 
