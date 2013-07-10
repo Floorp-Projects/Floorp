32c45cb67c3ee79e2e1f1f499efea1bb0fda2434
Wes Kocher <wkocher@mozilla.com>
2013-07-10 15:06:01 -0700

    Backout b131b1f08ffc for oranges on a CLOSED TREE

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -4388,12 +4388,8 @@ Selection::Collapse(nsINode* aParentNode, int32_t aOffset)
   if (!IsValidSelectionPoint(mFrameSelection, aParentNode))
     return NS_ERROR_FAILURE;
   nsresult result;
-
-  nsRefPtr<nsPresContext> presContext = GetPresContext();
-  if (presContext->Document() != aParentNode->OwnerDoc())
-    return NS_ERROR_FAILURE;
-
   // Delete all of the current ranges
+  nsRefPtr<nsPresContext>  presContext = GetPresContext();
   Clear(presContext);
 
   // Turn off signal for table selection
@@ -4620,10 +4616,6 @@ Selection::Extend(nsINode* aParentNode, int32_t aOffset)
   if (!IsValidSelectionPoint(mFrameSelection, aParentNode))
     return NS_ERROR_FAILURE;
 
-  nsRefPtr<nsPresContext> presContext = GetPresContext();
-  if (presContext->Document() != aParentNode->OwnerDoc())
-    return NS_ERROR_FAILURE;
-
   //mFrameSelection->InvalidateDesiredX();
 
   nsINode* anchorNode = GetAnchorNode();
@@ -4658,6 +4650,7 @@ Selection::Extend(nsINode* aParentNode, int32_t aOffset)
                                                   aParentNode, aOffset,
                                                   &disconnected);
 
+  nsRefPtr<nsPresContext>  presContext = GetPresContext();
   nsRefPtr<nsRange> difRange = new nsRange(aParentNode);
   if ((result1 == 0 && result3 < 0) || (result1 <= 0 && result2 < 0)){//a1,2  a,1,2
     //select from 1 to 2 unless they are collapsed
