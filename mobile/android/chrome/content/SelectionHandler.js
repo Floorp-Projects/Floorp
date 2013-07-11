85c2e6cd094eca02cd553a9bdd66b27eaabb4e6f
Kartikaya Gupta <kgupta@mozilla.com>
2013-07-11 17:58:13 -0400

    Bug 891175 - Fix positioning of caret as fallout from widget scaling. r=jimm, margaret

--- mobile/android/chrome/content/SelectionHandler.js
+++ mobile/android/chrome/content/SelectionHandler.js
@@ -332,7 +332,12 @@ var SelectionHandler = {
 
       // Get rect of editor
       let editorBounds = this._domWinUtils.sendQueryContentEvent(this._domWinUtils.QUERY_EDITOR_RECT, 0, 0, 0, 0);
-      let editorRect = new Rect(editorBounds.left, editorBounds.top, editorBounds.width, editorBounds.height);
+      // the return value from sendQueryContentEvent is in LayoutDevice pixels and we want CSS pixels, so
+      // divide by the pixel ratio
+      let editorRect = new Rect(editorBounds.left / window.devicePixelRatio,
+                                editorBounds.top / window.devicePixelRatio,
+                                editorBounds.width / window.devicePixelRatio,
+                                editorBounds.height / window.devicePixelRatio);
 
       // Use intersection of the text rect and the editor rect
       let rect = new Rect(textBounds.left, textBounds.top, textBounds.width, textBounds.height);
@@ -498,8 +503,10 @@ var SelectionHandler = {
       // The left and top properties returned are relative to the client area
       // of the window, so we don't need to account for a sub-frame offset.
       let cursor = this._domWinUtils.sendQueryContentEvent(this._domWinUtils.QUERY_CARET_RECT, this._targetElement.selectionEnd, 0, 0, 0);
-      let x = cursor.left;
-      let y = cursor.top + cursor.height;
+      // the return value from sendQueryContentEvent is in LayoutDevice pixels and we want CSS pixels, so
+      // divide by the pixel ratio
+      let x = cursor.left / window.devicePixelRatio;
+      let y = (cursor.top + cursor.height) / window.devicePixelRatio;
       positions = [{ handle: this.HANDLE_TYPE_MIDDLE,
                      left: x + scrollX.value,
                      top: y + scrollY.value,
