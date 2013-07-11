64baaa78ece61ba251a05a94cd1d908a6112ee77
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2013-07-12 06:26:55 +0900

    Bug 888870 -  Add Windows 8.1 support to WinUtils::GetWindowsVersion. r=jimm, a=bajaj

--- widget/windows/WinUtils.h
+++ widget/windows/WinUtils.h
@@ -44,10 +44,9 @@ public:
     WINXP_VERSION     = 0x501,
     WIN2K3_VERSION    = 0x502,
     VISTA_VERSION     = 0x600,
-    // WIN2K8_VERSION    = VISTA_VERSION,
     WIN7_VERSION      = 0x601,
-    // WIN2K8R2_VERSION  = WIN7_VERSION
-    WIN8_VERSION      = 0x602
+    WIN8_VERSION      = 0x602,
+    WIN8_1_VERSION    = 0x603
   };
   static WinVersion GetWindowsVersion();
 
