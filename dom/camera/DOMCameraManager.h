d665c0e1a51e7d19d94fc1a768d8786501e06d26
Boris Zbarsky <bzbarsky@mit.edu>
2013-07-12 10:35:44 -0400

    Bug 838146 part 2.  Implement WebIDL API on Navigator for all our nonstandard extensions that are not behind build-time flags.  r=smaug, sr=peterv

--- dom/camera/DOMCameraManager.h
+++ dom/camera/DOMCameraManager.h
@@ -45,8 +45,9 @@ public:
                                                          nsIObserver)
   NS_DECL_NSIOBSERVER
 
+  static bool CheckPermission(nsPIDOMWindow* aWindow);
   static already_AddRefed<nsDOMCameraManager>
-    CheckPermissionAndCreateInstance(nsPIDOMWindow* aWindow);
+    CreateInstance(nsPIDOMWindow* aWindow);
   static bool IsWindowStillActive(uint64_t aWindowId);
 
   void Register(mozilla::nsDOMCameraControl* aDOMCameraControl);
