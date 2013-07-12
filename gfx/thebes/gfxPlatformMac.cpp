9362336521ff730240fd3206c0b8abc0805b9e8b
Matt Woodrow <mwoodrow@mozilla.com>
2013-07-12 17:19:29 -0400

    Bug 892966 - Allow CG to be enabled as an azure content backend. r=jrmuizel

--- gfx/thebes/gfxPlatformMac.cpp
+++ gfx/thebes/gfxPlatformMac.cpp
@@ -71,7 +71,7 @@ gfxPlatformMac::gfxPlatformMac()
     mFontAntiAliasingThreshold = ReadAntiAliasingThreshold();
 
     uint32_t canvasMask = (1 << BACKEND_CAIRO) | (1 << BACKEND_SKIA) | (1 << BACKEND_COREGRAPHICS);
-    uint32_t contentMask = 0;
+    uint32_t contentMask = (1 << BACKEND_COREGRAPHICS);
     InitBackendPrefs(canvasMask, contentMask);
 }
 
