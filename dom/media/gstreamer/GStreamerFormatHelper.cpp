4b81ff4ab272bc832a8b865beb786b7e569a4b19
Edwin Flores <eflores@mozilla.com>
2013-07-15 19:39:00 +1200

    Bug 981869 - Blacklist crashy flump3dec gstreamer plugin - r=kinetik

--- dom/media/gstreamer/GStreamerFormatHelper.cpp
+++ dom/media/gstreamer/GStreamerFormatHelper.cpp
@@ -8,6 +8,7 @@
 #include "nsCharSeparatedTokenizer.h"
 #include "nsString.h"
 #include "GStreamerLoader.h"
+#include "mozilla/Preferences.h"
 
 #define ENTRY_FORMAT(entry) entry[0]
 #define ENTRY_CAPS(entry) entry[1]
@@ -62,6 +63,10 @@ static char const * const sDefaultCodecCaps[][2] = {
   {"audio/x-m4a", "audio/mpeg, mpegversion=(int)4"},
   {"audio/mp3", "audio/mpeg, layer=(int)3"},
   {"audio/mpeg", "audio/mpeg, layer=(int)3"}
+};
+
+static char const * const sPluginBlacklist[] = {
+  "flump3dec",
 };
 
 GStreamerFormatHelper::GStreamerFormatHelper()
@@ -203,21 +208,58 @@ GstCaps* GStreamerFormatHelper::ConvertFormatsToCaps(const char* aMIMEType,
   return caps;
 }
 
+/* static */ bool
+GStreamerFormatHelper::IsBlacklistEnabled()
+{
+  static bool sBlacklistEnabled;
+  static bool sBlacklistEnabledCached = false;
+
+  if (!sBlacklistEnabledCached) {
+    Preferences::AddBoolVarCache(&sBlacklistEnabled,
+                                 "media.gstreamer.enable-blacklist", true);
+    sBlacklistEnabledCached = true;
+  }
+
+  return sBlacklistEnabled;
+}
+
+/* static */ bool
+GStreamerFormatHelper::IsPluginFeatureBlacklisted(GstPluginFeature *aFeature,
+                                                  FactoryType aTypes)
+{
+  if (!IsBlacklistEnabled()) {
+    return false;
+  }
+
+  const gchar *className =
+    gst_element_factory_get_klass(GST_ELEMENT_FACTORY_CAST(aFeature));
+
+  const gchar *factoryName =
+    gst_plugin_feature_get_name(aFeature);
+
+  if ((!(aTypes & FactoryTypeDecoder) && strstr(className, "Decoder")) ||
+      (!(aTypes & FactoryTypeDemuxer) && strstr(className, "Demuxer")) ||
+      (!(aTypes & FactoryTypeParser) && strstr(className, "Parser"))) {
+    return false;
+  }
+
+  for (unsigned int i = 0; i < G_N_ELEMENTS(sPluginBlacklist); i++) {
+    if (!strcmp(factoryName, sPluginBlacklist[i])) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 static gboolean FactoryFilter(GstPluginFeature *aFeature, gpointer)
 {
   if (!GST_IS_ELEMENT_FACTORY(aFeature)) {
     return FALSE;
   }
 
-  // TODO _get_klass doesn't exist in 1.0
-  const gchar *className =
-    gst_element_factory_get_klass(GST_ELEMENT_FACTORY_CAST(aFeature));
-
-  if (!strstr(className, "Decoder") && !strstr(className, "Demux")) {
-    return FALSE;
-  }
-
-  return gst_plugin_feature_get_rank(aFeature) >= GST_RANK_MARGINAL;
+  return !GStreamerFormatHelper::IsPluginFeatureBlacklisted(aFeature,
+                                                            (FactoryType)(FactoryTypeDecoder|FactoryTypeDemuxer));
 }
 
 /**
--- dom/media/gstreamer/GStreamerReader.h
+++ dom/media/gstreamer/GStreamerReader.h
@@ -25,6 +25,8 @@
 #include "ImageContainer.h"
 #include "nsRect.h"
 
+struct GstURIDecodeBin;
+
 namespace mozilla {
 
 namespace dom {
@@ -89,6 +91,30 @@ private:
 
   static GstBusSyncReply ErrorCb(GstBus *aBus, GstMessage *aMessage, gpointer aUserData);
   GstBusSyncReply Error(GstBus *aBus, GstMessage *aMessage);
+
+  /*
+   * We attach this callback to playbin so that when uridecodebin is
+   * constructed, we can then list for its autoplug-sort signal to blacklist
+   * the elements it can construct.
+   */
+  static void ElementAddedCb(GstBin *aPlayBin,
+                             GstElement *aElement,
+                             gpointer aUserData);
+
+  /*
+   * Called on the autoplug-sort signal emitted by uridecodebin for filtering
+   * the elements it uses.
+   */
+  static GValueArray *ElementFilterCb(GstURIDecodeBin *aBin,
+                                      GstPad *aPad,
+                                      GstCaps *aCaps,
+                                      GValueArray *aFactories,
+                                      gpointer aUserData);
+
+  GValueArray *ElementFilter(GstURIDecodeBin *aBin,
+                             GstPad *aPad,
+                             GstCaps *aCaps,
+                             GValueArray *aFactories);
 
   /* Called on the source-setup signal emitted by playbin. Used to
    * configure appsrc .
@@ -169,7 +195,7 @@ private:
   static bool ShouldAutoplugFactory(GstElementFactory* aFactory, GstCaps* aCaps);
 
   /* Called by decodebin during autoplugging. We use it to apply our
-   * container/codec whitelist.
+   * container/codec blacklist.
    */
   static GValueArray* AutoplugSortCb(GstElement* aElement,
                                      GstPad* aPad, GstCaps* aCaps,
