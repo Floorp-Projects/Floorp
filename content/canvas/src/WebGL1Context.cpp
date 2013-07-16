2b5f67396152ea05f6a73441e54727d76f55e807
Guillaume Abadie <gabadie@mozilla.com>
2013-07-16 10:32:24 -0400

    Bug 890049 - WebGL2 Replace WebGLContext by WebGLContext with one subclass WebGL1Context. r=jgilbert

---
+++ content/canvas/src/WebGL1Context.cpp
@@ -0,0 +1,50 @@
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "WebGL1Context.h"
+#include "mozilla/dom/WebGLRenderingContextBinding.h"
+
+#include "mozilla/Telemetry.h"
+
+using namespace mozilla;
+
+// -----------------------------------------------------------------------------
+// CONSTRUCTOR & DESTRUCTOR
+
+WebGL1Context::WebGL1Context()
+    : WebGLContext()
+{
+
+}
+
+WebGL1Context::~WebGL1Context()
+{
+
+}
+
+
+// -----------------------------------------------------------------------------
+// IMPLEMENT nsWrapperCache
+
+JSObject*
+WebGL1Context::WrapObject(JSContext *cx, JS::Handle<JSObject*> scope)
+{
+    return dom::WebGLRenderingContextBinding::Wrap(cx, scope, this);
+}
+
+
+// -----------------------------------------------------------------------------
+// INSTANCING nsIDOMWebGLRenderingContext
+
+nsresult
+NS_NewCanvasRenderingContextWebGL(nsIDOMWebGLRenderingContext** aResult)
+{
+    Telemetry::Accumulate(Telemetry::CANVAS_WEBGL_USED, 1);
+    nsIDOMWebGLRenderingContext* ctx = new WebGL1Context();
+
+    NS_ADDREF(*aResult = ctx);
+    return NS_OK;
+}
+
