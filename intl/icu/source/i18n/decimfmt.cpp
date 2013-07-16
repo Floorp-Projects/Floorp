3671ab424236ff8fd52759bfd526587c24d9ecd7
Norbert Lindenberg <mozilladev@lindenbergsoftware.com>
2013-07-16 10:39:56 -0700

    Bug 853706 - Backported fix for formatting 0 with significant digits from ICU, and add a warning about this backported fix to update-icu.sh.  r=jwalden

--- intl/icu/source/i18n/decimfmt.cpp
+++ intl/icu/source/i18n/decimfmt.cpp
@@ -1714,6 +1714,14 @@ DecimalFormat::subformat(UnicodeString& appendTo,
                 appendTo.append(*grouping);
                 handler.addAttribute(kGroupingSeparatorField, currentLength, appendTo.length());
             }
+        } 
+
+        // This handles the special case of formatting 0. For zero only, we count the 
+        // zero to the left of the decimal point as one signficant digit. Ordinarily we 
+        // do not count any leading 0's as significant. If the number we are formatting 
+        // is not zero, then either sigCount or digits.getCount() will be non-zero. 
+        if (sigCount == 0 && digits.getCount() == 0) { 
+          sigCount = 1; 
         }
 
         // TODO(dlf): this looks like it was a bug, we marked the int field as ending
