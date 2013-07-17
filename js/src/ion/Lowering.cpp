fa0f6ed5b2021f301105ac7263d119486a4de3db
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2013-07-17 11:51:19 -0700

    Bug 892426 - Add an additional temp-Register to SetElementIC. r=jandem a=abillings

--- js/src/ion/Lowering.cpp
+++ js/src/ion/Lowering.cpp
@@ -2427,7 +2427,7 @@ LIRGenerator::visitSetElementCache(MSetElementCache *ins)
 
     LInstruction *lir;
     if (ins->value()->type() == MIRType_Value) {
-        lir = new LSetElementCacheV(useRegister(ins->object()), temp());
+        lir = new LSetElementCacheV(useRegister(ins->object()), temp(), temp());
 
         if (!useBox(lir, LSetElementCacheV::Index, ins->index()))
             return false;
@@ -2437,7 +2437,7 @@ LIRGenerator::visitSetElementCache(MSetElementCache *ins)
         lir = new LSetElementCacheT(
             useRegister(ins->object()),
             useRegisterOrConstant(ins->value()),
-            temp());
+            temp(), temp());
 
         if (!useBox(lir, LSetElementCacheT::Index, ins->index()))
             return false;
