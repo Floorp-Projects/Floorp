d098b84f3be7168363a44e213fcbf39cfbc29be3
Steve Fink <sfink@mozilla.com>
2013-07-17 11:08:05 -0700

    Bug 895027 - Print out commands executed to make it easier to debug individual scripts. r=NPOTB
    DONTBUILD

--- js/src/devtools/rootAnalysis/analyze.py
+++ js/src/devtools/rootAnalysis/analyze.py
@@ -34,6 +34,20 @@ def fill(command, config):
                 print("  %s" % fragment)
         raise Hell
 
+def print_command(command, outfile=None, env=None):
+    output = ' '.join(command)
+    if outfile:
+        output += ' > ' + outfile
+    if env:
+        changed = {}
+        e = os.environ
+        for key,value in env.items():
+            if (key not in e) or (e[key] != value):
+                changed[key] = value
+        if changed:
+            output = ' '.join(key + "='" + value + "'" for key, value in changed.items()) + ' ' + output
+    print output
+
 def generate_hazards(config, outfilename):
     jobs = []
     for i in range(config['jobs']):
@@ -47,7 +61,7 @@ def generate_hazards(config, outfilename):
                        config)
         outfile = 'rootingHazards.%s' % (i+1,)
         output = open(outfile, 'w')
-        print(' '.join(command) + ' > ' + outfile)
+        print_command(command, outfile=outfile, env=env(config))
         jobs.append((command, Popen(command, stdout=output, env=env(config))))
 
     final_status = 0
@@ -61,7 +75,7 @@ def generate_hazards(config, outfilename):
 
     with open(outfilename, 'w') as output:
         command = ['cat'] + [ 'rootingHazards.%s' % (i+1,) for i in range(config['jobs']) ]
-        print(' '.join(command) + ' > ' + outfilename)
+        print_command(command, outfile=outfilename)
         subprocess.call(command, stdout=output)
 
 JOBS = { 'dbs':
@@ -109,8 +123,8 @@ def run_job(name, config):
         command(config, outfilename)
     else:
         command = fill(command, config)
-        print(' '.join(command))
         temp = '%s.tmp' % name
+        print_command(command, outfile=outfilename, env=env(config))
         with open(temp, 'w') as output:
             subprocess.check_call(command, stdout=output, env=env(config))
         if outfilename is not None:
