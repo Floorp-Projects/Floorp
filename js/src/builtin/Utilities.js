dfd9f717808dcc4c54037972eec7220fb4d676e0
Ryan VanderMeulen <ryanvm@gmail.com>
2013-07-18 13:06:48 -0400

    Backed out changeset c1025190b208 (bug 866847) for jit-test failures. CLOSED TREE

--- js/src/builtin/Utilities.js
+++ js/src/builtin/Utilities.js
@@ -73,12 +73,7 @@ var std_String_toUpperCase = String.prototype.toUpperCase;
 var std_WeakMap_get = WeakMap.prototype.get;
 var std_WeakMap_has = WeakMap.prototype.has;
 var std_WeakMap_set = WeakMap.prototype.set;
-var std_Map_has = Map.prototype.has;
-var std_Set_has = Set.prototype.has;
-var std_Map_iterator = Map().iterator;
-var std_Set_iterator = Set().iterator;
-var std_Map_iterator_next = Object.getPrototypeOf(Map().iterator()).next;
-var std_Set_iterator_next = Object.getPrototypeOf(Set().iterator()).next;
+
 
 /********** List specification type **********/
 
