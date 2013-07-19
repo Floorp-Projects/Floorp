babf05d4041d1991adb3032fb28d8165f4144d00
Ed Morley <emorley@mozilla.com>
2013-07-19 11:27:04 +0100

    Backed out changeset 4303c1af09f6 (bug 895409) for Android 2.2 reftest failures on a CLOSED TREE

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -762,7 +762,3 @@ pref("media.useAudioChannelService", false);
 
 // Turn on the CSP 1.0 parser for Content Security Policy headers
 pref("security.csp.speccompliant", true);
-
-// Enable hardware-accelerated Skia canvas
-pref("gfx.canvas.azure.backends", "skia");
-pref("gfx.canvas.azure.accelerated", true);
