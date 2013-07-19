fda8f8c233206d79d8850139ced5841768c51dfc
Joshua Cranmer <Pidgeot18@gmail.com>
2013-07-18 21:22:23 -0500

    Bug 884061 - Part 3g: Use NS_DECL_THREADSAFE_ISUPPORTS in embedding/, r=bsmedberg
    --HG--
    extra : rebase_source : 945c7378a49ec5ffb235925cfc7986ba9d6061bc

--- embedding/components/printingui/src/win/nsPrintProgress.cpp
+++ embedding/components/printingui/src/win/nsPrintProgress.cpp
@@ -14,14 +14,14 @@
 #include "nsAtomicRefcnt.h"
 
 #if 0
-NS_IMPL_THREADSAFE_ADDREF(nsPrintProgress)
-NS_IMPL_THREADSAFE_RELEASE(nsPrintProgress)
+NS_IMPL_ADDREF(nsPrintProgress)
+NS_IMPL_RELEASE(nsPrintProgress)
 #else
 NS_IMETHODIMP_(nsrefcnt) nsPrintProgress::AddRef(void)
 {
   NS_PRECONDITION(int32_t(mRefCnt) >= 0, "illegal refcnt");
   nsrefcnt count;
-  count = NS_AtomicIncrementRefcnt(mRefCnt);
+  count = ++mRefCnt;
   //NS_LOG_ADDREF(this, count, "nsPrintProgress", sizeof(*this));
   return count;
 }
@@ -30,7 +30,7 @@ NS_IMETHODIMP_(nsrefcnt) nsPrintProgress::Release(void)
 {
   nsrefcnt count;
   NS_PRECONDITION(0 != mRefCnt, "dup release");
-  count = NS_AtomicDecrementRefcnt(mRefCnt);
+  count = --mRefCnt;
   //NS_LOG_RELEASE(this, count, "nsPrintProgress");
   if (0 == count) {
     mRefCnt = 1; /* stabilize */
