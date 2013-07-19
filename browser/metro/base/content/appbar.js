725ff39ae5dff72fb75d38c6d46053b18198eec6
Jonathan Wilde <jwilde@jwilde.me>
2013-07-18 18:28:24 -0700

    Bug 894949 - Defect - Plural form strings are excessively complex for both devs and translators. f=flod, r=mbrubeck

--- browser/metro/base/content/appbar.js
+++ browser/metro/base/content/appbar.js
@@ -45,10 +45,9 @@ var Appbar = {
 
       case 'MozContextActionsChange':
         let actions = aEvent.actions;
-        let noun = aEvent.noun;
-        let qty = aEvent.qty;
+        let setName = aEvent.target.contextSetName;
         // could transition in old, new buttons?
-        this.showContextualActions(actions, noun, qty);
+        this.showContextualActions(actions, setName);
         break;
 
       case "selectionchange":
@@ -161,14 +160,15 @@ var Appbar = {
     }
   },
 
-  showContextualActions: function(aVerbs, aNoun, aQty) {
+  showContextualActions: function(aVerbs, aSetName) {
     // When the appbar is not visible, we want the icons to refresh right away
     let immediate = !Elements.contextappbar.isShowing;
 
-    if (aVerbs.length)
+    if (aVerbs.length) {
       Elements.contextappbar.show();
-    else
+    } else {
       Elements.contextappbar.hide();
+    }
 
     // Look up all of the buttons for the verbs that should be visible.
     let idsToVisibleVerbs = new Map();
@@ -187,7 +187,7 @@ var Appbar = {
       let verb = idsToVisibleVerbs.get(button.id);
       if (verb != undefined) {
         // Button should be visible, and may or may not be showing.
-        this._updateContextualActionLabel(button, verb, aNoun, aQty);
+        this._updateContextualActionLabel(button, verb, aSetName);
         if (button.hidden) {
           toShow.push(button);
         }
@@ -223,18 +223,12 @@ var Appbar = {
     this.showContextualActions([]);
   },
 
-  _updateContextualActionLabel: function(aBtnNode, aVerb, aNoun, aQty) {
-    // True if action modifies the noun for the grid (bookmark, top site, etc.),
-    // causing the label to be pluralized by the number of selected items.
-    let modifiesNoun = aBtnNode.getAttribute("modifies-noun") == "true";
-    if (modifiesNoun && (!aNoun || isNaN(aQty))) {
-      throw new Error("Appbar._updateContextualActionLabel: " +
-                      "missing noun/quantity for " + aVerb);
-    }
-
-    let labelName = "contextAppbar." + aVerb + (modifiesNoun ? "." + aNoun : "");
-    let label = Strings.browser.GetStringFromName(labelName);
-    aBtnNode.label = modifiesNoun ? PluralForm.get(aQty, label) : label;
+  _updateContextualActionLabel: function(aButton, aVerb, aSetName) {
+    // True if the action's label string contains the set name and
+    // thus has to be selected based on the list passed in.
+    let usesSetName = aButton.hasAttribute("label-uses-set-name");
+    let name = "contextAppbar2." + aVerb + (usesSetName ? "." + aSetName : "");
+    aButton.label = Strings.browser.GetStringFromName(name);
   },
 
   _onTileSelectionChanged: function _onTileSelectionChanged(aEvent){
@@ -255,10 +249,8 @@ var Appbar = {
     // fire event with these verbs as payload
     let event = document.createEvent("Events");
     event.actions = verbs;
-    event.noun = activeTileset.contextNoun;
-    event.qty = activeTileset.selectedItems.length;
     event.initEvent("MozContextActionsChange", true, false);
-    Elements.contextappbar.dispatchEvent(event);
+    activeTileset.dispatchEvent(event);
 
     if (verbs.length) {
       Elements.contextappbar.show(); // should be no-op if we're already showing
