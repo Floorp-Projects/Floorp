1b2fff0d4ac5c8cdef9d565adb7730bdaaa57813
Benjamin Smedberg <benjamin@smedbergs.us>
2013-07-19 10:02:48 -0400

    Bug 889788 - Plugin doorhanger breaks when plugins are dynamically removed from a page. This implements the short-term solution of hiding the plugin doorhanger when no more plugins are on the page (the better long-term solution will keep showing the UI and track all the plugins a page ever uses). This also implements a short-term solution when a user enables a plugin. Also bug 887088 - Short-term UI solution: when a user loads multiple tabs from a site and enables a plugin on one of them, the plugins are not enabled on other tabs but the "continue allowing" button does nothing. This patch makes the "Continue Allowing" button enable existing plugins of that type. r=jaws, r=jschoenick, a=bajaj

--- browser/base/content/browser-plugins.js
+++ browser/base/content/browser-plugins.js
@@ -217,14 +217,21 @@ var gPluginHandler = {
   },
 
   handleEvent : function(event) {
-    let plugin = event.target;
-    let doc = plugin.ownerDocument;
-
-    // We're expecting the target to be a plugin.
-    if (!(plugin instanceof Ci.nsIObjectLoadingContent))
-      return;
+    let plugin;
+    let doc;
 
     let eventType = event.type;
+    if (eventType === "PluginRemoved") {
+      doc = event.target;
+    }
+    else {
+      plugin = event.target;
+      doc = plugin.ownerDocument;
+
+      if (!(plugin instanceof Ci.nsIObjectLoadingContent))
+        return;
+    }
+
     if (eventType == "PluginBindingAttached") {
       // The plugin binding fires this event when it is created.
       // As an untrusted event, ensure that this object actually has a binding
@@ -304,12 +311,13 @@ var gPluginHandler = {
         break;
 
       case "PluginInstantiated":
+      case "PluginRemoved":
         this._showClickToPlayNotification(browser);
         break;
     }
 
     // Hide the in-content UI if it's too big. The crashed plugin handler already did this.
-    if (eventType != "PluginCrashed") {
+    if (eventType != "PluginCrashed" && eventType != "PluginRemoved") {
       let overlay = doc.getAnonymousElementByAttribute(plugin, "class", "mainBox");
       if (overlay != null && this.isTooSmall(plugin, overlay))
         overlay.style.visibility = "hidden";
@@ -685,18 +693,12 @@ var gPluginHandler = {
 
     switch (aNewState) {
       case "allownow":
-        if (aPluginInfo.fallbackType == Ci.nsIObjectLoadingContent.PLUGIN_ACTIVE) {
-          return;
-        }
         permission = Ci.nsIPermissionManager.ALLOW_ACTION;
         expireType = Ci.nsIPermissionManager.EXPIRE_SESSION;
         expireTime = Date.now() + Services.prefs.getIntPref(kPrefSessionPersistMinutes) * 60 * 1000;
         break;
 
       case "allowalways":
-        if (aPluginInfo.fallbackType == Ci.nsIObjectLoadingContent.PLUGIN_ACTIVE) {
-          return;
-        }
         permission = Ci.nsIPermissionManager.ALLOW_ACTION;
         expireType = Ci.nsIPermissionManager.EXPIRE_TIME;
         expireTime = Date.now() +
@@ -704,12 +706,14 @@ var gPluginHandler = {
         break;
 
       case "block":
-        if (aPluginInfo.fallbackType != Ci.nsIObjectLoadingContent.PLUGIN_ACTIVE) {
-          return;
-        }
         permission = Ci.nsIPermissionManager.PROMPT_ACTION;
         expireType = Ci.nsIPermissionManager.EXPIRE_NEVER;
         expireTime = 0;
+        break;
+
+      // In case a plugin has already been allowed in another tab, the "continue allowing" button
+      // shouldn't change any permissions but should run the plugin-enablement code below.
+      case "continue":
         break;
 
       default:
@@ -718,11 +722,13 @@ var gPluginHandler = {
     }
 
     let browser = aNotification.browser;
-    Services.perms.add(browser.currentURI, aPluginInfo.permissionString,
-                       permission, expireType, expireTime);
+    if (aNewState != "continue") {
+      Services.perms.add(browser.currentURI, aPluginInfo.permissionString,
+                         permission, expireType, expireTime);
 
-    if (aNewState == "block") {
-      return;
+      if (aNewState == "block") {
+        return;
+      }
     }
 
     // Manually activate the plugins that would have been automatically
