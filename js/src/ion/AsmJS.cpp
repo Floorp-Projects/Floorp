99cf38d1bc654e5e79096d8550000980a092308f
Hannes Verschore <hv1989@gmail.com>
2013-07-19 16:06:57 -0700

    Bug 886287 - OdinMonkey: Push the descriptor/argc filling the full pointer size, r=luke a=lsblakk

--- js/src/ion/AsmJS.cpp
+++ js/src/ion/AsmJS.cpp
@@ -5696,7 +5696,7 @@ GenerateFFIIonExit(ModuleCompiler &m, const ModuleCompiler::ExitDescriptor &exit
 
     // 1. Descriptor
     uint32_t descriptor = MakeFrameDescriptor(masm.framePushed() + extraBytes, IonFrame_Entry);
-    masm.store32(Imm32(descriptor), Address(StackPointer, 0));
+    masm.storePtr(ImmWord(uintptr_t(descriptor)), Address(StackPointer, 0));
 
     // 2. Callee
     Register callee = ABIArgGenerator::NonArgReturnVolatileReg0;
@@ -5722,7 +5722,7 @@ GenerateFFIIonExit(ModuleCompiler &m, const ModuleCompiler::ExitDescriptor &exit
 
     // 3. Argc
     unsigned argc = exit.argTypes().length();
-    masm.store32(Imm32(argc), Address(StackPointer, 2 * sizeof(size_t)));
+    masm.storePtr(ImmWord(uintptr_t(argc)), Address(StackPointer, 2 * sizeof(size_t)));
 
     // 4. |this| value
     masm.storeValue(UndefinedValue(), Address(StackPointer, 3 * sizeof(size_t)));
