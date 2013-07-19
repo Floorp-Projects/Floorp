946fedf8d880259f46132ebd3771d00be7ad3cad
Reuben Morais <reuben.morais@gmail.com>
2013-07-18 20:01:52 -0700

    Backed out changeset 1b779d67a022 (bug 893117)

--- content/html/content/src/HTMLUnknownElement.h
+++ content/html/content/src/HTMLUnknownElement.h
@@ -2,17 +2,18 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#ifndef mozilla_dom_HTMLUnknownElement_h
-#define mozilla_dom_HTMLUnknownElement_h
+#ifndef HTMLUnknownElement_h___
+#define HTMLUnknownElement_h___
 
 #include "mozilla/Attributes.h"
 #include "nsGenericHTMLElement.h"
+#include "nsIDOMHTMLUnknownElement.h"
 
 namespace mozilla {
 namespace dom {
 
 class HTMLUnknownElement MOZ_FINAL : public nsGenericHTMLElement
-                                   , public nsIDOMHTMLElement
+                                   , public nsIDOMHTMLUnknownElement
 {
 public:
   HTMLUnknownElement(already_AddRefed<nsINodeInfo> aNodeInfo)
@@ -47,4 +48,4 @@ protected:
 } // namespace dom
 } // namespace mozilla
 
-#endif /* mozilla_dom_HTMLUnknownElement_h */
+#endif /* HTMLUnknownElement_h___ */
