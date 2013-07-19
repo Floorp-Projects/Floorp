6b6589c40459c203018cf7941a1402e920ad1374
Robert Strong <robert.bugzilla@gmail.com>
2013-07-19 14:19:53 -0700

    Wallpaper fix - Bug 895473 - [Mac and Linux] Custom distribution settings for yahoo de and es distributions are not being set. r=mconnor, a=lsblakk

--- browser/components/distribution.js
+++ browser/components/distribution.js
@@ -265,13 +265,18 @@ DistributionCustomizer.prototype = {
 
     let partnerAbout = Cc["@mozilla.org/supports-string;1"].
       createInstance(Ci.nsISupportsString);
-    if (globalPrefs["about." + this._locale]) {
-      partnerAbout.data = this._ini.getString("Global", "about." + this._locale);
-    } else {
-      partnerAbout.data = this._ini.getString("Global", "about");
+    try {
+      if (globalPrefs["about." + this._locale]) {
+        partnerAbout.data = this._ini.getString("Global", "about." + this._locale);
+      } else {
+        partnerAbout.data = this._ini.getString("Global", "about");
+      }
+      defaults.setComplexValue("distribution.about",
+                               Ci.nsISupportsString, partnerAbout);
+    } catch (e) {
+      /* ignore bad prefs due to bug 895473 and move on */
+      Cu.reportError(e);
     }
-    defaults.setComplexValue("distribution.about",
-                             Ci.nsISupportsString, partnerAbout);
 
     if (sections["Preferences"]) {
       for (let key in enumerate(this._ini.getKeys("Preferences"))) {
