aea142f941d0a45b02b9bf9d50c061da35f65877
David Zbarsky <dzbarsky@gmail.com>
2013-07-18 17:43:38 -0700

    Bug 893117: Remove nsIDOMHTMLUnknownElement r=peterv

--- content/html/content/src/HTMLUnknownElement.h
+++ content/html/content/src/HTMLUnknownElement.h
@@ -2,18 +2,17 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#ifndef HTMLUnknownElement_h___
-#define HTMLUnknownElement_h___
+#ifndef mozilla_dom_HTMLUnknownElement_h
+#define mozilla_dom_HTMLUnknownElement_h
 
 #include "mozilla/Attributes.h"
 #include "nsGenericHTMLElement.h"
-#include "nsIDOMHTMLUnknownElement.h"
 
 namespace mozilla {
 namespace dom {
 
 class HTMLUnknownElement MOZ_FINAL : public nsGenericHTMLElement
-                                   , public nsIDOMHTMLUnknownElement
+                                   , public nsIDOMHTMLElement
 {
 public:
   HTMLUnknownElement(already_AddRefed<nsINodeInfo> aNodeInfo)
@@ -48,4 +47,4 @@ protected:
 } // namespace dom
 } // namespace mozilla
 
-#endif /* HTMLUnknownElement_h___ */
+#endif /* mozilla_dom_HTMLUnknownElement_h */
