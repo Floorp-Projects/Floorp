a52ceb3a718aa2f688d409cf71fd3b9b1caa00f1
L. David Baron <dbaron@dbaron.org>
2013-07-20 12:14:24 -0700

    Bug 896138 patch 2:  Add a RestyleManager class.  r=heycam
    The later patches in this series will move restyling code into this file
    from nsCSSFrameConstructor and nsFrameManager.

---
+++ layout/base/RestyleManager.cpp
@@ -0,0 +1,20 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Code responsible for managing style changes: tracking what style
+ * changes need to happen, scheduling them, and doing them.
+ */
+
+#include "RestyleManager.h"
+
+namespace mozilla {
+
+RestyleManager::RestyleManager(nsPresContext* aPresContext)
+  : mPresContext(aPresContext)
+{
+}
+
+} // namespace mozilla
