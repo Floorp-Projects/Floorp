759c3a5f7aedb59638ee3443ce2be8e5a806b18a
Gregory Szorc <gps@mozilla.com>
2013-07-22 13:24:45 -0700

    Backout b817406485f8 (bug 884421) for intermittent WinXP failures (bug 896093)
    CLOSED TREE

--- services/sync/tests/unit/test_service_startup.js
+++ services/sync/tests/unit/test_service_startup.js
@@ -13,6 +13,8 @@ function run_test() {
   _("When imported, Service.onStartup is called");
   initTestLogging("Trace");
 
+  new SyncTestingInfrastructure();
+
   // Test fixtures
   Service.identity.username = "johndoe";
 
