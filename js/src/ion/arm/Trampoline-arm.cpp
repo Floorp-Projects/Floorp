63b53ebbbefa8387dc85fd1aae1e28312893ff5d
Jan de Mooij <jdemooij@mozilla.com>
2013-07-23 09:23:42 +0200

    Bug 866888 part 4 - Move bailout tail code to its own stub. r=h4writer

--- js/src/ion/arm/Trampoline-arm.cpp
+++ js/src/ion/arm/Trampoline-arm.cpp
@@ -346,7 +346,11 @@ IonRuntime::generateInvalidator(JSContext *cx)
     // remove the space that this frame was using before the bailout
     // (computed by InvalidationBailout)
     masm.ma_add(sp, r1, sp);
-    masm.generateBailoutTail(r1, r2);
+
+    // Jump to shared bailout tail. The BailoutInfo pointer has to be in r2.
+    IonCode *bailoutTail = cx->compartment()->ionCompartment()->getBailoutTail();
+    masm.branch(bailoutTail);
+
     Linker linker(masm);
     IonCode *code = linker.newCode(cx, JSC::OTHER_CODE);
     IonSpew(IonSpew_Invalidate, "   invalidation thunk created at %p", (void *) code->raw());
@@ -456,7 +460,7 @@ IonRuntime::generateArgumentsRectifier(JSContext *cx, ExecutionMode mode, void *
 }
 
 static void
-GenerateBailoutThunk(MacroAssembler &masm, uint32_t frameClass)
+GenerateBailoutThunk(JSContext *cx, MacroAssembler &masm, uint32_t frameClass)
 {
     // the stack should look like:
     // [IonFrame]
@@ -510,10 +514,6 @@ GenerateBailoutThunk(MacroAssembler &masm, uint32_t frameClass)
     masm.mov(sp, r1);
     masm.setupAlignedABICall(2);
 
-    // Copy the present stack pointer into a temp register (it happens to be the
-    // argument register)
-    //masm.as_mov(r0, O2Reg(sp));
-
     // Decrement sp by another 4, so we keep alignment
     // Not Anymore!  pushing both the snapshotoffset as well as the
     // masm.as_sub(sp, sp, Imm8(4));
@@ -550,7 +550,10 @@ GenerateBailoutThunk(MacroAssembler &masm, uint32_t frameClass)
                           + bailoutFrameSize) // everything else that was pushed on the stack
                     , sp);
     }
-    masm.generateBailoutTail(r1, r2);
+
+    // Jump to shared bailout tail. The BailoutInfo pointer has to be in r2.
+    IonCode *bailoutTail = cx->compartment()->ionCompartment()->getBailoutTail();
+    masm.branch(bailoutTail);
 }
 
 IonCode *
@@ -563,7 +566,7 @@ IonRuntime::generateBailoutTable(JSContext *cx, uint32_t frameClass)
         masm.ma_bl(&bailout);
     masm.bind(&bailout);
 
-    GenerateBailoutThunk(masm, frameClass);
+    GenerateBailoutThunk(cx, masm, frameClass);
 
     Linker linker(masm);
     return linker.newCode(cx, JSC::OTHER_CODE);
@@ -573,7 +576,7 @@ IonCode *
 IonRuntime::generateBailoutHandler(JSContext *cx)
 {
     MacroAssembler masm(cx);
-    GenerateBailoutThunk(masm, NO_FRAME_SIZE_CLASS_ID);
+    GenerateBailoutThunk(cx, masm, NO_FRAME_SIZE_CLASS_ID);
 
     Linker linker(masm);
     return linker.newCode(cx, JSC::OTHER_CODE);
@@ -831,6 +834,17 @@ IonRuntime::generateExceptionTailStub(JSContext *cx)
     MacroAssembler masm;
 
     masm.handleFailureWithHandlerTail();
+
+    Linker linker(masm);
+    return linker.newCode(cx, JSC::OTHER_CODE);
+}
+
+IonCode *
+IonRuntime::generateBailoutTailStub(JSContext *cx)
+{
+    MacroAssembler masm;
+
+    masm.generateBailoutTail(r1, r2);
 
     Linker linker(masm);
     return linker.newCode(cx, JSC::OTHER_CODE);
