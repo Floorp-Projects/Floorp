0f5ec02bbb5a08f0d55d27630c3a62da7032baac
Ms2ger <ms2ger@gmail.com>
2013-07-24 09:40:01 +0200

    Bug 896280 - Pass MutableHandleValue to JS::Call(); r=terrence

--- js/ipc/JavaScriptChild.cpp
+++ js/ipc/JavaScriptChild.cpp
@@ -494,7 +494,7 @@ JavaScriptChild::AnswerCall(const ObjectId &objId, const nsTArray<JSParam> &argv
     uint32_t oldOpts =
         JS_SetOptions(cx, JS_GetOptions(cx) | JSOPTION_DONT_REPORT_UNCAUGHT);
 
-    jsval rval;
+    RootedValue rval(cx);
     bool success = JS::Call(cx, vals[1], vals[0], vals.length() - 2, vals.begin() + 2, &rval);
 
     JS_SetOptions(cx, oldOpts);
