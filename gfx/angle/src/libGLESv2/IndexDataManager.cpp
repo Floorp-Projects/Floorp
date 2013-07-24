cb2f7b1a9dc2bb165ccda68195fad383a30f524d
Guillaume Abadie <gabadie@mozilla.com>
2013-07-24 17:57:41 -0400

    bug 890277 - ANGLE libGLESv2 Integer Overflow - r=bjacob, a=bajaj

--- gfx/angle/src/libGLESv2/IndexDataManager.cpp
+++ gfx/angle/src/libGLESv2/IndexDataManager.cpp
@@ -380,7 +380,8 @@ void StreamingIndexBuffer::reserveSpace(UINT requiredSpace, GLenum type)
 
         mWritePosition = 0;
     }
-    else if (mWritePosition + requiredSpace > mBufferSize)   // Recycle
+    else if (mWritePosition + requiredSpace > mBufferSize ||
+             mWritePosition + requiredSpace < mWritePosition)   // Recycle
     {
         void *dummy;
         mIndexBuffer->Lock(0, 1, &dummy, D3DLOCK_DISCARD);
