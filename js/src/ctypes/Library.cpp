e0741679b63c1cb8ef30eb83346f62756367fb2d
Nicholas Nethercote <nnethercote@mozilla.com>
2013-07-23 17:34:18 -0700

    Bug 888088 (part 2) - Fix #include ordering in js/src/ctypes/.  r=nnethercote.

--- js/src/ctypes/Library.cpp
+++ js/src/ctypes/Library.cpp
@@ -4,11 +4,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "ctypes/Library.h"
+
 #include "jscntxt.h"
 #include "jsstr.h"
-#include "ctypes/Library.h"
-#include "ctypes/CTypes.h"
 #include "prlink.h"
+
+#include "ctypes/CTypes.h"
 
 namespace js {
 namespace ctypes {
