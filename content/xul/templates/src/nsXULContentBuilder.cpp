6da71ef53ddf198596d0d2af7a6634f399ef25f3
Ms2ger <ms2ger@gmail.com>
2013-07-24 09:39:46 +0200

    Bug 776517 - Move GetExistingAttrNameFromQName from nsIContent to dom::Element; r=smaug

--- content/xul/templates/src/nsXULContentBuilder.cpp
+++ content/xul/templates/src/nsXULContentBuilder.cpp
@@ -146,7 +146,7 @@ protected:
      * @param aResult result to look up variable->value bindings in
      */
     nsresult
-    AddPersistentAttributes(nsIContent* aTemplateNode,
+    AddPersistentAttributes(Element* aTemplateNode,
                             nsIXULTemplateResult* aResult,
                             nsIContent* aRealNode);
 
@@ -513,15 +513,15 @@ nsXULContentBuilder::BuildContentFromTemplate(nsIContent *aTemplateNode,
         bool isGenerationElement = false;
         bool isUnique = aIsUnique;
 
-        {
-            // We identify the resource element by presence of a
-            // "uri='rdf:*'" attribute. (We also support the older
-            // "uri='...'" syntax.)
-            if (tmplKid->HasAttr(kNameSpaceID_None, nsGkAtoms::uri) && aMatch->IsActive()) {
-                isGenerationElement = true;
-                isUnique = false;
-            }
+        // We identify the resource element by presence of a
+        // "uri='rdf:*'" attribute. (We also support the older
+        // "uri='...'" syntax.)
+        if (tmplKid->HasAttr(kNameSpaceID_None, nsGkAtoms::uri) && aMatch->IsActive()) {
+            isGenerationElement = true;
+            isUnique = false;
         }
+
+        MOZ_ASSERT_IF(isGenerationElement, tmplKid->IsElement());
 
         nsIAtom *tag = tmplKid->Tag();
 
@@ -679,7 +679,8 @@ nsXULContentBuilder::BuildContentFromTemplate(nsIContent *aTemplateNode,
 
             // Add any persistent attributes
             if (isGenerationElement) {
-                rv = AddPersistentAttributes(tmplKid, aChild, realKid);
+                rv = AddPersistentAttributes(tmplKid->AsElement(), aChild,
+                                             realKid);
                 if (NS_FAILED(rv)) return rv;
             }
 
@@ -783,7 +784,7 @@ nsXULContentBuilder::CopyAttributesToElement(nsIContent* aTemplateNode,
 }
 
 nsresult
-nsXULContentBuilder::AddPersistentAttributes(nsIContent* aTemplateNode,
+nsXULContentBuilder::AddPersistentAttributes(Element* aTemplateNode,
                                              nsIXULTemplateResult* aResult,
                                              nsIContent* aRealNode)
 {
