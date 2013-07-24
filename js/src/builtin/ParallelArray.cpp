f417d34176fb431bc579a86aa0ed234328256f9f
Emanuel Hoogeveen <emanuel.hoogeveen@gmail.com>
2013-07-23 17:34:18 -0700

    Bug 888088 (part 1) - Fix #include ordering in js/src/builtin/.  r=nnethercote.

--- js/src/builtin/ParallelArray.cpp
+++ js/src/builtin/ParallelArray.cpp
@@ -4,10 +4,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "builtin/ParallelArray.h"
+
 #include "jsapi.h"
 #include "jsobj.h"
-
-#include "builtin/ParallelArray.h"
 
 #include "vm/GlobalObject.h"
 #include "vm/String.h"
