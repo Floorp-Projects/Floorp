929d125f24632a3fe7c9e142dbd3133a620f024a
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2013-07-24 22:35:43 -0700

    Bug 889182 - mozapps/extensions xpcshell tests cannot be run concurrently. r=Unfocused

--- toolkit/mozapps/extensions/test/xpcshell/test_corrupt.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_corrupt.js
@@ -6,7 +6,14 @@
 
 
 Components.utils.import("resource://testing-common/httpd.js");
-var testserver;
+// Create and configure the HTTP server.
+var testserver = new HttpServer();
+testserver.start(-1);
+gPort = testserver.identity.primaryPort;
+
+// register files with server
+testserver.registerDirectory("/addons/", do_get_file("addons"));
+mapFile("/data/test_corrupt.rdf", testserver);
 
 // The test extension uses an insecure update url.
 Services.prefs.setBoolPref(PREF_EM_CHECK_UPDATE_SECURITY, false);
@@ -41,7 +48,7 @@ var addon3 = {
   id: "addon3@tests.mozilla.org",
   version: "1.0",
   name: "Test 3",
-  updateURL: "http://localhost:4444/data/test_corrupt.rdf",
+  updateURL: "http://localhost:" + gPort + "/data/test_corrupt.rdf",
   targetApplications: [{
     id: "xpcshell@tests.mozilla.org",
     minVersion: "1",
@@ -54,7 +61,7 @@ var addon4 = {
   id: "addon4@tests.mozilla.org",
   version: "1.0",
   name: "Test 4",
-  updateURL: "http://localhost:4444/data/test_corrupt.rdf",
+  updateURL: "http://localhost:" + gPort + "/data/test_corrupt.rdf",
   targetApplications: [{
     id: "xpcshell@tests.mozilla.org",
     minVersion: "1",
@@ -142,12 +149,6 @@ function run_test() {
   writeInstallRDFForExtension(addon7, profileDir);
   writeInstallRDFForExtension(theme1, profileDir);
   writeInstallRDFForExtension(theme2, profileDir);
-
-  // Create and configure the HTTP server.
-  testserver = new HttpServer();
-  testserver.registerDirectory("/addons/", do_get_file("addons"));
-  testserver.registerDirectory("/data/", do_get_file("data"));
-  testserver.start(4444);
 
   // Startup the profile and setup the initial state
   startupManager();
