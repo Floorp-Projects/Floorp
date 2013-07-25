b806b3e3c993667dd0f85398df371375e48f4d56
Scott Johnson <sjohnson@mozilla.com>
2013-07-25 10:34:16 -0500

    Bug 743402, Part 2: Pull GetEffectiveComputedHeight() into nsSplittableFrame and refactor it to utilize consumed height for paginated content. [r=roc]

--- layout/generic/nsBlockFrame.h
+++ layout/generic/nsBlockFrame.h
@@ -796,13 +796,6 @@ protected:
   void SetOverflowLines(FrameLines* aOverflowLines);
   void DestroyOverflowLines();
 
-  // Determine the computed height that's in effect for this block
-  // frame (that is, our computed height minus the heights of our
-  // previous in-flows).
-  // XXXbz this clearly makes laying out a block with N in-flows
-  // O(N^2)!  Good thing the constant is tiny.
-  nscoord GetEffectiveComputedHeight(const nsHTMLReflowState& aReflowState) const;
-
   /**
    * This class is useful for efficiently modifying the out of flow
    * overflow list. It gives the client direct writable access to
