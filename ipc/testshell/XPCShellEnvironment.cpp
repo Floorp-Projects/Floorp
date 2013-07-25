07e0aa63105c6a2c26e104df97e5e35ff3f82e91
Bobby Holley <bobbyholley@gmail.com>
2013-07-25 11:29:20 -0700

    Bug 889714 - Removed unused ShouldReportWarnings and ShouldCompoleOnly [sic] machinery. r=mrbkap
    lol @ 'ShouldCompoleOnly'.

--- ipc/testshell/XPCShellEnvironment.cpp
+++ ipc/testshell/XPCShellEnvironment.cpp
@@ -1,4 +1,5 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* vim: set ts=8 sts=4 et sw=4 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
@@ -116,12 +117,6 @@ ScriptErrorReporter(JSContext *cx,
 
     if (!report) {
         fprintf(stderr, "%s\n", message);
-        return;
-    }
-
-    /* Conditionally ignore reported warnings. */
-    if (JSREPORT_IS_WARNING(report->flags) &&
-        !Environment(cx)->ShouldReportWarnings()) {
         return;
     }
 
@@ -290,8 +285,7 @@ Load(JSContext *cx,
         if (!script)
             return JS_FALSE;
 
-        if (!Environment(cx)->ShouldCompileOnly() &&
-            !JS_ExecuteScript(cx, obj, script, result.address())) {
+        if (!JS_ExecuteScript(cx, obj, script, result.address())) {
             return JS_FALSE;
         }
     }
@@ -554,7 +548,7 @@ ProcessFile(JSContext *cx,
                .setFileAndLine(filename, 1)
                .setPrincipals(env->GetPrincipal());
         JSScript* script = JS::Compile(cx, obj, options, file);
-        if (script && !env->ShouldCompileOnly())
+        if (script)
             (void)JS_ExecuteScript(cx, obj, script, result.address());
 
         return;
@@ -594,22 +588,20 @@ ProcessFile(JSContext *cx,
         if (script) {
             JSErrorReporter older;
 
-            if (!env->ShouldCompileOnly()) {
-                ok = JS_ExecuteScript(cx, obj, script, result.address());
-                if (ok && result != JSVAL_VOID) {
-                    /* Suppress error reports from JS_ValueToString(). */
-                    older = JS_SetErrorReporter(cx, NULL);
-                    str = JS_ValueToString(cx, result);
-                    JSAutoByteString bytes;
-                    if (str)
-                        bytes.encodeLatin1(cx, str);
-                    JS_SetErrorReporter(cx, older);
+            ok = JS_ExecuteScript(cx, obj, script, result.address());
+            if (ok && result != JSVAL_VOID) {
+                /* Suppress error reports from JS_ValueToString(). */
+                older = JS_SetErrorReporter(cx, NULL);
+                str = JS_ValueToString(cx, result);
+                JSAutoByteString bytes;
+                if (str)
+                    bytes.encodeLatin1(cx, str);
+                JS_SetErrorReporter(cx, older);
 
-                    if (!!bytes)
-                        fprintf(stdout, "%s\n", bytes.ptr());
-                    else
-                        ok = JS_FALSE;
-                }
+                if (!!bytes)
+                    fprintf(stdout, "%s\n", bytes.ptr());
+                else
+                    ok = JS_FALSE;
             }
         }
     } while (!hitEOF && !env->IsQuitting());
@@ -670,9 +662,7 @@ XPCShellEnvironment::XPCShellEnvironment()
 :   mCx(NULL),
     mJSPrincipals(NULL),
     mExitCode(0),
-    mQuitting(JS_FALSE),
-    mReportWarnings(JS_TRUE),
-    mCompileOnly(JS_FALSE)
+    mQuitting(JS_FALSE)
 {
 }
 
@@ -851,24 +841,22 @@ XPCShellEnvironment::EvaluateString(const nsString& aString,
      return false;
   }
 
-  if (!ShouldCompileOnly()) {
-      if (aResult) {
-          aResult->Truncate();
-      }
+  if (aResult) {
+      aResult->Truncate();
+  }
 
-      JS::Rooted<JS::Value> result(mCx);
-      JSBool ok = JS_ExecuteScript(mCx, global, script, result.address());
-      if (ok && result != JSVAL_VOID) {
-          JSErrorReporter old = JS_SetErrorReporter(mCx, NULL);
-          JSString* str = JS_ValueToString(mCx, result);
-          nsDependentJSString depStr;
-          if (str)
-              depStr.init(mCx, str);
-          JS_SetErrorReporter(mCx, old);
+  JS::Rooted<JS::Value> result(mCx);
+  JSBool ok = JS_ExecuteScript(mCx, global, script, result.address());
+  if (ok && result != JSVAL_VOID) {
+      JSErrorReporter old = JS_SetErrorReporter(mCx, NULL);
+      JSString* str = JS_ValueToString(mCx, result);
+      nsDependentJSString depStr;
+      if (str)
+          depStr.init(mCx, str);
+      JS_SetErrorReporter(mCx, old);
 
-          if (!depStr.IsEmpty() && aResult) {
-              aResult->Assign(depStr);
-          }
+      if (!depStr.IsEmpty() && aResult) {
+          aResult->Assign(depStr);
       }
   }
 
