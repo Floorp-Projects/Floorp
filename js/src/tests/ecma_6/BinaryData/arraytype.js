30bdd22f04152fa1633a4d3c7028ad435aa7e8fe
Nikhil Marathe <nsm.nikhil@gmail.com>
2013-07-25 17:59:59 -0700

    Bug 578700 - BinaryData ArrayType method implementations. r=nmatsakis
    --HG--
    extra : amend_source : 8ae08d1ce9a57328dd6a3f8028abf21d0b806222

--- js/src/tests/ecma_6/BinaryData/arraytype.js
+++ js/src/tests/ecma_6/BinaryData/arraytype.js
@@ -34,6 +34,9 @@ function runTests() {
     assertEq(A.prototype.__proto__, ArrayType.prototype.prototype);
     assertEq(typeof A.prototype.fill, "function");
 
+    var X = { __proto__: A };
+    assertThrows(function() X.repeat(42));
+
     var a = new A();
     assertEq(a.__proto__, A.prototype);
     assertEq(a.length, 10);
@@ -46,6 +49,10 @@ function runTests() {
     for (var i = 0; i < a.length; i++)
         assertEq(a[i], i*2);
 
+    a.forEach(function(val, i) {
+        assertEq(val, i*2);
+        assertEq(arguments[2], a);
+    });
 
     // Range.
     assertThrows(function() a[i] = 5);
@@ -55,7 +62,7 @@ function runTests() {
     // constructor takes initial value
     var b = new A(a);
     for (var i = 0; i < a.length; i++)
-        assertEq(a[i], i*2);
+        assertEq(b[i], i*2);
 
     var b = new A([0, 1, 0, 1, 0, 1, 0, 1, 0, 1]);
     for (var i = 0; i < b.length; i++)
@@ -94,8 +101,80 @@ function runTests() {
     var as = new AllSprites();
     assertEq(as.length, 65536);
 
+    // test methods
+    var c = new A();
+    c.fill(3);
+    for (var i = 0; i < c.length; i++)
+        assertEq(c[i], 3);
 
-    as.foo = "bar";
+    assertThrows(function() c.update([3.14, 4.52, 5]));
+    //assertThrows(function() c.update([3000, 0, 1, 1, 1, 1, 1, 1, 1, 1]));
+
+    assertThrows(function() Vec3.prototype.fill.call(c, 2));
+
+    var updatingPos = new Vec3();
+    updatingPos.update([5, 3, 1]);
+    assertEq(updatingPos[0], 5);
+    assertEq(updatingPos[1], 3);
+    assertEq(updatingPos[2], 1);
+
+    var d = A.repeat(10);
+    for (var i = 0; i < d.length; i++)
+        assertEq(d[i], 10);
+
+    assertThrows(function() ArrayType.prototype.repeat.call(d, 2));
+
+    var MA = new ArrayType(uint32, 5);
+    var ma = new MA([1, 2, 3, 4, 5]);
+
+    var mb = ma.subarray(2);
+    assertEq(mb.length, 3);
+    assertEq(mb[0], 3);
+    assertEq(mb[1], 4);
+    assertEq(mb[2], 5);
+
+    assertThrows(function() ma.subarray());
+    assertThrows(function() ma.subarray(2.14));
+    assertThrows(function() ma.subarray({}));
+    assertThrows(function() ma.subarray(2, []));
+
+    // check similarity even though mb's ArrayType
+    // is not script accessible
+    var Similar = new ArrayType(uint32, 3);
+    var sim = new Similar();
+    sim.update(mb);
+    assertEq(sim[0], 3);
+    assertEq(sim[1], 4);
+    assertEq(sim[2], 5);
+
+    var range = ma.subarray(0, 3);
+    assertEq(range.length, 3);
+    assertEq(range[0], 1);
+    assertEq(range[1], 2);
+    assertEq(range[2], 3);
+
+    assertEq(ma.subarray(ma.length).length, 0);
+    assertEq(ma.subarray(ma.length, ma.length-1).length, 0);
+
+    var rangeNeg = ma.subarray(-2);
+    assertEq(rangeNeg.length, 2);
+    assertEq(rangeNeg[0], 4);
+    assertEq(rangeNeg[1], 5);
+
+    var rangeNeg = ma.subarray(-5, -3);
+    assertEq(rangeNeg.length, 2);
+    assertEq(rangeNeg[0], 1);
+    assertEq(rangeNeg[1], 2);
+
+    assertEq(ma.subarray(-2, -3).length, 0);
+    assertEq(ma.subarray(-6).length, ma.length);
+
+    var modifyOriginal = ma.subarray(2);
+    modifyOriginal[0] = 42;
+    assertEq(ma[2], 42);
+
+    ma[4] = 97;
+    assertEq(modifyOriginal[2], 97);
 
     var indexPropDesc = Object.getOwnPropertyDescriptor(as, '0');
     assertEq(typeof indexPropDesc == "undefined", false);
