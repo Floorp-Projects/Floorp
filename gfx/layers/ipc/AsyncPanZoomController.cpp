ddd57002747f75179ea40bbb2c99cbe74e4f55a3
Ryan VanderMeulen <ryanvm@gmail.com>
2013-07-25 22:55:25 -0400

    Backed out changeset 9a867e5a4ef5 (bug 895417) for suspicion of causing OSX asserts.

--- gfx/layers/ipc/AsyncPanZoomController.cpp
+++ gfx/layers/ipc/AsyncPanZoomController.cpp
@@ -272,22 +272,20 @@ AsyncPanZoomController::ReceiveInputEvent(const nsInputEvent& aEvent,
     for (uint32_t i = 0; i < touches.Length(); ++i) {
       nsIDOMTouch* touch = touches[i];
       if (touch) {
-        CSSPoint refCSSPoint = WidgetSpaceToCompensatedViewportSpace(
+        CSSPoint refPoint = WidgetSpaceToCompensatedViewportSpace(
           ScreenPoint::FromUnknownPoint(gfx::Point(
             touch->mRefPoint.x, touch->mRefPoint.y)),
           currentResolution);
-        LayoutDevicePoint refPoint = refCSSPoint * mFrameMetrics.mDevPixelsPerCSSPixel;
         touch->mRefPoint = nsIntPoint(refPoint.x, refPoint.y);
       }
     }
     break;
   }
   default: {
-    CSSPoint refCSSPoint = WidgetSpaceToCompensatedViewportSpace(
+    CSSPoint refPoint = WidgetSpaceToCompensatedViewportSpace(
       ScreenPoint::FromUnknownPoint(gfx::Point(
         aOutEvent->refPoint.x, aOutEvent->refPoint.y)),
       currentResolution);
-    LayoutDevicePoint refPoint = refCSSPoint * mFrameMetrics.mDevPixelsPerCSSPixel;
     aOutEvent->refPoint = nsIntPoint(refPoint.x, refPoint.y);
     break;
   }
