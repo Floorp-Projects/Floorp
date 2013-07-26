4bf6035d55be2480d0d8c3f90ec30aa8b98e79c9
Ryan VanderMeulen <ryanvm@gmail.com>
2013-07-26 14:08:29 -0400

    Backed out changesets f2862e93b710, 4fe81f58dbef, and f8f855f0b7dc (bug 896353) for mochitest-1 asserts.

--- content/media/webaudio/MediaStreamAudioDestinationNode.cpp
+++ content/media/webaudio/MediaStreamAudioDestinationNode.cpp
@@ -5,7 +5,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "MediaStreamAudioDestinationNode.h"
-#include "nsIDocument.h"
 #include "mozilla/dom/AudioStreamTrack.h"
 #include "mozilla/dom/MediaStreamAudioDestinationNodeBinding.h"
 #include "AudioNodeEngine.h"
@@ -74,11 +73,6 @@ MediaStreamAudioDestinationNode::MediaStreamAudioDestinationNode(AudioContext* a
   MediaStreamDestinationEngine* engine = new MediaStreamDestinationEngine(this, tus);
   mStream = aContext->Graph()->CreateAudioNodeStream(engine, MediaStreamGraph::INTERNAL_STREAM);
   mPort = tus->AllocateInputPort(mStream, 0);
-
-  nsIDocument* doc = aContext->GetParentObject()->GetExtantDoc();
-  if (doc) {
-    mDOMStream->CombineWithPrincipal(doc->NodePrincipal());
-  }
 }
 
 void
