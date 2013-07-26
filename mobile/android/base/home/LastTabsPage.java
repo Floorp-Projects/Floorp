ac948da5bc75c73e58a1813813d51b50d183248f
Sriram Ramasubramanian <sriram@mozilla.com>
2013-07-26 15:48:18 -0700

    Bug 898501: LOADER_ID OCD in LastTabsPage. [r=lucasr]

--- mobile/android/base/home/LastTabsPage.java
+++ mobile/android/base/home/LastTabsPage.java
@@ -37,7 +37,7 @@ public class LastTabsPage extends HomeFragment {
     private static final String LOGTAG = "GeckoLastTabsPage";
 
     // Cursor loader ID for the session parser
-    private static final int LAST_TABS_LOADER_ID = 0;
+    private static final int LOADER_ID_LAST_TABS = 0;
 
     // Adapter for the list of search results
     private LastTabsAdapter mAdapter;
@@ -135,7 +135,7 @@ public class LastTabsPage extends HomeFragment {
 
     @Override
     protected void load() {
-        getLoaderManager().initLoader(LAST_TABS_LOADER_ID, null, mCursorLoaderCallbacks);
+        getLoaderManager().initLoader(LOADER_ID_LAST_TABS, null, mCursorLoaderCallbacks);
     }
 
     private void openAllTabs() {
