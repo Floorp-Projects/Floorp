5e41a79af5da507d33b9528074d09cc3d33f5113
Paul Adenot <paul@paul.cx>
2013-07-26 18:46:32 +0200

    Bug 894941 - Import SSE-optimized routines for the speex resampler. r=ehsan

--- media/libspeex_resampler/update.sh
+++ media/libspeex_resampler/update.sh
@@ -5,9 +5,10 @@
 # Usage: ./update.sh <libspeex_src_directory>
 #
 # Copies the needed files from a directory containing the original
-# libspeex sources that we need for HTML5 media playback rate change.
+# libspeex sources.
 
 cp $1/libspeex/resample.c src
+cp $1/libspeex/resample_sse.h src
 cp $1/libspeex/arch.h src
 cp $1/libspeex/stack_alloc.h src
 cp $1/libspeex/fixed_generic.h src
