eba33bf28a9e92e80a9467f07119de81aa0a7771
Guilherme GonÃ§alves <ggp@mozilla.com>
2013-07-26 16:12:12 -0400

    Bug 884921 - navigator.geolocation should never be null. r=dougt

--- dom/base/Navigator.cpp
+++ dom/base/Navigator.cpp
@@ -961,10 +961,6 @@ Navigator::GetDeviceStorages(const nsAString& aType,
 Geolocation*
 Navigator::GetGeolocation(ErrorResult& aRv)
 {
-  if (!Preferences::GetBool("geo.enabled", true)) {
-    return nullptr;
-  }
-
   if (mGeolocation) {
     return mGeolocation;
   }
