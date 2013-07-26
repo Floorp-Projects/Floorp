e933d57c93efa60806c357cbaf41cb655ff4f7c9
Nicholas Nethercote <nnethercote@mozilla.com>
2013-07-25 23:40:57 -0700

    Bug 898263 (part 8) - Move the Handle* and Rooted* types out of jsprvtd.h.  r=jorendorff.
    --HG--
    extra : rebase_source : 599e2408848fc7973e83e6661f24292e1191e3b8

--- js/src/jsprvtd.h
+++ js/src/jsprvtd.h
@@ -15,13 +15,7 @@
 
 typedef uintptr_t   jsatomid;
 
-/* String typedefs. */
-class JSAtom;
-
 namespace js {
-
-class PropertyName;
-class Shape;
 
 typedef JSNative             Native;
 typedef JSParallelNative     ParallelNative;
@@ -29,21 +23,6 @@ typedef JSThreadSafeNative   ThreadSafeNative;
 typedef JSPropertyOp         PropertyOp;
 typedef JSStrictPropertyOp   StrictPropertyOp;
 typedef JSPropertyDescriptor PropertyDescriptor;
-
-namespace types { struct TypeObject; }
-
-typedef JS::Handle<Shape*>             HandleShape;
-typedef JS::Handle<types::TypeObject*> HandleTypeObject;
-typedef JS::Handle<JSAtom*>            HandleAtom;
-typedef JS::Handle<PropertyName*>      HandlePropertyName;
-
-typedef JS::MutableHandle<Shape*>      MutableHandleShape;
-typedef JS::MutableHandle<JSAtom*>     MutableHandleAtom;
-
-typedef JS::Rooted<Shape*>             RootedShape;
-typedef JS::Rooted<types::TypeObject*> RootedTypeObject;
-typedef JS::Rooted<JSAtom*>            RootedAtom;
-typedef JS::Rooted<PropertyName*>      RootedPropertyName;
 
 enum XDRMode {
     XDR_ENCODE,
