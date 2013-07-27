9368a4d62093290e533549cb95415206a0109507
Bobby Holley <bobbyholley@gmail.com>
2013-07-26 18:56:51 -0700

    Bug 889911 - Replace mozJSLoaderErrorReporter with SystemErrorReporter and remove the former. r=mrbkap

--- js/xpconnect/loader/mozJSSubScriptLoader.cpp
+++ js/xpconnect/loader/mozJSSubScriptLoader.cpp
@@ -50,10 +50,6 @@ using namespace JS;
 #define LOAD_ERROR_NOSPEC "Failed to get URI spec.  This is bad."
 #define LOAD_ERROR_CONTENTTOOBIG "ContentLength is too large"
 
-// We just use the same reporter as the component loader
-extern void
-mozJSLoaderErrorReporter(JSContext *cx, const char *message, JSErrorReport *rep);
-
 mozJSSubScriptLoader::mozJSSubScriptLoader() : mSystemPrincipal(nullptr)
 {
     // Force construction of the JS component loader.  We may need it later.
@@ -123,7 +119,7 @@ mozJSSubScriptLoader::ReadScript(nsIURI *uri, JSContext *cx, JSObject *targetObj
 
     /* set our own error reporter so we can report any bad things as catchable
      * exceptions, including the source/line number */
-    er = JS_SetErrorReporter(cx, mozJSLoaderErrorReporter);
+    er = JS_SetErrorReporter(cx, xpc::SystemErrorReporter);
 
     JS::CompileOptions options(cx);
     options.setPrincipals(nsJSPrincipals::get(principal))
