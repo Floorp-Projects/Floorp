e6b9e7aacd0d7dd123ec92eb65bf5cd35c26e2f3
Brian Nicholson <bnicholson@mozilla.com>
2013-07-29 16:32:01 -0400

    Bug 895702 - Remove unnecessary bytes length check. r=rnewman

--- mobile/android/base/home/HomeFragment.java
+++ mobile/android/base/home/HomeFragment.java
@@ -120,7 +120,7 @@ abstract class HomeFragment extends Fragment {
 
                 // FIXME: bug 897772
                 Bitmap bitmap = null;
-                if (info.favicon != null && info.favicon.length > 0) {
+                if (info.favicon != null) {
                     bitmap = BitmapUtils.decodeByteArray(info.favicon);
                 }
 
