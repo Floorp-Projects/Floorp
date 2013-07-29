08444399f6f71b2b91e502eda02650e0dbcd1e9d
James Kitchener <jkitch.bug@gmail.com>
2013-07-29 12:36:23 -0400

    Bug 827713 - Part 1: Changes to mmultiscripts to support merging of script elements. r=karlt

--- layout/mathml/nsMathMLmmultiscriptsFrame.h
+++ layout/mathml/nsMathMLmmultiscriptsFrame.h
@@ -12,6 +12,9 @@
 
 //
 // <mmultiscripts> -- attach prescripts and tensor indices to a base 
+// <msub> -- attach a subscript to a base
+// <msubsup> -- attach a subscript-superscript pair to a base
+// <msup> -- attach a superscript to a base
 //
 
 class nsMathMLmmultiscriptsFrame : public nsMathMLContainerFrame {
@@ -28,16 +31,21 @@ public:
         bool                 aPlaceOrigin,
         nsHTMLReflowMetrics& aDesiredSize) MOZ_OVERRIDE;
 
+  static nsresult
+  PlaceMultiScript(nsPresContext*      aPresContext,
+                    nsRenderingContext& aRenderingContext,
+                    bool                 aPlaceOrigin,
+                    nsHTMLReflowMetrics& aDesiredSize,
+                    nsMathMLContainerFrame* aForFrame,
+                    nscoord              aUserSubScriptShift,
+                    nscoord              aUserSupScriptShift,
+                    nscoord              aScriptSpace);
+
 protected:
   nsMathMLmmultiscriptsFrame(nsStyleContext* aContext) : nsMathMLContainerFrame(aContext) {}
   virtual ~nsMathMLmmultiscriptsFrame();
   
-private:
-  nscoord mSubScriptShift;
-  nscoord mSupScriptShift;
 
-  void
-  ProcessAttributes();
 };
 
 #endif /* nsMathMLmmultiscriptsFrame_h___ */
