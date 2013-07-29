df6b6a7e333bbaaaa3086d9d3885869810e49cf7
Margaret Leibovic <margaret.leibovic@gmail.com>
2013-07-29 08:40:40 -0700

    Backout 1491af6fc2ae

--- mobile/android/base/home/VisitedPage.java
+++ mobile/android/base/home/VisitedPage.java
@@ -23,14 +23,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 /**
  * Fragment that displays frecency search results in a ListView.
  */
-public class MostVisitedPage extends HomeFragment {
+public class VisitedPage extends HomeFragment {
     // Logging tag name
-    private static final String LOGTAG = "GeckoMostVisitedPage";
+    private static final String LOGTAG = "GeckoVisitedPage";
 
     // Cursor loader ID for search query
     private static final int FRECENCY_LOADER_ID = 0;
@@ -47,17 +46,16 @@ public class MostVisitedPage extends HomeFragment {
     // Empty message view
     private View mEmptyMessage;
 
+    // Buttons container
+    private View mButtonsContainer;
+
     // Callbacks used for the search and favicon cursor loaders
     private CursorLoaderCallbacks mCursorLoaderCallbacks;
 
     // On URL open listener
     private OnUrlOpenListener mUrlOpenListener;
 
-    public static MostVisitedPage newInstance() {
-        return new MostVisitedPage();
-    }
-
-    public MostVisitedPage() {
+    public VisitedPage() {
         mUrlOpenListener = null;
     }
 
@@ -82,16 +80,14 @@ public class MostVisitedPage extends HomeFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_most_visited_page, container, false);
+        return inflater.inflate(R.layout.home_visited_page, container, false);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        final TextView title = (TextView) view.findViewById(R.id.title);
-        title.setText(R.string.home_most_visited_title);
+        super.onViewCreated(view, savedInstanceState);
 
-        mEmptyMessage = view.findViewById(R.id.empty_message);
-        mList = (HomeListView) view.findViewById(R.id.list);
+        mList = (HomeListView) view.findViewById(R.id.visited_list);
 
         mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -107,12 +103,32 @@ public class MostVisitedPage extends HomeFragment {
         });
 
         registerForContextMenu(mList);
+
+        mEmptyMessage = view.findViewById(R.id.empty_message);
+        mButtonsContainer = view.findViewById(R.id.buttons_container);
+
+        final View historyButton = view.findViewById(R.id.history_button);
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showHistoryPage();
+            }
+        });
+
+        final View tabsButton = view.findViewById(R.id.tabs_button);
+        tabsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTabsPage();
+            }
+        });
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         mList = null;
+        mButtonsContainer = null;
         mEmptyMessage = null;
     }
 
@@ -132,6 +148,16 @@ public class MostVisitedPage extends HomeFragment {
     @Override
     protected void load() {
         getLoaderManager().initLoader(FRECENCY_LOADER_ID, null, mCursorLoaderCallbacks);
+    }
+
+    private void showHistoryPage() {
+        final HistoryPage historyPage = HistoryPage.newInstance();
+        showSubPage(historyPage);
+    }
+
+    private void showTabsPage() {
+        final LastTabsPage lastTabsPage = LastTabsPage.newInstance();
+        showSubPage(lastTabsPage);
     }
 
     private static class FrecencyCursorLoader extends SimpleCursorLoader {
@@ -188,6 +214,9 @@ public class MostVisitedPage extends HomeFragment {
                 // Only set empty view once cursor is loaded to avoid
                 // flashing the empty message before loading.
                 mList.setEmptyView(mEmptyMessage);
+
+                final int buttonsVisibility = (c.getCount() == 0 ? View.GONE : View.VISIBLE);
+                mButtonsContainer.setVisibility(buttonsVisibility);
 
                 mAdapter.swapCursor(c);
 
