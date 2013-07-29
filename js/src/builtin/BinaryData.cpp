475b6ff1d068ab37b233fa96b1032a3e34c48f85
Nikhil Marathe <nsm.nikhil@gmail.com>
2013-07-29 10:22:11 -0700

    Bug 898494 - Fix lack of rooting in Binary Data. r=nmatsakis
    --HG--
    extra : rebase_source : d81cb1c34cb6767966ccc4006ed504c7d0e935ac

--- js/src/builtin/BinaryData.cpp
+++ js/src/builtin/BinaryData.cpp
@@ -58,7 +58,7 @@ DataThrowError(JSContext *cx, unsigned argc, Value *vp)
 }
 
 static void
-ReportTypeError(JSContext *cx, Value fromValue, const char *toType)
+ReportTypeError(JSContext *cx, HandleValue fromValue, const char *toType)
 {
     char *valueStr = JS_EncodeString(cx, JS_ValueToString(cx, fromValue));
     JS_ReportErrorNumber(cx, js_GetErrorMessage, NULL, JSMSG_CANT_CONVERT_TO,
@@ -67,7 +67,7 @@ ReportTypeError(JSContext *cx, Value fromValue, const char *toType)
 }
 
 static void
-ReportTypeError(JSContext *cx, Value fromValue, JSString *toType)
+ReportTypeError(JSContext *cx, HandleValue fromValue, JSString *toType)
 {
     const char *fnName = JS_EncodeString(cx, toType);
     ReportTypeError(cx, fromValue, fnName);
@@ -78,7 +78,7 @@ ReportTypeError(JSContext *cx, Value fromValue, JSString *toType)
 // called.
 // So yes this call is with side effects.
 static bool
-ReportTypeError(JSContext *cx, Value fromValue, HandleObject exemplar)
+ReportTypeError(JSContext *cx, HandleValue fromValue, HandleObject exemplar)
 {
     RootedValue v(cx, ObjectValue(*exemplar));
     ReportTypeError(cx, fromValue, ToString<CanGC>(cx, v));
@@ -677,10 +677,11 @@ ArrayType::create(JSContext *cx, HandleObject arrayTypeGlobal,
                                   NULL, NULL, JSPROP_READONLY | JSPROP_PERMANENT))
         return NULL;
 
-    obj->setFixedSlot(SLOT_MEMSIZE,
-                      Int32Value(::GetMemSize(cx, elementType) * length));
+    RootedValue slotMemsizeVal(cx, Int32Value(::GetMemSize(cx, elementType) * length));
+    obj->setFixedSlot(SLOT_MEMSIZE, slotMemsizeVal);
 
-    obj->setFixedSlot(SLOT_ALIGN, Int32Value(::GetAlign(cx, elementType)));
+    RootedValue slotAlignVal(cx, Int32Value(::GetAlign(cx, elementType)));
+    obj->setFixedSlot(SLOT_ALIGN, slotAlignVal);
 
     RootedObject prototypeObj(cx,
         SetupAndGetPrototypeObjectForComplexTypeInstance(cx, arrayTypeGlobal));
@@ -769,7 +770,8 @@ DataInstanceUpdate(JSContext *cx, unsigned argc, Value *vp)
 
     RootedObject thisObj(cx, args.thisv().toObjectOrNull());
     if (!IsBlock(thisObj)) {
-        ReportTypeError(cx, ObjectValue(*thisObj), "BinaryData block");
+        RootedValue thisObjVal(cx, ObjectValue(*thisObj));
+        ReportTypeError(cx, thisObjVal, "BinaryData block");
         return false;
     }
 
@@ -825,8 +827,8 @@ ArrayType::repeat(JSContext *cx, unsigned int argc, Value *vp)
 
     RootedObject thisObj(cx, args.thisv().toObjectOrNull());
     if (!IsArrayType(thisObj)) {
-        JSString *valueStr = JS_ValueToString(cx, args.thisv());
         char *valueChars = const_cast<char*>("(unknown type)");
+        RootedString valueStr(cx, JS_ValueToString(cx, args.thisv()));
         if (valueStr)
             valueChars = JS_EncodeString(cx, valueStr);
         JS_ReportErrorNumber(cx, js_GetErrorMessage, NULL, JSMSG_INCOMPATIBLE_PROTO, "ArrayType", "repeat", valueChars);
@@ -899,7 +901,7 @@ BinaryArray::createEmpty(JSContext *cx, HandleObject type)
 JSObject *
 BinaryArray::create(JSContext *cx, HandleObject type)
 {
-    JSObject *obj = createEmpty(cx, type);
+    RootedObject obj(cx, createEmpty(cx, type));
     if (!obj)
         return NULL;
 
@@ -915,7 +917,7 @@ BinaryArray::create(JSContext *cx, HandleObject type)
 JSObject *
 BinaryArray::create(JSContext *cx, HandleObject type, HandleValue initial)
 {
-    JSObject *obj = create(cx, type);
+    RootedObject obj(cx, create(cx, type));
     if (!obj)
         return NULL;
 
@@ -931,7 +933,7 @@ BinaryArray::create(JSContext *cx, HandleObject type,
                     HandleObject owner, size_t offset)
 {
     JS_ASSERT(IsBlock(owner));
-    JSObject *obj = createEmpty(cx, type);
+    RootedObject obj(cx, createEmpty(cx, type));
     if (!obj)
         return NULL;
 
@@ -1034,7 +1036,8 @@ JSBool BinaryArray::subarray(JSContext *cx, unsigned int argc, Value *vp)
 
     RootedObject thisObj(cx, &args.thisv().toObject());
     if (!IsBinaryArray(thisObj)) {
-        ReportTypeError(cx, ObjectValue(*thisObj), "binary array");
+        RootedValue thisObjVal(cx, ObjectValue(*thisObj));
+        ReportTypeError(cx, thisObjVal, "binary array");
         return false;
     }
 
@@ -1104,7 +1107,8 @@ BinaryArray::fill(JSContext *cx, unsigned int argc, Value *vp)
 
     RootedObject thisObj(cx, args.thisv().toObjectOrNull());
     if (!IsBinaryArray(thisObj)) {
-        ReportTypeError(cx, ObjectValue(*thisObj), "binary array");
+        RootedValue thisObjVal(cx, ObjectValue(*thisObj));
+        ReportTypeError(cx, thisObjVal, "binary array");
         return false;
     }
 
@@ -1114,7 +1118,8 @@ BinaryArray::fill(JSContext *cx, unsigned int argc, Value *vp)
     RootedObject type(cx, GetType(thisObj));
     RootedObject funArrayType(cx, funArrayTypeVal.toObjectOrNull());
     if (!IsSameBinaryDataType(cx, funArrayType, type)) {
-        ReportTypeError(cx, ObjectValue(*thisObj), funArrayType);
+        RootedValue thisObjVal(cx, ObjectValue(*thisObj));
+        ReportTypeError(cx, thisObjVal, funArrayType);
         return false;
     }
 
@@ -1636,7 +1641,8 @@ StructType::create(JSContext *cx, HandleObject structTypeGlobal,
         return NULL;
 
     if (!StructType::layout(cx, obj, fields)) {
-        ReportTypeError(cx, ObjectValue(*fields), "StructType field specifier");
+        RootedValue fieldsVal(cx, ObjectValue(*fields));
+        ReportTypeError(cx, fieldsVal, "StructType field specifier");
         return NULL;
     }
 
@@ -1732,9 +1738,9 @@ StructType::toString(JSContext *cx, unsigned int argc, Value *vp)
         contents.append(IdToString(cx, it->name));
         contents.append(": ");
 
-        Value fieldStringVal;
+        RootedValue fieldStringVal(cx);
         if (!JS_CallFunctionName(cx, it->type,
-                                 "toString", 0, NULL, &fieldStringVal))
+                                 "toString", 0, NULL, fieldStringVal.address()))
             return false;
 
         contents.append(fieldStringVal.toString());
@@ -1769,7 +1775,7 @@ BinaryStruct::createEmpty(JSContext *cx, HandleObject type)
 JSObject *
 BinaryStruct::create(JSContext *cx, HandleObject type)
 {
-    JSObject *obj = createEmpty(cx, type);
+    RootedObject obj(cx, createEmpty(cx, type));
     if (!obj)
         return NULL;
 
@@ -1787,7 +1793,7 @@ BinaryStruct::create(JSContext *cx, HandleObject type,
                      HandleObject owner, size_t offset)
 {
     JS_ASSERT(IsBlock(owner));
-    JSObject *obj = createEmpty(cx, type);
+    RootedObject obj(cx, createEmpty(cx, type));
     if (!obj)
         return NULL;
 
@@ -1808,7 +1814,7 @@ BinaryStruct::construct(JSContext *cx, unsigned int argc, Value *vp)
         return false;
     }
 
-    JSObject *obj = create(cx, callee);
+    RootedObject obj(cx, create(cx, callee));
 
     if (obj)
         args.rval().setObject(*obj);
@@ -2235,7 +2241,7 @@ js_InitBinaryDataClasses(JSContext *cx, HandleObject obj)
     JS_ASSERT(obj->is<GlobalObject>());
     Rooted<GlobalObject *> global(cx, &obj->as<GlobalObject>());
 
-    JSObject *funProto = JS_GetFunctionPrototype(cx, global);
+    RootedObject funProto(cx, JS_GetFunctionPrototype(cx, global));
 #define BINARYDATA_NUMERIC_DEFINE(constant_, type_)\
     do {\
         RootedObject numFun(cx, JS_DefineObject(cx, global, #type_,\
