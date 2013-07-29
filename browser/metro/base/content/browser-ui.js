ce1ba8d15173a0bbea85af39022a492857a899a2
Jonathan Wilde <jwilde@jwilde.me>
2013-07-29 14:35:17 -0700

    Bug 897830 - Part 1 - Rename "flyout" to "flyoutpanel" for clarity. r=timA

--- browser/metro/base/content/browser-ui.js
+++ browser/metro/base/content/browser-ui.js
@@ -1036,7 +1036,7 @@ var BrowserUI = {
       case "cmd_remoteTabs":
 #ifdef MOZ_SERVICES_SYNC
         if (Weave.Status.checkSetup() == Weave.CLIENT_NOT_CONFIGURED) {
-          FlyoutPanelsUI.show('SyncFlyout');
+          FlyoutPanelsUI.show('SyncFlyoutPanel');
         } else {
           PanelUI.show("remotetabs-container");
         }
@@ -1506,17 +1506,17 @@ var SettingsCharm = {
     // Options
     this.addEntry({
         label: Strings.browser.GetStringFromName("optionsCharm"),
-        onselected: function() FlyoutPanelsUI.show('PrefsFlyout')
+        onselected: function() FlyoutPanelsUI.show('PrefsFlyoutPanel')
     });
     // Sync
     this.addEntry({
         label: Strings.browser.GetStringFromName("syncCharm"),
-        onselected: function() FlyoutPanelsUI.show('SyncFlyout')
+        onselected: function() FlyoutPanelsUI.show('SyncFlyoutPanel')
     });
     // About
     this.addEntry({
         label: Strings.browser.GetStringFromName("aboutCharm1"),
-        onselected: function() FlyoutPanelsUI.show('AboutFlyout')
+        onselected: function() FlyoutPanelsUI.show('AboutFlyoutPanel')
     });
     // Help
     this.addEntry({
