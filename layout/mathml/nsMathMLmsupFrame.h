ab9671a83e2ffd8679b5986513657f9d4c427ca1
Ryan VanderMeulen <ryanvm@gmail.com>
2013-07-29 15:19:34 -0400

    Backed out 6 changesets (bug 827713) for reftest failures.
    Backed out changeset 014cc3de08fb (bug 827713)
    Backed out changeset 4113172193aa (bug 827713)
    Backed out changeset 27a5c8dd5ff7 (bug 827713)
    Backed out changeset 6448c7e05f11 (bug 827713)
    Backed out changeset b67a72618c66 (bug 827713)
    Backed out changeset 1279664e0d41 (bug 827713)

---
+++ layout/mathml/nsMathMLmsupFrame.h
@@ -0,0 +1,45 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef nsMathMLmsupFrame_h___
+#define nsMathMLmsupFrame_h___
+
+#include "mozilla/Attributes.h"
+#include "nsCOMPtr.h"
+#include "nsMathMLContainerFrame.h"
+
+//
+// <msup> -- attach a superscript to a base
+//
+
+class nsMathMLmsupFrame : public nsMathMLContainerFrame {
+public:
+  NS_DECL_FRAMEARENA_HELPERS
+
+  friend nsIFrame* NS_NewMathMLmsupFrame(nsIPresShell* aPresShell, nsStyleContext* aContext);
+
+  NS_IMETHOD
+  TransmitAutomaticData() MOZ_OVERRIDE;
+
+  virtual nsresult
+  Place(nsRenderingContext& aRenderingContext,
+        bool                 aPlaceOrigin,
+        nsHTMLReflowMetrics& aDesiredSize) MOZ_OVERRIDE;
+
+  static nsresult
+  PlaceSuperScript (nsPresContext*      aPresContext,
+                    nsRenderingContext& aRenderingContext,
+                    bool                 aPlaceOrigin,
+                    nsHTMLReflowMetrics& aDesiredSize,
+                    nsMathMLContainerFrame* aForFrame,
+                    nscoord              aUserSupScriptShift,
+                    nscoord              aScriptSpace);
+
+protected:
+  nsMathMLmsupFrame(nsStyleContext* aContext) : nsMathMLContainerFrame(aContext) {}
+  virtual ~nsMathMLmsupFrame();
+};
+
+#endif /* nsMathMLmsupFrame_h___ */
