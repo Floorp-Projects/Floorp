088093d5d41f268f541167ee9131df7cce121056
Gina Yeh <gyeh@mozilla.com>
2013-07-29 17:32:34 +0800

    Bug 842948 - Patch 5: Implement UpdateNotification, r=echou

--- dom/bluetooth/linux/BluetoothDBusService.h
+++ dom/bluetooth/linux/BluetoothDBusService.h
@@ -163,6 +163,23 @@ public:
                   const nsAString& aMessage) MOZ_OVERRIDE;
 
 private:
+  /**
+   * For DBus Control method of "UpdateNotification", event id should be
+   * specified as following:
+   * (Please see specification of AVRCP 1.3, Table 5.28 for more details.)
+   */
+  enum ControlEventId {
+    EVENT_PLAYBACK_STATUS_CHANGED            = 0x01,
+    EVENT_TRACK_CHANGED                      = 0x02,
+    EVENT_TRACK_REACHED_END                  = 0x03,
+    EVENT_TRACK_REACHED_START                = 0x04,
+    EVENT_PLAYBACK_POS_CHANGED               = 0x05,
+    EVENT_BATT_STATUS_CHANGED                = 0x06,
+    EVENT_SYSTEM_STATUS_CHANGED              = 0x07,
+    EVENT_PLAYER_APPLICATION_SETTING_CHANGED = 0x08,
+    EVENT_UNKNOWN
+  };
+
   nsresult SendGetPropertyMessage(const nsAString& aPath,
                                   const char* aInterface,
                                   void (*aCB)(DBusMessage *, void *),
@@ -174,6 +191,8 @@ private:
   nsresult SendSetPropertyMessage(const char* aInterface,
                                   const BluetoothNamedValue& aValue,
                                   BluetoothReplyRunnable* aRunnable);
+
+  void UpdateNotification(ControlEventId aEventId, uint64_t aData);
 
   void DisconnectAllAcls(const nsAString& aAdapterPath);
 
