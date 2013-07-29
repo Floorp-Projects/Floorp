4396b363003d12a61650999a34b2857d385528fb
Sriram Ramasubramanian <sriram@mozilla.com>
2013-07-29 15:05:11 -0700

    Bug 898501: HomeFragment should handle FaviconsLoader. [r=lucasr]

--- mobile/android/base/home/HomeFragment.java
+++ mobile/android/base/home/HomeFragment.java
@@ -22,8 +22,12 @@ import org.mozilla.gecko.util.UiAsyncTask;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.Loader;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -46,6 +50,9 @@ abstract class HomeFragment extends Fragment {
 
     // URL to Title replacement regex.
     private static final String REGEX_URL_TO_TITLE = "^([a-z]+://)?(www\\.)?";
+
+    // Cursor loader ID for favicons query
+    private static final int LOADER_ID_FAVICONS = 100;
 
     protected void showSubPage(Fragment subPage) {
         getActivity().getSupportFragmentManager().beginTransaction()
@@ -249,5 +256,39 @@ abstract class HomeFragment extends Fragment {
         }
 
         load();
+    }
+
+    /**
+     * Cursor loader callbacks that takes care loading favicons into memory.
+     */
+    abstract class HomeCursorLoaderCallbacks implements LoaderCallbacks<Cursor> {
+
+        // Callback for favicons loaded in memory.
+        public abstract void onFaviconsLoaded();
+
+        public void loadFavicons(Cursor cursor) {
+            FaviconsLoader.restartFromCursor(getLoaderManager(), LOADER_ID_FAVICONS, this, cursor);
+        }
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            if (id == LOADER_ID_FAVICONS) {
+                return FaviconsLoader.createInstance(getActivity(), args);
+            }
+
+            return null;
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
+            if (loader.getId() == LOADER_ID_FAVICONS) {
+                onFaviconsLoaded();
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            // Do nothing by default.
+        }
     }
 }
