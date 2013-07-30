3c2650a5ca7fffd5efa7c740af17a3f19b7b0512
Jon Coppeard <jcoppeard@mozilla.com>
2013-07-30 13:52:30 +0100

    Backout 80e1116e4599 for Android failures on a CLOSED TREE

--- js/src/ion/arm/Architecture-arm.h
+++ js/src/ion/arm/Architecture-arm.h
@@ -196,17 +196,8 @@ class FloatRegisters
 
     static const uint32_t AllMask = (1 << Total) - 1;
 
-    static const uint32_t NonVolatileMask =
-        (1 << d8) |
-        (1 << d9) |
-        (1 << d10) |
-        (1 << d11) |
-        (1 << d12) |
-        (1 << d13) |
-        (1 << d14) |
-        (1 << d15);
-
-    static const uint32_t VolatileMask = AllMask & ~NonVolatileMask;
+    static const uint32_t VolatileMask = AllMask;
+    static const uint32_t NonVolatileMask = 0;
 
     static const uint32_t WrapperMask = VolatileMask;
 
