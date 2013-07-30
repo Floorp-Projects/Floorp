23052400271e13a0a8fd548a1e10aac702ab814e
David Zbarsky <dzbarsky@gmail.com>
2013-07-30 14:55:10 -0700

    Bug 893117: Remove nsIDOMHTMLTableCellElement r=peterv

--- browser/metro/base/content/Util.js
+++ browser/metro/base/content/Util.js
@@ -228,7 +228,7 @@ let Util = {
             aElement instanceof Ci.nsIDOMHTMLLIElement ||
             aElement instanceof Ci.nsIDOMHTMLPreElement ||
             aElement instanceof Ci.nsIDOMHTMLHeadingElement ||
-            aElement instanceof Ci.nsIDOMHTMLTableCellElement ||
+            aElement instanceof HTMLTableCellElement ||
             aElement instanceof Ci.nsIDOMHTMLBodyElement);
   },
 
