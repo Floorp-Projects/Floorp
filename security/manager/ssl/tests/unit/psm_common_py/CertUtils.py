bdffff0d8b225569c1dea2ab9b18451a91553bbf
Camilo Viecco <cviecco@mozilla.com>
2013-07-30 11:18:40 -0700

    Bug 895601: Add tests to cert verification in psm. r=keeler.

---
+++ security/manager/ssl/tests/unit/psm_common_py/CertUtils.py
@@ -0,0 +1,139 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# This file requires openssl 1.0.0 at least
+
+import os
+import random
+
+def init_dsa(db_dir):
+    """
+    Initialize dsa parameters
+
+    Sets up a set of params to be reused for DSA key generation
+
+    Arguments:
+      db_dir     -- location of the temporary params for the certificate
+    """
+    dsa_key_params = db_dir + "/dsa_param.pem"
+    os.system ("openssl dsaparam -out "+ dsa_key_params + " 2048")
+
+
+def generate_cert_generic(db_dir, dest_dir, serial_num,  key_type, name,
+                          ext_text, signer_key_filename = "",
+                          signer_cert_filename = ""):
+    """
+    Generate an x509 certificate with a sha256 signature
+
+    Preconditions:
+      if dsa keys are to be generated init_dsa must have been called before.
+
+
+    Arguments:
+      db_dir     -- location of the temporary params for the certificate
+      dest_dir   -- location of the x509 cert
+      serial_num -- serial number for the cert (must be unique for each signer
+                    key)
+      key_type   -- the type of key generated: potential values: 'rsa', 'dsa',
+                    or any of the curves found by 'openssl ecparam -list_curves'
+      name       -- the common name for the cert, will match the prefix of the
+                    output cert
+      ext_text   -- the text for the x509 extensions to be added to the
+                    certificate
+      signer_key_filename -- the filename of the key from which the cert will
+                    be signed if null the cert will be self signed (think CA
+                    roots).
+      signer_cert_filename -- the certificate that will sign the certificate
+                    (used to extract signer info) it must be in DER format.
+
+    output:
+      key_name   -- the filename of the key file (PEM format)
+      cert_name  -- the filename of the output certificate (DER format)
+    """
+    key_name = db_dir + "/"+ name + ".key"
+    if key_type == 'rsa':
+      os.system ("openssl genpkey -algorithm RSA -out " + key_name +
+                 " -pkeyopt rsa_keygen_bits:2048")
+    elif key_type == 'dsa':
+      dsa_key_params = db_dir + "/dsa_param.pem"
+      os.system("openssl gendsa -out " + key_name + "  " + dsa_key_params)
+    else:
+      #assume is ec
+      os.system("openssl ecparam -out " + key_name + " -name "+ key_type +
+                " -genkey");
+    csr_name =  db_dir + "/"+ name + ".csr"
+    os.system ("openssl req -new -key " + key_name + " -days 3650" +
+               " -extensions v3_ca -batch -out " + csr_name +
+               " -utf8 -subj '/CN=" + name + "'")
+
+    extensions_filename = db_dir + "/openssl-exts"
+    f = open(extensions_filename,'w')
+    f.write(ext_text)
+    f.close()
+
+    cert_name =  dest_dir + "/"+ name + ".der"
+    if not signer_key_filename:
+        signer_key_filename = key_name;
+        os.system ("openssl x509 -req -sha256 -days 3650 -in " + csr_name +
+                   " -signkey " + signer_key_filename +
+                   " -set_serial " + str(serial_num) +
+                   " -extfile " + extensions_filename +
+                   " -outform DER -out "+ cert_name)
+    else:
+        os.system ("openssl x509 -req -sha256 -days 3650 -in " + csr_name +
+                   " -CAkey " + signer_key_filename +
+                   " -CA " + signer_cert_filename + " -CAform DER " +
+                   " -set_serial " + str(serial_num) + " -out " + cert_name +
+                   " -outform DER  -extfile " + extensions_filename)
+    return key_name, cert_name
+
+
+
+def generate_int_and_ee(db_dir, dest_dir, ca_key, ca_cert, name, int_ext_text,
+                        ee_ext_text, key_type = 'rsa'):
+    """
+    Generate an intermediate and ee signed by the generated intermediate. The
+    name of the intermediate files will be the name '.der' or '.key'. The name
+    of the end entity files with be "ee-"+ name plus the appropiate prefixes.
+    The serial number will be generated radomly so it is potentially possible
+    to have problem (but very unlikely).
+
+    Arguments:
+      db_dir     -- location of the temporary params for the certificate
+      dest_dir   -- location of the x509 cert
+      ca_key     -- The filename of the key that will be used to sign the
+                    intermediate (PEM FORMAT)
+      ca_cert    -- The filename of the cert that will be used to sign the
+                    intermediate, it MUST be the private key for the ca_key.
+                    The file must be in DER format.
+      name       -- the common name for the intermediate, will match the prefix
+                    of the output intermediate. The ee will have the name
+                    prefixed with "ee-"
+      int_ext_text  -- the text for the x509 extensions to be added to the
+                    intermediate certificate
+      ee_ext_text  -- the text for the x509 extensions to be added to the
+                    end entity certificate
+      key_type   -- the type of key generated: potential values: 'rsa', 'dsa',
+                    or any of the curves found by 'openssl ecparam -list_curves'
+
+    output:
+      int_key   -- the filename of the intermeidate key file (PEM format)
+      int_cert  -- the filename of the intermediate certificate (DER format)
+      ee_key    -- the filename of the end entity key file (PEM format)
+      ee_cert   -- the filename of the end entity certficate (DER format)
+
+    """
+    [int_key, int_cert] = generate_cert_generic(db_dir, dest_dir,
+                                                random.randint(100,40000000),
+                                                key_type, "int-" + name,
+                                                int_ext_text,
+                                                ca_key, ca_cert)
+    [ee_key, ee_cert] = generate_cert_generic(db_dir, dest_dir,
+                                              random.randint(100,40000000),
+                                              key_type,  name,
+                                              ee_ext_text, int_key, int_cert)
+
+    return int_key, int_cert, ee_key, ee_cert
+
+
