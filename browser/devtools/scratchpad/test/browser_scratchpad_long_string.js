475a9f3d9069ba6694ec075386683507248f55c8
Brandon Benvie <bbenvie@mozilla.com>
2013-07-30 16:27:44 -0700

    Bug 899218 - Scratchpad doesn't handle long strings. r=robcee

---
+++ browser/devtools/scratchpad/test/browser_scratchpad_long_string.js
@@ -0,0 +1,30 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+function test()
+{
+  waitForExplicitFinish();
+
+  gBrowser.selectedTab = gBrowser.addTab();
+  gBrowser.selectedBrowser.addEventListener("load", function onLoad() {
+    gBrowser.selectedBrowser.removeEventListener("load", onLoad, true);
+    openScratchpad(runTests);
+  }, true);
+
+  content.location = "data:text/html;charset=utf8,<p>test long string in Scratchpad</p>";
+}
+
+function runTests()
+{
+  let sp = gScratchpadWindow.Scratchpad;
+
+  sp.setText("'0'.repeat(10000)");
+
+  sp.display().then(() => {
+    is(sp.getText(), "'0'.repeat(10000)\n" +
+                     "/*\n" + "0".repeat(10000) + "\n*/",
+       "display()ing a long string works");
+    finish();
+  });
+}
