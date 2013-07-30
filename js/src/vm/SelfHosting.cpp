0d0e4ccc1eb74b342be099cff73d8bfbe06939db
Terrence Cole <terrence@mozilla.com>
2013-07-30 15:09:44 -0700

    Bug 899827 - Allow self-hosting cloning to work on nursery things; r=till

--- js/src/vm/SelfHosting.cpp
+++ js/src/vm/SelfHosting.cpp
@@ -830,6 +830,21 @@ CloneProperties(JSContext *cx, HandleObject obj, HandleObject clone, CloneMemory
     return true;
 }
 
+static gc::AllocKind
+GetObjectAllocKindForClone(JSRuntime *rt, JSObject *obj)
+{
+    if (!gc::IsInsideNursery(rt, (void *)obj))
+        return obj->tenuredGetAllocKind();
+
+    if (obj->is<JSFunction>())
+        return obj->as<JSFunction>().getAllocKind();
+
+    gc::AllocKind kind = gc::GetGCObjectFixedSlotsKind(obj->numFixedSlots());
+    if (CanBeFinalizedInBackground(kind, obj->getClass()))
+        kind = GetBackgroundAllocKind(kind);
+    return kind;
+}
+
 static JSObject *
 CloneObject(JSContext *cx, HandleObject srcObj, CloneMemory &clonedObjects)
 {
@@ -869,7 +884,8 @@ CloneObject(JSContext *cx, HandleObject srcObj, CloneMemory &clonedObjects)
     } else {
         JS_ASSERT(srcObj->isNative());
         clone = NewObjectWithGivenProto(cx, srcObj->getClass(), NULL, cx->global(),
-                                        srcObj->tenuredGetAllocKind(), SingletonObject);
+                                        GetObjectAllocKindForClone(cx->runtime(), srcObj),
+                                        SingletonObject);
     }
     if (!clone || !clonedObjects.relookupOrAdd(p, srcObj.get(), clone.get()) ||
         !CloneProperties(cx, srcObj, clone, clonedObjects))
