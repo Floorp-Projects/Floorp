9dcb97c82c3f1513a7abec4b38f16a61ca2b9aa2
Andrea Marchesini <amarchesini@mozilla.com>
2013-07-31 10:12:46 +0200

    Bug 899546 - ENSURE_SUCCESS macro for ErrorResult, r=bz

--- dom/bindings/ErrorResult.h
+++ dom/bindings/ErrorResult.h
@@ -15,6 +15,7 @@
 
 #include "js/Value.h"
 #include "nscore.h"
+#include "nsStringGlue.h"
 #include "mozilla/Assertions.h"
 
 namespace mozilla {
@@ -144,6 +145,32 @@ private:
   // reference, not by value.
   ErrorResult(const ErrorResult&) MOZ_DELETE;
 };
+
+/******************************************************************************
+ ** Macros for checking results
+ ******************************************************************************/
+
+#define ENSURE_SUCCESS(res, ret)                                          \
+  do {                                                                    \
+    if (res.Failed()) {                                                   \
+      nsCString msg;                                                      \
+      msg.AppendPrintf("ENSURE_SUCCESS(%s, %s) failed with "              \
+                       "result 0x%X", #res, #ret, res.ErrorCode());       \
+      NS_WARNING(msg.get());                                              \
+      return ret;                                                         \
+    }                                                                     \
+  } while(0)
+
+#define ENSURE_SUCCESS_VOID(res)                                          \
+  do {                                                                    \
+    if (res.Failed()) {                                                   \
+      nsCString msg;                                                      \
+      msg.AppendPrintf("ENSURE_SUCCESS_VOID(%s) failed with "             \
+                       "result 0x%X", #res, res.ErrorCode());             \
+      NS_WARNING(msg.get());                                              \
+      return;                                                             \
+    }                                                                     \
+  } while(0)
 
 } // namespace mozilla
 
