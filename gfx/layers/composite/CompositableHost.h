02b496a1407536cd3fe253c46d3b701421b1999e
Nicholas Cameron <ncameron@mozilla.com>
2013-08-01 16:52:57 +1200

    Bug 874721. Ensure a compositable is valid before use. r=Bas

--- gfx/layers/composite/CompositableHost.h
+++ gfx/layers/composite/CompositableHost.h
@@ -185,13 +185,18 @@ public:
   virtual void Attach(Layer* aLayer, Compositor* aCompositor)
   {
     MOZ_ASSERT(aCompositor, "Compositor is required");
+    MOZ_ASSERT(!IsAttached());
     SetCompositor(aCompositor);
     SetLayer(aLayer);
+    mAttached = true;
   }
-  void Detach() {
+  void Detach()
+  {
     SetLayer(nullptr);
     SetCompositor(nullptr);
+    mAttached = false;
   }
+  bool IsAttached() { return mAttached; }
 
   virtual void Dump(FILE* aFile=nullptr,
                     const char* aPrefix="",
@@ -217,6 +222,7 @@ protected:
   Compositor* mCompositor;
   Layer* mLayer;
   RefPtr<TextureHost> mFirstTexture;
+  bool mAttached;
 };
 
 class CompositableParentManager;
