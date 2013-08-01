f26140905af6cc2496556ad43af0381e6795beff
Nicholas Cameron <ncameron@mozilla.com>
2013-08-01 17:54:10 +1200

    Minor bustage fix (misapplied patch from bug 899435). r=me

--- gfx/layers/d3d9/CompositorD3D9.h
+++ gfx/layers/d3d9/CompositorD3D9.h
@@ -110,6 +110,7 @@ public:
 private:
   // ensure mSize is up to date with respect to mWidget
   void EnsureSize();
+  void SetSamplerForFilter(gfx::Filter aFilter);
   void PaintToTarget();
   void SetMask(const EffectChain &aEffectChain, uint32_t aMaskTexture);
 
