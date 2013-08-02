57f881022c1ce37d04b2558ab9b69f11ca046dd7
Neil Deakin <neil@mozilla.com>
2013-08-02 11:37:05 -0400

    Bug 894736, pass clipboard type to data transfer so that the correct clipboard is used when pasting, r=ehsan

--- content/events/src/nsEventStateManager.cpp
+++ content/events/src/nsEventStateManager.cpp
@@ -2027,7 +2027,7 @@ nsEventStateManager::GenerateDragGesture(nsPresContext* aPresContext,
       }
 
       nsRefPtr<nsDOMDataTransfer> dataTransfer =
-        new nsDOMDataTransfer(NS_DRAGDROP_START, false);
+        new nsDOMDataTransfer(NS_DRAGDROP_START, false, -1);
       if (!dataTransfer)
         return;
 
