01de1a2f77d36e31b4f9d66a1d994a5cadb4350d
Ehsan Akhgari <ehsan@mozilla.com>
2013-08-02 12:09:54 -0400

    Bug 579517 follow-up: Remove NSPR types that crept in

--- layout/xul/base/src/nsListBoxBodyFrame.cpp
+++ layout/xul/base/src/nsListBoxBodyFrame.cpp
@@ -563,7 +563,7 @@ nsListBoxBodyFrame::GetItemAtIndex(int32_t aIndex, nsIDOMElement** aItem)
   if (aIndex < 0)
     return NS_OK;
 
-  PRInt32 itemCount = 0;
+  int32_t itemCount = 0;
   FlattenedChildIterator iter(mContent);
   for (nsIContent* child = iter.GetNextChild(); child; child = iter.GetNextChild()) {
     // we hit a list row, check if it is the one we are looking for
