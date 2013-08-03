702b026e01d900a0eef962454b4596b1b5df2e24
Joseph Myers <e_mayilme@hotmail.com>
2013-08-03 09:57:53 +0530

    Bug 897305 - Improve String.prototype.repeat() performance; r=tschneidereit

--- js/src/jit-test/tests/basic/string-repeat.js
+++ js/src/jit-test/tests/basic/string-repeat.js
@@ -6,10 +6,13 @@ assertEq("abc".repeat(1), "abc");
 assertEq("abc".repeat(2), "abcabc");
 assertEq("abc".repeat(3), "abcabcabc");
 assertEq("a".repeat(10), "aaaaaaaaaa");
+assertEq("abc".repeat(0), "");
+assertEq("abc".repeat(2.9), "abcabc");
+
 var myobj = {toString : (function () "abc"), repeat : String.prototype.repeat};
 assertEq(myobj.repeat(1), "abc");
 assertEq(myobj.repeat(2), "abcabc");
-assertEq("abc".repeat(0), "");
+
 assertThrowsInstanceOf(function() {
                          "abc".repeat(-1);
                        }, RangeError,
@@ -17,6 +20,11 @@ assertThrowsInstanceOf(function() {
 assertThrowsInstanceOf(function() {
                          "abc".repeat(Number.POSITIVE_INFINITY);
                        }, RangeError,
-                       "String.prototype.repeat should raise RangeError on positive infinity arguments");
+                       "String.prototype.repeat should raise RangeError on excedding maximum string length");
+assertThrowsInstanceOf(function() {
+                         "abc".repeat(1 << 29);
+                       }, RangeError,
+                       "String.prototype.repeat should raise RangeError on excedding maximum string length");
+
 assertEq("".repeat(5), "");
-assertEq("abc".repeat(2.0), "abcabc");
+assertEq("".repeat(1 << 29), "");
