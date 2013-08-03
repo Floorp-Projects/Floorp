5763479f23827c3ccc2a726e579eb59f548fd349
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2013-08-02 19:19:26 -0700

    Bug 867856 - Fix intermittent timeout of bug743094.js test case. r=h4writer, a=test-only

--- js/src/jit-test/tests/auto-regress/bug743094.js
+++ js/src/jit-test/tests/auto-regress/bug743094.js
@@ -1,11 +1,15 @@
-// |jit-test| error:InternalError
-
 // Binary: cache/js-dbg-32-a9a18824b4c1-linux
 // Flags: --ion-eager
 //
 
-gczeal(2)
+var i = 0;
+
+gczeal(2);
 function test() {
-  typeof (new test("1")) != 'function'
+  if (i++ > 2500)
+    return "function";
+  var res = typeof (new test("1")) != 'function';
+  return res ? "function" : "string";
 }
+
 test();
