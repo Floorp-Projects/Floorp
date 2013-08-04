ab73ff808dac3024518516b30fd9815c7a16c5f2
Nicholas Cameron <ncameron@mozilla.com>
2013-08-05 11:20:02 +1200

    backout incorrect change from warning to assertion from bug 874721. r=me

--- widget/xpwidgets/nsBaseWidget.cpp
+++ widget/xpwidgets/nsBaseWidget.cpp
@@ -966,7 +966,7 @@ void nsBaseWidget::CreateCompositor(int aWidth, int aHeight)
     return;
   }
 
-  NS_ASSERTION(false, "Failed to create an OMT compositor.");
+  NS_WARNING("Failed to create an OMT compositor.");
   DestroyCompositor();
   // Compositor child had the only reference to LayerManager and will have
   // deallocated it when being freed.
