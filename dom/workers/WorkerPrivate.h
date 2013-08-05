3fe181c01b61230860bb64ae94d4c665254d7dce
Ryan VanderMeulen <ryanvm@gmail.com>
2013-08-05 19:21:17 -0400

    Backed out changeset a4dd0dc4e354 (bug 824652) for Android M1 orange.

--- dom/workers/WorkerPrivate.h
+++ dom/workers/WorkerPrivate.h
@@ -909,6 +909,10 @@ private:
                 nsCOMPtr<nsIContentSecurityPolicy>& aCSP, bool aEvalAllowed,
                 bool aReportCSPViolations, bool aXHRParamsAllowed);
 
+  static bool
+  GetContentSecurityPolicy(JSContext *aCx,
+                           nsIContentSecurityPolicy** aCsp);
+
   bool
   Dispatch(WorkerRunnable* aEvent, EventQueue* aQueue);
 
