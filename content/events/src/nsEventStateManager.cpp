11dc8570d702f4b0aeb28e15645f1fca5ea9bd12
David Keeler <dkeeler@mozilla.com>
2013-08-06 15:00:07 -0700

    Bug 516753 - Forward content context menu events to the parent. r=felipc

--- content/events/src/nsEventStateManager.cpp
+++ content/events/src/nsEventStateManager.cpp
@@ -1585,6 +1585,7 @@ CrossProcessSafeEvent(const nsEvent& aEvent)
     case NS_MOUSE_BUTTON_DOWN:
     case NS_MOUSE_BUTTON_UP:
     case NS_MOUSE_MOVE:
+    case NS_CONTEXTMENU:
       return true;
     default:
       return false;
