122afb1e4753a4dd896ee0ed3b9bb305475dadb6
Ryan VanderMeulen <ryanvm@gmail.com>
2013-08-06 08:30:32 -0400

    Backed out changesets ede868d47878 and 5fd0b863fac7 (bug 900321) for Marionette failures.

--- dom/system/gonk/ril_worker.js
+++ dom/system/gonk/ril_worker.js
@@ -980,19 +980,17 @@ let RIL = {
         this.enterICCPUK2(options);
         break;
       case "nck":
-      case "cck": // Fall through.
-      case "spck": {
-        let type = GECKO_PERSO_LOCK_TO_CARD_PERSO_LOCK[options.lockType];
-        this.enterDepersonalization(type, options.pin, options);
+        options.type = CARD_PERSOSUBSTATE_SIM_NETWORK;
+        this.enterDepersonalization(options);
         break;
-      }
-      case "nckPuk":
-      case "cckPuk": // Fall through.
-      case "spckPuk": {
-        let type = GECKO_PERSO_LOCK_TO_CARD_PERSO_LOCK[options.lockType];
-        this.enterDepersonalization(type, options.puk, options);
+      case "cck":
+        options.type = CARD_PERSOSUBSTATE_SIM_CORPORATE;
+        this.enterDepersonalization(options);
         break;
-      }
+      case "spck":
+        options.type = CARD_PERSOSUBSTATE_SIM_SERVICE_PROVIDER;
+        this.enterDepersonalization(options);
+        break;
       default:
         options.errorMsg = "Unsupported Card Lock.";
         options.success = false;
@@ -1041,13 +1039,13 @@ let RIL = {
    *
    * @param type
    *        Integer indicating the network personalization be deactivated.
-   * @param password
-   *        String containing the password.
+   * @param pin
+   *        String containing the pin.
    */
-  enterDepersonalization: function enterDepersonalization(type, password, options) {
+  enterDepersonalization: function enterDepersonalization(options) {
     Buf.newParcel(REQUEST_ENTER_NETWORK_DEPERSONALIZATION_CODE, options);
-    Buf.writeUint32(type);
-    Buf.writeString(password);
+    Buf.writeUint32(options.type);
+    Buf.writeString(options.pin);
     Buf.sendParcel();
   },
 
