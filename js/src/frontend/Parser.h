752dffc67967cdf8e5731d8db2a81bb32e624377
Andy Wingo <wingo@igalia.com>
2013-08-06 13:01:51 +0200

    Bug 885695 - Simplify detection of legacy generators. r=Waldo, r=jorendorff

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -105,10 +105,19 @@ struct ParseContext : public GenericParseContext
 
     const unsigned  staticLevel;    /* static compilation unit nesting level */
 
-    uint32_t        parenDepth;     /* nesting depth of parens that might turn out
-                                       to be generator expressions */
-    uint32_t        yieldCount;     /* number of |yield| tokens encountered at
-                                       non-zero depth in current paren tree */
+    // Functions start off being parsed as NotGenerator.
+    // NotGenerator transitions to LegacyGenerator on parsing "yield" in JS 1.7.
+    enum GeneratorParseMode { NotGenerator, LegacyGenerator };
+    GeneratorParseMode generatorParseMode;
+
+    // lastYieldOffset stores the offset of the last yield that was parsed.
+    // NoYieldOffset is its initial value.
+    static const uint32_t NoYieldOffset = UINT32_MAX;
+    uint32_t         lastYieldOffset;
+
+    bool isGenerator() const { return generatorParseMode != NotGenerator; }
+    bool isLegacyGenerator() const { return generatorParseMode == LegacyGenerator; }
+
     Node            blockNode;      /* parse node for a block with let declarations
                                        (block with its own lexical scope)  */
   private:
@@ -192,11 +201,6 @@ struct ParseContext : public GenericParseContext
     bool generateFunctionBindings(ExclusiveContext *cx, LifoAlloc &alloc,
                                   InternalHandle<Bindings*> bindings) const;
 
-  public:
-    uint32_t         yieldOffset;   /* offset of a yield expression that might
-                                       be an error if we turn out to be inside
-                                       a generator expression.  Zero means
-                                       there isn't one. */
   private:
     ParseContext    **parserPC;     /* this points to the Parser's active pc
                                        and holds either |this| or one of
@@ -246,13 +250,12 @@ struct ParseContext : public GenericParseContext
         blockChain(prs->context),
         maybeFunction(maybeFunction),
         staticLevel(staticLevel),
-        parenDepth(0),
-        yieldCount(0),
+        generatorParseMode(NotGenerator),
+        lastYieldOffset(NoYieldOffset),
         blockNode(ParseHandler::null()),
         decls_(prs->context, prs->alloc),
         args_(prs->context),
         vars_(prs->context),
-        yieldOffset(0),
         parserPC(&prs->pc),
         oldpc(prs->pc),
         lexdeps(prs->context),
