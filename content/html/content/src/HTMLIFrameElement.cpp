14573f364d21e1cceefa43e9c92a829a165cb3c3
Bob Owen <bobowencode@gmail.com>
2013-08-06 11:01:23 +0100

    Bug 886262 - Ensure sandbox flags and, where necessary, null principal are set for child objects. r=bz

--- content/html/content/src/HTMLIFrameElement.cpp
+++ content/html/content/src/HTMLIFrameElement.cpp
@@ -222,23 +222,18 @@ HTMLIFrameElement::AfterSetAttr(int32_t aNameSpaceID, nsIAtom* aName,
                                 bool aNotify)
 {
   if (aName == nsGkAtoms::sandbox && aNameSpaceID == kNameSpaceID_None) {
-    // Parse the new value of the sandbox attribute, and if we have a docshell
-    // set its sandbox flags appropriately.
+    // If we have an nsFrameLoader, parse the new value of the sandbox
+    // attribute and apply the new sandbox flags.
     if (mFrameLoader) {
-      nsCOMPtr<nsIDocShell> docshell = mFrameLoader->GetExistingDocShell();
-
-      if (docshell) {
-        uint32_t newFlags = 0;
-        // If a nullptr aValue is passed in, we want to clear the sandbox flags
-        // which we will do by setting them to 0.
-        if (aValue) {
-          nsAutoString strValue;
-          aValue->ToString(strValue);
-          newFlags = nsContentUtils::ParseSandboxAttributeToFlags(
-            strValue);
-        }   
-        docshell->SetSandboxFlags(newFlags);
-      }
+      // If a nullptr aValue is passed in, we want to clear the sandbox flags
+      // which we will do by setting them to 0.
+      uint32_t newFlags = 0;
+      if (aValue) {
+        nsAutoString strValue;
+        aValue->ToString(strValue);
+        newFlags = nsContentUtils::ParseSandboxAttributeToFlags(strValue);
+      }   
+      mFrameLoader->ApplySandboxFlags(newFlags);
     }
   }
   return nsGenericHTMLElement::AfterSetAttr(aNameSpaceID, aName, aValue,
