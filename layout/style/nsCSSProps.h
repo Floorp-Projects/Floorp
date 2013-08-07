7e460a7f771f59aff7b719ddd21422ea4f0be473
Daniel Holbert <dholbert@cs.stanford.edu>
2013-08-07 15:32:16 -0700

    (no bug) comment-tweak, to explicitly mention a pref name instead of hinting at it. (no review) DONTBUILD because comment-only change

--- layout/style/nsCSSProps.h
+++ layout/style/nsCSSProps.h
@@ -470,7 +470,8 @@ public:
   static const int32_t kContentKTable[];
   static const int32_t kCursorKTable[];
   static const int32_t kDirectionKTable[];
-  // Not const because we modify its entries when CSS prefs change.
+  // Not const because we modify its entries when the pref
+  // "layout.css.flexbox.enabled" changes:
   static int32_t kDisplayKTable[];
   static const int32_t kElevationKTable[];
   static const int32_t kEmptyCellsKTable[];
