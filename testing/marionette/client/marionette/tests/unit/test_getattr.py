80171045f97c4167460d101bf90d8596816f8762
Jonathan Griffin <jgriffin@mozilla.com>
2013-08-06 17:34:30 -0700

    Bug 790787 - Enable more unit tests on B2G. r=mdas

--- testing/marionette/client/marionette/tests/unit/test_getattr.py
+++ testing/marionette/client/marionette/tests/unit/test_getattr.py
@@ -2,8 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import os
 from marionette_test import MarionetteTestCase
+
 
 class TestGetAttribute(MarionetteTestCase):
     def test_getAttribute(self):
@@ -11,23 +11,3 @@ class TestGetAttribute(MarionetteTestCase):
         self.marionette.navigate(test_html)
         l = self.marionette.find_element("id", "mozLink")
         self.assertEqual("mozLink", l.get_attribute("id"))
-
-class TestGetAttributeChrome(MarionetteTestCase):
-    def setUp(self):
-        MarionetteTestCase.setUp(self)
-        self.marionette.set_context("chrome")
-        self.win = self.marionette.current_window_handle
-        self.marionette.execute_script("window.open('chrome://marionette/content/test.xul', 'foo', 'chrome,centerscreen');")
-        self.marionette.switch_to_window('foo')
-        self.assertNotEqual(self.win, self.marionette.current_window_handle)
-
-    def tearDown(self):
-        self.assertNotEqual(self.win, self.marionette.current_window_handle)
-        self.marionette.execute_script("window.close();")
-        self.marionette.switch_to_window(self.win)
-        MarionetteTestCase.tearDown(self)
-
-    def test_getAttribute(self):
-        el = self.marionette.execute_script("return window.document.getElementById('textInput');")
-        self.assertEqual(el.get_attribute("id"), "textInput")
-
