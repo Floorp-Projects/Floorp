3e7aa71c4eeb8e595fa3cbe4e1ae61c03cc4b19a
Ed Morley <emorley@mozilla.com>
2013-08-07 15:50:13 -0700

    Backed out changeset 95cefee173eb (bug 897913)

--- js/xpconnect/src/xpcpublic.h
+++ js/xpconnect/src/xpcpublic.h
@@ -457,7 +457,7 @@ inline bool IsDOMProxy(JSObject *obj)
 
 typedef JSObject*
 (*DefineInterface)(JSContext *cx, JS::Handle<JSObject*> global,
-                   JS::Handle<jsid> id, bool defineOnGlobal);
+                   JS::Handle<jsid> id, bool *enabled);
 
 typedef JSObject*
 (*ConstructNavigatorProperty)(JSContext *cx, JS::Handle<JSObject*> naviObj);
