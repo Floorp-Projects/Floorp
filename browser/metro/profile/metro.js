ebd6eea0b231a48e667099ea142c9ecaf53f90f6
Brian R. Bondy <netzen@gmail.com>
2013-08-07 14:45:24 -0400

    Bug 898842 - Increase fling momentum. r=jimm

--- browser/metro/profile/metro.js
+++ browser/metro/profile/metro.js
@@ -30,6 +30,7 @@ pref("prompts.tab_modal.enabled", true);
 // Enable off main thread compositing
 pref("layers.offmainthreadcomposition.enabled", true);
 pref("layers.async-pan-zoom.enabled", false);
+pref("gfx.axis.fling_friction", "0.002");
 
 // Enable Microsoft TSF support by default for imes.
 pref("intl.enable_tsf_support", true);
