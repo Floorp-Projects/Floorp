ec66739453eded37055a70302c55034711ba53ff
Irving Reid <irving@mozilla.com>
2013-08-08 15:56:22 -0400

    Bug 853388: Save and load XPIProvider state to/from a JSON file; r=unfocused

--- toolkit/mozapps/extensions/test/xpcshell/test_bug659772.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_bug659772.js
@@ -108,14 +108,8 @@ function run_test_1() {
       shutdownManager();
 
       // Make it look like the next time the app is started it has a new DB schema
-      let dbfile = gProfD.clone();
-      dbfile.append("extensions.sqlite");
-      let db = AM_Cc["@mozilla.org/storage/service;1"].
-               getService(AM_Ci.mozIStorageService).
-               openDatabase(dbfile);
-      db.schemaVersion = 1;
+      changeXPIDBVersion(1);
       Services.prefs.setIntPref("extensions.databaseSchema", 1);
-      db.close();
 
       let jsonfile = gProfD.clone();
       jsonfile.append("extensions");
@@ -255,14 +249,8 @@ function run_test_2() {
       shutdownManager();
 
       // Make it look like the next time the app is started it has a new DB schema
-      let dbfile = gProfD.clone();
-      dbfile.append("extensions.sqlite");
-      let db = AM_Cc["@mozilla.org/storage/service;1"].
-               getService(AM_Ci.mozIStorageService).
-               openDatabase(dbfile);
-      db.schemaVersion = 1;
+      changeXPIDBVersion(1);
       Services.prefs.setIntPref("extensions.databaseSchema", 1);
-      db.close();
 
       let jsonfile = gProfD.clone();
       jsonfile.append("extensions");
--- toolkit/mozapps/extensions/test/xpcshell/test_locked.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_locked.js
@@ -254,16 +254,13 @@ function run_test_1() {
     do_check_eq(t2.pendingOperations, AddonManager.PENDING_NONE);
     do_check_true(isThemeInAddonsList(profileDir, t2.id));
 
-    // After shutting down the database won't be open so we can lock it
+    // After shutting down the database won't be open so we can
+    // mess with permissions
     shutdownManager();
     var dbfile = gProfD.clone();
-    dbfile.append("extensions.sqlite");
-    let connection = Services.storage.openUnsharedDatabase(dbfile);
-    connection.executeSimpleSQL("PRAGMA synchronous = FULL");
-    connection.executeSimpleSQL("PRAGMA locking_mode = EXCLUSIVE");
-    // Force the DB to become locked
-    connection.beginTransactionAs(connection.TRANSACTION_EXCLUSIVE);
-    connection.commitTransaction();
+    dbfile.append(EXTENSIONS_DB);
+    var savedPermissions = dbfile.permissions;
+    dbfile.permissions = 0;
 
     startupManager(false);
 
@@ -431,7 +428,7 @@ function run_test_1() {
         do_check_eq(t2.pendingOperations, AddonManager.PENDING_NONE);
         do_check_true(isThemeInAddonsList(profileDir, t2.id));
 
-        connection.close();
+        dbfile.permissions = savedPermissions;
 
         // After allowing access to the original DB things should go back to as
         // they were previously
