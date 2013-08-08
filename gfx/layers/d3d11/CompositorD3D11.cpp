6a1db492e224c98ceddc5891ff73c77eadad9f4c
Nicholas Cameron <ncameron@mozilla.com>
2013-08-09 11:30:18 +1200

    Bug 902528. Check we still have a render target in BeginFrame. r=Bas

--- gfx/layers/d3d11/CompositorD3D11.cpp
+++ gfx/layers/d3d11/CompositorD3D11.cpp
@@ -569,7 +569,9 @@ CompositorD3D11::BeginFrame(const Rect* aClipRectIn,
 {
   UpdateRenderTarget();
 
-  if (mSize.width == 0 || mSize.height == 0) {
+  // Failed to create a render target.
+  if (!mDefaultRT ||
+      mSize.width == 0 || mSize.height == 0) {
     *aRenderBoundsOut = Rect();
     return;
   }
