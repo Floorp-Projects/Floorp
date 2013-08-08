4aa4b3a494c62acd981007d861b9e6e55a9baffe
Irving Reid <irving@mozilla.com>
2013-08-08 15:56:26 -0400

    Bug 853388: Upgrade existing SQLITE databases to JSON; r=unfocused

--- toolkit/mozapps/extensions/test/xpcshell/test_locked.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_locked.js
@@ -257,10 +257,8 @@ function run_test_1() {
     // After shutting down the database won't be open so we can
     // mess with permissions
     shutdownManager();
-    var dbfile = gProfD.clone();
-    dbfile.append(EXTENSIONS_DB);
-    var savedPermissions = dbfile.permissions;
-    dbfile.permissions = 0;
+    var savedPermissions = gExtensionsJSON.permissions;
+    gExtensionsJSON.permissions = 0;
 
     startupManager(false);
 
@@ -428,11 +426,12 @@ function run_test_1() {
         do_check_eq(t2.pendingOperations, AddonManager.PENDING_NONE);
         do_check_true(isThemeInAddonsList(profileDir, t2.id));
 
-        dbfile.permissions = savedPermissions;
-
         // After allowing access to the original DB things should go back to as
         // they were previously
-        restartManager();
+        shutdownManager();
+        gExtensionsJSON.permissions = savedPermissions;
+        startupManager();
+
 
         // Shouldn't have seen any startup changes
         check_startup_changes(AddonManager.STARTUP_CHANGE_INSTALLED, []);
