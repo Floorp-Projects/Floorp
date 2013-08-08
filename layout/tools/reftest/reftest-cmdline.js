264c533f69fd1275cf1886021782afda871135e0
Mark Hammond <mhammond@skippinet.com.au>
2013-08-08 15:56:46 +1000

    Back out revision 58e2271ca9d0 (bug 897811) for browser-chrome test failures.

--- layout/tools/reftest/reftest-cmdline.js
+++ layout/tools/reftest/reftest-cmdline.js
@@ -106,7 +106,7 @@ RefTestCmdLineHandler.prototype =
     // Setting this pref makes tests run much faster there.
     branch.setBoolPref("security.fileuri.strict_origin_policy", false);
     // Disable the thumbnailing service
-    branch.setBoolPref("browser.pagethumbnails.capturing_disabled", true);
+    branch.setBoolPref("browser.pagethumbnails.capturing_disabled", false);
 
     var wwatch = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
                            .getService(nsIWindowWatcher);
