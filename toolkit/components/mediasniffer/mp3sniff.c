c0ce3d719e223152cb0ae8b16fa58da68329d477
Ralph Giles <giles@mozilla.com>
2013-08-09 10:27:00 -0700

    Bug 865553 - Handle MPEG version 2 packet lengths. r=padenot, a=akeybl,webaudio
    Previous code didn't adjust bitrate and scale factors correctly
    for computing frame sizes on mpeg 2 and 2.5 files. See
    http://blog.coryhill.net/2009/06/calculating-mp3-frame-length.html
    for the extra factor of two.
    
    Removes a case for handling length calculation for layer-1 files,
    which we don't support.
    
    Adds a tagless mpeg-2 test case for the new sniffer code.

--- toolkit/components/mediasniffer/mp3sniff.c
+++ toolkit/components/mediasniffer/mp3sniff.c
@@ -27,17 +27,23 @@ typedef struct {
 /* Parse the 4-byte header in p and fill in the header struct. */
 static void mp3_parse(const uint8_t *p, mp3_header *header)
 {
-  const int bitrates[16] =
+  const int bitrates[2][16] = {
+        /* MPEG version 1 layer 3 bitrates. */
 	{0,  32000,  40000,  48000,  56000,  64000,  80000,  96000,
-         112000, 128000, 160000, 192000, 224000, 256000, 320000, 0};
+         112000, 128000, 160000, 192000, 224000, 256000, 320000, 0},
+        /* MPEG Version 2 and 2.5 layer 3 bitrates */
+        {0, 8000, 16000, 24000, 32000, 40000, 48000, 56000, 64000,
+         80000, 96000, 112000, 128000, 144000, 160000, 0} };
   const int samplerates[4] = {44100, 48000, 32000, 0};
 
-  header->version = (p[1] & 0x08) >> 3;
+  header->version = (p[1] & 0x18) >> 3;
   header->layer = 4 - ((p[1] & 0x06) >> 1);
   header->errp = (p[1] & 0x01);
 
-  header->bitrate = bitrates[(p[2] & 0xf0) >> 4];
+  header->bitrate = bitrates[(header->version & 1) ? 0 : 1][(p[2] & 0xf0) >> 4];
   header->freq = samplerates[(p[2] & 0x0c) >> 2];
+  if (header->version == 2) header->freq >>= 1;
+  else if (header->version == 0) header->freq >>= 2;
   header->pad = (p[2] & 0x02) >> 1;
   header->priv = (p[2] & 0x01);
 
@@ -54,12 +60,9 @@ static int mp3_framesize(mp3_header *header)
   int size;
   int scale;
 
-  if (header->layer == 1) scale = 48;
+  if ((header->version & 1) == 0) scale = 72;
   else scale = 144;
-
   size = header->bitrate * scale / header->freq;
-  /* divide by an extra factor of 2 for MPEG-2? */
-
   if (header->pad) size += 1;
 
   return size;
@@ -69,7 +72,7 @@ static int is_mp3(const uint8_t *p, long length) {
   /* Do we have enough room to see a 4 byte header? */
   if (length < 4) return 0;
   /* Do we have a sync pattern? */
-  if (p[0] == 0xff && (p[1]&0xe0) == 0xe0) {
+  if (p[0] == 0xff && (p[1] & 0xe0) == 0xe0) {
     /* Do we have any illegal field values? */
     if (((p[1] & 0x06) >> 1) == 0) return 0;  /* No layer 4 */
     if (((p[2] & 0xf0) >> 4) == 15) return 0; /* Bitrate can't be 1111 */
@@ -131,7 +134,7 @@ int mp3_sniff(const uint8_t *buf, long length)
     } else if (is_mp3(p, avail)) {
       mp3_parse(p, &header);
       skip = mp3_framesize(&header);
-      if (skip < 4 || skip + 4 > avail) {
+      if (skip < 4 || skip + 4 >= avail) {
         return 0;
       }
       p += skip;
