82c212ea4b5ade607cf0988518acf367b3e1e7da
Hannes Verschore <hv1989@gmail.com>
2013-08-10 01:16:20 +0200

    Bug 898832: Fix topcrash regression, r=bhackett a=bajaj

--- js/src/ion/IonBuilder.cpp
+++ js/src/ion/IonBuilder.cpp
@@ -4928,6 +4928,9 @@ ArgumentTypesMatch(MDefinition *def, types::StackTypeSet *calleeTypes)
     if (def->type() == MIRType_Value)
         return false;
 
+    if (def->type() == MIRType_Object)
+        return calleeTypes->unknownObject();
+
     return calleeTypes->mightBeType(ValueTypeFromMIRType(def->type()));
 }
 
