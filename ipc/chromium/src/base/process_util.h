e9da6d90ddb4d859d42f60909051837529e63a4b
Mike Hommey <mh+mozilla@glandium.org>
2013-08-10 15:54:00 +0900

    Bug 901414 - Fix ipc/chromium to build for GNU/kFreeBSD. r=bsmedberg, a=lsblakk

--- ipc/chromium/src/base/process_util.h
+++ ipc/chromium/src/base/process_util.h
@@ -13,7 +13,7 @@
 #if defined(OS_WIN)
 #include <windows.h>
 #include <tlhelp32.h>
-#elif defined(OS_LINUX)
+#elif defined(OS_LINUX) || defined(__GLIBC__)
 #include <dirent.h>
 #include <limits.h>
 #include <sys/types.h>
@@ -298,7 +298,7 @@ class NamedProcessIterator {
   const ProcessEntry* NextProcessEntry();
 
  private:
-#if !defined(OS_BSD)
+#if !defined(OS_BSD) || defined(__GLIBC__)
   // Determines whether there's another process (regardless of executable)
   // left in the list of all processes.  Returns true and sets entry_ to
   // that process's info if there is one, false otherwise.
@@ -316,7 +316,7 @@ class NamedProcessIterator {
 #if defined(OS_WIN)
   HANDLE snapshot_;
   bool started_iteration_;
-#elif defined(OS_LINUX)
+#elif defined(OS_LINUX) || defined(__GLIBC__)
   DIR *procfs_dir_;
 #elif defined(OS_BSD)
   std::vector<ProcessEntry> content;
@@ -325,7 +325,7 @@ class NamedProcessIterator {
   std::vector<kinfo_proc> kinfo_procs_;
   size_t index_of_kinfo_proc_;
 #endif
-#if !defined(OS_BSD)
+#if !defined(OS_BSD) || defined(__GLIBC__)
   ProcessEntry entry_;
   const ProcessFilter* filter_;
 #endif
