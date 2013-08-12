c571d1b19c12ddca15805a0da9ccfdb280f95b5d
Thomas Zimmermann <tdz@users.sourceforge.net>
2013-08-12 10:31:58 +0200

    Bug 888595 - Convert BluetoothManager bindings to WebIDL. r=bz, r=echou
    This patch converts the BluetoothManager to WebIDL and removes the
    old XPIDL bindings. The only user of BluetoothManager is the class
    Navigator, which has been adapted to the change.

--- dom/base/Navigator.cpp
+++ dom/base/Navigator.cpp
@@ -59,7 +59,6 @@
 #include "Telephony.h"
 #endif
 #ifdef MOZ_B2G_BT
-#include "nsIDOMBluetoothManager.h"
 #include "BluetoothManager.h"
 #endif
 #include "nsIDOMCameraManager.h"
@@ -1249,7 +1248,7 @@ Navigator::GetMozMobileConnection(ErrorResult& aRv)
 #endif // MOZ_B2G_RIL
 
 #ifdef MOZ_B2G_BT
-nsIDOMBluetoothManager*
+bluetooth::BluetoothManager*
 Navigator::GetMozBluetooth(ErrorResult& aRv)
 {
   if (!mBluetooth) {
@@ -1276,7 +1275,7 @@ Navigator::EnsureMessagesManager()
   nsresult rv;
   nsCOMPtr<nsIDOMNavigatorSystemMessages> messageManager =
     do_CreateInstance("@mozilla.org/system-message-manager;1", &rv);
-  
+
   nsCOMPtr<nsIDOMGlobalPropertyInitializer> gpi =
     do_QueryInterface(messageManager);
   NS_ENSURE_TRUE(gpi, NS_ERROR_FAILURE);
