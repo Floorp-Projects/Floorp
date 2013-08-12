b2e775683b626d94e80b749abe718aa9b9c708f2
Paul Adenot <paul@paul.cx>
2013-08-12 14:40:49 +0200

    Bug 903476 - Update libspeex to opus-tools version, as it is the current upstream. r=ehsan
    --HG--
    extra : rebase_source : 4dd23aa8f0e7f0919e1b8f37d9efce11e6b8d8cc

--- media/libspeex_resampler/src/resample_sse.h
+++ media/libspeex_resampler/src/resample_sse.h
@@ -41,15 +41,23 @@ static inline float inner_product_single(const float *a, const float *b, unsigne
 {
    int i;
    float ret;
-   __m128 sum = _mm_setzero_ps();
-   for (i=0;i<len;i+=8)
+   if (1)
    {
-      sum = _mm_add_ps(sum, _mm_mul_ps(_mm_loadu_ps(a+i), _mm_loadu_ps(b+i)));
-      sum = _mm_add_ps(sum, _mm_mul_ps(_mm_loadu_ps(a+i+4), _mm_loadu_ps(b+i+4)));
+      __m128 sum = _mm_setzero_ps();
+      for (i=0;i<len;i+=8)
+      {
+         sum = _mm_add_ps(sum, _mm_mul_ps(_mm_loadu_ps(a+i), _mm_loadu_ps(b+i)));
+         sum = _mm_add_ps(sum, _mm_mul_ps(_mm_loadu_ps(a+i+4), _mm_loadu_ps(b+i+4)));
+      }
+      sum = _mm_add_ps(sum, _mm_movehl_ps(sum, sum));
+      sum = _mm_add_ss(sum, _mm_shuffle_ps(sum, sum, 0x55));
+      _mm_store_ss(&ret, sum);
    }
-   sum = _mm_add_ps(sum, _mm_movehl_ps(sum, sum));
-   sum = _mm_add_ss(sum, _mm_shuffle_ps(sum, sum, 0x55));
-   _mm_store_ss(&ret, sum);
+   else
+   {
+      ret = 0;
+      for (i=0;i<len;i++) ret += a[i] * b[i];
+   }
    return ret;
 }
 
@@ -57,21 +65,37 @@ static inline float inner_product_single(const float *a, const float *b, unsigne
 static inline float interpolate_product_single(const float *a, const float *b, unsigned int len, const spx_uint32_t oversample, float *frac) {
   int i;
   float ret;
-  __m128 sum = _mm_setzero_ps();
-  __m128 f = _mm_loadu_ps(frac);
-  for(i=0;i<len;i+=2)
+  if (1)
   {
-    sum = _mm_add_ps(sum, _mm_mul_ps(_mm_load1_ps(a+i), _mm_loadu_ps(b+i*oversample)));
-    sum = _mm_add_ps(sum, _mm_mul_ps(_mm_load1_ps(a+i+1), _mm_loadu_ps(b+(i+1)*oversample)));
+    __m128 sum = _mm_setzero_ps();
+    __m128 f = _mm_loadu_ps(frac);
+    for(i=0;i<len;i+=2)
+    {
+      sum = _mm_add_ps(sum, _mm_mul_ps(_mm_load1_ps(a+i), _mm_loadu_ps(b+i*oversample)));
+      sum = _mm_add_ps(sum, _mm_mul_ps(_mm_load1_ps(a+i+1), _mm_loadu_ps(b+(i+1)*oversample)));
+    }
+    sum = _mm_mul_ps(f, sum);
+    sum = _mm_add_ps(sum, _mm_movehl_ps(sum, sum));
+    sum = _mm_add_ss(sum, _mm_shuffle_ps(sum, sum, 0x55));
+    _mm_store_ss(&ret, sum);
   }
-   sum = _mm_mul_ps(f, sum);
-   sum = _mm_add_ps(sum, _mm_movehl_ps(sum, sum));
-   sum = _mm_add_ss(sum, _mm_shuffle_ps(sum, sum, 0x55));
-   _mm_store_ss(&ret, sum);
-   return ret;
+  else
+  {
+    float accum[4] = {0,0,0,0};
+    for(i=0;i<len;i++)
+    {
+      const float curr_in=a[i];
+      accum[0] += curr_in * b[i * oversample + 0];
+      accum[1] += curr_in * b[i * oversample + 1];
+      accum[2] += curr_in * b[i * oversample + 2];
+      accum[3] += curr_in * b[i * oversample + 3];
+    }
+    ret = accum[0] * frac[0] + accum[1] * frac[1] + accum[2] * frac[2] + accum[3] * frac[3];
+  }
+  return ret;
 }
 
-#ifdef _USE_SSE2
+#ifdef __SSE2__
 #include <emmintrin.h>
 #define OVERRIDE_INNER_PRODUCT_DOUBLE
 
@@ -91,7 +115,7 @@ static inline double inner_product_double(const float *a, const float *b, unsign
       sum = _mm_add_pd(sum, _mm_cvtps_pd(t));
       sum = _mm_add_pd(sum, _mm_cvtps_pd(_mm_movehl_ps(t, t)));
    }
-   sum = _mm_add_sd(sum, _mm_unpackhi_pd(sum, sum));
+   sum = _mm_add_sd(sum, (__m128d) _mm_movehl_ps((__m128) sum, (__m128) sum));
    _mm_store_sd(&ret, sum);
    return ret;
 }
@@ -120,7 +144,7 @@ static inline double interpolate_product_double(const float *a, const float *b, 
   sum1 = _mm_mul_pd(f1, sum1);
   sum2 = _mm_mul_pd(f2, sum2);
   sum = _mm_add_pd(sum1, sum2);
-  sum = _mm_add_sd(sum, _mm_unpackhi_pd(sum, sum));
+  sum = _mm_add_sd(sum, (__m128d) _mm_movehl_ps((__m128) sum, (__m128) sum));
   _mm_store_sd(&ret, sum);
   return ret;
 }
