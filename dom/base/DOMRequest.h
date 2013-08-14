bca34ae153f43a9917d312738cba080c06994ec8
Andrew McCreight <amccreight@mozilla.com>
2013-08-14 11:29:20 -0700

    Bug 889193 - Remove DOMRequest::mRooted. r=smaug

--- dom/base/DOMRequest.h
+++ dom/base/DOMRequest.h
@@ -24,7 +24,6 @@ protected:
   JS::Heap<JS::Value> mResult;
   nsCOMPtr<nsISupports> mError;
   bool mDone;
-  bool mRooted;
 
 public:
   NS_DECL_ISUPPORTS_INHERITED
@@ -78,16 +77,14 @@ public:
 
   virtual ~DOMRequest()
   {
-    if (mRooted) {
-      UnrootResultVal();
-    }
+    mResult = JSVAL_VOID;
+    NS_DROP_JS_OBJECTS(this, DOMRequest);
   }
 
 protected:
   void FireEvent(const nsAString& aType, bool aBubble, bool aCancelable);
 
   void RootResultVal();
-  void UnrootResultVal();
 
   void Init(nsIDOMWindow* aWindow);
 };
