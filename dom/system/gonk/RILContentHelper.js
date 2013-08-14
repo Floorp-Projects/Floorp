293e70fcd2cd63ce26b0ded3b186f65b45f2673a
Szu-Yu Chen [:aknow] <szchen@mozilla.com>
2013-08-14 08:50:36 -0400

    Bug 887690 - Part 3: Add emergency callback mode support (ril). r=hsinyi

--- dom/system/gonk/RILContentHelper.js
+++ dom/system/gonk/RILContentHelper.js
@@ -75,6 +75,7 @@ const RIL_IPC_MSG_NAMES = [
   "RIL:SelectNetwork",
   "RIL:SelectNetworkAuto",
   "RIL:CallStateChanged",
+  "RIL:EmergencyCbModeChanged",
   "RIL:VoicemailNotification",
   "RIL:VoicemailInfoChanged",
   "RIL:CallError",
@@ -104,7 +105,8 @@ const RIL_IPC_MSG_NAMES = [
   "RIL:UpdateIccContact",
   "RIL:SetRoamingPreference",
   "RIL:GetRoamingPreference",
-  "RIL:CdmaCallWaiting"
+  "RIL:CdmaCallWaiting",
+  "RIL:ExitEmergencyCbMode"
 ];
 
 XPCOMUtils.defineLazyServiceGetter(this, "cpmm",
@@ -1136,6 +1138,24 @@ RILContentHelper.prototype = {
     return request;
   },
 
+  exitEmergencyCbMode: function exitEmergencyCbMode(window) {
+    if (window == null) {
+      throw Components.Exception("Can't get window object",
+                                  Cr.NS_ERROR_UNEXPECTED);
+    }
+    let request = Services.DOMRequest.createRequest(window);
+    let requestId = this.getRequestId(request);
+
+    cpmm.sendAsyncMessage("RIL:ExitEmergencyCbMode", {
+      clientId: 0,
+      data: {
+        requestId: requestId,
+      }
+    });
+
+    return request;
+  },
+
   _mobileConnectionListeners: null,
   _telephonyListeners: null,
   _cellBroadcastListeners: null,
@@ -1606,6 +1626,15 @@ RILContentHelper.prototype = {
                            "notifyCdmaCallWaiting",
                            [msg.json.data]);
         break;
+      case "RIL:ExitEmergencyCbMode":
+        this.handleExitEmergencyCbMode(msg.json);
+        break;
+      case "RIL:EmergencyCbModeChanged":
+        let data = msg.json.data;
+        this._deliverEvent("_mobileConnectionListeners",
+                           "notifyEmergencyCbModeChanged",
+                           [data.active, data.timeoutMs]);
+        break;
     }
   },
 
@@ -1789,6 +1818,20 @@ RILContentHelper.prototype = {
 
     let status = new DOMCLIRStatus(message);
     this.fireRequestSuccess(message.requestId, status);
+  },
+
+  handleExitEmergencyCbMode: function handleExitEmergencyCbMode(message) {
+    let requestId = message.requestId;
+    let request = this.takeRequest(requestId);
+    if (!request) {
+      return;
+    }
+
+    if (!message.success) {
+      Services.DOMRequest.fireError(request, message.errorMsg);
+      return;
+    }
+    Services.DOMRequest.fireSuccess(request, null);
   },
 
   handleSendCancelMMI: function handleSendCancelMMI(message) {
