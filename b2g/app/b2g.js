6a8a44af5a649774a7aeaf68fcc4653fd3a5282f
Matt Woodrow <mwoodrow@mozilla.com>
2013-08-15 10:49:45 +1200

    Bug 914984 - Enable Azure content for b2g. r=nrc

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -257,6 +257,8 @@ pref("layers.acceleration.disabled", false);
 pref("layers.offmainthreadcomposition.async-animations", true);
 pref("layers.async-video.enabled", true);
 pref("layers.async-pan-zoom.enabled", true);
+pref("gfx.content.azure.enabled", true);
+pref("gfx.content.azure.backends", "cairo");
 #endif
 
 // Web Notifications
