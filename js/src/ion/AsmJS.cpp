8852dd694e10e35b24ebf63505ba397f02e79db3
Luke Wagner <luke@mozilla.com>
2013-08-15 08:54:46 -0400

    Bug 893519 - OdinMonkey: require compile-and-go. r=bbouvier, a=lsblakk

--- js/src/ion/AsmJS.cpp
+++ js/src/ion/AsmJS.cpp
@@ -6291,6 +6291,9 @@ js::CompileAsmJS(JSContext *cx, AsmJSParser &parser, ParseNode *stmtList, bool *
     if (!cx->hasOption(JSOPTION_ASMJS))
         return Warn(cx, JSMSG_USE_ASM_TYPE_FAIL, "Disabled by javascript.options.asmjs in about:config");
 
+    if (!parser.options().compileAndGo)
+        return Warn(cx, JSMSG_USE_ASM_TYPE_FAIL, "Temporarily disabled for event-handler and other cloneable scripts");
+
     if (cx->compartment()->debugMode())
         return Warn(cx, JSMSG_USE_ASM_TYPE_FAIL, "Disabled by debugger");
 
