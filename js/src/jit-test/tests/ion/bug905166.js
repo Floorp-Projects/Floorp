daf047f480e1447e8536e6e738b1a8565ffae0b0
Dan Gohman <sunfish@google.com>
2013-08-14 21:30:44 -0700

    Bug 905166 - IonMonkey - Handle NaN cases explicitly in Range::min and Range::max. r=nbp, a=abillings

---
+++ js/src/jit-test/tests/ion/bug905166.js
@@ -0,0 +1,9 @@
+function test() {
+  LastIndexOf("hello");
+  LastIndexOf("hello");
+  eval("");
+  function LastIndexOf(s) {
+     x = Math.min(Math.max(Math.pow(-1, 0.5), 0), s.length);
+     0 <= x;
+  }
+} test();
