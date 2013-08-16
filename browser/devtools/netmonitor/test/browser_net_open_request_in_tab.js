88e4d88c1394d6902114807c7c1fc7ec122951c4
Avinash Kundaliya <avinash@avinash.com.np>
2013-08-16 15:33:11 -0700

    Bug 889944 - add "open in new tab" context menu item on resources; r=harth

---
+++ browser/devtools/netmonitor/test/browser_net_open_request_in_tab.js
@@ -0,0 +1,37 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Tests if Open in new tab works.
+ */
+
+function test() {
+  waitForExplicitFinish();
+
+  initNetMonitor(CUSTOM_GET_URL).then(([aTab, aDebuggee, aMonitor]) => {
+    info("Starting test...");
+
+    let { NetMonitorView } = aMonitor.panelWin;
+    let { RequestsMenu } = NetMonitorView;
+
+    RequestsMenu.lazyUpdate = false;
+
+    waitForNetworkEvents(aMonitor, 1).then(() => {
+      let requestItem = RequestsMenu.getItemAtIndex(0);
+      RequestsMenu.selectedItem = requestItem;
+
+      gBrowser.tabContainer.addEventListener("TabOpen", function(event) {
+        ok(true, "A new tab has been opened ");
+        removeTab(event.target);
+        cleanUp();
+      }, false);
+
+      RequestsMenu.openRequestInTab();
+    });
+
+    aDebuggee.performRequests(1);
+    function cleanUp(){
+      teardown(aMonitor).then(finish);
+    }
+  });
+}
