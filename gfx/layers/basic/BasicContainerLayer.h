3dcdbb87c5e3c64b31cc741df050779774864fe4
Arnaud Sourioux <six.dsn@gmail.com>
2013-08-16 09:18:36 -0400

    Bug 903112 - Replace this with MOZ_THIS_IN_INITIALIZER_LIST() in gfx/. r=jfkthame

--- gfx/layers/basic/BasicContainerLayer.h
+++ gfx/layers/basic/BasicContainerLayer.h
@@ -178,7 +178,8 @@ class BasicContainerLayer : public ContainerLayer, public BasicImplData {
 
 public:
   BasicContainerLayer(BasicLayerManager* aManager) :
-    ContainerLayer(aManager, static_cast<BasicImplData*>(this))
+    ContainerLayer(aManager,
+                   static_cast<BasicImplData*>(MOZ_THIS_IN_INITIALIZER_LIST()))
   {
     MOZ_COUNT_CTOR(BasicContainerLayer);
     mSupportsComponentAlphaChildren = true;
