8a6478ed80af76b618700fcfef2c705bfa6f6b3f
Mark Capella <markcapella@twcny.rr.com>
2013-08-16 16:45:42 -0400

    Bug 864589 - Show/hide text selection handles if a selection is programatically added/removed, r=margaret, ehsan

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -4444,6 +4444,10 @@ Selection::CollapseToStart()
   if (!firstRange)
     return NS_ERROR_FAILURE;
 
+  if (mFrameSelection) {
+    int16_t reason = mFrameSelection->PopReason() | nsISelectionListener::COLLAPSETOSTART_REASON;
+    mFrameSelection->PostReason(reason);
+  }
   return Collapse(firstRange->GetStartParent(), firstRange->StartOffset());
 }
 
@@ -4464,6 +4468,10 @@ Selection::CollapseToEnd()
   if (!lastRange)
     return NS_ERROR_FAILURE;
 
+  if (mFrameSelection) {
+    int16_t reason = mFrameSelection->PopReason() | nsISelectionListener::COLLAPSETOEND_REASON;
+    mFrameSelection->PostReason(reason);
+  }
   return Collapse(lastRange->GetEndParent(), lastRange->EndOffset());
 }
 
