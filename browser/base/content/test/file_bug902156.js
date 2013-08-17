6789aca698c8ca56347f99f37856175fa303eded
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2013-08-16 22:15:02 -0700

    Bug 902156 - Mochitest for Persist 'disable protection' option for Mixed Content Blocker. r=smaug, r=tanvi, a=bajaj

---
+++ browser/base/content/test/file_bug902156.js
@@ -0,0 +1,5 @@
+/* 
+ * Once the mixed content blocker is disabled for the page, this scripts loads
+ * and updates the text inside the div container.
+ */
+document.getElementById("mctestdiv").innerHTML = "Mixed Content Blocker disabled";
