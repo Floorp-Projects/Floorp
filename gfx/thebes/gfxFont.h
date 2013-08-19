e75649fb2d2c45ef80ea5122faa66435dcd3038d
Robert O'Callahan <robert@ocallahan.org>
2013-08-20 01:08:44 +1200

    Bug 906643. Part 4: Make each gfxFontEntry track its gfxFonts with SVG glyphs. r=jfkthame
    --HG--
    extra : rebase_source : cd2cbdc8f6722e71f523c3fbe19e515a0e44f043

--- gfx/thebes/gfxFont.h
+++ gfx/thebes/gfxFont.h
@@ -324,7 +324,7 @@ public:
     // can be safely dereferenced.
     virtual nsresult ReadCMAP();
 
-    bool TryGetSVGData();
+    bool TryGetSVGData(gfxFont* aFont);
     bool HasSVGGlyph(uint32_t aGlyphId);
     bool GetSVGGlyphExtents(gfxContext *aContext, uint32_t aGlyphId,
                             gfxRect *aResult);
@@ -414,6 +414,10 @@ public:
     // Caller must call gfxFontEntry::ReleaseGrFace when finished with it.
     gr_face* GetGrFace();
     virtual void ReleaseGrFace(gr_face* aFace);
+
+    // Called to notify that aFont is being destroyed. Needed when we're tracking
+    // the fonts belonging to this font entry.
+    void NotifyFontDestroyed(gfxFont* aFont);
     
     // For memory reporting
     virtual void SizeOfExcludingThis(mozilla::MallocSizeOf aMallocSizeOf,
@@ -457,7 +461,8 @@ public:
     nsAutoArrayPtr<uint8_t> mUVSData;
     gfxUserFontData* mUserFontData;
     gfxSVGGlyphs    *mSVGGlyphs;
-
+    // list of gfxFonts that are using SVG glyphs
+    nsTArray<gfxFont*> mFontsUsingSVGGlyphs;
     nsTArray<gfxFontFeature> mFeatureSettings;
     uint32_t         mLanguageOverride;
 
