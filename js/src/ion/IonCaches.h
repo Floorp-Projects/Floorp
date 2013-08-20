975b3e85f12fb0f6f74ccf615e9555b9b5c6f603
Jan de Mooij <jdemooij@mozilla.com>
2013-08-20 12:45:31 +0200

    Bug 905993 - Fix SetElementIC temp register. r=nbp, a=abillings

--- js/src/ion/IonCaches.h
+++ js/src/ion/IonCaches.h
@@ -721,7 +721,7 @@ class SetElementIC : public RepatchIonCache
         return object_;
     }
     Register tempToUnboxIndex() const {
-        return temp_;
+        return tempToUnboxIndex_;
     }
     Register temp() const {
         return temp_;
