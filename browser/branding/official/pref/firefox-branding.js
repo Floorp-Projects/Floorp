8a184899bf369e73a8128fb86872a98890a23f27
Gavin Sharp <gavin@gavinsharp.com>
2013-08-20 14:16:25 -0700

    Bug 907404 - Disable the in-product whatsnew page, so that manual upgrades are consistent with updater upgrades wrt showing the whatsnew page (updater upgrades disable whatsnew via snippets). r=dolske, a=asa, a=akeybl

--- browser/branding/official/pref/firefox-branding.js
+++ browser/branding/official/pref/firefox-branding.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("startup.homepage_override_url","https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/whatsnew/?oldversion=%OLD_VERSION%");
+pref("startup.homepage_override_url","");
 pref("startup.homepage_welcome_url","https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/");
 // Interval: Time between checks for a new version (in seconds)
 pref("app.update.interval", 43200); // 12 hours
