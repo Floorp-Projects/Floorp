d3eb484306d77fffde06dc788ecf60005a5c2f43
Simon Montagu <smontagu@smontagu.org>
2013-08-21 09:18:16 +0300

    Backout last push to correct checkin comment

--- netwerk/test/unit/test_idn_urls.js
+++ netwerk/test/unit/test_idn_urls.js
@@ -270,12 +270,7 @@ const testcases = [
 
     // Katakana and Hangul
     ["ハングル한글",
-                 "xn--qck1c2d4a2366lkmzb",           false, false, false],
-
-    // Thai (also tests that node with over 63 UTF-8 octets doesn't fail)
-    ["เครื่องทําน้ําทําน้ําแข็ง",
-                 "xn--22cdjb2fanb9fyepcbbb9dwh4a3igze4fdcd",
-                                                     false, true, true]
+                 "xn--qck1c2d4a2366lkmzb",           false, false, false]
 ];
 
 
