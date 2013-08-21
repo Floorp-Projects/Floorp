51f127fc43062b5501796bde0bbc7fb3fd6f2dee
Stephen Pohl <spohl.mozilla.bugs@gmail.com>
2013-08-21 12:21:59 -0400

    Revert fd6c7792b048 from bug 673875 for causing bug 907275. r=smichaud, a=bajaj

--- widget/cocoa/nsChildView.mm
+++ widget/cocoa/nsChildView.mm
@@ -3837,6 +3837,8 @@ NSEvent* gLastDragMouseDownEvent = nil;
 
 - (void)beginGestureWithEvent:(NSEvent *)anEvent
 {
+  NS_ASSERTION(mGestureState == eGestureState_None, "mGestureState should be eGestureState_None");
+
   if (!anEvent)
     return;
 
@@ -4202,7 +4204,6 @@ NSEvent* gLastDragMouseDownEvent = nil;
   }
 
   __block BOOL animationCanceled = NO;
-  __block BOOL geckoSwipeEventSent = NO;
   // At this point, anEvent is the first scroll wheel event in a two-finger
   // horizontal gesture that we've decided to treat as a swipe.  When we call
   // [NSEvent trackSwipeEventWithOptions:...], the OS interprets all
@@ -4256,7 +4257,7 @@ NSEvent* gLastDragMouseDownEvent = nil;
                direction:0.0
                    delta:gestureAmount];
 
-    if (phase == NSEventPhaseEnded && !geckoSwipeEventSent) {
+    if (phase == NSEventPhaseEnded) {
       // The result of the swipe is now known, so the main event can be sent.
       // The animation might continue even after this event was sent, so
       // don't tear down the animation overlay yet.
@@ -4269,7 +4270,6 @@ NSEvent* gLastDragMouseDownEvent = nil;
       // gestureAmount is negative when it will be '-1' at isComplete, and
       // positive when it will be '1'.  And phase is never equal to
       // NSEventPhaseEnded when gestureAmount will be '0' at isComplete.
-      geckoSwipeEventSent = YES;
       [self sendSwipeEvent:anEvent
                   withKind:NS_SIMPLE_GESTURE_SWIPE
          allowedDirections:&allowedDirectionsCopy
