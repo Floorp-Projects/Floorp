28b7b3b2e10aa46e2521ca38e1993842f9f306b2
Jared Wein <jwein@mozilla.com>
2013-08-21 15:33:46 -0400

    Bug 907796 - Add error reporters to the Task.spawns within browser/components/customizableui. r=Gijs

--- browser/components/customizableui/content/panelUI.js
+++ browser/components/customizableui/content/panelUI.js
@@ -185,7 +185,7 @@ const PanelUI = {
         CustomizableUI.registerMenuPanel(this.contents);
         this.endBatchUpdate();
       }
-    }.bind(this));
+    }.bind(this)).then(null, ERROR);
   },
 
   /**
