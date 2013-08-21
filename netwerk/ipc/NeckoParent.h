d5ca1ef02317e47df1f05b8417d4b0f1c5d34137
David Zbarsky <dzbarsky@gmail.com>
2013-08-21 02:49:43 -0400

    Bug 890570 - PRemoteOpenFile Constructor doesn't need PBrowser r=jduell

--- netwerk/ipc/NeckoParent.h
+++ netwerk/ipc/NeckoParent.h
@@ -88,12 +88,10 @@ protected:
   virtual bool DeallocPWebSocketParent(PWebSocketParent*);
   virtual PTCPSocketParent* AllocPTCPSocketParent();
 
-  virtual PRemoteOpenFileParent* AllocPRemoteOpenFileParent(const URIParams& aFileURI,
-                                                            PBrowserParent* aBrowser)
+  virtual PRemoteOpenFileParent* AllocPRemoteOpenFileParent(const URIParams& aFileURI)
                                                             MOZ_OVERRIDE;
   virtual bool RecvPRemoteOpenFileConstructor(PRemoteOpenFileParent* aActor,
-                                              const URIParams& aFileURI,
-                                              PBrowserParent* aBrowser)
+                                              const URIParams& aFileURI)
                                               MOZ_OVERRIDE;
   virtual bool DeallocPRemoteOpenFileParent(PRemoteOpenFileParent* aActor)
                                             MOZ_OVERRIDE;
