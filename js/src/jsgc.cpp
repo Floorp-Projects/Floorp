fd078190630d72c53a54a9149a4cea389af25740
Terrence Cole <terrence@mozilla.com>
2013-08-21 12:43:22 -0700

    Bug 907791 - Suppress some AsmJS rooting non-hazards in the static analysis; r=bhackett

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -5140,6 +5140,12 @@ AutoMaybeTouchDeadZones::~AutoMaybeTouchDeadZones()
     runtime->gcManipulatingDeadZones = manipulatingDeadZones;
 }
 
+AutoSuppressGC::AutoSuppressGC(JSRuntime *rt)
+  : suppressGC_(rt->mainThread.suppressGC)
+{
+    suppressGC_++;
+}
+
 AutoSuppressGC::AutoSuppressGC(JSContext *cx)
   : suppressGC_(cx->runtime()->mainThread.suppressGC)
 {
