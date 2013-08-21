51c7623263958782da4575df0917792d1a740b19
Andrew Wang <iforgot120@gmail.com>
2013-08-21 08:32:47 -0400

    Bug 901126 - Split browser_newtab_drag_drop.js into two tests. r=ttaubert, a=test-only

--- browser/base/content/test/newtab/browser_newtab_drag_drop.js
+++ browser/base/content/test/newtab/browser_newtab_drag_drop.js
@@ -71,47 +71,4 @@ function runTests() {
 
   yield simulateDrop(4, 0);
   checkGrid("3,1p,2p,4,0p,5p,6,7,8");
-
-  // drag a new site onto the very first cell
-  yield setLinks("0,1,2,3,4,5,6,7,8");
-  setPinnedLinks(",,,,,,,7,8");
-
-  yield addNewTabPageTab();
-  checkGrid("0,1,2,3,4,5,6,7p,8p");
-
-  yield simulateDrop(0);
-  checkGrid("99p,0,1,2,3,4,5,7p,8p");
-
-  // drag a new site onto the grid and make sure that pinned cells don't get
-  // pushed out
-  yield setLinks("0,1,2,3,4,5,6,7,8");
-  setPinnedLinks(",,,,,,,7,8");
-
-  yield addNewTabPageTab();
-  checkGrid("0,1,2,3,4,5,6,7p,8p");
-
-  yield simulateDrop(7);
-  checkGrid("0,1,2,3,4,5,7p,99p,8p");
-
-  // drag a new site beneath a pinned cell and make sure the pinned cell is
-  // not moved
-  yield setLinks("0,1,2,3,4,5,6,7,8");
-  setPinnedLinks(",,,,,,,,8");
-
-  yield addNewTabPageTab();
-  checkGrid("0,1,2,3,4,5,6,7,8p");
-
-  yield simulateDrop(7);
-  checkGrid("0,1,2,3,4,5,6,99p,8p");
-
-  // drag a new site onto a block of pinned sites and make sure they're shifted
-  // around accordingly
-  yield setLinks("0,1,2,3,4,5,6,7,8");
-  setPinnedLinks("0,1,2,,,,,,");
-
-  yield addNewTabPageTab();
-  checkGrid("0p,1p,2p");
-
-  yield simulateDrop(1);
-  checkGrid("0p,99p,1p,2p,3,4,5,6,7");
 }
