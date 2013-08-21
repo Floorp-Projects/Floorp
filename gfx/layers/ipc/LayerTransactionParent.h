29edd550a1a86ed9569d5ad15a70f2df732116e9
Nicholas Cameron <ncameron@mozilla.com>
2013-08-21 13:28:53 +1200

    Bug 902929. Be explicit about re-attaching async video compositable hosts. r=nical

--- gfx/layers/ipc/LayerTransactionParent.h
+++ gfx/layers/ipc/LayerTransactionParent.h
@@ -106,7 +106,9 @@ protected:
   virtual PCompositableParent* AllocPCompositableParent(const TextureInfo& aInfo) MOZ_OVERRIDE;
   virtual bool DeallocPCompositableParent(PCompositableParent* actor) MOZ_OVERRIDE;
 
-  void Attach(ShadowLayerParent* aLayerParent, CompositableParent* aCompositable);
+  void Attach(ShadowLayerParent* aLayerParent,
+              CompositableParent* aCompositable,
+              bool aIsAsyncVideo);
 
 private:
   nsRefPtr<LayerManagerComposite> mLayerManager;
