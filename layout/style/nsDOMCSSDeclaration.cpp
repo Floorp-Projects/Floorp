1e4c7f3a26c37de14ecccb03964a5cc66f65fa27
Ms2ger <ms2ger@gmail.com>
2013-08-22 08:29:57 +0200

    Bug 905751 - Don't include binding headers in ns{I,DOM}CSSDeclaration.h; r=roc

--- layout/style/nsDOMCSSDeclaration.cpp
+++ layout/style/nsDOMCSSDeclaration.cpp
@@ -6,10 +6,12 @@
 /* base class for DOM objects for element.style and cssStyleRule.style */
 
 #include "nsDOMCSSDeclaration.h"
+
 #include "nsCSSParser.h"
 #include "nsCSSStyleSheet.h"
 #include "mozilla/css/Rule.h"
 #include "mozilla/css/Declaration.h"
+#include "mozilla/dom/CSS2PropertiesBinding.h"
 #include "nsCSSProps.h"
 #include "nsCOMPtr.h"
 #include "mozAutoDocUpdate.h"
@@ -18,6 +20,12 @@ using namespace mozilla;
 
 nsDOMCSSDeclaration::~nsDOMCSSDeclaration()
 {
+}
+
+/* virtual */ JSObject*
+nsDOMCSSDeclaration::WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope)
+{
+  return dom::CSS2PropertiesBinding::Wrap(aCx, aScope, this);
 }
 
 NS_INTERFACE_TABLE_HEAD(nsDOMCSSDeclaration)
