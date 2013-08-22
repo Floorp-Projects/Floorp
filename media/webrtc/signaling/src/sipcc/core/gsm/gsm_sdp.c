fe5cc151d2bb15892f8a1d8aeb341d3ad264407a
Ryan VanderMeulen <ryanvm@gmail.com>
2013-08-22 15:01:03 -0400

    Backed out changesets 3adaeac9d567 and ee85a2f2ecb1 (bug 880067) for Windows bustage on a CLOSED TREE.

--- media/webrtc/signaling/src/sipcc/core/gsm/gsm_sdp.c
+++ media/webrtc/signaling/src/sipcc/core/gsm/gsm_sdp.c
@@ -1,6 +1,4 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * vim: set ts=8 sts=4 et sw=4 tw=99:
- * This Source Code Form is subject to the terms of the Mozilla Public
+/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
@@ -4404,189 +4402,6 @@ fsmdef_media_t* gsmsdp_find_media_by_media_type(fsmdef_dcb_t *dcb_p, sdp_media_e
 }
 
 /*
- * gsmsdp_add_rtcp_fb
- *
- * Description:
- *  Adds a=rtcp-fb attributes to local SDP for supported video codecs
- *
- * Parameters:
- *   level - SDP media level (between 1 and the # of m-lines)
- *   sdp_p - pointer to local SDP
- *   codec - the codec type that the attributes should be added for
- *   types - a bitmask of rtcp-fb types to add, taken from
- *           sdp_rtcp_fb_bitmask_e
- *
- * returns
- *  CC_CAUSE_OK - success
- *  any other code - failure
- */
-cc_causes_t
-gsmsdp_add_rtcp_fb (int level, sdp_t *sdp_p,
-                    rtp_ptype codec, unsigned int types)
-{
-    int num_pts;
-    int pt_codec;
-    sdp_payload_ind_e indicator;
-
-    int pt_index;
-    unsigned int j;
-    num_pts = sdp_get_media_num_payload_types(sdp_p, level);
-    for (pt_index = 1; pt_index <= num_pts; pt_index++) {
-        pt_codec = sdp_get_media_payload_type (sdp_p, level, pt_index,
-                                               &indicator);
-        if ((pt_codec & 0xFF) == codec) {
-            int pt = GET_DYN_PAYLOAD_TYPE_VALUE(pt_codec);
-
-            /* Add requested a=rtcp-fb:nack attributes */
-            for (j = 0; j < SDP_MAX_RTCP_FB_NACK; j++) {
-                if (types & SDP_RTCP_FB_NACK_TO_BITMAP(j)) {
-                    gsmsdp_set_rtcp_fb_nack_attribute(level, sdp_p, pt, j);
-                }
-            }
-
-            /* Add requested a=rtcp-fb:ack attributes */
-            for (j = 0; j < SDP_MAX_RTCP_FB_ACK; j++) {
-                if (types & SDP_RTCP_FB_ACK_TO_BITMAP(j)) {
-                    gsmsdp_set_rtcp_fb_nack_attribute(level, sdp_p, pt, j);
-                }
-            }
-
-            /* Add requested a=rtcp-fb:ccm attributes */
-            for (j = 0; j < SDP_MAX_RTCP_FB_CCM; j++) {
-                if (types & SDP_RTCP_FB_CCM_TO_BITMAP(j)) {
-                    gsmsdp_set_rtcp_fb_ccm_attribute(level, sdp_p, pt, j);
-                }
-            }
-
-        }
-    }
-    return CC_CAUSE_OK;
-}
-
-
-/*
- * gsmsdp_negotiate_rtcp_fb
- *
- * Description:
- *  Negotiates a=rtcp-fb attributes to local SDP for supported video codecs
- *
- * Parameters:
- *   cc_sdp_p - local and remote SDP
- *   media    - The media structure for the current level to be negotiated
- *   offer    - True if the remote SDP is an offer
- *
- * returns
- *  CC_CAUSE_OK - success
- *  any other code - failure
- */
-cc_causes_t
-gsmsdp_negotiate_rtcp_fb (cc_sdp_t *cc_sdp_p,
-                          fsmdef_media_t *media,
-                          boolean offer)
-{
-    int level = media->level;
-    int pt_codec;
-    int remote_pt;
-    sdp_payload_ind_e indicator;
-    int pt_index, i;
-    sdp_rtcp_fb_nack_type_e nack_type;
-    sdp_rtcp_fb_ack_type_e ack_type;
-    sdp_rtcp_fb_ccm_type_e ccm_type;
-    uint32_t fb_types = 0;
-
-    int num_pts = sdp_get_media_num_payload_types(cc_sdp_p->dest_sdp, level);
-
-    /*
-     * Remove any previously negotiated rtcp-fb attributes from the
-     * local SDP
-     */
-    sdp_result_e result = SDP_SUCCESS;
-    while (result == SDP_SUCCESS) {
-        result = sdp_delete_attr (cc_sdp_p->src_sdp, level, 0,
-                                  SDP_ATTR_RTCP_FB, 1);
-    }
-
-    /*
-     * For each remote payload type, determine what feedback types are
-     * requested.
-     */
-    for (pt_index = 1; pt_index <= num_pts; pt_index++) {
-        int pt_codec = sdp_get_media_payload_type (cc_sdp_p->dest_sdp,
-                                                   level, pt_index, &indicator);
-        remote_pt = GET_DYN_PAYLOAD_TYPE_VALUE(pt_codec);
-        int codec = pt_codec & 0xFF;
-        fb_types = 0;
-
-        /* a=rtcp-fb:nack */
-        i = 1;
-        do {
-            nack_type = sdp_attr_get_rtcp_fb_nack(cc_sdp_p->dest_sdp,
-                                                  level, remote_pt, i);
-            if (nack_type >= 0 && nack_type < SDP_MAX_RTCP_FB_NACK) {
-                fb_types |= SDP_RTCP_FB_NACK_TO_BITMAP(nack_type);
-            }
-            i++;
-        } while (nack_type != SDP_RTCP_FB_NACK_NOT_FOUND);
-
-        /* a=rtcp-fb:ack */
-        i = 1;
-        do {
-            ack_type = sdp_attr_get_rtcp_fb_ack(cc_sdp_p->dest_sdp,
-                                                level, remote_pt, i);
-            if (ack_type >= 0 && ack_type < SDP_MAX_RTCP_FB_ACK) {
-                fb_types |= SDP_RTCP_FB_ACK_TO_BITMAP(ack_type);
-            }
-            i++;
-        } while (ack_type != SDP_RTCP_FB_CCM_NOT_FOUND);
-
-        /* a=rtcp-fb:ccm */
-        i = 1;
-        do {
-            ccm_type = sdp_attr_get_rtcp_fb_ccm(cc_sdp_p->dest_sdp,
-                                                level, remote_pt, i);
-            if (ccm_type >= 0 && ccm_type < SDP_MAX_RTCP_FB_CCM) {
-                fb_types |= SDP_RTCP_FB_CCM_TO_BITMAP(ccm_type);
-            }
-            i++;
-        } while (ccm_type != SDP_RTCP_FB_CCM_NOT_FOUND);
-
-        /*
-         * Mask out the types that we do not support
-         */
-        switch (codec) {
-            case RTP_VP8:
-                fb_types &=
-                  SDP_RTCP_FB_NACK_TO_BITMAP(SDP_RTCP_FB_NACK_BASIC) |
-                  SDP_RTCP_FB_NACK_TO_BITMAP(SDP_RTCP_FB_NACK_PLI) |
-                  SDP_RTCP_FB_CCM_TO_BITMAP(SDP_RTCP_FB_CCM_FIR);
-                break;
-            default:
-                fb_types = 0;
-        }
-
-        /*
-         * Now, in our local SDP, set rtcp-fb types that both we and the
-         * remote party support
-         */
-        if (fb_types) {
-            gsmsdp_add_rtcp_fb (level, cc_sdp_p->src_sdp, codec, fb_types);
-        }
-
-        /*
-         * Finally, update the media record for this payload type to
-         * reflect the expected feedback types
-         */
-        for (i = 0; i < media->num_payloads; i++) {
-            if (media->payloads[i].remote_rtp_pt == remote_pt) {
-                media->payloads[i].video.rtcp_fb_types = fb_types;
-            }
-        }
-    }
-
-    return CC_CAUSE_OK;
-}
-
-/*
  * gsmsdp_negotiate_media_lines
  *
  * Description:
@@ -4916,11 +4731,6 @@ gsmsdp_negotiate_media_lines (fsm_fcb_t *fcb_p, cc_sdp_t *sdp_p, boolean initial
                 update_local_ret_value = TRUE;
             }
 
-            /* Negotiate rtcp feedback mechanisms */
-            if (media && media_type == SDP_MEDIA_VIDEO) {
-                gsmsdp_negotiate_rtcp_fb (dcb_p->sdp, media, offer);
-            }
-
             /*
              * Negotiate rtcp-mux
              */
@@ -5003,6 +4813,19 @@ gsmsdp_negotiate_media_lines (fsm_fcb_t *fcb_p, cc_sdp_t *sdp_p, boolean initial
             /* Not a support media type stream */
             unsupported_line = TRUE;
             break;
+        }
+
+        /* TODO (abr) -- temporarily hardcode rtcb-fb attributes to match our
+           actual behavior. This really needs to be a negotiation, with the
+           results of the negotiation propagating into the codec configuration.
+           See Bug 880067. */
+        if (media && media_type == SDP_MEDIA_VIDEO) {
+            gsmsdp_set_rtcp_fb_nack_attribute(media->level, sdp_p->src_sdp,
+                                              SDP_ALL_PAYLOADS,
+                                              SDP_RTCP_FB_NACK_UNSPECIFIED);
+            gsmsdp_set_rtcp_fb_ccm_attribute(media->level, sdp_p->src_sdp,
+                                             SDP_ALL_PAYLOADS,
+                                             SDP_RTCP_FB_CCM_FIR);
         }
 
         if (unsupported_line) {
@@ -5448,14 +5271,6 @@ gsmsdp_add_media_line (fsmdef_dcb_t *dcb_p, const cc_media_cap_t *media_cap,
 
           gsmsdp_set_local_sdp_direction(dcb_p, media, media->direction);
 
-          /* Add supported rtcp-fb types */
-          if (media_cap->type == SDP_MEDIA_VIDEO) {
-              gsmsdp_add_rtcp_fb (level, dcb_p->sdp->src_sdp, RTP_VP8,
-                  SDP_RTCP_FB_NACK_TO_BITMAP(SDP_RTCP_FB_NACK_BASIC) |
-                  SDP_RTCP_FB_NACK_TO_BITMAP(SDP_RTCP_FB_NACK_PLI) |
-                  SDP_RTCP_FB_CCM_TO_BITMAP(SDP_RTCP_FB_CCM_FIR));
-          }
-
           /*
            * wait until here to set ICE candidates as SDP is now initialized
            */
@@ -5582,6 +5397,20 @@ gsmsdp_create_local_sdp (fsmdef_dcb_t *dcb_p, boolean force_streams_enabled,
                     level = level - 1;
                 }
             }
+
+            /* TODO (abr) -- temporarily hardcode rtcb-fb attributes to match
+               our actual behavior. This really needs to be a negotiation, with
+               the results of the negotiation propagating into the codec
+               configuration.  See Bug 880067. */
+            if (media_cap->type == SDP_MEDIA_VIDEO) {
+                gsmsdp_set_rtcp_fb_nack_attribute(level, dcb_p->sdp->src_sdp,
+                                                  SDP_ALL_PAYLOADS,
+                                                  SDP_RTCP_FB_NACK_UNSPECIFIED);
+                gsmsdp_set_rtcp_fb_ccm_attribute(level, dcb_p->sdp->src_sdp,
+                                                 SDP_ALL_PAYLOADS,
+                                                 SDP_RTCP_FB_CCM_FIR);
+            }
+
         }
         /* next capability */
         media_cap++;
