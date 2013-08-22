e19febf53b8804f6cac208f6a16c4383732ee2d6
Luke Wagner <luke@mozilla.com>
2013-08-22 13:54:34 -0500

    Bug 908255 - tidy up 'non-destructive' functions, give StringHashPolicy a scarier name (r=terrence)

--- js/src/vm/String.h
+++ js/src/vm/String.h
@@ -271,21 +271,21 @@ class JSString : public js::gc::Cell
     inline bool getChar(js::ExclusiveContext *cx, size_t index, jschar *code);
 
     /*
-     * Returns chars() if the string is already linear or flat. Otherwise
-     * returns NULL if a new array of chars must be allocated.
+     * A string has "pure" chars if it can return a pointer to its chars
+     * infallibly without mutating anything so they are safe to be from off the
+     * main thread. If a string does not have pure chars, the caller can call
+     * copyNonPureChars to allocate a copy of the chars which is also a
+     * non-mutating threadsafe operation. Beware, this is an O(n) operation
+     * (involving a DAG traversal for ropes).
      */
-    inline const jschar *maybeChars() const;
-    inline const jschar *maybeCharsZ() const;
-
-    /*
-     * Fallible operations to get an array of chars non-destructively. These
-     * operations are thread safe.
-     */
-
-    inline bool getCharsNonDestructive(js::ThreadSafeContext *cx,
-                                       js::ScopedJSFreePtr<jschar> &out) const;
-    inline bool getCharsZNonDestructive(js::ThreadSafeContext *cx,
-                                        js::ScopedJSFreePtr<jschar> &out) const;
+    bool hasPureChars() const { return isLinear(); }
+    bool hasPureCharsZ() const { return isFlat(); }
+    inline const jschar *pureChars() const;
+    inline const jschar *pureCharsZ() const;
+    inline bool copyNonPureChars(js::ThreadSafeContext *cx,
+                                 js::ScopedJSFreePtr<jschar> &out) const;
+    inline bool copyNonPureCharsZ(js::ThreadSafeContext *cx,
+                                  js::ScopedJSFreePtr<jschar> &out) const;
 
     /* Fallible conversions to more-derived string types. */
 
@@ -454,14 +454,11 @@ class JSString : public js::gc::Cell
 
 class JSRope : public JSString
 {
-    bool getCharsNonDestructiveInternal(js::ThreadSafeContext *cx,
-                                        js::ScopedJSFreePtr<jschar> &out,
-                                        bool nullTerminate) const;
-
-    bool getCharsNonDestructive(js::ThreadSafeContext *cx,
-                                js::ScopedJSFreePtr<jschar> &out) const;
-    bool getCharsZNonDestructive(js::ThreadSafeContext *cx,
-                                 js::ScopedJSFreePtr<jschar> &out) const;
+    bool copyNonPureCharsInternal(js::ThreadSafeContext *cx,
+                                  js::ScopedJSFreePtr<jschar> &out,
+                                  bool nullTerminate) const;
+    bool copyNonPureChars(js::ThreadSafeContext *cx, js::ScopedJSFreePtr<jschar> &out) const;
+    bool copyNonPureCharsZ(js::ThreadSafeContext *cx, js::ScopedJSFreePtr<jschar> &out) const;
 
     enum UsingBarrier { WithIncrementalBarrier, NoBarrier };
     template<UsingBarrier b>
@@ -527,8 +524,7 @@ JS_STATIC_ASSERT(sizeof(JSLinearString) == sizeof(JSString));
 
 class JSDependentString : public JSLinearString
 {
-    bool getCharsZNonDestructive(js::ThreadSafeContext *cx,
-                                 js::ScopedJSFreePtr<jschar> &out) const;
+    bool copyNonPureCharsZ(js::ThreadSafeContext *cx, js::ScopedJSFreePtr<jschar> &out) const;
 
     friend class JSString;
     JSFlatString *undepend(js::ExclusiveContext *cx);
@@ -1059,39 +1055,33 @@ JSString::getCharsZ(js::ExclusiveContext *cx)
 }
 
 JS_ALWAYS_INLINE const jschar *
-JSString::maybeChars() const
+JSString::pureChars() const
 {
-    if (isLinear())
-        return asLinear().chars();
-    return NULL;
+    JS_ASSERT(hasPureChars());
+    return asLinear().chars();
 }
 
 JS_ALWAYS_INLINE const jschar *
-JSString::maybeCharsZ() const
+JSString::pureCharsZ() const
 {
-    if (isFlat())
-        return asFlat().chars();
-    return NULL;
+    JS_ASSERT(hasPureCharsZ());
+    return asFlat().charsZ();
 }
 
 JS_ALWAYS_INLINE bool
-JSString::getCharsNonDestructive(js::ThreadSafeContext *cx,
-                                 js::ScopedJSFreePtr<jschar> &out) const
+JSString::copyNonPureChars(js::ThreadSafeContext *cx, js::ScopedJSFreePtr<jschar> &out) const
 {
-    /* If string is already linear, use maybeChars instead. */
-    JS_ASSERT(!isLinear());
-    return asRope().getCharsNonDestructive(cx, out);
+    JS_ASSERT(!hasPureChars());
+    return asRope().copyNonPureChars(cx, out);
 }
 
 JS_ALWAYS_INLINE bool
-JSString::getCharsZNonDestructive(js::ThreadSafeContext *cx,
-                                  js::ScopedJSFreePtr<jschar> &out) const
+JSString::copyNonPureCharsZ(js::ThreadSafeContext *cx, js::ScopedJSFreePtr<jschar> &out) const
 {
-    /* If string is already flat, use maybeCharsZ instead. */
-    JS_ASSERT(!isFlat());
+    JS_ASSERT(!hasPureChars());
     if (isDependent())
-        return asDependent().getCharsZNonDestructive(cx, out);
-    return asRope().getCharsZNonDestructive(cx, out);
+        return asDependent().copyNonPureCharsZ(cx, out);
+    return asRope().copyNonPureCharsZ(cx, out);
 }
 
 JS_ALWAYS_INLINE JSLinearString *
