38dd8e648531ebb69228d450ae1f7f5206977583
Nicholas Nethercote <nnethercote@mozilla.com>
2013-08-21 22:26:56 -0700

    Bug 909178 (part 1) - Move |jsid| from jsapi.h into js/Id.h.  r=luke.

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -23,6 +23,7 @@
 
 #include "js/CallArgs.h"
 #include "js/HashTable.h"
+#include "js/Id.h"
 #include "js/RootingAPI.h"
 #include "js/Utility.h"
 #include "js/Value.h"
@@ -1160,47 +1161,6 @@ JS_NumberValue(double d)
 
 /************************************************************************/
 
-/*
- * A jsid is an identifier for a property or method of an object which is
- * either a 31-bit signed integer, interned string or object.  Also, there is
- * an additional jsid value, JSID_VOID, which does not occur in JS scripts but
- * may be used to indicate the absence of a valid jsid.
- *
- * A jsid is not implicitly convertible to or from a jsval; JS_ValueToId or
- * JS_IdToValue must be used instead.
- */
-
-#define JSID_TYPE_STRING                 0x0
-#define JSID_TYPE_INT                    0x1
-#define JSID_TYPE_VOID                   0x2
-#define JSID_TYPE_OBJECT                 0x4
-#define JSID_TYPE_MASK                   0x7
-
-/*
- * Avoid using canonical 'id' for jsid parameters since this is a magic word in
- * Objective-C++ which, apparently, wants to be able to #include jsapi.h.
- */
-#define id iden
-
-static JS_ALWAYS_INLINE bool
-JSID_IS_STRING(jsid id)
-{
-    return (JSID_BITS(id) & JSID_TYPE_MASK) == 0;
-}
-
-static JS_ALWAYS_INLINE JSString *
-JSID_TO_STRING(jsid id)
-{
-    JS_ASSERT(JSID_IS_STRING(id));
-    return (JSString *)JSID_BITS(id);
-}
-
-static JS_ALWAYS_INLINE bool
-JSID_IS_ZERO(jsid id)
-{
-    return JSID_BITS(id) == 0;
-}
-
 JS_PUBLIC_API(bool)
 JS_StringHasBeenInterned(JSContext *cx, JSString *str);
 
@@ -1213,104 +1173,6 @@ JS_StringHasBeenInterned(JSContext *cx, JSString *str);
  */
 JS_PUBLIC_API(jsid)
 INTERNED_STRING_TO_JSID(JSContext *cx, JSString *str);
-
-static JS_ALWAYS_INLINE bool
-JSID_IS_INT(jsid id)
-{
-    return !!(JSID_BITS(id) & JSID_TYPE_INT);
-}
-
-static JS_ALWAYS_INLINE int32_t
-JSID_TO_INT(jsid id)
-{
-    JS_ASSERT(JSID_IS_INT(id));
-    return ((uint32_t)JSID_BITS(id)) >> 1;
-}
-
-#define JSID_INT_MIN  0
-#define JSID_INT_MAX  INT32_MAX
-
-static JS_ALWAYS_INLINE bool
-INT_FITS_IN_JSID(int32_t i)
-{
-    return i >= 0;
-}
-
-static JS_ALWAYS_INLINE jsid
-INT_TO_JSID(int32_t i)
-{
-    jsid id;
-    JS_ASSERT(INT_FITS_IN_JSID(i));
-    JSID_BITS(id) = ((i << 1) | JSID_TYPE_INT);
-    return id;
-}
-
-static JS_ALWAYS_INLINE bool
-JSID_IS_OBJECT(jsid id)
-{
-    return (JSID_BITS(id) & JSID_TYPE_MASK) == JSID_TYPE_OBJECT &&
-           (size_t)JSID_BITS(id) != JSID_TYPE_OBJECT;
-}
-
-static JS_ALWAYS_INLINE JSObject *
-JSID_TO_OBJECT(jsid id)
-{
-    JS_ASSERT(JSID_IS_OBJECT(id));
-    return (JSObject *)(JSID_BITS(id) & ~(size_t)JSID_TYPE_MASK);
-}
-
-static JS_ALWAYS_INLINE jsid
-OBJECT_TO_JSID(JSObject *obj)
-{
-    jsid id;
-    JS_ASSERT(obj != NULL);
-    JS_ASSERT(((size_t)obj & JSID_TYPE_MASK) == 0);
-    JSID_BITS(id) = ((size_t)obj | JSID_TYPE_OBJECT);
-    return id;
-}
-
-static JS_ALWAYS_INLINE bool
-JSID_IS_GCTHING(jsid id)
-{
-    return JSID_IS_STRING(id) || JSID_IS_OBJECT(id);
-}
-
-static JS_ALWAYS_INLINE void *
-JSID_TO_GCTHING(jsid id)
-{
-    return (void *)(JSID_BITS(id) & ~(size_t)JSID_TYPE_MASK);
-}
-
-/*
- * A void jsid is not a valid id and only arises as an exceptional API return
- * value, such as in JS_NextProperty. Embeddings must not pass JSID_VOID into
- * JSAPI entry points expecting a jsid and do not need to handle JSID_VOID in
- * hooks receiving a jsid except when explicitly noted in the API contract.
- */
-
-static JS_ALWAYS_INLINE bool
-JSID_IS_VOID(const jsid id)
-{
-    JS_ASSERT_IF(((size_t)JSID_BITS(id) & JSID_TYPE_MASK) == JSID_TYPE_VOID,
-                 JSID_BITS(id) == JSID_TYPE_VOID);
-    return ((size_t)JSID_BITS(id) == JSID_TYPE_VOID);
-}
-
-static JS_ALWAYS_INLINE bool
-JSID_IS_EMPTY(const jsid id)
-{
-    return ((size_t)JSID_BITS(id) == JSID_TYPE_OBJECT);
-}
-
-#undef id
-
-#ifdef JS_USE_JSID_STRUCT_TYPES
-extern JS_PUBLIC_DATA(const jsid) JSID_VOID;
-extern JS_PUBLIC_DATA(const jsid) JSID_EMPTY;
-#else
-# define JSID_VOID ((jsid)JSID_TYPE_VOID)
-# define JSID_EMPTY ((jsid)JSID_TYPE_OBJECT)
-#endif
 
 /*
  * Returns true iff the given jsval is immune to GC and can be used across
