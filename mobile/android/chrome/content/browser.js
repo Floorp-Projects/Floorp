eb07c8dcb3548fc8ae068abb64c4c20d9270fe66
Tim Taubert <ttaubert@mozilla.com>
2013-08-22 09:22:16 +0200

    Backed out changeset 91d27f0ac7dd (bug 355063)

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -2670,7 +2670,6 @@ Tab.prototype = {
     this.browser.sessionHistory.addSHistoryListener(this);
 
     this.browser.addEventListener("DOMContentLoaded", this, true);
-    this.browser.addEventListener("DOMFormHasPassword", this, true);
     this.browser.addEventListener("DOMLinkAdded", this, true);
     this.browser.addEventListener("DOMTitleChanged", this, true);
     this.browser.addEventListener("DOMWindowClose", this, true);
@@ -2819,7 +2818,6 @@ Tab.prototype = {
     this.browser.sessionHistory.removeSHistoryListener(this);
 
     this.browser.removeEventListener("DOMContentLoaded", this, true);
-    this.browser.removeEventListener("DOMFormHasPassword", this, true);
     this.browser.removeEventListener("DOMLinkAdded", this, true);
     this.browser.removeEventListener("DOMTitleChanged", this, true);
     this.browser.removeEventListener("DOMWindowClose", this, true);
@@ -3374,11 +3372,6 @@ Tab.prototype = {
         if (docURI.startsWith("about:reader"))
           new AboutReader(this.browser.contentDocument, this.browser.contentWindow);
 
-        break;
-      }
-
-      case "DOMFormHasPassword": {
-        LoginManagerContent.onFormPassword(aEvent);
         break;
       }
 
