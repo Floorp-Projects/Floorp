166bf211b327631c9863819c10dac758ac03b78f
Guillaume Abadie <gabadie@mozilla.com>
2013-08-21 21:11:03 -0400

    bug 903480 - [WebGL 2.0] implement TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN - r=jgilbert

--- content/canvas/src/WebGLContext.h
+++ content/canvas/src/WebGLContext.h
@@ -737,7 +737,10 @@ public:
     JS::Value GetQueryObject(JSContext* cx, WebGLQuery *query, WebGLenum pname);
 
 private:
-    bool ValidateTargetParameter(WebGLenum target, const char* infos);
+    WebGLRefPtr<WebGLQuery> mActiveOcclusionQuery;
+    WebGLRefPtr<WebGLQuery> mActiveTransformFeedbackQuery;
+
+    bool ValidateQueryTargetParameter(WebGLenum target, const char* infos);
     WebGLRefPtr<WebGLQuery>& GetActiveQueryByTarget(WebGLenum target);
 
 // -----------------------------------------------------------------------------
@@ -1137,7 +1140,6 @@ protected:
     WebGLRefPtr<WebGLFramebuffer> mBoundFramebuffer;
     WebGLRefPtr<WebGLRenderbuffer> mBoundRenderbuffer;
     WebGLRefPtr<WebGLVertexArray> mBoundVertexArray;
-    WebGLRefPtr<WebGLQuery> mActiveOcclusionQuery;
 
     LinkedList<WebGLTexture> mTextures;
     LinkedList<WebGLBuffer> mBuffers;
