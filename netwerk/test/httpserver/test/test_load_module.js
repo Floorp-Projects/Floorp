40dea9f967afce6ac72e4a6b1a60373ad6f4311d
Geoff Brown <gbrown@mozilla.com>
2013-08-23 09:11:34 -0600

    Bug 905344 - Change server port to -1 in test_load_module.js; r=waldo

--- netwerk/test/httpserver/test/test_load_module.js
+++ netwerk/test/httpserver/test/test_load_module.js
@@ -8,7 +8,7 @@ function run_test() {
   Components.utils.import("resource://testing-common/httpd.js");
 
   let server = new HttpServer();
-  server.start(8080);
+  server.start(-1);
 
   do_test_pending();
 
