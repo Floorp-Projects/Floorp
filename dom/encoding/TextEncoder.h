2e79ae8ee45bb8838e5ad4aead3daadbfafdfc2f
Kyle Huey <khuey@kylehuey.com>
2013-08-22 22:17:10 -0700

    Bug 903772: Part 8 - Remove TextEncoderBase. r=emk

--- dom/encoding/TextEncoder.h
+++ dom/encoding/TextEncoder.h
@@ -6,14 +6,16 @@
 #define mozilla_dom_textencoder_h_
 
 #include "mozilla/dom/NonRefcountedDOMObject.h"
-#include "mozilla/dom/TextEncoderBase.h"
 #include "mozilla/dom/TextEncoderBinding.h"
+#include "mozilla/dom/TypedArray.h"
+#include "nsIUnicodeEncoder.h"
 
 namespace mozilla {
+class ErrorResult;
+
 namespace dom {
 
-class TextEncoder MOZ_FINAL
-  : public NonRefcountedDOMObject, public TextEncoderBase
+class TextEncoder MOZ_FINAL : public NonRefcountedDOMObject
 {
 public:
   // The WebIDL constructor.
@@ -56,10 +58,60 @@ public:
                    const nsAString& aString,
                    const TextEncodeOptions& aOptions,
                    ErrorResult& aRv) {
-    return TextEncoderBase::Encode(aCx, aObj, aString, aOptions.mStream, aRv);
+    return TextEncoder::Encode(aCx, aObj, aString, aOptions.mStream, aRv);
+  }
+
+protected:
+
+  /**
+   * Validates provided encoding and throws an exception if invalid encoding.
+   * If no encoding is provided then mEncoding is default initialised to "utf-8".
+   *
+   * @param aEncoding    Optional encoding (case insensitive) provided.
+   *                     (valid values are "utf-8", "utf-16", "utf-16be")
+   *                     Default value is "utf-8" if no encoding is provided.
+   * @return aRv         EncodingError exception else null.
+   */
+  void Init(const nsAString& aEncoding, ErrorResult& aRv);
+
+public:
+  /**
+   * Return the encoding name.
+   *
+   * @param aEncoding, current encoding.
+   */
+  void GetEncoding(nsAString& aEncoding);
+
+  /**
+   * Encodes incoming utf-16 code units/ DOM string to the requested encoding.
+   *
+   * @param aCx        Javascript context.
+   * @param aObj       the wrapper of the TextEncoder
+   * @param aString    utf-16 code units to be encoded.
+   * @param aOptions   Streaming option. Initialised by default to false.
+   *                   If the streaming option is false, then the encoding
+   *                   algorithm state will get reset. If set to true then
+   *                   the previous encoding is reused/continued.
+   * @return JSObject* The Uint8Array wrapped in a JS object.
+   */
+  JSObject* Encode(JSContext* aCx,
+                   JS::Handle<JSObject*> aObj,
+                   const nsAString& aString,
+                   const bool aStream,
+                   ErrorResult& aRv);
+
+protected:
+  JSObject*
+  CreateUint8Array(JSContext* aCx, JS::Handle<JSObject*> aObj, 
+                   char* aBuf, uint32_t aLen) const
+  {
+    return Uint8Array::Create(aCx, aObj, aLen,
+                              reinterpret_cast<uint8_t*>(aBuf));
   }
 
 private:
+  nsCString mEncoding;
+  nsCOMPtr<nsIUnicodeEncoder> mEncoder;
 };
 
 } // dom
