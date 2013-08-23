8a0b4fdb06ad04757d15d7aa105b316ebbd3e788
Sam Foster <sfoster@mozilla.com>
2013-08-23 13:58:55 -0700

    Bug 848137 - Begin break out of Utils.js into a ContentUtil.jsm module. r=mbrubeck

--- browser/metro/base/content/Util.js
+++ browser/metro/base/content/Util.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+Components.utils.import("resource:///modules/ContentUtil.jsm");
+
 let Util = {
   /*
    * General purpose utilities
@@ -35,44 +37,6 @@ let Util = {
   // auto-connection process.
   forceOnline: function forceOnline() {
     Services.io.offline = false;
-  },
-
-  // Pass several objects in and it will combine them all into the first object and return it.
-  // NOTE: Deep copy is not supported
-  extend: function extend() {
-    // copy reference to target object
-    let target = arguments[0] || {};
-    let length = arguments.length;
-
-    if (length === 1) {
-      return target;
-    }
-
-    // Handle case when target is a string or something
-    if (typeof target != "object" && typeof target != "function") {
-      target = {};
-    }
-
-    for (let i = 1; i < length; i++) {
-      // Only deal with non-null/undefined values
-      let options = arguments[i];
-      if (options != null) {
-        // Extend the base object
-        for (let name in options) {
-          let copy = options[name];
-
-          // Prevent never-ending loop
-          if (target === copy)
-            continue;
-
-          if (copy !== undefined)
-            target[name] = copy;
-        }
-      }
-    }
-
-    // Return the modified object
-    return target;
   },
 
   /*
@@ -540,3 +504,11 @@ Util.Timeout.prototype = {
   }
 };
 
+// Mixin the ContentUtil module exports
+{
+  for (let name in ContentUtil) {
+    let copy = ContentUtil[name];
+    if (copy !== undefined)
+      Util[name] = copy;
+  }
+}
