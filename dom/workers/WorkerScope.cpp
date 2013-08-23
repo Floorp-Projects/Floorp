ab6f97b733431e15454ec8e8a30593697dee9e48
Kyle Huey <khuey@kylehuey.com>
2013-08-22 22:17:10 -0700

    Bug 903772: Part 7 - Remove the worker thread TextEncoder implementation. r=emk

--- dom/workers/WorkerScope.cpp
+++ dom/workers/WorkerScope.cpp
@@ -1021,7 +1021,7 @@ CreateDedicatedWorkerGlobalScope(JSContext* aCx)
   if (!FileReaderSyncBinding_workers::GetConstructorObject(aCx, global) ||
       !ImageDataBinding::GetConstructorObject(aCx, global) ||
       !TextDecoderBinding::GetConstructorObject(aCx, global) ||
-      !TextEncoderBinding_workers::GetConstructorObject(aCx, global) ||
+      !TextEncoderBinding::GetConstructorObject(aCx, global) ||
       !XMLHttpRequestBinding_workers::GetConstructorObject(aCx, global) ||
       !XMLHttpRequestUploadBinding_workers::GetConstructorObject(aCx, global) ||
       !URLBinding_workers::GetConstructorObject(aCx, global) ||
