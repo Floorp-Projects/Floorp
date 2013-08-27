63d2c82af275b3743f3c9699cb8083fbaa2b4946
Bobby Holley <bobbyholley@gmail.com>
2013-08-27 15:21:39 -0700

    Bug 899367 - Have nsGlobalWindow go through nsJSContext to access the outer. r=mccr8

--- dom/base/nsJSEnvironment.h
+++ dom/base/nsJSEnvironment.h
@@ -80,6 +80,9 @@ public:
   virtual void WillInitializeContext() MOZ_OVERRIDE;
   virtual void DidInitializeContext() MOZ_OVERRIDE;
 
+  virtual void SetWindowProxy(JS::Handle<JSObject*> aWindowProxy) MOZ_OVERRIDE;
+  virtual JSObject* GetWindowProxy() MOZ_OVERRIDE;
+
   static void LoadStart();
   static void LoadEnd();
 
