30462584f98ebe952ea2ff94803005ab02692889
Chenxia Liu <liuche@mozilla.com>
2013-08-27 11:31:19 -0700

    Bug 906668 - [fig] Reading list text is cut off on small screens. r=sriram

--- mobile/android/base/home/ReadingListPage.java
+++ mobile/android/base/home/ReadingListPage.java
@@ -29,7 +29,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
 import android.widget.AdapterView;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
