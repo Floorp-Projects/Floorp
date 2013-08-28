36a553b84d8e04e38480f0787633af265e818f12
Boris Zbarsky <bzbarsky@mit.edu>
2013-08-28 00:14:57 -0400

    Bug 909645 part 3.  Make including SpeechRecognition.h and MediaManager.h not pull ipdl headers.  r=ms2ger

--- content/media/webspeech/recognition/test/FakeSpeechRecognitionService.cpp
+++ content/media/webspeech/recognition/test/FakeSpeechRecognitionService.cpp
@@ -12,6 +12,8 @@
 #include "SpeechRecognitionAlternative.h"
 #include "SpeechRecognitionResult.h"
 #include "SpeechRecognitionResultList.h"
+#include "nsIObserverService.h"
+#include "mozilla/Services.h"
 
 namespace mozilla {
 
