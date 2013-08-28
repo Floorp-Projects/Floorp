056bbb71e71a7cb756da972051a27728bf646b15
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2013-08-28 10:30:02 -0400

    Bug 909927 - Minimize the #includes in layout/forms; r=mats

--- layout/forms/nsSelectsAreaFrame.cpp
+++ layout/forms/nsSelectsAreaFrame.cpp
@@ -3,8 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include "nsSelectsAreaFrame.h"
-#include "nsCOMPtr.h"
-#include "nsIDOMHTMLOptionElement.h"
 #include "nsIContent.h"
 #include "nsListControlFrame.h"
 #include "nsDisplayList.h"
