66b0dc9b6252c8049c315cf868da634434f91887
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2013-08-28 08:10:55 -0400

    Bug 909573 - getManifestFor should accept a manifest url instead of an origin. r=fabrice

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -743,7 +743,7 @@ var AlertsHelper = {
     this._listeners[uid] = listener;
 
     let app = DOMApplicationRegistry.getAppByManifestURL(listener.manifestURL);
-    DOMApplicationRegistry.getManifestFor(app.origin, function(manifest) {
+    DOMApplicationRegistry.getManifestFor(app.manifestURL, function(manifest) {
       let helper = new ManifestHelper(manifest, app.origin);
       let getNotificationURLFor = function(messages) {
         if (!messages)
@@ -800,7 +800,7 @@ var AlertsHelper = {
     // If we have a manifest URL, get the icon and title from the manifest
     // to prevent spoofing.
     let app = DOMApplicationRegistry.getAppByManifestURL(manifestUrl);
-    DOMApplicationRegistry.getManifestFor(app.origin, function(aManifest) {
+    DOMApplicationRegistry.getManifestFor(manifestUrl, function(aManifest) {
       let helper = new ManifestHelper(aManifest, app.origin);
       send(helper.name, helper.iconURLForSize(128));
     });
@@ -893,7 +893,7 @@ var WebappsHelper = {
 
     switch(topic) {
       case "webapps-launch":
-        DOMApplicationRegistry.getManifestFor(json.origin, function(aManifest) {
+        DOMApplicationRegistry.getManifestFor(json.manifestURL, function(aManifest) {
           if (!aManifest)
             return;
 
