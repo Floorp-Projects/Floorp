d9479c6586a465778f573737c35685bf60c0489b
Ralph Giles <giles@mozilla.com>
2013-08-28 15:39:26 -0700

    Bug 865256 - Part 4: Implement custom waveforms. r=ehsan,padenot; a=lsblakk
    Copy the periodicWave data into a shareable buffer.
    
    Add a SetBuffer call to send the buffer data to the
    OscillatorNodeEngine. Call into blink's PeriodicWave
    implementation to generate bandlimited tables bracketing
    the desired frequency and interpolate the output data
    from them.
    
    Change the PeriodicWave constructor to only take one
    length, since both arrays must be the same size.
    
    Change OscillatorNode's SetType to throw INVALID_STATE
    instead of NOT_IMPLEMENTED if js tries to assign
    type = 'custom' directly.

--- content/media/webaudio/PeriodicWave.cpp
+++ content/media/webaudio/PeriodicWave.cpp
@@ -18,13 +18,30 @@ NS_IMPL_CYCLE_COLLECTION_UNROOT_NATIVE(PeriodicWave, Release)
 
 PeriodicWave::PeriodicWave(AudioContext* aContext,
                            const float* aRealData,
-                           uint32_t aRealDataLength,
                            const float* aImagData,
-                           uint32_t aImagDataLength)
+                           const uint32_t aLength,
+                           ErrorResult& aRv)
   : mContext(aContext)
 {
   MOZ_ASSERT(aContext);
   SetIsDOMBinding();
+
+  // Caller should have checked this and thrown.
+  MOZ_ASSERT(aLength > 0);
+  MOZ_ASSERT(aLength <= 4096);
+  mLength = aLength;
+
+  // Copy coefficient data. The two arrays share an allocation.
+  mCoefficients = new ThreadSharedFloatArrayBufferList(2);
+  float* buffer = static_cast<float*>(malloc(aLength*sizeof(float)*2));
+  if (buffer == nullptr) {
+    aRv.Throw(NS_ERROR_OUT_OF_MEMORY);
+    return;
+  }
+  PodCopy(buffer, aRealData, aLength);
+  mCoefficients->SetData(0, buffer, buffer);
+  PodCopy(buffer+aLength, aImagData, aLength);
+  mCoefficients->SetData(1, nullptr, buffer+aLength);
 }
 
 JSObject*
@@ -33,6 +50,6 @@ PeriodicWave::WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope)
   return PeriodicWaveBinding::Wrap(aCx, aScope, this);
 }
 
-}
-}
+} // namespace dom
+} // namespace mozilla
 
