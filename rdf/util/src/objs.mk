2b057f3b531660d982db7d57d89774cd93d8c63f
Brian O'Keefe <bokeefe@alum.wpi.edu>
2013-08-30 09:09:06 -0400

    Bug 865673 - Convert objs.mk to objs.mozbuild. r=gps

--- rdf/util/src/objs.mk
+++
@@ -1,9 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-RDF_UTIL_SRC_LCPPSRCS = \
-	nsRDFResource.cpp \
-	$(NULL)
-
-RDF_UTIL_SRC_CPPSRCS = $(addprefix $(topsrcdir)/rdf/util/src/, $(RDF_UTIL_SRC_LCPPSRCS))
