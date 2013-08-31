3894bec6d4ad46c180ae70c436de0c8cb59b7deb
Ryan VanderMeulen <ryanvm@gmail.com>
2013-08-30 20:12:06 -0400

    Backed out changeset e90d6be6dbf6 (bug 886329) for test failures. a=backout

--- addon-sdk/source/lib/sdk/panel/utils.js
+++ addon-sdk/source/lib/sdk/panel/utils.js
@@ -246,12 +246,7 @@ function make(document) {
   let viewFrame = createFrame(panel, frameOptions);
   setupPanelFrame(viewFrame);
 
-  function onDisplayChange({type, target}) {
-    // Events from child element like <select /> may propagate (dropdowns are
-    // popups too), in which case frame loader shouldn't be swapped.
-    // See Bug 886329
-    if (target !== this) return;
-
+  function onDisplayChange({type}) {
     try { swapFrameLoaders(backgroundFrame, viewFrame); }
     catch(error) { console.exception(error); }
     events.emit(EVENT_NAMES[type], { subject: panel });
