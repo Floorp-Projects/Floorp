6914706b30a08a8d242c535459cc23515c5c9b4c
Chris Double <chris.double@double.co.nz>
2013-09-03 14:39:41 +1200

    Bug 845734 - Remove Sony devices from JellyBean Stagefright blocklist - r=bjacob

--- widget/android/GfxInfo.cpp
+++ widget/android/GfxInfo.cpp
@@ -467,8 +467,6 @@ GfxInfo::GetFeatureStatusImpl(int32_t aFeature,
         // Blocklist:
         //   Samsung devices from bug 812881 and 853522.
         //   Motorola XT890 from bug 882342.
-        //   All Sony devices (Bug 845734)
-
         bool isBlocklisted =
           cModel.Find("GT-P3100", true) != -1 ||
           cModel.Find("GT-P3110", true) != -1 ||
@@ -476,8 +474,7 @@ GfxInfo::GetFeatureStatusImpl(int32_t aFeature,
           cModel.Find("GT-P5100", true) != -1 ||
           cModel.Find("GT-P5110", true) != -1 ||
           cModel.Find("GT-P5113", true) != -1 ||
-          cModel.Find("XT890", true) != -1 ||
-          cManufacturer.Find("Sony", true) != -1;
+          cModel.Find("XT890", true) != -1;
 
         if (isBlocklisted) {
           *aStatus = nsIGfxInfo::FEATURE_BLOCKED_DEVICE;
