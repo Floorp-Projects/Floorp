d28ff95c206d472cd8f4c420f6f8d456750ea6bf
Steve Fink <sfink@mozilla.com>
2013-09-04 16:35:01 -0700

    Bug 916988 - AutoCompartment constructor leads to too many false positives, r=terrence
    --HG--
    extra : rebase_source : 196d35acf9e9cb41743373f5dacbdff5890bc7ef

--- js/src/devtools/rootAnalysis/annotations.js
+++ js/src/devtools/rootAnalysis/annotations.js
@@ -143,6 +143,10 @@ var ignoreFunctions = {
     // up wrapping a pending exception. See bug 898815 for the heavyweight fix.
     "void js::AutoCompartment::~AutoCompartment(int32)" : true,
     "void JSAutoCompartment::~JSAutoCompartment(int32)" : true,
+
+    // And these are workarounds to avoid even more analysis work,
+    // which would sadly still be needed even with bug 898815.
+    "void js::AutoCompartment::AutoCompartment(js::ExclusiveContext*, JSCompartment*)": true,
 };
 
 function ignoreGCFunction(fun)
