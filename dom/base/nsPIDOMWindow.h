bae165774214716cf70c08f8f55395937e291450
Robert O'Callahan <robert@ocallahan.org>
2013-09-04 23:37:46 +1200

    Bug 911889. Part 4. Add API to detect whether an nsGlobalWindow is running a timeout handler. r=bz
    --HG--
    extra : rebase_source : b20f70c0cb287fa3766f91172b99ad1c16329f8f

--- dom/base/nsPIDOMWindow.h
+++ dom/base/nsPIDOMWindow.h
@@ -189,6 +189,8 @@ public:
     return mDoc;
   }
 
+  virtual NS_HIDDEN_(bool) IsRunningTimeout() = 0;
+
 protected:
   // Lazily instantiate an about:blank document if necessary, and if
   // we have what it takes to do so.
