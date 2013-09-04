ad68f1c90849b5a228efc41d0c94e6ca79a5b742
David Burns <dburns@mozilla.com>
2013-09-04 09:05:43 +0100

    Bug 912244: Correct screenshot with element highlighting in Marionette; r=jgriffin

--- testing/marionette/client/marionette/tests/unit/test_screenshot.py
+++ testing/marionette/client/marionette/tests/unit/test_screenshot.py
@@ -10,6 +10,13 @@ class ScreenshotTests(MarionetteTestCase):
         self.assertEqual('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAVUlEQVRoge3PsQ0AIAzAsI78fzBwBhHykD2ePev80LweAAGJB1ILpBZILZBaILVAaoHUAqkFUgukFkgtkFogtUBqgdQCqQVSC6QWSC2QWiC1QGp9A7ma+7nyXgOpzQAAAABJRU5ErkJggg==',
                          self.marionette.screenshot(element=el))
 
+    def testWeCanTakeAScreenShotWithHighlightOfAnElement(self):
+        test_url = self.marionette.absolute_url('html5Page.html')
+        self.marionette.navigate(test_url)
+        el = self.marionette.find_element('id', 'green')
+        self.assertEqual('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAV0lEQVRoge3PQRGAQAwAsWINvXgsNnI3+4iAzM7sDWZn9vneoxXRFNEU0RTRFNEU0RTRFNEU0RTRFNEU0RTRFNEU0RTRFNEU0RTRFNEU0RTRFNHcF7nBD/Ha5Ye4BbsYAAAAAElFTkSuQmCC',
+                         self.marionette.screenshot(element=el, highlights=[el]))
+
     def testWeCanTakeAScreenShotEntireCanvas(self):
         test_url = self.marionette.absolute_url('html5Page.html')
         self.marionette.navigate(test_url)
