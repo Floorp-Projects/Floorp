a8458bf4b4dddcecab8517c511928f1aa00cb480
Andrea Marchesini <amarchesini@mozilla.com>
2013-09-04 13:07:21 -0400

    Bug 887364 - URL API for main thread. r=ehsan

--- dom/base/URL.h
+++ dom/base/URL.h
@@ -5,11 +5,15 @@
 #ifndef URL_h___
 #define URL_h___
 
-#include "nscore.h"
+#include "mozilla/dom/BindingDeclarations.h"
+#include "nsCycleCollectionParticipant.h"
+#include "nsAutoPtr.h"
 #include "nsString.h"
 
 class nsIDOMBlob;
 class nsISupports;
+class nsIURI;
+class nsPIDOMWindow;
 
 namespace mozilla {
 
@@ -25,7 +29,27 @@ struct objectURLOptions;
 class URL MOZ_FINAL
 {
 public:
+  NS_INLINE_DECL_CYCLE_COLLECTING_NATIVE_REFCOUNTING(URL)
+  NS_DECL_CYCLE_COLLECTION_NATIVE_CLASS(URL)
+
+  URL(nsPIDOMWindow* aWindow, nsIURI* aURI);
+
   // WebIDL methods
+  nsPIDOMWindow* GetParentObject() const
+  {
+    return mWindow;
+  }
+
+  JSObject*
+  WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope);
+
+  static already_AddRefed<URL>
+  Constructor(const GlobalObject& aGlobal, const nsAString& aUrl,
+              URL& aBase, ErrorResult& aRv);
+  static already_AddRefed<URL>
+  Constructor(const GlobalObject& aGlobal, const nsAString& aUrl,
+              const nsAString& aBase, ErrorResult& aRv);
+
   static void CreateObjectURL(const GlobalObject& aGlobal,
                               nsIDOMBlob* aBlob,
                               const objectURLOptions& aOptions,
@@ -33,23 +57,73 @@ public:
                               ErrorResult& aError);
   static void CreateObjectURL(const GlobalObject& aGlobal,
                               DOMMediaStream& aStream,
-                              const mozilla::dom::objectURLOptions& aOptions,
+                              const objectURLOptions& aOptions,
                               nsString& aResult,
-                              mozilla::ErrorResult& aError);
+                              ErrorResult& aError);
   static void CreateObjectURL(const GlobalObject& aGlobal,
                               MediaSource& aSource,
                               const objectURLOptions& aOptions,
                               nsString& aResult,
-                              mozilla::ErrorResult& aError);
+                              ErrorResult& aError);
   static void RevokeObjectURL(const GlobalObject& aGlobal,
                               const nsAString& aURL);
 
+  void GetHref(nsString& aHref) const;
+
+  void SetHref(const nsAString& aHref, ErrorResult& aRv);
+
+  void GetOrigin(nsString& aOrigin) const;
+
+  void GetProtocol(nsString& aProtocol) const;
+
+  void SetProtocol(const nsAString& aProtocol);
+
+  void GetUsername(nsString& aUsername) const;
+
+  void SetUsername(const nsAString& aUsername);
+
+  void GetPassword(nsString& aPassword) const;
+
+  void SetPassword(const nsAString& aPassword);
+
+  void GetHost(nsString& aHost) const;
+
+  void SetHost(const nsAString& aHost);
+
+  void GetHostname(nsString& aHostname) const;
+
+  void SetHostname(const nsAString& aHostname);
+
+  void GetPort(nsString& aPort) const;
+
+  void SetPort(const nsAString& aPort);
+
+  void GetPathname(nsString& aPathname) const;
+
+  void SetPathname(const nsAString& aPathname);
+
+  void GetSearch(nsString& aRetval) const;
+
+  void SetSearch(const nsAString& aArg);
+
+  void GetHash(nsString& aRetval) const;
+
+  void SetHash(const nsAString& aArg);
+
 private:
+  nsIURI* GetURI() const
+  {
+    return mURI;
+  }
+
   static void CreateObjectURLInternal(nsISupports* aGlobal, nsISupports* aObject,
                                       const nsACString& aScheme,
-                                      const mozilla::dom::objectURLOptions& aOptions,
+                                      const objectURLOptions& aOptions,
                                       nsString& aResult,
-                                      mozilla::ErrorResult& aError);
+                                      ErrorResult& aError);
+
+  nsRefPtr<nsPIDOMWindow> mWindow;
+  nsCOMPtr<nsIURI> mURI;
 };
 
 }
--- dom/workers/URL.h
+++ dom/workers/URL.h
@@ -16,6 +16,13 @@ BEGIN_WORKERS_NAMESPACE
 class URL : public EventTarget
 {
 public: // Methods for WebIDL
+  static URL*
+  Constructor(const GlobalObject& aGlobal, const nsAString& aUrl,
+              URL& aBase, ErrorResult& aRv);
+  static URL*
+  Constructor(const GlobalObject& aGlobal, const nsAString& aUrl,
+              const nsAString& aBase, ErrorResult& aRv);
+
   static void
   CreateObjectURL(const GlobalObject& aGlobal,
                   JSObject* aArg, const objectURLOptions& aOptions,
@@ -28,6 +35,56 @@ public: // Methods for WebIDL
 
   static void
   RevokeObjectURL(const GlobalObject& aGlobal, const nsAString& aUrl);
+
+  void GetHref(nsString& aHref) const;
+
+  void SetHref(const nsAString& aHref, ErrorResult& aRv);
+
+  void GetOrigin(nsString& aOrigin) const;
+
+  void GetProtocol(nsString& aProtocol) const;
+
+  void SetProtocol(const nsAString& aProtocol);
+
+  void GetUsername(nsString& aUsername) const;
+
+  void SetUsername(const nsAString& aUsername);
+
+  void GetPassword(nsString& aPassword) const;
+
+  void SetPassword(const nsAString& aPassword);
+
+  void GetHost(nsString& aHost) const;
+
+  void SetHost(const nsAString& aHost);
+
+  void GetHostname(nsString& aHostname) const;
+
+  void SetHostname(const nsAString& aHostname);
+
+  void GetPort(nsString& aPort) const;
+
+  void SetPort(const nsAString& aPort);
+
+  void GetPathname(nsString& aPathname) const;
+
+  void SetPathname(const nsAString& aPathname);
+
+  void GetSearch(nsString& aSearch) const;
+
+  void SetSearch(const nsAString& aSearch);
+
+  void GetHash(nsString& aHost) const;
+
+  void SetHash(const nsAString& aHash);
+
+private:
+  mozilla::dom::URL* GetURL() const
+  {
+    return mURL;
+  }
+
+  nsRefPtr<mozilla::dom::URL> mURL;
 };
 
 END_WORKERS_NAMESPACE
