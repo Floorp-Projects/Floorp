89c6629e22669d1511be7b3a0a8f72ba5f33eeac
Robert O'Callahan <robert@ocallahan.org>
2013-09-04 23:47:25 +1200

    Bug 911889. Part 7: A single change to CSS 'transform' should not be treated as animation. r=dbaron

--- layout/base/ActiveLayerTracker.cpp
+++ layout/base/ActiveLayerTracker.cpp
@@ -162,11 +162,7 @@ ActiveLayerTracker::IsStyleAnimated(nsIFrame* aFrame, nsCSSProperty aProperty)
 {
   LayerActivity* layerActivity = GetLayerActivity(aFrame);
   if (layerActivity) {
-    // XXX should we really treat a single change to transform as animation?
-    uint8_t minStyleChangesToBeConsideredAnimation =
-        aProperty == eCSSProperty_opacity ? 2 : 1;
-    if (layerActivity->RestyleCountForProperty(aProperty) >=
-        minStyleChangesToBeConsideredAnimation) {
+    if (layerActivity->RestyleCountForProperty(aProperty) >= 2) {
       return true;
     }
   }
