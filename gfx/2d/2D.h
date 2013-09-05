510868e3f60cf1774d86d43b830addd2476e8000
Brian Birtles <birtles@gmail.com>
2013-09-05 09:59:54 +0900

    Bug 912305 - Fix documentation of RadialGradientPattern constructor; r=bas, DONTBUILD (comment-only)

--- gfx/2d/2D.h
+++ gfx/2d/2D.h
@@ -246,8 +246,10 @@ class RadialGradientPattern : public Pattern
 {
 public:
   /*
-   * aBegin Start of the linear gradient
-   * aEnd End of the linear gradient
+   * aCenter1 Center of the inner (focal) circle.
+   * aCenter2 Center of the outer circle.
+   * aRadius1 Radius of the inner (focal) circle.
+   * aRadius2 Radius of the outer circle.
    * aStops GradientStops object for this gradient, this should match the
    *        backend type of the draw target this pattern will be used with.
    * aMatrix A matrix that transforms the pattern into user space
