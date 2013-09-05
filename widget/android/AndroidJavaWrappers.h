6a279504edc1f1708cf57e33431d34c51c1f9658
Wes Johnston <wjohnston@mozilla.com>
2013-09-05 12:32:04 -0700

    Bug 896350 - Use the event queue when adding notification observers. r=blassey, a=bajaj

--- widget/android/AndroidJavaWrappers.h
+++ widget/android/AndroidJavaWrappers.h
@@ -15,6 +15,7 @@
 #include "nsRect.h"
 #include "nsString.h"
 #include "nsTArray.h"
+#include "nsIObserver.h"
 #include "mozilla/gfx/Rect.h"
 #include "mozilla/dom/Touch.h"
 #include "InputData.h"
@@ -530,6 +531,14 @@ public:
         return event;
     }
 
+    static AndroidGeckoEvent* MakeAddObserver(const nsAString &key, nsIObserver *observer) {
+        AndroidGeckoEvent *event = new AndroidGeckoEvent();
+        event->Init(ADD_OBSERVER);
+        event->mCharacters.Assign(key);
+        event->mObserver = observer;
+        return event;
+    }
+
     int Action() { return mAction; }
     int Type() { return mType; }
     bool AckNeeded() { return mAckNeeded; }
@@ -578,6 +587,7 @@ public:
     nsTouchEvent MakeTouchEvent(nsIWidget* widget);
     MultiTouchInput MakeMultiTouchInput(nsIWidget* widget);
     void UnionRect(nsIntRect const& aRect);
+    nsIObserver *Observer() { return mObserver; }
 
 protected:
     int mAction;
@@ -608,6 +618,7 @@ protected:
     short mScreenOrientation;
     nsRefPtr<RefCountedJavaObject> mByteBuffer;
     int mWidth, mHeight;
+    nsCOMPtr<nsIObserver> mObserver;
 
     void ReadIntArray(nsTArray<int> &aVals,
                       JNIEnv *jenv,
@@ -710,6 +721,7 @@ public:
         REMOVE_OBSERVER = 34,
         LOW_MEMORY = 35,
         NETWORK_LINK_CHANGE = 36,
+        ADD_OBSERVER = 37,
         dummy_java_enum_list_end
     };
 
