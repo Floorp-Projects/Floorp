622c6e4d21e90bab51ef9573629b2cfd1d6f14b9
Tom Schuster <evilpies@gmail.com>
2013-09-05 19:24:29 -0400

    Bug 910838 - Electrolysis: Don't update browser information with non-toplevel WebProgress notifications. r=felipe

--- toolkit/content/browser-child.js
+++ toolkit/content/browser-child.js
@@ -56,17 +56,17 @@ let WebProgressListener = {
   },
 
   onLocationChange: function onLocationChange(aWebProgress, aRequest, aLocationURI, aFlags) {
-    let spec = aLocationURI ? aLocationURI.spec : "";
-    let charset = content.document.characterSet;
-
     let json = this._setupJSON(aWebProgress, aRequest);
     let objects = this._setupObjects(aWebProgress);
 
-    json.documentURI = aWebProgress.DOMWindow.document.documentURIObject.spec;
-    json.location = spec;
-    json.canGoBack = docShell.canGoBack;
-    json.canGoForward = docShell.canGoForward;
-    json.charset = charset.toString();
+    json.location = aLocationURI ? aLocationURI.spec : "";
+
+    if (json.isTopLevel) {
+      json.canGoBack = docShell.canGoBack;
+      json.canGoForward = docShell.canGoForward;
+      json.documentURI = content.document.documentURIObject.spec;
+      json.charset = content.document.characterSet;
+    }
 
     sendAsyncMessage("Content:LocationChange", json, objects);
   },
