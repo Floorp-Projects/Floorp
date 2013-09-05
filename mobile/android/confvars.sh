9a8a05a52fc40e66686d9f47d05a869e4d208576
Chenxia Liu <liuche@mozilla.com>
2013-09-05 11:16:23 -0400

    Bug 903535 - Don't build UI for wifi/cell tower data collection and reporting on Release builds. r=gbrown, a=akeybl

--- mobile/android/confvars.sh
+++ mobile/android/confvars.sh
@@ -63,8 +63,10 @@ MOZ_PAY=1
 # Enable UI for healthreporter
 MOZ_SERVICES_HEALTHREPORT=1
 
-# Wifi-AP/cell tower data reporting is always enabled.
+# Wifi-AP/cell tower data reporting is enabled on non-release builds.
+if test ! "$RELEASE_BUILD"; then
 MOZ_DATA_REPORTING=1
+fi
 
 # Enable the production cert for verifying signed packaged apps.
 MOZ_B2G_CERTDATA=1
