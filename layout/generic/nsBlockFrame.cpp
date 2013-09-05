40eba2df9db8c5fe2a3721cb16899077fd3fd4de
Daniel Holbert <dholbert@cs.stanford.edu>
2013-09-05 11:59:09 -0700

    Bug 883514: During block reflow, check floats after reflowing bullet frames, instead of before. r=dbaron a=dveditz

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -1060,8 +1060,6 @@ nsBlockFrame::Reflow(nsPresContext*           aPresContext,
 #endif
   }
 
-  CheckFloats(state);
-
   // Place the "marker" (bullet) frame if it is placed next to a block
   // child.
   //
@@ -1104,6 +1102,8 @@ nsBlockFrame::Reflow(nsPresContext*           aPresContext,
     }
     // Otherwise just leave the bullet where it is, up against our top padding.
   }
+
+  CheckFloats(state);
 
   // Compute our final size
   nscoord bottomEdgeOfChildren;
