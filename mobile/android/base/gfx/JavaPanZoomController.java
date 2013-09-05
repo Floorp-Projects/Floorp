3c03192f9a1b2ca93a3b02a294d203c17bf62554
Kartikaya Gupta <kgupta@mozilla.com>
2013-09-04 21:58:40 -0400

    Bug 911510 - Add a pref to override the dead zone threshold. r=bnicholson

--- mobile/android/base/gfx/JavaPanZoomController.java
+++ mobile/android/base/gfx/JavaPanZoomController.java
@@ -147,7 +147,9 @@ class JavaPanZoomController
 
         mMode = AxisLockMode.STANDARD;
 
-        String[] prefs = { "ui.scrolling.axis_lock_mode", "ui.scrolling.negate_wheel_scrollY" };
+        String[] prefs = { "ui.scrolling.axis_lock_mode",
+                           "ui.scrolling.negate_wheel_scrollY",
+                           "ui.scrolling.gamepad_dead_zone" };
         mNegateWheelScrollY = false;
         PrefsHelper.getPrefs(prefs, new PrefsHelper.PrefHandlerBase() {
             @Override public void prefValue(String pref, String value) {
@@ -161,6 +163,13 @@ class JavaPanZoomController
                     }
                 }
             }
+
+            @Override public void prefValue(String pref, int value) {
+                if (pref.equals("ui.scrolling.gamepad_dead_zone")) {
+                    GamepadUtils.overrideDeadZoneThreshold((float)value / 1000f);
+                }
+            }
+
             @Override public void prefValue(String pref, boolean value) {
                 if (pref.equals("ui.scrolling.negate_wheel_scrollY")) {
                     mNegateWheelScrollY = value;
