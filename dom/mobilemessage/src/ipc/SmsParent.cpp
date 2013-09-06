d594e76d07acead48431834cceef0ed44413c736
"sasikala.paruchuri" <sasikala.paruchuri@lge.com>
2013-09-06 19:13:57 +0800

    Bug 912517 - [Message] Crash occuring in MMS when killing the application while the MMS is sending. r=vicamo Change-Id: I4f91efbd7fdc9f47de8f738b4610ce7f27fa5a92

--- dom/mobilemessage/src/ipc/SmsParent.cpp
+++ dom/mobilemessage/src/ipc/SmsParent.cpp
@@ -584,6 +584,8 @@ SmsRequestParent::SendReply(const MessageReply& aReply)
 NS_IMETHODIMP
 SmsRequestParent::NotifyMessageSent(nsISupports *aMessage)
 {
+  NS_ENSURE_TRUE(!mActorDestroyed, NS_ERROR_FAILURE);
+
   nsCOMPtr<nsIDOMMozMmsMessage> mms = do_QueryInterface(aMessage);
   if (mms) {
     MmsMessage *msg = static_cast<MmsMessage*>(mms.get());
@@ -613,6 +615,8 @@ SmsRequestParent::NotifySendMessageFailed(int32_t aError)
 NS_IMETHODIMP
 SmsRequestParent::NotifyMessageGot(nsISupports *aMessage)
 {
+  NS_ENSURE_TRUE(!mActorDestroyed, NS_ERROR_FAILURE);
+
   nsCOMPtr<nsIDOMMozMmsMessage> mms = do_QueryInterface(aMessage);
   if (mms) {
     MmsMessage *msg = static_cast<MmsMessage*>(mms.get());
