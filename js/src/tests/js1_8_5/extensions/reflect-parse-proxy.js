8d745769da8fe9240c67cd4535b30ced0e17e806
Jason Orendorff <jorendorff@mozilla.com>
2013-09-06 21:40:49 -0500

    Bug 905774 - Make Reflect.parse play better with proxies. Remove GetPropertyDefault. r=bhackett.

---
+++ js/src/tests/js1_8_5/extensions/reflect-parse-proxy.js
@@ -0,0 +1,21 @@
+// |reftest| skip-if(!xulRuntime.shell)
+// bug 905774
+
+// Proxy options
+var opts = new Proxy({loc: false}, {});
+assertEq(Reflect.parse("0;", opts).loc === null, true);
+opts.loc = true;
+assertEq(Reflect.parse("0;", opts).loc !== null, true);
+delete opts.loc;
+assertEq(Reflect.parse("0;", opts).loc !== null, true);  // default is true
+
+// Proxy builder
+var builder = {
+    program: function (body) { return body.join(); },
+    expressionStatement: function (expr) { return expr + ";" },
+    literal: function (val) { return "" + val; }
+};
+opts = {builder: new Proxy(builder, {})};
+assertEq(Reflect.parse("0xff;", opts), "255;");
+
+reportCompare(0, 0, 'ok');
