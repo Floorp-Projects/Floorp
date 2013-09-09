7a10df67fda0d3c4d087444b3137d5ac10625554
Douglas Crosher <dtc-moz@scieneer.com>
2013-09-10 01:52:45 +1000

    Bug 913867 - OdinMonkey: correct heap access index folding of bitwise-and of zero, r=luke

--- js/src/jit/AsmJS.cpp
+++ js/src/jit/AsmJS.cpp
@@ -39,6 +39,7 @@ using namespace js::jit;
 
 using mozilla::AddToHash;
 using mozilla::ArrayLength;
+using mozilla::CountLeadingZeroes32;
 using mozilla::DebugOnly;
 using mozilla::HashGeneric;
 using mozilla::IsNaN;
@@ -3117,8 +3118,10 @@ FoldMaskedArrayIndex(FunctionCompiler &f, ParseNode **indexExpr, int32_t *mask,
     if (IsLiteralUint32(maskNode, &mask2)) {
         // Flag the access to skip the bounds check if the mask ensures that an 'out of
         // bounds' access can not occur based on the current heap length constraint.
-        if (mozilla::CountLeadingZeroes32(f.m().minHeapLength() - 1) <= mozilla::CountLeadingZeroes32(mask2))
+        if (mask2 == 0 ||
+            CountLeadingZeroes32(f.m().minHeapLength() - 1) <= CountLeadingZeroes32(mask2)) {
             *needsBoundsCheck = NO_BOUNDS_CHECK;
+        }
         *mask &= mask2;
         *indexExpr = indexNode;
         return true;
