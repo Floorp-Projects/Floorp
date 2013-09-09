49c0aba389f4de8b5f18223334de7e40c97f28a4
Szu-Yu Chen [:aknow] <szchen@mozilla.com>
2013-09-09 08:19:49 -0400

    Bug 912884 - Part 2: Update test case. r=hsinyi

--- dom/system/gonk/tests/test_ril_worker_ecm.js
+++ dom/system/gonk/tests/test_ril_worker_ecm.js
@@ -28,14 +28,15 @@ function _getWorker() {
 
 var timeoutCallback = null;
 var timeoutDelayMs = 0;
-var TIMER_ID = 1234;
+const TIMER_ID = 1234;
+const TIMEOUT_VALUE = 300000;  // 5 mins.
 
 // No window in xpcshell-test. Create our own timer mechanism.
 
 function setTimeout(callback, timeoutMs) {
   timeoutCallback = callback;
   timeoutDelayMs = timeoutMs;
-  do_check_eq(timeoutMs, 300000);  // 5 mins.
+  do_check_eq(timeoutMs, TIMEOUT_VALUE);
   return TIMER_ID;
 }
 
@@ -56,19 +57,22 @@ add_test(function test_enter_emergencyCbMode() {
   let workerHelper = _getWorker();
   let worker = workerHelper.worker;
 
-  worker.RIL[UNSOLICITED_ENTER_EMERGENCY_CALLBACK_MODE]();
-  let postedMessage = workerHelper.postedMessage;
+  // Do it twice. Should always send the event.
+  for (let i = 0; i < 2; ++i) {
+    worker.RIL[UNSOLICITED_ENTER_EMERGENCY_CALLBACK_MODE]();
+    let postedMessage = workerHelper.postedMessage;
 
-  // Should store the mode.
-  do_check_eq(worker.RIL._isInEmergencyCbMode, true);
+    // Should store the mode.
+    do_check_eq(worker.RIL._isInEmergencyCbMode, true);
 
-  // Should notify change.
-  do_check_eq(postedMessage.rilMessageType, "emergencyCbModeChange");
-  do_check_eq(postedMessage.active, true);
-  do_check_eq(postedMessage.timeoutMs, 300000);
+    // Should notify change.
+    do_check_eq(postedMessage.rilMessageType, "emergencyCbModeChange");
+    do_check_eq(postedMessage.active, true);
+    do_check_eq(postedMessage.timeoutMs, TIMEOUT_VALUE);
 
-  // Should start timer.
-  do_check_eq(worker.RIL._exitEmergencyCbModeTimeoutID, TIMER_ID);
+    // Should start timer.
+    do_check_eq(worker.RIL._exitEmergencyCbModeTimeoutID, TIMER_ID);
+  }
 
   run_next_test();
 });
