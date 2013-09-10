9e6b93ed9268fd6f0127a40f1b683f8e556f492e
Guilherme GonÃ§alves <ggp@mozilla.com>
2013-09-10 13:12:54 -0400

    Bug 902145 - Ignore all speech events while aborting. r=smaug

--- content/media/webspeech/recognition/SpeechRecognition.cpp
+++ content/media/webspeech/recognition/SpeechRecognition.cpp
@@ -138,6 +138,11 @@ SpeechRecognition::ProcessEvent(SpeechEvent* aEvent)
          GetName(aEvent),
          GetName(mCurrentState));
 
+  if (mAborted && aEvent->mType != EVENT_ABORT) {
+    // ignore all events while aborting
+    return;
+  }
+
   Transition(aEvent);
 }
 
@@ -297,9 +302,6 @@ SpeechRecognition::Transition(SpeechEvent* aEvent)
           MOZ_CRASH("Invalid event EVENT_COUNT");
       }
       break;
-    case STATE_ABORTING:
-      DoNothing(aEvent);
-      break;
     case STATE_COUNT:
       MOZ_CRASH("Invalid state STATE_COUNT");
   }
@@ -377,6 +379,7 @@ SpeechRecognition::Reset()
   mEstimationSamples = 0;
   mBufferedSamples = 0;
   mSpeechDetectionTimer->Cancel();
+  mAborted = false;
 }
 
 void
@@ -489,9 +492,6 @@ void
 SpeechRecognition::AbortSilently(SpeechEvent* aEvent)
 {
   bool stopRecording = StateBetween(STATE_ESTIMATING, STATE_RECOGNIZING);
-
-  // prevent reentrancy from DOM events
-  SetState(STATE_ABORTING);
 
   if (mRecognitionService) {
     mRecognitionService->Abort();
@@ -734,6 +734,11 @@ SpeechRecognition::Stop()
 void
 SpeechRecognition::Abort()
 {
+  if (mAborted) {
+    return;
+  }
+
+  mAborted = true;
   nsRefPtr<SpeechEvent> event = new SpeechEvent(this, EVENT_ABORT);
   NS_DispatchToMainThread(event);
 }
@@ -891,7 +896,6 @@ SpeechRecognition::GetName(FSMState aId)
     "STATE_WAITING_FOR_SPEECH",
     "STATE_RECOGNIZING",
     "STATE_WAITING_FOR_RESULT",
-    "STATE_ABORTING"
   };
 
   MOZ_ASSERT(aId < STATE_COUNT);
