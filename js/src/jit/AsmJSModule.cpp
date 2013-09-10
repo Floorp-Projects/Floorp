3c39c05b470554810022e2fc8001905df8ae138b
Luke Wagner <luke@mozilla.com>
2013-09-09 21:26:38 -0500

    Bug 913977 - OdinMonkey: mark modules as linked at the start of linking (r=dougc)

--- js/src/jit/AsmJSModule.cpp
+++ js/src/jit/AsmJSModule.cpp
@@ -22,8 +22,13 @@
 using namespace js;
 
 void
-AsmJSModule::patchHeapAccesses(ArrayBufferObject *heap, JSContext *cx)
+AsmJSModule::initHeap(Handle<ArrayBufferObject*> heap, JSContext *cx)
 {
+    JS_ASSERT(linked_);
+    JS_ASSERT(!maybeHeap_);
+    maybeHeap_ = heap;
+    heapDatum() = heap->dataPointer();
+
     JS_ASSERT(IsPowerOfTwo(heap->byteLength()));
 #if defined(JS_CPU_X86)
     uint8_t *heapOffset = heap->dataPointer();
