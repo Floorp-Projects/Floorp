3feb8d84caf9277f3007df8b45adf229170bdbd9
Ralph Giles <giles@mozilla.com>
2013-09-10 11:29:00 -0700

    Bug 865256 - Part 3a: Add AudioBufferPeakValue utility. r=ehsan, a=lsblakk
    This is an equivalent, C-only implementation of blink's
    VectorMath::maxmgv or Apple's vDSP_maxmgv. It finds the
    maximum absolute value of the elements in a float buffer.
    Used by blink's PeriodicWave implementation for normalization.

--- content/media/AudioNodeEngine.h
+++ content/media/AudioNodeEngine.h
@@ -136,6 +136,11 @@ void BufferComplexMultiply(const float* aInput,
                            uint32_t aSize);
 
 /**
+ * Vector maximum element magnitude ( max(abs(aInput)) ).
+ */
+float AudioBufferPeakValue(const float* aInput, uint32_t aSize);
+
+/**
  * In place gain. aScale == 1.0f should be optimized.
  */
 void AudioBufferInPlaceScale(float aBlock[WEBAUDIO_BLOCK_SIZE],
