84ce98aed05eb0b77d54e831f063d5d0977231c6
pzhang <pzhang@mozilla.com>
2013-09-11 13:41:52 +0800

    Bug 911063 - Add marionette tests for WebFM. r=khuey

---
+++ dom/fmradio/test/marionette/test_cancel_seek.js
@@ -0,0 +1,69 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+MARIONETTE_TIMEOUT = 10000;
+
+SpecialPowers.addPermission("fmradio", true, document);
+
+let FMRadio = window.navigator.mozFMRadio;
+
+function verifyInitialState() {
+  log("Verifying initial state.");
+  ok(FMRadio);
+  is(FMRadio.enabled, false);
+  setUp();
+}
+
+function setUp() {
+  let frequency = FMRadio.frequencyLowerBound + FMRadio.channelWidth;
+  FMRadio.enable(frequency);
+  FMRadio.onenabled = seek;
+}
+
+function seek() {
+  log("Seek up");
+  var request = FMRadio.seekUp();
+  ok(request);
+
+  var seekUpIsCancelled = false;
+  request.onerror = function() {
+    seekUpIsCancelled = true;
+  };
+
+  var seekUpCompleted = false;
+  request.onsuccess = function() {
+    ok(!seekUpIsCancelled);
+    seekUpCompleted = true;
+  };
+
+  log("Seek up");
+  var cancelSeekReq = FMRadio.cancelSeek();
+  ok(cancelSeekReq);
+
+  // There are two possibilities which depends on the system
+  // process scheduling (bug 911063 comment 0):
+  //   - seekup action is canceled
+  //   - seekup's onsuccess fires before cancelSeek's onerror
+
+  cancelSeekReq.onsuccess = function() {
+    ok(seekUpIsCancelled, "Seekup request failed.");
+    cleanUp();
+  };
+
+  cancelSeekReq.onerror = function() {
+    ok(seekUpCompleted);
+    cleanUp();
+  };
+}
+
+function cleanUp() {
+  FMRadio.disable();
+  FMRadio.ondisabled = function() {
+    FMRadio.ondisabled = null;
+    ok(!FMRadio.enabled);
+    finish();
+  };
+}
+
+verifyInitialState();
+
---
+++ dom/fmradio/test/marionette/test_enable_disable.js
@@ -0,0 +1,85 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+MARIONETTE_TIMEOUT = 10000;
+
+SpecialPowers.addPermission("fmradio", true, document);
+
+let FMRadio = window.navigator.mozFMRadio;
+
+function verifyInitialState() {
+  log("Verifying initial state.");
+  ok(FMRadio);
+  is(FMRadio.enabled, false);
+
+  log("Verifying attributes when disabled.");
+  is(FMRadio.frequency, null);
+  ok(FMRadio.frequencyLowerBound);
+  ok(FMRadio.frequencyUpperBound);
+  ok(FMRadio.frequencyUpperBound > FMRadio.frequencyLowerBound);
+  ok(FMRadio.channelWidth);
+
+  enableFMRadio();
+}
+
+function enableFMRadio() {
+  log("Verifying behaviors when enabled.");
+  var frequency = FMRadio.frequencyLowerBound + FMRadio.channelWidth;
+  var request = FMRadio.enable(frequency);
+  ok(request, "FMRadio.enable(r" + frequency + ") returns request");
+
+  request.onsuccess = function() {
+    ok(FMRadio.enabled);
+    ok(typeof FMRadio.frequency == "number");
+    ok(FMRadio.frequency > FMRadio.frequencyLowerBound);
+  };
+
+  request.onerror = function() {
+    ok(null, "Failed to enable");
+  };
+
+  var enabled = false;
+  FMRadio.onenabled = function() {
+    FMRadio.onenabled = null;
+    enabled = FMRadio.enabled;
+  };
+
+  FMRadio.onfrequencychange = function() {
+    log("Check if 'onfrequencychange' event is fired after the 'enabled' event");
+    FMRadio.onfrequencychange = null;
+    ok(enabled, "FMRadio is enabled when handling `onfrequencychange`");
+    disableFMRadio();
+  };
+}
+
+function disableFMRadio() {
+  log("Verify behaviors when disabled");
+
+  // There are two possibilities which depends on the system
+  // process scheduling (bug 911063 comment 0):
+  //   - seek fails
+  //   - seek's onsuccess fires before disable's onsucess
+  var seekRequest = FMRadio.seekUp();
+  var seekCompletes = false;
+  var failedToSeek = false;
+  seekRequest.onerror = function() {
+    ok(!seekCompletes);
+    failedToSeek = true;
+  };
+
+  seekRequest.onsuccess = function() {
+    ok(!failedToSeek);
+    seekCompletes = true;
+  };
+
+  FMRadio.disable();
+  FMRadio.ondisabled = function() {
+    FMRadio.ondisabled = null;
+    ok(seekCompletes || failedToSeek);
+    ok(!FMRadio.enabled);
+    finish();
+  };
+}
+
+verifyInitialState();
+
