5cee5adbdc17b30b14f06a50865f6d9230a022a6
Jim Mathies <jmathies@mozilla.com>
2013-09-11 13:09:46 -0500

    Bug 914829 - MetroInput should forward touch input to apz first, then to content. r=kats, tabraldes

--- widget/windows/winrt/MetroInput.h
+++ widget/windows/winrt/MetroInput.h
@@ -196,6 +196,8 @@ private:
   bool mTouchStartDefaultPrevented;
   bool mTouchMoveDefaultPrevented;
   bool mIsFirstTouchMove;
+  bool mCancelable;
+  bool mTouchCancelSent;
 
   // In the old Win32 way of doing things, we would receive a WM_TOUCH event
   // that told us the state of every touchpoint on the touch surface.  If
@@ -270,8 +272,9 @@ private:
 
   // Sync event dispatching
   void DispatchEventIgnoreStatus(nsGUIEvent *aEvent);
+  void DispatchTouchCancel();
 
-   nsDeque mInputEventQueue;
+  nsDeque mInputEventQueue;
   static nsEventStatus sThrowawayStatus;
 };
 
