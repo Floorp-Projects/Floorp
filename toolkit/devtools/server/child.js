1621ca2f66880fe1b9ed0659a0c60515ca7d84ce
Alexandre Poirot <poirot.alex@gmail.com>
2013-09-11 05:45:00 +0200

    Bug 912213 - Fix conflicts between two ContentTabActor classes r=paul

--- toolkit/devtools/server/child.js
+++ toolkit/devtools/server/child.js
@@ -21,7 +21,7 @@ let onConnect = DevToolsUtils.makeInfallible(function (msg) {
 
   let conn = DebuggerServer.connectToParent(prefix, mm);
 
-  let actor = new DebuggerServer.ContentTabActor(conn, content);
+  let actor = new DebuggerServer.ContentAppActor(conn, content);
   let actorPool = new ActorPool(conn);
   actorPool.addActor(actor);
   conn.addActorPool(actorPool);
