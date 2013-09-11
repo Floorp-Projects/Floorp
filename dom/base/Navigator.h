9a950a6241ef7de59be3531d887e5e13f3a29516
Andrea Marchesini <amarchesini@mozilla.com>
2013-09-11 15:47:46 +0200

    Bug 871445 - patch 1 - DataStoreService and getDataStores(), r=mounir, r=fabrice, r=ehsan

--- dom/base/Navigator.h
+++ dom/base/Navigator.h
@@ -55,6 +55,8 @@ class BatteryManager;
 class FMRadio;
 #endif
 
+class Promise;
+
 class DesktopNotificationCenter;
 class MobileMessageManager;
 class MozIdleObserver;
@@ -181,6 +183,8 @@ public:
   // The XPCOM GetDoNotTrack is ok
   Geolocation* GetGeolocation(ErrorResult& aRv);
   battery::BatteryManager* GetBattery(ErrorResult& aRv);
+  already_AddRefed<Promise> GetDataStores(const nsAString &aName,
+                                          ErrorResult& aRv);
   void Vibrate(uint32_t aDuration, ErrorResult& aRv);
   void Vibrate(const nsTArray<uint32_t>& aDuration, ErrorResult& aRv);
   void GetAppCodeName(nsString& aAppCodeName, ErrorResult& aRv)
