53c74c34f6d078160738496a26e7f7970c5d8c32
Benoit Jacob <bjacob@mozilla.com>
2013-09-10 21:10:47 -0400

    Bug 912974 - Add two missing include guards. r=bjacob

--- gfx/gl/GfxTexturesReporter.h
+++ gfx/gl/GfxTexturesReporter.h
@@ -4,6 +4,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef GFXTEXTURESREPORTER_H_
+#define GFXTEXTURESREPORTER_H_
+
 #include "nsIMemoryReporter.h"
 #include "GLTypes.h"
 
@@ -46,3 +49,5 @@ private:
 
 }
 }
+
+#endif // GFXTEXTURESREPORTER_H_
