a36db9f52a837b4846fdb81f4cc7e1c5f05a5ae8
Jonas Finnemann Jensen <jopsen@gmail.com>
2013-09-12 10:47:37 -0400

    Bug 902587 - Part 1: Refactor IO Interposer with filename support. r=aklotz

--- tools/profiler/ProfilerIOInterposeObserver.cpp
+++ tools/profiler/ProfilerIOInterposeObserver.cpp
@@ -2,34 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "ProfilerIOInterposeObserver.h"
-
 #include "GeckoProfiler.h"
+#include "ProfilerIOInterposeObserver.h"
 
 using namespace mozilla;
 
-ProfilerIOInterposeObserver::ProfilerIOInterposeObserver()
+void ProfilerIOInterposeObserver::Observe(Observation& aObservation)
 {
-  IOInterposer* interposer = IOInterposer::GetInstance();
-  if (interposer) {
-    interposer->Register(IOInterposeObserver::OpAll, this);
+  // TODO: The profile might want to take notice of non-main-thread IO, as
+  // well as noting what files or references causes the IO.
+  if (NS_IsMainThread()) {
+    const char* ops[] = {"none", "read", "write", "invalid", "fsync"};
+    PROFILER_MARKER(ops[aObservation.ObservedOperation()]);
   }
 }
-
-ProfilerIOInterposeObserver::~ProfilerIOInterposeObserver()
-{
-  IOInterposer* interposer = IOInterposer::GetInstance();
-  if (interposer) {
-    interposer->Deregister(IOInterposeObserver::OpAll, this);
-  }
-}
-
-void ProfilerIOInterposeObserver::Observe(IOInterposeObserver::Operation aOp,
-                                          double& aDuration,
-                                          const char* aModuleInfo)
-{
-  MOZ_ASSERT(NS_IsMainThread());
-  const char* ops[] = {"none", "read", "write", "invalid", "fsync"};
-  PROFILER_MARKER(ops[aOp]);
-}
-
