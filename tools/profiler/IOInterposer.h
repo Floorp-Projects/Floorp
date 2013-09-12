ba4b7d2318c1bc7383d1e13c4830ab02f373fd0a
Jonas Finnemann Jensen <jopsen@gmail.com>
2013-09-12 10:47:37 -0400

    Bug 902587 - Part 1: Refactor IO Interposer with filename support. r=aklotz

--- tools/profiler/IOInterposer.h
+++ tools/profiler/IOInterposer.h
@@ -2,20 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef IOINTERPOSER_H_
-#define IOINTERPOSER_H_
+#ifndef mozilla_IOInterposer_h
+#define mozilla_IOInterposer_h
 
 #include "mozilla/Attributes.h"
-#include "mozilla/Mutex.h"
-#include "mozilla/StaticPtr.h"
+#include "mozilla/TimeStamp.h"
 #include "mozilla/XPCOM.h"
 
 namespace mozilla {
 
 /**
- * Interface for I/O interposer observers. This is separate from
- * IOInterposer itself because in the future we might use the I/O
- * Interposer for shutdown write poisioning.
+ * Interface for I/O interposer observers. This is separate from the
+ * IOInterposer because we have multiple uses for these observations.
  */
 class IOInterposeObserver
 {
@@ -29,78 +27,223 @@ public:
     OpWriteFSync = (OpWrite | OpFSync),
     OpAll = (OpRead | OpWrite | OpFSync)
   };
-  virtual void Observe(Operation aOp, double& aDuration,
-                       const char* aModuleInfo) = 0;
-};
 
-/**
- * Interface for I/O Interposer modules that actually generate the I/O
- * Interposer events. Some concrete examples include NSPR, SQLite, ETW
- */
-class IOInterposerModule
-{
-public:
-  virtual ~IOInterposerModule() {}
-  virtual void Enable(bool aEnable) = 0;
+  /** A representation of an I/O observation  */
+  class Observation
+  {
+  protected:
+    Observation()
+    {
+    }
+  public:
+    Observation(Operation aOperation, const TimeStamp& aStart,
+                const TimeStamp& aEnd, const char* aReference)
+     : mOperation(aOperation), mStart(aStart), mEnd(aEnd),
+       mReference(aReference)
+    {
+    }
 
-protected:
-  IOInterposerModule() {}
+    /**
+     * Operation observed, this is either OpRead, OpWrite or OpFSync,
+     * combinations of these flags are only used when registering observers.
+     */
+    Operation ObservedOperation() const
+    {
+      return mOperation;
+    }
 
-private:
-  IOInterposerModule(const IOInterposerModule&);
-  IOInterposerModule& operator=(const IOInterposerModule&);
-};
+    /** Time at which the I/O operation was started */
+    TimeStamp Start() const
+    {
+      return mStart;
+    }
 
-/**
- * This class is reponsible for managing the state of I/O interposer
- * modules and ensuring that events are routed to the appropriate
- * observers. All methods except for Enable should be called from the
- * main thread.
- */
-class IOInterposer MOZ_FINAL : public IOInterposeObserver
-{
-public:
-  ~IOInterposer();
+    /**
+     * Time at which the I/O operation ended, for asynchronous methods this is
+     * the time at which the call initiating the asynchronous request returned.
+     */
+    TimeStamp End() const
+    {
+      return mEnd;
+    }
+
+    /**
+     * Duration of the operation, for asynchronous I/O methods this is the
+     * duration of the call initiating the asynchronous request.
+     */
+    TimeDuration Duration() const
+    {
+      return mEnd - mStart;
+    }
+
+    /**
+     * IO reference, function name or name of component (sqlite) that did IO
+     * this is in addition the generic operation. This attribute may be platform
+     * specific, but should only take a finite number of distinct values.
+     * E.g. sqlite-commit, CreateFile, NtReadFile, fread, fsync, mmap, etc.
+     * I.e. typically the platform specific function that did the IO.
+     */
+    const char* Reference() const
+    {
+      return mReference;
+    }
+
+#ifdef MOZ_ENABLE_PROFILER_SPS
+    /** Request filename associated with the I/O operation, null if unknown */
+    virtual const char* Filename();
+#else
+    const char* Filename() { return nullptr; }
+#endif /* MOZ_ENABLE_PROFILER_SPS */
+
+    virtual ~Observation();
+  protected:
+    Operation   mOperation;
+    TimeStamp   mStart;
+    TimeStamp   mEnd;
+    const char* mReference;
+  };
 
   /**
-   * It is safe to call this method off of the main thread.
+   * Invoked whenever an implementation of the IOInterposeObserver should
+   * observe aObservation. Implement this and do your thing...
+   * But do consider if it is wise to use IO functions in this method, they are
+   * likely to cause recursion :)
+   * At least, see PoisonIOInterposer.h and register your handle as a debug file
+   * even, if you don't initialize the poison IO interposer, someone else might.
    *
-   * @param aEnable true to enable all modules, false to disable all modules
+   * Remark: Observations may occur on any thread.
    */
-  void Enable(bool aEnable);
+  virtual void Observe(Observation& aObservation) = 0;
 
-  void Register(IOInterposeObserver::Operation aOp,
-                IOInterposeObserver* aObserver);
-  void Deregister(IOInterposeObserver::Operation aOp,
-                  IOInterposeObserver* aObserver);
-  void Observe(IOInterposeObserver::Operation aOp, double& aDuration,
-               const char* aModuleInfo);
+  virtual ~IOInterposeObserver();
+};
 
-  static IOInterposer* GetInstance();
+#ifdef MOZ_ENABLE_PROFILER_SPS
+
+/**
+ * Class offering the public static IOInterposer API.
+ *
+ * This class is responsible for ensuring that events are routed to the
+ * appropriate observers. Methods Init() and Clear() should only be called from
+ * the main-thread at startup and shutdown, respectively.
+ *
+ * Remark: Instances of this class will never be created, you should consider it
+ * to be a namespace containing static functions. The class is created to
+ * facilitate to a private static instance variable sObservedOperations.
+ * As we want to access this from an inline static methods, we have to do this
+ * trick.
+ */
+class IOInterposer MOZ_FINAL
+{
+  // Track whether or not a given type of observation is being observed
+  static IOInterposeObserver::Operation sObservedOperations;
+
+  // No instance of class should be created, they'd be empty anyway.
+  IOInterposer();
+public:
+
+  /**
+   * This function must be called from the main-thread when no other threads are
+   * running before any of the other methods on this class may be used.
+   *
+   * IO reports can however, safely assume that IsObservedOperation() will
+   * return false, until the IOInterposer is initialized.
+   *
+   * Remark, it's safe to call this method multiple times, so just call it when
+   * you to utilize IO interposing.
+   */
+  static void Init();
 
   /**
    * This function must be called from the main thread, and furthermore
-   * it must be called when no other threads are executing. Effectivly this
-   * restricts us to calling it only during shutdown.
+   * it must be called when no other threads are executing. Effectively
+   * restricting us to calling it only during shutdown.
+   *
+   * Callers should take care that no other consumers are subscribed to events,
+   * as these events will stop when this function is called.
+   *
+   * In practice, we don't use this method, as the IOInterposer is used for
+   * late-write checks.
    */
-  static void ClearInstance();
+  static void Clear();
 
-private:
-  IOInterposer();
-  bool Init();
-  IOInterposer(const IOInterposer&);
-  IOInterposer& operator=(const IOInterposer&);
+  /**
+   * Report IO to registered observers.
+   * Notice that the reported operation must be either OpRead, OpWrite or
+   * OpFSync. You are not allowed to report an observation with OpWriteFSync or
+   * OpAll, these are just auxiliary values for use with Register().
+   *
+   * If the IO call you're reporting does multiple things, write and fsync, you
+   * can choose to call Report() twice once with write and once with FSync. You
+   * may not call Report() with OpWriteFSync! The Observation::mOperation
+   * attribute is meant to be generic, not perfect.
+   *
+   * Notice that there is no reason to report an observation with an operation
+   * which is not being observed. Use IsObservedOperation() to check if the
+   * operation you are about to report is being observed. This is especially
+   * important if you are constructing expensive observations containing
+   * filename and full-path.
+   *
+   * Remark: Init() must be called before any IO is reported. But
+   * IsObservedOperation() will return false until Init() is called.
+   */
+  static void Report(IOInterposeObserver::Observation& aObservation);
 
-  // mMutex guards access to mModules, particularly to provide
-  // mutual exclusion during Enable()
-  mozilla::Mutex  mMutex;
-  nsTArray<IOInterposerModule*>   mModules;
-  nsTArray<IOInterposeObserver*>  mReadObservers;
-  nsTArray<IOInterposeObserver*>  mWriteObservers;
-  nsTArray<IOInterposeObserver*>  mFSyncObservers;
+  /**
+   * Return whether or not an operation is observed. Reporters should not
+   * report operations that are not being observed by anybody. This mechanism
+   * allows us to not report IO when no observers are registered.
+   */
+  static inline bool IsObservedOperation(IOInterposeObserver::Operation aOp) {
+    // The quick reader may observe that no locks are being employed here,
+    // hence, the result of the operations is truly undefined. However, most
+    // computers will usually return either true or false, which is good enough.
+    // If we occasionally report more or less IO than is being observed than
+    // that is not a problem.
+    return (sObservedOperations & aOp);
+  }
+
+  /**
+   * Register IOInterposeObserver, the observer object will receive all
+   * observations for the given operation aOp.
+   *
+   * Remark: Init() must be called before observers are registered.
+   */
+  static void Register(IOInterposeObserver::Operation aOp,
+                       IOInterposeObserver* aObserver);
+
+  /**
+   * Unregister an IOInterposeObserver for a given operation
+   * Remark: It is always safe to unregister for all operations, even if yoú
+   * didn't register for them all.
+   * I.e. IOInterposer::Unregister(IOInterposeObserver::OpAll, aObserver)
+   *
+   * Remark: Init() must be called before observers are unregistered
+   */
+  static void Unregister(IOInterposeObserver::Operation aOp,
+                         IOInterposeObserver* aObserver);
 };
+
+#else /* MOZ_ENABLE_PROFILER_SPS */
+
+class IOInterposer MOZ_FINAL
+{
+  IOInterposer();
+public:
+  static inline void Init()                                               {}
+  static inline void Clear()                                              {}
+  static inline void Report(IOInterposeObserver::Observation& aOb)        {}
+  static inline void Register(IOInterposeObserver::Operation aOp,
+                              IOInterposeObserver* aObserver)             {}
+  static inline void Unregister(IOInterposeObserver::Operation aOp,
+                                IOInterposeObserver* aObserver)           {}
+  static inline bool IsObservedOperation(IOInterposeObserver::Operation aOp) {
+    return false;
+  }
+};
+
+#endif /* MOZ_ENABLE_PROFILER_SPS */
 
 } // namespace mozilla
 
-#endif // IOINTERPOSER_H_
-
+#endif // mozilla_IOInterposer_h
