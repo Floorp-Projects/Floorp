a04b4d233246fa78ef2ea61ceb5722da56b11346
Ehsan Akhgari <ehsan@mozilla.com>
2013-09-13 18:12:07 +0200

    Bug 881959 - Tell the MediaStreamGraph when changes to the graph occur. r=roc, a=akeybl

--- content/media/MediaStreamGraph.cpp
+++ content/media/MediaStreamGraph.cpp
@@ -2303,6 +2303,7 @@ MediaStreamGraphImpl::MediaStreamGraphImpl(bool aRealtime)
   , mPostedRunInStableState(false)
   , mRealtime(aRealtime)
   , mNonRealtimeProcessing(false)
+  , mStreamOrderDirty(false)
 {
 #ifdef PR_LOGGING
   if (!gMediaStreamGraphLog) {
@@ -2454,6 +2455,13 @@ MediaStreamGraph::StartNonRealtimeProcessing(uint32_t aTicksToProcess)
   graph->mNonRealtimeTicksToProcess = aTicksToProcess;
   graph->mNonRealtimeProcessing = true;
   graph->EnsureRunInStableState();
+}
+
+void
+ProcessedMediaStream::AddInput(MediaInputPort* aPort)
+{
+  mInputs.AppendElement(aPort);
+  GraphImpl()->SetStreamOrderDirty();
 }
 
 }
