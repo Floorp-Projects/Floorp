dac0937181736cbe4b50689e424060a7e03dd5d9
Ben Tian <btian@mozilla.com>
2013-09-14 06:51:00 +0800

    Bug 915628 - [Bluetooth] Define new log levels and add logs in profile controller, r=echou

--- dom/bluetooth/linux/BluetoothDBusService.h
+++ dom/bluetooth/linux/BluetoothDBusService.h
@@ -4,8 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_bluetooth_bluetoothdbuseventservice_h__
-#define mozilla_dom_bluetooth_bluetoothdbuseventservice_h__
+#ifndef mozilla_dom_bluetooth_bluetoothdbusservice_h__
+#define mozilla_dom_bluetooth_bluetoothdbusservice_h__
 
 #include "mozilla/Attributes.h"
 #include "BluetoothCommon.h"
