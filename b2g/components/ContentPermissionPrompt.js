5fe6c9270d8a6e8b893aac70f5d05747c2cde3d1
Alive Kuo <alive@mozilla.com>
2013-09-13 08:07:25 -0400

    Bug 915468 - Provide manifestURL in permission request for gaia to do localization. r=fabrice

--- b2g/components/ContentPermissionPrompt.js
+++ b2g/components/ContentPermissionPrompt.js
@@ -249,13 +249,8 @@ ContentPermissionPrompt.prototype = {
       return;
     }
 
-    // When it's an app, get the manifest to add the l10n application name.
-    let app = DOMApplicationRegistry.getAppByLocalId(principal.appId);
-    DOMApplicationRegistry.getManifestFor(app.manifestURL, function getManifest(aManifest) {
-      let helper = new ManifestHelper(aManifest, app.origin);
-      details.appName = helper.name;
-      browser.shell.sendChromeEvent(details);
-    });
+    details.manifestURL = DOMApplicationRegistry.getManifestURLByLocalId(principal.appId);
+    browser.shell.sendChromeEvent(details);
   },
 
   classID: Components.ID("{8c719f03-afe0-4aac-91ff-6c215895d467}"),
