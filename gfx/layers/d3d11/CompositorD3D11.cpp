4202bb63ca652ff4af385f29025a489e3c172362
Brian R. Bondy <netzen@gmail.com>
2013-09-15 15:30:11 -0700

    Bug 877225 - Transition from snapped view shows stretched page for a moment. r=bas

--- gfx/layers/d3d11/CompositorD3D11.cpp
+++ gfx/layers/d3d11/CompositorD3D11.cpp
@@ -63,6 +63,7 @@ CompositorD3D11::CompositorD3D11(nsIWidget* aWidget)
   : mAttachments(nullptr)
   , mWidget(aWidget)
   , mHwnd(nullptr)
+  , mDisableSequenceForNextFrame(false)
 {
   sBackend = LAYERS_D3D11;
 }
@@ -270,7 +271,7 @@ CompositorD3D11::Initialize()
     swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
     // Use double buffering to enable flip
     swapDesc.BufferCount = 2;
-    swapDesc.Scaling = DXGI_SCALING_STRETCH;
+    swapDesc.Scaling = DXGI_SCALING_NONE;
     // All Metro style apps must use this SwapEffect
     swapDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
     swapDesc.Flags = 0;
@@ -689,13 +690,13 @@ CompositorD3D11::EndFrame()
   nsIntSize oldSize = mSize;
   EnsureSize();
   if (oldSize == mSize) {
-    mSwapChain->Present(0, 0);
-
+    mSwapChain->Present(0, mDisableSequenceForNextFrame ? DXGI_PRESENT_DO_NOT_SEQUENCE : 0);
+    mDisableSequenceForNextFrame = false;
     if (mTarget) {
       PaintToTarget();
     }
   }
-
+  
   mCurrentRT = nullptr;
 }
 
@@ -758,6 +759,7 @@ CompositorD3D11::VerifyBufferSize()
     mSwapChain->ResizeBuffers(2, mSize.width, mSize.height,
                               DXGI_FORMAT_B8G8R8A8_UNORM,
                               0);
+    mDisableSequenceForNextFrame = true;
 #endif
   } else {
     mSwapChain->ResizeBuffers(1, mSize.width, mSize.height,
