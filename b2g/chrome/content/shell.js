655360468577fb4ffc6fe068e218225eef0550ea
Alexandre Poirot <poirot.alex@gmail.com>
2013-09-16 09:30:00 +0200

    Bug 916237 - Enable inspector actor in b2g parent process. r=paul, a=lsblakk

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -1047,6 +1047,7 @@ let RemoteDebugger = {
       DebuggerServer.addActors('chrome://browser/content/dbg-browser-actors.js');
       DebuggerServer.addActors("resource://gre/modules/devtools/server/actors/webapps.js");
       DebuggerServer.registerModule("devtools/server/actors/device");
+      DebuggerServer.registerModule("devtools/server/actors/inspector")
 
       DebuggerServer.onConnectionChange = function(what) {
         AdbController.updateState();
