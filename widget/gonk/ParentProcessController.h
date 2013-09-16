2a304ec3fd3c1eb61f5b66c123f914ebf27f7074
Botond Ballo <botond@mozilla.com>
2013-09-16 17:45:31 -0400

    Bug 912657 - Add support for APZC in the B2G parent process (without turning it on everywhere). r=ajones

---
+++ widget/gonk/ParentProcessController.h
@@ -0,0 +1,34 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef __mozilla_widget_DynamicToolbarController_h__
+#define __mozilla_widget_DynamicToolbarController_h__
+
+#include "mozilla/layers/GeckoContentController.h"
+
+namespace mozilla {
+namespace widget {
+
+class ParentProcessController : public mozilla::layers::GeckoContentController
+{
+    typedef mozilla::layers::FrameMetrics FrameMetrics;
+
+public:
+    virtual void RequestContentRepaint(const FrameMetrics& aFrameMetrics) MOZ_OVERRIDE;
+    virtual void PostDelayedTask(Task* aTask, int aDelayMs) MOZ_OVERRIDE;
+
+    // No-ops
+    virtual void HandleDoubleTap(const CSSIntPoint& aPoint) MOZ_OVERRIDE {}
+    virtual void HandleSingleTap(const CSSIntPoint& aPoint) MOZ_OVERRIDE {}
+    virtual void HandleLongTap(const CSSIntPoint& aPoint) MOZ_OVERRIDE {}
+    virtual void SendAsyncScrollDOMEvent(bool aIsRoot,
+                                         const CSSRect &aContentRect,
+                                         const CSSSize &aScrollableSize) MOZ_OVERRIDE {}
+};
+
+}
+}
+
+#endif /*__mozilla_widget_DynamicToolbarController_h__ */
