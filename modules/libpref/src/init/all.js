29a91b52787d20e3948ee19cf3f8a6268f87f4a8
EKR <ekr@rtfm.com>
2013-09-16 17:42:39 -0700

    Bug 842549 - Part 3: PC.js changes for trickle r=abr

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -230,6 +230,7 @@ pref("media.navigator.video.default_minfps",10);
 pref("media.peerconnection.enabled", true);
 pref("media.navigator.permission.disabled", false);
 pref("media.peerconnection.default_iceservers", "[{\"url\": \"stun:23.21.150.121\"}]");
+pref("media.peerconnection.trickle_ice", true);
 pref("media.peerconnection.use_document_iceservers", true);
 // These values (aec, agc, and noice) are from media/webrtc/trunk/webrtc/common_types.h
 // kXxxUnchanged = 0, kXxxDefault = 1, and higher values are specific to each 
