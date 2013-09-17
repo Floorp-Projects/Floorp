46142d7b558087acc1b3e2118f6ba7d7328bcaaf
Gregory Szorc <gps@mozilla.com>
2013-09-16 17:50:04 -0700

    Bug 911375 - Part 3: Remove PurgeManifest; r=glandium

--- python/mozbuild/mozpack/test/test_manifests.py
+++ python/mozbuild/mozpack/test/test_manifests.py
@@ -14,41 +14,8 @@ from mozpack.copier import (
 )
 from mozpack.manifests import (
     InstallManifest,
-    PurgeManifest,
-    UnreadablePurgeManifest,
 )
 from mozpack.test.test_files import TestWithTmpDir
-
-
-class TestPurgeManifest(TestWithTmpDir):
-    def test_construct(self):
-        m = PurgeManifest()
-        self.assertEqual(m.relpath, '')
-        self.assertEqual(len(m.entries), 0)
-
-    def test_serialization(self):
-        m = PurgeManifest(relpath='rel')
-        m.add('foo')
-        m.add('bar')
-        p = self.tmppath('m')
-        m.write(path=p)
-
-        self.assertTrue(os.path.exists(p))
-
-        m2 = PurgeManifest(path=p)
-        self.assertEqual(m.relpath, m2.relpath)
-        self.assertEqual(m.entries, m2.entries)
-        self.assertEqual(m, m2)
-
-    def test_unknown_version(self):
-        p = self.tmppath('bad')
-
-        with open(p, 'wt') as fh:
-            fh.write('2\n')
-            fh.write('not relevant')
-
-        with self.assertRaises(UnreadablePurgeManifest):
-            PurgeManifest(path=p)
 
 
 class TestInstallManifest(TestWithTmpDir):
