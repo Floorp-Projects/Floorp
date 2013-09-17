9a31e9df80268bd091c3c9852a278d1e6a6b7575
Eddy Bruel <ejpbruel@mozilla.com>
2013-09-17 15:37:00 +0300

    Make Debugger.Source load the source text if it is not retained (bug 916845). r=jimb

--- js/src/jsscript.cpp
+++ js/src/jsscript.cpp
@@ -991,19 +991,18 @@ ScriptSource::adjustDataSize(size_t nbytes)
 }
 
 /* static */ bool
-JSScript::loadSource(JSContext *cx, HandleScript script, bool *worked)
+JSScript::loadSource(JSContext *cx, ScriptSource *ss, bool *worked)
 {
-    JS_ASSERT(!script->scriptSource()->hasSourceData());
+    JS_ASSERT(!ss->hasSourceData());
     *worked = false;
-    if (!cx->runtime()->sourceHook || !script->scriptSource()->sourceRetrievable())
+    if (!cx->runtime()->sourceHook || !ss->sourceRetrievable())
         return true;
     jschar *src = NULL;
     uint32_t length;
-    if (!cx->runtime()->sourceHook(cx, script, &src, &length))
+    if (!cx->runtime()->sourceHook(cx, ss->filename(), &src, &length))
         return false;
     if (!src)
         return true;
-    ScriptSource *ss = script->scriptSource();
     ss->setSource(src, length);
     *worked = true;
     return true;
--- js/src/jsscript.h
+++ js/src/jsscript.h
@@ -797,7 +797,7 @@ class JSScript : public js::gc::Cell
 
     JSFlatString *sourceData(JSContext *cx);
 
-    static bool loadSource(JSContext *cx, js::HandleScript scr, bool *worked);
+    static bool loadSource(JSContext *cx, js::ScriptSource *ss, bool *worked);
 
     void setSourceObject(js::ScriptSourceObject *object);
     js::ScriptSourceObject *sourceObject() const;
