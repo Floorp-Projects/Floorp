6279351c2595b38e3a6bbc1dc66a58e11fd2e4d3
Paolo Amadini <paolo.mozmail@amadzone.org>
2013-09-17 11:48:20 +0200

    Bug 916433 - The task in the "tail.js" file of the Downloads API tests is not executed. r=felipe

--- toolkit/components/jsdownloads/test/unit/test_DownloadLegacy.js
+++ toolkit/components/jsdownloads/test/unit/test_DownloadLegacy.js
@@ -16,3 +16,9 @@ let gUseLegacySaver = true;
 
 let scriptFile = do_get_file("common_test_Download.js");
 Services.scriptloader.loadSubScript(NetUtil.newURI(scriptFile).spec);
+
+////////////////////////////////////////////////////////////////////////////////
+//// Termination
+
+let tailFile = do_get_file("tail.js");
+Services.scriptloader.loadSubScript(NetUtil.newURI(tailFile).spec);
