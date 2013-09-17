f1d92faedd31a5a3235b62c1ca5ab7efcd6a85be
Ehsan Akhgari <ehsan@mozilla.com>
2013-09-17 07:42:40 -0400

    Backed out 2 debugging changesets (bug 916757)
    Backed out changeset 1713de744990 (bug 916757)
    Backed out changeset 861e1c86d759 (bug 916757)

--- nsprpub/pr/src/md/windows/w95thred.c
+++ nsprpub/pr/src/md/windows/w95thred.c
@@ -5,8 +5,6 @@
 
 #include "primpl.h"
 #include <process.h>  /* for _beginthreadex() */
-#include <stdio.h>
-#include <windows.h>
 
 #if defined(_MSC_VER) && _MSC_VER <= 1200
 /*
@@ -219,10 +217,6 @@ _PR_MD_CLEAN_THREAD(PRThread *thread)
 
     if (thread->md.handle) {
         rv = CloseHandle(thread->md.handle);
-        if (!rv) {
-          fprintf(stderr, "CloseHandle failed, handle: %x, last error: %x\n",
-                  (void*)thread->md.handle, GetLastError());
-        }
         PR_ASSERT(rv);
         thread->md.handle = 0;
     }
