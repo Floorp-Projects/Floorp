f3595e0b0674d2abf9741d9e8152a98edae92d32
Ed Morley <emorley@mozilla.com>
2013-09-17 17:14:55 +0100

    Backed out changeset fb89f2090779 (bug 881959)

--- content/media/webaudio/AudioNode.h
+++ content/media/webaudio/AudioNode.h
@@ -130,10 +130,6 @@ public:
     return nullptr;
   }
 
-  virtual const DelayNode* AsDelayNode() const {
-    return nullptr;
-  }
-
   AudioContext* GetParentObject() const
   {
     return mContext;
