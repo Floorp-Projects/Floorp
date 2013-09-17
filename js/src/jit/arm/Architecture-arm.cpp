a09358a2db0f1fe347066137919ff2268cb2b35d
Luke Wagner <luke@mozilla.com>
2013-09-17 17:01:14 -0500

    Bug 917064 - ARM: enable HWCAP_ARMv7 on qemu (r=mjrosenb)
    --HG--
    extra : rebase_source : d92c495130f557f379b09ab445918521a74bf378

--- js/src/jit/arm/Architecture-arm.cpp
+++ js/src/jit/arm/Architecture-arm.cpp
@@ -60,6 +60,13 @@ uint32_t getFlags()
         }
         close(fd);
     }
+
+#if defined(__ARM_ARCH_7__) || defined (__ARM_ARCH_7A__)
+    flags = HWCAP_ARMv7;
+#endif
+    isSet = true;
+    return flags;
+
 #elif defined(WTF_OS_ANDROID) || defined(MOZ_B2G)
     FILE *fp = fopen("/proc/cpuinfo", "r");
     if (!fp)
@@ -98,7 +105,7 @@ uint32_t getFlags()
     return flags;
 #endif
 
-    return false;
+    return 0;
 }
 
 bool hasMOVWT()
