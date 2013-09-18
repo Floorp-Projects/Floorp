cc2da265ae3114821989d51749554804e683523c
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2013-09-18 23:49:31 +0200

    Bug 917271 - Fix race condition in browser_bug624734.js by checking for the completion of the async places call, r=dao

--- browser/base/content/test/browser_bug624734.js
+++ browser/base/content/test/browser_bug624734.js
@@ -4,6 +4,15 @@
 
 // Bug 624734 - Star UI has no tooltip until bookmarked page is visited
 
+function finishTest() {
+  is(BookmarkingUI.button.getAttribute("buttontooltiptext"),
+     BookmarkingUI._unstarredTooltip,
+     "Star icon should have the unstarred tooltip text");
+
+  gBrowser.removeCurrentTab();
+  finish();
+}
+
 function test() {
   waitForExplicitFinish();
 
@@ -11,12 +20,11 @@ function test() {
   tab.linkedBrowser.addEventListener("load", (function(event) {
     tab.linkedBrowser.removeEventListener("load", arguments.callee, true);
 
-    is(BookmarkingUI.button.getAttribute("buttontooltiptext"),
-       BookmarkingUI._unstarredTooltip,
-       "Star icon should have the unstarred tooltip text");
-
-    gBrowser.removeCurrentTab();
-    finish();
+    if (BookmarkingUI._pendingStmt) {
+      waitForCondition(function() !BookmarkingUI._pendingStmt, finishTest, "BookmarkingUI held pending statement for too long");
+    } else {
+      finishTest();
+    }
   }), true);
 
   tab.linkedBrowser.loadURI("http://example.com/browser/browser/base/content/test/dummy_page.html");
