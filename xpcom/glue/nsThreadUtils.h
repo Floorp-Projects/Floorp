50f33b797cf3e60c6482bf73725f3884a752e461
Benoit Jacob <bjacob@mozilla.com>
2013-09-18 18:50:32 -0400

    Bug 913847 - split NS_IsMainThread and NS_GetMainThread into a new MainThreadUtils.h header - r=ehsan

--- xpcom/glue/nsThreadUtils.h
+++ xpcom/glue/nsThreadUtils.h
@@ -9,7 +9,7 @@
 
 #include "prthread.h"
 #include "prinrval.h"
-#include "nscore.h"
+#include "MainThreadUtils.h"
 #include "nsIThreadManager.h"
 #include "nsIThread.h"
 #include "nsIRunnable.h"
@@ -17,7 +17,6 @@
 #include "nsStringGlue.h"
 #include "nsCOMPtr.h"
 #include "nsAutoPtr.h"
-#include "mozilla/threads/nsThreadIDs.h"
 #include "mozilla/Likely.h"
 
 //-----------------------------------------------------------------------------
@@ -90,41 +89,6 @@ NS_NewNamedThread(const char (&name)[LEN],
  */
 extern NS_COM_GLUE NS_METHOD
 NS_GetCurrentThread(nsIThread **result);
-
-/**
- * Get a reference to the main thread.
- *
- * @param result
- *   The resulting nsIThread object.
- */
-extern NS_COM_GLUE NS_METHOD
-NS_GetMainThread(nsIThread **result);
-
-#if defined(MOZILLA_INTERNAL_API) && defined(XP_WIN)
-bool NS_IsMainThread();
-#elif defined(MOZILLA_INTERNAL_API) && defined(NS_TLS)
-// This is defined in nsThreadManager.cpp and initialized to `Main` for the
-// main thread by nsThreadManager::Init.
-extern NS_TLS mozilla::threads::ID gTLSThreadID;
-#ifdef MOZ_ASAN
-// Temporary workaround, see bug 895845
-MOZ_ASAN_BLACKLIST static
-#else
-inline
-#endif
-bool NS_IsMainThread()
-{
-  return gTLSThreadID == mozilla::threads::Main;
-}
-#else
-/**
- * Test to see if the current thread is the main thread.
- *
- * @returns true if the current thread is the main thread, and false
- * otherwise.
- */
-extern NS_COM_GLUE bool NS_IsMainThread();
-#endif
 
 /**
  * Dispatch the given event to the current thread.
