441b3a6c6551ede902331ce9d25ff467036759eb
Shu-yu Guo <shu@rfrn.org>
2013-09-18 14:03:00 -0700

    Bug 917401 - Fix incorrect blocked register assignment in LSRA. (r=jandem)

--- js/src/jit/LinearScan.cpp
+++ js/src/jit/LinearScan.cpp
@@ -1080,7 +1080,7 @@ LinearScanAllocator::findBestBlockedRegister(CodePosition *nextUsed)
             if (nextUsePos[reg.code()] != CodePosition::MIN) {
                 CodePosition pos = i->intersect(current);
                 if (pos != CodePosition::MIN && pos < nextUsePos[reg.code()]) {
-                    nextUsePos[reg.code()] = pos;
+                    nextUsePos[reg.code()] = (pos == current->start()) ? CodePosition::MIN : pos;
                     IonSpew(IonSpew_RegAlloc, "   Register %s next used %u (fixed)", reg.name(), pos.pos());
                 }
             }
