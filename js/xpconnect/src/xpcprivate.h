03a9cc18eeec9ec34781e5fc252e06eeead26f73
Benoit Jacob <bjacob@mozilla.com>
2013-09-18 18:50:32 -0400

    Bug 913847 - Include MainThreadUtils.h instead of nsThreadUtils.h - r=ehsan

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -127,7 +127,7 @@
 #include "nsXPIDLString.h"
 #include "nsAutoJSValHolder.h"
 
-#include "nsThreadUtils.h"
+#include "MainThreadUtils.h"
 #include "nsIJSEngineTelemetryStats.h"
 
 #include "nsIConsoleService.h"
