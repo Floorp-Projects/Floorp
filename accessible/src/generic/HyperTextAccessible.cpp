0c36b90bba6e9eb79887cf09fe2e7e2a7dbfe19a
Alexander Surkov <surkov.alexander@gmail.com>
2013-09-18 10:28:53 -0400

    Bug 672717 - Broken caret when moving into/out of embedded objects with right arrow, r=tbsaunde

--- accessible/src/generic/HyperTextAccessible.cpp
+++ accessible/src/generic/HyperTextAccessible.cpp
@@ -593,14 +593,8 @@ HyperTextAccessible::DOMPointToHypertextOffset(nsINode* aNode,
     // addTextOffset, to put us after the embedded object char. We'll only treat the offset as
     // before the embedded object char if we end at the very beginning of the child.
     addTextOffset = addTextOffset > 0;
-    }
-    else {
-      // Start offset, inclusive
-      // Make sure the offset lands on the embedded object character in order to indicate
-      // the true inner offset is inside the subtree for that link
-      addTextOffset =
-        (nsAccUtils::TextLength(descendantAcc) == addTextOffset) ? 1 : 0;
-    }
+    } else
+      addTextOffset = 0;
 
     descendantAcc = parentAcc;
   }
