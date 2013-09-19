d9bd8c70bb78906a4fd35715d5089e190c6899cf
Benoit Jacob <bjacob@mozilla.com>
2013-09-19 16:02:03 -0400

    Bug 918330 - GfxInfoCollector.h should not include jsapi.h - r=ehsan

---
+++ widget/xpwidgets/GfxInfoCollector.cpp
@@ -0,0 +1,53 @@
+/* vim: se cin sw=2 ts=2 et : */
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "GfxInfoCollector.h"
+#include "jsapi.h"
+#include "nsStringGlue.h"
+
+using namespace mozilla;
+using namespace widget;
+
+void
+InfoObject::DefineProperty(const char *name, int value)
+{
+  if (!mOk)
+    return;
+
+  mOk = JS_DefineProperty(mCx, mObj, name, INT_TO_JSVAL(value), NULL, NULL, JSPROP_ENUMERATE);
+}
+
+void
+InfoObject::DefineProperty(const char *name, nsAString &value)
+{
+  if (!mOk)
+    return;
+
+  const nsString &flat = PromiseFlatString(value);
+  JSString *string = JS_NewUCStringCopyN(mCx, static_cast<const jschar*>(flat.get()), flat.Length());
+  if (!string)
+    mOk = false;
+
+  if (!mOk)
+    return;
+
+  mOk = JS_DefineProperty(mCx, mObj, name, STRING_TO_JSVAL(string), NULL, NULL, JSPROP_ENUMERATE);
+}
+
+void
+InfoObject::DefineProperty(const char *name, const char *value)
+{
+  nsAutoString string = NS_ConvertASCIItoUTF16(value);
+  DefineProperty(name, string);
+}
+
+InfoObject::InfoObject(JSContext *aCx) : mCx(aCx), mObj(aCx), mOk(true)
+{
+  mObj = JS_NewObject(mCx, NULL, NULL, NULL);
+  if (!mObj)
+    mOk = false;
+}
