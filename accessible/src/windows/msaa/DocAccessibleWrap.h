c33efa6d70142d1b62c7f9bdfa0d622c324fa61a
Alexander Surkov <surkov.alexander@gmail.com>
2013-09-19 19:55:03 -0400

    Bug 917973 - tear off ISimpleDOMDocument, r=tbsaunde
    --HG--
    rename : accessible/src/windows/msaa/DocAccessibleWrap.cpp => accessible/src/windows/sdn/sdnDocAccessible.cpp
    rename : accessible/src/windows/msaa/DocAccessibleWrap.h => accessible/src/windows/sdn/sdnDocAccessible.h

--- accessible/src/windows/msaa/DocAccessibleWrap.h
+++ accessible/src/windows/msaa/DocAccessibleWrap.h
@@ -1,57 +1,27 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* For documentation of the accessibility architecture, 
- * see http://lxr.mozilla.org/seamonkey/source/accessible/accessible-docs.html
- */
-
 #ifndef mozilla_a11y_DocAccessibleWrap_h__
 #define mozilla_a11y_DocAccessibleWrap_h__
 
-#include "ISimpleDOMDocument.h"
-
 #include "DocAccessible.h"
-#include "nsIDocShellTreeItem.h"
 
 namespace mozilla {
 namespace a11y {
 
-class DocAccessibleWrap : public DocAccessible,
-                          public ISimpleDOMDocument
+class DocAccessibleWrap : public DocAccessible
 {
 public:
   DocAccessibleWrap(nsIDocument* aDocument, nsIContent* aRootContent,
                     nsIPresShell* aPresShell);
   virtual ~DocAccessibleWrap();
 
-    // IUnknown
-    STDMETHODIMP_(ULONG) AddRef();
-    STDMETHODIMP_(ULONG) Release();
-    STDMETHODIMP      QueryInterface(REFIID, void**);
+  DECL_IUNKNOWN_INHERITED
 
-    // ISimpleDOMDocument
-    virtual /* [id][propget] */ HRESULT STDMETHODCALLTYPE get_URL( 
-        /* [out] */ BSTR __RPC_FAR *url);
-    
-    virtual /* [id][propget] */ HRESULT STDMETHODCALLTYPE get_title( 
-        /* [out] */ BSTR __RPC_FAR *title);
-    
-    virtual /* [id][propget] */ HRESULT STDMETHODCALLTYPE get_mimeType( 
-        /* [out] */ BSTR __RPC_FAR *mimeType);
-    
-    virtual /* [id][propget] */ HRESULT STDMETHODCALLTYPE get_docType( 
-        /* [out] */ BSTR __RPC_FAR *docType);
-    
-    virtual /* [id][propget] */ HRESULT STDMETHODCALLTYPE get_nameSpaceURIForID( 
-        /* [in] */ short nameSpaceID,
-        /* [out] */ BSTR __RPC_FAR *nameSpaceURI);
-
-    virtual /* [id] */ HRESULT STDMETHODCALLTYPE put_alternateViewMediaTypes( 
-        /* [in] */ BSTR __RPC_FAR *commaSeparatedMediaTypes);
-
-    // IAccessible
+  // IAccessible
 
     // Override get_accValue to provide URL when no other value is available
     virtual /* [id][propget] */ HRESULT STDMETHODCALLTYPE get_accValue( 
