b3303d897fbb7da9c9be0150aeca0f4235a4fa3b
Robert Bindar <robertbindar@gmail.com>
2013-09-19 08:50:21 -0400

    Bug 915090 - Ping test and local servers for reaching tests. r=mcmanus

--- netwerk/test/unit/test_about_networking.js
+++ netwerk/test/unit/test_about_networking.js
@@ -5,15 +5,20 @@
 
 const Cc = Components.classes;
 const Ci = Components.interfaces;
+const Cu = Components.utils;
 
-let gDashboard = Cc['@mozilla.org/network/dashboard;1']
+Cu.import("resource://testing-common/httpd.js");
+
+const gDashboard = Cc['@mozilla.org/network/dashboard;1']
   .getService(Ci.nsIDashboard);
 
-const RESOLVE_DISABLE_IPV6 = (1 << 5);
+const gServerSocket = Components.classes["@mozilla.org/network/server-socket;1"]
+                             .createInstance(Components.interfaces.nsIServerSocket);
+const gHttpServer = new HttpServer();
 
 add_test(function test_http() {
   gDashboard.requestHttpConnections(function(data) {
-    do_check_neq(data.host.indexOf("example.com"), -1);
+    do_check_neq(data.host.indexOf("localhost"), -1);
 
     run_next_test();
   });
@@ -21,31 +26,53 @@ add_test(function test_http() {
 
 add_test(function test_dns() {
   gDashboard.requestDNSInfo(function(data) {
-    do_check_neq(data.hostname.indexOf("example.com"), -1);
+    do_check_neq(data.hostname.indexOf("localhost"), -1);
+
+    do_test_pending();
+    gHttpServer.stop(do_test_finished);
 
     run_next_test();
   });
 });
 
 add_test(function test_sockets() {
-  let dns = Cc["@mozilla.org/network/dns-service;1"].getService(Ci.nsIDNSService);
-  let record = dns.resolve("example.com", RESOLVE_DISABLE_IPV6);
-  let answer = record.getNextAddrAsString();
+  let sts = Cc["@mozilla.org/network/socket-transport-service;1"]
+    .getService(Ci.nsISocketTransportService);
+  let threadManager = Cc["@mozilla.org/thread-manager;1"].getService();
 
-  gDashboard.requestSockets(function(data) {
-    do_check_neq(data.host.indexOf(answer), -1);
+  let transport = sts.createTransport(null, 0, "127.0.0.1",
+                                      gServerSocket.port, null);
+  let listener = {
+    onTransportStatus: function(aTransport, aStatus, aProgress, aProgressMax) {
+      if (aStatus == Ci.nsISocketTransport.STATUS_CONNECTED_TO) {
+        gDashboard.requestSockets(function(data) {
+          gServerSocket.close();
 
-    run_next_test();
-  });
+          do_check_neq(data.host.indexOf("127.0.0.1"), -1);
+
+          run_next_test();
+        });
+      }
+    }
+  };
+  transport.setEventSink(listener, threadManager.currentThread);
+
+  transport.openOutputStream(Ci.nsITransport.OPEN_BLOCKING, 0, 0);
 });
 
 function run_test() {
   let ioService = Cc["@mozilla.org/network/io-service;1"]
     .getService(Ci.nsIIOService);
-  let uri = ioService.newURI("http://example.com", null, null);
+
+  gHttpServer.start(-1);
+
+  let uri = ioService.newURI("http://localhost:" + gHttpServer.identity.primaryPort,
+                             null, null);
   let channel = ioService.newChannelFromURI(uri);
 
   channel.open();
+
+  gServerSocket.init(-1, true, -1);
 
   run_next_test();
 }
