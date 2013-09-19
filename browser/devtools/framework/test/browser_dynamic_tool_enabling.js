cb47ef771c9b354588660d458629937a1d8a3f00
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2013-09-19 09:11:44 +0200

    Bug 917706 - add tests for updating disabled/hidden values dynamically, r=test-only
    --HG--
    extra : rebase_source : b7be8eefe9387310328bc2cafe98f8ef75685447

---
+++ browser/devtools/framework/test/browser_dynamic_tool_enabling.js
@@ -0,0 +1,40 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Tests that toggling prefs immediately (de)activates the relevant menuitem
+
+let gItemsToTest = {
+  "menu_devToolbar": "devtools.toolbar.enabled",
+  "menu_devAppMgr": "devtools.appmanager.enabled",
+  "menu_chromeDebugger": ["devtools.chrome.enabled", "devtools.debugger.remote-enabled", "devtools.debugger.chrome-enabled"],
+  "javascriptConsole": "devtools.errorconsole.enabled",
+  "menu_devtools_connect": "devtools.debugger.remote-enabled",
+};
+
+function expectedAttributeValueFromPrefs(prefs) {
+  return prefs.every((pref) => Services.prefs.getBoolPref(pref)) ?
+         "" : "true";
+}
+
+function checkItem(el, prefs) {
+  let expectedValue = expectedAttributeValueFromPrefs(prefs);
+  is(el.getAttribute("disabled"), expectedValue, "disabled attribute should match current pref state");
+  is(el.getAttribute("hidden"), expectedValue, "hidden attribute should match current pref state");
+}
+
+function test() {
+  for (let k in gItemsToTest) {
+    let el = document.getElementById(k);
+    let prefs = gItemsToTest[k];
+    if (typeof prefs == "string") {
+      prefs = [prefs];
+    }
+    checkItem(el, prefs);
+    for (let pref of prefs) {
+      Services.prefs.setBoolPref(pref, !Services.prefs.getBoolPref(pref));
+      checkItem(el, prefs);
+      Services.prefs.setBoolPref(pref, !Services.prefs.getBoolPref(pref));
+      checkItem(el, prefs);
+    }
+  }
+}
