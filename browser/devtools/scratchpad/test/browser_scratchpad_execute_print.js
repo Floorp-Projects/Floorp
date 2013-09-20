ff54039ba423e6cdacaeeb7d6d63cd4b0a4c0e47
Anton Kovalyov <anton@mozilla.com>
2013-09-20 11:34:10 -0700

    Bug 912260 - Make Scratchpad use CodeMirror. r=robcee r=msucan

--- browser/devtools/scratchpad/test/browser_scratchpad_execute_print.js
+++ browser/devtools/scratchpad/test/browser_scratchpad_execute_print.js
@@ -23,7 +23,7 @@ function runTests()
     method: "run",
     prepare: function() {
       content.wrappedJSObject.foobarBug636725 = 1;
-      sp.setText("++window.foobarBug636725");
+      sp.editor.setText("++window.foobarBug636725");
     },
     then: function([code, , result]) {
       is(code, sp.getText(), "code is correct");
@@ -47,34 +47,15 @@ function runTests()
       is(sp.getText(), "++window.foobarBug636725\n/*\n3\n*/",
          "display() shows evaluation result in the textbox");
 
-      is(sp.selectedText, "\n/*\n3\n*/", "selectedText is correct");
+      is(sp.editor.getSelection(), "\n/*\n3\n*/", "getSelection is correct");
     }
   },
   {
     method: "run",
     prepare: function() {
-      let selection = sp.getSelectionRange();
-      is(selection.start, 24, "selection.start is correct");
-      is(selection.end, 32, "selection.end is correct");
-
-      // Test selection run() and display().
-
-      sp.setText("window.foobarBug636725 = 'a';\n" +
-                 "window.foobarBug636725 = 'b';");
-
-      sp.selectRange(1, 2);
-
-      selection = sp.getSelectionRange();
-
-      is(selection.start, 1, "selection.start is 1");
-      is(selection.end, 2, "selection.end is 2");
-
-      sp.selectRange(0, 29);
-
-      selection = sp.getSelectionRange();
-
-      is(selection.start, 0, "selection.start is 0");
-      is(selection.end, 29, "selection.end is 29");
+      sp.editor.setText("window.foobarBug636725 = 'a';\n" +
+        "window.foobarBug636725 = 'b';");
+      sp.editor.setSelection({ line: 0, ch: 0 }, { line: 0, ch: 29 });
     },
     then: function([code, , result]) {
       is(code, "window.foobarBug636725 = 'a';", "code is correct");
@@ -91,10 +72,9 @@ function runTests()
   {
     method: "display",
     prepare: function() {
-      sp.setText("window.foobarBug636725 = 'c';\n" +
+      sp.editor.setText("window.foobarBug636725 = 'c';\n" +
                  "window.foobarBug636725 = 'b';");
-
-      sp.selectRange(0, 22);
+      sp.editor.setSelection({ line: 0, ch: 0 }, { line: 0, ch: 22 });
     },
     then: function() {
       is(content.wrappedJSObject.foobarBug636725, "a",
@@ -106,27 +86,19 @@ function runTests()
                        "window.foobarBug636725 = 'b';",
          "display() shows evaluation result in the textbox");
 
-      is(sp.selectedText, "\n/*\na\n*/", "selectedText is correct");
+      is(sp.editor.getSelection(), "\n/*\na\n*/", "getSelection is correct");
     }
-  }]
-
+  }];
 
   runAsyncCallbackTests(sp, tests).then(function() {
-    let selection = sp.getSelectionRange();
-    is(selection.start, 22, "selection.start is correct");
-    is(selection.end, 30, "selection.end is correct");
-
-    sp.deselect();
-
-    ok(!sp.selectedText, "selectedText is empty");
-
-    selection = sp.getSelectionRange();
-    is(selection.start, selection.end, "deselect() works");
+    ok(sp.editor.somethingSelected(), "something is selected");
+    sp.editor.dropSelection();
+    ok(!sp.editor.somethingSelected(), "something is no longer selected");
+    ok(!sp.editor.getSelection(), "getSelection is empty");
 
     // Test undo/redo.
-
-    sp.setText("foo1");
-    sp.setText("foo2");
+    sp.editor.setText("foo1");
+    sp.editor.setText("foo2");
     is(sp.getText(), "foo2", "editor content updated");
     sp.undo();
     is(sp.getText(), "foo1", "undo() works");
