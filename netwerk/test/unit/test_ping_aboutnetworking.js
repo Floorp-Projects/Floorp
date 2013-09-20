b4b33f6eda52275cbf109ab0d7cab412c468e2d1
Ed Morley <emorley@mozilla.com>
2013-09-20 12:33:47 +0100

    Bug 913807 - Fix up merge conflicts

--- netwerk/test/unit/test_ping_aboutnetworking.js
+++ netwerk/test/unit/test_ping_aboutnetworking.js
@@ -3,9 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-
 const gDashboard = Cc['@mozilla.org/network/dashboard;1']
   .getService(Ci.nsIDashboard);
 
