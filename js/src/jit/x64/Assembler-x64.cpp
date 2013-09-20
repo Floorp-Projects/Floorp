f3de7a6cf3327c06e6b5a4001b8a4151d5aa574b
Dan Gohman <sunfish@google.com>
2013-09-20 06:17:15 -0700

    Bug 918023 - IonMonkey: Misc cleanups. r=kvijayan

--- js/src/jit/x64/Assembler-x64.cpp
+++ js/src/jit/x64/Assembler-x64.cpp
@@ -146,7 +146,7 @@ Assembler::finish()
         return;
 
     // Emit the jump table.
-    masm.align(16);
+    masm.align(SizeOfJumpTableEntry);
     extendedJumpTable_ = masm.size();
 
     // Now that we know the offset to the jump table, squirrel it into the
