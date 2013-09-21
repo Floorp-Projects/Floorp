e64e49e6e8cc9ed93c9b8b733eed30a9f81b6bf8
Gabor Krizsanits <gkrizsanits@mozilla.com>
2013-09-21 15:25:46 +0200

    Bug 892214 - Rename DOMConstructors to GlobalProperties in SandboxOptions. r=bholley. r=mossop, a=lsblakk

--- addon-sdk/source/lib/sdk/content/worker.js
+++ addon-sdk/source/lib/sdk/content/worker.js
@@ -130,13 +130,13 @@ const WorkerSandbox = EventEmitter.compose({
     // Even if this principal is for a domain that is specified in the multiple
     // domain principal.
     let principals  = window;
-    let wantDOMConstructors = []
+    let wantGlobalProperties = []
     if (EXPANDED_PRINCIPALS.length > 0 && !worker._injectInDocument) {
       principals = EXPANDED_PRINCIPALS.concat(window);
       // We have to replace XHR constructor of the content document
       // with a custom cross origin one, automagically added by platform code:
       delete proto.XMLHttpRequest;
-      wantDOMConstructors.push("XMLHttpRequest");
+      wantGlobalProperties.push("XMLHttpRequest");
     }
 
     // Instantiate trusted code in another Sandbox in order to prevent content
@@ -149,7 +149,7 @@ const WorkerSandbox = EventEmitter.compose({
     let content = this._sandbox = sandbox(principals, {
       sandboxPrototype: proto,
       wantXrays: true,
-      wantDOMConstructors: wantDOMConstructors,
+      wantGlobalProperties: wantGlobalProperties,
       sameZoneAs: window
     });
     // We have to ensure that window.top and window.parent are the exact same
