60bf3907d25c0cfd9ee867972921ad859c0e25a1
Jim Chen <nchen@mozilla.com>
2013-09-23 17:23:37 -0400

    Bug 909940 - Disable updating the URL bar type for HTC keyboards. r=cpeterson, a=akeybl

--- mobile/android/base/InputMethods.java
+++ mobile/android/base/InputMethods.java
@@ -61,6 +61,11 @@ final class InputMethods {
         return METHOD_HTC_TOUCH_INPUT.equals(inputMethod);
     }
 
+    public static boolean shouldDisableUrlBarUpdate(Context context) {
+        String inputMethod = getCurrentInputMethod(context);
+        return METHOD_HTC_TOUCH_INPUT.equals(inputMethod);
+    }
+
     public static boolean shouldDelayUrlBarUpdate(Context context) {
         String inputMethod = getCurrentInputMethod(context);
         return METHOD_SAMSUNG.equals(inputMethod) ||
