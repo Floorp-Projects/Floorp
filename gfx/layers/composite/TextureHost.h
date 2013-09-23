32a2e7bb30413e5d99d85cd7251cc06198079bd8
Nicolas Silva <nical@mozilla.com>
2013-09-23 13:28:40 +0200

    Bug 918078 - Do not require locking in TextureHost::PrintInfo. r=BenWa

--- gfx/layers/composite/TextureHost.h
+++ gfx/layers/composite/TextureHost.h
@@ -120,10 +120,6 @@ public:
 
   virtual void SetCompositableQuirks(CompositableQuirks* aQuirks);
 
-#ifdef MOZ_LAYERS_HAVE_LOG
-  virtual void PrintInfo(nsACString& aTo, const char* aPrefix);
-#endif
-
 protected:
   RefPtr<CompositableQuirks> mQuirks;
 };
@@ -389,13 +385,8 @@ public:
   virtual void SetCompositableQuirks(CompositableQuirks* aQuirks);
 
 #ifdef MOZ_LAYERS_HAVE_LOG
-  virtual void PrintInfo(nsACString& aTo, const char* aPrefix)
-  {
-    RefPtr<TextureSource> source = GetTextureSources();
-    if (source) {
-      source->PrintInfo(aTo, aPrefix);
-    }
-  }
+  virtual const char *Name() { return "TextureHost"; }
+  virtual void PrintInfo(nsACString& aTo, const char* aPrefix);
 #endif
 
 protected:
@@ -489,6 +480,10 @@ public:
 
   virtual uint8_t* GetBuffer() MOZ_OVERRIDE;
 
+#ifdef MOZ_LAYERS_HAVE_LOG
+  virtual const char *Name() MOZ_OVERRIDE { return "ShmemTextureHost"; }
+#endif
+
 protected:
   ipc::Shmem* mShmem;
   ISurfaceAllocator* mDeallocator;
@@ -513,6 +508,10 @@ public:
   virtual void DeallocateSharedData() MOZ_OVERRIDE;
 
   virtual uint8_t* GetBuffer() MOZ_OVERRIDE;
+
+#ifdef MOZ_LAYERS_HAVE_LOG
+  virtual const char *Name() MOZ_OVERRIDE { return "MemoryTextureHost"; }
+#endif
 
 protected:
   uint8_t* mBuffer;
