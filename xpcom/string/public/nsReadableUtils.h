a04c43320d0491cf44465c593e38c9eeffd7220e
Ehsan Akhgari <ehsan@mozilla.com>
2013-09-23 13:23:56 -0400

    Bug 918923 - Part 1: Fix the include-what-you-use pragmas in the string headers; r=bsmedberg

--- xpcom/string/public/nsReadableUtils.h
+++ xpcom/string/public/nsReadableUtils.h
@@ -2,7 +2,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-// IWYU pragma: private, include "nsStringGlue.h"
+// IWYU pragma: private, include "nsString.h"
 
 #ifndef nsReadableUtils_h___
 #define nsReadableUtils_h___
