31f5a1ac5d3c5216a2b4db31741450609623cc38
Benjamin Peterson <benjamin@python.org>
2013-09-23 08:35:33 -0400

    Bug 919175 - Fix some unused variable warnings on optimized builds in jit/. r=jandem

--- js/src/jit/Lowering.cpp
+++ js/src/jit/Lowering.cpp
@@ -512,7 +512,7 @@ bool
 LIRGenerator::visitAssertFloat32(MAssertFloat32 *assertion)
 {
     MIRType type = assertion->input()->type();
-    bool checkIsFloat32 = assertion->mustBeFloat32();
+    DebugOnly<bool> checkIsFloat32 = assertion->mustBeFloat32();
 
     if (!allowFloat32Optimizations())
         return true;
@@ -1972,7 +1972,7 @@ LIRGenerator::visitTypeBarrier(MTypeBarrier *ins)
     bool needTemp = !types->unknownObject() && types->getObjectCount() > 0;
 
     MIRType inputType = ins->getOperand(0)->type();
-    MIRType outputType = ins->type();
+    DebugOnly<MIRType> outputType = ins->type();
 
     JS_ASSERT(inputType == outputType);
 
@@ -2465,7 +2465,7 @@ LIRGenerator::visitStoreTypedArrayElement(MStoreTypedArrayElement *ins)
     JS_ASSERT(ins->index()->type() == MIRType_Int32);
 
     if (ins->isFloatArray()) {
-        bool optimizeFloat32 = allowFloat32Optimizations();
+        DebugOnly<bool> optimizeFloat32 = allowFloat32Optimizations();
         JS_ASSERT_IF(optimizeFloat32 && ins->arrayType() == ScalarTypeRepresentation::TYPE_FLOAT32,
                      ins->value()->type() == MIRType_Float32);
         JS_ASSERT_IF(!optimizeFloat32 || ins->arrayType() == ScalarTypeRepresentation::TYPE_FLOAT64,
@@ -2494,7 +2494,7 @@ LIRGenerator::visitStoreTypedArrayElementHole(MStoreTypedArrayElementHole *ins)
     JS_ASSERT(ins->length()->type() == MIRType_Int32);
 
     if (ins->isFloatArray()) {
-        bool optimizeFloat32 = allowFloat32Optimizations();
+        DebugOnly<bool> optimizeFloat32 = allowFloat32Optimizations();
         JS_ASSERT_IF(optimizeFloat32 && ins->arrayType() == ScalarTypeRepresentation::TYPE_FLOAT32,
                      ins->value()->type() == MIRType_Float32);
         JS_ASSERT_IF(!optimizeFloat32 || ins->arrayType() == ScalarTypeRepresentation::TYPE_FLOAT64,
