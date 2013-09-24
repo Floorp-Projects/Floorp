9f697120a109da354087018e95efd01a1df7cb9c
Kannan Vijayan <kvijayan@mozilla.com>
2013-09-24 16:38:45 -0400

    Bug 905903 - Properly trace ICTypeMonitor chains for pre-barrier when unlinking them. r=terrence, a=lsblakk

--- js/src/jit/BaselineIC.cpp
+++ js/src/jit/BaselineIC.cpp
@@ -468,7 +468,10 @@ ICTypeMonitor_Fallback::resetMonitorStubChain(Zone *zone)
         // We are removing edges from monitored stubs to gcthings (IonCode).
         // Perform one final trace of all monitor stubs for incremental GC,
         // as it must know about those edges.
-        this->trace(zone->barrierTracer());
+        if (hasFallbackStub_) {
+            for (ICStub *s = firstMonitorStub_; !s->isTypeMonitor_Fallback(); s = s->next())
+                s->trace(zone->barrierTracer());
+        }
     }
 
     firstMonitorStub_ = this;
