4752bb93101fcea38f22a8448cd3ef9790af45eb
Benoit Girard <b56girard@gmail.com>
2013-09-24 15:23:36 -0400

    Bug 918833 - Improved profiler thread register. r=ehsan

--- tools/profiler/platform-macos.cc
+++ tools/profiler/platform-macos.cc
@@ -362,11 +362,23 @@ bool Sampler::RegisterCurrentThread(const char* aName,
   if (!Sampler::sRegisteredThreadsMutex)
     return false;
 
-  set_tls_stack_top(stackTop);
 
   mozilla::MutexAutoLock lock(*Sampler::sRegisteredThreadsMutex);
 
-  ThreadInfo* info = new ThreadInfo(aName, gettid(),
+  int id = gettid();
+  for (uint32_t i = 0; i < sRegisteredThreads->size(); i++) {
+    ThreadInfo* info = sRegisteredThreads->at(i);
+    if (info->ThreadId() == id) {
+      // Thread already registered. This means the first unregister will be
+      // too early.
+      ASSERT(false);
+      return false;
+    }
+  }
+
+  set_tls_stack_top(stackTop);
+
+  ThreadInfo* info = new ThreadInfo(aName, id,
     aIsMainThread, aPseudoStack, stackTop);
 
   if (sActiveSampler) {
