b175ce8d7982fff67bb993efd6f377dcca414da5
Karl Tomlinson <karlt+@karlt.net>
2013-09-24 13:45:41 +1200

    b=914033 use AudioContext::RegisterActiveNode() in OscillatorNode r=ehsan
    Storing references on the AudioContext instead of on the AudioNodes will allow
    the AudioContext to report them to the cycle collector until offline rendering
    starts.

--- content/media/webaudio/AudioContext.h
+++ content/media/webaudio/AudioContext.h
@@ -229,7 +229,6 @@ public:
 
   void UnregisterAudioBufferSourceNode(AudioBufferSourceNode* aNode);
   void UnregisterPannerNode(PannerNode* aNode);
-  void UnregisterOscillatorNode(OscillatorNode* aNode);
   void UnregisterScriptProcessorNode(ScriptProcessorNode* aNode);
   void UpdatePannerSource();
 
@@ -262,7 +261,6 @@ private:
   // These are all weak pointers.
   nsTHashtable<nsPtrHashKey<PannerNode> > mPannerNodes;
   nsTHashtable<nsPtrHashKey<AudioBufferSourceNode> > mAudioBufferSourceNodes;
-  nsTHashtable<nsPtrHashKey<OscillatorNode> > mOscillatorNodes;
   // Hashset containing all ScriptProcessorNodes in order to stop them.
   // These are all weak pointers.
   nsTHashtable<nsPtrHashKey<ScriptProcessorNode> > mScriptProcessorNodes;
