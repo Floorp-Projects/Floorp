dbe2c747268d8a73a1dd584856563025dfd2ad92
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2013-09-25 09:37:22 -0400

    Bug 919506 - Fix browser.bookmarks.autoExportHTML not working due to bug 910885. r=mak, a=lsblakk

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -1204,7 +1204,7 @@ BrowserGlue.prototype = {
       // potential hangs (bug 518683).  The asynchronous shutdown operations
       // will then be handled by a shutdown service (bug 435058).
       waitingForHTMLExportToComplete = false;
-      BookmarkHTMLUtils.exportToFile(FileUtils.getFile("BMarks", [])).then(
+      BookmarkHTMLUtils.exportToFile(Services.dirsvc.get("BMarks", Ci.nsIFile)).then(
         function onSuccess() {
           waitingForHTMLExportToComplete = true;
         },
