6e09f7a0d457add94f96a1ddb143af32f94cc710
Georg Fritzsche <georg.fritzsche@googlemail.com>
2013-09-26 10:12:51 +0200

    Bug 916542 - Guard against non-plugin elements in DOMWindowUtils.plugins. r=jaws, a=lsblakk

--- browser/base/content/browser-plugins.js
+++ browser/base/content/browser-plugins.js
@@ -795,7 +795,9 @@ var gPluginHandler = {
     let contentDoc = aBrowser.contentDocument;
     let cwu = contentWindow.QueryInterface(Ci.nsIInterfaceRequestor)
                            .getInterface(Ci.nsIDOMWindowUtils);
-    let plugins = cwu.plugins;
+    // cwu.plugins may contain non-plugin <object>s, filter them out
+    let plugins = cwu.plugins.filter((plugin) =>
+      plugin.getContentTypeForMIMEType(plugin.actualType) == Ci.nsIObjectLoadingContent.TYPE_PLUGIN);
     if (plugins.length == 0) {
       if (notification) {
         PopupNotifications.remove(notification);
