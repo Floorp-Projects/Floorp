fb13bfe47b45634a788aaef00027435c837046a7
Adam Roach [:abr] <adam@nostrum.com>
2013-09-25 19:58:16 -0500

    Bug 842549 - Part 5: Fix slots cleanup when VcmSIPCCBinding is destroyed r=ekr

--- media/webrtc/signaling/src/media/VcmSIPCCBinding.cpp
+++ media/webrtc/signaling/src/media/VcmSIPCCBinding.cpp
@@ -125,10 +125,14 @@ VcmSIPCCBinding::~VcmSIPCCBinding ()
 {
     assert(gSelf);
     gSelf = NULL;
-    SyncRunnable::DispatchToThread(gSTSThread,
-                                   WrapRunnable(this,
-                                                &VcmSIPCCBinding::disconnect_all));
-    disconnect_all();
+    // In case we're torn down while STS is still running,
+    // we try to dispatch to STS to disconnect all of the
+    // ICE signals. If STS is no longer running, this will
+    // harmlessly fail.
+    SyncRunnable::DispatchToThread(
+      gSTSThread,
+      WrapRunnable(this, &VcmSIPCCBinding::disconnect_all),
+      true);
 }
 
 void VcmSIPCCBinding::CandidateReady(NrIceMediaStream* stream,
