32aaf156be3eb6bb81bf8e145632f5455fd2e43c
Chris Kitching <ckitching@mozilla.com>
2013-09-27 10:09:09 -0400

    Bug 913985: Part 2 - Add annotations to all remaining JNI entry points, marking them for generations. r=kats

--- mobile/android/base/gfx/LayerRenderer.java
+++ mobile/android/base/gfx/LayerRenderer.java
@@ -12,6 +12,8 @@ import org.mozilla.gecko.Tabs;
 import org.mozilla.gecko.gfx.Layer.RenderContext;
 import org.mozilla.gecko.gfx.RenderTask;
 import org.mozilla.gecko.mozglue.DirectBufferAllocator;
+import org.mozilla.gecko.mozglue.generatorannotations.GeneratorOptions;
+import org.mozilla.gecko.mozglue.generatorannotations.WrapElementForJNI;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -19,7 +21,6 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
-import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.RectF;
@@ -437,6 +438,7 @@ public class LayerRenderer implements Tabs.OnTabsChangedListener {
         }
     }
 
+    @GeneratorOptions(generatedClassName = "LayerRendererFrame")
     public class Frame {
         // The timestamp recording the start of this frame.
         private long mFrameStartTime;
@@ -490,6 +492,7 @@ public class LayerRenderer implements Tabs.OnTabsChangedListener {
         }
 
         /** This function is invoked via JNI; be careful when modifying signature. */
+        @WrapElementForJNI(allowMultithread = true)
         public void beginDrawing() {
             mFrameStartTime = System.nanoTime();
 
@@ -579,6 +582,7 @@ public class LayerRenderer implements Tabs.OnTabsChangedListener {
         }
 
         /** This function is invoked via JNI; be careful when modifying signature. */
+        @WrapElementForJNI(allowMultithread = true)
         public void drawBackground() {
             GLES20.glDisable(GLES20.GL_SCISSOR_TEST);
 
@@ -611,6 +615,7 @@ public class LayerRenderer implements Tabs.OnTabsChangedListener {
         }
 
         /** This function is invoked via JNI; be careful when modifying signature. */
+        @WrapElementForJNI(allowMultithread = true)
         public void drawForeground() {
             /* Draw any extra layers that were added (likely plugins) */
             if (mExtraLayers.size() > 0) {
@@ -661,6 +666,7 @@ public class LayerRenderer implements Tabs.OnTabsChangedListener {
         }
 
         /** This function is invoked via JNI; be careful when modifying signature. */
+        @WrapElementForJNI(allowMultithread = true)
         public void endDrawing() {
             // If a layer update requires further work, schedule another redraw
             if (!mUpdated)
