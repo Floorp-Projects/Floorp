df224766fccf1eadb02c0fd0a9a58f0bc569498e
Masayuki Nakano <masayuki@d-toybox.com>
2013-09-27 15:20:54 +0900

    Bug 920377 part.5 Get rid of nsAnimationEvent r=roc

--- content/events/src/nsEventDispatcher.cpp
+++ content/events/src/nsEventDispatcher.cpp
@@ -747,7 +747,7 @@ nsEventDispatcher::CreateEvent(mozilla::dom::EventTarget* aOwner,
                                       static_cast<nsTransitionEvent*>(aEvent));
     case NS_ANIMATION_EVENT:
       return NS_NewDOMAnimationEvent(aDOMEvent, aOwner, aPresContext,
-                                     static_cast<nsAnimationEvent*>(aEvent));
+               static_cast<InternalAnimationEvent*>(aEvent));
     default:
       // For all other types of events, create a vanilla event object.
       return NS_NewDOMEvent(aDOMEvent, aOwner, aPresContext, aEvent);
