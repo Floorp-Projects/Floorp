00cc4934c81076920959d72f2285134d810d62a3
Alexander Surkov <surkov.alexander@gmail.com>
2013-09-27 10:15:19 -0400

    Bug 466481 - Arabic and Hebrew characters bounds are incorrect in a11y APIs, r=tbsaunde

--- accessible/src/generic/HyperTextAccessible.cpp
+++ accessible/src/generic/HyperTextAccessible.cpp
@@ -188,13 +188,14 @@ HyperTextAccessible::GetBoundsForString(nsIFrame* aFrame, uint32_t aStartRendere
     nsPoint frameTextStartPoint;
     rv = frame->GetPointFromOffset(startContentOffset, &frameTextStartPoint);
     NS_ENSURE_SUCCESS(rv, nsIntRect());
-    frameScreenRect.x += frameTextStartPoint.x;
 
     // Use the point for the end offset to calculate the width
     nsPoint frameTextEndPoint;
     rv = frame->GetPointFromOffset(startContentOffset + frameSubStringLength, &frameTextEndPoint);
     NS_ENSURE_SUCCESS(rv, nsIntRect());
-    frameScreenRect.width = frameTextEndPoint.x - frameTextStartPoint.x;
+
+    frameScreenRect.x += std::min(frameTextStartPoint.x, frameTextEndPoint.x);
+    frameScreenRect.width = std::abs(frameTextStartPoint.x - frameTextEndPoint.x);
 
     screenRect.UnionRect(frameScreenRect, screenRect);
 
