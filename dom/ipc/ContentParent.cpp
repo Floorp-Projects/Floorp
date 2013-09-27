7f31c5a3c8c3ee739066fb0687254fd42e9405a6
Chris Kitching <ckitching@mozilla.com>
2013-09-27 10:10:37 -0400

    Bug 913985: Part 5 - Refactor the Bridge to use the generated code. r=kats

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -1560,7 +1560,7 @@ ContentParent::RecvGetShowPasswordSetting(bool* showPassword)
 #ifdef MOZ_WIDGET_ANDROID
     NS_ASSERTION(AndroidBridge::Bridge() != nullptr, "AndroidBridge is not available");
     if (AndroidBridge::Bridge() != nullptr)
-        *showPassword = AndroidBridge::Bridge()->GetShowPasswordSetting();
+        *showPassword = GeckoAppShell::GetShowPasswordSetting();
 #endif
     return true;
 }
