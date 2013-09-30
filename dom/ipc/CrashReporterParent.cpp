e0afa017f1c09e23d4eb47109271ab243b672950
Ben Turner <bent.mozilla@gmail.com>
2013-09-30 10:13:13 -0700

    Bug 771765, tentative bustage fix on CLOSED TREE

--- dom/ipc/CrashReporterParent.cpp
+++ dom/ipc/CrashReporterParent.cpp
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include "CrashReporterParent.h"
 #include "mozilla/dom/ContentParent.h"
+#include "nsExceptionHandler.h"
 #include "nsXULAppAPI.h"
 
 #include <time.h>
