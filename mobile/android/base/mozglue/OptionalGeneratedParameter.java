1af5322a28399669f2a7738ba62dbc7e9882599f
Ed Morley <emorley@mozilla.com>
2013-09-30 11:06:03 +0100

    Backed out changeset 044255192754 (bug 913985) for Android mochitest-7 test_asmjs.html failures

--- mobile/android/base/mozglue/OptionalGeneratedParameter.java
+++ mobile/android/base/mozglue/OptionalGeneratedParameter.java
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.gecko.mozglue.generatorannotations;
+package org.mozilla.gecko.mozglue;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
