b4eea06a717b3534a3ad74247f6cbe2f2b190168
Ed Morley <emorley@mozilla.com>
2013-09-30 11:05:44 +0100

    Backed out changeset 08844da6d9ed (bug 913985)

--- mobile/android/base/sqlite/SQLiteBridgeException.java
+++ mobile/android/base/sqlite/SQLiteBridgeException.java
@@ -5,9 +5,6 @@
 
 package org.mozilla.gecko.sqlite;
 
-import org.mozilla.gecko.mozglue.generatorannotations.WrapEntireClassForJNI;
-
-@WrapEntireClassForJNI
 public class SQLiteBridgeException extends RuntimeException {
     static final long serialVersionUID = 1L;
 
