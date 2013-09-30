f11e2b6fef31dbede7b4e8842e49d57e59891141
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2013-10-01 00:35:32 +0900

    Bug 885107 - Sync testharness with the latest upstream. r=Ms2ger

--- dom/imptests/updateTestharness.py
+++ dom/imptests/updateTestharness.py
@@ -7,13 +7,19 @@ from __future__ import unicode_literals
 
 import subprocess
 
-repo = "https://dvcs.w3.org/hg/resources"
+repo = "https://github.com/w3c/testharness.js"
 dest = "resources-upstream"
-files = ["testharness.js", "testharness.css", "idlharness.js", "WebIDLParser.js"]
+files = [{"f":"testharness.js"},
+         {"f":"testharness.css"},
+         {"f":"idlharness.js"},
+         {"d":"webidl2/lib/webidl2.js", "f":"WebIDLParser.js"}]
 
-subprocess.check_call(["hg", "clone", repo, dest])
+subprocess.check_call(["git", "clone", repo, dest])
+subprocess.check_call(["git", "submodule", "init"], cwd=dest)
+subprocess.check_call(["git", "submodule", "update"], cwd=dest)
 for f in files:
-    subprocess.check_call(["cp", "%s/%s" % (dest, f), f])
-    subprocess.check_call(["hg", "add", f])
+    path = f["d"] if "d" in f else f["f"]
+    subprocess.check_call(["cp", "%s/%s" % (dest, path), f["f"]])
+    subprocess.check_call(["hg", "add", f["f"]])
 subprocess.check_call(["rm", "-rf", dest])
 
