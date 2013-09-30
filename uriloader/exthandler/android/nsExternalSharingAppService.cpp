4a2adc68200134cd94751953cb9944a2b255a5e6
Ed Morley <emorley@mozilla.com>
2013-09-30 11:05:32 +0100

    Backed out changeset 196a2b00d509 (bug 913985)

--- uriloader/exthandler/android/nsExternalSharingAppService.cpp
+++ uriloader/exthandler/android/nsExternalSharingAppService.cpp
@@ -31,7 +31,11 @@ nsExternalSharingAppService::ShareWithDefault(const nsAString & data,
 {
   NS_NAMED_LITERAL_STRING(sendAction, "android.intent.action.SEND");
   const nsString emptyString = EmptyString();
-  return GeckoAppShell::OpenUriExternal(data, mime, emptyString,emptyString, sendAction, title) ? NS_OK : NS_ERROR_FAILURE;
+  if (AndroidBridge::Bridge())
+    return GeckoAppShell::OpenUriExternal(data, mime,
+                                          emptyString,emptyString, sendAction, title) ? NS_OK : NS_ERROR_FAILURE;
+
+  return NS_ERROR_FAILURE;
 }
 
 NS_IMETHODIMP
