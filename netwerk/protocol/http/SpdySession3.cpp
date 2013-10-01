7036587a468f18247d6bb910bc2e9738301bc99f
Patrick McManus <mcmanus@ducksong.com>
2013-10-01 17:48:06 -0400

    bug 922109 - spdy/3 settings frame does not restart window starved streams r=hurley

--- netwerk/protocol/http/SpdySession3.cpp
+++ netwerk/protocol/http/SpdySession3.cpp
@@ -1622,20 +1622,7 @@ SpdySession3::HandleWindowUpdate(SpdySession3 *self)
     return NS_OK;
   }
 
-  int64_t oldRemoteWindow = self->mInputFrameDataStream->RemoteWindow();
   self->mInputFrameDataStream->UpdateRemoteWindow(delta);
-
-  LOG3(("SpdySession3::HandleWindowUpdate %p stream 0x%X window "
-        "%d increased by %d.\n", self, streamID, oldRemoteWindow, delta));
-
-  // If the stream had a <=0 window, that has now opened
-  // schedule it for writing again
-  if (oldRemoteWindow <= 0 &&
-      self->mInputFrameDataStream->RemoteWindow() > 0) {
-    self->mReadyForWrite.Push(self->mInputFrameDataStream);
-    self->SetWriteCallbacks();
-  }
-
   self->ResetDownstreamState();
   return NS_OK;
 }
