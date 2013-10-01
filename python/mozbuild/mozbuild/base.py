d68410c3863790c55005950215036cf17ea05d6b
Gregory Szorc <gps@mozilla.com>
2013-10-01 10:55:07 +0200

    Bug 920679 - Improve build environment detection in mach; r=glandium

--- python/mozbuild/mozbuild/base.py
+++ python/mozbuild/mozbuild/base.py
@@ -506,8 +506,14 @@ class MachCommandBase(MozbuildObject):
     """
 
     def __init__(self, context):
-        MozbuildObject.__init__(self, context.topdir, context.settings,
-            context.log_manager)
+        # Attempt to discover topobjdir through environment detection, as it is
+        # more reliable than mozconfig when cwd is inside an objdir.
+        dummy = MozbuildObject.from_environment(cwd=context.cwd)
+
+        topsrcdir = dummy.topsrcdir or context.topdir
+
+        MozbuildObject.__init__(self, topsrcdir, context.settings,
+            context.log_manager, topobjdir=dummy._topobjdir)
 
         self._mach_context = context
 
