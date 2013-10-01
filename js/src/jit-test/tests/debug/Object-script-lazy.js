fd3941a1e4292ca1ba0bb2ae9fb482a56f8c5e22
Jim Blandy <jimb@mozilla.com>
2013-10-01 16:16:44 -0700

    Bug 912536: Forbid access to non-debuggee environments. r=jorendorff

--- js/src/jit-test/tests/debug/Object-script-lazy.js
+++ js/src/jit-test/tests/debug/Object-script-lazy.js
@@ -1,11 +1,13 @@
-// Asking for the script of a Debugger.Object whose referent is a lazy
-// function should work.
+// Asking for the script of a Debugger.Object should work, even when lazy
+// functions are involved.
 
-// Note that putting a compartment in debug mode automatically de-lazifies
-// all its functions (so that findScripts works); so the only way to get a
-// Debugger.Object whose referent is a lazy function is to get one
-// referring to a non-debuggee function. (I don't think we should be
-// handing out non-debuggee scripts anyway.)
+// Note that we never hand out the scripts of non-debuggee functions, and
+// putting a compartment in debug mode automatically de-lazifies all its
+// functions. (This ensures that findScripts works, too.)
+//
+// So here we create a reference to a non-debuggee function, verify that we
+// can't access its interesting properties, make it a debuggee, and verify
+// that everything becomes available.
 
 // Create functions f, g in a non-debuggee compartment.
 var g1 = newGlobal();
@@ -21,14 +23,21 @@ g2.g = g1.g;
 
 // At this point, g1.f should still be a lazy function. Unwrapping a D.O
 // referring to g2's CCW of f should yield a D.O referring to f directly.
-// Asking for that second D.O's script should de-lazify f.
+// Asking for that second D.O's script should yield null, because it's not
+// a debuggee.
 var fDO = g2w.getOwnPropertyDescriptor('f').value;
 assertEq(fDO.global, g2w);
 assertEq(fDO.unwrap().global === g2w, false);
-assertEq(fDO.unwrap().script instanceof Debugger.Script, true);
+assertEq(fDO.unwrap().script, null);
 
 // Similarly for g1.g, and asking for its parameter names.
 var gDO = g2w.getOwnPropertyDescriptor('g').value;
 assertEq(gDO.global, g2w);
 assertEq(gDO.unwrap().global === g2w, false);
+assertEq(gDO.unwrap().parameterNames, undefined);
+
+// Add g1 as a debuggee, and verify that we can get everything.
+dbg.addDebuggee(g1);
+assertEq(fDO.unwrap().script instanceof Debugger.Script, true);
 assertEq(gDO.unwrap().parameterNames instanceof Array, true);
+
