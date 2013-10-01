51354b7c07f48c512255c2ecf1e5163486c3b907
Thomas Zimmermann <tdz@users.sourceforge.net>
2013-10-01 10:20:39 +0200

    Bug 919913: Cleanup DBusUtils, r=qdot
    This patch cleans up the DBus utilitys and helper classes. All functions
    for sending have been removed. Their users have been converted to the
    new methods in RawDBusConnection. Include statements have been cleaned
    up as well. Some methods of DBusMessageRefPtr have been moved from the
    header to the source file to prevent inclusion of the DBus API from within
    the header file.

--- ipc/dbus/DBusUtils.h
+++ ipc/dbus/DBusUtils.h
@@ -19,17 +19,15 @@
 #ifndef mozilla_ipc_dbus_dbusutils_h__
 #define mozilla_ipc_dbus_dbusutils_h__
 
-#include <dbus/dbus.h>
 #include "mozilla/RefPtr.h"
-#include "mozilla/Scoped.h"
 
 // LOGE and free a D-Bus error
 // Using #define so that __FUNCTION__ resolves usefully
 #define LOG_AND_FREE_DBUS_ERROR_WITH_MSG(err, msg) log_and_free_dbus_error(err, __FUNCTION__, msg);
 #define LOG_AND_FREE_DBUS_ERROR(err) log_and_free_dbus_error(err, __FUNCTION__);
 
-struct DBusMessage;
 struct DBusError;
+struct DBusMessage;
 
 namespace mozilla {
 namespace ipc {
@@ -37,16 +35,19 @@ namespace ipc {
 class DBusMessageRefPtr
 {
 public:
-  DBusMessageRefPtr(DBusMessage* aMsg) : mMsg(aMsg)
+  DBusMessageRefPtr(DBusMessage* aMsg);
+  ~DBusMessageRefPtr();
+
+  operator DBusMessage* ()
   {
-    if (mMsg) dbus_message_ref(mMsg);
+    return mMsg;
   }
-  ~DBusMessageRefPtr()
+
+  DBusMessage* get()
   {
-    if (mMsg) dbus_message_unref(mMsg);
+    return mMsg;
   }
-  operator DBusMessage*() { return mMsg; }
-  DBusMessage* get() { return mMsg; }
+
 private:
   DBusMessage* mMsg;
 };
@@ -89,77 +90,9 @@ protected:
   }
 };
 
-typedef void (*DBusCallback)(DBusMessage *, void *);
-
 void log_and_free_dbus_error(DBusError* err,
                              const char* function,
                              DBusMessage* msg = nullptr);
-
-dbus_bool_t dbus_func_send(DBusConnection *aConnection,
-                           dbus_uint32_t *aSerial,
-                           DBusMessage *aMessage);
-
-dbus_bool_t dbus_func_args_send(DBusConnection *aConnection,
-                                dbus_uint32_t *aSerial,
-                                const char *aPath,
-                                const char *aInterface,
-                                const char *aFunction,
-                                int aFirstArgType, ...);
-
-dbus_bool_t dbus_func_send_async(DBusConnection* conn,
-                                 DBusMessage* msg,
-                                 int timeout_ms,
-                                 DBusCallback user_cb,
-                                 void* user);
-
-dbus_bool_t dbus_func_args_async(DBusConnection* conn,
-                                 int timeout_ms,
-                                 DBusCallback reply,
-                                 void* user,
-                                 const char* path,
-                                 const char* ifc,
-                                 const char* func,
-                                 int first_arg_type,
-                                 ...);
-
-dbus_bool_t dbus_func_send_and_block(DBusConnection* aConnection,
-                                     int aTimeout,
-                                     DBusMessage** aReply,
-                                     DBusError* aError,
-                                     DBusMessage* aMessage);
-
-DBusMessage*  dbus_func_args(DBusConnection* conn,
-                             const char* path,
-                             const char* ifc,
-                             const char* func,
-                             int first_arg_type,
-                             ...);
-
-DBusMessage*  dbus_func_args_error(DBusConnection* conn,
-                                   DBusError* err,
-                                   const char* path,
-                                   const char* ifc,
-                                   const char* func,
-                                   int first_arg_type,
-                                   ...);
-
-DBusMessage*  dbus_func_args_timeout(DBusConnection* conn,
-                                     int timeout_ms,
-                                     DBusError* err,
-                                     const char* path,
-                                     const char* ifc,
-                                     const char* func,
-                                     int first_arg_type,
-                                     ...);
-
-DBusMessage*  dbus_func_args_timeout_valist(DBusConnection* conn,
-                                            int timeout_ms,
-                                            DBusError* err,
-                                            const char* path,
-                                            const char* ifc,
-                                            const char* func,
-                                            int first_arg_type,
-                                            va_list args);
 
 int dbus_returns_int32(DBusMessage *reply);
 
