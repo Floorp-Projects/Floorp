10b1cd9d6195b3d27bfd27032a3c9ee0b3fc2176
Aaron Klotz <aklotz@mozilla.com>
2013-10-01 13:53:19 -0600

    Bug 888534: Add support for Create, Stat and Close operations to IOInterposer. r=jonasfj

--- tools/profiler/ProfilerIOInterposeObserver.cpp
+++ tools/profiler/ProfilerIOInterposeObserver.cpp
@@ -10,15 +10,34 @@ using namespace mozilla;
 
 void ProfilerIOInterposeObserver::Observe(Observation& aObservation)
 {
-  // TODO: The profile might want to take notice of non-main-thread IO, as
-  // well as noting what files or references causes the IO.
-  if (NS_IsMainThread()) {
-    const char* ops[] = {"none", "read", "write", "invalid", "fsync"};
-    ProfilerBacktrace* stack = profiler_get_backtrace();
-    IOMarkerPayload* markerPayload = new IOMarkerPayload(aObservation.Reference(),
-                                                         aObservation.Start(),
-                                                         aObservation.End(),
-                                                         stack);
-    PROFILER_MARKER_PAYLOAD(ops[aObservation.ObservedOperation()], markerPayload);
+  const char* str = nullptr;
+
+  switch (aObservation.ObservedOperation()) {
+    case IOInterposeObserver::OpCreateOrOpen:
+      str = "create/open";
+      break;
+    case IOInterposeObserver::OpRead:
+      str = "read";
+      break;
+    case IOInterposeObserver::OpWrite:
+      str = "write";
+      break;
+    case IOInterposeObserver::OpFSync:
+      str = "fsync";
+      break;
+    case IOInterposeObserver::OpStat:
+      str = "stat";
+      break;
+    case IOInterposeObserver::OpClose:
+      str = "close";
+      break;
+    default:
+      return;
   }
+  ProfilerBacktrace* stack = profiler_get_backtrace();
+  IOMarkerPayload* markerPayload = new IOMarkerPayload(aObservation.Reference(),
+                                                       aObservation.Start(),
+                                                       aObservation.End(),
+                                                       stack);
+  PROFILER_MARKER_PAYLOAD(str, markerPayload);
 }
