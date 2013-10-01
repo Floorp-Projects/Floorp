91e875a8b6ccde6d90b9d55d8bcc1081f42e8ae5
Neil Rashbrook <neil@parkwaycc.co.uk>
2013-10-01 19:15:59 +0100

    Bug 921751 Make Preprocessor.py's -o option honour requested newline type r=ted

--- config/Preprocessor.py
+++ config/Preprocessor.py
@@ -160,7 +160,7 @@ class Preprocessor:
         except OSError as error:
           if error.errno != errno.EEXIST:
             raise
-      self.out = open(options.output, 'w')
+      self.out = open(options.output, 'wb')
     if defaultToStdin and len(args) == 0:
       args = [sys.stdin]
     includes.extend(args)
