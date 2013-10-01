90a0897c95d43b33effbeaf05d7d1659b60a57a7
Lucas Rocha <lucasr@mozilla.com>
2013-10-01 14:22:15 +0100

    Bug 906230 - Add HardwareUtils.isLowMemoryPlatform(). r=kats, a=lsblakk

--- xpcom/base/nsMemoryImpl.cpp
+++ xpcom/base/nsMemoryImpl.cpp
@@ -22,6 +22,11 @@
 
 #ifdef ANDROID
 #include <stdio.h>
+
+// Minimum memory threshold for a device to be considered
+// a low memory platform. This value has be in sync with
+// Java's equivalent threshold, defined in
+// mobile/android/base/util/HardwareUtils.java
 #define LOW_MEMORY_THRESHOLD_KB (384 * 1024)
 #endif
 
