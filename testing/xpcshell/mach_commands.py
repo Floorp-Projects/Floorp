c54df6244c9d3253ce05865666372f82bb95e798
Gregory Szorc <gps@mozilla.com>
2013-10-01 18:36:44 +0200

    Bug 920184 - Have mach xpcshell-test and mochitest-* update test files automatically; r=ted

--- testing/xpcshell/mach_commands.py
+++ testing/xpcshell/mach_commands.py
@@ -226,10 +226,15 @@ class MachCommands(MachCommandBase):
     @CommandArgument('--rerun-failures', action='store_true',
         help='Reruns failures from last time.')
     def run_xpcshell_test(self, **params):
+        from mozbuild.controller.building import BuildDriver
+
         # We should probably have a utility function to ensure the tree is
         # ready to run tests. Until then, we just create the state dir (in
         # case the tree wasn't built with mach).
         self._ensure_state_subdir_exists('.')
+
+        driver = self._spawn(BuildDriver)
+        driver.install_tests(remove=False)
 
         xpcshell = self._spawn(XPCShellRunner)
 
