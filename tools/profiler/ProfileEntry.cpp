174cf8cb08c7c6f45f0fb8a9cf6de83c21b6804c
Phil Ringnalda <philringnalda@gmail.com>
2013-10-01 22:46:48 -0700

    Backed out changeset 2eb7e93512b1 (bug 918825) for browser-chrome crashes

--- tools/profiler/ProfileEntry.cpp
+++ tools/profiler/ProfileEntry.cpp
@@ -4,6 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include <ostream>
+#include "GeckoProfilerImpl.h"
 #include "platform.h"
 #include "nsThreadUtils.h"
 #include "nsXULAppAPI.h"
