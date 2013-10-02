1f249b4fc7c8d6a125cb642313e5ad786326c3b5
Ed Morley <emorley@mozilla.com>
2013-10-02 15:58:47 +0100

    Backed out changeset 9ee3b91945ce (bug 918940)

--- content/html/content/src/HTMLTextAreaElement.cpp
+++ content/html/content/src/HTMLTextAreaElement.cpp
@@ -889,9 +889,6 @@ HTMLTextAreaElement::SetSelectionRange(uint32_t aSelectionStart,
       rv = textControlFrame->SetSelectionRange(aSelectionStart, aSelectionEnd, dir);
       if (NS_SUCCEEDED(rv)) {
         rv = textControlFrame->ScrollSelectionIntoView();
-        nsRefPtr<nsAsyncDOMEvent> event =
-          new nsAsyncDOMEvent(this, NS_LITERAL_STRING("select"), true, false);
-        event->PostDOMEvent();
       }
     }
   }
@@ -899,103 +896,6 @@ HTMLTextAreaElement::SetSelectionRange(uint32_t aSelectionStart,
   if (NS_FAILED(rv)) {
     aError.Throw(rv);
   }
-}
-
-void
-HTMLTextAreaElement::SetRangeText(const nsAString& aReplacement,
-                                  ErrorResult& aRv)
-{
-  int32_t start, end;
-  aRv = GetSelectionRange(&start, &end);
-  if (aRv.Failed()) {
-    if (mState.IsSelectionCached()) {
-      start = mState.GetSelectionProperties().mStart;
-      end = mState.GetSelectionProperties().mEnd;
-      aRv = NS_OK;
-    }
-  }
-
-  SetRangeText(aReplacement, start, end, mozilla::dom::SelectionMode::Preserve,
-               aRv, start, end);
-}
-
-void
-HTMLTextAreaElement::SetRangeText(const nsAString& aReplacement,
-                                  uint32_t aStart, uint32_t aEnd,
-                                  const SelectionMode& aSelectMode,
-                                  ErrorResult& aRv, int32_t aSelectionStart,
-                                  int32_t aSelectionEnd)
-{
-  if (aStart > aEnd) {
-    aRv.Throw(NS_ERROR_DOM_INDEX_SIZE_ERR);
-    return;
-  }
-
-  nsAutoString value;
-  GetValueInternal(value, false);
-  uint32_t inputValueLength = value.Length();
-
-  if (aStart > inputValueLength) {
-    aStart = inputValueLength;
-  }
-
-  if (aEnd > inputValueLength) {
-    aEnd = inputValueLength;
-  }
-
-  if (aSelectionStart == -1 && aSelectionEnd == -1) {
-      aRv = GetSelectionRange(&aSelectionStart, &aSelectionEnd);
-      if (aRv.Failed()) {
-        if (mState.IsSelectionCached()) {
-          aSelectionStart = mState.GetSelectionProperties().mStart;
-          aSelectionEnd = mState.GetSelectionProperties().mEnd;
-          aRv = NS_OK;
-        }
-      }
-  }
-
-  if (aStart < aEnd) {
-    value.Replace(aStart, aEnd - aStart, aReplacement);
-    SetValueInternal(value, false);
-  }
-
-  uint32_t newEnd = aStart + aReplacement.Length();
-  int32_t delta =  aReplacement.Length() - (aEnd - aStart);
-
-  switch (aSelectMode) {
-    case mozilla::dom::SelectionMode::Select:
-    {
-      aSelectionStart = aStart;
-      aSelectionEnd = newEnd;
-    }
-    break;
-    case mozilla::dom::SelectionMode::Start:
-    {
-      aSelectionStart = aSelectionEnd = aStart;
-    }
-    break;
-    case mozilla::dom::SelectionMode::End:
-    {
-      aSelectionStart = aSelectionEnd = newEnd;
-    }
-    break;
-    case mozilla::dom::SelectionMode::Preserve:
-    {
-      if ((uint32_t)aSelectionStart > aEnd)
-        aSelectionStart += delta;
-      else if ((uint32_t)aSelectionStart > aStart)
-       aSelectionStart = aStart;
-
-      if ((uint32_t)aSelectionEnd > aEnd)
-        aSelectionEnd += delta;
-      else if ((uint32_t)aSelectionEnd > aStart)
-        aSelectionEnd = newEnd;
-    }
-    break;
-  }
-
-  Optional<nsAString> direction;
-  SetSelectionRange(aSelectionStart, aSelectionEnd, direction, aRv);
 }
 
 nsresult
