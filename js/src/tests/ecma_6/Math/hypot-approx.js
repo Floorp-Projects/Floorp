550fb79a0c1a84b0923ab615e2c15719af8e30b0
David Caabeiro <david.caabeiro@gmail.com>
2013-10-02 17:27:50 +0200

    Bug 896264 - Implement Math.hypot(). r=jorendorff.

--- js/src/tests/ecma_6/Math/hypot-approx.js
+++ js/src/tests/ecma_6/Math/hypot-approx.js
@@ -1,14 +1,23 @@
-// |reftest| skip
-// Math.hypot is disabled pending the resolution of spec issues (bug 896264).
-
 for (var i = -20; i < 20; i++) {
     assertEq(Math.hypot(+0, i), Math.abs(i));
     assertEq(Math.hypot(-0, i), Math.abs(i));
 }
 
-assertNear(Math.hypot(1e300, 1e300), 1.4142135623730952e+300);
+// The implementation must avoid underlow.
+// The implementation must avoid overflow, where possible.
+// The implementation must minimise rounding errors.
+
 assertNear(Math.hypot(1e-300, 1e-300), 1.414213562373095e-300);
+assertNear(Math.hypot(1e-300, 1e-300, 1e-300), 1.732050807568877e-300);
+
+assertNear(Math.hypot(1e-3, 1e-3, 1e-3), 0.0017320508075688772);
+
+assertNear(Math.hypot(1e300, 1e300), 1.4142135623730952e+300);
+assertNear(Math.hypot(1e100, 1e200, 1e300), 1e300);
+
 assertNear(Math.hypot(1e3, 1e-3), 1000.0000000005);
+assertNear(Math.hypot(1e-300, 1e300), 1e300);
+assertNear(Math.hypot(1e3, 1e-3, 1e3, 1e-3), 1414.2135623738021555);
 
 for (var i = 1, j = 1; i < 2; i += 0.05, j += 0.05)
     assertNear(Math.hypot(i, j), Math.sqrt(i * i + j * j));
