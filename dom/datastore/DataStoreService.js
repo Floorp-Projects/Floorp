0df0b6ee1ad74bb000879f951d2794ea8acf27b8
Ed Morley <emorley@mozilla.com>
2013-10-02 17:57:25 +0100

    Backed out changeset 2767ff221c92 (bug 871445)

--- dom/datastore/DataStoreService.js
+++ dom/datastore/DataStoreService.js
@@ -17,7 +17,6 @@ const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 Cu.import('resource://gre/modules/Services.jsm');
 Cu.import('resource://gre/modules/DataStore.jsm');
-Cu.import("resource://gre/modules/DataStoreDB.jsm");
 Cu.import("resource://gre/modules/DOMRequestHelper.jsm");
 
 XPCOMUtils.defineLazyServiceGetter(this, "cpmm",
@@ -28,54 +27,37 @@ XPCOMUtils.defineLazyServiceGetter(this, "ppmm",
                                    "@mozilla.org/parentprocessmessagemanager;1",
                                    "nsIMessageBroadcaster");
 
-XPCOMUtils.defineLazyServiceGetter(this, "permissionManager",
-                                   "@mozilla.org/permissionmanager;1",
-                                   "nsIPermissionManager");
-
-XPCOMUtils.defineLazyServiceGetter(this, "secMan",
-                                   "@mozilla.org/scriptsecuritymanager;1",
-                                   "nsIScriptSecurityManager");
-
 /* DataStoreService */
 
 const DATASTORESERVICE_CID = Components.ID('{d193d0e2-c677-4a7b-bb0a-19155b470f2e}');
-const REVISION_VOID = "void";
 
 function DataStoreService() {
   debug('DataStoreService Constructor');
 
-  this.inParent = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime)
-                    .processType == Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT;
-
-  if (this.inParent) {
-    let obs = Services.obs;
-    if (!obs) {
-      debug("DataStore Error: observer-service is null!");
-      return;
-    }
-
-    obs.addObserver(this, 'webapps-clear-data', false);
+  let obs = Services.obs;
+  if (!obs) {
+    debug("DataStore Error: observer-service is null!");
+    return;
   }
 
-  let self = this;
-  cpmm.addMessageListener("datastore-first-revision-created",
-                          function(aMsg) { self.receiveMessage(aMsg); });
+  obs.addObserver(this, 'webapps-clear-data', false);
+
+  let inParent = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime)
+                   .processType == Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT;
+  if (inParent) {
+    ppmm.addMessageListener("DataStore:Get", this);
+  }
 }
 
 DataStoreService.prototype = {
-  inParent: false,
-
   // Hash of DataStores
   stores: {},
   accessStores: {},
-  pendingRequests: {},
 
-  installDataStore: function(aAppId, aName, aOrigin, aOwner, aReadOnly) {
+  installDataStore: function(aAppId, aName, aOwner, aReadOnly) {
     debug('installDataStore - appId: ' + aAppId + ', aName: ' +
-          aName + ', aOrigin: ' + aOrigin + ', aOwner:' + aOwner +
-          ', aReadOnly: ' + aReadOnly);
-
-    this.checkIfInParent();
+          aName + ', aOwner:' + aOwner + ', aReadOnly: ' +
+          aReadOnly);
 
     if (aName in this.stores && aAppId in this.stores[aName]) {
       debug('This should not happen');
@@ -86,21 +68,13 @@ DataStoreService.prototype = {
       this.stores[aName] = {};
     }
 
-    // A DataStore is enabled when it has a first valid revision.
-    this.stores[aName][aAppId] = { origin: aOrigin, owner: aOwner,
-                                   readOnly: aReadOnly, enabled: false };
-
-    this.addPermissions(aAppId, aName, aOrigin, aOwner, aReadOnly);
-
-    this.createFirstRevisionId(aAppId, aName, aOwner);
+    this.stores[aName][aAppId] = { owner: aOwner, readOnly: aReadOnly };
   },
 
-  installAccessDataStore: function(aAppId, aName, aOrigin, aOwner, aReadOnly) {
-    debug('installAccessDataStore - appId: ' + aAppId + ', aName: ' +
-          aName + ', aOrigin: ' + aOrigin + ', aOwner:' + aOwner +
-          ', aReadOnly: ' + aReadOnly);
-
-    this.checkIfInParent();
+  installAccessDataStore: function(aAppId, aName, aOwner, aReadOnly) {
+    debug('installDataStore - appId: ' + aAppId + ', aName: ' +
+          aName + ', aOwner:' + aOwner + ', aReadOnly: ' +
+          aReadOnly);
 
     if (aName in this.accessStores && aAppId in this.accessStores[aName]) {
       debug('This should not happen');
@@ -111,231 +85,56 @@ DataStoreService.prototype = {
       this.accessStores[aName] = {};
     }
 
-    this.accessStores[aName][aAppId] = { origin: aOrigin, owner: aOwner,
-                                         readOnly: aReadOnly };
-    this.addAccessPermissions(aAppId, aName, aOrigin, aOwner, aReadOnly);
-  },
-
-  checkIfInParent: function() {
-    if (!this.inParent) {
-      throw "DataStore can execute this operation just in the parent process";
-    }
-  },
-
-  createFirstRevisionId: function(aAppId, aName, aOwner) {
-    debug("createFirstRevisionId database: " + aName);
-
-    let self = this;
-    let db = new DataStoreDB();
-    db.init(aOwner, aName);
-    db.revisionTxn(
-      'readwrite',
-      function(aTxn, aRevisionStore) {
-        debug("createFirstRevisionId - transaction success");
-
-        let request = aRevisionStore.openCursor(null, 'prev');
-        request.onsuccess = function(aEvent) {
-          let cursor = aEvent.target.result;
-          if (!cursor) {
-            // If the revision doesn't exist, let's create the first one.
-            db.addRevision(aRevisionStore, 0, REVISION_VOID, function() {
-              debug("First revision created.");
-              if (aName in self.stores && aAppId in self.stores[aName]) {
-                self.stores[aName][aAppId].enabled = true;
-                ppmm.broadcastAsyncMessage('datastore-first-revision-created',
-                                           { name: aName, owner: aOwner });
-              }
-            });
-          }
-        };
-      }
-    );
-  },
-
-  addPermissions: function(aAppId, aName, aOrigin, aOwner, aReadOnly) {
-    // When a new DataStore is installed, the permissions must be set for the
-    // owner app.
-    let permission = "indexedDB-chrome-" + aName + '|' + aOwner;
-    this.resetPermissions(aAppId, aOrigin, aOwner, permission, aReadOnly);
-
-    // For any app that wants to have access to this DataStore we add the
-    // permissions.
-    if (aName in this.accessStores) {
-      for (let appId in this.accessStores[aName]) {
-        // ReadOnly is decided by the owner first.
-        let readOnly = aReadOnly || this.accessStores[aName][appId].readOnly;
-        this.resetPermissions(appId, this.accessStores[aName][appId].origin,
-                              this.accessStores[aName][appId].owner,
-                              permission, readOnly);
-      }
-    }
-  },
-
-  addAccessPermissions: function(aAppId, aName, aOrigin, aOwner, aReadOnly) {
-    // When an app wants to have access to a DataStore, the permissions must be
-    // set.
-    if (!(aName in this.stores)) {
-      return;
-    }
-
-    for (let appId in this.stores[aName]) {
-      let permission = "indexedDB-chrome-" + aName + '|' + this.stores[aName][appId].owner;
-      // The ReadOnly is decied by the owenr first.
-      let readOnly = this.stores[aName][appId].readOnly || aReadOnly;
-      this.resetPermissions(aAppId, aOrigin, aOwner, permission, readOnly);
-    }
-  },
-
-  resetPermissions: function(aAppId, aOrigin, aOwner, aPermission, aReadOnly) {
-    debug("ResetPermissions - appId: " + aAppId + " - origin: " + aOrigin +
-          " - owner: " + aOwner + " - permissions: " + aPermission +
-          " - readOnly: " + aReadOnly);
-
-    let uri = Services.io.newURI(aOrigin, null, null);
-    let principal = secMan.getAppCodebasePrincipal(uri, aAppId, false);
-
-    let result = permissionManager.testExactPermissionFromPrincipal(principal,
-                                                                    aPermission + '-write');
-
-    if (aReadOnly && result == Ci.nsIPermissionManager.ALLOW_ACTION) {
-      debug("Write permission removed");
-      permissionManager.removeFromPrincipal(principal, aPermission + '-write');
-    } else if (!aReadOnly && result != Ci.nsIPermissionManager.ALLOW_ACTION) {
-      debug("Write permission added");
-      permissionManager.addFromPrincipal(principal, aPermission + '-write',
-                                         Ci.nsIPermissionManager.ALLOW_ACTION);
-    }
-
-    result = permissionManager.testExactPermissionFromPrincipal(principal,
-                                                                aPermission + '-read');
-    if (result != Ci.nsIPermissionManager.ALLOW_ACTION) {
-      debug("Read permission added");
-      permissionManager.addFromPrincipal(principal, aPermission + '-read',
-                                         Ci.nsIPermissionManager.ALLOW_ACTION);
-    }
-
-    result = permissionManager.testExactPermissionFromPrincipal(principal, aPermission);
-    if (result != Ci.nsIPermissionManager.ALLOW_ACTION) {
-      debug("Generic permission added");
-      permissionManager.addFromPrincipal(principal, aPermission,
-                                         Ci.nsIPermissionManager.ALLOW_ACTION);
-    }
+    this.accessStores[aName][aAppId] = { owner: aOwner, readOnly: aReadOnly };
   },
 
   getDataStores: function(aWindow, aName) {
     debug('getDataStores - aName: ' + aName);
 
-    let self = this;
+    // This method can be called in the child so we need to send a request to
+    // the parent and create DataStore object here.
+
     return new aWindow.Promise(function(resolve, reject) {
-      // If this request comes from the main process, we have access to the
-      // window, so we can skip the ipc communication.
-      if (self.inParent) {
-        let stores = self.getDataStoresInfo(aName, aWindow.document.nodePrincipal.appId);
-        self.getDataStoreCreate(aWindow, resolve, stores);
-      } else {
-        // This method can be called in the child so we need to send a request
-        // to the parent and create DataStore object here.
-        new DataStoreServiceChild(aWindow, aName, function(aStores) {
-          debug("DataStoreServiceChild callback!");
-          self.getDataStoreCreate(aWindow, resolve, aStores);
-        });
-      }
+      new DataStoreServiceChild(aWindow, aName, resolve);
     });
   },
 
-  getDataStoresInfo: function(aName, aAppId) {
-    debug('GetDataStoresInfo');
+  receiveMessage: function(aMessage) {
+    if (aMessage.name != 'DataStore:Get') {
+      return;
+    }
+
+    let msg = aMessage.data;
+
+    // This is a security issue and it will be fixed by Bug 916091
+    let appId = msg.appId;
 
     let results = [];
 
-    if (aName in this.stores) {
-      if (aAppId in this.stores[aName]) {
-        results.push({ name: aName,
-                       owner: this.stores[aName][aAppId].owner,
-                       readOnly: false,
-                       enabled: this.stores[aName][aAppId].enabled });
+    if (msg.name in this.stores) {
+      if (appId in this.stores[msg.name]) {
+        results.push({ store: this.stores[msg.name][appId],
+                       readOnly: false });
       }
 
-      for (var i in this.stores[aName]) {
-        if (i == aAppId) {
+      for (var i in this.stores[msg.name]) {
+        if (i == appId) {
           continue;
         }
 
-        let access = this.getDataStoreAccess(aName, aAppId);
+        let access = this.getDataStoreAccess(msg.name, appId);
         if (!access) {
           continue;
         }
 
-        let readOnly = this.stores[aName][i].readOnly || access.readOnly;
-        results.push({ name: aName,
-                       owner: this.stores[aName][i].owner,
-                       readOnly: readOnly,
-                       enabled: this.stores[aName][i].enabled });
+        let readOnly = this.stores[msg.name][i].readOnly || access.readOnly;
+        results.push({ store: this.stores[msg.name][i],
+                       readOnly: readOnly });
       }
     }
 
-    return results;
-  },
-
-  getDataStoreCreate: function(aWindow, aResolve, aStores) {
-    debug("GetDataStoreCreate");
-
-    let results = [];
-
-    if (!aStores.length) {
-      aResolve(results);
-      return;
-    }
-
-    let pendingDataStores = [];
-
-    for (let i = 0; i < aStores.length; ++i) {
-      if (!aStores[i].enabled) {
-        pendingDataStores.push(aStores[i].owner);
-      }
-    }
-
-    if (!pendingDataStores.length) {
-      this.getDataStoreResolve(aWindow, aResolve, aStores);
-      return;
-    }
-
-    if (!(aStores[0].name in this.pendingRequests)) {
-      this.pendingRequests[aStores[0].name] = [];
-    }
-
-    this.pendingRequests[aStores[0].name].push({ window: aWindow,
-                                                 resolve: aResolve,
-                                                 stores: aStores,
-                                                 pendingDataStores: pendingDataStores });
-  },
-
-  getDataStoreResolve: function(aWindow, aResolve, aStores) {
-    debug("GetDataStoreResolve");
-
-    let callbackPending = aStores.length;
-    let results = [];
-
-    if (!callbackPending) {
-      aResolve(results);
-      return;
-    }
-
-    for (let i = 0; i < aStores.length; ++i) {
-      let obj = new DataStore(aWindow, aStores[i].name,
-                              aStores[i].owner, aStores[i].readOnly);
-      let exposedObj = aWindow.DataStore._create(aWindow, obj);
-      results.push(exposedObj);
-
-      obj.retrieveRevisionId(
-        function() {
-          --callbackPending;
-          if (!callbackPending) {
-            aResolve(results);
-          }
-        }
-      );
-    }
+    msg.stores = results;
+    aMessage.target.sendAsyncMessage("DataStore:Get:Return", msg);
   },
 
   getDataStoreAccess: function(aName, aAppId) {
@@ -363,50 +162,12 @@ DataStoreService.prototype = {
 
     for (let key in this.stores) {
       if (params.appId in this.stores[key]) {
-        this.deleteDatabase(key, this.stores[key][params.appId].owner);
         delete this.stores[key][params.appId];
       }
 
       if (!this.stores[key].length) {
         delete this.stores[key];
       }
-    }
-  },
-
-  deleteDatabase: function(aName, aOwner) {
-    debug("delete database: " + aName);
-
-    let db = new DataStoreDB();
-    db.init(aOwner, aName);
-    db.delete();
-  },
-
-  receiveMessage: function(aMsg) {
-    debug("receiveMessage");
-    let data = aMsg.json;
-
-    if (!(data.name in this.pendingRequests)) {
-      return;
-    }
-
-    for (let i = 0; i < this.pendingRequests[data.name].length;) {
-      let pos = this.pendingRequests[data.name][i].pendingDataStores.indexOf(data.owner);
-      if (pos != -1) {
-        this.pendingRequests[data.name][i].pendingDataStores.splice(pos, 1);
-        if (!this.pendingRequests[data.name][i].pendingDataStores.length) {
-          this.getDataStoreResolve(this.pendingRequests[data.name][i].window,
-                                   this.pendingRequests[data.name][i].resolve,
-                                   this.pendingRequests[data.name][i].stores);
-          this.pendingRequests[data.name].splice(i, 1);
-          continue;
-        }
-      }
-
-      ++i;
-    }
-
-    if (!this.pendingRequests[data.name].length) {
-      delete this.pendingRequests[data.name];
     }
   },
 
@@ -423,17 +184,18 @@ DataStoreService.prototype = {
 
 /* DataStoreServiceChild */
 
-function DataStoreServiceChild(aWindow, aName, aCallback) {
+function DataStoreServiceChild(aWindow, aName, aResolve) {
   debug("DataStoreServiceChild created");
-  this.init(aWindow, aName, aCallback);
+  this.init(aWindow, aName, aResolve);
 }
 
 DataStoreServiceChild.prototype = {
   __proto__: DOMRequestIpcHelper.prototype,
 
-  init: function(aWindow, aName, aCallback) {
-    debug("DataStoreServiceChild init");
-    this._callback = aCallback;
+  init: function(aWindow, aName, aResolve) {
+    this._window = aWindow;
+    this._name = aName;
+    this._resolve = aResolve;
 
     this.initDOMRequestHelper(aWindow, [ "DataStore:Get:Return" ]);
 
@@ -443,12 +205,35 @@ DataStoreServiceChild.prototype = {
   },
 
   receiveMessage: function(aMessage) {
-    debug("DataStoreServiceChild receiveMessage");
     if (aMessage.name != 'DataStore:Get:Return') {
       return;
     }
+    let msg = aMessage.data;
+    let self = this;
 
-    this._callback(aMessage.data.stores);
+    let callbackPending = msg.stores.length;
+    let results = [];
+
+    if (!callbackPending) {
+      this._resolve(results);
+      return;
+    }
+
+    for (let i = 0; i < msg.stores.length; ++i) {
+      let obj = new DataStore(this._window, this._name,
+                              msg.stores[i].owner, msg.stores[i].readOnly);
+      let exposedObj = this._window.DataStore._create(this._window, obj);
+      results.push(exposedObj);
+
+      obj.retrieveRevisionId(
+        function() {
+          --callbackPending;
+          if (!callbackPending) {
+            self._resolve(results);
+          }
+        }
+      );
+    }
   }
 }
 
