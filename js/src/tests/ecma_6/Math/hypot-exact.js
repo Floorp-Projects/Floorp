dd295d0092183ac465c3dc85d07cb5013b914a84
David Caabeiro <david.caabeiro@gmail.com>
2013-10-02 17:27:50 +0200

    Bug 896264 - Implement Math.hypot(). r=jorendorff.

--- js/src/tests/ecma_6/Math/hypot-exact.js
+++ js/src/tests/ecma_6/Math/hypot-exact.js
@@ -1,7 +1,7 @@
-// |reftest| skip
-// Math.hypot is disabled pending the resolution of spec issues (bug 896264).
-
 // Properties of Math.hypot that are guaranteed by the spec.
+
+// If no arguments are passed, the result is +0.
+assertEq(Math.hypot(), +0);
 
 // If any argument is +∞, the result is +∞.
 // If any argument is −∞, the result is +∞.
@@ -31,8 +31,7 @@ for (var inf of [Infinity, -Infinity]) {
 }
 
 // If no argument is +∞ or −∞, and any argument is NaN, the result is NaN.
-assertEq(Math.hypot(), NaN);
-assertEq(Math.hypot(1), NaN);
+assertEq(Math.hypot(NaN), NaN);
 
 assertEq(Math.hypot(NaN, 0), NaN);
 assertEq(Math.hypot(0, NaN), NaN);
