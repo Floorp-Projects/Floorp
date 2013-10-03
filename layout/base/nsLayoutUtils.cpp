b95c636898ffb045d33b8bf5c60705d9a0eef055
Cameron McCormack <cam@mcc.id.au>
2013-10-04 04:48:41 +1000

    Bug 921731 - Part 0: Add "layout.css.unset-value.enabled" pref. r=bzbarsky

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -431,6 +431,22 @@ nsLayoutUtils::CSSFiltersEnabled()
   return sCSSFiltersEnabled;
 }
 
+bool
+nsLayoutUtils::UnsetValueEnabled()
+{
+  static bool sUnsetValueEnabled;
+  static bool sUnsetValuePrefCached = false;
+
+  if (!sUnsetValuePrefCached) {
+    sUnsetValuePrefCached = true;
+    Preferences::AddBoolVarCache(&sUnsetValueEnabled,
+                                 "layout.css.unset-value.enabled",
+                                 false);
+  }
+
+  return sUnsetValueEnabled;
+}
+
 void
 nsLayoutUtils::UnionChildOverflow(nsIFrame* aFrame,
                                   nsOverflowAreas& aOverflowAreas)
