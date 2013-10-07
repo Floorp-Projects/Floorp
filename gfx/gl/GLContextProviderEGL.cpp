61139eb6e98eb103d154a2036ad24420b2e10eaf
Jeff Gilbert <jgilbert@mozilla.com>
2013-10-07 15:56:08 -0700

    Bug 917160 - AssureBlitted before switching EGLSurfaces with MakeCurrent. r=bjacob, a=akeybl

--- gfx/gl/GLContextProviderEGL.cpp
+++ gfx/gl/GLContextProviderEGL.cpp
@@ -475,6 +475,15 @@ public:
             return;
 
         // Else, surface changed...
+        if (Screen()) {
+            /* Blit `draw` to `read` if we need to, before we potentially juggle
+             * `read` around. If we don't, we might attach a different `read`,
+             * and *then* hit AssureBlitted, which will blit a dirty `draw` onto
+             * the wrong `read`!
+             */
+            Screen()->AssureBlitted();
+        }
+
         mCurSurface = eglSurface;
         MakeCurrent(true);
     }
