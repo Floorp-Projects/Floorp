9546f698c5b3ad348cd80df243009ef02e0bcc54
Raymond Lee <raymond@raysquare.com>
2013-10-07 14:52:08 +0800

    Bug 859695 - OS.File should be adopted in PlacesBackups.jsm and PlacesUtils.jsm. r=mak

--- toolkit/components/places/tests/bookmarks/test_466303-json-remove-backups.js
+++ toolkit/components/places/tests/bookmarks/test_466303-json-remove-backups.js
@@ -9,39 +9,40 @@ const NUMBER_OF_BACKUPS = 1;
 function run_test() {
   do_test_pending();
 
-  // Get bookmarkBackups directory
-  var bookmarksBackupDir = PlacesBackups.folder;
-
-  // Create an html dummy backup in the past
-  var htmlBackupFile = bookmarksBackupDir.clone();
-  htmlBackupFile.append("bookmarks-2008-01-01.html");
-  if (htmlBackupFile.exists())
-    htmlBackupFile.remove(false);
-  do_check_false(htmlBackupFile.exists());
-  htmlBackupFile.create(Ci.nsILocalFile.NORMAL_FILE_TYPE, 0600);
-  do_check_true(htmlBackupFile.exists());
-
-  // Create a json dummy backup in the past
-  var jsonBackupFile = bookmarksBackupDir.clone();
-  jsonBackupFile.append("bookmarks-2008-01-31.json");
-  if (jsonBackupFile.exists())
-    jsonBackupFile.remove(false);
-  do_check_false(jsonBackupFile.exists());
-  jsonBackupFile.create(Ci.nsILocalFile.NORMAL_FILE_TYPE, 0600);
-  do_check_true(jsonBackupFile.exists());
-
-  // Export bookmarks to JSON.
-  var backupFilename = PlacesBackups.getFilenameForDate();
-  var rx = new RegExp("^" + backupFilename.replace(/\.json/, "") + "(_[0-9]+){0,1}\.json$");
-  var files = bookmarksBackupDir.directoryEntries;
-  var entry;
-  while (files.hasMoreElements()) {
-    entry = files.getNext().QueryInterface(Ci.nsIFile);
-    if (entry.leafName.match(rx))
-      entry.remove(false);
-  }
-
   Task.spawn(function() {
+    // Get bookmarkBackups directory
+    let backupFolder = yield PlacesBackups.getBackupFolder();
+    let bookmarksBackupDir = new FileUtils.File(backupFolder);
+
+    // Create an html dummy backup in the past
+    var htmlBackupFile = bookmarksBackupDir.clone();
+    htmlBackupFile.append("bookmarks-2008-01-01.html");
+    if (htmlBackupFile.exists())
+      htmlBackupFile.remove(false);
+    do_check_false(htmlBackupFile.exists());
+    htmlBackupFile.create(Ci.nsILocalFile.NORMAL_FILE_TYPE, 0600);
+    do_check_true(htmlBackupFile.exists());
+
+    // Create a json dummy backup in the past
+    var jsonBackupFile = bookmarksBackupDir.clone();
+    jsonBackupFile.append("bookmarks-2008-01-31.json");
+    if (jsonBackupFile.exists())
+      jsonBackupFile.remove(false);
+    do_check_false(jsonBackupFile.exists());
+    jsonBackupFile.create(Ci.nsILocalFile.NORMAL_FILE_TYPE, 0600);
+    do_check_true(jsonBackupFile.exists());
+
+    // Export bookmarks to JSON.
+    var backupFilename = PlacesBackups.getFilenameForDate();
+    var rx = new RegExp("^" + backupFilename.replace(/\.json/, "") + "(_[0-9]+){0,1}\.json$");
+    var files = bookmarksBackupDir.directoryEntries;
+    var entry;
+    while (files.hasMoreElements()) {
+      entry = files.getNext().QueryInterface(Ci.nsIFile);
+      if (entry.leafName.match(rx))
+        entry.remove(false);
+    }
+
     yield PlacesBackups.create(NUMBER_OF_BACKUPS);
     files = bookmarksBackupDir.directoryEntries;
     while (files.hasMoreElements()) {
