a7f5e5b4def2e5abf8ed523857db38d874760827
Malini Das <mdas@mozilla.com>
2013-10-08 16:11:45 -0400

    Bug 909129 - fix leaking imported scripts from marionette, r=dburns

--- testing/marionette/marionette-listener.js
+++ testing/marionette/marionette-listener.js
@@ -96,7 +96,7 @@ function registerSelf() {
 
   if (register[0]) {
     listenerId = register[0].id;
-    importedScripts = FileUtils.File(register[0].importedScripts);
+    importedScripts = FileUtils.getFile('TmpD', ['marionetteContentScripts']);
     startListeners();
   }
 }
