27c7c255bfc25352893a9f90b5c294930a3861f3
Peter Van der Beken <peterv@propagandism.org>
2013-10-08 18:18:40 +0200

    Bug 932322 - Make Window's WebIDL properties be own properties of window. r=bz.

--- js/src/tests/js1_5/LexicalConventions/lexical-001.js
+++ js/src/tests/js1_5/LexicalConventions/lexical-001.js
@@ -27,96 +27,96 @@ var BUGNUMBER = '49233';
 var summary = 'Testing numeric literals that begin with 0';
 var statprefix = 'Testing ';
 var quote = "'";
-var status = new Array();
+var asString = new Array();
 var actual = new Array();
 var expect = new Array();
 
 
-status[0]=showStatus('01')
+  asString[0]='01'
   actual[0]=01
   expect[0]=1
 
-  status[1]=showStatus('07')
+  asString[1]='07'
   actual[1]=07
   expect[1]=7
 
-  status[2]=showStatus('08')
+  asString[2]='08'
   actual[2]=08
   expect[2]=8
 
-  status[3]=showStatus('09')
+  asString[3]='09'
   actual[3]=09
   expect[3]=9
 
-  status[4]=showStatus('010')
+  asString[4]='010'
   actual[4]=010
   expect[4]=8
 
-  status[5]=showStatus('017')
+  asString[5]='017'
   actual[5]=017
   expect[5]=15
 
-  status[6]=showStatus('018')
+  asString[6]='018'
   actual[6]=018
   expect[6]=18
 
-  status[7]=showStatus('019')
+  asString[7]='019'
   actual[7]=019
   expect[7]=19
 
-  status[8]=showStatus('079')
+  asString[8]='079'
   actual[8]=079
   expect[8]=79
 
-  status[9]=showStatus('0079')
+  asString[9]='0079'
   actual[9]=0079
   expect[9]=79
 
-  status[10]=showStatus('099')
+  asString[10]='099'
   actual[10]=099
   expect[10]=99
 
-  status[11]=showStatus('0099')
+  asString[11]='0099'
   actual[11]=0099
   expect[11]=99
 
-  status[12]=showStatus('000000000077')
+  asString[12]='000000000077'
   actual[12]=000000000077
   expect[12]=63
 
-  status[13]=showStatus('000000000078')
+  asString[13]='000000000078'
   actual[13]=000000000078
   expect[13]=78
 
-  status[14]=showStatus('0000000000770000')
+  asString[14]='0000000000770000'
   actual[14]=0000000000770000
   expect[14]=258048
 
-  status[15]=showStatus('0000000000780000')
+  asString[15]='0000000000780000'
   actual[15]=0000000000780000
   expect[15]=780000
 
-  status[16]=showStatus('0765432198')
+  asString[16]='0765432198'
   actual[16]=0765432198
   expect[16]=765432198
 
-  status[17]=showStatus('00076543219800')
+  asString[17]='00076543219800'
   actual[17]=00076543219800
   expect[17]=76543219800
 
-  status[18]=showStatus('0000001001007')
+  asString[18]='0000001001007'
   actual[18]=0000001001007
   expect[18]=262663
 
-  status[19]=showStatus('0000001001009')
+  asString[19]='0000001001009'
   actual[19]=0000001001009
   expect[19]=1001009
 
-  status[20]=showStatus('070')
+  asString[20]='070'
   actual[20]=070
   expect[20]=56
 
-  status[21]=showStatus('080')
+  asString[21]='080'
   actual[21]=080
   expect[21]=80
 
@@ -140,9 +140,9 @@ function test()
   printStatus (summary);
 
 
-  for (i=0; i !=status.length; i++)
+  for (i=0; i !=asString.length; i++)
   {
-    reportCompare (expect[i], actual[i], status[i]);
+    reportCompare (expect[i], actual[i], showStatus(asString[i]));
   }
 
   exitFunc ('test');
