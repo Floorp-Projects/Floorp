6d659c197863facab79d6d6c82ed9e4a0c22e5ac
Dan Gohman <sunfish@google.com>
2013-10-09 08:28:03 -0700

    Bug 924649 - IonMonkey: Implement extraName() for several LInstruction subclasses. r=jandem

--- js/src/jit/x86/LIR-x86.h
+++ js/src/jit/x86/LIR-x86.h
@@ -26,6 +26,9 @@ class LBox : public LInstructionHelper<2, 1, 0>
     MIRType type() const {
         return type_;
     }
+    const char *extraName() const {
+        return StringFromMIRType(type_);
+    }
 };
 
 class LBoxFloatingPoint : public LInstructionHelper<2, 1, 1>
@@ -45,6 +48,9 @@ class LBoxFloatingPoint : public LInstructionHelper<2, 1, 1>
 
     MIRType type() const {
         return type_;
+    }
+    const char *extraName() const {
+        return StringFromMIRType(type_);
     }
 };
 
@@ -83,6 +89,9 @@ class LUnboxFloatingPoint : public LInstructionHelper<1, 2, 0>
 
     MIRType type() const {
         return type_;
+    }
+    const char *extraName() const {
+        return StringFromMIRType(type_);
     }
 };
 
