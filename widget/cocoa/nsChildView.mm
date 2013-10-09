48a9353e7d4ead31133f41d1134647f3c71fa289
Markus Stange <mstange@themasta.com>
2013-10-09 10:39:22 -0400

    Bug 914437 - Don't composite windows that are not open. r=mattwoodrow

--- widget/cocoa/nsChildView.mm
+++ widget/cocoa/nsChildView.mm
@@ -2034,15 +2034,15 @@ nsChildView::CleanupWindowEffects()
   mTitlebarImage = nullptr;
 }
 
-void
+bool
 nsChildView::PreRender(LayerManager* aManager)
 {
   nsAutoPtr<GLManager> manager(GLManager::CreateGLManager(aManager));
   if (!manager) {
-    return;
+    return true;
   }
   NSOpenGLContext *glContext = (NSOpenGLContext *)manager->gl()->GetNativeData(GLContext::NativeGLContext);
-  [(ChildView*)mView preRender:glContext];
+  return [(ChildView*)mView preRender:glContext];
 }
 
 void
@@ -2437,7 +2437,9 @@ nsChildView::CleanupRemoteDrawing()
 void
 nsChildView::DoRemoteComposition(const nsIntRect& aRenderRect)
 {
-  [(ChildView*)mView preRender:mGLPresenter->GetNSOpenGLContext()];
+  if (![(ChildView*)mView preRender:mGLPresenter->GetNSOpenGLContext()]) {
+    return;
+  }
   mGLPresenter->BeginFrame(aRenderRect.Size());
 
   // Draw the result from the basic compositor.
@@ -2876,18 +2878,26 @@ NSEvent* gLastDragMouseDownEvent = nil;
   NS_OBJC_END_TRY_ABORT_BLOCK;
 }
 
--(void)preRender:(NSOpenGLContext *)aGLContext
+- (bool)preRender:(NSOpenGLContext *)aGLContext
 {
-  NS_OBJC_BEGIN_TRY_ABORT_BLOCK;
+  NS_OBJC_BEGIN_TRY_ABORT_BLOCK_RETURN;
 
   if (!mGLContext) {
     [self setGLContext:aGLContext];
   }
 
+  if ([[self window] isKindOfClass:[BaseWindow class]] &&
+      ![(BaseWindow*)[self window] isVisibleOrBeingShown]) {
+    // Before the window is shown, our GL context's front FBO is not
+    // framebuffer complete, so we refuse to render.
+    return false;
+  }
   [aGLContext setView:self];
   [aGLContext update];
 
-  NS_OBJC_END_TRY_ABORT_BLOCK;
+  return true;
+
+  NS_OBJC_END_TRY_ABORT_BLOCK_RETURN(false);
 }
 
 - (void)dealloc
