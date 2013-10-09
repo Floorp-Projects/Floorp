966c41951b0aa49d5f2e99bf9a21f50b79249211
Markus Stange <mstange@themasta.com>
2013-10-09 10:39:22 -0400

    Bug 914437 - Don't composite windows that are not open. r=mattwoodrow

--- widget/cocoa/nsChildView.h
+++ widget/cocoa/nsChildView.h
@@ -345,7 +345,7 @@ enum {
 - (BOOL)isInFailingLeftClickThrough;
 
 - (void)setGLContext:(NSOpenGLContext *)aGLContext;
-- (void)preRender:(NSOpenGLContext *)aGLContext;
+- (bool)preRender:(NSOpenGLContext *)aGLContext;
 
 - (BOOL)isCoveringTitlebar;
 
@@ -553,7 +553,7 @@ public:
   virtual gfxASurface* GetThebesSurface();
   virtual void PrepareWindowEffects() MOZ_OVERRIDE;
   virtual void CleanupWindowEffects() MOZ_OVERRIDE;
-  virtual void PreRender(LayerManager* aManager) MOZ_OVERRIDE;
+  virtual bool PreRender(LayerManager* aManager) MOZ_OVERRIDE;
   virtual void DrawWindowOverlay(LayerManager* aManager, nsIntRect aRect) MOZ_OVERRIDE;
 
   virtual void UpdateThemeGeometries(const nsTArray<ThemeGeometry>& aThemeGeometries);
