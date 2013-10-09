6c6f914226aefa75294f98fcdd14bc8fd68a8cff
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2013-10-09 11:15:41 +0900

    Bug 919929 - AlertsService for Metro should send alertshow and alertfinished observer. r=jimm

--- browser/metro/base/content/helperui/AlertsHelper.js
+++ browser/metro/base/content/helperui/AlertsHelper.js
@@ -4,21 +4,37 @@
 
 var AlertsHelper = {
   _listener: null,
-  _cookie: "",
 
   showAlertNotification: function ah_show(aImageURL, aTitle, aText, aTextClickable, aCookie, aListener) {
-    Services.obs.addObserver(this, "metro_native_toast_clicked", false);
+    if (aListener) {
+      Services.obs.addObserver(this, "metro_native_toast_clicked", false);
+      Services.obs.addObserver(this, "metro_native_toast_dismissed", false);
+      Services.obs.addObserver(this, "metro_native_toast_shown", false);
+    }
     this._listener = aListener;
-    this._cookie = aCookie;
 
-    Services.metro.showNativeToast(aTitle, aText, aImageURL);
+    Services.metro.showNativeToast(aTitle, aText, aImageURL, aCookie);
+  },
+
+  closeAlert: function ah_close() {
+    if (this._listener) {
+      Services.obs.removeObserver(this, "metro_native_toast_shown");
+      Services.obs.removeObserver(this, "metro_native_toast_clicked");
+      Services.obs.removeObserver(this, "metro_native_toast_dismissed");
+      this._listener = null;
+    }
   },
 
   observe: function(aSubject, aTopic, aData) {
     switch (aTopic) {
       case "metro_native_toast_clicked":
-        Services.obs.removeObserver(this, "metro_native_toast_clicked");
-        this._listener.observe(null, "alertclickcallback", this._cookie);
+        this._listener.observe(null, "alertclickcallback", aData);
+        break;
+      case "metro_native_toast_shown":
+        this._listener.observe(null, "alertshow", aData);
+        break;
+      case "metro_native_toast_dismissed":
+        this._listener.observe(null, "alertfinished", aData);
         break;
     }
   }
