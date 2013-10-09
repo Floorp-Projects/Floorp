fc3c60d0e92fee3213ed8f3bfdeae0606646f444
Markus Stange <mstange@themasta.com>
2013-10-09 10:39:23 -0400

    Bug 886999 - Lock the GL context before compositing on the compositor thread or before calling setView or update on the main thread. r=mattwoodrow

--- widget/nsIWidget.h
+++ widget/nsIWidget.h
@@ -96,8 +96,8 @@ typedef void* nsNativeWidget;
 #endif
 
 #define NS_IWIDGET_IID \
-{ 0x8e2afc1c, 0x7087, 0x4ec2, \
-  { 0xac, 0xc6, 0xd4, 0xf2, 0x3e, 0x13, 0xd2, 0xb7 } }
+{ 0xa1f684e6, 0x2ae1, 0x4513, \
+  { 0xb6, 0x89, 0xf4, 0xd4, 0xfe, 0x9d, 0x2c, 0xdb } }
 
 /*
  * Window shadow styles
@@ -1223,6 +1223,15 @@ class nsIWidget : public nsISupports {
      * OMTC is not enabled.
      */
     virtual bool PreRender(LayerManager* aManager) = 0;
+
+    /**
+     * Called after rendering using OpenGL. Not called when rendering was
+     * cancelled by a negative return value from PreRender.
+     *
+     * Always called from the compositing thread, which may be the main-thread if
+     * OMTC is not enabled.
+     */
+    virtual void PostRender(LayerManager* aManager) = 0;
 
     /**
      * Called before the LayerManager draws the layer tree.
