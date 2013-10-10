59b55ded4111cbfcfd117bad05c6c470c29edb59
Dan Gohman <sunfish@google.com>
2013-10-10 05:31:05 -0700

    Bug 925088 - IonMonkey: Micro-optimize x86 and x64's branchTruncateDouble. r=jandem

--- js/src/jit/x64/MacroAssembler-x64.h
+++ js/src/jit/x64/MacroAssembler-x64.h
@@ -1040,12 +1040,14 @@ class MacroAssemblerX64 : public MacroAssemblerX86Shared
     void loadConstantFloat32(float f, const FloatRegister &dest);
 
     void branchTruncateDouble(const FloatRegister &src, const Register &dest, Label *fail) {
-        const uint64_t IndefiniteIntegerValue = 0x8000000000000000;
-        JS_ASSERT(dest != ScratchReg);
         cvttsd2sq(src, dest);
-        movq(ImmWord(IndefiniteIntegerValue), ScratchReg);
-        cmpq(dest, ScratchReg);
-        j(Assembler::Equal, fail);
+
+        // cvttsd2sq returns 0x8000000000000000 on failure. Test for it by
+        // subtracting 1 and testing overflow (this avoids the need to
+        // materialize that value in a register).
+        cmpq(dest, Imm32(1));
+        j(Assembler::Overflow, fail);
+
         movl(dest, dest); // Zero upper 32-bits.
     }
 
