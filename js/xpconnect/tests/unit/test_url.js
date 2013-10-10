dab932f0bdb76382eae2106578f66ed7b56ae42b
Andrea Marchesini <amarchesini@mozilla.com>
2013-10-10 08:56:01 +0200

    Bug 920015 - Expose DOM URL to js modules, r=ehsan, f=emk, r=bz, r=bholley, r=smaug
    --HG--
    rename : js/xpconnect/tests/unit/test_textDecoder.js => js/xpconnect/tests/unit/test_url.js

---
+++ js/xpconnect/tests/unit/test_url.js
@@ -0,0 +1,10 @@
+function run_test() {
+  var Cu = Components.utils;
+  var sb = new Cu.Sandbox('http://www.example.com',
+                          { wantGlobalProperties: ["URL"] });
+  sb.do_check_eq = do_check_eq;
+  Cu.evalInSandbox('do_check_eq(new URL("http://www.example.com").host, "www.example.com");',
+                   sb);
+  Cu.importGlobalProperties(["URL"]);
+  do_check_eq(new URL("http://www.example.com").host, "www.example.com");
+}
