6504c9894c210ab37949113369e30fd6f5ef300d
Josh Aas <joshmoz@gmail.com>
2013-10-11 13:58:35 -0500

    Bug 925016: Always use method_exchangeImplementations when swizzling methods on OS X. r=smichaud

--- widget/cocoa/nsToolkit.mm
+++ widget/cocoa/nsToolkit.mm
@@ -290,10 +290,7 @@ nsToolkit* nsToolkit::GetToolkit()
 // Since the Method type becomes an opaque type as of Objective-C 2.0, we'll
 // have to switch to using accessor methods like method_exchangeImplementations()
 // when we build 64-bit binaries that use Objective-C 2.0 (on and for Leopard
-// and above).  But these accessor methods aren't available in Objective-C 1
-// (or on Tiger).  So we need to access Method's members directly for (Tiger-
-// capable) binaries (32-bit or 64-bit) that use Objective-C 1 (as long as we
-// keep supporting Tiger).
+// and above).
 //
 // Be aware that, if aClass doesn't have an orgMethod selector but one of its
 // superclasses does, the method substitution will (in effect) take place in
@@ -321,13 +318,7 @@ nsresult nsToolkit::SwizzleMethods(Class aClass, SEL orgMethod, SEL posedMethod,
   if (!original || !posed)
     return NS_ERROR_FAILURE;
 
-#ifdef __LP64__
   method_exchangeImplementations(original, posed);
-#else
-  IMP aMethodImp = original->method_imp;
-  original->method_imp = posed->method_imp;
-  posed->method_imp = aMethodImp;
-#endif
 
   return NS_OK;
 
