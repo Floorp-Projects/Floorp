da98a4a1837b06dd8fd28e8fd928780fed3731ec
Gregory Szorc <gps@mozilla.com>
2013-10-11 07:22:49 -0700

    Bug 585016 - Move buildlist.py to a mozbuild action; r=mshal

--- js/src/config/makefiles/target_libs.mk
+++ js/src/config/makefiles/target_libs.mk
@@ -25,8 +25,8 @@ ifdef IS_COMPONENT
 	$(INSTALL) $(IFLAGS2) $(SHARED_LIBRARY) $(FINAL_TARGET)/components
 	$(ELF_DYNSTR_GC) $(FINAL_TARGET)/components/$(SHARED_LIBRARY)
 ifndef NO_COMPONENTS_MANIFEST
-	@$(PYTHON) $(MOZILLA_DIR)/config/buildlist.py $(FINAL_TARGET)/chrome.manifest "manifest components/components.manifest"
-	@$(PYTHON) $(MOZILLA_DIR)/config/buildlist.py $(FINAL_TARGET)/components/components.manifest "binary-component $(SHARED_LIBRARY)"
+	$(call py_action,buildlist,$(FINAL_TARGET)/chrome.manifest "manifest components/components.manifest")
+	$(call py_action,buildlist,$(FINAL_TARGET)/components/components.manifest "binary-component $(SHARED_LIBRARY)")
 endif
 endif # IS_COMPONENT
 endif # SHARED_LIBRARY
