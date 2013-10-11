17dc758ff93ab9c0db49017863512b360d685a7f
Stephen Pohl <spohl.mozilla.bugs@gmail.com>
2013-10-11 10:50:25 -0400

    Backout 751bcb37cdb6 for bustage on a CLOSED TREE. r=me

--- widget/MouseEvents.h
+++ widget/MouseEvents.h
@@ -318,8 +318,7 @@ public:
     deltaMode(nsIDOMWheelEvent::DOM_DELTA_PIXEL),
     customizedByUserPrefs(false), isMomentum(false), isPixelOnlyDevice(false),
     lineOrPageDeltaX(0), lineOrPageDeltaY(0), scrollType(SCROLL_DEFAULT),
-    overflowDeltaX(0.0), overflowDeltaY(0.0),
-    mViewPortIsOverscrolled(false)
+    overflowDeltaX(0.0), overflowDeltaY(0.0)
   {
   }
 
@@ -400,12 +399,6 @@ public:
   double overflowDeltaX;
   double overflowDeltaY;
 
-  // Whether or not the parent of the currently overscrolled frame is the
-  // ViewPort. This is false in situations when an element on the page is being
-  // overscrolled (such as a text field), but true when the 'page' is being
-  // overscrolled.
-  bool mViewPortIsOverscrolled;
-
   void AssignWheelEventData(const WidgetWheelEvent& aEvent, bool aCopyTargets)
   {
     AssignMouseEventBaseData(aEvent, aCopyTargets);
@@ -422,7 +415,6 @@ public:
     scrollType = aEvent.scrollType;
     overflowDeltaX = aEvent.overflowDeltaX;
     overflowDeltaY = aEvent.overflowDeltaY;
-    mViewPortIsOverscrolled = aEvent.mViewPortIsOverscrolled;
   }
 };
 
