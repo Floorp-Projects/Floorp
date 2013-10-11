14a0bc1a507094ddf0e6467704d75605ff29109f
Ryan VanderMeulen <ryanvm@gmail.com>
2013-10-11 14:11:56 -0400

    Backed out changeset 1cb746254315 (bug 916126) for Windows mochitest-other orange. ba=backout

--- toolkit/components/downloads/nsDownloadManager.cpp
+++ toolkit/components/downloads/nsDownloadManager.cpp
@@ -2525,7 +2525,10 @@ nsDownload::~nsDownload()
 }
 
 NS_IMETHODIMP nsDownload::SetSha256Hash(const nsACString& aHash) {
-  return NS_ERROR_NOT_IMPLEMENTED;
+  MOZ_ASSERT(NS_IsMainThread(), "Must call SetSha256Hash on main thread");
+  // This will be used later to query the application reputation service.
+  mHash = aHash;
+  return NS_OK;
 }
 
 #ifdef MOZ_ENABLE_GIO
@@ -2590,7 +2593,6 @@ nsDownload::SetState(DownloadState aState)
 #endif
     case nsIDownloadManager::DOWNLOAD_FINISHED:
     {
-      // Do what exthandler would have done if necessary
       nsresult rv = ExecuteDesiredAction();
       if (NS_FAILED(rv)) {
         // We've failed to execute the desired action.  As a result, we should
@@ -2932,6 +2934,8 @@ nsDownload::OnStateChange(nsIWebProgress *aWebProgress,
                           nsIRequest *aRequest, uint32_t aStateFlags,
                           nsresult aStatus)
 {
+  MOZ_ASSERT(NS_IsMainThread(), "Must call OnStateChange in main thread");
+
   // We don't want to lose access to our member variables
   nsRefPtr<nsDownload> kungFuDeathGrip = this;
 
@@ -3191,10 +3195,12 @@ nsDownload::Finalize()
 nsresult
 nsDownload::ExecuteDesiredAction()
 {
-  // If we have a temp file and we have resumed, we have to do what the
-  // external helper app service would have done.
-  if (!mTempFile || !WasResumed())
+  // nsExternalHelperAppHandler is the only caller of AddDownload that sets a
+  // tempfile parameter. In this case, execute the desired action according to
+  // the saved mime info.
+  if (!mTempFile) {
     return NS_OK;
+  }
 
   // We need to bail if for some reason the temp file got removed
   bool fileExists;
