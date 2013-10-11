4112bb1f851efab6b8c9eed42f5d1232f843e959
Benoit Girard <b56girard@gmail.com>
2013-10-11 16:47:47 -0400

    Bug 921212 - Rotate buffer in place to avoid gralloc surface allocation. r=Bas, a=koi+

--- gfx/2d/DrawTargetCairo.cpp
+++ gfx/2d/DrawTargetCairo.cpp
@@ -377,6 +377,7 @@ NeedIntermediateSurface(const Pattern& aPattern, const DrawOptions& aOptions)
 
 DrawTargetCairo::DrawTargetCairo()
   : mContext(nullptr)
+  , mLockedBits(nullptr)
   , mPathObserver(nullptr)
 {
 }
@@ -390,6 +391,7 @@ DrawTargetCairo::~DrawTargetCairo()
   if (mSurface) {
     cairo_surface_destroy(mSurface);
   }
+  MOZ_ASSERT(!mLockedBits);
 }
 
 IntSize
@@ -413,6 +415,31 @@ DrawTargetCairo::Snapshot()
                                      CairoContentToGfxFormat(content),
                                      this);
   return mSnapshot;
+}
+
+bool
+DrawTargetCairo::LockBits(uint8_t** aData, IntSize* aSize,
+                          int32_t* aStride, SurfaceFormat* aFormat)
+{
+  if (cairo_surface_get_type(mSurface) == CAIRO_SURFACE_TYPE_IMAGE) {
+    WillChange();
+
+    mLockedBits = cairo_image_surface_get_data(mSurface);
+    *aData = mLockedBits;
+    *aSize = GetSize();
+    *aStride = cairo_image_surface_get_stride(mSurface);
+    *aFormat = GetFormat();
+    return true;
+  }
+
+  return false;
+}
+
+void
+DrawTargetCairo::ReleaseBits(uint8_t* aData)
+{
+  MOZ_ASSERT(mLockedBits == aData);
+  mLockedBits = nullptr;
 }
 
 void
@@ -1075,6 +1102,7 @@ DrawTargetCairo::WillChange(const Path* aPath /* = nullptr */)
     mPathObserver->PathWillChange();
     mPathObserver = nullptr;
   }
+  MOZ_ASSERT(!mLockedBits);
 }
 
 void
