b2ef254383c3ae6a58943cc95875e84c20c0d309
Jan de Mooij <jdemooij@mozilla.com>
2013-10-12 15:39:22 +0200

    Bug 925312 - Fix small bug in ArrayJoin. r=terrence

--- js/src/jsarray.cpp
+++ js/src/jsarray.cpp
@@ -1086,14 +1086,14 @@ ArrayJoin(JSContext *cx, CallArgs &args)
 
     // Steps 4 and 5
     RootedString sepstr(cx, nullptr);
+    JS::Anchor<JSString *> anchor(nullptr);
+    const jschar *sepchars;
+    size_t seplen;
     if (!Locale && args.hasDefined(0)) {
         sepstr = ToString<CanGC>(cx, args[0]);
         if (!sepstr)
             return false;
-    }
-    const jschar *sepchars;
-    size_t seplen;
-    if (sepstr) {
+        anchor = sepstr;
         sepchars = sepstr->getChars(cx);
         if (!sepchars)
             return false;
