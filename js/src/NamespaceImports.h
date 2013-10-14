0ee6a10203b696204ca00becdb771ce41d7cc1ab
Eddy Bruel <ejpbruel@mozilla.com>
2013-10-14 13:25:47 +0200

    Bug 880330 - Replace options bitfield in JSContext with a struct; r=bholley

--- js/src/NamespaceImports.h
+++ js/src/NamespaceImports.h
@@ -38,6 +38,11 @@ template <typename T> class AutoVectorRooter;
 template<typename K, typename V> class AutoHashMapRooter;
 template<typename T> class AutoHashSetRooter;
 
+class JS_PUBLIC_API(ContextOptions);
+class JS_PUBLIC_API(AutoSaveContextOptions);
+
+JS_PUBLIC_API(ContextOptions &) ContextOptionsRef(JSContext *cx);
+
 }
 
 // Do the importing.
