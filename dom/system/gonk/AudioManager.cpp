06c55c5586e19a95f720d8d224649a840ed47d29
Marco Chen <mchen@mozilla.com>
2013-10-14 18:45:35 +0800

    Bug 923173 - Use NS_WARNING for unexpected name to avoid crashing. r=ehsan

--- dom/system/gonk/AudioManager.cpp
+++ dom/system/gonk/AudioManager.cpp
@@ -138,7 +138,8 @@ public:
       static_cast<AudioManager *>(audioManager.get())->SetStreamVolumeIndex(
         AUDIO_STREAM_BLUETOOTH_SCO, volIndex);
     } else {
-      MOZ_ASSUME_UNREACHABLE("unexpected audio channel for volume control");
+      MOZ_ASSUME_UNREACHABLE("unexpected audio channel for initializing "
+                             "volume control");
     }
 
     return NS_OK;
