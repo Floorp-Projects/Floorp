5fa063762365ae7e868740bd83820159e2cd7320
Nick Fitzgerald <fitzgen@gmail.com>
2013-10-14 11:44:00 -0700

    Bug 924442 - Disallow pretty printing when a source is black boxed; r=vporof

---
+++ browser/devtools/debugger/test/browser_dbg_pretty-print-10.js
@@ -0,0 +1,67 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Make sure that we disable the pretty print button for black boxed sources,
+ * and that clicking it doesn't do anything.
+ */
+
+const TAB_URL = EXAMPLE_URL + "doc_pretty-print.html";
+
+let gTab, gDebuggee, gPanel, gDebugger;
+let gEditor, gSources;
+
+function test() {
+  initDebugger(TAB_URL).then(([aTab, aDebuggee, aPanel]) => {
+    gTab = aTab;
+    gDebuggee = aDebuggee;
+    gPanel = aPanel;
+    gDebugger = gPanel.panelWin;
+    gEditor = gDebugger.DebuggerView.editor;
+    gSources = gDebugger.DebuggerView.Sources;
+
+    waitForSourceShown(gPanel, "code_ugly.js")
+      .then(testSourceIsUgly)
+      .then(blackBoxSource)
+      .then(waitForThreadEvents.bind(null, gPanel, "blackboxchange"))
+      .then(clickPrettyPrintButton)
+      .then(testSourceIsStillUgly)
+      .then(() => closeDebuggerAndFinish(gPanel))
+      .then(null, aError => {
+        ok(false, "Got an error: " + DevToolsUtils.safeErrorString(aError));
+      });
+  });
+}
+
+function testSourceIsUgly() {
+  ok(!gEditor.getText().contains("\n    "),
+     "The source shouldn't be pretty printed yet.");
+}
+
+function blackBoxSource() {
+  const checkbox = gDebugger.document.querySelector(
+    ".selected .side-menu-widget-item-checkbox");
+  checkbox.click();
+}
+
+function clickPrettyPrintButton() {
+  EventUtils.sendMouseEvent({ type: "click" },
+                            gDebugger.document.getElementById("pretty-print"),
+                            gDebugger);
+}
+
+function testSourceIsStillUgly() {
+  const { source } = gSources.selectedItem.attachment;
+  return gDebugger.DebuggerController.SourceScripts.getText(source).then(([, text]) => {
+    ok(!text.contains("\n    "));
+  });
+}
+
+registerCleanupFunction(function() {
+  gTab = null;
+  gDebuggee = null;
+  gPanel = null;
+  gDebugger = null;
+  gEditor = null;
+  gSources = null;
+});
