6645e77fc2c6a5e19ee2697c609d182456abfd64
Honza Bambas <honzab.moz@firemni.cz>
2013-10-15 20:08:26 +0200

    Bug 924938 - Correctly include CacheLog.h as the first file in a module to force logging in opt builds, r=michal

--- netwerk/cache2/CacheStorage.cpp
+++ netwerk/cache2/CacheStorage.cpp
@@ -2,10 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "CacheLog.h"
 #include "CacheStorage.h"
 #include "CacheStorageService.h"
 #include "CacheEntry.h"
-#include "CacheLog.h"
 
 #include "OldWrappers.h"
 
