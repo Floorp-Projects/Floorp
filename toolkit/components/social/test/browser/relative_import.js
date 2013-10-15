94c317fdbeef077eff33f1bf03cdef98d8fcf182
Florian Quèze <florian@queze.net>
2013-10-16 00:59:19 +0200

    Bug 926902 - Incorrect error locations for errors from scripts importScripts'ed by the frameworker, r=markh.

--- toolkit/components/social/test/browser/relative_import.js
+++ toolkit/components/social/test/browser/relative_import.js
@@ -1,5 +1,8 @@
 dump("relative_import file\n");
 
+// Please keep 'causeError' on line 4; we test the error location.
+function causeError() { does_not_exist(); }
+
 testVar = "oh hai";
 function testFunc() {
   return "oh hai";
