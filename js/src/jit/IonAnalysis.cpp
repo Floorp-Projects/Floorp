eb1177dfd1e46c8a9cce82eafa2915f3007204d4
Luke Wagner <luke@mozilla.com>
2013-10-15 12:13:34 -0500

    Bug 900669 - OdinMonkey: asm.js caching (shell-only support) (r=sstangl)
    --HG--
    extra : rebase_source : c48a7a8cd83a9af6f39884b800a026016353c7c6

--- js/src/jit/IonAnalysis.cpp
+++ js/src/jit/IonAnalysis.cpp
@@ -1274,6 +1274,8 @@ void
 jit::AssertGraphCoherency(MIRGraph &graph)
 {
 #ifdef DEBUG
+    if (!js_IonOptions.assertGraphConsistency)
+        return;
     AssertBasicGraphCoherency(graph);
     AssertReversePostOrder(graph);
 #endif
@@ -1287,6 +1289,8 @@ jit::AssertExtendedGraphCoherency(MIRGraph &graph)
     // are split)
 
 #ifdef DEBUG
+    if (!js_IonOptions.assertGraphConsistency)
+        return;
     AssertGraphCoherency(graph);
 
     uint32_t idx = 0;
