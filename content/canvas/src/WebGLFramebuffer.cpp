93f44b676a9dfa4590b724ec400bcb6f50eb261b
Dan Glastonbury <dglastonbury@mozilla.com>
2013-10-16 08:27:59 -0400

    Bug 843668 - Implement WebGL -draft- extension EXT_sRGB. r=jgilbert
    https://www.khronos.org/registry/webgl/extensions/EXT_sRGB/
    See content/canvas/test/webgl/conformance/extensions/ext-sRGB.html for example
    usage.

--- content/canvas/src/WebGLFramebuffer.cpp
+++ content/canvas/src/WebGLFramebuffer.cpp
@@ -132,7 +132,9 @@ WebGLFramebuffer::Attachment::IsComplete() const {
                     format == LOCAL_GL_LUMINANCE ||
                     format == LOCAL_GL_LUMINANCE_ALPHA ||
                     format == LOCAL_GL_RGB ||
-                    format == LOCAL_GL_RGBA);
+                    format == LOCAL_GL_RGBA ||
+                    format == LOCAL_GL_SRGB_EXT ||
+                    format == LOCAL_GL_SRGB_ALPHA_EXT);
         }
         MOZ_CRASH("Invalid WebGL attachment poin?");
     }
@@ -153,7 +155,8 @@ WebGLFramebuffer::Attachment::IsComplete() const {
                  mAttachmentPoint < GLenum(LOCAL_GL_COLOR_ATTACHMENT0 + WebGLContext::sMaxColorAttachments)) {
             return (format == LOCAL_GL_RGB565 ||
                     format == LOCAL_GL_RGB5_A1 ||
-                    format == LOCAL_GL_RGBA4);
+                    format == LOCAL_GL_RGBA4 ||
+                    format == LOCAL_GL_SRGB8_ALPHA8_EXT);
         }
         MOZ_CRASH("Invalid WebGL attachment poin?");
     }
