098902f7e9cb380e5319e7dc8958f1bcbb710942
Nathan Froyd <froydnj@mozilla.com>
2013-10-16 15:25:36 -0400

    Bug 927551 - make darwin xptcall sources compilable in a singlepass universal build; r=bsmedberg,glandium

---
+++ xpcom/reflect/xptcall/src/md/unix/xptcinvoke_darwin.cpp
@@ -0,0 +1,14 @@
+/* -*- Mode: C -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#if defined(__i386__)
+#include "xptcinvoke_gcc_x86_unix.cpp"
+#elif defined(__x86_64__)
+#include "xptcinvoke_x86_64_unix.cpp"
+#elif defined(__ppc__)
+#include "xptcinvoke_ppc_rhapsody.cpp"
+#else
+#error unknown cpu architecture
+#endif
