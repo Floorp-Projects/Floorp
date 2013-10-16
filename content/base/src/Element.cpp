ab52d0f3dd055794d5ff4a8e465a7bf33324f4dd
Olli Pettay <Olli.Pettay@helsinki.fi>
2013-10-16 22:29:08 +0300

    Bug 926361 - Make sure mutation observers can't see the parent when unbinding. r=jst, a=abillings

--- content/base/src/Element.cpp
+++ content/base/src/Element.cpp
@@ -1168,7 +1168,9 @@ Element::UnbindFromTree(bool aDeep, bool aNullParent)
       nsIDocument::UnlockPointer();
     }
     if (GetParent()) {
-      NS_RELEASE(mParent);
+      nsINode* p = mParent;
+      mParent = nullptr;
+      NS_RELEASE(p);
     } else {
       mParent = nullptr;
     }
