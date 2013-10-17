1b8b9121624ae23ac04bba998d6a044237adc318
Sam Foster <sfoster@mozilla.com>
2013-10-17 12:14:08 -0700

    Bug 927226 - Add build-in animation for start grids. r=rsilveira,mbrubeck

--- browser/metro/base/tests/mochitest/helpers/ViewStateHelper.js
+++ browser/metro/base/tests/mochitest/helpers/ViewStateHelper.js
@@ -7,6 +7,7 @@
 
 const snappedSize = 330;
 const portraitSize = 900;
+const maxPortraitHeight = 900;
 
 function setSnappedViewstate() {
   ok(isLandscapeMode(), "setSnappedViewstate expects landscape mode to work.");
@@ -32,9 +33,14 @@ function setPortraitViewstate() {
   let browser = Browser.selectedBrowser;
 
   let fullWidth = browser.clientWidth;
+  let fullHeight = browser.clientHeight;
   let padding = fullWidth - portraitSize;
 
   browser.style.borderRight = padding + "px solid gray";
+
+  // cap the height to create more even surface for testing on
+  if (fullHeight > maxPortraitHeight)
+    browser.style.borderBottom = (fullHeight - maxPortraitHeight) + "px solid gray";
 
   ContentAreaObserver._updateViewState("portrait");
 
@@ -47,6 +53,7 @@ function restoreViewstate() {
   ok(isLandscapeMode(), "restoreViewstate should restore landscape mode.");
 
   Browser.selectedBrowser.style.removeProperty("border-right");
+  Browser.selectedBrowser.style.removeProperty("border-bottom");
 
   yield waitForMs(0);
 }
