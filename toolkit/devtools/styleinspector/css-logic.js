46c61d54cf50f2d51081cfe96bacefe376063c85
Michael Ratcliffe <mratcliffe@mozilla.com>
2013-10-17 19:03:52 +0100

    Bug 918716 - Add color swatches to devtools output parser. r=jwalker

--- toolkit/devtools/styleinspector/css-logic.js
+++ toolkit/devtools/styleinspector/css-logic.js
@@ -51,8 +51,6 @@ const RX_PSEUDO = /\s*:?:([\w-]+)(\(?\)?)\s*/g;
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-let {colorUtils} = require("devtools/css-color");
-
 function CssLogic()
 {
   // The cache of examined CSS properties.
@@ -1614,7 +1612,7 @@ function CssSelectorInfo(aSelector, aProperty, aValue, aStatus)
   this.selector = aSelector;
   this.property = aProperty;
   this.status = aStatus;
-  this.value = colorUtils.processCSSString(aValue);
+  this.value = aValue;
   let priority = this.selector.cssRule.getPropertyPriority(this.property);
   this.important = (priority === "important");
 }
