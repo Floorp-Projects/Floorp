c70c1927ae0af0dd52bfaab85a22cb2f26c955dd
Masayuki Nakano <masayuki@d-toybox.com>
2013-10-18 15:10:24 +0900

    Bug 920425 part.19 Use mozilla::WidgetEvent::AsTextEvent() r=smaug

--- content/events/src/nsEventStateManager.cpp
+++ content/events/src/nsEventStateManager.cpp
@@ -1303,7 +1303,7 @@ nsEventStateManager::PreHandleEvent(nsPresContext* aPresContext,
     break;
   case NS_TEXT_TEXT:
     {
-      WidgetTextEvent *textEvent = static_cast<WidgetTextEvent*>(aEvent);
+      WidgetTextEvent *textEvent = aEvent->AsTextEvent();
       if (IsTargetCrossProcess(textEvent)) {
         // Will not be handled locally, remote the event
         if (GetCrossProcessTarget()->SendTextEvent(*textEvent)) {
