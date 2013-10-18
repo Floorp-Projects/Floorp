41a335c5160654faa4d4d56317797f0a6145521a
Ryan VanderMeulen <ryanvm@gmail.com>
2013-10-18 10:21:38 -0400

    Backed out changeset 6150269410b2 (bug 921212) for bustage on a CLOSED TREE.

--- gfx/2d/DrawTargetCairo.cpp
+++ gfx/2d/DrawTargetCairo.cpp
@@ -399,7 +399,6 @@ NeedIntermediateSurface(const Pattern& aPattern, const DrawOptions& aOptions)
 
 DrawTargetCairo::DrawTargetCairo()
   : mContext(nullptr)
-  , mLockedBits(nullptr)
 {
 }
 
@@ -409,7 +408,6 @@ DrawTargetCairo::~DrawTargetCairo()
   if (mSurface) {
     cairo_surface_destroy(mSurface);
   }
-  MOZ_ASSERT(!mLockedBits);
 }
 
 IntSize
@@ -433,31 +431,6 @@ DrawTargetCairo::Snapshot()
                                      CairoContentToGfxFormat(content),
                                      this);
   return mSnapshot;
-}
-
-bool
-DrawTargetCairo::LockBits(uint8_t** aData, IntSize* aSize,
-                          int32_t* aStride, SurfaceFormat* aFormat)
-{
-  if (cairo_surface_get_type(mSurface) == CAIRO_SURFACE_TYPE_IMAGE) {
-    WillChange();
-
-    mLockedBits = cairo_image_surface_get_data(mSurface);
-    *aData = mLockedBits;
-    *aSize = GetSize();
-    *aStride = cairo_image_surface_get_stride(mSurface);
-    *aFormat = GetFormat();
-    return true;
-  }
-
-  return false;
-}
-
-void
-DrawTargetCairo::ReleaseBits(uint8_t* aData)
-{
-  MOZ_ASSERT(mLockedBits = aData);
-  mLockedBits = nullptr;
 }
 
 void
@@ -1166,7 +1139,6 @@ void
 DrawTargetCairo::WillChange(const Path* aPath /* = nullptr */)
 {
   MarkSnapshotIndependent();
-  MOZ_ASSERT(!mLiveSurface);
 }
 
 void
