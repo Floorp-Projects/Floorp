fee640f7b20a3633a7c617b465bf40d320899c3c
Terrence Cole <terrence@mozilla.com>
2013-10-18 11:43:35 -0700

    Bug 928056 - Fix ordering of pause and state change when entering GC; r=billm

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -4076,9 +4076,10 @@ EndSweepPhase(JSRuntime *rt, JSGCInvocationKind gckind, bool lastGC)
 namespace {
 
 /* ...while this class is to be used only for garbage collection. */
-class AutoGCSession : AutoTraceSession
+class AutoGCSession
 {
     AutoPauseWorkersForTracing pause;
+    AutoTraceSession session;
 
   public:
     explicit AutoGCSession(JSRuntime *rt);
@@ -4105,8 +4106,8 @@ AutoTraceSession::~AutoTraceSession()
 }
 
 AutoGCSession::AutoGCSession(JSRuntime *rt)
-  : AutoTraceSession(rt, MajorCollecting),
-    pause(rt)
+  : pause(rt),
+    session(rt, MajorCollecting)
 {
     runtime->gcIsNeeded = false;
     runtime->gcInterFrameGC = true;
