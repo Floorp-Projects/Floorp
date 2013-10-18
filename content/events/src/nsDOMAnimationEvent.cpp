e6ff91d50a60cfe636797d96febfd54e381d43ab
Masayuki Nakano <masayuki@d-toybox.com>
2013-10-18 15:10:22 +0900

    Bug 920425 part.11 Use mozilla::WidgetEvent::AsAnimationEvent() r=smaug

--- content/events/src/nsDOMAnimationEvent.cpp
+++ content/events/src/nsDOMAnimationEvent.cpp
@@ -46,9 +46,10 @@ nsDOMAnimationEvent::Constructor(const mozilla::dom::GlobalObject& aGlobal,
 
   aRv = e->InitEvent(aType, aParam.mBubbles, aParam.mCancelable);
 
-  e->AnimationEvent()->animationName = aParam.mAnimationName;
-  e->AnimationEvent()->elapsedTime = aParam.mElapsedTime;
-  e->AnimationEvent()->pseudoElement = aParam.mPseudoElement;
+  InternalAnimationEvent* internalEvent = e->mEvent->AsAnimationEvent();
+  internalEvent->animationName = aParam.mAnimationName;
+  internalEvent->elapsedTime = aParam.mElapsedTime;
+  internalEvent->pseudoElement = aParam.mPseudoElement;
 
   e->SetTrusted(trusted);
   return e.forget();
@@ -57,7 +58,7 @@ nsDOMAnimationEvent::Constructor(const mozilla::dom::GlobalObject& aGlobal,
 NS_IMETHODIMP
 nsDOMAnimationEvent::GetAnimationName(nsAString & aAnimationName)
 {
-  aAnimationName = AnimationEvent()->animationName;
+  aAnimationName = mEvent->AsAnimationEvent()->animationName;
   return NS_OK;
 }
 
@@ -68,10 +69,16 @@ nsDOMAnimationEvent::GetElapsedTime(float *aElapsedTime)
   return NS_OK;
 }
 
+float
+nsDOMAnimationEvent::ElapsedTime()
+{
+  return mEvent->AsAnimationEvent()->elapsedTime;
+}
+
 NS_IMETHODIMP
 nsDOMAnimationEvent::GetPseudoElement(nsAString& aPseudoElement)
 {
-  aPseudoElement = AnimationEvent()->pseudoElement;
+  aPseudoElement = mEvent->AsAnimationEvent()->pseudoElement;
   return NS_OK;
 }
 
