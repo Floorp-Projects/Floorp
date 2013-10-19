51acd839d47f281ce57f61ec8a5be43d11572063
Georg Fritzsche <georg.fritzsche@googlemail.com>
2013-10-19 17:15:18 +0200

    Bug 853694 - Cannot send crash reports for click-to-play plugins. r=jaws

--- browser/base/content/browser-plugins.js
+++ browser/base/content/browser-plugins.js
@@ -804,6 +804,10 @@ var gPluginHandler = {
       // so the pluginHost.getPermissionStringForType call is protected
       if (gPluginHandler.canActivatePlugin(plugin) &&
           aPluginInfo.permissionString == pluginHost.getPermissionStringForType(plugin.actualType)) {
+        let overlay = this.getPluginUI(plugin, "main");
+        if (overlay) {
+          overlay.removeEventListener("click", gPluginHandler._overlayClickListener, true);
+        }
         plugin.playPlugin();
       }
     }
