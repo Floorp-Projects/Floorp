0baca52c27f568c7b2e62130d0865b7370aba62d
Alive Kuo <alive@mozilla.com>
2013-10-21 19:16:00 +0100

    Bug 927310 - dispatch activitydone event from mozbrowser iframe

--- dom/activities/src/ActivityRequestHandler.js
+++ dom/activities/src/ActivityRequestHandler.js
@@ -9,6 +9,7 @@ const Ci = Components.interfaces;
 const Cu = Components.utils;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
 
 XPCOMUtils.defineLazyServiceGetter(this, "cpmm",
                                    "@mozilla.org/childprocessmessagemanager;1",
@@ -53,6 +54,7 @@ ActivityRequestHandler.prototype = {
       "id": this._id,
       "result": aResult
     });
+    Services.obs.notifyObservers(null, "activity-success", this._id);
   },
 
   postError: function arh_postError(aError) {
@@ -60,6 +62,7 @@ ActivityRequestHandler.prototype = {
       "id": this._id,
       "error": aError
     });
+    Services.obs.notifyObservers(null, "activity-error", this._id);
   },
 
   classID: Components.ID("{9326952a-dbe3-4d81-a51f-d9c160d96d6b}"),
