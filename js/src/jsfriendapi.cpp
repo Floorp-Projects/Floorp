f3b136c799ac111ffc1d3600005b431283d3e2d7
Steve Fink <sfink@mozilla.com>
2013-10-21 13:53:37 -0700

    Bug 929151 - Disable GGC when object metadata is used, r=terrence

--- js/src/jsfriendapi.cpp
+++ js/src/jsfriendapi.cpp
@@ -1120,11 +1120,7 @@ js::AutoCTypesActivityCallback::AutoCTypesActivityCallback(JSContext *cx,
 JS_FRIEND_API(void)
 js::SetObjectMetadataCallback(JSContext *cx, ObjectMetadataCallback callback)
 {
-    // Clear any jitcode in the runtime, which behaves differently depending on
-    // whether there is a creation callback.
-    ReleaseAllJITCode(cx->runtime()->defaultFreeOp());
-
-    cx->compartment()->objectMetadataCallback = callback;
+    cx->compartment()->setObjectMetadataCallback(callback);
 }
 
 JS_FRIEND_API(bool)
