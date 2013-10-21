d09f1827031b6f9a9a6e99e037e13eabe67dda98
Camilo Viecco <cviecco@mozilla.com>
2013-10-21 14:27:46 -0700

    Bug 917380 - Part 1 - Add filters for EV root list. r=briansmith

--- security/manager/ssl/src/nsIdentityChecking.cpp
+++ security/manager/ssl/src/nsIdentityChecking.cpp
@@ -14,6 +14,7 @@
 #include "nsILineInputStream.h"
 #include "nsPromiseFlatString.h"
 #include "nsTArray.h"
+#include "nsNSSCertTrust.h"
 
 #include "cert.h"
 #include "base64.h"
@@ -808,6 +809,19 @@ register_oid(const SECItem *oid_item, const char *oid_name)
   return SECOID_AddEntry(&od);
 }
 
+static void
+addToCertListIfTrusted(CERTCertList* certList, CERTCertificate *cert) {
+  CERTCertTrust nssTrust;
+  if (CERT_GetCertTrust(cert, &nssTrust) != SECSuccess) {
+    return;
+  }
+  unsigned int flags = SEC_GET_TRUST_FLAGS(&nssTrust, trustSSL);
+
+  if (flags & CERTDB_TRUSTED_CA) {
+    CERT_AddCertToListTail(certList, CERT_DupCertificate(cert));
+  }
+}
+
 #ifdef PSM_ENABLE_TEST_EV_ROOTS
 class nsMyTrustedEVInfoClass : public nsMyTrustedEVInfo
 {
@@ -1058,8 +1072,9 @@ getRootsForOidFromExternalRootsFile(CERTCertList* certList,
     nsMyTrustedEVInfoClass *ev = testEVInfos->ElementAt(i);
     if (!ev)
       continue;
-    if (policyOIDTag == ev->oid_tag)
-      CERT_AddCertToListTail(certList, CERT_DupCertificate(ev->cert));
+    if (policyOIDTag == ev->oid_tag) {
+      addToCertListIfTrusted(certList, ev->cert);
+    }
   }
 
   return false;
@@ -1100,8 +1115,9 @@ getRootsForOid(SECOidTag oid_tag)
     nsMyTrustedEVInfo &entry = myTrustedEVInfos[iEV];
     if (!entry.oid_name) // invalid or placeholder list entry
       continue;
-    if (entry.oid_tag == oid_tag)
-      CERT_AddCertToListTail(certList, CERT_DupCertificate(entry.cert));
+    if (entry.oid_tag == oid_tag) {
+      addToCertListIfTrusted(certList, entry.cert);
+    }
   }
 
 #ifdef PSM_ENABLE_TEST_EV_ROOTS
