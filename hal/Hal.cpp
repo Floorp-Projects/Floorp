9aa6e88650216a5ec0c9bcba4fc2398d93349e35
George Wright <gwright@mozilla.com>
2013-10-22 14:15:24 -0500

    Bug 920160 - Add prefs for SkiaGL cache size. r=snorp, a=koi+

--- hal/Hal.cpp
+++ hal/Hal.cpp
@@ -1185,5 +1185,12 @@ StopDiskSpaceWatcher()
   PROXY_IF_SANDBOXED(StopDiskSpaceWatcher());
 }
 
+uint32_t
+GetTotalSystemMemory()
+{
+  return hal_impl::GetTotalSystemMemory();
+}
+
+
 } // namespace hal
 } // namespace mozilla
