63abc18db863d5f28d17a81a0dbe41ac27d1e9dc
John Daggett <jdaggett@mozilla.com>
2013-10-22 13:06:20 +0200

    Bug 915440 - move null URL fix to after format hint is processed. r=dbaron

--- layout/style/nsFontFaceLoader.cpp
+++ layout/style/nsFontFaceLoader.cpp
@@ -622,12 +622,6 @@ nsUserFontSet::InsertRule(nsCSSFontFaceRule* aRule, uint8_t aSheetType,
       case eCSSUnit_URL:
         face->mIsLocal = false;
         face->mURI = val.GetURLValue();
-        if (!face->mURI) {
-          // if URI not valid, omit from src array
-          srcArray.RemoveElementAt(srcArray.Length() - 1);
-          NS_WARNING("null url in @font-face rule");
-          continue;
-        }
         face->mReferrer = val.GetURLStructValue()->mReferrer;
         face->mOriginPrincipal = val.GetURLStructValue()->mOriginPrincipal;
         NS_ASSERTION(face->mOriginPrincipal, "null origin principal in @font-face rule");
@@ -662,6 +656,12 @@ nsUserFontSet::InsertRule(nsCSSFontFaceRule* aRule, uint8_t aSheetType,
             face->mFormatFlags |= FLAG_FORMAT_UNKNOWN;
           }
           i++;
+        }
+        if (!face->mURI) {
+          // if URI not valid, omit from src array
+          srcArray.RemoveElementAt(srcArray.Length() - 1);
+          NS_WARNING("null url in @font-face rule");
+          continue;
         }
         break;
       default:
