e02564e1432cd3518d6db55cd402c844383602c1
Jason Orendorff <jorendorff@mozilla.com>
2013-10-23 11:42:25 -0500

    Bug 901979 - Assertion failure: !global->nativeLookup(cx, id), at ../jsobjinlines.h:1125. r=Waldo.

---
+++ js/src/jit-test/tests/proxy/bug901979-2.js
@@ -0,0 +1,31 @@
+// A proxy on the prototype chain of the global should not observe anything at
+// all about lazy resolution of globals.
+
+var global = this;
+var status = "pass";
+
+// This is a little tricky. There are two proxies.
+// 1. handler is a proxy that fails the test if you try to call a method on it.
+var metaHandler = {
+  get: _ => { status = "SMASH"; },
+  has: _ => { status = "SMASH"; },
+  invoke: _ => { status = "SMASH"; }
+};
+var handler = new Proxy({}, metaHandler);
+
+// 2. Then we create a proxy using 'handler' as its handler. This means the test
+//    will fail if *any* method of the handler is called, not just get/has/invoke.
+var angryProxy = new Proxy(Object.create(null), handler);
+this.__proto__ = angryProxy;
+Object.prototype.__proto__ = angryProxy;
+
+// Trip the alarm once, to make sure the proxies are working.
+this.nonExistingProperty;
+assertEq(status, "SMASH");
+
+// OK. Reset the status and run the actual test.
+status = "pass";
+Map;
+ArrayBuffer;
+Date;
+assertEq(status, "pass");
