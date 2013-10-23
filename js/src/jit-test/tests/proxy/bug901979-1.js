48c3f090ac29e8eecf55a852c2a248c4934d65c5
Jason Orendorff <jorendorff@mozilla.com>
2013-10-23 11:42:25 -0500

    Bug 901979 - Assertion failure: !global->nativeLookup(cx, id), at ../jsobjinlines.h:1125. r=Waldo.

---
+++ js/src/jit-test/tests/proxy/bug901979-1.js
@@ -0,0 +1,12 @@
+// A proxy on the prototype chain of the global can't intercept lazy definition of globals.
+// Thanks to André Bargull for this one.
+
+var global = this;
+var status = "pass";
+var handler = {
+  get: function get(t, pk, r) { status = "FAIL get"; },
+  has: function has(t, pk) { status = "FAIL has"; }
+};
+Object.prototype.__proto__ = new Proxy(Object.create(null), handler);
+Map;
+assertEq(status, "pass");
