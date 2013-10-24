f17bde93dc1dd6486c2e05479f5f7af68a219d21
Matt Woodrow <mwoodrow@mozilla.com>
2013-10-24 16:35:29 +0200

    Bug 844819 - Don't create DrawTargets for invalid cairo surfaces. r=Bas, a=lsblakk

--- gfx/thebes/gfxContext.cpp
+++ gfx/thebes/gfxContext.cpp
@@ -567,6 +567,10 @@ gfxContext::DrawSurface(gfxASurface *surface, const gfxSize& size)
     RefPtr<SourceSurface> surf =
       gfxPlatform::GetPlatform()->GetSourceSurfaceForSurface(mDT, surface);
 
+    if (!surf) {
+      return;
+    }
+
     Rect rect(0, 0, Float(size.width), Float(size.height));
     rect.Intersect(Rect(0, 0, Float(surf->GetSize().width), Float(surf->GetSize().height)));
 
@@ -1383,6 +1387,7 @@ gfxContext::SetSource(gfxASurface *surface, const gfxPoint& offset)
     CurrentState().sourceSurfCairo = surface;
     CurrentState().sourceSurface =
       gfxPlatform::GetPlatform()->GetSourceSurfaceForSurface(mDT, surface);
+    CurrentState().color = Color(0, 0, 0, 0);
   }
 }
 
@@ -1485,6 +1490,10 @@ gfxContext::Mask(gfxASurface *surface, const gfxPoint& offset)
     // Lifetime needs to be limited here as we may simply wrap surface's data.
     RefPtr<SourceSurface> sourceSurf =
       gfxPlatform::GetPlatform()->GetSourceSurfaceForSurface(mDT, surface);
+
+    if (!sourceSurf) {
+      return;
+    }
 
     gfxPoint pt = surface->GetDeviceOffset();
 
