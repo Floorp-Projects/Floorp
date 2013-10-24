3e4fd22c7288c52320576a4ebafa59ed8e81f7d6
Nick Alexander <nalexander@mozilla.com>
2013-10-24 15:38:14 -0700

    Bug 925185 - Part 1: Add add_java_jar to moz.build. r=gps

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -31,6 +31,7 @@ from .data import (
     PreprocessedWebIDLFile,
     Program,
     ReaderSummary,
+    SandboxWrapped,
     TestWebIDLFile,
     TestManifest,
     VariablePassthru,
@@ -247,6 +248,9 @@ class TreeMetadataEmitter(LoggingMixin):
             for path in sandbox.get('%s_MANIFESTS' % prefix, []):
                 for obj in self._process_test_manifest(sandbox, info, path):
                     yield obj
+
+        for name, jar in sandbox.get('JAVA_JAR_TARGETS', {}).items():
+            yield SandboxWrapped(sandbox, jar)
 
     def _process_test_manifest(self, sandbox, info, manifest_path):
         flavor, install_prefix, filter_inactive = info
