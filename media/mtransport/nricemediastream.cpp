38720907e388168ea24c3d1e1672f0f2739fc400
Byron Campen [:bwc] <docfaraday@gmail.com>
2013-10-24 11:45:10 -0700

    Bug 906990 - Part 6: Add a codeword field to NrIceCandidatePair so related logging can be pulled from RLogRingBuffer. Proof of concept in ice_unittest. r=ekr

--- media/mtransport/nricemediastream.cpp
+++ media/mtransport/nricemediastream.cpp
@@ -291,6 +291,7 @@ nsresult NrIceMediaStream::GetCandidatePairs(std::vector<NrIceCandidatePair>*
     pair.nominated = p1->peer_nominated || p1->nominated;
     pair.selected = p1->local->component &&
                     p1->local->component->active == p1;
+    pair.codeword = p1->codeword;
 
     if (!ToNrIceCandidate(*(p1->local), &pair.local) ||
         !ToNrIceCandidate(*(p1->remote), &pair.remote)) {
