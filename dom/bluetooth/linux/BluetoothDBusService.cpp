1ddd1f40ebdc691a14790d7e2b38172a808aed3e
Ed Morley <emorley@mozilla.com>
2013-10-25 10:52:17 +0100

    Backed out changeset 9302086dcead (bug 920551)

--- dom/bluetooth/linux/BluetoothDBusService.cpp
+++ dom/bluetooth/linux/BluetoothDBusService.cpp
@@ -2635,7 +2635,6 @@ BluetoothDBusService::IsConnected(const uint16_t aServiceUuid)
   return profile->IsConnected();
 }
 
-#ifdef MOZ_B2G_RIL
 void
 BluetoothDBusService::AnswerWaitingCall(BluetoothReplyRunnable* aRunnable)
 {
@@ -2668,7 +2667,6 @@ BluetoothDBusService::ToggleCalls(BluetoothReplyRunnable* aRunnable)
 
   DispatchBluetoothReply(aRunnable, BluetoothValue(true), EmptyString());
 }
-#endif // MOZ_B2G_RIL
 
 class OnUpdateSdpRecordsRunnable : public nsRunnable
 {
