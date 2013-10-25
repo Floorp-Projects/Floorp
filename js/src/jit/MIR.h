bd0c8a911620b34c7f27a267d397726fee5cd5c6
Dan Gohman <sunfish@google.com>
2013-10-25 15:58:03 -0700

    Bug 926627 - IonMonkey: Remove the code for optimizing away truncates of MToFloat32. r=nbp a=abillings

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -2755,8 +2755,6 @@ class MToFloat32
     }
 
     void computeRange();
-    bool truncate();
-    bool isOperandTruncated(size_t index) const;
 
     bool canConsumeFloat32() const { return true; }
     bool canProduceFloat32() const { return true; }
