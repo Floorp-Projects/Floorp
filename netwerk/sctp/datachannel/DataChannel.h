756f0284a33bd9f0bc3b73a352932a73128bc8c1
Randell Jesup <rjesup@jesup.org>
2013-10-25 16:08:18 -0400

    Bug 930778: Support DataChannel ACK for unordered channels r=tuexen

--- netwerk/sctp/datachannel/DataChannel.h
+++ netwerk/sctp/datachannel/DataChannel.h
@@ -207,10 +207,11 @@ private:
   DataChannel* FindChannelByStream(uint16_t stream);
   uint16_t FindFreeStream();
   bool RequestMoreStreams(int32_t aNeeded = 16);
-  int32_t SendControlMessage(void *msg, uint32_t len, uint16_t streamOut);
+  int32_t SendControlMessage(void *msg, uint32_t len, uint16_t stream);
   int32_t SendOpenRequestMessage(const nsACString& label, const nsACString& protocol,
-                                 uint16_t streamOut,
+                                 uint16_t stream,
                                  bool unordered, uint16_t prPolicy, uint32_t prValue);
+  int32_t SendOpenAckMessage(uint16_t stream);
   int32_t SendMsgInternal(DataChannel *channel, const char *data,
                           uint32_t length, uint32_t ppid);
   int32_t SendBinary(DataChannel *channel, const char *data,
@@ -226,13 +227,15 @@ private:
   void ProcessQueuedOpens();
   void ClearResets();
   void SendOutgoingStreamReset();
-  void ResetOutgoingStream(uint16_t streamOut);
+  void ResetOutgoingStream(uint16_t stream);
   void HandleOpenRequestMessage(const struct rtcweb_datachannel_open_request *req,
                                 size_t length,
-                                uint16_t streamIn);
-  void HandleUnknownMessage(uint32_t ppid, size_t length, uint16_t streamIn);
-  void HandleDataMessage(uint32_t ppid, const void *buffer, size_t length, uint16_t streamIn);
-  void HandleMessage(const void *buffer, size_t length, uint32_t ppid, uint16_t streamIn);
+                                uint16_t stream);
+  void HandleOpenAckMessage(const struct rtcweb_datachannel_ack *ack,
+                            size_t length, uint16_t stream);
+  void HandleUnknownMessage(uint32_t ppid, size_t length, uint16_t stream);
+  void HandleDataMessage(uint32_t ppid, const void *buffer, size_t length, uint16_t stream);
+  void HandleMessage(const void *buffer, size_t length, uint32_t ppid, uint16_t stream);
   void HandleAssociationChangeEvent(const struct sctp_assoc_change *sac);
   void HandlePeerAddressChangeEvent(const struct sctp_paddr_change *spc);
   void HandleRemoteErrorEvent(const struct sctp_remote_error *sre);
