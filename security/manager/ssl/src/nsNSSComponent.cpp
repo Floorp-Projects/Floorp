73fc0bf55b00f66197503f3a1ad8ac8177de884c
Brian Smith <brian@briansmith.org>
2013-10-26 01:01:37 -0700

    Bug 733647 - Enable TLS 1.1 by default. r=wtc, a=bajaj

--- security/manager/ssl/src/nsNSSComponent.cpp
+++ security/manager/ssl/src/nsNSSComponent.cpp
@@ -960,14 +960,15 @@ void nsNSSComponent::setValidationOptions()
   SSL_ClearSessionCache();
 }
 
-// Enable the TLS versions given in the prefs, defaulting to SSL 3.0 and
-// TLS 1.0 when the prefs aren't set or when they are set to invalid values.
+// Enable the TLS versions given in the prefs, defaulting to SSL 3.0 (min
+// version) and TLS 1.1 (max version) when the prefs aren't set or set to
+// invalid values.
 nsresult
 nsNSSComponent::setEnabledTLSVersions()
 {
-  // keep these values in sync with security-prefs.js and firefox.js
+  // keep these values in sync with security-prefs.js
   static const int32_t PSM_DEFAULT_MIN_TLS_VERSION = 0;
-  static const int32_t PSM_DEFAULT_MAX_TLS_VERSION = 1;
+  static const int32_t PSM_DEFAULT_MAX_TLS_VERSION = 2;
 
   int32_t minVersion = Preferences::GetInt("security.tls.version.min",
                                            PSM_DEFAULT_MIN_TLS_VERSION);
