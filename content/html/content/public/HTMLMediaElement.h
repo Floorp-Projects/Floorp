d00bdf243195c2b196f6a1d3e4c7159fd34ac241
Carsten "Tomcat" Book <cbook@mozilla.com>
2013-10-28 15:34:08 +0100

    Backed out changeset 5506e7033c85 (bug 925594) perma-orange mochitest test-failure on b2g-ics
    --HG--
    extra : transplant_source : %19E%9F_%BF%84%27%11%0D%DE%CA.%E7%09%9D%FD3%1A2U

--- content/html/content/public/HTMLMediaElement.h
+++ content/html/content/public/HTMLMediaElement.h
@@ -17,7 +17,6 @@
 #include "DecoderTraits.h"
 #include "nsIAudioChannelAgent.h"
 #include "mozilla/Attributes.h"
-#include "mozilla/dom/AudioChannelBinding.h"
 #include "mozilla/dom/TextTrackManager.h"
 
 // Define to output information on decoding and painting framerate
@@ -514,8 +513,12 @@ public:
 
   double MozFragmentEnd();
 
-  AudioChannel MozAudioChannelType() const;
-  void SetMozAudioChannelType(AudioChannel aValue, ErrorResult& aRv);
+  // XPCOM GetMozAudioChannelType() is OK
+
+  void SetMozAudioChannelType(const nsAString& aValue, ErrorResult& aRv)
+  {
+    SetHTMLAttr(nsGkAtoms::mozaudiochannel, aValue, aRv);
+  }
 
   TextTrackList* TextTracks() const;
 
