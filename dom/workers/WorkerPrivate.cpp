ccc885658724770e9dbdd4b4dafe298353b37ba6
Nils Maier <maierman@web.de>
2013-10-28 04:53:00 +0000

    Bug 929297 - Part 2: Avoid calling amIAddonManager in reporters off the main process. r=njn

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -1990,6 +1990,11 @@ private:
 
     mAlreadyMappedToAddon = true;
 
+    if (XRE_GetProcessType() != GeckoProcessType_Default) {
+      // Only try to access the service from the main process.
+      return;
+    }
+
     nsAutoCString addonId;
     bool ok;
     nsCOMPtr<amIAddonManager> addonManager =
