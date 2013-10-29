0f400117f31920cdbf7989da1c40c06ac9591736
Vicamo Yang <vyang@mozilla.com>
2013-10-29 16:12:39 +0800

    Bug 920551 - 2.c/2: fix CellBroadcast. r=hsinyi

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -4522,7 +4522,11 @@ pref("dom.telephony.enabled", false);
 pref("dom.telephony.defaultServiceId", 0);
 
 // Cell Broadcast API
+#ifdef MOZ_B2G_RIL
+pref("dom.cellbroadcast.enabled", true);
+#else
 pref("dom.cellbroadcast.enabled", false);
+#endif
 
 // ICC API
 pref("dom.icc.enabled", false);
