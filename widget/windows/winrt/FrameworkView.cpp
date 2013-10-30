b595aa98fcae5f783f65d5df81f9c879cdb08637
Rodrigo Silveira <rsilveira@mozilla.com>
2013-10-29 21:29:07 -0700

    Bug 927172 - Can't touch scroll when second monitor is attached r=jimm

--- widget/windows/winrt/FrameworkView.cpp
+++ widget/windows/winrt/FrameworkView.cpp
@@ -258,6 +258,7 @@ FrameworkView::UpdateWidgetSizeAndPosition()
   NS_ASSERTION(mWindow, "SetWindow must be called before UpdateWidgetSizeAndPosition!");
   NS_ASSERTION(mWidget, "SetWidget must be called before UpdateWidgetSizeAndPosition!");
 
+  UpdateBounds();
   mWidget->Move(0, 0);
   mWidget->Resize(0, 0, mWindowBounds.width, mWindowBounds.height, true);
   mWidget->SizeModeChanged();
@@ -284,19 +285,32 @@ void FrameworkView::SetDpi(float aDpi)
     LogFunction();
 
     mDPI = aDpi;
-    // Often a DPI change implies a window size change.
-    NS_ASSERTION(mWindow, "SetWindow must be called before SetDpi!");
-    Rect logicalBounds;
-    mWindow->get_Bounds(&logicalBounds);
-
-    // convert to physical (device) pixels
-    mWindowBounds = MetroUtils::LogToPhys(logicalBounds);
 
     // notify the widget that dpi has changed
     if (mWidget) {
       mWidget->ChangedDPI();
+      UpdateBounds();
     }
   }
+}
+
+void
+FrameworkView::UpdateBounds()
+{
+  if (!mWidget)
+    return;
+
+  RECT winRect;
+  GetClientRect(mWidget->GetICoreWindowHWND(), &winRect);
+
+  Rect logicalBounds;
+  logicalBounds.X = winRect.left;
+  logicalBounds.Y = winRect.top;
+  logicalBounds.Width = winRect.right - winRect.left; 
+  logicalBounds.Height = winRect.bottom - winRect.top;
+
+  // convert to physical (device) pixels
+  mWindowBounds = MetroUtils::LogToPhys(logicalBounds);
 }
 
 void
@@ -307,6 +321,7 @@ FrameworkView::SetWidget(MetroWidget* aWidget)
   LogFunction();
   mWidget = aWidget;
   mWidget->FindMetroWindow();
+  UpdateBounds();
 }
 
 ////////////////////////////////////////////////////
@@ -393,11 +408,6 @@ FrameworkView::OnWindowSizeChanged(ICoreWindow* aSender, IWindowSizeChangedEvent
   if (mShuttingDown) {
     return S_OK;
   }
-
-  NS_ASSERTION(mWindow, "SetWindow must be called before OnWindowSizeChanged!");
-  Rect logicalBounds;
-  mWindow->get_Bounds(&logicalBounds);
-  mWindowBounds = MetroUtils::LogToPhys(logicalBounds);
 
   UpdateWidgetSizeAndPosition();
   return S_OK;
