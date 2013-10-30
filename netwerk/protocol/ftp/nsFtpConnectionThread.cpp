584db6655d92ffde538a66d9e73a769672f98448
John Shih <jshih@mozilla.com>
2013-10-30 18:00:17 +0800

    Bug 922924 - Centralize the code for network per-app metering. r=jduell

--- netwerk/protocol/ftp/nsFtpConnectionThread.cpp
+++ netwerk/protocol/ftp/nsFtpConnectionThread.cpp
@@ -1691,6 +1691,10 @@ nsFtpState::Init(nsFtpChannel *channel)
     // initialize counter for network metering
     mCountRecv = 0;
 
+#ifdef MOZ_WIDGET_GONK
+    NS_GetActiveNetworkInterface(mActiveNetwork);
+#endif
+
     mKeepRunning = true;
     mSuppliedEntityID = channel->EntityID();
 
@@ -2199,22 +2203,6 @@ nsresult
 nsFtpState::SaveNetworkStats(bool enforce)
 {
 #ifdef MOZ_WIDGET_GONK
-    MOZ_ASSERT(NS_IsMainThread());
-
-    // Obtain active network
-    nsresult rv;
-    if (!mActiveNetwork) {
-        nsCOMPtr<nsINetworkManager> networkManager =
-            do_GetService("@mozilla.org/network/manager;1", &rv);
-
-        if (NS_FAILED(rv) || !networkManager) {
-            mActiveNetwork = nullptr;
-            return rv;
-        }
-
-        networkManager->GetActive(getter_AddRefs(mActiveNetwork));
-    }
-
     // Obtain app id
     uint32_t appId;
     bool isInBrowser;
@@ -2237,6 +2225,7 @@ nsFtpState::SaveNetworkStats(bool enforce)
         return NS_OK;
     }
 
+    nsresult rv;
     nsCOMPtr<nsINetworkStatsServiceProxy> networkStatsServiceProxy =
         do_GetService("@mozilla.org/networkstatsServiceProxy;1", &rv);
     if (NS_FAILED(rv)) {
