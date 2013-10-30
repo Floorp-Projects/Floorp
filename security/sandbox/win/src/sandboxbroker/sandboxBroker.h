eba93af7a1ae02c02a1adcaefd95c363b6f65f9d
Brian R. Bondy <netzen@gmail.com>
2013-10-30 16:58:52 -0700

    Bug 925571 - Initial Windows content process sandbox broker code. r=aklotz

---
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef __SECURITY_SANDBOX_SANDBOXBROKER_H__
+#define __SECURITY_SANDBOX_SANDBOXBROKER_H__
+
+#ifdef SANDBOX_EXPORTS
+#define SANDBOX_EXPORT __declspec(dllexport)
+#else
+#define SANDBOX_EXPORT __declspec(dllimport)
+#endif
+
+namespace sandbox {
+  class BrokerServices;
+}
+
+namespace mozilla {
+
+class SANDBOX_EXPORT SandboxBroker
+{
+public:
+  SandboxBroker();
+  bool LaunchApp(const wchar_t *aPath, const wchar_t *aArguments,
+                 void **aProcessHandle);
+  virtual ~SandboxBroker();
+
+private:
+  sandbox::BrokerServices *mBrokerService;
+};
+
+} // mozilla
+
+#endif
