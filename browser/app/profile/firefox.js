b366b7221e8ec09f4a2328001c86d683de133643
Monica Chew <mmc@mozilla.com>
2013-10-31 14:15:14 -0700

    Bug 895476: Enable updating local lists for application reputation (r=gcp)

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -818,6 +818,9 @@ pref("urlclassifier.gethashnoise", 4);
 // a gethash request will be forced to check that the result is still in
 // the database.
 pref("urlclassifier.max-complete-age", 2700);
+// Tables for application reputation.
+pref("urlclassifier.download_block_table", "goog-badbinurl-shavar");
+pref("urlclassifier.download_allow_table", "goog-downloadwhite-digest256");
 #endif
 
 pref("browser.geolocation.warning.infoURL", "https://www.mozilla.org/%LOCALE%/firefox/geolocation/");
