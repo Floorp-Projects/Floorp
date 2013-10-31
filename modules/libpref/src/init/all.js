54296bc3652e3432ed8aa5fedc5f88287722470b
Vicamo Yang <vyang@mozilla.com>
2013-10-31 21:08:13 +0800

    Bug 920551 - 2.e/2: fix MobileConnection. r=hsinyi,khuey, a=koi+

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -4536,7 +4536,11 @@ pref("dom.icc.enabled", false);
 #endif
 
 // Mobile Connection API
+#ifdef MOZ_B2G_RIL
+pref("dom.mobileconnection.enabled", true);
+#else
 pref("dom.mobileconnection.enabled", false);
+#endif
 
 // Voice Mail API
 pref("dom.voicemail.enabled", false);
