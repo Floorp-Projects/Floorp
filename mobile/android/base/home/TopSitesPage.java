6edd6350a8ebb88737096418458e13c03d7df4f6
Richard Newman <rnewman@mozilla.com>
2013-10-31 10:35:16 -0700

    Bug 931918 - Part 0: tidying and logging. r=bnicholson

--- mobile/android/base/home/TopSitesPage.java
+++ mobile/android/base/home/TopSitesPage.java
@@ -515,6 +515,7 @@ public class TopSitesPage extends HomeFragment {
 
     public class TopSitesGridAdapter extends CursorAdapter {
         // Cache to store the thumbnails.
+        // Ensure that this is only accessed from the UI thread.
         private Map<String, Bitmap> mThumbnails;
 
         public TopSitesGridAdapter(Context context, Cursor cursor) {
