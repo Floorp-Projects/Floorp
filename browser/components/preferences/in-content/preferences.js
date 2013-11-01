dddd9e2bd7f9b4128fbfe1a4f89fb607fb05dec7
Tim Taubert <ttaubert@mozilla.com>
2013-11-01 12:08:37 +0100

    Backed out changeset 109710b0f5f6 (bug 754344)

--- browser/components/preferences/in-content/preferences.js
+++ browser/components/preferences/in-content/preferences.js
@@ -12,13 +12,11 @@ const Cr = Components.results;
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 
-addEventListener("DOMContentLoaded", function onLoad() {
-  removeEventListener("DOMContentLoaded", onLoad);
-  init_all();
-});
-
 function init_all() {
   document.documentElement.instantApply = true;
+  window.history.replaceState("landing", document.title);
+  window.addEventListener("popstate", onStatePopped, true);
+  updateCommands();
   gMainPane.init();
   gPrivacyPane.init();
   gAdvancedPane.init();
@@ -29,32 +27,12 @@ function init_all() {
   var initFinished = document.createEvent("Event");
   initFinished.initEvent("Initialized", true, true);
   document.dispatchEvent(initFinished);
-
-  let categories = document.getElementById("categories");
-  categories.addEventListener("select", event => gotoPref(event.target.value));
-  window.addEventListener("popstate", event => selectCategory(event.state));
-
-  if (history.length > 1 && history.state) {
-    updateCommands();
-    selectCategory(history.state);
-  } else {
-    history.replaceState("paneGeneral", document.title);
-  }
-}
-
-function selectCategory(name) {
-  let categories = document.getElementById("categories");
-  let item = categories.querySelector(".category[value=" + name + "]");
-  categories.selectedItem = item;
 }
 
 function gotoPref(page) {
-  if (history.state != page) {
-    window.history.pushState(page, document.title);
-  }
-
-  updateCommands();
   search(page, "data-category");
+  window.history.pushState(page, document.title);
+  updateCommands();
 }
  
 function cmd_back() {
@@ -63,6 +41,11 @@ function cmd_back() {
  
 function cmd_forward() {
   window.history.forward();
+}
+
+function onStatePopped(aEvent) {
+  updateCommands();
+  search(aEvent.state, "data-category");
 }
 
 function updateCommands() {
