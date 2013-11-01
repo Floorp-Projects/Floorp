f2ffb79520a3dc83869fbf9275d3c7996b818311
Meadhbh Hamrick <mhamrick@mozilla.com>
2013-11-01 04:48:57 -0700

    Bug 861266: Enable TLS 1.2 by default, r=briansmith, a=bbajaj

--- security/manager/ssl/src/nsNSSComponent.cpp
+++ security/manager/ssl/src/nsNSSComponent.cpp
@@ -967,14 +967,14 @@ void nsNSSComponent::setValidationOptions()
 }
 
 // Enable the TLS versions given in the prefs, defaulting to SSL 3.0 (min
-// version) and TLS 1.1 (max version) when the prefs aren't set or set to
+// version) and TLS 1.2 (max version) when the prefs aren't set or set to
 // invalid values.
 nsresult
 nsNSSComponent::setEnabledTLSVersions()
 {
   // keep these values in sync with security-prefs.js
   static const int32_t PSM_DEFAULT_MIN_TLS_VERSION = 0;
-  static const int32_t PSM_DEFAULT_MAX_TLS_VERSION = 2;
+  static const int32_t PSM_DEFAULT_MAX_TLS_VERSION = 3;
 
   int32_t minVersion = Preferences::GetInt("security.tls.version.min",
                                            PSM_DEFAULT_MIN_TLS_VERSION);
