45e93927a0e77694b2d6afda8aafb0160929cbc2
Jim Mathies <jmathies@mozilla.com>
2013-11-01 05:09:37 -0500

    Bug 930995 - Display port should be set sync when the apz requests a repaint. r=kats

--- widget/windows/winrt/APZController.h
+++ widget/windows/winrt/APZController.h
@@ -7,18 +7,23 @@
 #include "mozwrlbase.h"
 
 #include "mozilla/layers/GeckoContentController.h"
+#include "mozilla/layers/APZCTreeManager.h"
 #include "FrameMetrics.h"
 #include "Units.h"
+
+class nsIWidgetListener;
 
 namespace mozilla {
 namespace widget {
 namespace winrt {
 
-class APZController : public mozilla::layers::GeckoContentController
+class APZController :
+  public mozilla::layers::GeckoContentController
 {
   typedef mozilla::layers::FrameMetrics FrameMetrics;
 
 public:
+  // GeckoContentController interface
   virtual void RequestContentRepaint(const FrameMetrics& aFrameMetrics);
   virtual void HandleDoubleTap(const mozilla::CSSIntPoint& aPoint);
   virtual void HandleSingleTap(const mozilla::CSSIntPoint& aPoint);
@@ -27,6 +32,16 @@ public:
   virtual void PostDelayedTask(Task* aTask, int aDelayMs);
   virtual void HandlePanBegin();
   virtual void HandlePanEnd();
+  
+  void SetWidgetListener(nsIWidgetListener* aWidgetListener);
+  void UpdateScrollOffset(const mozilla::layers::ScrollableLayerGuid& aScrollLayerId, CSSIntPoint& aScrollOffset);
+
+public:
+  static nsRefPtr<mozilla::layers::APZCTreeManager> sAPZC;
+
+private:
+  nsIWidgetListener* mWidgetListener;
+  CSSIntPoint mLastScrollOffset;
 };
 
 } } }
