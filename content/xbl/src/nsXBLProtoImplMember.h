a4eb7e9498187f513d2a8dbb0c370f24069036ac
Bobby Holley <bobbyholley@gmail.com>
2013-11-01 15:31:56 +0100

    Bug 911864 - Introduce an explicit opt-in attribute for exposing methods and properties to untrusted content. r=smaug, a=bajaj

--- content/xbl/src/nsXBLProtoImplMember.h
+++ content/xbl/src/nsXBLProtoImplMember.h
@@ -61,7 +61,12 @@ struct nsXBLTextWithLineNumber
 class nsXBLProtoImplMember
 {
 public:
-  nsXBLProtoImplMember(const PRUnichar* aName) :mNext(nullptr) { mName = ToNewUnicode(nsDependentString(aName)); }
+  nsXBLProtoImplMember(const PRUnichar* aName)
+    : mNext(nullptr)
+    , mExposeToUntrustedContent(false)
+  {
+    mName = ToNewUnicode(nsDependentString(aName));
+  }
   virtual ~nsXBLProtoImplMember() {
     nsMemory::Free(mName);
     NS_CONTENT_DELETE_LIST_MEMBER(nsXBLProtoImplMember, this, mNext);
@@ -69,6 +74,8 @@ public:
 
   nsXBLProtoImplMember* GetNext() { return mNext; }
   void SetNext(nsXBLProtoImplMember* aNext) { mNext = aNext; }
+  bool ShouldExposeToUntrustedContent() { return mExposeToUntrustedContent; }
+  void SetExposeToUntrustedContent(bool aExpose) { mExposeToUntrustedContent = aExpose; }
   const PRUnichar* GetName() { return mName; }
 
   virtual nsresult InstallMember(JSContext* aCx,
@@ -86,6 +93,11 @@ public:
 protected:
   nsXBLProtoImplMember* mNext;  // The members of an implementation are chained.
   PRUnichar* mName;               // The name of the field, method, or property.
+
+  bool mExposeToUntrustedContent; // If this binding is installed on an element
+                                  // in an untrusted scope, should this
+                                  // implementation member be accessible to the
+                                  // content?
 };
 
 #endif // nsXBLProtoImplMember_h__
