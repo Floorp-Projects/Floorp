07b35fd3293884b42fa337e00ab659a7a018b070
Doug Turner <doug.turner@gmail.com>
2013-10-31 22:29:58 -0700

    Backout of 0f687f920370

--- dom/media/MediaManager.h
+++ dom/media/MediaManager.h
@@ -405,7 +405,7 @@ public:
   NS_DECL_NSIOBSERVER
   NS_DECL_NSIMEDIAMANAGERSERVICE
 
-  MediaEngine* GetBackend(uint64_t aWindowId = 0, bool aFake = false);
+  MediaEngine* GetBackend(uint64_t aWindowId = 0);
   StreamListeners *GetWindowListeners(uint64_t aWindowId) {
     NS_ASSERTION(NS_IsMainThread(), "Only access windowlist on main thread");
 
@@ -469,7 +469,7 @@ private:
 
   static StaticRefPtr<MediaManager> sSingleton;
 
-#ifdef MOZ_B2G_CAMERA
+#ifdef MOZ_WIDGET_GONK
   nsRefPtr<nsDOMCameraManager> mCameraManager;
 #endif
 };
