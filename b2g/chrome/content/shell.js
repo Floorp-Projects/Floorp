62fd805ab4c4aa79f33fc74a4d5b7837a1b28cea
Fabrice Desré <fabrice@mozilla.com>
2013-10-31 23:35:58 -0700

    Bug 914843 - fix b2g DirectoryProvider on desktop r=gwagner

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -322,6 +322,9 @@ var shell = {
     ppmm.addMessageListener("mail-handler", this);
     ppmm.addMessageListener("app-notification-send", AlertsHelper);
     ppmm.addMessageListener("file-picker", this);
+    ppmm.addMessageListener("getProfD", function(message) {
+      return Services.dirsvc.get("ProfD", Ci.nsIFile).path;
+    });
   },
 
   stop: function shell_stop() {
