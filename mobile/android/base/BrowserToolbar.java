5b5bd0750b66797e67cadaf746fa1ea0204901e6
Sriram Ramasubramanian <sriram@mozilla.com>
2013-11-01 15:56:41 -0700

    Bug 934001 - ImageView's setImageBitmap() is worse. r=mfinkle, a=lsblakk

--- mobile/android/base/BrowserToolbar.java
+++ mobile/android/base/BrowserToolbar.java
@@ -1175,7 +1175,7 @@ public class BrowserToolbar extends GeckoRelativeLayout
             image = Bitmap.createScaledBitmap(image, mFaviconSize, mFaviconSize, false);
             mFavicon.setImageBitmap(image);
         } else {
-            mFavicon.setImageBitmap(null);
+            mFavicon.setImageDrawable(null);
         }
     }
 
