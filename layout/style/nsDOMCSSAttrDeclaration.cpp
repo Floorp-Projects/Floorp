2a55bc82ed15b4855c08deb32437f710f21a29de
Robert O'Callahan <robert@ocallahan.org>
2013-11-03 23:14:26 -0800

    Backout changeset 3fb9ea77d858 for bug 876321 due to test failures (CLOSED TREE)

--- layout/style/nsDOMCSSAttrDeclaration.cpp
+++ layout/style/nsDOMCSSAttrDeclaration.cpp
@@ -178,9 +178,7 @@ nsDOMCSSAttributeDeclaration::SetPropertyValue(const nsCSSProperty aPropID,
   // Scripted modifications to style.opacity or style.transform
   // could immediately force us into the animated state if heuristics suggest
   // this is scripted animation.
-  if (aPropID == eCSSProperty_opacity || aPropID == eCSSProperty_transform ||
-      aPropID == eCSSProperty_left || aPropID == eCSSProperty_top ||
-      aPropID == eCSSProperty_right || aPropID == eCSSProperty_bottom) {
+  if (aPropID == eCSSProperty_opacity || aPropID == eCSSProperty_transform) {
     nsIFrame* frame = mElement->GetPrimaryFrame();
     if (frame) {
       ActiveLayerTracker::NotifyInlineStyleRuleModified(frame, aPropID);
