1ea55247a7eb7422cc75aeefd86b0758bd765e05
Andrew Halberstadt <ahalberstadt@mozilla.com>
2013-11-04 09:53:42 -0500

    Bug 932782 - b2g desktop mochitest mach target should use the new manifest, r=jgriffin, DONTBUILD, a=NPOTB

--- testing/mochitest/mach_commands.py
+++ testing/mochitest/mach_commands.py
@@ -136,6 +136,8 @@ class MochitestRunner(MozbuildObject):
                 print('Specified test path does not exist: %s' % test_root_file)
                 return 1
             options.testPath = test_path
+        elif conditions.is_b2g_desktop:
+            options.testManifest = 'b2g-desktop.json'
         else:
             options.testManifest = 'b2g.json'
 
