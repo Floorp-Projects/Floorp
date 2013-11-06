0cf5297baa214bdefde2d8cc4d0eee622a111c8b
Eddy Bruel <ejpbruel@mozilla.com>
2013-11-06 20:04:12 +0100

    Bug 927116 - Implement parser support for import declarations; r=jorendorff

--- js/src/frontend/ParseNode.h
+++ js/src/frontend/ParseNode.h
@@ -128,6 +128,9 @@ class UpvarCookie
     F(ARRAYPUSH) \
     F(LEXICALSCOPE) \
     F(LET) \
+    F(IMPORT) \
+    F(IMPORT_SPEC_LIST) \
+    F(IMPORT_SPEC) \
     F(SEQ) \
     F(FORIN) \
     F(FOROF) \
