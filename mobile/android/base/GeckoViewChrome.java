f8ec0ce9abf31353d133ee95ee3b1cbf758c7d5e
Mark Finkle <mfinkle@mozilla.com>
2013-11-06 17:58:29 -0500

    Bug 880121 - Add support for Browser (Chrome) application interfaces to GeckoView r=blassey

---
+++ mobile/android/base/GeckoViewChrome.java
@@ -0,0 +1,14 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko;
+
+public class GeckoViewChrome implements GeckoView.ChromeDelegate {
+    /**
+    * Tell the host application that Gecko is ready to handle requests.
+    * @param view The GeckoView that initiated the callback.
+    */
+    public void onReady(GeckoView view) {}
+}
