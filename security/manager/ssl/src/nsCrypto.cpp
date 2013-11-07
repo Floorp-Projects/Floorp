625272dc94dc7faf22c9d897bb266869208476b7
David Keeler <dkeeler@mozilla.com>
2013-11-07 10:42:59 -0800

    bug 934716 - generateCRMFRequest: accept only the first repeated keygen parameter r=cviecco

--- security/manager/ssl/src/nsCrypto.cpp
+++ security/manager/ssl/src/nsCrypto.cpp
@@ -553,11 +553,14 @@ nsConvertToActualKeyGenParams(uint32_t keyGenMech, char *params,
               next_input, name, name_len, value, value_len,
               next_input))
       {
-        if (PL_strncmp(name, "curve", std::min(name_len, 5)) == 0)
+        // use only the first specified curve
+        if (!curve && PL_strncmp(name, "curve", std::min(name_len, 5)) == 0)
         {
           curve = PL_strndup(value, value_len);
         }
-        else if (PL_strncmp(name, "popcert", std::min(name_len, 7)) == 0)
+        // use only the first specified popcert
+        else if (!keyPairInfo->ecPopCert &&
+                 PL_strncmp(name, "popcert", std::min(name_len, 7)) == 0)
         {
           char *certstr = PL_strndup(value, value_len);
           if (certstr) {
