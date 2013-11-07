0f4f57672faae8b8cf44bb50172966043e46a726
Nick Fitzgerald <fitzgen@gmail.com>
2013-11-07 09:59:44 -0500

    Bug 930141 - Replace escodegen with pretty-fast in the debugger and scratchpad pretty printing backend. r=gps, r=dcamp, r=past, r=benvie, a=bajaj

--- toolkit/devtools/server/actors/pretty-print-worker.js
+++ toolkit/devtools/server/actors/pretty-print-worker.js
@@ -8,11 +8,11 @@
  * This file is meant to be loaded as a ChromeWorker. It accepts messages which
  * have data of the form:
  *
- *     { id, url, indent, ast }
+ *     { id, url, indent, source }
  *
  * Where `id` is a unique ID to identify this request, `url` is the url of the
  * source being pretty printed, `indent` is the number of spaces to indent the
- * code by, and `ast` is the source's abstract syntax tree.
+ * code by, and `source` is the source text.
  *
  * On success, the worker responds with a message of the form:
  *
@@ -20,25 +20,23 @@
  *
  * Where `id` is the same unique ID from the request, `code` is the pretty
  * printed source text, and `mappings` is an array or source mappings from the
- * pretty printed code to the AST's source locations.
+ * pretty printed code back to the ugly source text.
  *
  * In the case of an error, the worker responds with a message of the form:
  *
- *     { error }
+ *     { id, error }
  */
 
-importScripts("resource://gre/modules/devtools/escodegen/escodegen.worker.js");
+importScripts("resource://gre/modules/devtools/acorn.js");
+importScripts("resource://gre/modules/devtools/source-map.js");
+importScripts("resource://gre/modules/devtools/pretty-fast.js");
 
-self.onmessage = ({ data: { id, url, indent, ast } }) => {
+self.onmessage = (event) => {
+  const { data: { id, url, indent, source } } = event;
   try {
-    const prettified = escodegen.generate(ast, {
-      format: {
-        indent: {
-          style: " ".repeat(indent)
-        }
-      },
-      sourceMap: url,
-      sourceMapWithCode: true
+    const prettified = prettyFast(source, {
+      url: url,
+      indent: " ".repeat(indent)
     });
 
     self.postMessage({
@@ -48,6 +46,7 @@ self.onmessage = ({ data: { id, url, indent, ast } }) => {
     });
   } catch (e) {
     self.postMessage({
+      id: id,
       error: e.message + "\n" + e.stack
     });
   }
