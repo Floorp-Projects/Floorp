1deca9fe7cca53e1261a94e07c68f33623cccf85
Randy Lin <rlin@mozilla.com>
2013-11-10 06:30:01 +0800

    Bug 936352 - Media Recording - Fix wrong pointer usage in encoder module from Bug 919905. r=roc

--- content/media/encoder/EncodedFrameContainer.h
+++ content/media/encoder/EncodedFrameContainer.h
@@ -26,19 +26,20 @@ public:
     mEncodedFrames.AppendElement(aEncodedFrame);
   }
   // Retrieve all of the encoded frames
-  const nsTArray<nsAutoPtr<EncodedFrame> >& GetEncodedFrames() const
+  const nsTArray<nsRefPtr<EncodedFrame> >& GetEncodedFrames() const
   {
     return mEncodedFrames;
   }
 private:
   // This container is used to store the video or audio encoded packets.
   // Muxer should check mFrameType and get the encoded data type from mEncodedFrames.
-  nsTArray<nsAutoPtr<EncodedFrame> > mEncodedFrames;
+  nsTArray<nsRefPtr<EncodedFrame> > mEncodedFrames;
 };
 
 // Represent one encoded frame
 class EncodedFrame
 {
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(EncodedFrame)
 public:
   EncodedFrame() :
     mTimeStamp(0),
