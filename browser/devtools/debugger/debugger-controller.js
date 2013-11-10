78421adb62e97873c049629e9d77737b102369e1
Phil Ringnalda <philringnalda@gmail.com>
2013-11-09 23:26:47 -0800

    Back out 854623def9c6:a976c1645d70 (bug 917583) for Linux/Windows bc bustage CLOSED TREE

--- browser/devtools/debugger/debugger-controller.js
+++ browser/devtools/debugger/debugger-controller.js
@@ -1398,36 +1398,12 @@ EventListeners.prototype = {
    */
   scheduleEventListenersFetch: function() {
     let getListeners = aCallback => gThreadClient.eventListeners(aResponse => {
-      if (aResponse.error) {
-        let msg = "Error getting event listeners: " + aResponse.message;
-        DevToolsUtils.reportException("scheduleEventListenersFetch", msg);
-        return;
-      }
+      this._onEventListeners(aResponse);
 
-      promise.all(aResponse.listeners.map(listener => {
-        const deferred = promise.defer();
-
-        gThreadClient.pauseGrip(listener.function).getDefinitionSite(aResponse => {
-          if (aResponse.error) {
-            const msg = "Error getting function definition site: " + aResponse.message;
-            DevToolsUtils.reportException("scheduleEventListenersFetch", msg);
-            deferred.reject(msg);
-            return;
-          }
-
-          listener.function.url = aResponse.url;
-          deferred.resolve(listener);
-        });
-
-        return deferred.promise;
-      })).then(listeners => {
-        this._onEventListeners(listeners);
-
-        // Notify that event listeners were fetched and shown in the view,
-        // and callback to resume the active thread if necessary.
-        window.emit(EVENTS.EVENT_LISTENERS_FETCHED);
-        aCallback && aCallback();
-      });
+      // Notify that event listeners were fetched and shown in the view,
+      // and callback to resume the active thread if necessary.
+      window.emit(EVENTS.EVENT_LISTENERS_FETCHED);
+      aCallback && aCallback();
     });
 
     // Make sure we're not sending a batch of closely repeated requests.
@@ -1442,11 +1418,18 @@ EventListeners.prototype = {
   },
 
   /**
-   * Callback for a debugger's successful active thread eventListeners() call.
+   * Callback for the debugger's active thread eventListeners() method.
    */
-  _onEventListeners: function(aListeners) {
+  _onEventListeners: function(aResponse) {
+    if (aResponse.error) {
+      let msg = "Error getting event listeners: " + aResponse.message;
+      Cu.reportError(msg);
+      dumpn(msg);
+      return;
+    }
+
     // Add all the listeners in the debugger view event linsteners container.
-    for (let listener of aListeners) {
+    for (let listener of aResponse.listeners) {
       DebuggerView.EventListeners.addListener(listener, { staged: true });
     }
 
