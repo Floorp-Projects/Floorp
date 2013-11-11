58f043730c9806db28ef5b6a690f2e6d7fc7a420
Ms2ger <ms2ger@gmail.com>
2013-11-11 08:56:59 +0100

    Bug 933326 - Part b: Remove xpc_qsThrowMethodFailedWithCcx and xpc_qsThrowMethodFailedWithDetails; r=bholley

--- js/xpconnect/src/XPCQuickStubs.cpp
+++ js/xpconnect/src/XPCQuickStubs.cpp
@@ -298,21 +298,6 @@ xpc_qsThrowMethodFailed(JSContext *cx, nsresult rv, jsval *vp)
     return ThrowCallFailed(cx, rv, ifaceName, memberId, nullptr);
 }
 
-bool
-xpc_qsThrowMethodFailedWithCcx(XPCCallContext &ccx, nsresult rv)
-{
-    ThrowBadResult(rv, ccx);
-    return false;
-}
-
-bool
-xpc_qsThrowMethodFailedWithDetails(JSContext *cx, nsresult rv,
-                                   const char *ifaceName,
-                                   const char *memberName)
-{
-    return ThrowCallFailed(cx, rv, ifaceName, JSID_VOIDHANDLE, memberName);
-}
-
 static void
 ThrowBadArg(JSContext *cx, nsresult rv, const char *ifaceName,
             jsid memberId, const char *memberName, unsigned paramnum)
