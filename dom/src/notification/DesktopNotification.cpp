ccec8456187c295ba0624be8637b189c19175e70
Reuben Morais <reuben.morais@gmail.com>
2013-11-11 21:56:21 -0200

    Bug 904298 - Use CheckPermission to check for the desktop-notification permission. r=sicking

--- dom/src/notification/DesktopNotification.cpp
+++ dom/src/notification/DesktopNotification.cpp
@@ -119,13 +119,15 @@ DesktopNotification::PostDesktopNotification()
   // to nsIObservers, thus cookies must be unique to differentiate observers.
   nsString uniqueName = NS_LITERAL_STRING("desktop-notification:");
   uniqueName.AppendInt(sCount++);
+  nsIPrincipal* principal = GetOwner()->GetDoc()->NodePrincipal();
   return alerts->ShowAlertNotification(mIconURL, mTitle, mDescription,
                                        true,
                                        uniqueName,
                                        mObserver,
                                        uniqueName,
                                        NS_LITERAL_STRING("auto"),
-                                       EmptyString());
+                                       EmptyString(),
+                                       principal);
 }
 
 DesktopNotification::DesktopNotification(const nsAString & title,
