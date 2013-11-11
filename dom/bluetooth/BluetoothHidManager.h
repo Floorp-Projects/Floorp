f7412e11d8f9883273737c0d4b31b0449d4d4be8
Gregor Wagner <anygregor@gmail.com>
2013-11-11 10:28:56 -0800

    Backout 26407a874057 for causing Bug 937199.

--- dom/bluetooth/BluetoothHidManager.h
+++ dom/bluetooth/BluetoothHidManager.h
@@ -18,14 +18,28 @@ class BluetoothHidManager : public BluetoothProfileManagerBase
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIOBSERVER
-  BT_DECL_PROFILE_MGR_BASE
+
+  static BluetoothHidManager* Get();
+  ~BluetoothHidManager();
+
+  // The following functions are inherited from BluetoothProfileManagerBase
+  virtual void OnGetServiceChannel(const nsAString& aDeviceAddress,
+                                   const nsAString& aServiceUuid,
+                                   int aChannel) MOZ_OVERRIDE;
+  virtual void OnUpdateSdpRecords(const nsAString& aDeviceAddress) MOZ_OVERRIDE;
+  virtual void GetAddress(nsAString& aDeviceAddress) MOZ_OVERRIDE;
+  virtual bool IsConnected() MOZ_OVERRIDE;
+  virtual void Connect(const nsAString& aDeviceAddress,
+                       BluetoothProfileController* aController) MOZ_OVERRIDE;
+  virtual void Disconnect(BluetoothProfileController* aController)
+                          MOZ_OVERRIDE;
+  virtual void OnConnect(const nsAString& aErrorStr) MOZ_OVERRIDE;
+  virtual void OnDisconnect(const nsAString& aErrorStr) MOZ_OVERRIDE;
+
   virtual void GetName(nsACString& aName)
   {
     aName.AssignLiteral("HID");
   }
-
-  static BluetoothHidManager* Get();
-  ~BluetoothHidManager();
 
   // HID-specific functions
   void HandleInputPropertyChanged(const BluetoothSignal& aSignal);
