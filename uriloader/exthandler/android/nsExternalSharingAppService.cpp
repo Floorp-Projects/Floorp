f7dde798037d425a2825b693bfcafc870f7c066e
Chris Kitching <chriskitching@linux.com>
2013-11-12 10:41:01 -0800

    Bug 913985: Part 6 - Remove redundant AndroidBridge::Bridge() checks. r=kats

--- uriloader/exthandler/android/nsExternalSharingAppService.cpp
+++ uriloader/exthandler/android/nsExternalSharingAppService.cpp
@@ -31,11 +31,7 @@ nsExternalSharingAppService::ShareWithDefault(const nsAString & data,
 {
   NS_NAMED_LITERAL_STRING(sendAction, "android.intent.action.SEND");
   const nsString emptyString = EmptyString();
-  if (AndroidBridge::Bridge())
-    return GeckoAppShell::OpenUriExternal(data, mime,
-                                          emptyString,emptyString, sendAction, title) ? NS_OK : NS_ERROR_FAILURE;
-
-  return NS_ERROR_FAILURE;
+  return GeckoAppShell::OpenUriExternal(data, mime, emptyString,emptyString, sendAction, title) ? NS_OK : NS_ERROR_FAILURE;
 }
 
 NS_IMETHODIMP
