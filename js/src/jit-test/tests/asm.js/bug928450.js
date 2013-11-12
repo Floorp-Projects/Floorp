fce11902b66d6c804ae7c65a31d3aaf757518bee
Dan Gohman <sunfish@google.com>
2013-11-12 05:46:43 -0800

    Bug 933104 - IonMonkey: Check for empty ranges after intersecting fractional ranges. r=nbp

--- js/src/jit-test/tests/asm.js/bug928450.js
+++ js/src/jit-test/tests/asm.js/bug928450.js
@@ -1,6 +1,18 @@
 (function() {
-    "use asm"
+    "use asm";
     function f() {
-        i((1.5 != 2.) ? 3 : 0)
+        i((1.5 != 2.) ? 3 : 0);
     }
+    return f;
+})();
+
+// Bug 933104
+(function() {
+    "use asm";
+    function f(x) {
+        x = +x;
+        x = -2.;
+        (x > -1.5) ? 0 : 0;
+    }
+    return f;
 })()
