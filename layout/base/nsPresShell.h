05249e274ff5f7ee228fc2882fb688530bc5e090
Olli Pettay <bugs@pettay.fi>
2013-11-12 07:54:10 -0500

    Bug 930381 - Follow the COM rules when calling ProcessSynthMouseMoveEvent. r=mats, smaug, tnikkel, a=abillings

--- layout/base/nsPresShell.h
+++ layout/base/nsPresShell.h
@@ -624,8 +624,10 @@ protected:
       }
     }
     virtual void WillRefresh(mozilla::TimeStamp aTime) MOZ_OVERRIDE {
-      if (mPresShell)
-        mPresShell->ProcessSynthMouseMoveEvent(mFromScroll);
+      if (mPresShell) {
+        nsRefPtr<PresShell> shell = mPresShell;
+        shell->ProcessSynthMouseMoveEvent(mFromScroll);
+      }
     }
   private:
     PresShell* mPresShell;
