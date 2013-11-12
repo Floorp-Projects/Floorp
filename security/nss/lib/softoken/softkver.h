0f3ae8c0f9642d32dee3b6f12703960ccaf6eb89
Kai Engert <kaie@kuix.de>
2013-11-12 13:00:31 -0500

    Bug 935959, Update Mozilla to NSS 3.15.3 (new alternative NSS branch), r=briansmith, a=akeybl

--- security/nss/lib/softoken/softkver.h
+++ security/nss/lib/softoken/softkver.h
@@ -25,10 +25,10 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define SOFTOKEN_VERSION  "3.15.2" SOFTOKEN_ECC_STRING
+#define SOFTOKEN_VERSION  "3.15.3" SOFTOKEN_ECC_STRING
 #define SOFTOKEN_VMAJOR   3
 #define SOFTOKEN_VMINOR   15
-#define SOFTOKEN_VPATCH   2
+#define SOFTOKEN_VPATCH   3
 #define SOFTOKEN_VBUILD   0
 #define SOFTOKEN_BETA     PR_FALSE
 
