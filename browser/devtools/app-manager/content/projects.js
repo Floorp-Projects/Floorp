bbe087b559159cdf892f9c87de785147668fce80
J. Ryan Stinnett <jryans@gmail.com>
2013-11-13 13:34:29 -0600

    Bug 937607 - Add explicit save button for the manifest editor. r=paul

--- browser/devtools/app-manager/content/projects.js
+++ browser/devtools/app-manager/content/projects.js
@@ -184,10 +184,13 @@ let UI = {
   update: function(button, location) {
     button.disabled = true;
     let project = AppProjects.get(location);
-    this.manifestEditor.save()
-        .then(() => {
-          return this.validate(project);
-        })
+
+    // Update the manifest editor view, in case the manifest was modified
+    // outside of the app manager.  This can happen in parallel with the other
+    // steps.
+    this._showManifestEditor(project);
+
+    this.validate(project)
         .then(() => {
            // Install the app to the device if we are connected,
            // and there is no error
@@ -208,6 +211,11 @@ let UI = {
           alert(message);
           this.connection.log(message);
         });
+  },
+
+  saveManifest: function(button) {
+    button.disabled = true;
+    this.manifestEditor.save().then(() => button.disabled = false);
   },
 
   reload: function (project) {
@@ -428,6 +436,9 @@ let UI = {
   },
 
   _showManifestEditor: function(project) {
+    if (this.manifestEditor) {
+      this.manifestEditor.destroy();
+    }
     let editorContainer = document.querySelector("#lense .manifest-editor");
     this.manifestEditor = new ManifestEditor(project);
     return this.manifestEditor.show(editorContainer);
