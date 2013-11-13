02cc7b7f310611f5eccdfff387ed2518598b970b
Nick Alexander <nalexander@mozilla.com>
2013-11-13 15:27:13 -0800

    Bug 892025 - Implement Android client for FxAccount auth server. r=rnewman

--- mobile/android/base/sync/net/BaseResource.java
+++ mobile/android/base/sync/net/BaseResource.java
@@ -444,4 +444,8 @@ public class BaseResource implements Resource {
   public void post(ExtendedJSONObject o) throws UnsupportedEncodingException {
     post(jsonEntity(o));
   }
+
+  public void post(JSONObject jsonObject) throws UnsupportedEncodingException {
+    post(jsonEntity(jsonObject));
+  }
 }
