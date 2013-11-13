2c5aaa295148b89a1e7456e024c8308bd0883e17
Shao Hang Kao <skao@mozilla.com>
2013-11-13 17:57:45 +0800

    Bug 935873 - MMS retry should handle modified input stream, Part 2 - test case, r=vyang

--- dom/mobilemessage/tests/header_helpers.js
+++ dom/mobilemessage/tests/header_helpers.js
@@ -53,9 +53,7 @@ function do_check_throws(func, result, stack)
 function wsp_test_func(func, data, expect) {
   let result_str = JSON.stringify(func(data));
   let expect_str = JSON.stringify(expect);
-  if (result_str !== expect_str) {
-    do_throw("expect value: '" + expect_str + "', got '" + result_str + "'");
-  }
+  do_check_eq(result_str, expect_str);
 }
 
 /**
