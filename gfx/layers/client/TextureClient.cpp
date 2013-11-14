69478b80a7b0c1ac419e0ce9852ff00c4a37c52a
Nicholas Cameron <ncameron@mozilla.com>
2013-11-15 10:54:25 +1300

    Bug 915940. Memory reporter for memory image surfaces.  r=njn,mattwoodrow This might even help diagnose the problems causing a CLOSED TREE.

--- gfx/layers/client/TextureClient.cpp
+++ gfx/layers/client/TextureClient.cpp
@@ -214,6 +214,7 @@ MemoryTextureClient::Allocate(uint32_t aSize)
 {
   MOZ_ASSERT(!mBuffer);
   mBuffer = new uint8_t[aSize];
+  GfxMemoryImageReporter::DidAlloc(mBuffer);
   mBufSize = aSize;
   return true;
 }
@@ -231,9 +232,10 @@ MemoryTextureClient::MemoryTextureClient(CompositableClient* aCompositable,
 MemoryTextureClient::~MemoryTextureClient()
 {
   MOZ_COUNT_DTOR(MemoryTextureClient);
-  if (ShouldDeallocateInDestructor()) {
+  if (ShouldDeallocateInDestructor() && mBuffer) {
     // if the buffer has never been shared we must deallocate it or ir would
     // leak.
+    GfxMemoryImageReporter::WillFree(mBuffer);
     delete mBuffer;
   }
 }
