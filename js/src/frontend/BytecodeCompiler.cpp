89e154a1ed0814b082173af6a0c94852336759dd
Carsten "Tomcat" Book <cbook@mozilla.com>
2013-11-14 10:00:48 +0100

    Backed out changeset 9f3212effb9f (bug 934799) as result of the discussion of Bug 937997 Comment 48 Trees Closed due to OOM on a CLOSED TREE

--- js/src/frontend/BytecodeCompiler.cpp
+++ js/src/frontend/BytecodeCompiler.cpp
@@ -141,9 +141,7 @@ CanLazilyParse(ExclusiveContext *cx, const ReadOnlyCompileOptions &options)
 {
     return options.canLazilyParse &&
         options.compileAndGo &&
-        options.sourcePolicy == CompileOptions::SAVE_SOURCE &&
-        !(cx->compartment()->debugMode() &&
-          cx->compartment()->runtimeFromAnyThread()->debugHooks.newScriptHook);
+        options.sourcePolicy == CompileOptions::SAVE_SOURCE;
 }
 
 void
