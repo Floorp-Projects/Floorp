cf74123e6ab83c18914bb0146e1ba707e39c9489
Benoit Jacob <bjacob@mozilla.com>
2013-11-15 11:28:59 -0500

    Bug 925608 - 7/8: rename ProvideEGLSurface to CreateEGLSurfaceForCompositor - r=kats

--- widget/android/AndroidBridge.h
+++ widget/android/AndroidBridge.h
@@ -228,7 +228,7 @@ public:
 
     // Switch Java to composite with the Gecko Compositor thread
     void RegisterCompositor(JNIEnv* env = nullptr);
-    EGLSurface ProvideEGLSurface();
+    EGLSurface CreateEGLSurfaceForCompositor();
 
     bool GetStaticStringField(const char *classID, const char *field, nsAString &result, JNIEnv* env = nullptr);
 
