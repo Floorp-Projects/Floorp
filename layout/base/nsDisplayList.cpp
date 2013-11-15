783d56d17f231c694942cf9791e4b3829ad160bf
Chris Lord <chrislord.net@gmail.com>
2013-11-15 17:06:29 +0000

    Bug 933556 - Anchor fixed, left+right/top+bottom positioned content to center. r=roc
    If a fixed position element has both top and bottom, or both left and right
    positioning, set the fixed position anchor to the center of the corresponding
    axis. This causes it to remain center-aligned when async zooming.

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -3387,14 +3387,25 @@ void nsDisplayFixedPosition::SetFixedPositionLayerData(Layer* const aLayer,
   // Work out the anchor point for this fixed position layer. We assume that
   // any positioning set (left/top/right/bottom) indicates that the
   // corresponding side of its container should be the anchor point,
-  // defaulting to top-left.
+  // defaulting to top-left. If both sides of an axis are specified, we align
+  // to the center.
   LayerPoint anchor = anchorRect.TopLeft();
 
   const nsStylePosition* position = mFixedPosFrame->StylePosition();
-  if (position->mOffset.GetRightUnit() != eStyleUnit_Auto)
-    anchor.x = anchorRect.XMost();
-  if (position->mOffset.GetBottomUnit() != eStyleUnit_Auto)
-    anchor.y = anchorRect.YMost();
+  if (position->mOffset.GetRightUnit() != eStyleUnit_Auto) {
+    if (position->mOffset.GetLeftUnit() != eStyleUnit_Auto) {
+      anchor.x = anchorRect.x + anchorRect.width / 2.f;
+    } else {
+      anchor.x = anchorRect.XMost();
+    }
+  }
+  if (position->mOffset.GetBottomUnit() != eStyleUnit_Auto) {
+    if (position->mOffset.GetTopUnit() != eStyleUnit_Auto) {
+      anchor.y = anchorRect.y + anchorRect.height / 2.f;
+    } else {
+      anchor.y = anchorRect.YMost();
+    }
+  }
 
   aLayer->SetFixedPositionAnchor(anchor);
 
