1e6ecbf0e0557503a0006fc2df93eac649a0692b
Ryan VanderMeulen <ryanvm@gmail.com>
2013-11-15 08:32:23 -0500

    Backed out changeset f3c4d03ad677 (bug 922924) for bustage.

--- netwerk/protocol/http/nsHttpTransaction.h
+++ netwerk/protocol/http/nsHttpTransaction.h
@@ -340,6 +340,10 @@ private:
 
 // These members are used for network per-app metering (bug 746073)
 // Currently, they are only available on gonk.
+public:
+    const static uint64_t NETWORK_STATS_THRESHOLD = 65536;
+
+private:
     uint64_t                           mCountRecv;
     uint64_t                           mCountSent;
     uint32_t                           mAppId;
@@ -347,6 +351,7 @@ private:
     nsMainThreadPtrHandle<nsINetworkInterface> mActiveNetwork;
 #endif
     nsresult                           SaveNetworkStats(bool);
+    void                               GetActiveNetwork();
     void                               CountRecvBytes(uint64_t recvBytes)
     {
         mCountRecv += recvBytes;
