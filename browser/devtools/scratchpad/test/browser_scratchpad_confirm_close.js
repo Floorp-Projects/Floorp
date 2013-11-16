a18e0bff959012a6b4000f28a22f00fddb5d919f
Peiyong Lin <pylaurent1314@gmail.com>
2013-11-15 21:47:02 -0500

    Bug 937953 - Remove bug numbers from all Scratchpad test file names and update browser.ini. r=benvie

--- browser/devtools/scratchpad/test/browser_scratchpad_confirm_close.js
+++ browser/devtools/scratchpad/test/browser_scratchpad_confirm_close.js
@@ -1,6 +1,7 @@
 /* vim: set ts=2 et sw=2 tw=80: */
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
+/* Bug 653427 */
 
 let tempScope = {};
 Cu.import("resource://gre/modules/NetUtil.jsm", tempScope);
