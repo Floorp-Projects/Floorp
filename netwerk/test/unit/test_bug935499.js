173af748059861c078240ebf258565fca4a755ee
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2013-11-16 18:30:12 +0900

    Bug 935499. r=honzab, a=lsblakk

---
+++ netwerk/test/unit/test_bug935499.js
@@ -0,0 +1,10 @@
+var Ci = Components.interfaces;
+var Cc = Components.classes;
+
+function run_test() {
+  var idnService = Cc["@mozilla.org/network/idn-service;1"]
+                   .getService(Ci.nsIIDNService);
+
+  var isASCII = {};
+  do_check_eq(idnService.convertToDisplayIDN("xn--", isASCII), "xn--");
+}
