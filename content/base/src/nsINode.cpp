6135d5a2866e7c458c4f16e4210985d3dd3df5aa
Boris Zbarsky <bzbarsky@mit.edu>
2013-11-17 00:10:18 -0500

    Bug 937465.  Warn when cloneNode() and importNode() are used without the boolean "deep" argument on nodes that have kids.  r=sicking

--- content/base/src/nsINode.cpp
+++ content/base/src/nsINode.cpp
@@ -2558,6 +2558,17 @@ nsINode::CloneNode(bool aDeep, ErrorResult& aError)
   return result.forget();
 }
 
+already_AddRefed<nsINode>
+nsINode::CloneNode(mozilla::ErrorResult& aError)
+{
+  if (HasChildNodes()) {
+    // Flag it as an error, not a warning, to make people actually notice.
+    OwnerDoc()->WarnOnceAbout(nsIDocument::eUnsafeCloneNode, true);
+  }
+  return CloneNode(true, aError);
+}
+
+
 nsDOMAttributeMap*
 nsINode::GetAttributes()
 {
