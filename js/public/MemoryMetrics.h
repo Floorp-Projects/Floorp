cb3eece0fc045a06938d7eefbfcea9e1a3273264
Jim Blandy <jimb@mozilla.com>
2013-11-18 13:48:45 -0800

    Bug 937404 - Remove the unused NotableStringInfo copy constructor.  r=jimb
    --HG--
    extra : rebase_source : 9af09cb34a2bda3b1cbee61c85617a25c5876106

--- js/public/MemoryMetrics.h
+++ js/public/MemoryMetrics.h
@@ -272,7 +272,6 @@ struct NotableStringInfo : public StringInfo
 {
     NotableStringInfo();
     NotableStringInfo(JSString *str, const StringInfo &info);
-    NotableStringInfo(const NotableStringInfo& info);
     NotableStringInfo(NotableStringInfo &&info);
     NotableStringInfo &operator=(NotableStringInfo &&info);
 
@@ -286,10 +285,10 @@ struct NotableStringInfo : public StringInfo
         return js::MemoryReportingSundriesThreshold();
     }
 
-    // The amount of memory we requested for |buffer|; i.e.
-    // buffer = malloc(bufferSize).
-    size_t bufferSize;
     char *buffer;
+
+  private:
+    NotableStringInfo(const NotableStringInfo& info) MOZ_DELETE;
 };
 
 // These measurements relate directly to the JSRuntime, and not to zones and
