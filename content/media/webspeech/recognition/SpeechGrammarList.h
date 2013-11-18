df00a8d0fac50df15999e0c2bf5d8f9d56c3213d
Ehsan Akhgari <ehsan@mozilla.com>
2013-11-17 19:48:02 -0500

    Bug 939569 - Part 1: Stop using #pragma once in webspeech code; r=smaug

--- content/media/webspeech/recognition/SpeechGrammarList.h
+++ content/media/webspeech/recognition/SpeechGrammarList.h
@@ -4,7 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#pragma once
+#ifndef mozilla_dom_SpeechGrammarList_h
+#define mozilla_dom_SpeechGrammarList_h
 
 #include "EnableWebSpeechRecognitionCheck.h"
 #include "mozilla/Attributes.h"
@@ -59,3 +60,5 @@ private:
 
 } // namespace dom
 } // namespace mozilla
+
+#endif
