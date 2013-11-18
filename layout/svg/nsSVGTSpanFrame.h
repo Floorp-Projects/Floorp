a2e2acfb91c4b40bf80927d0c3c47ed795a000a3
Jonathan Watt <jwatt@jwatt.org>
2013-11-18 14:29:51 +0000

    Bug 889736, part 1 - Remove nsSVGTextFrame and nsSVGGlyphFrame. r=heycam

--- layout/svg/nsSVGTSpanFrame.h
+++ layout/svg/nsSVGTSpanFrame.h
@@ -19,7 +19,6 @@ class nsIContent;
 class nsIFrame;
 class nsIPresShell;
 class nsStyleContext;
-class nsSVGGlyphFrame;
 
 namespace mozilla {
 class nsISVGPoint;
@@ -73,8 +72,6 @@ public:
   virtual float GetComputedTextLength() MOZ_OVERRIDE;
   virtual float GetSubStringLength(uint32_t charnum, uint32_t fragmentChars) MOZ_OVERRIDE;
   virtual int32_t GetCharNumAtPosition(mozilla::nsISVGPoint *point) MOZ_OVERRIDE;
-  NS_IMETHOD_(nsSVGGlyphFrame *) GetFirstGlyphFrame() MOZ_OVERRIDE;
-  NS_IMETHOD_(nsSVGGlyphFrame *) GetNextGlyphFrame() MOZ_OVERRIDE;
   NS_IMETHOD_(void) SetWhitespaceCompression(bool aCompressWhitespace) MOZ_OVERRIDE;
 };
 
