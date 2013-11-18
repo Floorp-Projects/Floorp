9ccb3e1d28491074fed16f5d5103acb57d174527
Phil Ringnalda <philringnalda@gmail.com>
2013-11-18 07:21:11 -0800

    Back out 134f2dc02468 (bug 939659) for assertions

--- extensions/spellcheck/src/mozInlineSpellWordUtil.h
+++ extensions/spellcheck/src/mozInlineSpellWordUtil.h
@@ -3,9 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozInlineSpellWordUtil_h
-#define mozInlineSpellWordUtil_h
-
 #include "nsCOMPtr.h"
 #include "nsIDOMDocument.h"
 #include "nsIDocument.h"
@@ -162,5 +159,3 @@ private:
   nsresult MakeRange(NodeOffset aBegin, NodeOffset aEnd, nsRange** aRange);
   nsresult MakeRangeForWord(const RealWord& aWord, nsRange** aRange);
 };
-
-#endif
