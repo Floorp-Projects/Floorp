37e5ffc058934c9a2d74881795ec092b89cb7439
Benoit Jacob <bjacob@mozilla.com>
2013-11-17 21:04:38 -0500

    Bug 938970 - 1/5. Move MacIOSurfaceTexture* classes to separate files, minimize the number of cpp files including MacIOSurface.h - r=mattwoodrow

---
+++ gfx/layers/opengl/MacIOSurfaceTextureClientOGL.h
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef MOZILLA_GFX_MACIOSURFACETEXTURECLIENTOGL_H
+#define MOZILLA_GFX_MACIOSURFACETEXTURECLIENTOGL_H
+
+#include "mozilla/layers/TextureClientOGL.h"
+
+class MacIOSurface;
+
+namespace mozilla {
+namespace layers {
+
+class MacIOSurfaceTextureClientOGL : public TextureClient
+{
+public:
+  MacIOSurfaceTextureClientOGL(TextureFlags aFlags);
+
+  virtual ~MacIOSurfaceTextureClientOGL();
+
+  void InitWith(MacIOSurface* aSurface);
+
+  virtual bool IsAllocated() const MOZ_OVERRIDE { return !!mSurface; }
+
+  virtual bool ToSurfaceDescriptor(SurfaceDescriptor& aOutDescriptor) MOZ_OVERRIDE;
+
+  virtual gfx::IntSize GetSize() const;
+
+  virtual TextureClientData* DropTextureData() MOZ_OVERRIDE;
+
+protected:
+  RefPtr<MacIOSurface> mSurface;
+};
+
+}
+}
+
+#endif // MOZILLA_GFX_MACIOSURFACETEXTURECLIENTOGL_H
