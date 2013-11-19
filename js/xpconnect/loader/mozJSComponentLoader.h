b0fd94582c45f7903b2ab89c85ceaf05b5687188
Carsten "Tomcat" Book <cbook@mozilla.com>
2013-11-19 14:13:15 +0100

    Backed out changeset 161bfee3f57a (bug 939608) PGO build issue

--- js/xpconnect/loader/mozJSComponentLoader.h
+++ js/xpconnect/loader/mozJSComponentLoader.h
@@ -4,9 +4,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozJSComponentLoader_h
-#define mozJSComponentLoader_h
-
 #include "mozilla/ModuleLoader.h"
 #include "nsISupports.h"
 #include "nsIObserver.h"
@@ -146,5 +143,3 @@ class mozJSComponentLoader : public mozilla::ModuleLoader,
     bool mInitialized;
     bool mReuseLoaderGlobal;
 };
-
-#endif /* mozJSComponentLoader_h */
