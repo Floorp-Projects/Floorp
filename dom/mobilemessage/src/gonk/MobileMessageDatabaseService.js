e82a80115cf3774272c53c9de2460b8a8d5722db
Jan Varga <jan.varga@gmail.com>
2013-11-19 08:36:12 +0100

    Bug 921478 - Remove BackstagePass IDB constructor resolve hook and use Cu.importGlobalProperties. r=bholley,bent

--- dom/mobilemessage/src/gonk/MobileMessageDatabaseService.js
+++ dom/mobilemessage/src/gonk/MobileMessageDatabaseService.js
@@ -9,6 +9,7 @@ const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/PhoneNumberUtils.jsm");
+Cu.importGlobalProperties(["indexedDB"]);
 
 const RIL_MOBILEMESSAGEDATABASESERVICE_CONTRACTID =
   "@mozilla.org/mobilemessage/rilmobilemessagedatabaseservice;1";
