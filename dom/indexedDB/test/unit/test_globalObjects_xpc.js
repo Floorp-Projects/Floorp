584cd96d9f6eaaa6268074e9e03015f0af661488
Jan Varga <jan.varga@gmail.com>
2013-11-19 08:36:12 +0100

    Bug 921478 - Remove BackstagePass IDB constructor resolve hook and use Cu.importGlobalProperties. r=bholley,bent

---
+++ dom/indexedDB/test/unit/test_globalObjects_xpc.js
@@ -0,0 +1,26 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+var testGenerator = testSteps();
+
+function testSteps()
+{
+  const name = "Splendid Test";
+
+  // Test for IDBKeyRange and indexedDB availability in xpcshell.
+  let keyRange = IDBKeyRange.only(42);
+  ok(keyRange, "Got keyRange");
+
+  let request = indexedDB.open(name, 1);
+  request.onerror = errorHandler;
+  request.onsuccess = grabEventAndContinueHandler;
+  let event = yield undefined;
+
+  let db = event.target.result;
+  ok(db, "Got database");
+
+  finishTest();
+  yield undefined;
+}
