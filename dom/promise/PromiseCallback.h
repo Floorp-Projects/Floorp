1294bcf695248e0f6e3e9db4868c70b3bb9718fb
Nikhil Marathe <nsm.nikhil@gmail.com>
2013-11-19 10:39:51 -0800

    Bug 939909 - Get rid of Optional<> from internal Promise functions. r=bz

--- dom/promise/PromiseCallback.h
+++ dom/promise/PromiseCallback.h
@@ -24,7 +24,7 @@ public:
   PromiseCallback();
   virtual ~PromiseCallback();
 
-  virtual void Call(const Optional<JS::Handle<JS::Value> >& aValue) = 0;
+  virtual void Call(JS::Handle<JS::Value> aValue) = 0;
 
   enum Task {
     Resolve,
@@ -46,7 +46,7 @@ public:
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(WrapperPromiseCallback,
                                            PromiseCallback)
 
-  void Call(const Optional<JS::Handle<JS::Value> >& aValue) MOZ_OVERRIDE;
+  void Call(JS::Handle<JS::Value> aValue) MOZ_OVERRIDE;
 
   WrapperPromiseCallback(Promise* aNextPromise, AnyCallback* aCallback);
   ~WrapperPromiseCallback();
@@ -64,7 +64,7 @@ public:
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(SimpleWrapperPromiseCallback,
                                            PromiseCallback)
 
-  void Call(const Optional<JS::Handle<JS::Value> >& aValue) MOZ_OVERRIDE;
+  void Call(JS::Handle<JS::Value> aValue) MOZ_OVERRIDE;
 
   SimpleWrapperPromiseCallback(Promise* aPromise,
                                AnyCallback* aCallback);
@@ -84,7 +84,7 @@ public:
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(ResolvePromiseCallback,
                                            PromiseCallback)
 
-  void Call(const Optional<JS::Handle<JS::Value> >& aValue) MOZ_OVERRIDE;
+  void Call(JS::Handle<JS::Value> aValue) MOZ_OVERRIDE;
 
   ResolvePromiseCallback(Promise* aPromise);
   ~ResolvePromiseCallback();
@@ -102,7 +102,7 @@ public:
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(RejectPromiseCallback,
                                            PromiseCallback)
 
-  void Call(const Optional<JS::Handle<JS::Value> >& aValue) MOZ_OVERRIDE;
+  void Call(JS::Handle<JS::Value> aValue) MOZ_OVERRIDE;
 
   RejectPromiseCallback(Promise* aPromise);
   ~RejectPromiseCallback();
