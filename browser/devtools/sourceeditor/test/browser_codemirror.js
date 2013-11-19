c4c08ac4d57b169336a5fd5cb7a8b76e650e561f
Anton Kovalyov <anton@mozilla.com>
2013-11-19 10:54:19 -0800

    Bug 920570 - requestLongerTimeout for browser_codemirror and re-enable it on Linux. a=test-only

--- browser/devtools/sourceeditor/test/browser_codemirror.js
+++ browser/devtools/sourceeditor/test/browser_codemirror.js
@@ -8,6 +8,7 @@ const HOST = 'mochi.test:8888';
 const URI  = "http://" + HOST + "/browser/browser/devtools/sourceeditor/test/codemirror.html";
 
 function test() {
+  requestLongerTimeout(2);
   waitForExplicitFinish();
 
   let tab = gBrowser.addTab();
