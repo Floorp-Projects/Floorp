2e90b7626235665b54d503ccae4c89a55d9a280c
Alexander Surkov <surkov.alexander@gmail.com>
2013-11-19 16:01:15 -0500

    Bug 935698 - isolate nsIAccessibleValue implementation into separate class, r=tbsaunde

--- accessible/src/windows/ia2/ia2AccessibleValue.cpp
+++ accessible/src/windows/ia2/ia2AccessibleValue.cpp
@@ -13,6 +13,8 @@
 #include "Accessible-inl.h"
 #include "IUnknownImpl.h"
 
+#include "mozilla/FloatingPoint.h"
+
 using namespace mozilla::a11y;
 
 // IUnknown
@@ -55,10 +57,9 @@ ia2AccessibleValue::get_currentValue(VARIANT* aCurrentValue)
   if (valueAcc->IsDefunct())
     return CO_E_OBJNOTCONNECTED;
 
-  double currentValue = 0;
-  nsresult rv = valueAcc->GetCurrentValue(&currentValue);
-  if (NS_FAILED(rv))
-    return GetHRESULT(rv);
+  double currentValue = valueAcc->CurValue();
+  if (IsNaN(currentValue))
+    return S_FALSE;
 
   aCurrentValue->vt = VT_R8;
   aCurrentValue->dblVal = currentValue;
@@ -79,8 +80,7 @@ ia2AccessibleValue::setCurrentValue(VARIANT aValue)
   if (aValue.vt != VT_R8)
     return E_INVALIDARG;
 
-  nsresult rv = valueAcc->SetCurrentValue(aValue.dblVal);
-  return GetHRESULT(rv);
+  return valueAcc->SetCurValue(aValue.dblVal) ? S_OK : E_FAIL;
 
   A11Y_TRYBLOCK_END
 }
@@ -99,10 +99,9 @@ ia2AccessibleValue::get_maximumValue(VARIANT* aMaximumValue)
   if (valueAcc->IsDefunct())
     return CO_E_OBJNOTCONNECTED;
 
-  double maximumValue = 0;
-  nsresult rv = valueAcc->GetMaximumValue(&maximumValue);
-  if (NS_FAILED(rv))
-    return GetHRESULT(rv);
+  double maximumValue = valueAcc->MaxValue();
+  if (IsNaN(maximumValue))
+    return S_FALSE;
 
   aMaximumValue->vt = VT_R8;
   aMaximumValue->dblVal = maximumValue;
@@ -125,10 +124,9 @@ ia2AccessibleValue::get_minimumValue(VARIANT* aMinimumValue)
   if (valueAcc->IsDefunct())
     return CO_E_OBJNOTCONNECTED;
 
-  double minimumValue = 0;
-  nsresult rv = valueAcc->GetMinimumValue(&minimumValue);
-  if (NS_FAILED(rv))
-    return GetHRESULT(rv);
+  double minimumValue = valueAcc->MinValue();
+  if (IsNaN(minimumValue))
+    return S_FALSE;
 
   aMinimumValue->vt = VT_R8;
   aMinimumValue->dblVal = minimumValue;
