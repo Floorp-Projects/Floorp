74ca982be5d1d5596bba4eb675499f3d99f58f89
Benjamin Smedberg <benjamin@smedbergs.us>
2013-11-19 08:17:03 -0500

    Bug 932854 - Add telemetry for showing the plugin infobar and its two buttons. r=jaws, a=lsblakk

--- browser/base/content/browser-plugins.js
+++ browser/base/content/browser-plugins.js
@@ -960,6 +960,9 @@ var gPluginHandler = {
         return;
       }
 
+      Services.telemetry.getHistogramById("PLUGINS_INFOBAR_SHOWN").
+        add(true);
+
       let message;
       // Icons set directly cannot be manipulated using moz-image-region, so
       // we use CSS classes instead.
@@ -1004,6 +1007,9 @@ var gPluginHandler = {
           label: gNavigatorBundle.getString("pluginBlockNow.label"),
           accessKey: gNavigatorBundle.getString("pluginBlockNow.accesskey"),
           callback: function() {
+            Services.telemetry.getHistogramById("PLUGINS_INFOBAR_BLOCK").
+              add(true);
+
             Services.perms.addFromPrincipal(aBrowser.contentDocument.nodePrincipal,
                                             "plugin-hidden-notification",
                                             Services.perms.DENY_ACTION);
@@ -1013,6 +1019,9 @@ var gPluginHandler = {
           label: gNavigatorBundle.getString("offlineApps.allow"),
           accessKey: gNavigatorBundle.getString("offlineApps.allowAccessKey"),
           callback: function() {
+            Services.telemetry.getHistogramById("PLUGINS_INFOBAR_ALLOW").
+              add(true);
+
             let curNotification =
               PopupNotifications.getNotification("click-to-play-plugins",
                                                  aBrowser);
