ea691b049e1839925941412dca698413eb6a1bb8
Jon Coppeard <jcoppeard@mozilla.com>
2013-11-19 22:53:32 +0000

    Bug 939993 - Check that AddPtrs are used only with matching Lookup values r=sfink

--- js/src/vm/Shape.cpp
+++ js/src/vm/Shape.cpp
@@ -1466,6 +1466,7 @@ BaseShape::getUnowned(ExclusiveContext *cx, const StackBaseShape &base)
     if (!table.initialized() && !table.init())
         return nullptr;
 
+    uint64_t originalGcNumber = cx->zone()->gcNumber();
     BaseShapeSet::AddPtr p = table.lookupForAdd(&base);
 
     if (p)
@@ -1481,7 +1482,14 @@ BaseShape::getUnowned(ExclusiveContext *cx, const StackBaseShape &base)
 
     UnownedBaseShape *nbase = static_cast<UnownedBaseShape *>(nbase_);
 
-    if (!table.relookupOrAdd(p, &base, nbase))
+    /*
+     * If a GC has occurred then the hash we calculated may be invalid, as it is
+     * based on the objects inside StackBaseShape, which may have been moved.
+     */
+    bool gcHappened = cx->zone()->gcNumber() != originalGcNumber;
+    bool added = gcHappened ? table.putNew(&base, nbase)
+                            : table.relookupOrAdd(p, &base, nbase);
+    if (!added)
         return nullptr;
 
     return nbase;
@@ -1593,6 +1601,7 @@ EmptyShape::getInitialShape(ExclusiveContext *cx, const Class *clasp, TaggedProt
         return nullptr;
 
     typedef InitialShapeEntry::Lookup Lookup;
+    uint64_t originalGcNumber = cx->zone()->gcNumber();
     InitialShapeSet::AddPtr p =
         table.lookupForAdd(Lookup(clasp, proto, parent, metadata, nfixed, objectFlags));
 
@@ -1614,11 +1623,17 @@ EmptyShape::getInitialShape(ExclusiveContext *cx, const Class *clasp, TaggedProt
         return nullptr;
     new (shape) EmptyShape(nbase, nfixed);
 
-    if (!table.relookupOrAdd(p, Lookup(clasp, protoRoot, parentRoot, metadataRoot, nfixed, objectFlags),
-                             InitialShapeEntry(shape, protoRoot)))
-    {
+    /*
+     * If a GC has occurred, then the hash we calculated may be invalid, as it
+     * is based on objects which may have been moved.
+     */
+    Lookup lookup(clasp, protoRoot, parentRoot, metadataRoot, nfixed, objectFlags);
+    InitialShapeEntry entry(shape, protoRoot);
+    bool gcHappened = cx->zone()->gcNumber() != originalGcNumber;
+    bool added = gcHappened ? table.putNew(lookup, entry)
+                            : table.relookupOrAdd(p, lookup, entry);
+    if (!added)
         return nullptr;
-    }
 
     return shape;
 }
