e7cf4b8ef79881fe3f65445b3e64e4e05eeb7b9b
Richard Newman <rnewman@mozilla.com>
2013-11-18 17:33:35 -0800

    Backed out changeset a14c36a25b92 (bug 935499) for xpcshell test failures on a CLOSED TREE.

--- netwerk/test/unit/test_bug935499.js
+++
@@ -1,7 +0,0 @@
-function run_test() {
-  var idnService = Cc["@mozilla.org/network/idn-service;1"]
-                   .getService(Ci.nsIIDNService);
-
-  var isASCII = {};
-  do_check_eq(idnService.convertToDisplayIDN("xn--", isASCII), "xn--");
-}
