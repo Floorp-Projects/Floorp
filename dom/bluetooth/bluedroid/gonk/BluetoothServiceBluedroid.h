9cd39a5dff0d99d21a01a5d286dd9feeb0b2fb0e
Eric Chou <echou@mozilla.com>
2013-11-20 15:01:54 +0800

    Bug 936995 - Fire event Disable after Bluetooth is really disabled, r=gyeh

--- dom/bluetooth/bluedroid/gonk/BluetoothServiceBluedroid.h
+++ dom/bluetooth/bluedroid/gonk/BluetoothServiceBluedroid.h
@@ -192,6 +192,9 @@ public:
   virtual nsresult
   SendInputMessage(const nsAString& aDeviceAddresses,
                    const nsAString& aMessage) MOZ_OVERRIDE;
+
+  BluetoothServiceBluedroid();
+  ~BluetoothServiceBluedroid();
 };
 
 END_BLUETOOTH_NAMESPACE
