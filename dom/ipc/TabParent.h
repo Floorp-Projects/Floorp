cbb878f27bb21a03fb2605e0baad6c49f6329c99
Phil Ringnalda <philringnalda@gmail.com>
2013-11-19 21:59:10 -0800

    Back out 4983f1debf4c (bug 918880) for b2g mochitest-3 bustage CLOSED TREE

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -234,7 +234,6 @@ public:
             const URIParams& aDocumentURI,
             const bool& stickDocument) MOZ_OVERRIDE;
     virtual bool DeallocPOfflineCacheUpdateParent(POfflineCacheUpdateParent* actor);
-    virtual bool RecvSetOfflinePermission(const IPC::Principal& principal);
 
     bool GetGlobalJSObject(JSContext* cx, JSObject** globalp);
 
