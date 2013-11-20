802e5e41ca75550d1cf1222a071ba97b2de04776
Benoit Girard <b56girard@gmail.com>
2013-11-19 23:55:27 -0500

    Bug 936566 - Print mask layers in layers dump. r=nrc

--- gfx/layers/Layers.cpp
+++ gfx/layers/Layers.cpp
@@ -1261,6 +1261,12 @@ Layer::LogSelf(const char* aPrefix)
   nsAutoCString str;
   PrintInfo(str, aPrefix);
   MOZ_LAYERS_LOG(("%s", str.get()));
+
+  if (mMaskLayer) {
+    nsAutoCString pfx(aPrefix);
+    pfx += "   \\ MaskLayer ";
+    mMaskLayer->LogSelf(pfx.get());
+  }
 }
 
 nsACString&
@@ -1304,6 +1310,9 @@ Layer::PrintInfo(nsACString& aTo, const char* aPrefix)
                      mStickyPositionData->mOuter.width, mStickyPositionData->mOuter.height,
                      mStickyPositionData->mInner.x, mStickyPositionData->mInner.y,
                      mStickyPositionData->mInner.width, mStickyPositionData->mInner.height);
+  }
+  if (mMaskLayer) {
+    aTo.AppendPrintf(" [mMaskLayer=%p]", mMaskLayer.get());
   }
 
   return aTo;
