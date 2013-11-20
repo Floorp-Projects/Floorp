60fc45811df8acdd3323927a3dbf7cd30fa74df2
Eric Chou <echou@mozilla.com>
2013-11-20 15:17:00 +0800

    Backed out changeset 40ecdab580b3 (bug 936995) for potential JB build error

--- dom/bluetooth/bluedroid/gonk/BluetoothServiceBluedroid.h
+++ dom/bluetooth/bluedroid/gonk/BluetoothServiceBluedroid.h
@@ -192,9 +192,6 @@ public:
   virtual nsresult
   SendInputMessage(const nsAString& aDeviceAddresses,
                    const nsAString& aMessage) MOZ_OVERRIDE;
-
-  BluetoothServiceBluedroid();
-  ~BluetoothServiceBluedroid();
 };
 
 END_BLUETOOTH_NAMESPACE
