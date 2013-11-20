02981998d091befe4cc9d6c6df542b202c831661
Benoit Jacob <bjacob@mozilla.com>
2013-11-20 17:50:54 -0500

    Bug 834243 - avoid calling serverSurfaceChanged before the compositor is created - r=vladv

--- mobile/android/base/gfx/LayerView.java
+++ mobile/android/base/gfx/LayerView.java
@@ -514,8 +514,13 @@ public class LayerView extends FrameLayout implements Tabs.OnTabsChangedListener
      * TextureView instead of a SurfaceView, the first phase is skipped.
      */
     private void onSizeChanged(int width, int height) {
-        if (!mGLController.isServerSurfaceValid() || mSurfaceView == null) {
-            surfaceChanged(width, height);
+        if (!mGLController.isCompositorCreated()) {
+            return;
+        }
+
+        surfaceChanged(width, height);
+
+        if (mSurfaceView == null) {
             return;
         }
 
