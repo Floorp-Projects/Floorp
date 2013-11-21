8fdefc9de902c6546221bfbbc04dccfc05750ff9
Nathan Froyd <froydnj@mozilla.com>
2013-11-21 09:11:35 -0500

    Bug 940637 - part 0b - add a license header and editor modelines to MemoryStats.js; r=jmaher

--- testing/mochitest/tests/SimpleTest/MemoryStats.js
+++ testing/mochitest/tests/SimpleTest/MemoryStats.js
@@ -1,3 +1,9 @@
+/* -*- js-indent-level: 4; tab-width: 4; indent-tabs-mode: nil -*- */
+/* vim:set ts=4 sw=4 sts=4 et: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 var MemoryStats = {};
 
 /**
