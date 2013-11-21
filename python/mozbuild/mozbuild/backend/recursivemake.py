66b13e4bd04608faa8eca7051bfd44624765eb58
Ehsan Akhgari <ehsan@mozilla.com>
2013-11-21 15:39:44 -0500

    Bug 941824 - #define MOZ_UNIFIED_BUILD for everything that is compiled in unified mode; r=gps

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -644,6 +644,7 @@ class RecursiveMakeBackend(CommonBackend):
             # handle source files being added/removed/renamed.  Therefore, we
             # generate them here also to make sure everything's up-to-date.
             with self._write_file(os.path.join(output_directory, unified_file)) as f:
+                f.write('#define MOZ_UNIFIED_BUILD\n')
                 includeTemplate = '#include "%(cppfile)s"'
                 if poison_windows_h:
                     includeTemplate += (
