fc8d7db0caf1b2b9421a3002ed26781c7adeb2e8
Josh Matthews <josh@joshmatthews.net>
2013-11-21 17:58:16 +0800

    Bug 915184 - Wait for the default about:privatebrowsing page to appear before continuing the test. r=ehsan, a=test-only

--- browser/base/content/test/browser_save_private_link_perwindowpb.js
+++ browser/base/content/test/browser_save_private_link_perwindowpb.js
@@ -95,8 +95,6 @@ function test() {
                                            aWindow.gBrowser.contentWindow);
       });
     });
-
-    aWindow.gBrowser.selectedBrowser.loadURI(testURI);
   }
 
   function testOnWindow(aOptions, aCallback) {
