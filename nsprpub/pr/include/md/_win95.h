6f4ffd123d7137fd4d2edaca2471d8d0babacc30
Ted Mielczarek <ted@mielczarek.org>
2013-11-22 14:42:35 -0500

    bug 942207 - Update NSPR to NSPR_4_10_3_BETA1

--- nsprpub/pr/include/md/_win95.h
+++ nsprpub/pr/include/md/_win95.h
@@ -527,6 +527,12 @@ extern PRStatus _MD_MemUnmap(void *addr, PRUint32 size);
 extern PRStatus _MD_CloseFileMap(struct PRFileMap *fmap);
 #define _MD_CLOSE_FILE_MAP _MD_CloseFileMap
 
+extern PRStatus _MD_SyncMemMap(
+    PRFileDesc *fd,
+    void *addr,
+    PRUint32 len);
+#define _MD_SYNC_MEM_MAP _MD_SyncMemMap
+
 /* --- Named semaphores stuff --- */
 #define _PR_HAVE_NAMED_SEMAPHORES
 #define _MD_OPEN_SEMAPHORE            _PR_MD_OPEN_SEMAPHORE
