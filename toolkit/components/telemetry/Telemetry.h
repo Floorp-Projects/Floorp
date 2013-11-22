d43fbda2610b8991089d73feb84244a18ed7099e
Jim Chen <nchen@mozilla.com>
2013-11-22 14:17:31 -0500

    Bug 932865 - Record inactive thread hang stats inside telemetry; r=vladan

--- toolkit/components/telemetry/Telemetry.h
+++ toolkit/components/telemetry/Telemetry.h
@@ -175,6 +175,20 @@ void RecordChromeHang(uint32_t duration,
                       ProcessedStack &aStack);
 #endif
 
+class ThreadHangStats;
+
+/**
+ * Move a ThreadHangStats to Telemetry storage. Normally Telemetry queries
+ * for active ThreadHangStats through BackgroundHangMonitor, but once a
+ * thread exits, the thread's copy of ThreadHangStats needs to be moved to
+ * inside Telemetry using this function.
+ *
+ * @param aStats ThreadHangStats to save; the data inside aStats
+ *               will be moved and aStats should be treated as
+ *               invalid after this function returns
+ */
+void RecordThreadHangStats(ThreadHangStats& aStats);
+
 /**
  * Record a failed attempt at locking the user's profile.
  *
