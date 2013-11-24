0b9f5ab339b935e3379a9515b3a293fb2c7df3b3
Bill McCloskey <wmccloskey@mozilla.com>
2013-11-23 21:32:32 -0800

    Bug 682048 - Change Metro frame script handling to support anon/global scope (r=jimm,mbrubeck)

--- browser/metro/base/content/contenthandlers/FindHandler.js
+++ browser/metro/base/content/contenthandlers/FindHandler.js
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+let Ci = Components.interfaces;
+let Cc = Components.classes;
+
 dump("### FindHandler.js loaded\n");
 
 var FindHandler = {
@@ -83,5 +86,6 @@ var FindHandler = {
     timer.once(0);
   }
 };
+this.FindHandler = FindHandler;
 
 FindHandler.init();
