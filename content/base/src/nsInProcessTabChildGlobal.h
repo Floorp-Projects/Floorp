2ea8967b0fc3be2c101cc2988e2ec48c6a95bbcb
Bill McCloskey <wmccloskey@mozilla.com>
2013-11-23 21:32:45 -0800

    Bug 673569 - Let each frame script have its own anonymous scope (r=smaug,Waldo,mrbkap,bsmedberg)

--- content/base/src/nsInProcessTabChildGlobal.h
+++ content/base/src/nsInProcessTabChildGlobal.h
@@ -119,7 +119,7 @@ public:
 
   virtual JSContext* GetJSContextForEventHandlers() MOZ_OVERRIDE { return nsContentUtils::GetSafeJSContext(); }
   virtual nsIPrincipal* GetPrincipal() MOZ_OVERRIDE { return mPrincipal; }
-  void LoadFrameScript(const nsAString& aURL);
+  void LoadFrameScript(const nsAString& aURL, bool aRunInGlobalScope);
   void Disconnect();
   void SendMessageToParent(const nsString& aMessage, bool aSync,
                            const nsString& aJSON,
