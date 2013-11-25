9950de364128e8255a89956b7120a11a9d12443c
Benjamin Smedberg <benjamin@smedbergs.us>
2013-11-25 15:06:17 -0500

    Bug 938794 - Annotate OOM size as infallible string or data structures abort, r=froydnj
    --HG--
    extra : rebase_source : f84278dfbba92c6d75458b525a559b6f8598500f

--- xpcom/glue/nsBaseHashtable.h
+++ xpcom/glue/nsBaseHashtable.h
@@ -122,7 +122,7 @@ public:
   void Put(KeyType aKey, const UserDataType& aData)
   {
     if (!Put(aKey, aData, fallible_t()))
-      NS_RUNTIMEABORT("OOM");
+      NS_ABORT_OOM(this->mTable.entrySize * this->mTable.entryCount);
   }
 
   bool Put(KeyType aKey, const UserDataType& aData, const fallible_t&) NS_WARN_UNUSED_RESULT
