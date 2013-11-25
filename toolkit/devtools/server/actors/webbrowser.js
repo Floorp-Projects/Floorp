86dcfd1caa8251c4a97137d6ccec010a5a47cbf5
Shu-yu Guo <shu@rfrn.org>
2013-11-24 22:51:19 -0800

    Bug 941787 - Fix race between tab closure event and nested event loop popping in devtools browser actor. r=panos, a=lsblakk

--- toolkit/devtools/server/actors/webbrowser.js
+++ toolkit/devtools/server/actors/webbrowser.js
@@ -470,6 +470,9 @@ function BrowserTabActor(aConnection, aBrowser, aTabBrowser)
   this._extraActors = {};
 
   this._onWindowCreated = this.onWindowCreated.bind(this);
+
+  // Number of event loops nested.
+  this._nestedEventLoopDepth = 0;
 }
 
 // XXX (bug 710213): BrowserTabActor attach/detach/exit/disconnect is a
@@ -618,6 +621,9 @@ BrowserTabActor.prototype = {
                        type: "tabDetached" });
     }
 
+    // Pop all nested event loops if we haven't already.
+    while (this._nestedEventLoopDepth > 0)
+      this.postNest();
     this._browser = null;
     this._tabbrowser = null;
   },
@@ -766,6 +772,7 @@ BrowserTabActor.prototype = {
                           .getInterface(Ci.nsIDOMWindowUtils);
     windowUtils.suppressEventHandling(true);
     windowUtils.suspendTimeouts();
+    this._nestedEventLoopDepth++;
   },
 
   /**
@@ -774,6 +781,8 @@ BrowserTabActor.prototype = {
   postNest: function BTA_postNest(aNestData) {
     if (!this.window) {
       // The tab is already closed.
+      dbg_assert(this._nestedEventLoopDepth === 0,
+                 "window shouldn't be closed before all nested event loops have been popped");
       return;
     }
     let windowUtils = this.window
@@ -785,6 +794,7 @@ BrowserTabActor.prototype = {
       this._pendingNavigation.resume();
       this._pendingNavigation = null;
     }
+    this._nestedEventLoopDepth--;
   },
 
   /**
