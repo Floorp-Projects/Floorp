38e94eeefc2af73a92cc5eb2956060e0e546c086
Wes Johnston <wjohnston@mozilla.com>
2013-11-25 09:42:25 -0800

    Bug 921112 - Expose MCC/MNC in payments on Android. r=mfinkle

---
+++ mobile/android/base/tests/testMozPay.java
@@ -0,0 +1,10 @@
+package org.mozilla.gecko.tests;
+
+import org.mozilla.gecko.*;
+
+
+public class testMozPay extends JavascriptTest {
+    public testMozPay() {
+        super("testMozPay.js");
+    }
+}
