18dda872fd267ee48cd0f681ec1e0fd57206536e
Andrew McCreight <continuation@gmail.com>
2013-11-25 14:58:22 -0800

    Bug 905382, part 1 - Implement a read barrier for GC things. r=jonco

--- js/public/GCAPI.h
+++ js/public/GCAPI.h
@@ -311,6 +311,31 @@ ExposeObjectToActiveJS(JSObject *obj)
     ExposeGCThingToActiveJS(obj, JSTRACE_OBJECT);
 }
 
+/*
+ * If a GC is currently marking, mark the object black.
+ */
+static JS_ALWAYS_INLINE void
+MarkGCThingAsLive(JSRuntime *rt_, void *thing, JSGCTraceKind kind)
+{
+    shadow::Runtime *rt = shadow::Runtime::asShadowRuntime(rt_);
+#ifdef JSGC_GENERATIONAL
+    /*
+     * Any object in the nursery will not be freed during any GC running at that time.
+     */
+    if (js::gc::IsInsideNursery(rt, thing))
+        return;
+#endif
+    if (IsIncrementalBarrierNeededOnGCThing(rt, thing, kind))
+        IncrementalReferenceBarrier(thing, kind);
+}
+
+static JS_ALWAYS_INLINE void
+MarkStringAsLive(Zone *zone, JSString *string)
+{
+    JSRuntime *rt = JS::shadow::Zone::asShadowZone(zone)->runtimeFromMainThread();
+    MarkGCThingAsLive(rt, string, JSTRACE_STRING);
+}
+
 } /* namespace JS */
 
 #endif /* js_GCAPI_h */
