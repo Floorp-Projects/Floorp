2cd89976b46c265aff530dae4416ca914ebcc051
Nicholas Cameron <ncameron@mozilla.com>
2013-11-26 15:07:44 +1300

    Bug 943122. Don't repeat 'Mask layer:' in layer dumps. r=BenWa

--- gfx/layers/Layers.cpp
+++ gfx/layers/Layers.cpp
@@ -1199,8 +1199,9 @@ Layer::Dump(FILE* aFile, const char* aPrefix, bool aDumpHtml)
   }
 
   if (Layer* mask = GetMaskLayer()) {
+    fprintf_stderr(aFile, "%s  Mask layer:\n", aPrefix);
     nsAutoCString pfx(aPrefix);
-    pfx += "  Mask layer: ";
+    pfx += "    ";
     mask->Dump(aFile, pfx.get(), aDumpHtml);
   }
 
