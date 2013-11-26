69b1f7b81d6b5870bd34217efacab45e32e78d24
Nicholas Cameron <ncameron@mozilla.com>
2013-11-27 11:15:17 +1300

    Bug 924403. Rename OGL shaders files. r=mattwoodrow
    --HG--
    rename : gfx/layers/opengl/LayerManagerOGLProgram.cpp => gfx/layers/opengl/OGLShaderProgram.cpp
    rename : gfx/layers/opengl/LayerManagerOGLProgram.h => gfx/layers/opengl/OGLShaderProgram.h
    rename : gfx/layers/opengl/LayerManagerOGLShaders.h => gfx/layers/opengl/OGLShaders.h
    rename : gfx/layers/opengl/LayerManagerOGLShaders.txt => gfx/layers/opengl/OGLShaders.txt

--- gfx/layers/opengl/GLManager.h
+++ gfx/layers/opengl/GLManager.h
@@ -7,7 +7,7 @@
 #define MOZILLA_GFX_GLMANAGER_H
 
 #include "mozilla/gfx/Types.h"          // for SurfaceFormat
-#include "LayerManagerOGLProgram.h"
+#include "OGLShaderProgram.h"
 
 namespace mozilla {
 namespace gl {
