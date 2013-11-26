48ffa77e9a6fd5de66efb49cfcf2164241f9ac2e
Kannan Vijayan <kvijayan@mozilla.com>
2013-11-26 12:46:33 -0500

    Bug 942604 - Add correct Float32 handling to MSetFrameArgument. r=bbouvier

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -8325,7 +8325,8 @@ class MGetFrameArgument
 
 // This MIR instruction is used to set an argument value in the frame.
 class MSetFrameArgument
-  : public MUnaryInstruction
+  : public MUnaryInstruction,
+    public NoFloatPolicy<0>
 {
     uint32_t argno_;
 
@@ -8356,6 +8357,9 @@ class MSetFrameArgument
     }
     AliasSet getAliasSet() const {
         return AliasSet::Store(AliasSet::FrameArgument);
+    }
+    TypePolicy *typePolicy() {
+        return this;
     }
 };
 
