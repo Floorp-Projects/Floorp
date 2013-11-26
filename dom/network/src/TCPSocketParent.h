af3684d5b6af3e5db6bf11725fc41370a6de1fc7
Ed Morley <emorley@mozilla.com>
2013-11-26 17:30:55 +0000

    Backed out changeset 8c69c6e6fc39 (bug 942633) for build failures on a CLOSED TREE

--- dom/network/src/TCPSocketParent.h
+++ dom/network/src/TCPSocketParent.h
@@ -2,9 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_TCPSocketParent_h
-#define mozilla_dom_TCPSocketParent_h
-
 #include "mozilla/net/PTCPSocketParent.h"
 #include "nsITCPSocketParent.h"
 #include "nsCycleCollectionParticipant.h"
@@ -66,5 +63,3 @@ private:
 
 } // namespace dom
 } // namespace mozilla
-
-#endif
