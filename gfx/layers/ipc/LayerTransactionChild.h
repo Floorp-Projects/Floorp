3f0470e2acd2cde315487c0f9ada9cc6d36e1c15
Benoit Jacob <bjacob@mozilla.com>
2013-11-27 10:18:38 -0500

    Bug 933082 - Part 1: make LayerTransactionChild reference-counted - r=nical

--- gfx/layers/ipc/LayerTransactionChild.h
+++ gfx/layers/ipc/LayerTransactionChild.h
@@ -13,16 +13,20 @@
 #include "mozilla/Attributes.h"         // for MOZ_OVERRIDE
 #include "mozilla/ipc/ProtocolUtils.h"
 #include "mozilla/layers/PLayerTransactionChild.h"
+#include "mozilla/RefPtr.h"
 
 namespace mozilla {
+
+namespace layout {
+class RenderFrameChild;
+}
+
 namespace layers {
 
 class LayerTransactionChild : public PLayerTransactionChild
+                            , public AtomicRefCounted<LayerTransactionChild>
 {
 public:
-  LayerTransactionChild() { }
-  ~LayerTransactionChild() { }
-
   /**
    * Clean this up, finishing with Send__delete__().
    *
@@ -32,7 +36,16 @@ public:
    */
   void Destroy();
 
+  bool IPCOpen() const { return mIPCOpen; }
+
 protected:
+  LayerTransactionChild()
+    : mIPCOpen(false)
+  {}
+  ~LayerTransactionChild() { }
+  friend class AtomicRefCounted<LayerTransactionChild>;
+  friend class detail::RefCounted<LayerTransactionChild, detail::AtomicRefCount>;
+
   virtual PGrallocBufferChild*
   AllocPGrallocBufferChild(const gfxIntSize&,
                       const uint32_t&, const uint32_t&,
@@ -46,6 +59,21 @@ protected:
   virtual PCompositableChild* AllocPCompositableChild(const TextureInfo& aInfo) MOZ_OVERRIDE;
   virtual bool DeallocPCompositableChild(PCompositableChild* actor) MOZ_OVERRIDE;
   virtual void ActorDestroy(ActorDestroyReason why) MOZ_OVERRIDE;
+
+  void AddIPDLReference() {
+    MOZ_ASSERT(mIPCOpen == false);
+    mIPCOpen = true;
+    AddRef();
+  }
+  void ReleaseIPDLReference() {
+    MOZ_ASSERT(mIPCOpen == true);
+    mIPCOpen = false;
+    Release();
+  }
+  friend class CompositorChild;
+  friend class layout::RenderFrameChild;
+
+  bool mIPCOpen;
 };
 
 } // namespace layers
