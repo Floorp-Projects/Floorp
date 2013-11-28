70a51235338a6d332b5402c524fb83be6d10e47a
Carsten "Tomcat" Book <cbook@mozilla.com>
2013-11-28 12:09:46 +0100

    Backed out changeset b064b9271e0e (bug 942712) Build bustage on a CLOSED TREE

--- dom/bluetooth/BluetoothService.cpp
+++ dom/bluetooth/BluetoothService.cpp
@@ -164,9 +164,6 @@ public:
     BluetoothSignal signal(signalName, NS_LITERAL_STRING(KEY_MANAGER), true);
     gBluetoothService->DistributeSignal(signal);
 
-    // Event 'AdapterAdded' has to be fired after firing 'Enabled'
-    gBluetoothService->TryFiringAdapterAdded();
-
     return NS_OK;
   }
 
@@ -511,8 +508,6 @@ BluetoothService::StartStopBluetooth(bool aStart, bool aIsStartup)
                                           LazyIdleThread::ManualShutdown);
   }
 
-  mAdapterAddedReceived = false;
-
   nsCOMPtr<nsIRunnable> runnable = new ToggleBtTask(aStart, aIsStartup);
   nsresult rv = mBluetoothThread->Dispatch(runnable, NS_DISPATCH_NORMAL);
   NS_ENSURE_SUCCESS(rv, rv);
@@ -789,20 +784,6 @@ BluetoothService::Observe(nsISupports* aSubject, const char* aTopic,
 
   MOZ_ASSERT(false, "BluetoothService got unexpected topic!");
   return NS_ERROR_UNEXPECTED;
-}
-
-void
-BluetoothService::TryFiringAdapterAdded()
-{
-  MOZ_ASSERT(NS_IsMainThread());
-
-  if (IsToggling() || !mAdapterAddedReceived) {
-    return;
-  }
-
-  BluetoothSignal signal(NS_LITERAL_STRING("AdapterAdded"),
-                         NS_LITERAL_STRING(KEY_MANAGER), true);
-  DistributeSignal(signal);
 }
 
 void
