8f06ab6020766dbc693802446389a3c17c2f7627
Chris Peterson <cpeterson@mozilla.com>
2013-11-30 21:42:59 -0800

    Bug 944961 - Build rdf in unified mode. r=bsmedberg

--- rdf/base/src/nsContainerEnumerator.cpp
+++ rdf/base/src/nsContainerEnumerator.cpp
@@ -36,11 +36,6 @@
 
 ////////////////////////////////////////////////////////////////////////
 
-static NS_DEFINE_CID(kRDFServiceCID,        NS_RDFSERVICE_CID);
-static NS_DEFINE_CID(kRDFContainerUtilsCID, NS_RDFCONTAINERUTILS_CID);
-
-////////////////////////////////////////////////////////////////////////
-
 class ContainerEnumeratorImpl : public nsISimpleEnumerator {
 private:
     // pseudo-constants
@@ -84,6 +79,8 @@ ContainerEnumeratorImpl::Init()
 {
     if (gRefCnt++ == 0) {
         nsresult rv;
+
+        NS_DEFINE_CID(kRDFServiceCID, NS_RDFSERVICE_CID);
         nsCOMPtr<nsIRDFService> rdf = do_GetService(kRDFServiceCID);
         NS_ASSERTION(rdf != nullptr, "unable to acquire resource manager");
         if (! rdf)
@@ -93,6 +90,7 @@ ContainerEnumeratorImpl::Init()
         NS_ASSERTION(NS_SUCCEEDED(rv), "unable to get resource");
         if (NS_FAILED(rv)) return rv;
 
+        NS_DEFINE_CID(kRDFContainerUtilsCID, NS_RDFCONTAINERUTILS_CID);
         rv = CallGetService(kRDFContainerUtilsCID, &gRDFC);
         if (NS_FAILED(rv)) return rv;
     }
