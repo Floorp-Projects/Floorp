9566d097cbabd06965c317f8ffb1507bdeaa018e
Mike Hommey <mh+mozilla@glandium.org>
2013-12-03 06:34:21 +0900

    Bug 943728 - Replace double quotes with single quotes in Makefiles (or remove them when it makes sense). r=mshal

--- media/webrtc/trunk/tools/gyp/pylib/gyp/generator/mozmake.py
+++ media/webrtc/trunk/tools/gyp/pylib/gyp/generator/mozmake.py
@@ -75,7 +75,7 @@ endif
 ifeq (WINNT,$(OS_TARGET))
 # These get set via VC project file settings for normal GYP builds.
 DEFINES += -DUNICODE -D_UNICODE
-LOCAL_INCLUDES += -I"$(MOZ_DIRECTX_SDK_PATH)/include"
+LOCAL_INCLUDES += -I'$(MOZ_DIRECTX_SDK_PATH)/include'
 endif
 
 # Don't use STL wrappers when compiling Google code.
@@ -97,7 +97,7 @@ endif
 
 define COPY_SRC
 $(notdir $(1)): $(1)
-	$$(INSTALL) $$(IFLAGS1) "$$<" .
+	$$(INSTALL) $$(IFLAGS1) '$$<' .
 
 endef # COPY_SRC
 ifdef COPY_SRCS
