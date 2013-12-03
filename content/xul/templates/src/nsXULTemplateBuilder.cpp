5ef9494663255009cce40fe405efcf82eceeea96
Ehsan Akhgari <ehsan@mozilla.com>
2013-12-03 16:10:30 -0500

    Backed out changeset 582c563a8e1a (bug 945587) for bustage on CLOSED TREE

--- content/xul/templates/src/nsXULTemplateBuilder.cpp
+++ content/xul/templates/src/nsXULTemplateBuilder.cpp
@@ -142,7 +142,8 @@ nsXULTemplateBuilder::InitGlobals()
     if (gRefCnt++ == 0) {
         // Initialize the global shared reference to the service
         // manager and get some shared resource objects.
-        rv = CallGetService(NS_RDFSERVICE_CID, &gRDFService);
+        NS_DEFINE_CID(kRDFServiceCID, NS_RDFSERVICE_CID);
+        rv = CallGetService(kRDFServiceCID, &gRDFService);
         if (NS_FAILED(rv))
             return rv;
 
