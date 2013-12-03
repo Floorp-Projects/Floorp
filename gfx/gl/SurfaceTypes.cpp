a7a217d078f4e86a33bda3b24b441a90fdc44184
Benoit Jacob <bjacob@mozilla.com>
2013-12-03 13:44:38 -0500

    Bug 944703 - Part 1: make SurfaceCaps hold a strong reference to its ISurfaceAllocator - r=jgilbert

---
+++ gfx/gl/SurfaceTypes.cpp
@@ -0,0 +1,59 @@
+/* -*- Mode: c++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 40; -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "SurfaceTypes.h"
+#include "mozilla/layers/ISurfaceAllocator.h"
+
+namespace mozilla {
+namespace gfx {
+
+SurfaceCaps::SurfaceCaps()
+{
+  Clear();
+}
+
+SurfaceCaps::SurfaceCaps(const SurfaceCaps& other)
+{
+  *this = other;
+}
+
+SurfaceCaps&
+SurfaceCaps::operator=(const SurfaceCaps& other)
+{
+  any = other.any;
+  color = other.color;
+  alpha = other.alpha;
+  bpp16 = other.bpp16;
+  depth = other.depth;
+  stencil = other.stencil;
+  antialias = other.antialias;
+  preserve = other.preserve;
+  surfaceAllocator = other.surfaceAllocator;
+
+  return *this;
+}
+
+void
+SurfaceCaps::Clear()
+{
+  any = false;
+  color = false;
+  alpha = false;
+  bpp16 = false;
+  depth = false;
+  stencil = false;
+  antialias = false;
+  preserve = false;
+  surfaceAllocator = nullptr;
+}
+
+
+
+SurfaceCaps::~SurfaceCaps()
+{
+}
+
+}
+}
