1cf7aa7a16de36058cf7e8563c62958f03c7693f
Fabrice Desré <fabrice@mozilla.com>
2013-12-03 09:48:01 -0800

    Bug 944859 - osfile_async_worker.js worker is never shut down in the parent process. r=yoric

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -848,5 +848,8 @@ pref("media.webspeech.synth.enabled", true);
 pref("dom.mozDownloads.enabled", true);
 pref("dom.downloads.max_retention_days", 7);
 
+// Inactivity time in milliseconds after which we shut down the OS.File worker.
+pref("osfile.reset_worker_delay", 5000);
+
 // The URL of the Firefox Accounts auth server backend
 pref("identity.fxaccounts.auth.uri", "https://api-accounts.dev.lcip.org/v1");
