ea1c909659469d5d1e2191f86ae83b3ced1c5bcb
Ehsan Akhgari <ehsan@mozilla.com>
2013-12-03 17:03:45 -0500

    Bug 944212 - Build xpfe/appshell in unified mode; r=bsmedberg
    --HG--
    extra : rebase_source : 0a36b534d3df5418e4fc91ccfe8dbb236f3ec8c5

--- xpfe/appshell/src/nsAppShellWindowEnumerator.h
+++ xpfe/appshell/src/nsAppShellWindowEnumerator.h
@@ -3,6 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef nsAppShellWindowEnumerator_h
+#define nsAppShellWindowEnumerator_h
+
 #include "nsCOMPtr.h"
 #include "nsString.h"
 
@@ -158,3 +161,5 @@ public:
 protected:
   virtual nsWindowInfo *FindNext();
 };
+
+#endif
