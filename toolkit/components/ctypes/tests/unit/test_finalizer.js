3e42ba7ed16626c736e53d9879aee0a8a67c30e0
Ehsan Akhgari <ehsan@mozilla.com>
2013-12-03 15:48:12 -0500

    Bug 943999 - Build toolkit/components/ctypes/test in unified mode; r=bsmedberg

--- toolkit/components/ctypes/tests/unit/test_finalizer.js
+++ toolkit/components/ctypes/tests/unit/test_finalizer.js
@@ -136,7 +136,7 @@ function run_test()
       released: released
     }
   );
-  const rect_t = new ctypes.StructType("RECT",
+  const rect_t = new ctypes.StructType("myRECT",
                                        [{ top   : ctypes.int32_t },
                                         { left  : ctypes.int32_t },
                                         { bottom: ctypes.int32_t },
@@ -185,7 +185,7 @@ function run_test()
   );
   samples.push(
     {
-      name: "size_t, release returns RECT",
+      name: "size_t, release returns myRECT",
       acquire: library.declare("test_finalizer_acq_size_t",
                            ctypes.default_abi,
                            ctypes.size_t,
