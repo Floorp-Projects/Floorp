cac85954e509ad07f9cf9ae1db4284fa31a5cd6b
Boris Zbarsky <bzbarsky@mit.edu>
2013-12-02 19:08:07 -0500

    Bug 936056 - Be consistent about the thisobj we pass to getters. r=jorendorff, a=bajaj

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -1493,6 +1493,11 @@ struct JSJitInfo {
         AliasEverything
     };
 
+    bool isDOMJitInfo() const
+    {
+        return type != OpType_None;
+    }
+
     union {
         JSJitGetterOp getter;
         JSJitSetterOp setter;
@@ -1501,6 +1506,9 @@ struct JSJitInfo {
 
     uint32_t protoID;
     uint32_t depth;
+    // type not being OpType_None means this is a DOM method.  If you
+    // change that, come up with a different way of implementing
+    // isDOMJitInfo().
     OpType type;
     bool isInfallible;      /* Is op fallible? False in setters. */
     bool isMovable;         /* Is op movable?  To be movable the op must not
