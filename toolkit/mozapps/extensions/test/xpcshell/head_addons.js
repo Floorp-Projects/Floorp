37aaad48b943f2cd310436e7d2ef6207991c0779
Irving Reid <irving@mozilla.com>
2013-12-02 19:46:41 -0500

    Bug 944006: Don't load XPIProviderUtils if no addons and no schemaVersion; r=unfocused

--- toolkit/mozapps/extensions/test/xpcshell/head_addons.js
+++ toolkit/mozapps/extensions/test/xpcshell/head_addons.js
@@ -377,10 +377,8 @@ function startupManager(aAppChanged) {
     do_throw("Test attempt to startup manager that was already started.");
 
   if (aAppChanged || aAppChanged === undefined) {
-    var file = gProfD.clone();
-    file.append("extensions.ini");
-    if (file.exists())
-      file.remove(true);
+    if (gExtensionsINI.exists())
+      gExtensionsINI.remove(true);
   }
 
   gInternalManager = AM_Cc["@mozilla.org/addons/integration;1"].
@@ -473,14 +471,12 @@ function loadAddonsList() {
     themes: []
   };
 
-  var file = gProfD.clone();
-  file.append("extensions.ini");
-  if (!file.exists())
+  if (!gExtensionsINI.exists())
     return;
 
   var factory = AM_Cc["@mozilla.org/xpcom/ini-parser-factory;1"].
                 getService(AM_Ci.nsIINIParserFactory);
-  var parser = factory.createINIParser(file);
+  var parser = factory.createINIParser(gExtensionsINI);
   gAddonsList.extensions = readDirectories("ExtensionDirs");
   gAddonsList.themes = readDirectories("ThemeDirs");
 }
@@ -1214,6 +1210,14 @@ if ("nsIWindowsRegKey" in AM_Ci) {
 // Get the profile directory for tests to use.
 const gProfD = do_get_profile();
 
+const EXTENSIONS_DB = "extensions.json";
+let gExtensionsJSON = gProfD.clone();
+gExtensionsJSON.append(EXTENSIONS_DB);
+
+const EXTENSIONS_INI = "extensions.ini";
+let gExtensionsINI = gProfD.clone();
+gExtensionsINI.append(EXTENSIONS_INI);
+
 // Enable more extensive EM logging
 Services.prefs.setBoolPref("extensions.logging.enabled", true);
 
@@ -1398,10 +1402,6 @@ function do_exception_wrap(func) {
     }
   };
 }
-
-const EXTENSIONS_DB = "extensions.json";
-let gExtensionsJSON = gProfD.clone();
-gExtensionsJSON.append(EXTENSIONS_DB);
 
 /**
  * Change the schema version of the JSON extensions database
