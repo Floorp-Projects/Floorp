dc216559cb9014f40beb9566c8b710092833a9b4
Steven Lee <slee@mozilla.com>
2013-12-05 09:29:07 -0500

    Bug 926746 - Part 1: Merge fakeperm into B2G. r=mwu

--- widget/gonk/nsAppShell.cpp
+++ widget/gonk/nsAppShell.cpp
@@ -29,6 +29,7 @@
 #include <unistd.h>
 
 #include "base/basictypes.h"
+#include "GonkPermission.h"
 #include "nscore.h"
 #ifdef MOZ_OMX_DECODER
 #include "MediaResourceManagerService.h"
@@ -750,6 +751,7 @@ nsAppShell::Init()
 #if ANDROID_VERSION >= 18
         android::FakeSurfaceComposer::instantiate();
 #endif
+        GonkPermissionService::instantiate();
     }
 
     nsCOMPtr<nsIObserverService> obsServ = GetObserverService();
