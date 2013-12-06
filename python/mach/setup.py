03bc411bbe719383f862beeaa8225f70d0a4ab32
Andrew Halberstadt <ahalberstadt@mozilla.com>
2013-12-06 09:24:09 -0500

    Bug 942275 - Add support for setuptools' entry points to mach, r=gps * * * Bug 942275 - Ignore load_from_entry_point if setuptools not present, r=gps

--- python/mach/setup.py
+++ python/mach/setup.py
@@ -11,6 +11,7 @@ setup(
     description='CLI frontend to mozilla-central.',
     license='MPL 2.0',
     packages=['mach'],
-    version=VERSION
+    version=VERSION,
+    tests_require=['mock']
 )
 
