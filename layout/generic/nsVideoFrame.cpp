738e31ed98f4771a4e6047cd758fff06db834ee5
Carsten "Tomcat" Book <cbook@mozilla.com>
2013-12-06 08:41:32 +0100

    Backed out changeset a0906b92810e (bug 937317) WinXP Build Bustage on a CLOSED TREE

--- layout/generic/nsVideoFrame.cpp
+++ layout/generic/nsVideoFrame.cpp
@@ -26,6 +26,7 @@
 #include "ImageContainer.h"
 #include "ImageLayers.h"
 #include "nsContentList.h"
+#include "mozilla/dom/ScriptSettings.h"
 #include <algorithm>
 
 using namespace mozilla;
@@ -73,6 +74,9 @@ nsVideoFrame::CreateAnonymousContent(nsTArray<ContentInfo>& aElements)
     element = NS_NewHTMLImageElement(nodeInfo.forget());
     mPosterImage = element;
     NS_ENSURE_TRUE(mPosterImage, NS_ERROR_OUT_OF_MEMORY);
+
+    // See bug 604262.
+    AutoSystemCaller asc;
 
     // Set the nsImageLoadingContent::ImageState() to 0. This means that the
     // image will always report its state as 0, so it will never be reframed
