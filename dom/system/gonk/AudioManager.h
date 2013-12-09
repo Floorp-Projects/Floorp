0d3a767ee1e1ab1259f41dae0b5c56a2fe0d9c2f
Birunthan Mohanathas <birunthan@mohanathas.com>
2013-12-08 21:53:11 -0500

    Bug 798033 - Remove 'using namespace' from dom/ headers. r=echou

--- dom/system/gonk/AudioManager.h
+++ dom/system/gonk/AudioManager.h
@@ -28,8 +28,6 @@
       {0x89, 0x10, 0xf9, 0x3c, 0x55, 0xe6, 0x62, 0xec}}
 #define NS_AUDIOMANAGER_CONTRACTID "@mozilla.org/telephony/audiomanager;1"
 
-using namespace mozilla::dom;
-
 namespace mozilla {
 namespace hal {
 class SwitchEvent;
