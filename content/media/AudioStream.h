2d7afabc14c8adbb8f48b951d2786176139d826f
Paul Adenot <paul@paul.cx>
2013-12-09 20:54:49 +0100

    Bug 944707 - Stop locking when getting the preferred samplerate from the AudioStream. r=kinetik
    --HG--
    extra : rebase_source : bbf7b305d13f252e7c743bb9abf12da078db865b

--- content/media/AudioStream.h
+++ content/media/AudioStream.h
@@ -179,8 +179,11 @@ public:
   // Returns the maximum number of channels supported by the audio hardware.
   static int MaxNumberOfChannels();
 
-  // Returns the samplerate the systems prefer, because it is the
-  // samplerate the hardware/mixer supports.
+  // Queries the samplerate the hardware/mixer runs at, and stores it.
+  // Can be called on any thread. When this returns, it is safe to call
+  // PreferredSampleRate without locking.
+  static void InitPreferredSampleRate();
+  // Get the aformentionned sample rate. Does not lock.
   static int PreferredSampleRate();
 
   AudioStream();
