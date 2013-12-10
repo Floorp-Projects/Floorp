3a2d8af198510726b063a217438fcf2591f4dfcf
Randell Jesup <rjesup@jesup.org>
2013-12-10 01:03:21 -0500

    Bug 853356: backout p3

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -225,8 +225,7 @@ public:
     virtual bool DeallocPDocumentRendererParent(PDocumentRendererParent* actor);
 
     virtual PContentPermissionRequestParent*
-    AllocPContentPermissionRequestParent(const InfallibleTArray<PermissionRequest>& aRequests,
-                                         const IPC::Principal& aPrincipal);
+    AllocPContentPermissionRequestParent(const nsCString& aType, const nsCString& aAccess, const IPC::Principal& aPrincipal);
     virtual bool DeallocPContentPermissionRequestParent(PContentPermissionRequestParent* actor);
 
     virtual POfflineCacheUpdateParent* AllocPOfflineCacheUpdateParent(
