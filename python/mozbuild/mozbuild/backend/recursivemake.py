e396fc159d907387dcc1b1501c07cbc42b10210f
Gregory Szorc <gps@mozilla.com>
2013-12-10 16:18:11 +0900

    Bug 774572 - Part 2: Define JAR_MANIFESTS in moz.build files; r=glandium
    Every directory with a jar.mn now has JAR_MANIFESTS defined in its
    moz.build file.
    
    We also removed the may_skip special consideration of jar.mn files
    because this information is now available during tier traversal by the
    reader courtesy of the variables being present in moz.build files.
    
    --HG--
    extra : rebase_source : 21049b15e6bd9cf65b0805ccaccc4ba5aae93c98
    extra : amend_source : 0b1ea866d725beef92d37c6f6d475369ac002e19

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -687,11 +687,6 @@ class RecursiveMakeBackend(CommonBackend):
                     self.log(logging.DEBUG, 'stub_makefile',
                         {'path': makefile}, 'Creating stub Makefile: {path}')
 
-                # Can't skip directories with a jar.mn for the 'libs' tier.
-                if bf.relobjdir in self._may_skip['libs'] and \
-                        os.path.exists(mozpath.join(srcdir, 'jar.mn')):
-                    self._may_skip['libs'].remove(bf.relobjdir)
-
                 obj = self.Substitution()
                 obj.output_path = makefile
                 obj.input_path = makefile_in
