2786d33ba8007063e526618b8c48d293882201d9
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2013-12-11 21:59:04 +0100

    Bug 947987 - Adjust how Australis' CustomizableUI deals with removable widgets, r=jaws

--- browser/components/customizableui/test/browser_938995_indefaultstate_nonremovable.js
+++ browser/components/customizableui/test/browser_938995_indefaultstate_nonremovable.js
@@ -10,16 +10,17 @@ let gTests = [
       let navbar = document.getElementById("nav-bar");
       ok(CustomizableUI.inDefaultState, "Should start in default state");
 
-      CustomizableUI.createWidget({id: kWidgetId, removable: false, label: "Test"});
+      let button = createDummyXULButton(kWidgetId, "Test non-removable inDefaultState handling");
       CustomizableUI.addWidgetToArea(kWidgetId, CustomizableUI.AREA_NAVBAR);
+      button.setAttribute("removable", "false");
       ok(CustomizableUI.inDefaultState, "Should still be in default state after navbar addition");
-      CustomizableUI.destroyWidget(kWidgetId);
+      button.remove();
 
-      CustomizableUI.createWidget({id: kWidgetId, removable: false, label: "Test"});
+      button = createDummyXULButton(kWidgetId, "Test non-removable inDefaultState handling");
       CustomizableUI.addWidgetToArea(kWidgetId, CustomizableUI.AREA_PANEL);
+      button.setAttribute("removable", "false");
       ok(CustomizableUI.inDefaultState, "Should still be in default state after panel addition");
-      CustomizableUI.destroyWidget(kWidgetId);
-
+      button.remove();
       ok(CustomizableUI.inDefaultState, "Should be in default state after destroying both widgets");
     },
     teardown: null
