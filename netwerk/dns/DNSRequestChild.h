8e0bcc936890637235c6e1a29f76ae37a1da4086
Jason Duell <jduell.mcbugs@gmail.com>
2013-12-11 11:03:51 -0800

    Bug 945066 - Remote nsIDNSService to child. r=jdm, a=lsblakk

---
+++ netwerk/dns/DNSRequestChild.h
@@ -0,0 +1,58 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set sw=2 ts=8 et tw=80 : */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_net_DNSRequestChild_h
+#define mozilla_net_DNSRequestChild_h
+
+#include "mozilla/net/PDNSRequestChild.h"
+#include "nsICancelable.h"
+#include "nsIDNSRecord.h"
+#include "nsIDNSListener.h"
+#include "nsIEventTarget.h"
+
+namespace mozilla {
+namespace net {
+
+class DNSRequestChild
+  : public PDNSRequestChild
+  , public nsICancelable
+{
+public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSICANCELABLE
+
+  DNSRequestChild(const nsCString& aHost, const uint32_t& aFlags,
+                  nsIDNSListener *aListener, nsIEventTarget *target);
+  virtual ~DNSRequestChild() {}
+
+  void AddIPDLReference() {
+    AddRef();
+  }
+  void ReleaseIPDLReference() {
+    // we don't need an 'mIPCOpen' variable until/unless we add calls that might
+    // try to send IPDL msgs to parent after ReleaseIPDLReference is called
+    // (when IPDL channel torn down).
+    Release();
+  }
+
+  // Sends IPDL request to parent
+  void StartRequest();
+  void CallOnLookupComplete();
+
+private:
+  virtual bool Recv__delete__(const DNSRequestResponse& reply) MOZ_OVERRIDE;
+
+  nsCOMPtr<nsIDNSListener>  mListener;
+  nsCOMPtr<nsIEventTarget>  mTarget;
+  nsCOMPtr<nsIDNSRecord>    mResultRecord;
+  nsresult                  mResultStatus;
+  nsCString                 mHost;
+  uint16_t                  mFlags;
+};
+
+} // namespace net
+} // namespace mozilla
+#endif // mozilla_net_DNSRequestChild_h
