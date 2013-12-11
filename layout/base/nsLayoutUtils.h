ee55ed090166b4e72b0c0d75c7593c9ad0bf8862
Ryan VanderMeulen <ryanvm@gmail.com>
2013-12-11 18:47:03 -0500

    Backed out changeset d8fb025ca7d2 (bug 948531) for suspicion of causing OSX 10.6 debug mochitest-5 orange. CLOSED TREE

--- layout/base/nsLayoutUtils.h
+++ layout/base/nsLayoutUtils.h
@@ -70,12 +70,6 @@ class HTMLVideoElement;
 namespace layers {
 class Layer;
 }
-
-template <class AnimationsOrTransitions>
-extern AnimationsOrTransitions* HasAnimationOrTransition(nsIContent* aContent,
-                                                         nsIAtom* aAnimationProperty,
-                                                         nsCSSProperty aProperty);
-
 } // namespace mozilla
 
 /**
