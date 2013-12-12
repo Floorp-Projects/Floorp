fdf487effce6950a047ad50d0c44e79f59c6f6a9
Matt Brubeck <mbrubeck@mozilla.com>
2013-12-11 16:23:36 -0800

    Bug 946296 - Add a pref to completely disable add-on providers in Metro [r=Unfocused a=lsblakk]

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_default_providers_pref.js
@@ -0,0 +1,13 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Tests the extensions.defaultProviders.enabled pref which turns
+// off the default XPIProvider and LightweightThemeManager.
+
+function run_test() {
+  Services.prefs.setBoolPref("extensions.defaultProviders.enabled", false);
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+  startupManager();
+  do_check_false(AddonManager.isInstallEnabled("application/x-xpinstall"));
+  Services.prefs.clearUserPref("extensions.defaultProviders.enabled");
+}
