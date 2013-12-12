070b7335273ff4617009506c247f96564769bb34
Jon Coppeard <jcoppeard@mozilla.com>
2013-12-12 11:22:00 +0000

    Bug 948423 - Use relookupOrAdd() to insert into has type representation hash map following possible mutation by GC r=sfink

---
+++ js/src/jit-test/tests/gc/bug-948423.js
@@ -0,0 +1,20 @@
+var ArrayType = TypedObject.ArrayType;
+var StructType = TypedObject.StructType;
+var uint8 = TypedObject.uint8;
+var uint32 = TypedObject.uint32;
+var ObjectType = TypedObject.Object;
+function runTests() {
+  (function DimensionLinkedToUndimension() {
+    var UintsA = uint32.array();
+    var FiveUintsA = UintsA.dimension(5);
+    var FiveUintsB = uint32.array(5);
+    assertEq(true, 
+	FiveUintsA.equivalent(FiveUintsB)
+	);
+  })();
+  (function PrototypeHierarchy() {
+    schedulegc(3);
+    var Uint8s = uint8.array();
+  })();
+}
+runTests();
