05a63386f2ae5583f67f68ba947acc24687832a9
Lucas Rocha <lucasr@mozilla.com>
2013-12-12 12:02:51 +0000

    Bug 942231 - Use 'page' terminology in HomePager instead of 'tab' (r=margaret)

--- mobile/android/base/home/HomeAdapter.java
+++ mobile/android/base/home/HomeAdapter.java
@@ -21,22 +21,22 @@ import java.util.EnumMap;
 class HomeAdapter extends FragmentStatePagerAdapter {
 
     private final Context mContext;
-    private final ArrayList<TabInfo> mTabs;
+    private final ArrayList<PageInfo> mPageInfos;
     private final EnumMap<Page, Fragment> mPages;
 
-    private OnAddTabListener mAddTabListener;
+    private OnAddPageListener mAddPageListener;
 
-    interface OnAddTabListener {
-        public void onAddTab(String title);
+    interface OnAddPageListener {
+        public void onAddPage(String title);
     }
 
-    final class TabInfo {
+    final class PageInfo {
         private final Page page;
         private final Class<?> clss;
         private final Bundle args;
         private final String title;
 
-        TabInfo(Page page, Class<?> clss, Bundle args, String title) {
+        PageInfo(Page page, Class<?> clss, Bundle args, String title) {
             this.page = page;
             this.clss = clss;
             this.args = args;
@@ -49,31 +49,31 @@ class HomeAdapter extends FragmentStatePagerAdapter {
 
         mContext = context;
 
-        mTabs = new ArrayList<TabInfo>();
+        mPageInfos = new ArrayList<PageInfo>();
         mPages = new EnumMap<Page, Fragment>(Page.class);
     }
 
     @Override
     public int getCount() {
-        return mTabs.size();
+        return mPageInfos.size();
     }
 
     @Override
     public Fragment getItem(int position) {
-        TabInfo info = mTabs.get(position);
+        PageInfo info = mPageInfos.get(position);
         return Fragment.instantiate(mContext, info.clss.getName(), info.args);
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        TabInfo info = mTabs.get(position);
+        PageInfo info = mPageInfos.get(position);
         return info.title.toUpperCase();
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         Fragment fragment = (Fragment) super.instantiateItem(container, position);
-        mPages.put(mTabs.get(position).page, fragment);
+        mPages.put(mPageInfos.get(position).page, fragment);
 
         return fragment;
     }
@@ -81,36 +81,36 @@ class HomeAdapter extends FragmentStatePagerAdapter {
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
         super.destroyItem(container, position, object);
-        mPages.remove(mTabs.get(position).page);
+        mPages.remove(mPageInfos.get(position).page);
     }
 
-    public void setOnAddTabListener(OnAddTabListener listener) {
-        mAddTabListener = listener;
+    public void setOnAddPageListener(OnAddPageListener listener) {
+        mAddPageListener = listener;
     }
 
-    public void addTab(Page page, Class<?> clss, Bundle args, String title) {
-        addTab(-1, page, clss, args, title);
+    public void addPage(Page page, Class<?> clss, Bundle args, String title) {
+        addPage(-1, page, clss, args, title);
     }
 
-    public void addTab(int index, Page page, Class<?> clss, Bundle args, String title) {
-        TabInfo info = new TabInfo(page, clss, args, title);
+    public void addPage(int index, Page page, Class<?> clss, Bundle args, String title) {
+        PageInfo info = new PageInfo(page, clss, args, title);
 
         if (index >= 0) {
-            mTabs.add(index, info);
+            mPageInfos.add(index, info);
         } else {
-            mTabs.add(info);
+            mPageInfos.add(info);
         }
 
         notifyDataSetChanged();
 
-        if (mAddTabListener != null) {
-            mAddTabListener.onAddTab(title);
+        if (mAddPageListener != null) {
+            mAddPageListener.onAddPage(title);
         }
     }
 
     public int getItemPosition(Page page) {
-        for (int i = 0; i < mTabs.size(); i++) {
-            TabInfo info = mTabs.get(i);
+        for (int i = 0; i < mPageInfos.size(); i++) {
+            PageInfo info = mPageInfos.get(i);
             if (info.page == page) {
                 return i;
             }
@@ -120,13 +120,13 @@ class HomeAdapter extends FragmentStatePagerAdapter {
     }
 
     public Page getPageAtPosition(int position) {
-        TabInfo info = mTabs.get(position);
+        PageInfo info = mPageInfos.get(position);
         return info.page;
     }
 
     public void setCanLoadHint(boolean canLoadHint) {
         // Update fragment arguments for future instances
-        for (TabInfo info : mTabs) {
+        for (PageInfo info : mPageInfos) {
             info.args.putBoolean(HomePager.CAN_LOAD_ARG, canLoadHint);
         }
 
