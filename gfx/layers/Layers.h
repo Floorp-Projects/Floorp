ad5753efbe4dcc3f0b3cda1f7d7b2288ee85df84
Kartikaya Gupta <kgupta@mozilla.com>
2013-12-12 13:34:50 -0500

    Bug 949310. r=BenWa, a=lsblakk

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -973,6 +973,7 @@ public:
   }
 
   enum ScrollDirection {
+    NONE,
     VERTICAL,
     HORIZONTAL
   };
@@ -984,11 +985,9 @@ public:
    */
   void SetScrollbarData(FrameMetrics::ViewID aScrollId, ScrollDirection aDir)
   {
-    if (mIsScrollbar ||
-        mScrollbarTargetId != aScrollId ||
+    if (mScrollbarTargetId != aScrollId ||
         mScrollbarDirection != aDir) {
       MOZ_LAYERS_LOG_IF_SHADOWABLE(this, ("Layer::Mutated(%p) ScrollbarData", this));
-      mIsScrollbar = true;
       mScrollbarTargetId = aScrollId;
       mScrollbarDirection = aDir;
       Mutated();
@@ -1019,7 +1018,6 @@ public:
   FrameMetrics::ViewID GetStickyScrollContainerId() { return mStickyPositionData->mScrollId; }
   const LayerRect& GetStickyScrollRangeOuter() { return mStickyPositionData->mOuter; }
   const LayerRect& GetStickyScrollRangeInner() { return mStickyPositionData->mInner; }
-  bool GetIsScrollbar() { return mIsScrollbar; }
   FrameMetrics::ViewID GetScrollbarTargetContainerId() { return mScrollbarTargetId; }
   ScrollDirection GetScrollbarDirection() { return mScrollbarDirection; }
   Layer* GetMaskLayer() const { return mMaskLayer; }
@@ -1393,7 +1391,6 @@ protected:
     LayerRect mInner;
   };
   nsAutoPtr<StickyPositionData> mStickyPositionData;
-  bool mIsScrollbar;
   FrameMetrics::ViewID mScrollbarTargetId;
   ScrollDirection mScrollbarDirection;
   DebugOnly<uint32_t> mDebugColorIndex;
