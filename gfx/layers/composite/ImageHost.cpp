3bb6a6d1fe632f86db5c9a8bf971162abac8f33d
Nicolas Silva <nical@mozilla.com>
2013-12-11 20:44:49 -0500

    Bug 897452 - Part 4 - Make sure we release all GL resources asap when shutting down the communication channel - r=bjacob

--- gfx/layers/composite/ImageHost.cpp
+++ gfx/layers/composite/ImageHost.cpp
@@ -38,6 +38,12 @@ ImageHost::~ImageHost() {}
 void
 ImageHost::UseTextureHost(TextureHost* aTexture)
 {
+  if (mFrontBuffer) {
+    // XXX - When we implement sharing textures between several compositables
+    // we will need to not remove the compositor if there is another compositable
+    // using the texture.
+    mFrontBuffer->SetCompositor(nullptr);
+  }
   CompositableHost::UseTextureHost(aTexture);
   mFrontBuffer = aTexture;
 }
@@ -148,6 +154,15 @@ ImageHost::Composite(EffectChain& aEffectChain,
                                      aTransform);
   }
   mFrontBuffer->Unlock();
+}
+
+void
+ImageHost::SetCompositor(Compositor* aCompositor)
+{
+  if (mFrontBuffer && mCompositor != aCompositor) {
+    mFrontBuffer->SetCompositor(aCompositor);
+  }
+  CompositableHost::SetCompositor(aCompositor);
 }
 
 void
