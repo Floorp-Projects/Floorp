73e5106d95ceda23d5d149fb2082ed165de65cd7
Blair McBride <bmcbride@mozilla.com>
2013-12-12 02:08:00 +0000

    Bug 926712 - Use WebIDL to expose InstallTrigger, r=Mossop,bholley

---
+++ toolkit/mozapps/extensions/internal/Content.js
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+(function() {
+
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+
+let {Services} = Cu.import("resource://gre/modules/Services.jsm", {});
+
+let nsIFile = Components.Constructor("@mozilla.org/file/local;1", "nsIFile",
+                                     "initWithPath");
+
+const MSG_JAR_FLUSH = "AddonJarFlush";
+
+
+try {
+  if (Services.appinfo.processType !== Services.appinfo.PROCESS_TYPE_DEFAULT) {
+  // Propagate JAR cache flush notifications across process boundaries.
+    addMessageListener(MSG_JAR_FLUSH, function jar_flushMessageListener(message) {
+      let file = new nsIFile(message.data);
+      Services.obs.notifyObservers(file, "flush-cache-entry", null);
+    });
+  }
+} catch(e) {
+  Cu.reportError(e);
+}
+
+})();
