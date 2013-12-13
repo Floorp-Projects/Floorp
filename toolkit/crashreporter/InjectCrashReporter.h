68090aef2315ef95df51e144bf5006ddfb71a588
Ehsan Akhgari <ehsan@mozilla.com>
2013-12-13 09:55:39 -0500

    Bug 940821 - Build crashreporter in unified mode; r=ted

--- toolkit/crashreporter/InjectCrashReporter.h
+++ toolkit/crashreporter/InjectCrashReporter.h
@@ -3,6 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef InjectCrashReporter_h
+#define InjectCrashReporter_h
+
 #include "nsThreadUtils.h"
 #include <windows.h>
 
@@ -21,3 +24,5 @@ private:
 };
   
 } // Namespace mozilla
+
+#endif
