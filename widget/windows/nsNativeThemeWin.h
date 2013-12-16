3502047c0a428413f8d85911fbe273314955df3f
Ehsan Akhgari <ehsan@mozilla.com>
2013-12-15 19:00:54 -0500

    Bug 950508 - Build widget/windows in unified mode; r=roc

--- widget/windows/nsNativeThemeWin.h
+++ widget/windows/nsNativeThemeWin.h
@@ -4,6 +4,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef nsNativeThemeWin_h
+#define nsNativeThemeWin_h
+
 #include "nsITheme.h"
 #include "nsCOMPtr.h"
 #include "nsIAtom.h"
@@ -118,3 +121,5 @@ private:
   TimeStamp mProgressDeterminateTimeStamp;
   TimeStamp mProgressIndeterminateTimeStamp;
 };
+
+#endif
