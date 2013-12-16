4fa6bafdfba573a16d052cfc0fa2c9dc10b54769
Peiyong Lin <pylaurent1314@gmail.com>
2013-12-16 18:00:34 -0500

    Bug 946754 - [Australis] Switch CustomizableUI tests from using homebrewed test runner to built-in add_task. r=mconley

--- browser/components/customizableui/test/browser_892955_isWidgetRemovable_for_removed_widgets.js
+++ browser/components/customizableui/test/browser_892955_isWidgetRemovable_for_removed_widgets.js
@@ -2,36 +2,29 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+"use strict";
+
 const kWidgetId = "test-892955-remove-widget";
 
-let gTests = [
-  {
-    desc: "Removing a destroyed widget should work.",
-    run: function() {
-      let widgetSpec = {
-        id: kWidgetId,
-        defaultArea: CustomizableUI.AREA_NAVBAR
-      };
+// Removing a destroyed widget should work.
+add_task(function() {
+  let widgetSpec = {
+    id: kWidgetId,
+    defaultArea: CustomizableUI.AREA_NAVBAR
+  };
 
-      CustomizableUI.createWidget(widgetSpec);
-      CustomizableUI.destroyWidget(kWidgetId);
-      let noError = true;
-      try {
-        CustomizableUI.removeWidgetFromArea(kWidgetId);
-      } catch (ex) {
-        noError = false;
-        Cu.reportError(ex);
-      }
-      ok(noError, "Shouldn't throw an error removing a destroyed widget.");
-    }
+  CustomizableUI.createWidget(widgetSpec);
+  CustomizableUI.destroyWidget(kWidgetId);
+  let noError = true;
+  try {
+    CustomizableUI.removeWidgetFromArea(kWidgetId);
+  } catch (ex) {
+    noError = false;
+    Cu.reportError(ex);
   }
-];
+  ok(noError, "Shouldn't throw an error removing a destroyed widget.");
+});
 
-function asyncCleanup() {
+add_task(function asyncCleanup() {
   yield resetCustomization();
-}
-
-function test() {
-  waitForExplicitFinish();
-  runTests(gTests, asyncCleanup);
-}
+});
