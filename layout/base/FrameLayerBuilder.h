54a2b48649f1f6fcb84aa7a86bbf8a0f5225075f
Robert O'Callahan <robert@ocallahan.org>
2013-12-17 01:16:24 +1300

    Bug 945203. Part 8: Add FrameLayerBuilder support for capturing nsDisplayEventRegions data into ThebesLayers. r=mattwoodrow
    --HG--
    extra : rebase_source : e61c1bcfa700ac88523fb34eab7c6afd470d0d68

--- layout/base/FrameLayerBuilder.h
+++ layout/base/FrameLayerBuilder.h
@@ -13,6 +13,7 @@
 #include "nsIFrame.h"
 #include "ImageLayers.h"
 #include "DisplayItemClip.h"
+#include "mozilla/layers/LayersTypes.h"
 
 class nsDisplayListBuilder;
 class nsDisplayList;
@@ -30,6 +31,7 @@ class ThebesLayer;
 
 class FrameLayerBuilder;
 class LayerManagerData;
+class ThebesLayerData;
 
 enum LayerState {
   LAYER_NONE,
@@ -144,6 +146,7 @@ public:
   typedef layers::ImageLayer ImageLayer;
   typedef layers::LayerManager LayerManager;
   typedef layers::BasicLayerManager BasicLayerManager;
+  typedef layers::EventRegions EventRegions;
 
   FrameLayerBuilder() :
     mRetainingManager(nullptr),
@@ -161,7 +164,8 @@ public:
 
   static void Shutdown();
 
-  void Init(nsDisplayListBuilder* aBuilder, LayerManager* aManager);
+  void Init(nsDisplayListBuilder* aBuilder, LayerManager* aManager,
+            ThebesLayerData* aLayerData = nullptr);
 
   /**
    * Call this to notify that we have just started a transaction on the
@@ -287,7 +291,7 @@ public:
    * aItem must have an underlying frame.
    * @param aTopLeft offset from active scrolled root to reference frame
    */
-  void AddThebesDisplayItem(ThebesLayer* aLayer,
+  void AddThebesDisplayItem(ThebesLayerData* aLayer,
                             nsDisplayItem* aItem,
                             const DisplayItemClip& aClip,
                             nsIFrame* aContainerLayerFrame,
@@ -582,6 +586,11 @@ public:
     return mThebesLayerItems.GetEntry(aLayer);
   }
 
+  ThebesLayerData* GetContainingThebesLayerData()
+  {
+    return mContainingThebesLayer;
+  }
+
 protected:
   void RemoveThebesItemsAndOwnerDataForLayerSubtree(Layer* aLayer,
                                                     bool aRemoveThebesItems,
@@ -621,6 +630,13 @@ protected:
    * clipping data) to be rendered in the layer.
    */
   nsTHashtable<ThebesLayerItemsEntry> mThebesLayerItems;
+
+  /**
+   * When building layers for an inactive layer, this is where the
+   * inactive layer will be placed.
+   */
+  ThebesLayerData*                    mContainingThebesLayer;
+
   /**
    * Saved generation counter so we can detect DOM changes.
    */
