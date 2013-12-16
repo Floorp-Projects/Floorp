35742ed5cc11446a0d655cc236209886d0991e14
Edgar Chen <echen@mozilla.com>
2013-12-16 15:53:02 -0500

    Bug 946753 - Marionette doesn't report error if the test script in manifest is missing. r=jgriffin, a=test-only

--- testing/marionette/client/marionette/runtests.py
+++ testing/marionette/client/marionette/runtests.py
@@ -616,8 +616,9 @@ class MarionetteTestRunner(object):
             manifest = TestManifest()
             manifest.read(filepath)
 
-            all_tests = manifest.active_tests(disabled=False)
-            manifest_tests = manifest.active_tests(disabled=False,
+            all_tests = manifest.active_tests(exists=False, disabled=False)
+            manifest_tests = manifest.active_tests(exists=False,
+                                                   disabled=False,
                                                    device=self.device,
                                                    app=self.appName)
             skip_tests = list(set([x['path'] for x in all_tests]) -
