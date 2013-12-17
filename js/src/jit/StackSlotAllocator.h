f0897baea41e903ed0185b00ca18d052550f3854
Dan Gohman <sunfish@google.com>
2013-12-17 08:46:37 -0800

    Bug 950703 - IonMonkey: Use 32-bit stack slots for 32-bit floats on 32-bit platforms. r=jandem

--- js/src/jit/StackSlotAllocator.h
+++ js/src/jit/StackSlotAllocator.h
@@ -18,18 +18,11 @@ class StackSlotAllocator
     js::Vector<uint32_t, 4, SystemAllocPolicy> doubleSlots;
     uint32_t height_;
 
-  public:
-    StackSlotAllocator() : height_(0)
-    { }
-
     void freeSlot(uint32_t index) {
         normalSlots.append(index);
     }
     void freeDoubleSlot(uint32_t index) {
         doubleSlots.append(index);
-    }
-    void freeValueSlot(uint32_t index) {
-        freeDoubleSlot(index);
     }
 
     uint32_t allocateDoubleSlot() {
@@ -50,9 +43,47 @@ class StackSlotAllocator
         }
         return ++height_;
     }
-    uint32_t allocateValueSlot() {
-        return allocateDoubleSlot();
+
+  public:
+    StackSlotAllocator() : height_(0)
+    { }
+
+    void freeSlot(LDefinition::Type type, uint32_t index) {
+        switch (type) {
+          case LDefinition::FLOAT32:
+          case LDefinition::OBJECT:
+          case LDefinition::SLOTS:
+#ifdef JS_PUNBOX64
+          case LDefinition::BOX:
+#endif
+          case LDefinition::GENERAL: return freeSlot(index);
+#ifdef JS_NUNBOX32
+          case LDefinition::TYPE:
+          case LDefinition::PAYLOAD:
+#endif
+          case LDefinition::DOUBLE:  return freeDoubleSlot(index);
+          default: MOZ_ASSUME_UNREACHABLE("Unknown slot type");
+        }
     }
+
+    uint32_t allocateSlot(LDefinition::Type type) {
+        switch (type) {
+          case LDefinition::FLOAT32:
+          case LDefinition::OBJECT:
+          case LDefinition::SLOTS:
+#ifdef JS_PUNBOX64
+          case LDefinition::BOX:
+#endif
+          case LDefinition::GENERAL: return allocateSlot();
+#ifdef JS_NUNBOX32
+          case LDefinition::TYPE:
+          case LDefinition::PAYLOAD:
+#endif
+          case LDefinition::DOUBLE:  return allocateDoubleSlot();
+          default: MOZ_ASSUME_UNREACHABLE("Unknown slot type");
+        }
+    }
+
     uint32_t stackHeight() const {
         return height_;
     }
