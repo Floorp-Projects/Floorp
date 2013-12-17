e7bcad4f1e6fec012727cea168f84da25bffbcaf
Mats Palmgren <matspal@gmail.com>
2013-12-17 13:30:24 +0000

    Bug 950444 - Fix some indentation and code style issues in setRangeText.  r=ehsan

--- content/html/content/src/HTMLTextAreaElement.cpp
+++ content/html/content/src/HTMLTextAreaElement.cpp
@@ -939,14 +939,14 @@ HTMLTextAreaElement::SetRangeText(const nsAString& aReplacement,
   }
 
   if (aSelectionStart == -1 && aSelectionEnd == -1) {
-      aRv = GetSelectionRange(&aSelectionStart, &aSelectionEnd);
-      if (aRv.Failed()) {
-        if (mState.IsSelectionCached()) {
-          aSelectionStart = mState.GetSelectionProperties().mStart;
-          aSelectionEnd = mState.GetSelectionProperties().mEnd;
-          aRv = NS_OK;
-        }
+    aRv = GetSelectionRange(&aSelectionStart, &aSelectionEnd);
+    if (aRv.Failed()) {
+      if (mState.IsSelectionCached()) {
+        aSelectionStart = mState.GetSelectionProperties().mStart;
+        aSelectionEnd = mState.GetSelectionProperties().mEnd;
+        aRv = NS_OK;
       }
+    }
   }
 
   if (aStart <= aEnd) {
@@ -976,15 +976,17 @@ HTMLTextAreaElement::SetRangeText(const nsAString& aReplacement,
     break;
     case mozilla::dom::SelectionMode::Preserve:
     {
-      if ((uint32_t)aSelectionStart > aEnd)
+      if ((uint32_t)aSelectionStart > aEnd) {
         aSelectionStart += delta;
-      else if ((uint32_t)aSelectionStart > aStart)
-       aSelectionStart = aStart;
+      } else if ((uint32_t)aSelectionStart > aStart) {
+        aSelectionStart = aStart;
+      }
 
-      if ((uint32_t)aSelectionEnd > aEnd)
+      if ((uint32_t)aSelectionEnd > aEnd) {
         aSelectionEnd += delta;
-      else if ((uint32_t)aSelectionEnd > aStart)
+      } else if ((uint32_t)aSelectionEnd > aStart) {
         aSelectionEnd = newEnd;
+      }
     }
     break;
   }
