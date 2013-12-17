b7ccb495286721616a1d884d6c9e261c8a75cf02
Olli Pettay <Olli.Pettay@helsinki.fi>
2013-12-17 15:13:43 +0200

    Bug 950337 - Random failures in privatebrowsing/test/browser/browser_privatebrowsing_cache.js, r=ehsan
    --HG--
    extra : rebase_source : e5a5ce1ce2ef607429b354b2605159d1d02aec17

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_cache.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_cache.js
@@ -103,9 +103,7 @@ function getStorageEntryCount(device, goon) {
 }
 
 function get_cache_for_private_window () {
-  let win = OpenBrowserWindow({private: true});
-  win.addEventListener("load", function () {
-    win.removeEventListener("load", arguments.callee, false);
+  let win = whenNewWindowLoaded({private: true}, function() {
 
     executeSoon(function() {
 
@@ -135,5 +133,5 @@ function get_cache_for_private_window () {
         });
       }, true);
     });
-  }, false);
+  });
 }
