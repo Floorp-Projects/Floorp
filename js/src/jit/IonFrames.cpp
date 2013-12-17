113bf053d0b54dac961eb4b1564ec5d5f9e95fa2
Dan Gohman <sunfish@google.com>
2013-12-17 08:46:37 -0800

    Bug 950703 - SpiderMonkey: Merge x86 and ARM's IonFrames.h files into a single file. r=jandem

--- js/src/jit/IonFrames.cpp
+++ js/src/jit/IonFrames.cpp
@@ -1750,5 +1750,26 @@ IonFrameIterator::dump() const
     fputc('\n', stderr);
 }
 
+IonJSFrameLayout *
+InvalidationBailoutStack::fp() const
+{
+    return (IonJSFrameLayout *) (sp() + ionScript_->frameSize());
+}
+
+void
+InvalidationBailoutStack::checkInvariants() const
+{
+#ifdef DEBUG
+    IonJSFrameLayout *frame = fp();
+    CalleeToken token = frame->calleeToken();
+    JS_ASSERT(token);
+
+    uint8_t *rawBase = ionScript()->method()->raw();
+    uint8_t *rawLimit = rawBase + ionScript()->method()->instructionsSize();
+    uint8_t *osiPoint = osiPointReturnAddress();
+    JS_ASSERT(rawBase <= osiPoint && osiPoint <= rawLimit);
+#endif
+}
+
 } // namespace jit
 } // namespace js
