3cff8334336fb86bf06cd05f0cd9e0343faa83e4
Dave Hunt <dhunt@mozilla.com>
2013-12-18 17:30:25 +0000

    Bug 867600 - Change hardcoded sleeps into waits. r=mdas, a=test-only

--- testing/marionette/client/marionette/tests/unit/test_submit.py
+++ testing/marionette/client/marionette/tests/unit/test_submit.py
@@ -2,9 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import time
-from marionette_test import MarionetteTestCase
+from by import By
 from errors import NoSuchElementException
+from marionette_test import MarionetteTestCase
+from wait import Wait
 
 
 class TestSubmit(MarionetteTestCase):
@@ -12,27 +13,21 @@ class TestSubmit(MarionetteTestCase):
     def test_should_be_able_to_submit_forms(self):
         test_html = self.marionette.absolute_url("formPage.html")
         self.marionette.navigate(test_html)
-        self.marionette.find_element("name", "login").submit()
+        self.marionette.find_element(By.NAME, "login").submit()
         self.assertEqual(self.marionette.title, "We Arrive Here")
 
     def test_should_submit_a_form_when_any_input_element_within_that_form_is_submitted(self):
         test_html = self.marionette.absolute_url("formPage.html")
         self.marionette.navigate(test_html)
-        self.marionette.find_element("id", "checky").submit()
-        for i in range(5):
-            try:
-                self.marionette.find_element('id', 'email')
-            except NoSuchElementException:
-                time.sleep(1)
+        self.marionette.find_element(By.ID, "checky").submit()
+        Wait(self.marionette, ignored_exceptions=NoSuchElementException).until(
+            lambda m: m.find_element(By.ID, 'email'))
         self.assertEqual(self.marionette.title, "We Arrive Here")
 
     def test_should_submit_a_form_when_any_element_wihin_that_form_is_submitted(self):
         test_html = self.marionette.absolute_url("formPage.html")
         self.marionette.navigate(test_html)
-        self.marionette.find_element("xpath", "//form/p").submit()
-        for i in range(5):
-            try:
-                self.marionette.find_element('id', 'email')
-            except NoSuchElementException:
-                time.sleep(1)
+        self.marionette.find_element(By.XPATH, "//form/p").submit()
+        Wait(self.marionette, ignored_exceptions=NoSuchElementException).until(
+            lambda m: m.find_element(By.ID, 'email'))
         self.assertEqual(self.marionette.title, "We Arrive Here")
