9b9bddbf5b06600071daed9e6f5492de49e3a9a6
Mark Hammond <mhammond@skippinet.com.au>
2013-12-19 16:38:35 +1100

    Have the sync setup dialog do the right thing if identity.fxaccounts.enabled=true

--- browser/base/content/browser-syncui.js
+++ browser/base/content/browser-syncui.js
@@ -279,13 +279,20 @@ let gSyncUI = {
    */
 
   openSetup: function SUI_openSetup(wizardType) {
-    let win = Services.wm.getMostRecentWindow("Weave:AccountSetup");
-    if (win)
-      win.focus();
-    else {
-      window.openDialog("chrome://browser/content/sync/setup.xul",
-                        "weaveSetup", "centerscreen,chrome,resizable=no",
-                        wizardType);
+    let xps = Components.classes["@mozilla.org/weave/service;1"]
+                                .getService(Components.interfaces.nsISupports)
+                                .wrappedJSObject;
+    if (xps.fxAccountsEnabled) {
+      switchToTabHavingURI("about:accounts", true);
+    } else {
+      let win = Services.wm.getMostRecentWindow("Weave:AccountSetup");
+      if (win)
+        win.focus();
+      else {
+        window.openDialog("chrome://browser/content/sync/setup.xul",
+                          "weaveSetup", "centerscreen,chrome,resizable=no",
+                          wizardType);
+      }
     }
   },
 
