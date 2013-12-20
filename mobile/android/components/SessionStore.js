7111ab80f3147531ac4002301491cc38007570e1
Mark Finkle <mfinkle@mozilla.com>
2013-12-20 16:41:11 -0500

    Bug 952473 - SessionStore should only watch for top-level pageshow events. r=wesj, a=bajaj

--- mobile/android/components/SessionStore.js
+++ mobile/android/components/SessionStore.js
@@ -223,7 +223,9 @@ SessionStore.prototype = {
       }
       case "pageshow": {
         let browser = aEvent.currentTarget;
-        this.onTabLoad(window, browser, aEvent.persisted);
+        // Top-level changes only
+        if (aEvent.originalTarget == browser.contentDocument)
+          this.onTabLoad(window, browser, aEvent.persisted);
         break;
       }
     }
