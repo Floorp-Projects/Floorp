8c2572ef3cc15e06015e6735c1e14f1d3274ba4e
Ehsan Akhgari <ehsan@mozilla.com>
2013-12-20 13:05:29 -0500

    Backed out 4 changesets (bug 940708) for ASAN bustage
    Backed out changeset 8b081bf3d16c (bug 940708)
    Backed out changeset 8d1da29cee34 (bug 940708)
    Backed out changeset aedcac877ab0 (bug 940708)
    Backed out changeset d4aec392a165 (bug 940708)
    
    --HG--
    extra : rebase_source : dec5c491a2233dfe7fe2153559e01313d3583a33

--- python/mozbuild/mozbuild/frontend/gyp_reader.py
+++ python/mozbuild/mozbuild/frontend/gyp_reader.py
@@ -14,7 +14,6 @@ from .sandbox import (
     GlobalNamespace,
 )
 from .sandbox_symbols import VARIABLES
-from .reader import SandboxValidationError
 
 # Define this module as gyp.generator.mozbuild so that gyp can use it
 # as a generator under the name "mozbuild".
@@ -72,7 +71,7 @@ def encode(value):
     return value
 
 
-def read_from_gyp(config, path, output, vars, non_unified_sources = set()):
+def read_from_gyp(config, path, output, vars):
     """Read a gyp configuration and emits GypSandboxes for the backend to
     process.
 
@@ -83,7 +82,6 @@ def read_from_gyp(config, path, output, vars, non_unified_sources = set()):
     """
 
     time_start = time.time()
-    all_sources = set()
 
     # gyp expects plain str instead of unicode. The frontend code gives us
     # unicode strings, so convert them.
@@ -162,18 +160,11 @@ def read_from_gyp(config, path, output, vars, non_unified_sources = set()):
                 name = name[3:]
             # The sandbox expects an unicode string.
             sandbox['LIBRARY_NAME'] = name.decode('utf-8')
-            # gyp files contain headers and asm sources in sources lists.
-            sources = set(mozpath.normpath(mozpath.join(sandbox['SRCDIR'], f))
-                for f in spec.get('sources', [])
-                if mozpath.splitext(f)[-1] != '.h')
-            asm_sources = set(f for f in sources if f.endswith('.S'))
-
-            unified_sources = sources - non_unified_sources - asm_sources
-            sources -= unified_sources
-            all_sources |= sources
             # The sandbox expects alphabetical order when adding sources
-            sandbox['SOURCES'] = alphabetical_sorted(sources)
-            sandbox['UNIFIED_SOURCES'] = alphabetical_sorted(unified_sources)
+            sources = alphabetical_sorted(spec.get('sources', []))
+            # gyp files contain headers in sources lists.
+            sandbox['SOURCES'] = \
+                [f for f in sources if mozpath.splitext(f)[-1] != '.h']
 
             for define in target_conf.get('defines', []):
                 if '=' in define:
@@ -197,7 +188,3 @@ def read_from_gyp(config, path, output, vars, non_unified_sources = set()):
         sandbox.execution_time = time.time() - time_start
         yield sandbox
         time_start = time.time()
-#    remainder = non_unified_sources - all_sources
-#    if remainder:
-#        raise SandboxValidationError('%s defined as non_unified_source, but is '
-#            'not defined as a source' % ', '.join(remainder))
--- python/mozbuild/mozbuild/frontend/sandbox_symbols.py
+++ python/mozbuild/mozbuild/frontend/sandbox_symbols.py
@@ -581,7 +581,6 @@ VARIABLES = {
             'variables': dict,
             'input': unicode,
             'sandbox_vars': dict,
-            'non_unified_sources': StrictOrderingOnAppendList,
         }), list,
         """Defines a list of object directories handled by gyp configurations.
 
@@ -596,9 +595,6 @@ VARIABLES = {
             - sandbox_vars, a dictionary containing variables and values to
               pass to the mozbuild processor on top of those derived from gyp
               configuration.
-            - non_unified_sources, a list containing sources files, relative to
-              the current moz.build, that should be excluded from source file
-              unification.
 
         Typical use looks like:
             GYP_DIRS += ['foo', 'bar']
