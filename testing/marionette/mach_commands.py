ce323ccd988caea7bb72fb0cede27ff7e5e2a0fe
Dave Hunt <dhunt@mozilla.com>
2013-12-20 13:12:40 +0000

    Bug 950641 - Automatically use the appropriate GeckoInstance class. r=jgriffin

--- testing/marionette/mach_commands.py
+++ testing/marionette/mach_commands.py
@@ -55,8 +55,6 @@ def run_marionette(tests, b2g_path=None, emulator=None, testtype=None,
     else:
         options.bin = bin
         path, exe = os.path.split(options.bin)
-        if 'b2g' in exe:
-            options.app = 'b2gdesktop'
 
     options.address = address
 
