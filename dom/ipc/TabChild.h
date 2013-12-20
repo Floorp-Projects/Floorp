3cb843090b7598c4760796b32aaa83125b6df185
Vivien Nicolas <21@vingtetun.org>
2013-12-20 10:11:01 +0100

    Bug 950300 - Cancel APZC panning is the first touchmove is preventDefault'ed. r=kats, a=1.3+

--- dom/ipc/TabChild.h
+++ dom/ipc/TabChild.h
@@ -498,6 +498,7 @@ private:
     ScreenOrientation mOrientation;
     bool mUpdateHitRegion;
     bool mContextMenuHandled;
+    bool mWaitingTouchListeners;
 
     DISALLOW_EVIL_CONSTRUCTORS(TabChild);
 };
