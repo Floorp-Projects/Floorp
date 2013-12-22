252332f9d779541d82b87b9e17b57a4d5a978443
Jonathan Kew <jkew@mozilla.com>
2013-12-22 19:25:53 +0000

    bug 950590 - part 2 - make SetUserFontSet support updating a gfxFontGroup's user font set on the fly, and use this in canvas rendering context. r=roc a=lsblakk

--- gfx/thebes/gfxFont.h
+++ gfx/thebes/gfxFont.h
@@ -3513,6 +3513,9 @@ public:
     gfxTextPerfMetrics *GetTextPerfMetrics() { return mTextPerf; }
     void SetTextPerfMetrics(gfxTextPerfMetrics *aTextPerf) { mTextPerf = aTextPerf; }
 
+    // This will call UpdateFontList() if the user font set is changed.
+    void SetUserFontSet(gfxUserFontSet *aUserFontSet);
+
     // If there is a user font set, check to see whether the font list or any
     // caches need updating.
     virtual void UpdateFontList();
@@ -3568,10 +3571,6 @@ protected:
     gfxTextRun *MakeSpaceTextRun(const Parameters *aParams, uint32_t aFlags);
     gfxTextRun *MakeBlankTextRun(uint32_t aLength,
                                  const Parameters *aParams, uint32_t aFlags);
-
-    // Used for construction/destruction.  Not intended to change the font set
-    // as invalidation of font lists and caches is not considered.
-    void SetUserFontSet(gfxUserFontSet *aUserFontSet);
 
     // Initialize the list of fonts
     void BuildFontList();
