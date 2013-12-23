3cae089e8654649faf51cd98a9d3b52b8d8ce17e
Bevis Tseng <btseng@mozilla.com>
2013-12-23 11:31:43 +0800

    Bug 951999 - [B2G][DSDS] Need to add SIM id into the WAP Push message. r=gene

--- dom/wappush/src/gonk/WapPushManager.js
+++ dom/wappush/src/gonk/WapPushManager.js
@@ -136,7 +136,8 @@ this.WapPushManager = {
       sender:         sender,
       contentType:    msg.contentType,
       content:        msg.content,
-      authInfo:       authInfo
+      authInfo:       authInfo,
+      serviceId:      options.serviceId
     });
   },
 
