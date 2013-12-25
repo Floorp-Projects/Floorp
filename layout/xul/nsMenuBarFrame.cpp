a87d10337bb7c7336f33a0a9f7b0ff0ff6fccd48
Benjamin Peterson <benjamin@python.org>
2013-12-24 22:12:43 -0600

    Backed out changeset 84a3ca9be2eb (bug 940040) for linux bustage

--- layout/xul/nsMenuBarFrame.cpp
+++ layout/xul/nsMenuBarFrame.cpp
@@ -149,7 +149,12 @@ nsMenuBarFrame::ToggleMenuActiveState()
     if (firstFrame) {
       // Activate the menu bar
       SetActive(true);
+
+#if (MOZ_WIDGET_GTK == 2)
+      firstFrame->OpenMenu(true);
+#else
       firstFrame->SelectMenu(true);
+#endif
       
       // Track this item for keyboard navigation.
       mCurrentMenu = firstFrame;
