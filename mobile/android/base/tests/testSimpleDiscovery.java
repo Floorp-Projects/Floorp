ebb3eaa16ffa87f45be60440ad725e6d782d8f41
Mark Finkle <mfinkle@mozilla.com>
2013-12-27 17:25:45 -0500

    Bug 938571 - Add discovery support for second-screen devices (test) r=rnewman

---
+++ mobile/android/base/tests/testSimpleDiscovery.java
@@ -0,0 +1,10 @@
+package org.mozilla.gecko.tests;
+
+import org.mozilla.gecko.*;
+
+
+public class testSimpleDiscovery extends JavascriptTest {
+    public testSimpleDiscovery() {
+        super("testSimpleDiscovery.js");
+    }
+}
