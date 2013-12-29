011ac0d3fe2319261571eaffd84a2a7e615ad92a
Mark Finkle <mfinkle@mozilla.com>
2013-12-28 22:14:13 -0500

    Bug 952894 - Use existing triggers to clear open search engines state. r=rnewman, a=lsblakk

--- mobile/android/base/toolbar/BrowserToolbar.java
+++ mobile/android/base/toolbar/BrowserToolbar.java
@@ -360,10 +360,7 @@ public class BrowserToolbar extends GeckoRelativeLayout
                         menu.findItem(R.id.add_to_launcher).setVisible(false);
                     }
 
-                    if (!tab.hasFeeds()) {
-                        menu.findItem(R.id.subscribe).setVisible(false);
-                    }
-
+                    menu.findItem(R.id.subscribe).setVisible(tab.hasFeeds());
                     menu.findItem(R.id.add_search_engine).setVisible(tab.hasOpenSearch());
                 } else {
                     // if there is no tab, remove anything tab dependent
