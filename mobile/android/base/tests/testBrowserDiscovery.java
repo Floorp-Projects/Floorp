d7aa154f74c05e1cbdca1560d52523cc132abc52
Mark Finkle <mfinkle@mozilla.com>
2013-12-28 22:15:09 -0500

    Bug 952894 - Browser search and feed discovery tests r=rnewman

---
+++ mobile/android/base/tests/testBrowserDiscovery.java
@@ -0,0 +1,10 @@
+package org.mozilla.gecko.tests;
+
+import org.mozilla.gecko.*;
+
+
+public class testBrowserDiscovery extends JavascriptTest {
+    public testBrowserDiscovery() {
+        super("testBrowserDiscovery.js");
+    }
+}
