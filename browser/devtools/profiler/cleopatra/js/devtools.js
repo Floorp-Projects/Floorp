88a8f924dd8a792b179994a4864a11eda8319bec
Chris Peterson <cpeterson@mozilla.com>
2013-12-29 20:37:55 -0800

    Bug 955891 - Part 1: Cleopatra: Fix some JS warnings. r=anton

--- browser/devtools/profiler/cleopatra/js/devtools.js
+++ browser/devtools/profiler/cleopatra/js/devtools.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+"use strict";
+
 var gInstanceUID;
 var gParsedQS;
 var gHideSourceLinks;
@@ -95,8 +97,6 @@ window.addEventListener("message", onParentMessage);
  */
 function initUI() {
   gHideSourceLinks = getParam("ext") === "true";
-  gLightMode = true;
-
   gFileList = { profileParsingFinished: function () {} };
   gInfoBar = { display: function () {} };
 
