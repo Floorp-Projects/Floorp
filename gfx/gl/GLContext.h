277b7c507af0bf66aa70f73891c8062841e86c26
Benoit Jacob <bjacob@mozilla.com>
2014-01-02 10:17:28 -0500

    Bug 949816 - Move override-EGLSurface feature out of GLContext - r=jgilbert

--- gfx/gl/GLContext.h
+++ gfx/gl/GLContext.h
@@ -2443,21 +2443,6 @@ public:
     bool IsOwningThreadCurrent();
     void DispatchToOwningThread(nsIRunnable *event);
 
-    /**
-     * Only on EGL.
-     *
-     * If surf is non-null, this sets it to temporarily override this context's
-     * primary surface. This makes this context current against this surface,
-     * and subsequent MakeCurrent calls will continue using this surface as long
-     * as this override is set.
-     *
-     * If surf is null, this removes any previously set override, and makes the
-     * context current again against its primary surface.
-     */
-    virtual void SetEGLSurfaceOverride(EGLSurface surf) {
-        MOZ_CRASH("Must be called against a GLContextEGL.");
-    }
-
     static void PlatformStartup();
 
 public:
