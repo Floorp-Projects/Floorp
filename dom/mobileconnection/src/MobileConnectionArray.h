67b607691200b1cc9d6ebf659624e35255b4688d
Edgar Chen <echen@mozilla.com>
2014-01-02 11:06:17 +0800

    Bug 956655 - Part 2: Move MobileConnection DOM classes to namespacing mozilla::dom. r=smaug

--- dom/mobileconnection/src/MobileConnectionArray.h
+++ dom/mobileconnection/src/MobileConnectionArray.h
@@ -8,13 +8,12 @@
 #define mozilla_dom_network_MobileConnectionArray_h__
 
 #include "nsWrapperCache.h"
-#include "mozilla/dom/network/MobileConnection.h"
+#include "mozilla/dom/MobileConnection.h"
 
 class nsIDOMMozMobileConnection;
 
 namespace mozilla {
 namespace dom {
-namespace network {
 
 class MobileConnectionArray MOZ_FINAL : public nsISupports,
                                         public nsWrapperCache
@@ -57,7 +56,6 @@ private:
   nsTArray<nsRefPtr<MobileConnection>> mMobileConnections;
 };
 
-} // namespace network
 } // namespace dom
 } // namespace mozilla
 
