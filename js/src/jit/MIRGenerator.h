cd0a276d1abb1b9a83c4fd834d5aba3c5d4ac3c8
Kannan Vijayan <kvijayan@mozilla.com>
2014-01-02 18:42:21 -0500

    Bug 951528. r=jandem, a=bajaj

--- js/src/jit/MIRGenerator.h
+++ js/src/jit/MIRGenerator.h
@@ -125,6 +125,10 @@ class MIRGenerator
         return asmJSGlobalAccesses_;
     }
 
+    bool modifiesFrameArguments() const {
+        return modifiesFrameArguments_;
+    }
+
   public:
     JSCompartment *compartment;
 
@@ -142,6 +146,11 @@ class MIRGenerator
     AsmJSHeapAccessVector asmJSHeapAccesses_;
     AsmJSGlobalAccessVector asmJSGlobalAccesses_;
     uint32_t minAsmJSHeapLength_;
+
+    // Keep track of whether frame arguments are modified during execution.
+    // RegAlloc needs to know this as spilling values back to their register
+    // slots is not compatible with that.
+    bool modifiesFrameArguments_;
 
 #if defined(JS_ION_PERF)
     AsmJSPerfSpewer asmJSPerfSpewer_;
