68b52bb95737cb9746fd15c1bbfd08e5521fec3b
Ryan VanderMeulen <ryanvm@gmail.com>
2014-01-02 18:27:44 -0500

    Bug 945585 - Bustage fix. a=bustage CLOSED TREE

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -1646,7 +1646,7 @@ nsDocument::Release()
       NS_ADDREF_THIS();
       return mRefCnt.get();
     }
-    mRefCnt.incr(base);
+    mRefCnt.incr();
     nsNodeUtils::LastRelease(this);
     mRefCnt.decr(base);
     if (shouldDelete) {
