cdedfeb4d53d04c1e370d4a923fd9e309c0041cc
Benoit Jacob <bjacob@mozilla.com>
2014-01-02 10:17:20 -0500

    Bug 949813 - Remove mIsGlobalSharedContext from GLContext - r=jgilbert

--- gfx/gl/GLContext.h
+++ gfx/gl/GLContext.h
@@ -2435,9 +2435,6 @@ public:
     virtual void *GetNativeData(NativeDataType aType) { return nullptr; }
     GLContext *GetSharedContext() { return mSharedContext; }
 
-    bool IsGlobalSharedContext() { return mIsGlobalSharedContext; }
-    void SetIsGlobalSharedContext(bool aIsOne) { mIsGlobalSharedContext = aIsOne; }
-
     /**
      * Returns true if the thread on which this context was created is the currently
      * executing thread.
