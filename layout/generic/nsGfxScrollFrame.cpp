3243237480c7cba61e6605186aafb17bc5e66786
Timothy Nikkel <tnikkel@gmail.com>
2014-01-03 14:48:57 -0600

    Bug 942189 - Only avoid creating scroll info layers for root scroll frames in display root documents (not root content documents) because those have already had RecordFrameMetrics on them. r=roc, a=lsblakk

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -2427,7 +2427,10 @@ ScrollFrameHelper::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
     shouldBuildLayer =
       wantSubAPZC &&
       (wantLayerV || wantLayerH) &&
-      (!mIsRoot || !mOuter->PresContext()->IsRootContentDocument());
+      // If we are the root scroll frame for the display root then we don't need a scroll
+      // info layer to make a RecordFrameMetrics call for us as
+      // nsDisplayList::PaintForFrame already calls RecordFrameMetrics for us.
+      (!mIsRoot || aBuilder->RootReferenceFrame()->PresContext() != mOuter->PresContext());
   }
 
   if (shouldBuildLayer) {
