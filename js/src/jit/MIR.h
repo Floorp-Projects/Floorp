50fccdd929a3fb5d0cee6599a9962a613fca2c28
Jan de Mooij <jdemooij@mozilla.com>
2014-01-04 13:32:35 +0100

    Bug 953114 - Fix GetElementIC typed array issue. r=efaust, a=bajaj

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -6989,6 +6989,9 @@ class MGetElementCache
     bool monitoredResult() const {
         return monitoredResult_;
     }
+
+    bool allowDoubleResult() const;
+
     TypePolicy *typePolicy() {
         if (type() == MIRType_Value)
             return &PolicyV;
