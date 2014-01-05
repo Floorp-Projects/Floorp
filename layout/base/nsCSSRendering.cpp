7990f8174fea3f4eb10d9ca41662b93d91003966
Mats Palmgren <matspal@gmail.com>
2014-01-05 23:31:14 +0000

    Bug 955712 - Don't draw background color when not needed.  r=roc

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -2614,14 +2614,17 @@ nsCSSRendering::PaintBackgroundColorWithSC(nsPresContext* aPresContext,
   // background colors.
   bool drawBackgroundImage;
   bool drawBackgroundColor;
-
   nscolor bgColor = DetermineBackgroundColor(aPresContext,
                                              aBackgroundSC,
                                              aForFrame,
                                              drawBackgroundImage,
                                              drawBackgroundColor);
 
-  NS_ASSERTION(drawBackgroundColor, "Should not be trying to paint a background color if we don't have one");
+  NS_ASSERTION(drawBackgroundImage || drawBackgroundColor,
+               "Should not be trying to paint a background if we don't have one");
+  if (!drawBackgroundColor) {
+    return;
+  }
 
   // Compute the outermost boundary of the area that might be painted.
   gfxContext *ctx = aRenderingContext.ThebesContext();
