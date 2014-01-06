c245df3b522d029ee7f04fe33a6ee838057c8697
Brian Smith <brian@briansmith.org>
2014-01-06 14:02:47 -0800

    Bug 898431: Update NSS to NSS 3.15.4 RTM (NSS_3_15_4_RTM), r=me, a=bbajaj

--- security/nss/lib/ssl/sslcon.c
+++ security/nss/lib/ssl/sslcon.c
@@ -627,8 +627,9 @@ ssl2_SendServerFinishedMessage(sslSocket *ss)
 		(*ss->sec.uncache)(sid);
 	    rv = (SECStatus)sent;
 	} else if (!ss->opt.noCache) {
-	    /* Put the sid in session-id cache, (may already be there) */
-	    (*ss->sec.cache)(sid);
+	    if (sid->cached == never_cached) {
+		(*ss->sec.cache)(sid);
+	    }
 	    rv = SECSuccess;
 	}
 	ssl_FreeSID(sid);
@@ -2170,7 +2171,7 @@ ssl2_ClientRegSessionID(sslSocket *ss, PRUint8 *s)
 	sid->peerCert = CERT_DupCertificate(ss->sec.peerCert);
 
     }
-    if (!ss->opt.noCache)
+    if (!ss->opt.noCache && sid->cached == never_cached)
 	(*ss->sec.cache)(sid);
 }
 
