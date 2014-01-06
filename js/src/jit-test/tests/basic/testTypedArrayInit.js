0ab95c79e6e10e9eccac479d9bf41b6d01e3de86
Steve Fink <sfink@mozilla.com>
2014-01-06 14:43:19 -0800

    Bug 956434 - Apply gczeal to object cache allocations, r=terrence

---
+++ js/src/jit-test/tests/basic/testTypedArrayInit.js
@@ -0,0 +1,23 @@
+// Arrays should be initialized to zero
+
+function f() {
+  for (var ctor of [ Int8Array,
+                     Uint8Array,
+                     Uint8ClampedArray,
+                     Int16Array,
+                     Uint16Array,
+                     Int32Array,
+                     Uint32Array,
+                     Float32Array,
+                     Float64Array ])
+  {
+    for (var len of [ 3, 30, 300, 3000, 30000 ]) {
+      var arr = ctor(len);
+      for (var i = 0; i < arr.length; i++)
+        assertEq(arr[i], 0);
+    }
+  }
+}
+
+f();
+f();
