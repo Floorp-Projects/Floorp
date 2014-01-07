79322a37219f8b7a11b5706b57af3a805b48d864
Nicolas Silva <nical@mozilla.com>
2014-01-07 17:20:03 +0100

    Bug 900244 - New D3D9 Textures for BufferTextures. r=nrc

--- gfx/layers/composite/TextureHost.cpp
+++ gfx/layers/composite/TextureHost.cpp
@@ -151,6 +151,11 @@ TemporaryRef<TextureHost> CreateTextureHostD3D11(const SurfaceDescriptor& aDesc,
                                                  ISurfaceAllocator* aDeallocator,
                                                  TextureFlags aFlags);
 
+// implemented in TextureD3D9.cpp
+TemporaryRef<TextureHost> CreateTextureHostD3D9(const SurfaceDescriptor& aDesc,
+                                                ISurfaceAllocator* aDeallocator,
+                                                TextureFlags aFlags);
+
 // static
 TemporaryRef<TextureHost>
 TextureHost::Create(const SurfaceDescriptor& aDesc,
@@ -173,7 +178,7 @@ TextureHost::Create(const SurfaceDescriptor& aDesc,
     case LAYERS_D3D11:
       return CreateTextureHostD3D11(aDesc, aDeallocator, aFlags);
     case LAYERS_D3D9:
-      // XXX - not implemented yet
+      return CreateTextureHostD3D9(aDesc, aDeallocator, aFlags);
 #endif
     default:
       MOZ_CRASH("Couldn't create texture host");
