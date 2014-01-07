df6aa29852cbeab5534562a197efc8c412a519b0
Lukas Blakk <lsblakk@mozilla.com>
2014-01-07 15:51:39 -0800

    Removes the value for define EARLY_BETA_OR_EARLIER a=release-mgmt

--- build/defines.sh
+++ build/defines.sh
@@ -1,3 +1,4 @@
 # Define indicating that this build is prior to one of the early betas. To be
 # unset mid-way through the beta cycle.
-EARLY_BETA_OR_EARLIER=0
+# We have built Beta 4 for Firefox 27, removing the value for this define
+EARLY_BETA_OR_EARLIER=
