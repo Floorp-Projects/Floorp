2bc58720fcf21368db275a31745f9c5d011fda61
Martin Thomson <martin.thomson@gmail.com>
2014-01-08 11:55:04 -0800

    Bug 884573 - Add a=identity support for sipcc. r=abr

--- media/webrtc/signaling/src/sipcc/include/ccsdp.h
+++ media/webrtc/signaling/src/sipcc/include/ccsdp.h
@@ -246,6 +246,7 @@ typedef enum {
     SDP_ATTR_RTCP_FB,  /* RFC 4585 */
     SDP_ATTR_SETUP,
     SDP_ATTR_CONNECTION,
+    SDP_ATTR_IDENTITY,
     SDP_MAX_ATTR_TYPES,
     SDP_ATTR_INVALID
 } sdp_attr_e;
