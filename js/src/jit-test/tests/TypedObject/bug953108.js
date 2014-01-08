bb310dda26370729c1310021b1f9cb6ca746bf17
Nicholas D. Matsakis <nmatsakis@mozilla.com>
2014-01-08 04:36:17 -0500

    Bug 953108 - Initialized TypedObject module when initializing SIMD module r=till

---
+++ js/src/jit-test/tests/TypedObject/bug953108.js
@@ -0,0 +1,10 @@
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/licenses/publicdomain/
+ */
+
+if (!this.hasOwnProperty("TypedObject"))
+  quit();
+
+var float32x4 = SIMD.float32x4;
+float32x4.array(1);
