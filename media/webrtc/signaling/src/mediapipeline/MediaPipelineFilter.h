8c01b21fd43744428daaa5f7e28e7887f6ab067e
Byron Campen [:bwc] <docfaraday@gmail.com>
2014-01-09 15:12:25 -0800

    Bug 786234 - Part 2.1: RTCP filtering logic. r=abr

--- media/webrtc/signaling/src/mediapipeline/MediaPipelineFilter.h
+++ media/webrtc/signaling/src/mediapipeline/MediaPipelineFilter.h
@@ -52,12 +52,15 @@ class MediaPipelineFilter {
   // the filter about ssrcs)
   bool Filter(const webrtc::RTPHeader& header, uint32_t correlator = 0);
 
+  typedef enum {
+    FAIL,
+    PASS,
+    UNSUPPORTED
+  } Result;
+
   // RTCP doesn't have things like the RTP correlator, and uses its own
   // payload types too.
-  // RTCP receiver reports use _our_ ssrcs
-  bool FilterRTCPReceiverReport(uint32_t ssrc);
-  // The rest of RTCP uses their ssrcs
-  bool FilterRTCP(uint32_t ssrc);
+  Result FilterRTCP(const unsigned char* data, size_t len) const;
 
   void AddLocalSSRC(uint32_t ssrc);
   void AddRemoteSSRC(uint32_t ssrc);
@@ -68,7 +71,33 @@ class MediaPipelineFilter {
 
   void IncorporateRemoteDescription(const MediaPipelineFilter& remote_filter);
 
+  // Some payload types
+  static const uint8_t SENDER_REPORT_T = 200;
+  static const uint8_t RECEIVER_REPORT_T = 201;
+
  private:
+  static const uint8_t MAYBE_LOCAL_SSRC = 1;
+  static const uint8_t MAYBE_REMOTE_SSRC = 2;
+
+  // Payload type is always in the second byte
+  static const size_t PT_OFFSET = 1;
+  // First SSRC always starts at the fifth byte.
+  static const size_t FIRST_SSRC_OFFSET = 4;
+
+  static const size_t RECEIVER_REPORT_START_SR = 7*4;
+  static const size_t SENDER_REPORT_START_RR = 2*4;
+  static const size_t RECEIVER_REPORT_SIZE = 6*4;
+
+  bool CheckRtcpSsrc(const unsigned char* data,
+                     size_t len,
+                     size_t ssrc_offset,
+                     uint8_t flags) const;
+
+
+  bool CheckRtcpReport(const unsigned char* data,
+                        size_t len,
+                        size_t first_rr_offset) const;
+
   uint32_t correlator_;
   // The number of filters we manage here is quite small, so I am optimizing
   // for readability.
