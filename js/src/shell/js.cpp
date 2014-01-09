1fcec3490be53d364f4bd6586b1eca7202910230
Jeff Walden <jwalden@mit.edu>
2014-01-09 15:54:54 -0600

    Replace a few manual JSPropertySpec[] terminators with JS_PS_END.  No bug, r=trivial

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -4878,7 +4878,7 @@ static const JSPropertySpec dom_props[] = {
      { (JSPropertyOp)dom_genericGetter, &dom_x_getterinfo },
      { (JSStrictPropertyOp)dom_genericSetter, &dom_x_setterinfo }
     },
-    {nullptr,0,0,JSOP_NULLWRAPPER, JSOP_NULLWRAPPER}
+    JS_PS_END
 };
 
 static const JSFunctionSpec dom_methods[] = {
