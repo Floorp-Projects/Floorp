1ee0b5a7c1658dd28df5f8dd85163e46c6980d85
Jan de Mooij <jdemooij@mozilla.com>
2014-01-09 11:07:59 +0100

    Bug 951632 - Fix shell metadata hook to skip functions from other compartments. r=luke

---
+++ js/src/jit-test/tests/basic/bug951632.js
@@ -0,0 +1,9 @@
+setObjectMetadataCallback(true);
+var g = newGlobal()
+g.eval("function f(a) { return h(); }");
+g.h = function () {
+    return [1, 2, 3];
+};
+var o = getObjectMetadata(g.f(5));
+assertEq(o.stack.length, 1);
+assertEq(o.stack[0], g.h);
