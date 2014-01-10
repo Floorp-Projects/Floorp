99f63da0dab209664157e5754fc6b9b1dd2bfd06
Ben Turner <bent.mozilla@gmail.com>
2014-01-10 12:58:31 -0800

    Bug 957828 - Move stack machinery to cpp file, r=bsmedberg.

--- ipc/glue/MessageChannel.h
+++ ipc/glue/MessageChannel.h
@@ -11,10 +11,7 @@
 #include "base/basictypes.h"
 #include "base/message_loop.h"
 
-#include "mozilla/Assertions.h"
-#include "mozilla/DebugOnly.h"
 #include "mozilla/Monitor.h"
-#include "mozilla/Move.h"
 #include "mozilla/Vector.h"
 #include "mozilla/WeakPtr.h"
 #include "mozilla/ipc/Transport.h"
@@ -45,6 +42,9 @@ class MessageChannel : HasResultCodes
     friend class ProcessLink;
     friend class ThreadLink;
     friend class AutoEnterRPCTransaction;
+
+    class CxxStackFrame;
+    class InterruptFrame;
 
     typedef mozilla::Monitor Monitor;
 
@@ -275,133 +275,6 @@ class MessageChannel : HasResultCodes
     MessageListener *Listener() const {
         return mListener.get();
     }
-
-    enum Direction { IN_MESSAGE, OUT_MESSAGE };
-    struct InterruptFrame {
-        enum Semantics { INTR_SEMS, SYNC_SEMS, ASYNC_SEMS };
-
-        InterruptFrame(Direction direction, const Message* msg)
-          : mMessageName(strdup(msg->name())),
-            mMessageRoutingId(msg->routing_id()),
-            mMesageSemantics(msg->is_interrupt() ? INTR_SEMS :
-                             msg->is_sync() ? SYNC_SEMS :
-                             ASYNC_SEMS),
-            mDirection(direction),
-            mMoved(false)
-        {
-            MOZ_ASSERT(mMessageName);
-        }
-
-        InterruptFrame(InterruptFrame&& aOther)
-        {
-            MOZ_ASSERT(aOther.mMessageName);
-            mMessageName = aOther.mMessageName;
-            aOther.mMessageName = nullptr;
-            aOther.mMoved = true;
-
-            mMessageRoutingId = aOther.mMessageRoutingId;
-            mMesageSemantics = aOther.mMesageSemantics;
-            mDirection = aOther.mDirection;
-        }
-
-        ~InterruptFrame()
-        {
-            MOZ_ASSERT_IF(!mMessageName, mMoved);
-
-            if (mMessageName)
-                free(const_cast<char*>(mMessageName));
-        }
-
-        InterruptFrame& operator=(InterruptFrame&& aOther)
-        {
-            MOZ_ASSERT(&aOther != this);
-            this->~InterruptFrame();
-            new (this) InterruptFrame(mozilla::Move(aOther));
-            return *this;
-        }
-
-        bool IsInterruptIncall() const
-        {
-            return INTR_SEMS == mMesageSemantics && IN_MESSAGE == mDirection;
-        }
-
-        bool IsInterruptOutcall() const
-        {
-            return INTR_SEMS == mMesageSemantics && OUT_MESSAGE == mDirection;
-        }
-
-        void Describe(int32_t* id, const char** dir, const char** sems,
-                      const char** name) const
-        {
-            *id = mMessageRoutingId;
-            *dir = (IN_MESSAGE == mDirection) ? "in" : "out";
-            *sems = (INTR_SEMS == mMesageSemantics) ? "intr" :
-                    (SYNC_SEMS == mMesageSemantics) ? "sync" :
-                    "async";
-            *name = mMessageName;
-        }
-
-        const char* mMessageName;
-        int32_t mMessageRoutingId;
-        Semantics mMesageSemantics;
-        Direction mDirection;
-        DebugOnly<bool> mMoved;
-
-    private:
-        InterruptFrame(const InterruptFrame& aOther) MOZ_DELETE;
-        InterruptFrame& operator=(const InterruptFrame&) MOZ_DELETE;
-    };
-
-    class MOZ_STACK_CLASS CxxStackFrame
-    {
-      public:
-        CxxStackFrame(MessageChannel& that, Direction direction, const Message* msg)
-          : mThat(that)
-        {
-            mThat.AssertWorkerThread();
-
-            if (mThat.mCxxStackFrames.empty())
-                mThat.EnteredCxxStack();
-
-            mThat.mCxxStackFrames.append(InterruptFrame(direction, msg));
-
-            const InterruptFrame& frame = mThat.mCxxStackFrames.back();
-
-            if (frame.IsInterruptIncall())
-                mThat.EnteredCall();
-
-            mThat.mSawInterruptOutMsg |= frame.IsInterruptOutcall();
-        }
-
-        ~CxxStackFrame() {
-            mThat.AssertWorkerThread();
-
-            MOZ_ASSERT(!mThat.mCxxStackFrames.empty());
-
-            bool exitingCall = mThat.mCxxStackFrames.back().IsInterruptIncall();
-            mThat.mCxxStackFrames.shrinkBy(1);
-
-            bool exitingStack = mThat.mCxxStackFrames.empty();
-
-            // mListener could have gone away if Close() was called while
-            // MessageChannel code was still on the stack
-            if (!mThat.mListener)
-                return;
-
-            if (exitingCall)
-                mThat.ExitedCall();
-
-            if (exitingStack)
-                mThat.ExitedCxxStack();
-        }
-      private:
-        MessageChannel& mThat;
-
-        // disable harmful methods
-        CxxStackFrame() MOZ_DELETE;
-        CxxStackFrame(const CxxStackFrame&) MOZ_DELETE;
-        CxxStackFrame& operator=(const CxxStackFrame&) MOZ_DELETE;
-    };
 
     void DebugAbort(const char* file, int line, const char* cond,
                     const char* why,
