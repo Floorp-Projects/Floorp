744b3a38838f49959ea4274b7c6682d044f374e4
Ehsan Akhgari <ehsan@mozilla.com>
2014-01-10 01:36:35 -0500

    Bug 957356 - Use the correct string type for expat's XML_Char depending on the language to use; r=hsivonen

--- parser/expat/expat_config.h
+++ parser/expat/expat_config.h
@@ -27,10 +27,19 @@
 #define XMLIMPORT
 
 #define XML_UNICODE
-typedef char16_t XML_Char;
 typedef char XML_LChar;
+/*
+ * The char16_t type is only usable in C++ code, so we need this ugly hack to
+ * select a binary compatible C type for the expat C code to use.
+ */
+#ifdef __cplusplus
+typedef char16_t XML_Char;
 #define XML_T(x) (char16_t)x
-#define XML_L(x) x
+#else
+#include <stdint.h>
+typedef uint16_t XML_Char;
+#define XML_T(x) (uint16_t)x
+#endif
 
 #define XML_DTD
 #define XML_NS
