84dc4542648a612287fe40c96f86fc5458753b9b
Shu-yu Guo <shu@rfrn.org>
2014-01-10 17:12:17 -0800

    Bug 919638 - Followup: unbreak compiling without NSPR. (r=pnkfelix)

--- js/src/vm/Monitor.h
+++ js/src/vm/Monitor.h
@@ -33,9 +33,8 @@ class Monitor
     PRLock *lock_;
     PRCondVar *condVar_;
 
-
-    void assertIsHoldingLock() const {
-        PR_ASSERT_CURRENT_THREAD_OWNS_LOCK(lock_);
+    bool isFor(Monitor &monitor) const {
+        return lock_ == monitor.lock_;
     }
 
   public:
@@ -80,23 +79,43 @@ class AutoLockMonitor
 #endif
     }
 
-    void wait() {
+    void wait(PRCondVar *condVar) {
 #ifdef JS_THREADSAFE
         mozilla::DebugOnly<PRStatus> status =
-          PR_WaitCondVar(monitor.condVar_, PR_INTERVAL_NO_TIMEOUT);
-        JS_ASSERT(status == PR_SUCCESS);
+          PR_WaitCondVar(condVar, PR_INTERVAL_NO_TIMEOUT);
+        MOZ_ASSERT(status == PR_SUCCESS);
+#endif
+    }
+
+    void wait() {
+#ifdef JS_THREADSAFE
+        wait(monitor.condVar_);
+#endif
+    }
+
+    void notify(PRCondVar *condVar) {
+#ifdef JS_THREADSAFE
+        mozilla::DebugOnly<PRStatus> status = PR_NotifyCondVar(condVar);
+        MOZ_ASSERT(status == PR_SUCCESS);
 #endif
     }
 
     void notify() {
 #ifdef JS_THREADSAFE
-        PR_NotifyCondVar(monitor.condVar_);
+        notify(monitor.condVar_);
+#endif
+    }
+
+    void notifyAll(PRCondVar *condVar) {
+#ifdef JS_THREADSAFE
+        mozilla::DebugOnly<PRStatus> status = PR_NotifyAllCondVar(monitor.condVar_);
+        MOZ_ASSERT(status == PR_SUCCESS);
 #endif
     }
 
     void notifyAll() {
 #ifdef JS_THREADSAFE
-        PR_NotifyAllCondVar(monitor.condVar_);
+        notifyAll(monitor.condVar_);
 #endif
     }
 };
