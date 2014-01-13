9886906ee1c4fa051bf55142d49d034b17deae82
Alex Henrie <alexhenrie24@gmail.com>
2014-01-13 08:13:10 -0500

    Bug 157846. Main patch: Propagate text <input> padding down to its scrolled child and add 1px left/right padding to stylesheets as needed. Fix tests. r=roc Original patch by Charly Molter. Help from Robert O'Callahan and Jim Mathies.
    --HG--
    extra : rebase_source : eacfe5696e9831f149157bbd7fe697c0f8020604

--- layout/forms/nsTextControlFrame.cpp
+++ layout/forms/nsTextControlFrame.cpp
@@ -190,17 +190,6 @@ nsTextControlFrame::CalcIntrinsicSize(nsRenderingContext* aRenderingContext,
     if (PresContext()->CompatibilityMode() == eCompatibility_FullStandards) {
       aIntrinsicSize.width += 1;
     }
-
-    // Also add in the padding of our value div child.  Note that it hasn't
-    // been reflowed yet, so we can't get its used padding, but it shouldn't be
-    // using percentage padding anyway.
-    nsMargin childPadding;
-    nsIFrame* firstChild = GetFirstPrincipalChild();
-    if (firstChild && firstChild->StylePadding()->GetPadding(childPadding)) {
-      aIntrinsicSize.width += childPadding.LeftRight();
-    } else {
-      NS_ERROR("Percentage padding on value div?");
-    }
   }
 
   // Increment width with cols * letter-spacing.
@@ -535,32 +524,24 @@ nsTextControlFrame::ReflowTextControlChild(nsIFrame*                aKid,
                                            nsHTMLReflowMetrics& aParentDesiredSize)
 {
   // compute available size and frame offsets for child
-  nsSize availSize(aReflowState.ComputedWidth(), 
-                   aReflowState.ComputedHeight());
-  availSize.width = std::max(availSize.width, 0);
-  availSize.height = std::max(availSize.height, 0);
-  
+  nsSize availSize(aReflowState.ComputedWidth() +
+                   aReflowState.ComputedPhysicalPadding().LeftRight(),
+                   NS_UNCONSTRAINEDSIZE);
+
   nsHTMLReflowState kidReflowState(aPresContext, aReflowState, 
-                                   aKid, availSize);
+                                   aKid, availSize, -1, -1, nsHTMLReflowState::CALLER_WILL_INIT);
+  // Override padding with our computed padding in case we got it from theming or percentage
+  kidReflowState.Init(aPresContext, -1, -1, nullptr, &aReflowState.ComputedPhysicalPadding());
 
   // Set computed width and computed height for the child
-  nscoord width = availSize.width;
-  width -= kidReflowState.ComputedPhysicalMargin().LeftRight() +
-              kidReflowState.ComputedPhysicalBorderPadding().LeftRight();
-  width = std::max(width, 0);
-  kidReflowState.SetComputedWidth(width);
+  kidReflowState.SetComputedWidth(aReflowState.ComputedWidth());
+  kidReflowState.SetComputedHeight(aReflowState.ComputedHeight());
 
-  nscoord height = availSize.height;
-  height -= kidReflowState.ComputedPhysicalMargin().TopBottom() +
-              kidReflowState.ComputedPhysicalBorderPadding().TopBottom();
-  height = std::max(height, 0);       
-  kidReflowState.SetComputedHeight(height); 
-
-  // compute the offsets
-  nscoord xOffset = aReflowState.ComputedPhysicalBorderPadding().left
-                      + kidReflowState.ComputedPhysicalMargin().left;
-  nscoord yOffset = aReflowState.ComputedPhysicalBorderPadding().top
-                      + kidReflowState.ComputedPhysicalMargin().top;
+  // Offset the frame by the size of the parent's border
+  nscoord xOffset = aReflowState.ComputedPhysicalBorderPadding().left -
+                    aReflowState.ComputedPhysicalPadding().left;
+  nscoord yOffset = aReflowState.ComputedPhysicalBorderPadding().top -
+                    aReflowState.ComputedPhysicalPadding().top;
 
   // reflow the child
   nsHTMLReflowMetrics desiredSize(aReflowState.GetWritingMode());
