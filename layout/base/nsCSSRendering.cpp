b1c02148173655fd5744b64f1a43ea297aea693a
Horia Iosif Olaru <olaru@adobe.com>
2014-01-14 22:57:49 +0200

    Bug 959674 - Check if any blend mode other than normal should be applied to the image and draw the background-color if so. r=roc

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -1768,7 +1768,8 @@ nsCSSRendering::DetermineBackgroundColor(nsPresContext* aPresContext,
   if (aDrawBackgroundColor &&
       bg->BottomLayer().mRepeat.mXRepeat == NS_STYLE_BG_REPEAT_REPEAT &&
       bg->BottomLayer().mRepeat.mYRepeat == NS_STYLE_BG_REPEAT_REPEAT &&
-      bg->BottomLayer().mImage.IsOpaque()) {
+      bg->BottomLayer().mImage.IsOpaque() &&
+      bg->BottomLayer().mBlendMode == NS_STYLE_BLEND_NORMAL) {
     aDrawBackgroundColor = false;
   }
 
@@ -4353,12 +4354,12 @@ CSSSizeOrRatio::ComputeConcreteSize() const
       mWidth,
       double(mRatio.height) / mRatio.width);
     return nsSize(mWidth, height);
-  } 
+  }
 
   MOZ_ASSERT(mHasHeight);
   nscoord width = NSCoordSaturatingNonnegativeMultiply(
     mHeight,
-    double(mRatio.width) / mRatio.height);      
+    double(mRatio.width) / mRatio.height);
   return nsSize(width, mHeight);
 }
 
