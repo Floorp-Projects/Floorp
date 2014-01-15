c58ccf42946ce47e766e114b4e021ca44fd15800
Robert O'Callahan <robert@ocallahan.org>
2014-01-16 00:13:08 +1300

    Bug 952893. Part 3: Tighten TimeVarying invariants so that GetAt always returns an end time when the value actually changes. r=padenot

--- content/media/webaudio/AudioDestinationNode.h
+++ content/media/webaudio/AudioDestinationNode.h
@@ -93,16 +93,12 @@ private:
 
   // Audio Channel Type.
   AudioChannel mAudioChannel;
-<<<<<<< /home/roc/mozilla-central/content/media/webaudio/AudioDestinationNode.h
   bool mIsOffline;
   bool mHasFinished;
-=======
-
   TimeStamp mStartedBlockingDueToBeingOnlyNode;
   double mExtraCurrentTime;
   double mExtraCurrentTimeSinceLastStartedBlocking;
   bool mExtraCurrentTimeUpdatedSinceLastStableState;
->>>>>>> /tmp/AudioDestinationNode.h~other.MvuUBx
 };
 
 }
