709f3449d608c69cd05ad0f5fab59aa4063b38e1
Ralph Giles <giles@mozilla.com>
2014-01-15 10:05:26 -0800

    Bug 946639 - Enable libvpx axv2 optimations on linux. r=cpearce
    Enable avx2 optimizations on linux now that our build machines
    have a new enough toolchains to support them.

--- media/libvpx/vpx_config_x86-linux-gcc.h
+++ media/libvpx/vpx_config_x86-linux-gcc.h
@@ -28,7 +28,7 @@
 #define HAVE_SSSE3 1
 #define HAVE_SSE4_1 1
 #define HAVE_AVX 1
-#define HAVE_AVX2 0
+#define HAVE_AVX2 1
 #define HAVE_ALTIVEC 0
 #define HAVE_VPX_PORTS 1
 #define HAVE_STDINT_H 1
