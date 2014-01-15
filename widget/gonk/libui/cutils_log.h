d0d6f968b442ea05885b226738c97a52e3620379
Kai-Zhen Li <kli@mozilla.com>
2014-01-15 18:26:33 +0800

    Bug 959912 - fix build break on gonk-kk caused by different log header. r=mwu

--- widget/gonk/libui/cutils_log.h
+++ widget/gonk/libui/cutils_log.h
@@ -25,7 +25,8 @@
 // supports O_APPEND.  These calls have mutex-protected data structures
 // and so are NOT reentrant.  Do not use LOG in a signal handler.
 //
-#ifndef _LIBS_CUTILS_LOG_H
+#if !defined(_LIBS_CUTILS_LOG_H) && !defined(_LIBS_LOG_LOG_H)
+#define _LIBS_LOG_LOG_H
 #define _LIBS_CUTILS_LOG_H
 
 #include <stdio.h>
@@ -37,8 +38,13 @@
 #endif
 #include <stdarg.h>
 
+#if ANDROID_VERSION >= 19
+#include <log/uio.h>
+#include <log/logd.h>
+#else
 #include <cutils/uio.h>
 #include <cutils/logd.h>
+#endif
 
 #ifdef __cplusplus
 extern "C" {
