98e8e7538c57fb6296c74b43800e1984a92fd1c3
Allison Naaktgeboren <ally@mozilla.com>
2014-01-15 11:32:22 -0800

    Bug 947078 - Make the "Include URLs" option for the crash reporter into a checkbox instead of a toggle.r=mbrubeck a=metro-only

--- browser/metro/base/content/browser-ui.js
+++ browser/metro/base/content/browser-ui.js
@@ -964,6 +964,7 @@ var BrowserUI = {
       case "cmd_undoCloseTab":
       case "cmd_actions":
       case "cmd_panel":
+      case "cmd_reportingCrashesSubmitURLs":
       case "cmd_flyout_back":
       case "cmd_sanitize":
       case "cmd_volumeLeft":
@@ -1070,6 +1071,10 @@ var BrowserUI = {
         break;
       case "cmd_flyout_back":
         FlyoutPanelsUI.onBackButton();
+        break;
+      case "cmd_reportingCrashesSubmitURLs":
+        let urlCheckbox = document.getElementById("prefs-reporting-submitURLs");
+        Services.prefs.setBoolPref('app.crashreporter.submitURLs', urlCheckbox.checked);
         break;
       case "cmd_panel":
         PanelUI.toggle();
