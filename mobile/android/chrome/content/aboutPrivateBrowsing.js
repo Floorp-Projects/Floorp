87a7efe5102ee8e6aa5133832ecaea20f703bd82
Neil Bleasdale <nb.mozd@gmail.com>
2014-01-15 09:35:54 -0500

    Bug 956484 - Move inline js from /mobile/android/chrome/content/aboutPrivateBrowsing.xhtml into separate js file. r=mbrubeck

---
+++ mobile/android/chrome/content/aboutPrivateBrowsing.js
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
+
+if (!PrivateBrowsingUtils.isWindowPrivate(window)) {
+  document.addEventListener("DOMContentLoaded", function () {
+    document.body.setAttribute("class", "normal");
+  }, false);
+}
