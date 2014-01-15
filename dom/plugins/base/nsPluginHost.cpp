295adb355bd22fbc98392cd72c1cd15452c407e5
James Willcox <snorp@snorp.net>
2014-01-15 09:16:40 -0600

    Bug 935676 - Always try to reload invlalid plugins on Android r=jaas

--- dom/plugins/base/nsPluginHost.cpp
+++ dom/plugins/base/nsPluginHost.cpp
@@ -2633,6 +2633,8 @@ nsPluginHost::ReadPluginInfo()
     mCachedPlugins = tag;
   }
 
+// On Android we always want to try to load a plugin again (Flash). Bug 935676.
+#ifndef MOZ_WIDGET_ANDROID
   if (hasInvalidPlugins) {
     if (!ReadSectionHeader(reader, "INVALID")) {
       return rv;
@@ -2656,6 +2658,7 @@ nsPluginHost::ReadPluginInfo()
       mInvalidPlugins = invalidTag;
     }
   }
+#endif
 
   // flip the pref so we don't import the legacy flags again
   Preferences::SetBool("plugin.importedState", true);
