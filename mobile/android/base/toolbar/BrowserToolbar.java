7d3539c15c354f1fa9e18e4fe5c1730b21203d64
Brian Nicholson <bnicholson@mozilla.com>
2014-01-14 16:38:46 -0800

    Bug 949458 - Give the tab counter view its own rendering layer. r=blassey, a=bajaj

--- mobile/android/base/toolbar/BrowserToolbar.java
+++ mobile/android/base/toolbar/BrowserToolbar.java
@@ -279,6 +279,9 @@ public class BrowserToolbar extends GeckoRelativeLayout
 
         mTabs = (ShapedButton) findViewById(R.id.tabs);
         mTabsCounter = (TabCounter) findViewById(R.id.tabs_counter);
+        if (Build.VERSION.SDK_INT >= 11) {
+            mTabsCounter.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
 
         mBack = (ImageButton) findViewById(R.id.back);
         setButtonEnabled(mBack, false);
