fcf09eb5738a1a7f166845aef8c9335ff70650aa
Luke Wagner <luke@mozilla.com>
2014-01-16 10:56:57 -0600

    Bug 916612 - Inflate stackDepth width in try notes (r=wingo)

--- js/src/vm/Xdr.h
+++ js/src/vm/Xdr.h
@@ -22,7 +22,7 @@ namespace js {
  * and saved versions. If deserialization fails, the data should be
  * invalidated if possible.
  */
-static const uint32_t XDR_BYTECODE_VERSION = uint32_t(0xb973c0de - 161);
+static const uint32_t XDR_BYTECODE_VERSION = uint32_t(0xb973c0de - 162);
 
 class XDRBuffer {
   public:
