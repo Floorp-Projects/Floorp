eebf05f209275a7a516e5650f4b93cd8bb46770c
David Burns <dburns@mozilla.com>
2014-01-16 15:57:18 +0000

    Bug 927285 - Fix for intermittent on test_submit.py. a=test-only

--- testing/marionette/client/marionette/tests/unit/test_submit.py
+++ testing/marionette/client/marionette/tests/unit/test_submit.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import time
+
 from by import By
 from errors import NoSuchElementException
 from marionette_test import MarionetteTestCase
@@ -14,12 +16,37 @@ class TestSubmit(MarionetteTestCase):
         test_html = self.marionette.absolute_url("formPage.html")
         self.marionette.navigate(test_html)
         self.marionette.find_element(By.NAME, "login").submit()
-        self.assertEqual(self.marionette.title, "We Arrive Here")
+        for x in xrange(1, 10):
+            try:
+                self.marionette.find_element(By.NAME, "login")
+                time.sleep(1)
+            except NoSuchElementException:
+                break
+
+        Wait(self.marionette, timeout=10, ignored_exceptions=NoSuchElementException)\
+            .until(lambda m: m.find_element(By.ID, 'email'))
+        title = self.marionette.title
+        count = 0
+        while self.marionette.title == '' and count <= 10:
+            time.sleep(1)
+            title = self.marionette.title
+            count = count + 1
+            if title != '':
+                break
+        self.assertEqual(title, "We Arrive Here")
 
     def test_should_submit_a_form_when_any_input_element_within_that_form_is_submitted(self):
         test_html = self.marionette.absolute_url("formPage.html")
         self.marionette.navigate(test_html)
         self.marionette.find_element(By.ID, "checky").submit()
+
+        for x in xrange(1, 10):
+            try:
+                self.marionette.find_element(By.ID, "checky")
+                time.sleep(1)
+            except NoSuchElementException:
+                break
+
         Wait(self.marionette, ignored_exceptions=NoSuchElementException).until(
             lambda m: m.find_element(By.ID, 'email'))
         self.assertEqual(self.marionette.title, "We Arrive Here")
@@ -28,6 +55,14 @@ class TestSubmit(MarionetteTestCase):
         test_html = self.marionette.absolute_url("formPage.html")
         self.marionette.navigate(test_html)
         self.marionette.find_element(By.XPATH, "//form/p").submit()
+
+        for x in xrange(1, 10):
+            try:
+                self.marionette.find_element(By.XPATH, "//form/p")
+                time.sleep(1)
+            except NoSuchElementException:
+                break
+
         Wait(self.marionette, ignored_exceptions=NoSuchElementException).until(
             lambda m: m.find_element(By.ID, 'email'))
         self.assertEqual(self.marionette.title, "We Arrive Here")
