55bd71570507b029820587352a76d692e996c0f8
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2014-01-16 16:57:13 +0100

    Bug 956524. r=bz, a=lsblakk

--- layout/base/nsDocumentViewer.cpp
+++ layout/base/nsDocumentViewer.cpp
@@ -1231,6 +1231,13 @@ nsDocumentViewer::PermitUnloadInternal(bool aCallerClosesWindow,
 }
 
 NS_IMETHODIMP
+nsDocumentViewer::GetBeforeUnloadFiring(bool* aInEvent)
+{
+  *aInEvent = mInPermitUnload;
+  return NS_OK;
+}
+
+NS_IMETHODIMP
 nsDocumentViewer::ResetCloseWindow()
 {
   mCallerIsClosingWindow = false;
