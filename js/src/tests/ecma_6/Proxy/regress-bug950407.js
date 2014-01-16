650335832aba715781330a42e90440db600d4cbd
Eric Faust <efaustbmo@gmail.com>
2014-01-16 15:09:50 -0800

    Bug 950407 - Fix ES6 Proxy forwarding, and apropriately throw on indirect ArrayBuffer __proto__ sets. (r=bholley,Waldo sr=mrbkap)

---
+++ js/src/tests/ecma_6/Proxy/regress-bug950407.js
@@ -0,0 +1,11 @@
+var ab = ArrayBuffer(5);
+var p = new Proxy(ab, {});
+var ps = Object.getOwnPropertyDescriptor(Object.prototype, "__proto__").set;
+var threw = 0;
+try {
+    ps.call(p, {});
+} catch(ex) {
+    threw = 1;
+}
+
+reportCompare(1, threw, "Setting __proto__ on a proxy to an ArrayBuffer must throw.");
