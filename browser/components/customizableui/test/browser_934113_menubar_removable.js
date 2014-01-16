4f7991fb32c2ec68509fbd7e0b0177ed4d9a2878
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-01-16 09:44:49 +0100

    Backed out changeset d614fa6b53b2 (bug 889120) [australis] for suspicion of pgo perma-orange

--- browser/components/customizableui/test/browser_934113_menubar_removable.js
+++ browser/components/customizableui/test/browser_934113_menubar_removable.js
@@ -4,6 +4,8 @@
 
 "use strict";
 
+Services.prefs.setBoolPref("browser.uiCustomization.skipSourceNodeCheck", true);
+
 // Attempting to drag the menubar to the navbar shouldn't work.
 add_task(function() {
   yield startCustomizing();
@@ -21,5 +23,6 @@ add_task(function() {
 
 add_task(function asyncCleanup() {
   yield endCustomizing();
+  Services.prefs.clearUserPref("browser.uiCustomization.skipSourceNodeCheck");
   yield resetCustomization();
 });
