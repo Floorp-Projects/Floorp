9d1744d14d93cc86c12ca78677ef937a7537a47c
Ryan VanderMeulen <ryanvm@gmail.com>
2014-01-17 14:50:58 -0500

    Backed out changeset 8a5c1027e6e6 (bug 953303) for bustage.

--- gfx/layers/opengl/CompositorOGL.h
+++ gfx/layers/opengl/CompositorOGL.h
@@ -252,11 +252,6 @@ private:
    */
   bool mFrameInProgress;
 
-  /*
-   * Clear aRect on FrameBuffer.
-   */
-  virtual void clearFBRect(const gfx::Rect* aRect);
-
   /* Start a new frame. If aClipRectIn is null and aClipRectOut is non-null,
    * sets *aClipRectOut to the screen dimensions.
    */
