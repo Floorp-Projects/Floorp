3a90450125cbd75ce0f4a9e4fccece5c62a9c803
Cameron McCormack <cam@mcc.id.au>
2014-01-18 18:08:22 +1100

    Bug 960899 - Add debug function and lldb command to print frame state symbolically. r=dbaron

--- python/lldbutils/lldbutils/layout.py
+++ python/lldbutils/lldbutils/layout.py
@@ -8,8 +8,13 @@ def frametreelimited(debugger, command, result, dict):
     """Dumps the subtree of a frame tree rooted at the given nsIFrame*."""
     debugger.HandleCommand('expr (' + command + ')->DumpFrameTreeLimited()')
 
+def pstate(debugger, command, result, dict):
+    """Displays a frame's state bits symbolically."""
+    debugger.HandleCommand('expr mozilla::PrintFrameState(' + command + ')')
+
 def init(debugger):
     debugger.HandleCommand('command script add -f lldbutils.layout.frametree frametree')
     debugger.HandleCommand('command script add -f lldbutils.layout.frametreelimited frametreelimited')
     debugger.HandleCommand('command alias ft frametree')
     debugger.HandleCommand('command alias ftl frametreelimited')
+    debugger.HandleCommand('command script add -f lldbutils.layout.pstate pstate');
