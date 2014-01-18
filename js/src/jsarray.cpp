b90e87e13b0b7568993eb6806920fca1f09795ef
Jan de Mooij <jdemooij@mozilla.com>
2014-01-18 14:11:11 +0100

    Bug 959161 - Also use eager allocation for Array(x) inlined in Ion. r=luke

--- js/src/jsarray.cpp
+++ js/src/jsarray.cpp
@@ -3046,10 +3046,8 @@ js_Array(JSContext *cx, unsigned argc, Value *vp)
      * Allocate dense elements eagerly for small arrays, to avoid reallocating
      * elements when filling the array.
      */
-    static const uint32_t ArrayEagerAllocationMaxLength = 2048;
-
     RootedObject obj(cx);
-    obj = (length <= ArrayEagerAllocationMaxLength)
+    obj = (length <= ArrayObject::EagerAllocationMaxLength)
           ? NewDenseAllocatedArray(cx, length)
           : NewDenseUnallocatedArray(cx, length);
     if (!obj)
