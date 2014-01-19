fb0c9dc369752545615958be03ae856a24d22b8c
Richard Newman <rnewman@mozilla.com>
2014-01-18 19:49:25 -0800

    Bug 929066 - Handle skew in HAWK requests. r=nalexander

--- mobile/android/base/sync/net/Resource.java
+++ mobile/android/base/sync/net/Resource.java
@@ -4,9 +4,14 @@
 
 package org.mozilla.gecko.sync.net;
 
+import java.net.URI;
+
 import ch.boye.httpclientandroidlib.HttpEntity;
 
 public interface Resource {
+  public abstract URI getURI();
+  public abstract String getURIString();
+  public abstract String getHostname();
   public abstract void get();
   public abstract void delete();
   public abstract void post(HttpEntity body);
