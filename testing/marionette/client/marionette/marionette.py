271e70663d6fa21c8a9e77d9c46917216f04f97d
Ryan VanderMeulen <ryanvm@gmail.com>
2014-01-21 14:47:41 -0500

    Backed out 3 changesets (bug 941132, bug 941136, bug 961792) for breaking the Marionette harness. CLOSED TREE
    Backed out changeset 301044380886 (bug 961792)
    Backed out changeset 236a56500ef3 (bug 941132)
    Backed out changeset 02130207774e (bug 941136)

--- testing/marionette/client/marionette/marionette.py
+++ testing/marionette/client/marionette/marionette.py
@@ -148,17 +148,10 @@ class HTMLElement(object):
 
     @property
     def location(self):
-        """Get an element's location on the page.
-
-        The returned point will contain the x and y coordinates of the
-        top left-hand corner of the given element.  The point (0,0)
-        refers to the upper-left corner of the document.
-
-        :returns: a dictionary containing x and y as entries
-
-        """
-
-        return self.marionette._send_message("getElementLocation", "value", id=self.id)
+        '''
+        A dictionary with the x and y location of an element
+        '''
+        return self.marionette._send_message('getElementPosition', 'value', id=self.id)
 
     def value_of_css_property(self, property_name):
         '''
@@ -875,20 +868,10 @@ class Marionette(object):
         return response
 
     def get_url(self):
-        """Get a string representing the current URL.
-
-        On Desktop this returns a string representation of the URL of
-        the current top level browsing context.  This is equivalent to
-        document.location.href.
-
-        When in the context of the chrome, this returns the canonical
-        URL of the current resource.
-
-        :returns: string representation of URL
-
-        """
-
-        response = self._send_message("getCurrentUrl", "value")
+        '''
+        Returns the url of the active page in the browser.
+        '''
+        response = self._send_message('getUrl', 'value')
         return response
 
     def get_window_type(self):
