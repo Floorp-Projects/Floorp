0b128943325a56d6d589e000eb24ba3cce6a351c
Mike Shal <mshal@mozilla.com>
2014-01-21 18:01:22 -0500

    Bug 875013 - VPATH removals in build, roboextender, crashreporter; r=ted

--- toolkit/crashreporter/google-breakpad/src/client/windows/crash_generation/objs.mk
+++
@@ -1,6 +0,0 @@
-objs_crash_generation = \
-  client_info.cc \
-  crash_generation_client.cc \
-  crash_generation_server.cc \
-  minidump_generator.cc \
-  $(NULL)
--- toolkit/crashreporter/google-breakpad/src/common/windows/objs.mk
+++
@@ -1,4 +0,0 @@
-objs_common = \
-  guid_string.cc \
-  string_utils.cc \
-  $(NULL)
