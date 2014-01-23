b8323c22bc1474808af5e8bc6455f86501956bed
Sid Stamm <sstamm@mozilla.com>
2014-01-23 15:34:59 -0800

    Bug 911547 - Make nsIContentSecurityPolicy serializable and trigger read/write from nsPrincipal. r=jst, r=grobinson, a=lsblakk

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -2690,7 +2690,7 @@ nsDocument::InitCSP(nsIChannel* aChannel)
   aChannel->GetURI(getter_AddRefs(selfURI));
 
   // Store the request context for violation reports
-  csp->ScanRequestData(httpChannel);
+  csp->SetRequestContext(nullptr, nullptr, nullptr, aChannel);
 
   // ----- if the doc is an app and we want a default CSP, apply it.
   if (applyAppDefaultCSP) {
