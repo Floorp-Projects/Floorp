324c853be9ed374315be925a85abbfb8ab15428a
Wes Kocher <wkocher@mozilla.com>
2014-01-22 20:12:03 -0800

    Backed out changeset 47c7cd88eee1 (bug 961047) for b2g scrolling failures

--- b2g/chrome/content/settings.js
+++ b2g/chrome/content/settings.js
@@ -651,10 +651,3 @@ SettingsListener.observe("accessibility.screenreader", false, function(value) {
     });
   });
 })();
-
-// =================== AsyncPanZoom ======================
-
-SettingsListener.observe('apz.force-enable', true, function(value) {
-  Services.prefs.setBoolPref('dom.browser_frames.useAsyncPanZoom', value);
-});
-
