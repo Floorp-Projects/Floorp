32a738a04a3433b43452f48d8b2f0c1953afae7e
Vivien Nicolas <vnicolas@mozilla.com>
2014-01-23 18:45:00 +0100

    Bug 961047 - Ensure all mozbrowser iframes use APZ when APZ is enabled. r=kats,fabrice

--- b2g/chrome/content/settings.js
+++ b2g/chrome/content/settings.js
@@ -651,3 +651,10 @@ SettingsListener.observe("accessibility.screenreader", false, function(value) {
     });
   });
 })();
+
+// =================== AsyncPanZoom ======================
+
+SettingsListener.observe('apz.force-enable', false, function(value) {
+  Services.prefs.setBoolPref('dom.browser_frames.useAsyncPanZoom', value);
+});
+
