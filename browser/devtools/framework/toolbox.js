ef946f5c7027e66cda562d6caa2dd21f5992555b
Peiyong Lin <pylaurent1314@gmail.com>
2014-01-24 17:28:41 +0100

    Bug 961771 - Disable highlighter when switching tools. r=pbrosset

--- browser/devtools/framework/toolbox.js
+++ browser/devtools/framework/toolbox.js
@@ -73,6 +73,7 @@ function Toolbox(target, selectedTool, hostType, hostOptions) {
   this._refreshHostTitle = this._refreshHostTitle.bind(this);
   this._splitConsoleOnKeypress = this._splitConsoleOnKeypress.bind(this)
   this.destroy = this.destroy.bind(this);
+  this.stopPicker = this.stopPicker.bind(this);
 
   this._target.on("close", this.destroy);
 
@@ -1112,6 +1113,7 @@ Toolbox.prototype = {
 
     let done = () => {
       this.emit("picker-started");
+      this.on("select", this.stopPicker);
       deferred.resolve();
     };
 
@@ -1156,6 +1158,7 @@ Toolbox.prototype = {
 
     let done = () => {
       this.emit("picker-stopped");
+      this.off("select", this.stopPicker);
       deferred.resolve();
     };
 
