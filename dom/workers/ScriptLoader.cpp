8f4aefe572519b4123560ca27deb528f62b9c03f
Andrew McCreight <continuation@gmail.com>
2014-01-23 17:20:53 -0800

    Bug 947592 - Streamline ReportLoadError. r=bent a=abillings

--- dom/workers/ScriptLoader.cpp
+++ dom/workers/ScriptLoader.cpp
@@ -872,8 +872,7 @@ void ReportLoadError(JSContext* aCx, const nsAString& aURL,
       break;
 
     default:
-      JS_ReportError(aCx, "Failed to load script: %s (nsresult = 0x%x)",
-                     url.get(), aLoadResult);
+      JS_ReportError(aCx, "Failed to load script (nsresult = 0x%x)", aLoadResult);
   }
 }
 
