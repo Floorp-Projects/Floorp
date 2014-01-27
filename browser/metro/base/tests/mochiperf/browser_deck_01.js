41a5965148471bb8a46786211b2d3c20636ca285
Jim Mathies <jmathies@mozilla.com>
2014-01-27 17:45:03 -0600

    Bug 964418 - Update mochiperf tests to use dom utils frame time recording and add some apzc tests. r=mbrubeck, a=metro-only CLOSED TREE

--- browser/metro/base/tests/mochiperf/browser_deck_01.js
+++ browser/metro/base/tests/mochiperf/browser_deck_01.js
@@ -12,19 +12,17 @@ gTests.push({
   run: function run() {
     yield addTab("about:mozilla");
     yield hideContextUI();
-    yield waitForMs(5000);
+    yield hideNavBar();
+    yield waitForMs(3000);
 
     let shiftDataSet = new Array();
     let paintDataSet = new Array();
     let stopwatch = new StopWatch();
-
-    var paintCount = 0;
-    function onPaint() {
-      paintCount++;
-    }
+    let win = Browser.selectedTab.browser.contentWindow;
+    let domUtils = win.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowUtils);
 
     for (let idx = 0; idx < 10; idx++) {
-      window.addEventListener("MozAfterPaint", onPaint, true);
+      let recordingHandle = domUtils.startFrameTimeRecording();
       stopwatch.start();
       let promise = waitForEvent(window, "MozDeckOffsetChanged");
       ContentAreaObserver.shiftBrowserDeck(300);
@@ -34,22 +32,21 @@ gTests.push({
       yield promise;
       stopwatch.stop();
       yield waitForMs(500);
-      window.removeEventListener("MozAfterPaint", onPaint, true);
+      let intervals = domUtils.stopFrameTimeRecording(recordingHandle);
       shiftDataSet.push(stopwatch.time());
-      paintDataSet.push(paintCount);
-      paintCount = 0;
+      paintDataSet.push(intervals.length);
     }
     
     PerfTest.declareTest("ecb5fbec-0b3d-490f-8d4a-13fa8963e54a",
                          "shift browser deck", "browser", "ux",
                          "Triggers multiple SKB deck shifting operations using an offset " +
                          "value of 300px. Measures total time in milliseconds for a up/down " +
-                         "shift operation plus the total number of paints. Strips outliers.");
+                         "shift operation plus the total number of frames. Strips outliers.");
     let shiftAverage = PerfTest.computeAverage(shiftDataSet, { stripOutliers: true });
     let paintAverage = PerfTest.computeAverage(paintDataSet, { stripOutliers: true });
     PerfTest.declareNumericalResults([
       { value: shiftAverage, desc: "msec" },
-      { value: paintAverage, desc: "paint count" },
+      { value: paintAverage, desc: "frame count" },
     ]);
   }
 });
