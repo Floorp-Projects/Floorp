f2be62f3d6276fcf4e6ef73d24ffaddf0724ebcf
Andrea Marchesini <amarchesini@mozilla.com>
2014-01-29 08:16:57 -0500

    Bug 964892 - WebIDL 'assert' method should be implemented as '_assert_'. r=bz

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -6069,8 +6069,10 @@ class CppKeywords():
 
     @staticmethod
     def checkMethodName(name):
+        # Double '_' because 'assert' and '_assert' cannot be used in MS2013 compiler.
+        # Bug 964892 and bug 963560.
         if name in CppKeywords.keywords:
-          name = '_' + name
+          name = '_' + name + '_'
         return name
 
 class CGStaticMethod(CGAbstractStaticBindingMethod):
