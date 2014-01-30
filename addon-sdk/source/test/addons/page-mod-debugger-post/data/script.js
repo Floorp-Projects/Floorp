43fc4a1aa856b9629a8cd23b264b0d3c72c29744
Wes Kocher <wkocher@mozilla.com>
2014-01-29 17:03:11 -0800

    Backed out changeset 7e243174acf5 (bug 964545) to fix a CLOSED TREE

--- addon-sdk/source/test/addons/page-mod-debugger-post/data/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-unsafeWindow.runDebuggerStatement = function() {
-  window.document.body.setAttribute('style', 'background-color: red');
-  debugger;
-  window.document.body.setAttribute('style', 'background-color: green');
-}
