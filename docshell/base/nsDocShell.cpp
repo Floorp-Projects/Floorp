6b706c4e90766198d36864e829304a3fd5ee81ea
Michael Henretty <mhenretty@mozilla.com>
2014-01-30 15:06:34 -0800

    Bug 959800 - [Offline] mozbrowser iframes don't display correct offline content. r=fabrice, bz

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -4791,6 +4791,10 @@ nsDocShell::LoadErrorPage(nsIURI *aURI, const char16_t *aURL,
       errorPageUrl.AppendASCII(manifestParam.get());
     }
 
+    nsAutoCString frameType(FrameTypeToString(mFrameType));
+    errorPageUrl.AppendLiteral("&f=");
+    errorPageUrl.AppendASCII(frameType.get());
+
     nsCOMPtr<nsIURI> errorPageURI;
     rv = NS_NewURI(getter_AddRefs(errorPageURI), errorPageUrl);
     NS_ENSURE_SUCCESS(rv, rv);
