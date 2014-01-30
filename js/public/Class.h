ff831b9835565647027e55de8abfd4506e7463b9
Eric Faust <efaustbmo@gmail.com>
2014-01-29 17:20:15 -0800

    Bug 924720 - Part 0: Free up spare JSClass flags. (r=jwalden)

--- js/public/Class.h
+++ js/public/Class.h
@@ -541,9 +541,7 @@ struct JSClass {
 #define JSCLASS_INTERNAL_FLAG2          (1<<(JSCLASS_HIGH_FLAGS_SHIFT+2))
 #define JSCLASS_INTERNAL_FLAG3          (1<<(JSCLASS_HIGH_FLAGS_SHIFT+3))
 
-// Indicate whether the proto or ctor should be frozen.
-#define JSCLASS_FREEZE_PROTO            (1<<(JSCLASS_HIGH_FLAGS_SHIFT+4))
-#define JSCLASS_FREEZE_CTOR             (1<<(JSCLASS_HIGH_FLAGS_SHIFT+5))
+// Bits 21 and 22 unused.
 
 // Reserved for embeddings.
 #define JSCLASS_USERBIT2                (1<<(JSCLASS_HIGH_FLAGS_SHIFT+6))
