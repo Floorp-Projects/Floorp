9ebf73d519d302a6d6b584e48d60764bbaa50d53
Richard Newman <rnewman@mozilla.com>
2014-01-31 11:56:17 -0800

    Bug 966174 - Skip straight back over Get Started when successfully setting up FxA. r=nalexander

--- mobile/android/base/fxa/activities/FxAccountGetStartedActivity.java
+++ mobile/android/base/fxa/activities/FxAccountGetStartedActivity.java
@@ -85,15 +85,21 @@ public class FxAccountGetStartedActivity extends AccountAuthenticatorActivity {
    */
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    Logger.debug(LOG_TAG, "onActivityResult: " + requestCode);
+    Logger.debug(LOG_TAG, "onActivityResult: " + requestCode + ", " + resultCode);
     if (requestCode != CHILD_REQUEST_CODE) {
       super.onActivityResult(requestCode, resultCode, data);
       return;
     }
+
+    this.setResult(requestCode, data);
     if (data != null) {
       this.setAccountAuthenticatorResult(data.getExtras());
+
+      // We want to drop ourselves off the back stack if the user successfully
+      // created or signed in to an account. We can easily determine this by
+      // checking for the presence of response data.
+      this.finish();
     }
-    this.setResult(requestCode, data);
   }
 
   protected void linkifyOldFirefoxLink() {
