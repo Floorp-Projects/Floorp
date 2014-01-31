3413057bc76ececb0032ebf0c91c4ea2d8bbc320
L. David Baron <dbaron@dbaron.org>
2014-01-31 12:28:00 -0800

    Bug 966415:  Add more line breaks to nsCSSPropAliasList.h for better diffs and blame.  r=bzbarsky

--- layout/style/nsCSSPropAliasList.h
+++ layout/style/nsCSSPropAliasList.h
@@ -35,26 +35,95 @@
 
  ******/
 
-CSS_PROP_ALIAS(-moz-transform-origin, transform_origin, MozTransformOrigin, "layout.css.prefixes.transforms")
-CSS_PROP_ALIAS(-moz-perspective-origin, perspective_origin, MozPerspectiveOrigin, "layout.css.prefixes.transforms")
-CSS_PROP_ALIAS(-moz-perspective, perspective, MozPerspective, "layout.css.prefixes.transforms")
-CSS_PROP_ALIAS(-moz-transform-style, transform_style, MozTransformStyle, "layout.css.prefixes.transforms")
-CSS_PROP_ALIAS(-moz-backface-visibility, backface_visibility, MozBackfaceVisibility, "layout.css.prefixes.transforms")
-CSS_PROP_ALIAS(-moz-border-image, border_image, MozBorderImage, "layout.css.prefixes.border-image")
-CSS_PROP_ALIAS(-moz-transition, transition, MozTransition, "layout.css.prefixes.transitions")
-CSS_PROP_ALIAS(-moz-transition-delay, transition_delay, MozTransitionDelay, "layout.css.prefixes.transitions")
-CSS_PROP_ALIAS(-moz-transition-duration, transition_duration, MozTransitionDuration, "layout.css.prefixes.transitions")
-CSS_PROP_ALIAS(-moz-transition-property, transition_property, MozTransitionProperty, "layout.css.prefixes.transitions")
-CSS_PROP_ALIAS(-moz-transition-timing-function, transition_timing_function, MozTransitionTimingFunction, "layout.css.prefixes.transitions")
-CSS_PROP_ALIAS(-moz-animation, animation, MozAnimation, "layout.css.prefixes.animations")
-CSS_PROP_ALIAS(-moz-animation-delay, animation_delay, MozAnimationDelay, "layout.css.prefixes.animations")
-CSS_PROP_ALIAS(-moz-animation-direction, animation_direction, MozAnimationDirection, "layout.css.prefixes.animations")
-CSS_PROP_ALIAS(-moz-animation-duration, animation_duration, MozAnimationDuration, "layout.css.prefixes.animations")
-CSS_PROP_ALIAS(-moz-animation-fill-mode, animation_fill_mode, MozAnimationFillMode, "layout.css.prefixes.animations")
-CSS_PROP_ALIAS(-moz-animation-iteration-count, animation_iteration_count, MozAnimationIterationCount, "layout.css.prefixes.animations")
-CSS_PROP_ALIAS(-moz-animation-name, animation_name, MozAnimationName, "layout.css.prefixes.animations")
-CSS_PROP_ALIAS(-moz-animation-play-state, animation_play_state, MozAnimationPlayState, "layout.css.prefixes.animations")
-CSS_PROP_ALIAS(-moz-animation-timing-function, animation_timing_function, MozAnimationTimingFunction, "layout.css.prefixes.animations")
-CSS_PROP_ALIAS(-moz-box-sizing, box_sizing, MozBoxSizing, "")
-CSS_PROP_ALIAS(font-feature-settings, font_feature_settings, FontFeatureSettings, "layout.css.font-features.enabled")
-CSS_PROP_ALIAS(font-language-override, font_language_override, FontLanguageOverride, "layout.css.font-features.enabled")
+CSS_PROP_ALIAS(-moz-transform-origin,
+               transform_origin,
+               MozTransformOrigin,
+               "layout.css.prefixes.transforms")
+CSS_PROP_ALIAS(-moz-perspective-origin,
+               perspective_origin,
+               MozPerspectiveOrigin,
+               "layout.css.prefixes.transforms")
+CSS_PROP_ALIAS(-moz-perspective,
+               perspective,
+               MozPerspective,
+               "layout.css.prefixes.transforms")
+CSS_PROP_ALIAS(-moz-transform-style,
+               transform_style,
+               MozTransformStyle,
+               "layout.css.prefixes.transforms")
+CSS_PROP_ALIAS(-moz-backface-visibility,
+               backface_visibility,
+               MozBackfaceVisibility,
+               "layout.css.prefixes.transforms")
+CSS_PROP_ALIAS(-moz-border-image,
+               border_image,
+               MozBorderImage,
+               "layout.css.prefixes.border-image")
+CSS_PROP_ALIAS(-moz-transition,
+               transition,
+               MozTransition,
+               "layout.css.prefixes.transitions")
+CSS_PROP_ALIAS(-moz-transition-delay,
+               transition_delay,
+               MozTransitionDelay,
+               "layout.css.prefixes.transitions")
+CSS_PROP_ALIAS(-moz-transition-duration,
+               transition_duration,
+               MozTransitionDuration,
+               "layout.css.prefixes.transitions")
+CSS_PROP_ALIAS(-moz-transition-property,
+               transition_property,
+               MozTransitionProperty,
+               "layout.css.prefixes.transitions")
+CSS_PROP_ALIAS(-moz-transition-timing-function,
+               transition_timing_function,
+               MozTransitionTimingFunction,
+               "layout.css.prefixes.transitions")
+CSS_PROP_ALIAS(-moz-animation,
+               animation,
+               MozAnimation,
+               "layout.css.prefixes.animations")
+CSS_PROP_ALIAS(-moz-animation-delay,
+               animation_delay,
+               MozAnimationDelay,
+               "layout.css.prefixes.animations")
+CSS_PROP_ALIAS(-moz-animation-direction,
+               animation_direction,
+               MozAnimationDirection,
+               "layout.css.prefixes.animations")
+CSS_PROP_ALIAS(-moz-animation-duration,
+               animation_duration,
+               MozAnimationDuration,
+               "layout.css.prefixes.animations")
+CSS_PROP_ALIAS(-moz-animation-fill-mode,
+               animation_fill_mode,
+               MozAnimationFillMode,
+               "layout.css.prefixes.animations")
+CSS_PROP_ALIAS(-moz-animation-iteration-count,
+               animation_iteration_count,
+               MozAnimationIterationCount,
+               "layout.css.prefixes.animations")
+CSS_PROP_ALIAS(-moz-animation-name,
+               animation_name,
+               MozAnimationName,
+               "layout.css.prefixes.animations")
+CSS_PROP_ALIAS(-moz-animation-play-state,
+               animation_play_state,
+               MozAnimationPlayState,
+               "layout.css.prefixes.animations")
+CSS_PROP_ALIAS(-moz-animation-timing-function,
+               animation_timing_function,
+               MozAnimationTimingFunction,
+               "layout.css.prefixes.animations")
+CSS_PROP_ALIAS(-moz-box-sizing,
+               box_sizing,
+               MozBoxSizing,
+               "")
+CSS_PROP_ALIAS(font-feature-settings,
+               font_feature_settings,
+               FontFeatureSettings,
+               "layout.css.font-features.enabled")
+CSS_PROP_ALIAS(font-language-override,
+               font_language_override,
+               FontLanguageOverride,
+               "layout.css.font-features.enabled")
