01fa7e9f98eee1d63c63f389954f9e7b094e347a
Olli Pettay <Olli.Pettay@helsinki.fi>
2014-01-30 21:35:44 -0800

    Backout Bug 930793 - Remove favor performance mode (we have still racy b2g tests), a=backout

--- content/base/src/nsContentSink.h
+++ content/base/src/nsContentSink.h
@@ -237,6 +237,9 @@ public:
   static void NotifyDocElementCreated(nsIDocument* aDoc);
 
 protected:
+  void
+  FavorPerformanceHint(bool perfOverStarvation, uint32_t starvationDelay);
+
   inline int32_t GetNotificationInterval()
   {
     if (mDynamicLowerValue) {
