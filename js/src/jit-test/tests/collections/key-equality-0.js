df8d096956d3bae4241da5fd12b53d0367b7ea1a
Till Schneidereit <till@tillschneidereit.net>
2014-02-01 23:30:44 +0100

    Bug 952870 - Treat -0 and 0 as the same key in Maps and Sets. r=jorendorff

--- js/src/jit-test/tests/collections/key-equality-0.js
+++ js/src/jit-test/tests/collections/key-equality-0.js
@@ -1,37 +1,43 @@
-// -0 is a distinct key from +0.
+// -0 is treated as the same key as +0.
 
 var s = new Set;
 s.add(-0);
-assertEq(s.has(0), false);
+assertEq(s.has(0), true);
 assertEq(s.has(-0), true);
 
-assertEq(s.delete(0), false);
-assertEq(s.has(-0), true);
+assertEq(s.delete(0), true);
+assertEq(s.has(-0), false);
+assertEq(s.has(0), false);
 
 s.add(0);
-assertEq(s.delete(-0), true);
 assertEq(s.has(0), true);
+assertEq(s.has(-0), true);
+assertEq(s.delete(-0), true);
 assertEq(s.has(-0), false);
+assertEq(s.has(0), false);
 
 var m = new Map;
 m.set(-0, 'x');
+assertEq(m.has(0), true);
+assertEq(m.get(0), 'x');
+assertEq(m.has(-0), true);
+assertEq(m.get(-0), 'x');
+
+assertEq(m.delete(0), true);
+assertEq(m.has(-0), false);
+assertEq(m.get(-0), undefined);
 assertEq(m.has(0), false);
 assertEq(m.get(0), undefined);
-assertEq(m.has(-0), true);
-assertEq(m.get(-0), 'x');
 
-assertEq(m.delete(0), false);
-assertEq(m.has(-0), true);
-assertEq(m.get(-0), 'x');
-
+m.set(-0, 'x');
 m.set(0, 'y');
 assertEq(m.has(0), true);
 assertEq(m.get(0), 'y');
 assertEq(m.has(-0), true);
-assertEq(m.get(-0), 'x');
+assertEq(m.get(-0), 'y');
 
 assertEq(m.delete(-0), true);
-assertEq(m.has(0), true);
-assertEq(m.get(0), 'y');
+assertEq(m.has(0), false);
+assertEq(m.get(0), undefined);
 assertEq(m.has(-0), false);
 assertEq(m.get(-0), undefined);
