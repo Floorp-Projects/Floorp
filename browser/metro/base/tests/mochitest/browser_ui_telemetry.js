7f1dc84318092395f4b30aef76fd7a6adb976fb8
Sam Foster <sfoster@mozilla.com>
2014-02-02 14:59:56 -0800

    Bug 950904 - Add UITelemetry probe to capture window width & height. r=jimm

---
+++ browser/metro/base/tests/mochitest/browser_ui_telemetry.js
@@ -0,0 +1,26 @@
+// -*- Mode: js2; tab-width: 2; indent-tabs-mode: nil; js2-basic-offset: 2; js2-skip-preprocessor-directives: t; -*-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+function test() {
+  runTests();
+}
+
+function getSimpleMeasurementsFromTelemetryPing() {
+  return Cu.import("resource://gre/modules/TelemetryPing.jsm", {}).
+    TelemetryPing.getPayload().simpleMeasurements;
+}
+
+gTests.push({
+  desc: "Test browser-ui telemetry",
+  run: function testBrowserUITelemetry() {
+    // startup should have registered simple measures function
+    let simpleMeasurements = getSimpleMeasurementsFromTelemetryPing();
+    ok(simpleMeasurements, "simpleMeasurements are truthy");
+    ok(simpleMeasurements.UITelemetry["metro-ui"]["window-width"], "window-width measurement was captured");
+    ok(simpleMeasurements.UITelemetry["metro-ui"]["window-height"], "window-height measurement was captured");
+  }
+});
