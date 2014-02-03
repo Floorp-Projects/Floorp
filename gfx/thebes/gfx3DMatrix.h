b02b3b60a5d977c6a71b6990a1eb3d5e0c064119
Matt Woodrow <mwoodrow@mozilla.com>
2014-02-04 12:25:22 +1300

    Bug 952011 - Add gfx3DMatrix API for untransformed rects and points. r=bjacob

--- gfx/thebes/gfx3DMatrix.h
+++ gfx/thebes/gfx3DMatrix.h
@@ -250,6 +250,25 @@ public:
   gfxPoint ProjectPoint(const gfxPoint& aPoint) const;
   gfxRect ProjectRectBounds(const gfxRect& aRect) const;
 
+  /**
+   * Transforms a point by the inverse of this matrix. In the case of perspective transforms, some screen
+   * points have no equivalent in the untransformed plane (if they exist past the vanishing point). To
+   * avoid this, we need to specify the bounds of the untransformed plane to restrict the search area.
+   *
+   * @param aPoint Point to untransform.
+   * @param aChildBounds Bounds of the untransformed plane.
+   * @param aOut Untransformed point.
+   * @return Returns true if a point was found within a ChildBounds, false otherwise.
+   */
+  bool UntransformPoint(const gfxPoint& aPoint, const gfxRect& aChildBounds, gfxPoint* aOut) const;
+
+
+  /**
+   * Same as UntransformPoint, but untransforms a rect and returns the bounding rect of the result.
+   * Returns an empty rect if the result doesn't intersect aChildBounds.
+   */
+  gfxRect UntransformBounds(const gfxRect& aRect, const gfxRect& aChildBounds) const;
+
 
   /**
    * Inverts this matrix, if possible. Otherwise, the matrix is left
