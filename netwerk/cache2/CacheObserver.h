969b224bdbeb80e0fa7f4f31e8b62eeb1eb4187b
Honza Bambas <honzab.moz@firemni.cz>
2014-02-04 01:53:00 +0100

    Bug 913820 - Base for size limit preferences in HTTP cache v2, r=michal

--- netwerk/cache2/CacheObserver.h
+++ netwerk/cache2/CacheObserver.h
@@ -25,21 +25,41 @@ class CacheObserver : public nsIObserver
   static CacheObserver* Self() { return sSelf; }
 
   // Access to preferences
+  static bool const UseNewCache();
+  static bool const UseDiskCache()
+    { return sUseDiskCache; }
+  static bool const UseMemoryCache()
+    { return sUseMemoryCache; }
   static uint32_t const MemoryLimit() // <0.5MB,1024MB>, result in bytes.
     { return std::max(512U, std::min(1048576U, sMemoryLimit)) << 10; }
-  static bool const UseNewCache();
+  static uint32_t const DiskCacheCapacity() // result in bytes.
+    { return sDiskCacheCapacity << 10; }
+  static uint32_t const MaxMemoryEntrySize() // result in bytes.
+    { return sMaxMemoryEntrySize << 10; }
+  static uint32_t const MaxDiskEntrySize() // result in bytes.
+    { return sMaxDiskEntrySize << 10; }
+  static uint32_t const CompressionLevel()
+    { return sCompressionLevel; }
   static uint32_t const HalfLifeSeconds()
     { return sHalfLifeHours * 60 * 60; }
   static int32_t const HalfLifeExperiment()
     { return sHalfLifeExperiment; }
+
+  static bool const EntryIsTooBig(int64_t aSize, bool aUsingDisk);
 
 private:
   static CacheObserver* sSelf;
 
   void AttachToPreferences();
 
-  static uint32_t sMemoryLimit;
   static uint32_t sUseNewCache;
+  static bool sUseDiskCache;
+  static bool sUseMemoryCache;
+  static uint32_t sMemoryLimit;
+  static uint32_t sDiskCacheCapacity;
+  static uint32_t sMaxMemoryEntrySize;
+  static uint32_t sMaxDiskEntrySize;
+  static uint32_t sCompressionLevel;
   static uint32_t sHalfLifeHours;
   static int32_t sHalfLifeExperiment;
 };
