f37d0f3ab98a90306a3c93a2cc812fa2cdaf7553
Michael Comella <michael.l.comella@gmail.com>
2014-02-04 19:41:29 -0800

    Bug 960709 - Part 3: Add methods to HealthRecorder interface for correctness. r=rnewman

--- mobile/android/base/health/HealthRecorder.java
+++ mobile/android/base/health/HealthRecorder.java
@@ -7,17 +7,25 @@ package org.mozilla.gecko.health;
 
 import android.content.SharedPreferences;
 
+import org.json.JSONObject;
+
 /**
  * HealthRecorder is an interface into the Firefox Health Report storage system.
  */
 public interface HealthRecorder {
     public void setCurrentSession(SessionInformation session);
-    public void recordSessionEnd(String reason, SharedPreferences.Editor editor);
+    public void checkForOrphanSessions();
 
     public void recordGeckoStartupTime(long duration);
     public void recordJavaStartupTime(long duration);
+    public void recordSearch(final String engineID, final String location);
+    public void recordSessionEnd(String reason, SharedPreferences.Editor editor);
+    public void recordSessionEnd(String reason, SharedPreferences.Editor editor, final int environment);
 
     public void onAppLocaleChanged(String to);
+    public void onAddonChanged(String id, JSONObject json);
+    public void onAddonUninstalling(String id);
+    public void onEnvironmentChanged();
     public void onEnvironmentChanged(final boolean startNewSession, final String sessionEndReason);
 
     public void close();
