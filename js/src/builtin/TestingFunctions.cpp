34a0dacb1088552e3ef91a3cd27fe418718bda04
Brian Hackett <bhackett1024@gmail.com>
2014-02-05 11:40:35 -0700

    Bug 941805 - Make the pool of JS workers be per process rather than per runtime, r=billm.

--- js/src/builtin/TestingFunctions.cpp
+++ js/src/builtin/TestingFunctions.cpp
@@ -1406,7 +1406,11 @@ static bool
 WorkerThreadCount(JSContext *cx, unsigned argc, jsval *vp)
 {
     CallArgs args = CallArgsFromVp(argc, vp);
-    args.rval().setNumber(static_cast<double>(cx->runtime()->workerThreadCount()));
+#ifdef JS_THREADSAFE
+    args.rval().setInt32(cx->runtime()->useHelperThreads() ? WorkerThreadState().threadCount : 0);
+#else
+    args.rval().setInt32(0);
+#endif
     return true;
 }
 
