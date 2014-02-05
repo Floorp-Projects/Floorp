6e33da93a13ead33ee640cc2265bd81bfa58315a
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2014-02-05 00:31:41 -0800

    Bug 967370 - UITour: Remove duplication in browser chrome tests. r=Unfocused a=sylvestre [Australis]

--- browser/modules/test/browser_UITour2.js
+++ browser/modules/test/browser_UITour2.js
@@ -9,74 +9,8 @@ let gContentWindow;
 
 Components.utils.import("resource:///modules/UITour.jsm");
 
-function loadTestPage(callback, host = "https://example.com/") {
-   if (gTestTab)
-    gBrowser.removeTab(gTestTab);
-
-  let url = getRootDirectory(gTestPath) + "uitour.html";
-  url = url.replace("chrome://mochitests/content/", host);
-
-  gTestTab = gBrowser.addTab(url);
-  gBrowser.selectedTab = gTestTab;
-
-  gTestTab.linkedBrowser.addEventListener("load", function onLoad() {
-    gTestTab.linkedBrowser.removeEventListener("load", onLoad);
-
-    gContentWindow = Components.utils.waiveXrays(gTestTab.linkedBrowser.contentDocument.defaultView);
-    gContentAPI = gContentWindow.Mozilla.UITour;
-
-    waitForFocus(callback, gContentWindow);
-  }, true);
-}
-
 function test() {
-  Services.prefs.setBoolPref("browser.uitour.enabled", true);
-  let testUri = Services.io.newURI("http://example.com", null, null);
-  Services.perms.add(testUri, "uitour", Services.perms.ALLOW_ACTION);
-
-  waitForExplicitFinish();
-
-  registerCleanupFunction(function() {
-    delete window.UITour;
-    delete window.gContentWindow;
-    delete window.gContentAPI;
-    if (gTestTab)
-      gBrowser.removeTab(gTestTab);
-    delete window.gTestTab;
-    Services.prefs.clearUserPref("browser.uitour.enabled", true);
-    Services.perms.remove("example.com", "uitour");
-  });
-
-  function done() {
-    if (gTestTab)
-      gBrowser.removeTab(gTestTab);
-    gTestTab = null;
-
-    let highlight = document.getElementById("UITourHighlightContainer");
-    is_element_hidden(highlight, "Highlight should be closed/hidden after UITour tab is closed");
-
-    let tooltip = document.getElementById("UITourTooltip");
-    is_element_hidden(tooltip, "Tooltip should be closed/hidden after UITour tab is closed");
-
-    ok(!PanelUI.panel.hasAttribute("noautohide"), "@noautohide on the menu panel should have been cleaned up");
-
-    is(UITour.pinnedTabs.get(window), null, "Any pinned tab should be closed after UITour tab is closed");
-
-    executeSoon(nextTest);
-  }
-
-  function nextTest() {
-    if (tests.length == 0) {
-      finish();
-      return;
-    }
-    let test = tests.shift();
-    info("Starting " + test.name);
-    loadTestPage(function() {
-      test(done);
-    });
-  }
-  nextTest();
+  UITourTest();
 }
 
 let tests = [
