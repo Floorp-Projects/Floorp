6dd5f8347152ba262af30abac85cbd14d57af632
Ryan VanderMeulen <ryanvm@gmail.com>
2014-02-05 15:32:15 -0500

    Backed out changeset fa9b60c88118 (bug 457296) for bustage on a CLOSED TREE.

--- extensions/cookie/test/unit/test_permmanager_mailto.js
+++ extensions/cookie/test/unit/test_permmanager_mailto.js
@@ -15,16 +15,13 @@ function run_test() {
   Services.perms.add(uri, kType, kCapability);
   do_check_true(permission_exists(kTestAddr, kType, kCapability));
 
-  // remove the permission, and make sure it was removed
-  Services.perms.remove(kTestAddr, kType);
-  do_check_false(permission_exists(kTestAddr, kType, kCapability));
+  Services.perms.removeAll();
 
   uri = Services.io.newURI("mailto:" + kTestAddr, null, null);
   Services.perms.add(uri, kType, kCapability);
   do_check_true(permission_exists(kTestAddr, kType, kCapability));
 
-  Services.perms.remove(kTestAddr, kType);
-  do_check_false(permission_exists(kTestAddr, kType, kCapability));
+  Services.perms.removeAll();
 }
 
 function permission_exists(aHost, aType, aCapability) {
