87f49f23e0b5f9fd20ab93dfcbb79ce2c7c3c3e3
Wes Kocher <wkocher@mozilla.com>
2014-02-04 19:26:24 -0800

    Backed out changeset ed9194eec73b (bug 958796) for multiple OSX debug failures on a CLOSED TREE

--- nsprpub/pr/tests/foreign.c
+++ nsprpub/pr/tests/foreign.c
@@ -79,7 +79,9 @@ static void sproc_start(void *arg, PRSize size)
 #endif  /* defined(IRIX) && !defined(_PR_PTHREADS) */
 
 #if defined(WIN32)
+#if defined(WINCE)
 #include <windows.h>
+#endif
 #include <process.h>  /* for _beginthreadex() */
 
 static PRUintn __stdcall windows_start(void *arg)
@@ -172,7 +174,7 @@ static PRStatus NSPRPUB_TESTS_CreateThread(StartFn start, void *arg)
                 0U, /* DWORD - initial thread stack size, in bytes */
                 windows_start, /* LPTHREAD_START_ROUTINE - pointer to thread function */
                 start_object, /* LPVOID - argument for new thread */
-                STACK_SIZE_PARAM_IS_A_RESERVATION, /*DWORD dwCreationFlags - creation flags */
+                0U, /*DWORD dwCreationFlags - creation flags */
                 &id /* LPDWORD - pointer to returned thread identifier */ );
 
             rv = (NULL == th) ? PR_FAILURE : PR_SUCCESS;
