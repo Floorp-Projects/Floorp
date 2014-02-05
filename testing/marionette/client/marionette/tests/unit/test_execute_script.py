45576a38ba9e926c4e53dccf777936f5363949a1
Andreas Tolfsen <ato@mozilla.com>
2014-02-05 17:04:42 -0500

    Bug 938228 - Fix marshaling of NodeList in executeScript. r=mdas

--- testing/marionette/client/marionette/tests/unit/test_execute_script.py
+++ testing/marionette/client/marionette/tests/unit/test_execute_script.py
@@ -2,8 +2,16 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette_test import MarionetteTestCase
+import urllib
+
+from by import By
 from marionette import JavascriptException, MarionetteException
+from marionette_test import MarionetteTestCase
+
+def inline(doc):
+    return "data:text/html;charset=utf-8,%s" % urllib.quote(doc)
+
+elements = inline("<p>foo</p> <p>bar</p>")
 
 class TestExecuteContent(MarionetteTestCase):
     def test_stack_trace(self):
@@ -46,6 +54,16 @@ let prefs = Components.classes["@mozilla.org/preferences-service;1"]
         self.assertEqual(self.marionette.execute_script("return {'foo': [1, 'a', 2]};"),
                          {'foo': [1, 'a', 2]})
 
+    # Bug 938228 identifies a problem with unmarshaling NodeList
+    # objects from the DOM.  document.querySelectorAll returns this
+    # construct.
+    def test_unmarshal_element_collection(self):
+        self.marionette.navigate(elements)
+        expected = self.marionette.find_elements(By.TAG_NAME, "p")
+        actual = self.marionette.execute_script(
+            "return document.querySelectorAll('p')")
+        self.assertEqual(expected, actual)
+
     def test_sandbox_reuse(self):
         # Sandboxes between `execute_script()` invocations are shared.
         self.marionette.execute_script("this.foobar = [23, 42];")
@@ -64,10 +82,28 @@ let prefs = Components.classes["@mozilla.org/preferences-service;1"]
 class TestExecuteChrome(TestExecuteContent):
     def setUp(self):
         super(TestExecuteChrome, self).setUp()
+        self.win = self.marionette.current_window_handle
         self.marionette.set_context("chrome")
+        self.marionette.execute_script(
+            "window.open('chrome://marionette/content/test.xul', 'xul', 'chrome')")
+        self.marionette.switch_to_window("xul")
+        self.assertNotEqual(self.win, self.marionette.current_window_handle)
+
+    def tearDown(self):
+        self.marionette.execute_script("window.close()")
+        self.marionette.switch_to_window(self.win)
+        self.assertEqual(self.win, self.marionette.current_window_handle)
+        super(TestExecuteChrome, self).tearDown()
 
     def test_execute_permission(self):
-        self.assertEqual(1, self.marionette.execute_script("var c = Components.classes;return 1;"))
+        self.assertEqual(1, self.marionette.execute_script(
+            "var c = Components.classes; return 1;"))
+
+    def test_unmarshal_element_collection(self):
+        expected = self.marionette.find_elements(By.TAG_NAME, "textbox")
+        actual = self.marionette.execute_script(
+            "return document.querySelectorAll('textbox')")
+        self.assertEqual(expected, actual)
 
     def test_sandbox_reuse(self):
         pass
