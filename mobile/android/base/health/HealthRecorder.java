4923ac4cde94e17a50285b1bed8eb5e4684c5cb9
Michael Comella <michael.l.comella@gmail.com>
2014-02-04 19:41:29 -0800

    Bug 960709 - Part 1: Factor out HealthRecorder interface. r=rnewman

---
+++ mobile/android/base/health/HealthRecorder.java
@@ -0,0 +1,24 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.health;
+
+import android.content.SharedPreferences;
+
+/**
+ * HealthRecorder is an interface into the Firefox Health Report storage system.
+ */
+public interface HealthRecorder {
+    public void setCurrentSession(SessionInformation session);
+    public void recordSessionEnd(String reason, SharedPreferences.Editor editor);
+
+    public void recordGeckoStartupTime(long duration);
+    public void recordJavaStartupTime(long duration);
+
+    public void onAppLocaleChanged(String to);
+    public void onEnvironmentChanged(final boolean startNewSession, final String sessionEndReason);
+
+    public void close();
+}
