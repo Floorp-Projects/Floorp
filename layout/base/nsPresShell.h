1d482d7cb52df3a75cb2553da4ae9d4002745bb8
Olli Pettay <Olli.Pettay@helsinki.fi>
2014-02-06 23:55:58 +0200

    Bug 967236 - Evict touches more aggressively, r=wesj
    --HG--
    extra : rebase_source : 28b75719e1b884574e7fa22cd62758bdb80afdca

--- layout/base/nsPresShell.h
+++ layout/base/nsPresShell.h
@@ -705,6 +705,8 @@ protected:
   static void ClearImageVisibilityVisited(nsView* aView, bool aClear);
   static void MarkImagesInListVisible(const nsDisplayList& aList);
 
+  void EvictTouches();
+
   // A list of images that are visible or almost visible.
   nsTHashtable< nsRefPtrHashKey<nsIImageLoadingContent> > mVisibleImages;
 
