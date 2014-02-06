055ea9cebd4e9047e58c284a897c60872b622a97
Alexandre Poirot <poirot.alex@gmail.com>
2014-02-06 17:36:44 +0100

    Bug 968857 - [SpecialPowers] Big files are truncated when loaded with loadChromeScript. r=ted
    With this patch, loadChromeScript now completely reads the input. This also
    moves the loading code in its own function.
    
    This also improves error reporting.

--- testing/specialpowers/content/SpecialPowersObserverAPI.js
+++ testing/specialpowers/content/SpecialPowersObserverAPI.js
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
 
 Components.utils.import("resource://gre/modules/Services.jsm");
 
@@ -160,6 +161,48 @@ SpecialPowersObserverAPI.prototype = {
     return Services.io.newURI(url, null, null);
   },
 
+  _readUrlAsString: function(aUrl) {
+    // Fetch script content as we can't use scriptloader's loadSubScript
+    // to evaluate http:// urls...
+    var scriptableStream = Cc["@mozilla.org/scriptableinputstream;1"]
+                             .getService(Ci.nsIScriptableInputStream);
+    var channel = Services.io.newChannel(aUrl, null, null);
+    var input = channel.open();
+    scriptableStream.init(input);
+
+    var str;
+    var buffer = [];
+
+    while ((str = scriptableStream.read(4096))) {
+      buffer.push(str);
+    }
+
+    var output = buffer.join("");
+
+    scriptableStream.close();
+    input.close();
+
+    var status;
+    try {
+      channel.QueryInterface(Ci.nsIHttpChannel);
+      status = channel.responseStatus;
+    } catch(e) {
+      /* The channel is not a nsIHttpCHannel, but that's fine */
+      dump("-*- _readUrlAsString: Got an error while fetching " +
+           "chrome script '" + aUrl + "': (" + e.name + ") " + e.message + ". " +
+           "Ignoring.\n");
+    }
+
+    if (status == 404) {
+      throw new SpecialPowersException(
+        "Error while executing chrome script '" + aUrl + "':\n" +
+        "The script doesn't exists. Ensure you have registered it in " +
+        "'support-files' in your mochitest.ini.");
+    }
+
+    return output;
+  },
+
   /**
    * messageManager callback function
    * This will get requests from our API in the window and process them in chrome for it
@@ -300,16 +343,7 @@ SpecialPowersObserverAPI.prototype = {
         var url = aMessage.json.url;
         var id = aMessage.json.id;
 
-        // Fetch script content as we can't use scriptloader's loadSubScript
-        // to evaluate http:// urls...
-        var scriptableStream = Cc["@mozilla.org/scriptableinputstream;1"]
-                                 .getService(Ci.nsIScriptableInputStream);
-        var channel = Services.io.newChannel(url, null, null);
-        var input = channel.open();
-        scriptableStream.init(input);
-        var jsScript = scriptableStream.read(input.available());
-        scriptableStream.close();
-        input.close();
+        var jsScript = this._readUrlAsString(url);
 
         // Setup a chrome sandbox that has access to sendAsyncMessage
         // and addMessageListener in order to communicate with
@@ -332,7 +366,8 @@ SpecialPowersObserverAPI.prototype = {
           Components.utils.evalInSandbox(jsScript, sb, "1.8", url, 1);
         } catch(e) {
           throw new SpecialPowersException("Error while executing chrome " +
-                                           "script '" + url + "':\n" + e);
+                                           "script '" + url + "':\n" + e + "\n" +
+                                           e.fileName + ":" + e.lineNumber);
         }
         return undefined;	// See comment at the beginning of this function.
 
