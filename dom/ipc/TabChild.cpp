71078c57ef000a948badcbd8c9161d4721f7d59b
Olli Pettay <Olli.Pettay@helsinki.fi>
2014-02-06 16:16:41 +0200

    Bug 964462, simplify ipc offline resource refcounting, r=mayhemer
    --HG--
    extra : rebase_source : 362ce98a10c25f7e3263f23736a18d2bcfb16e75

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -2108,7 +2108,7 @@ bool
 TabChild::DeallocPOfflineCacheUpdateChild(POfflineCacheUpdateChild* actor)
 {
   OfflineCacheUpdateChild* offlineCacheUpdate = static_cast<OfflineCacheUpdateChild*>(actor);
-  delete offlineCacheUpdate;
+  NS_RELEASE(offlineCacheUpdate);
   return true;
 }
 
