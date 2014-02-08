2aa9bfdb9c8ad002cbc208fcafb2d7bc3e886da6
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2014-02-08 02:04:21 +0000

    Bug 958869 - Don't show Australis button context menu on subviews or the menubar. r=MattN, a=sledru

--- browser/components/customizableui/content/panelUI.js
+++ browser/components/customizableui/content/panelUI.js
@@ -295,6 +295,7 @@ const PanelUI = {
       tempPanel.setAttribute("type", "arrow");
       tempPanel.setAttribute("id", "customizationui-widget-panel");
       tempPanel.setAttribute("class", "cui-widget-panel");
+      tempPanel.setAttribute("context", "");
       document.getElementById(CustomizableUI.AREA_NAVBAR).appendChild(tempPanel);
       // If the view has a footer, set a convenience class on the panel.
       tempPanel.classList.toggle("cui-widget-panelWithFooter",
