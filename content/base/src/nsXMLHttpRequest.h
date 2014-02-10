d0ac6a87af2d4fefaee7d179c1788be716e5da09
Olli Pettay <Olli.Pettay@helsinki.fi>
2014-02-10 20:35:25 +0200

    Bug 969671 - Warn about use of sync XHR in the main thread, r=sicking/ehsan

--- content/base/src/nsXMLHttpRequest.h
+++ content/base/src/nsXMLHttpRequest.h
@@ -533,6 +533,14 @@ public:
 
   virtual void DisconnectFromOwner() MOZ_OVERRIDE;
 
+  static void SetDontWarnAboutSyncXHR(bool aVal)
+  {
+    sDontWarnAboutSyncXHR = aVal;
+  }
+  static bool DontWarnAboutSyncXHR()
+  {
+    return sDontWarnAboutSyncXHR;
+  }
 protected:
   nsresult DetectCharset();
   nsresult AppendToResponseText(const char * aBuffer, uint32_t aBufferLen);
@@ -736,6 +744,25 @@ protected:
 
   // Helper object to manage our XPCOM scriptability bits
   nsXMLHttpRequestXPCOMifier* mXPCOMifier;
+
+  static bool sDontWarnAboutSyncXHR;
+};
+
+class MOZ_STACK_CLASS AutoDontWarnAboutSyncXHR
+{
+public:
+  AutoDontWarnAboutSyncXHR() : mOldVal(nsXMLHttpRequest::DontWarnAboutSyncXHR())
+  {
+    nsXMLHttpRequest::SetDontWarnAboutSyncXHR(true);
+  }
+
+  ~AutoDontWarnAboutSyncXHR()
+  {
+    nsXMLHttpRequest::SetDontWarnAboutSyncXHR(mOldVal);
+  }
+
+private:
+  bool mOldVal;
 };
 
 // A shim class designed to expose the non-DOM interfaces of
