bb29805d17535a275aacb37e6615845e8dee0857
Brian Grinstead <bgrinstead@mozilla.com>
2014-02-10 09:33:47 -0600

    Bug 966308 - DevTools Themes - Make dark theme the default;r=robcee

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1181,7 +1181,7 @@ pref("devtools.shadereditor.enabled", false);
 pref("devtools.chrome.enabled", false);
 
 // Default theme ("dark" or "light")
-pref("devtools.theme", "light");
+pref("devtools.theme", "dark");
 
 // Display the introductory text
 pref("devtools.gcli.hideIntro", false);
