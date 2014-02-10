0c0dd9cba0cacadd0af7c43f84d7f8c1063353ad
Hannes Verschore <hv1989@gmail.com>
2014-02-10 14:22:36 +0100

    Bug 957542 - Set minimum slots of ArrayObject to 2 to fix GGC, r=jandem

--- js/src/vm/ObjectImpl.cpp
+++ js/src/vm/ObjectImpl.cpp
@@ -365,6 +365,8 @@ js::ObjectImpl::dynamicSlotsCount(uint32_t nfixed, uint32_t span, const Class *c
     // this because slots are uncommon in that case.
     if (clasp != &ArrayObject::class_ && span <= SLOT_CAPACITY_MIN)
         return SLOT_CAPACITY_MIN;
+    if (clasp == &ArrayObject::class_ && span <= SLOT_CAPACITY_ARRAYOBJECT_MIN)
+        return SLOT_CAPACITY_ARRAYOBJECT_MIN;
 
     uint32_t slots = mozilla::RoundUpPow2(span);
     MOZ_ASSERT(slots >= span);
