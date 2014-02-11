6fbfc62ee2274d962cb905304d0ee921484a63f6
"Kan-Ru Chen (陳侃如)" <kanru@kanru.info>
2014-02-11 13:57:24 +0800

    Bug 963366 - Hide navigator.requestWakeLock behind pref dom.wakelock.enabled. r=ehsan
    ---
     b2g/app/b2g.js                                     |    3 +++
     .../mochitest/priority/test_HighPriority.html      |    3 ++-
     dom/power/test/browser_wakelocks.js                |    9 ++++++---
     dom/power/test/mochitest.ini                       |    2 ++
     dom/power/test/test_bug957893.html                 |   21 +++++++++++---------
     dom/power/test/test_bug957899.html                 |   11 +++++++---
     dom/power/test/test_wakelock_not_exposed.html      |   17 ++++++++++++++++
     dom/webidl/MozWakeLock.webidl                      |    2 +-
     dom/webidl/Navigator.webidl                        |    2 +-
     modules/libpref/src/init/all.js                    |    3 +++
     10 files changed, 55 insertions(+), 18 deletions(-)
     create mode 100644 dom/power/test/test_wakelock_not_exposed.html

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -4505,3 +4505,6 @@ pref("urlclassifier.download_allow_table", "");
 
 // Turn off Spatial navigation by default.
 pref("snav.enabled", false);
+
+// Wakelock is disabled by default.
+pref("dom.wakelock.enabled", false);
