8e349ff6c4b62a3f2ea18cc63ccb0b6b99760d56
Marina Samuel <msamuel@mozilla.com>
2014-02-11 14:00:19 -0500

    Bug 855930 - Add telemetry for max and current number of tabs. r=sfoster

--- browser/metro/base/tests/mochitest/browser_ui_telemetry.js
+++ browser/metro/base/tests/mochitest/browser_ui_telemetry.js
@@ -26,3 +26,41 @@ gTests.push({
     ok(simpleMeasurements.UITelemetry["metro-ui"]["window-height"], "window-height measurement was captured");
   }
 });
+
+gTests.push({
+  desc: "Test tab count telemetry",
+  run: function() {
+    // Wait for Session Manager to be initialized.
+    yield waitForCondition(() => window.__SSID);
+
+    Services.obs.notifyObservers(null, "reset-telemetry-vars", null);
+    yield waitForCondition(function () {
+      let simpleMeasurements = getTelemetryPayload().simpleMeasurements;
+      return simpleMeasurements.UITelemetry["metro-tabs"]["currTabCount"] == 1;
+    });
+
+    let simpleMeasurements = getTelemetryPayload().simpleMeasurements;
+    is(simpleMeasurements.UITelemetry["metro-tabs"]["currTabCount"], 1);
+    is(simpleMeasurements.UITelemetry["metro-tabs"]["maxTabCount"], 1);
+
+    let tab2 = Browser.addTab("about:mozilla");
+    simpleMeasurements = getTelemetryPayload().simpleMeasurements;
+    is(simpleMeasurements.UITelemetry["metro-tabs"]["currTabCount"], 2);
+    is(simpleMeasurements.UITelemetry["metro-tabs"]["maxTabCount"], 2);
+
+    let tab3 = Browser.addTab("about:config");
+    simpleMeasurements = getTelemetryPayload().simpleMeasurements;
+    is(simpleMeasurements.UITelemetry["metro-tabs"]["currTabCount"], 3);
+    is(simpleMeasurements.UITelemetry["metro-tabs"]["maxTabCount"], 3);
+
+    Browser.closeTab(tab2, { forceClose: true } );
+    simpleMeasurements = getTelemetryPayload().simpleMeasurements;
+    is(simpleMeasurements.UITelemetry["metro-tabs"]["currTabCount"], 2);
+    is(simpleMeasurements.UITelemetry["metro-tabs"]["maxTabCount"], 3);
+
+    Browser.closeTab(tab3, { forceClose: true } );
+    simpleMeasurements = getTelemetryPayload().simpleMeasurements;
+    is(simpleMeasurements.UITelemetry["metro-tabs"]["currTabCount"], 1);
+    is(simpleMeasurements.UITelemetry["metro-tabs"]["maxTabCount"], 3);
+  }
+});
