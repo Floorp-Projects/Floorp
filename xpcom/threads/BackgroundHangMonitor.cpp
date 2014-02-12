4c293f0bf6aa92b11bf5cc14187f7398fe864b03
Jim Chen <nchen@mozilla.com>
2014-02-12 10:24:21 -0500

    Bug 970358 - Disable BackgroundHangMonitor on release builds. r=froydnj, a=sledru

--- xpcom/threads/BackgroundHangMonitor.cpp
+++ xpcom/threads/BackgroundHangMonitor.cpp
@@ -408,6 +408,7 @@ BackgroundHangThread::NotifyActivity()
 BackgroundHangThread*
 BackgroundHangThread::FindThread()
 {
+#ifdef MOZ_ENABLE_BACKGROUND_HANG_MONITOR
   if (sTlsKey.initialized()) {
     // Use TLS if available
     return sTlsKey.get();
@@ -425,6 +426,7 @@ BackgroundHangThread::FindThread()
       return thread;
     }
   }
+#endif
   // Current thread is not initialized
   return nullptr;
 }
@@ -433,15 +435,18 @@ BackgroundHangThread::FindThread()
 void
 BackgroundHangMonitor::Startup()
 {
+#ifdef MOZ_ENABLE_BACKGROUND_HANG_MONITOR
   MOZ_ASSERT(!BackgroundHangManager::sInstance, "Already initialized");
   ThreadStackHelper::Startup();
   BackgroundHangThread::Startup();
   BackgroundHangManager::sInstance = new BackgroundHangManager();
+#endif
 }
 
 void
 BackgroundHangMonitor::Shutdown()
 {
+#ifdef MOZ_ENABLE_BACKGROUND_HANG_MONITOR
   MOZ_ASSERT(BackgroundHangManager::sInstance, "Not initialized");
   /* Scope our lock inside Shutdown() because the sInstance object can
      be destroyed as soon as we set sInstance to nullptr below, and
@@ -449,6 +454,7 @@ BackgroundHangMonitor::Shutdown()
   BackgroundHangManager::sInstance->Shutdown();
   BackgroundHangManager::sInstance = nullptr;
   ThreadStackHelper::Shutdown();
+#endif
 }
 
 BackgroundHangMonitor::BackgroundHangMonitor(const char* aName,
@@ -456,15 +462,19 @@ BackgroundHangMonitor::BackgroundHangMonitor(const char* aName,
                                              uint32_t aMaxTimeoutMs)
   : mThread(BackgroundHangThread::FindThread())
 {
+#ifdef MOZ_ENABLE_BACKGROUND_HANG_MONITOR
   if (!mThread) {
     mThread = new BackgroundHangThread(aName, aTimeoutMs, aMaxTimeoutMs);
   }
+#endif
 }
 
 BackgroundHangMonitor::BackgroundHangMonitor()
   : mThread(BackgroundHangThread::FindThread())
 {
+#ifdef MOZ_ENABLE_BACKGROUND_HANG_MONITOR
   MOZ_ASSERT(mThread, "Thread not initialized for hang monitoring");
+#endif
 }
 
 BackgroundHangMonitor::~BackgroundHangMonitor()
@@ -474,13 +484,17 @@ BackgroundHangMonitor::~BackgroundHangMonitor()
 void
 BackgroundHangMonitor::NotifyActivity()
 {
+#ifdef MOZ_ENABLE_BACKGROUND_HANG_MONITOR
   mThread->NotifyActivity();
+#endif
 }
 
 void
 BackgroundHangMonitor::NotifyWait()
 {
+#ifdef MOZ_ENABLE_BACKGROUND_HANG_MONITOR
   mThread->NotifyWait();
+#endif
 }
 
 
