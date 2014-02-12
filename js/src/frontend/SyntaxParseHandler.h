81bc09ceb086ceba6383ae533c8342c6cc6a45ff
Andy Wingo <wingo@igalia.com>
2014-02-12 18:46:24 +0100

    Bug 962599 - Store let-bound variables in the fixed part of stack frames  r=luke r=jandem

--- js/src/frontend/SyntaxParseHandler.h
+++ js/src/frontend/SyntaxParseHandler.h
@@ -157,14 +157,15 @@ class SyntaxParseHandler
     bool addCatchBlock(Node catchList, Node letBlock,
                        Node catchName, Node catchGuard, Node catchBody) { return true; }
 
-    void setLeaveBlockResult(Node block, Node kid, bool leaveBlockExpr) {}
-
     void setLastFunctionArgumentDefault(Node funcpn, Node pn) {}
     Node newFunctionDefinition() { return NodeGeneric; }
     void setFunctionBody(Node pn, Node kid) {}
     void setFunctionBox(Node pn, FunctionBox *funbox) {}
     void addFunctionArgument(Node pn, Node argpn) {}
+
     Node newLexicalScope(ObjectBox *blockbox) { return NodeGeneric; }
+    void setLexicalScopeBody(Node block, Node body) {}
+
     bool isOperationWithoutParens(Node pn, ParseNodeKind kind) {
         // It is OK to return false here, callers should only use this method
         // for reporting strict option warnings and parsing code which the
