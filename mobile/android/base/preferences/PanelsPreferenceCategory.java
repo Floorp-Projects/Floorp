ce606c53d60d98606ace55a99583599e15a45106
Chenxia Liu <liuche@mozilla.com>
2014-02-13 15:33:46 -0800

    Bug 977980 - Add "Remove" as an option for dynamic Home panels. r=margaret

--- mobile/android/base/preferences/PanelsPreferenceCategory.java
+++ mobile/android/base/preferences/PanelsPreferenceCategory.java
@@ -86,8 +86,10 @@ public class PanelsPreferenceCategory extends CustomListCategory {
 
     private void displayHomeConfig(HomeConfig.State configState) {
         for (PanelConfig panelConfig : configState) {
+            final boolean isRemovable = panelConfig.isDynamic();
+
             // Create and add the pref.
-            final PanelsPreference pref = new PanelsPreference(getContext(), PanelsPreferenceCategory.this);
+            final PanelsPreference pref = new PanelsPreference(getContext(), PanelsPreferenceCategory.this, isRemovable);
             pref.setTitle(panelConfig.getTitle());
             pref.setKey(panelConfig.getId());
             // XXX: Pull icon from PanelInfo.
