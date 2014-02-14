90072503d3c6decff99931d99e81542c67afaaa9
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-02-14 11:02:12 -0800

    Bug 935264 - Add more test cases to testHomeBanner (and update it to use UITest). r=mcomella a=test-only

--- mobile/android/base/tests/testHomeBanner.java
+++ mobile/android/base/tests/testHomeBanner.java
@@ -1,37 +1,71 @@
 package org.mozilla.gecko.tests;
 
-import org.mozilla.gecko.*;
+import static org.mozilla.gecko.tests.helpers.AssertionHelper.*;
 
-public class testHomeBanner extends BaseTest {
+import org.mozilla.gecko.Actions;
+import org.mozilla.gecko.R;
+import org.mozilla.gecko.tests.helpers.*;
+
+import android.view.View;
+
+public class testHomeBanner extends UITest {
 
     private static final String TEST_URL = "chrome://roboextender/content/robocop_home_banner.html";
     private static final String TEXT = "The quick brown fox jumps over the lazy dog.";
 
-    @Override
-    protected int getTestType() {
-        return TEST_MOCHITEST;
+    public void testHomeBanner() {
+        GeckoHelper.blockForReady();
+
+        // Make sure the banner is not visible.
+        mAboutHome.assertVisible()
+                  .assertBannerNotVisible();
+
+        addBannerMessage();
+
+        // Load about:home again, and make sure the onshown handler is called.
+        Actions.EventExpecter eventExpecter = getActions().expectGeckoEvent("TestHomeBanner:MessageShown");
+        NavigationHelper.enterAndLoadUrl("about:home");
+        eventExpecter.blockForEvent();
+
+        // Verify that the banner is visible with the correct text.
+        mAboutHome.assertBannerText(TEXT);
+
+        // Test to make sure the onclick handler is called.
+        eventExpecter = getActions().expectGeckoEvent("TestHomeBanner:MessageClicked");
+        mAboutHome.clickOnBanner();
+        eventExpecter.blockForEvent();
+
+        // Verify that the banner isn't visible after navigating away from about:home.
+        NavigationHelper.enterAndLoadUrl("about:firefox");
+
+        // AboutHomeComponent calls mSolo.getView, which will fail an assertion if the
+        // view is not present, so we need to use findViewById in this case.
+        final View banner = getActivity().findViewById(R.id.home_banner);
+        assertTrue("The HomeBanner is not visible", banner == null || banner.getVisibility() != View.VISIBLE);
+
+        removeBannerMessage();
+
+        // Verify that the banner no longer appears.
+        NavigationHelper.enterAndLoadUrl("about:home");
+        mAboutHome.assertVisible()
+                  .assertBannerNotVisible();
     }
 
-    public void testHomeBanner() {
-        blockForGeckoReady();
-
-        Actions.EventExpecter eventExpecter = mActions.expectGeckoEvent("TestHomeBanner:MessageAdded");
-
-        // Add a message to the home banner
-        inputAndLoadUrl(TEST_URL + "#addMessage");
+    /**
+     * Loads the roboextender page to add a message to the banner.
+     */
+    private void addBannerMessage() {
+        final Actions.EventExpecter eventExpecter = getActions().expectGeckoEvent("TestHomeBanner:MessageAdded");
+        NavigationHelper.enterAndLoadUrl(TEST_URL + "#addMessage");
         eventExpecter.blockForEvent();
+    }
 
-        // Load about:home, and test to make sure the onshown handler is called
-        eventExpecter = mActions.expectGeckoEvent("TestHomeBanner:MessageShown");
-        inputAndLoadUrl("about:home");
-        eventExpecter.blockForEvent();
-
-        // Verify that the correct message text showed up in the banner
-        mAsserter.ok(waitForText(TEXT), "banner text", "correct text appeared in the home banner");
-
-        // Test to make sure the onclick handler is called
-        eventExpecter = mActions.expectGeckoEvent("TestHomeBanner:MessageClicked");
-        mSolo.clickOnText(TEXT);
+    /**
+     * Loads the roboextender page to remove the message from the banner.
+     */
+    private void removeBannerMessage() {
+        final Actions.EventExpecter eventExpecter = getActions().expectGeckoEvent("TestHomeBanner:MessageRemoved");
+        NavigationHelper.enterAndLoadUrl(TEST_URL + "#removeMessage");
         eventExpecter.blockForEvent();
     }
 }
