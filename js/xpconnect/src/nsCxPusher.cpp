e9a6a87759a0db79d753aed98f54cd513bfdd1ed
Phil Ringnalda <philringnalda@gmail.com>
2014-02-14 18:35:30 -0800

    Back out 4fc776ee6852:6a0ebe4ddd01 (bug 968335) and e7d697004921 (bug 923904) for b2g emulator build bustage CLOSED TREE

--- js/xpconnect/src/nsCxPusher.cpp
+++ js/xpconnect/src/nsCxPusher.cpp
@@ -115,7 +115,6 @@ AutoCxPusher::AutoCxPusher(JSContext* cx, bool allowNull)
   if (!stack->Push(cx)) {
     MOZ_CRASH();
   }
-  mStackDepthAfterPush = stack->Count();
 
 #ifdef DEBUG
   mPushedContext = cx;
@@ -162,14 +161,6 @@ AutoCxPusher::~AutoCxPusher()
   MOZ_ASSERT(mPushedContext == nsXPConnect::XPConnect()->GetCurrentJSContext());
   XPCJSRuntime::Get()->GetJSContextStack()->Pop();
   mScx = nullptr;
-}
-
-bool
-AutoCxPusher::IsStackTop()
-{
-  uint32_t currentDepth = XPCJSRuntime::Get()->GetJSContextStack()->Count();
-  MOZ_ASSERT(currentDepth >= mStackDepthAfterPush);
-  return currentDepth == mStackDepthAfterPush;
 }
 
 AutoJSContext::AutoJSContext(MOZ_GUARD_OBJECT_NOTIFIER_ONLY_PARAM_IN_IMPL)
