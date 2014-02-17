1a35fd28c05081017dff6f3c06c9750e931fff4c
Brian Smith <brian@briansmith.org>
2014-02-17 13:19:54 -0800

    Bug 915931, Part 4: Expand OCSP xpcshell tests to test insanity::pkix, r=keeler, a=test-only
    --HG--
    extra : rebase_source : 953b7f71748bd4806e7b986c250a9e109c5dfa44
    extra : source : 571a1bf8c8abc5203b9026cdacdfd36321183533

--- security/manager/ssl/tests/unit/test_ocsp_required.js
+++ security/manager/ssl/tests/unit/test_ocsp_required.js
@@ -9,27 +9,45 @@
 // response with a bad signature. With security.OCSP.require set to true,
 // this should fail (but it also shouldn't cause assertion failures).
 
+let gOCSPRequestCount = 0;
+
 function run_test() {
   do_get_profile();
   Services.prefs.setBoolPref("security.OCSP.require", true);
 
-  let args = [ ["bad-signature", "localhostAndExampleCom", "unused" ] ];
-  let ocspResponses = generateOCSPResponses(args, "tlsserver");
-  let ocspResponseBadSignature = ocspResponses[0];
-  let ocspRequestCount = 0;
-
-  let ocspResponder = new HttpServer();
-  ocspResponder.registerPrefixHandler("/", function(request, response) {
-    response.setStatusLine(request.httpVersion, 200, "OK");
-    response.setHeader("Content-Type", "application/ocsp-response");
-    response.write(ocspResponseBadSignature);
-    ocspRequestCount++;
-  });
-  ocspResponder.start(8080);
-
   // We don't actually make use of stapling in this test. This is just how we
   // get a TLS connection.
   add_tls_server_setup("OCSPStaplingServer");
+
+  let args = [["bad-signature", "localhostAndExampleCom", "unused"]];
+  let ocspResponses = generateOCSPResponses(args, "tlsserver");
+  let ocspResponseBadSignature = ocspResponses[0];
+
+  let ocspResponder = new HttpServer();
+  ocspResponder.registerPrefixHandler("/", function (request, response) {
+    response.setStatusLine(request.httpVersion, 200, "OK");
+    response.setHeader("Content-Type", "application/ocsp-response");
+    response.write(ocspResponseBadSignature);
+    gOCSPRequestCount++;
+  });
+  ocspResponder.start(8080);
+
+  add_tests_in_mode(true);
+  add_tests_in_mode(false);
+
+  add_test(function () { ocspResponder.stop(run_next_test); });
+
+  run_next_test();
+}
+
+function add_tests_in_mode(useInsanity)
+{
+  add_test(function () {
+    Services.prefs.setBoolPref("security.use_insanity_verification",
+                               useInsanity);
+    run_next_test();
+  });
+
   add_connection_test("ocsp-stapling-none.example.com",
                       getXPCOMStatusFromNSS(SEC_ERROR_OCSP_INVALID_SIGNING_CERT));
   // bug 964493 - using a cached OCSP response with a bad signature would cause
@@ -38,7 +56,11 @@ function run_test() {
   // of the error handling code.
   add_connection_test("ocsp-stapling-none.example.com",
                       getXPCOMStatusFromNSS(SEC_ERROR_OCSP_INVALID_SIGNING_CERT));
-  add_test(function() { ocspResponder.stop(run_next_test); });
-  add_test(function() { do_check_eq(ocspRequestCount, 1); run_next_test(); });
-  run_next_test();
+  add_test(function () {
+    // XXX(bug 915932): special case for insanity::pkix due to the temporary
+    // lack of an OCSP cache.
+    do_check_eq(gOCSPRequestCount, useInsanity ? 2 : 1);
+    gOCSPRequestCount = 0;
+    run_next_test();
+  });
 }
