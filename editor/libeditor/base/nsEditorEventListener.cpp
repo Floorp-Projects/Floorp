7bc2f1f0340d911d187fa7e2af1df5dfba066df9
Ehsan Akhgari <ehsan@mozilla.com>
2014-02-16 19:09:52 -0500

    Bug 966155 - Ensure that we handle the native key bindings for all key events in designMode documents. r=Neil, a=sledru

--- editor/libeditor/base/nsEditorEventListener.cpp
+++ editor/libeditor/base/nsEditorEventListener.cpp
@@ -1034,6 +1034,12 @@ nsEditorEventListener::ShouldHandleNativeKeyBindings(nsIDOMEvent* aKeyEvent)
     return false;
   }
 
+  nsCOMPtr<nsIDocument> doc = mEditor->GetDocument();
+  if (doc->HasFlag(NODE_IS_EDITABLE)) {
+    // Don't need to perform any checks in designMode documents.
+    return true;
+  }
+
   nsIContent* editingHost = htmlEditor->GetActiveEditingHost();
   if (!editingHost) {
     return false;
