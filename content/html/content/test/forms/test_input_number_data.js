d1ff41df15dcf44384b2e761b953ccc5450eabd6
Jonathan Watt <jwatt@jwatt.org>
2014-02-18 20:00:35 +0000

    Bug 969928 - Fix a bug in the de-localization code that can cause validation issues with locales that use '.' as their grouping separator. r=dholbert

--- content/html/content/test/forms/test_input_number_data.js
+++ content/html/content/test/forms/test_input_number_data.js
@@ -16,6 +16,15 @@ var tests = [
     langTag: "de", inputWithGrouping: "123.456,78",
     inputWithoutGrouping: "123456,78", value: 123456.78
   },
+  // Extra german test to check that a locale that uses '.' as its grouping
+  // separator doesn't result in it being invalid (due to step mismatch) due
+  // to the de-localization code mishandling numbers that look like other
+  // numbers formatted for English speakers (i.e. treating this as 123.456
+  // instead of 123456):
+  { desc: "German (test 2)",
+    langTag: "de", inputWithGrouping: "123.456",
+    inputWithoutGrouping: "123456", value: 123456
+  },
   { desc: "Hebrew",
     langTag: "he", inputWithGrouping: "123,456.78",
     inputWithoutGrouping: "123456.78", value: 123456.78
