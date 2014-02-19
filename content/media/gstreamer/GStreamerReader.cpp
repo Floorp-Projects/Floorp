250690408628a640d8bde52619b94bd49a7a0dd6
Alessandro Decina <alessandro.d@gmail.com>
2014-02-19 11:30:09 -0500

    Bug 973744 - Remove matroska seek hack in the gstreamer media backend. r=edwin

--- content/media/gstreamer/GStreamerReader.cpp
+++ content/media/gstreamer/GStreamerReader.cpp
@@ -372,27 +372,6 @@ nsresult GStreamerReader::ReadMetadata(MediaInfo* aInfo,
     /* we couldn't get this to play */
     return ret;
 
-  /* FIXME: workaround for a bug in matroskademux. This seek makes matroskademux
-   * parse the index */
-  LOG(PR_LOG_DEBUG, ("doing matroskademux seek hack"));
-  if (gst_element_seek_simple(mPlayBin, GST_FORMAT_TIME,
-        GST_SEEK_FLAG_FLUSH, 0)) {
-    /* after a seek we need to wait again for ASYNC_DONE */
-    message = gst_bus_timed_pop_filtered(mBus, 5 * GST_SECOND,
-       (GstMessageType)(GST_MESSAGE_ASYNC_DONE | GST_MESSAGE_ERROR));
-    if (message == NULL || GST_MESSAGE_TYPE(message) != GST_MESSAGE_ASYNC_DONE) {
-      LOG(PR_LOG_DEBUG, ("matroskademux seek hack failed: %p", message));
-      gst_element_set_state(mPlayBin, GST_STATE_NULL);
-      if (message) {
-        gst_message_unref(message);
-      }
-      return NS_ERROR_FAILURE;
-    }
-    LOG(PR_LOG_DEBUG, ("matroskademux seek hack completed"));
-  } else {
-    LOG(PR_LOG_DEBUG, ("matroskademux seek hack failed (non fatal)"));
-  }
-
   bool isMP3 = mDecoder->GetResource()->GetContentType().EqualsASCII(AUDIO_MP3);
   if (isMP3) {
     ParseMP3Headers();
