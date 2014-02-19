e68e8378a68fe90bfdb4f813b0e284ea802c4d40
Richard Newman <rnewman@mozilla.com>
2014-02-18 19:20:27 -0800

    Bug 917942 - Create a JS interface to Sync configuration. r=margaret

---
+++ mobile/android/base/tests/testAccounts.java
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.tests;
+
+import android.app.Activity;
+import org.mozilla.gecko.*;
+import org.mozilla.gecko.fxa.activities.FxAccountGetStartedActivity;
+
+public class testAccounts extends JavascriptTest {
+    public testAccounts() {
+        super("testAccounts.js");
+    }
+
+    @Override
+    public void testJavascript() throws Exception {
+        super.testJavascript();
+        Activity activity = mSolo.getCurrentActivity();
+        System.out.println("Current activity: " + activity);
+        mAsserter.ok(activity instanceof FxAccountGetStartedActivity, "checking activity", "setup activity launched");
+    }
+}
