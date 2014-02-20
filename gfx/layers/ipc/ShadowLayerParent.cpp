755940561fe649e2fe8a700aedcd267ddc02e67c
Benoit Jacob <bjacob@mozilla.com>
2014-02-20 11:40:55 -0500

    Bug 968833 - 1/2 - Make PLayerTransaction check actual layer types before casting - r=jrmuizel

--- gfx/layers/ipc/ShadowLayerParent.cpp
+++ gfx/layers/ipc/ShadowLayerParent.cpp
@@ -10,6 +10,12 @@
 #include "nsDebug.h"                    // for NS_RUNTIMEABORT
 #include "nsISupportsImpl.h"            // for Layer::AddRef, etc
 
+#include "mozilla/layers/ThebesLayerComposite.h"
+#include "mozilla/layers/CanvasLayerComposite.h"
+#include "mozilla/layers/ColorLayerComposite.h"
+#include "mozilla/layers/ImageLayerComposite.h"
+#include "mozilla/layers/ContainerLayerComposite.h"
+
 namespace mozilla {
 namespace layers {
 
@@ -39,10 +45,52 @@ ShadowLayerParent::Destroy()
   }
 }
 
-ContainerLayer*
-ShadowLayerParent::AsContainer() const
+ContainerLayerComposite*
+ShadowLayerParent::AsContainerLayerComposite() const
 {
-  return static_cast<ContainerLayer*>(AsLayer());
+  return mLayer && mLayer->GetType() == Layer::TYPE_CONTAINER
+         ? static_cast<ContainerLayerComposite*>(mLayer.get())
+         : nullptr;
+}
+
+CanvasLayerComposite*
+ShadowLayerParent::AsCanvasLayerComposite() const
+{
+  return mLayer && mLayer->GetType() == Layer::TYPE_CANVAS
+         ? static_cast<CanvasLayerComposite*>(mLayer.get())
+         : nullptr;
+}
+
+ColorLayerComposite*
+ShadowLayerParent::AsColorLayerComposite() const
+{
+  return mLayer && mLayer->GetType() == Layer::TYPE_COLOR
+         ? static_cast<ColorLayerComposite*>(mLayer.get())
+         : nullptr;
+}
+
+ImageLayerComposite*
+ShadowLayerParent::AsImageLayerComposite() const
+{
+  return mLayer && mLayer->GetType() == Layer::TYPE_IMAGE
+         ? static_cast<ImageLayerComposite*>(mLayer.get())
+         : nullptr;
+}
+
+RefLayerComposite*
+ShadowLayerParent::AsRefLayerComposite() const
+{
+  return mLayer && mLayer->GetType() == Layer::TYPE_REF
+         ? static_cast<RefLayerComposite*>(mLayer.get())
+         : nullptr;
+}
+
+ThebesLayerComposite*
+ShadowLayerParent::AsThebesLayerComposite() const
+{
+  return mLayer && mLayer->GetType() == Layer::TYPE_THEBES
+         ? static_cast<ThebesLayerComposite*>(mLayer.get())
+         : nullptr;
 }
 
 void
