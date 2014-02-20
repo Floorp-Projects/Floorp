df1f09f07244449618997e82e9b17dc89be7dffe
Andrew McCreight <amccreight@mozilla.com>
2014-02-19 16:10:58 -0800

    Backout bug 951491 on suspicion of causing Valgrind leaks

--- dom/base/nsGlobalWindow.h
+++ dom/base/nsGlobalWindow.h
@@ -1413,10 +1413,6 @@ protected:
   // should be displayed.
   bool                   mFocusByKeyOccurred : 1;
 
-  // Ensure that a call to ResumeTimeouts() after FreeInnerObjects() does nothing.
-  // This member is only used by inner windows.
-  bool                   mInnerObjectsFreed : 1;
-
   // Indicates whether this window wants gamepad input events
   bool                   mHasGamepad : 1;
 #ifdef MOZ_GAMEPAD
