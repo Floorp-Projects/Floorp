6861540ecaee9b44bf3820bf02417c9557b80d85
Sean Stangl <sstangl@mozilla.com>
2014-02-20 14:48:42 -0800

    Bug 933001 - Part 4/5 - Add SharedArrayBuffer tests.

---
+++ js/src/jit-test/tests/sharedbuf/is-zeroed.js
@@ -0,0 +1,12 @@
+// Test that the SharedArrayBuffer memory is properly zeroed.
+
+function f() {
+    var x = new SharedArrayBuffer(4096);
+    var y = new Int32Array(x);
+    assertEq(y[0], 0);
+    assertEq(y[1], 0);
+    assertEq(y[1023], 0);
+}
+
+if (typeof SharedArrayBuffer !== "undefined")
+    f();
