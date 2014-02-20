56569bb68e38c89128b892c5fbe4df2b451a7c35
Cameron McCormack <cam@mcc.id.au>
2014-02-20 14:57:02 +1100

    Bug 974749 - Make nsFrame::DumpFrameTree wrapper functions use stderr so they can go to logcat. r=tn

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -5249,12 +5249,12 @@ int32_t nsFrame::ContentIndexInContainer(const nsIFrame* aFrame)
 }
 
 /**
- * List a frame tree to stdout. Meant to be called from gdb.
+ * List a frame tree to stderr. Meant to be called from gdb.
  */
 void
 DebugListFrameTree(nsIFrame* aFrame)
 {
-  ((nsFrame*)aFrame)->List(stdout);
+  ((nsFrame*)aFrame)->List(stderr);
 }
 
 void
@@ -5384,13 +5384,13 @@ nsFrame::MakeFrameName(const nsAString& aType, nsAString& aResult) const
 void
 nsIFrame::DumpFrameTree()
 {
-  RootFrameList(PresContext(), stdout);
+  RootFrameList(PresContext(), stderr);
 }
 
 void
 nsIFrame::DumpFrameTreeLimited()
 {
-  List(stdout);
+  List(stderr);
 }
 
 void
