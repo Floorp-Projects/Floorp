587ec871fae883c9d0301397187fae8d1223db1d
Andrew McCreight <amccreight@mozilla.com>
2014-02-19 17:17:58 -0800

    Backout bug 966762 for Valgrind leaks

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -1806,14 +1806,6 @@ NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(nsGlobalWindow)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mCrypto)
 NS_IMPL_CYCLE_COLLECTION_UNLINK_END
 
-#ifdef DEBUG
-void
-nsGlobalWindow::RiskyUnlink()
-{
-  NS_CYCLE_COLLECTION_INNERNAME.Unlink(this);
-}
-#endif
-
 struct TraceData
 {
   const TraceCallbacks& callbacks;
