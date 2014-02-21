e9b39b6c363e6023dc28b6f44bb8aeff61542d12
Ralph Giles <giles@mozilla.com>
2014-02-21 13:57:00 -0800

    Bug 964559 - Expect empty string for canPlayType failure. r=doublec
    As the mpeg and dash tests already knew, canPlayType() returns an
    empty string ("") instead of "no" for unsupported formats. The
    can_play_no_foo tests have been broken for some time.

--- content/media/test/can_play_type_wave.js
+++ content/media/test/can_play_type_wave.js
@@ -1,6 +1,6 @@
 function check_wave(v, enabled) {
   function check(type, expected) {
-    is(v.canPlayType(type), enabled ? expected : "no", type);
+    is(v.canPlayType(type), enabled ? expected : "", type);
   }
 
   // Wave types
