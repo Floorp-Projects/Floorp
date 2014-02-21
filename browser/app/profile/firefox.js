0093a833c490bed1647bd9fe14679a255b761eff
Bill McCloskey <wmccloskey@mozilla.com>
2014-02-20 17:26:41 -0800

    Bug 974007 - Change OMTC behavior when browser.tabs.remote is set (r=mattwoodrow,felipe)

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -977,11 +977,11 @@ pref("dom.ipc.plugins.enabled.x86_64", true);
 pref("dom.ipc.plugins.enabled", true);
 #endif
 
-#if defined(NIGHTLY_BUILD) && defined(XP_MACOSX)
-// In Nightly, browser.tabs.remote is enabled on platforms that
-// support OMTC. However, users won't actually get remote tabs unless
-// they enable browser.tabs.remote.autostart or they use the "New OOP
-// Window" menu option.
+#if defined(NIGHTLY_BUILD)
+// browser.tabs.remote is enabled on nightly. However, users won't
+// actually get remote tabs unless they enable
+// browser.tabs.remote.autostart or they use the "New OOP Window" menu
+// option.
 pref("browser.tabs.remote", true);
 #else
 pref("browser.tabs.remote", false);
