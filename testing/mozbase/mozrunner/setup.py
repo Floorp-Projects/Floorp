ecea08e5af9f860820b14efa42cb401cdca3c088
Henrik Skupin <mail@hskupin.info>
2014-02-22 09:28:23 +0100

    Bug 975312 - Add missing version bump for mozrunner 5.35. r=ahal DONTBUILD

--- testing/mozbase/mozrunner/setup.py
+++ testing/mozbase/mozrunner/setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup
 
 PACKAGE_NAME = 'mozrunner'
-PACKAGE_VERSION = '5.34'
+PACKAGE_VERSION = '5.35'
 
 desc = """Reliable start/stop/configuration of Mozilla Applications (Firefox, Thunderbird, etc.)"""
 
