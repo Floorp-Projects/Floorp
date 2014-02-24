a65d2679e058ed6405d9c5e8fa80b7abf33eaac9
Robert O'Callahan <robert@ocallahan.org>
2014-02-24 20:17:58 +1300

    Bug 975931. Part 7: Enable APZC in reftests. r=dbaron

--- layout/tools/reftest/reftest.js
+++ layout/tools/reftest/reftest.js
@@ -267,6 +267,7 @@ this.OnRefTestLoad = function OnRefTestLoad(win)
     gBrowser.setAttribute("id", "browser");
     gBrowser.setAttribute("type", "content-primary");
     gBrowser.setAttribute("remote", gBrowserIsRemote ? "true" : "false");
+    gBrowser.setAttribute("mozasyncpanzoom", "true");
     // Make sure the browser element is exactly 800x1000, no matter
     // what size our window is
     gBrowser.setAttribute("style", "min-width: 800px; min-height: 1000px; max-width: 800px; max-height: 1000px");
