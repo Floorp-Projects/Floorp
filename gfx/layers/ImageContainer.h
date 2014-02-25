ead4d27e3d01c692568f7e6ff2d8af3ed228db31
Michael Wu <mwu@mozilla.com>
2014-02-24 22:37:51 -0500

    Bug 962670 - Store decoded images in VolatileBuffers, r=seth,jrmuizel

--- gfx/layers/ImageContainer.h
+++ gfx/layers/ImageContainer.h
@@ -26,6 +26,7 @@
 #include "nsSize.h"                     // for nsIntSize
 #include "nsTArray.h"                   // for nsTArray
 #include "mozilla/Atomics.h"
+#include "mozilla/WeakPtr.h"
 #include "nsThreadUtils.h"
 #include "mozilla/gfx/2D.h"
 #include "nsDataHashtable.h"
@@ -380,7 +381,7 @@ struct RemoteImageData {
  * updates the shared state to point to the new image and the old image
  * is immediately released (not true in Normal or Asynchronous modes).
  */
-class ImageContainer {
+class ImageContainer : public SupportsWeakPtr<ImageContainer> {
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(ImageContainer)
 public:
 
