ae602a246690bb8849ee8c16ac2a11beb72830b4
Jim Blandy <jimb@mozilla.com>
2014-02-26 15:16:19 -0800

    Bug 973130: Remove extraneous code from ActivationIterator::settle. r=hv1989
    The 'if' condition in this function can never be true, because it's negated
    in the immediately preceding 'while' condition.

--- js/src/vm/Stack.cpp
+++ js/src/vm/Stack.cpp
@@ -1401,9 +1401,8 @@ ActivationIterator::operator++()
 void
 ActivationIterator::settle()
 {
-    while (!done() && activation_->isJit() && !activation_->asJit()->isActive()) {
-        if (activation_->asJit()->isActive())
-            jitTop_ = activation_->asJit()->prevIonTop();
+    // Stop at the next active activation. No need to update jitTop_, since
+    // we don't iterate over an active jit activation.
+    while (!done() && activation_->isJit() && !activation_->asJit()->isActive())
         activation_ = activation_->prev();
-    }
 }
