8b18eca111ae0db111d7ebc67ec568ff73992b20
J. Ryan Stinnett <jryans@gmail.com>
2014-02-25 22:22:05 -0600

    Bug 976679 - Move event-emitter to toolkit. r=paul
    --HG--
    rename : browser/devtools/shared/event-emitter.js => toolkit/devtools/event-emitter.js
    rename : browser/devtools/shared/test/browser_eventemitter_basic.js => toolkit/devtools/tests/mochitest/test_eventemitter_basic.html

--- browser/devtools/sourceeditor/editor.js
+++ browser/devtools/sourceeditor/editor.js
@@ -19,7 +19,7 @@ const XUL_NS      = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.x
 const MAX_VERTICAL_OFFSET = 3;
 
 const {Promise: promise} = Cu.import("resource://gre/modules/Promise.jsm", {});
-const events  = require("devtools/shared/event-emitter");
+const events  = require("devtools/toolkit/event-emitter");
 
 Cu.import("resource://gre/modules/Services.jsm");
 const L10N = Services.strings.createBundle(L10N_BUNDLE);
