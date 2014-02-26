461b5556bdbc78c0a64b441b31f2cd49cc6b6709
Masayuki Nakano <masayuki@d-toybox.com>
2014-02-26 14:23:56 +0900

    Bug 975688 part.3 Rename nsDOMBeforeUnloadEvent to mozilla::dom::BeforeUnloadEvent r=smaug

--- dom/events/BeforeUnloadEvent.h
+++ dom/events/BeforeUnloadEvent.h
@@ -3,28 +3,31 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef nsDOMBeforeUnloadEvent_h__
-#define nsDOMBeforeUnloadEvent_h__
+#ifndef mozilla_dom_BeforeUnloadEvent_h_
+#define mozilla_dom_BeforeUnloadEvent_h_
 
 #include "nsIDOMBeforeUnloadEvent.h"
 #include "nsDOMEvent.h"
 #include "mozilla/dom/BeforeUnloadEventBinding.h"
 
-class nsDOMBeforeUnloadEvent : public nsDOMEvent,
-                               public nsIDOMBeforeUnloadEvent
+namespace mozilla {
+namespace dom {
+
+class BeforeUnloadEvent : public nsDOMEvent,
+                          public nsIDOMBeforeUnloadEvent
 {
 public:
-  nsDOMBeforeUnloadEvent(mozilla::dom::EventTarget* aOwner,
-                         nsPresContext* aPresContext,
-                         mozilla::WidgetEvent* aEvent)
-  : nsDOMEvent(aOwner, aPresContext, aEvent)
+  BeforeUnloadEvent(EventTarget* aOwner,
+                    nsPresContext* aPresContext,
+                    WidgetEvent* aEvent)
+    : nsDOMEvent(aOwner, aPresContext, aEvent)
   {
   }
 
   virtual JSObject* WrapObject(JSContext* aCx,
                                JS::Handle<JSObject*> aScope) MOZ_OVERRIDE
   {
-    return mozilla::dom::BeforeUnloadEventBinding::Wrap(aCx, aScope, this);
+    return BeforeUnloadEventBinding::Wrap(aCx, aScope, this);
   }
 
   NS_DECL_ISUPPORTS_INHERITED
@@ -34,8 +37,12 @@ public:
 
   // nsIDOMBeforeUnloadEvent Interface
   NS_DECL_NSIDOMBEFOREUNLOADEVENT
+
 protected:
   nsString mText;
 };
 
-#endif // nsDOMBeforeUnloadEvent_h__
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_BeforeUnloadEvent_h_
