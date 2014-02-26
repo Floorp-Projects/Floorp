456674b133e483a9998d58b69ef1cbaf6edce855
Jim Blandy <jimb@mozilla.com>
2014-02-26 15:20:00 -0800

    Bug 969786: Add an 'introduction script' compilation option to ReadOnlyCompileOptions, OwningCompileOptions, and CompileOptions. r=sfink

--- js/src/jsfun.cpp
+++ js/src/jsfun.cpp
@@ -1532,7 +1532,7 @@ FunctionConstructor(JSContext *cx, unsigned argc, Value *vp, GeneratorKind gener
            .setFileAndLine(filename, 1)
            .setNoScriptRval(false)
            .setCompileAndGo(true)
-           .setIntroductionInfo(introducerFilename, introductionType, lineno, pcOffset);
+           .setIntroductionInfo(introducerFilename, introductionType, lineno, script, pcOffset);
 
     unsigned n = args.length() ? args.length() - 1 : 0;
     if (n > 0) {
