3a40cfc9bb0fe88ff21f8de843607d22a4fc89fd
Masayuki Nakano <masayuki@d-toybox.com>
2014-02-26 14:23:57 +0900

    Bug 975688 part.5 Rename nsDOMCommandEvent to mozilla::dom::CommandEvent r=smaug

--- dom/events/CommandEvent.h
+++ dom/events/CommandEvent.h
@@ -3,21 +3,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef nsDOMCommandEvent_h__
-#define nsDOMCommandEvent_h__
+#ifndef mozilla_dom_CommandEvent_h_
+#define mozilla_dom_CommandEvent_h_
 
 #include "nsIDOMCommandEvent.h"
 #include "nsDOMEvent.h"
 #include "mozilla/dom/CommandEventBinding.h"
 #include "mozilla/EventForwards.h"
 
-class nsDOMCommandEvent : public nsDOMEvent,
-                          public nsIDOMCommandEvent
+namespace mozilla {
+namespace dom {
+
+class CommandEvent : public nsDOMEvent,
+                     public nsIDOMCommandEvent
 {
 public:
-  nsDOMCommandEvent(mozilla::dom::EventTarget* aOwner,
-                    nsPresContext* aPresContext,
-                    mozilla::WidgetCommandEvent* aEvent);
+  CommandEvent(EventTarget* aOwner,
+               nsPresContext* aPresContext,
+               WidgetCommandEvent* aEvent);
 
   NS_DECL_ISUPPORTS_INHERITED
 
@@ -29,17 +32,20 @@ public:
   virtual JSObject* WrapObject(JSContext* aCx,
                                JS::Handle<JSObject*> aScope) MOZ_OVERRIDE
   {
-    return mozilla::dom::CommandEventBinding::Wrap(aCx, aScope, this);
+    return CommandEventBinding::Wrap(aCx, aScope, this);
   }
 
   void InitCommandEvent(const nsAString& aType,
                         bool aCanBubble,
                         bool aCancelable,
                         const nsAString& aCommand,
-                        mozilla::ErrorResult& aRv)
+                        ErrorResult& aRv)
   {
     aRv = InitCommandEvent(aType, aCanBubble, aCancelable, aCommand);
   }
 };
 
-#endif // nsDOMCommandEvent_h__
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_CommandEvent_h_
