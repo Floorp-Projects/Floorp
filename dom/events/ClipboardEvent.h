33f3ae50bcc8599cf1db7c5b5249ddd736f3c113
Masayuki Nakano <masayuki@d-toybox.com>
2014-02-26 14:23:56 +0900

    Bug 975688 part.4 Rename nsDOMClipboardEvent to mozilla::dom::ClipboardEvent r=smaug

--- dom/events/ClipboardEvent.h
+++ dom/events/ClipboardEvent.h
@@ -3,21 +3,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef nsDOMClipboardEvent_h_
-#define nsDOMClipboardEvent_h_
+#ifndef mozilla_dom_ClipboardEvent_h_
+#define mozilla_dom_ClipboardEvent_h_
 
 #include "nsIDOMClipboardEvent.h"
 #include "nsDOMEvent.h"
 #include "mozilla/EventForwards.h"
 #include "mozilla/dom/ClipboardEventBinding.h"
 
-class nsDOMClipboardEvent : public nsDOMEvent,
-                            public nsIDOMClipboardEvent
+namespace mozilla {
+namespace dom {
+
+class ClipboardEvent : public nsDOMEvent,
+                       public nsIDOMClipboardEvent
 {
 public:
-  nsDOMClipboardEvent(mozilla::dom::EventTarget* aOwner,
-                      nsPresContext* aPresContext,
-                      mozilla::InternalClipboardEvent* aEvent);
+  ClipboardEvent(EventTarget* aOwner,
+                 nsPresContext* aPresContext,
+                 InternalClipboardEvent* aEvent);
 
   NS_DECL_ISUPPORTS_INHERITED
 
@@ -29,16 +32,19 @@ public:
   virtual JSObject* WrapObject(JSContext* aCx,
                                JS::Handle<JSObject*> aScope) MOZ_OVERRIDE
   {
-    return mozilla::dom::ClipboardEventBinding::Wrap(aCx, aScope, this);
+    return ClipboardEventBinding::Wrap(aCx, aScope, this);
   }
 
-  static already_AddRefed<nsDOMClipboardEvent>
-  Constructor(const mozilla::dom::GlobalObject& aGlobal,
+  static already_AddRefed<ClipboardEvent>
+  Constructor(const GlobalObject& aGlobal,
               const nsAString& aType,
-              const mozilla::dom::ClipboardEventInit& aParam,
-              mozilla::ErrorResult& aRv);
+              const ClipboardEventInit& aParam,
+              ErrorResult& aRv);
 
   nsIDOMDataTransfer* GetClipboardData();
 };
 
-#endif // nsDOMClipboardEvent_h_
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_ClipboardEvent_h_
