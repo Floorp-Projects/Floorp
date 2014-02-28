67371b1fae3b245332ae0097f890c85ff954380d
Wes Kocher <wkocher@mozilla.com>
2014-02-28 14:33:22 -0800

    Backed out 2 changesets (bug 969382) for potentially breaking the build on a CLOSED TREE
    Backed out changeset 70fcde1101a1 (bug 969382)
    Backed out changeset 8eb38600d76a (bug 969382)

--- js/src/vm/ErrorObject.cpp
+++ js/src/vm/ErrorObject.cpp
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "vm/ErrorObject-inl.h"
+#include "vm/ErrorObject.h"
 
 #include "jsexn.h"
 
@@ -125,7 +125,7 @@ js::ErrorObject::getOrCreateErrorReport(JSContext *cx)
 
     // Filename.
     JSAutoByteString filenameStr;
-    if (!filenameStr.encodeLatin1(cx, fileName(cx)))
+    if (!filenameStr.encodeLatin1(cx, fileName()))
         return nullptr;
     report.filename = filenameStr.ptr();
 
