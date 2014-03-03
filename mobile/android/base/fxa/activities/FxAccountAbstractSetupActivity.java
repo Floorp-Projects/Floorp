853399f332702cf90b21ac24173ae560413b026f
Brad Lassey <blassey@mozilla.com>
2014-03-03 15:20:18 -0800

    Bug 962606 - Suggestions are present for password field, switch to use text transforms to reveal text. r=nalexander, a=sledru

--- mobile/android/base/fxa/activities/FxAccountAbstractSetupActivity.java
+++ mobile/android/base/fxa/activities/FxAccountAbstractSetupActivity.java
@@ -28,6 +28,8 @@ import android.content.Intent;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
+import android.text.method.SingleLineTransformationMethod;
 import android.util.Patterns;
 import android.view.KeyEvent;
 import android.view.View;
@@ -64,21 +66,24 @@ abstract public class FxAccountAbstractSetupActivity extends FxAccountAbstractAc
       @SuppressWarnings("deprecation")
       @Override
       public void onClick(View v) {
-        boolean isShown = 0 == (passwordEdit.getInputType() & InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        boolean isShown = passwordEdit.getTransformationMethod() instanceof SingleLineTransformationMethod;
+
         // Changing input type loses position in edit text; let's try to maintain it.
         int start = passwordEdit.getSelectionStart();
         int stop = passwordEdit.getSelectionEnd();
-        passwordEdit.setInputType(passwordEdit.getInputType() ^ InputType.TYPE_TEXT_VARIATION_PASSWORD);
-        passwordEdit.setSelection(start, stop);
+
         if (isShown) {
+          passwordEdit.setTransformationMethod(PasswordTransformationMethod.getInstance());
           showPasswordButton.setText(R.string.fxaccount_password_show);
           showPasswordButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.fxaccount_password_button_show_background));
           showPasswordButton.setTextColor(getResources().getColor(R.color.fxaccount_password_show_textcolor));
         } else {
+          passwordEdit.setTransformationMethod(SingleLineTransformationMethod.getInstance());
           showPasswordButton.setText(R.string.fxaccount_password_hide);
           showPasswordButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.fxaccount_password_button_hide_background));
           showPasswordButton.setTextColor(getResources().getColor(R.color.fxaccount_password_hide_textcolor));
         }
+        passwordEdit.setSelection(start, stop);
       }
     });
   }
