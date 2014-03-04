5f234a5847b662a46cdd3cf498724afc83d24192
Steven MacLeod <smacleod@mozilla.com>
2014-03-03 20:22:09 -0500

    Bug 971778 - Remove the richlistbox from the sync datatype selection in preferences. r=ttaubert, a=gavin

--- browser/components/preferences/sync.js
+++ browser/components/preferences/sync.js
@@ -147,11 +147,8 @@ let gSyncPane = {
         document.getElementById("fxaEmailAddress2").textContent = data.email;
         document.getElementById("fxaEmailAddress3").textContent = data.email;
         document.getElementById("fxaSyncComputerName").value = Weave.Service.clientsEngine.localName;
-        let enginesList = document.getElementById("fxaSyncEnginesList")
-        enginesList.disabled = enginesListDisabled;
-        // *sigh* - disabling the <richlistbox> draws each item as if it is disabled,
-        // but doesn't disable the checkboxes.
-        for (let checkbox of enginesList.querySelectorAll("checkbox")) {
+        let engines = document.getElementById("fxaSyncEngines")
+        for (let checkbox of engines.querySelectorAll("checkbox")) {
           checkbox.disabled = enginesListDisabled;
         }
       });
