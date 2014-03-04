55c4a7d9e4e1d336d43d0a03f9a4729366061595
Drew Willcoxon <adw@mozilla.com>
2014-03-03 17:39:35 -0800

    Bug 936195 - Australis UITour: Give webpage a way to query what UI targets are available. r=unfocused, a=sylvestre

---
+++ browser/modules/test/browser_UITour_availableTargets.js
@@ -0,0 +1,83 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+let gTestTab;
+let gContentAPI;
+let gContentWindow;
+
+Components.utils.import("resource:///modules/UITour.jsm");
+
+function test() {
+  UITourTest();
+}
+
+let tests = [
+  function test_availableTargets(done) {
+    gContentAPI.getConfiguration("availableTargets", (data) => {
+      ok_targets(data, [
+        "accountStatus",
+        "addons",
+        "appMenu",
+        "backForward",
+        "bookmarks",
+        "customize",
+        "help",
+        "home",
+        "pinnedTab",
+        "quit",
+        "search",
+        "searchProvider",
+        "urlbar",
+      ]);
+      ok(UITour.availableTargetsCache.has(window),
+         "Targets should now be cached");
+      done();
+    });
+  },
+
+  function test_availableTargets_changeWidgets(done) {
+    CustomizableUI.removeWidgetFromArea("bookmarks-menu-button");
+    ok(!UITour.availableTargetsCache.has(window),
+       "Targets should be evicted from cache after widget change");
+    gContentAPI.getConfiguration("availableTargets", (data) => {
+      ok_targets(data, [
+        "accountStatus",
+        "addons",
+        "appMenu",
+        "backForward",
+        "customize",
+        "help",
+        "home",
+        "pinnedTab",
+        "quit",
+        "search",
+        "searchProvider",
+        "urlbar",
+      ]);
+      ok(UITour.availableTargetsCache.has(window),
+         "Targets should now be cached again");
+      CustomizableUI.reset();
+      ok(!UITour.availableTargetsCache.has(window),
+         "Targets should not be cached after reset");
+      done();
+    });
+  },
+];
+
+function ok_targets(actualData, expectedTargets) {
+  // Depending on how soon after page load this is called, the selected tab icon
+  // may or may not be showing the loading throbber.  Check for its presence and
+  // insert it into expectedTargets if it's visible.
+  let selectedTabIcon =
+    document.getAnonymousElementByAttribute(gBrowser.selectedTab,
+                                            "anonid",
+                                            "tab-icon-image");
+  if (selectedTabIcon && UITour.isElementVisible(selectedTabIcon))
+    expectedTargets.push("selectedTabIcon");
+
+  ok(Array.isArray(actualData.targets), "data.targets should be an array");
+  is(actualData.targets.sort().toString(), expectedTargets.sort().toString(),
+     "Targets should be as expected");
+}
