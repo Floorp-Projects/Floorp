ba969eace3c7ced7e6fdae1fb15c544302bbf8ca
Josh Dover <gerfuls@gmail.com>
2014-03-04 15:15:44 -0800

    Bug 975055 - Show previous filter for back row item. r=lucasr

--- mobile/android/base/home/PanelViewAdapter.java
+++ mobile/android/base/home/PanelViewAdapter.java
@@ -6,6 +6,9 @@
 package org.mozilla.gecko.home;
 
 import org.mozilla.gecko.home.HomeConfig.ItemType;
+import org.mozilla.gecko.home.PanelLayout.FilterManager;
+
+import org.mozilla.gecko.R;
 
 import android.content.Context;
 import android.database.Cursor;
@@ -14,8 +17,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 class PanelViewAdapter extends CursorAdapter {
+    private static final int VIEW_TYPE_ITEM = 0;
+    private static final int VIEW_TYPE_BACK = 1;
+
 	private final Context mContext;
 	private final ItemType mItemType;
+    private FilterManager mFilterManager;
 
     public PanelViewAdapter(Context context, ItemType itemType) {
         super(context, null, 0);
@@ -23,14 +30,83 @@ class PanelViewAdapter extends CursorAdapter {
         mItemType = itemType;
     }
 
+    public void setFilterManager(FilterManager manager) {
+        mFilterManager = manager;
+    }
+
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public final int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount() + (isShowingBack() ? 1 : 0);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isShowingBack() && position == 0) {
+            return VIEW_TYPE_BACK;
+        } else {
+            return VIEW_TYPE_ITEM;
+        }
+    }
+
+    @Override
+    public final View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = newView(parent.getContext(), position, parent);
+        }
+
+        bindView(convertView, position);
+        return convertView;
+    }
+
+    private View newView(Context context, int position, ViewGroup parent) {
+        if (getItemViewType(position) == VIEW_TYPE_BACK) {
+            return new PanelBackItemView(context);
+        } else {
+            return PanelItemView.create(context, mItemType);
+        }
+    }
+
+    private void bindView(View view, int position) {
+        if (isShowingBack()) {
+            if (position == 0) {
+                final PanelBackItemView item = (PanelBackItemView) view;
+                item.updateFromFilter(mFilterManager.getPreviousFilter());
+                return;
+            }
+
+            position--;
+        }
+
+        final Cursor cursor = getCursor(position);
         final PanelItemView item = (PanelItemView) view;
         item.updateFromCursor(cursor);
     }
 
+    private boolean isShowingBack() {
+        return (mFilterManager != null ? mFilterManager.canGoBack() : false);
+    }
+
+    private final Cursor getCursor(int position) {
+        final Cursor cursor = getCursor();
+        if (cursor == null || !cursor.moveToPosition(position)) {
+            throw new IllegalStateException("Couldn't move cursor to position " + position);
+        }
+
+        return cursor;
+    }
+
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return PanelItemView.create(mContext, mItemType);
+    public final void bindView(View view, Context context, Cursor cursor) {
+        // Do nothing.
+    }
+
+    @Override
+    public final View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
     }
 }
