46255bea46d5292aedc46eec8a16d9e7f2a4e375
Ryan VanderMeulen <ryanvm@gmail.com>
2014-03-05 11:55:36 -0500

    Backed out changeset cb67f9c0da24 (bug 978741) for Android bustage.

--- mobile/android/base/home/HomePager.java
+++ mobile/android/base/home/HomePager.java
@@ -44,6 +44,7 @@ public class HomePager extends ViewPager {
     private HomeBanner mHomeBanner;
     private int mDefaultPageIndex = -1;
 
+    private final ViewPager.OnPageChangeListener mPageChangeListener;
     private final OnAddPanelListener mAddPanelListener;
 
     private final HomeConfig mConfig;
@@ -131,7 +132,8 @@ public class HomePager extends ViewPager {
 
         mOriginalBackground = getBackground();
 
-        setOnPageChangeListener(new PageChangeListener());
+        mPageChangeListener = new PageChangeListener();
+        setOnPageChangeListener(mPageChangeListener);
     }
 
     @Override
@@ -258,10 +260,9 @@ public class HomePager extends ViewPager {
     public void setCurrentItem(int item, boolean smoothScroll) {
         super.setCurrentItem(item, smoothScroll);
 
-        // Android doesn't call onPageSelected when there is only one page. Make sure we activate
-        // the banner in this case.
-        if (mHomeBanner != null && getAdapter().getCount() == 1) {
-            mHomeBanner.setActive(true);
+        // Android doesn't call this when there is only one item
+        if (getAdapter().getCount() == 1 && mPageChangeListener != null) {
+            mPageChangeListener.onPageSelected(0);
         }
     }
 
