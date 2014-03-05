dea41c84f3ff658a5a32dffa6cccd9e53d69e047
Yuan Xulei <xyuan@mozilla.com>
2014-03-05 16:40:48 +0800

    Bug 934367 - Implement createFile for Directory. r=dhylands

--- dom/filesystem/Directory.h
+++ dom/filesystem/Directory.h
@@ -21,10 +21,16 @@
 #ifdef CreateDirectory
 #undef CreateDirectory
 #endif
+// Undefine the macro of CreateFile to avoid Directory#CreateFile being replaced
+// by Directory#CreateFileW.
+#ifdef CreateFile
+#undef CreateFile
+#endif
 
 namespace mozilla {
 namespace dom {
 
+class CreateFileOptions;
 class FileSystemBase;
 class Promise;
 class StringOrFileOrDirectory;
@@ -54,6 +60,9 @@ public:
 
   void
   GetName(nsString& aRetval) const;
+
+  already_AddRefed<Promise>
+  CreateFile(const nsAString& aPath, const CreateFileOptions& aOptions);
 
   already_AddRefed<Promise>
   CreateDirectory(const nsAString& aPath);
