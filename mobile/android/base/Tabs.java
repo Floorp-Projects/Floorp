3a586081542934cadd425dd15fac12f26e0dd82a
Christopher Su <csu@uw.edu>
2014-03-05 09:05:07 -0500

    Bug 943629 - Remove unnecessary comment in Tabs. r=mcomella

--- mobile/android/base/Tabs.java
+++ mobile/android/base/Tabs.java
@@ -567,7 +567,6 @@ public class Tabs implements GeckoEventListener {
         notifyListeners(tab, msg, "");
     }
 
-    // Throws if not initialized.
     public void notifyListeners(final Tab tab, final TabEvents msg, final Object data) {
         if (tab == null &&
             msg != TabEvents.RESTORED) {
