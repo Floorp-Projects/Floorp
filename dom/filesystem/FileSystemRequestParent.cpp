09d9e522a7b75e38cde874143e8fdb4f6ed21b9d
Yuan Xulei <xyuan@mozilla.com>
2014-03-05 16:40:48 +0800

    Bug 934367 - Implement createFile for Directory. r=dhylands

--- dom/filesystem/FileSystemRequestParent.cpp
+++ dom/filesystem/FileSystemRequestParent.cpp
@@ -6,6 +6,7 @@
 #include "mozilla/dom/FileSystemRequestParent.h"
 
 #include "CreateDirectoryTask.h"
+#include "CreateFileTask.h"
 #include "GetFileOrDirectoryTask.h"
 #include "RemoveTask.h"
 
@@ -40,6 +41,7 @@ FileSystemRequestParent::Dispatch(ContentParent* aParent,
   switch (aParams.type()) {
 
     FILESYSTEM_REQUEST_PARENT_DISPATCH_ENTRY(CreateDirectory)
+    FILESYSTEM_REQUEST_PARENT_DISPATCH_ENTRY(CreateFile)
     FILESYSTEM_REQUEST_PARENT_DISPATCH_ENTRY(GetFileOrDirectory)
     FILESYSTEM_REQUEST_PARENT_DISPATCH_ENTRY(Remove)
 
