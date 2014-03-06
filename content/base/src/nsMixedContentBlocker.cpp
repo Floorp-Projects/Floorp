2b755e46f61925718abfc25d6134a4cb62ffed1c
Richard Barnes <rbarnes@mozilla.com>
2014-03-06 08:52:50 -0500

    Bug 936340 - Implement navigator.sendBeacon. r=sicking

--- content/base/src/nsMixedContentBlocker.cpp
+++ content/base/src/nsMixedContentBlocker.cpp
@@ -242,6 +242,8 @@ nsMixedContentBlocker::ShouldLoad(uint32_t aContentType,
   // spoofing attacks (e.g. make a "grant permission" button look like a
   // "refuse permission" button).
   //
+  // TYPE_BEACON: Beacon requests are similar to TYPE_PING, but are default on.
+  //
   // TYPE_WEBSOCKET: The Websockets API requires browsers to
   // reject mixed-content websockets: "If secure is false but the origin of
   // the entry script has a scheme component that is itself a secure protocol,
@@ -285,6 +287,7 @@ nsMixedContentBlocker::ShouldLoad(uint32_t aContentType,
     case TYPE_MEDIA:
     case TYPE_OBJECT_SUBREQUEST:
     case TYPE_PING:
+    case TYPE_BEACON:
       classification = eMixedDisplay;
       break;
 
