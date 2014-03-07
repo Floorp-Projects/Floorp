f3e08fea6f86f356b4295f189b47cd0880365cf0
Bill McCloskey <wmccloskey@mozilla.com>
2014-03-06 18:25:09 -0800

    Bug 972728 - Invalidate more often in basic compositor (r=mattwoodrow)

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -2050,9 +2050,7 @@ nsWindow::OnExposeEvent(cairo_t *cr)
         ? static_cast<ClientLayerManager*>(GetLayerManager())
         : nullptr;
 
-    if (clientLayers && mCompositorParent &&
-        !gdk_screen_is_composited(gdk_window_get_screen(mGdkWindow)))
-    {
+    if (clientLayers && mCompositorParent) {
         // We need to paint to the screen even if nothing changed, since if we
         // don't have a compositing window manager, our pixels could be stale.
         clientLayers->SetNeedsComposite(true);
