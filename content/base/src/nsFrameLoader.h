b7fe13ed8d180538e7214c782f2d131e91847acb
Blake Kaplan <mrbkap@gmail.com>
2014-03-10 08:57:07 -0400

    Bug 980586 - Remove old e10s prompt code. r=smaug

--- content/base/src/nsFrameLoader.h
+++ content/base/src/nsFrameLoader.h
@@ -431,7 +431,6 @@ private:
   // it may lose the flag.
   bool mNetworkCreated : 1;
 
-  bool mDelayRemoteDialogs : 1;
   bool mRemoteBrowserShown : 1;
   bool mRemoteFrame : 1;
   bool mClipSubdocument : 1;
