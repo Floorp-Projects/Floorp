05bc257d5a9b631c279a92349b6825b7421c3fec
Reuben Morais <reuben.morais@gmail.com>
2014-03-11 19:41:48 -0300

    Bug 946239 - Divert download channels to the parent process. r=bz

--- uriloader/exthandler/ExternalHelperAppParent.h
+++ uriloader/exthandler/ExternalHelperAppParent.h
@@ -8,6 +8,7 @@
 #include "nsIChannel.h"
 #include "nsIMultiPartChannel.h"
 #include "nsIResumableChannel.h"
+#include "nsIStreamListener.h"
 #include "nsHashPropertyBag.h"
 
 namespace IPC {
@@ -20,6 +21,10 @@ namespace ipc {
 class OptionalURIParams;
 } // namespace ipc
 
+namespace net {
+class PChannelDiverterParent;
+} // namespace net
+
 namespace dom {
 
 class ContentParent;
@@ -30,6 +35,7 @@ class ExternalHelperAppParent : public PExternalHelperAppParent
                               , public nsIChannel
                               , public nsIMultiPartChannel
                               , public nsIResumableChannel
+                              , public nsIStreamListener
 {
     typedef mozilla::ipc::OptionalURIParams OptionalURIParams;
 
@@ -39,12 +45,16 @@ public:
     NS_DECL_NSICHANNEL
     NS_DECL_NSIMULTIPARTCHANNEL
     NS_DECL_NSIRESUMABLECHANNEL
+    NS_DECL_NSISTREAMLISTENER
+    NS_DECL_NSIREQUESTOBSERVER
 
     bool RecvOnStartRequest(const nsCString& entityID) MOZ_OVERRIDE;
     bool RecvOnDataAvailable(const nsCString& data,
                              const uint64_t& offset,
                              const uint32_t& count) MOZ_OVERRIDE;
     bool RecvOnStopRequest(const nsresult& code) MOZ_OVERRIDE;
+
+    bool RecvDivertToParentUsing(PChannelDiverterParent* diverter) MOZ_OVERRIDE;
 
     ExternalHelperAppParent(const OptionalURIParams& uri, const int64_t& contentLength);
     void Init(ContentParent *parent,
@@ -57,10 +67,16 @@ public:
               PBrowserParent* aBrowser);
     virtual ~ExternalHelperAppParent();
 
+protected:
+  virtual void ActorDestroy(ActorDestroyReason why) MOZ_OVERRIDE;
+  void Delete();
+
 private:
   nsCOMPtr<nsIStreamListener> mListener;
   nsCOMPtr<nsIURI> mURI;
   bool mPending;
+  DebugOnly<bool> mDiverted;
+  bool mIPCClosed;
   nsLoadFlags mLoadFlags;
   nsresult mStatus;
   int64_t mContentLength;
