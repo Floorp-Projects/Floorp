842db7ac2ae7580959e97ede8d637a0ea8987503
Andy Wingo <wingo@igalia.com>
2014-03-11 11:05:07 +0100

    Bug 981522 - Disable Ion-compilation of unalised let-bindings in Aurora  r=jandem a=sylvestre

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -1744,9 +1744,6 @@ IonBuilder::inspectOpcode(JSOp op)
       case JSOP_INSTANCEOF:
         return jsop_instanceof();
 
-      case JSOP_DEBUGLEAVEBLOCK:
-        return true;
-
       default:
 #ifdef DEBUG
         return abort("Unsupported opcode: %s (line %d)", js_CodeName[op], info().lineno(pc));
