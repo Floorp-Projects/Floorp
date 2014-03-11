f4b05c79c672c0c7416efac4ce1ff8c175c5453a
Mark Hammond <mhammond@skippinet.com.au>
2014-03-10 18:33:00 -0700

    Bug 956605 - Have about:accounts do the right thing based on user logged in states. r=ckarlof, a=sledru

--- browser/base/content/test/general/content_aboutAccounts.js
+++ browser/base/content/test/general/content_aboutAccounts.js
@@ -5,9 +5,16 @@
 // This file is loaded as a "content script" for browser_aboutAccounts tests
 "use strict";
 
-addEventListener("DOMContentLoaded", function domContentLoaded(event) {
-  removeEventListener("DOMContentLoaded", domContentLoaded);
+addEventListener("load", function load(event) {
+  if (event.target != content.document) {
+    return;
+  }
+//  content.document.removeEventListener("load", load, true);
   sendAsyncMessage("test:document:load");
+}, true);
+
+addEventListener("DOMContentLoaded", function domContentLoaded(event) {
+  removeEventListener("DOMContentLoaded", domContentLoaded, true);
   let iframe = content.document.getElementById("remote");
   iframe.addEventListener("load", function iframeLoaded(event) {
     if (iframe.contentWindow.location.href == "about:blank" ||
@@ -17,4 +24,25 @@ addEventListener("DOMContentLoaded", function domContentLoaded(event) {
     iframe.removeEventListener("load", iframeLoaded, true);
     sendAsyncMessage("test:iframe:load", {url: iframe.getAttribute("src")});
   }, true);
+}, true);
+
+// Return the visibility state of a list of ids.
+addMessageListener("test:check-visibilities", function (message) {
+  let result = {};
+  for (let id of message.data.ids) {
+    let elt = content.document.getElementById(id);
+    if (elt) {
+      let displayStyle = content.window.getComputedStyle(elt).display;
+      if (displayStyle == 'none') {
+        result[id] = false;
+      } else if (displayStyle == 'block') {
+        result[id] = true;
+      } else {
+        result[id] = "strange: " + displayStyle; // tests should fail!
+      }
+    } else {
+      result[id] = "doesn't exist: " + id;
+    }
+  }
+  sendAsyncMessage("test:check-visibilities-response", result);
 });
