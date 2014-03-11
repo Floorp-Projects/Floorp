e2ae2138bcc205293be70c03dc01b9033a76e5f0
Andrea Marchesini <amarchesini@mozilla.com>
2014-03-11 10:46:04 +0000

    Bug 923247 - patch 1 - window in the AudioChannelAgent, r=mchen

--- content/html/content/src/HTMLMediaElement.cpp
+++ content/html/content/src/HTMLMediaElement.cpp
@@ -3879,9 +3879,11 @@ void HTMLMediaElement::UpdateAudioChannelPlayingState()
       nsCOMPtr<nsIDOMHTMLVideoElement> video = do_QueryObject(this);
       // Use a weak ref so the audio channel agent can't leak |this|.
       if (AUDIO_CHANNEL_NORMAL == mAudioChannelType && video) {
-        mAudioChannelAgent->InitWithVideo(mAudioChannelType, this, true);
+        mAudioChannelAgent->InitWithVideo(OwnerDoc()->GetWindow(),
+                                          mAudioChannelType, this, true);
       } else {
-        mAudioChannelAgent->InitWithWeakCallback(mAudioChannelType, this);
+        mAudioChannelAgent->InitWithWeakCallback(OwnerDoc()->GetWindow(),
+                                                 mAudioChannelType, this);
       }
       mAudioChannelAgent->SetVisibilityState(!OwnerDoc()->Hidden());
     }
