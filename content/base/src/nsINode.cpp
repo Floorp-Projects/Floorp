2347f2898187ef3f2e9d7980d3dc317fbefd115b
Robert O'Callahan <robert@ocallahan.org>
2014-03-12 09:11:38 +0800

    Bug 917755. Part 5.5: Implement getBoxQuads DOM API. r=jst

--- content/base/src/nsINode.cpp
+++ content/base/src/nsINode.cpp
@@ -103,6 +103,7 @@
 #include <algorithm>
 #include "nsGlobalWindow.h"
 #include "nsDOMMutationObserver.h"
+#include "GeometryUtils.h"
 
 using namespace mozilla;
 using namespace mozilla::dom;
@@ -1138,6 +1139,14 @@ nsINode::PreHandleEvent(EventChainPreVisitor& aVisitor)
   // This is only here so that we can use the NS_DECL_NSIDOMTARGET macro
   NS_ABORT();
   return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+void
+nsINode::GetBoxQuads(const BoxQuadOptions& aOptions,
+                     nsTArray<nsRefPtr<DOMQuad> >& aResult,
+                     mozilla::ErrorResult& aRv)
+{
+  mozilla::GetBoxQuads(this, aOptions, aResult, aRv);
 }
 
 nsresult
