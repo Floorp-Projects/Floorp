0e286453d69e3eca7daeed2b97154818d78ed4b6
Geoff Brown <gbrown@mozilla.com>
2014-03-12 09:42:00 -0600

    Bug 979612 - Fix testPictureLinkContextMenu on Android 2.3; r=margaret

--- mobile/android/base/tests/testPictureLinkContextMenu.java
+++ mobile/android/base/tests/testPictureLinkContextMenu.java
@@ -20,8 +20,8 @@ public class testPictureLinkContextMenu extends ContentContextMenuTest {
 
         PICTURE_PAGE_URL=getAbsoluteUrl("/robocop/robocop_picture_link.html");
         BLANK_PAGE_URL=getAbsoluteUrl("/robocop/robocop_blank_02.html");
-        inputAndLoadUrl(PICTURE_PAGE_URL);
-        waitForText(PICTURE_PAGE_TITLE);
+        loadAndPaint(PICTURE_PAGE_URL);
+        verifyPageTitle(PICTURE_PAGE_TITLE);
 
         verifyContextMenuItems(photoMenuItems);
         verifyCopyOption(photoMenuItems[0], "Firefox.jpg"); // Test the "Copy Image Location" option
