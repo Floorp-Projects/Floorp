8f42efaf535a12848db98030ed180497f68f0c9b
Jan Keromnes <janx@linux.com>
2014-03-13 09:53:04 -0400

    Bug 982707 - Move the "devtools.dump.emit" pref from firefox.js to all.js. r=ochameau

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -560,6 +560,9 @@ pref("devtools.debugger.forbid-certified-apps", true);
 // DevTools default color unit
 pref("devtools.defaultColorUnit", "hex");
 
+// Used for devtools debugging
+pref("devtools.dump.emit", false);
+
 // view source
 pref("view_source.syntax_highlight", true);
 pref("view_source.wrap_long_lines", false);
