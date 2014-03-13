c7a5c70ed19b721631ffdd94f1fcb7b01b8a2714
Vicamo Yang <vyang@mozilla.com>
2014-03-13 13:44:43 +0900

    Bug 944625 - B2G Emulator-x86: fix undeclared __NR_sendto, __NR_recvfrom. r=jld,kang

--- security/sandbox/linux/seccomp_filter.h
+++ security/sandbox/linux/seccomp_filter.h
@@ -111,6 +111,15 @@
 #define SECCOMP_WHITELIST_ARCH_DESKTOP_LINUX
 #endif
 
+/* Architecture-specific syscalls for B2G */
+#if defined(__i386__)
+#define SECCOMP_WHITELIST_ARCH_B2G_LOW
+#else
+#define SECCOMP_WHITELIST_ARCH_B2G_LOW \
+  ALLOW_SYSCALL(sendto), \
+  ALLOW_SYSCALL(recvfrom),
+#endif
+
 /* B2G specific syscalls */
 #if defined(MOZ_B2G)
 
@@ -125,8 +134,7 @@
   ALLOW_SYSCALL(poll),
 
 #define SECCOMP_WHITELIST_B2G_LOW \
-  ALLOW_SYSCALL(sendto), \
-  ALLOW_SYSCALL(recvfrom), \
+  SECCOMP_WHITELIST_ARCH_B2G_LOW \
   ALLOW_SYSCALL(getdents64), \
   ALLOW_SYSCALL(epoll_ctl), \
   ALLOW_SYSCALL(sched_yield), \
