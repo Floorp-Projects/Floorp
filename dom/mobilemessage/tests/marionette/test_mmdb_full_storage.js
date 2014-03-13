a4a7d832d9947aa9ba0522bac69f5f4f5d7df14d
Vicamo Yang <vyang@mozilla.com>
2014-03-13 13:44:53 +0900

    Bug 980354 - 2/2: add more test cases for storage full event. r=hsinyi

--- dom/mobilemessage/tests/marionette/test_mmdb_full_storage.js
+++ dom/mobilemessage/tests/marionette/test_mmdb_full_storage.js
@@ -8,82 +8,112 @@ const DBNAME = "test_mmdb_full_storage:" + newUUID();
 
 let gIsDiskFull = true;
 
-let gMessageToSave = {
-  type: "sms",
-  sender: "+0987654321",
-  receiver: "+1234567890",
-  body: "quick fox jump over the lazy dog",
-  deliveryStatusRequested: false,
-  timestamp: Date.now(),
-  iccId: "1029384756"
-};
+function newSavableMessage() {
+  return {
+    type: "sms",
+    sender: "+0987654321",
+    receiver: "+1234567890",
+    body: "quick fox jump over the lazy dog",
+    deliveryStatusRequested: false,
+    messageClass: "normal",
+    timestamp: Date.now(),
+    iccId: "1029384756"
+  };
+}
+
+function isFileNoDeviceSpaceError(aErrorResult) {
+  is(aErrorResult, Cr.NS_ERROR_FILE_NO_DEVICE_SPACE, "Database error code");
+}
+
+function isCallbackStorageFullError(aErrorCode) {
+  is(aErrorCode, Ci.nsIMobileMessageCallback.STORAGE_FULL_ERROR,
+     "nsIMobileMessageCallback error code");
+}
 
 function testSaveSendingMessage(aMmdb) {
   log("testSaveSendingMessage()");
 
-  let deferred = Promise.defer();
-
-  aMmdb.saveSendingMessage(gMessageToSave,
-                          { notify : function(aRv, aDomMessage) {
-    if (aRv === Cr.NS_ERROR_FILE_NO_DEVICE_SPACE) {
-      ok(true, "Forbidden due to storage full.");
-      deferred.resolve(aMmdb);
-    } else {
-      ok(false, "Unexpected result: " + aRv);
-      deferred.reject(aRv);
-    }
-  }});
-
-  return deferred.promise;
+  gIsDiskFull = true;
+  return saveSendingMessage(aMmdb, newSavableMessage())
+    // Resolved/rejected results are both [<Cr.NS_ERROR_FOO>, <DOM message>],
+    // and we need only the error code in both cases.
+    .then((aValue) => aValue[0],
+          (aValue) => aValue[0])
+    .then(isFileNoDeviceSpaceError);
 }
 
-function testSaveReceivingMessage(aMmdb) {
-  log("testSaveReceivingMessage()");
+function testSaveReceivedMessage(aMmdb) {
+  log("testSaveReceivedMessage()");
 
-  let deferred = Promise.defer();
-
-  aMmdb.saveReceivedMessage(gMessageToSave,
-                            { notify : function(aRv, aDomMessage) {
-    if (aRv === Cr.NS_ERROR_FILE_NO_DEVICE_SPACE) {
-      ok(true, "Forbidden due to storage full.");
-      deferred.resolve(aMmdb);
-    } else {
-      ok(false, "Unexpected result: " + aRv);
-      deferred.reject(aRv);
-    }
-  }});
-
-  return deferred.promise;
+  gIsDiskFull = true;
+  return saveReceivedMessage(aMmdb, newSavableMessage())
+    // Resolved/rejected results are both [<Cr.NS_ERROR_FOO>, <DOM message>],
+    // and we need only the error code in both cases.
+    .then((aValue) => aValue[0],
+          (aValue) => aValue[0])
+    .then(isFileNoDeviceSpaceError);
 }
 
-function testGetMessage(aMmdb) {
-  log("testGetMessage()");
+function testGetMessageRecordById(aMmdb) {
+  log("testGetMessageRecordById()");
 
-  let deferred = Promise.defer();
+  gIsDiskFull = false;
+  return saveReceivedMessage(aMmdb, newSavableMessage())
+    // Resolved result is [<Cr.NS_ERROR_FOO>, <DOM message>],
+    .then(function(aValue) {
+      let domMessage = aValue[1];
 
-  aMmdb.getMessage(1,
-                   { notifyGetMessageFailed : function(aRv) {
-    if (aRv === Ci.nsIMobileMessageCallback.NOT_FOUND_ERROR) {
-      ok(true, "Getting message successfully!");
-      deferred.resolve(aMmdb);
-    } else {
-      ok(false, "Unexpected result: " + aRv);
-      deferred.reject(aRv);
-    }
-  }});
+      gIsDiskFull = true;
+      return getMessageRecordById(aMmdb, domMessage.id);
+    });
+}
 
-  return deferred.promise;
+function testMarkMessageRead(aMmdb) {
+  log("testMarkMessageRead()");
+
+  gIsDiskFull = false;
+  return saveReceivedMessage(aMmdb, newSavableMessage())
+    // Resolved/rejected results are both [<Cr.NS_ERROR_FOO>, <DOM message>].
+    .then(function(aValue) {
+      let domMessage = aValue[1];
+
+      gIsDiskFull = true;
+      return markMessageRead(aMmdb, domMessage.id, true)
+        .then(null, (aValue) => aValue)
+        .then(isCallbackStorageFullError);
+    });
+}
+
+function testCreateMessageCursor(aMmdb) {
+  log("testCreateMessageCursor()");
+
+  gIsDiskFull = true;
+  return createMessageCursor(aMmdb, {}, false);
+}
+
+function testCreateThreadCursor(aMmdb) {
+  log("testCreateThreadCursor()");
+
+  gIsDiskFull = true;
+  return createThreadCursor(aMmdb);
 }
 
 startTestBase(function testCaseMain() {
 
   let mmdb = newMobileMessageDB();
-  mmdb.isDiskFull = function() {
-    return gIsDiskFull;
-  };
   return initMobileMessageDB(mmdb, DBNAME, 0)
-         .then(testSaveSendingMessage)
-         .then(testSaveReceivingMessage)
-         .then(testGetMessage)
-         .then(closeMobileMessageDB);
+    .then(function() {
+      mmdb.isDiskFull = function() {
+        return gIsDiskFull;
+      };
+    })
+
+    .then(() => testSaveSendingMessage(mmdb))
+    .then(() => testSaveReceivedMessage(mmdb))
+    .then(() => testGetMessageRecordById(mmdb))
+    .then(() => testMarkMessageRead(mmdb))
+    .then(() => testCreateMessageCursor(mmdb))
+    .then(() => testCreateThreadCursor(mmdb))
+
+    .then(() => closeMobileMessageDB(mmdb));
 });
