08302cdf42f862c00a59648a0d33c5487f2eb92c
Bill McCloskey <wmccloskey@mozilla.com>
2014-03-14 11:45:53 -0700

    Bug 938359 - [e10s] Support middle-click scroll (r=felipe)

---
+++ toolkit/content/browser-content.js
@@ -0,0 +1,234 @@
+/* -*- Mode: javascript; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+let Cc = Components.classes;
+let Ci = Components.interfaces;
+let Cu = Components.utils;
+
+Cu.import("resource://gre/modules/Services.jsm");
+
+var global = this;
+
+let ClickEventHandler = {
+  init: function init() {
+    this._scrollable = null;
+    this._scrolldir = "";
+    this._startX = null;
+    this._startY = null;
+    this._screenX = null;
+    this._screenY = null;
+    this._lastFrame = null;
+
+    Cc["@mozilla.org/eventlistenerservice;1"]
+      .getService(Ci.nsIEventListenerService)
+      .addSystemEventListener(global, "mousedown", this, true);
+
+    addMessageListener("Autoscroll:Stop", this);
+  },
+
+  isAutoscrollBlocker: function(node) {
+    let mmPaste = Services.prefs.getBoolPref("middlemouse.paste");
+    let mmScrollbarPosition = Services.prefs.getBoolPref("middlemouse.scrollbarPosition");
+
+    while (node) {
+      if ((node instanceof content.HTMLAnchorElement || node instanceof content.HTMLAreaElement) &&
+          node.hasAttribute("href")) {
+        return true;
+      }
+
+      if (mmPaste && (node instanceof content.HTMLInputElement ||
+                      node instanceof content.HTMLTextAreaElement)) {
+        return true;
+      }
+
+      if (node instanceof content.XULElement && mmScrollbarPosition
+          && (node.localName == "scrollbar" || node.localName == "scrollcorner")) {
+        return true;
+      }
+
+      node = node.parentNode;
+    }
+    return false;
+  },
+
+  startScroll: function(event) {
+    // this is a list of overflow property values that allow scrolling
+    const scrollingAllowed = ['scroll', 'auto'];
+
+    // go upward in the DOM and find any parent element that has a overflow
+    // area and can therefore be scrolled
+    for (this._scrollable = event.originalTarget; this._scrollable;
+         this._scrollable = this._scrollable.parentNode) {
+      // do not use overflow based autoscroll for <html> and <body>
+      // Elements or non-html elements such as svg or Document nodes
+      // also make sure to skip select elements that are not multiline
+      if (!(this._scrollable instanceof content.HTMLElement) ||
+          ((this._scrollable instanceof content.HTMLSelectElement) && !this._scrollable.multiple)) {
+        continue;
+      }
+
+      var overflowx = this._scrollable.ownerDocument.defaultView
+                          .getComputedStyle(this._scrollable, '')
+                          .getPropertyValue('overflow-x');
+      var overflowy = this._scrollable.ownerDocument.defaultView
+                          .getComputedStyle(this._scrollable, '')
+                          .getPropertyValue('overflow-y');
+      // we already discarded non-multiline selects so allow vertical
+      // scroll for multiline ones directly without checking for a
+      // overflow property
+      var scrollVert = this._scrollable.scrollTopMax &&
+        (this._scrollable instanceof content.HTMLSelectElement ||
+         scrollingAllowed.indexOf(overflowy) >= 0);
+
+      // do not allow horizontal scrolling for select elements, it leads
+      // to visual artifacts and is not the expected behavior anyway
+      if (!(this._scrollable instanceof content.HTMLSelectElement) &&
+          this._scrollable.scrollLeftMax &&
+          scrollingAllowed.indexOf(overflowx) >= 0) {
+        this._scrolldir = scrollVert ? "NSEW" : "EW";
+        break;
+      } else if (scrollVert) {
+        this._scrolldir = "NS";
+        break;
+      }
+    }
+
+    if (!this._scrollable) {
+      this._scrollable = event.originalTarget.ownerDocument.defaultView;
+      if (this._scrollable.scrollMaxX > 0) {
+        this._scrolldir = this._scrollable.scrollMaxY > 0 ? "NSEW" : "EW";
+      } else if (this._scrollable.scrollMaxY > 0) {
+        this._scrolldir = "NS";
+      } else {
+        this._scrollable = null; // abort scrolling
+        return;
+      }
+    }
+
+    Cc["@mozilla.org/eventlistenerservice;1"]
+      .getService(Ci.nsIEventListenerService)
+      .addSystemEventListener(global, "mousemove", this, true);
+    addEventListener("pagehide", this, true);
+
+    sendAsyncMessage("Autoscroll:Start", {scrolldir: this._scrolldir,
+                                          screenX: event.screenX,
+                                          screenY: event.screenY});
+    this._ignoreMouseEvents = true;
+    this._startX = event.screenX;
+    this._startY = event.screenY;
+    this._screenX = event.screenX;
+    this._screenY = event.screenY;
+    this._scrollErrorX = 0;
+    this._scrollErrorY = 0;
+    this._lastFrame = content.mozAnimationStartTime;
+
+    content.mozRequestAnimationFrame(this);
+  },
+
+  stopScroll: function() {
+    if (this._scrollable) {
+      this._scrollable = null;
+
+      Cc["@mozilla.org/eventlistenerservice;1"]
+        .getService(Ci.nsIEventListenerService)
+        .removeSystemEventListener(global, "mousemove", this, true);
+      removeEventListener("pagehide", this, true);
+    }
+  },
+
+  accelerate: function(curr, start) {
+    const speed = 12;
+    var val = (curr - start) / speed;
+
+    if (val > 1)
+      return val * Math.sqrt(val) - 1;
+    if (val < -1)
+      return val * Math.sqrt(-val) + 1;
+    return 0;
+  },
+
+  roundToZero: function(num) {
+    if (num > 0)
+      return Math.floor(num);
+    return Math.ceil(num);
+  },
+
+  autoscrollLoop: function(timestamp) {
+    if (!this._scrollable) {
+      // Scrolling has been canceled
+      return;
+    }
+
+    // avoid long jumps when the browser hangs for more than
+    // |maxTimeDelta| ms
+    const maxTimeDelta = 100;
+    var timeDelta = Math.min(maxTimeDelta, timestamp - this._lastFrame);
+    // we used to scroll |accelerate()| pixels every 20ms (50fps)
+    var timeCompensation = timeDelta / 20;
+    this._lastFrame = timestamp;
+
+    var actualScrollX = 0;
+    var actualScrollY = 0;
+    // don't bother scrolling vertically when the scrolldir is only horizontal
+    // and the other way around
+    if (this._scrolldir != 'EW') {
+      var y = this.accelerate(this._screenY, this._startY) * timeCompensation;
+      var desiredScrollY = this._scrollErrorY + y;
+      actualScrollY = this.roundToZero(desiredScrollY);
+      this._scrollErrorY = (desiredScrollY - actualScrollY);
+    }
+    if (this._scrolldir != 'NS') {
+      var x = this.accelerate(this._screenX, this._startX) * timeCompensation;
+      var desiredScrollX = this._scrollErrorX + x;
+      actualScrollX = this.roundToZero(desiredScrollX);
+      this._scrollErrorX = (desiredScrollX - actualScrollX);
+    }
+
+    if (this._scrollable instanceof content.Window) {
+      this._scrollable.scrollBy(actualScrollX, actualScrollY);
+    } else { // an element with overflow
+      this._scrollable.scrollLeft += actualScrollX;
+      this._scrollable.scrollTop += actualScrollY;
+    }
+    content.mozRequestAnimationFrame(this);
+  },
+
+  sample: function(timestamp) {
+    this.autoscrollLoop(timestamp);
+  },
+
+  handleEvent: function(event) {
+    if (event.type == "mousemove") {
+      this._screenX = event.screenX;
+      this._screenY = event.screenY;
+    } else if (event.type == "mousedown") {
+      if (event.isTrusted &
+          !event.defaultPrevented &&
+          event.button == 1 &&
+          !this._scrollable &&
+          !this.isAutoscrollBlocker(event.originalTarget)) {
+        this.startScroll(event);
+      }
+    } else if (event.type == "pagehide") {
+      if (this._scrollable) {
+        var doc =
+          this._scrollable.ownerDocument || this._scrollable.document;
+        if (doc == event.target) {
+          sendAsyncMessage("Autoscroll:Cancel");
+        }
+      }
+    }
+  },
+
+  receiveMessage: function(msg) {
+    switch (msg.name) {
+      case "Autoscroll:Stop": {
+        this.stopScroll();
+        break;
+      }
+    }
+  },
+};
+ClickEventHandler.init();
