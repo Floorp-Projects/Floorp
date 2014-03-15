5df541575c92907a55e3797d6610b8eda96b6825
Ryan VanderMeulen <ryanvm@gmail.com>
2014-03-15 19:50:09 -0400

    Backed out changeset a1d95da23e97 (bug 976002) for Android xpcshell failures.

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -23,10 +23,7 @@ Cu.import('resource://gre/modules/NetworkStatsService.jsm');
 // Identity
 Cu.import('resource://gre/modules/SignInToWebsite.jsm');
 SignInToWebsiteController.init();
-
-#ifdef MOZ_SERVICES_FXACCOUNTS
 Cu.import('resource://gre/modules/FxAccountsMgmtService.jsm');
-#endif
 
 Cu.import('resource://gre/modules/DownloadsAPI.jsm');
 
