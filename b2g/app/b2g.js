1901650d7e51df98ce3621974acff3af462cbf46
Ryan VanderMeulen <ryanvm@gmail.com>
2014-03-15 19:50:09 -0400

    Backed out changeset a1d95da23e97 (bug 976002) for Android xpcshell failures.

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -907,9 +907,3 @@ pref("browser.autofocus", false);
 
 // Enable wakelock
 pref("dom.wakelock.enabled", true);
-
-// Enable sync and mozId with Firefox Accounts.
-#ifdef MOZ_SERVICES_FXACCOUNTS
-pref("services.sync.fxaccounts.enabled", true);
-pref("identity.fxaccounts.enabled", true);
-#endif
