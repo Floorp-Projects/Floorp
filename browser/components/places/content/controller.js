0377790278b5d0d8f8eaeacf759e36d32713193f
Asaf Romano <aromano@mozilla.com>
2014-03-15 23:49:07 +0200

    Bug 983623 - Async transactions: Add a preference for turning it, implement undo & redo commands. r=mak

--- browser/components/places/content/controller.js
+++ browser/components/places/content/controller.js
@@ -124,11 +124,38 @@ PlacesController.prototype = {
   },
 
   isCommandEnabled: function PC_isCommandEnabled(aCommand) {
+    if (PlacesUIUtils.useAsyncTransactions) {
+      switch (aCommand) {
+      case "cmd_cut":
+      case "placesCmd_cut":
+      case "cmd_copy":
+      case "cmd_paste":
+      case "cmd_delete":
+      case "placesCmd_delete":
+      case "placesCmd_moveBookmarks":
+      case "cmd_paste":
+      case "placesCmd_paste":
+      case "placesCmd_new:folder":
+      case "placesCmd_new:livemark":
+      case "placesCmd_new:bookmark":
+      case "placesCmd_new:separator":
+      case "placesCmd_sortBy:name":
+      case "placesCmd_createBookmark":
+        return false;
+      }
+    }
+
     switch (aCommand) {
     case "cmd_undo":
-      return PlacesUtils.transactionManager.numberOfUndoItems > 0;
+      if (!PlacesUIUtils.useAsyncTransactions)
+        return PlacesUtils.transactionManager.numberOfUndoItems > 0;
+
+      return PlacesTransactions.topUndoEntry != null;
     case "cmd_redo":
-      return PlacesUtils.transactionManager.numberOfRedoItems > 0;
+      if (!PlacesUIUtils.useAsyncTransactions)
+        return PlacesUtils.transactionManager.numberOfRedoItems > 0;
+
+      return PlacesTransactions.topRedoEntry != null;
     case "cmd_cut":
     case "placesCmd_cut":
       var nodes = this._view.selectedNodes;
