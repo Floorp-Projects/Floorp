f546d230e97423aeca23b8749ba18538cc9b4d5a
Dave Townsend <dtownsend@oxymoronical.com>
2014-03-18 13:07:49 -0700

    Backed out changeset a9de76765bad on a CLOSED TREE from bug 850336 for its test taking too long.

--- browser/devtools/shared/inplace-editor.js
+++ browser/devtools/shared/inplace-editor.js
@@ -372,11 +372,6 @@ InplaceEditor.prototype = {
     this.input.setSelectionRange(newValue.start, newValue.end);
     this._doValidation();
 
-    // Call the user's change handler if available.
-    if (this.change) {
-      this.change(this.input.value.trim());
-    }
-
     return true;
   },
 
