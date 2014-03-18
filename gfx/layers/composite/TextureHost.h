2645937e1d9cb25d9fc4e0bf867057866df9bb4f
Nicolas Silva <nical@mozilla.com>
2014-03-18 16:29:07 +0100

    Bug 984437 - Remove GrallocDeprecatedTextureHostOGL. r=sortaro

--- gfx/layers/composite/TextureHost.h
+++ gfx/layers/composite/TextureHost.h
@@ -761,9 +761,7 @@ public:
    * retain a SurfaceDescriptor.
    * Ownership of the SurfaceDescriptor passes to this.
    */
-  // only made virtual to allow overriding in GrallocDeprecatedTextureHostOGL, for hacky fix in gecko 23 for bug 862324.
-  // see bug 865908 about fixing this.
-  virtual void SetBuffer(SurfaceDescriptor* aBuffer, ISurfaceAllocator* aAllocator);
+  void SetBuffer(SurfaceDescriptor* aBuffer, ISurfaceAllocator* aAllocator);
 
   // used only for hacky fix in gecko 23 for bug 862324
   // see bug 865908 about fixing this.
