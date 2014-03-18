9a52a78475f9732b1ec83a18719b03bf55874af9
Botond Ballo <botond@mozilla.com>
2014-03-18 16:15:11 -0400

    Bug 985185 - Identify scroll info layers in the APZC tree log. r=tn

--- gfx/layers/composite/APZCTreeManager.cpp
+++ gfx/layers/composite/APZCTreeManager.cpp
@@ -241,6 +241,7 @@ APZCTreeManager::UpdatePanZoomControllerTree(CompositorParent* aCompositor,
         mApzcTreeLog << "APZC " << guid
                      << "\tcb=" << visible
                      << "\tsr=" << container->GetFrameMetrics().mScrollableRect
+                     << (aLayer->GetVisibleRegion().IsEmpty() ? "\tscrollinfo" : "")
                      << "\t" << container->GetFrameMetrics().GetContentDescription();
 
         // Bind the APZC instance into the tree of APZCs
