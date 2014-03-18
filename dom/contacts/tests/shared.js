85e40fdd0be5db83c5466341f313d1eb526111e6
Ryan VanderMeulen <ryanvm@gmail.com>
2014-03-18 15:27:18 -0400

    Backed out changeset f835989efa64 (bug 949537) for mochitest failures. CLOSED TREE

--- dom/contacts/tests/shared.js
+++ dom/contacts/tests/shared.js
@@ -402,17 +402,15 @@ function next() {
   }
 }
 
-SimpleTest.waitForExplicitFinish();
-
 function start_tests() {
   // Skip tests on Android < 4.0 due to test failures on tbpl (see bugs 897924 & 888891)
   let androidVersion = SpecialPowers.Cc['@mozilla.org/system-info;1']
                                     .getService(SpecialPowers.Ci.nsIPropertyBag2)
                                     .getProperty('version');
   if (!isAndroid || androidVersion >= 14) {
-    next();
+    SimpleTest.waitForExplicitFinish();
+    addLoadEvent(next);
   } else {
     ok(true, "Skip tests on Android < 4.0 (bugs 897924 & 888891");
-    SimpleTest.finish();
   }
 }
