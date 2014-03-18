c31ac857735678d7463f401e06dc8cf7f8ec8835
Star Cheng <scheng@mozilla.com>
2014-03-18 16:44:41 +0800

    Bug 975955 - Hold a cpu wakelock while playing video. r=roc, a=1.3+

--- content/html/content/src/HTMLVideoElement.cpp
+++ content/html/content/src/HTMLVideoElement.cpp
@@ -244,7 +244,7 @@ void
 HTMLVideoElement::NotifyOwnerDocumentActivityChanged()
 {
   HTMLMediaElement::NotifyOwnerDocumentActivityChanged();
-  WakeLockUpdate();
+  UpdateScreenWakeLock();
 }
 
 already_AddRefed<VideoPlaybackQuality>
@@ -281,17 +281,19 @@ HTMLVideoElement::GetVideoPlaybackQuality()
 void
 HTMLVideoElement::WakeLockCreate()
 {
-  WakeLockUpdate();
+  HTMLMediaElement::WakeLockCreate();
+  UpdateScreenWakeLock();
 }
 
 void
 HTMLVideoElement::WakeLockRelease()
 {
-  WakeLockUpdate();
+  UpdateScreenWakeLock();
+  HTMLMediaElement::WakeLockRelease();
 }
 
 void
-HTMLVideoElement::WakeLockUpdate()
+HTMLVideoElement::UpdateScreenWakeLock()
 {
   bool hidden = OwnerDoc()->Hidden();
 
