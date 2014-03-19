06e078cf79f187c435057570d4afe1c743b8ddcc
Marco Bonardo <mbonardo@mozilla.com>
2014-03-19 14:33:02 +0100

    Bug 818584 - Discard consecutive duplicate bookmark backups by comparing hashes r=mano Original patch by Raymond Lee <raymond@appcoast.com> f=mak

--- toolkit/components/places/tests/bookmarks/test_477583_json-backup-in-future.js
+++ toolkit/components/places/tests/bookmarks/test_477583_json-backup-in-future.js
@@ -22,11 +22,10 @@ function run_test() {
     dateObj.setYear(dateObj.getFullYear() + 1);
     let name = PlacesBackups.getFilenameForDate(dateObj);
     do_check_eq(name, "bookmarks-" + dateObj.toLocaleFormat("%Y-%m-%d") + ".json");
-    let rx = new RegExp("^" + name.replace(/\.json/, "") + "(_[0-9]+){0,1}\.json$");
     let files = bookmarksBackupDir.directoryEntries;
     while (files.hasMoreElements()) {
       let entry = files.getNext().QueryInterface(Ci.nsIFile);
-      if (entry.leafName.match(rx))
+      if (PlacesBackups.filenamesRegex.test(entry.leafName))
         entry.remove(false);
     }
 
@@ -43,8 +42,7 @@ function run_test() {
     let mostRecentBackupFile = yield PlacesBackups.getMostRecentBackup();
     do_check_neq(mostRecentBackupFile, null);
     let todayFilename = PlacesBackups.getFilenameForDate();
-    rx = new RegExp("^" + todayFilename.replace(/\.json/, "") + "(_[0-9]+){0,1}\.json$");
-    do_check_true(OS.Path.basename(mostRecentBackupFile).match(rx).length > 0);
+    do_check_true(PlacesBackups.filenamesRegex.test(OS.Path.basename(mostRecentBackupFile)));
 
     // Check that future backup has been removed.
     do_check_false(futureBackupFile.exists());
