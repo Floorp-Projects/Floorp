aeb6534b4d3ca3f9d8794b4806fe1da945db7dd5
Dave Townsend <dtownsend@oxymoronical.com>
2014-03-19 10:38:05 -0700

    Bug 850336: Devtools box model should be editable. r=pbrosset

--- browser/devtools/shared/inplace-editor.js
+++ browser/devtools/shared/inplace-editor.js
@@ -372,6 +372,11 @@ InplaceEditor.prototype = {
     this.input.setSelectionRange(newValue.start, newValue.end);
     this._doValidation();
 
+    // Call the user's change handler if available.
+    if (this.change) {
+      this.change(this.input.value.trim());
+    }
+
     return true;
   },
 
