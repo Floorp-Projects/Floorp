2f043190e9d3c1a40dac1710ff4d820bf4109d67
Ryan VanderMeulen <ryanvm@gmail.com>
2014-03-19 13:19:45 -0400

    Backed out changeset 9394e6901453 (bug 958008) for causing bug 960224.

--- gfx/thebes/gfxAndroidPlatform.h
+++ gfx/thebes/gfxAndroidPlatform.h
@@ -35,9 +35,6 @@ public:
     virtual already_AddRefed<gfxASurface>
     CreateOffscreenSurface(const IntSize& size,
                            gfxContentType contentType);
-    virtual already_AddRefed<gfxASurface>
-    OptimizeImage(gfxImageSurface *aSurface,
-                  gfxImageFormat format) MOZ_OVERRIDE;
     
     virtual gfxImageFormat GetOffscreenFormat() { return mOffscreenFormat; }
     
