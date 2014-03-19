fb961b06a8d8854e2d421545e446e92923339754
Dave Hylands <dhylands@mozilla.com>
2014-03-19 11:58:01 -0700

    Bug 983848 - Don't use settings db in UpdatePrompt when running under xpcshell. r=bent, a=bajaj

--- b2g/components/UpdatePrompt.js
+++ b2g/components/UpdatePrompt.js
@@ -46,6 +46,21 @@ XPCOMUtils.defineLazyServiceGetter(Services, "settings",
                                    "@mozilla.org/settingsService;1",
                                    "nsISettingsService");
 
+XPCOMUtils.defineLazyServiceGetter(Services, 'env',
+                                   '@mozilla.org/process/environment;1',
+                                   'nsIEnvironment');
+
+function useSettings() {
+  // When we're running in the real phone, then we can use settings.
+  // But when we're running as part of xpcshell, there is no settings database
+  // and trying to use settings in this scenario causes lots of weird
+  // assertions at shutdown time.
+  if (typeof useSettings.result === "undefined") {
+    useSettings.result = !Services.env.get("XPCSHELL_TEST_PROFILE_DIR");
+  }
+  return useSettings.result;
+}
+
 function UpdateCheckListener(updatePrompt) {
   this._updatePrompt = updatePrompt;
 }
@@ -183,8 +198,10 @@ UpdatePrompt.prototype = {
 
   showUpdateHistory: function UP_showUpdateHistory(aParent) { },
   showUpdateInstalled: function UP_showUpdateInstalled() {
-    let lock = Services.settings.createLock();
-    lock.set("deviceinfo.last_updated", Date.now(), null, null);
+    if (useSettings()) {
+      let lock = Services.settings.createLock();
+      lock.set("deviceinfo.last_updated", Date.now(), null, null);
+    }
   },
 
   // Custom functions
@@ -200,10 +217,12 @@ UpdatePrompt.prototype = {
   },
 
   setUpdateStatus: function UP_setUpdateStatus(aStatus) {
-    log("Setting gecko.updateStatus: " + aStatus);
+     if (useSettings()) {
+       log("Setting gecko.updateStatus: " + aStatus);
 
-    let lock = Services.settings.createLock();
-    lock.set("gecko.updateStatus", aStatus, null);
+       let lock = Services.settings.createLock();
+       lock.set("gecko.updateStatus", aStatus, null);
+     }
   },
 
   showApplyPrompt: function UP_showApplyPrompt(aUpdate) {
@@ -379,20 +398,22 @@ UpdatePrompt.prototype = {
 #endif
     }
 
-    // Save current os version in deviceinfo.previous_os
-    let lock = Services.settings.createLock({
-      handle: callbackAfterSet,
-      handleAbort: function(error) {
-        log("Abort callback when trying to set previous_os: " + error);
-        callbackAfterSet();
-      }
-    });
-    lock.get("deviceinfo.os", {
-      handle: function(name, value) {
-        log("Set previous_os to: " + value);
-        lock.set("deviceinfo.previous_os", value, null, null);
-      }
-    });
+    if (useSettings()) {
+      // Save current os version in deviceinfo.previous_os
+      let lock = Services.settings.createLock({
+        handle: callbackAfterSet,
+        handleAbort: function(error) {
+          log("Abort callback when trying to set previous_os: " + error);
+          callbackAfterSet();
+        }
+      });
+      lock.get("deviceinfo.os", {
+        handle: function(name, value) {
+          log("Set previous_os to: " + value);
+          lock.set("deviceinfo.previous_os", value, null, null);
+        }
+      });
+    }
   },
 
   forceUpdateCheck: function UP_forceUpdateCheck() {
