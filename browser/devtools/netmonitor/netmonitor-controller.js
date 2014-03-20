c7e98f098b1f58953ff2f18f5687ab261d0c462d
Thomas Andersen <thomas@mr-andersen.no>
2014-03-20 02:02:08 +0100

    Bug 859059 - Implement "Copy as curl". r=msucan, r=vp

--- browser/devtools/netmonitor/netmonitor-controller.js
+++ browser/devtools/netmonitor/netmonitor-controller.js
@@ -140,6 +140,9 @@ Object.defineProperty(this, "NetworkHelper", {
 XPCOMUtils.defineLazyServiceGetter(this, "clipboardHelper",
   "@mozilla.org/widget/clipboardhelper;1", "nsIClipboardHelper");
 
+XPCOMUtils.defineLazyModuleGetter(this, "Curl",
+  "resource:///modules/devtools/Curl.jsm");
+
 /**
  * Object defining the network monitor controller components.
  */
