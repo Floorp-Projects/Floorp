3577a9473b1d24ef07d46a7fe9f5d53ac4eef5f3
Paolo Amadini <paolo.mozmail@amadzone.org>
2014-03-20 22:21:05 +0100

    Bug 984872 - Remove deprecated promise.js usage in Firefox for Desktop. r=mak

--- browser/components/places/tests/browser/head.js
+++ browser/components/places/tests/browser/head.js
@@ -5,7 +5,7 @@
 XPCOMUtils.defineLazyModuleGetter(this, "NetUtil",
   "resource://gre/modules/NetUtil.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "Promise",
-  "resource://gre/modules/commonjs/sdk/core/promise.js");
+  "resource://gre/modules/Promise.jsm");
 
 // We need to cache this before test runs...
 let cachedLeftPaneFolderIdGetter;
