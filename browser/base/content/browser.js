d33bfd0bf4d17101ccd0b702cbddef3ba9ec2993
Phil Ringnalda <philringnalda@gmail.com>
2014-03-20 20:57:39 -0700

    Back out bbb44c125553 (bug 940393) for breaking WinXP

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1186,33 +1186,6 @@ var gBrowserInit = {
       WindowsPrefSync.init();
     }
 
-#ifdef XP_WIN
-    if (window.matchMedia("-moz-os-version: windows-win8") &&
-        window.matchMedia("-moz-windows-default-theme")) {
-      let windows8WindowFrameColor = Cu.import("resource:///modules/Windows8WindowFrameColor.jsm", {}).Windows8WindowFrameColor;
-      let windowFrameColor = windows8WindowFrameColor.get();
-
-      // Formula from W3C Techniques For Accessibility Evaluation And
-      // Repair Tools, Section 2.2 http://www.w3.org/TR/AERT#color
-      let brightnessThreshold = 125;
-      let colorThreshold = 500;
-      let bY = windowFrameColor[0] * .299 +
-               windowFrameColor[1] * .587 +
-               windowFrameColor[2] * .114;
-      let fY = 0; // Default to black for foreground text.
-      let brightnessDifference = Math.abs(bY - fY);
-      // Color difference calculation is simplified because black is 0 for R,G,B.
-      let colorDifference = windowFrameColor[0] + windowFrameColor[1] + windowFrameColor[2];
-
-      // Brightness is defined within {0, 255}. Set an attribute
-      // if the window frame color doesn't reach these thresholds
-      // so the theme can be adjusted for readability.
-      if (brightnessDifference < brightnessThreshold && colorDifference < colorThreshold) {
-        document.documentElement.setAttribute("darkwindowframe", "true");
-      }
-    }
-#endif
-
     SessionStore.promiseInitialized.then(() => {
       // Bail out if the window has been closed in the meantime.
       if (window.closed) {
