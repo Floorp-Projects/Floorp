d896763f97c94379e3de12d79d6cb510c2e2e6cd
Nick Alexander <nalexander@mozilla.com>
2014-03-21 16:42:02 -0700

    Bug 986114 - Follow-up: Fix bustage on a CLOSED TREE.

--- mobile/android/base/db/BrowserProvider.java
+++ mobile/android/base/db/BrowserProvider.java
@@ -1317,7 +1317,7 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
         // require the transaction to be upgraded from a reader to a writer.
         final int updated = updateBookmarks(uri, values, selection, selectionArgs);
         try {
-            cleanUpSomeDeletedRecords(uri, Bookmarks.CONTENT_URI, TABLE_BOOKMARKS);
+            cleanUpSomeDeletedRecords(uri, TABLE_BOOKMARKS);
         } catch (Exception e) {
             // We don't care.
             Log.e(LOGTAG, "Unable to clean up deleted bookmark records: ", e);
