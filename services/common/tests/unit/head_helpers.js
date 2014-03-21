87ad192da14f7e87cfe8bf5e9894910ecbe327f5
Mark Hammond <mhammond@skippinet.com.au>
2014-03-21 14:39:35 +1100

    Bug 985212 - check if the username looks like an email address to determine what sync to use. r=rnewman

--- services/common/tests/unit/head_helpers.js
+++ services/common/tests/unit/head_helpers.js
@@ -173,16 +173,15 @@ function uninstallFakePAC() {
   Cm.nsIComponentRegistrar.unregisterFactory(CID, PACSystemSettings);
 }
 
-// We want to ensure the legacy provider is used for most of these tests,
-// including after a service.startOver.  The tests that know how to deal with
+// Many tests do service.startOver() and don't expect the provider type to
+// change (whereas by default, a startOver will do exactly that so FxA is
+// subsequently used). The tests that know how to deal with
 // the Firefox Accounts identity hack things to ensure that still works.
-function setDefaultIdentityConfig() {
+function ensureStartOverKeepsIdentity() {
   Cu.import("resource://gre/modules/Services.jsm");
-  Services.prefs.setBoolPref("services.sync.fxaccounts.enabled", false);
   Services.prefs.setBoolPref("services.sync-testing.startOverKeepIdentity", true);
   do_register_cleanup(function() {
-    Services.prefs.clearUserPref("services.sync.fxaccounts.enabled");
     Services.prefs.clearUserPref("services.sync-testing.startOverKeepIdentity");
   });
 }
-setDefaultIdentityConfig();
+ensureStartOverKeepsIdentity();
