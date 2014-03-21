1d77e7d2322af744fe98a0dd02cb2976dd95c756
Mark Hammond <mhammond@skippinet.com.au>
2014-03-21 14:22:02 +1100

    Bug 970167 - Disable passwords engine when a master password is set. r=rnewman, a=sledru

--- browser/base/content/sync/customize.js
+++ browser/base/content/sync/customize.js
@@ -4,6 +4,20 @@
 
 "use strict";
 
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+
+Cu.import("resource://gre/modules/Services.jsm");
+
+let service = Cc["@mozilla.org/weave/service;1"]
+                .getService(Ci.nsISupports)
+                .wrappedJSObject;
+
+if (!service.allowPasswordsEngine) {
+  let checkbox = document.getElementById("fxa-pweng-chk");
+  checkbox.checked = false;
+  checkbox.disabled = true;
+}
+
 addEventListener("dialogaccept", function () {
   window.arguments[0].accepted = true;
 });
