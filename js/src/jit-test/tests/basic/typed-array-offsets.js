3591403a3b2bc143f54779ba0f799fcbd55757f2
Boris Zbarsky <bzbarsky@mit.edu>
2014-03-20 23:19:42 -0400

    Bug 980962.  Fix in-tree consumers to not construct typed arrays without "new".   r=jorendorff

--- js/src/jit-test/tests/basic/typed-array-offsets.js
+++ js/src/jit-test/tests/basic/typed-array-offsets.js
@@ -2,9 +2,9 @@ function f(x, y) {
   for (var i = 0; i < 100; i++)
     assertEq(x[0], y);
 }
-var a = ArrayBuffer(20);
-var b = Int32Array(a, 12, 2);
-var c = Int32Array(a, 0, 2);
+var a = new ArrayBuffer(20);
+var b = new Int32Array(a, 12, 2);
+var c = new Int32Array(a, 0, 2);
 b[0] = 10;
 f(b, 10);
 c[0] = 20;
