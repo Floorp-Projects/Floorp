44a1ec64d8b3111e5745547de904f5498f77dba7
Mark Hammond <mhammond@skippinet.com.au>
2014-03-21 14:22:02 +1100

    Bug 970167 - Disable passwords engine when a master password is set. r=rnewman, a=sledru

--- browser/components/preferences/sync.js
+++ browser/components/preferences/sync.js
@@ -154,6 +154,17 @@ let gSyncPane = {
         for (let checkbox of engines.querySelectorAll("checkbox")) {
           checkbox.disabled = enginesListDisabled;
         }
+
+        let checkbox = document.getElementById("fxa-pweng-chk");
+        let help = document.getElementById("fxa-pweng-help");
+        let allowPasswordsEngine = service.allowPasswordsEngine;
+
+        if (!allowPasswordsEngine) {
+          checkbox.checked = false;
+        }
+
+        checkbox.disabled = !allowPasswordsEngine;
+        help.hidden = allowPasswordsEngine;
       });
     // If fxAccountEnabled is false and we are in a "not configured" state,
     // then fxAccounts is probably fully disabled rather than just unconfigured,
