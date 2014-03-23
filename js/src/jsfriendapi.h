f41ea8b8cbb5cf95e66291958ab015784a1c41a3
Bobby Holley <bobbyholley@gmail.com>
2014-03-23 11:02:12 -0300

    Bug 975042 - Remove the special COW support for Date. r=peterv
    As soon as Date is on Xrays, this stuff won't work anyway. Henceforth, content
    access to chrome Date objects is forbidden, and APIs should use something like
    |new contentWindow.Date()| for any Date object they wish to expose to content.

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -1925,9 +1925,6 @@ IdToValue(jsid id)
 }
 
 extern JS_FRIEND_API(bool)
-IsReadOnlyDateMethod(JS::IsAcceptableThis test, JS::NativeImpl method);
-
-extern JS_FRIEND_API(bool)
 IsTypedArrayThisCheck(JS::IsAcceptableThis test);
 
 /*
