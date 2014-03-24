8caafd17cbb467dd996fc3495bcd4174f0758962
Jon Coppeard <jcoppeard@mozilla.com>
2014-03-24 21:45:23 +0000

    Bug 986843 - Replace AutoHoldZone with AutoCompartmentRooter r=terrence a=sylvestre

--- js/src/gc/RootMarking.cpp
+++ js/src/gc/RootMarking.cpp
@@ -794,7 +794,7 @@ js::gc::MarkRuntime(JSTracer *trc, bool useSavedRoots)
          * no need to do this for minor GCs.
          */
         for (CompartmentsIter c(rt, SkipAtoms); !c.done(); c.next())
-            c->mark(trc);
+            c->markRoots(trc);
 
         /*
          * The embedding can register additional roots here.
