815829610c45680f8284f1f731f01f6471ede6dc
Jon Coppeard <jcoppeard@mozilla.com>
2014-03-24 21:45:23 +0000

    Bug 986843 - Replace AutoHoldZone with AutoCompartmentRooter r=terrence

--- js/src/gc/Zone.cpp
+++ js/src/gc/Zone.cpp
@@ -24,7 +24,6 @@ using namespace js::gc;
 JS::Zone::Zone(JSRuntime *rt)
   : JS::shadow::Zone(rt, &rt->gcMarker),
     allocator(this),
-    hold(false),
     ionUsingBarriers_(false),
     active(false),
     gcScheduled(false),
