de8f0657d63a5d4a85285c25d32e6abc5565befe
Bobby Holley <bobbyholley@gmail.com>
2014-03-24 11:30:20 -0300

    Bug 986940 - Remove Cu.createDateIn. r=gabor
    When we switch ObjectWrapper.jsm to use Cu.cloneInto, this became unused.

--- js/xpconnect/src/XPCComponents.cpp
+++ js/xpconnect/src/XPCComponents.cpp
@@ -3067,34 +3067,6 @@ nsXPCComponents_Utils::CreateArrayIn(HandleValue vobj, JSContext *cx,
     return NS_OK;
 }
 
-/* jsval createDateIn(in jsval vobj, in long long msec); */
-NS_IMETHODIMP
-nsXPCComponents_Utils::CreateDateIn(HandleValue vobj, int64_t msec, JSContext *cx,
-                                    MutableHandleValue rval)
-{
-    if (!cx)
-        return NS_ERROR_FAILURE;
-
-    // first argument must be an object
-    if (!vobj.isObject())
-        return NS_ERROR_XPC_BAD_CONVERT_JS;
-
-    RootedObject obj(cx);
-    {
-        JSObject *scope = js::UncheckedUnwrap(&vobj.toObject());
-        JSAutoCompartment ac(cx, scope);
-        obj =  JS_NewDateObjectMsec(cx, msec);
-        if (!obj)
-            return NS_ERROR_FAILURE;
-    }
-
-    if (!JS_WrapObject(cx, &obj))
-        return NS_ERROR_FAILURE;
-
-    rval.setObject(*obj);
-    return NS_OK;
-}
-
 /* void makeObjectPropsNormal(jsval vobj); */
 NS_IMETHODIMP
 nsXPCComponents_Utils::MakeObjectPropsNormal(HandleValue vobj, JSContext *cx)
