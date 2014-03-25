505ac456673589129ff2e82c00f6e5c8f27b5655
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2014-03-25 21:09:25 +0900

    Bug 986347 - Restore accidentaly removed HasBogusPopupsDropShadowOnMultiMonitor() call. r=jimm

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -494,7 +494,8 @@ nsWindow::Create(nsIWidget *aParent,
       parent = nullptr;
     }
 
-    if (IsVistaOrLater() && !IsWin8OrLater()) {
+    if (IsVistaOrLater() && !IsWin8OrLater() &&
+        HasBogusPopupsDropShadowOnMultiMonitor()) {
       extendedStyle |= WS_EX_COMPOSITED;
     }
 
