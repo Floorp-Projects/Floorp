967e67b7b086a9c15bb92434714e70c537d17711
Steven Michaud <smichaud@pobox.com>
2014-03-25 11:32:58 -0500

    Bug 986976 - do_QueryInterface abuse in various .mm files. r=neil

--- widget/cocoa/nsPrintSettingsX.h
+++ widget/cocoa/nsPrintSettingsX.h
@@ -9,9 +9,14 @@
 #include "nsPrintSettingsImpl.h"  
 #import <Cocoa/Cocoa.h>
 
+#define NS_PRINTSETTINGSX_IID \
+{ 0x0DF2FDBD, 0x906D, 0x4726, \
+  { 0x9E, 0x4D, 0xCF, 0xE0, 0x87, 0x8D, 0x70, 0x7C } }
+
 class nsPrintSettingsX : public nsPrintSettings
 {
 public:
+  NS_DECLARE_STATIC_IID_ACCESSOR(NS_PRINTSETTINGSX_IID)
   NS_DECL_ISUPPORTS_INHERITED
 
   nsPrintSettingsX();
@@ -44,5 +49,7 @@ protected:
 
   NSPrintInfo* mPrintInfo;
 };
+
+NS_DEFINE_STATIC_IID_ACCESSOR(nsPrintSettingsX, NS_PRINTSETTINGSX_IID)
 
 #endif // nsPrintSettingsX_h_
