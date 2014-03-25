0b7f832b546b49ba536c656a84d460b0e8f5363c
Mihai Sucan <mihai.sucan@gmail.com>
2014-03-25 15:18:58 +0200

    Bug 984857 - Fix for intermittent browser_webconsole_output_02.js,browser_webconsole_output_03.js | This test exceeded the timeout threshold; r=me

--- browser/devtools/webconsole/test/browser_webconsole_output_03.js
+++ browser/devtools/webconsole/test/browser_webconsole_output_03.js
@@ -153,17 +153,11 @@ let inputTests = [
 ];
 
 function test() {
-  addTab(TEST_URI);
-  browser.addEventListener("load", function onLoad() {
-    browser.removeEventListener("load", onLoad, true);
-    openConsole().then((hud) => {
-      return checkOutputForInputs(hud, inputTests);
-    }).then(finishUp);
-  }, true);
-}
-
-function finishUp() {
-  inputTests = null;
-
-  finishTest();
+  requestLongerTimeout(2);
+  Task.spawn(function*() {
+    const {tab} = yield loadTab(TEST_URI);
+    const hud = yield openConsole(tab);
+    yield checkOutputForInputs(hud, inputTests);
+    inputTests = null;
+  }).then(finishTest);
 }
