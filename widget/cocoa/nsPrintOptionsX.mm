24118964eb9964b1cd63ddf7b1bf73bf0446bed4
Steven Michaud <smichaud@pobox.com>
2014-03-25 11:32:58 -0500

    Bug 986976 - do_QueryInterface abuse in various .mm files. r=neil

--- widget/cocoa/nsPrintOptionsX.mm
+++ widget/cocoa/nsPrintOptionsX.mm
@@ -24,7 +24,7 @@ nsPrintOptionsX::ReadPrefs(nsIPrintSettings* aPS, const nsAString& aPrinterName,
   rv = nsPrintOptions::ReadPrefs(aPS, aPrinterName, aFlags);
   NS_ASSERTION(NS_SUCCEEDED(rv), "nsPrintOptions::ReadPrefs() failed");
   
-  nsCOMPtr<nsPrintSettingsX> printSettingsX(do_QueryInterface(aPS));
+  nsRefPtr<nsPrintSettingsX> printSettingsX(do_QueryObject(aPS));
   if (!printSettingsX)
     return NS_ERROR_NO_INTERFACE;
   rv = printSettingsX->ReadPageFormatFromPrefs();
@@ -59,7 +59,7 @@ nsPrintOptionsX::WritePrefs(nsIPrintSettings* aPS, const nsAString& aPrinterName
   rv = nsPrintOptions::WritePrefs(aPS, aPrinterName, aFlags);
   NS_ASSERTION(NS_SUCCEEDED(rv), "nsPrintOptions::WritePrefs() failed");
 
-  nsCOMPtr<nsPrintSettingsX> printSettingsX(do_QueryInterface(aPS));
+  nsRefPtr<nsPrintSettingsX> printSettingsX(do_QueryObject(aPS));
   if (!printSettingsX)
     return NS_ERROR_NO_INTERFACE;
   rv = printSettingsX->WritePageFormatToPrefs();
