44fa3946152f2c808164e7c3cb6f2e15dab595c9
Mihai Sucan <mihai.sucan@gmail.com>
2014-03-25 14:34:05 +0200

    Bug 950430 - Potential fix for intermittent browser_webconsole_chrome.js | Test timed out; r=me

--- browser/devtools/webconsole/test/browser_webconsole_chrome.js
+++ browser/devtools/webconsole/test/browser_webconsole_chrome.js
@@ -3,33 +3,34 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Tests that code completion works properly.
+// Tests that code completion works properly in chrome tabs, like about:credits.
 
 function test() {
-  addTab("about:credits");
-  browser.addEventListener("load", function onLoad() {
-    browser.removeEventListener("load", onLoad, true);
-    openConsole(null, testChrome);
-  }, true);
+  Task.spawn(function*() {
+    const {tab} = yield loadTab("about:credits");
+    ok(tab, "tab loaded");
+
+    const hud = yield openConsole(tab);
+    ok(hud, "we have a console");
+    ok(hud.iframeWindow, "we have the console UI window");
+
+    let jsterm = hud.jsterm;
+    ok(jsterm, "we have a jsterm");
+
+    let input = jsterm.inputNode;
+    ok(hud.outputNode, "we have an output node");
+
+    // Test typing 'docu'.
+    input.value = "docu";
+    input.setSelectionRange(4, 4);
+
+    let deferred = promise.defer();
+
+    jsterm.complete(jsterm.COMPLETE_HINT_ONLY, function() {
+      is(jsterm.completeNode.value, "    ment", "'docu' completion");
+      deferred.resolve(null);
+    });
+
+    yield deferred.promise;
+  }).then(finishTest);
 }
-
-function testChrome(hud) {
-  ok(hud, "we have a console");
-
-  ok(hud.iframeWindow, "we have the console UI window");
-
-  let jsterm = hud.jsterm;
-  ok(jsterm, "we have a jsterm");
-
-  let input = jsterm.inputNode;
-  ok(hud.outputNode, "we have an output node");
-
-  // Test typing 'docu'.
-  input.value = "docu";
-  input.setSelectionRange(4, 4);
-  jsterm.complete(jsterm.COMPLETE_HINT_ONLY, function() {
-    is(jsterm.completeNode.value, "    ment", "'docu' completion");
-    executeSoon(finishTest);
-  });
-}
-
