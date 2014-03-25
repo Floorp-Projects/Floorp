72500018c46a53f98eecad18c5cdefaa1bf6410a
Andrea Marchesini <amarchesini@mozilla.com>
2014-03-25 18:37:28 +0000

    Bug 910010 - Implementation of PFileDescriptorSet - part 2, r=bent

--- dom/ipc/FileDescriptorSetParent.h
+++ dom/ipc/FileDescriptorSetParent.h
@@ -2,12 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_FileDescriptorSetChild_h__
-#define mozilla_dom_FileDescriptorSetChild_h__
+#ifndef mozilla_dom_FileDescriptorSetParent_h__
+#define mozilla_dom_FileDescriptorSetParent_h__
 
 #include "mozilla/Assertions.h"
 #include "mozilla/Attributes.h"
-#include "mozilla/dom/PFileDescriptorSetChild.h"
+#include "mozilla/dom/PFileDescriptorSetParent.h"
 #include "nsTArray.h"
 
 namespace mozilla {
@@ -20,11 +20,11 @@ class FileDescriptor;
 
 namespace dom {
 
-class ContentChild;
+class ContentParent;
 
-class FileDescriptorSetChild MOZ_FINAL: public PFileDescriptorSetChild
+class FileDescriptorSetParent MOZ_FINAL: public PFileDescriptorSetParent
 {
-  friend class ContentChild;
+  friend class ContentParent;
 
 public:
   typedef mozilla::ipc::FileDescriptor FileDescriptor;
@@ -33,8 +33,8 @@ public:
   ForgetFileDescriptors(nsTArray<FileDescriptor>& aFileDescriptors);
 
 private:
-  FileDescriptorSetChild(const FileDescriptor& aFileDescriptor);
-  ~FileDescriptorSetChild();
+  FileDescriptorSetParent(const FileDescriptor& aFileDescriptor);
+  ~FileDescriptorSetParent();
 
   virtual bool
   RecvAddFileDescriptor(const FileDescriptor& aFileDescriptor) MOZ_OVERRIDE;
@@ -45,4 +45,4 @@ private:
 } // namespace dom
 } // namespace mozilla
 
-#endif // mozilla_dom_FileDescriptorSetChild_h__
+#endif // mozilla_dom_FileDescriptorSetParent_h__
