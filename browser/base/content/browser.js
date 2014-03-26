79565412af8e1be18eaac75df4f99b17aa0b7dc5
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2014-03-26 12:16:00 -0400

    Bug 985815 - propagate collapsed state to other windows, add test. r=jaws, a=sledru.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4342,12 +4342,11 @@ function onViewToolbarsPopupShowing(aEvent, aInsertPoint) {
 
 function onViewToolbarCommand(aEvent) {
   var toolbarId = aEvent.originalTarget.getAttribute("toolbarId");
-  var toolbar = document.getElementById(toolbarId);
   var isVisible = aEvent.originalTarget.getAttribute("checked") == "true";
-  setToolbarVisibility(toolbar, isVisible);
+  CustomizableUI.setToolbarVisibility(toolbarId, isVisible);
 }
 
-function setToolbarVisibility(toolbar, isVisible) {
+function setToolbarVisibility(toolbar, isVisible, persist=true) {
   let hidingAttribute;
   if (toolbar.getAttribute("type") == "menubar") {
     hidingAttribute = "autohide";
@@ -4359,7 +4358,10 @@ function setToolbarVisibility(toolbar, isVisible) {
   }
 
   toolbar.setAttribute(hidingAttribute, !isVisible);
-  document.persist(toolbar.id, hidingAttribute);
+  if (persist) {
+    document.persist(toolbar.id, hidingAttribute);
+  }
+
   let eventParams = {
     detail: {
       visible: isVisible
