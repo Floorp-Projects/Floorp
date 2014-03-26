3464116a97045db365986012ce431294f33acd98
Fabrice Desré <fabrice@mozilla.com>
2014-03-25 21:56:35 -0700

    Bug 982491 - Group apps in activities chains in the same process r=cervantes,gene

--- dom/messages/SystemMessageInternal.js
+++ dom/messages/SystemMessageInternal.js
@@ -342,7 +342,8 @@ SystemMessageInternal.prototype = {
     // To prevent the hacked child process from sending commands to parent
     // to manage system messages, we need to check its manifest URL.
     if (["SystemMessageManager:Register",
-         "SystemMessageManager:Unregister",
+         // TODO: fix bug 988142 to re-enable.
+         // "SystemMessageManager:Unregister",
          "SystemMessageManager:GetPendingMessages",
          "SystemMessageManager:HasPendingMessages",
          "SystemMessageManager:Message:Return:OK",
