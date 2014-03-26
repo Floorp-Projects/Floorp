c82587d760ba62cf1f118477c1782868864bea63
Ed Morley <emorley@mozilla.com>
2014-03-26 18:11:11 +0000

    Backed out changeset 738a4b230194 (bug 988326) for assertions

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -667,7 +667,8 @@ ChunkPool::get(JSRuntime *rt)
         chunk = Chunk::allocate(rt);
         if (!chunk)
             return nullptr;
-        JS_ASSERT(chunk->info.numArenasFreeCommitted == 0);
+        JS_ASSERT(chunk->info.numArenasFreeCommitted == ArenasPerChunk);
+        rt->gcNumArenasFreeCommitted += ArenasPerChunk;
     }
     JS_ASSERT(chunk->unused());
     JS_ASSERT(!rt->gcChunkSet.has(chunk));
@@ -804,19 +805,24 @@ Chunk::init(JSRuntime *rt)
      */
     bitmap.clear();
 
-    /*
-     * Decommit the arenas. We do this after poisoning so that if the OS does
-     * not have to recycle the pages, we still get the benefit of poisoning.
-     */
-    decommitAllArenas(rt);
+    /* Initialize the arena tracking bitmap. */
+    decommittedArenas.clear(false);
 
     /* Initialize the chunk info. */
-    info.freeArenasHead = nullptr;
+    info.freeArenasHead = &arenas[0].aheader;
     info.lastDecommittedArenaOffset = 0;
     info.numArenasFree = ArenasPerChunk;
-    info.numArenasFreeCommitted = 0;
+    info.numArenasFreeCommitted = ArenasPerChunk;
     info.age = 0;
     info.trailer.runtime = rt;
+
+    /* Initialize the arena header state. */
+    for (unsigned i = 0; i < ArenasPerChunk; i++) {
+        arenas[i].aheader.setAsNotAllocated();
+        arenas[i].aheader.next = (i + 1 < ArenasPerChunk)
+                                 ? &arenas[i + 1].aheader
+                                 : nullptr;
+    }
 
     /* The rest of info fields are initialized in PickChunk. */
 }
@@ -993,7 +999,6 @@ Chunk::releaseArena(ArenaHeader *aheader)
     } else {
         rt->gcChunkSet.remove(this);
         removeFromAvailableList();
-        decommitAllArenas(rt);
         rt->gcChunkPool.put(this);
     }
 }
@@ -2617,11 +2622,12 @@ GCHelperThread::threadLoop()
                 /* OOM stops the background allocation. */
                 if (!chunk) {
 #if JS_TRACE_LOGGING
-                    logger->log(TraceLogging::GC_ALLOCATING_STOP);
+            logger->log(TraceLogging::GC_ALLOCATING_STOP);
 #endif
                     break;
                 }
-                JS_ASSERT(chunk->info.numArenasFreeCommitted == 0);
+                JS_ASSERT(chunk->info.numArenasFreeCommitted == ArenasPerChunk);
+                rt->gcNumArenasFreeCommitted += ArenasPerChunk;
                 rt->gcChunkPool.put(chunk);
             } while (state == ALLOCATING && rt->gcChunkPool.wantBackgroundAllocation(rt));
             if (state == ALLOCATING)
