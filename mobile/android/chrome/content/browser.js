6f87ab91564a3b9ac1b202e8495938826966497f
Wes Johnston <wjohnston@mozilla.com>
2014-03-26 10:18:02 -0700

    Bug 942270 - Gingerbread compatibility of GeckoActionView. r=margaret

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -8391,9 +8391,19 @@ HTMLContextMenuItem.prototype = Object.create(ContextMenuItem.prototype, {
       // If this is a menu item, show a new context menu with the submenu in it
       if (elt instanceof Ci.nsIDOMHTMLMenuElement) {
         try {
-          NativeWindow.contextmenus.menuitems = [];
-          NativeWindow.contextmenus._addHTMLContextMenuItemsForMenu(elt, target);
-          NativeWindow.contextmenus._innerShow(target);
+          NativeWindow.contextmenus.menus = {};
+
+          let elt = this.menuElementRef.get();
+          let target = this.targetElementRef.get();
+          if (!elt) {
+            return;
+          }
+
+          var items = NativeWindow.contextmenus._getHTMLContextMenuItemsForMenu(elt, target);
+          if (items.length > 0) {
+            NativeWindow.contextmenus._addMenuItems(items, "link");
+          }
+
         } catch(ex) {
           Cu.reportError(ex);
         }
