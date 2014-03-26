c5cc50fc1c19b06da532497e27471344e0a32daf
Wes Johnston <wjohnston@mozilla.com>
2014-03-26 10:18:01 -0700

    Bug 942270 - Fix tests for quickshare. r=gbrown, a=lsblakk

---
+++ build/mobile/robocop/RobocopShare2.java
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class RobocopShare2 extends FragmentActivity {
+    private static Bundle sArguments;
+
+    @Override
+    public void onCreate(Bundle arguments) {
+        super.onCreate(arguments);
+    }
+}
