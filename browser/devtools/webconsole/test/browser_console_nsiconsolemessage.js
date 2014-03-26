46bcb75246f0c74f7abd2c662fe8782d3b91c760
Mihai Sucan <mihai.sucan@gmail.com>
2014-03-26 13:43:27 +0200

    Bug 987784 - Update the browser console default prefs to match those of the web console; r=robcee

--- browser/devtools/webconsole/test/browser_console_nsiconsolemessage.js
+++ browser/devtools/webconsole/test/browser_console_nsiconsolemessage.js
@@ -11,74 +11,63 @@ const TEST_URI = "data:text/html;charset=utf8,<title>bug859756</title>\n" +
 
 function test()
 {
-  addTab(TEST_URI);
-  browser.addEventListener("load", function onLoad() {
-    browser.removeEventListener("load", onLoad, true);
+  const FILTER_PREF = "devtools.browserconsole.filter.jslog";
+  Services.prefs.setBoolPref(FILTER_PREF, true);
+
+  registerCleanupFunction(() => {
+    Services.prefs.clearUserPref(FILTER_PREF);
+  });
+
+  Task.spawn(function*() {
+    const {tab} = yield loadTab(TEST_URI);
 
     // Test for cached nsIConsoleMessages.
     Services.console.logStringMessage("test1 for bug859756");
 
     info("open web console");
-    openConsole(null, consoleOpened);
-  }, true);
-}
+    let hud = yield openConsole(tab);
 
-function consoleOpened(hud)
-{
-  ok(hud, "web console opened");
-  Services.console.logStringMessage("do-not-show-me");
-  content.console.log("foobarz");
+    ok(hud, "web console opened");
+    Services.console.logStringMessage("do-not-show-me");
+    content.console.log("foobarz");
 
-  waitForMessages({
-    webconsole: hud,
-    messages: [
-      {
+    yield waitForMessages({
+      webconsole: hud,
+      messages: [{
         text: "foobarz",
         category: CATEGORY_WEBDEV,
         severity: SEVERITY_LOG,
-      },
-    ],
-  }).then(() => {
+      }],
+    });
+
     let text = hud.outputNode.textContent;
     is(text.indexOf("do-not-show-me"), -1,
        "nsIConsoleMessages are not displayed");
     is(text.indexOf("test1 for bug859756"), -1,
        "nsIConsoleMessages are not displayed (confirmed)");
-    closeConsole(null, onWebConsoleClose);
-  });
-}
 
-function onWebConsoleClose()
-{
-  info("web console closed");
-  HUDService.toggleBrowserConsole().then(onBrowserConsoleOpen);
-}
+    yield closeConsole(tab);
 
-function onBrowserConsoleOpen(hud)
-{
-  ok(hud, "browser console opened");
-  Services.console.logStringMessage("test2 for bug859756");
+    info("web console closed");
+    hud = yield HUDService.toggleBrowserConsole();
+    ok(hud, "browser console opened");
 
-  waitForMessages({
-    webconsole: hud,
-    messages: [
-      {
+    Services.console.logStringMessage("test2 for bug859756");
+
+    let results = yield waitForMessages({
+      webconsole: hud,
+      messages: [{
         text: "test1 for bug859756",
         category: CATEGORY_JS,
-      },
-      {
+      }, {
         text: "test2 for bug859756",
         category: CATEGORY_JS,
-      },
-      {
+      }, {
         text: "do-not-show-me",
         category: CATEGORY_JS,
-      },
-    ],
-  }).then(testFiltering);
+      }],
+    });
 
-  function testFiltering(results)
-  {
     let msg = [...results[2].matched][0];
     ok(msg, "message element for do-not-show-me (nsIConsoleMessage)");
     isnot(msg.textContent.indexOf("do-not-show"), -1, "element content is correct");
@@ -87,9 +76,5 @@ function onBrowserConsoleOpen(hud)
     hud.setFilterState("jslog", false);
 
     ok(msg.classList.contains("filtered-by-type"), "element is filtered");
-
-    hud.setFilterState("jslog", true);
-
-    finishTest();
-  }
+  }).then(finishTest);
 }
