1ed19e094cbd8304d9ebfd5a0fd2b4f6cfd35a8b
Wes Johnston <wjohnston@mozilla.com>
2014-03-26 10:18:01 -0700

    Bug 942270 - Support quickshare in context menus. r=bnicholson, a=lsblakk

--- mobile/android/base/widget/GeckoActionProvider.java
+++ mobile/android/base/widget/GeckoActionProvider.java
@@ -18,6 +18,7 @@ import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.SubMenu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.text.TextUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -47,16 +48,40 @@ public class GeckoActionProvider extends ActionProvider {
 
     private static HashMap<String, GeckoActionProvider> mProviders = new HashMap<String, GeckoActionProvider>();
 
+    private static String getFilenameFromMimeType(String mimeType) {
+        String[] mime = mimeType.split("/");
+        if (mime.length == 1) {
+            return "history-" + mime[0] + ".xml";
+        }
+
+        // Separate out tel and mailto for their own media types
+        if ("text".equals(mime[0])) {
+            if ("tel".equals(mime[1])) {
+                return "history-phone.xml";
+            } else if ("mailto".equals(mime[1])) {
+                return "history-email.xml";
+            } else if ("html".equals(mime[1])) {
+                return DEFAULT_HISTORY_FILE_NAME;
+            }
+        }
+
+        return "history-" + mime[0] + ".xml";
+    }
+
     // Gets the action provider for a particular mimetype
-    public static GeckoActionProvider getForType(String type, Context context) {
-        if (!mProviders.keySet().contains(type)) {
+    public static GeckoActionProvider getForType(String mimeType, Context context) {
+        if (!mProviders.keySet().contains(mimeType)) {
             GeckoActionProvider provider = new GeckoActionProvider(context);
 
-            String subType = type.substring(0, type.indexOf("/"));
-            provider.setHistoryFileName("history-" + subType + ".xml");
-            mProviders.put(type, provider);
+            // For empty types, we just return a default provider
+            if (TextUtils.isEmpty(mimeType)) {
+                return provider;
+            }
+
+            provider.setHistoryFileName(getFilenameFromMimeType(mimeType));
+            mProviders.put(mimeType, provider);
         }
-        return mProviders.get(type);
+        return mProviders.get(mimeType);
     }
 
     public GeckoActionProvider(Context context) {
