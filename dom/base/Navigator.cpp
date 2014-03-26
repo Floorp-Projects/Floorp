a8947aa0a4d3e702fbd865aa4627789bdfddc267
Ehsan Akhgari <ehsan@mozilla.com>
2014-03-26 00:15:14 -0400

    Bug 987387 - Make navigator.sendBeacon() respect private browsing mode. r=jdm, a=lsblakk

--- dom/base/Navigator.cpp
+++ dom/base/Navigator.cpp
@@ -91,6 +91,8 @@
 
 #include "nsIUploadChannel2.h"
 #include "nsFormData.h"
+#include "nsIPrivateBrowsingChannel.h"
+#include "nsIDocShell.h"
 
 namespace mozilla {
 namespace dom {
@@ -1136,6 +1138,18 @@ Navigator::SendBeacon(const nsAString& aUrl,
   if (NS_FAILED(rv)) {
     aRv.Throw(rv);
     return false;
+  }
+
+  nsCOMPtr<nsIPrivateBrowsingChannel> pbChannel = do_QueryInterface(channel);
+  if (pbChannel) {
+    nsIDocShell* docShell = mWindow->GetDocShell();
+    nsCOMPtr<nsILoadContext> loadContext = do_QueryInterface(docShell);
+    if (loadContext) {
+      rv = pbChannel->SetPrivate(loadContext->UsePrivateBrowsing());
+      if (NS_FAILED(rv)) {
+        NS_WARNING("Setting the privacy status on the beacon channel failed");
+      }
+    }
   }
 
   nsCOMPtr<nsIHttpChannel> httpChannel = do_QueryInterface(channel);
