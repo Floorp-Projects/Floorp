7d10e922e785dd0a30e3808dbaa650301da8ea93
Ed Morley <emorley@mozilla.com>
2014-03-27 19:09:29 +0000

    Backed out changeset 894e40121370 (bug 917226) for browser-chrome failures; DONTBUILD since fixing commit message CLOSED TREE

--- browser/devtools/debugger/debugger-view.js
+++ browser/devtools/debugger/debugger-view.js
@@ -390,7 +390,8 @@ let DebuggerView = {
     this._setEditorText(L10N.getStr("loadingText"));
     this._editorSource = { url: aSource.url, promise: deferred.promise };
 
-    DebuggerController.SourceScripts.getText(aSource).then(([, aText, aContentType]) => {
+    DebuggerController.SourceScripts.getText(aSource)
+                                    .then(([, aText, aContentType]) => {
       // Avoid setting an unexpected source. This may happen when switching
       // very fast between sources that haven't been fetched yet.
       if (this._editorSource.url != aSource.url) {
@@ -468,7 +469,8 @@ let DebuggerView = {
 
     // Make sure the requested source client is shown in the editor, then
     // update the source editor's caret position and debug location.
-    return this._setEditorSource(sourceForm, aFlags).then(([,, aContentType]) => {
+    return this._setEditorSource(sourceForm, aFlags)
+               .then(([,, aContentType]) => {
       // Record the contentType learned from fetching
       sourceForm.contentType = aContentType;
       // Line numbers in the source editor should start from 1. If invalid
--- browser/devtools/shadereditor/shadereditor.js
+++ browser/devtools/shadereditor/shadereditor.js
@@ -8,6 +8,7 @@ const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Task.jsm");
+Cu.import("resource://gre/modules/devtools/Loader.jsm");
 Cu.import("resource:///modules/devtools/SideMenuWidget.jsm");
 Cu.import("resource:///modules/devtools/ViewHelpers.jsm");
 
