b02c210cf97b9a6ced9b0c6f0012020e5dec4ceb
Nick Alexander <nalexander@mozilla.com>
2014-03-27 10:16:31 -0700

    Bug 983350 - Include User-Agent header for FxAccount and TokenServer requests. r=rnewman, a=sylvestre
    --HG--
    extra : source : f5109b04e0839942c4a8c9d9d26f853a368731fc
    extra : amend_source : e62361f27b3e445d7486beb9c9edf39e69ed116a

--- mobile/android/base/sync/jpake/stage/DeleteChannel.java
+++ mobile/android/base/sync/jpake/stage/DeleteChannel.java
@@ -9,6 +9,7 @@ import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
 
 import org.mozilla.gecko.background.common.log.Logger;
+import org.mozilla.gecko.sync.SyncConstants;
 import org.mozilla.gecko.sync.jpake.JPakeClient;
 import org.mozilla.gecko.sync.net.BaseResource;
 import org.mozilla.gecko.sync.net.BaseResourceDelegate;
@@ -36,6 +37,10 @@ public class DeleteChannel {
       return;
     }
     httpResource.delegate = new BaseResourceDelegate(httpResource) {
+      @Override
+      public String getUserAgent() {
+        return SyncConstants.USER_AGENT;
+      }
 
       @Override
       public void addHeaders(HttpRequestBase request, DefaultHttpClient client) {
