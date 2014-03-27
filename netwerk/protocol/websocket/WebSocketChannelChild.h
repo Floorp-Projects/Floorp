2a59ad54ea57ef46ebbd8277792e23b7a052cfc1
Steve Workman <sworkman@mozilla.com>
2014-03-27 13:58:19 -0700

    Bug 925623 - Support delivery of WebSocket events off-main-thread in WebSocketChannel r=jduell

--- netwerk/protocol/websocket/WebSocketChannelChild.h
+++ netwerk/protocol/websocket/WebSocketChannelChild.h
@@ -14,6 +14,7 @@
 namespace mozilla {
 namespace net {
 
+class ChannelEvent;
 class ChannelEventQueue;
 
 class WebSocketChannelChild : public BaseWebSocketChannel,
@@ -24,6 +25,7 @@ class WebSocketChannelChild : public BaseWebSocketChannel,
   ~WebSocketChannelChild();
 
   NS_DECL_ISUPPORTS
+  NS_DECL_NSITHREADRETARGETABLEREQUEST
 
   // nsIWebSocketChannel methods BaseWebSocketChannel didn't implement for us
   //
@@ -33,6 +35,7 @@ class WebSocketChannelChild : public BaseWebSocketChannel,
   NS_IMETHOD SendMsg(const nsACString &aMsg);
   NS_IMETHOD SendBinaryMsg(const nsACString &aMsg);
   NS_IMETHOD SendBinaryStream(nsIInputStream *aStream, uint32_t aLength);
+  nsresult SendBinaryStream(OptionalInputStreamParams *aStream, uint32_t aLength);
   NS_IMETHOD GetSecurityInfo(nsISupports **aSecurityInfo);
 
   void AddIPDLReference();
@@ -53,6 +56,8 @@ class WebSocketChannelChild : public BaseWebSocketChannel,
   void OnAcknowledge(const uint32_t& aSize);
   void OnServerClose(const uint16_t& aCode, const nsCString& aReason);
   void AsyncOpenFailed();  
+
+  void DispatchToTargetThread(ChannelEvent *aChannelEvent);
 
   nsRefPtr<ChannelEventQueue> mEventQ;
   bool mIPCOpen;
