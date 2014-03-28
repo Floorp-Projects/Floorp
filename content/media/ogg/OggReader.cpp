ce46495cea46382d63968c4f1a6819a71761686d
Chris Pearce <cpearce@mozilla.com>
2014-03-28 15:50:28 +1300

    Bug 778077 - Implement HTMLMediaElement.fastSeek(time). r=cajbir
    Implement HTMLMediaElement.fastSeek(), basically by changing all the
    MediaDecoderReader::Seek() overrides to not call
    MediaDecoderReader::DecodeToTarget(), and have MediaDecoderReader::DecodeSeek()
    call DecodeToTarget() if we're doing an accurate (non-fast) seek.
    
    Update gizmo.mp4 to have a keyframe every second, instead of only 1 keyframe at
    the start of stream. This makes the unit test I added more useful for mp4...
    
    I pushed most of the seek target clamping logic in MediaDecoder up into
    HTMLMediaElement, so that we're clamping in fewer places. Note
    MediaDecoderStateMachine::Seek() still sanity checks the seek target.
    
    We have to update the currentTime/MediaDecoder playback position after a seek
    completes now, rather than assuming the seek always got it exactly right.
    
    Removed those pesky assertions about seek target lying in the first frame after
    seek, since actually sometimes the media doesn't have samples for all streams
    after a seek (either due to the media being encoded like that, or because of a
    bug in the platform's decoder, not entirely sure).
    
    Green: https://tbpl.mozilla.org/?tree=Try&rev=b028258565e2

--- content/media/ogg/OggReader.cpp
+++ content/media/ogg/OggReader.cpp
@@ -1315,9 +1315,9 @@ nsresult OggReader::SeekInUnbuffered(int64_t aTarget,
 }
 
 nsresult OggReader::Seek(int64_t aTarget,
-                           int64_t aStartTime,
-                           int64_t aEndTime,
-                           int64_t aCurrentTime)
+                         int64_t aStartTime,
+                         int64_t aEndTime,
+                         int64_t aCurrentTime)
 {
   NS_ASSERTION(mDecoder->OnDecodeThread(), "Should be on decode thread.");
   if (mIsChained)
@@ -1379,10 +1379,7 @@ nsresult OggReader::Seek(int64_t aTarget,
     }
   }
 
-  // The decode position must now be either close to the seek target, or
-  // we've seeked to before the keyframe before the seek target. Decode
-  // forward to the seek target frame.
-  return DecodeToTarget(aTarget);
+  return NS_OK;
 }
 
 // Reads a page from the media resource.
