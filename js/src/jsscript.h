33d0ff6dd65ada3e8f64d2450591089605294040
Till Schneidereit <till@tillschneidereit.net>
2014-03-27 23:50:46 -0400

    Bug 976536 - Don't relazify inlined functions. r=jandem, a=sledru

--- js/src/jsscript.h
+++ js/src/jsscript.h
@@ -746,6 +746,9 @@ class JSScript : public js::gc::BarrieredCell<JSScript>
     // Script has been reused for a clone.
     bool hasBeenCloned_:1;
 
+    // Script has been inlined at least once, and can't be relazified.
+    bool hasBeenInlined_:1;
+
     // Script came from eval(), and is still active.
     bool isActiveEval_:1;
 
@@ -928,10 +931,12 @@ class JSScript : public js::gc::BarrieredCell<JSScript>
     }
     bool hasRunOnce() const { return hasRunOnce_; }
     bool hasBeenCloned() const { return hasBeenCloned_; }
+    bool hasBeenInlined() const { return hasBeenInlined_; }
 
     void setTreatAsRunOnce() { treatAsRunOnce_ = true; }
     void setHasRunOnce() { hasRunOnce_ = true; }
     void setHasBeenCloned() { hasBeenCloned_ = true; }
+    void setHasBeenInlined() { hasBeenInlined_ = true; }
 
     bool isActiveEval() const { return isActiveEval_; }
     bool isCachedEval() const { return isCachedEval_; }
@@ -1147,7 +1152,7 @@ class JSScript : public js::gc::BarrieredCell<JSScript>
     bool isRelazifiable() const {
         return (selfHosted() || lazyScript) &&
                !(analyzedArgsUsage() && needsArgsObj()) &&
-               !isGenerator() && !hasBaselineScript() && !hasAnyIonScript();
+               !isGenerator() && !hasBaselineScript() && !hasAnyIonScript() && !hasBeenInlined();
     }
     void setLazyScript(js::LazyScript *lazy) {
         lazyScript = lazy;
