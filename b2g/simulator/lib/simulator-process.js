85920c573f9ebab4e4a29f23e08ee2520dc12340
Alexandre Poirot <poirot.alex@gmail.com>
2014-04-01 09:15:06 -0400

    Bug 990057 - Fix Promise exception in simulator. r=jryans

--- b2g/simulator/lib/simulator-process.js
+++ b2g/simulator/lib/simulator-process.js
@@ -17,7 +17,7 @@ const Runtime = require("sdk/system/runtime");
 const Self = require("sdk/self");
 const URL = require("sdk/url");
 const Subprocess = require("subprocess");
-const { Promise } = Cu.import("resource://gre/modules/Promise.jsm", {});
+const { Promise: promise } = Cu.import("resource://gre/modules/Promise.jsm", {});
 
 const { rootURI: ROOT_URI } = require('@loader/options');
 const PROFILE_URL = ROOT_URI + "profile/";
@@ -112,7 +112,7 @@ exports.SimulatorProcess = Class({
 
   // request a b2g instance kill
   kill: function() {
-    let deferred = Promise.defer();
+    let deferred = promise.defer();
     if (this.process) {
       this.once("exit", (exitCode) => {
         this.shuttingDown = false;
@@ -125,7 +125,7 @@ exports.SimulatorProcess = Class({
       }
       return deferred.promise;
     } else {
-      return Promise.resolve(undefined);
+      return promise.resolve(undefined);
     }
   },
 
