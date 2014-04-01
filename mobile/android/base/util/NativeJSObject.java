d291d3bec453742e0a6250fe59b149804e27a185
Jim Chen <nchen@mozilla.com>
2014-04-01 15:16:52 -0400

    Bug 984458 - a. Add NativeJSContainer implementation; r=blassey

---
+++ mobile/android/base/util/NativeJSObject.java
@@ -0,0 +1,17 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.util;
+
+import org.mozilla.gecko.mozglue.JNITarget;
+
+/**
+ * NativeJSObject is a wrapper around the SpiderMonkey JSAPI to make it possible to
+ * access Javascript objects in Java.
+ */
+@JNITarget
+public class NativeJSObject
+{
+}
