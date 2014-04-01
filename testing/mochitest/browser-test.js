cf3333bb4816d4635e26d1782bf48f2efb5a4878
Paolo Amadini <paolo.mozmail@amadzone.org>
2014-04-01 13:45:48 +0200

    Bug 985954 - Remove deprecated promise.js usage in the testing framework. r=ted

--- testing/mochitest/browser-test.js
+++ testing/mochitest/browser-test.js
@@ -86,7 +86,7 @@ function Tester(aTests, aDumper, aCallback) {
   this.SimpleTest = simpleTestScope.SimpleTest;
   this.MemoryStats = simpleTestScope.MemoryStats;
   this.Task = Components.utils.import("resource://gre/modules/Task.jsm", null).Task;
-  this.Promise = Components.utils.import("resource://gre/modules/commonjs/sdk/core/promise.js", null).Promise;
+  this.Promise = Components.utils.import("resource://gre/modules/Promise.jsm", null).Promise;
   this.Assert = Components.utils.import("resource://testing-common/Assert.jsm", null).Assert;
 }
 Tester.prototype = {
