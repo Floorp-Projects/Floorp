5f01c6e65c28668960c06b6fb31b0835f000e2b9
Ryan VanderMeulen <ryanvm@gmail.com>
2014-04-01 13:18:52 -0400

    Backed out changeset 65338f03492b (bug 990065) and changeset 8c3b772518f2 (bug 990057) for B2G desktop mochitest failures. CLOSED TREE

--- b2g/simulator/lib/main.js
+++ b2g/simulator/lib/main.js
@@ -6,7 +6,7 @@
 const { Cc, Ci, Cu } = require("chrome");
 
 const { SimulatorProcess } = require("./simulator-process");
-const { Promise: promise } = Cu.import("resource://gre/modules/Promise.jsm", {});
+const { Promise } = Cu.import("resource://gre/modules/Promise.jsm", {});
 const Self = require("sdk/self");
 const System = require("sdk/system");
 const { Simulator } = Cu.import("resource://gre/modules/devtools/Simulator.jsm");
@@ -23,12 +23,12 @@ function launch({ port }) {
   process.remoteDebuggerPort = port;
   process.run();
 
-  return promise.resolve();
+  return Promise.resolve();
 }
 
 function close() {
   if (!process) {
-    return promise.resolve();
+    return Promise.resolve();
   }
   let p = process;
   process = null;
