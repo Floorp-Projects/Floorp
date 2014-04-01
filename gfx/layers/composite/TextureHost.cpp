ebcd6e546f00199d6de4f5838a365754a6232cef
Sotaro Ikeda <sikeda@mozilla.com>
2014-03-31 20:24:55 -0700

    Bug 989215 - clear recycle callback in TextureParent::ActorDestroy() r=nical

--- gfx/layers/composite/TextureHost.cpp
+++ gfx/layers/composite/TextureHost.cpp
@@ -844,6 +844,10 @@ TextureParent::ActorDestroy(ActorDestroyReason why)
     mTextureHost->ForgetSharedData();
   }
 
+  // Clear recycle callback.
+  mTextureHost->ClearRecycleCallback();
+  mWaitForClientRecycle = nullptr;
+
   mTextureHost->mActor = nullptr;
   mTextureHost = nullptr;
 }
