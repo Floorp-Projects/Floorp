83eb7ca9ebc8c14f040441816fc60c88c7c59b24
Wes Kocher <wkocher@mozilla.com>
2014-04-03 15:15:26 -0700

    Backed out changeset a17657a71b01 (bug 991376) for pushing tests around, causing chunked browser-chrome bustage on a CLOSED TREE

--- browser/devtools/debugger/debugger-panes.js
+++ browser/devtools/debugger/debugger-panes.js
@@ -1812,12 +1812,6 @@ VariableBubbleView.prototype = {
   },
 
   /**
-   * Specifies whether literals can be (redundantly) inspected in a popup.
-   * This behavior is deprecated, but still tested in a few places.
-   */
-  _ignoreLiterals: true,
-
-  /**
    * Searches for an identifier underneath the specified position in the
    * source editor, and if found, opens a VariablesView inspection popup.
    *
@@ -1856,8 +1850,7 @@ VariableBubbleView.prototype = {
     let identifierInfo = parsedSource.getIdentifierAt({
       line: scriptLine + 1,
       column: scriptColumn,
-      scriptIndex: scriptInfo.index,
-      ignoreLiterals: this._ignoreLiterals
+      scriptIndex: scriptInfo.index
     });
 
     // If the info is null, we're not hovering any identifier.
