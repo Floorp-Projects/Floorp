a5902d45bc1afd014339be8e1fdd06f239a32a82
Vladimir Vukicevic <vladimir@pobox.com>
2014-04-03 13:29:22 +0800

    Bug 912196. Add support for ANGLE D3D11 path. r=jgilbert
    There are currently problems with the color format chosen
    which breaks MSAA resolution. But we might as well land this
    now.
    
    --HG--
    extra : rebase_source : 1287e5c059e263d03b0d612ce5cf17e93bc5b033

--- gfx/gl/GLContext.cpp
+++ gfx/gl/GLContext.cpp
@@ -1740,10 +1740,9 @@ GLContext::UpdatePixelFormat()
     MOZ_ASSERT(caps.color == !!format.green);
     MOZ_ASSERT(caps.color == !!format.blue);
 
-    MOZ_ASSERT(caps.alpha == !!format.alpha);
-
     // These we either must have if they're requested, or
     // we can have if they're not.
+    MOZ_ASSERT(caps.alpha == !!format.alpha || !caps.alpha);
     MOZ_ASSERT(caps.depth == !!format.depth || !caps.depth);
     MOZ_ASSERT(caps.stencil == !!format.stencil || !caps.stencil);
 
