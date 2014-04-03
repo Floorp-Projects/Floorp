7f6e8b96b9dcc8fa663165fa84733e05869ac352
Chris Pearce <cpearce@mozilla.com>
2014-04-04 10:39:42 +1300

    Bug 986947 - Make MP3 contained in MP4 playback again on Windows with WMF backend. r=padenot
    Report that we can play MP3 inside MP4 on Windows Vista and later in
    HTMLMediaElement.canPlayType. Chrome and IE on Windows match this behaviour.
    
    Add a test file with MP3 contained inside MP4. Note the B2G emulator can't play
    this file, so I added a codecs parameter to the file's mime type so that
    decoder backends have to opt-in to testing with it.
    
    Enable playback of MP3 inside MP4 in WMFReader.
    
    Change from reporting the IMFSourceReader's duration inside WMFReader, to
    instead report the IMFSourceReader's duration as the media "end time". This is
    needed because the MP3-contained-in-MP4 file's first sample output by the
    IMFSourceReader has a non-zero timestamp, and the MediaDecoderStateMachine
    assumes that the media samples will be in the range
    [$firstSampleStartTime, $firstSampleStartTime+$reportedDuration]. But that's
    not the case here, the IMFSourceReader seems to output samples in the range
    [0,$reportedDuration]. This assumption mismatch means on the
    MP3-contained-in-MP4 file we end up trying to seek after what the
    IMFSourceReader assumes is the end of the file, which fails and causes
    test failures.

--- content/media/DecoderTraits.cpp
+++ content/media/DecoderTraits.cpp
@@ -404,6 +404,13 @@ DecoderTraits::CanHandleMediaType(const char* aMIMEType,
     }
   }
 #endif
+#ifdef MOZ_DIRECTSHOW
+  // Note: DirectShow should come before WMF, so that we prefer DirectShow's
+  // MP3 support over WMF's.
+  if (DirectShowDecoder::GetSupportedCodecs(nsDependentCString(aMIMEType), &codecList)) {
+    result = CANPLAY_MAYBE;
+  }
+#endif
 #ifdef MOZ_WMF
   if (IsWMFSupportedType(nsDependentCString(aMIMEType))) {
     if (!aHaveRequestedCodecs) {
@@ -412,11 +419,6 @@ DecoderTraits::CanHandleMediaType(const char* aMIMEType,
     return WMFDecoder::CanPlayType(nsDependentCString(aMIMEType),
                                    aRequestedCodecs)
            ? CANPLAY_YES : CANPLAY_NO;
-  }
-#endif
-#ifdef MOZ_DIRECTSHOW
-  if (DirectShowDecoder::GetSupportedCodecs(nsDependentCString(aMIMEType), &codecList)) {
-    result = CANPLAY_MAYBE;
   }
 #endif
 #ifdef MOZ_APPLEMEDIA
@@ -523,8 +525,8 @@ InstantiateDecoder(const nsACString& aType, MediaDecoderOwner* aOwner)
   }
 #endif
 #ifdef MOZ_DIRECTSHOW
-  // Note: DirectShow decoder must come before WMFDecoder, else the pref
-  // "media.directshow.preferred" won't be honored.
+  // Note: DirectShow should come before WMF, so that we prefer DirectShow's
+  // MP3 support over WMF's.
   if (IsDirectShowSupportedType(aType)) {
     decoder = new DirectShowDecoder();
     return decoder.forget();
