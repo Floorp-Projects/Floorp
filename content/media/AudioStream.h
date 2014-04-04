41864e01d948e0c05bbffc7afa7050d813c38d08
Ed Morley <emorley@mozilla.com>
2014-04-04 16:32:19 +0100

    Backed out changeset 2ac8fe9a90c5 (bug 948269) for timeouts in gaia-integration tests; CLOSED TREE

--- content/media/AudioStream.h
+++ content/media/AudioStream.h
@@ -221,9 +221,6 @@ public:
   // Block until buffered audio data has been consumed.
   void Drain();
 
-  // Break any blocking operation and set the stream to shutdown.
-  void Cancel();
-
   // Start the stream.
   void Start();
 
