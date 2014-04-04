241c921f7bb2a768ab47a5b1274ae9cb83987d7b
Benjamin Bouvier <benj@benj.me>
2014-04-04 15:07:55 +0200

    Bug 991510: Make arrow functions not validate as asm.js modules; r=luke

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -119,6 +119,10 @@ struct ParseContext : public GenericParseContext
     bool isLegacyGenerator() const { return generatorKind() == LegacyGenerator; }
     bool isStarGenerator() const { return generatorKind() == StarGenerator; }
 
+    bool isArrowFunction() const {
+        return sc->isFunctionBox() && sc->asFunctionBox()->function()->isArrow();
+    }
+
     uint32_t        blockScopeDepth; /* maximum depth of nested block scopes, in slots */
     Node            blockNode;      /* parse node for a block with let declarations
                                        (block with its own lexical scope)  */
