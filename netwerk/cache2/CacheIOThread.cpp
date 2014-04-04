66ee902a420fbaed04484ae9d1dc5dae965747cb
Honza Bambas <honzab.moz@firemni.cz>
2014-04-04 14:42:05 +0200

    Bug 976866 - HTTP cache v2: Specific dispatch method for eviction events, r=michal

--- netwerk/cache2/CacheIOThread.cpp
+++ netwerk/cache2/CacheIOThread.cpp
@@ -60,6 +60,28 @@ nsresult CacheIOThread::Dispatch(nsIRunnable* aRunnable, uint32_t aLevel)
   if (mShutdown && (PR_GetCurrentThread() != mThread))
     return NS_ERROR_UNEXPECTED;
 
+  return DispatchInternal(aRunnable, aLevel);
+}
+
+nsresult CacheIOThread::DispatchAfterPendingOpens(nsIRunnable* aRunnable)
+{
+  MonitorAutoLock lock(mMonitor);
+
+  if (mShutdown && (PR_GetCurrentThread() != mThread))
+    return NS_ERROR_UNEXPECTED;
+
+  // Move everything from later executed OPEN level to the OPEN_PRIORITY level
+  // where we post the (eviction) runnable.
+  mEventQueue[OPEN_PRIORITY].AppendElements(mEventQueue[OPEN]);
+  mEventQueue[OPEN].Clear();
+
+  return DispatchInternal(aRunnable, OPEN_PRIORITY);
+}
+
+nsresult CacheIOThread::DispatchInternal(nsIRunnable* aRunnable, uint32_t aLevel)
+{
+  mMonitor.AssertCurrentThreadOwns();
+
   mEventQueue[aLevel].AppendElement(aRunnable);
   if (mLowestLevelWaiting > aLevel)
     mLowestLevelWaiting = aLevel;
