ebe098a4751b0fbb83421039790f0dcc35c6a79f
Jim Chen <nchen@mozilla.com>
2014-04-04 12:33:50 -0400

    Bug 989046 - b. Drop JSON.stringify when calling handleGeckoMessage; r=mfinkle

--- mobile/android/base/tests/robocop_testharness.js
+++ mobile/android/base/tests/robocop_testharness.js
@@ -3,12 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-let bridge = SpecialPowers.Cc["@mozilla.org/android/bridge;1"]
-                          .getService(SpecialPowers.Ci.nsIAndroidBridge);
-
 function sendMessageToJava(message) {
-  let data = JSON.stringify(message);
-  bridge.handleGeckoMessage(data);
+  SpecialPowers.Services.androidBridge.handleGeckoMessage(message);
 }
 
 function _evalURI(uri, sandbox) {
