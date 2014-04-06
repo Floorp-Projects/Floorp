fecfb0d5313340949dd618ce452d54948c51224c
Bobby Holley <bobbyholley@gmail.com>
2014-04-06 12:32:39 -0700

    Bug 986730 - Put all anonymous content into the XBL scope. r=smaug

--- content/base/public/nsINode.h
+++ content/base/public/nsINode.h
@@ -414,7 +414,9 @@ protected:
   //
   mozilla::dom::ParentObject GetParentObjectInternal(nsINode* aNativeParent) const {
     mozilla::dom::ParentObject p(aNativeParent);
-    p.mUseXBLScope = ChromeOnlyAccess();
+    // Note that mUseXBLScope is a no-op for chrome, and other places where we
+    // don't use XBL scopes.
+    p.mUseXBLScope = IsInAnonymousSubtree();
     return p;
   }
 
