364caa62f24817b26692fdd04ff227f909c58335
Dave Townsend <dtownsend@oxymoronical.com>
2014-04-07 12:49:43 -0700

    Bug 989374: Consolidate logic from browser_dbg_addon-* tests. r=fitzgen

--- browser/devtools/debugger/test/browser_dbg_addon-modules.js
+++ browser/devtools/debugger/test/browser_dbg_addon-modules.js
@@ -3,135 +3,46 @@
 
 // Make sure the add-on actor can see loaded JS Modules from an add-on
 
-const ADDON4_URL = EXAMPLE_URL + "addon4.xpi";
-
-let gAddon, gClient, gThreadClient, gDebugger, gSources, gTitle;
-
-function onMessage(event) {
-  try {
-    let json = JSON.parse(event.data);
-    switch (json.name) {
-      case "toolbox-title":
-        gTitle = json.data.value;
-        break;
-    }
-  } catch(e) {
-    DevToolsUtils.reportException("onMessage", e);
-  }
-}
+const ADDON_URL = EXAMPLE_URL + "addon4.xpi";
 
 function test() {
   Task.spawn(function () {
-    if (!DebuggerServer.initialized) {
-      DebuggerServer.init(() => true);
-      DebuggerServer.addBrowserActors();
-    }
+    let addon = yield addAddon(ADDON_URL);
+    let addonDebugger = yield initAddonDebugger(ADDON_URL);
 
-    gBrowser.selectedTab = gBrowser.addTab();
-    let iframe = document.createElement("iframe");
-    document.documentElement.appendChild(iframe);
+    is(addonDebugger.title, "Debugger - Test add-on with JS Modules", "Saw the right toolbox title.");
 
-    window.addEventListener("message", onMessage);
+    // Check the inital list of sources is correct
+    let groups = yield addonDebugger.getSourceGroups();
+    is(groups[0].name, "browser_dbg_addon4@tests.mozilla.org", "Add-on code should be the first group");
+    is(groups.length, 1, "Should be only one group.");
 
-    let transport = DebuggerServer.connectPipe();
-    gClient = new DebuggerClient(transport);
+    let sources = groups[0].sources;
+    is(sources.length, 2, "Should be two sources");
+    ok(sources[0].url.endsWith("/browser_dbg_addon4@tests.mozilla.org.xpi!/bootstrap.js"), "correct url for bootstrap code")
+    is(sources[0].label, "bootstrap.js", "correct label for bootstrap code")
+    is(sources[1].url, "resource://browser_dbg_addon4/test.jsm", "correct url for addon code")
+    is(sources[1].label, "test.jsm", "correct label for addon code")
 
-    let connected = promise.defer();
-    gClient.connect(connected.resolve);
-    yield connected.promise;
-
-    yield installAddon();
-    let debuggerPanel = yield initAddonDebugger(gClient, ADDON4_URL, iframe);
-    gDebugger = debuggerPanel.panelWin;
-    gThreadClient = gDebugger.gThreadClient;
-    gSources = gDebugger.DebuggerView.Sources;
-
-    yield testSources(false);
-
+    // Load a new module and check it appears in the list of sources
     Cu.import("resource://browser_dbg_addon4/test2.jsm", {});
 
-    yield testSources(true);
+    groups = yield addonDebugger.getSourceGroups();
+    is(groups[0].name, "browser_dbg_addon4@tests.mozilla.org", "Add-on code should be the first group");
+    is(groups.length, 1, "Should be only one group.");
+
+    sources = groups[0].sources;
+    is(sources.length, 3, "Should be three sources");
+    ok(sources[0].url.endsWith("/browser_dbg_addon4@tests.mozilla.org.xpi!/bootstrap.js"), "correct url for bootstrap code")
+    is(sources[0].label, "bootstrap.js", "correct label for bootstrap code")
+    is(sources[1].url, "resource://browser_dbg_addon4/test.jsm", "correct url for addon code")
+    is(sources[1].label, "test.jsm", "correct label for addon code")
+    is(sources[2].url, "resource://browser_dbg_addon4/test2.jsm", "correct url for addon code")
+    is(sources[2].label, "test2.jsm", "correct label for addon code")
 
     Cu.unload("resource://browser_dbg_addon4/test2.jsm");
-
-    yield uninstallAddon();
-    yield closeConnection();
-    yield debuggerPanel._toolbox.destroy();
-    iframe.remove();
-    window.removeEventListener("message", onMessage);
+    yield addonDebugger.destroy();
+    yield removeAddon(addon);
     finish();
   });
 }
-
-function installAddon () {
-  return addAddon(ADDON4_URL).then(aAddon => {
-    gAddon = aAddon;
-  });
-}
-
-function testSources(expectSecondModule) {
-  let deferred = promise.defer();
-  let foundAddonModule = false;
-  let foundAddonModule2 = false;
-  let foundAddonBootstrap = false;
-
-  gThreadClient.getSources(({sources}) => {
-    ok(sources.length, "retrieved sources");
-
-    for (let source of sources) {
-      let url = source.url.split(" -> ").pop();
-      let { label, group } = gSources.getItemByValue(source.url).attachment;
-
-      if (url.indexOf("resource://browser_dbg_addon4/test.jsm") === 0) {
-        is(label, "test.jsm", "correct label for addon code");
-        is(group, "browser_dbg_addon4@tests.mozilla.org", "addon module is in the add-on's group");
-        foundAddonModule = true;
-      } else if (url.indexOf("resource://browser_dbg_addon4/test2.jsm") === 0) {
-        is(label, "test2.jsm", "correct label for addon code");
-        is(group, "browser_dbg_addon4@tests.mozilla.org", "addon module is in the add-on's group");
-        foundAddonModule2 = true;
-      } else if (url.endsWith("/browser_dbg_addon4@tests.mozilla.org.xpi!/bootstrap.js")) {
-        is(label, "bootstrap.js", "correct label for bootstrap code");
-        is(group, "browser_dbg_addon4@tests.mozilla.org", "addon bootstrap script is in the add-on's group");
-        foundAddonBootstrap = true;
-      } else {
-        ok(false, "Saw an unexpected source: " + url);
-      }
-    }
-
-    ok(foundAddonModule, "found JS module for the addon in the list");
-    is(foundAddonModule2, expectSecondModule, "saw the second addon module");
-    ok(foundAddonBootstrap, "found bootstrap script for the addon in the list");
-
-    is(gTitle, "Debugger - Test add-on with JS Modules", "Saw the right toolbox title.");
-
-    let groups = gDebugger.document.querySelectorAll(".side-menu-widget-group-title .name");
-    is(groups[0].value, "browser_dbg_addon4@tests.mozilla.org", "Add-on code should be the first group");
-    is(groups.length, 1, "Should be only one group.");
-
-    deferred.resolve();
-  });
-
-  return deferred.promise;
-}
-
-function uninstallAddon() {
-  return removeAddon(gAddon);
-}
-
-function closeConnection () {
-  let deferred = promise.defer();
-  gClient.close(deferred.resolve);
-  return deferred.promise;
-}
-
-registerCleanupFunction(function() {
-  gClient = null;
-  gAddon = null;
-  gThreadClient = null;
-  gDebugger = null;
-  gSources = null;
-  while (gBrowser.tabs.length > 1) {
-    gBrowser.removeCurrentTab();
-  }
-});
