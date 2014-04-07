0b19de8cef5bf49d4d9f060996eec5942a2fd81f
James Kitchener <jkitch.bug@gmail.com>
2014-04-07 06:56:00 +0200

    Bug 557478 - Defer mfrac operator spacing to outermost embellished operator. r=fredw

--- layout/mathml/nsMathMLmrowFrame.cpp
+++ layout/mathml/nsMathMLmrowFrame.cpp
@@ -53,3 +53,17 @@ nsMathMLmrowFrame::AttributeChanged(int32_t  aNameSpaceID,
 
   return nsMathMLContainerFrame::AttributeChanged(aNameSpaceID, aAttribute, aModType);
 }
+
+/* virtual */ eMathMLFrameType
+nsMathMLmrowFrame::GetMathMLFrameType()
+{
+  if (!IsMrowLike()) {
+    nsIMathMLFrame* child = do_QueryFrame(mFrames.FirstChild());
+    if (child) {
+      // We only have one child, so we return the frame type of that child as if
+      // we didn't exist.
+      return child->GetMathMLFrameType();
+    }
+  }
+  return nsMathMLFrame::GetMathMLFrameType();
+}
