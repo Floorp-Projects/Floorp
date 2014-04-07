852d079189e54da1723afa298569ed41cd1645dc
Steve Workman <sworkman@mozilla.com>
2014-04-07 11:26:22 -0700

    Bug 990268 - Send FTPChannelParent::mStatus in IPDL messages to FTPChannelChild r=mayhemer

--- netwerk/protocol/ftp/FTPChannelParent.cpp
+++ netwerk/protocol/ftp/FTPChannelParent.cpp
@@ -312,8 +312,8 @@ FTPChannelParent::OnStartRequest(nsIRequest* aRequest, nsISupports* aContext)
   chan->GetURI(getter_AddRefs(uri));
   SerializeURI(uri, uriparam);
 
-  if (mIPCClosed || !SendOnStartRequest(contentLength, contentType,
-                                       lastModified, entityID, uriparam)) {
+  if (mIPCClosed || !SendOnStartRequest(mStatus, contentLength, contentType,
+                                        lastModified, entityID, uriparam)) {
     return NS_ERROR_UNEXPECTED;
   }
 
@@ -366,7 +366,7 @@ FTPChannelParent::OnDataAvailable(nsIRequest* aRequest,
   if (NS_FAILED(rv))
     return rv;
 
-  if (mIPCClosed || !SendOnDataAvailable(data, aOffset, aCount))
+  if (mIPCClosed || !SendOnDataAvailable(mStatus, data, aOffset, aCount))
     return NS_ERROR_UNEXPECTED;
 
   return NS_OK;
