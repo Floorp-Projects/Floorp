acb00db9753839e4b2982d15a65df16521241d15
Nick Alexander <nalexander@mozilla.com>
2014-04-08 11:19:37 -0700

    Bug 985766 - Always include payload verification hashes in FxAccountClient* requests. r=rnewman
    ========
    
    https://github.com/mozilla-services/android-sync/commit/130f1a5d529a33ff2206fd58378860affa05c016
    Author: Nick Alexander <nalexander@mozilla.com>
        Bug 985766 - Part 4: Explain why we do not include payload verification hashes in Sync storage endpoint requests.
    
    ========
    
    https://github.com/mozilla-services/android-sync/commit/5cb1211b48d6f9352a918d69c98e4a4c001958ac
    Author: Nick Alexander <nalexander@mozilla.com>
    Date:   Tue Apr 8 11:19:03 2014 -0700
    
        Bug 985766 - Part 3: Always include payload verification hashes in FxAccountClient* requests.
    
    ========
    
    https://github.com/mozilla-services/android-sync/commit/616193c43683d01a8f8558957cf1a3849a9596f1
    Author: Nick Alexander <nalexander@mozilla.com>
    Date:   Tue Apr 8 11:18:10 2014 -0700
    
        Bug 985766 - Part 2: Don't include payload verification to non-enclosing HAWK requests.
    
    ========
    
    https://github.com/mozilla-services/android-sync/commit/c0c032db0970b0056ad51f2b2bb73ec2bf468cee
    Author: Nick Alexander <nalexander@mozilla.com>
    Date:   Thu Apr 3 11:05:30 2014 -0700
    
        Bug 985766 - Part 1: Remove little-used HawkAuthHeaderProvider constructor.

--- mobile/android/base/background/fxa/FxAccountClient10.java
+++ mobile/android/base/background/fxa/FxAccountClient10.java
@@ -176,32 +176,35 @@ public class FxAccountClient10 {
 
     protected final byte[] tokenId;
     protected final byte[] reqHMACKey;
-    protected final boolean payload;
     protected final SkewHandler skewHandler;
 
     /**
      * Create a delegate for an un-authenticated resource.
      */
     public ResourceDelegate(final Resource resource, final RequestDelegate<T> delegate) {
-      this(resource, delegate, null, null, false);
+      this(resource, delegate, null, null);
     }
 
     /**
      * Create a delegate for a Hawk-authenticated resource.
+     * <p>
+     * Every Hawk request that encloses an entity (PATCH, POST, and PUT) will
+     * include the payload verification hash.
      */
-    public ResourceDelegate(final Resource resource, final RequestDelegate<T> delegate, final byte[] tokenId, final byte[] reqHMACKey, final boolean authenticatePayload) {
+    public ResourceDelegate(final Resource resource, final RequestDelegate<T> delegate, final byte[] tokenId, final byte[] reqHMACKey) {
       super(resource);
       this.delegate = delegate;
       this.reqHMACKey = reqHMACKey;
       this.tokenId = tokenId;
-      this.payload = authenticatePayload;
       this.skewHandler = SkewHandler.getSkewHandlerForResource(resource);
     }
 
     @Override
     public AuthHeaderProvider getAuthHeaderProvider() {
       if (tokenId != null && reqHMACKey != null) {
-        return new HawkAuthHeaderProvider(Utils.byte2Hex(tokenId), reqHMACKey, payload, skewHandler.getSkewInSeconds());
+        // We always include the payload verification hash for FxA Hawk-authenticated requests.
+        final boolean includePayloadVerificationHash = true;
+        return new HawkAuthHeaderProvider(Utils.byte2Hex(tokenId), reqHMACKey, includePayloadVerificationHash, skewHandler.getSkewInSeconds());
       }
       return super.getAuthHeaderProvider();
     }
@@ -483,7 +486,7 @@ public class FxAccountClient10 {
       return;
     }
 
-    resource.delegate = new ResourceDelegate<TwoTokens>(resource, delegate, tokenId, reqHMACKey, false) {
+    resource.delegate = new ResourceDelegate<TwoTokens>(resource, delegate, tokenId, reqHMACKey) {
       @Override
       public void handleSuccess(int status, HttpResponse response, ExtendedJSONObject body) {
         try {
@@ -519,7 +522,7 @@ public class FxAccountClient10 {
       return;
     }
 
-    resource.delegate = new ResourceDelegate<Void>(resource, delegate, tokenId, reqHMACKey, false) {
+    resource.delegate = new ResourceDelegate<Void>(resource, delegate, tokenId, reqHMACKey) {
       @Override
       public void handleSuccess(int status, HttpResponse response, ExtendedJSONObject body) {
         delegate.handleSuccess(null);
@@ -608,7 +611,7 @@ public class FxAccountClient10 {
       return;
     }
 
-    resource.delegate = new ResourceDelegate<TwoKeys>(resource, delegate, tokenId, reqHMACKey, false) {
+    resource.delegate = new ResourceDelegate<TwoKeys>(resource, delegate, tokenId, reqHMACKey) {
       @Override
       public void handleSuccess(int status, HttpResponse response, ExtendedJSONObject body) {
         try {
@@ -670,7 +673,7 @@ public class FxAccountClient10 {
       return;
     }
 
-    resource.delegate = new ResourceDelegate<StatusResponse>(resource, delegate, tokenId, reqHMACKey, false) {
+    resource.delegate = new ResourceDelegate<StatusResponse>(resource, delegate, tokenId, reqHMACKey) {
       @Override
       public void handleSuccess(int status, HttpResponse response, ExtendedJSONObject body) {
         try {
@@ -712,7 +715,7 @@ public class FxAccountClient10 {
       return;
     }
 
-    resource.delegate = new ResourceDelegate<String>(resource, delegate, tokenId, reqHMACKey, true) {
+    resource.delegate = new ResourceDelegate<String>(resource, delegate, tokenId, reqHMACKey) {
       @Override
       public void handleSuccess(int status, HttpResponse response, ExtendedJSONObject body) {
         String cert = body.getString("cert");
@@ -753,7 +756,7 @@ public class FxAccountClient10 {
       return;
     }
 
-    resource.delegate = new ResourceDelegate<Void>(resource, delegate, tokenId, reqHMACKey, false) {
+    resource.delegate = new ResourceDelegate<Void>(resource, delegate, tokenId, reqHMACKey) {
       @Override
       public void handleSuccess(int status, HttpResponse response, ExtendedJSONObject body) {
         try {
