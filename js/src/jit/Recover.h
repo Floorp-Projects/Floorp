3c81ea0eda8784b2eb654555561fb0ab6aa8788f
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2014-04-08 07:21:18 -0700

    Bug 989748 - Add RResumePoint to read recovery data. r=jandem

---
+++ js/src/jit/Recover.h
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=8 sts=4 et sw=4 tw=99:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef jit_Recover_h
+#define jit_Recover_h
+
+#include "jit/Snapshots.h"
+
+namespace js {
+namespace jit {
+
+class RResumePoint
+{
+  private:
+    uint32_t pcOffset_;           // Offset from script->code.
+    uint32_t numOperands_;        // Number of slots.
+
+    RResumePoint(CompactBufferReader &reader);
+
+  public:
+    static void readRecoverData(CompactBufferReader &reader, RInstructionStorage *raw);
+
+    uint32_t pcOffset() const {
+        return pcOffset_;
+    }
+    uint32_t numOperands() const {
+        return numOperands_;
+    }
+};
+
+}
+}
+
+#endif /* jit_Recover_h */
