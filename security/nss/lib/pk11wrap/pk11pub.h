6d40cad913e4b44475a1cd739e0b324dcbfb25e8
Michael Shuen <mz_mhs-ctb@outlook.com>
2014-04-08 11:27:31 -0700

    Bug 984608 - SECKEY_EncodeDERSubjectPublicKeyInfo and PK11_DEREncodePublicKey take non-const SECKEYPublicKey*. r=briansmith

--- security/nss/lib/pk11wrap/pk11pub.h
+++ security/nss/lib/pk11wrap/pk11pub.h
@@ -585,7 +585,7 @@ SECStatus PK11_WrapPrivKey(PK11SlotInfo *slot, PK11SymKey *wrappingKey,
  * The caller of PK11_DEREncodePublicKey should free the returned SECItem with
  * a SECITEM_FreeItem(..., PR_TRUE) call.
  */
-SECItem* PK11_DEREncodePublicKey(SECKEYPublicKey *pubk);
+SECItem* PK11_DEREncodePublicKey(const SECKEYPublicKey *pubk);
 PK11SymKey* PK11_CopySymKeyForSigning(PK11SymKey *originalKey,
 	CK_MECHANISM_TYPE mech);
 SECKEYPrivateKeyList* PK11_ListPrivKeysInSlot(PK11SlotInfo *slot,
