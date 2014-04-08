a9b3c31ca875f654a996e3a3bba20174a0d52a90
Dan Gohman <sunfish@mozilla.com>
2014-04-08 06:53:42 -0700

    Bug 993174 - SpiderMonkey: Fix missing declarations under --disable-unified-compilation. r=mrosenberg, a=NPOTB

--- js/src/jit/arm/Simulator-arm.cpp
+++ js/src/jit/arm/Simulator-arm.cpp
@@ -37,6 +37,25 @@
 #include "jit/AsmJS.h"
 #include "vm/Runtime.h"
 
+extern "C" {
+
+int64_t
+__aeabi_idivmod(int x, int y)
+{
+    uint32_t lo = uint32_t(x / y);
+    uint32_t hi = uint32_t(x % y);
+    return (int64_t(hi) << 32) | lo;
+}
+
+int64_t
+__aeabi_uidivmod(int x, int y)
+{
+    uint32_t lo = uint32_t(x) / uint32_t(y);
+    uint32_t hi = uint32_t(x) % uint32_t(y);
+    return (int64_t(hi) << 32) | lo;
+}
+}
+
 namespace js {
 namespace jit {
 
@@ -4270,23 +4289,4 @@ JSRuntime::setSimulatorRuntime(js::jit::SimulatorRuntime *srt)
 {
     MOZ_ASSERT(!simulatorRuntime_);
     simulatorRuntime_ = srt;
-}
-
-extern "C" {
-
-int64_t
-__aeabi_idivmod(int x, int y)
-{
-    uint32_t lo = uint32_t(x / y);
-    uint32_t hi = uint32_t(x % y);
-    return (int64_t(hi) << 32) | lo;
-}
-
-int64_t
-__aeabi_uidivmod(int x, int y)
-{
-    uint32_t lo = uint32_t(x) / uint32_t(y);
-    uint32_t hi = uint32_t(x) % uint32_t(y);
-    return (int64_t(hi) << 32) | lo;
-}
 }
