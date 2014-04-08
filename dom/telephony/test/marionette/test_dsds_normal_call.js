e64795c441dbc9d4e863d61988efdcd84a8caa61
Hsin-Yi Tsai <htsai@mozilla.com>
2014-04-08 14:10:06 +0800

    Bug 989728 - [B2G] [RIL] split test_conference.js into smaller pieces to avoid timeout failure. r=vicamo * * * split test_conference.js - p4 - split test_conference

--- dom/telephony/test/marionette/test_dsds_normal_call.js
+++ dom/telephony/test/marionette/test_dsds_normal_call.js
@@ -4,170 +4,6 @@
 MARIONETTE_TIMEOUT = 60000;
 MARIONETTE_HEAD_JS = 'head.js';
 
-/**
- * The functions are created to provide the string format of the emulator call
- * list results.
- *
- * Usage:
- *   let outInfo = OutCallStrPool("911");
- *   outInfo.ringing == "outbound to 911        : ringing"
- *   outInfo.active  == "outbound to 911        : active"
- */
-function CallStrPool(prefix, number) {
-  let padding = "           : ";
-  let numberInfo = prefix + number + padding.substr(number.length);
-
-  let info = {};
-  let states = ["ringing", "incoming", "active", "held"];
-  for (let state of states) {
-    info[state] = numberInfo + state;
-  }
-
-  return info;
-}
-
-function OutCallStrPool(number) {
-  return CallStrPool("outbound to  ", number);
-}
-
-function InCallStrPool(number) {
-  return CallStrPool("inbound from ", number);
-}
-
-function checkEventCallState(event, call, state) {
-  is(call, event.call, "event.call");
-  is(call.state, state, "call state");
-}
-
-function checkTelephonyActiveAndCalls(active, calls) {
-  is(telephony.active, active, "telephony.active");
-  is(telephony.calls.length, calls.length, "telephony.calls");
-  for (let i = 0; i < calls.length; ++i) {
-    is(telephony.calls[i], calls[i]);
-  }
-}
-
-function checkEmulatorCallList(expectedCallList) {
-  let deferred = Promise.defer();
-
-  emulator.run("gsm list", function(result) {
-    log("Call list is now: " + result);
-    for (let i = 0; i < expectedCallList.length; ++i) {
-      is(result[i], expectedCallList[i], "emulator calllist");
-    }
-    is(result[expectedCallList.length], "OK", "emulator calllist");
-    deferred.resolve();
-  });
-
-  return deferred.promise;
-}
-
-// Promise.
-function checkAll(active, calls, callList) {
-  checkTelephonyActiveAndCalls(active, calls);
-  return checkEmulatorCallList(callList);
-}
-
-function dial(number, serviceId) {
-  serviceId = typeof serviceId !== "undefined" ? serviceId : 0;
-  log("Make an outgoing call: " + number + ", serviceId: " + serviceId);
-
-  let deferred = Promise.defer();
-
-  telephony.dial(number).then(call => {
-    ok(call);
-    is(call.number, number);
-    is(call.state, "dialing");
-
-    call.onalerting = function onalerting(event) {
-      call.onalerting = null;
-      log("Received 'onalerting' call event.");
-      is(call.serviceId, serviceId);
-      checkEventCallState(event, call, "alerting");
-      deferred.resolve(call);
-    };
-  });
-
-  return deferred.promise;
-}
-
-function remoteDial(number) {
-  log("Simulating an incoming call.");
-
-  let deferred = Promise.defer();
-
-  telephony.onincoming = function onincoming(event) {
-    log("Received 'incoming' call event.");
-    telephony.onincoming = null;
-
-    let call = event.call;
-
-    ok(call);
-    is(call.number, number);
-    is(call.state, "incoming");
-
-    deferred.resolve(call);
-  };
-  emulator.run("gsm call " + number);
-
-  return deferred.promise;
-}
-
-function answer(call) {
-  log("Answering the incoming call.");
-
-  let deferred = Promise.defer();
-
-  call.onconnecting = function onconnectingIn(event) {
-    log("Received 'connecting' call event for incoming call.");
-    call.onconnecting = null;
-    checkEventCallState(event, call, "connecting");
-  };
-
-  call.onconnected = function onconnectedIn(event) {
-    log("Received 'connected' call event for incoming call.");
-    call.onconnected = null;
-    checkEventCallState(event, call, "connected");
-    ok(!call.onconnecting);
-    deferred.resolve(call);
-  };
-  call.answer();
-
-  return deferred.promise;
-}
-
-function remoteAnswer(call) {
-  log("Remote answering the call.");
-
-  let deferred = Promise.defer();
-
-  call.onconnected = function onconnected(event) {
-    log("Received 'connected' call event.");
-    call.onconnected = null;
-    checkEventCallState(event, call, "connected");
-    deferred.resolve(call);
-  };
-  emulator.run("gsm accept " + call.number);
-
-  return deferred.promise;
-}
-
-function remoteHangUp(call) {
-  log("Remote hanging up the call.");
-
-  let deferred = Promise.defer();
-
-  call.ondisconnected = function ondisconnected(event) {
-    log("Received 'disconnected' call event.");
-    call.ondisconnected = null;
-    checkEventCallState(event, call, "disconnected");
-    deferred.resolve(call);
-  };
-  emulator.run("gsm cancel " + call.number);
-
-  return deferred.promise;
-}
-
 function muxModem(id) {
   let deferred = Promise.defer();
 
@@ -180,36 +16,36 @@ function muxModem(id) {
 
 function testOutgoingCallForServiceId(number, serviceId) {
   let outCall;
-  let outInfo = OutCallStrPool(number);
+  let outInfo = gOutCallStrPool(number);
 
   return Promise.resolve()
-    .then(() => dial(number, serviceId))
+    .then(() => gDial(number, serviceId))
     .then(call => {
       outCall = call;
       is(outCall.serviceId, serviceId);
     })
-    .then(() => checkAll(outCall, [outCall], [outInfo.ringing]))
-    .then(() => remoteAnswer(outCall))
-    .then(() => checkAll(outCall, [outCall], [outInfo.active]))
-    .then(() => remoteHangUp(outCall))
-    .then(() => checkAll(null, [], []));
+    .then(() => gCheckAll(outCall, [outCall], '', [], [outInfo.ringing]))
+    .then(() => gRemoteAnswer(outCall))
+    .then(() => gCheckAll(outCall, [outCall], '', [], [outInfo.active]))
+    .then(() => gRemoteHangUp(outCall))
+    .then(() => gCheckAll(null, [], '', [], []));
 }
 
 function testIncomingCallForServiceId(number, serviceId) {
   let inCall;
-  let inInfo = InCallStrPool(number);
+  let inInfo = gInCallStrPool(number);
 
   return Promise.resolve()
-    .then(() => remoteDial(number))
+    .then(() => gRemoteDial(number))
     .then(call => {
       inCall = call;
       is(inCall.serviceId, serviceId);
     })
-    .then(() => checkAll(null, [inCall], [inInfo.incoming]))
-    .then(() => answer(inCall))
-    .then(() => checkAll(inCall, [inCall], [inInfo.active]))
-    .then(() => remoteHangUp(inCall))
-    .then(() => checkAll(null, [], []));
+    .then(() => gCheckAll(null, [inCall], '', [], [inInfo.incoming]))
+    .then(() => gAnswer(inCall))
+    .then(() => gCheckAll(inCall, [inCall], '', [], [inInfo.active]))
+    .then(() => gRemoteHangUp(inCall))
+    .then(() => gCheckAll(null, [], '', [], []));
 }
 
 function testOutgoingCall() {
