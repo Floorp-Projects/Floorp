3ba6e8b857982f199b97e0246555023f3cb3819d
Henry Chang <hchang@mozilla.com>
2014-04-09 15:42:47 +0800

    Bug 993732 - Should use renamed properties done by bug 988129. pending r=gene
    Landed directly on mozilla-central in order to fix the Firefox OS bustage in today's build.

--- dom/messages/SystemMessageManager.js
+++ dom/messages/SystemMessageManager.js
@@ -230,8 +230,8 @@ SystemMessageManager.prototype = {
         // handle it again, which is redundant.
         cpmm.sendAsyncMessage("SystemMessageManager:Message:Return:OK",
                               { type: msg.type,
-                                manifest: this._manifest,
-                                uri: this._uri,
+                                manifestURL: this._manifestURL,
+                                pageURL: this._pageURL,
                                 msgID: msg.msgID });
       }
 
