98e070c38ee752a31d3a95dc5ba0eac6d39a49d3
Nick Alexander <nalexander@mozilla.com>
2014-04-09 09:20:15 -0700

    Bug 988437 - Part 2: Make Firefox Account Android Account type unique per package. r=rnewman, a=sylvestre
    This bumps every package's account type so that any issues shake out on
    Nightly (which otherwise would not have experienced an account type
    change).
    
    The trivial change to the AndroidManifest is to ensure that the new
    Account type is picked up by the build system, in the hopes of avoiding
    a clobber.

--- mobile/android/defs.mk
+++ mobile/android/defs.mk
@@ -1,23 +1,22 @@
 MOZ_ANDROID_SHARED_ID = $(ANDROID_PACKAGE_NAME).sharedID
+# Android Sync Account types are not per-package: release and beta
+# share an Account type, and aurora and nightly share an Account type.
 MOZ_ANDROID_SHARED_ACCOUNT_TYPE = $(ANDROID_PACKAGE_NAME)_sync
-MOZ_ANDROID_SHARED_FXACCOUNT_TYPE = $(ANDROID_PACKAGE_NAME)_account
+# Firefox Accounts Account types are per-package.
+MOZ_ANDROID_SHARED_FXACCOUNT_TYPE = $(ANDROID_PACKAGE_NAME)_fxaccount
 
 # We released these builds to the public with shared IDs and need to
 # keep them consistent.
 ifeq (org.mozilla.firefox,$(ANDROID_PACKAGE_NAME))
 MOZ_ANDROID_SHARED_ID = org.mozilla.firefox.sharedID
 MOZ_ANDROID_SHARED_ACCOUNT_TYPE = org.mozilla.firefox_sync
-MOZ_ANDROID_SHARED_FXACCOUNT_TYPE = org.mozilla.firefox_account
 else ifeq (org.mozilla.firefox_beta,$(ANDROID_PACKAGE_NAME))
 MOZ_ANDROID_SHARED_ID = org.mozilla.firefox.sharedID
 MOZ_ANDROID_SHARED_ACCOUNT_TYPE = org.mozilla.firefox_sync
-MOZ_ANDROID_SHARED_FXACCOUNT_TYPE = org.mozilla.firefox_account
 else ifeq (org.mozilla.fennec_aurora,$(ANDROID_PACKAGE_NAME))
 MOZ_ANDROID_SHARED_ID = org.mozilla.fennec.sharedID
 MOZ_ANDROID_SHARED_ACCOUNT_TYPE = org.mozilla.fennec_sync
-MOZ_ANDROID_SHARED_FXACCOUNT_TYPE = org.mozilla.fennec_account
 else ifeq (org.mozilla.fennec,$(ANDROID_PACKAGE_NAME))
 MOZ_ANDROID_SHARED_ID = org.mozilla.fennec.sharedID
 MOZ_ANDROID_SHARED_ACCOUNT_TYPE = org.mozilla.fennec_sync
-MOZ_ANDROID_SHARED_FXACCOUNT_TYPE = org.mozilla.fennec_account
 endif
