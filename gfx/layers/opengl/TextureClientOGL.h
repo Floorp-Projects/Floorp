4a95ad5ec8c3833d297b81907bfca3746723b303
Nicolas Silva <nical@mozilla.com>
2014-04-10 10:14:28 +0200

    Bug 990871 - Move TextureClientDrawTarget into TextureClient. r=mattwoodrow

--- gfx/layers/opengl/TextureClientOGL.h
+++ gfx/layers/opengl/TextureClientOGL.h
@@ -64,6 +64,16 @@ public:
     return nullptr;
   }
 
+  virtual gfx::SurfaceFormat GetFormat() const MOZ_OVERRIDE
+  {
+    return gfx::SurfaceFormat::UNKNOWN;
+  }
+
+  virtual bool AllocateForSurface(gfx::IntSize aSize, TextureAllocationFlags aFlags) MOZ_OVERRIDE
+  {
+    return false;
+  }
+
 protected:
   gl::SharedTextureHandle mHandle;
   gfx::IntSize mSize;
@@ -99,6 +109,16 @@ public:
   void InitWith(gfx::SurfaceStream* aStream);
 
   virtual gfx::IntSize GetSize() const { return gfx::IntSize(); }
+
+  virtual gfx::SurfaceFormat GetFormat() const MOZ_OVERRIDE
+  {
+    return gfx::SurfaceFormat::UNKNOWN;
+  }
+
+  virtual bool AllocateForSurface(gfx::IntSize aSize, TextureAllocationFlags aFlags) MOZ_OVERRIDE
+  {
+    return false;
+  }
 
 protected:
   bool mIsLocked;
