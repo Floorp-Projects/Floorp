30bcf961462737625c16bc8f85c54afd66b33b22
Till Schneidereit <till@tillschneidereit.net>
2014-04-10 14:07:03 +0200

    Bug 988416 - Part 1: Extract self-hosting #defines into builtin/SelfHostingDefines.h. r=jwalden

---
+++ js/src/builtin/SelfHostingDefines.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=8 sts=4 et sw=4 tw=99:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Specialized .h file to be used by both JS and C++ code.
+
+#ifndef builtin_SelfHostingDefines_h
+#define builtin_SelfHostingDefines_h
+
+// Utility macros.
+#define TO_INT32(x) ((x) | 0)
+#define TO_UINT32(x) ((x) >>> 0)
+#define IS_UINT32(x) ((x) >>> 0 === (x))
+
+// Assertions.
+#ifdef DEBUG
+#define assert(b, info) if (!(b)) AssertionFailed(info)
+#else
+#define assert(b, info) // Elided assertion.
+#endif
+
+// Unforgeable versions of ARRAY.push(ELEMENT) and ARRAY.slice.
+#define ARRAY_PUSH(ARRAY, ELEMENT) \
+  callFunction(std_Array_push, ARRAY, ELEMENT);
+#define ARRAY_SLICE(ARRAY, ELEMENT) \
+  callFunction(std_Array_slice, ARRAY, ELEMENT);
+
+#endif
