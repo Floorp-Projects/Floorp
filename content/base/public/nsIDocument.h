af32e9b5f37ab9799cfc82581ce7b0d5207f866e
Andrew McCreight <continuation@gmail.com>
2014-04-10 07:05:20 -0700

    Bug 993194, part 1 - Add a comment to nsDocument about mNodeInfoManager. r=smaug

--- content/base/public/nsIDocument.h
+++ content/base/public/nsIDocument.h
@@ -2313,8 +2313,9 @@ protected:
   // A reference to the element last returned from GetRootElement().
   mozilla::dom::Element* mCachedRootElement;
 
-  // We hold a strong reference to mNodeInfoManager through mNodeInfo
-  nsNodeInfoManager* mNodeInfoManager; // [STRONG]
+  // This is a weak reference, but we hold a strong reference to mNodeInfo,
+  // which in turn holds a strong reference to this mNodeInfoManager.
+  nsNodeInfoManager* mNodeInfoManager;
   nsRefPtr<mozilla::css::Loader> mCSSLoader;
   nsRefPtr<mozilla::css::ImageLoader> mStyleImageLoader;
   nsRefPtr<nsHTMLStyleSheet> mAttrStyleSheet;
