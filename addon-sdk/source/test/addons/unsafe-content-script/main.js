55832b686cbeae3dd57eddd724e57bf14b2f954c
Gabor Krizsanits <gkrizsanits@mozilla.com>
2014-04-10 19:52:54 +0200

    Bug 821809 - Part3: nsEp for content-script. r=rFobic a=abillings

---
+++ addon-sdk/source/test/addons/unsafe-content-script/main.js
@@ -0,0 +1,61 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const { create: makeFrame } = require("sdk/frame/utils");
+const { window } = require("sdk/addon/window");
+const { Loader } = require('sdk/test/loader');
+const loader = Loader(module);
+const Worker = loader.require("sdk/content/worker").Worker;
+
+exports.testMembranelessMode = function(assert, done) {
+
+  let url = "data:text/html;charset=utf-8," + encodeURIComponent(
+    '<script>' +
+    'function runTest() {' +
+    '  assert(fuu.bar == 42, "Content-script objects should be accessible to content with' +
+    '         the unsafe-content-script flag on.");' +
+    '}' +
+    '</script>'
+  );
+
+  let element = makeFrame(window.document, {
+    nodeName: "iframe",
+    type: "content",
+    allowJavascript: true,
+    allowPlugins: true,
+    allowAuth: true,
+    uri: url
+  });
+
+  element.addEventListener("DOMContentLoaded", onDOMReady, false);
+
+  function onDOMReady() {
+    let worker = Worker({
+      window: element.contentWindow,
+      contentScript:
+        'new ' + function () {
+          var assert = function assert(v, msg) {
+            self.port.emit("assert", { assertion: v, msg: msg });
+          }
+          var done = function done() {
+            self.port.emit("done");
+          }
+          window.wrappedJSObject.fuu = { bar: 42 };
+          window.wrappedJSObject.assert = assert;
+          window.wrappedJSObject.runTest();
+          done();
+        }
+    });
+    worker.port.on("done", function () {
+      element.parentNode.removeChild(element);
+      done();
+    });
+    worker.port.on("assert", function (data) {
+      assert.ok(data.assertion, data.msg);
+    });
+  }
+};
+
+require("sdk/test/runner").runTestsFromModule(module);
