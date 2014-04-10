0737bf6e03a9e3b6e53da171a1ed841e6509329c
Richard Newman <rnewman@mozilla.com>
2014-04-10 10:09:34 -0700

    Bug 970176 - Part 0: remove AppConstants dependency on .App by moving things to TestConstants. r=nalexander

--- mobile/android/tests/browser/junit3/src/tests/BrowserTestCase.java
+++ mobile/android/tests/browser/junit3/src/tests/BrowserTestCase.java
@@ -12,13 +12,34 @@ import android.test.ActivityInstrumentationTestCase2;
 /**
  * BrowserTestCase provides helper methods for testing.
  */
+@SuppressWarnings("unchecked")
 public class BrowserTestCase extends ActivityInstrumentationTestCase2<Activity> {
     @SuppressWarnings("unused")
     private static String LOG_TAG = "BrowserTestCase";
 
-    @SuppressWarnings("unchecked")
+    /**
+     * The Java Class instance that launches the browser.
+     * <p>
+     * This should always agree with {@link AppConstants#BROWSER_INTENT_CLASS_NAME}.
+     */
+    public static final Class<? extends Activity> BROWSER_INTENT_CLASS;
+
+    // Use reflection here so we don't have to either (a) preprocess this
+    // file, or (b) get access to Robocop's TestConstants class from these
+    // instrumentation tests.
+    static {
+        Class<? extends Activity> cl;
+        try {
+            cl = (Class<? extends Activity>) Class.forName(AppConstants.BROWSER_INTENT_CLASS_NAME);
+        } catch (ClassNotFoundException e) {
+            // Oh well.
+            cl = Activity.class;
+        }
+        BROWSER_INTENT_CLASS = cl;
+    }
+
     public BrowserTestCase() {
-        super((Class<Activity>) AppConstants.BROWSER_INTENT_CLASS);
+        super((Class<Activity>) BROWSER_INTENT_CLASS);
     }
 
     public Context getApplicationContext() {
