7c91eb350517e1e34f67f7f3fed0852a1db8939f
Till Schneidereit <till@tillschneidereit.net>
2014-04-11 17:07:27 +0200

    Bug 911147 - Implement Array.prototype.fill. r=jorendorff

--- js/src/jsarray.cpp
+++ js/src/jsarray.cpp
@@ -2983,6 +2983,8 @@ static const JSFunctionSpec array_methods[] = {
     JS_SELF_HOSTED_FN("find",        "ArrayFind",        1,0),
     JS_SELF_HOSTED_FN("findIndex",   "ArrayFindIndex",   1,0),
 
+    JS_SELF_HOSTED_FN("fill",        "ArrayFill",        3,0),
+
     JS_SELF_HOSTED_FN("@@iterator",  "ArrayValues",      0,0),
     JS_SELF_HOSTED_FN("entries",     "ArrayEntries",     0,0),
     JS_SELF_HOSTED_FN("keys",        "ArrayKeys",        0,0),
