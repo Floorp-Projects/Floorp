e42151f8879aa2fd1715acc762f08210c360e7ed
Tim Taubert <tim@timtaubert.de>
2014-04-11 09:12:47 +0200

    No Bug - Fix browser_net_timing-division.js leak when run as the last test on a CLOSED TREE rs=past

--- browser/devtools/netmonitor/test/head.js
+++ browser/devtools/netmonitor/test/head.js
@@ -137,7 +137,7 @@ function teardown(aMonitor) {
   let deferred = promise.defer();
   let tab = aMonitor.target.tab;
 
-  aMonitor.once("destroyed", deferred.resolve);
+  aMonitor.once("destroyed", () => executeSoon(deferred.resolve));
   removeTab(tab);
 
   return deferred.promise;
