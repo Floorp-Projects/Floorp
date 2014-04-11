d6a4aef8a66e14a09d550c79ae509694b396385d
Vicamo Yang <vyang@mozilla.com>
2014-04-11 22:28:07 +0800

    Bug 995107: fix RIL DSDS marionette test cases on emulator-jb,kk. r=echen

--- dom/icc/tests/marionette/test_icc_detected_undetected_event.js
+++ dom/icc/tests/marionette/test_icc_detected_undetected_event.js
@@ -4,6 +4,8 @@
 MARIONETTE_TIMEOUT = 30000;
 MARIONETTE_HEAD_JS = "icc_header.js";
 
+let origNumICCs = iccManager.iccIds.length;
+
 function setRadioEnabled(enabled) {
   let connection = navigator.mozMobileConnections[0];
   ok(connection);
@@ -26,12 +28,8 @@ taskHelper.push(function testIccUndetectedEvent() {
     log("got icc undetected event");
     iccManager.removeEventListener("iccundetected", oniccundetected);
 
-    // TODO: Bug 932650 - B2G RIL: WebIccManager API - add marionette tests for
-    //                    multi-sim
-    // In single sim scenario, there is only one sim card, we can use below way
-    // to check iccIds.
     is(evt.iccId, iccId, "icc " + evt.iccId + " becomes undetected");
-    is(iccManager.iccIds.length, 0,
+    is(iccManager.iccIds.length, origNumICCs - 1,
        "iccIds.length becomes to " + iccManager.iccIds.length);
     is(iccManager.getIccById(evt.iccId), null,
        "should not get a valid icc object here");
@@ -47,12 +45,8 @@ taskHelper.push(function testIccDetectedEvent() {
     log("got icc detected event");
     iccManager.removeEventListener("iccdetected", oniccdetected);
 
-    // TODO: Bug 932650 - B2G RIL: WebIccManager API - add marionette tests for
-    //                    multi-sim
-    // In single sim scenario, there is only one sim card, we can use below way
-    // to check iccIds.
     is(evt.iccId, iccId, "icc " + evt.iccId + " is detected");
-    is(iccManager.iccIds.length, 1,
+    is(iccManager.iccIds.length, origNumICCs,
        "iccIds.length becomes to " + iccManager.iccIds.length);
     ok(iccManager.getIccById(evt.iccId) instanceof MozIcc,
        "should get a valid icc object here");
