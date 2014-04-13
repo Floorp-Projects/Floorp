aefae4320035179b578ec617d5ef96dec430b93b
Ryan VanderMeulen <ryanvm@gmail.com>
2014-04-13 16:20:33 -0400

    Backed out changeset ab4c12c6028b (bug 988155) for xpcshell failures.

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1282,12 +1282,10 @@ pref("devtools.eyedropper.zoom", 6);
 // - expandtab: expand Tab characters to spaces.
 // - keymap: which keymap to use (can be 'default', 'emacs' or 'vim')
 // - autoclosebrackets: whether to permit automatic bracket/quote closing.
-// - detectindentation: whether to detect the indentation from the file
 pref("devtools.editor.tabsize", 4);
 pref("devtools.editor.expandtab", true);
 pref("devtools.editor.keymap", "default");
 pref("devtools.editor.autoclosebrackets", true);
-pref("devtools.editor.detectindentation", true);
 
 // Enable the Font Inspector
 pref("devtools.fontinspector.enabled", true);
