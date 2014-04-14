cffac7528ef218106346b3db9ac23e8a785be161
Eric Faust <efaustbmo@gmail.com>
2014-04-14 15:52:26 -0700

    Bug 978235 - ES6 Proxies: Implement [[IsExtensible]] trap. (r=jorendorff, terrence over IRC)

--- js/src/jit/BaselineJIT.h
+++ js/src/jit/BaselineJIT.h
@@ -312,7 +312,7 @@ struct BaselineScript
 inline bool
 IsBaselineEnabled(JSContext *cx)
 {
-    return cx->runtime()->options().baseline();
+    return false && cx->runtime()->options().baseline();
 }
 
 MethodStatus
