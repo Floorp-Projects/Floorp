a6d5cbb4bf96c7bc1f68463cbec134562d55e381
Heather Arthur <fayearthur@gmail.com>
2014-04-14 19:25:00 +0200

    Bug 993162 - Make default indent size 2 spaces. r=jwalker

--- browser/devtools/sourceeditor/test/browser_detectindent.js
+++ browser/devtools/sourceeditor/test/browser_detectindent.js
@@ -62,16 +62,10 @@ function test() {
   waitForExplicitFinish();
 
   setup((ed, win) => {
-    is(ed.getOption("indentUnit"), 4,
-       "4 spaces before code added");
+    is(ed.getOption("indentUnit"), 2,
+       "2 spaces before code added");
     is(ed.getOption("indentWithTabs"), false,
        "spaces is default");
-
-    ed.setText(TWO_SPACES_CODE);
-    is(ed.getOption("indentUnit"), 2,
-       "2 spaces detected in 2 space code");
-    is(ed.getOption("indentWithTabs"), false,
-       "spaces detected in 2 space code");
 
     ed.setText(FOUR_SPACES_CODE);
     is(ed.getOption("indentUnit"), 4,
@@ -79,9 +73,15 @@ function test() {
     is(ed.getOption("indentWithTabs"), false,
        "spaces detected in 4 space code");
 
+    ed.setText(TWO_SPACES_CODE);
+    is(ed.getOption("indentUnit"), 2,
+       "2 spaces detected in 2 space code");
+    is(ed.getOption("indentWithTabs"), false,
+       "spaces detected in 2 space code");
+
     ed.setText(TABS_CODE);
-    is(ed.getOption("indentUnit"), 4,
-       "4 space indentation unit");
+    is(ed.getOption("indentUnit"), 2,
+       "2 space indentation unit");
     is(ed.getOption("indentWithTabs"), true,
        "tabs detected in majority tabs code");
 
