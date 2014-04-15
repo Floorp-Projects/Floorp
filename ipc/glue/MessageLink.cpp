8bfa98d29c8b9060a771aaef68f147db5a762f35
Cervantes Yu <cyu@mozilla.com>
2014-04-15 21:26:30 +0800

    Bug 977359 - Part 2: Test case for the deadlock in the Nuwa process. r=gal, a=1.3+

--- ipc/glue/MessageLink.cpp
+++ ipc/glue/MessageLink.cpp
@@ -10,6 +10,11 @@
 #include "mozilla/ipc/BrowserProcessSubThread.h"
 #include "mozilla/ipc/ProtocolUtils.h"
 
+#ifdef MOZ_NUWA_PROCESS
+#include "ipc/Nuwa.h"
+#include "mozilla/Preferences.h"
+#endif
+
 #include "nsDebug.h"
 #include "nsISupportsImpl.h"
 #include "nsXULAppAPI.h"
@@ -123,6 +128,17 @@ ProcessLink::Open(mozilla::ipc::Transport* aTransport, MessageLoop *aIOLoop, Sid
                 FROM_HERE,
                 NewRunnableMethod(this, &ProcessLink::OnTakeConnectedChannel));
         }
+
+#ifdef MOZ_NUWA_PROCESS
+        if (IsNuwaProcess() &&
+            Preferences::GetBool("dom.ipc.processPrelaunch.testMode")) {
+            // The pref value is turned on in a deadlock test against the Nuwa
+            // process. The sleep here makes it easy to trigger the deadlock
+            // that an IPC channel is still opening but the worker loop is
+            // already frozen.
+            sleep(5);
+        }
+#endif
 
         // Should not wait here if something goes wrong with the channel.
         while (!mChan->Connected() && mChan->mChannelState != ChannelError) {
