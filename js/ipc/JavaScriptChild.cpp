a21c0e0c8a7c401af58d49c49fb1e2668fc82634
Eric Faust <efaustbmo@gmail.com>
2014-04-15 12:56:51 -0700

    Backed out changeset f1660c847ca2 (bug 978235) for landing with random debug code.

--- js/ipc/JavaScriptChild.cpp
+++ js/ipc/JavaScriptChild.cpp
@@ -439,8 +439,6 @@ JavaScriptChild::AnswerIsExtensible(const ObjectId &objId, ReturnStatus *rs, boo
     if (!obj)
         return false;
 
-    JSAutoCompartment comp(cx, obj);
-
     bool extensible;
     if (!JS_IsExtensible(cx, obj, &extensible))
         return fail(cx, rs);
