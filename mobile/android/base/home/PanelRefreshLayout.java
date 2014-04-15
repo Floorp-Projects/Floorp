ff9098c44bee138e01fc522caaa75949c0e64b5f
Josh Dover <gerfuls@gmail.com>
2014-04-15 15:51:00 +0200

    Bug 970707 - Pass refresh to JS callback. r=lucas

--- mobile/android/base/home/PanelRefreshLayout.java
+++ mobile/android/base/home/PanelRefreshLayout.java
@@ -7,13 +7,19 @@ package org.mozilla.gecko.home;
 
 import org.mozilla.gecko.R;
 
+import org.mozilla.gecko.GeckoAppShell;
+import org.mozilla.gecko.GeckoEvent;
 import org.mozilla.gecko.home.PanelLayout.DatasetBacked;
 import org.mozilla.gecko.home.PanelLayout.FilterManager;
 import org.mozilla.gecko.widget.GeckoSwipeRefreshLayout;
 import org.mozilla.gecko.widget.GeckoSwipeRefreshLayout.OnRefreshListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -24,19 +30,30 @@ import android.view.View;
  * while providing the refresh gesture support on top of it.
  */
 class PanelRefreshLayout extends GeckoSwipeRefreshLayout implements DatasetBacked {
+    private static final String LOGTAG = "GeckoPanelRefreshLayout";
+
+    private static final String JSON_KEY_PANEL_ID = "panelId";
+    private static final String JSON_KEY_VIEW_INDEX = "viewIndex";
+
+    private final String panelId;
+    private final int viewIndex;
     private final DatasetBacked datasetBacked;
 
     /**
      * @param context Android context.
      * @param childView ListView or GridView. Must implement {@code DatasetBacked}.
+     * @param panelId The ID from the {@code PanelConfig}.
+     * @param viewIndex The index from the {@code ViewConfig}.
      */
-    public PanelRefreshLayout(Context context, View childView) {
+    public PanelRefreshLayout(Context context, View childView, String panelId, int viewIndex) {
         super(context);
 
         if (!(childView instanceof DatasetBacked)) {
             throw new IllegalArgumentException("View must implement DatasetBacked to be refreshed");
         }
 
+        this.panelId = panelId;
+        this.viewIndex = viewIndex;
         this.datasetBacked = (DatasetBacked) childView;
 
         setOnRefreshListener(new RefreshListener());
@@ -50,6 +67,7 @@ class PanelRefreshLayout extends GeckoSwipeRefreshLayout implements DatasetBacke
     @Override
     public void setDataset(Cursor cursor) {
         datasetBacked.setDataset(cursor);
+        setRefreshing(false);
     }
 
     @Override
@@ -60,7 +78,18 @@ class PanelRefreshLayout extends GeckoSwipeRefreshLayout implements DatasetBacke
     private class RefreshListener implements OnRefreshListener {
         @Override
         public void onRefresh() {
-            setRefreshing(false);
+            final JSONObject response = new JSONObject();
+            try {
+                response.put(JSON_KEY_PANEL_ID, panelId);
+                response.put(JSON_KEY_VIEW_INDEX, viewIndex);
+            } catch (JSONException e) {
+                Log.e(LOGTAG, "Could not create refresh message", e);
+                return;
+            }
+
+            final GeckoEvent event =
+                GeckoEvent.createBroadcastEvent("HomePanels:RefreshView", response.toString());
+            GeckoAppShell.sendEventToGecko(event);
         }
     }
 }
