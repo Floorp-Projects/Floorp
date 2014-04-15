77f19837e2463a8f2c0c2cdc19c049cc94c19718
Dan Glastonbury <dglastonbury@mozilla.com>
2014-04-15 14:48:52 +1000

    Bug 966630 - Clamp level to TexImage operations to [0..31]. r=jgilbert, a=abillings

--- content/canvas/src/WebGLContextValidate.cpp
+++ content/canvas/src/WebGLContextValidate.cpp
@@ -929,6 +929,19 @@ WebGLContext::ValidateTexImageSize(GLenum target, GLint level,
 {
     MOZ_ASSERT(level >= 0, "level should already be validated");
 
+    /* Bug 966630: maxTextureSize >> level runs into "undefined"
+     * behaviour depending on ISA. For example, on Intel shifts
+     * amounts are mod 64 (in 64-bit mode on 64-bit dest) and mod 32
+     * otherwise. This means 16384 >> 0x10000001 == 8192 which isn't
+     * what would be expected. Make the required behaviour explicit by
+     * clamping to a shift of 31 bits if level is greater than that
+     * ammount. This will give 0 that if (!maxAllowedSize) is
+     * expecting.
+     */
+
+    if (level > 31)
+        level = 31;
+
     const GLuint maxTexImageSize = MaxTextureSizeForTarget(target) >> level;
     const bool isCubemapTarget = IsTexImageCubemapTarget(target);
 
