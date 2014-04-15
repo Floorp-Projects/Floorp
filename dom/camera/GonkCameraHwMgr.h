0ec7f4edeac6e9277042e4cc2d3d9d02be6e35e0
Mike Habicher <mikeh@mozilla.com>
2014-04-15 15:46:40 -0400

    Bug 986024 - expose resumeContinuousFocus() method to DOM/JS, r=bz,dhylands

--- dom/camera/GonkCameraHwMgr.h
+++ dom/camera/GonkCameraHwMgr.h
@@ -78,7 +78,7 @@ public:
   virtual int      GetSensorOrientation(uint32_t aType = RAW_SENSOR_ORIENTATION);
 
   virtual int      AutoFocus();
-  virtual void     CancelAutoFocus();
+  virtual int      CancelAutoFocus();
   virtual int      StartFaceDetection();
   virtual int      StopFaceDetection();
   virtual int      TakePicture();
