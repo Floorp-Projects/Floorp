e5d0c709df055dd3046223035d86c6f80915e3f6
Henri Sivonen <hsivonen@hsivonen.fi>
2014-04-15 15:58:01 +0300

    Bug 937056 - Remove UTF-7 code from mozilla-central. r=emk.

--- intl/uconv/ucvlatin/nsUCvLatinCID.h
+++ intl/uconv/ucvlatin/nsUCvLatinCID.h
@@ -189,16 +189,6 @@
 #define NS_VPSTOUNICODE_CID \
   { 0x6394eeb0, 0xfc3d, 0x11d2, {0xb3, 0xb8, 0x0, 0x80, 0x5f, 0x8a, 0x66, 0x70}}
 
-// Class ID for our UTF7ToUnicode charset converter
-// {77CFAAF1-1CF4-11d3-8AAF-00600811A836}
-#define NS_UTF7TOUNICODE_CID \
-  { 0x77cfaaf1, 0x1cf4, 0x11d3, {0x8a, 0xaf, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36}}
-
-// Class ID for our MUTF7ToUnicode charset converter
-// {B57F97C1-0D70-11d3-8AAE-00600811A836}
-#define NS_MUTF7TOUNICODE_CID \
-  { 0xb57f97c1, 0xd70, 0x11d3, {0x8a, 0xae, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36}}
-
 // Class ID for our UnicodeToISO88592 charset converter
 // {7B8556A6-EC79-11d2-8AAC-00600811A836}
 #define NS_UNICODETOISO88592_CID \
@@ -379,16 +369,6 @@
 // {6394EEC0-FC3D-11d2-B3B8-00805F8A6670}
 #define NS_UNICODETOVPS_CID \
   { 0x6394eec0, 0xfc3d, 0x11d2, {0xb3, 0xb8, 0x0, 0x80, 0x5f, 0x8a, 0x66, 0x70}}
-
-// Class ID for our UnicodeToUTF7 charset converter
-// {77CFAAF2-1CF4-11d3-8AAF-00600811A836}
-#define NS_UNICODETOUTF7_CID \
-  { 0x77cfaaf2, 0x1cf4, 0x11d3, {0x8a, 0xaf, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36}}
-
-// Class ID for our UnicodeToMUTF7 charset converter
-// {B57F97C2-0D70-11d3-8AAE-00600811A836}
-#define NS_UNICODETOMUTF7_CID \
-  { 0xb57f97c2, 0xd70, 0x11d3, {0x8a, 0xae, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36}}
 
 // Class ID for our CP1255ToUnicode charset converter
 // {BA6151A1-1DFA-11d3-B3BF-00805F8A6670}
