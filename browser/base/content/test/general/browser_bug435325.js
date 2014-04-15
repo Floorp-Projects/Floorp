965033416b28a4fabfe178654fa007e9e450bb38
Nathan Froyd <froydnj@mozilla.com>
2014-04-15 16:14:10 -0400

    Bug 996019 - Fix browser_bug435325.js to not connect to example.com. r=ehsan, a=test-only

--- browser/base/content/test/general/browser_bug435325.js
+++ browser/base/content/test/general/browser_bug435325.js
@@ -51,6 +51,11 @@ function checkPage() {
   // Now press the "Try Again" button
   ok(gBrowser.contentDocument.getElementById("errorTryAgain"),
     "The error page has got a #errorTryAgain element");
+
+  // Re-enable the proxy so example.com is resolved to localhost, rather than
+  // the actual example.com.
+  Services.prefs.setIntPref("network.proxy.type", proxyPrefValue);
+
   gBrowser.contentDocument.getElementById("errorTryAgain").click();
 
   ok(!Services.io.offline, "After clicking the Try Again button, we're back " +
@@ -60,7 +65,6 @@ function checkPage() {
 }
 
 registerCleanupFunction(function() {
-  Services.prefs.setIntPref("network.proxy.type", proxyPrefValue);
   Services.prefs.setBoolPref("browser.cache.disk.enable", true);
   Services.prefs.setBoolPref("browser.cache.memory.enable", true);
   Services.io.offline = false;
