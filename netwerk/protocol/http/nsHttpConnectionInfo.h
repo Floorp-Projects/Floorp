7df71d973edd096df8c78ce0aee6d8c0bb399df3
Patrick McManus <mcmanus@ducksong.com>
2014-04-16 09:23:20 -0400

    bug 378637 part 9 - https proxy info added to connection info and reqeuest head r=hurley
    --HG--
    extra : rebase_source : 30bfa9973c18e2a53ece4ea5056b38c151f49cf0

--- netwerk/protocol/http/nsHttpConnectionInfo.h
+++ netwerk/protocol/http/nsHttpConnectionInfo.h
@@ -26,7 +26,7 @@ public:
     nsHttpConnectionInfo(const nsACString &host, int32_t port,
                          const nsACString &username,
                          nsProxyInfo* proxyInfo,
-                         bool usingSSL=false);
+                         bool endToEndSSL = false);
 
     virtual ~nsHttpConnectionInfo()
     {
@@ -65,10 +65,7 @@ public:
     int32_t       Port() const           { return mPort; }
     const char   *Username() const       { return mUsername.get(); }
     nsProxyInfo  *ProxyInfo()            { return mProxyInfo; }
-    bool          UsingHttpProxy() const { return mUsingHttpProxy; }
-    bool          UsingSSL() const       { return mUsingSSL; }
-    bool          UsingConnect() const   { return mUsingConnect; }
-    int32_t       DefaultPort() const    { return mUsingSSL ? NS_HTTPS_DEFAULT_PORT : NS_HTTP_DEFAULT_PORT; }
+    int32_t       DefaultPort() const    { return mEndToEndSSL ? NS_HTTPS_DEFAULT_PORT : NS_HTTP_DEFAULT_PORT; }
     void          SetAnonymous(bool anon)
                                          { mHashKey.SetCharAt(anon ? 'A' : '.', 2); }
     bool          GetAnonymous() const   { return mHashKey.CharAt(2) == 'A'; }
@@ -77,8 +74,23 @@ public:
 
     const nsCString &GetHost() { return mHost; }
 
-    // Returns true for any kind of proxy (http, socks, etc..)
+    // Returns true for any kind of proxy (http, socks, https, etc..)
     bool UsingProxy();
+
+    // Returns true when proxying over HTTP or HTTPS
+    bool UsingHttpProxy() const { return mUsingHttpProxy || mUsingHttpsProxy; }
+
+    // Returns true when proxying over HTTPS
+    bool UsingHttpsProxy() const { return mUsingHttpsProxy; }
+
+    // Returns true when a resource is in SSL end to end (e.g. https:// uri)
+    bool EndToEndSSL() const { return mEndToEndSSL; }
+
+    // Returns true when at least first hop is SSL (e.g. proxy over https or https uri)
+    bool FirstHopSSL() const { return mEndToEndSSL || mUsingHttpsProxy; }
+
+    // Returns true when CONNECT is used to tunnel through the proxy (e.g. https:// or ws://)
+    bool UsingConnect() const { return mUsingConnect; }
 
     // Returns true when mHost is an RFC1918 literal.
     bool HostIsLocalIPLiteral() const;
@@ -90,7 +102,8 @@ private:
     nsCString              mUsername;
     nsCOMPtr<nsProxyInfo>  mProxyInfo;
     bool                   mUsingHttpProxy;
-    bool                   mUsingSSL;
+    bool                   mUsingHttpsProxy;
+    bool                   mEndToEndSSL;
     bool                   mUsingConnect;  // if will use CONNECT with http proxy
 
 // for nsRefPtr
