de3c05bf317e8a95cf8d378540b986e682b30f14
Jonathan Watt <jwatt@jwatt.org>
2014-04-16 17:17:22 +0100

    Bug 996673 - Stop using |#pragma once| in Moz2D headers. r=Bas

--- gfx/2d/DrawTargetSkia.h
+++ gfx/2d/DrawTargetSkia.h
@@ -3,7 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#pragma once
+#ifndef _MOZILLA_GFX_SOURCESURFACESKIA_H
+#define _MOZILLA_GFX_SOURCESURFACESKIA_H
 
 #ifdef USE_SKIA_GPU
 #include "skia/GrContext.h"
@@ -137,3 +138,5 @@ private:
 
 }
 }
+
+#endif // _MOZILLA_GFX_SOURCESURFACESKIA_H
