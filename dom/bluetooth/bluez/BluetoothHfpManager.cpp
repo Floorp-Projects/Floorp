d253db2dbaf0434a0a2071164b5139ff88bbce3e
Shawn Huang <shuang@mozilla.com>
2014-04-17 14:26:35 +0800

    Bug 994411 - [bluez] Ignore pending MO call index, while making MO call with bluetooth headset, r=echou

--- dom/bluetooth/bluez/BluetoothHfpManager.cpp
+++ dom/bluetooth/bluez/BluetoothHfpManager.cpp
@@ -1392,6 +1392,13 @@ BluetoothHfpManager::HandleCallStateChanged(uint32_t aCallIndex,
     return;
   }
 
+  // aCallIndex can be UINT32_MAX for the pending outgoing call state update.
+  // aCallIndex will be updated again after real call state changes. See Bug
+  // 990467.
+  if (aCallIndex == UINT32_MAX) {
+    return;
+  }
+
   while (aCallIndex >= mCurrentCallArray.Length()) {
     Call call;
     mCurrentCallArray.AppendElement(call);
