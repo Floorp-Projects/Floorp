4ed13e4ab7b9ae33e9f31f9154b916c5813f0ccd
Paul Adenot <paul@paul.cx>
2014-04-17 15:42:00 +0200

    Bug 996770 - Add logging to cubeb's test_latency. r=kinetik

--- media/libcubeb/tests/test_latency.cpp
+++ media/libcubeb/tests/test_latency.cpp
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <cubeb/cubeb.h>
 #include <assert.h>
+#include <stdio.h>
+
+#define LOG(msg) fprintf(stderr, "%s\n", msg);
 
 int main(int argc, char * argv[])
 {
@@ -13,16 +16,20 @@ int main(int argc, char * argv[])
   uint32_t preferred_rate;
   uint32_t latency_ms;
 
+  LOG("latency_test start");
   rv = cubeb_init(&ctx, "Cubeb audio test");
   assert(rv == CUBEB_OK && "Cubeb init failed.");
+  LOG("cubeb_init ok");
 
   rv = cubeb_get_max_channel_count(ctx, &max_channels);
   assert(rv == CUBEB_OK && "Could not query the max channe count.");
   assert(max_channels > 0 && "Invalid max channel count.");
+  LOG("cubeb_get_max_channel_count ok");
 
   rv = cubeb_get_preferred_sample_rate(ctx, &preferred_rate);
   assert(rv == CUBEB_OK && "Could not query the preferred sample rate.");
   assert(preferred_rate && "Invalid preferred sample rate.");
+  LOG("cubeb_get_preferred_sample_rate ok");
 
   cubeb_stream_params params = {
     CUBEB_SAMPLE_FLOAT32NE,
@@ -32,8 +39,10 @@ int main(int argc, char * argv[])
   rv = cubeb_get_min_latency(ctx, params, &latency_ms);
   assert(rv == CUBEB_OK && "Could not query the minimal latency.");
   assert(latency_ms && "Invalid minimal latency.");
+  LOG("cubeb_get_min_latency ok");
 
   cubeb_destroy(ctx);
+  LOG("cubeb_destroy ok");
 
   return EXIT_SUCCESS;
 }
