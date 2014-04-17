ba889c98facd21f88e4e8872bcc77faad6bfce1e
Mats Palmgren <matspal@gmail.com>
2014-04-17 12:11:07 +0000

    Bug 613659 - Add "skip sides" and border/frame size parameters to GetBorderRadii().  Add an inline convenience method for the old signature.  r=cam

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -1277,7 +1277,8 @@ nsIFrame::OutsetBorderRadii(nscoord aRadii[8], const nsMargin &aOffsets)
 }
 
 /* virtual */ bool
-nsIFrame::GetBorderRadii(nscoord aRadii[8]) const
+nsIFrame::GetBorderRadii(const nsSize& aFrameSize, const nsSize& aBorderArea,
+                         int aSkipSides, nscoord aRadii[8]) const
 {
   if (IsThemed()) {
     // When we're themed, the native theme code draws the border and
@@ -1292,9 +1293,9 @@ nsIFrame::GetBorderRadii(nscoord aRadii[8]) const
     }
     return false;
   }
-  nsSize size = GetSize();
-  return ComputeBorderRadii(StyleBorder()->mBorderRadius, size, size,
-                            GetSkipSides(), aRadii);
+  return ComputeBorderRadii(StyleBorder()->mBorderRadius,
+                            aFrameSize, aBorderArea,
+                            aSkipSides, aRadii);
 }
 
 bool
