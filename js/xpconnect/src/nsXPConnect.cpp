3b3a7fe701b51b100a6adfbf282af8d6e87f477e
Bobby Holley <bobbyholley@gmail.com>
2014-04-20 11:48:13 -0700

    Bug 993772 - Introduce a singleton compilation scope. r=mrbkap

--- js/xpconnect/src/nsXPConnect.cpp
+++ js/xpconnect/src/nsXPConnect.cpp
@@ -88,7 +88,7 @@ nsXPConnect::nsXPConnect()
 
 nsXPConnect::~nsXPConnect()
 {
-    mRuntime->DeleteJunkScope();
+    mRuntime->DeleteSingletonScopes();
     mRuntime->DestroyJSContextStack();
 
     // In order to clean up everything properly, we need to GC twice: once now,
