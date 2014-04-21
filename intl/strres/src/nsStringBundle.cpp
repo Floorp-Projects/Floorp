e88ad9a1f2142c300cc373dbd0cc845d0fdc6710
Andrew McCreight <continuation@gmail.com>
2014-04-21 09:41:57 -0700

    Bug 997963, part 6 - Use a refptr in nsStringBundleService::getStringBundle. r=ehsan

--- intl/strres/src/nsStringBundle.cpp
+++ intl/strres/src/nsStringBundle.cpp
@@ -589,12 +589,8 @@ nsStringBundleService::getStringBundle(const char *aURLSpec,
   } else {
 
     // hasn't been cached, so insert it into the hash table
-    nsStringBundle* bundle = new nsStringBundle(aURLSpec, mOverrideStrings);
-    NS_ADDREF(bundle);
-
-    cacheEntry = insertIntoCache(bundle, &completeKey);
-    NS_RELEASE(bundle);         // cache should now be holding a ref
-                                // in the cacheEntry
+    nsRefPtr<nsStringBundle> bundle = new nsStringBundle(aURLSpec, mOverrideStrings);
+    cacheEntry = insertIntoCache(bundle.forget(), &completeKey);
   }
 
   // at this point the cacheEntry should exist in the hashtable,
@@ -610,7 +606,7 @@ nsStringBundleService::getStringBundle(const char *aURLSpec,
 }
 
 bundleCacheEntry_t *
-nsStringBundleService::insertIntoCache(nsIStringBundle* aBundle,
+nsStringBundleService::insertIntoCache(already_AddRefed<nsIStringBundle> aBundle,
                                        nsCStringKey* aHashKey)
 {
   bundleCacheEntry_t *cacheEntry;
