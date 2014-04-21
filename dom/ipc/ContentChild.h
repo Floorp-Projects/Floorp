80894662e6798fbb8607de70d8eb5d5f08e14132
Reuben Morais <reuben.morais@gmail.com>
2014-04-21 14:18:53 -0300

    Bug 992313 - Keep track of registered idle observers so we don't call into a freed pointer. r=roc, a=1.4+

--- dom/ipc/ContentChild.h
+++ dom/ipc/ContentChild.h
@@ -10,7 +10,12 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/dom/PContentChild.h"
 #include "mozilla/dom/ipc/Blob.h"
+#include "nsHashKeys.h"
+#include "nsIObserver.h"
+#include "nsTHashtable.h"
+
 #include "nsWeakPtr.h"
+
 
 struct ChromePackage;
 class nsIDOMBlob;
@@ -289,6 +294,8 @@ private:
 
     InfallibleTArray<nsAutoPtr<AlertObserver> > mAlertObservers;
     nsRefPtr<ConsoleListener> mConsoleListener;
+
+    nsTHashtable<nsPtrHashKey<nsIObserver>> mIdleObservers;
 
     /**
      * An ID unique to the process containing our corresponding
