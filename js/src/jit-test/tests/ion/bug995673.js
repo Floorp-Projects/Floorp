50110a30d55a75d9c996e5b2f2861d271174a6a0
Hannes Verschore <hv1989@gmail.com>
2014-04-22 16:46:18 +0200

    Bug 995673 - IonMonkey: Don't remove MToXXX instructions, when an object flows through, r=jandem

---
+++ js/src/jit-test/tests/ion/bug995673.js
@@ -0,0 +1,16 @@
+var total = 0
+
+x = [ [] ]
+x[0].valueOf = function () {
+  total++;
+}
+function f(y) {
+    y != Math.abs()
+}
+(function() {
+    f()
+    f(x[0])
+    f(x[0])
+})()
+
+assertEq(total, 2)
