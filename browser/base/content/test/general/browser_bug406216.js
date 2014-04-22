1224d5458bdbe0e34a542191957ed14e5be377c7
Nathan Froyd <froydnj@mozilla.com>
2014-04-22 14:19:35 -0400

    Bug 999618 - fix browser-chrome tests in browser/base/content/test/general to not remove the original tab; r=ehsan

--- browser/base/content/test/general/browser_bug406216.js
+++ browser/base/content/test/general/browser_bug406216.js
@@ -21,8 +21,6 @@ function test() {
 
 function addTab(aURI, aIndex) {
   var tab = gBrowser.addTab(aURI);
-  if (aIndex == 0)
-    gBrowser.removeTab(gBrowser.tabs[0]);
 
   tab.linkedBrowser.addEventListener("load", function (event) {
     event.currentTarget.removeEventListener("load", arguments.callee, true);
@@ -32,23 +30,22 @@ function addTab(aURI, aIndex) {
 }
 
 function doTabsTest() {
-  is(gBrowser.tabs.length, URIS.length, "Correctly opened all expected tabs");
+  is(gBrowser.tabs.length - 1, URIS.length, "Correctly opened all expected tabs");
 
   // sample of "close related tabs" feature
   gBrowser.tabContainer.addEventListener("TabClose", function (event) {
     event.currentTarget.removeEventListener("TabClose", arguments.callee, true);
     var closedTab = event.originalTarget;
     var scheme = closedTab.linkedBrowser.currentURI.scheme;
-    Array.slice(gBrowser.tabs).forEach(function (aTab) {
-      if (aTab != closedTab && aTab.linkedBrowser.currentURI.scheme == scheme)
+    Array.slice(gBrowser.tabs).forEach(function (aTab, aIndex) {
+      if (aIndex != 0 && aTab != closedTab && aTab.linkedBrowser.currentURI.scheme == scheme)
         gBrowser.removeTab(aTab);
     });
   }, true);
 
-  gBrowser.removeTab(gBrowser.tabs[0]);
-  is(gBrowser.tabs.length, 1, "Related tabs are not closed unexpectedly");
+  gBrowser.removeTab(gBrowser.tabs[1]);
+  is(gBrowser.tabs.length - 1, 1, "Related tabs are not closed unexpectedly");
 
-  gBrowser.addTab("about:blank");
-  gBrowser.removeTab(gBrowser.tabs[0]);
+  gBrowser.removeTab(gBrowser.tabs[1]);
   finish();
 }
