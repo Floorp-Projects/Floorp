dfce057489d76f2f318b6fec3a61195116786be9
Peter Chang <pchang@mozilla.com>
2014-04-22 19:07:00 +0200

    Bug 997367 - fix crash at CrossProcessCompositorParent::GetCompositionManager. r=roc

--- gfx/layers/ipc/CompositorParent.cpp
+++ gfx/layers/ipc/CompositorParent.cpp
@@ -979,17 +979,9 @@ CompositorParent::AllocateLayerTreeId()
 }
 
 static void
-EraseLayerState(uint64_t aId)
+RemoveIndirectTree(uint64_t aId)
 {
   sIndirectLayerTrees.erase(aId);
-}
-
-/*static*/ void
-CompositorParent::DeallocateLayerTreeId(uint64_t aId)
-{
-  MOZ_ASSERT(NS_IsMainThread());
-  CompositorLoop()->PostTask(FROM_HERE,
-                             NewRunnableFunction(&EraseLayerState, aId));
 }
 
 static void
@@ -1183,12 +1175,6 @@ CompositorParent::GetIndirectShadowTree(uint64_t aId)
     return nullptr;
   }
   return &cit->second;
-}
-
-static void
-RemoveIndirectTree(uint64_t aId)
-{
-  sIndirectLayerTrees.erase(aId);
 }
 
 void
