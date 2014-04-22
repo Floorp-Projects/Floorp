0c4ede6a3ebc387f4714d06a980c1a7da7201fdd
Nick Fitzgerald <fitzgen@mozilla.com>
2014-04-22 09:11:00 +0200

    Bug 984696 - Save more detailed source notes so that Debugger.Script.prototype.getAllColumnOffsets can offer more for source mapped and/or pretty printed JS debugging. r=ejpbruel

--- js/src/jit-test/tests/debug/Script-getAllColumnOffsets-02.js
+++ js/src/jit-test/tests/debug/Script-getAllColumnOffsets-02.js
@@ -1,4 +1,4 @@
-// getColumnOffsets correctly places the various parts of a ForStatement.
+// getColumnOffsets correctly places multiple variable declarations.
 
 var global = newGlobal();
 Debugger(global).onDebuggerStatement = function (frame) {
@@ -14,6 +14,8 @@ Debugger(global).onDebuggerStatement = function (frame) {
 };
 
 global.log = '';
-global.eval("function f(n) { for (var i = 0; i < n; ++i) log += '. '; log += '! '; } debugger;");
+global.eval("function f(n){var w0,x1=3,y2=4,z3=9} debugger;");
 global.f(3);
-assertEq(global.log, "25 32 44 . 39 32 44 . 39 32 44 . 39 32 57 ! 69 ");
+
+// Should have hit each variable declared.
+assertEq(global.log, "18 21 26 31 33 ");
