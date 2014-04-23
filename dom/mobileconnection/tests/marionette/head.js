69d9429fe402c5d49968848e7bcbf1a2cc16a807
Edgar Chen <echen@mozilla.com>
2014-04-23 15:07:17 +0800

    Bug 929701 - Part 4-1: Convert test_mobile_mmi.js to Promise. r=hsinyi

--- dom/mobileconnection/tests/marionette/head.js
+++ dom/mobileconnection/tests/marionette/head.js
@@ -388,6 +388,23 @@ function selectNetworkAutomaticallyAndWait() {
 }
 
 /**
+ * Send a MMI message.
+ *
+ * Fulfill params: An object contains MMI result.
+ * Reject params: A DOMMMIError.
+ *
+ * @param aMmi
+ *        A MMI string.
+ *
+ * @return A deferred promise.
+ */
+function sendMMI(aMmi) {
+  let request = mobileConnection.sendMMI(aMmi);
+  return wrapDomRequestAsPromise(request)
+    .then(() => request.result, () => { throw request.error });
+}
+
+/**
  * Set data connection enabling state and wait for "datachange" event.
  *
  * Resolve if data connection state changed to the expected one.  Never reject.
