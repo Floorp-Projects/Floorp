2ed4eecc7c1ebb0ff1a6f9e68ba89f49358aae9e
Brian Hackett <bhackett1024@gmail.com>
2014-04-23 14:12:31 -0700

    Bug 995982 - Set up forwarding pointers properly when allocating typed array objects in the nursery. r=sfink

--- js/src/vm/TypedArrayObject.h
+++ js/src/vm/TypedArrayObject.h
@@ -51,7 +51,11 @@ class TypedArrayObject : public ArrayBufferViewObject
     AllocKindForLazyBuffer(size_t nbytes)
     {
         JS_ASSERT(nbytes <= INLINE_BUFFER_LIMIT);
-        int dataSlots = (nbytes - 1) / sizeof(Value) + 1;
+
+        // Make sure there is space for at least one slot, for forwarding
+        // pointers during nursery collection.
+        int dataSlots = Max<int>(1, (int) (nbytes - 1) / sizeof(Value) + 1);
+
         JS_ASSERT(int(nbytes) <= dataSlots * int(sizeof(Value)));
         return gc::GetGCObjectKind(FIXED_DATA_START + dataSlots);
     }
