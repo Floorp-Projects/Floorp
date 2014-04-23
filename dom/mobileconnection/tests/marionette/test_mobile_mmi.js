ed10aa2e35601ea74f8e958d706c057fc249eefd
Edgar Chen <echen@mozilla.com>
2014-04-23 15:07:17 +0800

    Bug 929701 - Part 4-1: Convert test_mobile_mmi.js to Promise. r=hsinyi

--- dom/mobileconnection/tests/marionette/test_mobile_mmi.js
+++ dom/mobileconnection/tests/marionette/test_mobile_mmi.js
@@ -1,107 +1,46 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-MARIONETTE_TIMEOUT = 20000;
+MARIONETTE_TIMEOUT = 60000;
+MARIONETTE_HEAD_JS = "head.js";
 
-SpecialPowers.addPermission("mobileconnection", true, document);
-
-// Permission changes can't change existing Navigator.prototype
-// objects, so grab our objects from a new Navigator
-let ifr = document.createElement("iframe");
-let mobileConnection;
-ifr.onload = function() {
-  mobileConnection = ifr.contentWindow.navigator.mozMobileConnections[0];
-
-  tasks.run();
-};
-document.body.appendChild(ifr);
-
-let tasks = {
-  // List of test functions. Each of them should call |tasks.next()| when
-  // completed or |tasks.abort()| to jump to the last one.
-  _tasks: [],
-  _nextTaskIndex: 0,
-
-  push: function(func) {
-    this._tasks.push(func);
-  },
-
-  next: function() {
-    let index = this._nextTaskIndex++;
-    let task = this._tasks[index];
-    try {
-      task();
-    } catch (ex) {
-      ok(false, "test task[" + index + "] throws: " + ex);
-      // Run last task as clean up if possible.
-      if (index != this._tasks.length - 1) {
-        this.abort();
-      }
-    }
-  },
-
-  abort: function() {
-    this._tasks[this._tasks.length - 1]();
-  },
-
-  run: function() {
-    this.next();
-  }
-};
-
-tasks.push(function verifyInitialState() {
-  log("Verifying initial state.");
-
-  ok(mobileConnection instanceof ifr.contentWindow.MozMobileConnection,
-      "mobileConnection is instanceof " + mobileConnection.constructor);
-
-  tasks.next();
-});
-
-tasks.push(function testGettingIMEI() {
+function testGettingIMEI() {
   log("Test *#06# ...");
 
-  let request = mobileConnection.sendMMI("*#06#");
-  ok(request instanceof DOMRequest,
-     "request is instanceof " + request.constructor);
+  let MMI_CODE = "*#06#";
+  return sendMMI(MMI_CODE)
+    .then(function resolve(aResult) {
+      ok(true, MMI_CODE + " success");
+      is(aResult.serviceCode, "scImei", "Service code IMEI");
+      // IMEI is hardcoded as "000000000000000".
+      // See it here {B2G_HOME}/external/qemu/telephony/android_modem.c
+      // (The result of +CGSN).
+      is(aResult.statusMessage, "000000000000000", "Emulator IMEI");
+      is(aResult.additionalInformation, undefined, "No additional information");
+    }, function reject() {
+      ok(false, MMI_CODE + " should not fail");
+    });
+}
 
-  request.onsuccess = function onsuccess(event) {
-    ok(true, "request success");
-    is(typeof event.target.result, "object", "typeof result object");
-    ok(event.target.result instanceof Object, "result instanceof Object");
-    is(event.target.result.statusMessage, "000000000000000", "Emulator IMEI");
-    is(event.target.result.serviceCode, "scImei", "Service code IMEI");
-    is(event.target.result.additionalInformation, undefined,
-       "No additional information");
-    tasks.next();
-  }
-  request.onerror = function onerror() {
-    ok(false, "request should not error");
-    tasks.abort();
-  };
-});
-
-tasks.push(function testInvalidMMICode(){
+function testInvalidMMICode() {
   log("Test invalid MMI code ...");
 
-  let request = mobileConnection.sendMMI("InvalidMMICode");
-  ok(request instanceof DOMRequest,
-     "request is instanceof " + request.constructor);
+  let MMI_CODE = "InvalidMMICode";
+  return sendMMI(MMI_CODE)
+    .then(function resolve() {
+      ok(false, MMI_CODE + " should not success");
+    }, function reject(aError) {
+      ok(true, MMI_CODE + " fail");
+      is(aError.name, "emMmiError", "MMI error name");
+      is(aError.message, "", "No message");
+      is(aError.serviceCode, "", "No serviceCode");
+      is(aError.additionalInformation, null, "No additional information");
+    });
+}
 
-  request.onsuccess = function onsuccess(event) {
-    ok(false, "request should not success");
-    tasks.abort();
-  };
-
-  request.onerror = function onerror() {
-    ok(true, "request error");
-    is(request.error.name, "emMmiError", "MMI error name");
-    tasks.next();
-  };
-});
-
-// WARNING: All tasks should be pushed before this!!!
-tasks.push(function cleanUp() {
-  SpecialPowers.removePermission("mobileconnection", document);
-  finish();
+// Start test
+startTestCommon(function() {
+   return Promise.resolve()
+    .then(() => testGettingIMEI())
+    .then(() => testInvalidMMICode());
 });
