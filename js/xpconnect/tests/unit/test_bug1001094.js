7158358e981634ac52f624987cc80ce9df69f919
Bobby Holley <bobbyholley@gmail.com>
2014-04-24 13:23:27 -0700

    Bug 1001094 - Give nsJSID classinfo. r=mrbkap

---
+++ js/xpconnect/tests/unit/test_bug1001094.js
@@ -0,0 +1,4 @@
+function run_test() {
+  // Make sure nsJSID implements classinfo.
+  do_check_eq(Components.ID("{a6e2a27f-5521-4b35-8b52-99799a744aee}").equals, Components.ID("{daa47351-7d2e-44a7-b8e3-281802a1eab7}").equals);
+}
