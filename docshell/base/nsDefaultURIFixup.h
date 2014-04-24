5367622c272a3671d185389da013786f23d446b5
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2014-04-24 23:42:00 +0100

    Bug 693808 - part 1: entering numbers+Enter in the location bar should bring search results immediately if domain is not whitelisted, r=bz

--- docshell/base/nsDefaultURIFixup.h
+++ docshell/base/nsDefaultURIFixup.h
@@ -9,6 +9,8 @@
 
 #include "nsIURIFixup.h"
 
+class nsDefaultURIFixupInfo;
+
 /* Header file */
 class nsDefaultURIFixup : public nsIURIFixup
 {
@@ -25,11 +27,36 @@ private:
     /* additional members */
     nsresult FileURIFixup(const nsACString &aStringURI, nsIURI** aURI);
     nsresult ConvertFileToStringURI(const nsACString& aIn, nsCString& aOut);
-    void KeywordURIFixup(const nsACString &aStringURI, nsIInputStream** aPostData, nsIURI** aURI);
+    nsresult FixupURIProtocol(const nsACString& aIn, nsIURI** aURI);
+    void KeywordURIFixup(const nsACString &aStringURI,
+                         nsDefaultURIFixupInfo* aFixupInfo,
+                         nsIInputStream** aPostData);
     bool PossiblyByteExpandedFileName(const nsAString& aIn);
     bool PossiblyHostPortUrl(const nsACString& aUrl);
     bool MakeAlternateURI(nsIURI *aURI);
     bool IsLikelyFTP(const nsCString& aHostSpec);
 };
 
+class nsDefaultURIFixupInfo : public nsIURIFixupInfo
+{
+public:
+    NS_DECL_ISUPPORTS
+    NS_DECL_NSIURIFIXUPINFO
+
+    nsDefaultURIFixupInfo(const nsACString& aOriginalInput);
+
+    friend class nsDefaultURIFixup;
+
+protected:
+    virtual ~nsDefaultURIFixupInfo();
+
+private:
+    nsCOMPtr<nsISupports> mConsumer;
+    nsCOMPtr<nsIURI> mPreferredURI;
+    nsCOMPtr<nsIURI> mFixedURI;
+    bool mFixupUsedKeyword;
+    bool mInputHasProtocol;
+    bool mInputHostHasDot;
+    nsAutoCString mOriginalInput;
+};
 #endif
