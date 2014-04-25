83e8056756379b0bb35d2f0ec1b3e520f50fd23c
Ryan VanderMeulen <ryanvm@gmail.com>
2014-04-25 14:08:12 -0400

    Backed out changeset 7f3e428b4a5d (bug 991904) for xpcshell failures. CLOSED TREE

--- toolkit/devtools/tests/unit/head_devtools.js
+++ toolkit/devtools/tests/unit/head_devtools.js
@@ -4,7 +4,6 @@ const Ci = Components.interfaces;
 const Cu = Components.utils;
 const Cr = Components.results;
 
-Cu.import("resource://gre/modules/devtools/Loader.jsm");
 Cu.import("resource://gre/modules/devtools/DevToolsUtils.jsm");
 
 // Register a console listener, so console messages don't just disappear
