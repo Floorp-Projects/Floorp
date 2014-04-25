05047a8c9c97dd1ab43d922ad632d4e4a5a79206
Tim Taubert <ttaubert@mozilla.com>
2014-04-25 18:01:04 +0200

    Bug 1001521 - Fix tabview tests that remove the original tab. r=smacleod, a=test-only

--- browser/components/tabview/test/browser_tabview_bug608158.js
+++ browser/components/tabview/test/browser_tabview_bug608158.js
@@ -3,34 +3,26 @@
 
 function test() {
   waitForExplicitFinish();
-
-  window.addEventListener("tabviewshown", onTabViewWindowLoaded, false);
-  TabView.toggle();
+  newWindowWithTabView(onTabViewWindowLoaded);
 }
 
-function onTabViewWindowLoaded() {
-  window.removeEventListener("tabviewshown", onTabViewWindowLoaded, false);
-
-  let contentWindow = document.getElementById("tab-view").contentWindow;
+function onTabViewWindowLoaded(win) {
+  let contentWindow = win.TabView.getContentWindow();
 
   is(contentWindow.GroupItems.groupItems.length, 1, 
      "There is one group item on startup");
-  is(gBrowser.tabs.length, 1, "There is one tab on startup");
+  is(win.gBrowser.tabs.length, 1, "There is one tab on startup");
   let groupItem = contentWindow.GroupItems.groupItems[0];
 
   hideGroupItem(groupItem, function () {
-    let onTabViewHidden = function() {
-      window.removeEventListener("tabviewhidden", onTabViewHidden, false);
+    hideTabView(() => {
       is(contentWindow.GroupItems.groupItems.length, 1, 
          "There is still one group item");
       isnot(groupItem, contentWindow.GroupItems.groupItems[0], 
             "The initial group item is not the same as the final group item");
-      is(gBrowser.tabs.length, 1, "There is only one tab");
-      ok(!TabView.isVisible(), "Tab View is hidden");
-      finish();
-    };
-    window.addEventListener("tabviewhidden", onTabViewHidden, false);
-
-    TabView.hide();
+      is(win.gBrowser.tabs.length, 1, "There is only one tab");
+      ok(!win.TabView.isVisible(), "Tab View is hidden");
+      promiseWindowClosed(win).then(finish);
+    }, win);
   });
 }
