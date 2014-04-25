a3010e64139445258847167741b3272ab32e05d5
Tim Taubert <ttaubert@mozilla.com>
2014-04-25 18:01:04 +0200

    Bug 1001521 - Fix tabview tests that remove the original tab r=smacleod

--- browser/components/tabview/test/browser_tabview_bug685692.js
+++ browser/components/tabview/test/browser_tabview_bug685692.js
@@ -4,44 +4,38 @@
 function test() {
   waitForExplicitFinish();
 
-  gBrowser.addTab("http://example.com/");
-  gBrowser.addTab("http://example.com/");
+  newWindowWithTabView(win => {
+    win.gBrowser.addTab("http://example.com/");
+    win.gBrowser.addTab("http://example.com/");
 
-  registerCleanupFunction(function () {
-    while (gBrowser.tabs.length > 1)
-      gBrowser.removeTab(gBrowser.tabs[1]);
-    hideTabView();
-  })
-
-  afterAllTabsLoaded(function() {
-    showTabView(function() {
-      let cw = TabView.getContentWindow();
+    afterAllTabsLoaded(function() {
+      let cw = win.TabView.getContentWindow();
 
       let groupItemOne = cw.GroupItems.groupItems[0];
       is(groupItemOne.getChildren().length, 3, "The number of tabs in group one is 3");
- 
+
       // create a group with a blank tab
-      let groupItemTwo = createGroupItemWithBlankTabs(window, 400, 400, 40, 1);
+      let groupItemTwo = createGroupItemWithBlankTabs(win, 400, 400, 40, 1);
       is(groupItemTwo.getChildren().length, 1, "The number of tabs in group two is 1");
 
       cw.UI.setActive(groupItemOne);
 
-      moveTabToAnotherGroup(groupItemOne.getChild(2).tab, groupItemOne, groupItemTwo, function() {
-        moveTabToAnotherGroup(groupItemOne.getChild(1).tab, groupItemOne, groupItemTwo, function() {
+      moveTabToAnotherGroup(win, groupItemOne.getChild(2).tab, groupItemOne, groupItemTwo, function() {
+        moveTabToAnotherGroup(win, groupItemOne.getChild(1).tab, groupItemOne, groupItemTwo, function() {
           groupItemOne.close();
-          hideTabView(finish);
+          promiseWindowClosed(win).then(finish);
         });
       });
     });
   });
 }
 
-function moveTabToAnotherGroup(targetTab, groupItemOne, groupItemTwo, callback) {
+function moveTabToAnotherGroup(win, targetTab, groupItemOne, groupItemTwo, callback) {
   hideTabView(function() {
     let tabCountInGroupItemOne = groupItemOne.getChildren().length;
     let tabCountInGroupItemTwo = groupItemTwo.getChildren().length;
 
-    TabView.moveTabTo(targetTab, groupItemTwo.id);
+    win.TabView.moveTabTo(targetTab, groupItemTwo.id);
 
     showTabView(function() {
       is(groupItemOne.getChildren().length, --tabCountInGroupItemOne, "The number of tab items in group one is decreased");
@@ -49,6 +43,6 @@ function moveTabToAnotherGroup(targetTab, groupItemOne, groupItemTwo, callback) 
       is(groupItemTwo.getChild(tabCountInGroupItemTwo-1).tab, targetTab, "The last tab is the moved tab");
 
       callback();
-    });
-  });
+    }, win);
+  }, win);
 }
