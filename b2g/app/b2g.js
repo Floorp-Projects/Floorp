5080a4a5d56dd1eb8b7b4dae4f17025ca0e06c9b
Kevin Grandon <kevingrandon@yahoo.com>
2014-04-25 15:01:00 +0200

    Bug 1001673 - Decrease default context menu delay r=vingtetun

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -569,7 +569,7 @@ pref("extensions.getAddons.cache.enabled", false);
 
 // Context Menu
 pref("ui.click_hold_context_menus", true);
-pref("ui.click_hold_context_menus.delay", 750);
+pref("ui.click_hold_context_menus.delay", 400);
 
 // Enable device storage
 pref("device.storage.enabled", true);
