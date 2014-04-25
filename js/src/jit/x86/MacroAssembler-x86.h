57b9f578e2f6c5dc354d8a890eb25a0a2b1d1c79
Tooru Fujisawa <arai_a@mac.com>
2014-04-25 15:39:04 +0900

    Bug 889456 - Implement JSOP_INITELEM_INC and JSOP_SPREAD in the baseline compiler. r=jandem

--- js/src/jit/x86/MacroAssembler-x86.h
+++ js/src/jit/x86/MacroAssembler-x86.h
@@ -1035,6 +1035,9 @@ class MacroAssemblerX86 : public MacroAssemblerX86Shared
         bind(&noOverflow);
     }
 
+    void incrementInt32Value(const Address &addr) {
+        addl(Imm32(1), payloadOf(addr));
+    }
 
     // If source is a double, load it into dest. If source is int32,
     // convert it to double. Else, branch to failure.
