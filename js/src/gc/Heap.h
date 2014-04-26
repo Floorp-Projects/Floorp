7bea6a6f69ad171648c364626fcba154c1d6516c
Jon Coppeard <jcoppeard@mozilla.com>
2014-04-26 09:30:04 +0100

    Bug 988950 - Store chunk location in the chunk trailer r=terrence

--- js/src/gc/Heap.h
+++ js/src/gc/Heap.h
@@ -606,8 +606,17 @@ ArenaHeader::getThingSize() const
  */
 struct ChunkTrailer
 {
+    /* The index the chunk in the nursery, or LocationTenuredHeap. */
+    uint32_t        location;
+
+#if JS_BITS_PER_WORD == 64
+    uint32_t        padding;
+#endif
+
     JSRuntime       *runtime;
 };
+
+static_assert(sizeof(ChunkTrailer) == 2 * sizeof(uintptr_t), "ChunkTrailer size is incorrect.");
 
 /* The chunk header (located at the end of the chunk to preserve arena alignment). */
 struct ChunkInfo
@@ -623,7 +632,7 @@ struct ChunkInfo
      * Calculating sizes and offsets is simpler if sizeof(ChunkInfo) is
      * architecture-independent.
      */
-    char            padding[16];
+    char            padding[20];
 #endif
 
     /*
