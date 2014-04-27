a1c0b77af422b0b011057f7919440a85e415079d
Matthew Gregan <kinetik@flim.org>
2014-04-27 22:11:00 +1200

    Bug 1002320 - Switch video readers at the earliest possible point rather than the latest.  r=cajbir

--- content/media/mediasource/SubBufferDecoder.h
+++ content/media/mediasource/SubBufferDecoder.h
@@ -21,6 +21,7 @@ public:
   // of the caller to manage the memory of the MediaResource object.
   SubBufferDecoder(MediaResource* aResource, MediaSourceDecoder* aParentDecoder)
     : BufferDecoder(aResource), mParentDecoder(aParentDecoder), mReader(nullptr)
+    , mMediaDuration(-1), mMediaStartTime(0)
   {
   }
 
@@ -72,10 +73,21 @@ public:
     return mMediaDuration;
   }
 
+  int64_t GetMediaStartTime()
+  {
+    return mMediaStartTime;
+  }
+
+  void SetMediaStartTime(int64_t aMediaStartTime)
+  {
+    mMediaStartTime = aMediaStartTime;
+  }
+
 private:
   MediaSourceDecoder* mParentDecoder;
   nsAutoPtr<MediaDecoderReader> mReader;
   int64_t mMediaDuration;
+  int64_t mMediaStartTime;
 };
 
 } // namespace mozilla
