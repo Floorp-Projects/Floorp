d29d5f2d513640e13b5c9bff7bbf471072acd2b8
Bevis Tseng <btseng@mozilla.com>
2014-04-28 11:54:07 +0800

    Bug 824717 - Part 1: Define DOMMobileMessageError:DOMError to hook an additional nsISupports object like nsIDOMMozSmsMessage/nsIDOMMozMmsMessage when error occurs. r=hsinyi, r=smaug

---
+++ dom/mobilemessage/src/DOMMobileMessageError.h
@@ -0,0 +1,45 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_MobileMessageError_h
+#define mozilla_dom_MobileMessageError_h
+
+#include "mozilla/dom/DOMError.h"
+
+class nsIDOMMozMmsMessage;
+class nsIDOMMozSmsMessage;
+
+namespace mozilla {
+namespace dom {
+
+class OwningMozSmsMessageOrMozMmsMessage;
+
+class DOMMobileMessageError MOZ_FINAL : public DOMError
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(DOMMobileMessageError, DOMError)
+
+  DOMMobileMessageError(nsPIDOMWindow* aWindow, const nsAString& aName,
+                        nsIDOMMozSmsMessage* aSms);
+
+  DOMMobileMessageError(nsPIDOMWindow* aWindow, const nsAString& aName,
+                        nsIDOMMozMmsMessage* aMms);
+
+  virtual JSObject*
+  WrapObject(JSContext* aCx) MOZ_OVERRIDE;
+
+  void GetData(OwningMozSmsMessageOrMozMmsMessage& aRetVal) const;
+
+private:
+  nsCOMPtr<nsIDOMMozSmsMessage> mSms;
+  nsCOMPtr<nsIDOMMozMmsMessage> mMms;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_MobileMessageError_h
