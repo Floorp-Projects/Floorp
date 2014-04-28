cf6b815cd20e490dd4cb23d0947c1b182efd174f
Wan-Teh Chang <wtc@google.com>
2014-04-27 20:39:24 -0700

    Bug 993569 - Update Mozilla 31 to use NSS 3.16.1 Beta 3. The main change is https://hg.mozilla.org/projects/nss/rev/7e8485a5ed49.

--- security/nss/lib/freebl/blapit.h
+++ security/nss/lib/freebl/blapit.h
@@ -1,5 +1,5 @@
 /*
- * blapit.h - public data structures for the crypto library
+ * blapit.h - public data structures for the freebl library
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
