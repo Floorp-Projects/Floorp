fce819567cf3280beec2a3485d9d71435b8eb051
William Angell <william.h.angell@gmail.com>
2014-04-28 10:18:00 +0100

    Bug 974465 - Replaced all asterisk imports in Marionette client with explicit imports; r=automatedtester

--- testing/marionette/client/marionette/runner/mixins/__init__.py
+++ testing/marionette/client/marionette/runner/mixins/__init__.py
@@ -2,6 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from endurance import *
-from reporting import *
-from b2g import *
+from endurance import (
+        EnduranceOptionsMixin, EnduranceTestCaseMixin, MemoryEnduranceTestCaseMixin
+        )
+from reporting import (
+        HTMLReportingOptionsMixin, HTMLReportingTestResultMixin,
+        HTMLReportingTestRunnerMixin
+        )
+from b2g import B2GTestCaseMixin, B2GTestResultMixin
