4b16d90d3b6777e3ecdb7bd38a03d105579a835c
Alexandre Poirot <poirot.alex@gmail.com>
2014-04-28 14:53:00 -0400

    Bug 961745 - Introduce b2g/dev application to build a mulet. r=mshal, r=fabrice

---
+++ b2g/dev/confvars.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_B2G=1
+MOZ_MULET=1
+
+. ${srcdir}/browser/confvars.sh
