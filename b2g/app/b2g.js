7cd071602c6e7fb9e578fe7f97b5729f97ad8564
Kartikaya Gupta <kgupta@mozilla.com>
2014-04-28 13:56:00 +0200

    Bug 945789 - Tune APZ physics prefs based on UX feedback. r=botond

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -899,9 +899,9 @@ pref("osfile.reset_worker_delay", 5000);
 pref("apz.asyncscroll.throttle", 40);
 pref("apz.pan_repaint_interval", 16);
 
-// Maximum fling velocity in inches/ms.  Slower devices may need to reduce this
-// to avoid checkerboarding.  Note, float value must be set as a string.
-pref("apz.max_velocity_inches_per_ms", "0.0375");
+// APZ physics settings, tuned by UX designers
+pref("apz.max_velocity_inches_per_ms", "0.07");
+pref("apz.fling_friction", "0.003");
 
 // Tweak default displayport values to reduce the risk of running out of
 // memory when zooming in
