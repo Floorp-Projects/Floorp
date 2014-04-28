3fc9d2ac1aa0d67dee3d93a12a867e3089ec28a6
Bevis Tseng <btseng@mozilla.com>
2014-04-28 13:28:12 +0800

    Bug 824717 - Part 6: Notify SendMesageFailed with saved DOM Message if available. r=vyang

--- dom/mobilemessage/src/gonk/MmsService.js
+++ dom/mobilemessage/src/gonk/MmsService.js
@@ -1955,8 +1955,7 @@ MmsService.prototype = {
       .setMessageReadStatusByEnvelopeId(envelopeId, address, readStatus,
                                         (function(aRv, aDomMessage) {
       if (!Components.isSuccessCode(aRv)) {
-        // Notifying observers the read status is error.
-        Services.obs.notifyObservers(aDomMessage, kSmsReadSuccessObserverTopic, null);
+        if (DEBUG) debug("Failed to update read status: " + aRv);
         return;
       }
 
@@ -2171,7 +2170,7 @@ MmsService.prototype = {
       // If the messsage has been deleted (because the sending process is
       // cancelled), we don't need to reset the its delievery state/status.
       if (aErrorCode == Ci.nsIMobileMessageCallback.NOT_FOUND_ERROR) {
-        aRequest.notifySendMessageFailed(aErrorCode);
+        aRequest.notifySendMessageFailed(aErrorCode, aDomMessage);
         Services.obs.notifyObservers(aDomMessage, kSmsFailedObserverTopic, null);
         return;
       }
@@ -2188,7 +2187,7 @@ MmsService.prototype = {
         // TODO bug 832140 handle !Components.isSuccessCode(aRv)
         if (!isSentSuccess) {
           if (DEBUG) debug("Sending MMS failed.");
-          aRequest.notifySendMessageFailed(aErrorCode);
+          aRequest.notifySendMessageFailed(aErrorCode, aDomMessage);
           Services.obs.notifyObservers(aDomMessage, kSmsFailedObserverTopic, null);
           return;
         }
@@ -2214,7 +2213,8 @@ MmsService.prototype = {
       if (!Components.isSuccessCode(aRv)) {
         if (DEBUG) debug("Error! Fail to save sending message! rv = " + aRv);
         aRequest.notifySendMessageFailed(
-          gMobileMessageDatabaseService.translateCrErrorToMessageCallbackError(aRv));
+          gMobileMessageDatabaseService.translateCrErrorToMessageCallbackError(aRv),
+          aDomMessage);
         Services.obs.notifyObservers(aDomMessage, kSmsFailedObserverTopic, null);
         return;
       }
