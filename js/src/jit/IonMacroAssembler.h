da7dbdf8b2cf84f8afeb4c0817df55f90ad253e4
Jan de Mooij <jdemooij@mozilla.com>
2014-04-29 08:54:46 +0200

    Bug 1000942 - Eliminate some unnecessary object type barriers. r=bhackett

--- js/src/jit/IonMacroAssembler.h
+++ js/src/jit/IonMacroAssembler.h
@@ -300,7 +300,7 @@ class MacroAssembler : public MacroAssemblerSpecific
     // Emits a test of a value against all types in a TypeSet. A scratch
     // register is required.
     template <typename Source, typename TypeSet>
-    void guardTypeSet(const Source &address, const TypeSet *types, Register scratch, Label *miss);
+    void guardTypeSet(const Source &address, const TypeSet *types, BarrierKind kind, Register scratch, Label *miss);
     template <typename TypeSet>
     void guardObjectType(Register obj, const TypeSet *types, Register scratch, Label *miss);
     template <typename Source>
