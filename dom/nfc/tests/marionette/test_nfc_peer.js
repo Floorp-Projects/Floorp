7874282e4907f34927bce7dae486a1e376c30b4b
Siddartha Pothapragada <Siddartha.Pothapragada@telekom.com>
2014-04-30 10:17:00 +0200

    Bug 959059 - B2G NFC: Marionette test to check P2P registration for an incorrect manifest Url. r=allstars.chh

--- dom/nfc/tests/marionette/test_nfc_peer.js
+++ dom/nfc/tests/marionette/test_nfc_peer.js
@@ -5,6 +5,7 @@ MARIONETTE_TIMEOUT = 30000;
 MARIONETTE_HEAD_JS = 'head.js';
 
 let MANIFEST_URL = "app://system.gaiamobile.org/manifest.webapp";
+let INCORRECT_MANIFEST_URL = "app://xyz.gaiamobile.org/manifest.webapp";
 
 function peerReadyCb(evt) {
   log("peerReadyCb called");
@@ -25,11 +26,35 @@ function handleTechnologyDiscoveredRE0(msg) {
 
   let request = nfc.checkP2PRegistration(MANIFEST_URL);
   request.onsuccess = function (evt) {
+    is(request.result, true, "check for P2P registration result");
     nfc.notifyUserAcceptedP2P(MANIFEST_URL);
   }
 
   request.onerror = function () {
     ok(false, "checkP2PRegistration failed.");
+    toggleNFC(false, runNextTest);
+  }
+}
+
+function handleTechnologyDiscoveredRE0ForP2PRegFailure(msg) {
+  log("Received \'nfc-manager-tech-discovered\'");
+  is(msg.type, "techDiscovered", "check for correct message type");
+  is(msg.techList[0], "P2P", "check for correct tech type");
+
+  nfc.onpeerready = peerReadyCb;
+
+  let request = nfc.checkP2PRegistration(INCORRECT_MANIFEST_URL);
+  request.onsuccess = function (evt) {
+    is(request.result, false, "check for P2P registration result");
+
+    nfc.onpeerready = null;
+    toggleNFC(false, runNextTest);
+  }
+
+  request.onerror = function () {
+    ok(false, "checkP2PRegistration failed.");
+
+    nfc.onpeerready = null;
     toggleNFC(false, runNextTest);
   }
 }
@@ -55,8 +80,18 @@ function testPeerReady() {
   });
 }
 
+function testCheckP2PRegFailure() {
+  window.navigator.mozSetMessageHandler(
+    "nfc-manager-tech-discovered", handleTechnologyDiscoveredRE0ForP2PRegFailure);
+
+  toggleNFC(true, function() {
+    activateRE(0);
+  });
+}
+
 let tests = [
-  testPeerReady
+  testPeerReady,
+  testCheckP2PRegFailure
 ];
 
 SpecialPowers.pushPermissions(
