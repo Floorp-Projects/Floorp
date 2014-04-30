16b1942e16fbf3c0e236cca3f474a252d2eca182
Wes Kocher <wkocher@mozilla.com>
2014-04-30 16:35:20 -0700

    Backed out changeset 7a4f15c7cec5 (bug 1003159) for m-oth bustage on a CLOSED TREE

--- browser/branding/nightly/pref/firefox-branding.js
+++ browser/branding/nightly/pref/firefox-branding.js
@@ -8,8 +8,7 @@ pref("startup.homepage_welcome_url", "https://www.mozilla.org/projects/firefox/%
 pref("app.update.interval", 7200); // 2 hours
 // The time interval between the downloading of mar file chunks in the
 // background (in seconds)
-// 0 means "download everything at once"
-pref("app.update.download.backgroundInterval", 0);
+pref("app.update.download.backgroundInterval", 60);
 // Give the user x seconds to react before showing the big UI. default=12 hours
 pref("app.update.promptWaitTime", 43200);
 // URL user can browse to manually if for some reason all update installation
