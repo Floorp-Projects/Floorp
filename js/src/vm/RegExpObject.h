ba4af2e903945d937512f95c42e7bae5d76608b9
Ryan VanderMeulen <ryanvm@gmail.com>
2014-04-30 15:56:36 -0400

    Backed out changesets 00edef0582f1 and 7796f1b42487 (bug 965712) for making OSX mochitest-dt more timeout-prone in the debugger tests. CLOSED TREE

--- js/src/vm/RegExpObject.h
+++ js/src/vm/RegExpObject.h
@@ -145,7 +145,6 @@ class RegExpShared
 
     RegExpFlag         flags;
     unsigned           parenCount;
-    bool               canStringMatch;
 
 #if ENABLE_YARR_JIT
     /* Note: Native code is valid only if |codeBlock.isFallBack() == false|. */
@@ -205,11 +204,7 @@ class RegExpShared
 
     /* Accessors */
 
-    size_t getParenCount() const {
-        JS_ASSERT(isCompiled() || canStringMatch);
-        return parenCount;
-    }
-
+    size_t getParenCount() const        { JS_ASSERT(isCompiled()); return parenCount; }
     void incRef()                       { activeUseCount++; }
     void decRef()                       { JS_ASSERT(activeUseCount > 0); activeUseCount--; }
 
