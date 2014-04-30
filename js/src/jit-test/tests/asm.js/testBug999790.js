0795b0784c231040e36abc0b161f4fa775c1d149
Benjamin Bouvier <benj@benj.me>
2014-04-30 21:31:28 +0200

    Bug 999790: Make asm.js modules and functions constructible; r=luke

---
+++ js/src/jit-test/tests/asm.js/testBug999790.js
@@ -0,0 +1,69 @@
+function CanBeConstructed(f) {
+    var caught = false;
+    try {
+        new f;
+    } catch (e) {
+        caught = true;
+    }
+    return !caught;
+}
+
+function IsConstructedFunction(f) {
+    return f.hasOwnProperty('length')
+        && f.hasOwnProperty('name')
+        && f.hasOwnProperty('arguments')
+        && f.hasOwnProperty('caller')
+        && f.hasOwnProperty('prototype')
+        && f.prototype.hasOwnProperty('constructor')
+        && f.prototype.constructor === f;
+}
+
+function IsntConstructedFunction(f) {
+    return !f.hasOwnProperty('length')
+        && !f.hasOwnProperty('name')
+        && !f.hasOwnProperty('arguments')
+        && !f.hasOwnProperty('caller')
+        && !f.hasOwnProperty('prototype')
+}
+
+var m = function() {
+    "use asm"
+    function g(){}
+    return g;
+};
+assertEq(CanBeConstructed(m), true, "asm.js modules can't be constructed");
+
+var objM = new m;
+assertEq(IsConstructedFunction(objM), true);
+
+var g = m();
+assertEq(CanBeConstructed(g), true, "asm.js functions can't be constructed");
+// g is a ctor returning an primitive value, thus an empty object
+assertEq(Object.getOwnPropertyNames(new g).length, 0);
+
+var n = function() {
+    "use asm"
+    function g(){return 42.0}
+    function h(){return 42}
+    return {
+        g: g,
+        h: h
+    };
+};
+assertEq(CanBeConstructed(n), true, "asm.js modules can't be constructed");
+
+var objN = new n;
+// objN is an object with attributes g and h
+assertEq(IsntConstructedFunction(objN), true);
+assertEq(objN.hasOwnProperty('g'), true);
+assertEq(objN.hasOwnProperty('h'), true);
+
+assertEq(IsConstructedFunction(objN.g), true);
+assertEq(IsConstructedFunction(objN.h), true);
+
+var h = n().h;
+assertEq(CanBeConstructed(h), true, "asm.js functions can't be constructed");
+// h is a ctor returning an primitive value, thus an empty object
+assertEq(Object.getOwnPropertyNames(new h).length, 0);
+
+assertEq(typeof(function() {"use asm"; return {}}.prototype) !== 'undefined', true);
--- js/src/jit/AsmJSLink.cpp
+++ js/src/jit/AsmJSLink.cpp
@@ -515,6 +515,16 @@ CallAsmJS(JSContext *cx, unsigned argc, Value *vp)
             return false;
     }
 
+    if (callArgs.isConstructing()) {
+        // By spec, when a function is called as a constructor and this function
+        // returns a primary type, which is the case for all asm.js exported
+        // functions, the returned value is discarded and an empty object is
+        // returned instead.
+        JSObject *obj = NewBuiltinClassInstance(cx, &JSObject::class_);
+        callArgs.rval().set(ObjectValue(*obj));
+        return true;
+    }
+
     switch (func.returnType()) {
       case AsmJSModule::Return_Void:
         callArgs.rval().set(UndefinedValue());
@@ -536,7 +546,7 @@ NewExportedFunction(JSContext *cx, const AsmJSModule::ExportedFunction &func,
 {
     RootedPropertyName name(cx, func.name());
     JSFunction *fun = NewFunction(cx, NullPtr(), CallAsmJS, func.numArgs(),
-                                  JSFunction::NATIVE_FUN, cx->global(), name,
+                                  JSFunction::ASMJS_CTOR, cx->global(), name,
                                   JSFunction::ExtendedFinalizeKind);
     if (!fun)
         return nullptr;
@@ -798,8 +808,8 @@ js::NewAsmJSModuleFunction(ExclusiveContext *cx, JSFunction *origFun, HandleObje
 {
     RootedPropertyName name(cx, origFun->name());
 
-    JSFunction::Flags flags = origFun->isLambda() ? JSFunction::NATIVE_LAMBDA_FUN
-                                                  : JSFunction::NATIVE_FUN;
+    JSFunction::Flags flags = origFun->isLambda() ? JSFunction::ASMJS_LAMBDA_CTOR
+                                                  : JSFunction::ASMJS_CTOR;
     JSFunction *moduleFun = NewFunction(cx, NullPtr(), LinkAsmJS, origFun->nargs(),
                                         flags, NullPtr(), name,
                                         JSFunction::ExtendedFinalizeKind, TenuredObject);
