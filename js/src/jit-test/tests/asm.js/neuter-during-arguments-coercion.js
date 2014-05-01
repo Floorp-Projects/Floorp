3c1179b08e124b7d0c2d4c2edc7017c4884a4ac9
Jeff Walden <jwalden@mit.edu>
2014-05-01 14:30:19 -0700

    Bug 991981 - Test for asm.js ArrayBuffer heap neutering during arguments coercion.  r=luke

---
+++ js/src/jit-test/tests/asm.js/neuter-during-arguments-coercion.js
@@ -0,0 +1,27 @@
+load(libdir + "asm.js");
+
+function f(stdlib, foreign, buffer)
+{
+  "use asm";
+  var i32 = new stdlib.Int32Array(buffer);
+  function set(v)
+  {
+    v=v|0;
+    i32[5] = v;
+  }
+  return set;
+}
+if (isAsmJSCompilationAvailable())
+  assertEq(isAsmJSModule(f), true);
+
+var i32 = new Int32Array(4096);
+var buffer = i32.buffer;
+var set = f(this, null, buffer);
+if (isAsmJSCompilationAvailable())
+  assertEq(isAsmJSFunction(set), true);
+
+try
+{
+  set({ valueOf: function() { neuter(buffer, "same-data"); return 17; } });
+}
+catch (e) { /* if an exception thrown, swallow */ }
