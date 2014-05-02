5cb35f0d9e7fb8c848636ac8aacf66a9c1519759
Benjamin Bouvier <benj@benj.me>
2014-05-02 13:11:13 +0200

    Bug 996076: Add SIMD.int32x4.{equal,greaterThan,lessThan} to the interpreter; r=nmatsakis

---
+++ js/src/tests/ecma_6/TypedObject/simd/int32x4equal.js
@@ -0,0 +1,26 @@
+// |reftest| skip-if(!this.hasOwnProperty("SIMD"))
+var BUGNUMBER = 996076;
+var float32x4 = SIMD.float32x4;
+var int32x4 = SIMD.int32x4;
+
+var summary = 'int32x4 equal';
+
+function test() {
+  print(BUGNUMBER + ": " + summary);
+
+  // FIXME -- Bug 948379: Amend to check for correctness of border cases.
+
+  var a = int32x4(1, 20, 30, 40);
+  var b = int32x4(10, 20, 30, 4);
+  var c = SIMD.int32x4.equal(a, b);
+  assertEq(c.x, 0);
+  assertEq(c.y, -1);
+  assertEq(c.z, -1);
+  assertEq(c.w, 0);
+
+  if (typeof reportCompare === "function")
+    reportCompare(true, true);
+}
+
+test();
+
---
+++ js/src/tests/ecma_6/TypedObject/simd/int32x4greaterthan.js
@@ -0,0 +1,26 @@
+// |reftest| skip-if(!this.hasOwnProperty("SIMD"))
+var BUGNUMBER = 996076;
+var float32x4 = SIMD.float32x4;
+var int32x4 = SIMD.int32x4;
+
+var summary = 'int32x4 greaterThan';
+
+function test() {
+  print(BUGNUMBER + ": " + summary);
+
+  // FIXME -- Bug 948379: Amend to check for correctness of border cases.
+
+  var a = int32x4(1, 20, 3, 40);
+  var b = int32x4(10, 2, 30, 4);
+  var c = SIMD.int32x4.greaterThan(b,a);
+  assertEq(c.x, -1);
+  assertEq(c.y, 0);
+  assertEq(c.z, -1);
+  assertEq(c.w, 0);
+
+  if (typeof reportCompare === "function")
+    reportCompare(true, true);
+}
+
+test();
+
