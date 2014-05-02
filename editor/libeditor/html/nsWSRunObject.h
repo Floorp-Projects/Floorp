7bbb8ae9adf0fad140754876c44d9962689a39ba
Aryeh Gregor <ayg@aryeh.name>
2014-05-02 14:11:26 +0300

    Bug 1003894 part 4 - Clean up nsWSRunObject::DeleteWSBackward; r=ehsan

--- editor/libeditor/html/nsWSRunObject.h
+++ editor/libeditor/html/nsWSRunObject.h
@@ -180,6 +180,11 @@ class MOZ_STACK_CLASS nsWSRunObject
     // adjusting ws.
     // example of fixup: trailingws before {aStartNode,aStartOffset}
     //                   needs to be removed.
+    static nsresult PrepareToDeleteRange(nsHTMLEditor* aHTMLEd,
+                                         nsCOMPtr<nsINode>* aStartNode,
+                                         int32_t* aStartOffset,
+                                         nsCOMPtr<nsINode>* aEndNode,
+                                         int32_t* aEndOffset);
     static nsresult PrepareToDeleteRange(nsHTMLEditor *aHTMLEd, 
                                          nsCOMPtr<nsIDOMNode> *aStartNode,
                                          int32_t *aStartOffset, 
