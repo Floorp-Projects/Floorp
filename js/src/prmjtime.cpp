bbcf843537c2fe1e1dcd761b03d97a9da65e4485
Jan de Mooij <jdemooij@mozilla.com>
2014-05-03 11:08:23 +0200

    Bug 1004923 part 3 - Cleanup Windows PRMJ_Now. r=njn

--- js/src/prmjtime.cpp
+++ js/src/prmjtime.cpp
@@ -259,9 +259,6 @@ def PRMJ_Now():
 int64_t
 PRMJ_Now()
 {
-    double lowresTime, highresTimerValue;
-    FILETIME ft;
-    LARGE_INTEGER now;
     bool calibrated = false;
     bool needsCalibration = false;
     int64_t returnedTime;
@@ -294,34 +291,32 @@ PRMJ_Now()
         }
 
 
-        /* Calculate a low resolution time */
+        // Calculate a low resolution time.
+        FILETIME ft;
         GetSystemTimeAsFileTime(&ft);
-        lowresTime = FileTimeToUnixMicroseconds(ft);
+        double lowresTime = FileTimeToUnixMicroseconds(ft);
 
         if (calibration.freq > 0.0) {
-            double highresTime, diff;
-
-            DWORD timeAdjustment, timeIncrement;
-            BOOL timeAdjustmentDisabled;
-
-            /* Default to 15.625 ms if the syscall fails */
-            double skewThreshold = 15625.25;
-            /* Grab high resolution time */
+            // Grab high resolution time.
+            LARGE_INTEGER now;
             QueryPerformanceCounter(&now);
-            highresTimerValue = double(now.QuadPart);
+            double highresTimerValue = double(now.QuadPart);
 
             MUTEX_LOCK(&calibration.data_lock);
-            highresTime = calibration.offset + PRMJ_USEC_PER_SEC*
+            double highresTime = calibration.offset + PRMJ_USEC_PER_SEC *
                  (highresTimerValue-calibration.timer_offset)/calibration.freq;
             cachedOffset = calibration.offset;
 
-            /* On some dual processor/core systems, we might get an earlier time
-               so we cache the last time that we returned */
+            // On some dual processor/core systems, we might get an earlier time
+            // so we cache the last time that we returned.
             calibration.last = js::Max(calibration.last, int64_t(highresTime));
             returnedTime = calibration.last;
             MUTEX_UNLOCK(&calibration.data_lock);
 
-            /* Rather than assume the NT kernel ticks every 15.6ms, ask it */
+            // Rather than assume the NT kernel ticks every 15.6ms, ask it.
+            double skewThreshold;
+            DWORD timeAdjustment, timeIncrement;
+            BOOL timeAdjustmentDisabled;
             if (GetSystemTimeAdjustment(&timeAdjustment,
                                         &timeIncrement,
                                         &timeAdjustmentDisabled)) {
@@ -332,50 +327,51 @@ PRMJ_Now()
                     /* timeIncrement is in units of 100ns */
                     skewThreshold = timeIncrement/10.0;
                 }
+            } else {
+                // Default to 15.625 ms if the syscall fails.
+                skewThreshold = 15625.25;
             }
 
-            /* Check for clock skew */
-            diff = lowresTime - highresTime;
+            // Check for clock skew.
+            double diff = lowresTime - highresTime;
 
-            /* For some reason that I have not determined, the skew can be
-               up to twice a kernel tick. This does not seem to happen by
-               itself, but I have only seen it triggered by another program
-               doing some kind of file I/O. The symptoms are a negative diff
-               followed by an equally large positive diff. */
+            // For some reason that I have not determined, the skew can be
+            // up to twice a kernel tick. This does not seem to happen by
+            // itself, but I have only seen it triggered by another program
+            // doing some kind of file I/O. The symptoms are a negative diff
+            // followed by an equally large positive diff.
             if (mozilla::Abs(diff) > 2 * skewThreshold) {
-                /*fprintf(stderr,"Clock skew detected (diff = %f)!\n", diff);*/
-
                 if (calibrated) {
-                    /* If we already calibrated once this instance, and the
-                       clock is still skewed, then either the processor(s) are
-                       wildly changing clockspeed or the system is so busy that
-                       we get switched out for long periods of time. In either
-                       case, it would be infeasible to make use of high
-                       resolution results for anything, so let's resort to old
-                       behavior for this call. It's possible that in the
-                       future, the user will want the high resolution timer, so
-                       we don't disable it entirely. */
+                    // If we already calibrated once this instance, and the
+                    // clock is still skewed, then either the processor(s) are
+                    // wildly changing clockspeed or the system is so busy that
+                    // we get switched out for long periods of time. In either
+                    // case, it would be infeasible to make use of high
+                    // resolution results for anything, so let's resort to old
+                    // behavior for this call. It's possible that in the
+                    // future, the user will want the high resolution timer, so
+                    // we don't disable it entirely.
                     returnedTime = int64_t(lowresTime);
                     needsCalibration = false;
                 } else {
-                    /* It is possible that when we recalibrate, we will return a
-                       value less than what we have returned before; this is
-                       unavoidable. We cannot tell the different between a
-                       faulty QueryPerformanceCounter implementation and user
-                       changes to the operating system time. Since we must
-                       respect user changes to the operating system time, we
-                       cannot maintain the invariant that Date.now() never
-                       decreases; the old implementation has this behavior as
-                       well. */
+                    // It is possible that when we recalibrate, we will return a
+                    // value less than what we have returned before; this is
+                    // unavoidable. We cannot tell the different between a
+                    // faulty QueryPerformanceCounter implementation and user
+                    // changes to the operating system time. Since we must
+                    // respect user changes to the operating system time, we
+                    // cannot maintain the invariant that Date.now() never
+                    // decreases; the old implementation has this behavior as
+                    // well.
                     needsCalibration = true;
                 }
             } else {
-                /* No detectable clock skew */
+                // No detectable clock skew.
                 returnedTime = int64_t(highresTime);
                 needsCalibration = false;
             }
         } else {
-            /* No high resolution timer is available, so fall back */
+            // No high resolution timer is available, so fall back.
             returnedTime = int64_t(lowresTime);
         }
     } while (needsCalibration);
