38ae43e4f93f81d661d48910012bd16d69c2aefa
Jared Wein <jwein@mozilla.com>
2014-05-04 15:54:29 -0500

    Bug 738797 - Enable the in-content preferences by default. r=Unfocused
    --HG--
    extra : rebase_source : 4c06ec299a082f4a2c587fe5cbeff3b0e5b21548

--- browser/base/content/test/general/browser_offlineQuotaNotification.js
+++ browser/base/content/test/general/browser_offlineQuotaNotification.js
@@ -38,6 +38,11 @@ function checkPreferences(prefsWin) {
 }
 
 function test() {
+  if (Services.prefs.getBoolPref("browser.preferences.inContent")) {
+    // Bug 881576 - ensure this works with inContent prefs.
+    todo(false, "Bug 881576 - this test needs to be updated for inContent prefs");
+    return;
+  }
   waitForExplicitFinish();
   gBrowser.selectedBrowser.addEventListener("load", function onload() {
     gBrowser.selectedBrowser.removeEventListener("load", onload, true);
@@ -50,18 +55,13 @@ function test() {
         // window to open - which we track either via a window watcher (for
         // the window-based prefs) or via an "Initialized" event (for
         // in-content prefs.)
-        if (Services.prefs.getBoolPref("browser.preferences.inContent")) {
-          // Bug 881576 - ensure this works with inContent prefs.
-          todo(false, "Bug 881576 - this test needs to be updated for inContent prefs");
-        } else {
-          Services.ww.registerNotification(function wwobserver(aSubject, aTopic, aData) {
-            if (aTopic != "domwindowopened")
-              return;
-            Services.ww.unregisterNotification(wwobserver);
-            checkPreferences(aSubject);
-          });
-          PopupNotifications.panel.firstElementChild.button.click();
-        }
+        Services.ww.registerNotification(function wwobserver(aSubject, aTopic, aData) {
+          if (aTopic != "domwindowopened")
+            return;
+          Services.ww.unregisterNotification(wwobserver);
+          checkPreferences(aSubject);
+        });
+        PopupNotifications.panel.firstElementChild.button.click();
       });
     };
     Services.prefs.setIntPref("offline-apps.quota.warn", 1);
