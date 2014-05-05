d49ef7c04cb23a8b399c785b36c0751e9271f0b2
Boris Zbarsky <bzbarsky@mit.edu>
2014-05-05 16:01:57 -0400

    Bug 997285 part 1.  Add JS_GetErrorPrototype to JSAPI.  r=jorendorff

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -1826,6 +1826,13 @@ JS_GetObjectPrototype(JSContext *cx, JS::HandleObject forObj);
 extern JS_PUBLIC_API(JSObject *)
 JS_GetArrayPrototype(JSContext *cx, JS::HandleObject forObj);
 
+/*
+ * Returns the original value of |Error.prototype| from the global
+ * object of the current compartment of cx.
+ */
+extern JS_PUBLIC_API(JSObject *)
+JS_GetErrorPrototype(JSContext *cx);
+
 extern JS_PUBLIC_API(JSObject *)
 JS_GetGlobalForObject(JSContext *cx, JSObject *obj);
 
