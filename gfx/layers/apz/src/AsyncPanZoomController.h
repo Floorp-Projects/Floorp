bd551516bb7905067fd0572d6e4c7d49e5a3fe84
Botond Ballo <botond@mozilla.com>
2014-05-05 17:46:57 -0400

    Bug 998025 - Remove some unused AsyncPanZoomController and Axis methods. r=kats

--- gfx/layers/apz/src/AsyncPanZoomController.h
+++ gfx/layers/apz/src/AsyncPanZoomController.h
@@ -807,10 +807,6 @@ public:
     return !mParent || (mParent->mLayersId != mLayersId);
   }
 
-  bool IsRootForLayersId(const uint64_t& aLayersId) const {
-    return (mLayersId == aLayersId) && IsRootForLayersId();
-  }
-
 private:
   // This is a raw pointer to avoid introducing a reference cycle between
   // AsyncPanZoomController and APZCTreeManager. Since these objects don't
