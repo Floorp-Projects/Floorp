1cdd7759bb2ec6fe66c7541ee54791031f99f770
Doug Turner <doug.turner@gmail.com>
2014-05-05 14:30:42 -0700

    Bug 995430 - send header when user is signed into a children\'s account. r=mcmanus, a=gal

--- netwerk/protocol/http/nsHttpHandler.h
+++ netwerk/protocol/http/nsHttpHandler.h
@@ -436,6 +436,7 @@ private:
 
     // for broadcasting safe hint;
     bool           mSafeHintEnabled;
+    bool           mParentalControlEnabled;
 
     // Whether telemetry is reported or not
     uint32_t           mTelemetryEnabled : 1;
