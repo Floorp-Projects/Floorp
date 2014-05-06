a6f970777aa114f5ff810f3aaad6eeeb460f6842
Mark Banner <standard8@mozilla.com>
2014-05-06 20:11:30 +0100

    Bug 975550 Handle python 2.6 when preventing invalid utf-8 being writting to test files. r=ted,a=test-only

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -82,7 +82,7 @@ import mozinfo
 _cleanup_encoding_re = re.compile(u'[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\x9f\\\\]')
 def _cleanup_encoding_repl(m):
     c = m.group(0)
-    return '\\\\' if c == '\\' else '\\x{:02X}'.format(ord(c))
+    return '\\\\' if c == '\\' else '\\x{0:02X}'.format(ord(c))
 def cleanup_encoding(s):
     """S is either a byte or unicode string.  Either way it may
        contain control characters, unpaired surrogates, reserved code
