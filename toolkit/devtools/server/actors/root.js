d05845c75b4040321ae117169ef26a49819718d8
Ryan VanderMeulen <ryanvm@gmail.com>
2014-05-07 14:18:52 -0400

    Backed out changeset 7ae6a7b925be for landing with the wrong bug number.

--- toolkit/devtools/server/actors/root.js
+++ toolkit/devtools/server/actors/root.js
@@ -10,7 +10,6 @@ const { Ci } = require("chrome");
 const Services = require("Services");
 const { ActorPool, appendExtraActors, createExtraActors } = require("devtools/server/actors/common");
 const { DebuggerServer } = require("devtools/server/main");
-const { dumpProtocolSpec } = require("devtools/server/protocol");
 
 /* Root actor for the remote debugging protocol. */
 
@@ -294,7 +293,9 @@ RootActor.prototype = {
     return JSON.parse(JSON.stringify(aRequest));
   },
 
-  onProtocolDescription: dumpProtocolSpec,
+  onProtocolDescription: function (aRequest) {
+    return protocol.dumpProtocolSpec()
+  },
 
   /* Support for DebuggerServer.addGlobalActor. */
   _createExtraActors: createExtraActors,
