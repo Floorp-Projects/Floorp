3b7d34e657ed5bee30ca80bcd101fe6f07879556
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-05-07 10:38:27 +0200

    Backed out changeset 22347f9e52cd (bug 989890) for bc1 testfailures

--- browser/components/preferences/in-content/preferences.js
+++ browser/components/preferences/in-content/preferences.js
@@ -55,14 +55,6 @@ function search(aQuery, aAttribute) {
   let elements = document.getElementById("mainPrefPane").children;
   for (let element of elements) {
     let attributeValue = element.getAttribute(aAttribute);
-    element.hidden = (attributeValue && attributeValue != aQuery);
+    element.hidden = (attributeValue != aQuery);
   }
-}
-
-function helpButtonCommand() {
-  let pane = history.state;
-  let categories = document.getElementById("categories");
-  let helpTopic = categories.querySelector(".category[value=" + pane + "]")
-                            .getAttribute("helpTopic");
-  openHelpLink(helpTopic);
 }
