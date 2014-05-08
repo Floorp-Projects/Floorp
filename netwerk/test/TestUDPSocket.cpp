940010eacd794b2c02d560cc1e2b36037b3120dc
Neil Rashbrook <neil@parkwaycc.co.uk>
2014-05-08 20:08:38 +0100

    Bug 514280 Only use nsCOMPtr for interfaces r=bsmedberg

--- netwerk/test/TestUDPSocket.cpp
+++ netwerk/test/TestUDPSocket.cpp
@@ -322,7 +322,7 @@ main(int32_t argc, char *argv[])
   if (NS_WARN_IF(!timer)) {
     return -1;
   }
-  nsCOMPtr<MulticastTimerCallback> timerCb = new MulticastTimerCallback();
+  nsRefPtr<MulticastTimerCallback> timerCb = new MulticastTimerCallback();
 
   // The following multicast tests using multiple sockets require a firewall
   // exception on Windows XP before they pass.  For now, we'll skip them here.
