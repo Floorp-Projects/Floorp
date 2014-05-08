e9d0bcc63cb93c42fdc7c58bfa6b49d886b995f2
Wes Kocher <wkocher@mozilla.com>
2014-05-08 16:06:40 -0700

    Backed out changeset 21d743a319c6 (bug 967475) for apparently introducing two hazards to the build

--- dom/src/notification/NotificationStorage.js
+++ dom/src/notification/NotificationStorage.js
@@ -36,7 +36,7 @@ function NotificationStorage() {
 
 NotificationStorage.prototype = {
 
-  put: function(origin, id, title, dir, lang, body, tag, icon, alertName) {
+  put: function(origin, id, title, dir, lang, body, tag, icon) {
     if (DEBUG) { debug("PUT: " + id + ": " + title); }
     var notification = {
       id: id,
@@ -45,9 +45,7 @@ NotificationStorage.prototype = {
       lang: lang,
       body: body,
       tag: tag,
-      icon: icon,
-      alertName: alertName,
-      timestamp: new Date().getTime()
+      icon: icon
     };
 
     this._notifications[id] = notification;
