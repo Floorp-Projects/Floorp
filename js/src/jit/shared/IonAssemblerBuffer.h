cb1e92505498427a875b07e86039af1512fc9aba
Marty Rosenberg <mrosenberg@mozilla.com>
2014-05-08 14:18:00 -0700

    Bug 969012 - Not only flag an oom, but also, don't continue running the current function; r=terrence

--- js/src/jit/shared/IonAssemblerBuffer.h
+++ js/src/jit/shared/IonAssemblerBuffer.h
@@ -245,8 +245,10 @@ struct AssemblerBuffer
     // Break the instruction stream so we can go back and edit it at this point
     void perforate() {
         Slice *tmp = newSlice(LifoAlloc_);
-        if (!tmp)
+        if (!tmp) {
             m_oom = true;
+            return;
+        }
         bufferSize += tail->size();
         tail->setNext(tmp);
         tail = tmp;
