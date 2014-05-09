5951e1a76d1cf502f5a610a210700d839f6121fe
Randell Jesup <rjesup@jesup.org>
2014-05-09 14:40:32 -0400

    Bug 981780 - Fix --disable-webrtc. r=glandium, a=sledru

---
+++ media/libyuv/build/mac/find_sdk.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import sys
+import subprocess
+
+def main():
+  return subprocess.call([sys.executable, "../webrtc/trunk/build/mac/find_sdk.py"] + sys.argv[1:])
+
+if __name__ == '__main__':
+  sys.exit(main())
