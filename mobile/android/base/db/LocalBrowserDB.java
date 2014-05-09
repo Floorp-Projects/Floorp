aa32361387bbf181733af9efe4ce79d2734d0147
Lucas Rocha <lucasr@mozilla.com>
2014-05-09 16:34:54 +0100

    Bug 997782 - Only include visited pages in the top sites query (r=wesj)

--- mobile/android/base/db/LocalBrowserDB.java
+++ mobile/android/base/db/LocalBrowserDB.java
@@ -240,8 +240,10 @@ public class LocalBrowserDB implements BrowserDB.BrowserDBIface {
 
     @Override
     public Cursor getTopSites(ContentResolver cr, int limit) {
-        // Filter out bookmarks that don't have real parents (e.g. pinned sites or reading list items)
-        String selection = DBUtils.concatenateWhere("", Combined.URL + " NOT IN (SELECT " +
+        // Filter out unvisited bookmarks and the ones that don't have real
+        // parents (e.g. pinned sites or reading list items).
+        String selection = DBUtils.concatenateWhere(Combined.HISTORY_ID + " <> -1",
+                                             Combined.URL + " NOT IN (SELECT " +
                                              Bookmarks.URL + " FROM bookmarks WHERE " +
                                              DBUtils.qualifyColumn("bookmarks", Bookmarks.PARENT) + " < ? AND " +
                                              DBUtils.qualifyColumn("bookmarks", Bookmarks.IS_DELETED) + " == 0)");
