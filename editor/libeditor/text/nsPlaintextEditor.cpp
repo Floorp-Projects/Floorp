62d4061c1d5122dcfad42f945b504db5521ab5fe
Ehsan Akhgari <ehsan@mozilla.com>
2014-05-09 17:02:29 -0400

    Bug 1007940 - Improve the performance of editor initialization; r=bzbarsky a=sylvestre
    --HG--
    extra : transplant_source : %D6l%FA%BE8%E5%0E%5E%A5%E5Sj%C1D%9A%20u%DC%09%BE

--- editor/libeditor/text/nsPlaintextEditor.cpp
+++ editor/libeditor/text/nsPlaintextEditor.cpp
@@ -123,7 +123,6 @@ NS_IMETHODIMP nsPlaintextEditor::Init(nsIDOMDocument *aDoc,
   nsresult res = NS_OK, rulesRes = NS_OK;
   if (mRules) {
     mRules->DetachEditor();
-    mRules = nullptr;
   }
   
   if (1)
@@ -316,9 +315,10 @@ nsPlaintextEditor::UpdateMetaCharset(nsIDOMDocument* aDocument,
 
 NS_IMETHODIMP nsPlaintextEditor::InitRules()
 {
-  MOZ_ASSERT(!mRules);
-  // instantiate the rules for this text editor
-  mRules = new nsTextEditRules();
+  if (!mRules) {
+    // instantiate the rules for this text editor
+    mRules = new nsTextEditRules();
+  }
   return mRules->Init(this);
 }
 
