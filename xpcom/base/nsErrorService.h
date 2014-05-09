a0a597323925d86bcc0ea6f9cc6fbf6a23416bb4
Andrew McCreight <continuation@gmail.com>
2014-05-09 13:47:36 -0700

    Bug 972605, part 1 - Rename nsErrorService arguments to aFoo form. r=froydnj

--- xpcom/base/nsErrorService.h
+++ xpcom/base/nsErrorService.h
@@ -17,9 +17,9 @@ class nsInt2StrHashtable
 public:
   nsInt2StrHashtable();
 
-  nsresult  Put(uint32_t key, const char* aData);
-  char*     Get(uint32_t key);
-  nsresult  Remove(uint32_t key);
+  nsresult  Put(uint32_t aKey, const char* aData);
+  char*     Get(uint32_t aKey);
+  nsresult  Remove(uint32_t aKey);
 
 protected:
   nsObjectHashtable mHashtable;
@@ -34,7 +34,7 @@ public:
   nsErrorService() {}
 
   static nsresult
-  Create(nsISupports* outer, const nsIID& aIID, void* *aInstancePtr);
+  Create(nsISupports* aOuter, const nsIID& aIID, void** aInstancePtr);
 
 private:
   ~nsErrorService() {}
