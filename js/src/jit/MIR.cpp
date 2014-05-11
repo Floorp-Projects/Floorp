28de1d7f12ebff5fccfa308f0cebea3236f0a731
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2014-05-11 16:27:12 -0700

    Bug 1005532 - IonMonkey: Recover MNewObject. r=sstangl

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -477,6 +477,13 @@ MConstant::NewAsmJS(TempAllocator &alloc, const Value &v, MIRType type)
     return constant;
 }
 
+MConstant *
+MConstant::NewConstraintlessObject(TempAllocator &alloc, JSObject *v)
+{
+    return new(alloc) MConstant(v);
+}
+
+
 types::TemporaryTypeSet *
 jit::MakeSingletonTypeSet(types::CompilerConstraintList *constraints, JSObject *obj)
 {
@@ -501,6 +508,13 @@ MConstant::MConstant(const js::Value &vp, types::CompilerConstraintList *constra
         setResultTypeSet(MakeSingletonTypeSet(constraints, &vp.toObject()));
     }
 
+    setMovable();
+}
+
+MConstant::MConstant(JSObject *obj)
+  : value_(ObjectValue(*obj))
+{
+    setResultType(MIRType_Object);
     setMovable();
 }
 
@@ -2765,8 +2779,8 @@ MBeta::printOpcode(FILE *fp) const
 bool
 MNewObject::shouldUseVM() const
 {
-    return templateObject()->hasSingletonType() ||
-           templateObject()->hasDynamicSlots();
+    JSObject *obj = templateObject();
+    return obj->hasSingletonType() || obj->hasDynamicSlots();
 }
 
 bool
