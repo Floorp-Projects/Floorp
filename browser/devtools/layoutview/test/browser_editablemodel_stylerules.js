efa9eddd0c35e87cdce401b300e863a9943d36ad
Patrick Brosset <pbrosset@mozilla.com>
2014-05-12 16:51:10 +0200

    Bug 997198 - Quick layout-view tests cleanup; r=bgrins

--- browser/devtools/layoutview/test/browser_editablemodel_stylerules.js
+++ browser/devtools/layoutview/test/browser_editablemodel_stylerules.js
@@ -1,105 +1,107 @@
+/* vim: set ts=2 et sw=2 tw=80: */
 /* Any copyright is dedicated to the Public Domain.
-   http://creativecommons.org/publicdomain/zero/1.0/ */
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test that units are displayed correctly when editing values in the box model
+// and that values are retrieved and parsed correctly from the back-end
+
+const TEST_URI = "<style>" +
+  "div { margin: 10px; padding: 3px }" +
+  "#div1 { margin-top: 5px }" +
+  "#div2 { border-bottom: 1em solid black; }" +
+  "#div3 { padding: 2em; }" +
+  "</style>" +
+  "<div id='div1'></div><div id='div2'></div><div id='div3'></div>";
 
 function getStyle(node, property) {
   return node.style.getPropertyValue(property);
 }
 
-let doc;
-let inspector;
-
 let test = asyncTest(function*() {
-  let style = "div { margin: 10px; padding: 3px } #div1 { margin-top: 5px } #div2 { border-bottom: 1em solid black; } #div3 { padding: 2em; }";
-  let html = "<style>" + style + "</style><div id='div1'></div><div id='div2'></div><div id='div3'></div>"
+  yield addTab("data:text/html," + encodeURIComponent(TEST_URI));
+  let {toolbox, inspector, view} = yield openLayoutView();
 
-  let content = yield loadTab("data:text/html," + encodeURIComponent(html));
-  doc = content.document;
-
-  let target = TargetFactory.forTab(gBrowser.selectedTab);
-  let toolbox = yield gDevTools.showToolbox(target, "inspector");
-  inspector = toolbox.getCurrentPanel();
-
-  inspector.sidebar.select("layoutview");
-  yield inspector.sidebar.once("layoutview-ready");
-  yield runTests();
+  yield runTests(inspector, view);
   // TODO: Closing the toolbox in this test leaks - bug 994314
-  // yield gDevTools.closeToolbox(target);
+  // yield destroyToolbox(inspector);
 });
 
 addTest("Test that entering units works",
-function*() {
-  let node = doc.getElementById("div1");
+function*(inspector, view) {
+  let node = content.document.getElementById("div1");
   is(getStyle(node, "padding-top"), "", "Should have the right padding");
-  let view = yield selectNode(node);
+  yield selectNode(node, inspector);
 
-  let span = view.document.querySelector(".padding.top > span");
+  let span = view.doc.querySelector(".padding.top > span");
   is(span.textContent, 3, "Should have the right value in the box model.");
 
-  EventUtils.synthesizeMouseAtCenter(span, {}, view);
-  let editor = view.document.querySelector(".styleinspector-propertyeditor");
+  EventUtils.synthesizeMouseAtCenter(span, {}, view.doc.defaultView);
+  let editor = view.doc.querySelector(".styleinspector-propertyeditor");
   ok(editor, "Should have opened the editor.");
   is(editor.value, "3px", "Should have the right value in the editor.");
 
-  EventUtils.synthesizeKey("1", {}, view);
-  yield waitForUpdate();
-  EventUtils.synthesizeKey("e", {}, view);
-  yield waitForUpdate();
+  EventUtils.synthesizeKey("1", {}, view.doc.defaultView);
+  yield waitForUpdate(inspector);
+  EventUtils.synthesizeKey("e", {}, view.doc.defaultView);
+  yield waitForUpdate(inspector);
 
   is(getStyle(node, "padding-top"), "", "An invalid value is handled cleanly");
 
-  EventUtils.synthesizeKey("m", {}, view);
-  yield waitForUpdate();
+  EventUtils.synthesizeKey("m", {}, view.doc.defaultView);
+  yield waitForUpdate(inspector);
 
   is(editor.value, "1em", "Should have the right value in the editor.");
   is(getStyle(node, "padding-top"), "1em", "Should have updated the padding.");
 
-  EventUtils.synthesizeKey("VK_RETURN", {}, view);
+  EventUtils.synthesizeKey("VK_RETURN", {}, view.doc.defaultView);
 
   is(getStyle(node, "padding-top"), "1em", "Should be the right padding.")
   is(span.textContent, 16, "Should have the right value in the box model.");
 });
 
 addTest("Test that we pick up the value from a higher style rule",
-function*() {
-  let node = doc.getElementById("div2");
+function*(inspector, view) {
+  let node = content.document.getElementById("div2");
   is(getStyle(node, "border-bottom-width"), "", "Should have the right border-bottom-width");
-  let view = yield selectNode(node);
+  yield selectNode(node, inspector);
 
-  let span = view.document.querySelector(".border.bottom > span");
+  let span = view.doc.querySelector(".border.bottom > span");
   is(span.textContent, 16, "Should have the right value in the box model.");
 
-  EventUtils.synthesizeMouseAtCenter(span, {}, view);
-  let editor = view.document.querySelector(".styleinspector-propertyeditor");
+  EventUtils.synthesizeMouseAtCenter(span, {}, view.doc.defaultView);
+  let editor = view.doc.querySelector(".styleinspector-propertyeditor");
   ok(editor, "Should have opened the editor.");
   is(editor.value, "1em", "Should have the right value in the editor.");
 
-  EventUtils.synthesizeKey("0", {}, view);
-  yield waitForUpdate();
+  EventUtils.synthesizeKey("0", {}, view.doc.defaultView);
+  yield waitForUpdate(inspector);
 
   is(editor.value, "0", "Should have the right value in the editor.");
   is(getStyle(node, "border-bottom-width"), "0px", "Should have updated the border.");
 
-  EventUtils.synthesizeKey("VK_RETURN", {}, view);
+  EventUtils.synthesizeKey("VK_RETURN", {}, view.doc.defaultView);
 
   is(getStyle(node, "border-bottom-width"), "0px", "Should be the right border-bottom-width.")
   is(span.textContent, 0, "Should have the right value in the box model.");
 });
 
 addTest("Test that shorthand properties are parsed correctly",
-function*() {
-  let node = doc.getElementById("div3");
+function*(inspector, view) {
+  let node = content.document.getElementById("div3");
   is(getStyle(node, "padding-right"), "", "Should have the right padding");
-  let view = yield selectNode(node);
+  yield selectNode(node, inspector);
 
-  let span = view.document.querySelector(".padding.right > span");
+  let span = view.doc.querySelector(".padding.right > span");
   is(span.textContent, 32, "Should have the right value in the box model.");
 
-  EventUtils.synthesizeMouseAtCenter(span, {}, view);
-  let editor = view.document.querySelector(".styleinspector-propertyeditor");
+  EventUtils.synthesizeMouseAtCenter(span, {}, view.doc.defaultView);
+  let editor = view.doc.querySelector(".styleinspector-propertyeditor");
   ok(editor, "Should have opened the editor.");
   is(editor.value, "2em", "Should have the right value in the editor.");
 
-  EventUtils.synthesizeKey("VK_RETURN", {}, view);
+  EventUtils.synthesizeKey("VK_RETURN", {}, view.doc.defaultView);
 
   is(getStyle(node, "padding-right"), "", "Should be the right padding.")
   is(span.textContent, 32, "Should have the right value in the box model.");
