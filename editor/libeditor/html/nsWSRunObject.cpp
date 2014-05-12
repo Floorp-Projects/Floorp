2f2c67caa7b7167d1485845538461ec78f579bab
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-05-12 14:12:53 +0200

    Backed out changeset ae46dc18288d (bug 1004522)

--- editor/libeditor/html/nsWSRunObject.cpp
+++ editor/libeditor/html/nsWSRunObject.cpp
@@ -155,18 +155,19 @@ nsWSRunObject::PrepareToDeleteRange(nsHTMLEditor* aHTMLEd,
   return leftWSObj.PrepareToDeleteRangePriv(&rightWSObj);
 }
 
-nsresult
-nsWSRunObject::PrepareToDeleteNode(nsHTMLEditor* aHTMLEd,
-                                   nsIContent* aContent)
+nsresult 
+nsWSRunObject::PrepareToDeleteNode(nsHTMLEditor *aHTMLEd, 
+                                   nsIDOMNode *aNode)
 {
-  NS_ENSURE_TRUE(aContent && aHTMLEd, NS_ERROR_NULL_POINTER);
-
-  nsCOMPtr<nsINode> parent = aContent->GetParentNode();
+  nsCOMPtr<nsINode> node(do_QueryInterface(aNode));
+  NS_ENSURE_TRUE(node && aHTMLEd, NS_ERROR_NULL_POINTER);
+  
+  nsCOMPtr<nsINode> parent = node->GetParentNode();
   NS_ENSURE_STATE(parent);
-  int32_t offset = parent->IndexOf(aContent);
+  int32_t offset = parent->IndexOf(node);
 
   nsWSRunObject leftWSObj(aHTMLEd, parent, offset);
-  nsWSRunObject rightWSObj(aHTMLEd, parent, offset + 1);
+  nsWSRunObject rightWSObj(aHTMLEd, parent, offset+1);
 
   return leftWSObj.PrepareToDeleteRangePriv(&rightWSObj);
 }
