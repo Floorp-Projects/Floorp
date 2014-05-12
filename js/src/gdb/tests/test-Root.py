810de18382a6be18da1c19e1974d935fcb55b7d7
Ryan VanderMeulen <ryanvm@gmail.com>
2014-05-12 14:40:48 -0400

    Backed out changeset 025b1ec330bc (bug 1007862) for bustage on a CLOSED TREE.

--- js/src/gdb/tests/test-Root.py
+++ js/src/gdb/tests/test-Root.py
@@ -3,7 +3,8 @@
 assert_subprinter_registered('SpiderMonkey', 'instantiations-of-JS::Rooted')
 assert_subprinter_registered('SpiderMonkey', 'instantiations-of-JS::Handle')
 assert_subprinter_registered('SpiderMonkey', 'instantiations-of-JS::MutableHandle')
-assert_subprinter_registered('SpiderMonkey', 'instantiations-of-js::BarrieredBase')
+assert_subprinter_registered('SpiderMonkey', 'instantiations-of-js::PreBarriered')
+assert_subprinter_registered('SpiderMonkey', 'js::PreBarrieredValue')
 
 run_fragment('Root.handle')
 
@@ -16,12 +17,3 @@ run_fragment('Root.HeapSlot')
 # not a public type, I'm not sure how to avoid doing *something* ugly.
 assert_pretty('array->elements[0]', '$jsval("plinth")')
 
-run_fragment('Root.barriers');
-
-assert_pretty('prebarriered', '(JSObject *)  [object Object]');
-assert_pretty('heapptr', '(JSObject *)  [object Object]');
-assert_pretty('relocatable', '(JSObject *)  [object Object]');
-assert_pretty('val', '$jsval((JSObject *)  [object Object])');
-assert_pretty('heapValue', '$jsval((JSObject *)  [object Object])');
-assert_pretty('prebarrieredValue', '$jsval((JSObject *)  [object Object])');
-assert_pretty('relocatableValue', '$jsval((JSObject *)  [object Object])');
