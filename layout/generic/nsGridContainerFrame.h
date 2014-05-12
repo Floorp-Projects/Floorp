31507136d631a6bc5fc820b584e8ff35888bf2a0
Mats Palmgren <matspal@gmail.com>
2014-05-12 21:16:05 +0000

    Bug 1008969 - part 0, make nsGridContainerFrame final for now, and remove some unnecessary boilerplate code, etc.  r=dholbert

--- layout/generic/nsGridContainerFrame.h
+++ layout/generic/nsGridContainerFrame.h
@@ -1,43 +1,41 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set ts=2 et sw=2 tw=80: */
-
 /* This Source Code is subject to the terms of the Mozilla Public License
  * version 2.0 (the "License"). You can obtain a copy of the License at
  * http://mozilla.org/MPL/2.0/. */
 
-/* rendering object for CSS "display: grid" */
+/* rendering object for CSS "display: grid | inline-grid" */
 
 #ifndef nsGridContainerFrame_h___
 #define nsGridContainerFrame_h___
 
 #include "nsContainerFrame.h"
 
+/**
+ * Factory function.
+ * @return a newly allocated nsGridContainerFrame (infallible)
+ */
 nsIFrame* NS_NewGridContainerFrame(nsIPresShell* aPresShell,
                                    nsStyleContext* aContext);
 
-typedef nsContainerFrame nsGridContainerFrameSuper;
-
-class nsGridContainerFrame : public nsGridContainerFrameSuper {
-
+class nsGridContainerFrame MOZ_FINAL : public nsContainerFrame
+{
+public:
   NS_DECL_FRAMEARENA_HELPERS
   NS_DECL_QUERYFRAME_TARGET(nsGridContainerFrame)
   NS_DECL_QUERYFRAME
 
-  // Factory method:
-  friend nsIFrame* NS_NewGridContainerFrame(nsIPresShell* aPresShell,
-                                            nsStyleContext* aContext);
-
-public:
   // nsIFrame overrides
   virtual nsIAtom* GetType() const MOZ_OVERRIDE;
+
 #ifdef DEBUG_FRAME_DUMP
   virtual nsresult GetFrameName(nsAString& aResult) const MOZ_OVERRIDE;
 #endif
 
 protected:
-  // Protected constructor & destructor
-  nsGridContainerFrame(nsStyleContext* aContext) : nsGridContainerFrameSuper(aContext) {}
-  virtual ~nsGridContainerFrame();
+  friend nsIFrame* NS_NewGridContainerFrame(nsIPresShell* aPresShell,
+                                            nsStyleContext* aContext);
+  nsGridContainerFrame(nsStyleContext* aContext) : nsContainerFrame(aContext) {}
 };
 
 #endif /* nsGridContainerFrame_h___ */
