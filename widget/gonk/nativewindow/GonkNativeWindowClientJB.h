cf2dda853606f5eebcb2e8851252c488ac3535e4
Robert O'Callahan <robert@ocallahan.org>
2014-05-11 23:37:14 -0700

    Bug 1001320. Part 5: Use #pragma GCC visibility on B2G. r=glandium

--- widget/gonk/nativewindow/GonkNativeWindowClientJB.h
+++ widget/gonk/nativewindow/GonkNativeWindowClientJB.h
@@ -31,9 +31,10 @@
 #include <utils/threads.h>
 #include <utils/KeyedVector.h>
 
+#include "mozilla/Types.h"
 #include "GonkBufferQueue.h"
 
-struct ANativeWindow_Buffer;
+struct MOZ_EXPORT ANativeWindow_Buffer;
 
 namespace android {
 
