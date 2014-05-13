a464b09dd1337b744eb3bec09e10a788ce6fbc98
Paul Adenot <paul@paul.cx>
2014-05-13 14:09:44 +0200

    Bug 1007223. r=ehsan, a=lsblakk

--- content/media/webaudio/MediaBufferDecoder.h
+++ content/media/webaudio/MediaBufferDecoder.h
@@ -32,7 +32,6 @@ struct WebAudioDecodeJob MOZ_FINAL
   // The callbacks are only necessary for asynchronous operation.
   WebAudioDecodeJob(const nsACString& aContentType,
                     dom::AudioContext* aContext,
-                    const dom::ArrayBuffer& aBuffer,
                     dom::DecodeSuccessCallback* aSuccessCallback = nullptr,
                     dom::DecodeErrorCallback* aFailureCallback = nullptr);
   ~WebAudioDecodeJob();
@@ -58,7 +57,6 @@ struct WebAudioDecodeJob MOZ_FINAL
 
   size_t SizeOfExcludingThis(mozilla::MallocSizeOf aMallocSizeOf) const;
 
-  JS::Heap<JSObject*> mArrayBuffer;
   nsCString mContentType;
   uint32_t mWriteIndex;
   nsRefPtr<dom::AudioContext> mContext;
