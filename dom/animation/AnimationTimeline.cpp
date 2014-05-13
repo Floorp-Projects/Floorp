e004208ce0c72a092dbf8d2e0582a7496fb29f7f
Brian Birtles <birtles@gmail.com>
2014-05-13 16:22:12 +0900

    Bug 998246 part 2 - Add AnimationTimeline interface; r=bz, r=glandium

---
+++ dom/animation/AnimationTimeline.cpp
@@ -0,0 +1,25 @@
+/* vim: set shiftwidth=2 tabstop=8 autoindent cindent expandtab: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "AnimationTimeline.h"
+#include "mozilla/dom/AnimationTimelineBinding.h"
+#include "nsContentUtils.h"
+
+namespace mozilla {
+namespace dom {
+
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(AnimationTimeline, mDocument)
+
+NS_IMPL_CYCLE_COLLECTION_ROOT_NATIVE(AnimationTimeline, AddRef)
+NS_IMPL_CYCLE_COLLECTION_UNROOT_NATIVE(AnimationTimeline, Release)
+
+JSObject*
+AnimationTimeline::WrapObject(JSContext* aCx)
+{
+  return AnimationTimelineBinding::Wrap(aCx, this);
+}
+
+} // namespace dom
+} // namespace mozilla
