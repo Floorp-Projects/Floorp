489653439747a8b58db6ab51a25303101468d0d9
Inanc Seylan <acimilan@yahoo.com>
2014-05-13 12:12:28 -0700

    Bug 1003802 - Add recover functionality for BitNot; r=nbp

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -3338,6 +3338,11 @@ class MBitNot
         return AliasSet::None();
     }
     void computeRange(TempAllocator &alloc);
+
+    bool writeRecoverData(CompactBufferWriter &writer) const;
+    bool canRecoverOnBailout() const {
+        return specialization_ != MIRType_None;
+    }
 };
 
 class MTypeOf
