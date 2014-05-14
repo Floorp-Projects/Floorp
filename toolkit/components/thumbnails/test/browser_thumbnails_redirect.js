41ad4c4c94ee65838d8b05c4914e51396f2c0036
Mark Hammond <mhammond@skippinet.com.au>
2014-05-14 11:09:31 +1000

    Bug 916497 - PageThumbs no longer expires thumbnails being tested to fix intermittent oranges. r=adw, a=test-only

--- toolkit/components/thumbnails/test/browser_thumbnails_redirect.js
+++ toolkit/components/thumbnails/test/browser_thumbnails_redirect.js
@@ -3,11 +3,16 @@
 
 const URL = "http://mochi.test:8888/browser/toolkit/components/thumbnails/" +
             "test/background_red_redirect.sjs";
+// loading URL will redirect us to...
+const FINAL_URL = "http://mochi.test:8888/browser/toolkit/components/" +
+                  "thumbnails/test/background_red.html";
 
 /**
  * These tests ensure that we save and provide thumbnails for redirecting sites.
  */
 function runTests() {
+  dontExpireThumbnailURLs([URL, FINAL_URL]);
+
   // Kick off history by loading a tab first or the test fails in single mode.
   yield addTab(URL);
   gBrowser.removeTab(gBrowser.selectedTab);
