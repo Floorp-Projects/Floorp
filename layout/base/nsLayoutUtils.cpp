0fa6987e714746a65b4f8cdd57dfd051e3877f97
Ed Morley <emorley@mozilla.com>
2014-05-14 15:55:04 +0100

    Backed out changeset 83fd85b082d1 (bug 1009679) for crashtest and reftest failures; CLOSED TREE

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -1561,10 +1561,12 @@ GetAnimatedGeometryRootForFrame(nsIFrame* aFrame,
     if (!parent)
       break;
     nsIAtom* parentType = parent->GetType();
+#ifdef ANDROID
     // Treat the slider thumb as being as an active scrolled root
-    // so that it can move without repainting.
+    // on mobile so that it can move without repainting.
     if (parentType == nsGkAtoms::sliderFrame)
       break;
+#endif
     // Sticky frames are active if their nearest scrollable frame
     // is also active, just keep a record of sticky frames that we
     // encounter for now.
