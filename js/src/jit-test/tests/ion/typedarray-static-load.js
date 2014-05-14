515a5a297ca2422b4efa34c82fc971842c3c0828
Jeff Walden <jwalden@mit.edu>
2014-05-14 10:31:43 -0700

    Bug 1009952 - Tests for {g,s}etElemTryTypedStatic failing to watch out for neutering.  r=sfink

---
+++ js/src/jit-test/tests/ion/typedarray-static-load.js
@@ -0,0 +1,12 @@
+var buffer = new ArrayBuffer(512 * 1024);
+var ta = new Uint8Array(buffer);
+
+function load() { return ta[0x1234]; }
+
+load();
+load();
+load();
+
+neuter(buffer, "change-data");
+
+load();
