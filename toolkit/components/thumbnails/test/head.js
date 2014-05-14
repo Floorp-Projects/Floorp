7167b83bc869fcfb5407b80f7264d4cb31a888a2
Mark Hammond <mhammond@skippinet.com.au>
2014-05-14 11:09:31 +1000

    Bug 916497 - PageThumbs no longer expires thumbnails being tested to fix intermittent oranges. r=adw, a=test-only

--- toolkit/components/thumbnails/test/head.js
+++ toolkit/components/thumbnails/test/head.js
@@ -121,6 +121,9 @@ function whenLoaded(aElement, aCallback = next) {
  */
 function captureAndCheckColor(aRed, aGreen, aBlue, aMessage) {
   let browser = gBrowser.selectedBrowser;
+  // We'll get oranges if the expiration filter removes the file during the
+  // test.
+  dontExpireThumbnailURLs([browser.currentURI.spec]);
 
   // Capture the screenshot.
   PageThumbs.captureAndStore(browser, function () {
@@ -325,6 +328,9 @@ function bgCaptureIfMissing(aURL, aOptions) {
 }
 
 function bgCaptureWithMethod(aMethodName, aURL, aOptions = {}) {
+  // We'll get oranges if the expiration filter removes the file during the
+  // test.
+  dontExpireThumbnailURLs([aURL]);
   if (!aOptions.onDone)
     aOptions.onDone = next;
   BackgroundPageThumbs[aMethodName](aURL, aOptions);
