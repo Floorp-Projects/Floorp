1834b36def40b929c9626ae0b8967e9a383524c8
Dan Gohman <sunfish@mozilla.com>
2014-05-14 21:51:57 -0700

    Bug 1004363 - IonMonkey: Replace a bug number comment with an actual explanatory comment. r=mjrosenb

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -1029,10 +1029,25 @@ MPhi::congruentTo(const MDefinition *ins) const
 {
     if (!ins->isPhi())
         return false;
-    // Since we do not know which predecessor we are merging from, we must
-    // assume that phi instructions in different blocks are not equal.
-    // (Bug 674656)
-    if (ins->block()->id() != block()->id())
+
+    // Phis in different blocks may have different control conditions.
+    // For example, these phis:
+    //
+    //   if (p)
+    //     goto a
+    //   a:
+    //     t = phi(x, y)
+    //
+    //   if (q)
+    //     goto b
+    //   b:
+    //     s = phi(x, y)
+    //
+    // have identical operands, but they are not equvalent because t is
+    // effectively p?x:y and s is effectively q?x:y.
+    //
+    // For now, consider phis in different blocks incongruent.
+    if (ins->block() != block())
         return false;
 
     return congruentIfOperandsEqual(ins);
