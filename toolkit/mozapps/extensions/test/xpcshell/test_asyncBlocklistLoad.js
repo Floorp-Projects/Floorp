196b7f6a1528d855c61c2c39a618e749079c2f02
Roberto A. Vitillo <rvitillo@mozilla.com>
2014-05-15 09:14:00 -0400

    Bug 988304 - Avoid main-thread IO for {profile}\blocklist.xml. r=irving

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_asyncBlocklistLoad.js
@@ -0,0 +1,44 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+function run_test() {
+  run_next_test();
+}
+
+add_task(function () {
+  let blocklist = AM_Cc["@mozilla.org/extensions/blocklist;1"].
+                  getService().wrappedJSObject;
+  let scope = Components.utils.import("resource://gre/modules/osfile.jsm");
+
+  // sync -> async
+  blocklist._loadBlocklist();
+  do_check_true(blocklist._isBlocklistLoaded());
+  yield blocklist._preloadBlocklist();
+  do_check_false(blocklist._isBlocklistPreloaded());
+  blocklist._clear();
+
+  // async -> sync
+  yield blocklist._preloadBlocklist();
+  do_check_false(blocklist._isBlocklistLoaded());
+  do_check_true(blocklist._isBlocklistPreloaded());
+  blocklist._loadBlocklist();
+  do_check_true(blocklist._isBlocklistLoaded());
+  do_check_false(blocklist._isBlocklistPreloaded());
+  blocklist._clear();
+
+  // async -> sync -> async
+  let read = scope.OS.File.read;
+  scope.OS.File.read = function(...args) {
+    return new Promise((resolve, reject) => {
+      do_execute_soon(() => {
+        blocklist._loadBlocklist();
+        resolve(read(...args));
+      });
+    });
+  }
+
+  yield blocklist._preloadBlocklist();
+  do_check_true(blocklist._isBlocklistLoaded());
+  do_check_false(blocklist._isBlocklistPreloaded());
+});
