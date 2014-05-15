1f9e84070807476aecd8263f4f6363b5589d4e93
Erik Vold <evold@mozilla.com>
2014-05-15 12:39:59 -0700

    Bug 1011107 - Uplift Add-on SDK to Firefox

--- addon-sdk/source/lib/sdk/content/content.js
+++ addon-sdk/source/lib/sdk/content/content.js
@@ -4,10 +4,16 @@
 "use strict";
 
 module.metadata = {
-  "stability": "unstable"
+  "stability": "deprecated"
 };
 
-exports.Loader = require('./loader').Loader;
-exports.Symbiont = require('../deprecated/symbiont').Symbiont;
-exports.Worker = require('./worker').Worker;
+const { deprecateFunction } = require('../util/deprecate');
+
+exports.Loader = deprecateFunction(require('./loader').Loader,
+  '`sdk/content/content` is deprecated. Please use `sdk/content/loader` directly.');
+exports.Symbiont = deprecateFunction(require('../deprecated/symbiont').Symbiont,
+  'Both `sdk/content/content` and `sdk/deprecated/symbiont` are deprecated. ' +
+  '`sdk/core/heritage` supersedes Symbiont for inheritance.');
+exports.Worker = deprecateFunction(require('./worker').Worker,
+  '`sdk/content/content` is deprecated. Please use `sdk/content/worker` directly.');
 
