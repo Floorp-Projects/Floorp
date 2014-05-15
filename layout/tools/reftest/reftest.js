a5bc91d972b43af023c474287e84954264662186
Benoit Jacob <bjacob@mozilla.com>
2014-05-15 11:50:16 -0400

    Bug 1004483 - Adjust WebGL reftest.list for the fact that Skia blending doesn't handle superluminant pixels - r=jrmuizel

--- layout/tools/reftest/reftest.js
+++ layout/tools/reftest/reftest.js
@@ -625,6 +625,7 @@ function BuildConditionSandbox(aURL) {
     var info = gfxInfo.getInfo();
     sandbox.azureQuartz = info.AzureCanvasBackend == "quartz";
     sandbox.azureSkia = info.AzureCanvasBackend == "skia";
+    sandbox.skiaContent = info.AzureContentBackend == "skia";
     sandbox.azureSkiaGL = info.AzureSkiaAccelerated; // FIXME: assumes GL right now
     // true if we are using the same Azure backend for rendering canvas and content
     sandbox.contentSameGfxBackendAsCanvas = info.AzureContentBackend == info.AzureCanvasBackend
