e2ffedaaeee4523a46f618826a303bcd498b0e92
Victor Porof <vporof@mozilla.com>
2014-05-15 15:28:00 -0400

    Bug 1007200 - Create a framerate actor, r=rcampbell,benwa

--- toolkit/devtools/server/main.js
+++ toolkit/devtools/server/main.js
@@ -401,6 +401,7 @@ var DebuggerServer = {
     this.registerModule("devtools/server/actors/gcli");
     this.registerModule("devtools/server/actors/tracer");
     this.registerModule("devtools/server/actors/memory");
+    this.registerModule("devtools/server/actors/framerate");
     this.registerModule("devtools/server/actors/eventlooplag");
     this.registerModule("devtools/server/actors/layout");
     if ("nsIProfiler" in Ci) {
