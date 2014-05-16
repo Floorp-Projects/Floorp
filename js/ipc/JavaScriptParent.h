4ea552c32844de5d4974d4f32ad04de99e2eac0d
Bill McCloskey <wmccloskey@mozilla.com>
2014-05-16 16:40:36 -0700

    Bug 996785 - Move CPOW wrapper owner code (r=mrbkap)

--- js/ipc/JavaScriptParent.h
+++ js/ipc/JavaScriptParent.h
@@ -8,73 +8,23 @@
 #ifndef mozilla_jsipc_JavaScriptParent__
 #define mozilla_jsipc_JavaScriptParent__
 
-#include "JavaScriptShared.h"
+#include "JavaScriptBase.h"
 #include "mozilla/jsipc/PJavaScriptParent.h"
-#include "js/Class.h"
-
-#ifdef XP_WIN
-#undef GetClassName
-#undef GetClassInfo
-#endif
 
 namespace mozilla {
 namespace jsipc {
 
-class JavaScriptParent
-  : public PJavaScriptParent,
-    public JavaScriptShared
+class JavaScriptParent : public JavaScriptBase<PJavaScriptParent>
 {
   public:
     JavaScriptParent();
 
     bool init();
 
-  public:
-    // Fundamental proxy traps. These are required.
-    // (The traps should be in the same order like js/src/jsproxy.h)
-    bool preventExtensions(JSContext *cx, JS::HandleObject proxy);
-    bool getPropertyDescriptor(JSContext *cx, JS::HandleObject proxy, JS::HandleId id,
-                               JS::MutableHandle<JSPropertyDescriptor> desc);
-    bool getOwnPropertyDescriptor(JSContext *cx, JS::HandleObject proxy, JS::HandleId id,
-                                  JS::MutableHandle<JSPropertyDescriptor> desc);
-    bool defineProperty(JSContext *cx, JS::HandleObject proxy, JS::HandleId id,
-                        JS::MutableHandle<JSPropertyDescriptor> desc);
-    bool getOwnPropertyNames(JSContext *cx, JS::HandleObject proxy, JS::AutoIdVector &props);
-    bool delete_(JSContext *cx, JS::HandleObject proxy, JS::HandleId id, bool *bp);
-    bool enumerate(JSContext *cx, JS::HandleObject proxy, JS::AutoIdVector &props);
-
-    // Derived proxy traps. Implementing these is useful for perfomance.
-    bool has(JSContext *cx, JS::HandleObject proxy, JS::HandleId id, bool *bp);
-    bool hasOwn(JSContext *cx, JS::HandleObject proxy, JS::HandleId id, bool *bp);
-    bool get(JSContext *cx, JS::HandleObject proxy, JS::HandleObject receiver,
-             JS::HandleId id, JS::MutableHandleValue vp);
-    bool set(JSContext *cx, JS::HandleObject proxy, JS::HandleObject receiver,
-             JS::HandleId id, bool strict, JS::MutableHandleValue vp);
-    bool keys(JSContext *cx, JS::HandleObject proxy, JS::AutoIdVector &props);
-    // We use "iterate" provided by the base class here.
-
-    // SpiderMonkey Extensions.
-    bool isExtensible(JSContext *cx, JS::HandleObject proxy, bool *extensible);
-    bool call(JSContext *cx, JS::HandleObject proxy, const JS::CallArgs &args);
-    bool objectClassIs(JSContext *cx, JS::HandleObject obj, js::ESClassValue classValue);
-    const char* className(JSContext *cx, JS::HandleObject proxy);
-
-    virtual void ActorDestroy(ActorDestroyReason why);
-
     void decref();
     void incref();
 
-    bool active() { return !inactive_; }
-
     void drop(JSObject *obj);
-
-    nsresult instanceOf(JSObject *obj, const nsID *id, bool *bp);
-
-    /*
-     * Check that |obj| is a DOM wrapper whose prototype chain contains
-     * |prototypeID| at depth |depth|.
-     */
-    bool domInstanceOf(JSContext *cx, JSObject *obj, int prototypeID, int depth, bool *bp);
 
     mozilla::ipc::IProtocol*
     CloneProtocol(Channel* aChannel, ProtocolCloneContext* aCtx) MOZ_OVERRIDE;
@@ -83,29 +33,9 @@ class JavaScriptParent
     JSObject *fromId(JSContext *cx, ObjectId objId);
     bool toId(JSContext *cx, JSObject *obj, ObjectId *idp);
 
-    bool getPropertyNames(JSContext *cx, JS::HandleObject proxy, uint32_t flags,
-                          JS::AutoIdVector &props);
-    ObjectId idOf(JSObject *obj);
-
-    // Catastrophic IPC failure.
-    bool ipcfail(JSContext *cx);
-
-    // Check whether a return status is okay, and if not, propagate its error.
-    bool ok(JSContext *cx, const ReturnStatus &status);
-
   private:
     uintptr_t refcount_;
-    bool inactive_;
 };
-
-bool
-IsCPOW(JSObject *obj);
-
-nsresult
-InstanceOf(JSObject *obj, const nsID *id, bool *bp);
-
-bool
-DOMInstanceOf(JSContext *cx, JSObject *obj, int prototypeID, int depth, bool *bp);
 
 } // jsipc
 } // mozilla
---
+++ js/ipc/WrapperOwner.h
@@ -0,0 +1,153 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=8 sw=4 et tw=80:
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_jsipc_WrapperOwner_h__
+#define mozilla_jsipc_WrapperOwner_h__
+
+#include "JavaScriptShared.h"
+#include "mozilla/ipc/ProtocolUtils.h"
+#include "js/Class.h"
+
+#ifdef XP_WIN
+#undef GetClassName
+#undef GetClassInfo
+#endif
+
+namespace js {
+class BaseProxyHandler;
+} // js
+
+namespace mozilla {
+namespace jsipc {
+
+class WrapperOwner : public JavaScriptShared
+{
+  public:
+    typedef mozilla::ipc::IProtocolManager<
+                       mozilla::ipc::IProtocol>::ActorDestroyReason
+           ActorDestroyReason;
+
+    WrapperOwner();
+    bool init();
+
+    // Fundamental proxy traps. These are required.
+    // (The traps should be in the same order like js/src/jsproxy.h)
+    bool preventExtensions(JSContext *cx, JS::HandleObject proxy);
+    bool getPropertyDescriptor(JSContext *cx, JS::HandleObject proxy, JS::HandleId id,
+                               JS::MutableHandle<JSPropertyDescriptor> desc);
+    bool getOwnPropertyDescriptor(JSContext *cx, JS::HandleObject proxy, JS::HandleId id,
+                                  JS::MutableHandle<JSPropertyDescriptor> desc);
+    bool defineProperty(JSContext *cx, JS::HandleObject proxy, JS::HandleId id,
+                        JS::MutableHandle<JSPropertyDescriptor> desc);
+    bool getOwnPropertyNames(JSContext *cx, JS::HandleObject proxy, JS::AutoIdVector &props);
+    bool delete_(JSContext *cx, JS::HandleObject proxy, JS::HandleId id, bool *bp);
+    bool enumerate(JSContext *cx, JS::HandleObject proxy, JS::AutoIdVector &props);
+
+    // Derived proxy traps. Implementing these is useful for perfomance.
+    bool has(JSContext *cx, JS::HandleObject proxy, JS::HandleId id, bool *bp);
+    bool hasOwn(JSContext *cx, JS::HandleObject proxy, JS::HandleId id, bool *bp);
+    bool get(JSContext *cx, JS::HandleObject proxy, JS::HandleObject receiver,
+             JS::HandleId id, JS::MutableHandleValue vp);
+    bool set(JSContext *cx, JS::HandleObject proxy, JS::HandleObject receiver,
+             JS::HandleId id, bool strict, JS::MutableHandleValue vp);
+    bool keys(JSContext *cx, JS::HandleObject proxy, JS::AutoIdVector &props);
+    // We use "iterate" provided by the base class here.
+
+    // SpiderMonkey Extensions.
+    bool isExtensible(JSContext *cx, JS::HandleObject proxy, bool *extensible);
+    bool call(JSContext *cx, JS::HandleObject proxy, const JS::CallArgs &args);
+    bool objectClassIs(JSContext *cx, JS::HandleObject obj, js::ESClassValue classValue);
+    const char* className(JSContext *cx, JS::HandleObject proxy);
+
+    nsresult instanceOf(JSObject *obj, const nsID *id, bool *bp);
+
+    /*
+     * Check that |obj| is a DOM wrapper whose prototype chain contains
+     * |prototypeID| at depth |depth|.
+     */
+    bool domInstanceOf(JSContext *cx, JSObject *obj, int prototypeID, int depth, bool *bp);
+
+    bool active() { return !inactive_; }
+
+    virtual void drop(JSObject *obj) = 0;
+
+    virtual void ActorDestroy(ActorDestroyReason why);
+
+  protected:
+    ObjectId idOf(JSObject *obj);
+
+    static js::BaseProxyHandler *ProxyHandler();
+
+  private:
+    bool getPropertyNames(JSContext *cx, JS::HandleObject proxy, uint32_t flags,
+                          JS::AutoIdVector &props);
+
+    // Catastrophic IPC failure.
+    bool ipcfail(JSContext *cx);
+
+    // Check whether a return status is okay, and if not, propagate its error.
+    bool ok(JSContext *cx, const ReturnStatus &status);
+
+    bool inactive_;
+
+    /*** Dummy call handlers ***/
+  public:
+    virtual bool CallPreventExtensions(const ObjectId &objId, ReturnStatus *rs) = 0;
+    virtual bool CallGetPropertyDescriptor(const ObjectId &objId, const nsString &id,
+                                           ReturnStatus *rs,
+                                           PPropertyDescriptor *out) = 0;
+    virtual bool CallGetOwnPropertyDescriptor(const ObjectId &objId,
+                                              const nsString &id,
+                                              ReturnStatus *rs,
+                                              PPropertyDescriptor *out) = 0;
+    virtual bool CallDefineProperty(const ObjectId &objId, const nsString &id,
+                                    const PPropertyDescriptor &flags,
+                                    ReturnStatus *rs) = 0;
+    virtual bool CallDelete(const ObjectId &objId, const nsString &id,
+                            ReturnStatus *rs, bool *success) = 0;
+
+    virtual bool CallHas(const ObjectId &objId, const nsString &id,
+                         ReturnStatus *rs, bool *bp) = 0;
+    virtual bool CallHasOwn(const ObjectId &objId, const nsString &id,
+                            ReturnStatus *rs, bool *bp) = 0;
+    virtual bool CallGet(const ObjectId &objId, const ObjectId &receiverId,
+                         const nsString &id,
+                         ReturnStatus *rs, JSVariant *result) = 0;
+    virtual bool CallSet(const ObjectId &objId, const ObjectId &receiverId,
+                         const nsString &id, const bool &strict,
+                         const JSVariant &value, ReturnStatus *rs, JSVariant *result) = 0;
+
+    virtual bool CallIsExtensible(const ObjectId &objId, ReturnStatus *rs,
+                                  bool *result) = 0;
+    virtual bool CallCall(const ObjectId &objId, const nsTArray<JSParam> &argv,
+                          ReturnStatus *rs, JSVariant *result,
+                          nsTArray<JSParam> *outparams) = 0;
+    virtual bool CallObjectClassIs(const ObjectId &objId, const uint32_t &classValue,
+                                   bool *result) = 0;
+    virtual bool CallClassName(const ObjectId &objId, nsString *result) = 0;
+
+    virtual bool CallGetPropertyNames(const ObjectId &objId, const uint32_t &flags,
+                                      ReturnStatus *rs, nsTArray<nsString> *names) = 0;
+    virtual bool CallInstanceOf(const ObjectId &objId, const JSIID &iid,
+                                ReturnStatus *rs, bool *instanceof) = 0;
+    virtual bool CallDOMInstanceOf(const ObjectId &objId, const int &prototypeID, const int &depth,
+                                   ReturnStatus *rs, bool *instanceof) = 0;
+};
+
+bool
+IsCPOW(JSObject *obj);
+
+nsresult
+InstanceOf(JSObject *obj, const nsID *id, bool *bp);
+
+bool
+DOMInstanceOf(JSContext *cx, JSObject *obj, int prototypeID, int depth, bool *bp);
+
+} // jsipc
+} // mozilla
+
+#endif // mozilla_jsipc_WrapperOwner_h__
