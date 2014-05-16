648f19233bb481caf667ba8daff523c0f1732f39
Matthew Gregan <kinetik@flim.org>
2014-05-16 17:09:28 +1200

    Bug 1010163 - Cast out warning demons on a CLOSED TREE.

--- content/media/wave/WaveReader.cpp
+++ content/media/wave/WaveReader.cpp
@@ -577,7 +577,7 @@ WaveReader::LoadListChunk(uint32_t aChunkSize,
     uint32_t length = ReadUint32LE(&p);
 
     // Subchunk shall not exceed parent chunk.
-    if (end - p < length) {
+    if (uint32_t(end - p) < length) {
       break;
     }
 
