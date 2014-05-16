e41409a512ed0774224b5b5d38ea7a4fe3062e0f
Shih-Chiang Chien <schien@mozilla.com>
2014-05-16 15:01:27 -0400

    Bug 1241368 - provide JS implemented MDNS service as a fallback. r=nwgh,nalexander.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1881,6 +1881,9 @@ pref("network.proxy.autoconfig_retry_interval_max", 300);  // 5 minutes
 // Use the HSTS preload list by default
 pref("network.stricttransportsecurity.preloadlist", true);
 
+// Use JS mDNS as a fallback
+pref("network.mdns.use_js_fallback", false);
+
 pref("converter.html2txt.structs",          true); // Output structured phrases (strong, em, code, sub, sup, b, i, u)
 pref("converter.html2txt.header_strategy",  1); // 0 = no indention; 1 = indention, increased with header level; 2 = numbering and slight indention
 // Whether we include ruby annotation in the text despite whether it
