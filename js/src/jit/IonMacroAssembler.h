61a35c214b18e6255e381ecf53f0cc99a7241bee
Jan de Mooij <jdemooij@mozilla.com>
2014-05-16 09:59:40 +0200

    Bug 1009664 - Remove duplicate fixed slot initialization in some cases. r=terrence

--- js/src/jit/IonMacroAssembler.h
+++ js/src/jit/IonMacroAssembler.h
@@ -802,17 +802,18 @@ class MacroAssembler : public MacroAssemblerSpecific
     void copySlotsFromTemplate(Register obj, const JSObject *templateObj,
                                uint32_t start, uint32_t end);
     void fillSlotsWithUndefined(Address addr, Register temp, uint32_t start, uint32_t end);
-    void initGCSlots(Register obj, Register temp, JSObject *templateObj);
+    void initGCSlots(Register obj, Register temp, JSObject *templateObj, bool initFixedSlots);
 
   public:
     void callMallocStub(size_t nbytes, Register result, Label *fail);
     void callFreeStub(Register slots);
     void createGCObject(Register result, Register temp, JSObject *templateObj,
-                        gc::InitialHeap initialHeap, Label *fail);
+                        gc::InitialHeap initialHeap, Label *fail, bool initFixedSlots = true);
 
     void newGCThing(Register result, Register temp, JSObject *templateObj,
                      gc::InitialHeap initialHeap, Label *fail);
-    void initGCThing(Register obj, Register temp, JSObject *templateObj);
+    void initGCThing(Register obj, Register temp, JSObject *templateObj,
+                     bool initFixedSlots = true);
 
     void newGCString(Register result, Register temp, Label *fail);
     void newGCFatInlineString(Register result, Register temp, Label *fail);
