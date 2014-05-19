87bd433cdaaf775b12a31a6012f8db5de7e3cf65
Phil Ringnalda <philringnalda@gmail.com>
2014-05-19 07:12:33 -0700

    Bug 1009788 followup, make the test not fail when parallelJS is off, r=jonco

--- js/src/jit-test/tests/parallel/bug1009788.js
+++ js/src/jit-test/tests/parallel/bug1009788.js
@@ -1,5 +1,7 @@
-for (var a = 0; a < 2000; a++) {
-    Array.buildPar(500, (function() {
-        return {}
-    }))
+if (getBuildConfiguration().parallelJS) {
+    for (var a = 0; a < 2000; a++) {
+        Array.buildPar(500, (function() {
+            return {}
+        }))
+    }
 }
