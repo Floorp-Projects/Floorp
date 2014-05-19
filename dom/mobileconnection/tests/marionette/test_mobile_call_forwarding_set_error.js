7e58ae111bf360f157bd0ed2b48617f2f1d46069
Edgar Chen <echen@mozilla.com>
2014-05-19 19:10:58 +0800

    Bug 861725 - Part 2: Revise the call forwarding test cases. r=vicamo

--- dom/mobileconnection/tests/marionette/test_mobile_call_forwarding_set_error.js
+++ dom/mobileconnection/tests/marionette/test_mobile_call_forwarding_set_error.js
@@ -7,22 +7,21 @@ MARIONETTE_HEAD_JS = "head.js";
 const TEST_NUMBER = "0912345678";
 const TEST_TIME_SECONDS = 5;
 const TEST_DATA = [
-  // Test get calling forwarding.
-  // TODO: Bug 861725 - B2G Emulator: support call forwarding
-  // Currently emulator doesn't support REQUEST_QUERY_CALL_FORWARD_STATUS, so
-  // we expect to get a 'RequestNotSupported' error here.
+  // Currently emulator doesn't support CALL_FORWARD_REASON_ALL_CALL_FORWARDING
+  // and CALL_FORWARD_REASON_ALL_CONDITIONAL_CALL_FORWARDING, so
+  // we expect to get a 'GenericFailure' error here.
   {
     options: {
       action: MozMobileConnection.CALL_FORWARD_ACTION_DISABLE,
-      reason: MozMobileConnection.CALL_FORWARD_REASON_UNCONDITIONAL,
+      reason: MozMobileConnection.CALL_FORWARD_REASON_ALL_CALL_FORWARDING,
     },
-    expectedErrorMsg: "RequestNotSupported"
+    errorMsg: "GenericFailure"
   }, {
     options: {
       action: MozMobileConnection.CALL_FORWARD_ACTION_ENABLE,
-      reason: MozMobileConnection.CALL_FORWARD_REASON_MOBILE_BUSY,
+      reason: MozMobileConnection.CALL_FORWARD_REASON_ALL_CONDITIONAL_CALL_FORWARDING,
     },
-    expectedErrorMsg: "RequestNotSupported"
+    errorMsg: "GenericFailure"
   },
   // Test passing invalid action. We expect to get a 'InvalidParameter' error.
   {
@@ -31,13 +30,13 @@ const TEST_DATA = [
       action: MozMobileConnection.CALL_FORWARD_ACTION_QUERY_STATUS,
       reason: MozMobileConnection.CALL_FORWARD_REASON_MOBILE_BUSY,
     },
-    expectedErrorMsg: "InvalidParameter"
+    errorMsg: "InvalidParameter"
   }, {
     options: {
       action: 10 /* Invalid action */,
       reason: MozMobileConnection.CALL_FORWARD_REASON_MOBILE_BUSY,
     },
-    expectedErrorMsg: "InvalidParameter"
+    errorMsg: "InvalidParameter"
   },
   // Test passing invalid reason. We expect to get a 'InvalidParameter' error.
   {
@@ -45,11 +44,11 @@ const TEST_DATA = [
       action: MozMobileConnection.CALL_FORWARD_ACTION_DISABLE,
       reason: 10 /*Invalid reason*/,
     },
-    expectedErrorMsg: "InvalidParameter"
+    errorMsg: "InvalidParameter"
   }
 ];
 
-function testSetCallForwardingOption(aOptions, aExpectedErrorMsg) {
+function testSetCallForwardingOption(aOptions, aErrorMsg) {
   log("Test setting call forwarding to " + JSON.stringify(aOptions));
 
   aOptions.number = TEST_NUMBER;
@@ -57,9 +56,9 @@ function testSetCallForwardingOption(aOptions, aExpectedErrorMsg) {
 
   return setCallForwardingOption(aOptions)
     .then(function resolve() {
-      ok(!aExpectedErrorMsg, "setCallForwardingOption success");
+      ok(false, "setCallForwardingOption success");
     }, function reject(aError) {
-      is(aError.name, aExpectedErrorMsg, "failed to setCallForwardingOption");
+      is(aError.name, aErrorMsg, "failed to setCallForwardingOption");
     });
 }
 
@@ -69,7 +68,7 @@ startTestCommon(function() {
   for (let i = 0; i < TEST_DATA.length; i++) {
     let data = TEST_DATA[i];
     promise = promise.then(() => testSetCallForwardingOption(data.options,
-                                                             data.expectedErrorMsg));
+                                                             data.errorMsg));
   }
   return promise;
 });
