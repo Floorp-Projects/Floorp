df74cff0c5b20d87f3052105ea6b52e3a5f319ac
Boris Zbarsky <bzbarsky@mit.edu>
2014-05-19 16:37:59 -0400

    Bug 1010658 part 1.  Stop using the DOMClass stored in DOM_PROTO_INSTANCE_CLASS_SLOT for doing type checks in the jit, and do them directly on the instance classes instead.  r=efaust

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -5399,7 +5399,7 @@ dom_constructor(JSContext* cx, unsigned argc, JS::Value *vp)
 }
 
 static bool
-InstanceClassHasProtoAtDepth(JSObject *protoObject, uint32_t protoID, uint32_t depth)
+InstanceClassHasProtoAtDepth(const Class *clasp, uint32_t protoID, uint32_t depth)
 {
     /* There's only a single (fake) DOM object in the shell, so just return true. */
     return true;
