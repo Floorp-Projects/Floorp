16f121ba19d2d6620f2f01fe4a3d21dfa81810d5
Alexandru Chiriac <alexandru.chiriac@ebs.ro>
2014-05-20 15:55:46 -0700

    Bug 997049 - Fixed "Switch to tab" functionality for Reading List entries. r=margaret

--- mobile/android/base/home/ReadingListRow.java
+++ mobile/android/base/home/ReadingListRow.java
@@ -8,7 +8,6 @@ package org.mozilla.gecko.home;
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.Tab;
 import org.mozilla.gecko.Tabs;
-import org.mozilla.gecko.AboutPages;
 import org.mozilla.gecko.home.TwoLinePageRow;
 
 import android.content.Context;
@@ -29,9 +28,9 @@ public class ReadingListRow extends TwoLinePageRow {
         String pageUrl = getUrl();
 
         boolean isPrivate = Tabs.getInstance().getSelectedTab().isPrivate();
-        Tab tab = Tabs.getInstance().getFirstTabForUrl(pageUrl, isPrivate);
+        Tab tab = Tabs.getInstance().getFirstReaderTabForUrl(pageUrl, isPrivate);
 
-        if (tab != null && AboutPages.isAboutReader(tab.getURL())) {
+        if (tab != null) {
             setUrl(R.string.switch_to_tab);
             setSwitchToTabIcon(R.drawable.ic_url_bar_tab);
         } else {
