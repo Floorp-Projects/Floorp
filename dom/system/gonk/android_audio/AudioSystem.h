8d463532ff4b86a907ee067ea48fad6f7dd4b093
Ryan VanderMeulen <ryanvm@gmail.com>
2014-05-20 12:17:51 -0400

    Backed out changeset 8433a3609a54 (bug 1001320) for suspected B2G device image bustage. CLOSED TREE

--- dom/system/gonk/android_audio/AudioSystem.h
+++ dom/system/gonk/android_audio/AudioSystem.h
@@ -17,8 +17,6 @@
 #ifndef ANDROID_AUDIOSYSTEM_H_
 #define ANDROID_AUDIOSYSTEM_H_
 
-#pragma GCC visibility push(default)
-
 #include <utils/RefBase.h>
 #include <utils/threads.h>
 #include "IAudioFlinger.h"
@@ -962,7 +960,5 @@ private:
 };
 
 };  // namespace android
-
-#pragma GCC visibility pop
 
 #endif  /*ANDROID_AUDIOSYSTEM_H_*/
