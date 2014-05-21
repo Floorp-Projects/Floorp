83932ddc68bb8eb12eb514f3f59c6b793750ac70
Paul Rouget <paul@mozilla.com>
2014-05-21 09:29:43 +0200

    Bug 1010387 - [appmgr v2] write tests and make good use of promises & tasks. r=janx r=jryans
    ---
     browser/devtools/app-manager/app-validator.js    |   2 +-
     browser/devtools/webide/content/newapp.js        |  30 +++-
     browser/devtools/webide/content/webide.js        | 180 +++++++++++++++--------
     browser/devtools/webide/modules/app-manager.js   |  27 +++-
     browser/devtools/webide/moz.build                |   1 +
     browser/devtools/webide/test/app.zip             | Bin 0 -> 480 bytes
     browser/devtools/webide/test/app/index.html      |   6 +
     browser/devtools/webide/test/app/manifest.webapp |   5 +
     browser/devtools/webide/test/chrome.ini          |  13 ++
     browser/devtools/webide/test/head.js             |  81 ++++++++++
     browser/devtools/webide/test/hosted_app.manifest |   3 +
     browser/devtools/webide/test/templates.json      |  14 ++
     browser/devtools/webide/test/test_basic.html     |  39 +++++
     browser/devtools/webide/test/test_import.html    |  65 ++++++++
     browser/devtools/webide/test/test_newapp.html    |  48 ++++++
     browser/devtools/webide/test/test_runtime.html   | 118 +++++++++++++++
     browser/devtools/webide/themes/details.css       |   1 -
     browser/devtools/webide/themes/newapp.css        |   4 -
     18 files changed, 558 insertions(+), 79 deletions(-)
     create mode 100644 browser/devtools/webide/test/app.zip
     create mode 100644 browser/devtools/webide/test/app/index.html
     create mode 100644 browser/devtools/webide/test/app/manifest.webapp
     create mode 100644 browser/devtools/webide/test/chrome.ini
     create mode 100644 browser/devtools/webide/test/head.js
     create mode 100644 browser/devtools/webide/test/hosted_app.manifest
     create mode 100644 browser/devtools/webide/test/templates.json
     create mode 100644 browser/devtools/webide/test/test_basic.html
     create mode 100644 browser/devtools/webide/test/test_import.html
     create mode 100644 browser/devtools/webide/test/test_newapp.html
     create mode 100644 browser/devtools/webide/test/test_runtime.html

---
+++ browser/devtools/webide/test/head.js
@@ -0,0 +1,81 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const {utils: Cu, classes: Cc, interfaces: Ci} = Components;
+
+Cu.import('resource://gre/modules/Services.jsm');
+Cu.import("resource://gre/modules/FileUtils.jsm");
+Cu.import("resource://gre/modules/Task.jsm");
+
+const {Promise: promise} = Cu.import("resource://gre/modules/devtools/deprecated-sync-thenables.js", {});
+const {devtools} = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
+const {require} = devtools;
+const {AppProjects} = require("devtools/app-manager/app-projects");
+
+const TEST_BASE = "chrome://mochitests/content/chrome/browser/devtools/webide/test/";
+
+Services.prefs.setBoolPref("devtools.webide.enabled", true);
+
+SimpleTest.registerCleanupFunction(() => {
+  Services.prefs.clearUserPref("devtools.webide.enabled");
+});
+
+function openWebIDE() {
+  info("opening WebIDE");
+
+  let deferred = promise.defer();
+
+  let ww = Cc["@mozilla.org/embedcomp/window-watcher;1"].getService(Ci.nsIWindowWatcher);
+  let win = ww.openWindow(null, "chrome://webide/content/", "webide", "chrome,centerscreen,resizable", null);
+
+  win.addEventListener("load", function onLoad() {
+    win.removeEventListener("load", onLoad);
+    info("WebIDE open");
+    SimpleTest.executeSoon(() => {
+      deferred.resolve(win);
+    });
+  });
+
+  return deferred.promise;
+}
+
+function closeWebIDE(win) {
+  info("Closing WebIDE");
+
+  let deferred = promise.defer();
+
+  win.addEventListener("unload", function onUnload() {
+    win.removeEventListener("unload", onUnload);
+    info("WebIDE closed");
+    SimpleTest.executeSoon(() => {
+      deferred.resolve();
+    });
+  });
+
+  win.close();
+
+  return deferred.promise;
+}
+
+function removeAllProjects() {
+  let deferred = promise.defer();
+  AppProjects.load().then(() => {
+    let projects = AppProjects.store.object.projects;
+    for (let i = 0; i < projects.length; i++) {
+      AppProjects.remove(projects[i].location);
+    }
+    deferred.resolve();
+  });
+
+  return deferred.promise;
+}
+function nextTick() {
+  let deferred = promise.defer();
+  SimpleTest.executeSoon(() => {
+    deferred.resolve();
+  });
+
+  return deferred.promise;
+}
