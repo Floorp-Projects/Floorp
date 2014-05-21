57a3e46be9ca2d1e669fe28751d85f18c2a1327d
Randell Jesup <rjesup@jesup.org>
2014-05-21 03:28:22 -0400

    Bug 921622 - AudioStream rework. r=padenot

--- content/media/AudioStream.h
+++ content/media/AudioStream.h
@@ -45,13 +45,17 @@ public:
   void UpdateWritePosition(uint32_t aCount);
   // Get the read position of the stream, in microseconds.
   // Called on the state machine thead.
-  uint64_t GetPosition();
+  // Assumes the AudioStream lock is held and thus calls Unlocked versions
+  // of AudioStream funcs.
+  uint64_t GetPositionUnlocked();
   // Get the read position of the stream, in frames.
   // Called on the state machine thead.
   uint64_t GetPositionInFrames();
   // Set the playback rate.
   // Called on the audio thread.
-  void SetPlaybackRate(double aPlaybackRate);
+  // Assumes the AudioStream lock is held and thus calls Unlocked versions
+  // of AudioStream funcs.
+  void SetPlaybackRateUnlocked(double aPlaybackRate);
   // Get the current playback rate.
   // Called on the audio thread.
   double GetPlaybackRate();
@@ -279,8 +283,6 @@ public:
   int GetChannels() { return mChannels; }
   int GetOutChannels() { return mOutChannels; }
 
-  // This should be called before attempting to use the time stretcher.
-  nsresult EnsureTimeStretcherInitialized();
   // Set playback rate as a multiple of the intrinsic playback rate. This is to
   // be called only with aPlaybackRate > 0.0.
   nsresult SetPlaybackRate(double aPlaybackRate);
@@ -288,6 +290,13 @@ public:
   nsresult SetPreservesPitch(bool aPreservesPitch);
 
   size_t SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const;
+
+protected:
+  friend class AudioClock;
+
+  // Shared implementation of underflow adjusted position calculation.
+  // Caller must own the monitor.
+  int64_t GetPositionInFramesUnlocked();
 
 private:
   friend class AudioInitTask;
@@ -324,10 +333,6 @@ private:
   long GetUnprocessed(void* aBuffer, long aFrames, int64_t &aTime);
   long GetTimeStretched(void* aBuffer, long aFrames, int64_t &aTime);
   long GetUnprocessedWithSilencePadding(void* aBuffer, long aFrames, int64_t &aTime);
-
-  // Shared implementation of underflow adjusted position calculation.
-  // Caller must own the monitor.
-  int64_t GetPositionInFramesUnlocked();
 
   int64_t GetLatencyInFrames();
   void GetBufferInsertTime(int64_t &aTimeMs);
