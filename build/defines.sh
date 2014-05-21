dc05a0d9e589051ae9c412166e6b7440a6bcef45
Lukas Blakk <lsblakk@mozilla.com>
2014-05-21 08:32:23 -0700

    updating EARLY_BETA_OR_EARLIER a=release-mgmt

--- build/defines.sh
+++ build/defines.sh
@@ -1,3 +1,4 @@
 # Define indicating that this build is prior to one of the early betas. To be
 # unset mid-way through the beta cycle.
-EARLY_BETA_OR_EARLIER=1
+# We are past Beta 6 now, disabling this for remainder of beta cycle
+EARLY_BETA_OR_EARLIER=
