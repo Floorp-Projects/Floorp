17e986172bb419b8cbc579f0dd3dcf6adcfed7a5
Ryan VanderMeulen <ryanvm@gmail.com>
2014-05-22 09:36:20 -0400

    Backed out changesets 6ca8f23d819e, 70f39dc8a9c4, and d8896351e5f1 (bug 1010747) for landing without approval.

--- js/src/jit/Lowering.h
+++ js/src/jit/Lowering.h
@@ -120,7 +120,6 @@ class LIRGenerator : public LIRGeneratorSpecific
     bool visitRsh(MRsh *ins);
     bool visitUrsh(MUrsh *ins);
     bool visitFloor(MFloor *ins);
-    bool visitCeil(MCeil *ins);
     bool visitRound(MRound *ins);
     bool visitMinMax(MMinMax *ins);
     bool visitAbs(MAbs *ins);
