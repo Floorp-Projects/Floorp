21194ded0b609081c68283a46a3dd12762de20cc
Benjamin Bouvier <benj@benj.me>
2014-05-22 12:03:08 +0200

    Bug 1010747: Implement Ceil (floating-point) -> int32 in Ion; r=sunfish,mjrosenb

--- js/src/jit/MOpcodes.h
+++ js/src/jit/MOpcodes.h
@@ -185,6 +185,7 @@ namespace jit {
     _(RunOncePrologue)                                                      \
     _(Rest)                                                                 \
     _(Floor)                                                                \
+    _(Ceil)                                                                 \
     _(Round)                                                                \
     _(In)                                                                   \
     _(InstanceOf)                                                           \
