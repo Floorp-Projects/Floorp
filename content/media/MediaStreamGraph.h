61af68b4bde867858cfe5140d07b18a5f5371054
Paul Adenot <paul@paul.cx>
2014-05-22 14:49:10 +0200

    Backed out changeset 8dd41701dd92 (bug 989921) on a CLOSED TREE

--- content/media/MediaStreamGraph.h
+++ content/media/MediaStreamGraph.h
@@ -18,6 +18,7 @@
 #include "MainThreadUtils.h"
 #include "nsAutoRef.h"
 #include "speex/speex_resampler.h"
+#include "AudioMixer.h"
 #include "mozilla/dom/AudioChannelBinding.h"
 
 class nsIRunnable;
