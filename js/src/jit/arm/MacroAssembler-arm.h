294a4692de4dc21e3f708201ddf5c9ffd19f2124
Ryan VanderMeulen <ryanvm@gmail.com>
2014-05-22 09:39:08 -0400

    Backed out changesets 0db12290df12, 80950d72bd71, and 586ed41fa2d1 (bug 1010747) for landing without approval.

--- js/src/jit/arm/MacroAssembler-arm.h
+++ js/src/jit/arm/MacroAssembler-arm.h
@@ -1454,8 +1454,6 @@ class MacroAssemblerARMCompat : public MacroAssemblerARM
     }
     void floor(FloatRegister input, Register output, Label *handleNotAnInt);
     void floorf(FloatRegister input, Register output, Label *handleNotAnInt);
-    void ceil(FloatRegister input, Register output, Label *handleNotAnInt);
-    void ceilf(FloatRegister input, Register output, Label *handleNotAnInt);
     void round(FloatRegister input, Register output, Label *handleNotAnInt, FloatRegister tmp);
     void roundf(FloatRegister input, Register output, Label *handleNotAnInt, FloatRegister tmp);
 
