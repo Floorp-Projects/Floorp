0629a4a901a341d5e2c3f8508a8d9ba4a06ffe51
Jason Orendorff <jorendorff@mozilla.com>
2014-05-22 08:18:02 -0500

    Bug 987995, part 4 - Add new crash reporter annotations for JS out-of-memory conditions. r=mccr8,r=bsmedberg.

---
+++ toolkit/crashreporter/test/unit/test_crash_after_js_oom_reported.js
@@ -0,0 +1,34 @@
+function run_test()
+{
+  if (!("@mozilla.org/toolkit/crash-reporter;1" in Components.classes)) {
+    dump("INFO | test_crash_after_js_oom_reported.js | Can't test crashreporter in a non-libxul build.\n");
+    return;
+  }
+
+  do_crash(
+    function() {
+      crashType = CrashTestUtils.CRASH_MOZ_CRASH;
+      crashReporter.annotateCrashReport("TestingOOMCrash", "Yes");
+
+      // GC now to avoid having it happen randomly later, which would make the
+      // test bogusly fail. See comment below.
+      Components.utils.forceGC();
+
+      Components.utils.getJSTestingFunctions().reportOutOfMemory();
+    },
+    function(mdump, extra) {
+      do_check_eq(extra.TestingOOMCrash, "Yes");
+
+      // The JSOutOfMemory field is absent if the JS engine never reported OOM,
+      // "Reported" if it did, and "Recovered" if it reported OOM but
+      // subsequently completed a full GC cycle. Since this test calls
+      // reportOutOfMemory() and then crashes, we expect "Reported".
+      //
+      // Theoretically, GC can happen any time, so it is just possible that
+      // this property could be "Recovered" even if the implementation is
+      // correct. More likely, though, that indicates a bug, so only accept
+      // "Reported".
+      do_check_eq(extra.JSOutOfMemory, "Reported");
+    },
+    true);
+}
