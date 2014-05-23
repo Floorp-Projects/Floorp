afa249ae944736bd12819611f13bbf0a480f70ed
Richard Newman <rnewman@mozilla.com>
2014-05-22 17:34:04 -0700

    Bug 1014338 - Part 0: move Distribution class into its own package. r=margaret
    --HG--
    rename : mobile/android/base/Distribution.java => mobile/android/base/distribution/Distribution.java

--- mobile/android/base/distribution/Distribution.java
+++ mobile/android/base/distribution/Distribution.java
@@ -3,11 +3,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.gecko;
+package org.mozilla.gecko.distribution;
 
+import org.mozilla.gecko.GeckoAppShell;
+import org.mozilla.gecko.GeckoEvent;
+import org.mozilla.gecko.GeckoSharedPrefs;
 import org.mozilla.gecko.mozglue.RobocopTarget;
 import org.mozilla.gecko.util.ThreadUtils;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
