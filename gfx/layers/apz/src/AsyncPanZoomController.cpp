c6431dd1f7f933af3ff9ec00211c8b7996a4c134
Botond Ballo <botond@mozilla.com>
2014-05-23 14:32:10 -0400

    Bug 1015331 - Do not leak Task object created during fling handoff. r=kats

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -1728,6 +1728,7 @@ bool AsyncPanZoomController::SampleContentTransformForFrame(const TimeStamp& aSa
 
   for (uint32_t i = 0; i < deferredTasks.length(); ++i) {
     deferredTasks[i]->Run();
+    delete deferredTasks[i];
   }
 
   // Cancel the mAsyncScrollTimeoutTask because we will fire a
