83adf2e0f3b7d4f6cc0c01bfb55fdcbbea1155f1
Kartikaya Gupta <kgupta@mozilla.com>
2014-05-23 21:08:51 -0400

    Bug 1015278 - Replace the std::string with a char[20] in FrameMetrics to make it cross-process shmem friendly. r=botond

--- gfx/ipc/GfxMessageUtils.h
+++ gfx/ipc/GfxMessageUtils.h
@@ -763,7 +763,8 @@ struct ParamTraits<mozilla::layers::FrameMetrics>
     WriteParam(aMsg, aParam.mHasScrollgrab);
     WriteParam(aMsg, aParam.mUpdateScrollOffset);
     WriteParam(aMsg, aParam.mScrollGeneration);
-    WriteParam(aMsg, aParam.mContentDescription);
+    aMsg->WriteBytes(aParam.mContentDescription,
+                     sizeof(aParam.mContentDescription));
     WriteParam(aMsg, aParam.mTransformScale);
   }
 
@@ -789,7 +790,9 @@ struct ParamTraits<mozilla::layers::FrameMetrics>
             ReadParam(aMsg, aIter, &aResult->mHasScrollgrab) &&
             ReadParam(aMsg, aIter, &aResult->mUpdateScrollOffset) &&
             ReadParam(aMsg, aIter, &aResult->mScrollGeneration) &&
-            ReadParam(aMsg, aIter, &aResult->mContentDescription) &&
+            aMsg->ReadBytes(aIter,
+                            reinterpret_cast<const char**>(&aResult->mContentDescription),
+                            sizeof(aResult->mContentDescription)) &&
             ReadParam(aMsg, aIter, &aResult->mTransformScale));
   }
 };
