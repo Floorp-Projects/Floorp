7bc4b97e7c5ef18bf396981cd5a23a887582d6fb
Sotaro Ikeda <sikeda@mozilla.com>
2014-05-24 09:14:45 -0700

    Bug 1006957 - Handle buffer ownership between SurfaceStream and CanvasClient r=nical

--- gfx/layers/client/SimpleTextureClientPool.cpp
+++ gfx/layers/client/SimpleTextureClientPool.cpp
@@ -64,7 +64,7 @@ SimpleTextureClientPool::GetTextureClient(bool aAutoRecycle)
   RefPtr<TextureClient> textureClient;
   if (mAvailableTextureClients.size()) {
     textureClient = mAvailableTextureClients.top();
-    textureClient->WaitReleaseFence();
+    textureClient->WaitForBufferOwnership();
     mAvailableTextureClients.pop();
     RECYCLE_LOG("%s Skip allocate (%i left), returning %p\n", (mFormat == SurfaceFormat::B8G8R8A8?"poolA":"poolX"), mAvailableTextureClients.size(), textureClient.get());
 
