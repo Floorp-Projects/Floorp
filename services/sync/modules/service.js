30ae14ca7dc568bdc7b20ab2dc413b5c936bf3d9
Mark Hammond <mhammond@skippinet.com.au>
2014-05-26 09:48:41 +1000

    Bug 1013448 (part 2) - add telemetry probes for sync. r=rnewman

--- services/sync/modules/service.js
+++ services/sync/modules/service.js
@@ -1247,6 +1247,9 @@ Sync11Service.prototype = {
     return this._lock("service.js: sync",
                       this._notify("sync", "", function onNotify() {
 
+      let histogram = Services.telemetry.getHistogramById("WEAVE_START_COUNT");
+      histogram.add(1);
+
       let synchronizer = new EngineSynchronizer(this);
       let cb = Async.makeSpinningCallback();
       synchronizer.onComplete = cb;
@@ -1255,6 +1258,9 @@ Sync11Service.prototype = {
       // wait() throws if the first argument is truthy, which is exactly what
       // we want.
       let result = cb.wait();
+
+      histogram = Services.telemetry.getHistogramById("WEAVE_COMPLETE_SUCCESS_COUNT");
+      histogram.add(1);
 
       // We successfully synchronized. Now let's update our declined engines.
       let meta = this.recordManager.get(this.metaURL);
