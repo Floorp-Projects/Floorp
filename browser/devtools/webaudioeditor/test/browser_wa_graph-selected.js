fea77157d9ecd2ce64fa6392db821a9c9d7becf4
Jordan Santell <jsantell@gmail.com>
2014-05-26 11:32:00 -0400

    Bug 1013544 - Highlight active node in web audio editor. r=vp

---
+++ browser/devtools/webaudioeditor/test/browser_wa_graph-selected.js
@@ -0,0 +1,52 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Tests that SVG nodes and edges were created for the Graph View.
+ */
+
+function spawnTest() {
+  let [target, debuggee, panel] = yield initWebAudioEditor(SIMPLE_CONTEXT_URL);
+  let { panelWin } = panel;
+  let { gFront, $, $$, EVENTS } = panelWin;
+
+  let started = once(gFront, "start-context");
+
+  reload(target);
+
+  let [actors] = yield Promise.all([
+    get3(gFront, "create-node"),
+    waitForGraphRendered(panelWin, 3, 2)
+  ]);
+
+  let [destId, oscId, gainId] = actors.map(actor => actor.actorID);
+
+  ok(!findGraphNode(panelWin, destId).classList.contains("selected"),
+    "No nodes selected on start. (destination)");
+  ok(!findGraphNode(panelWin, oscId).classList.contains("selected"),
+    "No nodes selected on start. (oscillator)");
+  ok(!findGraphNode(panelWin, gainId).classList.contains("selected"),
+    "No nodes selected on start. (gain)");
+
+  yield clickGraphNode(panelWin, oscId);
+
+  ok(findGraphNode(panelWin, oscId).classList.contains("selected"),
+    "Selected node has class 'selected'.");
+  ok(!findGraphNode(panelWin, destId).classList.contains("selected"),
+    "Non-selected nodes do not have class 'selected'.");
+  ok(!findGraphNode(panelWin, gainId).classList.contains("selected"),
+    "Non-selected nodes do not have class 'selected'.");
+
+  yield clickGraphNode(panelWin, gainId);
+
+  ok(!findGraphNode(panelWin, oscId).classList.contains("selected"),
+    "Previously selected node no longer has class 'selected'.");
+  ok(!findGraphNode(panelWin, destId).classList.contains("selected"),
+    "Non-selected nodes do not have class 'selected'.");
+  ok(findGraphNode(panelWin, gainId).classList.contains("selected"),
+    "Newly selected node now has class 'selected'.");
+
+  yield teardown(panel);
+  finish();
+}
+
