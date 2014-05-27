4533f817af043ed014d17799d6afc824c4fc25ed
Mike Shal <mshal@mozilla.com>
2014-05-27 13:07:24 -0400

    Bug 1015443 - make test_iter_modules_in_path case insensitive; r=glandium
    From d2c5353da28b5134eeea078a6fac09f023b220ab Mon Sep 17 00:00:00 2001
    ---
     python/mozbuild/mozbuild/test/test_pythonutil.py | 2 +-
     1 file changed, 1 insertion(+), 1 deletion(-)

--- python/mozbuild/mozbuild/test/test_pythonutil.py
+++ python/mozbuild/mozbuild/test/test_pythonutil.py
@@ -10,7 +10,7 @@ import unittest
 
 class TestIterModules(unittest.TestCase):
     def test_iter_modules_in_path(self):
-        mozbuild_path = os.path.dirname(os.path.dirname(__file__))
+        mozbuild_path = os.path.normcase(os.path.dirname(os.path.dirname(__file__)))
         paths = list(iter_modules_in_path(mozbuild_path))
         self.assertEquals(sorted(paths), [
             os.path.join(os.path.abspath(mozbuild_path), '__init__.py'),
