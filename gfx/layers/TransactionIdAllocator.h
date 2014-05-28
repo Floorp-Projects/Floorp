7facd077474511f1edb46d9b023466775c78ae51
Matt Woodrow <mwoodrow@mozilla.com>
2014-05-29 09:42:17 +1200

    Bug 854421 - Part 6: Add gfx API for allocating transaction ids. r=roc

---
+++ gfx/layers/TransactionIdAllocator.h
@@ -0,0 +1,52 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef GFX_TRANSACTION_ID_ALLOCATOR_H
+#define GFX_TRANSACTION_ID_ALLOCATOR_H
+
+#include "nsISupportsImpl.h"
+
+namespace mozilla {
+namespace layers {
+
+class TransactionIdAllocator {
+public:
+  NS_INLINE_DECL_REFCOUNTING(TransactionIdAllocator)
+
+  virtual ~TransactionIdAllocator() {}
+
+  /**
+   * Allocate a unique id number for the current refresh tick, can
+   * only be called while IsInRefresh().
+   *
+   * If too many id's are allocated without being returned then
+   * the refresh driver will suspend until they catch up.
+   */
+  virtual uint64_t GetTransactionId() = 0;
+
+  /**
+   * Notify that all work (including asynchronous composites)
+   * for a given transaction id has been completed.
+   *
+   * If the refresh driver has been suspended because
+   * of having too many outstanding id's, then this may
+   * resume it.
+   */
+  virtual void NotifyTransactionCompleted(uint64_t aTransactionId) = 0;
+
+  /**
+   * Revoke a transaction id that isn't needed to track
+   * completion of asynchronous work. This is similar
+   * to NotifyTransactionCompleted except avoids
+   * return ordering issues.
+   */
+  virtual void RevokeTransactionId(uint64_t aTransactionId) = 0;
+};
+
+}
+}
+
+
+#endif /* GFX_TRANSACTION_ID_ALLOCATOR_H */
