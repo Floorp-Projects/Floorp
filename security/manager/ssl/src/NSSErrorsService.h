4434286b6b3f03ba760fec5202b41b53e691be58
David Keeler <dkeeler@mozilla.com>
2014-05-28 15:28:03 -0700

    bug 1006710 - add class of PSM errors to SEC and SSL errors r=briansmith

--- security/manager/ssl/src/NSSErrorsService.h
+++ security/manager/ssl/src/NSSErrorsService.h
@@ -2,14 +2,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef NSSErrorsService_h
+#define NSSErrorsService_h
+
 #include "nsINSSErrorsService.h"
 
-#include "nsIStringBundle.h"
-#include "nsCOMPtr.h"
 #include "mozilla/Attributes.h"
+#include "nsCOMPtr.h"
+#include "nsIStringBundle.h"
+#include "prerror.h"
 
 namespace mozilla {
 namespace psm {
+
+enum PSMErrorCodes {
+  PSM_ERROR_KEY_PINNING_FAILURE = (nsINSSErrorsService::PSM_ERROR_BASE + 0)
+};
+
+void RegisterPSMErrorTable();
 
 class NSSErrorsService MOZ_FINAL : public nsINSSErrorsService
 {
@@ -24,8 +34,13 @@ private:
   nsCOMPtr<nsIStringBundle> mNSSErrorsBundle;
 };
 
+bool IsNSSErrorCode(PRErrorCode code);
+nsresult GetXPCOMFromNSSError(PRErrorCode code);
+
 } // psm
 } // mozilla
 
 #define NS_NSSERRORSSERVICE_CID \
   { 0x9ef18451, 0xa157, 0x4d17, { 0x81, 0x32, 0x47, 0xaf, 0xef, 0x21, 0x36, 0x89 } }
+
+#endif // NSSErrorsService_h
