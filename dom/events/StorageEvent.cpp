9ba916093a4b8342b5b98b144938f20794d33342
Andrea Marchesini <amarchesini@mozilla.com>
2014-05-28 05:36:00 -0400

    Bug 1016053 - document.createElement('StorageEvent') must be supported. r=smaug

---
+++ dom/events/StorageEvent.cpp
@@ -0,0 +1,115 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/StorageEvent.h"
+#include "nsIDOMStorage.h"
+
+namespace mozilla {
+namespace dom {
+
+NS_IMPL_CYCLE_COLLECTION_CLASS(StorageEvent)
+
+NS_IMPL_ADDREF_INHERITED(StorageEvent, Event)
+NS_IMPL_RELEASE_INHERITED(StorageEvent, Event)
+
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INHERITED(StorageEvent, Event)
+  NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mStorageArea)
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
+
+NS_IMPL_CYCLE_COLLECTION_TRACE_BEGIN_INHERITED(StorageEvent, Event)
+NS_IMPL_CYCLE_COLLECTION_TRACE_END
+
+NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(StorageEvent, Event)
+  NS_IMPL_CYCLE_COLLECTION_UNLINK(mStorageArea)
+NS_IMPL_CYCLE_COLLECTION_UNLINK_END
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION_INHERITED(StorageEvent)
+NS_INTERFACE_MAP_END_INHERITING(Event)
+
+StorageEvent::StorageEvent(EventTarget* aOwner)
+  : Event(aOwner, nullptr, nullptr)
+{
+}
+
+StorageEvent::~StorageEvent()
+{
+}
+
+StorageEvent*
+StorageEvent::AsStorageEvent()
+{
+  return this;
+}
+
+JSObject*
+StorageEvent::WrapObject(JSContext* aCx)
+{
+  return StorageEventBinding::Wrap(aCx, this);
+}
+
+already_AddRefed<StorageEvent>
+StorageEvent::Constructor(EventTarget* aOwner,
+                          const nsAString& aType,
+                          const StorageEventInit& aEventInitDict)
+{
+  nsRefPtr<StorageEvent> e = new StorageEvent(aOwner);
+
+  bool trusted = e->Init(aOwner);
+  e->InitEvent(aType, aEventInitDict.mBubbles, aEventInitDict.mCancelable);
+  e->mKey = aEventInitDict.mKey;
+  e->mOldValue = aEventInitDict.mOldValue;
+  e->mNewValue = aEventInitDict.mNewValue;
+  e->mUrl = aEventInitDict.mUrl;
+  e->mStorageArea = aEventInitDict.mStorageArea;
+  e->SetTrusted(trusted);
+
+  return e.forget();
+}
+
+already_AddRefed<StorageEvent>
+StorageEvent::Constructor(const GlobalObject& aGlobal,
+                          const nsAString& aType,
+                          const StorageEventInit& aEventInitDict,
+                          ErrorResult& aRv)
+{
+  nsCOMPtr<EventTarget> owner = do_QueryInterface(aGlobal.GetAsSupports());
+  return Constructor(owner, aType, aEventInitDict);
+}
+
+void
+StorageEvent::InitStorageEvent(const nsAString& aType, bool aCanBubble,
+                               bool aCancelable, const nsAString& aKey,
+                               const nsAString& aOldValue,
+                               const nsAString& aNewValue,
+                               const nsAString& aURL,
+                               nsIDOMStorage* aStorageArea,
+                               ErrorResult& aRv)
+{
+  aRv = InitEvent(aType, aCanBubble, aCancelable);
+  if (aRv.Failed()) {
+    return;
+  }
+
+  mKey = aKey;
+  mOldValue = aOldValue;
+  mNewValue = aNewValue;
+  mUrl = aURL;
+  mStorageArea = aStorageArea;
+}
+
+nsresult
+NS_NEWDOMStorageEvent(nsIDOMEvent** aDOMEvent, EventTarget* aOwner)
+{
+  nsRefPtr<StorageEvent> e = new StorageEvent(aOwner);
+
+  e->SetTrusted(e->Init(aOwner));
+  e.forget(aDOMEvent);
+
+  return NS_OK;
+}
+
+} // namespace dom
+} // namespace mozilla
---
+++ dom/events/StorageEvent.h
@@ -0,0 +1,90 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_StorageEvent_h
+#define mozilla_dom_StorageEvent_h
+
+#include "mozilla/Attributes.h"
+#include "mozilla/ErrorResult.h"
+#include "mozilla/dom/BindingUtils.h"
+#include "mozilla/dom/Event.h"
+#include "mozilla/dom/StorageEventBinding.h"
+
+class nsIDOMStorage;
+
+namespace mozilla {
+namespace dom {
+
+// Helper for EventDispatcher.
+nsresult NS_NEWDOMStorageEvent(nsIDOMEvent** aDOMEvent, EventTarget* aOwner);
+
+class StorageEvent : public Event
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS_INHERITED(StorageEvent, Event)
+
+  StorageEvent(EventTarget* aOwner);
+  virtual ~StorageEvent();
+
+protected:
+  nsString mKey;
+  nsString mOldValue;
+  nsString mNewValue;
+  nsString mUrl;
+  nsCOMPtr<nsIDOMStorage> mStorageArea;
+
+public:
+  virtual StorageEvent* AsStorageEvent();
+
+  virtual JSObject* WrapObject(JSContext* aCx) MOZ_OVERRIDE;
+
+  static already_AddRefed<StorageEvent>
+  Constructor(EventTarget* aOwner, const nsAString& aType,
+              const StorageEventInit& aEventInitDict);
+
+  static already_AddRefed<StorageEvent>
+  Constructor(const GlobalObject& aGlobal, const nsAString& aType,
+              const StorageEventInit& aEventInitDict, ErrorResult& aRv);
+
+  void InitStorageEvent(const nsAString& aType, bool aCanBubble,
+                        bool aCancelable, const nsAString& aKey,
+                        const nsAString& aOldValue,
+                        const nsAString& aNewValue,
+                        const nsAString& aURL,
+                        nsIDOMStorage* aStorageArea,
+                        ErrorResult& aRv);
+
+  void GetKey(nsString& aRetVal) const
+  {
+    aRetVal = mKey;
+  }
+
+  void GetOldValue(nsString& aRetVal) const
+  {
+    aRetVal = mOldValue;
+  }
+
+  void GetNewValue(nsString& aRetVal) const
+  {
+    aRetVal = mNewValue;
+  }
+
+  void GetUrl(nsString& aRetVal) const
+  {
+    aRetVal = mUrl;
+  }
+
+  nsIDOMStorage* GetStorageArea() const
+  {
+    return mStorageArea;
+  }
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_StorageEvent_h
