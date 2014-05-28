e961683e23f1f02a97498af6031f0584a64819ba
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2014-05-28 19:03:27 +0900

    Bug 982298 - Remove importing Timer.jsm. r=bz

--- toolkit/content/tests/browser/browser_bug982298.js
+++ toolkit/content/tests/browser/browser_bug982298.js
@@ -1,8 +1,6 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-Components.utils.import("resource://gre/modules/Timer.jsm");
-
 const Ci = Components.interfaces;
 
 let tab;
