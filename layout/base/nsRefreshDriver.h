ae564f53f2b623bc001346f8d9ee9d10dd8a3607
Ed Morley <emorley@mozilla.com>
2014-05-29 10:55:47 +0100

    Backed out changeset 123998cc3f92 (bug 854421)

--- layout/base/nsRefreshDriver.h
+++ layout/base/nsRefreshDriver.h
@@ -63,8 +63,7 @@ public:
   virtual void DidRefresh() = 0;
 };
 
-class nsRefreshDriver MOZ_FINAL : public mozilla::layers::TransactionIdAllocator,
-                                  public nsARefreshObserver {
+class nsRefreshDriver MOZ_FINAL : public mozilla::layers::TransactionIdAllocator {
 public:
   nsRefreshDriver(nsPresContext *aPresContext);
   ~nsRefreshDriver();
@@ -276,12 +275,6 @@ public:
   void NotifyTransactionCompleted(uint64_t aTransactionId) MOZ_OVERRIDE;
   void RevokeTransactionId(uint64_t aTransactionId) MOZ_OVERRIDE;
 
-  bool IsWaitingForPaint();
-
-  // nsARefreshObserver
-  NS_IMETHOD_(MozExternalRefCountType) AddRef(void) { return TransactionIdAllocator::AddRef(); }
-  NS_IMETHOD_(MozExternalRefCountType) Release(void) { return TransactionIdAllocator::Release(); }
-  virtual void WillRefresh(mozilla::TimeStamp aTime);
 private:
   typedef nsTObserverArray<nsARefreshObserver*> ObserverArray;
   typedef nsTHashtable<nsISupportsHashKey> RequestTable;
@@ -334,8 +327,6 @@ private:
 
   nsPresContext *mPresContext; // weak; pres context passed in constructor
                                // and unset in Disconnect
-
-  nsRefPtr<nsRefreshDriver> mRootRefresh;
 
   // The most recently allocated transaction id.
   uint64_t mPendingTransaction;
