6a569f76d9115fbdc3322a7c0e58db64f1742175
Mark Banner <mbanner@mozilla.com>
2014-05-29 21:13:46 +0100

    Bug 1000007 Separate out loop from the social api, and add a loop specific service and API injection. r=mhammond

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -172,6 +172,9 @@ let gInitialPages = [
 #include browser-feeds.js
 #include browser-fullScreen.js
 #include browser-fullZoom.js
+#ifdef MOZ_LOOP
+#include browser-loop.js
+#endif
 #include browser-places.js
 #include browser-plugins.js
 #include browser-safebrowsing.js
