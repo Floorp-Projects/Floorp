925730398d7b4ac5e5963218d3a61be0b95ea515
Mark Banner <mbanner@mozilla.com>
2014-05-29 21:13:43 +0100

    Bug 985596 Set up initial desktop conversation window. r=dmose

--- browser/components/loop/content/js/conversation.js
+++ browser/components/loop/content/js/conversation.js
@@ -3,36 +3,130 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /*global loop*/
-// XXX This file needs unit testing
+
+Components.utils.import("resource://gre/modules/Services.jsm");
 
 var loop = loop || {};
-loop.conversation = (function(_, __) {
+loop.conversation = (function(TB) {
   "use strict";
 
-  // XXX: baseApiUrl should be configurable (browser pref)
-  var baseApiUrl = "http://localhost:5000";
+  var baseServerUrl = Services.prefs.getCharPref("loop.server");
+
+  /**
+   * App router.
+   * @type {loop.webapp.Router}
+   */
+  var router;
+
+  /**
+   * Current conversation model instance.
+   * @type {loop.webapp.ConversationModel}
+   */
+  var conversation;
+
+  var ConversationRouter = loop.shared.router.BaseRouter.extend({
+    _conversation: undefined,
+    activeView: undefined,
+
+    routes: {
+      "start/:version": "start",
+      "call/ongoing": "conversation",
+      "call/ended": "ended"
+    },
+
+    /**
+     * Loads and render current active view.
+     *
+     * @param {loop.shared.BaseView} view View.
+     */
+    loadView : function(view) {
+      if (this.activeView) {
+        this.activeView.hide();
+      }
+      this.activeView = view.render().show();
+    },
+
+    initialize: function(options) {
+      options = options || {};
+      if (!options.conversation) {
+        throw new Error("missing required conversation");
+      }
+      this._conversation = options.conversation;
+
+      this.listenTo(this._conversation, "session:ready", this._onSessionReady);
+      this.listenTo(this._conversation, "session:ended", this._onSessionEnded);
+    },
+
+    /**
+     * Navigates to conversation when the call session is ready.
+     */
+    _onSessionReady: function() {
+      this.navigate("call/ongoing", {trigger: true});
+    },
+
+    /**
+     * Navigates to ended state when the call has ended
+     */
+    _onSessionEnded: function() {
+      this.navigate("call/ended", {trigger: true});
+    },
+
+    /**
+     * start is the initial route that does any necessary prompting and set
+     * up for the call.
+     *
+     * @param {String} loopVersion The version from the push notification, set
+     *                             by the router from the URL.
+     */
+    start: function(loopVersion) {
+      // XXX For now, we just kick the conversation straight away, bug 990678
+      // will implement the follow-ups.
+      this._conversation.set({loopVersion: loopVersion});
+      this._conversation.initiate({
+        baseServerUrl: baseServerUrl,
+        outgoing: false
+      });
+    },
+
+    /**
+     * conversation is the route when the conversation is active. The start
+     * route should be navigated to first.
+     */
+    conversation: function() {
+      if (!this._conversation.isSessionReady()) {
+        // XXX: notify user that something has gone wrong.
+        console.error("Error: navigated to conversation route without " +
+          "the start route to initialise the call first");
+        return;
+      }
+
+      this.loadView(
+        new loop.shared.views.ConversationView({
+          sdk: TB,
+          model: this._conversation
+      }));
+    },
+
+    /**
+     * ended does any necessary work to end the call.
+     */
+    ended: function() {
+      // XXX Later we implement the end-of call here (bug 974873)
+      window.close();
+    }
+  });
 
   /**
    * Panel initialisation.
    */
   function init() {
-    // Send a message to the server to get the call info
-    this.client = new loop.Client({
-      baseApiUrl: baseApiUrl
-    });
-
-    // Get the call information
-    this.client.requestCallsInfo(function(err, calls) {
-      if (err) {
-        console.error("Error getting call data: ", err);
-        return;
-      }
-
-      console.log("Received Calls Data: ", calls);
-    });
+    conversation = new loop.shared.models.ConversationModel();
+    router = new ConversationRouter({conversation: conversation});
+    Backbone.history.start();
   }
 
   return {
+    ConversationRouter: ConversationRouter,
     init: init
   };
-})(_);
+})(window.TB);
