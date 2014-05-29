a32cbc6b4e8a2ce743a444e58395a05b8586f67c
Nicolas Perriault <nperriault@gmail.com>
2014-05-29 21:20:11 +0100

    Bug 1005041: In progress feedback when getting a call url, r=dmose

--- browser/components/loop/content/shared/js/views.js
+++ browser/components/loop/content/shared/js/views.js
@@ -335,11 +335,11 @@ loop.shared.views = (function(_, OT, l10n) {
   var UnsupportedView = BaseView.extend({
     template: _.template([
       '<div>',
-        '<h2 data-l10n-id="incompatible_browser"></h2>',
-        '<p data-l10n-id="powered_by_webrtc"></p>',
-        '<p data-l10n-id="use_latest_firefox" ',
-          'data-l10n-args=\'{"ff_url": "https://www.mozilla.org/firefox/"}\'>',
-        '</p>',
+      '  <h2 data-l10n-id="incompatible_browser"></h2>',
+      '  <p data-l10n-id="powered_by_webrtc"></p>',
+      '  <p data-l10n-id="use_latest_firefox" ',
+      '    data-l10n-args=\'{"ff_url": "https://www.mozilla.org/firefox/"}\'>',
+      '  </p>',
       '</div>'
     ].join(""))
   });
