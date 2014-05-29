b32d187a34de98417bf37cecff29f9e1fb19d7ba
Chris Peterson <cpeterson@mozilla.com>
2014-05-28 19:02:11 -0700

    Bug 1019381 - Cherrypick clang and gcc warning fixes from upstream jemalloc. r=glandium

--- memory/jemalloc/update.sh
+++ memory/jemalloc/update.sh
@@ -19,6 +19,7 @@ patch -p1 < ../0002-Use-ULL-prefix-instead-of-LLU-for-unsigned-long-long.patch
 patch -p1 < ../0003-Don-t-use-msvc_compat-s-C99-headers-with-MSVC-versio.patch
 patch -p1 < ../0004-Try-to-use-__builtin_ffsl-if-ffsl-is-unavailable.patch
 patch -p1 < ../0005-Check-for-__builtin_ffsl-before-ffsl.patch
+patch -p1 < ../0006-Fix-clang-warnings.patch
 
 cd ..
 hg addremove -q src
