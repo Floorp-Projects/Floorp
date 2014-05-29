934c87866ffa5c1c34e7a2fa9a49b97bc942eeec
Nicolas Perriault <nperriault@gmail.com>
2014-05-29 21:20:11 +0100

    Bug 979880: Added basic view toggling utility in app router. r=dmose,r=ametaireau

--- test/webapp_test.js
+++ test/webapp_test.js
@@ -2,12 +2,42 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* global loop */
+
 var expect = chai.expect;
 
-describe("test", function() {
+describe("Router", function() {
   "use strict";
 
-  it("is true", function() {
-    expect(true).eql(true);
+  var router;
+
+  beforeEach(function() {
+    router = new loop.webapp.Router();
+  });
+
+  describe("#constructor", function() {
+    it("should define a default active view", function() {
+      expect(router.view).to.be.an.instanceOf(loop.webapp.HomeView);
+    });
+  });
+
+  describe("#loadView", function() {
+    it("should set the active view", function() {
+      router.loadView(new loop.webapp.CallLauncherView({token: "fake"}));
+
+      expect(router.view).to.be.an.instanceOf(loop.webapp.CallLauncherView);
+    });
+  });
+});
+
+describe("CallLauncherView", function() {
+  "use strict";
+
+  describe("#constructor", function() {
+    it("should require a token option", function() {
+      expect(function() {
+        new loop.webapp.CallLauncherView();
+      }).to.Throw(Error, /missing required token/);
+    });
   });
 });
