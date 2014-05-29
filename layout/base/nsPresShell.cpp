9cf416412d7e6813d7ae06bc050ef1fc4d8dd645
Ed Morley <emorley@mozilla.com>
2014-05-29 10:56:01 +0100

    Backed out changeset 743bd08df703 (bug 854421)

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -4037,6 +4037,7 @@ PresShell::FlushPendingNotifications(mozilla::ChangesToFlush aFlush)
       }
 
       if (aFlush.mFlushAnimations &&
+          nsLayoutUtils::AreAsyncAnimationsEnabled() &&
           !mPresContext->StyleUpdateForAllAnimationsIsUpToDate()) {
         mPresContext->AnimationManager()->
           FlushAnimations(CommonAnimationManager::Cannot_Throttle);
