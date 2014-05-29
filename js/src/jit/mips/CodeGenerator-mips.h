a3de89f9c39aa9d70aa6d083567e6d81293e25ba
Branislav Rankov <branislav.rankov@imgtec.com>
2014-05-29 13:41:22 +0200

    Bug 1017036 - IonMonkey MIPS: Implemented visitCeil, visitCeilF. r=nbp

--- js/src/jit/mips/CodeGenerator-mips.h
+++ js/src/jit/mips/CodeGenerator-mips.h
@@ -212,6 +212,8 @@ class CodeGeneratorMIPS : public CodeGeneratorShared
     virtual bool visitMathF(LMathF *math);
     virtual bool visitFloor(LFloor *lir);
     virtual bool visitFloorF(LFloorF *lir);
+    virtual bool visitCeil(LCeil *lir);
+    virtual bool visitCeilF(LCeilF *lir);
     virtual bool visitRound(LRound *lir);
     virtual bool visitRoundF(LRoundF *lir);
     virtual bool visitTruncateDToInt32(LTruncateDToInt32 *ins);
