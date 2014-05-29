53416f004b689514d6750f8434964850d8f33235
Nicolas Perriault <nperriault@gmail.com>
2014-05-29 21:20:11 +0100

    Bug 983697 - Fill loop-client repository with basic server & readme. r=dmose

---
+++ browser/components/loop/standalone/server.js
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+
+app.listen(3000);
+console.log("Serving repository root over HTTP at http://localhost:3000/");
+console.log("Static contents are available at http://localhost:3000/static/");
+console.log("Tests are viewable at http://localhost:3000/test/");
+console.log("Use this for development only.");
