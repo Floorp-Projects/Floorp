334add86d2520a523f10b48cc50260e808335380
Terrence Cole <terrence@mozilla.com>
2014-05-28 17:34:36 -0700

    Bug 1013531 - Clarify the naming of the rooting analysis supression guards; r=sfink

--- dom/bindings/BindingUtils.h
+++ dom/bindings/BindingUtils.h
@@ -2717,15 +2717,12 @@ CreateGlobal(JSContext* aCx, T* aNative, nsWrapperCache* aCache,
                                  JS::DontFireOnNewGlobalHook, aOptions));
   if (!aGlobal) {
     NS_WARNING("Failed to create global");
-    return nullptr;
+    return false;
   }
 
   JSAutoCompartment ac(aCx, aGlobal);
 
   {
-    JS::AutoAssertNoGC nogc;
-
-    // The setup of our global needs to be done before a GC happens.
     js::SetReservedSlot(aGlobal, DOM_OBJECT_SLOT, PRIVATE_TO_JSVAL(aNative));
     NS_ADDREF(aNative);
 
