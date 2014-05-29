f2f84e0dbaae6d1fea283d4c5fe3c9992aaaf67d
Nicolas Perriault <nperriault@gmail.com>
2014-05-29 21:20:11 +0100

    Bug 979880: Added basic view toggling utility in app router. r=dmose,r=ametaireau

--- static/js/webapp.js
+++ static/js/webapp.js
@@ -8,11 +8,118 @@ var loop = loop || {};
 loop.webapp = (function() {
   "use strict";
 
+  var router;
+
+  /**
+   * Base Backbone view.
+   */
+  var BaseView = Backbone.View.extend({
+    hide: function() {
+      this.$el.hide();
+      return this;
+    },
+
+    show: function() {
+      this.$el.show();
+      return this;
+    }
+  });
+
+  /**
+   * Homepage view.
+   */
+  var HomeView = BaseView.extend({
+    el: "#home"
+  });
+
+  /**
+   * Call launcher view.
+   */
+  var CallLauncherView = BaseView.extend({
+    el: "#call-launcher",
+
+    events: {
+      "click button": "launchCall"
+    },
+
+    initialize: function(options) {
+      options = options || {};
+      if (!options.token) {
+        throw new Error("missing required token");
+      }
+      this.token = options.token;
+    },
+
+    launchCall: function(event) {
+      event.preventDefault();
+      // XXX: request the loop server for call information using this.token
+    }
+  });
+
+  /**
+   * Call view.
+   */
+  var CallView = BaseView.extend({
+    el: "#call"
+  });
+
+  /**
+   * App Router. Allows defining a main active view and ease toggling it when
+   * the active route changes.
+   * @link http://mikeygee.com/blog/backbone.html
+   */
+  var Router = Backbone.Router.extend({
+    view: undefined,
+
+    routes: {
+        "": "home",
+        "call/:token": "call"
+    },
+
+    initialize: function() {
+      this.loadView(new HomeView());
+    },
+
+    /**
+     * Loads and render current active view.
+     *
+     * @param {BaseView} view View.
+     */
+    loadView : function(view) {
+      this.view && this.view.hide();
+      this.view = view.render().show();
+    },
+
+    /**
+     * Main entry point.
+     */
+    home: function() {
+      this.loadView(new HomeView());
+    },
+
+    /**
+     * Call setup view.
+     *
+     * @param  {String} token Call token.
+     */
+    call: function(token) {
+      this.loadView(new CallLauncherView({token: token}));
+    }
+  });
+
+  /**
+   * App initialization.
+   */
   function init() {
-    console.log("Loop app started.");
+    router = new Router();
+    Backbone.history.start();
   }
 
   return {
-    init: init
+    init: init,
+    BaseView: BaseView,
+    HomeView: HomeView,
+    Router: Router,
+    CallLauncherView: CallLauncherView
   };
 })();
