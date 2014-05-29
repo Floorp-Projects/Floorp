c5b96155c75334a45500ce03a2422bfb8d7c665a
Dan Gohman <sunfish@mozilla.com>
2014-05-28 20:12:06 -0700

    Bug 1005113 - IonMonkey: Mark more LUses as AtStart on x86/x64. r=bhackett

--- js/src/jit/shared/Lowering-shared-inl.h
+++ js/src/jit/shared/Lowering-shared-inl.h
@@ -293,6 +293,14 @@ LIRGeneratorShared::useOrConstant(MDefinition *mir)
 }
 
 LAllocation
+LIRGeneratorShared::useOrConstantAtStart(MDefinition *mir)
+{
+    if (mir->isConstant())
+        return LAllocation(mir->toConstant()->vp());
+    return useAtStart(mir);
+}
+
+LAllocation
 LIRGeneratorShared::useRegisterOrConstant(MDefinition *mir)
 {
     if (mir->isConstant())
