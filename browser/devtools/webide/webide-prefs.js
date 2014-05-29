8db7211131cb53e61be7d2faf8c299ffe4c8488c
Paul Rouget <paul@mozilla.com>
2014-05-29 13:40:35 +0200

    Backed out changeset 2fb11b6f1dfe - fail in test_webappsActor.js

--- browser/devtools/webide/webide-prefs.js
+++ browser/devtools/webide/webide-prefs.js
@@ -6,4 +6,3 @@
 pref("devtools.webide.showProjectEditor", true);
 pref("devtools.webide.templatesURL", "http://fixme/");
 pref("devtools.webide.lastprojectlocation", "");
-pref("devtools.webide.enableLocalRuntime", false);
