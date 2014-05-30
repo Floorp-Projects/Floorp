943fc3ce075a1558d0723192d082e90ede067e58
Jason Duell <jduell.mcbugs@gmail.com>
2014-05-30 12:50:51 -0700

    Bug 1010434 - NeckoParent matches a wrong app for the request of a new RemoteOpenFile. r=bent

--- netwerk/ipc/RemoteOpenFileChild.cpp
+++ netwerk/ipc/RemoteOpenFileChild.cpp
@@ -175,7 +175,8 @@ RemoteOpenFileChild::Init(nsIURI* aRemoteOpenUri, nsIURI* aAppUri)
 nsresult
 RemoteOpenFileChild::AsyncRemoteFileOpen(int32_t aFlags,
                                          nsIRemoteOpenFileListener* aListener,
-                                         nsITabChild* aTabChild)
+                                         nsITabChild* aTabChild,
+                                         nsILoadContext *aLoadContext)
 {
   if (!mFile) {
     return NS_ERROR_NOT_INITIALIZED;
@@ -227,7 +228,8 @@ RemoteOpenFileChild::AsyncRemoteFileOpen(int32_t aFlags,
   OptionalURIParams appUri;
   SerializeURI(mAppURI, appUri);
 
-  gNeckoChild->SendPRemoteOpenFileConstructor(this, uri, appUri);
+  IPC::SerializedLoadContext loadContext(aLoadContext);
+  gNeckoChild->SendPRemoteOpenFileConstructor(this, loadContext, uri, appUri);
 
   // The chrome process now has a logical ref to us until it calls Send__delete.
   AddIPDLReference();
