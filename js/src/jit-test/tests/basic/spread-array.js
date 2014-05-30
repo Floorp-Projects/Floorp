ebb6441c1ac34568cba7572e2070446bf46b44ea
Tooru Fujisawa <arai_a@mac.com>
2014-05-30 20:27:45 +0900

    Bug 1014813 - Always set array length for JSOP_INITELEM_INC with hole. r=jandem

--- js/src/jit-test/tests/basic/spread-array.js
+++ js/src/jit-test/tests/basic/spread-array.js
@@ -11,6 +11,7 @@ assertEqArray([1,, ...[2]], [1,, 2]);
 assertEqArray([1,, ...[2],, 3,, 4,], [1,, 2,, 3,, 4,]);
 assertEqArray([...[1, 2, 3],,,,], [1, 2, 3,,,,]);
 assertEqArray([,,...[1, 2, 3],,,,], [,,1,2,3,,,,]);
+assertEqArray([...[1, 2, 3],,,,...[]], [1,2,3,,,,]);
 
 assertEqArray([...[undefined]], [undefined]);
 
