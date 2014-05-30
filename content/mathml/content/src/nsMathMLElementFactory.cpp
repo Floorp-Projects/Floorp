a39cee4de38d9c807e723888adb92b2016132277
Ms2ger <ms2ger@gmail.com>
2014-05-30 09:36:53 +0200

    Bug 741295 - Treat 'id' and 'class' as global attributes for all elements; r=bz

--- content/mathml/content/src/nsMathMLElementFactory.cpp
+++ content/mathml/content/src/nsMathMLElementFactory.cpp
@@ -13,11 +13,6 @@ using namespace mozilla::dom;
 nsresult
 NS_NewMathMLElement(Element** aResult, already_AddRefed<nsINodeInfo>&& aNodeInfo)
 {
-  nsCOMPtr<nsINodeInfo> ni = aNodeInfo;
-  ni->SetIDAttributeAtom(nsGkAtoms::id);
-
-  nsMathMLElement* it = new nsMathMLElement(ni.forget());
-
-  NS_ADDREF(*aResult = it);
+  NS_ADDREF(*aResult = new nsMathMLElement(aNodeInfo));
   return NS_OK;
 }
