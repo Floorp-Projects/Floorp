f49e9ae3a1b1aaf3dd0405df067260b6ad2028fc
Michael Comella <michael.l.comella@gmail.com>
2014-05-30 12:13:25 -0700

    Bug 1014999 - Add remote tabs setup/verification panels for tablet. r=lucasr

--- mobile/android/base/tabspanel/RemoteTabsVerificationPanel.java
+++ mobile/android/base/tabspanel/RemoteTabsVerificationPanel.java
@@ -8,7 +8,6 @@ import org.mozilla.gecko.R;
 import org.mozilla.gecko.fxa.FirefoxAccounts;
 import org.mozilla.gecko.fxa.login.State;
 import org.mozilla.gecko.tabspanel.TabsPanel.PanelView;
-import org.mozilla.gecko.util.HardwareUtils;
 
 import android.content.Context;
 import android.util.AttributeSet;
@@ -98,11 +97,6 @@ class RemoteTabsVerificationPanel extends LinearLayout implements PanelView {
 
     @Override
     public void show() {
-        // We don't have a tablet implementation of this panel.
-        if (HardwareUtils.isTablet()) {
-            return;
-        }
-
         refresh();
         setVisibility(View.VISIBLE);
     }
