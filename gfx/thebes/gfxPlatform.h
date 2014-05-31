0bf25101939f15348a25515ecf94e3bce64c4dd3
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2014-05-31 15:11:29 +0900

    Bug 1018034 - Remove the "gfx.font_rendering.harfbuzz.scripts" pref. r=jfkthame

--- gfx/thebes/gfxPlatform.h
+++ gfx/thebes/gfxPlatform.h
@@ -389,13 +389,6 @@ public:
      */
     bool UseGraphiteShaping();
 
-    /**
-     * Whether to use the harfbuzz shaper (depending on script complexity).
-     *
-     * This allows harfbuzz to be enabled selectively via the preferences.
-     */
-    bool UseHarfBuzzForScript(int32_t aScriptCode);
-
     // check whether format is supported on a platform or not (if unclear, returns true)
     virtual bool IsFontFormatSupported(nsIURI *aFontURI, uint32_t aFormatFlags) { return false; }
 
@@ -609,9 +602,6 @@ protected:
     // whether to always search font cmaps globally 
     // when doing system font fallback
     int8_t  mFallbackUsesCmaps;
-
-    // which scripts should be shaped with harfbuzz
-    int32_t mUseHarfBuzzScripts;
 
     // max character limit for words in word cache
     int32_t mWordCacheCharLimit;
