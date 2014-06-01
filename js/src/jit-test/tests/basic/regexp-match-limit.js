8029e7e0f5f0a14f04bfa4b2cfcc94f1734ef0c9
Till Schneidereit <till@tillschneidereit.net>
2014-06-01 18:03:38 +0200

    Bug 998785 - Don't throw error for regexps Yarr bails on. r=jandem, a=sledru

--- js/src/jit-test/tests/basic/regexp-match-limit.js
+++
@@ -1,9 +0,0 @@
-// See bug 953013
-
-load(libdir + "asserts.js");
-
-function test() {
-    var input = Array(2499999+1).join('a');
-    var result = /^([\w])+$/.test(input);
-}
-assertThrowsInstanceOf(test, InternalError);
