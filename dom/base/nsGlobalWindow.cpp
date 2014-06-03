205a271608145a21239f7b0e490ea9d6a53bc87c
Catalin Badea <cbadea@mozilla.com>
2014-06-03 14:01:00 -0400

    Bug 1019761 - Fix null dialogArguments deref. r=bz

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -13635,6 +13635,11 @@ nsGlobalWindow::GetDialogArguments(JSContext* aCx, ErrorResult& aError)
   MOZ_ASSERT(IsModalContentWindow(),
              "This should only be called on modal windows!");
 
+  if (!mDialogArguments) {
+    MOZ_ASSERT(mIsClosed, "This window should be closed!");
+    return JS::UndefinedValue();
+  }
+
   // This does an internal origin check, and returns undefined if the subject
   // does not subsumes the origin of the arguments.
   JS::Rooted<JSObject*> wrapper(aCx, GetWrapper());
