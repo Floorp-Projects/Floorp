11367a7bea41c9d7e5d20b98e19f8a5350c85176
Ehsan Akhgari <ehsan@mozilla.com>
2014-06-03 18:28:18 -0400

    Bug 1015783 - Add a devtools API for Web Audio; r=padenot,smaug
    See bug 980506 for an extensive discussion about this.  This patch adds
    three APIs to AudioNode in order for us to be able to build awesome
    devtools on top of it.
    
    * Weak reference API.
      This patch allows one to hold a weak reference to all AudioNode's
      using Components.utils.getWeakReference().  That way, the devtool's
      inspection code would not change the lifetime of AudioNodes.
    * AudioNode.id
      This is a chrome-only unique and monotonically incrementing ID for
      AudioNode objects.  It is supposed to be used in order for the
      devtools to be able to identify a node without having to keep it
      alive.
    * webaudio-node-demise
      This is an observer notification that is called every time an
      AudioNode gets destroyed inside Gecko.  The ID of the corresponding
      node is passed to this notification.

--- content/media/webaudio/AudioNode.h
+++ content/media/webaudio/AudioNode.h
@@ -16,6 +16,7 @@
 #include "MediaStreamGraph.h"
 #include "WebAudioUtils.h"
 #include "mozilla/MemoryReporting.h"
+#include "nsWeakReference.h"
 
 namespace mozilla {
 
@@ -82,7 +83,8 @@ private:
  * still alive, and will still be alive when it receives a message from the
  * engine.
  */
-class AudioNode : public DOMEventTargetHelper
+class AudioNode : public DOMEventTargetHelper,
+                  public nsSupportsWeakReference
 {
 protected:
   // You can only use refcounting to delete this object
@@ -132,6 +134,8 @@ public:
   // constant for the lifetime of the node. Both default to 1.
   virtual uint16_t NumberOfInputs() const { return 1; }
   virtual uint16_t NumberOfOutputs() const { return 1; }
+
+  uint32_t Id() const { return mId; }
 
   uint32_t ChannelCount() const { return mChannelCount; }
   virtual void SetChannelCount(uint32_t aChannelCount, ErrorResult& aRv)
@@ -266,6 +270,12 @@ private:
   uint32_t mChannelCount;
   ChannelCountMode mChannelCountMode;
   ChannelInterpretation mChannelInterpretation;
+  const uint32_t mId;
+#ifdef DEBUG
+  // In debug builds, check to make sure that the node demise notification has
+  // been properly sent before the node is destroyed.
+  bool mDemiseNotified;
+#endif
 };
 
 }
