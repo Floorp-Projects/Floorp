a7c9284e2d4f31f77e6f8ba1bfd488b9a58d899f
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2014-06-03 12:42:33 +0100

    Bug 979207 - Add logging and better yielding to browser_968447_bookmarks_toolbar_items_in_panel.js. r=mikedeboer, a=test-only

--- browser/components/customizableui/test/browser_968447_bookmarks_toolbar_items_in_panel.js
+++ browser/components/customizableui/test/browser_968447_bookmarks_toolbar_items_in_panel.js
@@ -11,22 +11,42 @@ add_task(function() {
   yield startCustomizing();
   CustomizableUI.addWidgetToArea("personal-bookmarks", CustomizableUI.AREA_PANEL);
   yield endCustomizing();
+
   yield PanelUI.show();
+
   let bookmarksToolbarPlaceholder = document.getElementById(buttonId);
   ok(bookmarksToolbarPlaceholder.classList.contains("toolbarbutton-1"),
      "Button should have toolbarbutton-1 class");
   is(bookmarksToolbarPlaceholder.getAttribute("wrap"), "true",
      "Button should have the 'wrap' attribute");
-  yield PanelUI.hide();
 
-  let newWin = yield openAndLoadWindow();
+  info("Waiting for panel to close");
+  let panelHiddenPromise = promisePanelHidden(window);
+  PanelUI.hide();
+  yield panelHiddenPromise;
+
+  info("Waiting for window to open");
+  let newWin = yield openAndLoadWindow({}, true);
+
+  info("Waiting for panel in new window to open");
   yield newWin.PanelUI.show();
   let newWinBookmarksToolbarPlaceholder = newWin.document.getElementById(buttonId);
   ok(newWinBookmarksToolbarPlaceholder.classList.contains("toolbarbutton-1"),
      "Button in new window should have toolbarbutton-1 class");
   is(newWinBookmarksToolbarPlaceholder.getAttribute("wrap"), "true",
      "Button in new window should have 'wrap' attribute");
-  yield newWin.PanelUI.hide();
+
+  info("Waiting for panel in new window to close");
+  panelHiddenPromise = promisePanelHidden(newWin);
+  newWin.PanelUI.hide();
+  yield panelHiddenPromise;
+
+  info("Waiting for new window to close");
   yield promiseWindowClosed(newWin);
+});
+
+add_task(function asyncCleanUp() {
+  yield endCustomizing();
   CustomizableUI.reset();
 });
+
