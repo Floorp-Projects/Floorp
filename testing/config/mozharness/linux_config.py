b6e1dbab82a2e27dc5f98f71751cfea268c79f6e
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-06-03 08:46:09 +0200

    Backed out changeset 05ddfd9f4f09 (bug 815002) for hitting bug 1017634 again.

--- testing/config/mozharness/linux_config.py
+++ testing/config/mozharness/linux_config.py
@@ -12,8 +12,7 @@ config = {
         "--extra-profile-file=tests/bin/plugins", "--symbols-path=%(symbols_path)s",
         "--certificate-path=tests/certs", "--autorun", "--close-when-done",
         "--console-level=INFO", "--setpref=webgl.force-enabled=true",
-        "--quiet",
-        "--use-test-media-devices"
+        "--quiet"
     ],
     "webapprt_options": [
         "--app=%(app_path)s", "--utility-path=tests/bin",
