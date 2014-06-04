aa964f21c141e508a129d916da48c54b9f75cda0
Tim Taubert <ttaubert@mozilla.com>
2014-06-04 08:33:13 +0200

    Bug 923315 - Ensure that sessionstore.js writes don't interfere with browser_upgrade_backup.js. r=yoric, a=test-only

--- browser/components/sessionstore/test/browser_upgrade_backup.js
+++ browser/components/sessionstore/test/browser_upgrade_backup.js
@@ -16,6 +16,10 @@ function test() {
       const PREF_UPGRADE = "browser.sessionstore.upgradeBackup.latestBuildID";
       let buildID = Services.appinfo.platformBuildID;
 
+      // Write state once before starting the test to
+      // ensure sessionstore.js writes won't happen in between.
+      yield forceSaveState();
+
       // Force backup to take place with a file decided by us
       Services.prefs.setCharPref(PREF_UPGRADE, "");
       let contents = "browser_upgrade_backup.js";
@@ -28,13 +32,13 @@ function test() {
       is((yield OS.File.exists(pathBackup)), true, "upgrade backup file has been created");
 
       let data = yield OS.File.read(pathBackup);
-      is(contents, (new TextDecoder()).decode(data), "upgrade backup contains the expected contents");
+      is(new TextDecoder().decode(data), contents, "upgrade backup contains the expected contents");
 
       // Ensure that we don't re-backup by accident
       yield OS.File.writeAtomic(pathStore, "something else entirely", { tmpPath: pathStore + ".tmp" });
       yield SessionStore._internal._performUpgradeBackup();
       data = yield OS.File.read(pathBackup);
-      is(contents, (new TextDecoder()).decode(data), "upgrade backup hasn't changed");
+      is(new TextDecoder().decode(data), contents, "upgrade backup hasn't changed");
 
     } catch (ex) {
       ok(false, "Uncaught error: " + ex + " at " + ex.stack);
