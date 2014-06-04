03ce6fee26f48abab497752bb9da2f81d4d13318
Jonathan Watt <jwatt@jwatt.org>
2014-06-04 08:08:39 +0100

    Bug 1019753, part 2 - Use Moz2D DataSourceSurfaces instead of Thebes gfxImageSurfaces in DocumentRendererParent.cpp and ImageEncoder.cpp. r=mattwoodrow
    --HG--
    extra : rebase_source : fc39f0aede22e48ea853b1978eddeb8cc8c43a60

--- content/canvas/src/DocumentRendererParent.cpp
+++ content/canvas/src/DocumentRendererParent.cpp
@@ -2,11 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/gfx/2D.h"
 #include "mozilla/ipc/DocumentRendererParent.h"
-#include "gfxImageSurface.h"
+#include "mozilla/RefPtr.h"
 #include "gfxPattern.h"
 #include "nsICanvasRenderingContextInternal.h"
 
+using namespace mozilla;
+using namespace mozilla::gfx;
 using namespace mozilla::ipc;
 
 DocumentRendererParent::DocumentRendererParent()
@@ -28,12 +31,12 @@ void DocumentRendererParent::DrawToCanvas(const nsIntSize& aSize,
     if (!mCanvas || !mCanvasContext)
         return;
 
-    nsRefPtr<gfxImageSurface> surf =
-        new gfxImageSurface(reinterpret_cast<uint8_t*>(const_cast<nsCString&>(aData).BeginWriting()),
-                            gfxIntSize(aSize.width, aSize.height),
-                            aSize.width * 4,
-                            gfxImageFormat::ARGB32);
-    nsRefPtr<gfxPattern> pat = new gfxPattern(surf);
+    RefPtr<DataSourceSurface> dataSurface =
+        Factory::CreateWrappingDataSourceSurface(reinterpret_cast<uint8_t*>(const_cast<nsCString&>(aData).BeginWriting()),
+                                                 aSize.width * 4,
+                                                 IntSize(aSize.width, aSize.height),
+                                                 SurfaceFormat::B8G8R8A8);
+    nsRefPtr<gfxPattern> pat = new gfxPattern(dataSurface, Matrix());
 
     gfxRect rect(gfxPoint(0, 0), gfxSize(aSize.width, aSize.height));
     mCanvasContext->NewPath();
