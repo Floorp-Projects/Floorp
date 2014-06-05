ca6f29a1ce03262dbc04da560a4f701d01a8dee7
Jan de Mooij <jdemooij@mozilla.com>
2014-06-05 12:02:00 +0200

    Bug 1019585 part 2 - Make RopeMatch work with Latin1 strings. r=luke

--- js/src/jit-test/tests/latin1/search.js
+++ js/src/jit-test/tests/latin1/search.js
@@ -20,3 +20,41 @@ function testSearchFlat() {
     assertEq("fooBar12345\u1200".search("5\u1201"), -1);
 }
 testSearchFlat();
+
+function testSearchRope() {
+    // Tests for the RopeMatch algorithm.
+    var s1 = "foobarbaz0123456789".repeat(10);
+    s1.indexOf("333"); // flatten
+    s1 = toLatin1(s1);
+
+    var ropeMixed = s1 + "abcdef\u1200";
+    assertEq(isLatin1(ropeMixed), false);
+
+    var abc = toLatin1("abc");
+    var baz = toLatin1("baz");
+
+    // Mixed + Latin1
+    assertEq(ropeMixed.search(abc), 190);
+    assertEq(ropeMixed.search(baz), 6);
+
+    // Mixed + TwoByte
+    assertEq(ropeMixed.search("def\u1200"), 193);
+
+    // Latin1 + Latin1
+    s1 = "foobarbaz0123456789".repeat(10);
+    var ropeLatin1 = s1 + toLatin1("abcdef\u00AA");
+    assertEq(isLatin1(ropeLatin1), false);
+    assertEq(ropeLatin1.search(abc), 190);
+
+    // Latin1 + TwoByte
+    assertEq(ropeLatin1.search("\u1200bc".substr(1)), 191);
+
+    // TwoByte + Latin1
+    s1 = "foobarbaz0123456789\u11AA".repeat(10);
+    var ropeTwoByte = s1 + "abcdef\u1200";
+    assertEq(ropeTwoByte.search(abc), 200);
+
+    // TwoByte + TwoByte
+    assertEq(ropeTwoByte.search("def\u1200"), 203);
+}
+testSearchRope();
