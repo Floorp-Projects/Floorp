91bbb9e83100358b279283e2b49a76a6612a2b12
Ryan VanderMeulen <ryanvm@gmail.com>
2014-06-05 18:03:11 -0400

    Bug 1021287 - Update NSPR to 4.10.6 RTM. r=kaie, a=lsblakk

--- nsprpub/pr/include/prinit.h
+++ nsprpub/pr/include/prinit.h
@@ -31,10 +31,10 @@ PR_BEGIN_EXTERN_C
 ** The format of the version string is
 **     "<major version>.<minor version>[.<patch level>] [<Beta>]"
 */
-#define PR_VERSION  "4.10.5"
+#define PR_VERSION  "4.10.6"
 #define PR_VMAJOR   4
 #define PR_VMINOR   10
-#define PR_VPATCH   5
+#define PR_VPATCH   6
 #define PR_BETA     PR_FALSE
 
 /*
