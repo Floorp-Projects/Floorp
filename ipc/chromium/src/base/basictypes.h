3a7649c68615c97bf011172c162863a197d688cb
Daniel Holbert <dholbert@cs.stanford.edu>
2014-06-04 18:13:02 -0700

    Bug 1020643: Annotate COMPILE_ASSERT's typedef as permissibly unused, to fix GCC 4.8 build warning. r=Waldo

--- ipc/chromium/src/base/basictypes.h
+++ ipc/chromium/src/base/basictypes.h
@@ -154,6 +154,15 @@ inline To implicit_cast(From const &f) {
   return f;
 }
 
+// The COMPILE_ASSERT macro (below) creates an otherwise-unused typedef.  This
+// triggers compiler warnings with gcc 4.8 and higher, so mark the typedef
+// as permissibly-unused to disable the warnings.
+#  if defined(__GNUC__)
+#    define COMPILE_ASSERT_UNUSED_ATTRIBUTE __attribute__((unused))
+#  else
+#    define COMPILE_ASSERT_UNUSED_ATTRIBUTE /* nothing */
+#  endif
+
 // The COMPILE_ASSERT macro can be used to verify that a compile time
 // expression is true. For example, you could use it to verify the
 // size of a static array:
@@ -176,7 +185,8 @@ struct CompileAssert {
 };
 
 #define COMPILE_ASSERT(expr, msg) \
-  typedef CompileAssert<(bool(expr))> msg[bool(expr) ? 1 : -1]
+  typedef CompileAssert<(bool(expr))> msg[bool(expr) ? 1 : -1] \
+  COMPILE_ASSERT_UNUSED_ATTRIBUTE
 #endif
 
 // Implementation details of COMPILE_ASSERT:
