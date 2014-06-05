0a32d271a3d8ad1a381285055353106b362aa882
Richard Newman <rnewman@mozilla.com>
2014-06-04 19:21:33 -0700

    Bug 1019981 - Part 1: remove Language section from preferences if locale switching is disabled. r=nalexander

--- mobile/android/base/LocaleManager.java
+++ mobile/android/base/LocaleManager.java
@@ -18,6 +18,11 @@ import android.content.res.Resources;
  */
 public interface LocaleManager {
     void initialize(Context context);
+
+    /**
+     * @return true if locale switching is enabled.
+     */
+    boolean isEnabled();
     Locale getCurrentLocale(Context context);
     String getAndApplyPersistedLocale(Context context);
     void correctLocale(Context context, Resources resources, Configuration newConfig);
