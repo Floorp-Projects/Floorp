beaa8787a1c6721714cde0a1edc07123329074a2
Nathan Froyd <froydnj@mozilla.com>
2014-06-05 14:47:13 -0400

    Bug 1019160 - part 4 - use jsids to access JS-implemented interface properties; r=bz

--- dom/bindings/CallbackInterface.cpp
+++ dom/bindings/CallbackInterface.cpp
@@ -13,15 +13,18 @@ namespace mozilla {
 namespace dom {
 
 bool
-CallbackInterface::GetCallableProperty(JSContext* cx, const char* aPropName,
+CallbackInterface::GetCallableProperty(JSContext* cx, JS::Handle<jsid> aPropId,
                                        JS::MutableHandle<JS::Value> aCallable)
 {
-  if (!JS_GetProperty(cx, CallbackPreserveColor(), aPropName, aCallable)) {
+  if (!JS_GetPropertyById(cx, CallbackPreserveColor(), aPropId, aCallable)) {
     return false;
   }
   if (!aCallable.isObject() ||
       !JS_ObjectIsCallable(cx, &aCallable.toObject())) {
-    nsPrintfCString description("Property '%s'", aPropName);
+    char* propName =
+      JS_EncodeString(cx, JS_FORGET_STRING_FLATNESS(JSID_TO_FLAT_STRING(aPropId)));
+    nsPrintfCString description("Property '%s'", propName);
+    JS_free(cx, propName);
     ThrowErrorMessage(cx, MSG_NOT_CALLABLE, description.get());
     return false;
   }
