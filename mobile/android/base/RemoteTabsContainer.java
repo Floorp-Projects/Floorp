bf9029f72f923258ce5ed3f96cf02496b55c02db
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-06-06 11:25:08 -0700

    Bug 1021123 - Improve colors in remote tabs pull-to-refresh. r=mcomella a=sledru

--- mobile/android/base/RemoteTabsContainer.java
+++ mobile/android/base/RemoteTabsContainer.java
@@ -42,8 +42,8 @@ public class RemoteTabsContainer extends GeckoSwipeRefreshLayout
         list = (RemoteTabsList) child;
 
         // Must be called after the child view has been added.
-        setColorScheme(R.color.swipe_refresh_orange_dark, R.color.background_tabs,
-                       R.color.swipe_refresh_orange_dark, R.color.background_tabs);
+        setColorScheme(R.color.swipe_refresh_orange1, R.color.swipe_refresh_orange2,
+                       R.color.swipe_refresh_orange3, R.color.swipe_refresh_orange4);
     }
 
 
