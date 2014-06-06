c9849998cf4f59b0017744394ea263ae94c68413
Jason Orendorff <jorendorff@mozilla.com>
2014-06-06 14:13:16 -0400

    Bug 904723, part 4 - In Array.from, only fetch the @@iterator property once. r=till. DONTBUILD.

--- js/src/tests/ecma_6/Array/from_proxy.js
+++ js/src/tests/ecma_6/Array/from_proxy.js
@@ -36,9 +36,17 @@ assertDeepEq(log, ["define 0", "define 1", "define 2", "set length"]);
 // calls handler.get on it.
 log = [];
 assertDeepEq(Array.from(new LoggingProxy([3, 4, 5])), [3, 4, 5]);
-assertDeepEq(log, ["get @@iterator", "get @@iterator",
+assertDeepEq(log, ["get @@iterator",
                    "get length", "get 0", "get length", "get 1", "get length", "get 2",
                    "get length"]);
+
+// Array-like iteration only gets the length once.
+log = [];
+var arr = [5, 6, 7];
+arr["@@iterator"] = undefined;
+assertDeepEq(Array.from(new LoggingProxy(arr)), [5, 6, 7]);
+assertDeepEq(log, ["get @@iterator",
+                   "get length", "get 0", "get 1", "get 2"]);
 
 if (typeof reportCompare === 'function')
     reportCompare(0, 0);
