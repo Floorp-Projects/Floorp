0fcf041a2cf3ac408a383a80ea3f21577d5e37e2
Sankha Narayan Guria <sankha93@gmail.com>
2014-06-06 17:32:15 +0200

    Bug 1011541 - Implement Div Recover instruction. r=nbp

--- js/src/jit/Recover.h
+++ js/src/jit/Recover.h
@@ -27,6 +27,7 @@ namespace jit {
     _(Add)                                      \
     _(Sub)                                      \
     _(Mul)                                      \
+    _(Div)                                      \
     _(Mod)                                      \
     _(NewObject)                                \
     _(NewDerivedTypedObject)
@@ -218,6 +219,21 @@ class RMul MOZ_FINAL : public RInstruction
 
   public:
     RINSTRUCTION_HEADER_(Mul)
+
+    virtual uint32_t numOperands() const {
+        return 2;
+    }
+
+    bool recover(JSContext *cx, SnapshotIterator &iter) const;
+};
+
+class RDiv MOZ_FINAL : public RInstruction
+{
+  private:
+    bool isFloatOperation_;
+
+  public:
+    RINSTRUCTION_HEADER_(Div)
 
     virtual uint32_t numOperands() const {
         return 2;
