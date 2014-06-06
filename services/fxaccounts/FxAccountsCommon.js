41c38f2e3151a684689f178ee42b2181caf1419b
Sam Penrose <spenrose@mozilla.com>
2014-06-06 10:42:22 -0700

    Bug 1008901 - Fire 'onlogin' when the account is verified. r=jedp

--- services/fxaccounts/FxAccountsCommon.js
+++ services/fxaccounts/FxAccountsCommon.js
@@ -83,6 +83,8 @@ this.POLL_STEP          = 1000 * 3;         // 3 seconds
 this.ONLOGIN_NOTIFICATION = "fxaccounts:onlogin";
 this.ONVERIFIED_NOTIFICATION = "fxaccounts:onverified";
 this.ONLOGOUT_NOTIFICATION = "fxaccounts:onlogout";
+// Internal to services/fxaccounts only
+this.ON_FXA_UPDATE_NOTIFICATION = "fxaccounts:update";
 
 // UI Requests.
 this.UI_REQUEST_SIGN_IN_FLOW = "signInFlow";
