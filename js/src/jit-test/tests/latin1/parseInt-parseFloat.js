0bb824d1821879b62a4589e50820698508af58ac
Jan de Mooij <jdemooij@mozilla.com>
2014-06-06 11:17:51 +0200

    Bug 1020420 part 3 - Make parseFloat work with Latin1 strings. r=njn

--- js/src/jit-test/tests/latin1/parseInt-parseFloat.js
+++ js/src/jit-test/tests/latin1/parseInt-parseFloat.js
@@ -6,7 +6,7 @@ function testParseInt() {
     assertEq(parseInt(toLatin1("xyz")), NaN);
     assertEq(parseInt(toLatin1("1234GHI"), 17), 94298);
     assertEq(parseInt(toLatin1("9007199254749999")), 9007199254750000);
-    assertEq(parseInt(toLatin1("9007199254749998"), 16), 10378291982571444000);
+    assertEq(parseInt(toLatin1("  9007199254749998"), 16), 10378291982571444000);
 
     // TwoByte
     assertEq(parseInt("12345abc\u1200"), 12345);
@@ -15,6 +15,25 @@ function testParseInt() {
     assertEq(parseInt("\u1200"), NaN);
     assertEq(parseInt("1234GHI\u1200", 17), 94298);
     assertEq(parseInt("9007199254749999\u1200"), 9007199254750000);
-    assertEq(parseInt("9007199254749998\u1200", 16), 10378291982571444000);
+    assertEq(parseInt("  9007199254749998\u1200", 16), 10378291982571444000);
 }
 testParseInt();
+
+function testParseFloat() {
+    // Latin1
+    assertEq(parseFloat(toLatin1("3.1415")), 3.1415);
+    assertEq(parseFloat(toLatin1("  -1234")), -1234);
+    assertEq(parseFloat(toLatin1("\u00AA")), NaN);
+    assertEq(parseFloat(toLatin1("Infinityabc")), Infinity);
+    assertEq(parseFloat(toLatin1("-Infinity")), -Infinity);
+    assertEq(parseFloat(toLatin1("\t\t\t+Infinity")), Infinity);
+
+    // TwoByte
+    assertEq(parseFloat("3.1415\u0FFF"), 3.1415);
+    assertEq(parseFloat("  -1234\u0FFF"), -1234);
+    assertEq(parseFloat("\u00AA\u0FFF"), NaN);
+    assertEq(parseFloat("Infinityabc\u0FFF"), Infinity);
+    assertEq(parseFloat("-Infinity\u0FFF"), -Infinity);
+    assertEq(parseFloat("\t\t\t+Infinity\u0FFF"), Infinity);
+}
+testParseFloat();
