c204af086a5a2d8c3bffe56ce91cfcdf05a8c6ea
Patrick McManus <mcmanus@ducksong.com>
2014-06-07 11:54:38 -0400

    bug 1022195 - http2 allow indexing of cookie crumbs with decent entropy r=hurley

--- netwerk/protocol/http/Http2Compression.cpp
+++ netwerk/protocol/http/Http2Compression.cpp
@@ -1062,10 +1062,12 @@ Http2Compressor::EncodeHeaderBlock(const nsCString &nvInput,
         }
         nsDependentCSubstring cookie = Substring(beginBuffer + nextCookie,
                                                  beginBuffer + semiSpaceIndex);
-        ProcessHeader(nvPair(name, cookie), true);
+        // cookies less than 20 bytes are not indexed
+        ProcessHeader(nvPair(name, cookie), name.Length() < 20);
         nextCookie = semiSpaceIndex + 2;
       }
     } else {
+      // allow indexing of every non-cookie except authorization
       ProcessHeader(nvPair(name, value), name.EqualsLiteral("authorization"));
     }
   }
