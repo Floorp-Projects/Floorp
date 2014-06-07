5eb7d501d62eaabccd1e894c12a93d80d34ba53a
Randell Jesup <rjesup@jesup.org>
2014-06-07 13:19:12 -0400

    Bug 1022212: enable WebRTC load adaptation by default rs=jesup,gcp

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -250,7 +250,7 @@ pref("media.apple.mp3.enabled", true);
 #ifdef MOZ_WEBRTC
 pref("media.navigator.enabled", true);
 pref("media.navigator.video.enabled", true);
-pref("media.navigator.load_adapt", false);
+pref("media.navigator.load_adapt", true);
 pref("media.navigator.load_adapt.measure_interval",1000);
 pref("media.navigator.load_adapt.avg_seconds",3);
 pref("media.navigator.load_adapt.high_load","0.90");
