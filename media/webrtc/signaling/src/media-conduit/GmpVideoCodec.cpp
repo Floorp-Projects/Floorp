a1431b3ebc9f335ec0e8d7ba2cfbc44cc9232d38
Randell Jesup <rjesup@jesup.org>
2014-06-08 17:25:18 -0400

    Bug 999704: Implement GMP codec interface to webrtc (not enabled yet) r=joshmoz,ehugg,jesup,pkerr

---
+++ media/webrtc/signaling/src/media-conduit/GmpVideoCodec.cpp
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "WebrtcGmpVideoCodec.h"
+#include "GmpVideoCodec.h"
+
+namespace mozilla {
+
+VideoEncoder* GmpVideoCodec::CreateEncoder() {
+  return static_cast<VideoEncoder*>(new WebrtcGmpVideoEncoder());
+}
+
+VideoDecoder* GmpVideoCodec::CreateDecoder() {
+  return static_cast<VideoDecoder*>(new WebrtcGmpVideoDecoder());
+}
+
+}
