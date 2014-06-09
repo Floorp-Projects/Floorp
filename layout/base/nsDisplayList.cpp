65679f5d77c2d8f07ded8fd40642b62323553ced
Robert O'Callahan <robert@ocallahan.org>
2014-06-09 16:47:59 +1200

    Bug 1022612. Part 9: nsDisplayScrollInfoLayer destructor does not need to destroy ScrollLayerCount. r=mattwoodrow
    Getting the timing of this right without processing display items in reverse
    order is hard. But it doesn't matter if this property sticks around anyway.

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -4122,8 +4122,6 @@ nsDisplayScrollInfoLayer::nsDisplayScrollInfoLayer(
 
 nsDisplayScrollInfoLayer::~nsDisplayScrollInfoLayer()
 {
-  FrameProperties props = mScrolledFrame->Properties();
-  props.Remove(nsIFrame::ScrollLayerCount());
   MOZ_COUNT_DTOR(nsDisplayScrollInfoLayer);
 }
 
