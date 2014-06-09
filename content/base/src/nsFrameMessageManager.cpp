1a40304c9b4b6b697bf244483e2f0f5c3a12eb73
Ed Morley <emorley@mozilla.com>
2014-06-09 11:28:05 +0100

    Backed out changeset a7872dbfa5ce (bug 879475)

--- content/base/src/nsFrameMessageManager.cpp
+++ content/base/src/nsFrameMessageManager.cpp
@@ -1870,6 +1870,8 @@ NS_NewParentProcessMessageManager(nsIMessageBroadcaster** aResult)
 {
   NS_ASSERTION(!nsFrameMessageManager::sParentProcessManager,
                "Re-creating sParentProcessManager");
+  NS_ENSURE_TRUE(XRE_GetProcessType() == GeckoProcessType_Default,
+                 NS_ERROR_NOT_AVAILABLE);
   nsRefPtr<nsFrameMessageManager> mm = new nsFrameMessageManager(nullptr,
                                                                  nullptr,
                                                                  MM_CHROME | MM_PROCESSMANAGER | MM_BROADCASTER);
