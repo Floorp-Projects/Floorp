3725dde4d0d6a67f1aa0b9e1e663d35cca00c222
Kartikaya Gupta <kgupta@mozilla.com>
2014-06-09 14:02:40 -0400

    Bug 1022398 - Remove declaration for method that was removed a long time ago. r=nical

--- gfx/layers/LayersLogging.h
+++ gfx/layers/LayersLogging.h
@@ -16,7 +16,6 @@
 #include "nsRegion.h"                   // for nsIntRegion
 #include "nscore.h"                     // for nsACString, etc
 
-class gfx3DMatrix;
 struct gfxRGBA;
 struct nsIntPoint;
 struct nsIntRect;
@@ -44,10 +43,6 @@ AppendToString(nsACString& s, FrameMetrics::ViewID n,
 
 nsACString&
 AppendToString(nsACString& s, const gfxRGBA& c,
-               const char* pfx="", const char* sfx="");
-
-nsACString&
-AppendToString(nsACString& s, const gfx3DMatrix& m,
                const char* pfx="", const char* sfx="");
 
 nsACString&
