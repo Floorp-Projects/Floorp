896dfec6e23a52ab2ba5d34159f69f3aed71e880
Matt Woodrow <mwoodrow@mozilla.com>
2014-06-10 17:51:24 -0400

    Bug 940845 - Part 6: Cache blurs when possible. r=Bas

--- gfx/thebes/gfxBlur.h
+++ gfx/thebes/gfxBlur.h
@@ -86,6 +86,8 @@ public:
         return mContext;
     }
 
+    mozilla::TemporaryRef<mozilla::gfx::SourceSurface> DoBlur(mozilla::gfx::DrawTarget* aDT, mozilla::gfx::IntPoint* aTopLeft);
+
     /**
      * Does the actual blurring/spreading and mask applying. Users of this
      * object must have drawn whatever they want to be blurred onto the internal
@@ -128,6 +130,8 @@ public:
                               const gfxRGBA& aShadowColor,
                               const gfxRect& aDirtyRect,
                               const gfxRect& aSkipRect);
+
+    static void ShutdownBlurCache();
 
 
 
