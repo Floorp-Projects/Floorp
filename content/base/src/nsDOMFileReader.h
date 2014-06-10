e04008415ce477b05dbaad94ef1da45b7c44f650
Shihua Zheng <szheng@mozilla.com>
2014-06-10 11:48:36 -0700

    Bug 1017765 - Replace nsChannel with nsAsyncInputStream for FileReader API, r=bent

--- content/base/src/nsDOMFileReader.h
+++ content/base/src/nsDOMFileReader.h
@@ -17,14 +17,12 @@
 #include "nsIJSNativeInitializer.h"
 #include "prtime.h"                
 #include "nsITimer.h"              
+#include "nsIAsyncInputStream.h"
 
 #include "nsIDOMFile.h"
 #include "nsIDOMFileReader.h"
 #include "nsIDOMFileList.h"
-#include "nsIInputStream.h"
 #include "nsCOMPtr.h"
-#include "nsIStreamLoader.h"
-#include "nsIChannel.h"
 
 #include "FileIOObject.h"
 
@@ -50,12 +48,11 @@ public:
 
   // FileIOObject overrides
   virtual void DoAbort(nsAString& aEvent) MOZ_OVERRIDE;
-  NS_IMETHOD DoOnStopRequest(nsIRequest* aRequest, nsISupports* aContext,
-                             nsresult aStatus, nsAString& aSuccessEvent,
-                             nsAString& aTerminationEvent) MOZ_OVERRIDE;
-  NS_IMETHOD DoOnDataAvailable(nsIRequest* aRequest, nsISupports* aContext,
-                               nsIInputStream* aInputStream, uint64_t aOffset,
-                               uint32_t aCount) MOZ_OVERRIDE;
+
+  virtual nsresult DoReadData(nsIAsyncInputStream* aStream, uint64_t aCount) MOZ_OVERRIDE;
+
+  virtual nsresult DoOnLoadEnd(nsresult aStatus, nsAString& aSuccessEvent,
+                               nsAString& aTerminationEvent) MOZ_OVERRIDE;
 
   nsPIDOMWindow* GetParentObject() const
   {
@@ -71,11 +68,13 @@ public:
     MOZ_ASSERT(aBlob);
     ReadFileContent(aCx, aBlob, EmptyString(), FILE_AS_ARRAYBUFFER, aRv);
   }
+
   void ReadAsText(nsIDOMBlob* aBlob, const nsAString& aLabel, ErrorResult& aRv)
   {
     MOZ_ASSERT(aBlob);
     ReadFileContent(nullptr, aBlob, aLabel, FILE_AS_TEXT, aRv);
   }
+
   void ReadAsDataURL(nsIDOMBlob* aBlob, ErrorResult& aRv)
   {
     MOZ_ASSERT(aBlob);
@@ -142,8 +141,7 @@ protected:
   eDataFormat mDataFormat;
 
   nsString mResult;
-  nsCOMPtr<nsIPrincipal> mPrincipal;
-  
+
   JS::Heap<JSObject*> mResultArrayBuffer;
 };
 
