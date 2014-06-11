9d48dfe9799c978dcbd704994e938382bbbba97d
Kartikaya Gupta <kgupta@mozilla.com>
2014-06-11 12:42:45 -0400

    Bug 1023882 - Part 6 - Move all SetRepeatTransaction calls into ClientTiledThebesLayer so it's easier to follow the flow. r=Cwiiis

--- gfx/layers/client/TiledContentClient.cpp
+++ gfx/layers/client/TiledContentClient.cpp
@@ -1064,13 +1064,10 @@ ClientTiledLayerBuffer::ComputeProgressiveUpdateRegion(const nsIntRegion& aInval
     // sure it happens in the same transaction by requesting this work be
     // repeated immediately.
     // If this is unnecessary, the remaining work will be done tile-by-tile in
-    // subsequent transactions.
-    if (!drawingLowPrecision && paintInSingleTransaction) {
-      return true;
-    }
-
-    mManager->SetRepeatTransaction();
-    return false;
+    // subsequent transactions. The caller code is responsible for scheduling
+    // the subsequent transactions as long as we don't set the mPaintFinished
+    // flag to true.
+    return (!drawingLowPrecision && paintInSingleTransaction);
   }
 
   // We're not repeating painting and we've not requested a repeat transaction,
