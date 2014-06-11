8322207d9d9af12fd55e53ad9d654ed9e121eb4b
Shihua Zheng <szheng@mozilla.com>
2014-06-11 16:39:04 -0700

    Bug 1024170 - Replace zone calls in JSCompartment::wrap with zoneFromAnyThread. r=bhackett

--- js/src/jscompartment.cpp
+++ js/src/jscompartment.cpp
@@ -271,12 +271,13 @@ JSCompartment::wrap(JSContext *cx, JSString **strp)
 
     /* If the string is already in this compartment, we are done. */
     JSString *str = *strp;
-    if (str->zone() == zone())
+    if (str->zoneFromAnyThread() == zone())
         return true;
 
     /* If the string is an atom, we don't have to copy. */
     if (str->isAtom()) {
-        JS_ASSERT(cx->runtime()->isAtomsZone(str->zone()));
+        JS_ASSERT(str->isPermanentAtom() ||
+                  cx->runtime()->isAtomsZone(str->zone()));
         return true;
     }
 
