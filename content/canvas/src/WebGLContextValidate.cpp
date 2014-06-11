d29e99ca32eb196d4d7d9078e8a7508ecd38d7da
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-06-10 18:20:12 -0700

    Bug 973815 - Implements WebGL extension EXT_blend_minmax - r=jgilbert,bzbarsky

--- content/canvas/src/WebGLContextValidate.cpp
+++ content/canvas/src/WebGLContextValidate.cpp
@@ -328,8 +328,7 @@ bool WebGLContext::ValidateBlendEquationEnum(GLenum mode, const char *info)
             return true;
         case LOCAL_GL_MIN:
         case LOCAL_GL_MAX:
-            if (IsWebGL2()) {
-                // http://www.opengl.org/registry/specs/EXT/blend_minmax.txt
+            if (IsExtensionEnabled(WebGLExtensionID::EXT_blend_minmax)) {
                 return true;
             }
             break;
