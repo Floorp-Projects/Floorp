e888797ec6b5fddba73e875be07e7b0f8a09d3df
Victor Porof <vporof@mozilla.com>
2014-06-12 12:16:59 -0400

    Bug 957952 - Hopefully fix browser_net_timing-division.js by waiting for the first request to finish. r=vporof, a=test-only

--- browser/devtools/netmonitor/test/browser_net_timing-division.js
+++ browser/devtools/netmonitor/test/browser_net_timing-division.js
@@ -15,17 +15,50 @@ function test() {
     RequestsMenu.lazyUpdate = false;
 
     waitForNetworkEvents(aMonitor, 2).then(() => {
-      let divisions = $all(".requests-menu-timings-division[division-scale=second]");
+      let millisecondDivs = $all(".requests-menu-timings-division[division-scale=millisecond]");
+      let secondDivs = $all(".requests-menu-timings-division[division-scale=second]");
+      let minuteDivs = $all(".requests-menu-timings-division[division-scale=minute]");
 
-      ok(divisions.length,
+      info("Number of millisecond divisions: " + millisecondDivs.length);
+      info("Number of second divisions: " + secondDivs.length);
+      info("Number of minute divisions: " + minuteDivs.length);
+
+      for (let div of millisecondDivs) {
+        info("Millisecond division: " + div.getAttribute("value"));
+      }
+      for (let div of secondDivs) {
+        info("Second division: " + div.getAttribute("value"));
+      }
+      for (let div of minuteDivs) {
+        info("Minute division: " + div.getAttribute("value"));
+      }
+
+      is(RequestsMenu.itemCount, 2,
+        "There should be only two requests made.");
+
+      let firstRequest = RequestsMenu.getItemAtIndex(0);
+      let lastRequest = RequestsMenu.getItemAtIndex(1);
+
+      info("First request happened at: " +
+        firstRequest.attachment.responseHeaders.headers.find(e => e.name == "Date").value);
+      info("Last request happened at: " +
+        lastRequest.attachment.responseHeaders.headers.find(e => e.name == "Date").value);
+
+      ok(secondDivs.length,
         "There should be at least one division on the seconds time scale.");
-      ok(divisions[0].getAttribute("value").match(/\d+\.\d{2}\s\w+/),
+      ok(secondDivs[0].getAttribute("value").match(/\d+\.\d{2}\s\w+/),
         "The division on the seconds time scale looks legit.");
 
       teardown(aMonitor).then(finish);
     });
 
-    aDebuggee.performRequests(1);
-    window.setTimeout(() => aDebuggee.performRequests(1), 2000);
+    aDebuggee.get(Math.random(), () => {
+      // Timeout needed for having enough divisions on the time scale.
+      setTimeout(() => {
+        aDebuggee.get(Math.random(), () => {
+          // Done.
+        });
+      }, 3000);
+    });
   });
 }
