cbd7d46b26a766a325c1492e9b3a775b99b23c34
Paul Rouget <paul@mozilla.com>
2014-06-12 14:47:35 +0800

    Bug 1007057 - build appmgr v2: remove configure option. r=mshal r=ochameau

--- browser/devtools/webide/content/webide.js
+++ browser/devtools/webide/content/webide.js
@@ -157,12 +157,14 @@ let UI = {
   unbusy: function() {
     document.querySelector("window").classList.remove("busy")
     this.updateCommands();
+    this._busyPromise = null;
   },
 
   busyUntil: function(promise, operationDescription) {
     // Freeze the UI until the promise is resolved. A 30s timeout
     // will unfreeze the UI, just in case the promise never gets
     // resolved.
+    this._busyPromise = promise;
     let timeout = setTimeout(() => {
       this.unbusy();
       UI.reportError("error_operationTimeout", operationDescription);
