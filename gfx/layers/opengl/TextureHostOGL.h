3b154f79dbd19272588eee3f8a8a005b3c088c79
Sotaro Ikeda <sikeda@mozilla.com>
2014-06-12 18:46:25 -0700

    Bug 1024144 - Deliver acquire fence to HwComposer. r=sushil, r=nical, a=2.0+

--- gfx/layers/opengl/TextureHostOGL.h
+++ gfx/layers/opengl/TextureHostOGL.h
@@ -174,7 +174,10 @@ public:
 
   virtual void SetAcquireFence(const android::sp<android::Fence>& aAcquireFence);
 
-  virtual android::sp<android::Fence> GetAcquireFence();
+  /**
+   * Return a acquireFence's Fence and clear a reference to the Fence.
+   */
+  virtual android::sp<android::Fence> GetAndResetAcquireFence();
 
   virtual void WaitAcquireFenceSyncComplete();
 
