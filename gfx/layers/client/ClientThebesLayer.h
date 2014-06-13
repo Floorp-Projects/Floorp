ffcda6a27d4e00b503541f9aad8214ab415b6edf
Chris Lord <chrislord.net@gmail.com>
2014-06-13 17:11:08 +0100

    Bug 1006084 - Allow ThebesLayers to be recreated when the creation hint changes. r=mattwoodrow

--- gfx/layers/client/ClientThebesLayer.h
+++ gfx/layers/client/ClientThebesLayer.h
@@ -27,15 +27,17 @@ class CompositableClient;
 class ShadowableLayer;
 class SpecificLayerAttributes;
 
-class ClientThebesLayer : public ThebesLayer, 
+class ClientThebesLayer : public ThebesLayer,
                           public ClientLayer {
 public:
   typedef RotatedContentBuffer::PaintState PaintState;
   typedef RotatedContentBuffer::ContentType ContentType;
 
-  ClientThebesLayer(ClientLayerManager* aLayerManager) :
+  ClientThebesLayer(ClientLayerManager* aLayerManager,
+                    LayerManager::ThebesLayerCreationHint aCreationHint = LayerManager::NONE) :
     ThebesLayer(aLayerManager,
-                static_cast<ClientLayer*>(MOZ_THIS_IN_INITIALIZER_LIST())),
+                static_cast<ClientLayer*>(MOZ_THIS_IN_INITIALIZER_LIST()),
+                aCreationHint),
     mContentClient(nullptr)
   {
     MOZ_COUNT_CTOR(ClientThebesLayer);
