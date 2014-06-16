54fa326eb615ec7ab0bd0ce404a09ac3847edb67
Corentin Cos <corentin.cos@gmail.com>
2014-06-17 00:32:18 +0200

    Bug 1016413 - Tests for fmp4 metadata backend. r=rillian
    The reporting of metadata with fmp4 backend is tested here with the
    file gizmo.metadata.mp4, and some changes are made on test_metadata.html
    to integrate tests on mp4 files with restriction to certain platforms.
    A 'type' field has also been added to gMetadataTests' elements in
    manifest.js in order to correctly test the type with canPlayType method
    for instance.

--- content/media/test/manifest.js
+++ content/media/test/manifest.js
@@ -486,7 +486,9 @@ var gAspectRatioTests = [
 // Used by test_metadata.html.
 var gMetadataTests = [
   // Ogg Vorbis files
-  { name:"short-video.ogv", tags: {
+  { name:"short-video.ogv",
+    type:"video/ogg",
+    tags: {
       TITLE:"Lepidoptera",
       ARTIST:"Epoq",
       ALBUM:"Kahvi Collective",
@@ -494,7 +496,9 @@ var gMetadataTests = [
       COMMENT:"http://www.kahvi.org",
     }
   },
-  { name:"bug516323.ogv", tags: {
+  { name:"bug516323.ogv",
+    type:"video/ogg",
+    tags: {
       GENRE:"Open Movie",
       ENCODER:"Audacity",
       TITLE:"Elephants Dream",
@@ -502,7 +506,9 @@ var gMetadataTests = [
       COMMENTS:"Audio Description"
     }
   },
-  { name:"bug516323.indexed.ogv", tags: {
+  { name:"bug516323.indexed.ogv",
+    type:"video/ogg",
+    tags: {
       GENRE:"Open Movie",
       ENCODER:"Audacity",
       TITLE:"Elephants Dream",
@@ -510,17 +516,26 @@ var gMetadataTests = [
       COMMENTS:"Audio Description"
     }
   },
-  { name:"detodos.opus", tags: {
+  { name:"detodos.opus",
+    type:"audio/ogg; codecs=opus",
+    tags: {
       title:"De todos. Para todos.",
       artist:"Mozilla.org"
     }
   },
-  { name:"sound.ogg", tags: { } },
-  { name:"small-shot.ogg", tags: {
+  { name:"sound.ogg",
+    type:"audio/ogg",
+    tags: { }
+  },
+  { name:"small-shot.ogg",
+    type:"video/ogg",
+    tags: {
       title:"Pew SFX"
     }
   },
-  { name:"badtags.ogg", tags: {
+  { name:"badtags.ogg",
+    type:"audio/ogg",
+    tags: {
       // We list only the valid tags here, and verify
       // the invalid ones are filtered out.
       title:"Invalid comments test file",
@@ -535,41 +550,70 @@ var gMetadataTests = [
       // "not-utf-8":"invalid sequences: \xff\xfe\xfa\xfb\0eol"
     }
   },
-  { name:"wave_metadata.wav", tags: {
+  { name:"wave_metadata.wav",
+    type:"audio/x-wav",
+    tags: {
       name:"Track Title",
       artist:"Artist Name",
       comments:"Comments",
     }
   },
-  { name:"wave_metadata_utf8.wav", tags: {
+  { name:"wave_metadata_utf8.wav",
+    type:"audio/x-wav",
+    tags: {
       name:"歌曲名稱",
       artist:"作曲者",
       comments:"註解",
     }
   },
-  { name:"wave_metadata_unknown_tag.wav", tags: {
+  { name:"wave_metadata_unknown_tag.wav",
+    type:"audio/x-wav",
+    tags: {
       name:"Track Title",
       comments:"Comments",
     }
   },
-  { name:"wave_metadata_bad_len.wav", tags: {
+  { name:"wave_metadata_bad_len.wav",
+    type:"audio/x-wav",
+    tags: {
       name:"Track Title",
       artist:"Artist Name",
     }
   },
-  { name:"wave_metadata_bad_no_null.wav", tags: {
+  { name:"wave_metadata_bad_no_null.wav",
+    type:"audio/x-wav",
+    tags: {
       name:"Track Title",
       artist:"Artist Name",
       comments:"Comments!!",
     }
   },
-  { name:"wave_metadata_bad_utf8.wav", tags: {
+  { name:"wave_metadata_bad_utf8.wav",
+    type:"audio/x-wav",
+    tags: {
       name:"歌曲名稱",
       comments:"註解",
     }
   },
-  { name:"wavedata_u8.wav", tags: { }
+  { name:"wavedata_u8.wav",
+    type:"audio/x-wav",
+    tags: { }
   },
+  { name:"gizmo.metadata.mp4",
+    type:"video/mp4",
+    tags: {
+      artist:"Artist name",
+      title:"Title name",
+      album:"Album name",
+      albumArtist:"Album Artist name",
+      genre:"Genre name",
+      date:"20140326T002053.000Z",
+      year:"2014",
+      writer:"Composer name",
+      discNumber:"1/0",
+      cdTrackNumber:"1/0"
+    }
+  }
 ];
 
 function checkMetadata(msg, e, test) {
