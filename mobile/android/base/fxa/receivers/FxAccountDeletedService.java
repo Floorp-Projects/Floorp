9646de514194e79bd754955237d7b89ee90ac04f
Richard Newman <rnewman@mozilla.com>
2014-06-16 16:18:59 -0700

    Bug 1025937 - Silently drop null intents in background IntentServices. r=mcomella, a=sledru

--- mobile/android/base/fxa/receivers/FxAccountDeletedService.java
+++ mobile/android/base/fxa/receivers/FxAccountDeletedService.java
@@ -29,6 +29,11 @@ public class FxAccountDeletedService extends IntentService {
 
   @Override
   protected void onHandleIntent(final Intent intent) {
+    // Intent can, in theory, be null. Bug 1025937.
+    if (intent == null) {
+        Logger.debug(LOG_TAG, "Short-circuiting on null intent.");
+    }
+
     final Context context = this;
 
     long intentVersion = intent.getLongExtra(
