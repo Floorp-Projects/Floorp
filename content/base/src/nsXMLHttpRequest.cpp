845b351d4434cbde16b0e5c713c9a60a60d60a64
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-06-16 10:56:04 +0200

    Backed out changeset 8fc4cf38057e (bug 1011748) for mochitest-e10s-1 test failures

--- content/base/src/nsXMLHttpRequest.cpp
+++ content/base/src/nsXMLHttpRequest.cpp
@@ -1169,17 +1169,7 @@ nsXMLHttpRequest::GetStatusText(nsCString& aStatusText)
     return;
   }
 
-
-  // Check the current XHR state to see if it is valid to obtain the statusText
-  // value.  This check is to prevent the status text for redirects from being
-  // available before all the redirects have been followed and HTTP headers have
-  // been received.
-  uint16_t readyState;
-  GetReadyState(&readyState);
-  if (readyState != OPENED && readyState != UNSENT) {
-    httpChannel->GetResponseStatusText(aStatusText);
-  }
-
+  httpChannel->GetResponseStatusText(aStatusText);
 
 }
 
