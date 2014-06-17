08fc0aebce2bf5aebe813870e4ced99662db9abe
Eddy Bruel <ejpbruel@mozilla.com>
2014-06-17 21:51:43 +0200

    Bug 1003095 - Clean up how we provide several APIs;r=past

--- toolkit/devtools/worker-loader.js
+++ toolkit/devtools/worker-loader.js
@@ -262,6 +262,10 @@ function WorkerDebuggerLoader(options) {
 
 this.WorkerDebuggerLoader = WorkerDebuggerLoader;
 
+const chrome = { CC: undefined, Cc: undefined, ChromeWorker: undefined,
+                 Cm: undefined, Ci: undefined, Cu: undefined,
+                 Cr: undefined, components: undefined };
+
 // The default instance of the worker debugger loader is defined differently
 // depending on whether it is loaded from the main thread or a worker thread.
 if (typeof Components === "object") {
@@ -302,39 +306,36 @@ if (typeof Components === "object") {
       return sandbox.Debugger;
     })();
 
-    // TODO: Either replace these built-in modules with vacuous objects, or
-    // provide them in a way that does not depend on the use of Components.
+    // TODO: Replace this with native promises: bug 1016301
     const { Promise } = Cu.import("resource://gre/modules/Promise.jsm", {});;
-    const { Services } = Cu.import("resource://gre/modules/Services.jsm", {});;
-    let SourceMap = {};
-    Cu.import("resource://gre/modules/devtools/SourceMap.jsm", SourceMap);
     const Timer = Cu.import("resource://gre/modules/Timer.jsm", {});
-    const chrome = { CC: undefined, Cc: undefined, ChromeWorker: undefined,
-                     Cm: undefined, Ci: undefined, Cu: undefined,
-                     Cr: undefined, components: undefined };
     const xpcInspector = Cc["@mozilla.org/jsinspector;1"].
                          getService(Ci.nsIJSInspector);
 
     this.worker = new WorkerDebuggerLoader({
       createSandbox: createSandbox,
       globals: {
+        "isWorker": true,
+        "Debugger": Debugger,
         "promise": Promise,
         "reportError": Cu.reportError,
+        "setInterval": Timer.setInterval,
+        "setTimeout": Timer.setTimeout,
+        "clearInterval": Timer.clearInterval,
+        "clearTimeout": Timer.clearTimeout,
+        "xpcInspector": xpcInspector,
       },
       loadInSandbox: loadInSandbox,
       modules: {
-        "Debugger": Debugger,
-        "Services": Object.create(Services),
-        "Timer": Object.create(Timer),
+        "Services": {},
         "chrome": chrome,
-        "source-map": SourceMap,
-        "xpcInspector": xpcInspector,
       },
       paths: {
         "": "resource://gre/modules/commonjs/",
         "devtools": "resource:///modules/devtools",
         "devtools/server": "resource://gre/modules/devtools/server",
         "devtools/toolkit": "resource://gre/modules/devtools",
+        "source-map": "resource://gre/modules/devtools/source-map",
         "xpcshell-test": "resource://test",
       }
     });
