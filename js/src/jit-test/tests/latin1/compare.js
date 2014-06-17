6344ccf0e8dd1c4b3ee819987ecd81f67c82bc67
Jan de Mooij <jdemooij@mozilla.com>
2014-06-17 11:28:50 +0200

    Bug 1025875 part 3 - Make CompareStrings and CompareAtoms work with Latin1 strings. r=njn

---
+++ js/src/jit-test/tests/latin1/compare.js
@@ -0,0 +1,20 @@
+function test() {
+    var arr = [
+	toLatin1("abc"),
+	toLatin1("abcd"),
+	toLatin1("123\u00ff")
+    ];
+    for (var i = 0; i < arr.length; i++) {
+	for (var j = 0; j < arr.length; j++) {
+	    var s1 = arr[i];
+	    var s2 = arr[j];
+	    var s1tb = "\u1200" + s1;
+	    var s2tb = "\u1200" + s2;
+	    assertEq(s1 < s2, s1tb < s2tb);
+	    assertEq(s1 > s2, s1tb > s2tb);
+	    assertEq(s1 <= s2, s1tb <= s2tb);
+	    assertEq(s1 >= s2, s1tb >= s2tb);
+	}
+    }
+}
+test();
