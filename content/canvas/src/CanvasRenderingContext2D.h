836bcb28a443c1fa4b483ac7c8030948eb6cad6c
Jonathan Watt <jwatt@jwatt.org>
2014-06-17 10:37:46 +0100

    Bug 1022821, part 3 - In content code, stop returning RefPtr and, where not an out-param, stop using RefPtr arguments. r=gw280

--- content/canvas/src/CanvasRenderingContext2D.h
+++ content/canvas/src/CanvasRenderingContext2D.h
@@ -85,11 +85,14 @@ public:
                 const gfx::Point& aCP2,
                 const gfx::Point& aCP3);
 
-  mozilla::RefPtr<mozilla::gfx::Path> GetPath(const CanvasWindingRule& winding,
-                                              const mozilla::RefPtr<mozilla::gfx::DrawTarget>& mTarget) const;
+  TemporaryRef<gfx::Path> GetPath(const CanvasWindingRule& aWinding,
+                                  const gfx::DrawTarget* aTarget) const;
 
   explicit CanvasPath(nsISupports* aParent);
-  CanvasPath(nsISupports* aParent, RefPtr<gfx::PathBuilder> mPathBuilder);
+  // TemporaryRef arg because the return value from Path::CopyToBuilder() is
+  // passed directly and we can't drop the only ref to have a raw pointer.
+  CanvasPath(nsISupports* aParent,
+             TemporaryRef<gfx::PathBuilder> aPathBuilder);
   virtual ~CanvasPath() {}
 
 private:
