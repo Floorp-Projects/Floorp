a33aed0216ac17192f5cf3ba2a75e6de5d897778
Jonathan Watt <jwatt@jwatt.org>
2014-06-17 18:35:50 +0100

    Bug 1025489 - Stop using gfxImageSurface in the GL code. r=Bas

--- gfx/gl/GLTextureImage.cpp
+++ gfx/gl/GLTextureImage.cpp
@@ -6,11 +6,11 @@
 #include "GLTextureImage.h"
 #include "GLContext.h"
 #include "gfxContext.h"
-#include "gfxImageSurface.h"
 #include "gfxPlatform.h"
 #include "gfxUtils.h"
 #include "gfx2DGlue.h"
 #include "gfxImageSurface.h"
+#include "mozilla/gfx/2D.h"
 #include "ScopedGLHelpers.h"
 #include "GLUploadHelpers.h"
 
@@ -18,6 +18,8 @@
 #ifdef XP_MACOSX
 #include "TextureImageCGL.h"
 #endif
+
+using namespace mozilla::gfx;
 
 namespace mozilla {
 namespace gl {
@@ -516,10 +518,6 @@ TiledTextureImage::EndUpdate()
 
     RefPtr<gfx::SourceSurface> updateSnapshot = mUpdateDrawTarget->Snapshot();
     RefPtr<gfx::DataSourceSurface> updateData = updateSnapshot->GetDataSurface();
-    nsRefPtr<gfxASurface> updateSurface = new gfxImageSurface(updateData->GetData(),
-                                                              gfx::ThebesIntSize(updateData->GetSize()),
-                                                              updateData->Stride(),
-                                                              gfx::SurfaceFormatToImageFormat(updateData->GetFormat()));
 
     // upload tiles from temp surface
     for (unsigned i = 0; i < mImages.Length(); i++) {
@@ -535,11 +533,18 @@ TiledTextureImage::EndUpdate()
         subregion.MoveBy(-xPos, -yPos); // Tile-local space
         // copy tile from temp target
         gfx::DrawTarget* drawTarget = mImages[i]->BeginUpdate(subregion);
-        nsRefPtr<gfxContext> ctx = new gfxContext(drawTarget);
-        gfxUtils::ClipToRegion(ctx, subregion);
-        ctx->SetOperator(gfxContext::OPERATOR_SOURCE);
-        ctx->SetSource(updateSurface, gfxPoint(-xPos, -yPos));
-        ctx->Paint();
+        MOZ_ASSERT(drawTarget->GetType() == BackendType::CAIRO,
+                   "updateSnapshot should not have been converted to data");
+        gfxUtils::ClipToRegion(drawTarget, subregion);
+        Size size(updateData->GetSize().width,
+                  updateData->GetSize().height);
+        drawTarget->DrawSurface(updateData,
+                                Rect(Point(-xPos, -yPos), size),
+                                Rect(Point(0, 0), size),
+                                DrawSurfaceOptions(),
+                                DrawOptions(1.0, CompositionOp::OP_SOURCE,
+                                            AntialiasMode::NONE));
+        drawTarget->PopClip();
         mImages[i]->EndUpdate();
     }
 
