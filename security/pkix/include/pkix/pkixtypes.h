5705e249f067620122638217d798abfef58919b7
Camilo Viecco <cviecco@mozilla.com>
2014-06-17 08:20:14 -0700

    Bug 1000548 - Fix leaking plarenas in mozilla::pkix. r=keeler a=sledru

--- security/pkix/include/pkix/pkixtypes.h
+++ security/pkix/include/pkix/pkixtypes.h
@@ -25,7 +25,14 @@
 
 namespace mozilla { namespace pkix {
 
-typedef ScopedPtr<PLArenaPool, PL_FreeArenaPool> ScopedPLArenaPool;
+inline void
+PORT_FreeArena_false(PLArenaPool *arena) {
+  // PL_FreeArenaPool can't be used because it doesn't actually free the
+  // memory, which doesn't work well with memory analysis tools
+  return PORT_FreeArena(arena, PR_FALSE);
+}
+
+typedef ScopedPtr<PLArenaPool, PORT_FreeArena_false> ScopedPLArenaPool;
 
 typedef ScopedPtr<CERTCertificate, CERT_DestroyCertificate>
         ScopedCERTCertificate;
