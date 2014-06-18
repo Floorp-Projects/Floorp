f43e314973f05ea63bf017a0286b76b87b58a1ff
Eddy Bruel <ejpbruel@mozilla.com>
2014-06-18 15:26:53 +0200

    Bug 1014141 - Fix a regression in the remote debugger. r=marco, r=past, a=lmandel

--- toolkit/devtools/server/actors/webbrowser.js
+++ toolkit/devtools/server/actors/webbrowser.js
@@ -48,6 +48,8 @@ function allAppShellDOMWindows(aWindowType)
   }
 }
 
+exports.allAppShellDOMWindows = allAppShellDOMWindows;
+
 /**
  * Retrieve the window type of the top-level window |aWindow|.
  */
