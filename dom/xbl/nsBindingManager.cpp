66ae57c571d14d5ce8d821f32b5020b3ba2b4a69
Cameron McCormack <cam@mcc.id.au>
2014-06-19 17:02:56 +1000

    Bug 1025725 - Part 3: Encapsulate nsXBLPrototypeResources::mStyleSheetList. r=bzbarsky

--- dom/xbl/nsBindingManager.cpp
+++ dom/xbl/nsBindingManager.cpp
@@ -817,13 +817,7 @@ EnumAppendAllSheets(nsRefPtrHashKey<nsIContent> *aKey, void* aClosure)
     static_cast<nsTArray<nsCSSStyleSheet*>*>(aClosure);
   for (nsXBLBinding *binding = boundContent->GetXBLBinding(); binding;
        binding = binding->GetBaseBinding()) {
-    nsXBLPrototypeResources::sheet_array_type* sheets =
-      binding->PrototypeBinding()->GetStyleSheets();
-    if (sheets) {
-      // Copy from nsTArray<nsRefPtr<nsCSSStyleSheet> > to
-      // nsTArray<nsCSSStyleSheet*>.
-      array->AppendElements(*sheets);
-    }
+    binding->PrototypeBinding()->AppendStyleSheetsTo(*array);
   }
   return PL_DHASH_NEXT;
 }
