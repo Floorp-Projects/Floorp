255a665287536c458e95dda64c352a3eff8e4e08
Benoit Jacob <bjacob@mozilla.com>
2014-06-18 22:28:59 -0400

    Bug 1027251 - Fix or whitelist dangerous public destructors in layout/ - r=dbaron

--- layout/style/AnimationCommon.h
+++ layout/style/AnimationCommon.h
@@ -282,6 +282,10 @@ struct ComputedTiming
  */
 struct ElementAnimation
 {
+protected:
+  virtual ~ElementAnimation() { }
+
+public:
   ElementAnimation()
     : mIsRunningOnCompositor(false)
     , mLastNotification(LAST_NOTIFICATION_NONE)
@@ -291,7 +295,6 @@ struct ElementAnimation
   // FIXME: If we succeed in moving transition-specific code to a type of
   // AnimationEffect (as per the Web Animations API) we should remove these
   // virtual methods.
-  virtual ~ElementAnimation() { }
   virtual ElementPropertyTransition* AsTransition() { return nullptr; }
   virtual const ElementPropertyTransition* AsTransition() const {
     return nullptr;
