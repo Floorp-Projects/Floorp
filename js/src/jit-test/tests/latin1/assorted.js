1f0f83b15d704153f3dd6c7bbdaa21c3529527d1
Jan de Mooij <jdemooij@mozilla.com>
2014-06-20 16:20:24 +0200

    Bug 1027528 part 10 - Make ObjectToSource work with Latin1 strings. r=njn

--- js/src/jit-test/tests/latin1/assorted.js
+++ js/src/jit-test/tests/latin1/assorted.js
@@ -5,3 +5,21 @@ assertEq(ast.body[0].id.name, "f");
 // Reflect.parse TwoByte
 var ast = Reflect.parse("function f\u1200() { return 3; }");
 assertEq(ast.body[0].id.name, "f\u1200");
+
+// obj.toSource Latin1
+var o = {};
+Object.defineProperty(o, "prop", {get: function() { return 1; },
+                                  set: function() { return 2; },
+                                  enumerable: true, configurable: true});
+assertEq(o.toSource(), "({get prop () { return 1; }, set prop () { return 2; }})");
+
+// obj.toSource TwoByte
+Object.defineProperty(o, "prop", {get: function() { return "\u1200"; },
+                                  set: function() { return "\u1200"; },
+                                  enumerable: true});
+assertEq(o.toSource(), '({get prop () { return "\\u1200"; }, set prop () { return "\\u1200"; }})');
+
+var ff = function() { return 10; };
+ff.toSource = function() { return "((11))"; }
+Object.defineProperty(o, "prop", {get: ff, set: ff, enumerable: true});
+assertEq(o.toSource(), "({prop:((11)), prop:((11))})");
