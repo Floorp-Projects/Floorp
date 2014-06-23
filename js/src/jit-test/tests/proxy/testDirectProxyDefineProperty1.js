fbcad11723913b931d502902715f1e3fadbb218a
Jason Orendorff <jorendorff@mozilla.com>
2014-06-23 10:57:02 -0500

    Bug 645416, part 23 - Implement ValueToId for symbols. This makes symbols work as property keys. r=efaust.

--- js/src/jit-test/tests/proxy/testDirectProxyDefineProperty1.js
+++ js/src/jit-test/tests/proxy/testDirectProxyDefineProperty1.js
@@ -1,13 +1,15 @@
 // Forward to the target if the trap is not defined
 var target = {};
-Object.defineProperty(Proxy(target, {}), 'foo', {
-    value: 'bar',
-    writable: true,
-    enumerable: false,
-    configurable: true
-});
-var desc = Object.getOwnPropertyDescriptor(target, 'foo');
-assertEq(desc.value, 'bar');
-assertEq(desc.writable, true);
-assertEq(desc.enumerable, false);
-assertEq(desc.configurable, true);
+for (var key of ['foo', Symbol("quux")]) {
+    Object.defineProperty(Proxy(target, {}), key, {
+        value: 'bar',
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+    var desc = Object.getOwnPropertyDescriptor(target, key);
+    assertEq(desc.value, 'bar');
+    assertEq(desc.writable, true);
+    assertEq(desc.enumerable, false);
+    assertEq(desc.configurable, true);
+}
---
+++ js/src/tests/ecma_6/Symbol/property-accessor.js
@@ -0,0 +1,39 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/licenses/publicdomain/ */
+
+var obj = {};
+var sym = Symbol();
+
+var gets = 0;
+var sets = [];
+Object.defineProperty(obj, sym, {
+    get: function () { return ++gets; },
+    set: function (v) { sets.push(v); }
+});
+
+// getter
+for (var i = 1; i < 9; i++)
+    assertEq(obj[sym], i);
+
+// setter
+var expected = [];
+for (var i = 0; i < 9; i++) {
+    assertEq(obj[sym] = i, i);
+    expected.push(i);
+}
+assertDeepEq(sets, expected);
+
+// increment operator
+gets = 0;
+sets = [];
+assertEq(obj[sym]++, 1);
+assertDeepEq(sets, [2]);
+
+// assignment
+gets = 0;
+sets = [];
+assertEq(obj[sym] *= 12, 12);
+assertDeepEq(sets, [12]);
+
+if (typeof reportCompare === "function")
+    reportCompare(0, 0);
