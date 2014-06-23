6d2ba0780a3c3962f6475c9f042b9d69948d7e39
Honza Bambas <honzab.moz@firemni.cz>
2014-06-23 19:43:40 +0200

    Bug 1023748 - Allow NTLMv1 over SSL/TLS by default. r=jduell, a=sledru

--- security/manager/ssl/src/nsNTLMAuthModule.cpp
+++ security/manager/ssl/src/nsNTLMAuthModule.cpp
@@ -15,8 +15,6 @@
 #include "mozilla/Telemetry.h"
 #include "mozilla/Preferences.h"
 
-static bool sNTLMv1Enabled = false;
-
 #ifdef PR_LOGGING
 static PRLogModuleInfo *
 GetNTLMLog()
@@ -757,18 +755,6 @@ nsNTLMAuthModule::~nsNTLMAuthModule()
 nsresult
 nsNTLMAuthModule::InitTest()
 {
-  static bool prefObserved = false;
-  if (!prefObserved) {
-    mozilla::Preferences::AddBoolVarCache(
-      &sNTLMv1Enabled, "network.negotiate-auth.allow-insecure-ntlm-v1", sNTLMv1Enabled);
-    prefObserved = true;
-  }
-
-  if (!sNTLMv1Enabled) {
-    // Unconditionally disallow usage of the generic module.
-    return NS_ERROR_NOT_AVAILABLE;
-  }
-
   nsNSSShutDownPreventionLock locker;
   //
   // disable NTLM authentication when FIPS mode is enabled.
