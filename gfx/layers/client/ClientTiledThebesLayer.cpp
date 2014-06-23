048e27ce05310207464d68ba50da9b11399d3c32
Kartikaya Gupta <kgupta@mozilla.com>
2014-06-23 08:42:01 -0400

    Bug 1021085 - Fix up the transform used for the compositor side composition bounds. r=Cwiiis

--- gfx/layers/client/ClientTiledThebesLayer.cpp
+++ gfx/layers/client/ClientTiledThebesLayer.cpp
@@ -176,10 +176,10 @@ ClientTiledThebesLayer::BeginPaint()
   TILING_PRLOG(("TILING 0x%p: Resolution %f\n", this, mPaintData.mResolution.scale));
 
   // Store the applicable composition bounds in this layer's Layer units.
-  gfx3DMatrix transformToCompBounds =
+  mPaintData.mTransformToCompBounds =
     GetTransformToAncestorsParentLayer(this, scrollAncestor);
   mPaintData.mCompositionBounds = ApplyParentLayerToLayerTransform(
-    transformToCompBounds.Inverse(), ParentLayerRect(scrollMetrics.mCompositionBounds));
+    mPaintData.mTransformToCompBounds.Inverse(), ParentLayerRect(scrollMetrics.mCompositionBounds));
   TILING_PRLOG_OBJ(("TILING 0x%p: Composition bounds %s\n", this, tmpstr.get()), mPaintData.mCompositionBounds);
 
   // Calculate the scroll offset since the last transaction
