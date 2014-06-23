0c03caf3e9316f8856875c9b701ff77053f899e8
David Burns <dburns@mozilla.com>
2014-06-23 10:24:08 +0100

    bug 1024642: Add rebeccapurple to mochitest colors; r=jmaher

--- testing/mochitest/MochiKit/Color.js
+++ testing/mochitest/MochiKit/Color.js
@@ -871,6 +871,7 @@ MochiKit.Color.Color._namedColors = {
     plum: "#dda0dd",
     powderblue: "#b0e0e6",
     purple: "#800080",
+    rebeccapurple: "#663399",
     red: "#ff0000",
     rosybrown: "#bc8f8f",
     royalblue: "#4169e1",
