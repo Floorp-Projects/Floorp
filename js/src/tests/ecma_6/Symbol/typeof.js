d05b2005e20f607446dc5afb02bcba8530ef75af
Jason Orendorff <jorendorff@mozilla.com>
2014-06-23 10:56:51 -0500

    Bug 645417, part 15 - Tests for ToObject on symbols. r=sfink.
    The test as-base-value.js is not testing symbols as property keys (which is
    implemented in a later patch), but on the other side of the . operator: as
    things that can have property accesses done to them, just like you can do
    "name".length or (3.14).toString().
    
    --HG--
    extra : rebase_source : 3dac7660999bd021ec32c13985471e1608a29f64

--- js/src/tests/ecma_6/Symbol/typeof.js
+++ js/src/tests/ecma_6/Symbol/typeof.js
@@ -5,5 +5,7 @@ assertEq(typeof Symbol(), "symbol");
 assertEq(typeof Symbol("ponies"), "symbol");
 assertEq(typeof Symbol.for("ponies"), "symbol");
 
+assertEq(typeof Object(Symbol()), "object");
+
 if (typeof reportCompare === "function")
     reportCompare(0, 0);
