297a9b2844bb87a5ea4644aa2c38a551ee756f9f
Eric Phan <eric.phan@ensimag.grenoble-inp.fr>
2014-06-23 22:08:34 +1200

    Bug 1015985 - Set decoder seekable members outside of ReadMetadata by StateMachine. r=cpearce

--- content/media/fmp4/MP4Reader.cpp
+++ content/media/fmp4/MP4Reader.cpp
@@ -243,16 +243,19 @@ MP4Reader::ReadMetadata(MediaInfo* aInfo,
     ReentrantMonitorAutoEnter mon(mDecoder->GetReentrantMonitor());
     mDecoder->SetMediaDuration(duration);
   }
-  // We can seek if we get a duration *and* the reader reports that it's
-  // seekable.
-  if (!mDecoder->GetResource()->IsTransportSeekable() || !mDemuxer->CanSeek()) {
-    mDecoder->SetMediaSeekable(false);
-  }
 
   *aInfo = mInfo;
   *aTags = nullptr;
 
   return NS_OK;
+}
+
+bool
+MP4Reader::IsMediaSeekable()
+{
+  // We can seek if we get a duration *and* the reader reports that it's
+  // seekable.
+  return mDecoder->GetResource()->IsTransportSeekable() && mDemuxer->CanSeek();
 }
 
 bool
