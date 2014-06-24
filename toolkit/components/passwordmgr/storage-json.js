e7396ccb03cb197f8080a8169510aaaa6e60e283
Blake Kaplan <mrbkap@gmail.com>
2014-06-24 16:57:40 -0400

    Bug 1029128 - Fix strict-mode error in the JSON storage file. r=dolske

--- toolkit/components/passwordmgr/storage-json.js
+++ toolkit/components/passwordmgr/storage-json.js
@@ -11,6 +11,8 @@
 ////////////////////////////////////////////////////////////////////////////////
 //// Globals
 
+"use strict";
+
 const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
@@ -137,12 +139,10 @@ this.LoginManagerStorage_json.prototype = {
     addLogin : function (login) {
         this._store.ensureDataReady();
 
-        let encUsername, encPassword;
-
         // Throws if there are bogus values.
         LoginHelper.checkLoginValues(login);
 
-        [encUsername, encPassword, encType] = this._encryptLogin(login);
+        let [encUsername, encPassword, encType] = this._encryptLogin(login);
 
         // Clone the login, so we don't modify the caller's object.
         let loginClone = login.clone();
