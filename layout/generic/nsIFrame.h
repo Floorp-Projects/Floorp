bf86808e28389de316c9dd04124df847cc6b40d0
Mats Palmgren <mats@mozilla.com>
2014-06-24 17:52:19 +0000

    Bug 1025669 - part 0.5, Add LogicalMargin::ApplySkipSides.  r=jfkthame

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -2315,14 +2315,6 @@ public:
    *       passed in, indicating that it should be used to determine if sides
    *       should be skipped during reflow.
    */
-#define LOGICAL_SIDE_B_START 1
-#define LOGICAL_SIDE_I_START 2
-#define LOGICAL_SIDE_B_END   4
-#define LOGICAL_SIDE_I_END   8
-#define LOGICAL_SIDES_I_BOTH (LOGICAL_SIDE_I_START | LOGICAL_SIDE_I_END)
-#define LOGICAL_SIDES_B_BOTH (LOGICAL_SIDE_B_START | LOGICAL_SIDE_B_END)
-#define LOGICAL_SIDES_ALL (LOGICAL_SIDE_I_START | LOGICAL_SIDE_I_END | \
-                           LOGICAL_SIDE_B_START | LOGICAL_SIDE_B_END)
   int GetSkipSides(const nsHTMLReflowState* aReflowState = nullptr) const;
   virtual int
   GetLogicalSkipSides(const nsHTMLReflowState* aReflowState = nullptr) const {
