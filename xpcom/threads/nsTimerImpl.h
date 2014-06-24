988f672c39423cd0243f97e7455b446db7f29cb1
Ryan VanderMeulen <ryanvm@gmail.com>
2014-06-24 08:48:27 -0400

    Backed out changeset 81c145939eee (bug 1024765) for bustage.

--- xpcom/threads/nsTimerImpl.h
+++ xpcom/threads/nsTimerImpl.h
@@ -58,9 +58,7 @@ public:
   friend struct TimerAdditionComparator;
 
   void Fire();
-  // If a failure is encountered, the reference is returned to the caller
-  static already_AddRefed<nsTimerImpl> PostTimerEvent(
-      already_AddRefed<nsTimerImpl> aTimerRef);
+  nsresult PostTimerEvent();
   void SetDelayInternal(uint32_t aDelay);
 
   NS_DECL_THREADSAFE_ISUPPORTS
