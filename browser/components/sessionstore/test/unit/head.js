409d9e236e3be70907f9e9142b1370db907c0953
David Rajchenbach-Teller <dteller@mozilla.com>
2014-06-24 16:51:22 +0200

    Bug 883609 - Porting tests to new Session Restore backup mechanism;r=ttaubert
    --HG--
    rename : browser/components/sessionstore/test/browser_833286_atomic_backup.js => browser/components/sessionstore/test/browser_backup_recovery.js

--- browser/components/sessionstore/test/unit/head.js
+++ browser/components/sessionstore/test/unit/head.js
@@ -5,29 +5,28 @@ let Ci = Components.interfaces;
 Components.utils.import("resource://gre/modules/Services.jsm");
 
 // Call a function once initialization of SessionStartup is complete
-let afterSessionStartupInitialization =
-  function afterSessionStartupInitialization(cb) {
-    do_print("Waiting for session startup initialization");
-    let observer = function() {
-      try {
-        do_print("Session startup initialization observed");
-        Services.obs.removeObserver(observer, "sessionstore-state-finalized");
-        cb();
-      } catch (ex) {
-        do_throw(ex);
-      }
-    };
+function afterSessionStartupInitialization(cb) {
+  do_print("Waiting for session startup initialization");
+  let observer = function() {
+    try {
+      do_print("Session startup initialization observed");
+      Services.obs.removeObserver(observer, "sessionstore-state-finalized");
+      cb();
+    } catch (ex) {
+      do_throw(ex);
+    }
+  };
 
-    // We need the Crash Monitor initialized for sessionstartup to run
-    // successfully.
-    Components.utils.import("resource://gre/modules/CrashMonitor.jsm");
-    CrashMonitor.init();
+  // We need the Crash Monitor initialized for sessionstartup to run
+  // successfully.
+  Components.utils.import("resource://gre/modules/CrashMonitor.jsm");
+  CrashMonitor.init();
 
-    // Start sessionstartup initialization.
-    let startup = Cc["@mozilla.org/browser/sessionstartup;1"].
-      getService(Ci.nsIObserver);
-    Services.obs.addObserver(startup, "final-ui-startup", false);
-    Services.obs.addObserver(startup, "quit-application", false);
-    Services.obs.notifyObservers(null, "final-ui-startup", "");
-    Services.obs.addObserver(observer, "sessionstore-state-finalized", false);
+  // Start sessionstartup initialization.
+  let startup = Cc["@mozilla.org/browser/sessionstartup;1"].
+    getService(Ci.nsIObserver);
+  Services.obs.addObserver(startup, "final-ui-startup", false);
+  Services.obs.addObserver(startup, "quit-application", false);
+  Services.obs.notifyObservers(null, "final-ui-startup", "");
+  Services.obs.addObserver(observer, "sessionstore-state-finalized", false);
 };
