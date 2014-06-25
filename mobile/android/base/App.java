d8cdee4463aeeb000c067b5c87c56fcd52a4cf23
Nick Alexander <nalexander@mozilla.com>
2014-06-24 18:20:13 -0700

    Bug 929865 - Part 2: Use <activity-alias> to wrap activities in generated namespace. r=bnicholson,myk

--- mobile/android/base/App.java
+++ mobile/android/base/App.java
@@ -3,13 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#filter substitution
-package @ANDROID_PACKAGE_NAME@;
-
-import org.mozilla.gecko.BrowserApp;
+package org.mozilla.gecko;
 
 /**
  * This class serves only as a namespace wrapper for BrowserApp.
  */
 public class App extends BrowserApp {}
-
