744d001a3576dafe8170ef2c42994e236ff80246
Luke Wagner <luke@mozilla.com>
2014-06-25 11:33:02 -0500

    Bug 1030175 - OdinMonkey: use the InterruptMode to avoid faulting for finished Ion compilations (r=bhackett)

--- js/src/jit/AsmJSSignalHandlers.cpp
+++ js/src/jit/AsmJSSignalHandlers.cpp
@@ -8,6 +8,7 @@
 
 #include "assembler/assembler/MacroAssembler.h"
 #include "jit/AsmJSModule.h"
+#include "vm/Runtime.h"
 
 using namespace js;
 using namespace js::jit;
@@ -1030,14 +1031,25 @@ js::EnsureAsmJSSignalHandlersInstalled(JSRuntime *rt)
 // js::HandleExecutionInterrupt. The memory is un-protected from the signal
 // handler after control flow is redirected.
 void
-js::RequestInterruptForAsmJSCode(JSRuntime *rt)
+js::RequestInterruptForAsmJSCode(JSRuntime *rt, int interruptModeRaw)
 {
-    JS_ASSERT(rt->currentThreadOwnsInterruptLock());
+    switch (JSRuntime::InterruptMode(interruptModeRaw)) {
+      case JSRuntime::RequestInterruptMainThread:
+      case JSRuntime::RequestInterruptAnyThread:
+        break;
+      case JSRuntime::RequestInterruptAnyThreadDontStopIon:
+      case JSRuntime::RequestInterruptAnyThreadForkJoin:
+        // It is ok to wait for asm.js execution to complete; we aren't trying
+        // to break an iloop or anything. Avoid the overhead of protecting all
+        // the code and taking a fault.
+        return;
+    }
 
     AsmJSActivation *activation = rt->mainThread.asmJSActivationStackFromAnyThread();
     if (!activation)
         return;
 
+    JS_ASSERT(rt->currentThreadOwnsInterruptLock());
     activation->module().protectCode(rt);
 }
 
