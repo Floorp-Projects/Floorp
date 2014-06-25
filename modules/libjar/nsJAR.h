59478902851fbfc8d74143038b8ab7431a8fa373
Benoit Jacob <bjacob@mozilla.com>
2014-06-24 22:09:14 -0400

    Bug 1028588 - Fix dangerous public destructors in libjar/ - r=aklotz

--- modules/libjar/nsJAR.h
+++ modules/libjar/nsJAR.h
@@ -59,10 +59,13 @@ class nsJAR : public nsIZipReader
   // Allows nsZipReaderCache to access mOuterZipEntry
   friend class nsZipReaderCache;
 
+  private:
+
+    virtual ~nsJAR();
+
   public:
 
     nsJAR();
-    virtual ~nsJAR();
 
     NS_DEFINE_STATIC_CID_ACCESSOR( NS_ZIPREADER_CID )
 
@@ -136,9 +139,10 @@ public:
     NS_DECL_NSIZIPENTRY
 
     nsJARItem(nsZipItem* aZipItem);
-    virtual ~nsJARItem() {}
 
 private:
+    virtual ~nsJARItem() {}
+
     uint32_t     mSize;             /* size in original file */
     uint32_t     mRealsize;         /* inflated size */
     uint32_t     mCrc32;
@@ -188,13 +192,14 @@ public:
   NS_DECL_NSIOBSERVER
 
   nsZipReaderCache();
-  virtual ~nsZipReaderCache();
 
   nsresult ReleaseZip(nsJAR* reader);
 
   typedef nsRefPtrHashtable<nsCStringHashKey, nsJAR> ZipsHashtable;
 
 protected:
+
+  virtual ~nsZipReaderCache();
 
   mozilla::Mutex        mLock;
   uint32_t              mCacheSize;
