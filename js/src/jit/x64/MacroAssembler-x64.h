b704918225653904ac9ab05e55db98a827f1f73b
Luke Wagner <luke@mozilla.com>
2014-06-25 15:15:46 -0500

    Bug 1030446 - Store and use exit-fp instead of exit-sp for asm.js stack unwinding (r=dougc)

--- js/src/jit/x64/MacroAssembler-x64.h
+++ js/src/jit/x64/MacroAssembler-x64.h
@@ -109,9 +109,6 @@ class MacroAssemblerX64 : public MacroAssemblerX86Shared
         call(target);
         appendCallSite(desc);
     }
-    void callExit(AsmJSImmPtr target, uint32_t stackArgBytes) {
-        call(CallSiteDesc::Exit(), target);
-    }
 
     // Refers to the upper 32 bits of a 64-bit Value operand.
     // On x86_64, the upper 32 bits do not necessarily only contain the type.
