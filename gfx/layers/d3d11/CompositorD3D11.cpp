a51e2c4bcd2233f312e835ffe4732356aac504c7
Benoit Jacob <bjacob@mozilla.com>
2014-06-24 22:09:14 -0400

    Bug 1029673 - Correctly report OMTC compositing in crash reports - r=Bas

--- gfx/layers/d3d11/CompositorD3D11.cpp
+++ gfx/layers/d3d11/CompositorD3D11.cpp
@@ -15,6 +15,7 @@
 #include "mozilla/layers/Effects.h"
 #include "nsWindowsHelpers.h"
 #include "gfxPrefs.h"
+#include "gfxCrashReporterUtils.h"
 
 #include "mozilla/EnumeratedArray.h"
 
@@ -106,6 +107,10 @@ CompositorD3D11::~CompositorD3D11()
 bool
 CompositorD3D11::Initialize()
 {
+  bool force = gfxPrefs::LayersAccelerationForceEnabled();
+
+  ScopedGfxFeatureReporter reporter("D3D11 Layers", force);
+
   if (!gfxPlatform::CanUseDirect3D11()) {
     NS_WARNING("Direct3D 11-accelerated layers are not supported on this system.");
     return false;
@@ -355,6 +360,7 @@ CompositorD3D11::Initialize()
                                        DXGI_MWA_NO_WINDOW_CHANGES);
   }
 
+  reporter.SetSuccessful();
   return true;
 }
 
