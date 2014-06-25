a23c5b80c6ee8a0d4e9d9897cf21ec5f6f9aed0f
Jan de Mooij <jdemooij@mozilla.com>
2014-06-25 10:12:17 +0200

    Bug 1028866 part 5 - Make NewString deflate to Latin1 if Latin1 strings are enabled and add NewStringDontDeflate. r=luke

--- js/src/jscompartment.cpp
+++ js/src/jscompartment.cpp
@@ -308,7 +308,7 @@ CopyStringPure(JSContext *cx, JSString *str)
     if (!str->asRope().copyTwoByteCharsZ(cx, copiedChars))
         return nullptr;
 
-    return NewString<CanGC>(cx, copiedChars.forget(), len);
+    return NewStringDontDeflate<CanGC>(cx, copiedChars.forget(), len);
 }
 
 bool
