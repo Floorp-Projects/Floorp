a0af4a78370b6f219e892db5ce907b5a9278ad47
Nick Fitzgerald <fitzgen@mozilla.com>
2014-06-25 12:21:00 -0400

    Bug 1026188 - Allow embedders to save JS execution stacks. r=sfink

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -73,6 +73,7 @@
 #include "vm/NumericConversions.h"
 #include "vm/RegExpStatics.h"
 #include "vm/Runtime.h"
+#include "vm/SavedStacks.h"
 #include "vm/Shape.h"
 #include "vm/SharedArrayObject.h"
 #include "vm/StopIterationObject.h"
@@ -6559,3 +6560,14 @@ JS::SetOutOfMemoryCallback(JSRuntime *rt, OutOfMemoryCallback cb, void *data)
     rt->oomCallbackData = data;
 }
 
+JS_PUBLIC_API(bool)
+JS::CaptureCurrentStack(JSContext *cx, JS::MutableHandleObject stackp)
+{
+    JSCompartment *compartment = cx->compartment();
+    JS_ASSERT(compartment);
+    Rooted<SavedFrame *> frame(cx);
+    if (!compartment->savedStacks().saveCurrentStack(cx, &frame))
+        return false;
+    stackp.set(frame.get());
+    return true;
+}
