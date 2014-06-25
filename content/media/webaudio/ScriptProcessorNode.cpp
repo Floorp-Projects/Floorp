5212071b75c797c11f72ce89b9a944822e43680b
Steve Fink <sfink@mozilla.com>
2014-06-25 14:37:55 -0700

    Bug 1022773 - Return value rooting for content/, r=smaug, a=abillings

--- content/media/webaudio/ScriptProcessorNode.cpp
+++ content/media/webaudio/ScriptProcessorNode.cpp
@@ -402,14 +402,14 @@ private:
           return NS_OK;
         }
 
+        AutoJSAPI jsapi;
+        JSContext* cx = jsapi.cx();
+
         // Get the global for the context so that we can enter its compartment.
-        JSObject* global = node->Context()->GetGlobalJSObject();
+        JS::Rooted<JSObject*> global(cx, node->Context()->GetGlobalJSObject());
         if (NS_WARN_IF(!global)) {
           return NS_OK;
         }
-
-        AutoJSAPI jsapi;
-        JSContext* cx = jsapi.cx();
         JSAutoCompartment ac(cx, global);
 
         // Create the input buffer
