bf9b6e648353c84c425e208ce80920df4095f941
Luke Wagner <luke@mozilla.com>
2014-06-25 15:15:46 -0500

    Bug 1030446 - Store and use exit-fp instead of exit-sp for asm.js stack unwinding (r=dougc)

--- js/src/jit/AsmJSFrameIterator.cpp
+++ js/src/jit/AsmJSFrameIterator.cpp
@@ -13,80 +13,42 @@ using namespace js;
 using namespace js::jit;
 
 static uint8_t *
-ReturnAddressForExitCall(uint8_t **psp)
+ReturnAddressFromFP(uint8_t *fp)
 {
-    uint8_t *sp = *psp;
-#if defined(JS_CODEGEN_X86) || defined(JS_CODEGEN_X64)
-    // For calls to Ion/C++ on x86/x64, the exitSP is the SP right before the call
-    // to C++. Since the call instruction pushes the return address, we know
-    // that the return address is 1 word below exitSP.
-    return *(uint8_t**)(sp - sizeof(void*));
-#elif defined(JS_CODEGEN_ARM)
-    // For calls to Ion/C++ on ARM, the *caller* pushes the return address on
-    // the stack. For Ion, this is just part of the ABI. For C++, the return
-    // address is explicitly pushed before the call since we cannot expect the
-    // callee to immediately push lr. This means that exitSP points to the
-    // return address.
-    return *(uint8_t**)sp;
-#elif defined(JS_CODEGEN_MIPS)
-    // On MIPS we have two cases: an exit to C++ will store the return address
-    // at ShadowStackSpace above sp; an exit to Ion will store the return
-    // address at sp. To distinguish the two cases, the low bit of sp (which is
-    // aligned and therefore zero) is set for Ion exits.
-    if (uintptr_t(sp) & 0x1) {
-        sp = *psp -= 0x1;  // Clear the low bit
-        return *(uint8_t**)sp;
-    }
-    return *(uint8_t**)(sp + ShadowStackSpace);
-#else
-# error "Unknown architecture!"
-#endif
+    // In asm.js code, the "frame" consists of a single word: the saved
+    // return address of the caller.
+    static_assert(AsmJSFrameSize == sizeof(void*), "Frame size mismatch");
+    return *(uint8_t**)fp;
 }
 
-static uint8_t *
-ReturnAddressForJitCall(uint8_t *sp)
+AsmJSFrameIterator::AsmJSFrameIterator(const AsmJSActivation &activation)
+  : module_(&activation.module()),
+    fp_(activation.exitFP())
 {
-    // Once inside JIT code, sp always points to the word before the return
-    // address.
-    return *(uint8_t**)(sp - sizeof(void*));
-}
-
-AsmJSFrameIterator::AsmJSFrameIterator(const AsmJSActivation *activation)
-  : module_(nullptr)
-{
-    if (!activation || activation->isInterruptedSP())
+    if (!fp_)
         return;
-
-    module_ = &activation->module();
-    sp_ = activation->exitSP();
-
-    settle(ReturnAddressForExitCall(&sp_));
+    settle(ReturnAddressFromFP(fp_));
 }
 
 void
 AsmJSFrameIterator::operator++()
 {
-    settle(ReturnAddressForJitCall(sp_));
+    JS_ASSERT(!done());
+    fp_ += callsite_->stackDepth();
+    settle(ReturnAddressFromFP(fp_));
 }
 
 void
 AsmJSFrameIterator::settle(uint8_t *returnAddress)
 {
     callsite_ = module_->lookupCallSite(returnAddress);
-    if (!callsite_ || callsite_->isEntry()) {
-        module_ = nullptr;
-        return;
-    }
+    JS_ASSERT(callsite_);
 
     if (callsite_->isEntry()) {
-        module_ = nullptr;
+        fp_ = nullptr;
+        JS_ASSERT(done());
         return;
     }
-
-    sp_ += callsite_->stackDepth();
-
-    if (callsite_->isExit())
-        return settle(ReturnAddressForJitCall(sp_));
 
     JS_ASSERT(callsite_->isNormal());
 }
