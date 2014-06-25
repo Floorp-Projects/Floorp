50be3aa14550a57d0270caa655f4dea34b7fe90d
Wes Kocher <wkocher@mozilla.com>
2014-06-24 18:33:46 -0700

    Backed out 2 changesets (bug 1027890) for B2G Windows Build bustage on a CLOSED TREE
    Backed out changeset bcd694f0e95d (bug 1027890)
    Backed out changeset 49cadfcde709 (bug 1027890)

--- python/mozbuild/mozbuild/base.py
+++ python/mozbuild/mozbuild/base.py
@@ -396,7 +396,7 @@ class MozbuildObject(ProcessExecutionMixin):
             srcdir=False, allow_parallel=True, line_handler=None,
             append_env=None, explicit_env=None, ignore_errors=False,
             ensure_exit_code=0, silent=True, print_directory=True,
-            pass_thru=False, num_jobs=0):
+            pass_thru=False, num_jobs=0, force_pymake=False):
         """Invoke make.
 
         directory -- Relative directory to look for Makefile in.
@@ -408,10 +408,11 @@ class MozbuildObject(ProcessExecutionMixin):
         silent -- If True (the default), run make in silent mode.
         print_directory -- If True (the default), have make print directories
         while doing traversal.
+        force_pymake -- If True, pymake will be used instead of GNU make.
         """
         self._ensure_objdir_exists()
 
-        args = self._make_path()
+        args = self._make_path(force_pymake=force_pymake)
 
         if directory:
             args.extend(['-C', directory.replace(os.sep, '/')])
@@ -434,7 +435,9 @@ class MozbuildObject(ProcessExecutionMixin):
             args.append('-s')
 
         # Print entering/leaving directory messages. Some consumers look at
-        # these to measure progress.
+        # these to measure progress. Ideally, we'd do everything with pymake
+        # and use hooks in its API. Unfortunately, it doesn't provide that
+        # feature... yet.
         if print_directory:
             args.append('-w')
 
@@ -472,28 +475,44 @@ class MozbuildObject(ProcessExecutionMixin):
 
         return fn(**params)
 
-    def _make_path(self):
-        baseconfig = os.path.join(self.topsrcdir, 'config', 'baseconfig.mk')
-        for test in ('gmake', 'make', 'mozmake', 'gnumake'):
+    def _make_path(self, force_pymake=False):
+        if self._is_windows() and not force_pymake:
+            # Use gnumake if it's available and we can verify it's a working
+            # version.
+            baseconfig = os.path.join(self.topsrcdir, 'config', 'baseconfig.mk')
+            if os.path.exists(baseconfig):
+                try:
+                    make = which.which('gnumake')
+                    subprocess.check_call([make, '-f', baseconfig, 'HOST_OS_ARCH=WINNT'],
+                        stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
+                    return [make]
+                except subprocess.CalledProcessError:
+                    pass
+                except which.WhichError:
+                    pass
+
+            # Use mozmake if it's available.
             try:
-                make = which.which(test)
-                if os.path.exists(baseconfig):
-                    cmd = [make, '-f', baseconfig]
-                    if self._is_windows():
-                        cmd.append('HOST_OS_ARCH=WINNT')
-                    subprocess.check_call(cmd, stdout=open(os.devnull, 'wb'),
-                        stderr=subprocess.STDOUT)
-                return [make]
-            except subprocess.CalledProcessError:
-                pass
+                return [which.which('mozmake')]
             except which.WhichError:
                 pass
 
-        if self._is_windows():
-            raise Exception('Could not find a suitable make implementation.\n'
-                'Please use MozillaBuild 1.9 or newer')
-        else:
-            raise Exception('Could not find a suitable make implementation.')
+        if self._is_windows() or force_pymake:
+            make_py = os.path.join(self.topsrcdir, 'build', 'pymake',
+                'make.py').replace(os.sep, '/')
+
+            # We might want to consider invoking with the virtualenv's Python
+            # some day. But, there is a chicken-and-egg problem w.r.t. when the
+            # virtualenv is created.
+            return [sys.executable, make_py]
+
+        for test in ['gmake', 'make']:
+            try:
+                return [which.which(test)]
+            except which.WhichError:
+                continue
+
+        raise Exception('Could not find a suitable make implementation.')
 
     def _run_command_in_srcdir(self, **args):
         return self.run_process(cwd=self.topsrcdir, **args)
