ba04f4e5a9f8ce4b1bc185c02eb4f1a1673fd497
Luke Wagner <luke@mozilla.com>
2014-06-25 14:26:03 -0500

    Bug 1030446 - Move AsmJSFrameIterator to its own file (r=dougc)
    --HG--
    extra : rebase_source : 850ff8faa5b356c7b8709191a717e1982fd81e36

---
+++ js/src/jit/AsmJSFrameIterator.h
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=8 sts=4 et sw=4 tw=99:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef jit_AsmJSFrameIterator_h
+#define jit_AsmJSFrameIterator_h
+
+#include <stdint.h>
+
+class JSAtom;
+
+namespace js {
+
+class AsmJSActivation;
+class AsmJSModule;
+namespace jit { struct CallSite; }
+
+// Iterates over the frames of a single AsmJSActivation.
+class AsmJSFrameIterator
+{
+    const AsmJSModule *module_;
+    const jit::CallSite *callsite_;
+    uint8_t *sp_;
+
+    void settle(uint8_t *returnAddress);
+
+  public:
+    explicit AsmJSFrameIterator(const AsmJSActivation *activation);
+    void operator++();
+    bool done() const { return !module_; }
+    JSAtom *functionDisplayAtom() const;
+    unsigned computeLine(uint32_t *column) const;
+};
+
+} // namespace js
+
+#endif // jit_AsmJSFrameIterator_h
