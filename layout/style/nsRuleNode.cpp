d2fc9fbc6b8f7e2a4730feaec6421ccb90f0e6c8
Brian Birtles <birtles@gmail.com>
2014-06-27 08:57:11 +0900

    Bug 1010067 part 3 - Rename nsAnimation to mozilla::StyleAnimation; r=dbaron

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -4981,7 +4981,7 @@ nsRuleNode::ComputeDisplayData(void* aStartStruct,
 
   // Fill in the animations we just allocated with the appropriate values.
   for (uint32_t i = 0; i < numAnimations; ++i) {
-    nsAnimation *animation = &display->mAnimations[i];
+    StyleAnimation *animation = &display->mAnimations[i];
 
     if (i >= animDelay.num) {
       animation->SetDelay(display->mAnimations[i % animDelay.num].GetDelay());
