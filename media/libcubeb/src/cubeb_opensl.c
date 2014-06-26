0d25fe041ba694b2baea9bb2df7fbcbf971277c3
Bruce Sun <brsun@mozilla.com>
2014-06-26 14:59:48 +0800

    Bug 1017438 - Use SL_PLAYEVENT_HEADATMARKER event to trigger CUBEB_STATE_DRAINED state callback. r=kinetik

--- media/libcubeb/src/cubeb_opensl.c
+++ media/libcubeb/src/cubeb_opensl.c
@@ -8,6 +8,7 @@
 #include <assert.h>
 #include <dlfcn.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <SLES/OpenSLES.h>
 #if defined(__ANDROID__)
 #include "android/sles_definitions.h"
@@ -42,6 +43,7 @@ struct cubeb {
 
 struct cubeb_stream {
   cubeb * context;
+  pthread_mutex_t mutex;
   SLObjectItf playerObj;
   SLPlayItf play;
   SLBufferQueueItf bufq;
@@ -50,6 +52,7 @@ struct cubeb_stream {
   long queuebuf_len;
   long bytespersec;
   long framesize;
+  long written;
   int draining;
   cubeb_stream_type stream_type;
 
@@ -59,26 +62,38 @@ struct cubeb_stream {
 
   cubeb_resampler * resampler;
   unsigned int inputrate;
+  unsigned int outputrate;
   unsigned int latency;
 };
+
+static void
+play_callback(SLPlayItf caller, void * user_ptr, SLuint32 event)
+{
+  cubeb_stream * stm = user_ptr;
+  assert(stm);
+  switch (event) {
+    case SL_PLAYEVENT_HEADATMARKER:
+      pthread_mutex_lock(&stm->mutex);
+      assert(stm->draining);
+      stm->draining = 0;
+      pthread_mutex_unlock(&stm->mutex);
+      stm->state_callback(stm, stm->user_ptr, CUBEB_STATE_DRAINED);
+      break;
+    default:
+      break;
+  }
+}
 
 static void
 bufferqueue_callback(SLBufferQueueItf caller, void * user_ptr)
 {
   cubeb_stream * stm = user_ptr;
+  assert(stm);
   SLBufferQueueState state;
   SLresult res;
 
   res = (*stm->bufq)->GetState(stm->bufq, &state);
   assert(res == SL_RESULT_SUCCESS);
-
-  if (stm->draining) {
-    if (!state.count) {
-      stm->draining = 0;
-      stm->state_callback(stm, stm->user_ptr, CUBEB_STATE_DRAINED);
-    }
-    return;
-  }
 
   if (state.count > 1)
     return;
@@ -86,24 +101,40 @@ bufferqueue_callback(SLBufferQueueItf caller, void * user_ptr)
   SLuint32 i;
   for (i = state.count; i < NBUFS; i++) {
     void *buf = stm->queuebuf[stm->queuebuf_idx];
-    long written = cubeb_resampler_fill(stm->resampler, buf,
+    long written = 0;
+    pthread_mutex_lock(&stm->mutex);
+    int draining = stm->draining;
+    pthread_mutex_unlock(&stm->mutex);
+
+    if (!draining) {
+      written = cubeb_resampler_fill(stm->resampler, buf,
                                         stm->queuebuf_len / stm->framesize);
-    if (written == CUBEB_ERROR) {
-      (*stm->play)->SetPlayState(stm->play, SL_PLAYSTATE_STOPPED);
-      return;
+      if (written < 0 || written * stm->framesize > stm->queuebuf_len) {
+        (*stm->play)->SetPlayState(stm->play, SL_PLAYSTATE_STOPPED);
+        return;
+      }
     }
 
-    if (written) {
-      res = (*stm->bufq)->Enqueue(stm->bufq, buf, written * stm->framesize);
-      assert(res == SL_RESULT_SUCCESS);
-      stm->queuebuf_idx = (stm->queuebuf_idx + 1) % NBUFS;
-    } else if (!i) {
-      stm->state_callback(stm, stm->user_ptr, CUBEB_STATE_DRAINED);
-      return;
+    // Keep sending silent data even in draining mode to prevent the audio
+    // back-end from being stopped automatically by OpenSL/ES.
+    memset(buf + written * stm->framesize, 0, stm->queuebuf_len - written * stm->framesize);
+    res = (*stm->bufq)->Enqueue(stm->bufq, buf, stm->queuebuf_len);
+    assert(res == SL_RESULT_SUCCESS);
+    stm->queuebuf_idx = (stm->queuebuf_idx + 1) % NBUFS;
+    if (written > 0) {
+      pthread_mutex_lock(&stm->mutex);
+      stm->written += written;
+      pthread_mutex_unlock(&stm->mutex);
     }
 
-    if ((written * stm->framesize) < stm->queuebuf_len) {
+    if (!draining && written * stm->framesize < stm->queuebuf_len) {
+      pthread_mutex_lock(&stm->mutex);
+      int64_t written_duration = INT64_C(1000) * stm->written * stm->framesize / stm->bytespersec;
       stm->draining = 1;
+      pthread_mutex_unlock(&stm->mutex);
+      // Use SL_PLAYEVENT_HEADATMARKER event from slPlayCallback of SLPlayItf
+      // to make sure all the data has been processed.
+      (*stm->play)->SetMarkerPosition(stm->play, (SLmillisecond)written_duration);
       return;
     }
   }
@@ -443,6 +474,9 @@ opensl_stream_init(cubeb * ctx, cubeb_stream ** stream, char const * stream_name
   stm->stream_type = stream_params.stream_type;
   stm->framesize = stream_params.channels * sizeof(int16_t);
 
+  int r = pthread_mutex_init(&stm->mutex, NULL);
+  assert(r == 0);
+
   SLDataLocator_BufferQueue loc_bufq;
   loc_bufq.locatorType = SL_DATALOCATOR_BUFFERQUEUE;
   loc_bufq.numBuffers = NBUFS;
@@ -486,6 +520,7 @@ opensl_stream_init(cubeb * ctx, cubeb_stream ** stream, char const * stream_name
     return CUBEB_ERROR;
   }
 
+  stm->outputrate = preferred_sampling_rate;
   stm->bytespersec = preferred_sampling_rate * stm->framesize;
   stm->queuebuf_len = (stm->bytespersec * latency) / (1000 * NBUFS);
   // round up to the next multiple of stm->framesize, if needed.
@@ -545,6 +580,18 @@ opensl_stream_init(cubeb * ctx, cubeb_stream ** stream, char const * stream_name
     return CUBEB_ERROR;
   }
 
+  res = (*stm->play)->RegisterCallback(stm->play, play_callback, stm);
+  if (res != SL_RESULT_SUCCESS) {
+    opensl_stream_destroy(stm);
+    return CUBEB_ERROR;
+  }
+
+  res = (*stm->play)->SetCallbackEventsMask(stm->play, (SLuint32)SL_PLAYEVENT_HEADATMARKER);
+  if (res != SL_RESULT_SUCCESS) {
+    opensl_stream_destroy(stm);
+    return CUBEB_ERROR;
+  }
+
   res = (*stm->bufq)->RegisterCallback(stm->bufq, bufferqueue_callback, stm);
   if (res != SL_RESULT_SUCCESS) {
     opensl_stream_destroy(stm);
@@ -564,6 +611,7 @@ opensl_stream_destroy(cubeb_stream * stm)
   for (i = 0; i < NBUFS; i++) {
     free(stm->queuebuf[i]);
   }
+  pthread_mutex_destroy(&stm->mutex);
 
   cubeb_resampler_destroy(stm->resampler);
 
@@ -613,10 +661,18 @@ opensl_stream_get_position(cubeb_stream * stm, uint64_t * position)
     return CUBEB_ERROR;
   }
 
-  if (msec > mixer_latency) {
-    *position = samplerate * (msec - mixer_latency) / 1000;
-  } else {
+  int64_t compensated_position = samplerate * (msec - mixer_latency) / 1000;
+  pthread_mutex_lock(&stm->mutex);
+  int64_t maximum_position = stm->written * (int64_t)stm->inputrate / stm->outputrate;
+  pthread_mutex_unlock(&stm->mutex);
+  assert(maximum_position >= 0);
+
+  if (compensated_position < 0) {
     *position = 0;
+  } else if(compensated_position > maximum_position) {
+    *position = maximum_position;
+  } else {
+    *position = compensated_position;
   }
   return CUBEB_OK;
 }
