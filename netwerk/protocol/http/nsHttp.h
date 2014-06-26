33b8c693e55f47847fbe02070db68fc2a005b8d2
Nicholas Hurley <hurley@todesschaf.org>
2014-06-25 17:33:15 -0700

    Bug 1026609 - HTTP/2 draft 13 part 1 - client implementation. r=mcmanus

--- netwerk/protocol/http/nsHttp.h
+++ netwerk/protocol/http/nsHttp.h
@@ -35,13 +35,14 @@ namespace net {
         // 25 was spdy/4a2
         // 26 was http/2-draft08 and http/2-draft07 (they were the same)
         // 27 was http/2-draft09, h2-10, and h2-11
-        HTTP2_VERSION_DRAFT12 = 28
+        // 28 was http/2-draft12
+        HTTP2_VERSION_DRAFT13 = 29
     };
 
 typedef uint8_t nsHttpVersion;
 
-#define NS_HTTP2_DRAFT_VERSION HTTP2_VERSION_DRAFT12
-#define NS_HTTP2_DRAFT_TOKEN "h2-12"
+#define NS_HTTP2_DRAFT_VERSION HTTP2_VERSION_DRAFT13
+#define NS_HTTP2_DRAFT_TOKEN "h2-13"
 
 //-----------------------------------------------------------------------------
 // http connection capabilities
