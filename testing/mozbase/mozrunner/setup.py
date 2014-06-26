a25e10455fa58b6449b7f305f8d0832944433c8d
Jonathan Griffin <jgriffin@mozilla.com>
2014-06-26 14:51:58 -0700

    Bug 1026259 - Improve wait_for_net to reduce the chances of dropping wifi when calling B2GRunner.start(), r=ahal, a=test-only

--- testing/mozbase/mozrunner/setup.py
+++ testing/mozbase/mozrunner/setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup
 
 PACKAGE_NAME = 'mozrunner'
-PACKAGE_VERSION = '5.37'
+PACKAGE_VERSION = '5.37.1'
 
 desc = """Reliable start/stop/configuration of Mozilla Applications (Firefox, Thunderbird, etc.)"""
 
