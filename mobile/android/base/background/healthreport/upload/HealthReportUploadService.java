d758defe014308a0957ab606739b7c0b90f76828
Richard Newman <rnewman@mozilla.com>
2014-06-26 09:48:48 -0700

    Bug 1030740 - Add return statements inadvertently omitted from Bug 1025937. r=mcomella, a=sylvestre

--- mobile/android/base/background/healthreport/upload/HealthReportUploadService.java
+++ mobile/android/base/background/healthreport/upload/HealthReportUploadService.java
@@ -45,7 +45,8 @@ public class HealthReportUploadService extends BackgroundService {
 
     // Intent can be null. Bug 1025937.
     if (intent == null) {
-        Logger.debug(LOG_TAG, "Short-circuiting on null intent.");
+      Logger.debug(LOG_TAG, "Short-circuiting on null intent.");
+      return;
     }
 
     if (HealthReportConstants.UPLOAD_FEATURE_DISABLED) {
