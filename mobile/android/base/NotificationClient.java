4edd9ce5cd3810c90875fcbc2588fc86fbdcab9f
Wes Johnston <wjohnston@mozilla.com>
2014-06-27 13:25:00 -0700

    Bug 1004495 - Revamp notification handlers to work when Gecko is not running. r=bnicholson

--- mobile/android/base/NotificationClient.java
+++ mobile/android/base/NotificationClient.java
@@ -20,7 +20,7 @@ public abstract class NotificationClient {
     private static final String LOGTAG = "GeckoNotificationClient";
 
     private volatile NotificationHandler mHandler;
-    private boolean mReady;
+    private boolean mReady = false;
     private final LinkedList<Runnable> mTaskQueue = new LinkedList<Runnable>();
     private final ConcurrentHashMap<Integer, UpdateRunnable> mUpdatesMap =
             new ConcurrentHashMap<Integer, UpdateRunnable>();
@@ -142,10 +142,6 @@ public abstract class NotificationClient {
      * @see NotificationHandler#remove(int)
      */
     public synchronized void remove(final int notificationID) {
-        if (!mReady) {
-            return;
-        }
-
         mTaskQueue.add(new Runnable() {
             @Override
             public void run() {
@@ -153,6 +149,13 @@ public abstract class NotificationClient {
                 mUpdatesMap.remove(notificationID);
             }
         });
+
+        // If mReady == false, we haven't added any notifications yet. That can happen if Fennec is being
+        // started in response to clicking a notification. Call bind() to ensure the task we posted above is run.
+        if (!mReady) {
+            bind();
+        }
+
         notify();
     }
 
