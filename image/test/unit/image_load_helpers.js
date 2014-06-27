3930e27f88fa54639a8a122c4b5657d71bcf2d40
Monica Chew <mmc@mozilla.com>
2014-06-27 13:57:36 -0700

    Bug 1029781: Set NS_LOAD_CLASSIFY_URI in image loader (r=seth)

--- image/test/unit/image_load_helpers.js
+++ image/test/unit/image_load_helpers.js
@@ -16,6 +16,9 @@ const FRAME_COMPLETE = 0x04;
 const LOAD_COMPLETE = 0x08;
 const DECODE_COMPLETE = 0x10;
 
+// Safebrowsing requires that the profile dir is set.
+do_get_profile();
+
 // An implementation of imgIScriptedNotificationObserver with the ability to
 // call specified functions on onStartRequest and onStopRequest.
 function ImageListener(start_callback, stop_callback)
