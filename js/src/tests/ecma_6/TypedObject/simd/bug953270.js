ae431cd48c765aa94538841f1a323a610cb12f5c
ProgramFOX <programfox@hotmail.be>
2014-06-30 18:29:15 +0200

    Bug 1031198: Renamed SIMD functions X4.toY4 and X4.bitsToY4 into Y4.fromX4 and Y4.fromX4Bits; r=bbouvier

--- js/src/tests/ecma_6/TypedObject/simd/bug953270.js
+++ js/src/tests/ecma_6/TypedObject/simd/bug953270.js
@@ -13,14 +13,14 @@ var summary = 'Handles';
 
 var int32x4 = SIMD.int32x4;
 var a = int32x4((4294967295), 200, 300, 400);
-var c = SIMD.int32x4.bitsToFloat32x4(a);
+var c = SIMD.float32x4.fromInt32x4Bits(a);
 
 // NaN canonicalization occurs when extracting out x lane:
 assertEq(c.x, NaN);
 
 // but underlying bits are faithfully transmitted
 // (though reinterpreted as a signed integer):
-var d = SIMD.float32x4.bitsToInt32x4(c);
+var d = SIMD.int32x4.fromFloat32x4Bits(c);
 assertEq(d.x, -1);
 
 reportCompare(true, true);
