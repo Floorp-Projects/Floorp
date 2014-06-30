1e5f14f99d040d15a6e53c788bf8c5b52309145e
Branislav Rankov <branislav.rankov@imgtec.com>
2014-06-30 16:28:17 +0200

    Bug 1020359 - IonMonkey MIPS: Add missing or32. r=nbp

--- js/src/jit/mips/MacroAssembler-mips.h
+++ js/src/jit/mips/MacroAssembler-mips.h
@@ -1020,6 +1020,7 @@ public:
     void and32(Imm32 imm, Register dest);
     void and32(Imm32 imm, const Address &dest);
     void and32(const Address &src, Register dest);
+    void or32(Imm32 imm, Register dest);
     void or32(Imm32 imm, const Address &dest);
     void xor32(Imm32 imm, Register dest);
     void xorPtr(Imm32 imm, Register dest);
