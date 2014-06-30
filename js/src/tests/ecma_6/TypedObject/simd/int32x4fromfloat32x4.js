71c425bdffba2c75ad05b827d39842ccd0d3d3c5
ProgramFOX <programfox@hotmail.be>
2014-06-30 18:29:15 +0200

    Bug 1031198: Renamed SIMD functions X4.toY4 and X4.bitsToY4 into Y4.fromX4 and Y4.fromX4Bits; r=bbouvier

--- js/src/tests/ecma_6/TypedObject/simd/int32x4fromfloat32x4.js
+++ js/src/tests/ecma_6/TypedObject/simd/int32x4fromfloat32x4.js
@@ -3,7 +3,7 @@ var BUGNUMBER = 946042;
 var float32x4 = SIMD.float32x4;
 var int32x4 = SIMD.int32x4;
 
-var summary = 'float32x4 toInt32x4';
+var summary = 'int32x4 fromFloat32x4';
 
 function test() {
   print(BUGNUMBER + ": " + summary);
@@ -11,7 +11,7 @@ function test() {
   // FIXME -- Bug 948379: Amend to check for correctness of border cases.
 
   var a = float32x4(1.1, 2.2, 3.3, 4.6);
-  var c = SIMD.float32x4.toInt32x4(a);
+  var c = SIMD.int32x4.fromFloat32x4(a);
   assertEq(c.x, 1);
   assertEq(c.y, 2);
   assertEq(c.z, 3);
