bdcb32984581dea610c4202569a32fe8019de5ca
Jason Orendorff <jorendorff@mozilla.com>
2014-06-30 14:22:44 -0500

    Bug 1032208, part 1 - Fix JSON.stringify treatment of symbols. r=jandem.
    --HG--
    extra : rebase_source : 9336bc1f4ccacc15369ae6297d8cf1bbc0a66cba

---
+++ js/src/tests/ecma_6/Symbol/json-stringify-values.js
@@ -0,0 +1,33 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/licenses/publicdomain/ */
+
+// To JSON.stringify, symbols are the same as undefined.
+
+var symbols = [
+    Symbol(),
+    Symbol.for("ponies"),
+    Symbol.iterator
+];
+
+for (var sym of symbols) {
+    assertEq(JSON.stringify(sym), undefined);
+    assertEq(JSON.stringify([sym]), "[null]");
+
+    // JSON.stringify skips symbol-valued properties!
+    assertEq(JSON.stringify({x: sym}), '{}');
+
+    // However such properties are passed to the replacerFunction if any.
+    var replacer = function (key, val) {
+        assertEq(typeof this, "object");
+        if (typeof val === "symbol") {
+            assertEq(val, sym);
+            return "ding";
+        }
+        return val;
+    };
+    assertEq(JSON.stringify(sym, replacer), '"ding"');
+    assertEq(JSON.stringify({x: sym}, replacer), '{"x":"ding"}');
+}
+
+if (typeof reportCompare === 'function')
+    reportCompare(0, 0, 'ok');
