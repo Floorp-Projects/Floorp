f61aebde0b1f28e4ee1618d5d943f59a24abd30f
Edgar Chen <echen@mozilla.com>
2014-07-01 18:37:32 +0800

    Bug 843452 - Part 3-3: MobileConnectionIPCService for content process. r=smaug

--- dom/mobileconnection/src/ipc/MobileConnectionChild.cpp
+++ dom/mobileconnection/src/ipc/MobileConnectionChild.cpp
@@ -139,6 +139,19 @@ MobileConnectionChild::GetNetworkSelectionMode(nsAString& aMode)
   aMode = mNetworkSelectionMode;
 }
 
+bool
+MobileConnectionChild::SendRequest(MobileConnectionRequest aRequest,
+                                   nsIMobileConnectionCallback* aRequestCallback)
+{
+  NS_ENSURE_TRUE(mLive, false);
+
+  // Deallocated in MobileConnectionChild::DeallocPMobileConnectionRequestChild().
+  MobileConnectionRequestChild* actor = new MobileConnectionRequestChild(aRequestCallback);
+  SendPMobileConnectionRequestConstructor(actor, aRequest);
+
+  return true;
+}
+
 void
 MobileConnectionChild::ActorDestroy(ActorDestroyReason why)
 {
