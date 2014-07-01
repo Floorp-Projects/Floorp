e6bb636984ec1b105e089d2e8a22ffc4d4ea5cfb
Steve Fink <sfink@mozilla.com>
2014-07-01 14:23:33 -0700

    Bug 1022773 - Return value rooting for XPConnect, r=bholley, a=abillings

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -984,8 +984,8 @@ public:
     bool AttachComponentsObject(JSContext *aCx);
 
     // Returns the JS object reflection of the Components object.
-    JSObject*
-    GetComponentsJSObject();
+    bool
+    GetComponentsJSObject(JS::MutableHandleObject obj);
 
     JSObject*
     GetGlobalJSObject() const {
@@ -1003,7 +1003,7 @@ public:
     }
 
     JSObject*
-    GetExpandoChain(JSObject *target);
+    GetExpandoChain(JS::HandleObject target);
 
     bool
     SetExpandoChain(JSContext *cx, JS::HandleObject target, JS::HandleObject chain);
