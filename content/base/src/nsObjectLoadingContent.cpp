1bac649376bb5f21b4f20b8bbe3dca5986b02977
Terrence Cole <terrence@mozilla.com>
2014-07-01 09:11:01 -0700

    Bug 1032876 - Do not do excessive and unnecessary plugin proto teardown; r=johns

--- content/base/src/nsObjectLoadingContent.cpp
+++ content/base/src/nsObjectLoadingContent.cpp
@@ -2450,7 +2450,7 @@ nsObjectLoadingContent::UnloadObject(bool aResetState)
 
   mScriptRequested = false;
 
-  if (!mInstanceOwner) {
+  if (mIsStopping) {
     // The protochain is normally thrown out after a plugin stops, but if we
     // re-enter while stopping a plugin and try to load something new, we need
     // to throw away the old protochain in the nested unload.
