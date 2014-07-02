4006fff8ac08c9716f343433996ee87fc4303303
Gregory Szorc <gps@mozilla.com>
2014-07-02 16:49:36 -0700

    Bug 1033836 - Rename services-common to services/common; rs=rnewman
    The upcoming build system patches don't support hypthens in path names.
    Changing this for that reason is kind of silly, but it's the easiest
    way. Besides, nothing else uses hyphens in directory names.

--- services/datareporting/tests/xpcshell/head.js
+++ services/datareporting/tests/xpcshell/head.js
@@ -8,7 +8,7 @@ do_get_profile();
 
 (function initTestingInfrastructure() {
   let ns = {};
-  Components.utils.import("resource://testing-common/services-common/logging.js",
+  Components.utils.import("resource://testing-common/services/common/logging.js",
                           ns);
 
   ns.initTestLogging();
