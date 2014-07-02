a4f975bc9479e9eb4e7a9aaf99704fce73469203
Jonathan Watt <jwatt@jwatt.org>
2014-07-02 11:42:26 +0100

    Bug 1023336, part 1 - Add a Moz2D helper to consistently limit the size of data-wrapping DataSourceSurface buffers. r=Bas, a=sylvestre

--- gfx/2d/DataSurfaceHelpers.cpp
+++ gfx/2d/DataSurfaceHelpers.cpp
@@ -5,6 +5,9 @@
 
 #include "2D.h"
 #include "DataSurfaceHelpers.h"
+#include "Logging.h"
+#include "mozilla/CheckedInt.h"
+#include "mozilla/MathAlgorithms.h"
 
 namespace mozilla {
 namespace gfx {
@@ -135,6 +138,31 @@ SurfaceToPackedBGR(DataSourceSurface *aSurface)
   aSurface->Unmap();
 
   return imageBuffer;
+}
+
+size_t
+BufferSizeFromStrideAndHeight(int32_t aStride,
+                              int32_t aHeight,
+                              int32_t aExtraBytes)
+{
+  if (MOZ_UNLIKELY(aHeight <= 0)) {
+    return 0;
+  }
+
+  // We limit the length returned to values that can be represented by int32_t
+  // because we don't want to allocate buffers any bigger than that. This
+  // allows for a buffer size of over 2 GiB which is already rediculously
+  // large and will make the process janky. (Note the choice of the signed type
+  // is deliberate because we specifically don't want the returned value to
+  // overflow if someone stores the buffer length in an int32_t variable.)
+
+  CheckedInt32 requiredBytes =
+    CheckedInt32(aStride) * CheckedInt32(aHeight) + CheckedInt32(aExtraBytes);
+  if (MOZ_UNLIKELY(!requiredBytes.isValid())) {
+    gfxWarning() << "Buffer size too big; returning zero";
+    return 0;
+  }
+  return requiredBytes.value();
 }
 
 }
--- gfx/2d/DataSurfaceHelpers.h
+++ gfx/2d/DataSurfaceHelpers.h
@@ -45,6 +45,22 @@ SurfaceToPackedBGRA(DataSourceSurface *aSurface);
 uint8_t*
 SurfaceToPackedBGR(DataSourceSurface *aSurface);
 
+/**
+ * Multiplies aStride and aHeight and makes sure the result is limited to
+ * something sane. To keep things consistent, this should always be used
+ * wherever we allocate a buffer based on surface stride and height.
+ *
+ * @param aExtra Optional argument to specify an additional number of trailing
+ *   bytes (useful for creating intermediate surfaces for filters, for
+ *   example).
+ *
+ * @return The result of the multiplication if it is acceptable, or else zero.
+ */
+size_t
+BufferSizeFromStrideAndHeight(int32_t aStride,
+                              int32_t aHeight,
+                              int32_t aExtraBytes = 0);
+
 }
 }
 
