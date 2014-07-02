74a506daf6239a911886ec3dc213ddf6b3eb69db
Honza Bambas <honzab.moz@firemni.cz>
2014-07-02 19:53:00 +0200

    Backing out 6c5c6cbad330 (bug 1032992) for Adnroid failures - GC doesn't work there as on other platforms..., r=backout

--- netwerk/test/unit/head_cache2.js
+++ netwerk/test/unit/head_cache2.js
@@ -45,8 +45,6 @@ const NOTWANTED =       1 << 11;
 const COMPLETE =        1 << 12;
 // Don't write meta/data and don't set valid in the callback, consumer will do it manually
 const DONTFILL =        1 << 13;
-// Used in combination with METAONLY, don't call setValid() on the entry after metadata has been set
-const DONTSETVALID =    1 << 14;
 
 var log_c2 = true;
 function LOG_C2(o, m)
@@ -194,13 +192,8 @@ OpenCallback.prototype =
         entry.metaDataReady();
         if (self.behavior & METAONLY) {
           // Since forcing GC/CC doesn't trigger OnWriterClosed, we have to set the entry valid manually :(
-          if (!(self.behavior & DONTSETVALID))
-            entry.setValid();
-
+          entry.setValid();
           entry.close();
-          if (self.behavior & WAITFORWRITE)
-            self.goon(entry);
-
           return;
         }
         do_execute_soon(function() { // emulate more network latency
