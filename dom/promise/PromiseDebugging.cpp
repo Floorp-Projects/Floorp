e6f74ae48bfe983e06f7bd69d57c540622fcff76
Boris Zbarsky <bzbarsky@mit.edu>
2014-07-02 00:19:48 -0400

    Bug 966471.  Add some facilities for examining the state of a promise via a PromiseDebugging utility namespace.  r=nsm,peterv

---
+++ dom/promise/PromiseDebugging.cpp
@@ -0,0 +1,40 @@
+/* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/PromiseDebugging.h"
+
+#include "js/Value.h"
+
+#include "mozilla/dom/BindingDeclarations.h"
+#include "mozilla/dom/Promise.h"
+#include "mozilla/dom/PromiseDebuggingBinding.h"
+
+namespace mozilla {
+namespace dom {
+
+/* static */ void
+PromiseDebugging::GetState(GlobalObject&, Promise& aPromise,
+                           PromiseDebuggingStateHolder& aState)
+{
+  switch (aPromise.mState) {
+  case Promise::Pending:
+    aState.mState = PromiseDebuggingState::Pending;
+    break;
+  case Promise::Resolved:
+    aState.mState = PromiseDebuggingState::Fulfilled;
+    JS::ExposeValueToActiveJS(aPromise.mResult);
+    aState.mValue = aPromise.mResult;
+    break;
+  case Promise::Rejected:
+    aState.mState = PromiseDebuggingState::Rejected;
+    JS::ExposeValueToActiveJS(aPromise.mResult);
+    aState.mReason = aPromise.mResult;
+    break;
+  }
+}
+
+} // namespace dom
+} // namespace mozilla
