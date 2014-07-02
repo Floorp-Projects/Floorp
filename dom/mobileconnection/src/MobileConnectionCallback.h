4d2f74ec3bc4915bcc39184ea57dc603cff13ecf
Edgar Chen <echen@mozilla.com>
2014-07-02 17:38:23 +0800

    Bug 843452 - Part 2-3: MobileConnection DOM changes for MobileConnectionService interface. r=smaug

---
+++ dom/mobileconnection/src/MobileConnectionCallback.h
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_MobileConnectionCallback_h
+#define mozilla_dom_MobileConnectionCallback_h
+
+#include "mozilla/dom/DOMRequest.h"
+#include "nsCOMPtr.h"
+#include "nsIMobileConnectionService.h"
+
+namespace mozilla {
+namespace dom {
+
+/**
+ * A callback object for handling asynchronous request/response. This object is
+ * created when an asynchronous request is made and should be destroyed after
+ * Notify*Success/Error is called.
+ * The modules hold the reference of MobileConnectionCallback in OOP mode and
+ * non-OOP mode are different.
+ * - OOP mode: MobileConnectionRequestChild
+ * - non-OOP mode: MobileConnectionGonkService
+ * The reference should be released after Notify*Success/Error is called.
+ */
+class MobileConnectionCallback MOZ_FINAL : public nsIMobileConnectionCallback
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIMOBILECONNECTIONCALLBACK
+
+  MobileConnectionCallback(nsPIDOMWindow* aWindow, DOMRequest* aRequest);
+
+private:
+  ~MobileConnectionCallback() {}
+
+  nsresult
+  NotifySuccess(JS::Handle<JS::Value> aResult);
+
+  nsCOMPtr<nsPIDOMWindow> mWindow;
+  nsRefPtr<DOMRequest> mRequest;
+};
+
+} // name space dom
+} // name space mozilla
+
+#endif // mozilla_dom_MobileConnectionCallback_h
