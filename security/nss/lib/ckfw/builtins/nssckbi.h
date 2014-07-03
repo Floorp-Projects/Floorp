e0f2edd89ee217317032a0f5458627cdbdc2810a
Kai Engert <kaie@kuix.de>
2014-07-03 20:25:20 +0200

    Bug 1029561, Update to NSS 3.16.3 after July 1st to include root CA updates, r=rrelyea, a=lmandel

--- security/nss/lib/ckfw/builtins/nssckbi.h
+++ security/nss/lib/ckfw/builtins/nssckbi.h
@@ -44,9 +44,9 @@
  * whether we may use its full range (0-255) or only 0-99 because
  * of the comment in the CK_VERSION type definition.
  */
-#define NSS_BUILTINS_LIBRARY_VERSION_MAJOR 1
-#define NSS_BUILTINS_LIBRARY_VERSION_MINOR 98
-#define NSS_BUILTINS_LIBRARY_VERSION "1.98"
+#define NSS_BUILTINS_LIBRARY_VERSION_MAJOR 2
+#define NSS_BUILTINS_LIBRARY_VERSION_MINOR 0
+#define NSS_BUILTINS_LIBRARY_VERSION "2.0"
 
 /* These version numbers detail the semantic changes to the ckfw engine. */
 #define NSS_BUILTINS_HARDWARE_VERSION_MAJOR 1
