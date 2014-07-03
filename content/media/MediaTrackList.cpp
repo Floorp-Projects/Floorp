cb4e6b4e65d966dfca0f155cc77ab764f666bb8a
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-07-03 11:50:27 +0200

    Backed out changeset 645a7aeef102 (bug 744896) for B2G ICS Emulator Opt Test failures on a CLOSED TREE

--- content/media/MediaTrackList.cpp
+++ content/media/MediaTrackList.cpp
@@ -105,15 +105,6 @@ MediaTrackList::RemoveTrack(const nsRefPtr<MediaTrack>& aTrack)
   CreateAndDispatchTrackEventRunner(aTrack, NS_LITERAL_STRING("removetrack"));
 }
 
-void
-MediaTrackList::RemoveTracks()
-{
-  while (!mTracks.IsEmpty()) {
-    nsRefPtr<MediaTrack> track = mTracks.LastElement();
-    RemoveTrack(track);
-  }
-}
-
 already_AddRefed<AudioTrack>
 MediaTrackList::CreateAudioTrack(const nsAString& aId,
                                  const nsAString& aKind,
