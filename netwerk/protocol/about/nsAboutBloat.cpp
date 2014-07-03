e62864656cbf5ac8a396a5a0576ec9faeb690d6a
Wes Kocher <wkocher@mozilla.com>
2014-07-02 17:29:33 -0700

    Backed out 2 changesets (bug 1028187) for mochitest-oth orange
    Backed out changeset 01cba8ff52dd (bug 1028187)
    Backed out changeset caf43baf3306 (bug 1028187)

--- netwerk/protocol/about/nsAboutBloat.cpp
+++ netwerk/protocol/about/nsAboutBloat.cpp
@@ -10,7 +10,6 @@
 
 #include "nsAboutBloat.h"
 #include "nsStringStream.h"
-#include "nsDOMString.h"
 #include "nsIURI.h"
 #include "nsCOMPtr.h"
 #include "nsNetUtil.h"
@@ -123,13 +122,6 @@ nsAboutBloat::GetURIFlags(nsIURI *aURI, uint32_t *result)
 {
     *result = 0;
     return NS_OK;
-}
-
-NS_IMETHODIMP
-nsAboutBloat::GetIndexedDBOriginPostfix(nsIURI *aURI, nsAString &result)
-{
-    SetDOMStringToNull(result);
-    return NS_ERROR_NOT_IMPLEMENTED;
 }
 
 nsresult
