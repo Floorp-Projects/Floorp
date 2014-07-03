be48dd093011b53025a063e1ec2f191846fd7db8
Ryan VanderMeulen <ryanvm@gmail.com>
2014-07-03 13:31:31 -0400

    Backed out changesets 4cd69faa8bc0 and 54252e150439 (bug 976963) for B2G mochitest failures.

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -7171,9 +7171,6 @@ PresShell::HandleEvent(nsIFrame* aFrame,
 
         if (pointerCapturingContent) {
           if (nsIFrame* capturingFrame = pointerCapturingContent->GetPrimaryFrame()) {
-            // If pointer capture is set, we should suppress pointerover/pointerenter events
-            // for all elements except element which have pointer capture. (Code in EventStateManager)
-            pointerEvent->retargetedByPointerCapture = (frame != capturingFrame);
             frame = capturingFrame;
           }
 
