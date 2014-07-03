49eb84d1f56d461b4d7a7cc6484cd8eb98be6969
Luke Wagner <luke@mozilla.com>
2014-07-03 17:00:26 -0500

    Bug 1034330 - OdinMonkey: have masm.assumeUnreachable just generate a breakpoint in asm.js (r=bbouvier)
    --HG--
    extra : rebase_source : f924c22b82a068fc2f5bc4834f1c4c3bcd799a2b

--- js/src/jit/AsmJS.cpp
+++ js/src/jit/AsmJS.cpp
@@ -5872,7 +5872,7 @@ AssertStackAlignment(MacroAssembler &masm)
     Label ok;
     JS_ASSERT(IsPowerOfTwo(StackAlignment));
     masm.branchTestPtr(Assembler::Zero, StackPointer, Imm32(StackAlignment - 1), &ok);
-    masm.assumeUnreachable("Stack should be aligned.");
+    masm.breakpoint();
     masm.bind(&ok);
 #endif
 }
