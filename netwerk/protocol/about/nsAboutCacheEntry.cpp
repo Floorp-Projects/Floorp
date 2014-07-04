93552370c045167d0276e07b4f03c534b6952f72
Tim Taubert <ttaubert@mozilla.com>
2014-07-03 17:16:08 -0700

    Bug 1028187 - Enable IndexedDB for about:looppanel and about:loopconversation, sharing an origin r=bz

--- netwerk/protocol/about/nsAboutCacheEntry.cpp
+++ netwerk/protocol/about/nsAboutCacheEntry.cpp
@@ -7,6 +7,7 @@
 #include "nsAboutCache.h"
 #include "nsICacheStorage.h"
 #include "CacheObserver.h"
+#include "nsDOMString.h"
 #include "nsNetUtil.h"
 #include "prprf.h"
 #include "nsEscape.h"
@@ -106,6 +107,13 @@ nsAboutCacheEntry::GetURIFlags(nsIURI *aURI, uint32_t *result)
 {
     *result = nsIAboutModule::HIDE_FROM_ABOUTABOUT;
     return NS_OK;
+}
+
+NS_IMETHODIMP
+nsAboutCacheEntry::GetIndexedDBOriginPostfix(nsIURI *aURI, nsAString &result)
+{
+    SetDOMStringToNull(result);
+    return NS_ERROR_NOT_IMPLEMENTED;
 }
 
 //-----------------------------------------------------------------------------
