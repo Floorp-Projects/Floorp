ef4510d1f8322c928086e5af55c9b671ee5dcf94
Gregory Szorc <gps@mozilla.com>
2014-07-03 17:03:10 -0700

    Bug 1028407 - Establish connection to server faster; r=jgriffin
    There was a 5 second sleep() in the Marionette client that made
    establishing connections to servers take a long time. The history of
    this value is unclear, but bug comments seem to indicate that Gecko
    wasn't completely initialized during the observer notification that
    starts the server.
    
    We insert some code to let the JavaScript event loop spin for a few
    ticks before starting the Marionette server. This should hopefully give
    Gecko enough time to initialize.

--- testing/marionette/components/marionettecomponent.js
+++ testing/marionette/components/marionettecomponent.js
@@ -107,7 +107,20 @@ MarionetteComponent.prototype = {
         this.finalUiStartup = true;
         this.observerService.removeObserver(this, aTopic);
         this.observerService.addObserver(this, "xpcom-shutdown", false);
-        this.init();
+
+        // Other Gecko services start during observers. This may cause race
+        // conditions with Marionette. We wait a few event loop ticks before
+        // initializing.
+        // NOTE: This is a bit hacky. If we have a dependency on something,
+        // we should explicitly wait for that thing. This solution is little
+        // better than a sleep() in hopes time fixes the problem.
+        Services.tm.currentThread.dispatch(() => {
+          Services.tm.currentThread.dispatch(() => {
+            Services.tm.currentThread.dispatch(() => {
+              this.init();
+            }, Ci.nsIThread.DISPATCH_NORMAL);
+          }, Ci.nsIThread.DISPATCH_NORMAL);
+        }, Ci.nsIThread.DISPATCH_NORMAL);
         break;
       case "domwindowopened":
         this.observerService.removeObserver(this, aTopic);
