7f29c9d8f8db409c48bb2296fcbe03b0c1572cb4
Boris Zbarsky <bzbarsky@mit.edu>
2014-07-04 01:21:57 -0400

    Bug 1018583 part 1.  Remove the execute-in-sandbox mode for javascript: URIs, and use the don't-execute mode wherever we used the sandbox one.  r=bholley,dao

--- docshell/test/browser/browser_loadDisallowInherit.js
+++ docshell/test/browser/browser_loadDisallowInherit.js
@@ -47,7 +47,9 @@ function test() {
 
   let urls = [
     "data:text/html,<body>hi",
-    "javascript:1;"
+    // We used to test javascript: here as well, but now that we no longer run
+    // javascript: in a sandbox, we end up not running it at all in the
+    // DISALLOW_INHERIT_OWNER case, so never actually do a load for it at all.
   ];
 
   function nextTest() {
