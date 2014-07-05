8d2b029b97f1ed8c1feeefeb0a7b5051bb8dd67a
Richard Newman <rnewman@mozilla.com>
2014-07-05 11:00:42 -0700

    Bug 1034509 - Fix timing issue in testDistribution. r=nalexander

--- mobile/android/base/util/ThreadUtils.java
+++ mobile/android/base/util/ThreadUtils.java
@@ -154,10 +154,16 @@ public final class ThreadUtils {
             return;
         }
 
-        final String message = "Expected thread " +
-                               expectedThreadId + " (\"" + expectedThread.getName() +
-                               "\"), but running on thread " +
-                               currentThreadId + " (\"" + currentThread.getName() + ")";
+        final String message;
+        if (expected) {
+            message = "Expected thread " + expectedThreadId +
+                      " (\"" + expectedThread.getName() + "\"), but running on thread " +
+                      currentThreadId + " (\"" + currentThread.getName() + "\")";
+        } else {
+            message = "Expected anything but " + expectedThreadId +
+                      " (\"" + expectedThread.getName() + "\"), but running there.";
+        }
+
         final IllegalThreadStateException e = new IllegalThreadStateException(message);
 
         switch (behavior) {
