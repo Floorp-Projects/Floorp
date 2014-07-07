6e8f1368305d6095dc5174d78b2180bce3a647de
Randell Jesup <rjesup@jesup.org>
2014-07-07 14:45:36 -0400

    Bug 1035067 - Don't hint we expect a track if we're not going to receive it. r=ehugg, a=lmandel

--- media/webrtc/signaling/src/sipcc/core/gsm/gsm_sdp.c
+++ media/webrtc/signaling/src/sipcc/core/gsm/gsm_sdp.c
@@ -5262,7 +5262,10 @@ gsmsdp_negotiate_media_lines (fsm_fcb_t *fcb_p, cc_sdp_t *sdp_p, boolean initial
                       /*
                        * Add track to remote streams in dcb
                        */
-                      if (SDP_MEDIA_APPLICATION != media_type) {
+                      if (SDP_MEDIA_APPLICATION != media_type &&
+                          /* Do not expect to receive media if we're sendonly! */
+                          (media->direction == SDP_DIRECTION_SENDRECV ||
+                           media->direction == SDP_DIRECTION_RECVONLY)) {
                           int pc_stream_id = -1;
 
                           /* This is a hack to keep all the media in a single
