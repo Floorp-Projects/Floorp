323bb78c942a92ac1bab308e48386b1c5189dfa0
Sid Stamm <sstamm@mozilla.com>
2014-07-07 11:59:37 -0700

    Bug 991466 - remove the specCompliant flag from CSP since it's no longer needed. (r=ckerschb,jst)

--- content/base/src/nsCSPContext.cpp
+++ content/base/src/nsCSPContext.cpp
@@ -286,8 +286,7 @@ nsCSPContext::RemovePolicy(uint32_t aIndex)
 NS_IMETHODIMP
 nsCSPContext::AppendPolicy(const nsAString& aPolicyString,
                            nsIURI* aSelfURI,
-                           bool aReportOnly,
-                           bool aSpecCompliant)
+                           bool aReportOnly)
 {
   CSPCONTEXTLOG(("nsCSPContext::AppendPolicy: %s",
                  NS_ConvertUTF16toUTF8(aPolicyString).get()));
@@ -1215,16 +1214,6 @@ nsCSPContext::Read(nsIObjectInputStream* aStream)
     rv = aStream->ReadBoolean(&reportOnly);
     NS_ENSURE_SUCCESS(rv, rv);
 
-    bool specCompliant = false;
-    rv = aStream->ReadBoolean(&specCompliant);
-    NS_ENSURE_SUCCESS(rv, rv);
-
-    // Using the new backend, we don't support non-spec-compliant policies, so
-    // skip any of those, will be fixed in bug 991466
-    if (!specCompliant) {
-      continue;
-    }
-
     nsCSPPolicy* policy = nsCSPParser::parseContentSecurityPolicy(policyString,
                                                                   mSelfURI,
                                                                   reportOnly,
@@ -1254,8 +1243,6 @@ nsCSPContext::Write(nsIObjectOutputStream* aStream)
     mPolicies[p]->toString(polStr);
     aStream->WriteWStringZ(polStr.get());
     aStream->WriteBoolean(mPolicies[p]->getReportOnlyFlag());
-    // Setting specCompliant boolean for backwards compatibility (fix in bug 991466)
-    aStream->WriteBoolean(true);
   }
   return NS_OK;
 }
