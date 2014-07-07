41ae5a98fce39e9dd7de48ab7f5221a5e09f5742
Geoff Brown <gbrown@mozilla.com>
2014-07-07 13:19:10 -0600

    Bug 995696 - Make testNewTab more robust, and enable on Android 2.3; r=snorp

--- mobile/android/base/tests/testNewTab.java
+++ mobile/android/base/tests/testNewTab.java
@@ -42,57 +42,7 @@ public class testNewTab extends BaseTest {
         mAsserter.is(tabCountInt, expectedTabCount, "Number of tabs increased");
 
         // cleanup: close all opened tabs
-        //closeTabs();
-    }
-
-    private void closeTabs() {
-        final int closeTabId = closeTab.getId();
-        String tabCountText = null;
-
-        // open tabs panel
-        boolean clicked = tabs.click();
-        if (!clicked) {
-            mAsserter.ok(clicked != false, "checking that tabs clicked", "tabs element clicked");
-        }
-
-        // wait for closeTab to appear (this is usually immediate)
-        boolean success = waitForTest(new BooleanTest() {
-            @Override
-            public boolean test() {
-                View closeTabView = getActivity().findViewById(closeTabId);
-                if (closeTabView == null) {
-                    return false;
-                }
-                return true;
-            }
-        }, MAX_WAIT_MS);
-        if (!success) {
-            mAsserter.ok(success != false, "waiting for close tab view", "close tab view available");
-        }
-
-        // close tabs until only one remains
-        tabCountText = tabCount.getText();
-        tabCountInt = Integer.parseInt(tabCountText);
-        while (tabCountInt > 1) {
-            clicked = closeTab.click();
-            if (!clicked) {
-                mAsserter.ok(clicked != false, "checking that close_tab clicked", "close_tab element clicked");
-            }
-
-            success = waitForCondition(new Condition() {
-                @Override
-                public boolean isSatisfied() {
-                    String newTabCountText = tabCount.getText();
-                    int newTabCount = Integer.parseInt(newTabCountText);
-                    if (newTabCount < tabCountInt) {
-                        tabCountInt = newTabCount;
-                        return true;
-                    }
-                    return false;
-                }
-            }, MAX_WAIT_MS);
-            mAsserter.ok(success, "Checking tab closed", "number of tabs now "+tabCountInt);
-        }
+        closeAddedTabs();
     }
 
     private void getTabCount(final int expected) {
