7a3e2cd1c442215c43fc4465102b79a8722621eb
Lucas Rocha <lucasr@mozilla.com>
2014-07-07 15:59:52 +0100

    Bug 1035131 - Factor out methods to encode/decode user-entered:// URLs. r=mfinkle, a=lmandel

--- mobile/android/base/home/TopSitesGridView.java
+++ mobile/android/base/home/TopSitesGridView.java
@@ -14,6 +14,7 @@ import org.mozilla.gecko.ThumbnailHelper;
 import org.mozilla.gecko.db.BrowserContract.TopSites;
 import org.mozilla.gecko.db.TopSitesCursorWrapper;
 import org.mozilla.gecko.home.HomePager.OnUrlOpenListener;
+import org.mozilla.gecko.util.StringUtils;
 
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -106,7 +107,7 @@ public class TopSitesGridView extends GridView {
                 TopSitesGridItemView item = (TopSitesGridItemView) view;
 
                 // Decode "user-entered" URLs before loading them.
-                String url = HomeFragment.decodeUserEnteredUrl(item.getUrl());
+                String url = StringUtils.decodeUserEnteredUrl(item.getUrl());
                 int type = item.getType();
 
                 // If the url is empty, the user can pin a site.
