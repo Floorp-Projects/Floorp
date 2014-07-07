224407baaa3ea977e17a1b2319011eedcc4bccde
Sushil Chauhan <sushilchauhan@codeaurora.org>
2014-07-07 12:58:26 -0700

    Bug 1027343 - Geometry change flag needs to be set for modified layer tree. r=mattwoodrow, a=2.0+

--- gfx/layers/composite/LayerManagerComposite.cpp
+++ gfx/layers/composite/LayerManagerComposite.cpp
@@ -247,6 +247,9 @@ LayerManagerComposite::EndTransaction(DrawThebesLayerCallback aCallback,
 
     Render();
     mGeometryChanged = false;
+  } else {
+    // Modified layer tree
+    mGeometryChanged = true;
   }
 
   mCompositor->ClearTargetContext();
@@ -399,6 +402,8 @@ LayerManagerComposite::Render()
       }
     }
     mCompositor->EndFrameForExternalComposition(mWorldMatrix);
+    // Reset the invalid region as compositing is done
+    mInvalidRegion.SetEmpty();
     return;
   }
 
