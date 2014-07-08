f8804b2ff69981d7d9549b87b07076444a29f7b0
Tooru Fujisawa <arai_a@mac.com>
2014-07-08 02:25:00 -0400

    Bug 923028 - Part 2: Emit loop instead of JSOP_SPREAD. r=jandem

--- js/src/jit/BaselineCompiler.cpp
+++ js/src/jit/BaselineCompiler.cpp
@@ -2370,34 +2370,6 @@ BaselineCompiler::emit_JSOP_INITELEM_INC()
     return true;
 }
 
-typedef bool (*SpreadFn)(JSContext *, HandleObject, HandleValue,
-                         HandleValue, MutableHandleValue);
-static const VMFunction SpreadInfo = FunctionInfo<SpreadFn>(js::SpreadOperation);
-
-bool
-BaselineCompiler::emit_JSOP_SPREAD()
-{
-    // Load index and iterator in R0 and R1, but keep values on the stack for
-    // the decompiler.
-    frame.syncStack(0);
-    masm.loadValue(frame.addressOfStackValue(frame.peek(-2)), R0);
-    masm.loadValue(frame.addressOfStackValue(frame.peek(-1)), R1);
-
-    prepareVMCall();
-
-    pushArg(R1);
-    pushArg(R0);
-    masm.extractObject(frame.addressOfStackValue(frame.peek(-3)), R0.scratchReg());
-    pushArg(R0.scratchReg());
-
-    if (!callVM(SpreadInfo))
-        return false;
-
-    frame.popn(2);
-    frame.push(R0);
-    return true;
-}
-
 bool
 BaselineCompiler::emit_JSOP_GETLOCAL()
 {
