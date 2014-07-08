f99120cd1b8abce0fcc2e966d91706b2fdf3fd16
Ben Kelly <ben@wanderview.com>
2014-07-08 14:57:16 -0400

    Bug 1013571 Reland support for PBackground on workers. r=bent

--- ipc/glue/BackgroundChild.h
+++ ipc/glue/BackgroundChild.h
@@ -37,6 +37,11 @@ class PBackgroundChild;
 // (assuming success) GetForCurrentThread() will return the same actor every
 // time.
 //
+// CloseForCurrentThread() will close the current PBackground actor.  Subsequent
+// calls to GetForCurrentThread will return null.  CloseForCurrentThread() may
+// only be called exactly once for each thread-specific actor.  Currently it is
+// illegal to call this before the PBackground actor has been created.
+//
 // The PBackgroundChild actor and all its sub-protocol actors will be
 // automatically destroyed when its designated thread completes.
 class BackgroundChild MOZ_FINAL
@@ -55,6 +60,10 @@ public:
   // See above.
   static bool
   GetOrCreateForCurrentThread(nsIIPCBackgroundChildCreateCallback* aCallback);
+
+  // See above.
+  static void
+  CloseForCurrentThread();
 
 private:
   // Only called by ContentChild or ContentParent.
