24d3b9ebd9a0764d791ad740517e5ff1f995d343
Mark Banner <standard8@mozilla.com>
2014-07-08 07:37:57 +0100

    Bug 1032017 Remove MOZ_SERVICES_FXACCOUNTS flags as it is no longer necessary. Also fixes test_hawkrequest.js failures in m-c. r=glandium

--- mobile/android/confvars.sh
+++ mobile/android/confvars.sh
@@ -58,9 +58,6 @@ MOZ_PAY=1
 # Enable UI for healthreporter
 MOZ_SERVICES_HEALTHREPORT=1
 
-# Enable FirefoxAccounts
-MOZ_SERVICES_FXACCOUNTS=1
-
 # Wifi-AP/cell tower data reporting is enabled on non-release builds.
 if test ! "$RELEASE_BUILD"; then
 MOZ_DATA_REPORTING=1
