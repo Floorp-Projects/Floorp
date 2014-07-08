cbbc1e4a4e872dc0603a5e92c85f9814767d24c4
Andrew McCreight <continuation@gmail.com>
2014-07-08 13:46:24 -0700

    Bug 1034914 - Clean up CreateContentIterator and give nsFilteredContentIterator a private dtor. r=ehsan

--- editor/txtsvc/nsFilteredContentIterator.h
+++ editor/txtsvc/nsFilteredContentIterator.h
@@ -18,10 +18,7 @@ class nsIDOMRange;
 class nsINode;
 class nsITextServicesFilter;
 
-/**
- * 
- */
-class nsFilteredContentIterator : public nsIContentIterator
+class nsFilteredContentIterator MOZ_FINAL : public nsIContentIterator
 {
 public:
 
@@ -30,8 +27,6 @@ public:
   NS_DECL_CYCLE_COLLECTION_CLASS(nsFilteredContentIterator)
 
   nsFilteredContentIterator(nsITextServicesFilter* aFilter);
-
-  virtual ~nsFilteredContentIterator();
 
   /* nsIContentIterator */
   virtual nsresult Init(nsINode* aRoot);
@@ -50,6 +45,8 @@ public:
 
 protected:
   nsFilteredContentIterator() { }
+
+  virtual ~nsFilteredContentIterator();
 
   // enum to give us the direction
   typedef enum {eDirNotSet, eForward, eBackward} eDirectionType;
@@ -73,13 +70,5 @@ protected:
   bool                            mIsOutOfRange;
   eDirectionType                  mDirection;
 };
-
-namespace mozilla {
-template<>
-struct HasDangerousPublicDestructor<nsFilteredContentIterator>
-{
-  static const bool value = true;
-};
-}
 
 #endif
