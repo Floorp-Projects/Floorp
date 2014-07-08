7f293adeeda767d4f6f4bb30077adf73129c2365
jvoisin <julien.voisin@dustri.org>
2014-07-08 05:53:00 +0100

    Bug 1035786 - Fix namespace bug in Linux sandbox LOG_ERROR macro. r=jld

--- security/sandbox/linux/Sandbox.cpp
+++ security/sandbox/linux/Sandbox.cpp
@@ -55,7 +55,7 @@ namespace mozilla {
 #define LOG_ERROR(args...) __android_log_print(ANDROID_LOG_ERROR, "Sandbox", ## args)
 #elif defined(PR_LOGGING)
 static PRLogModuleInfo* gSeccompSandboxLog;
-#define LOG_ERROR(args...) PR_LOG(gSeccompSandboxLog, PR_LOG_ERROR, (args))
+#define LOG_ERROR(args...) PR_LOG(mozilla::gSeccompSandboxLog, PR_LOG_ERROR, (args))
 #else
 #define LOG_ERROR(args...)
 #endif
