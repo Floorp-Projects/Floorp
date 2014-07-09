cd42cad5687814847b4360fe60a479bc3a48efed
Milan Sreckovic <milan@mozilla.com>
2014-07-09 14:21:49 -0400

    Bug 918386 - Add webidl for adapter subsys.  Only relevant on Windows.  r=vvukicevic.

--- widget/windows/GfxInfo.h
+++ widget/windows/GfxInfo.h
@@ -28,6 +28,7 @@ public:
   NS_IMETHOD GetAdapterDriver(nsAString & aAdapterDriver);
   NS_IMETHOD GetAdapterVendorID(nsAString & aAdapterVendorID);
   NS_IMETHOD GetAdapterDeviceID(nsAString & aAdapterDeviceID);
+  NS_IMETHOD GetAdapterSubsysID(nsAString & aAdapterSubsysID);
   NS_IMETHOD GetAdapterRAM(nsAString & aAdapterRAM);
   NS_IMETHOD GetAdapterDriverVersion(nsAString & aAdapterDriverVersion);
   NS_IMETHOD GetAdapterDriverDate(nsAString & aAdapterDriverDate);
@@ -35,6 +36,7 @@ public:
   NS_IMETHOD GetAdapterDriver2(nsAString & aAdapterDriver);
   NS_IMETHOD GetAdapterVendorID2(nsAString & aAdapterVendorID);
   NS_IMETHOD GetAdapterDeviceID2(nsAString & aAdapterDeviceID);
+  NS_IMETHOD GetAdapterSubsysID2(nsAString & aAdapterSubsysID);
   NS_IMETHOD GetAdapterRAM2(nsAString & aAdapterRAM);
   NS_IMETHOD GetAdapterDriverVersion2(nsAString & aAdapterDriverVersion);
   NS_IMETHOD GetAdapterDriverDate2(nsAString & aAdapterDriverDate);
@@ -72,7 +74,7 @@ private:
   nsString mDeviceKeyDebug;
   nsString mAdapterVendorID;
   nsString mAdapterDeviceID;
-  uint32_t mAdapterSubsysID;
+  nsString mAdapterSubsysID;
   nsString mDeviceString2;
   nsString mDriverVersion2;
   nsString mDeviceID2;
@@ -80,7 +82,7 @@ private:
   nsString mDeviceKey2;
   nsString mAdapterVendorID2;
   nsString mAdapterDeviceID2;
-  uint32_t mAdapterSubsysID2;
+  nsString mAdapterSubsysID2;
   uint32_t mWindowsVersion;
   bool mHasDualGPU;
   bool mIsGPU2Active;
