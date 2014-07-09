72a9088d217ff29a7fe845093473af11d818ae46
Matt Woodrow <mwoodrow@mozilla.com>
2014-07-09 14:01:54 +1200

    Bug 1034247 - Avoid propogating scale factors down to ThebesLayers if it would result in them being larger than the max texture size. r=roc

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -4731,12 +4731,7 @@ nsDisplayTransform::ShouldPrerenderTransformedContent(nsDisplayListBuilder* aBui
   refSize += nsSize(refSize.width / 8, refSize.height / 8);
   nsSize frameSize = aFrame->GetVisualOverflowRectRelativeToSelf().Size();
   if (frameSize <= refSize) {
-    // Bug 717521 - pre-render max 4096 x 4096 device pixels.
-    nscoord max = aFrame->PresContext()->DevPixelsToAppUnits(4096);
-    nsRect visual = aFrame->GetVisualOverflowRect();
-    if (visual.width <= max && visual.height <= max) {
-      return true;
-    }
+    return true;
   }
 
   if (aLogAnimations) {
