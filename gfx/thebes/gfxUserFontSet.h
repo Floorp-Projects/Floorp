e0d35db426d937bed119ded80042c180ae755ad9
Cameron McCormack <cam@mcc.id.au>
2014-07-09 21:04:15 +1000

    Bug 1031202 - Factor out gfxMixedFontFamily lookup/creation into a helper function. r=jdaggett

--- gfx/thebes/gfxUserFontSet.h
+++ gfx/thebes/gfxUserFontSet.h
@@ -188,13 +188,15 @@ public:
     // Whether there is a face with this family name
     bool HasFamily(const nsAString& aFamilyName) const
     {
-        return GetFamily(aFamilyName) != nullptr;
+        return LookupFamily(aFamilyName) != nullptr;
     }
 
-    gfxFontFamily *GetFamily(const nsAString& aName) const;
+    // Look up and return the gfxMixedFontFamily in mFontFamilies with
+    // the given name
+    gfxMixedFontFamily* LookupFamily(const nsAString& aName) const;
 
     // Lookup a font entry for a given style, returns null if not loaded.
-    // aFamily must be a family returned by our GetFamily method.
+    // aFamily must be a family returned by our LookupFamily method.
     gfxFontEntry *FindFontEntry(gfxFontFamily *aFamily,
                                 const gfxFontStyle& aFontStyle,
                                 bool& aNeedsBold,
@@ -429,6 +431,10 @@ protected:
 
     // helper method for performing the actual userfont set rebuild
     virtual void DoRebuildUserFontSet() = 0;
+
+    // creates a new gfxMixedFontFamily in mFontFamilies, or returns an existing
+    // family if there is one
+    gfxMixedFontFamily* GetFamily(const nsAString& aFamilyName);
 
     // font families defined by @font-face rules
     nsRefPtrHashtable<nsStringHashKey, gfxMixedFontFamily> mFontFamilies;
