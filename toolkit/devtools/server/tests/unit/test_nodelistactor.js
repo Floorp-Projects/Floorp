61727b38c70b62461aeb6ac342472ebc53f2f852
Sami Jaktholm <sjakthol@outlook.com>
2014-07-09 04:40:00 -0400

    Bug 1036366 - Fix 'TypeError: this.nodelist is null' if a NodeListActor is initialized with a null nodelist. r=pbrosset

---
+++ toolkit/devtools/server/tests/unit/test_nodelistactor.js
@@ -0,0 +1,26 @@
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+// Test that a NodeListActor initialized with null nodelist doesn't cause
+// exceptions when calling NodeListActor.form.
+
+const { NodeListActor } = devtools.require("devtools/server/actors/inspector");
+
+function run_test() {
+  check_actor_for_list(null);
+  check_actor_for_list([]);
+  check_actor_for_list(["fakenode"]);
+}
+
+function check_actor_for_list(nodelist) {
+  do_print("Checking NodeListActor with nodelist '" + nodelist + "' works.");
+  let actor = new NodeListActor({}, nodelist);
+  let form = actor.form();
+
+  // No exception occured as a exceptions abort the test.
+  ok(true, "No exceptions occured.");
+  equal(form.length, nodelist ? nodelist.length : 0,
+    "NodeListActor reported correct length.");
+}
