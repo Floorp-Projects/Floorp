cdce692a1bd50425603fcdb5902dfa62e48f373b
Jonathan Watt <jwatt@jwatt.org>
2014-07-10 10:58:07 +0100

    Bug 933019, part 5 - Remove code for handling Thebes backed gfxContext from various files under gfx/thebes/. r=Bas

--- gfx/thebes/gfxWindowsPlatform.h
+++ gfx/thebes/gfxWindowsPlatform.h
@@ -60,7 +60,6 @@ struct DCFromContext {
     DCFromContext(gfxContext *aContext) {
         dc = nullptr;
         nsRefPtr<gfxASurface> aSurface = aContext->CurrentSurface();
-        NS_ASSERTION(aSurface || !aContext->IsCairo(), "DCFromContext: null surface");
         if (aSurface &&
             (aSurface->GetType() == gfxSurfaceType::Win32 ||
              aSurface->GetType() == gfxSurfaceType::Win32Printing))
