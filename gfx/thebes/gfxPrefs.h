aa2ece0f219f12fcef734f147bf849964bbfa279
Kearwood (Kip) Gilbert <kgilbert@mozilla.com>
2014-07-10 11:52:40 -0700

    Bug 1022825 - Implement Asynchronous Smooth Scrolling on Compositor Thread. r=kats
    - Extended nsIScrollableFrame and nsGfxScrollFrame to return destination
      of smooth scrolls which are to be animated on the compositor thread.
    - Added apz.smooth_scroll_repaint_interval preference.
    - Implemented AsyncPanZoomController::PanZoomState::SMOOTH_MSD_SCROLL state
      and AsyncPanZoomController::SmoothScrollAnimation class to animate smooth
      scroll animations on the compositor thread.
    - Extended FrameMetrics to report requests for smooth scrolls to be animated
      on the compositor thread and their corresponding destination positions.
    - AsyncPanZoomController now checks FrameMetrics for requests to perform
      smooth scrolling on the compositor thread.  It will ensure that they
      are cancelled as needed by mousewheel, touchpanel, keyboard, and
      CSSOM-View instant scrolling DOM methods.
    - The layout/generic/test/test_scroll_behavior.html mochitest has been
      commented as depending on Bug 1062609 before being enabled for APZ.

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -158,6 +158,7 @@ private:
   DECL_GFX_PREF(Live, "apz.overscroll.snap_back.mass",         APZOverscrollSnapBackMass, float, 1000.0f);
   DECL_GFX_PREF(Live, "apz.pan_repaint_interval",              APZPanRepaintInterval, int32_t, 250);
   DECL_GFX_PREF(Live, "apz.printtree",                         APZPrintTree, bool, false);
+  DECL_GFX_PREF(Live, "apz.smooth_scroll_repaint_interval",    APZSmoothScrollRepaintInterval, int32_t, 75);
   DECL_GFX_PREF(Live, "apz.subframe.enabled",                  APZSubframeEnabled, bool, false);
   DECL_GFX_PREF(Once, "apz.test.logging_enabled",              APZTestLoggingEnabled, bool, false);
   DECL_GFX_PREF(Live, "apz.touch_start_tolerance",             APZTouchStartTolerance, float, 1.0f/4.5f);
