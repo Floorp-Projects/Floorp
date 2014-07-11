d87bd9f2a200b7d562681a118e58eb0f48c1c138
Sami Jaktholm <sjakthol@outlook.com>
2014-07-11 00:54:00 -0400

    Bug 1037280 - Inspector: Wait for toolbox to be destroyed before ending test run. r=pbrosset

--- browser/devtools/inspector/test/head.js
+++ browser/devtools/inspector/test/head.js
@@ -49,9 +49,9 @@ SimpleTest.registerCleanupFunction(() => {
   Services.prefs.clearUserPref("devtools.inspector.activeSidebar");
 });
 
-registerCleanupFunction(() => {
+registerCleanupFunction(function*() {
   let target = TargetFactory.forTab(gBrowser.selectedTab);
-  gDevTools.closeToolbox(target);
+  yield gDevTools.closeToolbox(target);
 
   // Move the mouse outside inspector. If the test happened fake a mouse event
   // somewhere over inspector the pointer is considered to be there when the
