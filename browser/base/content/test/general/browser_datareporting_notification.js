7ee43c896416b2d6201f3ac6461dcba8822d7241
Wes Kocher <wkocher@mozilla.com>
2014-07-11 17:00:08 -0700

    Backed out changeset 8673477f5fc2 (bug 862563) for Windows mochitest-5 failures

--- browser/base/content/test/general/browser_datareporting_notification.js
+++ browser/base/content/test/general/browser_datareporting_notification.js
@@ -2,49 +2,30 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-let originalPolicy = null;
-
-/**
- * Display a datareporting notification to the user.
- *
- * @param  {String} name
- */
 function sendNotifyRequest(name) {
   let ns = {};
-  Cu.import("resource://gre/modules/services/datareporting/policy.jsm", ns);
-  Cu.import("resource://gre/modules/Preferences.jsm", ns);
+  Components.utils.import("resource://gre/modules/services/datareporting/policy.jsm", ns);
+  Components.utils.import("resource://gre/modules/Preferences.jsm", ns);
 
-  let service = Cc["@mozilla.org/datareporting/service;1"]
-                  .getService(Ci.nsISupports)
-                  .wrappedJSObject;
+  let service = Components.classes["@mozilla.org/datareporting/service;1"]
+                                  .getService(Components.interfaces.nsISupports)
+                                  .wrappedJSObject;
   ok(service.healthReporter, "Health Reporter instance is available.");
-
-  Cu.import("resource://gre/modules/Promise.jsm", ns);
-  let deferred = ns.Promise.defer();
-
-  if (!originalPolicy) {
-    originalPolicy = service.policy;
-  }
 
   let policyPrefs = new ns.Preferences("testing." + name + ".");
   ok(service._prefs, "Health Reporter prefs are available.");
   let hrPrefs = service._prefs;
 
   let policy = new ns.DataReportingPolicy(policyPrefs, hrPrefs, service);
-  policy.dataSubmissionPolicyBypassNotification = false;
-  service.policy = policy;
   policy.firstRunDate = new Date(Date.now() - 24 * 60 * 60 * 1000);
 
-  service.healthReporter.onInit().then(function onSuccess () {
-    is(policy.ensureUserNotified(), false, "User not notified about data policy on init.");
-    ok(policy._userNotifyPromise, "_userNotifyPromise defined.");
-    policy._userNotifyPromise.then(
-      deferred.resolve.bind(deferred),
-      deferred.reject.bind(deferred)
-    );
-  }.bind(this), deferred.reject.bind(deferred));
+  is(policy.notifyState, policy.STATE_NOTIFY_UNNOTIFIED, "Policy is in unnotified state.");
 
-  return [policy, deferred.promise];
+  service.healthReporter.onInit().then(function onInit() {
+    is(policy.ensureNotifyResponse(new Date()), false, "User has not responded to policy.");
+  });
+
+  return policy;
 }
 
 /**
@@ -74,7 +55,6 @@ function waitForNotificationClose(notification, cb) {
 let dumpAppender, rootLogger;
 
 function test() {
-  registerCleanupFunction(cleanup);
   waitForExplicitFinish();
 
   let ns = {};
@@ -84,41 +64,29 @@ function test() {
   dumpAppender.level = ns.Log.Level.All;
   rootLogger.addAppender(dumpAppender);
 
-  closeAllNotifications().then(function onSuccess () {
-    let notification = document.getElementById("global-notificationbox");
+  let notification = document.getElementById("global-notificationbox");
+  let policy;
 
-    notification.addEventListener("AlertActive", function active() {
-      notification.removeEventListener("AlertActive", active, true);
-      is(notification.allNotifications.length, 1, "Notification Displayed.");
+  notification.addEventListener("AlertActive", function active() {
+    notification.removeEventListener("AlertActive", active, true);
 
-      executeSoon(function afterNotification() {
-        waitForNotificationClose(notification.currentNotification, function onClose() {
-          is(notification.allNotifications.length, 0, "No notifications remain.");
-          is(policy.dataSubmissionPolicyAcceptedVersion, 1, "Version pref set.");
-          ok(policy.dataSubmissionPolicyNotifiedDate.getTime() > -1, "Date pref set.");
-          test_multiple_windows();
-        });
-        notification.currentNotification.close();
+    executeSoon(function afterNotification() {
+      is(policy.notifyState, policy.STATE_NOTIFY_WAIT, "Policy is waiting for user response.");
+      ok(!policy.dataSubmissionPolicyAccepted, "Data submission policy not yet accepted.");
+
+      waitForNotificationClose(notification.currentNotification, function onClose() {
+        is(policy.notifyState, policy.STATE_NOTIFY_COMPLETE, "Closing info bar completes user notification.");
+        ok(policy.dataSubmissionPolicyAccepted, "Data submission policy accepted.");
+        is(policy.dataSubmissionPolicyResponseType, "accepted-info-bar-dismissed",
+           "Reason for acceptance was info bar dismissal.");
+        is(notification.allNotifications.length, 0, "No notifications remain.");
+        test_multiple_windows();
       });
-    }, true);
-
-    let [policy, promise] = sendNotifyRequest("single_window_notified");
-
-    is(policy.dataSubmissionPolicyAcceptedVersion, 0, "No version should be set on init.");
-    is(policy.dataSubmissionPolicyNotifiedDate.getTime(), 0, "No date should be set on init.");
-    is(policy.userNotifiedOfCurrentPolicy, false, "User not notified about datareporting policy.");
-
-    promise.then(function () {
-      is(policy.dataSubmissionPolicyAcceptedVersion, 1, "Policy version set.");
-      is(policy.dataSubmissionPolicyNotifiedDate.getTime() > 0, true, "Policy date set.");
-      is(policy.userNotifiedOfCurrentPolicy, true, "User notified about datareporting policy.");
-    }.bind(this), function (err) {
-      throw err;
+      notification.currentNotification.close();
     });
+  }, true);
 
-  }.bind(this), function onError (err) {
-    throw err;
-  });
+  policy = sendNotifyRequest("single_window_notified");
 }
 
 function test_multiple_windows() {
@@ -130,7 +98,7 @@ function test_multiple_windows() {
     let notification2 = window2.document.getElementById("global-notificationbox");
     ok(notification2, "2nd window has a global notification box.");
 
-    let [policy, promise] = sendNotifyRequest("multiple_window_behavior");
+    let policy;
     let displayCount = 0;
     let prefWindowClosed = false;
     let mutationObserversRemoved = false;
@@ -161,8 +129,8 @@ function test_multiple_windows() {
 
         dump("Finishing multiple window test.\n");
         rootLogger.removeAppender(dumpAppender);
-        dumpAppender = null;
-        rootLogger = null;
+        delete dumpAppender;
+        delete rootLogger;
         finish();
       }
       let closeCount = 0;
@@ -175,8 +143,12 @@ function test_multiple_windows() {
         }
 
         ok(true, "Closing info bar on one window closed them on all.");
-        is(policy.userNotifiedOfCurrentPolicy, true, "Data submission policy accepted.");
 
+        is(policy.notifyState, policy.STATE_NOTIFY_COMPLETE,
+           "Closing info bar with multiple windows completes notification.");
+        ok(policy.dataSubmissionPolicyAccepted, "Data submission policy accepted.");
+        is(policy.dataSubmissionPolicyResponseType, "accepted-info-bar-button-pressed",
+           "Policy records reason for acceptance was button press.");
         is(notification1.allNotifications.length, 0, "No notifications remain on main window.");
         is(notification2.allNotifications.length, 0, "No notifications remain on 2nd window.");
 
@@ -220,20 +192,7 @@ function test_multiple_windows() {
       executeSoon(onAlertDisplayed);
     }, true);
 
-    promise.then(null, function onError(err) {
-      throw err;
-    });
+    policy = sendNotifyRequest("multiple_window_behavior");
   });
 }
 
-function cleanup () {
-  // In case some test fails.
-  if (originalPolicy) {
-    let service = Cc["@mozilla.org/datareporting/service;1"]
-                    .getService(Ci.nsISupports)
-                    .wrappedJSObject;
-    service.policy = originalPolicy;
-  }
-
-  return closeAllNotifications();
-}
