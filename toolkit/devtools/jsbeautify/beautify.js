4ad570d57950f0777fc1db0bcb6931731d017f91
Michael Ratcliffe <mratcliffe@mozilla.com>
2014-07-13 23:12:50 +0100

    Backed out changeset 0ba8be1413f7 (bug 1033387) for xpc test failures

--- toolkit/devtools/jsbeautify/beautify.js
+++
@@ -1,7 +0,0 @@
-var { cssBeautify } = require("devtools/jsbeautify/beautify-css");
-var { htmlBeautify } = require("devtools/jsbeautify/beautify-html");
-var { jsBeautify } = require("devtools/jsbeautify/beautify-js");
-
-exports.css = cssBeautify;
-exports.html = htmlBeautify;
-exports.js = jsBeautify;
