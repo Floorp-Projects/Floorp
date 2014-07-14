efaaf1cf972a0002a69b28b12153dc2f76b29e01
Nicholas Nethercote <nnethercote@mozilla.com>
2014-07-13 23:27:44 -0700

    Bug 1038038 (part 1) - Reduce ShapeTable::MIN_SIZE_LOG2. r=bhackett.
    An entryCount of zero is common, due to initBoundFunction(), so it's worth
    choosing a value that doesn't waste too much space in that case.

--- js/src/vm/Shape.h
+++ js/src/vm/Shape.h
@@ -130,7 +130,10 @@ static const uint32_t SHAPE_MAXIMUM_SLOT = JS_BIT(24) - 2;
 struct ShapeTable {
     static const uint32_t HASH_BITS     = mozilla::tl::BitSize<HashNumber>::value;
     static const uint32_t MIN_ENTRIES   = 7;
-    static const uint32_t MIN_SIZE_LOG2 = 4;
+
+    // This value is low because it's common for a ShapeTable to be created
+    // with an entryCount of zero.
+    static const uint32_t MIN_SIZE_LOG2 = 2;
     static const uint32_t MIN_SIZE      = JS_BIT(MIN_SIZE_LOG2);
 
     int             hashShift;          /* multiplicative hash shift */
