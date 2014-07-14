dfefaa4dffbe0a1e85b614ce119228d0ab95138d
Emanuel Hoogeveen <emanuel.hoogeveen@gmail.com>
2014-07-14 23:40:00 +0200

    Bug 1036784 - Extend freeSpanAllocate to do all the things that FreeList::allocate does, and rename it to freeListAllocate. r=terrence

--- js/src/gc/Heap.h
+++ js/src/gc/Heap.h
@@ -192,6 +192,14 @@ class FreeSpan
         return !first;
     }
 
+    static size_t offsetOfFirst() {
+        return offsetof(FreeSpan, first);
+    }
+
+    static size_t offsetOfLast() {
+        return offsetof(FreeSpan, last);
+    }
+
     // Like nextSpan(), but no checking of the following span is done.
     FreeSpan *nextSpanUnchecked() const {
         return reinterpret_cast<FreeSpan *>(last);
