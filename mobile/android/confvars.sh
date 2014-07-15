eca7a145037134aee83f68abcbb3cfc8b77a6b10
Nick Alexander <nalexander@mozilla.com>
2014-07-15 13:50:22 -0700

    Bug 1024708 - Post: Don't build MLS Stumbler in Fennec by default. r=me

--- mobile/android/confvars.sh
+++ mobile/android/confvars.sh
@@ -78,5 +78,5 @@ MOZ_WEBGL_CONFORMANT=1
 # Don't enable the Search Activity.
 # MOZ_ANDROID_SEARCH_ACTIVITY=1
 
-# Enable the Mozilla Location Service stumbler.
-MOZ_ANDROID_MLS_STUMBLER=1
+# Don't enable the Mozilla Location Service stumbler.
+# MOZ_ANDROID_MLS_STUMBLER=1
