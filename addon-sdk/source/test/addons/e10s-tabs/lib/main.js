664e06b87f050b1a563e29621c58ff612467c9ad
Erik Vold <evold@mozilla.com>
2014-07-15 06:19:40 -0700

    Bug 1037374 - Uplift Add-on SDK to Firefox r=me

--- addon-sdk/source/test/addons/e10s-tabs/lib/main.js
+++ addon-sdk/source/test/addons/e10s-tabs/lib/main.js
@@ -4,10 +4,16 @@
 'use strict';
 
 const { merge } = require('sdk/util/object');
+const { get } = require('sdk/preferences/service');
 
 merge(module.exports, require('./test-tab'));
 merge(module.exports, require('./test-tab-events'));
 merge(module.exports, require('./test-tab-observer'));
 merge(module.exports, require('./test-tab-utils'));
+
+// e10s tests should not ride the train to aurora
+if (get('app.update.channel') !== 'nightly') {
+  module.exports = {};
+}
 
 require('sdk/test/runner').runTestsFromModule(module);
