32993f60808b036a43f8d2c9152dad52ca06283c
Mark Banner <standard8@mozilla.com>
2014-07-15 10:03:03 +0100

    Bug 1033362 Implement basic mochitests for MozLoopAPI. r=dolske,r=glandium

---
+++ browser/components/loop/test/mochitest/head.js
@@ -0,0 +1,71 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+var gMozLoopAPI;
+
+function promiseGetMozLoopAPI() {
+  let deferred = Promise.defer();
+  let loopPanel = document.getElementById("loop-notification-panel");
+  let btn = document.getElementById("loop-call-button");
+
+  // Wait for the popup to be shown, then we can get the iframe and
+  // wait for the iframe's load to be completed.
+  loopPanel.addEventListener("popupshown", function onpopupshown() {
+    loopPanel.removeEventListener("popupshown", onpopupshown, true);
+    let iframe = document.getElementById(btn.getAttribute("notificationFrameId"));
+
+    if (iframe.contentDocument &&
+        iframe.contentDocument.readyState == "complete") {
+      gMozLoopAPI = iframe.contentWindow.navigator.wrappedJSObject.mozLoop;
+
+      deferred.resolve();
+    } else {
+      iframe.addEventListener("load", function panelOnLoad(e) {
+        iframe.removeEventListener("load", panelOnLoad, true);
+
+        gMozLoopAPI = iframe.contentWindow.navigator.wrappedJSObject.mozLoop;
+
+        // We do this in an execute soon to allow any other event listeners to
+        // be handled, just in case.
+        deferred.resolve();
+      }, true);
+    }
+  }, true);
+
+  // Now we're setup, click the button.
+  btn.click();
+
+  // Remove the iframe after each test. This also avoids mochitest complaining
+  // about leaks on shutdown as we intentionally hold the iframe open for the
+  // life of the application.
+  registerCleanupFunction(function() {
+    loopPanel.hidePopup();
+    loopPanel.removeChild(document.getElementById(btn.getAttribute("notificationFrameId")));
+  });
+
+  return deferred.promise;
+}
+
+/**
+ * Loads the loop panel by clicking the button and waits for its open to complete.
+ * It also registers
+ *
+ * This assumes that the tests are running in a generatorTest.
+ */
+function loadLoopPanel() {
+  // Set prefs to ensure we don't access the network externally.
+  Services.prefs.setCharPref("services.push.serverURL", "ws://localhost/");
+  Services.prefs.setCharPref("loop.server", "http://localhost/");
+
+  registerCleanupFunction(function() {
+    Services.prefs.clearUserPref("services.push.serverURL");
+    Services.prefs.clearUserPref("loop.server");
+  });
+
+  // Turn off animations to make tests quicker.
+  let loopPanel = document.getElementById("loop-notification-panel");
+  loopPanel.setAttribute("animate", "false");
+
+  // Now get the actual API.
+  yield promiseGetMozLoopAPI();
+}
