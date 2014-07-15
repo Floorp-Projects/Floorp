d3614ef268fd17d355c7611f37b52de9224c840a
Max Vujovic <mvujovic@adobe.com>
2014-07-15 11:50:00 +0200

    Bug 948265 - Remove kPrimitiveIndexSourceAlpha since nsSVGFilterInstance creates ToAlpha filter nodes now. r=mstange

--- gfx/src/FilterSupport.h
+++ gfx/src/FilterSupport.h
@@ -284,9 +284,8 @@ class FilterPrimitiveDescription MOZ_FINAL {
 public:
   enum {
     kPrimitiveIndexSourceGraphic = -1,
-    kPrimitiveIndexSourceAlpha = -2,
-    kPrimitiveIndexFillPaint = -3,
-    kPrimitiveIndexStrokePaint = -4
+    kPrimitiveIndexFillPaint = -2,
+    kPrimitiveIndexStrokePaint = -3
   };
 
   FilterPrimitiveDescription();
