8ffcdc003f9b5d4ce77413694ce4e247ada90149
Ed Lee <edilee@mozilla.com>
2014-07-16 15:44:13 -0700

    Bug 1038997 - Backout newtab changes including layout and styling changes for beta [r=adw a=sylvestre] Revert bug 980014, bug 978338, bug 991210; fix up search form

--- browser/base/content/test/newtab/head.js
+++ browser/base/content/test/newtab/head.js
@@ -14,8 +14,7 @@ Cu.import("resource://gre/modules/NewTabUtils.jsm", tmp);
 Cc["@mozilla.org/moz/jssubscript-loader;1"]
   .getService(Ci.mozIJSSubScriptLoader)
   .loadSubScript("chrome://browser/content/sanitize.js", tmp);
-Cu.import("resource://gre/modules/Timer.jsm", tmp);
-let {Promise, NewTabUtils, Sanitizer, clearTimeout} = tmp;
+let {Promise, NewTabUtils, Sanitizer} = tmp;
 
 let uri = Services.io.newURI("about:newtab", null, null);
 let principal = Services.scriptSecurityManager.getNoAppCodebasePrincipal(uri);
@@ -62,13 +61,6 @@ registerCleanupFunction(function () {
 
   Services.prefs.clearUserPref(PREF_NEWTAB_ENABLED);
   Services.prefs.clearUserPref(PREF_NEWTAB_DIRECTORYSOURCE);
-
-  // Stop any update timers to prevent unexpected updates in later tests
-  let timer = NewTabUtils.allPages._scheduleUpdateTimeout;
-  if (timer) {
-    clearTimeout(timer);
-    delete NewTabUtils.allPages._scheduleUpdateTimeout;
-  }
 });
 
 /**
