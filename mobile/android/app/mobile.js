d2ef1b90ce895dbb0707094271feb80c20285668
Jon Coppeard <jcoppeard@mozilla.com>
2014-07-16 10:01:20 +0100

    Bug 1017141 - Add prefs for setting min/max empty chunk counts r=bent

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -428,6 +428,12 @@ pref("javascript.options.mem.gc_low_frequency_heap_growth", 120);
 pref("javascript.options.mem.high_water_mark", 16);
 pref("javascript.options.mem.gc_allocation_threshold_mb", 3);
 pref("javascript.options.mem.gc_decommit_threshold_mb", 1);
+#ifdef JSGC_GENERATIONAL
+pref("javascript.options.mem.gc_min_empty_chunk_count", 1);
+#else
+pref("javascript.options.mem.gc_min_empty_chunk_count", 0);
+#endif
+pref("javascript.options.mem.gc_max_empty_chunk_count", 2);
 #else
 pref("javascript.options.mem.high_water_mark", 32);
 #endif
