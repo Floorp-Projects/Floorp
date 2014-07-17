c55b45acd3f77aef913e099eced7dfd7eae0a36f
Ryan VanderMeulen <ryanvm@gmail.com>
2014-07-17 11:24:47 -0400

    Backed out 46 changesets (bug 1022612) for B2G mochitest permafails on a CLOSED TREE.
    Backed out changeset 34b3014a3112 (bug 1022612)
    Backed out changeset 6ae9316fd909 (bug 1022612)
    Backed out changeset b8f3749c95eb (bug 1022612)
    Backed out changeset caab10bf6ca3 (bug 1022612)
    Backed out changeset 0c57c620c898 (bug 1022612)
    Backed out changeset fac64141a00a (bug 1022612)
    Backed out changeset bf0df1c9d68b (bug 1022612)
    Backed out changeset b42054800020 (bug 1022612)
    Backed out changeset 667793b21194 (bug 1022612)
    Backed out changeset f14ada64fe1b (bug 1022612)
    Backed out changeset 75b837686bdf (bug 1022612)
    Backed out changeset 66de53183a22 (bug 1022612)
    Backed out changeset 0ff86ced4d46 (bug 1022612)
    Backed out changeset 18eecc5b1ef7 (bug 1022612)
    Backed out changeset 2763c4878de5 (bug 1022612)
    Backed out changeset b72413ecc385 (bug 1022612)
    Backed out changeset b23f1081afb8 (bug 1022612)
    Backed out changeset f7e2c6a72043 (bug 1022612)
    Backed out changeset 959917c9027d (bug 1022612)
    Backed out changeset 0268a46f4880 (bug 1022612)
    Backed out changeset 3388856a80ad (bug 1022612)
    Backed out changeset e4b17cf0f806 (bug 1022612)
    Backed out changeset 2f4e9da0e4b6 (bug 1022612)
    Backed out changeset 489f6a7c0c03 (bug 1022612)
    Backed out changeset 8369d9ad7ad3 (bug 1022612)
    Backed out changeset 0758d2a06002 (bug 1022612)
    Backed out changeset f2ae9cb22edb (bug 1022612)
    Backed out changeset 9c48c6ee5dc2 (bug 1022612)
    Backed out changeset fe7134400f08 (bug 1022612)
    Backed out changeset cc2c5397ca8b (bug 1022612)
    Backed out changeset a3d1a3e8b39d (bug 1022612)
    Backed out changeset 8974b74b0eb0 (bug 1022612)
    Backed out changeset 75f7dbb5a2a6 (bug 1022612)
    Backed out changeset 2aa04a071e60 (bug 1022612)
    Backed out changeset f2ab1bcd4c39 (bug 1022612)
    Backed out changeset da9152b6ea29 (bug 1022612)
    Backed out changeset 58abf5b0e148 (bug 1022612)
    Backed out changeset 797058a09ad2 (bug 1022612)
    Backed out changeset ea3e99a92ff0 (bug 1022612)
    Backed out changeset adc4a4a7aa73 (bug 1022612)
    Backed out changeset 7b18dedd1505 (bug 1022612)
    Backed out changeset 055dd1921e8e (bug 1022612)
    Backed out changeset 42fa2c97e989 (bug 1022612)
    Backed out changeset cd594236388f (bug 1022612)
    Backed out changeset 9eadc5fee43d (bug 1022612)
    Backed out changeset 5cc8d30ff7c9 (bug 1022612)

--- layout/xul/nsBoxFrame.cpp
+++ layout/xul/nsBoxFrame.cpp
@@ -1320,8 +1320,8 @@ nsBoxFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
     // in calculating glass margins on Windows.
     const nsStyleDisplay* styles = StyleDisplay();
     if (styles && styles->mAppearance == NS_THEME_WIN_EXCLUDE_GLASS) {
-      aBuilder->AddWindowOpaqueRegion(
-          nsRect(aBuilder->ToReferenceFrame(this), GetSize()));
+      nsRect rect = nsRect(aBuilder->ToReferenceFrame(this), GetSize());
+      aBuilder->AddExcludedGlassRegion(rect);
     }
   }
 
@@ -2012,9 +2012,6 @@ public:
   virtual void HitTest(nsDisplayListBuilder* aBuilder, const nsRect& aRect,
                        HitTestState* aState,
                        nsTArray<nsIFrame*> *aOutFrames) MOZ_OVERRIDE;
-  virtual bool ShouldFlattenAway(nsDisplayListBuilder* aBuilder) MOZ_OVERRIDE {
-    return false;
-  }
   NS_DISPLAY_DECL_NAME("XULEventRedirector", TYPE_XUL_EVENT_REDIRECTOR)
 private:
   nsIFrame* mTargetFrame;
