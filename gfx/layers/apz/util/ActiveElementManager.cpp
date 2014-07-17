80ae1b2f26f433849459f7486401a64f140acc68
Kartikaya Gupta <kgupta@mozilla.com>
2014-07-16 23:06:36 -0400

    Bug 995475 - Clear the active element on touch end, if the touch wasn't a click. r=botond

--- gfx/layers/apz/util/ActiveElementManager.cpp
+++ gfx/layers/apz/util/ActiveElementManager.cpp
@@ -118,6 +118,11 @@ ActiveElementManager::HandleTouchEnd(bool aWasClick)
   CancelTask();
   if (aWasClick) {
     SetActive(mTarget);
+  } else {
+    // We might reach here if mCanBePan was false on touch-start and
+    // so we set the element active right away. Now it turns out the
+    // action was not a click so we need to reset the active element.
+    ResetActive();
   }
 
   ResetTouchBlockState();
