7eeefeded2e21cf3a6e02fdcac244ca620ea265f
Nicholas Nethercote <nnethercote@mozilla.com>
2014-07-17 21:14:20 -0700

    Bug 1039965 - Avoid slop in JS arrays. r=bhackett,terrence.
    This patch changes JS array resizing to always allocate power-of-two sized slot
    requests. Previously it would mostly make slight-more-than-power-of-two sized
    requests, which cause lots of slop.
    
    Also, shrinkElements() now only does a reallocation if it would result in going
    down a size class. E.g. if you pop all the elements from a 1000-element array,
    it would realloc 999, then 998, then 997, all the way down the minimum size.
    Now it does 512, then 256, down to the minimum size (which is 8).
    
    I confirmed with DMD that the element allocations now have zero slop. This
    reduces peak RSS loading a couple of large PDF files (four times each) with
    pdf.js by 10s of MiBs.

--- js/src/jsobj.h
+++ js/src/jsobj.h
@@ -602,6 +602,7 @@ class JSObject : public js::ObjectImpl
         return true;
     }
 
+    static uint32_t goodAllocated(uint32_t count);
     bool growElements(js::ThreadSafeContext *cx, uint32_t newcap);
     void shrinkElements(js::ThreadSafeContext *cx, uint32_t cap);
     void setDynamicElements(js::ObjectElements *header) {
