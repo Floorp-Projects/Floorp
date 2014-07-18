0f6c1707f17c298d9f1a6ae38368611d133f12a0
Jeff Muizelaar <jmuizelaar@mozilla.com>
2014-07-18 14:25:34 -0400

    Bug 1023473. Pad out tile contents by one pixel. r=BenWa
    This fixes reading unitialized data during texture filtering.
    It's complicated because we can have regions of initialized data
    and padding out a region is much harder than a rectangle.
    
    We currently only take this path for tiled things because of
    the artifacts that show up during the sampling that helps
    in overscroll. It's also not a great long term solution because
    it only works for software backends.

--- gfx/tests/gtest/TestRegion.cpp
+++ gfx/tests/gtest/TestRegion.cpp
@@ -3,8 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <algorithm>
+
 #include "gtest/gtest.h"
 #include "nsRegion.h"
+
+using namespace std;
 
 class TestLargestRegion {
 public:
@@ -172,6 +176,7 @@ TEST(Gfx, RegionScaleToInside) {
 
 }
 
+
 TEST(Gfx, RegionSimplify) {
   { // ensure simplify works on a single rect
     nsRegion r(nsRect(0,100,200,100));
@@ -280,6 +285,183 @@ TEST(Gfx, RegionSimplify) {
     // just make sure this doesn't crash.
     nsRegion r;
     r.SimplifyOutwardByArea(100);
+  }
+}
+
+#define DILATE_VALUE 0x88
+#define REGION_VALUE 0xff
+
+struct RegionBitmap {
+  RegionBitmap(unsigned char *bitmap, int width, int height) : bitmap(bitmap), width(width), height(height) {}
+
+  void clear() {
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+	bitmap[x + y * width] = 0;
+      }
+    }
+  }
+
+  void set(nsRegion &region) {
+    clear();
+    nsRegionRectIterator iter(region);
+    for (const nsRect* r = iter.Next(); r; r = iter.Next()) {
+      for (int y = r->y; y < r->YMost(); y++) {
+        for (int x = r->x; x < r->XMost(); x++) {
+	  bitmap[x + y * width] = REGION_VALUE;
+	}
+      }
+    }
+  }
+
+  void dilate() {
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+	if (bitmap[x + y * width] == REGION_VALUE) {
+	  for (int yn = max(y - 1, 0); yn <= min(y + 1, height - 1); yn++) {
+	    for (int xn = max(x - 1, 0); xn <= min(x + 1, width - 1); xn++) {
+	      if (bitmap[xn + yn * width] == 0)
+		bitmap[xn + yn * width] = DILATE_VALUE;
+	    }
+	  }
+	}
+      }
+    }
+  }
+  void compare(RegionBitmap &reference) {
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+	EXPECT_EQ(bitmap[x + y * width], reference.bitmap[x + y * width]);
+      }
+    }
+  }
+
+  unsigned char *bitmap;
+  int width;
+  int height;
+};
+
+void VisitEdge(void *closure, VisitSide side, int x1, int y1, int x2, int y2)
+{
+  RegionBitmap *visitor = static_cast<RegionBitmap*>(closure);
+  unsigned char *bitmap = visitor->bitmap;
+  const int width = visitor->width;
+
+  if (side == VisitSide::TOP) {
+    while (x1 != x2) {
+      bitmap[x1 + (y1 - 1) * width] = DILATE_VALUE;
+      x1++;
+    }
+  } else if (side == VisitSide::BOTTOM) {
+    while (x1 != x2) {
+      bitmap[x1 + y1 * width] = DILATE_VALUE;
+      x1++;
+    }
+  } else if (side == VisitSide::LEFT) {
+    while (y1 != y2) {
+      bitmap[x1 - 1 + y1 *width] = DILATE_VALUE;
+      y1++;
+    }
+  } else if (side == VisitSide::RIGHT) {
+    while (y1 != y2) {
+      bitmap[x1 + y1 * width] = DILATE_VALUE;
+      y1++;
+    }
+  }
+}
+
+void TestVisit(nsRegion &r)
+{
+  unsigned char reference[600 * 600];
+  unsigned char result[600 * 600];
+  RegionBitmap ref(reference, 600, 600);
+  RegionBitmap res(result, 600, 600);
+
+  ref.set(r);
+  ref.dilate();
+
+  res.set(r);
+  r.VisitEdges(VisitEdge, &res);
+  res.compare(ref);
+}
+
+TEST(Gfx, RegionVisitEdges) {
+  { // visit edges
+    nsRegion r(nsRect(20, 20, 100, 100));
+    r.Or(r, nsRect(20, 120, 200, 100));
+    TestVisit(r);
+  }
+
+  { // two rects side by side - 1 pixel inbetween
+    nsRegion r(nsRect(20, 20, 100, 100));
+    r.Or(r, nsRect(121, 20, 100, 100));
+    TestVisit(r);
+  }
+
+  { // two rects side by side - 2 pixels inbetween
+    nsRegion r(nsRect(20, 20, 100, 100));
+    r.Or(r, nsRect(122, 20, 100, 100));
+    TestVisit(r);
+  }
+
+  {
+    // only corner of the rects are touching
+    nsRegion r(nsRect(20, 20, 100, 100));
+    r.Or(r, nsRect(120, 120, 100, 100));
+
+    TestVisit(r);
+  }
+
+  {
+    // corners are 1 pixel away
+    nsRegion r(nsRect(20, 20, 100, 100));
+    r.Or(r, nsRect(121, 120, 100, 100));
+
+    TestVisit(r);
+  }
+
+  {
+    // vertically separated
+    nsRegion r(nsRect(20, 20, 100, 100));
+    r.Or(r, nsRect(120, 125, 100, 100));
+
+    TestVisit(r);
+  }
+
+  {
+    // not touching
+    nsRegion r(nsRect(20, 20, 100, 100));
+    r.Or(r, nsRect(130, 120, 100, 100));
+    r.Or(r, nsRect(240, 20, 100, 100));
+
+    TestVisit(r);
+  }
+
+  { // rect with a hole in it
+    nsRegion r(nsRect(20, 20, 100, 100));
+    r.Sub(r, nsRect(40, 40, 10, 10));
+
+    TestVisit(r);
+  }
+  {
+    // left overs
+    nsRegion r(nsRect(20, 20, 10, 10));
+    r.Or(r, nsRect(50, 20, 10, 10));
+    r.Or(r, nsRect(90, 20, 10, 10));
+    r.Or(r, nsRect(24, 30, 10, 10));
+    r.Or(r, nsRect(20, 40, 15, 10));
+    r.Or(r, nsRect(50, 40, 15, 10));
+    r.Or(r, nsRect(90, 40, 15, 10));
+
+    TestVisit(r);
+  }
+
+  {
+    // vertically separated
+    nsRegion r(nsRect(20, 20, 100, 100));
+    r.Or(r, nsRect(120, 125, 100, 100));
+
+    TestVisit(r);
   }
 
 }
