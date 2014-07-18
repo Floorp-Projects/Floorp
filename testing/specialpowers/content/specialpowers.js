b4fda10ae45a698d62256dad832253be3301314d
Bobby Holley <bobbyholley@gmail.com>
2014-07-17 21:34:48 -0700

    Bug 1038844 - Use the opt-out for the SpecialPowers scope. r=mrbkap,r=ted

--- testing/specialpowers/content/specialpowers.js
+++ testing/specialpowers/content/specialpowers.js
@@ -106,15 +106,6 @@ SpecialPowers.prototype.executeAfterFlushingMessageQueue = function(aCallback) {
   sendAsyncMessage("SPPingService", { op: "ping" });
 };
 
-// Expose everything but internal APIs (starting with underscores) to
-// web content.  We cannot use Object.keys to view SpecialPowers.prototype since
-// we are using the functions from SpecialPowersAPI.prototype
-SpecialPowers.prototype.__exposedProps__ = {};
-for (var i in SpecialPowers.prototype) {
-  if (i.charAt(0) != "_")
-    SpecialPowers.prototype.__exposedProps__[i] = "r";
-}
-
 // Attach our API to the window.
 function attachSpecialPowersToWindow(aWindow) {
   try {
