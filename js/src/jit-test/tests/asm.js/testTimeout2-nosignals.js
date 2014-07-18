c213b53fb4afbdf32f8f23b0f3e137dfa12f95ec
Benjamin Bouvier <benj@benj.me>
2014-07-18 12:14:51 +0200

    Bug 986673: Tests; r=luke

---
+++ js/src/jit-test/tests/asm.js/testTimeout2-nosignals.js
@@ -0,0 +1,9 @@
+// |jit-test| exitstatus: 6;
+
+load(libdir + "asm.js");
+
+setJitCompilerOption("signals.enable", 0);
+var g = asmLink(asmCompile(USE_ASM + "/* no-signals */ function g() { while(1) {} } return g"));
+timeout(1);
+g();
+assertEq(true, false);
