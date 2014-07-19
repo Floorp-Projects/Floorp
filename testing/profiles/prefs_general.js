70e405b19877c280cd161fc34e191041a7eb68e9
Georgios Kontaxis <gkontaxis@mozilla.com>
2014-07-18 17:26:24 -0700

    Bug 1029887: Test DOM nodes blocked by NS_ERROR_TRACKING_URI (r=mmc)

--- testing/profiles/prefs_general.js
+++ testing/profiles/prefs_general.js
@@ -85,6 +85,8 @@ user_pref("urlclassifier.updateinterval", 172800);
 user_pref("browser.safebrowsing.gethashURL", "http://%(server)s/safebrowsing-dummy/gethash");
 user_pref("browser.safebrowsing.updateURL", "http://%(server)s/safebrowsing-dummy/update");
 user_pref("browser.safebrowsing.appRepURL", "http://%(server)s/safebrowsing-dummy/update");
+user_pref("browser.trackingprotection.gethashURL", "http://%(server)s/safebrowsing-dummy/gethash");
+user_pref("browser.trackingprotection.updateURL", "http://%(server)s/safebrowsing-dummy/update");
 // Point update checks to the local testing server for fast failures
 user_pref("extensions.update.url", "http://%(server)s/extensions-dummy/updateURL");
 user_pref("extensions.update.background.url", "http://%(server)s/extensions-dummy/updateBackgroundURL");
