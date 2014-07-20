8b0959af84db03c914935c7f6ceb47f81a1cda2e
Ehsan Akhgari <ehsan@mozilla.com>
2014-07-20 19:19:05 -0400

    Backed out changeset 4b792c35d0be (bug 1041325) because of build bustage

--- security/sandbox/chromium/base/win/scoped_handle.h
+++ security/sandbox/chromium/base/win/scoped_handle.h
@@ -18,7 +18,10 @@ namespace win {
 
 // TODO(rvargas): remove this with the rest of the verifier.
 #if defined(COMPILER_MSVC)
-#include <intrin.h>
+// MSDN says to #include <intrin.h>, but that breaks the VS2005 build.
+extern "C" {
+  void* _ReturnAddress();
+}
 #define BASE_WIN_GET_CALLER _ReturnAddress()
 #elif defined(COMPILER_GCC)
 #define BASE_WIN_GET_CALLER __builtin_extract_return_addr(\\
