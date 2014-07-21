8a3e5d92be0faebe29c4f1150f96ef543111b92d
Benjamin Bouvier <benj@benj.me>
2014-07-21 10:06:38 +0200

    Bug 1040823: Simplify tests from bug 986673; r=tests-only

--- js/src/jit-test/tests/asm.js/testTimeout2-nosignals.js
+++ js/src/jit-test/tests/asm.js/testTimeout2-nosignals.js
@@ -3,7 +3,7 @@
 load(libdir + "asm.js");
 
 setJitCompilerOption("signals.enable", 0);
-var g = asmLink(asmCompile(USE_ASM + "/* no-signals */ function g() { while(1) {} } return g"));
+var g = asmLink(asmCompile(USE_ASM + "function g() { while(1) {} } return g"));
 timeout(1);
 g();
 assertEq(true, false);
--- js/src/jit-test/tests/asm.js/testTimeout3-nosignals.js
+++ js/src/jit-test/tests/asm.js/testTimeout3-nosignals.js
@@ -3,7 +3,7 @@
 load(libdir + "asm.js");
 
 setJitCompilerOption("signals.enable", 0);
-var f = asmLink(asmCompile(USE_ASM + "/* no-signals */ function f(i) { i=i|0; if (!i) return; f((i-1)|0); f((i-1)|0); f((i-1)|0); f((i-1)|0); f((i-1)|0); } return f"));
+var f = asmLink(asmCompile(USE_ASM + "function f(i) { i=i|0; if (!i) return; f((i-1)|0); f((i-1)|0); f((i-1)|0); f((i-1)|0); f((i-1)|0); } return f"));
 timeout(1);
 f(100);
 assertEq(true, false);
