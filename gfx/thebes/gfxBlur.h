f7b0452a7596856e8213482bd2980c72e9073e47
Jeff Muizelaar <jmuizelaar@mozilla.com>
2014-07-21 11:06:11 -0400

    Bug 1038218. Convert some of gfxAlphaBlur's nsAutoPtr's to UniquePtrs. r=bas

--- gfx/thebes/gfxBlur.h
+++ gfx/thebes/gfxBlur.h
@@ -11,6 +11,7 @@
 #include "nsAutoPtr.h"
 #include "gfxPoint.h"
 #include "mozilla/RefPtr.h"
+#include "mozilla/UniquePtr.h"
 
 class gfxContext;
 struct gfxRect;
@@ -149,7 +150,7 @@ protected:
      /**
       * The object that actually does the blurring for us.
       */
-    mozilla::gfx::AlphaBoxBlur *mBlur;
+    mozilla::UniquePtr<mozilla::gfx::AlphaBoxBlur> mBlur;
 };
 
 #endif /* GFX_BLUR_H */
