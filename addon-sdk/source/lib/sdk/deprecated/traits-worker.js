54b6ef3afcba57b1b8f5ca93c20359845f891986
Bobby Holley <bobbyholley@gmail.com>
2014-07-22 16:14:26 -0700

    Bug 742444 - Rejigger unsafeWindow definition to not rely on passing waivers across origins. r=gabor

--- addon-sdk/source/lib/sdk/deprecated/traits-worker.js
+++ addon-sdk/source/lib/sdk/deprecated/traits-worker.js
@@ -166,14 +166,20 @@ const WorkerSandbox = EventEmitter.compose({
       // We need "this === window === top" to be true in toplevel scope:
       get window() content,
       get top() top,
-      get parent() parent,
-      // Use the Greasemonkey naming convention to provide access to the
-      // unwrapped window object so the content script can access document
-      // JavaScript values.
-      // NOTE: this functionality is experimental and may change or go away
-      // at any time!
-      get unsafeWindow() window.wrappedJSObject
+      get parent() parent
     });
+    // Use the Greasemonkey naming convention to provide access to the
+    // unwrapped window object so the content script can access document
+    // JavaScript values.
+    // NOTE: this functionality is experimental and may change or go away
+    // at any time!
+    //
+    // Note that because waivers aren't propagated between origins, we
+    // need the unsafeWindow getter to live in the sandbox.
+    var unsafeWindowGetter =
+      new content.Function('return window.wrappedJSObject || window;');
+    Object.defineProperty(content, 'unsafeWindow', {get: unsafeWindowGetter});
+
 
     // Load trusted code that will inject content script API.
     let ContentWorker = load(content, CONTENT_WORKER_URL);
