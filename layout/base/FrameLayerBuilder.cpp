a5622f2cf8cf37c586fc4c8ae6697a7464a1507b
Robert O'Callahan <robert@ocallahan.org>
2014-07-22 23:14:40 +1200

    Bug 1041530. Be more careful about contributing opaque areas to the window region. r=mattwoodrow

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -2635,10 +2635,11 @@ ContainerState::ComputeOpaqueRect(nsDisplayItem* aItem,
         opaqueClipped.Contains(mContainerBounds)) {
       aList->SetIsOpaque();
     }
-    // Add opaque areas to the "exclude glass" region. Only do this for
-    // ThebesLayers which are direct children of the root layer; this means
-    // they can't have transforms or opacity wrapping them.
-    if (!mContainerLayer->GetParent()) {
+    // Add opaque areas to the "exclude glass" region. Only do this when our
+    // container layer is going to be the rootmost layer, otherwise transforms
+    // etc will mess us up (and opaque contributions from other containers are
+    // not needed).
+    if (!nsLayoutUtils::GetCrossDocParentFrame(mContainerFrame)) {
       mBuilder->AddWindowOpaqueRegion(opaqueClipped);
     }
     opaquePixels = ScaleRegionToInsidePixels(opaqueClipped, snapOpaque);
