1d2255f6b730ad833a54b5baca26c21ac352ef80
Mike Conley <mconley@mozilla.com>
2014-07-23 15:20:05 -0700

    Bug 1042398 - Tests. r=bholley, a=sledru

---
+++ js/xpconnect/tests/unit/test_xrayed_iterator.js
@@ -0,0 +1,36 @@
+const Cu = Components.utils;
+function run_test() {
+
+  var toEval = [
+    "var customIterator = {",
+    "  _array: [6, 7, 8, 9],",
+    "  __iterator__: function() {",
+    "    for (var i = 0; i < this._array.length; ++i)",
+    "      yield this._array[i];",
+    "  }",
+    "}"
+  ].join('\n');
+
+  function checkIterator(iterator) {
+    var control = [6, 7, 8, 9];
+    var i = 0;
+    for (var item in iterator) {
+      do_check_eq(item, control[i]);
+      ++i;
+    }
+  }
+
+  // First, try in our own scope.
+  eval(toEval);
+  checkIterator(customIterator);
+
+  // Next, try a vanilla CCW.
+  var sbChrome = Cu.Sandbox(this);
+  Cu.evalInSandbox(toEval, sbChrome, '1.7');
+  checkIterator(sbChrome.customIterator);
+
+  // Finally, try an Xray waiver.
+  var sbContent = Cu.Sandbox('http://www.example.com');
+  Cu.evalInSandbox(toEval, sbContent, '1.7');
+  checkIterator(Cu.waiveXrays(sbContent.customIterator));
+}
