de52287c94be465488e9582d71e983910cddf0df
Mike Hommey <mh+mozilla@glandium.org>
2014-07-25 07:14:40 +0900

    Bug 1043344 - Move libraries and programs build to the compile tier. r=gps

--- python/mozbuild/mozbuild/frontend/reader.py
+++ python/mozbuild/mozbuild/frontend/reader.py
@@ -103,6 +103,10 @@ def is_read_allowed(path, config):
     return False
 
 
+class PathWithTrigger(unicode):
+    __slots__ = ('trigger',)
+
+
 class SandboxCalledError(SandboxError):
     """Represents an error resulting from calling the error() function."""
 
@@ -280,7 +284,8 @@ class MozbuildSandbox(Sandbox):
         data.is_library = True
         return data
 
-    def _add_tier_directory(self, tier, reldir, static=False, external=False):
+    def _add_tier_directory(self, tier, reldir, static=False, external=False,
+            trigger=None):
         """Register a tier directory with the build."""
         if isinstance(reldir, text_type):
             reldir = [reldir]
@@ -302,6 +307,8 @@ class MozbuildSandbox(Sandbox):
                 raise Exception('Directory has already been registered with '
                     'tier: %s' % path)
 
+            path = PathWithTrigger(path)
+            path.trigger = trigger
             self['TIERS'][tier][key].append(path)
 
     def _export(self, varname):
