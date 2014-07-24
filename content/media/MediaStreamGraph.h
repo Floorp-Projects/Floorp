d72f9c686ee72feef5656a72b8a11f2826b56ea3
Karl Tomlinson <karlt+@karlt.net>
2014-07-25 08:23:40 +1200

    b=1022945 make some SourceMediaStream methods,classes protected r=roc
    including ResampleAudioToGraphSampleRate() which assumes a graph.
    
    --HG--
    extra : rebase_source : d6884dc8d36e4985e14558cc78d69e99ffeba921

--- content/media/MediaStreamGraph.h
+++ content/media/MediaStreamGraph.h
@@ -727,8 +727,6 @@ public:
   void AddTrack(TrackID aID, TrackRate aRate, TrackTicks aStart,
                 MediaSegment* aSegment);
 
-  struct TrackData;
-  void ResampleAudioToGraphSampleRate(TrackData* aTrackData, MediaSegment* aSegment);
   /**
    * Append media data to a track. Ownership of aSegment remains with the caller,
    * but aSegment is emptied.
@@ -795,10 +793,13 @@ public:
    */
   TrackTicks GetBufferedTicks(TrackID aID);
 
+  void RegisterForAudioMixing();
+
   // XXX need a Reset API
 
   friend class MediaStreamGraphImpl;
 
+protected:
   struct ThreadAndRunnable {
     void Init(nsIEventTarget* aTarget, nsIRunnable* aRunnable)
     {
@@ -840,10 +841,10 @@ public:
     bool mHaveEnough;
   };
 
-  void RegisterForAudioMixing();
   bool NeedsMixing();
 
-protected:
+  void ResampleAudioToGraphSampleRate(TrackData* aTrackData, MediaSegment* aSegment);
+
   TrackData* FindDataForTrack(TrackID aID)
   {
     for (uint32_t i = 0; i < mUpdateTracks.Length(); ++i) {
