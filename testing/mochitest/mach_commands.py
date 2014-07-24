75a8e87b196620bed48185b96c2d6a30b00b0773
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-07-24 06:23:00 +0200

    Bug 1042979 - Clean up unused imports, variables, and dead code in mochitest. r=ahal

--- testing/mochitest/mach_commands.py
+++ testing/mochitest/mach_commands.py
@@ -7,7 +7,6 @@ from __future__ import unicode_literals
 import logging
 import mozpack.path
 import os
-import platform
 import sys
 import warnings
 import which
@@ -141,14 +140,11 @@ class MochitestRunner(MozbuildObject):
         parser = B2GOptions()
         options = parser.parse_args([])[0]
 
-        test_path_dir = False;
         if test_path:
             test_root_file = mozpack.path.join(self.mochitest_dir, 'tests', test_path)
             if not os.path.exists(test_root_file):
                 print('Specified test path does not exist: %s' % test_root_file)
                 return 1
-            if os.path.isdir(test_root_file):
-                test_path_dir = True;
             options.testPath = test_path
 
         for k, v in kwargs.iteritems():
