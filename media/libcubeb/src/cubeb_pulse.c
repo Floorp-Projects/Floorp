f25d9ecb3a20e0688d374bb0d32a114acf777cba
Paul Adenot <paul@paul.cx>
2014-07-24 17:05:23 +0200

    Bug 1027713 - Part 3 - Add a cubeb API to signal that the output device changed.  r=kinetik
    The reentrant mutex is needed so that users can call back into cubeb's API from
    the callback.

--- media/libcubeb/src/cubeb_pulse.c
+++ media/libcubeb/src/cubeb_pulse.c
@@ -750,5 +750,6 @@ static struct cubeb_ops const pulse_ops = {
   .stream_set_volume = pulse_stream_set_volume,
   .stream_set_panning = pulse_stream_set_panning,
   .stream_get_current_output_device = NULL,
-  .stream_output_device_destroy = NULL
+  .stream_output_device_destroy = NULL,
+  .stream_register_device_changed_callback = NULL
 };
