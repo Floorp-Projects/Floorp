ec3ed3fca2e39a5c17240030a768c9af194f155e
Tom Schuster <evilpies@gmail.com>
2014-07-25 14:26:40 +0200

    Bug 1043588 - Remove some unused functions from OldDebugAPI. r=jimb

--- js/public/OldDebugAPI.h
+++ js/public/OldDebugAPI.h
@@ -28,12 +28,6 @@ class FrameIter;
 class ScriptSource;
 }
 
-// Raw JSScript* because this needs to be callable from a signal handler.
-extern JS_PUBLIC_API(unsigned)
-JS_PCToLineNumber(JSContext *cx, JSScript *script, jsbytecode *pc);
-
-extern JS_PUBLIC_API(const char *)
-JS_GetScriptFilename(JSScript *script);
 
 namespace JS {
 
@@ -58,9 +52,6 @@ typedef enum JSTrapStatus {
     JSTRAP_THROW,
     JSTRAP_LIMIT
 } JSTrapStatus;
-
-extern JS_PUBLIC_API(JSCompartment *)
-JS_EnterCompartmentOfScript(JSContext *cx, JSScript *target);
 
 extern JS_PUBLIC_API(JSString *)
 JS_DecompileScript(JSContext *cx, JS::HandleScript script, const char *name, unsigned indent);
@@ -114,64 +105,21 @@ JS_SetSingleStepMode(JSContext *cx, JS::HandleScript script, bool singleStep);
 
 /************************************************************************/
 
+// Raw JSScript* because this needs to be callable from a signal handler.
+extern JS_PUBLIC_API(unsigned)
+JS_PCToLineNumber(JSContext *cx, JSScript *script, jsbytecode *pc);
+
 extern JS_PUBLIC_API(jsbytecode *)
 JS_LineNumberToPC(JSContext *cx, JSScript *script, unsigned lineno);
-
-extern JS_PUBLIC_API(jsbytecode *)
-JS_EndPC(JSContext *cx, JSScript *script);
-
-extern JS_PUBLIC_API(bool)
-JS_GetLinePCs(JSContext *cx, JSScript *script,
-              unsigned startLine, unsigned maxLines,
-              unsigned* count, unsigned** lines, jsbytecode*** pcs);
-
-extern JS_PUBLIC_API(unsigned)
-JS_GetFunctionArgumentCount(JSContext *cx, JSFunction *fun);
-
-extern JS_PUBLIC_API(bool)
-JS_FunctionHasLocalNames(JSContext *cx, JSFunction *fun);
-
-/*
- * N.B. The mark is in the context temp pool and thus the caller must take care
- * to call JS_ReleaseFunctionLocalNameArray in a LIFO manner (wrt to any other
- * call that may use the temp pool.
- */
-extern JS_PUBLIC_API(uintptr_t *)
-JS_GetFunctionLocalNameArray(JSContext *cx, JSFunction *fun, void **markp);
-
-extern JS_PUBLIC_API(JSAtom *)
-JS_LocalNameToAtom(uintptr_t w);
-
-extern JS_PUBLIC_API(JSString *)
-JS_AtomKey(JSAtom *atom);
-
-extern JS_PUBLIC_API(void)
-JS_ReleaseFunctionLocalNameArray(JSContext *cx, void *mark);
 
 extern JS_PUBLIC_API(JSScript *)
 JS_GetFunctionScript(JSContext *cx, JS::HandleFunction fun);
 
-extern JS_PUBLIC_API(JSNative)
-JS_GetFunctionNative(JSContext *cx, JSFunction *fun);
-
-JS_PUBLIC_API(JSFunction *)
-JS_GetScriptFunction(JSContext *cx, JSScript *script);
-
-extern JS_PUBLIC_API(JSObject *)
-JS_GetParentOrScopeChain(JSContext *cx, JSObject *obj);
 
 /************************************************************************/
 
-/*
- * This is almost JS_GetClass(obj)->name except that certain debug-only
- * proxies are made transparent. In particular, this function turns the class
- * of any scope (returned via JS_GetFrameScopeChain or JS_GetFrameCalleeObject)
- * from "Proxy" to "Call", "Block", "With" etc.
- */
 extern JS_PUBLIC_API(const char *)
-JS_GetDebugClassName(JSObject *obj);
-
-/************************************************************************/
+JS_GetScriptFilename(JSScript *script);
 
 extern JS_PUBLIC_API(const jschar *)
 JS_GetScriptSourceMap(JSContext *cx, JSScript *script);
@@ -181,13 +129,6 @@ JS_GetScriptBaseLineNumber(JSContext *cx, JSScript *script);
 
 extern JS_PUBLIC_API(unsigned)
 JS_GetScriptLineExtent(JSContext *cx, JSScript *script);
-
-extern JS_PUBLIC_API(JSVersion)
-JS_GetScriptVersion(JSContext *cx, JSScript *script);
-
-extern JS_PUBLIC_API(bool)
-JS_GetScriptIsSelfHosted(JSScript *script);
-
 
 /************************************************************************/
 
