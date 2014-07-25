4d3b523e11bc311e21d160a29d676203a2e2de02
Birunthan Mohanathas <birunthan@mohanathas.com>
2014-07-25 14:15:35 -0700

    Bug 946065 - Part 1: Move content/canvas/ to dom/ and flatten subdirectories. r=ehsan

--- dom/canvas/test/webgl-conformance/resources/cors-util.js
+++ dom/canvas/test/webgl-conformance/resources/cors-util.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 function getExampleDotComURL(conformanceDirRelativePath) {
-  var prefix = 'http://example.com/tests/content/canvas/test/webgl-conformance/';
+  var prefix = 'http://example.com/tests/dom/canvas/test/webgl-conformance/';
   return prefix + conformanceDirRelativePath;
 }
 
