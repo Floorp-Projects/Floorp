394b0b1cf24efb2f5a1cfd65b79b5d081ff27ed1
Ralph Giles <giles@mozilla.com>
2014-07-24 17:08:00 -0700

    Bug 1043695 - Add a frame reorder queue. r=cpearce
    This fixes the frame drops on test.mp4. For now it just
    assumes a reorder depth of 3, but we need to plumb the
    actual value through the demuxer.
    
    Based on now-removed code from Edwin's FFmpegH264Decoder.

--- content/media/fmp4/apple/AppleVTDecoder.cpp
+++ content/media/fmp4/apple/AppleVTDecoder.cpp
@@ -110,6 +110,7 @@ AppleVTDecoder::Input(mp4_demuxer::MP4Sample* aSample)
 nsresult
 AppleVTDecoder::Flush()
 {
+  mReorderQueue.Clear();
   return Drain();
 }
 
@@ -121,7 +122,7 @@ AppleVTDecoder::Drain()
     LOG("Error %d draining frames", rv);
     return NS_ERROR_FAILURE;
   }
-  return NS_OK;
+  return DrainReorderedFrames();
 }
 
 //
@@ -187,6 +188,15 @@ PlatformCallback(void* decompressionOutputRefCon,
   // Forward the data back to an object method which can access
   // the correct MP4Reader callback.
   decoder->OutputFrame(image, frameRef);
+}
+
+nsresult
+AppleVTDecoder::DrainReorderedFrames()
+{
+  while (!mReorderQueue.IsEmpty()) {
+    mCallback->Output(mReorderQueue.Pop());
+  }
+  return NS_OK;
 }
 
 // Copy and return a decoded frame.
@@ -270,7 +280,14 @@ AppleVTDecoder::OutputFrame(CVPixelBufferRef aImage,
   // Unlock the returned image data.
   CVPixelBufferUnlockBaseAddress(aImage, kCVPixelBufferLock_ReadOnly);
 
-  mCallback->Output(data.forget());
+  // Frames come out in DTS order but we need to output them
+  // in composition order.
+  mReorderQueue.Push(data.forget());
+  if (mReorderQueue.Length() > 2) {
+    VideoData* readyData = mReorderQueue.Pop();
+    mCallback->Output(readyData);
+  }
+
   return NS_OK;
 }
 
