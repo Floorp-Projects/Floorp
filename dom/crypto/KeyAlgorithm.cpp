f8f8dd34df078b62b8c21a205fe87d554fb15db5
Tim Taubert <ttaubert@mozilla.com>
2014-07-26 08:00:24 +0200

    Bug 1034855 - Introduce EcKeyAlgorithm r=rbarnes,smaug

--- dom/crypto/KeyAlgorithm.cpp
+++ dom/crypto/KeyAlgorithm.cpp
@@ -7,6 +7,7 @@
 #include "mozilla/dom/KeyAlgorithm.h"
 #include "mozilla/dom/WebCryptoCommon.h"
 #include "mozilla/dom/AesKeyAlgorithm.h"
+#include "mozilla/dom/EcKeyAlgorithm.h"
 #include "mozilla/dom/HmacKeyAlgorithm.h"
 #include "mozilla/dom/RsaKeyAlgorithm.h"
 #include "mozilla/dom/RsaHashedKeyAlgorithm.h"
@@ -87,6 +88,10 @@ KeyAlgorithm::Create(nsIGlobalObject* aGlobal, JSStructuredCloneReader* aReader)
     }
     case SCTAG_AESKEYALG: {
       algorithm = AesKeyAlgorithm::Create(aGlobal, aReader);
+      break;
+    }
+    case SCTAG_ECKEYALG: {
+      algorithm = EcKeyAlgorithm::Create(aGlobal, aReader);
       break;
     }
     case SCTAG_HMACKEYALG: {
