57474ac5e9d4fa2d9cca250c02ba3397423ffcb3
Chris Pearce <cpearce@mozilla.com>
2014-07-28 11:20:34 +1200

    Bug 1042373 - Implement GMPDecryptor interface for use in GMPs. r=jesup

---
+++ content/media/gmp/GMPDecryptorChild.h
@@ -0,0 +1,127 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef GMPDecryptorChild_h_
+#define GMPDecryptorChild_h_
+
+#include "mozilla/gmp/PGMPDecryptorChild.h"
+#include "gmp-decryption.h"
+#include "mozilla/gmp/GMPTypes.h"
+#include "GMPEncryptedBufferDataImpl.h"
+
+namespace mozilla {
+namespace gmp {
+
+class GMPChild;
+
+class GMPDecryptorChild : public GMPDecryptorCallback
+                        , public GMPDecryptorHost
+                        , public PGMPDecryptorChild
+{
+public:
+  GMPDecryptorChild(GMPChild* aPlugin);
+  ~GMPDecryptorChild();
+
+  void Init(GMPDecryptor* aSession);
+
+  // GMPDecryptorCallback
+  virtual void ResolveNewSessionPromise(uint32_t aPromiseId,
+                                        const char* aSessionId,
+                                        uint32_t aSessionIdLength) MOZ_OVERRIDE;
+  virtual void ResolvePromise(uint32_t aPromiseId) MOZ_OVERRIDE;
+
+  virtual void RejectPromise(uint32_t aPromiseId,
+                             GMPDOMException aException,
+                             const char* aMessage,
+                             uint32_t aMessageLength) MOZ_OVERRIDE;
+
+  virtual void SessionMessage(const char* aSessionId,
+                              uint32_t aSessionIdLength,
+                              const uint8_t* aMessage,
+                              uint32_t aMessageLength,
+                              const char* aDestinationURL,
+                              uint32_t aDestinationURLLength) MOZ_OVERRIDE;
+
+  virtual void ExpirationChange(const char* aSessionId,
+                                 uint32_t aSessionIdLength,
+                                 GMPTimestamp aExpiryTime) MOZ_OVERRIDE;
+
+   virtual void SessionClosed(const char* aSessionId,
+                             uint32_t aSessionIdLength) MOZ_OVERRIDE;
+
+  virtual void SessionError(const char* aSessionId,
+                            uint32_t aSessionIdLength,
+                            GMPDOMException aException,
+                            uint32_t aSystemCode,
+                            const char* aMessage,
+                            uint32_t aMessageLength) MOZ_OVERRIDE;
+
+  virtual void KeyIdUsable(const char* aSessionId,
+                           uint32_t aSessionIdLength,
+                           const uint8_t* aKeyId,
+                           uint32_t aKeyIdLength) MOZ_OVERRIDE;
+
+  virtual void KeyIdNotUsable(const char* aSessionId,
+                              uint32_t aSessionIdLength,
+                              const uint8_t* aKeyId,
+                              uint32_t aKeyIdLength) MOZ_OVERRIDE;
+
+  virtual void SetCapabilities(uint64_t aCaps) MOZ_OVERRIDE;
+
+  virtual void Decrypted(GMPBuffer* aBuffer, GMPErr aResult) MOZ_OVERRIDE;
+
+  // GMPDecryptorHost
+  virtual void GetNodeId(const char** aOutNodeId,
+                         uint32_t* aOutNodeIdLength) MOZ_OVERRIDE;
+
+  virtual void GetSandboxVoucher(const uint8_t** aVoucher,
+                                 uint8_t* aVoucherLength) MOZ_OVERRIDE;
+
+  virtual void GetPluginVoucher(const uint8_t** aVoucher,
+                                uint8_t* aVoucherLength) MOZ_OVERRIDE;
+private:
+
+  // GMPDecryptorChild
+  virtual bool RecvInit() MOZ_OVERRIDE;
+
+  virtual bool RecvCreateSession(const uint32_t& aPromiseId,
+                                 const nsCString& aInitDataType,
+                                 const nsTArray<uint8_t>& aInitData,
+                                 const GMPSessionType& aSessionType) MOZ_OVERRIDE;
+
+  virtual bool RecvLoadSession(const uint32_t& aPromiseId,
+                               const nsCString& aSessionId) MOZ_OVERRIDE;
+
+  virtual bool RecvUpdateSession(const uint32_t& aPromiseId,
+                                 const nsCString& aSessionId,
+                                 const nsTArray<uint8_t>& aResponse) MOZ_OVERRIDE;
+
+  virtual bool RecvCloseSession(const uint32_t& aPromiseId,
+                                const nsCString& aSessionId) MOZ_OVERRIDE;
+
+  virtual bool RecvRemoveSession(const uint32_t& aPromiseId,
+                                 const nsCString& aSessionId) MOZ_OVERRIDE;
+
+  virtual bool RecvDecrypt(const uint32_t& aId,
+                           const nsTArray<uint8_t>& aBuffer,
+                           const GMPDecryptionData& aMetadata);
+
+  // Resolve/reject promise on completion.
+  virtual bool RecvSetServerCertificate(const uint32_t& aPromiseId,
+                                        const nsTArray<uint8_t>& aServerCert) MOZ_OVERRIDE;
+
+  virtual bool RecvDecryptingComplete() MOZ_OVERRIDE;
+
+  GMPChild* mPlugin;
+
+  // GMP's GMPDecryptor implementation.
+  // Only call into this on the (GMP process) main thread.
+  GMPDecryptor* mSession;
+};
+
+} // namespace gmp
+} // namespace mozilla
+
+#endif // GMPDecryptorChild_h_
---
+++ content/media/gmp/GMPDecryptorParent.h
@@ -0,0 +1,113 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef GMPDecryptorParent_h_
+#define GMPDecryptorParent_h_
+
+#include "mozilla/gmp/PGMPDecryptorParent.h"
+#include "mozilla/RefPtr.h"
+#include "gmp-decryption.h"
+#include "GMPDecryptorProxy.h"
+namespace mp4_demuxer {
+class CryptoSample;
+}
+
+namespace mozilla {
+namespace gmp {
+
+class GMPParent;
+
+class GMPDecryptorParent MOZ_FINAL : public GMPDecryptorProxy
+                                   , public PGMPDecryptorParent
+{
+public:
+  NS_INLINE_DECL_REFCOUNTING(GMPDecryptorParent)
+
+  GMPDecryptorParent(GMPParent *aPlugin);
+
+  // GMPDecryptorProxy
+  virtual nsresult Init(GMPDecryptorProxyCallback* aCallback) MOZ_OVERRIDE;
+
+  virtual void CreateSession(uint32_t aPromiseId,
+                             const nsCString& aInitDataType,
+                             const nsTArray<uint8_t>& aInitData,
+                             GMPSessionType aSessionType) MOZ_OVERRIDE;
+
+  virtual void LoadSession(uint32_t aPromiseId,
+                           const nsCString& aSessionId) MOZ_OVERRIDE;
+
+  virtual void UpdateSession(uint32_t aPromiseId,
+                             const nsCString& aSessionId,
+                             const nsTArray<uint8_t>& aResponse) MOZ_OVERRIDE;
+
+  virtual void CloseSession(uint32_t aPromiseId,
+                            const nsCString& aSessionId) MOZ_OVERRIDE;
+
+  virtual void RemoveSession(uint32_t aPromiseId,
+                             const nsCString& aSessionId) MOZ_OVERRIDE;
+
+  virtual void SetServerCertificate(uint32_t aPromiseId,
+                                    const nsTArray<uint8_t>& aServerCert) MOZ_OVERRIDE;
+
+  virtual void Decrypt(uint32_t aId,
+                       const mp4_demuxer::CryptoSample& aCrypto,
+                       const nsTArray<uint8_t>& aBuffer) MOZ_OVERRIDE;
+
+  virtual void Close() MOZ_OVERRIDE;
+
+  void Shutdown();
+
+private:
+  ~GMPDecryptorParent();
+
+  // PGMPDecryptorParent
+
+  virtual bool RecvResolveNewSessionPromise(const uint32_t& aPromiseId,
+                                            const nsCString& aSessionId) MOZ_OVERRIDE;
+
+  virtual bool RecvResolvePromise(const uint32_t& aPromiseId) MOZ_OVERRIDE;
+
+  virtual bool RecvRejectPromise(const uint32_t& aPromiseId,
+                                 const GMPDOMException& aException,
+                                 const nsCString& aMessage) MOZ_OVERRIDE;
+
+  virtual bool RecvSessionMessage(const nsCString& aSessionId,
+                                  const nsTArray<uint8_t>& aMessage,
+                                  const nsCString& aDestinationURL) MOZ_OVERRIDE;
+
+  virtual bool RecvExpirationChange(const nsCString& aSessionId,
+                                    const double& aExpiryTime) MOZ_OVERRIDE;
+
+  virtual bool RecvSessionClosed(const nsCString& aSessionId) MOZ_OVERRIDE;
+
+  virtual bool RecvSessionError(const nsCString& aSessionId,
+                                const GMPDOMException& aException,
+                                const uint32_t& aSystemCode,
+                                const nsCString& aMessage) MOZ_OVERRIDE;
+
+  virtual bool RecvKeyIdUsable(const nsCString& aSessionId,
+                                const nsTArray<uint8_t>& aKeyId) MOZ_OVERRIDE;
+
+  virtual bool RecvKeyIdNotUsable(const nsCString& aSessionId,
+                                  const nsTArray<uint8_t>& aKeyId) MOZ_OVERRIDE;
+
+  virtual bool RecvDecrypted(const uint32_t& aId,
+                             const GMPErr& aErr,
+                             const nsTArray<uint8_t>& aBuffer) MOZ_OVERRIDE;
+
+  virtual bool RecvSetCaps(const uint64_t& aCaps) MOZ_OVERRIDE;
+
+  virtual void ActorDestroy(ActorDestroyReason aWhy) MOZ_OVERRIDE;
+  virtual bool Recv__delete__() MOZ_OVERRIDE;
+
+  bool mIsOpen;
+  nsRefPtr<GMPParent> mPlugin;
+  GMPDecryptorProxyCallback* mCallback;
+};
+
+} // namespace gmp
+} // namespace mozilla
+
+#endif // GMPDecryptorChild_h_
