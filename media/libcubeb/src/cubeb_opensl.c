656b2eb4ddb1af7d81baa2a40dda99cf86d7bee1
Gian-Carlo Pascutto <gpascutto@mozilla.com>
2014-07-28 11:29:31 +0200

    Bug 1015932 - Move OpenSLES Realize into the engine broker. r=padenot

--- media/libcubeb/src/cubeb_opensl.c
+++ media/libcubeb/src/cubeb_opensl.c
@@ -271,7 +271,7 @@ opensl_init(cubeb ** context, char const * context_name)
     return CUBEB_ERROR;
   }
 
-  res = (*ctx->engObj)->Realize(ctx->engObj, SL_BOOLEAN_FALSE);
+  res = cubeb_realize_sles_engine(ctx->engObj);
   if (res != SL_RESULT_SUCCESS) {
     opensl_destroy(ctx);
     return CUBEB_ERROR;
