6b2ed3392df30f2dece1b1193fcfa1225fd8f786
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-07-28 08:21:16 +0200

    Backed out changeset 689b75c98f2f (bug 1015932)

--- content/media/systemservices/OpenSLESProvider.h
+++ content/media/systemservices/OpenSLESProvider.h
@@ -13,14 +13,11 @@
 extern "C" {
 #endif
 extern MOZ_EXPORT
-SLresult mozilla_get_sles_engine(SLObjectItf * aObjectm,
-                                 SLuint32 aOptionCount,
-                                 const SLEngineOption *aOptions);
+int mozilla_get_sles_engine(SLObjectItf * aObjectm,
+                            SLuint32 aOptionCount,
+                            const SLEngineOption *aOptions);
 extern MOZ_EXPORT
 void mozilla_destroy_sles_engine(SLObjectItf * aObjectm);
-/* Realize is always in synchronous mode. */
-extern MOZ_EXPORT
-SLresult mozilla_realize_sles_engine(SLObjectItf aObjectm);
 #ifdef __cplusplus
 }
 #endif
@@ -34,31 +31,28 @@ namespace mozilla {
 
 class OpenSLESProvider {
 public:
-    static SLresult Get(SLObjectItf * aObjectm,
+    static int Get(SLObjectItf * aObjectm,
                    SLuint32 aOptionCount,
                    const SLEngineOption *aOptions);
     static void Destroy(SLObjectItf * aObjectm);
-    static SLresult Realize(SLObjectItf aObjectm);
 private:
     OpenSLESProvider();
     ~OpenSLESProvider();
     OpenSLESProvider(OpenSLESProvider const&); // NO IMPLEMENTATION
     void operator=(OpenSLESProvider const&);   // NO IMPLEMENTATION
     static OpenSLESProvider& getInstance();
-    SLresult GetEngine(SLObjectItf * aObjectm,
-                       SLuint32 aOptionCount,
-                       const SLEngineOption *aOptions);
-    SLresult ConstructEngine(SLObjectItf * aObjectm,
-                             SLuint32 aOptionCount,
-                             const SLEngineOption *aOptions);
-    SLresult RealizeEngine(SLObjectItf aObjectm);
+    int GetEngine(SLObjectItf * aObjectm,
+                  SLuint32 aOptionCount,
+                  const SLEngineOption *aOptions);
+    int ConstructEngine(SLObjectItf * aObjectm,
+                        SLuint32 aOptionCount,
+                        const SLEngineOption *aOptions);
     void DestroyEngine(SLObjectItf * aObjectm);
 
     // Protect all our internal variables
     mozilla::Mutex mLock;
     SLObjectItf mSLEngine;
     int mSLEngineUsers;
-    bool mIsRealized;
     void *mOpenSLESLib;
 };
 
