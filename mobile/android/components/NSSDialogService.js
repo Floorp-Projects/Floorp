28babd104e5d53636d5d40e8ca876be9b6a63455
Mark Finkle <mfinkle@mozilla.com>
2014-07-29 15:48:40 -0400

    Bug 989577 - Lazy load Prompt.jsm everywhere r=wesj

--- mobile/android/components/NSSDialogService.js
+++ mobile/android/components/NSSDialogService.js
@@ -8,7 +8,9 @@ const Cc = Components.classes;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
-Cu.import("resource://gre/modules/Prompt.jsm");
+
+XPCOMUtils.defineLazyModuleGetter(this, "Prompt",
+                                  "resource://gre/modules/Prompt.jsm");
 
 // -----------------------------------------------------------------------
 // NSS Dialog Service
