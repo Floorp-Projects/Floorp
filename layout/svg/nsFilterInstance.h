3d587cf0f71f85989fbd470e0259ada95a11df50
Max Vujovic <mvujovic@adobe.com>
2014-07-29 09:48:16 -0700

    Bug 948265 - Keep track of the filter region for each FilterPrimitiveDescription. r=mstange

--- layout/svg/nsFilterInstance.h
+++ layout/svg/nsFilterInstance.h
@@ -220,6 +220,11 @@ private:
   void ComputeNeededBoxes();
 
   /**
+   * Returns the output bounds of the final FilterPrimitiveDescription.
+   */
+  nsIntRect OutputFilterSpaceBounds() const;
+
+  /**
    * Compute the scale factors between user space and filter space.
    */
   nsresult ComputeUserSpaceToFilterSpaceScale();
@@ -277,12 +282,6 @@ private:
    */
   gfxMatrix               mFilterSpaceToFrameSpaceInCSSPxTransform;
   gfxMatrix               mFrameSpaceInCSSPxToFilterSpaceTransform;
-
-  /**
-   * The "filter region", in the filtered element's user space.
-   */
-  gfxRect                 mUserSpaceBounds;
-  nsIntRect               mFilterSpaceBounds;
 
   /**
    * The scale factors between user space and filter space.
