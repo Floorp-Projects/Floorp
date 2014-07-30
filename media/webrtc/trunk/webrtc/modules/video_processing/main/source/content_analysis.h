123e51f0798584628ff5a02b10f95d393d5dbb48
Randell Jesup <rjesup@jesup.org>
2014-07-30 17:35:05 -0400

    Bug 1043808: Don't allow content_analysis to half-initialize on small capture sizes r=pkerr  a=sylvestre

--- media/webrtc/trunk/webrtc/modules/video_processing/main/source/content_analysis.h
+++ media/webrtc/trunk/webrtc/modules/video_processing/main/source/content_analysis.h
@@ -66,7 +66,7 @@ class VPMContentAnalysis {
 #endif
 
   const uint8_t* orig_frame_;
-  uint8_t* prev_frame_;
+  scoped_ptr<uint8_t> prev_frame_;
   int width_;
   int height_;
   int skip_num_;
@@ -80,7 +80,7 @@ class VPMContentAnalysis {
   bool first_frame_;
   bool ca_Init_;
 
-  VideoContentMetrics*   content_metrics_;
+  scoped_ptr<VideoContentMetrics> content_metrics_;
 };
 
 }  // namespace webrtc
