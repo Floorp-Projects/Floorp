6bf90d47e1133b1315c9d51231d5d5e5ee27c8a7
Ed Morley <emorley@mozilla.com>
2014-07-30 16:49:43 +0100

    Backed out changeset d50d7e88f35e (bug 1012951) for LSan failures

--- security/sandbox/linux/SandboxFilter.h
+++ security/sandbox/linux/SandboxFilter.h
@@ -11,25 +11,17 @@ struct sock_fprog;
 struct sock_filter;
 
 namespace mozilla {
-
-enum SandboxType {
-  kSandboxContentProcess,
-  kSandboxMediaPlugin
-};
-
-class SandboxFilter {
-  sock_filter *mFilter;
-  sock_fprog *mProg;
-  const sock_fprog **mStored;
-public:
+  class SandboxFilter {
+    sock_filter *mFilter;
+    sock_fprog *mProg;
+    const sock_fprog **mStored;
+  public:
   // RAII: on construction, builds the filter and stores it in the
   // provided variable (with optional logging); on destruction, frees
   // the filter and nulls out the pointer.
-  SandboxFilter(const sock_fprog** aStored, SandboxType aBox,
-                bool aVerbose = false);
-  ~SandboxFilter();
-};
-
-} // namespace mozilla
+    SandboxFilter(const sock_fprog** aStored, bool aVerbose = false);
+    ~SandboxFilter();
+  };
+}
 
 #endif
