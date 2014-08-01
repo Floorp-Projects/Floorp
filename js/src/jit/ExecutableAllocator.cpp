4e7aa64ff7788df8ed5e0bb04ffb8eba9fddea5b
Jan de Mooij <jdemooij@mozilla.com>
2014-08-01 16:39:52 +0200

    Bug 1046585 part 3 - Move ExecutableAllocator into jit/. r=sunfish
    --HG--
    rename : js/src/assembler/jit/ExecutableAllocator.cpp => js/src/jit/ExecutableAllocator.cpp
    rename : js/src/assembler/jit/ExecutableAllocator.h => js/src/jit/ExecutableAllocator.h
    rename : js/src/assembler/jit/ExecutableAllocatorPosix.cpp => js/src/jit/ExecutableAllocatorPosix.cpp
    rename : js/src/assembler/jit/ExecutableAllocatorWin.cpp => js/src/jit/ExecutableAllocatorWin.cpp

--- js/src/jit/ExecutableAllocator.cpp
+++ js/src/jit/ExecutableAllocator.cpp
@@ -25,7 +25,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "assembler/jit/ExecutableAllocator.h"
+#include "jit/ExecutableAllocator.h"
 
 #include "js/MemoryMetrics.h"
 
