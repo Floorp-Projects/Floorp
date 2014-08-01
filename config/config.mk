e1583db863adc428987231413a471287328fa311
Nathan Froyd <froydnj@mozilla.com>
2014-08-01 12:53:05 -0400

    Bug 1042226 - move DEFINES += -DAB_CD=$(AB_CD) pattern into config.mk; r=mshal

--- config/config.mk
+++ config/config.mk
@@ -699,6 +699,8 @@ sysinstall_cmd = install_cmd
 # MOZ_UI_LOCALE directly, but use an intermediate variable that can be
 # overridden by the command line. (Besides, AB_CD is prettier).
 AB_CD = $(MOZ_UI_LOCALE)
+# Many locales directories want this definition.
+DEFINES += -DAB_CD=$(AB_CD)
 
 ifndef L10NBASEDIR
   L10NBASEDIR = $(error L10NBASEDIR not defined by configure)
