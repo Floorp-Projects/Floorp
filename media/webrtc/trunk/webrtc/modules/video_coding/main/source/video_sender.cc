61276f1e0157aaf28ef6e2bc355eec522b176dc3
Randell Jesup <rjesup@jesup.org>
2014-08-01 17:27:50 -0400

    Bug 1047442: reacquire _sendCritSect within webrtc Encoded() callback (upstream patch) r=pkerr a=sylvestre

--- media/webrtc/trunk/webrtc/modules/video_coding/main/source/video_sender.cc
+++ media/webrtc/trunk/webrtc/modules/video_coding/main/source/video_sender.cc
@@ -107,6 +107,7 @@ int32_t VideoSender::InitializeSender() {
   _codecDataBase.ResetSender();
   _encoder = NULL;
   _encodedFrameCallback.SetTransportCallback(NULL);
+  _encodedFrameCallback.SetCritSect(_sendCritSect);
   _mediaOpt.Reset();  // Resetting frame dropper
   return VCM_OK;
 }
