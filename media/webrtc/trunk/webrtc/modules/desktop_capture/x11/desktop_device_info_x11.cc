096f2ca6eba531ba10fd4940f46e7a79ca5f10b5
Roman Skalish <rskalish@lohika.com>
2014-08-01 02:14:00 -0400

    Bug 1041369 - Rescan window list on each getUserMedia window/screensharing request. r=jesup, r=gcp

--- media/webrtc/trunk/webrtc/modules/desktop_capture/x11/desktop_device_info_x11.cc
+++ media/webrtc/trunk/webrtc/modules/desktop_capture/x11/desktop_device_info_x11.cc
@@ -22,19 +22,44 @@ DesktopDeviceInfoX11::DesktopDeviceInfoX11() {
 DesktopDeviceInfoX11::~DesktopDeviceInfoX11() {
 }
 
-int32_t DesktopDeviceInfoX11::Init() {
 #if !defined(MULTI_MONITOR_SCREENSHARE)
+int32_t DesktopDeviceInfoX11::MultiMonitorScreenshare()
+{
   DesktopDisplayDevice *pDesktopDeviceInfo = new DesktopDisplayDevice;
-  if(pDesktopDeviceInfo){
+  if (pDesktopDeviceInfo) {
     pDesktopDeviceInfo->setScreenId(0);
     pDesktopDeviceInfo->setDeviceName("Primary Monitor");
     pDesktopDeviceInfo->setUniqueIdName("\\screen\\monitor#1");
 
     desktop_display_list_[pDesktopDeviceInfo->getScreenId()] = pDesktopDeviceInfo;
   }
+  return 0;
+}
+#endif
+
+int32_t DesktopDeviceInfoX11::Init() {
+#if !defined(MULTI_MONITOR_SCREENSHARE)
+  MultiMonitorScreenshare();
 #endif
 
   initializeWindowList();
+
+  return 0;
+}
+
+int32_t DesktopDeviceInfoX11::Refresh() {
+#if !defined(MULTI_MONITOR_SCREENSHARE)
+  std::map<intptr_t,DesktopDisplayDevice*>::iterator iterDevice;
+  for (iterDevice=desktop_display_list_.begin(); iterDevice!=desktop_display_list_.end(); iterDevice++){
+    DesktopDisplayDevice * pDesktopDisplayDevice = iterDevice->second;
+    delete pDesktopDisplayDevice;
+    iterDevice->second = NULL;
+  }
+  desktop_display_list_.clear();
+  MultiMonitorScreenshare();
+#endif
+
+  RefreshWindowList();
 
   return 0;
 }
