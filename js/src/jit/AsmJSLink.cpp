ebdb7669bd5072ada438a11dc000d8dfa899e67f
Tooru Fujisawa <arai_a@mac.com>
2014-08-01 05:40:00 -0400

    Bug 1034576 - Move math functions to the js namespace. r=bbouvier

--- js/src/jit/AsmJSLink.cpp
+++ js/src/jit/AsmJSLink.cpp
@@ -185,11 +185,11 @@ ValidateMathBuiltinFunction(JSContext *cx, AsmJSModule::Global &global, HandleVa
       case AsmJSMathBuiltin_floor: native = math_floor; break;
       case AsmJSMathBuiltin_exp: native = math_exp; break;
       case AsmJSMathBuiltin_log: native = math_log; break;
-      case AsmJSMathBuiltin_pow: native = js_math_pow; break;
-      case AsmJSMathBuiltin_sqrt: native = js_math_sqrt; break;
-      case AsmJSMathBuiltin_min: native = js_math_min; break;
-      case AsmJSMathBuiltin_max: native = js_math_max; break;
-      case AsmJSMathBuiltin_abs: native = js_math_abs; break;
+      case AsmJSMathBuiltin_pow: native = math_pow; break;
+      case AsmJSMathBuiltin_sqrt: native = math_sqrt; break;
+      case AsmJSMathBuiltin_min: native = math_min; break;
+      case AsmJSMathBuiltin_max: native = math_max; break;
+      case AsmJSMathBuiltin_abs: native = math_abs; break;
       case AsmJSMathBuiltin_atan2: native = math_atan2; break;
       case AsmJSMathBuiltin_imul: native = math_imul; break;
       case AsmJSMathBuiltin_fround: native = math_fround; break;
