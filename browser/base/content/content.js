d39fe7ffd8da24ff11c958066f8abb12758d4568
Vlad Filippov <vlad.filippov@gmail.com>
2014-08-01 01:42:00 -0400

    Bug 1022064 - Add WebChannel Communication API and FxAccountsOAuthClient API to facilitate Firefox Accounts OAuth authentication. r=MattN, sr=gavin

--- browser/base/content/content.js
+++ browser/base/content/content.js
@@ -216,6 +216,33 @@ let AboutHomeListener = {
 AboutHomeListener.init(this);
 
 
+// An event listener for custom "WebChannelMessageToChrome" events on pages
+addEventListener("WebChannelMessageToChrome", function (e) {
+  // if target is window then we want the document principal, otherwise fallback to target itself.
+  let principal = e.target.nodePrincipal ? e.target.nodePrincipal : e.target.document.nodePrincipal;
+
+  if (e.detail) {
+    sendAsyncMessage("WebChannelMessageToChrome", e.detail, null, principal);
+  } else  {
+    Cu.reportError("WebChannel message failed. No message detail.");
+  }
+}, true, true);
+
+// Add message listener for "WebChannelMessageToContent" messages from chrome scripts
+addMessageListener("WebChannelMessageToContent", function (e) {
+  if (e.data) {
+    content.dispatchEvent(new content.CustomEvent("WebChannelMessageToContent", {
+      detail: Cu.cloneInto({
+        id: e.data.id,
+        message: e.data.message,
+      }, content),
+    }));
+  } else {
+    Cu.reportError("WebChannel message failed. No message data.");
+  }
+});
+
+
 let ContentSearchMediator = {
 
   whitelist: new Set([
