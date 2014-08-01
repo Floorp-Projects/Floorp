4c2968f2c7580eefb95e8e1e3f893496439ae802
Roman Skalish <rskalish@lohika.com>
2014-08-01 02:14:00 -0400

    Bug 1041369 - Rescan window list on each getUserMedia window/screensharing request. r=jesup, r=gcp

--- media/webrtc/trunk/webrtc/modules/desktop_capture/x11/desktop_device_info_x11.cc
+++ media/webrtc/trunk/webrtc/modules/desktop_capture/x11/desktop_device_info_x11.cc
@@ -22,19 +22,38 @@ DesktopDeviceInfoX11::DesktopDeviceInfoX11() {
 DesktopDeviceInfoX11::~DesktopDeviceInfoX11() {
 }
 
-int32_t DesktopDeviceInfoX11::Init() {
 #if !defined(MULTI_MONITOR_SCREENSHARE)
+int32_t DesktopDeviceInfoX11::MultiMonitorScreenshare()
+{
   DesktopDisplayDevice *pDesktopDeviceInfo = new DesktopDisplayDevice;
-  if(pDesktopDeviceInfo){
+  if (pDesktopDeviceInfo) {
     pDesktopDeviceInfo->setScreenId(0);
     pDesktopDeviceInfo->setDeviceName("Primary Monitor");
     pDesktopDeviceInfo->setUniqueIdName("\\screen\\monitor#1");
 
     desktop_display_list_[pDesktopDeviceInfo->getScreenId()] = pDesktopDeviceInfo;
   }
+  return 0;
+}
+#endif
+
+int32_t DesktopDeviceInfoX11::Init() {
+#if !defined(MULTI_MONITOR_SCREENSHARE)
+  MultiMonitorScreenshare();
 #endif
 
   initializeWindowList();
+
+  return 0;
+}
+
+int32_t DesktopDeviceInfoX11::Refresh() {
+#if !defined(MULTI_MONITOR_SCREENSHARE)
+  desktop_display_list_.clear();
+  MultiMonitorScreenshare();
+#endif
+
+  RefreshWindowList();
 
   return 0;
 }
