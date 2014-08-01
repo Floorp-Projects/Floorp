548ed5a2641c3225a9ccced2da08dc8dc067bd10
Drew Willcoxon <adw@mozilla.com>
2014-08-01 12:00:49 -0700

    Bug 1028985 - Provide search suggestions on Firefox new tab page (about:newtab). r=MattN

--- browser/base/content/test/newtab/browser_newtab_search.js
+++ browser/base/content/test/newtab/browser_newtab_search.js
@@ -8,6 +8,7 @@ const ENGINE_NO_LOGO = "searchEngineNoLogo.xml";
 const ENGINE_1X_LOGO = "searchEngine1xLogo.xml";
 const ENGINE_2X_LOGO = "searchEngine2xLogo.xml";
 const ENGINE_1X_2X_LOGO = "searchEngine1x2xLogo.xml";
+const ENGINE_SUGGESTIONS = "searchSuggestionEngine.xml";
 
 const SERVICE_EVENT_NAME = "ContentSearchService";
 
@@ -141,6 +142,50 @@ function runTests() {
     promiseClick(manageBox),
   ]).then(TestRunner.next);
 
+  // Add the engine that provides search suggestions and switch to it.
+  let suggestionEngine = null;
+  yield promiseNewSearchEngine(ENGINE_SUGGESTIONS, 0).then(engine => {
+    suggestionEngine = engine;
+    TestRunner.next();
+  });
+  Services.search.currentEngine = suggestionEngine;
+  yield promiseSearchEvents(["CurrentEngine"]).then(TestRunner.next);
+  yield checkCurrentEngine(ENGINE_SUGGESTIONS, false, false);
+
+  // Avoid intermittent failures.
+  gSearch()._suggestionController.remoteTimeout = 5000;
+
+  // Type an X in the search input.  This is only a smoke test.  See
+  // browser_searchSuggestionUI.js for comprehensive content search suggestion
+  // UI tests.
+  let input = $("text");
+  input.focus();
+  EventUtils.synthesizeKey("x", {});
+  let suggestionsPromise = promiseSearchEvents(["Suggestions"]);
+
+  // Wait for the search suggestions to become visible and for the Suggestions
+  // message.
+  let table = getContentDocument().getElementById("searchSuggestionTable");
+  info("Waiting for suggestions table to open");
+  let observer = new MutationObserver(() => {
+    if (input.getAttribute("aria-expanded") == "true") {
+      observer.disconnect();
+      ok(!table.hidden, "Search suggestion table unhidden");
+      TestRunner.next();
+    }
+  });
+  observer.observe(input, {
+    attributes: true,
+    attributeFilter: ["aria-expanded"],
+  });
+  yield undefined;
+  yield suggestionsPromise.then(TestRunner.next);
+
+  // Empty the search input, causing the suggestions to be hidden.
+  EventUtils.synthesizeKey("a", { accelKey: true });
+  EventUtils.synthesizeKey("VK_DELETE", {});
+  ok(table.hidden, "Search suggestion table hidden");
+
   // Done.  Revert the current engine and remove the new engines.
   Services.search.currentEngine = oldCurrentEngine;
   yield promiseSearchEvents(["CurrentEngine"]).then(TestRunner.next);
@@ -264,6 +309,11 @@ function checkCurrentEngine(basename, has1xLogo, has2xLogo) {
     ok(/^url\("blob:/.test(logo.style.backgroundImage), "Logo URI"); //"
   }
 
+  if (logo.hidden) {
+    executeSoon(TestRunner.next);
+    return;
+  }
+
   // "selected" attributes of engines in the panel
   let panel = searchPanel();
   promisePanelShown(panel).then(() => {
@@ -283,7 +333,7 @@ function checkCurrentEngine(basename, has1xLogo, has2xLogo) {
     }
     TestRunner.next();
   });
-  panel.openPopup(logoImg());
+  panel.openPopup(logo);
 }
 
 function promisePanelShown(panel) {
