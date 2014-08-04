83f3a7a847e7d6ad6135553c4ab58de8e70197c5
Masayuki Nakano <masayuki@d-toybox.com>
2014-08-04 14:28:56 +0900

    Bug 1046101 part.29 Rename NS_CONTENT_COMMAND_EVENT to eContentCommandEventClass r=smaug

--- widget/windows/winrt/MetroWidget.cpp
+++ widget/windows/winrt/MetroWidget.cpp
@@ -661,7 +661,7 @@ MetroWidget::DispatchScrollEvent(mozilla::WidgetGUIEvent* aEvent)
       newEvent = static_cast<WidgetGUIEvent*>(wheelEvent);
     }
     break;
-    case NS_CONTENT_COMMAND_EVENT:
+    case eContentCommandEventClass:
     {
       WidgetContentCommandEvent* oldEvent = aEvent->AsContentCommandEvent();
       WidgetContentCommandEvent* cmdEvent =
