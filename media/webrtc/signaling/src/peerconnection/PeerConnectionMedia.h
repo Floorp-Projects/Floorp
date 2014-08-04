5962e2523de2733ffd9b8c9ec3be28d71817e802
Martin Thomson <martin.thomson@gmail.com>
2014-08-04 16:31:45 -0400

    Bug 1048261 - Safe dispatch from DTLS connect to PeerConnectionImpl. r=jesup, r=bwc, a=abillings

--- media/webrtc/signaling/src/peerconnection/PeerConnectionMedia.h
+++ media/webrtc/signaling/src/peerconnection/PeerConnectionMedia.h
@@ -362,8 +362,10 @@ class PeerConnectionMedia : public sigslot::has_slots<> {
   void AddTransportFlow(int aIndex, bool aRtcp,
                         const mozilla::RefPtr<mozilla::TransportFlow> &aFlow);
   void ConnectDtlsListener_s(const mozilla::RefPtr<mozilla::TransportFlow>& aFlow);
-  void DtlsConnected(mozilla::TransportLayer* aFlow,
-                     mozilla::TransportLayer::State state);
+  void DtlsConnected_s(mozilla::TransportLayer* aFlow,
+                       mozilla::TransportLayer::State state);
+  static void DtlsConnected_m(const std::string& aParentHandle,
+                              bool aPrivacyRequested);
 
   mozilla::RefPtr<mozilla::MediaSessionConduit> GetConduit(int aStreamIndex, bool aReceive) {
     int index_inner = aStreamIndex * 2 + (aReceive ? 0 : 1);
@@ -406,6 +408,8 @@ class PeerConnectionMedia : public sigslot::has_slots<> {
 
   // The parent PC
   PeerConnectionImpl *mParent;
+  // and a loose handle on it for event driven stuff
+  std::string mParentHandle;
 
   // A list of streams returned from GetUserMedia
   // This is only accessed on the main thread (with one special exception)
