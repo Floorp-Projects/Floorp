065cf239b19a80b46c05f0a2d73b7e9dba104667
Brian Smith <brian@briansmith.org>
2014-08-04 10:59:21 -0700

    Bug 1048642, Part 2: Change GenerateKeyPair return type from SECStatus to Result, r=cviecco
    --HG--
    extra : rebase_source : 652277e952d224175ea57d4509124ff8180440cb

--- security/pkix/test/lib/pkixtestutil.h
+++ security/pkix/test/lib/pkixtestutil.h
@@ -79,8 +79,8 @@ extern const PRTime ONE_DAY;
 mozilla::pkix::Time YMDHMS(int16_t year, int16_t month, int16_t day,
                            int16_t hour, int16_t minutes, int16_t seconds);
 
-SECStatus GenerateKeyPair(/*out*/ ScopedSECKEYPublicKey& publicKey,
-                          /*out*/ ScopedSECKEYPrivateKey& privateKey);
+Result GenerateKeyPair(/*out*/ ScopedSECKEYPublicKey& publicKey,
+                       /*out*/ ScopedSECKEYPrivateKey& privateKey);
 
 // The result will be owned by the arena
 const SECItem* ASCIIToDERName(PLArenaPool* arena, const char* cn);
