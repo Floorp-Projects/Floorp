b09c99eab65fc5c6ad2a88f5e40a1c890f895f7b
Martin Thomson <martin.thomson@gmail.com>
2014-08-04 11:49:00 -0400

    Bug 996237 - Limit WebRTC cipher profiles. r=ekr

--- media/mtransport/transportlayer.h
+++ media/mtransport/transportlayer.h
@@ -29,7 +29,7 @@ enum {
 };
 
 #define TRANSPORT_LAYER_ID(name) \
-  virtual const std::string id() { return name; } \
+  virtual const std::string id() const { return name; } \
   static std::string ID() { return name; }
 
 // Abstract base class for network transport layers.
@@ -81,10 +81,10 @@ class TransportLayer : public sigslot::has_slots<> {
                          SignalPacketReceived;
 
   // Return the layer id for this layer
-  virtual const std::string id() = 0;
+  virtual const std::string id() const = 0;
 
   // The id of the flow
-  const std::string& flow_id() {
+  const std::string& flow_id() const {
     return flow_id_;
   }
 
