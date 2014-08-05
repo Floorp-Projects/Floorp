d782967bba114ac782e7f1711494e3797be28de7
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2014-08-05 11:47:08 -0700

    Bug 908933 - Part1 - CSP: Call ShouldLoad inside ShouldProcess for TYPE_OBJECT. r=ckerschb

--- dom/security/nsCSPService.cpp
+++ dom/security/nsCSPService.cpp
@@ -205,11 +205,38 @@ CSPService::ShouldProcess(uint32_t         aContentType,
                           nsIPrincipal     *aRequestPrincipal,
                           int16_t          *aDecision)
 {
-  if (!aContentLocation)
+  if (!aContentLocation) {
     return NS_ERROR_FAILURE;
+  }
 
-  *aDecision = nsIContentPolicy::ACCEPT;
-  return NS_OK;
+  if (MOZ_LOG_TEST(gCspPRLog, LogLevel::Debug)) {
+    nsAutoCString location;
+    aContentLocation->GetSpec(location);
+    MOZ_LOG(gCspPRLog, LogLevel::Debug,
+        ("CSPService::ShouldProcess called for %s", location.get()));
+  }
+
+  // ShouldProcess is only relevant to TYPE_OBJECT, so let's convert the
+  // internal contentPolicyType to the mapping external one.
+  // If it is not TYPE_OBJECT, we can return at this point.
+  // Note that we should still pass the internal contentPolicyType
+  // (aContentType) to ShouldLoad().
+  uint32_t policyType =
+    nsContentUtils::InternalContentPolicyTypeToExternal(aContentType);
+
+  if (policyType != nsIContentPolicy::TYPE_OBJECT) {
+    *aDecision = nsIContentPolicy::ACCEPT;
+    return NS_OK;
+  }
+
+  return ShouldLoad(aContentType,
+                    aContentLocation,
+                    aRequestOrigin,
+                    aRequestContext,
+                    aMimeTypeGuess,
+                    aExtra,
+                    aRequestPrincipal,
+                    aDecision);
 }
 
 /* nsIChannelEventSink implementation */
