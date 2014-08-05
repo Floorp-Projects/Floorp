1b4e40f829ceeb3ed0996edd49b8cf2bfcaa2b5c
Chris Pearce <cpearce@mozilla.com>
2014-08-05 19:56:05 +1200

    Bug 1048107 - Add GMPTimer implementation and placeholder GMP clock. r=jesup

--- content/media/gmp/GMPChild.h
+++ content/media/gmp/GMPChild.h
@@ -8,6 +8,7 @@
 
 #include "mozilla/gmp/PGMPChild.h"
 #include "GMPSharedMemManager.h"
+#include "GMPTimerChild.h"
 #include "gmp-entrypoints.h"
 #include "prlink.h"
 
@@ -27,6 +28,9 @@ public:
             IPC::Channel* aChannel);
   bool LoadPluginLibrary(const std::string& aPluginPath);
   MessageLoop* GMPMessageLoop();
+
+  // Main thread only.
+  GMPTimerChild* GetGMPTimers();
 
   // GMPSharedMem
   virtual void CheckThread() MOZ_OVERRIDE;
@@ -51,10 +55,15 @@ private:
   virtual bool DeallocPGMPAudioDecoderChild(PGMPAudioDecoderChild* aActor) MOZ_OVERRIDE;
   virtual bool RecvPGMPAudioDecoderConstructor(PGMPAudioDecoderChild* aActor) MOZ_OVERRIDE;
 
+  virtual PGMPTimerChild* AllocPGMPTimerChild() MOZ_OVERRIDE;
+  virtual bool DeallocPGMPTimerChild(PGMPTimerChild* aActor) MOZ_OVERRIDE;
+
   virtual bool RecvCrashPluginNow() MOZ_OVERRIDE;
 
   virtual void ActorDestroy(ActorDestroyReason aWhy) MOZ_OVERRIDE;
   virtual void ProcessingError(Result aWhat) MOZ_OVERRIDE;
+
+  nsRefPtr<GMPTimerChild> mTimerChild;
 
   PRLibrary* mLib;
   GMPGetAPIFunc mGetAPIFunc;
