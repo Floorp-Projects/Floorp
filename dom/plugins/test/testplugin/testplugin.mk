426076cdefaf6eccb12e14284445b6b0c20b1bb9
Mike Hommey <mh+mozilla@glandium.org>
2014-08-06 07:25:33 +0900

    Bug 1045783 - Move most OS_LIBS to moz.build and do some related cleanup. r=mshal

--- dom/plugins/test/testplugin/testplugin.mk
+++ dom/plugins/test/testplugin/testplugin.mk
@@ -3,17 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ifeq ($(MOZ_WIDGET_TOOLKIT),qt)
-include $(topsrcdir)/config/config.mk
-OS_LIBS += \
-                $(MOZ_QT_LIBS) \
-                $(XLDFLAGS) \
-                $(XLIBS)
-endif
-
 ifeq ($(MOZ_WIDGET_TOOLKIT),windows)
-OS_LIBS  += $(call EXPAND_LIBNAME,msimg32)
-
 # Windows opt builds without PGO break nptest.dll
 MOZ_OPTIMIZE=
 endif
@@ -31,10 +21,4 @@ INSTALL_TARGETS += \
 else
 TEST_PLUGIN_DEST = $(DIST)/plugins
 INSTALL_TARGETS += TEST_PLUGIN
-endif
-
-include $(topsrcdir)/config/rules.mk
-
-ifneq (,$(filter gtk2 gtk3,$(MOZ_WIDGET_TOOLKIT)))
-OS_LIBS += $(MOZ_GTK2_LIBS) $(XLDFLAGS) $(XLIBS) $(XEXT_LIBS)
 endif
