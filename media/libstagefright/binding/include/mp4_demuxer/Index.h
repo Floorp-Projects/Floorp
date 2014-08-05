d66cd54dfd76e11e6be7fd610a02be30345af212
Anthony Jones <ajones@mozilla.com>
2014-08-05 18:12:48 +1200

    Bug 1045909 - Fix buffer range calculation for fMP4; r=edwin

--- media/libstagefright/binding/include/mp4_demuxer/Index.h
+++ media/libstagefright/binding/include/mp4_demuxer/Index.h
@@ -6,26 +6,28 @@
 #define INDEX_H_
 
 #include "media/stagefright/MediaSource.h"
+#include "mozilla/Monitor.h"
 #include "mp4_demuxer/mp4_demuxer.h"
 
 namespace mp4_demuxer
 {
 
 template <typename T> class Interval;
+class MoofParser;
 
 class Index
 {
 public:
-  Index() {}
-
-  void Init(
-    const stagefright::Vector<stagefright::MediaSource::Indice>& aIndex);
+  Index(const stagefright::Vector<stagefright::MediaSource::Indice>& aIndex,
+        Stream* aSource, uint32_t aTrackId);
   void ConvertByteRangesToTimeRanges(
     const nsTArray<mozilla::MediaByteRange>& aByteRanges,
     nsTArray<Interval<Microseconds>>* aTimeRanges);
 
 private:
+  mozilla::Monitor mMonitor;
   nsTArray<stagefright::MediaSource::Indice> mIndex;
+  nsAutoPtr<MoofParser> mMoofParser;
 };
 }
 
