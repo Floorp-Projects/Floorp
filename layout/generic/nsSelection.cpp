689b374303def8118d423e8541078cc11fe15113
Ryan VanderMeulen <ryanvm@gmail.com>
2014-08-05 13:58:50 -0400

    Backed out changeset 80d85e3fdf41 (bug 1046573) for bustage. CLOSED TREE

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -362,12 +362,6 @@ nsFrameSelection::nsFrameSelection()
   //alwaysoverrides these values.
   mDelayedMouseEventIsShift = false;
   mDelayedMouseEventClickCount = 0;
-
-  nsCOMPtr<nsIObserverService> observerService =
-    mozilla::services::GetObserverService();
-  if (observerService) {
-    observerService->NotifyObservers(this, "selection-object-created", nullptr);
-  }
 }
 
 
