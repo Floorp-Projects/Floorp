8e812e80c8da07a0c500c418de27d240c0154837
Ed Morley <emorley@mozilla.com>
2014-08-05 13:33:17 +0100

    Backed out changeset 2d35b006a2f6 (bug 1042729)

--- js/src/jit/MIRGraph.cpp
+++ js/src/jit/MIRGraph.cpp
@@ -728,7 +728,6 @@ MBasicBlock::discardResumePoint(MResumePoint *rp, ReferencesType refType /* = Re
 {
     if (refType & RefType_DiscardOperands)
         rp->discardUses();
-#ifdef DEBUG
     MResumePointIterator iter = resumePointsBegin();
     while (*iter != rp) {
         // We should reach it before reaching the end.
@@ -736,7 +735,6 @@ MBasicBlock::discardResumePoint(MResumePoint *rp, ReferencesType refType /* = Re
         iter++;
     }
     resumePoints_.removeAt(iter);
-#endif
 }
 
 void
