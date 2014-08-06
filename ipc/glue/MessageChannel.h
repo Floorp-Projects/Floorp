97cdc713bf99f2f33f76de44c4ac30a73817c669
Bill McCloskey <wmccloskey@mozilla.com>
2014-08-05 21:44:42 -0700

    Bug 1042587 - Block scripts when using CPOWs (r=bent)

--- ipc/glue/MessageChannel.h
+++ ipc/glue/MessageChannel.h
@@ -89,6 +89,13 @@ class MessageChannel : HasResultCodes
         mAbortOnError = true;
     }
 
+    void BlockScripts();
+
+    bool ShouldBlockScripts() const
+    {
+        return mBlockScripts;
+    }
+
     // Asynchronously send a message to the other side of the channel
     bool Send(Message* aMsg);
 
@@ -639,6 +646,9 @@ class MessageChannel : HasResultCodes
     // Should the channel abort the process from the I/O thread when
     // a channel error occurs?
     bool mAbortOnError;
+
+    // Should we prevent scripts from running while dispatching urgent messages?
+    bool mBlockScripts;
 };
 
 bool
