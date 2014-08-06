c74b304fec5387741f789dfcca3be3f9fdeae026
Blake Wu <bwu@mozilla.com>
2014-08-06 00:20:00 -0400

    Bug 941302 - Part 3: Changes for Adding Gonk Decode Module. r=cpearce

--- content/media/fmp4/MP4Reader.cpp
+++ content/media/fmp4/MP4Reader.cpp
@@ -230,8 +230,14 @@ private:
   nsString mInitDataType;
 };
 
-bool MP4Reader::IsWaitingMediaResources()
-{
+bool MP4Reader::IsWaitingOnCodecResource() {
+#ifdef MOZ_GONK_MEDIACODEC
+  return mVideo.mDecoder && mVideo.mDecoder->IsWaitingMediaResources();
+#endif
+  return false;
+}
+
+bool MP4Reader::IsWaitingOnCDMResource() {
   nsRefPtr<CDMProxy> proxy;
   {
     ReentrantMonitorAutoEnter mon(mDecoder->GetReentrantMonitor());
@@ -251,6 +257,15 @@ bool MP4Reader::IsWaitingMediaResources()
     LOG("MP4Reader::IsWaitingMediaResources() capsKnown=%d", caps.AreCapsKnown());
     return !caps.AreCapsKnown();
   }
+}
+
+bool MP4Reader::IsWaitingMediaResources()
+{
+  // IsWaitingOnCDMResource() *must* come first, because we don't know whether
+  // we can create a decoder until the CDM is initialized and it has told us
+  // whether *it* will decode, or whether we need to create a PDM to do the
+  // decoding
+  return IsWaitingOnCDMResource() || IsWaitingOnCodecResource();
 }
 
 void
@@ -294,7 +309,12 @@ MP4Reader::ReadMetadata(MediaInfo* aInfo,
     // an encrypted stream and we need to wait for a CDM to be set, we don't
     // need to reinit the demuxer.
     mDemuxerInitialized = true;
+  } else if (mPlatform && !IsWaitingMediaResources()) {
+    *aInfo = mInfo;
+    *aTags = nullptr;
+    return NS_OK;
   }
+
   if (mDemuxer->Crypto().valid) {
     if (!sIsEMEEnabled) {
       // TODO: Need to signal DRM/EME required somehow...
@@ -733,6 +753,38 @@ MP4Reader::GetBuffered(dom::TimeRanges* aBuffered, int64_t aStartTime)
   }
 
   return NS_OK;
+}
+
+bool MP4Reader::IsDormantNeeded()
+{
+#ifdef MOZ_GONK_MEDIACODEC
+  return mVideo.mDecoder && mVideo.mDecoder->IsDormantNeeded();
+#endif
+  return false;
+}
+
+void MP4Reader::ReleaseMediaResources()
+{
+#ifdef MOZ_GONK_MEDIACODEC
+  // Before freeing a video codec, all video buffers needed to be released
+  // even from graphics pipeline.
+  VideoFrameContainer* container = mDecoder->GetVideoFrameContainer();
+  if (container) {
+    container->ClearCurrentFrame();
+  }
+  if (mVideo.mDecoder) {
+    mVideo.mDecoder->ReleaseMediaResources();
+  }
+#endif
+}
+
+void MP4Reader::NotifyResourcesStatusChanged()
+{
+#ifdef MOZ_GONK_MEDIACODEC
+  if (mDecoder) {
+    mDecoder->NotifyWaitingForResourcesStatusChanged();
+  }
+#endif
 }
 
 } // namespace mozilla
