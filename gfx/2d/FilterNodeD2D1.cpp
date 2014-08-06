8235a2b538d54feaa8fe58c6800f39e7ac095ee6
Nicolas Silva <nsilva@mozilla.com>
2014-08-06 14:40:03 +0200

    Bug 1025138 - Integrate DrawTargetTiled preffed off. r=mattwoodrow

--- gfx/2d/FilterNodeD2D1.cpp
+++ gfx/2d/FilterNodeD2D1.cpp
@@ -128,6 +128,10 @@ D2D1_CHANNEL_SELECTOR D2DChannelSelector(uint32_t aMode)
 
 TemporaryRef<ID2D1Image> GetImageForSourceSurface(DrawTarget *aDT, SourceSurface *aSurface)
 {
+  if (aDT->IsTiledDrawTarget() || aDT->IsDualDrawTarget()) {
+      MOZ_CRASH("Incompatible draw target type!");
+      return nullptr;
+  }
   switch (aDT->GetBackendType()) {
     case BackendType::DIRECT2D1_1:
       return static_cast<DrawTargetD2D1*>(aDT)->GetImageForSurface(aSurface, ExtendMode::CLAMP);
