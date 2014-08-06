aafad78dca21e143335974dc3211928b7efd8e08
Jeff Walden <jwalden@mit.edu>
2014-08-06 16:37:48 -0700

    Bug 1046964 - Warn when encountering a string expression statement, that would be a directive but isn't in the right position, when respecting the non-directive would change semantics.  r=njn

---
+++ js/src/tests/ecma_5/extensions/misplaced-inconsistent-directive.js
@@ -0,0 +1,65 @@
+// |reftest| skip-if(!xulRuntime.shell) -- needs evaluate()
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/licenses/publicdomain/
+ * Contributor:
+ *   Jeff Walden <jwalden+code@mit.edu>
+ */
+
+//-----------------------------------------------------------------------------
+var BUGNUMBER = 1046964;
+var summary =
+  "Misplaced directives (e.g. 'use strict') should trigger warnings if they " +
+  "contradict the actually-used semantics";
+
+print(BUGNUMBER + ": " + summary);
+
+/**************
+ * BEGIN TEST *
+ **************/
+
+options("strict");
+options("werror");
+
+function evaluateNoRval(code)
+{
+  evaluate(code, { compileAndGo: true, noScriptRval: true });
+}
+
+function expectSyntaxError(code)
+{
+  try
+  {
+    evaluateNoRval(code);
+    throw new Error("didn't throw");
+  }
+  catch (e)
+  {
+    assertEq(e instanceof SyntaxError, true,
+             "should have thrown a SyntaxError, instead got:\n" +
+             "    " + e + "\n" +
+             "when evaluating:\n" +
+             "    " + code);
+  }
+}
+
+expectSyntaxError("function f1() {} 'use strict'; function f2() {}");
+expectSyntaxError("function f3() { var x; 'use strict'; }");
+
+expectSyntaxError("function f4() {} 'use asm'; function f5() {}");
+expectSyntaxError("function f6() { var x; 'use strict'; }");
+expectSyntaxError("'use asm'; function f7() {}");
+
+// No errors expected -- useless non-directives, but not contrary to used
+// semantics.
+evaluateNoRval("'use strict'; function f8() {} 'use strict'; function f9() {}");
+evaluateNoRval("'use strict'; function f10() { var z; 'use strict' }");
+
+evaluateNoRval("function f11() { 'use asm'; return {}; }");
+
+/******************************************************************************/
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
+
+print("Tests complete");
