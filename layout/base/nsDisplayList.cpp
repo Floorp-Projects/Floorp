075f97aa3732cc41ba7d31ce1ea7fda4df2d53e0
Robert O'Callahan <robert@ocallahan.org>
2014-08-06 17:19:25 +1200

    Bug 1048752. Part 11: Remove nsLayoutUtils.h from nsDisplayList.h. r=tn
    --HG--
    extra : rebase_source : 02251076442a5c4251e8d9484cb428a4c07f3a13

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -2004,6 +2004,22 @@ nsDisplayBackgroundImage::IsSingleFixedPositionImage(nsDisplayListBuilder* aBuil
 }
 
 bool
+nsDisplayBackgroundImage::ShouldFixToViewport(LayerManager* aManager)
+{
+  // APZ doesn't (yet) know how to scroll the visible region for these type of
+  // items, so don't layerize them if it's enabled.
+  if (nsLayoutUtils::UsesAsyncScrolling() ||
+      (aManager && aManager->ShouldAvoidComponentAlphaLayers())) {
+    return false;
+  }
+
+  // Put background-attachment:fixed background images in their own
+  // compositing layer, unless we have APZ enabled
+  return mBackgroundStyle->mLayers[mLayer].mAttachment == NS_STYLE_BG_ATTACHMENT_FIXED &&
+         !mBackgroundStyle->mLayers[mLayer].mImage.IsEmpty();
+}
+
+bool
 nsDisplayBackgroundImage::TryOptimizeToImageLayer(LayerManager* aManager,
                                                   nsDisplayListBuilder* aBuilder)
 {
