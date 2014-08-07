789c541082be9fff627e4ad4966e5c3cf25181bf
Wes Kocher <wkocher@mozilla.com>
2014-08-06 18:32:52 -0700

    Backed out 2 changesets (bug 1048110) for various Android mochitest failures on a CLOSED TREE
    Backed out changeset 1cf6f4f27811 (bug 1048110)
    Backed out changeset b89f54e21c0b (bug 1048110)

--- gfx/layers/client/TiledContentClient.cpp
+++ gfx/layers/client/TiledContentClient.cpp
@@ -327,11 +327,6 @@ ClientTiledLayerBuffer::GetContentType(SurfaceMode* aMode) const
         content = gfxContentType::COLOR;
       }
 #endif
-  } else if (mode == SurfaceMode::SURFACE_OPAQUE) {
-    if (mThebesLayer->MayResample()) {
-      mode = SurfaceMode::SURFACE_SINGLE_CHANNEL_ALPHA;
-      content = gfxContentType::COLOR_ALPHA;
-    }
   }
 
   if (aMode) {
