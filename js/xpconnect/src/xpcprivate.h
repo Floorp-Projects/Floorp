28d565a7582e5e085d6849686a9acfe1deae4ce9
Bobby Holley <bobbyholley@gmail.com>
2014-08-06 23:32:23 -0400

    Bug 1042436 - Warn once to the console when XrayWrappers deny access to an object. r=gabor, sr=smaug, a=sledru

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -3650,6 +3650,7 @@ public:
         , forcePermissiveCOWs(false)
         , adoptedNode(false)
         , donatedNode(false)
+        , warnedAboutXrays(false)
         , scriptability(c)
         , scope(nullptr)
     {
@@ -3704,6 +3705,10 @@ public:
     // for telemetry. See bug 928476.
     bool adoptedNode;
     bool donatedNode;
+
+    // Whether we've emitted a warning about a property that was filtered out
+    // by XrayWrappers. See XrayWrapper.cpp.
+    bool warnedAboutXrays;
 
     // The scriptability of this compartment.
     Scriptability scriptability;
