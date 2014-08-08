eecf6f03c14fe468004c00de1bf1754c58a3ea48
Ryan VanderMeulen <ryanvm@gmail.com>
2014-08-08 15:22:17 -0400

    Backed out changesets 1cf7b5810eb5, 4cb373d9ce33, and 6037db66624b (bug 1042436) for bustage.

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -3474,7 +3474,6 @@ public:
         , universalXPConnectEnabled(false)
         , adoptedNode(false)
         , donatedNode(false)
-        , warnedAboutXrays(false)
         , scriptability(c)
         , scope(nullptr)
     {
@@ -3494,10 +3493,6 @@ public:
     // for telemetry. See bug 928476.
     bool adoptedNode;
     bool donatedNode;
-
-    // Whether we've emitted a warning about a property that was filtered out
-    // by XrayWrappers. See XrayWrapper.cpp.
-    bool warnedAboutXrays;
 
     // The scriptability of this compartment.
     Scriptability scriptability;
