5324e3a9e0dad5ef6ac8a81774b95ecffc4f5dbc
Peter Van der Beken <peterv@propagandism.org>
2014-08-08 15:03:29 +0200

    Bug 1036186 - Reset Migration wizard no longer skips the first step to choose a browser. r=smaug.

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -428,7 +428,6 @@ static const nsConstructorFuncMapData kConstructorFuncMap[] =
 #undef NS_DEFINE_CONSTRUCTOR_FUNC_DATA
 
 nsIXPConnect *nsDOMClassInfo::sXPConnect = nullptr;
-nsIScriptSecurityManager *nsDOMClassInfo::sSecMan = nullptr;
 bool nsDOMClassInfo::sIsInitialized = false;
 
 
@@ -779,18 +778,10 @@ nsDOMClassInfo::Init()
   nsScriptNameSpaceManager *nameSpaceManager = GetNameSpaceManager();
   NS_ENSURE_TRUE(nameSpaceManager, NS_ERROR_NOT_INITIALIZED);
 
-  nsresult rv = CallGetService(nsIXPConnect::GetCID(), &sXPConnect);
-  NS_ENSURE_SUCCESS(rv, rv);
+  NS_ADDREF(sXPConnect = nsContentUtils::XPConnect());
 
   nsCOMPtr<nsIXPCFunctionThisTranslator> elt = new nsEventListenerThisTranslator();
   sXPConnect->SetFunctionThisTranslator(NS_GET_IID(nsIDOMEventListener), elt);
-
-  nsCOMPtr<nsIScriptSecurityManager> sm =
-    do_GetService("@mozilla.org/scriptsecuritymanager;1", &rv);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  sSecMan = sm;
-  NS_ADDREF(sSecMan);
 
   AutoSafeJSContext cx;
 
@@ -1041,9 +1032,6 @@ nsDOMClassInfo::Init()
   }
 
   RegisterExternalClasses();
-
-  // Register new DOM bindings
-  mozilla::dom::Register(nameSpaceManager);
 
   sIsInitialized = true;
 
@@ -1523,11 +1511,8 @@ NS_GetDOMClassInfoInstance(nsDOMClassInfoID aID)
     return nullptr;
   }
 
-  if (!nsDOMClassInfo::sIsInitialized) {
-    nsresult rv = nsDOMClassInfo::Init();
-
-    NS_ENSURE_SUCCESS(rv, nullptr);
-  }
+  nsresult rv = RegisterDOMNames();
+  NS_ENSURE_SUCCESS(rv, nullptr);
 
   if (!sClassInfoData[aID].mCachedClassInfo) {
     nsDOMClassInfoData& data = sClassInfoData[aID];
@@ -1588,7 +1573,6 @@ nsDOMClassInfo::ShutDown()
   sWrappedJSObject_id = JSID_VOID;
 
   NS_IF_RELEASE(sXPConnect);
-  NS_IF_RELEASE(sSecMan);
   sIsInitialized = false;
 }
 
