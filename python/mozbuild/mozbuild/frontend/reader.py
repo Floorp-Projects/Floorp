0ae84fc8ece09353465e8c26beb72ed189472392
Joshua Cranmer <Pidgeot18@gmail.com>
2014-08-07 20:16:13 -0500

    Bustage fix for bug 1035599, r+a=bustage-fix for CLOSED TREE

--- python/mozbuild/mozbuild/frontend/reader.py
+++ python/mozbuild/mozbuild/frontend/reader.py
@@ -200,8 +200,6 @@ class MozbuildSandbox(Sandbox):
         will be treated as relative to topsrcdir.
         """
         if os.path.isabs(path):
-            # If the path isn't in Unix-style, this is going to be problematic.
-            assert path[0] == '/'
             if filesystem_absolute:
                 return path
             for root in [self.topsrcdir] + self.external_source_dirs:
