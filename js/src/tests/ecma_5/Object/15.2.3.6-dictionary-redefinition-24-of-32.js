78dac66baf23e5b9fcc481051d51834ba022e52b
Jeff Walden <jwalden@mit.edu>
2014-08-08 19:49:38 -0700

    Further split up the dictionary-property-redefinition tests (beyond their current eight-way split, to a thirty-two-way split) so they don't potentially time out so readily.  No bug, r=me as mechanical

--- js/src/tests/ecma_5/Object/15.2.3.6-dictionary-redefinition-24-of-32.js
+++ js/src/tests/ecma_5/Object/15.2.3.6-dictionary-redefinition-24-of-32.js
@@ -3,4 +3,4 @@
 // http://creativecommons.org/licenses/publicdomain/
 
 load("ecma_5/Object/defineProperty-setup.js");
-runDictionaryPropertyPresentTestsFraction(8, 8);
+runDictionaryPropertyPresentTestsFraction(24, 32);
