726b120ca70c72aad242abab2f39b29d3b8dc6c4
Ryan VanderMeulen <ryanvm@gmail.com>
2014-08-09 15:14:09 -0400

    Backed out changesets 37df1d58af4a and 792b068b7805 (bug 1025445) for Gaia unit test failures.

--- js/src/jit-test/tests/ion/bug1025445.js
+++
@@ -1,8 +0,0 @@
-setJitCompilerOption("baseline.usecount.trigger", 10);
-setJitCompilerOption("ion.usecount.trigger", 20);
-
-var y = 1;
-for (var i = 0; i < 64; i++) {
-  assertEq(Math.floor(y), y);
-  y *= 2;
-}
