3bd6fe6f31f6f7154f9d517e83a473afcab76b63
Ben Turner <bent.mozilla@gmail.com>
2014-08-08 18:19:33 -0700

    Bug 1044322 - Reset the channel listener on the correct thread, r=bsmedberg.

--- ipc/glue/MessageLink.h
+++ ipc/glue/MessageLink.h
@@ -141,6 +141,14 @@ class ProcessLink
   public:
     explicit ProcessLink(MessageChannel *chan);
     virtual ~ProcessLink();
+
+    // The ProcessLink will register itself as the IPC::Channel::Listener on the
+    // transport passed here. If the transport already has a listener registered
+    // then a listener chain will be established (the ProcessLink listener
+    // methods will be called first and may call some methods on the original
+    // listener as well). Once the channel is closed (either via normal shutdown
+    // or a pipe error) the chain will be destroyed and the original listener
+    // will again be registered.
     void Open(Transport* aTransport, MessageLoop *aIOLoop, Side aSide);
     
     // Run on the I/O thread, only when using inter-process link.
