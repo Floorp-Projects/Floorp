b2e7fa6fbc428eeb5f9873d7de7243564dd548e9
Terrence Cole <terrence@mozilla.com>
2014-08-08 17:00:46 -0700

    Bug 1033442 - Add allocation functions to Cell to make it more obvious which allocator to use; r=jonco

--- js/src/jit/BaselineCompiler.cpp
+++ js/src/jit/BaselineCompiler.cpp
@@ -178,7 +178,7 @@ BaselineCompiler::compile()
     // Note: There is an extra entry in the bytecode type map for the search hint, see below.
     size_t bytecodeTypeMapEntries = script->nTypeSets() + 1;
 
-    BaselineScript *baselineScript = BaselineScript::New(cx, prologueOffset_.offset(),
+    BaselineScript *baselineScript = BaselineScript::New(script, prologueOffset_.offset(),
                                                          epilogueOffset_.offset(),
                                                          spsPushToggleOffset_.offset(),
                                                          postDebugPrologueOffset_.offset(),
