0c909995751242d29a2852d2362b48de042a887f
Brian Birtles <birtles@gmail.com>
2014-08-10 17:06:44 +1000

    Bug 1040543 part 1 - Move/Rename ElementAnimations to dom::AnimationPlayer; r=bz
    This patch renames mozilla::ElementAnimations to mozilla::dom::AnimationPlayer
    and moves the code from layout/style/AnimationCommon.cpp to
    dom/animation/AnimationPlayer.cpp.
    
    It also moves various helper classes needed by AnimationPlayer to
    AnimationPlayer.cpp and moves them from the mozilla::css namespace to the
    mozilla namespace.
    
    Beyond that, there are no functional changes contained in this patch.
    
    The renaming of various members and variables that used to refer to
    ElementAnimation objects but now refer to AnimationPlayer objects--to give them
    a more appropriate name--is performed in a subsequent patch.
    
    --HG--
    rename : layout/style/AnimationCommon.cpp => dom/animation/AnimationPlayer.cpp
    rename : layout/style/AnimationCommon.h => dom/animation/AnimationPlayer.h

--- gfx/layers/Layers.cpp
+++ gfx/layers/Layers.cpp
@@ -7,7 +7,6 @@
 
 #include "Layers.h"
 #include <algorithm>                    // for max, min
-#include "AnimationCommon.h"            // for ComputedTimingFunction
 #include "CompositableHost.h"           // for CompositableHost
 #include "ImageContainer.h"             // for ImageContainer, etc
 #include "ImageLayers.h"                // for ImageLayer
@@ -20,6 +19,7 @@
 #include "gfx2DGlue.h"
 #include "mozilla/DebugOnly.h"          // for DebugOnly
 #include "mozilla/Telemetry.h"          // for Accumulate
+#include "mozilla/dom/AnimationPlayer.h" // for ComputedTimingFunction
 #include "mozilla/gfx/2D.h"             // for DrawTarget
 #include "mozilla/gfx/BaseSize.h"       // for BaseSize
 #include "mozilla/gfx/Matrix.h"         // for Matrix4x4
@@ -394,12 +394,13 @@ Layer::SetAnimations(const AnimationArray& aAnimations)
   mAnimationData.Clear();
   for (uint32_t i = 0; i < mAnimations.Length(); i++) {
     AnimData* data = mAnimationData.AppendElement();
-    InfallibleTArray<nsAutoPtr<css::ComputedTimingFunction> >& functions = data->mFunctions;
+    InfallibleTArray<nsAutoPtr<ComputedTimingFunction> >& functions =
+      data->mFunctions;
     const InfallibleTArray<AnimationSegment>& segments =
       mAnimations.ElementAt(i).segments();
     for (uint32_t j = 0; j < segments.Length(); j++) {
       TimingFunction tf = segments.ElementAt(j).sampleFn();
-      css::ComputedTimingFunction* ctf = new css::ComputedTimingFunction();
+      ComputedTimingFunction* ctf = new ComputedTimingFunction();
       switch (tf.type()) {
         case TimingFunction::TCubicBezierFunction: {
           CubicBezierFunction cbf = tf.get_CubicBezierFunction();
