30db2cfb7c242de4d4319861dc12e315a8d1a555
Martijn Wargers <mwargers@mozilla.com>
2014-08-10 20:03:50 -0700

    Bug 1032878 - Fix for various failing mochitest-plain/-chrome tests. r=jmaher, r=akachkach, r=surkov, r=cpearce

--- dom/browser-element/mochitest/browserElement_KeyEvents.js
+++ dom/browser-element/mochitest/browserElement_KeyEvents.js
@@ -82,6 +82,11 @@ function eventHandler(e) {
   }
 
   if (nbEvents == 0) {
+    //removeEventListener, otherwise a key event is fired
+    //for some reason, with keyCode 95
+    removeEventListener('keydown', eventHandler);
+    removeEventListener('keypress', eventHandler);
+    removeEventListener('keyup', eventHandler);
     SimpleTest.finish();
     return;
   }
