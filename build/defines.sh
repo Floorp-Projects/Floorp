193c86c3372396ad64858e792325902faf4d90fb
Lawrence Mandel <lmandel@mozilla.com>
2014-08-11 16:58:35 -0400

    Post Beta 4: disable EARLY_BETA_OR_EARLIER a=me

--- build/defines.sh
+++ build/defines.sh
@@ -1,3 +1,4 @@
 # Define indicating that this build is prior to one of the early betas. To be
-# unset mid-way through the beta cycle.
-EARLY_BETA_OR_EARLIER=1
+# unset mid-way through the beta cycle. Note that the value must be cleared 
+# in order to unset.
+EARLY_BETA_OR_EARLIER=
