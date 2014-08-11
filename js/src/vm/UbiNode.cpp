9a4dbca42a1b6b058dff51d3aa7c1b7e1d092507
Jim Blandy <jimb@mozilla.com>
2014-08-11 12:46:39 -0700

    Bug 1003302: Implement ubi::Node::compartment. r=terrence

--- js/src/vm/UbiNode.cpp
+++ js/src/vm/UbiNode.cpp
@@ -32,6 +32,7 @@ const jschar *Concrete<void>::typeName() const      { MOZ_CRASH("null ubi::Node"
 size_t Concrete<void>::size() const                 { MOZ_CRASH("null ubi::Node"); }
 EdgeRange *Concrete<void>::edges(JSContext *) const { MOZ_CRASH("null ubi::Node"); }
 JS::Zone *Concrete<void>::zone() const              { MOZ_CRASH("null ubi::Node"); }
+JSCompartment *Concrete<void>::compartment() const  { MOZ_CRASH("null ubi::Node"); }
 
 Node::Node(JSGCTraceKind kind, void *ptr)
 {
