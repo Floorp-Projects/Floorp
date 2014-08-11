214fe8b147983e63f5d8aed7293e157b7bbd889b
William Chen <wchen@mozilla.com>
2014-08-11 11:26:52 -0700

    Bug 1029710 - Add null check for container in ShadowRoot::IsPooledNode. r=smaug

--- content/base/src/ShadowRoot.cpp
+++ content/base/src/ShadowRoot.cpp
@@ -583,7 +583,7 @@ ShadowRoot::IsPooledNode(nsIContent* aContent, nsIContent* aContainer,
     return true;
   }
 
-  if (aContainer->IsHTML(nsGkAtoms::content)) {
+  if (aContainer && aContainer->IsHTML(nsGkAtoms::content)) {
     // Fallback content will end up in pool if its parent is a child of the host.
     HTMLContentElement* content = static_cast<HTMLContentElement*>(aContainer);
     return content->IsInsertionPoint() && content->MatchedNodes().IsEmpty() &&
