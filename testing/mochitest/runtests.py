5425a7f6c1718ce42e7dc2dd9e0b196de9fc724e
Tim Taubert <ttaubert@mozilla.com>
2014-08-12 21:23:29 +0200

    Bug 1052371 - Fix m-bc shutdown leak detector. r=akachkach, a=test-only

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -1318,7 +1318,7 @@ class Mochitest(MochitestUtilsMixin):
              symbolsPath=None,
              timeout=-1,
              onLaunch=None,
-             webapprtChrome=False,
+             detectShutdownLeaks=False,
              screenshotOnFail=False,
              testPath=None,
              bisectChunk=None,
@@ -1373,7 +1373,7 @@ class Mochitest(MochitestUtilsMixin):
           testUrl = testUrl.replace("&", "\\&")
         args.append(testUrl)
 
-      if mozinfo.info["debug"] and not webapprtChrome:
+      if detectShutdownLeaks:
         shutdownLeaks = ShutdownLeaks(log.info)
       else:
         shutdownLeaks = None
@@ -1725,6 +1725,9 @@ class Mochitest(MochitestUtilsMixin):
       if options.vmwareRecording:
         self.startVMwareRecording(options);
 
+      # detect shutdown leaks for m-bc runs
+      detectShutdownLeaks = mozinfo.info["debug"] and options.browserChrome and not options.webapprtChrome
+
       log.info("runtests.py | Running tests: start.\n")
       try:
         status = self.runApp(testURL,
@@ -1737,7 +1740,7 @@ class Mochitest(MochitestUtilsMixin):
                              symbolsPath=options.symbolsPath,
                              timeout=timeout,
                              onLaunch=onLaunch,
-                             webapprtChrome=options.webapprtChrome,
+                             detectShutdownLeaks=detectShutdownLeaks,
                              screenshotOnFail=options.screenshotOnFail,
                              testPath=options.testPath,
                              bisectChunk=options.bisectChunk,
