84de446bfdffbf205221c6d28e34142b2cc08c82
Georgios Kontaxis <gkontaxis@mozilla.com>
2014-08-12 13:53:15 -0700

    Bug 1050348: nsChannelClassifier will update the security state of the document and fire a security state change event to include the nsIWebProgressListener::STATE_BLOCKED_TRACKING_CONTENT or STATE_LOADED_TRACKING_CONTENT flag depending on whether it is about to cancel a channel or skip tracking protection classification respectively. r=mcmanus

--- netwerk/base/src/nsChannelClassifier.h
+++ netwerk/base/src/nsChannelClassifier.h
@@ -30,6 +30,9 @@ private:
     bool HasBeenClassified(nsIChannel *aChannel);
     // Whether or not tracking protection should be enabled on this channel.
     nsresult ShouldEnableTrackingProtection(nsIChannel *aChannel, bool *result);
+    // If we are blocking tracking content, update the corresponding flag in
+    // the respective docshell and call nsISecurityEventSink::onSecurityChange.
+    nsresult SetBlockedTrackingContent(nsIChannel *channel);
 };
 
 #endif
