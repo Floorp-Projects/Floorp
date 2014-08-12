95bd91b3cdf24d4cad7eb81206b1774798e1487e
Gregory Szorc <gps@mozilla.com>
2014-08-12 09:23:33 -0700

    Bug 1052526 - Treat appending None to List as empty list; r=glandium
    As a special exception, we change List's + and += operators to treat
    "+ None" and "+= None" as "+ []" and "+= []" respectively.
    
    This is a hack to make moz.build files simpler so they don't have to
    perform "is x" checks before appending x.
    
    While I was here, I fixed the implementation of List.__add__ to return a
    List instead of list.
    
    --HG--
    extra : rebase_source : e61db4be4cf8144b092609d3baf84c372b9cf9a0
    extra : amend_source : dfabc00765582764426f7cf74e79354a0bfb0824

--- python/mozbuild/mozbuild/util.py
+++ python/mozbuild/mozbuild/util.py
@@ -249,12 +249,16 @@ class List(list):
         return list.__setslice__(self, i, j, sequence)
 
     def __add__(self, other):
+        # Allow None is a special case because it makes undefined variable
+        # references in moz.build behave better.
+        other = [] if other is None else other
         if not isinstance(other, list):
             raise ValueError('Only lists can be appended to lists.')
 
-        return list.__add__(self, other)
+        return List(list.__add__(self, other))
 
     def __iadd__(self, other):
+        other = [] if other is None else other
         if not isinstance(other, list):
             raise ValueError('Only lists can be appended to lists.')
 
