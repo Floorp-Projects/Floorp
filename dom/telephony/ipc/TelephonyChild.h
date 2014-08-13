59e5a8048dcd5bda41d0ee60050f51abad8f0b9c
Szu-Yu Chen [:aknow] <szchen@mozilla.com>
2014-08-12 23:51:00 -0400

    Bug 1051715 - Part 2: Also pass number in notifyDialSuccess. r=hsinyi

--- dom/telephony/ipc/TelephonyChild.h
+++ dom/telephony/ipc/TelephonyChild.h
@@ -83,7 +83,8 @@ protected:
   RecvNotifyDialError(const nsString& aError) MOZ_OVERRIDE;
 
   virtual bool
-  RecvNotifyDialSuccess(const uint32_t& aCallIndex) MOZ_OVERRIDE;
+  RecvNotifyDialSuccess(const uint32_t& aCallIndex,
+                        const nsString& aNumber) MOZ_OVERRIDE;
 
 private:
   nsCOMPtr<nsITelephonyListener> mListener;
