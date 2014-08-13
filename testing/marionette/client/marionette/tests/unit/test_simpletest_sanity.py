2c45aa6d65f0f54560270aa49e27c79ea5e57563
Julien Pagès <j.parkouss@gmail.com>
2014-08-13 07:19:00 -0400

    Bug 820154 - Implement todo() in marionette-simpletest. r=jgriffin

--- testing/marionette/client/marionette/tests/unit/test_simpletest_sanity.py
+++ testing/marionette/client/marionette/tests/unit/test_simpletest_sanity.py
@@ -16,16 +16,16 @@ class SimpletestSanityTest(MarionetteTestCase):
             sentPass1 = "is(true, true, 'isTest3');" + self.callFinish
             sentPass2 = "is(true, true, 'isTest4');" + self.callFinish
 
-            self.assertEqual(1, self.marionette.execute_script(sentFail1)["failed"])
+            self.assertEqual(1, len(self.marionette.execute_script(sentFail1)["failures"]))
             self.assertEqual(0, self.marionette.execute_script(sentFail2)["passed"])
             self.assertEqual(1, self.marionette.execute_script(sentPass1)["passed"])
-            self.assertEqual(0, self.marionette.execute_script(sentPass2)["failed"])
+            self.assertEqual(0, len(self.marionette.execute_script(sentPass2)["failures"]))
 
             self.marionette.set_script_timeout(1000)
-            self.assertEqual(1, self.marionette.execute_async_script(sentFail1)["failed"])
+            self.assertEqual(1, len(self.marionette.execute_async_script(sentFail1)["failures"]))
             self.assertEqual(0, self.marionette.execute_async_script(sentFail2)["passed"])
             self.assertEqual(1, self.marionette.execute_async_script(sentPass1)["passed"])
-            self.assertEqual(0, self.marionette.execute_async_script(sentPass2)["failed"])
+            self.assertEqual(0, len(self.marionette.execute_async_script(sentPass2)["failures"]))
 
         self.marionette.set_context("content")
         runtests()
@@ -39,15 +39,15 @@ class SimpletestSanityTest(MarionetteTestCase):
            sentPass1 = "isnot(true, false, 'isnotTest1');" + self.callFinish
            sentPass2 = "isnot(true, false, 'isnotTest2');" + self.callFinish
 
-           self.assertEqual(1, self.marionette.execute_script(sentFail1)["failed"]);
+           self.assertEqual(1, len(self.marionette.execute_script(sentFail1)["failures"]));
            self.assertEqual(0, self.marionette.execute_script(sentFail2)["passed"]);
-           self.assertEqual(0, self.marionette.execute_script(sentPass1)["failed"]);
+           self.assertEqual(0, len(self.marionette.execute_script(sentPass1)["failures"]));
            self.assertEqual(1, self.marionette.execute_script(sentPass2)["passed"]);
 
            self.marionette.set_script_timeout(1000)
-           self.assertEqual(1, self.marionette.execute_async_script(sentFail1)["failed"]);
+           self.assertEqual(1, len(self.marionette.execute_async_script(sentFail1)["failures"]));
            self.assertEqual(0, self.marionette.execute_async_script(sentFail2)["passed"]);
-           self.assertEqual(0, self.marionette.execute_async_script(sentPass1)["failed"]);
+           self.assertEqual(0, len(self.marionette.execute_async_script(sentPass1)["failures"]));
            self.assertEqual(1, self.marionette.execute_async_script(sentPass2)["passed"]);
 
         self.marionette.set_context("content")
@@ -62,15 +62,15 @@ class SimpletestSanityTest(MarionetteTestCase):
             sentPass1 = "ok(1==1, 'testOk3');" + self.callFinish
             sentPass2 = "ok(1==1, 'testOk4');" + self.callFinish
 
-            self.assertEqual(1, self.marionette.execute_script(sentFail1)["failed"]);
+            self.assertEqual(1, len(self.marionette.execute_script(sentFail1)["failures"]));
             self.assertEqual(0, self.marionette.execute_script(sentFail2)["passed"]);
-            self.assertEqual(0, self.marionette.execute_script(sentPass1)["failed"]);
+            self.assertEqual(0, len(self.marionette.execute_script(sentPass1)["failures"]));
             self.assertEqual(1, self.marionette.execute_script(sentPass2)["passed"]);
 
             self.marionette.set_script_timeout(1000)
-            self.assertEqual(1, self.marionette.execute_async_script(sentFail1)["failed"]);
+            self.assertEqual(1, len(self.marionette.execute_async_script(sentFail1)["failures"]));
             self.assertEqual(0, self.marionette.execute_async_script(sentFail2)["passed"]);
-            self.assertEqual(0, self.marionette.execute_async_script(sentPass1)["failed"]);
+            self.assertEqual(0, len(self.marionette.execute_async_script(sentPass1)["failures"]));
             self.assertEqual(1, self.marionette.execute_async_script(sentPass2)["passed"]);
 
         self.marionette.set_context("content")
@@ -78,3 +78,25 @@ class SimpletestSanityTest(MarionetteTestCase):
         self.marionette.set_context("chrome")
         runtests()
 
+    def test_todo(self):
+        def runtests():
+            sentFail1 = "todo(1==1, 'testTodo1', TEST_UNEXPECTED_PASS, TEST_KNOWN_FAIL);" + self.callFinish
+            sentFail2 = "todo(1==1, 'testTodo2', TEST_UNEXPECTED_PASS, TEST_KNOWN_FAIL);" + self.callFinish
+            sentPass1 = "todo(1==2, 'testTodo3');" + self.callFinish
+            sentPass2 = "todo(1==2, 'testTodo4');" + self.callFinish
+
+            self.assertEqual(1, len(self.marionette.execute_script(sentFail1)["unexpectedSuccesses"]));
+            self.assertEqual(0, len(self.marionette.execute_script(sentFail2)["expectedFailures"]));
+            self.assertEqual(0, len(self.marionette.execute_script(sentPass1)["unexpectedSuccesses"]));
+            self.assertEqual(1, len(self.marionette.execute_script(sentPass2)["expectedFailures"]));
+
+            self.marionette.set_script_timeout(1000)
+            self.assertEqual(1, len(self.marionette.execute_async_script(sentFail1)["unexpectedSuccesses"]));
+            self.assertEqual(0, len(self.marionette.execute_async_script(sentFail2)["expectedFailures"]));
+            self.assertEqual(0, len(self.marionette.execute_async_script(sentPass1)["unexpectedSuccesses"]));
+            self.assertEqual(1, len(self.marionette.execute_async_script(sentPass2)["expectedFailures"]));
+
+        self.marionette.set_context("content")
+        runtests()
+        self.marionette.set_context("chrome")
+        runtests()
