2988a96173c6c99a6b2fe93e0ddd0218db86344f
Matthew Gregan <kinetik@flim.org>
2014-08-14 01:14:00 +1200

    Bug 1053674 - Remove subclass of StateMachine and use our own task queue.  r=cpearce
    --HG--
    rename : content/media/mediasource/MediaSourceDecoder.cpp => content/media/mediasource/MediaSourceReader.cpp
    extra : rebase_source : a34a9a5c0274bae5c7f065535242316d605e8e65

--- content/media/mediasource/MediaSourceReader.h
+++ content/media/mediasource/MediaSourceReader.h
@@ -71,8 +71,7 @@ public:
   nsresult Seek(int64_t aTime, int64_t aStartTime, int64_t aEndTime,
                 int64_t aCurrentTime) MOZ_OVERRIDE;
   already_AddRefed<SubBufferDecoder> CreateSubDecoder(const nsACString& aType,
-                                                      MediaSourceDecoder* aParentDecoder,
-                                                      MediaTaskQueue* aTaskQueue);
+                                                      MediaSourceDecoder* aParentDecoder);
 
   void Shutdown();
 
@@ -108,6 +107,8 @@ private:
 
   nsTArray<nsRefPtr<SubBufferDecoder>> mPendingDecoders;
   nsTArray<nsRefPtr<SubBufferDecoder>> mDecoders;
+
+  nsRefPtr<MediaTaskQueue> mTaskQueue;
 
   int32_t mActiveVideoDecoder;
   int32_t mActiveAudioDecoder;
