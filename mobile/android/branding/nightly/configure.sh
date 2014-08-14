9829e1c3d86f403511d5c560c0d537b5980a28bb
Lucas Rocha <lucasr@mozilla.com>
2014-08-14 14:51:30 +0100

    Bug 1053252 - Change package name for larch so it can be installed beside Nightly (r=margaret)

--- mobile/android/branding/nightly/configure.sh
+++ mobile/android/branding/nightly/configure.sh
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=Nightly
+ANDROID_PACKAGE_NAME=org.mozilla.fennec_larch
+MOZ_APP_DISPLAYNAME=Larch Nightly
 MOZ_UPDATER=1
 MOZ_ANDROID_ANR_REPORTER=1
