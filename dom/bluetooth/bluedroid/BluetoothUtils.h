3c727c57eb2e1eca65211ab7af1eabdf4c04948b
Thomas Zimmermann <tdz@users.sourceforge.net>
2014-08-14 18:12:52 +0200

    Bug 1048915: Cleanup |BluetoothServiceBluedroid| and related functions, r=shuang
    This patch removes unsued code from |BluetoothServiceBluedroid|, related
    functions, and cleans up the file. It
    
     * removes callbacks,
     * removes callback helper tasks,
     * removes helper functions, and
     * cleans up global variables.

--- dom/bluetooth/bluedroid/BluetoothUtils.h
+++ dom/bluetooth/bluedroid/BluetoothUtils.h
@@ -19,15 +19,8 @@ class BluetoothValue;
 class BluetoothReplyRunnable;
 
 void
-StringToBdAddressType(const nsAString& aBdAddress,
-                      bt_bdaddr_t *aRetBdAddressType);
-
-void
 BdAddressTypeToString(bt_bdaddr_t* aBdAddressType,
                       nsAString& aRetBdAddress);
-
-uint16_t
-UuidToServiceClassInt(bt_uuid_t* p_uuid);
 
 uint16_t
 UuidToServiceClassInt(const BluetoothUuid& mUuid);
