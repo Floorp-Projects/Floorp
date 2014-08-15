31975c6dab26860ee69c4386af5324d3c8bd1892
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2014-08-15 16:51:55 -0700

    Bug 1048526 - Create a Loop test server for querying OAuth URL parameters to use. r=ckarlof,vladikoff,jaws

---
+++ browser/components/loop/test/mochitest/browser_loop_fxa_server.js
@@ -0,0 +1,72 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Test the server mocking FxA integration endpoints on the Loop server.
+ */
+
+"use strict";
+
+const BASE_URL = "http://mochi.test:8888/browser/browser/components/loop/test/mochitest/loop_fxa.sjs?";
+
+registerCleanupFunction(function* () {
+  yield promiseDeletedOAuthParams(BASE_URL);
+});
+
+add_task(function* required_setup_params() {
+  let params = {
+    client_id: "my_client_id",
+    content_uri: "https://example.com/content/",
+    oauth_uri: "https://example.com/oauth/",
+    profile_uri: "https://example.com/profile/",
+    state: "my_state",
+  };
+  let request = yield promiseOAuthParamsSetup(BASE_URL, params);
+  is(request.status, 200, "Check /setup_params status");
+  request = yield promiseParams();
+  is(request.status, 200, "Check /fxa-oauth/params status");
+  for (let param of Object.keys(params)) {
+    is(request.response[param], params[param], "Check /fxa-oauth/params " + param);
+  }
+});
+
+add_task(function* optional_setup_params() {
+  let params = {
+    action: "signin",
+    client_id: "my_client_id",
+    content_uri: "https://example.com/content/",
+    oauth_uri: "https://example.com/oauth/",
+    profile_uri: "https://example.com/profile/",
+    scope: "profile",
+    state: "my_state",
+  };
+  let request = yield promiseOAuthParamsSetup(BASE_URL, params);
+  is(request.status, 200, "Check /setup_params status");
+  request = yield promiseParams();
+  is(request.status, 200, "Check /fxa-oauth/params status");
+  for (let param of Object.keys(params)) {
+    is(request.response[param], params[param], "Check /fxa-oauth/params " + param);
+  }
+});
+
+add_task(function* delete_setup_params() {
+  yield promiseDeletedOAuthParams(BASE_URL);
+  let request = yield promiseParams();
+  is(Object.keys(request.response).length, 0, "Params should have been deleted");
+});
+
+function promiseParams() {
+  let deferred = Promise.defer();
+  let xhr = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].
+              createInstance(Ci.nsIXMLHttpRequest);
+  xhr.open("POST", BASE_URL + "/fxa-oauth/params", true);
+  xhr.responseType = "json";
+  xhr.addEventListener("load", () => {
+    info("/fxa-oauth/params response:\n" + JSON.stringify(xhr.response, null, 4));
+    deferred.resolve(xhr);
+  });
+  xhr.addEventListener("error", error => deferred.reject(error));
+  xhr.send();
+
+  return deferred.promise;
+}
