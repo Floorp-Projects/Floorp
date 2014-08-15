f8b190410244a6689fb10341fb27184c0d9239d6
Wes Johnston <wjohnston@mozilla.com>
2014-08-14 23:17:04 -0700

    Bug 732169 - Associate prompts with tabs. r=lucasr

--- mobile/android/base/Tabs.java
+++ mobile/android/base/Tabs.java
@@ -63,6 +63,8 @@ public class Tabs implements GeckoEventListener {
 
     private static final long PERSIST_TABS_AFTER_MILLISECONDS = 1000 * 5;
 
+    public static final int INVALID_TAB_ID = -1;
+
     private static AtomicInteger sTabId = new AtomicInteger(0);
     private volatile boolean mInitialTabsAdded;
 
