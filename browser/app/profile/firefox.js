657c4ada6fcde4d0d67f731b3ddf5c6bc48382dc
Mark Banner <standard8@mozilla.com>
2014-08-15 22:54:29 +0100

    Bug 1022594 Part 2. Desktop client needs ability to decline an incoming call - set up a basic websocket protocol and use for both desktop and standalone UI. r=dmose

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1582,6 +1582,7 @@ pref("loop.retry_delay.start", 60000);
 pref("loop.retry_delay.limit", 300000);
 pref("loop.feedback.baseUrl", "https://input.mozilla.org/api/v1/feedback");
 pref("loop.feedback.product", "Loop");
+pref("loop.debug.websocket", false);
 
 // serverURL to be assigned by services team
 pref("services.push.serverURL", "wss://push.services.mozilla.com/");
