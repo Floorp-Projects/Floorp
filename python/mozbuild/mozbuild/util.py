da7087f252f583efc2fa83ccf862d9dcc60eea11
Mike Hommey <mh+mozilla@glandium.org>
2014-08-15 13:52:17 +0900

    Bug 1053080 - Improve mozbuild.util.memoize and add memoized_property. r=gps

--- python/mozbuild/mozbuild/util.py
+++ python/mozbuild/mozbuild/util.py
@@ -10,6 +10,7 @@ from __future__ import unicode_literals
 import copy
 import difflib
 import errno
+import functools
 import hashlib
 import os
 import stat
@@ -20,7 +21,6 @@ from collections import (
     defaultdict,
     OrderedDict,
 )
-from functools import wraps
 from StringIO import StringIO
 
 
@@ -723,12 +723,44 @@ class OrderedDefaultDict(OrderedDict):
             return value
 
 
-def memoize(func):
-    cache = {}
+class memoize(dict):
+    '''A decorator to memoize the results of function calls depending
+    on its arguments.
+    Both functions and instance methods are handled, although in the
+    instance method case, the results are cache in the instance itself.
+    '''
+    def __init__(self, func):
+        self.func = func
+        functools.update_wrapper(self, func)
 
-    @wraps(func)
-    def wrapper(*args):
+    def __call__(self, *args):
+        if args not in self:
+            self[args] = self.func(*args)
+        return self[args]
+
+    def method_call(self, instance, *args):
+        name = '_%s' % self.func.__name__
+        if not hasattr(instance, name):
+            setattr(instance, name, {})
+        cache = getattr(instance, name)
         if args not in cache:
-            cache[args] = func(*args)
+            cache[args] = self.func(instance, *args)
         return cache[args]
-    return wrapper
+
+    def __get__(self, instance, cls):
+        return functools.update_wrapper(
+            functools.partial(self.method_call, instance), self.func)
+
+
+class memoized_property(object):
+    '''A specialized version of the memoize decorator that works for
+    class instance properties.
+    '''
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, cls):
+        name = '_%s' % self.func.__name__
+        if not hasattr(instance, name):
+            setattr(instance, name, self.func(instance))
+        return getattr(instance, name)
