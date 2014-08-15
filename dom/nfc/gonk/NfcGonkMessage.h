989e862d77982b6070efdc39a37132e6622fb886
Yoshi Huang <allstars.chh@mozilla.com>
2014-08-15 10:04:51 +0800

    Bug 1053116 - Part 2: add error code in NfcGonkMessages.h. r=dimi

--- dom/nfc/gonk/NfcGonkMessage.h
+++ dom/nfc/gonk/NfcGonkMessage.h
@@ -11,33 +11,68 @@ namespace mozilla {
 #define NFCD_MINOR_VERSION 7
 
 enum NfcRequest {
-  eNfcRequest_Config = 0,
-  eNfcRequest_Connect,
-  eNfcRequest_Close,
-  eNfcRequest_GetDetailsNDEF,
-  eNfcRequest_ReadNDEF,
-  eNfcRequest_WriteNDEF,
-  eNfcRequest_MakeReadOnlyNDEF,
+  ConfigReq = 0,
+  ConnectReq,
+  CloseReq,
+  GetDetailsNDEFReq,
+  ReadNDEFReq,
+  WriteNDEFReq,
+  MakeReadOnlyNDEFReq,
 };
 
 enum NfcResponse {
-  eNfcResponse_General = 1000,
-  eNfcResponse_Config,
-  eNfcResponse_GetDetailsNDEF,
-  eNfcResponse_ReadNDEF,
+  GeneralRsp = 1000,
+  ConfigRsp,
+  GetDetailsNDEFRsp,
+  ReadNDEFRsp,
 };
 
 enum NfcNotification {
-  eNfcNotification_Initialized = 2000,
-  eNfcNotification_TechDiscovered,
-  eNfcNotification_TechLost,
+  Initialized = 2000,
+  TechDiscovered,
+  TechLost,
 };
 
 enum NfcTechlogy {
-  eNfcTechlogy_NDEF = 0,
-  eNfcTechlogy_NDEFWritable,
-  eNfcTechlogy_NDEFFormattable,
-  eNfcTechlogy_P2P,
+  NDEF = 0,
+  NDEFWritable,
+  NDEFFormattable,
+  P2P,
+};
+
+enum NfcErrorCode {
+  Success = 0,
+  IOErr = -1,
+  Cancelled = -2,
+  Timeout = -3,
+  BusyErr = -4,
+  ConnectErr = -5,
+  DisconnectErr = -6,
+  ReadErr = -7,
+  WriteErr = -8,
+  InvalidParam = -9,
+  InsufficientResources = -10,
+  SocketCreation = -11,
+  SocketNotConnected = -12,
+  BufferTooSmall = -13,
+  SapUsed = -14,
+  ServiceNameUsed = -15,
+  SocketOptions = -16,
+  FailEnableDiscovery = -17,
+  FailDisableDiscovery = -18,
+  NotInitialized = -19,
+  InitializeFail = -20,
+  DeinitializeFail = -21,
+  SeConnected = -22,
+  NoSeConnected = -23,
+  NotSupported = -24,
+  BadSessionId = -25,
+  LostTech = -26,
+  BadTechType = -27,
+  SelectSeFail = -28,
+  DeselectSeFail = -29,
+  FailEnableLowPowerMode = -30,
+  FailDisableLowPowerMode = -31,
 };
 
 } // namespace mozilla
