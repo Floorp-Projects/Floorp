d258a184af21d867338a4152fd8f7d62098b0a50
ffxbld <>
2014-08-16 03:15:25 -0700

    No bug, Automated HPKP preload list update from host bld-linux64-spot-329 - a=hpkp-update

--- security/manager/boot/src/StaticHPKPins.h
+++ security/manager/boot/src/StaticHPKPins.h
@@ -736,6 +736,7 @@ struct TransportSecurityPreload {
 
 /* Sort hostnames for binary search. */
 static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
+  { "2mdn.net", true, false, false, -1, &kPinset_google_root_pems },
   { "accounts.firefox.com", true, false, false, 4, &kPinset_mozilla_fxa },
   { "accounts.google.com", true, false, false, -1, &kPinset_google_root_pems },
   { "addons.mozilla.net", true, false, true, 2, &kPinset_mozilla },
@@ -1060,12 +1061,13 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
   { "www.twitter.com", true, false, false, -1, &kPinset_twitterCom },
   { "xbrlsuccess.appspot.com", true, false, false, -1, &kPinset_google_root_pems },
   { "youtu.be", true, false, false, -1, &kPinset_google_root_pems },
+  { "youtube-nocookie.com", true, false, false, -1, &kPinset_google_root_pems },
   { "youtube.com", true, false, false, -1, &kPinset_google_root_pems },
   { "ytimg.com", true, false, false, -1, &kPinset_google_root_pems },
 };
 
-// Pinning Preload List Length = 326;
+// Pinning Preload List Length = 328;
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1416046032356000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1416650879721000);
