6aad25a1b05b312c917ae0cf16fd0ff27fa5207b
Kannan Vijayan <kvijayan@mozilla.com>
2014-08-17 15:58:16 -0400

    Bug 1054340 - Remove PcOffset IR instructions. r=h4writer

--- js/src/jit/Lowering.h
+++ js/src/jit/Lowering.h
@@ -141,7 +141,6 @@ class LIRGenerator : public LIRGeneratorSpecific
     bool visitFromCharCode(MFromCharCode *ins);
     bool visitStringSplit(MStringSplit *ins);
     bool visitStart(MStart *start);
-    bool visitPcOffset(MPcOffset *pcOffset);
     bool visitOsrEntry(MOsrEntry *entry);
     bool visitNop(MNop *nop);
     bool visitLimitedTruncate(MLimitedTruncate *nop);
