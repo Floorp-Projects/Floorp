0cddedc732e1d61377c78ae7cd4224ccfa6184e4
Lucas Rocha <lucasr@mozilla.com>
2014-08-18 10:40:09 +0100

    Bug 1054268 - Show search icon in about:home (r=margaret)

--- mobile/android/base/AboutPages.java
+++ mobile/android/base/AboutPages.java
@@ -55,8 +55,6 @@ public class AboutPages {
     }
 
     private static final String[] DEFAULT_ICON_PAGES = new String[] {
-        HOME,
-
         ADDONS,
         CONFIG,
         DOWNLOADS,
@@ -72,10 +70,15 @@ public class AboutPages {
         return DEFAULT_ICON_PAGES;
     }
 
-    public static boolean isDefaultIconPage(final String url) {
+    public static boolean isBuiltinIconPage(final String url) {
         if (url == null ||
             !url.startsWith("about:")) {
             return false;
+        }
+
+        // about:home uses a separate search built-in icon.
+        if (isAboutHome(url)) {
+            return true;
         }
 
         // TODO: it'd be quicker to not compare the "about:" part every time.
