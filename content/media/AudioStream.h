3bd84ad2afd489fdfb69b95bd78b888ff13659f5
Nathan Froyd <froydnj@mozilla.com>
2014-08-18 13:12:08 -0400

    Bug 1055141 - use a UniquePtr for AudioStream's mCubebStream member; r=kinetik

--- content/media/AudioStream.h
+++ content/media/AudioStream.h
@@ -8,28 +8,30 @@
 
 #include "AudioSampleFormat.h"
 #include "nsAutoPtr.h"
-#include "nsAutoRef.h"
 #include "nsCOMPtr.h"
 #include "nsThreadUtils.h"
 #include "Latency.h"
 #include "mozilla/dom/AudioChannelBinding.h"
-#include "mozilla/StaticMutex.h"
 #include "mozilla/RefPtr.h"
+#include "mozilla/StaticMutex.h"
+#include "mozilla/UniquePtr.h"
 
 #include "cubeb/cubeb.h"
-
-template <>
-class nsAutoRefTraits<cubeb_stream> : public nsPointerRefTraits<cubeb_stream>
-{
-public:
-  static void Release(cubeb_stream* aStream) { cubeb_stream_destroy(aStream); }
-};
 
 namespace soundtouch {
 class SoundTouch;
 }
 
 namespace mozilla {
+
+template<>
+struct DefaultDelete<cubeb_stream>
+{
+  void operator()(cubeb_stream* aStream) const
+  {
+    cubeb_stream_destroy(aStream);
+  }
+};
 
 class AudioStream;
 class FrameHistory;
@@ -387,9 +389,8 @@ private:
   // frames.
   CircularByteBuffer mBuffer;
 
-  // Owning reference to a cubeb_stream.  cubeb_stream_destroy is called by
-  // nsAutoRef's destructor.
-  nsAutoRef<cubeb_stream> mCubebStream;
+  // Owning reference to a cubeb_stream.
+  UniquePtr<cubeb_stream> mCubebStream;
 
   uint32_t mBytesPerFrame;
 
