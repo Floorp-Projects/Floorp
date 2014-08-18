f19d15e618aa9f99d4d753c63c139e1d2766855d
Nicholas Hurley <hurley@todesschaf.org>
2014-08-18 14:59:32 -0700

    Bug 1054173 - Do not fail connections when receiving request headers. r=mcmanus

--- netwerk/protocol/http/Http2Stream.cpp
+++ netwerk/protocol/http/Http2Stream.cpp
@@ -848,7 +848,7 @@ Http2Stream::ConvertResponseHeaders(Http2Decompressor *decompressor,
   nsresult rv =
     decompressor->DecodeHeaderBlock(reinterpret_cast<const uint8_t *>(aHeadersIn.BeginReading()),
                                     aHeadersIn.Length(),
-                                    aHeadersOut);
+                                    aHeadersOut, false);
   if (NS_FAILED(rv)) {
     LOG3(("Http2Stream::ConvertHeaders %p decode Error\n", this));
     return NS_ERROR_ILLEGAL_VALUE;
@@ -902,7 +902,7 @@ Http2Stream::ConvertPushHeaders(Http2Decompressor *decompressor,
   nsresult rv =
     decompressor->DecodeHeaderBlock(reinterpret_cast<const uint8_t *>(aHeadersIn.BeginReading()),
                                     aHeadersIn.Length(),
-                                    aHeadersOut);
+                                    aHeadersOut, true);
   if (NS_FAILED(rv)) {
     LOG3(("Http2Stream::ConvertPushHeaders %p Error\n", this));
     return NS_ERROR_ILLEGAL_VALUE;
