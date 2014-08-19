8a0e340d462b1c99fb3a15442a2e35daaee88266
Tom Schuster <evilpies@gmail.com>
2014-08-19 12:42:13 +0200

    Bug 792439 - Implement ES6 WeakSet. r=till

---
+++ js/src/jit-test/tests/collections/WeakSet-delete.js
@@ -0,0 +1,28 @@
+var ws = new WeakSet;
+
+// Delete on empty
+assertEq(ws.delete({}), false);
+
+// Delete existing
+var value = {};
+ws.add(value);
+assertEq(ws.has(value), true);
+assertEq(ws.delete(value), true);
+assertEq(ws.has(value), false);
+
+// Delete after clear
+ws.add(value);
+assertEq(ws.has(value), true);
+ws.clear();
+assertEq(ws.delete(value), false);
+assertEq(ws.has(value), false);
+
+// Delete non-empty
+for (var i = 0; i < 10; i++)
+    ws.add({});
+assertEq(ws.add(value), ws);
+assertEq(ws.has(value), true);
+assertEq(ws.delete(value), true);
+assertEq(ws.has(value), false);
+assertEq(ws.delete(value), false);
+assertEq(ws.has(value), false);
