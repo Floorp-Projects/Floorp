054c8703005733d794ba820aa10faf783250b835
Monica Chew <mmc@mozilla.com>
2014-08-19 11:11:10 -0700

    Bug 1055670: Disable remote lookups (r=gcp,a=lmandel)

--- toolkit/components/downloads/test/unit/test_app_rep.js
+++ toolkit/components/downloads/test/unit/test_app_rep.js
@@ -164,21 +164,6 @@ add_test(function test_nullCallback() {
   }
 });
 
-add_test(function test_disabled() {
-  let counts = get_telemetry_counts();
-  Services.prefs.setCharPref("browser.safebrowsing.appRepURL", "");
-  gAppRep.queryReputation({
-    sourceURI: createURI("http://example.com"),
-    fileSize: 12,
-  }, function onComplete(aShouldBlock, aStatus) {
-    // We should be getting NS_ERROR_NOT_AVAILABLE if the service is disabled
-    do_check_eq(Cr.NS_ERROR_NOT_AVAILABLE, aStatus);
-    do_check_false(aShouldBlock);
-    check_telemetry(counts.total + 1, counts.shouldBlock, counts.listCounts);
-    run_next_test();
-  });
-});
-
 // Set up the local whitelist.
 add_test(function test_local_list() {
   // Construct a response with redirect urls.
