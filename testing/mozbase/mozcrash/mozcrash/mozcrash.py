58c37989aa91b59787f6c7669e832b22d661bcc2
James Graham <james@hoppipolla.co.uk>
2014-08-19 16:15:19 +0100

    Bug 1055521 - Expose mozcrash.log_crashes for structured logging of crashes, r=ted.mielczarek

--- testing/mozbase/mozcrash/mozcrash/mozcrash.py
+++ testing/mozbase/mozcrash/mozcrash/mozcrash.py
@@ -2,8 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-__all__ = ['check_for_crashes',
-           'check_for_java_exception']
+__all__ = [
+    'check_for_crashes',
+    'check_for_java_exception',
+    'log_crashes'
+]
 
 import glob
 import os
