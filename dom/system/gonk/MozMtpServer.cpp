ab397217a204e04acd6cbcad347599a32e5d6a1a
Dave Hylands <dhylands@mozilla.com>
2014-08-19 13:41:35 -0700

    Bug 1049240 - Integrate MTP with the AutoMounter. r=echou

--- dom/system/gonk/MozMtpServer.cpp
+++ dom/system/gonk/MozMtpServer.cpp
@@ -16,46 +16,34 @@
 #include <unistd.h>
 
 #include <cutils/properties.h>
+#include <private/android_filesystem_config.h>
 
+#include "base/message_loop.h"
 #include "mozilla/FileUtils.h"
 #include "mozilla/Scoped.h"
+#include "mozilla/StaticPtr.h"
+#include "nsAutoPtr.h"
 #include "nsThreadUtils.h"
+#include "nsXULAppAPI.h"
+
+#include "Volume.h"
 
 using namespace android;
 using namespace mozilla;
-USING_MTP_NAMESPACE
+BEGIN_MTP_NAMESPACE
 
 class MtpServerRunnable : public nsRunnable
 {
 public:
+  MtpServerRunnable(int aMtpUsbFd, MozMtpServer* aMozMtpServer)
+    : mMozMtpServer(aMozMtpServer),
+      mMtpUsbFd(aMtpUsbFd)
+  {
+  }
+
   nsresult Run()
   {
-    const char *mtpUsbFilename = "/dev/mtp_usb";
-    const char *productName = "FirefoxOS";
-    const char *storageDir = "/storage/sdcard";
-
-    mFd = open(mtpUsbFilename, O_RDWR);
-    if (mFd.get() < 0) {
-      MTP_LOG("open of '%s' failed", mtpUsbFilename);
-      return NS_OK;
-    }
-
-    MTP_LOG("MozMtpServer open done, fd: %d. Start reading.", mFd.get());
-
-    ScopedDeletePtr<MozMtpDatabase> database;
-    ScopedDeletePtr<MtpServer> server;
-    ScopedDeletePtr<MtpStorage> storage;
-
-    database = new MozMtpDatabase(storageDir);
-    server = new MtpServer(mFd.get(), database, false, 1023, 0664, 0775);
-    storage = new MtpStorage(MTP_STORAGE_FIXED_RAM,       // id
-                             storageDir,                  // filePath
-                             productName,                 // description
-                             100uLL * 1024uLL * 1024uLL,  // reserveSpace
-                             false,                       // removable
-                             2uLL * 1024uLL * 1024uLL * 1024uLL); // maxFileSize
-
-    server->addStorage(storage);
+    nsRefPtr<RefCountedMtpServer> server = mMozMtpServer->GetMtpServer();
 
     MTP_LOG("MozMtpServer started");
     server->run();
@@ -65,16 +53,51 @@ public:
   }
 
 private:
-  ScopedClose mFd;
+  nsRefPtr<MozMtpServer> mMozMtpServer;
+  ScopedClose mMtpUsbFd; // We want to hold this open while the server runs
 };
+
+already_AddRefed<RefCountedMtpServer>
+MozMtpServer::GetMtpServer()
+{
+  nsRefPtr<RefCountedMtpServer> server = mMtpServer;
+  return server.forget();
+}
+
+already_AddRefed<MozMtpDatabase>
+MozMtpServer::GetMozMtpDatabase()
+{
+  nsRefPtr<MozMtpDatabase> db = mMozMtpDatabase;
+  return db.forget();
+}
 
 void
 MozMtpServer::Run()
 {
+  MOZ_ASSERT(MessageLoop::current() == XRE_GetIOMessageLoop());
+
+  const char *mtpUsbFilename = "/dev/mtp_usb";
+  ScopedClose mtpUsbFd(open(mtpUsbFilename, O_RDWR));
+  if (mtpUsbFd.get() < 0) {
+    MTP_ERR("open of '%s' failed", mtpUsbFilename);
+    return;
+  }
+  MTP_LOG("Opened '%s' fd %d", mtpUsbFilename, mtpUsbFd.get());
+
+  mMozMtpDatabase = new MozMtpDatabase();
+  mMtpServer = new RefCountedMtpServer(mtpUsbFd.get(),        // fd
+                                       mMozMtpDatabase.get(), // MtpDatabase
+                                       false,                 // ptp?
+                                       AID_MEDIA_RW,          // file group
+                                       0664,                  // file permissions
+                                       0775);                 // dir permissions
+
   nsresult rv = NS_NewNamedThread("MtpServer", getter_AddRefs(mServerThread));
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return;
   }
   MOZ_ASSERT(mServerThread);
-  mServerThread->Dispatch(new MtpServerRunnable(), 0);
+  mServerThread->Dispatch(new MtpServerRunnable(mtpUsbFd.forget(), this), NS_DISPATCH_NORMAL);
 }
+
+END_MTP_NAMESPACE
