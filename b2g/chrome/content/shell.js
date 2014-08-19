f69bf9ee7136c7a05d64b465fe492cd8ddc874f7
James Hobin <jhobin@mozilla.com>
2014-08-19 13:50:00 -0400

    Bug 1023201 - Screenshots are missing 1px in height. r=fabrice

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -862,11 +862,15 @@ window.addEventListener('ContentStart', function ss_onContentStart() {
     try {
       var canvas = document.createElementNS('http://www.w3.org/1999/xhtml',
                                             'canvas');
-      var width = window.innerWidth;
-      var height = window.innerHeight;
+      var docRect = document.body.getBoundingClientRect();
+      var width = docRect.width;
+      var height = docRect.height;
+
+      // Convert width and height from CSS pixels (potentially fractional)
+      // to device pixels (integer).
       var scale = window.devicePixelRatio;
-      canvas.setAttribute('width', width * scale);
-      canvas.setAttribute('height', height * scale);
+      canvas.setAttribute('width', Math.round(width * scale));
+      canvas.setAttribute('height', Math.round(height * scale));
 
       var context = canvas.getContext('2d');
       var flags =
