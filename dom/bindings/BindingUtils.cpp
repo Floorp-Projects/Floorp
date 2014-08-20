61163cede40c91e7d71cbbb552bccaf8c56820c6
Bobby Holley <bobbyholley@gmail.com>
2014-08-19 18:12:15 -0700

    Bug 1036214 - Do a subsumes check on object and any parameters (and things containing them) to JS-implemented WebIDL. r=bz, a=sledru

--- dom/bindings/BindingUtils.cpp
+++ dom/bindings/BindingUtils.cpp
@@ -2615,5 +2615,12 @@ AssertReturnTypeMatchesJitinfo(const JSJitInfo* aJitInfo,
 }
 #endif
 
+bool
+CallerSubsumes(JSObject *aObject)
+{
+  nsIPrincipal* objPrin = nsContentUtils::ObjectPrincipal(js::UncheckedUnwrap(aObject));
+  return nsContentUtils::SubjectPrincipal()->Subsumes(objPrin);
+}
+
 } // namespace dom
 } // namespace mozilla
