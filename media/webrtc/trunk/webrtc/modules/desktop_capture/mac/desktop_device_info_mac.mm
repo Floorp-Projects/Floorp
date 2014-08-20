58fc6afcf94d38afc71db12d5f5d2c30fd145b4b
Matthew A. Miller <linuxwolf@outer-planes.net>
2014-08-20 16:05:59 -0600

    Bug 1036653 - Mac-specific support for app sharing, r=jesup,gcp

--- media/webrtc/trunk/webrtc/modules/desktop_capture/mac/desktop_device_info_mac.mm
+++ media/webrtc/trunk/webrtc/modules/desktop_capture/mac/desktop_device_info_mac.mm
@@ -3,7 +3,10 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "webrtc/modules/desktop_capture/mac/desktop_device_info_mac.h"
+#include <AppKit/AppKit.h>
 #include <Cocoa/Cocoa.h>
+#include <unistd.h>
+#include <stdio.h>
 
 namespace webrtc {
 
@@ -25,7 +28,7 @@ DesktopDeviceInfoMac::~DesktopDeviceInfoMac() {
 }
 
 #if !defined(MULTI_MONITOR_SCREENSHARE)
-int32_t DesktopDeviceInfoMac::MultiMonitorScreenshare()
+void DesktopDeviceInfoMac::MultiMonitorScreenshare()
 {
   DesktopDisplayDevice *pDesktopDeviceInfo = new DesktopDisplayDevice;
   if (pDesktopDeviceInfo) {
@@ -35,29 +38,50 @@ int32_t DesktopDeviceInfoMac::MultiMonitorScreenshare()
 
     desktop_display_list_[pDesktopDeviceInfo->getScreenId()] = pDesktopDeviceInfo;
   }
-  return 0;
 }
 #endif
 
-int32_t DesktopDeviceInfoMac::Init() {
+void DesktopDeviceInfoMac::InitializeScreenList() {
 #if !defined(MULTI_MONITOR_SCREENSHARE)
   MultiMonitorScreenshare();
 #endif
-
-  initializeWindowList();
-
-  return 0;
 }
+void DesktopDeviceInfoMac::InitializeApplicationList() {
+  //List all running applications (excluding background processes).
 
-int32_t DesktopDeviceInfoMac::Refresh() {
-#if !defined(MULTI_MONITOR_SCREENSHARE)
-  desktop_display_list_.clear();
-  MultiMonitorScreenshare();
-#endif
+  NSArray *running = [[NSWorkspace sharedWorkspace] runningApplications];
+  for (NSRunningApplication *ra in running) {
+    if (ra.activationPolicy != NSApplicationActivationPolicyRegular)
+      continue;
 
-  RefreshWindowList();
+    ProcessId pid = ra.processIdentifier;
+    if (pid == 0) {
+      continue;
+    }
+    if (pid == getpid()) {
+      continue;
+    }
 
-  return 0;
+    DesktopApplication *pDesktopApplication = new DesktopApplication;
+    if (!pDesktopApplication) {
+      continue;
+    }
+
+    pDesktopApplication->setProcessId(pid);
+
+    NSString *str;
+    str = [ra.executableURL absoluteString];
+    pDesktopApplication->setProcessPathName([str UTF8String]);
+
+    str = ra.localizedName;
+    pDesktopApplication->setProcessAppName([str UTF8String]);
+
+    char idStr[64];
+    snprintf(idStr, sizeof(idStr), "%ld", pDesktopApplication->getProcessId());
+    pDesktopApplication->setUniqueIdName(idStr);
+
+    desktop_application_list_[pDesktopApplication->getProcessId()] = pDesktopApplication;
+  }
 }
 
 } //namespace webrtc
