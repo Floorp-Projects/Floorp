cf6dbf76f6b1e2ea1c887a0ff56beec0f187c73e
Jared Wein <jwein@mozilla.com>
2014-08-21 15:57:41 -0400

    Bug 1007336 - Set lightweight themes in customization mode. r=gijs

---
+++ browser/components/customizableui/test/browser_1007336_lwthemes_in_customize_mode.js
@@ -0,0 +1,60 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+  * License, v. 2.0. If a copy of the MPL was not distributed with this
+  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const DEFAULT_THEME_ID = "{972ce4c6-7e08-4474-a285-3208198ce6fd}";
+
+add_task(function () {
+  Services.prefs.clearUserPref("lightweightThemes.usedThemes");
+  Services.prefs.clearUserPref("lightweightThemes.recommendedThemes");
+
+  yield startCustomizing();
+
+  let themesButton = document.getElementById("customization-lwtheme-button");
+  let popup = document.getElementById("customization-lwtheme-menu");
+
+  let popupShownPromise = popupShown(popup);
+  EventUtils.synthesizeMouseAtCenter(themesButton, {});
+  info("Clicked on themes button");
+  yield popupShownPromise;
+
+  let header = document.getElementById("customization-lwtheme-menu-header");
+  let recommendedHeader = document.getElementById("customization-lwtheme-menu-recommended");
+
+  is(header.nextSibling.nextSibling, recommendedHeader,
+     "There should only be one theme (default) in the 'My Themes' section by default");
+  is(header.nextSibling.theme.id, DEFAULT_THEME_ID, "That theme should be the default theme");
+
+  let firstLWTheme = recommendedHeader.nextSibling;
+  let firstLWThemeId = firstLWTheme.theme.id;
+  let themeChangedPromise = promiseObserverNotified("lightweight-theme-changed");
+  firstLWTheme.doCommand();
+  info("Clicked on first theme");
+  yield themeChangedPromise;
+
+  popupShownPromise = popupShown(popup);
+  EventUtils.synthesizeMouseAtCenter(themesButton, {});
+  info("Clicked on themes button");
+  yield popupShownPromise;
+
+  is(header.nextSibling.theme.id, DEFAULT_THEME_ID, "The first theme should be the Default theme");
+  let installedThemeId = header.nextSibling.nextSibling.theme.id;
+  ok(installedThemeId.startsWith(firstLWThemeId),
+     "The second theme in the 'My Themes' section should be the newly installed theme: " +
+     "Installed theme id: " + installedThemeId + "; First theme ID: " + firstLWThemeId);
+  is(header.nextSibling.nextSibling.nextSibling, recommendedHeader,
+     "There should be two themes in the 'My Themes' section");
+
+  let defaultTheme = header.nextSibling;
+  defaultTheme.doCommand();
+  is(Services.prefs.getBoolPref("lightweightThemes.isThemeSelected"), false, "No lwtheme should be selected");
+});
+
+add_task(function asyncCleanup() {
+  yield endCustomizing();
+
+  Services.prefs.clearUserPref("lightweightThemes.usedThemes");
+  Services.prefs.clearUserPref("lightweightThemes.recommendedThemes");
+})
