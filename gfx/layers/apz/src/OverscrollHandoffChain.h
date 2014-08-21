f15645d84872841ebcf3433a2c5936fac264a9d9
Botond Ballo <botond@mozilla.com>
2014-08-21 15:27:51 -0400

    Bug 1049109 - Clear overscroll along entire handoff chain at end of pinch. r=kats

--- gfx/layers/apz/src/OverscrollHandoffChain.h
+++ gfx/layers/apz/src/OverscrollHandoffChain.h
@@ -94,6 +94,9 @@ public:
   // Cancel animations all the way up the chain.
   void CancelAnimations() const;
 
+  // Clear overscroll all the way up the chain.
+  void ClearOverscroll() const;
+
   // Snap back the APZC that is overscrolled, if any.
   void SnapBackOverscrolledApzc() const;
 
@@ -102,6 +105,9 @@ public:
   bool CanBePanned(const AsyncPanZoomController* aApzc) const;
 private:
   std::vector<nsRefPtr<AsyncPanZoomController>> mChain;
+
+  typedef void (AsyncPanZoomController::*APZCMethod)();
+  void ForEachApzc(APZCMethod aMethod) const;
 };
 
 // Don't pollute other files with this macro for now.
