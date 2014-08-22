a60abf19dae2e824aabfba968d316d44a97610d9
Chris Pearce <cpearce@mozilla.com>
2014-08-22 15:56:54 +1200

    Bug 1057196 - Make MediaCache cache size bigger for mochitests. r=kinetik

--- testing/profiles/prefs_general.js
+++ testing/profiles/prefs_general.js
@@ -35,7 +35,7 @@ user_pref("dom.min_background_timeout_value", 1000);
 user_pref("test.mousescroll", true);
 user_pref("security.default_personal_cert", "Select Automatically"); // Need to client auth test be w/o any dialogs
 user_pref("network.http.prompt-temp-redirect", false);
-user_pref("media.cache_size", 100);
+user_pref("media.cache_size", 1000);
 user_pref("media.volume_scale", "0.01");
 user_pref("security.warn_viewing_mixed", false);
 user_pref("app.update.enabled", false);
