4b4225411e4ea42f5a4010d647c80e386ca720b7
Ted Clancy <tclancy@mozilla.com>
2014-08-23 14:21:00 +0200

    Date:   Tue Aug 12 08:59:47 2014 -0400
    Bug 1055427 - Part 1: Adding KillChild() method to nsIMessageManager. r=sicking

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -161,6 +161,7 @@ public:
     virtual bool CheckManifestURL(const nsAString& aManifestURL) MOZ_OVERRIDE;
     virtual bool CheckAppHasPermission(const nsAString& aPermission) MOZ_OVERRIDE;
     virtual bool CheckAppHasStatus(unsigned short aStatus) MOZ_OVERRIDE;
+    virtual bool KillChild() MOZ_OVERRIDE;
 
     /** Notify that a tab is beginning its destruction sequence. */
     void NotifyTabDestroying(PBrowserParent* aTab);
