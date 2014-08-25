181e149b106c16d4c24d5a9150ce98fd99851cf7
Jeff Muizelaar <jmuizelaar@mozilla.com>
2014-08-25 11:09:39 -0400

    Bug 1048916. Convert a bunch of layers from nsAutoPtr to UniquePtr. r=roc
    This makes the ownership of LayerProperties more clear.

--- gfx/layers/LayerTreeInvalidation.h
+++ gfx/layers/LayerTreeInvalidation.h
@@ -7,6 +7,7 @@
 #define GFX_LAYER_TREE_INVALIDATION_H
 
 #include "nsRegion.h"                   // for nsIntRegion
+#include "mozilla/UniquePtr.h"          // for UniquePtr
 
 class nsPresContext;
 struct nsIntPoint;
@@ -42,7 +43,7 @@ struct LayerProperties
    * @param Layer tree to copy, or nullptr if we have no 
    * initial layer tree.
    */
-  static LayerProperties* CloneFrom(Layer* aRoot);
+  static UniquePtr<LayerProperties> CloneFrom(Layer* aRoot);
 
   /**
    * Clear all invalidation status from this layer tree.
