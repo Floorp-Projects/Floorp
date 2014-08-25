e6c5a9202f68ef64f20f21c0f7a4c9147cfad166
Johnny Stenback <jst@mozilla.com>
2014-08-25 16:21:35 -0700

    Bug 798188. Use NS_WRAPPERCACHE_INTERFACE_TABLE_ENTRY in table based QI implementations. r=peterv
    ---
     content/base/src/Attr.cpp                                  | 2 +-
     content/base/src/DOMRect.cpp                               | 2 +-
     content/base/src/FragmentOrElement.cpp                     | 2 +-
     content/base/src/nsContentList.cpp                         | 2 +-
     content/base/src/nsDOMAttributeMap.cpp                     | 2 +-
     content/base/src/nsDocument.cpp                            | 2 +-
     content/html/content/src/HTMLFormControlsCollection.cpp    | 2 +-
     content/html/content/src/HTMLOptionsCollection.cpp         | 2 +-
     content/html/content/src/HTMLPropertiesCollection.cpp      | 4 ++--
     content/html/content/src/HTMLTableElement.cpp              | 2 +-
     content/media/webspeech/synth/SpeechSynthesisUtterance.cpp | 1 -
     dom/events/PaintRequest.cpp                                | 2 +-
     dom/xbl/XBLChildrenElement.cpp                             | 2 +-
     13 files changed, 13 insertions(+), 14 deletions(-)

--- content/html/content/src/HTMLPropertiesCollection.cpp
+++ content/html/content/src/HTMLPropertiesCollection.cpp
@@ -61,7 +61,7 @@ HTMLPropertiesCollection::~HTMLPropertiesCollection()
 }
 
 NS_INTERFACE_TABLE_HEAD(HTMLPropertiesCollection)
-    NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+    NS_WRAPPERCACHE_INTERFACE_TABLE_ENTRY
     NS_INTERFACE_TABLE(HTMLPropertiesCollection,
                        nsIDOMHTMLCollection,
                        nsIHTMLCollection,
@@ -408,7 +408,7 @@ NS_IMPL_CYCLE_COLLECTING_ADDREF(PropertyNodeList)
 NS_IMPL_CYCLE_COLLECTING_RELEASE(PropertyNodeList)
 
 NS_INTERFACE_TABLE_HEAD(PropertyNodeList)
-    NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+    NS_WRAPPERCACHE_INTERFACE_TABLE_ENTRY
     NS_INTERFACE_TABLE(PropertyNodeList,
                        nsIDOMNodeList,
                        nsINodeList,
