b9185ac3aa28ce3667751462e326d156e6a4056f
Terrence Cole <terrence@mozilla.com>
2014-08-25 09:29:51 -0700

    Bug 1057393 - Fix a regression on octane-zlib; r=sfink

--- js/src/vm/MallocProvider.h
+++ js/src/vm/MallocProvider.h
@@ -59,17 +59,8 @@ struct MallocProvider
 
     template <class T>
     T *pod_malloc(size_t numElems) {
-        T *p = js_pod_malloc<T>(numElems);
-        if (MOZ_LIKELY(p)) {
-            client()->updateMallocCounter(numElems * sizeof(T));
-            return p;
-        }
-        if (numElems & mozilla::tl::MulOverflowMask<sizeof(T)>::value) {
-            client()->reportAllocationOverflow();
-            return nullptr;
-        }
-        client()->onOutOfMemory(nullptr, numElems * sizeof(T));
-        return nullptr;
+        MOZ_ASSERT(numElems > 0);
+        return pod_malloc_with_extra<T, T>(numElems - 1);
     }
 
     template <class T, class U>
@@ -83,7 +74,7 @@ struct MallocProvider
             client()->reportAllocationOverflow();
             return nullptr;
         }
-        T *p = (T *)js_pod_malloc<uint8_t>(bytes);
+        T *p = reinterpret_cast<T *>(js_pod_malloc<uint8_t>(bytes));
         if (MOZ_LIKELY(p)) {
             client()->updateMallocCounter(bytes);
             return p;
@@ -104,23 +95,29 @@ struct MallocProvider
     }
 
     template <class T>
-    T *
-    pod_calloc(size_t numElems) {
-        T *p = pod_malloc<T>(numElems);
-        if (MOZ_UNLIKELY(!p))
-            return nullptr;
-        memset(p, 0, numElems * sizeof(T));
-        return p;
+    T *pod_calloc(size_t numElems) {
+        MOZ_ASSERT(numElems > 0);
+        return pod_calloc_with_extra<T, T>(numElems - 1);
     }
 
     template <class T, class U>
-    T *
-    pod_calloc_with_extra(size_t numExtra) {
-        T *p = pod_malloc_with_extra<T, U>(numExtra);
-        if (MOZ_UNLIKELY(!p))
+    T *pod_calloc_with_extra(size_t numExtra) {
+        if (numExtra & mozilla::tl::MulOverflowMask<sizeof(U)>::value) {
+            client()->reportAllocationOverflow();
             return nullptr;
-        memset(p, 0, sizeof(T) + numExtra * sizeof(U));
-        return p;
+        }
+        size_t bytes = sizeof(T) + numExtra * sizeof(U);
+        if (bytes < sizeof(T)) {
+            client()->reportAllocationOverflow();
+            return nullptr;
+        }
+        T *p = reinterpret_cast<T *>(js_pod_calloc<uint8_t>(bytes));
+        if (MOZ_LIKELY(p)) {
+            client()->updateMallocCounter(bytes);
+            return p;
+        }
+        client()->onOutOfMemory(nullptr, bytes);
+        return nullptr;
     }
 
     template <class T>
