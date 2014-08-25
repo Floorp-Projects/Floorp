caa1811bd1a7e6f516472269a854bf004f2af938
Jamin Liu <jaliu@mozilla.com>
2014-08-25 14:51:20 +0800

    Bug 1032755 - Add NS_ERROR_MODULE_DOM_BLUETOOTH to nsError list and allow it to be used to create dom exception. r=smaug, f=dhylands, btian

--- xpcom/base/nsError.h
+++ xpcom/base/nsError.h
@@ -71,6 +71,7 @@
 #define NS_ERROR_MODULE_DOM_FILEHANDLE 34
 #define NS_ERROR_MODULE_SIGNED_JAR 35
 #define NS_ERROR_MODULE_DOM_FILESYSTEM 36
+#define NS_ERROR_MODULE_DOM_BLUETOOTH 37
 
 /* NS_ERROR_MODULE_GENERAL should be used by modules that do not
  * care if return code values overlap. Callers of methods that
