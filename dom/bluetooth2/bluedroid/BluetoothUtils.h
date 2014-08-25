e8291e381314ad448cd6281231919a5e7e636db7
Jamin Liu <jaliu@mozilla.com>
2014-08-25 14:51:29 +0800

    Bug 1032755 - Return a DOMException with error message when BluetoothReplyRunnable rejects the promise. r=btian

--- dom/bluetooth2/bluedroid/BluetoothUtils.h
+++ dom/bluetooth2/bluedroid/BluetoothUtils.h
@@ -38,15 +38,54 @@ bool
 BroadcastSystemMessage(const nsAString& aType,
                        const BluetoothValue& aData);
 
+/**
+ * Dispatch Bluetooth reply to main thread. The reply will contain an error
+ * string if the request fails.
+ *
+ * This function is mainly designed for DOMRequest-based methods which return
+ * 'DOMRequest'. If aErrorStr is not empty, the DOMRequest property 'error.name'
+ * would be updated to aErrorStr right before the callback function 'onerror'
+ * is fired.
+ *
+ * @param aRunnable  the runnable to reply the bluetooth request.
+ * @param aValue     the Bluetooth value which is used to reply a Bluetooth
+ *                   request when the request finished successfully.
+ * @param aErrorStr  the error string which is used to reply a Bluetooth
+ *                   request when the request failed.
+ */
 void
 DispatchBluetoothReply(BluetoothReplyRunnable* aRunnable,
                        const BluetoothValue& aValue,
                        const nsAString& aErrorStr);
 
+/**
+ * Dispatch Bluetooth reply to main thread. The reply will contain an error
+ * status if the request fails.
+ *
+ * This function mainly designed for Bluetooth APIs which return 'Promise'.
+ * If aStatusCode is not STATUS_SUCCESS, the Promise would reject with an
+ * Exception object.
+ * The ns error used by Exception will be associated with aStatusCode.
+ * The name and messege of Exception are defined in dom/base/domerr.msg and
+ * will be filled automatically during promise rejection.
+ *
+ * @param aRunnable   the runnable to reply the bluetooth request.
+ * @param aValue      the Bluetooth value which is used to reply a Bluetooth
+ *                    request if the request finished successfully.
+ * @param aStatusCode the error status which is used to reply a Bluetooth
+ *                    request when the request failed.
+ */
+
+void
+DispatchBluetoothReply(BluetoothReplyRunnable* aRunnable,
+                       const BluetoothValue& aValue,
+                       const enum BluetoothStatus aStatusCode);
+
 void
 DispatchStatusChangedEvent(const nsAString& aType,
                            const nsAString& aDeviceAddress,
                            bool aStatus);
+
 
 END_BLUETOOTH_NAMESPACE
 
