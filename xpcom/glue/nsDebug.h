bd92c7e6aade9549eb8a0253355594a0a1e036e3
Botond Ballo <botond@mozilla.com>
2014-08-25 18:28:25 -0400

    Bug 965022 - Use MOZ_FORMAT_PRINTF for [f]printf_stderr, and fix incorrect usages. r=jrmuizel

--- xpcom/glue/nsDebug.h
+++ xpcom/glue/nsDebug.h
@@ -402,12 +402,13 @@ typedef void (*StderrCallback)(const char* aFmt, va_list aArgs);
 extern "C" {
 #endif
 
-NS_COM_GLUE void printf_stderr(const char* aFmt, ...);
+NS_COM_GLUE void printf_stderr(const char* aFmt, ...) MOZ_FORMAT_PRINTF(1, 2);
 
 NS_COM_GLUE void vprintf_stderr(const char* aFmt, va_list aArgs);
 
 // fprintf with special handling for stderr to print to the console
-NS_COM_GLUE void fprintf_stderr(FILE* aFile, const char* aFmt, ...);
+NS_COM_GLUE void fprintf_stderr(FILE* aFile, const char* aFmt, ...)
+    MOZ_FORMAT_PRINTF(2, 3);
 
 // used by the profiler to log stderr in the profiler for more
 // advanced performance debugging and display/layers visualization.
