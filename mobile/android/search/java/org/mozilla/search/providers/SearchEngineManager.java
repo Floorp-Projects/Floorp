dbabc0976887fd46e64898f27804b93fdfc95c51
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-08-25 15:28:11 -0700

    Bug 1054303 - Use open search plugins for search engines. r=bnicholson

--- mobile/android/search/java/org/mozilla/search/providers/SearchEngineManager.java
+++ mobile/android/search/java/org/mozilla/search/providers/SearchEngineManager.java
@@ -10,9 +10,15 @@ import android.os.AsyncTask;
 import android.text.TextUtils;
 import android.util.Log;
 
+import org.mozilla.gecko.AppConstants;
 import org.mozilla.gecko.GeckoSharedPrefs;
+import org.mozilla.gecko.util.GeckoJarReader;
 import org.mozilla.search.Constants;
 import org.mozilla.search.SearchPreferenceActivity;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SearchEngineManager implements SharedPreferences.OnSharedPreferenceChangeListener {
     private static final String LOG_TAG = "SearchEngineManager";
@@ -20,13 +26,6 @@ public class SearchEngineManager implements SharedPreferences.OnSharedPreference
     private Context context;
     private SearchEngineCallback changeCallback;
     private SearchEngine engine;
-
-    // Add new engines to this enum. Also update createInstance, the factory method below.
-    public static enum Engine {
-        BING,
-        GOOGLE,
-        YAHOO
-    }
 
     public static interface SearchEngineCallback {
         public void execute(SearchEngine engine);
@@ -63,73 +62,107 @@ public class SearchEngineManager implements SharedPreferences.OnSharedPreference
         engine = null;
     }
 
+    @Override
+    public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key) {
+        if (!TextUtils.equals(SearchPreferenceActivity.PREF_SEARCH_ENGINE_KEY, key)) {
+            return;
+        }
+        getEngineFromPrefs(changeCallback);
+    }
+
     /**
-     * Manually lookup the current search engine.
+     * Look up the current search engine in shared preferences.
+     * Creates a SearchEngine instance and caches it for use on the main thread.
      *
      * @param callback a SearchEngineCallback to be called after successfully looking
      *                 up the search engine. This will run on the UI thread.
      */
     private void getEngineFromPrefs(final SearchEngineCallback callback) {
-        final AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
+        final AsyncTask<Void, Void, SearchEngine> task = new AsyncTask<Void, Void, SearchEngine>() {
             @Override
-            protected String doInBackground(Void... params) {
-                return GeckoSharedPrefs.forApp(context).getString(SearchPreferenceActivity.PREF_SEARCH_ENGINE_KEY, null);
+            protected SearchEngine doInBackground(Void... params) {
+                final String identifier = GeckoSharedPrefs.forApp(context)
+                        .getString(SearchPreferenceActivity.PREF_SEARCH_ENGINE_KEY, Constants.DEFAULT_SEARCH_ENGINE)
+                        .toLowerCase();
+                return createEngine(identifier);
             }
 
             @Override
-            protected void onPostExecute(String engineName) {
-                updateEngine(engineName);
-                callback.execute(engine);
+            protected void onPostExecute(SearchEngine engine) {
+                // Only touch engine on the main thread.
+                SearchEngineManager.this.engine = engine;
+                if (callback != null) {
+                    callback.execute(engine);
+                }
             }
         };
         task.execute();
     }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (TextUtils.equals(SearchPreferenceActivity.PREF_SEARCH_ENGINE_KEY, key)) {
-            updateEngine(sharedPreferences.getString(key, null));
+    /**
+     * Creates a SearchEngine instance from an open search plugin.
+     * This method does disk I/O, call it from a background thread.
+     *
+     * @param identifier search engine identifier (e.g. "google")
+     * @return SearchEngine instance for identifier
+     */
+    private SearchEngine createEngine(String identifier) {
+        InputStream in = getEngineFromJar(identifier);
 
-            if (changeCallback != null) {
-                changeCallback.execute(engine);
+        // Fallback for standalone search activity.
+        if (in == null) {
+            in = getEngineFromAssets(identifier);
+        }
+
+        if (in == null) {
+            throw new IllegalArgumentException("Couldn't find search engine for identifier: " + identifier);
+        }
+
+        try {
+            try {
+                return new SearchEngine(identifier, in);
+            } finally {
+                in.close();
             }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Exception creating search engine", e);
+        } catch (XmlPullParserException e) {
+            Log.e(LOG_TAG, "Exception creating search engine", e);
+        }
+
+        return null;
+    }
+
+    /**
+     * Fallback for standalone search activity. These assets are not included
+     * in mozilla-central.
+     *
+     * @param identifier search engine identifier (e.g. "google")
+     * @return InputStream for open search plugin XML
+     */
+    private InputStream getEngineFromAssets(String identifier) {
+        try {
+            return context.getResources().getAssets().open(identifier + ".xml");
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Exception getting search engine from assets", e);
+            return null;
         }
     }
 
     /**
-     * Notify the searchEngineChangeListener that the default search engine has changed.
+     * Reads open search plugin XML file from the gecko jar. This will only work
+     * if the search activity is built as part of mozilla-central.
      *
-     * @param engineName The name of the new search engine. This should be a member
-     *                   of SearchEngineFactory.Engine. If null, then it will use the
-     *                   default search engine.
-     * @return true if this caused the engine to be changed.
+     * @param identifier search engine identifier (e.g. "google")
+     * @return InputStream for open search plugin XML
      */
-    private void updateEngine(String engineName) {
+    private InputStream getEngineFromJar(String identifier) {
+        // TODO: Get the real value for this
+        final String locale = "en-US";
 
-        if (TextUtils.isEmpty(engineName)) {
-            engineName = Constants.DEFAULT_SEARCH_ENGINE;
-        }
+        final String path = "!/chrome/" + locale + "/locale/" + locale + "/browser/searchplugins/" + identifier + ".xml";
+        final String url = "jar:jar:file://" + context.getPackageResourcePath() + "!/" + AppConstants.OMNIJAR_NAME + path;
 
-        try {
-            engine = createEngine(engineName);
-        } catch (IllegalArgumentException e) {
-            Log.e(LOG_TAG, "Search engine not found for " + engineName + " reverting to default engine.", e);
-            engine = createEngine(Constants.DEFAULT_SEARCH_ENGINE);
-        }
-    }
-
-    private static SearchEngine createEngine(String engineName) {
-        switch (Engine.valueOf(engineName)) {
-            case BING:
-                return new BingSearchEngine();
-            case GOOGLE:
-                return new GoogleSearchEngine();
-            case YAHOO:
-                return new YahooSearchEngine();
-        }
-
-        // The return statement is unreachable since Engine.valueOf will throw
-        // IllegalArgumentException if engineName cannot be resolved.
-        return null;
+        return GeckoJarReader.getStream(url);
     }
 }
