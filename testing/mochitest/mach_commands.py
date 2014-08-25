be1eaa06fee09f97edd3fe10f697023c2c52385f
Qeole <qeole@outlook.com>
2014-08-25 17:23:34 +0200

    Bug 1045209 - Updating tests for new OpenH264 plugin path handling. r=unfocused,ted,jmaher

--- testing/mochitest/mach_commands.py
+++ testing/mochitest/mach_commands.py
@@ -363,7 +363,7 @@ class MochitestRunner(MozbuildObject):
             if options.gmp_path is None:
                 # Need to fix the location of gmp_fake which might not be shipped in the binary
                 bin_path = self.get_binary_path()
-                options.gmp_path = os.path.join(os.path.dirname(bin_path), 'gmp-fake')
+                options.gmp_path = os.path.join(os.path.dirname(bin_path), 'gmp-fake', '1.0')
 
 
         logger_options = {key: value for key, value in vars(options).iteritems() if key.startswith('log')}
