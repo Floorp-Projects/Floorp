93af528f7b40ee95714994d0364fee11e5d36f41
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-08-25 15:28:10 -0700

    Bug 996753 - Telemetry probes for changing settings and hitting back. r=liuche, a=sledru

--- mobile/android/base/preferences/GeckoPreferences.java
+++ mobile/android/base/preferences/GeckoPreferences.java
@@ -471,6 +471,8 @@ OnSharedPreferenceChangeListener
         if (NO_TRANSITIONS) {
             overridePendingTransition(0, 0);
         }
+
+        Telemetry.sendUIEvent(TelemetryContract.Event.CANCEL, Method.BACK, "settings");
     }
 
     @Override
@@ -1007,6 +1009,9 @@ OnSharedPreferenceChangeListener
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final String prefName = preference.getKey();
         Log.i(LOGTAG, "Changed " + prefName + " = " + newValue);
+
+        Telemetry.sendUIEvent(TelemetryContract.Event.EDIT, Method.SETTINGS, prefName);
+
         if (PREFS_MP_ENABLED.equals(prefName)) {
             showDialog((Boolean) newValue ? DIALOG_CREATE_MASTER_PASSWORD : DIALOG_REMOVE_MASTER_PASSWORD);
 
