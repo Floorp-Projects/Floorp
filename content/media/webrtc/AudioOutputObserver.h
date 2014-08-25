dfdf8af4a3d7ce2f8c0eb56d3781291332bccc63
Paul Adenot <paul@paul.cx>
2014-08-25 14:13:08 +0200

    Bug 989921 - Allow the MediaStreamGraph mixer to send data back to multiple consumers. r=jesup

--- content/media/webrtc/AudioOutputObserver.h
+++ content/media/webrtc/AudioOutputObserver.h
@@ -20,11 +20,17 @@ typedef struct FarEndAudioChunk_ {
 } FarEndAudioChunk;
 
 // XXX Really a singleton currently
-class AudioOutputObserver // : public MSGOutputObserver
+class AudioOutputObserver : public MixerCallbackReceiver
 {
 public:
   AudioOutputObserver();
   virtual ~AudioOutputObserver();
+
+  void MixerCallback(AudioDataValue* aMixedBuffer,
+                     AudioSampleFormat aFormat,
+                     uint32_t aChannels,
+                     uint32_t aFrames,
+                     uint32_t aSampleRate) MOZ_OVERRIDE;
 
   void Clear();
   void InsertFarEnd(const AudioDataValue *aBuffer, uint32_t aFrames, bool aOverran,
