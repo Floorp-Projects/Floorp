15bf754264523007384df793d6f711501645eef4
Anthony Jones <ajones@mozilla.com>
2014-08-26 16:53:53 +1200

    Bug 1056485 - Fix tfdt handling for files with multiple tracks; r=edwin

--- media/libstagefright/frameworks/av/media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/frameworks/av/media/libstagefright/MPEG4Extractor.cpp
@@ -2578,35 +2578,37 @@ status_t MPEG4Source::parseChunk(off64_t *offset) {
             break;
         }
 
-        case FOURCC('t', 'f', 'd', 't'):
-        {
+        case FOURCC('t', 'f', 'h', 'd'): {
             status_t err;
-            if ((err = parseTrackFragmentData(data_offset, chunk_data_size)) != OK) {
+            if ((err = parseTrackFragmentHeader(data_offset, chunk_data_size)) != OK) {
                 return err;
             }
             *offset += chunk_size;
             break;
         }
 
-        case FOURCC('t', 'f', 'h', 'd'): {
-                status_t err;
-                if ((err = parseTrackFragmentHeader(data_offset, chunk_data_size)) != OK) {
+        case FOURCC('t', 'f', 'd', 't'):
+        {
+            status_t err;
+            if (mLastParsedTrackId == mTrackId) {
+                if ((err = parseTrackFragmentData(data_offset, chunk_data_size)) != OK) {
                     return err;
                 }
-                *offset += chunk_size;
-                break;
+            }
+            *offset += chunk_size;
+            break;
         }
 
         case FOURCC('t', 'r', 'u', 'n'): {
-                status_t err;
-                if (mLastParsedTrackId == mTrackId) {
-                    if ((err = parseTrackFragmentRun(data_offset, chunk_data_size)) != OK) {
-                        return err;
-                    }
+            status_t err;
+            if (mLastParsedTrackId == mTrackId) {
+                if ((err = parseTrackFragmentRun(data_offset, chunk_data_size)) != OK) {
+                    return err;
                 }
+            }
 
-                *offset += chunk_size;
-                break;
+            *offset += chunk_size;
+            break;
         }
 
         case FOURCC('s', 'a', 'i', 'z'): {
