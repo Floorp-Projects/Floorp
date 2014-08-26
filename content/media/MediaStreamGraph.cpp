f408b70e40724e19b645c4e65962474aac153ec0
Paul Adenot <paul@paul.cx>
2014-08-26 17:02:31 +0200

    Bug 848954 - Part 24 - Clean up the clock getter, because the right thing is now done by the GraphDriver. r=roc

--- content/media/MediaStreamGraph.cpp
+++ content/media/MediaStreamGraph.cpp
@@ -334,15 +334,9 @@ MediaStreamGraphImpl::StreamTimeToGraphTime(MediaStream* aStream,
 GraphTime
 MediaStreamGraphImpl::GetAudioPosition(MediaStream* aStream)
 {
-  if (!mMixedAudioStream) {
-    return IterationEnd();
-  }
-  int64_t positionInFrames = mMixedAudioStream->GetPositionInFrames();
-  if (positionInFrames < 0) {
-    return IterationEnd();
-  }
-  return aStream->mAudioOutputStreams[0].mAudioPlaybackStartTime +
-    RateConvertTicksRoundDown(GraphRate(), mSampleRate, positionInFrames);
+  /* This is correlated to the audio clock when using an AudioCallbackDriver,
+   * and is using a system timer otherwise. */
+  return IterationEnd();
 }
 
 GraphTime
