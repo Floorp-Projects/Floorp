0032f2d653780ec6f9edcf07b1ce507fc1fe2e53
Ting-Yu Chou <janus926@gmail.com>
2014-08-26 10:22:21 +0800

    Bug 1057065 - Remove obsolete code causes extra preallocated process to complement the patch of bug 1033618. r=khuey

--- content/base/src/nsFrameLoader.cpp
+++ content/base/src/nsFrameLoader.cpp
@@ -467,13 +467,6 @@ nsFrameLoader::ReallyStartLoadingInternal()
           mPendingFrameSent = true;
         }
       }
-      if (Preferences::GetBool("dom.ipc.processPrelaunch.enabled", false) &&
-          !ContentParent::PreallocatedProcessReady()) {
-
-        ContentParent::RunAfterPreallocatedProcessReady(
-            new DelayedStartLoadingRunnable(this));
-        return NS_ERROR_FAILURE;
-      }
 
       TryRemoteBrowser();
 
