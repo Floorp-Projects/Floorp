6bfd2fff5a3cb12f96f226e26726b01067c660c2
Shih-Chiang Chien <schien@mozilla.com>
2014-08-27 10:28:03 +0800

    Bug 1080474 - Part 1 - device manager for Presentation API. r=fabrice.

---
+++ dom/presentation/PresentationSessionRequest.h
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_PresentationSessionRequest_h__
+#define mozilla_dom_PresentationSessionRequest_h__
+
+#include "nsIPresentationSessionRequest.h"
+#include "nsCOMPtr.h"
+#include "nsString.h"
+
+namespace mozilla {
+namespace dom {
+
+class PresentationSessionRequest MOZ_FINAL : public nsIPresentationSessionRequest
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIPRESENTATIONSESSIONREQUEST
+
+  PresentationSessionRequest(nsIPresentationDevice* aDevice,
+                             const nsAString& aUrl,
+                             const nsAString& aPresentationId,
+                             nsIPresentationControlChannel* aControlChannel);
+
+private:
+  virtual ~PresentationSessionRequest();
+
+  nsString mUrl;
+  nsString mPresentationId;
+  nsCOMPtr<nsIPresentationDevice> mDevice;
+  nsCOMPtr<nsIPresentationControlChannel> mControlChannel;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif /* mozilla_dom_PresentationSessionRequest_h__ */
+
