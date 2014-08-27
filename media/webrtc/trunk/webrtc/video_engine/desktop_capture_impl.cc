8a6703412627e563e2a5074b7044c8434c97c7a8
Matthew A. Miller <linuxwolf@outer-planes.net>
2014-08-27 10:58:14 -0600

    Bug 1047743 - Screen sharing with multiple screens is broken (Mac only). r=jesup * * * Bug 1047743 - interdiff to not impact other platforms

--- media/webrtc/trunk/webrtc/video_engine/desktop_capture_impl.cc
+++ media/webrtc/trunk/webrtc/video_engine/desktop_capture_impl.cc
@@ -382,7 +382,7 @@ int32_t DesktopCaptureImpl::Init(const char* uniqueId,
       return -1;
     }
 
-    ScreenId screenid = webrtc::kFullDesktopScreenId;
+    ScreenId screenid = atoi(uniqueId);
     pScreenCapturer->SelectScreen(screenid);
     pScreenCapturer->SetMouseShapeObserver(this);
 
