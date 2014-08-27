5fc7b6aaaf89aa1cc24f79c27470f36cf62a083b
Dave Hunt <dhunt@mozilla.com>
2014-08-27 10:32:00 -0400

    Bug 1052433 - Remove the device reboot in B2GDeviceRunner. r=ahalberstadt

--- testing/mozbase/mozrunner/mozrunner/base/device.py
+++ testing/mozbase/mozrunner/mozrunner/base/device.py
@@ -72,9 +72,6 @@ class DeviceRunner(BaseRunner):
         # to see if we have the homescreen running, or something, before waiting here
         self.device.wait_for_net()
 
-        if not isinstance(self.device, Emulator):
-            self.device.reboot()
-
         if not self.device.wait_for_net():
             raise Exception("Network did not come up when starting device")
         self.app_ctx.stop_application()
