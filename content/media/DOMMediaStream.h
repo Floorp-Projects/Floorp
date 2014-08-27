ea7d74a01c53b112a3b194a9f43669787cdf7be9
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-08-27 08:40:11 +0200

    Backed out changeset ef1c321a4a32 (bug 1057955)

--- content/media/DOMMediaStream.h
+++ content/media/DOMMediaStream.h
@@ -100,8 +100,6 @@ public:
    */
   virtual void SetTrackEnabled(TrackID aTrackID, bool aEnabled);
 
-  virtual void StopTrack(TrackID aTrackID);
-
   bool IsFinished();
   /**
    * Returns a principal indicating who may access this stream. The stream contents
