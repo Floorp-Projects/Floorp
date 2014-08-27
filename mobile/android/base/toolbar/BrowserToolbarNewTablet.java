809e1d45fcec666221d6d71aff969fbf6d8b467e
Michael Comella <michael.l.comella@gmail.com>
2014-08-27 15:34:04 -0700

    Bug 1046203 - Part 6: Copy-pasta rough BrowserToolbarNewTablet forward button behavior. r=margaret

--- mobile/android/base/toolbar/BrowserToolbarNewTablet.java
+++ mobile/android/base/toolbar/BrowserToolbarNewTablet.java
@@ -5,10 +5,14 @@
 
 package org.mozilla.gecko.toolbar;
 
+import org.mozilla.gecko.R;
 import org.mozilla.gecko.animation.PropertyAnimator;
+import org.mozilla.gecko.animation.ViewHelper;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.AttributeSet;
+import android.view.View;
 
 /**
  * A toolbar implementation for the tablet redesign (bug 1014156).
@@ -16,8 +20,17 @@ import android.util.AttributeSet;
  */
 class BrowserToolbarNewTablet extends BrowserToolbarTabletBase {
 
+    private static final int FORWARD_ANIMATION_DURATION = 450;
+
+    private final int urlBarViewOffset;
+    private final int defaultForwardMargin;
+
     public BrowserToolbarNewTablet(final Context context, final AttributeSet attrs) {
         super(context, attrs);
+
+        final Resources res = getResources();
+        urlBarViewOffset = res.getDimensionPixelSize(R.dimen.url_bar_offset_left);
+        defaultForwardMargin = res.getDimensionPixelSize(R.dimen.forward_default_offset);
     }
 
     @Override
@@ -35,8 +48,94 @@ class BrowserToolbarNewTablet extends BrowserToolbarTabletBase {
         hideUrlEditLayout();
     }
 
+    // TODO: Copy-pasta from BrowserToolbarTablet - implement a correctly working version.
     @Override
     protected void animateForwardButton(final ForwardButtonAnimation animation) {
-        // TODO: Implement.
+        // If the forward button is not visible, we must be
+        // in the phone UI.
+        if (forwardButton.getVisibility() != View.VISIBLE) {
+            return;
+        }
+
+        final boolean showing = (animation == ForwardButtonAnimation.SHOW);
+
+        // if the forward button's margin is non-zero, this means it has already
+        // been animated to be visible¸ and vice-versa.
+        MarginLayoutParams fwdParams = (MarginLayoutParams) forwardButton.getLayoutParams();
+        if ((fwdParams.leftMargin > defaultForwardMargin && showing) ||
+            (fwdParams.leftMargin == defaultForwardMargin && !showing)) {
+            return;
+        }
+
+        // We want the forward button to show immediately when switching tabs
+        final PropertyAnimator forwardAnim =
+                new PropertyAnimator(isSwitchingTabs ? 10 : FORWARD_ANIMATION_DURATION);
+        final int width = Math.round(forwardButton.getWidth() * .75f);
+
+        forwardAnim.addPropertyAnimationListener(new PropertyAnimator.PropertyAnimationListener() {
+            @Override
+            public void onPropertyAnimationStart() {
+                if (!showing) {
+                    // Set the margin before the transition when hiding the forward button. We
+                    // have to do this so that the favicon isn't clipped during the transition
+                    MarginLayoutParams layoutParams =
+                        (MarginLayoutParams) urlDisplayLayout.getLayoutParams();
+                    layoutParams.leftMargin = 0;
+
+                    // Do the same on the URL edit container
+                    layoutParams = (MarginLayoutParams) urlEditLayout.getLayoutParams();
+                    layoutParams.leftMargin = 0;
+
+                    requestLayout();
+                    // Note, we already translated the favicon, site security, and text field
+                    // in prepareForwardAnimation, so they should appear to have not moved at
+                    // all at this point.
+                }
+            }
+
+            @Override
+            public void onPropertyAnimationEnd() {
+                if (showing) {
+                    MarginLayoutParams layoutParams =
+                        (MarginLayoutParams) urlDisplayLayout.getLayoutParams();
+                    layoutParams.leftMargin = urlBarViewOffset;
+
+                    layoutParams = (MarginLayoutParams) urlEditLayout.getLayoutParams();
+                    layoutParams.leftMargin = urlBarViewOffset;
+                }
+
+                urlDisplayLayout.finishForwardAnimation();
+
+                MarginLayoutParams layoutParams = (MarginLayoutParams) forwardButton.getLayoutParams();
+                layoutParams.leftMargin = defaultForwardMargin + (showing ? width : 0);
+                ViewHelper.setTranslationX(forwardButton, 0);
+
+                requestLayout();
+            }
+        });
+
+        prepareForwardAnimation(forwardAnim, animation, width);
+        forwardAnim.start();
+    }
+
+    private void prepareForwardAnimation(PropertyAnimator anim, ForwardButtonAnimation animation, int width) {
+        if (animation == ForwardButtonAnimation.HIDE) {
+            anim.attach(forwardButton,
+                      PropertyAnimator.Property.TRANSLATION_X,
+                      -width);
+            anim.attach(forwardButton,
+                      PropertyAnimator.Property.ALPHA,
+                      0);
+
+        } else {
+            anim.attach(forwardButton,
+                      PropertyAnimator.Property.TRANSLATION_X,
+                      width);
+            anim.attach(forwardButton,
+                      PropertyAnimator.Property.ALPHA,
+                      1);
+        }
+
+        urlDisplayLayout.prepareForwardAnimation(anim, animation, width);
     }
 }
