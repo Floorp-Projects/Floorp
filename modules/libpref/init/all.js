a8ded9bf3a4443fbfe4cfdb6976ff8205c97ca97
Dirk Schulze <krit@webkit.org>
2014-08-28 03:09:00 +0200

    Bug 1058519 - Ship mask-type CSS property. r=heycam

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1963,11 +1963,7 @@ pref("layout.css.dpi", -1);
 pref("layout.css.devPixelsPerPx", "-1.0");
 
 // Is support for CSS Masking features enabled?
-#ifdef RELEASE_BUILD
-pref("layout.css.masking.enabled", false);
-#else
 pref("layout.css.masking.enabled", true);
-#endif
 
 // Is support for mix-blend-mode enabled?
 pref("layout.css.mix-blend-mode.enabled", true);
