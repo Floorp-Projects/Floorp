02efc55c674b0dd00061b901add119dd86a0732d
James Kolb <jck1089@gmail.com>
2014-08-29 23:04:34 +0900

    Bug 1028288 Add canvas global transparency support to svgs. r=seth

--- gfx/thebes/gfxUtils.cpp
+++ gfx/thebes/gfxUtils.cpp
@@ -407,7 +407,7 @@ CreateSamplingRestrictedDrawable(gfxDrawable* aDrawable,
     nsRefPtr<gfxContext> tmpCtx = new gfxContext(target);
     tmpCtx->SetOperator(OptimalFillOperator());
     aDrawable->Draw(tmpCtx, needed - needed.TopLeft(), true,
-                    GraphicsFilter::FILTER_FAST, gfxMatrix().Translate(needed.TopLeft()));
+                    GraphicsFilter::FILTER_FAST, 1.0, gfxMatrix().Translate(needed.TopLeft()));
     RefPtr<SourceSurface> surface = target->Snapshot();
 
     nsRefPtr<gfxDrawable> drawable = new gfxSurfaceDrawable(surface, size, gfxMatrix().Translate(-needed.TopLeft()));
@@ -567,7 +567,8 @@ gfxUtils::DrawPixelSnapped(gfxContext*         aContext,
                            const ImageRegion&  aRegion,
                            const SurfaceFormat aFormat,
                            GraphicsFilter      aFilter,
-                           uint32_t            aImageFlags)
+                           uint32_t            aImageFlags,
+                           gfxFloat            aOpacity)
 {
     PROFILER_LABEL("gfxUtils", "DrawPixelSnapped",
       js::ProfileEntry::Category::GRAPHICS);
@@ -617,7 +618,7 @@ gfxUtils::DrawPixelSnapped(gfxContext*         aContext,
     }
 #endif
 
-    drawable->Draw(aContext, aRegion.Rect(), doTile, aFilter);
+    drawable->Draw(aContext, aRegion.Rect(), doTile, aFilter, aOpacity);
 }
 
 /* static */ int
