d1c769ce630076beb0d959d4786ef98e60e5d721
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-08-29 12:05:58 -0700

    Bug 1057629 - Use search plugins that ship with Fennec to populate options in search activity settings. r=bnicholson

--- mobile/android/search/java/org/mozilla/search/providers/SearchEngineManager.java
+++ mobile/android/search/java/org/mozilla/search/providers/SearchEngineManager.java
@@ -17,8 +17,12 @@ import org.mozilla.search.Constants;
 import org.mozilla.search.SearchPreferenceActivity;
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchEngineManager implements SharedPreferences.OnSharedPreferenceChangeListener {
     private static final String LOG_TAG = "SearchEngineManager";
@@ -100,6 +104,55 @@ public class SearchEngineManager implements SharedPreferences.OnSharedPreference
     }
 
     /**
+     * Creates a list of SearchEngine instances from all available open search plugins.
+     * This method does disk I/O, call it from a background thread.
+     *
+     * @return List of SearchEngine instances
+     */
+    public List<SearchEngine> getAllEngines() {
+        // First try to read the engine list from the jar.
+        final String url = getSearchPluginsJarUrl("list.txt");
+        InputStream in = GeckoJarReader.getStream(url);
+
+        // Fallback for standalone search activity.
+        if (in == null) {
+            try {
+                in = context.getResources().getAssets().open("engines/list.txt");
+            } catch (IOException e) {
+                throw new IllegalStateException("Error reading list.txt");
+            }
+        }
+
+        final List<SearchEngine> list = new ArrayList<SearchEngine>();
+        InputStreamReader isr = null;
+
+        try {
+            isr = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(isr);
+            String identifier;
+            while ((identifier = br.readLine()) != null) {
+                list.add(createEngine(identifier));
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Error creating all search engines from list.txt");
+        } finally {
+            if (isr != null) {
+                try {
+                    isr.close();
+                } catch (IOException e) {
+                    // Ignore.
+                }
+            }
+            try {
+                in.close();
+            } catch (IOException e) {
+                // Ignore.
+            }
+        }
+        return list;
+    }
+
+    /**
      * Creates a SearchEngine instance from an open search plugin.
      * This method does disk I/O, call it from a background thread.
      *
@@ -142,7 +195,7 @@ public class SearchEngineManager implements SharedPreferences.OnSharedPreference
      */
     private InputStream getEngineFromAssets(String identifier) {
         try {
-            return context.getResources().getAssets().open(identifier + ".xml");
+            return context.getResources().getAssets().open("engines/" + identifier + ".xml");
         } catch (IOException e) {
             Log.e(LOG_TAG, "Exception getting search engine from assets", e);
             return null;
@@ -157,12 +210,21 @@ public class SearchEngineManager implements SharedPreferences.OnSharedPreference
      * @return InputStream for open search plugin XML
      */
     private InputStream getEngineFromJar(String identifier) {
+        final String url = getSearchPluginsJarUrl(identifier + ".xml");
+        return GeckoJarReader.getStream(url);
+    }
+
+    /**
+     * Gets the jar URL for a file in the searchplugins directory
+     *
+     * @param fileName
+     * @return
+     */
+    private String getSearchPluginsJarUrl(String fileName) {
         // TODO: Get the real value for this
         final String locale = "en-US";
 
-        final String path = "!/chrome/" + locale + "/locale/" + locale + "/browser/searchplugins/" + identifier + ".xml";
-        final String url = "jar:jar:file://" + context.getPackageResourcePath() + "!/" + AppConstants.OMNIJAR_NAME + path;
-
-        return GeckoJarReader.getStream(url);
+        final String path = "!/chrome/" + locale + "/locale/" + locale + "/browser/searchplugins/" + fileName;
+        return "jar:jar:file://" + context.getPackageResourcePath() + "!/" + AppConstants.OMNIJAR_NAME + path;
     }
 }
