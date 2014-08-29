6820ba10cd7a9c6446cee9f19b2a2d7bd769ced7
Mark Banner <standard8@mozilla.com>
2014-08-29 11:22:18 +0100

    Bug 1045643 Part 2 - Notify the Loop server when the client has local media up and remote media being received, so that it can update the call connection status. r=nperriault

--- browser/components/loop/content/js/conversation.js
+++ browser/components/loop/content/js/conversation.js
@@ -194,6 +194,9 @@ loop.conversation = (function(OT, mozL10n) {
         this.navigate("call/declineAndBlock", {trigger: true});
       }.bind(this));
       this._conversation.once("call:incoming", this.startCall, this);
+      this._conversation.once("change:publishedStream", this._checkConnected, this);
+      this._conversation.once("change:subscribedStream", this._checkConnected, this);
+
       this._client.requestCallsInfo(loopVersion, function(err, sessionData) {
         if (err) {
           console.error("Failed to get the sessionData", err);
@@ -233,6 +236,18 @@ loop.conversation = (function(OT, mozL10n) {
         this._handleSessionError();
         return;
       }.bind(this));
+    },
+
+    /**
+     * Checks if the streams have been connected, and notifies the
+     * websocket that the media is now connected.
+     */
+    _checkConnected: function() {
+      // Check we've had both local and remote streams connected before
+      // sending the media up message.
+      if (this._conversation.streamsConnected()) {
+        this._websocket.mediaUp();
+      }
     },
 
     /**
