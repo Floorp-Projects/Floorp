730aca3c113070da65591b58c065fa8073f5a6fa
Randell Jesup <rjesup@jesup.org>
2014-08-29 21:29:17 -0400

    Bug 1060249: disable frame motion/complexity analysis in webrtc on Gonk r=gcp

--- media/webrtc/trunk/webrtc/modules/video_processing/main/source/content_analysis.cc
+++ media/webrtc/trunk/webrtc/modules/video_processing/main/source/content_analysis.cc
@@ -58,6 +58,7 @@ VideoContentMetrics* VPMContentAnalysis::ComputeContentMetrics(
     if (VPM_OK != Initialize(inputFrame.width(), inputFrame.height()))
       return NULL;
   }
+#if !defined(WEBRTC_GONK)
   // Compute motion metrics
   if (ca_Init_) {
     // Only interested in the Y plane.
@@ -75,6 +76,7 @@ VideoContentMetrics* VPMContentAnalysis::ComputeContentMetrics(
 
     first_frame_ =  false;
   }
+#endif
 
   return ContentMetrics();
 }
@@ -123,10 +125,12 @@ int32_t VPMContentAnalysis::Initialize(int width, int height) {
     return VPM_MEMORY;
   }
 
+#if !defined(WEBRTC_GONK)
   prev_frame_.reset(new uint8_t[width_ * height_]);  // Y only.
   if (!prev_frame_) {
     return VPM_MEMORY;
   }
+#endif
 
   // ok, all initialized
   ca_Init_ = true;
