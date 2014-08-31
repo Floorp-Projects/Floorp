fb7d0ca04d260c3cde28ef03e35ea937b9e6e12e
chiajung hung <chung@mozilla.com>
2014-08-31 21:00:00 +0200

    Bug 1060121 - Make WebGLContext::TexImage2D avoid readback for video elements on Windows. r=jgilbert

--- gfx/gl/GLBlitHelper.cpp
+++ gfx/gl/GLBlitHelper.cpp
@@ -187,6 +187,9 @@ bool
 GLBlitHelper::InitTexQuadProgram(BlitType target)
 {
     const char kTexBlit_VertShaderSource[] = "\
+        #ifdef GL_ES                                  \n\
+        precision mediump float;                      \n\
+        #endif                                        \n\
         attribute vec2 aPosition;                     \n\
                                                       \n\
         uniform float uYflip;                         \n\
@@ -263,6 +266,9 @@ GLBlitHelper::InitTexQuadProgram(BlitType target)
     [B] [1.16438, 2.01723, 0.00000] [Cr - 0.50196]
     */
     const char kTexYUVPlanarBlit_FragShaderSource[] = "\
+        #ifdef GL_ES                                                        \n\
+        precision mediump float                                             \n\
+        #endif                                                              \n\
         varying vec2 vTexCoord;                                             \n\
         uniform sampler2D uYTexture;                                        \n\
         uniform sampler2D uCbTexture;                                       \n\
