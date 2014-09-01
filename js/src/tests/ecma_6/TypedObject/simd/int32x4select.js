6c42d288a8bf470bac66cd3a791335ae0682534b
Douglas Crosher <dtc-moz@scieneer.com>
2014-09-02 01:49:20 +1000

    Bug 1060437 - SIMD: Implement float32x4.select in the interpreter; r=bbouvier

--- js/src/tests/ecma_6/TypedObject/simd/int32x4select.js
+++ js/src/tests/ecma_6/TypedObject/simd/int32x4select.js
@@ -1,17 +1,17 @@
 // |reftest| skip-if(!this.hasOwnProperty("SIMD"))
-var BUGNUMBER = 946042;
-var float32x4 = SIMD.float32x4;
+var BUGNUMBER = 1060437;
 var int32x4 = SIMD.int32x4;
 
 var summary = 'int32x4 select';
 
+const INT32_MAX = Math.pow(2, 31) - 1;
+const INT32_MIN = INT32_MAX + 1 | 0;
+
 function test() {
   print(BUGNUMBER + ": " + summary);
 
-  // FIXME -- Bug 948379: Amend to check for correctness of border cases.
-
-  var a = float32x4(0.0,4.0,9.0,16.0)
-  var b = float32x4(1.0,2.0,3.0,4.0)
+  var a = int32x4(0,4,9,16)
+  var b = int32x4(1,2,3,4)
   var sel_ttff = int32x4.bool(true, true, false, false);
   var c = SIMD.int32x4.select(sel_ttff,a,b);
   assertEq(c.x, 0);
@@ -19,9 +19,22 @@ function test() {
   assertEq(c.z, 3);
   assertEq(c.w, 4);
 
+  var a2 = int32x4(INT32_MAX,INT32_MIN,9,16)
+  var c = SIMD.int32x4.select(sel_ttff,a2,b);
+  assertEq(c.x, INT32_MAX);
+  assertEq(c.y, INT32_MIN);
+  assertEq(c.z, 3);
+  assertEq(c.w, 4);
+
+  var b2 = int32x4(1,2,INT32_MAX,INT32_MIN)
+  var c = SIMD.int32x4.select(sel_ttff,a2,b2);
+  assertEq(c.x, INT32_MAX);
+  assertEq(c.y, INT32_MIN);
+  assertEq(c.z, INT32_MAX);
+  assertEq(c.w, INT32_MIN);
+
   if (typeof reportCompare === "function")
     reportCompare(true, true);
 }
 
 test();
-
