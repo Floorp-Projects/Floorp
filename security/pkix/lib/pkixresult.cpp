c440945cc8625f38752e76edb8616235d5095e55
Brian Smith <brian@briansmith.org>
2014-08-31 19:42:36 -0700

    Bug 1063013, Part 4: Move MapResultToName and MAP_LIST out of pkixnss.h/pkixnss.cpp, r=keeler

--- security/pkix/lib/pkixresult.cpp
+++ security/pkix/lib/pkixresult.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*- *- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set ts=8 sts=2 et sw=2 tw=80: */
 /* This code is made available to you under your choice of the following sets
  * of licensing terms:
@@ -7,7 +7,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-/* Copyright 2014 Mozilla Contributors
+/* Copyright 2013 Mozilla Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,28 +21,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef mozilla_pkix__pkixgtest_h
-#define mozilla_pkix__pkixgtest_h
 
-#include <ostream>
-
-#include "gtest/gtest.h"
 #include "pkix/Result.h"
 
-// PrintTo must be in the same namespace as the type we're overloading it for.
+#include "pkix/nullptr.h"
+
 namespace mozilla { namespace pkix {
 
-inline void
-PrintTo(const Result& result, ::std::ostream* os)
+const char*
+MapResultToName(Result result)
 {
-  const char* stringified = MapResultToName(result);
-  if (stringified) {
-    *os << stringified;
-  } else {
-    *os << "mozilla::pkix::Result(" << static_cast<unsigned int>(result) << ")";
+  switch (result)
+  {
+#define MOZILLA_PKIX_MAP(mozilla_pkix_result, nss_result) \
+    case mozilla_pkix_result: return #mozilla_pkix_result;
+
+    MOZILLA_PKIX_MAP_LIST
+
+#undef MOZILLA_PKIX_MAP
+
+    default:
+      assert(false);
+      return nullptr;
   }
 }
 
 } } // namespace mozilla::pkix
-
-#endif // mozilla_pkix__pkixgtest_h
