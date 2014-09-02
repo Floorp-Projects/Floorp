94adb30f77b545ea7831f9592f9b3b30a63fb558
Nicholas Nethercote <nnethercote@mozilla.com>
2014-09-01 22:56:05 -0700

    Bug 1062709 (part 2, attempt 2) - Clean up stack printing and fixing. r=dbaron.
    --HG--
    extra : rebase_source : 626fd23a14ec90cfc9807c3d555169ec6463d19d

--- toolkit/xre/nsSigHandlers.cpp
+++ toolkit/xre/nsSigHandlers.cpp
@@ -63,8 +63,9 @@ static void PrintStackFrame(uint32_t aFrameNumber, void *aPC, void *aSP,
   nsCodeAddressDetails details;
 
   NS_DescribeCodeAddress(aPC, &details);
-  NS_FormatCodeAddressDetails(aFrameNumber, aPC, &details, buf, sizeof(buf));
-  fputs(buf, stdout);
+  NS_FormatCodeAddressDetails(buf, sizeof(buf), aFrameNumber, aPC, &details);
+  fprintf(stdout, "%s\n", buf);
+  fflush(stdout);
 }
 
 }
