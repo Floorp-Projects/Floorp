f9f97be19ecba3c0b62fa776339880bff708522a
Ryan VanderMeulen <ryanvm@gmail.com>
2014-09-02 09:19:24 -0400

    Backed out changeset 56bd99846222 (bug 1030124) for bustage.

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -4649,16 +4649,11 @@ nsDisplayTransform::GetResultingTransformMatrixInternal(const FrameTransformProp
     FrameTransformProperties props(frame->GetParent(),
                                    aAppUnitsPerPixel,
                                    nullptr);
-
-    // If this frame isn't transformed (but we exist for backface-visibility),
-    // then we're not a reference frame so no offset to origin will be added. Our
-    // parent transform however *is* the reference frame, so we pass true for
-    // aOffsetByOrigin to convert into the correct coordinate space.
     gfx3DMatrix parent =
       GetResultingTransformMatrixInternal(props,
                                           aOrigin - frame->GetPosition(),
                                           aAppUnitsPerPixel, nullptr,
-                                          aOutAncestor, !frame->IsTransformed());
+                                          aOutAncestor, false);
 
     result = nsLayoutUtils::ChangeMatrixBasis(offsetBetweenOrigins, result) * parent;
     if (aOffsetByOrigin) {
@@ -4793,7 +4788,7 @@ nsDisplayTransform::GetTransform()
        */
       mTransform =
         GetResultingTransformMatrix(mFrame, ToReferenceFrame(), scale,
-                                    nullptr, nullptr, mFrame->IsTransformed()));
+                                    nullptr, nullptr, true);
     }
   }
   return mTransform;
