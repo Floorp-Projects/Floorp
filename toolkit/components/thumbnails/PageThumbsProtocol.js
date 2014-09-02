5242c5a28a42b9821c7aa269f3ba5072bf0c4f86
Tim Taubert <ttaubert@mozilla.com>
2014-09-02 12:23:39 -0400

    Bug 1046645 - Revert part of the first patch and remove URI_IS_LOCAL_FILE from PageThumbsProtocolHandler again

--- toolkit/components/thumbnails/PageThumbsProtocol.js
+++ toolkit/components/thumbnails/PageThumbsProtocol.js
@@ -53,7 +53,6 @@ Protocol.prototype = {
   get protocolFlags() {
     return Ci.nsIProtocolHandler.URI_DANGEROUS_TO_LOAD |
            Ci.nsIProtocolHandler.URI_IS_LOCAL_RESOURCE |
-           Ci.nsIProtocolHandler.URI_IS_LOCAL_FILE |
            Ci.nsIProtocolHandler.URI_NORELATIVE |
            Ci.nsIProtocolHandler.URI_NOAUTH;
   },
