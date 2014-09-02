e63feb3849c999ce89103ce18d1fcbf1fa171564
Phil Ringnalda <philringnalda@gmail.com>
2014-09-01 18:07:57 -0700

    Backed out 8 changesets (bug 982338, bug 1057212, bug 1059033) for build bustage CLOSED TREE
    Backed out changeset 4df1bd30364d (bug 982338)
    Backed out changeset b68664a02904 (bug 1057212)
    Backed out changeset 07b3695aa02f (bug 1059033)
    Backed out changeset bb3885b57d48 (bug 1059033)
    Backed out changeset 76897f52ac2c (bug 1059033)
    Backed out changeset 31e89a2a409f (bug 1059033)
    Backed out changeset 8e81f6f74182 (bug 1059033)
    Backed out changeset c5bbf22f2f28 (bug 1059033)

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -237,7 +237,6 @@ private:
   DECL_GFX_PREF(Live, "layers.effect.invert",                  LayersEffectInvert, bool, false);
 
   DECL_GFX_PREF(Once, "layers.enable-tiles",                   LayersTilesEnabled, bool, false);
-  DECL_GFX_PREF(Once, "layers.force-enable-tiles",             LayersTilesForceEnabled, bool, false);
   DECL_GFX_PREF(Once, "layers.simple-tiles",                   LayersUseSimpleTiles, bool, false);
   DECL_GFX_PREF(Once, "layers.force-per-tile-drawing",         PerTileDrawing, bool, false);
   DECL_GFX_PREF(Once, "layers.tiled-drawtarget.enabled",       TiledDrawTargetEnabled, bool, false);
