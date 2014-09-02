43e77fabd17c4d210080b2cf1ffd50c5b1292af6
Phil Ringnalda <philringnalda@gmail.com>
2014-09-01 19:22:06 -0700

    Backed out changeset b9e3a9f5d04a (bug 1036849) for build bustage, a=backout

--- content/media/fmp4/MP4Reader.h
+++ content/media/fmp4/MP4Reader.h
@@ -61,12 +61,9 @@ public:
                                int64_t aStartTime) MOZ_OVERRIDE;
 
   // For Media Resource Management
-  virtual void SetIdle() MOZ_OVERRIDE;
   virtual bool IsWaitingMediaResources() MOZ_OVERRIDE;
   virtual bool IsDormantNeeded() MOZ_OVERRIDE;
   virtual void ReleaseMediaResources() MOZ_OVERRIDE;
-  virtual void SetSharedDecoderManager(SharedDecoderManager* aManager)
-    MOZ_OVERRIDE;
 
   virtual nsresult ResetDecode() MOZ_OVERRIDE;
 
@@ -181,6 +178,7 @@ private:
   uint64_t mLastReportedNumDecodedFrames;
 
   DecoderData& GetDecoderData(mp4_demuxer::TrackType aTrack);
+  MediaDataDecoder* Decoder(mp4_demuxer::TrackType aTrack);
 
   layers::LayersBackend mLayersBackendType;
 
@@ -194,7 +192,6 @@ private:
 
   bool mIndexReady;
   Monitor mIndexMonitor;
-  nsRefPtr<SharedDecoderManager> mSharedDecoderManager;
 };
 
 } // namespace mozilla
--- content/media/mediasource/MediaSourceReader.cpp
+++ content/media/mediasource/MediaSourceReader.cpp
@@ -14,7 +14,6 @@
 #include "MediaSourceDecoder.h"
 #include "MediaSourceUtils.h"
 #include "SourceBufferDecoder.h"
-#include "SharedDecoderManager.h"
 
 #ifdef MOZ_FMP4
 #include "MP4Decoder.h"
@@ -42,7 +41,6 @@ MediaSourceReader::MediaSourceReader(MediaSourceDecoder* aDecoder)
   , mDropAudioBeforeThreshold(false)
   , mDropVideoBeforeThreshold(false)
   , mEnded(false)
-  , mSharedDecoderManager(new SharedDecoderManager())
 {
 }
 
@@ -356,7 +354,6 @@ MediaSourceReader::CreateSubDecoder(const nsACString& aType)
   // reader's callback.
   RefPtr<MediaDataDecodedListener<MediaSourceReader>> callback =
     new MediaDataDecodedListener<MediaSourceReader>(this, GetTaskQueue());
-  reader->SetSharedDecoderManager(mSharedDecoderManager);
   reader->SetCallback(callback);
   reader->SetTaskQueue(GetTaskQueue());
   reader->Init(nullptr);
