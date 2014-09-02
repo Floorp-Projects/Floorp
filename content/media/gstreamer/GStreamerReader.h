e547bcf9c2db486b9694b28ea085ed9b31873b49
Ehsan Akhgari <ehsan@mozilla.com>
2014-09-02 09:46:56 -0400

    Bug 1061252 - Fix more bad implicit constructors in media code; r=roc

--- content/media/gstreamer/GStreamerReader.h
+++ content/media/gstreamer/GStreamerReader.h
@@ -38,7 +38,7 @@ class GStreamerReader : public MediaDecoderReader
   typedef gfx::IntRect IntRect;
 
 public:
-  GStreamerReader(AbstractMediaDecoder* aDecoder);
+  explicit GStreamerReader(AbstractMediaDecoder* aDecoder);
   virtual ~GStreamerReader();
 
   virtual nsresult Init(MediaDecoderReader* aCloneDonor);
