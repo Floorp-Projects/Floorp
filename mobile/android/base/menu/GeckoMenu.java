101b0dbae89128057701daa89ee081475aa255a4
Lucas Rocha <lucasr@mozilla.com>
2014-09-02 10:47:16 +0100

    Bug 1050780 - Avoid disabled items in GeckoMenu's adapter (r=margaret)

--- mobile/android/base/menu/GeckoMenu.java
+++ mobile/android/base/menu/GeckoMenu.java
@@ -786,7 +786,9 @@ public class GeckoMenu extends ListView
 
         @Override
         public boolean isEnabled(int position) {
-            return getItem(position).isEnabled();
+            // Setting this to true is a workaround to fix disappearing
+            // dividers in the menu in L (bug 1050780).
+            return true;
         }
 
         public void addMenuItem(GeckoMenuItem menuItem) {
