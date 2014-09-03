5ea407a80c9d233495ba80c0701b001ae2965e3f
Masayuki Nakano <masayuki@d-toybox.com>
2014-09-03 10:38:20 +0900

    Bug 1052343 part.5 Recreate nsTextStore instance at each focus change r=emk

--- widget/windows/winrt/MetroWidget.cpp
+++ widget/windows/winrt/MetroWidget.cpp
@@ -1571,9 +1571,9 @@ MetroWidget::NotifyIME(const IMENotification& aIMENotification)
       nsTextStore::CommitComposition(true);
       return NS_OK;
     case NOTIFY_IME_OF_FOCUS:
-      return nsTextStore::OnFocusChange(true, this, mInputContext.mIMEState);
+      return nsTextStore::OnFocusChange(true, this, mInputContext);
     case NOTIFY_IME_OF_BLUR:
-      return nsTextStore::OnFocusChange(false, this, mInputContext.mIMEState);
+      return nsTextStore::OnFocusChange(false, this, mInputContext);
     case NOTIFY_IME_OF_SELECTION_CHANGE:
       return nsTextStore::OnSelectionChange();
     case NOTIFY_IME_OF_TEXT_CHANGE:
