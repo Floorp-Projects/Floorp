2b9698cb5a1f7f7d02d27988ebb23a0d6b0649db
David Keeler <dkeeler@mozilla.com>
2014-09-03 10:12:55 -0700

    bug 1057123 - mozilla::pkix: allow end-entity certificates to assert keyCertSign in some cases r=briansmith

--- security/pkix/test/gtest/pkixcheck_CheckKeyUsage_tests.cpp
+++ security/pkix/test/gtest/pkixcheck_CheckKeyUsage_tests.cpp
@@ -195,7 +195,9 @@ TEST_F(pkixcheck_CheckKeyUsage, simpleCases)
   ASSERT_SimpleCase(3, 0x08, KeyUsage::keyAgreement);
 }
 
-// Only CAs are allowed to assert keyCertSign
+// Only CAs are allowed to assert keyCertSign.
+// End-entity certs may assert it along with other key usages if keyCertSign
+// isn't the required key usage. This is for compatibility.
 TEST_F(pkixcheck_CheckKeyUsage, keyCertSign)
 {
   NAMED_SIMPLE_KU(good, 2, 0x04);
@@ -221,6 +223,16 @@ TEST_F(pkixcheck_CheckKeyUsage, keyCertSign)
   ASSERT_BAD(CheckKeyUsage(EndEntityOrCA::MustBeEndEntity, &twoByteNotGood,
                            KeyUsage::keyCertSign));
   ASSERT_BAD(CheckKeyUsage(EndEntityOrCA::MustBeCA, &twoByteNotGood,
+                           KeyUsage::keyCertSign));
+
+  // If an end-entity certificate does assert keyCertSign, this is allowed
+  // as long as that isn't the required key usage.
+  NAMED_SIMPLE_KU(digitalSignatureAndKeyCertSign, 2, 0x84);
+  ASSERT_EQ(Success, CheckKeyUsage(EndEntityOrCA::MustBeEndEntity,
+                                   &digitalSignatureAndKeyCertSign,
+                                   KeyUsage::digitalSignature));
+  ASSERT_BAD(CheckKeyUsage(EndEntityOrCA::MustBeEndEntity,
+                           &digitalSignatureAndKeyCertSign,
                            KeyUsage::keyCertSign));
 }
 
