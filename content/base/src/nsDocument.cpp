ab4e47a8d99f11db7e17971fc77b1f5c56f596a0
Boris Zbarsky <bzbarsky@mit.edu>
2014-09-03 00:09:42 -0400

    Bug 1061202 - Make sure to not double-add app-theme-changed observers when a document has OnPageShow called on it twice without an OnPageHide call in between. r=khuey, a=bajaj

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -1601,6 +1601,12 @@ nsDocument::~nsDocument()
 
   NS_ASSERTION(!mIsShowing, "Destroying a currently-showing document");
 
+  // Note: This assert is only non-fatal because mochitest-bc triggers
+  // it... as well as the preceding assert about !mIsShowing.
+  NS_ASSERTION(!mObservingAppThemeChanged,
+               "Document leaked to shutdown, then the observer service dropped "
+               "its ref to us so we were able to go away.");
+
   if (IsTopLevelContentDocument()) {
     //don't report for about: pages
     nsCOMPtr<nsIPrincipal> principal = GetPrincipal();
@@ -8873,7 +8879,10 @@ nsDocument::OnPageShow(bool aPersisted,
                         "chrome-page-shown" :
                         "content-page-shown",
                       nullptr);
-  os->AddObserver(this, "app-theme-changed", /* ownsWeak */ false);
+  if (!mObservingAppThemeChanged) {
+    os->AddObserver(this, "app-theme-changed", /* ownsWeak */ false);
+    mObservingAppThemeChanged = true;
+  }
 
   DispatchPageTransition(target, NS_LITERAL_STRING("pageshow"), aPersisted);
 }
@@ -8949,6 +8958,7 @@ nsDocument::OnPageHide(bool aPersisted,
                         nullptr);
 
     os->RemoveObserver(this, "app-theme-changed");
+    mObservingAppThemeChanged = false;
   }
 
   DispatchPageTransition(target, NS_LITERAL_STRING("pagehide"), aPersisted);
