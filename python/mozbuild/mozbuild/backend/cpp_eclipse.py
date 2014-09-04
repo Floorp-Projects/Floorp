616a24457709f5b36812f71e3ffe22c5f31fe8a7
Benoit Girard <b56girard@gmail.com>
2014-09-04 02:08:33 -0400

    Bug 1062726 - Add ./mach ide [visualstudio,eclipse] command. r=gps

--- python/mozbuild/mozbuild/backend/cpp_eclipse.py
+++ python/mozbuild/mozbuild/backend/cpp_eclipse.py
@@ -27,7 +27,7 @@ class CppEclipseBackend(CommonBackend):
         CommonBackend._init(self)
 
         self._paths_to_defines = {}
-        self._workspace_dir = self.get_workspace_path()
+        self._workspace_dir = self._get_workspace_path()
         self._project_dir = os.path.join(self._workspace_dir, 'gecko')
         self._overwriting_workspace = os.path.isdir(self._workspace_dir)
 
@@ -48,12 +48,16 @@ class CppEclipseBackend(CommonBackend):
         self.summary.backend_detailed_summary = types.MethodType(detailed,
             self.summary)
 
-    def get_workspace_path(self):
+    def _get_workspace_path(self):
+        return CppEclipseBackend.get_workspace_path(self.environment.topsrcdir, self.environment.topobjdir)
+
+    @staticmethod
+    def get_workspace_path(topsrcdir, topobjdir):
         # Eclipse doesn't support having the workspace inside the srcdir.
         # Since most people have their objdir inside their srcdir it's easier
         # and more consistent to just put the workspace along side the srcdir
-        srcdir_parent = os.path.dirname(self.environment.topsrcdir)
-        workspace_dirname = "eclipse_" + os.path.basename(self.environment.topobjdir)
+        srcdir_parent = os.path.dirname(topsrcdir)
+        workspace_dirname = "eclipse_" + os.path.basename(topobjdir)
         return os.path.join(srcdir_parent, workspace_dirname)
 
     def consume_object(self, obj):
