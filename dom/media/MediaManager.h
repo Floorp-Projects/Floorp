314836f9376118508a7f3206ca0161d330268290
Jan-Ivar Bruaroey <jib@mozilla.com>
2014-09-05 16:02:54 -0400

    Bug 1062981 - Disable bfcache for pages active MediaManager. r=smaug, r=jesup, a=lmandel

--- dom/media/MediaManager.h
+++ dom/media/MediaManager.h
@@ -538,6 +538,11 @@ public:
   // from MediaManager thread.
   static MediaManager* Get();
 
+  static bool Exists()
+  {
+    return !!sSingleton;
+  }
+
   static nsIThread* GetThread() {
     return Get()->mMediaThread;
   }
