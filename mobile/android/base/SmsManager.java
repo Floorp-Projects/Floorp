5e50c1feeaed97bf5000c68e1fa3a7c6ac384b35
Chris Kitching <chriskitching@linux.com>
2014-09-04 18:43:09 -0700

    Bug 1062112: Part 1h: Remove redundant keywords from GeckoSmsManager. r=nalexander

--- mobile/android/base/SmsManager.java
+++ mobile/android/base/SmsManager.java
@@ -24,16 +24,16 @@ public class SmsManager {
     }
 
     public interface ISmsManager {
-        public void start();
-        public void stop();
-        public void shutdown();
+        void start();
+        void stop();
+        void shutdown();
 
-        public void send(String aNumber, String aMessage, int aRequestId);
-        public void getMessage(int aMessageId, int aRequestId);
-        public void deleteMessage(int aMessageId, int aRequestId);
-        public void createMessageList(long aStartDate, long aEndDate, String[] aNumbers, int aNumbersCount, String aDelivery, boolean aHasRead, boolean aRead, long aThreadId, boolean aReverse, int aRequestId);
-        public void getNextMessageInList(int aListId, int aRequestId);
-        public void clearMessageList(int aListId);
+        void send(String aNumber, String aMessage, int aRequestId);
+        void getMessage(int aMessageId, int aRequestId);
+        void deleteMessage(int aMessageId, int aRequestId);
+        void createMessageList(long aStartDate, long aEndDate, String[] aNumbers, int aNumbersCount, String aDelivery, boolean aHasRead, boolean aRead, long aThreadId, boolean aReverse, int aRequestId);
+        void getNextMessageInList(int aListId, int aRequestId);
+        void clearMessageList(int aListId);
     }
 }
 
