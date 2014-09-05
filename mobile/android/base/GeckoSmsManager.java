8fb5b961981b6b4440acd28a1ddbf5286b59cfca
Chris Kitching <chriskitching@linux.com>
2014-09-04 18:13:05 -0700

    Bug 1062112: Part 1a: Move ISmsManager. r=nalexander

--- mobile/android/base/GeckoSmsManager.java
+++ mobile/android/base/GeckoSmsManager.java
@@ -24,6 +24,7 @@ import android.telephony.SmsMessage;
 import android.util.Log;
 
 import static android.telephony.SmsMessage.MessageClass;
+import static org.mozilla.gecko.SmsManager.ISmsManager;
 
 import java.util.ArrayList;
 
