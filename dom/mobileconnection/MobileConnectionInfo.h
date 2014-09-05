fe17ac2d6184011f80fedf2505b8ff7924593d8d
Ehsan Akhgari <ehsan@mozilla.com>
2014-09-04 23:22:49 -0400

    Bug 1063086 - Fix more bad implicit constructors in DOM; r=baku

--- dom/mobileconnection/MobileConnectionInfo.h
+++ dom/mobileconnection/MobileConnectionInfo.h
@@ -25,7 +25,7 @@ public:
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(MobileConnectionInfo)
 
-  MobileConnectionInfo(nsPIDOMWindow* aWindow);
+  explicit MobileConnectionInfo(nsPIDOMWindow* aWindow);
 
   MobileConnectionInfo(const nsAString& aState, bool aConnected,
                        bool aEmergencyCallsOnly, bool aRoaming,
