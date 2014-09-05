adbc589a65fcf9af7960400b43b37db4fe5db865
Chris Kitching <chriskitching@linux.com>
2014-09-04 18:13:05 -0700

    Bug 1062112: Part 1a: Move ISmsManager. r=nalexander

--- mobile/android/base/SmsManager.java
+++ mobile/android/base/SmsManager.java
@@ -5,8 +5,7 @@
 
 package org.mozilla.gecko;
 
-class SmsManager
-{
+public class SmsManager {
     static private ISmsManager sInstance;
 
     static public ISmsManager getInstance() {
@@ -17,18 +16,18 @@ class SmsManager
         }
         return sInstance;
     }
+
+    public interface ISmsManager {
+        public void start();
+        public void stop();
+        public void shutdown();
+
+        public void send(String aNumber, String aMessage, int aRequestId);
+        public void getMessage(int aMessageId, int aRequestId);
+        public void deleteMessage(int aMessageId, int aRequestId);
+        public void createMessageList(long aStartDate, long aEndDate, String[] aNumbers, int aNumbersCount, String aDelivery, boolean aHasRead, boolean aRead, long aThreadId, boolean aReverse, int aRequestId);
+        public void getNextMessageInList(int aListId, int aRequestId);
+        public void clearMessageList(int aListId);
+    }
 }
 
-interface ISmsManager
-{
-  public void start();
-  public void stop();
-  public void shutdown();
-
-  public void send(String aNumber, String aMessage, int aRequestId);
-  public void getMessage(int aMessageId, int aRequestId);
-  public void deleteMessage(int aMessageId, int aRequestId);
-  public void createMessageList(long aStartDate, long aEndDate, String[] aNumbers, int aNumbersCount, String aDelivery, boolean aHasRead, boolean aRead, long aThreadId, boolean aReverse, int aRequestId);
-  public void getNextMessageInList(int aListId, int aRequestId);
-  public void clearMessageList(int aListId);
-}
