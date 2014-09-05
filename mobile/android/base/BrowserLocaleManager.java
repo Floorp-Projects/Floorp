65dfac00f1eca61e096fe53ce9bfb7b86a4c090a
Richard Newman <rnewman@mozilla.com>
2014-09-05 09:38:22 -0700

    Bug 1063274 - Add getLanguage method to BrowserLocaleManager. r=margaret

--- mobile/android/base/BrowserLocaleManager.java
+++ mobile/android/base/BrowserLocaleManager.java
@@ -80,6 +80,32 @@ public class BrowserLocaleManager implements LocaleManager {
     }
 
     /**
+     * Sometimes we want just the language for a locale, not the entire
+     * language tag. But Java's .getLanguage method is wrong.
+     *
+     * This method is equivalent to the first part of {@link #getLanguageTag(Locale)}.
+     *
+     * @return a language string, such as "he" for the Hebrew locales.
+     */
+    public static String getLanguage(final Locale locale) {
+        final String language = locale.getLanguage();  // Can, but should never be, an empty string.
+        // Modernize certain language codes.
+        if (language.equals("iw")) {
+            return "he";
+        }
+
+        if (language.equals("in")) {
+            return "id";
+        }
+
+        if (language.equals("ji")) {
+            return "yi";
+        }
+
+        return language;
+    }
+
+    /**
      * Gecko uses locale codes like "es-ES", whereas a Java {@link Locale}
      * stringifies as "es_ES".
      *
@@ -91,17 +117,8 @@ public class BrowserLocaleManager implements LocaleManager {
         // If this were Java 7:
         // return locale.toLanguageTag();
 
-        String language = locale.getLanguage();  // Can, but should never be, an empty string.
-        // Modernize certain language codes.
-        if (language.equals("iw")) {
-            language = "he";
-        } else if (language.equals("in")) {
-            language = "id";
-        } else if (language.equals("ji")) {
-            language = "yi";
-        }
-
-        String country = locale.getCountry();    // Can be an empty string.
+        final String language = getLanguage(locale);
+        final String country = locale.getCountry();    // Can be an empty string.
         if (country.equals("")) {
             return language;
         }
