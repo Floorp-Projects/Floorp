5bed8ddd385921ea90ead2773d9a1b957eeda513
Ryan VanderMeulen <ryanvm@gmail.com>
2014-09-08 18:20:32 -0400

    Backed out changeset 080344c7c80b (bug 1042715) for Android bustage.

--- mobile/android/base/tests/testRestrictedProfiles.java
+++
@@ -1,9 +0,0 @@
-package org.mozilla.gecko.tests;
-
-
-
-public class testRestrictedProfiles extends JavascriptTest {
-    public testRestrictedProfiles() {
-        super("testRestrictedProfiles.js");
-    }
-}
