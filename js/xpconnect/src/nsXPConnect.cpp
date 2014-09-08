06cc2ae3c28cb8df2aefb078eabc99ec327c9945
Bobby Holley <bobbyholley@gmail.com>
2014-09-08 13:36:22 -0700

    Bug 1062631 - Remove exception swallowing code in NS_ScriptErrorReporter. r=bz
    See bug 1062631 comment 0.

--- js/xpconnect/src/nsXPConnect.cpp
+++ js/xpconnect/src/nsXPConnect.cpp
@@ -1373,14 +1373,6 @@ nsXPConnect::ReadFunction(nsIObjectInputStream *stream, JSContext *cx, JSObject 
     return ReadScriptOrFunction(stream, cx, nullptr, functionObjp);
 }
 
-NS_IMETHODIMP
-nsXPConnect::MarkErrorUnreported(JSContext *cx)
-{
-    XPCContext *xpcc = XPCContext::GetXPCContext(cx);
-    xpcc->MarkErrorUnreported();
-    return NS_OK;
-}
-
 /* These are here to be callable from a debugger */
 extern "C" {
 JS_EXPORT_API(void) DumpJSStack()
