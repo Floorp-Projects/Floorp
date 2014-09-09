be76304697cc60f8f42e95bf4d3d8623c8a00337
Jan-Ivar Bruaroey <jib@mozilla.com>
2014-09-09 01:12:24 -0400

    Bug 1063808 - Support old constraint-like RTCOfferOptions for a bit. r=smaug, r=abr

--- dom/media/tests/mochitest/pc.js
+++ dom/media/tests/mochitest/pc.js
@@ -2141,7 +2141,18 @@ PeerConnectionWrapper.prototype = {
     if (!options) {
       return 0;
     }
-    if (options.offerToReceiveAudio) {
+
+    var offerToReceiveAudio = options.offerToReceiveAudio;
+
+    // TODO: Remove tests of old constraint-like RTCOptions soon (Bug 1064223).
+    if (options.mandatory && options.mandatory.OfferToReceiveAudio !== undefined) {
+      offerToReceiveAudio = options.mandatory.OfferToReceiveAudio;
+    } else if (options.optional && options.optional[0] &&
+               options.optional[0].OfferToReceiveAudio !== undefined) {
+      offerToReceiveAudio = options.optional[0].OfferToReceiveAudio;
+    }
+
+    if (offerToReceiveAudio) {
       return 1;
     } else {
       return 0;
@@ -2179,7 +2190,18 @@ PeerConnectionWrapper.prototype = {
     if (!options) {
       return 0;
     }
-    if (options.offerToReceiveVideo) {
+
+    var offerToReceiveVideo = options.offerToReceiveVideo;
+
+    // TODO: Remove tests of old constraint-like RTCOptions soon (Bug 1064223).
+    if (options.mandatory && options.mandatory.OfferToReceiveVideo !== undefined) {
+      offerToReceiveVideo = options.mandatory.OfferToReceiveVideo;
+    } else if (options.optional && options.optional[0] &&
+               options.optional[0].OfferToReceiveVideo !== undefined) {
+      offerToReceiveVideo = options.optional[0].OfferToReceiveVideo;
+    }
+
+    if (offerToReceiveVideo) {
       return 1;
     } else {
       return 0;
