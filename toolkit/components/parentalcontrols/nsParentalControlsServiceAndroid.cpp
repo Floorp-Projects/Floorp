6ba11ae36cc1c43554f7175a1adf152e96af7170
Wes Johnston <wjohnston@mozilla.com>
2014-09-09 15:08:37 -0700

    Bug 1058150 - Use restricted profiles for guest mode. r=mfinkle

--- toolkit/components/parentalcontrols/nsParentalControlsServiceAndroid.cpp
+++ toolkit/components/parentalcontrols/nsParentalControlsServiceAndroid.cpp
@@ -14,7 +14,7 @@ nsParentalControlsService::nsParentalControlsService() :
   mEnabled(false)
 {
   if (mozilla::AndroidBridge::HasEnv()) {
-    mEnabled = mozilla::widget::android::GeckoAppShell::IsUserRestricted();
+    mEnabled = mozilla::widget::android::RestrictedProfiles::IsUserRestricted();
   }
 }
 
@@ -32,7 +32,11 @@ nsParentalControlsService::GetParentalControlsEnabled(bool *aResult)
 NS_IMETHODIMP
 nsParentalControlsService::GetBlockFileDownloadsEnabled(bool *aResult)
 {
-  return NS_ERROR_NOT_AVAILABLE;
+  bool res;
+  IsAllowed(nsIParentalControlsService::DOWNLOAD, NULL, &res);
+  *aResult = res;
+
+  return NS_OK;
 }
 
 NS_IMETHODIMP
@@ -63,5 +67,32 @@ nsParentalControlsService::RequestURIOverrides(nsIArray *aTargets,
                                                nsIInterfaceRequestor *aWindowContext,
                                                bool *_retval)
 {
+  return NS_ERROR_NOT_AVAILABLE;
+}
+
+NS_IMETHODIMP
+nsParentalControlsService::IsAllowed(int16_t aAction,
+                                     nsIURI *aUri,
+                                     bool *_retval)
+{
+  nsresult rv = NS_OK;
+  *_retval = true;
+
+  if (!mEnabled) {
+    return rv;
+  }
+
+  if (mozilla::AndroidBridge::HasEnv()) {
+    nsAutoCString url;
+    if (aUri) {
+      rv = aUri->GetSpec(url);
+      NS_ENSURE_SUCCESS(rv, rv);
+    }
+
+    *_retval = mozilla::widget::android::RestrictedProfiles::IsAllowed(aAction,
+                                                    NS_ConvertUTF8toUTF16(url));
+    return rv;
+  }
+
   return NS_ERROR_NOT_AVAILABLE;
 }
