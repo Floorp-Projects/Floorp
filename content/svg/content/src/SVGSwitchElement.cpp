cfdab96ef6d6b9f006a4286f257c04619a0d1111
Robert Longson <longsonr@gmail.com>
2014-09-09 13:00:03 +0100

    Bug 1064063 - switch elements ignore default if allowReorder is set. r=dholbert

--- content/svg/content/src/SVGSwitchElement.cpp
+++ content/svg/content/src/SVGSwitchElement.cpp
@@ -125,8 +125,8 @@ nsIContent *
 SVGSwitchElement::FindActiveChild() const
 {
   bool allowReorder = AttrValueIs(kNameSpaceID_None,
-                                    nsGkAtoms::allowReorder,
-                                    nsGkAtoms::yes, eCaseMatters);
+                                  nsGkAtoms::allowReorder,
+                                  nsGkAtoms::yes, eCaseMatters);
 
   const nsAdoptingString& acceptLangs =
     Preferences::GetLocalizedString("intl.accept_languages");
@@ -134,6 +134,7 @@ SVGSwitchElement::FindActiveChild() const
   if (allowReorder && !acceptLangs.IsEmpty()) {
     int32_t bestLanguagePreferenceRank = -1;
     nsIContent *bestChild = nullptr;
+    nsIContent *defaultChild = nullptr;
     for (nsIContent* child = nsINode::GetFirstChild();
          child;
          child = child->GetNextSibling()) {
@@ -152,7 +153,12 @@ SVGSwitchElement::FindActiveChild() const
             // best possible match
             return child;
           case -1:
-            // not found
+            // no match
+            break;
+          case -2:
+            // no systemLanguage attribute. If there's nothing better
+            // we'll use the last such child.
+            defaultChild = child;
             break;
           default:
             if (bestLanguagePreferenceRank == -1 ||
@@ -167,7 +173,7 @@ SVGSwitchElement::FindActiveChild() const
          bestChild = child;
       }
     }
-    return bestChild;
+    return bestChild ? bestChild : defaultChild;
   }
 
   for (nsIContent* child = nsINode::GetFirstChild();
--- content/svg/content/src/SVGTests.h
+++ content/svg/content/src/SVGTests.h
@@ -42,7 +42,8 @@ public:
    * exactly equals one of the language names or exactly equals a prefix of
    * one of the language names in the systemLanguage attribute.
    * @returns 2 * the lowest index in the aAcceptLangs that matches + 1
-   * if only the prefix matches, or -1 if no indices match.
+   * if only the prefix matches, -2 if there's no systemLanguage attribute,
+   * or -1 if no indices match.
    * XXX This algorithm is O(M*N).
    */
   int32_t GetBestLanguagePreferenceRank(const nsSubstring& aAcceptLangs) const;
