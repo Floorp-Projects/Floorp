9ad6d9f69c1278f2b1269516de1b479deca8f602
Wander Lairson Costa <wcosta@mozilla.com>
2014-09-09 13:44:00 +0200

    Bug 1061800 - Add breakpad ids to profiler in Linux. r=BenWa
    By default, breakpad uses the build-id section for ids, if it isn't
    found, it uses the .text section. On the later case, the stripped
    libraries will have different ids from their non-stripped counterparts.
    
    See Bug 1062459 for details.

--- tools/profiler/shared-libraries-linux.cc
+++ tools/profiler/shared-libraries-linux.cc
@@ -15,6 +15,34 @@
 #include "platform.h"
 #include "shared-libraries.h"
 
+#include "common/linux/file_id.h"
+#include <algorithm>
+
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
+
+// Get the breakpad Id for the binary file pointed by bin_name
+static std::string getId(const char *bin_name)
+{
+  using namespace google_breakpad;
+  using namespace std;
+
+  uint8_t identifier[kMDGUIDSize];
+  char id_str[37]; // magic number from file_id.h
+
+  FileID file_id(bin_name);
+  if (file_id.ElfFileIdentifier(identifier)) {
+    FileID::ConvertIdentifierToString(identifier, id_str, ARRAY_SIZE(id_str));
+    // ConvertIdentifierToString converts the identifier to a string with
+    // some dashes (don't ask me why), but we need it raw, so remove the dashes.
+    char *id_end = remove(id_str, id_str + strlen(id_str), '-');
+    // Also add an extra "0" by the end.  google-breakpad does it for
+    // consistency with PDB files so we need to do too.
+    return string(id_str, id_end) + '0';
+  }
+
+  return "";
+}
+
 #if !defined(MOZ_WIDGET_GONK)
 // TODO fix me with proper include
 #include "nsDebug.h"
@@ -56,7 +84,8 @@ int dl_iterate_callback(struct dl_phdr_info *dl_info, size_t size, void *data)
     if (end > libEnd)
       libEnd = end;
   }
-  SharedLibrary shlib(libStart, libEnd, 0, "", dl_info->dlpi_name);
+  const char *name = dl_info->dlpi_name;
+  SharedLibrary shlib(libStart, libEnd, 0, getId(name), name);
   info.AddSharedLibrary(shlib);
 
   return 0;
@@ -121,7 +150,7 @@ SharedLibraryInfo SharedLibraryInfo::GetInfoForSelf()
       continue;
     }
 #endif
-    SharedLibrary shlib(start, end, offset, "", name);
+    SharedLibrary shlib(start, end, offset, getId(name), name);
     info.AddSharedLibrary(shlib);
     if (count > 10000) {
       LOG("Get maps failed");
