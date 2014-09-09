125e0802748922e3cf8f4fcacf3aad48925e7397
Ryan VanderMeulen <ryanvm@gmail.com>
2014-09-09 19:57:12 -0400

    Backed out changeset 96a507248c53 (bug 1059583) for Werror bustage. CLOSED TREE

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -1170,18 +1170,6 @@ private:
 
 } // end namespace mozilla
 
-bool
-WebGLContext::HasAlpha() const
-{
-  return gl->Caps().alpha;
-}
-
-static bool
-IsElementFullscreen(dom::Element* elem)
-{
-    return elem->State().HasState(NS_EVENT_STATE_FULL_SCREEN);
-}
-
 already_AddRefed<layers::CanvasLayer>
 WebGLContext::GetCanvasLayer(nsDisplayListBuilder* aBuilder,
                              CanvasLayer *aOldLayer,
@@ -1224,20 +1212,14 @@ WebGLContext::GetCanvasLayer(nsDisplayListBuilder* aBuilder,
     canvasLayer->SetUserData(&gWebGLLayerUserData, userData);
 
     CanvasLayer::Data data;
-
     data.mGLContext = gl;
     data.mSize = nsIntSize(mWidth, mHeight);
-    data.mHasAlpha = HasAlpha();
+    data.mHasAlpha = gl->Caps().alpha;
     data.mIsGLAlphaPremult = IsPremultAlpha() || !data.mHasAlpha;
-    data.mIsElemFullscreen = IsElementFullscreen(GetCanvas());
 
     canvasLayer->Initialize(data);
-
-    if (!data.mHasAlpha) {
-        uint32_t flags = canvasLayer->GetContentFlags();
-        flags |= Layer::CONTENT_OPAQUE;
-        canvasLayer->SetContentFlags(flags);
-    }
+    uint32_t flags = gl->Caps().alpha ? 0 : Layer::CONTENT_OPAQUE;
+    canvasLayer->SetContentFlags(flags);
     canvasLayer->Updated();
 
     mResetLayer = false;
