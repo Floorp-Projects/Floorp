cc28676e0c1d40a7363844b232970d416251021d
Eric Faust <efaustbmo@gmail.com>
2014-09-09 12:09:45 -0700

    Bug 1031092 - Part 1: Move jsproxy.* to new js/src/proxy/. (r=bholley)

--- js/src/proxy/jsproxy.h
+++ js/src/proxy/jsproxy.h
@@ -4,8 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef jsproxy_h
-#define jsproxy_h
+#ifndef proxy_jsproxy_h
+#define proxy_jsproxy_h
 
 #include "mozilla/Maybe.h"
 
