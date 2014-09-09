d68021c5cc7ac2ea2418bcfd5dd67ca3814d8213
Eric Faust <efaustbmo@gmail.com>
2014-09-09 14:31:20 -0700

    Bug 1031092 followup - Give up on moving jsproxy.h and jswrapper.h; fix bustage on a CLOSED TREE. (r=BurningManWasLastWeekRight?)

--- js/src/jswrapper.h
+++ js/src/jswrapper.h
@@ -4,8 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef proxy_jswrapper_h
-#define proxy_jswrapper_h
+#ifndef jswrapper_h
+#define jswrapper_h
 
 #include "mozilla/Attributes.h"
 
--- js/src/proxy/SecurityWrapper.cpp
+++ js/src/proxy/SecurityWrapper.cpp
@@ -4,9 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "jswrapper.h"
-
 #include "jsapi.h"
+#include "jswrapper.h"
 
 #include "jsatominlines.h"
 
