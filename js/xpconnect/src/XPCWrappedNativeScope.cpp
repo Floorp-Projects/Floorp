382f4efc2f44ae1a4d3dd443150a75e8df1a5d04
Felipe Gomes <felipc@gmail.com>
2014-09-10 15:53:55 -0300

    Bug 1063848 - Disable e10s in safe mode. r=bsmedberg,ally.

--- js/xpconnect/src/XPCWrappedNativeScope.cpp
+++ js/xpconnect/src/XPCWrappedNativeScope.cpp
@@ -14,6 +14,7 @@
 #include "mozilla/MemoryReporting.h"
 #include "mozilla/Preferences.h"
 #include "nsIAddonInterposition.h"
+#include "nsIXULRuntime.h"
 
 #include "mozilla/dom/BindingUtils.h"
 
@@ -207,7 +208,7 @@ CompartmentPerAddon()
 
     if (!initialized) {
         pref = Preferences::GetBool("dom.compartment_per_addon", false) ||
-               Preferences::GetBool("browser.tabs.remote.autostart", false);
+               BrowserTabsRemoteAutostart();
         initialized = true;
     }
 
