3c0efeaf4d1c9bee7f6e04d71e9f4eab483a7188
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-09-10 12:06:53 +0200

    Bug 1063518 - Hide MLS "Learn More" link when MLS is disabled. r=liuche, a=lmandel

--- mobile/android/base/preferences/GeckoPreferences.java
+++ mobile/android/base/preferences/GeckoPreferences.java
@@ -674,7 +674,9 @@ OnSharedPreferenceChangeListener
                     preferences.removePreference(pref);
                     i--;
                     continue;
-                } else if (AppConstants.RELEASE_BUILD && PREFS_GEO_REPORTING.equals(key)) {
+                } else if (AppConstants.RELEASE_BUILD &&
+                            (PREFS_GEO_REPORTING.equals(key) ||
+                             PREFS_GEO_LEARN_MORE.equals(key))) {
                     // We don't build wifi/cell tower collection in release builds, so hide the UI.
                     preferences.removePreference(pref);
                     i--;
