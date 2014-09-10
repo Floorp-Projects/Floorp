c87c16205e5b432a03e33817c6e7b749ec89a68e
Ryan VanderMeulen <ryanvm@gmail.com>
2014-09-10 17:26:53 -0400

    Backed out changeset 62d1670e37d9 (bug 896896) for bringing back bug 933733. a=lmandel

--- widget/windows/nsAppShell.cpp
+++ widget/windows/nsAppShell.cpp
@@ -308,7 +308,7 @@ nsAppShell::ProcessNextNativeEvent(bool mayWait)
       mozilla::HangMonitor::Suspend();
       {
         GeckoProfilerSleepRAII profiler_sleep;
-        WinUtils::WaitForMessage();
+        ::WaitMessage();
       }
     }
   } while (!gotMessage && mayWait);
