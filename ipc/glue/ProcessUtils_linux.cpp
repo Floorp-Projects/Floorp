9a906aa48f72600cdd12f29b7b67386f1bb45d97
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-09-10 14:52:47 +0200

    Backed out changeset 0a76964b8d8d (bug 1048011) for causing regression

--- ipc/glue/ProcessUtils_linux.cpp
+++ ipc/glue/ProcessUtils_linux.cpp
@@ -121,22 +121,12 @@ public:
 void
 ProcLoaderParent::ActorDestroy(ActorDestroyReason aWhy)
 {
-  if (aWhy == AbnormalShutdown) {
-    NS_WARNING("ProcLoaderParent is destroyed abnormally.");
-  }
-
-  if (sProcLoaderClientOnDeinit) {
-    // Get error for closing while the channel is already error.
-    return;
-  }
-
-  // Destroy self asynchronously.
-  ProcLoaderClientDeinit();
 }
 
 static void
 _ProcLoaderParentDestroy(PProcLoaderParent *aLoader)
 {
+  aLoader->Close();
   delete aLoader;
   sProcLoaderClientOnDeinit = false;
 }
@@ -145,6 +135,7 @@ bool
 ProcLoaderParent::RecvLoadComplete(const int32_t &aPid,
                                    const int32_t &aCookie)
 {
+  ProcLoaderClientDeinit();
   return true;
 }
 
