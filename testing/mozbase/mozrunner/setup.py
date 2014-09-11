bb6fe36f4ef8a45ba9708e556cd2f69b575f1755
Dave Hunt <dhunt@mozilla.com>
2014-09-11 07:57:00 -0400

    Bug 1066097 - Bump mozrunner version to 6.3. r=ahal

--- testing/mozbase/mozrunner/setup.py
+++ testing/mozbase/mozrunner/setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = 'mozrunner'
-PACKAGE_VERSION = '6.2'
+PACKAGE_VERSION = '6.3'
 
 desc = """Reliable start/stop/configuration of Mozilla Applications (Firefox, Thunderbird, etc.)"""
 
