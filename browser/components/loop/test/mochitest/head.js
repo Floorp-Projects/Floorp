35d61aae2c48038640e79eac2877d1dd65411ae3
Jared Wein <jwein@mozilla.com>
2014-09-11 15:36:14 -0400

    Bug 1047146 - Add current username to the Loop panel footer. r=mattn r=niko
    --HG--
    extra : rebase_source : ba9f55a9a28d1a4df6284582ded6d761e269d5cf

--- browser/components/loop/test/mochitest/head.js
+++ browser/components/loop/test/mochitest/head.js
@@ -58,17 +58,19 @@ function promiseGetMozLoopAPI() {
  *
  * This assumes that the tests are running in a generatorTest.
  */
-function loadLoopPanel() {
+function loadLoopPanel(aOverrideOptions = {}) {
   // Set prefs to ensure we don't access the network externally.
-  Services.prefs.setCharPref("services.push.serverURL", "ws://localhost/");
-  Services.prefs.setCharPref("loop.server", "http://localhost/");
+  Services.prefs.setCharPref("services.push.serverURL", aOverrideOptions.pushURL || "ws://localhost/");
+  Services.prefs.setCharPref("loop.server", aOverrideOptions.loopURL || "http://localhost/");
 
   // Turn off the network for loop tests, so that we don't
   // try to access the remote servers. If we want to turn this
   // back on in future, be careful to check for intermittent
   // failures.
   let wasOffline = Services.io.offline;
-  Services.io.offline = true;
+  if (!aOverrideOptions.stayOnline) {
+    Services.io.offline = true;
+  }
 
   registerCleanupFunction(function() {
     Services.prefs.clearUserPref("services.push.serverURL");
@@ -103,6 +105,7 @@ function resetFxA() {
   global.gFxAOAuthClientPromise = null;
   global.gFxAOAuthClient = null;
   global.gFxAOAuthTokenData = null;
+  global.gFxAOAuthProfile = null;
   const fxASessionPref = MozLoopServiceInternal.getSessionTokenPrefName(LOOP_SESSION_TYPE.FXA);
   Services.prefs.clearUserPref(fxASessionPref);
 }
@@ -116,6 +119,15 @@ function promiseDeletedOAuthParams(baseURL) {
   xhr.addEventListener("error", deferred.reject);
   xhr.send();
 
+  return deferred.promise;
+}
+
+function promiseObserverNotified(aTopic) {
+  let deferred = Promise.defer();
+  Services.obs.addObserver(function onNotification(aSubject, aTopic, aData) {
+    Services.obs.removeObserver(onNotification, aTopic);
+      deferred.resolve({subject: aSubject, data: aData});
+    }, aTopic, false);
   return deferred.promise;
 }
 
