4ce1e201e02e8d8cb324686a74e30ed01f04d8a8
Mark Finkle <mfinkle@mozilla.com>
2014-09-10 22:09:09 -0400

    Bug 887755 - Lightweight theme preview is broken. r=margaret, a=lmandel

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -89,6 +89,9 @@ XPCOMUtils.defineLazyModuleGetter(this, "CharsetMenu",
 XPCOMUtils.defineLazyModuleGetter(this, "NetErrorHelper",
                                   "resource://gre/modules/NetErrorHelper.jsm");
 
+XPCOMUtils.defineLazyModuleGetter(this, "PermissionsUtils",
+                                  "resource://gre/modules/PermissionsUtils.jsm");
+
 // Lazily-loaded browser scripts:
 [
   ["SelectHelper", "chrome://browser/content/SelectHelper.js"],
@@ -2840,6 +2843,9 @@ var LightWeightThemeWebInstaller = {
   },
 
   _isAllowed: function (node) {
+    // Make sure the whitelist has been imported to permissions
+    PermissionsUtils.importFromPrefs("xpinstall.", "install");
+
     let pm = Services.perms;
 
     let uri = node.ownerDocument.documentURIObject;
