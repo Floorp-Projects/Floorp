98d05ed44f71b50ff1df0be340890a2acc10720a
Benjamin Bouvier <benj@benj.me>
2014-09-11 08:50:10 +0200

    Bug 1051860: Optimize SimdValueX4 codegen for float32x4 with unpcklps; r=sunfish

--- js/src/jit/x86/LOpcodes-x86.h
+++ js/src/jit/x86/LOpcodes-x86.h
@@ -21,6 +21,8 @@
     _(AsmJSUInt32ToDouble)      \
     _(AsmJSUInt32ToFloat32)     \
     _(AsmJSLoadFuncPtr)         \
+    _(SimdValueInt32x4)         \
+    _(SimdValueFloat32x4)       \
     _(UDivOrMod)
 
 #endif /* jit_x86_LOpcodes_x86_h */
