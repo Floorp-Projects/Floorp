1bf5a203663a414d829332dc6a21c8fd933f7940
Nicolas Perriault <nperriault@gmail.com>
2014-09-11 20:54:48 +0100

    Bug 1062126: Loop panel UI shouldn't be fully reset when reopened, r=mikedeboer.
    --HG--
    extra : transplant_source : %FD%A1%23K%A9%9DW%D8%27a%CF%E7f%9F%AB%89K%A2%92%CE

---
+++ browser/components/loop/test/shared/mixins_test.js
@@ -0,0 +1,70 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* global loop, sinon */
+/* jshint newcap:false */
+
+var expect = chai.expect;
+
+describe("loop.shared.mixins", function() {
+  "use strict";
+
+  var sandbox;
+  var sharedMixins = loop.shared.mixins;
+
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  describe("loop.panel.DocumentVisibilityMixin", function() {
+    var comp, TestComp, onDocumentVisibleStub, onDocumentHiddenStub;
+
+    beforeEach(function() {
+      onDocumentVisibleStub = sandbox.stub();
+      onDocumentHiddenStub = sandbox.stub();
+
+      TestComp = React.createClass({
+        mixins: [loop.shared.mixins.DocumentVisibilityMixin],
+        onDocumentHidden: onDocumentHiddenStub,
+        onDocumentVisible: onDocumentVisibleStub,
+        render: function() {
+          return React.DOM.div();
+        }
+      });
+    });
+
+    function setupFakeVisibilityEventDispatcher(event) {
+      loop.shared.mixins.setRootObject({
+        document: {
+          addEventListener: function(_, fn) {
+            fn(event);
+          },
+          removeEventListener: sandbox.stub()
+        }
+      });
+    }
+
+    it("should call onDocumentVisible when document visibility changes to visible",
+      function() {
+        setupFakeVisibilityEventDispatcher({target: {hidden: false}});
+
+        comp = TestUtils.renderIntoDocument(TestComp());
+
+        sinon.assert.calledOnce(onDocumentVisibleStub);
+      });
+
+    it("should call onDocumentVisible when document visibility changes to hidden",
+      function() {
+        setupFakeVisibilityEventDispatcher({target: {hidden: true}});
+
+        comp = TestUtils.renderIntoDocument(TestComp());
+
+        sinon.assert.calledOnce(onDocumentHiddenStub);
+      });
+  });
+});
