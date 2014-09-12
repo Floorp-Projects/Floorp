384ef32e1461061420cb558c9f7e507f74a64a41
Matt Woodrow <mwoodrow@mozilla.com>
2014-09-12 17:18:21 +1200

    Bug 1044702 - Use Moz2D source clipping when we can. r=Bas
    --HG--
    extra : rebase_source : 24eb333909db5278ff1a8babf9b4b907650695fa

--- gfx/thebes/gfxDrawable.cpp
+++ gfx/thebes/gfxDrawable.cpp
@@ -27,12 +27,48 @@ gfxSurfaceDrawable::gfxSurfaceDrawable(SourceSurface* aSurface,
 }
 
 bool
+gfxSurfaceDrawable::DrawWithSamplingRect(gfxContext* aContext,
+                                         const gfxRect& aFillRect,
+                                         const gfxRect& aSamplingRect,
+                                         bool aRepeat,
+                                         const GraphicsFilter& aFilter,
+                                         gfxFloat aOpacity)
+{
+  // When drawing with CLAMP we can expand the sampling rect to the nearest pixel
+  // without changing the result.
+  gfxRect samplingRect = aSamplingRect;
+  samplingRect.RoundOut();
+  IntRect intRect(samplingRect.x, samplingRect.y, samplingRect.width, samplingRect.height);
+
+  IntSize size = mSourceSurface->GetSize();
+  if (!IntRect(0, 0, size.width, size.height).Contains(intRect)) {
+    return false;
+  }
+
+  DrawInternal(aContext, aFillRect, intRect, false, aFilter, aOpacity, gfxMatrix());
+  return true;
+}
+
+bool
 gfxSurfaceDrawable::Draw(gfxContext* aContext,
                          const gfxRect& aFillRect,
                          bool aRepeat,
                          const GraphicsFilter& aFilter,
                          gfxFloat aOpacity,
                          const gfxMatrix& aTransform)
+{
+  DrawInternal(aContext, aFillRect, IntRect(), aRepeat, aFilter, aOpacity, aTransform);
+  return true;
+}
+
+void
+gfxSurfaceDrawable::DrawInternal(gfxContext* aContext,
+                                 const gfxRect& aFillRect,
+                                 const IntRect& aSamplingRect,
+                                 bool aRepeat,
+                                 const GraphicsFilter& aFilter,
+                                 gfxFloat aOpacity,
+                                 const gfxMatrix& aTransform)
 {
     ExtendMode extend = ExtendMode::CLAMP;
 
@@ -44,7 +80,7 @@ gfxSurfaceDrawable::Draw(gfxContext* aContext,
     patternTransform.Invert();
 
     SurfacePattern pattern(mSourceSurface, extend,
-                           patternTransform, ToFilter(aFilter));
+                           patternTransform, ToFilter(aFilter), aSamplingRect);
 
     Rect fillRect = ToRect(aFillRect);
     DrawTarget* dt = aContext->GetDrawTarget();
@@ -64,7 +100,6 @@ gfxSurfaceDrawable::Draw(gfxContext* aContext,
                 AntialiasMode::SUBPIXEL;
         dt->FillRect(fillRect, pattern, DrawOptions(aOpacity, op, aaMode));
     }
-    return true;
 }
 
 gfxCallbackDrawable::gfxCallbackDrawable(gfxDrawingCallback* aCallback,
