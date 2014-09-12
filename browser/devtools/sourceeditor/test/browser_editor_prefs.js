998b74599e39b147b7e2f0c7c96264bd13074b94
Adrian Aichner <adrian.aichner@gmail.com>
2014-09-12 13:36:00 +0200

    Bug 1058183 - Expose code folding as an option for all editor instances. r=bgrins

--- browser/devtools/sourceeditor/test/browser_editor_prefs.js
+++ browser/devtools/sourceeditor/test/browser_editor_prefs.js
@@ -7,6 +7,7 @@
 // Test to make sure that the editor reacts to preference changes
 
 const TAB_SIZE    = "devtools.editor.tabsize";
+const ENABLE_CODE_FOLDING = "devtools.editor.enableCodeFolding";
 const EXPAND_TAB  = "devtools.editor.expandtab";
 const KEYMAP      = "devtools.editor.keymap";
 const AUTO_CLOSE  = "devtools.editor.autoclosebrackets";
@@ -17,6 +18,11 @@ function test() {
   waitForExplicitFinish();
   setup((ed, win) => {
 
+    Assert.deepEqual(ed.getOption("gutters"), [
+      "CodeMirror-linenumbers",
+      "breakpoints",
+      "CodeMirror-foldgutter"], "gutters is correct");
+
     ed.setText("Checking preferences.");
 
     info ("Turning prefs off");
@@ -24,14 +30,21 @@ function test() {
     ed.setOption("autocomplete", true);
 
     Services.prefs.setIntPref(TAB_SIZE, 2);
+    Services.prefs.setBoolPref(ENABLE_CODE_FOLDING, false);
     Services.prefs.setBoolPref(EXPAND_TAB, false);
     Services.prefs.setCharPref(KEYMAP, "default");
     Services.prefs.setBoolPref(AUTO_CLOSE, false);
     Services.prefs.setBoolPref(AUTOCOMPLETE, false);
     Services.prefs.setBoolPref(DETECT_INDENT, false);
 
+    Assert.deepEqual(ed.getOption("gutters"), [
+      "CodeMirror-linenumbers",
+      "breakpoints"], "gutters is correct");
+
     is(ed.getOption("tabSize"), 2, "tabSize is correct");
     is(ed.getOption("indentUnit"), 2, "indentUnit is correct");
+    is(ed.getOption("foldGutter"), false, "foldGutter is correct");
+    is(ed.getOption("enableCodeFolding"), undefined, "enableCodeFolding is correct");
     is(ed.getOption("indentWithTabs"), true, "indentWithTabs is correct");
     is(ed.getOption("keyMap"), "default", "keyMap is correct");
     is(ed.getOption("autoCloseBrackets"), "", "autoCloseBrackets is correct");
@@ -41,18 +54,45 @@ function test() {
     info ("Turning prefs on");
 
     Services.prefs.setIntPref(TAB_SIZE, 4);
+    Services.prefs.setBoolPref(ENABLE_CODE_FOLDING, true);
     Services.prefs.setBoolPref(EXPAND_TAB, true);
     Services.prefs.setCharPref(KEYMAP, "sublime");
     Services.prefs.setBoolPref(AUTO_CLOSE, true);
     Services.prefs.setBoolPref(AUTOCOMPLETE, true);
 
+    Assert.deepEqual(ed.getOption("gutters"), [
+      "CodeMirror-linenumbers",
+      "breakpoints",
+      "CodeMirror-foldgutter"], "gutters is correct");
+
     is(ed.getOption("tabSize"), 4, "tabSize is correct");
     is(ed.getOption("indentUnit"), 4, "indentUnit is correct");
+    is(ed.getOption("foldGutter"), true, "foldGutter is correct");
+    is(ed.getOption("enableCodeFolding"), undefined, "enableCodeFolding is correct");
     is(ed.getOption("indentWithTabs"), false, "indentWithTabs is correct");
     is(ed.getOption("keyMap"), "sublime", "keyMap is correct");
     is(ed.getOption("autoCloseBrackets"), "()[]{}''\"\"", "autoCloseBrackets is correct");
     is(ed.getOption("autocomplete"), true, "autocomplete is correct");
     ok(ed.isAutocompletionEnabled(), "Autocompletion is enabled");
+
+    info ("Forcing foldGutter off using enableCodeFolding");
+    ed.setOption("enableCodeFolding", false);
+
+    is(ed.getOption("foldGutter"), false, "foldGutter is correct");
+    is(ed.getOption("enableCodeFolding"), false, "enableCodeFolding is correct");
+    Assert.deepEqual(ed.getOption("gutters"), [
+      "CodeMirror-linenumbers",
+      "breakpoints"], "gutters is correct");
+
+    info ("Forcing foldGutter on using enableCodeFolding");
+    ed.setOption("enableCodeFolding", true);
+
+    is(ed.getOption("foldGutter"), true, "foldGutter is correct");
+    is(ed.getOption("enableCodeFolding"), true, "enableCodeFolding is correct");
+    Assert.deepEqual(ed.getOption("gutters"), [
+      "CodeMirror-linenumbers",
+      "breakpoints",
+      "CodeMirror-foldgutter"], "gutters is correct");
 
     info ("Checking indentation detection");
 
