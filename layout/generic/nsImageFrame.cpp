66f62efdb3b3bd75407766b7a5b0412ef599d1ed
Daniel Holbert <dholbert@cs.stanford.edu>
2014-09-12 13:15:24 -0700

    (no bug) Drop no-longer-correct "bitmap" qualifier from nsImageFrame header-comment. rs=tn
    (It's no longer correct because we use nsImageFrame to render e.g. <img src="whatever.svg">, which is not bitmap-flavored at all.)

--- layout/generic/nsImageFrame.cpp
+++ layout/generic/nsImageFrame.cpp
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* rendering object for replaced elements with bitmap image data */
+/* rendering object for replaced elements with image data */
 
 #include "nsImageFrame.h"
 
