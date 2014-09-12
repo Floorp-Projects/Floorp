9f5f88748cde32b694e1f93254dbed0a9a6ff10a
Manish Goregaokar <manishearth@gmail.com>
2014-09-12 10:31:59 -0700

    Bug 1046672 - Restrict self-xss changes to Release/beta channels; r=jwalker

--- browser/branding/nightly/pref/firefox-branding.js
+++ browser/branding/nightly/pref/firefox-branding.js
@@ -30,3 +30,7 @@ pref("browser.search.param.yahoo-fr-ja", "mozff");
 #ifdef MOZ_METRO
 pref("browser.search.param.yahoo-fr-metro", "");
 #endif
+
+// Number of usages of the web console or scratchpad.
+// If this is less than 5, then pasting code into the web console or scratchpad is disabled
+pref("devtools.selfxss.count", 5);
