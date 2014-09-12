22cf894067a25b12e6d3db28c2ee91bdb45377e5
Jim Mathies <jmathies@mozilla.com>
2014-09-12 09:49:38 -0500

    Bug 1060738 - Implement MessagePumpForNonMainUIThreads for Windows, a xpcom compatible subclass of chromium's MessagePumpForUI. r=tabraldes a=sylvestre

--- ipc/glue/MessagePump.h
+++ ipc/glue/MessagePump.h
@@ -6,10 +6,16 @@
 #define __IPC_GLUE_MESSAGEPUMP_H__
 
 #include "base/message_pump_default.h"
+#if defined(XP_WIN)
+#include "base/message_pump_win.h"
+#endif
+
 #include "base/time.h"
 #include "mozilla/Attributes.h"
+#include "mozilla/Mutex.h"
 #include "nsAutoPtr.h"
 #include "nsCOMPtr.h"
+#include "nsIThreadInternal.h"
 
 class nsIThread;
 class nsITimer;
@@ -88,6 +94,65 @@ private:
   ~MessagePumpForNonMainThreads()
   { }
 };
+
+#if defined(XP_WIN)
+// Extends the TYPE_UI message pump to process xpcom events. Currently only
+// implemented for Win.
+class MessagePumpForNonMainUIThreads MOZ_FINAL:
+  public base::MessagePumpForUI,
+  public nsIThreadObserver
+{
+public:
+  // We don't want xpcom refing, chromium controls our lifetime via
+  // RefCountedThreadSafe.
+  NS_IMETHOD_(MozExternalRefCountType) AddRef(void) {
+    return 2;
+  }
+  NS_IMETHOD_(MozExternalRefCountType) Release(void) {
+    return 1;
+  }
+  NS_IMETHOD QueryInterface(REFNSIID aIID, void** aInstancePtr);
+
+  NS_DECL_NSITHREADOBSERVER
+
+public:
+  MessagePumpForNonMainUIThreads() :
+    mThread(nullptr),
+    mInWait(false),
+    mWaitLock("mInWait")
+  {
+  }
+
+  // The main run loop for this thread.
+  virtual void DoRunLoop();
+
+protected:
+  nsIThread* mThread;
+
+  void SetInWait() {
+    MutexAutoLock lock(mWaitLock);
+    mInWait = true;
+  }
+
+  void ClearInWait() {
+    MutexAutoLock lock(mWaitLock);
+    mInWait = false;
+  }
+
+  bool GetInWait() {
+    MutexAutoLock lock(mWaitLock);
+    return mInWait;
+  }
+
+private:
+  ~MessagePumpForNonMainUIThreads()
+  {
+  }
+
+  bool mInWait;
+  mozilla::Mutex mWaitLock;
+};
+#endif // defined(XP_WIN)
 
 } /* namespace ipc */
 } /* namespace mozilla */
