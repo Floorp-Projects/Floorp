9fc7885297f62e491d99f82b7d5b9faf0c13b076
L. David Baron <dbaron@dbaron.org>
2014-09-13 06:17:36 -0700

    Bug 977991 patch 1 - Expose variant of RulesMatching on nsHTMLCSSStyleSheet that is less work to call.  r=birtles
    This allows nsStyleSet::RuleNodeWithReplacement to call it without
    constructing an entire (and unnecessary) ElementRuleProcessorData, which
    will happen in patch 3.

--- layout/style/nsHTMLCSSStyleSheet.h
+++ layout/style/nsHTMLCSSStyleSheet.h
@@ -16,7 +16,14 @@
 #include "nsDataHashtable.h"
 #include "nsIStyleRuleProcessor.h"
 
+class nsRuleWalker;
 struct MiscContainer;
+
+namespace mozilla {
+namespace dom {
+class Element;
+}
+}
 
 class nsHTMLCSSStyleSheet MOZ_FINAL : public nsIStyleRuleProcessor
 {
@@ -42,6 +49,12 @@ public:
     const MOZ_MUST_OVERRIDE MOZ_OVERRIDE;
   virtual size_t SizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf)
     const MOZ_MUST_OVERRIDE MOZ_OVERRIDE;
+
+  // Variant of RulesMatching method above that is specific to this
+  // rule processor.
+  void ElementRulesMatching(nsPresContext* aPresContext,
+                            mozilla::dom::Element* aElement,
+                            nsRuleWalker* aRuleWalker);
 
   void CacheStyleAttr(const nsAString& aSerialized, MiscContainer* aValue);
   void EvictStyleAttr(const nsAString& aSerialized, MiscContainer* aValue);
