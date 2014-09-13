1d4505fd18afefdf1a0d1cd2b7d6ed89c30ccefb
L. David Baron <dbaron@dbaron.org>
2014-09-13 06:17:37 -0700

    Bug 977991 patch 7 - Use the faster eRestyle_SVGAttrAnimations hint from bug 1058346 for SMIL-animated SVG attribute changes.  r=birtles
    This couldn't be done in bug 1058346 because it depends on patch 5 in this bug.

--- content/svg/content/src/nsSVGElement.cpp
+++ content/svg/content/src/nsSVGElement.cpp
@@ -925,7 +925,8 @@ nsSVGElement::WalkAnimatedContentStyleRules(nsRuleWalker* aRuleWalker)
     // want that to happen from SMIL-animated value of mapped attrs, so
     // ignore animated value for now, and request an animation restyle to
     // get our animated value noticed.
-    shell->RestyleForAnimation(this, eRestyle_Self);
+    shell->RestyleForAnimation(this,
+      eRestyle_SVGAttrAnimations | eRestyle_ChangeAnimationPhase);
   } else {
     // Ok, this is an animation restyle -- go ahead and update/walk the
     // animated content style rule.
