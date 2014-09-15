f44bf6294c0451a84b5b3887684056b43abce086
Michael Wu <mwu@mozilla.com>
2014-09-15 10:25:25 -0400

    Bug 1063733 - Optimize DataSourceSurface allocation. r=bas, r=seth, a=sledru

--- gfx/2d/SourceSurfaceRawData.cpp
+++ gfx/2d/SourceSurfaceRawData.cpp
@@ -44,7 +44,8 @@ SourceSurfaceRawData::GuaranteePersistance()
 
 bool
 SourceSurfaceAlignedRawData::Init(const IntSize &aSize,
-                                  SurfaceFormat aFormat)
+                                  SurfaceFormat aFormat,
+                                  bool aZero)
 {
   mFormat = aFormat;
   mStride = GetAlignedStride<16>(aSize.width * BytesPerPixel(aFormat));
@@ -53,7 +54,7 @@ SourceSurfaceAlignedRawData::Init(const IntSize &aSize,
   if (bufLen > 0) {
     static_assert(sizeof(decltype(mArray[0])) == 1,
                   "mArray.Realloc() takes an object count, so its objects must be 1-byte sized if we use bufLen");
-    mArray.Realloc(/* actually an object count */ bufLen);
+    mArray.Realloc(/* actually an object count */ bufLen, aZero);
     mSize = aSize;
   } else {
     mArray.Dealloc();
@@ -66,7 +67,8 @@ SourceSurfaceAlignedRawData::Init(const IntSize &aSize,
 bool
 SourceSurfaceAlignedRawData::InitWithStride(const IntSize &aSize,
                                             SurfaceFormat aFormat,
-                                            int32_t aStride)
+                                            int32_t aStride,
+                                            bool aZero)
 {
   mFormat = aFormat;
   mStride = aStride;
@@ -75,7 +77,7 @@ SourceSurfaceAlignedRawData::InitWithStride(const IntSize &aSize,
   if (bufLen > 0) {
     static_assert(sizeof(decltype(mArray[0])) == 1,
                   "mArray.Realloc() takes an object count, so its objects must be 1-byte sized if we use bufLen");
-    mArray.Realloc(/* actually an object count */ bufLen);
+    mArray.Realloc(/* actually an object count */ bufLen, aZero);
     mSize = aSize;
   } else {
     mArray.Dealloc();
