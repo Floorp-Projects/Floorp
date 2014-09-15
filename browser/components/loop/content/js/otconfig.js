3abbda0ef44e6a4f0421a9e8cdd8a61fed02d85c
Adam Roach [:abr] <adam@nostrum.com>
2014-09-15 15:39:12 -0700

    Bug 1017257 - Make loop code CSP-friendly (remove all inline script) r=Standard8

---
+++ browser/components/loop/content/js/otconfig.js
@@ -0,0 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+window.OTProperties = {
+  cdnURL: 'loop/',
+};
+window.OTProperties.assetURL = window.OTProperties.cdnURL + 'sdk-content/';
+window.OTProperties.configURL = window.OTProperties.assetURL + 'js/dynamic_config.min.js';
+window.OTProperties.cssURL = window.OTProperties.assetURL + 'css/ot.css';
