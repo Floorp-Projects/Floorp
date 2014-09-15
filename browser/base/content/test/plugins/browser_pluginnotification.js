e3e02bb8ff8fc21d330d1aeee1f36c65864fef3d
Mike Conley <mconley@mozilla.com>
2014-09-15 13:33:50 -0400

    Bug 899347 - [e10s] Make click-to-play work with e10s. Originally written by mbrubeck. r=gfritzsche,felipe,Unfocused.
    We now use message passing and content scripts in order to implement click-to-play. This allows
    us to use click-to-play in e10s windows.

--- browser/base/content/test/plugins/browser_pluginnotification.js
+++ browser/base/content/test/plugins/browser_pluginnotification.js
@@ -56,6 +56,7 @@ TabOpenListener.prototype = {
 
 function test() {
   waitForExplicitFinish();
+  SimpleTest.requestCompleteLog();
   requestLongerTimeout(2);
   registerCleanupFunction(function() {
     clearAllPluginPermissions();
@@ -797,7 +798,10 @@ function test24a() {
   // simulate "always allow"
   notification.reshow();
   PopupNotifications.panel.firstChild._primaryButton.click();
-  prepareTest(test24b, gHttpTestRoot + "plugin_test.html");
+  waitForCondition(() => objLoadingContent.activated, () => {
+    prepareTest(test24b, gHttpTestRoot + "plugin_test.html");
+  }, "Test 24a, plugin should now be activated.");
+
 }
 
 // did the "always allow" work as intended?
@@ -805,11 +809,11 @@ function test24b() {
   var plugin = gTestBrowser.contentDocument.getElementById("test");
   ok(plugin, "Test 24b, Found plugin in page");
   var objLoadingContent = plugin.QueryInterface(Ci.nsIObjectLoadingContent);
-  ok(objLoadingContent.activated, "Test 24b, plugin should be activated");
-  setAndUpdateBlocklist(gHttpTestRoot + "blockPluginVulnerableUpdatable.xml",
-  function() {
-    prepareTest(runAfterPluginBindingAttached(test24c), gHttpTestRoot + "plugin_test.html");
-  });
+  waitForCondition(() => objLoadingContent.activated, () => {
+    setAndUpdateBlocklist(gHttpTestRoot + "blockPluginVulnerableUpdatable.xml", () => {
+      prepareTest(runAfterPluginBindingAttached(test24c), gHttpTestRoot + "plugin_test.html");
+    });
+  }, "Test 24b, plugin should be activated");
 }
 
 // the plugin is now blocklisted, so it should not automatically load
@@ -820,13 +824,13 @@ function test24c() {
   ok(plugin, "Test 24c, Found plugin in page");
   var objLoadingContent = plugin.QueryInterface(Ci.nsIObjectLoadingContent);
   is(objLoadingContent.pluginFallbackType, Ci.nsIObjectLoadingContent.PLUGIN_VULNERABLE_UPDATABLE, "Test 24c, Plugin should be vulnerable/updatable");
-  ok(!objLoadingContent.activated, "Test 24c, plugin should not be activated");
+  waitForCondition(() => !objLoadingContent.activated, () => {
+    // simulate "always allow"
+    notification.reshow();
+    PopupNotifications.panel.firstChild._primaryButton.click();
 
-  // simulate "always allow"
-  notification.reshow();
-  PopupNotifications.panel.firstChild._primaryButton.click();
-
-  prepareTest(test24d, gHttpTestRoot + "plugin_test.html");
+    prepareTest(test24d, gHttpTestRoot + "plugin_test.html");
+  }, "Test 24c, plugin should not be activated");
 }
 
 // We should still be able to always allow a plugin after we've seen that it's
@@ -835,15 +839,14 @@ function test24d() {
   var plugin = gTestBrowser.contentDocument.getElementById("test");
   ok(plugin, "Test 24d, Found plugin in page");
   var objLoadingContent = plugin.QueryInterface(Ci.nsIObjectLoadingContent);
-  ok(objLoadingContent.activated, "Test 24d, plugin should be activated");
-
-  // this resets the vulnerable plugin permission
-  setAndUpdateBlocklist(gHttpTestRoot + "blockNoPlugins.xml",
-  function() {
-    clearAllPluginPermissions();
-    resetBlocklist();
-    prepareTest(test25, gTestRoot + "plugin_syncRemoved.html");
-  });
+  waitForCondition(() => objLoadingContent.activated, () => {
+    // this resets the vulnerable plugin permission
+    setAndUpdateBlocklist(gHttpTestRoot + "blockNoPlugins.xml", () => {
+      clearAllPluginPermissions();
+      resetBlocklist();
+      prepareTest(test25, gTestRoot + "plugin_syncRemoved.html");
+    });
+  }, "Test 24d, plugin should be activated");
 }
 
 function test25() {
