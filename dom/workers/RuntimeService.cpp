76d5902ffab2452e980891c220eec0d051ed560e
Gregor Wagner <anygregor@gmail.com>
2014-09-15 22:09:10 +0200

    Bug 1067552 - Assertion in RuntimeService::ShutdownIdleThreads. r=bent

--- dom/workers/RuntimeService.cpp
+++ dom/workers/RuntimeService.cpp
@@ -1691,7 +1691,7 @@ RuntimeService::ShutdownIdleThreads(nsITimer* aTimer, void* /* aClosure */)
     }
   }
 
-  NS_ASSERTION(nextExpiration.IsNull() || !expiredThreads.IsEmpty(),
+  NS_ASSERTION(!nextExpiration.IsNull() || !expiredThreads.IsEmpty(),
                "Should have a new time or there should be some threads to shut "
                "down");
 
