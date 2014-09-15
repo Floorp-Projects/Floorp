8ab19571ba9fa4c617f7c0957b9e38a22e53c7b0
Bevis Tseng <btseng@mozilla.com>
2014-09-15 17:44:46 +0800

    Bug 864484 - Part 6: Move broadcastCbsSystemMessage() From RadioInterfaceLayer to CellBroadcastService. r=vyang

--- dom/system/gonk/RadioInterfaceLayer.js
+++ dom/system/gonk/RadioInterfaceLayer.js
@@ -2104,8 +2104,6 @@ RadioInterface.prototype = {
         this.handleSmsMultipart(message);
         break;
       case "cellbroadcast-received":
-        message.timestamp = Date.now();
-        this.broadcastCbsSystemMessage(message);
         this.handleCellbroadcastMessageReceived(message);
         break;
       case "nitzTime":
@@ -2779,48 +2777,6 @@ RadioInterface.prototype = {
   },
 
   /**
-   * A helper to broadcast the system message to launch registered apps
-   * like CMAS app and etc.
-   *
-   * @param aName
-   *        The system message name.
-   * @param aMessage
-   *        The Cellbroadcast message received from ril_worker.
-   */
-  broadcastCbsSystemMessage: function(aMessage) {
-    // Create system message with the same structure of nsIDOMMozCellBroadcastMessage
-    // and nsIDOMMozCellBroadcastEtwsInfo.
-    let etws = (aMessage.etws != null)
-               ? {
-                    warningType: (aMessage.etws.warningType != null)
-                                 ? RIL.CB_ETWS_WARNING_TYPE_NAMES[aMessage.etws.warningType]
-                                 : null,
-                    emergencyUserAlert: aMessage.etws.emergencyUserAlert,
-                    popup: aMessage.etws.popup
-                 }
-               : null;
-
-    let systemMessage = {
-      serviceId: this.clientId,
-      gsmGeographicalScope: RIL.CB_GSM_GEOGRAPHICAL_SCOPE_NAMES[aMessage.geographicalScope],
-      messageCode: aMessage.messageCode,
-      messageId: aMessage.messageId,
-      language: aMessage.language,
-      body: aMessage.fullBody,
-      messageClass: aMessage.messageClass,
-      timestamp: aMessage.timestamp,
-      etws: etws,
-      cdmaServiceCategory: aMessage.serviceCategory
-    };
-
-    if (DEBUG) {
-      this.debug("CBS system message to be broadcasted: " + JSON.stringify(systemMessage));
-    }
-
-    gSystemMessenger.broadcastMessage("cellbroadcast-received", systemMessage);
-  },
-
-  /**
    * Set the setting value of "time.clock.automatic-update.available".
    */
   setClockAutoUpdateAvailable: function(value) {
@@ -3002,7 +2958,7 @@ RadioInterface.prototype = {
                              aMessage.language,
                              aMessage.fullBody,
                              aMessage.messageClass,
-                             aMessage.timestamp,
+                             Date.now(),
                              serviceCategory,
                              hasEtwsInfo,
                              (hasEtwsInfo && etwsInfo.warningType != null)
