8249f4c96aaad4550f914aa68718dd01f184501a
Adrian Aichner <adrian.aichner@gmail.com>
2014-09-15 17:18:16 -0700

    Bug 1064282 - Scratchpad should have statusbar like Page Source viewer. r=fitzgen
    ---
     browser/devtools/scratchpad/scratchpad.js          |   12 ++++++++++++
     browser/devtools/scratchpad/scratchpad.xul         |    1 +
     .../test/browser_scratchpad_run_error_goto_line.js |   11 +++++++++++
     .../chrome/browser/devtools/scratchpad.properties  |    5 +++++
     4 files changed, 29 insertions(+), 0 deletions(-)

--- browser/devtools/scratchpad/test/browser_scratchpad_run_error_goto_line.js
+++ browser/devtools/scratchpad/test/browser_scratchpad_run_error_goto_line.js
@@ -44,6 +44,17 @@ function runTests(sw)
     let cursor = sp.editor.getCursor();
     is(cursor.line + 1, inputLine, "jumpToLine goto error location (line)");
     is(cursor.ch + 1, 1, "jumpToLine goto error location (column)");
+  }, error => {
+    ok(false, error);
+    finish();
+  }).then(() => {
+    var statusBarField = sp.editor.container.ownerDocument.querySelector("#statusbar-line-col");
+    let { line, ch } = sp.editor.getCursor();
+    is(statusBarField.textContent, sp.strings.formatStringFromName(
+      "scratchpad.statusBarLineCol", [ line + 1, ch + 1], 2), "statusbar text is correct (" + statusBarField.textContent + ")");
+    finish();
+  }, error => {
+    ok(false, error);
     finish();
   });
 }
