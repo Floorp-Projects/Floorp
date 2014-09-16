566ebe278691b1ff2830999b2d9f58d925f7db5b
JW Wang <jwwang@mozilla.com>
2014-09-16 19:31:00 +1200

    Bug 1067858 - apply |AutoNoJSAPI| before calling mAudioChannelAgent->SetVisibilityState in order not to hit nsContentUtils::IsCallerChrome() in HTMLMediaElement::CanPlayChanged().

--- content/html/content/src/HTMLMediaElement.cpp
+++ content/html/content/src/HTMLMediaElement.cpp
@@ -3489,6 +3489,7 @@ void HTMLMediaElement::NotifyOwnerDocumentActivityChanged()
   // CanPlayChanged callback.
   if (UseAudioChannelService() && mPlayingThroughTheAudioChannel &&
       mAudioChannelAgent) {
+    AutoNoJSAPI nojsapi;
     mAudioChannelAgent->SetVisibilityState(!ownerDoc->Hidden());
   }
   bool suspendEvents = !ownerDoc->IsActive() || !ownerDoc->IsVisible();
