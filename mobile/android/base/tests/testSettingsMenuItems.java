9937005ad6a70e63ebe6d009e9c12beae44f2b28
Lucas Rocha <lucasr@lucasr.org>
2014-09-17 14:20:36 +0100

    Bug 1068005 - Hide new tablet UI toggle on phones (r=mcomella)

--- mobile/android/base/tests/testSettingsMenuItems.java
+++ mobile/android/base/tests/testSettingsMenuItems.java
@@ -159,9 +159,11 @@ public class testSettingsMenuItems extends PixelTest {
             String[] textReflowUi = { StringHelper.TEXT_REFLOW_LABEL };
             settingsMap.get(PATH_DISPLAY).add(textReflowUi);
 
-            // New tablet UI can only be enabled in non-release build
-            String[] newTabletUi = { StringHelper.NEW_TABLET_UI };
-            settingsMap.get(PATH_DISPLAY).add(newTabletUi);
+            // New tablet UI can only be enabled on tablets in non-release builds.
+            if (HardwareUtils.isTablet()) {
+                String[] newTabletUi = { StringHelper.NEW_TABLET_UI };
+                settingsMap.get(PATH_DISPLAY).add(newTabletUi);
+            }
 
             // New tablet UI: we don't allow a page title option.
             if (NewTabletUI.isEnabled(getActivity())) {
