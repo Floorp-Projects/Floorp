64b91b1c09dbcd20088c81bb9aa78363474ff7ea
Jon Coppeard <jcoppeard@mozilla.com>
2014-09-17 15:35:11 +0100

    Bug 650161 - Use the object moved hook to update the wrapper cache r=terrence r=bz r=bholley * * * Add exception for objectMoved hook as we do for finalize hook

--- js/xpconnect/src/XPCWrappedNativeJSOps.cpp
+++ js/xpconnect/src/XPCWrappedNativeJSOps.cpp
@@ -573,6 +573,17 @@ WrappedNativeFinalize(js::FreeOp *fop, JSObject *obj, WNHelperType helperType)
 }
 
 static void
+WrappedNativeObjectMoved(JSObject *obj, const JSObject *old)
+{
+    nsISupports* p = static_cast<nsISupports*>(xpc_GetJSPrivate(obj));
+    if (!p)
+        return;
+
+    XPCWrappedNative* wrapper = static_cast<XPCWrappedNative*>(p);
+    wrapper->FlatJSObjectMoved(obj, old);
+}
+
+static void
 XPC_WN_NoHelper_Finalize(js::FreeOp *fop, JSObject *obj)
 {
     WrappedNativeFinalize(fop, obj, WN_NOHELPER);
@@ -658,7 +669,9 @@ const XPCWrappedNativeJSClass XPC_WN_NoHelper_JSClass = {
         nullptr, // outerObject
         nullptr, // innerObject
         nullptr, // iteratorObject
-        true,   // isWrappedNative
+        true,    // isWrappedNative
+        nullptr, // weakmapKeyDelegateOp
+        WrappedNativeObjectMoved
     },
 
     // ObjectOps
@@ -1165,6 +1178,7 @@ XPCNativeScriptableShared::PopulateJSClass()
         mJSClass.base.trace = XPCWrappedNative::Trace;
 
     mJSClass.base.ext.isWrappedNative = true;
+    mJSClass.base.ext.objectMovedOp = WrappedNativeObjectMoved;
 }
 
 /***************************************************************************/
