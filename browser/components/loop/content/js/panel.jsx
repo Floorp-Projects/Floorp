2364474e1ff2f06e720aea45ca72642b83513ff6
Mark Banner <standard8@mozilla.com>
2014-09-17 19:23:30 +0100

    Bug 1068580 Remove the backbone router from the Loop panel. r=nperriault
    --HG--
    extra : transplant_source : %B6mP%1C2%896%BA%8C%5E%27%A0%29%1A%93%CC%11%C4%D2i

--- browser/components/loop/content/js/panel.jsx
+++ browser/components/loop/content/js/panel.jsx
@@ -522,37 +522,6 @@ loop.panel = (function(_, mozL10n) {
     }
   });
 
-  var PanelRouter = loop.desktopRouter.DesktopRouter.extend({
-    /**
-     * DOM document object.
-     * @type {HTMLDocument}
-     */
-    document: undefined,
-
-    routes: {
-      "": "home"
-    },
-
-    initialize: function(options) {
-      options = options || {};
-      if (!options.document) {
-        throw new Error("missing required document");
-      }
-    },
-
-    /**
-     * Default entry point.
-     */
-    home: function() {
-      this._notifications.reset();
-      var client = new loop.Client({
-        baseServerUrl: navigator.mozLoop.serverUrl
-      });
-      this.loadReactComponent(
-          <PanelView client={client} notifications={this._notifications}/>);
-    }
-  });
-
   /**
    * Panel initialisation.
    */
@@ -561,11 +530,15 @@ loop.panel = (function(_, mozL10n) {
     // else to ensure the L10n environment is setup correctly.
     mozL10n.initialize(navigator.mozLoop);
 
-    router = new PanelRouter({
-      document: document,
-      notifications: new sharedModels.NotificationCollection()
+    var client = new loop.Client({
+      baseServerUrl: navigator.mozLoop.serverUrl
     });
-    Backbone.history.start();
+    var notifications = new sharedModels.NotificationCollection()
+
+    React.renderComponent(<PanelView
+      client={client}
+      notifications={notifications}
+    />, document.querySelector("#main"));
 
     document.body.classList.add(loop.shared.utils.getTargetPlatform());
     document.body.setAttribute("dir", mozL10n.getDirection());
@@ -582,7 +555,6 @@ loop.panel = (function(_, mozL10n) {
     AvailabilityDropdown: AvailabilityDropdown,
     CallUrlResult: CallUrlResult,
     PanelView: PanelView,
-    PanelRouter: PanelRouter,
     SettingsDropdown: SettingsDropdown,
     ToSView: ToSView
   };
