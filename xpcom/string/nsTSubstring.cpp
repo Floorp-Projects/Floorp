936b447babd6c803ddc5367053c3d0b6658e0612
Nicholas Nethercote <nnethercote@mozilla.com>
2014-09-16 23:33:05 -0700

    Bug 1067699 (part 1) - Export double-conversion.h normally from MFBT. r=froydnj.

--- xpcom/string/nsTSubstring.cpp
+++ xpcom/string/nsTSubstring.cpp
@@ -3,8 +3,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/double-conversion.h"
 #include "mozilla/MemoryReporting.h"
-#include "double-conversion.h"
 
 using double_conversion::DoubleToStringConverter;
 
