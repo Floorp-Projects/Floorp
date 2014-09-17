bb852dd0a00f80920882deebd861e824ba09fc8a
Mark Hammond <mhammond@skippinet.com.au>
2014-09-17 14:52:44 +1000

    Bug 1053969 - get more social tests working with e10s. r=mixedpuppy

--- browser/base/content/test/social/browser_social_marks.js
+++ browser/base/content/test/social/browser_social_marks.js
@@ -73,7 +73,7 @@ var tests = {
   testButtonDisabledOnActivate: function(next) {
     // starting on about:blank page, share should be visible but disabled when
     // adding provider
-    is(gBrowser.contentDocument.location.href, "about:blank");
+    is(gBrowser.selectedTab.linkedBrowser.currentURI.spec, "about:blank");
     SocialService.addProvider(manifest2, function(provider) {
       is(provider.origin, manifest2.origin, "provider is installed");
       let id = SocialMarks._toolbarHelper.idFromOrigin(manifest2.origin);
