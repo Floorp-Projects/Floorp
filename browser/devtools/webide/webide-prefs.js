1f14261d90d89d5667fa19ac2fa695b324e98e68
Alexandre Poirot <poirot.alex@gmail.com>
2014-09-18 07:27:00 -0400

    Bug 1045660 - Remember last selected runtime in WebIDE. r=jryans

--- browser/devtools/webide/webide-prefs.js
+++ browser/devtools/webide/webide-prefs.js
@@ -15,3 +15,4 @@ pref("devtools.webide.simulatorAddonID", "fxos_#SLASHED_VERSION#_simulator@mozil
 pref("devtools.webide.adbAddonURL", "https://ftp.mozilla.org/pub/mozilla.org/labs/fxos-simulator/adb-helper/#OS#/adbhelper-#OS#-latest.xpi");
 pref("devtools.webide.adbAddonID", "adbhelper@mozilla.org");
 pref("devtools.webide.monitorWebSocketURL", "ws://localhost:9000");
+pref("devtools.webide.lastConnectedRuntime", "");
