cdb6f62835f66b27890c30729e7b46116428e7cc
Patrick Wang (Chih-Kai Wang) <kk1fff@patrickz.net>
2014-09-18 10:30:20 +0800

    Bug 1032125: Part 3: Don't send frame messages to Nuwa process after Nuwa has frozen. r=khuey

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -3681,6 +3681,12 @@ ContentParent::DoSendAsyncMessage(JSContext* aCx,
     if (aCpows && !GetCPOWManager()->Wrap(aCx, aCpows, &cpows)) {
         return false;
     }
+#ifdef MOZ_NUWA_PROCESS
+    if (IsNuwaProcess() && IsNuwaReady()) {
+        // Nuwa won't receive frame messages after it is frozen.
+        return true;
+    }
+#endif
     return SendAsyncMessage(nsString(aMessage), data, cpows, Principal(aPrincipal));
 }
 
