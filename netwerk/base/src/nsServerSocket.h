f88ab970838ea6f629b194f268b4ca3e94505db5
J. Ryan Stinnett <jryans@gmail.com>
2014-09-18 08:13:00 +0200

    Bug 242448 - Add a basic scriptable TLS server. r=honzab

--- netwerk/base/src/nsServerSocket.h
+++ netwerk/base/src/nsServerSocket.h
@@ -12,6 +12,11 @@
 
 //-----------------------------------------------------------------------------
 
+class nsIEventTarget;
+namespace mozilla { namespace net {
+union NetAddr;
+}} // namespace mozilla::net
+
 class nsServerSocket : public nsASocketHandler
                      , public nsIServerSocket
 {
@@ -29,20 +34,26 @@ public:
   virtual uint64_t ByteCountReceived() { return 0; }
   nsServerSocket();
 
-private:
-  virtual ~nsServerSocket();
+  virtual void CreateClientTransport(PRFileDesc* clientFD,
+                                     const mozilla::net::NetAddr& clientAddr);
+  virtual nsresult SetSocketDefaults() { return NS_OK; }
+  virtual nsresult OnSocketListen() { return NS_OK; }
 
+protected:
+  virtual ~nsServerSocket();
+  PRFileDesc*                       mFD;
+  nsCOMPtr<nsIServerSocketListener> mListener;
+
+private:
   void OnMsgClose();
   void OnMsgAttach();
-  
+
   // try attaching our socket (mFD) to the STS's poll list.
   nsresult TryAttach();
 
   // lock protects access to mListener; so it is not cleared while being used.
   mozilla::Mutex                    mLock;
-  PRFileDesc                       *mFD;
   PRNetAddr                         mAddr;
-  nsCOMPtr<nsIServerSocketListener> mListener;
   nsCOMPtr<nsIEventTarget>          mListenerTarget;
   bool                              mAttached;
   bool                              mKeepWhenOffline;
