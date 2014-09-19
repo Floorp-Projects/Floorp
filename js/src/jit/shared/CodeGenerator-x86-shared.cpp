f9469050577c3d79a256d039ee7588228498637a
Benjamin Bouvier <benj@benj.me>
2014-09-19 14:56:05 +0200

    Bug 1025100: Use INSERTPS for inserting float elements when SSE4.1 is available; r=sunfish

--- js/src/jit/shared/CodeGenerator-x86-shared.cpp
+++ js/src/jit/shared/CodeGenerator-x86-shared.cpp
@@ -2326,6 +2326,12 @@ CodeGeneratorX86Shared::visitSimdInsertElementF(LSimdInsertElementF *ins)
         return true;
     }
 
+    if (AssemblerX86Shared::HasSSE41()) {
+        // The input value is in the low float32 of the 'value' FloatRegister.
+        masm.insertps(value, output, masm.insertpsMask(SimdLane::LaneX, ins->lane()));
+        return true;
+    }
+
     unsigned component = unsigned(ins->lane());
     masm.reserveStack(Simd128DataSize);
     masm.storeAlignedFloat32x4(vector, Address(StackPointer, 0));
