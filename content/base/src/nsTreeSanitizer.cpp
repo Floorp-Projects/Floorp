884b94f16b3e462c24db68ce845c2b30bf814c0c
Henri Sivonen <hsivonen@hsivonen.fi>
2014-09-19 18:20:26 +0300

    Bug 1066483 - Drop SVG camelCase mappings filterRes, contentStyleType, contentScriptType and externalResourcesRequired. r=wchen.

--- content/base/src/nsTreeSanitizer.cpp
+++ content/base/src/nsTreeSanitizer.cpp
@@ -393,8 +393,6 @@ nsIAtom** const kAttributesSVG[] = {
   &nsGkAtoms::color, // color
   &nsGkAtoms::colorInterpolation, // color-interpolation
   &nsGkAtoms::colorInterpolationFilters, // color-interpolation-filters
-  // contentScriptType
-  // contentStyleType
   &nsGkAtoms::cursor, // cursor
   &nsGkAtoms::cx, // cx
   &nsGkAtoms::cy, // cy
@@ -416,7 +414,6 @@ nsIAtom** const kAttributesSVG[] = {
   &nsGkAtoms::fill_opacity, // fill-opacity
   &nsGkAtoms::fill_rule, // fill-rule
   &nsGkAtoms::filter, // filter
-  &nsGkAtoms::filterRes, // filterRes
   &nsGkAtoms::filterUnits, // filterUnits
   &nsGkAtoms::flood_color, // flood-color
   &nsGkAtoms::flood_opacity, // flood-opacity
