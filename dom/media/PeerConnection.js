880d0c60a3ec58723c121ed18118a2742dafd3b4
Jan-Ivar Bruaroey <jib@mozilla.com>
2014-09-19 15:25:06 -0400

    Bug 1070076 - Fix createOffer options arg legacy-syntax-warning to not trip on absent arg. r=jesup, a=sledru

--- dom/media/PeerConnection.js
+++ dom/media/PeerConnection.js
@@ -557,9 +557,10 @@ RTCPeerConnection.prototype = {
   createOffer: function(onSuccess, onError, options) {
 
     // TODO: Remove old constraint-like RTCOptions support soon (Bug 1064223).
+    // Note that webidl bindings make o.mandatory implicit but not o.optional.
     function convertLegacyOptions(o) {
-      if (!(o.mandatory || o.optional) ||
-          Object.keys(o).length != ((o.mandatory && o.optional)? 2 : 1)) {
+      if (!(Object.keys(o.mandatory).length || o.optional) ||
+          Object.keys(o).length != (o.optional? 2 : 1)) {
         return false;
       }
       let old = o.mandatory || {};
