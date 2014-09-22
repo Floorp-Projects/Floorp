c74f17a79c6d5a77b1ebc30a92880ff2ef88854c
Szu-Yu Chen [:aknow] <szchen@mozilla.com>
2014-09-22 01:35:00 -0400

    Bug 889737 - Part 07: Dial MMI (callback). r=vicamo

--- dom/telephony/TelephonyCallback.h
+++ dom/telephony/TelephonyCallback.h
@@ -5,15 +5,21 @@
 #ifndef mozilla_dom_TelephonyCallback_h
 #define mozilla_dom_TelephonyCallback_h
 
+#include "Telephony.h"
+#include "mozilla/dom/DOMRequest.h"
+#include "mozilla/dom/MozMobileConnectionBinding.h"
+#include "mozilla/dom/Promise.h"
+#include "mozilla/dom/ToJSValue.h"
+#include "nsAutoPtr.h"
 #include "nsCOMPtr.h"
 #include "nsITelephonyService.h"
+#include "nsJSUtils.h"
+#include "nsString.h"
+
+class nsPIDOMWindow;
 
 namespace mozilla {
 namespace dom {
-
-class Promise;
-class Telephony;
-
 namespace telephony {
 
 class TelephonyCallback MOZ_FINAL : public nsITelephonyCallback
@@ -22,15 +28,50 @@ public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSITELEPHONYCALLBACK
 
-  TelephonyCallback(Telephony* aTelephony, Promise* aPromise,
-                    uint32_t aServiceId);
+  TelephonyCallback(nsPIDOMWindow* aWindow, Telephony* aTelephony,
+                    Promise* aPromise, uint32_t aServiceId);
+
+  nsresult
+  NotifyDialMMISuccess(const nsAString& aStatusMessage);
+
+  template<typename T>
+  nsresult
+  NotifyDialMMISuccess(const nsAString& aStatusMessage, const T& aInfo)
+  {
+    AutoJSAPI jsapi;
+    if (!NS_WARN_IF(jsapi.Init(mWindow))) {
+      return NS_ERROR_FAILURE;
+    }
+
+    JSContext* cx = jsapi.cx();
+    JS::Rooted<JS::Value> info(cx);
+
+    if (!ToJSValue(cx, aInfo, &info)) {
+      JS_ClearPendingException(cx);
+      return NS_ERROR_TYPE_ERR;
+    }
+
+    return NotifyDialMMISuccess(cx, aStatusMessage, info);
+  }
 
 private:
   ~TelephonyCallback() {}
 
+  nsresult
+  NotifyDialMMISuccess(JSContext* aCx, const nsAString& aStatusMessage,
+                       JS::Handle<JS::Value> aInfo);
+
+  nsresult
+  NotifyDialMMISuccess(JSContext* aCx, const MozMMIResult& aResult);
+
+
+  nsCOMPtr<nsPIDOMWindow> mWindow;
   nsRefPtr<Telephony> mTelephony;
   nsRefPtr<Promise> mPromise;
   uint32_t mServiceId;
+
+  nsRefPtr<DOMRequest> mMMIRequest;
+  nsString mServiceCode;
 };
 
 } // namespace telephony
