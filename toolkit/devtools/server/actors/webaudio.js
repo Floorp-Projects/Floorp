cee331cef88ad53559a511a4fd3b2ba57dd5fb8a
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-09-22 12:26:54 +0200

    Backed out changeset 4d9e5795ee82 (bug 1066450) for test failures

--- toolkit/devtools/server/actors/webaudio.js
+++ toolkit/devtools/server/actors/webaudio.js
@@ -284,7 +284,6 @@ let WebAudioActor = exports.WebAudioActor = protocol.ActorClass({
 
     this._onDestroyNode = this._onDestroyNode.bind(this);
     this._onGlobalDestroyed = this._onGlobalDestroyed.bind(this);
-    this._onGlobalCreated = this._onGlobalCreated.bind(this);
   },
 
   destroy: function(conn) {
@@ -323,9 +322,6 @@ let WebAudioActor = exports.WebAudioActor = protocol.ActorClass({
       holdWeak: true,
       storeCalls: false
     });
-    // Bind to `window-ready` so we can reenable recording on the
-    // call watcher
-    on(this.tabActor, "window-ready", this._onGlobalCreated);
     // Bind to the `window-destroyed` event so we can unbind events between
     // the global destruction and the `finalize` cleanup method on the actor.
     on(this.tabActor, "window-destroyed", this._onGlobalDestroyed);
@@ -399,7 +395,6 @@ let WebAudioActor = exports.WebAudioActor = protocol.ActorClass({
     this.tabActor = null;
     this._initialized = false;
     off(this.tabActor, "window-destroyed", this._onGlobalDestroyed);
-    off(this.tabActor, "window-ready", this._onGlobalCreated);
     this._nativeToActorID = null;
     this._callWatcher.eraseRecording();
     this._callWatcher.finalize();
@@ -572,14 +567,6 @@ let WebAudioActor = exports.WebAudioActor = protocol.ActorClass({
       this._nativeToActorID.delete(nativeID);
       emit(this, "destroy-node", actor);
     }
-  },
-
-  /**
-   * Ensures that the new global has recording on
-   * so we can proxy the function calls.
-   */
-  _onGlobalCreated: function () {
-    this._callWatcher.resumeRecording();
   },
 
   /**
