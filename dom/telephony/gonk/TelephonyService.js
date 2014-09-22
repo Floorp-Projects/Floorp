8dca112b29326a2cb46e258435b03b6b1b809bc6
Szu-Yu Chen [:aknow] <szchen@mozilla.com>
2014-09-22 01:34:00 -0400

    Bug 889737 - Part 03: Refactoring: Rename DialSuccess to DialCallSuccess. r=hsinyi

--- dom/telephony/gonk/TelephonyService.js
+++ dom/telephony/gonk/TelephonyService.js
@@ -616,10 +616,10 @@ TelephonyService.prototype = {
         Object.keys(this._currentCalls[aClientId])[0];
 
       if (currentCdmaCallIndex == null) {
-        aCallback.notifyDialSuccess(response.callIndex, response.number);
+        aCallback.notifyDialCallSuccess(response.callIndex, response.number);
       } else {
         // RIL doesn't hold the 2nd call. We create one by ourselves.
-        aCallback.notifyDialSuccess(CDMA_SECOND_CALL_INDEX, response.number);
+        aCallback.notifyDialCallSuccess(CDMA_SECOND_CALL_INDEX, response.number);
         this._addCdmaChildCall(aClientId, response.number, currentCdmaCallIndex);
       }
     });
--- dom/telephony/ipc/TelephonyParent.cpp
+++ dom/telephony/ipc/TelephonyParent.cpp
@@ -536,11 +536,11 @@ TelephonyRequestParent::NotifyDialError(const nsAString& aError)
 }
 
 NS_IMETHODIMP
-TelephonyRequestParent::NotifyDialSuccess(uint32_t aCallIndex,
-                                          const nsAString& aNumber)
+TelephonyRequestParent::NotifyDialCallSuccess(uint32_t aCallIndex,
+                                              const nsAString& aNumber)
 {
   NS_ENSURE_TRUE(!mActorDestroyed, NS_ERROR_FAILURE);
 
-  return (SendNotifyDialSuccess(aCallIndex, nsString(aNumber)) &&
+  return (SendNotifyDialCallSuccess(aCallIndex, nsString(aNumber)) &&
           Send__delete__(this, DialResponse())) ? NS_OK : NS_ERROR_FAILURE;
 }
