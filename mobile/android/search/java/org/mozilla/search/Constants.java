86f20b8c20828ea60fbb600b05d1478f052fa172
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-09-22 17:46:29 -0700

    Bug 1051973 - Refine appearance of suggestions/search history items. r=wesj

--- mobile/android/search/java/org/mozilla/search/Constants.java
+++ mobile/android/search/java/org/mozilla/search/Constants.java
@@ -16,8 +16,6 @@ package org.mozilla.search;
  */
 public class Constants {
 
-    public static final int SUGGESTION_MAX = 5;
-
     public static final String ABOUT_BLANK = "about:blank";
 
     // TODO: Localize this with region.properties (or a similar solution). See bug 1065306.
