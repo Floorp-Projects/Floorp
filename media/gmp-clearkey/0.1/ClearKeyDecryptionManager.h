63b123469e716ecae0293416a73db08fdec931bd
Edwin Flores <eflores@mozilla.com>
2014-09-24 10:04:49 +1200

    Bug 1044742 - ClearKey CDM for testing EME - r=cpearce

---
+++ media/gmp-clearkey/0.1/ClearKeyDecryptionManager.h
@@ -0,0 +1,69 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef __ClearKeyDecryptor_h__
+#define __ClearKeyDecryptor_h_
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "ClearKeySession.h"
+#include "ClearKeyUtils.h"
+#include "gmp-api/gmp-decryption.h"
+#include "ScopedNSSTypes.h"
+
+class ClearKeyDecryptor;
+
+class ClearKeyDecryptionManager MOZ_FINAL : public GMPDecryptor
+{
+public:
+  ClearKeyDecryptionManager(GMPDecryptorHost* aHost);
+  ~ClearKeyDecryptionManager();
+
+  virtual void Init(GMPDecryptorCallback* aCallback) MOZ_OVERRIDE;
+
+  virtual void CreateSession(uint32_t aPromiseId,
+                             const char* aInitDataType,
+                             uint32_t aInitDataTypeSize,
+                             const uint8_t* aInitData,
+                             uint32_t aInitDataSize,
+                             GMPSessionType aSessionType) MOZ_OVERRIDE;
+
+  virtual void LoadSession(uint32_t aPromiseId,
+                           const char* aSessionId,
+                           uint32_t aSessionIdLength) MOZ_OVERRIDE;
+
+  virtual void UpdateSession(uint32_t aPromiseId,
+                             const char* aSessionId,
+                             uint32_t aSessionIdLength,
+                             const uint8_t* aResponse,
+                             uint32_t aResponseSize) MOZ_OVERRIDE;
+
+  virtual void CloseSession(uint32_t aPromiseId,
+                            const char* aSessionId,
+                            uint32_t aSessionIdLength) MOZ_OVERRIDE;
+
+  virtual void RemoveSession(uint32_t aPromiseId,
+                             const char* aSessionId,
+                             uint32_t aSessionIdLength) MOZ_OVERRIDE;
+
+  virtual void SetServerCertificate(uint32_t aPromiseId,
+                                    const uint8_t* aServerCert,
+                                    uint32_t aServerCertSize) MOZ_OVERRIDE;
+
+  virtual void Decrypt(GMPBuffer* aBuffer,
+                       GMPEncryptedBufferMetadata* aMetadata) MOZ_OVERRIDE;
+
+  virtual void DecryptingComplete() MOZ_OVERRIDE;
+
+private:
+  GMPDecryptorCallback* mCallback;
+  GMPDecryptorHost* mHost;
+
+  std::map<KeyId, ClearKeyDecryptor*> mDecryptors;
+  std::map<std::string, ClearKeySession*> mSessions;
+};
+
+#endif // __ClearKeyDecryptor_h__
---
+++ media/gmp-clearkey/0.1/gmp-clearkey.cpp
@@ -0,0 +1,49 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "ClearKeyDecryptionManager.h"
+
+#include "gmp-api/gmp-decryption.h"
+#include "gmp-api/gmp-platform.h"
+#include "mozilla/Attributes.h"
+#include "mozilla/NullPtr.h"
+
+static GMPPlatformAPI* sPlatform = nullptr;
+GMPPlatformAPI*
+GetPlatform()
+{
+  return sPlatform;
+}
+
+extern "C" {
+
+MOZ_EXPORT GMPErr
+GMPInit(GMPPlatformAPI* aPlatformAPI)
+{
+  sPlatform = aPlatformAPI;
+  return GMPNoErr;
+}
+
+MOZ_EXPORT GMPErr
+GMPGetAPI(const char* aApiName, void* aHostAPI, void** aPluginAPI)
+{
+  if (strcmp(aApiName, "eme-decrypt")) {
+    return GMPNotImplementedErr;
+  }
+
+  *aPluginAPI = new ClearKeyDecryptionManager(static_cast<GMPDecryptorHost*>(aHostAPI));
+
+  return GMPNoErr;
+}
+
+MOZ_EXPORT GMPErr
+GMPShutdown(void)
+{
+  return GMPNoErr;
+}
+
+}
