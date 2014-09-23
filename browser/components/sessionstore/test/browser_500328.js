8f26fac88e46351e1399a37d2bb6788f444c56bc
Bill McCloskey <wmccloskey@mozilla.com>
2014-09-23 15:48:53 -0700

    Bug 1059007 - Make session restore tests work in e10s (r=ttaubert)

--- browser/components/sessionstore/test/browser_500328.js
+++ browser/components/sessionstore/test/browser_500328.js
@@ -32,7 +32,7 @@ function checkState(tab) {
       elem.id = "new-elem";
       doc.body.appendChild(elem);
 
-      contentWindow.history.forward();
+      tab.linkedBrowser.goForward();
     }
     else if (popStateCount == 1) {
       popStateCount++;
@@ -42,30 +42,33 @@ function checkState(tab) {
       // deserialized in the content scope. And in this case, since RegExps are
       // not currently Xrayable (see bug 1014991), trying to pull |obj3| (a RegExp)
       // off of an Xrayed Object won't work. So we need to waive.
-      is(Cu.waiveXrays(aEvent.state).obj3.toString(), '/^a$/', "second popstate object.");
+      runInContent(tab.linkedBrowser, function(win, event) {
+        return Cu.waiveXrays(event.state).obj3.toString();
+      }, aEvent).then(function(stateStr) {
+        is(stateStr, '/^a$/', "second popstate object.");
 
-      // Make sure that the new-elem node is present in the document.  If it's
-      // not, then this history entry has a different doc identifier than the
-      // previous entry, which is bad.
-      let doc = contentWindow.document;
-      let newElem = doc.getElementById("new-elem");
-      ok(newElem, "doc should contain new-elem.");
-      newElem.parentNode.removeChild(newElem);
-      ok(!doc.getElementById("new-elem"), "new-elem should be removed.");
+        // Make sure that the new-elem node is present in the document.  If it's
+        // not, then this history entry has a different doc identifier than the
+        // previous entry, which is bad.
+        let doc = contentWindow.document;
+        let newElem = doc.getElementById("new-elem");
+        ok(newElem, "doc should contain new-elem.");
+        newElem.parentNode.removeChild(newElem);
+        ok(!doc.getElementById("new-elem"), "new-elem should be removed.");
 
-      // Clean up after ourselves and finish the test.
-      tab.linkedBrowser.removeEventListener("popstate", arguments.callee, true);
-      gBrowser.removeTab(tab);
-      finish();
+        tab.linkedBrowser.removeEventListener("popstate", arguments.callee, true);
+        gBrowser.removeTab(tab);
+        finish();
+      });
     }
-  }, true);
+  });
 
   // Set some state in the page's window.  When we go back(), the page should
   // be retrieved from bfcache, and this state should still be there.
   tab.linkedBrowser.contentWindow.testState = 'foo';
 
   // Now go back.  This should trigger the popstate event handler above.
-  tab.linkedBrowser.contentWindow.history.back();
+  tab.linkedBrowser.goBack();
 }
 
 function test() {
@@ -89,28 +92,27 @@ function test() {
       //   testURL        (state object: null)          <-- oldest
       //   testURL        (state object: {obj1:1})
       //   testURL?page2  (state object: {obj3:/^a$/})  <-- newest
-      let contentWindow = tab.linkedBrowser.contentWindow;
-      let history = contentWindow.history;
-      history.pushState({obj1:1}, "title-obj1");
-      history.pushState({obj2:2}, "title-obj2", "?page2");
-      history.replaceState({obj3:/^a$/}, "title-obj3");
+      function contentTest(win) {
+        let history = win.history;
+        history.pushState({obj1:1}, "title-obj1");
+        history.pushState({obj2:2}, "title-obj2", "?page2");
+        history.replaceState({obj3:/^a$/}, "title-obj3");
+      }
+      runInContent(browser, contentTest, null).then(function() {
+        SyncHandlers.get(tab.linkedBrowser).flush();
+        let state = ss.getTabState(tab);
+        gBrowser.removeTab(tab);
 
-      SyncHandlers.get(tab.linkedBrowser).flush();
-      let state = ss.getTabState(tab);
-      gBrowser.removeTab(tab);
+        // Restore the state into a new tab.  Things don't work well when we
+        // restore into the old tab, but that's not a real use case anyway.
+        let tab2 = gBrowser.addTab("about:blank");
+        ss.setTabState(tab2, state, true);
 
-      // Restore the state into a new tab.  Things don't work well when we
-      // restore into the old tab, but that's not a real use case anyway.
-      let tab2 = gBrowser.addTab("about:blank");
-      ss.setTabState(tab2, state, true);
-
-      // Run checkState() once the tab finishes loading its restored state.
-      whenTabRestored(tab2, function() {
-        SimpleTest.executeSoon(function() {
+        // Run checkState() once the tab finishes loading its restored state.
+        whenTabRestored(tab2, function() {
           checkState(tab2);
         });
       });
-
     });
   });
 }
