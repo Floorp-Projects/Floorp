bf3a35dfd6ac612dad04cb555798de5e9863ee58
Dan Glastonbury <dglastonbury@mozilla.com>
2014-09-23 15:33:55 +1000

    Bug 1048745 - WebGL2 - GL symbols for uniformui & non-square uniform matrix.; r=jgilbert
    --HG--
    extra : rebase_source : bd47b6bbde06ba7f8a945aa55f751d8a3c074946
    extra : source : 5b135f5f9492b2606c593ad0c800488f6a51274c

--- gfx/gl/GLContextFeatures.cpp
+++ gfx/gl/GLContextFeatures.cpp
@@ -486,6 +486,24 @@ static const FeatureInfo sFeatureInfoArr[] = {
         }
     },
     {
+        "uniform_matrix_nonsquare",
+        210, // OpenGL version
+        300, // OpenGL ES version
+        GLContext::Extension_None,
+        {
+            GLContext::Extensions_End
+        }
+    },
+    {
+        "uniform_uint",
+        300, // OpenGL version
+        300, // OpenGL ES version
+        GLContext::Extension_None,
+        {
+            GLContext::Extensions_End
+        }
+    },
+    {
         "vertex_array_object",
         300, // OpenGL version
         300, // OpenGL ES version
