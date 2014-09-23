b2cc285e18aa133239adfaf02cf74b8a12e303aa
Mike de Boer <mdeboer@mozilla.com>
2014-09-23 12:21:35 +0200

    Bug 1070045: shim contacts React classes when ES6 is not available in the browser used. r=Niko

--- browser/components/loop/ui/fake-mozLoop.js
+++ browser/components/loop/ui/fake-mozLoop.js
@@ -9,5 +9,12 @@
 navigator.mozLoop = {
   ensureRegistered: function() {},
   getLoopCharPref: function() {},
-  getLoopBoolPref: function() {}
+  getLoopBoolPref: function() {},
+  releaseCallData: function() {},
+  contacts: {
+    getAll: function(callback) {
+      callback(null, []);
+    },
+    on: function() {}
+  }
 };
