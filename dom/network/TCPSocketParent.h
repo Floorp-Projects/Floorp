8c06d1b1566d7070d51e469c99b80e3370a168f7
Blair McBride <bmcbride@mozilla.com>
2014-09-23 14:34:53 +1200

    Backout changeset ce59241c232f for build bustage on a CLOSED TREE

--- dom/network/TCPSocketParent.h
+++ dom/network/TCPSocketParent.h
@@ -23,7 +23,7 @@ class PBrowserParent;
 class TCPSocketParentBase : public nsITCPSocketParent
 {
 public:
-  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(TCPSocketParentBase)
+  NS_DECL_CYCLE_COLLECTION_CLASS(TCPSocketParentBase)
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
 
   void AddIPDLReference();
@@ -33,7 +33,6 @@ protected:
   TCPSocketParentBase();
   virtual ~TCPSocketParentBase();
 
-  JS::Heap<JSObject*> mIntermediaryObj;
   nsCOMPtr<nsITCPSocketIntermediary> mIntermediary;
   nsCOMPtr<nsIDOMTCPSocket> mSocket;
   bool mIPCOpen;
@@ -46,7 +45,7 @@ public:
   NS_DECL_NSITCPSOCKETPARENT
   NS_IMETHOD_(MozExternalRefCountType) Release() MOZ_OVERRIDE;
 
-  TCPSocketParent() {}
+  TCPSocketParent() : mIntermediaryObj(nullptr) {}
 
   virtual bool RecvOpen(const nsString& aHost, const uint16_t& aPort,
                         const bool& useSSL, const nsString& aBinaryType);
@@ -61,6 +60,8 @@ public:
 
 private:
   virtual void ActorDestroy(ActorDestroyReason why) MOZ_OVERRIDE;
+
+  JSObject* mIntermediaryObj;
 };
 
 } // namespace dom
