620bfb6528fc76b30ecdb87a0ecf5a69b647e94f
Bill McCloskey <wmccloskey@mozilla.com>
2014-09-24 09:17:33 +0200

    Bug 1068225 - Fix up assertion to check the right thing. r=bholley

--- js/xpconnect/src/XPCJSRuntime.cpp
+++ js/xpconnect/src/XPCJSRuntime.cpp
@@ -587,13 +587,8 @@ AddonWindowOrNull(JSObject *aObj)
     // Addons could theoretically change the prototype of the addon scope, but
     // we pretty much just want to crash if that happens so that we find out
     // about it and get them to change their code.
-    //
-    // XXXbholley - Except unfortunately, that breaks the world right now. See
-    // bug 1068163.
-    if (!js::IsCrossCompartmentWrapper(proto)) {
-        NS_WARNING("An addon modified its global prototype - it likely won't work right!");
-        return nullptr;
-    }
+    MOZ_RELEASE_ASSERT(js::IsCrossCompartmentWrapper(proto) ||
+                       xpc::IsSandboxPrototypeProxy(proto));
     JSObject *mainGlobal = js::UncheckedUnwrap(proto, /* stopAtOuter = */ false);
     MOZ_RELEASE_ASSERT(JS_IsGlobalObject(mainGlobal));
 
