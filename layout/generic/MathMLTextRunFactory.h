8caa56278b93b02e200848e8c49bda73bc5447b9
James Kitchener <jkitch.bug@internode.on.net>
2014-09-24 04:38:00 -0400

    Bug 1069929 - Apply dtls font feature setting for <mover> elements. r=roc

--- layout/generic/MathMLTextRunFactory.h
+++ layout/generic/MathMLTextRunFactory.h
@@ -25,6 +25,7 @@ public:
     // Style effects which may override single character <mi> behaviour
     MATH_FONT_STYLING_NORMAL   = 0x1, // fontstyle="normal" has been set.
     MATH_FONT_WEIGHT_BOLD      = 0x2, // fontweight="bold" has been set.
+    MATH_FONT_FEATURE_DTLS     = 0x4, // font feature dtls should be set
   };
 
 protected:
