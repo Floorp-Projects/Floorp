cae2f56d687dc5bc7b435bbd1188a3e369423a38
ffxbld <>
2014-09-23 20:30:35 -0400

    Automated checkin: version bump for fennec 32.0.3 release. DONTBUILD CLOSED TREE a=release

--- mobile/android/confvars.sh
+++ mobile/android/confvars.sh
@@ -5,7 +5,7 @@
 MOZ_APP_BASENAME=Fennec
 MOZ_APP_VENDOR=Mozilla
 
-MOZ_APP_VERSION=32.0.1
+MOZ_APP_VERSION=32.0.3
 MOZ_APP_UA_NAME=Firefox
 
 MOZ_BRANDING_DIRECTORY=mobile/android/branding/unofficial
