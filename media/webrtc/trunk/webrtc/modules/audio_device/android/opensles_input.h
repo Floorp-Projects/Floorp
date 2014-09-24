acc409a1ce07fead4d3c08ebf3922491b3110c8e
Gian-Carlo Pascutto <gpascutto@mozilla.com>
2014-09-24 19:11:58 +0200

    Bug 1059573 - Add an option to use hardware AEC for WebRTC. r=jesup,ted a=bajaj

--- media/webrtc/trunk/webrtc/modules/audio_device/android/opensles_input.h
+++ media/webrtc/trunk/webrtc/modules/audio_device/android/opensles_input.h
@@ -17,6 +17,8 @@
 
 #if !defined(WEBRTC_GONK)
 #include "webrtc/modules/audio_device/android/audio_manager_jni.h"
+#else
+#include "media/AudioEffect.h"
 #endif
 #include "webrtc/modules/audio_device/android/low_latency_event.h"
 #include "webrtc/modules/audio_device/include/audio_device.h"
@@ -143,6 +145,11 @@ class OpenSlesInput {
   // etc, so it should be called when starting recording.
   bool CreateAudioRecorder();
   void DestroyAudioRecorder();
+#if defined(WEBRTC_GONK) && defined(WEBRTC_HARDWARE_AEC_NS)
+  void SetupAECAndNS();
+  void SetupVoiceMode();
+  bool CheckPlatformAEC();
+#endif
 
   // When overrun happens there will be more frames received from OpenSL than
   // the desired number of buffers. It is possible to expand the number of
@@ -220,6 +227,10 @@ class OpenSlesInput {
   uint32_t rec_sampling_rate_;
   bool agc_enabled_;
 
+#if defined(WEBRTC_GONK) && defined(WEBRTC_HARDWARE_AEC_NS)
+  android::AudioEffect* aec_;
+  android::AudioEffect* ns_;
+#endif
   // Audio status
   uint16_t recording_delay_;
 
