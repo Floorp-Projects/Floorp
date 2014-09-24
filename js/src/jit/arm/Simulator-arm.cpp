3bbcf485814f85151ff0c91a150b6444d9f116dd
Lars T Hansen <lhansen@mozilla.com>
2014-09-24 13:52:11 +0200

    Bug 1071604 - Mechanisms for triggering the ARM simulator's REPL.  r=mjrosenb

--- js/src/jit/arm/Simulator-arm.cpp
+++ js/src/jit/arm/Simulator-arm.cpp
@@ -651,6 +651,7 @@ ReadLine(const char *prompt)
     return result;
 }
 
+// Observe that llvm-mc may have a different name on your system.  Make a symlink.
 static void
 DisassembleInstruction(uint32_t pc)
 {
