41e4f7937099c2eff0c87c25554b49fd7e4ba8e1
Hannes Verschore <hv1989@gmail.com>
2014-09-24 12:47:58 +0200

    Bug 1071879 - IonMonkey: Hoist operand when folding ternary structure, r=nbp

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -1225,8 +1225,12 @@ MPhi::foldsTernary()
     // If testArg is a number type we can:
     // - fold testArg ? testArg : 0 to testArg
     // - fold testArg ? 0 : testArg to 0
-    if (IsNumberType(testArg->type()) && c->vp()->toNumber() == 0)
+    if (IsNumberType(testArg->type()) && c->vp()->toNumber() == 0) {
+        // When folding to the constant we need to hoist it.
+        if (trueDef == c)
+            c->block()->moveBefore(pred->lastIns(), c);
         return trueDef;
+    }
 
     // If testArg is a string type we can:
     // - fold testArg ? testArg : "" to testArg
@@ -1234,6 +1238,9 @@ MPhi::foldsTernary()
     if (testArg->type() == MIRType_String &&
         c->vp()->toString() == GetIonContext()->runtime->emptyString())
     {
+        // When folding to the constant we need to hoist it.
+        if (trueDef == c)
+            c->block()->moveBefore(pred->lastIns(), c);
         return trueDef;
     }
 
