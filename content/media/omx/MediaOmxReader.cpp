75d49921dc85d5533b44a429df8a1dc95d5783b8
Blake Wu <bwu@mozilla.com>
2014-09-24 09:41:49 +0800

    Bug 1069857 - Clear seek timestamp in time. r=jwwang, r=cpearce, a=bajaj

--- content/media/omx/MediaOmxReader.cpp
+++ content/media/omx/MediaOmxReader.cpp
@@ -237,6 +237,7 @@ bool MediaOmxReader::DecodeVideoFrame(bool &aKeyframeSkip,
       return false;
     }
     doSeek = false;
+    mVideoSeekTimeUs = -1;
 
     // Ignore empty buffer which stagefright media read will sporadically return
     if (frame.mSize == 0 && !frame.mGraphicBuffer) {
@@ -251,7 +252,6 @@ bool MediaOmxReader::DecodeVideoFrame(bool &aKeyframeSkip,
 
     mSkipCount = 0;
 
-    mVideoSeekTimeUs = -1;
     aKeyframeSkip = false;
 
     IntRect picture = ToIntRect(mPicture);
