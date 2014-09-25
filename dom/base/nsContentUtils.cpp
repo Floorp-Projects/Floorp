e23df24c11aab5d0e6feb28319c8f1d37d4b318c
Bobby Holley <bobbyholley@gmail.com>
2014-09-25 10:48:05 +0200

    Bug 1072150 - Crash in SubjectPrincipal if there's no active AutoJSAPI. r=bz

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -2710,7 +2710,7 @@ nsContentUtils::SubjectPrincipal()
   MOZ_ASSERT(NS_IsMainThread());
   JSContext* cx = GetCurrentJSContext();
   if (!cx) {
-    return GetSystemPrincipal();
+    MOZ_CRASH("Accessing the Subject Principal without an AutoJSAPI on the stack is forbidden");
   }
 
   JSCompartment *compartment = js::GetContextCompartment(cx);
