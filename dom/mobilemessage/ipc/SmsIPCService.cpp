2124657509386fcf2acbda4076cf2012d21fbf13
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2014-09-25 10:10:42 -0400

    Bug 1072701 - Build some Firefox OS specific DOM code in unified mode; r=baku
    --HG--
    extra : rebase_source : 4a8812c18341c9851fb3265e1f8e6c77a95911c5

--- dom/mobilemessage/ipc/SmsIPCService.cpp
+++ dom/mobilemessage/ipc/SmsIPCService.cpp
@@ -19,14 +19,8 @@ using namespace mozilla::dom::mobilemessage;
 
 namespace {
 
-const char* kPrefRilNumRadioInterfaces = "ril.numRadioInterfaces";
 #define kPrefMmsDefaultServiceId "dom.mms.defaultServiceId"
 #define kPrefSmsDefaultServiceId "dom.sms.defaultServiceId"
-const char* kObservedPrefs[] = {
-  kPrefMmsDefaultServiceId,
-  kPrefSmsDefaultServiceId,
-  nullptr
-};
 
 // TODO: Bug 767082 - WebSMS: sSmsChild leaks at shutdown
 PSmsChild* gSmsChild;
@@ -86,6 +80,7 @@ SendCursorRequest(const IPCMobileMessageCursor& aRequest,
 uint32_t
 getDefaultServiceId(const char* aPrefKey)
 {
+  static const char* kPrefRilNumRadioInterfaces = "ril.numRadioInterfaces";
   int32_t id = mozilla::Preferences::GetInt(aPrefKey, 0);
   int32_t numRil = mozilla::Preferences::GetInt(kPrefRilNumRadioInterfaces, 1);
 
@@ -119,6 +114,11 @@ SmsIPCService::GetSingleton()
 
 SmsIPCService::SmsIPCService()
 {
+  static const char* kObservedPrefs[] = {
+    kPrefMmsDefaultServiceId,
+    kPrefSmsDefaultServiceId,
+    nullptr
+  };
   Preferences::AddStrongObservers(this, kObservedPrefs);
   mMmsDefaultServiceId = getDefaultServiceId(kPrefMmsDefaultServiceId);
   mSmsDefaultServiceId = getDefaultServiceId(kPrefSmsDefaultServiceId);
