21fbe5971f68993e1542a81734af987099c0283e
Ed Morley <emorley@mozilla.com>
2014-09-26 16:10:57 +0100

    Backed out changeset 6e5adc14dc82 (bug 964225)

---
+++ intl/uconv/ucvcn/nsUnicodeToGB2312V2.h
@@ -0,0 +1,50 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef nsUnicodeToGB2312V2_h___
+#define nsUnicodeToGB2312V2_h___
+
+#include "nsUCSupport.h"
+#include "nsGBKConvUtil.h"
+
+//----------------------------------------------------------------------
+// Class nsUnicodeToGB2312V2 [declaration]
+
+/**
+ * A character set converter from Unicode to GB2312.
+ *
+ * @created         06/Apr/1999
+ * @author  Catalin Rotaru [CATA]
+ */
+class nsUnicodeToGB2312V2 : public nsEncoderSupport
+{
+public:
+
+  /**
+   * Class constructor.
+   */
+  nsUnicodeToGB2312V2();
+
+protected:
+
+  NS_IMETHOD ConvertNoBuff(const char16_t * aSrc, 
+                            int32_t * aSrcLength, 
+                            char * aDest, 
+                            int32_t * aDestLength);
+
+  //--------------------------------------------------------------------
+  // Subclassing of nsEncoderSupport class [declaration]
+
+  NS_IMETHOD ConvertNoBuffNoErr(const char16_t * aSrc, int32_t * aSrcLength, 
+                                char * aDest, int32_t * aDestLength)
+  {
+    return NS_OK;
+  }   // just make it not abstract;
+
+protected:
+  nsGBKConvUtil mUtil;
+};
+
+#endif /* nsUnicodeToGB2312V2_h___ */
