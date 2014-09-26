02533aaee7b1acf72da6c2291c6c143619b7df42
Birunthan Mohanathas <birunthan@mohanathas.com>
2014-09-26 07:20:23 +0300

    Bug 1045289 - Change uses of "XPCom" to "XPCOM". r=froydnj

--- xpcom/build/nsXPCOMPrivate.h
+++ xpcom/build/nsXPCOMPrivate.h
@@ -4,8 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef nsXPComPrivate_h__
-#define nsXPComPrivate_h__
+#ifndef nsXPCOMPrivate_h__
+#define nsXPCOMPrivate_h__
 
 #include "nscore.h"
 #include "nsXPCOM.h"
