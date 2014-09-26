f1ba3d834d8d3788ef55a2b2ea83753bda4bd1b7
Randell Jesup <rjesup@jesup.org>
2014-09-26 14:13:17 -0400

    Bug 1033066 - Never let AudioSegments underflow mDuration and cause OOM allocation. r=karlt, a=sledru

--- content/media/AudioSegment.cpp
+++ content/media/AudioSegment.cpp
@@ -154,7 +154,8 @@ AudioSegment::WriteTo(uint64_t aID, AudioMixer& aMixer, uint32_t aOutputChannels
   // Offset in the buffer that will end up sent to the AudioStream, in samples.
   uint32_t offset = 0;
 
-  if (!GetDuration()) {
+  if (GetDuration() <= 0) {
+    MOZ_ASSERT(GetDuration() == 0);
     return;
   }
 
