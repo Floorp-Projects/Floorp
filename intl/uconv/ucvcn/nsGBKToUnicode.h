84f4adffcf733877173690b9aba372ee6373ab35
Ed Morley <emorley@mozilla.com>
2014-09-26 16:10:57 +0100

    Backed out changeset 6e5adc14dc82 (bug 964225)

--- intl/uconv/ucvcn/nsGBKToUnicode.h
+++ intl/uconv/ucvcn/nsGBKToUnicode.h
@@ -2,8 +2,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#ifndef nsGBKToUnicode_h___
-#define nsGBKToUnicode_h___
+#ifndef nsGBK2312ToUnicode_h___
+#define nsGBK2312ToUnicode_h___
 
 #include "nsCOMPtr.h"
 #include "nsIUnicodeDecoder.h"
@@ -11,23 +11,23 @@
 #include "nsGBKConvUtil.h"
 
 //----------------------------------------------------------------------
-// Class nsGB18030ToUnicode [declaration]
+// Class nsGBKToUnicode [declaration]
 
 /**
- * A character set converter from GB18030 to Unicode.
+ * A character set converter from GBK to Unicode.
  * 
  *
  * @created         07/Sept/1999
  * @author  Yueheng Xu, Yueheng.Xu@intel.com
  */
-class nsGB18030ToUnicode : public nsBufferDecoderSupport
+class nsGBKToUnicode : public nsBufferDecoderSupport
 {
 public:
 		  
   /**
    * Class constructor.
    */
-  nsGB18030ToUnicode() : nsBufferDecoderSupport(1)
+  nsGBKToUnicode() : nsBufferDecoderSupport(1)
   {
     mExtensionDecoder = nullptr;
     m4BytesDecoder = nullptr;
@@ -44,13 +44,25 @@ protected:
   nsCOMPtr<nsIUnicodeDecoder> mExtensionDecoder;
   nsCOMPtr<nsIUnicodeDecoder> m4BytesDecoder;
 
-  void CreateExtensionDecoder();
-  void Create4BytesDecoder();
+  virtual void CreateExtensionDecoder();
+  virtual void Create4BytesDecoder();
   bool TryExtensionDecoder(const char* aSrc, char16_t* aDest);
   bool Try4BytesDecoder(const char* aSrc, char16_t* aDest);
-  bool DecodeToSurrogate(const char* aSrc, char16_t* aDest);
+  virtual bool DecodeToSurrogate(const char* aSrc, char16_t* aDest);
 
 };
 
-#endif /* nsGBKToUnicode_h___ */
+
+class nsGB18030ToUnicode : public nsGBKToUnicode
+{
+public:
+  nsGB18030ToUnicode() {}
+  virtual ~nsGB18030ToUnicode() {}
+protected:
+  virtual void CreateExtensionDecoder();
+  virtual void Create4BytesDecoder();
+  virtual bool DecodeToSurrogate(const char* aSrc, char16_t* aDest);
+};
+
+#endif /* nsGBK2312ToUnicode_h___ */
 
