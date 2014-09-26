4d2e0520e3fb1c423d83494f6dd5142cb438cbf8
Walter Litwinczyk <wlitwinczyk@mozilla.com>
2014-09-26 15:06:38 -0700

    Bug 1015561 - WebGL change getParameter(STENCIL_[BACK]_REF) to return correct masked values and update debug cached value test. r=jgilbert

--- dom/canvas/WebGLContext.h
+++ dom/canvas/WebGLContext.h
@@ -836,6 +836,7 @@ private:
 public:
     void Disable(GLenum cap);
     void Enable(GLenum cap);
+    bool GetStencilBits(GLint* out_stencilBits);
     JS::Value GetParameter(JSContext* cx, GLenum pname, ErrorResult& rv);
     void GetParameter(JSContext* cx, GLenum pname,
                       JS::MutableHandle<JS::Value> retval, ErrorResult& rv) {
