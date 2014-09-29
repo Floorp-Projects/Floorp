d53750d0f23134e6737db905c9b90ec34e98e515
Blair McBride <bmcbride@mozilla.com>
2014-09-30 11:16:48 +1300

    Bug 1073846 - Wrong URL loads after autofill of a hostname that matches a search engine. r=mak

--- toolkit/components/places/tests/unifiedcomplete/test_searchEngine_host.js
+++ toolkit/components/places/tests/unifiedcomplete/test_searchEngine_host.js
@@ -1,6 +1,42 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+Cu.import("resource://testing-common/httpd.js");
+
+function* addTestEngines(items) {
+  let httpServer = new HttpServer();
+  httpServer.start(-1);
+  httpServer.registerDirectory("/", do_get_cwd());
+  let gDataUrl = "http://localhost:" + httpServer.identity.primaryPort + "/data/";
+  do_register_cleanup(() => httpServer.stop(() => {}));
+
+  let engines = [];
+
+  for (let item of items) {
+    do_print("Adding engine: " + item);
+    yield new Promise(resolve => {
+      Services.obs.addObserver(function obs(subject, topic, data) {
+        let engine = subject.QueryInterface(Ci.nsISearchEngine);
+        do_print("Observed " + data + " for " + engine.name);
+        if (data != "engine-added" || engine.name != item) {
+          return;
+        }
+
+        Services.obs.removeObserver(obs, "browser-search-engine-modified");
+        engines.push(engine);
+        resolve();
+      }, "browser-search-engine-modified", false);
+
+      do_print("`Adding engine from URL: " + gDataUrl + item);
+      Services.search.addEngine(gDataUrl + item,
+                                Ci.nsISearchEngine.DATA_XML, null, false);
+    });
+  }
+
+  return engines;
+}
+
+
 add_task(function* test_searchEngine_autoFill() {
   Services.search.addEngineWithDetails("MySearchEngine", "", "", "",
                                        "GET", "http://my.search.com/");
@@ -15,7 +51,7 @@ add_task(function* test_searchEngine_autoFill() {
   }
   yield promiseAddVisits(visits);
   addBookmark({ uri: uri, title: "Example bookmark" });
-  Assert.ok(frecencyForUrl(uri) > 10000);
+  ok(frecencyForUrl(uri) > 10000, "Adeded URI should have expected high frecency");
 
   do_log_info("Check search domain is autoFilled even if there's an higher frecency match");
   yield check_autocomplete({
@@ -26,3 +62,23 @@ add_task(function* test_searchEngine_autoFill() {
 
   yield cleanup();
 });
+
+add_task(function* test_searchEngine_noautoFill() {
+  let engineName = "engine-rel-searchform.xml";
+  let [engine] = yield addTestEngines([engineName]);
+  do_register_cleanup(() => Services.search.removeEngine(engine));
+  equal(engine.searchForm, "http://example.com/?search");
+
+  Services.prefs.setBoolPref("browser.urlbar.autoFill.typed", false);
+  yield promiseAddVisits(NetUtil.newURI("http://example.com/my/"));
+
+  do_print("Check search domain is not autoFilled if it matches a visited domain");
+  yield check_autocomplete({
+    search: "example",
+    autofilled: "example.com/",
+    completed: "example.com/"
+  });
+
+  yield cleanup();
+});
+
