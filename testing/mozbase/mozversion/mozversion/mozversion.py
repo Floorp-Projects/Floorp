816ab72ebee26f8396457ab88f9113cb8536f983
William Lachance <wlachance@mozilla.com>
2014-09-29 14:49:49 -0400

    Bug 1073698 - mozversion should accept binary_path without .exe extension on windows;r=davehunt

--- testing/mozbase/mozversion/mozversion/mozversion.py
+++ testing/mozbase/mozversion/mozversion/mozversion.py
@@ -93,7 +93,11 @@ class LocalVersion(Version):
 
         if binary:
             if not os.path.exists(binary):
-                raise IOError('Binary path does not exist: %s' % binary)
+                # on windows we should also try extending the binary path with
+                # .exe and see if that's valid
+                if (sys.platform == 'win32' and not binary.endswith('.exe') and
+                    not os.path.exists(binary + '.exe')):
+                    raise IOError('Binary path does not exist: %s' % binary)
             path = find_location(os.path.dirname(os.path.realpath(binary)))
         else:
             path = find_location(os.getcwd())
