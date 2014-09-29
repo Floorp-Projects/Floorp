914357c4d82cf9d18697c18dc281a5c08c6460ed
Robert Strong <robert.bugzilla@gmail.com>
2014-09-29 11:53:11 -0700

    Mac v2 signing - Bug 1074000 - Update xpcshell-tests for new OSX v2 bundle structure to run on comm-central. r=jmaher

--- testing/testsuite-targets.mk
+++ testing/testsuite-targets.mk
@@ -289,6 +289,12 @@ jstestbrowser:
 
 GARBAGE += $(addsuffix .log,$(MOCHITESTS) reftest crashtest jstestbrowser)
 
+ifeq ($(OS_ARCH),Darwin)
+xpcshell_path = $(TARGET_DIST)/$(MOZ_MACBUNDLE_NAME)/Contents/MacOS/xpcshell
+else
+xpcshell_path = $(LIBXUL_DIST)/bin/xpcshell
+endif
+
 # Execute all xpcshell tests in the directories listed in the manifest.
 # See also config/rules.mk 'xpcshell-tests' target for local execution.
 # Usage: |make [TEST_PATH=...] [EXTRA_TEST_ARGS=...] xpcshell-tests|.
@@ -309,7 +315,7 @@ xpcshell-tests:
 	  --xunit-suite-name=xpcshell \
           $(SYMBOLS_PATH) \
 	  $(TEST_PATH_ARG) $(EXTRA_TEST_ARGS) \
-	  $(LIBXUL_DIST)/bin/xpcshell
+	  $(xpcshell_path)
 
 B2G_XPCSHELL = \
 	rm -f ./@.log && \
