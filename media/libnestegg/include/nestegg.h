4c52a9fd0bfd14bd38feb7b4e70540b725f23c31
Matthew Gregan <kinetik@flim.org>
2014-09-29 13:13:10 +1300

    Bug 1074004 - Update nestegg from upstream.  r=giles

--- media/libnestegg/include/nestegg.h
+++ media/libnestegg/include/nestegg.h
@@ -381,8 +381,11 @@ int nestegg_sniff(unsigned char const * buffer, size_t length);
  * Set the underlying allocation function for library allocations.
  *
  * @param realloc_func The desired function.
+ * @retval 0 realloc_func(p, 0) does not act as free()
+ * @retval 1 realloc_func(p, 0) acts as free()
+ * @retval -1 malloc failed during realloc_func test
  */
-void nestegg_set_halloc_func(void * (* realloc_func)(void *, size_t));
+int nestegg_set_halloc_func(void * (* realloc_func)(void *, size_t));
 
 #if defined(__cplusplus)
 }
