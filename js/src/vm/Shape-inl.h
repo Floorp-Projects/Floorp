ac747f5ef1c35f7e9ab6bd40a4a9c5fd4a7f9ec3
Till Schneidereit <till@tillschneidereit.net>
2014-09-30 01:17:55 +0200

    Bug 1067942 - Change error message for attempted getter-only property writes to include the property name. r=terrence,Ms2ger

--- js/src/vm/Shape-inl.h
+++ js/src/vm/Shape-inl.h
@@ -96,7 +96,7 @@ Shape::set(JSContext* cx, HandleObject obj, HandleObject receiver, bool strict,
     }
 
     if (attrs & JSPROP_GETTER)
-        return js_ReportGetterOnlyAssignment(cx, strict);
+        return js_ReportGetterOnlyAssignment(cx, propid(), strict);
 
     RootedId id(cx, propid());
 
