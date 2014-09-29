22dc1ab86ac5ad913fda7a4e4f2371146683869d
Bas Schouten <bschouten@mozilla.com>
2014-09-29 17:57:54 +0200

    Bug 1060736 - Disable all acceleration on HD 3000 for driver versions 2321 or lower. r=nical, a=sledru

--- widget/windows/GfxInfo.cpp
+++ widget/windows/GfxInfo.cpp
@@ -976,12 +976,12 @@ GfxInfo::GetGfxDriverInfo()
       DRIVER_LESS_THAN, GfxDriverInfo::allDriverVersions );
 
     /**
-     * Disable D2D on Intel HD 3000 for graphics drivers <= 8.15.10.2321.
-     * See bug 1018278.
+     * Disable acceleration on Intel HD 3000 for graphics drivers <= 8.15.10.2321.
+     * See bug 1018278 and bug 1060736.
      */
     APPEND_TO_DRIVER_BLOCKLIST( DRIVER_OS_ALL,
         (nsAString&) GfxDriverInfo::GetDeviceVendor(VendorIntel), (GfxDeviceFamily*) GfxDriverInfo::GetDeviceFamily(IntelHD3000),
-      nsIGfxInfo::FEATURE_DIRECT2D, nsIGfxInfo::FEATURE_BLOCKED_DRIVER_VERSION,
+      GfxDriverInfo::allFeatures, nsIGfxInfo::FEATURE_BLOCKED_DRIVER_VERSION,
       DRIVER_LESS_THAN_OR_EQUAL, V(8,15,10,2321), "8.15.10.2342" );
 
     /* Disable D2D on Win7 on Intel HD Graphics on driver <= 8.15.10.2302
