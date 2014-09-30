43f4c6d3a472b152274f99babd1293fb0758cea0
Mark Banner <standard8@mozilla.com>
2014-09-30 20:44:05 +0100

    Bug 972017 Part 2 - Set up actions and a dispatcher and start to handle obtaining call data for outgoing Loop calls from the desktop client. r=mikedeboer
    --HG--
    extra : transplant_source : %5D%2Ci%EB%B3%21%1C%97%AD%CDo%90Gn%A1%E3%7D%A1%CDf

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1597,6 +1597,7 @@ pref("loop.retry_delay.limit", 300000);
 pref("loop.feedback.baseUrl", "https://input.mozilla.org/api/v1/feedback");
 pref("loop.feedback.product", "Loop");
 pref("loop.debug.loglevel", "Error");
+pref("loop.debug.dispatcher", false);
 pref("loop.debug.websocket", false);
 pref("loop.debug.sdk", false);
 
