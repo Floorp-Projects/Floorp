a5d9146e27cb69373c39b9b9f4b65e9732073a9e
Jeff Muizelaar <jmuizelaar@mozilla.com>
2014-09-30 16:20:48 -0400

    Bug 1066664 - Fix screenshot rotation. r=mwoodrow, a=bajaj
    This moves the screenshot (now-nonexistent) unrotation code from the opengl
    compositor into ClientLayerManager.cpp

--- widget/xpwidgets/WidgetUtils.cpp
+++ widget/xpwidgets/WidgetUtils.cpp
@@ -37,4 +37,56 @@ ComputeTransformForRotation(const nsIntRect& aBounds,
     return transform;
 }
 
+gfx::Matrix
+ComputeTransformForUnRotation(const nsIntRect& aBounds,
+                              ScreenRotation aRotation)
+{
+    gfx::Matrix transform;
+    static const gfx::Float floatPi = static_cast<gfx::Float>(M_PI);
+
+    switch (aRotation) {
+    case ROTATION_0:
+        break;
+    case ROTATION_90:
+        transform.PreTranslate(0, aBounds.height);
+        transform.PreRotate(floatPi * 3 / 2);
+        break;
+    case ROTATION_180:
+        transform.PreTranslate(aBounds.width, aBounds.height);
+        transform.PreRotate(floatPi);
+        break;
+    case ROTATION_270:
+        transform.PreTranslate(aBounds.width, 0);
+        transform.PreRotate(floatPi / 2);
+        break;
+    default:
+        MOZ_CRASH("Unknown rotation");
+    }
+    return transform;
+}
+
+nsIntRect RotateRect(nsIntRect aRect,
+                     const nsIntRect& aBounds,
+                     ScreenRotation aRotation)
+{
+  switch (aRotation) {
+    case ROTATION_0:
+      return aRect;
+    case ROTATION_90:
+      return nsIntRect(aRect.y,
+                       aBounds.width - aRect.x - aRect.width,
+                       aRect.height, aRect.width);
+    case ROTATION_180:
+      return nsIntRect(aBounds.width - aRect.x - aRect.width,
+                       aBounds.height - aRect.y - aRect.height,
+                       aRect.width, aRect.height);
+    case ROTATION_270:
+      return nsIntRect(aBounds.height - aRect.y - aRect.height,
+                       aRect.x,
+                       aRect.height, aRect.width);
+    default:
+      MOZ_CRASH("Unknown rotation");
+      return aRect;
+  }
+}
 } // namespace mozilla
