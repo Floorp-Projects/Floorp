56b287da38ef596a3cb62b84c45790a3719dea59
Andrea Marchesini <amarchesini@mozilla.com>
2014-09-30 18:35:32 +0100

    Bug 1073615 - One MediaStreamGraph singleton per audioChannel, r=roc

--- content/media/MediaStreamGraphImpl.h
+++ content/media/MediaStreamGraphImpl.h
@@ -654,6 +654,8 @@ public:
   nsRefPtr<AudioOutputObserver> mFarendObserverRef;
 #endif
 
+  uint32_t AudioChannel() const { return mAudioChannel; }
+
 private:
   virtual ~MediaStreamGraphImpl();
 
@@ -687,6 +689,9 @@ private:
   bool mCanRunMessagesSynchronously;
 #endif
 
+  // We use uint32_t instead AudioChannel because this is just used as key for
+  // the hashtable gGraphs.
+  uint32_t mAudioChannel;
 };
 
 }
