5fe316221d298265f8406a16c4e79838a468cd47
Kai Engert <kaie@kuix.de>
2014-10-01 19:30:41 +0200

    Bug 1075686, pick up NSS 3.17.2 beta 1 to fix bug 1057161

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -33,12 +33,12 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define NSS_VERSION  "3.17.1" _NSS_ECC_STRING _NSS_CUSTOMIZED
+#define NSS_VERSION  "3.17.2" _NSS_ECC_STRING _NSS_CUSTOMIZED " Beta"
 #define NSS_VMAJOR   3
 #define NSS_VMINOR   17
-#define NSS_VPATCH   1
+#define NSS_VPATCH   2
 #define NSS_VBUILD   0
-#define NSS_BETA     PR_FALSE
+#define NSS_BETA     PR_TRUE
 
 #ifndef RC_INVOKED
 
