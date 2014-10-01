6dda96be9beab31c4200fc16f8036ec319062612
Chenxia Liu <liuche@mozilla.com>
2014-10-01 10:26:17 -0700

    Bug 1074570 - Dismiss start pane on touch. r=rnewman, a=lmandel

--- mobile/android/base/StartPane.java
+++ mobile/android/base/StartPane.java
@@ -1,12 +1,16 @@
 package org.mozilla.gecko;
 
 import org.mozilla.gecko.fxa.activities.FxAccountGetStartedActivity;
+import org.mozilla.gecko.util.HardwareUtils;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 
 public class StartPane extends Activity {
@@ -35,6 +39,10 @@ public class StartPane extends Activity {
                 showBrowser();
             }
         });
+
+        if (!HardwareUtils.isTablet() && !HardwareUtils.isTelevision()) {
+            addDismissHandler();
+        }
     }
 
     private void showBrowser() {
@@ -47,5 +55,23 @@ public class StartPane extends Activity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         finish();
+    }
+
+    // Add handler for dismissing the StartPane on a single click.
+    private void addDismissHandler() {
+        final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                StartPane.this.finish();
+                return true;
+            }
+        });
+
+        findViewById(R.id.onboard_content).setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        });
     }
 }
