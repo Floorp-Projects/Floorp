bbc9e3696573fa49e48cefb9d30e2679ed80b5c4
Ethan Tseng <ettseng@mozilla.com>
2014-10-02 11:44:29 +0800

    Bug 1072050 - Add pref for setting device identifier in UA string. r=khuey, sr=sicking

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -876,6 +876,8 @@ pref("general.useragent.updates.enabled", true);
 pref("general.useragent.updates.url", "https://dynamicua.cdn.mozilla.net/0/%APP_ID%");
 pref("general.useragent.updates.interval", 604800); // 1 week
 pref("general.useragent.updates.retry", 86400); // 1 day
+// Device ID can be composed of letter, numbers, hyphen ("-") and dot (".")
+pref("general.useragent.device_id", "");
 
 // Make <audio> and <video> talk to the AudioChannelService.
 pref("media.useAudioChannelService", true);
