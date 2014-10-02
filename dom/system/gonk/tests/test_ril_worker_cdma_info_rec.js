34d2697297557c9cfc57ff009a07d703f1d26e55
Bevis Tseng <btseng@mozilla.com>
2014-10-02 18:26:37 +0800

    Bug 1072367 - Part 2: Decode EXTENDED_DISPLAY & SIGNAL properly. r=echen

--- dom/system/gonk/tests/test_ril_worker_cdma_info_rec.js
+++ dom/system/gonk/tests/test_ril_worker_cdma_info_rec.js
@@ -36,6 +36,10 @@ function newWorkerWithParcel(parcelBuf) {
     return buf[index++];
   };
 
+  context.Buf.seekIncoming = function(offset) {
+    index += offset / context.Buf.UINT32_SIZE;
+  };
+
   return worker;
 }
 
@@ -67,24 +71,15 @@ add_test(function test_extended_display() {
   let worker = newWorkerWithParcel([
                 0x01, // one inforemation record
                 0x07, // type: extended display
-                0x0E, // length: 14
-                0x80, // header byte
-                0x80, // Blank
-                0x81, // Skip
-                0x9B, // Text
-                0x09, 0x54, 0x65, 0x73, 0x74, 0x20, 0x49, 0x6E,
-                0x66, 0x6F, 0x00]);
+                0x12, // length: 18
+                0x54, 0x65, 0x73, 0x74, 0x20, 0x45, 0x78, 0x74,
+                0x65, 0x6E, 0x64, 0x65, 0x64, 0x20, 0x49, 0x6E,
+                0x66, 0x6F, 0x00, 0x00]);
   let context = worker.ContextPool._contexts[0];
   let helper = context.CdmaPDUHelper;
   let records = helper.decodeInformationRecord();
 
-  do_check_eq(records[0].extendedDisplay.indicator, 1);
-  do_check_eq(records[0].extendedDisplay.type, 0);
-  do_check_eq(records[0].extendedDisplay.records.length, 3);
-  do_check_eq(records[0].extendedDisplay.records[0].tag, 0x80);
-  do_check_eq(records[0].extendedDisplay.records[1].tag, 0x81);
-  do_check_eq(records[0].extendedDisplay.records[2].tag, 0x9B);
-  do_check_eq(records[0].extendedDisplay.records[2].content, "Test Info");
+  do_check_eq(records[0].display, "Test Extended Info");
 
   run_next_test();
 });
@@ -96,29 +91,19 @@ add_test(function test_mixed() {
   let worker = newWorkerWithParcel([
                 0x02, // two inforemation record
                 0x00, // type: display
-                0x09, // length: 9
+                0x0B, // length: 11
                 0x54, 0x65, 0x73, 0x74, 0x20, 0x49, 0x6E, 0x66,
-                0x6F, 0x00,
+                0x6F, 0x20, 0x31, 0x00,
                 0x07, // type: extended display
-                0x0E, // length: 14
-                0x80, // header byte
-                0x80, // Blank
-                0x81, // Skip
-                0x9B, // Text
-                0x09, 0x54, 0x65, 0x73, 0x74, 0x20, 0x49, 0x6E,
-                0x66, 0x6F, 0x00]);
+                0x0B, // length: 11
+                0x54, 0x65, 0x73, 0x74, 0x20, 0x49, 0x6E, 0x66,
+                0x6F, 0x20, 0x32, 0x00]);
   let context = worker.ContextPool._contexts[0];
   let helper = context.CdmaPDUHelper;
   let records = helper.decodeInformationRecord();
 
-  do_check_eq(records[0].display, "Test Info");
-  do_check_eq(records[1].extendedDisplay.indicator, 1);
-  do_check_eq(records[1].extendedDisplay.type, 0);
-  do_check_eq(records[1].extendedDisplay.records.length, 3);
-  do_check_eq(records[1].extendedDisplay.records[0].tag, 0x80);
-  do_check_eq(records[1].extendedDisplay.records[1].tag, 0x81);
-  do_check_eq(records[1].extendedDisplay.records[2].tag, 0x9B);
-  do_check_eq(records[1].extendedDisplay.records[2].content, "Test Info");
+  do_check_eq(records[0].display, "Test Info 1");
+  do_check_eq(records[1].display, "Test Info 2");
 
   run_next_test();
 });
@@ -143,6 +128,48 @@ add_test(function test_multiple() {
 
   do_check_eq(records[0].display, "Test Info 1");
   do_check_eq(records[1].display, "Test Info 2");
+
+  run_next_test();
+});
+
+/**
+ * Verify decoder for Signal Type
+ */
+add_test(function test_signal() {
+  let worker = newWorkerWithParcel([
+                0x01,   // one inforemation record
+                0x04,   // type: signal
+                0x01,   // isPresent: non-zero
+                0x00,   // signalType: Tone signal (00)
+                0x01,   // alertPitch: High pitch
+                0x03]); // signal: Abbreviated intercept (000011)
+  let context = worker.ContextPool._contexts[0];
+  let helper = context.CdmaPDUHelper;
+  let records = helper.decodeInformationRecord();
+
+  do_check_eq(records[0].signal.type, 0x00);
+  do_check_eq(records[0].signal.alertPitch, 0x01);
+  do_check_eq(records[0].signal.signal, 0x03);
+
+  run_next_test();
+});
+
+/**
+ * Verify decoder for Signal Type for Not Presented
+ */
+add_test(function test_signal_not_present() {
+  let worker = newWorkerWithParcel([
+                0x01,   // one inforemation record
+                0x04,   // type: signal
+                0x00,   // isPresent: zero
+                0x00,   // signalType: Tone signal (00)
+                0x01,   // alertPitch: High pitch
+                0x03]); // signal: Abbreviated intercept (000011)
+  let context = worker.ContextPool._contexts[0];
+  let helper = context.CdmaPDUHelper;
+  let records = helper.decodeInformationRecord();
+
+  do_check_eq(records.length, 0);
 
   run_next_test();
 });
