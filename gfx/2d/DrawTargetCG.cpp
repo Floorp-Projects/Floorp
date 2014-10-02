f467a473afca73aba8b99599c9328731987aea22
Matt Woodrow <mwoodrow@mozilla.com>
2014-10-02 21:14:46 +1300

    Bug 1070722 - Remove DrawTargetCG::OptimizeSourceSurface since we don't get any benefit without quartz high-quality downscaling enabled. r=jrmuizel

--- gfx/2d/DrawTargetCG.cpp
+++ gfx/2d/DrawTargetCG.cpp
@@ -251,16 +251,7 @@ GetRetainedImageFromSourceSurface(SourceSurface *aSurface)
 TemporaryRef<SourceSurface>
 DrawTargetCG::OptimizeSourceSurface(SourceSurface *aSurface) const
 {
-  if (aSurface->GetType() == SurfaceType::COREGRAPHICS_IMAGE ||
-      aSurface->GetType() == SurfaceType::COREGRAPHICS_CGCONTEXT) {
-    return aSurface;
-  }
-  RefPtr<DataSourceSurface> data = aSurface->GetDataSurface();
-
-  return CreateSourceSurfaceFromData(data->GetData(),
-                                     data->GetSize(),
-                                     data->Stride(),
-                                     data->GetFormat());
+  return aSurface;
 }
 
 class UnboundnessFixer
