df13c3a60ef89d63c35066a208ba58188097b328
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-10-03 14:37:19 -0400

    Bug 1075232 - Record which suggestion of the search screen was tapped in telemetry. r=mfinkle, a=lmandel

--- mobile/android/base/home/SearchEngineRow.java
+++ mobile/android/base/home/SearchEngineRow.java
@@ -89,7 +89,8 @@ class SearchEngineRow extends AnimatedHeightLayout {
                     if (v == mUserEnteredView) {
                         Telemetry.sendUIEvent(TelemetryContract.Event.LOAD_URL, TelemetryContract.Method.SUGGESTION, "user");
                     } else {
-                        Telemetry.sendUIEvent(TelemetryContract.Event.LOAD_URL, TelemetryContract.Method.SUGGESTION, "engine");
+                        final String extras = "engine." + (String) v.getTag();
+                        Telemetry.sendUIEvent(TelemetryContract.Event.LOAD_URL, TelemetryContract.Method.SUGGESTION, extras);
                     }
                     mSearchListener.onSearch(mSearchEngine, suggestion);
                 }
@@ -197,6 +198,9 @@ class SearchEngineRow extends AnimatedHeightLayout {
 
                 suggestionItem.setOnClickListener(mClickListener);
                 suggestionItem.setOnLongClickListener(mLongClickListener);
+
+                // Store the position of the suggestion for telemetry.
+                suggestionItem.setTag(String.valueOf(suggestionCounter));
 
                 final ImageView magnifier =
                         (ImageView) suggestionItem.findViewById(R.id.suggestion_magnifier);
