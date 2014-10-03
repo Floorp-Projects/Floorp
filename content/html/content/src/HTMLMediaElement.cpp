94dda74992123a7548470e79a6463da7cc0a4ae5
Randell Jesup <rjesup@jesup.org>
2014-10-03 16:20:10 -0400

    Bug 1077274: Clean up tracklists r=jib a=dveditz

--- content/html/content/src/HTMLMediaElement.cpp
+++ content/html/content/src/HTMLMediaElement.cpp
@@ -2836,6 +2836,8 @@ void HTMLMediaElement::SetupSrcMediaStreamPlayback(DOMMediaStream* aStream)
     GetSrcMediaStream()->AddVideoOutput(container);
   }
 
+  // Note: we must call DisconnectTrackListListeners(...)  before dropping
+  // mSrcStream
   mSrcStream->ConstructMediaTracks(AudioTracks(), VideoTracks());
 
   ChangeReadyState(nsIDOMHTMLMediaElement::HAVE_METADATA);
@@ -2854,6 +2856,8 @@ void HTMLMediaElement::EndSrcMediaStreamPlayback()
   if (stream) {
     stream->RemoveListener(mSrcStreamListener);
   }
+  mSrcStream->DisconnectTrackListListeners(AudioTracks(), VideoTracks());
+
   // Kill its reference to this element
   mSrcStreamListener->Forget();
   mSrcStreamListener = nullptr;
