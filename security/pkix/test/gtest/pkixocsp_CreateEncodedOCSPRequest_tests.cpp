8b975e4c9cdd075c02018cd4ab9f1082c9be026f
Brian Smith <brian@briansmith.org>
2014-10-03 15:52:38 -0700

    Bug 1077859: Make ENCODING_FAILED safe to use in static initializers, r=mmc

--- security/pkix/test/gtest/pkixocsp_CreateEncodedOCSPRequest_tests.cpp
+++ security/pkix/test/gtest/pkixocsp_CreateEncodedOCSPRequest_tests.cpp
@@ -87,7 +87,7 @@ protected:
                                   /*out*/ ByteString& issuerSPKI)
   {
     issuerDER = CNToDERName(issuerASCII);
-    ASSERT_NE(ENCODING_FAILED, issuerDER);
+    ASSERT_FALSE(ENCODING_FAILED(issuerDER));
 
     ScopedTestKeyPair keyPair(GenerateKeyPair());
     ASSERT_TRUE(keyPair);
