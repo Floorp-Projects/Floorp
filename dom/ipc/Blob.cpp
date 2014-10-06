5a0d84e7aadac1a8081d522306c13afa042eb1d7
Wes Kocher <wkocher@mozilla.com>
2014-10-06 14:49:07 -0700

    Backed out changeset abd8b7b6dbd1 (bug 1075302) for e10s bustage on a CLOSED TREE

--- dom/ipc/Blob.cpp
+++ dom/ipc/Blob.cpp
@@ -419,48 +419,15 @@ public:
   NS_IMETHOD
   Available(uint64_t* aAvailable) MOZ_OVERRIDE
   {
-    if (!IsOnOwningThread()) {
-      nsresult rv = BlockAndWaitForStream();
-      NS_ENSURE_SUCCESS(rv, rv);
-
-      rv = mStream->Available(aAvailable);
-      NS_ENSURE_SUCCESS(rv, rv);
-    }
-
-#ifdef DEBUG
-    if (NS_IsMainThread()) {
-      NS_WARNING("Someone is trying to do main-thread I/O...");
-    }
-#endif
-
-    nsresult rv;
-
-    // See if we already have our real stream.
-    nsCOMPtr<nsIInputStream> inputStream;
-    {
-      MonitorAutoLock lock(mMonitor);
-
-      inputStream = mStream;
-    }
-
-    // If we do then just call through.
-    if (inputStream) {
-      rv = inputStream->Available(aAvailable);
-      NS_ENSURE_SUCCESS(rv, rv);
-
-      return NS_OK;
-    }
-
-    // If the stream is already closed then we can't do anything.
-    if (!mBlobImpl) {
+    // See large comment in FileInputStreamWrapper::Available.
+    if (IsOnOwningThread()) {
       return NS_BASE_STREAM_CLOSED;
     }
 
-    // Otherwise fake it...
-    NS_WARNING("Available() called before real stream has been delivered, "
-               "guessing the amount of data available!");
+    nsresult rv = BlockAndWaitForStream();
+    NS_ENSURE_SUCCESS(rv, rv);
 
-    rv = mBlobImpl->GetSize(aAvailable);
+    rv = mStream->Available(aAvailable);
     NS_ENSURE_SUCCESS(rv, rv);
 
     return NS_OK;
