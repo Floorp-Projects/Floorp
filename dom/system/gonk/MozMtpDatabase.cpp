f6cbf356b613bd9d1e1470e83769a5234e36c137
eden-chuang <echuang@mozilla.com>
2014-10-08 00:12:00 +0200

    Bug 1074604 - Handle file "modified" event in MozMtpDatabase and MozMtpServer. r=dhylands, a=bajaj

--- dom/system/gonk/MozMtpDatabase.cpp
+++ dom/system/gonk/MozMtpDatabase.cpp
@@ -267,16 +267,17 @@ MozMtpDatabase::FileWatcherUpdate(RefCountedMtpServer* aMtpServer,
 
   MtpObjectHandle entryHandle = FindEntryByPath(filePath);
 
-  if (aEventType.EqualsLiteral("created")) {
+  if (aEventType.EqualsLiteral("modified")) {
+    // To update the file information to the newest, we remove the entry for
+    // the existing file, then re-add the entry for the file.
     if (entryHandle != 0) {
-      // The entry already exists. This means that we're being notified
-      // about a file added by MTP. So we can ignore it.
-
-      return;
+      MTP_LOG("About to call sendObjectRemoved Handle 0x%08x file %s", entryHandle, filePath.get());
+      aMtpServer->sendObjectRemoved(entryHandle);
+      RemoveEntry(entryHandle);
     }
     entryHandle = CreateEntryForFile(filePath, aFile);
     if (entryHandle == 0) {
-      // CreateEntryForFile didn't create a new entry. We can't tell MTP.
+      // creating entry for the file failed, don't tell MTP
       return;
     }
     MTP_LOG("About to call sendObjectAdded Handle 0x%08x file %s", entryHandle, filePath.get());
@@ -289,10 +290,8 @@ MozMtpDatabase::FileWatcherUpdate(RefCountedMtpServer* aMtpServer,
       // The entry has already been removed. We can't tell MTP.
       return;
     }
-
     MTP_LOG("About to call sendObjectRemoved Handle 0x%08x file %s", entryHandle, filePath.get());
     aMtpServer->sendObjectRemoved(entryHandle);
-
     RemoveEntry(entryHandle);
     return;
   }
@@ -616,11 +615,7 @@ MozMtpDatabase::endSendObject(const char* aPath,
   if (aSucceeded) {
     RefPtr<DbEntry> entry = GetEntry(aHandle);
     if (entry) {
-      if (mBeginSendObjectCalled) {
-        FileWatcherNotify(entry, "created");
-      } else {
-        FileWatcherNotify(entry, "modified");
-      }
+      FileWatcherNotify(entry, "modified");
     }
   } else {
     RemoveEntry(aHandle);
