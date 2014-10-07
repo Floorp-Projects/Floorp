40e93053200fe5192456eb73eb23102914ddf2cc
Tom Schuster <evilpies@gmail.com>
2014-10-07 16:05:11 +0200

    Bug 1065716 - Move jsonparser to vm/JSONParser. r=Waldo

--- js/src/gc/RootMarking.cpp
+++ js/src/gc/RootMarking.cpp
@@ -12,7 +12,6 @@
 
 #include "jscntxt.h"
 #include "jsgc.h"
-#include "jsonparser.h"
 #include "jsprf.h"
 #include "jstypes.h"
 #include "jswatchpoint.h"
@@ -25,6 +24,7 @@
 #include "jit/IonMacroAssembler.h"
 #include "js/HashTable.h"
 #include "vm/Debugger.h"
+#include "vm/JSONParser.h"
 #include "vm/PropDesc.h"
 
 #include "jsgcinlines.h"
