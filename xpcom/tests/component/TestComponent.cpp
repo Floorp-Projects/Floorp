0952d38528aeb44fc05ca7af282321055e1fd00e
Brian Marshall <bmarsd@gmail.com>
2014-10-08 16:08:28 -0700

    Bug 1079311 - Unlock mutex before logging error in RegisterContractIDLocked. r=bsmedberg

--- xpcom/tests/component/TestComponent.cpp
+++ xpcom/tests/component/TestComponent.cpp
@@ -8,8 +8,12 @@
 #define NS_TESTING_CID \
 { 0x335fb596, 0xe52d, 0x418f, \
   { 0xb0, 0x1c, 0x1b, 0xf1, 0x6c, 0xe5, 0xe7, 0xe4 } }
+#define NS_NONEXISTENT_CID \
+{ 0x1e61fb15, 0xead4, 0x45cd, \
+  { 0x80, 0x13, 0x40, 0x99, 0xa7, 0x10, 0xa2, 0xfa } }
 
 NS_DEFINE_NAMED_CID(NS_TESTING_CID);
+NS_DEFINE_NAMED_CID(NS_NONEXISTENT_CID);
 
 static nsresult
 DummyConstructorFunc(nsISupports* aOuter, const nsIID& aIID, void** aResult)
@@ -23,9 +27,15 @@ static const mozilla::Module::CIDEntry kTestCIDs[] = {
   { nullptr }
 };
 
+static const mozilla::Module::ContractIDEntry kTestContractIDs[] = {
+  { "@testing/foo", &kNS_NONEXISTENT_CID },
+  { nullptr }
+};
+
 static const mozilla::Module kTestModule = {
   mozilla::Module::kVersion,
-  kTestCIDs
+  kTestCIDs,
+  kTestContractIDs
 };
 
 NSMODULE_DEFN(dummy) = &kTestModule;
