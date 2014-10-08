69b4ea1074ab048b3bb59b523302043c333d0b8b
Nathan Froyd <froydnj@mozilla.com>
2014-10-08 16:05:36 -0400

    Bug 1080108 - don't redefine STATUS_FLOAT_* values in nsSigHandlers.cpp; r=bsmedberg
    Including <windows.h> picks these up, as recent builds indicate by warning:
    
    c:\builds\moz2_slave\m-in-w32-000000000000000000000\build\toolkit\xre\nsSigHandlers.cpp(341) : warning C4005: 'STATUS_FLOAT_MULTIPLE_FAULTS' : macro redefinition
            c:\Program Files (x86)\Windows Kits\8.0\include\um\winnt.h(2127) : see previous definition of 'STATUS_FLOAT_MULTIPLE_FAULTS'
    c:\builds\moz2_slave\m-in-w32-000000000000000000000\build\toolkit\xre\nsSigHandlers.cpp(342) : warning C4005: 'STATUS_FLOAT_MULTIPLE_TRAPS' : macro redefinition
            c:\Program Files (x86)\Windows Kits\8.0\include\um\winnt.h(2128) : see previous definition of 'STATUS_FLOAT_MULTIPLE_TRAPS'

--- toolkit/xre/nsSigHandlers.cpp
+++ toolkit/xre/nsSigHandlers.cpp
@@ -334,13 +334,6 @@ void InstallSignalHandlers(const char *ProgramName)
 #define X87SW(ctx) (ctx)->FloatSave.StatusWord
 #endif
 
-/*
- * SSE traps raise these exception codes, which are defined in internal NT headers
- * but not winbase.h
- */
-#define STATUS_FLOAT_MULTIPLE_FAULTS 0xC00002B4
-#define STATUS_FLOAT_MULTIPLE_TRAPS  0xC00002B5
-
 static LPTOP_LEVEL_EXCEPTION_FILTER gFPEPreviousFilter;
 
 LONG __stdcall FpeHandler(PEXCEPTION_POINTERS pe)
