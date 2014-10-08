846b797e54b6d457849001facb55d6dd2eb8417d
Jonathan Kew <jkew@mozilla.com>
2014-10-08 10:32:50 +0100

    Bug 1079314 - Remove the mWritingMode field from logical-coordinate objects in non-debug builds. r=smontagu

--- layout/tables/nsTableOuterFrame.cpp
+++ layout/tables/nsTableOuterFrame.cpp
@@ -512,8 +512,7 @@ nsTableOuterFrame::ComputeAutoSize(nsRenderingContext *aRenderingContext,
                                    bool aShrinkWrap)
 {
   nscoord kidAvailableWidth = aAvailableISize - aMargin.ISize(aWM);
-  NS_ASSERTION(aBorder == LogicalSize(aWM) &&
-               aPadding == LogicalSize(aWM),
+  NS_ASSERTION(aBorder.IsAllZero() && aPadding.IsAllZero(),
                "Table outer frames cannot have borders or paddings");
 
   // When we're shrink-wrapping, our auto size needs to wrap around the
