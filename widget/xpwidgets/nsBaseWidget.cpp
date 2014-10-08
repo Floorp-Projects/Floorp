3ce18c610831e0db25d252f302036d94981d9b00
Nicolas Silva <nsilva@mozilla.com>
2014-10-09 00:36:00 +0200

    Bug 1064107 - Ensure that gfxPlatform is initialized by the time we create the compositor. r=Bas, a=sledru

--- widget/xpwidgets/nsBaseWidget.cpp
+++ widget/xpwidgets/nsBaseWidget.cpp
@@ -888,6 +888,9 @@ CheckForBasicBackends(nsTArray<LayersBackend>& aHints)
 
 void nsBaseWidget::CreateCompositor(int aWidth, int aHeight)
 {
+  // This makes sure that gfxPlatforms gets initialized if it hasn't by now.
+  gfxPlatform::GetPlatform();
+
   MOZ_ASSERT(gfxPlatform::UsesOffMainThreadCompositing(),
              "This function assumes OMTC");
 
