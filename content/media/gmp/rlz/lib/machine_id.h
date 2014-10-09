9929adee97b8844dc4e24440c919bf4e93e2a893
Chris Pearce <cpearce@mozilla.com>
2014-10-09 17:01:03 +1300

    Bug 1060179 - Import librlz for device identity generation on Windows. r=glandium,dmajor

---
+++ content/media/gmp/rlz/lib/machine_id.h
@@ -0,0 +1,21 @@
+// Copyright 2012 Google Inc. All Rights Reserved.
+// Use of this source code is governed by an Apache-style license that can be
+// found in the COPYING file.
+
+#ifndef RLZ_LIB_MACHINE_ID_H_
+#define RLZ_LIB_MACHINE_ID_H_
+
+#include "base/string16.h"
+
+#include <string>
+
+namespace rlz_lib {
+
+// Retrieves a raw machine identifier string and a machine-specific
+// 4 byte value. GetMachineId() will SHA1 |data|, append |more_data|, compute
+// the Crc8 of that, and return a hex-encoded string of that data.
+bool GetRawMachineId(string16* data, int* more_data);
+
+}  // namespace rlz_lib
+
+#endif  // RLZ_LIB_MACHINE_ID_H_
