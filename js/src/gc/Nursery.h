9d503deed8af183680f0551f5f268e28b6874759
Terrence Cole <terrence@mozilla.com>
2014-10-08 17:46:33 -0700

    Bug 1074961 - Part 4: move refillFreeList into GCRuntime; r=sfink

--- js/src/gc/Nursery.h
+++ js/src/gc/Nursery.h
@@ -267,7 +267,7 @@ class Nursery
     HeapSlot *allocateHugeSlots(JS::Zone *zone, size_t nslots);
 
     /* Allocates a new GC thing from the tenured generation during minor GC. */
-    void *allocateFromTenured(JS::Zone *zone, gc::AllocKind thingKind);
+    gc::TenuredCell *allocateFromTenured(JS::Zone *zone, gc::AllocKind thingKind);
 
     struct TenureCountCache;
 
