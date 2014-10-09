ee42c7877023d484b82e3ac7693bb39e788d8aab
wesj <wjohnston@mozilla.com>
2014-10-08 21:55:00 -0700

    Bug 1070488 - Add files downloaded in private browsing to the Android Download Manager. r=paolo, a=lsblakk

--- toolkit/components/jsdownloads/src/DownloadPlatform.cpp
+++ toolkit/components/jsdownloads/src/DownloadPlatform.cpp
@@ -77,6 +77,11 @@ nsresult DownloadPlatform::DownloadDone(nsIURI* aSource, nsIFile* aTarget,
     // list, with a pref to disable.
     {
       bool addToRecentDocs = Preferences::GetBool(PREF_BDM_ADDTORECENTDOCS);
+#ifdef MOZ_WIDGET_ANDROID
+      if (addToRecentDocs) {
+        mozilla::widget::android::DownloadsIntegration::ScanMedia(path, NS_ConvertUTF8toUTF16(aContentType));
+      }
+#else
       if (addToRecentDocs && !aIsPrivate) {
 #ifdef XP_WIN
         ::SHAddToRecentDocs(SHARD_PATHW, path.get());
@@ -89,10 +94,9 @@ nsresult DownloadPlatform::DownloadDone(nsIURI* aSource, nsIFile* aTarget,
           gtk_recent_manager_add_item(manager, uri);
           g_free(uri);
         }
-#elif MOZ_WIDGET_ANDROID
-        mozilla::widget::android::DownloadsIntegration::ScanMedia(path, NS_ConvertUTF8toUTF16(aContentType));
 #endif
       }
+#endif
 #ifdef MOZ_ENABLE_GIO
       // Use GIO to store the source URI for later display in the file manager.
       GFile* gio_file = g_file_new_for_path(NS_ConvertUTF16toUTF8(path).get());
@@ -110,6 +114,7 @@ nsresult DownloadPlatform::DownloadDone(nsIURI* aSource, nsIFile* aTarget,
 #endif
     }
 #endif
+
 #ifdef XP_MACOSX
     // On OS X, make the downloads stack bounce.
     CFStringRef observedObject = ::CFStringCreateWithCString(kCFAllocatorDefault,
