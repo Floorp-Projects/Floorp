5e19affa544b708d72c6dbf4a74506494893595a
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-10-09 12:08:09 +0200

    Backed out changeset 46c1cce018de (bug 1059163) for frequent test failures of the new test

--- dom/inputmethod/forms.js
+++ dom/inputmethod/forms.js
@@ -224,8 +224,7 @@ let FormAssistant = {
   scrollIntoViewTimeout: null,
   _focusedElement: null,
   _focusCounter: 0, // up one for every time we focus a new element
-  _focusDeleteObserver: null,
-  _focusContentObserver: null,
+  _observer: null,
   _documentEncoder: null,
   _editor: null,
   _editing: false,
@@ -251,13 +250,9 @@ let FormAssistant = {
 
     if (this.focusedElement) {
       this.focusedElement.removeEventListener('compositionend', this);
-      if (this._focusDeleteObserver) {
-        this._focusDeleteObserver.disconnect();
-        this._focusDeleteObserver = null;
-      }
-      if (this._focusContentObserver) {
-        this._focusContentObserver.disconnect();
-        this._focusContentObserver = null;
+      if (this._observer) {
+        this._observer.disconnect();
+        this._observer = null;
       }
       if (this._selectionPrivate) {
         this._selectionPrivate.removeSelectionListener(this);
@@ -297,7 +292,7 @@ let FormAssistant = {
 
       // If our focusedElement is removed from DOM we want to handle it properly
       let MutationObserver = element.ownerDocument.defaultView.MutationObserver;
-      this._focusDeleteObserver = new MutationObserver(function(mutations) {
+      this._observer = new MutationObserver(function(mutations) {
         var del = [].some.call(mutations, function(m) {
           return [].some.call(m.removedNodes, function(n) {
             return n.contains(element);
@@ -310,23 +305,10 @@ let FormAssistant = {
         }
       });
 
-      this._focusDeleteObserver.observe(element.ownerDocument.body, {
+      this._observer.observe(element.ownerDocument.body, {
         childList: true,
         subtree: true
       });
-
-      // If contenteditable, also add a mutation observer on its content and
-      // call selectionChanged when a change occurs
-      if (isContentEditable(element)) {
-        this._focusContentObserver = new MutationObserver(function() {
-          this.updateSelection();
-        }.bind(this));
-
-        this._focusContentObserver.observe(element, {
-          childList: true,
-          subtree: true
-        });
-      }
     }
 
     this.focusedElement = element;
