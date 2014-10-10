79ded525d536f595ceda7e4b847857bdb0a64dec
Ryan VanderMeulen <ryanvm@gmail.com>
2014-10-10 17:14:20 -0400

    Backed out changesets 59c761f6c0eb and 132f861349a2 (bug 1075531) for robocop bustage.

--- mobile/android/base/tests/testAboutPage.java
+++ mobile/android/base/tests/testAboutPage.java
@@ -2,41 +2,34 @@ package org.mozilla.gecko.tests;
 
 import org.mozilla.gecko.Actions;
 import org.mozilla.gecko.Element;
-import org.mozilla.gecko.NewTabletUI;
 import org.mozilla.gecko.R;
-
-import android.app.Activity;
 
 /* Tests related to the about: page:
  *  - check that about: loads from the URL bar
  *  - check that about: loads from Settings/About...
  */
 public class testAboutPage extends PixelTest {
-    private void ensureTitleMatches(final String titleRegex, final String urlRegex) {
-        final Activity activity = getActivity();
-        final Element urlBarTitle = mDriver.findElement(activity, R.id.url_bar_title);
-
-        // TODO: We should also be testing what the page title preference value is.
-        final String expectedTitle = NewTabletUI.isEnabled(activity) ? urlRegex : titleRegex;
+    private void ensureTitleMatches(final String regex) {
+        Element urlBarTitle = mDriver.findElement(getActivity(), R.id.url_bar_title);
         mAsserter.isnot(urlBarTitle, null, "Got the URL bar title");
-        assertMatches(urlBarTitle.getText(), expectedTitle, "page title match");
+        assertMatches(urlBarTitle.getText(), regex, "page title match");
     }
 
     public void testAboutPage() {
         blockForGeckoReady();
 
         // Load the about: page and verify its title.
-        String url = StringHelper.ABOUT_SCHEME;
+        String url = "about:";
         loadAndPaint(url);
 
-        verifyPageTitle(StringHelper.ABOUT_LABEL, url);
+        ensureTitleMatches(StringHelper.ABOUT_LABEL);
 
         // Open a new page to remove the about: page from the current tab.
         url = getAbsoluteUrl(StringHelper.ROBOCOP_BLANK_PAGE_01_URL);
         inputAndLoadUrl(url);
 
         // At this point the page title should have been set.
-        verifyPageTitle(StringHelper.ROBOCOP_BLANK_PAGE_01_TITLE, url);
+        ensureTitleMatches(StringHelper.ROBOCOP_BLANK_PAGE_01_TITLE);
 
         // Set up listeners to catch the page load we're about to do.
         Actions.EventExpecter tabEventExpecter = mActions.expectGeckoEvent("Tab:Added");
@@ -52,6 +45,6 @@ public class testAboutPage extends PixelTest {
         contentEventExpecter.unregisterListener();
 
         // Grab the title to make sure the about: page was loaded.
-        verifyPageTitle(StringHelper.ABOUT_LABEL, StringHelper.ABOUT_SCHEME);
+        ensureTitleMatches(StringHelper.ABOUT_LABEL);
     }
 }
