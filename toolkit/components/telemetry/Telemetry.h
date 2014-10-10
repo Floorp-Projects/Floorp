c460713e4d50da0d07db36ab97a9cd4a99a45d65
Ryan VanderMeulen <ryanvm@gmail.com>
2014-10-10 16:24:54 -0400

    Backed out changesets fb6af789bc67 and 313123f12e95 (bug 818307) for non-unified bustage.

--- toolkit/components/telemetry/Telemetry.h
+++ toolkit/components/telemetry/Telemetry.h
@@ -17,9 +17,6 @@ namespace base {
 }
 
 namespace mozilla {
-namespace HangMonitor {
-  class HangAnnotations;
-}
 namespace Telemetry {
 
 #include "TelemetryHistogramEnums.h"
@@ -198,14 +195,12 @@ class ProcessedStack;
  * @param aStack - Array of PCs from the hung call stack
  * @param aSystemUptime - System uptime at the time of the hang, in minutes
  * @param aFirefoxUptime - Firefox uptime at the time of the hang, in minutes
- * @param aAnnotations - Any annotations to be added to the report
  */
 #if defined(MOZ_ENABLE_PROFILER_SPS)
 void RecordChromeHang(uint32_t aDuration,
                       ProcessedStack &aStack,
                       int32_t aSystemUptime,
-                      int32_t aFirefoxUptime,
-                      mozilla::HangMonitor::HangAnnotations* aAnnotations = nullptr);
+                      int32_t aFirefoxUptime);
 #endif
 
 class ThreadHangStats;
