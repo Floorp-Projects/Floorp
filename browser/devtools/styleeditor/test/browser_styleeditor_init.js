347bd21002cffcf7ae5395d58027e150fa22d2db
David Rajchenbach-Teller <dteller@mozilla.com>
2014-10-11 21:54:23 +0200

    Bug 1081549 - Wallpaper patch;r=yoric

--- browser/devtools/styleeditor/test/browser_styleeditor_init.js
+++ browser/devtools/styleeditor/test/browser_styleeditor_init.js
@@ -2,6 +2,13 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+///////////////////
+//
+// Whitelisting this test.
+// As part of bug 1077403, the leaking uncaught rejection should be fixed.
+//
+thisTestLeaksUncaughtRejectionsAndShouldBeFixed("TypeError: summary is undefined");
+
 const TESTCASE_URI = TEST_BASE + "simple.html";
 
 let gUI;
