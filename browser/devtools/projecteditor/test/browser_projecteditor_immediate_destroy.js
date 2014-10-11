67d2fb060fcca64bd4a60b31b0b17f8b7ebb8136
David Rajchenbach-Teller <dteller@mozilla.com>
2014-10-11 21:52:44 +0200

    Bug 1081548 - Wallpaper patch;r=yoric

--- browser/devtools/projecteditor/test/browser_projecteditor_immediate_destroy.js
+++ browser/devtools/projecteditor/test/browser_projecteditor_immediate_destroy.js
@@ -10,6 +10,7 @@
 // As part of bug 1077403, the leaking uncaught rejection should be fixed.
 //
 thisTestLeaksUncaughtRejectionsAndShouldBeFixed("destroy");
+thisTestLeaksUncaughtRejectionsAndShouldBeFixed("TypeError: this.window is null");
 
 // Test that projecteditor can be destroyed in various states of loading
 // without causing any leaks or exceptions.
