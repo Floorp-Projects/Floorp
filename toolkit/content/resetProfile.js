4f82da56cef5faa32715bae91c5d8b40d92467ef
Mark Hammond <mhammond@skippinet.com.au>
2014-10-11 12:37:16 +1100

    Bug 1063702 - Update all Reset Firefox strings and relevant UI to new UX requirements. r=MattN

--- toolkit/content/resetProfile.js
+++ toolkit/content/resetProfile.js
@@ -14,25 +14,6 @@ if (!("Cu" in window)) {
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/ResetProfile.jsm");
 
-// based on onImportItemsPageShow from migration.js
-function populateResetPane(aContainerID) {
-  let resetProfileItems = document.getElementById(aContainerID);
-  try {
-    let dataTypes = ResetProfile.getMigratedData();
-    for (let dataType of dataTypes) {
-      let label = document.createElement("label");
-      label.setAttribute("value", dataType);
-      resetProfileItems.appendChild(label);
-    }
-  } catch (ex) {
-    Cu.reportError(ex);
-  }
-}
-
-function onResetProfileLoad() {
-  populateResetPane("migratedItems");
-}
-
 function onResetProfileAccepted() {
   let retVals = window.arguments[0];
   retVals.reset = true;
