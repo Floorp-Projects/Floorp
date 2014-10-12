9675e029e51edcac225ec47d96976bf3338640c8
Dave Camp <dcamp@mozilla.com>
2014-10-12 09:18:00 +0300

    Bug 1081689 - Allow unregistration of a protocol.js type. r=paul, a=lsblakk

---
+++ toolkit/devtools/server/tests/unit/test_protocol_unregister.js
@@ -0,0 +1,44 @@
+const {types} = devtools.require("devtools/server/protocol");
+
+
+function run_test()
+{
+  types.addType("test", {
+    read: (v) => { return "successful read: " + v },
+    write: (v) => { return "successful write: " + v }
+  });
+
+  // Verify the type registered correctly.
+
+  let type = types.getType("test");
+  let arrayType = types.getType("array:test");
+  do_check_eq(type.read("foo"), "successful read: foo");
+  do_check_eq(arrayType.read(["foo"])[0], "successful read: foo");
+
+  types.removeType("test");
+
+  do_check_eq(type.name, "DEFUNCT:test");
+  try {
+    types.getType("test");
+    do_check_true(false, "getType should fail");
+  } catch(ex) {
+    do_check_eq(ex.toString(), "Error: Unknown type: test");
+  }
+
+  try {
+    type.read("foo");
+    do_check_true(false, "type.read should have thrown an exception.");
+  } catch(ex) {
+    do_check_eq(ex.toString(), "Error: Using defunct type: test");
+  }
+
+  try {
+    arrayType.read(["foo"]);
+    do_check_true(false, "array:test.read should have thrown an exception.");
+  } catch(ex) {
+    do_check_eq(ex.toString(), "Error: Using defunct type: test");
+  }
+
+}
+
+
