c05fd7f9da70e305a3e394f92765ba76fe0fba65
Chris Kitching <chriskitching@linux.com>
2014-10-12 02:52:38 +0100

    Bug 1081606: Don't call toArray with zero-size arrays. r=rnewman * * * Bug 1081606: Don't call toArray with zero-size arrays. r=rnewman

--- mobile/android/base/sync/repositories/android/FormHistoryRepositorySession.java
+++ mobile/android/base/sync/repositories/android/FormHistoryRepositorySession.java
@@ -173,7 +173,7 @@ public class FormHistoryRepositorySession extends
           }
         }
 
-        String guidsArray[] = guids.toArray(new String[0]);
+        String guidsArray[] = guids.toArray(new String[guids.size()]);
         delegate.onGuidsSinceSucceeded(guidsArray);
       }
     };
@@ -466,7 +466,7 @@ public class FormHistoryRepositorySession extends
   protected void flushInsertQueue() throws RemoteException {
     synchronized (recordsBufferMonitor) {
       if (recordsBuffer.size() > 0) {
-        final ContentValues[] outgoing = recordsBuffer.toArray(new ContentValues[0]);
+        final ContentValues[] outgoing = recordsBuffer.toArray(new ContentValues[recordsBuffer.size()]);
         recordsBuffer = new ArrayList<ContentValues>();
 
         if (outgoing == null || outgoing.length == 0) {
