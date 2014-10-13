79d8191869417672c55b50dd17cdccada6a5f60d
Steve Fink <sfink@mozilla.com>
2014-10-13 13:42:59 -0700

    Bug 1082141 - Typed objects are not ArrayBufferViews (yet), r=Waldo
    --HG--
    extra : rebase_source : 8538d9115313b9d690dfeac3a9042b483d778ac5

--- js/src/vm/ArrayBufferObject.cpp
+++ js/src/vm/ArrayBufferObject.cpp
@@ -985,7 +985,7 @@ template <>
 bool
 JSObject::is<js::ArrayBufferViewObject>() const
 {
-    return is<DataViewObject>() || is<TypedArrayObject>() || is<TypedObject>();
+    return is<DataViewObject>() || is<TypedArrayObject>();
 }
 
 void
