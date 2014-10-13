b77a12e5935beff0ca6d6f0b3394465f7bf15650
Gareth Aye <gaye@mozilla.com>
2014-10-12 21:37:27 -0700

    Bug 1067270 - Disable http2 r=fabrice a=bajaj

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -81,6 +81,7 @@ pref("network.http.max-persistent-connections-per-proxy", 20);
 pref("network.cookie.cookieBehavior", 0);
 
 // spdy
+pref("network.http.spdy.enabled.http2draft", false);
 pref("network.http.spdy.push-allowance", 32768);
 
 // See bug 545869 for details on why these are set the way they are
