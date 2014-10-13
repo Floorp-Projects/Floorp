0fb040851a08a23c1c1b2c606136e10c93ae688b
David Burns <dburns@mozilla.com>
2014-10-13 20:40:34 +0100

    Bug 1073539: Add the ability to set capabilities into Marionette; r=jgriffin

--- testing/marionette/client/marionette/tests/unit/test_capabilities.py
+++ testing/marionette/client/marionette/tests/unit/test_capabilities.py
@@ -52,3 +52,10 @@ class TestCapabilities(MarionetteTestCase):
         self.assertEqual(self.caps["XULappId"], self.appinfo["ID"])
         self.assertEqual(self.caps["appBuildId"], self.appinfo["appBuildID"])
         self.assertEqual(self.caps["version"], self.appinfo["version"])
+
+    def test_we_can_pass_in_capabilities_on_session_start(self):
+        self.marionette.delete_session()
+        capabilities = {"somethingAwesome": "cake"}
+        self.marionette.start_session(capabilities)
+        caps = self.marionette.session_capabilities
+        self.assertIn("somethingAwesome", caps)
