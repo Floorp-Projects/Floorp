99207baff7c02364405fe1865f113fb8e15c08dc
Edwin Flores <eflores@mozilla.com>
2014-10-14 11:05:00 +1300

    Bug 1059049 - Pass CDMProxy from MediaSourceDecoder to subreaders - r=cpearce,kinetik

--- content/media/MediaDecoderReader.h
+++ content/media/MediaDecoderReader.h
@@ -39,6 +39,9 @@ public:
 
   // True if this reader is waiting media resource allocation
   virtual bool IsWaitingMediaResources() { return false; }
+  // True if this reader is waiting for a Content Decryption Module to become
+  // available.
+  virtual bool IsWaitingOnCDMResource() { return false; }
   // True when this reader need to become dormant state
   virtual bool IsDormantNeeded() { return false; }
   // Release media resources they should be released in dormant state
