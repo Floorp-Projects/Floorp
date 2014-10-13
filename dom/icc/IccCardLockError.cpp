82fb6c03173a7d9ab803fa3968a599e11540b448
Edgar Chen <echen@mozilla.com>
2014-10-13 12:35:53 +0800

    Bug 1081810 - Part 1: Implement IccCardLockError in C++. r=smaug

---
+++ dom/icc/IccCardLockError.cpp
@@ -0,0 +1,48 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/IccCardLockError.h"
+#include "mozilla/dom/IccCardLockErrorBinding.h"
+
+namespace mozilla {
+namespace dom {
+
+NS_IMPL_ISUPPORTS_INHERITED0(IccCardLockError, DOMError)
+
+/* static */ already_AddRefed<IccCardLockError>
+IccCardLockError::Constructor(const GlobalObject& aGlobal,
+                              const nsAString& aLockType,
+                              const nsAString& aName,
+                              int16_t aRetryCount,
+                              ErrorResult& aRv)
+{
+  nsCOMPtr<nsPIDOMWindow> window = do_QueryInterface(aGlobal.GetAsSupports());
+  if (!window) {
+    aRv.Throw(NS_ERROR_FAILURE);
+    return nullptr;
+  }
+
+  nsRefPtr<IccCardLockError> result =
+    new IccCardLockError(window, aName, aLockType, aRetryCount);
+  return result.forget();
+}
+
+IccCardLockError::IccCardLockError(nsPIDOMWindow* aWindow,
+                                   const nsAString& aName,
+                                   const nsAString& aLockType,
+                                   int16_t aRetryCount)
+  : DOMError(aWindow, aName)
+  , mLockType(aLockType)
+  , mRetryCount(aRetryCount)
+{
+}
+
+JSObject*
+IccCardLockError::WrapObject(JSContext* aCx)
+{
+  return IccCardLockErrorBinding::Wrap(aCx, this);
+}
+
+} // namespace dom
+} // namespace mozilla
---
+++ dom/icc/IccCardLockError.h
@@ -0,0 +1,54 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_IccCardLockError_h
+#define mozilla_dom_IccCardLockError_h
+
+#include "mozilla/dom/DOMError.h"
+
+namespace mozilla {
+namespace dom {
+
+class IccCardLockError MOZ_FINAL : public DOMError
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+
+  IccCardLockError(nsPIDOMWindow* aWindow, const nsAString& aName,
+                   const nsAString& aLockType, int16_t aRetryCount);
+
+  static already_AddRefed<IccCardLockError>
+  Constructor(const GlobalObject& aGlobal, const nsAString& aLockType,
+              const nsAString& aName, int16_t aRetryCount,
+              ErrorResult& aRv);
+
+  virtual JSObject*
+  WrapObject(JSContext* aCx) MOZ_OVERRIDE;
+
+  // WebIDL interface
+
+  void
+  GetLockType(nsString& aLockType) const
+  {
+    aLockType = mLockType;
+  }
+
+  int16_t
+  RetryCount() const
+  {
+    return mRetryCount;
+  }
+
+private:
+  ~IccCardLockError() {}
+
+private:
+  nsString mLockType;
+  int16_t mRetryCount;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_IccCardLockError_h
