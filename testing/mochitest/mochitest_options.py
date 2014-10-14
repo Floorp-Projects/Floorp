59a2276bcb44f6e3c938c2d83fdb5adc84726c32
Andrew McCreight <continuation@gmail.com>
2014-10-14 13:04:00 +0200

    Bug 1082788 - Increase GMP process threshold a little. r=jesup

--- testing/mochitest/mochitest_options.py
+++ testing/mochitest/mochitest_options.py
@@ -613,6 +613,7 @@ class MochitestOptions(optparse.OptionParser):
         options.leakThresholds = {
             "default": options.defaultLeakThreshold,
             "tab": 10000, # See dependencies of bug 1051230.
+            "geckomediaplugin": 1000, # GMP rarely gets a log, but when it does, it leaks a little.
         }
 
         # Bug 1051230 - Leak logging does not yet work for tab processes on desktop.
