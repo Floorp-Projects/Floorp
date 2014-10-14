1ba2333d39db026c94fd270e6ef57074504cef65
Ryan VanderMeulen <ryanvm@gmail.com>
2014-10-14 12:41:43 -0400

    Backed out changeset f156971f0f4d (bug 1074832) for Android crashes. CLOSED TREE

--- netwerk/cache2/CacheIndex.h
+++ netwerk/cache2/CacheIndex.h
@@ -166,32 +166,32 @@ public:
     }
   }
 
-  const SHA1Sum::Hash * Hash() const { return &mRec->mHash; }
+  const SHA1Sum::Hash * Hash() { return &mRec->mHash; }
 
-  bool IsInitialized() const { return !!(mRec->mFlags & kInitializedMask); }
+  bool IsInitialized() { return !!(mRec->mFlags & kInitializedMask); }
 
-  uint32_t AppId() const { return mRec->mAppId; }
-  bool     Anonymous() const { return !!(mRec->mFlags & kAnonymousMask); }
-  bool     InBrowser() const { return !!(mRec->mFlags & kInBrowserMask); }
+  uint32_t AppId() { return mRec->mAppId; }
+  bool     Anonymous() { return !!(mRec->mFlags & kAnonymousMask); }
+  bool     InBrowser() { return !!(mRec->mFlags & kInBrowserMask); }
 
-  bool IsRemoved() const { return !!(mRec->mFlags & kRemovedMask); }
+  bool IsRemoved() { return !!(mRec->mFlags & kRemovedMask); }
   void MarkRemoved() { mRec->mFlags |= kRemovedMask; }
 
-  bool IsDirty() const { return !!(mRec->mFlags & kDirtyMask); }
+  bool IsDirty() { return !!(mRec->mFlags & kDirtyMask); }
   void MarkDirty() { mRec->mFlags |= kDirtyMask; }
   void ClearDirty() { mRec->mFlags &= ~kDirtyMask; }
 
-  bool IsFresh() const { return !!(mRec->mFlags & kFreshMask); }
+  bool IsFresh() { return !!(mRec->mFlags & kFreshMask); }
   void MarkFresh() { mRec->mFlags |= kFreshMask; }
 
   void     SetFrecency(uint32_t aFrecency) { mRec->mFrecency = aFrecency; }
-  uint32_t GetFrecency() const { return mRec->mFrecency; }
+  uint32_t GetFrecency() { return mRec->mFrecency; }
 
   void     SetExpirationTime(uint32_t aExpirationTime)
   {
     mRec->mExpirationTime = aExpirationTime;
   }
-  uint32_t GetExpirationTime() const { return mRec->mExpirationTime; }
+  uint32_t GetExpirationTime() { return mRec->mExpirationTime; }
 
   // Sets filesize in kilobytes.
   void     SetFileSize(uint32_t aFileSize)
@@ -205,12 +205,12 @@ public:
     mRec->mFlags |= aFileSize;
   }
   // Returns filesize in kilobytes.
-  uint32_t GetFileSize() const { return GetFileSize(mRec); }
+  uint32_t GetFileSize() { return GetFileSize(mRec); }
   static uint32_t GetFileSize(CacheIndexRecord *aRec)
   {
     return aRec->mFlags & kFileSizeMask;
   }
-  bool     IsFileEmpty() const { return GetFileSize() == 0; }
+  bool     IsFileEmpty() { return GetFileSize() == 0; }
 
   void WriteToBuf(void *aBuf)
   {
@@ -246,7 +246,7 @@ public:
     mRec->mFlags = NetworkEndian::readUint32(&src->mFlags);
   }
 
-  void Log() const {
+  void Log() {
     LOG(("CacheIndexEntry::Log() [this=%p, hash=%08x%08x%08x%08x%08x, fresh=%u,"
          " initialized=%u, removed=%u, dirty=%u, anonymous=%u, inBrowser=%u, "
          "appId=%u, frecency=%u, expirationTime=%u, size=%u]",
@@ -280,7 +280,6 @@ public:
   }
 
 private:
-  friend class CacheIndexEntryUpdate;
   friend class CacheIndex;
   friend class CacheIndexEntryAutoManage;
 
@@ -307,82 +306,6 @@ private:
   static const uint32_t kFileSizeMask    = 0x00FFFFFF;
 
   nsAutoPtr<CacheIndexRecord> mRec;
-};
-
-class CacheIndexEntryUpdate : public CacheIndexEntry
-{
-public:
-  explicit CacheIndexEntryUpdate(CacheIndexEntry::KeyTypePointer aKey)
-    : CacheIndexEntry(aKey)
-    , mUpdateFlags(0)
-  {
-    MOZ_COUNT_CTOR(CacheIndexEntryUpdate);
-    LOG(("CacheIndexEntryUpdate::CacheIndexEntryUpdate()"));
-  }
-  ~CacheIndexEntryUpdate()
-  {
-    MOZ_COUNT_DTOR(CacheIndexEntryUpdate);
-    LOG(("CacheIndexEntryUpdate::~CacheIndexEntryUpdate()"));
-  }
-
-  CacheIndexEntryUpdate& operator=(const CacheIndexEntry& aOther)
-  {
-    MOZ_ASSERT(memcmp(&mRec->mHash, &aOther.mRec->mHash,
-               sizeof(SHA1Sum::Hash)) == 0);
-    mUpdateFlags = 0;
-    *(static_cast<CacheIndexEntry *>(this)) = aOther;
-    return *this;
-  }
-
-  void InitNew()
-  {
-    mUpdateFlags = 0;
-    CacheIndexEntry::InitNew();
-  }
-
-  void SetFrecency(uint32_t aFrecency)
-  {
-    mUpdateFlags |= kFrecencyUpdatedMask;
-    CacheIndexEntry::SetFrecency(aFrecency);
-  }
-
-  void SetExpirationTime(uint32_t aExpirationTime)
-  {
-    mUpdateFlags |= kExpirationUpdatedMask;
-    CacheIndexEntry::SetExpirationTime(aExpirationTime);
-  }
-
-  void SetFileSize(uint32_t aFileSize)
-  {
-    mUpdateFlags |= kFileSizeUpdatedMask;
-    CacheIndexEntry::SetFileSize(aFileSize);
-  }
-
-  void ApplyUpdate(CacheIndexEntry *aDst) {
-    MOZ_ASSERT(memcmp(&mRec->mHash, &aDst->mRec->mHash,
-               sizeof(SHA1Sum::Hash)) == 0);
-    if (mUpdateFlags & kFrecencyUpdatedMask) {
-      aDst->mRec->mFrecency = mRec->mFrecency;
-    }
-    if (mUpdateFlags & kExpirationUpdatedMask) {
-      aDst->mRec->mExpirationTime = mRec->mExpirationTime;
-    }
-    aDst->mRec->mAppId = mRec->mAppId;
-    if (mUpdateFlags & kFileSizeUpdatedMask) {
-      aDst->mRec->mFlags = mRec->mFlags;
-    } else {
-      // Copy all flags except file size.
-      aDst->mRec->mFlags &= kFileSizeMask;
-      aDst->mRec->mFlags |= (mRec->mFlags & ~kFileSizeMask);
-    }
-  }
-
-private:
-  static const uint32_t kFrecencyUpdatedMask = 0x00000001;
-  static const uint32_t kExpirationUpdatedMask = 0x00000002;
-  static const uint32_t kFileSizeUpdatedMask = 0x00000004;
-
-  uint32_t mUpdateFlags;
 };
 
 class CacheIndexStats
@@ -474,7 +397,7 @@ public:
     return mSize;
   }
 
-  void BeforeChange(const CacheIndexEntry *aEntry) {
+  void BeforeChange(CacheIndexEntry *aEntry) {
 #ifdef DEBUG_STATS
     if (!mDisableLogging) {
       LOG(("CacheIndexStats::BeforeChange()"));
@@ -518,7 +441,7 @@ public:
     }
   }
 
-  void AfterChange(const CacheIndexEntry *aEntry) {
+  void AfterChange(CacheIndexEntry *aEntry) {
     MOZ_ASSERT(mStateLogged, "CacheIndexStats::AfterChange() - state not "
                "logged!");
 #ifdef DEBUG
@@ -719,7 +642,7 @@ private:
 
   // Merge all pending operations from mPendingUpdates into mIndex.
   void ProcessPendingOperations();
-  static PLDHashOperator UpdateEntryInIndex(CacheIndexEntryUpdate *aEntry,
+  static PLDHashOperator UpdateEntryInIndex(CacheIndexEntry *aEntry,
                                             void* aClosure);
 
   // Following methods perform writing of the index file.
@@ -1006,7 +929,7 @@ private:
 
   // We cannot add, remove or change any entry in mIndex in states READING and
   // WRITING. We track all changes in mPendingUpdates during these states.
-  nsTHashtable<CacheIndexEntryUpdate> mPendingUpdates;
+  nsTHashtable<CacheIndexEntry> mPendingUpdates;
 
   // Contains information statistics for mIndex + mPendingUpdates.
   CacheIndexStats               mIndexStats;
