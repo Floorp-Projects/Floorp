d7709acff167379082c0e47ed43cf1295edbcbcf
Mike de Boer <mdeboer@mozilla.com>
2014-10-15 14:04:02 +0200

    Bug 1081130 - Fix importing contacts with only a phone number and fetch the correct format. r=abr, a=sledru

--- browser/components/loop/test/mochitest/browser_GoogleImporter.js
+++ browser/components/loop/test/mochitest/browser_GoogleImporter.js
@@ -23,13 +23,13 @@ add_task(function* test_GoogleImport() {
   stats = yield promiseImport();
 
   // Assert the world.
-  Assert.equal(stats.total, 5, "Five contacts should get processed");
-  Assert.equal(stats.success, 5, "Five contacts should be imported");
+  Assert.equal(stats.total, 6, "Five contacts should get processed");
+  Assert.equal(stats.success, 6, "Five contacts should be imported");
 
   yield promiseImport();
-  Assert.equal(Object.keys(mockDb._store).length, 5, "Database should contain only five contact after reimport");
+  Assert.equal(Object.keys(mockDb._store).length, 6, "Database should contain only five contact after reimport");
 
-  let c = mockDb._store[mockDb._next_guid - 5];
+  let c = mockDb._store[mockDb._next_guid - 6];
   Assert.equal(c.name[0], "John Smith", "Full name should match");
   Assert.equal(c.givenName[0], "John", "Given name should match");
   Assert.equal(c.familyName[0], "Smith", "Family name should match");
@@ -39,7 +39,7 @@ add_task(function* test_GoogleImport() {
   Assert.equal(c.category[0], "google", "Category should match");
   Assert.equal(c.id, "http://www.google.com/m8/feeds/contacts/tester%40mochi.com/base/0", "UID should match and be scoped to provider");
 
-  c = mockDb._store[mockDb._next_guid - 4];
+  c = mockDb._store[mockDb._next_guid - 5];
   Assert.equal(c.name[0], "Jane Smith", "Full name should match");
   Assert.equal(c.givenName[0], "Jane", "Given name should match");
   Assert.equal(c.familyName[0], "Smith", "Family name should match");
@@ -49,7 +49,7 @@ add_task(function* test_GoogleImport() {
   Assert.equal(c.category[0], "google", "Category should match");
   Assert.equal(c.id, "http://www.google.com/m8/feeds/contacts/tester%40mochi.com/base/1", "UID should match and be scoped to provider");
 
-  c = mockDb._store[mockDb._next_guid - 3];
+  c = mockDb._store[mockDb._next_guid - 4];
   Assert.equal(c.name[0], "Davy Randall Jones", "Full name should match");
   Assert.equal(c.givenName[0], "Davy Randall", "Given name should match");
   Assert.equal(c.familyName[0], "Jones", "Family name should match");
@@ -59,7 +59,7 @@ add_task(function* test_GoogleImport() {
   Assert.equal(c.category[0], "google", "Category should match");
   Assert.equal(c.id, "http://www.google.com/m8/feeds/contacts/tester%40mochi.com/base/2", "UID should match and be scoped to provider");
 
-  c = mockDb._store[mockDb._next_guid - 2];
+  c = mockDb._store[mockDb._next_guid - 3];
   Assert.equal(c.name[0], "noname@example.com", "Full name should match");
   Assert.equal(c.email[0].type, "other", "Email type should match");
   Assert.equal(c.email[0].value, "noname@example.com", "Email should match");
@@ -67,11 +67,19 @@ add_task(function* test_GoogleImport() {
   Assert.equal(c.category[0], "google", "Category should match");
   Assert.equal(c.id, "http://www.google.com/m8/feeds/contacts/tester%40mochi.com/base/3", "UID should match and be scoped to provider");
 
-  c = mockDb._store[mockDb._next_guid - 1];
+  c = mockDb._store[mockDb._next_guid - 2];
   Assert.equal(c.name[0], "lycnix", "Full name should match");
   Assert.equal(c.email[0].type, "other", "Email type should match");
   Assert.equal(c.email[0].value, "lycnix", "Email should match");
   Assert.equal(c.email[0].pref, true, "Pref should match");
   Assert.equal(c.category[0], "google", "Category should match");
   Assert.equal(c.id, "http://www.google.com/m8/feeds/contacts/tester%40mochi.com/base/7", "UID should match and be scoped to provider");
+
+  c = mockDb._store[mockDb._next_guid - 1];
+  Assert.equal(c.name[0], "+31-6-12345678", "Full name should match");
+  Assert.equal(c.tel[0].type, "mobile", "Email type should match");
+  Assert.equal(c.tel[0].value, "+31-6-12345678", "Email should match");
+  Assert.equal(c.tel[0].pref, false, "Pref should match");
+  Assert.equal(c.category[0], "google", "Category should match");
+  Assert.equal(c.id, "http://www.google.com/m8/feeds/contacts/tester%40mochi.com/base/8", "UID should match and be scoped to provider");
 });
