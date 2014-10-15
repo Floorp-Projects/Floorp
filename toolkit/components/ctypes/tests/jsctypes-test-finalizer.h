f5c9e729c2257e39aa536a9f372a968185237074
Mike Hommey <mh+mozilla@glandium.org>
2014-10-15 14:46:38 +0900

    Bug 1080998 - Build libjsctypes-test without a dependency on mozalloc or mozglue. r=jorendorff

--- toolkit/components/ctypes/tests/jsctypes-test-finalizer.h
+++ toolkit/components/ctypes/tests/jsctypes-test-finalizer.h
@@ -2,10 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#define EXPORT_CDECL(type)   NS_EXPORT type
+#include "mozilla/Attributes.h"
+#include "mozilla/Types.h"
 
-NS_EXTERN_C
-{
+#define EXPORT_CDECL(type)   MOZ_EXPORT type
+
+MOZ_BEGIN_EXTERN_C
+
   EXPORT_CDECL(void) test_finalizer_start(size_t size);
   EXPORT_CDECL(void) test_finalizer_stop();
   EXPORT_CDECL(bool) test_finalizer_resource_is_acquired(size_t i);
@@ -51,4 +54,4 @@ NS_EXTERN_C
   EXPORT_CDECL(void) test_finalizer_rel_size_t_set_errno(size_t i);
   EXPORT_CDECL(void) reset_errno();
 
-}
+MOZ_END_EXTERN_C
