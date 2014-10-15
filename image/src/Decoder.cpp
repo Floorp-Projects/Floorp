49c6315afd2f89290d08bdc05343fe22578cd51a
Ryan VanderMeulen <ryanvm@gmail.com>
2014-10-15 19:00:12 -0400

    Backed out 5 changesets (bug 1079653) for mochitest-dt asserts on a CLOSED TREE.
    Backed out changeset aaac8c671299 (bug 1079653)
    Backed out changeset 59d1754eb01f (bug 1079653)
    Backed out changeset dfb0890b02be (bug 1079653)
    Backed out changeset 546f90c14465 (bug 1079653)
    Backed out changeset a8e760faf784 (bug 1079653)

--- image/src/Decoder.cpp
+++ image/src/Decoder.cpp
@@ -19,7 +19,6 @@ Decoder::Decoder(RasterImage &aImage)
   , mCurrentFrame(nullptr)
   , mImageData(nullptr)
   , mColormap(nullptr)
-  , mChunkCount(0)
   , mDecodeFlags(0)
   , mBytesDecoded(0)
   , mDecodeDone(false)
@@ -27,7 +26,6 @@ Decoder::Decoder(RasterImage &aImage)
   , mFrameCount(0)
   , mFailCode(NS_OK)
   , mNeedsNewFrame(false)
-  , mNeedsToFlushData(false)
   , mInitialized(false)
   , mSizeDecode(false)
   , mInFrame(false)
@@ -99,20 +97,10 @@ Decoder::Write(const char* aBuffer, uint32_t aCount, DecodeStrategy aStrategy)
   MOZ_ASSERT(!HasDecoderError(),
              "Not allowed to make more decoder calls after error!");
 
-  // Begin recording telemetry data.
-  TimeStamp start = TimeStamp::Now();
-  mChunkCount++;
-
   // Keep track of the total number of bytes written.
   mBytesDecoded += aCount;
 
-  // If we're flushing data, clear the flag.
-  if (aBuffer == nullptr && aCount == 0) {
-    MOZ_ASSERT(mNeedsToFlushData, "Flushing when we don't need to");
-    mNeedsToFlushData = false;
-  }
-
-  // If a data error occured, just ignore future data.
+  // If a data error occured, just ignore future data
   if (HasDataError())
     return;
 
@@ -134,9 +122,6 @@ Decoder::Write(const char* aBuffer, uint32_t aCount, DecodeStrategy aStrategy)
       WriteInternal(nullptr, 0, aStrategy);
     }
   }
-
-  // Finish telemetry.
-  mDecodeTime += (TimeStamp::Now() - start);
 }
 
 void
@@ -256,12 +241,6 @@ Decoder::AllocateFrame()
   // Mark ourselves as not needing another frame before talking to anyone else
   // so they can tell us if they need yet another.
   mNeedsNewFrame = false;
-
-  // If we've received any data at all, we may have pending data that needs to
-  // be flushed now that we have a frame to decode into.
-  if (mBytesDecoded > 0) {
-    mNeedsToFlushData = true;
-  }
 
   return rv;
 }
