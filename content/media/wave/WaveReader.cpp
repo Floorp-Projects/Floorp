abd91c68a9bdd489e90973c7f5826f53ef20aa19
Matthew Gregan <kinetik@flim.org>
2014-10-15 13:56:09 +1300

    Bug 1080986 - Check list chunk is large enough to read list ID before reading. r=giles, a=sledru

--- content/media/wave/WaveReader.cpp
+++ content/media/wave/WaveReader.cpp
@@ -551,7 +551,7 @@ WaveReader::LoadListChunk(uint32_t aChunkSize,
   static_assert(uint64_t(MAX_CHUNK_SIZE) < UINT_MAX / sizeof(char),
                 "MAX_CHUNK_SIZE too large for enumerator.");
 
-  if (aChunkSize > MAX_CHUNK_SIZE) {
+  if (aChunkSize > MAX_CHUNK_SIZE || aChunkSize < 4) {
     return false;
   }
 
@@ -561,7 +561,7 @@ WaveReader::LoadListChunk(uint32_t aChunkSize,
   }
 
   static const uint32_t INFO_LIST_MAGIC = 0x494e464f;
-  const char *p = chunk.get();
+  const char* p = chunk.get();
   if (ReadUint32BE(&p) != INFO_LIST_MAGIC) {
     return false;
   }
