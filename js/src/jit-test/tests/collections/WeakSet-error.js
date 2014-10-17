1f0c5084c37e613c1829d5ce483e56c77b38645c
Tom Schuster <evilpies@gmail.com>
2014-10-17 11:13:41 +0200

    Bug 1081978 - Make WeakSet closer to the spec. r=till

--- js/src/jit-test/tests/collections/WeakSet-error.js
+++ js/src/jit-test/tests/collections/WeakSet-error.js
@@ -1,24 +1,20 @@
 load(libdir + "asserts.js");
 
-function testMethod(name, hasArg = true) {
+function testMethod(name) {
     var method = WeakSet.prototype[name];
 
     assertThrowsInstanceOf(function() { method.call(1); }, TypeError);
     assertThrowsInstanceOf(function() { method.call({}); }, TypeError);
     assertThrowsInstanceOf(function() { method.call(new WeakMap); }, TypeError);
     assertThrowsInstanceOf(function() { method.call(WeakSet.prototype); }, TypeError);
-
-    if (hasArg) {
-        assertThrowsInstanceOf(function() { method.call(new WeakSet, 1); }, TypeError);
-        assertThrowsInstanceOf(function() { method.call(WeakSet.prototype, 1); }, TypeError);
-    }
 }
 
 testMethod("has");
 testMethod("add");
 testMethod("delete");
-testMethod("clear", false);
+testMethod("clear");
 
+assertThrowsInstanceOf(function() { var ws = new WeakSet(); ws.add(1); }, TypeError);
 assertThrowsInstanceOf(function() { new WeakSet({"@@iterator": 2}) }, TypeError);
 assertEq(typeof []["@@iterator"], "function"); // Make sure we fail when @@iterator is removed
 
