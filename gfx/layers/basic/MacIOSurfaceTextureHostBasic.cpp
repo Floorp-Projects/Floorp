86df145d30661b438a33f04eb335f124f869529b
Sotaro Ikeda <sikeda@mozilla.com>
2014-10-17 16:58:44 -0700

    Bug 1082986 - Use RefPtr to hold Compositor. r=nical, a=lsblakk

--- gfx/layers/basic/MacIOSurfaceTextureHostBasic.cpp
+++ gfx/layers/basic/MacIOSurfaceTextureHostBasic.cpp
@@ -4,7 +4,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "MacIOSurfaceTextureHostBasic.h"
-#include "mozilla/layers/BasicCompositor.h"
 #include "mozilla/gfx/MacIOSurface.h"
 
 namespace mozilla {
