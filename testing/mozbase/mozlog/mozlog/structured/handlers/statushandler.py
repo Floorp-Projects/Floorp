be7adede23789dd2e47ef8ec5b32264d372354af
Chris Manchester <cmanchester@mozilla.com>
2014-10-17 11:59:02 -0400

    Bug 1066785 - Log a summary of expected and unexpected statuses in the mozlog StatusHandler summary. r=jgraham, a=NPOTB

--- testing/mozbase/mozlog/mozlog/structured/handlers/statushandler.py
+++ testing/mozbase/mozlog/mozlog/structured/handlers/statushandler.py
@@ -10,17 +10,20 @@ from collections import (
 from mozlog.structured.structuredlog import log_levels
 
 RunSummary = namedtuple("RunSummary",
-                        ("unexpected", "skipped", "log_level_counts", "action_counts"))
+                        ("unexpected_statuses",
+                         "expected_statuses",
+                         "log_level_counts",
+                         "action_counts"))
 
 class StatusHandler(object):
     """A handler used to determine an overall status for a test run according
     to a sequence of log messages."""
 
     def __init__(self):
-        # The count of unexpected results (includes tests and subtests)
-        self.unexpected = 0
-        # The count of skip results (includes tests and subtests)
-        self.skipped = 0
+        # The count of each type of unexpected result status (includes tests and subtests)
+        self.unexpected_statuses = defaultdict(int)
+        # The count of each type of expected result status (includes tests and subtests)
+        self.expected_statuses = defaultdict(int)
         # The count of actions logged
         self.action_counts = defaultdict(int)
         # The count of messages logged at each log level
@@ -35,16 +38,17 @@ class StatusHandler(object):
             self.log_level_counts[data['level']] += 1
 
         if action in ('test_status', 'test_end'):
+            status = data['status']
             if 'expected' in data:
-                self.unexpected += 1
+                self.unexpected_statuses[status] += 1
+            else:
+                self.expected_statuses[status] += 1
 
-            if data['status'] == 'SKIP':
-                self.skipped += 1
 
     def summarize(self):
         return RunSummary(
-            self.unexpected,
-            self.skipped,
+            dict(self.unexpected_statuses),
+            dict(self.expected_statuses),
             dict(self.log_level_counts),
             dict(self.action_counts),
         )
