95067a43da3ac1d99d4fc2c5b7d0da68c3c6195e
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2014-10-17 16:05:21 +0900

    Bug 1071433 - Change order of audio/3gpp association for Gallery Apps. r=bz
    The AUDIO_3GPP has to come after the VIDEO_3GPP entry because the Gallery app
    on Firefox OS depends on the "3gp" extension mapping to the "video/3gpp" MIME
    type.

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -544,7 +544,6 @@ static nsExtraMimeTypeEntry extraMimeEntries [] =
   { AUDIO_OGG, "opus", "Opus Audio" },
 #ifdef MOZ_WIDGET_GONK
   { AUDIO_AMR, "amr", "Adaptive Multi-Rate Audio" },
-  { AUDIO_3GPP, "3gpp,3gp", "3GPP Audio" },
 #endif
   { VIDEO_WEBM, "webm", "Web Media Video" },
   { AUDIO_WEBM, "webm", "Web Media Audio" },
@@ -555,6 +554,12 @@ static nsExtraMimeTypeEntry extraMimeEntries [] =
   { AUDIO_WAV, "wav", "Waveform Audio" },
   { VIDEO_3GPP, "3gpp,3gp", "3GPP Video" },
   { VIDEO_3GPP2,"3g2", "3GPP2 Video" },
+#ifdef MOZ_WIDGET_GONK
+  // The AUDIO_3GPP has to come after the VIDEO_3GPP entry because the Gallery
+  // app on Firefox OS depends on the "3gp" extension mapping to the
+  // "video/3gpp" MIME type.
+  { AUDIO_3GPP, "3gpp,3gp", "3GPP Audio" },
+#endif
   { AUDIO_MIDI, "mid", "Standard MIDI Audio" }
 };
 
