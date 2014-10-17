9b4c29f73f612da7de571ae7ab9d575ac0f77867
Martyn Haigh <martyn.haigh@gmail.com>
2014-10-17 14:14:30 +0100

    Bug 1063586 - Audit tab related XML files to use new terminology (r=lucasr)

--- mobile/android/base/tabs/TabsListLayout.java
+++ mobile/android/base/tabs/TabsListLayout.java
@@ -65,8 +65,8 @@ class TabsListLayout extends TwoWayView
 
         setItemsCanFocus(true);
 
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TabsTray);
-        mIsPrivate = (a.getInt(R.styleable.TabsTray_tabs, 0x0) == 1);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TabsLayout);
+        mIsPrivate = (a.getInt(R.styleable.TabsLayout_tabs, 0x0) == 1);
         a.recycle();
 
         mTabsAdapter = new TabsListLayoutAdapter(mContext);
