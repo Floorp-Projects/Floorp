e15310d275a21ac910a2db80f9752896154734a5
Ralph Giles <giles@mozilla.com>
2014-10-17 11:31:00 -0700

    Bug 1084514 - Fix AndroidSourceTexture namespace issues. r=dholbert CLOSED TREE
    This worked in the unified build because it borrowed includes
    and namespace references from other files.

--- gfx/gl/GLBlitHelper.cpp
+++ gfx/gl/GLBlitHelper.cpp
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "GLBlitHelper.h"
+#include "AndroidSurfaceTexture.h"
 #include "GLContext.h"
 #include "ScopedGLHelpers.h"
 #include "mozilla/Preferences.h"
@@ -738,7 +739,7 @@ GLBlitHelper::BlitSurfaceTextureImage(layers::SurfaceTextureImage* stImage)
 
     surfaceTexture->UpdateTexImage();
 
-    Matrix4x4 transform;
+    gfx::Matrix4x4 transform;
     surfaceTexture->GetTransformMatrix(transform);
 
     mGL->fUniformMatrix4fv(mTextureTransformLoc, 1, false, &transform._11);
