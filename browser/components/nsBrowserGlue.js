95cd2f104ce8f00203689230e4e6ca7ea55127e0
Dão Gottwald <dao@mozilla.com>
2014-10-18 14:39:34 +0200

    Bug 1077740 - reset legacy homepages to about:home. r=gavin

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -1369,7 +1369,7 @@ BrowserGlue.prototype = {
   },
 
   _migrateUI: function BG__migrateUI() {
-    const UI_VERSION = 23;
+    const UI_VERSION = 24;
     const BROWSER_DOCURL = "chrome://browser/content/browser.xul";
     let currentUIVersion = 0;
     try {
@@ -1447,15 +1447,6 @@ BrowserGlue.prototype = {
       }
     }
 
-    if (currentUIVersion < 8) {
-      // Reset homepage pref for users who have it set to google.com/firefox
-      let uri = Services.prefs.getComplexValue("browser.startup.homepage",
-                                               Ci.nsIPrefLocalizedString).data;
-      if (uri && /^https?:\/\/(www\.)?google(\.\w{2,3}){1,2}\/firefox\/?$/.test(uri)) {
-        Services.prefs.clearUserPref("browser.startup.homepage");
-      }
-    }
-
     if (currentUIVersion < 9) {
       // This code adds the customizable downloads buttons.
       let currentset = xulStore.getValue(BROWSER_DOCURL, "nav-bar", "currentset");
@@ -1518,10 +1509,6 @@ BrowserGlue.prototype = {
           xulStore.setValue(BROWSER_DOCURL, "nav-bar", "currentset", currentset);
         }
       }
-    }
-
-    if (currentUIVersion < 13) {
-      /* Obsolete */
     }
 
     if (currentUIVersion < 14) {
@@ -1615,6 +1602,18 @@ BrowserGlue.prototype = {
                                                     Ci.nsIPrefLocalizedString).data;
           Services.search.currentEngine = Services.search.getEngineByName(name);
         } catch (ex) {}
+      }
+    }
+
+    if (currentUIVersion < 24) {
+      // Reset homepage pref for users who have it set to start.mozilla.org
+      // or google.com/firefox.
+      const HOMEPAGE_PREF = "browser.startup.homepage";
+      let uri = Services.prefs.getComplexValue(HOMEPAGE_PREF,
+                                               Ci.nsIPrefLocalizedString).data;
+      if (uri && (uri.startsWith("http://start.mozilla.org") ||
+                  /^https?:\/\/(www\.)?google\.[a-z.]+\/firefox/i.test(uri))) {
+        Services.prefs.clearUserPref(HOMEPAGE_PREF);
       }
     }
 
