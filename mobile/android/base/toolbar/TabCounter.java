bf0ae37c5db97ab6cca55d63fcd0a84e8635cea1
Richard Newman <rnewman@mozilla.com>
2014-10-20 12:05:40 -0700

    Bug 1084522 - Don't redefine layout attribute in IconTabWidget. r=lucasr, a=lmandel

--- mobile/android/base/toolbar/TabCounter.java
+++ mobile/android/base/toolbar/TabCounter.java
@@ -46,7 +46,7 @@ public class TabCounter extends ThemedTextSwitcher
         super(context, attrs);
 
         final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TabCounter);
-        mLayoutId = a.getResourceId(R.styleable.TabCounter_layout, R.layout.tabs_counter);
+        mLayoutId = a.getResourceId(R.styleable.TabCounter_android_layout, R.layout.tabs_counter);
         a.recycle();
 
         mInflater = LayoutInflater.from(context);
