dc53be8b8518f6c202702577159cfe7fea6c6aa4
Bobby Holley <bobbyholley@gmail.com>
2014-10-20 15:52:53 +0200

    Bug 1081985 - Remove prototype remapping and make COWs simple. r=gabor
    \o/

--- js/xpconnect/wrappers/ChromeObjectWrapper.h
+++ js/xpconnect/wrappers/ChromeObjectWrapper.h
@@ -15,11 +15,10 @@ namespace xpc {
 
 struct ExposedPropertiesOnly;
 
-// When chrome JS objects are exposed to content, they get a ChromeObjectWrapper.
-//
-// The base filtering wrapper here does most of the work for us. We define a
-// custom class here to introduce custom behavior with respect to the prototype
-// chain.
+// When a vanilla chrome JS object is exposed to content, we use a wrapper that
+// supports __exposedProps__ for legacy reasons. For extra security, we override
+// the traps that allow content to pass an object to chrome, and perform extra
+// security checks on them.
 #define ChromeObjectWrapperBase \
   FilteringWrapper<js::CrossCompartmentSecurityWrapper, ExposedPropertiesOnly>
 
@@ -28,34 +27,12 @@ class ChromeObjectWrapper : public ChromeObjectWrapperBase
   public:
     MOZ_CONSTEXPR ChromeObjectWrapper() : ChromeObjectWrapperBase(0) {}
 
-    virtual bool enter(JSContext *cx, JS::Handle<JSObject*> wrapper, JS::Handle<jsid> id,
-                       js::Wrapper::Action act, bool *bp) const MOZ_OVERRIDE;
-
     virtual bool defineProperty(JSContext *cx, JS::Handle<JSObject*> wrapper,
                                 JS::Handle<jsid> id,
                                 JS::MutableHandle<JSPropertyDescriptor> desc) const MOZ_OVERRIDE;
-    virtual bool has(JSContext *cx, JS::Handle<JSObject*> wrapper,
-                     JS::Handle<jsid> id, bool *bp) const MOZ_OVERRIDE;
-    virtual bool get(JSContext *cx, JS::Handle<JSObject*> wrapper, JS::Handle<JSObject*> receiver,
-                     JS::Handle<jsid> id, JS::MutableHandle<JS::Value> vp) const MOZ_OVERRIDE;
     virtual bool set(JSContext *cx, JS::Handle<JSObject*> wrapper,
                      JS::Handle<JSObject*> receiver, JS::Handle<jsid> id,
                      bool strict, JS::MutableHandle<JS::Value> vp) const MOZ_OVERRIDE;
-
-    virtual bool getPropertyDescriptor(JSContext *cx, JS::Handle<JSObject*> wrapper,
-                                       JS::Handle<jsid> id,
-                                       JS::MutableHandle<JSPropertyDescriptor> desc) const MOZ_OVERRIDE;
-    virtual bool objectClassIs(JS::Handle<JSObject*> obj, js::ESClassValue classValue,
-                               JSContext *cx) const MOZ_OVERRIDE;
-
-    // NB: One might think we'd need to implement enumerate(),
-    // getOwnEnumerablePropertyKeys(), iterate(), and ownPropertyKeys()
-    // here. However, ES5 built-in properties aren't enumerable (and
-    // SpiderMonkey's implementation seems to match the spec modulo
-    // Error.prototype.fileName and Error.prototype.lineNumber). Since we're
-    // only remapping the prototypes of standard objects, there would never be
-    // anything more to enumerate up the prototype chain. So we can actually
-    // skip these.
 
     static const ChromeObjectWrapper singleton;
 };
