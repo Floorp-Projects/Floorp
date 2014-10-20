2da0ec7d9720726ce52e48d0d6c634ccd495a125
Michael Comella <michael.l.comella@gmail.com>
2014-10-20 08:05:46 -0700

    Bug 1084542 - Move shared code from Back/ForwardButtons to NavButton. r=lucasr

---
+++ mobile/android/base/toolbar/NavButton.java
@@ -0,0 +1,71 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.toolbar;
+
+import org.mozilla.gecko.R;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.util.AttributeSet;
+
+abstract class NavButton extends ShapedButton {
+    protected final Path mBorderPath;
+    protected final Paint mBorderPaint;
+    protected final float mBorderWidth;
+
+    public NavButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        final Resources res = getResources();
+        mBorderWidth = res.getDimension(R.dimen.nav_button_border_width);
+
+        // Paint to draw the border.
+        mBorderPaint = new Paint();
+        mBorderPaint.setAntiAlias(true);
+        mBorderPaint.setStrokeWidth(mBorderWidth);
+        mBorderPaint.setStyle(Paint.Style.STROKE);
+
+        // Path is masked.
+        mBorderPath = new Path();
+
+        setPrivateMode(false);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        // Draw the border on top.
+        canvas.drawPath(mBorderPath, mBorderPaint);
+    }
+
+    // The drawable is constructed as per @drawable/url_bar_nav_button.
+    @Override
+    public void onLightweightThemeChanged() {
+        final Drawable drawable = mTheme.getDrawable(this);
+        if (drawable == null)
+            return;
+
+        final StateListDrawable stateList = new StateListDrawable();
+        stateList.addState(PRIVATE_PRESSED_STATE_SET, getColorDrawable(R.color.highlight_nav_pb));
+        stateList.addState(PRESSED_ENABLED_STATE_SET, getColorDrawable(R.color.highlight_nav));
+        stateList.addState(PRIVATE_FOCUSED_STATE_SET, getColorDrawable(R.color.highlight_nav_focused_pb));
+        stateList.addState(FOCUSED_STATE_SET, getColorDrawable(R.color.highlight_nav_focused));
+        stateList.addState(PRIVATE_STATE_SET, getColorDrawable(R.color.background_private));
+        stateList.addState(EMPTY_STATE_SET, drawable);
+
+        setBackgroundDrawable(stateList);
+    }
+
+    @Override
+    public void onLightweightThemeReset() {
+        setBackgroundResource(R.drawable.url_bar_nav_button);
+    }
+}
