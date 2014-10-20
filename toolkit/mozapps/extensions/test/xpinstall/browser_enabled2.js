c33362a45c8610f61862326071d7d086ad7e45ff
Dave Townsend <dtownsend@oxymoronical.com>
2014-10-20 10:22:48 -0700

    Bug 1080785: Fix and enable xpinstall tests in e10s. r=Unfocused

--- toolkit/mozapps/extensions/test/xpinstall/browser_enabled2.js
+++ toolkit/mozapps/extensions/test/xpinstall/browser_enabled2.js
@@ -6,15 +6,18 @@ function test() {
   Services.prefs.setBoolPref("xpinstall.enabled", false);
 
   gBrowser.selectedTab = gBrowser.addTab();
-  gBrowser.selectedBrowser.addEventListener("load", function() {
-    gBrowser.selectedBrowser.removeEventListener("load", arguments.callee, true);
-    // Allow the in-page load handler to run first
-    executeSoon(page_loaded);
-  }, true);
+
+  function loadListener() {
+    gBrowser.selectedBrowser.removeEventListener("load", loadListener, true);
+    gBrowser.contentWindow.addEventListener("PageLoaded", page_loaded, false);
+  }
+
+  gBrowser.selectedBrowser.addEventListener("load", loadListener, true);
   gBrowser.loadURI(TESTROOT + "enabled.html");
 }
 
 function page_loaded() {
+  gBrowser.contentWindow.removeEventListener("PageLoaded", page_loaded, false);
   Services.prefs.clearUserPref("xpinstall.enabled");
 
   var doc = gBrowser.contentDocument;
