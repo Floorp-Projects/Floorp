a2de49dabfd23d694b4cd53d23fd481d3c31f7d7
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-10-21 14:38:11 +0200

    Backed out changeset 4040623689b7 (bug 1064333)

--- toolkit/components/telemetry/tests/unit/test_TelemetryPingBuildID.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryPingBuildID.js
@@ -15,16 +15,10 @@
 
 "use strict"
 
-const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+const Cu = Components.utils;
 
 Cu.import("resource://gre/modules/Services.jsm", this);
 Cu.import("resource://gre/modules/TelemetryPing.jsm", this);
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-
-XPCOMUtils.defineLazyGetter(this, "gDatareportingService",
-  () => Cc["@mozilla.org/datareporting/service;1"]
-          .getService(Ci.nsISupports)
-          .wrappedJSObject);
 
 // Force the Telemetry enabled preference so that TelemetryPing.reset() doesn't exit early.
 Services.prefs.setBoolPref(TelemetryPing.Constants.PREF_ENABLED, true);
@@ -71,11 +65,5 @@ add_task(function* test_newBuild() {
 function run_test() {
   // Make sure we have a profile directory.
   do_get_profile();
-
-  // Send the needed startup notifications to the datareporting service
-  // to ensure that it has been initialized.
-  gDatareportingService.observe(null, "app-startup", null);
-  gDatareportingService.observe(null, "profile-after-change", null);
-
   run_next_test();
 }
