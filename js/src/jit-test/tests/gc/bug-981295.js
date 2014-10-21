4b6f61b98f652247d01030b15ec694c421b35b58
Emanuel Hoogeveen <emanuel.hoogeveen@gmail.com>
2014-10-21 11:50:00 +0200

    Bug 1084651 - Part 4a: Adjust some tests to preserve previous behavior.  r=wmccloskey

--- js/src/jit-test/tests/gc/bug-981295.js
+++ js/src/jit-test/tests/gc/bug-981295.js
@@ -4,6 +4,6 @@ var juneDate = new Date(2000, 5, 20, 0, 0, 0, 0);
 for (var i = 0; i < function(x) myObj(Date.prototype.toString.apply(x)); void i) {
     eval(a.text.replace(/@/g, ""))
 }
-gcslice(2600);
+gcslice(2601);
 function testcase() {}
 new Uint16Array(testcase);
