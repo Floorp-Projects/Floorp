51eba88842220e9e824bb1d543208d54669a774b
Thomas Zimmermann <tdz@users.sourceforge.net>
2014-10-21 10:52:12 +0200

    Bug 1083092: Data parameter for |mozilla::ipc::SocketIOSendTask| template, r=shawnjohnjr
    The data class in |SocketIOSendTask| is now a template parameter, instead
    of being hard-coded to |UnixSocketRawData|. The patch also adds soem minor
    cleanups to the file.

--- dom/bluetooth2/bluedroid/BluetoothSocket.cpp
+++ dom/bluetooth2/bluedroid/BluetoothSocket.cpp
@@ -666,7 +666,7 @@ BluetoothSocket::SendSocketData(UnixSocketRawData* aData)
   MOZ_ASSERT(!mImpl->IsShutdownOnMainThread());
 
   XRE_GetIOMessageLoop()->PostTask(
-    FROM_HERE, new SocketIOSendTask<DroidSocketImpl>(mImpl, aData));
+    FROM_HERE, new SocketIOSendTask<DroidSocketImpl, UnixSocketRawData>(mImpl, aData));
 
   return true;
 }
