43cb923784e0bb74bb048022446c50000b790bfa
Paul Adenot <paul@paul.cx>
2014-10-22 16:12:29 +0200

    Bug 1085356 - Better handling of OSX audio output devices switching when SourceMediaStream are present in the MSG. r=jesup a=lmandel
    On OSX, when the audio output device changes, the OS will call the audio
    callbacks in weird patterns, if at all, during a period of ~1s. If
    real-time SourceMediaStreams are present in the MediaStreamGraph, this means
    buffering will occur, and the overall latency between the MediaStreamGraph
    insertion time, and the actual output time will grow.
    
    To fix this, we detect when the output device changes, and we switch temporarily
    to a SystemClockDriver, that will pull from the SourceMediaStream, and simply
    discard all input data. Then, when we get audio callbacks called reliably
    (basically, when OSX is done switching to the other output), we switch back to
    the previous AudioCallbackDriver.
    
    We keep the previous AudioCallbackDriver alive using a self-reference. If an
    AudioCallbackDriver has a self-reference, that means it's in a state when a
    device is switching, so it's not linked to an MSG per se.

---
+++ content/media/SelfRef.h
@@ -0,0 +1,47 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef SELF_REF_H
+#define SELF_REF_H
+
+#include "mozilla/Attributes.h"
+
+namespace mozilla {
+
+template<class T>
+class SelfReference {
+public:
+  SelfReference() : mHeld(false) {}
+  ~SelfReference()
+  {
+    NS_ASSERTION(!mHeld, "Forgot to drop the self reference?");
+  }
+
+  void Take(T* t)
+  {
+    if (!mHeld) {
+      mHeld = true;
+      t->AddRef();
+    }
+  }
+  void Drop(T* t)
+  {
+    if (mHeld) {
+      mHeld = false;
+      t->Release();
+    }
+  }
+
+  operator bool() const { return mHeld; }
+
+  SelfReference(const SelfReference& aOther) MOZ_DELETE;
+  void operator=(const SelfReference& aOther) MOZ_DELETE;
+private:
+  bool mHeld;
+};
+} // mozilla
+
+#endif // SELF_REF_H
