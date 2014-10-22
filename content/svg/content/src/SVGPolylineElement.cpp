d8956f902d932057267cac77e3ba35437b633c5a
Jonathan Watt <jwatt@jwatt.org>
2014-10-22 10:48:37 +0100

    Bug 1085092 - Fix stroking of SVG polygon elements. r=longsonr, a=lsblakk

--- content/svg/content/src/SVGPolylineElement.cpp
+++ content/svg/content/src/SVGPolylineElement.cpp
@@ -5,6 +5,10 @@
 
 #include "mozilla/dom/SVGPolylineElement.h"
 #include "mozilla/dom/SVGPolylineElementBinding.h"
+#include "mozilla/gfx/2D.h"
+
+using namespace mozilla;
+using namespace mozilla::gfx;
 
 NS_IMPL_NS_NEW_NAMESPACED_SVG_ELEMENT(Polyline)
 
@@ -29,6 +33,26 @@ SVGPolylineElement::SVGPolylineElement(already_AddRefed<mozilla::dom::NodeInfo>&
 // nsIDOMNode methods
 
 NS_IMPL_ELEMENT_CLONE_WITH_INIT(SVGPolylineElement)
+
+//----------------------------------------------------------------------
+// nsSVGPathGeometryElement methods
+
+TemporaryRef<Path>
+SVGPolylineElement::BuildPath(PathBuilder* aBuilder)
+{
+  const SVGPointList &points = mPoints.GetAnimValue();
+
+  if (points.IsEmpty()) {
+    return nullptr;
+  }
+
+  aBuilder->MoveTo(points[0]);
+  for (uint32_t i = 1; i < points.Length(); ++i) {
+    aBuilder->LineTo(points[i]);
+  }
+
+  return aBuilder->Finish();
+}
 
 } // namespace dom
 } // namespace mozilla
