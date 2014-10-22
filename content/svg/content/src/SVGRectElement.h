5d50d54b0fc29d9f5bcc8e97af709b623e932082
Jonathan Watt <jwatt@jwatt.org>
2014-10-22 12:29:05 +0100

    Bug 1074161 - Avoid creating a Moz2D Path object to draw SVG <rect>, <image> and <line> elements. r=mattwoodrow

--- content/svg/content/src/SVGRectElement.h
+++ content/svg/content/src/SVGRectElement.h
@@ -30,6 +30,7 @@ public:
   virtual bool HasValidDimensions() const MOZ_OVERRIDE;
 
   // nsSVGPathGeometryElement methods:
+  virtual void GetAsSimplePath(SimplePath* aSimplePath) MOZ_OVERRIDE;
   virtual TemporaryRef<Path> BuildPath(PathBuilder* aBuilder = nullptr) MOZ_OVERRIDE;
 
   virtual nsresult Clone(mozilla::dom::NodeInfo *aNodeInfo, nsINode **aResult) const MOZ_OVERRIDE;
