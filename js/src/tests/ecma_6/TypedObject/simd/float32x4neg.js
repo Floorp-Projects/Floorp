740407faa16e6216605637bc5730e21def8174fb
ProgramFOX <programfox@hotmail.be>
2014-10-22 16:30:14 +0200

    Bug 948379 - added float32x4 border cases. r=bbouvier

--- js/src/tests/ecma_6/TypedObject/simd/float32x4neg.js
+++ js/src/tests/ecma_6/TypedObject/simd/float32x4neg.js
@@ -8,14 +8,26 @@ var summary = 'float32x4 neg';
 function test() {
   print(BUGNUMBER + ": " + summary);
 
-  // FIXME -- Bug 948379: Amend to check for correctness of border cases.
-
   var a = float32x4(1, 2, 3, 4);
   var c = SIMD.float32x4.neg(a);
   assertEq(c.x, -1);
   assertEq(c.y, -2);
   assertEq(c.z, -3);
   assertEq(c.w, -4);
+
+  var d = float32x4(0.999, -0.001, 3.78, 4.05);
+  var f = SIMD.float32x4.neg(d);
+  assertEq(f.x, Math.fround(-0.999));
+  assertEq(f.y, Math.fround(0.001));
+  assertEq(f.z, Math.fround(-3.78));
+  assertEq(f.w, Math.fround(-4.05));
+
+  var g = float32x4(NaN, -0, Infinity, -Infinity);
+  var i = SIMD.float32x4.neg(g);
+  assertEq(i.x, NaN);
+  assertEq(i.y, 0);
+  assertEq(i.z, -Infinity);
+  assertEq(i.w, Infinity);
 
   if (typeof reportCompare === "function")
     reportCompare(true, true);
