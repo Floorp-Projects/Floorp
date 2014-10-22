47d92c2b889b59faaf9eecbcc182d8e9f1d68fa4
Boris Zbarsky <bzbarsky@mit.edu>
2014-10-22 11:40:50 -0400

    Bug 1019191 part 17.  Make some of the remaining quickstub unwrap methods static.  r=peterv
    Note that we know that we always call UnwrapArg with an object, so
    xpc_qsUnwrapArgImpl knows v is always an object.

--- js/xpconnect/src/XPCQuickStubs.h
+++ js/xpconnect/src/XPCQuickStubs.h
@@ -12,41 +12,6 @@
 /* XPCQuickStubs.h - Support functions used only by Web IDL bindings, for now. */
 
 nsresult
-getWrapper(JSContext *cx,
-           JSObject *obj,
-           XPCWrappedNative **wrapper,
-           JSObject **cur,
-           XPCWrappedNativeTearOff **tearoff);
-
-nsresult
-castNative(JSContext *cx,
-           XPCWrappedNative *wrapper,
-           JSObject *cur,
-           XPCWrappedNativeTearOff *tearoff,
-           const nsIID &iid,
-           void **ppThis,
-           nsISupports **ppThisRef,
-           JS::MutableHandleValue vp);
-
-MOZ_ALWAYS_INLINE JSObject*
-xpc_qsUnwrapObj(jsval v, nsISupports **ppArgRef, nsresult *rv)
-{
-    *rv = NS_OK;
-    if (v.isObject()) {
-        return &v.toObject();
-    }
-
-    if (!v.isNullOrUndefined()) {
-        *rv = ((v.isInt32() && v.toInt32() == 0)
-               ? NS_ERROR_XPC_BAD_CONVERT_JS_ZERO_ISNOT_NULL
-               : NS_ERROR_XPC_BAD_CONVERT_JS);
-    }
-
-    *ppArgRef = nullptr;
-    return nullptr;
-}
-
-nsresult
 xpc_qsUnwrapArgImpl(JSContext *cx, JS::HandleValue v, const nsIID &iid, void **ppArg,
                     nsISupports **ppArgRef, JS::MutableHandleValue vp);
 
