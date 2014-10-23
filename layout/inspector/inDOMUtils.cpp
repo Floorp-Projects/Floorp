b93b9a1f55dd3fa9e7d584b338d5144e8763d7a9
L. David Baron <dbaron@dbaron.org>
2014-10-23 12:57:27 -0400

    Bug 1082899 patch 1 - Add a flags field to the CSS_PSEUDO_CLASS and related macros.  r=birtles

--- layout/inspector/inDOMUtils.cpp
+++ layout/inspector/inDOMUtils.cpp
@@ -976,9 +976,9 @@ GetStatesForPseudoClass(const nsAString& aStatePseudo)
   // An array of the states that are relevant for various pseudoclasses.
   // XXXbz this duplicates code in nsCSSRuleProcessor
   static const EventStates sPseudoClassStates[] = {
-#define CSS_PSEUDO_CLASS(_name, _value, _pref)	\
+#define CSS_PSEUDO_CLASS(_name, _value, _flags, _pref) \
     EventStates(),
-#define CSS_STATE_PSEUDO_CLASS(_name, _value, _pref, _states)	\
+#define CSS_STATE_PSEUDO_CLASS(_name, _value, _flags, _pref, _states) \
     _states,
 #include "nsCSSPseudoClassList.h"
 #undef CSS_STATE_PSEUDO_CLASS
