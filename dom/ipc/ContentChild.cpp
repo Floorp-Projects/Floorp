43bac8a222a96777921fd2776dc9373ee7f5f5f4
Vinay G Shetty <shettyvinayg@gmail.com>
2014-10-23 11:31:00 -0700

    Bug 811470 - Send geolocation error codes to the content process. r=jdm

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -1908,6 +1908,17 @@ ContentChild::RecvGeolocationUpdate(const GeoPosition& somewhere)
 }
 
 bool
+ContentChild::RecvGeolocationError(const uint16_t& errorCode)
+{
+    nsCOMPtr<nsIGeolocationUpdate> gs = do_GetService("@mozilla.org/geolocation/service;1");
+    if (!gs) {
+        return true;
+    }
+    gs->NotifyError(errorCode);
+    return true;
+}
+
+bool
 ContentChild::RecvUpdateDictionaryList(const InfallibleTArray<nsString>& aDictionaries)
 {
     mAvailableDictionaries = aDictionaries;
---
+++ dom/tests/unit/test_geolocation_position_unavailable.js
@@ -0,0 +1,36 @@
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+const Cr = Components.results;
+
+
+function successCallback() {
+  do_check_true(false);
+  do_test_finished();
+}
+
+function errorCallback(err) {
+  do_check_eq(Ci.nsIDOMGeoPositionError.POSITION_UNAVAILABLE, err.code);
+  do_test_finished();
+}
+
+function run_test()
+{
+  do_test_pending();
+
+  if (Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime)
+        .processType == Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT) {
+    // XPCShell does not get a profile by default. The geolocation service
+    // depends on the settings service which uses IndexedDB and IndexedDB
+    // needs a place where it can store databases.
+    do_get_profile();
+
+    var prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
+    prefs.setBoolPref("geo.wifi.scan", false);
+    prefs.setCharPref("geo.wifi.uri", "UrlNotUsedHere:");
+    prefs.setBoolPref("dom.testing.ignore_ipc_principal", true);
+  }
+
+  geolocation = Cc["@mozilla.org/geolocation;1"].getService(Ci.nsISupports);
+  geolocation.getCurrentPosition(successCallback, errorCallback);
+}
