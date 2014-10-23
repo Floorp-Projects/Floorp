2be907c12d40940684560bf5779d6575f9fb54b5
Markus Stange <mstange@themasta.com>
2014-10-23 22:44:39 +0200

    Bug 1085475 - Don't attempt to use vibrancy in 32-bit mode. r=smichaud

--- widget/cocoa/VibrancyManager.mm
+++ widget/cocoa/VibrancyManager.mm
@@ -165,8 +165,13 @@ VibrancyManager::CreateEffectView(VibrancyType aType, NSRect aRect)
 static bool
 ComputeSystemSupportsVibrancy()
 {
+#ifdef __x86_64__
   return NSClassFromString(@"NSAppearance") &&
       NSClassFromString(@"NSVisualEffectView");
+#else
+  // objc_allocateClassPair doesn't work in 32 bit mode, so turn off vibrancy.
+  return false;
+#endif
 }
 
 /* static */ bool
