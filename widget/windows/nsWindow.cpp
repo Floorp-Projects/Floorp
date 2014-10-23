2bfc579a4f976bfb65143ff9c3b73b4e8977014a
Jeff Muizelaar <jmuizelaar@mozilla.com>
2014-10-23 00:15:22 -0400

    Bug 1083071 - Disable D3D11 and D3D9 layers on broken drivers. r=bjacob, a=sledru
    This will hopefully prevent black screen problems that people are seeing.

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -6740,10 +6740,21 @@ nsWindow::GetPreferredCompositorBackends(nsTArray<LayersBackend>& aHints)
     if (prefs.mPreferOpenGL) {
       aHints.AppendElement(LayersBackend::LAYERS_OPENGL);
     }
-    if (!prefs.mPreferD3D9) {
-      aHints.AppendElement(LayersBackend::LAYERS_D3D11);
+
+    ID3D11Device* device = gfxWindowsPlatform::GetPlatform()->GetD3D11Device();
+    if (device && !DoesD3D11DeviceSupportResourceSharing(device)) {
+      // bug 1083071 - bad things - fall back to basic layers
+      // This should not happen aside from driver bugs, and in particular
+      // should not happen on our test machines, so let's NS_ERROR to ensure
+      // that we would catch it as a test failure.
+      NS_ERROR("Can't use Direct3D 11 because of a driver bug "
+        "causing resource sharing to fail");
+    } else {
+      if (!prefs.mPreferD3D9) {
+        aHints.AppendElement(LayersBackend::LAYERS_D3D11);
+      }
+      aHints.AppendElement(LayersBackend::LAYERS_D3D9);
     }
-    aHints.AppendElement(LayersBackend::LAYERS_D3D9);
   }
   aHints.AppendElement(LayersBackend::LAYERS_BASIC);
 }
