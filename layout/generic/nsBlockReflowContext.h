849da8dd9f41b112f6b18fae60f1aa6b5e54e047
Jonathan Kew <jkew@mozilla.com>
2014-10-24 12:24:38 +0100

    Bug 1086883 - Convert writing modes where necessary for orthogonal flows when collapsing margins in nsBlockReflowContext methods. r=smontagu

--- layout/generic/nsBlockReflowContext.h
+++ layout/generic/nsBlockReflowContext.h
@@ -54,9 +54,11 @@ public:
   }
 
   /**
-   * Computes the collapsed block-start margin for a block whose reflow state
-   * is in aRS.
-   * The computed margin is added into aMargin.
+   * Computes the collapsed block-start margin (in the context's parent's
+   * writing mode) for a block whose reflow state is in aRS.
+   * The computed margin is added into aMargin, whose writing mode is the
+   * parent's mode as found in mMetrics.GetWritingMode(); note this may not be
+   * the block's own writing mode as found in aRS.
    * If aClearanceFrame is null then this is the first optimistic pass which
    * shall assume that no frames have clearance, and we clear the HasClearance
    * on all frames encountered.
@@ -70,11 +72,11 @@ public:
    * We return true if we changed the clearance state of any line and marked it
    * dirty.
    */
-  static bool ComputeCollapsedBStartMargin(const nsHTMLReflowState& aRS,
-                                           nsCollapsingMargin* aMargin,
-                                           nsIFrame* aClearanceFrame,
-                                           bool* aMayNeedRetry,
-                                           bool* aIsEmpty = nullptr);
+  bool ComputeCollapsedBStartMargin(const nsHTMLReflowState& aRS,
+                                    nsCollapsingMargin* aMargin,
+                                    nsIFrame* aClearanceFrame,
+                                    bool* aMayNeedRetry,
+                                    bool* aIsEmpty = nullptr);
 
 protected:
   nsPresContext* mPresContext;
