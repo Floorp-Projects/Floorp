695e5463e3dd77d8e3b4726d445be1a469dc678b
Mason Chang <mchang@mozilla.com>
2014-10-23 18:50:31 -0700

    Bug 1048667. Small vsync framework. r=roc

--- widget/gonk/HwcComposer2D.cpp
+++ widget/gonk/HwcComposer2D.cpp
@@ -240,15 +240,16 @@ HwcComposer2D::RunVsyncEventControl(bool aEnable)
 void
 HwcComposer2D::Vsync(int aDisplay, nsecs_t aVsyncTimestamp)
 {
+    nsecs_t timeSinceInit = aVsyncTimestamp - sAndroidInitTime;
+    TimeStamp vsyncTime = sMozInitTime + TimeDuration::FromMicroseconds(timeSinceInit / 1000);
+
 #ifdef MOZ_ENABLE_PROFILER_SPS
     if (profiler_is_active()) {
-      nsecs_t timeSinceInit = aVsyncTimestamp - sAndroidInitTime;
-      TimeStamp vsyncTime = sMozInitTime + TimeDuration::FromMicroseconds(timeSinceInit / 1000);
-      CompositorParent::PostInsertVsyncProfilerMarker(vsyncTime);
+        CompositorParent::PostInsertVsyncProfilerMarker(vsyncTime);
     }
 #endif
 
-    GeckoTouchDispatcher::NotifyVsync(aVsyncTimestamp);
+    VsyncDispatcher::GetInstance()->NotifyVsync(vsyncTime, aVsyncTimestamp);
 }
 
 // Called on the "invalidator" thread (run from HAL).
