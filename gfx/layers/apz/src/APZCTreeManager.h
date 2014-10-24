57adf459d45f4322f036b0503aa98a9a72940f4e
Botond Ballo <botond@mozilla.com>
2014-10-24 15:49:38 -0400

    Bug 1076163 - Basic APZ support for css-driven resolution. r=kats,tn
    This makes APZ behave nicely with most uses of a css transform:scale.
    
    Summary of changes:
      - FrameMetrics::mCumulativeResolution now includes the css-driven resolution
        in addition to the pres-shell resolution.
      - Displayports are now stored in Screen pixels rather than Layer pixels.
        This is what we want anyways (as we'd like the displayport size to remain
        constant as a fraction of the screen size), but it was necessary to make
        this change as part of this patch because continuing to store them in
        Layer pixels in the presence of a css-driven resolution would have
        required a bunch of infrastructure to implement correctly.
    
    Remaining work:
      - Layout painting a scrollable layer at a resolution different from the
        scale induced by the css transform causes problems. These will go away
        with bug 1076192.
      - Different resolutions on the x and y axes are not supported. This is
        tracked by bug 1039967.

--- gfx/layers/apz/src/APZCTreeManager.h
+++ gfx/layers/apz/src/APZCTreeManager.h
@@ -245,7 +245,7 @@ public:
    * function simply delegates to that one, so that non-layers code
    * never needs to include AsyncPanZoomController.h
    */
-  static const LayerMargin CalculatePendingDisplayPort(
+  static const ScreenMargin CalculatePendingDisplayPort(
     const FrameMetrics& aFrameMetrics,
     const ScreenPoint& aVelocity,
     double aEstimatedPaintDuration);
