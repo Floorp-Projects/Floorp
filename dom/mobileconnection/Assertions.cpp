983b6409ccf284c9fe5ad1b3b0ffc25b367d764c
Jessica Jong <jjong@mozilla.com>
2014-10-24 17:12:33 +0800

    Bug 1052843 - Part 2: Use enums for MobileRadioState in nsIMobileConnectionService.idl (dom/ipc). r=echen

--- dom/mobileconnection/Assertions.cpp
+++ dom/mobileconnection/Assertions.cpp
@@ -17,6 +17,16 @@ ASSERT_NETWORK_SELECTION_MODE_EQUALITY(Manual, NETWORK_SELECTION_MODE_MANUAL);
 
 #undef ASSERT_NETWORK_SELECTION_MODE_EQUALITY
 
+#define ASSERT_MOBILE_RADIO_STATE_EQUALITY(webidlState, xpidlState) \
+  static_assert(static_cast<int32_t>(MobileRadioState::webidlState) == nsIMobileConnection::xpidlState, \
+                "MobileRadioState::" #webidlState " should equal to nsIMobileConnection::" #xpidlState)
+
+ASSERT_MOBILE_RADIO_STATE_EQUALITY(Enabling, MOBILE_RADIO_STATE_ENABLING);
+ASSERT_MOBILE_RADIO_STATE_EQUALITY(Enabled, MOBILE_RADIO_STATE_ENABLED);
+ASSERT_MOBILE_RADIO_STATE_EQUALITY(Disabling, MOBILE_RADIO_STATE_DISABLING);
+ASSERT_MOBILE_RADIO_STATE_EQUALITY(Disabled, MOBILE_RADIO_STATE_DISABLED);
+
+#undef ASSERT_MOBILE_RADIO_STATE_EQUALITY
+
 } // namespace dom
 } // namespace mozilla
-
