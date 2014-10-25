6563c20bc0cf1232ddcf008dca9593d5a8de5267
Jonathan Watt <jwatt@jwatt.org>
2014-10-25 13:55:55 +0100

    Bug 1086977 follow-up - Add some |#if defined(MOZ_CRASHREPORTER)| code to unbreak bug 1086977's bustage of --disable-crashreporter

--- dom/plugins/base/nsPluginsDirDarwin.cpp
+++ dom/plugins/base/nsPluginsDirDarwin.cpp
@@ -25,7 +25,9 @@
 #include "nsILocalFileMac.h"
 
 #include "nsCocoaFeatures.h"
+#if defined(MOZ_CRASHREPORTER)
 #include "nsExceptionHandler.h"
+#endif
 
 #include <string.h>
 #include <stdio.h>
@@ -480,12 +482,14 @@ nsresult nsPluginFile::GetPluginInfo(nsPluginInfo& info, PRLibrary **outLibrary)
       NS_WARNING("Preventing load of fbplugin (see bug 1086977)");
       return NS_ERROR_FAILURE;
     }
+#if defined(MOZ_CRASHREPORTER)
     // The block above assumes that "fbplugin" is the filename of the plugin
     // to be blocked, but be don't yet know for sure if this is true.  It might
     // also be the name of a file indirectly loaded by the plugin.  So for the
     // time being we must record extra information in our crash logs.
     CrashReporter::AnnotateCrashReport(NS_LITERAL_CSTRING("Bug 1086977"),
                                        fileName);
+#endif
   }
 
   // It's possible that our plugin has 2 entry points that'll give us mime type
@@ -495,6 +499,7 @@ nsresult nsPluginFile::GetPluginInfo(nsPluginInfo& info, PRLibrary **outLibrary)
 
   // Sadly we have to load the library for this to work.
   rv = LoadPlugin(outLibrary);
+#if defined(MOZ_CRASHREPORTER)
   if (nsCocoaFeatures::OnYosemiteOrLater()) {
     // If we didn't crash in LoadPlugin(), change the previous annotation so we
     // don't sow confusion.  Unfortunately there's not (yet) any way to get rid
@@ -502,6 +507,7 @@ nsresult nsPluginFile::GetPluginInfo(nsPluginInfo& info, PRLibrary **outLibrary)
     CrashReporter::AnnotateCrashReport(NS_LITERAL_CSTRING("Bug 1086977"),
                                        NS_LITERAL_CSTRING("Didn't crash, please ignore"));
   }
+#endif
   if (NS_FAILED(rv))
     return rv;
 
