f65fac9fe4df87d4ab32eaa72a80b3db9bedecc1
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-10-27 16:56:40 +0100

    Backed out changeset 9742b31c634a (bug 1073419) for bustage

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -84,7 +84,6 @@
 #include "nsConsoleService.h"
 #include "nsDebugImpl.h"
 #include "nsFrameMessageManager.h"
-#include "nsGeolocationSettings.h"
 #include "nsHashPropertyBag.h"
 #include "nsIAlertsService.h"
 #include "nsIAppsService.h"
@@ -3688,19 +3687,6 @@ ContentParent::RecvAddGeolocationListener(const IPC::Principal& aPrincipal,
     // creation of a new listener.
     RecvRemoveGeolocationListener();
     mGeolocationWatchID = AddGeolocationListener(this, aHighAccuracy);
-
-    // let the the settings cache know the origin of the new listener
-    nsAutoCString origin;
-    // hint to the compiler to use the conversion operator to nsIPrincipal*
-    nsCOMPtr<nsIPrincipal> principal = static_cast<nsIPrincipal*>(aPrincipal);
-    if (!principal) {
-      return true;
-    }
-    principal->GetOrigin(getter_Copies(origin));
-    nsRefPtr<nsGeolocationSettings> gs = nsGeolocationSettings::GetGeolocationSettings();
-    if (gs) {
-      gs->PutWatchOrigin(mGeolocationWatchID, origin);
-    }
     return true;
 }
 
@@ -3713,11 +3699,6 @@ ContentParent::RecvRemoveGeolocationListener()
             return true;
         }
         geo->ClearWatch(mGeolocationWatchID);
-
-        nsRefPtr<nsGeolocationSettings> gs = nsGeolocationSettings::GetGeolocationSettings();
-        if (gs) {
-          gs->RemoveWatchOrigin(mGeolocationWatchID);
-        }
         mGeolocationWatchID = -1;
     }
     return true;
@@ -3729,21 +3710,8 @@ ContentParent::RecvSetGeolocationHigherAccuracy(const bool& aEnable)
     // This should never be called without a listener already present,
     // so this check allows us to forgo securing privileges.
     if (mGeolocationWatchID != -1) {
-        nsCString origin;
-        nsRefPtr<nsGeolocationSettings> gs = nsGeolocationSettings::GetGeolocationSettings();
-        // get the origin stored for the curent watch ID
-        if (gs) {
-          gs->GetWatchOrigin(mGeolocationWatchID, origin);
-        }
-
-        // remove and recreate a new, high-accuracy listener
         RecvRemoveGeolocationListener();
         mGeolocationWatchID = AddGeolocationListener(this, aEnable);
-
-        // map the new watch ID to the origin
-        if (gs) {
-          gs->PutWatchOrigin(mGeolocationWatchID, origin);
-        }
     }
     return true;
 }
