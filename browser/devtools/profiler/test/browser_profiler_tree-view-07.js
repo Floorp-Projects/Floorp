6e1d5dd352375d157d399ace7709dff3c8672107
Christian Pommranz <christian.pommranz@gmail.com>
2014-10-28 05:21:00 -0400

    Bug 1080911 - Sort frames by samples instead of duration in the call tree. r=vp

--- browser/devtools/profiler/test/browser_profiler_tree-view-07.js
+++ browser/devtools/profiler/test/browser_profiler_tree-view-07.js
@@ -19,18 +19,18 @@ function test() {
 
   let A = treeRoot.getChild();
   let B = A.getChild();
-  let C = B.getChild();
+  let D = B.getChild();
 
-  is(C.root, treeRoot,
-    "The .A.B.C node has the correct root.");
-  is(C.parent, B,
-    "The .A.B.C node has the correct parent.");
-  is(C.level, 3,
-    "The .A.B.C node has the correct level.");
-  is(C.target.className, "call-tree-item",
-    "The .A.B.C node has the correct target node.");
-  is(C.container.id, "call-tree-container",
-    "The .A.B.C node has the correct container node.");
+  is(D.root, treeRoot,
+    "The .A.B.D node has the correct root.");
+  is(D.parent, B,
+    "The .A.B.D node has the correct parent.");
+  is(D.level, 3,
+    "The .A.B.D node has the correct level.");
+  is(D.target.className, "call-tree-item",
+    "The .A.B.D node has the correct target node.");
+  is(D.container.id, "call-tree-container",
+    "The .A.B.D node has the correct container node.");
 
   finish();
 }
@@ -44,7 +44,7 @@ let gSamples = [{
     { category: 32, location: "C (http://foo/bar/baz:56)" }
   ]
 }, {
-  time: 5 + 6,
+  time: 5 + 1,
   frames: [
     { category: 8,  location: "(root)" },
     { category: 8,  location: "A (http://foo/bar/baz:12)" },
@@ -52,7 +52,15 @@ let gSamples = [{
     { category: 64, location: "D (http://foo/bar/baz:78)" }
   ]
 }, {
-  time: 5 + 6 + 7,
+  time: 5 + 1 + 2,
+  frames: [
+    { category: 8,  location: "(root)" },
+    { category: 8,  location: "A (http://foo/bar/baz:12)" },
+    { category: 16, location: "B (http://foo/bar/baz:34)" },
+    { category: 64, location: "D (http://foo/bar/baz:78)" }
+  ]
+}, {
+  time: 5 + 1 + 2 + 7,
   frames: [
     { category: 8,   location: "(root)" },
     { category: 8,   location: "A (http://foo/bar/baz:12)" },
