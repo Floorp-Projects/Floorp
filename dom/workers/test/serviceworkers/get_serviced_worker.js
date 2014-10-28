b5d8e79f10168ba70f5631e3607ca0f2c9d79d3b
Catalin Badea <catalin.badea392@gmail.com>
2014-10-28 10:29:00 +0100

    Bug 982726 - Patch 1.3 - Fix crash caused by a race condition in PromiseHolder. r=baku

--- dom/workers/test/serviceworkers/get_serviced_worker.js
+++ dom/workers/test/serviceworkers/get_serviced_worker.js
@@ -8,9 +8,3 @@ onactivate = function(e) {
   // spam getServiced until the worker is closed.
   loop();
 }
-
-onclose = function(e) {
-  for (var i = 0; i < 100; ++i) {
-    self.clients.getServiced();
-  }
-}
