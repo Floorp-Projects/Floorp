500da3d0b8ce39b3a2ece8265f5aeb348abe9db1
Andrew Halberstadt <ahalberstadt@mozilla.com>
2014-10-28 17:37:24 -0400

    Bug 1080764 - Add 'anon' and 'anon attribute' search strategies to marionette.find_element(), r=AutomatedTester
    The 'anon' search strategy maps directly to nsIXULDocument.getAnonymousNodes() whereas the 'anon attribute'
    strategy maps to nsIXULDocument.getAnonymousElementByAttribute(). These strategies are needed for clients who
    wish to find and manipulate anonymous content, typically found in the Firefox chrome. For more details, see:
    https://developer.mozilla.org/en-US/docs/XBL/XBL_1.0_Reference/Anonymous_Content

--- testing/marionette/client/marionette/tests/unit/test_anonymous_content.py
+++ testing/marionette/client/marionette/tests/unit/test_anonymous_content.py
@@ -2,10 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from marionette import HTMLElement
 from marionette_test import MarionetteTestCase
-from errors import JavascriptException, NoSuchElementException
+from errors import NoSuchElementException
+from expected import element_present
+from wait import Wait
 
-class TestSwitchFrameChrome(MarionetteTestCase):
+class TestAnonymousContent(MarionetteTestCase):
     def setUp(self):
         MarionetteTestCase.setUp(self)
         self.marionette.set_context("chrome")
@@ -20,7 +23,7 @@ class TestSwitchFrameChrome(MarionetteTestCase):
         self.marionette.switch_to_window(self.win)
         MarionetteTestCase.tearDown(self)
 
-    def test_switch(self):
+    def test_switch_to_anonymous_frame(self):
         self.marionette.find_element("id", "testAnonymousContentBox")
         anon_browser_el = self.marionette.find_element("id", "browser")
         self.assertTrue("test_anonymous_content.xul" in self.marionette.get_url())
@@ -28,3 +31,22 @@ class TestSwitchFrameChrome(MarionetteTestCase):
         self.assertTrue("test.xul" in self.marionette.get_url())
         self.marionette.find_element("id", "testXulBox")
         self.assertRaises(NoSuchElementException, self.marionette.find_element, "id", "testAnonymousContentBox")
+
+    def test_find_anonymous_element_by_attribute(self):
+        el = Wait(self.marionette).until(element_present("id", "dia"))
+        self.assertEquals(HTMLElement, type(el.find_element("anon attribute", {"anonid": "buttons"})))
+        self.assertEquals(1, len(el.find_elements("anon attribute", {"anonid": "buttons"})))
+
+        with self.assertRaises(NoSuchElementException):
+            el.find_element("anon attribute", {"anonid": "nonexistent"})
+        self.assertEquals([], el.find_elements("anon attribute", {"anonid": "nonexistent"}))
+
+    def test_find_anonymous_children(self):
+        el = Wait(self.marionette).until(element_present("id", "dia"))
+        self.assertEquals(HTMLElement, type(el.find_element("anon", None)))
+        self.assertEquals(2, len(el.find_elements("anon", None)))
+
+        el = self.marionette.find_element("id", "framebox")
+        with self.assertRaises(NoSuchElementException):
+            el.find_element("anon", None)
+        self.assertEquals([], el.find_elements("anon", None))
