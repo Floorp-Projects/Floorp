d49ddfe09057541fdb5bd86a27bbef091dd56542
Andrea Marchesini <amarchesini@mozilla.com>
2014-10-29 16:54:35 +0000

    Bug 1090183 - WebSockets should work in SharedWorkers, r=smaug

---
+++ dom/workers/test/webSocket_sharedWorker.js
@@ -0,0 +1,20 @@
+onconnect = function(evt) {
+  var ws = new WebSocket("ws://mochi.test:8888/tests/dom/base/test/file_websocket_hello");
+
+  ws.onopen = function(e) {
+    evt.ports[0].postMessage({type: 'status', status: true, msg: 'OnOpen called' });
+    ws.send("data");
+  }
+
+  ws.onclose = function(e) {}
+
+  ws.onerror = function(e) {
+    evt.ports[0].postMessage({type: 'status', status: false, msg: 'onerror called!'});
+  }
+
+  ws.onmessage = function(e) {
+    evt.ports[0].postMessage({type: 'status', status: e.data == 'Hello world!', msg: 'Wrong data'});
+    ws.close();
+    evt.ports[0].postMessage({type: 'finish' });
+  }
+}
