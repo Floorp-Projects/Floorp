36dcb7cbc909144b0f1895beb26b43e831688188
Phil Ringnalda <philringnalda@gmail.com>
2014-10-28 23:26:07 -0700

    Back out 8a1dfdaaeb3c (bug 1063156) for Android bustage CLOSED TREE

--- toolkit/xre/nsXREDirProvider.cpp
+++ toolkit/xre/nsXREDirProvider.cpp
@@ -551,7 +551,9 @@ nsXREDirProvider::GetFiles(const char* aProperty, nsISimpleEnumerator** aResult)
 static void
 RegisterExtensionInterpositions(nsINIParser &parser)
 {
-#ifdef NIGHTLY_BUILD
+  if (!mozilla::BrowserTabsRemoteAutostart())
+    return;
+
   nsCOMPtr<nsIAddonInterposition> interposition =
     do_GetService("@mozilla.org/addons/multiprocess-shims;1");
 
@@ -570,7 +572,6 @@ RegisterExtensionInterpositions(nsINIParser &parser)
       continue;
   }
   while (true);
-#endif
 }
 
 static void
