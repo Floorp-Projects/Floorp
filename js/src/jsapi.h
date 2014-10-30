563f1538f6ff49e8bcdf1065a5104de0abef1096
Boris Zbarsky <bzbarsky@mit.edu>
2014-10-30 19:40:28 -0400

    Bug 1089026 part 1.  Eliminate the "parent" argument to JS_CloneFunctionObject to make callers use the scopeChain version if they want something other than the global.  r=shu,peterv

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -3527,14 +3527,14 @@ extern JS_PUBLIC_API(JSFunction *)
 JS_DefineFunctionById(JSContext *cx, JS::Handle<JSObject*> obj, JS::Handle<jsid> id, JSNative call,
                       unsigned nargs, unsigned attrs);
 
+namespace JS {
+
 /*
- * Clone a top-level function into a new scope. This function will dynamically
+ * Clone a top-level function into cx's global. This function will dynamically
  * fail if funobj was lexically nested inside some other function.
  */
 extern JS_PUBLIC_API(JSObject *)
-JS_CloneFunctionObject(JSContext *cx, JS::Handle<JSObject*> funobj, JS::Handle<JSObject*> parent);
-
-namespace JS {
+CloneFunctionObject(JSContext *cx, HandleObject funobj);
 
 /*
  * As above, but providing an explicit scope chain.  scopeChain must not include
