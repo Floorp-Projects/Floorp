d8a6a701fe5f7a1a6e845734b8af488bdddd9923
Boris Zbarsky <bzbarsky@mit.edu>
2014-10-30 17:43:42 -0400

    Bug 1088228 part 6.  Simplify the DOM GetParentObject setup, now that we no longer rely on it for scope chains.  r=peterv

--- dom/html/nsGenericHTMLElement.cpp
+++ dom/html/nsGenericHTMLElement.cpp
@@ -1896,16 +1896,6 @@ NS_IMPL_ISUPPORTS_INHERITED(nsGenericHTMLFormElement,
                             nsGenericHTMLElement,
                             nsIFormControl)
 
-mozilla::dom::ParentObject
-nsGenericHTMLFormElement::GetParentObject() const
-{
-  // We use the parent chain to implement the scope for event handlers.
-  if (mForm) {
-    return GetParentObjectInternal(mForm);
-  }
-  return nsGenericHTMLElement::GetParentObject();
-}
-
 nsINode*
 nsGenericHTMLFormElement::GetScopeChainParent() const
 {
