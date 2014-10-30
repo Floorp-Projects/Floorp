0412c79a706809ee318b1976754178997dbb00e9
Gregory Szorc <gps@mozilla.com>
2014-10-30 10:37:00 -0700

    Bug 1091722 - Don't require configure before generating docs; r=glandium
    Ever since Sphinx variable reading operates at the AST level (bug
    1071012), we don't technically need a fully configured environment in
    order to generate the documentation!
    
    This patch stubs out the config environment object with a fake one that
    provides the only needed context to generate the Sphinx docs. This
    allows us to build the Sphinx docs on a fresh clone of the tree with no
    configure and with no object directory.

--- tools/docs/mach_commands.py
+++ tools/docs/mach_commands.py
@@ -43,8 +43,14 @@ class Documentation(MachCommandBase):
         def remove_gyp_dirs(context):
             context['GYP_DIRS'][:] = []
 
-        reader = BuildReader(self.config_environment,
-            sandbox_post_eval_cb=remove_gyp_dirs)
+        # Reading the Sphinx variables doesn't require a full build context.
+        # Only define the parts we need.
+        class fakeconfig(object):
+            def __init__(self, topsrcdir):
+                self.topsrcdir = topsrcdir
+
+        config = fakeconfig(self.topsrcdir)
+        reader = BuildReader(config)
 
         for path, name, key, value in reader.find_sphinx_variables():
             reldir = os.path.dirname(path)
