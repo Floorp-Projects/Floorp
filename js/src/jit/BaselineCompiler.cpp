4934cbb089344d1b95a59860603f31023963c46a
Luke Wagner <luke@mozilla.com>
2014-10-30 17:35:35 -0500

    Bug 1091916 - simplify the interrupt/jitStackLimit situation (r=bhackett)
    --HG--
    extra : rebase_source : 4acdf2009f9f490f636b29ab1c3eeb3de3a12f75

--- js/src/jit/BaselineCompiler.cpp
+++ js/src/jit/BaselineCompiler.cpp
@@ -496,7 +496,7 @@ bool
 BaselineCompiler::emitStackCheck(bool earlyCheck)
 {
     Label skipCall;
-    uintptr_t *limitAddr = &cx->runtime()->mainThread.jitStackLimit;
+    void *limitAddr = cx->runtime()->mainThread.addressofJitStackLimit();
     uint32_t slotsSize = script->nslots() * sizeof(Value);
     uint32_t tolerance = earlyCheck ? slotsSize : 0;
 
@@ -646,7 +646,7 @@ BaselineCompiler::emitInterruptCheck()
     frame.syncStack(0);
 
     Label done;
-    void *interrupt = (void *)&cx->runtime()->interrupt;
+    void *interrupt = cx->runtimeAddressOfInterruptUint32();
     masm.branch32(Assembler::Equal, AbsoluteAddress(interrupt), Imm32(0), &done);
 
     prepareVMCall();
