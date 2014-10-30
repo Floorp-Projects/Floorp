284a2785bec00f053450baa5ed3c36a74750ec20
Wes Kocher <wkocher@mozilla.com>
2014-10-30 16:24:02 -0700

    Backed out 6 changesets (bug 1088228) for static analysis bustage
    Backed out changeset 13ecff800114 (bug 1088228)
    Backed out changeset de692c3335f2 (bug 1088228)
    Backed out changeset 2d449a2b4e1c (bug 1088228)
    Backed out changeset 49ac8f33ab70 (bug 1088228)
    Backed out changeset 920d50e84a17 (bug 1088228)
    Backed out changeset 55f4818378e4 (bug 1088228)

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -500,13 +500,6 @@ Element::WrapObject(JSContext *aCx)
   return obj;
 }
 
-/* virtual */
-nsINode*
-Element::GetScopeChainParent() const
-{
-  return OwnerDoc();
-}
-
 nsDOMTokenList*
 Element::ClassList()
 {
--- dom/html/HTMLLegendElement.h
+++ dom/html/HTMLLegendElement.h
@@ -54,7 +54,7 @@ public:
 
   virtual nsresult Clone(mozilla::dom::NodeInfo* aNodeInfo, nsINode** aResult) const MOZ_OVERRIDE;
 
-  Element* GetFormElement() const
+  Element* GetFormElement()
   {
     nsCOMPtr<nsIFormControl> fieldsetControl = do_QueryInterface(GetFieldSet());
 
@@ -77,10 +77,10 @@ public:
     SetHTMLAttr(nsGkAtoms::align, aAlign, aError);
   }
 
-  nsINode* GetScopeChainParent() const MOZ_OVERRIDE
-  {
+  ParentObject GetParentObject() {
     Element* form = GetFormElement();
-    return form ? form : nsGenericHTMLElement::GetScopeChainParent();
+    return form ? GetParentObjectInternal(form)
+                : nsGenericHTMLElement::GetParentObject();
   }
 
 protected:
@@ -92,7 +92,7 @@ protected:
    * Get the fieldset content element that contains this legend.
    * Returns null if there is no fieldset containing this legend.
    */
-  nsIContent* GetFieldSet() const;
+  nsIContent* GetFieldSet();
 };
 
 } // namespace dom
