88669f53861036c07de820082ace05b8c9c00efb
Ryan VanderMeulen <ryanvm@gmail.com>
2014-10-30 14:55:03 -0400

    Backed out changeset f201b7e443ea (bug 1090385) for bustage.

--- mobile/android/base/webapp/WebappImpl.java
+++ mobile/android/base/webapp/WebappImpl.java
@@ -18,7 +18,6 @@ import org.mozilla.gecko.GeckoThread;
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.Tab;
 import org.mozilla.gecko.Tabs;
-import org.mozilla.gecko.mozglue.ContextUtils.SafeIntent;
 import org.mozilla.gecko.util.NativeJSObject;
 import org.mozilla.gecko.webapp.InstallHelper.InstallCallback;
 
@@ -158,7 +157,7 @@ public class WebappImpl extends GeckoApp implements InstallCallback {
     }
 
     @Override
-    protected String getURIFromIntent(SafeIntent intent) {
+    protected String getURIFromIntent(Intent intent) {
         String uri = super.getURIFromIntent(intent);
         if (uri != null) {
             return uri;
