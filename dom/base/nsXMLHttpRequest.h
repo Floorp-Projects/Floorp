5d10f271037704b32991be8093d4d16832cfc9ba
Ryan VanderMeulen <ryanvm@gmail.com>
2014-10-31 13:58:52 -0400

    Backed out changeset 1cce9c56538f (bug 1087633) for static analysis bustage.

--- dom/base/nsXMLHttpRequest.h
+++ dom/base/nsXMLHttpRequest.h
@@ -27,7 +27,6 @@
 #include "nsIPrincipal.h"
 #include "nsIScriptObjectPrincipal.h"
 #include "nsISizeOfEventTarget.h"
-#include "nsIXPConnect.h"
 
 #include "mozilla/Assertions.h"
 #include "mozilla/DOMEventTargetHelper.h"
@@ -464,11 +463,6 @@ public:
   void Send(nsIInputStream* aStream, ErrorResult& aRv)
   {
     NS_ASSERTION(aStream, "Null should go to string version");
-    nsCOMPtr<nsIXPConnectWrappedJS> wjs = do_QueryInterface(aStream);
-    if (wjs) {
-      aRv.Throw(NS_ERROR_DOM_TYPE_ERR);
-      return;
-    }
     aRv = Send(RequestBody(aStream));
   }
   void SendAsBinary(const nsAString& aBody, ErrorResult& aRv);
