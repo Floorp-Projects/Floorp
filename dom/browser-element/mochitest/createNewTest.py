56d3253de4f1fa576e22cf94257b8c219892f8f4
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2014-10-31 10:39:15 +0800

    Bug 978681 - Fix createNewTest.py to use mochitest.ini. r=smaug

--- dom/browser-element/mochitest/createNewTest.py
+++ dom/browser-element/mochitest/createNewTest.py
@@ -40,7 +40,7 @@ js_template = textwrap.dedent("""\
 
     function runTest() {{
       var iframe = document.createElement('iframe');
-      SpecialPowers.wrap(iframe).mozbrowser = true;
+      iframe.setAttribute('mozbrowser', 'true');
 
       // FILL IN TEST
 
@@ -53,36 +53,39 @@ js_template = textwrap.dedent("""\
 def print_fill(s):
     print(textwrap.fill(textwrap.dedent(s)))
 
-def add_to_makefile(filenames):
-    """Add a list of filenames to this directory's Makefile.in, then open
-    $EDITOR and let the user move the filenames to their appropriate places in
-    the file.
+def add_to_ini(filename, test, support_file=None):
+    """Add test to mochitest config {filename}, then open
+    $EDITOR and let the user move the filenames to their appropriate
+    places in the file.
 
     """
-    lines_to_write = [''] + ['\t\t%s \\' % n for n in filenames]
-    with open('Makefile.in', 'a') as f:
+    lines_to_write = ['[{0}]'.format(test)]
+    if support_file:
+        lines_to_write += ['support-files = {0}'.format(support_file)]
+    lines_to_write += ['']
+    with open(filename, 'a') as f:
         f.write('\n'.join(lines_to_write))
 
     if 'EDITOR' not in os.environ or not os.environ['EDITOR']:
         print_fill("""\
-            Now open Makefile.in and move the filenames to their correct places.")
-            (Define $EDITOR and I'll open your editor for you next time.)""")
+            Now open {filename} and move the filenames to their correct places.")
+            (Define $EDITOR and I'll open your editor for you next time.)""".format(filename=filename))
         return
 
-    # Count the number of lines in Makefile.in.
-    with open('Makefile.in', 'r') as f:
+    # Count the number of lines in the file
+    with open(filename, 'r') as f:
         num_lines = len(f.readlines())
 
     try:
         subprocess.call([os.environ['EDITOR'],
                          '+%d' % (num_lines - len(lines_to_write) + 2),
-                         'Makefile.in'])
+                         filename])
     except Exception as e:
-        print_fill("Error opening $EDITOR: %s." % str(e))
+        print_fill("Error opening $EDITOR: {0}.".format(e))
         print()
         print_fill("""\
-            Please open Makefile.in and move the filenames at the bottom of the
-            file to their correct places.""")
+            Please open {filename} and move the filenames at the bottom of the
+            file to their correct places.""".format(filename=filename))
 
 def main(test_name, bug_number):
     global html_template, js_template
@@ -109,9 +112,11 @@ def main(test_name, bug_number):
     create_file('test_browserElement_inproc_{test}.html', html_template)
     create_file('test_browserElement_oop_{test}.html', html_template)
 
-    add_to_makefile([format(x) for x in ['browserElement_{test}.js',
-                                         'test_browserElement_inproc_{test}.html',
-                                         'test_browserElement_oop_{test}.html']])
+    add_to_ini('mochitest.ini',
+               format('test_browserElement_inproc_{test}.html'),
+               format('browserElement_{test}.js'))
+    add_to_ini('mochitest-oop.ini',
+               format('test_browserElement_oop_{test}.html'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Create a new browser-element testcase.")
