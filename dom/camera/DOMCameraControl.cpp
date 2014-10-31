357aa1e2dee42724dc6c56ecbe9a632ae003a111
Ryan VanderMeulen <ryanvm@gmail.com>
2014-10-31 19:15:42 -0400

    Backed out changesets 4cb1154b0190 and 506d33f3931e (bug 1090501) because it depends on another patch that was backed out.

--- dom/camera/DOMCameraControl.cpp
+++ dom/camera/DOMCameraControl.cpp
@@ -46,12 +46,6 @@ using namespace mozilla;
 using namespace mozilla::dom;
 using namespace mozilla::ipc;
 
-#ifdef MOZ_WIDGET_GONK
-StaticRefPtr<ICameraControl> nsDOMCameraControl::sCachedCameraControl;
-/* static */ nsresult nsDOMCameraControl::sCachedCameraControlStartResult = NS_OK;
-/* static */ nsCOMPtr<nsITimer> nsDOMCameraControl::sDiscardCachedCameraControlTimer;
-#endif
-
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION_INHERITED(nsDOMCameraControl)
   NS_INTERFACE_MAP_ENTRY(nsISupports)
 NS_INTERFACE_MAP_END_INHERITING(DOMMediaStream)
@@ -150,60 +144,6 @@ nsDOMCameraControl::DOMCameraConfiguration::~DOMCameraConfiguration()
   MOZ_COUNT_DTOR(nsDOMCameraControl::DOMCameraConfiguration);
 }
 
-#ifdef MOZ_WIDGET_GONK
-// This shoudl be long enough for even our slowest platforms.
-static const unsigned long kCachedCameraTimeoutMs = 3500;
-
-// Open the battery-door-facing camera by default.
-static const uint32_t kDefaultCameraId = 0;
-
-/* static */ void
-nsDOMCameraControl::PreinitCameraHardware()
-{
-  // Assume a default, minimal configuration. This should initialize the
-  // hardware, but won't (can't) start the preview.
-  nsRefPtr<ICameraControl> cameraControl = ICameraControl::Create(kDefaultCameraId);
-  if (NS_WARN_IF(!cameraControl)) {
-    return;
-  }
-
-  sCachedCameraControlStartResult = cameraControl->Start();
-  if (NS_WARN_IF(NS_FAILED(sCachedCameraControlStartResult))) {
-    return;
-  }
-
-  sCachedCameraControl = cameraControl;
-
-  nsCOMPtr<nsITimer> timer = do_CreateInstance(NS_TIMER_CONTRACTID);
-  if (NS_WARN_IF(!timer)) {
-    return;
-  }
-
-  nsresult rv = timer->InitWithFuncCallback(DiscardCachedCameraInstance,
-                                            nullptr,
-                                            kCachedCameraTimeoutMs,
-                                            nsITimer::TYPE_ONE_SHOT);
-  if (NS_WARN_IF(NS_FAILED(rv))) {
-    // If we can't start the timer, it's possible for an app to never grab the
-    // camera, leaving the hardware tied up indefinitely. Better to take the
-    // performance hit.
-    sCachedCameraControl = nullptr;
-    return;
-  }
-
-  sDiscardCachedCameraControlTimer = timer;
-}
-
-/* static */ void
-nsDOMCameraControl::DiscardCachedCameraInstance(nsITimer* aTimer, void* aClosure)
-{
-  MOZ_ASSERT(NS_IsMainThread());
-
-  sDiscardCachedCameraControlTimer = nullptr;
-  sCachedCameraControl = nullptr;
-}
-#endif
-
 nsDOMCameraControl::nsDOMCameraControl(uint32_t aCameraId,
                                        const CameraConfiguration& aInitialConfig,
                                        GetCameraCallback* aOnSuccess,
@@ -274,19 +214,7 @@ nsDOMCameraControl::nsDOMCameraControl(uint32_t aCameraId,
     config.mRecorderProfile = aInitialConfig.mRecorderProfile;
   }
 
-#ifdef MOZ_WIDGET_GONK
-  bool gotCached = false;
-  if (sCachedCameraControl && aCameraId == kDefaultCameraId) {
-    mCameraControl = sCachedCameraControl;
-    sCachedCameraControl = nullptr;
-    gotCached = true;
-  } else {
-    sCachedCameraControl = nullptr;
-#endif
-    mCameraControl = ICameraControl::Create(aCameraId);
-#ifdef MOZ_WIDGET_GONK
-  }
-#endif
+  mCameraControl = ICameraControl::Create(aCameraId);
   mCurrentConfiguration = initialConfig.forget();
 
   // Attach our DOM-facing media stream to our viewfinder stream.
@@ -300,20 +228,12 @@ nsDOMCameraControl::nsDOMCameraControl(uint32_t aCameraId,
   mListener = new DOMCameraControlListener(this, mInput);
   mCameraControl->AddListener(mListener);
 
-#ifdef MOZ_WIDGET_GONK
-  if (!gotCached || NS_FAILED(sCachedCameraControlStartResult)) {
-#endif
-    // Start the camera...
-    if (haveInitialConfig) {
-      rv = mCameraControl->Start(&config);
-    } else {
-      rv = mCameraControl->Start();
-    }
-#ifdef MOZ_WIDGET_GONK
+  // Start the camera...
+  if (haveInitialConfig) {
+    rv = mCameraControl->Start(&config);
   } else {
-    rv = mCameraControl->SetConfiguration(config);
+    rv = mCameraControl->Start();
   }
-#endif
   if (NS_FAILED(rv)) {
     mListener->OnUserError(DOMCameraControlListener::kInStartCamera, rv);
   }
