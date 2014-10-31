c298796f2a9b468b47ff034927af8fb3ae0c20b8
Michael Ratcliffe <mratcliffe@mozilla.com>
2014-10-31 12:47:28 +0000

    Bug 1072181 part 3 - New Aurora channel prefs r=paul

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1148,7 +1148,12 @@ pref("toolkit.crashreporter.infoURL",
 pref("app.support.baseURL", "https://support.mozilla.org/1/firefox/%VERSION%/%OS%/%LOCALE%/");
 
 // base url for web-based feedback pages
+#ifdef MOZ_DEV_EDITION
+pref("app.feedback.baseURL", "https://input.mozilla.org/%LOCALE%/feedback/firefoxdev/%VERSION%/");
+#else
 pref("app.feedback.baseURL", "https://input.mozilla.org/%LOCALE%/feedback/%APP%/%VERSION%/");
+#endif
+
 
 // Name of alternate about: page for certificate errors (when undefined, defaults to about:neterror)
 pref("security.alternate_certificate_error_page", "certerror");
@@ -1382,7 +1387,11 @@ pref("devtools.debugger.ui.variables-searchbox-visible", false);
 
 // Enable the Profiler and the Timeline
 pref("devtools.profiler.enabled", true);
+#ifdef MOZ_DEV_EDITION
+pref("devtools.timeline.enabled", true);
+#else
 pref("devtools.timeline.enabled", false);
+#endif
 
 // Enable perftools via build command
 #ifdef MOZ_DEVTOOLS_PERFTOOLS
