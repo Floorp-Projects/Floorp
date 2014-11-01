9d2629b8ab3c32aab31f137c6f7339dc4e36e519
Bas Schouten <bschouten@mozilla.com>
2014-11-01 14:02:20 -0400

    Bug 1064864. Ensure the copying bounds are sane. r=jrmuizel a=sylvestre
    This prevents things from going really bad on some drivers.

--- gfx/layers/d3d11/CompositorD3D11.cpp
+++ gfx/layers/d3d11/CompositorD3D11.cpp
@@ -468,7 +468,11 @@ CompositorD3D11::CreateRenderTargetFromSource(const gfx::IntRect &aRect,
     const IntSize& srcSize = sourceD3D11->GetSize();
     MOZ_ASSERT(srcSize.width >= 0 && srcSize.height >= 0,
                "render targets should have nonnegative sizes");
-    if (srcBox.right <= static_cast<uint32_t>(srcSize.width) &&
+    if (srcBox.left >= 0 &&
+        srcBox.top >= 0 &&
+        srcBox.left < srcBox.right &&
+        srcBox.top < srcBox.bottom &&
+        srcBox.right <= static_cast<uint32_t>(srcSize.width) &&
         srcBox.bottom <= static_cast<uint32_t>(srcSize.height)) {
       mContext->CopySubresourceRegion(texture, 0,
                                       0, 0, 0,
