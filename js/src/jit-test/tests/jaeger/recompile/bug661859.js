a5a8ab9659d1403d440325674909243be1128c26
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2014-11-04 11:16:06 +0100

    Bug 1087468 - IonMonkey: Inline functions with loops based on their arguments. r=h4writer

--- js/src/jit-test/tests/jaeger/recompile/bug661859.js
+++ js/src/jit-test/tests/jaeger/recompile/bug661859.js
@@ -7,9 +7,15 @@ var summary = 'Do not assert: top < ss->printer->script->depth';
 var actual = 'No Crash';
 var expect = 'No Crash';
 test();
-function test() {
+function notInlined(f) {
+    // prevent inlining this function, as a consequence, it prevent inlining
+    // Array.prototype.some (Bug 1087468)
+    with ({}) {}
+    return f;
+}
+function test(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z) {
     try {
-        p = [1].some(function (y) test()) ? 4 : 0x0041
+        p = [1].some(notInlined(function (y) test())) ? 4 : 0x0041;
     } catch (ex) {}
     reportCompare(expect, actual, summary)
 }
