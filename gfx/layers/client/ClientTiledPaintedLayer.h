95de67426b2a7b66e9f16686d7b04f4b992fd15c
Matt Woodrow <mwoodrow@mozilla.com>
2014-11-04 15:12:06 +1300

    Bug 1086642 - Simplify invalid reigon by area. r=jrmuizel

--- gfx/layers/client/ClientTiledPaintedLayer.h
+++ gfx/layers/client/ClientTiledPaintedLayer.h
@@ -55,6 +55,7 @@ public:
   virtual Layer* AsLayer() { return this; }
   virtual void InvalidateRegion(const nsIntRegion& aRegion) {
     mInvalidRegion.Or(mInvalidRegion, aRegion);
+    mInvalidRegion.SimplifyOutwardByArea(200*200);
     mInvalidRegion.SimplifyOutward(20);
     mValidRegion.Sub(mValidRegion, mInvalidRegion);
     mLowPrecisionValidRegion.Sub(mLowPrecisionValidRegion, mInvalidRegion);
