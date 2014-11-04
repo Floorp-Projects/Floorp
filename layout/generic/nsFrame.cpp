81efa1ff0fb5345edbd7c886b905cb27015726f0
Xidorn Quan [:xidorn] <quanxunzhen@gmail.com>
2014-11-04 15:45:00 +0100

    Bug 1085311 - Vertical writing-mode support for single-line <input> fields. r=jfkthame

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -3800,9 +3800,13 @@ nsFrame::GetCursor(const nsPoint& aPoint,
     // If this is editable, I-beam cursor is better for most elements.
     aCursor.mCursor =
       (mContent && mContent->IsEditable())
-      ? GetWritingMode().IsVertical()
-        ? NS_STYLE_CURSOR_VERTICAL_TEXT : NS_STYLE_CURSOR_TEXT
-      : NS_STYLE_CURSOR_DEFAULT;
+      ? NS_STYLE_CURSOR_TEXT : NS_STYLE_CURSOR_DEFAULT;
+  }
+  if (NS_STYLE_CURSOR_TEXT == aCursor.mCursor &&
+      GetWritingMode().IsVertical()) {
+    // Per CSS UI spec, UA may treat value 'text' as
+    // 'vertical-text' for vertical text.
+    aCursor.mCursor = NS_STYLE_CURSOR_VERTICAL_TEXT;
   }
 
   return NS_OK;
