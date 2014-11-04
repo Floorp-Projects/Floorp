34465bd8aabfcdfa28954a19ac3a4b3e9aae6c42
Michael Comella <michael.l.comella@gmail.com>
2014-11-04 04:02:13 -0800

    Bug 1086981 - Part 6: Drop keyboard when clicking the menu button. r=lucasr

--- mobile/android/base/toolbar/BrowserToolbar.java
+++ mobile/android/base/toolbar/BrowserToolbar.java
@@ -345,6 +345,8 @@ public abstract class BrowserToolbar extends ThemedRelativeLayout
             menuButton.setOnClickListener(new Button.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    // Drop the soft keyboard.
+                    urlEditLayout.clearFocus();
                     activity.openOptionsMenu();
                 }
             });
