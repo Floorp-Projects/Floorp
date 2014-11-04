b56fe2065f5ea3a6e6e26c13974cc204e7c5c94a
Hannes Verschore <hv1989@gmail.com>
2014-11-04 09:43:00 +0100

    Bug 1052839 - Selfhost substr/slice/substring, r=waldo,till

--- js/src/jit/Lowering.cpp
+++ js/src/jit/Lowering.cpp
@@ -2154,6 +2154,16 @@ LIRGenerator::visitStringReplace(MStringReplace *ins)
 }
 
 bool
+LIRGenerator::visitSubstr(MSubstr *ins)
+{
+    LSubstr *lir = new (alloc()) LSubstr(useFixed(ins->string(), CallTempReg1),
+                                         useRegister(ins->begin()),
+                                         useRegister(ins->length()),
+                                         temp());
+    return define(lir, ins) && assignSafepoint(lir, ins);
+}
+
+bool
 LIRGenerator::visitLambda(MLambda *ins)
 {
     if (ins->info().singletonType || ins->info().useNewTypeForClone) {
