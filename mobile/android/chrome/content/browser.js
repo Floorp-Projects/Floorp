bb6901bdc179820a23d3445f0a1b29071b3beb90
Kartikaya Gupta <kgupta@mozilla.com>
2014-11-04 09:53:17 -0500

    Bug 1078029 - Dispatch a dummy mouse event so that browser.js can pick up the event's retargeted target. r=wesj, a=lsblakk

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -4872,6 +4872,7 @@ var BrowserEventHandler = {
 
     BrowserApp.deck.addEventListener("DOMUpdatePageReport", PopupBlockerObserver.onUpdatePageReport, false);
     BrowserApp.deck.addEventListener("touchstart", this, true);
+    BrowserApp.deck.addEventListener("MozMouseHittest", this, true);
     BrowserApp.deck.addEventListener("click", InputWidgetHelper, true);
     BrowserApp.deck.addEventListener("click", SelectHelper, true);
 
@@ -4904,6 +4905,9 @@ var BrowserEventHandler = {
       case 'touchstart':
         this._handleTouchStart(aEvent);
         break;
+      case 'MozMouseHittest':
+        this._handleRetargetedTouchStart(aEvent);
+        break;
       case 'MozMagnifyGesture':
         this.observe(this, aEvent.type,
                      JSON.stringify({x: aEvent.screenX, y: aEvent.screenY,
@@ -4935,6 +4939,19 @@ var BrowserEventHandler = {
       if (this._scrollableElement != rootScrollable) {
         Messaging.sendRequest({ type: "Panning:Override" });
       }
+    }
+  },
+
+  _handleRetargetedTouchStart: function(aEvent) {
+    // we should only get this called just after a new touchstart with a single
+    // touch point.
+    if (!BrowserApp.isBrowserContentDocumentDisplayed() || aEvent.defaultPrevented) {
+      return;
+    }
+
+    let target = aEvent.target;
+    if (!target) {
+      return;
     }
 
     let uri = this._getLinkURI(target);
