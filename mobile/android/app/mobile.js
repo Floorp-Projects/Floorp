dd2f8d24143697dd12977bbe9f0a6cc604cf48e5
Brian Nicholson <bnicholson@mozilla.com>
2014-11-05 12:14:34 -0800

    Bug 1068425 - Implement tile reporting components. r=rnewman, a=lsblakk

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -663,6 +663,11 @@ pref("urlclassifier.gethash.timeout_ms", 5000);
 pref("urlclassifier.max-complete-age", 2700);
 #endif
 
+// URL for posting tiles metrics.
+#ifdef RELEASE_BUILD
+pref("browser.tiles.reportURL", "https://tiles.services.mozilla.com/v2/links/click");
+#endif
+
 // True if this is the first time we are showing about:firstrun
 pref("browser.firstrun.show.uidiscovery", true);
 pref("browser.firstrun.show.localepicker", false);
