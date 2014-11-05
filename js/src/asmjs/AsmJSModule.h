18d8f590e6398a14a24b79355949359dd0db6251
Luke Wagner <luke@mozilla.com>
2014-11-05 17:30:30 -0600

    Bug 1094570 - OdinMonkey: tidy up TryEnablingIon (r=hannes)

--- js/src/asmjs/AsmJSModule.h
+++ js/src/asmjs/AsmJSModule.h
@@ -1280,6 +1280,7 @@ class AsmJSModule
         MOZ_ASSERT(isFinished());
         return pc >= code_ && pc < (code_ + codeBytes());
     }
+  private:
     uint8_t *interpExitTrampoline(const Exit &exit) const {
         MOZ_ASSERT(isFinished());
         MOZ_ASSERT(exit.interpCodeOffset_);
@@ -1290,6 +1291,7 @@ class AsmJSModule
         MOZ_ASSERT(exit.ionCodeOffset_);
         return code_ + exit.ionCodeOffset_;
     }
+  public:
 
     // Lookup a callsite by the return pc (from the callee to the caller).
     // Return null if no callsite was found.
@@ -1416,6 +1418,17 @@ class AsmJSModule
     ExitDatum &exitIndexToGlobalDatum(unsigned exitIndex) const {
         MOZ_ASSERT(isFinished());
         return *(ExitDatum *)(globalData() + exitIndexToGlobalDataOffset(exitIndex));
+    }
+    bool exitIsOptimized(unsigned exitIndex) const {
+        MOZ_ASSERT(isFinished());
+        ExitDatum &exitDatum = exitIndexToGlobalDatum(exitIndex);
+        return exitDatum.exit != interpExitTrampoline(exit(exitIndex));
+    }
+    void optimizeExit(unsigned exitIndex, jit::IonScript *ionScript) const {
+        MOZ_ASSERT(!exitIsOptimized(exitIndex));
+        ExitDatum &exitDatum = exitIndexToGlobalDatum(exitIndex);
+        exitDatum.exit = ionExitTrampoline(exit(exitIndex));
+        exitDatum.ionScript = ionScript;
     }
     void detachIonCompilation(size_t exitIndex) const {
         MOZ_ASSERT(isFinished());
