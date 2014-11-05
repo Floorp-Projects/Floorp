40db9b80ade86c6704726abd4a23d85c2c3d852c
David Keeler <dkeeler@mozilla.com>
2014-11-05 13:54:21 -0800

    bug 1039642 - test that smart card events are no longer emitted after removing a PKCS#11 module r=jcj r=mmc a=metered

---
+++ security/manager/ssl/tests/unit/test_pkcs11_no_events_after_removal.js
@@ -0,0 +1,33 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// This test loads a testing PKCS #11 module that simulates a token being
+// inserted and removed from a slot every 50ms. This causes the observer
+// service to broadcast the observation topics "smartcard-insert" and
+// "smartcard-remove", respectively. This test ensures that these events
+// are no longer emitted once the module has been unloaded.
+
+// Ensure that the appropriate initialization has happened.
+do_get_profile();
+Cc["@mozilla.org/psm;1"].getService(Ci.nsISupports);
+
+let { Services } = Cu.import("resource://gre/modules/Services.jsm", {});
+
+function run_test() {
+  let pkcs11 = Cc["@mozilla.org/security/pkcs11;1"].getService(Ci.nsIPKCS11);
+  let libraryName = ctypes.libraryName("pkcs11testmodule");
+  let libraryFile = Services.dirsvc.get("CurWorkD", Ci.nsILocalFile);
+  libraryFile.append("pkcs11testmodule");
+  libraryFile.append(libraryName);
+  ok(libraryFile.exists());
+  pkcs11.addModule("PKCS11 Test Module", libraryFile.path, 0, 0);
+  pkcs11.deleteModule("PKCS11 Test Module");
+  Services.obs.addObserver(function() { do_check_true(false); },
+                           "smartcard-insert", false);
+  Services.obs.addObserver(function() { do_check_true(false); },
+                           "smartcard-remove", false);
+  do_timeout(500, do_test_finished);
+  do_test_pending();
+}
