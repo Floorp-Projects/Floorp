06820eb21698b9bcd51edaf040ef12143abee32b
Jean-Yves Avenard <jyavenard@mozilla.com>
2014-11-06 15:09:19 +1100

    Bug 1093880 - Delay decoder initialization until data has been appended. r=cajbir

--- dom/media/mediasource/TrackBuffer.h
+++ dom/media/mediasource/TrackBuffer.h
@@ -82,12 +82,15 @@ public:
 #endif
 
 private:
+  friend class DecodersToInitialize;
   ~TrackBuffer();
 
-  // Create a new decoder, set mCurrentDecoder to the new decoder, and queue
-  // the decoder for initialization.  The decoder is not considered
-  // initialized until it is added to mDecoders.
-  bool NewDecoder();
+  // Create a new decoder, set mCurrentDecoder to the new decoder and
+  // returns it. The new decoder must be queued using QueueInitializeDecoder
+  // for initialization.
+  // The decoder is not considered initialized until it is added to
+  // mInitializedDecoders.
+  already_AddRefed<SourceBufferDecoder> NewDecoder();
 
   // Helper for AppendData, ensures NotifyDataArrived is called whenever
   // data is appended to the current decoder's SourceBufferResource.
