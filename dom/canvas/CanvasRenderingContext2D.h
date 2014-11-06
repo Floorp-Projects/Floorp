4b9eaff168d7f487b70af978cac2ceaa8f77bcad
chiajung hung <chung@mozilla.com>
2014-11-06 23:28:00 +0100

    Bug 880114 - Enhance render video-to-SkiaGL performance by GPU-based color space conversioni.  r=gw280

--- dom/canvas/CanvasRenderingContext2D.h
+++ dom/canvas/CanvasRenderingContext2D.h
@@ -744,6 +744,10 @@ protected:
 
   RenderingMode mRenderingMode;
 
+  // Texture informations for fast video rendering
+  unsigned int mVideoTexture;
+  nsIntSize mCurrentVideoSize;
+
   // Member vars
   int32_t mWidth, mHeight;
 
