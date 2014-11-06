d8e094fb3e8b58d751f2f35f6e3edaa68921399e
Tomasz Kołodziejski <tkolodziejski@mozilla.com>
2014-11-06 09:41:00 -0500

    Bug 1058116 - Pass referrer and isPrivate to openURIInFrame instead of nullptr as aOpener. r=mconley, r=smaug, r=Margaret, r=ally

---
+++ dom/base/nsOpenURIInFrameParams.cpp
@@ -0,0 +1,45 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "nsOpenURIInFrameParams.h"
+
+NS_IMPL_ISUPPORTS(nsOpenURIInFrameParams, nsIOpenURIInFrameParams)
+
+nsOpenURIInFrameParams::nsOpenURIInFrameParams() :
+  mIsPrivate(false)
+{
+}
+
+nsOpenURIInFrameParams::~nsOpenURIInFrameParams() {
+}
+
+/* attribute DOMString referrer; */
+NS_IMETHODIMP
+nsOpenURIInFrameParams::GetReferrer(nsAString& aReferrer)
+{
+  aReferrer = mReferrer;
+  return NS_OK;
+}
+NS_IMETHODIMP
+nsOpenURIInFrameParams::SetReferrer(const nsAString& aReferrer)
+{
+  mReferrer = aReferrer;
+  return NS_OK;
+}
+
+/* attribute boolean isPrivate; */
+NS_IMETHODIMP
+nsOpenURIInFrameParams::GetIsPrivate(bool* aIsPrivate)
+{
+  NS_ENSURE_ARG_POINTER(aIsPrivate);
+  *aIsPrivate = mIsPrivate;
+  return NS_OK;
+}
+NS_IMETHODIMP
+nsOpenURIInFrameParams::SetIsPrivate(bool aIsPrivate)
+{
+  mIsPrivate = aIsPrivate;
+  return NS_OK;
+}
