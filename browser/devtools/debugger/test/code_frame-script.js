e529fb80e9f0a6a49267ca62efe17a280b33f4df
Eddy Bruël <ejpbruel@gmail.com>
2014-11-06 10:18:10 +0100

    Bug 1090929 - Enable the break-on-dom-tests;r=mratcliffe

--- browser/devtools/debugger/test/code_frame-script.js
+++ browser/devtools/debugger/test/code_frame-script.js
@@ -1,9 +1,25 @@
 "use strict";
 
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+const { loadSubScript } = Cc['@mozilla.org/moz/jssubscript-loader;1'].
+                          getService(Ci.mozIJSSubScriptLoader);
+
+const EventUtils = {};
+loadSubScript("chrome://marionette/content/EventUtils.js", EventUtils);
+
 dump("Frame script loaded.\n");
 
 addMessageListener("test:call", function (message) {
   dump("Calling function with name " + message.data + ".\n");
 
   XPCNativeWrapper.unwrap(content)[message.data]();
+  sendAsyncMessage("test:call");
+});
+
+addMessageListener("test:click", function (message) {
+  dump("Sending mouse click.\n");
+
+  let target = message.objects.target;
+  EventUtils.synthesizeMouseAtCenter(target, {},
+                                     target.ownerDocument.defaultView);
 });
