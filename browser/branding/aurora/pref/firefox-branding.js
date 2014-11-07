5c9ab0b6c67b6b6cbc6d3d659991496e21ecd920
Gavin Sharp <gavin@gavinsharp.com>
2014-11-07 21:03:55 +0000

    Bug 1091980 - Turn on firstrun experience for 35.0a2; r=jwalker

--- browser/branding/aurora/pref/firefox-branding.js
+++ browser/branding/aurora/pref/firefox-branding.js
@@ -4,9 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// We don't have pages ready for this, so leave these prefs empty for now (bug 648330)
 pref("startup.homepage_override_url","");
-pref("startup.homepage_welcome_url","");
+pref("startup.homepage_welcome_url","https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/");
 // The time interval between checks for a new version (in seconds)
 pref("app.update.interval", 28800); // 8 hours
 // The time interval between the downloading of mar file chunks in the
