0616fa5d4133ab4ce3ec9d1107989ff5ad3724af
Randell Jesup <rjesup@jesup.org>
2014-11-07 17:19:16 -0500

    Bug 1073215: deal with conflict from uplifts rs=merge-fix a=loop-only

--- browser/components/loop/test/mochitest/head.js
+++ browser/components/loop/test/mochitest/head.js
@@ -19,7 +19,7 @@ var gMozLoopAPI;
 function promiseGetMozLoopAPI() {
   return new Promise((resolve, reject) => {
     let loopPanel = document.getElementById("loop-notification-panel");
-    let btn = document.getElementById("loop-call-button");
+    let btn = document.getElementById("loop-button-throttled");
 
     // Wait for the popup to be shown if it's not already, then we can get the iframe and
     // wait for the iframe's load to be completed.
