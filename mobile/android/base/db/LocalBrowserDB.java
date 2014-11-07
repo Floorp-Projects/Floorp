7e41fca7e597063a52e6181f194f09f5beabec91
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-11-07 15:35:12 -0800

    Bug 1093871 - Telemetry probe for number of items in reading list. r=rnewman, a=lmandel

--- mobile/android/base/db/LocalBrowserDB.java
+++ mobile/android/base/db/LocalBrowserDB.java
@@ -549,6 +549,9 @@ public class LocalBrowserDB {
         } else if ("favicons".equals(database)) {
             uri = mFaviconsUriWithProfile;
             columns = new String[] { Favicons._ID };
+        } else if ("readinglist".equals(database)) {
+            uri = mReadingListUriWithProfile;
+            columns = new String[] { ReadingListItems._ID };
         }
         if (uri != null) {
             Cursor cursor = null;
