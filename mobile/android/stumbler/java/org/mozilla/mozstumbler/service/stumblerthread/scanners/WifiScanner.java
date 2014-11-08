e964b301849acea0059bfe5e9463b9b0cd7202e0
Garvan Keeley <gkeeley@mozilla.com>
2014-11-08 09:06:00 -0500

    Bug 1095914 - Fix NPE in WifiScanner. r=vng, a=lsblakk
    https://github.com/mozilla/MozStumbler/pull/1192
    1169: ACRA Report: NPE WifiScanner.java:98

--- mobile/android/stumbler/java/org/mozilla/mozstumbler/service/stumblerthread/scanners/WifiScanner.java
+++ mobile/android/stumbler/java/org/mozilla/mozstumbler/service/stumblerthread/scanners/WifiScanner.java
@@ -65,7 +65,11 @@ public class WifiScanner extends BroadcastReceiver {
     }
 
     private List<ScanResult> getScanResults() {
-        return (sIsTestMode)? mTestModeFakeScanResults : getWifiManager().getScanResults();
+        WifiManager manager = getWifiManager();
+        if (manager == null) {
+            return null;
+        }
+        return getWifiManager().getScanResults();
     }
 
 
@@ -105,8 +109,12 @@ public class WifiScanner extends BroadcastReceiver {
                 deactivatePeriodicScan();
             }
         } else if (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equals(action)) {
-            ArrayList<ScanResult> scanResults = new ArrayList<ScanResult>();
-            for (ScanResult scanResult : getScanResults()) {
+            final List<ScanResult> scanResultList = getScanResults();
+            if (scanResultList == null) {
+                return;
+            }
+            final ArrayList<ScanResult> scanResults = new ArrayList<ScanResult>();
+            for (ScanResult scanResult : scanResultList) {
                 scanResult.BSSID = BSSIDBlockList.canonicalizeBSSID(scanResult.BSSID);
                 if (shouldLog(scanResult)) {
                     scanResults.add(scanResult);
