515b9c01709c090ec146da4b0b5b320acad85d16
Matthew Gregan <kinetik@flim.org>
2014-11-11 08:58:52 +1300

    Bug 1085175. r=roc, a=dveditz

--- media/libnestegg/include/nestegg-stdint.h
+++ media/libnestegg/include/nestegg-stdint.h
@@ -1,6 +1,9 @@
 #ifdef _WIN32
 typedef __int64 int64_t;
 typedef unsigned __int64 uint64_t;
+#if !defined(INT64_MAX)
+#define INT64_MAX 9223372036854775807LL
+#endif
 #else
 #include <stdint.h>
 #endif
