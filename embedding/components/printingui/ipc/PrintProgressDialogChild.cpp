19a77b306e88314ea74ad585df56d7ffc5afd48e
Mike Conley <mconley@mozilla.com>
2014-11-10 15:18:06 -0500

    Bug 1090444 - The Printing Progress dialog does not show progress or close when opened from the child process. r=blassey.
    We now proxy progress updates from the child up to the parent over IPC.
    
    --HG--
    extra : rebase_source : a66557462e013ccf2b6e253c4b401d6baa0baa01

---
+++ embedding/components/printingui/ipc/PrintProgressDialogChild.cpp
@@ -0,0 +1,132 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/unused.h"
+#include "nsIObserver.h"
+#include "PrintProgressDialogChild.h"
+
+class nsIWebProgress;
+class nsIRequest;
+
+using mozilla::unused;
+
+namespace mozilla {
+namespace embedding {
+
+NS_IMPL_ISUPPORTS(PrintProgressDialogChild,
+                  nsIWebProgressListener,
+                  nsIPrintProgressParams)
+
+MOZ_IMPLICIT PrintProgressDialogChild::PrintProgressDialogChild(
+  nsIObserver* aOpenObserver) :
+  mOpenObserver(aOpenObserver)
+{
+  MOZ_COUNT_CTOR(PrintProgressDialogChild);
+}
+
+MOZ_IMPLICIT PrintProgressDialogChild::~PrintProgressDialogChild()
+{
+  // When the printing engine stops supplying information about printing
+  // progress, it'll drop references to us and destroy us. We need to signal
+  // the parent to decrement its refcount, as well as prevent it from attempting
+  // to contact us further.
+  unused << Send__delete__(this);
+  MOZ_COUNT_DTOR(PrintProgressDialogChild);
+}
+
+bool
+PrintProgressDialogChild::RecvDialogOpened()
+{
+  // nsPrintEngine's observer, which we're reporting to here, doesn't care
+  // what gets passed as the subject, topic or data, so we'll just send
+  // nullptrs.
+  mOpenObserver->Observe(nullptr, nullptr, nullptr);
+  return true;
+}
+
+// nsIWebProgressListener
+
+NS_IMETHODIMP
+PrintProgressDialogChild::OnStateChange(nsIWebProgress* aProgress,
+                                        nsIRequest* aRequest,
+                                        uint32_t aStateFlags,
+                                        nsresult aStatus)
+{
+  unused << SendStateChange(aStateFlags, aStatus);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+PrintProgressDialogChild::OnProgressChange(nsIWebProgress * aProgress,
+                                           nsIRequest * aRequest,
+                                           int32_t aCurSelfProgress,
+                                           int32_t aMaxSelfProgress,
+                                           int32_t aCurTotalProgress,
+                                           int32_t aMaxTotalProgress)
+{
+  unused << SendProgressChange(aCurSelfProgress, aMaxSelfProgress,
+                               aCurTotalProgress, aMaxTotalProgress);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+PrintProgressDialogChild::OnLocationChange(nsIWebProgress* aProgress,
+                                           nsIRequest* aRequest,
+                                           nsIURI* aURI,
+                                           uint32_t aFlags)
+{
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+PrintProgressDialogChild::OnStatusChange(nsIWebProgress* aProgress,
+                                         nsIRequest* aRequest,
+                                         nsresult aStatus,
+                                         const char16_t* aMessage)
+{
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+PrintProgressDialogChild::OnSecurityChange(nsIWebProgress* aProgress,
+                                           nsIRequest* aRequest,
+                                           uint32_t aState)
+{
+  return NS_OK;
+}
+
+// nsIPrintProgressParams
+
+NS_IMETHODIMP PrintProgressDialogChild::GetDocTitle(char16_t* *aDocTitle)
+{
+  NS_ENSURE_ARG(aDocTitle);
+
+  *aDocTitle = ToNewUnicode(mDocTitle);
+  return NS_OK;
+}
+
+NS_IMETHODIMP PrintProgressDialogChild::SetDocTitle(const char16_t* aDocTitle)
+{
+  mDocTitle = aDocTitle;
+  unused << SendDocTitleChange(nsString(aDocTitle));
+  return NS_OK;
+}
+
+NS_IMETHODIMP PrintProgressDialogChild::GetDocURL(char16_t **aDocURL)
+{
+  NS_ENSURE_ARG(aDocURL);
+
+  *aDocURL = ToNewUnicode(mDocURL);
+  return NS_OK;
+}
+
+NS_IMETHODIMP PrintProgressDialogChild::SetDocURL(const char16_t* aDocURL)
+{
+  mDocURL = aDocURL;
+  unused << SendDocURLChange(nsString(aDocURL));
+  return NS_OK;
+}
+
+} // namespace embedding
+} // namespace mozilla
