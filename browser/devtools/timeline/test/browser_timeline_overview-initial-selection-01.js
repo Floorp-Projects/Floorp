62bdf2a25b3fdd5ea64602ae717935806995568f
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-11-10 15:19:30 +0100

    Backed out changeset d92174bd0918 (bug 1069421) for dt test failures

--- browser/devtools/timeline/test/browser_timeline_overview-initial-selection-01.js
+++ browser/devtools/timeline/test/browser_timeline_overview-initial-selection-01.js
@@ -8,11 +8,8 @@
 
 let test = Task.async(function*() {
   let { target, panel } = yield initTimelinePanel(SIMPLE_URL);
-  let { $, EVENTS, TimelineView, TimelineController } = panel.panelWin;
+  let { EVENTS, TimelineView, TimelineController } = panel.panelWin;
   let { OVERVIEW_INITIAL_SELECTION_RATIO: selectionRatio } = panel.panelWin;
-
-  $("#memory-checkbox").checked = true;
-  yield TimelineController.updateMemoryRecording();
 
   yield TimelineController.toggleRecording();
   ok(true, "Recording has started.");
@@ -24,22 +21,19 @@ let test = Task.async(function*() {
     "The overview graph was updated a bunch of times.");
   ok((yield waitUntil(() => TimelineController.getMarkers().length > 0)),
     "There are some markers available.");
-  ok((yield waitUntil(() => TimelineController.getMemory().length > 0)),
-    "There are some memory measurements available now.");
 
   yield TimelineController.toggleRecording();
   ok(true, "Recording has ended.");
 
-  let interval = TimelineController.getInterval();
   let markers = TimelineController.getMarkers();
-  let selection = TimelineView.markersOverview.getSelection();
+  let selection = TimelineView.overview.getSelection();
 
   is((selection.start) | 0,
-     ((markers[0].start - interval.startTime) * TimelineView.markersOverview.dataScaleX) | 0,
+     ((markers[0].start - markers.startTime) * TimelineView.overview.dataScaleX) | 0,
     "The initial selection start is correct.");
 
   is((selection.end - selection.start) | 0,
-     (selectionRatio * TimelineView.markersOverview.width) | 0,
+     (selectionRatio * TimelineView.overview.width) | 0,
     "The initial selection end is correct.");
 
   yield teardown(panel);
