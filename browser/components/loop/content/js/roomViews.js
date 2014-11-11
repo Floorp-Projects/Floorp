6b13cb776d75d04165318629fc0e06f4b7adf244
Nicolas Perriault <nperriault@gmail.com>
2014-11-11 09:33:05 +0000

    Bug 1074686 - Part 3 Revamped view architecture for Desktop Loop rooms. r=Standard8 a=loop-only

--- browser/components/loop/content/js/roomViews.js
+++ browser/components/loop/content/js/roomViews.js
@@ -41,7 +41,11 @@ loop.roomViews = (function(mozL10n) {
     },
 
     getInitialState: function() {
-      return this.props.roomStore.getStoreState("activeRoom");
+      var storeState = this.props.roomStore.getStoreState("activeRoom");
+      return _.extend(storeState, {
+        // Used by the UI showcase.
+        roomState: this.props.roomState || storeState.roomState
+      });
     }
   };
 
@@ -72,25 +76,22 @@ loop.roomViews = (function(mozL10n) {
 
     render: function() {
       return (
-        React.DOM.div({className: "room-conversation-wrapper"}, 
-          React.DOM.div({className: "room-invitation-overlay"}, 
-            React.DOM.form({onSubmit: this.handleFormSubmit}, 
-              React.DOM.input({type: "text", ref: "roomName", 
-                placeholder: mozL10n.get("rooms_name_this_room_label")})
-            ), 
-            React.DOM.p(null, mozL10n.get("invite_header_text")), 
-            React.DOM.div({className: "btn-group call-action-group"}, 
-              React.DOM.button({className: "btn btn-info btn-email", 
-                      onClick: this.handleEmailButtonClick}, 
-                mozL10n.get("share_button2")
-              ), 
-              React.DOM.button({className: "btn btn-info btn-copy", 
-                      onClick: this.handleCopyButtonClick}, 
-                mozL10n.get("copy_url_button2")
-              )
-            )
+        React.DOM.div({className: "room-invitation-overlay"}, 
+          React.DOM.form({onSubmit: this.handleFormSubmit}, 
+            React.DOM.input({type: "text", ref: "roomName", 
+              placeholder: mozL10n.get("rooms_name_this_room_label")})
           ), 
-          DesktopRoomConversationView({roomStore: this.props.roomStore})
+          React.DOM.p(null, mozL10n.get("invite_header_text")), 
+          React.DOM.div({className: "btn-group call-action-group"}, 
+            React.DOM.button({className: "btn btn-info btn-email", 
+                    onClick: this.handleEmailButtonClick}, 
+              mozL10n.get("share_button2")
+            ), 
+            React.DOM.button({className: "btn btn-info btn-copy", 
+                    onClick: this.handleCopyButtonClick}, 
+              mozL10n.get("copy_url_button2")
+            )
+          )
         )
       );
     }
@@ -100,13 +101,12 @@ loop.roomViews = (function(mozL10n) {
    * Desktop room conversation view.
    */
   var DesktopRoomConversationView = React.createClass({displayName: 'DesktopRoomConversationView',
-    mixins: [ActiveRoomStoreMixin],
+    mixins: [ActiveRoomStoreMixin, loop.shared.mixins.DocumentTitleMixin],
 
     propTypes: {
       dispatcher: React.PropTypes.instanceOf(loop.Dispatcher).isRequired,
       video: React.PropTypes.object,
-      audio: React.PropTypes.object,
-      displayInvitation: React.PropTypes.bool
+      audio: React.PropTypes.object
     },
 
     getDefaultProps: function() {
@@ -116,89 +116,61 @@ loop.roomViews = (function(mozL10n) {
       };
     },
 
-    render: function() {
-      var localStreamClasses = React.addons.classSet({
-        local: true,
-        "local-stream": true,
-        "local-stream-audio": !this.props.video.enabled
-      });
-      return (
-        React.DOM.div({className: "room-conversation-wrapper"}, 
-          React.DOM.div({className: "video-layout-wrapper"}, 
-            React.DOM.div({className: "conversation room-conversation"}, 
-              React.DOM.div({className: "media nested"}, 
-                React.DOM.div({className: "video_wrapper remote_wrapper"}, 
-                  React.DOM.div({className: "video_inner remote"})
-                ), 
-                React.DOM.div({className: localStreamClasses})
-              ), 
-              sharedViews.ConversationToolbar({
-                video: this.props.video, 
-                audio: this.props.audio, 
-                publishStream: noop, 
-                hangup: noop})
-            )
-          )
-        )
-      );
-    }
-  });
-
-  /**
-   * Desktop room controller view.
-   */
-  var DesktopRoomControllerView = React.createClass({displayName: 'DesktopRoomControllerView',
-    mixins: [ActiveRoomStoreMixin, loop.shared.mixins.DocumentTitleMixin],
-
-    propTypes: {
-      dispatcher: React.PropTypes.instanceOf(loop.Dispatcher).isRequired
-    },
-
-    closeWindow: function() {
-      window.close();
-    },
-
-    _renderRoomView: function(roomState) {
-      switch (roomState) {
-        case ROOM_STATES.FAILED: {
-          return loop.conversation.GenericFailureView({
-            cancelCall: this.closeWindow}
-          );
-        }
-        case ROOM_STATES.INIT:
-        case ROOM_STATES.GATHER:
-        case ROOM_STATES.READY:
-        case ROOM_STATES.JOINED: {
-          return DesktopRoomInvitationView({
-            dispatcher: this.props.dispatcher, 
-            roomStore: this.props.roomStore}
-          );
-        }
-        // XXX needs bug 1074686/1074702
-        case ROOM_STATES.HAS_PARTICIPANTS: {
-          return DesktopRoomConversationView({
-            dispatcher: this.props.dispatcher, 
-            roomStore: this.props.roomStore}
-          );
-        }
+    _renderInvitationOverlay: function() {
+      if (this.state.roomState !== ROOM_STATES.HAS_PARTICIPANTS) {
+        return DesktopRoomInvitationView({
+          roomStore: this.props.roomStore, 
+          dispatcher: this.props.dispatcher}
+        );
       }
+      return null;
     },
 
     render: function() {
       if (this.state.roomName) {
         this.setTitle(this.state.roomName);
       }
-      return (
-        React.DOM.div({className: "room-conversation-wrapper"}, 
-          this._renderRoomView(this.state.roomState)
-        )
-      );
+
+      var localStreamClasses = React.addons.classSet({
+        local: true,
+        "local-stream": true,
+        "local-stream-audio": !this.props.video.enabled
+      });
+
+      switch(this.state.roomState) {
+        case ROOM_STATES.FAILED: {
+          return loop.conversation.GenericFailureView({
+            cancelCall: this.closeWindow}
+          );
+        }
+        default: {
+          return (
+            React.DOM.div({className: "room-conversation-wrapper"}, 
+              this._renderInvitationOverlay(), 
+              React.DOM.div({className: "video-layout-wrapper"}, 
+                React.DOM.div({className: "conversation room-conversation"}, 
+                  React.DOM.div({className: "media nested"}, 
+                    React.DOM.div({className: "video_wrapper remote_wrapper"}, 
+                      React.DOM.div({className: "video_inner remote"})
+                    ), 
+                    React.DOM.div({className: localStreamClasses})
+                  ), 
+                  sharedViews.ConversationToolbar({
+                    video: this.props.video, 
+                    audio: this.props.audio, 
+                    publishStream: noop, 
+                    hangup: noop})
+                )
+              )
+            )
+          );
+        }
+      }
     }
   });
 
   return {
     ActiveRoomStoreMixin: ActiveRoomStoreMixin,
-    DesktopRoomControllerView: DesktopRoomControllerView,
     DesktopRoomConversationView: DesktopRoomConversationView,
     DesktopRoomInvitationView: DesktopRoomInvitationView
   };
