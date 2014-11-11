8d432fef904c44acf721c2c34e49d9564ede639f
Ryan VanderMeulen <ryanvm@gmail.com>
2014-11-11 13:10:56 -0500

    Backed out changesets 857063c82323 and a19479860eb4 (bug 619521) for Valgrind failures. CLOSED TREE

--- layout/generic/MathMLTextRunFactory.h
+++ layout/generic/MathMLTextRunFactory.h
@@ -22,8 +22,7 @@ public:
       mSSTYScriptLevel(aSSTYScriptLevel) {}
 
   virtual void RebuildTextRun(nsTransformedTextRun* aTextRun,
-                              gfxContext* aRefContext,
-                              gfxMissingFontRecorder* aMFR) MOZ_OVERRIDE;
+                              gfxContext* aRefContext) MOZ_OVERRIDE;
   enum {
     // Style effects which may override single character <mi> behaviour
     MATH_FONT_STYLING_NORMAL   = 0x1, // fontstyle="normal" has been set.
--- layout/generic/nsTextRunTransformations.h
+++ layout/generic/nsTextRunTransformations.h
@@ -27,9 +27,7 @@ public:
                                     gfxFontGroup* aFontGroup, uint32_t aFlags,
                                     nsStyleContext** aStyles, bool aOwnsFactory = true);
 
-  virtual void RebuildTextRun(nsTransformedTextRun* aTextRun,
-                              gfxContext* aRefContext,
-                              gfxMissingFontRecorder* aMFR) = 0;
+  virtual void RebuildTextRun(nsTransformedTextRun* aTextRun, gfxContext* aRefContext) = 0;
 };
 
 /**
@@ -50,9 +48,7 @@ public:
     : mInnerTransformingTextRunFactory(aInnerTransformingTextRunFactory),
       mAllUppercase(aAllUppercase) {}
 
-  virtual void RebuildTextRun(nsTransformedTextRun* aTextRun,
-                              gfxContext* aRefContext,
-                              gfxMissingFontRecorder* aMFR) MOZ_OVERRIDE;
+  virtual void RebuildTextRun(nsTransformedTextRun* aTextRun, gfxContext* aRefContext) MOZ_OVERRIDE;
 
   // Perform a transformation on the given string, writing the result into
   // aConvertedString. If aAllUppercase is true, the transform is (global)
@@ -109,12 +105,11 @@ public:
    * are done and before we request any data from the textrun. Also always
    * called after a Create.
    */
-  void FinishSettingProperties(gfxContext* aRefContext,
-                               gfxMissingFontRecorder* aMFR)
+  void FinishSettingProperties(gfxContext* aRefContext)
   {
     if (mNeedsRebuild) {
       mNeedsRebuild = false;
-      mFactory->RebuildTextRun(this, aRefContext, aMFR);
+      mFactory->RebuildTextRun(this, aRefContext);
     }
   }
 
