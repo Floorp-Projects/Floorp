7ad759a6bab475c68c30c3ed75442a56f1960741
Victor Porof <vporof@mozilla.com>
2014-11-11 08:28:23 -0500

    Bug 1069421 - Add a memory graph to the timeline, r=pbrosset,paul
    --HG--
    rename : browser/devtools/shared/test/browser_graphs-09.js => browser/devtools/shared/test/browser_graphs-09a.js
    rename : browser/devtools/timeline/widgets/overview.js => browser/devtools/timeline/widgets/markers-overview.js

--- browser/devtools/timeline/test/browser_timeline_recording-without-memory.js
+++ browser/devtools/timeline/test/browser_timeline_recording-without-memory.js
@@ -2,17 +2,12 @@
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
 /**
- * Tests if the overview has an initial selection when recording has finished
- * and there is data available.
+ * Tests if the timeline actor isn't unnecessarily asked to record memory.
  */
 
 let test = Task.async(function*() {
   let { target, panel } = yield initTimelinePanel(SIMPLE_URL);
   let { $, EVENTS, TimelineView, TimelineController } = panel.panelWin;
-  let { OVERVIEW_INITIAL_SELECTION_RATIO: selectionRatio } = panel.panelWin;
-
-  $("#memory-checkbox").checked = true;
-  yield TimelineController.updateMemoryRecording();
 
   yield TimelineController.toggleRecording();
   ok(true, "Recording has started.");
@@ -24,23 +19,17 @@ let test = Task.async(function*() {
     "The overview graph was updated a bunch of times.");
   ok((yield waitUntil(() => TimelineController.getMarkers().length > 0)),
     "There are some markers available.");
-  ok((yield waitUntil(() => TimelineController.getMemory().length > 0)),
-    "There are some memory measurements available now.");
 
   yield TimelineController.toggleRecording();
   ok(true, "Recording has ended.");
 
-  let interval = TimelineController.getInterval();
   let markers = TimelineController.getMarkers();
-  let selection = TimelineView.markersOverview.getSelection();
+  let memory = TimelineController.getMemory();
 
-  is((selection.start) | 0,
-     ((markers[0].start - interval.startTime) * TimelineView.markersOverview.dataScaleX) | 0,
-    "The initial selection start is correct.");
-
-  is((selection.end - selection.start) | 0,
-     (selectionRatio * TimelineView.markersOverview.width) | 0,
-    "The initial selection end is correct.");
+  isnot(markers.length, 0,
+    "There are some markers available.");
+  is(memory.length, 0,
+    "There are no memory measurements available.");
 
   yield teardown(panel);
   finish();
