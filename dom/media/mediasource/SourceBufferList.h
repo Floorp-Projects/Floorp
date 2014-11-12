be63b73148a273cbf79f879a7b654b326ae2ba5a
Chris Double <chris.double@double.co.nz>
2014-11-12 17:13:10 +1300

    Backout 471009c10ae0 for web platform test failure on CLOSED TREE

--- dom/media/mediasource/SourceBufferList.h
+++ dom/media/mediasource/SourceBufferList.h
@@ -66,8 +66,9 @@ public:
   // Returns true if updating is true on any SourceBuffers in the list.
   bool AnyUpdating();
 
-  // Runs the range removal steps from the MSE specification on each SourceBuffer.
-  void RangeRemoval(double aStart, double aEnd);
+  // Calls Remove(aStart, aEnd) on each SourceBuffer in the list.  Aborts on
+  // first error, with result returned in aRv.
+  void Remove(double aStart, double aEnd, ErrorResult& aRv);
 
   // Mark all SourceBuffers input buffers as ended.
   void Ended();
