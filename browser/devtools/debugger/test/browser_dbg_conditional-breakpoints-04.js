75e180fd7585978a67a694a66a41727fbd4054b8
Eddy Bruël <ejpbruel@gmail.com>
2014-11-12 14:31:00 +0100

    Bug 1090929 - Enable the conditional breakpoint tests;r=mratcliffe

--- browser/devtools/debugger/test/browser_dbg_conditional-breakpoints-04.js
+++ browser/devtools/debugger/test/browser_dbg_conditional-breakpoints-04.js
@@ -9,12 +9,11 @@
 const TAB_URL = EXAMPLE_URL + "doc_conditional-breakpoints.html";
 
 function test() {
-  let gTab, gDebuggee, gPanel, gDebugger;
+  let gTab, gPanel, gDebugger;
   let gSources, gBreakpoints, gLocation;
 
-  initDebugger(TAB_URL).then(([aTab, aDebuggee, aPanel]) => {
+  initDebugger(TAB_URL).then(([aTab,, aPanel]) => {
     gTab = aTab;
-    gDebuggee = aDebuggee;
     gPanel = aPanel;
     gDebugger = gPanel.panelWin;
     gSources = gDebugger.DebuggerView.Sources;
@@ -56,7 +55,7 @@ function test() {
         ok(false, "Got an error: " + aError.message + "\n" + aError.stack);
       });
 
-    gDebuggee.ermahgerd();
+    callInTab(gTab, "ermahgerd");
   });
 
   function addBreakpoint() {
