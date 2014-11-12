b04d6b4c9a1f4ff81b9650427e78607cd80aa79f
Kannan Vijayan <kvijayan@mozilla.com>
2014-11-12 14:42:42 -0500

    Bug 1080462 - Fix up leak of native2bytecode mapping information. r=h4writer

--- js/src/jit/JitcodeMap.cpp
+++ js/src/jit/JitcodeMap.cpp
@@ -172,6 +172,12 @@ void
 JitcodeGlobalTable::removeEntry(void *startAddr)
 {
     JitcodeGlobalEntry query = JitcodeGlobalEntry::MakeQuery(startAddr);
+    JitcodeGlobalEntry result;
+    mozilla::DebugOnly<bool> success = tree_.contains(query, &result);
+    MOZ_ASSERT(success);
+
+    // Destroy entry before removing it from tree.
+    result.destroy();
     tree_.remove(query);
 }
 
