b3d649c2dbedde8c524dc562ed52cb2322fc2a4c
Bobby Holley <bobbyholley@gmail.com>
2014-11-11 20:50:21 -0800

    Bug 1091008 - Implement a sensible GetBuffered override for MediaSourceReader using the existing GetBuffered on MediaSource. r=cajbir

--- dom/media/mediasource/MediaSourceReader.h
+++ dom/media/mediasource/MediaSourceReader.h
@@ -81,6 +81,8 @@ public:
   void Seek(int64_t aTime, int64_t aStartTime, int64_t aEndTime,
             int64_t aCurrentTime) MOZ_OVERRIDE;
 
+  nsresult GetBuffered(dom::TimeRanges* aBuffered) MOZ_OVERRIDE;
+
   already_AddRefed<SourceBufferDecoder> CreateSubDecoder(const nsACString& aType);
 
   void AddTrackBuffer(TrackBuffer* aTrackBuffer);
