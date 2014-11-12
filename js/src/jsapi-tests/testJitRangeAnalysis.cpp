e6b3c54c2794882574847265f79923115b1b161f
Dan Gohman <sunfish@mozilla.com>
2014-11-12 12:38:31 -0800

    Bug 892702 - IonMonkey: Add unit tests for range analysis r=nbp

--- js/src/jsapi-tests/testJitRangeAnalysis.cpp
+++ js/src/jsapi-tests/testJitRangeAnalysis.cpp
@@ -16,136 +16,196 @@
 using namespace js;
 using namespace js::jit;
 
+static bool
+EquivalentRanges(const Range *a, const Range *b) {
+    if (a->hasInt32UpperBound() != b->hasInt32UpperBound())
+        return false;
+    if (a->hasInt32LowerBound() != b->hasInt32LowerBound())
+        return false;
+    if (a->hasInt32UpperBound() && (a->upper() != b->upper()))
+        return false;
+    if (a->hasInt32LowerBound() && (a->lower() != b->lower()))
+        return false;
+    if (a->canHaveFractionalPart() != b->canHaveFractionalPart())
+        return false;
+    if (a->canBeNegativeZero() != b->canBeNegativeZero())
+        return false;
+    if (a->canBeNaN() != b->canBeNaN())
+        return false;
+    if (a->canBeInfiniteOrNaN() != b->canBeInfiniteOrNaN())
+        return false;
+    if (!a->canBeInfiniteOrNaN() && (a->exponent() != b->exponent()))
+        return false;
+    return true;
+}
+
 BEGIN_TEST(testJitRangeAnalysis_MathSign)
 {
-    MinimalFunc func;
-    MBasicBlock *block = func.createEntryBlock();
+    MinimalAlloc func;
 
-    MParameter *p = func.createParameter();
-    block->add(p);
+    Range *xnan = new(func.alloc) Range();
 
-    MConstant *nan = MConstant::New(func.alloc, DoubleValue(JS::GenericNaN()));
-    block->add(nan);
-    MConstant *neginf = MConstant::New(func.alloc, DoubleValue(mozilla::NegativeInfinity<double>()));
-    block->add(neginf);
-    MConstant *negthreehalves = MConstant::New(func.alloc, DoubleValue(-1.5));
-    block->add(negthreehalves);
-    MConstant *negone = MConstant::New(func.alloc, DoubleValue(-1.0));
-    block->add(negone);
-    MConstant *neghalf = MConstant::New(func.alloc, DoubleValue(-0.5));
-    block->add(neghalf);
-    MConstant *negzero = MConstant::New(func.alloc, DoubleValue(-0.0));
-    block->add(negzero);
-    MConstant *zero = MConstant::New(func.alloc, DoubleValue(0.0));
-    block->add(zero);
-    MConstant *half = MConstant::New(func.alloc, DoubleValue(0.5));
-    block->add(half);
-    MConstant *one = MConstant::New(func.alloc, DoubleValue(1.0));
-    block->add(one);
-    MConstant *threehalves = MConstant::New(func.alloc, DoubleValue(1.5));
-    block->add(threehalves);
-    MConstant *inf = MConstant::New(func.alloc, DoubleValue(mozilla::PositiveInfinity<double>()));
-    block->add(inf);
+    Range *ninf = Range::NewDoubleSingletonRange(func.alloc, mozilla::NegativeInfinity<double>());
+    Range *n1_5 = Range::NewDoubleSingletonRange(func.alloc, -1.5);
+    Range *n1_0 = Range::NewDoubleSingletonRange(func.alloc, -1);
+    Range *n0_5 = Range::NewDoubleSingletonRange(func.alloc, -0.5);
+    Range *n0_0 = Range::NewDoubleSingletonRange(func.alloc, -0.0);
 
-    CHECK(mozilla::IsNaN(nan->value().toNumber()));
-    CHECK(mozilla::IsInfinite(neginf->value().toNumber()));
-    CHECK(mozilla::IsNegative(neginf->value().toNumber()));
-    CHECK(mozilla::IsFinite(negthreehalves->value().toNumber()));
-    CHECK(mozilla::IsNegative(negthreehalves->value().toNumber()));
-    CHECK(mozilla::IsFinite(negone->value().toNumber()));
-    CHECK(mozilla::IsNegative(negone->value().toNumber()));
-    CHECK(mozilla::IsFinite(neghalf->value().toNumber()));
-    CHECK(mozilla::IsNegative(neghalf->value().toNumber()));
-    CHECK(mozilla::IsFinite(negzero->value().toNumber()));
-    CHECK(mozilla::IsNegative(negzero->value().toNumber()));
-    CHECK(mozilla::IsNegativeZero(negzero->value().toNumber()));
-    CHECK(mozilla::IsFinite(negzero->value().toNumber()));
-    CHECK(!mozilla::IsNegative(zero->value().toNumber()));
-    CHECK(!mozilla::IsNegativeZero(zero->value().toNumber()));
-    CHECK(mozilla::IsFinite(half->value().toNumber()));
-    CHECK(!mozilla::IsNegative(half->value().toNumber()));
-    CHECK(mozilla::IsFinite(one->value().toNumber()));
-    CHECK(!mozilla::IsNegative(one->value().toNumber()));
-    CHECK(mozilla::IsFinite(threehalves->value().toNumber()));
-    CHECK(!mozilla::IsNegative(threehalves->value().toNumber()));
-    CHECK(mozilla::IsInfinite(inf->value().toNumber()));
-    CHECK(!mozilla::IsNegative(inf->value().toNumber()));
+    Range *p0_0 = Range::NewDoubleSingletonRange(func.alloc, 0.0);
+    Range *p0_5 = Range::NewDoubleSingletonRange(func.alloc, 0.5);
+    Range *p1_0 = Range::NewDoubleSingletonRange(func.alloc, 1.0);
+    Range *p1_5 = Range::NewDoubleSingletonRange(func.alloc, 1.5);
+    Range *pinf = Range::NewDoubleSingletonRange(func.alloc, mozilla::PositiveInfinity<double>());
 
-    MathCache cache;
+    Range *xnanSign = Range::sign(func.alloc, xnan);
 
-    MMathFunction *nanSign = MMathFunction::New(func.alloc, nan, MMathFunction::Sign, &cache);
-    block->add(nanSign);
-    MMathFunction *neginfSign = MMathFunction::New(func.alloc, neginf, MMathFunction::Sign, &cache);
-    block->add(neginfSign);
-    MMathFunction *negthreehalvesSign = MMathFunction::New(func.alloc, negthreehalves,
-                                                           MMathFunction::Sign, &cache);
-    block->add(negthreehalvesSign);
-    MMathFunction *negoneSign = MMathFunction::New(func.alloc, negone, MMathFunction::Sign, &cache);
-    block->add(negoneSign);
-    MMathFunction *neghalfSign = MMathFunction::New(func.alloc, neghalf, MMathFunction::Sign, &cache);
-    block->add(neghalfSign);
-    MMathFunction *negzeroSign = MMathFunction::New(func.alloc, negzero, MMathFunction::Sign, &cache);
-    block->add(negzeroSign);
-    MMathFunction *zeroSign = MMathFunction::New(func.alloc, zero, MMathFunction::Sign, &cache);
-    block->add(zeroSign);
-    MMathFunction *halfSign = MMathFunction::New(func.alloc, half, MMathFunction::Sign, &cache);
-    block->add(halfSign);
-    MMathFunction *oneSign = MMathFunction::New(func.alloc, one, MMathFunction::Sign, &cache);
-    block->add(oneSign);
-    MMathFunction *threehalvesSign = MMathFunction::New(func.alloc, threehalves,
-                                                        MMathFunction::Sign, &cache);
-    block->add(threehalvesSign);
-    MMathFunction *infSign = MMathFunction::New(func.alloc, inf, MMathFunction::Sign, &cache);
-    block->add(infSign);
+    Range *ninfSign = Range::sign(func.alloc, ninf);
+    Range *n1_5Sign = Range::sign(func.alloc, n1_5);
+    Range *n1_0Sign = Range::sign(func.alloc, n1_0);
+    Range *n0_5Sign = Range::sign(func.alloc, n0_5);
+    Range *n0_0Sign = Range::sign(func.alloc, n0_0);
 
-    MReturn *ret = MReturn::New(func.alloc, p);
-    block->end(ret);
+    Range *p0_0Sign = Range::sign(func.alloc, p0_0);
+    Range *p0_5Sign = Range::sign(func.alloc, p0_5);
+    Range *p1_0Sign = Range::sign(func.alloc, p1_0);
+    Range *p1_5Sign = Range::sign(func.alloc, p1_5);
+    Range *pinfSign = Range::sign(func.alloc, pinf);
 
-    if (!func.runRangeAnalysis())
-        return false;
+    CHECK(!xnanSign);
+    CHECK(EquivalentRanges(ninfSign, Range::NewInt32SingletonRange(func.alloc, -1)));
+    CHECK(EquivalentRanges(n1_5Sign, Range::NewInt32SingletonRange(func.alloc, -1)));
+    CHECK(EquivalentRanges(n1_0Sign, Range::NewInt32SingletonRange(func.alloc, -1)));
 
-    CHECK(!nanSign->range());
-    CHECK(neginfSign->range()->isInt32());
-    CHECK(neginfSign->range()->lower() == -1);
-    CHECK(neginfSign->range()->upper() == -1);
-    CHECK(negthreehalvesSign->range()->isInt32());
-    CHECK(negthreehalvesSign->range()->lower() == -1);
-    CHECK(negthreehalvesSign->range()->upper() == -1);
-    CHECK(negoneSign->range()->isInt32());
-    CHECK(negoneSign->range()->lower() == -1);
-    CHECK(negoneSign->range()->upper() == -1);
-    CHECK(neghalfSign->range()->isInt32());
-    CHECK(neghalfSign->range()->lower() == -1);
-
-    // This should ideally be -1, but range analysis can't represent the
+    // This should ideally be just -1, but range analysis can't represent the
     // specific fractional range of the constant.
-    CHECK(neghalfSign->range()->upper() == 0);
+    CHECK(EquivalentRanges(n0_5Sign, Range::NewInt32Range(func.alloc, -1, 0)));
 
-    CHECK(!negzeroSign->range()->canHaveFractionalPart());
-    CHECK(negzeroSign->range()->canBeNegativeZero());
-    CHECK(negzeroSign->range()->lower() == 0);
-    CHECK(negzeroSign->range()->upper() == 0);
-    CHECK(!zeroSign->range()->canHaveFractionalPart());
-    CHECK(!zeroSign->range()->canBeNegativeZero());
-    CHECK(zeroSign->range()->lower() == 0);
-    CHECK(zeroSign->range()->upper() == 0);
-    CHECK(halfSign->range()->isInt32());
+    CHECK(EquivalentRanges(n0_0Sign, Range::NewDoubleSingletonRange(func.alloc, -0.0)));
 
-    // This should ideally be 1, but range analysis can't represent the
+    CHECK(!n0_0Sign->canHaveFractionalPart());
+    CHECK(n0_0Sign->canBeNegativeZero());
+    CHECK(n0_0Sign->lower() == 0);
+    CHECK(n0_0Sign->upper() == 0);
+
+    CHECK(EquivalentRanges(p0_0Sign, Range::NewInt32SingletonRange(func.alloc, 0)));
+
+    CHECK(!p0_0Sign->canHaveFractionalPart());
+    CHECK(!p0_0Sign->canBeNegativeZero());
+    CHECK(p0_0Sign->lower() == 0);
+    CHECK(p0_0Sign->upper() == 0);
+
+    // This should ideally be just 1, but range analysis can't represent the
     // specific fractional range of the constant.
-    CHECK(halfSign->range()->lower() == 0);
+    CHECK(EquivalentRanges(p0_5Sign, Range::NewInt32Range(func.alloc, 0, 1)));
 
-    CHECK(halfSign->range()->upper() == 1);
-    CHECK(oneSign->range()->isInt32());
-    CHECK(oneSign->range()->lower() == 1);
-    CHECK(oneSign->range()->upper() == 1);
-    CHECK(threehalvesSign->range()->isInt32());
-    CHECK(threehalvesSign->range()->lower() == 1);
-    CHECK(threehalvesSign->range()->upper() == 1);
-    CHECK(infSign->range()->isInt32());
-    CHECK(infSign->range()->lower() == 1);
-    CHECK(infSign->range()->upper() == 1);
+    CHECK(EquivalentRanges(p1_0Sign, Range::NewInt32SingletonRange(func.alloc, 1)));
+    CHECK(EquivalentRanges(p1_5Sign, Range::NewInt32SingletonRange(func.alloc, 1)));
+    CHECK(EquivalentRanges(pinfSign, Range::NewInt32SingletonRange(func.alloc, 1)));
 
     return true;
 }
 END_TEST(testJitRangeAnalysis_MathSign)
+
+BEGIN_TEST(testJitRangeAnalysis_MathSignBeta)
+{
+    MinimalFunc func;
+    MathCache cache;
+
+    MBasicBlock *entry = func.createEntryBlock();
+    MBasicBlock *thenBlock = func.createBlock(entry);
+    MBasicBlock *elseBlock = func.createBlock(entry);
+    MBasicBlock *elseThenBlock = func.createBlock(elseBlock);
+    MBasicBlock *elseElseBlock = func.createBlock(elseBlock);
+
+    // if (p < 0)
+    MParameter *p = func.createParameter();
+    entry->add(p);
+    MConstant *c0 = MConstant::New(func.alloc, DoubleValue(0.0));
+    entry->add(c0);
+    MConstant *cm0 = MConstant::New(func.alloc, DoubleValue(-0.0));
+    entry->add(cm0);
+    MCompare *cmp = MCompare::New(func.alloc, p, c0, JSOP_LT);
+    entry->add(cmp);
+    entry->end(MTest::New(func.alloc, cmp, thenBlock, elseBlock));
+
+    // {
+    //   return Math.sign(p + -0);
+    // }
+    MAdd *thenAdd = MAdd::NewAsmJS(func.alloc, p, cm0, MIRType_Double);
+    thenBlock->add(thenAdd);
+    MMathFunction *thenSign = MMathFunction::New(func.alloc, thenAdd, MMathFunction::Sign, &cache);
+    thenBlock->add(thenSign);
+    MReturn *thenRet = MReturn::New(func.alloc, thenSign);
+    thenBlock->end(thenRet);
+
+    // else
+    // {
+    //   if (p >= 0)
+    MCompare *elseCmp = MCompare::New(func.alloc, p, c0, JSOP_GE);
+    elseBlock->add(elseCmp);
+    elseBlock->end(MTest::New(func.alloc, elseCmp, elseThenBlock, elseElseBlock));
+
+    //   {
+    //     return Math.sign(p + -0);
+    //   }
+    MAdd *elseThenAdd = MAdd::NewAsmJS(func.alloc, p, cm0, MIRType_Double);
+    elseThenBlock->add(elseThenAdd);
+    MMathFunction *elseThenSign = MMathFunction::New(func.alloc, elseThenAdd, MMathFunction::Sign, &cache);
+    elseThenBlock->add(elseThenSign);
+    MReturn *elseThenRet = MReturn::New(func.alloc, elseThenSign);
+    elseThenBlock->end(elseThenRet);
+
+    //   else
+    //   {
+    //     return Math.sign(p + -0);
+    //   }
+    // }
+    MAdd *elseElseAdd = MAdd::NewAsmJS(func.alloc, p, cm0, MIRType_Double);
+    elseElseBlock->add(elseElseAdd);
+    MMathFunction *elseElseSign = MMathFunction::New(func.alloc, elseElseAdd, MMathFunction::Sign, &cache);
+    elseElseBlock->add(elseElseSign);
+    MReturn *elseElseRet = MReturn::New(func.alloc, elseElseSign);
+    elseElseBlock->end(elseElseRet);
+
+    if (!func.runRangeAnalysis())
+        return false;
+
+    CHECK(!p->range());
+    CHECK(EquivalentRanges(c0->range(), Range::NewDoubleSingletonRange(func.alloc, 0.0)));
+    CHECK(EquivalentRanges(cm0->range(), Range::NewDoubleSingletonRange(func.alloc, -0.0)));
+
+    // On the (p < 0) side, p is negative and not -0 (surprise!)
+    CHECK(EquivalentRanges(thenAdd->range(),
+                           new(func.alloc) Range(Range::NoInt32LowerBound, 0,
+                                                 Range::IncludesFractionalParts,
+                                                 Range::ExcludesNegativeZero,
+                                                 Range::IncludesInfinity)));
+
+    // Consequently, its Math.sign value is not -0 either.
+    CHECK(EquivalentRanges(thenSign->range(),
+                           new(func.alloc) Range(-1, 0,
+                                                 Range::ExcludesFractionalParts,
+                                                 Range::ExcludesNegativeZero,
+                                                 0)));
+
+    // On the (p >= 0) side, p is not negative and may be -0 (surprise!)
+    CHECK(EquivalentRanges(elseThenAdd->range(),
+                           new(func.alloc) Range(0, Range::NoInt32UpperBound,
+                                                 Range::IncludesFractionalParts,
+                                                 Range::IncludesNegativeZero,
+                                                 Range::IncludesInfinity)));
+
+    // Consequently, its Math.sign value may be -0 too.
+    CHECK(EquivalentRanges(elseThenSign->range(),
+                           new(func.alloc) Range(0, 1,
+                                                 Range::ExcludesFractionalParts,
+                                                 Range::IncludesNegativeZero,
+                                                 0)));
+
+    // Otherwise, p may be NaN.
+    CHECK(elseElseAdd->range()->isUnknown());
+    CHECK(!elseElseSign->range());
+
+    return true;
+}
+END_TEST(testJitRangeAnalysis_MathSignBeta)
