3da2a0081399d2e98e5af8d28d77023cee076304
David Major <dmajor@mozilla.com>
2014-11-12 15:42:53 +1300

    Bug 1097283: Fix count/byte mismatches in NS_ABORT_OOM calls. r=froydnj

--- xpcom/string/nsTStringObsolete.cpp
+++ xpcom/string/nsTStringObsolete.cpp
@@ -386,7 +386,7 @@ nsTString_CharT::SetCharAt( char16_t aChar, uint32_t aIndex )
     return false;
 
   if (!EnsureMutable())
-    NS_ABORT_OOM(mLength);
+    AllocFailed(mLength);
 
   mData[aIndex] = CharT(aChar);
   return true;
@@ -401,7 +401,7 @@ void
 nsTString_CharT::StripChars( const char* aSet )
 {
   if (!EnsureMutable())
-    NS_ABORT_OOM(mLength);
+    AllocFailed(mLength);
 
   mLength = nsBufferRoutines<CharT>::strip_chars(mData, mLength, aSet);
 }
@@ -421,7 +421,7 @@ void
 nsTString_CharT::ReplaceChar( char_type aOldChar, char_type aNewChar )
 {
   if (!EnsureMutable()) // XXX do this lazily?
-    NS_ABORT_OOM(mLength);
+    AllocFailed(mLength);
 
   for (uint32_t i=0; i<mLength; ++i)
   {
@@ -434,7 +434,7 @@ void
 nsTString_CharT::ReplaceChar( const char* aSet, char_type aNewChar )
 {
   if (!EnsureMutable()) // XXX do this lazily?
-    NS_ABORT_OOM(mLength);
+    AllocFailed(mLength);
 
   char_type* data = mData;
   uint32_t lenRemaining = mLength;
