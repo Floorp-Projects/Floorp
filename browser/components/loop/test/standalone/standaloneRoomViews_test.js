5eea5e5a7fe59ce300e5b408477dbdc783c2bdd1
Nicolas Perriault <nperriault@gmail.com>
2014-11-12 15:20:54 +0000

    Bug 1074702 - Part 2: Room views for Loop standalone. r=Standard8 a=loop-only

---
+++ browser/components/loop/test/standalone/standaloneRoomViews_test.js
@@ -0,0 +1,184 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* global loop, sinon */
+
+var expect = chai.expect;
+
+describe("loop.standaloneRoomViews", function() {
+  "use strict";
+
+  var ROOM_STATES = loop.store.ROOM_STATES;
+  var sharedActions = loop.shared.actions;
+
+  var sandbox, dispatcher, activeRoomStore, dispatch;
+
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+    dispatcher = new loop.Dispatcher();
+    dispatch = sandbox.stub(dispatcher, "dispatch");
+    activeRoomStore = new loop.store.ActiveRoomStore({
+      dispatcher: dispatcher,
+      mozLoop: {},
+      sdkDriver: {}
+    });
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  describe("standaloneRoomView", function() {
+    function mountTestComponent() {
+      return TestUtils.renderIntoDocument(
+        loop.standaloneRoomViews.StandaloneRoomView({
+          dispatcher: dispatcher,
+          activeRoomStore: activeRoomStore
+        }));
+    }
+
+    describe("#componentWillUpdate", function() {
+      it("dispatch an `SetupStreamElements` action on room joined", function() {
+        activeRoomStore.setStoreState({roomState: ROOM_STATES.READY});
+        var view = mountTestComponent();
+
+        sinon.assert.notCalled(dispatch);
+
+        activeRoomStore.setStoreState({roomState: ROOM_STATES.JOINED});
+
+        sinon.assert.calledOnce(dispatch);
+        sinon.assert.calledWithExactly(dispatch,
+          sinon.match.instanceOf(sharedActions.SetupStreamElements));
+        sinon.assert.calledWithExactly(dispatch, sinon.match(function(value) {
+          return value.getLocalElementFunc() ===
+                 view.getDOMNode().querySelector(".local");
+        }));
+        sinon.assert.calledWithExactly(dispatch, sinon.match(function(value) {
+          return value.getRemoteElementFunc() ===
+                 view.getDOMNode().querySelector(".remote");
+        }));
+      });
+    });
+
+    describe("#publishStream", function() {
+      var view;
+
+      beforeEach(function() {
+        view = mountTestComponent();
+        view.setState({
+          audioMuted: true,
+          videoMuted: true
+        });
+      });
+
+      it("should mute local audio stream", function() {
+        TestUtils.Simulate.click(
+          view.getDOMNode().querySelector(".btn-mute-audio"));
+
+        sinon.assert.calledOnce(dispatch);
+        sinon.assert.calledWithExactly(dispatch, new sharedActions.SetMute({
+          type: "audio",
+          enabled: true
+        }));
+      });
+
+      it("should mute local video stream", function() {
+        TestUtils.Simulate.click(
+          view.getDOMNode().querySelector(".btn-mute-video"));
+
+        sinon.assert.calledOnce(dispatch);
+        sinon.assert.calledWithExactly(dispatch, new sharedActions.SetMute({
+          type: "video",
+          enabled: true
+        }));
+      });
+    });
+
+    describe("#render", function() {
+      var view;
+
+      beforeEach(function() {
+        view = mountTestComponent();
+      });
+
+      describe("Join button", function() {
+        function getJoinButton(view) {
+          return view.getDOMNode().querySelector(".btn-join");
+        }
+
+        it("should render the Join button when room isn't active", function() {
+          activeRoomStore.setStoreState({roomState: ROOM_STATES.READY});
+
+          expect(getJoinButton(view)).not.eql(null);
+        });
+
+        it("should not render the Join button when room is active",
+          function() {
+            activeRoomStore.setStoreState({roomState: ROOM_STATES.SESSION_CONNECTED});
+
+            expect(getJoinButton(view)).eql(null);
+          });
+
+        it("should join the room when clicking the Join button", function() {
+          activeRoomStore.setStoreState({roomState: ROOM_STATES.READY});
+
+          TestUtils.Simulate.click(getJoinButton(view));
+
+          sinon.assert.calledOnce(dispatch);
+          sinon.assert.calledWithExactly(dispatch, new sharedActions.JoinRoom());
+        });
+      });
+
+      describe("Leave button", function() {
+        function getLeaveButton(view) {
+          return view.getDOMNode().querySelector(".btn-hangup");
+        }
+
+        it("should disable the Leave button when the room state is READY",
+          function() {
+            activeRoomStore.setStoreState({roomState: ROOM_STATES.READY});
+
+            expect(getLeaveButton(view).disabled).eql(true);
+          });
+
+        it("should disable the Leave button when the room state is FAILED",
+          function() {
+            activeRoomStore.setStoreState({roomState: ROOM_STATES.FAILED});
+
+            expect(getLeaveButton(view).disabled).eql(true);
+          });
+
+        it("should enable the Leave button when the room state is SESSION_CONNECTED",
+          function() {
+            activeRoomStore.setStoreState({roomState: ROOM_STATES.SESSION_CONNECTED});
+
+            expect(getLeaveButton(view).disabled).eql(false);
+          });
+
+        it("should enable the Leave button when the room state is JOINED",
+          function() {
+            activeRoomStore.setStoreState({roomState: ROOM_STATES.JOINED});
+
+            expect(getLeaveButton(view).disabled).eql(false);
+          });
+
+        it("should enable the Leave button when the room state is HAS_PARTICIPANTS",
+          function() {
+            activeRoomStore.setStoreState({roomState: ROOM_STATES.HAS_PARTICIPANTS});
+
+            expect(getLeaveButton(view).disabled).eql(false);
+          });
+
+        it("should leave the room when clicking the Leave button", function() {
+          activeRoomStore.setStoreState({roomState: ROOM_STATES.HAS_PARTICIPANTS});
+
+          TestUtils.Simulate.click(getLeaveButton(view));
+
+          sinon.assert.calledOnce(dispatch);
+          sinon.assert.calledWithExactly(dispatch, new sharedActions.LeaveRoom());
+        });
+      });
+    });
+  });
+});
