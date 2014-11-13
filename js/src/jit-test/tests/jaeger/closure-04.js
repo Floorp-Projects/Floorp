53f00f60c4058cdee46336b9444cbe82259e5dd7
Jan de Mooij <jdemooij@mozilla.com>
2014-11-13 17:39:51 +0100

    Bug 1093573 part 13 - Handle closing legacy generators correctly. r=wingo,shu

--- js/src/jit-test/tests/jaeger/closure-04.js
+++ js/src/jit-test/tests/jaeger/closure-04.js
@@ -1,5 +1,12 @@
+var depth = 0;
 test();
 function test() {
+  // |test()| is called recursively. When the generator runs in the JIT, the
+  // recursion limit is ~20x higher than in the interpreter. Limit the depth
+  // here so that the test doesn't timeout or becomes extremely slow.
+  if (++depth > 400)
+     return;
+
   var catch1, catch2, catch3, finally1, finally2, finally3;
   function gen() {
     yield 1;
