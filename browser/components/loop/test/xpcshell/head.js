8a8161edc357d37f5c176e12739ed8bcd4fae4a4
Mike de Boer <mdeboer@mozilla.com>
2014-11-13 11:47:28 +0100

    Bug 1096326 - Rename loop.throttled pref to loop.throttled2 in places we missed. r=Unfocused, a=test-only

--- browser/components/loop/test/xpcshell/head.js
+++ browser/components/loop/test/xpcshell/head.js
@@ -28,7 +28,7 @@ var loopServer;
 
 // Ensure loop is always enabled for tests
 Services.prefs.setBoolPref("loop.enabled", true);
-Services.prefs.setBoolPref("loop.throttled", false);
+Services.prefs.setBoolPref("loop.throttled2", false);
 
 // Cleanup function for all tests
 do_register_cleanup(() => {
