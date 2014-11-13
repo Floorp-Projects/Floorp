f5a684b33cabd808bb5029a7d81a7aebc02a4c37
Anthony Jones <ajones@mozilla.com>
2014-11-13 14:48:08 +1300

    Bug 1097449 - Add locking around libav open/close; r=jya

--- dom/media/fmp4/ffmpeg/FFmpegDataDecoder.cpp
+++ dom/media/fmp4/ffmpeg/FFmpegDataDecoder.cpp
@@ -18,6 +18,7 @@ namespace mozilla
 {
 
 bool FFmpegDataDecoder<LIBAV_VER>::sFFmpegInitDone = false;
+Monitor FFmpegDataDecoder<LIBAV_VER>::sMonitor("FFmpegDataDecoder");
 
 FFmpegDataDecoder<LIBAV_VER>::FFmpegDataDecoder(MediaTaskQueue* aTaskQueue,
                                                 AVCodecID aCodecID)
@@ -58,6 +59,8 @@ ChoosePixelFormat(AVCodecContext* aCodecContext, const PixelFormat* aFormats)
 nsresult
 FFmpegDataDecoder<LIBAV_VER>::Init()
 {
+  MonitorAutoLock mon(sMonitor);
+
   FFMPEG_LOG("Initialising FFmpeg decoder.");
 
   if (!sFFmpegInitDone) {
@@ -130,6 +133,8 @@ FFmpegDataDecoder<LIBAV_VER>::Flush()
 nsresult
 FFmpegDataDecoder<LIBAV_VER>::Shutdown()
 {
+  MonitorAutoLock mon(sMonitor);
+
   if (sFFmpegInitDone) {
     avcodec_close(mCodecContext);
     av_freep(&mCodecContext);
