1a0fee8b5ba9da2d988e584a9b9ef99474fea232
Andrew McCreight <continuation@gmail.com>
2014-11-13 07:11:55 -0800

    Bug 868139 - Don't redo unmarking work in nsGlobalWindow::CanSkip if we already did it. r=smaug
    This causes quadratic behavior in forgetSkippable when there are many timeouts.
    
    To avoid this, we mark the CC generation of the last time we ran CanSkip on the window,
    and don't do it again if it hasn't changed.

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -1132,7 +1132,8 @@ nsGlobalWindow::nsGlobalWindow(nsGlobalWindow *aOuterWindow)
 #endif
     mCleanedUp(false),
     mDialogAbuseCount(0),
-    mAreDialogsEnabled(true)
+    mAreDialogsEnabled(true),
+    mCanSkipCCGeneration(0)
 {
   nsLayoutStatics::AddRef();
 
@@ -1683,6 +1684,10 @@ MarkXBLHandlers(nsXBLPrototypeHandler* aKey, JS::Heap<JSObject*>& aData, void* a
 
 NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_BEGIN(nsGlobalWindow)
   if (tmp->IsBlackForCC(false)) {
+    if (nsCCUncollectableMarker::InGeneration(tmp->mCanSkipCCGeneration)) {
+      return true;
+    }
+    tmp->mCanSkipCCGeneration = nsCCUncollectableMarker::sGeneration;
     if (tmp->mCachedXBLPrototypeHandlers) {
       tmp->mCachedXBLPrototypeHandlers->Enumerate(MarkXBLHandlers, nullptr);
     }
