7c6b6681c00abbebc59d3b18aadc704537fef508
Eddy Bruël <ejpbruel@gmail.com>
2014-11-13 19:36:37 +0100

    Bug 1090929 - Enable the breakpoints tests;r=mratcliffe

--- browser/devtools/debugger/test/browser_dbg_breakpoints-highlight.js
+++ browser/devtools/debugger/test/browser_dbg_breakpoints-highlight.js
@@ -8,12 +8,11 @@
 const TAB_URL = EXAMPLE_URL + "doc_script-switching-01.html";
 
 function test() {
-  let gTab, gDebuggee, gPanel, gDebugger;
+  let gTab, gPanel, gDebugger;
   let gEditor, gSources;
 
-  initDebugger(TAB_URL).then(([aTab, aDebuggee, aPanel]) => {
+  initDebugger(TAB_URL).then(([aTab,, aPanel]) => {
     gTab = aTab;
-    gDebuggee = aDebuggee;
     gPanel = aPanel;
     gDebugger = gPanel.panelWin;
     gEditor = gDebugger.DebuggerView.editor;
