ea694c39b64e1270628f386981921a5def88151c
Michael Ratcliffe <mratcliffe@mozilla.com>
2014-11-13 10:18:10 -0500

    Bug 1097762 - part 2 - add test for clearing flag histograms; r=froydnj

---
+++ toolkit/components/telemetry/tests/unit/test_TelemetryFlagClear.js
@@ -0,0 +1,17 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+const Cu = Components.utils;
+const {Services} = Cu.import("resource://gre/modules/Services.jsm", {});
+
+function run_test()
+{
+  let testFlag = Services.telemetry.getHistogramById("TELEMETRY_TEST_FLAG");
+  equal(JSON.stringify(testFlag.snapshot().counts), "[1,0,0]", "Original value is correct");
+  testFlag.add(1);
+  equal(JSON.stringify(testFlag.snapshot().counts), "[0,1,0]", "Value is correct after ping.");
+  testFlag.clear();
+  equal(JSON.stringify(testFlag.snapshot().counts), "[1,0,0]", "Value is correct after calling clear()");
+  testFlag.add(1);
+  equal(JSON.stringify(testFlag.snapshot().counts), "[0,1,0]", "Value is correct after ping.");
+}
