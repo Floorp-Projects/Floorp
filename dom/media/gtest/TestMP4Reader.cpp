b7f8072b057fe8f4d8cb17a34671ad15c77d2cc0
Ryan VanderMeulen <ryanvm@gmail.com>
2014-11-14 15:24:17 -0500

    Backed out changeset fa836f2dccb6 for introducing new Win7 leaks. CLOSED TREE

--- dom/media/gtest/TestMP4Reader.cpp
+++ dom/media/gtest/TestMP4Reader.cpp
@@ -36,8 +36,6 @@ public:
     decoder->SetResource(resource);
 
     reader->Init(nullptr);
-    reader->SetTaskQueue(
-      new MediaTaskQueue(SharedThreadPool::Get(NS_LITERAL_CSTRING("TestMP4Reader"))));
     {
       // This needs to be done before invoking GetBuffered. This is normally
       // done by MediaDecoderStateMachine.
@@ -57,10 +55,6 @@ public:
 private:
   virtual ~TestBinding()
   {
-    reader->GetTaskQueue()->Dispatch(NS_NewRunnableMethod(reader,
-                                                          &MP4Reader::Shutdown));
-    reader->GetTaskQueue()->Shutdown();
-
     decoder = nullptr;
     resource = nullptr;
     reader = nullptr;
