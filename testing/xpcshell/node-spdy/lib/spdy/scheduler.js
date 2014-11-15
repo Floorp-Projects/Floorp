443ae7c84324591a951524bf0d0eed2f06c41871
Patrick McManus <mcmanus@ducksong.com>
2014-11-14 20:04:19 -0500

    bug 1097944 - node-spdy 1.29.1 for spdy/3.1 updates 1/2 r=hurley

--- testing/xpcshell/node-spdy/lib/spdy/scheduler.js
+++ testing/xpcshell/node-spdy/lib/spdy/scheduler.js
@@ -1,3 +1,5 @@
+var spdy = require('../spdy');
+var utils = spdy.utils;
 var scheduler = exports;
 
 //
@@ -8,7 +10,10 @@ var scheduler = exports;
 function Scheduler(connection) {
   this.connection = connection;
   this.priorities = [[], [], [], [], [], [], [], []];
-  this._tickListener = null;
+  this._tickListener = this._tickListener.bind(this);
+  this._tickListening = false;
+  this._tickCallbacks = [];
+  this._corked = false;
 }
 
 //
@@ -26,35 +31,71 @@ exports.create = function create(connection) {
 // Use stream priority to invoke callbacks in right order
 //
 Scheduler.prototype.schedule = function schedule(stream, data) {
-  this.priorities[stream.priority].push(data);
+  // Ignore data from destroyed stream
+  if (stream._spdyState.destroyed)
+    return;
+  this.scheduleLast(stream, data);
+};
+
+//
+// ### function scheduleLast (stream, data)
+// #### @stream {spdy.Stream} Source stream
+// #### @data {Buffer} data to write on tick
+// Use stream priority to invoke callbacks in right order
+//
+Scheduler.prototype.scheduleLast = function scheduleLast(stream, data) {
+  var priority = stream._spdyState.priority;
+  priority = Math.min(priority, 7);
+  priority = Math.max(priority, 0);
+  this.priorities[priority].push(data);
+};
+
+//
+// ### function tickListener ()
+//
+Scheduler.prototype._tickListener = function tickListener() {
+  var priorities = this.priorities;
+  var tickCallbacks = this._tickCallbacks;
+
+  this._tickListening = false;
+  this.priorities = [[], [], [], [], [], [], [], []];
+  this._tickCallbacks = [];
+
+  // Run all priorities
+  for (var i = 0; i < 8; i++)
+    for (var j = 0; j < priorities[i].length; j++)
+      this.connection.write(priorities[i][j]);
+
+  // Invoke callbacks
+  for (var i = 0; i < tickCallbacks.length; i++)
+    tickCallbacks[i]();
+  if (this._corked) {
+    this.connection.uncork();
+    if (this._tickListening)
+      this.connection.cork();
+    else
+      this._corked = false;
+  }
 };
 
 //
 // ### function tick ()
 // Add .nextTick callback if not already present
 //
-Scheduler.prototype.tick = function tick() {
-  if (this._tickListener !== null) return;
-  var self = this;
-  this._tickListener = function() {
-    var priorities = self.priorities;
+Scheduler.prototype.tick = function tick(cb) {
+  if (cb)
+    this._tickCallbacks.push(cb);
+  if (this._tickListening)
+    return;
+  this._tickListening = true;
 
-    self._tickListener = null;
-    self.priorities = [[], [], [], [], [], [], [], []];
-
-    // Run all priorities
-    for (var i = 0; i < 8; i++) {
-      for (var j = 0; j < priorities[i].length; j++) {
-        self.connection.write(
-          priorities[i][j]
-        );
-      }
-    }
-  };
-
-  if (this.connection.parser.drained) {
-    process.nextTick(this._tickListener);
+  if (!this._corked) {
+    this.connection.cork();
+    this._corked = true;
+  }
+  if (!this.connection._spdyState.parser.needDrain) {
+    utils.nextTick(this._tickListener);
   } else {
-    this.connection.parser.once('drain', this._tickListener);
+    this.connection._spdyState.parser.once('drain', this._tickListener);
   }
 };
