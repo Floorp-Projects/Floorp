3d76a99a55c0e9e459020de43361d6d32eccc3b7
Cykesiopka <cykesiopka.bmo@gmail.com>
2014-11-17 14:02:03 -0800

    Bug 1084606 - Allow overrides for MOZILLA_PKIX_ERROR_INADEQUATE_KEY_SIZE. r=keeler a=lmandel

--- security/manager/ssl/tests/unit/tlsserver/cmd/BadCertServer.cpp
+++ security/manager/ssl/tests/unit/tlsserver/cmd/BadCertServer.cpp
@@ -59,6 +59,7 @@ const BadCertHost sBadCertHosts[] =
   { "nsCertTypeNotCritical.example.com", "nsCertTypeNotCritical" },
   { "nsCertTypeCriticalWithExtKeyUsage.example.com", "nsCertTypeCriticalWithExtKeyUsage" },
   { "nsCertTypeCritical.example.com", "nsCertTypeCritical" },
+  { "inadequate-key-size-ee.example.com", "inadequateKeySizeEE" },
   { nullptr, nullptr }
 };
 
