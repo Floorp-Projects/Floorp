d346dbff1dce5242e088de0e1903ff14e041b0f0
Bob Owen <bobowencode@gmail.com>
2014-11-18 15:09:55 +0000

    Bug 1041775 Part 2: Re-apply warn only sandbox changes to Chromium code after merge. r=tabraldes
    Originally landed as changsets:
    https://hg.mozilla.org/mozilla-central/rev/e7eef85c1b0a
    https://hg.mozilla.org/mozilla-central/rev/8d0aca89e1b2

--- security/sandbox/win/src/handle_interception.cc
+++ security/sandbox/win/src/handle_interception.cc
@@ -10,6 +10,9 @@
 #include "sandbox/win/src/sandbox_nt_util.h"
 #include "sandbox/win/src/sharedmem_ipc_client.h"
 #include "sandbox/win/src/target_services.h"
+#ifdef MOZ_CONTENT_SANDBOX
+#include "mozilla/warnonlysandbox/warnOnlySandbox.h"
+#endif
 
 namespace sandbox {
 
@@ -34,10 +37,16 @@ ResultCode DuplicateHandleProxy(HANDLE source_handle,
 
   if (answer.win32_result) {
     ::SetLastError(answer.win32_result);
+#ifdef MOZ_CONTENT_SANDBOX
+    mozilla::warnonlysandbox::LogBlocked("DuplicateHandle");
+#endif
     return SBOX_ERROR_GENERIC;
   }
 
   *target_handle = answer.handle;
+#ifdef MOZ_CONTENT_SANDBOX
+  mozilla::warnonlysandbox::LogAllowed("DuplicateHandle");
+#endif
   return SBOX_ALL_OK;
 }
 
