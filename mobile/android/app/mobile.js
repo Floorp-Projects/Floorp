d1ac5d01f032b1071e8c6740756f7ab1885d309e
James Willcox <snorp@snorp.net>
2014-11-18 17:28:49 -0600

    Back out bug 1097276 r=me a=lmandel

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -596,11 +596,11 @@ pref("media.preload.auto", 2);    // preload metadata if preload=auto
 // of at least 4.
 pref("media.video-queue.default-size", 3);
 
-// Disable the MediaCodec PlatformDecoderModule by default.
-pref("media.fragmented-mp4.exposed", false);
-pref("media.fragmented-mp4.enabled", false);
-pref("media.fragmented-mp4.android-media-codec.enabled", false);
-pref("media.fragmented-mp4.android-media-codec.preferred", false);
+// Enable the MediaCodec PlatformDecoderModule by default.
+pref("media.fragmented-mp4.exposed", true);
+pref("media.fragmented-mp4.enabled", true);
+pref("media.fragmented-mp4.android-media-codec.enabled", true);
+pref("media.fragmented-mp4.android-media-codec.preferred", true);
 
 // optimize images memory usage
 pref("image.mem.decodeondraw", true);
