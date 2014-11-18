365ff8e5cf7011daec4402eccf6f6ec1c2534cb0
Wes Kocher <wkocher@mozilla.com>
2014-11-17 22:43:57 -0800

    Backed out changeset b1a6ecfca674 (bug 1044736)

--- dom/apps/tests/file_test_widget.js
+++ dom/apps/tests/file_test_widget.js
@@ -1,7 +1,6 @@
 var gWidgetManifestURL = 'http://test/tests/dom/apps/tests/file_app.sjs?apptype=widget&getmanifest=true';
 var gInvalidWidgetManifestURL = 'http://test/tests/dom/apps/tests/file_app.sjs?apptype=invalidWidget&getmanifest=true';
 var gApp;
-var gHasBrowserPermission;
 
 function onError() {
   ok(false, "Error callback invoked");
@@ -83,34 +82,21 @@ function testApp(isValidWidget) {
 
 function testLimitedBrowserAPI(ifr) {
   var securitySensitiveCalls = [
-    { api: 'sendMouseEvent'      , args: ['mousedown', 0, 0, 0, 0, 0] },
-    { api: 'sendTouchEvent'      , args: ['touchstart', [0], [0], [0], [1], [1], [0], [1], 1, 0] },
-    { api: 'goBack'              , args: [] },
-    { api: 'goForward'           , args: [] },
-    { api: 'reload'              , args: [] },
-    { api: 'stop'                , args: [] },
-    { api: 'download'            , args: ['http://example.org'] },
-    { api: 'purgeHistory'        , args: [] },
-    { api: 'getScreenshot'       , args: [0, 0] },
-    { api: 'zoom'                , args: [0.1] },
-    { api: 'getCanGoBack'        , args: [] },
-    { api: 'getCanGoForward'     , args: [] },
-    { api: 'getContentDimensions', args: [] }
+    'sendMouseEvent',
+    'sendTouchEvent',
+    'goBack',
+    'goForward',
+    'reload',
+    'stop',
+    'download',
+    'purgeHistory',
+    'getScreenshot',
+    'zoom',
+    'getCanGoBack',
+    'getCanGoForward'
   ];
   securitySensitiveCalls.forEach( function(call) {
-    if (gHasBrowserPermission) {
-      isnot(typeof ifr[call.api], "undefined", call.api + " should be defined");
-      var didThrow;
-      try {
-        ifr[call.api].apply(ifr, call.args);
-      } catch (e) {
-        ok(e instanceof DOMException, "throw right exception type");
-        didThrow = e.code;
-      }
-      is(didThrow, DOMException.INVALID_NODE_TYPE_ERR, "call " + call.api + " should throw exception");
-    } else {
-      is(typeof ifr[call.api], "undefined", call.api + " should be hidden for widget");
-    }
+    is(typeof ifr[call], "undefined", call + " should be hidden for widget");
   });
 }
 
@@ -168,7 +154,7 @@ var tests = [
   // Permissions
   function() {
     SpecialPowers.pushPermissions(
-      [{ "type": "browser", "allow": gHasBrowserPermission ? 1 : 0, "context": document },
+      [{ "type": "browser", "allow": 1, "context": document },
        { "type": "embed-widgets", "allow": 1, "context": document },
        { "type": "webapps-manage", "allow": 1, "context": document }], runTest);
   },
