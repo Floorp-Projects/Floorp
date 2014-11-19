33c237873dd7628d458001d6d9f4debecb45efa0
James Long <longster@gmail.com>
2014-11-19 16:02:00 +0100

    Bug 1074745 - allow setting sourceMapURL of a Debugger.Source and move warning to compiler. r=jimb

--- js/src/jsscript.cpp
+++ js/src/jsscript.cpp
@@ -2103,17 +2103,6 @@ bool
 ScriptSource::setSourceMapURL(ExclusiveContext *cx, const char16_t *sourceMapURL)
 {
     MOZ_ASSERT(sourceMapURL);
-    if (hasSourceMapURL()) {
-        // Warn about the replacement, but use the new one.
-        if (cx->isJSContext()) {
-            JS_ReportErrorFlagsAndNumber(cx->asJSContext(), JSREPORT_WARNING,
-                                         js_GetErrorMessage, nullptr,
-                                         JSMSG_ALREADY_HAS_PRAGMA, filename_.get(),
-                                         "//# sourceMappingURL");
-        }
-
-        sourceMapURL_ = nullptr;
-    }
 
     size_t len = js_strlen(sourceMapURL) + 1;
     if (len == 1)
