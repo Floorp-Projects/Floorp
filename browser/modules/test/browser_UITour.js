54b1ebf68201daead6d9f9cb0754c00d594885d5
Gavin Sharp <gavin@gavinsharp.com>
2014-11-19 17:52:23 -0500

    Bug 1101790 - setTreatmentTag API for UITour. r=felipe a=gavin

--- browser/modules/test/browser_UITour.js
+++ browser/modules/test/browser_UITour.js
@@ -402,6 +402,13 @@ let tests = [
       });
     });
   },
+  function test_treatment_tag(done) {
+    gContentAPI.setTreatmentTag("foobar", "baz");
+    gContentAPI.getTreatmentTag("foobar", (data) => {
+      is(data.value, "baz", "set and retrieved treatmentTag");
+      done();
+    });
+  },  
 
   // Make sure this test is last in the file so the appMenu gets left open and done will confirm it got tore down.
   function cleanupMenus(done) {
