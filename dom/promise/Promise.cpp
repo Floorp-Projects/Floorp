c74221f2c7b1f6f4cdd49f3331b7f028d5edc3f8
David Rajchenbach-Teller <dteller@mozilla.com>
2014-11-19 14:31:06 +0100

    Bug 1083361 - Exposing a PromiseDebugging API to monitor uncaught DOM Promise. r=bz

--- dom/promise/Promise.cpp
+++ dom/promise/Promise.cpp
@@ -14,9 +14,11 @@
 #include "mozilla/dom/PromiseBinding.h"
 #include "mozilla/dom/ScriptSettings.h"
 #include "mozilla/dom/MediaStreamError.h"
+#include "mozilla/Atomics.h"
 #include "mozilla/CycleCollectedJSRuntime.h"
 #include "mozilla/Preferences.h"
 #include "PromiseCallback.h"
+#include "PromiseDebugging.h"
 #include "PromiseNativeHandler.h"
 #include "PromiseWorkerProxy.h"
 #include "nsContentUtils.h"
@@ -32,6 +34,12 @@
 
 namespace mozilla {
 namespace dom {
+
+namespace {
+// Generator used by Promise::GetID.
+Atomic<uintptr_t> gIDGenerator(0);
+}
+
 
 using namespace workers;
 
@@ -245,7 +253,11 @@ private:
 NS_IMPL_CYCLE_COLLECTION_CLASS(Promise)
 
 NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(Promise)
+#if defined(DOM_PROMISE_DEPRECATED_REPORTING)
   tmp->MaybeReportRejectedOnce();
+#else
+  tmp->mResult = JS::UndefinedValue();
+#endif // defined(DOM_PROMISE_DEPRECATED_REPORTING)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mGlobal)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mResolveCallbacks)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mRejectCallbacks)
@@ -282,8 +294,14 @@ Promise::Promise(nsIGlobalObject* aGlobal)
   , mRejectionStack(nullptr)
   , mFullfillmentStack(nullptr)
   , mState(Pending)
+#if defined(DOM_PROMISE_DEPRECATED_REPORTING)
   , mHadRejectCallback(false)
+#endif // defined(DOM_PROMISE_DEPRECATED_REPORTING)
+  , mTaskPending(false)
   , mResolvePending(false)
+  , mIsLastInChain(true)
+  , mWasNotifiedAsUncaught(false)
+  , mID(0)
 {
   MOZ_ASSERT(mGlobal);
 
@@ -294,7 +312,9 @@ Promise::Promise(nsIGlobalObject* aGlobal)
 
 Promise::~Promise()
 {
+#if defined(DOM_PROMISE_DEPRECATED_REPORTING)
   MaybeReportRejectedOnce();
+#endif // defined(DOM_PROMISE_DEPRECATED_REPORTING)
   mozilla::DropJSObjects(this);
 }
 
@@ -935,17 +955,26 @@ void
 Promise::AppendCallbacks(PromiseCallback* aResolveCallback,
                          PromiseCallback* aRejectCallback)
 {
-  if (aResolveCallback) {
-    mResolveCallbacks.AppendElement(aResolveCallback);
-  }
+  MOZ_ASSERT(aResolveCallback);
+  MOZ_ASSERT(aRejectCallback);
 
-  if (aRejectCallback) {
-    mHadRejectCallback = true;
-    mRejectCallbacks.AppendElement(aRejectCallback);
-
-    // Now that there is a callback, we don't need to report anymore.
-    RemoveFeature();
+  if (mIsLastInChain && mState == PromiseState::Rejected) {
+    // This rejection is now consumed.
+    PromiseDebugging::AddConsumedRejection(*this);
+    // Note that we may not have had the opportunity to call
+    // RunResolveTask() yet, so we may never have called
+    // `PromiseDebugging:AddUncaughtRejection`.
   }
+  mIsLastInChain = false;
+
+#if defined(DOM_PROMISE_DEPRECATED_REPORTING)
+  // Now that there is a callback, we don't need to report anymore.
+  mHadRejectCallback = true;
+  RemoveFeature();
+#endif // defined(DOM_PROMISE_DEPRECATED_REPORTING)
+
+  mResolveCallbacks.AppendElement(aResolveCallback);
+  mRejectCallbacks.AppendElement(aRejectCallback);
 
   // If promise's state is fulfilled, queue a task to process our fulfill
   // callbacks with promise's result. If promise's state is rejected, queue a
@@ -998,6 +1027,7 @@ Promise::DispatchToMicroTask(nsIRunnable* aRunnable)
   microtaskQueue.AppendElement(aRunnable);
 }
 
+#if defined(DOM_PROMISE_DEPRECATED_REPORTING)
 void
 Promise::MaybeReportRejected()
 {
@@ -1042,6 +1072,7 @@ Promise::MaybeReportRejected()
     new AsyncErrorReporter(CycleCollectedJSRuntime::Get()->Runtime(), xpcReport);
   NS_DispatchToMainThread(r);
 }
+#endif // defined(DOM_PROMISE_DEPRECATED_REPORTING)
 
 void
 Promise::MaybeResolveInternal(JSContext* aCx,
@@ -1131,6 +1162,16 @@ Promise::Settle(JS::Handle<JS::Value> aValue, PromiseState aState)
   JSAutoCompartment ac(cx, wrapper);
   JS::dbg::onPromiseSettled(cx, wrapper);
 
+  if (aState == PromiseState::Rejected &&
+      mIsLastInChain) {
+    // The Promise has just been rejected, and it is last in chain.
+    // We need to inform PromiseDebugging.
+    // If the Promise is eventually not the last in chain anymore,
+    // we will need to inform PromiseDebugging again.
+    PromiseDebugging::AddUncaughtRejection(*this);
+  }
+
+#if defined(DOM_PROMISE_DEPRECATED_REPORTING)
   // If the Promise was rejected, and there is no reject handler already setup,
   // watch for thread shutdown.
   if (aState == PromiseState::Rejected &&
@@ -1149,6 +1190,7 @@ Promise::Settle(JS::Handle<JS::Value> aValue, PromiseState aState)
       MaybeReportRejectedOnce();
     }
   }
+#endif // defined(DOM_PROMISE_DEPRECATED_REPORTING)
 
   EnqueueCallbackTasks();
 }
@@ -1183,6 +1225,7 @@ Promise::EnqueueCallbackTasks()
   }
 }
 
+#if defined(DOM_PROMISE_DEPRECATED_REPORTING)
 void
 Promise::RemoveFeature()
 {
@@ -1202,6 +1245,7 @@ PromiseReportRejectFeature::Notify(JSContext* aCx, workers::Status aStatus)
   // After this point, `this` has been deleted by RemoveFeature!
   return true;
 }
+#endif // defined(DOM_PROMISE_DEPRECATED_REPORTING)
 
 bool
 Promise::CaptureStack(JSContext* aCx, JS::Heap<JSObject*>& aTarget)
@@ -1442,6 +1486,14 @@ void Promise::MaybeRejectBrokenly(const nsRefPtr<DOMError>& aArg) {
 template<>
 void Promise::MaybeRejectBrokenly(const nsAString& aArg) {
   MaybeSomething(aArg, &Promise::MaybeReject);
+}
+
+uint64_t
+Promise::GetID() {
+  if (mID != 0) {
+    return mID;
+  }
+  return mID = ++gIDGenerator;
 }
 
 } // namespace dom
--- dom/promise/PromiseDebugging.cpp
+++ dom/promise/PromiseDebugging.cpp
@@ -4,17 +4,64 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "mozilla/dom/PromiseDebugging.h"
-
 #include "js/Value.h"
+#include "nsThreadUtils.h"
 
+#include "mozilla/CycleCollectedJSRuntime.h"
+#include "mozilla/ThreadLocal.h"
 #include "mozilla/TimeStamp.h"
+
 #include "mozilla/dom/BindingDeclarations.h"
+#include "mozilla/dom/ContentChild.h"
 #include "mozilla/dom/Promise.h"
+#include "mozilla/dom/PromiseDebugging.h"
 #include "mozilla/dom/PromiseDebuggingBinding.h"
 
 namespace mozilla {
 namespace dom {
+
+namespace {
+
+class FlushRejections: public nsCancelableRunnable
+{
+public:
+  static void Init() {
+    if (!sDispatched.init()) {
+      MOZ_CRASH("Could not initialize FlushRejections::sDispatched");
+    }
+    sDispatched.set(false);
+  }
+  static void DispatchNeeded() {
+    if (sDispatched.get()) {
+      // An instance of `FlushRejections` has already been dispatched
+      // and not run yet. No need to dispatch another one.
+      return;
+    }
+    sDispatched.set(true);
+    NS_DispatchToCurrentThread(new FlushRejections());
+  }
+  nsresult Run()
+  {
+    sDispatched.set(false);
+
+    // Call the callbacks if necessary.
+    // Note that these callbacks may in turn cause Promise to turn
+    // uncaught or consumed. Since `sDispatched` is `false`,
+    // `FlushRejections` will be called once again, on an ulterior
+    // tick.
+    PromiseDebugging::FlushUncaughtRejections();
+    return NS_OK;
+  }
+private:
+  // `true` if an instance of `FlushRejections` is currently dispatched
+  // and has not been executed yet.
+  static ThreadLocal<bool> sDispatched;
+};
+
+/* static */ ThreadLocal<bool>
+FlushRejections::sDispatched;
+
+} // namespace
 
 /* static */ void
 PromiseDebugging::GetState(GlobalObject&, Promise& aPromise,
@@ -35,6 +82,30 @@ PromiseDebugging::GetState(GlobalObject&, Promise& aPromise,
     aState.mReason = aPromise.mResult;
     break;
   }
+}
+
+/*static */ nsString
+PromiseDebugging::sIDPrefix;
+
+/* static */ void
+PromiseDebugging::Init()
+{
+  FlushRejections::Init();
+
+  // Generate a prefix for identifiers: "PromiseDebugging.$processid."
+  sIDPrefix = NS_LITERAL_STRING("PromiseDebugging.");
+  if (XRE_GetProcessType() == GeckoProcessType_Content) {
+    sIDPrefix.AppendInt(ContentChild::GetSingleton()->GetID());
+    sIDPrefix.Append('.');
+  } else {
+    sIDPrefix.AppendLiteral("0.");
+  }
+}
+
+/* static */ void
+PromiseDebugging::Shutdown()
+{
+  sIDPrefix.SetIsVoid(true);
 }
 
 /* static */ void
@@ -81,6 +152,108 @@ PromiseDebugging::GetTimeToSettle(GlobalObject&, Promise& aPromise,
   }
   return (aPromise.mSettlementTimestamp -
           aPromise.mCreationTimestamp).ToMilliseconds();
+}
+
+/* static */ void
+PromiseDebugging::AddUncaughtRejectionObserver(GlobalObject&,
+                                               UncaughtRejectionObserver& aObserver)
+{
+  CycleCollectedJSRuntime* storage = CycleCollectedJSRuntime::Get();
+  nsTArray<nsRefPtr<UncaughtRejectionObserver>>& observers = storage->mUncaughtRejectionObservers;
+  observers.AppendElement(&aObserver);
+}
+
+/* static */ void
+PromiseDebugging::RemoveUncaughtRejectionObserver(GlobalObject&,
+                                                  UncaughtRejectionObserver& aObserver)
+{
+  CycleCollectedJSRuntime* storage = CycleCollectedJSRuntime::Get();
+  nsTArray<nsRefPtr<UncaughtRejectionObserver>>& observers = storage->mUncaughtRejectionObservers;
+  for (size_t i = 0; i < observers.Length(); ++i) {
+    if (*observers[i] == aObserver) {
+      observers.RemoveElementAt(i);
+      return;
+    }
+  }
+}
+
+/* static */ void
+PromiseDebugging::AddUncaughtRejection(Promise& aPromise)
+{
+  CycleCollectedJSRuntime::Get()->mUncaughtRejections.AppendElement(&aPromise);
+  FlushRejections::DispatchNeeded();
+}
+
+/* void */ void
+PromiseDebugging::AddConsumedRejection(Promise& aPromise)
+{
+  CycleCollectedJSRuntime::Get()->mConsumedRejections.AppendElement(&aPromise);
+  FlushRejections::DispatchNeeded();
+}
+
+/* static */ void
+PromiseDebugging::GetPromiseID(GlobalObject&,
+                               Promise& aPromise,
+                               nsString& aID)
+{
+  uint64_t promiseID = aPromise.GetID();
+  aID = sIDPrefix;
+  aID.AppendInt(promiseID);
+}
+
+/* static */ void
+PromiseDebugging::FlushUncaughtRejections()
+{
+  CycleCollectedJSRuntime* storage = CycleCollectedJSRuntime::Get();
+
+  // The Promise that have been left uncaught (rejected and last in
+  // their chain) since the last call to this function.
+  nsTArray<nsRefPtr<Promise>> uncaught;
+  storage->mUncaughtRejections.SwapElements(uncaught);
+
+  // The Promise that have been left uncaught at some point, but that
+  // have eventually had their `then` method called.
+  nsTArray<nsRefPtr<Promise>> consumed;
+  storage->mConsumedRejections.SwapElements(consumed);
+
+  nsTArray<nsRefPtr<UncaughtRejectionObserver>>& observers = storage->mUncaughtRejectionObservers;
+
+  // Notify observers of uncaught Promise.
+
+  for (size_t i = 0; i < uncaught.Length(); ++i) {
+    nsRefPtr<Promise> promise = uncaught[i];
+    if (!promise->IsLastInChain()) {
+      // This promise is not the last in the chain anymore,
+      // so the error has been caught at some point.
+      continue;
+    }
+
+    // For the moment, the Promise is still at the end of the
+    // chain. Let's inform observers, so that they may decide whether
+    // to report it.
+    for (size_t j = 0; j < observers.Length(); ++j) {
+      ErrorResult rv;
+      observers[j]->OnLeftUncaught(*promise, rv);
+      // Ignore errors
+    }
+
+    promise->SetNotifiedAsUncaught();
+  }
+
+  // Notify observers of consumed Promise.
+
+  for (size_t i = 0; i < consumed.Length(); ++i) {
+    nsRefPtr<Promise> promise = consumed[i];
+    if (!promise->WasNotifiedAsUncaught()) {
+      continue;
+    }
+
+    MOZ_ASSERT(!promise->IsLastInChain());
+    for (size_t j = 0; j < observers.Length(); ++j) {
+      ErrorResult rv;
+      observers[j]->OnConsumed(*promise, rv); // Ignore errors
+    }
+  }
 }
 
 } // namespace dom
