f9cfe19c639cfc54f2665180dca21f93b271bda2
Jared Wein <jwein@mozilla.com>
2014-11-19 13:29:27 -0500

    Bug 1101006 - Refactor mozLoop.{get, set}LoopCharPref and mozLoop.{get, set}LoopBoolPref to mozLoop.{get, set}Pref that uses getPrefType. r=mikedeboer

--- browser/components/loop/test/xpcshell/test_loopservice_loop_prefs.js
+++ browser/components/loop/test/xpcshell/test_loopservice_loop_prefs.js
@@ -6,39 +6,39 @@ var fakeCharPrefName = "color";
 var fakeBoolPrefName = "boolean";
 var fakePrefValue = "green";
 
-function test_getLoopCharPref()
+function test_getLoopPref()
 {
   Services.prefs.setCharPref("loop." + fakeCharPrefName, fakePrefValue);
 
-  var returnedPref = MozLoopService.getLoopCharPref(fakeCharPrefName);
+  var returnedPref = MozLoopService.getLoopPref(fakeCharPrefName, Ci.nsIPrefBranch.PREF_STRING);
 
   Assert.equal(returnedPref, fakePrefValue,
     "Should return a char pref under the loop. branch");
   Services.prefs.clearUserPref("loop." + fakeCharPrefName);
 }
 
-function test_getLoopCharPref_not_found()
+function test_getLoopPref_not_found()
 {
-  var returnedPref = MozLoopService.getLoopCharPref(fakeCharPrefName);
+  var returnedPref = MozLoopService.getLoopPref(fakeCharPrefName);
 
   Assert.equal(returnedPref, null,
     "Should return null if a preference is not found");
 }
 
-function test_getLoopCharPref_non_coercible_type()
+function test_getLoopPref_non_coercible_type()
 {
   Services.prefs.setBoolPref("loop." + fakeCharPrefName, false);
 
-  var returnedPref = MozLoopService.getLoopCharPref(fakeCharPrefName);
+  var returnedPref = MozLoopService.getLoopPref(fakeCharPrefName, Ci.nsIPrefBranch.PREF_STRING);
 
   Assert.equal(returnedPref, null,
     "Should return null if the preference exists & is of a non-coercible type");
 }
 
-function test_setLoopCharPref()
+function test_setLoopPref()
 {
   Services.prefs.setCharPref("loop." + fakeCharPrefName, "red");
-  MozLoopService.setLoopCharPref(fakeCharPrefName, fakePrefValue);
+  MozLoopService.setLoopPref(fakeCharPrefName, fakePrefValue);
 
   var returnedPref = Services.prefs.getCharPref("loop." + fakeCharPrefName);
 
@@ -47,10 +47,10 @@ function test_setLoopCharPref()
   Services.prefs.clearUserPref("loop." + fakeCharPrefName);
 }
 
-function test_setLoopCharPref_new()
+function test_setLoopPref_new()
 {
   Services.prefs.clearUserPref("loop." + fakeCharPrefName);
-  MozLoopService.setLoopCharPref(fakeCharPrefName, fakePrefValue);
+  MozLoopService.setLoopPref(fakeCharPrefName, fakePrefValue, Ci.nsIPrefBranch.PREF_STRING);
 
   var returnedPref = Services.prefs.getCharPref("loop." + fakeCharPrefName);
 
@@ -59,28 +59,28 @@ function test_setLoopCharPref_new()
   Services.prefs.clearUserPref("loop." + fakeCharPrefName);
 }
 
-function test_setLoopCharPref_non_coercible_type()
+function test_setLoopPref_non_coercible_type()
 {
-  MozLoopService.setLoopCharPref(fakeCharPrefName, true);
+  MozLoopService.setLoopPref(fakeCharPrefName, true);
 
   ok(true, "Setting non-coercible type should not fail");
 }
 
 
-function test_getLoopBoolPref()
+function test_getLoopPref_bool()
 {
   Services.prefs.setBoolPref("loop." + fakeBoolPrefName, true);
 
-  var returnedPref = MozLoopService.getLoopBoolPref(fakeBoolPrefName);
+  var returnedPref = MozLoopService.getLoopPref(fakeBoolPrefName);
 
   Assert.equal(returnedPref, true,
     "Should return a bool pref under the loop. branch");
   Services.prefs.clearUserPref("loop." + fakeBoolPrefName);
 }
 
-function test_getLoopBoolPref_not_found()
+function test_getLoopPref_not_found_bool()
 {
-  var returnedPref = MozLoopService.getLoopBoolPref(fakeBoolPrefName);
+  var returnedPref = MozLoopService.getLoopPref(fakeBoolPrefName);
 
   Assert.equal(returnedPref, null,
     "Should return null if a preference is not found");
@@ -91,15 +91,15 @@ function run_test()
 {
   setupFakeLoopServer();
 
-  test_getLoopCharPref();
-  test_getLoopCharPref_not_found();
-  test_getLoopCharPref_non_coercible_type();
-  test_setLoopCharPref();
-  test_setLoopCharPref_new();
-  test_setLoopCharPref_non_coercible_type();
+  test_getLoopPref();
+  test_getLoopPref_not_found();
+  test_getLoopPref_non_coercible_type();
+  test_setLoopPref();
+  test_setLoopPref_new();
+  test_setLoopPref_non_coercible_type();
 
-  test_getLoopBoolPref();
-  test_getLoopBoolPref_not_found();
+  test_getLoopPref_bool();
+  test_getLoopPref_not_found_bool();
 
   do_register_cleanup(function() {
     Services.prefs.clearUserPref("loop." + fakeCharPrefName);
