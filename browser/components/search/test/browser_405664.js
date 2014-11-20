787319d8255b20bad78ae2077c0d437636752e70
Florian Quèze <florian@queze.net>
2014-11-20 20:55:38 +0100

    Bug 1088660 - Improve the search bar UI to support one-off searches, r=felipe, a=gavin.

--- browser/components/search/test/browser_405664.js
+++ browser/components/search/test/browser_405664.js
@@ -1,7 +1,10 @@
 function test() {
   var searchBar = BrowserSearch.searchBar;
   ok(searchBar, "got search bar");
-  
+
+  if (searchBar.getAttribute("oneoffui"))
+    return; // The oneoffui removes the menu that's being tested here.
+
   searchBar.focus();
 
   var pbo = searchBar._popup.popupBoxObject;
