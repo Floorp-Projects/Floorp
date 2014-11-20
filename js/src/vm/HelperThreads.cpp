93c4c06dbcace949e6a6b9f17a699bc5c58d6655
Hannes Verschore <hv1989@gmail.com>
2014-11-20 17:44:00 +0100

    Bug 1072903 - TraceLogger: Part 3: Refactor into TraceLoggerGraph, TraceLoggerThread and TraceLoggerThreadState, r=bbouvier

--- js/src/vm/HelperThreads.cpp
+++ js/src/vm/HelperThreads.cpp
@@ -1047,7 +1047,7 @@ HelperThread::handleIonWorkload()
     ionBuilder = builder;
     ionBuilder->setPauseFlag(&pause);
 
-    TraceLogger *logger = TraceLoggerForCurrentThread();
+    TraceLoggerThread *logger = TraceLoggerForCurrentThread();
     AutoTraceLog logScript(logger, TraceLogCreateTextId(logger, ionBuilder->script()));
     AutoTraceLog logCompile(logger, TraceLogger_IonCompilation);
 
@@ -1117,7 +1117,7 @@ CurrentHelperThread()
 void
 js::PauseCurrentHelperThread()
 {
-    TraceLogger *logger = TraceLoggerForCurrentThread();
+    TraceLoggerThread *logger = TraceLoggerForCurrentThread();
     AutoTraceLog logPaused(logger, TraceLogger_IonCompilationPaused);
 
     HelperThread *thread = CurrentHelperThread();
