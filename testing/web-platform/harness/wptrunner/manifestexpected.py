e8f72798057ac4fc4f79bfc5e5383139b444e03a
James Graham <james@hoppipolla.co.uk>
2014-11-20 16:29:59 +0000

    Bug 1097230 - Update to latest wptrunner, a=testonly

--- testing/web-platform/harness/wptrunner/manifestexpected.py
+++ testing/web-platform/harness/wptrunner/manifestexpected.py
@@ -2,6 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
+import urlparse
+
 from wptmanifest.backends import static
 from wptmanifest.backends.static import ManifestItem
 
@@ -27,21 +30,25 @@ def data_cls_getter(output_node, visited_node):
 
 
 class ExpectedManifest(ManifestItem):
-    def __init__(self, name, test_path):
+    def __init__(self, name, test_path, url_base):
         """Object representing all the tests in a particular manifest
 
         :param name: Name of the AST Node associated with this object.
                      Should always be None since this should always be associated with
                      the root node of the AST.
         :param test_path: Path of the test file associated with this manifest.
+        :param url_base: Base url for serving the tests in this manifest
         """
         if name is not None:
             raise ValueError("ExpectedManifest should represent the root node")
         if test_path is None:
             raise ValueError("ExpectedManifest requires a test path")
+        if url_base is None:
+            raise ValueError("ExpectedManifest requires a base url")
         ManifestItem.__init__(self, name)
         self.child_map = {}
         self.test_path = test_path
+        self.url_base = url_base
 
     def append(self, child):
         """Add a test to the manifest"""
@@ -59,6 +66,11 @@ class ExpectedManifest(ManifestItem):
 
         :param test_id: ID of the test to return."""
         return self.child_map.get(test_id)
+
+    @property
+    def url(self):
+        return urlparse.urljoin(self.url_base,
+                                "/".join(self.test_path.split(os.path.sep)))
 
 
 class TestNode(ManifestItem):
@@ -89,9 +101,7 @@ class TestNode(ManifestItem):
 
     @property
     def id(self):
-        components = self.parent.test_path.split("/")[:-1]
-        components.append(self.name)
-        url = "/" + "/".join(components)
+        url = urlparse.urljoin(self.parent.url, self.name)
         if self.test_type == "reftest":
             return (url, self.get("reftype"), self.get("refurl"))
         else:
@@ -134,12 +144,13 @@ class SubtestNode(TestNode):
         return True
 
 
-def get_manifest(metadata_root, test_path, run_info):
+def get_manifest(metadata_root, test_path, url_base, run_info):
     """Get the ExpectedManifest for a particular test path, or None if there is no
     metadata stored for that test path.
 
     :param metadata_root: Absolute path to the root of the metadata directory
     :param test_path: Path to the test(s) relative to the test root
+    :param url_base: Base url for serving the tests in this manifest
     :param run_info: Dictionary of properties of the test run for which the expectation
                      values should be computed.
     """
@@ -148,6 +159,7 @@ def get_manifest(metadata_root, test_path, run_info):
         with open(manifest_path) as f:
             return static.compile(f, run_info,
                                   data_cls_getter=data_cls_getter,
-                                  test_path=test_path)
+                                  test_path=test_path,
+                                  url_base=url_base)
     except IOError:
         return None
