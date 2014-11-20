fba93c8949d04c536bf0cdb519dc76b92ca1a373
Dão Gottwald <dao@mozilla.com>
2014-11-20 14:36:04 +0100

    Bug 1102018 - Fix and enable browser_bug481560.js for e10s. r=gijs

--- browser/base/content/test/general/browser_bug481560.js
+++ browser/base/content/test/general/browser_bug481560.js
@@ -1,14 +1,8 @@
 function test() {
   waitForExplicitFinish();
 
-  var win = openDialog(getBrowserURL(), "_blank", "chrome,all,dialog=no");
-
-  win.addEventListener("load", function () {
-    win.removeEventListener("load", arguments.callee, false);
-
-    win.content.addEventListener("focus", function () {
-      win.content.removeEventListener("focus", arguments.callee, false);
-
+  whenNewWindowLoaded(null, function (win) {
+    waitForFocus(function () {
       function onTabClose() {
         ok(false, "shouldn't have gotten the TabClose event for the last tab");
       }
@@ -22,6 +16,6 @@ function test() {
       tab.removeEventListener("TabClose", onTabClose, false);
 
       finish();
-    }, false);
-  }, false);
+    }, win);
+  });
 }
