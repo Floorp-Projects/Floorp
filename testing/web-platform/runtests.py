f099ecf5a5684c4bebda423e04979f52f36fc60d
James Graham <james@hoppipolla.co.uk>
2014-11-20 16:30:00 +0000

    Bug 1097230 - Build and configuration changes for web-platform-tests update. r=ted
    --HG--
    rename : testing/web-platform/moz.build => testing/web-platform/mozilla/tests/placeholder
    rename : testing/web-platform/moz.build => testing/web-platform/outbound/tests/placeholder
    extra : rebase_source : 45498e733e8de0631e75b50a9a38138a82414c53

--- testing/web-platform/runtests.py
+++ testing/web-platform/runtests.py
@@ -4,7 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
 import sys
+
+here = os.path.split(os.path.abspath(__file__))[0]
+sys.path.insert(0, os.path.join(here, "harness"))
 
 from wptrunner import wptrunner
 
