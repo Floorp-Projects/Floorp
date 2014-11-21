a288f3be7dfd2fe55ab1f85f29fce46b6e0cb30e
Mason Chang <mchang@mozilla.com>
2014-11-21 09:16:19 -0800

    Bug 1101974. Part 2: Initialize vsync source during compositor creation. r=mstange

--- gfx/thebes/gfxPlatform.h
+++ gfx/thebes/gfxPlatform.h
@@ -44,6 +44,9 @@ namespace gl {
 class GLContext;
 class SkiaGLGlue;
 }
+namespace layers {
+class VsyncSource;
+}
 namespace gfx {
 class DrawTarget;
 class SourceSurface;
@@ -583,17 +586,18 @@ public:
     static bool UsesOffMainThreadCompositing();
 
     bool HasEnoughTotalSystemMemoryForSkiaGL();
+
+    /**
+     * Initialize and get hardware vsync based on each platform.
+     */
+    virtual already_AddRefed<mozilla::layers::VsyncSource> GetVsyncSource();
+
 protected:
     gfxPlatform();
     virtual ~gfxPlatform();
 
     void AppendCJKPrefLangs(eFontPrefLang aPrefLangs[], uint32_t &aLen,
                             eFontPrefLang aCharLang, eFontPrefLang aPageLang);
-
-    /**
-     * Initialized hardware vsync based on each platform.
-     */
-    virtual void InitHardwareVsync() {}
 
     /**
      * Helper method, creates a draw target for a specific Azure backend.
