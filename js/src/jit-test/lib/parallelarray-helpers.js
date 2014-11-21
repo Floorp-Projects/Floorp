1bbf3525400bacc01c0f80713267dd9821196f55
Shu-yu Guo <shu@rfrn.org>
2014-11-20 16:11:57 -0800

    Bug 1097280 - Disable intermittent PJS tests. (r=me)

--- js/src/jit-test/lib/parallelarray-helpers.js
+++ js/src/jit-test/lib/parallelarray-helpers.js
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// disabled due to intermittent
+quit(0);
+
 // Explanation of minItemsTestingThreshold:
 //
 // If the volume of input items in a test is small, then all of them
