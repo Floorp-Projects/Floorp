decdbdce7c20cbd1956fb29edf9af2e9be31d5a9
Jared Wein <jwein@mozilla.com>
2014-11-21 15:53:54 -0500

    Bug 1101754 - Hide the rooms/contacts view until the Getting Started tour has been accessed or dismissed. r=mikedeboer

--- browser/components/loop/content/js/panel.jsx
+++ browser/components/loop/content/js/panel.jsx
@@ -165,12 +165,11 @@ loop.panel = (function(_, mozL10n) {
   });
 
   var GettingStartedView = React.createClass({
-    componentDidMount: function() {
-      navigator.mozLoop.setLoopPref("gettingStarted.seen", true);
-    },
-
     handleButtonClick: function() {
       navigator.mozLoop.openGettingStartedTour();
+      navigator.mozLoop.setLoopPref("gettingStarted.seen", true);
+      var event = new CustomEvent("GettingStartedSeen");
+      window.dispatchEvent(event);
     },
 
     render: function() {
@@ -694,6 +693,7 @@ loop.panel = (function(_, mozL10n) {
     getInitialState: function() {
       return {
         userProfile: this.props.userProfile || navigator.mozLoop.userProfile,
+        gettingStartedSeen: navigator.mozLoop.getLoopPref("gettingStarted.seen"),
       };
     },
 
@@ -741,6 +741,12 @@ loop.panel = (function(_, mozL10n) {
       this.updateServiceErrors();
     },
 
+    _gettingStartedSeen: function() {
+      this.setState({
+        gettingStartedSeen: navigator.mozLoop.getLoopPref("gettingStarted.seen"),
+      });
+    },
+
     /**
      * The rooms feature is hidden by default for now. Once it gets mainstream,
      * this method can be simplified.
@@ -750,7 +756,6 @@ loop.panel = (function(_, mozL10n) {
         return (
           <Tab name="call">
             <div className="content-area">
-              <GettingStartedView />
               <CallUrlResult client={this.props.client}
                              notifications={this.props.notifications}
                              callUrl={this.props.callUrl} />
@@ -762,7 +767,6 @@ loop.panel = (function(_, mozL10n) {
 
       return (
         <Tab name="rooms">
-          <GettingStartedView />
           <RoomList dispatcher={this.props.dispatcher}
                     store={this.props.roomStore}
                     userDisplayName={this._getUserDisplayName()}/>
@@ -786,10 +790,12 @@ loop.panel = (function(_, mozL10n) {
 
     componentDidMount: function() {
       window.addEventListener("LoopStatusChanged", this._onStatusChanged);
+      window.addEventListener("GettingStartedSeen", this._gettingStartedSeen);
     },
 
     componentWillUnmount: function() {
       window.removeEventListener("LoopStatusChanged", this._onStatusChanged);
+      window.removeEventListener("GettingStartedSeen", this._gettingStartedSeen);
     },
 
     _getUserDisplayName: function() {
@@ -799,6 +805,17 @@ loop.panel = (function(_, mozL10n) {
 
     render: function() {
       var NotificationListView = sharedViews.NotificationListView;
+
+      if (!this.state.gettingStartedSeen) {
+        return (
+          <div>
+            <NotificationListView notifications={this.props.notifications}
+                                  clearOnDocumentHidden={true} />
+            <GettingStartedView />
+            <ToSView />
+          </div>
+        );
+      }
 
       return (
         <div>
