35590d8e1732e55a9579cb1c8be8a4df81b76d44
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-11-21 08:03:49 +0100

    Backed out changeset 54c8f117c3db (bug 1083694)

--- js/src/vm/TraceLogging.h
+++ js/src/vm/TraceLogging.h
@@ -266,6 +266,14 @@ class TraceLoggerThread
     static unsigned offsetOfEnabled() {
         return offsetof(TraceLoggerThread, enabled);
     }
+
+  private:
+    void assertNoQuotes(const char *text) {
+#ifdef DEBUG
+        const char *quote = strchr(text, '"');
+        MOZ_ASSERT(!quote);
+#endif
+    }
 #endif
 };
 
