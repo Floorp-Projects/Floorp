174983d7b3b9ec522829bd83456ef4a907c76ee9
James Graham <james@hoppipolla.co.uk>
2014-11-24 12:56:30 +0000

    Bug 1103906 - Update web-platform-tests to revision 551060ba2eb3fa6d1c713c179d52fdf1132ea50c, a=testonly
    --HG--
    rename : testing/web-platform/tests/dom/nodes/Document-createComment.html => testing/web-platform/tests/dom/nodes/Document-createTextNode.html
    rename : testing/web-platform/tests/selectors-api/tests/submissions/Opera/level2-baseline.html => testing/web-platform/tests/selectors-api/tests/submissions/Opera/Element-matches.html
    rename : testing/web-platform/tests/selectors-api/tests/submissions/Opera/level2-baseline.html => testing/web-platform/tests/selectors-api/tests/submissions/Opera/ParentNode-find-findAll.html
    rename : testing/web-platform/tests/selectors-api/tests/submissions/Opera/level2-lib.js => testing/web-platform/tests/selectors-api/tests/submissions/Opera/ParentNode-find-findAll.js

--- testing/web-platform/tests/XMLHttpRequest/resources/auth4/auth.py
+++ testing/web-platform/tests/XMLHttpRequest/resources/auth4/auth.py
@@ -1,9 +1,10 @@
 import imp
 import os
 
+here = os.path.split(os.path.abspath(__file__))[0]
+
 def main(request, response):
-    auth = imp.load_source("", os.path.join(os.path.abspath(os.curdir),
-                                            "XMLHttpRequest",
-                                            "resources",
+    auth = imp.load_source("", os.path.join(here,
+                                            "..",
                                             "authentication.py"))
     return auth.main(request, response)
--- testing/web-platform/tests/dom/nodes/attributes.js
+++ testing/web-platform/tests/dom/nodes/attributes.js
@@ -1,5 +1,6 @@
 function attr_is(attr, v, ln, ns, p, n) {
   assert_equals(attr.value, v)
+  assert_equals(attr.nodeValue, v)
   assert_equals(attr.textContent, v)
   assert_equals(attr.localName, ln)
   assert_equals(attr.namespaceURI, ns)
