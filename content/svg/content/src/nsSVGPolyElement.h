2e8a126d995fcc9961c9c95c2f096ed506a23cd6
Ryan VanderMeulen <ryanvm@gmail.com>
2014-11-25 17:58:28 -0500

    Backed out changeset 4d72f5451cd0 (bug 1099197) for bustage.

--- content/svg/content/src/nsSVGPolyElement.h
+++ content/svg/content/src/nsSVGPolyElement.h
@@ -46,8 +46,6 @@ public:
   virtual bool IsMarkable() MOZ_OVERRIDE { return true; }
   virtual void GetMarkPoints(nsTArray<nsSVGMark> *aMarks) MOZ_OVERRIDE;
   virtual mozilla::TemporaryRef<Path> BuildPath(PathBuilder* aBuilder) MOZ_OVERRIDE;
-  virtual bool GetGeometryBounds(Rect* aBounds, Float aStrokeWidth,
-                                 const Matrix& aTransform) MOZ_OVERRIDE;
 
   // WebIDL
   already_AddRefed<mozilla::DOMSVGPointList> Points();
