886947316bec76911bb032dd5d8afb8117eb5ca5
Dan Minor <dminor@mozilla.com>
2014-11-25 13:10:02 -0500

    Bug 1101133 - Change jit-test packaging to work properly even if test-stage cleanup fails; r=ted

--- testing/testsuite-targets.mk
+++ testing/testsuite-targets.mk
@@ -520,11 +520,11 @@ endif
 
 stage-jittest: make-stage-dir
 	$(NSINSTALL) -D $(PKG_STAGE)/jit-test/tests
-	cp -RL $(topsrcdir)/js/src/jsapi.h $(PKG_STAGE)/jit-test
-	cp -RL $(topsrcdir)/js/src/jit-test $(PKG_STAGE)/jit-test/jit-test
-	cp -RL $(topsrcdir)/js/src/tests/ecma_6 $(PKG_STAGE)/jit-test/tests/ecma_6
-	cp -RL $(topsrcdir)/js/src/tests/js1_8_5 $(PKG_STAGE)/jit-test/tests/js1_8_5
-	cp -RL $(topsrcdir)/js/src/tests/lib $(PKG_STAGE)/jit-test/tests/lib
+	cp -RL $(topsrcdir)/js/src/jsapi.h $(PKG_STAGE)/jit-test/
+	cp -RL $(topsrcdir)/js/src/jit-test $(PKG_STAGE)/jit-test/
+	cp -RL $(topsrcdir)/js/src/tests/ecma_6 $(PKG_STAGE)/jit-test/tests/
+	cp -RL $(topsrcdir)/js/src/tests/js1_8_5 $(PKG_STAGE)/jit-test/tests/
+	cp -RL $(topsrcdir)/js/src/tests/lib $(PKG_STAGE)/jit-test/tests/
 
 stage-steeplechase: make-stage-dir
 	$(NSINSTALL) -D $(PKG_STAGE)/steeplechase/
