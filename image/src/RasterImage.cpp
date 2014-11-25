b4c2637dc603e73a266653978e3a49deb193784f
Carsten "Tomcat" Book <cbook@mozilla.com>
2014-11-25 14:13:58 +0100

    Backed out changeset f993ad4dc8f6 (bug 1060869)

--- image/src/RasterImage.cpp
+++ image/src/RasterImage.cpp
@@ -205,8 +205,7 @@ public:
     // Insert the new surface into the cache immediately. We need to do this so
     // that we won't start multiple scaling jobs for the same size.
     SurfaceCache::Insert(mDstRef.get(), ImageKey(mImage.get()),
-                         RasterSurfaceKey(mDstSize.ToIntSize(), mImageFlags),
-                         Lifetime::Transient);
+                         RasterSurfaceKey(mDstSize.ToIntSize(), mImageFlags));
 
     return true;
   }
@@ -260,9 +259,9 @@ public:
       NS_WARNING("HQ scaling failed");
 
       // Remove the frame from the cache since we know we don't need it.
-      SurfaceCache::RemoveSurface(ImageKey(mImage.get()),
-                                  RasterSurfaceKey(mDstSize.ToIntSize(),
-                                                   mImageFlags));
+      SurfaceCache::RemoveIfPresent(ImageKey(mImage.get()),
+                                    RasterSurfaceKey(mDstSize.ToIntSize(),
+                                                     mImageFlags));
 
       // Release everything we're holding, too.
       mSrcRef.reset();
@@ -365,7 +364,7 @@ RasterImage::~RasterImage()
   }
 
   // Release any HQ scaled frames from the surface cache.
-  SurfaceCache::RemoveImage(ImageKey(this));
+  SurfaceCache::Discard(this);
 
   mAnim = nullptr;
 
