46f6d764139475a61e2cc9c0b36f39ad05bd3406
Brad Lassey <blassey@mozilla.com>
2014-11-25 13:04:07 -0500

    bug 1104160 - crash in CPOWTimer::~CPOWTimer() r=billm

--- js/ipc/CPOWTimer.cpp
+++ js/ipc/CPOWTimer.cpp
@@ -9,10 +9,19 @@
 #include "xpcprivate.h"
 #include "CPOWTimer.h"
 
-CPOWTimer::~CPOWTimer() {
+CPOWTimer::~CPOWTimer()
+{
     /* This is a best effort to find the compartment responsible for this CPOW call */
-    xpc::CompartmentPrivate* compartment = xpc::CompartmentPrivate::Get(js::GetObjectCompartment(mozilla::dom::GetIncumbentGlobal()
-                                                                                                 ->GetGlobalJSObject()));
+    nsIGlobalObject *global = mozilla::dom::GetIncumbentGlobal();
+    if (!global)
+        return;
+    JSObject *obj = global->GetGlobalJSObject();
+    if (!obj)
+        return;
+    JSCompartment *compartment = js::GetObjectCompartment(obj);
+    xpc::CompartmentPrivate *compartmentPrivate = xpc::CompartmentPrivate::Get(compartment);
+    if (!compartmentPrivate)
+        return;
     PRIntervalTime time = PR_IntervalNow() - startInterval;
-    compartment->CPOWTime += time;
+    compartmentPrivate->CPOWTime += time;
 }
