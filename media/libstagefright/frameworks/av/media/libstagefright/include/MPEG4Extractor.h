e5ee500a8e0bed07471cc8cea9ad13d430579535
Matt Woodrow <mwoodrow@mozilla.com>
2014-11-26 18:00:31 +1300

    Bug 1102666 - Parse the edts block in MoofParser and offset composition time accordingly. r=ajones

--- media/libstagefright/frameworks/av/media/libstagefright/include/MPEG4Extractor.h
+++ media/libstagefright/frameworks/av/media/libstagefright/include/MPEG4Extractor.h
@@ -86,6 +86,11 @@ private:
         Track *next;
         sp<MetaData> meta;
         uint32_t timescale;
+        // Temporary storage for elst until we've
+        // parsed mdhd and can interpret them.
+        uint64_t segment_duration;
+        int64_t media_time;
+
         sp<SampleTable> sampleTable;
         bool includes_expensive_metadata;
         bool skipTrack;
@@ -140,6 +145,8 @@ private:
     status_t parseTrackHeader(off64_t data_offset, off64_t data_size);
 
     status_t parseSegmentIndex(off64_t data_offset, size_t data_size);
+
+    void storeEditList();
 
     Track *findTrackByMimePrefix(const char *mimePrefix);
 
