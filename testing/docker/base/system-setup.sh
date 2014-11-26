c5c858b98b7c80ea74476986624fa7305c3c3507
Wander Lairson Costa <wcosta@mozilla.com>
2014-11-26 10:11:29 -0800

    Bug 1087456 part1: Update base image to CentOS 7. r=lightsofapollo,garndt
    releng uses CentOS for CI. In order to reuse their packages, we also
    move from Ubuntu to CentOS for our base images.
    
    --HG--
    extra : rebase_source : 4c98b24c094391124c5c0239c9191ad83938d914
    extra : source : 7392eb8c25c4bec041ac480064c8bdd1c0bdecb8

--- testing/docker/base/system-setup.sh
+++ testing/docker/base/system-setup.sh
@@ -7,20 +7,22 @@ test `whoami` == 'root';
 
 ### Add worker user
 # Minimize the number of things which the build script can do, security-wise
-# it's not a problem to let the build script install things with apt-get. But it
+# it's not a problem to let the build script install things with yum. But it
 # really shouldn't do this, so let's forbid root access.
 useradd -d /home/worker -s /bin/bash -m worker;
 
+# Install extra package mirror
+yum install -y epel-release
+
 ### Install Useful Packages
 # First we update and upgrade to latest versions.
-apt-get update;
-apt-get upgrade -y;
+yum update -y
 
 # Let's install some goodies, ca-certificates is needed for https with hg.
 # sudo will be required anyway, but let's make it explicit. It nice to have
 # sudo around. We'll also install nano, this is pure bloat I know, but it's
 # useful a text editor.
-apt-get install -y                  \
+yum install -y                      \
   ca-certificates                   \
   sudo                              \
   nano                              \
@@ -28,47 +30,104 @@ apt-get install -y                  \
 
 # Then let's install all firefox build dependencies, these are extracted from
 # mozboot. See python/mozboot/bin/bootstrap.py in mozilla-central.
-apt-get install -y                  \
-  autoconf2.13                      \
-  build-essential                   \
-  ccache                            \
-  libasound2-dev                    \
-  libcurl4-openssl-dev              \
-  libdbus-1-dev                     \
-  libdbus-glib-1-dev                \
-  libgconf2-dev                     \
-  libgstreamer0.10-dev              \
-  libgstreamer-plugins-base0.10-dev \
-  libgtk2.0-dev                     \
-  libiw-dev                         \
-  libnotify-dev                     \
-  libpulse-dev                      \
-  libxt-dev                         \
+yum groupinstall -y                 \
+  "Development Tools"               \
+  "Development Libraries"           \
+  "GNOME Software Development"
+
+yum install -y                      \
+  alsa-lib-devel                    \
+  autoconf213                       \
+  curl-devel                        \
+  dbus-glib-devel                   \
+  glibc-static                      \
+  gstreamer-devel                   \
+  gstreamer-plugins-base-devel      \
+  gtk2-devel                        \
+  libstdc++-static                  \
+  libXt-devel                       \
   mercurial                         \
-  mesa-common-dev                   \
-  python-dev                        \
+  mesa-libGL-devel                  \
+  pulseaudio-libs-devel             \
+  wireless-tools-devel              \
+  yasm                              \
+  python-devel                      \
+  dbus-python                       \
+  ;
+
+yum install -y                      \
+  alsa-lib-devel                    \
+  libcurl-devel                     \
+  openssl-devel                     \
+  dbus-devel                        \
+  dbus-glib-devel                   \
+  GConf2-devel                      \
+  iw                                \
+  libnotify-devel                   \
+  pulseaudio-libs-devel             \
+  libXt-devel                       \
+  mercurial                         \
   unzip                             \
   uuid                              \
-  yasm                              \
-  xvfb                              \
+  xorg-x11-server-Xvfb              \
+  tcl                               \
+  tk                                \
+  ;
+
+# From Building B2G docs
+yum install -y                      \
+  install                           \
+  autoconf213                       \
+  bison                             \
+  bzip2                             \
+  ccache                            \
+  curl                              \
+  flex                              \
+  gawk                              \
+  gcc-c++                           \
+  git                               \
+  glibc-devel                       \
+  glibc-static                      \
+  libstdc++-static                  \
+  libX11-devel                      \
+  make                              \
+  mesa-libGL-devel                  \
+  ncurses-devel                     \
+  patch                             \
+  zlib-devel                        \
+  ncurses-devel.i686                \
+  readline-devel.i686               \
+  zlib-devel.i686                   \
+  libX11-devel.i686                 \
+  mesa-libGL-devel.i686             \
+  glibc-devel.i686                  \
+  libstdc++.i686                    \
+  libXrandr.i686                    \
   zip                               \
-  software-properties-common        \
+  perl-Digest-SHA                   \
+  wget                              \
   ;
 
 # Install some utilities, we'll be using nodejs in automation scripts, maybe we
 # shouldn't we can clean up later
-apt-get install -y                  \
+yum install -y                      \
   screen                            \
   vim                               \
-  wget                              \
-  curl                              \
   nodejs                            \
   ;
 
+# Install mozilla specific packages
+
+# puppetagain packages
+base_url="http://puppetagain.pub.build.mozilla.org/data/repos/yum/releng/public/CentOS/6/x86_64/"
+
+# Install gcc to build gecko
+rpm -ih $base_url/gcc473_0moz1-4.7.3-0moz1.x86_64.rpm
+
 ### Clean up from setup
-# Remove cached .deb packages. Cached package takes up a lot of space and
+# Remove cached packages. Cached package takes up a lot of space and
 # distributing them to workers is wasteful.
-apt-get clean;
+yum clean all
 
 # Remove the setup.sh setup, we don't really need this script anymore, deleting
 # it keeps the image as clean as possible.
