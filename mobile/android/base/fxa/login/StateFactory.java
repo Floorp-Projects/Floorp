90f3637af39eabf0b57e7d71a7ac7e1c9700ea15
Nick Alexander <nalexander@mozilla.com>
2014-11-25 17:26:18 -0800

    Backed out changeset 705b1be87804 (bug 1098667) that was not intended to be pushed.

--- mobile/android/base/fxa/login/StateFactory.java
+++ mobile/android/base/fxa/login/StateFactory.java
@@ -54,11 +54,8 @@ public class StateFactory {
     }
 
     final int v = version.intValue();
-    if (v == 3) {
-      // The most common case is the most recent version.
-      return fromJSONObjectV3(stateLabel, o);
-    }
     if (v == 2) {
+      // The most common case is the most recent version.
       return fromJSONObjectV2(stateLabel, o);
     }
     if (v == 1) {
@@ -134,23 +131,6 @@ public class StateFactory {
           o.getString("certificate"));
     default:
       return fromJSONObjectV1(stateLabel, o);
-    }
-  }
-
-  /**
-   * Exactly the same as {@link fromJSONObjectV2}, except that there's a new
-   * MigratedFromSyncV11 state.
-   */
-  protected static State fromJSONObjectV3(StateLabel stateLabel, ExtendedJSONObject o) throws InvalidKeySpecException, NoSuchAlgorithmException, NonObjectJSONException {
-    switch (stateLabel) {
-    case MigratedFromSync11:
-      return new MigratedFromSync11(
-          o.getString("email"),
-          o.getString("uid"),
-          o.getBoolean("verified"),
-          o.getString("password"));
-    default:
-      return fromJSONObjectV2(stateLabel, o);
     }
   }
 
