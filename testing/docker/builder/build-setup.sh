d764c08fba99afd047aa1587f9e8edc29aa1b62f
Wander Lairson Costa <wcosta@mozilla.com>
2014-11-26 10:11:29 -0800

    Bug 1087456 part3: create repositories parent dirs if they don't exist. r=lightsofapollo,garndt
    Older versions of mercurial cannot create inner directories if the
    parents don't exist.
    
    --HG--
    extra : rebase_source : 0a996db89416da64ca7be228814f208975a42570
    extra : source : 42a93ffc374f4ef08a8b2f31d1f7ce1067fead15

--- testing/docker/builder/build-setup.sh
+++ testing/docker/builder/build-setup.sh
@@ -3,11 +3,17 @@
 gecko_dir=/home/worker/mozilla-central/source
 gaia_dir=/home/worker/gaia/source
 
+create_parent_dir() {
+  parent_dir=$(dirname $1)
+  if [ ! -d "$parent_dir" ]; then
+    mkdir -p "$parent_dir"
+  fi
+}
 
 ### Firefox Build Setup
 # Clone mozilla-central
-if [ ! -d "$gecko_dir" ];
-then
+if [ ! -d "$gecko_dir" ]; then
+  create_parent_dir $gecko_dir
   hg clone https://hg.mozilla.org/mozilla-central/ $gecko_dir
 fi
 
@@ -18,7 +24,7 @@ mkdir -p /home/worker/.mozbuild/
 mkdir -p /home/worker/object-folder/
 
 ### Clone gaia in too
-if [ ! -d "$gaia_dir" ];
-then
+if [ ! -d "$gaia_dir" ]; then
+  create_parent_dir $gaia_dir
   hg clone https://hg.mozilla.org/integration/gaia-central/ $gaia_dir
 fi
