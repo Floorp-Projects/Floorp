26b5cb7aad76be0cb0ed891f98839691845c40a5
Xidorn Quan <quanxunzhen@gmail.com>
2014-11-26 15:52:48 +1100

    Bug 1052924 - Rewrite pref isize and min isize computation. r=dbaron
    Known regression:
    This patch changes AppendTextContainer, and put aside text containers
    which contain spanning annotations. This changes makes those text
    containers not be reflowed by the current code. It will be fixed in
    some later patch.

--- layout/generic/nsRubyBaseContainerFrame.cpp
+++ layout/generic/nsRubyBaseContainerFrame.cpp
@@ -14,6 +14,8 @@
 
 using namespace mozilla;
 
+#define RTC_ARRAY_SIZE 1
+
 //----------------------------------------------------------------------
 
 // Frame class boilerplate
@@ -52,6 +54,121 @@ nsRubyBaseContainerFrame::GetFrameName(nsAString& aResult) const
 }
 #endif
 
+class MOZ_STACK_CLASS PairEnumerator
+{
+public:
+  PairEnumerator(nsRubyBaseContainerFrame* aRBCFrame,
+                 const nsTArray<nsRubyTextContainerFrame*>& aRTCFrames);
+
+  void Next();
+  bool AtEnd() const;
+
+  uint32_t GetLevelCount() const { return mFrames.Length(); }
+  nsIFrame* GetFrame(uint32_t aIndex) const { return mFrames[aIndex]; }
+  nsIFrame* GetBaseFrame() const { return GetFrame(0); }
+  nsIFrame* GetTextFrame(uint32_t aIndex) const { return GetFrame(aIndex + 1); }
+
+private:
+  nsAutoTArray<nsIFrame*, RTC_ARRAY_SIZE + 1> mFrames;
+};
+
+PairEnumerator::PairEnumerator(
+    nsRubyBaseContainerFrame* aBaseContainer,
+    const nsTArray<nsRubyTextContainerFrame*>& aTextContainers)
+{
+  const uint32_t rtcCount = aTextContainers.Length();
+  mFrames.SetCapacity(rtcCount + 1);
+  mFrames.AppendElement(aBaseContainer->GetFirstPrincipalChild());
+  for (uint32_t i = 0; i < rtcCount; i++) {
+    nsIFrame* rtFrame = aTextContainers[i]->GetFirstPrincipalChild();
+    mFrames.AppendElement(rtFrame);
+  }
+}
+
+void
+PairEnumerator::Next()
+{
+  for (uint32_t i = 0, iend = mFrames.Length(); i < iend; i++) {
+    if (mFrames[i]) {
+      mFrames[i] = mFrames[i]->GetNextSibling();
+    }
+  }
+}
+
+bool
+PairEnumerator::AtEnd() const
+{
+  for (uint32_t i = 0, iend = mFrames.Length(); i < iend; i++) {
+    if (mFrames[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+nscoord
+nsRubyBaseContainerFrame::CalculateMaxSpanISize(
+    nsRenderingContext* aRenderingContext)
+{
+  nscoord max = 0;
+  uint32_t spanCount = mSpanContainers.Length();
+  for (uint32_t i = 0; i < spanCount; i++) {
+    nsIFrame* frame = mSpanContainers[i]->GetFirstPrincipalChild();
+    nscoord isize = frame->GetPrefISize(aRenderingContext);
+    max = std::max(max, isize);
+  }
+  return max;
+}
+
+static nscoord
+CalculatePairPrefISize(nsRenderingContext* aRenderingContext,
+                       const PairEnumerator& aEnumerator)
+{
+  nscoord max = 0;
+  uint32_t levelCount = aEnumerator.GetLevelCount();
+  for (uint32_t i = 0; i < levelCount; i++) {
+    nsIFrame* frame = aEnumerator.GetFrame(i);
+    if (frame) {
+      max = std::max(max, frame->GetPrefISize(aRenderingContext));
+    }
+  }
+  return max;
+}
+
+/* virtual */ void
+nsRubyBaseContainerFrame::AddInlineMinISize(
+    nsRenderingContext *aRenderingContext, nsIFrame::InlineMinISizeData *aData)
+{
+  if (!mSpanContainers.IsEmpty()) {
+    // Since spans are not breakable internally, use our pref isize
+    // directly if there is any span.
+    aData->currentLine += GetPrefISize(aRenderingContext);
+    return;
+  }
+
+  nscoord max = 0;
+  PairEnumerator enumerator(this, mTextContainers);
+  for (; !enumerator.AtEnd(); enumerator.Next()) {
+    // We use *pref* isize for computing the min isize of pairs
+    // because ruby bases and texts are unbreakable internally.
+    max = std::max(max, CalculatePairPrefISize(aRenderingContext, enumerator));
+  }
+  aData->currentLine += max;
+}
+
+/* virtual */ void
+nsRubyBaseContainerFrame::AddInlinePrefISize(
+    nsRenderingContext *aRenderingContext, nsIFrame::InlinePrefISizeData *aData)
+{
+  nscoord sum = 0;
+  PairEnumerator enumerator(this, mTextContainers);
+  for (; !enumerator.AtEnd(); enumerator.Next()) {
+    sum += CalculatePairPrefISize(aRenderingContext, enumerator);
+  }
+  sum = std::max(sum, CalculateMaxSpanISize(aRenderingContext));
+  aData->currentLine += sum;
+}
+
 /* virtual */ bool 
 nsRubyBaseContainerFrame::IsFrameOfType(uint32_t aFlags) const 
 {
@@ -62,12 +179,20 @@ nsRubyBaseContainerFrame::IsFrameOfType(uint32_t aFlags) const
 void nsRubyBaseContainerFrame::AppendTextContainer(nsIFrame* aFrame)
 {
   nsRubyTextContainerFrame* rtcFrame = do_QueryFrame(aFrame);
-  if (rtcFrame) {
+  MOZ_ASSERT(rtcFrame, "Must provide a ruby text container.");
+
+  nsIFrame* onlyChild = rtcFrame->PrincipalChildList().OnlyChild();
+  if (onlyChild && onlyChild->IsPseudoFrame(rtcFrame->GetContent())) {
+    // Per CSS Ruby spec, if the only child of an rtc frame is
+    // a pseudo rt frame, it spans all bases in the segment.
+    mSpanContainers.AppendElement(rtcFrame);
+  } else {
     mTextContainers.AppendElement(rtcFrame);
   }
 }
 
 void nsRubyBaseContainerFrame::ClearTextContainers() {
+  mSpanContainers.Clear();
   mTextContainers.Clear();
 }
 
