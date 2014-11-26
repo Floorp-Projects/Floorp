1c2aa200a89ea9e3446eab3eef4a2d81a1516b66
Jonathan Kew <jkew@mozilla.com>
2014-11-26 10:24:16 +0000

    Bug 1096224 - part 2 - Fix writing mode mismatch in margin calculation in PlaceBlock. r=smontagu

--- layout/generic/nsBlockReflowContext.cpp
+++ layout/generic/nsBlockReflowContext.cpp
@@ -352,7 +352,8 @@ nsBlockReflowContext::PlaceBlock(const nsHTMLReflowState&  aReflowState,
   WritingMode parentWM = mMetrics.GetWritingMode();
   if (NS_FRAME_IS_COMPLETE(aReflowStatus)) {
     aBEndMarginResult = mMetrics.mCarriedOutBEndMargin;
-    aBEndMarginResult.Include(aReflowState.ComputedLogicalMargin().BEnd(wm));
+    aBEndMarginResult.Include(aReflowState.ComputedLogicalMargin().
+      ConvertTo(parentWM, wm).BEnd(parentWM));
   } else {
     // The used bottom-margin is set to zero above a break.
     aBEndMarginResult.Zero();
