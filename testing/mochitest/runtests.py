043f471456188ba831320260683e584dc2bc407f
Geoff Brown <gbrown@mozilla.com>
2014-11-25 17:31:44 -0700

    Bug 1026290 - Do not filter logFile parameter from testConfig.js; r=chmanchester

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -2099,7 +2099,9 @@ class Mochitest(MochitestUtilsMixin):
     if "MOZ_HIDE_RESULTS_TABLE" in os.environ and os.environ["MOZ_HIDE_RESULTS_TABLE"] == "1":
       options.hideResultsTable = True
 
-    d = dict((k, v) for k, v in options.__dict__.iteritems() if not k.startswith('log'))
+    d = dict((k, v) for k, v in options.__dict__.items() if
+        (not k.startswith('log_') or
+         not any([k.endswith(fmt) for fmt in commandline.log_formatters.keys()])))
     d['testRoot'] = self.testRoot
     content = json.dumps(d)
 
