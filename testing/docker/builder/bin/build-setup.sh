8e9160372d18481c443ecd451f341120c1b9abcc
Greg Arndt <garndt@mozilla.com>
2014-11-26 10:11:33 -0800

    Bug 1085516 - gecko builds: Add Mulet Linux x64 Opt r=lightsofapollo, wcosta

--- testing/docker/builder/bin/build-setup.sh
+++ testing/docker/builder/bin/build-setup.sh
@@ -22,9 +22,3 @@ mkdir -p /home/worker/.mozbuild/
 
 # Create object-folder exists
 mkdir -p /home/worker/object-folder/
-
-### Clone gaia in too
-if [ ! -d "$gaia_dir" ]; then
-  create_parent_dir $gaia_dir
-  hg clone https://hg.mozilla.org/integration/gaia-central/ $gaia_dir
-fi
