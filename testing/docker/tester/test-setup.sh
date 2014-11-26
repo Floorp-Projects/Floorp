2e0ed4de29ba1b35fe0abf5978100a9d1c88846c
Greg Arndt <garndt@mozilla.com>
2014-11-26 10:11:40 -0800

    bug 1103031 - create artifacts directory in tester r=lightsofapollo
    --HG--
    extra : rebase_source : 36ba94c25ceef012f501819422554e5344288931
    extra : source : 1238e0af84c1e9001145814818bffa1426b2a888

--- testing/docker/tester/test-setup.sh
+++ testing/docker/tester/test-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ve
 
 pip install virtualenv;
-mkdir Documents; mkdir Pictures; mkdir Music; mkdir Videos;
+mkdir Documents; mkdir Pictures; mkdir Music; mkdir Videos; mkdir artifacts
 hg clone http://hg.mozilla.org/build/mozharness/
 echo 'Xvfb :0 -nolisten tcp -screen 0 1600x1200x24 &> /dev/null &' >> .bashrc
 chown -R worker:worker /home/worker/* /home/worker/.*
