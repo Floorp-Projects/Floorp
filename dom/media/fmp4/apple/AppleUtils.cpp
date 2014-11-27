2139e6a16a0b8047fe9b8cd2712278f677c81092
Ralph Giles <giles@mozilla.com>
2014-11-27 13:21:00 -0800

    Bug 1065111 - Remove obsolete logging macros. r=jya, a=sledru

--- dom/media/fmp4/apple/AppleUtils.cpp
+++ dom/media/fmp4/apple/AppleUtils.cpp
@@ -15,13 +15,4 @@ PRLogModuleInfo* GetAppleMediaLog() {
   }
   return log;
 }
-#define WARN(...) PR_LOG(GetAppleMediaLog(), PR_LOG_WARNING, (__VA_ARGS__))
-#else
-#define WARN(...)
 #endif
-
-#define PROPERTY_ID_FORMAT "%c%c%c%c"
-#define PROPERTY_ID_PRINT(x) ((x) >> 24), \
-                             ((x) >> 16) & 0xff, \
-                             ((x) >> 8) & 0xff, \
-                              (x) & 0xff
