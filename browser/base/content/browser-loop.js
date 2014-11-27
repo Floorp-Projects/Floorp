d3345e5e9054ed012bced107bc7085d9dd44cfa5
Mike de Boer <mdeboer@mozilla.com>
2014-11-27 12:41:41 +0100

    Bug 1074667: Generate system alert when someone joins a room. r=MattN a=lsblakk

--- browser/base/content/browser-loop.js
+++ browser/base/content/browser-loop.js
@@ -20,22 +20,52 @@ XPCOMUtils.defineLazyModuleGetter(this, "PanelFrame", "resource:///modules/Panel
     /**
      * Opens the panel for Loop and sizes it appropriately.
      *
-     * @param {event} event The event opening the panel, used to anchor
-     *                      the panel to the button which triggers it.
+     * @param {event}  event   The event opening the panel, used to anchor
+     *                         the panel to the button which triggers it.
+     * @param {String} [tabId] Identifier of the tab to select when the panel is
+     *                         opened. Example: 'rooms', 'contacts', etc.
      */
-    openCallPanel: function(event) {
+    openCallPanel: function(event, tabId = null) {
       let callback = iframe => {
+        // Helper function to show a specific tab view in the panel.
+        function showTab() {
+          if (!tabId) {
+            return;
+          }
+
+          let win = iframe.contentWindow;
+          let ev = new win.CustomEvent("UIAction", Cu.cloneInto({
+            detail: {
+              action: "selectTab",
+              tab: tabId
+            }
+          }, win));
+          win.dispatchEvent(ev);
+        }
+
+        // If the panel has been opened and initialized before, we can skip waiting
+        // for the content to load - because it's already there.
+        if (("contentWindow" in iframe) && iframe.contentWindow.document.readyState == "complete") {
+          showTab();
+          return;
+        }
+
         iframe.addEventListener("DOMContentLoaded", function documentDOMLoaded() {
           iframe.removeEventListener("DOMContentLoaded", documentDOMLoaded, true);
           injectLoopAPI(iframe.contentWindow);
+          iframe.contentWindow.addEventListener("loopPanelInitialized", function loopPanelInitialized() {
+            iframe.contentWindow.removeEventListener("loopPanelInitialized",
+              loopPanelInitialized);
+            showTab();
+          });
         }, true);
       };
 
       // Used to clear the temporary "login" state from the button.
       Services.obs.notifyObservers(null, "loop-status-changed", null);
 
-      PanelFrame.showPopup(window, event.target, "loop", null,
-                           "about:looppanel", null, callback);
+      PanelFrame.showPopup(window, event ? event.target : this.toolbarButton.node,
+                           "loop", null, "about:looppanel", null, callback);
     },
 
     /**
@@ -87,6 +117,67 @@ XPCOMUtils.defineLazyModuleGetter(this, "PanelFrame", "resource:///modules/Panel
         state = "active";
       }
       this.toolbarButton.node.setAttribute("state", state);
+    },
+
+    /**
+     * Show a desktop notification when 'do not disturb' isn't enabled.
+     *
+     * @param {Object} options Set of options that may tweak the appearance and
+     *                         behavior of the notification.
+     *                         Option params:
+     *                         - {String}   title       Notification title message
+     *                         - {String}   [message]   Notification body text
+     *                         - {String}   [icon]      Notification icon
+     *                         - {String}   [sound]     Sound to play
+     *                         - {String}   [selectTab] Tab to select when the panel
+     *                                                  opens
+     *                         - {Function} [onclick]   Callback to invoke when
+     *                                                  the notification is clicked.
+     *                                                  Opens the panel by default.
+     */
+    showNotification: function(options) {
+      if (MozLoopService.doNotDisturb) {
+        return;
+      }
+
+      if (!options.title) {
+        throw new Error("Missing title, can not display notification");
+      }
+
+      let notificationOptions = {
+        body: options.message || ""
+      };
+      if (options.icon) {
+        notificationOptions.icon = options.icon;
+      }
+      if (options.sound) {
+        // This will not do anything, until bug bug 1105222 is resolved.
+        notificationOptions.mozbehavior = {
+          soundFile: `chrome://browser/content/loop/shared/sounds/${options.sound}.ogg`
+        };
+      }
+
+      let notification = new window.Notification(options.title, notificationOptions);
+      notification.addEventListener("click", e => {
+        if (window.closed) {
+          return;
+        }
+
+        try {
+          window.focus();
+        } catch (ex) {}
+
+        // We need a setTimeout here, otherwise the panel won't show after the
+        // window received focus.
+        window.setTimeout(() => {
+          if (typeof options.onclick == "function") {
+            options.onclick();
+          } else {
+            // Open the Loop panel as a default action.
+            this.openCallPanel(null, options.selectTab || null);
+          }
+        }, 0);
+      });
     },
 
     /**
