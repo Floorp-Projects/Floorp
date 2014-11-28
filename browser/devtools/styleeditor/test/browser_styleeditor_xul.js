cc1583e281205428ed2bda6de10efde5571f4079
Patrick Brosset <pbrosset@mozilla.com>
2014-11-28 17:57:19 +0100

    Bug 1106079 - Avoid highlighter excpetions on XUL windows to break the style-editor. r=past, a=sledru

---
+++ browser/devtools/styleeditor/test/browser_styleeditor_xul.js
@@ -0,0 +1,20 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Test that the style-editor initializes correctly for XUL windows.
+
+waitForExplicitFinish();
+
+const TEST_URL = "about:config";
+
+let test = asyncTest(function*() {
+  let tab = yield addTab(TEST_URL);
+  let target = TargetFactory.forTab(tab);
+
+  let toolbox = yield gDevTools.showToolbox(target, "styleeditor");
+  let panel = toolbox.getCurrentPanel();
+  yield panel.UI.once("editor-added");
+
+  ok(panel, "The style-editor panel did initialize correctly for the XUL window");
+});
