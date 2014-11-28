4828cf49302d91bc2e36a066073b08bd3897aee9
Benoit Girard <b56girard@gmail.com>
2014-11-28 18:42:20 -0500

    Bug 1097941 - Properly disable paint-will-resample on b2g. r=jmuizel

--- gfx/layers/client/ClientTiledPaintedLayer.cpp
+++ gfx/layers/client/ClientTiledPaintedLayer.cpp
@@ -341,7 +341,7 @@ ClientTiledPaintedLayer::RenderLayer()
   TILING_LOG("TILING %p: Initial low-precision valid region %s\n", this, Stringify(mLowPrecisionValidRegion).c_str());
 
   nsIntRegion neededRegion = mVisibleRegion;
-#ifndef MOZ_GFX_OPTIMIZE_MOBILE
+#ifndef MOZ_IGNORE_PAINT_WILL_RESAMPLE
   // This is handled by PadDrawTargetOutFromRegion in TiledContentClient for mobile
   if (MayResample()) {
     // If we're resampling then bilinear filtering can read up to 1 pixel
