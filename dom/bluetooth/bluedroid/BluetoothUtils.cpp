6185257ca05a3152b9da835ae92e2648bbe1a585
Thomas Zimmermann <tdz@users.sourceforge.net>
2014-11-28 11:49:40 +0100

    Bug 1105308: Cleanup non-shared functions in Bluedroid's BluetoothUtils.{cpp,h}, r=btian
    This patch moves functions out of Bluedroid's BluetoothUtils.{cpp,h}
    that are specific to this backend.

--- dom/bluetooth/bluedroid/BluetoothUtils.cpp
+++ dom/bluetooth/bluedroid/BluetoothUtils.cpp
@@ -22,15 +22,6 @@
 
 BEGIN_BLUETOOTH_NAMESPACE
 
-uint16_t
-UuidToServiceClassInt(const BluetoothUuid& mUuid)
-{
-  // extract short UUID 0000xxxx-0000-1000-8000-00805f9b34fb
-  uint16_t shortUuid;
-  memcpy(&shortUuid, mUuid.mUuid + 2, sizeof(uint16_t));
-  return ntohs(shortUuid);
-}
-
 bool
 SetJsObject(JSContext* aContext,
             const BluetoothValue& aValue,
