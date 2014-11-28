5f43ef445fff4bf1938bd296ce222da7f51cb8be
Mike Conley <mconley@mozilla.com>
2014-11-28 10:59:29 -0500

    Bug 1106104 - Re-enable e10s by default on Nightly 37. r=felipe.
    --HG--
    extra : rebase_source : 6932153f7c212079ed61496da44f99e07f9ef2bf
    extra : amend_source : dfd66cef66a078ef13be69f6cf1d120e25224aa3

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -2469,7 +2469,7 @@ let DefaultBrowserCheck = {
 let E10SUINotification = {
   // Increase this number each time we want to roll out an
   // e10s testing period to Nightly users.
-  CURRENT_NOTICE_COUNT: 2,
+  CURRENT_NOTICE_COUNT: 3,
   CURRENT_PROMPT_PREF: "browser.displayedE10SPrompt.1",
   PREVIOUS_PROMPT_PREF: "browser.displayedE10SPrompt",
 
