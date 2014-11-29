881e442700defba9815b8c6b5e6d1e44727d73d3
Tooru Fujisawa <arai_a@mac.com>
2014-11-29 20:57:27 +0900

    Bug 1092538 - Ignore `null` iterable in Map, Set, WeakMap and WeakSet constructors. r=evilpie

--- js/src/jit-test/tests/collections/WeakMap-constructor-1.js
+++ js/src/jit-test/tests/collections/WeakMap-constructor-1.js
@@ -4,12 +4,9 @@ load(libdir + "asserts.js");
 
 new WeakMap();
 new WeakMap(undefined);
-
-// FIXME: bug 1092538
-// new WeakMap(null);
+new WeakMap(null);
 
 // FIXME: bug 1062075
 // assertThrowsInstanceOf(() => WeakMap(), TypeError);
 // assertThrowsInstanceOf(() => WeakMap(undefined), TypeError);
-// WeakMap(null) throws TypeError, but it's because of bug 1092538.
 // assertThrowsInstanceOf(() => WeakMap(null), TypeError);
