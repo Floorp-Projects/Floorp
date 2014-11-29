a15d3f6b209d376386023516c969f76ea13c272f
James Lal <james@lightsofapollo.com>
2014-11-29 15:05:44 -0800

    Bug 1105439 - Part 1 New gaia_props.py util for getting gaia version needed by the gecko r=garndt, wcosta
    Normalizes gaia tooling to a central file with a set of commands / help.

--- testing/docker/builder/bin/get_gaia_repo.js
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env node
-var fs = require('fs');
-var url = require('url');
-var data = fs.readFileSync('/home/worker/mozilla-central/source/b2g/config/gaia.json');
-
-var repo_path = JSON.parse(data).repo_path;
-console.log(url.resolve('https://hg.mozilla.org', repo_path));
--- testing/docker/builder/bin/get_gaia_revision.js
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env node
-var fs = require('fs');
-var data = fs.readFileSync('/home/worker/mozilla-central/source/b2g/config/gaia.json');
-console.log(JSON.parse(data).revision);
