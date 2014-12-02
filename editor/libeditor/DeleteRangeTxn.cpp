d2593803307b609ed15529ad89e8cbc15107e9d7
Daniel Holbert <dholbert@cs.stanford.edu>
2014-12-01 21:07:42 -0800

    Bug 1093191 part 3: Make .cpp files in libeditor #include their own .h file first. r=ehsan

--- editor/libeditor/DeleteRangeTxn.cpp
+++ editor/libeditor/DeleteRangeTxn.cpp
@@ -3,8 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "DeleteNodeTxn.h"
 #include "DeleteRangeTxn.h"
+
+#include "DeleteNodeTxn.h"
 #include "DeleteTextTxn.h"
 #include "mozilla/Assertions.h"
 #include "mozilla/dom/Selection.h"
--- editor/libeditor/nsHTMLEditor.cpp
+++ editor/libeditor/nsHTMLEditor.cpp
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "nsHTMLEditor.h"
+
 #include "mozilla/DebugOnly.h"
 #include "mozilla/EventStates.h"
 #include "mozilla/TextEvents.h"
@@ -11,7 +13,6 @@
 
 #include "nsUnicharUtils.h"
 
-#include "nsHTMLEditor.h"
 #include "nsHTMLEditRules.h"
 #include "nsTextEditUtils.h"
 #include "nsHTMLEditUtils.h"
