bced84e712ee1e82272bad8a2a07cd91cc691c2c
Bobby Holley <bobbyholley@gmail.com>
2014-12-01 21:51:02 -0800

    Bug 1104964 - Rename the task queue for hardware video decoding to something more obvious. r=cpearce, a=sledru
    Otherwise it will shadow the mTaskQueue that we're about to introduce on
    MediaDecoderReader.

--- dom/media/webm/WebMReader.h
+++ dom/media/webm/WebMReader.h
@@ -187,7 +187,7 @@ public:
   uint64_t GetLastVideoFrameTime();
   void SetLastVideoFrameTime(uint64_t aFrameTime);
   layers::LayersBackend GetLayersBackendType() { return mLayersBackendType; }
-  MediaTaskQueue* GetTaskQueue() { return mTaskQueue; }
+  MediaTaskQueue* GetVideoTaskQueue() { return mVideoTaskQueue; }
 
 protected:
 #ifdef MOZ_OPUS
@@ -282,7 +282,9 @@ private:
   int mVideoCodec;
 
   layers::LayersBackend mLayersBackendType;
-  nsRefPtr<MediaTaskQueue> mTaskQueue;
+
+  // For hardware video decoding.
+  nsRefPtr<MediaTaskQueue> mVideoTaskQueue;
 
   // Booleans to indicate if we have audio and/or video data
   bool mHasVideo;
