f06439c25300758b43ce535cd4905e782605dbf7
Nick Fitzgerald <fitzgen@gmail.com>
2014-12-08 13:39:55 -0800

    Bug 1103813 - Debugger.prototype.findObjects should only find objects in the debuggee compartments. r=shu

---
+++ js/src/jit-test/tests/debug/bug-1103813.js
@@ -0,0 +1,6 @@
+// Random chosen test: js/src/jit-test/tests/debug/Source-invisible.js
+newGlobal({
+    invisibleToDebugger: true
+})
+// Random chosen test: js/src/jit-test/tests/debug/Debugger-findObjects-05.js
+x = (new Debugger).findObjects()
