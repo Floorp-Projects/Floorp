2f3509ec3df00a60732b527658a65a981a070bb9
Chris Manchester <cmanchester@mozilla.com>
2014-12-08 17:21:59 -0500

    Bug 1108714 - Make marionette's skip_if_b2g run desktop tests. r=mdas

--- testing/marionette/client/marionette/marionette_test.py
+++ testing/marionette/client/marionette/marionette_test.py
@@ -82,11 +82,10 @@ def expectedFailure(func):
 
 def skip_if_b2g(target):
     def wrapper(self, *args, **kwargs):
-        if self.marionette.session_capabilities.has_key('b2g') and \
-            not self.marionette.session_capabilities['b2g'] == True:
-            return target(self, *args, **kwargs)
-        else:
+        if self.marionette.session_capabilities.get('b2g') == True:
             raise SkipTest('skipping due to b2g')
+        return target(self, *args, **kwargs)
+
     return wrapper
 
 def parameterized(func_suffix, *args, **kwargs):
