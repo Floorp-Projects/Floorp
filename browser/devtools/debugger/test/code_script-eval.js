95ca321b54670961ca6d9bccdedfc25acbf4f615
Phil Ringnalda <philringnalda@gmail.com>
2014-12-08 20:24:52 -0800

    Back out eb203c9db11d (bug 1107541) for e10s dt bustage CLOSED TREE

--- browser/devtools/debugger/test/code_script-eval.js
+++ browser/devtools/debugger/test/code_script-eval.js
@@ -4,7 +4,3 @@ var bar;
 function evalSource() {
   eval('bar = function() {\nvar x = 5;\n}');
 }
-
-function evalSourceWithSourceURL() {
-   eval('bar = function() {\nvar x = 6;\n} //# sourceURL=bar.js');
-}
