f3be105a672d6abc9ab43576894fd6b9a5db0785
David Burns <dburns@mozilla.com>
2014-12-09 20:33:03 +0000

    Bug 1107336: Move marionette code for driving the browser out of the runner; r=jgriffin
    --HG--
    rename : testing/marionette/client/marionette/application_cache.py => testing/marionette/driver/marionette_driver/application_cache.py
    rename : testing/marionette/client/marionette/by.py => testing/marionette/driver/marionette_driver/by.py
    rename : testing/marionette/client/marionette/date_time_value.py => testing/marionette/driver/marionette_driver/date_time_value.py
    rename : testing/marionette/client/marionette/decorators.py => testing/marionette/driver/marionette_driver/decorators.py
    rename : testing/marionette/client/marionette/errors.py => testing/marionette/driver/marionette_driver/errors.py
    rename : testing/marionette/client/marionette/expected.py => testing/marionette/driver/marionette_driver/expected.py
    rename : testing/marionette/client/marionette/geckoinstance.py => testing/marionette/driver/marionette_driver/geckoinstance.py
    rename : testing/marionette/client/marionette/gestures.py => testing/marionette/driver/marionette_driver/gestures.py
    rename : testing/marionette/client/marionette/keys.py => testing/marionette/driver/marionette_driver/keys.py
    rename : testing/marionette/client/marionette/marionette.py => testing/marionette/driver/marionette_driver/marionette.py
    rename : testing/marionette/client/marionette/selection.py => testing/marionette/driver/marionette_driver/selection.py
    rename : testing/marionette/client/marionette/wait.py => testing/marionette/driver/marionette_driver/wait.py
    extra : rebase_source : 224247b1c831015972d5c1043f194fc598c83838
    extra : source : c886cd910936b6f1f3e79bf973aab1c445770ea3

---
+++ testing/marionette/driver/marionette_driver/__init__.py
@@ -0,0 +1,3 @@
+from marionette_driver import ( errors, by, decorators, expected, geckoinstance,
+                                gestures, keys, marionette, selection, wait,
+                                application_cache, date_time_value )
