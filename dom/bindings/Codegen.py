56b96aa115f2a62995c140d2853138e89430b567
Boris Zbarsky <bzbarsky@mit.edu>
2014-12-09 14:44:37 -0500

    Bug 1012798 part 2.  Stop defining a value property named "window" on the global.  r=peterv

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -3427,6 +3427,12 @@ class CGUpdateMemberSlotsMethod(CGAbstractStaticMethod):
                 "JSJitGetterCallArgs args(&temp);\n")
         for m in self.descriptor.interface.members:
             if m.isAttr() and m.getExtendedAttribute("StoreInSlot"):
+                # Skip doing this for the "window" attribute on the Window
+                # interface, because that can't be gotten safely until we have
+                # hooked it up correctly to the outer window.
+                if (self.descriptor.interface.identifier.name == "Window" and
+                    m.identifier.name == "window"):
+                    continue
                 body += fill(
                     """
 
