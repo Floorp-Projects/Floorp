50fd4a2ed6079219bc4f871a460e145e697c5971
James Long <longster@gmail.com>
2014-12-09 15:00:00 -0500

    Bug 1107541 - Display eval sources with a sourceURL pragma correctly in the debugger. r=fitzgen, a=sledru

---
+++ browser/devtools/debugger/test/browser_dbg_sources-eval-02.js
@@ -0,0 +1,47 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Make sure eval scripts with the sourceURL pragma are correctly
+ * displayed
+ */
+
+const TAB_URL = EXAMPLE_URL + "doc_script-eval.html";
+
+function test() {
+  let gTab, gPanel, gDebugger;
+  let gSources, gBreakpoints, gEditor;
+
+  initDebugger(TAB_URL).then(([aTab,, aPanel]) => {
+    gTab = aTab;
+    gPanel = aPanel;
+    gDebugger = gPanel.panelWin;
+    gSources = gDebugger.DebuggerView.Sources;
+    gBreakpoints = gDebugger.DebuggerController.Breakpoints;
+    gEditor = gDebugger.DebuggerView.editor;
+
+    return Task.spawn(function*() {
+      yield waitForSourceShown(gPanel, "-eval.js");
+      is(gSources.values.length, 1, "Should have 1 source");
+
+      let newSource = waitForDebuggerEvents(gPanel, gDebugger.EVENTS.NEW_SOURCE);
+      callInTab(gTab, "evalSourceWithSourceURL");
+      yield newSource;
+
+      is(gSources.values.length, 2, "Should have 2 sources");
+
+      let item = gSources.getItemForAttachment(e => e.label == "bar.js");
+      ok(item, "Source label is incorrect.");
+      ok(item.attachment.group === 'http://example.com', 'Source group is incorrect');
+
+      let shown = waitForDebuggerEvents(gPanel, gDebugger.EVENTS.SOURCE_SHOWN);
+      gSources.selectedItem = item;
+      yield shown;
+
+      ok(gEditor.getText().indexOf('bar = function() {') === 0,
+         'Correct source is shown');
+
+      yield closeDebuggerAndFinish(gPanel);
+    });
+  });
+}
--- browser/devtools/debugger/test/code_script-eval.js
+++ browser/devtools/debugger/test/code_script-eval.js
@@ -4,3 +4,7 @@ var bar;
 function evalSource() {
   eval('bar = function() {\nvar x = 5;\n}');
 }
+
+function evalSourceWithSourceURL() {
+  eval('bar = function() {\nvar x = 6;\n} //# sourceURL=bar.js');
+}
