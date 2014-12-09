a06a042ff5bdb6c8cc265184d4642818a30ba9db
Wes Kocher <wkocher@mozilla.com>
2014-12-08 16:35:40 -0800

    Backed out changeset c8adfe4f5995 (bug 1107525) for arm spidermonkey bustage

--- js/src/jit-test/tests/debug/bug1107525.js
+++
@@ -1,9 +0,0 @@
-// |jit-test| error: InternalError
-enableSPSProfiling();
-var g = newGlobal();
-g.parent = this;
-g.eval("new Debugger(parent).onExceptionUnwind = function () { hits++; };");
-function f() {
-    var x = f();
-}
-f();
