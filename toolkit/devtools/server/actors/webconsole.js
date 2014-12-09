46c55add6815729151ce3e011db94848b4cfa919
J. Ryan Stinnett <jryans@gmail.com>
2014-12-09 14:35:13 -0600

    Bug 731318 - Show transferred size in Net Monitor. r=vporof

--- toolkit/devtools/server/actors/webconsole.js
+++ toolkit/devtools/server/actors/webconsole.js
@@ -83,6 +83,7 @@ function WebConsoleActor(aConnection, aParentActor)
 
   this.traits = {
     customNetworkRequest: !this._parentIsContentActor,
+    transferredResponseSize: true
   };
 }
 
@@ -1940,6 +1941,7 @@ NetworkEventActor.prototype =
       updateType: "responseContent",
       mimeType: aContent.mimeType,
       contentSize: aContent.text.length,
+      transferredSize: aContent.transferredSize,
       discardResponseBody: aDiscardedResponseBody,
     };
 
