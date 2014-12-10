83500ff8445532f7795c7080dc46522023d0a042
Kartikaya Gupta <kgupta@mozilla.com>
2014-12-10 05:07:00 -0500

    Bug 1089822 - Fix running reftest-remote on B2G using a local emulator build. r=ahal

--- layout/tools/reftest/runreftestb2g.py
+++ layout/tools/reftest/runreftestb2g.py
@@ -352,8 +352,6 @@ class B2GRemoteReftest(RefTest):
         if (os.name == "nt"):
             xpcshell += ".exe"
 
-        if (options.utilityPath):
-            paths.insert(0, options.utilityPath)
         options.utilityPath = self.findPath(paths, xpcshell)
         if options.utilityPath == None:
             print "ERROR: unable to find utility path for %s, please specify with --utility-path" % (os.name)
