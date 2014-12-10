970108676699914c883f2b763efe3150dcbfe185
Michael Comella <michael.l.comella@gmail.com>
2014-12-10 09:35:54 -0800

    Bug 1085771 - Part 4: Move color state selector to colors/. r=lucasr, a=sledru

--- mobile/android/base/tabs/TabStripItemView.java
+++ mobile/android/base/tabs/TabStripItemView.java
@@ -71,7 +71,7 @@ public class TabStripItemView extends ThemedLinearLayout
         final Resources res = context.getResources();
 
         final ColorStateList tabColors =
-                res.getColorStateList(R.drawable.new_tablet_tab_strip_item_bg);
+                res.getColorStateList(R.color.new_tablet_tab_strip_item_bg);
         backgroundDrawable = new ResizablePathDrawable(new TabCurveShape(), tabColors);
         setBackgroundDrawable(backgroundDrawable);
 
