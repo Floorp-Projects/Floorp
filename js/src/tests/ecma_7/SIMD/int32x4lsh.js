b535b7648ceea7f05dacf5bff01003bd2aa273e3
Benjamin Bouvier <benj@benj.me>
2014-12-10 13:25:49 +0100

    Bug 1108807: Rename SIMD shifts; r=till

--- js/src/tests/ecma_7/SIMD/int32x4lsh.js
+++ js/src/tests/ecma_7/SIMD/int32x4lsh.js
@@ -10,7 +10,7 @@ function test() {
 
   for (var bits = 0; bits < 32; bits++) {
       var a = int32x4(-1, 2, -3, 4);
-      var c = SIMD.int32x4.shiftLeft(a, bits);
+      var c = SIMD.int32x4.shiftLeftByScalar(a, bits);
       assertEq(c.x, -1 << bits);
       assertEq(c.y, 2 << bits);
       assertEq(c.z, -3 << bits);
@@ -21,7 +21,7 @@ function test() {
   var INT32_MIN = -Math.pow(2, 31);
 
   var d = int32x4(INT32_MAX, INT32_MIN, INT32_MAX, INT32_MIN);
-  var f = SIMD.int32x4.shiftLeft(d, 1);
+  var f = SIMD.int32x4.shiftLeftByScalar(d, 1);
   assertEq(f.x, (INT32_MAX << 1) | 0);
   assertEq(f.y, (INT32_MIN << 1) | 0);
   assertEq(f.z, (INT32_MAX << 1) | 0);
