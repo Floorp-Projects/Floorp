bebd00bf66651b2490fc4ed690dff47f5e8ca8f7
Bill McCloskey <wmccloskey@mozilla.com>
2014-12-11 10:09:44 -0800

    Backout bug 1099410 for increasing crash rate

--- dom/plugins/ipc/PluginModuleParent.h
+++ dom/plugins/ipc/PluginModuleParent.h
@@ -274,6 +274,8 @@ class PluginModuleContentParent : public PluginModuleParent
 #ifdef MOZ_CRASHREPORTER_INJECTOR
     void OnCrash(DWORD processID) MOZ_OVERRIDE {}
 #endif
+
+    static PluginModuleContentParent* sSavedModuleParent;
 };
 
 class PluginModuleChromeParent
