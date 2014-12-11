8c17675dcab3ad035cb30f4249312e6d62946487
Aaron Klotz <aklotz@mozilla.com>
2014-12-11 14:00:47 -0700

    Bug 1070755: Telemetry to track total time that the main thread is blocked during plugin initialization; r=vladan

--- toolkit/components/telemetry/Telemetry.h
+++ toolkit/components/telemetry/Telemetry.h
@@ -89,6 +89,7 @@ template<TimerResolution res>
 struct AccumulateDelta_impl
 {
   static void compute(ID id, TimeStamp start, TimeStamp end = TimeStamp::Now());
+  static void compute(ID id, const nsCString& key, TimeStamp start, TimeStamp end = TimeStamp::Now());
 };
 
 template<>
@@ -97,6 +98,9 @@ struct AccumulateDelta_impl<Millisecond>
   static void compute(ID id, TimeStamp start, TimeStamp end = TimeStamp::Now()) {
     Accumulate(id, static_cast<uint32_t>((end - start).ToMilliseconds()));
   }
+  static void compute(ID id, const nsCString& key, TimeStamp start, TimeStamp end = TimeStamp::Now()) {
+    Accumulate(id, key, static_cast<uint32_t>((end - start).ToMilliseconds()));
+  }
 };
 
 template<>
@@ -104,6 +108,9 @@ struct AccumulateDelta_impl<Microsecond>
 {
   static void compute(ID id, TimeStamp start, TimeStamp end = TimeStamp::Now()) {
     Accumulate(id, static_cast<uint32_t>((end - start).ToMicroseconds()));
+  }
+  static void compute(ID id, const nsCString& key, TimeStamp start, TimeStamp end = TimeStamp::Now()) {
+    Accumulate(id, key, static_cast<uint32_t>((end - start).ToMicroseconds()));
   }
 };
 
@@ -117,12 +124,24 @@ public:
     MOZ_GUARD_OBJECT_NOTIFIER_INIT;
   }
 
+  explicit AutoTimer(const nsCString& aKey, TimeStamp aStart = TimeStamp::Now() MOZ_GUARD_OBJECT_NOTIFIER_PARAM)
+    : start(aStart)
+    , key(aKey)
+  {
+    MOZ_GUARD_OBJECT_NOTIFIER_INIT;
+  }
+
   ~AutoTimer() {
-    AccumulateDelta_impl<res>::compute(id, start);
+    if (key.IsEmpty()) {
+      AccumulateDelta_impl<res>::compute(id, start);
+    } else {
+      AccumulateDelta_impl<res>::compute(id, key, start);
+    }
   }
 
 private:
   const TimeStamp start;
+  const nsCString key;
   MOZ_DECL_USE_GUARD_OBJECT_NOTIFIER
 };
 
