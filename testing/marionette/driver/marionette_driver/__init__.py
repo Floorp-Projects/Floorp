748a5b4c12f85b1856c6a68f2a23599125d0e3e9
David Burns <dburns@mozilla.com>
2014-12-11 15:56:44 +0000

    Bug 1107336: update tests not covered by manifest for when they are ready; r=jgriffin
    --HG--
    extra : rebase_source : f1338bff221c7e507f0fe2404539dcbfb0c1c476
    extra : source : 805dce7ae79f35f266853342369294be8aea5398

--- testing/marionette/driver/marionette_driver/__init__.py
+++ testing/marionette/driver/marionette_driver/__init__.py
@@ -1,3 +1,4 @@
 from marionette_driver import ( errors, by, decorators, expected, geckoinstance,
                                 gestures, keys, marionette, selection, wait,
                                 application_cache, date_time_value )
+from marionette_driver.gestures import smooth_scroll, pinch
