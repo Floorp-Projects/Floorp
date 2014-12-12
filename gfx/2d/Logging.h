bcb0da2105e0a2287a852feca4c651f1cc96bc45
Milan Sreckovic <milan@mozilla.com>
2014-12-12 15:35:48 -0500

    Bug 946069 - Assert/critical error if the CG contexts are invalid. Also, deal with the empty path when asking for the current point. r=mstange

--- gfx/2d/Logging.h
+++ gfx/2d/Logging.h
@@ -462,6 +462,18 @@ typedef Log<LOG_CRITICAL, CriticalLogger> CriticalLog;
 // See nsDebug.h and the NS_WARN_IF macro
 
 #ifdef __cplusplus
+ // For now, have MOZ2D_ERROR_IF available in debug and non-debug builds
+inline bool MOZ2D_error_if_impl(bool aCondition, const char* aExpr,
+                                const char* aFile, int32_t aLine)
+{
+  if (MOZ_UNLIKELY(aCondition)) {
+    gfxCriticalError() << aExpr << " at " << aFile << ":" << aLine;
+  }
+  return aCondition;
+}
+#define MOZ2D_ERROR_IF(condition) \
+  MOZ2D_error_if_impl(condition, #condition, __FILE__, __LINE__)
+
 #ifdef DEBUG
 inline bool MOZ2D_warn_if_impl(bool aCondition, const char* aExpr,
                                const char* aFile, int32_t aLine)
