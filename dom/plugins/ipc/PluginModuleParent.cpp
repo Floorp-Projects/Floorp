61710ec2c28bb8eeb0b2c7cb79037086fee7957a
Benjamin Smedberg <benjamin@smedbergs.us>
2014-12-12 14:13:28 -0500

    Bug 1110818 - Measure crash and breakpad rates for subprocesses (plugin/gmplugin/content), r=gfritzsche

--- dom/plugins/ipc/PluginModuleParent.cpp
+++ dom/plugins/ipc/PluginModuleParent.cpp
@@ -916,6 +916,8 @@ PluginModuleChromeParent::ActorDestroy(ActorDestroyReason why)
 #ifdef MOZ_CRASHREPORTER
         ProcessFirstMinidump();
 #endif
+        Telemetry::Accumulate(Telemetry::SUBPROCESS_ABNORMAL_ABORT,
+                              NS_LITERAL_CSTRING("plugin"), 1);
     }
 
     // We can't broadcast settings changes anymore.
