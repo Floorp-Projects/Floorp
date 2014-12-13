9c84bb8f408508b88cbdb7789bbacf79b1f1453f
Tooru Fujisawa <arai_a@mac.com>
2014-12-13 10:41:41 +0900

    Bug 1105608 - Throw SyntaxError for export-from. r=jorendorff

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -7120,6 +7120,7 @@ frontend::EmitTree(ExclusiveContext *cx, BytecodeEmitter *bce, ParseNode *pn)
 
       case PNK_IMPORT:
       case PNK_EXPORT:
+      case PNK_EXPORT_FROM:
        // TODO: Implement emitter support for modules
        bce->reportError(nullptr, JSMSG_MODULES_NOT_IMPLEMENTED);
        return false;
