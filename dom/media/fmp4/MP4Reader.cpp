d8d571414ff556da496be70715f4db255130794b
Nigel Babu <nigelbabu@gmail.com>
2014-12-15 12:40:46 +0530

    Backed out changeset 03e5a89726be (bug 1110608) for non-unified build failure on CLOSED TREE

--- dom/media/fmp4/MP4Reader.cpp
+++ dom/media/fmp4/MP4Reader.cpp
@@ -5,7 +5,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "MP4Reader.h"
-#include "MP4Stream.h"
 #include "MediaResource.h"
 #include "nsSize.h"
 #include "VideoUtils.h"
@@ -62,6 +61,63 @@ TrackTypeToStr(TrackType aTrack)
   }
 }
 #endif
+
+class MP4Stream : public Stream {
+public:
+
+  explicit MP4Stream(MediaResource* aResource)
+    : mResource(aResource)
+  {
+    MOZ_COUNT_CTOR(MP4Stream);
+    MOZ_ASSERT(aResource);
+  }
+  virtual ~MP4Stream() {
+    MOZ_COUNT_DTOR(MP4Stream);
+  }
+
+  virtual bool ReadAt(int64_t aOffset, void* aBuffer, size_t aCount,
+                      size_t* aBytesRead) MOZ_OVERRIDE
+  {
+    uint32_t sum = 0;
+    uint32_t bytesRead = 0;
+    do {
+      uint64_t offset = aOffset + sum;
+      char* buffer = reinterpret_cast<char*>(aBuffer) + sum;
+      uint32_t toRead = aCount - sum;
+      nsresult rv = mResource->ReadAt(offset, buffer, toRead, &bytesRead);
+      if (NS_FAILED(rv)) {
+        return false;
+      }
+      sum += bytesRead;
+    } while (sum < aCount && bytesRead > 0);
+    *aBytesRead = sum;
+    return true;
+  }
+
+  virtual bool CachedReadAt(int64_t aOffset, void* aBuffer, size_t aCount,
+                            size_t* aBytesRead) MOZ_OVERRIDE
+  {
+    nsresult rv = mResource->ReadFromCache(reinterpret_cast<char*>(aBuffer),
+                                           aOffset, aCount);
+    if (NS_FAILED(rv)) {
+      *aBytesRead = 0;
+      return false;
+    }
+    *aBytesRead = aCount;
+    return true;
+  }
+
+  virtual bool Length(int64_t* aSize) MOZ_OVERRIDE
+  {
+    if (mResource->GetLength() < 0)
+      return false;
+    *aSize = mResource->GetLength();
+    return true;
+  }
+
+private:
+  RefPtr<MediaResource> mResource;
+};
 
 MP4Reader::MP4Reader(AbstractMediaDecoder* aDecoder)
   : MediaDecoderReader(aDecoder)
