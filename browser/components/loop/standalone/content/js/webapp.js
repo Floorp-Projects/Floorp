b66ea427344ed3a42ad84c8dde8d13c8480f19f8
Romain Gauthier <romain.gauthier@monkeypatch.me>
2014-12-15 22:28:18 +0000

    Bug 1109325 - Change the help url for Hello's standalone rooms. r=nperriault a=sylvestre

--- browser/components/loop/standalone/content/js/webapp.js
+++ browser/components/loop/standalone/content/js/webapp.js
@@ -257,7 +257,7 @@ loop.webapp = (function($, _, OT, mozL10n) {
                                   {vendorShortname: mozL10n.get("vendorShortname")}), 
                className: "footer-logo"}), 
           React.DOM.div({className: "footer-external-links"}, 
-            React.DOM.a({target: "_blank", href: loop.config.guestSupportUrl}, 
+            React.DOM.a({target: "_blank", href: loop.config.generalSupportUrl}, 
               mozL10n.get("support_link")
             )
           )
