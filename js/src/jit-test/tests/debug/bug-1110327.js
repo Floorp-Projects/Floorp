d338d7045941d7b1106ade7b63392eae2d74bd19
Nick Fitzgerald <fitzgen@gmail.com>
2014-12-16 10:50:44 -0800

    Bug 1110327 - Fix assertion failure: dbg->isDebuggee(node.compartment()), at vm/Debugger.cpp. For Debugger.prototype.findObjects, do result accumulation in the ubi::BreadthFirst traversal callback rather than by iterating over the traversed set of nodes after the traversal. r=shu

---
+++ js/src/jit-test/tests/debug/bug-1110327.js
@@ -0,0 +1,5 @@
+// Randomly chosen test: js/src/jit-test/tests/debug/Debugger-debuggees-10.js
+x = newGlobal()
+x.t = this
+// Randomly chosen test: js/src/jit-test/tests/debug/Debugger-findObjects-06.js
+Debugger(x).findObjects()
