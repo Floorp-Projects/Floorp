1af51ca640ff8faa71134839b9150cb8159820c5
Kartikaya Gupta <kats@mozilla.com>
2014-12-15 23:28:19 -0800

    Add a helper for forwarding event APZ info to TabParent. (bug 1109985 part 1, r=dvander)

---
+++ gfx/layers/apz/util/InputAPZContext.h
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_layers_InputAPZContext_h
+#define mozilla_layers_InputAPZContext_h
+
+#include "FrameMetrics.h"
+
+namespace mozilla {
+namespace layers {
+
+// InputAPZContext is used to communicate the ScrollableLayerGuid and input
+// block ID from nsIWidget to RenderFrameParent. It is conceptually attached
+// to any WidgetInputEvent that has been processed by APZ directly from a
+// widget.
+class MOZ_STACK_CLASS InputAPZContext
+{
+private:
+  static ScrollableLayerGuid sGuid;
+  static uint64_t sBlockId;
+
+public:
+  static ScrollableLayerGuid GetTargetLayerGuid();
+  static uint64_t GetInputBlockId();
+
+  InputAPZContext(const ScrollableLayerGuid& aGuid,
+                    const uint64_t& aBlockId);
+  ~InputAPZContext();
+
+private:
+  ScrollableLayerGuid mOldGuid;
+  uint64_t mOldBlockId;
+};
+
+}
+}
+
+#endif /* mozilla_layers_InputAPZContext_h */
