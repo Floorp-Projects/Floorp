c6a9fc19269165942496c96069e7bc5ca1055f44
Wes Kocher <wkocher@mozilla.com>
2014-12-16 15:44:15 -0800

    Backed out changeset 50d264e4ffa4 (bug 1052123) for android/linux reftest failures

--- layout/generic/nsFrameState.cpp
+++ layout/generic/nsFrameState.cpp
@@ -17,7 +17,6 @@
 #include "nsImageFrame.h"
 #include "nsInlineFrame.h"
 #include "nsPlaceholderFrame.h"
-#include "nsRubyTextFrame.h"
 #include "nsSVGContainerFrame.h"
 #include "nsTableCellFrame.h"
 #include "nsTableRowFrame.h"
