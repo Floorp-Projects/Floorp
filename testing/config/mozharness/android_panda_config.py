da2c0c3ea163330a680a7811c0a821c04cbcafe7
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2014-12-16 11:23:17 -0500

    Bug 1070041 - Move more Mozharness job definition parameters into the tree. r=ahal
    * We are unifying the in-tree mozharness configs to follow the same
    structure
    * We are moving run_filename into the tree
    * We are also staring the move to manage the testdir value which
    is already in use for Android test jobs
    
    --HG--
    extra : rebase_source : 6d667cd50e54fe16edc5ddff5f29039c5ae82523

--- testing/config/mozharness/android_panda_config.py
+++ testing/config/mozharness/android_panda_config.py
@@ -3,86 +3,135 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 config = {
-    "mochitest_options": [
-        "--deviceIP=%(device_ip)s",
-        "--xre-path=../hostutils/xre",
-        "--utility-path=../hostutils/bin", "--certificate-path=certs",
-        "--app=%(app_name)s", "--console-level=INFO",
-        "--http-port=%(http_port)s", "--ssl-port=%(ssl_port)s",
-        "--run-only-tests=android.json", "--symbols-path=%(symbols_path)s",
-        "--quiet", "--log-raw=%(raw_log_file)s"
-    ],
-    "reftest_options": [
-        "--deviceIP=%(device_ip)s",
-        "--xre-path=../hostutils/xre",
-        "--utility-path=../hostutils/bin",
-        "--app=%(app_name)s",
-        "--ignore-window-size", "--bootstrap",
-        "--http-port=%(http_port)s", "--ssl-port=%(ssl_port)s",
-        "--symbols-path=%(symbols_path)s",
-        "reftest/tests/layout/reftests/reftest.list"
-    ],
-    "crashtest_options": [
-        "--deviceIP=%(device_ip)s",
-        "--xre-path=../hostutils/xre",
-        "--utility-path=../hostutils/bin",
-        "--app=%(app_name)s",
-        "--ignore-window-size", "--bootstrap",
-        "--http-port=%(http_port)s", "--ssl-port=%(ssl_port)s",
-        "--symbols-path=%(symbols_path)s",
-        "reftest/tests/testing/crashtest/crashtests.list"
-    ],
-    "jsreftest_options": [
-        "--deviceIP=%(device_ip)s",
-        "--xre-path=../hostutils/xre",
-        "--utility-path=../hostutils/bin",
-        "--app=%(app_name)s",
-        "--ignore-window-size", "--bootstrap",
-        "--extra-profile-file=jsreftest/tests/user.js", "jsreftest/tests/jstests.list",
-        "--http-port=%(http_port)s", "--ssl-port=%(ssl_port)s",
-        "--symbols-path=%(symbols_path)s"
-    ],
-    "robocop_options": [
-        "--deviceIP=%(device_ip)s",
-        "--xre-path=../hostutils/xre",
-        "--utility-path=../hostutils/bin",
-        "--certificate-path=certs",
-        "--app=%(app_name)s", "--console-level=INFO",
-        "--http-port=%(http_port)s", "--ssl-port=%(ssl_port)s",
-        "--symbols-path=%(symbols_path)s",
-        "--robocop=mochitest/robocop.ini"
-    ],
-    "xpcshell_options": [
-        "--deviceIP=%(device_ip)s",
-        "--xre-path=../hostutils/xre",
-        "--manifest=xpcshell/tests/xpcshell.ini",
-        "--build-info-json=xpcshell/mozinfo.json",
-        "--testing-modules-dir=modules",
-        "--local-lib-dir=../fennec",
-        "--apk=../%(apk_name)s",
-        "--no-logfiles",
-        "--symbols-path=%(symbols_path)s",
-        "--log-raw=%(raw_log_file)s",
-    ],
-    "jittest_options": [
-        "bin/js",
-        "--remote",
-        "-j", "1",
-        "--deviceTransport=sut",
-        "--deviceIP=%(device_ip)s",
-        "--localLib=../tests/bin",
-        "--no-slow",
-        "--no-progress",
-        "--tinderbox",
-        "--tbpl"
-     ],
-     "cppunittest_options": [
-        "--symbols-path=%(symbols_path)s",
-        "--xre-path=tests/bin",
-        "--dm_trans=SUT",
-        "--deviceIP=%(device_ip)s",
-        "--localBinDir=../tests/bin",
-        "--apk=%(apk_path)s",
-        "--skip-manifest=../tests/cppunittests/android_cppunittest_manifest.txt"
-     ],
+    "suite_definitions": {
+        "cppunittest": {
+            "options": [
+                "--symbols-path=%(symbols_path)s",
+                "--xre-path=tests/bin",
+                "--dm_trans=SUT",
+                "--deviceIP=%(device_ip)s",
+                "--localBinDir=../tests/bin",
+                "--apk=%(apk_path)s",
+                "--skip-manifest=../tests/cppunittests/android_cppunittest_manifest.txt"
+            ],
+            "run_filename": "remotecppunittests.py",
+            "testsdir": "cppunittests"
+        },
+        "crashtest": {
+            "options": [
+                "--deviceIP=%(device_ip)s",
+                "--xre-path=../hostutils/xre",
+                "--utility-path=../hostutils/bin",
+                "--app=%(app_name)s",
+                "--ignore-window-size",
+                "--bootstrap",
+                "--http-port=%(http_port)s",
+                "--ssl-port=%(ssl_port)s",
+                "--symbols-path=%(symbols_path)s",
+                "reftest/tests/testing/crashtest/crashtests.list"
+            ],
+            "run_filename": "remotereftest.py",
+            "testsdir": "reftest"
+        },
+        "jittest": {
+            "options": [
+                "bin/js",
+                "--remote",
+                "-j",
+                "1",
+                "--deviceTransport=sut",
+                "--deviceIP=%(device_ip)s",
+                "--localLib=../tests/bin",
+                "--no-slow",
+                "--no-progress",
+                "--tinderbox",
+                "--tbpl"
+            ],
+            "run_filename": "jit_test.py",
+            "testsdir": "jit-test/jit-test"
+        },
+        "jsreftest": {
+            "options": [
+                "--deviceIP=%(device_ip)s",
+                "--xre-path=../hostutils/xre",
+                "--utility-path=../hostutils/bin",
+                "--app=%(app_name)s",
+                "--ignore-window-size",
+                "--bootstrap",
+                "--extra-profile-file=jsreftest/tests/user.js",
+                "jsreftest/tests/jstests.list",
+                "--http-port=%(http_port)s",
+                "--ssl-port=%(ssl_port)s",
+                "--symbols-path=%(symbols_path)s"
+            ],
+            "run_filename": "remotereftest.py",
+            "testsdir": "reftest"
+        },
+        "mochitest": {
+            "options": [
+                "--deviceIP=%(device_ip)s",
+                "--xre-path=../hostutils/xre",
+                "--utility-path=../hostutils/bin",
+                "--certificate-path=certs",
+                "--app=%(app_name)s",
+                "--console-level=INFO",
+                "--http-port=%(http_port)s",
+                "--ssl-port=%(ssl_port)s",
+                "--run-only-tests=android.json",
+                "--symbols-path=%(symbols_path)s",
+                "--quiet",
+                "--log-raw=%(raw_log_file)s"
+            ],
+            "run_filename": "runtestsremote.py",
+            "testsdir": "mochitest"
+        },
+        "reftest": {
+            "options": [
+                "--deviceIP=%(device_ip)s",
+                "--xre-path=../hostutils/xre",
+                "--utility-path=../hostutils/bin",
+                "--app=%(app_name)s",
+                "--ignore-window-size",
+                "--bootstrap",
+                "--http-port=%(http_port)s",
+                "--ssl-port=%(ssl_port)s",
+                "--symbols-path=%(symbols_path)s",
+                "reftest/tests/layout/reftests/reftest.list"
+            ],
+            "run_filename": "remotereftest.py",
+            "testsdir": "reftest"
+        },
+        "robocop": {
+            "options": [
+                "--deviceIP=%(device_ip)s",
+                "--xre-path=../hostutils/xre",
+                "--utility-path=../hostutils/bin",
+                "--certificate-path=certs",
+                "--app=%(app_name)s",
+                "--console-level=INFO",
+                "--http-port=%(http_port)s",
+                "--ssl-port=%(ssl_port)s",
+                "--symbols-path=%(symbols_path)s",
+                "--robocop=mochitest/robocop.ini"
+            ],
+            "run_filename": "runtestsremote.py",
+            "testsdir": "mochitest"
+        },
+        "xpcshell": {
+            "options": [
+                "--deviceIP=%(device_ip)s",
+                "--xre-path=../hostutils/xre",
+                "--manifest=xpcshell/tests/xpcshell.ini",
+                "--build-info-json=xpcshell/mozinfo.json",
+                "--testing-modules-dir=modules",
+                "--local-lib-dir=../fennec",
+                "--apk=../%(apk_name)s",
+                "--no-logfiles",
+                "--symbols-path=%(symbols_path)s",
+                "--log-raw=%(raw_log_file)s"
+            ],
+            "run_filename": "remotexpcshelltests.py",
+            "testsdir": "xpcshell"
+        }
+    }
 }
