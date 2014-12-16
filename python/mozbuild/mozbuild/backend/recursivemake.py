910c26b5550c58deaeb8ba7ac1c0339ab6f8cb53
Nathan Froyd <froydnj@mozilla.com>
2014-12-16 15:13:27 -0500

    Bug 883954 - part 2 - add support for defining generating scripts for GENERATED_FILES; r=gps
    Now that we have proper moz.build objects for GENERATED_FILES, we can
    add 'script' flags and 'args' flags in moz.build for select
    GENERATED_FILES.  We restrict 'args' to being filenames for ease of
    implementing checks for file existence, and many (all?) of the examples
    of file generation throughout the tree don't need arbitrary strings or
    Python data.

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -406,7 +406,14 @@ class RecursiveMakeBackend(CommonBackend):
             self._process_exports(obj, obj.exports, backend_file)
 
         elif isinstance(obj, GeneratedFile):
-            backend_file.write('GENERATED_FILES += %s\n' % obj.filename)
+            backend_file.write('GENERATED_FILES += %s\n' % obj.output)
+            if obj.script:
+                backend_file.write("""{output}: {script}{inputs}
+\t$(call py_action,file_generate,{script} {output}{inputs})
+
+""".format(output=obj.output,
+           inputs=' ' + ' '.join(obj.inputs) if obj.inputs else '',
+           script=obj.script))
 
         elif isinstance(obj, TestHarnessFiles):
             self._process_test_harness_files(obj, backend_file)
