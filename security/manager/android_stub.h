301128304ae417b8cf046d2326451e80fa595ac3
Michael Wu <mwu@mozilla.com>
2014-12-16 21:35:09 -0500

    Bug 1103816 - Add support for gonk-L to android_stub.h, r=glandium

--- security/manager/android_stub.h
+++ security/manager/android_stub.h
@@ -15,6 +15,23 @@
 #include <sys/cdefs.h>
 #include <linux/kernel.h>
 
+#if ANDROID_VERSION >= 21
+#include <sys/resource.h>
+#include <unistd.h>
+
+static int getdtablesize(void)
+{
+    struct rlimit r;
+    if (getrlimit(RLIMIT_NOFILE, &r) < 0) {
+        return sysconf(_SC_OPEN_MAX);
+    }
+    return r.rlim_cur;
+}
+#else
+#define RTLD_NOLOAD 0
+extern int getdtablesize(void);
+#endif
+
 #define sysinfo(foo) -1
 
 #endif /* ANDROID_STUB_H */
