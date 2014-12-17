a666ddf9d7c2755a172361e0fffa228b3a64db34
Brian Birtles <birtles@gmail.com>
2014-12-18 08:42:41 +0900

    Bug 927349 part 5 - Cancel players that are no longer used; r=jwatt
    This patch adds a means of terminating an animation so that is has no effect.
    The procedure is defined by Web Animations:
    
      http://w3c.github.io/web-animations/#cancelling-a-player-section
    
    We don't implement all of this, however, since we don't currently support the
    finished promise or custom effects.
    
    In a later bug we will expose this as the cancel() method on AnimationPlayer.
    
    We call this method for terminated animations in nsAnimationManager and
    nsTransitionManager to ensure they get removed from the pending player tracker
    and so that, for example, the ready promise of CSS Animation player objects is
    rejected when the corresponding item is removed from animation-name.

--- layout/style/nsTransitionManager.cpp
+++ layout/style/nsTransitionManager.cpp
@@ -300,6 +300,7 @@ nsTransitionManager::StyleContextChanged(dom::Element *aElement,
                                              currentValue) ||
           currentValue != segment.mToValue) {
         // stop the transition
+        player->Cancel();
         players.RemoveElementAt(i);
         collection->UpdateAnimationGeneration(mPresContext);
       }
