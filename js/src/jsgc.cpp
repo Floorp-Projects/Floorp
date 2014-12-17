03b476e942d7c8e711db86ea8ac962e15e294256
Jon Coppeard <jcoppeard@mozilla.com>
2014-12-17 18:04:01 +0000

    Bug 650161 - Reduce parallelism in parallel update after compacting GC r=terrence

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -2586,8 +2586,10 @@ GCRuntime::updateAllCellPointersParallel(ArenasToUpdate &source)
 {
     AutoDisableProxyCheck noProxyCheck(rt); // These checks assert when run in parallel.
 
+    const size_t minTasks = 2;
     const size_t maxTasks = 8;
-    unsigned taskCount = Min(HelperThreadState().cpuCount, maxTasks);
+    unsigned taskCount = Min(Max(HelperThreadState().cpuCount / 2, minTasks),
+                             maxTasks);
     UpdateCellPointersTask updateTasks[maxTasks];
 
     AutoLockHelperThreadState lock;
