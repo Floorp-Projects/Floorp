2c397b31e3dbdef71e8ccfcbf5931c1d425b8738
Szu-Yu Chen [:aknow] <szchen@mozilla.com>
2014-12-17 23:47:55 +0800

    Bug 1100200 - Part 5: Merge emergency and radio-off related tests. r=hsinyi

--- dom/telephony/test/marionette/test_outgoing_radio_off.js
+++ dom/telephony/test/marionette/test_outgoing_radio_off.js
@@ -4,18 +4,57 @@
 MARIONETTE_TIMEOUT = 60000;
 MARIONETTE_HEAD_JS = 'head.js';
 
+let connection;
+const normalNumber = "0912345678";
+const emergencyNumber = "112";
+let outCall;
+
+function testDial_NormalNumber() {
+  return gSetRadioEnabled(connection, false)
+    .then(() => gDial(normalNumber))
+    .catch(cause => {
+      is(cause, "RadioNotAvailable");
+      return gCheckAll(null, [], "", [], []);
+    });
+}
+
+function testDial_EmergencyNumber() {
+  return gSetRadioEnabled(connection, false)
+    .then(() => gDial(emergencyNumber))
+    .then(call => { outCall = call; })
+    .then(() => gRemoteAnswer(outCall))
+    .then(() => gDelay(1000))  // See Bug 1018051 for the purpose of the delay.
+    .then(() => gRemoteHangUp(outCall));
+}
+
+function testDialEmergency_NormalNumber() {
+  return gSetRadioEnabled(connection, false)
+    .then(() => gDialEmergency(normalNumber))
+    .catch(cause => {
+      is(cause, "RadioNotAvailable");
+      return gCheckAll(null, [], "", [], []);
+    });
+}
+
+function testDialEmergency_EmergencyNumber() {
+  return gSetRadioEnabled(connection, false)
+    .then(() => gDialEmergency(emergencyNumber))
+    .then(call => { outCall = call; })
+    .then(() => gRemoteAnswer(outCall))
+    .then(() => gDelay(1000))  // See Bug 1018051 for the purpose of the delay.
+    .then(() => gRemoteHangUp(outCall));
+}
+
 startTestWithPermissions(['mobileconnection'], function() {
-  let connection = navigator.mozMobileConnections[0];
+  connection = navigator.mozMobileConnections[0];
   ok(connection instanceof MozMobileConnection,
      "connection is instanceof " + connection.constructor);
 
-  gSetRadioEnabled(connection, false)
-    .then(() => gDial("0912345678"))
-    .catch(cause => {
-      is(telephony.active, null);
-      is(telephony.calls.length, 0);
-      is(cause, "RadioNotAvailable");
-    })
+  Promise.resolve()
+    .then(() => testDial_NormalNumber())
+    .then(() => testDial_EmergencyNumber())
+    .then(() => testDialEmergency_NormalNumber())
+    .then(() => testDialEmergency_EmergencyNumber())
     .then(() => gSetRadioEnabled(connection, true))
     .catch(error => ok(false, "Promise reject: " + error))
     .then(finish);
