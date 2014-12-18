ab9e34977de6002ab047b2da5af601ff35b5a17c
Mason Chang <mchang@mozilla.com>
2014-12-18 08:30:06 -0800

    Bug 1101974. Part 4 - Refactor VsyncSource on OS X. r=mstange

--- gfx/thebes/gfxPlatformMac.h
+++ gfx/thebes/gfxPlatformMac.h
@@ -9,7 +9,12 @@
 #include "nsTArrayForwardDeclare.h"
 #include "gfxPlatform.h"
 
-namespace mozilla { namespace gfx { class DrawTarget; }}
+namespace mozilla {
+namespace gfx {
+class DrawTarget;
+class VsyncSource;
+} // gfx
+} // mozilla
 
 class gfxPlatformMac : public gfxPlatform {
 public:
@@ -67,7 +72,7 @@ public:
 
     virtual bool UseTiling() MOZ_OVERRIDE;
     virtual bool UseProgressivePaint() MOZ_OVERRIDE;
-    virtual void InitHardwareVsync() MOZ_OVERRIDE;
+    virtual already_AddRefed<mozilla::gfx::VsyncSource> CreateHardwareVsyncSource() MOZ_OVERRIDE;
 
     // lower threshold on font anti-aliasing
     uint32_t GetAntiAliasingThreshold() { return mFontAntiAliasingThreshold; }
