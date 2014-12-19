296261621dff911880536e53adf13efb34c61c1b
Nikhil Marathe <nsm.nikhil@gmail.com>
2014-12-19 05:01:53 -0800

    Bug 1113627 - Refactor lifecycle event dispatching and handling. r=baku,bz

--- dom/workers/ServiceWorkerManager.h
+++ dom/workers/ServiceWorkerManager.h
@@ -462,6 +462,13 @@ private:
                                         const nsAString& aName);
 
   void
+  FireUpdateFound(ServiceWorkerRegistrationInfo* aRegistration)
+  {
+    FireEventOnServiceWorkerRegistrations(aRegistration,
+                                          NS_LITERAL_STRING("updatefound"));
+  }
+
+  void
   FireControllerChange(ServiceWorkerRegistrationInfo* aRegistration);
 
   void
