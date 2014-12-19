8e3bc159b88041623176a30aec3de4ac1eb86c66
Jonathan Kew <jkew@mozilla.com>
2014-12-19 14:55:30 +0000

    Bug 1111525 - Remove unused lineFlags returned by nsILineIterator. r=roc

--- layout/generic/nsLineBox.cpp
+++ layout/generic/nsLineBox.cpp
@@ -639,12 +639,10 @@ NS_IMETHODIMP
 nsLineIterator::GetLine(int32_t aLineNumber,
                         nsIFrame** aFirstFrameOnLine,
                         int32_t* aNumFramesOnLine,
-                        nsRect& aLineBounds,
-                        uint32_t* aLineFlags)
+                        nsRect& aLineBounds)
 {
   NS_ENSURE_ARG_POINTER(aFirstFrameOnLine);
   NS_ENSURE_ARG_POINTER(aNumFramesOnLine);
-  NS_ENSURE_ARG_POINTER(aLineFlags);
 
   if ((aLineNumber < 0) || (aLineNumber >= mNumLines)) {
     *aFirstFrameOnLine = nullptr;
@@ -656,16 +654,6 @@ nsLineIterator::GetLine(int32_t aLineNumber,
   *aFirstFrameOnLine = line->mFirstChild;
   *aNumFramesOnLine = line->GetChildCount();
   aLineBounds = line->GetPhysicalBounds();
-
-  uint32_t flags = 0;
-  if (line->IsBlock()) {
-    flags |= NS_LINE_FLAG_IS_BLOCK;
-  }
-  else {
-    if (line->HasBreakAfter())
-      flags |= NS_LINE_FLAG_ENDS_IN_BREAK;
-  }
-  *aLineFlags = flags;
 
   return NS_OK;
 }
