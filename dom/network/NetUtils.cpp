95c784e97d30e90a39912e7124ef66c5976cbeb1
Vincent Chang <vchang@mozilla.com>
2014-12-19 17:27:21 +0800

    Bug 1113535 - (gonk-L-Wifi) Support dhcp client function in Android L. r=hchang

--- dom/network/NetUtils.cpp
+++ dom/network/NetUtils.cpp
@@ -178,9 +178,11 @@ int32_t NetUtils::do_dhcp_do_request(const char *ifname,
     char domains[PROPERTY_VALUE_MAX];
     ret = dhcp_do_request(ifname, ipaddr, gateway, prefixLength, dns,
                           server, lease, vendorinfo, domains);
-  } else if (sdkVersion == 19) {
-    // JB 4.4
+  } else if (sdkVersion >= 19) {
+    // KitKat 4.4.X
     // http://androidxref.com/4.4_r1/xref/system/core/libnetutils/dhcp_utils.c#18
+    // Lollipop 5.0
+    //http://androidxref.com/5.0.0_r2/xref/system/core/libnetutils/dhcp_utils.c#186
     DEFINE_DLFUNC(dhcp_do_request, int32_t, const char*, char*, char*,  uint32_t*, char**, char*, uint32_t*, char*, char*, char*)
     USE_DLFUNC(dhcp_do_request)
     char *dns[3] = {dns1, dns2, nullptr};
