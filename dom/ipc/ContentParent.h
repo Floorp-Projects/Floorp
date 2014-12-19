9569e27351d4c696a12958c5cf39273b428e188a
Blake Kaplan <mrbkap@gmail.com>
2014-12-19 12:07:04 -0500

    Bug 1113313 - Rename these functions to better reflect what they do. r=billm

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -722,13 +722,13 @@ private:
     RecvOpenAnonymousTemporaryFile(FileDescriptor* aFD) MOZ_OVERRIDE;
 
     virtual bool
-    RecvFormProcessValue(const nsString& oldValue, const nsString& challenge,
-                         const nsString& keytype, const nsString& keyparams,
-                         nsString* newValue) MOZ_OVERRIDE;
+    RecvKeygenProcessValue(const nsString& oldValue, const nsString& challenge,
+                           const nsString& keytype, const nsString& keyparams,
+                           nsString* newValue) MOZ_OVERRIDE;
 
     virtual bool
-    RecvFormProvideContent(nsString* aAttribute,
-                           nsTArray<nsString>* aContent) MOZ_OVERRIDE;
+    RecvKeygenProvideContent(nsString* aAttribute,
+                             nsTArray<nsString>* aContent) MOZ_OVERRIDE;
 
     virtual PFileDescriptorSetParent*
     AllocPFileDescriptorSetParent(const mozilla::ipc::FileDescriptor&) MOZ_OVERRIDE;
