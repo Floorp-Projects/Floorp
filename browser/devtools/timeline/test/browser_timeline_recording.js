e574be1e3dccb69847b8e0110323c12f74343b0a
Patrick Brosset <pbrosset@mozilla.com>
2014-12-19 02:59:00 +0100

    Bug 1113552 - Use add_task in all browser/devtools/timeline browser mochitests; r=paul

--- browser/devtools/timeline/test/browser_timeline_recording.js
+++ browser/devtools/timeline/test/browser_timeline_recording.js
@@ -5,7 +5,7 @@
  * Tests if the timeline can properly start and stop a recording.
  */
 
-let test = Task.async(function*() {
+add_task(function*() {
   let { target, panel } = yield initTimelinePanel(SIMPLE_URL);
   let { $, gFront, TimelineController } = panel.panelWin;
 
@@ -34,7 +34,4 @@ let test = Task.async(function*() {
   ok(TimelineController.getInterval().endTime >
      TimelineController.getInterval().startTime,
     "Some time has passed since the recording started.");
-
-  yield teardown(panel);
-  finish();
 });
