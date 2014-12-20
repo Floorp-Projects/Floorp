ac0b453d874ddb791ae342c892d380d2c46b9c74
Jeff Walden <jwalden@mit.edu>
2014-12-20 12:27:52 -0500

    Bug 1111244 - Reorder some #includes in prmjtime.cpp so the include style-checker doesn't throw a fit when an #if is removed shortly after this revision.  r=style-patrol
    --HG--
    extra : rebase_source : 9624950bd8b064dd296578550711b17af4116f24

--- js/src/prmjtime.cpp
+++ js/src/prmjtime.cpp
@@ -23,13 +23,15 @@
 #ifdef XP_WIN
 #include <windef.h>
 #include <winbase.h>
-#include <mmsystem.h> /* for timeBegin/EndPeriod */
 /* VC++ 8.0 or later */
 #if _MSC_VER >= 1400
 #define NS_HAVE_INVALID_PARAMETER_HANDLER 1
 #endif
 #ifdef NS_HAVE_INVALID_PARAMETER_HANDLER
 #include <crtdbg.h>   /* for _CrtSetReportMode */
+#endif
+#include <mmsystem.h> /* for timeBegin/EndPeriod */
+#ifdef NS_HAVE_INVALID_PARAMETER_HANDLER
 #include <stdlib.h>   /* for _set_invalid_parameter_handler */
 #endif
 
