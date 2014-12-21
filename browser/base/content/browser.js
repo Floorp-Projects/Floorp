248454b8c919f6c0c83bfbb43806287b215ae1c9
Phil Ringnalda <philringnalda@gmail.com>
2014-12-20 17:58:08 -0800

    Backed out 2 changesets (bug 1106432) for mochitest-a11y failures CLOSED TREE
    Backed out changeset 513130bd59ed (bug 1106432)
    Backed out changeset 4dbb2ddd0093 (bug 1106432)

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -3308,32 +3308,8 @@ const BrowserSearch = {
 
     if (hidden)
       browser.hiddenEngines = engines;
-    else {
-      browser.engines = engines;
-      if (browser == gBrowser.selectedBrowser)
-        this.updateSearchButton();
-    }
-  },
-
-  /**
-   * Update the browser UI to show whether or not additional engines are
-   * available when a page is loaded or the user switches tabs to a page that
-   * has search engines.
-   */
-  updateSearchButton: function() {
-    var searchBar = this.searchBar;
-
-    // The search bar binding might not be applied even though the element is
-    // in the document (e.g. when the navigation toolbar is hidden), so check
-    // for .searchButton specifically.
-    if (!searchBar || !searchBar.searchButton)
-      return;
-
-    var engines = gBrowser.selectedBrowser.engines;
-    if (engines && engines.length > 0)
-      searchBar.setAttribute("addengines", "true");
     else
-      searchBar.removeAttribute("addengines");
+      browser.engines = engines;
   },
 
   /**
@@ -4350,7 +4326,6 @@ var XULBrowserWindow = {
 
   asyncUpdateUI: function () {
     FeedHandler.updateFeeds();
-    BrowserSearch.updateSearchButton();
   },
 
   // Left here for add-on compatibility, see bug 752434
