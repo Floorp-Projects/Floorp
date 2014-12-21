278608061f2ab267ab29e78e99b95fbc277820b7
Nick Alexander <nalexander@mozilla.com>
2014-12-21 15:29:18 -0800

    Bug 1108771 - Part 2: Implement |mach bootstrap| for mobile/android on Debian-like systems. r=gps
    This adds a generic android Python module that handles:
    
    * downloading and unpacking Google's Android SDK and NDK bundles;
    * using the |android| tool to install additional Android packages;
    * printing a mozconfig snippet suitable for mobile/android builds.

--- python/mozboot/bin/bootstrap.py
+++ python/mozboot/bin/bootstrap.py
@@ -32,6 +32,7 @@ REPOSITORY_PATH_PREFIX = 'python/mozboot'
 
 REPOSITORY_PATHS = [
     'mozboot/__init__.py',
+    'mozboot/android.py',
     'mozboot/base.py',
     'mozboot/bootstrap.py',
     'mozboot/centos.py',
--- python/mozboot/mozboot/debian.py
+++ python/mozboot/mozboot/debian.py
@@ -2,6 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
+import sys
+
 from mozboot.base import BaseBootstrapper
 
 class DebianBootstrapper(BaseBootstrapper):
@@ -22,6 +25,8 @@ class DebianBootstrapper(BaseBootstrapper):
     # Subclasses can add packages to this variable to have them installed.
     DISTRO_PACKAGES = []
 
+    # These are common packages for building Firefox for Desktop
+    # (browser) for all Debian-derived distros (such as Ubuntu).
     BROWSER_COMMON_PACKAGES = [
         'libasound2-dev',
         'libcurl4-openssl-dev',
@@ -44,6 +49,21 @@ class DebianBootstrapper(BaseBootstrapper):
     # Subclasses can add packages to this variable to have them installed.
     BROWSER_DISTRO_PACKAGES = []
 
+    # These are common packages for building Firefox for Android
+    # (mobile/android) for all Debian-derived distros (such as Ubuntu).
+    MOBILE_ANDROID_COMMON_PACKAGES = [
+        'zlib1g-dev', # mobile/android requires system zlib.
+        'openjdk-7-jdk',
+        'ant',
+        'wget', # For downloading the Android SDK and NDK.
+        'libncurses5:i386', # See comments about i386 below.
+        'libstdc++6:i386',
+        'zlib1g:i386',
+    ]
+
+    # Subclasses can add packages to this variable to have them installed.
+    MOBILE_ANDROID_DISTRO_PACKAGES = []
+
     def __init__(self, version, dist_id):
         BaseBootstrapper.__init__(self)
 
@@ -52,12 +72,57 @@ class DebianBootstrapper(BaseBootstrapper):
 
         self.packages = self.COMMON_PACKAGES + self.DISTRO_PACKAGES
         self.browser_packages = self.BROWSER_COMMON_PACKAGES + self.BROWSER_DISTRO_PACKAGES
+        self.mobile_android_packages = self.MOBILE_ANDROID_COMMON_PACKAGES + self.MOBILE_ANDROID_DISTRO_PACKAGES
 
     def install_system_packages(self):
         self.apt_install(*self.packages)
 
     def install_browser_packages(self):
         self.apt_install(*self.browser_packages)
+
+    def install_mobile_android_packages(self):
+        import android
+
+        # Multi-part process:
+        # 1. System packages.
+        # 2. Android SDK and NDK.
+        # 3. Android packages.
+
+        # 1. This is hard to believe, but the Android SDK binaries are 32-bit
+        # and that conflicts with 64-bit Debian and Ubuntu installations out of
+        # the box.  The solution is to add the i386 architecture.  See
+        # "Troubleshooting Ubuntu" at
+        # http://developer.android.com/sdk/installing/index.html?pkg=tools.
+        self.run_as_root(['dpkg', '--add-architecture', 'i386'])
+        # self.apt_update()
+        self.apt_install(*self.mobile_android_packages)
+
+        # 2. The user may have an external Android SDK (in which case we save
+        # them a lengthy download), or they may have already completed the
+        # download. We unpack to ~/.mozbuild/{android-sdk-linux, android-ndk-r8e}.
+        mozbuild_path = os.environ.get('MOZBUILD_STATE_PATH', os.path.expanduser(os.path.join('~', '.mozbuild')))
+        self.sdk_path = os.environ.get('ANDROID_SDK_HOME', os.path.join(mozbuild_path, 'android-sdk-linux'))
+        self.ndk_path = os.environ.get('ANDROID_NDK_HOME', os.path.join(mozbuild_path, 'android-ndk-r8e'))
+        self.sdk_url = 'https://dl.google.com/android/android-sdk_r24.0.1-linux.tgz'
+        is_64bits = sys.maxsize > 2**32
+        if is_64bits:
+            self.ndk_url = 'https://dl.google.com/android/ndk/android-ndk-r8e-linux-x86_64.tar.bz2'
+        else:
+            self.ndk_url = 'https://dl.google.com/android/ndk/android-ndk-r8e-linux-x86.tar.bz2'
+        android.ensure_android_sdk_and_ndk(path=mozbuild_path,
+            sdk_path=self.sdk_path, sdk_url=self.sdk_url,
+            ndk_path=self.ndk_path, ndk_url=self.ndk_url)
+
+        # 3. We expect the |android| tool to at
+        # ~/.mozbuild/android-sdk-linux/tools/android.
+        android_tool = os.path.join(self.sdk_path, 'tools', 'android')
+        android.ensure_android_packages(android_tool=android_tool)
+
+    def suggest_mobile_android_mozconfig(self):
+        import android
+
+        android.suggest_mozconfig(sdk_path=self.sdk_path,
+            ndk_path=self.ndk_path)
 
     def _update_package_manager(self):
         self.run_as_root(['apt-get', 'update'])
