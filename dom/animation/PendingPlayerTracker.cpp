99fc4a3ae2bfd3e5b59ad5bfafd96349e0adc6e7
Brian Birtles <birtles@gmail.com>
2014-12-22 09:35:41 +0900

    Bug 927349 part 9 - Add PendingPlayerTracker::StartPendingPlayers; r=jwatt

--- dom/animation/PendingPlayerTracker.cpp
+++ dom/animation/PendingPlayerTracker.cpp
@@ -5,6 +5,8 @@
 
 #include "PendingPlayerTracker.h"
 
+#include "mozilla/dom/AnimationTimeline.h"
+
 using namespace mozilla;
 
 namespace mozilla {
@@ -30,6 +32,38 @@ bool
 PendingPlayerTracker::IsWaitingToPlay(dom::AnimationPlayer const& aPlayer) const
 {
   return mPlayPendingSet.Contains(const_cast<dom::AnimationPlayer*>(&aPlayer));
+}
+
+PLDHashOperator
+StartPlayerAtTime(nsRefPtrHashKey<dom::AnimationPlayer>* aKey,
+                  void* aReadyTime)
+{
+  dom::AnimationPlayer* player = aKey->GetKey();
+
+  // For animations that are waiting until their first frame has rendered
+  // before starting, we record the moment when they finish painting
+  // as the "ready time" and make any pending layer animations start at
+  // that time.
+  //
+  // Here we fast-forward the player's timeline to the same "ready time" and
+  // then tell the player to start at the timeline's current time.
+  //
+  // Redundant calls to FastForward with the same ready time are ignored by
+  // AnimationTimeline.
+  dom::AnimationTimeline* timeline = player->Timeline();
+  timeline->FastForward(*static_cast<const TimeStamp*>(aReadyTime));
+
+  player->StartNow();
+
+  return PL_DHASH_NEXT;
+}
+
+void
+PendingPlayerTracker::StartPendingPlayers(const TimeStamp& aReadyTime)
+{
+  mPlayPendingSet.EnumerateEntries(StartPlayerAtTime,
+                                   const_cast<TimeStamp*>(&aReadyTime));
+  mPlayPendingSet.Clear();
 }
 
 } // namespace mozilla
