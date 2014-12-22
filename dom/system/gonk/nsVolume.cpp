b08a5cc6eb2d9b3ddb19aa718abc18a6981fa952
Eden Chuang <echuang@mozilla.com>
2014-12-22 10:33:33 +0800

    Bug 1033952 - Implement "Removable" and "HotSwappable" APIs for device storage. r=dhylands

--- dom/system/gonk/nsVolume.cpp
+++ dom/system/gonk/nsVolume.cpp
@@ -59,7 +59,9 @@ nsVolume::nsVolume(const Volume* aVolume)
     mIsMediaPresent(aVolume->MediaPresent()),
     mIsSharing(aVolume->IsSharing()),
     mIsFormatting(aVolume->IsFormatting()),
-    mIsUnmounting(aVolume->IsUnmounting())
+    mIsUnmounting(aVolume->IsUnmounting()),
+    mIsRemovable(aVolume->IsRemovable()),
+    mIsHotSwappable(aVolume->IsHotSwappable())
 {
 }
 
@@ -116,6 +118,18 @@ bool nsVolume::Equals(nsIVolume* aVolume)
   bool isUnmounting;
   aVolume->GetIsUnmounting(&isUnmounting);
   if (mIsUnmounting != isUnmounting) {
+    return false;
+  }
+
+  bool isRemovable;
+  aVolume->GetIsRemovable(&isRemovable);
+  if (mIsRemovable != isRemovable) {
+    return false;
+  }
+
+  bool isHotSwappable;
+  aVolume->GetIsHotSwappable(&isHotSwappable);
+  if (mIsHotSwappable != isHotSwappable) {
     return false;
   }
 
@@ -200,6 +214,18 @@ NS_IMETHODIMP nsVolume::GetIsFake(bool *aIsFake)
   return NS_OK;
 }
 
+NS_IMETHODIMP nsVolume::GetIsRemovable(bool *aIsRemovable)
+{
+  *aIsRemovable = mIsRemovable;
+  return NS_OK;
+}
+
+NS_IMETHODIMP nsVolume::GetIsHotSwappable(bool *aIsHotSwappable)
+{
+  *aIsHotSwappable = mIsHotSwappable;
+  return NS_OK;
+}
+
 NS_IMETHODIMP nsVolume::Format()
 {
   MOZ_ASSERT(XRE_GetProcessType() == GeckoProcessType_Default);
@@ -277,11 +303,12 @@ nsVolume::LogState() const
 {
   if (mState == nsIVolume::STATE_MOUNTED) {
     LOG("nsVolume: %s state %s @ '%s' gen %d locked %d fake %d "
-        "media %d sharing %d formatting %d unmounting %d",
+        "media %d sharing %d formatting %d unmounting %d removable %d hotswappable %d",
         NameStr().get(), StateStr(), MountPointStr().get(),
         MountGeneration(), (int)IsMountLocked(), (int)IsFake(),
         (int)IsMediaPresent(), (int)IsSharing(),
-        (int)IsFormatting(), (int)IsUnmounting());
+        (int)IsFormatting(), (int)IsUnmounting(),
+        (int)IsRemovable(), (int)IsHotSwappable());
     return;
   }
 
@@ -300,6 +327,8 @@ void nsVolume::Set(nsIVolume* aVolume)
   aVolume->GetIsSharing(&mIsSharing);
   aVolume->GetIsFormatting(&mIsFormatting);
   aVolume->GetIsUnmounting(&mIsUnmounting);
+  aVolume->GetIsRemovable(&mIsRemovable);
+  aVolume->GetIsHotSwappable(&mIsHotSwappable);
 
   int32_t volMountGeneration;
   aVolume->GetMountGeneration(&volMountGeneration);
@@ -374,6 +403,24 @@ nsVolume::SetIsFake(bool aIsFake)
     // The media is always present for fake volumes.
     mIsMediaPresent = true;
     MOZ_ASSERT(!mIsSharing);
+  }
+}
+
+void
+nsVolume::SetIsRemovable(bool aIsRemovable)
+{
+  mIsRemovable = aIsRemovable;
+  if (!mIsRemovable) {
+    mIsHotSwappable = false;
+  }
+}
+
+void
+nsVolume::SetIsHotSwappable(bool aIsHotSwappable)
+{
+  mIsHotSwappable = aIsHotSwappable;
+  if (mIsHotSwappable) {
+    mIsRemovable = true;
   }
 }
 
