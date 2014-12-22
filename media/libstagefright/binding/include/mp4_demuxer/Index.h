06c7afc970f1d3b9205d644472210aeae8a49599
Bobby Holley <bobbyholley@gmail.com>
2014-12-21 22:04:36 -0800

    Bug 1114383 - Assert that access to mMoofs goes through a monitor. r=k17e, a=sledru

--- media/libstagefright/binding/include/mp4_demuxer/Index.h
+++ media/libstagefright/binding/include/mp4_demuxer/Index.h
@@ -37,7 +37,7 @@ public:
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(Index)
 
   Index(const stagefright::Vector<stagefright::MediaSource::Indice>& aIndex,
-        Stream* aSource, uint32_t aTrackId);
+        Stream* aSource, uint32_t aTrackId, Monitor* aMonitor);
 
   void UpdateMoofIndex(const nsTArray<mozilla::MediaByteRange>& aByteRanges);
   Microseconds GetEndCompositionIfBuffered(
@@ -56,6 +56,7 @@ private:
   Stream* mSource;
   nsTArray<Sample> mIndex;
   nsAutoPtr<MoofParser> mMoofParser;
+  Monitor* mMonitor;
 };
 }
 
