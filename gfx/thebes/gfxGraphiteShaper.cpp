3478d8e504fe6d3cc0af6fd6060015525d6aaaca
Jonathan Kew <jkew@mozilla.com>
2014-12-23 12:50:10 +0000

    Bug 1108616 - part 3 - Only do language-specific shaping when the language was explicitly tagged. r=jdaggett

--- gfx/thebes/gfxGraphiteShaper.cpp
+++ gfx/thebes/gfxGraphiteShaper.cpp
@@ -145,7 +145,7 @@ gfxGraphiteShaper::ShapeText(gfxContext      *aContext,
         grLang = MakeGraphiteLangTag(style->languageOverride);
     } else if (entry->mLanguageOverride) {
         grLang = MakeGraphiteLangTag(entry->mLanguageOverride);
-    } else {
+    } else if (style->explicitLanguage) {
         nsAutoCString langString;
         style->language->ToUTF8String(langString);
         grLang = GetGraphiteTagForLang(langString);
