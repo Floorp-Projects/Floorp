44b8e3ac7cbd3585f5b6f17cd0b9d209b2edaaf9
Jim Blandy <jimb@mozilla.com>
2014-12-23 16:47:52 -0800

    Bug 967234: Don't block self-hosted code in the devtool client code; Debugger takes care of this. r=ejpbruel
    --HG--
    extra : rebase_source : 6939941cb3c486a3541518a594b28af6b153f243

--- toolkit/devtools/server/actors/tracer.js
+++ toolkit/devtools/server/actors/tracer.js
@@ -20,11 +20,7 @@ function getFrameDepth(frame) {
     if (!frame.older) {
       frame.depth = 0;
     } else {
-      // Hide depth from self-hosted frames.
-      const increment = frame.script && frame.script.url == "self-hosted"
-        ? 0
-        : 1;
-      frame.depth = increment + getFrameDepth(frame.older);
+      frame.depth = getFrameDepth(frame.older) + 1;
     }
   }
 
@@ -265,10 +261,6 @@ TracerActor.prototype = {
    *        The stack frame that was entered.
    */
   onEnterFrame: function(aFrame) {
-    if (aFrame.script && aFrame.script.url == "self-hosted") {
-      return;
-    }
-
     Task.spawn(function*() {
       // This function might request original (i.e. source-mapped) location,
       // which is asynchronous. We need to ensure that packets are sent out
