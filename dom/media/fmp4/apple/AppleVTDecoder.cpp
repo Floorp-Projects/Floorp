ebe4ea282a96fb93eabf2679cb509b1ff75d9dd2
Jean-Yves Avenard <jyavenard@mozilla.com>
2014-12-24 23:11:35 +1100

    Bug 1111328 - Retrieve video dimensions from H264 SPS when possible. r=rillian, a=sledru

--- dom/media/fmp4/apple/AppleVTDecoder.cpp
+++ dom/media/fmp4/apple/AppleVTDecoder.cpp
@@ -11,6 +11,7 @@
 #include "AppleVTDecoder.h"
 #include "AppleVTLinker.h"
 #include "mp4_demuxer/DecoderData.h"
+#include "mp4_demuxer/H264.h"
 #include "MediaData.h"
 #include "MacIOSurfaceImage.h"
 #include "mozilla/ArrayUtils.h"
@@ -275,8 +276,8 @@ AppleVTDecoder::InitializeSession()
 
   rv = CMVideoFormatDescriptionCreate(kCFAllocatorDefault,
                                       kCMVideoCodecType_H264,
-                                      mConfig.image_width,
-                                      mConfig.image_height,
+                                      mPictureWidth,
+                                      mPictureHeight,
                                       extensions,
                                       &mFormat);
   if (rv != noErr) {
