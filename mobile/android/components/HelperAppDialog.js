18c8d8408d564a7b9969f0dbcdd7fe456e977c83
Paolo Amadini <paolo.mozmail@amadzone.org>
2014-12-24 18:10:04 +0000

    Bug 931776 - Use Downloads.jsm functions to get download directories in Firefox for Android. r=margaret

--- mobile/android/components/HelperAppDialog.js
+++ mobile/android/components/HelperAppDialog.js
@@ -9,9 +9,11 @@ const APK_MIME_TYPE = "application/vnd.android.package-archive";
 const PREF_BD_USEDOWNLOADDIR = "browser.download.useDownloadDir";
 const URI_GENERIC_ICON_DOWNLOAD = "drawable://alert_download";
 
+Cu.import("resource://gre/modules/Downloads.jsm");
 Cu.import("resource://gre/modules/FileUtils.jsm");
 Cu.import("resource://gre/modules/HelperApps.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/Task.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
 // -----------------------------------------------------------------------
@@ -229,16 +231,24 @@ HelperAppLauncherDialog.prototype = {
       Services.prefs.clearUserPref(this._getPrefName(mime));
   },
 
-  promptForSaveToFile: function hald_promptForSaveToFile(aLauncher, aContext, aDefaultFile, aSuggestedFileExt, aForcePrompt) {
-    // Retrieve the user's default download directory
-    let dnldMgr = Cc["@mozilla.org/download-manager;1"].getService(Ci.nsIDownloadManager);
-    let defaultFolder = dnldMgr.userDownloadsDirectory;
+  promptForSaveToFile: function () {
+    throw new Components.Exception("Async version must be used",
+                                   Cr.NS_ERROR_NOT_AVAILABLE);
+  },
 
-    try {
-      file = this.validateLeafName(defaultFolder, aDefaultFile, aSuggestedFileExt);
-    } catch (e) { }
-
-    return file;
+  promptForSaveToFileAsync: function (aLauncher, aContext, aDefaultFile,
+                                      aSuggestedFileExt, aForcePrompt) {
+    Task.spawn(function* () {
+      let file = null;
+      try {
+        let preferredDir = yield Downloads.getPreferredDownloadsDirectory();
+        file = this.validateLeafName(new FileUtils.File(preferredDir),
+                                     aDefaultFile, aSuggestedFileExt);
+      } finally {
+        // The file argument will be null in case any exception occurred.
+        aLauncher.saveDestinationAvailable(file);
+      }
+    }.bind(this)).catch(Cu.reportError);
   },
 
   validateLeafName: function hald_validateLeafName(aLocalFile, aLeafName, aFileExt) {
