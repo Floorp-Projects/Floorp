726cc336222c429dac70441e6166086ad475c822
Brian Birtles <birtles@gmail.com>
2014-12-26 08:49:20 +0900

    Bug 927349 part 29 - Disable failing browser_555547.js on Windows opt - pushed again without backing out parts 22-29 at the same time; r=me
    In https://hg.mozilla.org/integration/mozilla-inbound/rev/3ab2ff9b9f94
    I accidentally pushed part 29 along with a backout for parts 22-29. This patch
    reverts that change.
    
    If this changset causes problems, the following changesets should be backed out:
    
    https://hg.mozilla.org/integration/mozilla-inbound/rev/477f46897b0b
    https://hg.mozilla.org/integration/mozilla-inbound/rev/6c345954bce5
    https://hg.mozilla.org/integration/mozilla-inbound/rev/642e400b22a9
    https://hg.mozilla.org/integration/mozilla-inbound/rev/9bdf7c2279fa
    https://hg.mozilla.org/integration/mozilla-inbound/rev/79cac8c71159
    https://hg.mozilla.org/integration/mozilla-inbound/rev/cfe757e478d5
    https://hg.mozilla.org/integration/mozilla-inbound/rev/eaf834051cbd

--- layout/style/nsTransitionManager.cpp
+++ layout/style/nsTransitionManager.cpp
@@ -540,8 +540,12 @@ nsTransitionManager::ConsiderStartingTransition(
   segment.mTimingFunction.Init(tf);
 
   nsRefPtr<CSSTransitionPlayer> player = new CSSTransitionPlayer(timeline);
-  player->PlayFromStyle();
+  // The order of the following two calls is important since PlayFromStyle
+  // will add the player to the PendingPlayerTracker of its source content's
+  // document. When we come to make source writeable (bug 1049975) we should
+  // remove this dependency.
   player->SetSource(pt);
+  player->PlayFromStyle();
 
   if (!aElementTransitions) {
     aElementTransitions =
