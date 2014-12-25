55c1fca741644a9d2988b29d3aabbe4bff1960fc
Phil Ringnalda <philringnalda@gmail.com>
2014-12-25 08:16:14 -0800

    Back out 342ab0e1b537 (bug 1071769) for b2g reftest-14 and reftest-15 failures in position-dynamic-changes/

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3956,10 +3956,6 @@ pref("layers.tiled-drawtarget.enabled", true);
 pref("layers.offmainthreadcomposition.enabled", true);
 #endif
 
-#ifdef MOZ_WIDGET_GONK
-pref("layers.tiled-drawtarget.enabled", true);
-#endif
-
 // same effect as layers.offmainthreadcomposition.enabled, but specifically for
 // use with tests.
 pref("layers.offmainthreadcomposition.testing.enabled", false);
