b71f548568ff09afdf5719e444715e4e9007fe6a
Xidorn Quan <quanxunzhen@gmail.com>
2014-12-30 11:20:10 +1100

    Bug 1116037 part 12 - Move mLineSize to RubyReflowState. r=dbaron
    --HG--
    extra : source : 17ad55f13af3e96df7649f968c838bc48b323c68

--- layout/generic/RubyReflowState.cpp
+++ layout/generic/RubyReflowState.cpp
@@ -11,12 +11,14 @@
 using namespace mozilla;
 
 RubyReflowState::RubyReflowState(
+  WritingMode aLineWM,
   const nsTArray<nsRubyTextContainerFrame*>& aTextContainers)
   : mCurrentContainerIndex(kBaseContainerIndex)
 {
   uint32_t rtcCount = aTextContainers.Length();
   mTextContainers.SetCapacity(rtcCount);
   for (uint32_t i = 0; i < rtcCount; i++) {
-    mTextContainers.AppendElement(TextContainerInfo(aTextContainers[i]));
+    mTextContainers.AppendElement(
+      TextContainerInfo(aLineWM, aTextContainers[i]));
   }
 }
