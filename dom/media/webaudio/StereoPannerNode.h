a51ec0ffb288117eb4033bbe49f418aef526d248
Ehsan Akhgari <ehsan@mozilla.com>
2015-01-02 01:12:54 -0500

    Bug 1117038 - Mark virtual overridden functions as MOZ_OVERRIDE in Web Audio; r=padenot

--- dom/media/webaudio/StereoPannerNode.h
+++ dom/media/webaudio/StereoPannerNode.h
@@ -48,7 +48,7 @@ public:
   NS_DECL_ISUPPORTS_INHERITED
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(StereoPannerNode, AudioNode)
 
-  virtual const char* NodeType() const
+  virtual const char* NodeType() const MOZ_OVERRIDE
   {
     return "StereoPannerNode";
   }
