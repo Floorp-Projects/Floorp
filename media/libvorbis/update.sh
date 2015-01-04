e4712d359b8f85d31a1219200ef2ccf01d3bc5b3
Timothy B. Terriberry <tterribe@vt.edu>
2015-01-04 17:34:00 +0100

    Bug 1117571 - Check mode index in vorbis_packet_blocksize(), r=kinetik

--- media/libvorbis/update.sh
+++ media/libvorbis/update.sh
@@ -81,3 +81,4 @@ cp $1/lib/books/floor/floor_books.h ./lib/books/floor/
 cp $1/lib/books/uncoupled/res_books_uncoupled.h ./lib/books/uncoupled/
 
 # Add any patches against upstream here.
+patch -p3 < ./bug1117571-r19419.patch
