c337865f47964843bc15708e817d2813e7f1fdf7
Andrea Marchesini <amarchesini@mozilla.com>
2015-01-05 13:42:09 +0100

    Bug 1018320 - RequestSync API - patch 7 - reject promise when an exception is thrown, r=fabrice

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1081,3 +1081,6 @@ pref("dom.mozSettings.SettingsService.verbose.enabled", false);
 // IndexedDB transactions to be opened as readonly or keep everything as
 // readwrite.
 pref("dom.mozSettings.allowForceReadOnly", false);
+
+// RequestSync API is enabled by default on B2G.
+pref("dom.requestSync.enabled", true);
