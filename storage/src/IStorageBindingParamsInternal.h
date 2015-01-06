0eb98faebaa9359546379a667107c8bce1b082cf
Ehsan Akhgari <ehsan@mozilla.com>
2015-01-06 15:11:07 -0500

    Bug 1118414 - Mark virtual overridden functions as MOZ_OVERRIDE in storage; r=mak

--- storage/src/IStorageBindingParamsInternal.h
+++ storage/src/IStorageBindingParamsInternal.h
@@ -43,7 +43,7 @@ NS_DEFINE_STATIC_IID_ACCESSOR(IStorageBindingParamsInternal,
                               ISTORAGEBINDINGPARAMSINTERNAL_IID)
 
 #define NS_DECL_ISTORAGEBINDINGPARAMSINTERNAL \
-  already_AddRefed<mozIStorageError> bind(sqlite3_stmt *aStatement);
+  already_AddRefed<mozIStorageError> bind(sqlite3_stmt *aStatement) MOZ_OVERRIDE;
 
 } // storage
 } // mozilla
