356228a68013e711eecca7b69e1d4b050fbb04d4
Lars T Hansen <lhansen@mozilla.com>
2015-01-07 08:05:25 +0100

    Bug 1117735 - remove PJS test cases.  r=shu

--- js/src/jit-test/tests/parallel/bug976889.js
+++
@@ -1,6 +0,0 @@
-if (!getBuildConfiguration().parallelJS)
-  quit(0);
-
-Array.buildPar(5, function() {
-    return [].t = encodeURI
-})
--- js/src/jit-test/tests/parallel/bug977900.js
+++
@@ -1,5 +0,0 @@
-if (!getBuildConfiguration().parallelJS)
-    quit();
-
-function chooseMax(a, b) { return a>b?a:b;};
-var a = [1,2].scatterPar([0,0], -1, chooseMax);
