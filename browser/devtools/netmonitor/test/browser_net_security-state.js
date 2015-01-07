4cf98d536b1fb89fdf4bbebb1912203f13c8abae
Sami Jaktholm <sjakthol@outlook.com>
2015-01-07 09:38:00 +0100

    Bug 932179 - Part 3: Fix intermittent timeout in browser_net_security-state.js. r=vporof

--- browser/devtools/netmonitor/test/browser_net_security-state.js
+++ browser/devtools/netmonitor/test/browser_net_security-state.js
@@ -62,12 +62,12 @@ add_task(function* () {
     // occasionally hangs waiting for event timings that don't seem to appear...
     done = waitForNetworkEvents(monitor, 1);
     info("Requesting a resource over HTTP.");
-    debuggee.performRequests(1, "http://test1.example.com");
+    debuggee.performRequests(1, "http://test1.example.com" + CORS_SJS_PATH);
     yield done;
 
     done = waitForNetworkEvents(monitor, 1);
     info("Requesting a resource over HTTPS.");
-    debuggee.performRequests(1, "https://example.com");
+    debuggee.performRequests(1, "https://example.com" + CORS_SJS_PATH);
     yield done;
 
     is(RequestsMenu.itemCount, 3, "Three events logged.");
