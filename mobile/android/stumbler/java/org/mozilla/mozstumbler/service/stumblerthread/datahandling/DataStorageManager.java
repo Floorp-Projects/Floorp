bdda19d53b0734a83248c984ab9807782c9362da
Garvan Keeley <gkeeley@mozilla.com>
2015-01-06 19:31:00 -0500

    Bug 1116810 - Part 3: Stumbler java code for telemetry. r=vng

--- mobile/android/stumbler/java/org/mozilla/mozstumbler/service/stumblerthread/datahandling/DataStorageManager.java
+++ mobile/android/stumbler/java/org/mozilla/mozstumbler/service/stumblerthread/datahandling/DataStorageManager.java
@@ -69,6 +69,7 @@ public class DataStorageManager {
     private ReportBatchIterator mReportBatchIterator;
     private final ReportFileList mFileList;
     private Timer mFlushMemoryBuffersToDiskTimer;
+    private final PersistedStats mPersistedOnDiskUploadStats;
 
     static final String SEP_REPORT_COUNT = "-r";
     static final String SEP_WIFI_COUNT = "-w";
@@ -242,6 +243,7 @@ public class DataStorageManager {
         }
         mFileList = new ReportFileList();
         mFileList.update(mReportsDir);
+        mPersistedOnDiskUploadStats = new PersistedStats(baseDir);
     }
 
     public synchronized int getMaxWeeksStored() {
@@ -448,51 +450,6 @@ public class DataStorageManager {
         }
     }
 
-    public synchronized Properties readSyncStats() throws IOException {
-        if (!mStatsFile.exists()) {
-            return new Properties();
-        }
-
-        final FileInputStream input = new FileInputStream(mStatsFile);
-        try {
-            final Properties props = new Properties();
-            props.load(input);
-            return props;
-        } finally {
-            input.close();
-        }
-    }
-
-    public synchronized void incrementSyncStats(long bytesSent, long reports, long cells, long wifis) throws IOException {
-        if (reports + cells + wifis < 1) {
-            return;
-        }
-
-        final Properties properties = readSyncStats();
-        final long time = System.currentTimeMillis();
-        writeSyncStats(time,
-            Long.parseLong(properties.getProperty(DataStorageContract.Stats.KEY_BYTES_SENT, "0")) + bytesSent,
-            Long.parseLong(properties.getProperty(DataStorageContract.Stats.KEY_OBSERVATIONS_SENT, "0")) + reports,
-            Long.parseLong(properties.getProperty(DataStorageContract.Stats.KEY_CELLS_SENT, "0")) + cells,
-            Long.parseLong(properties.getProperty(DataStorageContract.Stats.KEY_WIFIS_SENT, "0")) + wifis);
-    }
-
-    public void writeSyncStats(long time, long bytesSent, long totalObs, long totalCells, long totalWifis) throws IOException {
-        final FileOutputStream out = new FileOutputStream(mStatsFile);
-        try {
-            final Properties props = new Properties();
-            props.setProperty(DataStorageContract.Stats.KEY_LAST_UPLOAD_TIME, String.valueOf(time));
-            props.setProperty(DataStorageContract.Stats.KEY_BYTES_SENT, String.valueOf(bytesSent));
-            props.setProperty(DataStorageContract.Stats.KEY_OBSERVATIONS_SENT, String.valueOf(totalObs));
-            props.setProperty(DataStorageContract.Stats.KEY_CELLS_SENT, String.valueOf(totalCells));
-            props.setProperty(DataStorageContract.Stats.KEY_WIFIS_SENT, String.valueOf(totalWifis));
-            props.setProperty(DataStorageContract.Stats.KEY_VERSION, String.valueOf(DataStorageContract.Stats.VERSION_CODE));
-            props.store(out, null);
-        } finally {
-            out.close();
-        }
-    }
-
     public synchronized void deleteAll() {
         if (mFileList.mFiles == null) {
             return;
@@ -508,5 +465,9 @@ public class DataStorageManager {
         if (mTracker != null) {
             mTracker.notifyStorageStateEmpty(isEmpty);
         }
+    }
+
+    public synchronized void incrementSyncStats(long bytesSent, long reports, long cells, long wifis) throws IOException {
+        mPersistedOnDiskUploadStats.incrementSyncStats(bytesSent, reports, cells, wifis);
     }
 }
