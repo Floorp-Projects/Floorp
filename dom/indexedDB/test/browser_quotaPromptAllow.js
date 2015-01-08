758dc83862446495a30680b4b34c0e8747b8a109
Sai Prathik <prathikcoding167@gmail.com>
2015-01-08 23:56:00 +0000

    Bug 1118768 - Redundant PopupNotifications.transitionsEnabled code removed, r=gijs

--- dom/indexedDB/test/browser_quotaPromptAllow.js
+++ dom/indexedDB/test/browser_quotaPromptAllow.js
@@ -12,7 +12,6 @@ function test()
 {
   waitForExplicitFinish();
   requestLongerTimeout(10);
-  PopupNotifications.transitionsEnabled = false;
   setPermission(testPageURL, "indexedDB");
   removePermission(testPageURL, "indexedDB-unlimited");
   Services.prefs.setIntPref("dom.indexedDB.warningQuota", 2);
@@ -110,7 +109,6 @@ function test2()
             unregisterAllPopupEventHandlers();
             removePermission(testPageURL, "indexedDB");
             Services.prefs.clearUserPref("dom.indexedDB.warningQuota");
-            PopupNotifications.transitionsEnabled = true;
             executeSoon(finish);
           });
           executeSoon(function() { dispatchEvent("indexedDB-done"); });
