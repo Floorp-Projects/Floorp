8cdc9a4fc74ff77b895aeeb5382278a88436d544
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-01-08 12:17:22 +0100

    Backed out changeset 321dfaa82c02 (bug 932179)

--- browser/devtools/netmonitor/test/browser_net_security-state.js
+++ browser/devtools/netmonitor/test/browser_net_security-state.js
@@ -62,12 +62,12 @@ add_task(function* () {
     // occasionally hangs waiting for event timings that don't seem to appear...
     done = waitForNetworkEvents(monitor, 1);
     info("Requesting a resource over HTTP.");
-    debuggee.performRequests(1, "http://test1.example.com" + CORS_SJS_PATH);
+    debuggee.performRequests(1, "http://test1.example.com");
     yield done;
 
     done = waitForNetworkEvents(monitor, 1);
     info("Requesting a resource over HTTPS.");
-    debuggee.performRequests(1, "https://example.com" + CORS_SJS_PATH);
+    debuggee.performRequests(1, "https://example.com");
     yield done;
 
     is(RequestsMenu.itemCount, 3, "Three events logged.");
