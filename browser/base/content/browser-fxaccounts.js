e9f2d67e3cccc891252536d6c4aa96464f99963b
Drew Willcoxon <adw@mozilla.com>
2015-01-08 09:28:55 -0800

    Bug 1112440 - Make the infobar update correctly to reflect "needs verified user" on about:accounts during Sync migration. r=markh

--- browser/base/content/browser-fxaccounts.js
+++ browser/base/content/browser-fxaccounts.js
@@ -266,11 +266,6 @@ let gFxAccounts = {
       Weave.Notifications.removeAll(this.SYNC_MIGRATION_NOTIFICATION_TITLE);
       return;
     }
-    if (gBrowser.currentURI.spec.split("?")[0] == "about:accounts") {
-      // If the current tab is about:accounts, assume the user just completed a
-      // migration step and don't bother them with a redundant notification.
-      return;
-    }
     let note = null;
     switch (this._migrationInfo.state) {
       case this.fxaMigrator.STATE_USER_FXA: {
