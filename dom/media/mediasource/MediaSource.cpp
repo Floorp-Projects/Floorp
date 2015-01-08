f92b4394296e3bcff12b923f18cc9be39d9c1179
Ralph Giles <giles@mozilla.com>
2015-01-08 12:34:00 -0800

    Bug 1119463 - Enable MSE for youtube-nocookie.com. r=kinetik a=mse
    Follow-up to bug 1119463. This is used by some sites for embedding.

--- dom/media/mediasource/MediaSource.cpp
+++ dom/media/mediasource/MediaSource.cpp
@@ -330,8 +330,9 @@ MediaSource::Enabled(JSContext* cx, JSObject* aGlobal)
     return true;
   }
 
-  // We want to restrict to YouTube only.  We define that as the
-  // origin being https://*.youtube.com.
+  // We want to restrict to YouTube only.
+  // We define that as the origin being https://*.youtube.com.
+  // We also support https://*.youtube-nocookie.com.
   nsIPrincipal* principal = nsContentUtils::ObjectPrincipal(global);
   nsCOMPtr<nsIURI> uri;
   if (NS_FAILED(principal->GetURI(getter_AddRefs(uri))) || !uri) {
@@ -348,9 +349,12 @@ MediaSource::Enabled(JSContext* cx, JSObject* aGlobal)
   NS_ENSURE_TRUE(tldServ, false);
 
   nsAutoCString eTLDplusOne;
-  return
-    NS_SUCCEEDED(tldServ->GetBaseDomain(uri, 0, eTLDplusOne)) &&
-    eTLDplusOne.EqualsLiteral("youtube.com");
+   if (NS_FAILED(tldServ->GetBaseDomain(uri, 0, eTLDplusOne))) {
+     return false;
+   }
+
+   return eTLDplusOne.EqualsLiteral("youtube.com") ||
+          eTLDplusOne.EqualsLiteral("youtube-nocookie.com");
 }
 
 bool
