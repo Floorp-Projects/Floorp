af28858a52d466cb41e8f7b1a3bdd83e40141cc0
Brian Smith <brian@briansmith.org>
2015-01-08 15:38:26 -0800

    Bug 1119072, Part 15: Replace uses of hash_map with unordered_map (toolkit), r=ted

--- toolkit/crashreporter/google-breakpad/src/common/windows/pdb_source_line_writer.h
+++ toolkit/crashreporter/google-breakpad/src/common/windows/pdb_source_line_writer.h
@@ -35,7 +35,7 @@
 
 #include <atlcomcli.h>
 
-#include <hash_map>
+#include <unordered_map>
 #include <string>
 
 struct IDiaEnumLineNumbers;
@@ -45,7 +45,7 @@ struct IDiaSymbol;
 namespace google_breakpad {
 
 using std::wstring;
-using stdext::hash_map;
+using std::unordered_map;
 
 // A structure that carries information that identifies a pdb file.
 struct PDBModuleInfo {
@@ -176,7 +176,7 @@ class PDBSourceLineWriter {
 
   // Store this ID in the cache as a duplicate for this filename.
   void StoreDuplicateFileID(const wstring &file, DWORD id) {
-    hash_map<wstring, DWORD>::iterator iter = unique_files_.find(file);
+    unordered_map<wstring, DWORD>::iterator iter = unique_files_.find(file);
     if (iter != unique_files_.end()) {
       // map this id to the previously seen one
       file_ids_[id] = iter->second;
@@ -188,7 +188,7 @@ class PDBSourceLineWriter {
   // but different unique IDs. The cache attempts to coalesce these into
   // one ID per unique filename.
   DWORD GetRealFileID(DWORD id) {
-    hash_map<DWORD, DWORD>::iterator iter = file_ids_.find(id);
+    unordered_map<DWORD, DWORD>::iterator iter = file_ids_.find(id);
     if (iter == file_ids_.end())
       return id;
     return iter->second;
@@ -224,9 +224,9 @@ class PDBSourceLineWriter {
   // There may be many duplicate filenames with different IDs.
   // This maps from the DIA "unique ID" to a single ID per unique
   // filename.
-  hash_map<DWORD, DWORD> file_ids_;
+  unordered_map<DWORD, DWORD> file_ids_;
   // This maps unique filenames to file IDs.
-  hash_map<wstring, DWORD> unique_files_;
+  unordered_map<wstring, DWORD> unique_files_;
 
   // Disallow copy ctor and operator=
   PDBSourceLineWriter(const PDBSourceLineWriter&);
