c17fa68a995beabfb71636d3a83542baf433f933
Trevor Saunders <trev.saunders@gmail.com>
2015-01-08 20:21:37 -0500

    bug 1077549 -  only support gcc 4.6+ r=glandium

--- b2g/confvars.sh
+++ b2g/confvars.sh
@@ -59,11 +59,7 @@ MOZ_B2G=1
 if test "$OS_TARGET" = "Android"; then
 MOZ_NUWA_PROCESS=1
 MOZ_B2G_LOADER=1
-# Warnings-as-errors cannot be enabled on gcc <= 4.4 due to bug 915555.
-if test "$GCC_MAJOR_VERSION" -gt 4 -o \
-   "$GCC_MAJOR_VERSION" -eq 4 -a "$GCC_MINOR_VERSION" -gt 4; then
 MOZ_ENABLE_WARNINGS_AS_ERRORS=1
-fi
 fi
 
 MOZ_JSDOWNLOADS=1
