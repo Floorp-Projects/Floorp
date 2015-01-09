0e1a4d2088856aa1586d3700110ebbc2c136e270
David Burns <dburns@mozilla.com>
2015-01-09 21:42:06 +0000

    Bug 1107336: selection tests imports for marionette; r=jgriffin
    --HG--
    extra : rebase_source : b8d709390daaf3027e0ba6a933a915110cf299ca
    extra : source : 5a2e085824ae4e61687730c89575c259fdaa9468

--- testing/marionette/client/marionette/__init__.py
+++ testing/marionette/client/marionette/__init__.py
@@ -2,33 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette_driver.gestures import smooth_scroll, pinch
-from marionette_driver.by import By
-from marionette_driver.marionette import Marionette, HTMLElement, Actions, MultiActions
 from marionette_test import MarionetteTestCase, MarionetteJSTestCase, CommonTestCase, expectedFailure, skip, SkipTest
-from marionette_driver.errors import (
-    ElementNotVisibleException,
-    ElementNotAccessibleException,
-    ErrorCodes,
-    FrameSendFailureError,
-    FrameSendNotInitializedError,
-    InvalidCookieDomainException,
-    InvalidElementStateException,
-    InvalidResponseException,
-    InvalidSelectorException,
-    JavascriptException,
-    MarionetteException,
-    MoveTargetOutOfBoundsException,
-    NoAlertPresentException,
-    NoSuchElementException,
-    NoSuchFrameException,
-    NoSuchWindowException,
-    ScriptTimeoutException,
-    StaleElementException,
-    TimeoutException,
-    UnableToSetCookieException,
-    XPathLookupException,
-)
 from runner import (
         B2GTestCaseMixin,
         B2GTestResultMixin,
@@ -50,6 +24,3 @@ from runner import (
         TestResult,
         TestResultCollection
 )
-from marionette_driver.wait import Wait
-from marionette_driver.date_time_value import DateTimeValue
-import marionette_driver.decorators
