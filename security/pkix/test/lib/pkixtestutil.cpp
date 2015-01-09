52f5ce5e189f0a0391e36ad5949ded3f517a771a
Jacek Caban <jacek@codeweavers.com>
2015-01-09 11:41:15 +0100

    Bug 1119179 - Avoid gmtime_r duplication if it's provided by mingw. r=bsmith

--- security/pkix/test/lib/pkixtestutil.cpp
+++ security/pkix/test/lib/pkixtestutil.cpp
@@ -224,7 +224,7 @@ Integer(long value)
 enum TimeEncoding { UTCTime = 0, GeneralizedTime = 1 };
 
 // Windows doesn't provide gmtime_r, but it provides something very similar.
-#ifdef WIN32
+#if defined(WIN32) && !defined(_POSIX_THREAD_SAFE_FUNCTIONS)
 static tm*
 gmtime_r(const time_t* t, /*out*/ tm* exploded)
 {
