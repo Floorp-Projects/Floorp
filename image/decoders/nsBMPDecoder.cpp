c601208b50f08a323919bc643d99c8c83050e0ab
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-01-09 16:24:14 +0100

    Backed out changeset f61a4592da85 (bug 1117607) for Reftest Failures/Crashes on a CLOSED TREE

--- image/decoders/nsBMPDecoder.cpp
+++ image/decoders/nsBMPDecoder.cpp
@@ -439,15 +439,10 @@ nsBMPDecoder::WriteInternal(const char* aBuffer, uint32_t aCount)
           return;
         }
       }
-
-      MOZ_ASSERT(!mImageData, "Already have a buffer allocated?");
-      nsresult rv = AllocateFrame(0, nsIntRect(nsIntPoint(), GetSize()),
-                                  gfx::SurfaceFormat::B8G8R8A8);
-      if (NS_FAILED(rv)) {
+      if (!mImageData) {
+        PostDecoderError(NS_ERROR_FAILURE);
         return;
       }
-
-      MOZ_ASSERT(mImageData, "Should have a buffer now");
 
       // Prepare for transparency
       if ((mBIH.compression == BI_RLE8) || (mBIH.compression == BI_RLE4)) {
