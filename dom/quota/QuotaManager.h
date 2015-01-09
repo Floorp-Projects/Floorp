0e1aa3fc9b46515bea01cc2b51e1a5d517a69d34
Jan Varga <jan.varga@gmail.com>
2015-01-09 16:56:23 +0100

    Bug 1119462 - Allow unlimited quota for explicit persistent storage; r=bent

--- dom/quota/QuotaManager.h
+++ dom/quota/QuotaManager.h
@@ -316,6 +316,9 @@ public:
                    bool* aHasUnlimStoragePerm);
 
   static bool
+  IsOriginWhitelistedForPersistentStorage(const nsACString& aOrigin);
+
+  static bool
   IsTreatedAsPersistent(PersistenceType aPersistenceType,
                         bool aIsApp);
 
