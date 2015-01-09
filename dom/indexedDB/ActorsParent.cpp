b5f051314d8e6f6064b2e847194cd7e08cb31854
Nigel Babu <nigelbabu@gmail.com>
2015-01-09 10:06:31 +0530

    Backed out changeset 15830cc2b55b (bug 1119462) for B2G M5 bustage CLOSED TREE

--- dom/indexedDB/ActorsParent.cpp
+++ dom/indexedDB/ActorsParent.cpp
@@ -10852,14 +10852,6 @@ FactoryOp::CheckPermission(ContentParent* aContentParent,
     return rv;
   }
 
-#if defined(MOZ_WIDGET_ANDROID) || defined(MOZ_WIDGET_GONK)
-  if (persistenceType == PERSISTENCE_TYPE_PERSISTENT &&
-      !QuotaManager::IsOriginWhitelistedForPersistentStorage(origin) &&
-      !isApp) {
-    return NS_ERROR_DOM_INDEXEDDB_NOT_ALLOWED_ERR;
-  }
-#endif
-
   PermissionRequestBase::PermissionValue permission;
 
   if (QuotaManager::IsFirstPromptRequired(persistenceType, origin, isApp)) {
