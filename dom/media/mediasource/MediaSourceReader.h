fe564ed1868cfcfe11a1a74ee5939f66337256e8
Matt Woodrow <mwoodrow@mozilla.com>
2015-01-12 09:47:56 +1300

    Bug 1118528 - Use fuzzy comparisons in MediaSourceReader::HaveData. r=cajbir

--- dom/media/mediasource/MediaSourceReader.h
+++ dom/media/mediasource/MediaSourceReader.h
@@ -130,12 +130,16 @@ public:
 #endif
 
 private:
-  bool SwitchAudioReader(int64_t aTarget);
-  bool SwitchVideoReader(int64_t aTarget);
+  // Switch the current audio/video reader to the reader that
+  // contains aTarget (or up to aError after target). Both
+  // aTarget and aError are in microseconds.
+  bool SwitchAudioReader(int64_t aTarget, int64_t aError = 0);
+  bool SwitchVideoReader(int64_t aTarget, int64_t aError = 0);
 
   // Return a reader from the set available in aTrackDecoders that has data
   // available in the range requested by aTarget.
   already_AddRefed<MediaDecoderReader> SelectReader(int64_t aTarget,
+                                                    int64_t aError,
                                                     const nsTArray<nsRefPtr<SourceBufferDecoder>>& aTrackDecoders);
   bool HaveData(int64_t aTarget, MediaData::Type aType);
 
