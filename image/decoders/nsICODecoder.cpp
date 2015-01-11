9a5e93b59aa4da7083e002b7c6f26063c5265a05
Seth Fowler <seth@mozilla.com>
2015-01-10 20:47:38 -0800

    Bug 1118092 - Manage invalidation policy directly in the image decoder. r=tn

--- image/decoders/nsICODecoder.cpp
+++ image/decoders/nsICODecoder.cpp
@@ -345,6 +345,7 @@ nsICODecoder::WriteInternal(const char* aBuffer, uint32_t aCount)
     if (mIsPNG) {
       mContainedDecoder = new nsPNGDecoder(mImage);
       mContainedDecoder->SetSizeDecode(IsSizeDecode());
+      mContainedDecoder->SetSendPartialInvalidations(mSendPartialInvalidations);
       mContainedDecoder->Init();
       if (!WriteToContainedDecoder(mSignature, PNGSIGNATURESIZE)) {
         return;
@@ -421,6 +422,7 @@ nsICODecoder::WriteInternal(const char* aBuffer, uint32_t aCount)
     mContainedDecoder = bmpDecoder;
     bmpDecoder->SetUseAlphaData(true);
     mContainedDecoder->SetSizeDecode(IsSizeDecode());
+    mContainedDecoder->SetSendPartialInvalidations(mSendPartialInvalidations);
     mContainedDecoder->Init();
 
     // The ICO format when containing a BMP does not include the 14 byte
