0bb2cd34b4fba576b4cafb2f80d442e89f085f4a
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-01-12 15:35:21 +0100

    Backed out changeset 0257e0be8c42 (bug 1119774)

--- image/src/ImageWrapper.cpp
+++ image/src/ImageWrapper.cpp
@@ -224,12 +224,6 @@ ImageWrapper::StartDecoding()
   return mInnerImage->StartDecoding();
 }
 
-NS_IMETHODIMP
-ImageWrapper::RequestDecodeForSize(const nsIntSize& aSize, uint32_t aFlags)
-{
-  return mInnerImage->RequestDecodeForSize(aSize, aFlags);
-}
-
 bool
 ImageWrapper::IsDecoded()
 {
