6bcf4ca2c18d9ba31c782c1015981e63ea90be97
Bobby Holley <bobbyholley@gmail.com>
2015-01-12 15:50:09 -0800

    Bug 1120629 - Cache data directly on MP4Stream rather than relying on the media cache. r=roc, a=mse

--- dom/media/fmp4/MP4Stream.cpp
+++ dom/media/fmp4/MP4Stream.cpp
@@ -11,6 +11,7 @@ namespace mozilla {
 
 MP4Stream::MP4Stream(MediaResource* aResource)
   : mResource(aResource)
+  , mPinCount(0)
 {
   MOZ_COUNT_CTOR(MP4Stream);
   MOZ_ASSERT(aResource);
@@ -19,25 +20,33 @@ MP4Stream::MP4Stream(MediaResource* aResource)
 MP4Stream::~MP4Stream()
 {
   MOZ_COUNT_DTOR(MP4Stream);
+  MOZ_ASSERT(mPinCount == 0);
 }
 
 bool
-MP4Stream::BlockingReadAt(int64_t aOffset, void* aBuffer, size_t aCount,
-                          size_t* aBytesRead)
+MP4Stream::BlockingReadIntoCache(int64_t aOffset, size_t aCount, Monitor* aToUnlock)
 {
+  MOZ_ASSERT(mPinCount > 0);
+  CacheBlock block(aOffset, aCount);
+
   uint32_t sum = 0;
   uint32_t bytesRead = 0;
   do {
     uint64_t offset = aOffset + sum;
-    char* buffer = reinterpret_cast<char*>(aBuffer) + sum;
+    char* buffer = reinterpret_cast<char*>(block.mBuffer.get()) + sum;
     uint32_t toRead = aCount - sum;
+    MonitorAutoUnlock unlock(*aToUnlock);
     nsresult rv = mResource->ReadAt(offset, buffer, toRead, &bytesRead);
     if (NS_FAILED(rv)) {
       return false;
     }
     sum += bytesRead;
   } while (sum < aCount && bytesRead > 0);
-  *aBytesRead = sum;
+
+  MOZ_ASSERT(block.mCount >= sum);
+  block.mCount = sum;
+
+  mCache.AppendElement(block);
   return true;
 }
 
@@ -64,6 +73,14 @@ bool
 MP4Stream::CachedReadAt(int64_t aOffset, void* aBuffer, size_t aCount,
                         size_t* aBytesRead)
 {
+  // First, check our local cache.
+  for (size_t i = 0; i < mCache.Length(); ++i) {
+    if (mCache[i].mOffset == aOffset && mCache[i].mCount >= aCount) {
+      memcpy(aBuffer, mCache[i].mBuffer, aCount);
+      *aBytesRead = aCount;
+      return true;
+    }
+  }
 
   nsresult rv = mResource->ReadFromCache(reinterpret_cast<char*>(aBuffer),
                                          aOffset, aCount);
