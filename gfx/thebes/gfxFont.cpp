eb619e22b35cd66fe4c5a11d6ebba31281089af7
Jonathan Kew <jkew@mozilla.com>
2015-01-12 14:24:05 +0000

    Bug 1120102 - Use absolute value of vhea descender field, because fonts differ in the sign of the coordinate system they assume. r=smontagu

--- gfx/thebes/gfxFont.cpp
+++ gfx/thebes/gfxFont.cpp
@@ -3454,7 +3454,7 @@ gfxFont::CreateVerticalMetrics()
             // Redistribute space between ascent/descent because we want a
             // centered vertical baseline by default.
             gfxFloat halfExtent = 0.5 * gfxFloat(mFUnitsConvFactor) *
-                (int16_t(vhea->ascender) - int16_t(vhea->descender));
+                (int16_t(vhea->ascender) + std::abs(int16_t(vhea->descender)));
             metrics->maxAscent = halfExtent;
             metrics->maxDescent = halfExtent;
             SET_SIGNED(externalLeading, vhea->lineGap);
