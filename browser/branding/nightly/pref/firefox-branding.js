a9e9ca8c6f45ba4cd7ab39c22e8b81e19abf3bbd
Abdelrhman Ahmed <a.ahmed1026@gmail.com>
2015-01-13 03:27:00 +0100

    Bug 1119560 - remove unused browser.search.param.yahoo-fr* prefs. r=gavin

--- browser/branding/nightly/pref/firefox-branding.js
+++ browser/branding/nightly/pref/firefox-branding.js
@@ -24,13 +24,6 @@ pref("app.update.url.details", "https://nightly.mozilla.org");
 // app.update.checkInstallTime is true.
 pref("app.update.checkInstallTime.days", 2);
 
-// code usage depends on contracts, please contact the Firefox module owner if you have questions
-pref("browser.search.param.yahoo-fr", "moz35");
-pref("browser.search.param.yahoo-fr-ja", "mozff");
-#ifdef MOZ_METRO
-pref("browser.search.param.yahoo-fr-metro", "");
-#endif
-
 // Number of usages of the web console or scratchpad.
 // If this is less than 5, then pasting code into the web console or scratchpad is disabled
 pref("devtools.selfxss.count", 5);
