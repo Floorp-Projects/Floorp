1395e14c3b7d93330329ed43bf34c9d80fd25b0f
Dimi Lee <dlee@mozilla.com>
2015-01-13 14:50:24 +0800

    Bug 916428 - [NFC] APIs for ISO 14443-4 tags (IsoDep Support). r=smaug, r=yoshi

---
+++ dom/nfc/MozIsoDepTech.h
@@ -0,0 +1,54 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=2 et sw=2 tw=80:
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_nfc_MozIsoDepTech_h__
+#define mozilla_dom_nfc_MozIsoDepTech_h__
+
+#include "mozilla/dom/MozNFCTagBinding.h"
+#include "mozilla/dom/MozIsoDepTechBinding.h"
+#include "nsCycleCollectionParticipant.h"
+#include "nsWrapperCache.h"
+#include "nsISupportsImpl.h"
+#include "nsPIDOMWindow.h"
+
+namespace mozilla {
+namespace dom {
+
+class Promise;
+
+class MozIsoDepTech : public nsISupports,
+                      public nsWrapperCache
+{
+public:
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(MozIsoDepTech)
+
+  already_AddRefed<Promise> Transceive(const Uint8Array& aCommand,
+                                       ErrorResult& aRv);
+
+  nsPIDOMWindow* GetParentObject() const { return mWindow; }
+
+  virtual JSObject* WrapObject(JSContext* aCx) MOZ_OVERRIDE;
+
+  static already_AddRefed<MozIsoDepTech>
+  Constructor(const GlobalObject& aGlobal, MozNFCTag& aNFCTag,
+              ErrorResult& aRv);
+
+private:
+  MozIsoDepTech(nsPIDOMWindow* aWindow, MozNFCTag& aNFCTag);
+  virtual ~MozIsoDepTech();
+
+  nsRefPtr<nsPIDOMWindow> mWindow;
+  nsRefPtr<MozNFCTag> mTag;
+
+  static const NFCTechType mTechnology = NFCTechType::ISO_DEP;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif  // mozilla_dom_nfc_MozIsoDepTech_h__
