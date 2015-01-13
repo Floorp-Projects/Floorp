510cb2cbda6587f5a92966a04a5e146574b74cbb
James Graham <james@hoppipolla.co.uk>
2015-01-13 09:42:41 +0000

    Bug 1102907 - Update web-platform-tests to revision 6e35a4cd58f8fa507fc90b9d7b7ff28153759cac, a=testonly

--- testing/web-platform/tests/content-security-policy/support/checkReport.sub.js
+++ testing/web-platform/tests/content-security-policy/support/checkReport.sub.js
@@ -31,7 +31,7 @@
     }
   }
 
-  var reportLocation = location.protocol + "//" + location.host + "/content-security-policy/support/report.py?op=take&timeout=3&reportID=" + reportID;
+  var reportLocation = location.protocol + "//" + location.host + "/content-security-policy/support/report.py?op=take&timeout=5&reportID=" + reportID;
 
   var reportTest = async_test("Violation report status OK.");
   reportTest.step(function () {
