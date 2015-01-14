5973bae469f014b99e6d5f3aee5b407d5d002707
Terrence Cole <terrence@mozilla.com>
2015-01-14 09:40:03 -0800

    Bug 1119537 - Part 2: Decommit foreground swept arenas concurrent with background sweeping; r=jonco

--- js/src/jsgc.h
+++ js/src/jsgc.h
@@ -1042,6 +1042,7 @@ class GCParallelTask
     }
 
     // Check if a task is actively running.
+    bool isRunningOutsideLock() const;
     bool isRunning() const;
 
     // This should be friended to HelperThread, but cannot be because it
