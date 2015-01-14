6e294f26b9e8f70011446f26b8ab9292f3463ea2
Mike Hommey <mh+mozilla@glandium.org>
2015-01-14 12:49:24 +0900

    Bug 1121314 - Avoid needing the arena in chunk_alloc_default to avoid possible infinite loops involving a0malloc. r=njn
    The infinite loop happens if chunk_alloc_arena needs to be called when a0malloc
    is called. It in turn calls chunk_alloc_default, which uses tsd, which calls
    a0malloc if it's the first time the tsd is being gotten from the current thread.
    tsd only uses a0malloc on platforms where there is no native thread local storage
    support, which, for Mozilla, essentially means anything that is not Linux.
    
    But the tsd is only neededto get the dss precedence setting of the given arena.
    That setting has no effect when dss is disabled, which it is on Windows and Mac.
    
    Moreover, the default setting for dss precedence is "secondary", which means
    jemalloc only tries dss after it failed to get memory with mmap/VirtualAlloc.
    Considering the cases where mmap/VirtualAlloc would fail essentially means
    there is shortage of address space, sbrk() is not going to have much more
    success, so we might as well disable dss support on all platforms, avoiding
    the infinite loop problem on Android and B2G as well.

--- memory/jemalloc/update.sh
+++ memory/jemalloc/update.sh
@@ -18,6 +18,7 @@ patch -p1 < ../0001-Dont-overwrite-VERSION-on-a-git-repository.patch
 patch -p1 < ../0002-Move-variable-declaration-to-the-top-its-block-for-M.patch
 patch -p1 < ../0003-Add-a-isblank-definition-for-MSVC-2013.patch
 patch -p1 < ../0004-Implement-stats.bookkeeping.patch
+patch -p1 < ../0005-Bug-1121314-Avoid-needing-the-arena-in-chunk_alloc_d.patch
 
 cd ..
 hg addremove -q src
