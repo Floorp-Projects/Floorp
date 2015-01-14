fdb555e3a690d8b225cc545edab0a24c4e83e67b
Anthony Jones <ajones@mozilla.com>
2015-01-14 16:57:14 +1300

    Bug 1116056 - Change MOZ_ASSERT() to NS_WARNING() in Box::Read(). r=jya, a=sledru

--- media/libstagefright/binding/Box.cpp
+++ media/libstagefright/binding/Box.cpp
@@ -98,7 +98,7 @@ Box::Read(nsTArray<uint8_t>* aDest)
                                        aDest->Length(), &bytes) ||
       bytes != aDest->Length()) {
     // Byte ranges are being reported incorrectly
-    MOZ_ASSERT(false);
+    NS_WARNING("Read failed in mp4_demuxer::Box::Read()");
     aDest->Clear();
   }
 }
