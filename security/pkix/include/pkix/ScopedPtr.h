849b6f934c4e0cf669f14cff2121f89930242bdd
Brian Smith <brian@briansmith.org>
2015-01-13 16:54:10 -0800

    Bug 1115906, Part 2: Annotate classes and member functions with override and final, r=keeler

--- security/pkix/include/pkix/ScopedPtr.h
+++ security/pkix/include/pkix/ScopedPtr.h
@@ -32,7 +32,7 @@ namespace mozilla { namespace pkix {
 // Similar to boost::scoped_ptr and std::unique_ptr. Does not support copying
 // or assignment.
 template <typename T, void (&Destroyer)(T*)>
-class ScopedPtr
+class ScopedPtr final
 {
 public:
   explicit ScopedPtr(T* value = nullptr) : mValue(value) { }
