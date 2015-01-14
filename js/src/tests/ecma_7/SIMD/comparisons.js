6c0c2ab2d42b22788e2a3b3cc1364ff0b4c3cce2
ProgramFOX <programfox@hotmail.be>
2015-01-14 19:21:30 +0100

    Bug 1031203 - Add test cases for float64x2. r=bbouvier

--- js/src/tests/ecma_7/SIMD/comparisons.js
+++ js/src/tests/ecma_7/SIMD/comparisons.js
@@ -6,6 +6,7 @@
  */
 
 var float32x4 = SIMD.float32x4;
+var float64x2 = SIMD.float64x2;
 var int32x4 = SIMD.int32x4;
 
 var fround = Math.fround;
@@ -15,41 +16,60 @@ function boolToSimdLogical(b) {
 }
 
 function testEqualFloat32x4(v, w) {
-    testBinaryFunc(v, w, float32x4.equal, (x, y) => boolToSimdLogical(fround(x) == fround(y)));
+    testBinaryCompare(v, w, float32x4.equal, (x, y) => boolToSimdLogical(fround(x) == fround(y)));
 }
 function testNotEqualFloat32x4(v, w) {
-    testBinaryFunc(v, w, float32x4.notEqual, (x, y) => boolToSimdLogical(fround(x) != fround(y)));
+    testBinaryCompare(v, w, float32x4.notEqual, (x, y) => boolToSimdLogical(fround(x) != fround(y)));
 }
 function testLessThanFloat32x4(v, w) {
-    testBinaryFunc(v, w, float32x4.lessThan, (x, y) => boolToSimdLogical(fround(x) < fround(y)));
+    testBinaryCompare(v, w, float32x4.lessThan, (x, y) => boolToSimdLogical(fround(x) < fround(y)));
 }
 function testLessThanOrEqualFloat32x4(v, w) {
-    testBinaryFunc(v, w, float32x4.lessThanOrEqual, (x, y) => boolToSimdLogical(fround(x) <= fround(y)));
+    testBinaryCompare(v, w, float32x4.lessThanOrEqual, (x, y) => boolToSimdLogical(fround(x) <= fround(y)));
 }
 function testGreaterThanFloat32x4(v, w) {
-    testBinaryFunc(v, w, float32x4.greaterThan, (x, y) => boolToSimdLogical(fround(x) > fround(y)));
+    testBinaryCompare(v, w, float32x4.greaterThan, (x, y) => boolToSimdLogical(fround(x) > fround(y)));
 }
 function testGreaterThanOrEqualFloat32x4(v, w) {
-    testBinaryFunc(v, w, float32x4.greaterThanOrEqual, (x, y) => boolToSimdLogical(fround(x) >= fround(y)));
+    testBinaryCompare(v, w, float32x4.greaterThanOrEqual, (x, y) => boolToSimdLogical(fround(x) >= fround(y)));
+}
+
+function testEqualFloat64x2(v, w) {
+    testBinaryCompare(v, w, float64x2.equal, (x, y) => boolToSimdLogical(x == y));
+}
+function testNotEqualFloat64x2(v, w) {
+    testBinaryCompare(v, w, float64x2.notEqual, (x, y) => boolToSimdLogical(x != y));
+}
+function testLessThanFloat64x2(v, w) {
+    testBinaryCompare(v, w, float64x2.lessThan, (x, y) => boolToSimdLogical(x < y));
+}
+function testLessThanOrEqualFloat64x2(v, w) {
+    testBinaryCompare(v, w, float64x2.lessThanOrEqual, (x, y) => boolToSimdLogical(x <= y));
+}
+function testGreaterThanFloat64x2(v, w) {
+    testBinaryCompare(v, w, float64x2.greaterThan, (x, y) => boolToSimdLogical(x > y));
+}
+function testGreaterThanOrEqualFloat64x2(v, w) {
+    testBinaryCompare(v, w, float64x2.greaterThanOrEqual, (x, y) => boolToSimdLogical(x >= y));
 }
 
 function testEqualInt32x4(v, w) {
-    testBinaryFunc(v, w, int32x4.equal, (x, y) => boolToSimdLogical(x == y));
+    testBinaryCompare(v, w, int32x4.equal, (x, y) => boolToSimdLogical(x == y));
 }
 function testNotEqualInt32x4(v, w) {
-    testBinaryFunc(v, w, int32x4.notEqual, (x, y) => boolToSimdLogical(x != y));
+    testBinaryCompare(v, w, int32x4.notEqual, (x, y) => boolToSimdLogical(x != y));
 }
 function testLessThanInt32x4(v, w) {
-    testBinaryFunc(v, w, int32x4.lessThan, (x, y) => boolToSimdLogical(x < y));
+    testBinaryCompare(v, w, int32x4.lessThan, (x, y) => boolToSimdLogical(x < y));
 }
 function testLessThanOrEqualInt32x4(v, w) {
-    testBinaryFunc(v, w, int32x4.lessThanOrEqual, (x, y) => boolToSimdLogical(x <= y));
+    testBinaryCompare(v, w, int32x4.lessThanOrEqual, (x, y) => boolToSimdLogical(x <= y));
 }
 function testGreaterThanInt32x4(v, w) {
-    testBinaryFunc(v, w, int32x4.greaterThan, (x, y) => boolToSimdLogical(x > y));
+    testBinaryCompare(v, w, int32x4.greaterThan, (x, y) => boolToSimdLogical(x > y));
 }
 function testGreaterThanOrEqualInt32x4(v, w) {
-    testBinaryFunc(v, w, int32x4.greaterThanOrEqual, (x, y) => boolToSimdLogical(x >= y));
+    testBinaryCompare(v, w, int32x4.greaterThanOrEqual, (x, y) => boolToSimdLogical(x >= y));
 }
 
 function test() {
@@ -72,6 +92,32 @@ function test() {
           testLessThanOrEqualFloat32x4(v, w);
           testGreaterThanFloat32x4(v, w);
           testGreaterThanOrEqualFloat32x4(v, w);
+      }
+  }
+
+  var float64x2val = [
+      float64x2(1, 20),
+      float64x2(10, 2),
+      float64x2(9.999, 2.1234),
+      float64x2(10, 2.1233),
+      float64x2(30.4443, 4),
+      float64x2(30.4444, 4.0001),
+      float64x2(NaN, -Infinity),
+      float64x2(+Infinity, NaN),
+      float64x2(+Infinity, -0),
+      float64x2(-0, -Infinity),
+      float64x2(13.37, 42.42),
+      float64x2(NaN, 0)
+  ];
+
+  for (v of float64x2val) {
+      for (w of float64x2val) {
+          testEqualFloat64x2(v, w);
+          testNotEqualFloat64x2(v, w);
+          testLessThanFloat64x2(v, w);
+          testLessThanOrEqualFloat64x2(v, w);
+          testGreaterThanFloat64x2(v, w);
+          testGreaterThanOrEqualFloat64x2(v, w);
       }
   }
 
