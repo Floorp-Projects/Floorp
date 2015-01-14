cc5f6453a161d22d8c511cc009738be3d33c82ec
ziyunfei <446240525@qq.com>
2015-01-14 10:06:00 +0100

    Bug 896608 - Implement ES6 %TypedArray%.{of, from}. r=evilpie, till

---
+++ js/src/tests/ecma_6/TypedArray/from_basics.js
@@ -0,0 +1,54 @@
+const constructors = [
+    Int8Array,
+    Uint8Array,
+    Uint8ClampedArray,
+    Int16Array,
+    Uint16Array,
+    Int32Array,
+    Uint32Array,
+    Float32Array,
+    Float64Array
+];
+
+for (var constructor of constructors) {
+    // 'from' method is identical for all typed array constructors.
+    assertEq(constructors[0].from === constructor.from, true);
+
+    // %TypedArray%.from copies arrays.
+    var src = new constructor([1, 2, 3]), copy = constructor.from(src);
+    assertEq(copy === src, false);
+    assertEq(copy instanceof constructor, true);
+    assertDeepEq(copy, src);
+
+    // Non-element properties are not copied.
+    var a = new constructor([0, 1]);
+    a.name = "lisa";
+    assertDeepEq(constructor.from(a), new constructor([0, 1]));
+
+    // %TypedArray%.from can copy non-iterable objects, if they're array-like.
+    src = {0: 0, 1: 1, length: 2};
+    copy = constructor.from(src);
+    assertEq(copy instanceof constructor, true);
+    assertDeepEq(copy, new constructor([0, 1]));
+
+    // Properties past the .length are not copied.
+    src = {0: "0", 1: "1", 2: "two", 9: "nine", name: "lisa", length: 2};
+    assertDeepEq(constructor.from(src), new constructor([0, 1]));
+
+    // If an object has neither an @@iterator method nor .length,
+    // then it's treated as zero-length.
+    assertDeepEq(constructor.from({}), new constructor());
+
+    // Primitives will be coerced to primitive wrapper first.
+    assertDeepEq(constructor.from(1), new constructor());
+    assertDeepEq(constructor.from("123"), new constructor([1, 2, 3]));
+    assertDeepEq(constructor.from(true), new constructor());
+    assertDeepEq(constructor.from(Symbol()), new constructor());
+
+    // Source object property order doesn't matter.
+    src = {length: 2, 1: "1", 0: "0"};
+    assertDeepEq(constructor.from(src), new constructor([0, 1]));
+}
+
+if (typeof reportCompare === "function")
+    reportCompare(true, true);
---
+++ js/src/tests/ecma_6/TypedArray/from_proxy.js
@@ -0,0 +1,69 @@
+const constructors = [
+    Int8Array,
+    Uint8Array,
+    Uint8ClampedArray,
+    Int16Array,
+    Uint16Array,
+    Int32Array,
+    Uint32Array,
+    Float32Array,
+    Float64Array
+];
+
+for (var constructor of constructors) {
+    // Two tests involving %TypedArray%.from and a Proxy.
+    var log = [];
+    function LoggingProxy(target) {
+        log.push("target", target);
+        var h = {
+            defineProperty: function (t, id) {
+                log.push("define", id);
+                return undefined;
+            },
+            has: function (t, id) {
+                log.push("has", id);
+                return id in t;
+            },
+            get: function (t, id) {
+                log.push("get", id);
+                return t[id];
+            },
+            set: function (t, id, v) {
+                log.push("set", id);
+                t[id] = v;
+            }
+        };
+        return new Proxy(Object(target), h);
+    }
+
+    // Unlike Array.from, %TypedArray%.from uses [[Put]] instead of [[DefineOwnProperty]].
+    // Hence, it calls handler.set to create new elements rather than handler.defineProperty.
+    // Additionally, it doesn't set the length property at the end.
+    LoggingProxy.from = constructor.from;
+    LoggingProxy.from([3, 4, 5]);
+    assertDeepEq(log, ["target", 3, "set", "0", "set", "1", "set", "2"]);
+
+    // When the argument passed to %TypedArray%.from is a Proxy, %TypedArray%.from
+    // calls handler.get on it.
+    log = [];
+    assertDeepEq(constructor.from(new LoggingProxy([3, 4, 5])), new constructor([3, 4, 5]));
+    assertDeepEq(log, ["target", [3, 4, 5],
+                       "get", Symbol.iterator,
+                       "get", "length", "get", "0",
+                       "get", "length", "get", "1",
+                       "get", "length", "get", "2",
+                       "get", "length"]);
+
+    // Array-like iteration only gets the length once.
+    log = [];
+    var arr = [5, 6, 7];
+    arr[Symbol.iterator] = undefined;
+    assertDeepEq(constructor.from(new LoggingProxy(arr)), new constructor([5, 6, 7]));
+    assertDeepEq(log, ["target", [5, 6, 7],
+                       "get", Symbol.iterator,
+                       "get", "length",
+                       "get", "0", "get", "1", "get", "2"]);
+}
+
+if (typeof reportCompare === "function")
+    reportCompare(true, true);
