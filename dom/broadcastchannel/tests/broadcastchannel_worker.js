e86b23c1ac0a3d2bc7542d1274aaa2f04351723c
Andrea Marchesini <amarchesini@mozilla.com>
2015-01-14 11:50:35 +0000

    Bug 966439 - BroadcastChannel API - patch 4 - tests for Workers and SharedWorkers, r=smaug
    --HG--
    rename : dom/broadcastchannel/tests/broadcastchannel_worker.js => dom/broadcastchannel/tests/broadcastchannel_worker_alive.js

--- dom/broadcastchannel/tests/broadcastchannel_worker.js
+++ dom/broadcastchannel/tests/broadcastchannel_worker.js
@@ -1,8 +1,18 @@
 onmessage = function(evt) {
+  if (evt.data != 0) {
+    var worker = new Worker("broadcastchannel_worker.js");
+    worker.onmessage = function(evt) {
+      postMessage(evt.data);
+    }
+    worker.postMessage(evt.data - 1);
+    return;
+  }
+
   var bc = new BroadcastChannel('foobar');
   bc.addEventListener('message', function(event) {
     postMessage(event.data == "hello world from the window" ? "OK" : "KO");
     bc.postMessage("hello world from the worker");
+    bc.close();
   }, false);
 
   postMessage("READY");
