6b2281a472574387eb7ae1b7087fc517c64e2570
Nicholas Hurley <hurley@todesschaf.org>
2015-01-14 13:59:04 -0800

    Bug 1009122 - use cache2 as storage for predictor data. r=honzab

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -92,7 +92,7 @@ pref("network.buffer.cache.count", 24);
 pref("network.buffer.cache.size",  16384);
 
 // predictive actions
-pref("network.predictor.enable", false); // disabled on b2g
+pref("network.predictor.enabled", false); // disabled on b2g
 pref("network.predictor.max-db-size", 2097152); // bytes
 pref("network.predictor.preserve", 50); // percentage of predictor data to keep when cleaning up
 
