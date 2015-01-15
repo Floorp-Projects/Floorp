4d42b88e0b4232279f0fd9fa9f06615f96f58f73
Ehsan Akhgari <ehsan@mozilla.com>
2015-01-15 11:24:49 -0500

    Bug 989012 - Part 1: Stop after passing over a non-selectable frame if one is found during the frame traversal; r=roc
    The caret movement code already handles unselectable text frames if we
    happen to land in the middle of one in nsTextFrame::PeekOffsetCharacter/Word.
    However, when performing frame traversal to find the next frame to jump
    to, we don't remember if we skipped over an unselectable frame, which causes
    us to jump one offset too much when the caret is on the boundary of
    selectable and unselectable content.  The test cases demonstrate the
    scenario.  Note that an <img alt=foo> is implemented by adding a
    generated content to the inline frame representing it, so as far as
    the caret movement code is concerned, both test cases are treated similarly.
    
    Note that we need to do this only when moving the selection, and not
    when extending it.  We are adding an aExtend argument to
    nsPeekOffsetStruct's constructor in order to be able to special case
    that.

--- accessible/generic/HyperTextAccessible.cpp
+++ accessible/generic/HyperTextAccessible.cpp
@@ -510,7 +510,7 @@ HyperTextAccessible::FindOffset(uint32_t aOffset, nsDirection aDirection,
   nsPeekOffsetStruct pos(aAmount, aDirection, innerContentOffset,
                          nsPoint(0, 0), kIsJumpLinesOk, kIsScrollViewAStop,
                          kIsKeyboardSelect, kIsVisualBidi,
-                         aWordMovementType);
+                         false, aWordMovementType);
   nsresult rv = frameAtOffset->PeekOffset(&pos);
 
   // PeekOffset fails on last/first lines of the text in certain cases.
