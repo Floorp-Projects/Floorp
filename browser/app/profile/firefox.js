bfc73a696ef6d2c65504c3d8a0eaf6a1376ae68d
Mark Banner <standard8@mozilla.com>
2015-01-16 18:34:30 +0000

    Bug 1121071 Part 1 - Remove old call url code from the Loop panel and related UI areas. r=mikedeboer

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1677,7 +1677,6 @@ pref("loop.CSP", "default-src 'self' about: file: chrome:; img-src 'self' data: 
 #endif
 pref("loop.oauth.google.redirect_uri", "urn:ietf:wg:oauth:2.0:oob:auto");
 pref("loop.oauth.google.scope", "https://www.google.com/m8/feeds");
-pref("loop.rooms.enabled", true);
 pref("loop.fxa_oauth.tokendata", "");
 pref("loop.fxa_oauth.profile", "");
 pref("loop.support_url", "https://support.mozilla.org/kb/group-conversations-firefox-hello-webrtc");
