0bdd4ee3620798603b969ac0603ad3bc84057ad4
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-01-16 23:48:25 +1100

    Bug 1120086: Re-open SourceBuffer after call to appendBuffer if in ended state. r=cajbir a=sledru
    --HG--
    extra : source : f1d37b792c4a29731d237effd798f918218c43e6
    extra : histedit_source : e1f365a8fb839b2acd1586625990d0f71fabdf7e

--- dom/media/mediasource/SourceBufferResource.cpp
+++ dom/media/mediasource/SourceBufferResource.cpp
@@ -196,6 +196,7 @@ SourceBufferResource::AppendData(const uint8_t* aData, uint32_t aLength)
   SBR_DEBUG("SourceBufferResource(%p)::AppendData(aData=%p, aLength=%u)", this, aData, aLength);
   ReentrantMonitorAutoEnter mon(mMonitor);
   mInputBuffer.AppendItem(aData, aLength);
+  mEnded = false;
   mon.NotifyAll();
 }
 
