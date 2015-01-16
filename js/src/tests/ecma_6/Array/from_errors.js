bbe0162fcfec6dc5b2b280dae4a8613032c03899
ziyunfei <446240525@qq.com>
2015-01-16 00:34:00 -0500

    Bug 1121391 - Update Array.from to match the spec. r=till

--- js/src/tests/ecma_6/Array/from_errors.js
+++ js/src/tests/ecma_6/Array/from_errors.js
@@ -130,6 +130,13 @@ assertThrowsValue(() => Array.from.call(C, arrayish, () => { throw exc; }), exc)
 assertEq(log, "lC0");
 assertEq(obj instanceof C, true);
 
+// It's a TypeError if the @@iterator property is a primitive (except null and undefined).
+for (var primitive of ["foo", 17, Symbol(), true]) {
+    assertThrowsInstanceOf(() => Array.from({[Symbol.iterator] : primitive}), TypeError);
+}
+assertDeepEq(Array.from({[Symbol.iterator]: null}), []);
+assertDeepEq(Array.from({[Symbol.iterator]: undefined}), []);
+
 // It's a TypeError if the iterator's .next() method returns a primitive.
 for (var primitive of [undefined, null, 17]) {
     assertThrowsInstanceOf(
