f08368d025aee925ba4fd55e9f99d2cd9781f46c
Phil Ringnalda <philringnalda@gmail.com>
2015-01-17 10:32:49 -0800

    Bug 786938 - Disable test_handlerApps.xhtml on OS X. a=test-only

--- uriloader/exthandler/tests/mochitest/handlerApps.js
+++ uriloader/exthandler/tests/mochitest/handlerApps.js
@@ -9,14 +9,6 @@ const Cc = SpecialPowers.Cc;
 
 function test() {
 
-  const isOSXMtnLion = navigator.userAgent.indexOf("Mac OS X 10.8") != -1;
-  const isOSXMavericks = navigator.userAgent.indexOf("Mac OS X 10.9") != -1;
-  if (isOSXMtnLion || isOSXMavericks) {
-    todo(false, "This test fails on OS X 10.8 and 10.9, see bug 786938");
-    SimpleTest.finish();
-    return;
-  }
-
   // set up the web handler object
   var webHandler = Cc["@mozilla.org/uriloader/web-handler-app;1"].
     createInstance(SpecialPowers.Ci.nsIWebHandlerApp);
