b099cc3f3e5d1ab8a4c867f2b1537e8703897420
Tim Taubert <ttaubert@mozilla.com>
2015-01-17 19:35:59 +0100

    Bug 1113178 - Fix all remaining tests using removeAllPages() r=mak

--- toolkit/mozapps/downloads/tests/chrome/utils.js
+++ toolkit/mozapps/downloads/tests/chrome/utils.js
@@ -146,15 +146,3 @@ function setCleanState()
   let win = getDMWindow();
   if (win) win.close();
 }
-
-/**
- * Clears history invoking callback when done.
- */
-function waitForClearHistory(aCallback) {
-  Components.utils.import("resource://gre/modules/PlacesUtils.jsm");
-  Services.obs.addObserver(function observeClearHistory(aSubject, aTopic) {
-    Services.obs.removeObserver(observeClearHistory, aTopic);
-    aCallback();
-  }, PlacesUtils.TOPIC_EXPIRATION_FINISHED, false);
-  PlacesUtils.bhistory.removeAllPages();
-}
