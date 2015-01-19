ad8a8ac5d9fc1dff7a1d8d4e1d0880f92c73c16c
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2015-01-19 17:40:12 +0000

    Bug 1012223 - make opening about:preferences faster, r=jaws
    --HG--
    extra : rebase_source : 23ef8bb84ae55f81bef21b822febf3482f827a92

--- browser/components/preferences/in-content/tests/browser_bug731866.js
+++ browser/components/preferences/in-content/tests/browser_bug731866.js
@@ -9,87 +9,37 @@ function test() {
   open_preferences(runTest);
 }
 
+let gElements;
+
+function checkElements(expectedPane) {
+  for (let element of gElements) {
+    // preferences elements fail is_element_visible checks because they are never visible.
+    if (element.nodeName == "preferences") {
+      continue;
+    }
+    let attributeValue = element.getAttribute("data-category");
+    if (attributeValue == "pane" + expectedPane) {
+      is_element_visible(element, expectedPane + " elements should be visible");
+    } else {
+      is_element_hidden(element, "Elements not in " + expectedPane + " should be hidden");
+    }
+  }
+}
+
 function runTest(win) {
   is(gBrowser.currentURI.spec, "about:preferences", "about:preferences loaded");
 
   let tab = win.document;
-  let elements = tab.getElementById("mainPrefPane").children;
+  gElements = tab.getElementById("mainPrefPane").children;
 
-  //Test if general pane is opened correctly
-  win.gotoPref("paneGeneral");
-  for (let element of elements) {
-    let attributeValue = element.getAttribute("data-category");
-    if (attributeValue == "paneGeneral") {
-      is_element_visible(element, "General elements should be visible");
-    } else {
-      is_element_hidden(element, "Non-General elements should be hidden");
-    }
-  }
+  let panes = [
+    "General", "Search", "Content", "Applications",
+    "Privacy", "Security", "Sync", "Advanced",
+  ];
 
-  //Test if content pane is opened correctly
-  win.gotoPref("paneContent");
-  for (let element of elements) {
-    let attributeValue = element.getAttribute("data-category");
-    if (attributeValue == "paneContent") {
-      is_element_visible(element, "Content elements should be visible");
-    } else {
-      is_element_hidden(element, "Non-Content elements should be hidden");
-    }
-  }
-
-  //Test if applications pane is opened correctly
-  win.gotoPref("paneApplications");
-  for (let element of elements) {
-    let attributeValue = element.getAttribute("data-category");
-    if (attributeValue == "paneApplications") {
-      is_element_visible(element, "Application elements should be visible");
-    } else {
-      is_element_hidden(element, "Non-Application elements should be hidden");
-    }
-  }
-
-  //Test if privacy pane is opened correctly
-  win.gotoPref("panePrivacy");
-  for (let element of elements) {
-    let attributeValue = element.getAttribute("data-category");
-    if (attributeValue == "panePrivacy") {
-      is_element_visible(element, "Privacy elements should be visible");
-    } else {
-      is_element_hidden(element, "Non-Privacy elements should be hidden");
-    }
-  }
-
-  //Test if security pane is opened correctly
-  win.gotoPref("paneSecurity");
-  for (let element of elements) {
-    let attributeValue = element.getAttribute("data-category");
-    if (attributeValue == "paneSecurity") {
-      is_element_visible(element, "Security elements should be visible");
-    } else {
-      is_element_hidden(element, "Non-Security elements should be hidden");
-    }
-  }
-
-  //Test if sync pane is opened correctly
-  win.gotoPref("paneSync");
-  for (let element of elements) {
-    let attributeValue = element.getAttribute("data-category");
-    if (attributeValue == "paneSync") {
-      is_element_visible(element, "Sync elements should be visible");
-    } else {
-      is_element_hidden(element, "Non-Sync elements should be hidden");
-    }
-  }
-
-  //Test if advanced pane is opened correctly
-  win.gotoPref("paneAdvanced");
-  for (let element of elements) {
-    let attributeValue = element.getAttribute("data-category");
-    if (attributeValue == "paneAdvanced") {
-      is_element_visible(element, "Advanced elements should be visible");
-    } else {
-      is_element_hidden(element, "Non-Advanced elements should be hidden");
-    }
+  for (let pane of panes) {
+    win.gotoPref("pane" + pane);
+    checkElements(pane);
   }
 
   gBrowser.removeCurrentTab();
