2f7cd0debc600eb6d250d9af928cf5735af4cfc4
Olli Pettay <Olli.Pettay@helsinki.fi>
2015-01-19 13:17:22 +0200

    Bug 1121473, define the list of void tags only in one place, r=hsivonen

--- dom/base/FragmentOrElement.h
+++ dom/base/FragmentOrElement.h
@@ -269,6 +269,10 @@ public:
   static void MarkUserData(void* aObject, nsIAtom* aKey, void* aChild,
                            void *aData);
 
+  /**
+   * Is the HTML local name a void element?
+   */
+  static bool IsHTMLVoid(nsIAtom* aLocalName);
 protected:
   virtual ~FragmentOrElement();
 
