7e053a8797ab84b5b96afe3cdbacef5c761f095f
Blair McBride <bmcbride@mozilla.com>
2015-01-19 22:55:15 +1300

    Bug 1075625 - AddonManager can race between provider startup / shutdown and methods that call back to providers. r=Mossop

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_provider_markSafe.js
@@ -0,0 +1,47 @@
+createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+
+let startupOrder = [];
+
+function mockAddonProvider(name) {
+  let mockProvider = {
+    markSafe: false,
+    apiAccessed: false,
+
+    startup() {
+      if (this.markSafe)
+        AddonManagerPrivate.markProviderSafe(this);
+
+      let uri = Services.io.newURI("beard://long", null, null);
+      AddonManager.isInstallEnabled("made-up-mimetype");
+    },
+    supportsMimetype(mimetype) {
+      this.apiAccessed = true;
+      return false;
+    },
+
+    get name() name,
+  };
+
+  return mockProvider;
+};
+
+function run_test() {
+  run_next_test();
+}
+
+add_task(function* testMarkSafe() {
+  do_print("Starting with provider normally");
+  let provider = mockAddonProvider("Mock1");
+  AddonManagerPrivate.registerProvider(provider);
+  startupManager();
+  ok(!provider.apiAccessed, "Provider API should not have been accessed");
+  AddonManagerPrivate.unregisterProvider(provider);
+  yield promiseShutdownManager();
+
+  do_print("Starting with provider that marks itself safe");
+  provider.apiAccessed = false;
+  provider.markSafe = true;
+  AddonManagerPrivate.registerProvider(provider);
+  startupManager();
+  ok(provider.apiAccessed, "Provider API should have been accessed");
+});
---
+++ toolkit/mozapps/extensions/test/xpcshell/test_provider_unsafe_access_startup.js
@@ -0,0 +1,53 @@
+createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+
+let startupOrder = [];
+
+function mockAddonProvider(name) {
+  let mockProvider = {
+    hasStarted: false,
+    unsafeAccess: false,
+
+    startupCallback: null,
+
+    startup() {
+      this.hasStarted = true;
+      startupOrder.push(this.name);
+      if (this.startupCallback)
+        this.startupCallback();
+    },
+    getAddonByID(id, callback) {
+      if (!this.hasStarted) {
+        unsafeAccess = true;
+      }
+      callback(null);
+    },
+
+    get name() name,
+  };
+
+  return mockProvider;
+};
+
+function run_test() {
+  run_next_test();
+}
+
+add_task(function* unsafeProviderStartup() {
+  let secondProvider = null;
+
+  yield new Promise(resolve => {
+    let firstProvider = mockAddonProvider("Mock1");
+    firstProvider.startupCallback = function() {
+      AddonManager.getAddonByID("does-not-exist", resolve);
+    };
+    AddonManagerPrivate.registerProvider(firstProvider);
+
+    secondProvider = mockAddonProvider("Mock2");
+    AddonManagerPrivate.registerProvider(secondProvider);
+
+    startupManager();
+  });
+
+  equal(startupOrder.join(","), ["Mock1", "Mock2"].join(","), "Mock providers should have hasStarted in expected order");
+  ok(!secondProvider.unsafeAccess, "Second registered mock provider should not have been accessed unsafely");
+});
