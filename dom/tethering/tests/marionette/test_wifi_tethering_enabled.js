6b0e5d7e6c80c52ece5782b605048f707e8a364d
Jessica Jong <jjong@mozilla.com>
2015-01-20 02:11:00 -0500

    Bug 1115299 - Part 2: Add test case for tethering with dun. r=echen

--- dom/tethering/tests/marionette/test_wifi_tethering_enabled.js
+++ dom/tethering/tests/marionette/test_wifi_tethering_enabled.js
@@ -7,5 +7,6 @@ MARIONETTE_HEAD_JS = 'head.js';
 
 gTestSuite.startTetheringTest(function() {
   return gTestSuite.ensureWifiEnabled(false)
-    .then(() => gTestSuite.setWifiTetheringEnabled(true));
+    .then(() => gTestSuite.setWifiTetheringEnabled(true))
+    .then(() => gTestSuite.setWifiTetheringEnabled(false));
 });
