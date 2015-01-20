aaf1e46238c1bf4b91c6a16a0e08744ddd689512
Matthew Gregan <kinetik@flim.org>
2015-01-20 18:39:00 +1300

    Bug 1124021 - Fix dangerous UniquePtr usage pattern in GMP.  r=cpearce

--- dom/media/gmp/GMPVideoEncoderParent.cpp
+++ dom/media/gmp/GMPVideoEncoderParent.cpp
@@ -15,6 +15,7 @@
 #include "nsThread.h"
 #include "nsThreadUtils.h"
 #include "runnable_utils.h"
+#include "GMPUtils.h"
 
 namespace mozilla {
 
@@ -125,7 +126,7 @@ GMPVideoEncoderParent::InitEncode(const GMPVideoCodec& aCodecSettings,
 }
 
 GMPErr
-GMPVideoEncoderParent::Encode(UniquePtr<GMPVideoi420Frame> aInputFrame,
+GMPVideoEncoderParent::Encode(GMPUniquePtr<GMPVideoi420Frame> aInputFrame,
                               const nsTArray<uint8_t>& aCodecSpecificInfo,
                               const nsTArray<GMPVideoFrameType>& aFrameTypes)
 {
@@ -136,7 +137,7 @@ GMPVideoEncoderParent::Encode(UniquePtr<GMPVideoi420Frame> aInputFrame,
 
   MOZ_ASSERT(mPlugin->GMPThread() == NS_GetCurrentThread());
 
-  UniquePtr<GMPVideoi420FrameImpl> inputFrameImpl(
+  GMPUniquePtr<GMPVideoi420FrameImpl> inputFrameImpl(
     static_cast<GMPVideoi420FrameImpl*>(aInputFrame.release()));
 
   // Very rough kill-switch if the plugin stops processing.  If it's merely
