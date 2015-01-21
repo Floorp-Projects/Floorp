f58ad85b9ff5c1866ecb9d7e03de086b58527dfb
Bob Owen <bobowencode@gmail.com>
2015-01-21 07:59:56 +0000

    Bug 1121479 Part 1: Turn on DEP process-level mitigation for the GMP sandbox. r=tabraldes

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -171,6 +171,12 @@ SandboxBroker::SetSecurityLevelForGMPlugin()
     mPolicy->SetDelayedIntegrityLevel(sandbox::INTEGRITY_LEVEL_UNTRUSTED);
   ret = ret && (sandbox::SBOX_ALL_OK == result);
 
+  sandbox::MitigationFlags mitigations =
+    sandbox::MITIGATION_DEP;
+
+  result = mPolicy->SetProcessMitigations(mitigations);
+  ret = ret && (sandbox::SBOX_ALL_OK == result);
+
   // Add the policy for the client side of a pipe. It is just a file
   // in the \pipe\ namespace. We restrict it to pipes that start with
   // "chrome." so the sandboxed process cannot connect to system services.
