4e93f4c853e9d497542dc938af078b532fc9600f
Edgar Chen <echen@mozilla.com>
2015-01-21 12:39:13 +0800

    Bug 1087968 - Part 3: Rewrite test_icc_* with Promise. r=hsinyi

--- dom/icc/tests/marionette/test_icc_access_invalid_object.js
+++ dom/icc/tests/marionette/test_icc_access_invalid_object.js
@@ -1,114 +1,101 @@
 /* Any copyright is dedicated to the Public Domain.
-   http://creativecommons.org/publicdomain/zero/1.0/ */
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 MARIONETTE_TIMEOUT = 30000;
-MARIONETTE_HEAD_JS = "icc_header.js";
+MARIONETTE_HEAD_JS = "head.js";
 
-function setRadioEnabled(enabled) {
-  let connection = navigator.mozMobileConnections[0];
-  ok(connection);
+function testInvalidIccObject(aIcc) {
+  // Test access iccInfo.
+  try {
+    is(aIcc.iccInfo, null, "iccInfo: expect to get null");
+  } catch(e) {
+    ok(false, "access iccInfo should not get exception");
+  }
 
-  let request  = connection.setRadioEnabled(enabled);
+  // Test access cardState.
+  try {
+    is(aIcc.cardState, null, "cardState: expect to get null");
+  } catch(e) {
+    ok(false, "access cardState should not get exception");
+  }
 
-  request.onsuccess = function onsuccess() {
-    log('setRadioEnabled: ' + enabled);
-  };
+  // Test STK related function.
+  try {
+    aIcc.sendStkResponse({}, {});
+    ok(false, "sendStkResponse() should get exception");
+  } catch(e) {}
+  try {
+    aIcc.sendStkMenuSelection(0, false);
+    ok(false, "sendStkMenuSelection() should get exception");
+  } catch(e) {}
+  try {
+    aIcc.sendStkTimerExpiration({});
+    ok(false, "sendStkTimerExpiration() should get exception");
+  } catch(e) {}
+  try {
+    aIcc.sendStkEventDownload({});
+    ok(false, "sendStkEventDownload() should get exception");
+  } catch(e) {}
 
-  request.onerror = function onerror() {
-    ok(false, "setRadioEnabled should be ok");
-  };
+  // Test card lock related function.
+  try {
+    aIcc.getCardLock("pin");
+    ok(false, "getCardLock() should get exception");
+  } catch(e) {}
+  try {
+    aIcc.unlockCardLock({});
+    ok(false, "unlockCardLock() should get exception");
+  } catch(e) {}
+  try {
+    aIcc.setCardLock({});
+    ok(false, "setCardLock() should get exception");
+  } catch(e) {}
+  try {
+    aIcc.getCardLockRetryCount("pin");
+    ok(false, "getCardLockRetryCount() should get exception");
+  } catch(e) {}
+
+  // Test contact related function.
+  try {
+    aIcc.readContacts("adn");
+    ok(false, "readContacts() should get exception");
+  } catch(e) {}
+  try {
+    aIcc.updateContact("adn", {});
+    ok(false, "updateContact() should get exception");
+  } catch(e) {}
+
+  // Test mvno function.
+  try {
+    aIcc.matchMvno("imsi");
+    ok(false, "matchMvno() should get exception");
+  } catch(e) {}
+
+  // Test service state function.
+  return aIcc.getServiceState("fdn").then(() => {
+    ok(false, "getServiceState() should be rejected");
+  }, () => {});
 }
 
-/* Test access invalid icc object */
-taskHelper.push(function testAccessRemovedIccObject() {
-  setRadioEnabled(false);
-  iccManager.addEventListener("iccundetected", function oniccundetected(evt) {
-    log("got icc undetected event");
-    iccManager.removeEventListener("iccundetected", oniccundetected);
-    is(evt.iccId, iccId, "icc " + evt.iccId + " becomes undetected");
+// Start tests
+startTestCommon(function() {
+  let icc = getMozIcc();
 
-    // Test access iccInfo.
-    try {
-      is(icc.iccInfo, null, "iccInfo: expect to get null");
-    } catch(e) {
-      ok(false, "access iccInfo should not get exception");
-    }
-
-    // Test access cardState.
-    try {
-      is(icc.cardState, null, "cardState: expect to get null");
-    } catch(e) {
-      ok(false, "access cardState should not get exception");
-    }
-
-    // Test STK related function.
-    try {
-      icc.sendStkResponse({}, {});
-      ok(false, "sendStkResponse() should get exception");
-    } catch(e) {}
-    try {
-      icc.sendStkMenuSelection(0, false);
-      ok(false, "sendStkMenuSelection() should get exception");
-    } catch(e) {}
-    try {
-      icc.sendStkTimerExpiration({});
-      ok(false, "sendStkTimerExpiration() should get exception");
-    } catch(e) {}
-    try {
-      icc.sendStkEventDownload({});
-      ok(false, "sendStkEventDownload() should get exception");
-    } catch(e) {}
-
-    // Test card lock related function.
-    try {
-      icc.getCardLock("");
-      ok(false, "getCardLock() should get exception");
-    } catch(e) {}
-    try {
-      icc.unlockCardLock({});
-      ok(false, "unlockCardLock() should get exception");
-    } catch(e) {}
-    try {
-      icc.setCardLock({});
-      ok(false, "setCardLock() should get exception");
-    } catch(e) {}
-    try {
-      icc.getCardLockRetryCount("");
-      ok(false, "getCardLockRetryCount() should get exception");
-    } catch(e) {}
-
-    // Test contact related function.
-    try {
-      icc.readContacts("");
-      ok(false, "readContacts() should get exception");
-    } catch(e) {}
-    try {
-      icc.updateContact("", {});
-      ok(false, "updateContact() should get exception");
-    } catch(e) {}
-
-    // Test secure element related function.
-    try {
-      icc.iccOpenChannel("");
-      ok(false, "iccOpenChannel() should get exception");
-    } catch(e) {}
-    try {
-      icc.iccExchangeAPDU(0, {});
-      ok(false, "iccExchangeAPDU() should get exception");
-    } catch(e) {}
-    try {
-      icc.iccCloseChannel(0);
-      ok(false, "iccCloseChannel() should get exception");
-    } catch(e) {}
-
+  return Promise.resolve()
+    // Turn off radio.
+    .then(() => {
+      let promises = [];
+      promises.push(setRadioEnabled(false));
+      promises.push(waitForTargetEvent(iccManager, "iccundetected"));
+      return Promise.all(promises);
+    })
+    // Test accessing invalid icc object.
+    .then(() => testInvalidIccObject(icc))
     // We should restore the radio status.
-    setRadioEnabled(true);
-    iccManager.addEventListener("iccdetected", function oniccdetected(evt) {
-      iccManager.removeEventListener("iccdetected", oniccdetected);
-      taskHelper.runNext();
+    .then(() => {
+      let promises = [];
+      promises.push(setRadioEnabled(true));
+      promises.push(waitForTargetEvent(iccManager, "iccdetected"));
+      return Promise.all(promises);
     });
-  });
 });
-
-// Start test
-taskHelper.runNext();
