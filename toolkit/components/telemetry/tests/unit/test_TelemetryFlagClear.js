fee93844aaaae4ca5649f9978e0177b8931cd191
Alessio Placitelli <alessio.placitelli@gmail.com>
2015-01-21 03:32:00 +0100

    Bug 1122049 - Add logging to Telemetry. r=gfritzsche

--- toolkit/components/telemetry/tests/unit/test_TelemetryFlagClear.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryFlagClear.js
@@ -1,9 +1,6 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-const Cu = Components.utils;
-const {Services} = Cu.import("resource://gre/modules/Services.jsm", {});
-
 function run_test()
 {
   let testFlag = Services.telemetry.getHistogramById("TELEMETRY_TEST_FLAG");
