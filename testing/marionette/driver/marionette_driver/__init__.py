6ac5c09770cc010298eb30ae4ec2be0e440e8480
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-01-21 16:12:13 +0100

    Backed out changeset c886cd910936 (bug 1107336)
    --HG--
    rename : testing/marionette/driver/marionette_driver/application_cache.py => testing/marionette/client/marionette/application_cache.py
    rename : testing/marionette/driver/marionette_driver/by.py => testing/marionette/client/marionette/by.py
    rename : testing/marionette/driver/marionette_driver/date_time_value.py => testing/marionette/client/marionette/date_time_value.py
    rename : testing/marionette/driver/marionette_driver/decorators.py => testing/marionette/client/marionette/decorators.py
    rename : testing/marionette/driver/marionette_driver/errors.py => testing/marionette/client/marionette/errors.py
    rename : testing/marionette/driver/marionette_driver/expected.py => testing/marionette/client/marionette/expected.py
    rename : testing/marionette/driver/marionette_driver/geckoinstance.py => testing/marionette/client/marionette/geckoinstance.py
    rename : testing/marionette/driver/marionette_driver/gestures.py => testing/marionette/client/marionette/gestures.py
    rename : testing/marionette/driver/marionette_driver/keys.py => testing/marionette/client/marionette/keys.py
    rename : testing/marionette/driver/marionette_driver/marionette.py => testing/marionette/client/marionette/marionette.py
    rename : testing/marionette/driver/marionette_driver/selection.py => testing/marionette/client/marionette/selection.py
    rename : testing/marionette/driver/marionette_driver/wait.py => testing/marionette/client/marionette/wait.py

--- testing/marionette/driver/marionette_driver/__init__.py
+++
@@ -1,3 +0,0 @@
-from marionette_driver import ( errors, by, decorators, expected, geckoinstance,
-                                gestures, keys, marionette, selection, wait,
-                                application_cache, date_time_value )
