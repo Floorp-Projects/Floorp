cecc7bf3eabbade06da7fb5a03b3768081f4fa31
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-01-21 16:11:42 +0100

    Backed out changeset 805dce7ae79f (bug 1107336)

--- testing/marionette/client/marionette/tests/unit/test_switch_frame_chrome.py
+++ testing/marionette/client/marionette/tests/unit/test_switch_frame_chrome.py
@@ -3,8 +3,8 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from marionette_test import MarionetteTestCase
-from marionette_driver.errors import JavascriptException
-from marionette_driver.by import By
+from errors import JavascriptException
+from by import By
 
 class TestSwitchFrameChrome(MarionetteTestCase):
     def setUp(self):
