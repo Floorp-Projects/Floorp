04c39632243503ab977db8ad613e34f783a132de
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2015-01-21 13:21:41 -0800

    Bug 1110602 - Don't remove tour tabs from originTabs when switching tabs so they can continue to get notifications. r=Unfocused, a=sledru

--- browser/base/content/browser-syncui.js
+++ browser/base/content/browser-syncui.js
@@ -305,7 +305,8 @@ let gSyncUI = {
           this.openPrefs();
         } else {
           // If the user is also in an uitour, set the entrypoint to `uitour`
-          if (UITour.originTabs.get(window) && UITour.originTabs.get(window).has(gBrowser.selectedTab)) {
+          if (UITour.tourBrowsersByWindow.get(window) &&
+              UITour.tourBrowsersByWindow.get(window).has(gBrowser.selectedBrowser)) {
             entryPoint = "uitour";
           }
           switchToTabHavingURI("about:accounts?entrypoint=" + entryPoint, true, {
