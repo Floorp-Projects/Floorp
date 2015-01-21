80bed1aa7082dacab8380f58ed2eee07a73b6fa3
Ian Moody <moz-ian@perix.co.uk>
2015-01-21 10:39:45 -0800

    Bug 972655 - Console should show XMLHttpRequests - Add XHR filter to webconsole. r=past

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1496,6 +1496,7 @@ pref("devtools.gcli.underscoreSrc", "https://cdnjs.cloudflare.com/ajax/libs/unde
 pref("devtools.webconsole.filter.network", true);
 pref("devtools.webconsole.filter.networkinfo", false);
 pref("devtools.webconsole.filter.netwarn", true);
+pref("devtools.webconsole.filter.netxhr", false);
 pref("devtools.webconsole.filter.csserror", true);
 pref("devtools.webconsole.filter.cssparser", false);
 pref("devtools.webconsole.filter.csslog", false);
@@ -1513,6 +1514,7 @@ pref("devtools.webconsole.filter.secwarn", true);
 pref("devtools.browserconsole.filter.network", true);
 pref("devtools.browserconsole.filter.networkinfo", false);
 pref("devtools.browserconsole.filter.netwarn", true);
+pref("devtools.browserconsole.filter.netxhr", false);
 pref("devtools.browserconsole.filter.csserror", true);
 pref("devtools.browserconsole.filter.cssparser", false);
 pref("devtools.browserconsole.filter.csslog", false);
