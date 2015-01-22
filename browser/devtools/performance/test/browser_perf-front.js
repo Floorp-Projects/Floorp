b2c93292b4cd517b214944639d777d8316c6b718
Victor Porof <vporof@mozilla.com>
2015-01-22 12:20:55 -0500

    Bug 1105014 - Part 3: Fix existing tests and add a few more, r=jsantell

--- browser/devtools/performance/test/browser_perf-front.js
+++ browser/devtools/performance/test/browser_perf-front.js
@@ -5,23 +5,32 @@
  * Test basic functionality of PerformanceFront, emitting start and endtime values
  */
 
-let WAIT = 1000;
+let WAIT_TIME = 1000;
 
 function spawnTest () {
   let { target, front } = yield initBackend(SIMPLE_URL);
 
-  let { startTime } = yield front.startRecording();
+  let { profilerStartTime, timelineStartTime } = yield front.startRecording();
 
-  ok(typeof startTime === "number", "front.startRecording() emits start time");
+  ok(typeof profilerStartTime === "number",
+    "The front.startRecording() emits a profiler start time.");
+  ok(typeof timelineStartTime === "number",
+    "The front.startRecording() emits a timeline start time.");
 
-  yield busyWait(WAIT);
+  yield busyWait(WAIT_TIME);
 
-  let { endTime } = yield front.stopRecording();
+  let { profilerEndTime, timelineEndTime } = yield front.stopRecording();
 
-  ok(typeof endTime === "number", "front.stopRecording() emits end time");
-  ok(endTime > startTime, "endTime is after startTime");
+  ok(typeof profilerEndTime === "number",
+    "The front.stopRecording() emits a profiler end time.");
+  ok(typeof timelineEndTime === "number",
+    "The front.stopRecording() emits a timeline end time.");
+
+  ok(profilerEndTime > profilerStartTime,
+    "The profilerEndTime is after profilerStartTime.");
+  ok(timelineEndTime > timelineStartTime,
+    "The timelineEndTime is after timelineStartTime.");
 
   yield removeTab(target.tab);
   finish();
-
 }
