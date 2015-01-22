e1bd637e6727e26397e589a3bccade746f28b496
Mark Capella <markcapella@twcny.rr.com>
2015-01-21 19:23:49 -0500

    Bug 851861 - Intermittent testFlingCorrectness, etc al. dragSync() consumers. r=mfinkle, a=sledru

--- mobile/android/base/tests/MotionEventHelper.java
+++ mobile/android/base/tests/MotionEventHelper.java
@@ -1,5 +1,8 @@
 package org.mozilla.gecko.tests;
 
+import org.mozilla.gecko.GeckoAppShell;
+import org.mozilla.gecko.gfx.LayerView;
+
 import android.app.Instrumentation;
 import android.os.SystemClock;
 import android.util.FloatMath;
@@ -14,11 +17,13 @@ class MotionEventHelper {
     private final Instrumentation mInstrumentation;
     private final int mSurfaceOffsetX;
     private final int mSurfaceOffsetY;
+    private final LayerView layerView;
 
     public MotionEventHelper(Instrumentation inst, int surfaceOffsetX, int surfaceOffsetY) {
         mInstrumentation = inst;
         mSurfaceOffsetX = surfaceOffsetX;
         mSurfaceOffsetY = surfaceOffsetY;
+        layerView = GeckoAppShell.getLayerView();
         Log.i(LOGTAG, "Initialized using offset (" + mSurfaceOffsetX + "," + mSurfaceOffsetY + ")");
     }
 
@@ -71,6 +76,8 @@ class MotionEventHelper {
         Thread t = new Thread() {
             @Override
             public void run() {
+                layerView.setIsLongpressEnabled(false);
+
                 int numEvents = (int)(durationMillis * DRAG_EVENTS_PER_SECOND / 1000);
                 float eventDx = (endX - startX) / numEvents;
                 float eventDy = (endY - startY) / numEvents;
@@ -93,6 +100,8 @@ class MotionEventHelper {
                 // do the last one using endX/endY directly to avoid rounding errors
                 downTime = move(downTime, endX, endY);
                 downTime = up(downTime, endX, endY);
+
+                layerView.setIsLongpressEnabled(true);
             }
         };
         t.start();
