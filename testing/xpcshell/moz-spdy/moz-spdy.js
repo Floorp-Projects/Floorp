c5cb2c09eb84f86fe9e379d80c1fe9fce57c7059
Patrick McManus <mcmanus@ducksong.com>
2015-01-22 12:07:43 -0500

    bug 1102923 - testing spdy server restart on unhandled exception r=test-only

--- testing/xpcshell/moz-spdy/moz-spdy.js
+++ testing/xpcshell/moz-spdy/moz-spdy.js
@@ -90,6 +90,17 @@ function executeRunLater(arg) {
 }
 
 function handleRequest(req, res) {
+  try {
+    realHandleRequest(req, res);
+  } catch (e) {
+    console.log("spdy server suffered unhandled exception. will restart " + e);
+    var p = webServer.address().port;
+    webServer.close();
+    webServer = spdy.createServer(options, handleRequest).listen(p, "0.0.0.0", 200, listenok);
+  }
+}
+
+function realHandleRequest(req, res) {
   var u = url.parse(req.url);
   var content = getHttpContent(u.pathname);
 
