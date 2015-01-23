b2440e18fd980c04f69b9f8fbcc645be8775045c
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2015-01-23 18:43:46 +0900

    Bug 512407 - Support Windows Location API. r=jdm,jmathies

--- dom/geolocation/nsGeolocation.cpp
+++ dom/geolocation/nsGeolocation.cpp
@@ -49,6 +49,10 @@ class nsIPrincipal;
 #include "CoreLocationLocationProvider.h"
 #endif
 
+#ifdef XP_WIN
+#include "WindowsLocationProvider.h"
+#endif
+
 // Some limit to the number of get or watch geolocation requests
 // that a window can make.
 #define MAX_GEO_REQUESTS_PER_WINDOW  1500
@@ -807,6 +811,12 @@ nsresult nsGeolocationService::Init()
 #ifdef MOZ_WIDGET_COCOA
   if (Preferences::GetBool("geo.provider.use_corelocation", false)) {
     mProvider = new CoreLocationLocationProvider();
+  }
+#endif
+
+#ifdef XP_WIN
+  if (Preferences::GetBool("geo.provider.ms-windows-location", false)) {
+    mProvider = new WindowsLocationProvider();
   }
 #endif
 
