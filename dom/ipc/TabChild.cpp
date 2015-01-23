b8d4206b47f9797ceb175acbdf1fbe0e94f201ea
Bill McCloskey <wmccloskey@mozilla.com>
2015-01-22 20:00:19 -0800

    Bug 1123461 - Rename CpowIdHolder to CrossProcessCpowHolder (r=dvander)

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -3008,7 +3008,7 @@ TabChild::RecvAsyncMessage(const nsString& aMessage,
     StructuredCloneData cloneData = UnpackClonedMessageDataForChild(aData);
     nsRefPtr<nsFrameMessageManager> mm =
       static_cast<nsFrameMessageManager*>(mTabChildGlobal->mMessageManager.get());
-    CpowIdHolder cpows(Manager(), aCpows);
+    CrossProcessCpowHolder cpows(Manager(), aCpows);
     mm->ReceiveMessage(static_cast<EventTarget*>(mTabChildGlobal),
                        aMessage, false, &cloneData, &cpows, aPrincipal, nullptr);
   }
