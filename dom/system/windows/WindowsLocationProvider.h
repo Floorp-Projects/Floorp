fad4d88a07b04085663c4ef9e5ba1dfac644f8fe
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2015-01-23 18:43:46 +0900

    Bug 512407 - Support Windows Location API. r=jdm,jmathies

---
+++ dom/system/windows/WindowsLocationProvider.h
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_WindowsLocationProvider_h__
+#define mozilla_dom_WindowsLocationProvider_h__
+
+#include "nsAutoPtr.h"
+#include "nsIGeolocationProvider.h"
+
+#include <LocationApi.h>
+
+namespace mozilla {
+namespace dom {
+
+class WindowsLocationProvider MOZ_FINAL : public nsIGeolocationProvider
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIGEOLOCATIONPROVIDER
+
+  WindowsLocationProvider();
+
+private:
+  nsRefPtr<ILocation> mLocation;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_WindowsLocationProvider_h__
