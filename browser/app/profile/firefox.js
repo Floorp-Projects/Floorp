2c41ca610f13781d38a626be96515bafc4b76d0d
Bob Owen <bobowencode@gmail.com>
2015-01-23 08:32:21 +0000

    Bug 1123245 Part 3: Add prefs for the Windows NPAPI process sandbox. r=bsmedberg

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1182,6 +1182,12 @@ pref("browser.tabs.remote.desktopbehavior", true);
 // This will require a restart.
 pref("security.sandbox.windows.log", false);
 
+// Controls whether the Windows NPAPI plugin process is sandboxed by default.
+// To get a different setting for a particular plugin replace "default", with
+// the plugin's nice file name, see: nsPluginTag::GetNiceFileName.
+pref("dom.ipc.plugins.sandbox.default", false);
+pref("dom.ipc.plugins.sandbox.flash", false);
+
 #if defined(MOZ_CONTENT_SANDBOX)
 // This controls whether the Windows content process sandbox is using a more
 // strict sandboxing policy.  This will require a restart.
