50d0fc4815137a86e322853a30b2ce0ad6c36384
Markus Stange <mstange@themasta.com>
2015-01-23 13:07:51 -0500

    Bug 1104036 - Make -moz-window-dragging work in rectilinear 2d transforms. r=roc, a=lmandel

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -513,7 +513,7 @@ public:
    */
   void AdjustWindowDraggingRegion(nsIFrame* aFrame);
 
-  const nsRegion& GetWindowDraggingRegion() { return mWindowDraggingRegion; }
+  const nsIntRegion& GetWindowDraggingRegion() { return mWindowDraggingRegion; }
 
   /**
    * Allocate memory in our arena. It will only be freed when this display list
@@ -875,7 +875,7 @@ private:
   nsRect                         mDirtyRect;
   nsRegion                       mWindowExcludeGlassRegion;
   nsRegion                       mWindowOpaqueRegion;
-  nsRegion                       mWindowDraggingRegion;
+  nsIntRegion                    mWindowDraggingRegion;
   // The display item for the Windows window glass background, if any
   nsDisplayItem*                 mGlassDisplayItem;
   nsTArray<DisplayItemClip*>     mDisplayItemClipsToDestroy;
