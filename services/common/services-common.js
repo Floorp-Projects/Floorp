db70551ac00887349c6171cb6ab6153c05db3238
Mark Hammond <mhammond@skippinet.com.au>
2015-01-23 16:32:17 +1100

    Backout a7e7d68537a8 (bug 1121325) for xpcshell and bc test failures

--- services/common/services-common.js
+++ services/common/services-common.js
@@ -9,4 +9,4 @@ pref("services.common.log.logger.rest.request", "Debug");
 pref("services.common.log.logger.rest.response", "Debug");
 
 pref("services.common.storageservice.sendVersionInfo", true);
-pref("services.common.log.logger.tokenserverclient", "Debug");
+pref("services.common.tokenserverclient.logger.level", "Info");
