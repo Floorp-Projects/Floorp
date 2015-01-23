f993d8d3ca31ffbdec9a07558d784802bc5dbe28
Richard Newman <rnewman@mozilla.com>
2015-01-22 20:23:38 -0800

    Bug 1123688 - Set BrowserDB factory in application startup, not only for GeckoApp subclasses. r=bnicholson
    The issue: only by launching GeckoApp (or a subclass) did we specify what kind of DB we wanted. If we don't specify, we get a StubBrowserDB.
    
    The search activity is not a GeckoApp, but it does implicitly access a profile.
    
    The same is true for Sync: a background Sync would eventually cause the CP to touch GeckoProfile to get the profile directory.
    
    Both of these avenues will poison the GeckoProfile cache, storing a profile with a database member that's a StubBrowserDB.
    
    If you subsequently launched the browser you'd get that GeckoProfile instance from the profile cache. StubBrowserDB deliberately returns null for some queries -- such as getTopSites. That causes us to throw here.
    
    The solution I chose: have *GeckoApplication* specify which kind of DB to use, and then have WebappImpl (the only subclass for which we want different behavior) override it.
    
    GeckoView consumers remain unaffected, because they have their own Application class, and so the usual GeckoProfile fallback applies.

--- mobile/android/base/webapp/WebappImpl.java
+++ mobile/android/base/webapp/WebappImpl.java
@@ -14,6 +14,7 @@ import org.json.JSONObject;
 import org.mozilla.gecko.GeckoApp;
 import org.mozilla.gecko.GeckoAppShell;
 import org.mozilla.gecko.GeckoEvent;
+import org.mozilla.gecko.GeckoProfile;
 import org.mozilla.gecko.GeckoThread;
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.Tab;
@@ -62,14 +63,13 @@ public class WebappImpl extends GeckoApp implements InstallCallback {
     @Override
     public boolean hasTabsSideBar() { return false; }
 
-    @Override
-    public BrowserDB.Factory getBrowserDBFactory() {
-        return new BrowserDB.Factory() {
+    public WebappImpl() {
+        GeckoProfile.setBrowserDBFactory(new BrowserDB.Factory() {
             @Override
             public BrowserDB get(String profileName, File profileDir) {
                 return new StubBrowserDB(profileName);
             }
-        };
+        });
     }
 
     @Override
