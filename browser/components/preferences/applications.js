6d54bd3f21c8335777cd6d5964cf89f81d674746
Chris Peterson <cpeterson@mozilla.com>
2015-01-24 23:39:14 -0800

    Bug 1129321 - Remove nonstandard let blocks from browser. r=ttaubert

--- browser/components/preferences/applications.js
+++ browser/components/preferences/applications.js
@@ -1841,10 +1841,9 @@ var gApplicationsPane = {
         return this._getIconURLForSystemDefault(aHandlerInfo);
 
       case Ci.nsIHandlerInfo.useHelperApp:
-        let (preferredApp = aHandlerInfo.preferredApplicationHandler) {
-          if (this.isValidHandlerApp(preferredApp))
-            return this._getIconURLForHandlerApp(preferredApp);
-        }
+        let preferredApp = aHandlerInfo.preferredApplicationHandler;
+        if (this.isValidHandlerApp(preferredApp))
+          return this._getIconURLForHandlerApp(preferredApp);
         break;
 
       // This should never happen, but if preferredAction is set to some weird
