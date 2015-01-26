18d71ee716727a7451d250dd135dfdbaece499e7
Aaron Klotz <aklotz@mozilla.com>
2015-01-26 14:54:50 -0700

    Bug 1121162 - Resume stream listener when async NPP_NewStream returns with failure. r=jimm, a=lmandel

--- dom/plugins/ipc/BrowserStreamParent.cpp
+++ dom/plugins/ipc/BrowserStreamParent.cpp
@@ -72,6 +72,9 @@ BrowserStreamParent::RecvAsyncNPP_NewStreamResult(const NPError& rv,
   }
 
   if (error != NPERR_NO_ERROR) {
+    // streamListener was suspended during async init. We must resume the stream
+    // request prior to calling _destroystream for cleanup to work correctly.
+    streamListener->ResumeRequest();
     // We need to clean up the stream
     parent::_destroystream(mNPP->GetNPP(), mStream, NPRES_DONE);
     unused << PBrowserStreamParent::Send__delete__(this);
