6efe155ae56ce850596b166d61d6bf99c2153d47
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-01-27 02:01:09 +1100

    Bug 1102642 - Use ref counted compressed data within mediasource. r=mattwoodrow, a=sledru

--- dom/media/mediasource/ContainerParser.cpp
+++ dom/media/mediasource/ContainerParser.cpp
@@ -8,9 +8,11 @@
 
 #include "WebMBufferedParser.h"
 #include "mozilla/Endian.h"
-#include "mp4_demuxer/BufferStream.h"
 #include "mp4_demuxer/MoofParser.h"
 #include "prlog.h"
+#include "MediaData.h"
+#include "MP4Stream.h"
+#include "SourceBufferResource.h"
 
 #ifdef PR_LOGGING
 extern PRLogModuleInfo* GetMediaSourceLog();
@@ -27,32 +29,38 @@ extern PRLogModuleInfo* GetMediaSourceAPILog();
 
 namespace mozilla {
 
+ContainerParser::ContainerParser()
+  : mInitData(new LargeDataBuffer())
+  , mHasInitData(false)
+{
+}
+
 bool
-ContainerParser::IsInitSegmentPresent(const uint8_t* aData, uint32_t aLength)
+ContainerParser::IsInitSegmentPresent(LargeDataBuffer* aData)
 {
 MSE_DEBUG("ContainerParser(%p)::IsInitSegmentPresent aLength=%u [%x%x%x%x]",
-            this, aLength,
-            aLength > 0 ? aData[0] : 0,
-            aLength > 1 ? aData[1] : 0,
-            aLength > 2 ? aData[2] : 0,
-            aLength > 3 ? aData[3] : 0);
+            this, aData->Length(),
+            aData->Length() > 0 ? (*aData)[0] : 0,
+            aData->Length() > 1 ? (*aData)[1] : 0,
+            aData->Length() > 2 ? (*aData)[2] : 0,
+            aData->Length() > 3 ? (*aData)[3] : 0);
 return false;
 }
 
 bool
-ContainerParser::IsMediaSegmentPresent(const uint8_t* aData, uint32_t aLength)
+ContainerParser::IsMediaSegmentPresent(LargeDataBuffer* aData)
 {
   MSE_DEBUG("ContainerParser(%p)::IsMediaSegmentPresent aLength=%u [%x%x%x%x]",
-            this, aLength,
-            aLength > 0 ? aData[0] : 0,
-            aLength > 1 ? aData[1] : 0,
-            aLength > 2 ? aData[2] : 0,
-            aLength > 3 ? aData[3] : 0);
+            this, aData->Length(),
+            aData->Length() > 0 ? (*aData)[0] : 0,
+            aData->Length() > 1 ? (*aData)[1] : 0,
+            aData->Length() > 2 ? (*aData)[2] : 0,
+            aData->Length() > 3 ? (*aData)[3] : 0);
   return false;
 }
 
 bool
-ContainerParser::ParseStartAndEndTimestamps(const uint8_t* aData, uint32_t aLength,
+ContainerParser::ParseStartAndEndTimestamps(LargeDataBuffer* aData,
                                             int64_t& aStart, int64_t& aEnd)
 {
   return false;
@@ -71,7 +79,7 @@ ContainerParser::GetRoundingError()
   return 0;
 }
 
-const nsTArray<uint8_t>&
+LargeDataBuffer*
 ContainerParser::InitData()
 {
   MOZ_ASSERT(mHasInitData);
@@ -87,9 +95,9 @@ public:
   static const unsigned NS_PER_USEC = 1000;
   static const unsigned USEC_PER_SEC = 1000000;
 
-  bool IsInitSegmentPresent(const uint8_t* aData, uint32_t aLength)
+  bool IsInitSegmentPresent(LargeDataBuffer* aData)
   {
-    ContainerParser::IsInitSegmentPresent(aData, aLength);
+    ContainerParser::IsInitSegmentPresent(aData);
     // XXX: This is overly primitive, needs to collect data as it's appended
     // to the SB and handle, rather than assuming everything is present in a
     // single aData segment.
@@ -100,16 +108,17 @@ public:
     // 0x18538067 // Segment (must be "unknown" size)
     // 0x1549a966 // -> Segment Info
     // 0x1654ae6b // -> One or more Tracks
-    if (aLength >= 4 &&
-        aData[0] == 0x1a && aData[1] == 0x45 && aData[2] == 0xdf && aData[3] == 0xa3) {
+    if (aData->Length() >= 4 &&
+        (*aData)[0] == 0x1a && (*aData)[1] == 0x45 && (*aData)[2] == 0xdf &&
+        (*aData)[3] == 0xa3) {
       return true;
     }
     return false;
   }
 
-  bool IsMediaSegmentPresent(const uint8_t* aData, uint32_t aLength)
+  bool IsMediaSegmentPresent(LargeDataBuffer* aData)
   {
-    ContainerParser::IsMediaSegmentPresent(aData, aLength);
+    ContainerParser::IsMediaSegmentPresent(aData);
     // XXX: This is overly primitive, needs to collect data as it's appended
     // to the SB and handle, rather than assuming everything is present in a
     // single aData segment.
@@ -120,17 +129,18 @@ public:
     // 0x18538067 // Segment (must be "unknown" size)
     // 0x1549a966 // -> Segment Info
     // 0x1654ae6b // -> One or more Tracks
-    if (aLength >= 4 &&
-        aData[0] == 0x1f && aData[1] == 0x43 && aData[2] == 0xb6 && aData[3] == 0x75) {
+    if (aData->Length() >= 4 &&
+        (*aData)[0] == 0x1f && (*aData)[1] == 0x43 && (*aData)[2] == 0xb6 &&
+        (*aData)[3] == 0x75) {
       return true;
     }
     return false;
   }
 
-  bool ParseStartAndEndTimestamps(const uint8_t* aData, uint32_t aLength,
+  bool ParseStartAndEndTimestamps(LargeDataBuffer* aData,
                                   int64_t& aStart, int64_t& aEnd)
   {
-    bool initSegment = IsInitSegmentPresent(aData, aLength);
+    bool initSegment = IsInitSegmentPresent(aData);
     if (initSegment) {
       mOffset = 0;
       mParser = WebMBufferedParser(0);
@@ -143,24 +153,27 @@ public:
     mapping.AppendElements(mOverlappedMapping);
     mOverlappedMapping.Clear();
     ReentrantMonitor dummy("dummy");
-    mParser.Append(aData, aLength, mapping, dummy);
+    mParser.Append(aData->Elements(), aData->Length(), mapping, dummy);
 
     // XXX This is a bit of a hack.  Assume if there are no timecodes
     // present and it's an init segment that it's _just_ an init segment.
     // We should be more precise.
     if (initSegment) {
-      uint32_t length = aLength;
+      uint32_t length = aData->Length();
       if (!mapping.IsEmpty()) {
         length = mapping[0].mSyncOffset;
-        MOZ_ASSERT(length <= aLength);
+        MOZ_ASSERT(length <= aData->Length());
       }
       MSE_DEBUG("WebMContainerParser(%p)::ParseStartAndEndTimestamps: Stashed init of %u bytes.",
                 this, length);
-
-      mInitData.ReplaceElementsAt(0, mInitData.Length(), aData, length);
+      if (!mInitData->ReplaceElementsAt(0, mInitData->Length(),
+                                        aData->Elements(), length)) {
+        // Unlikely OOM
+        return false;
+      }
       mHasInitData = true;
     }
-    mOffset += aLength;
+    mOffset += aData->Length();
 
     if (mapping.IsEmpty()) {
       return false;
@@ -205,50 +218,53 @@ class MP4ContainerParser : public ContainerParser {
 public:
   MP4ContainerParser() :mMonitor("MP4ContainerParser Index Monitor") {}
 
-  bool IsInitSegmentPresent(const uint8_t* aData, uint32_t aLength)
+  bool IsInitSegmentPresent(LargeDataBuffer* aData)
   {
-    ContainerParser::IsInitSegmentPresent(aData, aLength);
+    ContainerParser::IsInitSegmentPresent(aData);
     // Each MP4 atom has a chunk size and chunk type. The root chunk in an MP4
     // file is the 'ftyp' atom followed by a file type. We just check for a
     // vaguely valid 'ftyp' atom.
 
-    if (aLength < 8) {
+    if (aData->Length() < 8) {
       return false;
     }
 
-    uint32_t chunk_size = BigEndian::readUint32(aData);
+    uint32_t chunk_size = BigEndian::readUint32(aData->Elements());
     if (chunk_size < 8) {
       return false;
     }
 
-    return aData[4] == 'f' && aData[5] == 't' && aData[6] == 'y' &&
-           aData[7] == 'p';
+    return (*aData)[4] == 'f' && (*aData)[5] == 't' && (*aData)[6] == 'y' &&
+           (*aData)[7] == 'p';
   }
 
-  bool IsMediaSegmentPresent(const uint8_t* aData, uint32_t aLength)
+  bool IsMediaSegmentPresent(LargeDataBuffer* aData)
   {
-    ContainerParser::IsMediaSegmentPresent(aData, aLength);
-    if (aLength < 8) {
+    ContainerParser::IsMediaSegmentPresent(aData);
+    if (aData->Length() < 8) {
       return false;
     }
 
-    uint32_t chunk_size = BigEndian::readUint32(aData);
+    uint32_t chunk_size = BigEndian::readUint32(aData->Elements());
     if (chunk_size < 8) {
       return false;
     }
 
-    return (aData[4] == 'm' && aData[5] == 'o' && aData[6] == 'o' && aData[7] == 'f') ||
-           (aData[4] == 's' && aData[5] == 't' && aData[6] == 'y' && aData[7] == 'p');
+    return ((*aData)[4] == 'm' && (*aData)[5] == 'o' && (*aData)[6] == 'o' &&
+            (*aData)[7] == 'f') ||
+           ((*aData)[4] == 's' && (*aData)[5] == 't' && (*aData)[6] == 'y' &&
+            (*aData)[7] == 'p');
   }
 
-  bool ParseStartAndEndTimestamps(const uint8_t* aData, uint32_t aLength,
+  bool ParseStartAndEndTimestamps(LargeDataBuffer* aData,
                                   int64_t& aStart, int64_t& aEnd)
   {
     MonitorAutoLock mon(mMonitor); // We're not actually racing against anything,
                                    // but mParser requires us to hold a monitor.
-    bool initSegment = IsInitSegmentPresent(aData, aLength);
+    bool initSegment = IsInitSegmentPresent(aData);
     if (initSegment) {
-      mStream = new mp4_demuxer::BufferStream();
+      mResource = new SourceBufferResource(NS_LITERAL_CSTRING("video/mp4"));
+      mStream = new MP4Stream(mResource);
       // We use a timestampOffset of 0 for ContainerParser, and require
       // consumers of ParseStartAndEndTimestamps to add their timestamp offset
       // manually. This allows the ContainerParser to be shared across different
@@ -258,9 +274,11 @@ public:
       return false;
     }
 
-    mStream->AppendBytes(aData, aLength);
+    mResource->AppendData(aData);
     nsTArray<MediaByteRange> byteRanges;
-    byteRanges.AppendElement(mStream->GetByteRange());
+    MediaByteRange mbr =
+      MediaByteRange(mParser->mOffset, mResource->GetLength());
+    byteRanges.AppendElement(mbr);
     mParser->RebuildFragmentedIndex(byteRanges);
 
     if (initSegment) {
@@ -268,16 +286,18 @@ public:
       MSE_DEBUG("MP4ContainerParser(%p)::ParseStartAndEndTimestamps: Stashed init of %u bytes.",
                 this, range.mEnd - range.mStart);
 
-      mInitData.ReplaceElementsAt(0, mInitData.Length(),
-                                  aData + range.mStart,
-                                  range.mEnd - range.mStart);
+      if (!mInitData->ReplaceElementsAt(0, mInitData->Length(),
+                                        aData->Elements() + range.mStart,
+                                        range.mEnd - range.mStart)) {
+        // Super unlikely OOM
+        return false;
+      }
       mHasInitData = true;
     }
 
     mp4_demuxer::Interval<mp4_demuxer::Microseconds> compositionRange =
       mParser->GetCompositionRange(byteRanges);
-
-    mStream->DiscardBefore(mParser->mOffset);
+    mResource->EvictData(mParser->mOffset, mParser->mOffset);
 
     if (compositionRange.IsNull()) {
       return false;
@@ -297,8 +317,9 @@ public:
   }
 
 private:
-  nsRefPtr<mp4_demuxer::BufferStream> mStream;
+  nsRefPtr<MP4Stream> mStream;
   nsAutoPtr<mp4_demuxer::MoofParser> mParser;
+  nsRefPtr<SourceBufferResource> mResource;
   Monitor mMonitor;
 };
 
