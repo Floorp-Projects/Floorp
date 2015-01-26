9acf1460514e67143b980d80fa30de5fd0690d32
Andrew Comminos <andrew@morlunk.com>
2015-01-26 17:21:00 +0100

    Bug 1126094 - Adds high resolution display support to GdkScreen. r=karlt

--- widget/gtk/nsScreenGtk.cpp
+++ widget/gtk/nsScreenGtk.cpp
@@ -11,6 +11,7 @@
 #include <X11/Xatom.h>
 #endif
 #include <gtk/gtk.h>
+#include <dlfcn.h>
 
 static uint32_t sScreenId = 0;
 
@@ -85,11 +86,26 @@ nsScreenGtk :: GetColorDepth(int32_t *aColorDepth)
 void
 nsScreenGtk :: Init (GdkWindow *aRootWindow)
 {
+  gint width = gdk_screen_width();
+  gint height = gdk_screen_height();
+
+  // Since GDK 3.10
+  static auto sGdkScreenGetMonitorScaleFactorPtr = (gint (*)(GdkScreen*, gint))
+      dlsym(RTLD_DEFAULT, "gdk_screen_get_monitor_scale_factor");
+  if (sGdkScreenGetMonitorScaleFactorPtr) {
+      // FIXME: In the future, we'll want to fix this for GTK on Wayland which
+      // supports a variable scale factor per display.
+      GdkScreen *screen = gdk_window_get_screen(aRootWindow);
+      gint scale = sGdkScreenGetMonitorScaleFactorPtr(screen, 0);
+      width *= scale;
+      height *= scale;
+  }
+
   // We listen for configure events on the root window to pick up
   // changes to this rect.  We could listen for "size_changed" signals
   // on the default screen to do this, except that doesn't work with
   // versions of GDK predating the GdkScreen object.  See bug 256646.
-  mAvailRect = mRect = nsIntRect(0, 0, gdk_screen_width(), gdk_screen_height());
+  mAvailRect = mRect = nsIntRect(0, 0, width, height);
 
 #ifdef MOZ_X11
   // We need to account for the taskbar, etc in the available rect.
