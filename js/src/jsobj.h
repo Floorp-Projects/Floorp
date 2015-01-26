aa87c9002d234521fb67b76a69532e26fa8c544b
Brian Hackett <bhackett1024@gmail.com>
2015-01-25 17:00:18 -0700

    Bug 1116855 - Add default-disabled unboxed objects for use by interpreted constructors, r=jandem.

--- js/src/jsobj.h
+++ js/src/jsobj.h
@@ -599,6 +599,9 @@ class JSObject : public js::gc::Cell
     static size_t offsetOfType() { return offsetof(JSObject, type_); }
     js::HeapPtrTypeObject *addressOfType() { return &type_; }
 
+    // Maximum size in bytes of a JSObject.
+    static const size_t MAX_BYTE_SIZE = 4 * sizeof(void *) + 16 * sizeof(JS::Value);
+
   private:
     JSObject() = delete;
     JSObject(const JSObject &other) = delete;
@@ -1154,12 +1157,12 @@ GetInitialHeap(NewObjectKind newKind, const Class *clasp)
 
 // Specialized call for constructing |this| with a known function callee,
 // and a known prototype.
-extern PlainObject *
+extern JSObject *
 CreateThisForFunctionWithProto(JSContext *cx, js::HandleObject callee, JSObject *proto,
                                NewObjectKind newKind = GenericObject);
 
 // Specialized call for constructing |this| with a known function callee.
-extern PlainObject *
+extern JSObject *
 CreateThisForFunction(JSContext *cx, js::HandleObject callee, NewObjectKind newKind);
 
 // Generic call for constructing |this|.
