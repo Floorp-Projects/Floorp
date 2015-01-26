ee87cb155e468a66ac0a4f4c7b822f785bc618e2
Brian Hackett <bhackett1024@gmail.com>
2015-01-26 13:14:34 -0500

    Bug 1124018 - Null the allocation site table if initialization fails. r=jonco, a=abillings

--- js/src/jsinfer.cpp
+++ js/src/jsinfer.cpp
@@ -2232,6 +2232,7 @@ TypeCompartment::addAllocationSiteTypeObject(JSContext *cx, AllocationSiteKey ke
         allocationSiteTable = cx->new_<AllocationSiteTable>();
         if (!allocationSiteTable || !allocationSiteTable->init()) {
             js_delete(allocationSiteTable);
+            allocationSiteTable = nullptr;
             return nullptr;
         }
     }
