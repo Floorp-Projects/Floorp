bea951dedd87ea13aa8aa71637386bb454b1bf0e
Mike Conley <mconley@mozilla.com>
2015-01-27 18:32:40 -0500

    Bug 1077168 - Make WebappManager.jsm use the top window id to select which browser to show notifications in. r=felipe.

--- toolkit/content/browser-child.js
+++ toolkit/content/browser-child.js
@@ -588,7 +588,10 @@ let AutoCompletePopup = {
 
 // We may not get any responses to Browser:Init if the browser element
 // is torn down too quickly.
-let initData = sendSyncMessage("Browser:Init");
+let outerWindowID = content.QueryInterface(Ci.nsIInterfaceRequestor)
+                           .getInterface(Ci.nsIDOMWindowUtils)
+                           .outerWindowID;
+let initData = sendSyncMessage("Browser:Init", {outerWindowID: outerWindowID});
 if (initData.length) {
   docShell.useGlobalHistory = initData[0].useGlobalHistory;
   if (initData[0].initPopup) {
