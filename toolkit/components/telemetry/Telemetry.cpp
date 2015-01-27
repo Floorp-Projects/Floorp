7ed9dc5e302c2c545ed173c17c387de4f6e4e146
Aaron Klotz <aklotz@mozilla.com>
2015-01-27 13:59:49 -0700

    Bug 1122602: Correctly handle submission of empty chrome hang objects; r=vladan

--- toolkit/components/telemetry/Telemetry.cpp
+++ toolkit/components/telemetry/Telemetry.cpp
@@ -3211,11 +3211,17 @@ TelemetryImpl::RecordChromeHang(uint32_t aDuration,
   if (!sTelemetry || !sTelemetry->mCanRecord)
     return;
 
+  UniquePtr<HangAnnotations> annotations;
+  // We only pass aAnnotations if it is not empty.
+  if (aAnnotations && !aAnnotations->IsEmpty()) {
+    annotations = Move(aAnnotations);
+  }
+
   MutexAutoLock hangReportMutex(sTelemetry->mHangReportsMutex);
 
   sTelemetry->mHangReports.AddHang(aStack, aDuration,
                                    aSystemUptime, aFirefoxUptime,
-                                   Move(aAnnotations));
+                                   Move(annotations));
 }
 #endif
 
