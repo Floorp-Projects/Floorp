d1be33bfc20f50d027843c56e792288fe9f37272
Ehsan Akhgari <ehsan@mozilla.com>
2015-01-26 21:09:59 -0500

    Bug 1117034 - Mark some overridden functions in the tree as override

--- toolkit/system/dbus/nsNetworkManagerListener.h
+++ toolkit/system/dbus/nsNetworkManagerListener.h
@@ -20,9 +20,9 @@ public:
 
   nsresult Init();
 
-  virtual void RegisterWithConnection(DBusConnection* connection);
-  virtual void UnregisterWithConnection(DBusConnection* connection);
-  virtual bool HandleMessage(DBusMessage* msg);
+  virtual void RegisterWithConnection(DBusConnection* connection) MOZ_OVERRIDE;
+  virtual void UnregisterWithConnection(DBusConnection* connection) MOZ_OVERRIDE;
+  virtual bool HandleMessage(DBusMessage* msg) MOZ_OVERRIDE;
 
   /**
    * This gets called when NetworkManager sends us a StateChange signal,
