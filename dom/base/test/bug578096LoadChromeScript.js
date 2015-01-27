e64e389f2a85f00ddf72ef3b63909d667cf711de
Jed Davis <jld@mozilla.com>
2015-01-26 16:23:00 -0800

    Bug 1068838 - Fix file input element tests to open files in the parent process. r=sicking
    The dom/base tests changed here use SpecialPowers.loadChromeScript to
    construct a DOM File in the parent and send it to the child, using the
    existing remote-blob infrastructure.  The dom/html tests don't need a
    real file, so they now construct in-memory files.
    
    As a convenient side-effect, these tests are now enabled for desktop
    e10s (they were already being run out-of-process on B2G); most of them
    failed before this change due to the code that's being moved/removed.

--- dom/base/test/bug578096LoadChromeScript.js
+++ dom/base/test/bug578096LoadChromeScript.js
@@ -1,11 +1,13 @@
 var file;
+Components.utils.importGlobalProperties(["File"]);
+
 addMessageListener("file.create", function (message) {
   file = Components.classes["@mozilla.org/file/directory_service;1"]
              .getService(Components.interfaces.nsIProperties)
              .get("TmpD", Components.interfaces.nsIFile);
   file.append("foo.txt");
   file.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0600);
-  sendAsyncMessage("file.path", file.path);
+  sendAsyncMessage("file.created", new File(file));
 });
 
 addMessageListener("file.remove", function (message) {
