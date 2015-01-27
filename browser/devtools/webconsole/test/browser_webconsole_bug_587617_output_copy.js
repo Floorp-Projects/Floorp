d9b1a2504500800f1593d1234343260a28bfa51d
Brian Grinstead <bgrinstead@mozilla.com>
2015-01-27 13:26:11 -0500

    Bug 1119503 - Part 5: Make children of message-body class be preformatted to fix copied text of line number showing up on new line; r=past

--- browser/devtools/webconsole/test/browser_webconsole_bug_587617_output_copy.js
+++ browser/devtools/webconsole/test/browser_webconsole_bug_587617_output_copy.js
@@ -57,7 +57,9 @@ function consoleOpened(aHud) {
     controller = top.document.commandDispatcher.getControllerForCommand("cmd_copy");
     is(controller.isCommandEnabled("cmd_copy"), true, "cmd_copy is enabled");
 
-    let selection = HUD.iframeWindow.getSelection() + "";
+    // Remove new lines since getSelection() includes one between message and line
+    // number, but the clipboard doesn't (see bug 1119503)
+    let selection = (HUD.iframeWindow.getSelection() + "").replace(/\r?\n|\r/g, " ");
     isnot(selection.indexOf("bug587617"), -1,
           "selection text includes 'bug587617'");
 
@@ -80,7 +82,9 @@ function testContextMenuCopy() {
   let copyItem = contextMenu.querySelector("*[command='cmd_copy']");
   ok(copyItem, "the context menu on the output node has a \"Copy\" item");
 
-  let selection = HUD.iframeWindow.getSelection() + "";
+  // Remove new lines since getSelection() includes one between message and line
+  // number, but the clipboard doesn't (see bug 1119503)
+  let selection = (HUD.iframeWindow.getSelection() + "").replace(/\r?\n|\r/g, " ");
 
   copyItem.doCommand();
 
