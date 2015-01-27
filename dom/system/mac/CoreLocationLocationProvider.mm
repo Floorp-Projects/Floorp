ece1bd7793e8db0a1b75a547877241104a5cba7d
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-01-27 09:16:54 +0100

    Backed out changeset cc42921d3f36 (bug 1121497)

--- dom/system/mac/CoreLocationLocationProvider.mm
+++ dom/system/mac/CoreLocationLocationProvider.mm
@@ -211,8 +211,6 @@ CoreLocationLocationProvider::Startup()
     mCLObjects = clObjs.forget();
   }
 
-  // Must be stopped before starting or response (success or failure) is not guaranteed
-  [mCLObjects->mLocationManager stopUpdatingLocation];
   [mCLObjects->mLocationManager startUpdatingLocation];
   return NS_OK;
 }
