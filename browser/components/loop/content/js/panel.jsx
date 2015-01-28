a011b7e263bf5a78154a233ad342d796d80b3d63
Mark Banner <standard8@mozilla.com>
2015-01-28 16:43:31 +0000

    Bug 1066166 - Add localized tooltip captions for each tab button on the Loop panel. r=jaws

--- browser/components/loop/content/js/panel.jsx
+++ browser/components/loop/content/js/panel.jsx
@@ -67,6 +67,7 @@ loop.panel = (function(_, mozL10n) {
             <li className={cx({selected: isSelected})}
                 key={i}
                 data-tab-name={tabName}
+                title={mozL10n.get(tabName + "_tab_button_tooltip")}
                 onClick={this.handleSelectTab} />
           );
         }
