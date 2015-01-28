744968efa02f143ead6952a10d1e8b140c03e471
Ted Mielczarek <ted@mielczarek.org>
2015-01-28 15:13:24 -0500

    bug 1170584 - fix PluginMessageUtils on iOS. r=jimm
    --HG--
    extra : commitid : DH7ZYdphBB5
    extra : rebase_source : 4fbc43d53e807eea7f8951fa7a935ad4b05bb603

--- dom/plugins/ipc/PluginMessageUtils.h
+++ dom/plugins/ipc/PluginMessageUtils.h
@@ -105,7 +105,7 @@ struct NPRemoteWindow
 typedef HWND NativeWindowHandle;
 #elif defined(MOZ_X11)
 typedef XID NativeWindowHandle;
-#elif defined(XP_MACOSX) || defined(ANDROID) || defined(MOZ_WIDGET_QT)
+#elif defined(XP_DARWIN) || defined(ANDROID) || defined(MOZ_WIDGET_QT)
 typedef intptr_t NativeWindowHandle; // never actually used, will always be 0
 #else
 #error Need NativeWindowHandle for this platform
