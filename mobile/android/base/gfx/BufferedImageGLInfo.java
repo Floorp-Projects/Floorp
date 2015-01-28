1a28e149a243a2c67373d6f9bedc7d11cad07349
Jeff Muizelaar <jmuizelaar@mozilla.com>
2015-01-28 17:04:50 -0500

    Bug 1127005. Rename CairoImage to BufferedImage. r=kats
    try: -b do -p android-api-9,android-api-11,android-x86 -u all -t none

--- mobile/android/base/gfx/BufferedImageGLInfo.java
+++ mobile/android/base/gfx/BufferedImageGLInfo.java
@@ -7,28 +7,28 @@ package org.mozilla.gecko.gfx;
 
 import javax.microedition.khronos.opengles.GL10;
 
-/** Information needed to render Cairo bitmaps using OpenGL ES. */
-public class CairoGLInfo {
+/** Information needed to render buffered bitmaps using OpenGL ES. */
+public class BufferedImageGLInfo {
     public final int internalFormat;
     public final int format;
     public final int type;
 
-    public CairoGLInfo(int cairoFormat) {
-        switch (cairoFormat) {
-        case CairoImage.FORMAT_ARGB32:
+    public BufferedImageGLInfo(int bufferedImageFormat) {
+        switch (bufferedImageFormat) {
+        case BufferedImage.FORMAT_ARGB32:
             internalFormat = format = GL10.GL_RGBA; type = GL10.GL_UNSIGNED_BYTE;
             break;
-        case CairoImage.FORMAT_RGB24:
+        case BufferedImage.FORMAT_RGB24:
             internalFormat = format = GL10.GL_RGB; type = GL10.GL_UNSIGNED_BYTE;
             break;
-        case CairoImage.FORMAT_RGB16_565:
+        case BufferedImage.FORMAT_RGB16_565:
             internalFormat = format = GL10.GL_RGB; type = GL10.GL_UNSIGNED_SHORT_5_6_5;
             break;
-        case CairoImage.FORMAT_A8:
-        case CairoImage.FORMAT_A1:
-            throw new RuntimeException("Cairo FORMAT_A1 and FORMAT_A8 unsupported");
+        case BufferedImage.FORMAT_A8:
+        case BufferedImage.FORMAT_A1:
+            throw new RuntimeException("BufferedImage FORMAT_A1 and FORMAT_A8 unsupported");
         default:
-            throw new RuntimeException("Unknown Cairo format");
+            throw new RuntimeException("Unknown BufferedImage format");
         }
     }
 }
