24103f83c7e920a5dc4b404efb78befab32a70e7
Jim Mathies <jmathies@mozilla.com>
2015-01-28 09:15:54 -0600

    Bug 1126164 - Avoid setting clip regions on plugin windows if the same clip region was already set. r=roc, a=sledru

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -6541,6 +6541,10 @@ nsresult
 nsWindow::SetWindowClipRegion(const nsTArray<nsIntRect>& aRects,
                               bool aIntersectWithExisting)
 {
+  if (IsWindowClipRegionEqual(aRects)) {
+    return NS_OK;
+  }
+
   nsBaseWidget::SetWindowClipRegion(aRects, aIntersectWithExisting);
 
   HRGN dest = CreateHRGNFromArray(aRects);
