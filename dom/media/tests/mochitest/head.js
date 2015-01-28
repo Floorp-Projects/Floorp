89130381feca926b2455ac3e8f22ea1b69673599
Wes Kocher <wkocher@mozilla.com>
2015-01-27 16:56:02 -0800

    Backed out 12 changesets (bug 1118398, bug 1119593) for zmedia failures
    Backed out changeset db7c4ffd5a53 (bug 1119593)
    Backed out changeset 2832633668c3 (bug 1119593)
    Backed out changeset 2f38bbb30cc6 (bug 1119593)
    Backed out changeset 46b5bb0665e9 (bug 1119593)
    Backed out changeset e97ee52f7803 (bug 1119593)
    Backed out changeset f7f0adc1879d (bug 1119593)
    Backed out changeset 4ccecd0da80f (bug 1119593)
    Backed out changeset 2ea753b42366 (bug 1119593)
    Backed out changeset 28d59c42debb (bug 1119593)
    Backed out changeset a38d68f15ced (bug 1119593)
    Backed out changeset 1d565fc1473c (bug 1119593)
    Backed out changeset d28a04536e7e (bug 1118398)

--- dom/media/tests/mochitest/head.js
+++ dom/media/tests/mochitest/head.js
@@ -2,10 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-"use strict";
-
 var Cc = SpecialPowers.Cc;
 var Ci = SpecialPowers.Ci;
+var Cr = SpecialPowers.Cr;
 
 // Specifies whether we are using fake streams to run this automation
 var FAKE_ENABLED = true;
@@ -33,7 +32,7 @@ try {
  * @param {boolean} [meta.visible=false]
  *        Visibility of the media elements
  */
-function realCreateHTML(meta) {
+function createHTML(meta) {
   var test = document.getElementById('test');
 
   // Create the head content
@@ -47,13 +46,13 @@ function realCreateHTML(meta) {
 
   // Create the body content
   var anchor = document.createElement('a');
-  anchor.textContent = meta.title;
+  anchor.setAttribute('target', '_blank');
+
   if (meta.bug) {
     anchor.setAttribute('href', 'https://bugzilla.mozilla.org/show_bug.cgi?id=' + meta.bug);
-  } else {
-    anchor.setAttribute('target', '_blank');
   }
 
+  anchor.textContent = meta.title;
   document.body.insertBefore(anchor, test);
 
   var display = document.createElement('p');
@@ -82,16 +81,14 @@ function createMediaElement(type, label) {
   var element = document.getElementById(id);
 
   // Sanity check that we haven't created the element already
-  if (element) {
+  if (element)
     return element;
-  }
 
   element = document.createElement(type === 'audio' ? 'audio' : 'video');
   element.setAttribute('id', id);
   element.setAttribute('height', 100);
   element.setAttribute('width', 150);
   element.setAttribute('controls', 'controls');
-  element.setAttribute('autoplay', 'autoplay');
   document.getElementById('content').appendChild(element);
 
   return element;
@@ -104,32 +101,35 @@ function createMediaElement(type, label) {
  *
  * @param {Dictionary} constraints
  *        The constraints for this mozGetUserMedia callback
+ * @param {Function} onSuccess
+ *        The success callback if the stream is successfully retrieved
+ * @param {Function} onError
+ *        The error callback if the stream fails to be retrieved
  */
-function getUserMedia(constraints) {
+function getUserMedia(constraints, onSuccess, onError) {
   if (!("fake" in constraints) && FAKE_ENABLED) {
     constraints["fake"] = FAKE_ENABLED;
   }
 
   info("Call getUserMedia for " + JSON.stringify(constraints));
-  return navigator.mediaDevices.getUserMedia(constraints);
+  navigator.mozGetUserMedia(constraints, onSuccess, onError);
 }
 
-// These are the promises we use to track that the prerequisites for the test
-// are in place before running it.  Users of this file need to ensure that they
-// also provide a promise called `scriptsReady` as well.
-var setTestOptions;
-var testConfigured = new Promise(r => setTestOptions = r);
 
-function setupEnvironment() {
-  if (!window.SimpleTest) {
-    return Promise.resolve();
-  }
-
-  // Running as a Mochitest.
-  SimpleTest.requestFlakyTimeout("WebRTC inherently depends on timeouts");
-  window.finish = () => SimpleTest.finish();
-  SpecialPowers.pushPrefEnv({
-    'set': [
+/**
+ * Setup any Mochitest for WebRTC by enabling the preference for
+ * peer connections. As by bug 797979 it will also enable mozGetUserMedia()
+ * and disable the mozGetUserMedia() permission checking.
+ *
+ * @param {Function} aCallback
+ *        Test method to execute after initialization
+ */
+function runTest(aCallback) {
+  if (window.SimpleTest) {
+    // Running as a Mochitest.
+    SimpleTest.waitForExplicitFinish();
+    SimpleTest.requestFlakyTimeout("WebRTC inherently depends on timeouts");
+    SpecialPowers.pushPrefEnv({'set': [
       ['dom.messageChannel.enabled', true],
       ['media.peerconnection.enabled', true],
       ['media.peerconnection.identity.enabled', true],
@@ -137,38 +137,28 @@ function setupEnvironment() {
       ['media.peerconnection.default_iceservers', '[]'],
       ['media.navigator.permission.disabled', true],
       ['media.getusermedia.screensharing.enabled', true],
-      ['media.getusermedia.screensharing.allowed_domains', "mochi.test"]
-    ]
-  }, setTestOptions);
+      ['media.getusermedia.screensharing.allowed_domains', "mochi.test"]]
+    }, function () {
+      try {
+        aCallback();
+      }
+      catch (err) {
+        generateErrorCallback()(err);
+      }
+    });
+  } else {
+    // Steeplechase, let it call the callback.
+    window.run_test = function(is_initiator) {
+      var options = {is_local: is_initiator,
+                     is_remote: !is_initiator};
+      aCallback(options);
+    };
+    // Also load the steeplechase test code.
+    var s = document.createElement("script");
+    s.src = "/test.js";
+    document.head.appendChild(s);
+  }
 }
-
-// This is called by steeplechase; which provides the test configuration options
-// directly to the test through this function.  If we're not on steeplechase,
-// the test is configured directly and immediately.
-function run_test(is_initiator) {
-  var options = { is_local: is_initiator,
-                  is_remote: !is_initiator };
-
-  // Also load the steeplechase test code.
-  var s = document.createElement("script");
-  s.src = "/test.js";
-  s.onload = () => setTestOptions(options);
-  document.head.appendChild(s);
-}
-
-function runTestWhenReady(testFunc) {
-  setupEnvironment();
-  return Promise.all([scriptsReady, testConfigured]).then(() => {
-    try {
-      return testConfigured.then(options => testFunc(options));
-    } catch (e) {
-      ok(false, 'Error executing test: ' + e +
-         ((typeof e.stack === 'string') ?
-          (' ' + e.stack.split('\n').join(' ... ')) : ''));
-    }
-  });
-}
-
 
 /**
  * Checks that the media stream tracks have the expected amount of tracks
@@ -181,10 +171,10 @@ function runTestWhenReady(testFunc) {
  *                                     tracks being checked
  */
 function checkMediaStreamTracksByType(constraints, type, mediaStreamTracks) {
-  if (constraints[type]) {
+  if(constraints[type]) {
     is(mediaStreamTracks.length, 1, 'One ' + type + ' track shall be present');
 
-    if (mediaStreamTracks.length) {
+    if(mediaStreamTracks.length) {
       is(mediaStreamTracks[0].kind, type, 'Track kind should be ' + type);
       ok(mediaStreamTracks[0].id, 'Track id should be defined');
     }
@@ -208,26 +198,28 @@ function checkMediaStreamTracks(constraints, mediaStream) {
     mediaStream.getVideoTracks());
 }
 
-/*** Utility methods */
+/**
+ * Utility methods
+ */
 
-/** The dreadful setTimeout, use sparingly */
-function wait(time) {
-  return new Promise(r => setTimeout(r, time));
+/**
+ * Returns the contents of a blob as text
+ *
+ * @param {Blob} blob
+          The blob to retrieve the contents from
+ * @param {Function} onSuccess
+          Callback with the blobs content as parameter
+ */
+function getBlobContent(blob, onSuccess) {
+  var reader = new FileReader();
+
+  // Listen for 'onloadend' which will always be called after a success or failure
+  reader.onloadend = function (event) {
+    onSuccess(event.target.result);
+  };
+
+  reader.readAsText(blob);
 }
-
-/** The even more dreadful setInterval, use even more sparingly */
-function waitUntil(func, time) {
-  return new Promise(resolve => {
-    var interval = setInterval(() => {
-      if (func())  {
-        clearInterval(interval);
-        resolve();
-      }
-    }, time || 200);
-  });
-}
-
-/*** Test control flow methods */
 
 /**
  * Generates a callback function fired only under unexpected circumstances
@@ -246,7 +238,7 @@ function generateErrorCallback(message) {
    * @param {object} aObj
    *        The object fired back from the callback
    */
-  return aObj => {
+  return function (aObj) {
     if (aObj) {
       if (aObj.name && aObj.message) {
         ok(false, "Unexpected callback for '" + aObj.name +
@@ -260,14 +252,9 @@ function generateErrorCallback(message) {
       ok(false, "Unexpected callback with message = '" + message +
          "' at: " + JSON.stringify(stack));
     }
-    throw new Error("Unexpected callback");
+    SimpleTest.finish();
   }
 }
-
-var unexpectedEventArrived;
-var rejectOnUnexpectedEvent = new Promise((x, reject) => {
-  unexpectedEventArrived = reject;
-});
 
 /**
  * Generates a callback function fired only for unexpected events happening.
@@ -277,211 +264,29 @@ var rejectOnUnexpectedEvent = new Promise((x, reject) => {
  * @param {String} eventName
           Name of the unexpected event
  */
-function unexpectedEvent(message, eventName) {
+function unexpectedEventAndFinish(message, eventName) {
   var stack = new Error().stack.split("\n");
   stack.shift(); // Don't include this instantiation frame
 
-  return e => {
-    var details = "Unexpected event '" + eventName + "' fired with message = '" +
-        message + "' at: " + JSON.stringify(stack);
-    ok(false, details);
-    unexpectedEventArrived(new Error(details));
+  return function () {
+    ok(false, "Unexpected event '" + eventName + "' fired with message = '" +
+       message + "' at: " + JSON.stringify(stack));
+    SimpleTest.finish();
   }
 }
-
-/**
- * Implements the one-shot event pattern used throughout.  Each of the 'onxxx'
- * attributes on the wrappers can be set with a custom handler.  Prior to the
- * handler being set, if the event fires, it causes the test execution to halt.
- * That handler is used exactly once, after which the original, error-generating
- * handler is re-installed.  Thus, each event handler is used at most once.
- *
- * @param {object} wrapper
- *        The wrapper on which the psuedo-handler is installed
- * @param {object} obj
- *        The real source of events
- * @param {string} event
- *        The name of the event
- */
-function createOneShotEventWrapper(wrapper, obj, event) {
-  var onx = 'on' + event;
-  var unexpected = unexpectedEvent(wrapper, event);
-  wrapper[onx] = unexpected;
-  obj[onx] = e => {
-    info(wrapper + ': "on' + event + '" event fired');
-    e.wrapper = wrapper;
-    wrapper[onx](e);
-    wrapper[onx] = unexpected;
-  };
-}
-
-
-/**
- * This class executes a series of functions in a continuous sequence.
- * Promise-bearing functions are executed after the previous promise completes.
- *
- * @constructor
- * @param {object} framework
- *        A back reference to the framework which makes use of the class. It is
- *        passed to each command callback.
- * @param {function[]} commandList
- *        Commands to set during initialization
- */
-function CommandChain(framework, commandList) {
-  this._framework = framework;
-  this.commands = commandList || [ ];
-}
-
-CommandChain.prototype = {
-  /**
-   * Start the command chain.  This returns a promise that always resolves
-   * cleanly (this catches errors and fails the test case).
-   */
-  execute: function () {
-    return this.commands.reduce((prev, next, i) => {
-      if (typeof next !== 'function' || !next.name) {
-        throw new Error('registered non-function' + next);
-      }
-
-      return prev.then(() => {
-        info('Run step ' + (i + 1) + ': ' + next.name);
-        return Promise.race([ next(this._framework), rejectOnUnexpectedEvent ]);
-      });
-    }, Promise.resolve())
-      .catch(e =>
-             ok(false, 'Error in test execution: ' + e +
-                ((typeof e.stack === 'string') ?
-                 (' ' + e.stack.split('\n').join(' ... ')) : '')));
-  },
-
-  /**
-   * Add new commands to the end of the chain
-   */
-  append: function(commands) {
-    this.commands = this.commands.concat(commands);
-  },
-
-  /**
-   * Returns the index of the specified command in the chain.
-   */
-  indexOf: function(functionOrName) {
-    if (typeof functionOrName === 'string') {
-      return this.commands.findIndex(f => f.name === functionOrName);
-    }
-    return this.commands.indexOf(functionOrName);
-  },
-
-  /**
-   * Inserts the new commands after the specified command.
-   */
-  insertAfter: function(functionOrName, commands) {
-    this._insertHelper(functionOrName, commands, 1);
-  },
-
-  /**
-   * Inserts the new commands before the specified command.
-   */
-  insertBefore: function(functionOrName, commands) {
-    this._insertHelper(functionOrName, commands, 0);
-  },
-
-  _insertHelper: function(functionOrName, commands, delta) {
-    var index = this.indexOf(functionOrName);
-
-    if (index >= 0) {
-      this.commands = [].concat(
-        this.commands.slice(0, index + delta),
-        commands,
-        this.commands.slice(index + delta));
-    }
-  },
-
-  /**
-   * Removes the specified command, returns what was removed.
-   */
-  remove: function(functionOrName) {
-    var index = this.indexOf(functionOrName);
-    if (index >= 0) {
-      return this.commands.splice(index, 1);
-    }
-    return [];
-  },
-
-  /**
-   * Removes all commands after the specified one, returns what was removed.
-   */
-  removeAfter: function(functionOrName) {
-    var index = this.indexOf(functionOrName);
-    if (index >= 0) {
-      return this.commands.splice(index + 1);
-    }
-    return [];
-  },
-
-  /**
-   * Removes all commands before the specified one, returns what was removed.
-   */
-  removeBefore: function(functionOrName) {
-    var index = this.indexOf(functionOrName);
-    if (index >= 0) {
-      return this.commands.splice(0, index);
-    }
-    return [];
-  },
-
-  /**
-   * Replaces a single command, returns what was removed.
-   */
-  replace: function(functionOrName, commands) {
-    this.insertBefore(functionOrName, commands);
-    return this.remove(functionOrName);
-  },
-
-  /**
-   * Replaces all commands after the specified one, returns what was removed.
-   */
-  replaceAfter: function(functionOrName, commands) {
-    var oldCommands = this.removeAfter(functionOrName);
-    this.append(commands);
-    return oldCommands;
-  },
-
-  /**
-   * Replaces all commands before the specified one, returns what was removed.
-   */
-  replaceBefore: function(functionOrName, commands) {
-    var oldCommands = this.removeBefore(functionOrName);
-    this.insertBefore(functionOrName, commands);
-    return oldCommands;
-  },
-
-  /**
-   * Remove all commands whose name match the specified regex.
-   */
-  filterOut: function (id_match) {
-    this.commands = this.commands.filter(c => !id_match.test(c.name));
-  }
-};
-
 
 function IsMacOSX10_6orOlder() {
-  if (navigator.platform.indexOf("Mac") !== 0) {
-    return false;
-  }
+    var is106orOlder = false;
 
-  var version = Cc["@mozilla.org/system-info;1"]
-      .getService(Ci.nsIPropertyBag2)
-      .getProperty("version");
-  // the next line is correct: Mac OS 10.6 corresponds to Darwin version 10.x !
-  // Mac OS 10.7 is Darwin version 11.x. the |version| string we've got here
-  // is the Darwin version.
-  return (parseFloat(version) < 11.0);
+    if (navigator.platform.indexOf("Mac") == 0) {
+        var version = Cc["@mozilla.org/system-info;1"]
+                        .getService(SpecialPowers.Ci.nsIPropertyBag2)
+                        .getProperty("version");
+        // the next line is correct: Mac OS 10.6 corresponds to Darwin version 10.x !
+        // Mac OS 10.7 is Darwin version 11.x. the |version| string we've got here
+        // is the Darwin version.
+        is106orOlder = (parseFloat(version) < 11.0);
+    }
+    return is106orOlder;
 }
 
-(function(){
-  var el = document.createElement("link");
-  el.rel = "stylesheet";
-  el.type = "text/css";
-  el.href= "/tests/SimpleTest/test.css";
-  document.head.appendChild(el);
-}());
