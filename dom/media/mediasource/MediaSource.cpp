782bd5a77fa5774dffb694d61c487f39d4d490cb
Chris Double <chris.double@double.co.nz>
2015-01-29 15:35:55 +1300

    Bug 1112424 - Part 1: Add moz specific methods to retrieve debug data to media object IDL. r=bz, a=sledru
    Bug 1112424 adds an about:media page that gives debug information about HTMLMediaElements
    and in particular MediaSource objects that are live inside tabs. This patch adds
    methods to the MediaSource and HTMLMediaElement object to return the debug data as a string
    that gets displayed directly in the about:media page, inside a <pre> element.

--- dom/media/mediasource/MediaSource.cpp
+++ dom/media/mediasource/MediaSource.cpp
@@ -535,6 +535,12 @@ MediaSource::Dump(const char* aPath)
 }
 #endif
 
+void
+MediaSource::GetMozDebugReaderData(nsAString& aString)
+{
+  mDecoder->GetMozDebugReaderData(aString);
+}
+
 nsPIDOMWindow*
 MediaSource::GetParentObject() const
 {
