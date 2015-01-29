706d085dfa54411e7767c1e0f279d5e6afd0e1f6
Bobby Holley <bobbyholley@gmail.com>
2015-01-28 18:54:11 -0800

    Bug 1126465 - Implement the ability to disconnect outstanding promises. r=mattwoodrow

--- dom/media/MediaPromise.cpp
+++ dom/media/MediaPromise.cpp
@@ -23,5 +23,18 @@ DispatchMediaPromiseRunnable(nsIEventTarget* aEventTarget, nsIRunnable* aRunnabl
   return aEventTarget->Dispatch(aRunnable, NS_DISPATCH_NORMAL);
 }
 
+void
+AssertOnThread(MediaTaskQueue* aQueue)
+{
+  MOZ_ASSERT(aQueue->IsCurrentThreadIn());
+}
+
+void AssertOnThread(nsIEventTarget* aTarget)
+{
+  nsCOMPtr<nsIThread> targetThread = do_QueryInterface(aTarget);
+  MOZ_ASSERT(targetThread, "Don't know how to deal with threadpools etc here");
+  MOZ_ASSERT(NS_GetCurrentThread() == targetThread);
+}
+
 }
 } // namespace mozilla
