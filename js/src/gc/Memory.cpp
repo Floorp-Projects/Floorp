9b239725b52d826b17311696a28d59d6ed26d3d7
Martin Husemann <martin@NetBSD.ORG>
2015-01-29 04:40:00 -0500

    Bug 994133 - Make sure allocated memory has the upper 17 bits clear on NetBSD/sparc64. r=luke
    --HG--
    extra : rebase_source : 0a9f59238704281a360f05cc34b5e2589f3781f6

--- js/src/gc/Memory.cpp
+++ js/src/gc/Memory.cpp
@@ -379,7 +379,7 @@ static inline void *
 MapMemoryAt(void *desired, size_t length, int prot = PROT_READ | PROT_WRITE,
             int flags = MAP_PRIVATE | MAP_ANON, int fd = -1, off_t offset = 0)
 {
-#if defined(__ia64__)
+#if defined(__ia64__) || (defined(__sparc64__) && defined(__NetBSD__))
     MOZ_ASSERT(0xffff800000000000ULL & (uintptr_t(desired) + length - 1) == 0);
 #endif
     void *region = mmap(desired, length, prot, flags, fd, offset);
@@ -402,7 +402,7 @@ static inline void *
 MapMemory(size_t length, int prot = PROT_READ | PROT_WRITE,
           int flags = MAP_PRIVATE | MAP_ANON, int fd = -1, off_t offset = 0)
 {
-#if defined(__ia64__)
+#if defined(__ia64__) || (defined(__sparc64__) && defined(__NetBSD__))
     /*
      * The JS engine assumes that all allocated pointers have their high 17 bits clear,
      * which ia64's mmap doesn't support directly. However, we can emulate it by passing
