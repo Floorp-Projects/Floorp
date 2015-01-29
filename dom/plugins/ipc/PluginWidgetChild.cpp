159b20ef6caecf83a8425dc076a1090f64c622f0
Jim Mathies <jmathies@mozilla.com>
2015-01-29 13:41:53 -0600

    Bug 1095754 - In the content process provide a way to track and access PluginInstanceParent objects from PluginWidgetChild. r=aklotz

--- dom/plugins/ipc/PluginWidgetChild.cpp
+++ dom/plugins/ipc/PluginWidgetChild.cpp
@@ -4,6 +4,13 @@
 
 #include "mozilla/plugins/PluginWidgetChild.h"
 #include "PluginWidgetProxy.h"
+#include "mozilla/DebugOnly.h"
+#include "nsDebug.h"
+
+#if defined(XP_WIN)
+#include "mozilla/plugins/PluginInstanceParent.h"
+using mozilla::plugins::PluginInstanceParent;
+#endif
 
 namespace mozilla {
 namespace plugins {
@@ -26,6 +33,26 @@ PluginWidgetChild::ActorDestroy(ActorDestroyReason aWhy)
     mWidget->ChannelDestroyed();
   }
   mWidget = nullptr;
+}
+
+bool
+PluginWidgetChild::RecvUpdateWindow(const uintptr_t& aChildId)
+{
+#if defined(XP_WIN)
+  NS_ASSERTION(aChildId, "Expected child hwnd value for remote plugin instance.");
+  PluginInstanceParent* parentInstance =
+    PluginInstanceParent::LookupPluginInstanceByID(aChildId);
+  NS_ASSERTION(parentInstance, "Expected matching plugin instance");
+  if (parentInstance) {
+    // sync! update call to the plugin instance that forces the
+    // plugin to paint its child window.
+    parentInstance->CallUpdateWindow();
+  }
+  return true;
+#else
+  NS_NOTREACHED("PluginWidgetChild::RecvUpdateWindow calls unexpected on this platform.");
+  return false;
+#endif
 }
 
 } // namespace plugins
