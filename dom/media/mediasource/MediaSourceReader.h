96999f7ca993e18abf1b30d517f33ff4f1a7cc5c
Bobby Holley <bobbyholley@gmail.com>
2015-01-28 18:54:11 -0800

    Bug 1126465 - Introduce machinery to hold onto MediaPromise::Consumer references, and use it for MediaSourceReader subdecoders. r=mattwoodrow

--- dom/media/mediasource/MediaSourceReader.h
+++ dom/media/mediasource/MediaSourceReader.h
@@ -183,6 +183,9 @@ private:
   nsRefPtr<TrackBuffer> mAudioTrack;
   nsRefPtr<TrackBuffer> mVideoTrack;
 
+  MediaPromiseConsumerHolder<AudioDataPromise> mAudioRequest;
+  MediaPromiseConsumerHolder<VideoDataPromise> mVideoRequest;
+
   MediaPromiseHolder<AudioDataPromise> mAudioPromise;
   MediaPromiseHolder<VideoDataPromise> mVideoPromise;
 
