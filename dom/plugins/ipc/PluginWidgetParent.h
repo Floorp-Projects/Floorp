2aa06d02707dae6f2f014d4e7908bf4eeab23b41
Jim Mathies <jmathies@mozilla.com>
2015-01-29 13:41:56 -0600

    Bug 1095754 - Remove obsolete PPluginWidget apis and code. r=aklotz

--- dom/plugins/ipc/PluginWidgetParent.h
+++ dom/plugins/ipc/PluginWidgetParent.h
@@ -30,14 +30,8 @@ public:
   virtual void ActorDestroy(ActorDestroyReason aWhy) MOZ_OVERRIDE;
   virtual bool RecvCreate() MOZ_OVERRIDE;
   virtual bool RecvDestroy() MOZ_OVERRIDE;
-  virtual bool RecvShow(const bool& aState) MOZ_OVERRIDE;
   virtual bool RecvSetFocus(const bool& aRaise) MOZ_OVERRIDE;
-  virtual bool RecvInvalidate(const nsIntRect& aRect) MOZ_OVERRIDE;
   virtual bool RecvGetNativePluginPort(uintptr_t* value) MOZ_OVERRIDE;
-  virtual bool RecvResize(const nsIntRect& aRect) MOZ_OVERRIDE;
-  virtual bool RecvMove(const double& aX, const double& aY) MOZ_OVERRIDE;
-  virtual bool RecvSetWindowClipRegion(InfallibleTArray<nsIntRect>&& Regions,
-				       const bool& aIntersectWithExisting) MOZ_OVERRIDE;
 
 private:
   // The tab our connection is associated with.
