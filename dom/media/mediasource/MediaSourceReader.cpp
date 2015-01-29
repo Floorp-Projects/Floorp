70eb8041afb1397b26682aa9a84069530d2d4fb1
Chris Pearce <cpearce@mozilla.com>
2015-01-29 21:50:48 +1300

    Bug 1123535 - Make MP4Reader and WMFMediaDataDecoder support going dormant. r=kentuckyfriedtakahe a=sledru

--- dom/media/mediasource/MediaSourceReader.cpp
+++ dom/media/mediasource/MediaSourceReader.cpp
@@ -411,6 +411,11 @@ MediaSourceReader::ContinueShutdown()
   mVideoTrack = nullptr;
   mVideoReader = nullptr;
 
+  if (mSharedDecoderManager) {
+    mSharedDecoderManager->Shutdown();
+    mSharedDecoderManager = nullptr;
+  }
+
   MOZ_ASSERT(mAudioPromise.IsEmpty());
   MOZ_ASSERT(mVideoPromise.IsEmpty());
 
