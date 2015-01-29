3ee2d8f714aef637bccf8dfbcb10de914a8a2fa4
Jim Mathies <jmathies@mozilla.com>
2015-01-29 13:41:55 -0600

    Bug 1095754 - Add a new Layer helper method for retrieving the root clip on a particular layer. r=roc

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -1251,6 +1251,24 @@ public:
   Layer* GetMaskLayer() const { return mMaskLayer; }
 
 
+  /**
+   * Retrieve the root level visible region for |this| taking into account
+   * clipping applied to parent layers of |this| as well as subtracting
+   * visible regions of higher siblings of this layer and each ancestor.
+   *
+   * Note translation values for offsets of visible regions and accumulated
+   * aLayerOffset are integer rounded using Point's RoundedToInt.
+   *
+   * @param aResult - the resulting visible region of this layer.
+   * @param aLayerOffset - this layer's total offset from the root layer.
+   * @return - false if during layer tree traversal a parent or sibling
+   *  transform is found to be non-translational. This method returns early
+   *  in this case, results will not be valid. Returns true on successful
+   *  traversal.
+   */
+  bool GetVisibleRegionRelativeToRootLayer(nsIntRegion& aResult,
+                                           nsIntPoint* aLayerOffset);
+
   // Note that all lengths in animation data are either in CSS pixels or app
   // units and must be converted to device pixels by the compositor.
   AnimationArray& GetAnimations() { return mAnimations; }
