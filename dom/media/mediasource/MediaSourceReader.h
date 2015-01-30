a2a734717157922a2996e35250fab14e41367eed
Bobby Holley <bobbyholley@gmail.com>
2015-01-29 22:11:11 -0800

    Bug 1126465 - Cancel sample requests when seeks start, disallow them while seeks are happening, and assert against seeks when samples arrive. r=mattwoodrow
    The duplication of the IsSeeking() checks before all the Request{Audio,Video}Data
    callsites is ugly. We'll fix this in the next patch by applying the same disconnect
    treatment to the seek promise.

--- dom/media/mediasource/MediaSourceReader.h
+++ dom/media/mediasource/MediaSourceReader.h
@@ -177,6 +177,7 @@ private:
   bool HaveData(int64_t aTarget, MediaData::Type aType);
 
   void AttemptSeek();
+  bool IsSeeking() { return mPendingSeekTime != -1; }
 
   nsRefPtr<MediaDecoderReader> mAudioReader;
   nsRefPtr<MediaDecoderReader> mVideoReader;
