c393878aedb8ffabda71d38cde3f848f2f110187
Jim Mathies <jmathies@mozilla.com>
2015-01-30 10:37:03 -0600

    Bug 1127374 - Make ContentParent::RecvLoadPlugin less failure prone. r=billm

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -153,8 +153,8 @@ public:
                                         TabId* aTabId) MOZ_OVERRIDE;
     virtual bool RecvBridgeToChildProcess(const ContentParentId& aCpId) MOZ_OVERRIDE;
 
-    virtual bool RecvLoadPlugin(const uint32_t& aPluginId) MOZ_OVERRIDE;
-    virtual bool RecvConnectPluginBridge(const uint32_t& aPluginId) MOZ_OVERRIDE;
+    virtual bool RecvLoadPlugin(const uint32_t& aPluginId, nsresult* aRv) MOZ_OVERRIDE;
+    virtual bool RecvConnectPluginBridge(const uint32_t& aPluginId, nsresult* aRv) MOZ_OVERRIDE;
     virtual bool RecvFindPlugins(const uint32_t& aPluginEpoch,
                                  nsTArray<PluginTag>* aPlugins,
                                  uint32_t* aNewPluginEpoch) MOZ_OVERRIDE;
