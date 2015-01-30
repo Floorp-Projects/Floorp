8a6aea8cb62c249a62b1548f3c09e5e96cfc4019
Jeff Muizelaar <jmuizelaar@mozilla.com>
2015-01-30 16:04:44 -0500

    Bug 1136726. Remove PaintMode from TileLayer. r=kats
    PaintMode is never changed from the default.

--- mobile/android/base/gfx/TileLayer.java
+++ mobile/android/base/gfx/TileLayer.java
@@ -15,22 +15,10 @@ import android.opengl.GLES20;
 public abstract class TileLayer extends Layer {
     private static final String LOGTAG = "GeckoTileLayer";
 
-    public enum PaintMode { NORMAL, REPEAT, STRETCH };
-    private PaintMode mPaintMode;
-
-    public TileLayer(IntSize size, PaintMode paintMode) {
+    public TileLayer(IntSize size) {
         super(size);
-
-        mPaintMode = paintMode;
     }
-
-    protected boolean repeats() { return mPaintMode == PaintMode.REPEAT; }
-    protected boolean stretches() { return mPaintMode == PaintMode.STRETCH; }
 
     public abstract void destroy();
-
-    public void setPaintMode(PaintMode mode) {
-        mPaintMode = mode;
-    }
 }
 
