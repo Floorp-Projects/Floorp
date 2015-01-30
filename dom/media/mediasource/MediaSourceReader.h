da86947525b9d30a2e4128992f848caf082c43df
Bobby Holley <bobbyholley@gmail.com>
2015-01-29 22:11:11 -0800

    Bug 1126465 - Factor out calls to Request{Audio,Video}Data into a helper method. r=mattwoodrow, a=sledru

--- dom/media/mediasource/MediaSourceReader.h
+++ dom/media/mediasource/MediaSourceReader.h
@@ -160,6 +160,9 @@ private:
   SwitchReaderResult SwitchAudioReader(int64_t aTarget);
   SwitchReaderResult SwitchVideoReader(int64_t aTarget);
 
+  void DoAudioRequest();
+  void DoVideoRequest();
+
   void RequestAudioDataComplete(int64_t aTime);
   void RequestAudioDataFailed(nsresult aResult);
   void RequestVideoDataComplete(int64_t aTime);
