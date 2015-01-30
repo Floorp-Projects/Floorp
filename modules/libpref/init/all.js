290e887cb7754477018ded817625ee5cf48f35a1
Brad Lassey <blassey@mozilla.com>
2015-01-30 18:36:33 -0500

    bug 1071880 - Notify user of addons that are slowing their browser down significantly r=mossop

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4465,6 +4465,12 @@ pref("dom.mozSettings.SettingsService.verbose.enabled", false);
 // readwrite.
 pref("dom.mozSettings.allowForceReadOnly", false);
 
+// The interval at which to check for slow running addons
+pref("browser.addon-watch.interval", 15000);
+pref("browser.addon-watch.ignore", "[\"mochikit@mozilla.org\"]");
+// the percentage of time addons are allowed to use without being labeled slow
+pref("browser.addon-watch.percentage-limit", 1);
+
 // RequestSync API is disabled by default.
 pref("dom.requestSync.enabled", false);
 
