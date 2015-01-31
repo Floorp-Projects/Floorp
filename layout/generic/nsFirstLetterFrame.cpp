e0f7a0435c28f60b104ee137289771042cdb9c42
L. David Baron <dbaron@dbaron.org>
2015-01-31 11:00:44 -0800

    Bug 1106755 - Make more overflow area computation code apply only to floating ::first-letter. r=jfkthame, a=sledru
    In inline layout there's no need to do anything with overflow areas
    since RelativePositionFrames handles it.  Apparently doing some of the
    work messes things up somehow, so the easy fix is to make the inline
    ::first-letter codepath behave more like nsInlineFrame and not do any
    messing with overflow areas.  So this moves the rest of the overflow
    code in nsFirstLetterFrame::Reflow into the float-only codepath.  There
    don't appear to be any ordering issues with the movement, since the code
    that the FinishAndStoreOverflow call is moved across deals with creating
    and destroying continuations of the child.
    
    All the added tests pass with the patch, but without the patch
    overflow-inline-nooverflow.html fails.
    
    --HG--
    extra : histedit_source : 785b1ed730bc51c8f3a62a07d814d5d25c2eeec2

--- layout/generic/nsFirstLetterFrame.cpp
+++ layout/generic/nsFirstLetterFrame.cpp
@@ -226,6 +226,15 @@ nsFirstLetterFrame::Reflow(nsPresContext*          aPresContext,
     aMetrics.SetSize(wm, convertedSize);
     aMetrics.SetBlockStartAscent(kidMetrics.BlockStartAscent() +
                                  bp.BStart(wm));
+
+    // Ensure that the overflow rect contains the child textframe's
+    // overflow rect.
+    // Note that if this is floating, the overline/underline drawable
+    // area is in the overflow rect of the child textframe.
+    aMetrics.UnionOverflowAreasWithDesiredBounds();
+    ConsiderChildOverflow(aMetrics.mOverflowAreas, kid);
+
+    FinishAndStoreOverflow(&aMetrics);
   }
   else {
     // Pretend we are a span and reflow the child frame
@@ -247,12 +256,6 @@ nsFirstLetterFrame::Reflow(nsPresContext*          aPresContext,
     nsLayoutUtils::SetBSizeFromFontMetrics(this, aMetrics, aReflowState,
                                            bp, lineWM, wm);
   }
-
-  // Ensure that the overflow rect contains the child textframe's overflow rect.
-  // Note that if this is floating, the overline/underline drawable area is in
-  // the overflow rect of the child textframe.
-  aMetrics.UnionOverflowAreasWithDesiredBounds();
-  ConsiderChildOverflow(aMetrics.mOverflowAreas, kid);
 
   if (!NS_INLINE_IS_BREAK_BEFORE(aReflowStatus)) {
     // Create a continuation or remove existing continuations based on
@@ -287,8 +290,6 @@ nsFirstLetterFrame::Reflow(nsPresContext*          aPresContext,
       }
     }
   }
-
-  FinishAndStoreOverflow(&aMetrics);
 
   NS_FRAME_SET_TRUNCATION(aReflowStatus, aReflowState, aMetrics);
 }
