375c99aa5a332e284b6f14b393d4f4d8aca71f2a
Mike Conley <mconley@mozilla.com>
2015-01-30 20:54:43 -0500

    Bug 1090448 - Add initial serialization and deserialization capabilities for nsPrintOptionsGTK. r=karlt.

--- widget/gtk/nsPrintOptionsGTK.cpp
+++ widget/gtk/nsPrintOptionsGTK.cpp
@@ -5,6 +5,7 @@
 #include "nsPrintOptionsGTK.h"
 #include "nsPrintSettingsGTK.h"
 
+using namespace mozilla::embedding;
 
 /** ---------------------------------------------------
  *  See documentation in nsPrintOptionsWin.h
@@ -21,6 +22,27 @@ nsPrintOptionsGTK::nsPrintOptionsGTK()
  */
 nsPrintOptionsGTK::~nsPrintOptionsGTK()
 {
+}
+
+NS_IMETHODIMP
+nsPrintOptionsGTK::SerializeToPrintData(nsIPrintSettings* aSettings,
+                                        nsIWebBrowserPrint* aWBP,
+                                        PrintData* data)
+{
+  nsresult rv = nsPrintOptions::SerializeToPrintData(aSettings, aWBP, data);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsPrintOptionsGTK::DeserializeToPrintSettings(const PrintData& data,
+                                              nsIPrintSettings* settings)
+{
+  nsresult rv = nsPrintOptions::DeserializeToPrintSettings(data, settings);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  return NS_OK;
 }
 
 /* nsIPrintSettings CreatePrintSettings (); */
