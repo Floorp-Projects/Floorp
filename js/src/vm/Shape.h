f296f30fd5bbd23b0df78d8d38164c5651b362d9
Jason Orendorff <jorendorff@mozilla.com>
2015-02-02 13:59:55 -0600

    Bug 1128648 - Remove fretting about DynamicWithObject in Shape::set. r=efaust.

--- js/src/vm/Shape.h
+++ js/src/vm/Shape.h
@@ -962,7 +962,8 @@ class Shape : public gc::TenuredCell
                setter() == rawSetter;
     }
 
-    bool set(JSContext* cx, HandleObject obj, HandleObject receiver, bool strict, MutableHandleValue vp);
+    bool set(JSContext* cx, HandleNativeObject obj, HandleObject receiver, bool strict,
+             MutableHandleValue vp);
 
     BaseShape *base() const { return base_.get(); }
 
