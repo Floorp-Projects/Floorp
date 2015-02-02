8808deb073f8c60e8a8972c4363da3558836f118
James Willcox <snorp@snorp.net>
2015-02-02 15:25:22 -0600

    Bug 1100126 - Don't return garbage image data if we fail to blit the texture. r=jgilbert, a=sledru

--- gfx/layers/GLImages.cpp
+++ gfx/layers/GLImages.cpp
@@ -61,9 +61,9 @@ GLImage::GetAsSourceSurface()
 
   GLBlitHelper helper(sSnapshotContext);
 
-  helper.BlitImageToFramebuffer(this, size, fb.FB(), true);
-
-  ScopedBindFramebuffer bind(sSnapshotContext, fb.FB());
+  if (!helper.BlitImageToFramebuffer(this, size, fb.FB(), true)) {
+    return nullptr;
+  }
 
   RefPtr<gfx::DataSourceSurface> source =
         gfx::Factory::CreateDataSourceSurface(size, gfx::SurfaceFormat::B8G8R8A8);
@@ -71,6 +71,7 @@ GLImage::GetAsSourceSurface()
     return nullptr;
   }
 
+  ScopedBindFramebuffer bind(sSnapshotContext, fb.FB());
   ReadPixelsIntoDataSurface(sSnapshotContext, source);
   return source.forget();
 }
