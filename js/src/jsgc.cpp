192719a26bfdc1f3ad1c7435380e1719c193157a
Jon Coppeard <jcoppeard@mozilla.com>
2015-02-02 10:11:23 +0000

    Bug 1127246 - Add a post barrier to the baseShapes table r=terrence a=sylvestre

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -7077,6 +7077,7 @@ js::gc::CheckHashTablesAfterMovingGC(JSRuntime *rt)
         c->checkTypeObjectTablesAfterMovingGC();
         c->checkInitialShapesTableAfterMovingGC();
         c->checkWrapperMapAfterMovingGC();
+        c->checkBaseShapeTableAfterMovingGC();
         if (c->debugScopes)
             c->debugScopes->checkHashTablesAfterMovingGC(rt);
     }
