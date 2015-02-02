8a5532af7da8b1534b975e710faeca3e9058cfb9
James Willcox <snorp@snorp.net>
2015-02-02 15:25:22 -0600

    Bug 1100126 - Don't allow detaching a SurfaceTexture on PowerVR devices due to fencing issues. r=jgilbert, a=sledru

--- gfx/gl/AndroidSurfaceTexture.h
+++ gfx/gl/AndroidSurfaceTexture.h
@@ -61,8 +61,11 @@ public:
   // attached, we try to wait for it to become detached.
   nsresult Attach(GLContext* aContext, PRIntervalTime aTiemout = PR_INTERVAL_NO_TIMEOUT);
 
-  // This is a noop on ICS, and will always fail
   nsresult Detach();
+
+  // Ability to detach is based on API version (16+), and we also block PowerVR since it has some type
+  // of fencing problem. Bug 1100126.
+  bool CanDetach() { return mCanDetach; }
 
   GLContext* GetAttachedContext() { return mAttachedContext; }
 
@@ -94,6 +97,7 @@ private:
   ~AndroidSurfaceTexture();
 
   bool Init(GLContext* aContext, GLuint aTexture);
+  void UpdateCanDetach();
 
   GLuint mTexture;
   widget::sdk::SurfaceTexture::GlobalRef mSurfaceTexture;
@@ -101,6 +105,7 @@ private:
 
   Monitor mMonitor;
   GLContext* mAttachedContext;
+  bool mCanDetach;
 
   RefPtr<AndroidNativeWindow> mNativeWindow;
   int mID;
