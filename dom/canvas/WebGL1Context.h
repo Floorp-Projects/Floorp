be5c4501c6faf45b325c08cc7b98ae8417a0d269
Dan Glastonbury <dglastonbury@mozilla.com>
2015-02-03 12:06:20 +1000

    Bug 1145501 - Extend validation of BufferData usage. r=jgilbert

--- dom/canvas/WebGL1Context.h
+++ dom/canvas/WebGL1Context.h
@@ -34,7 +34,8 @@ private:
     virtual bool ValidateBufferTarget(GLenum target, const char* info) override;
     virtual bool ValidateBufferIndexedTarget(GLenum target, const char* info) override;
     virtual bool ValidateBufferForTarget(GLenum target, WebGLBuffer* buffer, const char* info) override;
-
+    virtual bool ValidateBufferUsageEnum(GLenum usage, const char* info) override;
+    
     virtual bool ValidateUniformMatrixTranspose(bool transpose, const char* info) override;
 };
 
