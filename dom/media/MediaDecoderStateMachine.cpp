eb27bb22f8df1f2e5db260071a313314215a4db0
Chris Pearce <cpearce@mozilla.com>
2015-02-04 10:17:28 +1300

    Bug 1123535 - Make dormant ResetPlayback assertion more lenient. r=jwwang

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1671,10 +1671,15 @@ void MediaDecoderStateMachine::PlayInternal()
 
 void MediaDecoderStateMachine::ResetPlayback()
 {
+  // We should be reseting because we're seeking, shutting down, or
+  // entering dormant state. We could also be in the process of going dormant,
+  // and have just switched to exiting dormant before we finished entering
+  // dormant, hence the DECODING_NONE case below.
   AssertCurrentThreadInMonitor();
   MOZ_ASSERT(mState == DECODER_STATE_SEEKING ||
              mState == DECODER_STATE_SHUTDOWN ||
-             mState == DECODER_STATE_DORMANT);
+             mState == DECODER_STATE_DORMANT ||
+             mState == DECODER_STATE_DECODING_NONE);
 
   // Audio thread should've been stopped at the moment. Otherwise, AudioSink
   // might be accessing AudioQueue outside of the decoder monitor while we
