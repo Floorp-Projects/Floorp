aec194eeb2140391d358efd858c5ebaee8f73102
Nicholas Nethercote <nnethercote@mozilla.com>
2015-02-03 00:03:28 -0800

    Bug 1129786 (part 2) - Instantiate nsDOMAttributeMap::mAttributeCache eagerly. r=bz.
    Now that empty PLDHashTables don't allocate any entry storage, we can undo the
    lazification of nsDOMAttributeMap::mAttributeCache from bug 1059056.

--- dom/base/nsDOMAttributeMap.h
+++ dom/base/nsDOMAttributeMap.h
@@ -11,7 +11,6 @@
 #define nsDOMAttributeMap_h
 
 #include "mozilla/MemoryReporting.h"
-#include "mozilla/UniquePtr.h"
 #include "mozilla/dom/Attr.h"
 #include "mozilla/ErrorResult.h"
 #include "nsCycleCollectionParticipant.h"
@@ -186,11 +185,9 @@ private:
   nsCOMPtr<Element> mContent;
 
   /**
-   * Cache of Attrs. It's usually empty, and thus initialized lazily.
+   * Cache of Attrs.
    */
-  mozilla::UniquePtr<AttrCache> mAttributeCache;
-
-  void EnsureAttributeCache();
+  AttrCache mAttributeCache;
 
   /**
    * SetNamedItem() (aWithNS = false) and SetNamedItemNS() (aWithNS =
