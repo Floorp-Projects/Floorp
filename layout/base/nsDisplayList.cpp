fca835eddcd4a79b99ceed1ef5117b03e54202ab
Brian Birtles <birtles@gmail.com>
2015-02-03 14:08:37 +0900

    Bug 1123196 part 2 - Don't resolve pending animations for nested calls to PaintRoot; r=mattwoodrow
    We resolve pending animations when painting has finished in
    nsDisplayList::PaintRoot. However, in the process we can trigger nested calls to
    PaintFrame (e.g. due to use of -moz-element). In that case, we shouldn't resolve
    pending animations until we complete the widget transaction.
    
    This patch adds a check that we're in a widget transaction before we resolve
    pending animations.

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -1688,7 +1688,7 @@ already_AddRefed<LayerManager> nsDisplayList::PaintRoot(nsDisplayListBuilder* aB
   aBuilder->SetIsCompositingCheap(temp);
   layerBuilder->DidEndTransaction();
 
-  if (document) {
+  if (document && widgetTransaction) {
     StartPendingAnimations(document, layerManager->GetAnimationReadyTime());
   }
 
