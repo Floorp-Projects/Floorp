7f80d25ce3a53006aed687a2df3942cfbd7c97de
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-02-03 18:52:36 +0200

    Bug 1120796 - Part 2: Replace ConvertibleToBool hackarounds with explicit bool operators. r=Waldo

--- mfbt/RangedPtr.h
+++ mfbt/RangedPtr.h
@@ -49,9 +49,6 @@ class RangedPtr
   T* const mRangeEnd;
 #endif
 
-  typedef void (RangedPtr::* ConvertibleToBool)();
-  void nonNull() {}
-
   void checkSanity()
   {
     MOZ_ASSERT(mRangeStart <= mPtr);
@@ -118,7 +115,7 @@ public:
 
   T* get() const { return mPtr; }
 
-  operator ConvertibleToBool() const { return mPtr ? &RangedPtr::nonNull : 0; }
+  explicit operator bool() const { return mPtr != nullptr; }
 
   /*
    * You can only assign one RangedPtr into another if the two pointers have
