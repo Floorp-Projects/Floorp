e859cbf0d576b08c443e5104fcb7afa520efd9f6
Jon Coppeard <jcoppeard@mozilla.com>
2015-02-04 16:12:06 +0000

    Bug 1122640 - Free nursery huge slots off main thread r=terrence

--- js/src/vm/HelperThreads.cpp
+++ js/src/vm/HelperThreads.cpp
@@ -738,6 +738,11 @@ GlobalHelperThreadState::canStartGCParallelTask()
     return !gcParallelWorklist().empty();
 }
 
+js::GCParallelTask::~GCParallelTask()
+{
+    join();
+}
+
 bool
 js::GCParallelTask::startWithLockHeld()
 {
