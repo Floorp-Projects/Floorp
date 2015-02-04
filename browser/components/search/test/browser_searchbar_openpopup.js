a98e61e293e41f176f294fa7a52324df7fbb643e
Florian Quèze <florian@queze.net>
2015-02-05 00:08:19 +0100

    Bug 1124900 - add tests for keyboard navigation in the search panel, r=Mossop.

--- browser/components/search/test/browser_searchbar_openpopup.js
+++ browser/components/search/test/browser_searchbar_openpopup.js
@@ -26,7 +26,7 @@ function* synthesizeNativeMouseClick(aElement) {
   let win = aElement.ownerDocument.defaultView;
   let x = win.mozInnerScreenX + (rect.left + rect.right) / 2;
   let y = win.mozInnerScreenY + (rect.top + rect.bottom) / 2;
- 
+
   // Wait for the mouseup event to occur before continuing.
   return new Promise((resolve, reject) => {
     function eventOccurred(e)
@@ -39,34 +39,6 @@ function* synthesizeNativeMouseClick(aElement) {
 
     utils.sendNativeMouseEvent(x * scale, y * scale, mouseDown, 0, null);
     utils.sendNativeMouseEvent(x * scale, y * scale, mouseUp, 0, null);
-  });
-}
-
-function promiseNewEngine(basename) {
-  return new Promise((resolve, reject) => {
-    info("Waiting for engine to be added: " + basename);
-    Services.search.init({
-      onInitComplete: function() {
-        let url = getRootDirectory(gTestPath) + basename;
-        let current = Services.search.currentEngine;
-        Services.search.addEngine(url, Ci.nsISearchEngine.TYPE_MOZSEARCH, "", false, {
-          onSuccess: function (engine) {
-            info("Search engine added: " + basename);
-            Services.search.currentEngine = engine;
-            registerCleanupFunction(() => {
-              Services.search.currentEngine = current;
-              Services.search.removeEngine(engine);
-              info("Search engine removed: " + basename);
-            });
-            resolve(engine);
-          },
-          onError: function (errCode) {
-            ok(false, "addEngine failed with error code " + errCode);
-            reject();
-          }
-        });
-      }
-    });
   });
 }
 
@@ -471,4 +443,3 @@ add_task(function* dont_rollup_oncaretmove() {
   EventUtils.synthesizeKey("VK_ESCAPE", {});
   yield promise;
 });
-
