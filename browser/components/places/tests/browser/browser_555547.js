22aedebbed0f89751c1e56e0e26017376502f7aa
Tim Taubert <ttaubert@mozilla.com>
2015-02-05 14:19:41 +0100

    Bug 1129697 - Fix browser_555547.js. r=mak, a=test-only

--- browser/components/places/tests/browser/browser_555547.js
+++ browser/components/places/tests/browser/browser_555547.js
@@ -2,58 +2,63 @@
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
 
-add_task(function* test() {
-  waitForExplicitFinish();
+"use strict";
 
+add_task(function* test() {
   let sidebarBox = document.getElementById("sidebar-box");
   is(sidebarBox.hidden, true, "The sidebar should be hidden");
 
   // Uncollapse the personal toolbar if needed.
   let toolbar = document.getElementById("PersonalToolbar");
   let wasCollapsed = toolbar.collapsed;
-  if (wasCollapsed)
+  if (wasCollapsed) {
     yield promiseSetToolbarVisibility(toolbar, true);
+  }
 
-  let sidebar = document.getElementById("sidebar");
-  sidebar.addEventListener("load", function() {
-    sidebar.removeEventListener("load", arguments.callee, true);
-    let tree = sidebar.contentDocument.getElementById("bookmarks-view");
+  let sidebar = yield promiseLoadedSidebar("viewBookmarksSidebar");
+  registerCleanupFunction(toggleSidebar);
 
-    // The Places view is build on load, so we enqueue our test.
-    executeSoon(function() {
-      // Focus the tree and check if its controller is returned.
-      tree.focus();
+  // Focus the tree and check if its controller is returned.
+  let tree = sidebar.contentDocument.getElementById("bookmarks-view");
+  tree.focus();
 
-      let controller = doGetPlacesControllerForCommand("placesCmd_copy");
-      let treeController = tree.controllers
-                               .getControllerForCommand("placesCmd_copy");
-      ok(controller == treeController, "tree controller was returned");
+  let controller = doGetPlacesControllerForCommand("placesCmd_copy");
+  let treeController = tree.controllers
+                           .getControllerForCommand("placesCmd_copy");
+  ok(controller == treeController, "tree controller was returned");
 
-      // Open the context menu for a toolbar item, and check if the toolbar's
-      // controller is returned.
-      let toolbarItems = document.getElementById("PlacesToolbarItems");
-      EventUtils.synthesizeMouse(toolbarItems.childNodes[0],
-                                 4, 4, { type: "contextmenu", button: 2 },
-                                 window);
-      controller = doGetPlacesControllerForCommand("placesCmd_copy");
-      let toolbarController = document.getElementById("PlacesToolbar")
-                                      .controllers
-                                      .getControllerForCommand("placesCmd_copy");
-      ok(controller == toolbarController, "the toolbar controller was returned");
+  // Open the context menu for a toolbar item, and check if the toolbar's
+  // controller is returned.
+  let toolbarItems = document.getElementById("PlacesToolbarItems");
+  EventUtils.synthesizeMouse(toolbarItems.childNodes[0],
+                             4, 4, { type: "contextmenu", button: 2 },
+                             window);
+  controller = doGetPlacesControllerForCommand("placesCmd_copy");
+  let toolbarController = document.getElementById("PlacesToolbar")
+                                  .controllers
+                                  .getControllerForCommand("placesCmd_copy");
+  ok(controller == toolbarController, "the toolbar controller was returned");
 
-      document.getElementById("placesContext").hidePopup();
+  document.getElementById("placesContext").hidePopup();
 
-      // Now that the context menu is closed, try to get the tree controller again.
-      tree.focus();
-      controller = doGetPlacesControllerForCommand("placesCmd_copy");
-      ok(controller == treeController, "tree controller was returned");
+  // Now that the context menu is closed, try to get the tree controller again.
+  tree.focus();
+  controller = doGetPlacesControllerForCommand("placesCmd_copy");
+  ok(controller == treeController, "tree controller was returned");
 
-      toggleSidebar();
-      if (wasCollapsed)
-        yield promiseSetToolbarVisibility(toolbar, false);
-
-      finish();
-    });
-  }, true);
-  toggleSidebar("viewBookmarksSidebar", true);
+  if (wasCollapsed) {
+    yield promiseSetToolbarVisibility(toolbar, false);
+  }
 });
+
+function promiseLoadedSidebar(cmd) {
+  return new Promise(resolve => {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.addEventListener("load", function onLoad() {
+      sidebar.removeEventListener("load", onLoad, true);
+      resolve(sidebar);
+    }, true);
+
+    toggleSidebar(cmd, true);
+  });
+}
