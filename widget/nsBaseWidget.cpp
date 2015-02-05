477b9ab40e642cc4a377b7854d6db748863a1381
Robert O'Callahan <robert@ocallahan.org>
2015-02-05 17:03:32 +1300

    Bug 1129774. Remove unused nsBaseWidget::GetDeviceContext. r=jmathies
    --HG--
    extra : rebase_source : 7097d97d21abcb4107838c21ab62061b8273e453

--- widget/nsBaseWidget.cpp
+++ widget/nsBaseWidget.cpp
@@ -127,7 +127,6 @@ nsBaseWidget::nsBaseWidget()
 , mForceLayersAcceleration(false)
 , mTemporarilyUseBasicLayerManager(false)
 , mUseAttachedEvents(false)
-, mContextInitialized(false)
 , mBounds(0,0,0,0)
 , mOriginalBounds(nullptr)
 , mClipRectCount(0)
@@ -1126,20 +1125,6 @@ CompositorChild* nsBaseWidget::GetRemoteRenderer()
 TemporaryRef<mozilla::gfx::DrawTarget> nsBaseWidget::StartRemoteDrawing()
 {
   return nullptr;
-}
-
-//-------------------------------------------------------------------------
-//
-// Return the used device context
-//
-//-------------------------------------------------------------------------
-nsDeviceContext* nsBaseWidget::GetDeviceContext()
-{
-  if (!mContextInitialized) {
-    mContext->Init(this);
-    mContextInitialized = true;
-  }
-  return mContext;
 }
 
 //-------------------------------------------------------------------------
