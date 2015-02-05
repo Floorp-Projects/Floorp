016da5d78c8b0ffc187b6d50f64aec94f6671381
Mark Hammond <mhammond@skippinet.com.au>
2015-02-05 12:24:06 +1100

    Bug 1121340 - Add telemetry to determine how consistent our geoip country code is with the platform's. r=gavin, a=lmandel

--- toolkit/components/search/tests/xpcshell/test_location.js
+++ toolkit/components/search/tests/xpcshell/test_location.js
@@ -18,6 +18,34 @@ function run_test() {
       let histogram = Services.telemetry.getHistogramById(hid);
       let snapshot = histogram.snapshot();
       deepEqual(snapshot.counts, [1,0,0]); // boolean probe so 3 buckets, expect 1 result for |0|.
+
+    }
+
+    // simple checks for our platform-specific telemetry.  We can't influence
+    // what they return (as we can't influence the countryCode the platform
+    // thinks we are in), but we can check the values are correct given reality.
+    // NOTE: head_search.js mocks the XULRuntime values, but saves the original
+    // OS in an OS global
+    if (Services.appinfo.OS == "Darwin") {
+      let gfxInfo2 = Cc["@mozilla.org/gfx/info;1"].getService(Ci.nsIGfxInfo2);
+      print("OSX says the country-code is", gfxInfo2.countryCode);
+      let expectedResult;
+      let hid;
+      // We know geoip said AU - if mac thinks US then we expect
+      // SEARCH_SERVICE_US_COUNTRY_MISMATCHED_PLATFORM_OSX with true (ie, a mismatch)
+      if (gfxInfo2.countryCode == "US") {
+        hid = "SEARCH_SERVICE_US_COUNTRY_MISMATCHED_PLATFORM_OSX";
+        expectedResult = [0,1,0]; // boolean probe so 3 buckets, expect 1 result for |1|.
+      } else {
+        // We are expecting SEARCH_SERVICE_NONUS_COUNTRY_MISMATCHED_PLATFORM_OSX
+        // with false if OSX says AU (not a mismatch) and true otherwise.
+        hid = "SEARCH_SERVICE_NONUS_COUNTRY_MISMATCHED_PLATFORM_OSX";
+        expectedResult = gfxInfo2.countryCode == "AU" ? [1,0,0] : [0,1,0];
+      }
+
+      let histogram = Services.telemetry.getHistogramById(hid);
+      let snapshot = histogram.snapshot();
+      deepEqual(snapshot.counts, expectedResult);
     }
     do_test_finished();
     run_next_test();
