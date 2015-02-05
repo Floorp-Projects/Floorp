fe0f3b41a65f0f7cc436326c14ebfea70ccfd25f
Felix Janda <felix.janda@posteo.de>
2015-02-05 22:24:20 +0100

    Bug 1130175 - nICEr: avoid sysctl.h include. r=bwc

--- media/mtransport/third_party/nICEr/src/stun/addrs.c
+++ media/mtransport/third_party/nICEr/src/stun/addrs.c
@@ -45,13 +45,11 @@ static char *RCSSTRING __UNUSED__="$Id: addrs.c,v 1.2 2008/04/28 18:21:30 ekr Ex
 #include <sys/param.h>
 #include <sys/socket.h>
 #ifndef ANDROID
-#include <sys/sysctl.h>
 #include <sys/syslog.h>
 #else
 #include <syslog.h>
 /* Work around an Android NDK < r8c bug */
 #undef __unused
-#include <linux/sysctl.h>
 #endif
 #ifndef LINUX
 #include <net/if.h>
@@ -122,6 +120,7 @@ static char *RCSSTRING __UNUSED__="$Id: addrs.c,v 1.2 2008/04/28 18:21:30 ekr Ex
  */
 
 #include <err.h>
+#include <sys/sysctl.h>
 
 static void stun_rt_xaddrs(caddr_t, caddr_t, struct rt_addrinfo *);
 static int stun_grab_addrs(char *name, int addrcount,
