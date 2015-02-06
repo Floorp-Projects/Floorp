8492e3403910b35a4745b0fc40cd91e0e3abffb8
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2015-02-06 13:54:28 -0800

    Bug 1087726: Make JS callers of ios.newChannel call ios.newChannel2 in browser/ - tests (r=gijs)

--- browser/base/content/test/general/browser_blob-channelname.js
+++ browser/base/content/test/general/browser_blob-channelname.js
@@ -5,7 +5,14 @@ Cu.import("resource://gre/modules/NetUtil.jsm");
 function test() {
     var file = new File([new Blob(['test'], {type: 'text/plain'})], "test-name");
     var url = URL.createObjectURL(file);
-    var channel = NetUtil.newChannel(url);
+    var channel = NetUtil.newChannel2(url,
+                                      null,
+                                      null,
+                                      null,      // aLoadingNode
+                                      Services.scriptSecurityManager.getSystemPrincipal(),
+                                      null,      // aTriggeringPrincipal
+                                      Ci.nsILoadInfo.SEC_NORMAL,
+                                      Ci.nsIContentPolicy.TYPE_OTHER);
 
     is(channel.contentDispositionFilename, 'test-name', "filename matches");
 }
