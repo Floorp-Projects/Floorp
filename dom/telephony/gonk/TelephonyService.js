7e94247d2a83a322135055f0795b136cbe9ee0f0
Szu-Yu Chen [:aknow] <szchen@mozilla.com>
2015-02-06 14:56:43 +0800

    Bug 1129402 - Fix IMEI request. r=hsinyi

--- dom/telephony/gonk/TelephonyService.js
+++ dom/telephony/gonk/TelephonyService.js
@@ -507,9 +507,8 @@ TelephonyService.prototype = {
       return;
     }
 
-    // Dial a non-emergency call when only emergency call is allowed.
-    if (!isEmergencyNumber &&
-        (aIsDialEmergency || this._isEmergencyOnly(aClientId))) {
+    // DialEmergency accepts only emergency number.
+    if (aIsDialEmergency && !isEmergencyNumber) {
       if (DEBUG) debug("Error: Dail a non-emergency by dialEmergency. Drop.");
       aCallback.notifyError(DIAL_ERROR_BAD_NUMBER);
       return;
@@ -591,6 +590,11 @@ TelephonyService.prototype = {
     }
 
     let isEmergency = gDialNumberUtils.isEmergency(aNumber);
+    if (!isEmergency && this._isEmergencyOnly()) {
+      if (DEBUG) debug("Error: Dail a normal call when emergencyCallsOnly. Drop");
+      aCallback.notifyError(DIAL_ERROR_BAD_NUMBER);
+    }
+
     if (isEmergency) {
       // Automatically select a proper clientId for emergency call.
       aClientId = gRadioInterfaceLayer.getClientIdForEmergencyCall() ;
