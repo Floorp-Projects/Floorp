349097687e0a80c366d0407b5ee669ca85a6c753
Xidorn Quan <quanxunzhen@gmail.com>
2015-02-06 13:20:42 +1100

    Bug 1053783 - Activate window after exiting fullscreen. r=jimm

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -2859,6 +2859,8 @@ nsWindow::MakeFullScreen(bool aFullScreen, nsIScreen* aTargetScreen)
       taskbarInfo->PrepareFullScreenHWND(mWnd, TRUE);
     }
   } else {
+    if (mSizeMode != nsSizeMode_Fullscreen)
+      return NS_OK;
     SetSizeMode(mOldSizeMode);
   }
 
@@ -2876,6 +2878,13 @@ nsWindow::MakeFullScreen(bool aFullScreen, nsIScreen* aTargetScreen)
   if (visible) {
     Show(true);
     Invalidate();
+
+    if (!aFullScreen && mOldSizeMode == nsSizeMode_Normal) {
+      // Ensure the window exiting fullscreen get activated. Window
+      // activation was bypassed by SetSizeMode, and hiding window for
+      // transition could also blur the current window.
+      DispatchFocusToTopLevelWindow(true);
+    }
   }
 
   // Notify the taskbar that we have exited full screen mode.
