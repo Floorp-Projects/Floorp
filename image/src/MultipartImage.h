1ac8d81b61f8ee99255b85e6d858fc7ab0a2e064
Glenn Randers-Pehrson <glennrp+bmo@gmail.com>
2015-02-05 21:34:00 -0800

    Bug 1102048 (Part 34, Multipart) - Make image/src files comply with the Mozilla Coding Style Guide. r=seth

--- image/src/MultipartImage.h
+++ image/src/MultipartImage.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef MOZILLA_IMAGELIB_MULTIPARTIMAGE_H_
-#define MOZILLA_IMAGELIB_MULTIPARTIMAGE_H_
+#ifndef mozilla_image_src_MultipartImage_h
+#define mozilla_image_src_MultipartImage_h
 
 #include "ImageWrapper.h"
 #include "IProgressObserver.h"
@@ -87,4 +87,4 @@ private:
 } // namespace image
 } // namespace mozilla
 
-#endif // MOZILLA_IMAGELIB_MULTIPARTIMAGE_H_
+#endif // mozilla_image_src_MultipartImage_h
