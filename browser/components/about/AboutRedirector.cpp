ba83b19df29b008159bbc8202df0228e245d56aa
Abdelrhman Ahmed <a.ahmed1026@gmail.com>
2015-02-08 06:13:00 -0500

    Bug 1129106 - Load about:reader in the child process. r=margaret

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -120,6 +120,7 @@ static RedirEntry kRedirMap[] = {
   { "reader", "chrome://global/content/reader/aboutReader.html",
     nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
     nsIAboutModule::ALLOW_SCRIPT |
+    nsIAboutModule::URI_MUST_LOAD_IN_CHILD |
     nsIAboutModule::HIDE_FROM_ABOUTABOUT },
 };
 static const int kRedirTotal = ArrayLength(kRedirMap);
