882829472da750fd75a110fb92cecb40cdbde547
Aaron Klotz <aklotz@mozilla.com>
2015-02-08 15:57:23 -0700

    Bug 1116806: Enable dom.ipc.plugins.asyncInit on non-nightly channels by default; r=vladan

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2425,7 +2425,13 @@ pref("dom.ipc.plugins.reportCrashURL", true);
 // Defaults to 30 seconds.
 pref("dom.ipc.plugins.unloadTimeoutSecs", 30);
 
+// Asynchronous plugin initialization should only be enabled on non-e10s
+// channels until some remaining bugs are resolved.
+#ifdef NIGHTLY_BUILD
 pref("dom.ipc.plugins.asyncInit", false);
+#else
+pref("dom.ipc.plugins.asyncInit", true);
+#endif
 
 pref("dom.ipc.processCount", 1);
 
