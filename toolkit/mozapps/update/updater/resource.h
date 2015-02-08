1c81cea2d8a327e1932ef3958b7fe45d9c181121
Robert Strong <robert.bugzilla@gmail.com>
2015-02-08 12:53:44 -0800

    Bug 1129209 - Use a separate manifest for comctl32.dll. r=bbondy, a=abillings

--- toolkit/mozapps/update/updater/resource.h
+++ toolkit/mozapps/update/updater/resource.h
@@ -15,6 +15,7 @@
 #define IDR_BACKUP_CERT                 1005
 #define IDS_UPDATER_IDENTITY            1006
 #define IDR_XPCSHELL_CERT               1007
+#define IDR_COMCTL32_MANIFEST           17
 
 // Next default values for new objects
 // 
