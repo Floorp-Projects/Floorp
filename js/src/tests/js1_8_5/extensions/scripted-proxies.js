6cf0c5a143adb52312fc771279ce552c0214c1d1
Ryan VanderMeulen <ryanvm@gmail.com>
2015-02-09 14:56:10 -0500

    Backed out changeset fd9745f7a697 (bug 1073816) for test_bug930091.js xpcshell failures. CLOSED TREE

--- js/src/tests/js1_8_5/extensions/scripted-proxies.js
+++ js/src/tests/js1_8_5/extensions/scripted-proxies.js
@@ -31,15 +31,10 @@ function test() {
 
     /* Test function proxies. */
     var proxy = Proxy.createFunction({
-        get: function(obj, name) {
-            return Function.prototype[name];
-        },
-        getOwnPropertyDescriptor: function(obj, name) {
-            return Object.getOwnPropertyDescriptor(Function.prototype, name);
-        },
-        fix: function() {
-            return ({});
-        }
+        get: function(obj,name) { return Function.prototype[name]; },
+	fix: function() {
+	    return ({});
+	}
     }, function() { return "call"; });
 
     assertEq(proxy(), "call");
@@ -55,15 +50,8 @@ function test() {
 
     /* Test function proxies as constructors. */
     var proxy = Proxy.createFunction({
-        get: function(obj, name) {
-            return Function.prototype[name];
-        },
-        getOwnPropertyDescriptor: function(obj, name) {
-            return Object.getOwnPropertyDescriptor(Function.prototype, name);
-        },
-        fix: function() {
-            return ({});
-        }
+        get: function(obj, name) { return Function.prototype[name]; },
+	fix: function() { return ({}); }
     },
     function() { var x = {}; x.origin = "call"; return x; },
     function() { var x = {}; x.origin = "new"; return x; })
