4b031a609b59a00f49e333795a749973e0ecbb26
Mason Chang <mchang@mozilla.com>
2015-02-09 16:53:09 +0800

    Bug 1123762. Enable the vsync aligned refresh driver on b2g. r=kats

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1102,8 +1102,10 @@ pref("dom.requestSync.enabled", true);
 pref("gfx.vsync.hw-vsync.enabled", true);
 pref("gfx.vsync.compositor", true);
 pref("gfx.touch.resample", true);
+pref("gfx.vsync.refreshdriver", true);
 #else
 pref("gfx.vsync.hw-vsync.enabled", false);
 pref("gfx.vsync.compositor", false);
 pref("gfx.touch.resample", false);
+pref("gfx.vsync.refreshdriver", false);
 #endif
