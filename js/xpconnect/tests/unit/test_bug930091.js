2f3b99032593efc2ad28c2eb788cce74f15840de
Tom Schuster <evilpies@gmail.com>
2015-02-09 19:42:20 +0100

    Bug 1073816 - Implement ES6 Function.prototype.bind length handling. r=Till,bholley

--- js/xpconnect/tests/unit/test_bug930091.js
+++ js/xpconnect/tests/unit/test_bug930091.js
@@ -17,9 +17,13 @@ function run_test() {
   sb.cow = { foopy: 2, __exposedProps__: { foopy: 'rw' } };
   sb.payload = Cu.evalInSandbox('new Object()', xosb);
   Cu.evalInSandbox(checkThrows.toSource(), sb);
-  Cu.evalInSandbox('checkThrows(Function.bind.call(fun, null, payload));', sb);
-  Cu.evalInSandbox('checkThrows(Function.bind.call(fun, payload));', sb);
+  Cu.evalInSandbox('checkThrows(function() { fun(payload); });', sb);
+  Cu.evalInSandbox('checkThrows(function() { Function.prototype.call.call(fun, payload); });', sb);
+  Cu.evalInSandbox('checkThrows(function() { Function.prototype.call.call(fun, null, payload); });', sb);
   Cu.evalInSandbox('checkThrows(function() { new fun(payload); });', sb);
   Cu.evalInSandbox('checkThrows(function() { cow.foopy = payload; });', sb);
   Cu.evalInSandbox('checkThrows(function() { Object.defineProperty(cow, "foopy", { value: payload }); });', sb);
+  // These fail for a different reason, .bind can't access the length/name property on the function.
+  Cu.evalInSandbox('checkThrows(function() { Function.bind.call(fun, null, payload); });', sb);
+  Cu.evalInSandbox('checkThrows(function() { Function.bind.call(fun, payload); });', sb);
 }
