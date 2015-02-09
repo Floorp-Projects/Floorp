11e34029678f70f9e6e0607a8051a026c7fac28e
Andrew McCreight <continuation@gmail.com>
2015-02-09 11:42:20 -0800

    Bug 1130128, part 1 - Compile scripts cached by nsFrameScriptExecutor in the compilation scope to avoid leaks. r=bholley

--- dom/base/nsFrameMessageManager.cpp
+++ dom/base/nsFrameMessageManager.cpp
@@ -1537,10 +1537,13 @@ nsFrameScriptExecutor::TryCacheLoadAndCompileScript(const nsAString& aURL,
 
   if (dataStringBuf && dataStringLength > 0) {
     AutoSafeJSContext cx;
-    JS::Rooted<JSObject*> global(cx, mGlobal->GetJSObject());
+    // Compile the script in the compilation scope instead of the current global
+    // to avoid keeping the current compartment alive.
+    JS::Rooted<JSObject*> global(cx, xpc::CompilationScope());
+
     if (global) {
       JSAutoCompartment ac(cx, global);
-      JS::CompileOptions options(cx);
+      JS::CompileOptions options(cx, JSVERSION_LATEST);
       options.setFileAndLine(url.get(), 1);
       options.setNoScriptRval(true);
       JS::Rooted<JSScript*> script(cx);
