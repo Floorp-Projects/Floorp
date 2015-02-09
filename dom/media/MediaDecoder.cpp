6229e7f1a941f222727c2df70df6cb8895ecc37b
Matthew Gregan <kinetik@flim.org>
2015-02-10 11:59:20 +1300

    Bug 1130923 - Remove some DASHDecoder remnants: RestrictedAccessMonitor and GetByteRangeForSeek.  r=cpearce a=lmandel

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -1552,7 +1552,7 @@ bool MediaDecoder::OnDecodeThread() const {
 }
 
 ReentrantMonitor& MediaDecoder::GetReentrantMonitor() {
-  return mReentrantMonitor.GetReentrantMonitor();
+  return mReentrantMonitor;
 }
 
 ImageContainer* MediaDecoder::GetImageContainer()
