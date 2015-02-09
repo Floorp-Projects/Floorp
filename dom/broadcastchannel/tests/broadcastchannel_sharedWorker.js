ec02db66f277d9710c8264dc77f35fe538e170f0
Andrea Marchesini <amarchesini@mozilla.com>
2015-02-09 08:33:01 +0000

    Bug 1130678 - Fix messaging ordering issue in broadcastchannel tests, r=mchang

--- dom/broadcastchannel/tests/broadcastchannel_sharedWorker.js
+++ dom/broadcastchannel/tests/broadcastchannel_sharedWorker.js
@@ -2,8 +2,8 @@ onconnect = function(evt) {
   evt.ports[0].onmessage = function(evt) {
     var bc = new BroadcastChannel('foobar');
     bc.addEventListener('message', function(event) {
-      evt.target.postMessage(event.data == "hello world from the window" ? "OK" : "KO");
-      bc.postMessage("hello world from the worker");
+      bc.postMessage(event.data == "hello world from the window" ?
+                       "hello world from the worker" : "KO");
       bc.close();
     }, false);
 
