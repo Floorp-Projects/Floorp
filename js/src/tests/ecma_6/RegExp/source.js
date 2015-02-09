24e0fdd81816ed63078d6cd245e0dc634aabe1b0
Tooru Fujisawa <arai_a@mac.com>
2015-02-10 02:04:43 +0900

    Bug 1130860 - Implement all of EscapeRegExpPattern instead of just escaping forward slashes. r=till

--- js/src/tests/ecma_6/RegExp/source.js
+++ js/src/tests/ecma_6/RegExp/source.js
@@ -7,9 +7,13 @@ assertEq(RegExp.prototype.source, "(?:)");
 assertEq(/foo/.source, "foo");
 assertEq(/foo/iymg.source, "foo");
 assertEq(/\//.source, "\\/");
+assertEq(/\n\r/.source, "\\n\\r");
+assertEq(/\u2028\u2029/.source, "\\u2028\\u2029");
 assertEq(RegExp("").source, "(?:)");
 assertEq(RegExp("", "mygi").source, "(?:)");
 assertEq(RegExp("/").source, "\\/");
+assertEq(RegExp("\n\r").source, "\\n\\r");
+assertEq(RegExp("\u2028\u2029").source, "\\u2028\\u2029");
 
 assertThrowsInstanceOf(() => genericSource(), TypeError);
 assertThrowsInstanceOf(() => genericSource(1), TypeError);
