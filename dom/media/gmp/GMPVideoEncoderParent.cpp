13c3cfc6e05124ea1667e1cf9ab691542e195982
Matthew Gregan <kinetik@flim.org>
2015-02-10 12:47:07 +1300

    Bug 1131340 - Avoid template aliasing since GCC 4.6 lacks support.  r=cpearce a=lmandel

--- dom/media/gmp/GMPVideoEncoderParent.cpp
+++ dom/media/gmp/GMPVideoEncoderParent.cpp
@@ -126,7 +126,7 @@ GMPVideoEncoderParent::InitEncode(const GMPVideoCodec& aCodecSettings,
 }
 
 GMPErr
-GMPVideoEncoderParent::Encode(GMPUniquePtr<GMPVideoi420Frame> aInputFrame,
+GMPVideoEncoderParent::Encode(GMPUnique<GMPVideoi420Frame>::Ptr aInputFrame,
                               const nsTArray<uint8_t>& aCodecSpecificInfo,
                               const nsTArray<GMPVideoFrameType>& aFrameTypes)
 {
@@ -137,7 +137,7 @@ GMPVideoEncoderParent::Encode(GMPUniquePtr<GMPVideoi420Frame> aInputFrame,
 
   MOZ_ASSERT(mPlugin->GMPThread() == NS_GetCurrentThread());
 
-  GMPUniquePtr<GMPVideoi420FrameImpl> inputFrameImpl(
+  GMPUnique<GMPVideoi420FrameImpl>::Ptr inputFrameImpl(
     static_cast<GMPVideoi420FrameImpl*>(aInputFrame.release()));
 
   // Very rough kill-switch if the plugin stops processing.  If it's merely
