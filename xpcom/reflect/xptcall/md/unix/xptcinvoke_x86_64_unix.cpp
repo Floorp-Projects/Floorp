2052b9ce5bf23a35326ed38b373e6bad78181d2e
L. David Baron <dbaron@dbaron.org>
2015-02-10 16:24:54 +1100

    Bug 680547 bustage fix:  Make #pragmas be #ifndef __clang__.  No review.

--- xpcom/reflect/xptcall/md/unix/xptcinvoke_x86_64_unix.cpp
+++ xpcom/reflect/xptcall/md/unix/xptcinvoke_x86_64_unix.cpp
@@ -108,8 +108,10 @@ invoke_copy_to_stack(uint64_t * d, uint32_t paramCount, nsXPTCVariant * s,
 // 32-byte alignment instead of 16-byte).  This seems to be the best
 // available workaround, given that this code, which should perhaps
 // better be written in assembly, is written in C++.
+#ifndef __clang__
 #pragma GCC push_options
 #pragma GCC target ("no-avx")
+#endif
 
 EXPORT_XPCOM_API(nsresult)
 NS_InvokeByIndex(nsISupports * that, uint32_t methodIndex,
@@ -175,4 +177,6 @@ NS_InvokeByIndex(nsISupports * that, uint32_t methodIndex,
     return result;
 }
 
+#ifndef __clang__
 #pragma GCC pop_options
+#endif
