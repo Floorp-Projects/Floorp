22b1c82346aa481b51799b09d7dc78e881205d3b
David Burns <dburns@mozilla.com>
2015-02-10 00:49:24 +0000

    Bug 1107336: correct loop imports for marionette; r=jgriffin
    --HG--
    extra : rebase_source : 81184a8bdee191e4e9592f885b165ac467b65520
    extra : source : 9aec81381396931906b3ebe40615618ba80024ed

--- testing/marionette/client/marionette/tests/unit/test_navigation.py
+++ testing/marionette/client/marionette/tests/unit/test_navigation.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette_test import MarionetteTestCase, skip_if_b2g, skip_if_e10s
 from errors import MarionetteException, TimeoutException
 
 class TestNavigate(MarionetteTestCase):
