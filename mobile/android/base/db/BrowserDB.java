832cb9a1a33a16c75929a30a266cd47da55b15d3
Richard Newman <rnewman@mozilla.com>
2015-02-10 16:42:13 -0800

    Bug 1131257 - Part 1: split LocalReadingListDB out of LocalBrowserDB. r=margaret
    Centralizing reading list access logic will make Bug 1130461 much easier. This bug is the first part of that.
    
    We follow the same pattern as for URLMetadata, TabsAccessor, and Searches; BrowserDB hands over a single class that's specialized to handle the Reading List.

--- mobile/android/base/db/BrowserDB.java
+++ mobile/android/base/db/BrowserDB.java
@@ -16,7 +16,6 @@ import org.mozilla.gecko.favicons.decoders.LoadFaviconResult;
 
 import android.content.ContentProviderOperation;
 import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.ContentObserver;
 import android.database.Cursor;
@@ -43,6 +42,7 @@ public interface BrowserDB {
     public abstract Searches getSearches();
     public abstract TabsAccessor getTabsAccessor();
     public abstract URLMetadata getURLMetadata();
+    public abstract ReadingListAccessor getReadingListAccessor();
 
     /**
      * Add default bookmarks to the database.
@@ -117,17 +117,6 @@ public interface BrowserDB {
      * Can return <code>null</code>.
      */
     public abstract Cursor getBookmarksInFolder(ContentResolver cr, long folderId);
-
-    /**
-     * Can return <code>null</code>.
-     */
-    public abstract Cursor getReadingList(ContentResolver cr);
-    public abstract Cursor getReadingListUnfetched(ContentResolver cr);
-    public abstract boolean isReadingListItem(ContentResolver cr, String uri);
-    public abstract void addReadingListItem(ContentResolver cr, ContentValues values);
-    public abstract void updateReadingListItem(ContentResolver cr, ContentValues values);
-    public abstract void removeReadingListItemWithURL(ContentResolver cr, String uri);
-
 
     /**
      * Get the favicon from the database, if any, associated with the given favicon URL. (That is,
