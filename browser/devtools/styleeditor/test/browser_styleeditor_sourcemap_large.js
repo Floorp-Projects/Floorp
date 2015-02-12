2ae200132da8fdd0c44b055236015f5fbd6ca91d
Brian Grinstead <bgrinstead@mozilla.com>
2015-02-12 07:34:24 -0800

    Bug 1128747 - Fire onCreate method synchronously in splitview to prevent multiple iframe editors from being appended into style editor. r=jwalker, a=lmandel

---
+++ browser/devtools/styleeditor/test/browser_styleeditor_sourcemap_large.js
@@ -0,0 +1,31 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Covers the case from Bug 1128747, where loading a sourcemapped
+// file prevents the correct editor from being selected on load,
+// and causes a second iframe to be appended when the user clicks
+// editor in the list.
+
+const TESTCASE_URI = TEST_BASE_HTTPS + "sourcemaps-large.html";
+
+add_task(function*() {
+  let {UI} = yield addTabAndOpenStyleEditors(2, null, TESTCASE_URI);
+
+  yield openEditor(UI.editors[0]);
+  let iframes = UI.selectedEditor.details.querySelectorAll("iframe");
+
+  is (iframes.length, 1, "There is only one editor iframe");
+  ok (UI.selectedEditor.summary.classList.contains("splitview-active"),
+    "The editor is selected");
+});
+
+function openEditor(editor) {
+  getLinkFor(editor).click();
+
+  return editor.getSourceEditor();
+}
+
+function getLinkFor(editor) {
+  return editor.summary.querySelector(".stylesheet-name");
+}
