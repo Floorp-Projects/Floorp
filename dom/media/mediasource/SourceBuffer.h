4d1b36a3fee6d4a2d8cbd7a6016cb444880e6024
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-02-12 18:52:12 +1100

    Bug 1130826: Run range removal algorithm when setting mediasource duration. r=cajbir a=lmandel
    --HG--
    extra : source : 5adf82c926e36c66dee8a89b34f62724c7063741
    extra : histedit_source : 520c17ac33fa94818f3003fb390fe6e02c766ff2

--- dom/media/mediasource/SourceBuffer.h
+++ dom/media/mediasource/SourceBuffer.h
@@ -113,8 +113,8 @@ public:
   double GetBufferedEnd();
 
   // Runs the range removal algorithm as defined by the MSE spec.
-  // RangeRemoval will queue a call to DoRangeRemoval.
   void RangeRemoval(double aStart, double aEnd);
+  // Actually remove data between aStart and aEnd
   void DoRangeRemoval(double aStart, double aEnd);
 
 #if defined(DEBUG)
@@ -126,6 +126,7 @@ private:
 
   friend class AsyncEventRunner<SourceBuffer>;
   friend class AppendDataRunnable;
+  friend class RangeRemovalRunnable;
   void DispatchSimpleEvent(const char* aName);
   void QueueAsyncSimpleEvent(const char* aName);
 
