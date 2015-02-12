3ed51f93ba89d68eb810726a707599725561320a
Mark Banner <standard8@mozilla.com>
2015-02-12 13:36:51 +0000

    Bug 1126289 - Enable screen sharing for Loop link-generators by default. r=mikedeboer

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1665,7 +1665,6 @@ pref("shumway.swf.whitelist", "http://g-ecx.images-amazon.com/*/AiryBasicRendere
 pref("image.mem.max_decoded_image_kb", 256000);
 
 pref("loop.enabled", true);
-pref("loop.screenshare.enabled", false);
 pref("loop.server", "https://loop.services.mozilla.com/v0");
 pref("loop.seenToS", "unseen");
 pref("loop.showPartnerLogo", true);
