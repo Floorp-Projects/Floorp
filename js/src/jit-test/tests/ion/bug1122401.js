c033e4247c9a426fb0f35e836a150d6240005220
Benjamin Bouvier <benj@benj.me>
2015-02-12 16:46:04 +0100

    Bug 1122401: Add other checks for SoftUDivMod codegen; r=nbp
    --HG--
    extra : rebase_source : f5ce77205216e35e34aa3b2a5c49d4adea667006

--- js/src/jit-test/tests/ion/bug1122401.js
+++ js/src/jit-test/tests/ion/bug1122401.js
@@ -3,3 +3,16 @@ function f(x) {
 }
 f(2);
 assertEq(f(1), 1);
+
+function g(x, y) {
+    var x1 = 0;
+    var x2 = Math.tan(y);
+    var t1 = (x1 >>> 0);
+    var t2 = (x2 >>> 0);
+    var a = t1 / t2;
+    var sub = 1 - a;
+    var e = sub | 0;
+    return e;
+}
+g(8, 4)
+assertEq(g(1, 0), 0);
