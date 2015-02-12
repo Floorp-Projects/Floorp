5b255e4647647f36283fa2f8ed8db2b542631de3
Steve Singer <steve@ssinger.info>
2015-02-12 13:45:00 +0100

    Bug 1130910 - Fix non-ion jit compile issues. r=jandem

--- js/src/jit/none/Architecture-none.h
+++ js/src/jit/none/Architecture-none.h
@@ -21,7 +21,11 @@ static const uint32_t AsmJSStackAlignment = 4;
 class Registers
 {
   public:
-    typedef uint8_t Code;
+    enum RegisterID {
+        r0 = 0,
+        invalid_reg
+    };
+    typedef RegisterID Code;
     typedef uint8_t SetType;
 
     static uint32_t SetSize(SetType) { MOZ_CRASH(); }
@@ -30,8 +34,8 @@ class Registers
     static const char *GetName(Code) { MOZ_CRASH(); }
     static Code FromName(const char *) { MOZ_CRASH(); }
 
-    static const Code StackPointer = 0;
-    static const Code Invalid = 0;
+    static const Code StackPointer = invalid_reg;
+    static const Code Invalid = invalid_reg;
     static const uint32_t Total = 1;
     static const uint32_t TotalPhys = 0;
     static const uint32_t Allocatable = 0;
@@ -51,13 +55,17 @@ typedef uint8_t PackedRegisterMask;
 class FloatRegisters
 {
   public:
-    typedef uint8_t Code;
+    enum FPRegisterID {
+        f0 = 0,
+        invalid_reg
+    };
+    typedef FPRegisterID  Code;
     typedef uint32_t SetType;
 
     static const char *GetName(Code) { MOZ_CRASH(); }
     static Code FromName(const char *) { MOZ_CRASH(); }
 
-    static const Code Invalid = 0;
+    static const Code Invalid = invalid_reg;
     static const uint32_t Total = 0;
     static const uint32_t TotalPhys = 0;
     static const uint32_t Allocatable = 0;
