76046ae417b21d241058a395076480ae2267abbb
Boris Zbarsky <bzbarsky@mit.edu>
2015-02-13 14:34:53 -0500

    Bug 1131796.  Don't parent the DOM proxy expando object to the proxy.  r=peterv

--- dom/bindings/DOMJSProxyHandler.cpp
+++ dom/bindings/DOMJSProxyHandler.cpp
@@ -112,9 +112,8 @@ DOMProxyHandler::EnsureExpandoObject(JSContext* cx, JS::Handle<JSObject*> obj)
     expandoAndGeneration = nullptr;
   }
 
-  JS::Rooted<JSObject*> parent(cx, js::GetObjectParent(obj));
   JS::Rooted<JSObject*> expando(cx,
-    JS_NewObjectWithGivenProto(cx, nullptr, JS::NullPtr(), parent));
+    JS_NewObjectWithGivenProto(cx, nullptr, JS::NullPtr()));
   if (!expando) {
     return nullptr;
   }
