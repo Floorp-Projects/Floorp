27e769dd6d698e4fb92a91932dc0d209b278c463
Ben Hsu <bhsu@mozilla.com>
2015-02-13 00:23:00 -0500

    Bug 1067629 - Part 4: Update the related testcase. r=aknow

--- dom/mobileconnection/tests/marionette/test_mobile_set_radio.js
+++ dom/mobileconnection/tests/marionette/test_mobile_set_radio.js
@@ -4,6 +4,26 @@
 MARIONETTE_TIMEOUT = 60000;
 MARIONETTE_HEAD_JS = "head.js";
 
+function testAutoRestoreRadioState(aEnabled, aServiceId) {
+  return setRadioEnabledAndWait(aEnabled, aServiceId)
+    .then(() => {
+      let expectedSequence = aEnabled ? ["enabling", "enabled"] :
+                                        ["disabling", "disabled"];
+
+      let p1 = waitForManagerEvent("radiostatechange", aServiceId, function() {
+        let mobileConn = getMozMobileConnectionByServiceId(aServiceId);
+        let expectedRadioState = expectedSequence.shift();
+        is(mobileConn.radioState, expectedRadioState, "Check radio state");
+        return expectedSequence.length === 0;
+      });
+
+      let command = "modem radio " + (aEnabled ? "disable" : "enable");
+      let p2 = runEmulatorCmdSafe(command);
+
+      return Promise.all([p1, p2]);
+    });
+}
+
 // Start tests
 startTestCommon(function() {
 
@@ -14,10 +34,17 @@ startTestCommon(function() {
     })
 
     // Test disabling/enabling radio power.
+    .then(() => log("Test disabling/enabling radio power"))
     .then(() => setRadioEnabledAndWait(false))
     .then(() => setRadioEnabledAndWait(true))
 
+    // Test auto-restore radio state
+    .then(() => log("Test auto-restore radio state"))
+    .then(() => testAutoRestoreRadioState(false))
+    .then(() => testAutoRestoreRadioState(true))
+
     // Test disabling radio when data is connected.
+    .then(() => log("Test disabling radio when data is connected"))
     .then(() => {
       let apnSettings = [[
         {"carrier":"T-Mobile US",
@@ -34,6 +61,7 @@ startTestCommon(function() {
     })
 
     // Restore test environment.
+    .then(() => log("Restore test environment"))
     .then(() => setDataApnSettings(origApnSettings))
     .then(() => setDataEnabled(false))
     .then(() => setRadioEnabledAndWait(true));
