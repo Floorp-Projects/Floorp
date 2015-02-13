bdc0ed9dd8abe88504e3dcc627c52200c204dfdf
Bevis Tseng <btseng@mozilla.com>
2015-02-13 14:46:49 +0800

    Bug 1114935 - Part 7: Add Backward Compatibility for v2.2 binary implementation. r=echen

--- dom/icc/IccManager.cpp
+++ dom/icc/IccManager.cpp
@@ -13,6 +13,8 @@
 // Service instantiation
 #include "ipc/IccIPCService.h"
 #if defined(MOZ_WIDGET_GONK) && defined(MOZ_B2G_RIL)
+#include "nsIRadioInterfaceLayer.h"
+#include "nsRadioInterfaceLayer.h"
 #include "nsIGonkIccService.h"
 #endif
 #include "nsXULAppAPI.h" // For XRE_GetProcessType()
@@ -145,7 +147,12 @@ NS_CreateIccService()
     service = new mozilla::dom::icc::IccIPCService();
 #if defined(MOZ_WIDGET_GONK) && defined(MOZ_B2G_RIL)
   } else {
-    service = do_GetService(GONK_ICC_SERVICE_CONTRACTID);
+    nsCOMPtr <nsIRadioInterfaceLayer> ril =
+      do_GetService(NS_RADIOINTERFACELAYER_CONTRACTID);
+    nsCOMPtr <nsIRadioInterfaceLayer_new> ril_new(do_QueryInterface(ril));
+
+    service = (ril_new) ? do_GetService(GONK_ICC_SERVICE_CONTRACTID)
+                        : do_GetService(NS_RILCONTENTHELPER_CONTRACTID);
 #endif
   }
 
