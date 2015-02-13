9490d92a7a8893058e9bcb89410ee43b152a6468
Yusuke Yamamoto <yu-yamamoto@kddi-tech.com>
2015-02-12 23:58:15 -0800

    Bug 839120 - Support SafeBrowsing in b2g r=gpascutto

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -70,6 +70,11 @@ XPCOMUtils.defineLazyServiceGetter(Services, 'captivePortalDetector',
                                   'nsICaptivePortalDetector');
 #endif
 
+#ifdef MOZ_SAFE_BROWSING
+XPCOMUtils.defineLazyModuleGetter(this, "SafeBrowsing",
+              "resource://gre/modules/SafeBrowsing.jsm");
+#endif
+
 function getContentWindow() {
   return shell.contentBrowser.contentWindow;
 }
@@ -362,6 +367,11 @@ var shell = {
     ppmm.addMessageListener("sms-handler", this);
     ppmm.addMessageListener("mail-handler", this);
     ppmm.addMessageListener("file-picker", this);
+#ifdef MOZ_SAFE_BROWSING
+    setTimeout(function() {
+      SafeBrowsing.init();
+    }, 5000);
+#endif
   },
 
   stop: function shell_stop() {
