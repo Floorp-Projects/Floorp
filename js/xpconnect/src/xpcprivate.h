f456caa314ce57f126d222f7aa74ccbb101795cd
Boris Zbarsky <bzbarsky@mit.edu>
2015-02-13 14:34:54 -0500

    Bug 1132184.  Remove the nsIXPCScriptable postCreate and postTransplant hooks.  r=peterv

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -1614,7 +1614,6 @@ public:
 
     bool WantPreCreate()                GET_IT(WANT_PRECREATE)
     bool WantCreate()                   GET_IT(WANT_CREATE)
-    bool WantPostCreate()               GET_IT(WANT_POSTCREATE)
     bool WantAddProperty()              GET_IT(WANT_ADDPROPERTY)
     bool WantDelProperty()              GET_IT(WANT_DELPROPERTY)
     bool WantGetProperty()              GET_IT(WANT_GETPROPERTY)
