d6cba3bd8804acfbb784215abc2fa6c0c185bfd2
Terrence Cole <terrence@mozilla.com>
2015-02-13 11:36:40 -0800

    Bug 1132286 - Simplify NewObjectCache allocation path now that we are exactly rooted; r=sfink

--- js/src/vm/Runtime-inl.h
+++ js/src/vm/Runtime-inl.h
@@ -38,15 +38,14 @@ NewObjectCache::fillGlobal(EntryIndex entry, const Class *clasp, js::GlobalObjec
     return fill(entry, clasp, global, kind, obj);
 }
 
-template <AllowGC allowGC>
 inline JSObject *
-NewObjectCache::newObjectFromHit(JSContext *cx, EntryIndex entry_, js::gc::InitialHeap heap)
+NewObjectCache::newObjectFromHit(JSContext *cx, EntryIndex entryIndex, js::gc::InitialHeap heap)
 {
     // The new object cache does not account for metadata attached via callbacks.
     MOZ_ASSERT(!cx->compartment()->hasObjectMetadataCallback());
 
-    MOZ_ASSERT(unsigned(entry_) < mozilla::ArrayLength(entries));
-    Entry *entry = &entries[entry_];
+    MOZ_ASSERT(unsigned(entryIndex) < mozilla::ArrayLength(entries));
+    Entry *entry = &entries[entryIndex];
 
     JSObject *templateObj = reinterpret_cast<JSObject *>(&entry->templateObject);
 
@@ -60,16 +59,6 @@ NewObjectCache::newObjectFromHit(JSContext *cx, EntryIndex entry_, js::gc::Initi
     if (cx->runtime()->gc.upcomingZealousGC())
         return nullptr;
 
-    // Trigger an identical allocation to the one that notified us of OOM
-    // so that we trigger the right kind of GC automatically.
-    if (allowGC) {
-        mozilla::DebugOnly<JSObject *> obj =
-            js::gc::AllocateObjectForCacheHit<allowGC>(cx, entry->kind, heap, group->clasp());
-        MOZ_ASSERT(!obj);
-        return nullptr;
-    }
-
-    MOZ_ASSERT(allowGC == NoGC);
     JSObject *obj = js::gc::AllocateObjectForCacheHit<NoGC>(cx, entry->kind, heap, group->clasp());
     if (obj) {
         copyCachedToObject(obj, templateObj, entry->kind);
@@ -78,6 +67,11 @@ NewObjectCache::newObjectFromHit(JSContext *cx, EntryIndex entry_, js::gc::Initi
         return obj;
     }
 
+    // Trigger an identical allocation to the one that notified us of OOM
+    // so that we trigger the right kind of GC automatically.
+    mozilla::DebugOnly<JSObject *> obj2 =
+        js::gc::AllocateObjectForCacheHit<CanGC>(cx, entry->kind, heap, group->clasp());
+    MOZ_ASSERT(!obj2);
     return nullptr;
 }
 
