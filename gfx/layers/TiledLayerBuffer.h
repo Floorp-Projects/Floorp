5385b6f957675830ca38f1074a433be4b889ce2e
Nicolas Silva <nsilva@mozilla.com>
2015-02-14 12:14:59 +0100

    Bug 1129871 - Favor passing tiles by ref rather than copy part 2. r=BenWa

--- gfx/layers/TiledLayerBuffer.h
+++ gfx/layers/TiledLayerBuffer.h
@@ -528,7 +528,7 @@ TiledLayerBuffer<Derived, Tile>::Update(const nsIntRegion& newValidRegion,
                  static_cast<unsigned>(index) < newRetainedTiles.Length(),
                  "index out of range");
 
-      Tile newTile = newRetainedTiles[index];
+      Tile& newTile = newRetainedTiles[index];
 
       // Try to reuse a tile from the old retained tiles that had no partially
       // valid content.
@@ -550,8 +550,6 @@ TiledLayerBuffer<Derived, Tile>::Update(const nsIntRegion& newValidRegion,
 #ifdef GFX_TILEDLAYER_PREF_WARNINGS
       printf_stderr("Store Validate tile %i, %i -> %i\n", tileStartX, tileStartY, index);
 #endif
-      newRetainedTiles[index] = newTile;
-
       y += height;
     }
 
