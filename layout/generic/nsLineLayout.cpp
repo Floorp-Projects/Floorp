7395fc46c800c1d3e67dbe6edc67cfd228a99263
Xidorn Quan <quanxunzhen@gmail.com>
2015-02-17 14:25:18 +1300

    Bug 1132008 part 3 - Remove unused code in nsLineLayout. r=dbaron
    This code was originally introduced in bug 1055665 part 3.

--- layout/generic/nsLineLayout.cpp
+++ layout/generic/nsLineLayout.cpp
@@ -104,9 +104,6 @@ nsLineLayout::nsLineLayout(nsPresContext* aPresContext,
   mTotalPlacedFrames = 0;
   mBStartEdge = 0;
   mTrimmableISize = 0;
-#ifdef DEBUG
-  mFinalLineBSize = nscoord_MIN;
-#endif
 
   mInflationMinFontSize =
     nsLayoutUtils::InflationMinFontSizeFor(aOuterReflowState->frame);
