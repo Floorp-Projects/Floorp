03dcbf223bc120060e512414790070c9e257084d
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-02-17 22:07:48 +1300

    Bug 1132796: Revert incorrect change on A CLOSED TREE. r=me
    Revision 355f5e2dee58 got overwritten in dad2b3245457

--- dom/media/mediasource/TrackBuffer.cpp
+++ dom/media/mediasource/TrackBuffer.cpp
@@ -341,7 +341,7 @@ TrackBuffer::EvictData(double aPlaybackTime,
                 playbackOffset, decoders[i]->GetResource()->GetSize());
       if (playbackOffset > 0) {
         toEvict -= decoders[i]->GetResource()->EvictData(playbackOffset,
-                                                         toEvict);
+                                                         playbackOffset);
       }
     }
   }
