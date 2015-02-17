b31dcf9f7d8086b0ebb491401e627a9a2effb339
Sean Stangl <sstangl@mozilla.com>
2015-02-17 15:59:59 -0800

    Bug 1134359 - Remove maybeStrSpecial. r=evilpie

--- js/src/frontend/TokenStream.h
+++ js/src/frontend/TokenStream.h
@@ -763,7 +763,7 @@ class MOZ_STACK_CLASS TokenStream
 
     bool getTokenInternal(TokenKind *ttp, Modifier modifier);
 
-    bool getStringOrTemplateToken(int qc, Token **tp);
+    bool getStringOrTemplateToken(int untilChar, Token **tp);
 
     int32_t getChar();
     int32_t getCharIgnoreEOL();
@@ -824,7 +824,6 @@ class MOZ_STACK_CLASS TokenStream
     mozilla::UniquePtr<char16_t[], JS::FreePolicy> displayURL_; // the user's requested source URL or null
     mozilla::UniquePtr<char16_t[], JS::FreePolicy> sourceMapURL_; // source map's filename or null
     CharBuffer          tokenbuf;           // current token string buffer
-    bool                maybeStrSpecial[256];   // speeds up string scanning
     uint8_t             isExprEnding[TOK_LIMIT];// which tokens definitely terminate exprs?
     ExclusiveContext    *const cx;
     bool                mutedErrors;
