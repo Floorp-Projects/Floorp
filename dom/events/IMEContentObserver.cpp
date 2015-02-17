8cb1f3a82b9910c06ed9b62ee93bd309368cbac2
Masayuki Nakano <masayuki@d-toybox.com>
2015-02-17 22:20:08 +0900

    Bug 1133629 - IMEContentObserver should grab itself before using ContentEventHandler because it causes flushing pending layout. r=smaug, a=sledru

--- dom/events/IMEContentObserver.cpp
+++ dom/events/IMEContentObserver.cpp
@@ -430,9 +430,11 @@ IMEContentObserver::OnMouseButtonEvent(nsPresContext* aPresContext,
     default:
       return false;
   }
-  if (NS_WARN_IF(!mWidget)) {
+  if (NS_WARN_IF(!mWidget) || NS_WARN_IF(mWidget->Destroyed())) {
     return false;
   }
+
+  nsRefPtr<IMEContentObserver> kungFuDeathGrip(this);
 
   WidgetQueryContentEvent charAtPt(true, NS_QUERY_CHARACTER_AT_POINT,
                                    aMouseEvent->widget);
@@ -441,6 +443,12 @@ IMEContentObserver::OnMouseButtonEvent(nsPresContext* aPresContext,
   handler.OnQueryCharacterAtPoint(&charAtPt);
   if (NS_WARN_IF(!charAtPt.mSucceeded) ||
       charAtPt.mReply.mOffset == WidgetQueryContentEvent::NOT_FOUND) {
+    return false;
+  }
+
+  // The widget might be destroyed during querying the content since it
+  // causes flushing layout.
+  if (!mWidget || NS_WARN_IF(mWidget->Destroyed())) {
     return false;
   }
 
