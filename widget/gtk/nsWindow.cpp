4ae7286acb00e6e6fd10b324c3ffc71d970268d0
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-02-17 14:32:11 +0100

    Backed out changeset d81339c7ac52 (bug 1128934)

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -11,8 +11,6 @@
 #include "mozilla/TextEvents.h"
 #include <algorithm>
 
-#include "GeckoProfiler.h"
-
 #include "prlink.h"
 #include "nsGTKToolkit.h"
 #include "nsIRollupListener.h"
@@ -1527,12 +1525,6 @@ nsWindow::GetScreenBounds(nsIntRect &aRect)
     return NS_OK;
 }
 
-gfx::IntSize
-nsWindow::GetClientSize()
-{
-  return gfx::IntSize(mBounds.width, mBounds.height);
-}
-
 NS_IMETHODIMP
 nsWindow::GetClientBounds(nsIntRect &aRect)
 {
@@ -1548,8 +1540,6 @@ nsWindow::GetClientBounds(nsIntRect &aRect)
 nsIntPoint
 nsWindow::GetClientOffset()
 {
-    PROFILER_LABEL("nsWindow", "GetClientOffset", js::ProfileEntry::Category::GRAPHICS);
-
     if (!mIsTopLevel) {
         return nsIntPoint(0, 0);
     }
