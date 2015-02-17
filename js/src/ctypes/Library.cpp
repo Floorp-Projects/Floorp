4cb3ff1a342353269e78ab8678529cd033e39a32
Steve Fink <sfink@mozilla.com>
2015-02-17 15:38:38 -0800

    Backed out 2 changesets (bug 1131424) for continuing problems on a CLOSED TREE
    Backed out changeset c43187545daa (bug 1131424)
    Backed out changeset 0e7e4e9718b7 (bug 1131424)

--- js/src/ctypes/Library.cpp
+++ js/src/ctypes/Library.cpp
@@ -6,7 +6,6 @@
 
 #include "ctypes/Library.h"
 
-#include "prerror.h"
 #include "prlink.h"
 
 #include "ctypes/CTypes.h"
@@ -147,17 +146,12 @@ Library::Create(JSContext* cx, jsval path_, const JSCTypesCallbacks* callbacks)
   PRLibrary* library = PR_LoadLibraryWithFlags(libSpec, 0);
 
   if (!library) {
-    char *error = (char*) JS_malloc(cx, PR_GetErrorTextLength() + 1);
-    if (error)
-      PR_GetErrorText(error);
-
 #ifdef XP_WIN
-    JS_ReportError(cx, "couldn't open library %hs: %s", pathChars, error);
+    JS_ReportError(cx, "couldn't open library %hs", pathChars);
 #else
-    JS_ReportError(cx, "couldn't open library %s: %s", pathBytes, error);
+    JS_ReportError(cx, "couldn't open library %s", pathBytes);
     JS_free(cx, pathBytes);
 #endif
-    JS_free(cx, error);
     return nullptr;
   }
 
