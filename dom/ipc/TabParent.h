8511a2344f2491478ada8285051fc7423d07b329
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-02-17 09:28:52 +0100

    Backed out changeset 516a170b8fda (bug 1131695) for android m4 perma failure on a CLOSED TREE

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -267,7 +267,6 @@ public:
     bool MapEventCoordinatesForChildProcess(mozilla::WidgetEvent* aEvent);
     void MapEventCoordinatesForChildProcess(const LayoutDeviceIntPoint& aOffset,
                                             mozilla::WidgetEvent* aEvent);
-    LayoutDeviceToCSSScale GetLayoutDeviceToCSSScale();
 
     virtual bool RecvRequestNativeKeyBindings(const mozilla::WidgetKeyboardEvent& aEvent,
                                               MaybeNativeKeyBinding* aBindings) MOZ_OVERRIDE;
