f01ccc2fd4e814b1d171193eb59cbf424f68e9d7
Kartikaya Gupta <kgupta@mozilla.com>
2015-02-16 21:41:49 -0500

    Bug 1131695 - Ensure that mouse and touch events sent via BrowserElementParent to the child are offsetted correctly. r=fabrice

--- dom/browser-element/mochitest/browserElement_SendEvent.js
+++ dom/browser-element/mochitest/browserElement_SendEvent.js
@@ -12,9 +12,26 @@ function runTest() {
   var iframe = document.createElement("iframe");
   iframe.setAttribute('mozbrowser', 'true');
   document.body.appendChild(iframe);
+  var x = 10;
+  var y = 10;
+  // First we force a reflow so that getChildProcessOffset actually returns
+  // meaningful data.
+  iframe.getBoundingClientRect();
+  // We need to make sure the event coordinates are actually inside the iframe,
+  // relative to the chome window.
+  var tabParent = SpecialPowers.wrap(iframe)
+                  .QueryInterface(SpecialPowers.Ci.nsIFrameLoaderOwner)
+                  .frameLoader.tabParent;
+  if (tabParent) {
+    let offsetX = {};
+    let offsetY = {};
+    tabParent.getChildProcessOffset(offsetX, offsetY);
+    x -= offsetX.value;
+    y -= offsetY.value;
+  }
 
   iframe.addEventListener("mozbrowserloadend", function onloadend(e) {
-    iframe.sendMouseEvent("mousedown", 10, 10, 0, 1, 0);
+    iframe.sendMouseEvent("mousedown", x, y, 0, 1, 0);
   });
 
   iframe.addEventListener("mozbrowserlocationchange", function onlocchange(e) {
@@ -24,11 +41,11 @@ function runTest() {
     switch (a.hash) {
       case "#mousedown":
         ok(true, "Receive a mousedown event.");
-        iframe.sendMouseEvent("mousemove", 10, 10, 0, 0, 0);
+        iframe.sendMouseEvent("mousemove", x, y, 0, 0, 0);
         break;
       case "#mousemove":
         ok(true, "Receive a mousemove event.");
-        iframe.sendMouseEvent("mouseup", 10, 10, 0, 1, 0);
+        iframe.sendMouseEvent("mouseup", x, y, 0, 1, 0);
         break;
       case "#mouseup":
         ok(true, "Receive a mouseup event.");
@@ -36,7 +53,7 @@ function runTest() {
       case "#click":
         ok(true, "Receive a click event.");
         if (SpecialPowers.getIntPref("dom.w3c_touch_events.enabled") != 0) {
-          iframe.sendTouchEvent("touchstart", [1], [10], [10], [2], [2],
+          iframe.sendTouchEvent("touchstart", [1], [x], [y], [2], [2],
                                 [20], [0.5], 1, 0);
         } else {
           iframe.removeEventListener('mozbrowserlocationchange', onlocchange);
@@ -45,16 +62,16 @@ function runTest() {
         break;
       case "#touchstart":
         ok(true, "Receive a touchstart event.");
-        iframe.sendTouchEvent("touchmove", [1], [10], [10], [2], [2],
+        iframe.sendTouchEvent("touchmove", [1], [x], [y], [2], [2],
                               [20], [0.5], 1, 0);
       case "#touchmove":
         ok(true, "Receive a touchmove event.");
-        iframe.sendTouchEvent("touchend", [1], [10], [10], [2], [2],
+        iframe.sendTouchEvent("touchend", [1], [x], [y], [2], [2],
                               [20], [0.5], 1, 0);
         break;
       case "#touchend":
         ok(true, "Receive a touchend event.");
-        iframe.sendTouchEvent("touchcancel", [1], [10], [10], [2], [2],
+        iframe.sendTouchEvent("touchcancel", [1], [x], [y], [2], [2],
                               [20], [0.5], 1, 0);
         iframe.removeEventListener('mozbrowserlocationchange', onlocchange);
         SimpleTest.finish();
