2b0e4efb61241fdbc7723e6f521ff09817e08f83
Michael Comella <michael.l.comella@gmail.com>
2015-02-18 14:23:19 -0800

    Bug 1133770 - Display the selected tab in the tab strip on device rotation. r=mhaigh
    This does not always work in the case that one of the last few tabs (to the
    right) are selected and the device is rotated from landscape to portrait.
    Filed bug 1134408 to track this.
    
    --HG--
    extra : rebase_source : f9071610c50fc344d55e54876252c7e2cdca09bd

--- mobile/android/base/tabs/TabStrip.java
+++ mobile/android/base/tabs/TabStrip.java
@@ -128,6 +128,10 @@ public class TabStrip extends ThemedLinearLayout {
         }
     }
 
+    public void refresh() {
+        tabStripView.refresh();
+    }
+
     @Override
     public void onLightweightThemeChanged() {
         final Drawable drawable = getTheme().getDrawable(this);
