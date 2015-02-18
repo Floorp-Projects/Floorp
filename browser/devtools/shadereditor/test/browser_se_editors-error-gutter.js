17adb0638272c7684e0769b2e4e884234395d0f2
Jordan Santell <jsantell@gmail.com>
2015-02-18 15:25:00 -0500

    Bug 1134082 - Fix uncaught promise exceptions in shader editor. r=vp

--- browser/devtools/shadereditor/test/browser_se_editors-error-gutter.js
+++ browser/devtools/shadereditor/test/browser_se_editors-error-gutter.js
@@ -1,13 +1,6 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-///////////////////
-//
-// Whitelisting this test.
-// As part of bug 1077403, the leaking uncaught rejection should be fixed. 
-//
-thisTestLeaksUncaughtRejectionsAndShouldBeFixed("Error: Shader Editor is still waiting for a WebGL context to be created.");
-
 /**
  * Tests if error indicators are shown in the editor's gutter and text area
  * when there's a shader compilation error.
