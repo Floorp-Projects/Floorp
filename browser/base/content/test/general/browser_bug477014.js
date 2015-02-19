7fa05a5ed4c0293a586b3e84932c10be9314f42f
Dave Townsend <dtownsend@oxymoronical.com>
2015-02-18 16:47:43 -0800

    Bug 1087966: Fix tab detach in e10s windows. r=mconley
    When swapping docshells we also have to swap any properties on remote-browsers
    that are cached from the content process. This includes things like the
    remoteWebNavigation etc. which in turn cache content information. Some of
    these also maintain message listeners that we have to switch to the new browser
    and message manager.

--- browser/base/content/test/general/browser_bug477014.js
+++ browser/base/content/test/general/browser_bug477014.js
@@ -6,50 +6,19 @@
 const iconURLSpec = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAHWSURBVHjaYvz//z8DJQAggJiQOe/fv2fv7Oz8rays/N+VkfG/iYnJfyD/1+rVq7ffu3dPFpsBAAHEAHIBCJ85c8bN2Nj4vwsDw/8zQLwKiO8CcRoQu0DxqlWrdsHUwzBAAIGJmTNnPgYa9j8UqhFElwPxf2MIDeIrKSn9FwSJoRkAEEAM0DD4DzMAyPi/G+QKY4hh5WAXGf8PDQ0FGwJ22d27CjADAAIIrLmjo+MXA9R2kAHvGBA2wwx6B8W7od6CeQcggKCmCEL8bgwxYCbUIGTDVkHDBia+CuotgACCueD3TDQN75D4xmAvCoK9ARMHBzAw0AECiBHkAlC0Mdy7x9ABNA3obAZXIAa6iKEcGlMVQHwWyjYuL2d4v2cPg8vZswx7gHyAAAK7AOif7SAbOqCmn4Ha3AHFsIDtgPq/vLz8P4MSkJ2W9h8ggBjevXvHDo4FQUQg/kdypqCg4H8lUIACnQ/SOBMYI8bAsAJFPcj1AAEEjwVQqLpAbXmH5BJjqI0gi9DTAAgDBBCcAVLkgmQ7yKCZxpCQxqUZhAECCJ4XgMl493ug21ZD+aDAXH0WLM4A9MZPXJkJIIAwTAR5pQMalaCABQUULttBGCCAGCnNzgABBgAMJ5THwGvJLAAAAABJRU5ErkJggg==";
 var testPage="data:text/plain,test bug 477014";
 
-function test() {
-  waitForExplicitFinish();
+add_task(function*() {
+  let tabToDetach = gBrowser.addTab(testPage);
+  yield waitForDocLoadComplete(tabToDetach.linkedBrowser);
 
-  var newWindow;
-  var tabToDetach;
-  var documentToDetach;
+  gBrowser.setIcon(tabToDetach, iconURLSpec);
+  tabToDetach.setAttribute("busy", "true");
 
-  function onPageShow(event) {
-    // we get here if the test is executed before the pageshow
-    // event for the window's first tab
-    if (!tabToDetach || documentToDetach != event.target)
-      return;
+  // detach and set the listener on the new window
+  let newWindow = gBrowser.replaceTabWithWindow(tabToDetach);
+  yield promiseWaitForEvent(tabToDetach.linkedBrowser, "SwapDocShells");
 
-    event.currentTarget.removeEventListener("pageshow", onPageShow, false);
+  is(newWindow.gBrowser.selectedTab.hasAttribute("busy"), true, "Busy attribute should be correct");
+  is(newWindow.gBrowser.getIcon(), iconURLSpec, "Icon should be correct");
 
-    if (!newWindow) {
-      // prepare the tab (set icon and busy state)
-      // we have to set these only after onState* notification, otherwise
-      // they're overriden
-      setTimeout(function() {
-        gBrowser.setIcon(tabToDetach, iconURLSpec);
-        tabToDetach.setAttribute("busy", "true");
-
-        // detach and set the listener on the new window
-        newWindow = gBrowser.replaceTabWithWindow(tabToDetach);
-        // wait for gBrowser to come along
-        newWindow.addEventListener("load", function () {
-          newWindow.removeEventListener("load", arguments.callee, false);
-          newWindow.gBrowser.addEventListener("pageshow", onPageShow, false);
-        }, false);
-      }, 0);
-      return;
-    }
-
-    is(newWindow.gBrowser.selectedTab.hasAttribute("busy"), true);
-    is(newWindow.gBrowser.getIcon(), iconURLSpec);
-    newWindow.close();
-    finish();
-  }
-
-  tabToDetach = gBrowser.addTab(testPage);
-  tabToDetach.linkedBrowser.addEventListener("load", function onLoad() {
-    tabToDetach.linkedBrowser.removeEventListener("load", onLoad, true);
-    documentToDetach = tabToDetach.linkedBrowser.contentDocument;
-    gBrowser.addEventListener("pageshow", onPageShow, false);
-  }, true);
-}
+  newWindow.close();
+});
