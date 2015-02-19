979c55a8511c22c054c8a58011c3977c19b6e187
Michael Comella <michael.l.comella@gmail.com>
2015-02-19 14:38:32 -0800

    Bug 1123904 - Display original title in toolbar in reader mode. r=margaret

--- mobile/android/base/toolbar/ToolbarDisplayLayout.java
+++ mobile/android/base/toolbar/ToolbarDisplayLayout.java
@@ -14,6 +14,7 @@ import org.mozilla.gecko.AppConstants.Versions;
 import org.mozilla.gecko.BrowserApp;
 import org.mozilla.gecko.NewTabletUI;
 import org.mozilla.gecko.R;
+import org.mozilla.gecko.ReaderModeUtils;
 import org.mozilla.gecko.SiteIdentity;
 import org.mozilla.gecko.SiteIdentity.SecurityMode;
 import org.mozilla.gecko.SiteIdentity.MixedMode;
@@ -360,10 +361,13 @@ public class ToolbarDisplayLayout extends ThemedLinearLayout
             return;
         }
 
-        CharSequence title = url;
+        String strippedURL = stripAboutReaderURL(url);
+
         if (mPrefs.shouldTrimUrls()) {
-            title = StringUtils.stripCommonSubdomains(StringUtils.stripScheme(url));
+            strippedURL = StringUtils.stripCommonSubdomains(StringUtils.stripScheme(strippedURL));
         }
+
+        CharSequence title = strippedURL;
 
         final String baseDomain = tab.getBaseDomain();
         if (!TextUtils.isEmpty(baseDomain)) {
@@ -380,6 +384,14 @@ public class ToolbarDisplayLayout extends ThemedLinearLayout
         }
 
         setTitle(title);
+    }
+
+    private String stripAboutReaderURL(final String url) {
+        if (!AboutPages.isAboutReader(url)) {
+            return url;
+        }
+
+        return ReaderModeUtils.getUrlFromAboutReader(url);
     }
 
     private void updateFavicon(Tab tab) {
