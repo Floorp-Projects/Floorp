e708d96534d897d4efb8d80b873616d7f4bf47e0
Benjamin Bouvier <benj@benj.me>
2015-02-19 17:28:28 +0100

    Bug 1134638: 1. Add OperationName to MSimdBinaryBitwise; r=sunfish

--- js/src/jit/LIR-Common.h
+++ js/src/jit/LIR-Common.h
@@ -520,6 +520,9 @@ class LSimdBinaryBitwiseX4 : public LInstructionHelper<1, 2, 0>
     MSimdBinaryBitwise::Operation operation() const {
         return mir_->toSimdBinaryBitwise()->operation();
     }
+    const char *extraName() const {
+        return MSimdBinaryBitwise::OperationName(operation());
+    }
     MIRType type() const {
         return mir_->type();
     }
