159638c466e663a26946df8dfb3824aea717a5d1
Valentin Gosu <valentin.gosu@gmail.com>
2015-02-20 17:10:33 +0200

    Bug 1132693 - Add back check for mManageOfflineStatus r=jduell

--- netwerk/base/nsIOService.cpp
+++ netwerk/base/nsIOService.cpp
@@ -1413,6 +1413,10 @@ nsIOService::OnNetworkLinkEvent(const char *data)
     if (mShutdown)
         return NS_ERROR_NOT_AVAILABLE;
 
+    if (!mManageOfflineStatus) {
+      return NS_OK;
+    }
+
     if (!strcmp(data, NS_NETWORK_LINK_DATA_DOWN)) {
         // check to make sure this won't collide with Autodial
         if (mSocketTransportService) {
