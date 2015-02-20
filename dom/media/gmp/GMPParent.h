ad3926b9fb001669abe8f6f8e1be49d36d57d18a
Chris Pearce <cpearce@mozilla.com>
2015-02-20 14:37:49 +1300

    Bug 1124031 part 1 - Expose GMP version on GMPParent. r=jesup a=lmandel ba=lmandel

--- dom/media/gmp/GMPParent.h
+++ dom/media/gmp/GMPParent.h
@@ -115,6 +115,8 @@ public:
   void SetNodeId(const nsACString& aNodeId);
   const nsACString& GetNodeId() const { return mNodeId; }
 
+  const nsCString& GetVersion() const;
+
   // Returns true if a plugin can be or is being used across multiple NodeIds.
   bool CanBeSharedCrossNodeIds() const;
 
