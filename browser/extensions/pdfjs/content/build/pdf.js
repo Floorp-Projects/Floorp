62c8f26ce0e032b7a5fcd0af8980153ba77ef335
Ryan VanderMeulen <ryanvm@gmail.com>
2015-02-20 17:01:13 -0500

    Bug 1135021 - Update pdf.js to version 1.0.1149. r=bdahl

--- browser/extensions/pdfjs/content/build/pdf.js
+++ browser/extensions/pdfjs/content/build/pdf.js
@@ -22,8 +22,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.0.1147';
-PDFJS.build = '6bb0a48';
+PDFJS.version = '1.0.1149';
+PDFJS.build = 'bc7a110';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
