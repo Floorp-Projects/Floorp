1f330db380d5dbbef1df6df563553ee9ad6b8631
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2015-02-20 16:18:05 -0800

    Bug 1113431 - Tests for referrer in click, middle click, and open new link in new {tab, window}. r=gijskruitbosch, a=sledru

---
+++ browser/base/content/test/referrer/browser_referrer_middle_click.js
@@ -0,0 +1,18 @@
+// Tests referrer on middle-click navigation.
+// Middle-clicks on the link, which opens it in a new tab.
+
+function startMiddleClickTestCase(aTestNumber) {
+  info("browser_referrer_middle_click: " +
+       getReferrerTestDescription(aTestNumber));
+  someTabLoaded(gTestWindow).then(function(aNewTab) {
+    gTestWindow.gBrowser.selectedTab = aNewTab;
+    checkReferrerAndStartNextTest(aTestNumber, null, aNewTab,
+                                  startMiddleClickTestCase);
+  });
+
+  clickTheLink(gTestWindow, "testlink", {button: 1});
+}
+
+function test() {
+  startReferrerTest(startMiddleClickTestCase);
+}
