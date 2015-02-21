0677fc490ecf1c66c87d42249fce600fec65873b
Ryan VanderMeulen <ryanvm@gmail.com>
2015-02-20 20:00:19 -0500

    Backed out changesets 4c2b179b71ae and c96050cdedd5 (bug 1133939) for asserts.

--- xpcom/tests/gtest/Helpers.h
+++ xpcom/tests/gtest/Helpers.h
@@ -7,7 +7,6 @@
 #ifndef __Helpers_h
 #define __Helpers_h
 
-#include "nsIAsyncOutputStream.h"
 #include "nsString.h"
 #include <stdint.h>
 
@@ -34,22 +33,6 @@ ConsumeAndValidateStream(nsIInputStream* aStream,
 void
 ConsumeAndValidateStream(nsIInputStream* aStream,
                          const nsACString& aExpectedData);
-
-class OutputStreamCallback MOZ_FINAL : public nsIOutputStreamCallback
-{
-public:
-  OutputStreamCallback();
-
-  bool Called() const { return mCalled; }
-
-private:
-  ~OutputStreamCallback();
-
-  bool mCalled;
-public:
-  NS_DECL_ISUPPORTS
-  NS_DECL_NSIOUTPUTSTREAMCALLBACK
-};
 
 } // namespace testing
 
