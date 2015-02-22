dcaa8d4b21bbc8a5bdb2475a321b792ceae3312c
Mark Finkle <mfinkle@mozilla.com>
2015-02-22 16:56:12 -0500

    Bug 1135406 - Remove unused browser.history preferences r=mcomella

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -298,11 +298,6 @@ pref("chrome.override_package.passwordmgr", "browser");
 // enable xul error pages
 pref("browser.xul.error_pages.enabled", true);
 
-pref("browser.history.grouping", "day");
-pref("browser.history.showSessions", false);
-pref("browser.sessionhistory.max_entries", 50);
-pref("browser.history_expire_sites", 40000);
-
 // disable color management
 pref("gfx.color_management.mode", 0);
 
