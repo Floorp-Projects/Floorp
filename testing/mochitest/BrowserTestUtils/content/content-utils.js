974ea5edd636774ce309fc317a93c42a44477f99
Steven MacLeod <smacleod@mozilla.com>
2015-02-22 23:11:22 -0500

    Bug 1093566 - Add modules for sharing mochitest-browser test code. r=Gijs, r=paolo, a=test-only
    Currently code used by many mochitest-browser tests is scattered
    throughout the tree in various head.js files. Many similar or identical
    helper methods are repeated throughout these files.
    
    This commit introduces a BrowserTestUtils.jsm module and includes it in
    the mochitest scope; the idea being these frequently re-implemented
    methods can live in a central place.
    
    A TestUtils.jsm module has also been introduced to contain code useful to
    all types of tests.

---
+++ testing/mochitest/BrowserTestUtils/content/content-utils.js
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+addEventListener("load", function(event) {
+  let subframe = event.target != content.document;
+  sendAsyncMessage("browser-test-utils:loadEvent",
+    {subframe: subframe, url: event.target.documentURI});
+}, true);
+
