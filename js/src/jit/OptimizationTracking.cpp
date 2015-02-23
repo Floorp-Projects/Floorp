9db9088e9dd81c78afc553fcf1a05cb2fae2eb4c
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-02-23 10:20:01 +0100

    Backed out changeset b70a4383ee84 (bug 1131429)

--- js/src/jit/OptimizationTracking.cpp
+++ js/src/jit/OptimizationTracking.cpp
@@ -1164,78 +1164,85 @@ FunctionFromTrackedType(const IonTrackedTypeWithAddendum &tracked)
     return ty.group()->maybeInterpretedFunction();
 }
 
-void
-IonTrackedOptimizationsTypeInfo::ForEachOpAdapter::readType(const IonTrackedTypeWithAddendum &tracked)
+// This adapter is needed as the internal API can deal with engine-internal
+// data structures directly, while the public API cannot.
+class ForEachTypeInfoAdapter : public IonTrackedOptimizationsTypeInfo::ForEachOp
 {
-    TypeSet::Type ty = tracked.type;
+    ForEachTrackedOptimizationTypeInfoOp &op_;
 
-    if (ty.isPrimitive() || ty.isUnknown() || ty.isAnyObject()) {
-        op_.readType("primitive", TypeSet::NonObjectTypeString(ty), nullptr, 0);
-        return;
-    }
+  public:
+    explicit ForEachTypeInfoAdapter(ForEachTrackedOptimizationTypeInfoOp &op)
+      : op_(op)
+    { }
 
-    char buf[512];
-    const uint32_t bufsize = mozilla::ArrayLength(buf);
+    void readType(const IonTrackedTypeWithAddendum &tracked) MOZ_OVERRIDE {
+        TypeSet::Type ty = tracked.type;
 
-    if (JSFunction *fun = FunctionFromTrackedType(tracked)) {
-        if (fun->isNative()) {
-            //
-            // Print out the absolute address of the function pointer.
-            //
-            // Note that this address is not usable without knowing the
-            // starting address at which our shared library is loaded. Shared
-            // library information is exposed by the profiler. If this address
-            // needs to be symbolicated manually (e.g., when it is gotten via
-            // debug spewing of all optimization information), it needs to be
-            // converted to an offset from the beginning of the shared library
-            // for use with utilities like `addr2line` on Linux and `atos` on
-            // OS X. Converting to an offset may be done via dladdr():
-            //
-            //   void *addr = JS_FUNC_TO_DATA_PTR(void *, fun->native());
-            //   uintptr_t offset;
-            //   Dl_info info;
-            //   if (dladdr(addr, &info) != 0)
-            //       offset = uintptr_t(addr) - uintptr_t(info.dli_fbase);
-            //
-            uintptr_t addr = JS_FUNC_TO_DATA_PTR(uintptr_t, fun->native());
-            JS_snprintf(buf, bufsize, "%llx", addr);
-            op_.readType("native", nullptr, buf, UINT32_MAX);
+        if (ty.isPrimitive() || ty.isUnknown() || ty.isAnyObject()) {
+            op_.readType("primitive", TypeSet::NonObjectTypeString(ty), nullptr, 0);
             return;
         }
 
-        PutEscapedString(buf, bufsize, fun->displayAtom(), 0);
-        const char *filename;
-        unsigned lineno;
-        InterpretedFunctionFilenameAndLineNumber(fun, &filename, &lineno);
-        op_.readType(tracked.constructor ? "constructor" : "function", buf, filename, lineno);
-        return;
+        char buf[512];
+        const uint32_t bufsize = mozilla::ArrayLength(buf);
+
+        if (JSFunction *fun = FunctionFromTrackedType(tracked)) {
+            if (fun->isNative()) {
+                //
+                // Print out the absolute address of the function pointer.
+                //
+                // Note that this address is not usable without knowing the
+                // starting address at which our shared library is loaded. Shared
+                // library information is exposed by the profiler. If this address
+                // needs to be symbolicated manually (e.g., when it is gotten via
+                // debug spewing of all optimization information), it needs to be
+                // converted to an offset from the beginning of the shared library
+                // for use with utilities like `addr2line` on Linux and `atos` on
+                // OS X. Converting to an offset may be done via dladdr():
+                //
+                //   void *addr = JS_FUNC_TO_DATA_PTR(void *, fun->native());
+                //   uintptr_t offset;
+                //   Dl_info info;
+                //   if (dladdr(addr, &info) != 0)
+                //       offset = uintptr_t(addr) - uintptr_t(info.dli_fbase);
+                //
+                uintptr_t addr = JS_FUNC_TO_DATA_PTR(uintptr_t, fun->native());
+                JS_snprintf(buf, bufsize, "%llx", addr);
+                op_.readType("native", nullptr, buf, UINT32_MAX);
+                return;
+            }
+
+            PutEscapedString(buf, bufsize, fun->displayAtom(), 0);
+            const char *filename;
+            unsigned lineno;
+            InterpretedFunctionFilenameAndLineNumber(fun, &filename, &lineno);
+            op_.readType(tracked.constructor ? "constructor" : "function", buf, filename, lineno);
+            return;
+        }
+
+        const char *className = ty.objectKey()->clasp()->name;
+        JS_snprintf(buf, bufsize, "[object %s]", className);
+
+        if (tracked.hasAllocationSite()) {
+            JSScript *script = tracked.script;
+            op_.readType("alloc site", buf,
+                         script->maybeForwardedScriptSource()->filename(),
+                         PCToLineNumber(script, script->offsetToPC(tracked.offset)));
+            return;
+        }
+
+        if (ty.isGroup()) {
+            op_.readType("prototype", buf, nullptr, UINT32_MAX);
+            return;
+        }
+
+        op_.readType("singleton", buf, nullptr, UINT32_MAX);
     }
 
-    const char *className = ty.objectKey()->clasp()->name;
-    JS_snprintf(buf, bufsize, "[object %s]", className);
-
-    if (tracked.hasAllocationSite()) {
-        JSScript *script = tracked.script;
-        op_.readType("alloc site", buf,
-                     script->maybeForwardedScriptSource()->filename(),
-                     PCToLineNumber(script, script->offsetToPC(tracked.offset)));
-        return;
+    void operator()(JS::TrackedTypeSite site, MIRType mirType) MOZ_OVERRIDE {
+        op_(site, StringFromMIRType(mirType));
     }
-
-    if (ty.isGroup()) {
-        op_.readType("prototype", buf, nullptr, UINT32_MAX);
-        return;
-    }
-
-    op_.readType("singleton", buf, nullptr, UINT32_MAX);
-}
-
-void
-IonTrackedOptimizationsTypeInfo::ForEachOpAdapter::operator()(JS::TrackedTypeSite site,
-                                                              MIRType mirType)
-{
-    op_(site, StringFromMIRType(mirType));
-}
+};
 
 JS_PUBLIC_API(void)
 JS::ForEachTrackedOptimizationTypeInfo(JSRuntime *rt, void *addr,
@@ -1244,7 +1251,7 @@ JS::ForEachTrackedOptimizationTypeInfo(JSRuntime *rt, void *addr,
     JitcodeGlobalTable *table = rt->jitRuntime()->getJitcodeGlobalTable();
     JitcodeGlobalEntry entry;
     table->lookupInfallible(addr, &entry, rt);
-    IonTrackedOptimizationsTypeInfo::ForEachOpAdapter adapter(op);
+    ForEachTypeInfoAdapter adapter(op);
     Maybe<uint8_t> index = entry.trackedOptimizationIndexAtAddr(addr);
     entry.trackedOptimizationTypeInfo(index.value()).forEach(adapter, entry.allTrackedTypes());
 }
