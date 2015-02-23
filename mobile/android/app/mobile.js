4f1dc016f9e784166dd07a254a19fa06999b1a1b
Phil Ringnalda <philringnalda@gmail.com>
2015-02-22 22:00:56 -0800

    Back out 66cf24f275b7 (bug 1124011) for frequent ASan crashes in test_memoryReporters.xul. CLOSED TREE

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -858,6 +858,3 @@ pref("reader.color_scheme.values", "[\"light\",\"dark\",\"auto\"]");
 
 // Whether to use a vertical or horizontal toolbar.
 pref("reader.toolbar.vertical", false);
-
-// Whether or not to display buttons related to reading list in reader view.
-pref("browser.readinglist.enabled", true);
