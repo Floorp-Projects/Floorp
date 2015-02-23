bae03e0aa20e57b8cb7a0234975414fccb175b6b
Michael Comella <michael.l.comella@gmail.com>
2015-02-23 13:11:21 -0800

    Bug 1134192 - Add ActivityUtils.isFullScreen. r=mfinkle
    A more complete solution would rework our fullscreen support to ensure the
    flags are consistently used (e.g. reader mode just uses low_profile even though
    ActivityUtils.setFullScreen does both low profile and fullscreen).

--- mobile/android/base/util/ActivityUtils.java
+++ mobile/android/base/util/ActivityUtils.java
@@ -36,4 +36,16 @@ public class ActivityUtils {
                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         }
     }
+
+    public static boolean isFullScreen(final Activity activity) {
+        final Window window = activity.getWindow();
+
+        if (Versions.feature11Plus) {
+            final int vis = window.getDecorView().getSystemUiVisibility();
+            return (vis & View.SYSTEM_UI_FLAG_FULLSCREEN) != 0;
+        } else {
+            final int flags = window.getAttributes().flags;
+            return ((flags & WindowManager.LayoutParams.FLAG_FULLSCREEN) != 0);
+        }
+    }
 }
