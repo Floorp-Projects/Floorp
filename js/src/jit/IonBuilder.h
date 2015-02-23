558b7457387dee93c859db0c3272f7494933c7bc
Benjamin Bouvier <benj@benj.me>
2015-02-23 19:10:07 +0100

    Bug 1134638: 8. Inline SIMD conversions in Ion; r=nbp

--- js/src/jit/IonBuilder.h
+++ js/src/jit/IonBuilder.h
@@ -809,11 +809,19 @@ class IonBuilder
     // SIMD intrinsics and natives.
     InliningStatus inlineConstructSimdObject(CallInfo &callInfo, SimdTypeDescr *target);
 
+    //  helpers
+    bool checkInlineSimd(CallInfo &callInfo, JSNative native, SimdTypeDescr::Type type,
+                         unsigned numArgs, InlineTypedObject **templateObj);
+    IonBuilder::InliningStatus boxSimd(CallInfo &callInfo, MInstruction *ins,
+                                       InlineTypedObject *templateObj);
+
     template <typename T>
     InliningStatus inlineBinarySimd(CallInfo &callInfo, JSNative native,
                                     typename T::Operation op, SimdTypeDescr::Type type);
     InliningStatus inlineUnarySimd(CallInfo &callInfo, JSNative native,
                                    MSimdUnaryArith::Operation op, SimdTypeDescr::Type type);
+    InliningStatus inlineSimdConvert(CallInfo &callInfo, JSNative native, bool isCast,
+                                     SimdTypeDescr::Type from, SimdTypeDescr::Type to);
 
     // Utility intrinsics.
     InliningStatus inlineIsCallable(CallInfo &callInfo);
