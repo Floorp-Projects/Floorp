0dca6dc2239226db9bc0b341444d7ed20ca1e015
Blake Wu <bwu@mozilla.com>
2015-02-24 11:19:54 +0800

    Bug 1133955 - Record the last seek time to decide the direction to seek. r=sotaro

--- dom/media/omx/OmxDecoder.h
+++ dom/media/omx/OmxDecoder.h
@@ -60,7 +60,7 @@ class OmxDecoder : public OMXCodecProxy::EventListener {
   int32_t mAudioChannels;
   int32_t mAudioSampleRate;
   int64_t mDurationUs;
-  int64_t mVideoLastFrameTime;
+  int64_t mLastSeekTime;
 
   VideoFrame mVideoFrame;
   AudioFrame mAudioFrame;
