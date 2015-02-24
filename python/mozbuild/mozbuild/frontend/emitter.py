cbb722617cc63172a1bd7dc090655f2d06b1f64b
Nathan Froyd <froydnj@mozilla.com>
2015-02-24 16:36:39 -0500

    Bug 1136383 - enable specifying method names for GENERATED_FILES scripts; r=gps
    The inputs to scripts for GENERATED_FILES are restricted to filenames
    only.  We have several examples in the tree, however, where a script
    takes non-filename arguments.  For converting those cases to use
    GENERATED_FILES, we first need to provide some way of "injecting"
    non-filename arguments into the script.
    
    This commit adds a method for doing that, by extending the .script flag
    on GENERATED_FILES to include an optional method name:
    
      f = GENERATED_FILES['foo']
      f.script = 'script.py:make_foo'
    
    will invoke the make_foo function found in script.py instead of the
    function named main.

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -524,7 +524,12 @@ class TreeMetadataEmitter(LoggingMixin):
                 flags = generated_files[f]
                 output = f
                 if flags.script:
-                    script = mozpath.join(context.srcdir, flags.script)
+                    method = "main"
+                    if ':' in flags.script:
+                        script, method = flags.script.split(':')
+                    else:
+                        script = flags.script
+                    script = mozpath.join(context.srcdir, script)
                     inputs = [mozpath.join(context.srcdir, i) for i in flags.inputs]
 
                     if not os.path.exists(script):
@@ -542,8 +547,9 @@ class TreeMetadataEmitter(LoggingMixin):
                                 % (f, i), context)
                 else:
                     script = None
+                    method = None
                     inputs = []
-                yield GeneratedFile(context, script, output, inputs)
+                yield GeneratedFile(context, script, method, output, inputs)
 
         test_harness_files = context.get('TEST_HARNESS_FILES')
         if test_harness_files:
