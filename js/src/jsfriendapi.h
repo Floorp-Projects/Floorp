4b52a3332a9e5c05b9d2780bbb77235b3a7aa80b
Boris Zbarsky <bzbarsky@mit.edu>
2015-02-24 16:04:22 -0500

    Bug 1134970 part 1.  Add JS friend API to allocate an object which is guaranteed to have no attached metadata.  r=waldo

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -60,6 +60,13 @@ extern JS_FRIEND_API(JSObject *)
 JS_NewObjectWithUniqueType(JSContext *cx, const JSClass *clasp, JS::HandleObject proto,
                            JS::HandleObject parent);
 
+// Allocate an object in exactly the same way as JS_NewObjectWithGivenProto, but
+// without invoking the metadata callback on it.  This allows creation of
+// internal bookkeeping objects that are guaranteed to not have metadata
+// attached to them.
+extern JS_FRIEND_API(JSObject *)
+JS_NewObjectWithoutMetadata(JSContext *cx, const JSClass *clasp, JS::Handle<JSObject*> proto);
+
 extern JS_FRIEND_API(uint32_t)
 JS_ObjectCountDynamicSlots(JS::HandleObject obj);
 
