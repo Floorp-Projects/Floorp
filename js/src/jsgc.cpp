2758b312fa5b14efdb5eed4cb969dd7b4501fec6
Jon Coppeard <jcoppeard@mozilla.com>
2015-02-24 09:40:02 +0000

    Bug 1133759 - Always shrink GC buffers at the end of a shrinking GC, even if we don't compact r=terrence

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -5520,10 +5520,6 @@ GCRuntime::compactPhase(JS::gcreason::Reason reason)
     }
 #endif
 
-    // Ensure execess chunks are returns to the system and free arenas
-    // decommitted.
-    shrinkBuffers();
-
 #ifdef DEBUG
     CheckHashTablesAfterMovingGC(rt);
     for (GCZonesIter zone(rt); !zone.done(); zone.next()) {
@@ -5568,6 +5564,10 @@ GCRuntime::finishCollection(JS::gcreason::Reason reason)
         MOZ_ASSERT(!zone->isCollecting());
         MOZ_ASSERT(!zone->wasGCStarted());
     }
+
+    // Ensure execess chunks are returned to the system and free arenas
+    // decommitted.
+    shrinkBuffers();
 
     lastGCTime = currentTime;
 
