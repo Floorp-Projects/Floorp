ab6511f23346597e63d62b8c340c7b95010f27ab
David Anderson <danderson@mozilla.com>
2015-02-24 13:39:01 -0800

    Implement GetMaxTextureSize in the basic compositor. (bug 1135883, r=mattwoodrow)

--- gfx/2d/DrawTargetCG.h
+++ gfx/2d/DrawTargetCG.h
@@ -183,6 +183,13 @@ public:
   CGContextRef GetCGContext() {
       return mCg;
   }
+
+  // 32767 is the maximum size supported by cairo. We clamp to that to make it
+  // easier to interoperate.
+  static size_t GetMaxSurfaceSize() {
+    return 32767;
+  }
+
 private:
   void MarkChanged();
 
