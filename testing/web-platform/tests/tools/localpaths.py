65c7ab66967e79794f6efa86e1fb7c28a6b901cd
James Graham <james@hoppipolla.co.uk>
2015-02-25 17:46:32 +0000

    Bug 1140145 - Update web-platform-tests to revision 9a2c04e06cb4e63b13b803722d345d085bf0debf, a=testonly
    --HG--
    rename : testing/web-platform/tests/DOMEvents/tests/submissions/Microsoft/converted/dispatchEvent.UNSPECIFIED_EVENT_TYPE_ERR.html => testing/web-platform/tests/csp/csp-inline-script.html
    rename : testing/web-platform/tests/DOMEvents/tests/submissions/Microsoft/converted/Determined.candidate.EventListeners.html => testing/web-platform/tests/dom/events/Event-dispatch-handlers-changed.html
    rename : testing/web-platform/tests/DOMEvents/tests/submissions/Microsoft/converted/dispatchEvent.NOT_SUPPORTED_ERR.html => testing/web-platform/tests/dom/events/Event-dispatch-redispatch.html
    rename : testing/web-platform/tests/DOMEvents/tests/submissions/Microsoft/converted/Propagation.path.target.moved.html => testing/web-platform/tests/dom/events/Event-dispatch-target-moved.html
    rename : testing/web-platform/tests/DOMEvents/tests/submissions/Microsoft/converted/dispatchEvent.DISPATCH_REQUEST_ERR.html => testing/web-platform/tests/dom/events/Event-dispatch-target-removed.html
    rename : testing/web-platform/tests/DOMEvents/event-phases-order.html => testing/web-platform/tests/html/dom/documents/dom-tree-accessors/Document.currentScript.html
    rename : testing/web-platform/tests/tools/scripts/html5lib_test.xml => testing/web-platform/tests/html/tools/html5lib_test.xml
    rename : testing/web-platform/tests/tools/scripts/html5lib_test_fragment.xml => testing/web-platform/tests/html/tools/html5lib_test_fragment.xml
    rename : testing/web-platform/tests/tools/scripts/update_html5lib_tests.py => testing/web-platform/tests/html/tools/update_html5lib_tests.py
    rename : testing/web-platform/tests/tools/scripts/lint.py => testing/web-platform/tests/tools/lint/lint.py
    rename : testing/web-platform/tests/tools/scripts/lint.whitelist => testing/web-platform/tests/tools/lint/lint.whitelist
    rename : testing/web-platform/tests/tools/scripts/_env.py => testing/web-platform/tests/tools/localpaths.py
    rename : testing/web-platform/tests/tools/scripts/manifest.py => testing/web-platform/tests/tools/manifest/manifest.py
    rename : testing/web-platform/tests/tools/scripts/manifest.py => testing/web-platform/tests/tools/manifest/tree.py
    rename : testing/web-platform/tests/serve.py => testing/web-platform/tests/tools/serve/serve.py

--- testing/web-platform/tests/tools/localpaths.py
+++ testing/web-platform/tests/tools/localpaths.py
@@ -2,8 +2,9 @@ import os
 import sys
 
 here = os.path.abspath(os.path.split(__file__)[0])
-repo_root = os.path.abspath(os.path.join(here, os.pardir, os.pardir))
+repo_root = os.path.abspath(os.path.join(here, os.pardir))
 
+sys.path.insert(0, os.path.join(repo_root, "tools"))
 sys.path.insert(0, os.path.join(repo_root, "tools", "six"))
 sys.path.insert(0, os.path.join(repo_root, "tools", "html5lib"))
 sys.path.insert(0, os.path.join(repo_root, "tools", "wptserve"))
--- testing/web-platform/tests/tools/runner/update_manifest.py
+++ testing/web-platform/tests/tools/runner/update_manifest.py
@@ -1,18 +1,19 @@
+import imp
 import json
 import os
 import sys
 
-here = os.path.split(__file__)[0]
-sys.path.insert(0, os.path.join(here, os.pardir, "scripts"))
-import _env
-root = _env.repo_root
+here = os.path.dirname(__file__)
+localpaths = imp.load_source("localpaths", os.path.abspath(os.path.join(here, os.pardir, "localpaths.py")))
+
+root = localpaths.repo_root
 
 import manifest
 
 def main(request, response):
     path = os.path.join(root, "MANIFEST.json")
-    manifest_file = manifest.load(path)
-    manifest.update(root, "/", manifest_file)
-    manifest.write(manifest_file, path)
+    manifest_file = manifest.manifest.load(path)
+    manifest.update.update(root, "/", manifest_file)
+    manifest.manifest.write(manifest_file, path)
 
     return [("Content-Type", "application/json")], json.dumps({"url": "/MANIFEST.json"})
