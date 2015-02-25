d8d8e5701b864370b49091049d31c7ee1f295680
J. Ryan Stinnett <jryans@gmail.com>
2015-02-25 08:55:06 -0600

    Bug 1136450 - WebIDE packaging check did not function correctly. r=ochameau, a=lsblakk

--- browser/devtools/webide/modules/build.js
+++ browser/devtools/webide/modules/build.js
@@ -37,7 +37,7 @@ const ProjectBuilding = exports.ProjectBuilding = {
 
   // If the app depends on some build step, run it before pushing the app
   build: Task.async(function* ({ project, logger }) {
-    if (!this.hasPrepackage(project)) {
+    if (!(yield this.hasPrepackage(project))) {
       return;
     }
 
@@ -168,4 +168,3 @@ const ProjectBuilding = exports.ProjectBuilding = {
     }
   }),
 };
-
