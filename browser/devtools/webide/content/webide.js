321db221c13d3f86add6d660497f4e647e657bbe
J. Ryan Stinnett <jryans@gmail.com>
2015-02-25 15:14:28 -0600

    Bug 1124326 - Improve packageDir support for Cordova. r=ochameau

--- browser/devtools/webide/content/webide.js
+++ browser/devtools/webide/content/webide.js
@@ -134,7 +134,7 @@ let UI = {
         AppManager.selectedProject.type != "mainProcess" &&
         AppManager.selectedProject.type != "runtimeApp" &&
         AppManager.selectedProject.type != "tab") {
-      AppManager.validateProject(AppManager.selectedProject);
+      AppManager.validateAndUpdateProject(AppManager.selectedProject);
     }
 
     // Hook to display promotional Developer Edition doorhanger. Only displayed once.
@@ -567,7 +567,7 @@ let UI = {
       menuindex: 1
     });
     this.projecteditor.on("onEditorSave", (editor, resource) => {
-      AppManager.validateProject(AppManager.selectedProject);
+      AppManager.validateAndUpdateProject(AppManager.selectedProject);
     });
     return this.projecteditor.loaded;
   },
@@ -666,9 +666,6 @@ let UI = {
         throw e;
       }
     }
-
-    // Validate project
-    yield AppManager.validateProject(project);
 
     // Select project
     AppManager.selectedProject = project;
@@ -1045,9 +1042,6 @@ let Cmds = {
       // Retrieve added project
       let project = AppProjects.get(ret.location);
 
-      // Validate project
-      yield AppManager.validateProject(project);
-
       // Select project
       AppManager.selectedProject = project;
 
@@ -1106,7 +1100,7 @@ let Cmds = {
           // The result of the validation process (storing names, icons, …) is not stored in
           // the IndexedDB database when App Manager v1 is used.
           // We need to run the validation again and update the name and icon of the app.
-          AppManager.validateProject(project).then(() => {
+          AppManager.validateAndUpdateProject(project).then(() => {
             panelItemNode.setAttribute("label", project.name);
             panelItemNode.setAttribute("image", project.icon);
           });
