6a8f1122ee8d6e8f28dcb6b545b64d45106d42b6
Michael Comella <michael.l.comella@gmail.com>
2015-02-24 16:12:32 -0800

    Bug 1056002 - Backout changeset c56275d516ec. r=mfinkle, a=lsblakk
    This change caused a lot of fullscreen issues (see the dependent bugs).

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -133,14 +133,11 @@ import android.view.ViewGroup;
 import android.view.ViewStub;
 import android.view.ViewTreeObserver;
 import android.view.Window;
-import android.view.WindowManager;
 import android.view.animation.Interpolator;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
-
-import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 public class BrowserApp extends GeckoApp
                         implements TabsPanel.TabsLayoutChangeListener,
@@ -244,8 +241,6 @@ public class BrowserApp extends GeckoApp
     private BrowserHealthReporter mBrowserHealthReporter;
 
     private ReadingListHelper mReadingListHelper;
-
-    private SystemBarTintManager mTintManager;
 
     // The tab to be selected on editing mode exit.
     private Integer mTargetTabForEditingMode;
@@ -687,8 +682,6 @@ public class BrowserApp extends GeckoApp
 
         final Context appContext = getApplicationContext();
 
-        setupSystemUITinting();
-
         mBrowserChrome = (ViewGroup) findViewById(R.id.browser_chrome);
         mActionBarFlipper = (ViewFlipper) findViewById(R.id.browser_actionbar);
         mActionBar = (ActionModeCompatView) findViewById(R.id.actionbar);
@@ -836,31 +829,6 @@ public class BrowserApp extends GeckoApp
             ViewStub stub = (ViewStub) findViewById(R.id.zoomed_view_stub);
             mZoomedView = (ZoomedView) stub.inflate();
         }
-    }
-
-    private void setupSystemUITinting() {
-        if (!Versions.feature19Plus) {
-            return;
-        }
-
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-        mTintManager = new SystemBarTintManager(this);
-        mTintManager.setTintColor(getResources().getColor(R.color.background_tabs));
-        updateSystemUITinting(mRootLayout.getSystemUiVisibility());
-
-        mRootLayout.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                updateSystemUITinting(visibility);
-            }
-        });
-    }
-
-    private void updateSystemUITinting(int visibility) {
-        final boolean shouldTint = (visibility & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0 &&
-                                   (visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0;
-        mTintManager.setStatusBarTintEnabled(shouldTint);
     }
 
     /**
@@ -2648,11 +2616,6 @@ public class BrowserApp extends GeckoApp
             if (view.getScrollX() != 0 || view.getScrollY() != 0) {
                 view.getHitRect(mTempRect);
                 mTempRect.offset(-view.getScrollX(), -view.getScrollY());
-
-                if (mTintManager != null) {
-                    SystemBarTintManager.SystemBarConfig config = mTintManager.getConfig();
-                    mTempRect.offset(0, -config.getPixelInsetTop(false));
-                }
 
                 int[] viewCoords = new int[2];
                 view.getLocationOnScreen(viewCoords);
