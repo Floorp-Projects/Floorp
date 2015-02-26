5077377c6c65202ffa248df3e9830c9587ea9a3f
Boris Zbarsky <bzbarsky@mit.edu>
2015-02-26 15:58:59 -0500

    Bug 1136925 part 1.  Stop passing a parent to JS_CloneObject.  r=waldo

--- js/src/jsfriendapi.cpp
+++ js/src/jsfriendapi.cpp
@@ -622,10 +622,10 @@ JS_SetAccumulateTelemetryCallback(JSRuntime *rt, JSAccumulateTelemetryDataCallba
 }
 
 JS_FRIEND_API(JSObject *)
-JS_CloneObject(JSContext *cx, HandleObject obj, HandleObject protoArg, HandleObject parent)
+JS_CloneObject(JSContext *cx, HandleObject obj, HandleObject protoArg)
 {
     Rooted<TaggedProto> proto(cx, TaggedProto(protoArg.get()));
-    return CloneObject(cx, obj, proto, parent);
+    return CloneObject(cx, obj, proto);
 }
 
 #ifdef DEBUG
