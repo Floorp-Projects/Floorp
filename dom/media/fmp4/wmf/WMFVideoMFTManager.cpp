4075e84ab6fa6462406239e4a19ebeb908c0208a
Karl Tomlinson <karlt+@karlt.net>
2015-02-26 14:24:33 +1300

    bug 1138238 ensure mUseHwAccel is set appropriately when WMFVideoMFTManager::Init() is called after Shutdown() r=cpearce

--- dom/media/fmp4/wmf/WMFVideoMFTManager.cpp
+++ dom/media/fmp4/wmf/WMFVideoMFTManager.cpp
@@ -77,7 +77,7 @@ WMFVideoMFTManager::WMFVideoMFTManager(
   , mImageContainer(aImageContainer)
   , mDXVAEnabled(aDXVAEnabled)
   , mLayersBackend(aLayersBackend)
-  , mUseHwAccel(false)
+  // mUseHwAccel is initialized in Init()
 {
   NS_ASSERTION(!NS_IsMainThread(), "Should not be on main thread.");
   MOZ_ASSERT(mImageContainer);
@@ -170,6 +170,7 @@ WMFVideoMFTManager::InitializeDXVA()
 TemporaryRef<MFTDecoder>
 WMFVideoMFTManager::Init()
 {
+  mUseHwAccel = false; // default value; changed if D3D setup succeeds.
   bool useDxva = InitializeDXVA();
 
   RefPtr<MFTDecoder> decoder(new MFTDecoder());
@@ -504,7 +505,7 @@ WMFVideoMFTManager::Shutdown()
 bool
 WMFVideoMFTManager::IsHardwareAccelerated() const
 {
-  return mUseHwAccel;
+  return mDecoder && mUseHwAccel;
 }
 
 } // namespace mozilla
