8120243aed553b4e47a7c6585087db2791d5a869
Ehsan Akhgari <ehsan@mozilla.com>
2015-02-26 15:26:12 -0500

    Bug 1125963 - Part 1: Fix serialization of the pre-wrap elements that Thunderbird relies on. r=bzbarsky, a=lmandel
    This ensures that the plaintext serializer doesn't use the preformatted
    text code path if we have encountered a pre-wrap element that
    Thunderbird uses (which means setting white-space: pre-wrap and width:
    NNch on the body element.)
    
    It also ensures that we use 0 as the wrap column number passed down to
    the plaintext serializer, instead of -1, which this code seems to be
    unable to handle properly.

--- dom/html/HTMLInputElement.cpp
+++ dom/html/HTMLInputElement.cpp
@@ -7219,7 +7219,7 @@ HTMLInputElement::GetCols()
 NS_IMETHODIMP_(int32_t)
 HTMLInputElement::GetWrapCols()
 {
-  return -1; // only textarea's can have wrap cols
+  return 0; // only textarea's can have wrap cols
 }
 
 NS_IMETHODIMP_(int32_t)
