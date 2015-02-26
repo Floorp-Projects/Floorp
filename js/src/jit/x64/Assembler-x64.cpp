c87901191dcff9c4a815f101390e53a991e53d16
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2015-02-26 12:18:29 +0100

    Bug 1112164 part 16 - Clean-up: Remove FloatRegister::GetSizeInBytes. r=bbouvier

--- js/src/jit/x64/Assembler-x64.cpp
+++ js/src/jit/x64/Assembler-x64.cpp
@@ -310,12 +310,6 @@ FloatRegister::ReduceSetForPush(const FloatRegisterSet &s)
     return s;
 }
 uint32_t
-FloatRegister::GetSizeInBytes(const FloatRegisterSet &s)
-{
-    uint32_t ret = s.size() * sizeof(double);
-    return ret;
-}
-uint32_t
 FloatRegister::GetPushSizeInBytes(const FloatRegisterSet &s)
 {
     SetType all = s.bits();
