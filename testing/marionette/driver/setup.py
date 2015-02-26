a1eed2574eb0ebc4fb25878b3ea73146d14edb22
Henrik Skupin <mail@hskupin.info>
2015-02-26 13:22:10 +0100

    Bug 1124667 - Release marionette-driver 0.2 and marionette-client 0.9. r=automatedtester, a=test-only
    --HG--
    extra : rebase_source : 0d071d37d3b74f4b034c5001b874c11331095452
    extra : source : dcc86f78d75eca856d39a364ba48f3e6fd2d8596

--- testing/marionette/driver/setup.py
+++ testing/marionette/driver/setup.py
@@ -1,8 +1,6 @@
-import os
 from setuptools import setup, find_packages
-import sys
 
-version = '0.1.0'
+version = '0.2'
 
 # dependencies
 with open('requirements.txt') as f:
@@ -12,16 +10,14 @@ setup(name='marionette_driver',
       version=version,
       description="Marionette Driver",
       long_description='See http://marionette-driver.readthedocs.org/',
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='mozilla',
       author='Auto-tools',
       author_email='tools-marionette@lists.mozilla.org',
       url='https://wiki.mozilla.org/Auto-tools/Projects/Marionette',
       license='MPL',
       packages=find_packages(),
-      #package_data={'marionette_driver': ['touch/*.js']},
       include_package_data=True,
       zip_safe=False,
       install_requires=deps,
       )
-
