1dcfc83a6382f740ff295db8964ae459dc148d34
Jonathan Watt <jwatt@jwatt.org>
2015-02-27 10:23:49 +0000

    Bug 1141498, part 1 - Stop using Node.remove() in the DOM animation test's testcommon.js. r=dholbert

--- dom/animation/test/testcommon.js
+++ dom/animation/test/testcommon.js
@@ -12,7 +12,9 @@ function addDiv(t) {
   var div = document.createElement('div');
   document.body.appendChild(div);
   if (t && typeof t.add_cleanup === 'function') {
-    t.add_cleanup(function() { div.remove(); });
+    t.add_cleanup(function() {
+      div.parentNode.removeChild(div);
+    });
   }
   return div;
 }
