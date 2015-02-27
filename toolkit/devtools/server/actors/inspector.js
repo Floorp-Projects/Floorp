7ec72829c72590ddbcf784817c1b870b802dfdd2
Mahdi Dibaiee <mdibaiee@aol.com>
2015-02-27 02:44:00 -0500

    Bug 984442 - Remove XUL attributes when their attributeName is deleted. r=pbrosset

--- toolkit/devtools/server/actors/inspector.js
+++ toolkit/devtools/server/actors/inspector.js
@@ -2451,7 +2451,9 @@ var WalkerActor = protocol.ActorClass({
       if (mutation.type === "attributes") {
         mutation.attributeName = change.attributeName;
         mutation.attributeNamespace = change.attributeNamespace || undefined;
-        mutation.newValue = targetNode.getAttribute(mutation.attributeName);
+        mutation.newValue = targetNode.hasAttribute(mutation.attributeName) ?
+                            targetNode.getAttribute(mutation.attributeName)
+                            : null;
       } else if (mutation.type === "characterData") {
         if (targetNode.nodeValue.length > gValueSummaryLength) {
           mutation.newValue = targetNode.nodeValue.substring(0, gValueSummaryLength);
