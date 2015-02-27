8e171666e9bf3b72781e17b7f064213167e7ada4
David Major <dmajor@mozilla.com>
2015-02-26 20:37:00 -0500

    Bug 1137050 - Don't SetThreadContext if the context didn't change. r=luke, a=lizzard

--- js/src/asmjs/AsmJSSignalHandlers.cpp
+++ js/src/asmjs/AsmJSSignalHandlers.cpp
@@ -958,7 +958,7 @@ RedirectIonBackedgesToInterruptCheck(JSRuntime *rt)
     }
 }
 
-static void
+static bool
 RedirectJitCodeToInterruptCheck(JSRuntime *rt, CONTEXT *context)
 {
     RedirectIonBackedgesToInterruptCheck(rt);
@@ -976,8 +976,11 @@ RedirectJitCodeToInterruptCheck(JSRuntime *rt, CONTEXT *context)
         if (module.containsFunctionPC(pc)) {
             activation->setResumePC(pc);
             *ppc = module.interruptExit();
+            return true;
         }
     }
+
+    return false;
 }
 
 #if !defined(XP_WIN)
@@ -1115,8 +1118,8 @@ js::InterruptRunningJitCode(JSRuntime *rt)
         CONTEXT context;
         context.ContextFlags = CONTEXT_CONTROL;
         if (GetThreadContext(thread, &context)) {
-            RedirectJitCodeToInterruptCheck(rt, &context);
-            SetThreadContext(thread, &context);
+            if (RedirectJitCodeToInterruptCheck(rt, &context))
+                SetThreadContext(thread, &context);
         }
         ResumeThread(thread);
     }
