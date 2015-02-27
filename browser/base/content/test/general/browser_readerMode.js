bce12602b2a53b4a137b7630df259a9630aebc75
Mark Capella <markcapella@twcny.rr.com>
2015-02-27 14:31:31 -0500

    Bug 1133489 - Hook up "Open ReadingList" button in desktop ReaderMode, r=unfocused a=readinglist

--- browser/base/content/test/general/browser_readerMode.js
+++ browser/base/content/test/general/browser_readerMode.js
@@ -2,9 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Test that the reader mode button appears and works properly on reader-able content.
-
-const PREF = "reader.parse-on-load.enabled";
+/**
+ * Test that the reader mode button appears and works properly on
+ * reader-able content, and that ReadingList button can open and close
+ * its Sidebar UI.
+ */
+const READER_PREF = "reader.parse-on-load.enabled";
+const READING_LIST_PREF = "browser.readinglist.enabled";
 
 const TEST_PATH = "http://example.com/browser/browser/base/content/test/general/";
 
@@ -12,14 +16,16 @@ let readerButton = document.getElementById("reader-mode-button");
 
 add_task(function* () {
   registerCleanupFunction(function() {
-    Services.prefs.clearUserPref(PREF);
+    Services.prefs.clearUserPref(READER_PREF);
+    Services.prefs.clearUserPref(READING_LIST_PREF);
     while (gBrowser.tabs.length > 1) {
       gBrowser.removeCurrentTab();
     }
   });
 
-  // Enable the reader mode button.
-  Services.prefs.setBoolPref(PREF, true);
+  // Enable the reader mode and ReadingList buttons.
+  Services.prefs.setBoolPref(READER_PREF, true);
+  Services.prefs.setBoolPref(READING_LIST_PREF, true);
 
   let tab = gBrowser.selectedTab = gBrowser.addTab();
   is_element_hidden(readerButton, "Reader mode button is not present on a new tab");
@@ -39,6 +45,25 @@ add_task(function* () {
 
   is(gURLBar.value, readerUrl, "gURLBar value is about:reader URL");
   is(gURLBar.textValue, url.substring("http://".length), "gURLBar is displaying original article URL");
+
+  // Readinglist button should be present, and status should be "closed".
+  let listButton;
+  yield promiseWaitForCondition(() =>
+    listButton = gBrowser.contentDocument.getElementById("list-button"));
+  is_element_visible(listButton, "List button is present on a reader-able page");
+  yield promiseWaitForCondition(() => !listButton.classList.contains("on"));
+  ok(!listButton.classList.contains("on"),
+    "List button should not indicate SideBar-ReadingList open.");
+  ok(!ReadingListUI.isSidebarOpen,
+    "The ReadingListUI should not indicate SideBar-ReadingList open.");
+
+  // After we click ReadingList button, status should be "open".
+  listButton.click();
+  yield promiseWaitForCondition(() => listButton.classList.contains("on"));
+  ok(listButton.classList.contains("on"),
+    "List button should now indicate SideBar-ReadingList open.");
+  ok(ReadingListUI.isSidebarOpen,
+    "The ReadingListUI should now indicate SideBar-ReadingList open.");
 
   readerButton.click();
   yield promiseTabLoadEvent(tab);
