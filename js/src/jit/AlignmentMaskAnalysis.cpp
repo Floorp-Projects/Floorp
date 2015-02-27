be98e615066b8f12767ed3b63e58639ff2228b60
Dan Gohman <sunfish@mozilla.com>
2015-02-27 11:02:59 -0800

    Bug 1137573 - OdinMonkey: Generalize alignment analysis to handle adds with multiple uses r=luke a=ryanvm

--- js/src/jit/AlignmentMaskAnalysis.cpp
+++ js/src/jit/AlignmentMaskAnalysis.cpp
@@ -21,9 +21,9 @@ IsAlignmentMask(uint32_t m)
 static void
 AnalyzeAsmHeapAddress(MDefinition *ptr, MIRGraph &graph)
 {
-    // Fold (a+i)&m to (a&m)+i, since the users of the BitAnd include heap
-    // accesses. This will expose the redundancy for GVN when expressions
-    // like this:
+    // Fold (a+i)&m to (a&m)+i, provided that this doesn't change the result,
+    // since the users of the BitAnd include heap accesses. This will expose
+    // the redundancy for GVN when expressions like this:
     //   a&m
     //   (a+1)&m,
     //   (a+2)&m,
@@ -33,42 +33,42 @@ AnalyzeAsmHeapAddress(MDefinition *ptr, MIRGraph &graph)
     //   (a&m)+2
     // and it will allow the constants to be folded by the
     // EffectiveAddressAnalysis pass.
+    //
+    // Putting the add on the outside might seem like it exposes other users of
+    // the expression to the possibility of i32 overflow, if we aren't in asm.js
+    // and they aren't naturally truncating. However, since we use MAdd::NewAsmJS
+    // with MIRType_Int32, we make sure that the value is truncated, just as it
+    // would be by the MBitAnd.
 
     if (!ptr->isBitAnd())
         return;
 
     MDefinition *lhs = ptr->toBitAnd()->getOperand(0);
     MDefinition *rhs = ptr->toBitAnd()->getOperand(1);
-    int lhsIndex = 0;
-    if (lhs->isConstantValue()) {
+    if (lhs->isConstantValue())
         mozilla::Swap(lhs, rhs);
-        lhsIndex = 1;
-    }
-    if (!lhs->isAdd() || !lhs->hasOneUse() || !rhs->isConstantValue())
+    if (!lhs->isAdd() || !rhs->isConstantValue())
         return;
 
     MDefinition *op0 = lhs->toAdd()->getOperand(0);
     MDefinition *op1 = lhs->toAdd()->getOperand(1);
-    int op0Index = 0;
-    if (op0->isConstantValue()) {
+    if (op0->isConstantValue())
         mozilla::Swap(op0, op1);
-        op0Index = 1;
-    }
     if (!op1->isConstantValue())
         return;
 
     uint32_t i = op1->constantValue().toInt32();
     uint32_t m = rhs->constantValue().toInt32();
-    if (!IsAlignmentMask(m) || ((i & m) != i))
+    if (!IsAlignmentMask(m) || (i & m) != i)
         return;
 
-    ptr->replaceAllUsesWith(lhs);
-    ptr->toBitAnd()->replaceOperand(lhsIndex, op0);
-    lhs->toAdd()->replaceOperand(op0Index, ptr);
-
-    MInstructionIterator iter = ptr->block()->begin(ptr->toBitAnd());
-    ++iter;
-    lhs->block()->moveBefore(*iter, lhs->toAdd());
+    // The pattern was matched! Produce the replacement expression.
+    MInstruction *and_ = MBitAnd::NewAsmJS(graph.alloc(), op0, rhs);
+    ptr->block()->insertBefore(ptr->toBitAnd(), and_);
+    MInstruction *add = MAdd::NewAsmJS(graph.alloc(), and_, op1, MIRType_Int32);
+    ptr->block()->insertBefore(ptr->toBitAnd(), add);
+    ptr->replaceAllUsesWith(add);
+    ptr->block()->discard(ptr->toBitAnd());
 }
 
 bool
