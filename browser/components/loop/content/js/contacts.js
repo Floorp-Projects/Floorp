c0cbdc6c4489d3d414ac98a2ebd267551a4624af
Mike de Boer <mdeboer@mozilla.com>
2015-02-27 11:08:28 +0100

    Bug 1069962: show a promo area in the contacts list for Gravatars. Show Gravatar icons upon granting permission. r=Standard8, a=lizzard

--- browser/components/loop/content/js/contacts.js
+++ browser/components/loop/content/js/contacts.js
@@ -81,6 +81,59 @@ loop.contacts = (function(_, mozL10n) {
     contact[field][0].value = value;
   };
 
+  const GravatarPromo = React.createClass({displayName: "GravatarPromo",
+    propTypes: {
+      handleUse: React.PropTypes.func.isRequired
+    },
+
+    getInitialState: function() {
+      return {
+        showMe: navigator.mozLoop.getLoopPref("contacts.gravatars.promo") &&
+          !navigator.mozLoop.getLoopPref("contacts.gravatars.show")
+      };
+    },
+
+    handleCloseButtonClick: function() {
+      navigator.mozLoop.setLoopPref("contacts.gravatars.promo", false);
+      this.setState({ showMe: false });
+    },
+
+    handleUseButtonClick: function() {
+      navigator.mozLoop.setLoopPref("contacts.gravatars.promo", false);
+      navigator.mozLoop.setLoopPref("contacts.gravatars.show", true);
+      this.setState({ showMe: false });
+      this.props.handleUse();
+    },
+
+    render: function() {
+      if (!this.state.showMe) {
+        return null;
+      }
+
+      let privacyUrl = navigator.mozLoop.getLoopPref("legal.privacy_url");
+      let message = mozL10n.get("gravatars_promo_message", {
+        "learn_more": React.renderToStaticMarkup(
+          React.createElement("a", {href: privacyUrl, target: "_blank"}, 
+            mozL10n.get("gravatars_promo_message_learnmore")
+          )
+        )
+      });
+      return (
+        React.createElement("div", {className: "contacts-gravatar-promo"}, 
+          React.createElement(Button, {additionalClass: "button-close", onClick: this.handleCloseButtonClick}), 
+          React.createElement("p", {dangerouslySetInnerHTML: {__html: message}}), 
+          React.createElement(ButtonGroup, null, 
+            React.createElement(Button, {caption: mozL10n.get("gravatars_promo_button_nothanks"), 
+                    onClick: this.handleCloseButtonClick}), 
+            React.createElement(Button, {caption: mozL10n.get("gravatars_promo_button_use"), 
+                    additionalClass: "button-accept", 
+                    onClick: this.handleUseButtonClick})
+          )
+        )
+      );
+    }
+  });
+
   const ContactDropdown = React.createClass({displayName: "ContactDropdown",
     propTypes: {
       handleAction: React.PropTypes.func.isRequired,
@@ -232,7 +285,9 @@ loop.contacts = (function(_, mozL10n) {
 
       return (
         React.createElement("li", {className: contactCSSClass, onMouseLeave: this.hideDropdownMenu}, 
-          React.createElement("div", {className: "avatar"}), 
+          React.createElement("div", {className: "avatar"}, 
+            React.createElement("img", {src: navigator.mozLoop.getUserAvatar(email.value)})
+          ), 
           React.createElement("div", {className: "details"}, 
             React.createElement("div", {className: "username"}, React.createElement("strong", null, names.firstName), " ", names.lastName, 
               React.createElement("i", {className: cx({"icon icon-google": this.props.contact.category[0] == "google"})}), 
@@ -462,6 +517,12 @@ loop.contacts = (function(_, mozL10n) {
       }
     },
 
+    handleUseGravatar: function() {
+      // We got permission to use Gravatar icons now, so we need to redraw the
+      // list entirely to show them.
+      this.refresh();
+    },
+
     sortContacts: function(contact1, contact2) {
       let comp = contact1.name[0].localeCompare(contact2.name[0]);
       if (comp !== 0) {
@@ -522,7 +583,8 @@ loop.contacts = (function(_, mozL10n) {
             React.createElement("input", {className: "contact-filter", 
                    placeholder: mozL10n.get("contacts_search_placesholder"), 
                    valueLink: this.linkState("filter")})
-            : null
+            : null, 
+            React.createElement(GravatarPromo, {handleUse: this.handleUseGravatar})
           ), 
           React.createElement("ul", {className: "contact-list"}, 
             shownContacts.available ?
