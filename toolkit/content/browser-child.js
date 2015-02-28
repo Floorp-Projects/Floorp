a18fa38ed3f81f8d6b373ec1af865771edb6c326
Andrew McCreight <amccreight@mozilla.com>
2015-02-28 11:16:58 -0800

    Backed out changeset a761ac6f8bcc for e10s bc3 opt thumbnail timeouts.

--- toolkit/content/browser-child.js
+++ toolkit/content/browser-child.js
@@ -195,15 +195,14 @@ let WebNavigation =  {
     addMessageListener("WebNavigation:Reload", this);
     addMessageListener("WebNavigation:Stop", this);
 
+    this._webNavigation = docShell.QueryInterface(Ci.nsIWebNavigation);
+    this._sessionHistory = this._webNavigation.sessionHistory;
+
     // Send a CPOW for the sessionHistory object. We need to make sure
     // it stays alive as long as the content script since CPOWs are
     // weakly held.
-    let history = this.webNavigation.sessionHistory;
+    let history = this._sessionHistory;
     sendAsyncMessage("WebNavigation:setHistory", {}, {history: history});
-  },
-
-  get webNavigation() {
-    return docShell.QueryInterface(Ci.nsIWebNavigation);
   },
 
   receiveMessage: function(message) {
@@ -230,18 +229,18 @@ let WebNavigation =  {
   },
 
   goBack: function() {
-    if (this.webNavigation.canGoBack) {
-      this.webNavigation.goBack();
+    if (this._webNavigation.canGoBack) {
+      this._webNavigation.goBack();
     }
   },
 
   goForward: function() {
-    if (this.webNavigation.canGoForward)
-      this.webNavigation.goForward();
+    if (this._webNavigation.canGoForward)
+      this._webNavigation.goForward();
   },
 
   gotoIndex: function(index) {
-    this.webNavigation.gotoIndex(index);
+    this._webNavigation.gotoIndex(index);
   },
 
   loadURI: function(uri, flags, referrer) {
@@ -251,15 +250,15 @@ let WebNavigation =  {
 #endif
     if (referrer)
       referrer = Services.io.newURI(referrer, null, null);
-    this.webNavigation.loadURI(uri, flags, referrer, null, null);
+    this._webNavigation.loadURI(uri, flags, referrer, null, null);
   },
 
   reload: function(flags) {
-    this.webNavigation.reload(flags);
+    this._webNavigation.reload(flags);
   },
 
   stop: function(flags) {
-    this.webNavigation.stop(flags);
+    this._webNavigation.stop(flags);
   }
 };
 
