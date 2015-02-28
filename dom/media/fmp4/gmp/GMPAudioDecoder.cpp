ee265c072b492662fc95fbff588f51af91635193
Chris Pearce <cpearce@mozilla.com>
2015-02-28 16:15:29 +1300

    Bug 1137957 - Fix non-unified build failure in GMPVideoDecoder. r=kinetik a=lmandel

--- dom/media/fmp4/gmp/GMPAudioDecoder.cpp
+++ dom/media/fmp4/gmp/GMPAudioDecoder.cpp
@@ -9,7 +9,7 @@
 namespace mozilla {
 
 #if defined(DEBUG)
-static bool IsOnGMPThread()
+bool IsOnGMPThread()
 {
   nsCOMPtr<mozIGeckoMediaPluginService> mps = do_GetService("@mozilla.org/gecko-media-plugin-service;1");
   MOZ_ASSERT(mps);
