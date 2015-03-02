685c655ce3abcb535e83b5eecd600cea1a89ae6c
Tooru Fujisawa <arai_a@mac.com>
2015-03-02 13:32:08 +0000

    Bug 1136490 - Fix unreliable test code to not fail with OOM r=terrence

--- js/src/jit-test/tests/gc/bug-1109913.js
+++ js/src/jit-test/tests/gc/bug-1109913.js
@@ -1,7 +1,7 @@
-try {
-    gcparam("maxBytes", gcparam("gcBytes"));
-    eval("\
-        gczeal(2, 1);\
-        newGlobal();\
-    ")
-} catch (e) {}
+// |jit-test| error: out of memory
+
+gcparam("maxBytes", gcparam("gcBytes"));
+eval(`
+    gczeal(2, 1);
+    newGlobal();
+`);
