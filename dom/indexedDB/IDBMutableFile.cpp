62bf58e2d9f292f081c888fd69559a1b4d1556ee
Jan Varga <jan.varga@gmail.com>
2015-03-02 22:14:22 +0100

    Bug 1067568 - Fix intermittent "ASSERTION: We don't know anyting about this file handle?!: 'Error', file dom/filehandle/FileService.cpp, line 234". r=bent, a=lsblakk

--- dom/indexedDB/IDBMutableFile.cpp
+++ dom/indexedDB/IDBMutableFile.cpp
@@ -319,10 +319,12 @@ IDBMutableFile::Open(FileMode aMode, ErrorResult& aError)
     return nullptr;
   }
 
-  if (mInvalidated) {
+  if (mDatabase->IsClosed()) {
     aError.Throw(NS_ERROR_DOM_FILEHANDLE_NOT_ALLOWED_ERR);
     return nullptr;
   }
+
+  MOZ_ASSERT(GetOwner());
 
   nsRefPtr<IDBFileHandle> fileHandle =
     IDBFileHandle::Create(aMode, FileHandleBase::NORMAL, this);
@@ -364,10 +366,17 @@ IDBMutableFile::GetFile(ErrorResult& aError)
 {
   MOZ_ASSERT(NS_IsMainThread());
 
-  // Do nothing if the window is closed
-  if (!GetOwner()) {
+  if (QuotaManager::IsShuttingDown() || FileService::IsShuttingDown()) {
+    aError.Throw(NS_ERROR_DOM_FILEHANDLE_UNKNOWN_ERR);
     return nullptr;
   }
+
+  if (mDatabase->IsClosed()) {
+    aError.Throw(NS_ERROR_DOM_FILEHANDLE_NOT_ALLOWED_ERR);
+    return nullptr;
+  }
+
+  MOZ_ASSERT(GetOwner());
 
   nsRefPtr<IDBFileHandle> fileHandle =
     IDBFileHandle::Create(FileMode::Readonly, FileHandleBase::PARALLEL, this);
