6ac5e568eef6ee09007f67acd5943d7d9bdc3fd6
Ben Kelly <ben@wanderview.com>
2015-03-02 14:24:00 +0100

    Bug 940273 - Part 7 - Initial tests for Service Worker Cache. r=ehsan

---
+++ dom/cache/test/mochitest/test_cache_add.js
@@ -0,0 +1,46 @@
+var singleUrl = './test_cache_add.js';
+var urlList = [
+  './helloworld.txt',
+  './foobar.txt',
+  './test_cache.js'
+];
+var cache;
+caches.open('adder').then(function(openCache) {
+  cache = openCache;
+  return cache.add('ftp://example.com/invalid');
+}).catch(function (err) {
+  is(err.name, 'NetworkError', 'add() should throw NetworkError for invalid scheme');
+  return cache.addAll(['http://example.com/valid', 'ftp://example.com/invalid']);
+}).catch(function (err) {
+  is(err.name, 'NetworkError', 'addAll() should throw NetworkError for invalid scheme');
+  var promiseList = urlList.map(function(url) {
+    return cache.match(url);
+  });
+  promiseList.push(cache.match(singleUrl));
+  return Promise.all(promiseList);
+}).then(function(resultList) {
+  is(urlList.length + 1, resultList.length, 'Expected number of results');
+  resultList.every(function(result) {
+    is(undefined, result, 'URLs should not already be in the cache');
+  });
+  return cache.add(singleUrl);
+}).then(function(result) {
+  is(undefined, result, 'Successful add() should resolve undefined');
+  return cache.addAll(urlList);
+}).then(function(result) {
+  is(undefined, result, 'Successful addAll() should resolve undefined');
+  var promiseList = urlList.map(function(url) {
+    return cache.match(url);
+  });
+  promiseList.push(cache.match(singleUrl));
+  return Promise.all(promiseList);
+}).then(function(resultList) {
+  is(urlList.length + 1, resultList.length, 'Expected number of results');
+  resultList.every(function(result) {
+    ok(!!result, 'Responses should now be in cache for each URL.');
+  });
+  workerTestDone();
+}).catch(function(err) {
+  ok(false, 'Caught error: ' + err);
+  workerTestDone();
+});
---
+++ dom/cache/test/mochitest/worker_wrapper.js
@@ -0,0 +1,101 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+//
+// Worker-side wrapper script for the worker_driver.js helper code.  See
+// the comments at the top of worker_driver.js for more information.
+
+function ok(a, msg) {
+  dump("OK: " + !!a + "  =>  " + a + ": " + msg + "\n");
+  postMessage({type: 'status', status: !!a, msg: a + ": " + msg });
+}
+
+function is(a, b, msg) {
+  dump("IS: " + (a===b) + "  =>  " + a + " | " + b + ": " + msg + "\n");
+  postMessage({type: 'status', status: a === b, msg: a + " === " + b + ": " + msg });
+}
+
+function workerTestArrayEquals(a, b) {
+  if (!Array.isArray(a) || !Array.isArray(b) || a.length != b.length) {
+    return false;
+  }
+  for (var i = 0, n = a.length; i < n; ++i) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function workerTestDone() {
+  postMessage({ type: 'finish' });
+}
+
+function workerTestGetPrefs(prefs, cb) {
+  addEventListener('message', function workerTestGetPrefsCB(e) {
+    if (e.data.type != 'returnPrefs' ||
+        !workerTestArrayEquals(prefs, e.data.prefs)) {
+      return;
+    }
+    removeEventListener('message', workerTestGetPrefsCB);
+    cb(e.data.result);
+  });
+  postMessage({
+    type: 'getPrefs',
+    prefs: prefs
+  });
+}
+
+function workerTestGetPermissions(permissions, cb) {
+  addEventListener('message', function workerTestGetPermissionsCB(e) {
+    if (e.data.type != 'returnPermissions' ||
+        !workerTestArrayEquals(permissions, e.data.permissions)) {
+      return;
+    }
+    removeEventListener('message', workerTestGetPermissionsCB);
+    cb(e.data.result);
+  });
+  postMessage({
+    type: 'getPermissions',
+    permissions: permissions
+  });
+}
+
+function workerTestGetVersion(cb) {
+  addEventListener('message', function workerTestGetVersionCB(e) {
+    if (e.data.type !== 'returnVersion') {
+      return;
+    }
+    removeEventListener('message', workerTestGetVersionCB);
+    cb(e.data.result);
+  });
+  postMessage({
+    type: 'getVersion'
+  });
+}
+
+function workerTestGetUserAgent(cb) {
+  addEventListener('message', function workerTestGetUserAgentCB(e) {
+    if (e.data.type !== 'returnUserAgent') {
+      return;
+    }
+    removeEventListener('message', workerTestGetUserAgentCB);
+    cb(e.data.result);
+  });
+  postMessage({
+    type: 'getUserAgent'
+  });
+}
+
+addEventListener('message', function workerWrapperOnMessage(e) {
+  removeEventListener('message', workerWrapperOnMessage);
+  var data = e.data;
+  try {
+    importScripts(data.script);
+  } catch(e) {
+    postMessage({
+      type: 'status',
+      status: false,
+      msg: 'worker failed to import ' + data.script + "; error: " + e.message
+    });
+  }
+});
