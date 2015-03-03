7d8a5e17d3521e7984e89028aec1099a76be6bb4
Dave Townsend <dtownsend@oxymoronical.com>
2015-03-03 14:07:08 -0800

    Bug 1139099: Dispatch DOMDocElementInserted to match the document-element-inserted observer notification. r=mrbkap

---
+++ dom/html/test/browser_DOMDocElementInserted.js
@@ -0,0 +1,24 @@
+// Tests that the DOMDocElementInserted event is visible on the frame
+add_task(function*() {
+  let tab = gBrowser.addTab();
+  let uri = "data:text/html;charset=utf-8,<html/>"
+
+  let eventPromise = ContentTask.spawn(tab.linkedBrowser, null, function() {
+    Cu.import("resource://gre/modules/PromiseUtils.jsm");
+    let deferred = PromiseUtils.defer();
+
+    let listener = (event) => {
+      removeEventListener("DOMDocElementInserted", listener, true);
+      deferred.resolve(event.target.documentURIObject.spec);
+    };
+    addEventListener("DOMDocElementInserted", listener, true);
+
+    return deferred.promise;
+  });
+
+  tab.linkedBrowser.loadURI(uri);
+  let loadedURI = yield eventPromise;
+  is(loadedURI, uri, "Should have seen the event for the right URI");
+
+  gBrowser.removeTab(tab);
+});
