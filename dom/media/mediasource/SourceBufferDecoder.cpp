0dd93a6d106e0115624126f777c7fdabde0bc36b
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-03-03 11:30:06 +1100

    Bug 1131433 - More non-unified build fixes. a=bustage

--- dom/media/mediasource/SourceBufferDecoder.cpp
+++ dom/media/mediasource/SourceBufferDecoder.cpp
@@ -18,9 +18,7 @@ extern PRLogModuleInfo* GetMediaSourceLog();
 #define __func__ __FUNCTION__
 #endif
 
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
-#define MSE_DEBUG(arg, ...) PR_LOG(GetMediaSourceLog(), PR_LOG_DEBUG, (TOSTRING(name) "SourceBufferDecoder(%p:%s)::%s: " arg, this, mResource->GetContentType().get(), __func__, ##__VA_ARGS__))
+#define MSE_DEBUG(arg, ...) PR_LOG(GetMediaSourceLog(), PR_LOG_DEBUG, ("SourceBufferDecoder(%p:%s)::%s: " arg, this, mResource->GetContentType().get(), __func__, ##__VA_ARGS__))
 #else
 #define MSE_DEBUG(...)
 #endif
