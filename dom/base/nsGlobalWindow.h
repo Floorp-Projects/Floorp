6ce3c7b8bdb932e9ce7ff3c7f24e644dadf79afb
Ehsan Akhgari <ehsan@mozilla.com>
2015-03-04 01:16:21 -0500

    Bug 1050456 - Part 2: Add a nsGlobalWindow::GetIsPrerendered helper

--- dom/base/nsGlobalWindow.h
+++ dom/base/nsGlobalWindow.h
@@ -1272,6 +1272,8 @@ public:
                            const nsAString &aPopupWindowFeatures);
   void FireOfflineStatusEventIfChanged();
 
+  bool GetIsPrerendered();
+
   // Inner windows only.
   nsresult ScheduleNextIdleObserverCallback();
   uint32_t GetFuzzTimeMS();
