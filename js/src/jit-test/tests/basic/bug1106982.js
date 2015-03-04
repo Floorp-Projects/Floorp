6302c2cc0688372edbd1e9eea321c2183d1c954d
Jason Orendorff <jorendorff@mozilla.com>
2015-03-03 18:13:03 -0600

    Bug 1137616 - Restore ReferenceError when a proxy is on the global object's prototype chain, regressed by rev 615f118f2787. r=jandem.

--- js/src/jit-test/tests/basic/bug1106982.js
+++ js/src/jit-test/tests/basic/bug1106982.js
@@ -1,13 +1,16 @@
 var x = "wrong";
 var t = {x: "x"};
+var hits = 0;
 var p = new Proxy(t, {
     has(t, id) {
         var found = id in t;
-        delete t[id];
+        if (++hits == 2)
+            delete t[id];
         return found;
     },
     get(t, id) { return t[id]; }
 });
 with (p)
     x += " x";
+assertEq(hits, 2);
 assertEq(t.x, "undefined x");
---
+++ js/src/jit-test/tests/basic/bug1137616.js
@@ -0,0 +1,7 @@
+// Accessing a name that isn't a global property is a ReferenceError,
+// even if a proxy is on the global's prototype chain.
+
+load(libdir + "asserts.js");
+var g = newGlobal();
+g.__proto__ = {};
+assertThrowsInstanceOf(() => g.eval("s += ''"), g.ReferenceError);
