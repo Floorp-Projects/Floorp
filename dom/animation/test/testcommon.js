bb3b4a7bf41da563ab044d147011e023db90cb22
Jonathan Watt <jwatt@jwatt.org>
2015-03-04 02:49:43 +0000

    Bug 1141710, part 7 - Create helpers to get the startTime corresponding to various points through the active duration. r=dholbert

--- dom/animation/test/testcommon.js
+++ dom/animation/test/testcommon.js
@@ -46,3 +46,19 @@ function waitForFrame() {
 function waitForAllPlayers(players) {
   return Promise.all(players.map(function(player) { return player.ready; }));
 }
+
+/**
+ * Returns a Promise that is resolved after the next two animation frames have
+ * occured (that is, after two consecutive requestAnimationFrame callbacks
+ * have been called).
+ */
+function waitForTwoAnimationFrames() {
+   return new Promise(function(resolve, reject) {
+     window.requestAnimationFrame(function() {
+       window.requestAnimationFrame(function() {
+         resolve();
+       });
+     });
+   });
+}
+
