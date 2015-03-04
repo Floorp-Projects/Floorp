448c672e09e1cd68e3ec9cda6c854f311c605520
Hannes Verschore <hv1989@gmail.com>
2015-03-04 16:17:27 +0100

    Bug 1139152: IonMonkey: Don't improve types at MTypeOf for unknown types, r=jandem

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -3508,6 +3508,8 @@ IonBuilder::improveTypesAtTypeOfCompare(MCompare *ins, bool trueBranch, MTest *t
     } else if (constant->toString() == TypeName(JSTYPE_FUNCTION, names)) {
         if (typeOf->inputMaybeCallableOrEmulatesUndefined() && trueBranch)
             filter.addType(TypeSet::AnyObjectType(), alloc_->lifoAlloc());
+    } else {
+        return true;
     }
 
     TemporaryTypeSet *type;
