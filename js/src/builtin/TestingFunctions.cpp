3311e1f58d3acbcd1350ee5d52f21ef8dcf8127c
Hannes Verschore <hv1989@gmail.com>
2015-03-05 11:53:10 +0100

    Bug 1138265 - TraceLogger: Throw more errors, r=bbouvier ON CLOSED TREE

--- js/src/builtin/TestingFunctions.cpp
+++ js/src/builtin/TestingFunctions.cpp
@@ -1850,6 +1850,7 @@ TimesAccessed(JSContext *cx, unsigned argc, jsval *vp)
     return true;
 }
 
+#ifdef JS_TRACE_LOGGING
 static bool
 EnableTraceLogger(JSContext *cx, unsigned argc, jsval *vp)
 {
@@ -1871,6 +1872,7 @@ DisableTraceLogger(JSContext *cx, unsigned argc, jsval *vp)
 
     return true;
 }
+#endif
 
 #ifdef DEBUG
 static bool
@@ -2670,6 +2672,7 @@ gc::ZealModeHelpText),
 "helperThreadCount()",
 "  Returns the number of helper threads available for off-main-thread tasks."),
 
+#ifdef JS_TRACE_LOGGING
     JS_FN_HELP("startTraceLogger", EnableTraceLogger, 0, 0,
 "startTraceLogger()",
 "  Start logging the mainThread.\n"
@@ -2679,6 +2682,7 @@ gc::ZealModeHelpText),
     JS_FN_HELP("stopTraceLogger", DisableTraceLogger, 0, 0,
 "stopTraceLogger()",
 "  Stop logging the mainThread."),
+#endif
 
     JS_FN_HELP("reportOutOfMemory", ReportOutOfMemory, 0, 0,
 "reportOutOfMemory()",
