95f1a2dccbd6cd445d390091f467d7db521baa75
David Zbarsky <dzbarsky@gmail.com>
2015-03-05 04:13:05 -0500

    Bug 1139258 - Use ScreenIntSize in PBrowser::Show/UpdateDimensions r=botond

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -240,8 +240,8 @@ public:
     // XXX/cjones: it's not clear what we gain by hiding these
     // message-sending functions under a layer of indirection and
     // eating the return values
-    void Show(const nsIntSize& size, bool aParentIsActive);
-    void UpdateDimensions(const nsIntRect& rect, const nsIntSize& size,
+    void Show(const ScreenIntSize& size, bool aParentIsActive);
+    void UpdateDimensions(const nsIntRect& rect, const ScreenIntSize& size,
                           const nsIntPoint& chromeDisp);
     void UpdateFrame(const layers::FrameMetrics& aFrameMetrics);
     void UIResolutionChanged();
@@ -436,7 +436,7 @@ protected:
     int32_t mEventCaptureDepth;
 
     nsIntRect mRect;
-    nsIntSize mDimensions;
+    ScreenIntSize mDimensions;
     ScreenOrientation mOrientation;
     float mDPI;
     CSSToLayoutDeviceScale mDefaultScale;
