7bd41547135d76d2a40251361acd1586c24de370
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2015-03-05 22:51:31 +0900

    Bug 1138882 - Add a pref to enable unrestricted RC4 fallback. r=keeler

--- netwerk/base/security-prefs.js
+++ netwerk/base/security-prefs.js
@@ -7,6 +7,7 @@ pref("security.tls.version.max", 3);
 pref("security.tls.version.fallback-limit", 3);
 pref("security.tls.insecure_fallback_hosts", "");
 pref("security.tls.insecure_fallback_hosts.use_static_list", true);
+pref("security.tls.unrestricted_rc4_fallback", false);
 
 pref("security.ssl.treat_unsafe_negotiation_as_broken", false);
 pref("security.ssl.require_safe_negotiation",  false);
