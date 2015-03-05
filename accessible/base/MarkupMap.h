5af446d8d722338002cb2e1e32d1a1ff11259a8b
Alexander Surkov <surkov.alexander@gmail.com>
2015-03-04 22:56:57 -0500

    Bug 1139576 - make accessible creation by tag name faster, r=marcoz

---
+++ accessible/base/MarkupMap.h
@@ -0,0 +1,111 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:expandtab:shiftwidth=2:tabstop=2:
+ */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+MARKUPMAP(a,
+          New_HTMLLink)
+
+MARKUPMAP(abbr,
+          New_HyperText)
+
+MARKUPMAP(acronym,
+          New_HyperText)
+
+MARKUPMAP(article,
+          New_HyperText)
+
+MARKUPMAP(aside,
+          New_HyperText)
+
+MARKUPMAP(blockquote,
+          New_HyperText)
+
+MARKUPMAP(dd,
+          New_HTMLDefinition)
+
+MARKUPMAP(dl,
+          New_HTMLList)
+
+MARKUPMAP(dt,
+          New_HTMLListitem)
+
+MARKUPMAP(figcaption,
+          New_HTMLFigcaption)
+
+MARKUPMAP(figure,
+          New_HTMLFigure)
+
+MARKUPMAP(form,
+          New_HyperText)
+
+MARKUPMAP(footer,
+          New_HyperText)
+
+MARKUPMAP(header,
+          New_HyperText)
+
+MARKUPMAP(h1,
+          New_HyperText)
+
+MARKUPMAP(h2,
+          New_HyperText)
+
+MARKUPMAP(h3,
+          New_HyperText)
+
+MARKUPMAP(h4,
+          New_HyperText)
+
+MARKUPMAP(h5,
+          New_HyperText)
+
+MARKUPMAP(h6,
+          New_HyperText)
+
+MARKUPMAP(label,
+          New_HTMLLabel)
+
+MARKUPMAP(legend,
+          New_HTMLLegend)
+
+MARKUPMAP(li,
+          New_HTMLListitem)
+
+MARKUPMAP(nav,
+          New_HyperText)
+
+MARKUPMAP(ol,
+          New_HTMLList)
+
+MARKUPMAP(option,
+          New_HTMLOption)
+
+MARKUPMAP(optgroup,
+          New_HTMLOptgroup)
+
+MARKUPMAP(output,
+          New_HTMLOutput)
+
+MARKUPMAP(progress,
+          New_HTMLProgress)
+
+MARKUPMAP(q,
+          New_HyperText)
+
+MARKUPMAP(section,
+          New_HyperText)
+
+MARKUPMAP(time,
+          New_HyperText)
+
+MARKUPMAP(td,
+          New_HTMLTableHeaderCellIfScope)
+
+MARKUPMAP(th,
+          New_HTMLTableHeaderCell)
+
+MARKUPMAP(ul,
+          New_HTMLList)
