689a5ca21e251ce796d05ca6583d77c98ec9bde1
Steven MacLeod <smacleod@mozilla.com>
2015-03-05 14:44:18 -0500

    Bug 1132566 - Make test e10s compatible: browser_privatebrowsing_localStorage_before_after.js; r=billm

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_localStorage_before_after.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_localStorage_before_after.js
@@ -10,57 +10,27 @@
 // Step 2: Load the same page in a non-private tab, ensuring that the storage instance reports only one item
 //   existing.
 
-function test() {
-  // initialization
-  waitForExplicitFinish();
-  let windowsToClose = [];
+add_task(function test() {
   let testURI = "about:blank";
   let prefix = 'http://mochi.test:8888/browser/browser/components/privatebrowsing/test/browser/';
 
-  function doTest(aIsPrivateMode, aWindow, aCallback) {
-    aWindow.gBrowser.selectedBrowser.addEventListener("load", function onLoad() {
-      aWindow.gBrowser.selectedBrowser.removeEventListener("load", onLoad, true);
+  // Step 1.
+  let privateWin = yield BrowserTestUtils.openNewBrowserWindow({private: true});
+  let privateBrowser = privateWin.gBrowser.addTab(
+    prefix + 'browser_privatebrowsing_localStorage_before_after_page.html').linkedBrowser;
+  yield BrowserTestUtils.browserLoaded(privateBrowser);
 
-      if (aIsPrivateMode) {
-        // do something when aIsPrivateMode is true
-        is(aWindow.gBrowser.contentWindow.document.title, '1', "localStorage should contain 1 item");
-      } else {
-        // do something when aIsPrivateMode is false
-        is(aWindow.gBrowser.contentWindow.document.title, 'null|0', 'localStorage should contain 0 items');
-      }
+  is(privateBrowser.contentTitle, '1', "localStorage should contain 1 item");
 
-      aCallback();
-    }, true);
+  // Step 2.
+  let win = yield BrowserTestUtils.openNewBrowserWindow();
+  let browser = win.gBrowser.addTab(
+    prefix + 'browser_privatebrowsing_localStorage_before_after_page2.html').linkedBrowser;
+  yield BrowserTestUtils.browserLoaded(browser);
 
-    aWindow.gBrowser.selectedBrowser.loadURI(testURI);
-  }
+  is(browser.contentTitle, 'null|0', 'localStorage should contain 0 items');
 
-  function testOnWindow(aOptions, aCallback) {
-    whenNewWindowLoaded(aOptions, function(aWin) {
-      windowsToClose.push(aWin);
-      // execute should only be called when need, like when you are opening
-      // web pages on the test. If calling executeSoon() is not necesary, then
-      // call whenNewWindowLoaded() instead of testOnWindow() on your test.
-      executeSoon(function() aCallback(aWin));
-    });
-  };
-
-   // this function is called after calling finish() on the test.
-  registerCleanupFunction(function() {
-    windowsToClose.forEach(function(aWin) {
-      aWin.close();
-    });
-  });
-
-  // test first when on private mode
-  testOnWindow({private: true}, function(aWin) {
-    testURI = prefix + 'browser_privatebrowsing_localStorage_before_after_page.html';
-    doTest(true, aWin, function() {
-      // then test when not on private mode
-      testOnWindow({}, function(aWin) {
-        testURI = prefix + 'browser_privatebrowsing_localStorage_before_after_page2.html';
-        doTest(false, aWin, finish);
-      });
-    });
-  });
-}
+  // Cleanup
+  yield BrowserTestUtils.closeWindow(privateWin);
+  yield BrowserTestUtils.closeWindow(win);
+});
