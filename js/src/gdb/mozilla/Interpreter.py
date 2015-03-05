5530563265629134c8cb7341fcaac7117be757c0
Jim Blandy <jimb@mozilla.com>
2015-03-04 17:34:49 -0800

    Bug 1140182: JS GDB pretty-printers: Implement pretty-printer for js::AbstractFramePtr. r=sfink

--- js/src/gdb/mozilla/Interpreter.py
+++ js/src/gdb/mozilla/Interpreter.py
@@ -12,6 +12,10 @@ class InterpreterTypeCache(object):
     def __init__(self):
         self.tValue = gdb.lookup_type('JS::Value')
         self.tJSOp = gdb.lookup_type('JSOp')
+        self.tScriptFrameIterData = gdb.lookup_type('js::ScriptFrameIter::Data')
+        self.tInterpreterFrame = gdb.lookup_type('js::InterpreterFrame')
+        self.tBaselineFrame = gdb.lookup_type('js::jit::BaselineFrame')
+        self.tRematerializedFrame = gdb.lookup_type('js::jit::RematerializedFrame')
 
 @pretty_printer('js::InterpreterRegs')
 class InterpreterRegs(object):
@@ -37,3 +41,40 @@ class InterpreterRegs(object):
         pc = 'pc = {} ({})'.format(pc.cast(self.cache.void_ptr_t), opcode)
         return '{{ {}, {}, {} }}'.format(fp_, sp, pc)
 
+@pretty_printer('js::AbstractFramePtr')
+class AbstractFramePtr(object):
+    Tag_ScriptFrameIterData = 0x0
+    Tag_InterpreterFrame = 0x1
+    Tag_BaselineFrame = 0x2
+    Tag_RematerializedFrame = 0x3
+    TagMask = 0x3
+
+    def __init__(self, value, cache):
+        self.value = value
+        self.cache = cache
+        if not cache.mod_Interpreter:
+            cache.mod_Interpreter = InterpreterTypeCache()
+        self.itc = cache.mod_Interpreter
+
+    def to_string(self):
+        ptr = self.value['ptr_']
+        tag = ptr & AbstractFramePtr.TagMask
+        ptr = ptr & ~AbstractFramePtr.TagMask
+        if tag == AbstractFramePtr.Tag_ScriptFrameIterData:
+            label = 'js::ScriptFrameIter::Data'
+            ptr = ptr.cast(self.itc.tScriptFrameIterData.pointer())
+        if tag == AbstractFramePtr.Tag_InterpreterFrame:
+            label = 'js::InterpreterFrame'
+            ptr = ptr.cast(self.itc.tInterpreterFrame.pointer())
+        if tag == AbstractFramePtr.Tag_BaselineFrame:
+            label = 'js::jit::BaselineFrame'
+            ptr = ptr.cast(self.itc.tBaselineFrame.pointer())
+        if tag == AbstractFramePtr.Tag_RematerializedFrame:
+            label = 'js::jit::RematerializedFrame'
+            ptr = ptr.cast(self.itc.tRematerializedFrame.pointer())
+        return 'AbstractFramePtr (({} *) {})'.format(label, ptr)
+
+    # Provide the ptr_ field as a child, so it prints after the pretty string
+    # provided above.
+    def children(self):
+        yield ('ptr_', self.value['ptr_'])
