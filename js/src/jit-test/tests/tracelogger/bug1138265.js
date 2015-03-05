01775237e6f930f0b8308767d1b0d3bab5f7f7a1
Hannes Verschore <hv1989@gmail.com>
2015-03-05 13:30:07 +0100

    Bug 1138265: update testcase to be more correct, r=bbouvier ON CLOSED TREE

--- js/src/jit-test/tests/tracelogger/bug1138265.js
+++ js/src/jit-test/tests/tracelogger/bug1138265.js
@@ -1,13 +1,18 @@
-// |jit-test| error:Error
-(function(b, foreign, p) {
-     "use asm"
-     var ff = foreign.ff
-     function f() {
-        ff() | 0
-     }
-     return f
-})(this, {
-    ff: startTraceLogger
-}, ArrayBuffer)()
+try {
+    (function(b, foreign, p) {
+         "use asm"
+         var ff = foreign.ff
+         function f() {
+            ff() | 0
+         }
+         return f
+    })(this, {
+        ff: startTraceLogger
+    }, ArrayBuffer)()
+} catch(e) {
+    // Will throw exception if odinmonkey is on top of the stack upon calling startTraceLogger.
+    // If there is another frame added in between (ion/baseline/interpreter).
+    // This will just run to completion.
+}
 
-
+assertEq(true, true);
