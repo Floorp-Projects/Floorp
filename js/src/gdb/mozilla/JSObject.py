3358ad3f7d35d42416baa772f78d88cbdf0797cf
Jim Blandy <jimb@mozilla.com>
2015-03-05 15:00:13 -0800

    Bug 1140179: Adapt to new JSObject structure, which indirects through the ObjectGroup. DONTBUILD r=sfink
    --HG--
    extra : rebase_source : 816b330641a1346413d46c5d136556289b85e27f

--- js/src/gdb/mozilla/JSObject.py
+++ js/src/gdb/mozilla/JSObject.py
@@ -13,6 +13,8 @@ class JSObjectTypeCache(object):
         baseshape_flags = gdb.lookup_type('js::BaseShape::Flag')
         self.flag_DELEGATE = prettyprinters.enum_value(baseshape_flags, 'js::BaseShape::DELEGATE')
         self.func_ptr_type = gdb.lookup_type('JSFunction').pointer()
+        self.class_NON_NATIVE = gdb.parse_and_eval('js::Class::NON_NATIVE')
+        self.NativeObject_ptr_t = gdb.lookup_type('js::NativeObject').pointer()
 
 # There should be no need to register this for JSFunction as well, since we
 # search for pretty-printers under the names of base classes, and
@@ -27,24 +29,30 @@ class JSObjectPtrOrRef(prettyprinters.Pointer):
         self.otc = cache.mod_JSObject
 
     def summary(self):
-        shape = deref(self.value['shape_'])
-        baseshape = deref(shape['base_'])
         group = deref(self.value['group_'])
-        class_name = group['clasp_']['name'].string()
-        flags = baseshape['flags']
-        is_delegate = bool(flags & self.otc.flag_DELEGATE)
-        name = None
-        if class_name == 'Function':
-            function = self.value
-            concrete_type = function.type.strip_typedefs()
-            if concrete_type.code == gdb.TYPE_CODE_REF:
-                function = function.address
-            function = function.cast(self.otc.func_ptr_type)
-            atom = deref(function['atom_'])
-            name = str(atom) if atom else '<unnamed>'
-        return '[object %s%s]%s' % (class_name,
-                                    ' ' + name if name else '',
-                                    ' delegate' if is_delegate else '')
+        classp = group['clasp_']
+        class_name = classp['name'].string()
+        non_native = classp['flags'] & self.otc.class_NON_NATIVE
+        if non_native:
+            return '[object {}]'.format(class_name)
+        else:
+            native = self.value.cast(self.otc.NativeObject_ptr_t)
+            shape = deref(native['shape_'])
+            baseshape = deref(shape['base_'])
+            flags = baseshape['flags']
+            is_delegate = bool(flags & self.otc.flag_DELEGATE)
+            name = None
+            if class_name == 'Function':
+                function = self.value
+                concrete_type = function.type.strip_typedefs()
+                if concrete_type.code == gdb.TYPE_CODE_REF:
+                    function = function.address
+                function = function.cast(self.otc.func_ptr_type)
+                atom = deref(function['atom_'])
+                name = str(atom) if atom else '<unnamed>'
+            return '[object {}{}]{}'.format(class_name,
+                                            ' ' + name if name else '',
+                                            ' delegate' if is_delegate else '')
 
 @ref_pretty_printer('JSObject')
 def JSObjectRef(value, cache): return JSObjectPtrOrRef(value, cache)
