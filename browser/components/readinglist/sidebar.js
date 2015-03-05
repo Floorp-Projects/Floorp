29bd33638e787189f7b56d4dc1a6802191f7dec3
Drew Willcoxon <adw>
2015-03-05 14:42:00 +1300

    Bug 1131362 - Update Readinglist.jsm consumers to use new API. r=Unfocused a=readinglist

--- browser/components/readinglist/sidebar.js
+++ browser/components/readinglist/sidebar.js
@@ -7,6 +7,7 @@
 const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 
 Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/Task.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource:///modules/readinglist/ReadingList.jsm");
 
@@ -20,6 +21,12 @@ let RLSidebar = {
    * @type {Element}
    */
   list: null,
+
+  /**
+   * A promise that's resolved when building the initial list completes.
+   * @type {Promise}
+   */
+  listPromise: null,
 
   /**
    * <template> element used for constructing list item elements.
@@ -53,7 +60,7 @@ let RLSidebar = {
     this.list.addEventListener("mousemove", event => this.onListMouseMove(event));
     this.list.addEventListener("keydown", event => this.onListKeyDown(event), true);
 
-    this.ensureListItems();
+    this.listPromise = this.ensureListItems();
     ReadingList.addListener(this);
 
     let initEvent = new CustomEvent("Initialized", {bubbles: true});
@@ -74,7 +81,7 @@ let RLSidebar = {
    * TODO: We may not want to show this new item right now.
    * TODO: We should guard against the list growing here.
    *
-   * @param {Readinglist.Item} item - Item that was added.
+   * @param {ReadinglistItem} item - Item that was added.
    */
   onItemAdded(item) {
     log.trace(`onItemAdded: ${item}`);
@@ -88,7 +95,7 @@ let RLSidebar = {
 
   /**
    * Handle an item being deleted from the ReadingList.
-   * @param {ReadingList.Item} item - Item that was deleted.
+   * @param {ReadingListItem} item - Item that was deleted.
    */
   onItemDeleted(item) {
     log.trace(`onItemDeleted: ${item}`);
@@ -103,7 +110,7 @@ let RLSidebar = {
 
   /**
    * Handle an item in the ReadingList having any of its properties changed.
-   * @param {ReadingList.Item} item - Item that was updated.
+   * @param {ReadingListItem} item - Item that was updated.
    */
   onItemUpdated(item) {
     log.trace(`onItemUpdated: ${item}`);
@@ -118,12 +125,12 @@ let RLSidebar = {
   /**
    * Update the element representing an item, ensuring it's in sync with the
    * underlying data.
-   * @param {ReadingList.Item} item - Item to use as a source.
+   * @param {ReadingListItem} item - Item to use as a source.
    * @param {Element} itemNode - Element to update.
    */
   updateItem(item, itemNode) {
     itemNode.setAttribute("id", "item-" + item.id);
-    itemNode.setAttribute("title", `${item.title}\n${item.url.spec}`);
+    itemNode.setAttribute("title", `${item.title}\n${item.url}`);
 
     itemNode.querySelector(".item-title").textContent = item.title;
     itemNode.querySelector(".item-domain").textContent = item.domain;
@@ -132,18 +139,16 @@ let RLSidebar = {
   /**
    * Ensure that the list is populated with the correct items.
    */
-  ensureListItems() {
-    ReadingList.getItems().then(items => {
-      for (let item of items) {
-        // TODO: Should be batch inserting via DocumentFragment
-        try {
-          this.onItemAdded(item);
-        } catch (e) {
-          log.warn("Error adding item", e);
-        }
+  ensureListItems: Task.async(function* () {
+    yield ReadingList.forEachItem(item => {
+      // TODO: Should be batch inserting via DocumentFragment
+      try {
+        this.onItemAdded(item);
+      } catch (e) {
+        log.warn("Error adding item", e);
       }
     });
-  },
+  }),
 
   /**
    * Get the number of items currently displayed in the list.
@@ -317,7 +322,7 @@ let RLSidebar = {
     }
 
     let item = this.getItemFromNode(itemNode);
-    this.openURL(item.url.spec, event);
+    this.openURL(item.url, event);
   },
 
   /**
--- browser/components/readinglist/test/browser/browser_sidebar_mouse_nav.js
+++ browser/components/readinglist/test/browser/browser_sidebar_mouse_nav.js
@@ -23,62 +23,60 @@ add_task(function*() {
   RLUtils.enabled = true;
 
   let itemData = [{
-    id: "00bd24c7-3629-40b0-acde-37aa81768735",
     url: "http://example.com/article1",
     title: "Article 1",
   }, {
-    id: "28bf7f19-cf94-4ceb-876a-ac1878342e0d",
     url: "http://example.com/article2",
     title: "Article 2",
   }, {
-    id: "7e5064ea-f45d-4fc7-8d8c-c067b7781e78",
     url: "http://example.com/article3",
     title: "Article 3",
   }, {
-    id: "8e72a472-8db8-4904-ba39-9672f029e2d0",
     url: "http://example.com/article4",
     title: "Article 4",
   }, {
-    id: "8d332744-37bc-4a1a-a26b-e9953b9f7d91",
     url: "http://example.com/article5",
     title: "Article 5",
   }];
   info("Adding initial mock data");
   yield RLUtils.addItem(itemData);
 
+  info("Fetching items");
+  let items = yield ReadingList.iterator({ sort: "url" }).items(itemData.length);
+
   info("Opening sidebar");
-  yield ReadingListUI.showSidebar();
+  yield RLSidebarUtils.showSidebar();
   RLSidebarUtils.expectNumItems(5);
   RLSidebarUtils.expectSelectedId(null);
   RLSidebarUtils.expectActiveId(null);
 
   info("Mouse move over item 1");
   yield mouseInteraction("mousemove", "SelectedItemChanged", RLSidebarUtils.list.children[0]);
-  RLSidebarUtils.expectSelectedId(itemData[0].id);
+  RLSidebarUtils.expectSelectedId(items[0].id);
   RLSidebarUtils.expectActiveId(null);
 
   info("Mouse move over item 2");
   yield mouseInteraction("mousemove", "SelectedItemChanged", RLSidebarUtils.list.children[1]);
-  RLSidebarUtils.expectSelectedId(itemData[1].id);
+  RLSidebarUtils.expectSelectedId(items[1].id);
   RLSidebarUtils.expectActiveId(null);
 
   info("Mouse move over item 5");
   yield mouseInteraction("mousemove", "SelectedItemChanged", RLSidebarUtils.list.children[4]);
-  RLSidebarUtils.expectSelectedId(itemData[4].id);
+  RLSidebarUtils.expectSelectedId(items[4].id);
   RLSidebarUtils.expectActiveId(null);
 
   info("Mouse move over item 1 again");
   yield mouseInteraction("mousemove", "SelectedItemChanged", RLSidebarUtils.list.children[0]);
-  RLSidebarUtils.expectSelectedId(itemData[0].id);
+  RLSidebarUtils.expectSelectedId(items[0].id);
   RLSidebarUtils.expectActiveId(null);
 
   info("Mouse click on item 1");
   yield mouseInteraction("click", "ActiveItemChanged", RLSidebarUtils.list.children[0]);
-  RLSidebarUtils.expectSelectedId(itemData[0].id);
-  RLSidebarUtils.expectActiveId(itemData[0].id);
+  RLSidebarUtils.expectSelectedId(items[0].id);
+  RLSidebarUtils.expectActiveId(items[0].id);
 
   info("Mouse click on item 3");
   yield mouseInteraction("click", "ActiveItemChanged", RLSidebarUtils.list.children[2]);
-  RLSidebarUtils.expectSelectedId(itemData[2].id);
-  RLSidebarUtils.expectActiveId(itemData[2].id);
+  RLSidebarUtils.expectSelectedId(items[2].id);
+  RLSidebarUtils.expectActiveId(items[2].id);
 });
