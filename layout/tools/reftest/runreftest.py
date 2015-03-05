748aeb48edcfecbb2204370c6e30a572244c39db
Alessio Placitelli <alessio.placitelli@gmail.com>
2015-03-05 08:47:00 +0100

    Bug 1137252 - Make Telemetry retain pings even if sending is disabled. r=vladan

--- layout/tools/reftest/runreftest.py
+++ layout/tools/reftest/runreftest.py
@@ -206,6 +206,9 @@ class RefTest(object):
     # Ensure that telemetry is disabled, so we don't connect to the telemetry
     # server in the middle of the tests.
     prefs['toolkit.telemetry.enabled'] = False
+    # Don't send Telemetry reports to the production server. This is
+    # needed as Telemetry sends pings also if FHR upload is enabled.
+    prefs['toolkit.telemetry.server'] = 'http://%(server)s/telemetry-dummy/'
     # Likewise for safebrowsing.
     prefs['browser.safebrowsing.enabled'] = False
     prefs['browser.safebrowsing.malware.enabled'] = False
