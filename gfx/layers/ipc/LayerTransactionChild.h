282fe7260a5dba43ce9818a2b033916b6252708c
Kartikaya Gupta <kgupta@mozilla.com>
2015-03-06 06:25:33 -0500

    Bug 1137875 - Have the LayerTransactionChild hold a copy of the layers id. r=nical

--- gfx/layers/ipc/LayerTransactionChild.h
+++ gfx/layers/ipc/LayerTransactionChild.h
@@ -51,11 +51,14 @@ public:
                                PTextureChild* aTexture,
                                const FenceHandle& aFence);
 
+  uint64_t GetId() const { return mId; }
+
 protected:
-  LayerTransactionChild()
+  explicit LayerTransactionChild(const uint64_t& aId)
     : mForwarder(nullptr)
     , mIPCOpen(false)
     , mDestroyed(false)
+    , mId(aId)
   {}
   ~LayerTransactionChild() { }
 
@@ -90,6 +93,7 @@ protected:
   ShadowLayerForwarder* mForwarder;
   bool mIPCOpen;
   bool mDestroyed;
+  uint64_t mId;
 };
 
 } // namespace layers
