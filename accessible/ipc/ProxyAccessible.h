ba9a14448784b4baf0d2f45461a5701b9a6daba3
Olli Pettay <Olli.Pettay@helsinki.fi>
2015-03-06 22:43:25 +0200

    Bug 1139887 - IPC Proxy for caretOffset, r=tbsaunde

--- accessible/ipc/ProxyAccessible.h
+++ accessible/ipc/ProxyAccessible.h
@@ -99,6 +99,9 @@ public:
   void Relations(nsTArray<RelationType>* aTypes,
                  nsTArray<nsTArray<ProxyAccessible*>>* aTargetSets) const;
 
+  int32_t CaretOffset();
+  bool SetCaretOffset(int32_t aOffset);
+
   int32_t CharacterCount();
   int32_t SelectionCount();
 
