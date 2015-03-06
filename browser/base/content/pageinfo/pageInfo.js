f832545205c09c1ad26151d9034e6d2b292cef28
Anush <anush@techstream.org>
2015-03-06 14:43:59 -0800

    Bug 418517 - Add "Select All" button to Page Info "Media" tab  r=jaws

--- browser/base/content/pageinfo/pageInfo.js
+++ browser/base/content/pageinfo/pageInfo.js
@@ -1240,6 +1240,14 @@ function doCopy()
   }
 }
 
+function doSelectAllMedia()
+{
+  var tree = document.getElementById("imagetree");
+
+  if (tree)
+    tree.view.selection.selectAll();
+}
+
 function doSelectAll()
 {
   var elem = document.commandDispatcher.focusedElement;
