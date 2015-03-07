c520b9231a5ca0613d6bcd8010453934f7e48c28
Shu-yu Guo <shu@rfrn.org>
2015-03-06 17:58:26 -0800

    Bug 1140180 - Stop leaking TypeLists when tracking optimizations. (r=djvj)

--- js/src/vm/TypeInference.h
+++ js/src/vm/TypeInference.h
@@ -445,7 +445,7 @@ class TypeSet
 
     /* Get a list of all types in this set. */
     typedef Vector<Type, 1, SystemAllocPolicy> TypeList;
-    bool enumerateTypes(TypeList *list) const;
+    template <class TypeListT> bool enumerateTypes(TypeListT *list) const;
 
     /*
      * Iterate through the objects in this set. getObjectCount overapproximates
