b8f6a0b882ddade9538a188acf0cec437ac35efa
Ehsan Akhgari <ehsan@mozilla.com>
2015-03-06 21:02:25 -0500

    Bug 1140658 - Part 5: Remove test_cache_quick_close.html because it makes no sense after bug 1131353; r=bkelly

--- dom/cache/test/mochitest/test_cache_quick_close.js
+++
@@ -1,6 +0,0 @@
-caches.open('closer').then(function (cache) {
-  cache.add('./test_cache.js').then(function() { }, function() { });
-});
-self.close();
-ok(true, 'Should not crash on close()');
-workerTestDone();
