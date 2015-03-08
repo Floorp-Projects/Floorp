e7d21891f1925fb9096d80d853fc5a39d38ec502
Chris Double <chris.double@double.co.nz>
2015-03-09 12:16:58 +1300

    Bug 1131884 - Video buffering calculation fails for some MP4 videos - r=jya a=lmandel
    Playback position used in calculating buffering time is set
    during metadata reading. This is at end of file for the
    video in the bug. As a result the buffering data is always
    wrong.
    
    Changed to not setting position during metadata - it is set
    during frame playback anyway.
    
    Also changes buffering timeout to 15s from 30s.

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -1568,7 +1568,6 @@ void MediaDecoder::StartProgressUpdates()
   mIgnoreProgressData = false;
   if (mResource) {
     mResource->SetReadMode(MediaCacheStream::MODE_PLAYBACK);
-    mDecoderPosition = mPlaybackPosition = mResource->Tell();
   }
 }
 
@@ -1583,7 +1582,7 @@ void MediaDecoder::SetLoadInBackground(bool aLoadInBackground)
 void MediaDecoder::UpdatePlaybackOffset(int64_t aOffset)
 {
   ReentrantMonitorAutoEnter mon(GetReentrantMonitor());
-  mPlaybackPosition = std::max(aOffset, mPlaybackPosition);
+  mPlaybackPosition = aOffset;
 }
 
 bool MediaDecoder::OnStateMachineThread() const
