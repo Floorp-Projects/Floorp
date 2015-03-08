be5e912ee8706702f1f43095036e849daaeeafd7
Phil Ringnalda <philringnalda@gmail.com>
2015-03-07 18:14:05 -0800

    Backed out 3 changesets (bug 1081819) for frequent mochitest-e10s failures
    Backed out changeset b78fd38002f5 (bug 1081819)
    Backed out changeset ff063b9a1ea2 (bug 1081819)
    Backed out changeset 322d60fc413e (bug 1081819)

--- dom/media/TrackUnionStream.cpp
+++ dom/media/TrackUnionStream.cpp
@@ -50,7 +50,8 @@ PRLogModuleInfo* gTrackUnionStreamLog;
 #endif
 
 TrackUnionStream::TrackUnionStream(DOMMediaStream* aWrapper) :
-  ProcessedMediaStream(aWrapper)
+  ProcessedMediaStream(aWrapper),
+  mFilterCallback(nullptr)
 {
 #ifdef PR_LOGGING
   if (!gTrackUnionStreamLog) {
@@ -113,7 +114,7 @@ TrackUnionStream::TrackUnionStream(DOMMediaStream* aWrapper) :
             break;
           }
         }
-        if (!found) {
+        if (!found && (!mFilterCallback || mFilterCallback(tracks.get()))) {
           bool trackFinished = false;
           trackAdded = true;
           uint32_t mapIndex = AddTrack(mInputs[i], tracks.get(), aFrom);
@@ -150,6 +151,14 @@ TrackUnionStream::TrackUnionStream(DOMMediaStream* aWrapper) :
       // We can make progress if we're not blocked
       mHasCurrentData = true;
     }
+  }
+
+  // Consumers may specify a filtering callback to apply to every input track.
+  // Returns true to allow the track to act as an input; false to reject it entirely.
+
+  void TrackUnionStream::SetTrackIDFilter(TrackIDFilterCallback aCallback)
+  {
+    mFilterCallback = aCallback;
   }
 
   // Forward SetTrackEnabled(output_track_id, enabled) to the Source MediaStream,
@@ -272,8 +281,6 @@ TrackUnionStream::TrackUnionStream(DOMMediaStream* aWrapper) :
         segment->AppendNullData(ticks);
         STREAM_LOG(PR_LOG_DEBUG+1, ("TrackUnionStream %p appending %lld ticks of null data to track %d",
                    this, (long long)ticks, outputTrack->GetID()));
-      } else if (InMutedCycle()) {
-        segment->AppendNullData(ticks);
       } else {
         MOZ_ASSERT(outputTrack->GetEnd() == GraphTimeToStreamTime(interval.mStart),
                    "Samples missing");
