520fb63e2c2a1685c79b95a52b574c5ea1073775
Bobby Holley <bobbyholley@gmail.com>
2015-03-10 09:59:30 -0700

    Bug 1135424 - Switch the MDSM to a task queue. r=mattwoodrow

--- dom/media/MediaPromise.cpp
+++ dom/media/MediaPromise.cpp
@@ -6,7 +6,6 @@
 
 #include "MediaPromise.h"
 
-#include "MediaDecoderStateMachineScheduler.h"
 #include "MediaTaskQueue.h"
 #include "nsThreadUtils.h"
 
@@ -25,12 +24,6 @@ DispatchMediaPromiseRunnable(nsIEventTarget* aEventTarget, nsIRunnable* aRunnabl
   return aEventTarget->Dispatch(aRunnable, NS_DISPATCH_NORMAL);
 }
 
-nsresult
-DispatchMediaPromiseRunnable(MediaDecoderStateMachineScheduler* aScheduler, nsIRunnable* aRunnable)
-{
-  return aScheduler->GetStateMachineThread()->Dispatch(aRunnable, NS_DISPATCH_NORMAL);
-}
-
 void
 AssertOnThread(MediaTaskQueue* aQueue)
 {
@@ -42,12 +35,6 @@ void AssertOnThread(nsIEventTarget* aTarget)
   nsCOMPtr<nsIThread> targetThread = do_QueryInterface(aTarget);
   MOZ_ASSERT(targetThread, "Don't know how to deal with threadpools etc here");
   MOZ_ASSERT(NS_GetCurrentThread() == targetThread);
-}
-
-void
-AssertOnThread(MediaDecoderStateMachineScheduler* aScheduler)
-{
-  MOZ_ASSERT(aScheduler->OnStateMachineThread());
 }
 
 }
