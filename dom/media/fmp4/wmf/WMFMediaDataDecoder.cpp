80d1bd36441a83aefe01b3cd8597b61aa7d49b66
Chris Pearce <cpearce@mozilla.com>
2015-03-11 22:01:19 +1300

    Bug 1141241 - Add nullcheck for mDecoder in WMFMediaDataDecoder::ProcessDrain(). r=mattwoodrow

--- dom/media/fmp4/wmf/WMFMediaDataDecoder.cpp
+++ dom/media/fmp4/wmf/WMFMediaDataDecoder.cpp
@@ -139,12 +139,14 @@ WMFMediaDataDecoder::Flush()
 void
 WMFMediaDataDecoder::ProcessDrain()
 {
-  // Order the decoder to drain...
-  if (FAILED(mDecoder->SendMFTMessage(MFT_MESSAGE_COMMAND_DRAIN, 0))) {
-    NS_WARNING("Failed to send DRAIN command to MFT");
+  if (mDecoder) {
+    // Order the decoder to drain...
+    if (FAILED(mDecoder->SendMFTMessage(MFT_MESSAGE_COMMAND_DRAIN, 0))) {
+      NS_WARNING("Failed to send DRAIN command to MFT");
+    }
+    // Then extract all available output.
+    ProcessOutput();
   }
-  // Then extract all available output.
-  ProcessOutput();
   mCallback->DrainComplete();
 }
 
