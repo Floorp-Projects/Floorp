3893eed7e53448081f6072c31169a27d719249fa
Frédéric Wang <fred.wang@free.fr>
2015-03-11 19:05:57 -0400

    Bug 1109022 - Add basic ATK roles for MathML elements, r=surkov

--- accessible/atk/AccessibleWrap.cpp
+++ accessible/atk/AccessibleWrap.cpp
@@ -707,6 +707,10 @@ getRoleCB(AtkObject *aAtkObj)
     aAtkObj->role = ATK_ROLE_LIST;
   else if (aAtkObj->role == ATK_ROLE_TABLE_ROW && !IsAtkVersionAtLeast(2, 1))
     aAtkObj->role = ATK_ROLE_LIST_ITEM;
+  else if (aAtkObj->role == ATK_ROLE_MATH && !IsAtkVersionAtLeast(2, 12))
+    aAtkObj->role = ATK_ROLE_PANEL;
+  else if (aAtkObj->role == ATK_ROLE_STATIC && !IsAtkVersionAtLeast(2, 16))
+    aAtkObj->role = ATK_ROLE_TEXT;
 
   return aAtkObj->role;
 }
