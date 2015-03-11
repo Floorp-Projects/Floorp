e24214fc3141c56510f378071eebba6da492a567
David Burns <dburns@mozilla.com>
2015-03-11 01:18:10 +0000

    NO BUG: correct import in marionette about_pages tests r=me DONTBUILD

--- testing/marionette/marionette-server.js
+++ testing/marionette/marionette-server.js
@@ -289,8 +289,13 @@ MarionetteServerConnection.prototype = {
     }
     else {
       this.curBrowser.executeWhenReady(() => {
-        this.messageManager.broadcastAsyncMessage(
-          "Marionette:" + name + this.curBrowser.curFrameId, values);
+        if (this.curBrowser.curFrameId) {
+          this.messageManager.broadcastAsyncMessage(
+            "Marionette:" + name + this.curBrowser.curFrameId, values);
+        }
+        else {
+          this.sendError("Unfortunately can not send call to listener as it does not exist", 500, null, commandId);
+        }
       });
     }
     return success;
