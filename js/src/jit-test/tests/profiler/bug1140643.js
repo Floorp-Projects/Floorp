190d5bd6ae828b8ed4d18b96b926a24a782a54c8
Shu-yu Guo <shu@rfrn.org>
2015-03-10 20:12:32 -0700

    Bug 1140643 - JitcodeGlobalMap marking must participate in iterative weak reference marking. (r=terrence)

---
+++ js/src/jit-test/tests/profiler/bug1140643.js
@@ -0,0 +1,14 @@
+// |jit-test| allow-oom
+enableSPSProfiling();
+loadFile('\
+for (var i = 0; i < 2; i++) {\
+    obj = { m: function () {} };\
+    obj.watch("m", function () { float32 = 0 + obj.foo; });\
+    obj.m = 0;\
+}\
+');
+gcparam("maxBytes", gcparam("gcBytes") + (1)*1024);
+newGlobal("same-compartment");
+function loadFile(lfVarx) {
+  evaluate(lfVarx, { noScriptRval : true, compileAndGo : true }); 
+}
