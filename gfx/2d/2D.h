6e3172ede683474dce489a01d8da9ee257d89a17
Ryan VanderMeulen <ryanvm@gmail.com>
2015-03-11 15:38:53 -0400

    Backed out changeset 6d66dccc94f8 (bug 1135883) for bustage.
    --HG--
    extra : rebase_source : f912e3ffcf8d526c2718029faa8980d997110075

--- gfx/2d/2D.h
+++ gfx/2d/2D.h
@@ -1166,8 +1166,6 @@ public:
   // This is a little hacky at the moment, but we want to have this data. Bug 1068613.
   static void SetLogForwarder(LogForwarder* aLogFwd);
 
-  static uint32_t GetMaxSurfaceSize(BackendType aType);
-
   static LogForwarder* GetLogForwarder() { return mLogForwarder; }
 
 private:
