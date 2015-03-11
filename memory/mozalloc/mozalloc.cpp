0beffc59bba1e49fac6756b425a7561bda4b556b
Wes Kocher <wkocher@mozilla.com>
2015-03-10 18:34:28 -0700

    Backed out changeset ff08cf105def (bug 1141731) for osx bustage

--- memory/mozalloc/mozalloc.cpp
+++ memory/mozalloc/mozalloc.cpp
@@ -17,7 +17,7 @@
 // mozalloc.cpp is part of the same library as mozmemory, thus MOZ_MEMORY_IMPL
 // is needed.
 #define MOZ_MEMORY_IMPL
-#include "mozmemory_wrap.h"
+#include "mozmemory.h"
 
 // See mozmemory_wrap.h for more details. This file is part of libmozglue, so
 // it needs to use _impl suffixes. However, with libmozglue growing, this is
