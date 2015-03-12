3d40e710386119bc523cf1a9b81ad608211d96ca
Joel Maher <jmaher@mozilla.com>
2015-03-12 11:30:01 -0400

    Backed out changeset 404caa140516 for landing with the wrong bug number

--- testing/config/mozharness/android_arm_4_3_config.py
+++ testing/config/mozharness/android_arm_4_3_config.py
@@ -27,7 +27,7 @@ config = {
                 "--certificate-path=%(certs_path)s", "--symbols-path=%(symbols_path)s",
                 "--quiet", "--log-raw=%(raw_log_file)s",
                 "--total-chunks=2",
-                "--manifest=tests/dom/canvas/test/_webgl-conformance.ini",
+                "--test-manifest=gl.json",
             ],
         },
         "robocop": {
