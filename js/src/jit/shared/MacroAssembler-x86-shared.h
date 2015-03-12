aaefd9ed17268c21b289b547e8fbe4bf1791b232
Benjamin Bouvier <benj@benj.me>
2015-03-12 14:58:59 +0100

    Bug 1135039: Generalize SimdGeneralShuffle codegen; r=sunfish

--- js/src/jit/shared/MacroAssembler-x86-shared.h
+++ js/src/jit/shared/MacroAssembler-x86-shared.h
@@ -974,6 +974,11 @@ class MacroAssemblerX86Shared : public Assembler
         vpxor(dest, dest, dest);
     }
 
+    template <class T, class Reg> inline void loadScalar(const Operand &src, Reg dest);
+    template <class T, class Reg> inline void storeScalar(Reg src, const Address &dest);
+    template <class T> inline void loadAlignedVector(const Address &src, FloatRegister dest);
+    template <class T> inline void storeAlignedVector(FloatRegister src, const Address &dest);
+
     void loadAlignedInt32x4(const Address &src, FloatRegister dest) {
         vmovdqa(Operand(src), dest);
     }
@@ -1411,6 +1416,42 @@ class MacroAssemblerX86Shared : public Assembler
   protected:
     bool buildOOLFakeExitFrame(void *fakeReturnAddr);
 };
+
+template <> inline void
+MacroAssemblerX86Shared::loadAlignedVector<int32_t>(const Address &src, FloatRegister dest) {
+    loadAlignedInt32x4(src, dest);
+}
+template <> inline void
+MacroAssemblerX86Shared::loadAlignedVector<float>(const Address &src, FloatRegister dest) {
+    loadAlignedFloat32x4(src, dest);
+}
+
+template <> inline void
+MacroAssemblerX86Shared::storeAlignedVector<int32_t>(FloatRegister src, const Address &dest) {
+    storeAlignedInt32x4(src, dest);
+}
+template <> inline void
+MacroAssemblerX86Shared::storeAlignedVector<float>(FloatRegister src, const Address &dest) {
+    storeAlignedFloat32x4(src, dest);
+}
+
+template <> inline void
+MacroAssemblerX86Shared::loadScalar<int32_t>(const Operand &src, Register dest) {
+    load32(src, dest);
+}
+template <> inline void
+MacroAssemblerX86Shared::loadScalar<float>(const Operand &src, FloatRegister dest) {
+    loadFloat32(src, dest);
+}
+
+template <> inline void
+MacroAssemblerX86Shared::storeScalar<int32_t>(Register src, const Address &dest) {
+    store32(src, dest);
+}
+template <> inline void
+MacroAssemblerX86Shared::storeScalar<float>(FloatRegister src, const Address &dest) {
+    storeFloat32(src, dest);
+}
 
 } // namespace jit
 } // namespace js
