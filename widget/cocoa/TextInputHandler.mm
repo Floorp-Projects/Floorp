815fe0bbde0c8faa08cb06abc2e43e8909d73520
Steven Michaud <smichaud@pobox.com>
2015-03-12 15:00:00 -0500

    Bug 1137229 - Fix breakage in IMEInputHandler::OnDestroyWidget(). r=masayuki a=lmandel

--- widget/cocoa/TextInputHandler.mm
+++ widget/cocoa/TextInputHandler.mm
@@ -3312,6 +3312,10 @@ IMEInputHandler::OnDestroyWidget(nsChildView* aDestroyingWidget)
     sFocusedIMEHandler->OnDestroyWidget(aDestroyingWidget);
   }
 
+  if (!TextInputHandlerBase::OnDestroyWidget(aDestroyingWidget)) {
+    return false;
+  }
+
   if (IsIMEComposing()) {
     // If our view is in the composition, we should clean up it.
     CancelIMEComposition();
