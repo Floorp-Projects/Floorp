e3ed5c6dc2e5d6d519dab3abc0e4bc71dc177781
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-03-11 18:05:52 -0700

    Bug 1137151: Marked destructors of ref-counted Bluetooth classes as protected, r=shuang a=relanding

--- dom/bluetooth/bluedroid/hfp-fallback/BluetoothHfpManager.h
+++ dom/bluetooth/bluedroid/hfp-fallback/BluetoothHfpManager.h
@@ -29,12 +29,14 @@ public:
   }
 
   static BluetoothHfpManager* Get();
-  virtual ~BluetoothHfpManager() { }
   static void InitHfpInterface(BluetoothProfileResultHandler* aRes);
   static void DeinitHfpInterface(BluetoothProfileResultHandler* aRes);
 
   bool ConnectSco();
   bool DisconnectSco();
+
+protected:
+  virtual ~BluetoothHfpManager() { }
 
 private:
   BluetoothHfpManager() { }
