3caec6e65a27e8bf789a9e975eeb62f111e557ab
Nicolas Silva <nsilva@mozilla.com>
2015-03-12 19:45:44 +0100

    Bug 1123080 - Use cairo's image backend as canvas fallback on windows. r=Bas a=lmandel

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -1167,7 +1167,12 @@ gfxPlatform::CreateOffscreenCanvasDrawTarget(const IntSize& aSize, SurfaceFormat
     return target.forget();
   }
 
+#ifdef XP_WIN
+  // On Windows, the fallback backend (Cairo) should use its image backend.
+  return Factory::CreateDrawTarget(mFallbackCanvasBackend, aSize, aFormat);
+#else
   return CreateDrawTargetForBackend(mFallbackCanvasBackend, aSize, aFormat);
+#endif
 }
 
 TemporaryRef<DrawTarget>
