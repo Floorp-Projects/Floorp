72e651062e0b98dc8e1b6098acb8cf041d001954
Neil Deakin <neil@mozilla.com>
2015-03-12 09:25:31 -0400

    Bug 1093677, rework form validation tests to work when there is a child process, r=felipe

--- browser/base/content/test/general/browser_bug595507.js
+++ browser/base/content/test/general/browser_bug595507.js
@@ -1,40 +1,36 @@
-var gInvalidFormPopup = document.getElementById('invalid-form-popup');
-ok(gInvalidFormPopup,
-   "The browser should have a popup to show when a form is invalid");
-
 /**
  * Make sure that the form validation error message shows even if the form is in an iframe.
  */
-function test()
-{
-  waitForExplicitFinish();
+add_task(function* () {
+  let uri = "<iframe src=\"data:text/html,<iframe name='t'></iframe><form target='t' action='data:text/html,'><input required id='i'><input id='s' type='submit'></form>\"</iframe>";
 
-  let uri = "data:text/html,<iframe src=\"data:text/html,<iframe name='t'></iframe><form target='t' action='data:text/html,'><input required id='i'><input id='s' type='submit'></form>\"</iframe>";
+  var gInvalidFormPopup = document.getElementById('invalid-form-popup');
+  ok(gInvalidFormPopup,
+     "The browser should have a popup to show when a form is invalid");
+
   let tab = gBrowser.addTab();
-
-  gInvalidFormPopup.addEventListener("popupshown", function() {
-    gInvalidFormPopup.removeEventListener("popupshown", arguments.callee, false);
-
-    let doc = gBrowser.contentDocument.getElementsByTagName('iframe')[0].contentDocument;
-    is(doc.activeElement, doc.getElementById('i'),
-       "First invalid element should be focused");
-
-    ok(gInvalidFormPopup.state == 'showing' || gInvalidFormPopup.state == 'open',
-       "The invalid form popup should be shown");
-
-    // Clean-up and next test.
-    gBrowser.removeTab(gBrowser.selectedTab, {animate: false});
-    executeSoon(finish);
-  }, false);
-
-  tab.linkedBrowser.addEventListener("load", function(aEvent) {
-    tab.linkedBrowser.removeEventListener("load", arguments.callee, true);
-    executeSoon(function() {
-      gBrowser.contentDocument.getElementsByTagName('iframe')[0].contentDocument
-        .getElementById('s').click();
-    });
-  }, true);
-
+  let browser = gBrowser.getBrowserForTab(tab);
   gBrowser.selectedTab = tab;
-  gBrowser.selectedBrowser.loadURI(uri);
-}
+
+  yield promiseTabLoadEvent(tab, "data:text/html," + escape(uri));
+
+  let popupShownPromise = promiseWaitForEvent(gInvalidFormPopup, "popupshown");
+
+  yield ContentTask.spawn(browser, {}, function* () {
+    content.document.getElementsByTagName('iframe')[0]
+           .contentDocument.getElementById('s').click();
+  });
+  yield popupShownPromise;
+
+  let activeElement = yield ContentTask.spawn(browser, {}, function* () {
+    let childdoc = content.document.getElementsByTagName('iframe')[0].contentDocument;
+    return childdoc.activeElement == childdoc.getElementById('i');
+  });
+  is(activeElement, true, "First invalid element should be focused");
+
+  ok(gInvalidFormPopup.state == 'showing' || gInvalidFormPopup.state == 'open',
+     "The invalid form popup should be shown");
+
+  gBrowser.removeCurrentTab();
+});
+
