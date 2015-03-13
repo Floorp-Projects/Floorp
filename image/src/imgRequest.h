79bbe5f49b80a125dbde6c6a82da52e630372665
Seth Fowler <seth@mozilla.com>
2015-03-13 11:43:32 -0700

    Bug 1142849 - Upliftable fix for imgRequest TSan violations. r=tn a=lmandel

--- image/src/imgRequest.h
+++ image/src/imgRequest.h
@@ -19,6 +19,7 @@
 #include "nsStringGlue.h"
 #include "nsError.h"
 #include "nsIAsyncVerifyRedirectCallback.h"
+#include "mozilla/Mutex.h"
 #include "mozilla/net/ReferrerPolicy.h"
 
 class imgCacheValidator;
@@ -130,6 +131,8 @@ public:
     return principal.forget();
   }
 
+  already_AddRefed<Image> GetImage();
+
   // Return the ProgressTracker associated with this imgRequest. It may live
   // in |mProgressTracker| or in |mImage.mProgressTracker|, depending on whether
   // mImage has been instantiated yet.
@@ -155,6 +158,9 @@ private:
   friend class imgCacheExpirationTracker;
   friend class imgRequestNotifyRunnable;
   friend class mozilla::image::ProgressTracker;
+
+  void SetImage(Image* aImage);
+  void SetProgressTracker(ProgressTracker* aProgressTracker);
 
   inline void SetLoadId(void *aLoadId) {
     mLoadId = aLoadId;
@@ -251,6 +257,8 @@ private:
   imgCacheValidator *mValidator;
   nsCOMPtr<nsIAsyncVerifyRedirectCallback> mRedirectCallback;
   nsCOMPtr<nsIChannel> mNewRedirectChannel;
+
+  mozilla::Mutex mMutex;
 
   // The ID of the inner window origin, used for error reporting.
   uint64_t mInnerWindowId;
