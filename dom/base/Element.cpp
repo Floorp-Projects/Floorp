a155531fbc3cbed803d8395f59668b55f86c1e2a
Cameron McCormack <cam@mcc.id.au>
2015-03-14 16:34:40 +1100

    Bug 1123523 - Part 3: Store a flag on AnimationPlayer for whether it is exposed by Element.getAnimationPlayers(). r=birtles

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -3188,7 +3188,7 @@ Element::GetAnimationPlayers(nsTArray<nsRefPtr<AnimationPlayer> >& aPlayers)
          playerIdx < collection->mPlayers.Length();
          playerIdx++) {
       AnimationPlayer* player = collection->mPlayers[playerIdx];
-      if (player->HasCurrentSource() || player->HasInEffectSource()) {
+      if (player->IsRelevant()) {
         aPlayers.AppendElement(player);
       }
     }
