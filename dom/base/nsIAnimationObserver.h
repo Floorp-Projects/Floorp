3df3ffc1944adf82df684ce1a88ebda79a670099
Cameron McCormack <cam@mcc.id.au>
2015-03-14 16:34:40 +1100

    Bug 1123523 - Part 1: Add an nsIAnimationObserver interface to watch adding/changing/removing AnimationPlayer objects. r=smaug

---
+++ dom/base/nsIAnimationObserver.h
@@ -0,0 +1,66 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef nsIAnimationObserver_h___
+#define nsIAnimationObserver_h___
+
+#include "nsIMutationObserver.h"
+
+namespace mozilla {
+namespace dom {
+class AnimationPlayer;
+}
+}
+
+#define NS_IANIMATION_OBSERVER_IID \
+{ 0xed025fc7, 0xdeda, 0x48b9, \
+  { 0x9c, 0x35, 0xf2, 0xb6, 0x1e, 0xeb, 0xd0, 0x8d } }
+
+class nsIAnimationObserver : public nsIMutationObserver
+{
+public:
+  NS_DECLARE_STATIC_IID_ACCESSOR(NS_IANIMATION_OBSERVER_IID)
+
+  virtual void AnimationAdded(mozilla::dom::AnimationPlayer* aPlayer) = 0;
+  virtual void AnimationChanged(mozilla::dom::AnimationPlayer* aPlayer) = 0;
+  virtual void AnimationRemoved(mozilla::dom::AnimationPlayer* aPlayer) = 0;
+};
+
+NS_DEFINE_STATIC_IID_ACCESSOR(nsIAnimationObserver, NS_IANIMATION_OBSERVER_IID)
+
+#define NS_DECL_NSIANIMATIONOBSERVER_ANIMATIONADDED                          \
+    virtual void AnimationAdded(mozilla::dom::AnimationPlayer* aPlayer)      \
+      MOZ_OVERRIDE;
+
+#define NS_DECL_NSIANIMATIONOBSERVER_ANIMATIONCHANGED                        \
+    virtual void AnimationChanged(mozilla::dom::AnimationPlayer* aPlayer)    \
+      MOZ_OVERRIDE;
+
+#define NS_DECL_NSIANIMATIONOBSERVER_ANIMATIONREMOVED                        \
+    virtual void AnimationRemoved(mozilla::dom::AnimationPlayer* aPlayer)    \
+      MOZ_OVERRIDE;
+
+#define NS_IMPL_NSIANIMATIONOBSERVER_STUB(class_)                            \
+void                                                                         \
+class_::AnimationAdded(mozilla::dom::AnimationPlayer* aPlayer)               \
+{                                                                            \
+}                                                                            \
+void                                                                         \
+class_::AnimationChanged(mozilla::dom::AnimationPlayer* aPlayer)             \
+{                                                                            \
+}                                                                            \
+void                                                                         \
+class_::AnimationRemoved(mozilla::dom::AnimationPlayer* aPlayer)             \
+{                                                                            \
+}                                                                            \
+NS_IMPL_NSIMUTATIONOBSERVER_CORE_STUB(class_)                                \
+NS_IMPL_NSIMUTATIONOBSERVER_CONTENT(class_)
+
+#define NS_DECL_NSIANIMATIONOBSERVER                                         \
+    NS_DECL_NSIANIMATIONOBSERVER_ANIMATIONADDED                              \
+    NS_DECL_NSIANIMATIONOBSERVER_ANIMATIONCHANGED                            \
+    NS_DECL_NSIANIMATIONOBSERVER_ANIMATIONREMOVED                            \
+    NS_DECL_NSIMUTATIONOBSERVER
+
+#endif // nsIAnimationObserver_h___
