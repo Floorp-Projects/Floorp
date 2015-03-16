a779113067b8125f894aa581f053f197917017a6
Kartikaya Gupta <kgupta@mozilla.com>
2015-03-16 08:34:31 -0400

    Bug 1136363 - Do a better job of falling back to sane metadata. r=mfinkle

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -6442,7 +6442,10 @@ var ViewportHandler = {
    * metadata is available for that document.
    */
   getMetadataForDocument: function getMetadataForDocument(aDocument) {
-    let metadata = this._metadata.get(aDocument, new ViewportMetadata());
+    let metadata = this._metadata.get(aDocument);
+    if (metadata === undefined) {
+      metadata = new ViewportMetadata();
+    }
     return metadata;
   },
 
