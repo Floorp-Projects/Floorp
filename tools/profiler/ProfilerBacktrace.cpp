7126984fe1e3b5672ac3482556a5b31f20efb6c5
chiajung hung <chung@mozilla.com>
2015-03-16 23:01:00 +0100

    Bug 1142181 - ProfilerBacktrace.cpp should #include its own .h file first, r=aklotz

--- tools/profiler/ProfilerBacktrace.cpp
+++ tools/profiler/ProfilerBacktrace.cpp
@@ -4,10 +4,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "JSStreamWriter.h"
 #include "ProfilerBacktrace.h"
-#include "SyncProfile.h"
 
+#include "JSStreamWriter.h"
+#include "SyncProfile.h"
 
 ProfilerBacktrace::ProfilerBacktrace(SyncProfile* aProfile)
   : mProfile(aProfile)
