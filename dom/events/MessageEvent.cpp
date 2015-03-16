96bdb9cb6a682cab82a03774371c45afc6a34c10
Catalin Badea <catalin.badea392@gmail.com>
2015-03-16 22:23:42 +0200

    Bug 1142015 - Add source for messages dispatched to a Service Worker. r=baku

--- dom/events/MessageEvent.cpp
+++ dom/events/MessageEvent.cpp
@@ -13,6 +13,9 @@
 #include "jsapi.h"
 #include "nsGlobalWindow.h" // So we can assign an nsGlobalWindow* to mWindowSource
 
+#include "ServiceWorker.h"
+#include "ServiceWorkerClient.h"
+
 namespace mozilla {
 namespace dom {
 
@@ -103,12 +106,14 @@ MessageEvent::GetSource(nsIDOMWindow** aSource)
 }
 
 void
-MessageEvent::GetSource(Nullable<OwningWindowProxyOrMessagePort>& aValue) const
+MessageEvent::GetSource(Nullable<OwningWindowProxyOrMessagePortOrClient>& aValue) const
 {
   if (mWindowSource) {
     aValue.SetValue().SetAsWindowProxy() = mWindowSource;
   } else if (mPortSource) {
     aValue.SetValue().SetAsMessagePort() = mPortSource;
+  } else if (mClientSource) {
+    aValue.SetValue().SetAsClient() = mClientSource;
   }
 }
 
@@ -204,6 +209,12 @@ void
 MessageEvent::SetSource(mozilla::dom::MessagePort* aPort)
 {
   mPortSource = aPort;
+}
+
+void
+MessageEvent::SetSource(mozilla::dom::workers::ServiceWorkerClient* aClient)
+{
+  mClientSource = aClient;
 }
 
 } // namespace dom
