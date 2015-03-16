d37cd81a4c99710659f2b1e64f1888600c138455
Ryan VanderMeulen <ryanvm@gmail.com>
2015-03-16 12:43:22 -0400

    Backed out changesets 2a76dd8f86c2, fa9401887405, and dd6e425826fc (bug 1131638) for build bustage.

--- dom/media/fmp4/PlatformDecoderModule.h
+++ dom/media/fmp4/PlatformDecoderModule.h
@@ -135,8 +135,6 @@ public:
   // Indicates if the video decoder requires AVCC format.
   virtual bool DecoderNeedsAVCC(const mp4_demuxer::VideoDecoderConfig& aConfig);
 
-  virtual void DisableHardwareAcceleration() {}
-
 protected:
   PlatformDecoderModule() {}
   virtual ~PlatformDecoderModule() {}
