1df47134f9af94887346c364cab83e3525533c6b
Tooru Fujisawa <arai_a@mac.com>
2015-03-18 05:42:14 +0900

    Bug 1141455 - Do not treat an element with tabindex as an interactive content in label. r=smaug

--- dom/html/HTMLButtonElement.h
+++ dom/html/HTMLButtonElement.h
@@ -37,7 +37,7 @@ public:
   NS_IMPL_FROMCONTENT_HTML_WITH_TAG(HTMLButtonElement, button)
 
   // Element
-  virtual bool IsInteractiveHTMLContent() const MOZ_OVERRIDE
+  virtual bool IsInteractiveHTMLContent(bool aIgnoreTabindex) const MOZ_OVERRIDE
   {
     return true;
   }
