aee91da74265478893d8e8e235921f1949910bc8
Bill McCloskey <wmccloskey@mozilla.com>
2015-03-17 11:24:33 -0700

    Back out bug 1103036 to resolve shutdown hangs a=backout

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -690,10 +690,7 @@ private:
         MOZ_ASSERT(NS_IsMainThread());
         MOZ_ASSERT(mTabChild);
 
-        // Check in case ActorDestroy was called after RecvDestroy message.
-        if (mTabChild->IPCOpen()) {
-            unused << PBrowserChild::Send__delete__(mTabChild);
-        }
+        unused << PBrowserChild::Send__delete__(mTabChild);
 
         mTabChild = nullptr;
         return NS_OK;
@@ -859,7 +856,6 @@ TabChild::TabChild(nsIContentChild* aManager,
   , mUniqueId(aTabId)
   , mDPI(0)
   , mDefaultScale(0)
-  , mIPCOpen(true)
   , mParentIsActive(false)
 {
   // preloaded TabChild should not be added to child map
@@ -1609,8 +1605,6 @@ TabChild::DestroyWindow()
 void
 TabChild::ActorDestroy(ActorDestroyReason why)
 {
-  mIPCOpen = false;
-
   DestroyWindow();
 
   if (mTabChildGlobal) {
