d95fcd35e38a247074857670a8cc30444e8345a8
Mike Conley <mconley@mozilla.com>
2015-03-17 13:28:32 -0400

    Bug 1148012 - Expose run ID through nsIObjectLoadingContent.idl. r=josh,smaug.
    The run ID for a plugin is retrieved and cached in the nsObjectLoadingContent
    on plugin instantiation.
    
    --HG--
    extra : rebase_source : d1e87b7751e6367c482b2420aa43bfadbce5e3e8

--- dom/plugins/base/nsNPAPIPluginInstance.h
+++ dom/plugins/base/nsNPAPIPluginInstance.h
@@ -286,6 +286,8 @@ public:
   // Returns the contents scale factor of the screen the plugin is drawn on.
   double GetContentsScaleFactor();
 
+  nsresult GetRunID(uint32_t *aRunID);
+
   static bool InPluginCallUnsafeForReentry() { return gInUnsafePluginCalls > 0; }
   static void BeginPluginCall(NSPluginCallReentry aReentryState)
   {
