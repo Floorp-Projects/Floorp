430aad8fb5e745c2a8695f880757b176079b22a4
Wes Kocher <wkocher@mozilla.com>
2015-03-16 18:08:17 -0700

    Backed out changeset 00579267379b (bug 1122225) for rc1 failures

--- mobile/android/chrome/content/aboutPasswords.js
+++ mobile/android/chrome/content/aboutPasswords.js
@@ -17,9 +17,6 @@ XPCOMUtils.defineLazyGetter(window, "gChromeWin", function()
     .getInterface(Ci.nsIDOMWindow)
     .QueryInterface(Ci.nsIDOMChromeWindow));
 
-XPCOMUtils.defineLazyModuleGetter(this, "Prompt",
-                                  "resource://gre/modules/Prompt.jsm");
-
 let debug = Cu.import("resource://gre/modules/AndroidLog.jsm", {}).AndroidLog.d.bind(null, "AboutPasswords");
 
 let gStringBundle = Services.strings.createBundle("chrome://browser/locale/aboutPasswords.properties");
@@ -128,33 +125,9 @@ let Passwords = {
 
     loginItem.setAttribute("loginID", login.guid);
     loginItem.className = "login-item list-item";
-
     loginItem.addEventListener("click", () => {
-      let prompt = new Prompt({
-        window: window,
-      });
-      let menuItems = [
-        { label: gStringBundle.GetStringFromName("passwordsMenu.copyPassword") },
-        { label: gStringBundle.GetStringFromName("passwordsMenu.copyUsername") },
-        { label: gStringBundle.GetStringFromName("passwordsMenu.details") } ];
-
-      prompt.setSingleChoiceItems(menuItems);
-      prompt.show((data) => {
-        // Switch on indices of buttons, as they were added when creating login item.
-        switch (data.button) {
-          case 0:
-            copyStringAndToast(login.password, gStringBundle.GetStringFromName("passwordsDetails.passwordCopied"));
-            break;
-          case 1:
-            copyStringAndToast(login.username, gStringBundle.GetStringFromName("passwordsDetails.usernameCopied"));
-            break;
-          case 2:
-            this._showDetails(loginItem);
-            history.pushState({ id: login.guid }, document.title);
-            break;
-        }
-      });
-
+      this._showDetails(loginItem);
+      history.pushState({ id: login.guid }, document.title);
     }, true);
 
     // Create item icon.
