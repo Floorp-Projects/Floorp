cdbc8d491c2691b3f482c91f60d25b0877c234b1
Nikhil Marathe <nsm.nikhil@gmail.com>
2015-03-18 09:47:19 -0700

    Bug 931249 - patch 3 - tests. r=bkelly

---
+++ dom/workers/test/serviceworkers/importscript_worker.js
@@ -0,0 +1,23 @@
+var counter = 0;
+function callByScript() {
+  ++counter;
+}
+
+importScripts(['importscript.sjs']);
+importScripts(['importscript.sjs']);
+
+onmessage = function(e) {
+  self.clients.matchAll().then(function(res) {
+    if (!res.length) {
+      dump("ERROR: no clients are currently controlled.\n");
+    }
+
+    try {
+      importScript(['importscript.sjs']);
+      res[0].postMessage("KO");
+      return;
+    } catch(e) {}
+
+    res[0].postMessage(counter == 2 ? "OK" : "KO");
+  });
+};
