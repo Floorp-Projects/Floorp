5e3bac8094aae0bb0787e57ee30617927f046e54
Andrew McCreight <continuation@gmail.com>
2015-03-18 11:36:03 -0700

    Bug 1142717, part 1 - Remove unused forward declaration of xpc_GetJSPrivate. r=bholley

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -2005,8 +2005,6 @@ private:
     XPCWrappedNativeTearOffChunk* mNextChunk;
 };
 
-void *xpc_GetJSPrivate(JSObject *obj);
-
 /***************************************************************************/
 // XPCWrappedNative the wrapper around one instance of a native xpcom object
 // to be used from JavaScript.
