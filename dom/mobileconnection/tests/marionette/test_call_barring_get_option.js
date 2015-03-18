196f1dbf51e5531e6de2ab7d46f9f779357ee6ca
Ben Hsu <bhsu@mozilla.com>
2015-03-18 04:28:00 -0400

    Bug 1027546 - Part 2: Update the existing testcases. r=aknow

--- dom/mobileconnection/tests/marionette/test_call_barring_get_option.js
+++ dom/mobileconnection/tests/marionette/test_call_barring_get_option.js
@@ -4,59 +4,12 @@
 MARIONETTE_TIMEOUT = 60000;
 MARIONETTE_HEAD_JS = "head.js";
 
-const TEST_DATA = [
-  // Test passing invalid program.
-  {
-    options: {
-      program: 5, /* Invalid program */
-      serviceClass: 0
-    },
-    expectedError: "InvalidParameter"
-  }, {
-    options: {
-      program: null,
-      serviceClass: 0
-    },
-    expectedError: "InvalidParameter"
-  }, {
-    options: {
-      /* Undefined program */
-      serviceClass: 0
-    },
-    expectedError: "InvalidParameter"
-  },
-  // Test passing invalid serviceClass.
-  {
-    options: {
-      program: MozMobileConnection.CALL_BARRING_PROGRAM_ALL_OUTGOING,
-      serviceClass: null
-    },
-    expectedError: "InvalidParameter"
-  }, {
-    options: {
-      program: MozMobileConnection.CALL_BARRING_PROGRAM_ALL_OUTGOING,
-      /* Undefined serviceClass */
-    },
-    expectedError: "InvalidParameter"
-  },
-  // TODO: Bug 1027546 - [B2G][Emulator] Support call barring
-  // Currently emulator doesn't support call barring, so we expect to get a
-  // 'RequestNotSupported' error here.
-  {
-    options: {
-      program: MozMobileConnection.CALL_BARRING_PROGRAM_ALL_OUTGOING,
-      serviceClass: 0
-    },
-    expectedError: "RequestNotSupported"
-  }
-];
-
-function testGetCallBarringOption(aOptions, aExpectedError) {
-  log("Test getting call barring to " + JSON.stringify(aOptions));
+function testGetCallBarringOption(aExpectedError, aOptions) {
+  log("Test getCallBarringOption with " + JSON.stringify(aOptions));
 
   return getCallBarringOption(aOptions)
     .then(function resolve(aResult) {
-      ok(false, "should not success");
+      ok(false, "should be rejected");
     }, function reject(aError) {
       is(aError.name, aExpectedError, "failed to getCallBarringOption");
     });
@@ -64,11 +17,32 @@ function testGetCallBarringOption(aOptions, aExpectedError) {
 
 // Start tests
 startTestCommon(function() {
-  let promise = Promise.resolve();
-  for (let i = 0; i < TEST_DATA.length; i++) {
-    let data = TEST_DATA[i];
-    promise = promise.then(() => testGetCallBarringOption(data.options,
-                                                          data.expectedError));
-  }
-  return promise;
+  return Promise.resolve()
+
+    // Test program
+    .then(() => testGetCallBarringOption("InvalidParameter", {
+      program: 5, /* Invalid program */
+      serviceClass: 0
+    }))
+
+    .then(() => testGetCallBarringOption("InvalidParameter", {
+      program: null, /* Invalid program */
+      serviceClass: 0
+    }))
+
+    .then(() => testGetCallBarringOption("InvalidParameter", {
+      /* Undefined program */
+      serviceClass: 0
+    }))
+
+    // Test serviceClass
+    .then(() => testGetCallBarringOption("InvalidParameter", {
+      program: MozMobileConnection.CALL_BARRING_PROGRAM_ALL_OUTGOING,
+      serviceClass: null /* Invalid serviceClass */
+    }))
+
+    .then(() => testGetCallBarringOption("InvalidParameter", {
+      program: MozMobileConnection.CALL_BARRING_PROGRAM_ALL_OUTGOING,
+      /* Undefined serviceClass */
+    }));
 });
