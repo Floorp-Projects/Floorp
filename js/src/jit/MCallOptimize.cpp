1a38bfb6c57bd01f3b228ebda0b27a1c119fb22b
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2015-03-18 15:12:47 +0100

    Bug 1137624 - Disable Array.join optimization. r=jandem, a=abillings

--- js/src/jit/MCallOptimize.cpp
+++ js/src/jit/MCallOptimize.cpp
@@ -64,8 +64,6 @@ IonBuilder::inlineNativeCall(CallInfo &callInfo, JSFunction *target)
         return inlineArrayPush(callInfo);
     if (native == js::array_concat)
         return inlineArrayConcat(callInfo);
-    if (native == js::array_join)
-        return inlineArrayJoin(callInfo);
     if (native == js::array_splice)
         return inlineArraySplice(callInfo);
 
