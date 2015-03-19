9b0c36f2ded60dc85220fa0b90a5c456b2982a6f
Jeff Gilbert <jgilbert@mozilla.com>
2015-03-18 17:19:22 -0700

    Bug 1143218 - Use mochitest subsuites to specify webgl tests. r=jmaher, r=gbrown, a=test-only
    try: -b do -p android-api-9,android-api-11 -u mochitests -t none

--- testing/config/mozharness/android_arm_config.py
+++ testing/config/mozharness/android_arm_config.py
@@ -28,8 +28,7 @@ config = {
                 "--http-port=%(http_port)s", "--ssl-port=%(ssl_port)s",
                 "--certificate-path=%(certs_path)s", "--symbols-path=%(symbols_path)s",
                 "--quiet", "--log-raw=%(raw_log_file)s",
-                "--total-chunks=4",
-                "--test-manifest=gl.json",
+                "--total-chunks=4", "--subsuite=webgl",
             ],
         },
         "robocop": {
