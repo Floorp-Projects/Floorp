bc02ba3cae239dae9d5c74fb45b904df8b75660a
Xidorn Quan <quanxunzhen@gmail.com>
2015-03-19 19:04:50 +1100

    Bug 1112474 part 2 - Fix ruby-position reftests with bsize adjustment. r=dholbert

--- layout/reftests/css-ruby/utils.js
+++ layout/reftests/css-ruby/utils.js
@@ -12,6 +12,22 @@ function setBSize(elem, bsize) {
   elem.style.lineHeight = bsize;
 }
 
+// Ruby annotations are placed based on block-axis size of inline boxes
+// instead of line box. Block-axis size of an inline box is the max
+// height of the font, while that of line box is line height. Hence we
+// sometimes need to explicitly set the block-axis size of an inline
+// box to a block to simulate the exact behavior, which is what the
+// following two functions do.
+
 function makeBSizeMatchInlineBox(block, inline) {
   setBSize(block, getBSize(inline));
+}
+
+function makeBSizeOfParentMatch(elems) {
+  // The size change is divided into two phases to avoid
+  // triggering reflow for every element.
+  for (var elem of elems)
+    elem.dataset.bsize = getBSize(elem);
+  for (var elem of elems)
+    setBSize(elem.parentNode, elem.dataset.bsize);
 }
