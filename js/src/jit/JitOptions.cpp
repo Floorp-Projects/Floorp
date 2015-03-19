7758ba238f8acd42a6bf5c2d9e2d310c75a7d225
Jan de Mooij <jdemooij@mozilla.com>
2015-03-19 15:10:07 +0100

    Bug 1142669 - Fix some inlining issues and inline scripts with loops. r=h4writer

--- js/src/jit/JitOptions.cpp
+++ js/src/jit/JitOptions.cpp
@@ -160,10 +160,6 @@ JitOptions::JitOptions()
     SET_DEFAULT(osrPcMismatchesBeforeRecompile, 6000);
 
     // The bytecode length limit for small function.
-    //
-    // The default for this was arrived at empirically via benchmarking.
-    // We may want to tune it further after other optimizations have gone
-    // in.
     SET_DEFAULT(smallFunctionMaxBytecodeLength_, 100);
 }
 
