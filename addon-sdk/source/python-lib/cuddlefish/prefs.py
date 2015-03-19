fa8ae31e0d4e7d95ead9fc2ac88c72a779ac57bf
Alessio Placitelli <alessio.placitelli@gmail.com>
2015-03-19 03:45:00 -0400

    Bug 1141052 - Change SelfSupport test preferences to use HTTPS instead of HTTP. r=gfritzsche

--- addon-sdk/source/python-lib/cuddlefish/prefs.py
+++ addon-sdk/source/python-lib/cuddlefish/prefs.py
@@ -62,7 +62,7 @@ DEFAULT_NO_CONNECTIONS_PREFS = {
     'browser.safebrowsing.gethashURL': 'http://localhost/safebrowsing-dummy/gethash',
     'browser.safebrowsing.reportURL': 'http://localhost/safebrowsing-dummy/report',
     'browser.safebrowsing.malware.reportURL': 'http://localhost/safebrowsing-dummy/malwarereport',
-    'browser.selfsupport.url': 'http://localhost/repair-dummy',
+    'browser.selfsupport.url': 'https://localhost/selfsupport-dummy',
     'browser.trackingprotection.gethashURL': 'http://localhost/safebrowsing-dummy/gethash',
     'browser.trackingprotection.updateURL': 'http://localhost/safebrowsing-dummy/update',
 
