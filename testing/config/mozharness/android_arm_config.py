26450e6426fee0ff0d040d60aa59fc873f4fdb14
Jeff Gilbert <jgilbert@mozilla.com>
2015-03-18 17:19:22 -0700

    Bug 1143218 - Use mochitest subsuites to specify webgl tests. r=jmaher,gbrown

--- testing/config/mozharness/android_arm_config.py
+++ testing/config/mozharness/android_arm_config.py
@@ -28,7 +28,7 @@ config = {
                 "--certificate-path=%(certs_path)s", "--symbols-path=%(symbols_path)s",
                 "--quiet", "--log-raw=%(raw_log_file)s",
                 "--total-chunks=4",
-                "--manifest=tests/dom/canvas/test/_webgl-conformance.ini",
+                "--subsuite=webgl",
             ],
         },
         "robocop": {
