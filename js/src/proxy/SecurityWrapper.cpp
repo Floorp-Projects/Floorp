9eec185e50bcef837db40a0f30a85adf626dbc81
Jason Orendorff <jorendorff@mozilla.com>
2015-03-20 14:03:24 -0500

    Bug 1054756, part 4 - Remove BaseProxyHandler::defaultValue. r=jandem.

--- js/src/proxy/SecurityWrapper.cpp
+++ js/src/proxy/SecurityWrapper.cpp
@@ -74,17 +74,6 @@ SecurityWrapper<Base>::isExtensible(JSContext* cx, HandleObject wrapper, bool* e
     return true;
 }
 
-// For security wrappers, we run the OrdinaryToPrimitive algorithm on the wrapper
-// itself, which means that the existing security policy on operations like
-// toString() will take effect and do the right thing here.
-template <class Base>
-bool
-SecurityWrapper<Base>::defaultValue(JSContext* cx, HandleObject wrapper,
-                                    JSType hint, MutableHandleValue vp) const
-{
-    return OrdinaryToPrimitive(cx, wrapper, hint, vp);
-}
-
 template <class Base>
 bool
 SecurityWrapper<Base>::getBuiltinClass(JSContext* cx, HandleObject wrapper,
