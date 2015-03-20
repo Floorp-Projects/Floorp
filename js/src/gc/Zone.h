2f3db56bccf92ad7d3dabc5400fc067f9969602a
Steve Fink <sfink@mozilla.com>
2015-03-19 22:03:15 -0700

    Bug 1120655 - Suppress zone/compartment collection while iterating, r=terrence, a=abillings
    --HG--
    extra : rebase_source : 5aeedcf6a28427c50ab050b04a5d27d27e166898
    extra : source : 74aab037e628b4f5d772cd9ad13731fdfc7dff02

--- js/src/gc/Zone.h
+++ js/src/gc/Zone.h
@@ -324,10 +324,11 @@ enum ZoneSelector {
 
 class ZonesIter
 {
+    gc::AutoEnterIteration iterMarker;
     JS::Zone **it, **end;
 
   public:
-    ZonesIter(JSRuntime *rt, ZoneSelector selector) {
+    ZonesIter(JSRuntime *rt, ZoneSelector selector) : iterMarker(&rt->gc) {
         it = rt->gc.zones.begin();
         end = rt->gc.zones.end();
 
@@ -398,12 +399,13 @@ struct CompartmentsInZoneIter
 template<class ZonesIterT>
 class CompartmentsIterT
 {
+    gc::AutoEnterIteration iterMarker;
     ZonesIterT zone;
     mozilla::Maybe<CompartmentsInZoneIter> comp;
 
   public:
     explicit CompartmentsIterT(JSRuntime *rt)
-      : zone(rt)
+      : iterMarker(&rt->gc), zone(rt)
     {
         if (zone.done())
             comp.emplace();
@@ -412,7 +414,7 @@ class CompartmentsIterT
     }
 
     CompartmentsIterT(JSRuntime *rt, ZoneSelector selector)
-      : zone(rt, selector)
+      : iterMarker(&rt->gc), zone(rt, selector)
     {
         if (zone.done())
             comp.emplace();
