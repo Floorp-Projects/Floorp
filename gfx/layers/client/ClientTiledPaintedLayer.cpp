d659e63bdcc230b78066e99f2cefaa9ecbfc91c9
Kartikaya Gupta <kgupta@mozilla.com>
2015-03-20 06:58:04 -0400

    Bug 1036967 - Followup to fix debug logging. r=me

--- gfx/layers/client/ClientTiledPaintedLayer.cpp
+++ gfx/layers/client/ClientTiledPaintedLayer.cpp
@@ -182,7 +182,7 @@ ClientTiledPaintedLayer::BeginPaint()
   // Store the resolution from the displayport ancestor layer. Because this is Gecko-side,
   // before any async transforms have occurred, we can use the zoom for this.
   mPaintData.mResolution = displayportMetrics.GetZoom();
-  TILING_LOG("TILING %p: Resolution %f\n", this, mPaintData.mResolution.scale);
+  TILING_LOG("TILING %p: Resolution %s\n", this, Stringify(mPaintData.mResolution).c_str());
 
   // Store the applicable composition bounds in this layer's Layer units.
   mPaintData.mTransformToCompBounds =
