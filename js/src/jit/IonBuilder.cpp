808513e632070c34e2c71c9c879ebf4e98e5468e
Jan de Mooij <jdemooij@mozilla.com>
2015-03-20 13:45:35 +0100

    Bug 1142669 part 2 - Lower the script inlining size limit if off-thread compilation is not available. r=h4writer

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -4892,7 +4892,8 @@ IonBuilder::makeInliningDecision(JSObject *targetArg, CallInfo &callInfo)
 
     // Callee must not be excessively large.
     // This heuristic also applies to the callsite as a whole.
-    if (targetScript->length() > optimizationInfo().inlineMaxTotalBytecodeLength()) {
+    bool offThread = options.offThreadCompilationAvailable();
+    if (targetScript->length() > optimizationInfo().inlineMaxBytecodePerCallSite(offThread)) {
         trackOptimizationOutcome(TrackedOutcome::CantInlineBigCallee);
         return DontInline(targetScript, "Vetoed: callee excessively large");
     }
@@ -4959,7 +4960,8 @@ IonBuilder::selectInliningTargets(const ObjectVector &targets, CallInfo &callInf
             // Enforce a maximum inlined bytecode limit at the callsite.
             if (inlineable && target->as<JSFunction>().isInterpreted()) {
                 totalSize += target->as<JSFunction>().nonLazyScript()->length();
-                if (totalSize > optimizationInfo().inlineMaxTotalBytecodeLength())
+                bool offThread = options.offThreadCompilationAvailable();
+                if (totalSize > optimizationInfo().inlineMaxBytecodePerCallSite(offThread))
                     inlineable = false;
             }
         } else {
