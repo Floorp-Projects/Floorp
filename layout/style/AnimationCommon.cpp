816420d635d350056277af63903e0b1db1139eab
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-03-20 10:25:28 +0100

    Backed out changeset 550a27b00121 (bug 1117603) for perma OSX m-oth test failures on a CLOSED TREE

--- layout/style/AnimationCommon.cpp
+++ layout/style/AnimationCommon.cpp
@@ -376,13 +376,21 @@ CommonAnimationManager::GetAnimationRule(mozilla::dom::Element* aElement,
     return nullptr;
   }
 
-  // FIXME: Remove this assignment.  See bug 1061364.
-  if (!IsAnimationManager()) {
+  // Animations should already be refreshed, but transitions may not be.
+  // Note that this is temporary, we would like both animations and transitions
+  // to both be refreshed by this point.
+  if (IsAnimationManager()) {
+    NS_WARN_IF_FALSE(!collection->mNeedsRefreshes ||
+                     collection->mStyleRuleRefreshTime ==
+                       mPresContext->RefreshDriver()->MostRecentRefresh(),
+                     "should already have refreshed style rule");
+  } else {
+    // FIXME: Remove this assignment.  See bug 1061364.
     collection->mNeedsRefreshes = true;
+    collection->EnsureStyleRuleFor(
+      mPresContext->RefreshDriver()->MostRecentRefresh(),
+      EnsureStyleRule_IsNotThrottled);
   }
-  collection->EnsureStyleRuleFor(
-    mPresContext->RefreshDriver()->MostRecentRefresh(),
-    EnsureStyleRule_IsNotThrottled);
 
   return collection->mStyleRule;
 }
