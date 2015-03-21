b257cc5cb56c7a734b72702e45620fd5ef17cc7c
Kyle Huey <khuey@kylehuey.com>
2015-03-20 19:19:28 -0700

    Bug 1145870. r=bz, a=sledru

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -9546,6 +9546,8 @@ nsDocShell::InternalLoad(nsIURI* aURI,
                          nsIDocShell** aDocShell,
                          nsIRequest** aRequest)
 {
+  MOZ_RELEASE_ASSERT(!mBlockNavigation);
+
   nsresult rv = NS_OK;
   mOriginalUriString.Truncate();
 
