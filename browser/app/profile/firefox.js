f446567a60e36b5eb529f1cfaacc7bb8f5713260
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2015-03-20 21:12:40 -0700

    Backed out changeset 91b402c7cd92 (bug 1145911) for leaks and other orange on bc and dt tests

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1879,8 +1879,8 @@ pref("dom.ipc.reportProcessHangs", true);
 pref("reader.parse-on-load.enabled", false);
 #endif
 
-// Enable ReadingList browser UI by default.
-pref("browser.readinglist.enabled", true);
+// Disable ReadingList browser UI by default.
+pref("browser.readinglist.enabled", false);
 pref("browser.readinglist.sidebarEverOpened", false);
 // Enable the readinglist engine by default.
 pref("readinglist.scheduler.enabled", true);
