b6172b8aafdeba820f9a1af8576a961469a9d5ac
Phil Ringnalda <philringnalda@gmail.com>
2015-03-21 09:23:28 -0700

    Backed out 2 changesets (bug 938845) for testSettingsMenuItems NullPointerExceptions
    Backed out changeset 8e8d8f82f9de (bug 938845)
    Backed out changeset 28b3258bc0dc (bug 938845)

--- mobile/android/base/tests/testPrivateBrowsing.java
+++ mobile/android/base/tests/testPrivateBrowsing.java
@@ -19,27 +19,27 @@ import org.mozilla.gecko.Actions;
 public class testPrivateBrowsing extends ContentContextMenuTest {
 
     public void testPrivateBrowsing() {
-        String bigLinkUrl = getAbsoluteUrl(mStringHelper.ROBOCOP_BIG_LINK_URL);
-        String blank1Url = getAbsoluteUrl(mStringHelper.ROBOCOP_BLANK_PAGE_01_URL);
-        String blank2Url = getAbsoluteUrl(mStringHelper.ROBOCOP_BLANK_PAGE_02_URL);
+        String bigLinkUrl = getAbsoluteUrl(StringHelper.ROBOCOP_BIG_LINK_URL);
+        String blank1Url = getAbsoluteUrl(StringHelper.ROBOCOP_BLANK_PAGE_01_URL);
+        String blank2Url = getAbsoluteUrl(StringHelper.ROBOCOP_BLANK_PAGE_02_URL);
 
         blockForGeckoReady();
 
-        inputAndLoadUrl(mStringHelper.ABOUT_BLANK_URL);
+        inputAndLoadUrl(StringHelper.ABOUT_BLANK_URL);
 
-        addTab(bigLinkUrl, mStringHelper.ROBOCOP_BIG_LINK_TITLE, true);
+        addTab(bigLinkUrl, StringHelper.ROBOCOP_BIG_LINK_TITLE, true);
 
         verifyTabCount(1);
 
         // Open the link context menu and verify the options
-        verifyContextMenuItems(mStringHelper.CONTEXT_MENU_ITEMS_IN_PRIVATE_TAB);
+        verifyContextMenuItems(StringHelper.CONTEXT_MENU_ITEMS_IN_PRIVATE_TAB);
 
         // Check that "Open Link in New Tab" is not in the menu
-        mAsserter.ok(!mSolo.searchText(mStringHelper.CONTEXT_MENU_ITEMS_IN_NORMAL_TAB[0]), "Checking that 'Open Link in New Tab' is not displayed in the context menu", "'Open Link in New Tab' is not displayed in the context menu");
+        mAsserter.ok(!mSolo.searchText(StringHelper.CONTEXT_MENU_ITEMS_IN_NORMAL_TAB[0]), "Checking that 'Open Link in New Tab' is not displayed in the context menu", "'Open Link in New Tab' is not displayed in the context menu");
 
         // Open the link in a new private tab and check that it is private
         Actions.EventExpecter privateTabEventExpector = mActions.expectGeckoEvent("Tab:Added");
-        mSolo.clickOnText(mStringHelper.CONTEXT_MENU_ITEMS_IN_PRIVATE_TAB[0]);
+        mSolo.clickOnText(StringHelper.CONTEXT_MENU_ITEMS_IN_PRIVATE_TAB[0]);
         String eventData = privateTabEventExpector.blockForEventData();
         privateTabEventExpector.unregisterListener();
 
@@ -47,7 +47,7 @@ public class testPrivateBrowsing extends ContentContextMenuTest {
         verifyTabCount(2);
 
         // Open a normal tab to check later that it was registered in the Firefox Browser History
-        addTab(blank2Url, mStringHelper.ROBOCOP_BLANK_PAGE_02_TITLE, false);
+        addTab(blank2Url, StringHelper.ROBOCOP_BLANK_PAGE_02_TITLE, false);
         verifyTabCount(2);
 
         // Get the history list and check that the links open in private browsing are not saved
