a65dfa5a8435ad8d642088ab2a9655967bc46bf8
Jan Keromnes <janx@linux.com>
2015-03-21 04:02:00 -0400

    Bug 1135018 - Make devices.js use a CDN. r=ochameau

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -837,6 +837,9 @@ pref("devtools.remote.wifi.visible", false);
 // Client must complete TLS handshake within this window (ms)
 pref("devtools.remote.tls-handshake-timeout", 10000);
 
+// URL of the remote JSON catalog used for device simulation
+pref("devtools.devices.url", "https://code.cdn.mozilla.net/devices/devices.json");
+
 // view source
 pref("view_source.syntax_highlight", true);
 pref("view_source.wrap_long_lines", false);
