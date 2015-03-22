f4b29249a20e0409e41c7e41f17c15b186026a39
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-03-22 13:13:42 +1100

    Bug 1145101 - Part 5: Re-use the same PDM when recreating a decoder. r=mattwoodrow, a=sledru
    This ensure proper balance of PDM/Media Data Decoder Shutdown()

--- dom/media/fmp4/MP4Reader.cpp
+++ dom/media/fmp4/MP4Reader.cpp
@@ -577,10 +577,10 @@ void
 MP4Reader::DisableHardwareAcceleration()
 {
   if (HasVideo() && !mIsEncrypted && mSharedDecoderManager) {
-    mPlatform->DisableHardwareAcceleration();
+    mSharedDecoderManager->DisableHardwareAcceleration();
 
     const VideoDecoderConfig& video = mDemuxer->VideoConfig();
-    if (!mSharedDecoderManager->Recreate(mPlatform, video, mLayersBackendType, mDecoder->GetImageContainer())) {
+    if (!mSharedDecoderManager->Recreate(video, mLayersBackendType, mDecoder->GetImageContainer())) {
       MonitorAutoLock mon(mVideo.mMonitor);
       mVideo.mError = true;
       if (mVideo.HasPromise()) {
