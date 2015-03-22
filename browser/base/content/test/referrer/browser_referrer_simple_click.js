d8671c078156879549dc5ab8b8cb23600afcec7f
Alex Verstak <averstak@google.com>
2015-03-22 13:15:00 -0400

    Bug 1144816 - Extend timeout for referrer tests, slow shutdown on mac os x e10s. r=Gijs

--- browser/base/content/test/referrer/browser_referrer_simple_click.js
+++ browser/base/content/test/referrer/browser_referrer_simple_click.js
@@ -13,6 +13,6 @@ function startSimpleClickTestCase(aTestNumber) {
 };
 
 function test() {
-  requestLongerTimeout(5);  // slowwww shutdown on e10s
+  requestLongerTimeout(10);  // slowwww shutdown on e10s
   startReferrerTest(startSimpleClickTestCase);
 }
