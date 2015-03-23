824c331c321796185609dc5af6091e30279f2750
Benjamin Bouvier <benj@benj.me>
2015-03-23 15:45:05 +0100

    Bug 1146363: Inline SIMD.int32x4.bool; r=sunfish

---
+++ js/src/jit-test/tests/SIMD/bool.js
@@ -0,0 +1,15 @@
+load(libdir + 'simd.js');
+
+setJitCompilerOption("ion.warmup.trigger", 50);
+
+const T = -1, F = 0;
+
+function f() {
+    for (var i = 0; i < 150; i++) {
+        assertEqX4(SIMD.int32x4.bool(i + 1, true, 'hey', null), [T, T, T, F]);
+        assertEqX4(SIMD.int32x4.bool(undefined, '', {}, objectEmulatingUndefined()), [F, F, T, F]);
+        assertEqX4(SIMD.int32x4.bool(null, NaN, false, Infinity), [F, F, F, T]);
+    }
+}
+
+f();
