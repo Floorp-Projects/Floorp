b4b2bceb52128d41a3829f1e2282b0b6f801194f
Wes Kocher <wkocher@mozilla.com>
2015-03-23 16:06:30 -0700

    Backed out changeset c58f5ffb38e0 (bug 1142379)

--- dom/media/test/manifest.js
+++ dom/media/test/manifest.js
@@ -647,7 +647,7 @@ var gMetadataTests = [
 // Test files for Encrypted Media Extensions
 var gEMETests = [
   {
-    name:"video-only with 2 keys",
+    name:"bipbop-cenc-videoinit.mp4",
     type:"video/mp4; codecs=\"avc1.64000d,mp4a.40.2\"",
     fragments:[ "bipbop-cenc-videoinit.mp4",
                 "bipbop-cenc-video1.m4s",
@@ -659,11 +659,10 @@ var gEMETests = [
       "7e571d047e571d047e571d047e571d04" : "7e5744447e5744447e5744447e574444",
     },
     sessionType:"temporary",
-    sessions:1,
     duration:1.60,
   },
   {
-    name:"video-only with 2 keys, CORS",
+    name:"bipbop-cenc-videoinit.mp4",
     type:"video/mp4; codecs=\"avc1.64000d,mp4a.40.2\"",
     fragments:[ "bipbop-cenc-videoinit.mp4",
                 "bipbop-cenc-video1.m4s",
@@ -675,12 +674,11 @@ var gEMETests = [
       "7e571d047e571d047e571d047e571d04" : "7e5744447e5744447e5744447e574444",
     },
     sessionType:"temporary",
-    sessions:1,
     crossOrigin:true,
     duration:1.60,
   },
   {
-    name:"audio&video tracks, both with all keys",
+    name:"bipbop-cenc-videoinit.mp4",
     type:"video/mp4; codecs=\"avc1.64000d,mp4a.40.2\"",
     tracks: [
       {
@@ -707,11 +705,10 @@ var gEMETests = [
       "7e571d047e571d047e571d047e571d04" : "7e5744447e5744447e5744447e574444",
     },
     sessionType:"temporary",
-    sessions:2,
     duration:1.60,
   },
   {
-    name:"audio&video tracks, both with all keys, CORS",
+    name:"bipbop-cenc-videoinit.mp4",
     type:"video/mp4; codecs=\"avc1.64000d,mp4a.40.2\"",
     tracks: [
       {
@@ -738,39 +735,7 @@ var gEMETests = [
       "7e571d047e571d047e571d047e571d04" : "7e5744447e5744447e5744447e574444",
     },
     sessionType:"temporary",
-    sessions:2,
     crossOrigin:true,
-    duration:1.60,
-  },
-  {
-    name:"audio&video tracks, each with its key",
-    type:"video/mp4; codecs=\"avc1.64000d,mp4a.40.2\"",
-    tracks: [
-      {
-        name:"audio",
-        type:"audio/mp4; codecs=\"mp4a.40.2\"",
-        fragments:[ "bipbop-cenc1-audioinit.mp4",
-                    "bipbop-cenc1-audio1.m4s",
-                    "bipbop-cenc1-audio2.m4s",
-                    "bipbop-cenc1-audio3.m4s",
-                  ],
-      },
-      {
-        name:"video",
-        type:"video/mp4; codecs=\"avc1.64000d\"",
-        fragments:[ "bipbop-cenc1-videoinit.mp4",
-                    "bipbop-cenc1-video1.m4s",
-                    "bipbop-cenc1-video2.m4s",
-                  ],
-      },
-    ],
-    keys: {
-      // "keyid" : "key"
-      "7e571d037e571d037e571d037e571d03" : "7e5733337e5733337e5733337e573333",
-      "7e571d047e571d047e571d047e571d04" : "7e5744447e5744447e5744447e574444",
-    },
-    sessionType:"temporary",
-    sessions:2,
     duration:1.60,
   },
 ];
