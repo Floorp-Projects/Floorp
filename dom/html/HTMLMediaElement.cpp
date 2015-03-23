77467ca8fae6fa5f96b8b853cb72286578fd824b
Wes Kocher <wkocher@mozilla.com>
2015-03-23 16:06:32 -0700

    Backed out changeset f3b3547c610a (bug 1144409) for eme test failures CLOSED TREE

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -3093,10 +3093,7 @@ void HTMLMediaElement::MetadataLoaded(const MediaInfo* aInfo,
     }
 
 #ifdef MOZ_EME
-    // Dispatch a distinct 'encrypted' event for each initData we have.
-    for (const auto& initData : aInfo->mCrypto.mInitDatas) {
-      DispatchEncrypted(initData.mInitData, initData.mType);
-    }
+    DispatchEncrypted(aInfo->mCrypto.mInitData, aInfo->mCrypto.mType);
 #endif
   }
 
