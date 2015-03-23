6543d49db031cbcf052a6b15c72b6bad7c8c7a21
Jason Orendorff <jorendorff@mozilla.com>
2015-03-23 14:32:27 -0500

    Bug 1138499, part 2 - Strengthen assertComplete() to require that both [[Get]] and [[Set]] be present on accessor properties. r=Waldo.
    --HG--
    extra : rebase_source : 171aabdb0c4291d35e70fc0d6bf404a17a7bdb9b
    extra : source : 9b51b38317d6a85390d73074fd6344e576d2605d

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -2618,6 +2618,7 @@ class PropertyDescriptorOperations
                                      JSPROP_SHARED |
                                      JSPROP_REDEFINE_NONCONFIGURABLE |
                                      SHADOWABLE)) == 0);
+        MOZ_ASSERT_IF(isAccessorDescriptor(), has(JSPROP_GETTER) && has(JSPROP_SETTER));
 #endif
     }
 
