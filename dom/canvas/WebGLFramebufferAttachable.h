28bb60187dc7b20af26eae4c0ca6c12532222870
Jeff Gilbert <jgilbert@mozilla.com>
2015-03-24 16:00:28 -0700

    Bug 1017865 - Refactor attach/detach for FB attachments. - r=kamidphish

--- dom/canvas/WebGLFramebufferAttachable.h
+++ dom/canvas/WebGLFramebufferAttachable.h
@@ -3,12 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef WEBGLFRAMEBUFFERATTACHABLE_H_
-#define WEBGLFRAMEBUFFERATTACHABLE_H_
+#ifndef WEBGL_FRAMEBUFFER_ATTACHABLE_H_
+#define WEBGL_FRAMEBUFFER_ATTACHABLE_H_
 
 #include "GLDefs.h"
-#include "nsTArray.h"
 #include "mozilla/WeakPtr.h"
+#include "nsTArray.h"
 #include "WebGLFramebuffer.h"
 #include "WebGLStrongTypes.h"
 
@@ -16,29 +16,13 @@ namespace mozilla {
 
 class WebGLFramebufferAttachable
 {
-    struct AttachmentPoint
-    {
-        AttachmentPoint(const WebGLFramebuffer* fb, FBAttachment attachment)
-            : mFB(fb)
-            , mAttachment(attachment)
-        {}
-
-        WeakPtr<const WebGLFramebuffer> mFB;
-        FBAttachment mAttachment;
-
-        bool operator==(const AttachmentPoint& o) const {
-          return mFB == o.mFB && mAttachment == o.mAttachment;
-        }
-    };
-
-    nsTArray<AttachmentPoint> mAttachmentPoints;
+    nsTArray<const WebGLFramebuffer::AttachPoint*> mAttachmentPoints;
 
 public:
-
     // Track FBO/Attachment combinations
-    void AttachTo(WebGLFramebuffer* fb, FBAttachment attachment);
-    void DetachFrom(WebGLFramebuffer* fb, FBAttachment attachment);
-    void NotifyFBsStatusChanged();
+    void MarkAttachment(const WebGLFramebuffer::AttachPoint& attachment);
+    void UnmarkAttachment(const WebGLFramebuffer::AttachPoint& attachment);
+    void InvalidateStatusOfAttachedFBs() const;
 };
 
 } // namespace mozilla
