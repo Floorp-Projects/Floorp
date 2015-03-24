4f778e424feec0953fab1c42a9398161242fc2fa
Patrick Brosset <pbrosset@mozilla.com>
2015-03-24 15:57:33 +0100

    Bug 1106272 - Part 3: Test for the selector highlighter feature in the rule-view. r=miker

--- browser/devtools/styleinspector/test/browser_ruleview_selector-highlighter_02.js
+++ browser/devtools/styleinspector/test/browser_ruleview_selector-highlighter_02.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-// Test that the selector highlighter is shown when hovering over a selector
+// Test that the selector highlighter is shown when clicking on a selector icon
 // in the rule-view
 
 // Note that in this test, we mock the highlighter front, merely testing the
@@ -22,12 +22,10 @@ const PAGE_CONTENT = [
   '<p>Testing the selector highlighter</p>'
 ].join("\n");
 
-const TYPE = "SelectorHighlighter";
-
 add_task(function*() {
   yield addTab("data:text/html;charset=utf-8," + PAGE_CONTENT);
 
-  let {inspector, view: rView} = yield openRuleView();
+  let {inspector, view} = yield openRuleView();
 
   // Mock the highlighter front to get the reference of the NodeFront
   let HighlighterFront = {
@@ -47,39 +45,41 @@ add_task(function*() {
   };
 
   // Inject the mock highlighter in the rule-view
-  rView.highlighters.promises[TYPE] = {
-    then: function(cb) {
-      cb(HighlighterFront);
-    }
-  };
+  view.selectorHighlighter = HighlighterFront;
 
-  let selectorSpan = getRuleViewSelector(rView, "body").firstElementChild;
+  let icon = getRuleViewSelectorHighlighterIcon(view, "body");
 
   info("Checking that the HighlighterFront's show/hide methods are called");
-  rView.highlighters._onMouseMove({target: selectorSpan});
+
+  info("Clicking once on the body selector highlighter icon");
+  yield clickSelectorIcon(icon, view);
   ok(HighlighterFront.isShown, "The highlighter is shown");
-  rView.highlighters._onMouseLeave();
+
+  info("Clicking once again on the body selector highlighter icon");
+  yield clickSelectorIcon(icon, view);
   ok(!HighlighterFront.isShown, "The highlighter is hidden");
 
   info("Checking that the right NodeFront reference and options are passed");
   yield selectNode("p", inspector);
-  selectorSpan = getRuleViewSelector(rView, "p").firstElementChild;
-  rView.highlighters._onMouseMove({target: selectorSpan});
+  icon = getRuleViewSelectorHighlighterIcon(view, "p");
+
+  yield clickSelectorIcon(icon, view);
   is(HighlighterFront.nodeFront.tagName, "P",
     "The right NodeFront is passed to the highlighter (1)");
   is(HighlighterFront.options.selector, "p",
     "The right selector option is passed to the highlighter (1)");
 
   yield selectNode("body", inspector);
-  selectorSpan = getRuleViewSelector(rView, "body").firstElementChild;
-  rView.highlighters._onMouseMove({target: selectorSpan});
+  icon = getRuleViewSelectorHighlighterIcon(view, "body");
+  yield clickSelectorIcon(icon, view);
   is(HighlighterFront.nodeFront.tagName, "BODY",
     "The right NodeFront is passed to the highlighter (2)");
   is(HighlighterFront.options.selector, "body",
     "The right selector option is passed to the highlighter (2)");
-
-  info("Checking that the highlighter gets hidden when hovering somewhere else");
-  let {valueSpan} = getRuleViewProperty(rView, "body", "background");
-  rView.highlighters._onMouseMove({target: valueSpan});
-  ok(!HighlighterFront.isShown, "The highlighter is hidden");
 });
+
+function* clickSelectorIcon(icon, view) {
+  let onToggled = view.once("ruleview-selectorhighlighter-toggled");
+  EventUtils.synthesizeMouseAtCenter(icon, {}, view.doc.defaultView);
+  yield onToggled;
+}
