4d747887c960df8a4dc56ffe8fc17afa8f32eb6a
Benjamin Bouvier <benj@benj.me>
2015-03-24 16:32:49 +0100

    Bug 1146295: Make some arguments facultative in splat/with functions; r=h4writer

--- js/src/tests/ecma_7/SIMD/splat.js
+++ js/src/tests/ecma_7/SIMD/splat.js
@@ -23,12 +23,15 @@ function test() {
     var bad = {valueOf: () => { throw new TestError(); }};
 
     TestSplatX4('int32x4', [0, undefined, 3.5, 42, -1337, INT32_MAX, INT32_MAX + 1, good], (x) => x | 0);
+    assertEqX4(SIMD.int32x4.splat(), [0, 0, 0, 0]);
     assertThrowsInstanceOf(() => SIMD.int32x4.splat(bad), TestError);
 
     TestSplatX4('float32x4', [0, undefined, 3.5, 42, -13.37, Infinity, NaN, -0, good], (x) => Math.fround(x));
+    assertEqX4(SIMD.float32x4.splat(), [NaN, NaN, NaN, NaN]);
     assertThrowsInstanceOf(() => SIMD.float32x4.splat(bad), TestError);
 
     TestSplatX2('float64x2', [0, undefined, 3.5, 42, -13.37, Infinity, NaN, -0, good], (x) => +x);
+    assertEqX4(SIMD.float64x2.splat(), [NaN, NaN]);
     assertThrowsInstanceOf(() => SIMD.float64x2.splat(bad), TestError);
 
     if (typeof reportCompare === "function")
