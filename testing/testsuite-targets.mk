de006f26a82a11e8eb9cc28eccf87549d6bfd124
Jonathan Griffin <jgriffin@mozilla.com>
2015-03-24 15:17:53 -0700

    Bug 1147029 - Land luciddream in-tree, r=ted

--- testing/testsuite-targets.mk
+++ testing/testsuite-targets.mk
@@ -407,6 +407,7 @@ package-tests: \
   stage-cppunittests \
   stage-jittest \
   stage-web-platform-tests \
+  stage-luciddream \
   $(NULL)
 ifdef MOZ_WEBRTC
 package-tests: stage-steeplechase
@@ -534,6 +535,11 @@ stage-steeplechase: make-stage-dir
 	cp -RL $(DIST)/xpi-stage/specialpowers $(PKG_STAGE)/steeplechase
 	cp -RL $(topsrcdir)/testing/profiles/prefs_general.js $(PKG_STAGE)/steeplechase
 
+LUCIDDREAM_DIR=$(PKG_STAGE)/luciddream
+stage-luciddream: make-stage-dir
+	$(NSINSTALL) -D $(LUCIDDREAM_DIR)
+	@(cd $(topsrcdir)/testing/luciddream && tar $(TAR_CREATE_FLAGS) - *) | (cd $(LUCIDDREAM_DIR)/ && tar -xf -)
+
 MARIONETTE_DIR=$(PKG_STAGE)/marionette
 stage-marionette: make-stage-dir
 	$(NSINSTALL) -D $(MARIONETTE_DIR)/tests
@@ -590,5 +596,6 @@ stage-instrumentation-tests: make-stage-dir
   stage-steeplechase \
   stage-web-platform-tests \
   stage-instrumentation-tests \
+  stage-luciddream \
   $(NULL)
 
