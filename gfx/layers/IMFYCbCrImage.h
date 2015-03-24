10e432584f4c9470752ce9ea5baa396c844e6770
Matt Woodrow <mwoodrow@mozilla.com>
2015-03-24 13:33:55 +1300

    Backed out changeset 0c23dcbc6bf7 (bug 1138967) for causing crashes

--- gfx/layers/IMFYCbCrImage.h
+++ gfx/layers/IMFYCbCrImage.h
@@ -16,11 +16,13 @@ namespace layers {
 class IMFYCbCrImage : public PlanarYCbCrImage
 {
 public:
-  IMFYCbCrImage(IMFMediaBuffer* aBuffer, IMF2DBuffer* a2DBuffer);
+  IMFYCbCrImage(IMFMediaBuffer* aBuffer, IMF2DBuffer* a2DBuffer)
+    : PlanarYCbCrImage(nullptr)
+    , mBuffer(aBuffer)
+    , m2DBuffer(a2DBuffer)
+  {}
 
   virtual bool IsValid() { return true; }
-
-  virtual TextureClient* GetTextureClient(CompositableClient* aClient) MOZ_OVERRIDE;
 
 protected:
   virtual uint8_t* AllocateBuffer(uint32_t aSize) MOZ_OVERRIDE {
@@ -28,11 +30,18 @@ protected:
     return nullptr;
   }
 
-  ~IMFYCbCrImage();
+  ~IMFYCbCrImage()
+  {
+    if (m2DBuffer) {
+      m2DBuffer->Unlock2D();
+    }
+    else {
+      mBuffer->Unlock();
+    }
+  }
 
   RefPtr<IMFMediaBuffer> mBuffer;
   RefPtr<IMF2DBuffer> m2DBuffer;
-  RefPtr<TextureClient> mTextureClient;
 };
 
 } // namepace layers
