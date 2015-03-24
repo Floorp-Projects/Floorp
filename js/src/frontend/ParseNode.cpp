5798244efaa1d3852cce664251d49ad4cb7bf267
Terrence Cole <terrence@mozilla.com>
2015-03-24 10:51:07 -0700

    Bug 977308 - Pre-tenure all objects attached to scripts; r=sfink
    --HG--
    extra : rebase_source : d5638a91ffca3008b353d68f480014066c94a2d1

--- js/src/frontend/ParseNode.cpp
+++ js/src/frontend/ParseNode.cpp
@@ -1097,6 +1097,7 @@ ObjectBox::ObjectBox(JSObject *object, ObjectBox* traceLink)
     emitLink(nullptr)
 {
     MOZ_ASSERT(!object->is<JSFunction>());
+    MOZ_ASSERT(object->isTenured());
 }
 
 ObjectBox::ObjectBox(JSFunction *function, ObjectBox* traceLink)
@@ -1106,6 +1107,7 @@ ObjectBox::ObjectBox(JSFunction *function, ObjectBox* traceLink)
 {
     MOZ_ASSERT(object->is<JSFunction>());
     MOZ_ASSERT(asFunctionBox()->function() == function);
+    MOZ_ASSERT(object->isTenured());
 }
 
 FunctionBox *
