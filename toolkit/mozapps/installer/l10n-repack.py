7be8888c6136cfa8867e0ec921451c3ee24f65da
Mike Hommey <mh+mozilla@glandium.org>
2015-03-26 14:31:15 +0900

    Bug 1147207 - Allow to give extra l10n directories to l10n-repack.py. r=gps, a=sledru
    This allows to use separate l10n staging directories for e.g. addons.

--- toolkit/mozapps/installer/l10n-repack.py
+++ toolkit/mozapps/installer/l10n-repack.py
@@ -28,12 +28,23 @@ NON_CHROME = set([
 ])
 
 
+def valid_extra_l10n(arg):
+    if '=' not in arg:
+        raise ValueError('Invalid value')
+    return tuple(arg.split('=', 1))
+
+
 def main():
     parser = ArgumentParser()
     parser.add_argument('build',
                         help='Directory containing the build to repack')
     parser.add_argument('l10n',
                         help='Directory containing the staged langpack')
+    parser.add_argument('extra_l10n', nargs='*', metavar='BASE=PATH',
+                        type=valid_extra_l10n,
+                        help='Extra directories with staged localized files '
+                             'to be considered under the given base in the '
+                             'repacked build')
     parser.add_argument('--non-resource', nargs='+', metavar='PATTERN',
                         default=[],
                         help='Extra files not to be considered as resources')
@@ -41,7 +52,8 @@ def main():
 
     buildconfig.substs['USE_ELF_HACK'] = False
     buildconfig.substs['PKG_SKIP_STRIP'] = True
-    l10n.repack(args.build, args.l10n, args.non_resource, NON_CHROME)
+    l10n.repack(args.build, args.l10n, extra_l10n=dict(args.extra_l10n),
+                non_resources=args.non_resource, non_chrome=NON_CHROME)
 
 
 if __name__ == "__main__":
