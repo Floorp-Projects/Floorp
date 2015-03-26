fac58baacd71cd60fe8af02ec64bcd2a9c1cfc27
Benjamin Chen <bechen@mozilla.com>
2015-03-26 17:06:06 +0800

    Bug 1139781 - Implement VideoPlaybackQuality for MediaCodecReader. r=cpearce

--- dom/media/omx/MediaCodecReader.cpp
+++ dom/media/omx/MediaCodecReader.cpp
@@ -1006,6 +1006,8 @@ MediaCodecReader::DecodeVideoFrameSync(int64_t aTimeThreshold)
     }
 
     if (v) {
+      // Notify mDecoder that we have decoded a video frame.
+      mDecoder->NotifyDecodedFrames(0, 1, 0);
       VideoQueue().Push(v);
     } else {
       NS_WARNING("Unable to create VideoData");
@@ -1817,6 +1819,10 @@ MediaCodecReader::GetCodecOutputData(Track& aTrack,
     }
 
     if (status == OK) {
+      // Notify mDecoder that we have parsed a video frame.
+      if (&aTrack == &mVideoTrack) {
+        mDecoder->NotifyDecodedFrames(1, 0, 0);
+      }
       if (!IsValidTimestampUs(aThreshold) || info.mTimeUs >= aThreshold) {
         // Get a valid output buffer.
         break;
