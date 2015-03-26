4d9444dff1e1908f4028afa9e900a8e3a1458160
Seth Fowler <seth@mozilla.com>
2015-03-26 15:39:47 -0700

    Bug 1147706 - Warn if we don't use the result of const methods on BaseRect and its subclasses. r=tn

--- gfx/src/nsRect.h
+++ gfx/src/nsRect.h
@@ -59,7 +59,7 @@ struct NS_GFX nsRect :
   // overflowing nscoord values in the 'width' and 'height' fields by
   // clamping the width and height values to nscoord_MAX if necessary.
 
-  nsRect SaturatingUnion(const nsRect& aRect) const
+  MOZ_WARN_UNUSED_RESULT nsRect SaturatingUnion(const nsRect& aRect) const
   {
     if (IsEmpty()) {
       return aRect;
@@ -70,7 +70,7 @@ struct NS_GFX nsRect :
     }
   }
 
-  nsRect SaturatingUnionEdges(const nsRect& aRect) const
+  MOZ_WARN_UNUSED_RESULT nsRect SaturatingUnionEdges(const nsRect& aRect) const
   {
 #ifdef NS_COORD_IS_FLOAT
     return UnionEdges(aRect);
@@ -107,7 +107,7 @@ struct NS_GFX nsRect :
 
 #ifndef NS_COORD_IS_FLOAT
   // Make all nsRect Union methods be saturating.
-  nsRect UnionEdges(const nsRect& aRect) const
+  MOZ_WARN_UNUSED_RESULT nsRect UnionEdges(const nsRect& aRect) const
   {
     return SaturatingUnionEdges(aRect);
   }
@@ -115,7 +115,7 @@ struct NS_GFX nsRect :
   {
     *this = aRect1.UnionEdges(aRect2);
   }
-  nsRect Union(const nsRect& aRect) const
+  MOZ_WARN_UNUSED_RESULT nsRect Union(const nsRect& aRect) const
   {
     return SaturatingUnion(aRect);
   }
@@ -148,17 +148,28 @@ struct NS_GFX nsRect :
   MOZ_WARN_UNUSED_RESULT inline nsRect
     ScaleToOtherAppUnitsRoundIn(int32_t aFromAPP, int32_t aToAPP) const;
 
-  inline nsIntRect ScaleToNearestPixels(float aXScale, float aYScale,
-                                        nscoord aAppUnitsPerPixel) const;
-  inline nsIntRect ToNearestPixels(nscoord aAppUnitsPerPixel) const;
+  MOZ_WARN_UNUSED_RESULT inline nsIntRect
+  ScaleToNearestPixels(float aXScale, float aYScale,
+                       nscoord aAppUnitsPerPixel) const;
+
+  MOZ_WARN_UNUSED_RESULT inline nsIntRect
+  ToNearestPixels(nscoord aAppUnitsPerPixel) const;
+
   // Note: this can turn an empty rectangle into a non-empty rectangle
-  inline nsIntRect ScaleToOutsidePixels(float aXScale, float aYScale,
-                                        nscoord aAppUnitsPerPixel) const;
+  MOZ_WARN_UNUSED_RESULT inline nsIntRect
+  ScaleToOutsidePixels(float aXScale, float aYScale,
+                       nscoord aAppUnitsPerPixel) const;
+
   // Note: this can turn an empty rectangle into a non-empty rectangle
-  inline nsIntRect ToOutsidePixels(nscoord aAppUnitsPerPixel) const;
-  inline nsIntRect ScaleToInsidePixels(float aXScale, float aYScale,
-                                       nscoord aAppUnitsPerPixel) const;
-  inline nsIntRect ToInsidePixels(nscoord aAppUnitsPerPixel) const;
+  MOZ_WARN_UNUSED_RESULT inline nsIntRect
+  ToOutsidePixels(nscoord aAppUnitsPerPixel) const;
+
+  MOZ_WARN_UNUSED_RESULT inline nsIntRect
+  ScaleToInsidePixels(float aXScale, float aYScale,
+                      nscoord aAppUnitsPerPixel) const;
+
+  MOZ_WARN_UNUSED_RESULT inline nsIntRect
+  ToInsidePixels(nscoord aAppUnitsPerPixel) const;
 
   // This is here only to keep IPDL-generated code happy. DO NOT USE.
   bool operator==(const nsRect& aRect) const
@@ -186,7 +197,8 @@ struct NS_GFX nsIntRect :
   {
   }
 
-  inline nsRect ToAppUnits(nscoord aAppUnitsPerPixel) const;
+  MOZ_WARN_UNUSED_RESULT inline nsRect
+  ToAppUnits(nscoord aAppUnitsPerPixel) const;
 
   // Returns a special nsIntRect that's used in some places to signify
   // "all available space".
