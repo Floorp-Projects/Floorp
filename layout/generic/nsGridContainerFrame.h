ec93089d08807bb7a422cd32f88c9b570eb810fd
Mats Palmgren <matspal@gmail.com>
2015-03-26 18:57:39 +0000

    Bug 1107783 - part 1, [css-grid] Implement abs.pos. grid item placement and reflow.  r=dholbert

--- layout/generic/nsGridContainerFrame.h
+++ layout/generic/nsGridContainerFrame.h
@@ -38,12 +38,25 @@ public:
   virtual nsresult GetFrameName(nsAString& aResult) const override;
 #endif
 
+  /**
+   * Return the containing block for aChild which MUST be an abs.pos. child
+   * of a grid container.  This is just a helper method for
+   * nsAbsoluteContainingBlock::Reflow - it's not meant to be used elsewhere.
+   */
+  static const nsRect& GridItemCB(nsIFrame* aChild);
+
   struct TrackSize {
     nscoord mBase;
     nscoord mLimit;
   };
 
+  // @see nsAbsoluteContainingBlock::Reflow about this magic number
+  static const nscoord VERY_LIKELY_A_GRID_CONTAINER = -123456789;
+
+  NS_DECLARE_FRAME_PROPERTY(GridItemContainingBlockRect, DeleteValue<nsRect>)
+
 protected:
+  typedef mozilla::LogicalPoint LogicalPoint;
   typedef mozilla::LogicalRect LogicalRect;
   typedef mozilla::WritingMode WritingMode;
   typedef mozilla::css::GridNamedArea GridNamedArea;
@@ -57,7 +70,12 @@ protected:
    * (both 1-based) where mStart < mEnd.  Before it's definite it can also
    * represent an auto position with a span, where mStart == 0 and mEnd is
    * the (non-zero positive) span.
-   * In both states the invariant mEnd > mStart holds.
+   * In both states the invariant mEnd > mStart holds (for normal flow items).
+   *
+   * For abs.pos. grid items, mStart and mEnd may both be zero, meaning
+   * "attach this side to the grid container containing block edge".
+   * Additionally, mEnd >= mStart holds when both are definite (non-zero),
+   * i.e. the invariant is slightly relaxed compared to normal flow items.
    */
   struct LineRange {
    LineRange(uint32_t aStart, uint32_t aEnd)
@@ -88,6 +106,15 @@ protected:
      */
     void ToPositionAndLength(const nsTArray<TrackSize>& aTrackSizes,
                              nscoord* aPos, nscoord* aLength) const;
+    /**
+     * Given an array of track sizes and a grid origin coordinate, adjust the
+     * abs.pos. containing block along an axis given by aPos and aLength.
+     * aPos and aLength should already be initialized to the grid container
+     * containing block for this axis before calling this method.
+     */
+    void ToPositionAndLengthForAbsPos(const nsTArray<TrackSize>& aTrackSizes,
+                                      nscoord aGridOrigin,
+                                      nscoord* aPos, nscoord* aLength) const;
 
     uint32_t mStart;  // the start line, or zero for 'auto'
     uint32_t mEnd;    // the end line, or the span length for 'auto'
@@ -182,6 +209,21 @@ protected:
                              const nsStylePosition* aStyle);
 
   /**
+   * As above but for an abs.pos. child.  Any 'auto' lines will be represented
+   * by zero in the LineRange result.
+   * @param aGridEnd the last line in the (final) implicit grid
+   */
+  LineRange
+  ResolveAbsPosLineRange(const nsStyleGridLine& aStart,
+                         const nsStyleGridLine& aEnd,
+                         const nsTArray<nsTArray<nsString>>& aLineNameList,
+                         uint32_t GridNamedArea::* aAreaStart,
+                         uint32_t GridNamedArea::* aAreaEnd,
+                         uint32_t aExplicitGridEnd,
+                         uint32_t aGridEnd,
+                         const nsStylePosition* aStyle);
+
+  /**
    * Return a GridArea with non-auto lines placed at a definite line number
    * and with placement errors resolved.  One or both positions may still be
    * 'auto'.
@@ -243,6 +285,15 @@ protected:
    */
   void PlaceAutoAutoInColOrder(uint32_t aStartCol, uint32_t aStartRow,
                                GridArea* aArea) const;
+
+  /**
+   * Place an abs.pos. child and return its grid area.
+   * @note the resulting area may still have 'auto' lines in one or both
+   * dimensions (represented as zero).
+   * @param aChild the abs.pos. grid item to place
+   * @param aStyle the StylePosition() for the grid container
+   */
+  GridArea PlaceAbsPos(nsIFrame* aChild, const nsStylePosition* aStyle);
 
   /**
    * Place all child frames into the grid and expand the (implicit) grid as
@@ -330,6 +381,22 @@ protected:
                                  const GridArea& aArea,
                                  const nsTArray<TrackSize>& aColSizes,
                                  const nsTArray<TrackSize>& aRowSizes) const;
+
+  /**
+   * Return the containing block for an abs.pos. grid item occupying aArea.
+   * Any 'auto' lines in the grid area will be aligned with grid container
+   * containing block on that side.
+   * @param aColSizes column track sizes
+   * @param aRowSizes row track sizes
+   * @param aGridOrigin the origin of the grid
+   * @param aGridCB the grid container containing block (its padding area)
+   */
+  LogicalRect ContainingBlockForAbsPos(const WritingMode& aWM,
+                                       const GridArea& aArea,
+                                       const nsTArray<TrackSize>& aColSizes,
+                                       const nsTArray<TrackSize>& aRowSizes,
+                                       const LogicalPoint& aGridOrigin,
+                                       const LogicalRect& aGridCB) const;
 
   /**
    * Reflow and place our children.
