7270350f3d29e7f8fcfb1aafc4685afbf11fcab6
Richard Newman <rnewman@mozilla.com>
2015-03-27 13:11:08 -0700

    Bug 1147473 - Follow-up: move ReadingListConstants to avoid build flag pain., a=readinglist

--- mobile/android/base/background/ReadingListConstants.java
+++ mobile/android/base/background/ReadingListConstants.java
@@ -2,10 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.gecko.reading;
+package org.mozilla.gecko.background;
 
 import org.mozilla.gecko.AppConstants;
 
+/**
+ * This is in 'background' not 'reading' so that it's still usable even when the
+ * Reading List feature is build-time disabled.
+ */
 public class ReadingListConstants {
   public static final String GLOBAL_LOG_TAG = "FxReadingList";
   public static final String USER_AGENT = "Firefox-Android-FxReader/" + AppConstants.MOZ_APP_VERSION + " (" + AppConstants.MOZ_APP_DISPLAYNAME + ")";
