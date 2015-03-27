d41fd71a288d2f526e43037b57ae1ca85144b58e
Andrew Overholt <overholt@mozilla.com>
2015-03-27 06:47:00 -0400

    Bug 1127552 - Add Telemetry to Fetch. r=nsm
    Note when the Fetch is coming from the main thread or a worker.  Also
    count Requests just passed through a Service Worker.

--- dom/workers/ServiceWorkerManager.cpp
+++ dom/workers/ServiceWorkerManager.cpp
@@ -2302,6 +2302,10 @@ private:
     if (NS_WARN_IF(rv.Failed())) {
       return false;
     }
+    // For Telemetry, note that this Request object was created by a Fetch event.
+    nsRefPtr<InternalRequest> internalReq = request->GetInternalRequest();
+    MOZ_ASSERT(internalReq);
+    internalReq->SetCreatedByFetchEvent();
 
     RootedDictionary<FetchEventInit> init(aCx);
     init.mRequest.Construct();
