b24216d5d2f5217d9efd82867686ed1be9a19d65
Dave Hylands <dhylands@mozilla.com>
2015-03-27 14:22:20 -0700

    Bug 1140597 - Don't try and process device storage notifications if the process isn't using device storage.

--- dom/devicestorage/DeviceStorage.h
+++ dom/devicestorage/DeviceStorage.h
@@ -196,6 +196,8 @@ public:
 
   explicit nsDOMDeviceStorage(nsPIDOMWindow* aWindow);
 
+  static int InstanceCount() { return sInstanceCount; }
+
   nsresult Init(nsPIDOMWindow* aWindow, const nsAString& aType,
                 const nsAString& aVolName);
 
@@ -318,6 +320,8 @@ private:
   EnumerateInternal(const nsAString& aName,
                     const EnumerationParameters& aOptions, bool aEditable,
                     ErrorResult& aRv);
+
+  static int sInstanceCount;
 
   nsString mStorageType;
   nsCOMPtr<nsIFile> mRootDirectory;
