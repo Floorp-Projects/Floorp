fcb2e29822974c1a93df5eaf29c60ceb5bc69fa8
Nick Alexander <nalexander@mozilla.com>
2015-03-27 08:27:28 -0700

    Bug 1148504 - Protect Firefox Account state with a critical section. r=rnewman, a=readinglist
    ========
    
    https://github.com/mozilla-services/android-sync/commit/8b1d353ee8c5f049c65d6ab437d3adee871ae8ec
    Author: Nick Alexander <nalexander@mozilla.com>
        Bug 1148504 - Part 2: Make updating Firefox Account state happen in a critical section.
    
        It's worth noting that the two consumers of the shared state lock will
        only race for a very short window -- essentially only when creating or
        re-connecting an account.
    
        That's because Reading List oauth tokens are long-lived and do not
        expire (yet) in response to remote Account state changes, such as
        updating the Account password.  So Sync and RL will race to initialize
        the Account state; eventually RL will get an oauth token; and that token
        will be cached forever until RL produces a 401 for the token or Android
        expires the token.
    
        Since Sync requests a token server token at the start of every sync, the
        lock will be constantly exercised, but should never block.
    
    ========
    
    https://github.com/mozilla-services/android-sync/commit/d7a8611810ebd6872df7ffdcf301e30520fc6ff9
    Author: Nick Alexander <nalexander@mozilla.com>
        Bug 1148504 - Part 1: Reduce scope of section that may set Account state.
    
        The only place that might throw a TokenServerException is the token
        server client code itself.  By handling such an exception earlier, we
        reduce the scope of the section that may update the Firefox Account
        state.  (This comes at the cost of threading AndroidFxAccount into
        syncWithAssertion, but c'est la vie.)
    
        This does not interact with the exist handling of 401s that we might see
        from the storage endpoint.  Those 401s never generated
        TokenServerExceptions; in fact, they were (essentially) ignored.  Since
        we fetch a fresh token every Sync, what was (and is) expected is that
        such 401s would be transient and fixed by authenticating with a fresher
        token.
    
        Test plan: manually verify that remotely changing the Firefox Account's
        password while the device is in the Married state does the following:
    
        1) uses the cached certificate to generate a local assertion;
        2) the assertion produces a 401 from the TokenServerException, since the
        certificate is no longer fresh;
        3) the TokenServerException drives the Account state to Cohabiting;
        4) the state machine discovers it cannot /sign, driving the Account
        state to Separated.

--- mobile/android/base/fxa/sync/FxAccountSyncDelegate.java
+++ mobile/android/base/fxa/sync/FxAccountSyncDelegate.java
@@ -6,32 +6,23 @@ package org.mozilla.gecko.fxa.sync;
 
 import java.util.concurrent.CountDownLatch;
 
-import org.mozilla.gecko.fxa.authenticator.AndroidFxAccount;
-import org.mozilla.gecko.fxa.login.Married;
 import org.mozilla.gecko.fxa.login.State;
-import org.mozilla.gecko.fxa.login.State.StateLabel;
-import org.mozilla.gecko.tokenserver.TokenServerException;
 
 import android.content.SyncResult;
 
 public class FxAccountSyncDelegate {
   protected final CountDownLatch latch;
   protected final SyncResult syncResult;
-  protected final AndroidFxAccount fxAccount;
 
-  public FxAccountSyncDelegate(CountDownLatch latch, SyncResult syncResult, AndroidFxAccount fxAccount) {
+  public FxAccountSyncDelegate(CountDownLatch latch, SyncResult syncResult) {
     if (latch == null) {
       throw new IllegalArgumentException("latch must not be null");
     }
     if (syncResult == null) {
       throw new IllegalArgumentException("syncResult must not be null");
     }
-    if (fxAccount == null) {
-      throw new IllegalArgumentException("fxAccount must not be null");
-    }
     this.latch = latch;
     this.syncResult = syncResult;
-    this.fxAccount = fxAccount;
   }
 
   /**
@@ -65,16 +56,6 @@ public class FxAccountSyncDelegate {
 
   public void handleError(Exception e) {
     setSyncResultSoftError();
-    // This is awful, but we need to propagate bad assertions back up the
-    // chain somehow, and this will do for now.
-    if (e instanceof TokenServerException) {
-      // We should only get here *after* we're locked into the married state.
-      State state = fxAccount.getState();
-      if (state.getStateLabel() == StateLabel.Married) {
-        Married married = (Married) state;
-        fxAccount.setState(married.makeCohabitingState());
-      }
-    }
     latch.countDown();
   }
 
