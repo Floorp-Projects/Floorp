144b580c7c4ff247241f534e02d892895e6f9778
Nicolas Silva <nsilva@mozilla.com>
2015-03-27 11:16:27 +0100

    Bug 1145981 - Do not crash when a DIB texture is updated without a compositor. r=jrmuizel, a=sledru

--- gfx/layers/TextureDIB.cpp
+++ gfx/layers/TextureDIB.cpp
@@ -145,6 +145,12 @@ DIBTextureHost::BindTextureSource(CompositableTextureSourceRef& aTexture)
 void
 DIBTextureHost::Updated(const nsIntRegion* aRegion)
 {
+  if (!mCompositor) {
+    // This can happen if we send textures to a compositable that isn't yet
+    // attached to a layer.
+    return;
+  }
+
   if (!mTextureSource) {
     mTextureSource = mCompositor->CreateDataTextureSource(mFlags);
   }
