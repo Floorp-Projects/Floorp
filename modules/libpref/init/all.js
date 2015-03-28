572f9f4f0de2f71a782c4170d494f10f90061863
Mats Palmgren <mats@mozilla.com>
2015-03-28 20:50:35 +0000

    Bug 1148104 - Enable fragmentation of floats in column layout in RELEASE builds.  r=roc

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2300,11 +2300,7 @@ pref("layout.frame_rate.precise", false);
 pref("layout.spammy_warnings.enabled", true);
 
 // Should we fragment floats inside CSS column layout?
-#ifdef RELEASE_BUILD
-pref("layout.float-fragments-inside-column.enabled", false);
-#else
 pref("layout.float-fragments-inside-column.enabled", true);
-#endif
 
 // Is support for the Web Animations API enabled?
 #ifdef RELEASE_BUILD
