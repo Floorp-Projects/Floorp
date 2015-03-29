52c7e44c24bc64494d028f8070636c6e0237ae9e
Robert O'Callahan <robert@ocallahan.org>
2015-03-30 11:54:58 +1300

    Bug 1143575. Rename AdvanceFrame to UpdateRenderedVideoFrames. r=cpearce
    --HG--
    extra : commitid : K6557ZbFFzs
    extra : rebase_source : 3409058bbbb2ef281ea943d4b92f348a5e7b8e2b

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -2320,7 +2320,7 @@ nsresult MediaDecoderStateMachine::RunStateMachine()
       // Start playback if necessary so that the clock can be properly queried.
       MaybeStartPlayback();
 
-      AdvanceFrame();
+      UpdateRenderedVideoFrames();
       NS_ASSERTION(!IsPlaying() ||
                    mLogicallySeeking ||
                    IsStateMachineScheduled(),
@@ -2393,7 +2393,7 @@ nsresult MediaDecoderStateMachine::RunStateMachine()
       {
         // Start playback if necessary to play the remaining media.
         MaybeStartPlayback();
-        AdvanceFrame();
+        UpdateRenderedVideoFrames();
         NS_ASSERTION(!IsPlaying() ||
                      mLogicallySeeking ||
                      IsStateMachineScheduled(),
@@ -2590,7 +2590,7 @@ int64_t MediaDecoderStateMachine::GetClock() const
   return clock_time;
 }
 
-void MediaDecoderStateMachine::AdvanceFrame()
+void MediaDecoderStateMachine::UpdateRenderedVideoFrames()
 {
   MOZ_ASSERT(OnTaskQueue());
   AssertCurrentThreadInMonitor();
