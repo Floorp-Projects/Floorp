47b941867584d118c9e881df29d7015775b37ab0
Jon Coppeard <jcoppeard@mozilla.com>
2015-03-30 11:03:35 +0100

    Bug 1146696 - Don't assume there are no arenas available after last ditch GC. r=terrence

---
+++ js/src/jit-test/tests/gc/bug-1146696.js
@@ -0,0 +1,24 @@
+// |jit-test| --no-ggc
+gc();
+dbg1 = new Debugger();
+root2 = newGlobal();
+dbg1.memory.onGarbageCollection = function(){}
+dbg1.addDebuggee(root2);
+for (var j = 0; j < 9999; ++j) {
+    try {
+        a
+    } catch (e) {}
+}
+gcparam("maxBytes", gcparam("gcBytes") + 8000);
+try {
+    function g(i) {
+        if (i == 0)
+            return;
+        var x = "";
+        function f() {}
+        eval('');
+        g(i - 1);
+    }
+    g(100);
+} catch (e) {
+}
