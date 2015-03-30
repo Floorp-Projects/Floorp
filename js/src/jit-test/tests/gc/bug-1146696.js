a97e920550c0b7a7b747409ccf3eb54773c00868
Jon Coppeard <jcoppeard@mozilla.com>
2015-03-30 13:32:31 +0100

    Bug 1146696 - Fix test failures on windows cgc builds r=me

--- js/src/jit-test/tests/gc/bug-1146696.js
+++ js/src/jit-test/tests/gc/bug-1146696.js
@@ -10,12 +10,15 @@ for (var j = 0; j < 9999; ++j) {
     } catch (e) {}
 }
 gcparam("maxBytes", gcparam("gcBytes") + 8000);
-function g(i) {
-    if (i == 0)
-        return;
-    var x = "";
-    function f() {}
-    eval('');
-    g(i - 1);
+try {
+    function g(i) {
+        if (i == 0)
+            return;
+        var x = "";
+        function f() {}
+        eval('');
+        g(i - 1);
+    }
+    g(100);
+} catch (e) {
 }
-g(100);
