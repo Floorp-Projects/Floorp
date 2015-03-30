4e5ebec357ec24e13ffefb08ef3433164fa2584c
Boris Kudryavtsev <boriskk.work@gmail.com>
2015-03-30 16:57:00 -0400

    Bug 1139158 - Actions made availabe via marionette_driver object. r=jgriffin

--- testing/marionette/driver/marionette_driver/__init__.py
+++ testing/marionette/driver/marionette_driver/__init__.py
@@ -5,4 +5,5 @@ from marionette_driver.by import By
 from marionette_driver.date_time_value import DateTimeValue
 from marionette_driver.gestures import smooth_scroll, pinch
 from marionette_driver.wait import Wait
+from marionette_driver.marionette import Actions
 
