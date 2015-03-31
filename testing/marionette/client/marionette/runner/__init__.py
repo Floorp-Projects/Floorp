a36bf0c84415c35655cfd738cb1929b5a33f6058
Andreas Tolfsen <ato@mozilla.com>
2015-03-31 16:45:27 +0100

    Bug 941085: File uploads support in Marionette
    Adds support for W3C WebDriver compatible file uploads, where additional
    calls to sendKeys on <input type=file multiple> will append files,
    rather than reset the field.
    
    r=dburns

--- testing/marionette/client/marionette/runner/__init__.py
+++ testing/marionette/client/marionette/runner/__init__.py
@@ -5,7 +5,7 @@
 from base import (
         B2GTestResultMixin, BaseMarionetteOptions, BaseMarionetteTestRunner,
         Marionette, MarionetteTest, MarionetteTestResult, MarionetteTextTestRunner,
-        MozHttpd, OptionParser, TestManifest, TestResult, TestResultCollection
+        OptionParser, TestManifest, TestResult, TestResultCollection
         )
 from mixins import (
         B2GTestCaseMixin, B2GTestResultMixin, EnduranceOptionsMixin,
