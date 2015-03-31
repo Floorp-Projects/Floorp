43c98618d3520c69571c8651534d5e15c71b5b50
David Parks <davidp99@gmail.com>
2015-03-31 13:39:02 -0700

    Bug 1075670 - Make event.screen[XY] work in content processes (r=smaug,kats,tn,joshmoz)

--- xpfe/appshell/nsWebShellWindow.cpp
+++ xpfe/appshell/nsWebShellWindow.cpp
@@ -71,6 +71,8 @@
 #include "mozilla/DebugOnly.h"
 #include "mozilla/MouseEvents.h"
 
+#include "nsPIWindowRoot.h"
+
 #ifdef XP_MACOSX
 #include "nsINativeMenuService.h"
 #define USE_NATIVE_MENUS
@@ -255,6 +257,15 @@ nsWebShellWindow::WindowMoved(nsIWidget* aWidget, int32_t x, int32_t y)
     nsCOMPtr<nsPIDOMWindow> window =
       mDocShell ? mDocShell->GetWindow() : nullptr;
     pm->AdjustPopupsOnWindowChange(window);
+  }
+
+  // Notify all tabs that the widget moved.
+  if (mDocShell && mDocShell->GetWindow()) {
+    nsCOMPtr<EventTarget> eventTarget = mDocShell->GetWindow()->GetTopWindowRoot();
+    nsContentUtils::DispatchChromeEvent(mDocShell->GetDocument(),
+                                        eventTarget,
+                                        NS_LITERAL_STRING("MozUpdateWindowPos"),
+                                        false, false, nullptr);
   }
 
   // Persist position, but not immediately, in case this OS is firing
