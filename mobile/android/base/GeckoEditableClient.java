a12a40c1949061cdc5d97c337b5117f47b395bd7
Jim Chen <nchen@mozilla.com>
2015-03-31 18:20:27 -0400

    Bug 1149189 - Backout e75d824c730d "Force Gecko update on endBatchEdit for range updates"; r=esawin

--- mobile/android/base/GeckoEditableClient.java
+++ mobile/android/base/GeckoEditableClient.java
@@ -14,7 +14,7 @@ import android.text.Editable;
 interface GeckoEditableClient {
     void sendEvent(GeckoEvent event);
     Editable getEditable();
-    void setUpdateGecko(boolean update, boolean force);
+    void setUpdateGecko(boolean update);
     void setSuppressKeyUp(boolean suppress);
     Handler getInputConnectionHandler();
     boolean setInputConnectionHandler(Handler handler);
