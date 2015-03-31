76f492f630ae4d2f728b0df37f62b1f2f633816d
Eddy Bruël <ejpbruel@gmail.com>
2015-03-31 16:28:17 +0200

    Bug 1147972 - Test setting a breakpoint on a line in a gcd script;r=jlong

--- toolkit/devtools/server/tests/unit/test_setBreakpoint-on-line-in-gcd-script.js
+++ toolkit/devtools/server/tests/unit/test_setBreakpoint-on-line-in-gcd-script.js
@@ -1,39 +1,40 @@
 "use strict";
 
-let SOURCE_URL = getFileUrl("setBreakpoint-on-line.js");
+let SOURCE_URL = getFileUrl("setBreakpoint-on-line-in-gcd-script.js");
 
 function run_test() {
   return Task.spawn(function* () {
     do_test_pending();
 
+    let global = createTestGlobal("test");
+    loadSubScript(SOURCE_URL, global);
+    Cu.forceGC(); Cu.forceGC(); Cu.forceGC();
+
     DebuggerServer.registerModule("xpcshell-test/testactors");
     DebuggerServer.init(() => true);
-
-    let global = createTestGlobal("test");
     DebuggerServer.addTestGlobal(global);
-
     let client = new DebuggerClient(DebuggerServer.connectPipe());
     yield connect(client);
 
     let { tabs } = yield listTabs(client);
     let tab = findTab(tabs, "test");
     let [, tabClient] = yield attachTab(client, tab);
-
     let [, threadClient] = yield attachThread(tabClient);
     yield resume(threadClient);
 
-    let promise = waitForNewSource(client, SOURCE_URL);
-    loadSubScript(SOURCE_URL, global);
-    let { source } = yield promise;
+    let { sources } = yield getSources(threadClient);
+    let source = findSource(sources, SOURCE_URL);
     let sourceClient = threadClient.source(source);
 
-    let location = { line: 5 };
+    let location = { line: 7 };
     let [packet, breakpointClient] = yield setBreakpoint(sourceClient, location);
-    do_check_false(packet.isPending);
+    do_check_true(packet.isPending);
     do_check_false("actualLocation" in packet);
 
     packet = yield executeOnNextTickAndWaitForPause(function () {
-      Cu.evalInSandbox("f()", global);
+      reload(tabClient).then(function () {
+        loadSubScript(SOURCE_URL, global);
+      });
     }, client);
     do_check_eq(packet.type, "paused");
     let why = packet.why;
@@ -48,10 +49,9 @@ function run_test() {
     do_check_eq(variables.a.value, 1);
     do_check_eq(variables.b.value.type, "undefined");
     do_check_eq(variables.c.value.type, "undefined");
-
     yield resume(threadClient);
-    yield close(client);
 
+    yield close(client);
     do_test_finished();
   });
 }
