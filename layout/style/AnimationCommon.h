3bf51911976d5f61872c9155474eeb8e35597405
L. David Baron <dbaron@dbaron.org>
2015-03-31 15:05:54 -0700

    Bug 847287 patch 3 - Add assertions about consistency of the flags for animating on the compositor.  r=birtles
    These flags were added in patch 2.

--- layout/style/AnimationCommon.h
+++ layout/style/AnimationCommon.h
@@ -65,6 +65,10 @@ public:
   virtual size_t SizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf)
     const MOZ_MUST_OVERRIDE override;
 
+#ifdef DEBUG
+  static void Initialize();
+#endif
+
   /**
    * Notify the manager that the pres context is going away.
    */
