10ec797643eeec92994d99ca72a75db1abe7fd8d
Glenn Randers-Pehrson <glennrp+bmo@gmail.com>
2015-03-31 10:50:00 -0700

    Bug 1102048 (Part 14, NotificationObserver) - Make image/src files comply with the Mozilla Coding Style Guide. r=seth

--- image/src/ScriptedNotificationObserver.cpp
+++ image/src/ScriptedNotificationObserver.cpp
@@ -24,30 +24,37 @@ NS_IMPL_CYCLE_COLLECTING_RELEASE(ScriptedNotificationObserver)
 ScriptedNotificationObserver::ScriptedNotificationObserver(
     imgIScriptedNotificationObserver* aInner)
 : mInner(aInner)
-{
-}
+{ }
 
 NS_IMETHODIMP
 ScriptedNotificationObserver::Notify(imgIRequest* aRequest,
                                      int32_t aType,
                                      const nsIntRect* /*aUnused*/)
 {
-  if (aType == imgINotificationObserver::SIZE_AVAILABLE)
+  if (aType == imgINotificationObserver::SIZE_AVAILABLE) {
     return mInner->SizeAvailable(aRequest);
-  if (aType == imgINotificationObserver::FRAME_UPDATE)
+  }
+  if (aType == imgINotificationObserver::FRAME_UPDATE) {
     return mInner->FrameUpdate(aRequest);
-  if (aType == imgINotificationObserver::FRAME_COMPLETE)
+  }
+  if (aType == imgINotificationObserver::FRAME_COMPLETE) {
     return mInner->FrameComplete(aRequest);
-  if (aType == imgINotificationObserver::DECODE_COMPLETE)
+  }
+  if (aType == imgINotificationObserver::DECODE_COMPLETE) {
     return mInner->DecodeComplete(aRequest);
-  if (aType == imgINotificationObserver::LOAD_COMPLETE)
+  }
+  if (aType == imgINotificationObserver::LOAD_COMPLETE) {
     return mInner->LoadComplete(aRequest);
-  if (aType == imgINotificationObserver::DISCARD)
+  }
+  if (aType == imgINotificationObserver::DISCARD) {
     return mInner->Discard(aRequest);
-  if (aType == imgINotificationObserver::IS_ANIMATED)
+  }
+  if (aType == imgINotificationObserver::IS_ANIMATED) {
     return mInner->IsAnimated(aRequest);
-  if (aType == imgINotificationObserver::HAS_TRANSPARENCY)
+  }
+  if (aType == imgINotificationObserver::HAS_TRANSPARENCY) {
     return mInner->HasTransparency(aRequest);
+  }
   return NS_OK;
 }
 
