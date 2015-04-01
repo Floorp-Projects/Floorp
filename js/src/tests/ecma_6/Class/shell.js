9b3a7b578292b65f8a02eb0650f0a40418e463e1
Jeff Walden <jwalden@mit.edu>
2015-04-01 16:39:33 -0400

    Bug 1149797 - Use the loop's *static* block object when freshening a loop's block object, then copy in values from the old cloned block.  Using the old cloned block directly isn't valid when the cloned block might be extended with additional variables created by eval or added by nested function statements.  r=shu, a=lizzard

--- js/src/tests/ecma_6/Class/shell.js
+++ js/src/tests/ecma_6/Class/shell.js
@@ -1,4 +1,6 @@
-// Enable "let" in shell builds. So silly.
+// NOTE: This only turns on 1.8.5 in shell builds.  The browser requires the
+//       futzing in js/src/tests/browser.js (which only turns on 1.8, the most
+//       the browser supports).
 if (typeof version != 'undefined')
   version(185);
 
