a1d3eac011b3f63691d0140bd5e0e758ad3d85ef
Masayuki Nakano <masayuki@d-toybox.com>
2015-04-01 12:54:53 +0900

    Bug 1137539 part.3 Implement TSFStaticSink::IsFreeChangJieActive() r=emk

--- widget/windows/nsTextStore.cpp
+++ widget/windows/nsTextStore.cpp
@@ -781,6 +781,12 @@ public:
                             NS_LITERAL_STRING("ATOK "));
   }
 
+  bool IsFreeChangJieActive() const
+  {
+    // FYI: The TIP name is misspelled...
+    return mActiveTIPKeyboardDescription.EqualsLiteral("Free CangJie IME 10");
+  }
+
 public: // ITfActiveLanguageProfileNotifySink
   STDMETHODIMP OnActivated(REFCLSID clsid, REFGUID guidProfile,
                            BOOL fActivated);
@@ -1181,9 +1187,6 @@ bool nsTextStore::sDoNotReturnNoLayoutErrorToEasyChangjei = false;
 bool nsTextStore::sDoNotReturnNoLayoutErrorToGoogleJaInputAtFirstChar = false;
 bool nsTextStore::sDoNotReturnNoLayoutErrorToGoogleJaInputAtCaret = false;
 
-// NOTE: Free ChangJie 2010 missspells its name...
-#define TIP_NAME_FREE_CHANG_JIE_2010 \
-  (NS_LITERAL_STRING("Free CangJie IME 10"))
 #define TIP_NAME_EASY_CHANGJEI \
   (NS_LITERAL_STRING( \
      "\x4E2D\x6587 (\x7E41\x9AD4) - \x6613\x9821\x8F38\x5165\x6CD5"))
@@ -3231,8 +3234,7 @@ nsTextStore::GetTextExt(TsViewCookie vcView,
     // TSF.  We need to check if this is necessary on Windows 10 before
     // disabling this on Windows 10.
     else if ((sDoNotReturnNoLayoutErrorToFreeChangJie &&
-              activeTIPKeyboardDescription.Equals(
-                                             TIP_NAME_FREE_CHANG_JIE_2010)) ||
+              kSink->IsFreeChangJieActive()) ||
              (sDoNotReturnNoLayoutErrorToEasyChangjei &&
               activeTIPKeyboardDescription.Equals(TIP_NAME_EASY_CHANGJEI))) {
       acpEnd = mComposition.mStart;
