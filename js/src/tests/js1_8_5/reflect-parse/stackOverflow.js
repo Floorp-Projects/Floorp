831b749b587c6cd7e8592d4bd177b6d44d7f51af
L. David Baron <dbaron@dbaron.org>
2015-03-31 19:37:54 -0700

    Backed out changeset e942133b1f9b (bug 1149769) for jittest-2, jsreftest, and spidermonkey test failures, on a CLOSED TREE.
    --HG--
    rename : js/src/tests/js1_8_5/reflect-parse/destructuring__proto__.js => js/src/tests/js1_8_5/extensions/reflect-parse-destructuring-__proto__.js
    rename : js/src/tests/js1_8_5/reflect-parse/proxyArgs.js => js/src/tests/js1_8_5/extensions/reflect-parse-proxy.js

--- js/src/tests/js1_8_5/reflect-parse/stackOverflow.js
+++
@@ -1,7 +0,0 @@
-// Bug 632024: no crashing on stack overflow
-try {
-    Reflect.parse(Array(3000).join("x + y - ") + "z")
-} catch (e) { }
-
-if (typeof reportCompare === 'function')
-    reportCompare(true, true);
