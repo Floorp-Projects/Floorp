68371afb220f87276a7c95dc5291b28bd045dbf7
Marina Samuel <msamuel@mozilla.com>
2015-04-01 17:26:46 -0400

    Bug 1105360: Only enhance tiles that are under the 'enhanced' key. r=adw

--- browser/base/content/test/newtab/browser_newtab_enhanced.js
+++ browser/base/content/test/newtab/browser_newtab_enhanced.js
@@ -4,11 +4,24 @@
 const PRELOAD_PREF = "browser.newtab.preload";
 
 gDirectorySource = "data:application/json," + JSON.stringify({
-  "directory": [{
+  "enhanced": [{
     url: "http://example.com/",
     enhancedImageURI: "data:image/png;base64,helloWORLD",
     title: "title",
+    type: "organic",
+  }],
+  "directory": [{
+    url: "http://example1.com/",
+    enhancedImageURI: "data:image/png;base64,helloWORLD2",
+    title: "title1",
     type: "organic"
+  }],
+  "suggested": [{
+    url: "http://example1.com/2",
+    imageURI: "data:image/png;base64,helloWORLD3",
+    title: "title2",
+    type: "affiliate",
+    frecent_sites: ["test.com"]
   }]
 });
 
@@ -33,7 +46,7 @@ function runTests() {
     };
   }
 
-  // Make the page have a directory link followed by a history link
+  // Make the page have a directory link, enhanced link, and history link
   yield setLinks("-1");
 
   // Test with enhanced = false
@@ -52,19 +65,29 @@ function runTests() {
   ({type, enhanced, title} = getData(0));
   is(type, "organic", "directory link is organic");
   isnot(enhanced, "", "directory link has enhanced image");
+  is(title, "title1");
+
+  ({type, enhanced, title} = getData(1));
+  is(type, "enhanced", "history link is enhanced");
+  isnot(enhanced, "", "history link has enhanced image");
   is(title, "title");
 
-  is(getData(1), null, "history link pushed out by directory link");
+  is(getData(2), null, "there are only 2 links, directory and enhanced history");
 
   // Test with a pinned link
   setPinnedLinks("-1");
   yield addNewTabPageTab();
   ({type, enhanced, title} = getData(0));
-  is(type, "history", "pinned history link is not enhanced");
-  is(enhanced, "", "pinned history link doesn't have enhanced image");
-  is(title, "site#-1");
+  is(type, "enhanced", "pinned history link is enhanced");
+  isnot(enhanced, "", "pinned history link has enhanced image");
+  is(title, "title");
 
-  is(getData(1), null, "directory link pushed out by pinned history link");
+  ({type, enhanced, title} = getData(1));
+  is(type, "organic", "directory link is organic");
+  isnot(enhanced, "", "directory link has enhanced image");
+  is(title, "title1");
+
+  is(getData(2), null, "directory link pushed out by pinned history link");
 
   // Test pinned link with enhanced = false
   yield addNewTabPageTab();
@@ -78,4 +101,42 @@ function runTests() {
 
   ok(getContentDocument().getElementById("newtab-intro-what"),
      "'What is this page?' link exists");
+
+  yield unpinCell(0);
+
+
+
+  // Test that a suggested tile is not enhanced by a directory tile
+  let origIsTopPlacesSite = NewTabUtils.isTopPlacesSite;
+  NewTabUtils.isTopPlacesSite = () => true;
+  yield setLinks("-1");
+
+  // Test with enhanced = false
+  yield addNewTabPageTab();
+  yield customizeNewTabPage("classic");
+  ({type, enhanced, title} = getData(0));
+  isnot(type, "enhanced", "history link is not enhanced");
+  is(enhanced, "", "history link has no enhanced image");
+  is(title, "site#-1");
+
+  is(getData(1), null, "there is only one link and it's a history link");
+
+
+  // Test with enhanced = true
+  yield addNewTabPageTab();
+  yield customizeNewTabPage("enhanced");
+
+  // Suggested link was not enhanced by directory link with same domain
+  ({type, enhanced, title} = getData(0));
+  is(type, "affiliate", "suggested link is affiliate");
+  is(enhanced, "", "suggested link has no enhanced image");
+  is(title, "title2");
+
+  // Enhanced history link shows up second
+  ({type, enhanced, title} = getData(1));
+  is(type, "enhanced", "pinned history link is enhanced");
+  isnot(enhanced, "", "pinned history link has enhanced image");
+  is(title, "title");
+
+  is(getData(2), null, "there is only a suggested link followed by an enhanced history link");
 }
