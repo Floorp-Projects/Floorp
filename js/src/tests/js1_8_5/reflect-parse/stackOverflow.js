92c643541e91443482763a9fd280fd054370dc71
Eric Faust <efaustbmo@gmail.com>
2015-03-31 18:29:10 -0700

    Bug 1149769 - Split reflect-parse.js reftest into its own subdirectory. (r=Waldo)

---
+++ js/src/tests/js1_8_5/reflect-parse/stackOverflow.js
@@ -0,0 +1,7 @@
+// Bug 632024: no crashing on stack overflow
+try {
+    Reflect.parse(Array(3000).join("x + y - ") + "z")
+} catch (e) { }
+
+if (typeof reportCompare === 'function')
+    reportCompare(true, true);
