d18cbaeb9ec33ca09fc7ad70ae345c4cc4b1cd4e
Wes Kocher <wkocher@mozilla.com>
2015-04-02 13:42:30 -0700

    Backed out changeset b84eb39d1b52 (bug 1101651)

--- dom/media/VideoSegment.cpp
+++ dom/media/VideoSegment.cpp
@@ -39,7 +39,6 @@ VideoFrame::TakeFrom(VideoFrame* aFrame)
   mForceBlack = aFrame->GetForceBlack();
 }
 
-#if !defined(MOZILLA_XPCOMRT_API)
 /* static */ already_AddRefed<Image>
 VideoFrame::CreateBlackImage(const gfxIntSize& aSize)
 {
@@ -84,7 +83,6 @@ VideoFrame::CreateBlackImage(const gfxIntSize& aSize)
 
   return image.forget();
 }
-#endif // !defined(MOZILLA_XPCOMRT_API)
 
 VideoChunk::VideoChunk()
 {}
