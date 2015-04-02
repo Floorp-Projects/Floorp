338db66b5a5ba7817760c5f17c7767135ace4af0
Eitan Isaacson <eitan@monotonous.org>
2015-04-02 09:37:24 -0700

    Bug 1086545 - Bind speech task streams to a parent stream held by voice registry. r=padenot

--- dom/media/webspeech/synth/nsSynthVoiceRegistry.h
+++ dom/media/webspeech/synth/nsSynthVoiceRegistry.h
@@ -11,6 +11,7 @@
 #include "nsISynthVoiceRegistry.h"
 #include "nsRefPtrHashtable.h"
 #include "nsTArray.h"
+#include "MediaStreamGraph.h"
 
 class nsISpeechService;
 
@@ -73,6 +74,8 @@ private:
   nsRefPtrHashtable<nsStringHashKey, VoiceData> mUriVoiceMap;
 
   SpeechSynthesisChild* mSpeechSynthChild;
+
+  nsRefPtr<ProcessedMediaStream> mStream;
 };
 
 } // namespace dom
