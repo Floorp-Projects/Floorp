622170b4e67f44f5d079690e66fcf880522ff96c
Andreas Tolfsen <ato@mozilla.com>
2015-04-02 15:16:00 +0100

    Bug 941085: File uploads support in Marionette
    Adds support for W3C WebDriver compatible file uploads, where additional
    calls to sendKeys on <input type=file multiple> will append files,
    rather than reset the field.
    
    r=dburns
    
    --HG--
    extra : amend_source : 093abcf640631778cda25cf800077dae5fa9b5f4

--- testing/marionette/error.js
+++ testing/marionette/error.js
@@ -10,6 +10,7 @@ const errors = [
   "ElementNotVisibleError",
   "FrameSendFailureError",
   "FrameSendNotInitializedError",
+  "IllegalArgumentError",
   "InvalidElementStateError",
   "JavaScriptError",
   "NoAlertOpenError",
@@ -158,6 +159,14 @@ this.FrameSendNotInitializedError = function(frame) {
 };
 FrameSendNotInitializedError.prototype = Object.create(WebDriverError.prototype);
 
+this.IllegalArgumentError = function(msg) {
+  WebDriverError.call(this, msg);
+  this.name = "IllegalArgumentError";
+  this.status = "illegal argument";
+  this.code = 13;  // unknown error
+};
+IllegalArgumentError.prototype = Object.create(WebDriverError.prototype);
+
 this.InvalidElementStateError = function(msg) {
   WebDriverError.call(this, msg);
   this.name = "InvalidElementStateError";
@@ -299,6 +308,7 @@ const errorObjs = [
   this.ElementNotVisibleError,
   this.FrameSendFailureError,
   this.FrameSendNotInitializedError,
+  this.IllegalArgumentError,
   this.InvalidElementStateError,
   this.JavaScriptError,
   this.NoAlertOpenError,
