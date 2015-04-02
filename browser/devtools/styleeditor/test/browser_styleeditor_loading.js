28804881eacd4b42ecad9a9077c6e7b0e7e37f56
Sami Jaktholm <sjakthol@outlook.com>
2015-04-02 21:14:07 +0300

    Bug 1148893 - Clean up browser_styleeditor_loading.js. r=bgrins

--- browser/devtools/styleeditor/test/browser_styleeditor_loading.js
+++ browser/devtools/styleeditor/test/browser_styleeditor_loading.js
@@ -1,37 +1,36 @@
 /* vim: set ts=2 et sw=2 tw=80: */
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+// Test that style editor loads correctly.
 
 const TESTCASE_URI = TEST_BASE_HTTP + "longload.html";
 
-
-function test()
-{
-  waitForExplicitFinish();
-
+add_task(function* () {
   // launch Style Editor right when the tab is created (before load)
   // this checks that the Style Editor still launches correctly when it is opened
   // *while* the page is still loading. The Style Editor should not signal that
   // it is loaded until the accompanying content page is loaded.
+  let tabAdded = addTab(TESTCASE_URI);
+  let target = TargetFactory.forTab(gBrowser.selectedTab);
+  let styleEditorLoaded = gDevTools.showToolbox(target, "styleeditor");
 
-  addTabAndCheckOnStyleEditorAdded(function(panel) {
-    content.location = TESTCASE_URI;
-  }, testEditorAdded);
-}
+  yield Promise.all([tabAdded, styleEditorLoaded]);
 
-function testEditorAdded(event, editor)
-{
-  let root = gPanelWindow.document.querySelector(".splitview-root");
+  let toolbox = gDevTools.getToolbox(target);
+  let panel = toolbox.getPanel("styleeditor");
+  let { panelWindow } = panel;
+
+  let root = panelWindow.document.querySelector(".splitview-root");
   ok(!root.classList.contains("loading"),
      "style editor root element does not have 'loading' class name anymore");
 
-  let button = gPanelWindow.document.querySelector(".style-editor-newButton");
+  let button = panelWindow.document.querySelector(".style-editor-newButton");
   ok(!button.hasAttribute("disabled"),
      "new style sheet button is enabled");
 
-  button = gPanelWindow.document.querySelector(".style-editor-importButton");
+  button = panelWindow.document.querySelector(".style-editor-importButton");
   ok(!button.hasAttribute("disabled"),
      "import button is enabled");
-
-  finish();
-}
+});
