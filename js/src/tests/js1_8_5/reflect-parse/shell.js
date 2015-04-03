9b160dfa9fc1288d1644c2cf113a282e2b79120e
Jeff Walden <jwalden@mit.edu>
2015-04-02 22:55:40 -0400

    Bug 1150779 - Split the rest of destructuring.js into several smaller destructuring tests.  r=efaust

--- js/src/tests/js1_8_5/reflect-parse/shell.js
+++ js/src/tests/js1_8_5/reflect-parse/shell.js
@@ -1,5 +1,59 @@
 loadRelativeToScript('PatternAsserts.js');
 
+// various combinations of identifiers and destructuring patterns:
+function makePatternCombinations(id, destr)
+{
+    return [
+      [ id(1)                                            ],
+      [ id(1),    id(2)                                  ],
+      [ id(1),    id(2),    id(3)                        ],
+      [ id(1),    id(2),    id(3),    id(4)              ],
+      [ id(1),    id(2),    id(3),    id(4),    id(5)    ],
+
+      [ destr(1)                                         ],
+      [ destr(1), destr(2)                               ],
+      [ destr(1), destr(2), destr(3)                     ],
+      [ destr(1), destr(2), destr(3), destr(4)           ],
+      [ destr(1), destr(2), destr(3), destr(4), destr(5) ],
+
+      [ destr(1), id(2)                                  ],
+
+      [ destr(1), id(2),    id(3)                        ],
+      [ destr(1), id(2),    id(3),    id(4)              ],
+      [ destr(1), id(2),    id(3),    id(4),    id(5)    ],
+      [ destr(1), id(2),    id(3),    id(4),    destr(5) ],
+      [ destr(1), id(2),    id(3),    destr(4)           ],
+      [ destr(1), id(2),    id(3),    destr(4), id(5)    ],
+      [ destr(1), id(2),    id(3),    destr(4), destr(5) ],
+
+      [ destr(1), id(2),    destr(3)                     ],
+      [ destr(1), id(2),    destr(3), id(4)              ],
+      [ destr(1), id(2),    destr(3), id(4),    id(5)    ],
+      [ destr(1), id(2),    destr(3), id(4),    destr(5) ],
+      [ destr(1), id(2),    destr(3), destr(4)           ],
+      [ destr(1), id(2),    destr(3), destr(4), id(5)    ],
+      [ destr(1), id(2),    destr(3), destr(4), destr(5) ],
+
+      [ id(1),    destr(2)                               ],
+
+      [ id(1),    destr(2), id(3)                        ],
+      [ id(1),    destr(2), id(3),    id(4)              ],
+      [ id(1),    destr(2), id(3),    id(4),    id(5)    ],
+      [ id(1),    destr(2), id(3),    id(4),    destr(5) ],
+      [ id(1),    destr(2), id(3),    destr(4)           ],
+      [ id(1),    destr(2), id(3),    destr(4), id(5)    ],
+      [ id(1),    destr(2), id(3),    destr(4), destr(5) ],
+
+      [ id(1),    destr(2), destr(3)                     ],
+      [ id(1),    destr(2), destr(3), id(4)              ],
+      [ id(1),    destr(2), destr(3), id(4),    id(5)    ],
+      [ id(1),    destr(2), destr(3), id(4),    destr(5) ],
+      [ id(1),    destr(2), destr(3), destr(4)           ],
+      [ id(1),    destr(2), destr(3), destr(4), id(5)    ],
+      [ id(1),    destr(2), destr(3), destr(4), destr(5) ]
+    ];
+}
+
 function runtest(main) {
     try {
         main();
