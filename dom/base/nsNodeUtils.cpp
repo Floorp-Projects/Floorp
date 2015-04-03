0f1773140e2e84c99016597b781f9ab70a12edab
Kartikaya Gupta <kgupta@mozilla.com>
2015-04-02 20:45:50 -0400

    Bug 1146843 - Revert part of cset 33c30e283fa8 because the code is used in Fennec. r=snorp, a=lizzard

--- dom/base/nsNodeUtils.cpp
+++ dom/base/nsNodeUtils.cpp
@@ -519,6 +519,9 @@ nsNodeUtils::CloneAndAdopt(nsINode *aNode, bool aClone, bool aDeep,
           if (elm->MayHavePaintEventListener()) {
             window->SetHasPaintEventListeners();
           }
+          if (elm->MayHaveTouchEventListener()) {
+            window->SetHasTouchEventListeners();
+          }
           if (elm->MayHaveMouseEnterLeaveEventListener()) {
             window->SetHasMouseEnterLeaveEventListeners();
           }
