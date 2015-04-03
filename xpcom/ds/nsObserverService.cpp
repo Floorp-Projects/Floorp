41bc62d3d4d5ae3c33ff59ebda3fb4257932624e
Randall Barker <rbarker@mozilla.com>
2015-04-03 15:52:00 +0200

    Bug 1093934 - Create a XPCOM library that can be used to support standalone WebRTC. r=nfroyd

--- xpcom/ds/nsObserverService.cpp
+++ xpcom/ds/nsObserverService.cpp
@@ -205,13 +205,17 @@ nsObserverService::~nsObserverService(void)
 void
 nsObserverService::RegisterReporter()
 {
+#if !defined(MOZILLA_XPCOMRT_API)
   RegisterWeakMemoryReporter(this);
+#endif // !defined(MOZILLA_XPCOMRT_API)
 }
 
 void
 nsObserverService::Shutdown()
 {
+#if !defined(MOZILLA_XPCOMRT_API)
   UnregisterWeakMemoryReporter(this);
+#endif // !defined(MOZILLA_XPCOMRT_API)
 
   mShuttingDown = true;
 
