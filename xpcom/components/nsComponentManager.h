033801d728d08ce423591be0f3833d62bb6c7f26
Randall Barker <rbarker@mozilla.com>
2015-04-03 15:54:00 +0200

    Bug 1097804 - Part 1 - Create a library containing nsISocketTransportService and nsIDNS that can be used to support standalone WebRTC. r=mcmanus

--- xpcom/components/nsComponentManager.h
+++ xpcom/components/nsComponentManager.h
@@ -69,6 +69,7 @@ extern const char staticComponentType[];
 
 #if defined(MOZILLA_XPCOMRT_API)
 extern const mozilla::Module kXPCOMRTModule;
+extern const mozilla::Module kNeckoStandaloneModule;
 #else
 extern const mozilla::Module kXPCOMModule;
 #endif
