f90c12880d5f5239fead74fcbe6f680406125917
Mats Palmgren <mats@mozilla.com>
2015-04-04 08:50:00 -0400

    Bug 1151220 - [css-grid] Fix a couple of typos in InitializeTrackSize. r=dholbert

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -937,7 +937,7 @@ InitializeTrackSize(nscoord aPercentageBasis,
 {
   // http://dev.w3.org/csswg/css-grid/#algo-init
   nscoord& base = aTrackSize->mBase;
-  switch (aMaxCoord.GetUnit()) {
+  switch (aMinCoord.GetUnit()) {
     case eStyleUnit_Enumerated:
     case eStyleUnit_FlexFraction:
       base = 0;
@@ -975,7 +975,7 @@ InitializeTrackSizes(nscoord aPercentageBasis,
   size_t i = 0;
   for (; i < len; ++i) {
     InitializeTrackSize(aPercentageBasis,
-                        aMinSizingFunctions[i], aMinSizingFunctions[i],
+                        aMinSizingFunctions[i], aMaxSizingFunctions[i],
                         &aResults[i]);
   }
   for (; i < aResults.Length(); ++i) {
