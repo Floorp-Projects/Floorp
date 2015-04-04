a2317fa684b3edb9a5fe335eb031b85687c86ccb
Sami Jaktholm <sjakthol@outlook.com>
2015-04-04 10:02:55 +0300

    Bug 1148893 - Part 13 - Clean up browser_styleeditor_import_rule.js. r=bgrinstead

--- browser/devtools/styleeditor/test/browser_styleeditor_import_rule.js
+++ browser/devtools/styleeditor/test/browser_styleeditor_import_rule.js
@@ -1,37 +1,25 @@
 /* vim: set ts=2 et sw=2 tw=80: */
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+// Test that style editor shows sheets loaded with @import rules.
 
 // http rather than chrome to improve coverage
 const TESTCASE_URI = TEST_BASE_HTTP + "import.html";
 
-let gUI;
+add_task(function* () {
+  let { ui } = yield openStyleEditorForURL(TESTCASE_URI);
 
-function test()
-{
-  waitForExplicitFinish();
-
-  addTabAndOpenStyleEditors(3, onEditorAdded);
-
-  content.location = TESTCASE_URI;
-}
-
-function onEditorAdded(panel)
-{
-  gUI = panel.UI;
-
-  is(gUI.editors.length, 3,
+  is(ui.editors.length, 3,
     "there are 3 stylesheets after loading @imports");
 
-  is(gUI.editors[0].styleSheet.href, TEST_BASE_HTTP + "simple.css",
+  is(ui.editors[0].styleSheet.href, TEST_BASE_HTTP + "simple.css",
     "stylesheet 1 is simple.css");
 
-  is(gUI.editors[1].styleSheet.href, TEST_BASE_HTTP + "import.css",
+  is(ui.editors[1].styleSheet.href, TEST_BASE_HTTP + "import.css",
     "stylesheet 2 is import.css");
 
-  is(gUI.editors[2].styleSheet.href, TEST_BASE_HTTP + "import2.css",
+  is(ui.editors[2].styleSheet.href, TEST_BASE_HTTP + "import2.css",
     "stylesheet 3 is import2.css");
-
-  gUI = null;
-  finish();
-}
+});
