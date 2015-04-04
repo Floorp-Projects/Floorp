68289c25da199f458827db2346c33f77239af779
Nikhil Marathe <nsm.nikhil@gmail.com>
2015-04-03 22:55:15 -0700

    Bug 1109751 - Consume FormData support in Fetch API. r=baku, a=lizzard

--- dom/fetch/Fetch.h
+++ dom/fetch/Fetch.h
@@ -114,6 +114,12 @@ public:
   }
 
   already_AddRefed<Promise>
+  FormData(ErrorResult& aRv)
+  {
+    return ConsumeBody(CONSUME_FORMDATA, aRv);
+  }
+
+  already_AddRefed<Promise>
   Json(ErrorResult& aRv)
   {
     return ConsumeBody(CONSUME_JSON, aRv);
@@ -160,7 +166,7 @@ private:
   {
     CONSUME_ARRAYBUFFER,
     CONSUME_BLOB,
-    // FormData not supported right now,
+    CONSUME_FORMDATA,
     CONSUME_JSON,
     CONSUME_TEXT,
   };
