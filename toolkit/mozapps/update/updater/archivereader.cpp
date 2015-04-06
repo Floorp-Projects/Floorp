7eaa9542734f450ed800eadd3d4bb4ba708661cc
Brian R. Bondy <netzen@gmail.com>
2015-04-06 12:03:19 -0400

    Bug 973933 - Temporarily disable Linux for MAR verification. r=rstrong

--- toolkit/mozapps/update/updater/archivereader.cpp
+++ toolkit/mozapps/update/updater/archivereader.cpp
@@ -82,7 +82,9 @@ ArchiveReader::VerifySignature()
     return ARCHIVE_NOT_OPEN;
   }
 
-#ifdef MOZ_VERIFY_MAR_SIGNATURE
+#ifndef MOZ_VERIFY_MAR_SIGNATURE
+  return OK;
+#else
 #ifdef UPDATER_XPCSHELL_CERT
   int rv = VerifyLoadedCert(mArchive, xpcshellCertData);
 #else
@@ -91,9 +93,8 @@ ArchiveReader::VerifySignature()
     rv = VerifyLoadedCert(mArchive, secondaryCertData);
   }
 #endif
-#endif
-
   return rv;
+#endif
 }
 
 /**
