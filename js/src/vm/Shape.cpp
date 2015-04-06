3be67294224162e31d938801161d11b32bc83dd1
Brian Hackett <bhackett1024@gmail.com>
2015-04-06 12:54:53 -0600

    Bug 1148922 - Handle insertion of new initial shapes via the metadata callback, r=luke.

--- js/src/vm/Shape.cpp
+++ js/src/vm/Shape.cpp
@@ -1503,6 +1503,10 @@ EmptyShape::insertInitialShape(ExclusiveContext* cx, HandleShape shape, HandleOb
 
     InitialShapeEntry& entry = const_cast<InitialShapeEntry&>(*p);
 
+    // The metadata callback can end up causing redundant changes of the initial shape.
+    if (entry.shape == shape)
+        return;
+
     /* The new shape had better be rooted at the old one. */
 #ifdef DEBUG
     Shape* nshape = shape;
