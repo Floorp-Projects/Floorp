72b8ab88fbc6e967eef7e29575a1bd0fa73e85db
Vladimir Vukicevic <vladimir@pobox.com>
2015-04-07 12:39:09 -0400

    Bug 1151905; remove unnecessary gfxVR.h includes; r=ehsan

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -226,6 +226,7 @@
 #include "nsLocation.h"
 #include "mozilla/dom/FontFaceSet.h"
 #include "mozilla/dom/BoxObject.h"
+#include "gfxVR.h"
 
 #ifdef MOZ_MEDIA_NAVIGATOR
 #include "mozilla/MediaManager.h"
@@ -11373,6 +11374,10 @@ bool
 nsDocument::IsFullScreenDoc()
 {
   return GetFullScreenElement() != nullptr;
+}
+
+FullScreenOptions::FullScreenOptions()
+{
 }
 
 class nsCallRequestFullScreen : public nsRunnable
