2350ad35919e777ff1015c74b3cdd4f257e9dcc3
Bobby Holley <bobbyholley@gmail.com>
2015-04-07 16:10:51 -0700

    Bug 1148571 - Dispatch AudioSink notifications asynchronously. r=jww

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -1539,7 +1539,7 @@ void MediaDecoder::SetLoadInBackground(bool aLoadInBackground)
 
 void MediaDecoder::UpdatePlaybackOffset(int64_t aOffset)
 {
-  ReentrantMonitorAutoEnter mon(GetReentrantMonitor());
+  GetReentrantMonitor().AssertCurrentThreadIn();
   mPlaybackPosition = aOffset;
 }
 
