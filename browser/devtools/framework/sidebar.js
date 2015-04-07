48e2e2ec81bc0014c725fcf680d4643fb9ad89ab
Brian Grinstead <bgrinstead@mozilla.com>
2015-04-07 14:27:00 +0200

    Bug 1151259 - Switch <toolbar> to <box> to get rid of -moz-appearance styles for devtools sidebar. r=jryans, a=sledru

--- browser/devtools/framework/sidebar.js
+++ browser/devtools/framework/sidebar.js
@@ -119,12 +119,12 @@ ToolSidebar.prototype = {
 
     let tabs = this._tabbox.tabs;
 
-    // Create a toolbar and insert it first in the tabbox
-    let allTabsToolbar = this._panelDoc.createElementNS(XULNS, "toolbar");
-    this._tabbox.insertBefore(allTabsToolbar, tabs);
+    // Create a container and insert it first in the tabbox
+    let allTabsContainer = this._panelDoc.createElementNS(XULNS, "box");
+    this._tabbox.insertBefore(allTabsContainer, tabs);
 
     // Move the tabs inside and make them flex
-    allTabsToolbar.appendChild(tabs);
+    allTabsContainer.appendChild(tabs);
     tabs.setAttribute("flex", "1");
 
     // Create the dropdown menu next to the tabs
@@ -134,7 +134,7 @@ ToolSidebar.prototype = {
     this._allTabsBtn.setAttribute("label", l10n("sidebar.showAllTabs.label"));
     this._allTabsBtn.setAttribute("tooltiptext", l10n("sidebar.showAllTabs.tooltip"));
     this._allTabsBtn.setAttribute("hidden", "true");
-    allTabsToolbar.appendChild(this._allTabsBtn);
+    allTabsContainer.appendChild(this._allTabsBtn);
 
     let menuPopup = this._panelDoc.createElementNS(XULNS, "menupopup");
     this._allTabsBtn.appendChild(menuPopup);
@@ -162,7 +162,7 @@ ToolSidebar.prototype = {
 
     // Moving back the tabs as a first child of the tabbox
     this._tabbox.insertBefore(tabs, this._tabbox.tabpanels);
-    this._tabbox.querySelector("toolbar").remove();
+    this._tabbox.querySelector("box").remove();
 
     this._allTabsBtn = null;
   },
