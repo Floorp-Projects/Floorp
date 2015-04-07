bbe1b48a59cd1ea4e24afa2b5583c7f13e2677c7
Jordan Santell <jsantell@gmail.com>
2015-04-08 00:16:00 +0200

    Bug 1147945 - Let the profiler's buffer size and sample rate be configurable via prefs. r=vp

--- browser/devtools/performance/modules/recording-model.js
+++ browser/devtools/performance/modules/recording-model.js
@@ -26,7 +26,9 @@ const RecordingModel = function (options={}) {
     withMemory: options.withMemory || false,
     withAllocations: options.withAllocations || false,
     allocationsSampleProbability: options.allocationsSampleProbability || 0,
-    allocationsMaxLogLength: options.allocationsMaxLogLength || 0
+    allocationsMaxLogLength: options.allocationsMaxLogLength || 0,
+    bufferSize: options.bufferSize || 0,
+    sampleFrequency: options.sampleFrequency || 1
   };
 };
 
