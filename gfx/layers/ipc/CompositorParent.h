842481bc9cc59969ec8cd0925f585bc732f442f9
Randall Barker <rbarker@mozilla.com>
2015-04-08 16:00:00 -0400

    Bug 1148149 - Support Android Presentation API. r=snorp, r=jgilbert

--- gfx/layers/ipc/CompositorParent.h
+++ gfx/layers/ipc/CompositorParent.h
@@ -223,6 +223,7 @@ public:
    * Returns true if a surface was obtained and the resume succeeded; false
    * otherwise.
    */
+  bool ScheduleResumeOnCompositorThread();
   bool ScheduleResumeOnCompositorThread(int width, int height);
 
   virtual void ScheduleComposition();
