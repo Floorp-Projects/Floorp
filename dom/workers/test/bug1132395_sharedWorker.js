0e9e04665a5a9958e483e9e9fe32f08bdf3cfa57
Andrea Marchesini <amarchesini@mozilla.com>
2015-04-08 17:59:08 +0100

    Bug 1134224 - test_bug1132395.html must wait until the port is actually available before sending messages. r=ehsan, a=test-only

--- dom/workers/test/bug1132395_sharedWorker.js
+++ dom/workers/test/bug1132395_sharedWorker.js
@@ -1,5 +1,7 @@
 onconnect = function(evt) {
-  var blob = new Blob(['123'], { type: 'text/plain' });
-  var url = URL.createObjectURL(blob);
-  evt.ports[0].postMessage('alive \\o/');
+  evt.ports[0].onmessage = function(e) {
+    var blob = new Blob(['123'], { type: 'text/plain' });
+    var url = URL.createObjectURL(blob);
+    evt.ports[0].postMessage('alive \\o/');
+  };
 }
