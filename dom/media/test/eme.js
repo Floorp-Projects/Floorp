0afa6a730c6fc94ab9b9daeb6b7011010c016462
Gerald Squelart <from_mozilla@squelart.com>
2015-04-09 14:38:20 +1000

    Bug 1152151 - [EME] Some tests keep working after they should have finished - r=edwin

--- dom/media/test/eme.js
+++ dom/media/test/eme.js
@@ -307,6 +307,22 @@ function LoadTest(test, elem, token)
   });
 }
 
+// Same as LoadTest, but manage a token+"_load" start&finished.
+// Also finish main token if loading fails.
+function LoadTestWithManagedLoadToken(test, elem, manager, token)
+{
+  manager.started(token + "_load");
+  return LoadTest(test, elem, token)
+  .catch(function (reason) {
+    ok(false, TimeStamp(token) + " - Error during load: " + reason);
+    manager.finished(token + "_load");
+    manager.finished(token);
+  })
+  .then(function () {
+    manager.finished(token + "_load");
+  });
+}
+
 function SetupEME(test, token, params)
 {
   var v = document.createElement("video");
