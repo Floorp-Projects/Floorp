e29a2f9c9a2745ce013044e44b71fcc16aafd3e4
Cervantes Yu <cyu@mozilla.com>
2015-04-09 15:26:50 +0800

    Bug 1142229, part 2 - Shut down the Nuwa process when its PContentParent::Close() is called. r=tlee

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -1855,6 +1855,14 @@ ContentChild::ActorDestroy(ActorDestroyReason why)
     }
     mIsAlive = false;
 
+#ifdef MOZ_NUWA_PROCESS
+    if (IsNuwaProcess()) {
+        // The Nuwa cannot go through the full XPCOM shutdown path or deadlock
+        // will result.
+        QuickExit();
+    }
+#endif
+
     XRE_ShutdownChildProcess();
 }
 
