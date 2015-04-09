d1b225c00c87f85e5958e438301f32a1537d341c
Eric Faust <efaustbmo@mozilla.com>
2015-04-08 22:40:51 -0700

    Bug 1141862 - Followup: Remove erronously landed debug spew. r=me

--- js/src/jsfun.cpp
+++ js/src/jsfun.cpp
@@ -721,8 +721,6 @@ inline void
 JSFunction::trace(JSTracer* trc)
 {
     if (isExtended()) {
-        if (isMethod())
-            fprintf(stderr, "It's a method! %p\n", this);
         TraceRange(trc, ArrayLength(toExtended()->extendedSlots),
                    (HeapValue*)toExtended()->extendedSlots, "nativeReserved");
     }
