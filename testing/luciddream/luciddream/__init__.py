f18d9b5e8ea6a036e6767e7e1a9fd408ac55b783
Alexandre Poirot <poirot.alex@gmail.com>
2015-04-09 11:46:00 -0400

    Bug 1147031 - Write mach command for luciddream. r=jgriffin, a=test-only

--- testing/luciddream/luciddream/__init__.py
+++ testing/luciddream/luciddream/__init__.py
@@ -10,8 +10,9 @@ from marionette.marionette_test import MarionetteTestCase, MarionetteJSTestCase
 from marionette_driver.errors import ScriptTimeoutException
 
 class LucidDreamTestCase(MarionetteTestCase):
-    def __init__(self, marionette_weakref, browser=None, **kwargs):
+    def __init__(self, marionette_weakref, browser=None, logger=None, **kwargs):
         self.browser = browser
+        self.logger = logger
         MarionetteTestCase.__init__(self, marionette_weakref, **kwargs)
 
     def run_js_test(self, filename, marionette):
