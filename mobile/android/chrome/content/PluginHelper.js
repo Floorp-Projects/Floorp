e76a66f73343bdc9c6d3a3d87189e190a224b489
Chenxia Liu <liuche@mozilla.com>
2015-04-09 16:04:21 -0700

    Bug 1147064 - Make negative/positive button order consistent. r=margaret
    --HG--
    extra : rebase_source : 091eab2370418bcfe6ec887ad7532ec5734349c9

--- mobile/android/chrome/content/PluginHelper.js
+++ mobile/android/chrome/content/PluginHelper.js
@@ -28,16 +28,6 @@ var PluginHelper = {
                                                        [uri.host], 1);
     let buttons = [
       {
-        label: Strings.browser.GetStringFromName("clickToPlayPlugins.activate"),
-        callback: function(aChecked) {
-          // If the user checked "Don't ask again", make a permanent exception
-          if (aChecked)
-            Services.perms.add(uri, "plugins", Ci.nsIPermissionManager.ALLOW_ACTION);
-
-          PluginHelper.playAllPlugins(aTab.browser.contentWindow);
-        }
-      },
-      {
         label: Strings.browser.GetStringFromName("clickToPlayPlugins.dontActivate"),
         callback: function(aChecked) {
           // If the user checked "Don't ask again", make a permanent exception
@@ -45,6 +35,16 @@ var PluginHelper = {
             Services.perms.add(uri, "plugins", Ci.nsIPermissionManager.DENY_ACTION);
 
           // Other than that, do nothing
+        }
+      },
+      {
+        label: Strings.browser.GetStringFromName("clickToPlayPlugins.activate"),
+        callback: function(aChecked) {
+          // If the user checked "Don't ask again", make a permanent exception
+          if (aChecked)
+            Services.perms.add(uri, "plugins", Ci.nsIPermissionManager.ALLOW_ACTION);
+
+          PluginHelper.playAllPlugins(aTab.browser.contentWindow);
         }
       }
     ];
