41bb0a61ba9aa86c3895f34a1972b63b9d135f6c
Valentin Gosu <valentin.gosu@gmail.com>
2015-04-09 06:33:03 +0300

    Bug 1134596 - Add network.offline-mirrors-connectivity pref to preserve previous behaviour r=honzab

--- netwerk/base/nsIOService.h
+++ netwerk/base/nsIOService.h
@@ -127,6 +127,9 @@ private:
     bool                                 mOfflineForProfileChange;
     bool                                 mManageLinkStatus;
     bool                                 mConnectivity;
+    // If true, the connectivity state will be mirrored by IOService.offline
+    // meaning if !mConnectivity, GetOffline() will return true
+    bool                                 mOfflineMirrorsConnectivity;
 
     // Used to handle SetOffline() reentrancy.  See the comment in
     // SetOffline() for more details.
