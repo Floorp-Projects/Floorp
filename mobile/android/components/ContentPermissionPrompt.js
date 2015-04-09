955bab529c7d7713557452b00204489ffa54544b
Chenxia Liu <liuche@mozilla.com>
2015-04-09 16:04:21 -0700

    Bug 1147064 - Make negative/positive button order consistent. r=margaret

--- mobile/android/components/ContentPermissionPrompt.js
+++ mobile/android/components/ContentPermissionPrompt.js
@@ -99,6 +99,16 @@ ContentPermissionPrompt.prototype = {
     let entityName = kEntities[perm.type];
 
     let buttons = [{
+      label: browserBundle.GetStringFromName(entityName + ".dontAllow"),
+      callback: function(aChecked) {
+        // If the user checked "Don't ask again", make a permanent exception
+        if (aChecked)
+          Services.perms.addFromPrincipal(request.principal, access, Ci.nsIPermissionManager.DENY_ACTION);
+
+        request.cancel();
+      }
+    },
+    {
       label: browserBundle.GetStringFromName(entityName + ".allow"),
       callback: function(aChecked) {
         // If the user checked "Don't ask again", make a permanent exception
@@ -110,16 +120,6 @@ ContentPermissionPrompt.prototype = {
         }
 
         request.allow();
-      }
-    },
-    {
-      label: browserBundle.GetStringFromName(entityName + ".dontAllow"),
-      callback: function(aChecked) {
-        // If the user checked "Don't ask again", make a permanent exception
-        if (aChecked)
-          Services.perms.addFromPrincipal(request.principal, access, Ci.nsIPermissionManager.DENY_ACTION);
-
-        request.cancel();
       }
     }];
 
