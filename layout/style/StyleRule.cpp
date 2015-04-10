eb82fe6bac7538d33f77f627b951489b2fcf3f39
Cameron McCormack <cam@mcc.id.au>
2015-04-10 10:41:35 +1000

    Bug 1149042 - Call AttributeWillChange before a style="" attribute gets created when touching element.style. r=smaug

--- layout/style/StyleRule.cpp
+++ layout/style/StyleRule.cpp
@@ -1009,7 +1009,7 @@ public:
 
   NS_IMETHOD GetParentRule(nsIDOMCSSRule **aParent) override;
   void DropReference(void);
-  virtual css::Declaration* GetCSSDeclaration(bool aAllocate) override;
+  virtual css::Declaration* GetCSSDeclaration(Operation aOperation) override;
   virtual nsresult SetCSSDeclaration(css::Declaration* aDecl) override;
   virtual void GetCSSParsingEnvironment(CSSParsingEnvironment& aCSSParseEnv) override;
   virtual nsIDocument* DocToUpdate() override;
@@ -1114,7 +1114,7 @@ DOMCSSDeclarationImpl::DropReference(void)
 }
 
 css::Declaration*
-DOMCSSDeclarationImpl::GetCSSDeclaration(bool aAllocate)
+DOMCSSDeclarationImpl::GetCSSDeclaration(Operation aOperation)
 {
   if (mRule) {
     return mRule->GetDeclaration();
