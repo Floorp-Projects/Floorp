0369e0617b7861b18ed28b4441dc376caa8e5d5e
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2015-04-10 15:41:57 +0200

    Bug 1147414 - Check of that Scalar Replacement is working as expected. r=h4writer

--- js/src/jit-test/tests/ion/recover-empty-new-object.js
+++ js/src/jit-test/tests/ion/recover-empty-new-object.js
@@ -1,13 +1,24 @@
 
+// Ion eager fails the test below because we have not yet created any
+// template object in baseline before running the content of the top-level
+// function.
+if (getJitCompilerOptions()["ion.warmup.trigger"] <= 20)
+    setJitCompilerOption("ion.warmup.trigger", 20);
+
 function f(a, b, c, d) { }
 
 function topLevel() {
-    for (var i = 0; i < 10000; i++) {
+    for (var i = 0; i < 30; i++) {
         var unused = {};
         var a = {};
         var b = {};
         var c = {};
         var d = {};
+        assertRecoveredOnBailout(unused, true);
+        assertRecoveredOnBailout(a, true);
+        assertRecoveredOnBailout(b, true);
+        assertRecoveredOnBailout(c, true);
+        assertRecoveredOnBailout(d, true);
         f(a, b, c, d);
     }
 }
