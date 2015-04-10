222952466c93ec92e0810aecee4c1b1230e0887a
Seth Fowler <mark.seth.fowler@gmail.com>
2015-04-09 17:52:23 -0700

    Bug 1146335 (Part 2) - Fix an off-by-one error in image::Downscaler. r=tn

--- image/src/Downscaler.cpp
+++ image/src/Downscaler.cpp
@@ -160,9 +160,15 @@ Downscaler::CommitRow()
                                /* use_sse2 = */ true);
   }
 
-  while (mLinesInBuffer == filterLength &&
-         mCurrentOutLine < mTargetSize.height) {
+  MOZ_ASSERT(mCurrentOutLine < mTargetSize.height,
+             "Writing past end of output");
+
+  while (mLinesInBuffer == filterLength) {
     DownscaleInputLine();
+
+    if (mCurrentOutLine == mTargetSize.height) {
+      break;  // We're done.
+    }
 
     GetFilterOffsetAndLength(mYFilter, mCurrentOutLine,
                              &filterOffset, &filterLength);
