a9a3456e9fa17759ba07bf1a74c60f7b83b93708
Ryan VanderMeulen <ryanvm@gmail.com>
2015-04-09 22:55:51 -0400

    Backed out changeset e4242ba4cc35 for robocop bustage.

--- toolkit/components/reader/JSDOMParser.js
+++ toolkit/components/reader/JSDOMParser.js
@@ -37,39 +37,6 @@
     dump("JSDOMParser error: " + m + "\n");
   }
 
-  // XML only defines these and the numeric ones:
-
-  var entityTable = {
-    "lt": "<",
-    "gt": ">",
-    "amp": "&",
-    "quot": '"',
-    "apos": "'",
-  };
-
-  var reverseEntityTable = {
-    "<": "&lt;",
-    ">": "&gt;",
-    "&": "&amp;",
-    '"': "&quot;",
-    "'": "&apos;",
-  };
-
-  function encodeHTML(s) {
-    return s.replace(/[&<>'"]/g, function(x) {
-      return reverseEntityTable[x];
-    });
-  }
-
-  function decodeHTML(str) {
-    return str.replace(/&(quot|amp|apos|lt|gt);/g, function(match, tag) {
-      return entityTable[tag];
-    }).replace(/&#(?:x([0-9a-z]{1,4})|([0-9]{1,4}));/gi, function(match, hex, numStr) {
-      var num = parseInt(hex || numStr, hex ? 16 : 10); // read num
-      return String.fromCharCode(num);
-    });
-  }
-
   // When a style is set in JS, map it to the corresponding CSS attribute
   var styleMap = {
     "alignmentBaseline": "alignment-baseline",
@@ -480,9 +447,7 @@
         }
         return oldNode;
       }
-    },
-
-    __JSDOMParser__: true,
+    }
   };
 
   for (var i in nodeTypes) {
@@ -491,27 +456,7 @@
 
   var Attribute = function (name, value) {
     this.name = name;
-    this._value = value;
-  };
-
-  Attribute.prototype = {
-    get value() {
-      return this._value;
-    },
-    setValue: function(newValue) {
-      this._value = newValue;
-      delete this._decodedValue;
-    },
-    setDecodedValue: function(newValue) {
-      this._value = encodeHTML(newValue);
-      this._decodedValue = newValue;
-    },
-    getDecodedValue: function() {
-      if (typeof this._decodedValue === "undefined") {
-        this._decodedValue = (this._value && decodeHTML(this._value)) || "";
-      }
-      return this._decodedValue;
-    },
+    this.value = value;
   };
 
   var Comment = function () {
@@ -534,27 +479,7 @@
 
     nodeName: "#text",
     nodeType: Node.TEXT_NODE,
-    get textContent() {
-      if (typeof this._textContent === "undefined") {
-        this._textContent = decodeHTML(this._innerHTML || "");
-      }
-      return this._textContent;
-    },
-    get innerHTML() {
-      if (typeof this._innerHTML === "undefined") {
-        this._innerHTML = encodeHTML(this._textContent || "");
-      }
-      return this._innerHTML;
-    },
-
-    set innerHTML(newHTML) {
-      this._innerHTML = newHTML;
-      delete this._textContent;
-    },
-    set textContent(newText) {
-      this._textContent = newText;
-      delete this._innerHTML;
-    },
+    textContent: ""
   }
 
   var Document = function () {
@@ -657,15 +582,13 @@
             // serialize attribute list
             for (var j = 0; j < child.attributes.length; j++) {
               var attr = child.attributes[j];
-              // the attribute value will be HTML escaped.
-              var val = attr.value;
-              var quote = (val.indexOf('"') === -1 ? '"' : "'");
-              arr.push(" " + attr.name + '=' + quote + val + quote);
+              var quote = (attr.value.indexOf('"') === -1 ? '"' : "'");
+              arr.push(" " + attr.name + '=' + quote + attr.value + quote);
             }
 
             if (child.localName in voidElems) {
               // if this is a self-closing element, end it here
-              arr.push(">");
+              arr.push("/>");
             } else {
               // otherwise, add its children
               arr.push(">");
@@ -673,8 +596,7 @@
               arr.push("</" + child.localName + ">");
             }
           } else {
-            // This is a text node, so asking for innerHTML won't recurse.
-            arr.push(child.innerHTML);
+            arr.push(child.textContent);
           }
         }
       }
@@ -736,7 +658,7 @@
       for (var i = this.attributes.length; --i >= 0;) {
         var attr = this.attributes[i];
         if (attr.name === name)
-          return attr.getDecodedValue();
+          return attr.value;
       }
       return undefined;
     },
@@ -745,11 +667,11 @@
       for (var i = this.attributes.length; --i >= 0;) {
         var attr = this.attributes[i];
         if (attr.name === name) {
-          attr.setDecodedValue(value);
+          attr.value = value;
           return;
         }
       }
-      this.attributes.push(new Attribute(name, encodeHTML(value)));
+      this.attributes.push(new Attribute(name, value));
     },
 
     removeAttribute: function (name) {
@@ -900,6 +822,9 @@
       // Read the attribute value (and consume the matching quote)
       var value = this.readString(c);
 
+      if (!value)
+        return;
+
       node.attributes.push(new Attribute(name, value));
 
       return;
@@ -969,7 +894,7 @@
      */
     match: function (str) {
       var strlen = str.length;
-      if (this.html.substr(this.currentChar, strlen).toLowerCase() === str.toLowerCase()) {
+      if (this.html.substr(this.currentChar, strlen) === str) {
         this.currentChar += strlen;
         return true;
       }
@@ -1029,10 +954,10 @@
         // looking for the same < all the time.
         this.currentChar--;
         if (n === -1) {
-          textNode.innerHTML += this.html.substring(this.currentChar, this.html.length);
+          textNode.textContent += this.html.substring(this.currentChar, this.html.length);
           this.currentChar = this.html.length;
         } else {
-          textNode.innerHTML += this.html.substring(this.currentChar, n);
+          textNode.textContent += this.html.substring(this.currentChar, n);
           this.currentChar = n;
         }
         if (!haveTextNode)
@@ -1065,7 +990,7 @@
      */
     readNode: function () {
       var c = this.nextChar();
-
+ 
       if (c === undefined)
         return null;
 
@@ -1075,10 +1000,10 @@
         var node = new Text();
         var n = this.html.indexOf("<", this.currentChar);
         if (n === -1) {
-          node.innerHTML = this.html.substring(this.currentChar, this.html.length);
+          node.textContent = this.html.substring(this.currentChar, this.html.length);
           this.currentChar = this.html.length;
         } else {
-          node.innerHTML = this.html.substring(this.currentChar, n);
+          node.textContent = this.html.substring(this.currentChar, n);
           this.currentChar = n;
         }
         return node;
