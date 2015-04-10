ce1112fb5b0a3fc92a3d79bc955a31012f3e8260
Jeff Muizelaar <jmuizelaar@mozilla.com>
2015-04-10 16:17:27 -0400

    Bug 1153381 - Add a D3D11 ANGLE blacklist. r=mstange, a=lmandel, ba=const-only-change
    This blocks the Intel 4500 style GPUs so that Google Maps works properly. IGNORE IDL CHANGES

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -2164,6 +2164,7 @@ gfxPlatform::OptimalFormatForContent(gfxContentType aContent)
 static bool sLayersSupportsD3D9 = false;
 static bool sLayersSupportsD3D11 = false;
 static bool sLayersSupportsDXVA = false;
+static bool sANGLESupportsD3D11 = false;
 static bool sBufferRotationCheckPref = true;
 static bool sPrefBrowserTabsRemoteAutostart = false;
 
@@ -2210,6 +2211,11 @@ InitLayersAccelerationPrefs()
             sLayersSupportsDXVA = true;
           }
         }
+        if (NS_SUCCEEDED(gfxInfo->GetFeatureStatus(nsIGfxInfo::FEATURE_DIRECT3D_11_ANGLE, &status))) {
+          if (status == nsIGfxInfo::FEATURE_STATUS_OK) {
+            sANGLESupportsD3D11 = true;
+          }
+        }
       }
     }
 #endif
@@ -2244,6 +2250,14 @@ gfxPlatform::CanUseDXVA()
   MOZ_ASSERT(sLayersAccelerationPrefsInitialized);
   return sLayersSupportsDXVA;
 }
+
+bool
+gfxPlatform::CanUseDirect3D11ANGLE()
+{
+  MOZ_ASSERT(sLayersAccelerationPrefsInitialized);
+  return sANGLESupportsD3D11;
+}
+
 
 bool
 gfxPlatform::BufferRotationEnabled()
