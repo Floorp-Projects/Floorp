87ca3080b6cf4bcb8db1f48fccf1a03d971d3883
Michael Comella <michael.l.comella@gmail.com>
2015-04-10 13:33:00 -0700

    Bug 1152489 - Prevent getMostRecentHomePanel() from being called on null selectedTab. r=mfinkle
    --HG--
    extra : rebase_source : b32614c59677fad7d2a47ca080a7661e2425a635

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -2143,7 +2143,14 @@ public class BrowserApp extends GeckoApp
         }
 
         final Tab selectedTab = Tabs.getInstance().getSelectedTab();
-        mTargetTabForEditingMode = (selectedTab != null ? selectedTab.getId() : null);
+        final String panelId;
+        if (selectedTab != null) {
+            mTargetTabForEditingMode = selectedTab.getId();
+            panelId = selectedTab.getMostRecentHomePanel();
+        } else {
+            mTargetTabForEditingMode = null;
+            panelId = null;
+        }
 
         final PropertyAnimator animator = new PropertyAnimator(250);
         animator.setUseHardwareLayer(false);
@@ -2152,7 +2159,6 @@ public class BrowserApp extends GeckoApp
 
         mBrowserToolbar.startEditing(url, animator);
 
-        final String panelId = selectedTab.getMostRecentHomePanel();
         showHomePagerWithAnimator(panelId, animator);
 
         animator.start();
