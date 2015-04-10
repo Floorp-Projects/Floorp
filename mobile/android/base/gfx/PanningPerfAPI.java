33666b43985fddc1ba369a5d47484637a885912c
Geoff Brown <gbrown@mozilla.com>
2015-04-10 14:41:37 -0600

    Bug 1151910 - Guard against ArrayIndexOutOfBoundsException in testCheck2; r=kats

--- mobile/android/base/gfx/PanningPerfAPI.java
+++ mobile/android/base/gfx/PanningPerfAPI.java
@@ -98,6 +98,10 @@ public class PanningPerfAPI {
         // is still executing. As values are added to this list last, we use
         // this number as the canonical number of recordings.
         int values = mCheckerboardAmounts.size();
+        if (values == 0) {
+            Log.w(LOGTAG, "stopCheckerboardRecording() found no checkerboard amounts!");
+            return mCheckerboardAmounts;
+        }
 
         // The score will be the sum of all the values in mCheckerboardAmounts,
         // so weight the checkerboard values by time so that frame-rate and
