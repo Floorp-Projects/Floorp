b3d900a61ed75083020b014f053e14d1dc11824b
Jeff Muizelaar <jmuizelaar@mozilla.com>
2015-04-10 16:17:27 -0400

    Bug 1153381 - Add a D3D11 ANGLE blacklist. r=mstange, a=sledru
    This blocks the Intel 4500 style GPUs so that Google Maps works properly.
    
    --HG--
    extra : histedit_source : c0fb2d1672fd40c1cdcb702afe42aec0a47bcc89

--- gfx/gl/GLLibraryEGL.cpp
+++ gfx/gl/GLLibraryEGL.cpp
@@ -270,7 +270,7 @@ GLLibraryEGL::EnsureInitialized()
             if (gfxPrefs::WebGLANGLEForceD3D11()) {
                 newDisplay = GetAndInitDisplay(*this,
                                                LOCAL_EGL_D3D11_ONLY_DISPLAY_ANGLE);
-            } else if (gfxPrefs::WebGLANGLETryD3D11()) {
+            } else if (gfxPrefs::WebGLANGLETryD3D11() && gfxPlatform::CanUseDirect3D11ANGLE()) {
                 newDisplay = GetAndInitDisplay(*this,
                                                LOCAL_EGL_D3D11_ELSE_D3D9_DISPLAY_ANGLE);
             }
