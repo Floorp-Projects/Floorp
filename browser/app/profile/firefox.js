0b741f20f3cb1d2bf971ea86cf3800f4dd648872
Shane Tomlinson <stomlinson@mozilla.com>
2015-04-11 13:23:08 +0100

    Bug 1146904: Add the FxAccountsWebChannel to drive Sync * * * fix tests From 6bb486068a8b002f222f4658989968d86df9eff1 Mon Sep 17 00:00:00 2001 ---  .../test/general/browser_fxa_web_channel.html      |   2 +-  .../test/general/browser_fxa_web_channel.js        | 131 +++++++++++----------  services/fxaccounts/tests/xpcshell/test_profile.js |  26 ----  3 files changed, 70 insertions(+), 89 deletions(-)

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1790,6 +1790,9 @@ pref("identity.fxaccounts.remote.force_auth.uri", "https://accounts.firefox.com/
 // The remote content URL shown for signin in. Must use HTTPS.
 pref("identity.fxaccounts.remote.signin.uri", "https://accounts.firefox.com/signin?service=sync&context=fx_desktop_v1");
 
+// The remote content URL where FxAccountsWebChannel messages originate.
+pref("identity.fxaccounts.remote.webchannel.uri", "https://accounts.firefox.com/");
+
 // The URL we take the user to when they opt to "manage" their Firefox Account.
 // Note that this will always need to be in the same TLD as the
 // "identity.fxaccounts.remote.signup.uri" pref.
