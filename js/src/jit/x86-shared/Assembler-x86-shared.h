315e4d75391bf70561460b02aa82375986d71674
Chris Peterson <cpeterson@mozilla.com>
2015-04-11 22:59:30 -0700

    Back out changeset 469f64dc67e2 (bug 1152661) for -Wbitfield-constant-conversion warnings-as-errors on OS X.

--- js/src/jit/x86-shared/Assembler-x86-shared.h
+++ js/src/jit/x86-shared/Assembler-x86-shared.h
@@ -36,23 +36,15 @@ class Operand
   public:
     explicit Operand(Register reg)
       : kind_(REG),
-        base_(reg.code()),
-        scale_(TimesOne),
-        index_(0),
-        disp_(0)
+        base_(reg.code())
     { }
     explicit Operand(FloatRegister reg)
       : kind_(FPREG),
-        base_(reg.encoding()),
-        scale_(TimesOne),
-        index_(0),
-        disp_(0)
+        base_(reg.encoding())
     { }
     explicit Operand(const Address& address)
       : kind_(MEM_REG_DISP),
         base_(address.base.code()),
-        scale_(TimesOne),
-        index_(0),
         disp_(address.offset)
     { }
     explicit Operand(const BaseIndex& address)
@@ -72,22 +64,14 @@ class Operand
     Operand(Register reg, int32_t disp)
       : kind_(MEM_REG_DISP),
         base_(reg.code()),
-        scale_(TimesOne),
-        index_(0),
         disp_(disp)
     { }
     explicit Operand(AbsoluteAddress address)
       : kind_(MEM_ADDRESS32),
-        base_(Registers::Invalid),
-        scale_(TimesOne),
-        index_(0),
         disp_(X86Encoding::AddressImmediate(address.addr))
     { }
     explicit Operand(PatchedAbsoluteAddress address)
       : kind_(MEM_ADDRESS32),
-        base_(Registers::Invalid),
-        scale_(TimesOne),
-        index_(0),
         disp_(X86Encoding::AddressImmediate(address.addr))
     { }
 
