dfc4f44f6d4e2632f0d78fd7d64f28fcf69bc56b
Chris Peterson <cpeterson@mozilla.com>
2015-04-11 23:03:26 -0700

    Bug 1152661 - Fix -Wuninitialized warnings about Operand member variables. r=jandem

--- js/src/jit/x86-shared/Assembler-x86-shared.h
+++ js/src/jit/x86-shared/Assembler-x86-shared.h
@@ -29,7 +29,7 @@ class Operand
   private:
     Kind kind_ : 4;
     // Used as a Register::Encoding and a FloatRegister::Encoding.
-    int32_t base_ : 5;
+    uint32_t base_ : 5;
     Scale scale_ : 3;
     Register::Encoding index_ : 5;
     int32_t disp_;
@@ -37,15 +37,23 @@ class Operand
   public:
     explicit Operand(Register reg)
       : kind_(REG),
-        base_(reg.encoding())
+        base_(reg.encoding()),
+        scale_(TimesOne),
+        index_(Registers::Invalid),
+        disp_(0)
     { }
     explicit Operand(FloatRegister reg)
       : kind_(FPREG),
-        base_(reg.encoding())
+        base_(reg.encoding()),
+        scale_(TimesOne),
+        index_(Registers::Invalid),
+        disp_(0)
     { }
     explicit Operand(const Address& address)
       : kind_(MEM_REG_DISP),
         base_(address.base.encoding()),
+        scale_(TimesOne),
+        index_(Registers::Invalid),
         disp_(address.offset)
     { }
     explicit Operand(const BaseIndex& address)
@@ -65,14 +73,22 @@ class Operand
     Operand(Register reg, int32_t disp)
       : kind_(MEM_REG_DISP),
         base_(reg.encoding()),
+        scale_(TimesOne),
+        index_(Registers::Invalid),
         disp_(disp)
     { }
     explicit Operand(AbsoluteAddress address)
       : kind_(MEM_ADDRESS32),
+        base_(Registers::Invalid),
+        scale_(TimesOne),
+        index_(Registers::Invalid),
         disp_(X86Encoding::AddressImmediate(address.addr))
     { }
     explicit Operand(PatchedAbsoluteAddress address)
       : kind_(MEM_ADDRESS32),
+        base_(Registers::Invalid),
+        scale_(TimesOne),
+        index_(Registers::Invalid),
         disp_(X86Encoding::AddressImmediate(address.addr))
     { }
 
