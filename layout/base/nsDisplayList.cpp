2e3d6daa53adcf1ce1c54cdd139efed70ee610d3
Kartikaya Gupta <kgupta@mozilla.com>
2015-04-13 16:11:08 -0400

    Bug 1148889 - Treat subclasses of scrollframes as animated geometry roots. r=mstange

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -1235,7 +1235,7 @@ nsDisplayListBuilder::IsAnimatedGeometryRoot(nsIFrame* aFrame, nsIFrame** aParen
     return true;
   }
 
-  if (parentType == nsGkAtoms::scrollFrame) {
+  if (parentType == nsGkAtoms::scrollFrame || parentType == nsGkAtoms::listControlFrame) {
     nsIScrollableFrame* sf = do_QueryFrame(parent);
     if (sf->IsScrollingActive(this) && sf->GetScrolledFrame() == aFrame) {
       return true;
