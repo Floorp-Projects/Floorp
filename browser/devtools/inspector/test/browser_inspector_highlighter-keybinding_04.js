3cb4f37e7ceaa67d66c856e9d9baf79168ae0fda
Patrick Brosset <pbrosset@mozilla.com>
2015-04-13 10:51:49 +0200

    Bug 988278 - Fixes ESCape keypress mess in the inspector to make sure the split console opens; r=miker
    This fixes 2 problems related to the split console not opening when it should.
    1 - After the inspector selection mode (pick mode) was canceled with ESC, pressing ESC once
    again did not open the split console, because the toolbox did not have the focus.
    2 - The markup-view tooltip (used to preview images) was eating the first ESC keypress when
    the markup-view was focused, even though the tooltip was hidden. This was forcing users to
    press ESC twice to open the split console.

---
+++ browser/devtools/inspector/test/browser_inspector_highlighter-keybinding_04.js
@@ -0,0 +1,45 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Tests that pressing ESC twice while in picker mode first stops the picker and
+// then opens the split-console (see bug 988278).
+
+const TEST_URL = "data:text/html;charset=utf8,<div></div>";
+
+add_task(function*() {
+  let {inspector, toolbox} = yield openInspectorForURL(TEST_URL);
+
+  info("Start the element picker");
+  yield toolbox.highlighterUtils.startPicker();
+
+  info("Start using the picker by hovering over nodes");
+  let onHover = toolbox.once("picker-node-hovered");
+  executeInContent("Test:SynthesizeMouse", {
+    options: {type: "mousemove"},
+    center: true,
+    selector: "div"
+  }, null, false);
+  yield onHover;
+
+  info("Press escape and wait for the picker to stop");
+  let onPickerStopped = toolbox.once("picker-stopped");
+  executeInContent("Test:SynthesizeKey", {
+    key: "VK_ESCAPE",
+    options: {}
+  }, null, false);
+  yield onPickerStopped;
+
+  info("Press escape again and wait for the split console to open");
+  let onSplitConsole = toolbox.once("split-console");
+  // The escape key is synthesized in the main process, which is where the focus
+  // should be after the picker was stopped.
+  EventUtils.synthesizeKey("VK_ESCAPE", {});
+  yield onSplitConsole;
+  ok(toolbox.splitConsole, "The split console is shown.");
+
+  // Hide the split console.
+  yield toolbox.toggleSplitConsole();
+});
