de88d3265bb0b31575394359c31853395fdbaa8b
Aaron Klotz <aklotz@mozilla.com>
2015-04-13 13:19:51 -0600

    Bug 1151804 - Ensure that streams are not prematurely terminated when asyncInit is true. r=lizzard

--- dom/plugins/base/nsNPAPIPluginStreamListener.h
+++ dom/plugins/base/nsNPAPIPluginStreamListener.h
@@ -113,7 +113,16 @@ protected:
     eStreamTypeSet      // The stream is fully initialized
   };
 
+  enum StreamStopMode
+  {
+    eNormalStop = 0,
+    eDoDeferredStop,
+    eStopPending
+  };
+
   virtual ~nsNPAPIPluginStreamListener();
+  bool MaybeRunStopBinding();
+
   char* mStreamBuffer;
   char* mNotifyURL;
   nsRefPtr<nsNPAPIPluginInstance> mInst;
@@ -131,6 +140,8 @@ protected:
   char* mResponseHeaderBuf;
   nsCOMPtr<nsITimer> mDataPumpTimer;
   nsCOMPtr<nsIAsyncVerifyRedirectCallback> mHTTPRedirectCallback;
+  StreamStopMode mStreamStopMode;
+  nsresult mPendingStopBindingStatus;
 
 public:
   nsRefPtr<nsPluginStreamListenerPeer> mStreamListenerPeer;
