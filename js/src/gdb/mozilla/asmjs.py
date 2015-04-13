6c610d0754fd9767165b96fca3326e950a5bd293
Jason Orendorff <jorendorff@mozilla.com>
2015-04-13 12:46:22 -0500

    Bug 1151957 - Rewrite GDB extension for ignoring asm.js segmentation faults in Python, fixing some bugs and bringing it under test. r=jimb.
    The GDB code being rewritten here did not work for users who build with -ggdb3
    on Linux, because sa_handler is a macro; see bug 1151957 comment 1. Also, it
    caused GDB to perform an invalid free in the JS shell debuggee process, on
    startup, if the previous run received SIGSEGV.
    
    --HG--
    rename : js/src/shell/js-gdb.gdb => js/src/gdb/mozilla/asmjs.py
    extra : rebase_source : ba061d1978a4cb40172e7a79a3597bc0f91e9232

---
+++ js/src/gdb/mozilla/asmjs.py
@@ -0,0 +1,40 @@
+"""
+In asm code, out-of-bounds heap accesses cause segfaults, which the engine
+handles internally. Make GDB ignore them.
+"""
+
+import gdb
+
+SIGSEGV = 11
+
+# A sigaction buffer for each inferior process.
+sigaction_buffers = {}
+
+def on_stop(event):
+    if isinstance(event, gdb.SignalEvent) and event.stop_signal == 'SIGSEGV':
+        # Allocate memory for sigaction, once per js shell process.
+        process = gdb.selected_inferior()
+        buf = sigaction_buffers.get(process)
+        if buf is None:
+            buf = gdb.parse_and_eval("(struct sigaction *) malloc(sizeof(struct sigaction))")
+            sigaction_buffers[process] = buf
+
+        # See if AsmJSFaultHandler is installed as the SIGSEGV signal action.
+        sigaction_fn = gdb.parse_and_eval('__sigaction')
+        sigaction_fn(SIGSEGV, 0, buf)
+        AsmJSFaultHandler = gdb.parse_and_eval("AsmJSFaultHandler")
+        if buf['__sigaction_handler']['sa_handler'] == AsmJSFaultHandler:
+            # Advise the user that magic is happening.
+            print "js/src/gdb/mozilla/asmjs.py: Allowing AsmJSFaultHandler to run."
+
+            # If AsmJSFaultHandler doesn't handle this segfault, it will unhook
+            # itself and re-raise.
+            gdb.execute("continue")
+
+def on_exited(event):
+    if event.inferior in sigaction_buffers:
+        del sigaction_buffers[event.inferior]
+
+def install():
+    gdb.events.stop.connect(on_stop)
+    gdb.events.exited.connect(on_exited)
---
+++ js/src/gdb/tests/test-asmjs.py
@@ -0,0 +1,15 @@
+# Test for special asmjs SIGSEGV-handling.
+#
+# Expected behavior is for the asm.js code in the following fragment to trigger
+# SIGSEGV. The code in js/src/gdb/mozilla/asmjs.py should prevent GDB from
+# handling that signal.
+
+run_fragment('asmjs.segfault')
+
+# If SIGSEGV handling is broken, GDB would have stopped at the SIGSEGV signal.
+# The breakpoint would not have hit, and run_fragment would have thrown.
+#
+# So if we get here, and the asm.js code actually ran, we win.
+
+assert_pretty('ok', 'true')
+assert_pretty('rval', '$jsval("ok")')
