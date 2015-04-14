59ee64bb549ad5f743e4b854ebc9a47ee8efb8bb
Shu-yu Guo <shu@rfrn.org>
2015-04-14 13:35:49 -0700

    Bug 1151154 - Don't fake resumption packets on tab navigation events in the frontend debugger. (r=past)

--- toolkit/devtools/server/actors/root.js
+++ toolkit/devtools/server/actors/root.js
@@ -154,6 +154,9 @@ RootActor.prototype = {
     // Trait added in Gecko 38, indicating that all features necessary for
     // grabbing allocations from the MemoryActor are available for the performance tool
     memoryActorAllocations: true,
+    // Added in Gecko 40, indicating that the backend isn't stupid about
+    // sending resumption packets on tab navigation.
+    noNeedToFakeResumptionOnNavigation: true,
     // Whether root actor exposes tab actors
     // if allowChromeProcess is true, you can fetch a ChromeActor instance
     // to debug chrome and any non-content ressource via getProcess request
