38c1e8f7b0aff95595c0287198eef8d13183ea52
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-04-15 16:12:17 +0200

    Backed out changeset 4cf74e040c0a (bug 1145049)

--- toolkit/devtools/server/main.js
+++ toolkit/devtools/server/main.js
@@ -1555,8 +1555,6 @@ DebuggerServerConnection.prototype = {
     this._extraPools.map(function(p) { p.cleanup(); });
     this._extraPools = null;
 
-    this.rootActor = null;
-    this._transport = null;
     DebuggerServer._connectionClosed(this);
   },
 
