35caca6a1571c67ca7946f420c39745be75e25c1
James Graham <james@hoppipolla.co.uk>
2015-04-15 16:00:44 +0100

    Bug 1155079 - Update web-platform-tests to revision 89b6e2bc460316c7f273712d22f0b2d3a3d0c5be, a =testonly

--- testing/web-platform/tests/tools/manifest/sourcefile.py
+++ testing/web-platform/tests/tools/manifest/sourcefile.py
@@ -49,11 +49,12 @@ class SourceFile(object):
     def __getstate__(self):
         # Remove computed properties if we pickle this class
         rv = self.__dict__.copy()
-        cached_properties = rv.get("__cached_properties__", set())
-        for key in rv.keys():
-            if key in cached_properties:
-                del rv[key]
+
         if "__cached_properties__" in rv:
+            cached_properties = rv["__cached_properties__"]
+            for key in rv.keys():
+                if key in cached_properties:
+                    del rv[key]
             del rv["__cached_properties__"]
         return rv
 
