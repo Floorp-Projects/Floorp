fe0f813b3fd61746a839f7e789e9cf235a618f72
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-04-15 09:55:53 +0200

    Backed out changeset 19f871364039 (bug 1110485) for causing bc1 winxp memory leaks on a CLOSED TREE

--- dom/cache/AutoUtils.h
+++ dom/cache/AutoUtils.h
@@ -9,7 +9,6 @@
 
 #include "mozilla/Attributes.h"
 #include "mozilla/dom/cache/PCacheTypes.h"
-#include "mozilla/dom/cache/Types.h"
 #include "mozilla/dom/cache/TypeUtils.h"
 #include "nsTArray.h"
 
@@ -30,7 +29,6 @@ class InternalRequest;
 namespace cache {
 
 class CacheStreamControlParent;
-class Manager;
 struct SavedRequest;
 struct SavedResponse;
 class StreamList;
@@ -43,56 +41,129 @@ class StreamList;
 // Note, these should only be used when *sending* streams across IPC.  The
 // deserialization case is handled by creating a ReadStream object.
 
-class MOZ_STACK_CLASS AutoChildOpArgs final
+class MOZ_STACK_CLASS AutoChildBase
 {
-public:
+protected:
   typedef TypeUtils::BodyAction BodyAction;
   typedef TypeUtils::ReferrerAction ReferrerAction;
   typedef TypeUtils::SchemeAction SchemeAction;
 
-  AutoChildOpArgs(TypeUtils* aTypeUtils, const CacheOpArgs& aOpArgs);
-  ~AutoChildOpArgs();
+  AutoChildBase(TypeUtils* aTypeUtils);
+  virtual ~AutoChildBase() = 0;
+
+  TypeUtils* mTypeUtils;
+  bool mSent;
+};
+
+class MOZ_STACK_CLASS AutoChildRequest final : public AutoChildBase
+{
+public:
+  explicit AutoChildRequest(TypeUtils* aTypeUtils);
+  ~AutoChildRequest();
 
   void Add(InternalRequest* aRequest, BodyAction aBodyAction,
            ReferrerAction aReferrerAction, SchemeAction aSchemeAction,
            ErrorResult& aRv);
-  void Add(InternalRequest* aRequest, BodyAction aBodyAction,
-           ReferrerAction aReferrerAction, SchemeAction aSchemeAction,
-           Response& aResponse, ErrorResult& aRv);
 
-  const CacheOpArgs& SendAsOpArgs();
+  const PCacheRequest& SendAsRequest();
+  const PCacheRequestOrVoid& SendAsRequestOrVoid();
 
 private:
-  TypeUtils* mTypeUtils;
-  CacheOpArgs mOpArgs;
-  bool mSent;
+  PCacheRequestOrVoid mRequestOrVoid;
 };
 
-class MOZ_STACK_CLASS AutoParentOpResult final
+class MOZ_STACK_CLASS AutoChildRequestList final : public AutoChildBase
 {
 public:
-  AutoParentOpResult(mozilla::ipc::PBackgroundParent* aManager,
-                     const CacheOpResult& aOpResult);
-  ~AutoParentOpResult();
+  AutoChildRequestList(TypeUtils* aTypeUtils, uint32_t aCapacity);
+  ~AutoChildRequestList();
 
-  void Add(CacheId aOpenedCacheId, Manager* aManager);
-  void Add(const SavedResponse& aSavedResponse, StreamList* aStreamList);
-  void Add(const SavedRequest& aSavedRequest, StreamList* aStreamList);
+  void Add(InternalRequest* aRequest, BodyAction aBodyAction,
+           ReferrerAction aReferrerAction, SchemeAction aSchemeAction,
+           ErrorResult& aRv);
 
-  const CacheOpResult& SendAsOpResult();
+  const nsTArray<PCacheRequest>& SendAsRequestList();
 
 private:
-  void SerializeResponseBody(const SavedResponse& aSavedResponse,
-                             StreamList* aStreamList,
-                             PCacheResponse* aResponseOut);
+  // Allocates ~5k inline in the stack-only class
+  nsAutoTArray<PCacheRequest, 32> mRequestList;
+};
+
+class MOZ_STACK_CLASS AutoChildRequestResponse final : public AutoChildBase
+{
+public:
+  explicit AutoChildRequestResponse(TypeUtils* aTypeUtils);
+  ~AutoChildRequestResponse();
+
+  void Add(InternalRequest* aRequest, BodyAction aBodyAction,
+           ReferrerAction aReferrerAction, SchemeAction aSchemeAction,
+           ErrorResult& aRv);
+  void Add(Response& aResponse, ErrorResult& aRv);
+
+  const CacheRequestResponse& SendAsRequestResponse();
+
+private:
+  CacheRequestResponse mRequestResponse;
+};
+
+class MOZ_STACK_CLASS AutoParentBase
+{
+protected:
+  explicit AutoParentBase(mozilla::ipc::PBackgroundParent* aManager);
+  virtual ~AutoParentBase() = 0;
 
   void SerializeReadStream(const nsID& aId, StreamList* aStreamList,
                            PCacheReadStream* aReadStreamOut);
 
   mozilla::ipc::PBackgroundParent* mManager;
-  CacheOpResult mOpResult;
   CacheStreamControlParent* mStreamControl;
   bool mSent;
+};
+
+class MOZ_STACK_CLASS AutoParentRequestList final : public AutoParentBase
+{
+public:
+  AutoParentRequestList(mozilla::ipc::PBackgroundParent* aManager,
+                        uint32_t aCapacity);
+  ~AutoParentRequestList();
+
+  void Add(const SavedRequest& aSavedRequest, StreamList* aStreamList);
+
+  const nsTArray<PCacheRequest>& SendAsRequestList();
+
+private:
+  // Allocates ~5k inline in the stack-only class
+  nsAutoTArray<PCacheRequest, 32> mRequestList;
+};
+
+class MOZ_STACK_CLASS AutoParentResponseList final : public AutoParentBase
+{
+public:
+  AutoParentResponseList(mozilla::ipc::PBackgroundParent* aManager,
+                         uint32_t aCapacity);
+  ~AutoParentResponseList();
+
+  void Add(const SavedResponse& aSavedResponse, StreamList* aStreamList);
+
+  const nsTArray<PCacheResponse>& SendAsResponseList();
+
+private:
+  // Allocates ~4k inline in the stack-only class
+  nsAutoTArray<PCacheResponse, 32> mResponseList;
+};
+
+class MOZ_STACK_CLASS AutoParentResponseOrVoid final : public AutoParentBase
+{
+public:
+  explicit AutoParentResponseOrVoid(mozilla::ipc::PBackgroundParent* aManager);
+  ~AutoParentResponseOrVoid();
+
+  void Add(const SavedResponse& aSavedResponse, StreamList* aStreamList);
+
+  const PCacheResponseOrVoid& SendAsResponseOrVoid();
+
+private:
+  PCacheResponseOrVoid mResponseOrVoid;
 };
 
 } // namespace cache
