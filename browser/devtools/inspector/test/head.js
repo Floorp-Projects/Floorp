8ebeee1053667136e215e6c06a3bb7ec40e63b3e
Alexandre Poirot <poirot.alex@gmail.com>
2015-04-15 13:55:29 +0200

    Bug 1137285 - Replace content.location = ... by a navigateTo request r=pbrosset

--- browser/devtools/inspector/test/head.js
+++ browser/devtools/inspector/test/head.js
@@ -78,6 +78,11 @@ let addTab = Task.async(function* (url) {
   return tab;
 });
 
+let navigateTo = function (toolbox, url) {
+  let activeTab = toolbox.target.activeTab;
+  return activeTab.navigateTo(url);
+};
+
 /**
  * Simple DOM node accesor function that takes either a node or a string css
  * selector as argument and returns the corresponding node
