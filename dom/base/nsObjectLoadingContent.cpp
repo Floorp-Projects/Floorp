4d04f5b2786fa1f9a638245e8d881557713d3f28
Aaron Klotz <aklotz@mozilla.com>
2015-04-15 16:24:28 -0600

    Bug 1155241 - Check mInstanceOwner for nullptr in nsObjectLoadingContent::PluginDestroyed. r=smaug, a=lmandel

--- dom/base/nsObjectLoadingContent.cpp
+++ dom/base/nsObjectLoadingContent.cpp
@@ -2720,8 +2720,10 @@ nsObjectLoadingContent::PluginDestroyed()
   // plugins in plugin host. Invalidate instance owner / prototype but otherwise
   // don't take any action.
   TeardownProtoChain();
-  mInstanceOwner->Destroy();
-  mInstanceOwner = nullptr;
+  if (mInstanceOwner) {
+    mInstanceOwner->Destroy();
+    mInstanceOwner = nullptr;
+  }
   return NS_OK;
 }
 
