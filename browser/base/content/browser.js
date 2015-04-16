5bebdaaed2a17881dea7d433ca308fdcd75f39fd
Jared Wein <jwein@mozilla.com>
2015-04-15 23:45:50 -0400

    Bug 1134501 - UITour: Force page into Reader View automatically whenever the ReaderView/ReadingList tour page is loaded. r=gijs, a=dolske

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4221,6 +4221,7 @@ var XULBrowserWindow = {
 
         BookmarkingUI.onLocationChange();
         SocialUI.updateState(location);
+        UITour.onLocationChange(location);
       }
 
       // Utility functions for disabling find
