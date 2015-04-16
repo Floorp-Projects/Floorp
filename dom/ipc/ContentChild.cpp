6aa0e3d43fdc1c476ed73c4476dcc371e4bf68b5
Neil Deakin <neil@mozilla.com>
2015-04-16 15:38:12 -0400

    Bug 1071562, check type now that CString is supported, r=smaug

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -2800,6 +2800,8 @@ ContentChild::RecvInvokeDragSession(nsTArray<IPCDataTransfer>&& aTransfers,
             BlobChild* blob = static_cast<BlobChild*>(item.data().get_PBlobChild());
             nsRefPtr<FileImpl> fileImpl = blob->GetBlobImpl();
             variant->SetAsISupports(fileImpl);
+          } else {
+            continue;
           }
           dataTransfer->SetDataWithPrincipal(NS_ConvertUTF8toUTF16(item.flavor()),
                                              variant, i,
