0721b4df96e2756b790ce1e13f293dfaae397131
Bruce Sun <brsun@mozilla.com>
2015-04-16 16:44:58 +0800

    Bug 1140952 - Implement read/write value of a descriptor for GATT client API (webapi part). f=jocelyn, r=btian, r=mrbkap

--- dom/bluetooth/bluetooth2/BluetoothGattDescriptor.cpp
+++ dom/bluetooth/bluetooth2/BluetoothGattDescriptor.cpp
@@ -52,3 +52,29 @@ BluetoothGattDescriptor::WrapObject(JSContext* aContext,
 {
   return BluetoothGattDescriptorBinding::Wrap(aContext, this, aGivenProto);
 }
+
+void
+BluetoothGattDescriptor::GetValue(JSContext* cx,
+                                  JS::MutableHandle<JSObject*> aValue) const
+{
+  MOZ_ASSERT(aValue);
+
+  aValue.set(mValue.IsEmpty()
+             ? nullptr
+             : ArrayBuffer::Create(cx, mValue.Length(), mValue.Elements()));
+}
+
+already_AddRefed<Promise>
+BluetoothGattDescriptor::ReadValue(ErrorResult& aRv)
+{
+  // TODO: This will be implemented by later patch set in the same bug.
+  return nullptr;
+}
+
+already_AddRefed<Promise>
+BluetoothGattDescriptor::WriteValue(
+  const RootedTypedArray<ArrayBuffer>& aValue, ErrorResult& aRv)
+{
+  // TODO: This will be implemented by later patch set in the same bug.
+  return nullptr;
+}
