c0b0aac2860a0c7833247018af796bd04f39f382
L. David Baron <dbaron@dbaron.org>
2015-04-16 18:36:29 -0700

    Bug 947753 / Bug 1122526 - Don't claim to support off-main-thread animations when the nearest widget is not using OMT compositing.  r=birtles
    This moves the test for whether off-main-thread compositor is enabled
    *earlier* in CanPerformOnCompositorThread, since
    CanAnimatePropertyOnCompositor is called only from
    CanPerformOnCompositorThread.  This change means we're using a more
    accurate test for whether we actually have off-main-thread compositing
    than the pref, since in some cases we won't use off-main-thread
    compositing for certain widgets (e.g., transparent widgets on Windows)
    even when the pref is enabled.
    
    --HG--
    extra : amend_source : 0f286f6d05a1ebe706c14dba94ae35449ea898c9

--- layout/style/AnimationCommon.cpp
+++ layout/style/AnimationCommon.cpp
@@ -506,15 +506,6 @@ AnimationCollection::CanAnimatePropertyOnCompositor(
   CanAnimateFlags aFlags)
 {
   bool shouldLog = nsLayoutUtils::IsAnimationLoggingEnabled();
-  if (!gfxPlatform::OffMainThreadCompositingEnabled()) {
-    if (shouldLog) {
-      nsCString message;
-      message.AppendLiteral("Performance warning: Compositor disabled");
-      LogAsyncAnimationFailure(message);
-    }
-    return false;
-  }
-
   nsIFrame* frame = nsLayoutUtils::GetStyleFrame(aElement);
   if (IsGeometricProperty(aProperty)) {
     if (shouldLog) {
@@ -579,6 +570,19 @@ AnimationCollection::CanPerformOnCompositorThread(
 {
   nsIFrame* frame = nsLayoutUtils::GetStyleFrame(mElement);
   if (!frame) {
+    return false;
+  }
+
+  nsIWidget* widget = frame->GetNearestWidget();
+  if (!widget ||
+      widget->GetLayerManager()->GetBackendType() !=
+        layers::LayersBackend::LAYERS_CLIENT) {
+    // No widget (huh?), or a widget not using off-main-thread compositor.
+    if (nsLayoutUtils::IsAnimationLoggingEnabled()) {
+      nsCString message;
+      message.AppendLiteral("Performance warning: Compositor disabled");
+      LogAsyncAnimationFailure(message);
+    }
     return false;
   }
 
