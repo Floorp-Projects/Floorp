c2aa4f0eed7ee9e5fcf5f010848d336bcdb8bdf6
Mike de Boer <mdeboer@mozilla.com>
2015-04-17 12:43:56 +0200

    Bug 1155195 - Disable Loop screensharing for Fx38. r=Standard8, a=sledru

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1688,7 +1688,7 @@ pref("shumway.swf.whitelist", "http://g-ecx.images-amazon.com/*/AiryBasicRendere
 pref("image.mem.max_decoded_image_kb", 256000);
 
 pref("loop.enabled", true);
-pref("loop.screenshare.enabled", true);
+pref("loop.screenshare.enabled", false);
 pref("loop.server", "https://loop.services.mozilla.com/v0");
 pref("loop.seenToS", "unseen");
 pref("loop.showPartnerLogo", true);
