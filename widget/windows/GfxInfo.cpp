25cd9bd45925364cd6277f636639d2c68ba91d96
Matt Woodrow <mwoodrow@mozilla.com>
2015-04-17 12:41:37 +1200

    Bug 1144257 - Blacklist DXVA for one NVIDIA driver that was causing crashes. r=ajones
    --HG--
    extra : rebase_source : df4e448320c4d041ccf3ff900c790d31a6e3694d

--- widget/windows/GfxInfo.cpp
+++ widget/windows/GfxInfo.cpp
@@ -1107,6 +1107,11 @@ GfxInfo::GetGfxDriverInfo()
       nsIGfxInfo::FEATURE_HARDWARE_VIDEO_DECODING, nsIGfxInfo::FEATURE_BLOCKED_DRIVER_VERSION,
       DRIVER_LESS_THAN, V(8,15,10,2622));
 
+    APPEND_TO_DRIVER_BLOCKLIST2(DRIVER_OS_ALL,
+      (nsAString&)GfxDriverInfo::GetDeviceVendor(VendorNVIDIA), (GfxDeviceFamily*)GfxDriverInfo::GetDeviceFamily(Nvidia8800GTS),
+      nsIGfxInfo::FEATURE_HARDWARE_VIDEO_DECODING, nsIGfxInfo::FEATURE_BLOCKED_DRIVER_VERSION,
+      DRIVER_EQUAL, V(9,18,13,4052));
+
     /* Bug 1137716: XXX this should really check for the matching Intel piece as well.
      * Unfortunately, we don't have the infrastructure to do that */
     APPEND_TO_DRIVER_BLOCKLIST_RANGE_GPU2(DRIVER_OS_WINDOWS_7,
