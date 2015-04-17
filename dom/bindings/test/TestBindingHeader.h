806a236c64d81266906852c1bd8cee83f3006da0
Boris Zbarsky <bzbarsky@mit.edu>
2015-04-17 12:13:25 -0400

    Bug 1129239.  Don't require 'optional' keyword on trailing dictionary arguments if the dictionary has a required member.  r=smaug

--- dom/bindings/test/TestBindingHeader.h
+++ dom/bindings/test/TestBindingHeader.h
@@ -712,6 +712,7 @@ public:
 
   // Dictionary tests
   void PassDictionary(JSContext*, const Dict&);
+  void PassDictionary2(JSContext*, const Dict&);
   void GetReadonlyDictionary(JSContext*, Dict&);
   void GetReadonlyNullableDictionary(JSContext*, Nullable<Dict>&);
   void GetWritableDictionary(JSContext*, Dict&);
