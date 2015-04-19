04cd7d1c7ae95b238d3d9bb8ebd99a0bb63b2b10
Ehsan Akhgari <ehsan@mozilla.com>
2015-04-19 13:22:35 -0400

    Bug 1156084 - Disallow AddRef() and Release() calls on the return value of methods returning XPCOM objects; r=jrmuizel
    When a method returns type D derived from RefCounted type B, there is an
    ImplicitCastExpr (or an ExplicitCastExpr, if there is an explicit cast
    to the base type in the code) in the AST between the CallExpr and
    MemberExpr, which we didn't take into account before.  This caused the
    analysis to not work on common patterns such as
    nsCOMPtr<nsIXPCOMInterface>.

--- dom/media/gmp/GMPService.cpp
+++ dom/media/gmp/GMPService.cpp
@@ -283,8 +283,8 @@ GeckoMediaPluginService::GetThread(nsIThread** aThread)
     InitializePlugins();
   }
 
-  NS_ADDREF(mGMPThread);
-  *aThread = mGMPThread;
+  nsCOMPtr<nsIThread> copy = mGMPThread;
+  copy.forget(aThread);
 
   return NS_OK;
 }
