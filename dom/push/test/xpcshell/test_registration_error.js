cfda49a5ee5132e950695a9a168310899e4a4aa4
Kit Cambridge <github@kitcambridge.be>
2015-04-19 12:06:31 +0200

    Bug 1150683 - Add xpcshell tests for nsIPushNotificationService. r=dougt
    ---
     dom/push/moz.build                                 |   4 +
     dom/push/test/xpcshell/head.js                     | 450 +++++++++++++++++++++
     dom/push/test/xpcshell/test_notification_ack.js    | 127 ++++++
     .../test/xpcshell/test_notification_duplicate.js   |  82 ++++
     dom/push/test/xpcshell/test_notification_error.js  | 127 ++++++
     .../test/xpcshell/test_notification_incomplete.js  | 109 +++++
     .../xpcshell/test_notification_version_string.js   |  72 ++++
     dom/push/test/xpcshell/test_register_case.js       |  64 +++
     dom/push/test/xpcshell/test_register_flush.js      | 103 +++++
     .../test/xpcshell/test_register_invalid_channel.js |  60 +++
     .../xpcshell/test_register_invalid_endpoint.js     |  62 +++
     .../test/xpcshell/test_register_invalid_json.js    |  61 +++
     dom/push/test/xpcshell/test_register_no_id.js      |  65 +++
     .../test/xpcshell/test_register_request_queue.js   |  65 +++
     dom/push/test/xpcshell/test_register_rollback.js   |  88 ++++
     dom/push/test/xpcshell/test_register_success.js    |  76 ++++
     dom/push/test/xpcshell/test_register_timeout.js    | 102 +++++
     dom/push/test/xpcshell/test_register_wrong_id.js   |  71 ++++
     dom/push/test/xpcshell/test_register_wrong_type.js |  67 +++
     dom/push/test/xpcshell/test_registration_error.js  |  39 ++
     .../xpcshell/test_registration_missing_scope.js    |  28 ++
     dom/push/test/xpcshell/test_registration_none.js   |  28 ++
     .../test/xpcshell/test_registration_success.js     |  67 +++
     .../test/xpcshell/test_unregister_empty_scope.js   |  37 ++
     dom/push/test/xpcshell/test_unregister_error.js    |  65 +++
     .../test/xpcshell/test_unregister_invalid_json.js  |  78 ++++
     .../test/xpcshell/test_unregister_not_found.js     |  35 ++
     dom/push/test/xpcshell/test_unregister_success.js  |  60 +++
     dom/push/test/xpcshell/xpcshell.ini                |  32 ++
     29 files changed, 2324 insertions(+)
     create mode 100644 dom/push/test/xpcshell/head.js
     create mode 100644 dom/push/test/xpcshell/test_notification_ack.js
     create mode 100644 dom/push/test/xpcshell/test_notification_duplicate.js
     create mode 100644 dom/push/test/xpcshell/test_notification_error.js
     create mode 100644 dom/push/test/xpcshell/test_notification_incomplete.js
     create mode 100644 dom/push/test/xpcshell/test_notification_version_string.js
     create mode 100644 dom/push/test/xpcshell/test_register_case.js
     create mode 100644 dom/push/test/xpcshell/test_register_flush.js
     create mode 100644 dom/push/test/xpcshell/test_register_invalid_channel.js
     create mode 100644 dom/push/test/xpcshell/test_register_invalid_endpoint.js
     create mode 100644 dom/push/test/xpcshell/test_register_invalid_json.js
     create mode 100644 dom/push/test/xpcshell/test_register_no_id.js
     create mode 100644 dom/push/test/xpcshell/test_register_request_queue.js
     create mode 100644 dom/push/test/xpcshell/test_register_rollback.js
     create mode 100644 dom/push/test/xpcshell/test_register_success.js
     create mode 100644 dom/push/test/xpcshell/test_register_timeout.js
     create mode 100644 dom/push/test/xpcshell/test_register_wrong_id.js
     create mode 100644 dom/push/test/xpcshell/test_register_wrong_type.js
     create mode 100644 dom/push/test/xpcshell/test_registration_error.js
     create mode 100644 dom/push/test/xpcshell/test_registration_missing_scope.js
     create mode 100644 dom/push/test/xpcshell/test_registration_none.js
     create mode 100644 dom/push/test/xpcshell/test_registration_success.js
     create mode 100644 dom/push/test/xpcshell/test_unregister_empty_scope.js
     create mode 100644 dom/push/test/xpcshell/test_unregister_error.js
     create mode 100644 dom/push/test/xpcshell/test_unregister_invalid_json.js
     create mode 100644 dom/push/test/xpcshell/test_unregister_not_found.js
     create mode 100644 dom/push/test/xpcshell/test_unregister_success.js
     create mode 100644 dom/push/test/xpcshell/xpcshell.ini

---
+++ dom/push/test/xpcshell/test_registration_error.js
@@ -0,0 +1,39 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+'use strict';
+
+const {PushDB, PushService} = serviceExports;
+
+function run_test() {
+  do_get_profile();
+  setPrefs({
+    userAgentID: '6faed1f0-1439-4aac-a978-db21c81cd5eb'
+  });
+  run_next_test();
+}
+
+add_task(function* test_registrations_error() {
+  let db = new PushDB();
+  do_register_cleanup(() => cleanupDatabase(db));
+
+  PushService.init({
+    networkInfo: new MockDesktopNetworkInfo(),
+    db: makeStub(db, {
+      getByScope(prev, scope, successCb, failureCb) {
+        failureCb('oops');
+      }
+    }),
+    makeWebSocket(uri) {
+      return new MockWebSocket(uri);
+    }
+  });
+
+  yield rejects(
+    PushNotificationService.registration('https://example.net/1'),
+    function(error) {
+      return error == 'Database error';
+    },
+    'Wrong message'
+  );
+});
