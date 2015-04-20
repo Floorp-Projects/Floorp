5f52539522990c152b779fdf4cf48d269f124adc
Chris Pearce <cpearce@mozilla.com>
2015-04-20 20:07:24 +1200

    Bug 1156131 - Use correct DLL on WinVista, 7, and 8 for WMF decoding in gmp-clearkey. r=edwin, a=sledru

--- media/gmp-clearkey/0.1/WMFSymbols.h
+++ media/gmp-clearkey/0.1/WMFSymbols.h
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-MFPLAT_FUNC(MFCreateSample);
-MFPLAT_FUNC(MFCreateAlignedMemoryBuffer);
-MFPLAT_FUNC(MFGetStrideForBitmapInfoHeader);
-MFPLAT_FUNC(MFCreateMediaType);
+MFPLAT_FUNC(MFCreateSample, "mfplat.dll");
+MFPLAT_FUNC(MFCreateAlignedMemoryBuffer, "mfplat.dll");
+MFPLAT_FUNC(MFGetStrideForBitmapInfoHeader, "evr.dll");
+MFPLAT_FUNC(MFCreateMediaType, "mfplat.dll");
--- media/gmp-clearkey/0.1/WMFUtils.h
+++ media/gmp-clearkey/0.1/WMFUtils.h
@@ -122,7 +122,7 @@ typedef int64_t Microseconds;
 #define GMP_SUCCEEDED(x) ((x) == GMPNoErr)
 #define GMP_FAILED(x) ((x) != GMPNoErr)
 
-#define MFPLAT_FUNC(_func) \
+#define MFPLAT_FUNC(_func, _dllname) \
   extern decltype(::_func)* _func;
 #include "WMFSymbols.h"
 #undef MFPLAT_FUNC
@@ -264,6 +264,15 @@ HRESULT
 CreateMFT(const CLSID& clsid,
           const char* aDllName,
           CComPtr<IMFTransform>& aOutMFT);
+
+enum CodecType {
+  H264,
+  AAC,
+};
+
+// Returns the name of the DLL that is needed to decode H.264 or AAC on
+// the given windows version we're running on.
+const char* WMFDecoderDllNameFor(CodecType aCodec);
 
 } // namespace wmf
 
