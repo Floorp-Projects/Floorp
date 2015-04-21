47b180edd677a92f282c93727bc955d322e80477
Kit Cambridge <github@kitcambridge.be>
2015-04-21 20:10:50 +0200

    Bug 1150683 - Add xpcshell tests for nsIPushNotificationService. r=dougt
    ---
     dom/push/moz.build                                 |   4 +
     dom/push/test/xpcshell/head.js                     | 450 +++++++++++++++++++++
     dom/push/test/xpcshell/test_notification_ack.js    | 127 ++++++
     .../test/xpcshell/test_notification_duplicate.js   |  82 ++++
     dom/push/test/xpcshell/test_notification_error.js  | 127 ++++++
     .../test/xpcshell/test_notification_incomplete.js  | 109 +++++
     .../xpcshell/test_notification_version_string.js   |  72 ++++
     dom/push/test/xpcshell/test_register_case.js       |  64 +++
     dom/push/test/xpcshell/test_register_flush.js      | 103 +++++
     .../test/xpcshell/test_register_invalid_channel.js |  60 +++
     .../xpcshell/test_register_invalid_endpoint.js     |  62 +++
     .../test/xpcshell/test_register_invalid_json.js    |  61 +++
     dom/push/test/xpcshell/test_register_no_id.js      |  65 +++
     .../test/xpcshell/test_register_request_queue.js   |  65 +++
     dom/push/test/xpcshell/test_register_rollback.js   |  88 ++++
     dom/push/test/xpcshell/test_register_success.js    |  76 ++++
     dom/push/test/xpcshell/test_register_timeout.js    | 102 +++++
     dom/push/test/xpcshell/test_register_wrong_id.js   |  71 ++++
     dom/push/test/xpcshell/test_register_wrong_type.js |  67 +++
     dom/push/test/xpcshell/test_registration_error.js  |  39 ++
     .../xpcshell/test_registration_missing_scope.js    |  28 ++
     dom/push/test/xpcshell/test_registration_none.js   |  28 ++
     .../test/xpcshell/test_registration_success.js     |  67 +++
     .../test/xpcshell/test_unregister_empty_scope.js   |  37 ++
     dom/push/test/xpcshell/test_unregister_error.js    |  65 +++
     .../test/xpcshell/test_unregister_invalid_json.js  |  78 ++++
     .../test/xpcshell/test_unregister_not_found.js     |  35 ++
     dom/push/test/xpcshell/test_unregister_success.js  |  60 +++
     dom/push/test/xpcshell/xpcshell.ini                |  32 ++
     29 files changed, 2324 insertions(+)
     create mode 100644 dom/push/test/xpcshell/head.js
     create mode 100644 dom/push/test/xpcshell/test_notification_ack.js
     create mode 100644 dom/push/test/xpcshell/test_notification_duplicate.js
     create mode 100644 dom/push/test/xpcshell/test_notification_error.js
     create mode 100644 dom/push/test/xpcshell/test_notification_incomplete.js
     create mode 100644 dom/push/test/xpcshell/test_notification_version_string.js
     create mode 100644 dom/push/test/xpcshell/test_register_case.js
     create mode 100644 dom/push/test/xpcshell/test_register_flush.js
     create mode 100644 dom/push/test/xpcshell/test_register_invalid_channel.js
     create mode 100644 dom/push/test/xpcshell/test_register_invalid_endpoint.js
     create mode 100644 dom/push/test/xpcshell/test_register_invalid_json.js
     create mode 100644 dom/push/test/xpcshell/test_register_no_id.js
     create mode 100644 dom/push/test/xpcshell/test_register_request_queue.js
     create mode 100644 dom/push/test/xpcshell/test_register_rollback.js
     create mode 100644 dom/push/test/xpcshell/test_register_success.js
     create mode 100644 dom/push/test/xpcshell/test_register_timeout.js
     create mode 100644 dom/push/test/xpcshell/test_register_wrong_id.js
     create mode 100644 dom/push/test/xpcshell/test_register_wrong_type.js
     create mode 100644 dom/push/test/xpcshell/test_registration_error.js
     create mode 100644 dom/push/test/xpcshell/test_registration_missing_scope.js
     create mode 100644 dom/push/test/xpcshell/test_registration_none.js
     create mode 100644 dom/push/test/xpcshell/test_registration_success.js
     create mode 100644 dom/push/test/xpcshell/test_unregister_empty_scope.js
     create mode 100644 dom/push/test/xpcshell/test_unregister_error.js
     create mode 100644 dom/push/test/xpcshell/test_unregister_invalid_json.js
     create mode 100644 dom/push/test/xpcshell/test_unregister_not_found.js
     create mode 100644 dom/push/test/xpcshell/test_unregister_success.js
     create mode 100644 dom/push/test/xpcshell/xpcshell.ini

---
+++ dom/push/test/xpcshell/test_notification_error.js
@@ -0,0 +1,127 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+'use strict';
+
+const {PushDB, PushService} = serviceExports;
+
+function run_test() {
+  do_get_profile();
+  setPrefs();
+  disableServiceWorkerEvents(
+    'https://example.com/a',
+    'https://example.com/b',
+    'https://example.com/c'
+  );
+  run_next_test();
+}
+
+add_task(function* test_notification_error() {
+  let db = new PushDB();
+  let promiseDB = promisifyDatabase(db);
+  do_register_cleanup(() => cleanupDatabase(db));
+  let records = [{
+    channelID: 'f04f1e46-9139-4826-b2d1-9411b0821283',
+    pushEndpoint: 'https://example.org/update/success-1',
+    scope: 'https://example.com/a',
+    version: 1
+  }, {
+    channelID: '3c3930ba-44de-40dc-a7ca-8a133ec1a866',
+    pushEndpoint: 'https://example.org/update/error',
+    scope: 'https://example.com/b',
+    version: 2
+  }, {
+    channelID: 'b63f7bef-0a0d-4236-b41e-086a69dfd316',
+    pushEndpoint: 'https://example.org/update/success-2',
+    scope: 'https://example.com/c',
+    version: 3
+  }];
+  for (let record of records) {
+    yield promiseDB.put(record);
+  }
+
+  let notifyPromise = Promise.all([
+    promiseObserverNotification(
+      'push-notification',
+      (subject, data) => data == 'https://example.com/a'
+    ),
+    promiseObserverNotification(
+      'push-notification',
+      (subject, data) => data == 'https://example.com/c'
+    )
+  ]);
+
+  let ackDefer = Promise.defer();
+  let ackDone = after(records.length, ackDefer.resolve);
+  PushService.init({
+    networkInfo: new MockDesktopNetworkInfo(),
+    db: makeStub(db, {
+      getByChannelID(prev, channelID, successCb, failureCb) {
+        if (channelID == '3c3930ba-44de-40dc-a7ca-8a133ec1a866') {
+          return failureCb('splines not reticulated');
+        }
+        return prev.call(this, channelID, successCb, failureCb);
+      }
+    }),
+    makeWebSocket(uri) {
+      return new MockWebSocket(uri, {
+        onHello(request) {
+          deepEqual(request.channelIDs.sort(), [
+            '3c3930ba-44de-40dc-a7ca-8a133ec1a866',
+            'b63f7bef-0a0d-4236-b41e-086a69dfd316',
+            'f04f1e46-9139-4826-b2d1-9411b0821283'
+          ], 'Wrong channel list');
+          this.serverSendMsg(JSON.stringify({
+            messageType: 'hello',
+            status: 200,
+            uaid: '3c7462fc-270f-45be-a459-b9d631b0d093'
+          }));
+          this.serverSendMsg(JSON.stringify({
+            messageType: 'notification',
+            updates: records.map(({channelID, version}) =>
+              ({channelID, version: ++version}))
+          }));
+        },
+        // Should acknowledge all received updates, even if updating
+        // IndexedDB fails.
+        onACK: ackDone
+      });
+    }
+  });
+
+  let [a, c] = yield waitForPromise(
+    notifyPromise,
+    DEFAULT_TIMEOUT,
+    'Timed out waiting for notifications'
+  );
+  let aPush = a.subject.QueryInterface(Ci.nsIPushObserverNotification);
+  equal(aPush.pushEndpoint, 'https://example.org/update/success-1',
+    'Wrong endpoint for notification A');
+  equal(aPush.version, 2, 'Wrong version for notification A');
+
+  let cPush = c.subject.QueryInterface(Ci.nsIPushObserverNotification);
+  equal(cPush.pushEndpoint, 'https://example.org/update/success-2',
+    'Wrong endpoint for notification C');
+  equal(cPush.version, 4, 'Wrong version for notification C');
+
+  yield waitForPromise(ackDefer.promise, DEFAULT_TIMEOUT,
+    'Timed out waiting for acknowledgements');
+
+  let aRecord = yield promiseDB.getByScope('https://example.com/a');
+  equal(aRecord.channelID, 'f04f1e46-9139-4826-b2d1-9411b0821283',
+    'Wrong channel ID for record A');
+  strictEqual(aRecord.version, 2,
+    'Should return the new version for record A');
+
+  let bRecord = yield promiseDB.getByScope('https://example.com/b');
+  equal(bRecord.channelID, '3c3930ba-44de-40dc-a7ca-8a133ec1a866',
+    'Wrong channel ID for record B');
+  strictEqual(bRecord.version, 2,
+    'Should return the previous version for record B');
+
+  let cRecord = yield promiseDB.getByScope('https://example.com/c');
+  equal(cRecord.channelID, 'b63f7bef-0a0d-4236-b41e-086a69dfd316',
+    'Wrong channel ID for record C');
+  strictEqual(cRecord.version, 4,
+    'Should return the new version for record C');
+});
