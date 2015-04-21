5fcf9c03a5bdd7cfa3a7c226562517fc6e638d77
Robert Strong <robert.bugzilla@gmail.com>
2015-04-21 07:30:42 -0700

    Bug 1155896 - xpcshell log statements truncated due to equality checks for long strings. r=spohl

--- toolkit/mozapps/update/tests/unit_base_updater/marAppApplyUpdateSuccess.js
+++ toolkit/mozapps/update/tests/unit_base_updater/marAppApplyUpdateSuccess.js
@@ -106,7 +106,6 @@ function finishCheckUpdateFinished() {
   checkAppBundleModTime();
   checkFilesAfterUpdateSuccess(getApplyDirFile, false, false);
   checkUpdateLogContents(LOG_COMPLETE_SUCCESS);
-  checkCallbackAppLog();
 
   standardInit();
 
