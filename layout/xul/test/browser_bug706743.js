23233c6ba7a87f49398ba79b563ea84f9cf22700
Neil Deakin <neil@mozilla.com>
2015-04-21 20:09:14 -0400

    Bug 1155355, e10s, rewrite and reenable browser tests in layout/xul/test, don't show tooltips during a drag, use system event listeners for tooltips, r=billm

--- layout/xul/test/browser_bug706743.js
+++ layout/xul/test/browser_bug706743.js
@@ -1,154 +1,84 @@
-function test() {
-  waitForExplicitFinish();
-  gBrowser.selectedTab = gBrowser.addTab();
+add_task(function* () {
+  const url = "data:text/html,<html><head></head><body>" +
+    "<a id=\"target\" href=\"about:blank\" title=\"This is tooltip text\" " +
+            "style=\"display:block;height:20px;margin:10px;\" " +
+            "onclick=\"return false;\">here is an anchor element</a></body></html>";
 
-  SpecialPowers.setIntPref("ui.tooltipDelay", 0);
+  let tab = yield BrowserTestUtils.openNewForegroundTab(gBrowser, url);
+  let browser = gBrowser.selectedBrowser;
 
-  let target;
-  let doc;
-  let win;
-  let callbackOnPopupShown;
-  let callbackOnPopupHidden;
+  yield new Promise(resolve => {
+    SpecialPowers.pushPrefEnv({"set": [["ui.tooltipDelay", 0]]}, resolve);
+  });
+
+  // Send a mousemove at a known position to start the test.
+  yield BrowserTestUtils.synthesizeMouse("#target", -5, -5, { type: "mousemove" }, browser);
+
+  // show tooltip by mousemove into target.
+  let popupShownPromise = BrowserTestUtils.waitForEvent(document, "popupshown");
+  yield BrowserTestUtils.synthesizeMouse("#target", 5, 15, { type: "mousemove" }, browser);
+  yield popupShownPromise;
+
+  // hide tooltip by mousemove to outside.
+  let popupHiddenPromise = BrowserTestUtils.waitForEvent(document, "popuphidden");
+  yield BrowserTestUtils.synthesizeMouse("#target", -5, 15, { type: "mousemove" }, browser);
+  yield popupHiddenPromise;
+
+  // mousemove into the target and start drag by emulation via nsIDragService.
+  // Note that on some platforms, we cannot actually start the drag by
+  // synthesized events.  E.g., Windows waits an actual mousemove event after
+  // dragstart.
+
+  // Emulate a buggy mousemove event.  widget might dispatch mousemove event
+  // during drag.
+
+  function tooltipNotExpected()
+  {
+    ok(false, "tooltip is shown during drag");
+  }
+  addEventListener("popupshown", tooltipNotExpected, true);
+
   let dragService = Components.classes["@mozilla.org/widget/dragservice;1"].
                       getService(Components.interfaces.nsIDragService);
+  dragService.startDragSession();
+  yield BrowserTestUtils.synthesizeMouse("#target", 5, 15, { type: "mousemove" }, browser);
 
-  let onPopupHidden = function (aEvent)
-  {
-    if (aEvent.originalTarget.localName != "tooltip") {
-      return;
-    }
-    if (callbackOnPopupHidden) {
-      setTimeout(callbackOnPopupHidden, 0);
-    }
-  }
+  yield new Promise(resolve => setTimeout(resolve, 100));
+  removeEventListener("popupshown", tooltipNotExpected, true);
+  dragService.endDragSession(true);
 
-  let onPopupShown = function (aEvent)
-  {
-    if (aEvent.originalTarget.localName != "tooltip") {
-      return;
-    }
-    if (callbackOnPopupShown) {
-      setTimeout(callbackOnPopupShown, 0);
-    }
-  }
+  yield BrowserTestUtils.synthesizeMouse("#target", -5, -5, { type: "mousemove" }, browser);
 
-  let finishTest = function ()
-  {
-    document.removeEventListener("popupshown", onPopupShown, true);
-    document.removeEventListener("popuphidden", onPopupHidden, true);
+  // If tooltip listener used a flag for managing D&D state, we would need
+  // to test if the tooltip is shown after drag.
 
-    SpecialPowers.clearUserPref("ui.tooltipDelay");
+  // show tooltip by mousemove into target.
+  popupShownPromise = BrowserTestUtils.waitForEvent(document, "popupshown");
+  yield BrowserTestUtils.synthesizeMouse("#target", 5, 15, { type: "mousemove" }, browser);
+  yield popupShownPromise;
 
-    gBrowser.removeCurrentTab();
-    finish();
-  }
+  // hide tooltip by mousemove to outside.
+  popupHiddenPromise = BrowserTestUtils.waitForEvent(document, "popuphidden");
+  yield BrowserTestUtils.synthesizeMouse("#target", -5, 15, { type: "mousemove" }, browser);
+  yield popupHiddenPromise;
 
-  let testHideTooltipByMouseDown = function ()
-  {
-    callbackOnPopupShown = function () {
-      callbackOnPopupShown = null;
-      ok(true, "tooltip is shown before testing mousedown");
+  // Show tooltip after mousedown
+  popupShownPromise = BrowserTestUtils.waitForEvent(document, "popupshown");
+  yield BrowserTestUtils.synthesizeMouse("#target", 5, 15, { type: "mousemove" }, browser);
+  yield popupShownPromise;
 
-      // hide tooltip by mousemove to outside.
-      callbackOnPopupHidden = function () {
-        callbackOnPopupHidden = null;
-        ok(true, "The tooltip is hidden by mousedown");
+  popupHiddenPromise = BrowserTestUtils.waitForEvent(document, "popuphidden");
+  yield BrowserTestUtils.synthesizeMouse("#target", 5, 15, { type: "mousedown" }, browser);
+  yield popupHiddenPromise;
 
-        EventUtils.synthesizeMouse(target, 5, 15, { type: "mouseup" }, win);
-        EventUtils.synthesizeMouse(target, -5, 15, { type: "mousemove" }, win);
+  yield BrowserTestUtils.synthesizeMouse("#target", 5, 15, { type: "mouseup" }, browser);
+  yield BrowserTestUtils.synthesizeMouse("#target", -5, 15, { type: "mousemove" }, browser);
 
-        setTimeout(finishTest, 0);
-      }
-      EventUtils.synthesizeMouse(target, 5, 15, { type: "mousedown" }, win);
-    }
-    EventUtils.synthesizeMouse(target, 5, 15, { type: "mousemove" }, win);
-  }
+  ok(true, "tooltips appear properly");
 
-  let testShowTooltipAgain = function ()
-  {
-    // If tooltip listener used a flag for managing D&D state, we would need
-    // to test if the tooltip is shown after drag.
-    callbackOnPopupShown = function () {
-      callbackOnPopupShown = null;
-      ok(true, "tooltip is shown after drag");
+  gBrowser.removeCurrentTab();
+});
 
-      // hide tooltip by mousemove to outside.
-      callbackOnPopupHidden = function () {
-        callbackOnPopupHidden = null;
-        ok(true, "The tooltip is hidden again");
-
-        setTimeout(testHideTooltipByMouseDown, 0);
-      }
-      EventUtils.synthesizeMouse(target, -5, 15, { type: "mousemove" }, win);
-    }
-    EventUtils.synthesizeMouse(target, 5, 15, { type: "mousemove" }, win);
-  }
-
-  let testDuringDnD = function ()
-  {
-    // mousemove into the target and start drag by emulation via nsIDragService.
-    // Note that on some platforms, we cannot actually start the drag by
-    // synthesized events.  E.g., Windows waits an actual mousemove event after
-    // dragstart.
-    callbackOnPopupShown = function () {
-      callbackOnPopupShown = null;
-      ok(false, "tooltip is shown during drag");
-    }
-    dragService.startDragSession();
-    // Emulate a buggy mousemove event.  widget might dispatch mousemove event
-    // during drag.
-    EventUtils.synthesizeMouse(target, 5, 15, { type: "mousemove" }, win);
-    setTimeout(function () {
-      callbackOnPopupShown = null;
-      ok(true, "tooltip isn't shown during drag");
-      dragService.endDragSession(true);
-      EventUtils.synthesizeMouse(target, -5, -5, { type: "mousemove" }, win);
-
-      setTimeout(testShowTooltipAgain, 0);
-    }, 100);
-  }
-
-  let onLoad = function (aEvent)
-  {
-    doc = gBrowser.contentDocument;
-    win = gBrowser.contentWindow;
-    target = doc.getElementById("target");
-
-    document.addEventListener("popupshown", onPopupShown, true);
-    document.addEventListener("popuphidden", onPopupHidden, true);
-
-    EventUtils.synthesizeMouse(target, -5, -5, { type: "mousemove" }, win);
-
-    // show tooltip by mousemove into target.
-    callbackOnPopupShown = function ()
-    {
-      callbackOnPopupShown = null;
-      ok(true, "The tooltip is shown");
-
-      // hide tooltip by mousemove to outside.
-      callbackOnPopupHidden = function () {
-        callbackOnPopupHidden = null;
-        ok(true, "The tooltip is hidden");
-
-        setTimeout(testDuringDnD, 0);
-      }
-      EventUtils.synthesizeMouse(target, -5, 15, { type: "mousemove" }, win);
-    }
-    EventUtils.synthesizeMouse(target, 5, 15, { type: "mousemove" }, win);
-  }
-
-  gBrowser.selectedBrowser.addEventListener("load",
-    function () {
-      gBrowser.selectedBrowser.
-        removeEventListener("load", arguments.callee, true);
-      setTimeout(onLoad, 0);
-   }, true);
-
-  content.location = "data:text/html,<html><head></head><body>" +
-    "<a id=\"target\" href=\"about:blank\" title=\"This is tooltip text\" " +
-       "style=\"display:block;height:20px;margin:10px;\" " +
-       "onclick=\"return false;\">here is an anchor element</a></body></html>";
-}
 
 
 
