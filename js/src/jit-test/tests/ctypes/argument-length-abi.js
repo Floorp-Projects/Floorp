1cfa4903f3bf8b18f48d9be80ad8a15d494da759
Tooru Fujisawa <arai_a@mac.com>
2015-04-22 20:08:21 +0900

    Backed out changeset 951ec7d134c2 (bug 891107) for xpcshell-test failure

--- js/src/jit-test/tests/ctypes/argument-length-abi.js
+++
@@ -1,9 +0,0 @@
-load(libdir + 'asserts.js');
-
-function test() {
-  assertTypeErrorMessage(() => { ctypes.default_abi.toSource(1); },
-                         "ABI.prototype.toSource takes no arguments");
-}
-
-if (typeof ctypes === "object")
-  test();
