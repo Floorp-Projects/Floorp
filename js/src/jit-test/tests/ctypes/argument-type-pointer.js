a74592cf8ff087a1d84369b8d5f6f23b1f17aba4
Tooru Fujisawa <arai_a@mac.com>
2015-04-22 20:08:19 +0900

    Backed out changeset 467559ddc08f (bug 891107) for xpcshell-test failure

--- js/src/jit-test/tests/ctypes/argument-type-pointer.js
+++
@@ -1,9 +0,0 @@
-load(libdir + 'asserts.js');
-
-function test() {
-  assertTypeErrorMessage(() => { ctypes.PointerType({}); },
-                         "argument of PointerType must be a CType");
-}
-
-if (typeof ctypes === "object")
-  test();
