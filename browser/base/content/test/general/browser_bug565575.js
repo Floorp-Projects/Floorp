a59d43859588e2b508c93cc9c92d2e425324974e
Neil Deakin <neil@mozilla.com>
2015-04-21 20:09:13 -0400

    Bug 1155348, e10s, rewrite and reenable browser_bug565575, r=felipe

--- browser/base/content/test/general/browser_bug565575.js
+++ browser/base/content/test/general/browser_bug565575.js
@@ -1,13 +1,14 @@
-function test() {
+add_task(function* () {
   gBrowser.selectedBrowser.focus();
-  BrowserOpenTab();
+
+  yield BrowserTestUtils.openNewForegroundTab(gBrowser, () => BrowserOpenTab(), false);
   ok(gURLBar.focused, "location bar is focused for a new tab");
 
-  gBrowser.selectedTab = gBrowser.tabs[0];
+  yield BrowserTestUtils.switchTab(gBrowser, gBrowser.tabs[0]);
   ok(!gURLBar.focused, "location bar isn't focused for the previously selected tab");
 
-  gBrowser.selectedTab = gBrowser.tabs[1];
+  yield BrowserTestUtils.switchTab(gBrowser, gBrowser.tabs[1]);
   ok(gURLBar.focused, "location bar is re-focused when selecting the new tab");
 
   gBrowser.removeCurrentTab();
-}
+});
