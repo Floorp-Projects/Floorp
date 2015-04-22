772748cbd82dabda1f09c247d2386d3da647229f
Jim Chen <nchen@mozilla.com>
2015-04-22 11:30:13 -0400

    Bug 1156943 - Make proper HangMonitor calls in Android nsAppShell; r=snorp

--- widget/android/AndroidJavaWrappers.h
+++ widget/android/AndroidJavaWrappers.h
@@ -505,6 +505,16 @@ public:
         return event;
     }
 
+    bool IsInputEvent() const {
+        return mType == AndroidGeckoEvent::MOTION_EVENT ||
+            mType == AndroidGeckoEvent::NATIVE_GESTURE_EVENT ||
+            mType == AndroidGeckoEvent::LONG_PRESS ||
+            mType == AndroidGeckoEvent::KEY_EVENT ||
+            mType == AndroidGeckoEvent::IME_EVENT ||
+            mType == AndroidGeckoEvent::IME_KEY_EVENT ||
+            mType == AndroidGeckoEvent::APZ_INPUT_EVENT;
+    }
+
     int Action() { return mAction; }
     int Type() { return mType; }
     bool AckNeeded() { return mAckNeeded; }
