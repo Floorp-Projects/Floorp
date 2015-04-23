9bdc0e769ff1b74ce3977b06cff048706a4187b5
Blake Wu <bwu@mozilla.com>
2015-04-23 20:40:32 +0800

    Bug 1146729 - [FFOS] Enable MP4Reader for MSE and local playback. r=cpearce. r=jwwang

--- dom/media/fmp4/MP4Decoder.cpp
+++ dom/media/fmp4/MP4Decoder.cpp
@@ -215,7 +215,11 @@ IsAndroidAvailable()
 static bool
 IsGonkMP4DecoderAvailable()
 {
+#ifndef MOZ_GONK_MEDIACODEC
+  return false;
+#else
   return Preferences::GetBool("media.fragmented-mp4.gonk.enabled", false);
+#endif
 }
 
 static bool
