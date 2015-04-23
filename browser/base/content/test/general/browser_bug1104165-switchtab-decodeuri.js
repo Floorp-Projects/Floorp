89049d0d32d3acf8b914244e84f089769c456f71
Marco Bonardo <mbonardo@mozilla.com>
2015-04-23 10:35:36 +0200

    Bug 1104165 - "Switch to Tab" doesn't work for some encoded URIs. r=dao

---
+++ browser/base/content/test/general/browser_bug1104165-switchtab-decodeuri.js
@@ -0,0 +1,29 @@
+add_task(function* test_switchtab_decodeuri() {
+  info("Opening first tab");
+  let tab = gBrowser.addTab("http://example.org/browser/browser/base/content/test/general/dummy_page.html#test%7C1");
+  yield promiseTabLoadEvent(tab);
+
+  info("Opening and selecting second tab");
+  let newTab = gBrowser.selectedTab = gBrowser.addTab();
+
+  info("Wait for autocomplete")
+  yield promiseAutocompleteResultPopup("dummy_page");
+
+  info("Select autocomplete popup entry");
+  EventUtils.synthesizeKey("VK_DOWN" , {});
+  ok(gURLBar.value.startsWith("moz-action:switchtab"), "switch to tab entry found");
+
+  info("switch-to-tab");
+  yield new Promise((resolve, reject) => {
+    // In case of success it should switch tab.
+    gBrowser.tabContainer.addEventListener("TabSelect", function select() {
+      gBrowser.tabContainer.removeEventListener("TabSelect", select, false);
+      is(gBrowser.selectedTab, tab, "Should have switched to the right tab");
+      resolve();
+    }, false);
+    EventUtils.synthesizeKey("VK_RETURN" , { });
+  });
+
+  gBrowser.removeCurrentTab();
+  yield PlacesTestUtils.clearHistory();
+});
