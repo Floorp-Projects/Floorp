90258f1a2711ef41b1257abf29e83c4a1196fdf6
Blake Kaplan <mrbkap@gmail.com>
2015-04-23 13:24:56 -0700

    Bug 1156939 - Don't stash a reference to a CPOW and then spin the event loop. r=mconley, a=test-only

--- browser/base/content/test/general/browser_bug710878.js
+++ browser/base/content/test/general/browser_bug710878.js
@@ -5,23 +5,20 @@ function test()
 {
   waitForExplicitFinish();
 
-  let doc;
-
   gBrowser.selectedTab = gBrowser.addTab();
   gBrowser.selectedBrowser.addEventListener("load", function onload() {
     gBrowser.selectedBrowser.removeEventListener("load", onload, true);
-    doc = content.document;
     waitForFocus(performTest, content);
   }, true);
 
-  content.location = "data:text/html,<a href='%23xxx'><span>word1 <span> word2 </span></span><span> word3</span></a>";
+  content.location = "data:text/html;charset=utf-8,<a href='%23xxx'><span>word1 <span> word2 </span></span><span> word3</span></a>";
 
   function performTest()
   {
+    let doc = content.document;
     let link = doc.querySelector("a");;
     let text = gatherTextUnder(link);
     is(text, "word1 word2 word3", "Text under link is correctly computed.");
-    doc = null;
     gBrowser.removeCurrentTab();
     finish();
   }
