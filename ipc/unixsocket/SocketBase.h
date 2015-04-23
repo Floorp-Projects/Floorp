90356da7cde23b918a044378623eecdd9d8f4146
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-04-23 13:48:47 +0200

    Bug 1156352: Refactor |UnixSocketIOBuffer|, r=kmachulis
    This patch moves memory management of |UnixSocketIOBuffer| into
    |UnixSocketBuffer| and extends |UnixSocketIOBuffer| with send and
    receive interfaces. The class is now the base for all socket-buffer
    classes, such as |UnixSocketRawData| and |BluetoothDaemonPDU|.

--- ipc/unixsocket/SocketBase.h
+++ ipc/unixsocket/SocketBase.h
@@ -23,12 +23,18 @@ namespace mozilla {
 namespace ipc {
 
 //
-// UnixSocketIOBuffer
+// UnixSocketBuffer
 //
 
-class UnixSocketIOBuffer
+/**
+ * |UnixSocketBuffer| implements a FIFO buffer that stores raw socket
+ * data, either for sending on a socket or received from a socket.
+ */
+class UnixSocketBuffer
 {
 public:
+  virtual ~UnixSocketBuffer();
+
   const uint8_t* GetData() const
   {
     return mData + mOffset;
@@ -110,15 +116,13 @@ public:
 protected:
 
   /* This constructor copies aData of aSize bytes length into the
-   * new instance of |UnixSocketIOBuffer|.
+   * new instance of |UnixSocketBuffer|.
    */
-  UnixSocketIOBuffer(const void* aData, size_t aSize);
+  UnixSocketBuffer(const void* aData, size_t aSize);
 
   /* This constructor reserves aAvailableSpace bytes of space.
    */
-  UnixSocketIOBuffer(size_t aAvailableSpace);
-
-  ~UnixSocketIOBuffer();
+  UnixSocketBuffer(size_t aAvailableSpace);
 
   size_t GetLeadingSpace() const
   {
@@ -165,6 +169,46 @@ private:
 };
 
 //
+// UnixSocketIOBuffer
+//
+
+/**
+ * |UnixSocketIOBuffer| is a |UnixSocketBuffer| that supports being
+ * received on a socket or being send on a socket. Network protocols
+ * might differ in their exact usage of Unix socket functions and
+ * |UnixSocketIOBuffer| provides a protocol-neutral interface.
+ */
+class UnixSocketIOBuffer : public UnixSocketBuffer
+{
+public:
+  virtual ~UnixSocketIOBuffer();
+
+  /**
+   * Receives data from aFd at the end of the buffer. The returned value
+   * is the number of newly received bytes, or 0 if the peer shut down
+   * its connection, or a negative value on errors.
+   */
+  virtual ssize_t Receive(int aFd) = 0;
+
+  /**
+   * Sends data to aFd from the beginning of the buffer. The returned value
+   * is the number of bytes written, or a negative value on error.
+   */
+  virtual ssize_t Send(int aFd) = 0;
+
+protected:
+
+  /* This constructor copies aData of aSize bytes length into the
+   * new instance of |UnixSocketIOBuffer|.
+   */
+  UnixSocketIOBuffer(const void* aData, size_t aSize);
+
+  /* This constructor reserves aAvailableSpace bytes of space.
+   */
+  UnixSocketIOBuffer(size_t aAvailableSpace);
+};
+
+//
 // UnixSocketRawData
 //
 
@@ -186,13 +230,13 @@ public:
    * is the number of newly received bytes, or 0 if the peer shut down
    * its connection, or a negative value on errors.
    */
-  ssize_t Receive(int aFd);
+  ssize_t Receive(int aFd) override;
 
   /**
    * Sends data to aFd from the beginning of the buffer. The returned value
    * is the number of bytes written, or a negative value on error.
    */
-  ssize_t Send(int aFd);
+  ssize_t Send(int aFd) override;
 };
 
 enum SocketConnectionStatus {
