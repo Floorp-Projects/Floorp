edf1f6bd6f6be6bee3be651c641fee8dba130e57
Fabrice Desré <fabrice@mozilla.com>
2015-04-23 13:35:49 -0700

    Bug 1144600 - Don't crash when submitting <keygen> on b2g r=dkeeler

--- security/manager/pki/src/nsNSSDialogHelper.cpp
+++ security/manager/pki/src/nsNSSDialogHelper.cpp
@@ -23,6 +23,12 @@ nsNSSDialogHelper::openDialog(
     nsISupports *params,
     bool modal)
 {
+#ifdef MOZ_WIDGET_GONK
+  // On b2g devices, we need to proxy the dialog creation & management
+  // to Gaia.
+  return NS_ERROR_NOT_IMPLEMENTED;
+#endif
+
   nsresult rv;
   nsCOMPtr<nsIWindowWatcher> windowWatcher = 
            do_GetService(NS_WINDOWWATCHER_CONTRACTID, &rv);
