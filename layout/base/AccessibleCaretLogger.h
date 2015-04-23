0705ea40267a4ae94ebca843248c983e5e8b91c1
Ting-Yu Lin <tlin@mozilla.com>
2015-04-24 01:52:00 +0200

    Bug 1110039 - Part 2.1 - Add logger facility. r=roc

---
+++ layout/base/AccessibleCaretLogger.h
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef AccessibleCaretLog_h
+#define AccessibleCaretLog_h
+
+#include "prlog.h"
+
+namespace mozilla {
+#ifdef PR_LOGGING
+
+PRLogModuleInfo* GetAccessibleCaretLog();
+
+#ifndef AC_LOG_BASE
+#define AC_LOG_BASE(...) PR_LOG(GetAccessibleCaretLog(), PR_LOG_DEBUG, (__VA_ARGS__));
+#endif
+
+#ifndef AC_LOGV_BASE
+#define AC_LOGV_BASE(...)                                                      \
+  PR_LOG(GetAccessibleCaretLog(), PR_LOG_DEBUG + 1, (__VA_ARGS__));
+#endif
+
+#else
+
+#ifndef AC_LOGV_BASE
+#define AC_LOGV_BASE(...)
+#endif
+
+#ifndef AC_LOGV_BASE
+#define AC_LOGV_BASE(...)
+#endif
+
+#endif // PR_LOGGING
+
+} // namespace mozilla
+
+#endif // AccessibleCaretLog_h
