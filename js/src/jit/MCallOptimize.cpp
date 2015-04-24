0044a67ae08f4769d3eea9dcfde5732d16ebbcc1
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2015-04-24 16:25:41 -0400

    Bug 1149119 - Use Atoms in the template object hold by Baseline. r=jandem, a=abillings

--- js/src/jit/MCallOptimize.cpp
+++ js/src/jit/MCallOptimize.cpp
@@ -1420,7 +1420,9 @@ IonBuilder::inlineConstantStringSplit(CallInfo& callInfo)
 
     Vector<MConstant*, 0, SystemAllocPolicy> arrayValues;
     for (uint32_t i = 0; i < initLength; i++) {
-        MConstant* value = MConstant::New(alloc(), templateObject->getDenseElement(i), constraints());
+        Value str = templateObject->getDenseElement(i);
+        MOZ_ASSERT(str.toString()->isAtom());
+        MConstant* value = MConstant::New(alloc(), str, constraints());
         if (!TypeSetIncludes(key.maybeTypes(), value->type(), value->resultTypeSet()))
             return InliningStatus_NotInlined;
 
