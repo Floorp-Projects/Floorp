c21c932ca9a384a4c40e639d51534df6eba533cf
Shu-yu Guo <shu@rfrn.org>
2015-04-26 00:00:09 -0700

    Bug 1157963 - Fix LazyScript::hasUncompiledEnclosingScript and don't delazify functions about to be finalized. r=jimb, a=lizzard

--- js/src/jit-test/tests/debug/bug980585.js
+++ js/src/jit-test/tests/debug/bug980585.js
@@ -2,7 +2,7 @@ var g = newGlobal();
 var dbg = new Debugger(g);
 
 try {
-  g.eval("function f() { var array = ['a', 'b']; [1].map(function () {}); return {array}; }");
+  g.eval("function f() { [1].map(function () {}); const x = 42; x = 43; } f();");
 } catch (e) {
   // Ignore the syntax error.
 }
