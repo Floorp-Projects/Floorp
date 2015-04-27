33bd6f43b17f491cffe0d42e36dc6684f293e046
Ryan VanderMeulen <ryanvm@gmail.com>
2015-04-27 14:04:24 -0400

    Backed out changeset c839f283bf28 (bug 1058695) for bustage. CLOSED TREE

--- dom/promise/Promise.cpp
+++ dom/promise/Promise.cpp
@@ -263,7 +263,7 @@ protected:
       // console though, for debugging.
     }
 
-    return rv.ErrorCode();
+    return NS_OK;
   }
 
 private:
@@ -489,24 +489,13 @@ Promise::PerformMicroTaskCheckpoint()
     return false;
   }
 
-  Maybe<AutoSafeJSContext> cx;
-  if (NS_IsMainThread()) {
-    cx.emplace();
-  }
-
   do {
     nsCOMPtr<nsIRunnable> runnable = microtaskQueue.ElementAt(0);
     MOZ_ASSERT(runnable);
 
     // This function can re-enter, so we remove the element before calling.
     microtaskQueue.RemoveElementAt(0);
-    nsresult rv = runnable->Run();
-    if (NS_WARN_IF(NS_FAILED(rv))) {
-      return false;
-    }
-    if (cx.isSome()) {
-      JS_CheckForInterrupt(cx.ref());
-    }
+    runnable->Run();
   } while (!microtaskQueue.IsEmpty());
 
   return true;
@@ -1082,10 +1071,6 @@ void
 Promise::AppendCallbacks(PromiseCallback* aResolveCallback,
                          PromiseCallback* aRejectCallback)
 {
-  if (mGlobal->IsDying()) {
-    return;
-  }
-
   MOZ_ASSERT(aResolveCallback);
   MOZ_ASSERT(aRejectCallback);
 
@@ -1312,12 +1297,7 @@ Promise::RejectInternal(JSContext* aCx,
 void
 Promise::Settle(JS::Handle<JS::Value> aValue, PromiseState aState)
 {
-  if (mGlobal->IsDying()) {
-    return;
-  }
-
   mSettlementTimestamp = TimeStamp::Now();
-
   SetResult(aValue);
   SetState(aState);
 
