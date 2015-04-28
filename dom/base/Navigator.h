6934a655d5bcdeb94eb42c8b857d9407c2af09be
Boris Zbarsky <bzbarsky@mit.edu>
2015-04-28 12:25:55 -0400

    Bug 1155946 part 2.  Add mayResolve methods to DOM classes with resolve hooks.  r=peterv

--- dom/base/Navigator.h
+++ dom/base/Navigator.h
@@ -281,6 +281,9 @@ public:
   bool DoResolve(JSContext* aCx, JS::Handle<JSObject*> aObject,
                  JS::Handle<jsid> aId,
                  JS::MutableHandle<JSPropertyDescriptor> aDesc);
+  // The return value is whether DoResolve might end up resolving the given id.
+  // If in doubt, return true.
+  static bool MayResolve(jsid aId);
   void GetOwnPropertyNames(JSContext* aCx, nsTArray<nsString>& aNames,
                            ErrorResult& aRv);
   void GetLanguages(nsTArray<nsString>& aLanguages);
