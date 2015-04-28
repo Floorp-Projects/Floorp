305cf394a206ea67a55f99ac58d5765f41579686
Shu-yu Guo <shu@rfrn.org>
2015-04-28 01:44:22 -0700

    Bug 1158407 - Stop using this one weird allocation fallback for MCreateThisWithTemplate. (r=terrence)

--- js/src/jit/VMFunctions.h
+++ js/src/jit/VMFunctions.h
@@ -632,8 +632,6 @@ class AutoDetectInvalidation
 };
 
 bool InvokeFunction(JSContext* cx, HandleObject obj0, uint32_t argc, Value* argv, Value* rval);
-JSObject* NewGCObject(JSContext* cx, gc::AllocKind allocKind, gc::InitialHeap initialHeap,
-                      size_t ndynamic, const js::Class* clasp);
 
 bool CheckOverRecursed(JSContext* cx);
 bool CheckOverRecursedWithExtra(JSContext* cx, BaselineFrame* frame,
