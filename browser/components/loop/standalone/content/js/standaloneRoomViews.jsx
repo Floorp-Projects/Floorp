200457994dcf9bc544c4c6859e3fe8a29710c58e
Mark Banner <standard8@mozilla.com>
2015-04-28 08:17:56 +0100

    Bug 1158800 - Don't show errors for context in conversations on the standalone UI as they may force the user into an unnecessary re-obtaining of the URL. r=dmose

--- browser/components/loop/standalone/content/js/standaloneRoomViews.jsx
+++ browser/components/loop/standalone/content/js/standaloneRoomViews.jsx
@@ -280,15 +280,28 @@ loop.standaloneRoomViews = (function(mozL10n) {
       roomInfoFailure: React.PropTypes.string
     },
 
+    getInitialState: function() {
+      return {
+        failureLogged: false
+      }
+    },
+
+    _logFailure: function(message) {
+      if (!this.state.failureLogged) {
+        console.error(mozL10n.get(message));
+        this.state.failureLogged = true;
+      }
+    },
+
     render: function() {
+      // For failures, we currently just log the messages - UX doesn't want them
+      // displayed on primary UI at the moment.
       if (this.props.roomInfoFailure === ROOM_INFO_FAILURES.WEB_CRYPTO_UNSUPPORTED) {
-        return (<h2 className="room-info-failure">
-          {mozL10n.get("room_information_failure_unsupported_browser")}
-        </h2>);
+        this._logFailure("room_information_failure_unsupported_browser");
+        return null;
       } else if (this.props.roomInfoFailure) {
-        return (<h2 className="room-info-failure">
-          {mozL10n.get("room_information_failure_not_available")}
-        </h2>);
+        this._logFailure("room_information_failure_not_available");
+        return null;
       }
 
       // We only support one item in the context Urls array for now.
