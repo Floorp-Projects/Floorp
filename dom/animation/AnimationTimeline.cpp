6bb747268eef9081b7511d70e0b2aef075f36313
Brian Birtles <birtles@gmail.com>
2015-04-28 16:41:09 +0900

    Bug 1150810 part 8 - Add AnimationTimeline::AddAnimations/RemoveAnimations; r=jwatt
    --HG--
    extra : commitid : 2zvcGrXcqJR
    extra : rebase_source : 791f417a3688cbdd9bd4c9cd24c46551b0d775f5

--- dom/animation/AnimationTimeline.cpp
+++ dom/animation/AnimationTimeline.cpp
@@ -9,7 +9,7 @@
 namespace mozilla {
 namespace dom {
 
-NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(AnimationTimeline, mWindow)
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(AnimationTimeline, mWindow, mAnimations)
 
 NS_IMPL_CYCLE_COLLECTING_ADDREF(AnimationTimeline)
 NS_IMPL_CYCLE_COLLECTING_RELEASE(AnimationTimeline)
@@ -18,6 +18,18 @@ NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(AnimationTimeline)
   NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
   NS_INTERFACE_MAP_ENTRY(nsISupports)
 NS_INTERFACE_MAP_END
+
+void
+AnimationTimeline::AddAnimation(Animation& aAnimation)
+{
+  mAnimations.PutEntry(&aAnimation);
+}
+
+void
+AnimationTimeline::RemoveAnimation(Animation& aAnimation)
+{
+  mAnimations.RemoveEntry(&aAnimation);
+}
 
 } // namespace dom
 } // namespace mozilla
