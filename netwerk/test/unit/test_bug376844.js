ff4abdaae2397fb91d6d82d23d505f091143ccbf
Valentin Gosu <valentin.gosu@gmail.com>
2015-04-28 18:29:22 +0300

    Bug 1040285 - Single Quotes should not be encoded in the path r=mcmanus,annevk

--- netwerk/test/unit/test_bug376844.js
+++ netwerk/test/unit/test_bug376844.js
@@ -1,7 +1,7 @@
 const testURLs = [
   ["http://example.com/<", "http://example.com/%3C"],
   ["http://example.com/>", "http://example.com/%3E"],
-  ["http://example.com/'", "http://example.com/%27"],
+  ["http://example.com/'", "http://example.com/'"],
   ["http://example.com/\"", "http://example.com/%22"],
   ["http://example.com/?<", "http://example.com/?%3C"],
   ["http://example.com/?>", "http://example.com/?%3E"],
