fb6534e52ff0b8e829cdea9dd0b4cc5e2b2c4ee9
ProgramFOX <programfox@hotmail.be>
2015-04-29 16:56:53 +0200

    Bug 1153602 - SIMD (interpreter): Added more test cases for ReciprocalSqrtApproximation. r=Waldo
    --HG--
    extra : rebase_source : ec9c31faf47304287c9988934266613298dfd6b3
    extra : histedit_source : d4592bc8fd6d2abcd2ce7c363d2b36dc1c4641e0

--- js/src/tests/ecma_7/SIMD/float64x2-arithmetic.js
+++ js/src/tests/ecma_7/SIMD/float64x2-arithmetic.js
@@ -52,7 +52,8 @@ function test() {
   for ([v, w] of [[float64x2(1, 2), float64x2(3, 4)],
                   [float64x2(1.894, 2.8909), float64x2(100.764, 200.987)],
                   [float64x2(-1, -2), float64x2(-14.54, 57)],
-                  [float64x2(+Infinity, -Infinity), float64x2(NaN, -0)]])
+                  [float64x2(+Infinity, -Infinity), float64x2(NaN, -0)],
+                  [float64x2(Math.pow(2, 31), Math.pow(2, -31)), float64x2(Math.pow(2, -1047), Math.pow(2, -149))]])
   {
       testAdd(v, w);
       testSub(v, w);
