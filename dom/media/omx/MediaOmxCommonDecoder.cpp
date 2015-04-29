505cd512eb5896f068cc465f56e3c1b3952f64a8
Sotaro Ikeda <sikeda@mozilla.com>
2015-04-29 08:36:49 -0700

    Bug 1158293 - Fix ResumeStateMachine()'s seek handling r=cpearce

--- dom/media/omx/MediaOmxCommonDecoder.cpp
+++ dom/media/omx/MediaOmxCommonDecoder.cpp
@@ -143,10 +143,19 @@ MediaOmxCommonDecoder::ResumeStateMachine()
   mRequestedSeekTarget = SeekTarget(timeUsecs,
                                     SeekTarget::Accurate,
                                     MediaDecoderEventVisibility::Suppressed);
+  // Call Seek of MediaDecoderStateMachine to suppress seek events.
+  RefPtr<nsRunnable> event =
+    NS_NewRunnableMethodWithArg<SeekTarget>(
+      GetStateMachine(),
+      &MediaDecoderStateMachine::Seek,
+      mRequestedSeekTarget);
+  GetStateMachine()->TaskQueue()->Dispatch(event);
+  mRequestedSeekTarget.Reset();
+
   mNextState = mPlayState;
   ChangeState(PLAY_STATE_LOADING);
   // exit dormant state
-  RefPtr<nsRunnable> event =
+  event =
     NS_NewRunnableMethodWithArg<bool>(
       GetStateMachine(),
       &MediaDecoderStateMachine::SetDormant,
