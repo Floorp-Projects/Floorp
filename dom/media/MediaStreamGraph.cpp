8b5867bcd77ee7f7c69a64fce2f6f11c074f8d83
Paul Adenot <paul@paul.cx>
2015-04-29 11:02:55 +0200

    Bug 1158427 - r=roc

--- dom/media/MediaStreamGraph.cpp
+++ dom/media/MediaStreamGraph.cpp
@@ -1869,9 +1869,9 @@ void
 MediaStreamGraphImpl::AppendMessage(ControlMessage* aMessage)
 {
   MOZ_ASSERT(NS_IsMainThread(), "main thread only");
-  NS_ASSERTION(!aMessage->GetStream() ||
-               !aMessage->GetStream()->IsDestroyed(),
-               "Stream already destroyed");
+  MOZ_ASSERT(!aMessage->GetStream() ||
+             !aMessage->GetStream()->IsDestroyed(),
+             "Stream already destroyed");
 
   if (mDetectedNotRunning &&
       mLifecycleState > LIFECYCLE_WAITING_FOR_MAIN_THREAD_CLEANUP) {
@@ -3207,6 +3207,8 @@ private:
 void
 MediaStreamGraph::NotifyWhenGraphStarted(AudioNodeStream* aStream)
 {
+  MOZ_ASSERT(NS_IsMainThread());
+
   class GraphStartedNotificationControlMessage : public ControlMessage
   {
   public:
@@ -3237,8 +3239,10 @@ MediaStreamGraph::NotifyWhenGraphStarted(AudioNodeStream* aStream)
     }
   };
 
-  MediaStreamGraphImpl* graphImpl = static_cast<MediaStreamGraphImpl*>(this);
-  graphImpl->AppendMessage(new GraphStartedNotificationControlMessage(aStream));
+  if (!aStream->IsDestroyed()) {
+    MediaStreamGraphImpl* graphImpl = static_cast<MediaStreamGraphImpl*>(this);
+    graphImpl->AppendMessage(new GraphStartedNotificationControlMessage(aStream));
+  }
 }
 
 void
