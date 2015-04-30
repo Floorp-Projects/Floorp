e7621cdf365a3158c279257932503e2dd0bee5c3
Marco Zehe <mzehe@mozilla.com>
2015-04-30 10:26:00 -0400

    Bug 1159872 - Make IAccessible2::Get_States once again return S_OK when it detects a defunct accessible. r=surkov CLOSED TREE
    --HG--
    extra : rebase_source : 9071ac95f0a4ffd75b77a485223f843eba1155d5
    extra : histedit_source : 34e050128803d0edd45e2359c45cf90c097f6b60

--- accessible/windows/ia2/ia2Accessible.cpp
+++ accessible/windows/ia2/ia2Accessible.cpp
@@ -354,7 +354,7 @@ ia2Accessible::get_states(AccessibleStates* aStates)
   AccessibleWrap* acc = static_cast<AccessibleWrap*>(this);
   if (acc->IsDefunct()) {
     *aStates = IA2_STATE_DEFUNCT;
-    return CO_E_OBJNOTCONNECTED;
+    return S_OK;
   }
 
   uint64_t state;
