fab732ebbd6ba4e8fb9ab3b85feb0721a0e287ae
Boris Zbarsky <bzbarsky@mit.edu>
2015-04-30 17:36:50 -0400

    Bug 1160311.  JITs should be enabled by default.  r=jorendorff

--- js/src/jsapi-tests/testChromeBuffer.cpp
+++ js/src/jsapi-tests/testChromeBuffer.cpp
@@ -63,6 +63,9 @@ BEGIN_TEST(testChromeBuffer)
      * buffer space.
      */
     {
+        JSRuntimeOptions oldOptions = JS::RuntimeOptionsRef(rt);
+        // Disable the JIT because if we don't this test fails.  See bug 1160414.
+        JS::RuntimeOptionsRef(rt).setIon(false).setBaseline(false);
         {
             JSAutoCompartment ac(cx, trusted_glob);
             const char* paramName = "x";
@@ -99,6 +102,7 @@ BEGIN_TEST(testChromeBuffer)
         JS::RootedValue rval(cx);
         CHECK(JS_CallFunction(cx, JS::NullPtr(), fun, JS::HandleValueArray(v), &rval));
         CHECK(rval.toInt32() == 100);
+        JS::RuntimeOptionsRef(rt) = oldOptions;
     }
 
     /*
