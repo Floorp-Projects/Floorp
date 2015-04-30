18c822b0d534111205f2c3b13b24caf90acd7426
Wander Lairson Costa <wcosta@mozilla.com>
2015-04-30 16:26:38 -0300

    Bug 1154947 part 3: Add aries nightly user, userdebug and eng builds.

--- testing/taskcluster/scripts/phone-builder/build-lightsaber.sh
+++ testing/taskcluster/scripts/phone-builder/build-lightsaber.sh
@@ -2,19 +2,6 @@
 
 . pre-build.sh
 
-if [ 0$B2G_DEBUG -ne 0 ]; then
-    DEBUG_SUFFIX=-debug
-fi
-
-if [ $TARGET == "aries" -o $TARGET == "shinano" ]; then
-  # caching objects might be dangerous for some devices (aka aries)
-  rm -rf $WORKSPACE/B2G/objdir*
-  rm -rf $WORKSPACE/B2G/out
-fi
-
-aws s3 cp s3://b2g-nightly-credentials/balrog_credentials .
-mar_file=b2g-${TARGET%%-*}-gecko-update.mar
-
 # We need different platform names for each variant (user, userdebug and
 # eng). We do not append variant suffix for "user" to keep compability with
 # verions already installed in the phones.
@@ -25,8 +12,7 @@ else
 fi
 
 ./mozharness/scripts/b2g_lightsaber.py \
-  --config b2g/taskcluster-lightsaber-nightly.py \
-  --config balrog/staging.py \
+  --config b2g/taskcluster-lightsaber.py \
   "$debug_flag" \
   --disable-mock \
   --variant=$VARIANT \
@@ -37,22 +23,18 @@ fi
   --b2g-config-dir=$TARGET \
   --checkout-revision=$GECKO_HEAD_REV \
   --base-repo=$GECKO_BASE_REPOSITORY \
-  --repo=$GECKO_HEAD_REPOSITORY \
-  --platform $PLATFORM \
-  --complete-mar-url https://queue.taskcluster.net/v1/task/$TASK_ID/runs/$RUN_ID/artifacts/public/build/$mar_file \
+  --repo=$GECKO_HEAD_REPOSITORY
 
 # Don't cache backups
 rm -rf $WORKSPACE/B2G/backup-*
-rm -f balrog_credentials
 
+# Move files into artifact locations!
 mkdir -p $HOME/artifacts
-mkdir -p $HOME/artifacts-public
 
-mv $WORKSPACE/B2G/upload-public/$mar_file $HOME/artifacts-public/
 mv $WORKSPACE/B2G/upload/sources.xml $HOME/artifacts/sources.xml
-#mv $WORKSPACE/B2G/upload/b2g-*.crashreporter-symbols.zip $HOME/artifacts/b2g-crashreporter-symbols.zip
+mv $WORKSPACE/B2G/upload/b2g-*.crashreporter-symbols.zip $HOME/artifacts/b2g-crashreporter-symbols.zip
 mv $WORKSPACE/B2G/upload/b2g-*.android-arm.tar.gz $HOME/artifacts/b2g-android-arm.tar.gz
 mv $WORKSPACE/B2G/upload/${TARGET}.zip $HOME/artifacts/${TARGET}.zip
 mv $WORKSPACE/B2G/upload/gaia.zip $HOME/artifacts/gaia.zip
-ccache -s
 
+ccache -s
