aa57a0c5ac1af4fd899cdaee0c3611b2e5e80d38
Michael Comella <michael.l.comella@gmail.com>
2015-04-30 16:09:47 -0700

    Bug 840574 - Clear the selected tab's applicationId on user action. r=margaret

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -2234,6 +2234,14 @@ public class BrowserApp extends GeckoApp
         // Expected to be fixed by bug 915825.
         hideHomePager(url);
         loadUrlOrKeywordSearch(url);
+        clearSelectedTabApplicationId();
+    }
+
+    private void clearSelectedTabApplicationId() {
+        final Tab selected = Tabs.getInstance().getSelectedTab();
+        if (selected != null) {
+            selected.setApplicationId(null);
+        }
     }
 
     private void loadUrlOrKeywordSearch(final String url) {
@@ -3572,6 +3580,7 @@ public class BrowserApp extends GeckoApp
             startActivity(intent);
         } else if (!maybeSwitchToTab(url, flags)) {
             openUrlAndStopEditing(url);
+            clearSelectedTabApplicationId();
         }
     }
 
