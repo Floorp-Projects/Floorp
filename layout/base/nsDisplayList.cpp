a606228cd87fec709c92400e4961de73bd3e9786
Brian Birtles <birtles@gmail.com>
2015-04-30 09:06:43 -0400

    Bug 1159082 - Rename *Readonly animation interfaces to *ReadOnly. r=smaug

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -426,7 +426,7 @@ AddAnimationsForProperty(nsIFrame* aFrame, nsCSSProperty aProperty,
     if (!anim->IsPlaying()) {
       continue;
     }
-    dom::KeyframeEffectReadonly* effect = anim->GetEffect();
+    dom::KeyframeEffectReadOnly* effect = anim->GetEffect();
     MOZ_ASSERT(effect, "A playing animation should have an effect");
     const AnimationProperty* property =
       effect->GetAnimationOfProperty(aProperty);
