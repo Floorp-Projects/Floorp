8c2f9c9b09dff69337527fefc5ef60fe00c0adbb
Ryan VanderMeulen <ryanvm@gmail.com>
2015-04-30 16:08:29 -0400

    Backed out changeset 31297813dd26 (bug 1159327) for bustage on a CLOSED TREE.

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -105,7 +105,6 @@
 
 #ifdef ACCESSIBILITY
 #include "nsAccessibilityService.h"
-#include "mozilla/a11y/Compatibility.h"
 #endif
 
 #include "nsCRT.h"
@@ -860,20 +859,17 @@ nsXULAppInfo::GetAccessibilityEnabled(bool* aResult)
 }
 
 NS_IMETHODIMP
-nsXULAppInfo::GetAccessibilityIsBlacklistedForE10S(bool* aResult)
+nsXULAppInfo::GetAccessibilityIsUIA(bool* aResult)
 {
   *aResult = false;
-#if defined(ACCESSIBILITY)
-#if defined(XP_WIN)
-  if (GetAccService() && mozilla::a11y::Compatibility::IsBlacklistedForE10S()) {
-    *aResult = true;
-  }
-#elif defined(XP_MACOSX)
-  if (GetAccService()) {
+#if defined(ACCESSIBILITY) && defined(XP_WIN)
+  // This is the same check the a11y service does to identify uia clients.
+  if (GetAccService() != nullptr &&
+      (::GetModuleHandleW(L"uiautomation") ||
+       ::GetModuleHandleW(L"uiautomationcore"))) {
     *aResult = true;
   }
 #endif
-#endif // defined(ACCESSIBILITY)
   return NS_OK;
 }
 
