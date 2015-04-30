8ef8e986c9a464aaa9585caae37782e2c275dbfe
Nick Alexander <nalexander@mozilla.com>
2015-04-30 14:28:14 -0700

    Bug 1160324 - Handle missing robocopApk files in runtestsremote.py. r=gbrown

--- testing/mozbase/mozlog/mozlog/structured/commandline.py
+++ testing/mozbase/mozlog/mozlog/structured/commandline.py
@@ -179,6 +179,8 @@ def setup_logging(suite, args, defaults=None):
     formatters = defaultdict(list)
     found = False
     found_stdout_logger = False
+    if args is None:
+        args = {}
     if not hasattr(args, 'iteritems'):
         args = vars(args)
 
