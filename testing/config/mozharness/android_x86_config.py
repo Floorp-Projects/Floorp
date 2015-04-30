3cd8444fdf4dd7cc979268b5d0c5c9d4998a654f
Andrew Halberstadt <ahalberstadt@mozilla.com>
2015-04-30 13:47:01 -0400

    Bug 1155338 - Move mach command arguments into mochitest harness, r=chmanchester,gbrown

--- testing/config/mozharness/android_x86_config.py
+++ testing/config/mozharness/android_x86_config.py
@@ -6,8 +6,7 @@ config = {
     "suite_definitions": {
         "mochitest": {
             "run_filename": "runtestsremote.py",
-            "options": ["--autorun", "--close-when-done", "--dm_trans=sut",
-                "--console-level=INFO", "--app=%(app)s", "--remote-webserver=%(remote_webserver)s",
+            "options": ["--dm_trans=sut", "--app=%(app)s", "--remote-webserver=%(remote_webserver)s",
                 "--xre-path=%(xre_path)s", "--utility-path=%(utility_path)s",
                 "--deviceIP=%(device_ip)s", "--devicePort=%(device_port)s",
                 "--http-port=%(http_port)s", "--ssl-port=%(ssl_port)s",
