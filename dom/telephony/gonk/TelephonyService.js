6541341e3b061e3f17df083e37f1b1bcb25d4b9b
Edgar Chen <echen@mozilla.com>
2015-04-30 12:35:55 +0800

    Bug 1159591 - Part 7: Move MMI logic from ril_worker to telephonyService (Call Barring Password). r=aknow

--- dom/telephony/gonk/TelephonyService.js
+++ dom/telephony/gonk/TelephonyService.js
@@ -891,6 +891,11 @@ TelephonyService.prototype = {
         this._clirMMI(aClientId, aMmi, aCallback);
         break;
 
+      // Change call barring password
+      case RIL.MMI_KS_SC_CHANGE_PASSWORD:
+        this._callBarringPasswordMMI(aClientId, aMmi, aCallback);
+        break;
+
       // Fall back to "sendMMI".
       default:
         this._sendMMI(aClientId, aMmi, aCallback);
@@ -1254,6 +1259,57 @@ TelephonyService.prototype = {
         aCallback.notifyDialMMIError(RIL.MMI_ERROR_KS_NOT_SUPPORTED);
         break;
     }
+  },
+
+  /**
+   * Handle change call barring password MMI code.
+   *
+   * @param aClientId
+   *        Client id.
+   * @param aMmi
+   *        Parsed MMI structure.
+   * @param aCallback
+   *        A nsITelephonyDialCallback object.
+   */
+  _callBarringPasswordMMI: function(aClientId, aMmi, aCallback) {
+    if (!this._isRadioOn(aClientId)) {
+      aCallback.notifyDialMMIError(RIL.GECKO_ERROR_RADIO_NOT_AVAILABLE);
+      return;
+    }
+
+    if (aMmi.procedure !== RIL.MMI_PROCEDURE_REGISTRATION &&
+        aMmi.procedure !== RIL.MMI_PROCEDURE_ACTIVATION) {
+      aCallback.notifyDialMMIError(RIL.MMI_ERROR_KS_INVALID_ACTION);
+      return;
+    }
+
+    if (aMmi.sia !== "" && aMmi.sia !== RIL.MMI_ZZ_BARRING_SERVICE) {
+      aCallback.notifyDialMMIError(RIL.MMI_ERROR_KS_NOT_SUPPORTED);
+      return;
+    }
+
+    let validPassword = aSi => /^[0-9]{4}$/.test(aSi);
+    if (!validPassword(aMmi.sib) || !validPassword(aMmi.sic) ||
+        !validPassword(aMmi.pwd)) {
+      aCallback.notifyDialMMIError(RIL.MMI_ERROR_KS_INVALID_PASSWORD);
+      return;
+    }
+
+    if (aMmi.sic !== aMmi.pwd) {
+      aCallback.notifyDialMMIError(RIL.MMI_ERROR_KS_MISMATCH_PASSWORD);
+      return;
+    }
+
+    let connection = gGonkMobileConnectionService.getItemByServiceId(aClientId);
+    connection.changeCallBarringPassword(aMmi.sib, aMmi.sic, {
+      QueryInterface: XPCOMUtils.generateQI([Ci.nsIMobileConnectionCallback]),
+      notifySuccess: function() {
+        aCallback.notifyDialMMISuccess(RIL.MMI_SM_KS_PASSWORD_CHANGED);
+      },
+      notifyError: function(aErrorMsg) {
+        aCallback.notifyDialMMIError(aErrorMsg);
+      },
+    });
   },
 
   _serviceCodeToKeyString: function(aServiceCode) {
