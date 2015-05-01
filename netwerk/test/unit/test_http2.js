33886a72bcd8a7d8a46b0d477f6860a8e7c9c969
Patrick McManus <mcmanus@ducksong.com>
2015-04-30 20:53:20 -0400

    Bug 1159944 - more alt-svc tests r=hurley

--- netwerk/test/unit/test_http2.js
+++ netwerk/test/unit/test_http2.js
@@ -318,7 +318,7 @@ function checkXhr(xhr) {
   do_test_finished();
 }
 
-// Fires off an XHR request over SPDY
+// Fires off an XHR request over h2
 function test_http2_xhr() {
   var req = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"]
             .createInstance(Ci.nsIXMLHttpRequest);
@@ -884,7 +884,7 @@ function run_test() {
   do_check_neq(serverPort, null);
   dump("using port " + serverPort + "\n");
 
-  // Set to allow the cert presented by our SPDY server
+  // Set to allow the cert presented by our H2 server
   do_get_profile();
   prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
   var oldPref = prefs.getIntPref("network.http.speculative-parallel-limit");
