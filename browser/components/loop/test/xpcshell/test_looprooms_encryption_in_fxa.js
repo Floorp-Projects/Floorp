887d95a4a7057454601ec1928c48654c2d5991f1
Mark Banner <standard8@mozilla.com>
2015-05-01 13:41:38 +0100

    Bug 1153806 - Enable encryption of Loop room context data. r=mikedeboer

--- browser/components/loop/test/xpcshell/test_looprooms_encryption_in_fxa.js
+++ browser/components/loop/test/xpcshell/test_looprooms_encryption_in_fxa.js
@@ -7,8 +7,6 @@
 Cu.import("resource://services-common/utils.js");
 const { LOOP_ROOMS_CACHE_FILENAME } = Cu.import("resource:///modules/loop/LoopRoomsCache.jsm", {});
 
-const kContextEnabledPref = "loop.contextInConverations.enabled";
-
 const kFxAKey = "uGIs-kGbYt1hBBwjyW7MLQ";
 
 // Rooms details as responded by the server.
@@ -251,14 +249,12 @@ function run_test() {
   setupFakeLoopServer();
 
   Services.prefs.setCharPref("loop.key.fxa", kFxAKey);
-  Services.prefs.setBoolPref(kContextEnabledPref, true);
 
   // Pretend we're signed into FxA.
   MozLoopServiceInternal.fxAOAuthTokenData = { token_type: "bearer" };
   MozLoopServiceInternal.fxAOAuthProfile = { email: "fake@invalid.com" };
 
   do_register_cleanup(function () {
-    Services.prefs.clearUserPref(kContextEnabledPref);
     Services.prefs.clearUserPref("loop.key.fxa");
 
     MozLoopServiceInternal.fxAOAuthTokenData = null;
