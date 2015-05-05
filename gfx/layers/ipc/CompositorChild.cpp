4d2342ddaea5044fa40573f80190fdf3fe045bfe
Kan-Ru Chen <kanru@kanru.info>
2015-05-05 14:32:47 +0800

    Bug 1154231 - Part 1. Use LRU to manage remote layers. r=mattwoodrow

--- gfx/layers/ipc/CompositorChild.cpp
+++ gfx/layers/ipc/CompositorChild.cpp
@@ -372,6 +372,16 @@ CompositorChild::AddOverfillObserver(ClientLayerManager* aLayerManager)
   mOverfillObservers.AppendElement(aLayerManager);
 }
 
+bool
+CompositorChild::RecvClearCachedResources(const uint64_t& aId)
+{
+  dom::TabChild* child = dom::TabChild::GetFrom(aId);
+  if (child) {
+    child->ClearCachedResources();
+  }
+  return true;
+}
+
 void
 CompositorChild::ActorDestroy(ActorDestroyReason aWhy)
 {
@@ -547,6 +557,26 @@ CompositorChild::SendResume()
     return true;
   }
   return PCompositorChild::SendResume();
+}
+
+bool
+CompositorChild::SendNotifyHidden(const uint64_t& id)
+{
+  MOZ_ASSERT(mCanSend);
+  if (!mCanSend) {
+    return true;
+  }
+  return PCompositorChild::SendNotifyHidden(id);
+}
+
+bool
+CompositorChild::SendNotifyVisible(const uint64_t& id)
+{
+  MOZ_ASSERT(mCanSend);
+  if (!mCanSend) {
+    return true;
+  }
+  return PCompositorChild::SendNotifyVisible(id);
 }
 
 bool
