2bd58bebb79d2877e8ab1853a1ee0cf6adc3f925
Jared Wein <jwein@mozilla.com>
2015-05-05 17:55:29 -0400

    Bug 1161654 - Import latest Pocket code. r=dolske

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1919,7 +1919,8 @@ pref("dom.serviceWorkers.enabled", true);
 #endif
 
 pref("browser.pocket.enabled", false);
-pref("browser.pocket.hostname", "localhost");
+pref("browser.pocket.api", "api.getpocket.com");
+pref("browser.pocket.site", "getpocket.com");
 pref("browser.pocket.oAuthConsumerKey", "40249-e88c401e1b1f2242d9e441c4");
 pref("browser.pocket.useLocaleList", true);
 pref("browser.pocket.enabledLocales", "en-US");
