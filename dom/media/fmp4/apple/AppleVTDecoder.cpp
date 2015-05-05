1dbbc64772c87754491fbaa1531e48b50515291f
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-05-06 09:58:09 +1000

    Bug 1154896: Report decoding errors back to MP4Reader. r=rillian a=lizzard
    --HG--
    extra : source : d9c414e61a09df49f0bf7b4bbe4d0f907ec818a9

--- dom/media/fmp4/apple/AppleVTDecoder.cpp
+++ dom/media/fmp4/apple/AppleVTDecoder.cpp
@@ -246,7 +246,9 @@ AppleVTDecoder::SubmitFrame(mp4_demuxer::MP4Sample* aSample)
                                          CreateAppleFrameRef(aSample),
                                          &infoFlags);
   if (rv != noErr && !(infoFlags & kVTDecodeInfo_FrameDropped)) {
+    LOG("AppleVTDecoder: Error %d VTDecompressionSessionDecodeFrame", rv);
     NS_WARNING("Couldn't pass frame to decoder");
+    mCallback->Error();
     return NS_ERROR_FAILURE;
   }
 
