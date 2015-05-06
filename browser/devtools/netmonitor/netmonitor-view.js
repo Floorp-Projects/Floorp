8e0f60c8dd579e6ab9e4082f3c64f5ef1fd3982b
Jan Keromnes <janx@linux.com>
2015-05-06 23:42:00 +0200

    Bug 1150717 - Implement "Copy URL Parameters" context menu item. r=bgrins

--- browser/devtools/netmonitor/netmonitor-view.js
+++ browser/devtools/netmonitor/netmonitor-view.js
@@ -567,6 +567,16 @@ RequestsMenuView.prototype = Heritage.extend(WidgetMethods, {
   },
 
   /**
+   * Copy the request url query string parameters from the currently selected item.
+   */
+  copyUrlParams: function() {
+    let selected = this.selectedItem.attachment;
+    let params = nsIURL(selected.url).query.split("&");
+    let string = params.join(Services.appinfo.OS === "WINNT" ? "\r\n" : "\n");
+    clipboardHelper.copyString(params.join("\n"), document);
+  },
+
+  /**
    * Copy a cURL command from the currently selected item.
    */
   copyAsCurl: function() {
@@ -1829,6 +1839,9 @@ RequestsMenuView.prototype = Heritage.extend(WidgetMethods, {
 
     let copyUrlElement = $("#request-menu-context-copy-url");
     copyUrlElement.hidden = !selectedItem;
+
+    let copyUrlParamsElement = $("#request-menu-context-copy-url-params");
+    copyUrlParamsElement.hidden = !selectedItem || !nsIURL(selectedItem.attachment.url).query;
 
     let copyAsCurlElement = $("#request-menu-context-copy-as-curl");
     copyAsCurlElement.hidden = !selectedItem || !selectedItem.attachment.responseContent;
