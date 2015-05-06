5e911272b0de648bda4beacfdf323c9f968c735b
Neil Deakin <neil@mozilla.com>
2015-05-06 13:17:45 -0400

    Bug 1093373 , e10s, fix and re-enable browser_bug647886 .js, r=gijs

--- browser/base/content/test/general/browser_bug647886.js
+++ browser/base/content/test/general/browser_bug647886.js
@@ -1,36 +1,25 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-function test() {
-  waitForExplicitFinish();
+add_task(function* () {
+  yield BrowserTestUtils.openNewForegroundTab(gBrowser, "http://example.com");
 
-  gBrowser.selectedTab = gBrowser.addTab();
-  gBrowser.selectedBrowser.addEventListener("load", function () {
-    gBrowser.selectedBrowser.removeEventListener("load", arguments.callee, true);
-
+  yield ContentTask.spawn(gBrowser.selectedBrowser, {}, function* () {
     content.history.pushState({}, "2", "2.html");
+  });
 
-    testBackButton();
-  }, true);
-
-  loadURI("http://example.com");
-}
-
-function testBackButton() {
   var backButton = document.getElementById("back-button");
   var rect = backButton.getBoundingClientRect();
 
   info("waiting for the history menu to open");
 
-  backButton.addEventListener("popupshown", function (event) {
-    backButton.removeEventListener("popupshown", arguments.callee, false);
-
-    ok(true, "history menu opened");
-    event.target.hidePopup();
-    gBrowser.removeTab(gBrowser.selectedTab);
-    finish();
-  }, false);
-
+  let popupShownPromise = BrowserTestUtils.waitForEvent(backButton, "popupshown");
   EventUtils.synthesizeMouseAtCenter(backButton, {type: "mousedown"});
   EventUtils.synthesizeMouse(backButton, rect.width / 2, rect.height, {type: "mouseup"});
-}
+  let event = yield popupShownPromise;
+
+  ok(true, "history menu opened");
+
+  event.target.hidePopup();
+  gBrowser.removeTab(gBrowser.selectedTab);
+});
