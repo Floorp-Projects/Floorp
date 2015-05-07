f5aeb2fcd026f91b83cc7cbe8c5cb54896b1b443
J. Ryan Stinnett <jryans@gmail.com>
2015-05-07 17:10:49 -0500

    Bug 1159043 - Record WebIDE runtime info in Telemetry. r=ochameau a=sylvestre

--- browser/devtools/webide/modules/app-manager.js
+++ browser/devtools/webide/modules/app-manager.js
@@ -129,6 +129,8 @@ let AppManager = exports.AppManager = {
    *   runtime-list:
    *     The list of available runtimes has changed, or any of the user-visible
    *     details (like names) for the non-selected runtimes has changed.
+   *   runtime-telemetry:
+   *     Detailed runtime telemetry has been recorded.  Used by tests.
    *   runtime-targets:
    *     The list of remote runtime targets available from the currently
    *     connected runtime (such as tabs or apps) has changed, or any of the
@@ -183,6 +185,7 @@ let AppManager = exports.AppManager = {
             // first.
             this._appsFront = front;
             this._listTabsResponse = response;
+            this._recordRuntimeInfo();
             this.update("runtime-global-actors");
           })
           .then(() => {
@@ -192,6 +195,7 @@ let AppManager = exports.AppManager = {
           });
         } else {
           this._listTabsResponse = response;
+          this._recordRuntimeInfo();
           this.update("runtime-global-actors");
         }
       });
@@ -497,6 +501,33 @@ let AppManager = exports.AppManager = {
 
     return deferred.promise;
   },
+
+  _recordRuntimeInfo: Task.async(function*() {
+    if (!this.connected) {
+      return;
+    }
+    let runtime = this.selectedRuntime;
+    this._telemetry.logKeyed("DEVTOOLS_WEBIDE_CONNECTED_RUNTIME_TYPE",
+                             runtime.type || "UNKNOWN", true);
+    this._telemetry.logKeyed("DEVTOOLS_WEBIDE_CONNECTED_RUNTIME_ID",
+                             runtime.id || "unknown", true);
+    if (!this.deviceFront) {
+      this.update("runtime-telemetry");
+      return;
+    }
+    let d = yield this.deviceFront.getDescription();
+    this._telemetry.logKeyed("DEVTOOLS_WEBIDE_CONNECTED_RUNTIME_PROCESSOR",
+                             d.processor, true);
+    this._telemetry.logKeyed("DEVTOOLS_WEBIDE_CONNECTED_RUNTIME_OS",
+                             d.os, true);
+    this._telemetry.logKeyed("DEVTOOLS_WEBIDE_CONNECTED_RUNTIME_PLATFORM_VERSION",
+                             d.platformversion, true);
+    this._telemetry.logKeyed("DEVTOOLS_WEBIDE_CONNECTED_RUNTIME_APP_TYPE",
+                             d.apptype, true);
+    this._telemetry.logKeyed("DEVTOOLS_WEBIDE_CONNECTED_RUNTIME_VERSION",
+                             d.version, true);
+    this.update("runtime-telemetry");
+  }),
 
   isMainProcessDebuggable: function() {
     // Fx <39 exposes chrome tab actors on RootActor
