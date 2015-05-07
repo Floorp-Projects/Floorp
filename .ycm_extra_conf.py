ae50f0f85c6df04224eeb04fefd7d07c53f6ba7a
Nicholas Hurley <hurley@todesschaf.org>
2015-05-07 13:48:00 +0200

    Bug 1162714 - Don't let YCM generate machc. r=ehsan

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -6,6 +6,10 @@ import imp
 import os
 from StringIO import StringIO
 import shlex
+import sys
+
+old_bytecode = sys.dont_write_bytecode
+sys.dont_write_bytecode = True
 
 path = os.path.join(os.path.dirname(__file__), 'mach')
 
@@ -14,6 +18,8 @@ if not os.path.exists(path):
     config = imp.load_module('_buildconfig', open(path), path, ('', 'r', imp.PY_SOURCE))
     path = os.path.join(config.topsrcdir, 'mach')
 mach_module = imp.load_module('_mach', open(path), path, ('', 'r', imp.PY_SOURCE))
+
+sys.dont_write_bytecode = old_bytecode
 
 def FlagsForFile(filename):
     mach = mach_module.get_mach()
