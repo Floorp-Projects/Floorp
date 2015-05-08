0c1c3eca7b90a6d95b29be71267114da6dc900ab
Wes Kocher <wkocher@mozilla.com>
2015-05-08 16:03:05 -0700

    Backed out 3 changesets (bug 1138818) for bc1 orange CLOSED TREE
    Backed out changeset 70121f99d5f1 (bug 1138818)
    Backed out changeset cb5842311d43 (bug 1138818)
    Backed out changeset 5b45e43dec40 (bug 1138818)

--- browser/base/content/test/newtab/browser_newtab_intro.js
+++ browser/base/content/test/newtab/browser_newtab_intro.js
@@ -2,96 +2,50 @@
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
 const INTRO_PREF = "browser.newtabpage.introShown";
-const UPDATE_INTRO_PREF = "browser.newtabpage.updateIntroShown";
 const PRELOAD_PREF = "browser.newtab.preload";
 
 function runTests() {
   let origIntro = Services.prefs.getBoolPref(INTRO_PREF);
-  let origUpdateIntro = Services.prefs.getBoolPref(UPDATE_INTRO_PREF);
   let origPreload = Services.prefs.getBoolPref(PRELOAD_PREF);
   registerCleanupFunction(_ => {
     Services.prefs.setBoolPref(INTRO_PREF, origIntro);
-    Services.prefs.setBoolPref(INTRO_PREF, origUpdateIntro);
     Services.prefs.setBoolPref(PRELOAD_PREF, origPreload);
   });
 
   // Test with preload false
   Services.prefs.setBoolPref(INTRO_PREF, false);
-  Services.prefs.setBoolPref(UPDATE_INTRO_PREF, false);
   Services.prefs.setBoolPref(PRELOAD_PREF, false);
 
-  let intro;
-  let brand = Services.strings.createBundle("chrome://branding/locale/brand.properties");
-  let brandShortName = brand.GetStringFromName("brandShortName");
+  let panel;
+  function maybeWaitForPanel() {
+    // If already open, no need to wait
+    if (panel.state == "open") {
+      executeSoon(TestRunner.next);
+      return;
+    }
+
+    // We're expecting the panel to open, so wait for it
+    panel.addEventListener("popupshown", TestRunner.next);
+    isnot(panel.state, "open", "intro panel can be slow to show");
+  }
 
   yield addNewTabPageTab();
-  intro = getContentDocument().getElementById("newtab-intro-mask");
-  is(intro.style.opacity, 1, "intro automatically shown on first opening");
-  is(getContentDocument().getElementById("newtab-intro-header").innerHTML,
-     'Welcome to New Tab on <span xmlns="http://www.w3.org/1999/xhtml" class="bold">' + brandShortName + '</span>!', "we show the first-run intro.");
+  panel = getContentDocument().getElementById("newtab-intro-panel");
+  yield maybeWaitForPanel();
+  is(panel.state, "open", "intro automatically shown on first opening");
   is(Services.prefs.getBoolPref(INTRO_PREF), true, "newtab remembers that the intro was shown");
-  is(Services.prefs.getBoolPref(UPDATE_INTRO_PREF), true, "newtab avoids showing update if intro was shown");
 
   yield addNewTabPageTab();
-  intro = getContentDocument().getElementById("newtab-intro-mask");
-  is(intro.style.opacity, 0, "intro not shown on second opening");
+  panel = getContentDocument().getElementById("newtab-intro-panel");
+  is(panel.state, "closed", "intro not shown on second opening");
 
   // Test with preload true
   Services.prefs.setBoolPref(INTRO_PREF, false);
   Services.prefs.setBoolPref(PRELOAD_PREF, true);
 
   yield addNewTabPageTab();
-  intro = getContentDocument().getElementById("newtab-intro-mask");
-  is(intro.style.opacity, 1, "intro automatically shown on preloaded opening");
-  is(getContentDocument().getElementById("newtab-intro-header").innerHTML,
-     'Welcome to New Tab on <span xmlns="http://www.w3.org/1999/xhtml" class="bold">' + brandShortName + '</span>!', "we show the first-run intro.");
+  panel = getContentDocument().getElementById("newtab-intro-panel");
+  yield maybeWaitForPanel();
+  is(panel.state, "open", "intro automatically shown on preloaded opening");
   is(Services.prefs.getBoolPref(INTRO_PREF), true, "newtab remembers that the intro was shown");
-  is(Services.prefs.getBoolPref(UPDATE_INTRO_PREF), true, "newtab avoids showing update if intro was shown");
-
-  // Test with first run true but update false
-  Services.prefs.setBoolPref(UPDATE_INTRO_PREF, false);
-
-  yield addNewTabPageTab();
-  intro = getContentDocument().getElementById("newtab-intro-mask");
-  is(intro.style.opacity, 1, "intro automatically shown on preloaded opening");
-  is(getContentDocument().getElementById("newtab-intro-header").innerHTML,
-     "New Tab got an update!", "we show the update intro.");
-  is(Services.prefs.getBoolPref(INTRO_PREF), true, "INTRO_PREF stays true");
-  is(Services.prefs.getBoolPref(UPDATE_INTRO_PREF), true, "newtab remembers that the update intro was show");
-
-  // Test clicking the 'next' and 'back' buttons.
-  let buttons = getContentDocument().getElementById("newtab-intro-buttons").getElementsByTagName("input");
-  let progress = getContentDocument().getElementById("newtab-intro-numerical-progress");
-  let back = buttons[0];
-  let next = buttons[1];
-
-  synthesizeNativeMouseLDown(next);
-  synthesizeNativeMouseLUp(next).then(() => {
-    is(progress.getAttribute("page"), 1, "we get to the 2nd page");
-    is(intro.style.opacity, 1, "intro visible");
-  }, Cu.reportError);
-
-  synthesizeNativeMouseLDown(next);
-  synthesizeNativeMouseLUp(next).then(() => {
-    is(progress.getAttribute("page"), 2, "we get to the 3rd page");
-    is(intro.style.opacity, 1, "intro visible");
-  }, Cu.reportError);
-
-  synthesizeNativeMouseLDown(back);
-  synthesizeNativeMouseLUp(back).then(() => {
-    is(progress.getAttribute("page"), 1, "go back to 2nd page");
-    is(intro.style.opacity, 1, "intro visible");
-  }, Cu.reportError);
-
-  synthesizeNativeMouseLDown(back);
-  synthesizeNativeMouseLUp(back).then(() => {
-    is(progress.getAttribute("page"), 0, "go back to 1st page");
-    is(intro.style.opacity, 1, "intro visible");
-  }, Cu.reportError);
-
-  synthesizeNativeMouseLDown(back);
-  synthesizeNativeMouseLUp(back).then(() => {
-    is(progress.getAttribute("page"), 0, "another back will 'skip tutorial'");
-    is(intro.style.opacity, 0, "intro exited");
-  }, Cu.reportError);
 }
