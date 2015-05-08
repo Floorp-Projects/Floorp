ef2f9a8b29a77a23476ec41e5ac2327a86bcfcc4
Mike Conley <mconley@mozilla.com>
2015-05-08 17:04:30 -0400

    Bug 1025146 - Make viewing the source of a page or frame via the context menu work with remote browsers. r=jryans.

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -993,7 +993,11 @@ nsContextMenu.prototype = {
 
   // Open new "view source" window with the frame's URL.
   viewFrameSource: function() {
-    BrowserViewSourceOfDocument(this.target.ownerDocument);
+    BrowserViewSourceOfDocument({
+      browser: this.browser,
+      URL: gContextMenuContentData.docLocation,
+      outerWindowID: gContextMenuContentData.frameOuterWindowID,
+    });
   },
 
   viewInfo: function() {
