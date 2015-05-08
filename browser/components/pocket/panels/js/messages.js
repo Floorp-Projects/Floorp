11322e4f4dd824ffd421f70c9d99596175a4e020
Jared Wein <jwein@mozilla.com>
2015-05-08 16:47:41 -0400

    Bug 1163111 - Update Pocket code to latest version (May 7th code drop).  r=dolske a=dolske

--- browser/components/pocket/panels/js/messages.js
+++ browser/components/pocket/panels/js/messages.js
@@ -1,11 +1,27 @@
 // Documentation of methods used here are at:
 // https://developer.mozilla.org/en-US/Add-ons/Code_snippets/Interaction_between_privileged_and_non-privileged_pages
 
-var Messaging = (function() {
+var pktPanelMessaging = (function() {
 
-	function addMessageListener(messageId, callback) {
+ function panelIdFromURL(url) {
+   var panelId = url.match(/panelId=([\w|\d|\.]*)&?/);
+        if (panelId && panelId.length > 1) {
+            return panelId[1];
+        }
 
-		document.addEventListener('PKT_' + messageId, function(e) {
+        return 0;
+ }
+
+ function prefixedMessageId(messageId) {
+   return 'PKT_' + messageId;
+ }
+
+ function panelPrefixedMessageId(panelId, messageId) {
+   return prefixedMessageId(panelId + '_' + messageId);
+ }
+
+ function addMessageListener(panelId, messageId, callback) {
+   document.addEventListener(panelPrefixedMessageId(panelId, messageId), function(e) {
 
 			callback(JSON.parse(e.target.getAttribute("payload"))[0]);
 
@@ -16,33 +32,36 @@ var Messaging = (function() {
 
 	}
 
-	function removeMessageListener(messageId, callback) {
-
-		document.removeEventListener('PKT_' + messageId, callback);
-
+	function removeMessageListener(panelId, messageId, callback) {
+   document.removeEventListener(panelPrefixedMessageId(panelId, messageId), callback);
 	}
 
-	function sendMessage(messageId, payload, callback) {
+ function sendMessage(panelId, messageId, payload, callback) {
+   // Payload needs to be an object in format:
+   // { panelId: panelId, data: {} }
+   var messagePayload = {
+     panelId: panelId,
+     data: (payload || {})
+   };
 
 		// Create a callback to listen for a response
 		if (callback) {
-			 // Message response id is messageId + "Response"
 	        var messageResponseId = messageId + "Response";
 	        var responseListener = function(responsePayload) {
 	            callback(responsePayload);
-	            removeMessageListener(messageResponseId, responseListener);
+	            removeMessageListener(panelId, messageResponseId, responseListener);
 	        }
 
-	        addMessageListener(messageResponseId, responseListener);
+	        addMessageListener(panelId, messageResponseId, responseListener);
 	    }
 
 	    // Send message
 		var element = document.createElement("PKTMessageFromPanelElement");
-		element.setAttribute("payload", JSON.stringify([payload]));
+		element.setAttribute("payload", JSON.stringify([messagePayload]));
 		document.documentElement.appendChild(element);
 
 		var evt = document.createEvent("Events");
-		evt.initEvent('PKT_'+messageId, true, false);
+		evt.initEvent(prefixedMessageId(messageId), true, false);
 		element.dispatchEvent(evt);
 	}
 
@@ -51,6 +70,7 @@ var Messaging = (function() {
      * Public functions
      */
     return {
+      panelIdFromURL: panelIdFromURL,
         addMessageListener : addMessageListener,
         removeMessageListener : removeMessageListener,
         sendMessage: sendMessage
