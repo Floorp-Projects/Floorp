aa7398773556798377c71659dc18fb4b1d3d9cbb
JW Wang <jwwang@mozilla.com>
2015-05-09 07:32:27 +0800

    Bug 1161405 - Part 2: Test case for parallelism of the thread pool. r=nfroyd, a=lizzard

--- xpcom/tests/gtest/TestThreadPool.cpp
+++ xpcom/tests/gtest/TestThreadPool.cpp
@@ -13,6 +13,7 @@
 #include "nsCOMPtr.h"
 #include "nsIRunnable.h"
 #include "mozilla/Atomics.h"
+#include "mozilla/Monitor.h"
 #include "gtest/gtest.h"
 
 class Task final : public nsIRunnable
@@ -57,4 +58,64 @@ TEST(ThreadPool, Main)
 
   pool->Shutdown();
   EXPECT_EQ(Task::sCount, 100);
+}
+
+TEST(ThreadPool, Parallelism)
+{
+  nsCOMPtr<nsIThreadPool> pool = do_CreateInstance(NS_THREADPOOL_CONTRACTID);
+  EXPECT_TRUE(pool);
+
+  // Dispatch and sleep to ensure we have an idle thread
+  nsCOMPtr<nsIRunnable> r0 = new nsRunnable();
+  pool->Dispatch(r0, NS_DISPATCH_SYNC);
+  PR_Sleep(PR_SecondsToInterval(2));
+
+  class Runnable1 : public nsRunnable {
+  public:
+    Runnable1(Monitor& aMonitor, bool& aDone)
+      : mMonitor(aMonitor), mDone(aDone) {}
+
+    NS_IMETHOD Run() override {
+      MonitorAutoLock mon(mMonitor);
+      if (!mDone) {
+        // Wait for a reasonable timeout since we don't want to block gtests
+        // forever should any regression happen.
+        mon.Wait(PR_SecondsToInterval(300));
+      }
+      EXPECT_TRUE(mDone);
+      return NS_OK;
+    }
+  private:
+    Monitor& mMonitor;
+    bool& mDone;
+  };
+
+  class Runnable2 : public nsRunnable {
+  public:
+    Runnable2(Monitor& aMonitor, bool& aDone)
+      : mMonitor(aMonitor), mDone(aDone) {}
+
+    NS_IMETHOD Run() override {
+      MonitorAutoLock mon(mMonitor);
+      mDone = true;
+      mon.NotifyAll();
+      return NS_OK;
+    }
+  private:
+    Monitor& mMonitor;
+    bool& mDone;
+  };
+
+  // Dispatch 2 events in a row. Since we are still within the thread limit,
+  // We should wake up the idle thread and spawn a new thread so these 2 events
+  // can run in parallel. We will time out if r1 and r2 run in sequence for r1
+  // won't finish until r2 finishes.
+  Monitor mon("ThreadPool::Parallelism");
+  bool done = false;
+  nsCOMPtr<nsIRunnable> r1 = new Runnable1(mon, done);
+  nsCOMPtr<nsIRunnable> r2 = new Runnable2(mon, done);
+  pool->Dispatch(r1, NS_DISPATCH_NORMAL);
+  pool->Dispatch(r2, NS_DISPATCH_NORMAL);
+
+  pool->Shutdown();
 }
