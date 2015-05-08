822a639bb17e64566129da761568ba50e33f68e4
Kan-Ru Chen <kanru@kanru.info>
2015-05-08 11:21:21 +0800

    Bug 1123237 - Part 6. A new API to get backtrace without allocating memory in profiler. r=mstange
    Based on patch from Ting-Yuan Huang <laszio.bugzilla@gmail.com>

--- tools/profiler/public/GeckoProfilerFunc.h
+++ tools/profiler/public/GeckoProfilerFunc.h
@@ -48,6 +48,7 @@ void mozilla_sampler_resume();
 
 ProfilerBacktrace* mozilla_sampler_get_backtrace();
 void mozilla_sampler_free_backtrace(ProfilerBacktrace* aBacktrace);
+void mozilla_sampler_get_backtrace_noalloc(char *output, size_t outputSize);
 
 bool mozilla_sampler_is_active();
 
