ff688a6b005302db71a4934197bda230860c425f
Jordan Santell <jsantell@mozilla.com>
2015-05-08 16:19:58 -0700

    Bug 1160900 - Display buffer status while recording a profile. r=vp

--- browser/devtools/performance/modules/recording-model.js
+++ browser/devtools/performance/modules/recording-model.js
@@ -107,6 +107,8 @@ RecordingModel.prototype = {
       totalSize: info.totalSize,
       generation: info.generation
     };
+    // initialize the _bufferPercent if the server supports it.
+    this._bufferPercent = info.position !== void 0 ? 0 : null;
 
     this._recording = true;
 
---
+++ browser/devtools/performance/test/browser_perf-compatibility-07.js
@@ -0,0 +1,29 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Tests that recording notices does not display any buffer
+ * status on servers that do not support buffer statuses.
+ */
+function spawnTest () {
+  let { panel } = yield initPerformance(SIMPLE_URL, void 0, {
+    TEST_MOCK_PROFILER_CHECK_TIMER: 10,
+    TEST_PROFILER_FILTER_STATUS: ["position", "totalSize", "generation"]
+  });
+  let { gFront: front, EVENTS, $, PerformanceController, PerformanceView, RecordingsView } = panel.panelWin;
+
+  yield startRecording(panel);
+
+  yield once(front._connection._profiler, "profiler-status");
+  ok(!$("#details-pane-container").getAttribute("buffer-status"),
+    "container does not have [buffer-status] attribute when not supported");
+
+  yield once(front._connection._profiler, "profiler-status");
+  ok(!$("#details-pane-container").getAttribute("buffer-status"),
+    "container does not have [buffer-status] attribute when not supported");
+
+  yield stopRecording(panel);
+
+  yield teardown(panel);
+  finish();
+}
