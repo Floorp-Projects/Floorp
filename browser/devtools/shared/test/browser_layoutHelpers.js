70acf190fd566c8121fb90b61820da38722b33ad
Brian Grinstead <bgrinstead@mozilla.com>
2015-05-08 20:39:23 -0700

    Bug 1127423 - Don't scroll horizontally when selecting an element in markup view;r=jryans

--- browser/devtools/shared/test/browser_layoutHelpers.js
+++ browser/devtools/shared/test/browser_layoutHelpers.js
@@ -2,100 +2,84 @@
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 // Tests that scrollIntoViewIfNeeded works properly.
+let {LayoutHelpers} = Cu.import("resource://gre/modules/devtools/LayoutHelpers.jsm", {});
 
-let imported = {};
-Components.utils.import("resource://gre/modules/devtools/LayoutHelpers.jsm",
-    imported);
-registerCleanupFunction(function () {
-  imported = {};
-});
-
-let LayoutHelpers = imported.LayoutHelpers;
 
 const TEST_URI = TEST_URI_ROOT + "browser_layoutHelpers.html";
 
-function test() {
-  addTab(TEST_URI, function(browser, tab) {
-    info("Starting browser_layoutHelpers.js");
-    let doc = browser.contentDocument;
-    runTest(doc.defaultView, doc.getElementById('some'));
-    gBrowser.removeCurrentTab();
-    finish();
-  });
-}
+add_task(function*() {
+  let [host, win, doc] = yield createHost("bottom", TEST_URI);
+  runTest(win);
+  host.destroy();
+});
 
-function runTest(win, some) {
+function runTest(win) {
   let lh = new LayoutHelpers(win);
+  let some = win.document.getElementById('some');
 
   some.style.top = win.innerHeight + 'px';
   some.style.left = win.innerWidth + 'px';
   // The tests start with a black 2x2 pixels square below bottom right.
   // Do not resize the window during the tests.
 
-  win.scroll(win.innerWidth / 2, win.innerHeight + 2);  // Above the viewport.
+  let xPos = Math.floor(win.innerWidth / 2);
+  win.scroll(xPos, win.innerHeight + 2);  // Above the viewport.
   lh.scrollIntoViewIfNeeded(some);
   is(win.scrollY, Math.floor(win.innerHeight / 2) + 1,
      'Element completely hidden above should appear centered.');
+  is(win.scrollX, xPos,
+     'scrollX position has not changed.');
 
   win.scroll(win.innerWidth / 2, win.innerHeight + 1);  // On the top edge.
   lh.scrollIntoViewIfNeeded(some);
   is(win.scrollY, win.innerHeight,
      'Element partially visible above should appear above.');
+  is(win.scrollX, xPos,
+     'scrollX position has not changed.');
 
   win.scroll(win.innerWidth / 2, 0);  // Just below the viewport.
   lh.scrollIntoViewIfNeeded(some);
   is(win.scrollY, Math.floor(win.innerHeight / 2) + 1,
      'Element completely hidden below should appear centered.');
+  is(win.scrollX, xPos,
+     'scrollX position has not changed.');
 
   win.scroll(win.innerWidth / 2, 1);  // On the bottom edge.
   lh.scrollIntoViewIfNeeded(some);
   is(win.scrollY, 2,
      'Element partially visible below should appear below.');
-
+  is(win.scrollX, xPos,
+     'scrollX position has not changed.');
 
   win.scroll(win.innerWidth / 2, win.innerHeight + 2);  // Above the viewport.
   lh.scrollIntoViewIfNeeded(some, false);
   is(win.scrollY, win.innerHeight,
      'Element completely hidden above should appear above ' +
      'if parameter is false.');
+  is(win.scrollX, xPos,
+     'scrollX position has not changed.');
 
   win.scroll(win.innerWidth / 2, win.innerHeight + 1);  // On the top edge.
   lh.scrollIntoViewIfNeeded(some, false);
   is(win.scrollY, win.innerHeight,
      'Element partially visible above should appear above ' +
      'if parameter is false.');
+  is(win.scrollX, xPos,
+     'scrollX position has not changed.');
 
   win.scroll(win.innerWidth / 2, 0);  // Below the viewport.
   lh.scrollIntoViewIfNeeded(some, false);
   is(win.scrollY, 2,
      'Element completely hidden below should appear below ' +
      'if parameter is false.');
+  is(win.scrollX, xPos,
+     'scrollX position has not changed.');
 
   win.scroll(win.innerWidth / 2, 1);  // On the bottom edge.
   lh.scrollIntoViewIfNeeded(some, false);
   is(win.scrollY, 2,
      'Element partially visible below should appear below ' +
      'if parameter is false.');
-
-  // The case of iframes.
-  win.scroll(0, 0);
-
-  let frame = win.document.getElementById('frame');
-  let fwin = frame.contentWindow;
-
-  frame.style.top = win.innerHeight + 'px';
-  frame.style.left = win.innerWidth + 'px';
-
-  fwin.addEventListener('load', function frameLoad() {
-    let some = fwin.document.getElementById('some');
-    lh.scrollIntoViewIfNeeded(some);
-    is(win.scrollX, Math.floor(win.innerWidth / 2) + 20,
-       'Scrolling from an iframe should center the iframe vertically.');
-    is(win.scrollY, Math.floor(win.innerHeight / 2) + 20,
-       'Scrolling from an iframe should center the iframe horizontally.');
-    is(fwin.scrollX, Math.floor(fwin.innerWidth / 2) + 1,
-       'Scrolling from an iframe should center the element vertically.');
-    is(fwin.scrollY, Math.floor(fwin.innerHeight / 2) + 1,
-       'Scrolling from an iframe should center the element horizontally.');
-  }, false);
+  is(win.scrollX, xPos,
+     'scrollX position has not changed.');
 }
