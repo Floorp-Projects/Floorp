a1170999ac2d23e9b286188934f7948483979ca9
Chenxia Liu <liuche@mozilla.com>
2015-05-08 17:40:15 -0700

    Bug 1126608 - Add list of logins. r=margaret

--- mobile/android/base/widget/DoorHanger.java
+++ mobile/android/base/widget/DoorHanger.java
@@ -208,7 +208,14 @@ public abstract class DoorHanger extends LinearLayout {
         mButtonsContainer.addView(button, sButtonParams);
     }
 
-    protected abstract Button createButtonInstance(String text, int id);
+    protected Button createButtonInstance(String text, int id) {
+        final Button button = (Button) LayoutInflater.from(getContext()).inflate(R.layout.doorhanger_button, null);
+        button.setText(text);
+        button.setOnClickListener(makeOnButtonClickListener(id));
+        return button;
+    }
+
+    protected abstract OnClickListener makeOnButtonClickListener(final int id);
 
     /*
      * Checks with persistence and timeout options to see if it's okay to remove a doorhanger.
