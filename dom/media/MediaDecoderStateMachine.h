f34e30b2c4d025738b05f52afcc2606c31d896e9
JW Wang <jwwang@mozilla.com>
2015-05-10 11:38:15 +0800

    Bug 1163469 - Move MediaDecoder::DecodedStreamData and related code to its own file. r=roc.

--- dom/media/MediaDecoderStateMachine.h
+++ dom/media/MediaDecoderStateMachine.h
@@ -98,6 +98,7 @@ namespace mozilla {
 class AudioSegment;
 class MediaTaskQueue;
 class AudioSink;
+class DecodedStreamData;
 
 /*
   The state machine class. This manages the decoding and seeking in the
@@ -117,10 +118,9 @@ class MediaDecoderStateMachine
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(MediaDecoderStateMachine)
 public:
   typedef MediaDecoderOwner::NextFrameStatus NextFrameStatus;
-  typedef MediaDecoder::DecodedStreamData DecodedStreamData;
   MediaDecoderStateMachine(MediaDecoder* aDecoder,
-                               MediaDecoderReader* aReader,
-                               bool aRealTime = false);
+                           MediaDecoderReader* aReader,
+                           bool aRealTime = false);
 
   nsresult Init(MediaDecoderStateMachine* aCloneDonor);
 
