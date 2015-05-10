d60a53661d1cd821cd68f423d8426fce2d935345
Jordan Santell <jsantell@mozilla.com>
2015-05-10 01:20:56 -0700

    Bug 1058898 - Enable e10s tests on the performance tool. r=vp, a=sledru

--- browser/devtools/performance/test/browser_perf-front-profiler-03.js
+++ browser/devtools/performance/test/browser_perf-front-profiler-03.js
@@ -16,18 +16,25 @@ let test = Task.async(function*() {
 
   let { panel: secondPanel } = yield initPerformance(SIMPLE_URL);
   let secondFront = secondPanel.panelWin.gFront;
+  loadFrameScripts();
 
   let alreadyActive = secondFront.once("profiler-already-active");
   yield secondFront.startRecording();
   yield alreadyActive;
 
-  yield teardown(firstPanel);
-  ok(nsIProfilerModule.IsActive(),
+  // Manually teardown the tabs so we can check profiler status
+  let tab1 = firstPanel.target.tab;
+  let tab2 = secondPanel.target.tab;
+  yield firstPanel._toolbox.destroy();
+  yield removeTab(tab1);
+  ok((yield PMM_isProfilerActive()),
     "The built-in profiler module should still be active.");
 
-  yield teardown(secondPanel);
-  ok(!nsIProfilerModule.IsActive(),
-    "The built-in profiler module should have been automatically stoped.");
+  yield secondPanel._toolbox.destroy();
+  ok(!(yield PMM_isProfilerActive()),
+    "The built-in profiler module should no longer be active.");
+  yield removeTab(tab2);
+  tab1 = tab2 = null;
 
   finish();
 });
--- browser/devtools/performance/test/browser_perf-shared-connection-03.js
+++ browser/devtools/performance/test/browser_perf-shared-connection-03.js
@@ -9,19 +9,21 @@ function spawnTest () {
   let { panel } = yield initPerformance(SIMPLE_URL);
   let front = panel.panelWin.gFront;
 
-  ok(!nsIProfilerModule.IsActive(),
+  loadFrameScripts();
+
+  ok(!(yield PMM_isProfilerActive()),
     "The built-in profiler module should not have been automatically started.");
 
   let result = yield front._request("profiler", "startProfiler");
   is(result.started, true,
     "The request finished successfully and the profiler should've been started.");
-  ok(nsIProfilerModule.IsActive(),
+  ok((yield PMM_isProfilerActive()),
     "The built-in profiler module should now be active.");
 
   result = yield front._request("profiler", "stopProfiler");
   is(result.started, false,
     "The request finished successfully and the profiler should've been stopped.");
-  ok(!nsIProfilerModule.IsActive(),
+  ok(!(yield PMM_isProfilerActive()),
     "The built-in profiler module should now be inactive.");
 
   yield teardown(panel);
