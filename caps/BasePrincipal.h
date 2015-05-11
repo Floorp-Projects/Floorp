9e3345280e5447c16f220eb65294fcea60eb7069
Bobby Holley <bobbyholley@gmail.com>
2015-05-11 14:25:59 -0700

    Bug 1164292 - Rebrand nsBasePrincipal into mozilla::BasePrincipal and give it its own file. r=gabor
    The goal here is to provide a common superclass for _all_ the principal
    implementations, rather than just nsPrincipal and nsExpandedPrincipal.

---
+++ caps/BasePrincipal.h
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_BasePrincipal_h
+#define mozilla_BasePrincipal_h
+
+#include "nsIPrincipal.h"
+#include "nsJSPrincipals.h"
+
+namespace mozilla {
+
+/*
+ * Base class from which all nsIPrincipal implementations inherit. Use this for
+ * default implementations and other commonalities between principal
+ * implementations.
+ *
+ * We should merge nsJSPrincipals into this class at some point.
+ */
+class BasePrincipal : public nsJSPrincipals
+{
+public:
+  BasePrincipal() {}
+  NS_IMETHOD GetCsp(nsIContentSecurityPolicy** aCsp);
+  NS_IMETHOD SetCsp(nsIContentSecurityPolicy* aCsp);
+
+protected:
+  virtual ~BasePrincipal() {}
+
+  nsCOMPtr<nsIContentSecurityPolicy> mCSP;
+};
+
+} // namespace mozilla
+
+#endif /* mozilla_BasePrincipal_h */
