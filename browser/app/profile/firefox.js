7b757254ba9e785239c1f3b895db36c8bc102650
Justin Dolske <dolske@mozilla.com>
2015-05-11 15:11:41 -0700

    Bug 1163645 - Pocket only enabled on en-US, hard-coded locales aren't picked up. r=adw a=dolske

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1917,4 +1917,4 @@ pref("browser.pocket.api", "api.getpocket.com");
 pref("browser.pocket.site", "getpocket.com");
 pref("browser.pocket.oAuthConsumerKey", "40249-e88c401e1b1f2242d9e441c4");
 pref("browser.pocket.useLocaleList", true);
-pref("browser.pocket.enabledLocales", "en-US");
+pref("browser.pocket.enabledLocales", "en-US de es-ES ja ru");
