0a3702094f941874b7bd0208098d6703c03f48ad
Neil Deakin <neil@mozilla.com>
2015-05-11 08:02:49 -0400

    Bug 1131575, e10s, fix and reenable browser_typeAheadFind.js, r=mikedeboer

--- browser/base/content/test/general/browser_typeAheadFind.js
+++ browser/base/content/test/general/browser_typeAheadFind.js
@@ -2,27 +2,21 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-let testWindow = null;
+add_task(function *() {
+  let testWindow = yield BrowserTestUtils.openNewBrowserWindow();
 
-function test() {
-  waitForExplicitFinish();
+  testWindow.gBrowser.loadURI("data:text/html,<h1>A Page</h1>");
+  yield BrowserTestUtils.browserLoaded(testWindow.gBrowser.selectedBrowser);
 
-  testWindow = OpenBrowserWindow();
-  whenDelayedStartupFinished(testWindow, function () {
-    let selectedBrowser = testWindow.gBrowser.selectedBrowser;
-    selectedBrowser.addEventListener("load", function onLoad() {
-      selectedBrowser.removeEventListener("load", onLoad, true);
-      ok(true, "load listener called");
-      waitForFocus(onFocus, testWindow.content);
-    }, true);
-    testWindow.gBrowser.loadURI("data:text/html,<h1>A Page</h1>");
-  });
-}
+  yield SimpleTest.promiseFocus(testWindow.gBrowser.selectedBrowser);
 
-function onFocus() {
   ok(!testWindow.gFindBarInitialized, "find bar is not initialized");
+
+  let findBarOpenPromise = promiseWaitForEvent(testWindow.gBrowser, "findbaropen");
   EventUtils.synthesizeKey("/", {}, testWindow);
+  yield findBarOpenPromise;
+
   ok(testWindow.gFindBarInitialized, "find bar is now initialized");
-  testWindow.close();
-  finish();
-}
+
+  yield BrowserTestUtils.closeWindow(testWindow);
+});
