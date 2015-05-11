dffe6345e3d7f2886d0aa1101ba101a717c4c049
Gerald Squelart <gsquelart@mozilla.com>
2015-05-11 00:38:00 -0400

    Bug 1163505 - PtrVector should disallow copies to prevent premature&double deletions. r=bwc

--- media/webrtc/signaling/src/common/PtrVector.h
+++ media/webrtc/signaling/src/common/PtrVector.h
@@ -16,6 +16,18 @@ namespace mozilla
 template <class T> class PtrVector
 {
 public:
+  PtrVector() = default;
+  PtrVector(const PtrVector&) = delete;
+  PtrVector(PtrVector&& aOther)
+    : values(std::move(aOther.values))
+  {}
+  PtrVector& operator=(const PtrVector&) = delete;
+  PtrVector& operator=(PtrVector&& aOther)
+  {
+    std::swap(values, aOther.values);
+    return *this;
+  }
+
   ~PtrVector()
   {
     for (T* value : values) { delete value; }
