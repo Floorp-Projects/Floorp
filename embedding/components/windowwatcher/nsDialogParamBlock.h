55f25cd4226dcf34a2bddee56cd6147ccc9eeb6b
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-05-11 12:35:14 -0700

    Bug 1130096 - Convert embedding/components/windowwatcher/ to Gecko style. r=mccr8

--- embedding/components/windowwatcher/nsDialogParamBlock.h
+++ embedding/components/windowwatcher/nsDialogParamBlock.h
@@ -2,7 +2,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
- 
+
 #ifndef __nsDialogParamBlock_h
 #define __nsDialogParamBlock_h
 
@@ -12,34 +12,33 @@
 
 // {4E4AAE11-8901-46cc-8217-DAD7C5415873}
 #define NS_DIALOGPARAMBLOCK_CID \
- {0x4e4aae11, 0x8901, 0x46cc, {0x82, 0x17, 0xda, 0xd7, 0xc5, 0x41, 0x58, 0x73}}
+  {0x4e4aae11, 0x8901, 0x46cc, {0x82, 0x17, 0xda, 0xd7, 0xc5, 0x41, 0x58, 0x73}}
 
 class nsString;
 
-class nsDialogParamBlock: public nsIDialogParamBlock
+class nsDialogParamBlock : public nsIDialogParamBlock
 {
-public: 	
+public:
   nsDialogParamBlock();
-   
+
   NS_DECL_NSIDIALOGPARAMBLOCK
-  NS_DECL_ISUPPORTS	
+  NS_DECL_ISUPPORTS
 
 protected:
   virtual ~nsDialogParamBlock();
 
 private:
+  enum { kNumInts = 8, kNumStrings = 16 };
 
-  enum {kNumInts = 8, kNumStrings = 16};
-
-  nsresult InBounds(int32_t inIndex, int32_t inMax) {
-    return inIndex >= 0 && inIndex < inMax ? NS_OK : NS_ERROR_ILLEGAL_VALUE;
+  nsresult InBounds(int32_t aIndex, int32_t aMax)
+  {
+    return aIndex >= 0 && aIndex < aMax ? NS_OK : NS_ERROR_ILLEGAL_VALUE;
   }
-  
+
   int32_t mInt[kNumInts];
   int32_t mNumStrings;
   nsString* mString;
-  nsCOMPtr<nsIMutableArray> mObjects;	
+  nsCOMPtr<nsIMutableArray> mObjects;
 };
 
 #endif
-
