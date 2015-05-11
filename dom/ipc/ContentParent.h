6c45277de4b52583977c5b76ba9095243b4006c4
Bob Owen <bobowencode@gmail.com>
2015-05-11 08:24:40 +0100

    Bug 1146874 Part 3: Add LaunchSubprocess function to ContentParent to remove fallible code from constructor. r=billm

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -433,7 +433,6 @@ private:
                   ContentParent* aOpener,
                   bool aIsForBrowser,
                   bool aIsForPreallocated,
-                  hal::ProcessPriority aInitialPriority = hal::PROCESS_PRIORITY_FOREGROUND,
                   bool aIsNuwaProcess = false);
 
 #ifdef MOZ_NUWA_PROCESS
@@ -446,7 +445,11 @@ private:
     // The common initialization for the constructors.
     void InitializeMembers();
 
-    // The common initialization logic shared by all constuctors.
+    // Launch the subprocess and associated initialization.
+    // Returns false if the process fails to start.
+    bool LaunchSubprocess(hal::ProcessPriority aInitialPriority = hal::PROCESS_PRIORITY_FOREGROUND);
+
+    // Common initialization after sub process launch or adoption.
     void InitInternal(ProcessPriority aPriority,
                       bool aSetupOffMainThreadCompositing,
                       bool aSendRegisteredChrome);
