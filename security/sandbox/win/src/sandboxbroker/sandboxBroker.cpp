2bc36c85cd20800626aa09ad055d3c4207e1a51e
Bob Owen <bobowencode@gmail.com>
2015-05-11 08:24:39 +0100

    Bug 1146874 - Part 1: Check that Windows sandboxed process starts correctly. r=tabraldes, a=lizzard

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -52,9 +52,12 @@ SandboxBroker::LaunchApp(const wchar_t *aPath,
   }
 
   // Ceate the sandboxed process
-  PROCESS_INFORMATION targetInfo;
+  PROCESS_INFORMATION targetInfo = {0};
   sandbox::ResultCode result;
   result = sBrokerService->SpawnTarget(aPath, aArguments, mPolicy, &targetInfo);
+  if (sandbox::SBOX_ALL_OK != result) {
+    return false;
+  }
 
   // The sandboxed process is started in a suspended state, resume it now that
   // we've set things up.
