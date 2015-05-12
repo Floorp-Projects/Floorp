700935223fd60f339af061485e99536cc574bcb1
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-05-12 15:02:36 -0700

    Bug 1038536 - Flatten image/src/ directory. r=seth
    --HG--
    rename : image/src/BMPFileHeaders.h => image/BMPFileHeaders.h
    rename : image/src/ClippedImage.cpp => image/ClippedImage.cpp
    rename : image/src/ClippedImage.h => image/ClippedImage.h
    rename : image/src/DecodePool.cpp => image/DecodePool.cpp
    rename : image/src/DecodePool.h => image/DecodePool.h
    rename : image/src/Decoder.cpp => image/Decoder.cpp
    rename : image/src/Decoder.h => image/Decoder.h
    rename : image/src/Downscaler.cpp => image/Downscaler.cpp
    rename : image/src/Downscaler.h => image/Downscaler.h
    rename : image/src/DynamicImage.cpp => image/DynamicImage.cpp
    rename : image/src/DynamicImage.h => image/DynamicImage.h
    rename : image/src/FrameAnimator.cpp => image/FrameAnimator.cpp
    rename : image/src/FrameAnimator.h => image/FrameAnimator.h
    rename : image/src/FrozenImage.cpp => image/FrozenImage.cpp
    rename : image/src/FrozenImage.h => image/FrozenImage.h
    rename : image/src/ICOFileHeaders.h => image/ICOFileHeaders.h
    rename : image/src/IProgressObserver.h => image/IProgressObserver.h
    rename : image/src/Image.cpp => image/Image.cpp
    rename : image/src/Image.h => image/Image.h
    rename : image/src/ImageFactory.cpp => image/ImageFactory.cpp
    rename : image/src/ImageFactory.h => image/ImageFactory.h
    rename : image/src/ImageMetadata.cpp => image/ImageMetadata.cpp
    rename : image/src/ImageMetadata.h => image/ImageMetadata.h
    rename : image/src/ImageOps.cpp => image/ImageOps.cpp
    rename : image/src/ImageOps.h => image/ImageOps.h
    rename : image/src/ImageRegion.h => image/ImageRegion.h
    rename : image/src/ImageURL.h => image/ImageURL.h
    rename : image/src/ImageWrapper.cpp => image/ImageWrapper.cpp
    rename : image/src/ImageWrapper.h => image/ImageWrapper.h
    rename : image/src/MultipartImage.cpp => image/MultipartImage.cpp
    rename : image/src/MultipartImage.h => image/MultipartImage.h
    rename : image/src/Orientation.h => image/Orientation.h
    rename : image/src/OrientedImage.cpp => image/OrientedImage.cpp
    rename : image/src/OrientedImage.h => image/OrientedImage.h
    rename : image/src/ProgressTracker.cpp => image/ProgressTracker.cpp
    rename : image/src/ProgressTracker.h => image/ProgressTracker.h
    rename : image/src/RasterImage.cpp => image/RasterImage.cpp
    rename : image/src/RasterImage.h => image/RasterImage.h
    rename : image/src/SVGDocumentWrapper.cpp => image/SVGDocumentWrapper.cpp
    rename : image/src/SVGDocumentWrapper.h => image/SVGDocumentWrapper.h
    rename : image/src/ScriptedNotificationObserver.cpp => image/ScriptedNotificationObserver.cpp
    rename : image/src/ScriptedNotificationObserver.h => image/ScriptedNotificationObserver.h
    rename : image/src/ShutdownTracker.cpp => image/ShutdownTracker.cpp
    rename : image/src/ShutdownTracker.h => image/ShutdownTracker.h
    rename : image/src/SourceBuffer.cpp => image/SourceBuffer.cpp
    rename : image/src/SourceBuffer.h => image/SourceBuffer.h
    rename : image/src/SurfaceCache.cpp => image/SurfaceCache.cpp
    rename : image/src/SurfaceCache.h => image/SurfaceCache.h
    rename : image/src/VectorImage.cpp => image/VectorImage.cpp
    rename : image/src/VectorImage.h => image/VectorImage.h
    rename : image/src/imgFrame.cpp => image/imgFrame.cpp
    rename : image/src/imgFrame.h => image/imgFrame.h
    rename : image/src/imgLoader.cpp => image/imgLoader.cpp
    rename : image/src/imgLoader.h => image/imgLoader.h
    rename : image/src/imgRequest.cpp => image/imgRequest.cpp
    rename : image/src/imgRequest.h => image/imgRequest.h
    rename : image/src/imgRequestProxy.cpp => image/imgRequestProxy.cpp
    rename : image/src/imgRequestProxy.h => image/imgRequestProxy.h
    rename : image/src/imgTools.cpp => image/imgTools.cpp
    rename : image/src/imgTools.h => image/imgTools.h

---
+++ image/ImageOps.h
@@ -0,0 +1,72 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_image_ImageOps_h
+#define mozilla_image_ImageOps_h
+
+#include "nsCOMPtr.h"
+#include "nsRect.h"
+
+class gfxDrawable;
+class imgIContainer;
+
+namespace mozilla {
+namespace image {
+
+class Image;
+struct Orientation;
+
+class ImageOps
+{
+public:
+  /**
+   * Creates a version of an existing image which does not animate and is frozen
+   * at the first frame.
+   *
+   * @param aImage         The existing image.
+   */
+  static already_AddRefed<Image> Freeze(Image* aImage);
+  static already_AddRefed<imgIContainer> Freeze(imgIContainer* aImage);
+
+  /**
+   * Creates a clipped version of an existing image. Animation is unaffected.
+   *
+   * @param aImage         The existing image.
+   * @param aClip          The rectangle to clip the image against.
+   */
+  static already_AddRefed<Image> Clip(Image* aImage, nsIntRect aClip);
+  static already_AddRefed<imgIContainer> Clip(imgIContainer* aImage,
+                                              nsIntRect aClip);
+
+  /**
+   * Creates a version of an existing image which is rotated and/or flipped to
+   * the specified orientation.
+   *
+   * @param aImage         The existing image.
+   * @param aOrientation   The desired orientation.
+   */
+  static already_AddRefed<Image> Orient(Image* aImage,
+                                        Orientation aOrientation);
+  static already_AddRefed<imgIContainer> Orient(imgIContainer* aImage,
+                                                Orientation aOrientation);
+
+  /**
+   * Creates an image from a gfxDrawable.
+   *
+   * @param aDrawable      The gfxDrawable.
+   */
+  static already_AddRefed<imgIContainer>
+  CreateFromDrawable(gfxDrawable* aDrawable);
+
+private:
+  // This is a static utility class, so disallow instantiation.
+  virtual ~ImageOps() = 0;
+};
+
+} // namespace image
+} // namespace mozilla
+
+#endif // mozilla_image_ImageOps_h
