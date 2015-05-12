204424e88b5f3e1254e2cdedcbae5aafe4a17f87
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2015-05-12 18:21:08 +0100

    Bug 1164410 - Fix l10n use in pocket. r=jaws, a=dolske

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -190,16 +190,18 @@ nsContextMenu.prototype = {
         let locale = Cc["@mozilla.org/chrome/chrome-registry;1"].
                      getService(Ci.nsIXULChromeRegistry).
                      getSelectedLocale("browser");
-        let url = "chrome://browser/content/browser-pocket-" + locale + ".properties";
-        let bundle = Services.strings.createBundle(url);
-        let item = document.getElementById("context-pocket");
-        try {
-          item.setAttribute("label", bundle.GetStringFromName("saveToPocketCmd.label"));
-          item.setAttribute("accesskey", bundle.GetStringFromName("saveToPocketCmd.accesskey"));
-        } catch (err) {
-          // GetStringFromName throws when the bundle doesn't exist.  In that
-          // case, the item will retain the browser-pocket.dtd en-US string that
-          // it has in the markup.
+        if (locale != "en-US") {
+          let url = "chrome://browser/content/browser-pocket-" + locale + ".properties";
+          let bundle = Services.strings.createBundle(url);
+          let item = document.getElementById("context-pocket");
+          try {
+            item.setAttribute("label", bundle.GetStringFromName("saveToPocketCmd.label"));
+            item.setAttribute("accesskey", bundle.GetStringFromName("saveToPocketCmd.accesskey"));
+          } catch (err) {
+            // GetStringFromName throws when the bundle doesn't exist.  In that
+            // case, the item will retain the browser-pocket.dtd en-US string that
+            // it has in the markup.
+          }
         }
       }
     }
