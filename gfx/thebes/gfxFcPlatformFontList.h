754ee751da277083c0a3bdca5ef6deec6b4b6c01
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-05-12 12:20:48 +0200

    Backed out changeset f630169085c1 (bug 1056479)

--- gfx/thebes/gfxFcPlatformFontList.h
+++ gfx/thebes/gfxFcPlatformFontList.h
@@ -28,13 +28,6 @@ public:
     static void Release(FcObjectSet *ptr) { FcObjectSetDestroy(ptr); }
 };
 
-template <>
-class nsAutoRefTraits<FcConfig> : public nsPointerRefTraits<FcConfig>
-{
-public:
-    static void Release(FcConfig *ptr) { FcConfigDestroy(ptr); }
-    static void AddRef(FcConfig *ptr) { FcConfigReference(ptr); }
-};
 
 // Helper classes used for clearning out user font data when cairo font
 // face is destroyed. Since multiple faces may use the same data, be
@@ -194,7 +187,13 @@ protected:
 
 class gfxFcPlatformFontList : public gfxPlatformFontList {
 public:
-    gfxFcPlatformFontList();
+    gfxFcPlatformFontList()
+        : mLocalNames(64), mGenericMappings(32)
+    {
+#ifdef MOZ_BUNDLED_FONTS
+        mBundledFontsInitialized = false;
+#endif
+    }
 
     // initialize font lists
     nsresult InitFontList() override;
@@ -224,8 +223,6 @@ public:
     bool GetStandardFamilyName(const nsAString& aFontName,
                                nsAString& aFamilyName) override;
 
-    FcConfig* GetLastConfig() const { return mLastConfig; }
-
     static FT_Library GetFTLibrary();
 
 protected:
@@ -239,8 +236,6 @@ protected:
     gfxFontFamily* FindGenericFamily(const nsAString& aGeneric,
                                      nsIAtom* aLanguage);
 
-    static void CheckFontUpdates(nsITimer *aTimer, void *aThis);
-
 #ifdef MOZ_BUNDLED_FONTS
     void ActivateBundledFonts();
     nsCString mBundledFontsPath;
@@ -253,9 +248,6 @@ protected:
 
     // caching generic/lang ==> font family
     nsRefPtrHashtable<nsCStringHashKey, gfxFontFamily> mGenericMappings;
-
-    nsCOMPtr<nsITimer> mCheckFontUpdatesTimer;
-    nsCountedRef<FcConfig> mLastConfig;
 
     static FT_Library sCairoFTLibrary;
 };
