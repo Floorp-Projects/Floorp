d218441faecdc242b161bb8c6b02666e35b730e6
Ms2ger <ms2ger@gmail.com>
2015-05-13 17:58:26 +0200

    Bug 1145395 - Introduce an overload of nsHTMLEditor::RemoveStyleInside that takes nsIContent&; r=ehsan

--- editor/libeditor/nsHTMLEditUtils.cpp
+++ editor/libeditor/nsHTMLEditUtils.cpp
@@ -27,15 +27,6 @@
 using namespace mozilla;
 
 ///////////////////////////////////////////////////////////////////////////
-//                  
-bool 
-nsHTMLEditUtils::IsBig(nsIDOMNode* aNode)
-{
-  return nsEditor::NodeIsType(aNode, nsGkAtoms::big);
-}
-
-
-///////////////////////////////////////////////////////////////////////////
 // IsInlineStyle true if node is an inline style
 //                  
 bool 
@@ -105,15 +96,6 @@ nsHTMLEditUtils::IsNodeThatCanOutdent(nsIDOMNode* aNode)
       || (nodeAtom == nsGkAtoms::dt)
       || (nodeAtom == nsGkAtoms::blockquote);
 }
-
-///////////////////////////////////////////////////////////////////////////
-//                  
-bool 
-nsHTMLEditUtils::IsSmall(nsIDOMNode* aNode)
-{
-  return nsEditor::NodeIsType(aNode, nsGkAtoms::small);
-}
-
 
 /********************************************************
  *  helper methods from nsHTMLEditRules
