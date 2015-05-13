71406043231fac501a4844fa69d07a1b5d0b3021
Ted Mielczarek <ted@mielczarek.org>
2015-05-13 14:50:11 -0400

    Bug 1164816 - Rewrite symbolstore.py to use concurrent.futures. r=gps, a=NPOTB

--- toolkit/crashreporter/tools/symbolstore.py
+++ toolkit/crashreporter/tools/symbolstore.py
@@ -32,8 +32,10 @@ import fnmatch
 import subprocess
 import ctypes
 import urlparse
+import concurrent.futures
 import multiprocessing
 import collections
+
 from optparse import OptionParser
 from xml.dom.minidom import parse
 
@@ -354,6 +356,36 @@ def StartJob(dumper, lock, srcdirRepoInfo, func_name, args):
     Dumper.srcdirRepoInfo = srcdirRepoInfo
     return getattr(dumper, func_name)(*args)
 
+class JobPool(object):
+    jobs = {}
+    executor = None
+
+    @classmethod
+    def init(cls, executor):
+        cls.executor = executor
+
+    @classmethod
+    def shutdown(cls):
+        cls.executor.shutdown()
+
+    @classmethod
+    def submit(cls, args, callback):
+        cls.jobs[cls.executor.submit(StartJob, *args)] = callback
+
+    @classmethod
+    def as_completed(cls):
+        '''Like concurrent.futures.as_completed, but allows adding new futures
+        between generator steps. Iteration will end when the generator has
+        yielded all completed futures and JobQueue.jobs is empty.
+        Yields (future, callback) pairs.
+        '''
+        while cls.jobs:
+            completed, _ = concurrent.futures.wait(cls.jobs.keys(), return_when=concurrent.futures.FIRST_COMPLETED)
+            for f in completed:
+                callback = cls.jobs[f]
+                del cls.jobs[f]
+                yield f, callback
+
 class Dumper:
     """This class can dump symbols from a file with debug info, and
     store the output in a directory structure that is valid for use as
@@ -401,48 +433,26 @@ class Dumper:
             self.parse_repo_manifest(repo_manifest)
         self.file_mapping = file_mapping or {}
 
-        # book-keeping to keep track of our jobs and the cleanup work per file tuple
+        # book-keeping to keep track of the cleanup work per file tuple
         self.files_record = {}
-        self.jobs_record = collections.defaultdict(int)
 
     @classmethod
-    def GlobalInit(cls, module=multiprocessing):
+    def GlobalInit(cls, executor=concurrent.futures.ProcessPoolExecutor):
         """Initialize the class globals for the multiprocessing setup; must
         be called before any Dumper instances are created and used. Test cases
-        may pass in a different module to supply Manager and Pool objects,
-        usually multiprocessing.dummy."""
-        num_cpus = module.cpu_count()
+        may pass in a different executor to use, usually
+        concurrent.futures.ThreadPoolExecutor."""
+        num_cpus = multiprocessing.cpu_count()
         if num_cpus is None:
             # assume a dual core machine if we can't find out for some reason
             # probably better on single core anyway due to I/O constraints
             num_cpus = 2
 
         # have to create any locks etc before the pool
-        cls.manager = module.Manager()
-        cls.jobs_condition = Dumper.manager.Condition()
-        cls.lock = Dumper.manager.RLock()
-        cls.srcdirRepoInfo = Dumper.manager.dict()
-        cls.pool = module.Pool(num_cpus)
-
-    def JobStarted(self, file_key):
-        """Increments the number of submitted jobs for the specified key file,
-        defined as the original file we processed; note that a single key file
-        can generate up to 1 + len(self.archs) jobs in the Mac case."""
-        with Dumper.jobs_condition:
-            self.jobs_record[file_key] += 1
-            Dumper.jobs_condition.notify_all()
-
-    def JobFinished(self, file_key):
-        """Decrements the number of submitted jobs for the specified key file,
-        defined as the original file we processed; once the count is back to 0,
-        remove the entry from our record."""
-        with Dumper.jobs_condition:
-            self.jobs_record[file_key] -= 1
-
-            if self.jobs_record[file_key] == 0:
-                del self.jobs_record[file_key]
-
-            Dumper.jobs_condition.notify_all()
+        manager = multiprocessing.Manager()
+        cls.lock = manager.RLock()
+        cls.srcdirRepoInfo = manager.dict()
+        JobPool.init(executor(max_workers=num_cpus))
 
     def output(self, dest, output_str):
         """Writes |output_str| to |dest|, holding |lock|;
@@ -535,16 +545,18 @@ class Dumper:
         pass
 
     def Finish(self, stop_pool=True):
-        """Wait for the expected number of jobs to be submitted, and then
-        wait for the pool to finish processing them. By default, will close
-        and clear the pool, but for testcases that need multiple runs, pass
-        stop_pool = False."""
-        with Dumper.jobs_condition:
-            while len(self.jobs_record) != 0:
-                Dumper.jobs_condition.wait()
+        '''Process all pending jobs and any jobs their callbacks submit.
+        By default, will shutdown the executor, but for testcases that
+        need multiple runs, pass stop_pool = False.'''
+        for job, callback in JobPool.as_completed():
+            try:
+                res = job.result()
+            except Exception as e:
+                self.output(sys.stderr, 'Job raised exception: %s' % e)
+                continue
+            callback(res)
         if stop_pool:
-            Dumper.pool.close()
-            Dumper.pool.join()
+            JobPool.shutdown()
 
     def Process(self, file_or_dir):
         """Process a file or all the (valid) files in a directory; processing is performed
@@ -568,14 +580,12 @@ class Dumper:
                     self.ProcessFiles((fullpath,))
 
     def SubmitJob(self, file_key, func_name, args, callback):
-        """Submits a job to the pool of workers; increments the number of submitted jobs."""
-        self.JobStarted(file_key)
-        res = Dumper.pool.apply_async(StartJob, args=(self, Dumper.lock, Dumper.srcdirRepoInfo, func_name, args), callback=callback)
+        """Submits a job to the pool of workers"""
+        JobPool.submit((self, Dumper.lock, Dumper.srcdirRepoInfo, func_name, args), callback)
 
     def ProcessFilesFinished(self, res):
         """Callback from multiprocesing when ProcessFilesWork finishes;
         run the cleanup work, if any"""
-        self.JobFinished(res['files'][-1])
         # only run the cleanup function once per tuple of files
         self.files_record[res['files']] += 1
         if self.files_record[res['files']] == len(self.archs):
@@ -734,7 +744,7 @@ class Dumper_Win32(Dumper):
                                        "CompressionType=LZX", "/D",
                                        "CompressionMemory=21",
                                        path, compressed_file],
-                                      stdout=open("NUL:","w"),
+                                      stdout=open(os.devnull, 'w'),
                                       stderr=subprocess.STDOUT)
             if success == 0 and os.path.exists(compressed_file):
                 os.unlink(path)
@@ -873,14 +883,12 @@ class Dumper_Mac(Dumper):
         # also note, files must be len 1 here, since we're the only ones
         # that ever add more than one file to the list
         self.output_pid(sys.stderr, "Submitting job for Mac pre-processing on file: %s" % (files[0]))
-        self.SubmitJob(files[0], 'ProcessFilesWorkMac', args=(files[0]), callback=self.ProcessFilesMacFinished)
+        self.SubmitJob(files[0], 'ProcessFilesWorkMac', args=(files[0],), callback=self.ProcessFilesMacFinished)
 
     def ProcessFilesMacFinished(self, result):
         if result['status']:
             # kick off new jobs per-arch with our new list of files
             Dumper.ProcessFiles(self, result['files'], after=AfterMac, after_arg=result['files'][0])
-        # only decrement jobs *after* that, since otherwise we'll remove the record for this file
-        self.JobFinished(result['files'][-1])
 
     def ProcessFilesWorkMac(self, file):
         """dump_syms on Mac needs to be run on a dSYM bundle produced
@@ -897,7 +905,7 @@ class Dumper_Mac(Dumper):
         # dsymutil takes --arch=foo instead of -a foo like everything else
         subprocess.call(["dsymutil"] + [a.replace('-a ', '--arch=') for a in self.archs if a]
                         + [file],
-                        stdout=open("/dev/null","w"))
+                        stdout=open(os.devnull, 'w'))
         if not os.path.exists(dsymbundle):
             # dsymutil won't produce a .dSYM for files without symbols
             self.output_pid(sys.stderr, "No symbols found in file: %s" % (file,))
@@ -922,7 +930,7 @@ class Dumper_Mac(Dumper):
                                                   rel_path))
         success = subprocess.call(["tar", "cjf", full_path, os.path.basename(file)],
                                   cwd=os.path.dirname(file),
-                                  stdout=open("/dev/null","w"), stderr=subprocess.STDOUT)
+                                  stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
         if success == 0 and os.path.exists(full_path):
             self.output(sys.stdout, rel_path)
 
