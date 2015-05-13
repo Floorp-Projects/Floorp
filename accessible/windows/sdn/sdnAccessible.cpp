ba4716dc3e713ca789fff0772b442c4cd176b90c
Marco Zehe <mzehe@mozilla.com>
2015-05-13 16:22:19 +0200

    Bug 1162434 - Fix ISimpleDOMNode Unique ID on 64 bit systems. r=surkov, a=sledru
    --HG--
    extra : transplant_source : z%F7%C2s%09%7D%9En%A9%E2E%C8%89b6%DCQ%9F%CF%C9

--- accessible/windows/sdn/sdnAccessible.cpp
+++ accessible/windows/sdn/sdnAccessible.cpp
@@ -106,8 +106,11 @@ sdnAccessible::get_nodeInfo(BSTR __RPC_FAR* aNodeName,
   // focus events, to correlate back to data nodes in their internal object
   // model.
   Accessible* accessible = GetAccessible();
-  *aUniqueID = - NS_PTR_TO_INT32(accessible ? accessible->UniqueID() :
-                                              static_cast<void*>(this));
+  if (accessible) {
+    *aUniqueID = AccessibleWrap::GetChildIDFor(accessible);
+  } else {
+    *aUniqueID = - NS_PTR_TO_INT32(static_cast<void*>(this));
+  }
 
   *aNumChildren = mNode->GetChildCount();
 
