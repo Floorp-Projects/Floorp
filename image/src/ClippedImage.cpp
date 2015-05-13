d57e957ceefa0bb3893cfb706f2689acc88940eb
Seth Fowler <mark.seth.fowler@gmail.com>
2015-05-13 00:23:44 -0700

    Bug 1163878 (Part 1) - Add an IsImageContainerAvailable method to imgIContainer. r=tn

--- image/src/ClippedImage.cpp
+++ image/src/ClippedImage.cpp
@@ -268,6 +268,15 @@ ClippedImage::GetFrameInternal(const nsIntSize& aSize,
   return mCachedSurface->Surface();
 }
 
+NS_IMETHODIMP_(bool)
+ClippedImage::IsImageContainerAvailable(LayerManager* aManager, uint32_t aFlags)
+{
+  if (!ShouldClip()) {
+    return InnerImage()->IsImageContainerAvailable(aManager, aFlags);
+  }
+  return false;
+}
+
 NS_IMETHODIMP_(already_AddRefed<ImageContainer>)
 ClippedImage::GetImageContainer(LayerManager* aManager, uint32_t aFlags)
 {
@@ -281,7 +290,7 @@ ClippedImage::GetImageContainer(LayerManager* aManager, uint32_t aFlags)
     return InnerImage()->GetImageContainer(aManager, aFlags);
   }
 
-  return (nullptr);
+  return nullptr;
 }
 
 static bool
