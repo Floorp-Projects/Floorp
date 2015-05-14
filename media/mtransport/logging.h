4d6eca83170f7d80371b278df9f91daaab54839b
Eric Rahm <erahm@mozilla.com>
2015-05-14 10:13:24 -0700

    Bug 1164622 - Part 1: Remove instances of #ifdef PR_LOGGING in media. r=froydnj
    PR_LOGGING is now always defined, we can remove #ifdefs checking for it.

--- media/mtransport/logging.h
+++ media/mtransport/logging.h
@@ -12,8 +12,6 @@
 #include <sstream>
 #include <prlog.h>
 
-#if defined(PR_LOGGING)
-
 #define ML_EMERG            1
 #define ML_ERROR            2
 #define ML_WARNING          3
@@ -21,7 +19,6 @@
 #define ML_INFO             5
 #define ML_DEBUG            6
 
-// PR_LOGGING is on --> make useful MTLOG macros
 #define MOZ_MTLOG_MODULE(n) \
   static PRLogModuleInfo* getLogModule() {      \
     static PRLogModuleInfo* log;                \
@@ -35,11 +32,5 @@
     std::stringstream str;                                              \
     str << b;                                                           \
     PR_LOG(getLogModule(), level, ("%s", str.str().c_str())); } while(0)
-
-#else
-// PR_LOGGING is off --> make no-op MTLOG macros
-#define MOZ_MTLOG_MODULE(n)
-#define MOZ_MTLOG(level, b)
-#endif // defined(PR_LOGGING)
 
 #endif // logging_h__
