98b2a5481ab78a38907748bb5b513ef90fda3748
Jeff Walden <jwalden@mit.edu>
2015-05-13 21:13:02 -0700

    Bug 1163851 - Actually respect the InHandling parameter in the parser.  |pc->parsingForInit| is now unused except for testing; it'll be removed once this has stuck a bit, as this change *does* change semantics in some edge cases.  r=efaust

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -6294,12 +6294,6 @@ BinaryOpParseNodeKindToJSOp(ParseNodeKind pnk)
     return ParseNodeKindToJSOp[pnk - PNK_BINOP_FIRST];
 }
 
-static bool
-IsBinaryOpToken(TokenKind tok, bool parsingForInit)
-{
-    return tok == TOK_IN ? !parsingForInit : TokenKindIsBinaryOp(tok);
-}
-
 static ParseNodeKind
 BinaryOpTokenKindToParseNodeKind(TokenKind tok)
 {
@@ -6377,10 +6371,8 @@ Parser<ParseHandler>::orExpr1(InHandling inHandling, YieldHandling yieldHandling
         if (!tokenStream.getToken(&tok))
             return null();
 
-        // FIXME: Change this to use |inHandling == InAllowed|, not
-        // |pc->parsingForInit|.
         ParseNodeKind pnk;
-        if (IsBinaryOpToken(tok, oldParsingForInit)) {
+        if (tok == TOK_IN ? inHandling == InAllowed : TokenKindIsBinaryOp(tok)) {
             pnk = BinaryOpTokenKindToParseNodeKind(tok);
         } else {
             tok = TOK_EOF;
