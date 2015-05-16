687e510c941aa5cc180cf207abfd7a5dbb395ebe
Ravi Shankar <wafflespeanut@gmail.com>
2015-05-16 01:52:00 -0400

    Bug 1163342 - WebIDE new project wizard silently fails / gets stuck in endless loop if target directory not writable. r=jryans

--- browser/devtools/webide/content/newapp.js
+++ browser/devtools/webide/content/newapp.js
@@ -129,12 +129,14 @@ function doOK() {
 
   // Create subfolder with fs-friendly name of project
   let subfolder = projectName.replace(/[\\/:*?"<>|]/g, '').toLowerCase();
+  let win = Services.wm.getMostRecentWindow("devtools:webide");
   folder.append(subfolder);
 
   try {
     folder.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
   } catch(e) {
-    console.error(e);
+    win.UI.reportError("error_folderCreationFailed");
+    window.close();
     return false;
   }
 
