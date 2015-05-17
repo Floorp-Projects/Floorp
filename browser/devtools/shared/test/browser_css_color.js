e176c69cc05e2791b14109ed3b130811a8e537dd
Ryan VanderMeulen <ryanvm@gmail.com>
2015-05-17 19:54:49 -0400

    Backed out 7 changesets (bug 1154809) for xpcshell failures.
    Backed out changeset 056f69eee8db (bug 1154809)
    Backed out changeset 13f93d585e42 (bug 1154809)
    Backed out changeset 1e967727087e (bug 1154809)
    Backed out changeset 1f3809022d39 (bug 1154809)
    Backed out changeset 7e730e9cb6b3 (bug 1154809)
    Backed out changeset 139959618516 (bug 1154809)
    Backed out changeset ea59187953f6 (bug 1154809)

--- browser/devtools/shared/test/browser_css_color.js
+++ browser/devtools/shared/test/browser_css_color.js
@@ -42,6 +42,7 @@ function testColorUtils(canvas) {
     testColorMatch(name, hex, hsl, rgb, color.rgba, canvas);
   }
 
+  testProcessCSSString();
   testSetAlpha();
 }
 
@@ -103,6 +104,20 @@ function testColorMatch(name, hex, hsl, rgb, rgba, canvas) {
   test(hex, "hex");
   test(hsl, "hsl");
   test(rgb, "rgb");
+}
+
+function testProcessCSSString() {
+  let before = "border: 1px solid red; border-radius: 5px; " +
+               "color rgb(0, 255, 0); font-weight: bold; " +
+               "background-color: transparent; " +
+               "border-top-color: rgba(0, 0, 255, 0.5);";
+  let expected = "border: 1px solid #F00; border-radius: 5px; " +
+                 "color #0F0; font-weight: bold; " +
+                 "background-color: transparent; " +
+                 "border-top-color: rgba(0, 0, 255, 0.5);";
+  let after = colorUtils.processCSSString(before);
+
+  is(after, expected, "CSS string processed correctly");
 }
 
 function testSetAlpha() {
