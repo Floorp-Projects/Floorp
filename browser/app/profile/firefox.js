113c22a60f3c5d0b14dd12c84e54acb52bd51cda
Alexandre Poirot <poirot.alex@gmail.com>
2015-05-18 20:27:32 +0200

    Bug 1059882 - Enable frame selection by default. r=jryans, a=sledru
    --HG--
    extra : transplant_source : ryU%25j%8EK%AF%E6%18%2A%03%97b6%93%956-%9D

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1379,7 +1379,7 @@ pref("devtools.toolbox.splitconsoleHeight", 100);
 
 // Toolbox Button preferences
 pref("devtools.command-button-pick.enabled", true);
-pref("devtools.command-button-frames.enabled", false);
+pref("devtools.command-button-frames.enabled", true);
 pref("devtools.command-button-splitconsole.enabled", true);
 pref("devtools.command-button-paintflashing.enabled", false);
 pref("devtools.command-button-tilt.enabled", false);
