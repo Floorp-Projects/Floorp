5cbef5bde7c7b156f8e81fba9d84be5575167244
Alexandre Poirot <poirot.alex@gmail.com>
2015-05-18 20:15:35 +0200

    Bug 1161072 - Reset docshell state (disabled js/cache, service workers) from actor instead of client. r=jryans

--- browser/devtools/framework/test/browser_toolbox_custom_host.js
+++ browser/devtools/framework/test/browser_toolbox_custom_host.js
@@ -3,15 +3,7 @@
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
 function test() {
-  Cu.import("resource://gre/modules/Services.jsm");
-  let temp = {}
-  Cu.import("resource:///modules/devtools/gDevTools.jsm", temp);
-  let DevTools = temp.DevTools;
-  Cu.import("resource://gre/modules/devtools/LayoutHelpers.jsm", temp);
-  let LayoutHelpers = temp.LayoutHelpers;
-
-  Cu.import("resource://gre/modules/devtools/Loader.jsm", temp);
-  let devtools = temp.devtools;
+  let {devtools} = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
 
   let Toolbox = devtools.Toolbox;
 
@@ -40,19 +32,27 @@ function test() {
     let json = JSON.parse(event.data);
     if (json.name == "toolbox-close") {
       ok("Got the `toolbox-close` message");
+      window.removeEventListener("message", onMessage);
       cleanup();
     }
   }
 
-  function testCustomHost(toolbox) {
+  function testCustomHost(t) {
+    toolbox = t;
     is(toolbox.doc.defaultView.top, window, "Toolbox is included in browser.xul");
     is(toolbox.doc, iframe.contentDocument, "Toolbox is in the custom iframe");
     executeSoon(() => gBrowser.removeCurrentTab());
   }
 
   function cleanup() {
-    window.removeEventListener("message", onMessage);
     iframe.remove();
-    finish();
+
+    // Even if we received "toolbox-close", the toolbox may still be destroying
+    // toolbox.destroy() returns a singleton promise that ensures
+    // everything is cleaned up before proceeding.
+    toolbox.destroy().then(() => {
+      toolbox = iframe = target = tab = null;
+      finish();
+    });
   }
 }
