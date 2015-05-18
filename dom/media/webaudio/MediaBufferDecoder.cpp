2b9d55f76542b7678ca0b4a95063d201476ba329
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-05-18 16:13:20 +1000

    Bug 1163359 - Ensure we read updated audio configuration. r=cpearce, a=abillings

--- dom/media/webaudio/MediaBufferDecoder.cpp
+++ dom/media/webaudio/MediaBufferDecoder.cpp
@@ -99,6 +99,7 @@ public:
     , mLength(aLength)
     , mDecodeJob(aDecodeJob)
     , mPhase(PhaseEnum::Decode)
+    , mFirstFrameDecoded(false)
   {
     MOZ_ASSERT(aBuffer);
     MOZ_ASSERT(NS_IsMainThread());
@@ -152,6 +153,7 @@ private:
   nsRefPtr<MediaDecoderReader> mDecoderReader;
   MediaInfo mMediaInfo;
   MediaQueue<AudioData> mAudioQueue;
+  bool mFirstFrameDecoded;
 };
 
 NS_IMETHODIMP
@@ -247,7 +249,7 @@ MediaDecodeTask::Decode()
 
   // Tell the decoder reader that we are not going to play the data directly,
   // and that we should not reject files with more channels than the audio
-  // bakend support.
+  // backend support.
   mDecoderReader->SetIgnoreAudioOutputFormat();
 
   nsAutoPtr<MetadataTags> tags;
@@ -280,6 +282,10 @@ MediaDecodeTask::SampleDecoded(AudioData* aData)
 {
   MOZ_ASSERT(!NS_IsMainThread());
   mAudioQueue.Push(aData);
+  if (!mFirstFrameDecoded) {
+    mDecoderReader->ReadUpdatedMetadata(&mMediaInfo);
+    mFirstFrameDecoded = true;
+  }
   RequestSample();
 }
 
