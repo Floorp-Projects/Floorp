70ee5944298d75de6bdc2d3b2de0c38e39827b44
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-05-18 15:42:32 +1000

    Bug 1163485: Part1. Enable MediaFormatReader by default. r=kentuckyfriedtakahe

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -454,9 +454,9 @@ pref("media.mediasource.mp4.enabled", true);
 pref("media.mediasource.webm.enabled", false);
 
 // Enable new MediaFormatReader architecture for mp4 in MSE
-pref("media.mediasource.format-reader.mp4", false);
+pref("media.mediasource.format-reader.mp4", true);
 // Enable new MediaFormatReader architecture for plain mp4.
-pref("media.format-reader.mp4", false);
+pref("media.format-reader.mp4", true);
 
 #ifdef MOZ_WEBSPEECH
 pref("media.webspeech.recognition.enable", false);
