6a378a43b981c8d4586cd3342e185e237f298db2
Jonathan Griffin <jgriffin@mozilla.com>
2015-05-18 16:36:15 -0700

    Bug 1149618 - Remove SpecialPowers from Marionette, r=dburns

--- testing/marionette/client/marionette/tests/unit/test_switch_remote_frame.py
+++ testing/marionette/client/marionette/tests/unit/test_switch_remote_frame.py
@@ -8,22 +8,24 @@ from marionette import MarionetteTestCase
 class TestSwitchRemoteFrame(MarionetteTestCase):
     def setUp(self):
         super(TestSwitchRemoteFrame, self).setUp()
-        self.oop_by_default = self.marionette.execute_script("""
-            try {
-              return SpecialPowers.getBoolPref('dom.ipc.browser_frames.oop_by_default');
-            }
-            catch(e) {}
-            """)
-        self.mozBrowserFramesEnabled = self.marionette.execute_script("""
-            try {
-              return SpecialPowers.getBoolPref('dom.mozBrowserFramesEnabled');
-            }
-            catch(e) {}
-            """)
-        self.marionette.execute_async_script(
-            'SpecialPowers.pushPrefEnv({"set": [["dom.ipc.browser_frames.oop_by_default", true], ["dom.mozBrowserFramesEnabled", true]]}, marionetteScriptFinished);')
-
         with self.marionette.using_context('chrome'):
+            self.oop_by_default = self.marionette.execute_script("""
+                try {
+                  return Services.prefs.getBoolPref('dom.ipc.browser_frames.oop_by_default');
+                }
+                catch(e) {}
+                """)
+            self.mozBrowserFramesEnabled = self.marionette.execute_script("""
+                try {
+                  return Services.prefs.getBoolPref('dom.mozBrowserFramesEnabled');
+                }
+                catch(e) {}
+                """)
+            self.marionette.execute_script("""
+                Services.prefs.setBoolPref("dom.ipc.browser_frames.oop_by_default", true);
+                Services.prefs.setBoolPref("dom.mozBrowserFramesEnabled", true);
+                """)
+
             self.multi_process_browser = self.marionette.execute_script("""
                 try {
                   return Services.appinfo.browserTabsRemoteAutostart;
@@ -31,13 +33,20 @@ class TestSwitchRemoteFrame(MarionetteTestCase):
                   return false;
                 }""")
 
+    def is_main_process(self):
+        return self.marionette.execute_script("""
+            return Components.classes["@mozilla.org/xre/app-info;1"].
+                getService(Components.interfaces.nsIXULRuntime).
+                processType == Components.interfaces.nsIXULRuntime.PROCESS_TYPE_DEFAULT;
+        """, sandbox='system')
+
     def test_remote_frame(self):
         self.marionette.navigate(self.marionette.absolute_url("test.html"))
         self.marionette.push_permission('browser', True)
         self.marionette.execute_script("""
             let iframe = document.createElement("iframe");
-            SpecialPowers.wrap(iframe).mozbrowser = true;
-            SpecialPowers.wrap(iframe).remote = true;
+            iframe.setAttribute('mozbrowser', true);
+            iframe.setAttribute('remote', true);
             iframe.id = "remote_iframe";
             iframe.style.height = "100px";
             iframe.style.width = "100%%";
@@ -46,9 +55,7 @@ class TestSwitchRemoteFrame(MarionetteTestCase):
             """ % self.marionette.absolute_url("test.html"))
         remote_iframe = self.marionette.find_element("id", "remote_iframe")
         self.marionette.switch_to_frame(remote_iframe)
-        main_process = self.marionette.execute_script("""
-            return SpecialPowers.isMainProcess();
-            """)
+        main_process = self.is_main_process()
         self.assertFalse(main_process)
 
     def test_remote_frame_revisit(self):
@@ -57,8 +64,8 @@ class TestSwitchRemoteFrame(MarionetteTestCase):
         self.marionette.push_permission('browser', True)
         self.marionette.execute_script("""
             let iframe = document.createElement("iframe");
-            SpecialPowers.wrap(iframe).mozbrowser = true;
-            SpecialPowers.wrap(iframe).remote = true;
+            iframe.setAttribute('mozbrowser', true);
+            iframe.setAttribute('remote', true);
             iframe.id = "remote_iframe";
             iframe.style.height = "100px";
             iframe.style.width = "100%%";
@@ -67,21 +74,15 @@ class TestSwitchRemoteFrame(MarionetteTestCase):
             """ % self.marionette.absolute_url("test.html"))
         self.marionette.switch_to_frame(self.marionette.find_element("id",
                                                                      "remote_iframe"))
-        main_process = self.marionette.execute_script("""
-            return SpecialPowers.isMainProcess();
-            """)
+        main_process = self.is_main_process()
         self.assertFalse(main_process)
         self.marionette.switch_to_frame()
-        main_process = self.marionette.execute_script("""
-            return SpecialPowers.isMainProcess();
-            """)
+        main_process = self.is_main_process()
         should_be_main_process = not self.multi_process_browser
         self.assertEqual(main_process, should_be_main_process)
         self.marionette.switch_to_frame(self.marionette.find_element("id",
                                                                      "remote_iframe"))
-        main_process = self.marionette.execute_script("""
-            return SpecialPowers.isMainProcess();
-            """)
+        main_process = self.is_main_process()
         self.assertFalse(main_process)
 
     def test_we_can_switch_to_a_remote_frame_by_index(self):
@@ -90,8 +91,8 @@ class TestSwitchRemoteFrame(MarionetteTestCase):
         self.marionette.push_permission('browser', True)
         self.marionette.execute_script("""
             let iframe = document.createElement("iframe");
-            SpecialPowers.wrap(iframe).mozbrowser = true;
-            SpecialPowers.wrap(iframe).remote = true;
+            iframe.setAttribute('mozbrowser', true);
+            iframe.setAttribute('remote', true);
             iframe.id = "remote_iframe";
             iframe.style.height = "100px";
             iframe.style.width = "100%%";
@@ -99,36 +100,31 @@ class TestSwitchRemoteFrame(MarionetteTestCase):
             document.body.appendChild(iframe);
             """ % self.marionette.absolute_url("test.html"))
         self.marionette.switch_to_frame(0)
-        main_process = self.marionette.execute_script("""
-            return SpecialPowers.isMainProcess();
-            """)
+        main_process = self.is_main_process()
         self.assertFalse(main_process)
         self.marionette.switch_to_frame()
-        main_process = self.marionette.execute_script("""
-            return SpecialPowers.isMainProcess();
-            """)
+        main_process = self.is_main_process()
         should_be_main_process = not self.multi_process_browser
         self.assertEqual(main_process, should_be_main_process)
         self.marionette.switch_to_frame(0)
-        main_process = self.marionette.execute_script("""
-            return SpecialPowers.isMainProcess();
-            """)
+        main_process = self.is_main_process()
         self.assertFalse(main_process)
 
     def tearDown(self):
-        if self.oop_by_default is None:
-            self.marionette.execute_script("""
-                SpecialPowers.clearUserPref('dom.ipc.browser_frames.oop_by_default');
-                """)
-        else:
-             self.marionette.execute_async_script(
-            'SpecialPowers.pushPrefEnv({"set": [["dom.ipc.browser_frames.oop_by_default", %s]]}, marionetteScriptFinished);' %
-            ('true' if self.oop_by_default else 'false'))
-        if self.mozBrowserFramesEnabled is None:
-            self.marionette.execute_script("""
-                SpecialPowers.clearUserPref('dom.mozBrowserFramesEnabled');
-                """)
-        else:
-            self.marionette.execute_async_script(
-            'SpecialPowers.pushPrefEnv({"set": [["dom.mozBrowserFramesEnabled", %s]]}, marionetteScriptFinished);' %
-            ('true' if self.mozBrowserFramesEnabled else 'false'))
+        with self.marionette.using_context('chrome'):
+            if self.oop_by_default is None:
+                self.marionette.execute_script("""
+                    Services.prefs.clearUserPref('dom.ipc.browser_frames.oop_by_default');
+                    """)
+            else:
+                self.marionette.execute_async_script(
+                    "Services.prefs.setBoolPref('dom.ipc.browser_frames.oop_by_default', %s);" %
+                    ('true' if self.oop_by_default else 'false'))
+            if self.mozBrowserFramesEnabled is None:
+                self.marionette.execute_script("""
+                    Services.prefs.clearUserPref('dom.mozBrowserFramesEnabled');
+                    """)
+            else:
+                self.marionette.execute_async_script(
+                    "Services.prefs.setBoolPref('dom.mozBrowserFramesEnabled', %s);" %
+                    ('true' if self.mozBrowserFramesEnabled else 'false'))
