bb5a648426f565392a9f5d92f391880c29e93fcf
Jim Chen <nchen@mozilla.com>
2015-05-19 11:27:18 -0400

    Bug 1164090 - Check for Windows path separator in BHR file name; r=snorp

--- xpcom/threads/ThreadStackHelper.cpp
+++ xpcom/threads/ThreadStackHelper.cpp
@@ -611,6 +611,11 @@ ThreadStackHelper::AppendJSEntry(const volatile StackEntry* aEntry,
       // Only keep the file base name for paths outside the above formats.
       basename = strrchr(filename, '/');
       basename = basename ? basename + 1 : filename;
+      // Look for Windows path separator as well.
+      filename = strrchr(basename, '\\');
+      if (filename) {
+        basename = filename + 1;
+      }
     }
 
     size_t len = PR_snprintf(buffer, sizeof(buffer), "%s:%u", basename, lineno);
