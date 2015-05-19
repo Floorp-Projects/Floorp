a790b349e5fe5ab4b7fa7511b4cc999e8b60ab0f
Cervantes Yu <cyu@mozilla.com>
2015-05-19 11:26:21 +0800

    Bug 1151672 - Part 1: Remove the calls to nsThreadManager::SetThreadWorking() and nsThreadManager::SetThreadIdle() due to backout of bug 970307. r=seth

--- image/DecodePool.cpp
+++ image/DecodePool.cpp
@@ -237,10 +237,6 @@ public:
         return work;
       }
 
-#ifdef MOZ_NUWA_PROCESS
-      nsThreadManager::get()->SetThreadIdle(nullptr);
-#endif // MOZ_NUWA_PROCESS
-
       // Nothing to do; block until some work is available.
       mMonitor.Wait();
     } while (true);
@@ -255,10 +251,6 @@ private:
     work.mType = Work::Type::DECODE;
     work.mDecoder = aQueue.ElementAt(0);
     aQueue.RemoveElementAt(0);
-
-#ifdef MOZ_NUWA_PROCESS
-    nsThreadManager::get()->SetThreadWorking();
-#endif // MOZ_NUWA_PROCESS
 
     return work;
   }
