3327554899726435d5bab48c6053a3cc04bfed98
Timothy Nikkel <tnikkel@gmail.com>
2015-05-19 13:46:09 -0500

    Backout bug 1153589 for causing bug 1166326. on a CLOSED TREE

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -570,8 +570,9 @@ pref("apz.test.logging_enabled", false);
 pref("gfx.hidpi.enabled", 2);
 #endif
 
-#if !defined(MOZ_WIDGET_ANDROID)
-// Containerless scrolling for root frames does not yet pass tests on Android.
+#if !defined(MOZ_WIDGET_GONK) && !defined(MOZ_WIDGET_ANDROID)
+// Containerless scrolling for root frames does not yet pass tests on Android
+// or B2G.
 pref("layout.scroll.root-frame-containers", false);
 #endif
 
