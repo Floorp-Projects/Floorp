14c51ad11022bae6d51e6fa3071ed1ca899bcc83
Ryan VanderMeulen <ryanvm@gmail.com>
2015-05-19 15:55:25 -0400

    Backed out changeset 8908f3baa0f7 (bug 1164632) for various xpcshell failures. CLOSED TREE

--- toolkit/devtools/server/actors/pretty-print-worker.js
+++ toolkit/devtools/server/actors/pretty-print-worker.js
@@ -27,24 +27,27 @@
  *     { id, error }
  */
 
-importScripts("resource://gre/modules/devtools/shared/worker-helper.js");
 importScripts("resource://gre/modules/devtools/acorn/acorn.js");
 importScripts("resource://gre/modules/devtools/source-map.js");
 importScripts("resource://gre/modules/devtools/pretty-fast.js");
 
-workerHelper.createTask(self, "pretty-print", ({ url, indent, source }) => {
+self.onmessage = (event) => {
+  const { data: { id, url, indent, source } } = event;
   try {
     const prettified = prettyFast(source, {
       url: url,
       indent: " ".repeat(indent)
     });
 
-    return {
+    self.postMessage({
+      id: id,
       code: prettified.code,
       mappings: prettified.map._mappings
-    };
+    });
+  } catch (e) {
+    self.postMessage({
+      id: id,
+      error: e.message + "\n" + e.stack
+    });
   }
-  catch(e) {
-    return new Error(e.message + "\n" + e.stack);
-  }
-});
+};
