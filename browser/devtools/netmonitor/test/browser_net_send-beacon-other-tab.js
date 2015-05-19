fee5b3b0cc1995949ae556f3960c8e07926c30c6
Panos Astithas <past@mozilla.com>
2015-05-19 12:28:18 +0300

    The network panel should only record beacons from the monitored page (bug 1160837). r=ochameau

---
+++ browser/devtools/netmonitor/test/browser_net_send-beacon-other-tab.js
@@ -0,0 +1,31 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Tests if beacons from other tabs are properly ignored.
+ */
+
+let test = Task.async(function*() {
+  let [, debuggee, monitor] = yield initNetMonitor(SIMPLE_URL);
+  let { RequestsMenu } = monitor.panelWin.NetMonitorView;
+  RequestsMenu.lazyUpdate = false;
+
+  let tab = yield addTab(SEND_BEACON_URL);
+  let beaconDebuggee = tab.linkedBrowser.contentWindow.wrappedJSObject;
+  info("Beacon tab added successfully.");
+
+  is(RequestsMenu.itemCount, 0, "The requests menu should be empty.");
+
+  beaconDebuggee.performRequest();
+  debuggee.location.reload();
+
+  yield waitForNetworkEvents(monitor, 1);
+  is(RequestsMenu.itemCount, 1, "Only the reload should be recorded.");
+  let request = RequestsMenu.getItemAtIndex(0);
+  is(request.attachment.method, "GET", "The method is correct.");
+  is(request.attachment.status, "200", "The status is correct.");
+
+  yield teardown(monitor);
+  removeTab(tab);
+  finish();
+});
