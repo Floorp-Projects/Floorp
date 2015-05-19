b347bbb095071816a3bf69c447a8ec54f42de863
Maksim Lebedev <Alessarik@gmail.com>
2015-05-19 07:03:00 +0200

    Bug 1000870 - Add some features in testing system. r=smaug

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -7628,6 +7628,8 @@ int16_t
 nsContentUtils::GetButtonsFlagForButton(int32_t aButton)
 {
   switch (aButton) {
+    case -1:
+      return WidgetMouseEvent::eNoButtonFlag;
     case WidgetMouseEvent::eLeftButton:
       return WidgetMouseEvent::eLeftButtonFlag;
     case WidgetMouseEvent::eMiddleButton:
