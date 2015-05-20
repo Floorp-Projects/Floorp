533d048a4b7c30e10f39c534a16c06ecd18508bd
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-05-20 09:54:27 +0200

    Bug 1164425: Rename |SocketBase::CloseSocket| to |SocketBase::Close|, r=kmachulis

--- ipc/unixsocket/ListenSocket.cpp
+++ ipc/unixsocket/ListenSocket.cpp
@@ -314,27 +314,6 @@ ListenSocket::~ListenSocket()
   MOZ_ASSERT(!mIO);
 }
 
-void
-ListenSocket::Close()
-{
-  MOZ_ASSERT(NS_IsMainThread());
-
-  if (!mIO) {
-    return;
-  }
-
-  // From this point on, we consider mIO as being deleted. We sever
-  // the relationship here so any future calls to listen or connect
-  // will create a new implementation.
-  mIO->ShutdownOnMainThread();
-
-  XRE_GetIOMessageLoop()->PostTask(FROM_HERE, new SocketIOShutdownTask(mIO));
-
-  mIO = nullptr;
-
-  NotifyDisconnect();
-}
-
 bool
 ListenSocket::Listen(UnixSocketConnector* aConnector,
                      ConnectionOrientedSocket* aCOSocket)
@@ -385,9 +364,24 @@ ListenSocket::GetSocketAddr(nsAString& aAddrStr)
 // |SocketBase|
 
 void
-ListenSocket::CloseSocket()
+ListenSocket::Close()
 {
-  Close();
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (!mIO) {
+    return;
+  }
+
+  // From this point on, we consider mIO as being deleted. We sever
+  // the relationship here so any future calls to listen or connect
+  // will create a new implementation.
+  mIO->ShutdownOnMainThread();
+
+  XRE_GetIOMessageLoop()->PostTask(FROM_HERE, new SocketIOShutdownTask(mIO));
+
+  mIO = nullptr;
+
+  NotifyDisconnect();
 }
 
 } // namespace ipc
