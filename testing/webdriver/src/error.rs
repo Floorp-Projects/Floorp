b0ce54af274fc8302e4792db1b26c902d42be776
James Graham <james@hoppipolla.co.uk>
2015-05-20 12:06:41 +0100

    webdriver: Support errors that are fatal i.e. should result in session teardown.
    Source-Repo: https://github.com/mozilla/webdriver-rust
    Source-Revision: 043d4007dba6bca256a7e8f838ad1ef12a7a8930

--- testing/webdriver/src/error.rs
+++ testing/webdriver/src/error.rs
@@ -30,7 +30,67 @@ pub enum ErrorStatus {
     UnknownError,
     UnknownPath,
     UnknownMethod,
-    UnsupportedOperation,
+    UnsupportedOperation
+}
+
+impl ErrorStatus {
+    pub fn status_code(&self) -> &'static str {
+        match self {
+            &ErrorStatus::ElementNotSelectable => "element not selectable",
+            &ErrorStatus::ElementNotVisible => "element not visible",
+            &ErrorStatus::InvalidArgument => "invalid argument",
+            &ErrorStatus::InvalidCookieDomain => "invalid cookie domain",
+            &ErrorStatus::InvalidElementCoordinates => "invalid element coordinates",
+            &ErrorStatus::InvalidElementState => "invalid element state",
+            &ErrorStatus::InvalidSelector => "invalid selector",
+            &ErrorStatus::InvalidSessionId => "invalid session id",
+            &ErrorStatus::JavascriptError => "javascript error",
+            &ErrorStatus::MoveTargetOutOfBounds => "move target out of bounds",
+            &ErrorStatus::NoSuchAlert => "no such alert",
+            &ErrorStatus::NoSuchElement => "no such element",
+            &ErrorStatus::NoSuchFrame => "no such frame",
+            &ErrorStatus::NoSuchWindow => "no such window",
+            &ErrorStatus::ScriptTimeout => "script timeout",
+            &ErrorStatus::SessionNotCreated => "session not created",
+            &ErrorStatus::StaleElementReference => "stale element reference",
+            &ErrorStatus::Timeout => "timeout",
+            &ErrorStatus::UnableToSetCookie => "unable to set cookie",
+            &ErrorStatus::UnexpectedAlertOpen => "unexpected alert open",
+            &ErrorStatus::UnknownError => "unknown error",
+            &ErrorStatus::UnknownPath => "unknown command",
+            &ErrorStatus::UnknownMethod => "unknown command",
+            &ErrorStatus::UnsupportedOperation => "unsupported operation"
+        }
+    }
+
+    pub fn http_status(&self) -> StatusCode {
+        match self {
+            &ErrorStatus::ElementNotSelectable => StatusCode::BadRequest,
+            &ErrorStatus::ElementNotVisible => StatusCode::BadRequest,
+            &ErrorStatus::InvalidArgument => StatusCode::BadRequest,
+            &ErrorStatus::InvalidCookieDomain => StatusCode::BadRequest,
+            &ErrorStatus::InvalidElementCoordinates => StatusCode::BadRequest,
+            &ErrorStatus::InvalidElementState => StatusCode::BadRequest,
+            &ErrorStatus::InvalidSelector => StatusCode::BadRequest,
+            &ErrorStatus::InvalidSessionId => StatusCode::NotFound,
+            &ErrorStatus::JavascriptError => StatusCode::InternalServerError,
+            &ErrorStatus::MoveTargetOutOfBounds => StatusCode::InternalServerError,
+            &ErrorStatus::NoSuchAlert => StatusCode::BadRequest,
+            &ErrorStatus::NoSuchElement => StatusCode::NotFound,
+            &ErrorStatus::NoSuchFrame => StatusCode::BadRequest,
+            &ErrorStatus::NoSuchWindow => StatusCode::BadRequest,
+            &ErrorStatus::ScriptTimeout => StatusCode::RequestTimeout,
+            &ErrorStatus::SessionNotCreated => StatusCode::InternalServerError,
+            &ErrorStatus::StaleElementReference => StatusCode::BadRequest,
+            &ErrorStatus::Timeout => StatusCode::RequestTimeout,
+            &ErrorStatus::UnableToSetCookie => StatusCode::InternalServerError,
+            &ErrorStatus::UnexpectedAlertOpen => StatusCode::InternalServerError,
+            &ErrorStatus::UnknownError => StatusCode::InternalServerError,
+            &ErrorStatus::UnknownPath => StatusCode::NotFound,
+            &ErrorStatus::UnknownMethod => StatusCode::MethodNotAllowed,
+            &ErrorStatus::UnsupportedOperation => StatusCode::InternalServerError
+        }
+    }
 }
 
 pub type WebDriverResult<T> = Result<T, WebDriverError>;
@@ -38,7 +98,8 @@ pub type WebDriverResult<T> = Result<T, WebDriverError>;
 #[derive(Debug)]
 pub struct WebDriverError {
     pub status: ErrorStatus,
-    pub message: String
+    pub message: String,
+    delete_session: bool
 }
 
 impl fmt::Display for WebDriverError {
@@ -51,70 +112,29 @@ impl WebDriverError {
     pub fn new(status: ErrorStatus, message: &str) -> WebDriverError {
         WebDriverError {
             status: status,
-            message: message.to_string()
+            message: message.to_string(),
+            delete_session: false
         }
     }
 
     pub fn status_code(&self) -> &'static str {
-        match self.status {
-            ErrorStatus::ElementNotSelectable => "element not selectable",
-            ErrorStatus::ElementNotVisible => "element not visible",
-            ErrorStatus::InvalidArgument => "invalid argument",
-            ErrorStatus::InvalidCookieDomain => "invalid cookie domain",
-            ErrorStatus::InvalidElementCoordinates => "invalid element coordinates",
-            ErrorStatus::InvalidElementState => "invalid element state",
-            ErrorStatus::InvalidSelector => "invalid selector",
-            ErrorStatus::InvalidSessionId => "invalid session id",
-            ErrorStatus::JavascriptError => "javascript error",
-            ErrorStatus::MoveTargetOutOfBounds => "move target out of bounds",
-            ErrorStatus::NoSuchAlert => "no such alert",
-            ErrorStatus::NoSuchElement => "no such element",
-            ErrorStatus::NoSuchFrame => "no such frame",
-            ErrorStatus::NoSuchWindow => "no such window",
-            ErrorStatus::ScriptTimeout => "script timeout",
-            ErrorStatus::SessionNotCreated => "session not created",
-            ErrorStatus::StaleElementReference => "stale element reference",
-            ErrorStatus::Timeout => "timeout",
-            ErrorStatus::UnableToSetCookie => "unable to set cookie",
-            ErrorStatus::UnexpectedAlertOpen => "unexpected alert open",
-            ErrorStatus::UnknownError => "unknown error",
-            ErrorStatus::UnknownPath => "unknown command",
-            ErrorStatus::UnknownMethod => "unknown command",
-            ErrorStatus::UnsupportedOperation => "unsupported operation",
-        }
+        self.status.status_code()
     }
 
     pub fn http_status(&self) -> StatusCode {
-        match self.status {
-            ErrorStatus::ElementNotSelectable => StatusCode::BadRequest,
-            ErrorStatus::ElementNotVisible => StatusCode::BadRequest,
-            ErrorStatus::InvalidArgument => StatusCode::BadRequest,
-            ErrorStatus::InvalidCookieDomain => StatusCode::BadRequest,
-            ErrorStatus::InvalidElementCoordinates => StatusCode::BadRequest,
-            ErrorStatus::InvalidElementState => StatusCode::BadRequest,
-            ErrorStatus::InvalidSelector => StatusCode::BadRequest,
-            ErrorStatus::InvalidSessionId => StatusCode::NotFound,
-            ErrorStatus::JavascriptError => StatusCode::InternalServerError,
-            ErrorStatus::MoveTargetOutOfBounds => StatusCode::InternalServerError,
-            ErrorStatus::NoSuchAlert => StatusCode::BadRequest,
-            ErrorStatus::NoSuchElement => StatusCode::NotFound,
-            ErrorStatus::NoSuchFrame => StatusCode::BadRequest,
-            ErrorStatus::NoSuchWindow => StatusCode::BadRequest,
-            ErrorStatus::ScriptTimeout => StatusCode::RequestTimeout,
-            ErrorStatus::SessionNotCreated => StatusCode::InternalServerError,
-            ErrorStatus::StaleElementReference => StatusCode::BadRequest,
-            ErrorStatus::Timeout => StatusCode::RequestTimeout,
-            ErrorStatus::UnableToSetCookie => StatusCode::InternalServerError,
-            ErrorStatus::UnexpectedAlertOpen => StatusCode::InternalServerError,
-            ErrorStatus::UnknownError => StatusCode::InternalServerError,
-            ErrorStatus::UnknownPath => StatusCode::NotFound,
-            ErrorStatus::UnknownMethod => StatusCode::MethodNotAllowed,
-            ErrorStatus::UnsupportedOperation => StatusCode::InternalServerError,
-        }
+        self.status.http_status()
     }
 
     pub fn to_json_string(&self) -> String {
         self.to_json().to_string()
+    }
+
+    pub fn set_delete_session(&mut self) {
+        self.delete_session = true
+    }
+
+    pub fn delete_session(&self) -> bool {
+        self.delete_session
     }
 }
 
