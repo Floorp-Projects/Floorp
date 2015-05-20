f4cd4bcb9a049ef5468cc68d21d88c8fe970e08a
Wander Lairson Costa <wcosta@mozilla.com>
2015-05-20 18:18:13 -0300

    Bug 1148630: Add support for upload symbols from docker image.

--- testing/docker/phone-builder/build.sh
+++ testing/docker/phone-builder/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-while getopts "t:i:k:" arg; do
+while getopts "t:i:k:s:" arg; do
   case $arg in
     t)
       TAG=$OPTARG
@@ -11,6 +11,9 @@ while getopts "t:i:k:" arg; do
     k)
       SECRET_KEY=$OPTARG
       ;;
+    s)
+      SOCORRO_TOKEN=$OPTARG
+      ;;
   esac
 done
 
@@ -19,12 +22,16 @@ pushd $(dirname $0)
 test $TAG
 test $KEY_ID
 test $SECRET_KEY
+test $SOCORRO_TOKEN
 
 (echo '[default]'
 echo "aws_access_key_id = $KEY_ID"
 echo "aws_secret_access_key = $SECRET_KEY") > config
 
+echo $SOCORRO_TOKEN > socorro.token
+
 docker build -t $TAG .
 rm -f config
+rm -f socorro.token
 
 popd
