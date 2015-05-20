512da23eda5d4dcaffff6da700f834bc0cd5434f
Nikhil Marathe <nsm.nikhil@gmail.com>
2015-05-20 12:04:32 -0700

    Bug 1162411 - Fix Request CORS bug. r=bz, a=lizzard

--- dom/tests/mochitest/fetch/test_request.js
+++ dom/tests/mochitest/fetch/test_request.js
@@ -23,7 +23,7 @@ function testDefaultCtor() {
 function testClone() {
   var orig = new Request("./cloned_request.txt", {
               method: 'POST',
-              headers: { "Content-Length": 5 },
+              headers: { "Sample-Header": "5" },
               body: "Sample body",
               mode: "same-origin",
               credentials: "same-origin",
@@ -33,9 +33,9 @@ function testClone() {
   ok(clone.method === "POST", "Request method is POST");
   ok(clone.headers instanceof Headers, "Request should have non-null Headers object");
 
-  is(clone.headers.get('content-length'), "5", "Response content-length should be 5.");
-  orig.headers.set('content-length', 6);
-  is(clone.headers.get('content-length'), "5", "Request content-length should be 5.");
+  is(clone.headers.get('sample-header'), "5", "Request sample-header should be 5.");
+  orig.headers.set('sample-header', 6);
+  is(clone.headers.get('sample-header'), "5", "Cloned Request sample-header should continue to be 5.");
 
   ok(clone.url === (new URL("./cloned_request.txt", self.location.href)).href,
        "URL should be resolved with entry settings object's API base URL");
@@ -122,6 +122,20 @@ function testBug1109574() {
   is(r1.bodyUsed, false, "Request with null body should not have bodyUsed set");
   // This should succeed.
   var r3 = new Request(r1);
+}
+
+function testHeaderGuard() {
+  var headers = {
+    "Cookie": "Custom cookie",
+    "Non-Simple-Header": "value",
+  };
+  var r1 = new Request("", { headers: headers });
+  ok(!r1.headers.has("Cookie"), "Default Request header should have guard request and prevent setting forbidden header.");
+  ok(r1.headers.has("Non-Simple-Header"), "Default Request header should have guard request and allow setting non-simple header.");
+
+  var r2 = new Request("", { mode: "no-cors", headers: headers });
+  ok(!r2.headers.has("Cookie"), "no-cors Request header should have guard request-no-cors and prevent setting non-simple header.");
+  ok(!r2.headers.has("Non-Simple-Header"), "no-cors Request header should have guard request-no-cors and prevent setting non-simple header.");
 }
 
 function testMethod() {
@@ -434,6 +448,7 @@ function runTest() {
   testUrlFragment();
   testMethod();
   testBug1109574();
+  testHeaderGuard();
   testModeCorsPreflightEnumValue();
 
   return Promise.resolve()
