07f4d250ef3855fbe3034e152d046519453c22de
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-05-20 10:50:43 +0200

    Bug 1164425: Cleanup interfaces of |ListenSocket|, r=kmachulis
    This patch removes |ListenSocket::GetSocketAddr|, which is unused
    and not thread-safe. The |Listen| methods now return error codes.

--- ipc/unixsocket/ListenSocket.cpp
+++ ipc/unixsocket/ListenSocket.cpp
@@ -31,8 +31,6 @@ public:
                  UnixSocketConnector* aConnector);
   ~ListenSocketIO();
 
-  void GetSocketAddr(nsAString& aAddrStr) const;
-
   // Task callback methods
   //
 
@@ -112,26 +110,6 @@ ListenSocketIO::~ListenSocketIO()
 {
   MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(IsShutdownOnMainThread());
-}
-
-void
-ListenSocketIO::GetSocketAddr(nsAString& aAddrStr) const
-{
-  if (!mConnector) {
-    NS_WARNING("No connector to get socket address from!");
-    aAddrStr.Truncate();
-    return;
-  }
-
-  nsCString addressString;
-  nsresult rv = mConnector->ConvertAddressToString(
-    *reinterpret_cast<const struct sockaddr*>(&mAddress), mAddressLength,
-    addressString);
-  if (NS_FAILED(rv)) {
-    return;
-  }
-
-  aAddrStr.Assign(NS_ConvertUTF8toUTF16(addressString));
 }
 
 void
@@ -314,51 +292,32 @@ ListenSocket::~ListenSocket()
   MOZ_ASSERT(!mIO);
 }
 
-bool
+nsresult
 ListenSocket::Listen(UnixSocketConnector* aConnector,
                      ConnectionOrientedSocket* aCOSocket)
 {
   MOZ_ASSERT(NS_IsMainThread());
-  MOZ_ASSERT(aConnector);
-  MOZ_ASSERT(aCOSocket);
+  MOZ_ASSERT(!mIO);
 
-  nsAutoPtr<UnixSocketConnector> connector(aConnector);
-
-  if (mIO) {
-    NS_WARNING("Socket already connecting/connected!");
-    return false;
-  }
-
-  mIO = new ListenSocketIO(XRE_GetIOMessageLoop(), this, connector.forget());
+  mIO = new ListenSocketIO(XRE_GetIOMessageLoop(), this, aConnector);
 
   // Prepared I/O object, now start listening.
   return Listen(aCOSocket);
 }
 
-bool
+nsresult
 ListenSocket::Listen(ConnectionOrientedSocket* aCOSocket)
 {
   MOZ_ASSERT(NS_IsMainThread());
-  MOZ_ASSERT(mIO);
   MOZ_ASSERT(aCOSocket);
+  MOZ_ASSERT(mIO);
 
   SetConnectionStatus(SOCKET_LISTENING);
 
   XRE_GetIOMessageLoop()->PostTask(
     FROM_HERE, new ListenSocketIO::ListenTask(mIO, aCOSocket->GetIO()));
 
-  return true;
-}
-
-void
-ListenSocket::GetSocketAddr(nsAString& aAddrStr)
-{
-  aAddrStr.Truncate();
-  if (!mIO || GetConnectionStatus() != SOCKET_CONNECTED) {
-    NS_WARNING("No socket currently open!");
-    return;
-  }
-  mIO->GetSocketAddr(aAddrStr);
+  return NS_OK;
 }
 
 // |SocketBase|
