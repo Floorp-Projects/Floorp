68600e64f460ea697d91fd5240c51751a7ffb2db
Dão Gottwald <dao@mozilla.com>
2015-05-20 12:56:32 +0200

    Bug 1165584 - Remove host from add-on installation error messages. r=dtownsend

--- browser/base/content/browser-addons.js
+++ browser/base/content/browser-addons.js
@@ -154,20 +154,20 @@ const gXPInstallObserver = {
           host = (install.sourceURI instanceof Ci.nsIStandardURL) &&
                  install.sourceURI.host;
 
-        let error = (host || install.error == 0) ? "addonError" : "addonLocalError";
-        if (install.error != 0)
+        let error = (host || install.error == 0) ? "addonInstallError" : "addonLocalInstallError";
+        let args;
+        if (install.error < 0) {
           error += install.error;
-        else if (install.addon.blocklistState == Ci.nsIBlocklistService.STATE_BLOCKED)
+          args = [brandShortName, install.name];
+        } else if (install.addon.blocklistState == Ci.nsIBlocklistService.STATE_BLOCKED) {
           error += "Blocklisted";
-        else
+          args = [install.name];
+        } else {
           error += "Incompatible";
+          args = [brandShortName, Services.appinfo.version, install.name];
+        }
 
-        messageString = gNavigatorBundle.getString(error);
-        messageString = messageString.replace("#1", install.name);
-        if (host)
-          messageString = messageString.replace("#2", host);
-        messageString = messageString.replace("#3", brandShortName);
-        messageString = messageString.replace("#4", Services.appinfo.version);
+        messageString = gNavigatorBundle.getFormattedString(error, args);
 
         PopupNotifications.show(browser, notificationID, messageString, anchorID,
                                 action, null, options);
