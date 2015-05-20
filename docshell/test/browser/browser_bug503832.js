7a12b63674d7de6d01f1abb352310005de4a4082
Neil Deakin <neil@mozilla.com>
2015-05-19 21:20:49 -0400

    Bug 1166269, e10s, fix and reenable browser_bug503832, r=felipe

--- docshell/test/browser/browser_bug503832.js
+++ docshell/test/browser/browser_bug503832.js
@@ -2,53 +2,51 @@
  * https://bugzilla.mozilla.org/show_bug.cgi?id=503832
  */
 
-function test() {
-    waitForExplicitFinish();
-
+add_task(function* () {
     var pagetitle = "Page Title for Bug 503832";
     var pageurl = "http://mochi.test:8888/browser/docshell/test/browser/file_bug503832.html";
     var fragmenturl = "http://mochi.test:8888/browser/docshell/test/browser/file_bug503832.html#firefox";
 
-    /* Global history observer that triggers for the two test URLs above. */
-    var historyObserver = {
-        onBeginUpdateBatch: function() {},
-        onEndUpdateBatch: function() {},
-        onVisit: function(aURI, aVisitID, aTime, aSessionId, aReferringId,
-                          aTransitionType, _added) {},
-        onTitleChanged: function(aURI, aPageTitle) {
-            aURI = aURI.spec;
-            switch (aURI) {
-            case pageurl:
-                is(aPageTitle, pagetitle, "Correct page title for " + aURI);
-                return;
-            case fragmenturl:
-                is(aPageTitle, pagetitle, "Correct page title for " + aURI);
-                // If titles for fragment URLs aren't set, this code
-                // branch won't be called and the test will timeout,
-                // resulting in a failure
-                historyService.removeObserver(historyObserver, false);
-
-                gBrowser.removeCurrentTab();
-
-                finish();
-            }
-        },
-        onDeleteURI: function(aURI) {},
-        onClearHistory: function() {},
-        onPageChanged: function(aURI, aWhat, aValue) {},
-        onDeleteVisits: function () {},
-        QueryInterface: function(iid) {
-            if (iid.equals(Ci.nsINavHistoryObserver) ||
-                iid.equals(Ci.nsISupports)) {
-                return this;
-            }
-            throw Cr.NS_ERROR_NO_INTERFACE;
-        }
-    };
-
     var historyService = Cc["@mozilla.org/browser/nav-history-service;1"]
                          .getService(Ci.nsINavHistoryService);
-    historyService.addObserver(historyObserver, false);
+
+    let fragmentPromise = new Promise(resolve => {
+        /* Global history observer that triggers for the two test URLs above. */
+        var historyObserver = {
+            onBeginUpdateBatch: function() {},
+            onEndUpdateBatch: function() {},
+            onVisit: function(aURI, aVisitID, aTime, aSessionId, aReferringId,
+                              aTransitionType, _added) {},
+            onTitleChanged: function(aURI, aPageTitle) {
+                aURI = aURI.spec;
+                switch (aURI) {
+                case pageurl:
+                    is(aPageTitle, pagetitle, "Correct page title for " + aURI);
+                    return;
+                case fragmenturl:
+                    is(aPageTitle, pagetitle, "Correct page title for " + aURI);
+                    // If titles for fragment URLs aren't set, this code
+                    // branch won't be called and the test will timeout,
+                    // resulting in a failure
+                    historyService.removeObserver(historyObserver, false);
+                    resolve();
+                }
+            },
+            onDeleteURI: function(aURI) {},
+            onClearHistory: function() {},
+            onPageChanged: function(aURI, aWhat, aValue) {},
+            onDeleteVisits: function () {},
+            QueryInterface: function(iid) {
+                if (iid.equals(Ci.nsINavHistoryObserver) ||
+                    iid.equals(Ci.nsISupports)) {
+                    return this;
+                }
+                throw Cr.NS_ERROR_NO_INTERFACE;
+            }
+        };
+
+        historyService.addObserver(historyObserver, false);
+    });
 
     /* Queries nsINavHistoryService and returns a single history entry
      * for a given URI */
@@ -71,18 +69,6 @@ function test() {
         return node;
     }
 
-
-    function onPageLoad() {
-        gBrowser.selectedBrowser.removeEventListener(
-            "DOMContentLoaded", onPageLoad, true);
-
-        // Now that the page is loaded, click on fragment link
-        EventUtils.sendMouseEvent({type:'click'}, 'firefox-link',
-                                  gBrowser.selectedBrowser.contentWindow);
-
-        // Test finishes in historyObserver.onTitleChanged() above
-    }
-
     // Make sure neither of the test pages haven't been loaded before.
     var info = getNavHistoryEntry(makeURI(pageurl));
     ok(!info, "The test page must not have been visited already.");
@@ -90,8 +76,12 @@ function test() {
     ok(!info, "The fragment test page must not have been visited already.");
 
     // Now open the test page in a new tab
-    gBrowser.selectedTab = gBrowser.addTab();
-    gBrowser.selectedBrowser.addEventListener(
-        "DOMContentLoaded", onPageLoad, true);
-    content.location = pageurl;
-}
+    yield BrowserTestUtils.openNewForegroundTab(gBrowser, pageurl);
+
+    // Now that the page is loaded, click on fragment link
+    yield BrowserTestUtils.synthesizeMouseAtCenter("#firefox-link", {},
+                                                   gBrowser.selectedBrowser);
+    yield fragmentPromise;
+
+    gBrowser.removeCurrentTab();
+});
