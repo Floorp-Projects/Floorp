764f88a63cbf4126a0261dcb231e10978ca22545
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2015-05-20 22:42:00 -0400

    Bug 1167056 - Fix broken indentation. r=gwagner
    --HG--
    extra : rebase_source : 9ff9faee43b47795f645ff7e641e51a290d4f115

--- testing/specialpowers/content/SpecialPowersObserverAPI.js
+++ testing/specialpowers/content/SpecialPowersObserverAPI.js
@@ -376,32 +376,32 @@ SpecialPowersObserverAPI.prototype = {
               return;
             }
           case "inject-app":
-	    {
+            {
               let aAppId = aMessage.json.appId;
-	      let aApp   = aMessage.json.app;
+              let aApp   = aMessage.json.app;
 
               let keys = Object.keys(Webapps.DOMApplicationRegistry.webapps);
-	      let exists = keys.indexOf(aAppId) !== -1;
-	      if (exists) {
+              let exists = keys.indexOf(aAppId) !== -1;
+              if (exists) {
                 return false;
-	      }
+              }
 
               Webapps.DOMApplicationRegistry.webapps[aAppId] = aApp;
-	      return true;
-	    }
-	  case "reject-app":
-	    {
+              return true;
+            }
+          case "reject-app":
+            {
               let aAppId = aMessage.json.appId;
 
               let keys = Object.keys(Webapps.DOMApplicationRegistry.webapps);
-	      let exists = keys.indexOf(aAppId) !== -1;
-	      if (!exists) {
+              let exists = keys.indexOf(aAppId) !== -1;
+              if (!exists) {
                 return false;
-	      }
+              }
 
               delete Webapps.DOMApplicationRegistry.webapps[aAppId];
-	      return true;
-	    }
+              return true;
+            }
           default:
             throw new SpecialPowersError("Invalid operation for SPWebAppsService");
         }
