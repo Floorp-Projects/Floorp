d76274d8ca433dcf71b7376f6ff8fd4c2914b3dc
Fabrice Desré <fabrice@mozilla.com>
2015-05-21 10:15:17 -0700

    Bug 1166916 - Javascript Error in post-fork-preload.js r=gwagner

--- dom/ipc/post-fork-preload.js
+++ dom/ipc/post-fork-preload.js
@@ -14,7 +14,7 @@
 
   Components.utils.import("resource://gre/modules/AppsServiceChild.jsm");
   Components.classes["@mozilla.org/network/protocol-proxy-service;1"].
-    getService(Ci["nsIProtocolProxyService"]);
+    getService(Components.interfaces["nsIProtocolProxyService"]);
 
   DOMApplicationRegistry.resetList();
 })(this);
