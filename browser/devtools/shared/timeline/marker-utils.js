750bd8d27ee68307626beb40845ad4f4e949386d
Jordan Santell <jsantell@mozilla.com>
2015-05-22 02:37:06 -0700

    Bug 1166494 -  part3: Correctly handle marker definition fields that are described via function rather than array. r=vp, a=sledru

--- browser/devtools/shared/timeline/marker-utils.js
+++ browser/devtools/shared/timeline/marker-utils.js
@@ -66,6 +66,16 @@ exports.getMarkerClassName = getMarkerClassName;
  */
 function getMarkerFields (marker) {
   let blueprint = TIMELINE_BLUEPRINT[marker.name];
+
+  if (typeof blueprint.fields === "function") {
+    let fields = blueprint.fields(marker);
+    // Add a ":" to the label since the localization files contain the ":"
+    // if not present. This should be changed, ugh.
+    return Object.keys(fields || []).map(label => {
+      let normalizedLabel = label.indexOf(":") !== -1 ? label : (label + ":");
+      return { label: normalizedLabel, value: fields[label] };
+    });
+  }
   return (blueprint.fields || []).reduce((fields, field) => {
     // Ensure this marker has this field present
     if (field.property in marker) {
