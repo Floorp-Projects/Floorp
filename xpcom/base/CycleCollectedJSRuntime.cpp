2b21934f950abcb49e5ed257fbbadc470f4b374e
Yanis Sellami <yanisellami@gmail.com>
2015-05-22 11:22:56 +0200

    Bug 967031 - Rename DumpHeapComplete to DumpHeap; r=terrence

--- xpcom/base/CycleCollectedJSRuntime.cpp
+++ xpcom/base/CycleCollectedJSRuntime.cpp
@@ -1073,7 +1073,7 @@ CycleCollectedJSRuntime::DeferredFinalize(nsISupports* aSupports)
 void
 CycleCollectedJSRuntime::DumpJSHeap(FILE* aFile)
 {
-  js::DumpHeapComplete(Runtime(), aFile, js::CollectNurseryBeforeDump);
+  js::DumpHeap(Runtime(), aFile, js::CollectNurseryBeforeDump);
 }
 
 
