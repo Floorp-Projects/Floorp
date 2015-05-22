0126380c1e7b55e3ac4b46a8f565b58ec57b777b
Eric Faust <efaustbmo@mozilla.com>
2015-05-22 13:09:44 -0700

    Bug 1164777 - Part 2: Make super.prop parse inside inside eval inside arrow functions. (r=shu)

---
+++ js/src/tests/ecma_6/Class/superPropEvalInsideArrow.js
@@ -0,0 +1,11 @@
+class foo {
+    constructor() { }
+
+    method() {
+        return (() => eval('super.toString'));
+    }
+}
+assertEq(new foo().method()(), Object.prototype.toString);
+
+if (typeof reportCompare === "function")
+    reportCompare(0,0,"OK");
