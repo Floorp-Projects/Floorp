8a574c6491daadab9f969abe5644d11fdfb27d62
Edgar Chen <echen@mozilla.com>
2015-05-22 18:14:35 +0800

    Bug 1166174 - Fix MobileConnection.getCellInfoList() and add test case for it. r=hsinyi

--- dom/mobileconnection/tests/marionette/test_mobile_neighboring_cell_ids.js
+++ dom/mobileconnection/tests/marionette/test_mobile_neighboring_cell_ids.js
@@ -2,37 +2,17 @@
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 MARIONETTE_TIMEOUT = 30000;
-// This test must run in chrome context.
-MARIONETTE_CONTEXT = "chrome";
+MARIONETTE_HEAD_JS = "head_chrome.js";
 
-let Promise = Cu.import("resource://gre/modules/Promise.jsm").Promise;
-
-let service = Cc["@mozilla.org/mobileconnection/mobileconnectionservice;1"]
-              .getService(Ci.nsIMobileConnectionService);
-ok(service, "service.constructor is " + service.constructor);
-
-let mobileConnection = service.getItemByServiceId(0);
-ok(mobileConnection, "mobileConnection.constructor is " + mobileConnection.constrctor);
-
-function testGetNeighboringCellIds() {
-  log("Test getting mobile neighboring cell ids");
-  let deferred = Promise.defer();
-
-  mobileConnection.getNeighboringCellIds({
-    notifyGetNeighboringCellIds: function(aResult) {
-      deferred.resolve(aResult);
-    },
-    notifyGetNeighboringCellIdsFailed: function(aError) {
-      deferred.reject(aError);
-    }
-  });
-  return deferred.promise;
-}
-
-// Start tests
-testGetNeighboringCellIds()
-  .then(function resolve(aResult) {
-    ok(false, "getNeighboringCellIds should not success");
-  }, function reject(aError) {
-    is(aError, "RequestNotSupported", "failed to getNeighboringCellIds");
-  }).then(finish);
+// Start test.
+startTestBase(function() {
+  // Emulator doesn't support REQUEST_GET_NEIGHBORING_CELL_IDS, so we expect to
+  // get an 'RequestNotSupported' error here.
+  return getNeighboringCellIds()
+    .then(() => {
+      ok(false, "should not success");
+    }, (aErrorMsg) => {
+      is(aErrorMsg, "RequestNotSupported",
+         "Failed to getNeighboringCellIds: " + aErrorMsg);
+    });
+});
