5912c81b060937427bbbd82e41ca4f9418ccb897
Fernando Jimenez <ferjmoreno@gmail.com>
2015-05-22 09:32:25 +0200

    Bug 1161684 - Allow JAR channels to be intercepted by service workers. Tests. r=jdm

---
+++ dom/workers/test/serviceworkers/app-protocol/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.indexOf('foo.txt') >= 0) {
+    var body = 'swresponse';
+    event.respondWith(new Response(body, {
+      headers: {'Content-Type': 'text/plain'}
+    }));
+  }
+});
+
+self.addEventListener('message', () => {
+  clients.claim();
+});
