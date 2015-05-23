294f741f41e7b2550be781f3d6f351d8365f9e04
Jeff Walden <jwalden@mit.edu>
2015-05-22 19:02:41 -0700

    Bug 1167845 - Rejigger {in,de}crement/assignment parsing to vet targets primarily through a single Parser::isValidSimpleAssignmentTarget method, so that this method can also be called outside the parser.  r=efaust

--- js/src/frontend/FullParseHandler.h
+++ js/src/frontend/FullParseHandler.h
@@ -72,6 +72,21 @@ class FullParseHandler
     typedef ParseNode* Node;
     typedef Definition* DefinitionNode;
 
+    bool isPropertyAccess(ParseNode* node) {
+        if (node->isKind(PNK_DOT) || node->isKind(PNK_ELEM))
+            return true;
+        return node->isKind(PNK_SUPERPROP) || node->isKind(PNK_SUPERELEM);
+    }
+
+    bool isFunctionCall(ParseNode* node) {
+        // Note: super() is a special form, *not* a function call.
+        return node->isKind(PNK_CALL);
+    }
+
+    bool isDestructuringTarget(ParseNode* node) {
+        return node->isKind(PNK_OBJECT) || node->isKind(PNK_ARRAY);
+    }
+
     FullParseHandler(ExclusiveContext* cx, LifoAlloc& alloc,
                      TokenStream& tokenStream, Parser<SyntaxParseHandler>* syntaxParser,
                      LazyScript* lazyOuterFunction)
@@ -88,7 +103,9 @@ class FullParseHandler
     void prepareNodeForMutation(ParseNode* pn) { return allocator.prepareNodeForMutation(pn); }
     const Token& currentToken() { return tokenStream.currentToken(); }
 
-    ParseNode* newName(PropertyName* name, uint32_t blockid, const TokenPos& pos) {
+    ParseNode* newName(PropertyName* name, uint32_t blockid, const TokenPos& pos,
+                       ExclusiveContext* cx)
+    {
         return new_<NameNode>(PNK_NAME, JSOP_GETNAME, name, blockid, pos);
     }
 
@@ -292,6 +309,14 @@ class FullParseHandler
         if (!element->isConstant())
             literal->pn_xflags |= PNX_NONCONST;
         literal->append(element);
+    }
+
+    ParseNode* newCall() {
+        return newList(PNK_CALL, JSOP_CALL);
+    }
+
+    ParseNode* newTaggedTemplate() {
+        return newList(PNK_TAGGED_TEMPLATE, JSOP_CALL);
     }
 
     ParseNode* newObjectLiteral(uint32_t begin) {
@@ -757,7 +782,7 @@ class FullParseHandler
     bool isCall(ParseNode* pn) {
         return pn->isKind(PNK_CALL);
     }
-    PropertyName* isGetProp(ParseNode* pn) {
+    PropertyName* maybeDottedProperty(ParseNode* pn) {
         return pn->is<PropertyAccess>() ? &pn->as<PropertyAccess>().name() : nullptr;
     }
     JSAtom* isStringExprStatement(ParseNode* pn, TokenPos* pos) {
@@ -766,6 +791,15 @@ class FullParseHandler
             return atom;
         }
         return nullptr;
+    }
+
+    void markAsAssigned(ParseNode* node) { node->markAsAssigned(); }
+    void adjustGetToSet(ParseNode* node) {
+        node->setOp(node->isOp(JSOP_GETLOCAL) ? JSOP_SETLOCAL : JSOP_SETNAME);
+    }
+    void maybeDespecializeSet(ParseNode* node) {
+        if (!(js_CodeSpec[node->getOp()].format & JOF_SET))
+            node->setOp(JSOP_SETNAME);
     }
 
     inline ParseNode* makeAssignment(ParseNode* pn, ParseNode* rhs);
