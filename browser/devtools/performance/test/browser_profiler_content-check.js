9be60180e48ce4d91877183ff8dbd3a0e3600452
Victor Porof <vporof@mozilla.com>
2015-05-24 12:12:20 -0400

    Bug 1167963 - FrameNode should not export `isContent`, r=jsantell, a=sledru

--- browser/devtools/performance/test/browser_profiler_content-check.js
+++ browser/devtools/performance/test/browser_profiler_content-check.js
@@ -7,44 +7,44 @@
  */
 
 function test() {
-  let { FrameNode } = devtools.require("devtools/performance/tree-model");
+  let FrameUtils = devtools.require("devtools/performance/frame-utils");
 
-  ok(FrameNode.isContent({ location: "http://foo" }),
+  ok(FrameUtils.isContent({ location: "http://foo" }),
     "Verifying content/chrome frames is working properly.");
-  ok(FrameNode.isContent({ location: "https://foo" }),
+  ok(FrameUtils.isContent({ location: "https://foo" }),
     "Verifying content/chrome frames is working properly.");
-  ok(FrameNode.isContent({ location: "file://foo" }),
-    "Verifying content/chrome frames is working properly.");
-
-  ok(!FrameNode.isContent({ location: "chrome://foo" }),
-    "Verifying content/chrome frames is working properly.");
-  ok(!FrameNode.isContent({ location: "resource://foo" }),
+  ok(FrameUtils.isContent({ location: "file://foo" }),
     "Verifying content/chrome frames is working properly.");
 
-  ok(!FrameNode.isContent({ location: "chrome://foo -> http://bar" }),
+  ok(!FrameUtils.isContent({ location: "chrome://foo" }),
     "Verifying content/chrome frames is working properly.");
-  ok(!FrameNode.isContent({ location: "chrome://foo -> https://bar" }),
-    "Verifying content/chrome frames is working properly.");
-  ok(!FrameNode.isContent({ location: "chrome://foo -> file://bar" }),
+  ok(!FrameUtils.isContent({ location: "resource://foo" }),
     "Verifying content/chrome frames is working properly.");
 
-  ok(!FrameNode.isContent({ location: "resource://foo -> http://bar" }),
+  ok(!FrameUtils.isContent({ location: "chrome://foo -> http://bar" }),
     "Verifying content/chrome frames is working properly.");
-  ok(!FrameNode.isContent({ location: "resource://foo -> https://bar" }),
+  ok(!FrameUtils.isContent({ location: "chrome://foo -> https://bar" }),
     "Verifying content/chrome frames is working properly.");
-  ok(!FrameNode.isContent({ location: "resource://foo -> file://bar" }),
-    "Verifying content/chrome frames is working properly.");
-
-  ok(!FrameNode.isContent({ category: 1, location: "chrome://foo" }),
-    "Verifying content/chrome frames is working properly.");
-  ok(!FrameNode.isContent({ category: 1, location: "resource://foo" }),
+  ok(!FrameUtils.isContent({ location: "chrome://foo -> file://bar" }),
     "Verifying content/chrome frames is working properly.");
 
-  ok(!FrameNode.isContent({ category: 1, location: "file://foo -> http://bar" }),
+  ok(!FrameUtils.isContent({ location: "resource://foo -> http://bar" }),
     "Verifying content/chrome frames is working properly.");
-  ok(!FrameNode.isContent({ category: 1, location: "file://foo -> https://bar" }),
+  ok(!FrameUtils.isContent({ location: "resource://foo -> https://bar" }),
     "Verifying content/chrome frames is working properly.");
-  ok(!FrameNode.isContent({ category: 1, location: "file://foo -> file://bar" }),
+  ok(!FrameUtils.isContent({ location: "resource://foo -> file://bar" }),
+    "Verifying content/chrome frames is working properly.");
+
+  ok(!FrameUtils.isContent({ category: 1, location: "chrome://foo" }),
+    "Verifying content/chrome frames is working properly.");
+  ok(!FrameUtils.isContent({ category: 1, location: "resource://foo" }),
+    "Verifying content/chrome frames is working properly.");
+
+  ok(!FrameUtils.isContent({ category: 1, location: "file://foo -> http://bar" }),
+    "Verifying content/chrome frames is working properly.");
+  ok(!FrameUtils.isContent({ category: 1, location: "file://foo -> https://bar" }),
+    "Verifying content/chrome frames is working properly.");
+  ok(!FrameUtils.isContent({ category: 1, location: "file://foo -> file://bar" }),
     "Verifying content/chrome frames is working properly.");
 
   finish();
