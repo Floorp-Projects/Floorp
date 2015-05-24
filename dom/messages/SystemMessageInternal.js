c9139ca305db311e1a128f52eaff83815b29c99b
Phil Ringnalda <philringnalda@gmail.com>
2015-05-24 13:59:43 -0700

    Back out ffb94ee54752 (bug 1162281) for build and test bustage CLOSED TREE

--- dom/messages/SystemMessageInternal.js
+++ dom/messages/SystemMessageInternal.js
@@ -273,7 +273,7 @@ SystemMessageInternal.prototype = {
                                    type: aType,
                                    msg: aMessage,
                                    extra: aExtra });
-      return Promise.resolve();
+      return;
     }
 
     // Give this message an ID so that we can identify the message and
@@ -285,50 +285,37 @@ SystemMessageInternal.prototype = {
 
     let shouldDispatchFunc = this._getMessageConfigurator(aType).shouldDispatch;
 
-    if (!this._pages.length) {
-      return Promise.resolve();
-    }
+    // Find pages that registered an handler for this type.
+    this._pages.forEach(function(aPage) {
+      if (aPage.type !== aType) {
+        return;
+      }
 
-    // Find pages that registered a handler for this type.
-    let promises = [];
-    for (let i = 0; i < this._pages.length; i++) {
-      let promise = ((page) => {
-        return new Promise((resolve, reject) => {
-          if (page.type !== aType) {
-            resolve();
-            return;
-          }
+      let doDispatch = () => {
+        let result = this._sendMessageCommon(aType,
+                                             aMessage,
+                                             messageID,
+                                             aPage.pageURL,
+                                             aPage.manifestURL,
+                                             aExtra);
+        debug("Returned status of sending message: " + result);
+      };
 
-          let doDispatch = () => {
-            let result = this._sendMessageCommon(aType,
-                                                 aMessage,
-                                                 messageID,
-                                                 page.pageURL,
-                                                 page.manifestURL,
-                                                 aExtra);
-            debug("Returned status of sending message: " + result);
-            resolve();
-          };
+      if ('function' !== typeof shouldDispatchFunc) {
+        // If the configurator has no 'shouldDispatch' defined,
+        // always dispatch this message.
+        doDispatch();
+        return;
+      }
 
-          if ('function' !== typeof shouldDispatchFunc) {
-            // If the configurator has no 'shouldDispatch' defined,
-            // always dispatch this message.
+      shouldDispatchFunc(aPage.manifestURL, aPage.pageURL, aType, aMessage, aExtra)
+        .then(aShouldDispatch => {
+          if (aShouldDispatch) {
             doDispatch();
-            return;
           }
-
-          shouldDispatchFunc(page.manifestURL, page.pageURL, aType, aMessage, aExtra)
-            .then(aShouldDispatch => {
-              if (aShouldDispatch) {
-                doDispatch();
-              }
-            });
         });
-      })(this._pages[i]);
-      promises.push(promise);
-    }
 
-    return Promise.all(promises);
+    }, this);
   },
 
   registerPage: function(aType, aPageURI, aManifestURI) {
