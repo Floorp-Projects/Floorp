84ac31749c4c6ea5fe2e4df3c56d0bbbece72798
Tim Taubert <ttaubert@mozilla.com>
2015-05-25 19:03:33 +0200

    Bug 1158855 - Don't collect children of SHEntries if one or more of those were dynamically added. r=Yoric, a=sledru

--- browser/components/sessionstore/test/browser_dynamic_frames.js
+++ browser/components/sessionstore/test/browser_dynamic_frames.js
@@ -66,11 +66,7 @@ add_task(function () {
 
   // Check URLs.
   ok(entries[0].url.startsWith("data:text/html"), "correct root url");
-  is(entries[0].children[0].url, "about:mozilla", "correct url for static frame");
-
-  // Check the number of children.
-  is(entries.length, 1, "there is one root entry ...");
-  is(entries[0].children.length, 1, "... with a single child entry");
+  ok(!entries[0].children, "no children collected");
 
   // Navigate the subframe.
   browser.messageManager.sendAsyncMessage("ss-test:click", {id: "lnk"});
@@ -82,13 +78,9 @@ add_task(function () {
   // Check URLs.
   ok(entries[0].url.startsWith("data:text/html"), "correct 1st root url");
   ok(entries[1].url.startsWith("data:text/html"), "correct 2nd root url");
-  is(entries[0].children[0].url, "about:mozilla", "correct url for 1st static frame");
-  is(entries[1].children[0].url, "about:robots", "correct url for 2ns static frame");
-
-  // Check the number of children.
-  is(entries.length, 2, "there are two root entries ...");
-  is(entries[0].children.length, 1, "... with a single child entry ...");
-  is(entries[1].children.length, 1, "... each");
+  ok(!entries.children, "no children collected");
+  ok(!entries[0].children, "no children collected");
+  ok(!entries[1].children, "no children collected");
 
   // Cleanup.
   gBrowser.removeTab(tab);
