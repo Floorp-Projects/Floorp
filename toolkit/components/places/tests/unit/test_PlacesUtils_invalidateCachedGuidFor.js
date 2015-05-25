00d8891808aa6aed974a334b5867f5e0c3a8f997
Marco Bonardo <mbonardo@mozilla.com>
2015-05-25 17:18:31 +0200

    Bug 1012597 - Part 1: provide a way to invalidate the Places GUIDs cache. r=rnewman

---
+++ toolkit/components/places/tests/unit/test_PlacesUtils_invalidateCachedGuidFor.js
@@ -0,0 +1,24 @@
+add_task(function* () {
+  do_print("Add a bookmark.");
+  let bm = yield PlacesUtils.bookmarks.insert({ url: "http://example.com/",
+                                                parentGuid: PlacesUtils.bookmarks.unfiledGuid });
+  let id = yield PlacesUtils.promiseItemId(bm.guid);
+  Assert.equal((yield PlacesUtils.promiseItemGuid(id)), bm.guid);
+
+  // Ensure invalidating a non-existent itemId doesn't throw.
+  PlacesUtils.invalidateCachedGuidFor(null);
+  PlacesUtils.invalidateCachedGuidFor(9999);
+
+  do_print("Change the GUID.");
+  let db = yield PlacesUtils.promiseWrappedConnection();
+  yield db.execute("UPDATE moz_bookmarks SET guid = :guid WHERE id = :id",
+                   { guid: "123456789012", id});
+  // The cache should still point to the wrong id.
+  Assert.equal((yield PlacesUtils.promiseItemGuid(id)), bm.guid);
+
+  do_print("Invalidate the cache.");
+  PlacesUtils.invalidateCachedGuidFor(id);
+  Assert.equal((yield PlacesUtils.promiseItemGuid(id)), "123456789012");
+  Assert.equal((yield PlacesUtils.promiseItemId("123456789012")), id);
+  yield Assert.rejects(PlacesUtils.promiseItemId(bm.guid), /no item found for the given GUID/);
+});
