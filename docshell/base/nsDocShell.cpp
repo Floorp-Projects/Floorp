57a6e37e8366a42be6ebe07c821b3114454ebc8f
Tom Tromey <tromey@mozilla.com>
2015-05-26 10:52:52 -0700

    Bug 1050500 - add fields to ProfileTimelineStackFrame; r=smaug, a=sledru

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -13937,7 +13937,7 @@ public:
     aMarker.mCauseName.Construct(GetCause());
 
     if (!mFunctionName.IsEmpty() || !mFileName.IsEmpty()) {
-      ProfileTimelineStackFrame stackFrame;
+      RootedDictionary<ProfileTimelineStackFrame> stackFrame(aCx);
       stackFrame.mLine.Construct(mLineNumber);
       stackFrame.mSource.Construct(mFileName);
       stackFrame.mFunctionDisplayName.Construct(mFunctionName);
