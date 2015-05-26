4aee8d72cea308b5f4a23d14eea585ee1ab2df0f
Maksim Lebedev <alessarik@gmail.com>
2015-05-26 02:00:00 +0200

    Bug 1166347 - Enabling pointer events in Nighly builds. r=mbrubeck. r=smaug

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4415,14 +4415,23 @@ pref("dom.mozPermissionSettings.enabled", false);
 pref("dom.w3c_touch_events.enabled", 2);
 #endif
 
+#ifdef NIGHTLY_BUILD
+#if defined(XP_WIN) || defined(XP_LINUX) || defined(XP_MACOSX)
 // W3C draft pointer events
+pref("dom.w3c_pointer_events.enabled", true);
+// W3C touch-action css property (related to touch and pointer events)
+pref("layout.css.touch_action.enabled", true);
+#else
 pref("dom.w3c_pointer_events.enabled", false);
+pref("layout.css.touch_action.enabled", false);
+#endif
+#else
+pref("dom.w3c_pointer_events.enabled", false);
+pref("layout.css.touch_action.enabled", false);
+#endif
 
 // W3C draft ImageCapture API
 pref("dom.imagecapture.enabled", false);
-
-// W3C touch-action css property (related to touch and pointer events)
-pref("layout.css.touch_action.enabled", false);
 
 // Enables some assertions in nsStyleContext that are too expensive
 // for general use, but might be useful to enable for specific tests.
