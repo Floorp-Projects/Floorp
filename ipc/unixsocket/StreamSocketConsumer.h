5a8451674e80e0a991963cc94ce44c8c6f746407
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-05-26 13:24:19 +0200

    Bug 1166638: Add |StreamSocketConsumer|, r=kmachulis
    |StreamSocketConsumer| receives events and data from a stream
    socket. It unifies all existing consumer and listener classes
    of the socket I/O users.

---
+++ ipc/unixsocket/StreamSocketConsumer.h
@@ -0,0 +1,60 @@
+/* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_ipc_streamsocketconsumer_h
+#define mozilla_ipc_streamsocketconsumer_h
+
+#include "nsAutoPtr.h"
+
+namespace mozilla {
+namespace ipc {
+
+class UnixSocketBuffer;
+
+/**
+ * |StreamSocketConsumer| handles socket events and received data.
+ */
+class StreamSocketConsumer
+{
+public:
+  /**
+   * Method to be called whenever data is received. Main-thread only.
+   *
+   * @param aIndex The index that has been given to the stream socket.
+   * @param aBuffer Data received from the socket.
+   */
+  virtual void ReceiveSocketData(int aIndex,
+                                 nsAutoPtr<UnixSocketBuffer>& aBuffer) = 0;
+
+  /**
+   * Callback for socket success. Main-thread only.
+   *
+   * @param aIndex The index that has been given to the stream socket.
+   */
+  virtual void OnConnectSuccess(int aIndex) = 0;
+
+  /**
+   * Callback for socket errors. Main-thread only.
+   *
+   * @param aIndex The index that has been given to the stream socket.
+   */
+  virtual void OnConnectError(int aIndex) = 0;
+
+  /**
+   * Callback for socket disconnect. Main-thread only.
+   *
+   * @param aIndex The index that has been given to the stream socket.
+   */
+  virtual void OnDisconnect(int aIndex) = 0;
+
+protected:
+  virtual ~StreamSocketConsumer();
+};
+
+}
+}
+
+#endif
