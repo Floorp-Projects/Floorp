5b9df4e5b5323e612b7004637fd22a4819c26b68
Mike Hommey <mh+mozilla@glandium.org>
2015-05-28 07:38:53 +0900

    Bug 991983 - Add a deprecation hint for GENERATED_SOURCES. r=gps

--- python/mozbuild/mozbuild/frontend/context.py
+++ python/mozbuild/mozbuild/frontend/context.py
@@ -1864,6 +1864,16 @@ DEPRECATION_HINTS = {
 
             NO_DIST_INSTALL = True
     ''',
+
+    'GENERATED_SOURCES': '''
+        Please use
+
+            SOURCES += [ '!foo.cpp' ]
+
+        instead of
+
+            GENERATED_SOURCES += [ 'foo.cpp']
+    ''',
 }
 
 # Make sure that all template variables have a deprecation hint.
