32c217fec6d2240365004d1c88594e1e2bede890
Yoshi Huang <allstars.chh@mozilla.com>
2015-05-27 17:31:43 +0800

    Bug 1168292 - Part 2: notify listener with correct tabId. r=dimi

--- dom/nfc/gonk/Nfc.js
+++ dom/nfc/gonk/Nfc.js
@@ -214,13 +214,26 @@ XPCOMUtils.defineLazyGetter(this, "gMessageManager", function () {
       });
     },
 
+    notifyFocusApp: function notifyFocusApp(options) {
+      let target, tabId;
+      if (this.eventListeners[this.focusApp]) {
+        target = this.eventListeners[this.focusApp];
+        tabId = this.focusApp;
+      } else {
+        target = this.eventListeners[NFC.SYSTEM_APP_ID];
+        tabId = NFC.SYSTEM_APP_ID;
+      }
+      options.tabId = tabId;
+
+      this.notifyDOMEvent(target, options);
+    },
+
     notifyDOMEvent: function notifyDOMEvent(target, options) {
       if (!target) {
         dump("invalid target");
         return;
       }
 
-      options.tabId = this.focusApp;
       target.sendAsyncMessage("NFC:DOMEvent", options);
     },
 
@@ -288,7 +301,8 @@ XPCOMUtils.defineLazyGetter(this, "gMessageManager", function () {
         return;
       }
 
-      this.notifyDOMEvent(target, {event: NFC.PEER_EVENT_READY,
+      this.notifyDOMEvent(target, {tabId: this.focusApp,
+                                   event: NFC.PEER_EVENT_READY,
                                    sessionToken: sessionToken});
     },
 
@@ -313,36 +327,26 @@ XPCOMUtils.defineLazyGetter(this, "gMessageManager", function () {
     },
 
     onTagFound: function onTagFound(message) {
-      let target = this.eventListeners[this.focusApp] ||
-                   this.eventListeners[NFC.SYSTEM_APP_ID];
-
       message.event = NFC.TAG_EVENT_FOUND;
-
-      this.notifyDOMEvent(target, message);
-
+      this.notifyFocusApp(message);
       delete message.event;
     },
 
     onTagLost: function onTagLost(sessionToken) {
-      let target = this.eventListeners[this.focusApp] ||
-                   this.eventListeners[NFC.SYSTEM_APP_ID];
-
-      this.notifyDOMEvent(target, { event: NFC.TAG_EVENT_LOST,
-                                    sessionToken: sessionToken });
+      this.notifyFocusApp({ event: NFC.TAG_EVENT_LOST,
+                            sessionToken: sessionToken });
     },
 
     onPeerEvent: function onPeerEvent(eventType, sessionToken) {
-      let target = this.eventListeners[this.focusApp] ||
-                   this.eventListeners[NFC.SYSTEM_APP_ID];
-
-      this.notifyDOMEvent(target, { event: eventType,
-                                    sessionToken: sessionToken });
+      this.notifyFocusApp({ event: eventType,
+                            sessionToken: sessionToken });
     },
 
     onRFStateChanged: function onRFStateChanged(rfState) {
       for (let id in this.eventListeners) {
         this.notifyDOMEvent(this.eventListeners[id],
-                            { event: NFC.RF_EVENT_STATE_CHANGED,
+                            { tabId: id,
+                              event: NFC.RF_EVENT_STATE_CHANGED,
                               rfState: rfState });
       }
     },
@@ -353,7 +357,8 @@ XPCOMUtils.defineLazyGetter(this, "gMessageManager", function () {
         return;
       }
 
-      this.notifyDOMEvent(target, { event: NFC.FOCUS_CHANGED,
+      this.notifyDOMEvent(target, { tabId: this.focusApp,
+                                    event: NFC.FOCUS_CHANGED,
                                     focus: focus });
     },
 
