7cade29011dfa4e0f71d758bb69e966c2d1ec806
Amanda Sambath <amanda.sambath@phelma.grenoble-inp.fr>
2015-05-28 11:59:15 +0200

    Bug 1158120 - Replace gfxIntSize by mozilla::gfx::IntSize in gfx/layers. r=nical

--- gfx/layers/opengl/CompositorOGL.cpp
+++ gfx/layers/opengl/CompositorOGL.cpp
@@ -126,8 +126,7 @@ CompositorOGL::CreateContext()
     caps.bpp16 = gfxPlatform::GetPlatform()->GetOffscreenFormat() == gfxImageFormat::RGB16_565;
 
     bool requireCompatProfile = true;
-    context = GLContextProvider::CreateOffscreen(gfxIntSize(mSurfaceSize.width,
-                                                            mSurfaceSize.height),
+    context = GLContextProvider::CreateOffscreen(mSurfaceSize,
                                                  caps, requireCompatProfile);
   }
 
@@ -1342,7 +1341,7 @@ CompositorOGL::EndFrameForExternalComposition(const gfx::Matrix& aTransform)
 }
 
 void
-CompositorOGL::SetDestinationSurfaceSize(const gfx::IntSize& aSize)
+CompositorOGL::SetDestinationSurfaceSize(const IntSize& aSize)
 {
   mSurfaceSize.width = aSize.width;
   mSurfaceSize.height = aSize.height;
