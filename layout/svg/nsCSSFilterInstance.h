80837c79a1176078141b070df3436b4d850eaa12
Markus Stange <mstange@themasta.com>
2015-05-28 15:51:58 -0400

    Bug 1153845 - For CSS filters, make the primitive subregions big enough to contain the whole pre-filter visual overflow rect. r=roc
    The BBox of the SVG element only includes its contents, not the SVG element's background color.

--- layout/svg/nsCSSFilterInstance.h
+++ layout/svg/nsCSSFilterInstance.h
@@ -36,14 +36,14 @@ public:
    *   aFilter during the lifetime of nsCSSFilterInstance.
    * @param aShadowFallbackColor The color that should be used for
    *   drop-shadow() filters that don't specify a shadow color.
-   * @param aTargetBBoxInFilterSpace The frame of element being filtered, in
-   *   filter space.
+   * @param aTargetBoundsInFilterSpace The pre-filter visual overflow rect of
+   *   the frame being filtered, in filter space.
    * @param aFrameSpaceInCSSPxToFilterSpaceTransform The transformation from
    *   the filtered element's frame space in CSS pixels to filter space.
    */
   nsCSSFilterInstance(const nsStyleFilter& aFilter,
                       nscolor aShadowFallbackColor,
-                      const nsIntRect& aTargetBBoxInFilterSpace,
+                      const nsIntRect& aTargetBoundsInFilterSpace,
                       const gfxMatrix& aFrameSpaceInCSSPxToFilterSpaceTransform);
 
   /**
@@ -119,10 +119,10 @@ private:
   nscolor mShadowFallbackColor;
 
   /**
-   * The bounding box of the element being filtered, in filter space. Used for
-   * input bounds if this CSS filter is the first in the filter chain.
+   * The pre-filter overflow rect of the frame being filtered, in filter space.
+   * Used for input bounds if this CSS filter is the first in the filter chain.
    */
-  nsIntRect mTargetBBoxInFilterSpace;
+  nsIntRect mTargetBoundsInFilterSpace;
 
   /**
    * The transformation from the filtered element's frame space in CSS pixels to
