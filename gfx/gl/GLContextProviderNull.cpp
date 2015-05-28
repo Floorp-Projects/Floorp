51475eee9b17ffc38babbf0ef6625c8e369d66bc
Amanda Sambath <amanda.sambath@phelma.grenoble-inp.fr>
2015-05-28 11:45:33 +0200

    Bug 1158120 - Replace gfxIntSize by mozilla::gfx::IntSize in gfx/gl part 1. r=nical

--- gfx/gl/GLContextProviderNull.cpp
+++ gfx/gl/GLContextProviderNull.cpp
@@ -21,7 +21,7 @@ GLContextProviderNull::CreateWrappingExisting(void*, void*)
 }
 
 already_AddRefed<GLContext>
-GLContextProviderNull::CreateOffscreen(const gfxIntSize&,
+GLContextProviderNull::CreateOffscreen(const gfx::IntSize&,
                                        const SurfaceCaps&,
                                        bool)
 {
