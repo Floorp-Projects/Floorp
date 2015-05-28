835ae0c177d881152c9d97c78ea8b4b51e636aea
Jan de Mooij <jdemooij@mozilla.com>
2015-05-28 10:16:24 +0200

    Bug 1160884 - Add KeepAlive instructions after elements/slots uses. r=nbp, a=abillings

--- js/src/jit/Ion.cpp
+++ js/src/jit/Ion.cpp
@@ -1486,6 +1486,13 @@ OptimizeMIR(MIRGenerator* mir)
         AssertGraphCoherency(graph);
     }
 
+    if (!mir->compilingAsmJS()) {
+        AutoTraceLog log(logger, TraceLogger_AddKeepAliveInstructions);
+        AddKeepAliveInstructions(graph);
+        IonSpewPass("Add KeepAlive Instructions");
+        AssertGraphCoherency(graph);
+    }
+
     return true;
 }
 
