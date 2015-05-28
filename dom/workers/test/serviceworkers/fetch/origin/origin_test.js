1315a45cc406be36bceeb4c752f91765a756b9c3
Ehsan Akhgari <ehsan@mozilla.com>
2015-05-28 14:06:19 -0400

    Bug 1164397 - Part 7: Add a test case for the redirected Response object being stored in the DOM Cache; r=jdm
    --HG--
    rename : dom/workers/test/serviceworkers/test_origin_after_redirect.html => dom/workers/test/serviceworkers/test_origin_after_redirect_cached.html

--- dom/workers/test/serviceworkers/fetch/origin/origin_test.js
+++ dom/workers/test/serviceworkers/fetch/origin/origin_test.js
@@ -1,3 +1,23 @@
+var prefix = "/tests/dom/workers/test/serviceworkers/fetch/origin/";
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    self.caches.open("origin-cache")
+      .then(c => {
+        return c.add(prefix + 'index.sjs');
+      })
+  );
+});
+
 self.addEventListener("fetch", function(event) {
-  event.respondWith(fetch(event.request));
+  if (event.request.url.indexOf("index-cached.sjs") >= 0) {
+    event.respondWith(
+      self.caches.open("origin-cache")
+        .then(c => {
+          return c.match(prefix + 'index.sjs');
+        })
+    );
+  } else {
+    event.respondWith(fetch(event.request));
+  }
 });
