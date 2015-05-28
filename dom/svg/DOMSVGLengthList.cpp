d7f1be6b9bc81d46c09d41f2fce6c1f9b636797b
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-05-28 11:07:43 -0700

    Bug 968520 - Add mozilla::fallible to FallibleTArray::InsertElementAt calls. r=froydnj

--- dom/svg/DOMSVGLengthList.cpp
+++ dom/svg/DOMSVGLengthList.cpp
@@ -266,7 +266,7 @@ DOMSVGLengthList::InsertItemBefore(DOMSVGLength& newItem,
   MaybeInsertNullInAnimValListAt(index);
 
   InternalList().InsertItem(index, domItem->ToSVGLength());
-  MOZ_ALWAYS_TRUE(mItems.InsertElementAt(index, domItem.get()));
+  MOZ_ALWAYS_TRUE(mItems.InsertElementAt(index, domItem.get(), fallible));
 
   // This MUST come after the insertion into InternalList(), or else under the
   // insertion into InternalList() the values read from domItem would be bad
@@ -380,7 +380,7 @@ DOMSVGLengthList::MaybeInsertNullInAnimValListAt(uint32_t aIndex)
   MOZ_ASSERT(animVal->mItems.Length() == mItems.Length(),
              "animVal list not in sync!");
 
-  MOZ_ALWAYS_TRUE(animVal->mItems.InsertElementAt(aIndex, nullptr));
+  MOZ_ALWAYS_TRUE(animVal->mItems.InsertElementAt(aIndex, nullptr, fallible));
 
   UpdateListIndicesFromIndex(animVal->mItems, aIndex + 1);
 }
