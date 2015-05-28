2a921a47435e899e3770d085907dbfc6e9c9dd14
Victor Porof <vporof@mozilla.com>
2015-05-28 18:09:25 -0400

    Backed out changeset 549e09a3e84d for breaking the web audio editor, r=permaorange

--- browser/devtools/shared/test/browser_graphs-15.js
+++ browser/devtools/shared/test/browser_graphs-15.js
@@ -19,7 +19,7 @@ for (let frameRate of FRAMES) {
   }
 }
 
-let {LineGraphWidget} = devtools.require("devtools/shared/widgets/Graphs");
+let {LineGraphWidget} = Cu.import("resource:///modules/devtools/Graphs.jsm", {});
 let {Promise} = devtools.require("resource://gre/modules/Promise.jsm");
 
 add_task(function*() {
--- browser/devtools/webaudioeditor/includes.js
+++ browser/devtools/webaudioeditor/includes.js
@@ -23,9 +23,8 @@ const STRINGS_URI = "chrome://browser/locale/devtools/webaudioeditor.properties"
 const L10N = new ViewHelpers.L10N(STRINGS_URI);
 const Telemetry = require("devtools/shared/telemetry");
 const telemetry = new Telemetry();
-
-devtools.lazyRequireGetter("LineGraphWidget",
-  "devtools/shared/widgets/Graphs", true);
+devtools.lazyImporter(this, "LineGraphWidget",
+  "resource:///modules/devtools/Graphs.jsm");
 
 // `AUDIO_NODE_DEFINITION` defined in the controller's initialization,
 // which describes all the properties of an AudioNode
