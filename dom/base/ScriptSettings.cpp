f9627534842100e3757098efca28f9396e9d1532
Tom Tromey <tromey@mozilla.com>
2015-05-28 11:57:00 -0400

    Bug 1168593 - Make AutoStableStringChars::init use MOZ_WARN_UNUSED_RESULT. r=jandem

--- dom/base/ScriptSettings.cpp
+++ dom/base/ScriptSettings.cpp
@@ -590,7 +590,10 @@ AutoEntryScript::DocshellEntryMonitor::Entry(JSContext* aCx, JSFunction* aFuncti
   if (rootedFunction) {
     JS::Rooted<JSString*> displayId(aCx, JS_GetFunctionDisplayId(rootedFunction));
     if (displayId) {
-      functionName.initTwoByte(aCx, displayId);
+      if (!functionName.initTwoByte(aCx, displayId)) {
+        JS_ClearPendingException(aCx);
+        return;
+      }
     }
   }
 
