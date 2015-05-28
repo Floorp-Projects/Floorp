6fd724199e6ffc9f37cb05e44aa0d62c84e37b0e
Michael Layzell <michael@thelayzells.com>
2015-05-28 09:02:00 -0400

    Bug 1168156 - Remove unused nsCSSValueTokenStream::mSheet. r=heycam

--- layout/style/nsCSSValue.h
+++ layout/style/nsCSSValue.h
@@ -1537,7 +1537,8 @@ public:
   nsCOMPtr<nsIURI> mBaseURI;
   nsCOMPtr<nsIURI> mSheetURI;
   nsCOMPtr<nsIPrincipal> mSheetPrincipal;
-  mozilla::CSSStyleSheet* mSheet;
+  // XXX Should store sheet here (see Bug 952338)
+  // mozilla::CSSStyleSheet* mSheet;
   uint32_t mLineNumber;
   uint32_t mLineOffset;
 
