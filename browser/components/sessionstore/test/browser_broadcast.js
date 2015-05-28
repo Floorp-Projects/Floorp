cb67482cecdff578cebf0b5c79c00a797742f419
Tim Taubert <ttaubert@mozilla.com>
2015-05-29 01:18:49 +0200

    Bug 1167579 - Fix intermittent browser_broadcast.js failures by removing unneeded check that was missing a flush() call. r=billm, a=test-only

--- browser/components/sessionstore/test/browser_broadcast.js
+++ browser/components/sessionstore/test/browser_broadcast.js
@@ -54,13 +54,9 @@ add_task(function flush_on_duplicate() {
 
   yield modifySessionStorage(browser, {test: "on-duplicate"});
   let tab2 = ss.duplicateTab(window, tab);
-  let {storage} = JSON.parse(ss.getTabState(tab2));
-  is(storage["http://example.com"].test, "on-duplicate",
-    "sessionStorage data has been flushed when duplicating tabs");
-
   yield promiseTabRestored(tab2);
   yield promiseRemoveTab(tab2);
-  [{state: {storage}}] = JSON.parse(ss.getClosedTabData(window));
+  let [{state: {storage}}] = JSON.parse(ss.getClosedTabData(window));
   is(storage["http://example.com"].test, "on-duplicate",
     "sessionStorage data has been flushed when duplicating tabs");
 
