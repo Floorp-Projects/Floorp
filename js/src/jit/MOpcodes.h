9fda4a24a9be2b0d12f194cb000cb147c4e27dd9
Jan de Mooij <jdemooij@mozilla.com>
2015-05-28 10:16:24 +0200

    Bug 1160884 - Add KeepAlive instructions after elements/slots uses. r=nbp, a=abillings

--- js/src/jit/MOpcodes.h
+++ js/src/jit/MOpcodes.h
@@ -142,6 +142,7 @@ namespace jit {
     _(StringReplace)                                                        \
     _(Lambda)                                                               \
     _(LambdaArrow)                                                          \
+    _(KeepAliveObject)                                                      \
     _(Slots)                                                                \
     _(Elements)                                                             \
     _(ConstantElements)                                                     \
