859e49874bf651a7b8a20b86b20a4d375fb8812e
Randell Jesup <rjesup@jesup.org>
2015-05-29 10:13:39 -0400

    Bug 1109338: Part 1: Separate UDPSocket logging from network logging r=dragana

--- netwerk/base/nsUDPSocket.cpp
+++ netwerk/base/nsUDPSocket.cpp
@@ -487,7 +487,7 @@ nsUDPSocket::AddOutputBytes(uint64_t aBytes)
 void
 nsUDPSocket::OnMsgClose()
 {
-  SOCKET_LOG(("nsUDPSocket::OnMsgClose [this=%p]\n", this));
+  UDPSOCKET_LOG(("nsUDPSocket::OnMsgClose [this=%p]\n", this));
 
   if (NS_FAILED(mCondition))
     return;
@@ -505,7 +505,7 @@ nsUDPSocket::OnMsgClose()
 void
 nsUDPSocket::OnMsgAttach()
 {
-  SOCKET_LOG(("nsUDPSocket::OnMsgAttach [this=%p]\n", this));
+  UDPSOCKET_LOG(("nsUDPSocket::OnMsgAttach [this=%p]\n", this));
 
   if (NS_FAILED(mCondition))
     return;
@@ -1314,7 +1314,7 @@ nsUDPSocket::SetSocketOption(const PRSocketOptionData& aOpt)
   }
 
   if (PR_SetSocketOption(mFD, &aOpt) != PR_SUCCESS) {
-    SOCKET_LOG(("nsUDPSocket::SetSocketOption [this=%p] failed for type %d, "
+    UDPSOCKET_LOG(("nsUDPSocket::SetSocketOption [this=%p] failed for type %d, "
       "error %d\n", this, aOpt.option, PR_GetError()));
     return NS_ERROR_FAILURE;
   }
