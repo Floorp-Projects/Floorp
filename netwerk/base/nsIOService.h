d9936e6d1274a7aa68727695c366e6b762444120
Dragana Damjanovic <dd.mozilla@gmail.com>
2015-05-29 02:04:00 -0400

    Bug 1152046 - Don't try to connect if we are shutting down. r=mayhemer, a=sledru

--- netwerk/base/nsIOService.h
+++ netwerk/base/nsIOService.h
@@ -78,6 +78,7 @@ public:
                                     nsAsyncRedirectVerifyHelper *helper);
 
     bool IsOffline() { return mOffline; }
+    bool IsShutdown() { return mShutdown; }
     bool IsLinkUp();
 
     // Should only be called from NeckoChild. Use SetAppOffline instead.
