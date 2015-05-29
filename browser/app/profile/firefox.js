7795849634d22f9adc05ff5d70db2c8322f5d16f
Jared Wein <jwein@mozilla.com>
2015-05-29 12:48:00 +0200

    Bug 1170304 - Persist state that Firefox was a user's default browser. r=dolske, a=lizzard
    --HG--
    extra : transplant_source : v%7B%03Z%B1%0F%84%F7%E4%18%3B%0E%CE%E8%3B%AC%A4%A12C

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -273,6 +273,7 @@ pref("general.autoScroll", true);
 // At startup, check if we're the default browser and prompt user if not.
 pref("browser.shell.checkDefaultBrowser", true);
 pref("browser.shell.shortcutFavicons",true);
+pref("browser.shell.mostRecentDateSetAsDefault", "");
 
 // 0 = blank, 1 = home (browser.startup.homepage), 2 = last visited page, 3 = resume previous browser session
 // The behavior of option 3 is detailed at: http://wiki.mozilla.org/Session_Restore
