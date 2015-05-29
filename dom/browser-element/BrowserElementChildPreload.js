930310e210e3195e9b8553b05f588ec0cc641013
Kevin Grandon <kevingrandon@yahoo.com>
2015-05-29 05:58:00 +0200

    Bug 1162844 - Add meta name="viewmode" to have configurable VR experiences. r=fabrice
    --HG--
    extra : rebase_source : cfabbfd656966e42b8bc00dfaf1bb6356118c5e0

--- dom/browser-element/BrowserElementChildPreload.js
+++ dom/browser-element/BrowserElementChildPreload.js
@@ -530,6 +530,7 @@ BrowserElementChild.prototype = {
     debug('Got metaChanged: (' + e.target.name + ') ' + e.target.content);
 
     let handlers = {
+      'viewmode': this._viewmodeChangedHandler,
       'theme-color': this._themeColorChangedHandler,
       'application-name': this._applicationNameChangedHandler
     };
@@ -700,6 +701,16 @@ BrowserElementChild.prototype = {
     }
 
     sendAsyncMsg('selectionstatechanged', detail);
+  },
+
+
+  _viewmodeChangedHandler: function(eventType, target) {
+    let meta = {
+      name: 'viewmode',
+      content: target.content,
+      type: eventType.replace('DOMMeta', '').toLowerCase()
+    };
+    sendAsyncMsg('metachange', meta);
   },
 
   _themeColorChangedHandler: function(eventType, target) {
