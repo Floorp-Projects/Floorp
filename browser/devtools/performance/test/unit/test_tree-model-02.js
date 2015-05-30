bd8e74552dff6ab66667c85e55e910ea332ef129
Jordan Santell <jsantell@mozilla.com>
2015-05-29 17:34:32 -0700

    Bug 1163356 - Migrate performance tool components to xpcshell tests where appropriate. r=vp
    --HG--
    rename : browser/devtools/performance/test/browser_profiler-frame-utils-01.js => browser/devtools/performance/test/unit/test_frame-utils-01.js
    rename : browser/devtools/performance/test/browser_profiler_tree-model-01.js => browser/devtools/performance/test/unit/test_tree-model-01.js
    rename : browser/devtools/performance/test/browser_profiler_tree-model-02.js => browser/devtools/performance/test/unit/test_tree-model-02.js
    rename : browser/devtools/performance/test/browser_profiler_tree-model-03.js => browser/devtools/performance/test/unit/test_tree-model-03.js
    rename : browser/devtools/performance/test/browser_profiler_tree-model-04.js => browser/devtools/performance/test/unit/test_tree-model-04.js
    rename : browser/devtools/performance/test/browser_profiler_tree-model-05.js => browser/devtools/performance/test/unit/test_tree-model-05.js
    rename : browser/devtools/performance/test/browser_profiler_tree-model-06.js => browser/devtools/performance/test/unit/test_tree-model-06.js
    rename : browser/devtools/performance/test/browser_profiler_tree-model-07.js => browser/devtools/performance/test/unit/test_tree-model-07.js

--- browser/devtools/performance/test/unit/test_tree-model-02.js
+++ browser/devtools/performance/test/unit/test_tree-model-02.js
@@ -5,7 +5,11 @@
  * Tests if a call tree model ignores samples with no timing information.
  */
 
-function test() {
+function run_test() {
+  run_next_test();
+}
+
+add_task(function test() {
   let { ThreadNode } = devtools.require("devtools/performance/tree-model");
 
   // Create a root node from a given samples array.
@@ -14,31 +18,29 @@ function test() {
 
   // Test the root node.
 
-  is(root.duration, 5,
+  equal(root.duration, 5,
     "The correct duration was calculated for the root node.");
 
-  is(root.calls.length, 1,
+  equal(root.calls.length, 1,
     "The correct number of child calls were calculated for the root node.");
   ok(getFrameNodePath(root, "A"),
     "The root node's only child call is correct.");
 
   // Test all the descendant nodes.
 
-  is(getFrameNodePath(root, "A").calls.length, 1,
+  equal(getFrameNodePath(root, "A").calls.length, 1,
     "The correct number of child calls were calculated for the 'A' node.");
   ok(getFrameNodePath(root, "A > B"),
     "The 'A' node's only child call is correct.");
 
-  is(getFrameNodePath(root, "A > B").calls.length, 1,
+  equal(getFrameNodePath(root, "A > B").calls.length, 1,
     "The correct number of child calls were calculated for the 'A > B' node.");
   ok(getFrameNodePath(root, "A > B > C"),
     "The 'A > B' node's only child call is correct.");
 
-  is(getFrameNodePath(root, "A > B > C").calls.length, 0,
+  equal(getFrameNodePath(root, "A > B > C").calls.length, 0,
     "The correct number of child calls were calculated for the 'A > B > C' node.");
-
-  finish();
-}
+});
 
 let gThread = synthesizeProfileForTest([{
   time: 5,
