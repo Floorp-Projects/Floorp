410ca4e0c3044f4b0ceb43c0d352636e34fb4e76
Jordan Santell <jsantell@mozilla.com>
2015-05-29 17:34:32 -0700

    Bug 1163356 - Migrate performance tool components to xpcshell tests where appropriate. r=vp

---
+++ browser/devtools/performance/test/unit/head.js
@@ -0,0 +1,42 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+
+let { devtools } = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
+const RecordingUtils = devtools.require("devtools/performance/recording-utils");
+
+/**
+ * Get a path in a FrameNode call tree.
+ */
+function getFrameNodePath(root, path) {
+  let calls = root.calls;
+  let node;
+  for (let key of path.split(" > ")) {
+    node = calls.find((node) => node.key == key);
+    if (!node) {
+      break;
+    }
+    calls = node.calls;
+  }
+  return node;
+}
+
+/**
+ * Synthesize a profile for testing.
+ */
+function synthesizeProfileForTest(samples) {
+  samples.unshift({
+    time: 0,
+    frames: [
+      { location: "(root)" }
+    ]
+  });
+
+  let uniqueStacks = new RecordingUtils.UniqueStacks();
+  return RecordingUtils.deflateThread({
+    samples: samples,
+    markers: []
+  }, uniqueStacks);
+}
