a5d85d165196c4a0f84d519ca6011ab268687d07
Chris Peterson <cpeterson@mozilla.com>
2015-05-31 19:39:16 -0700

    Bug 1194955 - Fix -Wunreachable-code warnings in dom/ipc and dom/plugins. r=jimm

--- dom/plugins/base/nsNPAPIPlugin.cpp
+++ dom/plugins/base/nsNPAPIPlugin.cpp
@@ -2670,7 +2670,6 @@ _getvalueforurl(NPP instance, NPNURLVariable variable, const char *url,
       return NPERR_NO_ERROR;
     }
 
-    break;
   default:
     // Fall through and return an error...
     ;
