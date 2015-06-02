363a0d1e266a7922117f38adacaa1c95978ce5da
Simon Montagu <smontagu@smontagu.org>
2015-06-03 00:18:38 +0300

    Bug 1170932: Improve error handling for the gbk encoder, r=emk

--- intl/uconv/ucvcn/nsUnicodeToGBK.cpp
+++ intl/uconv/ucvcn/nsUnicodeToGBK.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -79,30 +79,35 @@ void nsUnicodeToGB18030::Create4BytesEncoder()
   m4BytesEncoder = new nsUnicodeTo4BytesGB18030();
 }
 
-bool nsUnicodeToGB18030::EncodeSurrogate(
-  char16_t aSurrogateHigh,
-  char16_t aSurrogateLow,
-  char* aOut)
+nsresult nsUnicodeToGB18030::EncodeSurrogate(char16_t aSurrogateHigh,
+                                             char16_t aSurrogateLow,
+                                             char* aOut,
+                                             int32_t aDestLength,
+                                             int32_t aBufferLength)
 {
-  if( NS_IS_HIGH_SURROGATE(aSurrogateHigh) && 
+  if( NS_IS_HIGH_SURROGATE(aSurrogateHigh) &&
       NS_IS_LOW_SURROGATE(aSurrogateLow) )
   {
-    // notice that idx does not include the 0x10000 
+    // notice that idx does not include the 0x10000
     uint32_t idx = ((aSurrogateHigh - (char16_t)0xD800) << 10 ) |
                    (aSurrogateLow - (char16_t) 0xDC00);
 
+    if (aDestLength + 4 > aBufferLength) {
+      return NS_OK_UENC_MOREOUTPUT;
+    }
+
     unsigned char *out = (unsigned char*) aOut;
-    // notice this is from 0x90 for supplment planes
-    out[0] = (idx / (10*126*10)) + 0x90; 
+    // notice this is from 0x90 for supplementary planes
+    out[0] = (idx / (10*126*10)) + 0x90;
     idx %= (10*126*10);
     out[1] = (idx / (10*126)) + 0x30;
     idx %= (10*126);
     out[2] = (idx / (10)) + 0x81;
     out[3] = (idx % 10) + 0x30;
-    return true;
-  } 
-  return false; 
-} 
+    return NS_OK;
+  }
+  return NS_ERROR_UENC_NOMAPPING;
+}
 
 //----------------------------------------------------------------------
 // Class nsUnicodeToGBK [implementation]
@@ -122,70 +127,66 @@ void nsUnicodeToGBK::Create4BytesEncoder()
 {
   m4BytesEncoder = nullptr;
 }
-bool nsUnicodeToGBK::TryExtensionEncoder(
-  char16_t aChar,
-  char* aOut,
-  int32_t *aOutLen
-)
+
+nsresult nsUnicodeToGBK::TryExtensionEncoder(char16_t aChar,
+                                             char* aOut,
+                                             int32_t *aOutLen)
 {
-  if( NS_IS_HIGH_SURROGATE(aChar) || 
+  if( NS_IS_HIGH_SURROGATE(aChar) ||
       NS_IS_LOW_SURROGATE(aChar) )
   {
     // performance tune for surrogate characters
-    return false;
+    return NS_ERROR_UENC_NOMAPPING;
   }
   if(! mExtensionEncoder )
     CreateExtensionEncoder();
-  if(mExtensionEncoder) 
+  if(mExtensionEncoder)
   {
     int32_t len = 1;
-    nsresult res = NS_OK;
-    res = mExtensionEncoder->Convert(&aChar, &len, aOut, aOutLen);
-    if(NS_SUCCEEDED(res) && (*aOutLen > 0))
-      return true;
+    return mExtensionEncoder->Convert(&aChar, &len, aOut, aOutLen);
   }
-  return false;
+  return NS_ERROR_UENC_NOMAPPING;
 }
 
-bool nsUnicodeToGBK::Try4BytesEncoder(
+nsresult nsUnicodeToGBK::Try4BytesEncoder(
   char16_t aChar,
   char* aOut,
   int32_t *aOutLen
 )
 {
-  if( NS_IS_HIGH_SURROGATE(aChar) || 
+  if( NS_IS_HIGH_SURROGATE(aChar) ||
       NS_IS_LOW_SURROGATE(aChar) )
   {
     // performance tune for surrogate characters
-    return false;
+    return NS_ERROR_UENC_NOMAPPING;
   }
   if(! m4BytesEncoder )
     Create4BytesEncoder();
-  if(m4BytesEncoder) 
+  if(m4BytesEncoder)
   {
     int32_t len = 1;
     nsresult res = NS_OK;
     res = m4BytesEncoder->Convert(&aChar, &len, aOut, aOutLen);
     NS_ASSERTION(NS_FAILED(res) || ((1 == len) && (4 == *aOutLen)),
       "unexpect conversion length");
-    if(NS_SUCCEEDED(res) && (*aOutLen > 0))
-      return true;
+    return res;
   }
-  return false;
+  return NS_ERROR_UENC_NOMAPPING;
 }
-bool nsUnicodeToGBK::EncodeSurrogate(
-  char16_t aSurrogateHigh,
-  char16_t aSurrogateLow,
-  char* aOut)
-{
-  return false; // GBK cannot encode Surrogate, let the subclass encode it.
-} 
 
-NS_IMETHODIMP nsUnicodeToGBK::ConvertNoBuff(
-  const char16_t * aSrc, 
-  int32_t * aSrcLength, 
-  char * aDest, 
-  int32_t * aDestLength)
+nsresult nsUnicodeToGBK::EncodeSurrogate(char16_t aSurrogateHigh,
+                                         char16_t aSurrogateLow,
+                                         char* aOut,
+                                         int32_t aDestLength,
+                                         int32_t aBufferLength)
+{
+  return NS_ERROR_UENC_NOMAPPING; // GBK cannot encode Surrogate, let the subclass encode it.
+}
+
+NS_IMETHODIMP nsUnicodeToGBK::ConvertNoBuffNoErr(const char16_t * aSrc,
+                                                 int32_t * aSrcLength,
+                                                 char * aDest,
+                                                 int32_t * aDestLength)
 {
   int32_t iSrcLength = 0;
   int32_t iDestLength = 0;
@@ -217,59 +218,56 @@ NS_IMETHODIMP nsUnicodeToGBK::ConvertNoBuff(
         iDestLength +=2;
       } else {
         int32_t aOutLen = 2;
-        // make sure we still have 2 bytes for output first
-        if(iDestLength+2 > *aDestLength)
-        {
-          res = NS_OK_UENC_MOREOUTPUT;
-          break;
-        }
         // we cannot map in the common mapping. Let's try to
         // call the delegated 2 byte converter for the gbk or gb18030
         // unique 2 byte mapping
-        if(TryExtensionEncoder(unicode, aDest, &aOutLen))
-        {
+        res = TryExtensionEncoder(unicode, aDest, &aOutLen);
+        if (res == NS_OK) {
           iDestLength += aOutLen;
           aDest += aOutLen;
+        } else if (res == NS_OK_UENC_MOREOUTPUT) {
+          break;
         } else {
-          // make sure we still have 4 bytes for output first
-          if(iDestLength+4 > *aDestLength)
-          {
-            res = NS_OK_UENC_MOREOUTPUT;
-            break;
-          }
           // we still cannot map. Let's try to
-          // call the delegated GB18030 4 byte converter 
+          // call the delegated GB18030 4 byte converter
           aOutLen = 4;
           if( NS_IS_HIGH_SURROGATE(unicode) )
           {
             if((iSrcLength+1) < *aSrcLength ) {
-              if(EncodeSurrogate(aSrc[0],aSrc[1], aDest)) {
+              res = EncodeSurrogate(aSrc[0],aSrc[1], aDest,
+                                    iDestLength, *aDestLength);
+              if (res == NS_OK) {
                 // since we got a surrogate pair, we need to increment src.
-                iSrcLength++ ; 
+                iSrcLength++ ;
                 aSrc++;
                 iDestLength += aOutLen;
                 aDest += aOutLen;
               } else {
-                // only get a high surrogate, but not a low surrogate
-                res = NS_ERROR_UENC_NOMAPPING;
-                iSrcLength++;   // include length of the unmapped character
+                if (res == NS_ERROR_UENC_NOMAPPING) {
+                  // only get a high surrogate, but not a low surrogate
+                  iSrcLength++;   // include length of the unmapped character
+                }
                 break;
               }
             } else {
               mSurrogateHigh = aSrc[0];
+              res = NS_OK;
               break; // this will go to afterwhileloop
             }
           } else {
             if( NS_IS_LOW_SURROGATE(unicode) )
             {
               if(NS_IS_HIGH_SURROGATE(mSurrogateHigh)) {
-                if(EncodeSurrogate(mSurrogateHigh, aSrc[0], aDest)) {
+                res = EncodeSurrogate(mSurrogateHigh, aSrc[0], aDest,
+                                      iDestLength, *aDestLength);
+                if (res == NS_OK) {
                   iDestLength += aOutLen;
                   aDest += aOutLen;
                 } else {
-                  // only get a high surrogate, but not a low surrogate
-                  res = NS_ERROR_UENC_NOMAPPING;
-                  iSrcLength++;   // include length of the unmapped character
+                  if (res == NS_ERROR_UENC_NOMAPPING) {
+                    // only get a high surrogate, but not a low surrogate
+                    iSrcLength++;   // include length of the unmapped character
+                  }
                   break;
                 }
               } else {
@@ -279,22 +277,23 @@ NS_IMETHODIMP nsUnicodeToGBK::ConvertNoBuff(
                 break;
               }
             } else {
-              if(Try4BytesEncoder(unicode, aDest, &aOutLen))
-              {
+              res = Try4BytesEncoder(unicode, aDest, &aOutLen);
+              if (res == NS_OK) {
                 NS_ASSERTION((aOutLen == 4), "we should always generate 4 bytes here");
                 iDestLength += aOutLen;
                 aDest += aOutLen;
               } else {
-                res = NS_ERROR_UENC_NOMAPPING;
-                iSrcLength++;   // include length of the unmapped character
+                if (res == NS_ERROR_UENC_NOMAPPING) {
+                  iSrcLength++;   // include length of the unmapped character
+                }
                 break;
               }
             }
           }
         }
-      } 
+      }
     }
-    iSrcLength++ ; // Each unicode char just count as one in char16_t string;  	  
+    iSrcLength++ ; // Each unicode char just count as one in char16_t string;
     mSurrogateHigh = 0;
     aSrc++;
     if ( iDestLength >= (*aDestLength) && (iSrcLength < *aSrcLength) )
