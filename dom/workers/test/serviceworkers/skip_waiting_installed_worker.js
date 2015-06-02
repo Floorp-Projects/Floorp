32e71ba20abf7f994d4b7895a7e926f73b626e38
Jose Antonio Olivera Ortega <jaoo@jaoo.es>
2015-06-02 07:12:00 -0400

    Bug 1131352 - Part 2: Add ServiceWorkerGlobalScope skipWaiting(). r=nsm, r=baku

---
+++ dom/workers/test/serviceworkers/skip_waiting_installed_worker.js
@@ -0,0 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(self.skipWaiting());
+});
