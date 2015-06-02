a971efc32ccc4335a64de554ed093d36194549cd
Tooru Fujisawa <arai_a@mac.com>
2015-06-03 06:20:19 +0900

    Bug 1169199 - Check platform argument on linux SpiderMonkey shell build and specify target and host on 32bit build. r=sfink, a=test-only
    --HG--
    extra : transplant_source : %8A%F5w%B1%00n%85%F9I/u%C1%DE%D9%D9%83%95%7F%C4U

--- js/src/devtools/automation/autospider.sh
+++ js/src/devtools/automation/autospider.sh
@@ -88,8 +88,26 @@ elif [ "$OSTYPE" = "linux-gnu" ]; then
   MAKEFLAGS=-j4
   if [ "$VARIANT" = "arm-sim" ]; then
     USE_64BIT=false
-  elif [ "$UNAME_M" = "x86_64" ]; then
-    USE_64BIT=true
+  else
+    case "$platform" in
+    linux64)
+      USE_64BIT=true
+      ;;
+    linux64-debug)
+      USE_64BIT=true
+      ;;
+    linux)
+      USE_64BIT=false
+      ;;
+    linux-debug)
+      USE_64BIT=false
+      ;;
+    *)
+      if [ "$UNAME_M" = "x86_64" ]; then
+        USE_64BIT=true
+      fi
+      ;;
+    esac
   fi
 
   if [ "$UNAME_M" != "arm" ] && [ -n "$AUTOMATION" ]; then
@@ -127,6 +145,11 @@ else
     export CC="${CC:-/usr/bin/gcc} -m32"
     export CXX="${CXX:-/usr/bin/g++} -m32"
     export AR=ar
+  fi
+  if [ "$OSTYPE" = "linux-gnu" ]; then
+    if [ "$UNAME_M" != "arm" ] && [ -n "$AUTOMATION" ]; then
+      CONFIGURE_ARGS="$CONFIGURE_ARGS --target=i686-pc-linux --host=i686-pc-linux"
+    fi
   fi
 fi
 
