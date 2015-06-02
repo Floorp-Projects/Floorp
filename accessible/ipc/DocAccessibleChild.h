d19fd42378a1969aef29e9fecb6da4ac2f739940
Trevor Saunders <tbsaunde@tbsaunde.org>
2015-06-02 10:30:51 -0400

    bug 1170595 - switch to sending __delete__ from the parent instead of the child r=davidb, smaug
    After the child sends the __delete__ message to the parent there is a period of
    time in which the actor is registered, but the parent hasn't yet processed the
    __delete__ message.  During that time the parent can still try and send
    messages to the child, but that will crash the child process.  Fix this race by
    making the child send a shutdown message to the parent, and have the parent
    send __delete__ when it handles that message.

--- accessible/ipc/DocAccessibleChild.h
+++ accessible/ipc/DocAccessibleChild.h
@@ -32,8 +32,19 @@ public:
   { MOZ_COUNT_CTOR(DocAccessibleChild); }
   ~DocAccessibleChild()
   {
-    mDoc->SetIPCDoc(nullptr);
+    // Shutdown() should have been called, but maybe it isn't if the process is
+    // killed?
+    MOZ_ASSERT(!mDoc);
+    if (mDoc)
+      mDoc->SetIPCDoc(nullptr);
     MOZ_COUNT_DTOR(DocAccessibleChild);
+  }
+
+  void Shutdown()
+  {
+    mDoc->SetIPCDoc(nullptr);
+    mDoc = nullptr;
+    SendShutdown();
   }
 
   void ShowEvent(AccShowEvent* aShowEvent);
