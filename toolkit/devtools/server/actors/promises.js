99f34b5ad9669c210ebecd1b6757a8fe7f332aeb
Gabriel Luong <gabriel.luong@gmail.com>
2015-06-02 14:52:53 -0700

    Bug 1084525 - Part 3: Add attach and detach methods to PromisesActor r=fitzgen

--- toolkit/devtools/server/actors/promises.js
+++ toolkit/devtools/server/actors/promises.js
@@ -6,6 +6,7 @@
 
 const protocol = require("devtools/server/protocol");
 const { method, RetVal, Arg, types } = protocol;
+const { expectState } = require("devtools/server/actors/common");
 
 /**
  * The Promises Actor provides support for getting the list of live promises and
@@ -20,11 +21,50 @@ let PromisesActor = protocol.ActorClass({
    */
   initialize: function(conn, parent) {
     protocol.Actor.prototype.initialize.call(this, conn);
+
+    this.parent = parent;
+    this.state = "detached";
+    this._dbg = null;
   },
 
   destroy: function() {
     protocol.Actor.prototype.destroy.call(this, conn);
+
+    if (this.state === "attached") {
+      this.detach();
+    }
   },
+
+  get dbg() {
+    if (!this._dbg) {
+      this._dbg = this.parent.makeDebugger();
+    }
+    return this._dbg;
+  },
+
+  /**
+   * Attach to the PromisesActor.
+   */
+  attach: method(expectState("detached", function() {
+    this.dbg.addDebuggees();
+    this.state = "attached";
+  }, `attaching to the PromisesActor`), {
+    request: {},
+    response: {}
+  }),
+
+  /**
+   * Detach from the PromisesActor upon Debugger closing.
+   */
+  detach: method(expectState("attached", function() {
+    this.dbg.removeAllDebuggees();
+    this.dbg.enabled = false;
+    this._dbg = null;
+    this.state = "detached";
+  }, `detaching from the PromisesActor`), {
+    request: {},
+    response: {}
+  }),
 });
 
 exports.PromisesActor = PromisesActor;
