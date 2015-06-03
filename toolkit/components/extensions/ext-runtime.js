14e7d8ca0ab6a3b1be00557f86cb0cd5687819f4
Bill McCloskey <billm@mozilla.com>
2015-06-03 15:34:44 -0700

    Bug 1175770 - New extension API (r=Mossop)

---
+++ toolkit/components/extensions/ext-runtime.js
@@ -0,0 +1,47 @@
+Cu.import("resource://gre/modules/ExtensionUtils.jsm");
+let {
+  EventManager,
+  ignoreEvent,
+} = ExtensionUtils;
+
+extensions.registerAPI((extension, context) => {
+  return {
+    runtime: {
+      onStartup: new EventManager(context, "runtime.onStartup", fire => {
+        extension.onStartup = fire;
+        return () => {
+          extension.onStartup = null;
+        };
+      }).api(),
+
+      onInstalled: ignoreEvent(),
+
+      onMessage: context.messenger.onMessage("runtime.onMessage"),
+
+      onConnect: context.messenger.onConnect("runtime.onConnect"),
+
+      sendMessage: function(...args) {
+        let extensionId, message, options, responseCallback;
+        if (args.length == 1) {
+          message = args[0];
+        } else if (args.length == 2) {
+          [message, responseCallback] = args;
+        } else {
+          [extensionId, message, options, responseCallback] = args;
+        }
+        let recipient = {extensionId: extensionId ? extensionId : extension.id};
+        return context.messenger.sendMessage(Services.cpmm, message, recipient, responseCallback);
+      },
+
+      getManifest() {
+        return Cu.cloneInto(extension.manifest, context.cloneScope);
+      },
+
+      id: extension.id,
+
+      getURL: function(url) {
+        return extension.baseURI.resolve(url);
+      },
+    },
+  };
+});
---
+++ toolkit/components/extensions/ext-webNavigation.js
@@ -0,0 +1,70 @@
+XPCOMUtils.defineLazyModuleGetter(this, "ExtensionManagement",
+                                  "resource://gre/modules/ExtensionManagement.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "MatchPattern",
+                                  "resource://gre/modules/MatchPattern.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "WebNavigation",
+                                  "resource://gre/modules/WebNavigation.jsm");
+
+Cu.import("resource://gre/modules/ExtensionUtils.jsm");
+let {
+  SingletonEventManager,
+  ignoreEvent,
+  runSafe,
+} = ExtensionUtils;
+
+// Similar to WebRequestEventManager but for WebNavigation.
+function WebNavigationEventManager(context, eventName)
+{
+  let name = `webNavigation.${eventName}`;
+  let register = callback => {
+    let listener = data => {
+      if (!data.browser) {
+        return;
+      }
+
+      let tabId = TabManager.getBrowserId(data.browser);
+      if (tabId == -1) {
+        return;
+      }
+
+      let data2 = {
+        url: data.url,
+        timeStamp: Date.now(),
+        frameId: ExtensionManagement.getFrameId(data.windowId),
+        parentFrameId: ExtensionManagement.getParentFrameId(data.parentWindowId, data.windowId),
+      };
+
+      // Fills in tabId typically.
+      let result = {};
+      extensions.emit("fill-browser-data", data.browser, data2, result);
+      if (result.cancel) {
+        return;
+      }
+
+      return runSafe(context, callback, data2);
+    };
+
+    WebNavigation[eventName].addListener(listener);
+    return () => {
+      WebNavigation[eventName].removeListener(listener);
+    };
+  };
+
+  return SingletonEventManager.call(this, context, name, register);
+}
+
+WebNavigationEventManager.prototype = Object.create(SingletonEventManager.prototype);
+
+extensions.registerPrivilegedAPI("webNavigation", (extension, context) => {
+  return {
+    webNavigation: {
+      onBeforeNavigate: new WebNavigationEventManager(context, "onBeforeNavigate").api(),
+      onCommitted: new WebNavigationEventManager(context, "onCommitted").api(),
+      onDOMContentLoaded: new WebNavigationEventManager(context, "onDOMContentLoaded").api(),
+      onCompleted: new WebNavigationEventManager(context, "onCompleted").api(),
+      onErrorOccurred: new WebNavigationEventManager(context, "onErrorOccurred").api(),
+      onReferenceFragmentUpdated: new WebNavigationEventManager(context, "onReferenceFragmentUpdated").api(),
+      onCreatedNavigationTarget: ignoreEvent(),
+    },
+  };
+});
