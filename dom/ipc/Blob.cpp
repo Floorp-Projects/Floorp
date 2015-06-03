254c675d482bad3e7a225f080b978e1d14438a28
Jonathan Watt <jwatt@jwatt.org>
2015-06-03 01:11:16 +0100

    Bug 1173314 - Make GetMozFullPath and GetMozFullPathInternal const. r=sicking

--- dom/ipc/Blob.cpp
+++ dom/ipc/Blob.cpp
@@ -1918,7 +1918,7 @@ public:
   NS_DECL_ISUPPORTS_INHERITED
 
   virtual void
-  GetMozFullPathInternal(nsAString& aFileName, ErrorResult& aRv) override;
+  GetMozFullPathInternal(nsAString& aFileName, ErrorResult& aRv) const override;
 
   virtual already_AddRefed<BlobImpl>
   CreateSlice(uint64_t aStart,
@@ -2078,10 +2078,10 @@ public:
   SetLastModified(int64_t aLastModified) override;
 
   virtual void
-  GetMozFullPath(nsAString& aName, ErrorResult& aRv) override;
+  GetMozFullPath(nsAString& aName, ErrorResult& aRv) const override;
 
   virtual void
-  GetMozFullPathInternal(nsAString& aFileName, ErrorResult& aRv) override;
+  GetMozFullPathInternal(nsAString& aFileName, ErrorResult& aRv) const override;
 
   virtual uint64_t
   GetSize(ErrorResult& aRv) override;
@@ -2319,7 +2319,7 @@ NS_IMPL_QUERY_INTERFACE_INHERITED(BlobChild::RemoteBlobImpl,
 void
 BlobChild::
 RemoteBlobImpl::GetMozFullPathInternal(nsAString& aFilePath,
-                                       ErrorResult& aRv)
+                                       ErrorResult& aRv) const
 {
   if (!EventTargetIsOnCurrentThread(mActorTarget)) {
     MOZ_CRASH("Not implemented!");
@@ -2774,14 +2774,14 @@ RemoteBlobImpl::SetLastModified(int64_t aLastModified)
 
 void
 BlobParent::
-RemoteBlobImpl::GetMozFullPath(nsAString& aName, ErrorResult& aRv)
+RemoteBlobImpl::GetMozFullPath(nsAString& aName, ErrorResult& aRv) const
 {
   mBlobImpl->GetMozFullPath(aName, aRv);
 }
 
 void
 BlobParent::
-RemoteBlobImpl::GetMozFullPathInternal(nsAString& aFileName, ErrorResult& aRv)
+RemoteBlobImpl::GetMozFullPathInternal(nsAString& aFileName, ErrorResult& aRv) const
 {
   mBlobImpl->GetMozFullPathInternal(aFileName, aRv);
 }
