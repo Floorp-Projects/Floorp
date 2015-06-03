0693ade2299d41058af32e0f453ffcedc150be2b
Terrence Cole <terrence@mozilla.com>
2015-06-03 09:00:50 -0700

    Bug 1173864 - Make MarkStack private in GCMarker; r=jonco

--- js/src/gc/Marking.h
+++ js/src/gc/Marking.h
@@ -195,9 +195,6 @@ class GCMarker : public JSTracer
     bool shouldCheckCompartments() { return strictCompartmentChecking; }
 #endif
 
-    /* This is public exclusively for ScanRope. */
-    MarkStack stack;
-
   private:
 #ifdef DEBUG
     void checkZone(void* p);
@@ -257,7 +254,7 @@ class GCMarker : public JSTracer
             delayMarkingChildren(ptr);
     }
 
-    void pushValueArray(JSObject* obj, void* start, void* end) {
+    void pushValueArray(JSObject* obj, HeapSlot* start, HeapSlot* end) {
         checkZone(obj);
 
         MOZ_ASSERT(start <= end);
@@ -280,6 +277,9 @@ class GCMarker : public JSTracer
     bool restoreValueArray(JSObject* obj, void** vpp, void** endp);
     void saveValueRanges();
     inline void processMarkStackTop(SliceBudget& budget);
+
+    /* The mark stack. Pointers in this stack are "gray" in the GC sense. */
+    MarkStack stack;
 
     /* The color is only applied to objects and functions. */
     uint32_t color;
