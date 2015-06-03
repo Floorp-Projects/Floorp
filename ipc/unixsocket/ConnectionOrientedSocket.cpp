bc636a00f4b0b3b93f1313782bc4ac07d1b5cadf
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-06-03 11:53:50 +0200

    Bug 1170466: Inherit |ConnectionOrientedSocketIO| from |UnixSocketWatcher|, r=kmachulis
    Inheriting |ConncetionOrientedSocketIO| from |UnixSocketWatcher| will
    allow for sharing common I/O code of the sub-class.

--- ipc/unixsocket/ConnectionOrientedSocket.cpp
+++ ipc/unixsocket/ConnectionOrientedSocket.cpp
@@ -14,8 +14,19 @@ namespace ipc {
 //
 
 ConnectionOrientedSocketIO::ConnectionOrientedSocketIO(
-  nsIThread* aConsumerThread)
+  nsIThread* aConsumerThread,
+  MessageLoop* aIOLoop,
+  int aFd,
+  ConnectionStatus aConnectionStatus)
   : DataSocketIO(aConsumerThread)
+  , UnixSocketWatcher(aIOLoop, aFd, aConnectionStatus)
+{ }
+
+ConnectionOrientedSocketIO::ConnectionOrientedSocketIO(
+  nsIThread* aConsumerThread,
+  MessageLoop* aIOLoop)
+  : DataSocketIO(aConsumerThread)
+  , UnixSocketWatcher(aIOLoop)
 { }
 
 ConnectionOrientedSocketIO::~ConnectionOrientedSocketIO()
