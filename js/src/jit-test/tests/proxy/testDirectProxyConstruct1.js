5d84eb21cdbde8a800b6f867b55940915cf63c28
Eric Faust <efaustbmo@mozilla.com>
2015-06-03 02:01:14 -0700

    Bug 1141865 - Part 2: Plumb new.target on the stack and make it accessible to JSNatives. (r=jorendorff, r=jandem, r=shu)

--- js/src/jit-test/tests/proxy/testDirectProxyConstruct1.js
+++ js/src/jit-test/tests/proxy/testDirectProxyConstruct1.js
@@ -1,9 +1,11 @@
 // Forward to the target if the trap is undefined
+var p;
 var target = function (x, y) {
+    assertEq(new.target, p);
     this.foo = x + y;
 }
 
-for (let p of [new Proxy(target, {}), Proxy.revocable(target, {}).proxy]) {
+for (p of [new Proxy(target, {}), Proxy.revocable(target, {}).proxy]) {
     var obj = new p(2, 3);
     assertEq(obj.foo, 5);
     assertEq(Object.getPrototypeOf(obj), target.prototype);
