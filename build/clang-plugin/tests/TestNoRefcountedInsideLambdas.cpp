63b921afe7acce9a80407be27f3ed13f9ac17aca
Botond Ballo <botond@mozilla.com>
2015-06-03 16:51:36 -0400

    Bug 1170388 - Restrict the static analysis error given about raw pointers to refcounted objects inside a lambda, to the case where the raw pointer is captured. r=ehsan
    --HG--
    extra : rebase_source : 28cd2aa684abf505edbcdbc449c8cf056d3b0ee6
    extra : source : 28c2862d29b8a2cb0d03cf38430f17abc269225b

--- build/clang-plugin/tests/TestNoRefcountedInsideLambdas.cpp
+++ build/clang-plugin/tests/TestNoRefcountedInsideLambdas.cpp
@@ -19,40 +19,76 @@ void take(...);
 void foo() {
   R* ptr;
   SmartPtr<R> sp;
-  take([&]() {
-    ptr->method(); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be used inside a lambda}} expected-note{{Please consider using a smart pointer}}
+  take([&](R* argptr) {
+    R* localptr;
+    ptr->method(); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be captured by a lambda}} expected-note{{Please consider using a smart pointer}}
+    argptr->method();
+    localptr->method();
   });
-  take([&]() {
+  take([&](SmartPtr<R> argsp) {
+    SmartPtr<R> localsp;
     sp->method();
+    argsp->method();
+    localsp->method();
   });
-  take([&]() {
-    take(ptr); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be used inside a lambda}} expected-note{{Please consider using a smart pointer}}
+  take([&](R* argptr) {
+    R* localptr;
+    take(ptr); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be captured by a lambda}} expected-note{{Please consider using a smart pointer}}
+    take(argptr);
+    take(localptr);
   });
-  take([&]() {
+  take([&](SmartPtr<R> argsp) {
+    SmartPtr<R> localsp;
     take(sp);
+    take(argsp);
+    take(localsp);
   });
-  take([=]() {
-    ptr->method(); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be used inside a lambda}} expected-note{{Please consider using a smart pointer}}
+  take([=](R* argptr) {
+    R* localptr;
+    ptr->method(); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be captured by a lambda}} expected-note{{Please consider using a smart pointer}}
+    argptr->method();
+    localptr->method();
   });
-  take([=]() {
+  take([=](SmartPtr<R> argsp) {
+    SmartPtr<R> localsp;
     sp->method();
+    argsp->method();
+    localsp->method();
   });
-  take([=]() {
-    take(ptr); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be used inside a lambda}} expected-note{{Please consider using a smart pointer}}
+  take([=](R* argptr) {
+    R* localptr;
+    take(ptr); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be captured by a lambda}} expected-note{{Please consider using a smart pointer}}
+    take(argptr);
+    take(localptr);
   });
-  take([=]() {
+  take([=](SmartPtr<R> argsp) {
+    SmartPtr<R> localsp;
     take(sp);
+    take(argsp);
+    take(localsp);
   });
-  take([ptr]() {
-    ptr->method(); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be used inside a lambda}} expected-note{{Please consider using a smart pointer}}
+  take([ptr](R* argptr) {
+    R* localptr;
+    ptr->method(); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be captured by a lambda}} expected-note{{Please consider using a smart pointer}}
+    argptr->method();
+    localptr->method();
   });
-  take([sp]() {
+  take([sp](SmartPtr<R> argsp) {
+    SmartPtr<R> localsp;
     sp->method();
+    argsp->method();
+    localsp->method();
   });
-  take([ptr]() {
-    take(ptr); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be used inside a lambda}} expected-note{{Please consider using a smart pointer}}
+  take([ptr](R* argptr) {
+    R* localptr;
+    take(ptr); // expected-error{{Refcounted variable 'ptr' of type 'R' cannot be captured by a lambda}} expected-note{{Please consider using a smart pointer}}
+    take(argptr);
+    take(localptr);
   });
-  take([sp]() {
+  take([sp](SmartPtr<R> argsp) {
+    SmartPtr<R> localsp;
     take(sp);
+    take(argsp);
+    take(localsp);
   });
 }
