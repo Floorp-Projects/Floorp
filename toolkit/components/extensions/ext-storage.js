b1a00d7c72053d21693f52f4334007dc77ebeb06
Bill McCloskey <billm@mozilla.com>
2015-06-03 15:34:44 -0700

    Bug 1175770 - New extension API (r=Mossop)

---
+++ toolkit/components/extensions/ext-storage.js
@@ -0,0 +1,48 @@
+XPCOMUtils.defineLazyModuleGetter(this, "ExtensionStorage",
+                                  "resource://gre/modules/ExtensionStorage.jsm");
+
+Cu.import("resource://gre/modules/ExtensionUtils.jsm");
+let {
+  EventManager,
+  ignoreEvent,
+  runSafe,
+} = ExtensionUtils;
+
+extensions.registerPrivilegedAPI("storage", (extension, context) => {
+  return {
+    storage: {
+      local: {
+        get: function(keys, callback) {
+          ExtensionStorage.get(extension.id, keys).then(result => {
+            runSafe(context, callback, result);
+          });
+        },
+        set: function(items, callback) {
+          ExtensionStorage.set(extension.id, items).then(() => {
+            if (callback) {
+              runSafe(context, callback);
+            }
+          });
+        },
+        remove: function(items, callback) {
+          ExtensionStorage.remove(extension.id, items).then(() => {
+            if (callback) {
+              runSafe(context, callback);
+            }
+          });
+        },
+      },
+
+      onChanged: new EventManager(context, "storage.local.onChanged", fire => {
+        let listener = changes => {
+          fire(changes, "local");
+        };
+
+        ExtensionStorage.addOnChangedListener(extension.id, listener);
+        return () => {
+          ExtensionStorage.removeOnChangedListener(extension.id, listener);
+        };
+      }).api(),
+    },
+  };
+});
