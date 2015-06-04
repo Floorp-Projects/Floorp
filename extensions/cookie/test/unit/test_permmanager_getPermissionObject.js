b5ad02e8bc2aa6bca7032dc7d236192fc571c116
Michael Layzell <michael@thelayzells.com>
2015-06-04 11:00:25 -0400

    Bug 1173523 - Part 10: Update nsIPermission and nsIPermissionManager tests to use new API for nsIPermission, r=ehsan

--- extensions/cookie/test/unit/test_permmanager_getPermissionObject.js
+++ extensions/cookie/test/unit/test_permmanager_getPermissionObject.js
@@ -29,20 +29,20 @@ function run_test() {
   pm.addFromPrincipal(principal, "test/pobject", pm.ALLOW_ACTION);
   var rootPerm = pm.getPermissionObject(principal, "test/pobject", false);
   do_check_true(rootPerm != null);
-  do_check_eq(rootPerm.host, "example.com");
+  do_check_eq(rootPerm.principal.origin, "http://example.com");
   do_check_eq(rootPerm.type, "test/pobject");
   do_check_eq(rootPerm.capability, pm.ALLOW_ACTION);
   do_check_eq(rootPerm.expireType, pm.EXPIRE_NEVER);
 
   var rootPerm2 = pm.getPermissionObject(principal, "test/pobject", true);
   do_check_true(rootPerm != null);
-  do_check_eq(rootPerm.host, "example.com");
+  do_check_eq(rootPerm.principal.origin, "http://example.com");
 
   var subPerm = pm.getPermissionObject(subPrincipal, "test/pobject", true);
   do_check_null(subPerm);
   subPerm = pm.getPermissionObject(subPrincipal, "test/pobject", false);
   do_check_true(subPerm != null);
-  do_check_eq(subPerm.host, "example.com");
+  do_check_eq(subPerm.principal.origin, "http://example.com");
   do_check_eq(subPerm.type, "test/pobject");
   do_check_eq(subPerm.capability, pm.ALLOW_ACTION);
 
@@ -50,7 +50,7 @@ function run_test() {
   do_check_null(subPerm);
   subPerm = pm.getPermissionObject(subSubPrincipal, "test/pobject", false);
   do_check_true(subPerm != null);
-  do_check_eq(subPerm.host, "example.com");
+  do_check_eq(subPerm.principal.origin, "http://example.com");
 
   pm.addFromPrincipal(principal, "test/pobject", pm.DENY_ACTION, pm.EXPIRE_SESSION);
 
@@ -63,28 +63,28 @@ function run_test() {
   do_check_eq(rootPerm.expireType, pm.EXPIRE_SESSION);
 
   subPerm = pm.getPermissionObject(subPrincipal, "test/pobject", false);
-  do_check_eq(subPerm.host, "example.com");
+  do_check_eq(subPerm.principal.origin, "http://example.com");
   do_check_eq(subPerm.capability, pm.DENY_ACTION);
   do_check_eq(subPerm.expireType, pm.EXPIRE_SESSION);
 
   pm.addFromPrincipal(subPrincipal, "test/pobject", pm.PROMPT_ACTION);
   rootPerm = pm.getPermissionObject(principal, "test/pobject", true);
-  do_check_eq(rootPerm.host, "example.com");
+  do_check_eq(rootPerm.principal.origin, "http://example.com");
   do_check_eq(rootPerm.capability, pm.DENY_ACTION);
 
   subPerm = pm.getPermissionObject(subPrincipal, "test/pobject", true);
-  do_check_eq(subPerm.host, "sub.example.com");
+  do_check_eq(subPerm.principal.origin, "http://sub.example.com");
   do_check_eq(subPerm.capability, pm.PROMPT_ACTION);
 
   subPerm = pm.getPermissionObject(subPrincipal, "test/pobject", false);
-  do_check_eq(subPerm.host, "sub.example.com");
+  do_check_eq(subPerm.principal.origin, "http://sub.example.com");
   do_check_eq(subPerm.capability, pm.PROMPT_ACTION);
 
   subPerm = pm.getPermissionObject(subSubPrincipal, "test/pobject", true);
   do_check_null(subPerm);
 
   subPerm = pm.getPermissionObject(subSubPrincipal, "test/pobject", false);
-  do_check_eq(subPerm.host, "sub.example.com");
+  do_check_eq(subPerm.principal.origin, "http://sub.example.com");
   do_check_eq(subPerm.capability, pm.PROMPT_ACTION);
 
   pm.removeFromPrincipal(principal, "test/pobject");
