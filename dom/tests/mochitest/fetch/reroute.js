313557b8e336073bc281baa2f26d686c27e04400
Nikhil Marathe <nsm.nikhil@gmail.com>
2015-06-04 15:50:03 -0700

    Bug 1169044 - Patch 4 - Set referrer on intercepted request. r=jdm
    --HG--
    extra : rebase_source : eb69cd43d33c1f84d6fb4ea5080db0a6e240570a

--- dom/tests/mochitest/fetch/reroute.js
+++ dom/tests/mochitest/fetch/reroute.js
@@ -1,3 +1,19 @@
 onfetch = function(e) {
+  if (e.request.url.indexOf("Referer") >= 0) {
+    // Silently rewrite the referrer so the referrer test passes since the
+    // document/worker isn't aware of this service worker.
+    var url = e.request.url.substring(0, e.request.url.indexOf('?'));
+    url += '?headers=' + ({ 'Referer': self.location.href }).toSource();
+
+    e.respondWith(fetch(url, {
+      method: e.request.method,
+      headers: e.request.headers,
+      body: e.request.body,
+      mode: e.request.mode,
+      credentials: e.request.credentials,
+      cache: e.request.cache,
+    }));
+    return;
+  }
   e.respondWith(fetch(e.request));
 };
