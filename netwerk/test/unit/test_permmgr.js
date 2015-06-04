ec7f71eae310a964b7362101caed7e039e37301b
Michael Layzell <michael@thelayzells.com>
2015-06-04 11:00:25 -0400

    Bug 1173523 - Part 10: Update nsIPermission and nsIPermissionManager tests to use new API for nsIPermission, r=ehsan

--- netwerk/test/unit/test_permmgr.js
+++ netwerk/test/unit/test_permmgr.js
@@ -2,31 +2,31 @@
 
 var hosts = [
   // format: [host, type, permission]
-  ["mozilla.org", "cookie", 1],
-  ["mozilla.org", "image", 2],
-  ["mozilla.org", "popup", 3],
-  ["mozilla.com", "cookie", 1],
-  ["www.mozilla.com", "cookie", 2],
-  ["dev.mozilla.com", "cookie", 3]
+  ["http://mozilla.org", "cookie", 1],
+  ["http://mozilla.org", "image", 2],
+  ["http://mozilla.org", "popup", 3],
+  ["http://mozilla.com", "cookie", 1],
+  ["http://www.mozilla.com", "cookie", 2],
+  ["http://dev.mozilla.com", "cookie", 3]
 ];
 
 var results = [
   // format: [host, type, testPermission result, testExactPermission result]
   // test defaults
-  ["localhost", "cookie", 0, 0],
-  ["spreadfirefox.com", "cookie", 0, 0],
+  ["http://localhost", "cookie", 0, 0],
+  ["http://spreadfirefox.com", "cookie", 0, 0],
   // test different types
-  ["mozilla.org", "cookie", 1, 1],
-  ["mozilla.org", "image", 2, 2],
-  ["mozilla.org", "popup", 3, 3],
+  ["http://mozilla.org", "cookie", 1, 1],
+  ["http://mozilla.org", "image", 2, 2],
+  ["http://mozilla.org", "popup", 3, 3],
   // test subdomains
-  ["www.mozilla.org", "cookie", 1, 0],
-  ["www.dev.mozilla.org", "cookie", 1, 0],
+  ["http://www.mozilla.org", "cookie", 1, 0],
+  ["http://www.dev.mozilla.org", "cookie", 1, 0],
   // test different permissions on subdomains
-  ["mozilla.com", "cookie", 1, 1],
-  ["www.mozilla.com", "cookie", 2, 2],
-  ["dev.mozilla.com", "cookie", 3, 3],
-  ["www.dev.mozilla.com", "cookie", 3, 0]
+  ["http://mozilla.com", "cookie", 1, 1],
+  ["http://www.mozilla.com", "cookie", 2, 2],
+  ["http://dev.mozilla.com", "cookie", 3, 3],
+  ["http://www.dev.mozilla.com", "cookie", 3, 0]
 ];
 
 function run_test() {
@@ -45,16 +45,16 @@ function run_test() {
 
   // put a few hosts in
   for (var i = 0; i < hosts.length; ++i) {
-    var uri = ioService.newURI("http://" + hosts[i][0], null, null);
-    var principal = secMan.getNoAppCodebasePrincipal(uri);
+    let uri = ioService.newURI(hosts[i][0], null, null);
+    let principal = secMan.getNoAppCodebasePrincipal(uri);
 
     pm.addFromPrincipal(principal, hosts[i][1], hosts[i][2]);
   }
 
   // test the result
   for (var i = 0; i < results.length; ++i) {
-    var uri = ioService.newURI("http://" + results[i][0], null, null);
-    var principal = secMan.getNoAppCodebasePrincipal(uri);
+    let uri = ioService.newURI(results[i][0], null, null);
+    let principal = secMan.getNoAppCodebasePrincipal(uri);
 
     do_check_eq(pm.testPermissionFromPrincipal(principal, results[i][1]), results[i][2]);
     do_check_eq(pm.testExactPermissionFromPrincipal(principal, results[i][1]), results[i][3]);
@@ -72,16 +72,13 @@ function run_test() {
 
   // ... remove all the hosts ...
   for (var j = 0; j < perms.length; ++j) {
-    var uri = ioService.newURI("http://" + perms[j].host, null, null);
-    var principal = secMan.getNoAppCodebasePrincipal(uri);
-
-    pm.removeFromPrincipal(principal, perms[j].type);
+    pm.removePermission(perms[j]);
   }
   
   // ... ensure each and every element is equal ...
   for (var i = 0; i < hosts.length; ++i) {
     for (var j = 0; j < perms.length; ++j) {
-      if (hosts[i][0] == perms[j].host &&
+      if (perms[j].matchesURI(ioService.newURI(hosts[i][0], null, null), true) &&
           hosts[i][1] == perms[j].type &&
           hosts[i][2] == perms[j].capability) {
         perms.splice(j, 1);
@@ -102,7 +99,7 @@ function run_test() {
   var enumerator = pm.enumerator;
   do_check_eq(enumerator.hasMoreElements(), true);
   var ace = enumerator.getNext().QueryInterface(Components.interfaces.nsIPermission);
-  do_check_eq(ace.host, aceref);
+  do_check_eq(ace.principal.URI.asciiHost, aceref);
   do_check_eq(enumerator.hasMoreElements(), false);
 
   // test removeAll()
