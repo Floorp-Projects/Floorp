3dc52ee4ad4ec336495a5a4c46be9e214ef9af5c
Vladimir Vukicevic <vladimir@pobox.com>
2015-06-05 13:14:10 -0400

    Bug 1179944, [webvr] put back support for Oculus 0.5.0 runtime, for OSX and Linux; r=mstange

--- gfx/vr/gfxVRCardboard.cpp
+++ gfx/vr/gfxVRCardboard.cpp
@@ -98,8 +98,8 @@ HMDInfoCardboard::HMDInfoCardboard()
 
 #if 1
   int32_t xcoord = 0;
-  if (getenv("FAKE_CARDBOARD_SCREEN")) {
-      const char *env = getenv("FAKE_CARDBOARD_SCREEN");
+  if (PR_GetEnv("FAKE_CARDBOARD_SCREEN")) {
+      const char *env = PR_GetEnv("FAKE_CARDBOARD_SCREEN");
       nsresult err;
       xcoord = nsCString(env).ToInteger(&err);
       if (err != NS_OK) xcoord = 0;
@@ -274,6 +274,12 @@ HMDInfoCardboard::SetFOV(const VRFieldOfView& aFOVLeft,
   mEyeResolution.width = 1920 / 2;
   mEyeResolution.height = 1080;
 
+  if (PR_GetEnv("FAKE_CARDBOARD_SCREEN")) {
+    // for testing, make the eye resolution 2x of the screen
+    mEyeResolution.width *= 2;
+    mEyeResolution.height *= 2;
+  }
+
   mConfiguration.hmdType = mType;
   mConfiguration.value = 0;
   mConfiguration.fov[0] = aFOVLeft;
@@ -291,8 +297,13 @@ HMDInfoCardboard::FillDistortionConstants(uint32_t whichEye,
   // these modify the texture coordinates; texcoord * zw + xy
   values.eyeToSourceScaleAndOffset[0] = 0.0;
   values.eyeToSourceScaleAndOffset[1] = 0.0;
-  values.eyeToSourceScaleAndOffset[2] = 1.0;
-  values.eyeToSourceScaleAndOffset[3] = 1.0;
+  if (PR_GetEnv("FAKE_CARDBOARD_SCREEN")) {
+    values.eyeToSourceScaleAndOffset[2] = 2.0;
+    values.eyeToSourceScaleAndOffset[3] = 2.0;
+  } else {
+    values.eyeToSourceScaleAndOffset[2] = 1.0;
+    values.eyeToSourceScaleAndOffset[3] = 1.0;
+  }
 
   // Our mesh positions are in the [-1..1] clip space; we give appropriate offset
   // and scaling for the right viewport.  (In the 0..2 space for sanity)
