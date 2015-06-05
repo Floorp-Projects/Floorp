953d3573cb67414ad5b5e33a11755977078d5115
Fernando Jimenez <ferjmoreno@gmail.com>
2015-06-05 12:53:58 +0200

    Bug 1169613 - Use content type of synthesized response for JAR channel requests if available. Tests. r=jdm

--- dom/workers/test/serviceworkers/app-protocol/sw.js
+++ dom/workers/test/serviceworkers/app-protocol/sw.js
@@ -5,10 +5,16 @@ self.addEventListener('fetch', (event) => {
     }));
   }
 
-  if (event.request.url.indexOf('test_doc_load_interception.js') >=0 ) {
+  if (event.request.url.indexOf('test_doc_load_interception.js') >= 0 ) {
     var scriptContent = 'alert("OK: Script modified by service worker")';
     event.respondWith(new Response(scriptContent, {
       headers: {'Content-Type': 'application/javascript'}
+    }));
+  }
+
+  if (event.request.url.indexOf('test_custom_content_type') >= 0) {
+    event.respondWith(new Response('customContentType', {
+      headers: {'Content-Type': 'text/html'}
     }));
   }
 });
