f1e4c4ddb4bd3bc1b6e626ff55841a3a8d206e32
Ryan VanderMeulen <ryanvm@gmail.com>
2015-06-05 11:49:54 -0400

    Backed out changeset b117a76d0a25 (bug 1171638) for making Windows M(JP) basically permafail.

---
+++ addon-sdk/source/python-lib/cuddlefish/tests/addons/simplest-test/main.js
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { Cc, Ci } = require("chrome");
+
+exports.main = function(options, callbacks) {
+  // Close Firefox window. Firefox should quit.
+  require("sdk/deprecated/window-utils").activeBrowserWindow.close();
+
+  // But not on Mac where it stay alive! We have to request application quit.
+  if (require("sdk/system/runtime").OS == "Darwin") {
+    let appStartup = Cc['@mozilla.org/toolkit/app-startup;1'].
+                     getService(Ci.nsIAppStartup);
+    appStartup.quit(appStartup.eAttemptQuit);
+  }
+}
