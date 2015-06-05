d3a287c00e4ad28209ff85e2490c72acf64dd2bf
Ryan VanderMeulen <ryanvm@gmail.com>
2015-06-05 11:31:16 -0400

    Bug 1170143 - Fix Windows bustage to reopen a CLOSED TREE.

--- gfx/layers/d3d11/TextureD3D11.cpp
+++ gfx/layers/d3d11/TextureD3D11.cpp
@@ -565,7 +565,7 @@ DXGIYCbCrTextureClient::Create(ISurfaceAllocator* aAllocator,
                                const gfx::IntSize& aSizeCbCr)
 {
   if (!aHandleY || !aHandleCb || !aHandleCr ||
-      !aTextureY, || !aTextureCb || !aTextureCr) {
+      !aTextureY || !aTextureCb || !aTextureCr) {
     return nullptr;
   }
 
