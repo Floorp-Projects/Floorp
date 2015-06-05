9ad90d7f19e82e0aab709485be917e87e5450a7d
Fernando Jimenez <ferjmoreno@gmail.com>
2015-06-05 12:53:58 +0200

    Bug 1169613 - Use content type of synthesized response for JAR channel requests if available. Tests. r=jdm

--- dom/workers/test/serviceworkers/app-protocol/test.js
+++ dom/workers/test/serviceworkers/app-protocol/test.js
@@ -14,16 +14,26 @@ function done() {
   alert('DONE');
 }
 
-function testFetchAppResource(aExpectedResponse) {
-  return fetch('foo.txt').then(res => {
+function testFetchAppResource(aUrl,
+                              aExpectedResponse,
+                              aExpectedContentType) {
+  return fetch(aUrl).then(res => {
     ok(true, 'fetch should resolve');
     if (res.type == 'error') {
       ok(false, 'fetch failed');
     }
     ok(res.status == 200, 'status should be 200');
     ok(res.statusText == 'OK', 'statusText should be OK');
+    if (aExpectedContentType) {
+      var headers = res.headers.getAll('Content-Type');
+      ok(headers.length, "Headers length");
+      var contentType = res.headers.get('Content-Type');
+      ok(contentType == aExpectedContentType, ('content type ' +
+         contentType + ' should match with ' + aExpectedContentType));
+    }
     return res.text().then(body => {
-      ok(body == aExpectedResponse, 'body should match');
+      ok(body == aExpectedResponse, 'body ' + body +
+         ' should match with ' + aExpectedResponse);
     });
   });
 }
