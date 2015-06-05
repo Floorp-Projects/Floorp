5652a197fccec5654183214d7ef1d42b7505bc9e
Masayuki Nakano <masayuki@d-toybox.com>
2015-06-05 18:28:19 +0900

    Bug 1166436 part.4 mozilla::ContentCache should store text rects and caret rect and TabParent should use them r=m_kato

--- widget/ContentCache.cpp
+++ widget/ContentCache.cpp
@@ -13,6 +13,10 @@ namespace mozilla {
 
 using namespace widget;
 
+/*****************************************************************************
+ * mozilla::ContentCache
+ *****************************************************************************/
+
 ContentCache::ContentCache()
   : mCompositionStart(UINT32_MAX)
   , mCompositionEventsDuringRequest(0)
@@ -38,6 +42,68 @@ ContentCache::SetSelection(uint32_t aAnchorOffset, uint32_t aFocusOffset)
 {
   mSelection.mAnchor = aAnchorOffset;
   mSelection.mFocus = aFocusOffset;
+}
+
+bool
+ContentCache::InitTextRectArray(uint32_t aOffset,
+                                const RectArray& aTextRectArray)
+{
+  if (NS_WARN_IF(aOffset >= TextLength()) ||
+      NS_WARN_IF(aOffset + aTextRectArray.Length() > TextLength())) {
+    return false;
+  }
+  mTextRectArray.mStart = aOffset;
+  mTextRectArray.mRects = aTextRectArray;
+  return true;
+}
+
+bool
+ContentCache::GetTextRect(uint32_t aOffset,
+                          LayoutDeviceIntRect& aTextRect) const
+{
+  if (NS_WARN_IF(!mTextRectArray.InRange(aOffset))) {
+    aTextRect.SetEmpty();
+    return false;
+  }
+  aTextRect = mTextRectArray.GetRect(aOffset);
+  return true;
+}
+
+bool
+ContentCache::GetUnionTextRects(uint32_t aOffset,
+                                uint32_t aLength,
+                                LayoutDeviceIntRect& aUnionTextRect) const
+{
+  if (NS_WARN_IF(!mTextRectArray.InRange(aOffset, aLength))) {
+    aUnionTextRect.SetEmpty();
+    return false;
+  }
+  aUnionTextRect = mTextRectArray.GetUnionRect(aOffset, aLength);
+  return true;
+}
+
+bool
+ContentCache::InitCaretRect(uint32_t aOffset,
+                            const LayoutDeviceIntRect& aCaretRect)
+{
+  if (NS_WARN_IF(aOffset > TextLength())) {
+    return false;
+  }
+  mCaret.mOffset = aOffset;
+  mCaret.mRect = aCaretRect;
+  return true;
+}
+
+bool
+ContentCache::GetCaretRect(uint32_t aOffset,
+                           LayoutDeviceIntRect& aCaretRect) const
+{
+  if (mCaret.mOffset != aOffset) {
+    aCaretRect.SetEmpty();
+    return false;
+  }
+  aCaretRect = mCaret.mRect;
+  return true;
 }
 
 bool
@@ -99,6 +165,34 @@ ContentCache::RequestToCommitComposition(nsIWidget* aWidget,
   aLastString = mCommitStringByRequest;
   mCommitStringByRequest.Truncate(0);
   return mCompositionEventsDuringRequest;
+}
+
+/*****************************************************************************
+ * mozilla::ContentCache::TextRectArray
+ *****************************************************************************/
+
+LayoutDeviceIntRect
+ContentCache::TextRectArray::GetRect(uint32_t aOffset) const
+{
+  LayoutDeviceIntRect rect;
+  if (InRange(aOffset)) {
+    rect = mRects[aOffset - mStart];
+  }
+  return rect;
+}
+
+LayoutDeviceIntRect
+ContentCache::TextRectArray::GetUnionRect(uint32_t aOffset,
+                                          uint32_t aLength) const
+{
+  LayoutDeviceIntRect rect;
+  if (!InRange(aOffset, aLength)) {
+    return rect;
+  }
+  for (uint32_t i = 0; i < aLength; i++) {
+    rect = rect.Union(mRects[aOffset - mStart + i]);
+  }
+  return rect;
 }
 
 } // namespace mozilla
