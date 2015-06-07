8b3f3fad355083dc7b5743189b4210b9c4a7fd26
Kathy Brade <brade@pearlcrescent.com>
2015-06-07 09:02:00 -0400

    Bug 418986 - Resist fingerprinting by preventing exposure of screen and system info. r=mrbkap, r=heycam

--- dom/events/Event.cpp
+++ dom/events/Event.cpp
@@ -890,6 +890,13 @@ Event::GetScreenCoords(nsPresContext* aPresContext,
                        WidgetEvent* aEvent,
                        LayoutDeviceIntPoint aPoint)
 {
+  if (!nsContentUtils::IsCallerChrome() &&
+      nsContentUtils::ResistFingerprinting()) {
+    // When resisting fingerprinting, return client coordinates instead.
+    CSSIntPoint clientCoords = GetClientCoords(aPresContext, aEvent, aPoint, CSSIntPoint(0, 0));
+    return LayoutDeviceIntPoint(clientCoords.x, clientCoords.y);
+  }
+
   if (EventStateManager::sIsPointerLocked) {
     return EventStateManager::sLastScreenPoint;
   }
