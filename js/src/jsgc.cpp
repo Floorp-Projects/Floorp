0a49f8beda33def7d2d1da822ac9386bfaa410f6
Benjamin Bouvier <benj@benj.me>
2015-06-09 10:27:51 +0200

    Bug 1172824: Initialize a few members in CompartmentCheckTracer (CID 1304705); r=terrence

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -3680,7 +3680,9 @@ class CompartmentCheckTracer : public JS::CallbackTracer
     void trace(void** thingp, JS::TraceKind kind) override;
 
   public:
-    explicit CompartmentCheckTracer(JSRuntime* rt) : JS::CallbackTracer(rt) {}
+    explicit CompartmentCheckTracer(JSRuntime* rt)
+      : JS::CallbackTracer(rt), src(nullptr), zone(nullptr), compartment(nullptr)
+    {}
 
     Cell* src;
     JS::TraceKind srcKind;
