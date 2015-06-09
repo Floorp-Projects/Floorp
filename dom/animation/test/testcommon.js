08b54f6f2c83f64c49cb0b99f4dbc7a85711917c
Brian Birtles <birtles@gmail.com>
2015-06-09 11:13:54 +0900

    Bug 1171817 part 14 - Add AnimationPtrComparator class; r=dbaron
    Having created composite ordering methods for the different kinds of animations
    this patch adds a Comparator class so that they can be used to sort an
    array of such animations.
    
    This patch uses this Comparator object to sort the results returned by
    Element.getAnimations. For this case, the order in which we create animations
    and transitions happens to almost perfectly correspond with the composite
    ordering defined so that no sorting is necessary.
    
    One exception is that some -moz-* transitions may be created after transitions
    that they should sort before when sorting by transition property. In this
    case the sorting added in this patch should ensure they are returned in the
    correct sequence.
    
    Unfortunately, we can't easily test this since the test files we have are
    intended to be cross-browser (where -moz-* properties won't be supported).
    
    Once we implement AnimationTimeline.getAnimations (bug 1150810) we'll have
    a better opportunity to test this sorting. For now, the added tests in this
    patch just serve as a regression test that the sorting hasn't upset the
    already correct order (and an interop test in future once we move them to
    web-platform-tests).

--- dom/animation/test/testcommon.js
+++ dom/animation/test/testcommon.js
@@ -78,8 +78,9 @@ function flushComputedStyle(elem) {
 }
 
 for (var funcName of ["async_test", "assert_not_equals", "assert_equals",
-                      "assert_approx_equals", "assert_less_than_equal",
-                      "assert_between_inclusive", "assert_true", "assert_false",
+                      "assert_approx_equals", "assert_less_than",
+                      "assert_less_than_equal", "assert_between_inclusive",
+                      "assert_true", "assert_false",
                       "assert_class_string", "assert_throws", "test"]) {
   window[funcName] = opener[funcName].bind(opener);
 }
