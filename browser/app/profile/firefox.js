197c33cfb2d4c4a0a20cb9edeaac968e3faa555b
Francois Marier <francois@mozilla.com>
2015-06-10 17:35:18 +1200

    Bug 1165816 - Cancel remote application reputation requests after a certain timeout. r=gcp

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -975,6 +975,7 @@ pref("browser.safebrowsing.enabled", true);
 pref("browser.safebrowsing.malware.enabled", true);
 pref("browser.safebrowsing.downloads.enabled", true);
 pref("browser.safebrowsing.downloads.remote.enabled", true);
+pref("browser.safebrowsing.downloads.remote.timeout_ms", 10000);
 pref("browser.safebrowsing.debug", false);
 
 pref("browser.safebrowsing.updateURL", "https://safebrowsing.google.com/safebrowsing/downloads?client=SAFEBROWSING_ID&appver=%VERSION%&pver=2.2&key=%GOOGLE_API_KEY%");
