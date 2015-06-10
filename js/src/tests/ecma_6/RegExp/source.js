2d042b37bae5ea35ae5f10296740f55c709ecc3d
Ryan VanderMeulen <ryanvm@gmail.com>
2015-06-10 18:25:21 -0400

    Backed out changeset 6fe433fed5fb (bug 1150297) for suspicion of causing widespread test failures.

--- js/src/tests/ecma_6/RegExp/source.js
+++ js/src/tests/ecma_6/RegExp/source.js
@@ -3,8 +3,6 @@ var summary = "Implement RegExp.prototype.source";
 
 print(BUGNUMBER + ": " + summary);
 
-// source is moved to RegExp instance property again (bug 1138325), but keep
-// following behavior.
 assertEq(RegExp.prototype.source, "(?:)");
 assertEq(/foo/.source, "foo");
 assertEq(/foo/iymg.source, "foo");
@@ -16,6 +14,16 @@ assertEq(RegExp("", "mygi").source, "(?:)");
 assertEq(RegExp("/").source, "\\/");
 assertEq(RegExp("\n\r").source, "\\n\\r");
 assertEq(RegExp("\u2028\u2029").source, "\\u2028\\u2029");
+
+assertThrowsInstanceOf(() => genericSource(), TypeError);
+assertThrowsInstanceOf(() => genericSource(1), TypeError);
+assertThrowsInstanceOf(() => genericSource(""), TypeError);
+assertThrowsInstanceOf(() => genericSource({}), TypeError);
+assertThrowsInstanceOf(() => genericSource(new Proxy(/foo/, {get(){ return true; }})), TypeError);
+
+function genericSource(obj) {
+    return Object.getOwnPropertyDescriptor(RegExp.prototype, "source").get.call(obj);
+}
 
 if (typeof reportCompare === "function")
     reportCompare(true, true);
