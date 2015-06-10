ebacdfabcb5b260936a6acae9adcee2bf984a5d4
Eric Rahm <erahm@mozilla.com>
2015-06-10 15:12:33 -0700

    Bug 1146580 - Make FinalizationWitnessService listen for xpcom shutdown. r=bholley

--- toolkit/components/build/nsToolkitCompsModule.cpp
+++ toolkit/components/build/nsToolkitCompsModule.cpp
@@ -120,7 +120,7 @@ NS_GENERIC_FACTORY_CONSTRUCTOR(nsBrowserStatusFilter)
 #if defined(MOZ_UPDATER) && !defined(MOZ_WIDGET_ANDROID)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsUpdateProcessor)
 #endif
-NS_GENERIC_FACTORY_CONSTRUCTOR(FinalizationWitnessService)
+NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(FinalizationWitnessService, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR(NativeOSFileInternalsService)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(NativeFileWatcherService, Init)
 
