b0bcd120ae3f7a9cbde8297c5db77f3f6bb98c0c
Gabriel Luong <gabriel.luong@gmail.com>
2015-06-10 00:18:43 -0700

    Bug 1084525 - Part 7: Expose Promise life time in object grip r=fitzgen

---
+++ toolkit/devtools/server/tests/unit/test_promises_object_creationtimestamp.js
@@ -0,0 +1,69 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Test that we get the approximate time range for promise creation timestamp.
+ */
+
+"use strict";
+
+const { PromisesFront } = devtools.require("devtools/server/actors/promises");
+
+let events = devtools.require("sdk/event/core");
+
+add_task(function*() {
+  let client = yield startTestDebuggerServer("promises-object-test");
+  let chromeActors = yield getChromeActors(client);
+
+  ok(Promise.toString().contains("native code"), "Expect native DOM Promise.");
+
+  yield testPromiseCreationTimestamp(client, chromeActors, v => {
+    return new Promise(resolve => resolve(v));
+  });
+
+  let response = yield listTabs(client);
+  let targetTab = findTab(response.tabs, "promises-object-test");
+  ok(targetTab, "Found our target tab.");
+
+  yield testPromiseCreationTimestamp(client, targetTab, v => {
+    const debuggee = DebuggerServer.getTestGlobal("promises-object-test");
+    return debuggee.Promise.resolve(v);
+  });
+
+  yield close(client);
+});
+
+function* testPromiseCreationTimestamp(client, form, makePromise) {
+  let front = PromisesFront(client, form);
+  let resolution = "MyLittleSecret" + Math.random();
+
+  yield front.attach();
+  yield front.listPromises();
+
+  let onNewPromise = new Promise(resolve => {
+    events.on(front, "new-promises", promises => {
+      for (let p of promises) {
+        if (p.promiseState.state === "fulfilled" &&
+            p.promiseState.value === resolution) {
+          resolve(p);
+        }
+      }
+    });
+  });
+
+  let start = Date.now();
+  let promise = makePromise(resolution);
+  let end = Date.now();
+
+  let grip = yield onNewPromise;
+  ok(grip, "Found our new promise.");
+
+  let creationTimestamp = grip.promiseState.creationTimestamp;
+
+  ok(start - 1 <= creationTimestamp && creationTimestamp <= end + 1,
+    "Expect promise creation timestamp to be within elapsed time range.");
+
+  yield front.detach();
+  // Appease eslint
+  void promise;
+}
