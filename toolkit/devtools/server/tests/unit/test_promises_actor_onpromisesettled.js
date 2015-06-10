59478e85bffd1b721534d3fd1c39567d56b39775
Gabriel Luong <gabriel.luong@gmail.com>
2015-06-10 00:18:43 -0700

    Bug 1084525 - Part 7: Expose Promise life time in object grip r=fitzgen

--- toolkit/devtools/server/tests/unit/test_promises_actor_onpromisesettled.js
+++ toolkit/devtools/server/tests/unit/test_promises_actor_onpromisesettled.js
@@ -67,6 +67,8 @@ function oncePromiseSettled(front, resolution, resolveValue, rejectValue) {
       for (let p of promises) {
         equal(p.type, "object", "Expect type to be Object");
         equal(p.class, "Promise", "Expect class to be Promise");
+        equal(typeof p.promiseState.creationTimestamp, "number",
+          "Expect creation timestamp to be a number");
 
         if (p.promiseState.state === "fulfilled" &&
             p.promiseState.value === resolution) {
