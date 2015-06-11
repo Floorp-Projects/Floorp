76dbaf8375d7545d7c2accd21bc33d132b4d262e
Bobby Holley <bobbyholley@gmail.com>
2015-06-10 20:08:59 -0700

    Bug 1173641 - Remove now-unnecessary null-out in MediaDecoderReader::BreakCycles. r=jww
    As soon as a class has a mirror or canonical, it's going to hold onto the task
    queue until destruction anyway.

--- dom/media/MediaDecoderReader.cpp
+++ dom/media/MediaDecoderReader.cpp
@@ -338,7 +338,9 @@ MediaDecoderReader::RequestAudioData()
 void
 MediaDecoderReader::BreakCycles()
 {
-  mTaskQueue = nullptr;
+  // Nothing left to do here these days. We keep this method around so that, if
+  // we need it, we don't have to make all of the subclass implementations call
+  // the superclass method again.
 }
 
 nsRefPtr<ShutdownPromise>
