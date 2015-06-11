a2822037f7cc9c5a4fbd9c2d0e8210f072eefa22
Robert Strong <robert.bugzilla@gmail.com>
2015-06-10 19:36:26 -0700

    Bug 1171792 - An update is attempted from an update notification when it is not possible to update. r=spohl, a=lhenry

--- toolkit/mozapps/update/content/updates.js
+++ toolkit/mozapps/update/content/updates.js
@@ -444,8 +444,17 @@ var gUpdates = {
               return;
           }
         }
-        if (this.update.licenseURL)
+
+        let aus = CoC["@mozilla.org/updates/update-service;1"].
+                  getService(CoI.nsIApplicationUpdateService);
+        if (!aus.canApplyUpdates) {
+          aCallback("manualUpdate");
+          return;
+        }
+
+        if (this.update.licenseURL) {
           this.wiz.getPageById(this.updatesFoundPageId).setAttribute("next", "license");
+        }
 
         var self = this;
         this.getShouldCheckAddonCompatibility(function(shouldCheck) {
@@ -453,8 +462,7 @@ var gUpdates = {
             var incompatCheckPage = document.getElementById("incompatibleCheck");
             incompatCheckPage.setAttribute("next", self.updatesFoundPageId);
             aCallback(incompatCheckPage.id);
-          }
-          else {
+          } else {
             aCallback(self.updatesFoundPageId);
           }
         });
