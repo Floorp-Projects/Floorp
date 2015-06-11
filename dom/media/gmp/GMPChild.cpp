79d3a1f9865ce8fd5dd4026a7256d62a2e9b47dd
Gerald Squelart <gsquelart@mozilla.com>
2015-06-11 22:05:00 +0200

    Bug 1173631 - [EME] Implement async shutdown in clearkey GMP. r=cpearce

--- dom/media/gmp/GMPChild.cpp
+++ dom/media/gmp/GMPChild.cpp
@@ -587,6 +587,7 @@ GMPChild::ShutdownComplete()
 {
   LOGD("%s", __FUNCTION__);
   MOZ_ASSERT(mGMPMessageLoop == MessageLoop::current());
+  mAsyncShutdown = nullptr;
   SendAsyncShutdownComplete();
 }
 
