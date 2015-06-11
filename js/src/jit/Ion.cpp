18222a8851bca593bcd6ae93d0846ac3b3fca2a6
Brian Hackett <bhackett1024@gmail.com>
2015-06-11 08:38:59 -0700

    Bug 1172545 - Recover from OOM if Ion compilation is skipped due to not being able to allocate a 'this' object, r=jandem.

--- js/src/jit/Ion.cpp
+++ js/src/jit/Ion.cpp
@@ -2429,8 +2429,10 @@ jit::CanEnter(JSContext* cx, RunState& state)
             return Method_CantCompile;
         }
 
-        if (!state.maybeCreateThisForConstructor(cx))
+        if (!state.maybeCreateThisForConstructor(cx)) {
+            cx->recoverFromOutOfMemory();
             return Method_Skipped;
+        }
     }
 
     // If --ion-eager is used, compile with Baseline first, so that we
