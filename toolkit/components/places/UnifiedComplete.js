34bd148a2894fa26c0689752e9bbadc915fde825
Marco Bonardo <mbonardo@mozilla.com>
2015-06-11 13:20:46 -0700

    Bug 1162140 - Allow user to select search suggestions in Awesome Bar. r=adw

--- toolkit/components/places/UnifiedComplete.js
+++ toolkit/components/places/UnifiedComplete.js
@@ -966,7 +966,7 @@ Search.prototype = {
     match.engineAlias = alias;
     let query = this._searchTokens.slice(1).join(" ");
 
-    yield this._addSearchEngineMatch(match, query);
+    this._addSearchEngineMatch(match, query);
     return true;
   },
 
@@ -976,16 +976,17 @@ Search.prototype = {
       return;
 
     let query = this._originalSearchString;
-
-    yield this._addSearchEngineMatch(match, query);
+    this._addSearchEngineMatch(match, query);
   },
 
-  _addSearchEngineMatch: function* (match, query) {
+  _addSearchEngineMatch(match, query, suggestion) {
     let actionURLParams = {
       engineName: match.engineName,
       input: this._originalSearchString,
       searchQuery: query,
     };
+    if (suggestion)
+      actionURLParams.searchSuggestion = suggestion;
     if (match.engineAlias) {
       actionURLParams.alias = match.engineAlias;
     }
