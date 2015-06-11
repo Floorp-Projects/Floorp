f2a58d2261e87cf477755ba3b9e1aa0aa17b53cf
Masayuki Nakano <masayuki@d-toybox.com>
2015-06-11 19:50:15 +0900

    Bug 1130937 part.2 nsGtkIMModule should set candidiate window position to bottom left of the target clause in vertical writing mode r=m_kato

--- widget/TextRange.h
+++ widget/TextRange.h
@@ -195,6 +195,18 @@ class TextRangeArray final : public nsAutoTArray<TextRange, 10>
 
   NS_INLINE_DECL_REFCOUNTING(TextRangeArray)
 
+  const TextRange* GetTargetClause() const
+  {
+    for (uint32_t i = 0; i < Length(); ++i) {
+      const TextRange& range = ElementAt(i);
+      if (range.mRangeType == NS_TEXTRANGE_SELECTEDRAWTEXT ||
+          range.mRangeType == NS_TEXTRANGE_SELECTEDCONVERTEDTEXT) {
+        return &range;
+      }
+    }
+    return nullptr;
+  }
+
 public:
   bool IsComposing() const
   {
@@ -206,18 +218,20 @@ public:
     return false;
   }
 
-  // Returns target clase offset.  If there are selected clauses, this returns
+  // Returns target clause offset.  If there are selected clauses, this returns
   // the first selected clause offset.  Otherwise, 0.
   uint32_t TargetClauseOffset() const
   {
-    for (uint32_t i = 0; i < Length(); ++i) {
-      const TextRange& range = ElementAt(i);
-      if (range.mRangeType == NS_TEXTRANGE_SELECTEDRAWTEXT ||
-          range.mRangeType == NS_TEXTRANGE_SELECTEDCONVERTEDTEXT) {
-        return range.mStartOffset;
-      }
-    }
-    return 0;
+    const TextRange* range = GetTargetClause();
+    return range ? range->mStartOffset : 0;
+  }
+
+  // Returns target clause length.  If there are selected clauses, this returns
+  // the first selected clause length.  Otherwise, UINT32_MAX.
+  uint32_t TargetClauseLength() const
+  {
+    const TextRange* range = GetTargetClause();
+    return range ? range->Length() : UINT32_MAX;
   }
 
   bool Equals(const TextRangeArray& aOther) const
