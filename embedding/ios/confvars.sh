666df95a0763dcf4b56c7f41ceb727c67deee56c
Ted Mielczarek <ted@mielczarek.org>
2015-06-12 08:50:20 -0400

    bug 1172655 - make --enable-application=embedding/ios work (NPOTB)

---
+++ embedding/ios/confvars.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_APP_NAME=geckoembed
+MOZ_APP_DISPLAYNAME=GeckoEmbed
+MOZ_UPDATER=
+MOZ_CHROME_FILE_FORMAT=omni
+MOZ_APP_VERSION=$MOZILLA_VERSION
+MOZ_PLACES=1
+MOZ_EXTENSIONS_DEFAULT=" gio"
+MOZ_SERVICES_COMMON=1
+MOZ_SERVICES_CRYPTO=1
+MOZ_SERVICES_METRICS=1
+MOZ_SERVICES_SYNC=1
+MOZ_MEDIA_NAVIGATOR=1
+MOZ_SERVICES_HEALTHREPORT=1
+MOZ_DISABLE_EXPORT_JS=1
