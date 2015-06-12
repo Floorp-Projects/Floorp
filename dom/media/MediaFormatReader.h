9fc1af89d44cdfd55ca0c06c3c1a34228d7751f8
Ryan VanderMeulen <ryanvm@gmail.com>
2015-06-12 14:45:59 -0400

    Backed out 10 changesets (bug 1163223) for OSX asserts/crashes/leaks.
    Backed out changeset 8985172298c0 (bug 1163223)
    Backed out changeset f2c4e81d7abc (bug 1163223)
    Backed out changeset 4cbdd2f615c2 (bug 1163223)
    Backed out changeset a8271be8264e (bug 1163223)
    Backed out changeset a8f7e2ad9543 (bug 1163223)
    Backed out changeset cd763e4327f3 (bug 1163223)
    Backed out changeset 3bd4024e0a70 (bug 1163223)
    Backed out changeset 2d95a87fe0ce (bug 1163223)
    Backed out changeset c00a89bad6c8 (bug 1163223)
    Backed out changeset 074ba7f9496d (bug 1163223)
    
    CLOSED TREE

--- dom/media/MediaFormatReader.h
+++ dom/media/MediaFormatReader.h
@@ -75,8 +75,6 @@ public:
 
   media::TimeIntervals GetBuffered() override;
 
-  virtual bool ForceZeroStartTime() const override;
-
   // For Media Resource Management
   void SetIdle() override;
   bool IsDormantNeeded() override;
@@ -98,6 +96,8 @@ public:
   nsRefPtr<WaitForDataPromise> WaitForData(MediaData::Type aType) override;
 
   bool IsWaitingOnCDMResource() override;
+
+  int64_t ComputeStartTime(const VideoData* aVideo, const AudioData* aAudio) override;
 
   bool UseBufferingHeuristics() override
   {
