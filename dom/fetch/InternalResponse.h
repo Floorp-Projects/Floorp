dfa942d2fb33851480fa5bb77346ab79b549c4be
Ben Kelly <ben@wanderview.com>
2015-06-12 18:59:01 -0700

    Bug 1167808 P0 Cache.put() should use internal body of opaque Response. r=nsm

--- dom/fetch/InternalResponse.h
+++ dom/fetch/InternalResponse.h
@@ -118,6 +118,17 @@ public:
   }
 
   void
+  GetInternalBody(nsIInputStream** aStream)
+  {
+    if (mWrappedResponse) {
+      MOZ_ASSERT(!mBody);
+      return mWrappedResponse->GetBody(aStream);
+    }
+    nsCOMPtr<nsIInputStream> stream = mBody;
+    stream.forget(aStream);
+  }
+
+  void
   GetBody(nsIInputStream** aStream)
   {
     if (Type() == ResponseType::Opaque) {
@@ -125,12 +136,7 @@ public:
       return;
     }
 
-    if (mWrappedResponse) {
-      MOZ_ASSERT(!mBody);
-      return mWrappedResponse->GetBody(aStream);
-    }
-    nsCOMPtr<nsIInputStream> stream = mBody;
-    stream.forget(aStream);
+    return GetInternalBody(aStream);
   }
 
   void
