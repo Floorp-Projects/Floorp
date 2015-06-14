cbcbe51e16e91d97c88cf8af711c7b48f0d95aec
Chris Peterson <cpeterson@mozilla.com>
2015-06-13 22:50:03 -0700

    Bug 554186 - Part 1: Unimplement NPN_Status API. r=josh

--- dom/plugins/base/nsPluginInstanceOwner.h
+++ dom/plugins/base/nsPluginInstanceOwner.h
@@ -63,8 +63,6 @@ public:
                     nsIInputStream *aPostStream, 
                     void *aHeadersData, uint32_t aHeadersDataLen) override;
   
-  NS_IMETHOD ShowStatus(const char16_t *aStatusMsg) override;
-  
   // This can go away, just leaving it here to avoid changing the interface.
   NPError    ShowNativeContextMenu(NPMenu* menu, void* event) override;
   
