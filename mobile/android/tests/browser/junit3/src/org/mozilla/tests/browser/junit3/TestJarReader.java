1c7438d172ba326e640579583f070128ad5e505c
Nick Alexander <nalexander@mozilla.com>
2015-06-15 16:21:55 -0700

    Bug 1168407 - Pre: Add GeckoJarReader.extractStream. r=rnewman
    The use case is to ship a classes.dex file in a Gecko add-on.  This
    makes it easy to extract such a file from an add-on to a temporary
    location.  (Sadly, the Android Dex loading classes expect files, not
    streams.)

--- mobile/android/tests/browser/junit3/src/org/mozilla/tests/browser/junit3/TestJarReader.java
+++ mobile/android/tests/browser/junit3/src/org/mozilla/tests/browser/junit3/TestJarReader.java
@@ -3,10 +3,14 @@
 
 package org.mozilla.tests.browser.junit3;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Stack;
 
 import android.test.InstrumentationTestCase;
 import org.mozilla.gecko.AppConstants;
+import org.mozilla.gecko.util.FileUtils;
 import org.mozilla.gecko.util.GeckoJarReader;
 
 import android.content.Context;
@@ -45,5 +49,70 @@ public class TestJarReader extends InstrumentationTestCase {
         url = "jar:file://" + appPath + "BAD" + "!/" + AppConstants.OMNIJAR_NAME;
         stream = GeckoJarReader.getStream(context, "jar:" + url + "!/chrome/chrome/content/branding/favicon32.png");
         assertNull(stream);
+    }
+
+    protected void assertExtractStream(String url) throws IOException {
+        final File file = GeckoJarReader.extractStream(getInstrumentation().getTargetContext(), url, getInstrumentation().getContext().getCacheDir(), ".test");
+        assertNotNull(file);
+        try {
+            assertTrue(file.getName().endsWith("temp"));
+            final String contents = FileUtils.getFileContents(file);
+            assertNotNull(contents);
+            assertTrue(contents.length() > 0);
+        } finally {
+            file.delete();
+        }
+    }
+
+    public void testExtractStream() throws IOException {
+        String appPath = getInstrumentation().getTargetContext().getPackageResourcePath();
+        assertNotNull(appPath);
+
+        // We don't have a lot of good files to choose from.  package-name.txt isn't included in Gradle APKs.
+        assertExtractStream("jar:file://" + appPath + "!/resources.arsc");
+
+        final String url = GeckoJarReader.getJarURL(getInstrumentation().getTargetContext(), "chrome.manifest");
+        assertExtractStream(url);
+
+        // Now use an extracted copy of chrome.manifest to test further.
+        final File file = GeckoJarReader.extractStream(getInstrumentation().getTargetContext(), url, getInstrumentation().getContext().getCacheDir(), ".test");
+        assertNotNull(file);
+        try {
+            assertExtractStream("file://" + file.getAbsolutePath()); // file:// URI.
+            assertExtractStream(file.getAbsolutePath()); // Vanilla path.
+        } finally {
+            file.delete();
+        }
+    }
+
+    protected void assertExtractStreamFails(String url) throws IOException {
+        final File file = GeckoJarReader.extractStream(getInstrumentation().getTargetContext(), url, getInstrumentation().getContext().getCacheDir(), ".test");
+        assertNull(file);
+    }
+
+    public void testExtractStreamFailureCases() throws IOException {
+        String appPath = getInstrumentation().getTargetContext().getPackageResourcePath();
+        assertNotNull(appPath);
+
+        // First, a bad APK.
+        assertExtractStreamFails("jar:file://" + appPath + "BAD!/resources.arsc");
+
+        // Second, a bad file in the APK.
+        assertExtractStreamFails("jar:file://" + appPath + "!/BADresources.arsc");
+
+        // Now a bad file in the omnijar.
+        final String badUrl = GeckoJarReader.getJarURL(getInstrumentation().getTargetContext(), "BADchrome.manifest");
+        assertExtractStreamFails(badUrl);
+
+        // Now use an extracted copy of chrome.manifest to test further.
+        final String goodUrl = GeckoJarReader.getJarURL(getInstrumentation().getTargetContext(), "chrome.manifest");
+        final File file = GeckoJarReader.extractStream(getInstrumentation().getTargetContext(), goodUrl, getInstrumentation().getContext().getCacheDir(), ".test");
+        assertNotNull(file);
+        try {
+            assertExtractStreamFails("file://" + file.getAbsolutePath() + "BAD"); // Bad file:// URI.
+            assertExtractStreamFails(file.getAbsolutePath() + "BAD"); //Bad vanilla path.
+        } finally {
+            file.delete();
+        }
     }
 }
