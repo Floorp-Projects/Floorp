51e64c0d2c7e6f74786413331db6ebcc943f7004
Jocelyn Liu <joliu@mozilla.com>
2015-06-15 08:41:00 -0400

    Bug 1174717 - Passing fallible when calling AppendElement method for Sequence types in bluetooth2. r=shuang

--- dom/bluetooth/BluetoothCommon.h
+++ dom/bluetooth/BluetoothCommon.h
@@ -124,6 +124,18 @@ extern bool gBluetoothDebugFlag;
   } while(0)                                                         \
 
 /**
+ * Convert an enum value to string then append it to a fallible array.
+ */
+#define BT_APPEND_ENUM_STRING_FALLIBLE(array, enumType, enumValue)   \
+  do {                                                               \
+    uint32_t index = uint32_t(enumValue);                            \
+    nsAutoString name;                                               \
+    name.AssignASCII(enumType##Values::strings[index].value,         \
+                     enumType##Values::strings[index].length);       \
+    array.AppendElement(name, mozilla::fallible);                    \
+  } while(0)                                                         \
+
+/**
  * Resolve |promise| with |ret| if |x| is false.
  */
 #define BT_ENSURE_TRUE_RESOLVE(x, promise, ret)                      \
