c754b5fd8917dea29b7d9bc2a0334f1f2a623259
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-06-14 21:37:13 -0700

    Bug 1174220 - Part 1: Remove capacity constructor of MediaLargeByteBuffer. r=jya

--- dom/media/MediaData.h
+++ dom/media/MediaData.h
@@ -413,8 +413,6 @@ private:
   // It is designed to share potentially big byte arrays.
 class MediaLargeByteBuffer : public FallibleTArray<uint8_t> {
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(MediaLargeByteBuffer);
-  MediaLargeByteBuffer() = default;
-  explicit MediaLargeByteBuffer(size_t aCapacity) : FallibleTArray<uint8_t>(aCapacity) {}
 
 private:
   ~MediaLargeByteBuffer() {}
