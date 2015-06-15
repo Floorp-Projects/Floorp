9340699ac31bff8b2fcd76c125c928deab59e9ef
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-06-15 14:44:03 +0200

    Bug 1171017: Move classes from ipc/bluetooth to ipc/hal, r=shuang
    The class |DaemonSocket| and its helpers implement a service-
    neutral connection to a HAL daemon. This patch moves the code
    to an appropriate directory and breaks up the code into smaller
    pieces.

--- ipc/hal/DaemonSocketConsumer.cpp
+++ ipc/hal/DaemonSocketConsumer.cpp
@@ -4,10 +4,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "BluetoothDaemonConnectionConsumer.h"
+#include "DaemonSocketConsumer.h"
 
 namespace mozilla {
 namespace ipc {
+
+//
+// DaemonSocketIOConsumer
+//
+
+DaemonSocketIOConsumer::DaemonSocketIOConsumer()
+{ }
+
+DaemonSocketIOConsumer::~DaemonSocketIOConsumer()
+{ }
 
 //
 // DaemonSocketConsumer
