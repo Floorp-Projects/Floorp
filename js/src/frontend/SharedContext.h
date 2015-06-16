ef1aec3211d66533eac18e56c846a247917c62a5
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-06-16 10:46:03 +0200

    Backed out changeset 77283cc3e7e2 (bug 1165486)

--- js/src/frontend/SharedContext.h
+++ js/src/frontend/SharedContext.h
@@ -280,14 +280,6 @@ class GlobalSharedContext : public SharedContext
         }
         return false;
     }
-
-    bool inWith() const {
-        for (StaticScopeIter<CanGC> it(context, topStaticScope_); !it.done(); it++) {
-            if (it.type() == StaticScopeIter<CanGC>::With)
-                return true;
-        }
-        return false;
-    }
 };
 
 class FunctionBox : public ObjectBox, public SharedContext
