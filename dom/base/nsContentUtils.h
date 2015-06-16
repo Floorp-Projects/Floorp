83cd9ed67a23c75e50ea6d3e12aaefc368b53aa4
Ryan VanderMeulen <ryanvm@gmail.com>
2015-06-16 15:37:37 -0400

    Backed out changesets a08167680328 and b008f158e91f (bug 1093611) for talos svgr timeouts.

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -1943,14 +1943,6 @@ public:
   }
 
   /*
-   * Returns true if URL getters should percent decode the value of the segment
-   */
-  static bool GettersDecodeURLHash()
-  {
-    return sGettersDecodeURLHash && sEncodeDecodeURLHash;
-  }
-
-  /*
    * Returns true if the browser should attempt to prevent content scripts
    * from collecting distinctive information about the browser that could
    * be used to "fingerprint" and track the user across websites.
@@ -2500,7 +2492,6 @@ private:
   static bool sIsUserTimingLoggingEnabled;
   static bool sIsExperimentalAutocompleteEnabled;
   static bool sEncodeDecodeURLHash;
-  static bool sGettersDecodeURLHash;
   static bool sPrivacyResistFingerprinting;
 
   static nsHtml5StringParser* sHTMLFragmentParser;
