dd657302287f01f7af96a3e6721f1118ccf37556
Richard Newman <rnewman@mozilla.com>
2015-06-15 18:56:06 -0700

    Bug 1031569 - Don't query for non-exported activities, don't crash if we fail to launch one. r=mfinkle, a=sylvestre on a CLOSED TREE
    Tested youtube.com, send tab, sharing menu.

--- mobile/android/base/IntentHelper.java
+++ mobile/android/base/IntentHelper.java
@@ -102,7 +102,13 @@ public final class IntentHelper implements GeckoEventListener {
                                                        message.optString("title"));
         intent.setClassName(message.optString("packageName"), message.optString("className"));
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        ActivityHandlerHelper.startIntentForActivity(activity, intent, new ResultHandler(message));
+
+        final ResultHandler handler = new ResultHandler(message);
+        try {
+            ActivityHandlerHelper.startIntentForActivity(activity, intent, handler);
+        } catch (SecurityException e) {
+            Log.w(LOGTAG, "Forbidden to launch activity.", e);
+        }
     }
 
     private void openWebActivity(JSONObject message) throws JSONException {
