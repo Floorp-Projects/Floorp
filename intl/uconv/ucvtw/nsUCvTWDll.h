a5644ff815283bbac701f9b0952f13e3e76b6511
Henri Sivonen <hsivonen@hsivonen.fi>
2015-06-16 15:26:10 +0300

    Bug 912470 part 1 - Implement Encoding Standard-compliant big5 decoder. r=emk.

--- intl/uconv/ucvtw/nsUCvTWDll.h
+++ intl/uconv/ucvtw/nsUCvTWDll.h
@@ -7,9 +7,6 @@
 #define nsUCvTWDll_h_
 
 extern const uint16_t g_ufBig5Mapping[];
-extern const uint16_t g_utBIG5Mapping[];
 extern const uint16_t g_ASCIIMappingTable[];
-extern const uint16_t g_ufBig5HKSCSMapping[];
-extern const uint16_t g_utBig5HKSCSMapping[];
 
 #endif /* nsUCvTWDll_h_ */
