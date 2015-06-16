643b5eed6aeee835a038daafd9f140bd3dfb5d90
Panos Astithas <past@mozilla.com>
2015-06-16 04:05:00 +0200

    Bug 1160837 - The network panel should only record beacons from the monitored page. r=ochameau a=sledru

---
+++ browser/devtools/netmonitor/test/browser_net_send-beacon.js
@@ -0,0 +1,27 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Tests if beacons are handled correctly.
+ */
+
+let test = Task.async(function*() {
+  let [, debuggee, monitor] = yield initNetMonitor(SEND_BEACON_URL);
+  let { RequestsMenu } = monitor.panelWin.NetMonitorView;
+
+  RequestsMenu.lazyUpdate = false;
+
+  is(RequestsMenu.itemCount, 0, "The requests menu should be empty.");
+
+  debuggee.performRequest();
+
+  yield waitForNetworkEvents(monitor, 1);
+  is(RequestsMenu.itemCount, 1, "The beacon should be recorded.");
+  let request = RequestsMenu.getItemAtIndex(0);
+  is(request.attachment.method, "POST", "The method is correct.");
+  ok(request.attachment.url.endsWith("beacon_request"), "The URL is correct.");
+  is(request.attachment.status, "404", "The status is correct.");
+
+  yield teardown(monitor);
+  finish();
+});
