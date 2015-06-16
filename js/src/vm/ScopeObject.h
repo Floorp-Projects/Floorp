ea4c7752c71336f2d09767adc173ccd7aa693fd6
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-06-16 10:45:17 +0200

    Backed out changeset 42fcb204b544 (bug 1165486)

--- js/src/vm/ScopeObject.h
+++ js/src/vm/ScopeObject.h
@@ -1176,7 +1176,6 @@ CreateScopeObjectsForScopeChain(JSContext* cx, AutoObjectVector& scopeChain,
 bool HasNonSyntacticStaticScopeChain(JSObject* staticScope);
 
 #ifdef DEBUG
-void DumpStaticScopeChain(JSScript* script);
 bool
 AnalyzeEntrainedVariables(JSContext* cx, HandleScript script);
 #endif
