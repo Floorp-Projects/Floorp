c04deb201e089ce30d7db862432af63fcb8fa32b
Bill McCloskey <billm@mozilla.com>
2015-06-17 15:08:12 -0700

    Bug 1164543 - Add HasLocalInstance support (r=jimm)

--- dom/plugins/base/PluginPRLibrary.h
+++ dom/plugins/base/PluginPRLibrary.h
@@ -123,6 +123,7 @@ public:
                                          gfxContext* aCtx, const nsIntRect&) override;
     virtual void GetLibraryPath(nsACString& aPath) { aPath.Assign(mFilePath); }
     virtual nsresult GetRunID(uint32_t* aRunID) override { return NS_ERROR_NOT_IMPLEMENTED; }
+    virtual void SetHasLocalInstance() override { }
 
 private:
     NP_InitializeFunc mNP_Initialize;
