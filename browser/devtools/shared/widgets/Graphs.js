af7ea472870e859635f08d20a561888c9f0b2fa4
Victor Porof <vporof@mozilla.com>
2015-06-17 16:05:52 -0400

    Bug 1169135 - Make Graphs.jsm a require module, r=jsantell
    --HG--
    rename : browser/devtools/shared/widgets/Graphs.jsm => browser/devtools/shared/widgets/Graphs.js

--- browser/devtools/shared/widgets/Graphs.js
+++ browser/devtools/shared/widgets/Graphs.js
@@ -3,29 +3,25 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-const Cu = Components.utils;
+const { Cc, Ci, Cu, Cr } = require("chrome");
 
-Cu.import("resource:///modules/devtools/ViewHelpers.jsm");
-const promise = Cu.import("resource://gre/modules/Promise.jsm", {}).Promise;
-const {Task} = Cu.import("resource://gre/modules/Task.jsm", {});
-const {EventEmitter} = Cu.import("resource://gre/modules/devtools/event-emitter.js", {});
-const {DevToolsWorker} = Cu.import("resource://gre/modules/devtools/shared/worker.js", {});
-const {LayoutHelpers} = Cu.import("resource://gre/modules/devtools/LayoutHelpers.jsm", {});
+const { Task } = Cu.import("resource://gre/modules/Task.jsm", {});
+const { ViewHelpers } = require("resource:///modules/devtools/ViewHelpers.jsm");
+const { Heritage, setNamedTimeout, clearNamedTimeout } = require("resource:///modules/devtools/ViewHelpers.jsm");
 
-this.EXPORTED_SYMBOLS = [
-  "GraphCursor",
-  "GraphArea",
-  "GraphAreaDragger",
-  "GraphAreaResizer",
-  "AbstractCanvasGraph",
-  "LineGraphWidget",
-  "BarGraphWidget",
-  "CanvasGraphUtils"
-];
+loader.lazyRequireGetter(this, "promise");
+loader.lazyRequireGetter(this, "EventEmitter",
+  "devtools/toolkit/event-emitter");
+
+loader.lazyImporter(this, "DevToolsWorker",
+  "resource://gre/modules/devtools/shared/worker.js");
+loader.lazyImporter(this, "LayoutHelpers",
+  "resource://gre/modules/devtools/LayoutHelpers.jsm");
 
 const HTML_NS = "http://www.w3.org/1999/xhtml";
 const GRAPH_SRC = "chrome://browser/content/devtools/graphs-frame.xhtml";
 const WORKER_URL = "resource:///modules/devtools/GraphsWorker.js";
+
 const L10N = new ViewHelpers.L10N();
 
 // Generic constants.
@@ -2224,6 +2220,15 @@ function findFirst(array, predicate) {
     if (predicate(element)) return element;
   }
 }
+
+exports.GraphCursor = GraphCursor;
+exports.GraphArea = GraphArea;
+exports.GraphAreaDragger = GraphAreaDragger;
+exports.GraphAreaResizer = GraphAreaResizer;
+exports.AbstractCanvasGraph = AbstractCanvasGraph;
+exports.LineGraphWidget = LineGraphWidget;
+exports.BarGraphWidget = BarGraphWidget;
+exports.CanvasGraphUtils = CanvasGraphUtils;
 
 /**
  * Finds the last element in an array that validates a predicate.
