729612bdbc4fdd8ed40f7808a87f85a2935b6093
Nick Fitzgerald <fitzgen@gmail.com>
2015-06-17 11:12:23 -0700

    Bug 1149294 - Part 2: expose ChromeUtils and HeapSnapshot to workers; r=bholley

---
+++ toolkit/devtools/server/tests/unit/heap-snapshot-worker.js
@@ -0,0 +1,44 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+console.log("Initializing worker.");
+
+self.onmessage = e => {
+  console.log("Starting test.");
+  try {
+    const { filePath } = e.data;
+
+    ok(ChromeUtils, "Should have access to ChromeUtils");
+    ok(HeapSnapshot, "Should have access to HeapSnapshot");
+
+    ChromeUtils.saveHeapSnapshot(filePath, { globals: [this] });
+    ok(true, "Should be able to save a snapshot.");
+
+    const snapshot = ChromeUtils.readHeapSnapshot(filePath);
+    ok(snapshot, "Should be able to read a heap snapshot");
+    ok(snapshot instanceof HeapSnapshot, "Should be an instanceof HeapSnapshot");
+  } catch (e) {
+    ok(false, "Unexpected error inside worker:\n" + e.toString() + "\n" + e.stack);
+  } finally {
+    done();
+  }
+};
+
+// Proxy assertions to the main thread.
+function ok(val, msg) {
+  console.log("ok(" + !!val + ", \"" + msg + "\")");
+  self.postMessage({
+    type: "assertion",
+    passed: !!val,
+    msg,
+    stack: Error().stack
+  });
+}
+
+// Tell the main thread we are done with the tests.
+function done() {
+  console.log("done()");
+  self.postMessage({
+    type: "done"
+  });
+}
