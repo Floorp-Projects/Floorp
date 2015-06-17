eac68dba086a2d15700278bc85a7117f142cb6b8
Andrea Marchesini <amarchesini@mozilla.com>
2015-06-17 12:17:33 +0100

    Bug 1170246 - Improving nsGlobalWindow::ConvertDialogOptions and adding some gTests, r=ehsan

--- dom/base/nsGlobalWindow.h
+++ dom/base/nsGlobalWindow.h
@@ -910,6 +910,13 @@ public:
                   mozilla::ErrorResult& aRv);
   already_AddRefed<mozilla::dom::External> GetExternal(mozilla::ErrorResult& aRv);
 
+  // Exposed only for testing
+  static bool
+  TokenizeDialogOptions(nsAString& aToken, nsAString::const_iterator& aIter,
+                        nsAString::const_iterator aEnd);
+  static void
+  ConvertDialogOptions(const nsAString& aOptions, nsAString& aResult);
+
 protected:
   bool AlertOrConfirm(bool aAlert, const nsAString& aMessage,
                       mozilla::ErrorResult& aError);
