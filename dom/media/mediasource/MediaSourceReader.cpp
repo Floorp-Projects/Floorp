e8d95a9dfc80cc6b884958a95db0317c8604675f
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-06-17 22:53:45 +1000

    Bug 1173792 - Seek to original seeking position should video seeked time not found. r=cpearce, a=lizzard

--- dom/media/mediasource/MediaSourceReader.cpp
+++ dom/media/mediasource/MediaSourceReader.cpp
@@ -47,6 +47,7 @@ MediaSourceReader::MediaSourceReader(MediaSourceDecoder* aDecoder)
   : MediaDecoderReader(aDecoder)
   , mLastAudioTime(0)
   , mLastVideoTime(0)
+  , mOriginalSeekTime(-1)
   , mPendingSeekTime(-1)
   , mWaitingForSeekData(false)
   , mSeekToEnd(false)
@@ -831,6 +832,7 @@ MediaSourceReader::Seek(int64_t aTime, int64_t aIgnored /* Used only for ogg whi
 
   // Store pending seek target in case the track buffers don't contain
   // the desired time and we delay doing the seek.
+  mOriginalSeekTime = aTime;
   mPendingSeekTime = aTime;
 
   {
@@ -915,7 +917,8 @@ MediaSourceReader::DoAudioSeek()
   if (mSeekToEnd) {
     seekTime = LastSampleTime(MediaData::AUDIO_DATA);
   }
-  if (SwitchAudioSource(&seekTime) == SOURCE_NONE) {
+  if (SwitchAudioSource(&seekTime) == SOURCE_NONE &&
+      SwitchAudioSource(&mOriginalSeekTime) == SOURCE_NONE) {
     // Data we need got evicted since the last time we checked for data
     // availability. Abort current seek attempt.
     mWaitingForSeekData = true;
