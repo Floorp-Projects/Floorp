785c05633c5920679becefb5f509affd40d69fce
Dave Townsend <dtownsend@oxymoronical.com>
2015-06-19 12:01:28 -0700

    Bug 1176205: Disable search suggestions by default and hide the UI on non-nightly builds. r=adw

--- browser/components/preferences/tests/browser_privacypane_5.js
+++ browser/components/preferences/tests/browser_privacypane_5.js
@@ -12,15 +12,19 @@ function test() {
   }
   loader.loadSubScript(rootDir + "privacypane_tests_perwindow.js", this);
 
-  run_test_subset([
+  let tests = [
     test_locbar_suggestion_retention("history", true),
     test_locbar_suggestion_retention("bookmark", true),
-    test_locbar_suggestion_retention("searches", true),
     test_locbar_suggestion_retention("openpage", false),
     test_locbar_suggestion_retention("history", true),
     test_locbar_suggestion_retention("history", false),
+  ];
 
-    // reset all preferences to their default values once we're done
-    reset_preferences
-  ]);
+  if (AppConstants.NIGHTLY_BUILD)
+    tests.push(test_locbar_suggestion_retention("searches", true)),
+
+  // reset all preferences to their default values once we're done
+  tests.push(reset_preferences);
+
+  run_test_subset(tests);
 }
