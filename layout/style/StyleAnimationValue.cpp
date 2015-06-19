4502eec3528c89a437bc2dae0b853b4223ba0e0f
Daniel Holbert <dholbert@cs.stanford.edu>
2015-06-19 15:56:12 -0700

    Bug 1176270: Handle self-assignment in StyleAnimationValue::operator=. r=dbaron
    a=KWierso to land while checkins are metered

--- layout/style/StyleAnimationValue.cpp
+++ layout/style/StyleAnimationValue.cpp
@@ -3564,6 +3564,10 @@ StyleAnimationValue::StyleAnimationValue(nscolor aColor, ColorConstructorType)
 StyleAnimationValue&
 StyleAnimationValue::operator=(const StyleAnimationValue& aOther)
 {
+  if (this == &aOther) {
+    return *this;
+  }
+
   FreeValue();
 
   mUnit = aOther.mUnit;
