0302505d4e8975fa41005d2d064e8e963c1056b8
Andrei Oprea <andrei.br92@gmail.com>
2015-06-18 23:05:33 -0700

    Bug 1171933-Reimplement spinners that Hello lost after markdown extraction, r=dmose

--- browser/components/loop/standalone/content/js/standaloneRoomViews.jsx
+++ browser/components/loop/standalone/content/js/standaloneRoomViews.jsx
@@ -329,6 +329,9 @@ loop.standaloneRoomViews = (function(mozL10n) {
      * room state and other flags.
      *
      * @return {Boolean} True if remote video should be rended.
+     *
+     * XXX Refactor shouldRenderRemoteVideo & shouldRenderLoading to remove
+     *     overlapping cases.
      */
     shouldRenderRemoteVideo: function() {
       switch(this.state.roomState) {
@@ -365,6 +368,43 @@ loop.standaloneRoomViews = (function(mozL10n) {
           return true;
 
       }
+    },
+
+    /**
+     * Should we render a visual cue to the user (e.g. a spinner) that a local
+     * stream is on its way from the camera?
+     *
+     * @returns {boolean}
+     * @private
+     */
+    _shouldRenderLocalLoading: function () {
+      return this.state.roomState === ROOM_STATES.MEDIA_WAIT &&
+             !this.state.localSrcVideoObject;
+    },
+
+    /**
+     * Should we render a visual cue to the user (e.g. a spinner) that a remote
+     * stream is on its way from the other user?
+     *
+     * @returns {boolean}
+     * @private
+     */
+    _shouldRenderRemoteLoading: function() {
+      return this.state.roomState === ROOM_STATES.HAS_PARTICIPANTS &&
+             !this.state.remoteSrcVideoObject &&
+             !this.state.mediaConnected;
+    },
+
+    /**
+     * Should we render a visual cue to the user (e.g. a spinner) that a remote
+     * screen-share is on its way from the other user?
+     *
+     * @returns {boolean}
+     * @private
+     */
+    _shouldRenderScreenShareLoading: function() {
+      return this.state.receivingScreenShare &&
+             !this.state.screenShareVideoObject;
     },
 
     render: function() {
@@ -406,12 +446,14 @@ loop.standaloneRoomViews = (function(mozL10n) {
               <div className={remoteStreamClasses}>
                 <sharedViews.MediaView displayAvatar={!this.shouldRenderRemoteVideo()}
                   posterUrl={this.props.remotePosterUrl}
+                  isLoading={this._shouldRenderRemoteLoading()}
                   mediaType="remote"
                   srcVideoObject={this.state.remoteSrcVideoObject} />
               </div>
               <div className={screenShareStreamClasses}>
                 <sharedViews.MediaView displayAvatar={false}
                   posterUrl={this.props.screenSharePosterUrl}
+                  isLoading={this._shouldRenderScreenShareLoading()}
                   mediaType="screen-share"
                   srcVideoObject={this.state.screenShareVideoObject} />
               </div>
@@ -422,6 +464,7 @@ loop.standaloneRoomViews = (function(mozL10n) {
               <div className="local">
                 <sharedViews.MediaView displayAvatar={this.state.videoMuted}
                   posterUrl={this.props.localPosterUrl}
+                  isLoading={this._shouldRenderLocalLoading()}
                   mediaType="local"
                   srcVideoObject={this.state.localSrcVideoObject} />
               </div>
--- browser/components/loop/test/desktop-local/roomViews_test.js
+++ browser/components/loop/test/desktop-local/roomViews_test.js
@@ -528,6 +528,58 @@ describe("loop.roomViews", function () {
             loop.shared.views.FeedbackView);
         });
 
+      it("should display loading spinner when localSrcVideoObject is null",
+         function() {
+           activeRoomStore.setStoreState({
+             roomState: ROOM_STATES.MEDIA_WAIT,
+             localSrcVideoObject: null
+           });
+
+           view = mountTestComponent();
+
+           expect(view.getDOMNode().querySelector(".local .loading-stream"))
+               .not.eql(null);
+         });
+
+      it("should not display a loading spinner when local stream available",
+         function() {
+           activeRoomStore.setStoreState({
+             roomState: ROOM_STATES.MEDIA_WAIT,
+             localSrcVideoObject: { fake: "video" }
+           });
+
+           view = mountTestComponent();
+
+           expect(view.getDOMNode().querySelector(".local .loading-stream"))
+               .eql(null);
+         });
+
+      it("should display loading spinner when remote stream is not available",
+         function() {
+           activeRoomStore.setStoreState({
+             roomState: ROOM_STATES.HAS_PARTICIPANTS,
+             remoteSrcVideoObject: null
+           });
+
+           view = mountTestComponent();
+
+           expect(view.getDOMNode().querySelector(".remote .loading-stream"))
+               .not.eql(null);
+         });
+
+      it("should not display a loading spinner when remote stream available",
+         function() {
+           activeRoomStore.setStoreState({
+             roomState: ROOM_STATES.HAS_PARTICIPANTS,
+             remoteSrcVideoObject: { fake: "video" }
+           });
+
+           view = mountTestComponent();
+
+           expect(view.getDOMNode().querySelector(".remote .loading-stream"))
+               .eql(null);
+         });
+
       it("should display an avatar for remote video when the room has participants but video is not enabled",
         function() {
           activeRoomStore.setStoreState({
