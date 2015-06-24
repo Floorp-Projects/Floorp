1ef208849d944400b284813307381689cdd0fec9
Bobby Holley <bobbyholley@gmail.com>
2015-06-24 16:01:15 -0700

    Bug 1177282 - Create and destroy static SharedThreadPool state at startup/shutdown. r=cpearce

--- dom/media/SharedThreadPool.h
+++ dom/media/SharedThreadPool.h
@@ -35,10 +35,6 @@ public:
   static TemporaryRef<SharedThreadPool> Get(const nsCString& aName,
                                             uint32_t aThreadLimit = 4);
 
-  // Spins the event loop until all thread pools are shutdown.
-  // *Must* be called on the main thread.
-  static void SpinUntilShutdown();
-
   // We implement custom threadsafe AddRef/Release pair, that destroys the
   // the shared pool singleton when the refcount drops to 0. The addref/release
   // are implemented using locking, so it's not recommended that you use them
@@ -51,11 +47,17 @@ public:
   NS_FORWARD_SAFE_NSITHREADPOOL(mPool);
   NS_FORWARD_SAFE_NSIEVENTTARGET(mEventTarget);
 
+  // Creates necessary statics. Called once at startup.
+  static void InitStatics();
+
+  // Spins the event loop until all thread pools are shutdown.
+  // *Must* be called on the main thread.
+  static void SpinUntilEmpty();
+
 private:
 
-  // Creates necessary statics.
-  // Main thread only.
-  static void EnsureInitialized();
+  // Returns whether there are no pools in existence at the moment.
+  static bool IsEmpty();
 
   // Creates a singleton SharedThreadPool wrapper around aPool.
   // aName is the name of the aPool, and is used to lookup the
