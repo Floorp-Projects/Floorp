606f67f58460443ca8e7ebd1ceb73d16477a4250
Chris Manchester <cmanchester@mozilla.com>
2015-06-24 00:10:10 -0700

    Bug 1169600 - Remove message listeners intended to coordinate registering a new browser with marionette once the browser has been registered. r=ato
    --HG--
    extra : commitid : BlpX9XbqiYM

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -465,7 +465,7 @@ GeckoDriver.prototype.registerPromise = function() {
   const li = "Marionette:register";
 
   return new Promise((resolve) => {
-    this.mm.addMessageListener(li, function cb(msg) {
+    let cb = (msg) => {
       let wid = msg.json.value;
       let be = msg.target;
       let rv = this.registerBrowser(wid, be);
@@ -481,17 +481,19 @@ GeckoDriver.prototype.registerPromise = function() {
 
       // this is a sync message and listeners expect the ID back
       return rv;
-    }.bind(this));
+    };
+    this.mm.addMessageListener(li, cb);
   });
 };
 
 GeckoDriver.prototype.listeningPromise = function() {
   const li = "Marionette:listenersAttached";
   return new Promise((resolve) => {
-    this.mm.addMessageListener(li, function() {
-      this.mm.removeMessageListener(li, this);
+    let cb = () => {
+      this.mm.removeMessageListener(li, cb);
       resolve();
-    }.bind(this));
+    };
+    this.mm.addMessageListener(li, cb);
   });
 };
 
@@ -3184,8 +3186,7 @@ BrowserObj.prototype.register = function(uid, target) {
 BrowserObj.prototype.hasRemotenessChange = function() {
   // None of these checks are relevant on b2g or if we don't have a tab yet,
   // and may not apply on Fennec.
-  if (this.driver.appName != "Firefox" || this.tab === null ||
-      !this.browserForTab) {
+  if (this.driver.appName != "Firefox" || this.tab === null) {
     return false;
   }
 
