1a70a2e8ab2c3efef5cd012862add949d22c3df5
Ryan VanderMeulen <ryanvm@gmail.com>
2015-06-24 16:03:02 -0400

    Backed out changeset 257dc5010bad (bug 1176205) for browser_privacypane_5.js failures. CLOSED TREE

--- browser/components/preferences/in-content/privacy.js
+++ browser/components/preferences/in-content/privacy.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-Components.utils.import("resource://gre/modules/AppConstants.jsm");
-
 var gPrivacyPane = {
 
   /**
@@ -105,8 +103,6 @@ var gPrivacyPane = {
                      gPrivacyPane.showCookies);
     setEventListener("clearDataSettings", "command",
                      gPrivacyPane.showClearPrivateDataSettings);
-
-    document.getElementById("searchesSuggestion").hidden = !AppConstants.NIGHTLY_BUILD;
   },
 
   // HISTORY MODE
