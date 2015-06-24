bba7759d4975422e1cd1dd875ccd39429dee4d80
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-06-24 10:10:17 +0200

    Backed out changeset ae700a7a4c5e (bug 1176034)

--- dom/messagechannel/MessagePort.cpp
+++ dom/messagechannel/MessagePort.cpp
@@ -284,7 +284,7 @@ private:
     MOZ_CRASH("Failed to create a PBackgroundChild actor!");
   }
 
-  void ActorCreated(mozilla::ipc::PBackgroundChild* aActor) override
+  void ActorCreated(mozilla::ipc::PBackgroundChild* aActor)
   {
     ForceClose(mIdentifier);
   }
