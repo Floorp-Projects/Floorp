d13eaecff36e697fc07cd8496a72f48ea021fc99
Terrence Cole <terrence@mozilla.com>
2015-06-25 15:10:00 -0700

    Bug 1176890 - Start running jstests faster on windows; r=sfink

--- js/src/tests/lib/tasks_unix.py
+++ js/src/tests/lib/tasks_unix.py
@@ -184,6 +184,7 @@ def kill_undead(tasks, timeout):
 
 def run_all_tests(tests, prefix, results, options):
     # Copy and reverse for fast pop off end.
+    tests = list(tests)
     tests = tests[:]
     tests.reverse()
 
