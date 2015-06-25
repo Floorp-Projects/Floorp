58ff804f91eded52f2e9ac66864d1a97489c1622
Nick Alexander <nalexander@mozilla.com>
2015-06-24 23:12:00 -0700

    Bug 1162191 - Add |mach artifact| for installing downloaded Fennec binaries. r=gps DONTBUILD ON A CLOSED TREE: Android-only and the build changes are cosmetic.
    Very much a first cut, but I'd like to get some Fennec early adopters testing.
    
    This adds:
    
    * |mach artifact install| to fetch and install Fennec binaries;
    * |mach artifact last| to print details about what was last installed;
    * |mach artifact {print,clear}-caches|, for debugging.
    
    This code is exposed as a new mozbuild.artifacts Python but it's not
    particularly general.  My intention was to get things out of the mach command
    more than produce a general artifact fetching API.  We can leave that bike
    shed to Bug 1124378.
    
    I've been testing this with --disable-compile-environment and it works well
    locally, although there's no reason a knowledgeable developer couldn't use
    this in conjunction with a fully-built tree.  (I don't know when such a
    situation would arise, but I know of no technical impediment.)

--- python/mozbuild/mozbuild/util.py
+++ python/mozbuild/mozbuild/util.py
@@ -119,11 +119,12 @@ class FileAvoidWrite(StringIO):
     enabled by default because it a) doesn't make sense for binary files b)
     could add unwanted overhead to calls.
     """
-    def __init__(self, filename, capture_diff=False):
+    def __init__(self, filename, capture_diff=False, mode='rU'):
         StringIO.__init__(self)
         self.name = filename
         self._capture_diff = capture_diff
         self.diff = None
+        self.mode = mode
 
     def close(self):
         """Stop accepting writes, compare file contents, and rewrite if needed.
@@ -142,7 +143,7 @@ class FileAvoidWrite(StringIO):
         old_content = None
 
         try:
-            existing = open(self.name, 'rU')
+            existing = open(self.name, self.mode)
             existed = True
         except IOError:
             pass
