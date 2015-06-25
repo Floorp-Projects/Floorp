2082ddf73d97a3a0c4906dee08f8e497ea40f211
Gregory Szorc <gps@mozilla.com>
2015-06-25 12:11:34 -0700

    Bug 1176642 - Defer import of autotry and pprint; r=chmanchester
    This was the only mach_commands.py file importing these modules. Defer
    import so mach doesn't work so hard during startup.
    
    --HG--
    extra : commitid : FrmvodfVdUZ
    extra : rebase_source : 48233660e04bf043b99c6a067c468b532c41f889

--- testing/mach_commands.py
+++ testing/mach_commands.py
@@ -5,7 +5,6 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import os
-import pprint
 import sys
 
 from mach.decorators import (
@@ -14,7 +13,6 @@ from mach.decorators import (
     Command,
 )
 
-from autotry import AutoTry
 from mozbuild.base import MachCommandBase
 
 
@@ -455,6 +453,8 @@ class PushToTry(MachCommandBase):
 
         from mozbuild.testing import TestResolver
         from mozbuild.controller.building import BuildDriver
+        from autotry import AutoTry
+        import pprint
 
         print("mach try is under development, please file bugs blocking 1149670.")
 
