33ff352b01d4b282c3b7007145b6a6c21bb28265
Alexander Surkov <surkov.alexander@gmail.com>
2015-06-26 14:41:22 -0700

    Bug 1177268 - implement aria-rowcount/index and aria-colcount/index, r=marcoz

--- accessible/generic/ARIAGridAccessible-inl.h
+++ accessible/generic/ARIAGridAccessible-inl.h
@@ -10,35 +10,15 @@
 #include "ARIAGridAccessible.h"
 
 #include "AccIterator.h"
+#include "nsAccUtils.h"
 
 namespace mozilla {
 namespace a11y {
 
-inline Accessible*
-ARIAGridCellAccessible::TableFor(Accessible* aRow) const
-{
-  if (aRow) {
-    Accessible* table = aRow->Parent();
-    if (table) {
-      roles::Role tableRole = table->Role();
-      if (tableRole == roles::GROUPING) { // if there's a rowgroup.
-        table = table->Parent();
-        if (table)
-          tableRole = table->Role();
-      }
-
-      return tableRole == roles::TABLE || tableRole == roles::TREE_TABLE ?
-        table : nullptr;
-    }
-  }
-
-  return nullptr;
-}
-
 inline int32_t
 ARIAGridCellAccessible::RowIndexFor(Accessible* aRow) const
 {
-  Accessible* table = TableFor(aRow);
+  Accessible* table = nsAccUtils::TableFor(aRow);
   if (table) {
     int32_t rowIdx = 0;
     Accessible* row = nullptr;
