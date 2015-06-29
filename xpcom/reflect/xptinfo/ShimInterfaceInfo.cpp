2c8aa842362f9b627eb110ebdbc08d6fee23db28
Nathan Froyd <froydnj@mozilla.com>
2015-06-29 10:59:59 -0400

    Bug 1178317 - eliminate large static constructor from ShimInterfaceInfo.cpp; r=poiru
    Constructing kComponentsInterfaceShimMap required a static constructor
    on some compilers, due to a non-constexpr constructor and the necessity
    of copying non-constexpr things like nsIID.  This static constructor is
    large (several kilobytes of object code on x86-64) and completely
    unnecessary.
    
    To fix this, let's add a constexpr (well, MOZ_CONSTEXPR) constructor to
    ComponentsInterfaceShimEntry.  This change alone doesn't completely
    solve our problem, because the nsIID member still needs to be copied.
    But doing that copying is silly: we only use the IID for constructing a
    ShimInterfaceInfo in ShimInterfaceInfo::MaybeConstruct, and the
    ShimInterfaceInfo constructor takes a const reference.  So let's store a
    const reference in ComponentsInterfaceShimEntry, too, and make that
    structure significantly smaller in the process.

--- xpcom/reflect/xptinfo/ShimInterfaceInfo.cpp
+++ xpcom/reflect/xptinfo/ShimInterfaceInfo.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  * vim: set ts=8 sw=4 et tw=78:
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
@@ -280,9 +280,14 @@
 using namespace mozilla;
 
 struct ComponentsInterfaceShimEntry {
+  MOZ_CONSTEXPR
+  ComponentsInterfaceShimEntry(const char* aName, const nsIID& aIID,
+                               const dom::NativePropertyHooks* aNativePropHooks)
+    : geckoName(aName), iid(aIID), nativePropHooks(aNativePropHooks) {}
+
   const char *geckoName;
-  nsIID iid;
-  const mozilla::dom::NativePropertyHooks* nativePropHooks;
+  const nsIID& iid;
+  const dom::NativePropertyHooks* nativePropHooks;
 };
 
 #define DEFINE_SHIM_WITH_CUSTOM_INTERFACE(geckoName, domName) \
