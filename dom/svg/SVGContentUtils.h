4989c7295d6b384a51ada6a2af9edcdc8328e917
Tom Klein <twointofive@gmail.com>
2015-06-29 20:19:00 +0200

    Bug 1178159 - Ignore stroke-linecap:"square" on circle and ellipse. r=longsonr

--- dom/svg/SVGContentUtils.h
+++ dom/svg/SVGContentUtils.h
@@ -323,6 +323,12 @@ public:
    */
   static mozilla::TemporaryRef<mozilla::gfx::Path>
   GetPath(const nsAString& aPathString);
+
+  /**
+   *  Returns true if aContent is one of the elements whose stroke is guaranteed
+   *  to have no corners: circle or ellipse
+   */
+  static bool ShapeTypeHasNoCorners(const nsIContent* aContent);
 };
 
 #endif
