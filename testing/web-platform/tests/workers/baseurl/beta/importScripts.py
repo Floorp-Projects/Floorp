cd354cde97cd4bf17642cbe0b97e705a1e3adc20
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-06-30 15:58:51 +0200

    Backed out changeset 641b24306056 (bug 1178754) also need backedout on request on a CLOSED TREE
    --HG--
    rename : testing/web-platform/tests/conformance-checkers/html/media-queries/projection-novalid.html => testing/web-platform/tests/conformance-checkers/html/media-queries/004-isvalid.html
    rename : testing/web-platform/tests/conformance-checkers/html/media-queries/tv-novalid.html => testing/web-platform/tests/conformance-checkers/html/media-queries/021-isvalid.html
    rename : testing/web-platform/tests/conformance-checkers/html/media-queries/device-aspect-ratio-novalid.html => testing/web-platform/tests/conformance-checkers/html/media-queries/023-isvalid.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/001-1.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/001-1.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/001a.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/001a.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/001b.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/001b.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/002-1.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/002-1.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/002a.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/002a.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/002b.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/002b.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/003-1.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/003-1.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/003a.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/003a.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/003b.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/003b.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/004-1.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/004-1.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/004a.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/004a.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/004b.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/004b.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/005-1.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/005-1.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/005a.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/005a.html
    rename : testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/support/005b.html => testing/web-platform/tests/html/browsers/browsing-the-web/unloading-documents/005b.html
    rename : testing/web-platform/tests/subresource-integrity/crossorigin-anon-style.css.headers => testing/web-platform/tests/subresource-integrity/crossorigin-anon.js.headers

--- testing/web-platform/tests/workers/baseurl/beta/importScripts.py
+++
@@ -1,3 +0,0 @@
-def main(request, response):
-    return (302, "Moved"), [("Location", "../gamma/importScripts.js")], "postMessage('executed redirecting script');"
-
--- testing/web-platform/tests/workers/baseurl/beta/sharedworker.py
+++
@@ -1,3 +0,0 @@
-def main(request, response):
-    return (302, "Moved"), [("Location", "../gamma/sharedworker.js")], "postMessage('executed redirecting script');"
-
