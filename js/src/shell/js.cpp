ab9663a611d6b4d25f4a708937d627f533a01458
Jan de Mooij <jdemooij@mozilla.com>
2015-06-30 11:20:58 -0700

    Bug 1177892 part 2 - Remove PRIVATE_TO_JSVAL. r=evilpie

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -5436,7 +5436,7 @@ static void
 InitDOMObject(HandleObject obj)
 {
     /* Fow now just initialize to a constant we can check. */
-    SetReservedSlot(obj, DOM_OBJECT_SLOT, PRIVATE_TO_JSVAL((void*)0x1234));
+    SetReservedSlot(obj, DOM_OBJECT_SLOT, PrivateValue((void*)0x1234));
 }
 
 static bool
