3b0a318c705cfcc56988d5b3045f32d06cae054f
Nathan Froyd <froydnj@mozilla.com>
2015-07-01 12:27:43 -0400

    Bug 1192230 - clean up reference-counting in xpcom/; r=erahm

--- xpcom/io/nsStringStream.cpp
+++ xpcom/io/nsStringStream.cpp
@@ -380,8 +380,7 @@ NS_NewByteInputStream(nsIInputStream** aStreamResult,
 {
   NS_PRECONDITION(aStreamResult, "null out ptr");
 
-  nsStringInputStream* stream = new nsStringInputStream();
-  NS_ADDREF(stream);
+  nsRefPtr<nsStringInputStream> stream = new nsStringInputStream();
 
   nsresult rv;
   switch (aAssignment) {
@@ -400,11 +399,10 @@ NS_NewByteInputStream(nsIInputStream** aStreamResult,
   }
 
   if (NS_FAILED(rv)) {
-    NS_RELEASE(stream);
     return rv;
   }
 
-  *aStreamResult = stream;
+  stream.forget(aStreamResult);
   return NS_OK;
 }
 
@@ -422,12 +420,11 @@ NS_NewCStringInputStream(nsIInputStream** aStreamResult,
 {
   NS_PRECONDITION(aStreamResult, "null out ptr");
 
-  nsStringInputStream* stream = new nsStringInputStream();
-  NS_ADDREF(stream);
+  nsRefPtr<nsStringInputStream> stream = new nsStringInputStream();
 
   stream->SetData(aStringToRead);
 
-  *aStreamResult = stream;
+  stream.forget(aStreamResult);
   return NS_OK;
 }
 
@@ -442,10 +439,6 @@ nsStringInputStreamConstructor(nsISupports* aOuter, REFNSIID aIID,
     return NS_ERROR_NO_AGGREGATION;
   }
 
-  nsStringInputStream* inst = new nsStringInputStream();
-  NS_ADDREF(inst);
-  nsresult rv = inst->QueryInterface(aIID, aResult);
-  NS_RELEASE(inst);
-
-  return rv;
+  nsRefPtr<nsStringInputStream> inst = new nsStringInputStream();
+  return inst->QueryInterface(aIID, aResult);
 }
