c62399b74dd46a70744615ad9aa4f6ec52b5ae15
James Cheng <jacheng@mozilla.com>
2015-07-01 00:41:00 -0400

    Bug 1037329 - Part 2: Enable SystemUpdate API on b2g. r=fabrice

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -18,6 +18,7 @@ Cu.import('resource://gre/modules/Keyboard.jsm');
 Cu.import('resource://gre/modules/ErrorPage.jsm');
 Cu.import('resource://gre/modules/AlertsHelper.jsm');
 Cu.import('resource://gre/modules/RequestSyncService.jsm');
+Cu.import('resource://gre/modules/SystemUpdateService.jsm');
 #ifdef MOZ_WIDGET_GONK
 Cu.import('resource://gre/modules/NetworkStatsService.jsm');
 Cu.import('resource://gre/modules/ResourceStatsService.jsm');
