13705721295ac8c8c1c774831293ea595f758f64
Ehsan Akhgari <ehsan@mozilla.com>
2015-07-01 18:35:23 -0400

    Bug 1179399 - Part 1: Relax the ShouldIntercept checks when overriding JAR channel info; r=jdm

--- modules/libjar/nsJARChannel.cpp
+++ modules/libjar/nsJARChannel.cpp
@@ -683,8 +683,6 @@ nsJARChannel::OverrideSecurityInfo(nsISupports* aSecurityInfo)
                      "This can only be called when we don't have a security info object already");
   MOZ_RELEASE_ASSERT(aSecurityInfo,
                      "This can only be called with a valid security info object");
-  MOZ_RELEASE_ASSERT(ShouldIntercept(),
-                     "This can only be called on channels that can be intercepted");
   mSecurityInfo = aSecurityInfo;
   return NS_OK;
 }
@@ -694,8 +692,6 @@ nsJARChannel::OverrideURI(nsIURI* aRedirectedURI)
 {
   MOZ_RELEASE_ASSERT(mLoadFlags & LOAD_REPLACE,
                      "This can only happen if the LOAD_REPLACE flag is set");
-  MOZ_RELEASE_ASSERT(ShouldIntercept(),
-                     "This can only be called on channels that can be intercepted");
   mAppURI = aRedirectedURI;
 }
 
