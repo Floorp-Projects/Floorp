1070e64f6412040420855ae544c15ccbbc2db6ec
Jim Blandy <jimb@mozilla.com>
2015-06-30 23:47:37 -0700

    Bug 1061909: Implement 'allocationStack' breakdown for Debugger.Memory.prototype.takeCensus. r=fitzgen

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -4548,7 +4548,6 @@ EntryPoints(JSContext* cx, unsigned argc, Value* vp)
     return false;
 }
 
-
 static const JSFunctionSpecWithHelp shell_functions[] = {
     JS_FN_HELP("version", Version, 0, 0,
 "version([number])",
