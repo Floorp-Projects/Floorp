1cac7d25e119c091d1c9da8f1b9e59f9fbc7392b
Bill McCloskey <billm@mozilla.com>
2015-06-30 17:12:30 -0700

    Bug 1177013 - Telemetry for CPOW cancelation. r=dvander, a=ritu

--- ipc/glue/MessageChannel.cpp
+++ ipc/glue/MessageChannel.cpp
@@ -14,6 +14,7 @@
 #include "mozilla/DebugOnly.h"
 #include "mozilla/Move.h"
 #include "mozilla/SizePrintfMacros.h"
+#include "mozilla/Telemetry.h"
 #include "nsDebug.h"
 #include "nsISupportsImpl.h"
 #include "nsContentUtils.h"
@@ -2037,6 +2038,7 @@ void
 CancelCPOWs()
 {
     if (gParentProcessBlocker) {
+        mozilla::Telemetry::Accumulate(mozilla::Telemetry::IPC_TRANSACTION_CANCEL, true);
         gParentProcessBlocker->CancelCurrentTransaction();
     }
 }
