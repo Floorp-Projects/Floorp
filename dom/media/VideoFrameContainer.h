3f804c88be6bf4b6c91322be967bc868531ddd0f
Hamzata Diallo <tdiallo@openmobileww.com>
2015-07-02 15:19:35 -0700

    Bug 1178513 - Export libxul symbols needed by ACL. r=mattwoodrow

--- dom/media/VideoFrameContainer.h
+++ dom/media/VideoFrameContainer.h
@@ -34,7 +34,7 @@ class ImageContainer;
  * confusing.
  */
 class VideoFrameContainer {
-  ~VideoFrameContainer();
+  B2G_ACL_EXPORT ~VideoFrameContainer();
 
 public:
   typedef layers::ImageContainer ImageContainer;
@@ -46,7 +46,7 @@ public:
                       already_AddRefed<ImageContainer> aContainer);
 
   // Call on any thread
-  void SetCurrentFrame(const gfxIntSize& aIntrinsicSize, Image* aImage,
+  B2G_ACL_EXPORT void SetCurrentFrame(const gfxIntSize& aIntrinsicSize, Image* aImage,
                        TimeStamp aTargetTime);
   void ClearCurrentFrame(bool aResetSize = false);
   // Reset the VideoFrameContainer
@@ -61,8 +61,8 @@ public:
     INVALIDATE_FORCE
   };
   void Invalidate() { InvalidateWithFlags(INVALIDATE_DEFAULT); }
-  void InvalidateWithFlags(uint32_t aFlags);
-  ImageContainer* GetImageContainer();
+  B2G_ACL_EXPORT void InvalidateWithFlags(uint32_t aFlags);
+  B2G_ACL_EXPORT ImageContainer* GetImageContainer();
   void ForgetElement() { mElement = nullptr; }
 
 protected:
