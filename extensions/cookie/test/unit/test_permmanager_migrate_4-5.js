c78d1da6a5bb045ecfda1f5fae84aaf5020317a4
Michael Layzell <michael@thelayzells.com>
2015-07-02 16:50:26 -0400

    Bug 1172080 - Part 2: Use ^ instead of ! to delimit originAttributes from the URI in nsIPrincipal.origin, r=bholley

--- extensions/cookie/test/unit/test_permmanager_migrate_4-5.js
+++ extensions/cookie/test/unit/test_permmanager_migrate_4-5.js
@@ -94,33 +94,33 @@ add_task(function test() {
     // However, permissions for subdomains of foo.com will be present for both http:// and https://,
     // as they do not apply to any entry in the history
     // ["http://foo.com", "A", 1, 0, 0],
-    // ["http://foo.com!appId=1000", "A", 1, 0, 0],
-    // ["http://foo.com!appId=2000&inBrowser=1", "A", 1, 0, 0],
+    // ["http://foo.com^appId=1000", "A", 1, 0, 0],
+    // ["http://foo.com^appId=2000&inBrowser=1", "A", 1, 0, 0],
 
     ["http://sub.foo.com", "B", 1, 0, 0],
     ["http://subber.sub.foo.com", "B", 1, 0, 0],
 
     ["https://foo.com", "A", 1, 0, 0],
-    ["https://foo.com!appId=1000", "A", 1, 0, 0],
-    ["https://foo.com!appId=2000&inBrowser=1", "A", 1, 0, 0],
+    ["https://foo.com^appId=1000", "A", 1, 0, 0],
+    ["https://foo.com^appId=2000&inBrowser=1", "A", 1, 0, 0],
     ["https://sub.foo.com", "B", 1, 0, 0],
     ["https://subber.sub.foo.com", "B", 1, 0, 0],
 
     // bar.ca will have both http:// and https:// for all entries, because the foo did the bar a favour
     ["http://bar.ca", "B", 1, 0, 0],
     ["https://bar.ca", "B", 1, 0, 0],
-    ["http://bar.ca!appId=1000", "B", 1, 0, 0],
-    ["https://bar.ca!appId=1000", "B", 1, 0, 0],
-    ["http://bar.ca!appId=1000&inBrowser=1", "A", 1, 0, 0],
-    ["https://bar.ca!appId=1000&inBrowser=1", "A", 1, 0, 0],
+    ["http://bar.ca^appId=1000", "B", 1, 0, 0],
+    ["https://bar.ca^appId=1000", "B", 1, 0, 0],
+    ["http://bar.ca^appId=1000&inBrowser=1", "A", 1, 0, 0],
+    ["https://bar.ca^appId=1000&inBrowser=1", "A", 1, 0, 0],
     ["file:///some/path/to/file.html", "A", 1, 0, 0],
     ["file:///another/file.html", "A", 1, 0, 0],
 
     // Because we put ftp://some.subdomain.of.foo.com:8000/some/subdirectory in the history, we should
     // also have these entries
     ["ftp://foo.com:8000", "A", 1, 0, 0],
-    ["ftp://foo.com:8000!appId=1000", "A", 1, 0, 0],
-    ["ftp://foo.com:8000!appId=2000&inBrowser=1", "A", 1, 0, 0],
+    ["ftp://foo.com:8000^appId=1000", "A", 1, 0, 0],
+    ["ftp://foo.com:8000^appId=2000&inBrowser=1", "A", 1, 0, 0],
   ];
 
   let found = expected.map((it) => 0);
