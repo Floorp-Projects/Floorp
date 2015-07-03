75b4073eb3929040c81bc3c0d639f306b0701df5
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-07-03 14:26:56 +0200

    Backed out changeset 5f2ca3ac2fcb (bug 1048131) for test failures in android s4 test_captive_portal_not_found.js

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -66,9 +66,11 @@ XPCOMUtils.defineLazyGetter(this, "libcutils", function () {
 });
 #endif
 
+#ifdef MOZ_CAPTIVEDETECT
 XPCOMUtils.defineLazyServiceGetter(Services, 'captivePortalDetector',
                                   '@mozilla.org/toolkit/captive-detector;1',
                                   'nsICaptivePortalDetector');
+#endif
 
 #ifdef MOZ_SAFE_BROWSING
 XPCOMUtils.defineLazyModuleGetter(this, "SafeBrowsing",
