b06a6808bddd887513728e058e23e02798799d31
Ehsan Akhgari <ehsan@mozilla.com>
2015-07-04 16:24:50 -0400

    Bug 486262 - Part 1: Provide a visual indicator for tabs that are playing audio; r=dao,jaws

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -495,6 +495,12 @@ pref("browser.tabs.drawInTitlebar", true);
 // false  return to the adjacent tab (old default)
 pref("browser.tabs.selectOwnerOnClose", true);
 
+#ifdef RELEASE_BUILD
+pref("browser.tabs.showAudioPlayingIcon", false);
+#else
+pref("browser.tabs.showAudioPlayingIcon", true);
+#endif
+
 pref("browser.ctrlTab.previews", false);
 
 // By default, do not export HTML at shutdown.
