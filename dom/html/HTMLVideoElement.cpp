37a45c793dc1b05937df5606d0e950332719cbd3
Ehsan Akhgari <ehsan@mozilla.com>
2015-07-05 18:36:49 -0400

    Bug 1180535 - Dispatch the media-playback notification when navigating away from a page that has a media element playing; r=baku
    When navigating away from a document, we mute the playing media elements
    through the NotifyOwnerDocumentActivityChanged() notification.
    Sometimes, that function may notify the audio channel agent through its
    call to AddRemoveSelfReference() which may call
    UpdateAudioChannelPlayingState() and notify the agent, but when we're
    navigating away from the page, playingThroughTheAudioChannel will always
    be equal to mPlayingThroughTheAudioChannel, which causes us to not
    notify the audio channel agent.
    
    This patch fixes this by separating NotifyOwnerDocumentActivityChanged()
    from its internal consumers, and forcefully notifying the audio channel
    agent when we navigate away.

--- dom/html/HTMLVideoElement.cpp
+++ dom/html/HTMLVideoElement.cpp
@@ -192,11 +192,12 @@ HTMLVideoElement::WrapNode(JSContext* aCx, JS::Handle<JSObject*> aGivenProto)
   return HTMLVideoElementBinding::Wrap(aCx, this, aGivenProto);
 }
 
-void
-HTMLVideoElement::NotifyOwnerDocumentActivityChanged()
+bool
+HTMLVideoElement::NotifyOwnerDocumentActivityChangedInternal()
 {
-  HTMLMediaElement::NotifyOwnerDocumentActivityChanged();
+  bool pauseElement = HTMLMediaElement::NotifyOwnerDocumentActivityChangedInternal();
   UpdateScreenWakeLock();
+  return pauseElement;
 }
 
 already_AddRefed<VideoPlaybackQuality>
