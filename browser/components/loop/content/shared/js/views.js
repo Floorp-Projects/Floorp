e52d607e8e449aaec5bb480eee58e17086e34080
Mark Banner <standard8@mozilla.com>
2015-07-06 22:59:51 +0100

    Bug 1180671 - Rename 'l10n' to 'mozL10n' in Loop's views.jsx to be more consistent with uses elsewhere. r=mikedeboer

--- browser/components/loop/content/shared/js/views.js
+++ browser/components/loop/content/shared/js/views.js
@@ -4,7 +4,7 @@
 
 var loop = loop || {};
 loop.shared = loop.shared || {};
-loop.shared.views = (function(_, l10n) {
+loop.shared.views = (function(_, mozL10n) {
   "use strict";
 
   var sharedActions = loop.shared.actions;
@@ -62,7 +62,7 @@ loop.shared.views = (function(_, l10n) {
       var prefix = this.props.enabled ? "mute" : "unmute";
       var suffix = "button_title";
       var msgId = [prefix, this.props.scope, this.props.type, suffix].join("_");
-      return l10n.get(msgId);
+      return mozL10n.get(msgId);
     },
 
     render: function() {
@@ -130,7 +130,7 @@ loop.shared.views = (function(_, l10n) {
       var prefix = this.props.state === SCREEN_SHARE_STATES.ACTIVE ?
         "active" : "inactive";
 
-      return l10n.get(prefix + "_screenshare_button_title");
+      return mozL10n.get(prefix + "_screenshare_button_title");
     },
 
     render: function() {
@@ -169,10 +169,10 @@ loop.shared.views = (function(_, l10n) {
           ), 
           React.createElement("ul", {className: dropdownMenuClasses, ref: "menu"}, 
             React.createElement("li", {onClick: this._handleShareTabs}, 
-              l10n.get("share_tabs_button_title2")
+              mozL10n.get("share_tabs_button_title2")
             ), 
             React.createElement("li", {className: windowSharingClasses, onClick: this._handleShareWindows}, 
-              l10n.get("share_windows_button_title")
+              mozL10n.get("share_windows_button_title")
             )
           )
         )
@@ -226,7 +226,7 @@ loop.shared.views = (function(_, l10n) {
     },
 
     _getHangupButtonLabel: function() {
-      return this.props.hangupButtonLabel || l10n.get("hangup_button_caption2");
+      return this.props.hangupButtonLabel || mozL10n.get("hangup_button_caption2");
     },
 
     render: function() {
@@ -236,7 +236,7 @@ loop.shared.views = (function(_, l10n) {
             React.createElement("button", {className: "btn btn-hangup", 
                     disabled: !this.props.enableHangup, 
                     onClick: this.handleClickHangup, 
-                    title: l10n.get("hangup_button_title")}, 
+                    title: mozL10n.get("hangup_button_title")}, 
               this._getHangupButtonLabel()
             )
           ), 
@@ -261,7 +261,7 @@ loop.shared.views = (function(_, l10n) {
             React.createElement(MediaControlButton, {action: this.handleToggleEdit, 
                                 enabled: this.props.edit.enabled, 
                                 scope: "local", 
-                                title: l10n.get(this.props.edit.enabled ?
+                                title: mozL10n.get(this.props.edit.enabled ?
                                   "context_edit_tooltip" : "context_hide_tooltip"), 
                                 type: "edit", 
                                 visible: this.props.edit.visible})
@@ -794,7 +794,7 @@ loop.shared.views = (function(_, l10n) {
         return null;
       }
 
-      return React.createElement("p", null, l10n.get("context_inroom_label"));
+      return React.createElement("p", null, mozL10n.get("context_inroom_label"));
     },
 
     render: function() {
