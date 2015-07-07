de60595a08335a86f45a2316205a5d1d452c58a6
Ryan VanderMeulen <ryanvm@gmail.com>
2015-07-07 12:01:56 -0400

    Backed out changeset 57f4bd1baf13 (bug 1157941) for OSX 10.10 e10s browser_bug427559.js failures. CLOSED TREE

--- view/nsView.cpp
+++ view/nsView.cpp
@@ -90,10 +90,6 @@ nsView::~nsView()
     mParent->RemoveChild(this);
   }
 
-  if (mPreviousWindow) {
-    mPreviousWindow->SetPreviouslyAttachedWidgetListener(nullptr);
-  }
-
   // Destroy and release the widget
   DestroyWidget();
 
@@ -726,18 +722,6 @@ nsresult nsView::DetachFromTopLevelWidget()
   NS_PRECONDITION(mWindow, "null mWindow for DetachFromTopLevelWidget!");
 
   mWindow->SetAttachedWidgetListener(nullptr);
-  nsIWidgetListener* listener = mWindow->GetPreviouslyAttachedWidgetListener();
-
-  if (listener && listener->GetView()) {
-    // Ensure the listener doesn't think it's being used anymore
-    listener->GetView()->SetPreviousWidget(nullptr);
-  }
-
-  // If the new view's frame is paint suppressed then the window
-  // will want to use us instead until that's done
-  mWindow->SetPreviouslyAttachedWidgetListener(this);
-
-  mPreviousWindow = mWindow;
   mWindow = nullptr;
 
   mWidgetIsTopLevel = false;
@@ -1111,10 +1095,4 @@ nsView::HandleEvent(WidgetGUIEvent* aEvent,
   }
 
   return result;
-}
-
-bool
-nsView::IsPrimaryFramePaintSuppressed()
-{
-  return mFrame ? mFrame->PresContext()->PresShell()->IsPaintingSuppressed() : false;
 }
