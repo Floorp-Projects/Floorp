ae002bebfd29aa39875354ce1d05b4d89b90fd29
Gabor Krizsanits <gkrizsanits@mozilla.com>
2015-07-07 10:56:26 +0200

    Bug 1173451 - RemoteAddonsChild init should be optional. r=mconley

--- toolkit/content/browser-child.js
+++ toolkit/content/browser-child.js
@@ -490,9 +490,11 @@ addMessageListener("Browser:Thumbnail:CheckState", function (aMessage) {
 // The AddonsChild needs to be rooted so that it stays alive as long as
 // the tab.
 let AddonsChild = RemoteAddonsChild.init(this);
-addEventListener("unload", () => {
-  RemoteAddonsChild.uninit(AddonsChild);
-});
+if (AddonsChild) {
+  addEventListener("unload", () => {
+    RemoteAddonsChild.uninit(AddonsChild);
+  });
+}
 
 addMessageListener("NetworkPrioritizer:AdjustPriority", (msg) => {
   let webNav = docShell.QueryInterface(Ci.nsIWebNavigation);
