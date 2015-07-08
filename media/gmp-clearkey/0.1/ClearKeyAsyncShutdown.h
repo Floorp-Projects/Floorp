c4bd787f578730a87115b390a998520dede93ced
Chris Pearce <cpearce@mozilla.com>
2015-07-08 17:12:47 +1200

    Bug 1181436 - Use 'gmp-api/' prefix consistently in includes in gmp-clearkey. r=gerald

--- media/gmp-clearkey/0.1/ClearKeyAsyncShutdown.h
+++ media/gmp-clearkey/0.1/ClearKeyAsyncShutdown.h
@@ -17,7 +17,7 @@
 #ifndef __ClearKeyAsyncShutdown_h__
 #define __ClearKeyAsyncShutdown_h__
 
-#include "gmp-async-shutdown.h"
+#include "gmp-api/gmp-async-shutdown.h"
 #include "RefCounted.h"
 
 class ClearKeyAsyncShutdown : public GMPAsyncShutdown
