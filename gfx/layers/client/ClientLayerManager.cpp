2f7c3135adfc0e0892700c05e6fa9ddc27dd1ab4
Botond Ballo <botond@mozilla.com>
2015-07-08 19:18:28 -0400

    Bug 1177018 - Only enable APZ test logging for tests that actually use it. r=kats
    This avoids excessive amounts of test data accumulating which can slow tests down.
    
    As part of this change, the pref for enabling the logging was made 'Live'.
    
    --HG--
    extra : rebase_source : e1d0547e5d3125e9db44bb47ddfff3b4f8d50e99
    extra : source : 73fb13743a74a5a920d099fda70d9b1a1fa3a6ed

--- gfx/layers/client/ClientLayerManager.cpp
+++ gfx/layers/client/ClientLayerManager.cpp
@@ -231,9 +231,14 @@ ClientLayerManager::BeginTransactionWithTarget(gfxContext* aTarget)
   }
 
   // If this is a new paint, increment the paint sequence number.
-  if (!mIsRepeatTransaction && gfxPrefs::APZTestLoggingEnabled()) {
+  if (!mIsRepeatTransaction) {
+    // Increment the paint sequence number even if test logging isn't
+    // enabled in this process; it may be enabled in the parent process,
+    // and the parent process expects unique sequence numbers.
     ++mPaintSequenceNumber;
-    mApzTestData.StartNewPaint(mPaintSequenceNumber);
+    if (gfxPrefs::APZTestLoggingEnabled()) {
+      mApzTestData.StartNewPaint(mPaintSequenceNumber);
+    }
   }
 }
 
