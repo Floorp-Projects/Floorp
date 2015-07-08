fa668424568060e5b795ba0e4e792fcdb9abbae7
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-07-08 09:44:16 +0200

    Backed out changeset 42e80a07acd6 (bug 1177563) for bustage on a CLOSED TREE

--- layout/style/nsCSSRuleProcessor.h
+++ layout/style/nsCSSRuleProcessor.h
@@ -183,7 +183,6 @@ public:
     mInRuleProcessorCache = aVal;
   }
   bool IsInRuleProcessorCache() const { return mInRuleProcessorCache; }
-  bool IsUsedByMultipleStyleSets() const { return mStyleSetRefCnt > 1; }
 
 #ifdef XP_WIN
   // Cached theme identifier for the moz-windows-theme media query.
