767797824863776b5005cc21a8aced279bc5ed6e
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2015-07-09 01:12:49 -0400

    Bug 1113038 - Move test_openSearchPanel from browser_UITour3.js to its own file (browser_openSearchPanel.js).
    --HG--
    rename : browser/components/uitour/test/browser_UITour3.js => browser/components/uitour/test/browser_openSearchPanel.js
    extra : commitid : 9weLDhi42m0
    extra : rebase_source : 8255480f584c48c7ed847f69f4dbb3288261287f

--- browser/components/uitour/test/browser_UITour3.js
+++ browser/components/uitour/test/browser_UITour3.js
@@ -176,24 +176,4 @@ let tests = [
     // only happens after a tick.
     waitForCondition(() => searchbar.value == "", done, "Search term cleared");
   },
-
-  function test_openSearchPanel(done) {
-    let searchbar = document.getElementById("searchbar");
-
-    // If suggestions are enabled, the panel will attempt to use the network to connect
-    // to the suggestions provider, causing the test suite to fail.
-    Services.prefs.setBoolPref("browser.search.suggest.enabled", false);
-    registerCleanupFunction(() => {
-      Services.prefs.clearUserPref("browser.search.suggest.enabled");
-    });
-
-    ok(!searchbar.textbox.open, "Popup starts as closed");
-    gContentAPI.openSearchPanel(() => {
-      ok(searchbar.textbox.open, "Popup was opened");
-      searchbar.textbox.closePopup();
-      ok(!searchbar.textbox.open, "Popup was closed");
-      done();
-    });
-  },
-
 ];
