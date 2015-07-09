c106bbaf51fa7a10141e528b8982e126779fe0c5
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2015-07-09 14:47:34 -0700

    Bug 1168979 part 5 - Add allowed_build_tasks to distinguish builds that require symbol uploading; r=mrrrgn

--- testing/taskcluster/taskcluster_graph/commit_parser.py
+++ testing/taskcluster/taskcluster_graph/commit_parser.py
@@ -259,10 +259,18 @@ def parse_commit(message, jobs):
             else:
                 additional_parameters = {}
 
+            # Generate list of post build tasks that run on this build
+            post_build_jobs = []
+            for job_flag in jobs['flags']['post-build']:
+                job = jobs['post-build'][job_flag]
+                if 'allowed_build_tasks' in job and build_task not in job['allowed_build_tasks']:
+                    continue
+                post_build_jobs.append(copy.deepcopy(job))
+
             # Node for this particular build type
             result.append({
                 'task': build_task,
-                'post-build': jobs['post-build']['upload-symbols'],
+                'post-build': post_build_jobs,
                 'dependents': extract_tests_from_platform(
                     jobs['tests'], platform_builds, build_task, tests
                 ),
