bae1e652bf12203097a8ad9e898b604b1d62b1f8
Paul Adenot <paul@paul.cx>
2015-07-09 16:40:08 +0200

    Bug 1156472 - Part 1 - Allow to capture all HTMLMediaElements and AudioContexts for a document. r=baku,padenot
    This is built on top of the AudioChannel infrastructure. This patch does not
    actually implement the capture, it just does the plumbing to be able to notify
    all HTMLMediaElement/AudioContext for a document.

--- dom/audiochannel/AudioChannelAgent.cpp
+++ dom/audiochannel/AudioChannelAgent.cpp
@@ -35,6 +35,7 @@ NS_IMPL_CYCLE_COLLECTING_RELEASE(AudioChannelAgent)
 
 AudioChannelAgent::AudioChannelAgent()
   : mAudioChannelType(AUDIO_AGENT_CHANNEL_ERROR)
+  , mInnerWindowID(0)
   , mIsRegToService(false)
 {
 }
@@ -104,6 +105,10 @@ AudioChannelAgent::InitInternal(nsIDOMWindow* aWindow, int32_t aChannelType,
   }
 
   if (aWindow) {
+    nsCOMPtr<nsPIDOMWindow> pInnerWindow = do_QueryInterface(aWindow);
+    MOZ_ASSERT(pInnerWindow->IsInnerWindow());
+    mInnerWindowID = pInnerWindow->WindowID();
+
     nsCOMPtr<nsIDOMWindow> topWindow;
     aWindow->GetScriptableTop(getter_AddRefs(topWindow));
     mWindow = do_QueryInterface(topWindow);
@@ -190,4 +195,19 @@ uint64_t
 AudioChannelAgent::WindowID() const
 {
   return mWindow ? mWindow->WindowID() : 0;
+}
+
+void
+AudioChannelAgent::WindowAudioCaptureChanged(uint64_t aInnerWindowID)
+{
+  if (aInnerWindowID != mInnerWindowID) {
+    return;
+  }
+
+  nsCOMPtr<nsIAudioChannelAgentCallback> callback = GetCallback();
+  if (!callback) {
+    return;
+  }
+
+  callback->WindowAudioCaptureChanged();
 }
