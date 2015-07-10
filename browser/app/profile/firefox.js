71f41e4e0b5278aeef652da7407c136a785d4700
Florian Quèze <florian@queze.net>
2015-07-10 23:58:40 +0200

    Bug 1175218 - disable region defaults by making the geoSpecificDefaults.url pref empty to fix test bustage, a=KWierso on CLOSED TREE

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -408,7 +408,7 @@ pref("browser.search.order.3",                "chrome://browser-region/locale/re
 // This is disabled globally, and then enabled for individual locales
 // in firefox-l10n.js (eg. it's enabled for en-US).
 pref("browser.search.geoSpecificDefaults", false);
-pref("browser.search.geoSpecificDefaults.url", "https://search.services.mozilla.com/1/%APP%/%VERSION%/%CHANNEL%/%LOCALE%/%REGION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%");
+pref("browser.search.geoSpecificDefaults.url", "");
 
 // US specific default (used as a fallback if the geoSpecificDefaults request fails).
 pref("browser.search.defaultenginename.US",      "data:text/plain,browser.search.defaultenginename.US=Yahoo");
