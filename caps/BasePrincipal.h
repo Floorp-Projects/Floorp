c1edd3f94a5577afa8041d20ab4defb1d8e46972
Wes Kocher <wkocher@mozilla.com>
2015-07-09 17:43:08 -0700

    Backed out 5 changesets (bug 1180921) for build failures in BasePrincipal.cpp
    Backed out changeset d8c1a2e11a9a (bug 1180921)
    Backed out changeset f4dd8c53df5f (bug 1180921)
    Backed out changeset b272a0ebf5d8 (bug 1180921)
    Backed out changeset 8e86b6a7d201 (bug 1180921)
    Backed out changeset bbdebd7b8881 (bug 1180921)

--- caps/BasePrincipal.h
+++ caps/BasePrincipal.h
@@ -34,8 +34,7 @@ public:
   bool operator==(const OriginAttributes& aOther) const
   {
     return mAppId == aOther.mAppId &&
-           mInBrowser == aOther.mInBrowser &&
-           mAddonId == aOther.mAddonId;
+           mInBrowser == aOther.mInBrowser;
   }
   bool operator!=(const OriginAttributes& aOther) const
   {
@@ -106,10 +105,6 @@ protected:
 
   virtual nsresult GetOriginInternal(nsACString& aOrigin) = 0;
   virtual bool SubsumesInternal(nsIPrincipal* aOther, DocumentDomainConsideration aConsider) = 0;
-
-  // Helper to check whether this principal is associated with an addon that
-  // allows unprivileged code to load aURI.
-  bool AddonAllowsLoad(nsIURI* aURI);
 
   nsCOMPtr<nsIContentSecurityPolicy> mCSP;
   OriginAttributes mOriginAttributes;
