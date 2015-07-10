6b06abed938f207029d44da83400f7b44ddd8c51
Jan-Ivar Bruaroey <jib@mozilla.com>
2015-07-09 23:07:42 -0400

    Bug 1181428 - Move code to reuse test for active gum or persistent permission. r=mt

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -2631,8 +2631,10 @@ MediaManager::StopMediaStreams()
 }
 
 bool
-MediaManager::IsWindowActivelyCapturing(uint64_t aWindowId)
+MediaManager::IsActivelyCapturingOrHasAPermission(uint64_t aWindowId)
 {
+  // Does page currently have a gUM stream active?
+
   nsCOMPtr<nsISupportsArray> array;
   GetActiveMediaCaptureWindows(getter_AddRefs(array));
   uint32_t len;
@@ -2645,7 +2647,37 @@ MediaManager::IsWindowActivelyCapturing(uint64_t aWindowId)
       return true;
     }
   }
-  return false;
+
+  // Or are persistent permissions (audio or video) granted?
+
+  nsPIDOMWindow *window = static_cast<nsPIDOMWindow*>
+      (nsGlobalWindow::GetInnerWindowWithId(aWindowId));
+  if (NS_WARN_IF(!window)) {
+    return false;
+  }
+  // Check if this site has persistent permissions.
+  nsresult rv;
+  nsCOMPtr<nsIPermissionManager> mgr =
+    do_GetService(NS_PERMISSIONMANAGER_CONTRACTID, &rv);
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return false; // no permission manager no permissions!
+  }
+
+  uint32_t audio = nsIPermissionManager::UNKNOWN_ACTION;
+  uint32_t video = nsIPermissionManager::UNKNOWN_ACTION;
+  {
+    auto* principal = window->GetExtantDoc()->NodePrincipal();
+    rv = mgr->TestExactPermissionFromPrincipal(principal, "microphone", &audio);
+    if (NS_WARN_IF(NS_FAILED(rv))) {
+      return false;
+    }
+    rv = mgr->TestExactPermissionFromPrincipal(principal, "camera", &video);
+    if (NS_WARN_IF(NS_FAILED(rv))) {
+      return false;
+    }
+  }
+  return audio == nsIPermissionManager::ALLOW_ACTION ||
+         video == nsIPermissionManager::ALLOW_ACTION;
 }
 
 void
