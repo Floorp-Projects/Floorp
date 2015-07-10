9c5a735d3ea59b3ae085329960dfda75e8e1905d
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-07-10 09:27:24 +0200

    Backed out changeset f4d81e9a81e0 (bug 1181375)

--- gfx/src/DriverInitCrashDetection.cpp
+++ gfx/src/DriverInitCrashDetection.cpp
@@ -6,7 +6,6 @@
 #include "gfxPrefs.h"
 #include "nsAppDirectoryServiceDefs.h"
 #include "nsDirectoryServiceUtils.h"
-#include "nsExceptionHandler.h"
 #include "nsServiceManagerUtils.h"
 #include "nsString.h"
 #include "nsXULAppAPI.h"
@@ -71,10 +70,6 @@ DriverInitCrashDetection::~DriverInitCrashDetection()
     // If we attempted to initialize the driver, and got this far without
     // crashing, assume everything is okay.
     gfxPrefs::SetDriverInitStatus(int32_t(DriverInitStatus::Okay));
-
-    // Remove the crash report annotation.
-    CrashReporter::AnnotateCrashReport(NS_LITERAL_CSTRING("GraphicsStartupTest"),
-                                       NS_LITERAL_CSTRING(""));
   }
 }
 
@@ -109,9 +104,6 @@ DriverInitCrashDetection::AllowDriverInitAttempt()
   // If we crash, we'll just lose this. Not a big deal, next startup we'll
   // record the failure.
   RecordTelemetry(TelemetryState::EnvironmentChanged);
-
-  CrashReporter::AnnotateCrashReport(NS_LITERAL_CSTRING("GraphicsStartupTest"),
-                                     NS_LITERAL_CSTRING("1"));
 }
 
 bool
