3ebbb75cf487dbc1808fb0a02e59b6c51b476ae6
Kyle Machulis <kyle@nonpolynomial.com>
2015-07-10 12:12:35 -0700

    Bug 1165981 - Mochitests and test plugin for Win64 plugin disabling. r=bsmedberg, a=ritu

---
+++ dom/plugins/test/testplugin/flashplugin/nptest_name.cpp
@@ -0,0 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const char *sPluginName = "Flash Test Plug-in";
+const char *sPluginDescription = "Flash plug-in for testing purposes.";
+const char *sMimeDescription = "application/x-shockwave-flash-test:swf:Flash test type";
