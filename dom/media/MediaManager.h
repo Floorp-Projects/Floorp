f52fa2ded42a01389023c57768a37ad931b4994e
Jan-Ivar Bruaroey <jib@mozilla.com>
2015-07-09 23:07:42 -0400

    Bug 1181428 - Move code to reuse test for active gum or persistent permission. r=mt

--- dom/media/MediaManager.h
+++ dom/media/MediaManager.h
@@ -579,7 +579,7 @@ public:
 
   nsresult EnumerateDevices(nsPIDOMWindow* aWindow, dom::Promise& aPromise);
   void OnNavigation(uint64_t aWindowID);
-  bool IsWindowActivelyCapturing(uint64_t aWindowId);
+  bool IsActivelyCapturingOrHasAPermission(uint64_t aWindowId);
 
   MediaEnginePrefs mPrefs;
 
