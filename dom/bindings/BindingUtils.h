187a8debbd84fa3dd991e1852d7c9b406b6f4f5a
Boris Zbarsky <bzbarsky@mit.edu>
2015-07-09 22:51:28 -0400

    Bug 1180770 part 1.  Remove the unused ThrowNotEnoughArgsError.  r=peterv

--- dom/bindings/BindingUtils.h
+++ dom/bindings/BindingUtils.h
@@ -109,10 +109,6 @@ ThrowMethodFailedWithDetails(JSContext* cx, ErrorResult& rv,
     rv.ReportJSException(cx);
     return false;
   }
-  if (rv.IsNotEnoughArgsError()) {
-    rv.ReportNotEnoughArgsError(cx, ifaceName, memberName);
-    return false;
-  }
   rv.ReportGenericError(cx);
   return false;
 }
