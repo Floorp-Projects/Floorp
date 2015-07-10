8fc0b9874073b6ede5bc415f7df9ca7bb1fe4c3b
Kyle Machulis <kyle@nonpolynomial.com>
2015-07-10 12:12:35 -0700

    Bug 1165981 - Mochitests and test plugin for Win64 plugin disabling. r=bsmedberg, a=ritu
    --HG--
    extra : transplant_source : %C8g%25%82%7EH%1DN%D1%95%1ELU_%C0Q%E6%CEv%A5

---
+++ dom/plugins/test/testplugin/thirdplugin/nptest_name.cpp
@@ -0,0 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const char *sPluginName = "Third Test Plug-in";
+const char *sPluginDescription = "Third plug-in for testing purposes.";
+const char *sMimeDescription = "application/x-Third-Test:ts3:Third test type";
