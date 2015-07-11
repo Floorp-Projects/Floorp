7bba858760b7b77eed4b9265b557956a7308ba83
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-07-11 14:14:58 +0200

    Backed out changeset bcfbdb934c37 (bug 1113086) for breaking cpp tests with timeouts in  TestAudioChannelService.exe

--- dom/speakermanager/SpeakerManagerServiceChild.cpp
+++ dom/speakermanager/SpeakerManagerServiceChild.cpp
@@ -96,7 +96,7 @@ SpeakerManagerServiceChild::SetAudioChannelActive(bool aIsActive)
 SpeakerManagerServiceChild::SpeakerManagerServiceChild()
 {
   MOZ_ASSERT(NS_IsMainThread());
-  nsRefPtr<AudioChannelService> audioChannelService = AudioChannelService::GetOrCreate();
+  AudioChannelService* audioChannelService = AudioChannelService::GetOrCreateAudioChannelService();
   if (audioChannelService) {
     audioChannelService->RegisterSpeakerManager(this);
   }
@@ -105,7 +105,7 @@ SpeakerManagerServiceChild::SpeakerManagerServiceChild()
 
 SpeakerManagerServiceChild::~SpeakerManagerServiceChild()
 {
-  nsRefPtr<AudioChannelService> audioChannelService = AudioChannelService::GetOrCreate();
+  AudioChannelService* audioChannelService = AudioChannelService::GetOrCreateAudioChannelService();
   if (audioChannelService) {
     audioChannelService->UnregisterSpeakerManager(this);
   }
