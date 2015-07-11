4c3f246f15931400980d55c652f8e677dbda686d
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-07-11 14:13:41 +0200

    Backed out changeset 200c185e4d71 (bug 1113086)

--- dom/html/nsBrowserElement.cpp
+++ dom/html/nsBrowserElement.cpp
@@ -520,21 +520,14 @@ nsBrowserElement::GetAllowedAudioChannels(
 {
   aAudioChannels.Clear();
 
+  NS_ENSURE_TRUE_VOID(IsBrowserElementOrThrow(aRv));
+  NS_ENSURE_TRUE_VOID(IsNotWidgetOrThrow(aRv));
+
   // If empty, it means that this is the first call of this method.
   if (mBrowserElementAudioChannels.IsEmpty()) {
     nsCOMPtr<nsIFrameLoader> frameLoader = GetFrameLoader();
     if (!frameLoader) {
       aRv.Throw(NS_ERROR_FAILURE);
-      return;
-    }
-
-    bool isBrowserOrApp;
-    aRv = frameLoader->GetOwnerIsBrowserOrAppFrame(&isBrowserOrApp);
-    if (NS_WARN_IF(aRv.Failed())) {
-      return;
-    }
-
-    if (!isBrowserOrApp) {
       return;
     }
 
@@ -594,6 +587,14 @@ nsBrowserElement::GetAllowedAudioChannels(
       return;
     }
 
+    bool noapp = false;
+    Preferences::GetBool("dom.testing.browserElementAudioChannel.noapp", &noapp);
+
+    if (!noapp && !app) {
+      aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
+      return;
+    }
+
     // Normal is always allowed.
     nsTArray<nsRefPtr<BrowserElementAudioChannel>> channels;
 
@@ -608,6 +609,7 @@ nsBrowserElement::GetAllowedAudioChannels(
 
     channels.AppendElement(ac);
 
+    // app can be null in case we are in a test.
     if (app) {
       const nsAttrValue::EnumTable* audioChannelTable =
         AudioChannelService::GetAudioChannelTable();
