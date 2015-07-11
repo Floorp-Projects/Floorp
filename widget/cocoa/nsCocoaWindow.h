09d4d46e81d2803739394fc54a96557eba5ee398
Xidorn Quan <quanxunzhen@gmail.com>
2015-07-11 10:08:59 +1000

    Bug 1160014 part 1 - Implement common part of fullscreen transition. r=roc,smaug,dao
    This patch implements the code which is shared by all platforms for
    fullscreen transition.
    
    It adds two prefs for the duration of fullscreen transition. They can
    also be used to completely suppress the transition.
    
    In addition, this patch uses the newly added prefs to suppress the
    transition in all tests which use the DOM fullscreen.
    
    --HG--
    extra : source : a64f71ea9ceb37acbbfc4e4023880fb244daa738

--- widget/cocoa/nsCocoaWindow.h
+++ widget/cocoa/nsCocoaWindow.h
@@ -285,7 +285,7 @@ public:
                                         nsIWidget *aWidget, bool aActivate) override;
     NS_IMETHOD              SetSizeMode(int32_t aMode) override;
     NS_IMETHOD              HideWindowChrome(bool aShouldHide) override;
-    virtual void            PrepareForDOMFullscreenTransition() override;
+    virtual bool PrepareForFullscreenTransition(nsISupports** aData) override;
     void EnteredFullScreen(bool aFullScreen, bool aNativeMode = true);
     inline bool ShouldToggleNativeFullscreen(bool aFullScreen);
     NS_IMETHOD              MakeFullScreen(bool aFullScreen, nsIScreen* aTargetScreen = nullptr) override;
