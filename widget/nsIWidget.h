972868341ba104b86418020bb2e7ea9659a30753
Nicolas Silva <nsilva@mozilla.com>
2015-07-13 16:46:48 +0200

    Bug 1180688 - Detect whether the widget will be able to present frames with BasicCompositor on Mac. r=mstange, a=lmandel

--- widget/nsIWidget.h
+++ widget/nsIWidget.h
@@ -49,6 +49,7 @@ class PluginWidgetChild;
 }
 namespace layers {
 class Composer2D;
+class Compositor;
 class CompositorChild;
 class LayerManager;
 class LayerManagerComposite;
@@ -1716,6 +1717,16 @@ class nsIWidget : public nsISupports {
     virtual void EndRemoteDrawingInRegion(mozilla::gfx::DrawTarget* aDrawTarget, nsIntRegion& aInvalidRegion) {
       EndRemoteDrawing();
     }
+
+    /**
+     * A hook for the widget to prepare a Compositor, during the latter's initialization.
+     *
+     * If this method returns true, it means that the widget will be able to
+     * present frames from the compoositor.
+     * Returning false will cause the compositor's initialization to fail, and
+     * a different compositor backend will be used (if any).
+     */
+    virtual bool InitCompositor(mozilla::layers::Compositor*) { return true; }
 
     /**
      * Clean up any resources used by Start/EndRemoteDrawing.
