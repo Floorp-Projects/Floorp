3c7e0050c5a70da7f4500457aa5a31c94a0997c8
Ryan VanderMeulen <ryanvm@gmail.com>
2015-07-13 14:32:29 -0400

    Backed out changeset 0e127c7f69c4 (bug 1160166) for e10s LSAN leaks. CLOSED TREE

--- dom/plugins/base/nsPluginHost.cpp
+++ dom/plugins/base/nsPluginHost.cpp
@@ -2511,13 +2511,12 @@ nsPluginHost::FindPluginsForContent(uint32_t aPluginEpoch,
     return;
   }
 
-  uint32_t count;
-  nsIPluginTag** plugins;
-  GetPluginTags(&count, &plugins);
+  nsTArray<nsRefPtr<nsPluginTag>> plugins;
+  GetPlugins(plugins);
 
-  for (size_t i = 0; i < count; i++) {
-    nsRefPtr<nsPluginTag> tag = static_cast<nsPluginTag*>(plugins[i]);
-    NS_RELEASE(plugins[i]);
+  for (size_t i = 0; i < plugins.Length(); i++) {
+    nsRefPtr<nsPluginTag> tag = plugins[i];
+
     if (!nsNPAPIPlugin::RunPluginOOP(tag)) {
       // Don't expose non-OOP plugins to content processes since we have no way
       // to bridge them over.
