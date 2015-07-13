b9b8836fad287a4ef35bb98590f38655159f6f02
Peter Elmers <peter.elmers@yahoo.com>
2015-07-13 13:09:11 -0700

    Bug 1183291 - Factor idl-parser into a Python package. r=khuey
    Move Python code into an xpidl subdirectory, and include a setup.py to allow
    inclusion from pip install or requirements files. Change build directory
    variables appropriately.

--- xpcom/idl-parser/xpidl/header.py
+++ xpcom/idl-parser/xpidl/header.py
@@ -501,7 +501,8 @@ def write_interface(iface, fd):
 
     fd.write(iface_template_epilog)
 
-if __name__ == '__main__':
+
+def main():
     from optparse import OptionParser
     o = OptionParser()
     o.add_option('-I', action='append', dest='incdirs', default=['.'],
@@ -569,3 +570,6 @@ if __name__ == '__main__':
         print >>depfd, "%s: %s" % (options.outfile, " ".join(deps))
         for dep in deps:
             print >>depfd, "%s:" % dep
+
+if __name__ == '__main__':
+    main()
