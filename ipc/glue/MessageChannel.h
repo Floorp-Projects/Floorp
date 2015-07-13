7f4cb0ea80ddc6b42a84bbe599735cc5faa43aa8
Bill McCloskey <billm@mozilla.com>
2015-07-13 14:34:42 -0700

    Bug 1177013 - Bug fixes for CPOW cancelation. r=dvander, a=ritu

--- ipc/glue/MessageChannel.h
+++ ipc/glue/MessageChannel.h
@@ -554,7 +554,7 @@ class MessageChannel : HasResultCodes
      public:
        explicit AutoEnterTransaction(MessageChannel *aChan, int32_t aMsgSeqno)
         : mChan(aChan),
-          mNewTransaction(0),
+          mNewTransaction(INT32_MAX),
           mOldTransaction(mChan->mCurrentTransaction)
        {
            mChan->mMonitor->AssertCurrentThreadOwns();
