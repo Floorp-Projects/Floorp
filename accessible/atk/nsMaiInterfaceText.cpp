fe4723d489cf4ea4dbdb8632ff1a1ae2ec5cc717
Wes Kocher <wkocher@mozilla.com>
2015-07-14 14:35:53 -0700

    Backed out changeset 5a310370cec1 (bug 1173876)

--- accessible/atk/nsMaiInterfaceText.cpp
+++ accessible/atk/nsMaiInterfaceText.cpp
@@ -163,24 +163,22 @@ getTextAfterOffsetCB(AtkText *aText, gint aOffset,
                      AtkTextBoundary aBoundaryType,
                      gint *aStartOffset, gint *aEndOffset)
 {
-    nsAutoString autoStr;
-  int32_t startOffset = 0, endOffset = 0;
   AccessibleWrap* accWrap = GetAccessibleWrap(ATK_OBJECT(aText));
-  if (accWrap) {
-    HyperTextAccessible* text = accWrap->AsHyperText();
-    if (!text || !text->IsTextRole())
-      return nullptr;
+  if (!accWrap)
+    return nullptr;
 
-    text->TextAfterOffset(aOffset, aBoundaryType, &startOffset, &endOffset, autoStr);
-    ConvertTexttoAsterisks(accWrap, autoStr);
-  } else if (ProxyAccessible* proxy = GetProxy(ATK_OBJECT(aText))) {
-    proxy->GetTextAfterOffset(aOffset, aBoundaryType, autoStr, &startOffset,
-                              &endOffset);
-  }
+  HyperTextAccessible* text = accWrap->AsHyperText();
+  if (!text || !text->IsTextRole())
+    return nullptr;
+
+  nsAutoString autoStr;
+  int32_t startOffset = 0, endOffset = 0;
+  text->TextAfterOffset(aOffset, aBoundaryType, &startOffset, &endOffset, autoStr);
 
   *aStartOffset = startOffset;
   *aEndOffset = endOffset;
 
+  ConvertTexttoAsterisks(accWrap, autoStr);
   NS_ConvertUTF16toUTF8 cautoStr(autoStr);
   return (cautoStr.get()) ? g_strdup(cautoStr.get()) : nullptr;
 }
@@ -190,26 +188,23 @@ getTextAtOffsetCB(AtkText *aText, gint aOffset,
                   AtkTextBoundary aBoundaryType,
                   gint *aStartOffset, gint *aEndOffset)
 {
-  nsAutoString autoStr;
-  int32_t startOffset = 0, endOffset = 0;
   AccessibleWrap* accWrap = GetAccessibleWrap(ATK_OBJECT(aText));
-  if (accWrap) {
-    HyperTextAccessible* text = accWrap->AsHyperText();
-    if (!text || !text->IsTextRole())
-      return nullptr;
+  if (!accWrap)
+    return nullptr;
 
+  HyperTextAccessible* text = accWrap->AsHyperText();
+  if (!text || !text->IsTextRole())
+    return nullptr;
+
+    nsAutoString autoStr;
+    int32_t startOffset = 0, endOffset = 0;
     text->TextAtOffset(aOffset, aBoundaryType, &startOffset, &endOffset, autoStr);
+    *aStartOffset = startOffset;
+    *aEndOffset = endOffset;
+
     ConvertTexttoAsterisks(accWrap, autoStr);
-  } else if (ProxyAccessible* proxy = GetProxy(ATK_OBJECT(aText))) {
-    proxy->GetTextAtOffset(aOffset, aBoundaryType, autoStr, &startOffset,
-                           &endOffset);
-  }
-
-  *aStartOffset = startOffset;
-  *aEndOffset = endOffset;
-
-  NS_ConvertUTF16toUTF8 cautoStr(autoStr);
-  return (cautoStr.get()) ? g_strdup(cautoStr.get()) : nullptr;
+    NS_ConvertUTF16toUTF8 cautoStr(autoStr);
+    return (cautoStr.get()) ? g_strdup(cautoStr.get()) : nullptr;
 }
 
 static gunichar
@@ -238,25 +233,22 @@ getTextBeforeOffsetCB(AtkText *aText, gint aOffset,
                       AtkTextBoundary aBoundaryType,
                       gint *aStartOffset, gint *aEndOffset)
 {
+  AccessibleWrap* accWrap = GetAccessibleWrap(ATK_OBJECT(aText));
+  if (!accWrap)
+    return nullptr;
+
+  HyperTextAccessible* text = accWrap->AsHyperText();
+  if (!text || !text->IsTextRole())
+    return nullptr;
+
   nsAutoString autoStr;
   int32_t startOffset = 0, endOffset = 0;
-  AccessibleWrap* accWrap = GetAccessibleWrap(ATK_OBJECT(aText));
-  if (accWrap) {
-    HyperTextAccessible* text = accWrap->AsHyperText();
-    if (!text || !text->IsTextRole())
-      return nullptr;
-
-    text->TextBeforeOffset(aOffset, aBoundaryType,
-                           &startOffset, &endOffset, autoStr);
-    ConvertTexttoAsterisks(accWrap, autoStr);
-  } else if (ProxyAccessible* proxy = GetProxy(ATK_OBJECT(aText))) {
-    proxy->GetTextBeforeOffset(aOffset, aBoundaryType, autoStr, &startOffset,
-                               &endOffset);
-  }
-
+  text->TextBeforeOffset(aOffset, aBoundaryType,
+                         &startOffset, &endOffset, autoStr);
   *aStartOffset = startOffset;
   *aEndOffset = endOffset;
 
+  ConvertTexttoAsterisks(accWrap, autoStr);
   NS_ConvertUTF16toUTF8 cautoStr(autoStr);
   return (cautoStr.get()) ? g_strdup(cautoStr.get()) : nullptr;
 }
