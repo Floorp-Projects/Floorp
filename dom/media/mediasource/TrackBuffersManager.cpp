dff0d1173e84959318070efd90db5c90f4c926f0
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-07-14 20:51:49 +1000

    Bug 1183573: P1. Properly shutdown demuxers. r=gerald

--- dom/media/mediasource/TrackBuffersManager.cpp
+++ dom/media/mediasource/TrackBuffersManager.cpp
@@ -109,6 +109,7 @@ TrackBuffersManager::TrackBuffersManager(dom::SourceBuffer* aParent, MediaSource
 
 TrackBuffersManager::~TrackBuffersManager()
 {
+  ShutdownDemuxers();
 }
 
 bool
@@ -733,7 +734,7 @@ TrackBuffersManager::ScheduleSegmentParserLoop()
 }
 
 void
-TrackBuffersManager::CreateDemuxerforMIMEType()
+TrackBuffersManager::ShutdownDemuxers()
 {
   if (mVideoTracks.mDemuxer) {
     mVideoTracks.mDemuxer->BreakCycles();
@@ -744,6 +745,13 @@ TrackBuffersManager::CreateDemuxerforMIMEType()
     mAudioTracks.mDemuxer = nullptr;
   }
   mInputDemuxer = nullptr;
+}
+
+void
+TrackBuffersManager::CreateDemuxerforMIMEType()
+{
+  ShutdownDemuxers();
+
   if (mType.LowerCaseEqualsLiteral("video/webm") || mType.LowerCaseEqualsLiteral("audio/webm")) {
     NS_WARNING("Waiting on WebMDemuxer");
   // mInputDemuxer = new WebMDemuxer(mCurrentInputBuffer);
