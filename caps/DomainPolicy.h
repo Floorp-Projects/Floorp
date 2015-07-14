a606a39234ebbf4bfa8ad360bb52b18ed8347b90
Andrew McCreight <continuation@gmail.com>
2015-07-14 07:59:00 -0400

    Bug 1182966 - Part 3: Add some final annotations to DomainSet. r=mrbkap

--- caps/DomainPolicy.h
+++ caps/DomainPolicy.h
@@ -33,7 +33,7 @@ enum DomainSetType{
     SUPER_WHITELIST
 };
 
-class DomainSet : public nsIDomainSet
+class DomainSet final : public nsIDomainSet
 {
 public:
     NS_DECL_ISUPPORTS
@@ -51,7 +51,7 @@ protected:
     DomainSetType mType;
 };
 
-class DomainPolicy : public nsIDomainPolicy
+class DomainPolicy final : public nsIDomainPolicy
 {
 public:
     NS_DECL_ISUPPORTS
