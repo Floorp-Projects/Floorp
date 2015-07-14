224df9df8e00b42ca0e51449dc08fb2830aea901
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2015-07-14 18:06:45 +0100

    Bug 1181560 - ensure previous menus get closed when opening new ones, r=Enn, a=sylvestre
    --HG--
    extra : source : 58a514b9e26e5af579ce4544544bb949a62cda35

--- layout/xul/nsMenuFrame.cpp
+++ layout/xul/nsMenuFrame.cpp
@@ -425,6 +425,7 @@ nsMenuFrame::HandleEvent(nsPresContext* aPresContext,
     }
     else {
       if (!IsOpen()) {
+        menuParent->ChangeMenuItem(this, false, false);
         OpenMenu(false);
       }
     }
