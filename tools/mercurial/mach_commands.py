0f715c77110b29ba1a1877fe65938af340ad7178
Gregory Szorc <gps@mozilla.com>
2015-07-14 13:42:02 -0700

    Bug 1182677 - Touch last check file before main command invocation; r=smacleod
    While not common, there are a few cases where `mach mercurial-setup`
    fails. When checking the last time this command was executed, it is
    sufficient to record the intent to run the command, not the fact that it
    executed to completion.

--- tools/mercurial/mach_commands.py
+++ tools/mercurial/mach_commands.py
@@ -26,11 +26,20 @@ class VersionControlCommands(object):
     def mercurial_bootstrap(self, update_only=False):
         sys.path.append(os.path.dirname(__file__))
 
-
         config_paths = ['~/.hgrc']
         if sys.platform in ('win32', 'cygwin'):
             config_paths.insert(0, '~/mercurial.ini')
         config_paths = map(os.path.expanduser, config_paths)
+
+        # Touch a file so we can periodically prompt to update extensions.
+        #
+        # We put this before main command logic because the command can
+        # persistently fail and we want people to get credit for the
+        # intention, not whether the command is bug free.
+        state_path = os.path.join(self._context.state_dir,
+            'mercurial/setup.lastcheck')
+        with open(state_path, 'a'):
+            os.utime(state_path, None)
 
         if update_only:
             from hgsetup.update import MercurialUpdater
@@ -40,11 +49,5 @@ class VersionControlCommands(object):
             from hgsetup.wizard import MercurialSetupWizard
             wizard = MercurialSetupWizard(self._context.state_dir)
             result = wizard.run(map(os.path.expanduser, config_paths))
-
-        # Touch a file so we can periodically prompt to update extensions.
-        state_path = os.path.join(self._context.state_dir,
-            'mercurial/setup.lastcheck')
-        with open(state_path, 'a'):
-            os.utime(state_path, None)
 
         return result
