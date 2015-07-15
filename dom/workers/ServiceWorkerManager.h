e9f8ef973fe3ffcd150996a89595e3760e526bd8
Ben Kelly <ben@wanderview.com>
2015-07-15 12:21:40 -0700

    Bug 1181887 Fall back to network if ServiceWorker script fails to load. r=ehsan r=khuey

--- dom/workers/ServiceWorkerManager.h
+++ dom/workers/ServiceWorkerManager.h
@@ -420,11 +420,13 @@ private:
   NS_IMETHOD
   CreateServiceWorkerForWindow(nsPIDOMWindow* aWindow,
                                ServiceWorkerInfo* aInfo,
+                               nsIRunnable* aLoadFailedRunnable,
                                ServiceWorker** aServiceWorker);
 
   NS_IMETHOD
   CreateServiceWorker(nsIPrincipal* aPrincipal,
                       ServiceWorkerInfo* aInfo,
+                      nsIRunnable* aLoadFailedRunnable,
                       ServiceWorker** aServiceWorker);
 
   NS_IMETHODIMP
@@ -435,7 +437,8 @@ private:
 
   already_AddRefed<ServiceWorker>
   CreateServiceWorkerForScope(const OriginAttributes& aOriginAttributes,
-                              const nsACString& aScope);
+                              const nsACString& aScope,
+                              nsIRunnable* aLoadFailedRunnable);
 
   void
   InvalidateServiceWorkerRegistrationWorker(ServiceWorkerRegistrationInfo* aRegistration,
