fbb46a13deb17cc2f0ee6bdf70faaee2c683bea5
Michael Layzell <michael@thelayzells.com>
2015-07-15 17:01:02 -0400

    Bug 1147821 - Update IndexedDB to use common StorageAllowedForWindow logic, r=khuey

--- dom/workers/Workers.h
+++ dom/workers/Workers.h
@@ -273,7 +273,7 @@ struct WorkerLoadInfo
   bool mPrincipalIsSystem;
   bool mIsInPrivilegedApp;
   bool mIsInCertifiedApp;
-  bool mIndexedDBAllowed;
+  bool mStorageAllowed;
   bool mPrivateBrowsing;
   bool mServiceWorkersTestingInWindow;
 
