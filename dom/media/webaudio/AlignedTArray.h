554baacd58af6b72722cac497eeba27ed97615bc
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-07-15 12:58:22 -0700

    Bug 1182808 - Part 3: Rename AlignedTArray_Impl to AlignedTArray and remove existing AlignedTArray. r=padenot

--- dom/media/webaudio/AlignedTArray.h
+++ dom/media/webaudio/AlignedTArray.h
@@ -14,18 +14,18 @@
  * E: element type, must be a POD type.
  * N: N bytes alignment for the first element, defaults to 32
   */
-template <typename E, int N, typename Alloc>
-class AlignedTArray_Impl : public nsTArray_Impl<E, Alloc>
+template <typename E, int N = 32>
+class AlignedTArray : public nsTArray_Impl<E, nsTArrayInfallibleAllocator>
 {
   static_assert((N & (N-1)) == 0, "N must be power of 2");
-  typedef nsTArray_Impl<E, Alloc>                    base_type;
+  typedef nsTArray_Impl<E, nsTArrayInfallibleAllocator> base_type;
 public:
   typedef E                                          elem_type;
   typedef typename base_type::size_type              size_type;
   typedef typename base_type::index_type             index_type;
 
-  AlignedTArray_Impl() {}
-  explicit AlignedTArray_Impl(size_type capacity) : base_type(capacity+sExtra) {}
+  AlignedTArray() {}
+  explicit AlignedTArray(size_type capacity) : base_type(capacity + sExtra) {}
   elem_type* Elements() { return getAligned(base_type::Elements()); }
   const elem_type* Elements() const { return getAligned(base_type::Elements()); }
   elem_type& operator[](index_type i) { return Elements()[i];}
@@ -47,8 +47,8 @@ public:
   }
 
 private:
-  AlignedTArray_Impl(const AlignedTArray_Impl& other) = delete;
-  void operator=(const AlignedTArray_Impl& other) = delete;
+  AlignedTArray(const AlignedTArray& other) = delete;
+  void operator=(const AlignedTArray& other) = delete;
 
   static const size_type sPadding = N <= MOZ_ALIGNOF(E) ? 0 : N - MOZ_ALIGNOF(E);
   static const size_type sExtra = (sPadding + sizeof(E) - 1) / sizeof(E);
@@ -58,21 +58,6 @@ private:
   {
     return reinterpret_cast<U*>(((uintptr_t)p + N - 1) & ~(N-1));
   }
-};
-
-template <typename E, int N=32>
-class AlignedTArray : public AlignedTArray_Impl<E, N, nsTArrayInfallibleAllocator>
-{
-public:
-  typedef AlignedTArray_Impl<E, N, nsTArrayInfallibleAllocator> base_type;
-  typedef AlignedTArray<E, N>                                   self_type;
-  typedef typename base_type::size_type                         size_type;
-
-  AlignedTArray() {}
-  explicit AlignedTArray(size_type capacity) : base_type(capacity) {}
-private:
-  AlignedTArray(const AlignedTArray& other) = delete;
-  void operator=(const AlignedTArray& other) = delete;
 };
 
 #endif // AlignedTArray_h__
