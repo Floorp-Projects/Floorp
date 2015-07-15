7c9680546054d1cfaf039c951f163e9b7a294596
Nikhil Marathe <nsm.nikhil@gmail.com>
2015-07-15 13:12:44 -0700

    Bug 1153499 - Enable push and sw prefs. r=dougt,ehsan

--- dom/workers/test/serviceworkers/browser_force_refresh.js
+++ dom/workers/test/serviceworkers/browser_force_refresh.js
@@ -17,6 +17,7 @@ function test() {
   SpecialPowers.pushPrefEnv({'set': [['dom.serviceWorkers.enabled', true],
                                      ['dom.serviceWorkers.exemptFromPerDomainMax', true],
                                      ['dom.serviceWorkers.testing.enabled', true],
+                                     ['dom.serviceWorkers.interception.enabled', true],
                                      ['dom.caches.enabled', true]]},
                             function() {
     var url = gTestRoot + 'browser_base_force_refresh.html';
