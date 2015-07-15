1e1981b5ef02be69206c865edf7f46e08e7db6fc
Jon Coppeard <jcoppeard@mozilla.com>
2015-07-15 11:27:27 +0100

    Bug 1165054 - Add API to clear postbarrier callbacks and use it when CPOW tables are destroyed. r=terrence, a=ritu

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -6400,6 +6400,13 @@ GCRuntime::minorGC(JSContext* cx, JS::gcreason::Reason reason)
 }
 
 void
+GCRuntime::clearPostBarrierCallbacks()
+{
+    if (storeBuffer.hasPostBarrierCallbacks())
+        evictNursery();
+}
+
+void
 GCRuntime::disableGenerationalGC()
 {
     if (isGenerationalGCEnabled()) {
