d2be4f728e9d4340008678e9990781555fad8572
Lorien Hu <lorien@lorienhu.com>
2015-07-16 17:34:51 -0400

    Bug 1171995 - Part 1: Add FocusedChild to proxy accessibles r=tbsaunde

--- accessible/ipc/ProxyAccessible.h
+++ accessible/ipc/ProxyAccessible.h
@@ -277,6 +277,7 @@ public:
   double Step();
 
   void TakeFocus();
+  ProxyAccessible* FocusedChild();
   ProxyAccessible* ChildAtPoint(int32_t aX, int32_t aY,
                                 Accessible::EWhichChildAtPoint aWhichChild);
   nsIntRect Bounds();
