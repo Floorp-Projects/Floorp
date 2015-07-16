13e6b9a44a5d7dcf531bd445cde20b6aa867c4cc
Ryan VanderMeulen <ryanvm@gmail.com>
2015-07-16 16:51:36 -0400

    Backed out 7 changesets (bug 1168979) for breaking the gecko decision task on a CLOSED TREE.
    Backed out changeset 0d9496880482 (bug 1168979)
    Backed out changeset 913594264a8c (bug 1168979)
    Backed out changeset 18560f41c3fb (bug 1168979)
    Backed out changeset dd5644a30469 (bug 1168979)
    Backed out changeset 162318c4e626 (bug 1168979)
    Backed out changeset 5bfd2e72d330 (bug 1168979)
    Backed out changeset 0d97c2ab4fe4 (bug 1168979)

--- toolkit/crashreporter/tools/upload_symbols.py
+++ toolkit/crashreporter/tools/upload_symbols.py
@@ -19,16 +19,9 @@ import redo
 import requests
 import sys
 
-try:
-    from buildconfig import substs
-except ImportError:
-    # Allow standalone use of this script, for use in TaskCluster
-    from os import environ as substs
+from buildconfig import substs
 
 url = 'https://crash-stats.mozilla.com/symbols/upload'
-# Allow overwriting of the upload url with an environmental variable
-if 'SOCORRO_SYMBOL_UPLOAD_URL' in os.environ:
-    url = os.environ['SOCORRO_SYMBOL_UPLOAD_URL']
 MAX_RETRIES = 5
 
 def print_error(r):
