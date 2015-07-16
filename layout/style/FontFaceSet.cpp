2992b3a328473268cb719111aba0710fd48badf4
Cameron McCormack <cam@mcc.id.au>
2015-07-16 17:35:17 +1000

    Bug 1183484 - Cycle collect FontFaceSetIterator. r=bzbarsky, a=lmandel

--- layout/style/FontFaceSet.cpp
+++ layout/style/FontFaceSet.cpp
@@ -535,16 +535,18 @@ FontFaceSet::Size()
   return std::min<size_t>(total, INT32_MAX);
 }
 
-FontFaceSetIterator*
+already_AddRefed<FontFaceSetIterator>
 FontFaceSet::Entries()
 {
-  return new FontFaceSetIterator(this, true);
+  nsRefPtr<FontFaceSetIterator> it = new FontFaceSetIterator(this, true);
+  return it.forget();
 }
 
-FontFaceSetIterator*
+already_AddRefed<FontFaceSetIterator>
 FontFaceSet::Values()
 {
-  return new FontFaceSetIterator(this, false);
+  nsRefPtr<FontFaceSetIterator> it = new FontFaceSetIterator(this, false);
+  return it.forget();
 }
 
 void
