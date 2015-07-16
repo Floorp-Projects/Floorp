e3a383a71e48939258a64f69a23776a214a93e7c
J. Ryan Stinnett <jryans@gmail.com>
2015-07-16 01:12:50 -0500

    Bug 1074180 - Convert touch simulator to frame script for e10s. r=ochameau, a=ritu

--- browser/devtools/responsivedesign/test/browser_responsiveui_touch.js
+++ browser/devtools/responsivedesign/test/browser_responsiveui_touch.js
@@ -1,66 +1,50 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-function test() {
-  let url = "http://mochi.test:8888/browser/browser/devtools/responsivedesign/test/touch.html";
+"use strict";
 
+const TEST_URI = "http://mochi.test:8888/browser/browser/devtools/" +
+                 "responsivedesign/test/touch.html";
+
+add_task(function*() {
+  yield addTab(TEST_URI);
   let mgr = ResponsiveUI.ResponsiveUIManager;
+  let mgrOn = once(mgr, "on");
+  mgr.toggle(window, gBrowser.selectedTab);
+  yield mgrOn;
+  yield testWithNoTouch();
+  yield mgr.getResponsiveUIForTab(gBrowser.selectedTab).enableTouch();
+  yield testWithTouch();
+  yield mgr.getResponsiveUIForTab(gBrowser.selectedTab).disableTouch();
+  yield testWithNoTouch();
+  let mgrOff = once(mgr, "off");
+  mgr.toggle(window, gBrowser.selectedTab);
+  yield mgrOff;
+});
 
-  waitForExplicitFinish();
+function* testWithNoTouch() {
+  let div = content.document.querySelector("div");
+  let x = 2, y = 2;
+  yield BrowserTestUtils.synthesizeMouse("div", x, y,
+        { type: "mousedown", isSynthesized: false }, gBrowser.selectedBrowser);
+  x += 20; y += 10;
+  yield BrowserTestUtils.synthesizeMouse("div", x, y,
+        { type: "mousemove", isSynthesized: false }, gBrowser.selectedBrowser);
+  is(div.style.transform, "none", "touch didn't work");
+  yield BrowserTestUtils.synthesizeMouse("div", x, y,
+        { type: "mouseup", isSynthesized: false }, gBrowser.selectedBrowser);
+}
 
-  gBrowser.selectedTab = gBrowser.addTab();
-  gBrowser.selectedBrowser.addEventListener("load", function onload() {
-    gBrowser.selectedBrowser.removeEventListener("load", onload, true);
-    waitForFocus(startTest, content);
-  }, true);
-
-  content.location = url;
-
-  function startTest() {
-    mgr.once("on", function() {executeSoon(testWithNoTouch)});
-    mgr.once("off", function() {executeSoon(finishUp)});
-    mgr.toggle(window, gBrowser.selectedTab);
-  }
-
-  function testWithNoTouch() {
-    let div = content.document.querySelector("div");
-    let x = 2, y = 2;
-    EventUtils.synthesizeMouse(div, x, y, {type: "mousedown", isSynthesized: false}, content);
-    x += 20; y += 10;
-    EventUtils.synthesizeMouse(div, x, y, {type: "mousemove", isSynthesized: false}, content);
-    is(div.style.transform, "", "touch didn't work");
-    EventUtils.synthesizeMouse(div, x, y, {type: "mouseup", isSynthesized: false}, content);
-    testWithTouch();
-  }
-
-  function testWithTouch() {
-    mgr.getResponsiveUIForTab(gBrowser.selectedTab).enableTouch();
-    let div = content.document.querySelector("div");
-    let x = 2, y = 2;
-    EventUtils.synthesizeMouse(div, x, y, {type: "mousedown", isSynthesized: false}, content);
-    x += 20; y += 10;
-    EventUtils.synthesizeMouse(div, x, y, {type: "mousemove", isSynthesized: false}, content);
-    is(div.style.transform, "translate(20px, 10px)", "touch worked");
-    EventUtils.synthesizeMouse(div, x, y, {type: "mouseup", isSynthesized: false}, content);
-    is(div.style.transform, "none", "end event worked");
-    mgr.toggle(window, gBrowser.selectedTab);
-  }
-
-  function testWithTouchAgain() {
-    mgr.getResponsiveUIForTab(gBrowser.selectedTab).disableTouch();
-    let div = content.document.querySelector("div");
-    let x = 2, y = 2;
-    EventUtils.synthesizeMouse(div, x, y, {type: "mousedown", isSynthesized: false}, content);
-    x += 20; y += 10;
-    EventUtils.synthesizeMouse(div, x, y, {type: "mousemove", isSynthesized: false}, content);
-    is(div.style.transform, "", "touch didn't work");
-    EventUtils.synthesizeMouse(div, x, y, {type: "mouseup", isSynthesized: false}, content);
-    finishUp();
-  }
-
-
-  function finishUp() {
-    gBrowser.removeCurrentTab();
-    finish();
-  }
+function* testWithTouch() {
+  let div = content.document.querySelector("div");
+  let x = 2, y = 2;
+  yield BrowserTestUtils.synthesizeMouse("div", x, y,
+        { type: "mousedown", isSynthesized: false }, gBrowser.selectedBrowser);
+  x += 20; y += 10;
+  yield BrowserTestUtils.synthesizeMouse("div", x, y,
+        { type: "mousemove", isSynthesized: false }, gBrowser.selectedBrowser);
+  is(div.style.transform, "translate(20px, 10px)", "touch worked");
+  yield BrowserTestUtils.synthesizeMouse("div", x, y,
+        { type: "mouseup", isSynthesized: false }, gBrowser.selectedBrowser);
+  is(div.style.transform, "none", "end event worked");
 }
