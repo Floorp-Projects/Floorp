16bd3245e74868887c300fe3fb532436a4bd219c
Michael Comella <michael.l.comella@gmail.com>
2015-07-17 15:33:18 -0700

    Bug 864958 - Backout 4 changesets to prioritize 42 release. r=me
    Changesets were "66cd9d9bf23b::7ca978706f30". Note that
    https://hg.mozilla.org/integration/fx-team/rev/58334ada143a prevented a clean
    backout of https://hg.mozilla.org/integration/fx-team/rev/7ca978706f30.
    
    See https://bugzilla.mozilla.org/show_bug.cgi?id=864958#c51 for more
    motivation.

--- mobile/android/base/toolbar/BrowserToolbar.java
+++ mobile/android/base/toolbar/BrowserToolbar.java
@@ -133,8 +133,6 @@ public abstract class BrowserToolbar extends ThemedRelativeLayout
     protected TabHistoryController tabHistoryController;
 
     private final Paint shadowPaint;
-    private final int shadowColor;
-    private final int shadowPrivateColor;
     private final int shadowSize;
 
     private final ToolbarPrefs prefs;
@@ -202,9 +200,7 @@ public abstract class BrowserToolbar extends ThemedRelativeLayout
         shadowSize = res.getDimensionPixelSize(R.dimen.browser_toolbar_shadow_size);
 
         shadowPaint = new Paint();
-        shadowColor = res.getColor(R.color.url_bar_shadow);
-        shadowPrivateColor = res.getColor(R.color.url_bar_shadow_private);
-        shadowPaint.setColor(shadowColor);
+        shadowPaint.setColor(res.getColor(R.color.url_bar_shadow));
         shadowPaint.setStrokeWidth(0.0f);
 
         setUIMode(UIMode.DISPLAY);
@@ -845,8 +841,6 @@ public abstract class BrowserToolbar extends ThemedRelativeLayout
         tabsButton.setPrivateMode(isPrivate);
         menuButton.setPrivateMode(isPrivate);
         urlEditLayout.setPrivateMode(isPrivate);
-
-        shadowPaint.setColor(isPrivate ? shadowPrivateColor : shadowColor);
     }
 
     public void show() {
