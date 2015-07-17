994772622f83c5eacd203e95a9da8c5d2cc7314b
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2015-07-17 16:30:37 -0700

    Bug 1177176 - Support opening the Control Center panel via UITour.showMenu. r=bgrins
    --HG--
    extra : commitid : CCQiV9jgd8g
    extra : rebase_source : 26e015d467c92e8134ab2f05eb61df260001405e

---
+++ browser/components/uitour/test/browser_showMenu_controlCenter.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
+const CONTROL_CENTER_PANEL = gIdentityHandler._identityPopup;
+const CONTROL_CENTER_MENU_NAME = "controlCenter";
+
+let gTestTab;
+let gContentAPI;
+let gContentWindow;
+
+function test() {
+  UITourTest();
+}
+
+let tests = [
+  taskify(function* test_showMenu() {
+    is_element_hidden(CONTROL_CENTER_PANEL, "Panel should initially be hidden");
+    yield showMenuPromise(CONTROL_CENTER_MENU_NAME);
+    is_element_visible(CONTROL_CENTER_PANEL, "Panel should be visible after showMenu");
+    yield showMenuPromise(CONTROL_CENTER_MENU_NAME);
+    is_element_visible(CONTROL_CENTER_PANEL,
+                       "Panel should remain visible and callback called after a 2nd showMenu");
+
+    yield BrowserTestUtils.withNewTab({
+      gBrowser,
+      url: "about:blank"
+    }, function*() {
+      ok(true, "Tab opened");
+    });
+
+    is_element_hidden(CONTROL_CENTER_PANEL, "Panel should hide upon tab switch");
+  }),
+
+  taskify(function* test_hideMenu() {
+    is_element_hidden(CONTROL_CENTER_PANEL, "Panel should initially be hidden");
+    yield showMenuPromise(CONTROL_CENTER_MENU_NAME);
+    is_element_visible(CONTROL_CENTER_PANEL, "Panel should be visible after showMenu");
+    let hidePromise = promisePanelElementHidden(window, CONTROL_CENTER_PANEL);
+    gContentAPI.hideMenu(CONTROL_CENTER_MENU_NAME);
+    yield hidePromise;
+
+    is_element_hidden(CONTROL_CENTER_PANEL, "Panel should hide after hideMenu");
+  }),
+];
