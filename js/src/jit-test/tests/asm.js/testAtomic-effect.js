27ebf1a8c03ad4236e7f905966f844c8f63ae3e7
Benjamin Bouvier <benj@benj.me>
2015-07-17 11:24:46 +0200

    Bug 1184887: Add a setARMHwCapFlags testing function for the shell; r=h4writer
    --HG--
    extra : commitid : EiIcsACR3s1
    extra : rebase_source : 69294cbd1299f06a5b90050621d10da7df375546
    extra : amend_source : fbd41ca837af7c62e98b022c00cd00a47104ff0e

--- js/src/jit-test/tests/asm.js/testAtomic-effect.js
+++ js/src/jit-test/tests/asm.js/testAtomic-effect.js
@@ -2,8 +2,9 @@
 if (!this.Atomics)
     quit();
 
-function m(stdlib, ffi, heap)
-{
+load(libdir + "asm.js");
+
+var code = `
     "use asm";
 
     var HEAP32 = new stdlib.SharedInt32Array(heap);
@@ -16,35 +17,33 @@ function m(stdlib, ffi, heap)
     // kernel is derived from the large test case in that bug.
 
     function add_sharedEv(i1) {
-	i1 = i1 | 0;
-	var i2 = 0;
-	var xx = 0;
-	i2 = i1 + 4 | 0;
-	i1 = load(HEAP32, i2 >> 2) | 0;
-	_emscripten_asm_const_int(7, i2 | 0, i1 | 0) | 0;
-	add(HEAP32, i2 >> 2, 1) | 0;
-	_emscripten_asm_const_int(8, i2 | 0, load(HEAP32, i2 >> 2) | 0, i1 + 1 | 0) | 0;
-	return xx|0;
+        i1 = i1 | 0;
+        var i2 = 0;
+        var xx = 0;
+        i2 = i1 + 4 | 0;
+        i1 = load(HEAP32, i2 >> 2) | 0;
+        _emscripten_asm_const_int(7, i2 | 0, i1 | 0) | 0;
+        add(HEAP32, i2 >> 2, 1) | 0;
+        _emscripten_asm_const_int(8, i2 | 0, load(HEAP32, i2 >> 2) | 0, i1 + 1 | 0) | 0;
+        return xx|0;
     }
 
     return {add_sharedEv:add_sharedEv};
-}
-
-if (isAsmJSCompilationAvailable())
-    assertEq(isAsmJSModule(m), true);
+`;
 
 var x;
 
 var sab = new SharedArrayBuffer(65536);
 var ffi =
     { _emscripten_asm_const_int:
-      function (...rest) {
-	  //print("OUT: " + rest.join(" "));
-	  if (rest[0] == 8)
-	      x = rest[2];
-      }
+        function (...rest) {
+            //print("OUT: " + rest.join(" "));
+            if (rest[0] == 8)
+                x = rest[2];
+        }
     };
-var {add_sharedEv} = m(this, ffi, sab);
+var m = asmCompile('stdlib', 'ffi', 'heap', code);
+var {add_sharedEv} = asmLink(m, this, ffi, sab);
 add_sharedEv(13812);
 
 assertEq(x, 1);
