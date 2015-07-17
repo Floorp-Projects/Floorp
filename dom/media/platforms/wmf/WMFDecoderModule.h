19aa672b13aaae34656f4ac227cf0fc28494eff6
Mason Chang <mchang@mozilla.com>
2015-07-17 13:52:38 -0700

    Bug 1172841. Set number of WMF decoder threads based on CPU count. r=cpearce

--- dom/media/platforms/wmf/WMFDecoderModule.h
+++ dom/media/platforms/wmf/WMFDecoderModule.h
@@ -48,6 +48,9 @@ public:
 
   // Called on main thread.
   static void Init();
+
+  // Called from any thread, must call init first
+  static int GetNumDecoderThreads();
 private:
   bool ShouldUseDXVA(const VideoInfo& aConfig) const;
   bool mWMFInitialized;
