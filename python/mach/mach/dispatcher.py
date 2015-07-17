f2f7fb068fc98656d816c46819ae2a0659c0f8a1
Supradeep T R <tr.supradeep@gmail.com>
2015-07-17 13:42:12 -0700

    Bug 1184780 - Use command help instead of main help; r=gps

--- python/mach/mach/dispatcher.py
+++ python/mach/mach/dispatcher.py
@@ -105,10 +105,10 @@ class CommandAction(argparse.Action):
                     # -- is in command arguments
                     if '-h' in args[:args.index('--')] or '--help' in args[:args.index('--')]:
                         # Honor -h or --help only if it appears before --
-                        self._handle_main_help(parser, command)
+                        self._handle_command_help(parser, command)
                         sys.exit(0)
                 else:
-                    self._handle_main_help(parser, command)
+                    self._handle_command_help(parser, command)
                     sys.exit(0)
 
 
