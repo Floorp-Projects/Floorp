3bf367dc88eb51c8d02089ff26d49d389d5f4b6d
Julien Pagès <j.parkouss@gmail.com>
2015-07-17 21:06:12 +0200

    Bug 1183157 - make marionette --version flag also show the transport and driver package versions. r=dburns

--- testing/marionette/driver/marionette_driver/__init__.py
+++ testing/marionette/driver/marionette_driver/__init__.py
@@ -1,3 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+__version__ = '0.10'
+
+
 from marionette_driver import ( errors, by, decorators, expected, geckoinstance,
                                 gestures, keys, marionette, selection, wait,
                                 application_cache, date_time_value )
--- testing/marionette/transport/marionette_transport/__init__.py
+++ testing/marionette/transport/marionette_transport/__init__.py
@@ -1,1 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+__version__ = '0.5'
+
+
 from transport import MarionetteTransport
