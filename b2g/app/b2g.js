ef1a37e34c59ead25bb2a2ddf4b32f80fcb3b8d4
Alastor Wu <alwu@mozilla.com>
2015-07-17 17:25:25 +0800

    Bug 1184055 - Muted by default in b2g. r=baku

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1156,3 +1156,6 @@ pref("dom.serviceWorkers.enabled", false);
 
 // Retain at most 10 processes' layers buffers
 pref("layers.compositor-lru-size", 10);
+
+// In B2G by deafult any AudioChannelAgent is muted when created.
+pref("dom.audiochannel.mutedByDefault", true);
