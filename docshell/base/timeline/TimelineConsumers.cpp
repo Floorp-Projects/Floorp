90aed03d007b45477a62dcd13ebe6c99f2664e92
Victor Porof <vporof@mozilla.com>
2015-07-18 09:35:59 -0400

    Bug 1183229 - Add a way to count the number of timeline-observed docshells outside of nsDocShell, r=smaug

---
+++ docshell/base/timeline/TimelineConsumers.cpp
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/TimelineConsumers.h"
+
+namespace mozilla {
+
+unsigned long TimelineConsumers::sActiveConsumers = 0;
+
+void
+TimelineConsumers::AddConsumer()
+{
+  sActiveConsumers++;
+}
+
+void
+TimelineConsumers::RemoveConsumer()
+{
+  sActiveConsumers--;
+}
+
+bool
+TimelineConsumers::IsEmpty()
+{
+  return sActiveConsumers == 0;
+}
+
+} // namespace mozilla
