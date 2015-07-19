3fb73a3120b2d3faf3bacca50e1c6f0b349d537d
Chris Manchester <cmanchester@mozilla.com>
2015-07-18 22:05:06 -0700

    Bug 992983 - Run gtests from mozharness' desktop_unittest.py r=jlund

--- testing/mozharness/configs/unittests/win_unittest.py
+++ testing/mozharness/configs/unittests/win_unittest.py
@@ -38,6 +38,7 @@ config = {
         "reftest": "runreftest.py",
         "xpcshell": "runxpcshelltests.py",
         "cppunittest": "runcppunittests.py",
+        "gtest": "rungtests.py",
         "jittest": "jit_test.py",
         "mozbase": "test.py",
         "mozmill": "runtestlist.py",
@@ -49,6 +50,7 @@ config = {
         "reftest": ["reftest/*", "jsreftest/*"],
         "xpcshell": ["xpcshell/*"],
         "cppunittest": ["cppunittest/*"],
+        "gtest": ["gtest/*"],
         "jittest": ["jit-test/*"],
         "mozbase": ["mozbase/*"],
         "mozmill": ["mozmill/*"],
@@ -138,7 +140,16 @@ config = {
             ],
             "run_filename": "runxpcshelltests.py",
             "testsdir": "xpcshell"
-        }
+        },
+        "gtest": {
+            "options": [
+                "--xre-path=%(abs_res_dir)s",
+                "--cwd=%(gtest_dir)s",
+                "--symbols-path=%(symbols_path)s",
+                "%(binary_path)s",
+            ],
+            "run_filename": "rungtests.py",
+        },
     },
     # local mochi suites
     "all_mochitest_suites":
@@ -190,6 +201,9 @@ config = {
     },
     "all_cppunittest_suites": {
         "cppunittest": ['tests/cppunittest']
+    },
+    "all_gtest_suites": {
+        "gtest": []
     },
     "all_jittest_suites": {
         "jittest": []
