2afd1b5e3ce489c946301b323359a2456076675b
Lee Salzman <lsalzman@mozilla.com>
2015-07-20 14:53:24 -0400

    Bug 1185636 - Part 2 - Add some utility methods to Point and Size. r=jmuizelaar

--- gfx/2d/BasePoint.h
+++ gfx/2d/BasePoint.h
@@ -69,6 +69,10 @@ struct BasePoint {
     return Sub(-x, -y);
   }
 
+  T DotProduct(const Sub& aPoint) const {
+      return x * aPoint.x + y * aPoint.y;
+  }
+
   T Length() const {
     return hypot(x, y);
   }
