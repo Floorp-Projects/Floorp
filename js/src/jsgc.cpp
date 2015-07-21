ebbdb5143c686530c8b7b9265e7a62c7a6df0073
Jon Coppeard <jcoppeard@mozilla.com>
2015-07-21 11:33:08 +0100

    Bug 1146213 - Finish ongoing GC before parse task zone becomes a non-exclusive zone again r=bhackett

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -5519,7 +5519,6 @@ AutoTraceSession::AutoTraceSession(JSRuntime* rt, JS::HeapState heapState)
     runtime(rt),
     prevState(rt->heapState_)
 {
-    MOZ_ASSERT(rt->gc.isAllocAllowed());
     MOZ_ASSERT(rt->heapState_ == JS::HeapState::Idle);
     MOZ_ASSERT(heapState != JS::HeapState::Idle);
     MOZ_ASSERT_IF(heapState == JS::HeapState::MajorCollecting, rt->gc.nursery.isEmpty());
@@ -6128,6 +6127,8 @@ GCRuntime::collect(bool incremental, SliceBudget budget, JS::gcreason::Reason re
 
     /* The engine never locks across anything that could GC. */
     MOZ_ASSERT(!rt->currentThreadHasExclusiveAccess());
+
+    MOZ_ASSERT(isAllocAllowed());
 
     if (rt->mainThread.suppressGC)
         return;
