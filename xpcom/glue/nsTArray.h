e74d057885898248b14fae8cf7799c6506426db0
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-07-21 09:42:58 -0700

    Bug 1182723 - Properly handle self-assignment in nsTArray::operator=. r=mccr8, a=abillings

--- xpcom/glue/nsTArray.h
+++ xpcom/glue/nsTArray.h
@@ -882,7 +882,9 @@ public:
   // @param aOther The array object to copy.
   self_type& operator=(const self_type& aOther)
   {
-    ReplaceElementsAt(0, Length(), aOther.Elements(), aOther.Length());
+    if (this != &aOther) {
+      ReplaceElementsAt(0, Length(), aOther.Elements(), aOther.Length());
+    }
     return *this;
   }
 
@@ -891,8 +893,10 @@ public:
   // @param other  The array object to move from.
   self_type& operator=(self_type&& aOther)
   {
-    Clear();
-    SwapElements(aOther);
+    if (this != &aOther) {
+      Clear();
+      SwapElements(aOther);
+    }
     return *this;
   }
 
