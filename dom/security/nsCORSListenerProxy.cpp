f821af7776048d8469f82f7d17e1c78160ade57e
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-07-21 11:50:45 +0200

    Backed out changeset cc377dd50503 (bug 1152574) for causing memory leaks on a CLOSED TREE
    --HG--
    extra : rebase_source : 819a2a12c3fd9adb5a756292a287288efbdc01a3

--- dom/security/nsCORSListenerProxy.cpp
+++ dom/security/nsCORSListenerProxy.cpp
@@ -538,8 +538,14 @@ nsCORSListenerProxy::CheckRequestApproved(nsIRequest* aRequest)
   // Check if the request failed
   nsresult status;
   nsresult rv = aRequest->GetStatus(&status);
-  NS_ENSURE_SUCCESS(rv, rv);
-  NS_ENSURE_SUCCESS(status, status);
+  if (NS_FAILED(rv)) {
+    LogBlockedRequest(aRequest, "CORSRequestFailed", nullptr);
+    return rv;
+  }
+  if (NS_FAILED(status)) {
+    LogBlockedRequest(aRequest, "CORSRequestFailed", nullptr);
+    return status;
+  }
 
   // Test that things worked on a HTTP level
   nsCOMPtr<nsIHttpChannel> http = do_QueryInterface(aRequest);
