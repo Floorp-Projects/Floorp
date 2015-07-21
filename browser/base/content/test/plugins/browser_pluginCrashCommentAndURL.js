e28680689b4f7c99478fc9d6e0b005634af3d556
Trevor Rowbotham <smokey101stair@gmail.com>
2015-07-21 15:59:00 -0400

    Bug 1160780 - Completely rewrite the test to use Tasks instead of frame scripts to make the test more sane and attempt to fix an intermittent orange by switching from frame scripts to Tasks. r=jimm CLOSED TREE

--- browser/base/content/test/plugins/browser_pluginCrashCommentAndURL.js
+++ browser/base/content/test/plugins/browser_pluginCrashCommentAndURL.js
@@ -1,220 +1,200 @@
 Cu.import("resource://gre/modules/CrashSubmit.jsm", this);
 
-const CRASH_URL = "http://example.com/browser/browser/base/content/test/plugins/plugin_crashCommentAndURL.html";
 const SERVER_URL = "http://example.com/browser/toolkit/crashreporter/test/browser/crashreport.sjs";
 
-/**
- * Frame script that will be injected into the test browser
- * to cause plugin crashes, and then manipulate the crashed plugin
- * UI. The specific actions and checks that occur in the frame
- * script for the crashed plugin UI are set in the
- * test:crash-plugin message object sent from the parent. The actions
- * and checks that the parent can specify are:
- *
- * pleaseSubmitStyle: the display style that the pleaseSubmit anonymous element
- *                    should have - example "block", "none".
- * submitComment:     the comment that should be put into the crash report
- * urlOptIn:          true if the submitURLOptIn element should be checked.
- * sendCrashMessage:  if true, the frame script will send a
- *                    test:crash-plugin:crashed message when the plugin has
- *                    crashed. This is used for the last test case, and
- *                    causes the frame script to skip any of the crashed
- *                    plugin UI manipulation, since the last test shows
- *                    no crashed plugin UI.
- */
-function frameScript() {
-  function fail(reason) {
-    sendAsyncMessage("test:crash-plugin:fail", {
-      reason: `Failure from frameScript: ${reason}`,
-    });
-  }
+let gTestRoot = getRootDirectory(gTestPath).replace("chrome://mochitests/content/", "http://127.0.0.1:8888/");
+let gTestBrowser = null;
+let config = {};
 
-  addMessageListener("test:crash-plugin", (message) => {
-    addEventListener("PluginCrashed", function onPluginCrashed(event) {
-      removeEventListener("PluginCrashed", onPluginCrashed);
-
-      let doc = content.document;
-      let plugin = doc.getElementById("plugin");
-      if (!plugin) {
-        fail("Could not find plugin element");
-        return;
-      }
-
-      let getUI = (anonid) => {
-        return doc.getAnonymousElementByAttribute(plugin, "anonid", anonid);
-      };
-
-      let style = content.getComputedStyle(getUI("pleaseSubmit"));
-      if (style.display != message.data.pleaseSubmitStyle) {
-        fail("Submission UI visibility is not correct. Expected " +
-             `${message.data.pleaseSubmitStyle} and got ${style.display}`);
-        return;
-      }
-
-      if (message.data.sendCrashMessage) {
-        sendAsyncMessage("test:crash-plugin:crashed", {
-          crashID: event.pluginDumpID,
-        });
-        return;
-      }
-
-      if (message.data.submitComment) {
-        getUI("submitComment").value = message.data.submitComment;
-      }
-      getUI("submitURLOptIn").checked = message.data.urlOptIn;
-      getUI("submitButton").click();
-    });
-
-    let plugin = content.document.getElementById("test");
-    try {
-      plugin.crash()
-    } catch(e) {
-    }
-  });
-}
-
-function test() {
-  // Crashing the plugin takes up a lot of time, so extend the test timeout.
-  requestLongerTimeout(runs.length);
-  waitForExplicitFinish();
-  setTestPluginEnabledState(Ci.nsIPluginTag.STATE_ENABLED);
-
+add_task(function* () {
   // The test harness sets MOZ_CRASHREPORTER_NO_REPORT, which disables plugin
   // crash reports.  This test needs them enabled.  The test also needs a mock
   // report server, and fortunately one is already set up by toolkit/
   // crashreporter/test/Makefile.in.  Assign its URL to MOZ_CRASHREPORTER_URL,
   // which CrashSubmit.jsm uses as a server override.
-  let env = Cc["@mozilla.org/process/environment;1"].
-            getService(Components.interfaces.nsIEnvironment);
+  let env = Components.classes["@mozilla.org/process/environment;1"]
+                      .getService(Components.interfaces.nsIEnvironment);
   let noReport = env.get("MOZ_CRASHREPORTER_NO_REPORT");
-  let serverURL = env.get("MOZ_CRASHREPORTER_URL");
+  let serverUrl = env.get("MOZ_CRASHREPORTER_URL");
   env.set("MOZ_CRASHREPORTER_NO_REPORT", "");
   env.set("MOZ_CRASHREPORTER_URL", SERVER_URL);
 
-  let tab = gBrowser.loadOneTab("about:blank", { inBackground: false });
-  let browser = gBrowser.getBrowserForTab(tab);
-  let mm = browser.messageManager;
-  mm.loadFrameScript("data:,(" + frameScript.toString() + ")();", true);
+  gBrowser.selectedTab = gBrowser.addTab();
+  gTestBrowser = gBrowser.selectedBrowser;
 
-  mm.addMessageListener("test:crash-plugin:fail", (message) => {
-    ok(false, message.data.reason);
-  });
+  // Crash immediately
+  Services.prefs.setIntPref("dom.ipc.plugins.timeoutSecs", 0);
 
-  Services.obs.addObserver(onSubmitStatus, "crash-report-status", false);
-
-  registerCleanupFunction(function cleanUp() {
+  registerCleanupFunction(Task.async(function*() {
+    Services.prefs.clearUserPref("dom.ipc.plugins.timeoutSecs");
     env.set("MOZ_CRASHREPORTER_NO_REPORT", noReport);
-    env.set("MOZ_CRASHREPORTER_URL", serverURL);
-    Services.obs.removeObserver(onSubmitStatus, "crash-report-status");
+    env.set("MOZ_CRASHREPORTER_URL", serverUrl);
+    env = null;
+    config = null;
+    gTestBrowser = null;
     gBrowser.removeCurrentTab();
-  });
+    window.focus();
+  }));
+});
 
-  doNextRun();
-}
-
-let runs = [
-  {
+add_task(function* () {
+  config = {
     shouldSubmissionUIBeVisible: true,
     comment: "",
-    urlOptIn: false,
-  },
-  {
+    urlOptIn: false
+  };
+
+  setTestPluginEnabledState(Ci.nsIPluginTag.STATE_ENABLED);
+
+  let pluginCrashed = promisePluginCrashed();
+
+  yield promiseTabLoadEvent(gBrowser.selectedTab, gTestRoot + "plugin_crashCommentAndURL.html");
+
+  // Work around for delayed PluginBindingAttached
+  yield promiseUpdatePluginBindings(gTestBrowser);
+
+  // Wait for the plugin to crash
+  yield pluginCrashed;
+
+  let crashReportStatus = TestUtils.topicObserved("crash-report-status", onSubmitStatus);
+
+  // Test that the crash submission UI is actually visible and submit the crash report.
+  let crashUiVisible = yield ContentTask.spawn(gTestBrowser, config, function* (aConfig) {
+    let doc = content.document;
+    let plugin = doc.getElementById("plugin");
+    let pleaseSubmit = doc.getAnonymousElementByAttribute(plugin, "anonid", "pleaseSubmit");
+    let submitButton = doc.getAnonymousElementByAttribute(plugin, "anonid", "submitButton");
+    // Test that we don't send the URL when urlOptIn is false.
+    doc.getAnonymousElementByAttribute(plugin, "anonid", "submitURLOptIn").checked = aConfig.urlOptIn;
+    submitButton.click();
+    return content.getComputedStyle(pleaseSubmit).display == "block";
+  });
+
+  yield crashReportStatus;
+
+  is(crashUiVisible, config.shouldSubmissionUIBeVisible, "The crash UI should be visible");
+});
+
+add_task(function* () {
+  config = {
     shouldSubmissionUIBeVisible: true,
     comment: "a test comment",
-    urlOptIn: true,
-  },
-  {
-    width: 300,
-    height: 300,
+    urlOptIn: true
+  };
+
+  setTestPluginEnabledState(Ci.nsIPluginTag.STATE_ENABLED);
+
+  let pluginCrashed = promisePluginCrashed();
+
+  yield promiseTabLoadEvent(gBrowser.selectedTab, gTestRoot + "plugin_crashCommentAndURL.html");
+
+  // Work around for delayed PluginBindingAttached
+  yield promiseUpdatePluginBindings(gTestBrowser);
+
+  // Wait for the plugin to crash
+  yield pluginCrashed;
+
+  let crashReportStatus = TestUtils.topicObserved("crash-report-status", onSubmitStatus);
+
+  // Test that the crash submission UI is actually visible and submit the crash report.
+  let crashUiVisible = yield ContentTask.spawn(gTestBrowser, config, function* (aConfig) {
+    let doc = content.document;
+    let plugin = doc.getElementById("plugin");
+    let pleaseSubmit = doc.getAnonymousElementByAttribute(plugin, "anonid", "pleaseSubmit");
+    let submitButton = doc.getAnonymousElementByAttribute(plugin, "anonid", "submitButton");
+    // Test that we send the URL when urlOptIn is true.
+    doc.getAnonymousElementByAttribute(plugin, "anonid", "submitURLOptIn").checked = aConfig.urlOptIn;
+    doc.getAnonymousElementByAttribute(plugin, "anonid", "submitComment").value = aConfig.comment;
+    submitButton.click();
+    return content.getComputedStyle(pleaseSubmit).display == "block";
+  });
+
+  yield crashReportStatus;
+
+  is(crashUiVisible, config.shouldSubmissionUIBeVisible, "The crash UI should be visible");
+});
+
+add_task(function* () {
+  config = {
     shouldSubmissionUIBeVisible: false,
-  },
-];
+    comment: "",
+    urlOptIn: true
+  };
 
-let currentRun = null;
+  setTestPluginEnabledState(Ci.nsIPluginTag.STATE_ENABLED);
 
-function doNextRun() {
-  try {
-    if (!runs.length) {
-      finish();
-      return;
-    }
-    currentRun = runs.shift();
-    let args = ["width", "height"].reduce(function (memo, arg) {
-      if (arg in currentRun)
-        memo[arg] = currentRun[arg];
-      return memo;
-    }, {});
-    let mm = gBrowser.selectedBrowser.messageManager;
+  let pluginCrashed = promisePluginCrashed();
 
-    if (!currentRun.shouldSubmittionUIBeVisible) {
-      mm.addMessageListener("test:crash-plugin:crash", function onCrash(message) {
-        mm.removeMessageListener("test:crash-plugin:crash", onCrash);
+  // Make sure that the plugin container is too small to display the crash submission UI
+  yield promiseTabLoadEvent(gBrowser.selectedTab, gTestRoot + "plugin_crashCommentAndURL.html?" +
+                            encodeURIComponent(JSON.stringify({width: 300, height: 300})));
 
-        ok(!!message.data.crashID, "pluginDumpID should be set");
-        CrashSubmit.delete(message.data.crashID);
-        doNextRun();
+  // Work around for delayed PluginBindingAttached
+  yield promiseUpdatePluginBindings(gTestBrowser);
+
+  // Wait for the plugin to crash
+  yield pluginCrashed;
+
+  // Test that the crash submission UI is not visible and do not submit a crash report.
+  let crashUiVisible = yield ContentTask.spawn(gTestBrowser, {}, function* () {
+    let doc = content.document;
+    let plugin = doc.getElementById("plugin");
+    let pleaseSubmit = doc.getAnonymousElementByAttribute(plugin, "anonid", "pleaseSubmit");
+    return !!pleaseSubmit && content.getComputedStyle(pleaseSubmit).display == "block";
+  });
+
+  is(crashUiVisible, config.shouldSubmissionUIBeVisible, "Plugin crash UI should not be visible");
+});
+
+function promisePluginCrashed() {
+  return new ContentTask.spawn(gTestBrowser, {}, function* () {
+    yield new Promise((resolve) => {
+      addEventListener("PluginCrashed", function onPluginCrashed() {
+        removeEventListener("PluginCrashed", onPluginCrashed);
+        resolve();
       });
-    }
-
-    mm.sendAsyncMessage("test:crash-plugin", {
-      pleaseSubmitStyle: currentRun.shouldSubmissionUIBeVisible ? "block" : "none",
-      submitComment: currentRun.comment,
-      urlOptIn: currentRun.urlOptIn,
-      sendOnCrashMessage: !currentRun.shouldSubmissionUIBeVisible,
     });
-    gBrowser.loadURI(CRASH_URL + "?" +
-                     encodeURIComponent(JSON.stringify(args)));
-    // And now wait for the crash.
-  }
-  catch (err) {
-    failWithException(err);
-    finish();
-  }
+  })
 }
 
-function onSubmitStatus(subj, topic, data) {
-  try {
-    // Wait for success or failed, doesn't matter which.
-    if (data != "success" && data != "failed")
-      return;
+function onSubmitStatus(aSubject, aData) {
+  // Wait for success or failed, doesn't matter which.
+  if (aData != "success" && aData != "failed")
+    return false;
 
-    let propBag = subj.QueryInterface(Ci.nsIPropertyBag);
-    if (data == "success") {
-      let remoteID = getPropertyBagValue(propBag, "serverCrashID");
-      ok(!!remoteID, "serverCrashID should be set");
+  let propBag = aSubject.QueryInterface(Ci.nsIPropertyBag);
+  if (aData == "success") {
+    let remoteID = getPropertyBagValue(propBag, "serverCrashID");
+    ok(!!remoteID, "serverCrashID should be set");
 
-      // Remove the submitted report file.
-      let file = Cc["@mozilla.org/file/local;1"]
-                   .createInstance(Ci.nsILocalFile);
-      file.initWithPath(Services.crashmanager._submittedDumpsDir);
-      file.append(remoteID + ".txt");
-      ok(file.exists(), "Submitted report file should exist");
-      file.remove(false);
-    }
-
-    let extra = getPropertyBagValue(propBag, "extra");
-    ok(extra instanceof Ci.nsIPropertyBag, "Extra data should be property bag");
-
-    let val = getPropertyBagValue(extra, "PluginUserComment");
-    if (currentRun.comment)
-      is(val, currentRun.comment,
-         "Comment in extra data should match comment in textbox");
-    else
-      ok(val === undefined,
-         "Comment should be absent from extra data when textbox is empty");
-
-    val = getPropertyBagValue(extra, "PluginContentURL");
-    if (currentRun.urlOptIn)
-      is(val, gBrowser.currentURI.spec,
-         "URL in extra data should match browser URL when opt-in checked");
-    else
-      ok(val === undefined,
-         "URL should be absent from extra data when opt-in not checked");
+    // Remove the submitted report file.
+    let file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+    file.initWithPath(Services.crashmanager._submittedDumpsDir);
+    file.append(remoteID + ".txt");
+    ok(file.exists(), "Submitted report file should exist");
+    file.remove(false);
   }
-  catch (err) {
-    failWithException(err);
-  }
-  doNextRun();
+
+  let extra = getPropertyBagValue(propBag, "extra");
+  ok(extra instanceof Ci.nsIPropertyBag, "Extra data should be property bag");
+
+  let val = getPropertyBagValue(extra, "PluginUserComment");
+  if (config.comment)
+    is(val, config.comment,
+       "Comment in extra data should match comment in textbox");
+  else
+    ok(val === undefined,
+       "Comment should be absent from extra data when textbox is empty");
+
+  val = getPropertyBagValue(extra, "PluginContentURL");
+  if (config.urlOptIn)
+    is(val, gBrowser.currentURI.spec,
+       "URL in extra data should match browser URL when opt-in checked");
+  else
+    ok(val === undefined,
+       "URL should be absent from extra data when opt-in not checked");
+
+  return true;
 }
 
 function getPropertyBagValue(bag, key) {
@@ -223,8 +203,4 @@ function getPropertyBagValue(bag, key) {
   }
   catch (e if e.result == Cr.NS_ERROR_FAILURE) {}
   return val;
-}
-
-function failWithException(err) {
-  ok(false, "Uncaught exception: " + err + "\n" + err.stack);
 }
