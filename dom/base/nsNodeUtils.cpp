a5399dedae7981e2e56ba8d60b006f6438704b77
Robert O'Callahan <robert@ocallahan.org>
2015-07-21 16:13:53 +1200

    Bug 1184842. Add aOldValue parameter to nsNodeUtils::AttributeChanged. r=peterv

--- dom/base/nsNodeUtils.cpp
+++ dom/base/nsNodeUtils.cpp
@@ -135,7 +135,8 @@ void
 nsNodeUtils::AttributeChanged(Element* aElement,
                               int32_t aNameSpaceID,
                               nsIAtom* aAttribute,
-                              int32_t aModType)
+                              int32_t aModType,
+                              const nsAttrValue* aOldValue)
 {
   nsIDocument* doc = aElement->OwnerDoc();
   IMPL_MUTATION_NOTIFICATION(AttributeChanged, aElement,
