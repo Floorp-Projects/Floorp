b3ab5447ebea9681f29c4d408e42aa2a5be3449c
Mike de Boer <mdeboer@mozilla.com>
2015-07-21 17:20:39 +0200

    Bug 1183178 - Use more robust e10s-friendly tab loading mechanism to fix intermittent test failures. r=Standard8, a=test-only

--- browser/components/loop/test/mochitest/browser_mozLoop_sharingListeners.js
+++ browser/components/loop/test/mochitest/browser_mozLoop_sharingListeners.js
@@ -46,10 +46,10 @@ function promiseWindowIdReceivedNewTab(handlersParam = []) {
     }));
   });
 
-  let createdTab = gBrowser.selectedTab = gBrowser.addTab();
+  let createdTab = gBrowser.selectedTab = gBrowser.addTab("about:mozilla");
   createdTabs.push(createdTab);
 
-  promiseHandlers.push(promiseTabLoadEvent(createdTab, "about:mozilla"));
+  promiseHandlers.push(BrowserTestUtils.browserLoaded(createdTab.linkedBrowser));
 
   return Promise.all(promiseHandlers);
 }
