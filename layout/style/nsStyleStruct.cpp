75d12491bf05852de9fa4f7d78afcf0b767b0a6f
Robert O'Callahan <robert@ocallahan.org>
2015-07-22 16:36:56 +1200

    Bug 1172239 - Use nsChangeHint_UpdateComputedBSize to only dirty intrinsic sizes when necessary. r=bz, a=lmandel
    --HG--
    extra : transplant_source : %C2V%FD%A0x%9D%FCq%9F%8D%BE%C2%AC%13%DB%15%08%03%0E%88

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -1598,13 +1598,10 @@ nsChangeHint nsStylePosition::CalcDifference(const nsStylePosition& aOther) cons
       mMaxHeight != aOther.mMaxHeight) {
     // Height changes can affect descendant intrinsic sizes due to replaced
     // elements with percentage heights in descendants which also have
-    // percentage heights.  And due to our not-so-great computation of mVResize
-    // in nsHTMLReflowState, they do need to force reflow of the whole subtree.
-    // XXXbz due to XUL caching heights as well, height changes also need to
-    // clear ancestor intrinsics!
+    // percentage heights. This is handled via nsChangeHint_UpdateComputedBSize
+    // which clears intrinsic sizes for frames that have such replaced elements.
     return NS_CombineHint(hint, nsChangeHint_NeedReflow |
-        nsChangeHint_ClearAncestorIntrinsics |
-        nsChangeHint_ClearDescendantIntrinsics |
+        nsChangeHint_UpdateComputedBSize |
         nsChangeHint_ReflowChangesSizeOrPosition);
   }
 
