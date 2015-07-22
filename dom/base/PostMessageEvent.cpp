39771fe18ee4a035f8a376e1d7b7049bbc72cea2
Andrea Marchesini <amarchesini@mozilla.com>
2015-07-22 19:37:18 +0100

    Bug 1184557 - StructuredCloneHelper class for window to window postMessage, r=smaug

--- dom/base/PostMessageEvent.cpp
+++ dom/base/PostMessageEvent.cpp
@@ -23,246 +23,6 @@
 namespace mozilla {
 namespace dom {
 
-namespace {
-
-struct StructuredCloneInfo
-{
-  PostMessageEvent* event;
-  nsPIDOMWindow* window;
-
-  // This hashtable contains the transferred ports - used to avoid duplicates.
-  nsTArray<nsRefPtr<MessagePortBase>> transferredPorts;
-
-  // This array is populated when the ports are cloned.
-  nsTArray<nsRefPtr<MessagePortBase>> clonedPorts;
-};
-
-} // namespace
-
-const JSStructuredCloneCallbacks PostMessageEvent::sPostMessageCallbacks = {
-  PostMessageEvent::ReadStructuredClone,
-  PostMessageEvent::WriteStructuredClone,
-  nullptr,
-  PostMessageEvent::ReadTransferStructuredClone,
-  PostMessageEvent::TransferStructuredClone,
-  PostMessageEvent::FreeTransferStructuredClone
-};
-
-/* static */ JSObject*
-PostMessageEvent::ReadStructuredClone(JSContext* cx,
-                                      JSStructuredCloneReader* reader,
-                                      uint32_t tag,
-                                      uint32_t data,
-                                      void* closure)
-{
-  StructuredCloneInfo* scInfo = static_cast<StructuredCloneInfo*>(closure);
-  NS_ASSERTION(scInfo, "Must have scInfo!");
-
-  if (tag == SCTAG_DOM_BLOB) {
-    NS_ASSERTION(!data, "Data should be empty");
-
-    // What we get back from the reader is a BlobImpl.
-    // From that we create a new File.
-    BlobImpl* blobImpl;
-    if (JS_ReadBytes(reader, &blobImpl, sizeof(blobImpl))) {
-      MOZ_ASSERT(blobImpl);
-
-      // nsRefPtr<File> needs to go out of scope before toObjectOrNull() is
-      // called because the static analysis thinks dereferencing XPCOM objects
-      // can GC (because in some cases it can!), and a return statement with a
-      // JSObject* type means that JSObject* is on the stack as a raw pointer
-      // while destructors are running.
-      JS::Rooted<JS::Value> val(cx);
-      {
-        nsRefPtr<Blob> blob = Blob::Create(scInfo->window, blobImpl);
-        if (!ToJSValue(cx, blob, &val)) {
-          return nullptr;
-        }
-      }
-
-      return &val.toObject();
-    }
-  }
-
-  if (tag == SCTAG_DOM_FILELIST) {
-    NS_ASSERTION(!data, "Data should be empty");
-
-    // What we get back from the reader is a FileListClonedData.
-    // From that we create a new FileList.
-    FileListClonedData* fileListClonedData;
-    if (JS_ReadBytes(reader, &fileListClonedData, sizeof(fileListClonedData))) {
-      MOZ_ASSERT(fileListClonedData);
-
-      // nsRefPtr<FileList> needs to go out of scope before toObjectOrNull() is
-      // called because the static analysis thinks dereferencing XPCOM objects
-      // can GC (because in some cases it can!), and a return statement with a
-      // JSObject* type means that JSObject* is on the stack as a raw pointer
-      // while destructors are running.
-      JS::Rooted<JS::Value> val(cx);
-      {
-        nsRefPtr<FileList> fileList =
-          FileList::Create(scInfo->window, fileListClonedData);
-        if (!fileList || !ToJSValue(cx, fileList, &val)) {
-          return nullptr;
-        }
-      }
-
-      return &val.toObject();
-    }
-  }
-
-  const JSStructuredCloneCallbacks* runtimeCallbacks =
-    js::GetContextStructuredCloneCallbacks(cx);
-
-  if (runtimeCallbacks) {
-    return runtimeCallbacks->read(cx, reader, tag, data, nullptr);
-  }
-
-  return nullptr;
-}
-
-/* static */ bool
-PostMessageEvent::WriteStructuredClone(JSContext* cx,
-                                       JSStructuredCloneWriter* writer,
-                                       JS::Handle<JSObject*> obj,
-                                       void *closure)
-{
-  StructuredCloneInfo* scInfo = static_cast<StructuredCloneInfo*>(closure);
-  NS_ASSERTION(scInfo, "Must have scInfo!");
-
-  // See if this is a File/Blob object.
-  {
-    Blob* blob = nullptr;
-    if (NS_SUCCEEDED(UNWRAP_OBJECT(Blob, obj, blob))) {
-      BlobImpl* blobImpl = blob->Impl();
-      if (JS_WriteUint32Pair(writer, SCTAG_DOM_BLOB, 0) &&
-          JS_WriteBytes(writer, &blobImpl, sizeof(blobImpl))) {
-        scInfo->event->StoreISupports(blobImpl);
-        return true;
-      }
-    }
-  }
-
-  // See if this is a FileList object.
-  {
-    FileList* fileList = nullptr;
-    if (NS_SUCCEEDED(UNWRAP_OBJECT(FileList, obj, fileList))) {
-      nsRefPtr<FileListClonedData> fileListClonedData =
-        fileList->CreateClonedData();
-      MOZ_ASSERT(fileListClonedData);
-      FileListClonedData* ptr = fileListClonedData.get();
-      if (JS_WriteUint32Pair(writer, SCTAG_DOM_FILELIST, 0) &&
-          JS_WriteBytes(writer, &ptr, sizeof(ptr))) {
-        scInfo->event->StoreISupports(fileListClonedData);
-        return true;
-      }
-    }
-  }
-
-  const JSStructuredCloneCallbacks* runtimeCallbacks =
-    js::GetContextStructuredCloneCallbacks(cx);
-
-  if (runtimeCallbacks) {
-    return runtimeCallbacks->write(cx, writer, obj, nullptr);
-  }
-
-  return false;
-}
-
-/* static */ bool
-PostMessageEvent::ReadTransferStructuredClone(JSContext* aCx,
-                                              JSStructuredCloneReader* reader,
-                                              uint32_t tag, void* aData,
-                                              uint64_t aExtraData,
-                                              void* aClosure,
-                                              JS::MutableHandle<JSObject*> returnObject)
-{
-  StructuredCloneInfo* scInfo = static_cast<StructuredCloneInfo*>(aClosure);
-  NS_ASSERTION(scInfo, "Must have scInfo!");
-
-  if (tag == SCTAG_DOM_MAP_MESSAGEPORT) {
-    MOZ_ASSERT(!aData);
-    // aExtraData is the index of this port identifier.
-    ErrorResult rv;
-    nsRefPtr<MessagePort> port =
-      MessagePort::Create(scInfo->window,
-                          scInfo->event->GetPortIdentifier(aExtraData),
-                          rv);
-    if (NS_WARN_IF(rv.Failed())) {
-      return false;
-    }
-
-    scInfo->clonedPorts.AppendElement(port);
-
-    JS::Rooted<JS::Value> value(aCx);
-    if (!GetOrCreateDOMReflector(aCx, port, &value)) {
-      JS_ClearPendingException(aCx);
-      return false;
-    }
-
-    returnObject.set(&value.toObject());
-    return true;
-  }
-
-  return false;
-}
-
-/* static */ bool
-PostMessageEvent::TransferStructuredClone(JSContext* aCx,
-                                          JS::Handle<JSObject*> aObj,
-                                          void* aClosure,
-                                          uint32_t* aTag,
-                                          JS::TransferableOwnership* aOwnership,
-                                          void** aContent,
-                                          uint64_t* aExtraData)
-{
-  StructuredCloneInfo* scInfo = static_cast<StructuredCloneInfo*>(aClosure);
-  NS_ASSERTION(scInfo, "Must have scInfo!");
-
-  MessagePortBase* port = nullptr;
-  nsresult rv = UNWRAP_OBJECT(MessagePort, aObj, port);
-  if (NS_SUCCEEDED(rv)) {
-    if (scInfo->transferredPorts.Contains(port)) {
-      // No duplicates.
-      return false;
-    }
-
-    // We use aExtraData to store the index of this new port identifier.
-    MessagePortIdentifier* identifier =
-      scInfo->event->NewPortIdentifier(aExtraData);
-
-    if (!port->CloneAndDisentangle(*identifier)) {
-      return false;
-    }
-
-    scInfo->transferredPorts.AppendElement(port);
-
-    *aTag = SCTAG_DOM_MAP_MESSAGEPORT;
-    *aOwnership = JS::SCTAG_TMO_CUSTOM;
-    *aContent = nullptr;
-
-    return true;
-  }
-
-  return false;
-}
-
-/* static */ void
-PostMessageEvent::FreeTransferStructuredClone(uint32_t aTag,
-                                              JS::TransferableOwnership aOwnership,
-                                              void *aContent,
-                                              uint64_t aExtraData,
-                                              void* aClosure)
-{
-  if (aTag == SCTAG_DOM_MAP_MESSAGEPORT) {
-    MOZ_ASSERT(aClosure);
-    MOZ_ASSERT(!aContent);
-
-    StructuredCloneInfo* scInfo = static_cast<StructuredCloneInfo*>(aClosure);
-    MessagePort::ForceClose(scInfo->event->GetPortIdentifier(aExtraData));
-  }
-}
-
 PostMessageEvent::PostMessageEvent(nsGlobalWindow* aSource,
                                    const nsAString& aCallerOrigin,
                                    nsGlobalWindow* aTargetWindow,
@@ -280,20 +40,6 @@ PostMessageEvent::PostMessageEvent(nsGlobalWindow* aSource,
 PostMessageEvent::~PostMessageEvent()
 {
   MOZ_COUNT_DTOR(PostMessageEvent);
-}
-
-const MessagePortIdentifier&
-PostMessageEvent::GetPortIdentifier(uint64_t aId)
-{
-  MOZ_ASSERT(aId < mPortIdentifiers.Length());
-  return mPortIdentifiers[aId];
-}
-
-MessagePortIdentifier*
-PostMessageEvent::NewPortIdentifier(uint64_t* aPosition)
-{
-  *aPosition = mPortIdentifiers.Length();
-  return mPortIdentifiers.AppendElement();
 }
 
 NS_IMETHODIMP
@@ -347,13 +93,9 @@ PostMessageEvent::Run()
     }
   }
 
-  // Deserialize the structured clone data
   JS::Rooted<JS::Value> messageData(cx);
-  StructuredCloneInfo scInfo;
-  scInfo.event = this;
-  scInfo.window = targetWindow;
-
-  if (!mBuffer.read(cx, &messageData, &sPostMessageCallbacks, &scInfo)) {
+  nsCOMPtr<nsPIDOMWindow> window = targetWindow.get();
+  if (!Read(window, cx, &messageData)) {
     return NS_ERROR_DOM_DATA_CLONE_ERR;
   }
 
@@ -368,7 +110,7 @@ PostMessageEvent::Run()
                           EmptyString(), mSource);
 
   event->SetPorts(new MessagePortList(static_cast<dom::Event*>(event.get()),
-                                      scInfo.clonedPorts));
+                                      GetTransferredPorts()));
 
   // We can't simply call dispatchEvent on the window because doing so ends
   // up flipping the trusted bit on the event, and we don't want that to
@@ -390,20 +132,6 @@ PostMessageEvent::Run()
                             static_cast<dom::Event*>(event.get()),
                             &status);
   return NS_OK;
-}
-
-bool
-PostMessageEvent::Write(JSContext* aCx, JS::Handle<JS::Value> aMessage,
-                        JS::Handle<JS::Value> aTransfer, nsPIDOMWindow* aWindow)
-{
-  // We *must* clone the data here, or the JS::Value could be modified
-  // by script
-  StructuredCloneInfo scInfo;
-  scInfo.event = this;
-  scInfo.window = aWindow;
-
-  return mBuffer.write(aCx, aMessage, aTransfer, &sPostMessageCallbacks,
-                       &scInfo);
 }
 
 } // namespace dom
