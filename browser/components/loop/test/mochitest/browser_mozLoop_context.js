0b44fd7ca4cdd6078159d308a0e278b197066d49
Ryan VanderMeulen <ryanvm@gmail.com>
2015-07-22 14:17:14 -0400

    Bug 1183178 - Try to fix test failures. a=test-only

--- browser/components/loop/test/mochitest/browser_mozLoop_context.js
+++ browser/components/loop/test/mochitest/browser_mozLoop_context.js
@@ -29,7 +29,9 @@ add_task(function* test_mozLoop_getSelectedTabMetadata() {
   Assert.strictEqual(metadata.url, null, "URL should be empty for about:home");
   Assert.strictEqual(metadata.favicon, null, "Favicon should be empty for about:home");
   Assert.ok(metadata.title, "Title should be set for about:home");
-  Assert.deepEqual(metadata.previews, [], "No previews available for about:home");
+  // Filter out null elements in the previews - contentSearchUI adds some img
+  // elements with chrome:// srcs, which show up as null in metadata.previews.
+  Assert.deepEqual(metadata.previews.filter(e => e), [], "No previews available for about:home");
 
   yield BrowserTestUtils.removeTab(tab);
 });
