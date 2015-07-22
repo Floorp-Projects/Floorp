367d4612a65165cf211354c765ca055c9b9f1151
Andrea Marchesini <amarchesini@mozilla.com>
2015-07-22 17:03:07 +0100

    Bug 1185381 - Make FileList clonable - patch 1 - move code into FileList.h/.cpp, r=smaug

---
+++ dom/base/FileList.cpp
@@ -0,0 +1,47 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/FileList.h"
+#include "mozilla/dom/FileListBinding.h"
+
+namespace mozilla {
+namespace dom {
+
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(FileList, mFiles, mParent)
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(FileList)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY_AMBIGUOUS(nsISupports, nsIDOMFileList)
+  NS_INTERFACE_MAP_ENTRY(nsIDOMFileList)
+NS_INTERFACE_MAP_END
+
+NS_IMPL_CYCLE_COLLECTING_ADDREF(FileList)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(FileList)
+
+JSObject*
+FileList::WrapObject(JSContext* aCx, JS::Handle<JSObject*> aGivenProto)
+{
+  return mozilla::dom::FileListBinding::Wrap(aCx, this, aGivenProto);
+}
+
+NS_IMETHODIMP
+FileList::GetLength(uint32_t* aLength)
+{
+  *aLength = Length();
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+FileList::Item(uint32_t aIndex, nsISupports** aFile)
+{
+  nsCOMPtr<nsIDOMBlob> file = Item(aIndex);
+  file.forget(aFile);
+  return NS_OK;
+}
+
+} // namespace dom
+} // namespace mozilla
