b4358cbd90de89bc4b29adafdbcc5b00ed6313b4
Seth Fowler <mark.seth.fowler@gmail.com>
2015-07-22 15:49:49 -0700

    Bug 1186112 - Get rid of the #define'd constants in BMPFileHeaders.h. r=tn

--- image/decoders/nsICODecoder.cpp
+++ image/decoders/nsICODecoder.cpp
@@ -125,7 +125,7 @@ nsICODecoder::FillBitmapFileHeaderBuffer(int8_t* bfh)
   bfh[1] = 'M';
   int32_t dataOffset = 0;
   int32_t fileSize = 0;
-  dataOffset = BFH_LENGTH + BITMAPINFOSIZE;
+  dataOffset = BMPFILEHEADER::LENGTH + BITMAPINFOSIZE;
 
   // The color table is present only if BPP is <= 8
   if (mDirEntry.mBitCount <= 8) {
