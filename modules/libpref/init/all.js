dc5f931fe00e92f7025460e0f045ae03c72073b1
Jeff Gilbert <jgilbert@mozilla.com>
2015-07-21 17:41:57 -0700

    Bug 1171228 - Expose WEBGL_debug_renderer_info to web content on non-RELEASE_BUILDs. r=kamidphish, sr=jst, a=ritu
    --HG--
    extra : amend_source : cb550128b68cf4da28fcf18bbc7e7717a347b7a6
    extra : transplant_source : %FA%5D%3FB%0E%E6%9EV%96%2Bh%FA%40%B0Ym_%FA%12%DE

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4085,6 +4085,8 @@ pref("gl.msaa-level", 0);
 #else
 pref("gl.msaa-level", 2);
 #endif
+pref("gl.require-hardware", false);
+
 pref("webgl.force-enabled", false);
 pref("webgl.disabled", false);
 pref("webgl.disable-angle", false);
@@ -4103,7 +4105,9 @@ pref("webgl.enable-privileged-extensions", false);
 pref("webgl.bypass-shader-validation", false);
 pref("webgl.enable-prototype-webgl2", false);
 pref("webgl.disable-fail-if-major-performance-caveat", false);
-pref("gl.require-hardware", false);
+pref("webgl.disable-debug-renderer-info", false);
+pref("webgl.renderer-string-override", "");
+pref("webgl.vendor-string-override", "");
 
 #ifdef XP_WIN
 pref("webgl.angle.try-d3d11", true);
