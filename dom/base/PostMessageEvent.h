94ee9642d4122768aa7c351cafe580a9554e560c
Andrea Marchesini <amarchesini@mozilla.com>
2015-07-22 19:37:18 +0100

    Bug 1184557 - StructuredCloneHelper class for window to window postMessage, r=smaug

--- dom/base/PostMessageEvent.h
+++ dom/base/PostMessageEvent.h
@@ -7,7 +7,7 @@
 #ifndef mozilla_dom_PostMessageEvent_h
 #define mozilla_dom_PostMessageEvent_h
 
-#include "js/StructuredClone.h"
+#include "mozilla/dom/StructuredCloneHelper.h"
 #include "nsCOMPtr.h"
 #include "nsRefPtr.h"
 #include "nsTArray.h"
@@ -28,6 +28,7 @@ class MessagePortIdentifier;
  * which asynchronously creates and dispatches events.
  */
 class PostMessageEvent final : public nsRunnable
+                             , public StructuredCloneHelper
 {
 public:
   NS_DECL_NSIRUNNABLE
@@ -38,69 +39,14 @@ public:
                    nsIPrincipal* aProvidedPrincipal,
                    bool aTrustedCaller);
 
-  bool Write(JSContext* aCx, JS::Handle<JS::Value> aMessage,
-             JS::Handle<JS::Value> aTransfer, nsPIDOMWindow* aWindow);
-
 private:
   ~PostMessageEvent();
 
-  const MessagePortIdentifier& GetPortIdentifier(uint64_t aId);
-
-  MessagePortIdentifier* NewPortIdentifier(uint64_t* aPosition);
-
-  bool StoreISupports(nsISupports* aSupports)
-  {
-    mSupportsArray.AppendElement(aSupports);
-    return true;
-  }
-
-  static JSObject*
-  ReadStructuredClone(JSContext* cx,
-                      JSStructuredCloneReader* reader,
-                      uint32_t tag,
-                      uint32_t data,
-                      void* closure);
-
-  static bool
-  WriteStructuredClone(JSContext* cx,
-                       JSStructuredCloneWriter* writer,
-                       JS::Handle<JSObject*> obj,
-                       void *closure);
-
-  static bool
-  ReadTransferStructuredClone(JSContext* aCx,
-                              JSStructuredCloneReader* reader,
-                              uint32_t tag, void* aData,
-                              uint64_t aExtraData,
-                              void* aClosure,
-                              JS::MutableHandle<JSObject*> returnObject);
-
-  static bool
-  TransferStructuredClone(JSContext* aCx,
-                          JS::Handle<JSObject*> aObj,
-                          void* aClosure,
-                          uint32_t* aTag,
-                          JS::TransferableOwnership* aOwnership,
-                          void** aContent,
-                          uint64_t* aExtraData);
-
-  static void
-  FreeTransferStructuredClone(uint32_t aTag,
-                              JS::TransferableOwnership aOwnership,
-                              void *aContent,
-                              uint64_t aExtraData,
-                              void* aClosure);
-
-  static const JSStructuredCloneCallbacks sPostMessageCallbacks;
-
-  JSAutoStructuredCloneBuffer mBuffer;
   nsRefPtr<nsGlobalWindow> mSource;
   nsString mCallerOrigin;
   nsRefPtr<nsGlobalWindow> mTargetWindow;
   nsCOMPtr<nsIPrincipal> mProvidedPrincipal;
   bool mTrustedCaller;
-  nsTArray<nsCOMPtr<nsISupports>> mSupportsArray;
-  nsTArray<MessagePortIdentifier> mPortIdentifiers;
 };
 
 } // namespace dom
