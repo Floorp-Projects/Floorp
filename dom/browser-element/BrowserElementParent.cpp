ce6cce63e18ec0989f0f75954861bbdaf86ab907
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-07-22 08:13:08 +0200

    Backed out changeset 35c6e7e9c205 (bug 1135261) for test failures in gij in bookmark_edit_test.js  on a CLOSED TREE

--- dom/browser-element/BrowserElementParent.cpp
+++ dom/browser-element/BrowserElementParent.cpp
@@ -123,7 +123,7 @@ DispatchCustomDOMEvent(Element* aFrameElement, const nsAString& aEventName,
   }
   customEvent->SetTrusted(true);
   // Dispatch the event.
-  // We don't initialize aStatus here, as our callers have already done so.
+  *aStatus = nsEventStatus_eConsumeNoDefault;
   nsresult rv =
     EventDispatcher::DispatchDOMEvent(aFrameElement, nullptr,
                                       domEvent, presContext, aStatus);
@@ -182,7 +182,7 @@ BrowserElementParent::DispatchOpenWindowEvent(Element* aOpenerFrameElement,
     return BrowserElementParent::OPEN_WINDOW_CANCELLED;
   }
 
-  nsEventStatus status = nsEventStatus_eIgnore;
+  nsEventStatus status;
   bool dispatchSucceeded =
     DispatchCustomDOMEvent(aOpenerFrameElement,
                            NS_LITERAL_STRING("mozbrowseropenwindow"),
