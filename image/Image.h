072bb4b0a5c0efb696186dbe5fe0571f12bb6450
Seth Fowler <mark.seth.fowler@gmail.com>
2015-07-22 22:39:48 -0700

    Bug 1184996 (Part 1) - Create decoders with a DecoderFactory. r=tn

--- image/Image.h
+++ image/Image.h
@@ -129,18 +129,6 @@ private:
 class Image : public imgIContainer
 {
 public:
-  // Mimetype translation
-  enum eDecoderType {
-    eDecoderType_png     = 0,
-    eDecoderType_gif     = 1,
-    eDecoderType_jpeg    = 2,
-    eDecoderType_bmp     = 3,
-    eDecoderType_ico     = 4,
-    eDecoderType_icon    = 5,
-    eDecoderType_unknown = 6
-  };
-  static eDecoderType GetDecoderType(const char* aMimeType);
-
   /**
    * Flags for Image initialization.
    *
