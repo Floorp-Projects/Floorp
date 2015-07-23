c2220ab9a92ece20ae269adcfc53b2b8135acbc4
Neil Deakin <neil@mozilla.com>
2015-07-23 10:04:19 -0400

    Bug 1183623, redirect keyup events to keydown target when focus shifts from chrome to content in-between, r=smaug

---
+++ browser/base/content/test/general/browser_focusonkeydown.js
@@ -0,0 +1,26 @@
+add_task(function *()
+{
+  let keyUps = 0;
+
+  yield BrowserTestUtils.openNewForegroundTab(gBrowser, "data:text/html,<body>");
+
+  gURLBar.focus();
+
+  window.addEventListener("keyup", function countKeyUps(event) {
+    window.removeEventListener("keyup", countKeyUps, true);
+    if (event.originalTarget == gURLBar.inputField) {
+      keyUps++;
+    }
+  }, true);
+
+  gURLBar.addEventListener("keydown", function redirectFocus(event) {
+    gURLBar.removeEventListener("keydown", redirectFocus, true);
+    gBrowser.selectedBrowser.focus();
+  }, true);
+
+  EventUtils.synthesizeKey("v", { });
+
+  is(keyUps, 1, "Key up fired at url bar");
+
+  gBrowser.removeCurrentTab();
+});
