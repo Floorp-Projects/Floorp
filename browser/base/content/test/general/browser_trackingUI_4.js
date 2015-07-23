59d21a5a5548f2c6531e0af1e72a18ad2cd72314
Brian Grinstead <bgrinstead@mozilla.com>
2015-07-23 13:55:34 -0700

    Bug 1175858 - Style updates for browser tracking protection tests;r=MattN
    --HG--
    extra : commitid : 6cmb6kUpgGr

--- browser/base/content/test/general/browser_trackingUI_4.js
+++ browser/base/content/test/general/browser_trackingUI_4.js
@@ -1,10 +1,8 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-// Test that the Tracking Protection icon is properly animated in the identity
-// block when loading tabs and switching between tabs.
-// See also Bug 1175858.
+/*
+ * Test that the Tracking Protection icon is properly animated in the identity
+ * block when loading tabs and switching between tabs.
+ * See also Bug 1175858.
+ */
 
 const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 const PREF = "privacy.trackingprotection.enabled";
@@ -12,12 +10,12 @@ const PB_PREF = "privacy.trackingprotection.pbmode.enabled";
 const BENIGN_PAGE = "http://tracking.example.org/browser/browser/base/content/test/general/benignPage.html";
 const TRACKING_PAGE = "http://tracking.example.org/browser/browser/base/content/test/general/trackingPage.html";
 let TrackingProtection = null;
-let browser = null;
+let tabbrowser = null;
 
 let {UrlClassifierTestUtils} = Cu.import("resource://testing-common/UrlClassifierTestUtils.jsm", {});
 
 registerCleanupFunction(function() {
-  TrackingProtection = browser = null;
+  TrackingProtection = tabbrowser = null;
   UrlClassifierTestUtils.cleanupTestTrackers();
   Services.prefs.clearUserPref(PREF);
   Services.prefs.clearUserPref(PB_PREF);
@@ -34,76 +32,76 @@ function waitForSecurityChange(numChanges = 1) {
         n = n + 1;
         info ("Recieved onSecurityChange event " + n + " of " + numChanges);
         if (n >= numChanges) {
-          browser.removeProgressListener(listener);
+          tabbrowser.removeProgressListener(listener);
           resolve();
         }
       }
     };
-    browser.addProgressListener(listener);
+    tabbrowser.addProgressListener(listener);
   });
 }
 
 function* testTrackingProtectionAnimation() {
   info("Load a test page not containing tracking elements");
-  let benignTab = yield BrowserTestUtils.openNewForegroundTab(browser, BENIGN_PAGE);
+  let benignTab = yield BrowserTestUtils.openNewForegroundTab(tabbrowser, BENIGN_PAGE);
 
-  ok (!TrackingProtection.icon.hasAttribute("state"), "icon: no state");
-  ok (TrackingProtection.icon.hasAttribute("animate"), "icon: animate");
+  ok(!TrackingProtection.icon.hasAttribute("state"), "icon: no state");
+  ok(TrackingProtection.icon.hasAttribute("animate"), "icon: animate");
 
   info("Load a test page containing tracking elements");
-  let trackingTab = yield BrowserTestUtils.openNewForegroundTab(browser, TRACKING_PAGE);
+  let trackingTab = yield BrowserTestUtils.openNewForegroundTab(tabbrowser, TRACKING_PAGE);
 
-  ok (TrackingProtection.icon.hasAttribute("state"), "icon: state");
-  ok (TrackingProtection.icon.hasAttribute("animate"), "icon: animate");
+  ok(TrackingProtection.icon.hasAttribute("state"), "icon: state");
+  ok(TrackingProtection.icon.hasAttribute("animate"), "icon: animate");
 
   info("Switch from tracking -> benign tab");
   let securityChanged = waitForSecurityChange();
-  browser.selectedTab = benignTab;
+  tabbrowser.selectedTab = benignTab;
   yield securityChanged;
 
-  ok (!TrackingProtection.icon.hasAttribute("state"), "icon: no state");
-  ok (!TrackingProtection.icon.hasAttribute("animate"), "icon: no animate");
+  ok(!TrackingProtection.icon.hasAttribute("state"), "icon: no state");
+  ok(!TrackingProtection.icon.hasAttribute("animate"), "icon: no animate");
 
   info("Switch from benign -> tracking tab");
   securityChanged = waitForSecurityChange();
-  browser.selectedTab = trackingTab;
+  tabbrowser.selectedTab = trackingTab;
   yield securityChanged;
 
-  ok (TrackingProtection.icon.hasAttribute("state"), "icon: state");
-  ok (!TrackingProtection.icon.hasAttribute("animate"), "icon: no animate");
+  ok(TrackingProtection.icon.hasAttribute("state"), "icon: state");
+  ok(!TrackingProtection.icon.hasAttribute("animate"), "icon: no animate");
 
   info("Reload tracking tab");
   securityChanged = waitForSecurityChange(2);
-  browser.reload();
+  tabbrowser.reload();
   yield securityChanged;
 
-  ok (TrackingProtection.icon.hasAttribute("state"), "icon: state");
-  ok (TrackingProtection.icon.hasAttribute("animate"), "icon: animate");
+  ok(TrackingProtection.icon.hasAttribute("state"), "icon: state");
+  ok(TrackingProtection.icon.hasAttribute("animate"), "icon: animate");
 }
 
 add_task(function* testNormalBrowsing() {
   yield UrlClassifierTestUtils.addTestTrackers();
 
-  browser = gBrowser;
+  tabbrowser = gBrowser;
 
   TrackingProtection = gBrowser.ownerGlobal.TrackingProtection;
-  ok (TrackingProtection, "TP is attached to the browser window");
+  ok(TrackingProtection, "TP is attached to the browser window");
 
   Services.prefs.setBoolPref(PREF, true);
-  ok (TrackingProtection.enabled, "TP is enabled after setting the pref");
+  ok(TrackingProtection.enabled, "TP is enabled after setting the pref");
 
   yield testTrackingProtectionAnimation();
 });
 
 add_task(function* testPrivateBrowsing() {
   let privateWin = yield promiseOpenAndLoadWindow({private: true}, true);
-  browser = privateWin.gBrowser;
+  tabbrowser = privateWin.gBrowser;
 
-  TrackingProtection = browser.ownerGlobal.TrackingProtection;
-  ok (TrackingProtection, "TP is attached to the private window");
+  TrackingProtection = tabbrowser.ownerGlobal.TrackingProtection;
+  ok(TrackingProtection, "TP is attached to the private window");
 
   Services.prefs.setBoolPref(PB_PREF, true);
-  ok (TrackingProtection.enabled, "TP is enabled after setting the pref");
+  ok(TrackingProtection.enabled, "TP is enabled after setting the pref");
 
   yield testTrackingProtectionAnimation();
 
