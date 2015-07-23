c92c7651740aad917dbeed237f7ec9aa69180056
Ursula <usarracini@mozilla.com>
2015-07-23 14:47:35 -0400

    Bug 1142034 - Don't show 'Restore All Crashed Tabs' when only one tab has crashed. r=mconley
    --HG--
    extra : commitid : KI4QTK8C8S9
    extra : rebase_source : fbfb838e3e56b950ea3e37956d808b3deab3ce8c

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1139,7 +1139,9 @@ var gBrowserInit = {
 
     gBrowser.addEventListener("AboutTabCrashedLoad", function(event) {
 #ifdef MOZ_CRASHREPORTER
-      TabCrashReporter.onAboutTabCrashedLoad(gBrowser.getBrowserForDocument(event.target));
+      TabCrashReporter.onAboutTabCrashedLoad(gBrowser.getBrowserForDocument(event.target), {
+        crashedTabCount: SessionStore.crashedTabCount,
+      });
 #endif
     }, false, true);
 
@@ -1171,11 +1173,7 @@ var gBrowserInit = {
         SessionStore.reviveCrashedTab(tab);
         break;
       case "restoreAll":
-        for (let browserWin of browserWindows()) {
-          for (let tab of browserWin.gBrowser.tabs) {
-            SessionStore.reviveCrashedTab(tab);
-          }
-        }
+        SessionStore.reviveAllCrashedTabs();
         break;
       }
     }, false, true);
