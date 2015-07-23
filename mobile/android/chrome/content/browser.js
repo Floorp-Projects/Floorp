e71e33edd1558cb3e9eb940c29d699bc2efd3e07
Olli Pettay <Olli.Pettay@helsinki.fi>
2015-07-23 19:23:49 +0300

    Bug 1180382 - update display port after, not before Gecko has scrolled the page to the #hashtag.  r=kats, a=ritu

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -4687,7 +4687,9 @@ Tab.prototype = {
       this.contentDocumentIsDisplayed = false;
       this.hasTouchListener = false;
     } else {
-      this.sendViewportUpdate();
+      setTimeout(function() {
+        this.sendViewportUpdate();
+      }.bind(this), 0);
     }
   },
 
