64064843d390207586b6b97152838dcdc5fcb29e
Eric Rahm <erahm@mozilla.com>
2015-07-23 11:02:45 -0700

    Bug 1184293 - Don't call ThirdPartyUtil::GetBaseDomain if aUri is null. r=jduell
    Return early if aUri is null so as to avoid warnings about it.

--- dom/base/ThirdPartyUtil.cpp
+++ dom/base/ThirdPartyUtil.cpp
@@ -344,6 +344,10 @@ NS_IMETHODIMP
 ThirdPartyUtil::GetBaseDomain(nsIURI* aHostURI,
                               nsACString& aBaseDomain)
 {
+  if (!aHostURI) {
+    return NS_ERROR_INVALID_ARG;
+  }
+
   // Get the base domain. this will fail if the host contains a leading dot,
   // more than one trailing dot, or is otherwise malformed.
   nsresult rv = mTLDService->GetBaseDomain(aHostURI, 0, aBaseDomain);
