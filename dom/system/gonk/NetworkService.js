f718e9cb41ff370be33f13cee48b61b2cf35d8f9
Albert Crespell <alberto.crespell@gmail.com>
2015-07-24 13:16:02 +0200

    Bug 1177236 - Usage alert doesn't work when tethering is enabled. r=ethan

--- dom/system/gonk/NetworkService.js
+++ dom/system/gonk/NetworkService.js
@@ -269,6 +269,58 @@ NetworkService.prototype = {
     });
   },
 
+  setNetworkTetheringAlarm(aEnable, aInterface) {
+    // Method called when enabling disabling tethering, it checks if there is
+    // some alarm active and move from interfaceAlarm to globalAlarm because
+    // interfaceAlarm doens't work in tethering scenario due to forwarding.
+    debug("setNetworkTetheringAlarm for tethering" + aEnable);
+
+    let filename = aEnable ? "/proc/net/xt_quota/" + aInterface + "Alert" :
+                             "/proc/net/xt_quota/globalAlert";
+
+    let file = new FileUtils.File(filename);
+    if (!file) {
+      return;
+    }
+
+    NetUtil.asyncFetch({
+      uri: NetUtil.newURI(file),
+      loadUsingSystemPrincipal: true
+    }, (inputStream, status) => {
+      if (Components.isSuccessCode(status)) {
+        let data = NetUtil.readInputStreamToString(inputStream, inputStream.available())
+                          .split("\n");
+        if (data) {
+          let threshold = parseInt(data[0], 10);
+
+          this._setNetworkTetheringAlarm(aEnable, aInterface, threshold);
+        }
+      }
+    });
+  },
+
+  _setNetworkTetheringAlarm(aEnable, aInterface, aThreshold, aCallback) {
+    debug("_setNetworkTetheringAlarm for tethering" + aEnable);
+
+    let cmd = aEnable ? "setTetheringAlarm" : "removeTetheringAlarm";
+
+    let params = {
+      cmd: cmd,
+      ifname: aInterface,
+      threshold: aThreshold,
+    };
+
+    this.controlMessage(params, function(aData) {
+      let code = aData.resultCode;
+      let reason = aData.resultReason;
+      let enableString = aEnable ? "Enable" : "Disable";
+        debug(enableString + " tethering Alarm result: Code " + code + " reason " + reason);
+        if (aCallback) {
+          aCallback.networkUsageAlarmResult(null);
+        }
+    });
+  },
+
   setNetworkInterfaceAlarm: function(aInterfaceName, aThreshold, aCallback) {
     if (!aInterfaceName) {
       aCallback.networkUsageAlarmResult(-1);
@@ -286,7 +338,26 @@ NetworkService.prototype = {
         return
       }
 
-      self._setNetworkInterfaceAlarm(aInterfaceName, aThreshold, aCallback);
+      // Check if tethering is enabled
+      let params = {
+        cmd: "getTetheringStatus"
+      };
+
+      self.controlMessage(params, function(aResult) {
+        if (isError(aResult.resultCode)) {
+          aCallback.networkUsageAlarmResult(aResult.reason);
+          return;
+        }
+
+        if (aResult.resultReason.indexOf('started') == -1) {
+          // Tethering disabled, set interfaceAlarm
+          self._setNetworkInterfaceAlarm(aInterfaceName, aThreshold, aCallback);
+          return;
+        }
+
+        // Tethering enabled, set globalAlarm
+        self._setNetworkTetheringAlarm(true, aInterfaceName, aThreshold, aCallback);
+      });
     });
   },
 
@@ -545,13 +616,15 @@ NetworkService.prototype = {
     aConfig.cmd = "setWifiTethering";
 
     // The callback function in controlMessage may not be fired immediately.
-    this.controlMessage(aConfig, function(aData) {
+    this.controlMessage(aConfig, (aData) => {
       let code = aData.resultCode;
       let reason = aData.resultReason;
       let enable = aData.enable;
       let enableString = aEnable ? "Enable" : "Disable";
 
       debug(enableString + " Wifi tethering result: Code " + code + " reason " + reason);
+
+      this.setNetworkTetheringAlarm(aEnable, aConfig.externalIfname);
 
       if (isError(code)) {
         aCallback.wifiTetheringEnabledChange("netd command error");
@@ -565,13 +638,15 @@ NetworkService.prototype = {
   setUSBTethering: function(aEnable, aConfig, aCallback) {
     aConfig.cmd = "setUSBTethering";
     // The callback function in controlMessage may not be fired immediately.
-    this.controlMessage(aConfig, function(aData) {
+    this.controlMessage(aConfig, (aData) => {
       let code = aData.resultCode;
       let reason = aData.resultReason;
       let enable = aData.enable;
       let enableString = aEnable ? "Enable" : "Disable";
 
       debug(enableString + " USB tethering result: Code " + code + " reason " + reason);
+
+      this.setNetworkTetheringAlarm(aEnable, aConfig.externalIfname);
 
       if (isError(code)) {
         aCallback.usbTetheringEnabledChange("netd command error");
