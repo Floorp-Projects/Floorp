74f89c30a1a94c6ca70919b6882cc19c6fa50916
George Wright <george@mozilla.com>
2015-07-24 14:52:20 -0400

    Bug 1184276 - Add some logging and don't bail if we can't find an nsPluginTag in RecvGetBlocklistState to try and get more diagnostic info r=billm

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -1121,12 +1121,15 @@ ContentParent::RecvGetBlocklistState(const uint32_t& aPluginId,
 
     nsRefPtr<nsPluginHost> pluginHost = nsPluginHost::GetInst();
     if (!pluginHost) {
+        NS_WARNING("Plugin host not found");
         return false;
     }
     nsPluginTag* tag =  pluginHost->PluginWithId(aPluginId);
 
     if (!tag) {
-        return false;
+        // Default state is blocked anyway
+        NS_WARNING("Plugin tag not found. This should never happen, but to avoid a crash we're forcibly blocking it");
+        return true;
     }
 
     return NS_SUCCEEDED(tag->GetBlocklistState(aState));
