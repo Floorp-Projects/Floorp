be5d22a16af4b0c8deaa5d64ed19dc34cc6edeac
Mark Banner <standard8@mozilla.com>
2015-07-24 09:01:58 +0100

    Bug 1186541 - Fix a warning in StandaloneRoomView.shouldRenderRemoteVideo when the roomState is gather. r=mikedeboer

--- browser/components/loop/content/js/roomViews.jsx
+++ browser/components/loop/content/js/roomViews.jsx
@@ -638,6 +638,7 @@ loop.roomViews = (function(mozL10n) {
           return true;
 
         case ROOM_STATES.READY:
+        case ROOM_STATES.GATHER:
         case ROOM_STATES.INIT:
         case ROOM_STATES.JOINING:
         case ROOM_STATES.SESSION_CONNECTED:
