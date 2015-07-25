b96edf057a149999c41a0ed8dfddee87a936ce85
Michael Comella <michael.l.comella@gmail.com>
2015-07-24 17:09:35 -0700

    Bug 1188557 - Update ThemedView template to import the correct LWT. r=mhaigh
    I believe I just used an IDE to update the files last time, meaning the
    template never got updated.
    
    This was originally implemented but no longer used in bug 1177611.
    
    --HG--
    extra : commitid : 6Wp4qTCjyTk
    extra : rebase_source : 215f9eef9e203a4e2cbf26051204c6b25bdf3779
    extra : source : 14e9ba975f55242c773420d77143717acf7f8456

--- mobile/android/base/widget/ThemedImageButton.java
+++ mobile/android/base/widget/ThemedImageButton.java
@@ -7,8 +7,8 @@
 package org.mozilla.gecko.widget;
 
 import org.mozilla.gecko.GeckoApplication;
-import org.mozilla.gecko.R;
 import org.mozilla.gecko.lwt.LightweightTheme;
+import org.mozilla.gecko.R;
 
 import android.content.Context;
 import android.content.res.TypedArray;
