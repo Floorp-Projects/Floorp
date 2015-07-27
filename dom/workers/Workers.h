6e916540f5b9ed5afd743ffd99026e220947accf
Ehsan Akhgari <ehsan@mozilla.com>
2015-07-27 15:35:28 -0400

    Bug 1188091 - Fix the exposure of Push interfaces; r=dougt,bzbarsky
    Currently we don't check the dom.push.enabled pref in some cases for
    some of these interfaces.  This patch unifies how all of these
    interfaces are exposed to Window, Worker, and ServiceWorker.

--- dom/workers/Workers.h
+++ dom/workers/Workers.h
@@ -205,6 +205,7 @@ enum WorkerPreference
   WORKERPREF_DOM_CACHES_TESTING, // dom.caches.testing.enabled
   WORKERPREF_SERVICEWORKERS_TESTING, // dom.serviceWorkers.testing.enabled
   WORKERPREF_INTERCEPTION_OPAQUE_ENABLED, // dom.serviceWorkers.interception.opaque.enabled
+  WORKERPREF_PUSH, // dom.push.enabled
   WORKERPREF_COUNT
 };
 
