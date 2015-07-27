a35c25d602eee97b32eb2a34d0e0e68bd4af3be8
Ryan VanderMeulen <ryanvm@gmail.com>
2015-07-27 13:54:56 -0400

    Backed out changeset adbd8a7608e4 (bug 1176363) for mochitest-gl permacrashes.

--- gfx/layers/d3d11/TextureD3D11.h
+++ gfx/layers/d3d11/TextureD3D11.h
@@ -64,8 +64,6 @@ public:
 
   virtual gfx::DrawTarget* BorrowDrawTarget() override;
 
-  virtual void UpdateFromSurface(gfx::DataSourceSurface* aSurface) override;
-
   virtual bool AllocateForSurface(gfx::IntSize aSize,
                                   TextureAllocationFlags aFlags = ALLOC_DEFAULT) override;
 
