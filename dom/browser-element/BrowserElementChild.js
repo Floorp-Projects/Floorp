f86edb6f4fcac36ba95ea40ef608ccabaabc27b6
Jeremy Chen <jeremychen@mozilla.com>
2015-07-27 11:56:04 +0800

    Bug 1147329 - Handle in-process case for Cut/Copy/Paste feature. r=kanru

--- dom/browser-element/BrowserElementChild.js
+++ dom/browser-element/BrowserElementChild.js
@@ -41,7 +41,15 @@ if (!('BrowserElementIsPreloaded' in this)) {
       } catch (e) {
       }
     }
+  }
 
+  if(Services.appinfo.processType == Services.appinfo.PROCESS_TYPE_CONTENT) {
+    // general content apps
+    if (isTopBrowserElement(docShell)) {
+      Services.scriptloader.loadSubScript("chrome://global/content/BrowserElementCopyPaste.js");
+    }
+  } else {
+    // rocketbar in system app and other in-process case (ex. B2G desktop client)
     Services.scriptloader.loadSubScript("chrome://global/content/BrowserElementCopyPaste.js");
   }
 
