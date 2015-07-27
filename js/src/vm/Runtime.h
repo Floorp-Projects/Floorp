6aab24a4ece925b858065043490d158e663f352d
David Rajchenbach-Teller <dteller@mozilla.com>
2015-07-27 23:01:42 +0200

    Bug 1190074 - PerformanceGroup now uses mozilla::RefPtr;r=jandem

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -1514,6 +1514,11 @@ struct JSRuntime : public JS::shadow::Runtime,
         }
 
         /**
+         * Performance data on the entire runtime.
+         */
+        js::PerformanceGroupHolder performance;
+
+        /**
          * The number of times we have entered the event loop.
          * Used to reset counters whenever we enter the loop,
          * which may be caused either by having completed the
@@ -1523,17 +1528,6 @@ struct JSRuntime : public JS::shadow::Runtime,
          * Always incremented by 1, may safely overflow.
          */
         uint64_t iteration;
-
-        /**
-         * `true` if no stopwatch has been registered for the
-         * current run of the event loop, `false` until then.
-         */
-        bool isEmpty;
-
-        /**
-         * Performance data on the entire runtime.
-         */
-        js::PerformanceData performance;
 
         /**
          * Callback used to ask the embedding to determine in which
@@ -1547,9 +1541,9 @@ struct JSRuntime : public JS::shadow::Runtime,
          */
         JSCurrentPerfGroupCallback currentPerfGroupCallback;
 
-        Stopwatch()
-          : iteration(0)
-          , isEmpty(true)
+        explicit Stopwatch(JSRuntime* runtime)
+          : performance(runtime)
+          , iteration(0)
           , currentPerfGroupCallback(nullptr)
           , isMonitoringJank_(false)
           , isMonitoringCPOW_(false)
@@ -1566,7 +1560,6 @@ struct JSRuntime : public JS::shadow::Runtime,
          */
         void reset() {
             ++iteration;
-            isEmpty = true;
         }
         /**
          * Activate/deactivate stopwatch measurement of jank.
@@ -1653,6 +1646,9 @@ struct JSRuntime : public JS::shadow::Runtime,
         MonotonicTimeStamp userTimeFix;
 
     private:
+        Stopwatch(const Stopwatch&) = delete;
+        Stopwatch& operator=(const Stopwatch&) = delete;
+
         Groups groups_;
         friend struct js::PerformanceGroupHolder;
 
