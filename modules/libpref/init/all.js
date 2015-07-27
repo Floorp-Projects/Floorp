bf53a49eb2892fe28c44f9d073753b31cef92498
Ting-Yu Lin <tlin@mozilla.com>
2015-07-27 11:56:05 +0800

    Bug 1172382 - Enable AccessibleCaret on B2G. r=roc
    The new AccessibleCaret replaces TouchCaret and SelectionCarets. Disable
    them accordingly.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4739,7 +4739,7 @@ pref("browser.trackingprotection.gethashURL", "https://tracking.services.mozilla
 // Turn off Spatial navigation by default.
 pref("snav.enabled", false);
 
-// Turn off touch caret by default.
+// Original caret implementation on collapsed selection.
 pref("touchcaret.enabled", false);
 
 // This will inflate the size of the touch caret frame when checking if user
@@ -4751,7 +4751,7 @@ pref("touchcaret.inflatesize.threshold", 40);
 // In milliseconds. (0 means disable this feature)
 pref("touchcaret.expiration.time", 3000);
 
-// Turn off selection caret by default
+// Original caret implementation on non-collapsed selection.
 pref("selectioncaret.enabled", false);
 
 // This will inflate size of selection caret frame when we checking if
