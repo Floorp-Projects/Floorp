7580c189b082ad7a3ded1c606064c27e02973d6e
Jacek Caban <jacek@codeweavers.com>
2015-07-28 17:47:33 +0200

    Bug 1176731 - Don't mark static inline functions as MFBT_API in TimeStamp.h. r=Waldo

--- mozglue/misc/TimeStamp.h
+++ mozglue/misc/TimeStamp.h
@@ -426,8 +426,8 @@ public:
    * lower precision, usually 15.6 ms, but with very good performance benefit.
    * Use it for measurements of longer times, like >200ms timeouts.
    */
-  static MFBT_API TimeStamp Now() { return Now(true); }
-  static MFBT_API TimeStamp NowLoRes() { return Now(false); }
+  static TimeStamp Now() { return Now(true); }
+  static TimeStamp NowLoRes() { return Now(false); }
 
   /**
    * Return a timestamp representing the time when the current process was
