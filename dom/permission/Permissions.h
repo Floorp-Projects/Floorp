4b7e241a667c0e35920d26a6c80ad493eccaf825
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-07-28 06:33:46 -0700

    Bug 1105827 - Part 2: Add stub Permissions implementation. r=baku

---
+++ dom/permission/Permissions.h
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_Permissions_h_
+#define mozilla_dom_Permissions_h_
+
+#include "nsISupports.h"
+#include "nsPIDOMWindow.h"
+#include "nsWrapperCache.h"
+
+namespace mozilla {
+namespace dom {
+
+struct PermissionDescriptor;
+class Promise;
+
+class Permissions final
+  : public nsISupports
+  , public nsWrapperCache
+{
+public:
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(Permissions)
+
+  explicit Permissions(nsPIDOMWindow* aWindow);
+
+  nsPIDOMWindow* GetParentObject() const { return mWindow; }
+
+  JSObject* WrapObject(JSContext* aCx,
+                       JS::Handle<JSObject*> aGivenProto) override;
+
+  already_AddRefed<Promise> Query(const PermissionDescriptor& aPermission);
+
+private:
+  ~Permissions();
+
+  nsCOMPtr<nsPIDOMWindow> mWindow;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_permissions_h_
---
+++ dom/webidl/Permissions.webidl
@@ -0,0 +1,25 @@
+/* -*- Mode: IDL; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * The origin of this IDL file is
+ * https://w3c.github.io/permissions/#permissions-interface
+ */
+
+enum PermissionName {
+  "geolocation",
+  "notifications",
+  "push",
+  "midi"
+};
+
+dictionary PermissionDescriptor {
+  required PermissionName name;
+};
+
+[Exposed=(Window),
+ Pref="dom.permissions.enabled"]
+interface Permissions {
+  Promise<PermissionStatus> query(PermissionDescriptor permission);
+};
