8c8c620553063e3a9177a2f9a7cdef2b1e91c812
Wes Kocher <wkocher@mozilla.com>
2015-07-27 17:52:01 -0700

    Backed out 8 changesets (bug 1028418) for assertions in SavedStacks.cpp:103 CLOSED TREE
    Backed out changeset 8f20146ce3c8 (bug 1028418)
    Backed out changeset f6e78ff75f4b (bug 1028418)
    Backed out changeset caf840e71590 (bug 1028418)
    Backed out changeset ba47cb00a938 (bug 1028418)
    Backed out changeset 3f298220d712 (bug 1028418)
    Backed out changeset 7c2555a6e32e (bug 1028418)
    Backed out changeset ed9287cd152a (bug 1028418)
    Backed out changeset 88a5c0415403 (bug 1028418)

--- js/src/gc/Rooting.h
+++ js/src/gc/Rooting.h
@@ -20,7 +20,6 @@ class ArrayObject;
 class GlobalObject;
 class PlainObject;
 class ScriptSourceObject;
-class SavedFrame;
 class Shape;
 class ObjectGroup;
 
@@ -34,14 +33,12 @@ typedef JS::Handle<JSLinearString*>    HandleLinearString;
 typedef JS::Handle<PropertyName*>      HandlePropertyName;
 typedef JS::Handle<ArrayObject*>       HandleArrayObject;
 typedef JS::Handle<PlainObject*>       HandlePlainObject;
-typedef JS::Handle<SavedFrame*>        HandleSavedFrame;
 typedef JS::Handle<ScriptSourceObject*> HandleScriptSource;
 
 typedef JS::MutableHandle<Shape*>      MutableHandleShape;
 typedef JS::MutableHandle<JSAtom*>     MutableHandleAtom;
 typedef JS::MutableHandle<NativeObject*> MutableHandleNativeObject;
 typedef JS::MutableHandle<PlainObject*> MutableHandlePlainObject;
-typedef JS::MutableHandle<SavedFrame*> MutableHandleSavedFrame;
 
 typedef JS::Rooted<NativeObject*>      RootedNativeObject;
 typedef JS::Rooted<Shape*>             RootedShape;
@@ -52,7 +49,6 @@ typedef JS::Rooted<PropertyName*>      RootedPropertyName;
 typedef JS::Rooted<ArrayObject*>       RootedArrayObject;
 typedef JS::Rooted<GlobalObject*>      RootedGlobalObject;
 typedef JS::Rooted<PlainObject*>       RootedPlainObject;
-typedef JS::Rooted<SavedFrame*>        RootedSavedFrame;
 typedef JS::Rooted<ScriptSourceObject*> RootedScriptSource;
 
 } /* namespace js */
--- js/src/jit/JitFrames.h
+++ js/src/jit/JitFrames.h
@@ -181,10 +181,9 @@ class OsiIndex
 //   .. locals ..
 
 // The descriptor is organized into three sections:
-// [ frame size | has cached saved frame bit | frame type ]
+// [ frame size | constructing bit | frame type ]
 // < highest - - - - - - - - - - - - - - lowest >
-static const uintptr_t FRAMESIZE_SHIFT = 5;
-static const uintptr_t HASCACHEDSAVEDFRAME_BIT = 1 << 4;
+static const uintptr_t FRAMESIZE_SHIFT = 4;
 static const uintptr_t FRAMETYPE_BITS = 4;
 
 // Ion frames have a few important numbers associated with them:
@@ -283,7 +282,7 @@ void UpdateJitActivationsForMinorGC(JSRuntime* rt, JSTracer* trc);
 static inline uint32_t
 MakeFrameDescriptor(uint32_t frameSize, FrameType type)
 {
-    return 0 | (frameSize << FRAMESIZE_SHIFT) | type;
+    return (frameSize << FRAMESIZE_SHIFT) | type;
 }
 
 // Returns the JSScript associated with the topmost JIT frame.
@@ -344,13 +343,7 @@ class CommonFrameLayout
         return descriptor_ >> FRAMESIZE_SHIFT;
     }
     void setFrameDescriptor(size_t size, FrameType type) {
-        descriptor_ = 0 | (size << FRAMESIZE_SHIFT) | type;
-    }
-    bool hasCachedSavedFrame() const {
-        return descriptor_ & HASCACHEDSAVEDFRAME_BIT;
-    }
-    void setHasCachedSavedFrame() {
-        descriptor_ |= HASCACHEDSAVEDFRAME_BIT;
+        descriptor_ = (size << FRAMESIZE_SHIFT) | type;
     }
     uint8_t* returnAddress() const {
         return returnAddress_;
