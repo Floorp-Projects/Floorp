7b4b22281e706ab2e25e4ac3b3dd245382341e56
Mike Shal <mshal@mozilla.com>
2015-07-28 14:41:10 -0400

    Bug 1175655 - mozharness should use full hg revision; r=jlund

--- testing/mozharness/mozharness/mozilla/building/buildb2gbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildb2gbase.py
@@ -193,7 +193,7 @@ class B2GBuildBaseScript(BuildbotMixin, MockMixin,
                 )
             else:
                 return None
-        return revision[0:12] if revision else None
+        return revision
 
     def query_gecko_config_path(self):
         conf_file = self.config.get('gecko_config')
