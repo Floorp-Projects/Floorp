c135b555a77b5ca35e253fe8d941813462d62189
Paul Adenot <paul@paul.cx>
2015-07-29 18:24:15 +0200

    Bug 901633 - Part 2 - Make AudioChannelFormat and AudioSegment more generic.  r=roc
    --HG--
    extra : rebase_source : 2db81db6341466607917070eaf9a9a9d66a04059

--- dom/media/AudioSegment.cpp
+++ dom/media/AudioSegment.cpp
@@ -5,7 +5,6 @@
 
 #include "AudioSegment.h"
 
-#include "AudioStream.h"
 #include "AudioMixer.h"
 #include "AudioChannelFormat.h"
 #include "Latency.h"
@@ -13,49 +12,18 @@
 
 namespace mozilla {
 
-template <class SrcT, class DestT>
-static void
-InterleaveAndConvertBuffer(const SrcT** aSourceChannels,
-                           int32_t aLength, float aVolume,
-                           int32_t aChannels,
-                           DestT* aOutput)
+const uint8_t SilentChannel::gZeroChannel[MAX_AUDIO_SAMPLE_SIZE*SilentChannel::AUDIO_PROCESSING_FRAMES] = {0};
+
+template<>
+const float* SilentChannel::ZeroChannel<float>()
 {
-  DestT* output = aOutput;
-  for (int32_t i = 0; i < aLength; ++i) {
-    for (int32_t channel = 0; channel < aChannels; ++channel) {
-      float v = AudioSampleToFloat(aSourceChannels[channel][i])*aVolume;
-      *output = FloatToAudioSample<DestT>(v);
-      ++output;
-    }
-  }
+  return reinterpret_cast<const float*>(SilentChannel::gZeroChannel);
 }
 
-void
-InterleaveAndConvertBuffer(const void** aSourceChannels,
-                           AudioSampleFormat aSourceFormat,
-                           int32_t aLength, float aVolume,
-                           int32_t aChannels,
-                           AudioDataValue* aOutput)
+template<>
+const int16_t* SilentChannel::ZeroChannel<int16_t>()
 {
-  switch (aSourceFormat) {
-  case AUDIO_FORMAT_FLOAT32:
-    InterleaveAndConvertBuffer(reinterpret_cast<const float**>(aSourceChannels),
-                               aLength,
-                               aVolume,
-                               aChannels,
-                               aOutput);
-    break;
-  case AUDIO_FORMAT_S16:
-    InterleaveAndConvertBuffer(reinterpret_cast<const int16_t**>(aSourceChannels),
-                               aLength,
-                               aVolume,
-                               aChannels,
-                               aOutput);
-    break;
-   case AUDIO_FORMAT_SILENCE:
-    // nothing to do here.
-    break;
-  }
+  return reinterpret_cast<const int16_t*>(SilentChannel::gZeroChannel);
 }
 
 void
@@ -64,54 +32,6 @@ AudioSegment::ApplyVolume(float aVolume)
   for (ChunkIterator ci(*this); !ci.IsEnded(); ci.Next()) {
     ci->mVolume *= aVolume;
   }
-}
-
-static const int AUDIO_PROCESSING_FRAMES = 640; /* > 10ms of 48KHz audio */
-static const uint8_t gZeroChannel[MAX_AUDIO_SAMPLE_SIZE*AUDIO_PROCESSING_FRAMES] = {0};
-
-void
-DownmixAndInterleave(const nsTArray<const void*>& aChannelData,
-                     AudioSampleFormat aSourceFormat, int32_t aDuration,
-                     float aVolume, uint32_t aOutputChannels,
-                     AudioDataValue* aOutput)
-{
-  nsAutoTArray<const void*,GUESS_AUDIO_CHANNELS> channelData;
-  nsAutoTArray<float,AUDIO_PROCESSING_FRAMES*GUESS_AUDIO_CHANNELS> downmixConversionBuffer;
-  nsAutoTArray<float,AUDIO_PROCESSING_FRAMES*GUESS_AUDIO_CHANNELS> downmixOutputBuffer;
-
-  channelData.SetLength(aChannelData.Length());
-  if (aSourceFormat != AUDIO_FORMAT_FLOAT32) {
-    NS_ASSERTION(aSourceFormat == AUDIO_FORMAT_S16, "unknown format");
-    downmixConversionBuffer.SetLength(aDuration*aChannelData.Length());
-    for (uint32_t i = 0; i < aChannelData.Length(); ++i) {
-      float* conversionBuf = downmixConversionBuffer.Elements() + (i*aDuration);
-      const int16_t* sourceBuf = static_cast<const int16_t*>(aChannelData[i]);
-      for (uint32_t j = 0; j < (uint32_t)aDuration; ++j) {
-        conversionBuf[j] = AudioSampleToFloat(sourceBuf[j]);
-      }
-      channelData[i] = conversionBuf;
-    }
-  } else {
-    for (uint32_t i = 0; i < aChannelData.Length(); ++i) {
-      channelData[i] = aChannelData[i];
-    }
-  }
-
-  downmixOutputBuffer.SetLength(aDuration*aOutputChannels);
-  nsAutoTArray<float*,GUESS_AUDIO_CHANNELS> outputChannelBuffers;
-  nsAutoTArray<const void*,GUESS_AUDIO_CHANNELS> outputChannelData;
-  outputChannelBuffers.SetLength(aOutputChannels);
-  outputChannelData.SetLength(aOutputChannels);
-  for (uint32_t i = 0; i < (uint32_t)aOutputChannels; ++i) {
-    outputChannelData[i] = outputChannelBuffers[i] =
-        downmixOutputBuffer.Elements() + aDuration*i;
-  }
-  if (channelData.Length() > aOutputChannels) {
-    AudioChannelsDownMix(channelData, outputChannelBuffers.Elements(),
-                         aOutputChannels, aDuration);
-  }
-  InterleaveAndConvertBuffer(outputChannelData.Elements(), AUDIO_FORMAT_FLOAT32,
-                             aDuration, aVolume, aOutputChannels, aOutput);
 }
 
 void AudioSegment::ResampleChunks(SpeexResamplerState* aResampler, uint32_t aInRate, uint32_t aOutRate)
@@ -165,9 +85,9 @@ void
 AudioSegment::Mix(AudioMixer& aMixer, uint32_t aOutputChannels,
                   uint32_t aSampleRate)
 {
-  nsAutoTArray<AudioDataValue, AUDIO_PROCESSING_FRAMES* GUESS_AUDIO_CHANNELS>
+  nsAutoTArray<AudioDataValue, SilentChannel::AUDIO_PROCESSING_FRAMES* GUESS_AUDIO_CHANNELS>
   buf;
-  nsAutoTArray<const void*, GUESS_AUDIO_CHANNELS> channelData;
+  nsAutoTArray<const AudioDataValue*, GUESS_AUDIO_CHANNELS> channelData;
   uint32_t offsetSamples = 0;
   uint32_t duration = GetDuration();
 
@@ -197,11 +117,11 @@ AudioSegment::Mix(AudioMixer& aMixer, uint32_t aOutputChannels,
       // desired input and output channels.
       channelData.SetLength(c.mChannelData.Length());
       for (uint32_t i = 0; i < channelData.Length(); ++i) {
-        channelData[i] = c.mChannelData[i];
+        channelData[i] = static_cast<const AudioDataValue*>(c.mChannelData[i]);
       }
       if (channelData.Length() < aOutputChannels) {
         // Up-mix.
-        AudioChannelsUpMix(&channelData, aOutputChannels, gZeroChannel);
+        AudioChannelsUpMix(&channelData, aOutputChannels, SilentChannel::ZeroChannel<AudioDataValue>());
         for (uint32_t channel = 0; channel < aOutputChannels; channel++) {
           AudioDataValue* ptr =
             PointerForOffsetInChannel(buf.Elements(), outBufferLength,
@@ -246,9 +166,8 @@ AudioSegment::Mix(AudioMixer& aMixer, uint32_t aOutputChannels,
 void
 AudioSegment::WriteTo(uint64_t aID, AudioMixer& aMixer, uint32_t aOutputChannels, uint32_t aSampleRate)
 {
-  nsAutoTArray<AudioDataValue,AUDIO_PROCESSING_FRAMES*GUESS_AUDIO_CHANNELS> buf;
-  nsAutoTArray<const void*,GUESS_AUDIO_CHANNELS> channelData;
-  // Offset in the buffer that will end up sent to the AudioStream, in samples.
+  nsAutoTArray<AudioDataValue,SilentChannel::AUDIO_PROCESSING_FRAMES*GUESS_AUDIO_CHANNELS> buf;
+  // Offset in the buffer that will be written to the mixer, in samples.
   uint32_t offset = 0;
 
   if (GetDuration() <= 0) {
@@ -262,39 +181,23 @@ AudioSegment::WriteTo(uint64_t aID, AudioMixer& aMixer, uint32_t aOutputChannels
 
   for (ChunkIterator ci(*this); !ci.IsEnded(); ci.Next()) {
     AudioChunk& c = *ci;
-    uint32_t frames = c.mDuration;
 
-    // If we have written data in the past, or we have real (non-silent) data
-    // to write, we can proceed. Otherwise, it means we just started the
-    // AudioStream, and we don't have real data to write to it (just silence).
-    // To avoid overbuffering in the AudioStream, we simply drop the silence,
-    // here. The stream will underrun and output silence anyways.
-    if (c.mBuffer && c.mBufferFormat != AUDIO_FORMAT_SILENCE) {
-      channelData.SetLength(c.mChannelData.Length());
-      for (uint32_t i = 0; i < channelData.Length(); ++i) {
-        channelData[i] = c.mChannelData[i];
-      }
-      if (channelData.Length() < aOutputChannels) {
-        // Up-mix. Note that this might actually make channelData have more
-        // than aOutputChannels temporarily.
-        AudioChannelsUpMix(&channelData, aOutputChannels, gZeroChannel);
-      }
-      if (channelData.Length() > aOutputChannels) {
-        // Down-mix.
-        DownmixAndInterleave(channelData, c.mBufferFormat, frames,
-                             c.mVolume, aOutputChannels, buf.Elements() + offset);
-      } else {
-        InterleaveAndConvertBuffer(channelData.Elements(), c.mBufferFormat,
-                                   frames, c.mVolume,
-                                   aOutputChannels,
-                                   buf.Elements() + offset);
-      }
-    } else {
-      // Assumes that a bit pattern of zeroes == 0.0f
-      memset(buf.Elements() + offset, 0, aOutputChannels * frames * sizeof(AudioDataValue));
+    switch (c.mBufferFormat) {
+      case AUDIO_FORMAT_S16:
+        WriteChunk<int16_t>(c, aOutputChannels, buf.Elements() + offset);
+        break;
+      case AUDIO_FORMAT_FLOAT32:
+        WriteChunk<float>(c, aOutputChannels, buf.Elements() + offset);
+        break;
+      case AUDIO_FORMAT_SILENCE:
+        // The mixer is expecting interleaved data, so this is ok.
+        PodZero(buf.Elements() + offset, c.mDuration * aOutputChannels);
+        break;
+      default:
+        MOZ_ASSERT(false, "Not handled");
     }
 
-    offset += frames * aOutputChannels;
+    offset += c.mDuration * aOutputChannels;
 
 #if !defined(MOZILLA_XPCOMRT_API)
     if (!c.mTimeStamp.IsNull()) {
