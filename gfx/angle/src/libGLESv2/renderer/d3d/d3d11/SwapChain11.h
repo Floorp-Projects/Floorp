624f2491722c7da65c9f67c3de64d5a7f44bb20e
Jeff Muizelaar <jmuizelaar@mozilla.com>
2015-07-29 10:44:05 -0400

    Bug 1186431. Backout "Recreate the RenderTargetView on demand" r=jgilbert
    This reverts commit 8d5da151b0df

--- gfx/angle/src/libGLESv2/renderer/d3d/d3d11/SwapChain11.h
+++ gfx/angle/src/libGLESv2/renderer/d3d/d3d11/SwapChain11.h
@@ -39,7 +39,6 @@ class SwapChain11 : public SwapChain
     EGLint getWidth() const { return mWidth; }
     EGLint getHeight() const { return mHeight; }
     virtual void* getKeyedMutex() { return mKeyedMutex; };
-    virtual void releaseSync();
 
     static SwapChain11 *makeSwapChain11(SwapChain *swapChain);
 
