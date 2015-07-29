d28802036d4137b04d1231857175c693390985b3
David Rajchenbach-Teller <dteller@mozilla.com>
2015-07-29 19:01:05 +0200

    Bug 1181175 - Telemetry for finding out how often our process is rescheduled to another CPU. r=jandem, r=bsmedberg CLOSED TREE

--- toolkit/components/perfmonitoring/nsPerformanceStats.h
+++ toolkit/components/perfmonitoring/nsPerformanceStats.h
@@ -6,13 +6,16 @@
 #ifndef nsPerformanceStats_h
 #define nsPerformanceStats_h
 
+#include "nsIObserver.h"
+
 #include "nsIPerformanceStats.h"
 
-class nsPerformanceStatsService : public nsIPerformanceStatsService
+class nsPerformanceStatsService : public nsIPerformanceStatsService, nsIObserver
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIPERFORMANCESTATSSERVICE
+  NS_DECL_NSIOBSERVER
 
   nsPerformanceStatsService();
 
@@ -20,6 +23,8 @@ private:
   virtual ~nsPerformanceStatsService();
 
   const uint64_t mProcessId;
+  uint64_t mProcessStayed;
+  uint64_t mProcessMoved;
 protected:
 };
 
