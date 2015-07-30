52ee71547cdcbf7c0da5df53c5182f623d9df384
Arnaud Bienner <arnaud.bienner@gmail.com>
2015-07-30 19:45:25 +0200

    Bug 1186603 - Add a "Contains" method for string classes and use it in HTMLInputElement. r=nfroyd

--- dom/html/HTMLInputElement.cpp
+++ dom/html/HTMLInputElement.cpp
@@ -7213,7 +7213,7 @@ HTMLInputElement::SetFilePickerFiltersFromAccept(nsIFilePicker* filePicker)
       filterBundle->GetStringFromName(MOZ_UTF16("videoFilter"),
                                       getter_Copies(extensionListStr));
     } else if (token.First() == '.') {
-      if (token.FindChar(';') >= 0  || token.FindChar('*') >= 0) {
+      if (token.Contains(';') || token.Contains('*')) {
         // Ignore this filter as it contains reserved characters
         continue;
       }
