d636d52e607163abf6ed794013ebccfac6c9234c
Nick Robson <nicko.robson@gmail.com>
2015-07-30 15:00:00 +0200

    Bug 1075089 - Moved popup menu frame offset to LookAndFeel, fixed default offset for OS X. r=enn

--- widget/gtk/nsLookAndFeel.cpp
+++ widget/gtk/nsLookAndFeel.cpp
@@ -667,6 +667,10 @@ nsLookAndFeel::GetIntImpl(IntID aID, int32_t &aResult)
     case eIntID_ColorPickerAvailable:
         aResult = 1;
         break;
+    case eIntID_ContextMenuOffsetVertical:
+    case eIntID_ContextMenuOffsetHorizontal:
+        aResult = 2;
+        break;
     default:
         aResult = 0;
         res     = NS_ERROR_FAILURE;
