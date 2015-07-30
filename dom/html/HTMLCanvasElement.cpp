d2d60e2ca1c8192151f15a4112819c241b634cea
Kaku Kuo <tkuo@mozilla.com>
2015-07-30 20:49:00 +0200

    Bug 1044102 - Part 2 - Support ImageBitmap as CanvasImageSource. r=smaug

--- dom/html/HTMLCanvasElement.cpp
+++ dom/html/HTMLCanvasElement.cpp
@@ -45,13 +45,6 @@ using namespace mozilla::gfx;
 
 NS_IMPL_NS_NEW_HTML_ELEMENT(Canvas)
 
-namespace {
-
-typedef mozilla::dom::HTMLImageElementOrHTMLCanvasElementOrHTMLVideoElement
-HTMLImageOrCanvasOrVideoElement;
-
-} // namespace
-
 namespace mozilla {
 namespace dom {
 
@@ -283,10 +276,10 @@ HTMLCanvasElement::CopyInnerTo(Element* aDest)
     nsRefPtr<CanvasRenderingContext2D> context2d =
       static_cast<CanvasRenderingContext2D*>(cxt.get());
     if (context2d && !mPrintCallback) {
-      HTMLImageOrCanvasOrVideoElement element;
-      element.SetAsHTMLCanvasElement() = this;
+      CanvasImageSource source;
+      source.SetAsHTMLCanvasElement() = this;
       ErrorResult err;
-      context2d->DrawImage(element,
+      context2d->DrawImage(source,
                            0.0, 0.0, err);
       rv = err.StealNSResult();
     }
