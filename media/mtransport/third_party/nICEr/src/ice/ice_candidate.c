ec3ba81d1ebf69d07ec8b31b0a71c17d07ff1134
Nils Ohlmeier [:drno] <drno@ohlmeier.org>
2015-07-31 11:23:26 -0700

    Bug 1189198 - don't start STUN transactions with a protocol mis-match. r=mtseng
    --HG--
    extra : rebase_source : 9c51c1395dc99a19ae32ba79a54f8129f2166ae6

--- media/mtransport/third_party/nICEr/src/ice/ice_candidate.c
+++ media/mtransport/third_party/nICEr/src/ice/ice_candidate.c
@@ -554,8 +554,8 @@ int nr_ice_candidate_initialize(nr_ice_candidate *cand, NR_async_cb ready_cb, vo
         cand->state=NR_ICE_CAND_STATE_INITIALIZING;
 
         if(cand->stun_server->type == NR_ICE_STUN_SERVER_TYPE_ADDR) {
-          if(cand->base.ip_version != cand->stun_server->u.addr.ip_version) {
-            r_log(LOG_ICE, LOG_INFO, "ICE-CANDIDATE(%s): Skipping srflx/relayed candidate with different IP version (%u) than STUN/TURN server (%u).", cand->label,cand->base.ip_version,cand->stun_server->u.addr.ip_version);
+          if(nr_transport_addr_cmp(&cand->base,&cand->stun_server->u.addr,NR_TRANSPORT_ADDR_CMP_MODE_PROTOCOL)) {
+            r_log(LOG_ICE, LOG_INFO, "ICE-CANDIDATE(%s): Skipping srflx/relayed candidate because of IP version/transport mis-match with STUN/TURN server (%u/%s - %u/%s).", cand->label,cand->base.ip_version,cand->base.protocol==IPPROTO_UDP?"UDP":"TCP",cand->stun_server->u.addr.ip_version,cand->stun_server->u.addr.protocol==IPPROTO_UDP?"UDP":"TCP");
             ABORT(R_NOT_FOUND); /* Same error code when DNS lookup fails */
           }
 
