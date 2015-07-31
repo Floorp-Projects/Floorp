a434eb2d77609232d3db79d65be2e0f5b8054080
Michael Comella <michael.l.comella@gmail.com>
2015-07-30 19:16:41 -0700

    Bug 1189585 - Add DrawableUtil class. r=sebastian
    This includes a method to tint drawables.
    
    --HG--
    extra : commitid : DbqlZdnw4ZH
    extra : rebase_source : 75133c6aa6add290ce62432daaca0a33a145f0a2

---
+++ mobile/android/base/util/DrawableUtil.java
@@ -0,0 +1,21 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.util;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+
+public class DrawableUtil {
+
+    public static Drawable tintDrawable(final Context context, final int drawableID, final int colorID) {
+        final Drawable icon = DrawableCompat.wrap(
+                ContextCompat.getDrawable(context, drawableID).mutate());
+        DrawableCompat.setTint(icon, context.getResources().getColor(colorID));
+        return icon;
+    }
+}
