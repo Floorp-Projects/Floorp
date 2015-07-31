6255fca4d7eb041189209ba5c4f9e8bff55f5046
Mark Capella <markcapella@twcny.rr.com>
2015-07-31 17:25:29 -0400

    Bug 1188741 - Text selection action bar not appearing, r=kats

--- mobile/android/base/DynamicToolbar.java
+++ mobile/android/base/DynamicToolbar.java
@@ -106,6 +106,11 @@ public class DynamicToolbar {
             return;
         }
 
+        // Don't hide the ActionBar/Toolbar, if it's pinned open by TextSelection.
+        if (visible == false && pinFlags.contains(PinReason.ACTION_MODE)) {
+            return;
+        }
+
         final boolean isImmediate = transition == VisibilityTransition.IMMEDIATE;
         if (visible) {
             layerView.getLayerMarginsAnimator().showMargins(isImmediate);
