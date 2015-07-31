c62209ec7df0156437511ec4f0c93bad3c4729a1
Shu-yu Guo <shu@rfrn.org>
2015-07-30 22:17:04 -0700

    Bug 1179063 - Cleanup: remove dead argument to Parser::parse. (r=efaust)

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -686,7 +686,7 @@ MarkParser(JSTracer* trc, AutoGCRooter* parser)
  */
 template <typename ParseHandler>
 typename ParseHandler::Node
-Parser<ParseHandler>::parse(JSObject* chain)
+Parser<ParseHandler>::parse()
 {
     MOZ_ASSERT(checkOptionsCalled);
 
