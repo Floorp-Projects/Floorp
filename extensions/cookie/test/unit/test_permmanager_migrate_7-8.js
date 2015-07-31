d4a1d2a080b6a32dad2df8c532159a1d406fd5d5
Michael Layzell <michael@thelayzells.com>
2015-07-31 10:51:45 -0700

    Bug 1189070 - Don't discard localhost or IP address entries from the permissions database when migrating. r=ehsan, a=sledru
    --HG--
    extra : transplant_source : %1A%B2%8B%01%BB%21%5Cq%C7bdL%A9%99%F7%D4%C6%CC%7C%BA

--- extensions/cookie/test/unit/test_permmanager_migrate_7-8.js
+++ extensions/cookie/test/unit/test_permmanager_migrate_7-8.js
@@ -1,35 +1,10 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+XPCOMUtils.defineLazyModuleGetter(this, "PlacesTestUtils",
+                                  "resource://testing-common/PlacesTestUtils.jsm");
+
 let PERMISSIONS_FILE_NAME = "permissions.sqlite";
-
-/*
- * Prevent the nsINavHistoryService from being avaliable for the migration
- */
-
-let CONTRACT_ID = "@mozilla.org/browser/nav-history-service;1";
-let factory = {
-  createInstance: function() {
-    throw new Error("There is no history service");
-  },
-  lockFactory: function() {
-    throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
-  },
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIFactory])
-};
-
-let newClassID = Cc["@mozilla.org/uuid-generator;1"].getService(Ci.nsIUUIDGenerator).generateUUID();
-
-let registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
-let oldClassID = registrar.contractIDToCID(CONTRACT_ID);
-let oldFactory = Components.manager.getClassObject(Cc[CONTRACT_ID], Ci.nsIFactory);
-registrar.unregisterFactory(oldClassID, oldFactory);
-registrar.registerFactory(newClassID, "", CONTRACT_ID, factory);
-
-function cleanupFactory() {
-  registrar.unregisterFactory(newClassID, factory);
-  registrar.registerFactory(oldClassID, "", CONTRACT_ID, oldFactory);
-}
 
 function GetPermissionsFile(profile)
 {
@@ -37,10 +12,6 @@ function GetPermissionsFile(profile)
   file.append(PERMISSIONS_FILE_NAME);
   return file;
 }
-
-/*
- * Done nsINavHistoryService code
- */
 
 function run_test() {
   run_next_test();
@@ -50,17 +21,33 @@ add_task(function test() {
   /* Create and set up the permissions database */
   let profile = do_get_profile();
 
-  // Make sure that we can't resolve the nsINavHistoryService
-  try {
-    Cc['@mozilla.org/browser/nav-history-service;1'].getService(Ci.nsINavHistoryService);
-    do_check_true(false, "There shouldn't have been a nsINavHistoryService");
-  } catch (e) {
-    do_check_true(true, "There wasn't a nsINavHistoryService");
-  }
-
   let db = Services.storage.openDatabase(GetPermissionsFile(profile));
-  db.schemaVersion = 4;
+  db.schemaVersion = 7;
 
+  /*
+   * V5 table
+   */
+  db.executeSimpleSQL(
+    "CREATE TABLE moz_perms (" +
+      " id INTEGER PRIMARY KEY" +
+      ",origin TEXT" +
+      ",type TEXT" +
+      ",permission INTEGER" +
+      ",expireType INTEGER" +
+      ",expireTime INTEGER" +
+      ",modificationTime INTEGER" +
+    ")");
+
+  let stmt6Insert = db.createStatement(
+    "INSERT INTO moz_perms (" +
+      "id, origin, type, permission, expireType, expireTime, modificationTime" +
+    ") VALUES (" +
+      ":id, :origin, :type, :permission, :expireType, :expireTime, :modificationTime" +
+    ")");
+
+  /*
+   * V4 table
+   */
   db.executeSimpleSQL(
     "CREATE TABLE moz_hosts (" +
       " id INTEGER PRIMARY KEY" +
@@ -81,7 +68,36 @@ add_task(function test() {
       ":id, :host, :type, :permission, :expireType, :expireTime, :modificationTime, :appId, :isInBrowserElement" +
     ")");
 
+  /*
+   * The v4 table is a backup
+   */
+  db.executeSimpleSQL("CREATE TABLE moz_hosts_is_backup (dummy INTEGER PRIMARY KEY)");
+
   let id = 0;
+
+  function insertOrigin(origin, type, permission, expireType, expireTime, modificationTime) {
+    let thisId = id++;
+
+    stmt6Insert.bindByName("id", thisId);
+    stmt6Insert.bindByName("origin", origin);
+    stmt6Insert.bindByName("type", type);
+    stmt6Insert.bindByName("permission", permission);
+    stmt6Insert.bindByName("expireType", expireType);
+    stmt6Insert.bindByName("expireTime", expireTime);
+    stmt6Insert.bindByName("modificationTime", modificationTime);
+
+    stmt6Insert.execute();
+
+    return {
+      id: thisId,
+      origin: origin,
+      type: type,
+      permission: permission,
+      expireType: expireType,
+      expireTime: expireTime,
+      modificationTime: modificationTime
+    };
+  }
 
   function insertHost(host, type, permission, expireType, expireTime, modificationTime, appId, isInBrowserElement) {
     let thisId = id++;
@@ -111,6 +127,13 @@ add_task(function test() {
     };
   }
 
+  let created7 = [
+    insertOrigin("https://foo.com", "A", 2, 0, 0, 0),
+    insertOrigin("http://foo.com", "A", 2, 0, 0, 0),
+    insertOrigin("http://foo.com^appId=1000&inBrowser=1", "A", 2, 0, 0, 0),
+    insertOrigin("https://192.0.2.235", "A", 2, 0, 0),
+  ];
+
   // Add some rows to the database
   let created = [
     insertHost("foo.com", "A", 1, 0, 0, 0, 0, false),
@@ -124,7 +147,11 @@ add_task(function test() {
     insertHost("bar.ca", "A", 1, 0, 0, 0, 1000, true),
     insertHost("localhost", "A", 1, 0, 0, 0, 0, false),
     insertHost("127.0.0.1", "A", 1, 0, 0, 0, 0, false),
-    insertHost("263.123.555.676", "A", 1, 0, 0, 0, 0, false),
+    insertHost("192.0.2.235", "A", 1, 0, 0, 0, 0, false),
+    // Although ipv6 addresses are written with [] around the IP address,
+    // the .host property doesn't contain these []s, which means that we
+    // write it like this
+    insertHost("2001:db8::ff00:42:8329", "C", 1, 0, 0, 0, 0, false),
     insertHost("file:///some/path/to/file.html", "A", 1, 0, 0, 0, 0, false),
     insertHost("file:///another/file.html", "A", 1, 0, 0, 0, 0, false),
     insertHost("moz-nullprincipal:{8695105a-adbe-4e4e-8083-851faa5ca2d7}", "A", 1, 0, 0, 0, 0, false),
@@ -140,40 +167,30 @@ add_task(function test() {
   db = null;
 
   let expected = [
-    ["http://foo.com", "A", 1, 0, 0],
-    ["http://foo.com", "C", 1, 0, 0],
-    ["http://foo.com^appId=1000", "A", 1, 0, 0],
-    ["http://foo.com^appId=2000&inBrowser=1", "A", 1, 0, 0],
-    ["http://sub.foo.com", "B", 1, 0, 0],
-    ["http://subber.sub.foo.com", "B", 1, 0, 0],
-
-    ["https://foo.com", "A", 1, 0, 0],
-    ["https://foo.com", "C", 1, 0, 0],
-    ["https://foo.com^appId=1000", "A", 1, 0, 0],
-    ["https://foo.com^appId=2000&inBrowser=1", "A", 1, 0, 0],
-    ["https://sub.foo.com", "B", 1, 0, 0],
-    ["https://subber.sub.foo.com", "B", 1, 0, 0],
-
-    // bar.ca will have both http:// and https:// for all entries, because there are no associated history entries
-    ["http://bar.ca", "B", 1, 0, 0],
-    ["https://bar.ca", "B", 1, 0, 0],
-    ["http://bar.ca^appId=1000", "B", 1, 0, 0],
-    ["https://bar.ca^appId=1000", "B", 1, 0, 0],
-    ["http://bar.ca^appId=1000&inBrowser=1", "A", 1, 0, 0],
-    ["https://bar.ca^appId=1000&inBrowser=1", "A", 1, 0, 0],
-    ["file:///some/path/to/file.html", "A", 1, 0, 0],
-    ["file:///another/file.html", "A", 1, 0, 0],
+    // We should have kept the previously migrated entries
+    ["https://foo.com", "A", 2, 0, 0, 0],
+    ["http://foo.com", "A", 2, 0, 0, 0],
+    ["http://foo.com^appId=1000&inBrowser=1", "A", 2, 0, 0, 0],
 
     // Make sure that we also support localhost, and IP addresses
-    ["http://localhost", "A", 1, 0, 0],
-    ["https://localhost", "A", 1, 0, 0],
-    ["http://127.0.0.1", "A", 1, 0, 0],
-    ["https://127.0.0.1", "A", 1, 0, 0],
-    ["http://263.123.555.676", "A", 1, 0, 0],
-    ["https://263.123.555.676", "A", 1, 0, 0],
+    ["https://localhost:8080", "A", 1, 0, 0],
+    ["ftp://127.0.0.1:8080", "A", 1, 0, 0],
+
+    ["http://[2001:db8::ff00:42:8329]", "C", 1, 0, 0],
+    ["https://[2001:db8::ff00:42:8329]", "C", 1, 0, 0],
+    ["http://192.0.2.235", "A", 1, 0, 0],
+
+    // There should only be one entry of this type in the database
+    ["https://192.0.2.235", "A", 2, 0, 0],
   ];
 
   let found = expected.map((it) => 0);
+
+  // Add some places to the places database
+  yield PlacesTestUtils.addVisits(Services.io.newURI("https://foo.com/some/other/subdirectory", null, null));
+  yield PlacesTestUtils.addVisits(Services.io.newURI("ftp://some.subdomain.of.foo.com:8000/some/subdirectory", null, null));
+  yield PlacesTestUtils.addVisits(Services.io.newURI("ftp://127.0.0.1:8080", null, null));
+  yield PlacesTestUtils.addVisits(Services.io.newURI("https://localhost:8080", null, null));
 
   // Force initialization of the nsPermissionManager
   let enumerator = Services.perms.enumerator;
@@ -239,8 +256,11 @@ add_task(function test() {
     mozHostsCount.executeStep();
     do_check_eq(mozHostsCount.getInt64(0), created.length);
 
+    // Check that there are the right number of values in the permissions database
+    let mozPermsCount = db.createStatement("SELECT count(*) FROM moz_perms");
+    mozPermsCount.executeStep();
+    do_check_eq(mozPermsCount.getInt64(0), expected.length);
+
     db.close();
   }
-
-  cleanupFactory();
 });
