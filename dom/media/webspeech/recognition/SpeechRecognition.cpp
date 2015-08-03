3c78ae21c159c9c1d406e6209eda6b273238b515
Kelly Davis <kdavis@mozilla.com>
2015-08-02 22:20:00 -0400

    Bug 1185234 - Implement SpeechRecognition::interimResults. r=smaug

--- dom/media/webspeech/recognition/SpeechRecognition.cpp
+++ dom/media/webspeech/recognition/SpeechRecognition.cpp
@@ -116,6 +116,7 @@ SpeechRecognition::SpeechRecognition(nsPIDOMWindow* aOwnerWindow)
   , mAudioSamplesPerChunk(mEndpointer.FrameSize())
   , mSpeechDetectionTimer(do_CreateInstance(NS_TIMER_CONTRACTID))
   , mSpeechGrammarList(new SpeechGrammarList(GetParentObject()))
+  , mInterimResults(false)
 {
   SR_LOG("created SpeechRecognition");
 
@@ -669,16 +670,15 @@ SpeechRecognition::SetContinuous(bool aArg, ErrorResult& aRv)
 }
 
 bool
-SpeechRecognition::GetInterimResults(ErrorResult& aRv) const
+SpeechRecognition::InterimResults() const
 {
-  aRv.Throw(NS_ERROR_NOT_IMPLEMENTED);
-  return false;
+  return mInterimResults;
 }
 
 void
-SpeechRecognition::SetInterimResults(bool aArg, ErrorResult& aRv)
+SpeechRecognition::SetInterimResults(bool aArg)
 {
-  aRv.Throw(NS_ERROR_NOT_IMPLEMENTED);
+  mInterimResults = aArg;
   return;
 }
 
