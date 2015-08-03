05aa11d7043552ad6e4d93e0cddffc196e231c91
Marco Bonardo <mbonardo@mozilla.com>
2015-08-03 15:00:03 +0200

    Bug 1092607 - Intermittent failure in test_PlacesUtils_asyncGetBookmarkIds.js, r=ttaubert

--- toolkit/components/places/tests/unit/test_PlacesUtils_asyncGetBookmarkIds.js
+++ toolkit/components/places/tests/unit/test_PlacesUtils_asyncGetBookmarkIds.js
@@ -1,86 +1,77 @@
-/* Any copyright is dedicated to the Public Domain.
-   http://creativecommons.org/publicdomain/zero/1.0/ */
-
 /**
  * This file tests PlacesUtils.asyncGetBookmarkIds method.
  */
 
 const TEST_URL = "http://www.example.com/";
 
-[
-
-  function test_no_bookmark() {
-    PlacesUtils.asyncGetBookmarkIds(TEST_URL, (aItemIds, aURI) => {
-      do_check_eq(aItemIds.length, 0);
-      do_check_eq(aURI, TEST_URL);
-      run_next_test();
+let promiseAsyncGetBookmarkIds = Task.async(function* (url) {
+  yield PlacesTestUtils.promiseAsyncUpdates();
+  return new Promise(resolve => {
+    PlacesUtils.asyncGetBookmarkIds(url, (itemIds, uri) => {
+      Assert.equal(uri, url);
+      resolve({ itemIds, url });
     });
-  },
+  });
+});
 
-  function test_one_bookmark_nsIURI() {
-    let uri = NetUtil.newURI(TEST_URL);
-    let itemId = PlacesUtils.bookmarks.insertBookmark(
-      PlacesUtils.unfiledBookmarksFolderId, uri, "test",
-      PlacesUtils.bookmarks.DEFAULT_INDEX
-    );
-    PlacesUtils.asyncGetBookmarkIds(uri, (aItemIds, aURI) => {
-      do_check_eq(aItemIds.length, 1);
-      do_check_eq(aItemIds[0], itemId);
-      do_check_true(aURI.equals(uri));
-      PlacesUtils.bookmarks.removeItem(itemId);
-      run_next_test();
-    });
-  },
+add_task(function* test_no_bookmark() {
+  let { itemIds, url } = yield promiseAsyncGetBookmarkIds(TEST_URL);
+  Assert.equal(itemIds.length, 0);
+  Assert.equal(url, TEST_URL);
+});
 
-  function test_one_bookmark_spec() {
-    let uri = NetUtil.newURI(TEST_URL);
-    let itemId = PlacesUtils.bookmarks.insertBookmark(
-      PlacesUtils.unfiledBookmarksFolderId, uri, "test",
-      PlacesUtils.bookmarks.DEFAULT_INDEX
-    );
-    PlacesUtils.asyncGetBookmarkIds(TEST_URL, (aItemIds, aURI) => {
-      do_check_eq(aItemIds.length, 1);
-      do_check_eq(aItemIds[0], itemId);
-      do_check_eq(aURI, TEST_URL);
-      PlacesUtils.bookmarks.removeItem(itemId);
-      run_next_test();
-    });
-  },
+add_task(function* test_one_bookmark() {
+  let bookmark = yield PlacesUtils.bookmarks.insert({
+    parentGuid: PlacesUtils.bookmarks.unfiledGuid,
+    url: TEST_URL,
+    title: "test"
+  });
+  let itemId = yield PlacesUtils.promiseItemId(bookmark.guid);
+  {
+    let { itemIds, url } = yield promiseAsyncGetBookmarkIds(NetUtil.newURI(TEST_URL));
+    Assert.equal(itemIds.length, 1);
+    Assert.equal(itemIds[0], itemId);
+    Assert.equal(url.spec, TEST_URL);
+  }
+  {
+    let { itemIds, url } = yield promiseAsyncGetBookmarkIds(TEST_URL);
+    Assert.equal(itemIds.length, 1);
+    Assert.equal(itemIds[0], itemId);
+    Assert.equal(url, TEST_URL);
+  }
+  yield PlacesUtils.bookmarks.remove(bookmark);
+});
 
-  function test_multiple_bookmarks() {
-    let uri = NetUtil.newURI(TEST_URL);
-    let itemIds = [];
-    itemIds.push(PlacesUtils.bookmarks.insertBookmark(
-      PlacesUtils.unfiledBookmarksFolderId, uri, "test",
-      PlacesUtils.bookmarks.DEFAULT_INDEX
-    ));
-    itemIds.push(PlacesUtils.bookmarks.insertBookmark(
-      PlacesUtils.unfiledBookmarksFolderId, uri, "test",
-      PlacesUtils.bookmarks.DEFAULT_INDEX
-    ));
-    PlacesUtils.asyncGetBookmarkIds(uri, (aItemIds, aURI) => {
-      do_check_eq(aItemIds.length, 2);
-      do_check_true(do_compare_arrays(itemIds, aItemIds));
-      do_check_true(aURI.equals(uri));
-      itemIds.forEach(PlacesUtils.bookmarks.removeItem);
-      run_next_test();
-    });
-  },
+add_task(function* test_multiple_bookmarks() {
+  let ids = [];
+  let bookmark1 = yield PlacesUtils.bookmarks.insert({
+    parentGuid: PlacesUtils.bookmarks.unfiledGuid,
+    url: TEST_URL,
+    title: "test"
+  });
+  ids.push((yield PlacesUtils.promiseItemId(bookmark1.guid)));
+  let bookmark2 = yield PlacesUtils.bookmarks.insert({
+    parentGuid: PlacesUtils.bookmarks.unfiledGuid,
+    url: TEST_URL,
+    title: "test"
+  });
+  ids.push((yield PlacesUtils.promiseItemId(bookmark2.guid)));
 
-  function test_cancel() {
-    let pending = PlacesUtils.asyncGetBookmarkIds(TEST_URL, (aItemIds, aURI) => {
-      do_throw("A canceled pending statement should not be invoked");
-    });
-    pending.cancel();
-    PlacesUtils.asyncGetBookmarkIds(TEST_URL, (aItemIds, aURI) => {
-      do_check_eq(aItemIds.length, 0);
-      do_check_eq(aURI, TEST_URL);
-      run_next_test();
-    });
-  },
+  let { itemIds, url } = yield promiseAsyncGetBookmarkIds(TEST_URL);
+  Assert.deepEqual(ids, itemIds);
+  Assert.equal(url, TEST_URL);
 
-].forEach(add_test);
+  yield PlacesUtils.bookmarks.remove(bookmark1);
+  yield PlacesUtils.bookmarks.remove(bookmark2);
+});
 
-function run_test() {
-  run_next_test();
-}
+add_task(function* test_cancel() {
+  let pending = PlacesUtils.asyncGetBookmarkIds(TEST_URL, () => {
+    Assert.ok(false, "A canceled pending statement should not be invoked");
+  });
+  pending.cancel();
+
+  let { itemIds, url } = yield promiseAsyncGetBookmarkIds(TEST_URL);
+  Assert.equal(itemIds.length, 0);
+  Assert.equal(url, TEST_URL);
+});
