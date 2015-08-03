605fe5622889f50365e9bbfba37d3c84940e7b02
Sean Stangl <sstangl@mozilla.com>
2015-08-03 15:48:19 -0700

    Bug 1188650 - Part 2/2 - Make shared/LOpcodes-shared.h. r=nbp

--- js/src/jit/x64/LOpcodes-x64.h
+++ js/src/jit/x64/LOpcodes-x64.h
@@ -7,19 +7,10 @@
 #ifndef jit_x64_LOpcodes_x64_h
 #define jit_x64_LOpcodes_x64_h
 
+#include "jit/shared/LOpcodes-shared.h"
+
 #define LIR_CPU_OPCODE_LIST(_)      \
-    _(Box)                          \
-    _(Unbox)                        \
-    _(UnboxFloatingPoint)           \
-    _(DivI)                         \
-    _(DivPowTwoI)                   \
     _(DivOrModConstantI)            \
-    _(ModI)                         \
-    _(ModPowTwoI)                   \
-    _(PowHalfD)                     \
-    _(AsmJSUInt32ToDouble)          \
-    _(AsmJSUInt32ToFloat32)         \
-    _(AsmJSLoadFuncPtr)             \
     _(SimdValueInt32x4)             \
     _(SimdValueFloat32x4)           \
     _(UDivOrMod)                    \
