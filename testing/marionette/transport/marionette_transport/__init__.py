eb86088a30b313bc159a86f04479d83e7ed25ea0
Julien Pagès <j.parkouss@gmail.com>
2015-08-04 05:49:00 +0200

    Bug 1190817 - marionette install from pypi is broken. r=automatedtester

--- testing/marionette/transport/marionette_transport/__init__.py
+++ testing/marionette/transport/marionette_transport/__init__.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-__version__ = '0.5'
+__version__ = '0.6'
 
 
 from transport import MarionetteTransport
