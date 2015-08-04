616201b1ee24c016baf20ee5450c1d94677da35a
Kannan Vijayan <kvijayan@mozilla.com>
2015-08-04 09:58:00 -0400

    Bug 1145442 - Add documentation for JIT strategies. r=shu

---
+++ js/src/doc/JITOptimizations/config.sh
@@ -0,0 +1,15 @@
+base-url https://developer.mozilla.org/en-US/docs/Tools/
+
+markdown Strategies.md JIT-Optimization/Strategies
+  label 'jitstrategies'                  'JIT Optimization Strategies'
+  label 'getprop'    '#getproperty'      'JIT Optimization Strategies : GetProperty'
+  label 'setprop'    '#setproperty'      'JIT Optimization Strategies : SetProperty'
+  label 'getelem'    '#getelement'       'JIT Optimization Strategies : GetElement'
+  label 'setelem'    '#setelement'       'JIT Optimization Strategies : SetElement'
+  label 'call'       '#call'             'JIT Optimization Strategies : Call'
+
+markdown Strategies.md JIT-Optimization/Outcomes
+  label 'jitoutcomes'                    'JIT Optimization Outcomes'
+  label 'general'    '#general'          'JIT Optimization Outcomes : General Outcomes'
+  label 'ic'         '#inlinecache'      'JIT Optimization Outcomes : Inline Cache Outcomes'
+  label 'callinline' '#callinline'       'JIT Optimization Outcomes : Call Inlining Outcomes'
