e9d83e8296687e8d805d8ea92410f6cfa25432ab
Julien Pagès <j.parkouss@gmail.com>
2015-08-04 08:48:51 -0400

    Bug 1172918 - clean up talos options that are not used (mozharness bits). r=jmaher a=test-only

--- testing/mozharness/configs/talos/windows_config.py
+++ testing/mozharness/configs/talos/windows_config.py
@@ -29,8 +29,6 @@ config = {
     },
     "title": socket.gethostname().split('.')[0],
     "results_url": "http://graphs.mozilla.org/server/collect.cgi",
-    "datazilla_urls": ["https://datazilla.mozilla.org/talos"],
-    "datazilla_authfile": os.path.join(os.getcwd(), "oauth.txt"),
     "default_actions": [
         "clobber",
         "read-buildbot-config",
