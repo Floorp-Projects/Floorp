2f34d661aef744b1d700214f2e57e9cb04e9374b
Mike Hommey <mh+mozilla@glandium.org>
2015-08-05 13:47:43 +0900

    Backed out changeset 684252f11061 (bug 1188224) for make check bustage.

--- build/mach_bootstrap.py
+++ build/mach_bootstrap.py
@@ -9,7 +9,6 @@ import os
 import platform
 import sys
 import time
-import __builtin__
 
 
 STATE_DIR_FIRST_RUN = '''
@@ -328,65 +327,3 @@ def bootstrap(topsrcdir, mozilla_dir=None):
         mach.load_commands_from_file(os.path.join(mozilla_dir, path))
 
     return mach
-
-
-# Hook import such that .pyc/.pyo files without a corresponding .py file in
-# the source directory are essentially ignored. See further below for details
-# and caveats.
-# Objdirs outside the source directory are ignored because in most cases, if
-# a .pyc/.pyo file exists there, a .py file will be next to it anyways.
-class ImportHook(object):
-    def __init__(self, original_import):
-        self._original_import = original_import
-        # Assume the source directory is the parent directory of the one
-        # containing this file.
-        self._source_dir = os.path.normcase(os.path.abspath(
-            os.path.dirname(os.path.dirname(__file__)))) + os.sep
-        self._modules = set()
-
-    def __call__(self, name, globals=None, locals=None, fromlist=None,
-                 level=-1):
-        # name might be a relative import. Instead of figuring out what that
-        # resolves to, which is complex, just rely on the real import.
-        # Since we don't know the full module name, we can't check sys.modules,
-        # so we need to keep track of which modules we've already seen to avoid
-        # to stat() them again when they are imported multiple times.
-        module = self._original_import(name, globals, locals, fromlist, level)
-        resolved_name = module.__name__
-        if resolved_name in self._modules:
-            return module
-        self._modules.add(resolved_name)
-
-        # Builtin modules don't have a __file__ attribute.
-        if not hasattr(module, '__file__'):
-            return module
-
-        # Note: module.__file__ is not always absolute.
-        path = os.path.normcase(os.path.abspath(module.__file__))
-        # Note: we could avoid normcase and abspath above for non pyc/pyo
-        # files, but those are actually rare, so it doesn't really matter.
-        if not path.endswith(('.pyc', '.pyo')):
-            return module
-
-        # Ignore modules outside our source directory
-        if not path.startswith(self._source_dir):
-            return module
-
-        # If there is no .py corresponding to the .pyc/.pyo module we're
-        # loading, remove the .pyc/.pyo file, and reload the module.
-        # Since we already loaded the .pyc/.pyo module, if it had side
-        # effects, they will have happened already, and loading the module
-        # with the same name, from another directory may have the same side
-        # effects (or different ones). We assume it's not a problem for the
-        # python modules under our source directory (either because it
-        # doesn't happen or because it doesn't matter).
-        if not os.path.exists(module.__file__[:-1]):
-            os.remove(module.__file__)
-            del sys.modules[module.__name__]
-            module = self(name, globals, locals, fromlist, level)
-
-        return module
-
-
-# Install our hook
-__builtin__.__import__ = ImportHook(__builtin__.__import__)
