f44b98f664b9ce876615dbf512bd2192e94c6a36
Dave Townsend <dtownsend@oxymoronical.com>
2015-08-05 13:33:57 -0700

    Bug 1191421: Don't notify users about unsigned experiments that have been disabled. r=dao

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -491,8 +491,15 @@ BrowserGlue.prototype = {
         this._handleFlashHang();
         break;
       case "xpi-signature-changed":
-        if (JSON.parse(data).disabled.length)
-          this._notifyUnsignedAddonsDisabled();
+        let disabledAddons = JSON.parse(data).disabled;
+        AddonManager.getAddonsByIDs(disabledAddons, (addons) => {
+          for (let addon of addons) {
+            if (addon.type != "experiment") {
+              this._notifyUnsignedAddonsDisabled();
+              break;
+            }
+          }
+        });
         break;
       case "autocomplete-did-enter-text":
         this._handleURLBarTelemetry(subject.QueryInterface(Ci.nsIAutoCompleteInput));
@@ -1160,6 +1167,9 @@ BrowserGlue.prototype = {
     let disabledAddons = AddonManager.getStartupChanges(AddonManager.STARTUP_CHANGE_DISABLED);
     AddonManager.getAddonsByIDs(disabledAddons, (addons) => {
       for (let addon of addons) {
+        if (addon.type == "experiment")
+          continue;
+
         if (addon.signedState <= AddonManager.SIGNEDSTATE_MISSING) {
           this._notifyUnsignedAddonsDisabled();
           break;
