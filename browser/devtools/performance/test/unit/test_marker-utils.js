0a3107c1e4a50907fd61ac486d9a8e32996114cd
Jordan Santell <jsantell@mozilla.com>
2015-08-06 13:15:23 -0700

    Bug 1191153 - Print friendly GC reason names for GC markers in the performance tool. r=fitzgen

--- browser/devtools/performance/test/unit/test_marker-utils.js
+++ browser/devtools/performance/test/unit/test_marker-utils.js
@@ -39,6 +39,12 @@ add_task(function () {
   equal(fields[1].label, "Phase:", "getMarkerFields() correctly returns fields via function (3)");
   equal(fields[1].value, "Target", "getMarkerFields() correctly returns fields via function (4)");
 
+  fields = Utils.getMarkerFields({ name: "GarbageCollection", cause: "ALLOC_TRIGGER" });
+  equal(fields[0].value, "Too Many Allocations", "Uses L10N for GC reasons");
+
+  fields = Utils.getMarkerFields({ name: "GarbageCollection", cause: "NOT_A_GC_REASON" });
+  equal(fields[0].value, "NOT_A_GC_REASON", "Defaults to enum for GC reasons when not L10N'd");
+
   equal(Utils.getMarkerFields({ name: "Javascript", causeName: "Some Platform Field" })[0].value, "(Gecko)",
     "Correctly obfuscates JS markers when platform data is off.");
   Services.prefs.setBoolPref(PLATFORM_DATA_PREF, true);
