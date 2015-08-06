2e2bf85ab4889065d826431bb34a9ef898497dbd
Wes Kocher <wkocher@mozilla.com>
2015-08-05 21:21:57 -0700

    Backed out changeset e9d391fdf0be (bug 1179262) a=backout

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -152,7 +152,6 @@
 #include "nsIScriptGlobalObject.h"
 #include "nsIScriptObjectPrincipal.h"
 #include "nsIScriptSecurityManager.h"
-#include "nsIStreamConverterService.h"
 #include "nsIStringBundle.h"
 #include "nsIURI.h"
 #include "nsIURL.h"
@@ -6500,20 +6499,6 @@ nsContentUtils::AllowXULXBLForPrincipal(nsIPrincipal* aPrincipal)
          ((sAllowXULXBL_for_file && SchemeIs(princURI, "file")) ||
           IsSitePermAllow(aPrincipal, "allowXULXBL"));
 }
-
-bool
-nsContentUtils::IsPDFJSEnabled()
-{
-   nsCOMPtr<nsIStreamConverterService> convServ =
-     do_GetService("@mozilla.org/streamConverters;1");
-   nsresult rv = NS_ERROR_FAILURE;
-   bool canConvert = false;
-   if (convServ) {
-     rv = convServ->CanConvert("application/pdf", "text/html", &canConvert);
-   }
-   return NS_SUCCEEDED(rv) && canConvert;
-}
-
 
 already_AddRefed<nsIDocumentLoaderFactory>
 nsContentUtils::FindInternalContentViewer(const nsACString& aType,
