e35277a4919da762289a484b19aff9de04cb9fa0
Karl Tomlinson <karlt+@karlt.net>
2015-08-06 19:25:53 +1200

    bug 1191649 add notification of input node changes r=padenot
    --HG--
    extra : rebase_source : 71b101935ef10b5fa01008234897c0bb2c6979ba

--- dom/media/webaudio/AudioNode.cpp
+++ dom/media/webaudio/AudioNode.cpp
@@ -227,6 +227,7 @@ AudioNode::Connect(AudioNode& aDestination, uint32_t aOutput,
                             static_cast<uint16_t>(aInput),
                             static_cast<uint16_t>(aOutput));
   }
+  aDestination.NotifyInputsChanged();
 
   // This connection may have connected a panner and a source.
   Context()->UpdatePannerSource();
@@ -351,6 +352,7 @@ AudioNode::Disconnect(uint32_t aOutput, ErrorResult& aRv)
         // could be for different output ports.
         nsRefPtr<AudioNode> output = mOutputNodes[i].forget();
         mOutputNodes.RemoveElementAt(i);
+        output->NotifyInputsChanged();
         if (mStream) {
           nsRefPtr<nsIRunnable> runnable = new RunnableRelease(output.forget());
           mStream->RunAfterPendingUpdates(runnable.forget());
