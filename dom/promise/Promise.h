9408e8a73654d189c80a7f2f9889c59d6479d0c4
Alpha A. <alpha@a-alpha.net>
2015-08-06 17:18:30 +0200

    Bug 1086627 - Rename Promise constructs to more closely match the specification. r=nsm,jst

--- dom/promise/Promise.h
+++ dom/promise/Promise.h
@@ -75,7 +75,7 @@ class Promise : public nsISupports,
                 public SupportsWeakPtr<Promise>
 {
   friend class NativePromiseCallback;
-  friend class PromiseCallbackTask;
+  friend class PromiseReactionJob;
   friend class PromiseResolverTask;
   friend class PromiseTask;
 #if defined(DOM_PROMISE_DEPRECATED_REPORTING)
@@ -274,8 +274,8 @@ private:
   // This method enqueues promise's resolve/reject callbacks with promise's
   // result. It's executed when the resolver.resolve() or resolver.reject() is
   // called or when the promise already has a result and new callbacks are
-  // appended by then(), catch() or done().
-  void EnqueueCallbackTasks();
+  // appended by then() or catch().
+  void TriggerPromiseReactions();
 
   void Settle(JS::Handle<JS::Value> aValue, Promise::PromiseState aState);
   void MaybeSettle(JS::Handle<JS::Value> aValue, Promise::PromiseState aState);
--- dom/webidl/Promise.webidl
+++ dom/webidl/Promise.webidl
@@ -20,8 +20,6 @@ callback AnyCallback = any (any value);
  Exposed=(Window,Worker,System)]
 // Need to escape "Promise" so it's treated as an identifier.
 interface _Promise {
-  // TODO bug 875289 - static Promise fulfill(any value);
-
   // Disable the static methods when the interface object is supposed to be
   // disabled, just in case some code decides to walk over to .constructor from
   // the proto of a promise object or someone screws up and manages to create a
