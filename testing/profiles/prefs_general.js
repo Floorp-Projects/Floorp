2706f080e5f24d57b277b0b4f27ebabc2e82feb0
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-08-07 14:57:25 +0200

    Backed out changeset b9c50a97ee3e (bug 1182946) for m3 mulet test failures in test_SeekableAfterEndOfStreamSplit.html on a CLOSED TREE

--- testing/profiles/prefs_general.js
+++ testing/profiles/prefs_general.js
@@ -166,7 +166,7 @@ user_pref("layout.spammy_warnings.enabled", false);
 
 // Enable Media Source Extensions for testing
 user_pref("media.mediasource.mp4.enabled", true);
-user_pref("media.mediasource.webm.enabled", true);
+user_pref("media.mediasource.webm.enabled", false);
 
 // Enable mozContacts
 user_pref("dom.mozContacts.enabled", true);
