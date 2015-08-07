529527f16e397ae16d81a6fecf3d05152bfa1013
Sebastian Kaspari <s.kaspari@gmail.com>
2015-08-07 13:23:18 +0200

    Bug 1182514 - Add Lightweight theme for restricted profiles. r=margaret, a=lizzard

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -3098,6 +3098,10 @@ var LightWeightThemeWebInstaller = {
     BrowserApp.deck.addEventListener("InstallBrowserTheme", this, false, true);
     BrowserApp.deck.addEventListener("PreviewBrowserTheme", this, false, true);
     BrowserApp.deck.addEventListener("ResetBrowserThemePreview", this, false, true);
+
+    if (ParentalControls.parentalControlsEnabled && !this._manager.currentTheme) {
+      this._installParentalControlsTheme();
+    }
   },
 
   handleEvent: function (event) {
@@ -3132,6 +3136,18 @@ var LightWeightThemeWebInstaller = {
     Cu.import("resource://gre/modules/LightweightThemeManager.jsm", temp);
     delete this._manager;
     return this._manager = temp.LightweightThemeManager;
+  },
+
+  _installParentalControlsTheme: function() {
+    let mgr = this._manager;
+    let parentalControlsTheme = {
+      "headerURL": "resource://android/assets/parental_controls_theme.png",
+      "name": "Parental Controls Theme",
+      "id": "parental-controls-theme@mozilla.org"
+    };
+
+    mgr.addBuiltInTheme(parentalControlsTheme);
+    mgr.themeChanged(parentalControlsTheme);
   },
 
   _installRequest: function (event) {
