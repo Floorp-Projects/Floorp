39b062b54da6a57fc1c048605f6a3a72dd8c399b
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2015-08-07 13:41:49 +0900

    Bug 1166323 - Fix unexpcetd changed on previous landed. r=dkeeler

--- security/certverifier/ExtendedValidation.cpp
+++ security/certverifier/ExtendedValidation.cpp
@@ -1249,7 +1249,7 @@ IdentityInfoInit()
         continue;
       }
 #endif
-      //PR_NOT_REACHED("Could not find EV root in NSS storage");
+      PR_NOT_REACHED("Could not find EV root in NSS storage");
       continue;
     }
 
