e03e8ae801d179ee92e56a277fb3db837041b06e
Andrew Halberstadt <ahalberstadt@mozilla.com>
2015-08-07 10:19:32 -0400

    Bug 1192235 - Update misleading mochitest usage string, r=chmanchester, DONTBUILD

--- testing/mochitest/mochitest_options.py
+++ testing/mochitest/mochitest_options.py
@@ -1128,14 +1128,7 @@ container_map = {
 
 
 class MochitestArgumentParser(ArgumentParser):
-    """
-    Usage instructions for Mochitest.
-
-    All arguments are optional.
-    If --chrome is specified, chrome tests will be run instead of web content tests.
-    If --browser-chrome is specified, browser-chrome tests will be run instead of web content tests.
-    See <http://mochikit.com/doc/html/MochiKit/Logging.html> for details on the logging levels.
-    """
+    """%(prog)s [options] [test paths]"""
 
     _containers = None
     context = {}
