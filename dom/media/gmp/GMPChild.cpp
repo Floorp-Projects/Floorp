97fe26570f206e8271aa69a72fbff59f7a3282cf
Chris Pearce <cpearce@mozilla.com>
2015-08-11 10:27:41 +1200

    Bug 1190252 - Remove use of mozilla\/Tokenizer from GMPChild so bug 1190252 can be uplift to 41. r=gerald,a=ritu

--- dom/media/gmp/GMPChild.cpp
+++ dom/media/gmp/GMPChild.cpp
@@ -19,7 +19,7 @@
 #include "gmp-video-encode.h"
 #include "GMPPlatform.h"
 #include "mozilla/dom/CrashReporterChild.h"
-#include "mozilla/Tokenizer.h"
+#include "GMPUtils.h"
 #include "prio.h"
 
 using mozilla::dom::CrashReporterChild;
@@ -341,24 +341,6 @@ ReadIntoString(nsIFile* aFile,
   return rv;
 }
 
-static nsTArray<nsCString>
-SplitAt(Tokenizer::Token aDelim, const nsACString& aInput)
-{
-  nsTArray<nsCString> tokens;
-  Tokenizer tokenizer(aInput);
-
-  while (!tokenizer.HasFailed()) {
-    tokenizer.Record();
-    Tokenizer::Token token;
-    while (tokenizer.Next(token) && !token.Equals(aDelim))
-      ; // Skip up to next delimeter, or EOF.
-    nsAutoCString value;
-    tokenizer.Claim(value);
-    tokens.AppendElement(value);
-  }
-  return tokens;
-}
-
 // Pre-load DLLs that need to be used by the EME plugin but that can't be
 // loaded after the sandbox has started
 bool
@@ -387,7 +369,10 @@ GMPChild::PreLoadLibraries(const nsAString& aPluginPath)
     return false;
   }
 
-  nsTArray<nsCString> lines = SplitAt(Tokenizer::Token::NewLine(), info);
+  // Note: we pass "\r\n" to SplitAt so that we'll split lines delimited
+  // by \n (Unix), \r\n (Windows) and \r (old MacOSX).
+  nsTArray<nsCString> lines;
+  SplitAt("\r\n", info, lines);
   for (nsCString line : lines) {
     // Make lowercase.
     std::transform(line.BeginWriting(),
@@ -401,8 +386,8 @@ GMPChild::PreLoadLibraries(const nsAString& aPluginPath)
       continue;
     }
     // Line starts with "libraries:".
-    nsTArray<nsCString> libs = SplitAt(Tokenizer::Token::Char(','),
-                                       Substring(line, offset + strlen(libraries)));
+    nsTArray<nsCString> libs;
+    SplitAt(",", Substring(line, offset + strlen(libraries)), libs);
     for (nsCString lib : libs) {
       lib.Trim(" ");
       for (const char* whiteListedLib : whitelist) {
