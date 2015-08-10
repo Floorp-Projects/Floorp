6c50e2b4ecd496528363faa2fb4a50742edb2779
Brian Hackett <bhackett1024@gmail.com>
2015-08-09 18:55:49 -0600

    Bug 1190272 - Improve type checks when storing values into unboxed objects in Ion code. r=jandem, a=abillings

--- js/src/jit/IonCaches.cpp
+++ js/src/jit/IonCaches.cpp
@@ -3101,12 +3101,7 @@ GenerateSetUnboxed(JSContext* cx, MacroAssembler& masm, IonCache::StubAttacher& 
             MOZ_ASSERT(!UnboxedTypeNeedsPreBarrier(unboxedType));
     }
 
-    // If unboxed objects in this group have have never been converted to
-    // native objects then the type set check performed above ensures the value
-    // being written can be stored in the unboxed object.
-    Label* storeFailure = obj->group()->unboxedLayout().nativeGroup() ? &failure : nullptr;
-
-    masm.storeUnboxedProperty(address, unboxedType, value, storeFailure);
+    masm.storeUnboxedProperty(address, unboxedType, value, &failure);
 
     attacher.jumpRejoin(masm);
 
