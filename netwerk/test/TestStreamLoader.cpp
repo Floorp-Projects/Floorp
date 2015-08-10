47f95bad4848c108f2f6d93d3f8d2a1a34ad1ebd
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2015-08-10 10:20:40 -0700

    Bug 1188644 - Use channel->ascynOpen2 in netwerk/test (r=mcmanus)

--- netwerk/test/TestStreamLoader.cpp
+++ netwerk/test/TestStreamLoader.cpp
@@ -74,7 +74,7 @@ int main(int argc, char **argv)
     rv = NS_NewChannel(getter_AddRefs(chan),
                        uri,
                        systemPrincipal,
-                       nsILoadInfo::SEC_NORMAL,
+                       nsILoadInfo::SEC_ALLOW_CROSS_ORIGIN_DATA_INHERITS,
                        nsIContentPolicy::TYPE_OTHER);
 
     if (NS_FAILED(rv))
@@ -89,7 +89,7 @@ int main(int argc, char **argv)
     if (NS_FAILED(rv))
       return -1;
 
-    rv = chan->AsyncOpen(loader, nullptr);
+    rv = chan->AsyncOpen2(loader);
     if (NS_FAILED(rv))
       return -1;
 
