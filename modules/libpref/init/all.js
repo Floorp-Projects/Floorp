b4d20c0a13e7e5a06d76559c38da7c42015b2c20
Nikhil Marathe <nsm.nikhil@gmail.com>
2015-08-11 12:53:14 -0700

    Bug 1193365 - Disable push debug. r=kitcambridge a=sylvestre
    --HG--
    extra : source : 4fd08002000bf48aaa6b0113566ea55b83c39f6c

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4464,9 +4464,7 @@ pref("dom.mozAlarms.enabled", false);
 
 pref("dom.push.enabled", false);
 
-#if !defined(RELEASE_BUILD)
-pref("dom.push.debug", true);
-#endif
+pref("dom.push.debug", false);
 
 pref("dom.push.serverURL", "wss://push.services.mozilla.com/");
 pref("dom.push.userAgentID", "");
