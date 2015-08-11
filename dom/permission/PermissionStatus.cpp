af382947b14a700e7aa2ed97d8517870a25f4864
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-08-11 10:04:35 -0700

    Backed out changeset b78a97800675 (bug 1105827)

--- dom/permission/PermissionStatus.cpp
+++ dom/permission/PermissionStatus.cpp
@@ -6,7 +6,6 @@
 
 #include "mozilla/dom/PermissionStatus.h"
 
-#include "mozilla/AsyncEventDispatcher.h"
 #include "mozilla/Services.h"
 #include "mozilla/UniquePtr.h"
 #include "nsIPermissionManager.h"
@@ -112,18 +111,6 @@ PermissionStatus::GetPrincipal() const
   }
 
   return doc->NodePrincipal();
-}
-
-void
-PermissionStatus::PermissionChanged()
-{
-  auto oldState = mState;
-  UpdateState();
-  if (mState != oldState) {
-    nsRefPtr<AsyncEventDispatcher> eventDispatcher =
-      new AsyncEventDispatcher(this, NS_LITERAL_STRING("change"), false);
-    eventDispatcher->PostDOMEvent();
-  }
 }
 
 } // namespace dom
