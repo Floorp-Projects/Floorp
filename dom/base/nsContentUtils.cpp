3fc10d88bd342579c9b781e8879561d5d5600119
Kyle Huey <khuey@kylehuey.com>
2015-08-11 06:54:49 -0700

    Bug 1179909: Build fix. r=me CLOSED TREE

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -94,7 +94,6 @@
 #include "nsHostObjectProtocolHandler.h"
 #include "nsHtml5Module.h"
 #include "nsHtml5StringParser.h"
-#include "nsIAppShell.h"
 #include "nsIAsyncVerifyRedirectCallback.h"
 #include "nsICategoryManager.h"
 #include "nsIChannelEventSink.h"
@@ -346,7 +345,6 @@ namespace {
 
 static NS_DEFINE_CID(kParserServiceCID, NS_PARSERSERVICE_CID);
 static NS_DEFINE_CID(kCParserCID, NS_PARSER_CID);
-static NS_DEFINE_CID(kAppShellCID, NS_APPSHELL_CID);
 
 static PLDHashTable* sEventListenerManagersHash;
 
