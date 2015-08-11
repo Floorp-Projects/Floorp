122e190a5bfc5c6f7884b48403fda542db84e56e
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2015-08-11 14:12:29 +0900

    Bug 1187178 - Use MOZ_WINSDK_MAXVER instead of #ifndef. r=jimm

--- widget/windows/WindowsUIUtils.cpp
+++ widget/windows/WindowsUIUtils.cpp
@@ -33,7 +33,7 @@ using namespace ABI::Windows::Foundation;
 
 /* All of this is win10 stuff and we're compiling against win81 headers
  * for now, so we may need to do some legwork: */
-#ifndef UserInteractionMode
+#if MOZ_WINSDK_MAXVER < 0x0A000000
 namespace ABI {
   namespace Windows {
     namespace UI {
@@ -53,7 +53,7 @@ namespace ABI {
 #define RuntimeClass_Windows_UI_ViewManagement_UIViewSettings L"Windows.UI.ViewManagement.UIViewSettings"
 #endif
 
-#ifndef IUIViewSettings
+#if MOZ_WINSDK_MAXVER < 0x0A000000
 namespace ABI {
   namespace Windows {
     namespace UI {
