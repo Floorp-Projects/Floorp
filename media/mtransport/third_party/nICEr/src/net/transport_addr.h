d81e92647378bc1369ea2eb1fee6a02d57a4797b
EKR <ekr@rtfm.com>
2015-08-12 10:53:15 -0400

    Bug 1189041 - Add option to only gather addresses for default route. r=bwc a=sylvestre

--- media/mtransport/third_party/nICEr/src/net/transport_addr.h
+++ media/mtransport/third_party/nICEr/src/net/transport_addr.h
@@ -75,7 +75,7 @@ int nr_sockaddr_to_transport_addr(struct sockaddr *saddr, int protocol, int keep
 
 // addresses, ports in local byte order
 int nr_ip4_port_to_transport_addr(UINT4 ip4, UINT2 port, int protocol, nr_transport_addr *addr);
-int nr_str_port_to_transport_addr(const char *ip4, UINT2 port, int protocol, nr_transport_addr *addr);
+int nr_str_port_to_transport_addr(const char *str, UINT2 port, int protocol, nr_transport_addr *addr);
 int nr_ip6_port_to_transport_addr(struct in6_addr* addr6, UINT2 port, int protocol, nr_transport_addr *addr);
 
 int nr_transport_addr_get_addrstring(nr_transport_addr *addr, char *str, int maxlen);
