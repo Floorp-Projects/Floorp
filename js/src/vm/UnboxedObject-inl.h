7c53560012a43effdadcd1c892f820214b910740
David Major <dmajor@mozilla.com>
2015-08-12 15:31:33 -0400

    Bug 1193459 review follow-up: Copy the comment from TraceKind.h. DONTBUILD CLOSED TREE

--- js/src/vm/UnboxedObject-inl.h
+++ js/src/vm/UnboxedObject-inl.h
@@ -584,7 +584,8 @@ CopyBoxedOrUnboxedDenseElements(JSContext* cx, JSObject* dst, JSObject* src,
 /////////////////////////////////////////////////////////////////////
 
 // Goop to fix MSVC. See DispatchTraceKindTyped in TraceKind.h.
-#if defined(_MSC_VER) && !defined(__clang__)
+// The clang-cl front end defines _MSC_VER, but still requires the explicit
+// template declaration, so we must test for __clang__ here as well.
 # define DEPENDENT_TEMPLATE_HINT
 #else
 # define DEPENDENT_TEMPLATE_HINT template
