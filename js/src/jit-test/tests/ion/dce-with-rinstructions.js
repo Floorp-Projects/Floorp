7ad5df883ea9f24baff3516a2861f2fb0f5eb6df
Eric Faust <efaustbmo@gmail.com>
2015-08-13 14:51:16 -0700

    Bug 1188586 - Disable test for temporarily disabled feature on a CLOSED TREE. (r=itwasonlya3liner)

--- js/src/jit-test/tests/ion/dce-with-rinstructions.js
+++ js/src/jit-test/tests/ion/dce-with-rinstructions.js
@@ -475,7 +475,8 @@ function rpow_number(i) {
     var x = Math.pow(i, 3.14159);
     if (uceFault_pow_number(i) || uceFault_pow_number(i))
         assertEq(x, Math.pow(99, 3.14159));
-    assertRecoveredOnBailout(x, true);
+    // POW recovery temporarily disabled. See bug 1188586.
+    assertRecoveredOnBailout(x, false);
     return i;
 }
 
