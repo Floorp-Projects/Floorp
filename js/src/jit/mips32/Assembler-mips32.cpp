49f14db98f3cb325ffd201971c9f7d4b5f1254d5
Heiher <r@hev.cc>
2015-08-13 08:23:22 -0700

    Bug 1194072 - IonMonkey: MIPS32: Add an option to mark JIT pages as non-writable. r=nbp CLOSED TREE
    --HG--
    extra : histedit_source : f4b8845bd4deea656f32d3d9c58809d8cacf00a4

--- js/src/jit/mips32/Assembler-mips32.cpp
+++ js/src/jit/mips32/Assembler-mips32.cpp
@@ -155,11 +155,12 @@ InstImm::extractImm16(BOffImm16* dest)
 
 // Used to patch jumps created by MacroAssemblerMIPSCompat::jumpWithPatch.
 void
-jit::PatchJump(CodeLocationJump& jump_, CodeLocationLabel label)
+jit::PatchJump(CodeLocationJump& jump_, CodeLocationLabel label, ReprotectCode reprotect)
 {
     Instruction* inst1 = (Instruction*)jump_.raw();
     Instruction* inst2 = inst1->next();
 
+    MaybeAutoWritableJitCode awjc(inst1, 8, reprotect);
     Assembler::UpdateLuiOriValue(inst1, inst2, (uint32_t)label.raw());
 
     AutoFlushICache::flush(uintptr_t(inst1), 8);
