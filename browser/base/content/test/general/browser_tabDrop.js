c4dbb1eeebf5af4b6a648dc2115d8fc7cde1b4c9
Tooru Fujisawa <arai_a@mac.com>
2015-08-15 07:20:22 +0900

    Bug 92737 - Part 4: Open multiple tabs when multiple items are dropped on tab. r=enndeakin

--- browser/base/content/test/general/browser_tabDrop.js
+++ browser/base/content/test/general/browser_tabDrop.js
@@ -17,23 +17,63 @@ add_task(function* test_setup() {
   Services.search.currentEngine = engine;
 });
 
-add_task(function*() { yield drop("mochi.test/first", true); });
-add_task(function*() { yield drop("javascript:'bad'"); });
-add_task(function*() { yield drop("jAvascript:'bad'"); });
-add_task(function*() { yield drop("search this", true); });
-add_task(function*() { yield drop("mochi.test/second", true); });
-add_task(function*() { yield drop("data:text/html,bad"); });
-add_task(function*() { yield drop("mochi.test/third", true); });
+add_task(function*() { yield dropText("mochi.test/first", 1); });
+add_task(function*() { yield dropText("javascript:'bad'"); });
+add_task(function*() { yield dropText("jAvascript:'bad'"); });
+add_task(function*() { yield dropText("search this", 1); });
+add_task(function*() { yield dropText("mochi.test/second", 1); });
+add_task(function*() { yield dropText("data:text/html,bad"); });
+add_task(function*() { yield dropText("mochi.test/third", 1); });
 
-function* drop(text, valid = false) {
-  info(`Starting test for text:${text}; valid:${valid}`);
+// Single text/plain item, with multiple links.
+add_task(function*() { yield dropText("mochi.test/1\nmochi.test/2", 2); });
+add_task(function*() { yield dropText("javascript:'bad1'\nmochi.test/3", 0); });
+add_task(function*() { yield dropText("mochi.test/4\ndata:text/html,bad1", 0); });
+
+// Multiple text/plain items, with single and multiple links.
+add_task(function*() {
+  yield drop([[{type: "text/plain",
+                data: "mochi.test/5"}],
+              [{type: "text/plain",
+                data: "mochi.test/6\nmochi.test/7"}]], 3);
+});
+
+// Single text/x-moz-url item, with multiple links.
+// "text/x-moz-url" has titles in even-numbered lines.
+add_task(function*() {
+  yield drop([[{type: "text/x-moz-url",
+                data: "mochi.test/8\nTITLE8\nmochi.test/9\nTITLE9"}]], 2);
+});
+
+// Single item with multiple types.
+add_task(function*() {
+  yield drop([[{type: "text/plain",
+                data: "mochi.test/10"},
+               {type: "text/x-moz-url",
+                data: "mochi.test/11\nTITLE11"}]], 1);
+});
+
+function dropText(text, expectedTabOpenCount=0) {
+  return drop([[{type: "text/plain", data: text}]], expectedTabOpenCount);
+}
+
+function* drop(dragData, expectedTabOpenCount=0) {
+  let dragDataString = JSON.stringify(dragData);
+  info(`Starting test for datagData:${dragDataString}; expectedTabOpenCount:${expectedTabOpenCount}`);
   let scriptLoader = Cc["@mozilla.org/moz/jssubscript-loader;1"].
                      getService(Ci.mozIJSSubScriptLoader);
   let EventUtils = {};
   scriptLoader.loadSubScript("chrome://mochikit/content/tests/SimpleTest/EventUtils.js", EventUtils);
 
   let awaitDrop = BrowserTestUtils.waitForEvent(gBrowser.tabContainer, "drop");
-  let awaitTabOpen = valid && BrowserTestUtils.waitForEvent(gBrowser.tabContainer, "TabOpen");
+  let actualTabOpenCount = 0;
+  let openedTabs = [];
+  let checkCount = function(event) {
+    openedTabs.push(event.target);
+    actualTabOpenCount++;
+    return actualTabOpenCount == expectedTabOpenCount;
+  };
+  let awaitTabOpen = expectedTabOpenCount && BrowserTestUtils.waitForEvent(gBrowser.tabContainer, "TabOpen", false, checkCount);
   // A drop type of "link" onto an existing tab would normally trigger a
   // load in that same tab, but tabbrowser code in _getDragTargetTab treats
   // drops on the outer edges of a tab differently (loading a new tab
@@ -46,15 +86,18 @@ function* drop(text, valid = false) {
     screenX: 0,
     screenY: 0,
   };
-  EventUtils.synthesizeDrop(gBrowser.selectedTab, gBrowser.selectedTab, [[{type: "text/plain", data: text}]], "link", window, undefined, event);
+  EventUtils.synthesizeDrop(gBrowser.selectedTab, gBrowser.selectedTab, dragData, "link", window, undefined, event);
+  let tabsOpened = false;
   let tabOpened = false;
   if (awaitTabOpen) {
-    let tabOpenEvent = yield awaitTabOpen;
+    yield awaitTabOpen;
     info("Got TabOpen event");
-    tabOpened = true;
-    yield BrowserTestUtils.removeTab(tabOpenEvent.target);
+    tabsOpened = true;
+    for (let tab of openedTabs) {
+      yield BrowserTestUtils.removeTab(tab);
+    }
   }
-  is(tabOpened, valid, `Tab for ${text} should only open if valid`);
+  is(tabsOpened, !!expectedTabOpenCount, `Tabs for ${dragDataString} should only open if any of dropped items are valid`);
 
   yield awaitDrop;
   ok(true, "Got drop event");
