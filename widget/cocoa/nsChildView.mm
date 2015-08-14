aa6245a25ac943d267209bbb0d78b09143eab9c4
Tooru Fujisawa <arai_a@mac.com>
2015-08-15 06:33:59 +0900

    Bug 966986 - Part 1: Fix format of DataTransfer.getData(text/x-moz-url) and return each text and URL as a separated item on Mac. r=smichaud, vlad

--- widget/cocoa/nsChildView.mm
+++ widget/cocoa/nsChildView.mm
@@ -3144,7 +3144,7 @@ GLPresenter::EndFrame()
 // in our native NSView (it is set in |draggingEntered:|). It is unset when the
 // drag session ends for this view, either with the mouse exiting or when a drop
 // occurs in this view.
-NSPasteboardWrapper* globalDragPboard = nil;
+NSPasteboard* globalDragPboard = nil;
 
 // gLastDragView and gLastDragMouseDownEvent are used to communicate information
 // to the drag service during drag invocation (starting a drag in from the view).
@@ -5745,8 +5745,7 @@ PanGestureTypeForEvent(NSEvent* aEvent)
   // Set the global drag pasteboard that will be used for this drag session.
   // This will be set back to nil when the drag session ends (mouse exits
   // the view or a drop happens within the view).
-  globalDragPboard =
-    [[NSPasteboardWrapper alloc] initWithPasteboard:[sender draggingPasteboard]];
+  globalDragPboard = [[sender draggingPasteboard] retain];
 
   return [self doDragAction:eDragEnter sender:sender];
 
