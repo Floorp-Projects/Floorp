24c1a79f569dea4209373d1c4f3290aeea0b8fa3
Andrew Halberstadt <ahalberstadt@mozilla.com>
2015-08-14 16:54:08 -0400

    Bug 1181261 - Merge configs from testing/config/mozharness into mozharness proper, r=chmanchester
    The config files under testing/config/mozharness were created so that certain mozharness options
    such as test harness arguments could ride the trees, simplifying a lot of logic in mozharness.
    But now that mozharness itself is in-tree, these configs no longer serve any purpose. Instead
    they are merged into the main configs at testing/mozharness/configs.

--- testing/mozharness/configs/web_platform_tests/test_config.py
+++ testing/mozharness/configs/web_platform_tests/test_config.py
@@ -5,10 +5,15 @@
 # ***** END LICENSE BLOCK *****
 
 config = {
-    # test harness options are located in the gecko tree
-    "in_tree_config": "config/mozharness/web_platform_tests_config.py",
-
-    "options": [],
+    "options": [
+        "--prefs-root=%(test_path)s/prefs",
+        "--processes=1",
+        "--config=%(test_path)s/wptrunner.ini",
+        "--ca-cert-path=%(test_path)s/certs/cacert.pem",
+        "--host-key-path=%(test_path)s/certs/web-platform.test.key",
+        "--host-cert-path=%(test_path)s/certs/web-platform.test.pem",
+        "--certutil-binary=%(test_install_path)s/bin/certutil",
+    ],
 
     "default_actions": [
         'clobber',
