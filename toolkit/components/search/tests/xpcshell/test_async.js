c280d066f8185d16313df47995d2e912e8b5d548
Florian Quèze <florian@queze.net>
2015-08-15 11:08:58 +0200

    Bug 1194265 - Some default search engines should only be visible in specific countries, r=Mossop. a=ritu

--- toolkit/components/search/tests/xpcshell/test_async.js
+++ toolkit/components/search/tests/xpcshell/test_async.js
@@ -25,6 +25,10 @@ function run_test() {
     let engine = Services.search.getEngineByName("bug645970");
     do_check_neq(engine, null);
 
+    // Check the hidden engine is not loaded.
+    engine = Services.search.getEngineByName("hidden");
+    do_check_eq(engine, null);
+
     do_test_finished();
   });
 }
