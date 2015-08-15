3f3f06f3b485025f7db5edea626869ca29653ccf
Andreas Tolfsen <ato@mozilla.com>
2015-08-15 15:46:31 +0100

    Bug 1194968: Support for Marionette protocol adjustments in Python client
    Whilst it is not a goal for the Marionette protocol to be fully compatible
    with WebDriver due to the different transport mechanisms, bug 1153822
    adjusts the Marionette protocol closer to what is prescribed in the
    WebDriver specification.
    
    This patch adds support for the new protocol (level or version 2)
    to the Python bindings with backwards compatibility for the earlier,
    existing protocol (1).
    
    r=jgriffin

--- testing/marionette/transport/marionette_transport/transport.py
+++ testing/marionette/transport/marionette_transport/transport.py
@@ -10,11 +10,11 @@ import time
 
 
 class MarionetteTransport(object):
-    """ The Marionette socket client.  This speaks the same protocol
-        as the remote debugger inside Gecko, in which messages are
-        always preceded by the message length and a colon, e.g.,
+    """The Marionette socket client.  This speaks the same protocol
+    as the remote debugger inside Gecko, in which messages are always
+    preceded by the message length and a colon, e.g.:
 
-        20:{'command': 'test'}
+        20:{"command": "test"}
     """
 
     max_packet_length = 4096
@@ -25,26 +25,25 @@ class MarionetteTransport(object):
         self.port = port
         self.socket_timeout = socket_timeout
         self.sock = None
-        self.traits = None
-        self.applicationType = None
-        self.actor = 'root'
+        self.protocol = 1
+        self.application_type = None
 
     def _recv_n_bytes(self, n):
-        """ Convenience method for receiving exactly n bytes from
-            self.sock (assuming it's open and connected).
+        """Convenience method for receiving exactly n bytes from self.sock
+        (assuming it's open and connected).
         """
-        data = ''
+        data = ""
         while len(data) < n:
             chunk = self.sock.recv(n - len(data))
-            if chunk == '':
+            if chunk == "":
                 break
             data += chunk
         return data
 
     def receive(self):
-        """ Receive the next complete response from the server, and return
-            it as a dict.  Each response from the server is prepended by
-            len(message) + ':'.
+        """Receive the next complete response from the server, and
+        return it as a JSON structure.  Each response from the server
+        is prepended by len(message) + ":".
         """
         assert(self.sock)
         now = time.time()
@@ -69,8 +68,10 @@ class MarionetteTransport(object):
         raise socket.timeout('connection timed out after %d s' % self.socket_timeout)
 
     def connect(self):
-        """ Connect to the server and process the hello message we expect
-            to receive in response.
+        """Connect to the server and process the hello message we expect
+        to receive in response.
+
+        Return a tuple of the protocol level and the application type.
         """
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.settimeout(self.socket_timeout)
@@ -82,27 +83,22 @@ class MarionetteTransport(object):
             self.sock = None
             raise
         self.sock.settimeout(2.0)
+
         hello = self.receive()
-        self.traits = hello.get('traits')
-        self.applicationType = hello.get('applicationType')
+        self.protocol = hello.get("marionetteProtocol", 1)
+        self.application_type = hello.get("applicationType")
 
-        # get the marionette actor id
-        response = self.send({'to': 'root', 'name': 'getMarionetteID'})
-        self.actor = response['id']
+        return (self.protocol, self.application_type)
 
-    def send(self, msg):
-        """ Send a message on the socket, prepending it with len(msg) + ':'.
-        """
+    def send(self, data):
+        """Send a message on the socket, prepending it with len(msg) + ":"."""
         if not self.sock:
             self.connect()
-        if 'to' not in msg:
-            msg['to'] = self.actor
-        data = json.dumps(msg)
-        data = '%s:%s' % (len(data), data)
+        data = "%s:%s" % (len(data), data)
 
         for packet in [data[i:i + self.max_packet_length] for i in
                        range(0, len(data), self.max_packet_length)]:
-            try: 
+            try:
                 self.sock.send(packet)
             except IOError as e:
                 if e.errno == errno.EPIPE:
@@ -110,12 +106,10 @@ class MarionetteTransport(object):
                 else:
                     raise e
 
-        response = self.receive()
-        return response
+        return self.receive()
 
     def close(self):
-        """ Close the socket.
-        """
+        """Close the socket."""
         if self.sock:
             self.sock.close()
         self.sock = None
