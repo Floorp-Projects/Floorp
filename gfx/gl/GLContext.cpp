4cb8b3a4cb65fe64ce42225b696d170e1cdb3b8a
David Anderson <dvander@alliedmods.net>
2015-08-14 22:14:00 -0700

    Add driver crash guards to WebGL (bug 1190281 part 9, r=jgilbert,mattwoodrow)

--- gfx/gl/GLContext.cpp
+++ gfx/gl/GLContext.cpp
@@ -30,6 +30,7 @@
 #include "TextureGarbageBin.h"
 #include "gfx2DGlue.h"
 #include "gfxPrefs.h"
+#include "DriverCrashGuard.h"
 #include "mozilla/IntegerPrintfMacros.h"
 
 #include "OGLShaderProgram.h" // for ShaderProgramType
@@ -366,6 +367,11 @@ GLContext::InitWithPrefix(const char *prefix, bool trygl)
     if (mInitialized) {
         reporter.SetSuccessful();
         return true;
+    }
+
+    GLContextCrashGuard crashGuard;
+    if (crashGuard.Crashed()) {
+        return false;
     }
 
     mWorkAroundDriverBugs = gfxPrefs::WorkAroundDriverBugs();
