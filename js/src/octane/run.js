a50019d29264492494376cd530810e8b5c967ed9
Heiher <r@hev.cc>
2015-08-17 00:26:00 -0400

    Bug 1195198 - Octane: Restore run.js broken by Bug 1179063. r=terrence
    --HG--
    extra : rebase_source : 84a50961a198a094da642d7917007bc988282ea9

--- js/src/octane/run.js
+++ js/src/octane/run.js
@@ -28,7 +28,6 @@
 
 var base_dir = '';
 load(base_dir + 'base.js');
-/*
 load(base_dir + 'richards.js');
 load(base_dir + 'deltablue.js');
 load(base_dir + 'crypto.js');
@@ -43,14 +42,11 @@ load(base_dir + 'gbemu-part1.js');
 load(base_dir + 'gbemu-part2.js');
 load(base_dir + 'code-load.js');
 load(base_dir + 'box2d.js');
-*/
 load(base_dir + 'zlib.js');
 load(base_dir + 'zlib-data.js');
-/*
 load(base_dir + 'typescript.js');
 load(base_dir + 'typescript-input.js');
 load(base_dir + 'typescript-compiler.js');
-*/
 
 var success = true;
 
