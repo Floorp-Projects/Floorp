8203f9fd80792dfa64d1614441cb95d840181b76
John Lin <jolin@mozilla.com>
2015-08-17 15:12:43 -0700

    Bug 1186845 - Let MP4Decoder handle 3GPP files if Gonk PDM is available. r=cpearce
    --HG--
    extra : rebase_source : 22a376b9b4724169b2e5661aa4c724cd6303fa50

--- dom/media/fmp4/MP4Decoder.cpp
+++ dom/media/fmp4/MP4Decoder.cpp
@@ -14,6 +14,7 @@
 #include "mozilla/CDMProxy.h"
 #endif
 #include "mozilla/Logging.h"
+#include "nsMimeTypes.h"
 
 #ifdef XP_WIN
 #include "mozilla/WindowsVersion.h"
@@ -140,6 +141,12 @@ MP4Decoder::CanHandleMediaType(const nsACString& aType,
                                   aOutContainsAAC,
                                   aOutContainsMP3));
   }
+
+#ifdef MOZ_GONK_MEDIACODEC
+  if (aType.EqualsASCII(VIDEO_3GPP)) {
+    return Preferences::GetBool("media.fragmented-mp4.gonk.enabled", false);
+  }
+#endif
 
   if (!aType.EqualsASCII("video/mp4") ||
       !MP4Decoder::CanCreateH264Decoder()) {
