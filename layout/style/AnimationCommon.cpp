2cc4c76af9c5a7e0912e7a3649d344134a927e62
Brian Birtles <birtles@gmail.com>
2015-08-17 13:59:44 +0900

    Bug 1188251 part 7 - Move WillRefresh to CommonAnimationManager; r=dholbert
    nsTransitionManager::WillRefresh and nsAnimationManager::WillRefresh are now
    identical and all methods they call exist on CommonAnimationManager so we
    can unify them there.

--- layout/style/AnimationCommon.cpp
+++ layout/style/AnimationCommon.cpp
@@ -466,6 +466,24 @@ CommonAnimationManager::LayerAnimationRecordFor(nsCSSProperty aProperty)
   return nullptr;
 }
 
+/* virtual */ void
+CommonAnimationManager::WillRefresh(TimeStamp aTime)
+{
+  MOZ_ASSERT(mPresContext,
+             "refresh driver should not notify additional observers "
+             "after pres context has been destroyed");
+  if (!mPresContext->GetPresShell()) {
+    // Someone might be keeping mPresContext alive past the point
+    // where it has been torn down; don't bother doing anything in
+    // this case.  But do get rid of all our animations so we stop
+    // triggering refreshes.
+    RemoveAllElementCollections();
+    return;
+  }
+
+  FlushAnimations(Can_Throttle);
+}
+
 #ifdef DEBUG
 /* static */ void
 CommonAnimationManager::Initialize()
