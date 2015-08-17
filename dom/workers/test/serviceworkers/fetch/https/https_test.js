b5681e95be44a98d1c4aea2c4c4a73b03d8e1169
Nikhil Marathe <nsm.nikhil@gmail.com>
2015-08-17 15:08:58 -0700

    Bug 1193911 - Ensure synthetic Responses gets a valid channel info. r=ehsan
    Right now, synthetic Responses did not have a valid channel info. When these
    were saved in the Cache, and then restored, the restored Response did have
    a ChannelInfo, but that ChannelInfo did not have a valid security info.
    Passing this to respondWith() then caused the interception to fail.
    
    This patch modifies Response::Constructor() to initialize its ChannelInfo from
    the global. ChannelInfo can now initialize itself from a nsIDocument. All
    workers now store their ChannelInfo on the WorkerLoadInfo.
    
    --HG--
    extra : commitid : L1wltwPICd8
    extra : rebase_source : 8dab4c414eb50e02a00dd2cb3ee848b811060e70

--- dom/workers/test/serviceworkers/fetch/https/https_test.js
+++ dom/workers/test/serviceworkers/fetch/https/https_test.js
@@ -1,11 +1,20 @@
 self.addEventListener("install", function(event) {
   event.waitUntil(caches.open("cache").then(function(cache) {
-    return cache.add("index.html");
+    var synth = new Response('<!DOCTYPE html><script>window.parent.postMessage({status: "done-synth-sw"}, "*");</script>',
+                             {headers:{"Content-Type": "text/html"}});
+    return Promise.all([
+      cache.add("index.html"),
+      cache.put("synth-sw.html", synth),
+    ]);
   }));
 });
 
 self.addEventListener("fetch", function(event) {
   if (event.request.url.indexOf("index.html") >= 0) {
+    event.respondWith(caches.match(event.request));
+  } else if (event.request.url.indexOf("synth-sw.html") >= 0) {
+    event.respondWith(caches.match(event.request));
+  } else if (event.request.url.indexOf("synth-window.html") >= 0) {
     event.respondWith(caches.match(event.request));
   } else if (event.request.url.indexOf("synth.html") >= 0) {
     event.respondWith(new Response('<!DOCTYPE html><script>window.parent.postMessage({status: "done-synth"}, "*");</script>',
