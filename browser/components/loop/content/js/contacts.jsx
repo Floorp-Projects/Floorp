01c455d305cd536d1b5d68d18330e4c7890e8099
Andrei Oprea <andrei.br92@gmail.com>
2015-08-18 14:36:43 -0700

    Bug 1183615 - Implement refreshed design for the contacts list. r=mdeboer

--- browser/components/loop/content/js/contacts.jsx
+++ browser/components/loop/content/js/contacts.jsx
@@ -413,6 +413,63 @@ loop.contacts = (function(_, mozL10n) {
       window.removeEventListener("LoopStatusChanged", this._onStatusChanged);
     },
 
+    /*
+     * Filter a user by name, email or phone number.
+     * Takes in an input to filter by and returns a filter function which
+     * expects a contact.
+     *
+     * @returns {Function}
+     */
+    filterContact: function(filter) {
+      return function(contact) {
+        return getPreferred(contact, "name").toLocaleLowerCase().includes(filter) ||
+          getPreferred(contact, "email").value.toLocaleLowerCase().includes(filter) ||
+          getPreferred(contact, "tel").value.toLocaleLowerCase().includes(filter);
+      };
+    },
+
+    /*
+     * Takes all contacts, it groups and filters them before rendering.
+     */
+    _filterContactsList: function() {
+      let shownContacts = _.groupBy(this.contacts, function(contact) {
+        return contact.blocked ? "blocked" : "available";
+      });
+
+      if (this._shouldShowFilter()) {
+        let filter = this.state.filter.trim().toLocaleLowerCase();
+        let filterFn = this.filterContact(filter);
+        if (filter) {
+          if (shownContacts.available) {
+            shownContacts.available = shownContacts.available.filter(filterFn);
+            // Filter can return an empty array.
+            if (!shownContacts.available.length) {
+              shownContacts.available = null;
+            }
+          }
+          if (shownContacts.blocked) {
+            shownContacts.blocked = shownContacts.blocked.filter(filterFn);
+            // Filter can return an empty array.
+            if (!shownContacts.blocked.length) {
+              shownContacts.blocked = null;
+            }
+          }
+        }
+      }
+
+      return shownContacts;
+    },
+
+    /*
+     * Decide to render contacts filter based on the number of contacts.
+     *
+     * @returns {bool}
+     */
+    _shouldShowFilter: function() {
+      return Object.getOwnPropertyNames(this.contacts).length >=
+        MIN_CONTACTS_FOR_FILTERING;
+    },
+
     _onStatusChanged: function() {
       let profile = this.props.mozLoop.userProfile;
       let currUid = this._userProfile ? this._userProfile.uid : null;
@@ -531,6 +588,16 @@ loop.contacts = (function(_, mozL10n) {
       this.refresh();
     },
 
+    /*
+     * Callback triggered when clicking the `X` from the contacts filter.
+     * Clears the search query.
+     */
+    _handleFilterClear: function() {
+      this.setState({
+        filter: ""
+      });
+    },
+
     sortContacts: function(contact1, contact2) {
       let comp = contact1.name[0].localeCompare(contact2.name[0]);
       if (comp !== 0) {
@@ -541,9 +608,35 @@ loop.contacts = (function(_, mozL10n) {
       return contact1._guid - contact2._guid;
     },
 
+    _renderFilterClearButton: function() {
+      if (this.state.filter) {
+        return (
+          <button className="clear-search"
+                  onClick={this._handleFilterClear} />
+        );
+      }
+
+      return null;
+    },
+
+    _renderContactsFilter: function() {
+      if (this._shouldShowFilter()) {
+        return (
+          <div className="contact-filter-container">
+            <input className="contact-filter"
+                   placeholder={mozL10n.get("contacts_search_placesholder")}
+                   valueLink={this.linkState("filter")} />
+            {this._renderFilterClearButton()}
+          </div>
+        );
+      }
+
+      return null;
+    },
+
     _renderContactsList: function() {
       let cx = React.addons.classSet;
-
+      let shownContacts = this._filterContactsList();
       let viewForItem = item => {
         return (
           <ContactDetail contact={item}
@@ -552,92 +645,100 @@ loop.contacts = (function(_, mozL10n) {
         );
       };
 
-      let shownContacts = _.groupBy(this.contacts, function(contact) {
-        return contact.blocked ? "blocked" : "available";
-      });
-
-      let showFilter = Object.getOwnPropertyNames(this.contacts).length >=
-                       MIN_CONTACTS_FOR_FILTERING;
-      if (showFilter) {
-        let filter = this.state.filter.trim().toLocaleLowerCase();
-        if (filter) {
-          let filterFn = contact => {
-            return contact.name[0].toLocaleLowerCase().includes(filter) ||
-                   getPreferred(contact, "email").value.toLocaleLowerCase().includes(filter);
-          };
-          if (shownContacts.available) {
-            shownContacts.available = shownContacts.available.filter(filterFn);
-          }
-          if (shownContacts.blocked) {
-            shownContacts.blocked = shownContacts.blocked.filter(filterFn);
-          }
-        }
+      // If no contacts to show and filter is set, then none match the search.
+      if (!shownContacts.available && !shownContacts.blocked &&
+          this.state.filter) {
+        return (
+          <div className="contact-search-list-empty">
+            <p className="panel-text-large">
+              {mozL10n.get("no_search_results_message_heading")}
+            </p>
+            <p className="panel-text-medium">
+              {mozL10n.get("no_search_results_message_subheading")}
+            </p>
+          </div>
+        );
       }
 
-      if (shownContacts.available || shownContacts.blocked) {
+      // If no contacts to show and filter is not set, we don't have contacts.
+      if (!shownContacts.available && !shownContacts.blocked &&
+          !this.state.filter) {
         return (
-          <div>
-            <div className="contact-list-title">
-              {mozL10n.get("contact_list_title")}
-            </div>
-            <ul className="contact-list">
-              {shownContacts.available ?
-                shownContacts.available.sort(this.sortContacts).map(viewForItem) :
-                null}
-              {shownContacts.blocked && shownContacts.blocked.length > 0 ?
-                <div className="contact-separator">{mozL10n.get("contacts_blocked_contacts")}</div> :
-                null}
-              {shownContacts.blocked ?
-                shownContacts.blocked.sort(this.sortContacts).map(viewForItem) :
-                null}
-            </ul>
+          <div className="contact-list-empty">
+            <p className="panel-text-large">
+              {mozL10n.get("no_contacts_message_heading")}
+            </p>
+            <p className="panel-text-medium">
+              {mozL10n.get("no_contacts_import_or_add")}
+            </p>
           </div>
         );
       }
 
       return (
-        <div className="contact-list-empty">
-          <p className="panel-text-large">
-            {mozL10n.get("no_contacts_message_heading")}
-          </p>
-          <p className="panel-text-medium">
-            {mozL10n.get("no_contacts_import_or_add")}
-          </p>
+        <div>
+          {!this.state.filter ? <div className="contact-list-title">
+                                  {mozL10n.get("contact_list_title")}
+                                </div> : null}
+          <ul className="contact-list">
+            {shownContacts.available ?
+              shownContacts.available.sort(this.sortContacts).map(viewForItem) :
+              null}
+            {shownContacts.blocked && shownContacts.blocked.length > 0 ?
+              <div className="contact-separator">{mozL10n.get("contacts_blocked_contacts")}</div> :
+              null}
+            {shownContacts.blocked ?
+              shownContacts.blocked.sort(this.sortContacts).map(viewForItem) :
+              null}
+          </ul>
+        </div>
+      );
+    },
+
+    _renderAddContactButtons: function() {
+      let cx = React.addons.classSet;
+
+      if (this.state.filter) {
+        return null;
+      }
+
+      return (
+        <ButtonGroup additionalClass="contact-controls">
+          <Button additionalClass="secondary"
+            caption={this.state.importBusy ? mozL10n.get("importing_contacts_progress_button") :
+                                             mozL10n.get("import_contacts_button3")}
+              disabled={this.state.importBusy}
+              onClick={this.handleImportButtonClick} >
+              <div className={cx({"contact-import-spinner": true,
+                                 spinner: true,
+              busy: this.state.importBusy})} />
+          </Button>
+          <Button additionalClass="primary"
+            caption={mozL10n.get("new_contact_button")}
+            onClick={this.handleAddContactButtonClick} />
+        </ButtonGroup>
+      );
+    },
+
+    _renderGravatarPromoMessage: function() {
+      if (this.state.filter) {
+        return null;
+      }
+
+      return (
+        <div className="content-area">
+          <GravatarPromo handleUse={this.handleUseGravatar}/>
         </div>
       );
     },
 
     render: function() {
-      let cx = React.addons.classSet;
-      let showFilter = Object.getOwnPropertyNames(this.contacts).length >=
-                       MIN_CONTACTS_FOR_FILTERING;
-
       return (
         <div>
-          <div className="content-area">
-            {showFilter ?
-            <input className="contact-filter"
-                   placeholder={mozL10n.get("contacts_search_placesholder")}
-                   valueLink={this.linkState("filter")} />
-            : null }
-            <GravatarPromo handleUse={this.handleUseGravatar}/>
-          </div>
+          {this._renderContactsFilter()}
+          {this._renderGravatarPromoMessage()}
           {this._renderContactsList()}
-          <ButtonGroup additionalClass="contact-controls">
-            <Button additionalClass="secondary"
-              caption={this.state.importBusy
-                ? mozL10n.get("importing_contacts_progress_button")
-                : mozL10n.get("import_contacts_button3")}
-                disabled={this.state.importBusy}
-                onClick={this.handleImportButtonClick} >
-                <div className={cx({"contact-import-spinner": true,
-                                   spinner: true,
-                busy: this.state.importBusy})} />
-            </Button>
-            <Button additionalClass="primary"
-              caption={mozL10n.get("new_contact_button")}
-              onClick={this.handleAddContactButtonClick} />
-          </ButtonGroup>
+          {this._renderAddContactButtons()}
         </div>
       );
     }
