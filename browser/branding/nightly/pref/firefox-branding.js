39a53f6f52db7ebbbdf9a8d0bebe8e9d7172ed39
Kev Needham <kev@mozilla.com>
2015-08-18 10:50:00 -0400

    Bug 1195683 - Restore "fr" preferences for non-US Yahoo! searchplugins. r=Mossop, a=sledru
    --HG--
    extra : transplant_source : J%88%F0%7F%B06%DA%0DD%DE%D9bI%D9%E0%1F%C8E%24z

--- browser/branding/nightly/pref/firefox-branding.js
+++ browser/branding/nightly/pref/firefox-branding.js
@@ -24,6 +24,10 @@ pref("app.update.url.details", "https://nightly.mozilla.org");
 // app.update.checkInstallTime is true.
 pref("app.update.checkInstallTime.days", 2);
 
+// code usage depends on contracts, please contact the Firefox module owner if you have questions
+pref("browser.search.param.yahoo-fr", "moz35");
+pref("browser.search.param.yahoo-fr-ja", "mozff");
+
 // Number of usages of the web console or scratchpad.
 // If this is less than 5, then pasting code into the web console or scratchpad is disabled
 pref("devtools.selfxss.count", 5);
