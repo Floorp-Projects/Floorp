3390d15215160bf619ac973148b4b5b575e668f8
Seth Fowler <mark.seth.fowler@gmail.com>
2015-08-18 10:19:26 -0700

    Bug 1187401 (Part 2) - Eliminate the nsresult return value from RasterImage::SetMetadata, since it's not used anymore. r=tn

--- image/RasterImage.cpp
+++ image/RasterImage.cpp
@@ -942,20 +942,20 @@ RasterImage::OnAddedFrame(uint32_t aNewFrameCount,
   }
 }
 
-nsresult
+void
 RasterImage::SetMetadata(const ImageMetadata& aMetadata,
                          bool aFromMetadataDecode)
 {
   MOZ_ASSERT(NS_IsMainThread());
 
   if (mError) {
-    return NS_ERROR_FAILURE;
+    return;
   }
 
   if (aMetadata.HasSize()) {
     IntSize size = aMetadata.GetSize();
     if (size.width < 0 || size.height < 0) {
-      return NS_ERROR_INVALID_ARG;
+      return;
     }
 
     MOZ_ASSERT(aMetadata.HasOrientation());
@@ -966,7 +966,7 @@ RasterImage::SetMetadata(const ImageMetadata& aMetadata,
       NS_WARNING("Image changed size or orientation on redecode! "
                  "This should not happen!");
       DoError();
-      return NS_ERROR_UNEXPECTED;
+      return;
     }
 
     // Set the size and flag that we have it.
@@ -1010,8 +1010,6 @@ RasterImage::SetMetadata(const ImageMetadata& aMetadata,
     Set("hotspotX", intwrapx);
     Set("hotspotY", intwrapy);
   }
-
-  return NS_OK;
 }
 
 NS_IMETHODIMP
