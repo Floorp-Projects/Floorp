dea9486c28e6a96480a33ff6771e4eafeaacad80
Mike Hommey <mh+mozilla@glandium.org>
2015-08-19 15:55:34 +0900

    Bug 1120793 - Remove obsolete _Throw wrapping. r=froydnj

--- memory/mozalloc/throw_msvc.h
+++ memory/mozalloc/throw_msvc.h
@@ -10,8 +10,6 @@
 
 #if defined(MOZ_MSVC_STL_WRAP_RAISE)
 #  include "msvc_raise_wrappers.h"
-#elif defined(MOZ_MSVC_STL_WRAP_Throw)
-#  include "msvc_throw_wrapper.h"
 #else
 #  error "Unknown STL wrapper tactic"
 #endif
