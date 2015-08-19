2592182850685bc61c241099bc15ade06a975670
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2015-08-19 15:11:15 -0700

    Bug 1191178 - Part 1: Add a function to check frame timing preference value. r=smaug

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -254,6 +254,7 @@ bool nsContentUtils::sInitialized = false;
 bool nsContentUtils::sIsFullScreenApiEnabled = false;
 bool nsContentUtils::sTrustedFullScreenOnly = true;
 bool nsContentUtils::sIsCutCopyAllowed = true;
+bool nsContentUtils::sIsFrameTimingPrefEnabled = false;
 bool nsContentUtils::sIsPerformanceTimingEnabled = false;
 bool nsContentUtils::sIsResourceTimingEnabled = false;
 bool nsContentUtils::sIsUserTimingLoggingEnabled = false;
@@ -535,6 +536,9 @@ nsContentUtils::Init()
 
   Preferences::AddBoolVarCache(&sIsUserTimingLoggingEnabled,
                                "dom.performance.enable_user_timing_logging", false);
+
+  Preferences::AddBoolVarCache(&sIsFrameTimingPrefEnabled,
+                               "dom.enable_frame_timing", true);
 
   Preferences::AddBoolVarCache(&sIsExperimentalAutocompleteEnabled,
                                "dom.forms.autocomplete.experimental", false);
@@ -6760,6 +6764,13 @@ nsContentUtils::IsCutCopyAllowed()
   return (!IsCutCopyRestricted() &&
           EventStateManager::IsHandlingUserInput()) ||
          IsCallerChrome();
+}
+
+/* static */
+bool
+nsContentUtils::IsFrameTimingEnabled()
+{
+  return sIsFrameTimingPrefEnabled;
 }
 
 /* static */
