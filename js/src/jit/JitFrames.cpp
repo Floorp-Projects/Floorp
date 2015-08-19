bcd20cc9ef8e6c950e290a3da0955c30dfebab60
Eric Faust <efaustbmo@gmail.com>
2015-08-19 11:19:47 -0700

    Bug 1180054 - Fix bogus assert. (r=jandem)

--- js/src/jit/JitFrames.cpp
+++ js/src/jit/JitFrames.cpp
@@ -3292,7 +3292,9 @@ AssertJitStackInvariants(JSContext* cx)
 #if defined(JS_CODEGEN_X86)
                     + sizeof(void*) /* frame pointer */
 #endif
-                    + sizeof(Value) * (frames.callee()->nargs() + 1 /* |this| argument */ )
+                    + sizeof(Value) * (frames.callee()->nargs() +
+                                       1 /* |this| argument */ +
+                                       frames.isConstructing() /* new.target */)
                     + sizeof(JitFrameLayout);
                 MOZ_RELEASE_ASSERT(frameSize >= expectedFrameSize,
                   "The frame is large enough to hold all arguments");
