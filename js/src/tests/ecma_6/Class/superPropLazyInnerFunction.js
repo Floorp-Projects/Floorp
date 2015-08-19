8bfc540e2140565adf10508796993a4de01bd380
Eric Faust <efaustbmo@gmail.com>
2015-08-19 11:19:46 -0700

    Bug 1185961 - Properly install home object on methods in classes in lazy scripts. (r=shu)

---
+++ js/src/tests/ecma_6/Class/superPropLazyInnerFunction.js
@@ -0,0 +1,24 @@
+var test = `
+testcase();
+function testcase() {
+    var tokenCodes  = {
+      get try() {
+        try {
+          super.actual();
+        } catch (e) {}
+      }
+    };
+    var arr = [
+        'try',
+    ];
+    for (var i = 0; i < arr.length; i++) {
+        if (tokenCodes[arr[i]] !== i) {};
+    }
+}
+`;
+
+if (classesEnabled())
+    eval(test);
+
+if (typeof reportCompare === 'function')
+    reportCompare(0,0,"OK");
