9b6811fa018afd253413a5b48fb840c8f0efa0d0
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2015-08-19 16:32:54 +0100

    Bug 1192036 - import cookies from Edge, r=MattN
    --HG--
    extra : commitid : Atr9jgZIJ7U
    extra : rebase_source : 9688872e98ee1714500b8a853542f98224ac38fd

--- browser/components/migration/tests/unit/test_Edge_availability.js
+++ browser/components/migration/tests/unit/test_Edge_availability.js
@@ -1,7 +1,16 @@
+const EDGE_AVAILABLE_MIGRATIONS = 
+  MigrationUtils.resourceTypes.COOKIES |
+  MigrationUtils.resourceTypes.BOOKMARKS;
+
 add_task(function* () {
   let migrator = MigrationUtils.getMigrator("edge");
   Cu.import("resource://gre/modules/AppConstants.jsm");
   Assert.equal(!!(migrator && migrator.sourceExists), AppConstants.isPlatformAndVersionAtLeast("win", "10"),
                "Edge should be available for migration if and only if we're on Win 10+");
+  if (migrator) {
+    let migratableData = migrator.getMigrateData(null, false);
+    Assert.equal(migratableData, EDGE_AVAILABLE_MIGRATIONS,
+                 "All the data types we expect should be available");
+  }
 });
 
