fe819ad8d74ab67516061802420e60f14a2e4ebb
Nikhil Marathe <nsm.nikhil@gmail.com>
2015-08-19 16:21:25 -0700

    Bug 1180861 - Various ServiceWorker registration fixes to get test passing. r=bkelly,jgraham.
    This commit implements the following changes to get registration.https.html working.
    1) Fail with NS_ERROR_DOM_SECURITY_ERR where the spec requires it.
    2) Propagate JSExnType to ServiceWorkerManager::HandleError() so that a JS
       exception object with the correct .name can be created.
    3) Fail with security error on redirect failure.
    4) Check fetched script's mimetype.
    5) Add missing python server files for web-platform-tests.
    6) Update web-platform-tests expected data.
    7) Several tests have been changed to use TypeError or more appropriate JS
       errors based on my reading of the spec.
    
    --HG--
    extra : commitid : IxWo2IVUweU
    extra : rebase_source : c3c1ead153027bf84e7f239fd7125224fe25c3c0

--- dom/workers/ServiceWorkerContainer.cpp
+++ dom/workers/ServiceWorkerContainer.cpp
@@ -106,9 +106,11 @@ CheckForSlashEscapedCharsInPath(nsIURI* aURI)
 {
   MOZ_ASSERT(aURI);
 
+  // A URL that can't be downcast to a standard URL is an invalid URL and should
+  // be treated as such and fail with SecurityError.
   nsCOMPtr<nsIURL> url(do_QueryInterface(aURI));
   if (NS_WARN_IF(!url)) {
-    return NS_ERROR_FAILURE;
+    return NS_ERROR_DOM_SECURITY_ERR;
   }
 
   nsAutoCString path;
