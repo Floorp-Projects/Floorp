11a919f51cbd79670e38185031beacc678d4ee1a
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-08-19 15:20:09 +1000

    Bug 1195073: [MSE/webm] P1. Detect individual webm clusters. r=kinetik a=ritu
    WebMContainerParser was incorrectly reporting webm blocks rather than clusters, causing the webm demuxer to later fail to parse the remaining data.

--- dom/media/mediasource/ContainerParser.cpp
+++ dom/media/mediasource/ContainerParser.cpp
@@ -11,6 +11,7 @@
 #include "mozilla/ErrorResult.h"
 #include "mp4_demuxer/MoofParser.h"
 #include "mozilla/Logging.h"
+#include "mozilla/Maybe.h"
 #include "MediaData.h"
 #ifdef MOZ_FMP4
 #include "MP4Stream.h"
@@ -177,16 +178,6 @@ public:
         (*aData)[3] == 0x6b) {
       return true;
     }
-    // 0xa3 // SimpleBlock
-    if (aData->Length() >= 1 &&
-        (*aData)[0] == 0xa3) {
-      return true;
-    }
-    // 0xa1 // Block
-    if (aData->Length() >= 1 &&
-        (*aData)[0] == 0xa1) {
-      return true;
-    }
     return false;
   }
 
@@ -195,6 +186,17 @@ public:
   {
     bool initSegment = IsInitSegmentPresent(aData);
     if (initSegment) {
+      if (mLastMapping) {
+        // The last data contained a complete cluster but we can only detect it
+        // now that a new one is starting.
+        // We use mOffset as end position to ensure that any blocks not reported
+        // by WebMBufferParser are properly skipped.
+        mCompleteMediaSegmentRange = MediaByteRange(mLastMapping.ref().mSyncOffset,
+                                                    mOffset);
+        mLastMapping.reset();
+        MSE_DEBUG(WebMContainerParser, "New cluster found at start, ending previous one");
+        return false;
+      }
       mOffset = 0;
       mParser = WebMBufferedParser(0);
       mOverlappedMapping.Clear();
@@ -242,38 +244,71 @@ public:
       return false;
     }
 
-    uint32_t endIdx = mapping.Length() - 1;
-
-    // Calculate media range for first media segment
-    uint32_t segmentEndIdx = endIdx;
-    while (mapping[0].mSyncOffset != mapping[segmentEndIdx].mSyncOffset) {
-      segmentEndIdx -= 1;
-    }
-    if (segmentEndIdx > 0 && mOffset >= mapping[segmentEndIdx].mEndOffset) {
-      mCompleteMediaHeaderRange = MediaByteRange(mParser.mInitEndOffset,
-                                                 mapping[0].mEndOffset);
-      mCompleteMediaSegmentRange = MediaByteRange(mParser.mInitEndOffset,
-                                                  mapping[segmentEndIdx].mEndOffset);
-    }
-
-    // Exclude frames that we don't have enough data to cover the end of.
-    while (mOffset < mapping[endIdx].mEndOffset && endIdx > 0) {
-      endIdx -= 1;
-    }
-
-    if (endIdx == 0) {
+    if (mLastMapping &&
+        mLastMapping.ref().mSyncOffset != mapping[0].mSyncOffset) {
+      // The last data contained a complete cluster but we can only detect it
+      // now that a new one is starting.
+      // We use the start of the next cluster as end position to ensure that any
+      // blocks not reported by WebMBufferParser is properly skipped.
+      mCompleteMediaSegmentRange = MediaByteRange(mLastMapping.ref().mSyncOffset,
+                                                  mapping[0].mSyncOffset);
+      mOverlappedMapping.AppendElements(mapping);
+      mLastMapping.reset();
+      MSE_DEBUG(WebMContainerParser, "New cluster found at start, ending previous one");
       return false;
     }
 
-    uint64_t frameDuration = mapping[endIdx].mTimecode - mapping[endIdx - 1].mTimecode;
+    // Calculate media range for first media segment.
+
+    // Check if we have a cluster finishing in the current data.
+    uint32_t endIdx = mapping.Length() - 1;
+    bool foundNewCluster = false;
+    while (mapping[0].mSyncOffset != mapping[endIdx].mSyncOffset) {
+      endIdx -= 1;
+      foundNewCluster = true;
+    }
+
+    int32_t completeIdx = endIdx;
+    while (completeIdx >= 0 && mOffset < mapping[completeIdx].mEndOffset) {
+      MSE_DEBUG(WebMContainerParser, "block is incomplete, missing: %lld",
+                mapping[completeIdx].mEndOffset - mOffset);
+      completeIdx -= 1;
+    }
+
+    // Save parsed blocks for which we do not have all data yet.
+    mOverlappedMapping.AppendElements(mapping.Elements() + completeIdx + 1,
+                                      mapping.Length() - completeIdx - 1);
+
+    if (completeIdx < 0) {
+      mLastMapping.reset();
+      return false;
+    }
+
+    if (mCompleteMediaHeaderRange.IsNull()) {
+      mCompleteMediaHeaderRange = MediaByteRange(mapping[0].mSyncOffset,
+                                                 mapping[0].mEndOffset);
+    }
+    mLastMapping = Some(mapping[completeIdx]);
+
+    if (foundNewCluster && mOffset >= mapping[endIdx].mEndOffset) {
+      // We now have all information required to delimit a complete cluster.
+      mCompleteMediaSegmentRange = MediaByteRange(mapping[endIdx].mSyncOffset,
+                                                  mapping[endIdx].mEndOffset);
+    }
+
+    if (!completeIdx) {
+      return false;
+    }
+
+    uint64_t frameDuration =
+      mapping[completeIdx].mTimecode - mapping[completeIdx - 1].mTimecode;
     aStart = mapping[0].mTimecode / NS_PER_USEC;
-    aEnd = (mapping[endIdx].mTimecode + frameDuration) / NS_PER_USEC;
+    aEnd = (mapping[completeIdx].mTimecode + frameDuration) / NS_PER_USEC;
 
-    MSE_DEBUG(WebMContainerParser, "[%lld, %lld] [fso=%lld, leo=%lld, l=%u endIdx=%u]",
-              aStart, aEnd, mapping[0].mSyncOffset, mapping[endIdx].mEndOffset, mapping.Length(), endIdx);
-
-    mapping.RemoveElementsAt(0, endIdx + 1);
-    mOverlappedMapping.AppendElements(mapping);
+    MSE_DEBUG(WebMContainerParser, "[%lld, %lld] [fso=%lld, leo=%lld, l=%u processedIdx=%u fs=%lld]",
+              aStart, aEnd, mapping[0].mSyncOffset,
+              mapping[completeIdx].mEndOffset, mapping.Length(), completeIdx,
+              mCompleteMediaSegmentRange.mEnd);
 
     return true;
   }
@@ -288,6 +323,7 @@ private:
   WebMBufferedParser mParser;
   nsTArray<WebMTimeDataOffset> mOverlappedMapping;
   int64_t mOffset;
+  Maybe<WebMTimeDataOffset> mLastMapping;
 };
 
 #ifdef MOZ_FMP4
