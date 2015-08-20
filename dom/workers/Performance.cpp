ba23b80bbc10de4eb5ac8fa59d1a224c0b518294
Salvador de la Puente <salva@unoyunodiez.com>
2015-08-20 14:34:22 -0700

    Bug 1196157 - Marks left by performance marks should print the domain of the application and not the complete URL. r=baku

--- dom/workers/Performance.cpp
+++ dom/workers/Performance.cpp
@@ -68,8 +68,13 @@ void
 Performance::InsertUserEntry(PerformanceEntry* aEntry)
 {
   if (mWorkerPrivate->PerformanceLoggingEnabled()) {
-    PerformanceBase::LogEntry(aEntry,
-                              NS_ConvertUTF16toUTF8(mWorkerPrivate->ScriptURL()));
+    nsAutoCString uri;
+    nsCOMPtr<nsIURI> scriptURI = mWorkerPrivate->GetResolvedScriptURI();
+    if (!scriptURI || NS_FAILED(scriptURI->GetHost(uri))) {
+      // If we have no URI, just put in "none".
+      uri.AssignLiteral("none");
+    }
+    PerformanceBase::LogEntry(aEntry, uri);
   }
   PerformanceBase::InsertUserEntry(aEntry);
 }
