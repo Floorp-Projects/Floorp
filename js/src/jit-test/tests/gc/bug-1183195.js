533f98b01ca8d479ffb1706bef7ab5f94ec26cfc
Nathan Froyd <froydnj@mozilla.com>
2015-08-19 20:13:48 -0400

    Backout 8b61aebcd9bc (bug 1183195) for x64 Windows 8 JIT failures

--- js/src/jit-test/tests/gc/bug-1183195.js
+++
@@ -1,9 +0,0 @@
-// |jit-test| allow-oom; allow-unhandlable-oom
-
-if (!('oomAfterAllocations' in this))
-    quit();
-
-var buffer = new ArrayBuffer(100);
-view = new DataView(buffer, undefined, undefined);
-oomAfterAllocations(10);
-view = new DataView(buffer, 20, undefined);
