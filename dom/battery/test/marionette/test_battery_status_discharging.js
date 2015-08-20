5bcce87497c8acce3569b84bffdb2ba53380c47e
Wei-Cheng Pan <wpan@mozilla.com>
2015-08-20 15:39:47 +0800

    Bug 1050749 - Expose BatteryManager via getBattery() returning a Promise r=bz,baku

--- dom/battery/test/marionette/test_battery_status_discharging.js
+++ dom/battery/test/marionette/test_battery_status_discharging.js
@@ -3,17 +3,20 @@
 
 MARIONETTE_TIMEOUT = 10000;
 
-let battery = window.navigator.battery;
+let battery = null;
 let fromStatus = "discharging";
 let fromCharging = false;
 
 function verifyInitialState() {
-  ok(battery, "battery");
-  ok(battery.charging, "battery.charging");
-  runEmulatorCmd("power display", function (result) {
-    is(result.pop(), "OK", "power display successful");
-    ok(result.indexOf("status: Charging") !== -1, "power status charging");
-    setUp();
+  window.navigator.getBattery().then(function (b) {
+    battery = b;
+    ok(battery, "battery");
+    ok(battery.charging, "battery.charging");
+    runEmulatorCmd("power display", function (result) {
+      is(result.pop(), "OK", "power display successful");
+      ok(result.indexOf("status: Charging") !== -1, "power status charging");
+      setUp();
+    });
   });
 }
 
