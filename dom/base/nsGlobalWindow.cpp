74881c2a05a068d261d5ee25e7ea7c0e0ebb1b33
Olli Pettay <Olli.Pettay@helsinki.fi>
2015-08-20 18:36:06 +0300

    Bug 1196317 - Optimize CallbackObject::mCreationStack out from cycle collection graph when possible. r=mccr8, a=sledru

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -1918,10 +1918,7 @@ nsGlobalWindow::UnmarkGrayTimers()
     if (timeout->mScriptHandler) {
       Function* f = timeout->mScriptHandler->GetCallback();
       if (f) {
-        // Callable() already does xpc_UnmarkGrayObject.
-        DebugOnly<JS::Handle<JSObject*> > o = f->Callable();
-        MOZ_ASSERT(!JS::ObjectIsMarkedGray(o.value),
-                   "Should have been unmarked");
+        f->MarkForCC();
       }
     }
   }
