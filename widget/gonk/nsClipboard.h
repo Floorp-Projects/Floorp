541dc722d21f35b4fc756b050eed9f991c97210b
Boris Chiou <boris.chiou@gmail.com>
2015-08-19 22:52:00 -0400

    Bug 952456 - Part 1: Implement gonk/nsClipboard for rich text and raw image. r=fabrice
    Handle text/html and image MIME types on gonk/nsClipboard

--- widget/gonk/nsClipboard.h
+++ widget/gonk/nsClipboard.h
@@ -1,16 +1,16 @@
-/* -*- Mode: c++; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
- * This Source Code Form is subject to the terms of the Mozilla Public
+/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef nsClipbard_h__
 #define nsClipbard_h__
 
+#include "GonkClipboardData.h"
+#include "mozilla/UniquePtr.h"
 #include "nsIClipboard.h"
 
 class nsClipboard final : public nsIClipboard
 {
-  nsAutoString mClipboard;
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSICLIPBOARD
@@ -19,6 +19,9 @@ public:
 
 protected:
   ~nsClipboard() {}
+
+private:
+  mozilla::UniquePtr<mozilla::GonkClipboardData> mClipboard;
 };
 
 #endif
