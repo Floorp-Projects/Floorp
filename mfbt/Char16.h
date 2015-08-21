ad57c923982a64a3d9530d0ecb253d4dd4d78636
Mike Hommey <mh+mozilla@glandium.org>
2015-08-21 16:05:40 +0900

    Bug 1189967 - Avoid including <string> from Char16.h. r=nfroyd
    Since Char16.h is included everywhere, and MSVC 2015 uses the char16ptr_t trick
    it contains, we include <string> everywhere, but that has the side effect of
    breaking the build in subtle ways. One way around this would be to avoid including
    Char16.h in the first place, but that requires more work than I was ready to put
    in. So instead, just avoid including <string> by removing the conversion operator
    for std::wstring.

--- mfbt/Char16.h
+++ mfbt/Char16.h
@@ -49,7 +49,7 @@ typedef unsigned int char32_t;
 #endif
 
 #ifdef MOZ_USE_CHAR16_WRAPPER
-# include <string>
+# include <cstdint>
   /**
    * Win32 API extensively uses wchar_t, which is represented by a separated
    * builtin type than char16_t per spec. It's not the case for MSVC prior to
@@ -92,10 +92,6 @@ public:
   operator bool() const
   {
     return mPtr != nullptr;
-  }
-  operator std::wstring() const
-  {
-    return std::wstring(static_cast<const wchar_t*>(*this));
   }
 
   /* Explicit cast operators to allow things like (char16_t*)str. */
