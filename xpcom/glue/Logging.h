db0b826de25ae26512e69507da71d687d2779d18
Chris Peterson <cpeterson@mozilla.com>
2015-08-20 23:39:18 -0700

    Bug 1197563 - Polyfill __func__ for MSVC 2013 and earlier. r=froydnj

--- xpcom/glue/Logging.h
+++ xpcom/glue/Logging.h
@@ -66,5 +66,15 @@ inline bool log_test(const PRLogModuleInfo* module, LogLevel level) {
 #undef PR_LOG
 #undef PR_LOG_TEST
 
-#endif // mozilla_logging_h
+/*
+ * __func__ was standardized in C++11 and is supported by clang, gcc, and MSVC
+ * 2015. Here we polyfill __func__ for earlier versions of MSVC.
+ * http://blogs.msdn.com/b/vcblog/archive/2015/06/19/c-11-14-17-features-in-vs-2015-rtm.aspx
+ */
+#ifdef _MSC_VER
+#  if _MSC_VER < 1900
+#    define __func__ __FUNCTION__
+#  endif
+#endif
 
+#endif // mozilla_logging_h
