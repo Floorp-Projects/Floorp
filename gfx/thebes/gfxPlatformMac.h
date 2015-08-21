1c9a7c5978f7ff245caa248f5b61a16d425a579e
Lee Salzman <lsalzman@mozilla.com>
2015-08-21 15:53:13 -0400

    Bug 1170390 - Make gfxPlatform::CreateOffscreenSurface use explicit format rather than guess. r=jrmuizel, a=ritu

--- gfx/thebes/gfxPlatformMac.h
+++ gfx/thebes/gfxPlatformMac.h
@@ -26,8 +26,8 @@ public:
     }
 
     virtual already_AddRefed<gfxASurface>
-      CreateOffscreenSurface(const IntSize& size,
-                             gfxContentType contentType) override;
+      CreateOffscreenSurface(const IntSize& aSize,
+                             gfxImageFormat aFormat) override;
 
     mozilla::TemporaryRef<mozilla::gfx::ScaledFont>
       GetScaledFontForFont(mozilla::gfx::DrawTarget* aTarget, gfxFont *aFont) override;
