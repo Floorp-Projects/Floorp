5f99ca6c6bab431e4818b3fb6569fbf404e6d2a9
Patrick Brosset <pbrosset@mozilla.com>
2015-08-21 15:23:17 +0200

    Bug 1169563 - 2 - Add a scrubber element to timeline animation UI; r=zer0

---
+++ browser/devtools/animationinspector/test/browser_animation_timeline_scrubber_exists.js
@@ -0,0 +1,20 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Check that the timeline-based UI does have a scrubber element.
+
+add_task(function*() {
+  yield addTab(TEST_URL_ROOT + "doc_simple_animation.html");
+  let {panel} = yield openAnimationInspectorNewUI();
+
+  let timeline = panel.animationsTimelineComponent;
+  let scrubberEl = timeline.scrubberEl;
+
+  ok(scrubberEl, "The scrubber element exists");
+  ok(scrubberEl.classList.contains("scrubber"), "It has the right classname");
+  is(parseInt(timeline.win.getComputedStyle(scrubberEl).left, 10), 0,
+     "It's positioned to the left by default");
+});
