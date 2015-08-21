4e8c72ef7df8308db50e9f15a5a4567a2e8fda51
Wes Kocher <wkocher@mozilla.com>
2015-08-21 08:45:27 -0700

    Backed out changeset 28fa968ea46c (bug 1181261) for breaking mulet tests CLOSED TREE

--- testing/mozharness/configs/web_platform_tests/prod_config.py
+++ testing/mozharness/configs/web_platform_tests/prod_config.py
@@ -6,15 +6,10 @@
 import os
 
 config = {
-    "options": [
-        "--prefs-root=%(test_path)s/prefs",
-        "--processes=1",
-        "--config=%(test_path)s/wptrunner.ini",
-        "--ca-cert-path=%(test_path)s/certs/cacert.pem",
-        "--host-key-path=%(test_path)s/certs/web-platform.test.key",
-        "--host-cert-path=%(test_path)s/certs/web-platform.test.pem",
-        "--certutil-binary=%(test_install_path)s/bin/certutil",
-    ],
+    # test harness options are located in the gecko tree
+    "in_tree_config": "config/mozharness/web_platform_tests_config.py",
+
+    "options": [],
 
     "exes": {
         'python': '/tools/buildbot/bin/python',
