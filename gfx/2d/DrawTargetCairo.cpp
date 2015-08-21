057995e630e2a9dffabf10cbe0a87c628ca6147d
Lee Salzman <lsalzman@mozilla.com>
2015-08-21 15:50:23 -0400

    Bug 1170390 - Detect 16bpp cairo xlib surface format. r=jrmuizel, a=ritu

--- gfx/2d/DrawTargetCairo.cpp
+++ gfx/2d/DrawTargetCairo.cpp
@@ -661,6 +661,23 @@ DrawTargetCairo::GetSize()
   return mSize;
 }
 
+SurfaceFormat
+GfxFormatForCairoSurface(cairo_surface_t* surface)
+{
+  cairo_surface_type_t type = cairo_surface_get_type(surface);
+  if (type == CAIRO_SURFACE_TYPE_IMAGE) {
+    return CairoFormatToGfxFormat(cairo_image_surface_get_format(surface));
+  }
+#ifdef CAIRO_HAS_XLIB_SURFACE
+  // xlib is currently the only Cairo backend that creates 16bpp surfaces
+  if (type == CAIRO_SURFACE_TYPE_XLIB &&
+      cairo_xlib_surface_get_depth(surface) == 16) {
+    return SurfaceFormat::R5G6B5;
+  }
+#endif
+  return CairoContentToGfxFormat(cairo_surface_get_content(surface));
+}
+
 TemporaryRef<SourceSurface>
 DrawTargetCairo::Snapshot()
 {
