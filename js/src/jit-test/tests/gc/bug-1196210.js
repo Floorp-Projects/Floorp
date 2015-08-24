c44d4ebdcca0c446319b2f7188e17f57949d727c
Terrence Cole <terrence@mozilla.com>
2015-08-24 11:00:29 -0700

    Bug 1196210 - Fix an incomplete assertion in the nursery; r=jonco
    --HG--
    extra : rebase_source : f76bdbf091ae70f2da9874fc16deeca1eb800bcf

---
+++ js/src/jit-test/tests/gc/bug-1196210.js
@@ -0,0 +1,14 @@
+function foo() {
+  function testGeneratorDeepBail() {
+    gc();
+    for (let i = 0; Error(); i++) {
+      for each(var x in [{n: 1}, {n: 1}]) {
+        x[0] = 0;
+        Object.freeze(x);
+      }
+      if (i == 10)
+        break;
+    }
+  }
+  testGeneratorDeepBail();
+} foo();
