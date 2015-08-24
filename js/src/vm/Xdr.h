85a87158b6bb0c75b569e46583374fccda2f362f
Jon Coppeard <jcoppeard@mozilla.com>
2015-08-24 15:58:35 +0100

    Bug 930414 - Add initial bytecode emitter support for modules r=shu

--- js/src/vm/Xdr.h
+++ js/src/vm/Xdr.h
@@ -29,7 +29,7 @@ namespace js {
  *
  *  https://developer.mozilla.org/en-US/docs/SpiderMonkey/Internals/Bytecode
  */
-static const uint32_t XDR_BYTECODE_VERSION_SUBTRAHEND = 301;
+static const uint32_t XDR_BYTECODE_VERSION_SUBTRAHEND = 302;
 static const uint32_t XDR_BYTECODE_VERSION =
     uint32_t(0xb973c0de - XDR_BYTECODE_VERSION_SUBTRAHEND);
 
