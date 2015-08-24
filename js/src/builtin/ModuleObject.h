c3e4b21c90507bb2975bd36fb7baf19dd2d7cb4c
Jon Coppeard <jcoppeard@mozilla.com>
2015-08-24 15:58:35 +0100

    Bug 930414 - Add module requestedModules field r=shu

--- js/src/builtin/ModuleObject.h
+++ js/src/builtin/ModuleObject.h
@@ -7,9 +7,17 @@
 #ifndef builtin_ModuleObject_h
 #define builtin_ModuleObject_h
 
+#include "jsapi.h"
+
+#include "js/TraceableVector.h"
+
 #include "vm/NativeObject.h"
 
 namespace js {
+
+namespace frontend {
+class ParseNode;
+} /* namespace frontend */
 
 class ModuleObject : public NativeObject
 {
@@ -17,15 +25,20 @@ class ModuleObject : public NativeObject
     enum
     {
         ScriptSlot = 0,
+        RequestedModulesSlot,
         SlotCount
     };
 
     static const Class class_;
 
+    static bool isInstance(HandleValue value);
+
     static ModuleObject* create(ExclusiveContext* cx);
     void init(HandleScript script);
+    void initImportExportData(HandleArrayObject requestedModules);
 
     JSScript* script() const;
+    ArrayObject& requestedModules() const;
 
   private:
     static void trace(JSTracer* trc, JSObject* obj);
@@ -33,6 +46,33 @@ class ModuleObject : public NativeObject
 
 typedef Rooted<ModuleObject*> RootedModuleObject;
 typedef Handle<ModuleObject*> HandleModuleObject;
+
+// Process a module's parse tree to collate the import and export data used when
+// creating a ModuleObject.
+class MOZ_STACK_CLASS ModuleBuilder
+{
+  public:
+    explicit ModuleBuilder(JSContext* cx);
+
+    bool buildAndInit(frontend::ParseNode* pn, HandleModuleObject module);
+
+  private:
+    using AtomVector = TraceableVector<JSAtom*>;
+    using RootedAtomVector = JS::Rooted<AtomVector>;
+
+    JSContext* cx_;
+    RootedAtomVector requestedModules_;
+
+    bool processImport(frontend::ParseNode* pn);
+    bool processExportFrom(frontend::ParseNode* pn);
+
+    bool maybeAppendRequestedModule(HandleAtom module);
+
+    template <typename T>
+    ArrayObject* createArray(const TraceableVector<T>& vector);
+};
+
+JSObject* InitModuleClass(JSContext* cx, HandleObject obj);
 
 } // namespace js
 
