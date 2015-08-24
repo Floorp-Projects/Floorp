2a1178e67cf6335829fe8bd2f9ee5ecd21d523ac
Nick Fitzgerald <fitzgen@gmail.com>
2015-08-24 09:29:44 -0700

    Bug 1194424 - Part 2: Deserialize JS::ubi::StackFrame allocation stacks from core dumps; r=sfink

--- toolkit/devtools/server/DeserializedNode.h
+++ toolkit/devtools/server/DeserializedNode.h
@@ -8,7 +8,7 @@
 
 #include "js/UbiNode.h"
 #include "mozilla/devtools/CoreDump.pb.h"
-#include "mozilla/MaybeOneOf.h"
+#include "mozilla/Maybe.h"
 #include "mozilla/Move.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/Vector.h"
@@ -30,6 +30,7 @@ namespace devtools {
 class HeapSnapshot;
 
 using NodeId = uint64_t;
+using StackFrameId = uint64_t;
 
 // A `DeserializedEdge` represents an edge in the heap graph pointing to the
 // node with id equal to `DeserializedEdge::referent` that we deserialized from
@@ -57,24 +58,27 @@ struct DeserializedNode {
   using EdgeVector = Vector<DeserializedEdge>;
   using UniqueStringPtr = UniquePtr<char16_t[]>;
 
-  NodeId          id;
+  NodeId              id;
   // A borrowed reference to a string owned by this node's owning HeapSnapshot.
-  const char16_t* typeName;
-  uint64_t        size;
-  EdgeVector      edges;
+  const char16_t*     typeName;
+  uint64_t            size;
+  EdgeVector          edges;
+  Maybe<StackFrameId> allocationStack;
   // A weak pointer to this node's owning `HeapSnapshot`. Safe without
   // AddRef'ing because this node's lifetime is equal to that of its owner.
-  HeapSnapshot*   owner;
+  HeapSnapshot*       owner;
 
   DeserializedNode(NodeId id,
                    const char16_t* typeName,
                    uint64_t size,
                    EdgeVector&& edges,
+                   Maybe<StackFrameId> allocationStack,
                    HeapSnapshot& owner)
     : id(id)
     , typeName(typeName)
     , size(size)
     , edges(Move(edges))
+    , allocationStack(allocationStack)
     , owner(&owner)
   { }
   virtual ~DeserializedNode() { }
@@ -97,21 +101,81 @@ private:
   DeserializedNode& operator=(const DeserializedNode&) = delete;
 };
 
+static inline js::HashNumber
+hashIdDerivedFromPtr(uint64_t id)
+{
+    // NodeIds and StackFrameIds are always 64 bits, but they are derived from
+    // the original referents' addresses, which could have been either 32 or 64
+    // bits long. As such, NodeId and StackFrameId have little entropy in their
+    // bottom three bits, and may or may not have entropy in their upper 32
+    // bits. This hash should manage both cases well.
+    id >>= 3;
+    return js::HashNumber((id >> 32) ^ id);
+}
+
 struct DeserializedNode::HashPolicy
 {
   using Lookup = NodeId;
 
   static js::HashNumber hash(const Lookup& lookup) {
-    // NodeIds are always 64 bits, but they are derived from the original
-    // referents' addresses, which could have been either 32 or 64 bits long.
-    // As such, a NodeId has little entropy in its bottom three bits, and may or
-    // may not have entropy in its upper 32 bits. This hash should manage both
-    // cases well.
-    uint64_t id = lookup >> 3;
-    return js::HashNumber((id >> 32) ^ id);
+    return hashIdDerivedFromPtr(lookup);
   }
 
   static bool match(const DeserializedNode& existing, const Lookup& lookup) {
+    return existing.id == lookup;
+  }
+};
+
+// A `DeserializedStackFrame` is a stack frame referred to by a thing in the
+// heap graph that we deserialized from a core dump.
+struct DeserializedStackFrame {
+  StackFrameId        id;
+  Maybe<StackFrameId> parent;
+  uint32_t            line;
+  uint32_t            column;
+  // Borrowed references to strings owned by this DeserializedStackFrame's
+  // owning HeapSnapshot.
+  const char16_t*     source;
+  const char16_t*     functionDisplayName;
+  bool                isSystem;
+  bool                isSelfHosted;
+  // A weak pointer to this frame's owning `HeapSnapshot`. Safe without
+  // AddRef'ing because this frame's lifetime is equal to that of its owner.
+  HeapSnapshot*       owner;
+
+  explicit DeserializedStackFrame(StackFrameId id,
+                                  const Maybe<StackFrameId>& parent,
+                                  uint32_t line,
+                                  uint32_t column,
+                                  const char16_t* source,
+                                  const char16_t* functionDisplayName,
+                                  bool isSystem,
+                                  bool isSelfHosted,
+                                  HeapSnapshot& owner)
+    : id(id)
+    , parent(parent)
+    , line(line)
+    , column(column)
+    , source(source)
+    , functionDisplayName(functionDisplayName)
+    , isSystem(isSystem)
+    , isSelfHosted(isSelfHosted)
+    , owner(&owner)
+  {
+    MOZ_ASSERT(source);
+  }
+
+  struct HashPolicy;
+};
+
+struct DeserializedStackFrame::HashPolicy {
+  using Lookup = StackFrameId;
+
+  static js::HashNumber hash(const Lookup& lookup) {
+    return hashIdDerivedFromPtr(lookup);
+  }
+
+  static bool match(const DeserializedStackFrame& existing, const Lookup& lookup) {
     return existing.id == lookup;
   }
 };
