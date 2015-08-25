b2edf2387f8d1f675131e260aa3b9dc7535d8d21
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2015-08-25 14:51:00 +0200

    Bug 1167627 - Part 4: Use mozinfo in uriloader/.

--- uriloader/exthandler/tests/unit/test_handlerService.js
+++ uriloader/exthandler/tests/unit/test_handlerService.js
@@ -27,8 +27,7 @@ function run_test() {
   const rootPrefBranch = prefSvc.getBranch("");
   
   let noMailto = false;
-  let isWindows = ("@mozilla.org/windows-registry-key;1" in Components.classes);
-  if (isWindows) {
+  if (mozinfo.os == "win") {
     // Check mailto handler from registry.
     // If registry entry is nothing, no mailto handler
     let regSvc = Cc["@mozilla.org/windows-registry-key;1"].
@@ -44,8 +43,7 @@ function run_test() {
     regSvc.close();
   }
 
-  let isLinux = ("@mozilla.org/gio-service;1" in Components.classes);
-  if (isLinux) {
+  if (mozinfo.os == "linux") {
     // Check mailto handler from GIO
     // If there isn't one, then we have no mailto handler
     let gIOSvc = Cc["@mozilla.org/gio-service;1"].
@@ -463,7 +461,7 @@ function run_test() {
   do_check_eq(lolType, "application/lolcat");
 
   // test mailcap entries with needsterminal are ignored on non-Windows non-Mac.
-  if (!("@mozilla.org/windows-registry-key;1" in Cc) && !("nsILocalFileMac" in Ci)) {
+  if (mozinfo.os != "win" && mozinfo.os != "mac") {
     env.set('PERSONAL_MAILCAP', do_get_file('mailcap').path);
     handlerInfo = mimeSvc.getFromTypeAndExtension("text/plain", null);
     do_check_eq(handlerInfo.preferredAction, Ci.nsIHandlerInfo.useSystemDefault);
