82c53a4b246b2d4d3029041e6f0eecd6a4e82ec2
Kartikaya Gupta <kgupta@mozilla.com>
2015-08-26 15:08:00 -0400

    Bug 1198900 - Don't call OnTouchCancel when the current block is not a touch block. r=botond

--- gfx/layers/apz/src/Axis.cpp
+++ gfx/layers/apz/src/Axis.cpp
@@ -399,7 +399,7 @@ void Axis::EndTouch(uint32_t aTimestampMs) {
     mAsyncPanZoomController, Name(), mVelocity);
 }
 
-void Axis::CancelTouch() {
+void Axis::CancelGesture() {
   // mVelocityQueue is controller-thread only
   APZThreadUtils::AssertOnControllerThread();
 
