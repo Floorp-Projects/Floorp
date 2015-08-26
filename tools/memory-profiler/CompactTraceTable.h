49f7e379019c301d18d575cd2e218372ef2fc8c2
Kan-Ru Chen <kanru@kanru.info>
2015-08-26 18:55:55 +0800

    Bug 1123237 - Part 11. Don't use STL in memory-profiler. r=BenWa,cervantes

--- tools/memory-profiler/CompactTraceTable.h
+++ tools/memory-profiler/CompactTraceTable.h
@@ -7,12 +7,10 @@
 #ifndef memory_profiler_CompactTraceTable_h
 #define memory_profiler_CompactTraceTable_h
 
-#include "UncensoredAllocator.h"
-
-#include <functional>
-#include <utility>
-
 #include "mozilla/HashFunctions.h"
+
+#include "nsDataHashtable.h"
+#include "nsTArray.h"
 
 namespace mozilla {
 
@@ -24,67 +22,49 @@ struct TrieNode final
   {
     return parentIdx == t.parentIdx && nameIdx == t.nameIdx;
   }
-};
-
-} // namespace mozilla
-
-namespace std {
-template<>
-struct hash<mozilla::TrieNode>
-{
-  size_t operator()(const mozilla::TrieNode& v) const
+  uint32_t Hash() const
   {
-    uint64_t k = static_cast<uint64_t>(v.parentIdx) << 32 | v.nameIdx;
-    return std::hash<uint64_t>()(k);
+    return HashGeneric(parentIdx, nameIdx);
   }
 };
-#ifdef MOZ_REPLACE_MALLOC
-template<>
-struct hash<mozilla::u_string>
-{
-  size_t operator()(const mozilla::u_string& v) const
-  {
-    return mozilla::HashString(v.c_str());
-  }
-};
-#endif
-} // namespace std
-
-namespace mozilla {
 
 // This class maps a Node of type T to its parent's index in the
 // map. When serializing, the map is traversed and put into an ordered
-// vector of Nodes.
-template<typename T>
+// array of Nodes.
+template<typename KeyClass, typename T>
 class NodeIndexMap final
 {
 public:
   uint32_t Insert(const T& e)
   {
-    auto i = mMap.insert(std::make_pair(e, mMap.size()));
-    return i.first->second;
+    uint32_t index = mMap.Count();
+    if (!mMap.Get(e, &index)) {
+      mMap.Put(e, index);
+    }
+    return index;
   }
 
-  u_vector<T> Serialize() const
+  nsTArray<T> Serialize() const
   {
-    u_vector<T> v(mMap.size());
-    for (auto i: mMap) {
-      v[i.second] = i.first;
+    nsTArray<T> v;
+    v.SetLength(mMap.Count());
+    for (auto iter = mMap.ConstIter(); !iter.Done(); iter.Next()) {
+      v[iter.Data()] = iter.Key();
     }
     return v;
   }
 
   uint32_t Size() const
   {
-    return mMap.size();
+    return mMap.Count();
   }
 
   void Clear()
   {
-    mMap.clear();
+    mMap.Clear();
   }
 private:
-  u_unordered_map<T, uint32_t> mMap;
+  nsDataHashtable<KeyClass, uint32_t> mMap;
 };
 
 // Backtraces are stored in a trie to save spaces.
@@ -97,22 +77,22 @@ class CompactTraceTable final
 public:
   CompactTraceTable()
   {
-    mNames.Insert("(unknown)");
+    mNames.Insert(nsAutoCString("(unknown)"));
     mTraces.Insert(TrieNode{0, 0});
   }
 
-  u_vector<u_string> GetNames() const
+  nsTArray<nsCString> GetNames() const
   {
     return mNames.Serialize();
   }
 
-  u_vector<TrieNode> GetTraces() const
+  nsTArray<TrieNode> GetTraces() const
   {
     return mTraces.Serialize();
   }
 
   // Returns an ID to a stacktrace.
-  uint32_t Insert(const u_vector<u_string>& aRawStacktrace)
+  uint32_t Insert(const nsTArray<nsCString>& aRawStacktrace)
   {
     uint32_t parent = 0;
     for (auto& frame: aRawStacktrace) {
@@ -127,8 +107,8 @@ public:
     mTraces.Clear();
   }
 private:
-  NodeIndexMap<u_string> mNames;
-  NodeIndexMap<TrieNode> mTraces;
+  NodeIndexMap<nsCStringHashKey, nsCString> mNames;
+  NodeIndexMap<nsGenericHashKey<TrieNode>, TrieNode> mTraces;
 };
 
 } // namespace mozilla
