7403bad37b9105e3e015ed666ebe4051aaae2266
David Burns <dburns@mozilla.com>
2015-08-26 19:52:09 +0100

    Bug 1198542: Update Marionette element IDs to be valid UUID. r=ato
    This allows WebDriver compatibility to create valid URI.

--- testing/marionette/elements.js
+++ testing/marionette/elements.js
@@ -233,7 +233,8 @@ ElementManager.prototype = {
         delete this.seenItems[i];
       }
     }
-    let id = uuidGen.generateUUID().toString();
+    let uuid = uuidGen.generateUUID().toString();
+    let id = uuid.substring(1, uuid.length - 1);
     this.seenItems[id] = Components.utils.getWeakReference(element);
     return id;
   },
