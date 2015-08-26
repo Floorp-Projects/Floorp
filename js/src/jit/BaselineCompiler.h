a9af290ad7ccb5b89c2406c43b1fd5dce9de9fcd
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2015-08-26 20:48:05 +0200

    Bug 1190446 - Update Coverage information in Baseline. r=jandem

--- js/src/jit/BaselineCompiler.h
+++ js/src/jit/BaselineCompiler.h
@@ -265,6 +265,7 @@ class BaselineCompiler : public BaselineCompilerSpecific
     void emitIsDebuggeeCheck();
     bool emitDebugPrologue();
     bool emitDebugTrap();
+    void emitCoverage(jsbytecode* pc);
     bool emitTraceLoggerEnter();
     bool emitTraceLoggerExit();
 
