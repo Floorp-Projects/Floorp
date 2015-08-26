a7166a57ee968e36e27c111f848b6f5abac4544e
Sylvestre Ledru <sledru@mozilla.com>
2015-08-26 10:37:21 +0200

    Bug 1198590 - Disable dom.ipc.plugins.asyncInit r=me a=me

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2468,13 +2468,9 @@ pref("dom.ipc.plugins.reportCrashURL", true);
 // Defaults to 30 seconds.
 pref("dom.ipc.plugins.unloadTimeoutSecs", 30);
 
-// Asynchronous plugin initialization should only be enabled on non-e10s
-// channels until some remaining bugs are resolved.
-#ifdef E10S_TESTING_ONLY
+// Asynchronous plugin initialization is disabled in release
+// See bug #1198590
 pref("dom.ipc.plugins.asyncInit", false);
-#else
-pref("dom.ipc.plugins.asyncInit", true);
-#endif
 
 pref("dom.ipc.processCount", 1);
 
