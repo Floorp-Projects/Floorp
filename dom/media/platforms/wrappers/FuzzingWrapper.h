6d615ac309e46a4f64fcef6499cbd28399ef9b78
Gerald Squelart <gsquelart@mozilla.com>
2015-08-26 18:47:00 -0400

    Bug 1194518 - Part 3: Delaying decoder wrapper, ensures a decoder appears consistently slow. r=jya

--- dom/media/platforms/wrappers/FuzzingWrapper.h
+++ dom/media/platforms/wrappers/FuzzingWrapper.h
@@ -7,9 +7,35 @@
 #if !defined(FuzzingWrapper_h_)
 #define FuzzingWrapper_h_
 
+#include "mozilla/Pair.h"
 #include "PlatformDecoderModule.h"
 
 namespace mozilla {
+
+// Fuzzing wrapper for media decoders.
+//
+// DecoderFuzzingWrapper owns the DecoderCallbackFuzzingWrapper, and inserts
+// itself between the reader and the decoder.
+// DecoderCallbackFuzzingWrapper inserts itself between a decoder and its
+// callback.
+// Together they are used to introduce some fuzzing, (e.g. delay output).
+//
+// Normally:
+//          ====================================>
+//   reader                                       decoder
+//          <------------------------------------
+//
+// With fuzzing:
+//          ======> DecoderFuzzingWrapper ======>
+//   reader                  v                    decoder
+//          <-- DecoderCallbackFuzzingWrapper <--
+//
+// Creation order should be:
+// 1. Create DecoderCallbackFuzzingWrapper, give the expected callback target.
+// 2. Create actual decoder, give DecoderCallbackFuzzingWrapper as callback.
+// 3. Create DecoderFuzzingWrapper, give decoder and DecoderCallbackFuzzingWrapper.
+// DecoderFuzzingWrapper is what the reader sees as decoder, it owns the
+// real decoder and the DecoderCallbackFuzzingWrapper.
 
 class DecoderCallbackFuzzingWrapper : public MediaDataDecoderCallback
 {
@@ -17,6 +43,15 @@ public:
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(DecoderCallbackFuzzingWrapper)
 
   explicit DecoderCallbackFuzzingWrapper(MediaDataDecoderCallback* aCallback);
+
+  // Enforce a minimum interval between output frames (i.e., limit frame rate).
+  // Of course, if the decoder is even slower, this won't have any effect.
+  void SetVideoOutputMinimumInterval(TimeDuration aFrameOutputMinimumInterval);
+  // If false (default), if frames are delayed, any InputExhausted is delayed to
+  // be later sent after the corresponding delayed frame.
+  // If true, InputExhausted are passed through immediately; This could result
+  // in lots of frames being decoded and queued for delayed output!
+  void SetDontDelayInputExhausted(bool aDontDelayInputExhausted);
 
 private:
   virtual ~DecoderCallbackFuzzingWrapper();
@@ -30,6 +65,31 @@ private:
   bool OnReaderTaskQueue() override;
 
   MediaDataDecoderCallback* mCallback;
+
+  // Settings for minimum frame output interval & InputExhausted,
+  // should be set during init and then only read on mTaskQueue.
+  TimeDuration mFrameOutputMinimumInterval;
+  bool mDontDelayInputExhausted;
+  // Members for minimum frame output interval & InputExhausted,
+  // should only be accessed on mTaskQueue.
+  TimeStamp mPreviousOutput;
+  // First member is the frame to be delayed.
+  // Second member is true if an 'InputExhausted' arrived after that frame; in
+  // which case an InputExhausted will be sent after finally outputting the frame.
+  typedef Pair<nsRefPtr<MediaData>, bool> MediaDataAndInputExhausted;
+  nsTArray<MediaDataAndInputExhausted> mDelayedOutput;
+  nsRefPtr<MediaTimer> mDelayedOutputTimer;
+  // If draining, a 'DrainComplete' will be sent after all delayed frames have
+  // been output.
+  bool mDraining;
+  // All callbacks are redirected through this task queue, both to avoid locking
+  // and to have a consistent sequencing of callbacks.
+  nsRefPtr<TaskQueue> mTaskQueue;
+  void ScheduleOutputDelayedFrame();
+  void OutputDelayedFrame();
+public: // public for the benefit of DecoderFuzzingWrapper.
+  void ClearDelayedOutput();
+  void Shutdown();
 };
 
 class DecoderFuzzingWrapper : public MediaDataDecoder
