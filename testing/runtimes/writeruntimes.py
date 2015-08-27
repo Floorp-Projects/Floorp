f301cb98059667326190cbe8f28be938ebb0d0b2
Andrew Halberstadt <ahalberstadt@mozilla.com>
2015-08-27 17:37:26 -0400

    Bug 1199241 - Average runtime data across platforms instead of keeping it distinct, r=jgriffin
    Keeping distinct platform runtime files takes up a lot of space. It also adds complexity and
    greater possibility for mistakes, if a file for some arbitrary platform (like pgo) doesn't
    exist, there will be a failure. This patch removes all platform specific runtime files and
    replaces them with a single per suite file. The times in this file are averaged across all
    platforms the tests run on. With the added space savings, we take into account the 50th
    percentile of tests, instead of the 90th.

--- testing/runtimes/writeruntimes.py
+++ testing/runtimes/writeruntimes.py
@@ -9,7 +9,7 @@ import requests
 here = os.path.abspath(os.path.dirname(__file__))
 
 ACTIVE_DATA_URL = "http://activedata.allizom.org/query"
-PERCENTILE = 0.9 # ignore the bottom PERCENTILE*100% of numbers
+PERCENTILE = 0.5 # ignore the bottom PERCENTILE*100% of numbers
 
 def query_activedata(suite, platforms=None):
     platforms = ', "build.platform":%s' % json.dumps(platforms) if platforms else ''
@@ -18,7 +18,7 @@ def query_activedata(suite, platforms=None):
 {
     "from":"unittest",
     "limit":200000,
-    "groupby":["build.platform","result.test","build.type"],
+    "groupby":["result.test"],
     "select":{"value":"result.duration","aggregate":"average"},
     "where":{"and":[
         {"eq":{"suite":"%s"%s}},
@@ -35,72 +35,60 @@ def query_activedata(suite, platforms=None):
     return data
 
 def write_runtimes(data, suite, indir=here, outdir=here):
-    testdata = defaultdict(lambda: defaultdict(list))
-    for result in data:
-        # Each result is a list with four members: platform,
-        # test, build type, and duration.
-        platform = result[0]
-        buildtype = result[2]
-        testdata[platform][buildtype].append([result[1], result[3]])
+    data = dict(data)
 
-    for platform in testdata:
-        for buildtype in testdata[platform]:
-            print 'processing %s-%s results' % (platform, buildtype)
-            dirname = "%s-%s" % (platform, buildtype)
-            out_path = os.path.join(outdir, dirname)
-            in_path = os.path.join(indir, dirname)
-            outfilename = os.path.join(out_path, "%s.runtimes.json" % suite)
-            infilename = os.path.join(in_path, "%s.runtimes.json" % suite)
-            if not os.path.exists(out_path):
-                os.makedirs(out_path)
+    outfilename = os.path.join(outdir, "%s.runtimes.json" % suite)
+    infilename = os.path.join(indir, "%s.runtimes.json" % suite)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
-            # read in existing data, if any
-            indata = None
-            if os.path.exists(infilename):
-                with open(infilename, 'r') as f:
-                    indata = json.loads(f.read()).get('runtimes')
+    # read in existing data, if any
+    indata = None
+    if os.path.exists(infilename):
+        with open(infilename, 'r') as f:
+            indata = json.loads(f.read()).get('runtimes')
 
-            # identify a threshold of durations, below which we ignore
-            runtimes = []
-            for result in testdata[platform][buildtype]:
-                duration = int(result[1] * 1000) if result[1] else 0
-                if duration:
-                    runtimes.append(duration)
-            runtimes.sort()
-            threshold = runtimes[int(len(runtimes) * PERCENTILE)]
+    # identify a threshold of durations, below which we ignore
+    runtimes = []
+    for result in data.itervalues():
+        duration = int(result * 1000) if result else 0
+        if duration:
+            runtimes.append(duration)
+    runtimes.sort()
+    threshold = runtimes[int(len(runtimes) * PERCENTILE)]
 
-            # split the durations into two groups; excluded and specified
-            ommitted = []
-            specified = indata if indata else {}
-            current_tests = []
-            for test, duration in testdata[platform][buildtype]:
-                current_tests.append(test)
-                duration = int(duration * 1000) if duration else 0
-                if duration > 0 and duration < threshold:
-                    ommitted.append(duration)
-                    if test in specified:
-                        del specified[test]
-                elif duration >= threshold and test != "automation.py":
-                    original = specified.get(test, 0)
-                    if not original or abs(original - duration) > (original/20):
-                        # only write new data if it's > 20% different than original
-                        specified[test] = duration
-
-            # delete any test references no longer needed
-            to_delete = []
-            for test in specified:
-                if test not in current_tests:
-                    to_delete.append(test)
-            for test in to_delete:
+    # split the durations into two groups; ommitted and specified
+    ommitted = []
+    specified = indata if indata else {}
+    current_tests = []
+    for test, duration in data.iteritems():
+        current_tests.append(test)
+        duration = int(duration * 1000) if duration else 0
+        if duration > 0 and duration < threshold:
+            ommitted.append(duration)
+            if test in specified:
                 del specified[test]
+        elif duration >= threshold and test != "automation.py":
+            original = specified.get(test, 0)
+            if not original or abs(original - duration) > (original/20):
+                # only write new data if it's > 20% different than original
+                specified[test] = duration
 
-            avg = int(sum(ommitted)/len(ommitted))
+    # delete any test references no longer needed
+    to_delete = []
+    for test in specified:
+        if test not in current_tests:
+            to_delete.append(test)
+    for test in to_delete:
+        del specified[test]
 
-            results = {'excluded_test_average': avg,
-                       'runtimes': specified}
+    avg = int(sum(ommitted)/len(ommitted))
 
-            with open(outfilename, 'w') as f:
-                f.write(json.dumps(results, indent=2, sort_keys=True))
+    results = {'excluded_test_average': avg,
+               'runtimes': specified}
+
+    with open(outfilename, 'w') as f:
+        f.write(json.dumps(results, indent=2, sort_keys=True))
 
 
 def cli(args=sys.argv[1:]):
