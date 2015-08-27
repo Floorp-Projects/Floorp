9b20d4561e4b80332900ab36e85dc473d089e67c
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2015-08-27 14:51:00 -0400

    Bug 1199283 - nsContentUtils::sIsFrameTimingPrefEnabled should default to false. r=smaug

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -538,7 +538,7 @@ nsContentUtils::Init()
                                "dom.performance.enable_user_timing_logging", false);
 
   Preferences::AddBoolVarCache(&sIsFrameTimingPrefEnabled,
-                               "dom.enable_frame_timing", true);
+                               "dom.enable_frame_timing", false);
 
   Preferences::AddBoolVarCache(&sIsExperimentalAutocompleteEnabled,
                                "dom.forms.autocomplete.experimental", false);
