5aad27169462dd4502e3845cf6c263c13830cc2a
Nick Alexander <nalexander@mozilla.com>
2015-08-26 18:34:48 -0700

    Bug 1137898 - Migrate to android:versionCode scheme v1. r=rnewman,snorp a=sylvestre
    Android version codes serve multiple masters.  They indicate newer
    versions, yes; but they also break ties between versions with
    different features and requirements. High order bits effectively
    partition the space of versions and are valuable.  Since Android
    version codes are signed Java integers, we have 31 bits to work with.
    Mozilla's traditional build ID is YYYYMMDDhhmmss.  This was chopped to
    ten characters (YYYYMMDDhh, i.e., hourly build IDs) and then converted
    to a decimal.  This took many high order bits.  We will lose another
    high order bit in the 36th month of 2015 -- i.e., as soon as the year
    rolls over to 2016.  If we waited to lose the next higher order bit,
    we'd lose that one in the 46th month of 2017 -- i.e., as soon as the
    year rolls over to 2018.
    
    The following patch sacrifices a high order bit to change the version
    scheme, winning us roughly 15 years of hourly build IDs before we are
    forced to lose another high order bit.  So it's clearly to our
    advantage to change the scheme sooner rather than later -- we will
    sacrifice 1 bit for 15 years of build IDs, rather than keeping the
    current scheme and sacrificing (say) 2 bits for 3 years of build IDs.
    
    The resulting scheme produces build IDs that look like (in binary):
    
    0111 1000 0010 tttt tttt tttt tttt txpg
    
    The meaning of these build IDs is documented in the Python source code
    that generates them.
    
    --HG--
    extra : source : 5f17da82fc01a30de1b0add8560200bafa051602

--- python/mozbuild/mozbuild/test/test_android_version_code.py
+++ python/mozbuild/mozbuild/test/test_android_version_code.py
@@ -5,18 +5,60 @@
 from mozunit import main
 import unittest
 
-from mozbuild.android_version_code import android_version_code
+from mozbuild.android_version_code import (
+    android_version_code_v0,
+    android_version_code_v1,
+)
 
 class TestAndroidVersionCode(unittest.TestCase):
-    def test_basic(self):
+    def test_android_version_code_v0(self):
         # From https://treeherder.mozilla.org/#/jobs?repo=mozilla-central&revision=e25de9972a77.
         buildid = '20150708104620'
         arm_api9  = 2015070819
         arm_api11 = 2015070821
         x86_api9  = 2015070822
-        self.assertEqual(android_version_code(buildid, cpu_arch='armeabi', min_sdk=9, max_sdk=None), arm_api9)
-        self.assertEqual(android_version_code(buildid, cpu_arch='armeabi-v7a', min_sdk=11, max_sdk=None), arm_api11)
-        self.assertEqual(android_version_code(buildid, cpu_arch='x86', min_sdk=9, max_sdk=None), x86_api9)
+        self.assertEqual(android_version_code_v0(buildid, cpu_arch='armeabi', min_sdk=9, max_sdk=None), arm_api9)
+        self.assertEqual(android_version_code_v0(buildid, cpu_arch='armeabi-v7a', min_sdk=11, max_sdk=None), arm_api11)
+        self.assertEqual(android_version_code_v0(buildid, cpu_arch='x86', min_sdk=9, max_sdk=None), x86_api9)
+
+    def test_android_version_code_v1(self):
+        buildid = '20150825141628'
+        arm_api9  = 0b01111000001000000001001001110000
+        arm_api11 = 0b01111000001000000001001001110001
+        x86_api9  = 0b01111000001000000001001001110100
+        self.assertEqual(android_version_code_v1(buildid, cpu_arch='armeabi', min_sdk=9, max_sdk=None), arm_api9)
+        self.assertEqual(android_version_code_v1(buildid, cpu_arch='armeabi-v7a', min_sdk=11, max_sdk=None), arm_api11)
+        self.assertEqual(android_version_code_v1(buildid, cpu_arch='x86', min_sdk=9, max_sdk=None), x86_api9)
+
+    def test_android_version_code_v1_underflow(self):
+        '''Verify that it is an error to ask for v1 codes predating the cutoff.'''
+        buildid = '201508010000' # Earliest possible.
+        arm_api9 = 0b01111000001000000000000000000000
+        self.assertEqual(android_version_code_v1(buildid, cpu_arch='armeabi', min_sdk=9, max_sdk=None), arm_api9)
+        with self.assertRaises(ValueError) as cm:
+            underflow = '201507310000' # Latest possible (valid) underflowing date.
+            android_version_code_v1(underflow, cpu_arch='armeabi', min_sdk=9, max_sdk=None)
+        self.assertTrue('underflow' in cm.exception.message)
+
+    def test_android_version_code_v1_running_low(self):
+        '''Verify there is an informative message if one asks for v1 codes that are close to overflow.'''
+        with self.assertRaises(ValueError) as cm:
+            overflow = '20290801000000'
+            android_version_code_v1(overflow, cpu_arch='armeabi', min_sdk=9, max_sdk=None)
+        self.assertTrue('Running out of low order bits' in cm.exception.message)
+
+    def test_android_version_code_v1_overflow(self):
+        '''Verify that it is an error to ask for v1 codes that actually does overflow.'''
+        with self.assertRaises(ValueError) as cm:
+            overflow = '20310801000000'
+            android_version_code_v1(overflow, cpu_arch='armeabi', min_sdk=9, max_sdk=None)
+        self.assertTrue('overflow' in cm.exception.message)
+
+    def test_android_version_code_v0_relative_v1(self):
+        '''Verify that the first v1 code is greater than the equivalent v0 code.'''
+        buildid = '20150801000000'
+        self.assertGreater(android_version_code_v1(buildid, cpu_arch='armeabi', min_sdk=9, max_sdk=None),
+                           android_version_code_v0(buildid, cpu_arch='armeabi', min_sdk=9, max_sdk=None))
 
 
 if __name__ == '__main__':
