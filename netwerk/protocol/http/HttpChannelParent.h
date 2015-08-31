7be5c95a3e7483478b1ab72a9ab0d2c5eaddab41
Ehsan Akhgari <ehsan@mozilla.com>
2015-08-31 16:36:22 -0400

    Bug 1199049 - Part 6: Transfer the preflight parameters to the parent process in e10s mode; r=jduell

--- netwerk/protocol/http/HttpChannelParent.h
+++ netwerk/protocol/http/HttpChannelParent.h
@@ -120,7 +120,8 @@ protected:
                    const OptionalHttpResponseHead& aSynthesizedResponseHead,
                    const nsCString&           aSecurityInfoSerialization,
                    const uint32_t&            aCacheKey,
-                   const nsCString&           aSchedulingContextID);
+                   const nsCString&           aSchedulingContextID,
+                   const OptionalCorsPreflightArgs& aCorsPreflightArgs);
 
   virtual bool RecvSetPriority(const uint16_t& priority) override;
   virtual bool RecvSetClassOfService(const uint32_t& cos) override;
