ff8559e147c63246b1689472ad70656426c76b14
David Critchley <david@priologic.com>
2015-09-01 22:07:06 +0100

    Bug 1186396 - Remove remaining references to JQuery from Loop. r=andreio,r=Standard8

--- browser/components/loop/standalone/content/js/webapp.js
+++ browser/components/loop/standalone/content/js/webapp.js
@@ -3,7 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var loop = loop || {};
-loop.webapp = (function($, _, OT, mozL10n) {
+loop.webapp = (function(_, OT, mozL10n) {
   "use strict";
 
   loop.config = loop.config || {};
@@ -1122,4 +1122,4 @@ loop.webapp = (function($, _, OT, mozL10n) {
     WebappRootView: WebappRootView,
     FxOSConversationModel: FxOSConversationModel
   };
-})(jQuery, _, window.OT, navigator.mozL10n);
+})(_, window.OT, navigator.mozL10n);
