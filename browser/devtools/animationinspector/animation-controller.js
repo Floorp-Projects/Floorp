eb9897b7168caf00e60adbfc536261de9b2e0840
Patrick Brosset <pbrosset@mozilla.com>
2015-09-02 16:02:56 +0200

    Bug 1174060 - 4 - Minor eslint changes to animation-inspector; r=ednapiranha
    --HG--
    extra : commitid : I01lEzvCxVF
    extra : rebase_source : 387389832041b9afc58084f2802d3cabfd644ff4
    extra : histedit_source : 4529d496b136de8caeeb7390bac9f0fdf49870aa

--- browser/devtools/animationinspector/animation-controller.js
+++ browser/devtools/animationinspector/animation-controller.js
@@ -23,6 +23,7 @@ loader.lazyRequireGetter(this, "AnimationsFront",
 
 const STRINGS_URI = "chrome://browser/locale/devtools/animationinspector.properties";
 const L10N = new ViewHelpers.L10N(STRINGS_URI);
+const V3_UI_PREF = "devtools.inspector.animationInspectorV3";
 
 // Global toolbox/inspector, set when startup is called.
 let gToolbox, gInspector;
@@ -76,19 +77,20 @@ function destroy() {
  * @return {Object} An object with boolean properties.
  */
 let getServerTraits = Task.async(function*(target) {
-  let config = [{
-    name: "hasToggleAll", actor: "animations", method: "toggleAll"
-  }, {
-    name: "hasSetCurrentTime", actor: "animationplayer", method: "setCurrentTime"
-  }, {
-    name: "hasMutationEvents", actor: "animations", method: "stopAnimationPlayerUpdates"
-  }, {
-    name: "hasSetPlaybackRate", actor: "animationplayer", method: "setPlaybackRate"
-  }, {
-    name: "hasTargetNode", actor: "domwalker", method: "getNodeFromActor"
-  }, {
-    name: "hasSetCurrentTimes", actor: "animations", method: "setCurrentTimes"
-  }];
+  let config = [
+    { name: "hasToggleAll", actor: "animations",
+      method: "toggleAll" },
+    { name: "hasSetCurrentTime", actor: "animationplayer",
+      method: "setCurrentTime" },
+    { name: "hasMutationEvents", actor: "animations",
+     method: "stopAnimationPlayerUpdates" },
+    { name: "hasSetPlaybackRate", actor: "animationplayer",
+      method: "setPlaybackRate" },
+    { name: "hasTargetNode", actor: "domwalker",
+      method: "getNodeFromActor" },
+    { name: "hasSetCurrentTimes", actor: "animations",
+      method: "setCurrentTimes" }
+  ];
 
   let traits = {};
   for (let {name, actor, method} of config) {
@@ -96,7 +98,7 @@ let getServerTraits = Task.async(function*(target) {
   }
 
   // Special pref-based UI trait.
-  traits.isNewUI = Services.prefs.getBoolPref("devtools.inspector.animationInspectorV3");
+  traits.isNewUI = Services.prefs.getBoolPref(V3_UI_PREF);
 
   return traits;
 });
@@ -114,7 +116,8 @@ let getServerTraits = Task.async(function*(target) {
  *
  * Usage example:
  *
- * AnimationsController.on(AnimationsController.PLAYERS_UPDATED_EVENT, onPlayers);
+ * AnimationsController.on(AnimationsController.PLAYERS_UPDATED_EVENT,
+ *                         onPlayers);
  * function onPlayers() {
  *   for (let player of AnimationsController.animationPlayers) {
  *     // do something with player
@@ -126,7 +129,8 @@ let AnimationsController = {
 
   initialize: Task.async(function*() {
     if (this.initialized) {
-      return this.initialized.promise;
+      yield this.initialized.promise;
+      return;
     }
     this.initialized = promise.defer();
 
@@ -157,7 +161,8 @@ let AnimationsController = {
     }
 
     if (this.destroyed) {
-      return this.destroyed.promise;
+      yield this.destroyed.promise;
+      return;
     }
     this.destroyed = promise.defer();
 
@@ -272,7 +277,8 @@ let AnimationsController = {
   refreshAnimationPlayers: Task.async(function*(nodeFront) {
     yield this.destroyAnimationPlayers();
 
-    this.animationPlayers = yield this.animationsFront.getAnimationPlayersForNode(nodeFront);
+    this.animationPlayers = yield this.animationsFront
+                                      .getAnimationPlayersForNode(nodeFront);
     this.startAllAutoRefresh();
 
     // Start listening for animation mutations only after the first method call
