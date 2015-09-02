2c376f1008b34f57d1efcd66143368ae2327cc6c
Jonathan Watt <jwatt@jwatt.org>
2015-09-02 10:00:48 +0100

    Bug 1207570 - Make HTMLInputElement::GetFilesAndDirectories throw when the type is not file. r=baku

--- dom/html/HTMLInputElement.cpp
+++ dom/html/HTMLInputElement.cpp
@@ -4887,6 +4887,11 @@ MakeOrReuseFileSystem(const nsAString& aNewLocalRootPath,
 already_AddRefed<Promise>
 HTMLInputElement::GetFilesAndDirectories(ErrorResult& aRv)
 {
+  if (mType != NS_FORM_INPUT_FILE) {
+    aRv.Throw(NS_ERROR_DOM_INVALID_STATE_ERR);
+    return nullptr;
+  }
+
   if (mFilesAndDirectoriesPromise) {
     return nsRefPtr<Promise>(mFilesAndDirectoriesPromise).forget();
   }
