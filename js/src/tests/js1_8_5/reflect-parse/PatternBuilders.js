107b474028075ae6c4bb1bc75653a56a3c9f6381
Eric Faust <efaustbmo@mozilla.com>
2015-09-02 15:09:04 -0700

    Bug 1168992 - Part 3: Change PNK_SUPERPROP to PNK_DOT and fix reflection of super.prop. (r=Waldo)

--- js/src/tests/js1_8_5/reflect-parse/PatternBuilders.js
+++ js/src/tests/js1_8_5/reflect-parse/PatternBuilders.js
@@ -124,7 +124,7 @@ function letStmt(head, body) {
 }
 
 function superProp(id) {
-    return dotExpr(ident("super"), id);
+    return dotExpr(Pattern({ type: "Super" }), id);
 }
 function superElem(id) {
     return memExpr(ident("super"), id);
