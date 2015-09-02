2f55e19bf927324255cd7155bf5ff2f35a9297fb
Eric Faust <efaustbmo@mozilla.com>
2015-09-02 15:09:02 -0700

    Bug 1168992 - Part 1: Fix up reflection of classes. (r=Waldo)

--- js/src/jit-test/tests/modules/export-declaration.js
+++ js/src/jit-test/tests/modules/export-declaration.js
@@ -38,7 +38,7 @@ functionDeclaration = (id, params, body) => Pattern({
 });
 classDeclaration = (name) => Pattern({
     type: "ClassStatement",
-    name: name
+    id: name
 });
 variableDeclaration = (decls) => Pattern({
     type: "VariableDeclaration",
