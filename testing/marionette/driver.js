44bad0e712645b49ef32502a1bda9ce6e8ce0b07
Andreas Tolfsen <ato@mozilla.com>
2015-09-02 13:23:28 +0100

    Bug 1201050: Part 1: Use dispatcher for listener singleTap
    r=dburns

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -1805,7 +1805,7 @@ GeckoDriver.prototype.singleTap = function(cmd, resp) {
 
     case Context.CONTENT:
       this.addFrameCloseListener("tap");
-      yield this.listener.singleTap({id: id, corx: x, cory: y});
+      yield this.listener.singleTap(id, x, y);
       break;
   }
 };
