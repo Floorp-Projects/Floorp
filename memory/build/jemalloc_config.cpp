4fb6c55f9da5198ecc740ad54b02b0925dff991c
Mike Hommey <mh+mozilla@glandium.org>
2015-09-02 11:04:42 +0900

    Bug 1141079 - Make jemalloc_config.c a C++ source file. r=ted
    --HG--
    rename : memory/build/jemalloc_config.c => memory/build/jemalloc_config.cpp

--- memory/build/jemalloc_config.cpp
+++ memory/build/jemalloc_config.cpp
@@ -26,19 +26,19 @@
 #endif
 
 #define MOZ_MALLOC_OPTIONS "narenas:1,lg_chunk:20,tcache:false"
-MFBT_DATA const char * je_(malloc_conf) =
+MFBT_DATA const char* je_(malloc_conf) =
   MOZ_MALLOC_OPTIONS MOZ_MALLOC_PLATFORM_OPTIONS MOZ_MALLOC_BUILD_OPTIONS;
 
 #ifdef ANDROID
 #include <android/log.h>
 
 static void
-_je_malloc_message(void *cbopaque, const char *s)
+_je_malloc_message(void* cbopaque, const char* s)
 {
   __android_log_print(ANDROID_LOG_INFO, "GeckoJemalloc", "%s", s);
 }
 
-void (*je_(malloc_message))(void *, const char *s) = _je_malloc_message;
+void (*je_(malloc_message))(void*, const char* s) = _je_malloc_message;
 #endif
 
 #endif /* MOZ_JEMALLOC3 */
@@ -46,6 +46,6 @@ void (*je_(malloc_message))(void *, const char *s) = _je_malloc_message;
 /* Provide an abort function for use in jemalloc code */
 #include <mozilla/Assertions.h>
 
-void moz_abort() {
+extern "C" void moz_abort() {
   MOZ_CRASH();
 }
