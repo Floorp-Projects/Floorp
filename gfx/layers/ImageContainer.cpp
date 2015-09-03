9ded36196f0edf39a1472b546e821ac2f2c11bad
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-09-03 08:38:28 +0200

    Backed out changeset 446055bf8ee4 (bug 1199371)

--- gfx/layers/ImageContainer.cpp
+++ gfx/layers/ImageContainer.cpp
@@ -170,7 +170,6 @@ ImageContainer::ImageContainer(Mode flag)
 : mReentrantMonitor("ImageContainer.mReentrantMonitor"),
   mGenerationCounter(++sGenerationCounter),
   mPaintCount(0),
-  mAttachCount(0),
   mDroppedImageCount(0),
   mImageFactory(new ImageFactory()),
   mRecycleBin(new BufferRecycleBin()),
@@ -233,28 +232,6 @@ ImageContainer::CreateImage(ImageFormat aFormat)
     }
   }
   return mImageFactory->CreateImage(aFormat, mScaleHint, mRecycleBin);
-}
-
-void
-ImageContainer::NotifyAttached()
-{
-  ReentrantMonitorAutoEnter mon(mReentrantMonitor);
-  if (!mAttachCount++) {
-    if (IsAsync()) {
-      ImageBridgeChild::DispatchImageClientUpdate(mImageClient, this);
-    }
-  }
-}
-
-void
-ImageContainer::NotifyDetached()
-{
-  ReentrantMonitorAutoEnter mon(mReentrantMonitor);
-  if (!--mAttachCount) {
-    if (IsAsync()) {
-      ImageBridgeChild::FlushAllImagesAsync(mImageClient);
-    }
-  }
 }
 
 void
