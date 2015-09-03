6ac40622c36d33baf2d8e4f46e2aed9ad0b84ed5
Ehsan Akhgari <ehsan@mozilla.com>
2015-09-02 20:59:43 -0400

    Bug 1201229 - Return an empty string for a header when an error occurs; r=dragana
    This fixes nsIHttpChannel::GetRequestHeader() and
    nsIHttpChannel::GetResponseHeader() to always empty out their string
    argument even when they fail.  This prevents programming mistakes of
    passing the same string object to multiple of these calls and using the
    string value without checking the nsresult error code, since otherwise
    the string value may be unchanged from a previous call.
    
    Note that this doesn't affect JS consumers of these APIs since we only
    empty out the string argument in case the method fails, which will be
    translated to a JS exception, and the JS code will never get to see the
    emptied string.

--- netwerk/protocol/viewsource/nsViewSourceChannel.cpp
+++ netwerk/protocol/viewsource/nsViewSourceChannel.cpp
@@ -733,6 +733,7 @@ NS_IMETHODIMP
 nsViewSourceChannel::GetRequestHeader(const nsACString & aHeader,
                                       nsACString & aValue)
 {
+    aValue.Truncate();
     return !mHttpChannel ? NS_ERROR_NULL_POINTER :
         mHttpChannel->GetRequestHeader(aHeader, aValue);
 }
@@ -827,6 +828,7 @@ NS_IMETHODIMP
 nsViewSourceChannel::GetResponseHeader(const nsACString & aHeader,
                                        nsACString & aValue)
 {
+    aValue.Truncate();
     if (!mHttpChannel)
         return NS_ERROR_NULL_POINTER;
 
@@ -838,10 +840,9 @@ nsViewSourceChannel::GetResponseHeader(const nsACString & aHeader,
                         nsCaseInsensitiveCStringComparator()) &&
         !aHeader.Equals(NS_LITERAL_CSTRING("X-Frame-Options"),
                         nsCaseInsensitiveCStringComparator())) {
-        aValue.Truncate();
         return NS_OK;
     }
-        
+
     return mHttpChannel->GetResponseHeader(aHeader, aValue);
 }
 
