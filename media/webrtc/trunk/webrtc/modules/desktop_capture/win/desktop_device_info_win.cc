9704fc65696fedaf8e7c6ef59014a972931edc2b
Pavlo <ffcontribute@gmail.com>
2015-09-03 07:24:00 +0200

    Bug 1037997 - Added possibility to choose monitors during screen capturing. r=florian,jesup
    --HG--
    extra : rebase_source : 6b2476d5c03dffa27c8a5c570ec1392d332b81d2

--- media/webrtc/trunk/webrtc/modules/desktop_capture/win/desktop_device_info_win.cc
+++ media/webrtc/trunk/webrtc/modules/desktop_capture/win/desktop_device_info_win.cc
@@ -3,7 +3,9 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "webrtc/modules/desktop_capture/win/desktop_device_info_win.h"
+#include "webrtc/modules/desktop_capture/win/screen_capture_utils.h"
 #include "webrtc/modules/desktop_capture/win/win_shared.h"
+#include <inttypes.h>
 #include <stdio.h>
 
 // Duplicating declaration so that it always resolves in decltype use
@@ -31,27 +33,49 @@ DesktopDeviceInfoWin::DesktopDeviceInfoWin() {
 DesktopDeviceInfoWin::~DesktopDeviceInfoWin() {
 }
 
-#if !defined(MULTI_MONITOR_SCREENSHARE)
 void DesktopDeviceInfoWin::MultiMonitorScreenshare()
 {
-  DesktopDisplayDevice *pDesktopDeviceInfo = new DesktopDisplayDevice;
-  if (pDesktopDeviceInfo) {
-    pDesktopDeviceInfo->setScreenId(webrtc::kFullDesktopScreenId);
-    pDesktopDeviceInfo->setDeviceName("Primary Monitor");
+#if !defined(MULTI_MONITOR_SCREENSHARE)
+  DesktopDisplayDevice* desktop_device_info = new DesktopDisplayDevice;
+  if (desktop_device_info) {
+    desktop_device_info->setScreenId(webrtc::kFullDesktopScreenId);
+    desktop_device_info->setDeviceName("Primary Monitor");
 
-    char idStr[64];
-    _snprintf_s(idStr, sizeof(idStr), sizeof(idStr) - 1, "%ld", pDesktopDeviceInfo->getScreenId());
-    pDesktopDeviceInfo->setUniqueIdName(idStr);
-    desktop_display_list_[pDesktopDeviceInfo->getScreenId()] = pDesktopDeviceInfo;
+     char idStr[64];
+    _snprintf_s(idStr, sizeof(idStr), sizeof(idStr) - 1, "%" PRIdPTR, desktop_device_info->getScreenId());
+    desktop_device_info->setUniqueIdName(idStr);
+    desktop_display_list_[desktop_device_info->getScreenId()] = desktop_device_info;
   }
-}
+#else
+  ScreenCapturer::ScreenList screens;
+  GetScreenList(&screens);
+  auto num_of_screens = screens.size();
+
+  for (decltype(num_of_screens) i = 0; i < num_of_screens; ++i) {
+    DesktopDisplayDevice* desktop_device_info = new DesktopDisplayDevice;
+    if (desktop_device_info) {
+      desktop_device_info->setScreenId(screens[i].id);
+      if (1 >= num_of_screens) {
+        desktop_device_info->setDeviceName("Primary Monitor");
+      } else {
+        char nameStr[64];
+        _snprintf_s(nameStr, sizeof(nameStr), sizeof(nameStr) - 1, "Screen %" PRIdPTR, i + 1);
+        desktop_device_info->setDeviceName(nameStr);
+      }
+
+      char idStr[64];
+      _snprintf_s(idStr, sizeof(idStr), sizeof(idStr) - 1, "%" PRIdPTR, desktop_device_info->getScreenId());
+      desktop_device_info->setUniqueIdName(idStr);
+      desktop_display_list_[desktop_device_info->getScreenId()] = desktop_device_info;
+    }
+  }
 #endif
+}
 
 void DesktopDeviceInfoWin::InitializeScreenList() {
-#if !defined(MULTI_MONITOR_SCREENSHARE)
   MultiMonitorScreenshare();
-#endif
 }
+
 void DesktopDeviceInfoWin::InitializeApplicationList() {
   // List all running applications exclude background process.
   HWND hWnd;
