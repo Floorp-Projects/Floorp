8cdc08a5135f7285dafb5e92a20da668da28f30a
Byron Campen [:bwc] <docfaraday@gmail.com>
2015-09-04 15:25:37 -0500

    Bug 1186590 - Part 2 - Move hard-coded interface priority list into nrinterfaceprioritizer, and simplify some functions. r=drno

--- media/mtransport/nricectx.h
+++ media/mtransport/nricectx.h
@@ -216,7 +216,6 @@ class NrIceCtx {
   // TODO(ekr@rtfm.com): Too many bools here. Bug 1193437.
   static RefPtr<NrIceCtx> Create(const std::string& name,
                                  bool offerer,
-                                 bool set_interface_priorities = true,
                                  bool allow_loopback = false,
                                  bool tcp_enabled = true,
                                  bool allow_link_local = false,
