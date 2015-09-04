f4a5cb6cd8da4fd76f79945d403a8fab726aaf75
Masayuki Nakano <masayuki@d-toybox.com>
2015-09-04 19:54:11 +0900

    Bug 895274 part.88 Rename NS_XUL_POPUP_SHOWN to eXULPopupShown r=smaug

--- dom/events/EventNameList.h
+++ dom/events/EventNameList.h
@@ -700,7 +700,7 @@ NON_IDL_EVENT(popupshowing,
               EventNameType_XUL,
               eBasicEventClass)
 NON_IDL_EVENT(popupshown,
-              NS_XUL_POPUP_SHOWN,
+              eXULPopupShown,
               EventNameType_XUL,
               eBasicEventClass)
 NON_IDL_EVENT(popuphiding,
