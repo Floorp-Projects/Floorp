2519f3310af977b6714fee4695347635ee814fa0
Boris Zbarsky <bzbarsky@mit.edu>
2015-09-03 22:42:49 -0400

    Bug 1194764.  Fix the way we pass the outer window id to various things in context menu code to work in non-e10s mode too.  r=jryans a=ritu

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -1063,7 +1063,7 @@ nsContextMenu.prototype = {
     BrowserViewSourceOfDocument({
       browser: this.browser,
       URL: gContextMenuContentData.docLocation,
-      outerWindowID: gContextMenuContentData.frameOuterWindowID,
+      outerWindowID: this.frameOuterWindowID,
     });
   },
 
@@ -1377,7 +1377,7 @@ nsContextMenu.prototype = {
     urlSecurityCheck(this.linkURL, this.principal);
     this.saveHelper(this.linkURL, this.linkTextStr, null, true, this.ownerDoc,
                     gContextMenuContentData.documentURIObject,
-                    gContextMenuContentData.frameOuterWindowID,
+                    this.frameOuterWindowID,
                     this.linkDownload);
   },
 
@@ -1408,7 +1408,7 @@ nsContextMenu.prototype = {
       urlSecurityCheck(this.mediaURL, this.principal);
       var dialogTitle = this.onVideo ? "SaveVideoTitle" : "SaveAudioTitle";
       this.saveHelper(this.mediaURL, null, dialogTitle, false, doc, referrerURI,
-                      gContextMenuContentData.frameOuterWindowID, "");
+                      this.frameOuterWindowID, "");
     }
   },
 
