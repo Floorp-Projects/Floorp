bcc9baba911a10300b347a4a53f185d6ef4409e3
Nicolas Silva <nsilva@mozilla.com>
2015-09-04 12:11:33 +0200

    Bug 1198674 - null-check mFrameLoader in RenderFrameParent. r=sotaro

--- layout/ipc/RenderFrameParent.cpp
+++ layout/ipc/RenderFrameParent.cpp
@@ -417,7 +417,7 @@ RenderFrameParent::OwnerContentChanged(nsIContent* aContent)
   MOZ_ASSERT(mFrameLoader->GetOwnerContent() == aContent,
              "Don't build new map if owner is same!");
 
-  nsRefPtr<LayerManager> lm = GetFrom(mFrameLoader);
+  nsRefPtr<LayerManager> lm = mFrameLoader ? GetFrom(mFrameLoader) : nullptr;
   // Perhaps the document containing this frame currently has no presentation?
   if (lm && lm->GetBackendType() == LayersBackend::LAYERS_CLIENT) {
     ClientLayerManager *clientManager =
