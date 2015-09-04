716637f21102556d23579d56959b0018855481c0
Dave Townsend <dtownsend@oxymoronical.com>
2015-09-04 12:00:47 -0700

    Bug 1192921: Add an install location for system add-ons. r=rhelmer
    This adds two new directory install locations. One contains the default system
    add-ons that ship with the application, the other contains system add-on that
    will eventually be updatable at runtime.
    
    The updatable location tracks the expected list of add-ons in a pref. and only
    returns add-ons from that list when asked for its list of add-ons.
    
    After processFileChanges has scanned all add-ons and updated the database it
    checks if the updated system add-ons match the expected set. If not we ignore
    those add-ons when working out which add-ons should be visible. If they do match
    then we ignore the app-shipped system add-ons when working out which are
    visible.
    
    --HG--
    extra : commitid : LYCHZGSoGwj
    extra : rebase_source : c9bc96b36d23ba9b4374adead9b59059ccb02f39

---
+++ toolkit/mozapps/extensions/test/xpcshell/data/system_addons/app2/features/system_1_2/bootstrap.js
@@ -0,0 +1,18 @@
+Components.utils.import("resource://gre/modules/Services.jsm");
+
+const ID = "system1@tests.mozilla.org";
+const VERSION = "2.0";
+
+function install(data, reason) {
+}
+
+function startup(data, reason) {
+  Services.prefs.setCharPref("bootstraptest." + ID + ".active_version", VERSION);
+}
+
+function shutdown(data, reason) {
+  Services.prefs.clearUserPref("bootstraptest." + ID + ".active_version");
+}
+
+function uninstall(data, reason) {
+}
