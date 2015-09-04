aa9f15a3f8276c69eaa87879fd74bc819ff679c8
Wes Kocher <wkocher@mozilla.com>
2015-09-04 16:22:48 -0700

    Backed out changeset f7ccd13584ad (bug 1174546)

--- layout/generic/nsGridContainerFrame.h
+++ layout/generic/nsGridContainerFrame.h
@@ -54,8 +54,6 @@ public:
     nscoord mLimit;
   };
 
-  enum Dimension { eColDimension, eRowDimension };
-
   // @see nsAbsoluteContainingBlock::Reflow about this magic number
   static const nscoord VERY_LIKELY_A_GRID_CONTAINER = -123456789;
 
