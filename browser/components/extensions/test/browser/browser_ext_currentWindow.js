440294182d0b23319c5ddad74038d8a057c4c14d
Bill McCloskey <billm@mozilla.com>
2015-09-07 09:59:21 -0700

    Bug 1202483 - [webext] Support ImageData for browser.browserAction.setIcon (r=gabor)

--- browser/components/extensions/test/browser/browser_ext_currentWindow.js
+++ browser/components/extensions/test/browser/browser_ext_currentWindow.js
@@ -1,11 +1,3 @@
-let {CustomizableUI} = Cu.import("resource:///modules/CustomizableUI.jsm", {});
-
-function makeWidgetId(id)
-{
-  id = id.toLowerCase();
-  return id.replace(/[^a-z0-9_-]/g, "_");
-}
-
 function* focusWindow(win)
 {
   let fm = Cc["@mozilla.org/focus-manager;1"].getService(Ci.nsIFocusManager);
