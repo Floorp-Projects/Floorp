fc35c8aaef1ba39fe4d92fa7abdd8edf2b320dd1
Masayuki Nakano <masayuki@d-toybox.com>
2015-09-08 23:33:34 +0900

    Bug 895274 part.116 Rename NS_COPY to eCopy r=smaug

--- dom/base/nsGlobalWindowCommands.cpp
+++ dom/base/nsGlobalWindowCommands.cpp
@@ -520,7 +520,7 @@ nsClipboardCommand::DoCommand(const char *aCommandName, nsISupports *aContext)
   nsCOMPtr<nsIPresShell> presShell = docShell->GetPresShell();
   NS_ENSURE_TRUE(presShell, NS_ERROR_FAILURE);
 
-  EventMessage eventMessage = NS_COPY;
+  EventMessage eventMessage = eCopy;
   if (strcmp(aCommandName, "cmd_cut") == 0) {
     eventMessage = NS_CUT;
   }
