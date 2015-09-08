5389877353fe215f1732818e1cd63552fe99e1f4
Karl Tomlinson <karlt+@karlt.net>
2015-09-08 12:04:16 +1200

    bug 1197028 introduce AudioBlock to keep track of downstream references to AudioBlockBuffer r=padenot

--- dom/media/webaudio/AudioBlock.h
+++ dom/media/webaudio/AudioBlock.h
@@ -11,6 +11,94 @@
 namespace mozilla {
 
 /**
+ * An AudioChunk whose buffer contents need to be valid only for one
+ * processing block iteration, after which contents can be overwritten if the
+ * buffer has not been passed to longer term storage or to another thread,
+ * which may happen though AsAudioChunk() or AsMutableChunk().
+ *
+ * Use on graph thread only.
+ */
+class AudioBlock : private AudioChunk
+{
+public:
+  AudioBlock() {
+    mDuration = WEBAUDIO_BLOCK_SIZE;
+  }
+  AudioBlock(const AudioChunk& aChunk) {
+    mDuration = WEBAUDIO_BLOCK_SIZE;
+    operator=(aChunk);
+  }
+  ~AudioBlock();
+
+  using AudioChunk::GetDuration;
+  using AudioChunk::IsNull;
+  using AudioChunk::ChannelCount;
+  using AudioChunk::ChannelData;
+  using AudioChunk::SizeOfExcludingThisIfUnshared;
+  using AudioChunk::SizeOfExcludingThis;
+  // mDuration is not exposed.  Use GetDuration().
+  // mBuffer is not exposed.  Use SetBuffer().
+  using AudioChunk::mChannelData;
+  using AudioChunk::mVolume;
+  using AudioChunk::mBufferFormat;
+
+  const AudioChunk& AsAudioChunk() const { return *this; }
+  AudioChunk* AsMutableChunk() {
+    void ClearDownstreamMark();
+    return this;
+  }
+
+  void SetBuffer(ThreadSharedObject* aNewBuffer);
+  void SetNull(StreamTime aDuration) {
+    MOZ_ASSERT(aDuration == WEBAUDIO_BLOCK_SIZE);
+    SetBuffer(nullptr);
+    mChannelData.Clear();
+    mVolume = 1.0f;
+    mBufferFormat = AUDIO_FORMAT_SILENCE;
+  }
+
+  AudioBlock& operator=(const AudioChunk& aChunk) {
+    MOZ_ASSERT(aChunk.mDuration == WEBAUDIO_BLOCK_SIZE);
+    SetBuffer(aChunk.mBuffer);
+    mChannelData = aChunk.mChannelData;
+    mVolume = aChunk.mVolume;
+    mBufferFormat = aChunk.mBufferFormat;
+    return *this;
+  }
+
+  bool IsMuted() const { return mVolume == 0.0f; }
+
+  bool IsSilentOrSubnormal() const
+  {
+    if (!mBuffer) {
+      return true;
+    }
+
+    for (uint32_t i = 0, length = mChannelData.Length(); i < length; ++i) {
+      const float* channel = static_cast<const float*>(mChannelData[i]);
+      for (StreamTime frame = 0; frame < mDuration; ++frame) {
+        if (fabs(channel[frame]) >= FLT_MIN) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+private:
+  void ClearDownstreamMark();
+
+  // mBufferIsDownstreamRef is set only when mBuffer references an
+  // AudioBlockBuffer created in a different AudioBlock.  That can happen when
+  // this AudioBlock is on a node downstream from the node which created the
+  // buffer.  When this is set, the AudioBlockBuffer is notified that this
+  // reference does prevent the upstream node from re-using the buffer next
+  // iteration and modifying its contents.  The AudioBlockBuffer is also
+  // notified when mBuffer releases this reference.
+  bool mBufferIsDownstreamRef = false;
+};
+/**
  * Allocates, if necessary, aChannelCount buffers of WEBAUDIO_BLOCK_SIZE float
  * samples for writing to an AudioChunk.
  */
