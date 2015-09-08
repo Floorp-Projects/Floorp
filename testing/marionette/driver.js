45a427311d31569817c8c18c4ee35f145ec6bdb3
Andreas Tolfsen <ato@mozilla.com>
2015-09-08 15:26:19 +0100

    Bug 1202663: Use dispatcher for screen capture command in listener
    r=dburns

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -2562,6 +2562,8 @@ GeckoDriver.prototype.clearImportedScripts = function(cmd, resp) {
  *     PNG image encoded as base64 encoded string.
  */
 GeckoDriver.prototype.takeScreenshot = function(cmd, resp) {
+  let {id, highlights, full} = cmd.parameters;
+
   switch (this.context) {
     case Context.CHROME:
       let win = this.getCurrentWindow();
@@ -2604,10 +2606,7 @@ GeckoDriver.prototype.takeScreenshot = function(cmd, resp) {
       break;
 
     case Context.CONTENT:
-      resp.body.value = yield this.listener.takeScreenshot({
-        id: cmd.parameters.id,
-        highlights: cmd.parameters.highlights,
-        full: cmd.parameters.full});
+      return this.listener.takeScreenshot(id, highlights, full);
       break;
   }
 };
