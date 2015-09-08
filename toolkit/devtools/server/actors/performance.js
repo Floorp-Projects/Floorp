f1dd36d8031403b4ab1c0ea3e6ae1a898bb7ee66
Jordan Santell <jsantell@mozilla.com>
2015-09-08 15:24:03 -0700

    Bug 1171488 - Attach host and client system data to recording profiles r=vp,mossop

--- toolkit/devtools/server/actors/performance.js
+++ toolkit/devtools/server/actors/performance.js
@@ -22,6 +22,8 @@ loader.lazyRequireGetter(this, "normalizePerformanceFeatures",
   "devtools/toolkit/performance/utils", true);
 loader.lazyRequireGetter(this, "LegacyPerformanceFront",
   "devtools/toolkit/performance/legacy/front", true);
+loader.lazyRequireGetter(this, "getSystemInfo",
+  "devtools/toolkit/shared/system", true);
 
 const PIPE_TO_FRONT_EVENTS = new Set([
   "recording-started", "recording-stopping", "recording-stopped",
@@ -97,10 +99,13 @@ let PerformanceActor = exports.PerformanceActor = protocol.ActorClass({
     protocol.Actor.prototype.destroy.call(this);
   },
 
-  connect: method(function () {
-    this.bridge.connect();
-    return this.traits;
-  }, { response: RetVal("json") }),
+  connect: method(function (config) {
+    this.bridge.connect({ systemClient: config.systemClient });
+    return { traits: this.traits };
+  }, {
+    request: { options: Arg(0, "nullable:json") },
+    response: RetVal("json")
+  }),
 
   startRecording: method(Task.async(function *(options={}) {
     let normalizedOptions = normalizePerformanceFeatures(options, this.traits.features);
@@ -182,9 +187,17 @@ const PerformanceFront = exports.PerformanceFront = protocol.FrontClass(Performa
     protocol.Front.prototype.destroy.call(this);
   },
 
-  connect: custom(function () {
-    return this._connect().then(traits => this._traits = traits);
-  }, {
+  /**
+   * Conenct to the server, and handle once-off tasks like storing traits
+   * or system info.
+   */
+  connect: custom(Task.async(function *() {
+    let systemClient = yield getSystemInfo();
+    let { traits } = yield this._connect({ systemClient });
+    this._traits = traits;
+
+    return this._traits;
+  }), {
     impl: "_connect"
   }),
 
@@ -239,6 +252,8 @@ const PerformanceFront = exports.PerformanceFront = protocol.FrontClass(Performa
       model._allocations = recordingData.allocations;
       model._profile = recordingData.profile;
       model._configuration = recordingData.configuration || {};
+      model._systemHost = recordingData.systemHost;
+      model._systemClient = recordingData.systemClient;
       return model;
     });
   },
