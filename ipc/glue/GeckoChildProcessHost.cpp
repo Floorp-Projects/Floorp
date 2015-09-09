6af261e7f06c26841d49dece71e9004def66e078
Jacek Caban <jacek@codeweavers.com>
2015-09-09 14:17:12 +0200

    Bug 1201438 - Fixup for builds with disabled sandbox. a=sledru

--- ipc/glue/GeckoChildProcessHost.cpp
+++ ipc/glue/GeckoChildProcessHost.cpp
@@ -984,6 +984,7 @@ GeckoChildProcessHost::PerformAsyncLaunchInternal(std::vector<std::string>& aExt
   {
     base::LaunchApp(cmdLine, false, false, &process);
 
+#ifdef MOZ_SANDBOX
     // We need to be able to duplicate handles to non-sandboxed content
     // processes, so add it as a target peer.
     if (mProcessType == GeckoProcessType_Content) {
@@ -991,6 +992,7 @@ GeckoChildProcessHost::PerformAsyncLaunchInternal(std::vector<std::string>& aExt
         NS_WARNING("Failed to add content process as target peer.");
       }
     }
+#endif
   }
 
 #else
