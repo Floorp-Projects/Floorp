4d7e1e1915b98a95bb1f4cb60358f4d79fd4e32b
Brian Grinstead <bgrinstead@mozilla.com>
2015-09-09 15:19:36 -0700

    Bug 1203280 - Back out all non-test changes from changeset f460ad2878d9 (bug 1193158);r=MattN

--- browser/base/content/test/general/browser_permissions.js
+++ browser/base/content/test/general/browser_permissions.js
@@ -38,29 +38,3 @@ add_task(function* testMainViewVisible() {
   ok(is_hidden(gIdentityHandler._permissionsContainer), "The container is hidden");
   gIdentityHandler._identityPopup.hidden = true;
 });
-
-add_task(function* testSubviewListing() {
-  let {gIdentityHandler} = gBrowser.ownerGlobal;
-  gIdentityHandler.setPermission("install", 1);
-
-  info("Opening control center and expanding permissions subview");
-  gIdentityHandler._identityBox.click();
-
-  let menulists = gIdentityHandler._permissionSubviewList.querySelectorAll("menulist");
-  let perms = SitePermissions.listPermissions();
-
-  is(menulists.length, perms.length, "One menulist for each permission");
-
-  for (let i = 0; i < menulists.length; i++) {
-    let menulist = menulists[i];
-    let perm = perms[i];
-    let expectedValue = SitePermissions.get(gBrowser.currentURI, perm);
-    if (expectedValue == SitePermissions.UNKNOWN) {
-      expectedValue = SitePermissions.getDefault(perm);
-    }
-
-    is(menulist.id, "identity-popup-permission:" + perm, "Correct id for menulist: " + perm);
-    is(menulist.value, expectedValue, "Correct value on menulist: " + perm);
-  }
-  gIdentityHandler._identityPopup.hidden = true;
-});
