4cae64b30418dc64763c45b29b02cb5ba2815387
Matt Woodrow <mwoodrow@mozilla.com>
2015-09-09 11:38:02 -0400

    Backed out 358055206195 (bug 1196411) for causing intel crashes (bug 1202700).

--- dom/media/platforms/wmf/DXVA2Manager.h
+++ dom/media/platforms/wmf/DXVA2Manager.h
@@ -44,7 +44,7 @@ public:
 
   virtual ~DXVA2Manager();
 
-  virtual bool SupportsConfig(IMFMediaType* aType, float aFramerate) = 0;
+  virtual bool SupportsConfig(IMFMediaType* aType) { return true; }
 
 protected:
   Mutex mLock;
