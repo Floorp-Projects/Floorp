ef4300479e4c3ad4d34789d3e4a61badc6f069af
Ting-Yu Lin <tlin@mozilla.com>
2015-09-09 16:47:20 +0800

    Bug 1200194 - Add UpdateCaretsHint to guide appearance changing. r=roc
    This is the patch which fixed the bug.
    
    When calling OnScrollPositionChanged in cursor mode, we want the
    appearance of the caret to be preserved since the caret might be hidden
    due to timeout. We should respect the old appearance of the caret.
    
    Add a marionette test to ensure the caret does not appear due to
    ScrollPositionChanged.

--- layout/base/tests/marionette/test_touchcaret.py
+++ layout/base/tests/marionette/test_touchcaret.py
@@ -7,6 +7,7 @@ from marionette_driver.by import By
 from marionette_driver.marionette import Actions
 from marionette_driver.selection import SelectionManager
 from marionette import MarionetteTestCase
+import string
 
 
 class CommonCaretTestCase(object):
@@ -206,6 +207,31 @@ class CommonCaretTestCase(object):
         el.send_keys(content_to_add)
         assertFunc(non_target_content, sel.content)
 
+    def _test_caret_not_appear_when_typing_in_scrollable_content(self, el, assertFunc):
+        sel = SelectionManager(el)
+
+        content_to_add = '!'
+        target_content = sel.content + string.ascii_letters + content_to_add
+
+        el.tap()
+        sel.move_caret_to_end()
+
+        # Insert a long string to the end of the <input>, which triggers
+        # ScrollPositionChanged event.
+        el.send_keys(string.ascii_letters)
+
+        # The caret should not be visible. If it does appear wrongly due to the
+        # ScrollPositionChanged event, we can drag it to the front of the
+        # <input> to change the cursor position.
+        src_x, src_y = sel.touch_caret_location()
+        dest_x, dest_y = 0, 0
+        self.actions.flick(el, src_x, src_y, dest_x, dest_y).perform()
+
+        # The content should be inserted at the end of the <input>.
+        el.send_keys(content_to_add)
+
+        assertFunc(target_content, sel.content)
+
     ########################################################################
     # <input> test cases with touch caret enabled
     ########################################################################
@@ -220,6 +246,10 @@ class CommonCaretTestCase(object):
     def test_input_move_caret_to_front_by_dragging_touch_caret_to_top_left_corner(self):
         self.open_test_html()
         self._test_move_caret_to_front_by_dragging_touch_caret_to_front_of_content(self._input, self.assertEqual)
+
+    def test_input_caret_not_appear_when_typing_in_scrollable_content(self):
+        self.open_test_html()
+        self._test_caret_not_appear_when_typing_in_scrollable_content(self._input, self.assertEqual)
 
     def test_input_touch_caret_timeout(self):
         self.open_test_html(timeout_ms=1000)
