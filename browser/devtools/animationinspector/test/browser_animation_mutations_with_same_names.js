8f02926feefea41a32085f979134e88f878dd6d0
Patrick Brosset <pbrosset@mozilla.com>
2015-09-09 11:07:29 +0200

    Bug 1174060 - 6 - Tests for how delays are displayed in the timeline; r=ochameau
    Added tests to ensure negative and positive delays are shown correctly
    and that the timeScale window is computed correctly.
    Also added a test to ensure that animations with the same name but
    different nodes don't override each others in the UI.
    
    This commit also cleans up a lot of exceptions that were thrown while
    tests were running. These exceptions were due to pending protocol requests
    when tests ended.
    
    --HG--
    extra : commitid : FAEjthxcFYj
    extra : rebase_source : 090d41fce18e147b109f92afa81b4f2f76295348

---
+++ browser/devtools/animationinspector/test/browser_animation_mutations_with_same_names.js
@@ -0,0 +1,31 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Check that when animations are added later (through animation mutations) and
+// if these animations have the same names, then all of them are still being
+// displayed (which should be true as long as these animations apply to
+// different nodes).
+
+add_task(function*() {
+  yield addTab(TEST_URL_ROOT + "doc_negative_animation.html");
+  let {controller, panel} = yield openAnimationInspectorNewUI();
+
+  info("Wait until all animations have been added " +
+       "(they're added with setTimeout)");
+  while (controller.animationPlayers.length < 3) {
+    yield controller.once(controller.PLAYERS_UPDATED_EVENT);
+  }
+  yield waitForAllAnimationTargets(panel);
+
+  is(panel.animationsTimelineComponent.animations.length, 3,
+     "The timeline shows 3 animations too");
+
+  // Reduce the known nodeFronts to a set to make them unique.
+  let nodeFronts = new Set(panel.animationsTimelineComponent
+                                .targetNodes.map(n => n.nodeFront));
+  is(nodeFronts.size, 3,
+     "The animations are applied to 3 different node fronts");
+});
