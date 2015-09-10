e86c404663d98ae156fb3697fa34d731073dc073
Ben Hsu <driftersprt@gmail.com>
2015-09-11 03:09:00 +0800

    Bug 1147296 - Part 4: Expose new functions (head.js). r=btseng

--- dom/telephony/test/marionette/head.js
+++ dom/telephony/test/marionette/head.js
@@ -1482,6 +1482,25 @@ let Modem = Modems[0];
   this.gSetupConference = setupConference;
   this.gSetRadioEnabled = setRadioEnabled;
   this.gSetRadioEnabledAll = setRadioEnabledAll;
+
+  // Telephony helper
+  this.TelephonyHelper = {
+    dial:   dial,
+    answer: answer,
+    hangUp: hangUp,
+    hold:   hold,
+    resume: resume,
+    equals: equals,
+    createExptectedCall: createExptectedCall
+  };
+
+  // Remote Utils, TODO: This should be an array for multi-SIM scenarios
+  this.Remotes = [{
+    dial:   remoteDial,
+    answer: remoteAnswer,
+    hangUp: remoteHangUp
+  }];
+  this.Remote = this.Remotes[0];
 }());
 
 function _startTest(permissions, test) {
