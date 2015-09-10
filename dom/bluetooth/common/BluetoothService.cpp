21b42a6e6540b086ef9a01e2be6e4079286bf8cd
Jocelyn Liu <joliu@mozilla.com>
2015-09-10 10:55:02 +0800

    Bug 1199653 - Correctly set |sInShutdown| in BluetoothService for content processes. f=tzimmermann, r=shuang

--- dom/bluetooth/common/BluetoothService.cpp
+++ dom/bluetooth/common/BluetoothService.cpp
@@ -532,8 +532,6 @@ BluetoothService::HandleShutdown()
   // bluetooth is going away, and then we wait for them to acknowledge. Then we
   // close down all the bluetooth machinery.
 
-  sInShutdown = true;
-
   Cleanup();
 
   AutoInfallibleTArray<BluetoothParent*, 10> childActors;
@@ -633,6 +631,13 @@ BluetoothService::Observe(nsISupports* aSubject, const char* aTopic,
   }
 
   if (!strcmp(aTopic, NS_XPCOM_SHUTDOWN_OBSERVER_ID)) {
+    /**
+     * |sInShutdown| flag should be set for instances created in content
+     * processes or parent processes. Please see bug 1199653 for detailed
+     * information.
+     */
+    sInShutdown = true;
+
     return HandleShutdown();
   }
 
