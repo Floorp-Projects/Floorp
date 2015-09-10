4d5b278164fd18ceebdb4ffa58e3ec07d20c8419
Milan Sreckovic <milan@mozilla.com>
2015-09-10 10:55:00 +0200

    Bug 1203626 - remove the unused argument from nsTreeBodyFrame::GetTwistyRect. r=mattwoodrow

--- layout/xul/tree/nsTreeBodyFrame.h
+++ layout/xul/tree/nsTreeBodyFrame.h
@@ -325,7 +325,6 @@ protected:
                           nsRect& aImageRect,
                           nsRect& aTwistyRect,
                           nsPresContext* aPresContext,
-                          nsRenderingContext& aRenderingContext,
                           nsStyleContext* aTwistyContext);
 
   // Fetch an image from the image cache.
