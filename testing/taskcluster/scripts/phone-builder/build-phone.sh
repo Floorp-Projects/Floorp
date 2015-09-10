c8490465d9fb94d9304fc16357785f515c31e543
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-09-10 14:25:24 +0200

    Backed out 1 changesets (bug 1201179) for bustage on a CLOSED TREE
    Backed out changeset aa3cbda42b11 (bug 1201179)

--- testing/taskcluster/scripts/phone-builder/build-phone.sh
+++ testing/taskcluster/scripts/phone-builder/build-phone.sh
@@ -4,7 +4,7 @@
 
 if [ $TARGET == "aries" -o $TARGET == "shinano" ]; then
   # caching objects might be dangerous for some devices (aka aries)
-  rm -rf $gecko_objdir
+  rm -rf $WORKSPACE/B2G/objdir*
   rm -rf $WORKSPACE/B2G/out
 fi
 
@@ -23,7 +23,6 @@ $WORKSPACE/gecko/testing/mozharness/scripts/b2g_build.py \
   --target=$TARGET \
   --b2g-config-dir=$TARGET \
   --checkout-revision=$GECKO_HEAD_REV \
-  --repo=$WORKSPACE/gecko \
-  --gecko-objdir=$gecko_objdir
+  --repo=$WORKSPACE/gecko
 
 . post-build.sh
