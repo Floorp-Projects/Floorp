cbe225b2322409fb3fcf9e6b0cf0720b825179d8
Ehsan Akhgari <ehsan@mozilla.com>
2015-09-09 20:10:56 -0400

    Bug 1203386 - Fix two implicit constructors in ActorsChild.h; r=baku

--- dom/filehandle/ActorsChild.h
+++ dom/filehandle/ActorsChild.h
@@ -89,8 +89,8 @@ class BackgroundFileHandleChild
   FileHandleBase* mFileHandle;
 
 public:
-  BackgroundFileHandleChild(DEBUGONLY(PRThread* aOwningThread,)
-                            FileHandleBase* aFileHandle);
+  explicit BackgroundFileHandleChild(DEBUGONLY(PRThread* aOwningThread,)
+                                     FileHandleBase* aFileHandle);
 
   void
   SendDeleteMeInternal();
@@ -136,8 +136,8 @@ class BackgroundFileRequestChild final
 
 private:
   // Only created by FileHandleBase.
-  BackgroundFileRequestChild(DEBUGONLY(PRThread* aOwningThread,)
-                             FileRequestBase* aFileRequest);
+  explicit BackgroundFileRequestChild(DEBUGONLY(PRThread* aOwningThread,)
+                                      FileRequestBase* aFileRequest);
 
   // Only destroyed by BackgroundFileHandleChild.
   ~BackgroundFileRequestChild();
