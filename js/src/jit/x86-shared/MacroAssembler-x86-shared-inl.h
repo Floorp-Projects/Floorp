4f5ffc39c7fc4ccb077277b63678ac605636b181
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2015-09-11 13:47:55 +0200

    Bug 1199719 part 8 - Move MacroAssembler::not32 into the generic macro assembler. r=h4writer

--- js/src/jit/x86-shared/MacroAssembler-x86-shared-inl.h
+++ js/src/jit/x86-shared/MacroAssembler-x86-shared-inl.h
@@ -17,6 +17,12 @@ namespace jit {
 // Logical instructions
 
 void
+MacroAssembler::not32(Register reg)
+{
+    notl(reg);
+}
+
+void
 MacroAssembler::and32(Register src, Register dest)
 {
     andl(src, dest);
