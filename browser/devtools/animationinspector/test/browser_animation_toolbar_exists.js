1ccc78e3951a60941f2d16b5181bc88beef235da
Patrick Brosset <pbrosset@mozilla.com>
2015-09-11 13:05:01 +0200

    Bug 1155661 - 2 - Add a new timeline toolbar shown only when the timeline is displayed and non empty; r=zer0

--- browser/devtools/animationinspector/test/browser_animation_toolbar_exists.js
+++ browser/devtools/animationinspector/test/browser_animation_toolbar_exists.js
@@ -6,6 +6,8 @@
 
 // Test that the animation panel has a top toolbar that contains the play/pause
 // button and that is displayed at all times.
+// Also test that with the new UI, that toolbar gets replaced by the timeline
+// toolbar when there are animations to be displayed.
 
 add_task(function*() {
   yield addTab(TEST_URL_ROOT + "doc_simple_animation.html");
@@ -14,7 +16,8 @@ add_task(function*() {
 
   let toolbar = doc.querySelector("#global-toolbar");
   ok(toolbar, "The panel contains the toolbar element");
-  ok(toolbar.querySelector("#toggle-all"), "The toolbar contains the toggle button");
+  ok(toolbar.querySelector("#toggle-all"),
+     "The toolbar contains the toggle button");
   ok(isNodeVisible(toolbar), "The toolbar is visible");
 
   info("Select an animated node");
@@ -23,4 +26,25 @@ add_task(function*() {
   toolbar = doc.querySelector("#global-toolbar");
   ok(toolbar, "The panel still contains the toolbar element");
   ok(isNodeVisible(toolbar), "The toolbar is still visible");
+
+  ({inspector, window} = yield closeAnimationInspectorAndRestartWithNewUI());
+  doc = window.document;
+  toolbar = doc.querySelector("#global-toolbar");
+
+  ok(toolbar, "The panel contains the toolbar element with the new UI");
+  ok(!isNodeVisible(toolbar),
+     "The toolbar is hidden while there are animations");
+
+  let timelineToolbar = doc.querySelector("#timeline-toolbar");
+  ok(timelineToolbar, "The panel contains a timeline toolbar element");
+  ok(isNodeVisible(timelineToolbar),
+     "The timeline toolbar is visible when there are animations");
+
+  info("Select a node that has no animations");
+  yield selectNode(".still", inspector);
+
+  ok(isNodeVisible(toolbar),
+     "The toolbar is shown when there are no animations");
+  ok(!isNodeVisible(timelineToolbar),
+     "The timeline toolbar is hidden when there are no animations");
 });
