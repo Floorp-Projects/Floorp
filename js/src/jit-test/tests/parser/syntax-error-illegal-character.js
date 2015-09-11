5b0b9bd2710f0e958318580215a105d57fd3eb45
Tooru Fujisawa <arai_a@mac.com>
2015-09-11 23:33:12 +0900

    Bug 1203963 - Part 1: Call test_syntax in syntax-error-illegal-character.js. r=Waldo

--- js/src/jit-test/tests/parser/syntax-error-illegal-character.js
+++ js/src/jit-test/tests/parser/syntax-error-illegal-character.js
@@ -10,3 +10,5 @@ function check_syntax_error(e, code, name) {
   assertEq(e instanceof SyntaxError, true, name + ": " + code);
   assertEq(e.message, JSMSG_ILLEGAL_CHARACTER, name + ": " + code);
 }
+
+test_syntax(repl_expr_flags, check_syntax_error, false);
