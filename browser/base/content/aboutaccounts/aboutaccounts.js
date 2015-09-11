da32f107464e88958d6ab1c698831f4b561e8dda
Mark Hammond <mhammond@skippinet.com.au>
2015-09-11 10:41:03 +1000

    Bug 969474 - fix behaviour of 'back' button on about:accounts. r=adw

--- browser/base/content/aboutaccounts/aboutaccounts.js
+++ browser/base/content/aboutaccounts/aboutaccounts.js
@@ -127,12 +127,15 @@ let wrapper = {
       url += (url.includes("?") ? "&" : "?") + urlParamStr;
     }
     this.url = url;
-    iframe.src = url;
+    // Set the iframe's location with loadURI/LOAD_FLAGS_BYPASS_HISTORY to
+    // avoid having a new history entry being added.
+    let webNav = iframe.frameLoader.docShell.QueryInterface(Ci.nsIWebNavigation);
+    webNav.loadURI(url, Ci.nsIWebNavigation.LOAD_FLAGS_BYPASS_HISTORY, null, null, null);
   },
 
   retry: function () {
     let webNav = this.iframe.frameLoader.docShell.QueryInterface(Ci.nsIWebNavigation);
-    webNav.loadURI(this.url, null, null, null, null);
+    webNav.loadURI(this.url, Ci.nsIWebNavigation.LOAD_FLAGS_BYPASS_HISTORY, null, null, null);
   },
 
   iframeListener: {
