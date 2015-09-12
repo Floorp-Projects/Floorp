128723240ded9c43267d20cb8b2cd3d893883cd3
Mark Finkle <mfinkle@mozilla.com>
2015-09-12 19:26:51 -0400

    Bug 1198009 - Add support to BrowserHealthRecorder for making a snapshot of active experiments in a new profile r=rnewman

--- mobile/android/base/health/BrowserHealthRecorder.java
+++ mobile/android/base/health/BrowserHealthRecorder.java
@@ -642,6 +642,23 @@ public class BrowserHealthRecorder extends BroadcastReceiver implements HealthRe
                 try {
                     JSONObject json = message.getJSONObject("json");
                     JSONObject addons = json.getJSONObject("addons");
+
+                    // Treat active experiments as add-ons
+                    if (AppConstants.MOZ_SWITCHBOARD) {
+                        List<String> experiments = SwitchBoard.getActiveExperiments(GeckoAppShell.getContext());
+                        for (String experiment : experiments) {
+                            // Create a fake add-on name
+                            String fakeName = experiment + "@experiments.mozilla.org";
+                            try {
+                                // Create a dummy JSON object for the experiment.
+                                JSONObject fakeAddon = new JSONObject();
+                                fakeAddon.put("type", "experiment");
+                                addons.put(fakeName, fakeAddon);
+                            } catch (JSONException je) {
+                            }
+                        }
+                    }
+
                     Log.i(LOG_TAG, "Persisting " + addons.length() + " add-ons.");
                     profileCache.setJSONForAddons(addons);
 
