ebbf6d7456216d0447c9f32119a5c7f976ffc901
Daosheng Mu <daoshengmu@gmail.com>
2015-09-13 22:49:00 +0200

    Bug 981183 - Part 1 - Gathering timestamps at the call watcher actor. r=vporof

--- browser/devtools/canvasdebugger/snapshotslist.js
+++ browser/devtools/canvasdebugger/snapshotslist.js
@@ -420,7 +420,7 @@ let SnapshotsListView = Heritage.extend(WidgetMethods, {
 
       // Prepare all the function calls for serialization.
       yield DevToolsUtils.yieldingEach(functionCalls, (call, i) => {
-        let { type, name, file, line, argsPreview, callerPreview } = call;
+        let { type, name, file, line, timestamp, argsPreview, callerPreview } = call;
         return call.getDetails().then(({ stack }) => {
           data.calls[i] = {
             type: type,
@@ -428,6 +428,7 @@ let SnapshotsListView = Heritage.extend(WidgetMethods, {
             file: file,
             line: line,
             stack: stack,
+            timestamp: timestamp,
             argsPreview: argsPreview,
             callerPreview: callerPreview
           };
