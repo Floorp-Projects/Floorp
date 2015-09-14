6e0c3f5071ef8836d9026ecd15c07a9eb9806584
Masayuki Nakano <masayuki@d-toybox.com>
2015-09-15 00:14:36 +0900

    Bug 895274 part.250 Rename NS_AUDIO_COMPLETE to eAudioComplete r=smaug

--- dom/events/EventNameList.h
+++ dom/events/EventNameList.h
@@ -935,7 +935,7 @@ NON_IDL_EVENT(audioprocess,
               eBasicEventClass)
 
 NON_IDL_EVENT(complete,
-              NS_AUDIO_COMPLETE,
+              eAudioComplete,
               EventNameType_None,
               eBasicEventClass)
 
