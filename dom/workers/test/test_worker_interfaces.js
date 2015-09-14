3a5297b95908d8f3cd3531b98c279fa1e95dcb0e
Ehsan Akhgari <ehsan@mozilla.com>
2015-09-14 14:22:35 -0400

    Bug 1204397 - Part 2: Recover from a double negative trap, r=me

--- dom/workers/test/test_worker_interfaces.js
+++ dom/workers/test/test_worker_interfaces.js
@@ -174,7 +174,7 @@ var interfaceNamesInGlobalScope =
 // IMPORTANT: Do not change this list without review from a DOM peer!
     "Response",
 // IMPORTANT: Do not change this list without review from a DOM peer!
-    { name: "ServiceWorkerRegistration", b2g: false, nightlyAndroid: false },
+    { name: "ServiceWorkerRegistration", b2g: false, nightlyAndroid: true },
 // IMPORTANT: Do not change this list without review from a DOM peer!
     "TextDecoder",
 // IMPORTANT: Do not change this list without review from a DOM peer!
