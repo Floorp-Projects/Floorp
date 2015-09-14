c90766327d0d15af8b7907307bab27939dc1f127
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-09-14 15:34:33 -0700

    Bug 1186716: Error if SPS NAL parsing failed. r=rillian
    --HG--
    extra : rebase_source : f06f2da854db0df3e0472ead49deaeab051501b4

--- media/libstagefright/binding/H264.cpp
+++ media/libstagefright/binding/H264.cpp
@@ -494,12 +494,16 @@ H264::DecodeSPSFromExtraData(const mozilla::MediaByteBuffer* aExtraData, SPSData
     return false;
   }
 
+  reader.DiscardRemaining();
+
   nsRefPtr<mozilla::MediaByteBuffer> rawNAL = new mozilla::MediaByteBuffer;
   rawNAL->AppendElements(ptr, length);
 
   nsRefPtr<mozilla::MediaByteBuffer> sps = DecodeNALUnit(rawNAL);
 
-  reader.DiscardRemaining();
+  if (!sps) {
+    return false;
+  }
 
   return DecodeSPS(sps, aDest);
 }
