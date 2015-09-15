5b335d8306e06b2033d25af1a6db4a75e4d665e7
Patrick McManus <mcmanus@ducksong.com>
2015-09-15 18:12:37 -0400

    bug 1187239 - ontransportstatus SENDING_TO should not use request stream re-entrantly r=hurley r=bz

--- netwerk/base/nsTransportUtils.h
+++ netwerk/base/nsTransportUtils.h
@@ -14,16 +14,13 @@
  * that for example if the status value is the same from event to event, and
  * the previous event has not yet been delivered, then only one event will
  * be delivered.  The progress reported will be that from the second event.
- * If aCoalesceAllEvents is true, then any undelivered event will be replaced
- * with the next event if it arrives early enough.  This option should be used
- * cautiously since it can cause states to be effectively skipped.  Coalescing
- * events can help prevent a backlog of unprocessed transport events in the
- * case that the target thread is overworked.
+
+ * Coalescing events can help prevent a backlog of unprocessed transport
+ * events in the case that the target thread is overworked.
  */
 nsresult
 net_NewTransportEventSinkProxy(nsITransportEventSink **aResult,
                                nsITransportEventSink *aSink,
-                               nsIEventTarget *aTarget,
-                               bool aCoalesceAllEvents = false);
+                               nsIEventTarget *aTarget);
 
 #endif // nsTransportUtils_h__
