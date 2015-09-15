7ae6cd363447f3468cb541d7335c347580d8d005
Robert Longson <longsonr@gmail.com>
2015-09-15 22:10:48 +0100

    Bug 1175492 - unpref transform-origin percentage handling for SVG elements r=jwatt

--- layout/style/nsStyleTransformMatrix.cpp
+++ layout/style/nsStyleTransformMatrix.cpp
@@ -50,11 +50,12 @@ TransformReferenceBox::EnsureDimensionsAreCached()
   mIsCached = true;
 
   if (mFrame->GetStateBits() & NS_FRAME_SVG_LAYOUT) {
-    if (!nsLayoutUtils::SVGTransformOriginEnabled()) {
+    if (!nsLayoutUtils::SVGTransformBoxEnabled()) {
       mX = -mFrame->GetPosition().x;
       mY = -mFrame->GetPosition().y;
-      mWidth = 0;
-      mHeight = 0;
+      Size contextSize = nsSVGUtils::GetContextSize(mFrame);
+      mWidth = nsPresContext::CSSPixelsToAppUnits(contextSize.width);
+      mHeight = nsPresContext::CSSPixelsToAppUnits(contextSize.height);
     } else
     if (mFrame->StyleDisplay()->mTransformBox ==
           NS_STYLE_TRANSFORM_BOX_FILL_BOX) {
@@ -80,7 +81,7 @@ TransformReferenceBox::EnsureDimensionsAreCached()
                    NS_STYLE_TRANSFORM_BOX_BORDER_BOX,
                  "Unexpected value for 'transform-box'");
       // Percentages in transforms resolve against the width/height of the
-      // nearest viewport (or it's viewBox if one is applied), and the
+      // nearest viewport (or its viewBox if one is applied), and the
       // transform is relative to {0,0} in current user space.
       mX = -mFrame->GetPosition().x;
       mY = -mFrame->GetPosition().y;
