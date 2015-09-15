69f69c1e201315d9ee1f2b9f27d30da2027cab85
Nicolas Silva <nsilva@mozilla.com>
2015-09-15 17:58:06 +0200

    Bug 1156182 - Prevent widgets from creating a LayerManager during shutdown. r=sotaro

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -6542,6 +6542,11 @@ nsWindow::GetLayerManager(PLayerTransactionChild* aShadowManager,
                           LayerManagerPersistence aPersistence,
                           bool* aAllowRetaining)
 {
+    if (mIsDestroyed) {
+      // Prevent external code from triggering the re-creation of the LayerManager/Compositor
+      // during shutdown. Just return what we currently have, which is most likely null.
+      return mLayerManager;
+    }
     if (!mLayerManager && eTransparencyTransparent == GetTransparencyMode()) {
         mLayerManager = CreateBasicLayerManager();
     }
