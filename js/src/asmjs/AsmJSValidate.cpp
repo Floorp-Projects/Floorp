e9a2905b738b2a1dc326eb9877fab8a12251861c
Benjamin Bouvier <benj@benj.me>
2015-09-15 19:21:06 +0200

    Bug 1204847: Reinterpret the asmFunc pointer as an AsmFunction in case of offthread compile error; r=luke

--- js/src/asmjs/AsmJSValidate.cpp
+++ js/src/asmjs/AsmJSValidate.cpp
@@ -11041,7 +11041,7 @@ CheckFunctions(ModuleValidator& m, ScopedJSDeletePtr<ModuleCompileResults>* resu
 
         // If failure was triggered by a helper thread, report error.
         if (void* maybeFunc = HelperThreadState().maybeAsmJSFailedFunction()) {
-            ModuleValidator::Func* func = reinterpret_cast<ModuleValidator::Func*>(maybeFunc);
+            AsmFunction* func = reinterpret_cast<AsmFunction*>(maybeFunc);
             return m.failOffset(func->srcBegin(), "allocation failure during compilation");
         }
 
