5e38d30d6faa7077254b2465265070d95eb1ddee
Robert O'Callahan <robert@ocallahan.org>
2015-09-16 16:35:16 +1200

    Bug 1189506. Create StreamTimeToGraphTime/GraphTimeToStreamTime that don't take account of blocking, and call them from AudioNodeStream. r=karlt
    --HG--
    extra : commitid : KZL20bqIcTe
    extra : rebase_source : 001476a097968e6fa382949ababbf6bdd5e4f76d

--- dom/media/webaudio/AudioNodeStream.cpp
+++ dom/media/webaudio/AudioNodeStream.cpp
@@ -654,8 +654,8 @@ AudioNodeStream::FractionalTicksFromDestinationTime(AudioNodeStream* aDestinatio
   double offset = destinationFractionalTicks - destinationStreamTime;
 
   GraphTime graphTime =
-    aDestination->StreamTimeToGraphTimeWithBlocking(destinationStreamTime);
-  StreamTime thisStreamTime = GraphTimeToStreamTimeOptimistic(graphTime);
+    aDestination->StreamTimeToGraphTime(destinationStreamTime);
+  StreamTime thisStreamTime = GraphTimeToStreamTime(graphTime);
   double thisFractionalTicks = thisStreamTime + offset;
   MOZ_ASSERT(thisFractionalTicks >= 0.0);
   return thisFractionalTicks;
@@ -680,8 +680,9 @@ AudioNodeStream::DestinationTimeFromTicks(AudioNodeStream* aDestination,
                                           StreamTime aPosition)
 {
   MOZ_ASSERT(SampleRate() == aDestination->SampleRate());
-  GraphTime graphTime = StreamTimeToGraphTimeWithBlocking(aPosition);
-  StreamTime destinationTime = aDestination->GraphTimeToStreamTimeOptimistic(graphTime);
+
+  GraphTime graphTime = StreamTimeToGraphTime(aPosition);
+  StreamTime destinationTime = aDestination->GraphTimeToStreamTime(graphTime);
   return StreamTimeToSeconds(destinationTime);
 }
 
