b7b5e288871e05106af4ab978883a1ce9e0ab100
Jordan Lund <jlund@mozilla.com>
2015-09-16 12:51:39 +1200

    Bug 1191859 - Make bundleclone extension available inside mock, r=nthomas, a=NPOTB

--- testing/mozharness/configs/single_locale/mozilla-aurora_android-api-11.py
+++ testing/mozharness/configs/single_locale/mozilla-aurora_android-api-11.py
@@ -105,5 +105,6 @@ config = {
                       ],
     "mock_files": [
         ("/home/cltbld/.ssh", "/home/mock_mozilla/.ssh"),
+        ('/usr/local/lib/hgext', '/usr/local/lib/hgext'),
     ],
 }
