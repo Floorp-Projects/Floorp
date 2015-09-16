4de38847f5945dbf5d1510e490db4d4cb283974f
Robert O'Callahan <robert@ocallahan.org>
2015-09-16 16:15:55 +1200

    Bug 1189506. Simplify blocking code now that stream blocking decision are always independent of other streams. r=karlt
    --HG--
    extra : commitid : 7du3gdoRlpL
    extra : rebase_source : ea22fe1b46a9a27bbab3fc250f688aa9dae53250

--- dom/media/MediaStreamGraphImpl.h
+++ dom/media/MediaStreamGraphImpl.h
@@ -304,14 +304,6 @@ public:
    */
   void RecomputeBlocking(GraphTime aEndBlockingDecisions);
 
-  // The following methods are used to help RecomputeBlocking.
-  /**
-   * If aStream isn't already in aStreams, add it and recursively call
-   * AddBlockingRelatedStreamsToSet on all the streams whose blocking
-   * status could depend on or affect the state of aStream.
-   */
-  void AddBlockingRelatedStreamsToSet(nsTArray<MediaStream*>* aStreams,
-                                      MediaStream* aStream);
   /**
    * Mark a stream blocked at time aTime. If this results in decisions that need
    * to be revisited at some point in the future, *aEnd will be reduced to the
@@ -324,7 +316,7 @@ public:
    * in the future, *aEnd will be reduced to the first time in the future to
    * recompute those decisions.
    */
-  void RecomputeBlockingAt(const nsTArray<MediaStream*>& aStreams,
+  void RecomputeBlockingAt(MediaStream* aStream,
                            GraphTime aTime, GraphTime aEndBlockingDecisions,
                            GraphTime* aEnd);
   /**
