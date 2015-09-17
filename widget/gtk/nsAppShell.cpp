d6e6629dc7efd9263b3fa5e1ffd85d8d852a5006
Lee Salzman <lsalzman@mozilla.com>
2015-09-17 15:01:51 -0400

    Bug 1205741 - add xpm and svg to supported gdk-pixbuf formats. r=acomminos,dveditz

--- widget/gtk/nsAppShell.cpp
+++ widget/gtk/nsAppShell.cpp
@@ -116,7 +116,9 @@ nsAppShell::Init()
             strcmp(name, "png") &&
             strcmp(name, "gif") &&
             strcmp(name, "bmp") &&
-            strcmp(name, "ico")) {
+            strcmp(name, "ico") &&
+            strcmp(name, "xpm") &&
+            strcmp(name, "svg")) {
           gdk_pixbuf_format_set_disabled(format, TRUE);
         }
         g_free(name);
