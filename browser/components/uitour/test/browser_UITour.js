78809d79fcda0d7a84ead4cd0ddf416de667b51f
Florian Quèze <florian@queze.net>
2015-09-18 10:42:59 +0200

    Bug 1202372 - remove the UITour dead code related to the old searchbar UI, r=MattN.

--- browser/components/uitour/test/browser_UITour.js
+++ browser/components/uitour/test/browser_UITour.js
@@ -216,56 +216,6 @@ var tests = [
     gContentAPI.showHighlight("urlbar");
     waitForElementToBeVisible(highlight, checkDefaultEffect, "Highlight should be shown after showHighlight()");
   },
-  function test_highlight_search_engine(done) {
-    let highlight = document.getElementById("UITourHighlight");
-    gContentAPI.showHighlight("urlbar");
-    waitForElementToBeVisible(highlight, () => {
-
-      let searchbar = document.getElementById("searchbar");
-      done();
-      return; // The oneoffui removes the menu that's being tested here.
-
-      gContentAPI.showMenu("searchEngines", function() {
-        isnot(searchbar, null, "Should have found searchbar");
-        let searchPopup = document.getAnonymousElementByAttribute(searchbar,
-                                                                   "anonid",
-                                                                   "searchbar-popup");
-        isnot(searchPopup, null, "Should have found search popup");
-
-        function getEngineNode(identifier) {
-          let engineNode = null;
-          for (let node of searchPopup.children) {
-            if (node.engine.identifier == identifier) {
-              engineNode = node;
-              break;
-            }
-          }
-          isnot(engineNode, null, "Should have found search engine node in popup");
-          return engineNode;
-        }
-        let googleEngineNode = getEngineNode("google");
-        let bingEngineNode = getEngineNode("bing");
-
-        gContentAPI.showHighlight("searchEngine-google");
-        waitForCondition(() => googleEngineNode.getAttribute("_moz-menuactive") == "true", function() {
-          is_element_hidden(highlight, "Highlight panel should be hidden by highlighting search engine");
-
-          gContentAPI.showHighlight("searchEngine-bing");
-          waitForCondition(() => bingEngineNode.getAttribute("_moz-menuactive") == "true", function() {
-            isnot(googleEngineNode.getAttribute("_moz-menuactive"), "true", "Previous engine should no longer be highlighted");
-
-            gContentAPI.hideHighlight();
-            waitForCondition(() => bingEngineNode.getAttribute("_moz-menuactive") != "true", function() {
-              gContentAPI.hideMenu("searchEngines");
-              waitForCondition(() => searchPopup.state == "closed", function() {
-                done();
-              }, "Search dropdown should close");
-            }, "Menu item should get attribute removed");
-          }, "Menu item should get attribute to make it look active");
-        });
-      });
-    });
-  },
   function test_highlight_effect_unsupported(done) {
     function checkUnsupportedEffect() {
       is(highlight.getAttribute("active"), "none", "No effect should be used when an unsupported effect is requested");
@@ -367,7 +317,7 @@ var tests = [
       });
     });
   },
-  function test_select_search_engine(done) {
+  function test_search(done) {
     Services.search.init(rv => {
       if (!Components.isSuccessCode(rv)) {
         ok(false, "search service init failed: " + rv);
@@ -375,9 +325,21 @@ var tests = [
         return;
       }
       let defaultEngine = Services.search.defaultEngine;
-      gContentAPI.getConfiguration("availableTargets", data => {
-        let searchEngines = data.targets.filter(t => t.startsWith("searchEngine-"));
-        let someOtherEngineID = searchEngines.filter(t => t != "searchEngine-" + defaultEngine.identifier)[0];
+      gContentAPI.getConfiguration("search", data => {
+        let visibleEngines = Services.search.getVisibleEngines();
+        let expectedEngines = ["searchEngine-" + engine.identifier
+                               for (engine of visibleEngines)
+                                 if (engine.identifier)];
+
+        let engines = data.engines;
+        ok(Array.isArray(engines), "data.engines should be an array");
+        is(engines.sort().toString(), expectedEngines.sort().toString(),
+           "Engines should be as expected");
+
+        is(data.searchEngineIdentifier, defaultEngine.identifier,
+           "the searchEngineIdentifier property should contain the defaultEngine's identifier");
+
+        let someOtherEngineID = data.engines.filter(t => t != "searchEngine-" + defaultEngine.identifier)[0];
         someOtherEngineID = someOtherEngineID.replace(/^searchEngine-/, "");
 
         let observe = function (subject, topic, verb) {
