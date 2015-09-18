8d6b6f5494a4e9d401db661c30ae3d0af8a4591f
Karl Tomlinson <karlt+@karlt.net>
2015-09-18 12:16:38 +1200

    bug 1210266 remove unused AudioParam::Node() r=padenot

--- dom/media/webaudio/AudioParam.h
+++ dom/media/webaudio/AudioParam.h
@@ -145,11 +145,6 @@ public:
     return mDefaultValue;
   }
 
-  AudioNode* Node() const
-  {
-    return mNode;
-  }
-
   const nsTArray<AudioNode::InputNode>& InputNodes() const
   {
     return mInputNodes;
