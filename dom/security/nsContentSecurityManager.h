6d3847c4873a27bc77975abe3cad78ef80e09d08
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2015-09-18 09:27:15 -0700

    Bug 1204703 - Make nsContentSecurityManager scriptable (r=sicking)
    --HG--
    extra : source : 977d5b7ecba32a0617d40c231e2f16963bf4a4ef

--- dom/security/nsContentSecurityManager.h
+++ dom/security/nsContentSecurityManager.h
@@ -7,18 +7,31 @@
 #ifndef nsContentSecurityManager_h___
 #define nsContentSecurityManager_h___
 
+#include "nsIContentSecurityManager.h"
 #include "nsIChannel.h"
+
 class nsIStreamListener;
 
-class nsContentSecurityManager
-{
-private:
-  nsContentSecurityManager() {}
-  virtual ~nsContentSecurityManager() {}
+#define NS_CONTENTSECURITYMANAGER_CONTRACTID "@mozilla.org/contentsecuritymanager;1"
+// cdcc1ab8-3cea-4e6c-a294-a651fa35227f
+#define NS_CONTENTSECURITYMANAGER_CID \
+{ 0xcdcc1ab8, 0x3cea, 0x4e6c, \
+  { 0xa2, 0x94, 0xa6, 0x51, 0xfa, 0x35, 0x22, 0x7f } }
 
+class nsContentSecurityManager : public nsIContentSecurityManager
+{
 public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSICONTENTSECURITYMANAGER
+
+  nsContentSecurityManager() {}
+
   static nsresult doContentSecurityCheck(nsIChannel* aChannel,
                                          nsCOMPtr<nsIStreamListener>& aInAndOutListener);
+
+private:
+  virtual ~nsContentSecurityManager() {}
+
 };
 
 #endif /* nsContentSecurityManager_h___ */
