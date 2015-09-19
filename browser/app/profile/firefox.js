5fbb3506f5db13a60c7096aa435839846b9567e6
Mark Banner <standard8@mozilla.com>
2015-09-19 14:22:56 +0100

    Bug 1205591 - Add a basic service channel implementation for listening to Loop's link clicker and opening rooms when requested. r=mikedeboer

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1693,6 +1693,7 @@ pref("image.mem.max_decoded_image_kb", 256000);
 pref("loop.enabled", true);
 pref("loop.textChat.enabled", true);
 pref("loop.server", "https://loop.services.mozilla.com/v0");
+pref("loop.linkClicker.url", "https://hello.firefox.com/");
 pref("loop.gettingStarted.seen", false);
 pref("loop.gettingStarted.url", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/hello/start/");
 pref("loop.gettingStarted.resumeOnFirstJoin", false);
