9fd579e930fb402f38521c4cd7ca275eaeb6ca77
JW Wang <jwwang@mozilla.com>
2015-09-21 13:49:01 +0800

    Bug 1204430. Part 2 - mirror MediaDecoder::mMediaSeekable. r=kinetik.

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -351,7 +351,6 @@ MediaDecoder::MediaDecoder() :
   mDormantSupported(false),
   mLogicalPosition(0.0),
   mDuration(std::numeric_limits<double>::quiet_NaN()),
-  mMediaSeekable(true),
   mReentrantMonitor("media.decoder"),
   mIgnoreProgressData(false),
   mInfiniteStream(false),
@@ -407,7 +406,9 @@ MediaDecoder::MediaDecoder() :
   mPlaybackRateReliable(AbstractThread::MainThread(), true,
                         "MediaDecoder::mPlaybackRateReliable (Canonical)"),
   mDecoderPosition(AbstractThread::MainThread(), 0,
-                   "MediaDecoder::mDecoderPosition (Canonical)")
+                   "MediaDecoder::mDecoderPosition (Canonical)"),
+  mMediaSeekable(AbstractThread::MainThread(), true,
+                 "MediaDecoder::mMediaSeekable (Canonical)")
 {
   MOZ_COUNT_CTOR(MediaDecoder);
   MOZ_ASSERT(NS_IsMainThread());
@@ -1124,8 +1125,8 @@ MediaDecoder::IsTransportSeekable()
 
 bool MediaDecoder::IsMediaSeekable()
 {
+  MOZ_ASSERT(NS_IsMainThread());
   NS_ENSURE_TRUE(GetStateMachine(), false);
-  ReentrantMonitorAutoEnter mon(GetReentrantMonitor());
   return mMediaSeekable;
 }
 
