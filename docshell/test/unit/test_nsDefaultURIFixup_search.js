ae1ee788c905cd9c79a8ee3953bb11192a5c5520
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2015-09-22 16:26:15 +0100

    Bug 1199289 - r=valentin,dao
    --HG--
    extra : commitid : Gt0ahICl0FY
    extra : rebase_source : 4c21bbdc64d32ad1ea7291252632d13d1d326296

--- docshell/test/unit/test_nsDefaultURIFixup_search.js
+++ docshell/test/unit/test_nsDefaultURIFixup_search.js
@@ -1,6 +1,7 @@
-var urifixup = Cc["@mozilla.org/docshell/urifixup;1"].
+let urifixup = Cc["@mozilla.org/docshell/urifixup;1"].
                getService(Ci.nsIURIFixup);
 Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://gre/modules/AppConstants.jsm");
 
 Services.prefs.setBoolPref("keyword.enabled", true);
 
@@ -9,10 +10,10 @@ const kSearchEngineURL = "http://www.example.org/?search={searchTerms}";
 Services.search.addEngineWithDetails(kSearchEngineID, "", "", "", "get",
                                      kSearchEngineURL);
 
-var oldDefaultEngine = Services.search.defaultEngine;
+let oldDefaultEngine = Services.search.defaultEngine;
 Services.search.defaultEngine = Services.search.getEngineByName(kSearchEngineID);
 
-var selectedName = Services.search.defaultEngine.name;
+let selectedName = Services.search.defaultEngine.name;
 do_check_eq(selectedName, kSearchEngineID);
 
 do_register_cleanup(function() {
@@ -26,7 +27,9 @@ do_register_cleanup(function() {
   Services.prefs.clearUserPref("keyword.enabled");
 });
 
-var data = [
+let isWin = AppConstants.platform == "win";
+
+let data = [
   {
     // Valid should not be changed.
     wrong: 'https://example.com/this/is/a/test.html',
@@ -37,14 +40,71 @@ var data = [
     wrong: 'whatever://this/is/a/test.html',
     fixed: kSearchEngineURL.replace("{searchTerms}", encodeURIComponent('whatever://this/is/a/test.html')),
   },
+
+  // The following tests check that when a user:password is present in the URL
+  // `user:` isn't treated as an unknown protocol thus leaking the user and
+  // password to the search engine.
+  {
+    wrong: 'user:pass@example.com/this/is/a/test.html',
+    fixed: 'http://user:pass@example.com/this/is/a/test.html',
+  },
+  {
+    wrong: 'user@example.com:8080/this/is/a/test.html',
+    fixed: 'http://user@example.com:8080/this/is/a/test.html',
+  },
+  {
+    wrong: 'https:pass@example.com/this/is/a/test.html',
+    fixed: 'https://pass@example.com/this/is/a/test.html',
+  },
+  {
+    wrong: 'user:pass@example.com:8080/this/is/a/test.html',
+    fixed: 'http://user:pass@example.com:8080/this/is/a/test.html',
+  },
+  {
+    wrong: 'http:user:pass@example.com:8080/this/is/a/test.html',
+    fixed: 'http://user:pass@example.com:8080/this/is/a/test.html',
+  },
+  {
+    wrong: 'ttp:user:pass@example.com:8080/this/is/a/test.html',
+    fixed: 'http://user:pass@example.com:8080/this/is/a/test.html',
+  },
+  {
+    wrong: 'gobbledygook:user:pass@example.com:8080/this/is/a/test.html',
+    fixed: 'http://gobbledygook:user%3Apass@example.com:8080/this/is/a/test.html',
+  },
+  {
+    wrong: 'user:@example.com:8080/this/is/a/test.html',
+    fixed: 'http://user:@example.com:8080/this/is/a/test.html',
+  },
+  {
+    wrong: '//user:pass@example.com:8080/this/is/a/test.html',
+    fixed: (isWin ? "http:" : "file://") + '//user:pass@example.com:8080/this/is/a/test.html',
+  },
+  {
+    wrong: '://user:pass@example.com:8080/this/is/a/test.html',
+    fixed: 'http://user:pass@example.com:8080/this/is/a/test.html',
+  },
+  {
+    wrong: 'whatever://this/is/a@b/test.html',
+    fixed: kSearchEngineURL.replace("{searchTerms}", encodeURIComponent('whatever://this/is/a@b/test.html')),
+  },
 ];
 
+let extProtocolSvc = Cc["@mozilla.org/uriloader/external-protocol-service;1"]
+                     .getService(Ci.nsIExternalProtocolService);
+
+if (extProtocolSvc && extProtocolSvc.externalProtocolHandlerExists("mailto")) {
+  data.push({
+    wrong: "mailto:foo@bar.com",
+    fixed: "mailto:foo@bar.com"
+  });
+}
 
 function run_test() {
   run_next_test();
 }
 
-var len = data.length;
+let len = data.length;
 // Make sure we fix what needs fixing
 add_task(function test_fix_unknown_schemes() {
   for (let i = 0; i < len; ++i) {
