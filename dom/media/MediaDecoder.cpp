9f603783094cf0950bef63102b5f16c45b12ea84
JW Wang <jwwang@mozilla.com>
2015-09-22 10:56:17 +0800

    Bug 1206574 - Remove AbstractMediaDecoder::IsShutdown(). r=cpearce.

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -1302,12 +1302,6 @@ MediaDecoder::NotifyWaitingForResourcesStatusChanged()
   }
 }
 
-bool MediaDecoder::IsShutdown() const {
-  MOZ_ASSERT(NS_IsMainThread());
-  NS_ENSURE_TRUE(GetStateMachine(), true);
-  return mStateMachineIsShutdown;
-}
-
 // Drop reference to state machine.  Only called during shutdown dance.
 void MediaDecoder::BreakCycles() {
   SetStateMachine(nullptr);
