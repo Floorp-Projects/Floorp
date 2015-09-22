b4869c445480b3cb210a30bd41c8f68db1476f7c
Ehsan Akhgari <ehsan@mozilla.com>
2015-09-22 17:45:25 -0400

    Bug 1205495 - Correctly use the requests's body and redirect mode in reroute.js. r=nsm, a=test-only

--- dom/tests/mochitest/fetch/reroute.js
+++ dom/tests/mochitest/fetch/reroute.js
@@ -5,13 +5,17 @@ onfetch = function(e) {
     var url = e.request.url.substring(0, e.request.url.indexOf('?'));
     url += '?headers=' + ({ 'Referer': self.location.href }).toSource();
 
-    e.respondWith(fetch(url, {
-      method: e.request.method,
-      headers: e.request.headers,
-      body: e.request.body,
-      mode: e.request.mode,
-      credentials: e.request.credentials,
-      cache: e.request.cache,
+    e.respondWith(e.request.text().then(function(text) {
+      var body = text === '' ? undefined : text;
+      return fetch(url, {
+        method: e.request.method,
+        headers: e.request.headers,
+        body: body,
+        mode: e.request.mode,
+        credentials: e.request.credentials,
+        redirect: e.request.redirect,
+        cache: e.request.cache,
+      });
     }));
     return;
   }
