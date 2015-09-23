0edf2b740447d00a5c8469f6fef27cbdfc67cbbf
Tooru Fujisawa <arai_a@mac.com>
2015-09-23 18:39:13 +0900

    Bug 1207494 - Part 3: Remove use of expression closure from dom/browser-element/. r=smaug
    --HG--
    extra : commitid : IX6Ep2xpsM2
    extra : rebase_source : fb3dcb439af7dc4a729f776c72ac0cbaa944cb9f

--- dom/browser-element/mochitest/browserElement_NextPaint.js
+++ dom/browser-element/mochitest/browserElement_NextPaint.js
@@ -29,7 +29,7 @@ function runTest() {
     });
 
     // Force the iframe to repaint.
-    SimpleTest.executeSoon(function () iframe.src += '#next');
+    SimpleTest.executeSoon(() => iframe.src += '#next');
   });
 
   // Remove the first listener to make sure it's not called.
