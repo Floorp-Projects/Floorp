b3965f1343a7b230ed29228a855233dc44e5068e
Sebastian Kaspari <s.kaspari@gmail.com>
2015-09-24 15:27:56 +0200

    Bug 1202052 - Bypass cache on reload button long press. r=margaret, a=lizzard
    --HG--
    extra : source : 631931fb26588ddfcca4a0932ebe6f48702a4699

--- mobile/android/base/menu/GeckoMenu.java
+++ mobile/android/base/menu/GeckoMenu.java
@@ -5,6 +5,7 @@
 package org.mozilla.gecko.menu;
 
 import org.mozilla.gecko.AppConstants;
+import org.mozilla.gecko.GeckoAppShell;
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.util.ThreadUtils;
 import org.mozilla.gecko.util.ThreadUtils.AssertBehavior;
@@ -256,8 +257,12 @@ public class GeckoMenu extends ListView
             });
             ((MenuItemActionBar) actionView).setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
-                public boolean onLongClick(View v) {
-                    return handleMenuItemLongClick(menuItem);
+                public boolean onLongClick(View view) {
+                    if (handleMenuItemLongClick(menuItem)) {
+                        GeckoAppShell.vibrateOnHapticFeedbackEnabled(getResources().getIntArray(R.array.long_press_vibrate_msec));
+                        return true;
+                    }
+                    return false;
                 }
             });
         } else if (actionView instanceof MenuItemActionView) {
@@ -270,7 +275,11 @@ public class GeckoMenu extends ListView
             ((MenuItemActionView) actionView).setMenuItemLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View view) {
-                    return handleMenuItemLongClick(menuItem);
+                    if (handleMenuItemLongClick(menuItem)) {
+                        GeckoAppShell.vibrateOnHapticFeedbackEnabled(getResources().getIntArray(R.array.long_press_vibrate_msec));
+                        return true;
+                    }
+                    return false;
                 }
             });
         }
@@ -644,12 +653,15 @@ public class GeckoMenu extends ListView
     }
 
     boolean handleMenuItemLongClick(GeckoMenuItem item) {
-        if(!item.isEnabled()) {
+        if (!item.isEnabled()) {
             return false;
         }
 
-        if(mCallback != null) {
-            return mCallback.onMenuItemLongClick(item);
+        if (mCallback != null) {
+            if (mCallback.onMenuItemLongClick(item)) {
+                close();
+                return true;
+            }
         }
         return false;
     }
