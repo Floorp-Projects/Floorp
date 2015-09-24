004c8e46cf72548717c838c7f94f942fe9bb3909
Mike Hommey <mh+mozilla@glandium.org>
2015-09-24 10:40:54 +0900

    Bug 1207882 - Add an initial partial implementation of a new, faster, build backend. r=gps

--- python/mozbuild/mozbuild/config_status.py
+++ python/mozbuild/mozbuild/config_status.py
@@ -96,7 +96,8 @@ def config_status(topobjdir='.', topsrcdir='.',
     parser.add_option('-d', '--diff', action='store_true',
                       help='print diffs of changed files.')
     parser.add_option('-b', '--backend',
-                      choices=['RecursiveMake', 'AndroidEclipse', 'CppEclipse', 'VisualStudio'],
+                      choices=['RecursiveMake', 'AndroidEclipse', 'CppEclipse',
+                               'VisualStudio', 'FasterMake'],
                       default='RecursiveMake',
                       help='what backend to build (default: RecursiveMake).')
     options, args = parser.parse_args()
@@ -128,6 +129,9 @@ def config_status(topobjdir='.', topsrcdir='.',
     elif options.backend == 'VisualStudio':
         from mozbuild.backend.visualstudio import VisualStudioBackend
         backend_cls = VisualStudioBackend
+    elif options.backend == 'FasterMake':
+        from mozbuild.backend.fastermake import FasterMakeBackend
+        backend_cls = FasterMakeBackend
 
     the_backend = backend_cls(env)
 
