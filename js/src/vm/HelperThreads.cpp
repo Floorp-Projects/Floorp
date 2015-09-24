4c9ad62f5f6f25ee6fc89f70c78a6d9df9b99f5b
Jeff Walden <jwalden@mit.edu>
2015-09-24 12:51:56 -0700

    Bug 1101561 - Fix generator bootstrapping (for legacy and star generators both) to be OOM-safe.  r=jandem

--- js/src/vm/HelperThreads.cpp
+++ js/src/vm/HelperThreads.cpp
@@ -929,7 +929,8 @@ GlobalHelperThreadState::finishParseTask(JSContext* maybecx, JSRuntime* rt, void
         !EnsureConstructor(cx, global, JSProto_Function) ||
         !EnsureConstructor(cx, global, JSProto_RegExp) ||
         !EnsureConstructor(cx, global, JSProto_Iterator) ||
-        !EnsureConstructor(cx, global, JSProto_GeneratorFunction))
+        !GlobalObject::initLegacyGeneratorProto(cx, global) ||
+        !GlobalObject::initStarGenerators(cx, global))
     {
         LeaveParseTaskZone(rt, parseTask);
         return nullptr;
@@ -965,6 +966,17 @@ GlobalHelperThreadState::finishParseTask(JSContext* maybecx, JSRuntime* rt, void
     return script;
 }
 
+JSObject*
+GlobalObject::transliterateStarGeneratorFunctionPrototype(JSObject* proto)
+{
+    JSObject* genFuncProto =
+        &proto->global().getReservedSlot(STAR_GENERATOR_FUNCTION_PROTO).toObject();
+    if (proto != genFuncProto)
+        return nullptr;
+
+    return &getReservedSlot(STAR_GENERATOR_FUNCTION_PROTO).toObject();
+}
+
 void
 GlobalHelperThreadState::mergeParseTaskCompartment(JSRuntime* rt, ParseTask* parseTask,
                                                    Handle<GlobalObject*> global,
@@ -992,22 +1004,24 @@ GlobalHelperThreadState::mergeParseTaskCompartment(JSRuntime* rt, ParseTask* par
         if (!proto.isObject())
             continue;
 
-        JSProtoKey key = JS::IdentifyStandardPrototype(proto.toObject());
-        if (key == JSProto_Null) {
-            // Generator functions don't have Function.prototype as prototype
-            // but a different function object, so IdentifyStandardPrototype
-            // doesn't work. Just special-case it here.
-            if (IsStandardPrototype(proto.toObject(), JSProto_GeneratorFunction))
-                key = JSProto_GeneratorFunction;
-            else
-                continue;
-        }
-        MOZ_ASSERT(key == JSProto_Object || key == JSProto_Array ||
-                   key == JSProto_Function || key == JSProto_RegExp ||
-                   key == JSProto_Iterator || key == JSProto_GeneratorFunction);
+        JSObject* protoObj = proto.toObject();
 
-        JSObject* newProto = GetBuiltinPrototypePure(global, key);
-        MOZ_ASSERT(newProto);
+        // Generator functions don't have Function.prototype as prototype but a
+        // different function object, so IdentifyStandardPrototype won't work.
+        // Just special-case it here.
+        JSObject* newProto = global->transliterateStarGeneratorFunctionPrototype(protoObj);
+        if (!newProto) {
+            JSProtoKey key = JS::IdentifyStandardPrototype(protoObj);
+            if (key == JSProto_Null)
+                continue;
+
+            MOZ_ASSERT(key == JSProto_Object || key == JSProto_Array ||
+                       key == JSProto_Function || key == JSProto_RegExp ||
+                       key == JSProto_Iterator);
+
+            newProto = GetBuiltinPrototypePure(global, key);
+            MOZ_ASSERT(newProto);
+        }
 
         group->setProtoUnchecked(TaggedProto(newProto));
     }
