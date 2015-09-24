f0563215ef319b15680eb7210d5c34291164ecd6
Nicholas Nethercote <nnethercote@mozilla.com>
2015-09-24 00:48:15 -0700

    Bug 1207944 (part 9) - Remove SetColor(const gfxRGBA&). r=jwatt.

--- gfx/thebes/gfxContext.h
+++ gfx/thebes/gfxContext.h
@@ -269,7 +269,6 @@ public:
      * If CMS is not enabled, the color is treated as a device-space color
      * and this call is identical to SetDeviceColor().
      */
-    void SetColor(const gfxRGBA& aColor);
     void SetColor(const mozilla::gfx::Color& aColor);
 
     /**
