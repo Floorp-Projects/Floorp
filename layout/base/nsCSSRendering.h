b33cbd6c594b27231c7fcc6838600ee6a2d0d9ae
Nicholas Nethercote <nnethercote@mozilla.com>
2015-09-24 00:43:32 -0700

    Bug 1207944 (part 7) - Use gfx::Color instead of gfxRGBA in BlurCache. r=jwatt.
    This avoids several gfxRBGA-to-Color conversions.
    
    --HG--
    extra : rebase_source : 9b60c0dd0169bd8b208639dd3a28cad4591179dd

--- layout/base/nsCSSRendering.h
+++ layout/base/nsCSSRendering.h
@@ -832,6 +832,7 @@ protected:
  * This is very useful for creating drop shadows or silhouettes.
  */
 class nsContextBoxBlur {
+  typedef mozilla::gfx::Color Color;
   typedef mozilla::gfx::RectCornerRadii RectCornerRadii;
 
 public:
@@ -941,7 +942,7 @@ public:
                             int32_t aAppUnitsPerDevPixel,
                             RectCornerRadii* aCornerRadii,
                             nscoord aBlurRadius,
-                            const gfxRGBA& aShadowColor,
+                            const Color& aShadowColor,
                             const nsRect& aDirtyRect,
                             const gfxRect& aSkipRect);
 
