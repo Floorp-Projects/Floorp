72698cae59399ed9238c7a866aff46fc5044fdd0
Junior Hsu <juhsu@mozilla.com>
2015-09-24 16:16:47 +0800

    Bug 1148307 - Part1, separate object bruilder from nsIPresentationSessionTransport, r=smaug

--- dom/presentation/tests/mochitest/PresentationSessionChromeScript.js
+++ dom/presentation/tests/mochitest/PresentationSessionChromeScript.js
@@ -198,6 +198,7 @@ const mockedDevicePrompt = {
 
 const mockedSessionTransport = {
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIPresentationSessionTransport,
+                                         Ci.nsIPresentationTCPSessionTransportBuilder,
                                          Ci.nsIFactory]),
   createInstance: function(aOuter, aIID) {
     if (aOuter) {
@@ -214,13 +215,19 @@ const mockedSessionTransport = {
   get selfAddress() {
     return this._selfAddress;
   },
-  initWithSocketTransport: function(transport, callback) {
+  buildTCPSenderTransport: function(transport, listener) {
     sendAsyncMessage('data-transport-initialized');
-    this._callback = callback;
+    this._listener = listener;
+    this._type = Ci.nsIPresentationSessionTransportBuilder.TYPE_SENDER;
+
+    this._listener.onSessionTransport(this);
+    this._listener = null;
+
     this.simulateTransportReady();
   },
-  initWithChannelDescription: function(description, callback) {
-    this._callback = callback;
+  buildTCPReceiverTransport: function(description, listener) {
+    this._listener = listener;
+    this._type = Ci.nsIPresentationSessionTransportBuilder.TYPE_RECEIVER;
 
     var addresses = description.QueryInterface(Ci.nsIPresentationChannelDescription).tcpAddress;
     this._selfAddress = {
@@ -229,6 +236,9 @@ const mockedSessionTransport = {
                 addresses.queryElementAt(0, Ci.nsISupportsCString).data : "",
       port: description.QueryInterface(Ci.nsIPresentationChannelDescription).tcpPort,
     };
+
+    this._listener.onSessionTransport(this);
+    this._listener = null;
   },
   enableDataNotification: function() {
     sendAsyncMessage('data-transport-notification-enabled');
@@ -302,7 +312,7 @@ originalFactoryData.push(registerMockedFactory("@mozilla.org/presentation-device
 originalFactoryData.push(registerMockedFactory("@mozilla.org/network/server-socket;1",
                                                uuidGenerator.generateUUID(),
                                                mockedServerSocket));
-originalFactoryData.push(registerMockedFactory("@mozilla.org/presentation/presentationsessiontransport;1",
+originalFactoryData.push(registerMockedFactory("@mozilla.org/presentation/presentationtcpsessiontransport;1",
                                                uuidGenerator.generateUUID(),
                                                mockedSessionTransport));
 originalFactoryData.push(registerMockedFactory("@mozilla.org/network/manager;1",
