2b48f45c4716357abd4fdd3a8bad2955247bf7be
Bill McCloskey <billm@mozilla.com>
2015-09-24 13:00:53 -0700

    Bug 967873 - Start TabView when starting tests that might lazily load it (r=Gijs)

--- browser/base/content/test/general/browser_bug585558.js
+++ browser/base/content/test/general/browser_bug585558.js
@@ -14,6 +14,13 @@ function testAttrib(elem, attrib, attribValue, msg) {
 
 function test() {
   waitForExplicitFinish();
+
+  // Ensure TabView has been initialized already. Otherwise it could
+  // activate at an unexpected time and show/hide tabs.
+  TabView._initFrame(runTest);
+}
+
+function runTest() {
   is(gBrowser.tabs.length, 1, "one tab is open initially");
 
   // Add several new tabs in sequence, hiding some, to ensure that the
