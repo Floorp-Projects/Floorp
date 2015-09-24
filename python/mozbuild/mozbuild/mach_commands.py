bc77e84c025d22321ee963d1987677e573ff1036
Mike Hommey <mh+mozilla@glandium.org>
2015-09-24 10:40:54 +0900

    Bug 1207882 - Add an initial partial implementation of a new, faster, build backend. r=gps

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -559,7 +559,8 @@ class Build(MachCommandBase):
     # It would be nice to filter the choices below based on
     # conditions, but that is for another day.
     @CommandArgument('-b', '--backend',
-        choices=['RecursiveMake', 'AndroidEclipse', 'CppEclipse', 'VisualStudio'],
+        choices=['RecursiveMake', 'AndroidEclipse', 'CppEclipse',
+                 'VisualStudio', 'FasterMake'],
         default='RecursiveMake',
         help='Which backend to build (default: RecursiveMake).')
     def build_backend(self, backend='RecursiveMake', diff=False):
