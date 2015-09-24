baf69f08f6f70c27d32d77519f896fa6cfa5f240
Jared Wein <jwein@mozilla.com>
2015-09-24 14:18:00 +0200

    Bug 1208000 - Default browser dialog appears only 2 times. r=Gijs, a=lizzard

--- browser/components/shell/nsGNOMEShellService.cpp
+++ browser/components/shell/nsGNOMEShellService.cpp
@@ -352,7 +352,7 @@ nsGNOMEShellService::GetShouldSkipCheckDefaultBrowser(bool* aResult)
   if (NS_FAILED(rv)) {
     return rv;
   }
-  if (defaultBrowserCheckCount < 3) {
+  if (defaultBrowserCheckCount < 4) {
     *aResult = false;
     return prefs->SetIntPref(PREF_DEFAULTBROWSERCHECKCOUNT,
                              defaultBrowserCheckCount + 1);
