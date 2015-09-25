fa37b2f87bed2faa8d09c49c03817802a9808656
Alessio Placitelli <alessio.placitelli@gmail.com>
2015-09-25 06:02:00 +0200

    Bug 1208376 - Disable v2 data upload in Firefox 42+. r=gfritzsche, a=sledru

--- services/datareporting/datareporting-prefs.js
+++ services/datareporting/datareporting-prefs.js
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 pref("datareporting.policy.dataSubmissionEnabled", true);
+pref("datareporting.policy.dataSubmissionEnabled.v2", true);
 pref("datareporting.policy.firstRunTime", "0");
 pref("datareporting.policy.dataSubmissionPolicyNotifiedTime", "0");
 pref("datareporting.policy.dataSubmissionPolicyAcceptedVersion", 0);
