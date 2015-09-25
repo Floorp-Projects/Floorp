e9dd5ecb6919c3402132d6f76ef232e8fd85cb20
Chris Pearce <cpearce@mozilla.com>
2015-09-25 23:17:50 +1200

    Bug 1208349 - Ensure we query PDM whether it supports MP4 before assuming it supports MP4. r=jya

--- dom/media/fmp4/MP4Decoder.cpp
+++ dom/media/fmp4/MP4Decoder.cpp
@@ -275,7 +275,7 @@ CreateTestH264Decoder(layers::LayersBackend aBackend,
   PlatformDecoderModule::Init();
 
   nsRefPtr<PlatformDecoderModule> platform = PlatformDecoderModule::Create();
-  if (!platform) {
+  if (!platform || !platform->SupportsMimeType(NS_LITERAL_CSTRING("video/mp4"))) {
     return nullptr;
   }
 
@@ -331,7 +331,7 @@ CreateTestAACDecoder(AudioInfo& aConfig)
   PlatformDecoderModule::Init();
 
   nsRefPtr<PlatformDecoderModule> platform = PlatformDecoderModule::Create();
-  if (!platform) {
+  if (!platform || !platform->SupportsMimeType(NS_LITERAL_CSTRING("audio/mp4a-latm"))) {
     return nullptr;
   }
 
