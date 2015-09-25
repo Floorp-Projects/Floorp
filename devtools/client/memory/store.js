311b1128a296a624cb772fe4a27c59dcbe2fe908
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-09-25 11:03:26 +0200

    Backed out 1 changesets (bug 1201949) for bc7 test failures
    Backed out changeset f03e855428e7 (bug 1201949)

--- devtools/client/memory/store.js
+++
@@ -1,8 +0,0 @@
-const { combineReducers } = require("../shared/vendor/redux");
-const createStore = require("../shared/redux/create-store");
-const reducers = require("./reducers");
-const DevToolsUtils = require("devtools/shared/DevToolsUtils");
-
-module.exports = function () {
-  return createStore({ log: DevToolsUtils.testing })(combineReducers(reducers), {});
-};
