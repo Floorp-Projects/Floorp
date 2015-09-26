7ac029a2231903f685771ef8bee508a4295bd659
Luca Greco <luca.greco@alcacoop.it>
2015-09-26 09:24:00 +0200

    Bug 1204583 - preprocess and validate optional WebExtension runtime.connect params. r=billm,gabor

--- toolkit/components/extensions/ext-runtime.js
+++ toolkit/components/extensions/ext-runtime.js
@@ -6,6 +6,27 @@ var {
   ignoreEvent,
 } = ExtensionUtils;
 
+function processRuntimeConnectParams(...args) {
+  let extensionId, connectInfo;
+
+  // connect("...") and connect("...", { ... })
+  if (typeof args[0] == "string") {
+    extensionId = args.shift();
+  }
+
+  // connect({ ... }) and connect("...", { ... })
+  if (!!args[0] && typeof args[0] == "object") {
+    connectInfo = args.shift();
+  }
+
+  // raise errors on unexpected connect params (but connect() is ok)
+  if (args.length > 0) {
+    throw Error("invalid arguments to runtime.connect");
+  }
+
+  return { extensionId, connectInfo };
+}
+
 extensions.registerAPI((extension, context) => {
   return {
     runtime: {
@@ -22,11 +43,9 @@ extensions.registerAPI((extension, context) => {
 
       onConnect: context.messenger.onConnect("runtime.onConnect"),
 
-      connect: function(extensionId, connectInfo) {
-        if (!connectInfo) {
-          connectInfo = extensionId;
-          extensionId = null;
-        }
+      connect: function(...args) {
+        let { extensionId, connectInfo } = processRuntimeConnectParams(...args);
+
         let name = connectInfo && connectInfo.name || "";
         let recipient = extensionId ? {extensionId} : {extensionId: extension.id};
 
