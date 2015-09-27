0d2d5651a350bd0e4a3d26bc640ed20dfadb2662
Steve Singer <steve@ssinger.info>
2015-09-27 12:17:00 +0200

    Bug 1208867 - Add xxx64 operations to MacroAssembler-none. r=sstangl, a=lizzard

--- js/src/jit/none/Lowering-none.h
+++ js/src/jit/none/Lowering-none.h
@@ -87,6 +87,7 @@ class LIRGeneratorNone : public LIRGeneratorShared
     void visitSimdValueX4(MSimdValueX4* lir) { MOZ_CRASH(); }
     void visitSubstr(MSubstr*) { MOZ_CRASH(); }
     void visitSimdBinaryArith(js::jit::MSimdBinaryArith*) { MOZ_CRASH(); }
+    void visitRandom(js::jit::MRandom*) { MOZ_CRASH(); }
 
 };
 
