68cafe83b435a70b971ee4a785d6e07b20bc8ac0
Brian Birtles <birtles@gmail.com>
2015-09-28 12:38:41 +0900

    Bug 1195180 part 7 - Store animations in an array; r=heycam
    Currently AnimationTimeline stores animations in a hashmap which means that
    when we go to iterate over those animations to tick them we will visit them
    in an order that is non-deterministic.
    
    Although many of the observable effects of ticking an animation (e.g. CSS
    animation/transition events, mutation observer events) are later sorted so that
    the result does not depend on the order in which animations are ticked, this is
    not true for in all cases. In particular, the order in which Animation.finished
    promises are resolved will vary depending on the order in which animations are
    ticked. Likewise, for Animation finish events.
    
    Furthermore, it seems generally desirable to have a deterministic order for
    visiting animations in order to aid reproducing bugs.
    
    To achieve this, this patch switches the storage of animations in
    AnimationTimeline to use an array instead. However, when adding animations
    we need to determine if the animation to add already exists. To this end we
    also maintain a hashmap of the animations so we can quickly determine if
    the animation to add is a duplicate or not.

--- dom/animation/DocumentTimeline.cpp
+++ dom/animation/DocumentTimeline.cpp
@@ -111,19 +111,20 @@ DocumentTimeline::WillRefresh(mozilla::TimeStamp aTime)
   MOZ_ASSERT(mIsObservingRefreshDriver);
 
   bool needsTicks = false;
+  AnimationArray animationsToKeep(mAnimationOrder.Length());
 
-  for (auto iter = mAnimations.Iter(); !iter.Done(); iter.Next()) {
-    Animation* animation = iter.Get()->GetKey();
-
-    // Drop any animations which no longer need to be tracked by this timeline.
+  for (Animation* animation : mAnimationOrder) {
     if (animation->GetTimeline() != this ||
         (!animation->IsRelevant() && !animation->NeedsTicks())) {
-      iter.Remove();
+      mAnimations.RemoveEntry(animation);
       continue;
     }
 
     needsTicks |= animation->NeedsTicks();
+    animationsToKeep.AppendElement(animation);
   }
+
+  mAnimationOrder.SwapElements(animationsToKeep);
 
   if (!needsTicks) {
     // If another refresh driver observer destroys the nsPresContext,
@@ -142,7 +143,7 @@ DocumentTimeline::NotifyRefreshDriverCreated(nsRefreshDriver* aDriver)
              "Timeline should not be observing the refresh driver before"
              " it is created");
 
-  if (mAnimations.Count()) {
+  if (!mAnimationOrder.IsEmpty()) {
     aDriver->AddRefreshObserver(this, Flush_Style);
     mIsObservingRefreshDriver = true;
   }
