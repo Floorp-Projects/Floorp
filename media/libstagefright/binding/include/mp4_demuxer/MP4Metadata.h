a56452403df49c3bb33cfa109b826e26bd43de0a
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-09-28 12:32:51 +1000

    Bug 1207441 - [mp4] Ignore invalid tracks. r=gerald, a=sylvestre
    This is designed to minimize the chance of regressions.
    
    --HG--
    extra : source : ac303706f7148c8cfd830d9bf3c4e7064641e00f

--- media/libstagefright/binding/include/mp4_demuxer/MP4Metadata.h
+++ media/libstagefright/binding/include/mp4_demuxer/MP4Metadata.h
@@ -46,6 +46,9 @@ public:
 private:
   int32_t GetTrackNumber(mozilla::TrackID aTrackID);
   void UpdateCrypto(const stagefright::MetaData* aMetaData);
+  mozilla::UniquePtr<mozilla::TrackInfo> CheckTrack(const char* aMimeType,
+                                                    stagefright::MetaData* aMetaData,
+                                                    int32_t aIndex) const;
   nsAutoPtr<StageFrightPrivate> mPrivate;
   CryptoFile mCrypto;
   nsRefPtr<Stream> mSource;
