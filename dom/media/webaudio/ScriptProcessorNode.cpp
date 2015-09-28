70c6a938426daf275d039108da5c88186ddbbd44
Karl Tomlinson <karlt+@karlt.net>
2015-09-29 10:46:43 +1300

    bug 1209286 remove now unnecessary StreamTimeToDOMTime and DOMTimeToStreamTime r=padenot

--- dom/media/webaudio/ScriptProcessorNode.cpp
+++ dom/media/webaudio/ScriptProcessorNode.cpp
@@ -448,9 +448,7 @@ private:
         // them.  Otherwise, we may be able to get away without creating them!
         nsRefPtr<AudioProcessingEvent> event =
           new AudioProcessingEvent(aNode, nullptr, nullptr);
-        event->InitEvent(inputBuffer,
-                         inputChannelCount,
-                         context->StreamTimeToDOMTime(mPlaybackTime));
+        event->InitEvent(inputBuffer, inputChannelCount, mPlaybackTime);
         aNode->DispatchTrustedEvent(event);
 
         // Steal the output buffers if they have been set.
