e1e3cb3ebb90389ba271d778aa3880af12e91753
Jan Odvarko <odvarko@gmail.com>
2015-09-28 13:33:20 +0200

    Bug 1132203 - JSON Viewer for application/json documents; r=jlong
    --HG--
    extra : rebase_source : 0d9bd8b4f731d0a5219efac7db998bcaab69bea2

---
+++ devtools/client/jsonview/viewer-config.js
@@ -0,0 +1,31 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * RequireJS configuration for JSON Viewer.
+ *
+ * ReactJS library is shared among DevTools. Both, the minified (production)
+ * version and developer versions of the library are available.
+ *
+ * In order to use the developer version you need to specify the following
+ * in your .mozconfig (see also bug 1181646):
+ * ac_add_options --enable-debug-js-modules
+ *
+ * The path mapping uses paths fallback (a feature supported by RequireJS)
+ * See also: http://requirejs.org/docs/api.html#pathsfallbacks
+ */
+require.config({
+  baseUrl: ".",
+  paths: {
+    "react": [
+      "resource:///modules/devtools/client/shared/vendor/react-dev",
+      "resource:///modules/devtools/client/shared/vendor/react"
+    ]
+  }
+});
+
+// Load the main panel module
+requirejs(["json-viewer"]);
