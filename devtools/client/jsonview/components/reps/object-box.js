dd664dced52be19fbf52f77ccd4d5c28c25fa842
Jan Odvarko <odvarko@gmail.com>
2015-09-28 13:33:20 +0200

    Bug 1132203 - JSON Viewer for application/json documents; r=jlong

---
+++ devtools/client/jsonview/components/reps/object-box.js
@@ -0,0 +1,35 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+define(function(require, exports, module) {
+
+// Dependencies
+const React = require("react");
+const DOM = React.DOM;
+
+/**
+ * Renders a box for given object.
+ */
+const ObjectBox = React.createClass({
+  displayName: "ObjectBox",
+
+  render: function() {
+    var className = this.props.className;
+    var boxClassName = className ? " objectBox-" + className : "";
+
+    return (
+      DOM.span({className: "objectBox" + boxClassName, role: "presentation"},
+        this.props.children
+      )
+    )
+  }
+});
+
+// Exports from this module
+exports.ObjectBox = ObjectBox;
+});
