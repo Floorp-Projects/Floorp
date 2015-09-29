25e638e3aeec20a8645e520c91af28168a21f8c0
Mike Conley <mconley@mozilla.com>
2015-09-29 16:44:50 -0400

    Bug 1110511 - Move tab-crashing test helper function to BrowserTestUtils.jsm r=felipe

--- toolkit/content/tests/browser/browser_content_url_annotation.js
+++ toolkit/content/tests/browser/browser_content_url_annotation.js
@@ -10,72 +10,6 @@
 requestLongerTimeout(2);
 
 /**
- * Returns a Promise that resolves once a remote <xul:browser> has experienced
- * a crash. Resolves with the data from the .extra file (the crash annotations).
- *
- * @param browser
- *        The <xul:browser> that will crash
- * @return Promise
- */
-function crashBrowser(browser) {
-  let kv = {};
-  Cu.import("resource://gre/modules/KeyValueParser.jsm", kv);
-  // This frame script is injected into the remote browser, and used to
-  // intentionally crash the tab. We crash by using js-ctypes and dereferencing
-  // a bad pointer. The crash should happen immediately upon loading this
-  // frame script.
-  let frame_script = () => {
-    const Cu = Components.utils;
-    Cu.import("resource://gre/modules/ctypes.jsm");
-
-    let dies = function() {
-      privateNoteIntentionalCrash();
-      let zero = new ctypes.intptr_t(8);
-      let badptr = ctypes.cast(zero, ctypes.PointerType(ctypes.int32_t));
-      let crash = badptr.contents;
-    };
-
-    dump("Et tu, Brute?");
-    dies();
-  };
-
-  function checkSubject(subject, data) {
-    return subject instanceof Ci.nsIPropertyBag2 &&
-      subject.hasKey("abnormal");
-  };
-  let crashPromise = TestUtils.topicObserved('ipc:content-shutdown',
-                                             checkSubject);
-  let crashDataPromise = crashPromise.then(([subject, data]) => {
-    ok(subject instanceof Ci.nsIPropertyBag2);
-
-    let dumpID;
-    if ('nsICrashReporter' in Ci) {
-      dumpID = subject.getPropertyAsAString('dumpID');
-      ok(dumpID, "dumpID is present and not an empty string");
-    }
-
-    let extra = null;
-    if (dumpID) {
-      let minidumpDirectory = getMinidumpDirectory();
-      let extrafile = minidumpDirectory.clone();
-      extrafile.append(dumpID + '.extra');
-      ok(extrafile.exists(), 'found .extra file');
-      extra = kv.parseKeyValuePairsFromFile(extrafile);
-      removeFile(minidumpDirectory, dumpID + '.dmp');
-      removeFile(minidumpDirectory, dumpID + '.extra');
-    }
-
-    return extra;
-  });
-
-  // This frame script will crash the remote browser as soon as it is
-  // evaluated.
-  let mm = browser.messageManager;
-  mm.loadFrameScript("data:,(" + frame_script.toString() + ")();", false);
-  return crashDataPromise;
-}
-
-/**
  * Removes a file from a directory. This is a no-op if the file does not
  * exist.
  *
@@ -130,7 +64,7 @@ add_task(function* test_content_url_annotation() {
     yield promise;
 
     // Crash the tab
-    let annotations = yield crashBrowser(browser);
+    let annotations = yield BrowserTestUtils.crashBrowser(browser);
 
     ok("URL" in annotations, "annotated a URL");
     is(annotations.URL, redirect_url,
