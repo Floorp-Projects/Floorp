4f16a71a6747cf7c958be39cef3e959c734f0d08
Maja Frydrychowicz <mjzffr@gmail.com>
2015-09-29 07:48:47 -0400

    Bug 1197224 - Part 2: configure mozharness script to run media tests on Windows, r=jgriffin, DONTBUILD because NPOTB

--- testing/mozharness/configs/mediatests/buildbot_posix_config.py
+++ testing/mozharness/configs/mediatests/buildbot_posix_config.py
@@ -41,9 +41,9 @@ config = {
 
     "firefox_media_repo": 'https://github.com/mjzffr/firefox-media-tests.git',
     "firefox_media_branch": 'master',
-    "firefox_media_rev": '82c45fba24457b5fe447e967bbcaaec5eb14e3ee',
+    "firefox_media_rev": 'b11d6c3d7f6af166be314d2ac6673e78c1edb566',
     "firefox_ui_repo": 'https://github.com/mozilla/firefox-ui-tests.git',
-    "firefox_ui_branch": 'master',
+    "firefox_ui_branch": 'mozilla-central',
     "firefox_ui_rev": '6d6d57917f85399e903ac69b7e4297091b2d474c',
 
 }
