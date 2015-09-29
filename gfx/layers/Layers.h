a0c3fca880d05960f153380acb0b0051982c6879
Morris Tseng <mtseng@mozilla.com>
2015-09-29 11:51:25 +0100

    Bug 709490 - Part 7: If layer is not available, fallback to BasicCanvasLayer, r=roc

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -2269,15 +2269,17 @@ public:
     Data()
       : mBufferProvider(nullptr)
       , mGLContext(nullptr)
+      , mRenderer(nullptr)
       , mFrontbufferGLTex(0)
       , mSize(0,0)
       , mHasAlpha(false)
       , mIsGLAlphaPremult(true)
     { }
 
-    // One of these two must be specified for Canvas2D, but never both
+    // One of these three must be specified for Canvas2D, but never more than one
     PersistentBufferProvider* mBufferProvider; // A BufferProvider for the Canvas contents
     mozilla::gl::GLContext* mGLContext; // or this, for GL.
+    AsyncCanvasRenderer* mRenderer; // or this, for OffscreenCanvas
 
     // Frontbuffer override
     uint32_t mFrontbufferGLTex;
@@ -2397,8 +2399,6 @@ public:
   {
     return !!mAsyncRenderer;
   }
-
-  void SetAsyncRenderer(AsyncCanvasRenderer *aAsyncRenderer);
 
 protected:
   CanvasLayer(LayerManager* aManager, void* aImplData);
