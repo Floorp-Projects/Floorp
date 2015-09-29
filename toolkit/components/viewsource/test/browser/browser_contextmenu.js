60ff237bdbb22f556f3164edf98778352f9c8cb2
J. Ryan Stinnett <jryans@gmail.com>
2015-09-29 14:12:54 -0500

    Bug 1203395 - Repair view selection source in window mode. r=mconley, a=lizzard

--- toolkit/components/viewsource/test/browser/browser_contextmenu.js
+++ toolkit/components/viewsource/test/browser/browser_contextmenu.js
@@ -8,6 +8,7 @@ var gViewSourceWindow, gContextMenu, gCopyLinkMenuItem, gCopyEmailMenuItem;
 var expectedData = [];
 
 add_task(function *() {
+  // Full source in view source window
   let newWindow = yield loadViewSourceWindow(source);
   yield SimpleTest.promiseFocus(newWindow);
 
@@ -23,6 +24,7 @@ add_task(function *() {
     closeViewSourceWindow(newWindow, resolve);
   });
 
+  // Selection source in view source tab
   expectedData = [];
   let newTab = yield openDocumentSelect(source, "body");
   yield* onViewSourceWindowOpen(window, true);
@@ -36,6 +38,25 @@ add_task(function *() {
   }
 
   gBrowser.removeTab(newTab);
+
+  // Selection source in view source window
+  yield pushPrefs(["view_source.tab", false]);
+
+  expectedData = [];
+  newWindow = yield openDocumentSelect(source, "body");
+  yield SimpleTest.promiseFocus(newWindow);
+
+  yield* onViewSourceWindowOpen(newWindow, false);
+
+  contextMenu = newWindow.document.getElementById("viewSourceContextMenu");
+
+  for (let test of expectedData) {
+    yield* checkMenuItems(contextMenu, false, test[0], test[1], test[2], test[3]);
+  }
+
+  yield new Promise(resolve => {
+    closeViewSourceWindow(newWindow, resolve);
+  });
 });
 
 function* onViewSourceWindowOpen(aWindow, aIsTab) {
