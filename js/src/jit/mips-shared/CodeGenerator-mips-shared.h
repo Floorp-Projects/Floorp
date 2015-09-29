fa159e8c05de2117875c6f32b631939f64afb44f
Heiher <r@hev.cc>
2015-09-30 07:03:30 +0800

    Bug 1205566 - IonMonkey: MIPS: bailoutCmp32/Ptr optimization. r=nbp
    ---
     js/src/jit/mips-shared/CodeGenerator-mips-shared.h | 14 ++++++--------
     1 file changed, 6 insertions(+), 8 deletions(-)

--- js/src/jit/mips-shared/CodeGenerator-mips-shared.h
+++ js/src/jit/mips-shared/CodeGenerator-mips-shared.h
@@ -33,10 +33,9 @@ class CodeGeneratorMIPSShared : public CodeGeneratorShared
 
     template <typename T1, typename T2>
     void bailoutCmp32(Assembler::Condition c, T1 lhs, T2 rhs, LSnapshot* snapshot) {
-        Label skip;
-        masm.branch32(Assembler::InvertCondition(c), lhs, rhs, &skip);
-        bailout(snapshot);
-        masm.bind(&skip);
+        Label bail;
+        masm.branch32(c, lhs, rhs, &bail);
+        bailoutFrom(&bail, snapshot);
     }
     template<typename T>
     void bailoutCmp32(Assembler::Condition c, Operand lhs, T rhs, LSnapshot* snapshot) {
@@ -55,10 +54,9 @@ class CodeGeneratorMIPSShared : public CodeGeneratorShared
     }
     template <typename T1, typename T2>
     void bailoutCmpPtr(Assembler::Condition c, T1 lhs, T2 rhs, LSnapshot* snapshot) {
-        Label skip;
-        masm.branchPtr(Assembler::InvertCondition(c), lhs, rhs, &skip);
-        bailout(snapshot);
-        masm.bind(&skip);
+        Label bail;
+        masm.branchPtr(c, lhs, rhs, &bail);
+        bailoutFrom(&bail, snapshot);
     }
     void bailoutTestPtr(Assembler::Condition c, Register lhs, Register rhs, LSnapshot* snapshot) {
         Label bail;
