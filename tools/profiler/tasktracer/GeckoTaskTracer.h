5c7e6f4c0aa39a3ed4b9347254fd48c507f0fc8e
Phil Ringnalda <philringnalda@gmail.com>
2015-09-28 19:05:02 -0700

    Back out 6 changesets (bug 1186745) for cpptest failure in runnable_utils_unittest, nrappkit_unittest, test_nr_socket_unittest
    Backed out changeset c6142b815de0 (bug 1186745)
    Backed out changeset d8f740ef2430 (bug 1186745)
    Backed out changeset edc0b56d81fa (bug 1186745)
    Backed out changeset 383f8ac033ea (bug 1186745)
    Backed out changeset ce960a661987 (bug 1186745)
    Backed out changeset 3e9783023fb2 (bug 1186745)

--- tools/profiler/tasktracer/GeckoTaskTracer.h
+++ tools/profiler/tasktracer/GeckoTaskTracer.h
@@ -75,8 +75,7 @@ const PRTime GetStartTime();
 
 Task* CreateTracedTask(Task* aTask);
 
-already_AddRefed<nsIRunnable>
-CreateTracedRunnable(already_AddRefed<nsIRunnable>&& aRunnable);
+already_AddRefed<nsIRunnable> CreateTracedRunnable(nsIRunnable* aRunnable);
 
 // Free the TraceInfo allocated on a thread's TLS. Currently we are wrapping
 // tasks running on nsThreads and base::thread, so FreeTraceInfo is called at
