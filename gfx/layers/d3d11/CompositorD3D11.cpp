95fa9806aeb7385ee3b7959408ef24df424eafc1
David Anderson <dvander@alliedmods.net>
2015-09-29 11:20:01 -0700

    Fail gracefully when we can't get a texture sync handle. (bug 1207665 part 4, r=bas,vladan)

--- gfx/layers/d3d11/CompositorD3D11.cpp
+++ gfx/layers/d3d11/CompositorD3D11.cpp
@@ -23,6 +23,7 @@
 #include "mozilla/Services.h"
 
 #include "mozilla/EnumeratedArray.h"
+#include "mozilla/Telemetry.h"
 
 #include <dxgi1_2.h>
 
@@ -1365,7 +1366,10 @@ DeviceAttachmentsD3D11::InitSyncObject()
   if (FAILED(hr) || !mSyncHandle) {
     gfxCriticalError() << "Failed to get SharedHandle for sync texture. Result: "
                        << hexa(hr);
-    MOZ_CRASH();
+    NS_DispatchToMainThread(NS_NewRunnableFunction([] () -> void {
+      Accumulate(Telemetry::D3D11_SYNC_HANDLE_FAILURE, 1);
+    }));
+    return false;
   }
 
   return true;
