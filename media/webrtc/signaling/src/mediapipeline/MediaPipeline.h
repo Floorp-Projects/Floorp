bfde7d0156886526d0f4cdc09c0c8000a99ad19b
Andreas Pehrson <pehrsons@gmail.com>
2015-09-30 09:31:53 +0800

    Bug 1170958 - Add input stream and track as args to NotifyQueuedTrackChanges. r=roc This allows for tracking the input track of an added track (for ProcessedMediaStream tracks; SourceMediaStream tracks don't have input tracks) directly in the NotifyQueuedTrackChanges handler, which will be necessary for locking MediaInputPorts to specific tracks.

--- media/webrtc/signaling/src/mediapipeline/MediaPipeline.h
+++ media/webrtc/signaling/src/mediapipeline/MediaPipeline.h
@@ -482,7 +482,9 @@ public:
     virtual void NotifyQueuedTrackChanges(MediaStreamGraph* graph, TrackID tid,
                                           StreamTime offset,
                                           uint32_t events,
-                                          const MediaSegment& queued_media) override;
+                                          const MediaSegment& queued_media,
+                                          MediaStream* input_stream,
+                                          TrackID input_tid) override;
     virtual void NotifyPull(MediaStreamGraph* aGraph, StreamTime aDesiredTime) override {}
 
     // Implement MediaStreamDirectListener
@@ -629,7 +631,9 @@ class MediaPipelineReceiveAudio : public MediaPipelineReceive {
     virtual void NotifyQueuedTrackChanges(MediaStreamGraph* graph, TrackID tid,
                                           StreamTime offset,
                                           uint32_t events,
-                                          const MediaSegment& queued_media) override {}
+                                          const MediaSegment& queued_media,
+                                          MediaStream* input_stream,
+                                          TrackID input_tid) override {}
     virtual void NotifyPull(MediaStreamGraph* graph, StreamTime desired_time) override;
 
    private:
@@ -725,7 +729,9 @@ class MediaPipelineReceiveVideo : public MediaPipelineReceive {
     virtual void NotifyQueuedTrackChanges(MediaStreamGraph* graph, TrackID tid,
                                           StreamTime offset,
                                           uint32_t events,
-                                          const MediaSegment& queued_media) override {}
+                                          const MediaSegment& queued_media,
+                                          MediaStream* input_stream,
+                                          TrackID input_tid) override {}
     virtual void NotifyPull(MediaStreamGraph* graph, StreamTime desired_time) override;
 
     // Accessors for external writes from the renderer
