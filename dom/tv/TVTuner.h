e345e74e7c1403738d868b58ea18af276cddc60a
Mantaroh Yoshinaga <mantaroh@gmail.com>
2015-09-30 14:32:32 +0900

    Bug 1180589 part 2 - Add code to create a simulated mediastream; r=seanlin

--- dom/tv/TVTuner.h
+++ dom/tv/TVTuner.h
@@ -11,6 +11,8 @@
 // Include TVTunerBinding.h since enum TVSourceType can't be forward declared.
 #include "mozilla/dom/TVTunerBinding.h"
 
+#define VIDEO_TAG NS_LITERAL_STRING("video")
+
 class nsITVService;
 class nsITVTunerData;
 
@@ -58,6 +60,8 @@ public:
   already_AddRefed<DOMMediaStream> GetStream() const;
 
   IMPL_EVENT_HANDLER(currentsourcechanged);
+
+  nsresult ReloadMediaStream();
 
 private:
   explicit TVTuner(nsPIDOMWindow* aWindow);
