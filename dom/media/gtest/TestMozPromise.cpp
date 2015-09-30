fd7e9a2717b9ace6f236ca6d68351bda2c7be034
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-09-30 14:04:42 +1000

    Bug 1207312: P2. Amend gtest. r=bholley

--- dom/media/gtest/TestMozPromise.cpp
+++ dom/media/gtest/TestMozPromise.cpp
@@ -207,6 +207,8 @@ TEST(MozPromise, PromiseAllReject)
     promises.AppendElement(TestPromise::CreateAndResolve(22, __func__));
     promises.AppendElement(TestPromise::CreateAndReject(32.0, __func__));
     promises.AppendElement(TestPromise::CreateAndResolve(42, __func__));
+   // Ensure that more than one rejection doesn't cause a crash (bug #1207312)
+    promises.AppendElement(TestPromise::CreateAndReject(52.0, __func__));
 
     TestPromise::All(queue, promises)->Then(queue, __func__,
       DO_FAIL,
