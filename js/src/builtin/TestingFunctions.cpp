fc022c6a63f1c637e69949c420d31c5df33cfa10
Nick Fitzgerald <fitzgen@gmail.com>
2015-09-30 16:03:31 -0700

    Bug 1196461 - De-duplicate strings in heap snapshot core dumps; r=shu,jimb
    This changeset replaces all of the
    
        // char16_t[]
        optional bytes someProperty = 1;
    
    one- and two-byte string properties in the CoreDump.proto protobuf definition
    file with:
    
        oneof {
            // char16_t[]
            bytes  someProperty    = 1;
            uint64 somePropertyRef = 2;
        }
    
    The first time the N^th unique string is serialized, then someProperty is used
    and the full string is serialized in the protobuf message. All following times
    that string is serialized, somePropertyRef is used and its value is N.
    
    Among the other things, this also changes JS::ubi::Edge::name from a raw pointer
    with commented rules about who does or doesn't own and should and shouldn't free
    the raw pointer to a UniquePtr that enforces those rules rather than relying on
    developers reading and obeying the rules in the comments.

--- js/src/builtin/TestingFunctions.cpp
+++ js/src/builtin/TestingFunctions.cpp
@@ -2141,7 +2141,7 @@ struct FindPathHandler {
 
         // Record how we reached this node. This is the last edge on a
         // shortest path to this node.
-        EdgeName edgeName = DuplicateString(cx, edge.name);
+        EdgeName edgeName = DuplicateString(cx, edge.name.get());
         if (!edgeName)
             return false;
         *backEdge = mozilla::Move(BackEdge(origin, Move(edgeName)));
