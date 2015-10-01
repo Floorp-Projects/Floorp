a17db17f2291f8fccee9909949208f6f84c052e0
Gregory Szorc <gps@mozilla.com>
2015-09-30 17:38:53 -0700

    Bug 1208320 - Produce common tests archive via Python; r=glandium
    This doesn't change I/O or copy behavior at all. But it does remove a
    one-off make rule.

--- python/mozbuild/mozbuild/action/test_archive.py
+++ python/mozbuild/mozbuild/action/test_archive.py
@@ -11,6 +11,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import argparse
+import itertools
 import os
 import sys
 
@@ -24,6 +25,21 @@ STAGE = mozpath.join(buildconfig.topobjdir, 'dist', 'test-stage')
 
 
 ARCHIVE_FILES = {
+    'common': [
+        {
+            'source': STAGE,
+            'base': '',
+            'pattern': '**',
+            'ignore': [
+                'cppunittest/**',
+                'mochitest/**',
+                'reftest/**',
+                'talos/**',
+                'web-platform/**',
+                'xpcshell/**',
+            ],
+        },
+    ],
     'mochitest': [
         {
             'source': buildconfig.topobjdir,
@@ -77,6 +93,21 @@ ARCHIVE_FILES = {
         },
     ],
 }
+
+
+# "common" is our catch all archive and it ignores things from other archives.
+# Verify nothing sneaks into ARCHIVE_FILES without a corresponding exclusion
+# rule in the "common" archive.
+for k, v in ARCHIVE_FILES.items():
+    # Skip mozharness because it isn't staged.
+    if k in ('common', 'mozharness'):
+        continue
+
+    ignores = set(itertools.chain(*(e.get('ignore', [])
+                                  for e in ARCHIVE_FILES['common'])))
+
+    if not any(p.startswith('%s/' % k) for p in ignores):
+        raise Exception('"common" ignore list probably should contain %s' % k)
 
 
 def find_files(archive):
