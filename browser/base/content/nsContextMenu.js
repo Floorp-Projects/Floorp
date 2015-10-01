2e6f0756d0af00bc462aad0224dc49f165b9292f
J. Ryan Stinnett <jryans@gmail.com>
2015-10-01 17:21:29 -0500

    Bug 1210381 - Remove view-source: when copying links. r=mconley, a=sylvestre

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -1479,9 +1479,11 @@ nsContextMenu.prototype = {
   },
 
   copyLink: function() {
+    // If we're in a view source tab, remove the view-source: prefix
+    let linkURL = this.linkURL.replace(/^view-source:/, "");
     var clipboard = Cc["@mozilla.org/widget/clipboardhelper;1"].
                     getService(Ci.nsIClipboardHelper);
-    clipboard.copyString(this.linkURL);
+    clipboard.copyString(linkURL);
   },
 
   ///////////////
