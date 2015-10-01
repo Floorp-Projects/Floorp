906147b67054162d91dd38bf888ee1a3ed9ad8f1
Nigel Babu <nigelbabu@gmail.com>
2015-10-01 13:53:14 +0530

    Backed out changeset 04430cc26dba (bug 1180940) for Mulet Gij(4) bustage again.
    --HG--
    extra : amend_source : 3a0164d64d841349526acd11223cd88501a996f5

--- dom/media/webaudio/AudioContext.cpp
+++ dom/media/webaudio/AudioContext.cpp
@@ -117,21 +117,14 @@ AudioContext::AudioContext(nsPIDOMWindow* aWindow,
   }
 }
 
-nsresult
+void
 AudioContext::Init()
 {
   // We skip calling SetIsOnlyNodeForContext and the creation of the
   // audioChannelAgent during mDestination's constructor, because we can only
   // call them after mDestination has been set up.
-  if (!mIsOffline) {
-    nsresult rv = mDestination->CreateAudioChannelAgent();
-    if (NS_WARN_IF(NS_FAILED(rv))) {
-      return rv;
-    }
-    mDestination->SetIsOnlyNodeForContext(true);
-  }
-
-  return NS_OK;
+  mDestination->CreateAudioChannelAgent();
+  mDestination->SetIsOnlyNodeForContext(true);
 }
 
 AudioContext::~AudioContext()
@@ -167,10 +160,7 @@ AudioContext::Constructor(const GlobalObject& aGlobal,
   nsRefPtr<AudioContext> object =
     new AudioContext(window, false,
                      AudioChannelService::GetDefaultAudioChannel());
-  aRv = object->Init();
-  if (NS_WARN_IF(aRv.Failed())) {
-     return nullptr;
-  }
+  object->Init();
 
   RegisterWeakMemoryReporter(object);
 
@@ -189,10 +179,7 @@ AudioContext::Constructor(const GlobalObject& aGlobal,
   }
 
   nsRefPtr<AudioContext> object = new AudioContext(window, false, aChannel);
-  aRv = object->Init();
-  if (NS_WARN_IF(aRv.Failed())) {
-     return nullptr;
-  }
+  object->Init();
 
   RegisterWeakMemoryReporter(object);
 
