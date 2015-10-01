d37f3a5453f34478713205c09135cda42eb8e032
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2015-10-01 12:41:40 +0200

    Bug 1204554 part 1 - Remove unused JSContext from LcovWriteScript. r=bhackett

--- js/src/jsopcode.cpp
+++ js/src/jsopcode.cpp
@@ -1987,7 +1987,7 @@ struct LcovSourceFile
 };
 
 static bool
-LcovWriteScript(JSContext* cx, LcovSourceFile& lsf, JSScript* script)
+LcovWriteScript(LcovSourceFile& lsf, JSScript* script)
 {
     lsf.numFunctionsFound++;
     lsf.outFN.printf("FN:%d,", script->lineno());
@@ -2182,7 +2182,7 @@ GenerateLcovInfo(JSContext* cx, JSCompartment* comp, GenericPrinter& out)
             script = queue.popCopy();
 
             // Code the current script before pushing.
-            if (!LcovWriteScript(cx, lsf, script))
+            if (!LcovWriteScript(lsf, script))
                 return false;
 
             // Iterate from the last to the first object in order to have
