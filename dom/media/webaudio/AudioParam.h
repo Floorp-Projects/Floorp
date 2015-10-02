e327b7ee0d8098a852e6a2f39fbb6e673e7388aa
Karl Tomlinson <karlt+@karlt.net>
2015-10-02 14:24:51 +1300

    bug 1210267 remove custom AudioParam::Release and disconnect in destructor r=baku
    AudioParam has no derived type (and DisconnectFromGraphAndDestroyStream calls
    no virtual functions) so no need for special release.

--- dom/media/webaudio/AudioParam.h
+++ dom/media/webaudio/AudioParam.h
@@ -160,8 +160,6 @@ public:
     return mInputNodes.AppendElement();
   }
 
-  void DisconnectFromGraphAndDestroyStream();
-
   // May create the stream if it doesn't exist
   MediaStream* Stream();
 
@@ -208,6 +206,8 @@ private:
   }
 
   void SendEventToEngine(const AudioTimelineEvent& aEvent);
+
+  void DisconnectFromGraphAndDestroyStream();
 
   nsCycleCollectingAutoRefCnt mRefCnt;
   NS_DECL_OWNINGTHREAD
