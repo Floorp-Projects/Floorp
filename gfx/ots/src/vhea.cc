a6cde66db046eae9af6e34e416cce51e16ebf979
Frédéric Wang <fred.wang@free.fr>
2015-10-02 05:21:45 +0200

    Bug 1193050 - Update OTS to latest upstream version. r=jfkthame

--- gfx/ots/src/vhea.cc
+++ gfx/ots/src/vhea.cc
@@ -15,10 +15,10 @@
 
 namespace ots {
 
-bool ots_vhea_parse(OpenTypeFile *file, const uint8_t *data, size_t length) {
+bool ots_vhea_parse(Font *font, const uint8_t *data, size_t length) {
   Buffer table(data, length);
   OpenTypeVHEA *vhea = new OpenTypeVHEA;
-  file->vhea = vhea;
+  font->vhea = vhea;
 
   if (!table.ReadU32(&vhea->header.version)) {
     return OTS_FAILURE_MSG("Failed to read version");
@@ -28,30 +28,35 @@ bool ots_vhea_parse(OpenTypeFile *file, const uint8_t *data, size_t length) {
     return OTS_FAILURE_MSG("Bad vhea version %x", vhea->header.version);
   }
 
-  if (!ParseMetricsHeader(file, &table, &vhea->header)) {
+  if (!ParseMetricsHeader(font, &table, &vhea->header)) {
     return OTS_FAILURE_MSG("Failed to parse metrics in vhea");
   }
 
   return true;
 }
 
-bool ots_vhea_should_serialise(OpenTypeFile *file) {
+bool ots_vhea_should_serialise(Font *font) {
   // vhea should'nt serialise when vmtx doesn't exist.
   // Firefox developer pointed out that vhea/vmtx should serialise iff GSUB is
   // preserved. See http://crbug.com/77386
-  return file->vhea != NULL && file->vmtx != NULL &&
-      ots_gsub_should_serialise(file);
+  return font->vhea != NULL && font->vmtx != NULL &&
+      ots_gsub_should_serialise(font);
 }
 
-bool ots_vhea_serialise(OTSStream *out, OpenTypeFile *file) {
-  if (!SerialiseMetricsHeader(file, out, &file->vhea->header)) {
+bool ots_vhea_serialise(OTSStream *out, Font *font) {
+  if (!SerialiseMetricsHeader(font, out, &font->vhea->header)) {
     return OTS_FAILURE_MSG("Failed to write vhea metrics");
   }
   return true;
 }
 
-void ots_vhea_free(OpenTypeFile *file) {
-  delete file->vhea;
+void ots_vhea_reuse(Font *font, Font *other) {
+  font->vhea = other->vhea;
+  font->vhea_reused = true;
+}
+
+void ots_vhea_free(Font *font) {
+  delete font->vhea;
 }
 
 }  // namespace ots
