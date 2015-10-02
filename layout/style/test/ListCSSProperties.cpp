a0ad5efa14a15caec5842ebe5ec429682a884f54
Xidorn Quan <quanxunzhen@gmail.com>
2015-10-02 11:01:54 +1000

    Bug 1069192 part 3 - Restrict -moz-window-{dragging,shadow} to be chrome-only. r=dbaron

--- layout/style/test/ListCSSProperties.cpp
+++ layout/style/test/ListCSSProperties.cpp
@@ -111,7 +111,9 @@ const char *gInaccessibleProperties[] = {
     "-moz-script-size-multiplier",
     "-moz-script-min-size",
     "-moz-math-variant",
-    "-moz-math-display" // parsed by UA sheets only
+    "-moz-math-display", // parsed by UA sheets only
+    "-moz-window-dragging", // chrome-only internal properties
+    "-moz-window-shadow" // chrome-only internal properties
 };
 
 inline int
