5997fbb0e4738da1c31feb4ff165106ed7207b0a
Dustin J. Mitchell <dustin@mozilla.com>
2015-10-02 11:49:31 -0400

    Bug 1209663: remove unused docker images; r=mrrrgn DONOTBUILD
    --HG--
    extra : commitid : 99iJD5rNaqg
    extra : rebase_source : af0a1f15505af818682e9bb76b7513538130ccc0
    extra : amend_source : 8616c01111eefb2458851b69885064fb513020a7

--- testing/docker/desktop32-build/hacks.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-##
-# Install compiler hacks, necessary for builds to work properly,
-# but ultimately to be removed in favor of something cleaner
-##
-
-ln -s /usr/include/linux/a.out.h /usr/include/a.out.h
