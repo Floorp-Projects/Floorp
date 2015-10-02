8e79b528798d5a862583dc87b77fce09f291a25a
Nigel Babu <nigelbabu@gmail.com>
2015-10-02 15:08:43 +0530

    Backed out 3 changesets (bug 1069192) for OS X debug M(oth) crashes
    Backed out changeset e86111412050 (bug 1069192)
    Backed out changeset a5f11c5c5bcb (bug 1069192)
    Backed out changeset a4aaaac8f133 (bug 1069192)

--- layout/style/test/ListCSSProperties.cpp
+++ layout/style/test/ListCSSProperties.cpp
@@ -111,9 +111,7 @@ const char *gInaccessibleProperties[] = {
     "-moz-script-size-multiplier",
     "-moz-script-min-size",
     "-moz-math-variant",
-    "-moz-math-display", // parsed by UA sheets only
-    "-moz-window-dragging", // chrome-only internal properties
-    "-moz-window-shadow" // chrome-only internal properties
+    "-moz-math-display" // parsed by UA sheets only
 };
 
 inline int
