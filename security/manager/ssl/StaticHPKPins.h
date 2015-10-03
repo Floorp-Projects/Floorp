ee1f7915f18c74461ebf7360e3c8f21750a8fed7
ffxbld <>
2015-10-03 03:45:08 -0700

    No bug, Automated HPKP preload list update from host bld-linux64-spot-565 - a=hpkp-update

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -1035,6 +1035,7 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
   { "passwords.google.com", true, false, false, -1, &kPinset_google_root_pems },
   { "pinningtest.appspot.com", true, false, false, -1, &kPinset_test },
   { "pixel.facebook.com", true, false, false, -1, &kPinset_facebook },
+  { "pixel.google.com", true, false, false, -1, &kPinset_google_root_pems },
   { "platform.twitter.com", true, false, false, -1, &kPinset_twitterCDN },
   { "play.google.com", true, false, false, -1, &kPinset_google_root_pems },
   { "plus.google.com", true, false, false, -1, &kPinset_google_root_pems },
@@ -1092,8 +1093,8 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
   { "ytimg.com", true, false, false, -1, &kPinset_google_root_pems },
 };
 
-// Pinning Preload List Length = 363;
+// Pinning Preload List Length = 364;
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1451730678663000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1452335705339000);
