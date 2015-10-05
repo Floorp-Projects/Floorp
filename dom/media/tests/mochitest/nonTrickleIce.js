317ae75b429e4e54cc3914f31e2bd5f1e80b7261
Nigel Babu <nigelbabu@gmail.com>
2015-10-05 14:22:10 +0530

    Backed out changeset b958e25b1ecf (bug 1209744) for mochitest bustage

--- dom/media/tests/mochitest/nonTrickleIce.js
+++ dom/media/tests/mochitest/nonTrickleIce.js
@@ -2,11 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-function removeTrickleOption(desc) {
-  var sdp = desc.sdp.replace(/\r\na=ice-options:trickle\r\n/, "\r\n");
-  return new mozRTCSessionDescription({ type: desc.type, sdp: sdp });
-}
-
 function makeOffererNonTrickle(chain) {
   chain.replace('PC_LOCAL_SETUP_ICE_HANDLER', [
     function PC_LOCAL_SETUP_NOTRICKLE_ICE_HANDLER(test) {
@@ -19,7 +14,7 @@ function makeOffererNonTrickle(chain) {
   chain.replace('PC_REMOTE_GET_OFFER', [
     function PC_REMOTE_GET_FULL_OFFER(test) {
       return test.pcLocal.endOfTrickleIce.then(() => {
-        test._local_offer = removeTrickleOption(test.pcLocal.localDescription);
+        test._local_offer = test.pcLocal.localDescription;
         test._offer_constraints = test.pcLocal.constraints;
         test._offer_options = test.pcLocal.offerOptions;
       });
@@ -29,14 +24,11 @@ function makeOffererNonTrickle(chain) {
     function PC_REMOTE_REQUIRE_REMOTE_SDP_CANDIDATES(test) {
       info("test.pcLocal.localDescription.sdp: " + JSON.stringify(test.pcLocal.localDescription.sdp));
       info("test._local_offer.sdp" + JSON.stringify(test._local_offer.sdp));
-      is(test.pcRemote._pc.canTrickleIceCandidates, false,
-         "Remote thinks that trickle isn't supported");
       ok(!test.localRequiresTrickleIce, "Local does NOT require trickle");
       ok(test._local_offer.sdp.includes("a=candidate"), "offer has ICE candidates")
       ok(test._local_offer.sdp.includes("a=end-of-candidates"), "offer has end-of-candidates");
     }
   ]);
-  chain.remove('PC_REMOTE_CHECK_CAN_TRICKLE_SYNC');
 }
 
 function makeAnswererNonTrickle(chain) {
@@ -51,7 +43,7 @@ function makeAnswererNonTrickle(chain) {
   chain.replace('PC_LOCAL_GET_ANSWER', [
     function PC_LOCAL_GET_FULL_ANSWER(test) {
       return test.pcRemote.endOfTrickleIce.then(() => {
-        test._remote_answer = removeTrickleOption(test.pcRemote.localDescription);
+        test._remote_answer = test.pcRemote.localDescription;
         test._answer_constraints = test.pcRemote.constraints;
       });
     }
@@ -60,12 +52,9 @@ function makeAnswererNonTrickle(chain) {
     function PC_LOCAL_REQUIRE_REMOTE_SDP_CANDIDATES(test) {
       info("test.pcRemote.localDescription.sdp: " + JSON.stringify(test.pcRemote.localDescription.sdp));
       info("test._remote_answer.sdp" + JSON.stringify(test._remote_answer.sdp));
-      is(test.pcLocal._pc.canTrickleIceCandidates, false,
-         "Local thinks that trickle isn't supported");
       ok(!test.remoteRequiresTrickleIce, "Remote does NOT require trickle");
       ok(test._remote_answer.sdp.includes("a=candidate"), "answer has ICE candidates")
       ok(test._remote_answer.sdp.includes("a=end-of-candidates"), "answer has end-of-candidates");
     }
   ]);
-  chain.remove('PC_LOCAL_CHECK_CAN_TRICKLE_SYNC');
 }
