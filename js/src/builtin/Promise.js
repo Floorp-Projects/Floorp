ff993441129eadb3ef5865156829beedc7cdcd0e
Till Schneidereit <till@tillschneidereit.net>
2015-10-05 13:24:03 -0700

    Bug 1185106 - Part 1: ShellPromise boilerplate. (r=efaust)

---
+++ js/src/builtin/Promise.js
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+function Promise_all(iterable) {
+    global.print("Hi, I'm Promise_all. Please implement me.");
+}
+
+function Promise_race(iterable) {
+    global.print("Hi, I'm Promise_race. Please implement me.");
+}
+
+function Promise_reject(iterable) {
+    global.print("Hi, I'm Promise_reject. Please implement me.");
+}
+
+function Promise_resolve(iterable) {
+    global.print("Hi, I'm Promise_resolve. Please implement me.");
+}
+
+function Promise_catch(onRejected) {
+    global.print("Hi, I'm Promise_catch. Please implement me.");
+}
+
+function Promise_then(onFulfilled, onRejected) {
+    global.print("Hi, I'm Promise_then. Please implement me.");
+}
