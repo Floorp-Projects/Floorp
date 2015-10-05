a724db30d2ce310214d3f37f574de68557a550a1
Aaron Klotz <aklotz@mozilla.com>
2015-10-04 22:17:20 -0600

    Bug 1209464 - Fix missing neutered window region in MessageChannel::WaitForInterruptNotify. Regression from bug 1189709; r=jimm, a=sylvestre

--- ipc/glue/WindowsMessageLoop.cpp
+++ ipc/glue/WindowsMessageLoop.cpp
@@ -1132,6 +1132,8 @@ MessageChannel::WaitForInterruptNotify()
       NS_ASSERTION(timerId, "SetTimer failed!");
     }
 
+    NeuteredWindowRegion neuteredRgn(true);
+
     MSG msg = { 0 };
 
     // Don't get wrapped up in here if the child connection dies.
