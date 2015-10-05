93220c69d0ad55f5d3f6dc3b282b58b21f2e09b6
Wes Kocher <wkocher@mozilla.com>
2015-10-05 09:11:35 -0700

    Backed out 2 changesets (bug 1207665) for being the wrong patches for uplift a=backout
    Backed out changeset 1ca0650854b4 (bug 1207665)
    Backed out changeset 8de4694e5a54 (bug 1207665)

--- gfx/layers/d3d11/CompositorD3D11.cpp
+++ gfx/layers/d3d11/CompositorD3D11.cpp
@@ -21,7 +21,6 @@
 #include "mozilla/gfx/StackArray.h"
 
 #include "mozilla/EnumeratedArray.h"
-#include "mozilla/Telemetry.h"
 
 #include <dxgi1_2.h>
 
@@ -48,13 +47,11 @@ const FLOAT sBlendFactor[] = { 0, 0, 0, 0 };
 struct DeviceAttachmentsD3D11
 {
   DeviceAttachmentsD3D11(ID3D11Device* device)
-   : mSyncHandle(0),
-     mDevice(device),
+   : mDevice(device),
      mInitOkay(true)
   {}
 
   bool CreateShaders();
-  bool InitSyncObject();
 
   typedef EnumeratedArray<MaskType, MaskType::NumMaskTypes, RefPtr<ID3D11VertexShader>>
           VertexShaderArray;
@@ -83,7 +80,6 @@ struct DeviceAttachmentsD3D11
   RefPtr<ID3D11BlendState> mComponentBlendState;
   RefPtr<ID3D11BlendState> mDisabledBlendState;
   RefPtr<IDXGIResource> mSyncTexture;
-  HANDLE mSyncHandle;
 
   //
   // VR pieces
@@ -364,8 +360,24 @@ CompositorD3D11::Initialize()
       return false;
     }
 
-    if (!mAttachments->InitSyncObject()) {
-      return false;
+    if (!gfxWindowsPlatform::GetPlatform()->IsWARP()) {
+      // It's okay to do this on Windows 8. But for now we'll just bail
+      // whenever we're using WARP.
+      CD3D11_TEXTURE2D_DESC desc(DXGI_FORMAT_B8G8R8A8_UNORM, 1, 1, 1, 1,
+                                 D3D11_BIND_SHADER_RESOURCE |
+                                 D3D11_BIND_RENDER_TARGET);
+      desc.MiscFlags = D3D11_RESOURCE_MISC_SHARED_KEYEDMUTEX;
+
+      RefPtr<ID3D11Texture2D> texture;
+      hr = mDevice->CreateTexture2D(&desc, nullptr, byRef(texture));
+      if (FAILED(hr)) {
+        return false;
+      }
+
+      hr = texture->QueryInterface((IDXGIResource**)byRef(mAttachments->mSyncTexture));
+      if (FAILED(hr)) {
+        return false;
+      }
     }
     
     //
@@ -462,7 +474,14 @@ CompositorD3D11::GetTextureFactoryIdentifier()
   ident.mMaxTextureSize = GetMaxTextureSize();
   ident.mParentProcessId = XRE_GetProcessType();
   ident.mParentBackend = LayersBackend::LAYERS_D3D11;
-  ident.mSyncHandle = mAttachments->mSyncHandle;
+  if (mAttachments->mSyncTexture) {
+    HRESULT hr = mAttachments->mSyncTexture->GetSharedHandle(&ident.mSyncHandle);
+    if (FAILED(hr) || !ident.mSyncHandle) {
+      gfxCriticalError() << "Failed to get SharedHandle for sync texture. Result: "
+                         << hexa(hr);
+      MOZ_CRASH();
+    }
+  }
   ident.mSupportedBlendModes += gfx::CompositionOp::OP_SCREEN;
   ident.mSupportedBlendModes += gfx::CompositionOp::OP_MULTIPLY;
   return ident;
@@ -1326,45 +1345,6 @@ CompositorD3D11::UpdateRenderTarget()
 
   mDefaultRT = new CompositingRenderTargetD3D11(backBuf, IntPoint(0, 0));
   mDefaultRT->SetSize(mSize);
-}
-
-bool
-DeviceAttachmentsD3D11::InitSyncObject()
-{
-  // Sync object is not supported on WARP.
-  if (gfxWindowsPlatform::GetPlatform()->IsWARP()) {
-    return true;
-  }
-
-  // It's okay to do this on Windows 8. But for now we'll just bail
-  // whenever we're using WARP.
-  CD3D11_TEXTURE2D_DESC desc(DXGI_FORMAT_B8G8R8A8_UNORM, 1, 1, 1, 1,
-                             D3D11_BIND_SHADER_RESOURCE |
-                             D3D11_BIND_RENDER_TARGET);
-  desc.MiscFlags = D3D11_RESOURCE_MISC_SHARED_KEYEDMUTEX;
-
-  RefPtr<ID3D11Texture2D> texture;
-  HRESULT hr = mDevice->CreateTexture2D(&desc, nullptr, byRef(texture));
-  if (FAILED(hr)) {
-    return false;
-  }
-
-  hr = texture->QueryInterface((IDXGIResource**)byRef(mSyncTexture));
-  if (FAILED(hr)) {
-    return false;
-  }
-
-  hr = mSyncTexture->GetSharedHandle(&mSyncHandle);
-  if (FAILED(hr) || !mSyncHandle) {
-    gfxCriticalError() << "Failed to get SharedHandle for sync texture. Result: "
-                       << hexa(hr);
-    NS_DispatchToMainThread(NS_NewRunnableFunction([] () -> void {
-      Accumulate(Telemetry::D3D11_SYNC_HANDLE_FAILURE, 1);
-    }));
-    return false;
-  }
-
-  return true;
 }
 
 bool
