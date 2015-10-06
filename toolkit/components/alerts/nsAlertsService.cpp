e566f5a517a09e77bfb1b272ec90287948e4afa0
Phil Ringnalda <philringnalda@gmail.com>
2015-10-05 18:41:00 -0700

    Back out 2 changesets (bug 1202933) for Mulet timeouts in test_alerts.html CLOSED TREE
    Backed out changeset fbcc1bb170e9 (bug 1202933)
    Backed out changeset 1763e1484065 (bug 1202933)

--- toolkit/components/alerts/nsAlertsService.cpp
+++ toolkit/components/alerts/nsAlertsService.cpp
@@ -120,7 +120,7 @@ NS_IMETHODIMP nsAlertsService::ShowAlertNotification(const nsAString & aImageUrl
   // Use XUL notifications as a fallback if above methods have failed.
   rv = mXULAlerts.ShowAlertNotification(aImageUrl, aAlertTitle, aAlertText, aAlertTextClickable,
                                         aAlertCookie, aAlertListener, aAlertName,
-                                        aBidi, aLang, aPrincipal, aInPrivateBrowsing);
+                                        aBidi, aLang, aInPrivateBrowsing);
   return rv;
 #endif // !MOZ_WIDGET_ANDROID
 }
--- toolkit/components/alerts/nsXULAlerts.h
+++ toolkit/components/alerts/nsXULAlerts.h
@@ -25,8 +25,7 @@ public:
                                  const nsAString& aAlertText, bool aAlertTextClickable,
                                  const nsAString& aAlertCookie, nsIObserver* aAlertListener,
                                  const nsAString& aAlertName, const nsAString& aBidi,
-                                 const nsAString& aLang, nsIPrincipal* aPrincipal,
-                                 bool aInPrivateBrowsing);
+                                 const nsAString& aLang, bool aInPrivateBrowsing);
 
   nsresult CloseAlert(const nsAString& aAlertName);
 protected:
