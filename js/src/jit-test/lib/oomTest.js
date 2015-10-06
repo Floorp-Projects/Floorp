6bb08a260a7d60e2ff68086a67b1af1dc4c1eec7
Jon Coppeard <jcoppeard@mozilla.com>
2015-10-06 14:50:49 +0100

    Bug 1209911 - Add oomThreadTypes() test function to report the number of thread types we can simulate OOM on r=terrence

--- js/src/jit-test/lib/oomTest.js
+++ js/src/jit-test/lib/oomTest.js
@@ -1,34 +1,39 @@
 // Function to test OOM handling by repeatedly calling a function and failing
 // successive allocations.
 
-const verbose = false;
-
-if (!("oomAtAllocation" in this && "resetOOMFailure" in this))
+if (!("oomAtAllocation" in this && "resetOOMFailure" in this && "oomThreadTypes" in this))
     quit();
 
 if ("gczeal" in this)
     gczeal(0);
 
+const verbose = ("os" in this) && os.getenv("OOM_VERBOSE");
+
 // Test out of memory handing by calling a function f() while causing successive
 // memory allocations to fail.  Repeat until f() finishes without reaching the
 // failing allocation.
 function oomTest(f) {
-    var i = 1;
-    var more;
-    do {
+    for (let thread = 1; thread < oomThreadTypes(); thread++) {
         if (verbose)
-            print("fail at " + i);
-        try {
-            oomAtAllocation(i);
-            f();
-            more = resetOOMFailure();
-        } catch (e) {
-            // Ignore exceptions.
-            more = resetOOMFailure();
-        }
-        i++;
-    } while(more);
+            print("testing thread " + thread);
 
-    if (verbose)
-        print("finished after " + i);
+        var i = 1;
+        var more;
+        do {
+            if (verbose)
+                print("fail at " + i);
+            try {
+                oomAtAllocation(i, thread);
+                f();
+                more = resetOOMFailure();
+            } catch (e) {
+                // Ignore exceptions.
+                more = resetOOMFailure();
+            }
+            i++;
+        } while(more);
+
+        if (verbose)
+            print("finished after " + (i - 2) + " failures");
+    }
 }
