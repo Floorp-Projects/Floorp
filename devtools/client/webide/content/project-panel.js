889dacee39086bdb8949be43bc08591d8866bec4
Jennifer Fong <jfong@mozilla.com>
2015-10-06 14:52:00 +0200

    Bug 1209641 - Remove dropdown layout and use sidebars layout. r=jryans

--- devtools/client/webide/content/project-panel.js
+++ devtools/client/webide/content/project-panel.js
@@ -7,29 +7,5 @@ var ProjectPanel = {
   toggleSidebar: function() {
     document.querySelector("#project-listing-panel").setAttribute("sidebar-displayed", true);
     document.querySelector("#project-listing-splitter").setAttribute("sidebar-displayed", true);
-  },
-
-  showPopup: function() {
-    let deferred = promise.defer();
-
-    let panelNode = document.querySelector("#project-panel");
-    let panelVboxNode = document.querySelector("#project-panel > #project-panel-box");
-    let anchorNode = document.querySelector("#project-panel-button > .panel-button-anchor");
-
-    window.setTimeout(() => {
-      // Open the popup only when the projects are added.
-      // Not doing it in the next tick can cause mis-calculations
-      // of the size of the panel.
-      function onPopupShown() {
-        panelNode.removeEventListener("popupshown", onPopupShown);
-        deferred.resolve();
-      }
-
-      panelNode.addEventListener("popupshown", onPopupShown);
-      panelNode.openPopup(anchorNode);
-      panelVboxNode.scrollTop = 0;
-    }, 0);
-
-    return deferred.promise;
   }
 };
