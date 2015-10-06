d0194f77839fab2a7ab49c26bfed6f1e979b5efe
Tom Schuster <evilpies@gmail.com>
2015-10-06 17:04:09 +0100

    Bug 1211832 - Disable functions that can easily cause artificial OOMs. r=jonco

--- js/src/builtin/TestingFunctions.h
+++ js/src/builtin/TestingFunctions.h
@@ -12,7 +12,7 @@
 namespace js {
 
 bool
-DefineTestingFunctions(JSContext* cx, HandleObject obj, bool fuzzingSafe);
+DefineTestingFunctions(JSContext* cx, HandleObject obj, bool fuzzingSafe, bool disableOOMFunctions);
 
 bool
 testingFunc_assertFloat32(JSContext* cx, unsigned argc, Value* vp);
