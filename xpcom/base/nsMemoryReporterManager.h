ceee61f577a40d4c9655cbec50e0eafbf74004b4
Eric Rahm <erahm@mozilla.com>
2015-10-06 11:23:35 -0700

    Bug 1194555 - Part 5: Add option for async memory reporters. r=njn

--- xpcom/base/nsMemoryReporterManager.h
+++ xpcom/base/nsMemoryReporterManager.h
@@ -41,8 +41,8 @@ public:
     return static_cast<nsMemoryReporterManager*>(imgr.get());
   }
 
-  typedef nsTHashtable<nsRefPtrHashKey<nsIMemoryReporter>> StrongReportersTable;
-  typedef nsTHashtable<nsPtrHashKey<nsIMemoryReporter>> WeakReportersTable;
+  typedef nsDataHashtable<nsRefPtrHashKey<nsIMemoryReporter>, bool> StrongReportersTable;
+  typedef nsDataHashtable<nsPtrHashKey<nsIMemoryReporter>, bool> WeakReportersTable;
 
   // Inter-process memory reporting proceeds as follows.
   //
@@ -184,7 +184,7 @@ public:
 
 private:
   nsresult RegisterReporterHelper(nsIMemoryReporter* aReporter,
-                                  bool aForce, bool aStrongRef);
+                                  bool aForce, bool aStrongRef, bool aIsAsync);
   nsresult StartGettingReports();
   nsresult FinishReporting();
 
