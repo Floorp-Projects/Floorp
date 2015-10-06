218c28b8fd9d29c030ad55389b0c6dfc9f6f4e02
Shu-yu Guo <shu@rfrn.org>
2015-10-06 15:40:46 -0700

    Bug 1212128 - Annotate oomInGetJumpLabelForBranch with allow-oom. (rs=terrence)

--- js/src/jit-test/tests/gc/oomInGetJumpLabelForBranch.js
+++ js/src/jit-test/tests/gc/oomInGetJumpLabelForBranch.js
@@ -1,3 +1,3 @@
-// |jit-test| --no-threads
+// |jit-test| allow-oom; allow-unhandlable-oom; --no-threads
 load(libdir + 'oomTest.js');
 oomTest(() => getBacktrace({thisprops: gc() && delete addDebuggee.enabled}));
