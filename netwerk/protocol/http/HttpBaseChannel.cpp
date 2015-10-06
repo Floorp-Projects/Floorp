516c851691cca19dc2a9cb599894e895b578cc35
Ben Kelly <ben@wanderview.com>
2015-10-06 06:37:06 -0700

    Bug 1210941 P2 Use LOAD_BYPASS_SERVICE_WORKER in HttpBaseChannel instead of mForceNoIntercept. r=jduell

--- netwerk/protocol/http/HttpBaseChannel.cpp
+++ netwerk/protocol/http/HttpBaseChannel.cpp
@@ -77,7 +77,6 @@ HttpBaseChannel::HttpBaseChannel()
   , mResponseTimeoutEnabled(true)
   , mAllRedirectsSameOrigin(true)
   , mAllRedirectsPassTimingAllowCheck(true)
-  , mForceNoIntercept(false)
   , mResponseCouldBeSynthesized(false)
   , mSuspendCount(0)
   , mInitialRwin(0)
@@ -1572,6 +1571,8 @@ HttpBaseChannel::OverrideSecurityInfo(nsISupports* aSecurityInfo)
              "This can only be called when we don't have a security info object already");
   MOZ_RELEASE_ASSERT(aSecurityInfo,
                      "This can only be called with a valid security info object");
+  MOZ_ASSERT(!BypassServiceWorker(),
+             "This can only be called on channels that are not bypassing interception");
   MOZ_ASSERT(mResponseCouldBeSynthesized,
              "This can only be called on channels that can be intercepted");
   if (mSecurityInfo) {
@@ -2116,8 +2117,7 @@ HttpBaseChannel::GetLastModifiedTime(PRTime* lastModifiedTime)
 NS_IMETHODIMP
 HttpBaseChannel::ForceNoIntercept()
 {
-  mForceNoIntercept = true;
-  mResponseCouldBeSynthesized = false;
+  mLoadFlags |= LOAD_BYPASS_SERVICE_WORKER;
   return NS_OK;
 }
 
@@ -2262,12 +2262,18 @@ HttpBaseChannel::IsNavigation()
 }
 
 bool
+HttpBaseChannel::BypassServiceWorker() const
+{
+  return mLoadFlags & LOAD_BYPASS_SERVICE_WORKER;
+}
+
+bool
 HttpBaseChannel::ShouldIntercept()
 {
   nsCOMPtr<nsINetworkInterceptController> controller;
   GetCallback(controller);
   bool shouldIntercept = false;
-  if (controller && !mForceNoIntercept && mLoadInfo) {
+  if (controller && !BypassServiceWorker() && mLoadInfo) {
     nsContentPolicyType type = mLoadInfo->InternalContentPolicyType();
     nsresult rv = controller->ShouldPrepareForIntercept(mURI,
                                                         IsNavigation(),
@@ -2547,11 +2553,6 @@ HttpBaseChannel::SetupReplacementChannel(nsIURI       *newURI,
         LOG(("HttpBaseChannel::SetupReplacementChannel "
              "[this=%p] transferring chain of redirect cache-keys", this));
         httpInternal->SetCacheKeysRedirectChain(mRedirectedCachekeys.forget());
-    }
-
-    // Preserve any skip-serviceworker-flag.
-    if (mForceNoIntercept) {
-      httpInternal->ForceNoIntercept();
     }
 
     // Preserve CORS mode flag.
