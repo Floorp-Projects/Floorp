99fd14ce85088fa11a3ddddac3a1f9ddde624a30
Shu-yu Guo <shu@rfrn.org>
2015-10-06 14:00:30 -0700

    Bug 589199 - Fix jit-tests and js reftests. (r=efaust)

--- js/src/jit-test/tests/debug/Environment-setVariable-02.js
+++ js/src/jit-test/tests/debug/Environment-setVariable-02.js
@@ -4,7 +4,7 @@ var g = newGlobal();
 var dbg = new Debugger;
 var gw = dbg.addDebuggee(g);
 dbg.onDebuggerStatement = function (frame) {
-    frame.environment.parent.setVariable("x", gw);
+    frame.environment.parent.parent.setVariable("x", gw);
 };
 g.eval("var x = 1; debugger;");
 assertEq(g.x, g);
