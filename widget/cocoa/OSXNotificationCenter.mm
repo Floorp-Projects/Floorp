b8d676b40f837fcb803a872db62c810702c86a20
Phil Ringnalda <philringnalda@gmail.com>
2015-10-05 18:41:00 -0700

    Back out 2 changesets (bug 1202933) for Mulet timeouts in test_alerts.html CLOSED TREE
    Backed out changeset fbcc1bb170e9 (bug 1202933)
    Backed out changeset 1763e1484065 (bug 1202933)

--- widget/cocoa/OSXNotificationCenter.mm
+++ widget/cocoa/OSXNotificationCenter.mm
@@ -17,7 +17,6 @@
 #include "nsCOMPtr.h"
 #include "nsIObserver.h"
 #include "nsIContentPolicy.h"
-#include "nsAlertsUtils.h"
 #include "imgRequestProxy.h"
 
 using namespace mozilla;
@@ -240,21 +239,16 @@ OSXNotificationCenter::ShowAlertNotification(const nsAString & aImageUrl, const 
 
   Class unClass = NSClassFromString(@"NSUserNotification");
   id<FakeNSUserNotification> notification = [[unClass alloc] init];
-
-  notification.title = nsCocoaUtils::ToNSString(aAlertTitle);
-
-  nsAutoString hostPort;
-  nsAlertsUtils::GetSourceHostPort(aPrincipal, hostPort);
-  if (!hostPort.IsEmpty()) {
-    notification.subtitle = nsCocoaUtils::ToNSString(hostPort);
-  }
-
-  notification.informativeText = nsCocoaUtils::ToNSString(aAlertText);
+  notification.title = [NSString stringWithCharacters:(const unichar *)aAlertTitle.BeginReading()
+                                               length:aAlertTitle.Length()];
+  notification.informativeText = [NSString stringWithCharacters:(const unichar *)aAlertText.BeginReading()
+                                                         length:aAlertText.Length()];
   notification.soundName = NSUserNotificationDefaultSoundName;
   notification.hasActionButton = NO;
 
   // If this is not an application/extension alert, show additional actions dealing with permissions.
-  if (nsAlertsUtils::IsActionablePrincipal(aPrincipal)) {
+  if (aPrincipal && !nsContentUtils::IsSystemOrExpandedPrincipal(aPrincipal)
+      && !aPrincipal->GetIsNullPrincipal()) {
     nsCOMPtr<nsIStringBundleService> sbs = do_GetService(NS_STRINGBUNDLE_CONTRACTID);
     nsCOMPtr<nsIStringBundle> bundle;
     nsresult rv = sbs->CreateBundle("chrome://alerts/locale/alert.properties", getter_AddRefs(bundle));
@@ -278,7 +272,7 @@ OSXNotificationCenter::ShowAlertNotification(const nsAString & aImageUrl, const 
                                forKey:@"_alternateActionButtonTitles"];
     }
   }
-  NSString *alertName = nsCocoaUtils::ToNSString(aAlertName);
+  NSString *alertName = [NSString stringWithCharacters:(const unichar *)aAlertName.BeginReading() length:aAlertName.Length()];
   if (!alertName) {
     return NS_ERROR_FAILURE;
   }
@@ -338,7 +332,7 @@ OSXNotificationCenter::CloseAlert(const nsAString& aAlertName,
 {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT;
 
-  NSString *alertName = nsCocoaUtils::ToNSString(aAlertName);
+  NSString *alertName = [NSString stringWithCharacters:(const unichar *)aAlertName.BeginReading() length:aAlertName.Length()];
   CloseAlertCocoaString(alertName);
   return NS_OK;
 
