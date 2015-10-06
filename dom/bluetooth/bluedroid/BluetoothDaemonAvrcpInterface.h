51f0a22b42c71d2a4a50454f09797ac58a81d98f
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-10-06 10:08:14 +0200

    Bug 1209085: Replace trivial init ops by |UnpackPDUInitOp| in Bluetooth AVRCP backend, r=joliu

--- dom/bluetooth/bluedroid/BluetoothDaemonAvrcpInterface.h
+++ dom/bluetooth/bluedroid/BluetoothDaemonAvrcpInterface.h
@@ -243,14 +243,13 @@ protected:
     VolumeChangeNotification;
 
   typedef mozilla::ipc::DaemonNotificationRunnable2<
-    NotificationHandlerWrapper, void, int, int>
+    NotificationHandlerWrapper, void, uint8_t, uint8_t, int, int>
     PassthroughCmdNotification;
 
   class GetElementAttrInitOp;
   class GetPlayerAppAttrsTextInitOp;
   class GetPlayerAppValueInitOp;
   class GetPlayerAppValuesTextInitOp;
-  class PassthroughCmdInitOp;
   class RemoteFeatureInitOp;
 
   void RemoteFeatureNtf(const DaemonSocketPDUHeader& aHeader,
