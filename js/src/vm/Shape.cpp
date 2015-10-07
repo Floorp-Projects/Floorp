d62e5e01e6edd90ba6a4cb522d53ebb24f01cc94
Jan de Mooij <jdemooij@mozilla.com>
2015-10-07 11:40:13 +0100

    Bug 1193102 - Deal with OOM in NewObjectCache::invalidateEntriesForShape. r=bhackett

--- js/src/vm/Shape.cpp
+++ js/src/vm/Shape.cpp
@@ -1543,6 +1543,11 @@ NewObjectCache::invalidateEntriesForShape(JSContext* cx, HandleShape shape, Hand
 
     Rooted<GlobalObject*> global(cx, shape->compartment()->unsafeUnbarrieredMaybeGlobal());
     RootedObjectGroup group(cx, ObjectGroup::defaultNewGroup(cx, clasp, TaggedProto(proto)));
+    if (!group) {
+        purge();
+        cx->recoverFromOutOfMemory();
+        return;
+    }
 
     EntryIndex entry;
     if (lookupGlobal(clasp, global, kind, &entry))
