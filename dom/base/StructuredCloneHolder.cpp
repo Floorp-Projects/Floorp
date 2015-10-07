2d49c1199e11160de329675ed4f81489634c87a7
Andrea Marchesini <amarchesini@mozilla.com>
2015-10-07 20:32:14 +0100

    Bug 1212009 - ReadFullySerializableObjects() doesn't use the aIndex param and it can be removed, r=smaug

--- dom/base/StructuredCloneHolder.cpp
+++ dom/base/StructuredCloneHolder.cpp
@@ -383,8 +383,7 @@ StructuredCloneHolder::FreeBuffer(uint64_t* aBuffer,
 /* static */ JSObject*
 StructuredCloneHolder::ReadFullySerializableObjects(JSContext* aCx,
                                                     JSStructuredCloneReader* aReader,
-                                                    uint32_t aTag,
-                                                    uint32_t aIndex)
+                                                    uint32_t aTag)
 {
   if (aTag == SCTAG_DOM_IMAGEDATA) {
     return ReadStructuredCloneImageData(aCx, aReader);
@@ -928,7 +927,7 @@ StructuredCloneHolder::CustomReadHandler(JSContext* aCx,
                                             parent, GetImages(), aIndex);
    }
 
-  return ReadFullySerializableObjects(aCx, aReader, aTag, aIndex);
+  return ReadFullySerializableObjects(aCx, aReader, aTag);
 }
 
 bool
