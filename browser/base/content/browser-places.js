f56d5f7345f2ebea05f8a9db5839aa89ba4abd53
Marco Bonardo <mbonardo@mozilla.com>
2015-10-07 15:40:13 +0200

    Bug 1208063 - The name input field on the new bookmark dialog doesn't get focus. r=adw, a=sylvestre
    --HG--
    extra : source : b7060ab92ce56cf8f682971d28414e2741121358

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -229,20 +229,14 @@ var StarUI = {
 
     gEditItemOverlay.initPanel({ node: aNode
                                , hiddenRows: ["description", "location",
-                                              "loadInSidebar", "keyword"] });
+                                              "loadInSidebar", "keyword"]
+                               , focusedElement: "preferred" });
   }),
 
   panelShown:
   function SU_panelShown(aEvent) {
     if (aEvent.target == this.panel) {
-      if (!this._element("editBookmarkPanelContent").hidden) {
-        let fieldToFocus = "editBMPanel_" +
-          gPrefService.getCharPref("browser.bookmarks.editDialog.firstEditField");
-        var elt = this._element(fieldToFocus);
-        elt.focus();
-        elt.select();
-      }
-      else {
+      if (this._element("editBookmarkPanelContent").hidden) {
         // Note this isn't actually used anymore, we should remove this
         // once we decide not to bring back the page bookmarked notification
         this.panel.focus();
