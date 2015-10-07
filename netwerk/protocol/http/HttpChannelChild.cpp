a2befb3bb7fe75928ad24875d33d7243999f5622
Ehsan Akhgari <ehsan@mozilla.com>
2015-10-07 18:01:24 -0400

    Bug 1210077 - Null check gNeckoChild in HttpChannelChild::ResetInterception(); r=mcmanus

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -2242,6 +2242,8 @@ HttpChannelChild::DivertToParent(ChannelDiverterChild **aChild)
 void
 HttpChannelChild::ResetInterception()
 {
+  NS_ENSURE_TRUE_VOID(gNeckoChild != nullptr);
+
   if (mInterceptListener) {
     mInterceptListener->Cleanup();
   }
