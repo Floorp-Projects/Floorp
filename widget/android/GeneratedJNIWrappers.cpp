453c13945d7659d4c2908fb735656abcce67112a
Wes Kocher <wkocher@mozilla.com>
2015-10-07 09:05:01 -0700

    Backed out 2 changesets (bug 1200426) for android tpn bustage
    Backed out changeset 4dcc2fb45208 (bug 1200426)
    Backed out changeset 2dea192c7f33 (bug 1200426)

--- widget/android/GeneratedJNIWrappers.cpp
+++ widget/android/GeneratedJNIWrappers.cpp
@@ -985,9 +985,6 @@ constexpr char GeckoView::Window::DisposeNative_t::signature[];
 constexpr char GeckoView::Window::Open_t::name[];
 constexpr char GeckoView::Window::Open_t::signature[];
 
-constexpr char GeckoView::Window::SetLayerClient_t::name[];
-constexpr char GeckoView::Window::SetLayerClient_t::signature[];
-
 constexpr char PrefsHelper::name[];
 
 constexpr char PrefsHelper::GetPrefsById_t::name[];
