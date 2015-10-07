3e461adc0ad2b8e242487a2896101c18d2bb2c28
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-10-07 12:17:02 +0200

    Backed out changeset 79746e93020a (bug 1011020) for causing bug 1212267

--- layout/mathml/nsMathMLFrame.cpp
+++ layout/mathml/nsMathMLFrame.cpp
@@ -363,10 +363,9 @@ void nsDisplayMathMLBar::Paint(nsDisplayListBuilder* aBuilder,
 {
   // paint the bar with the current text color
   DrawTarget* drawTarget = aCtx->GetDrawTarget();
-  Rect rect =
-    NSRectToNonEmptySnappedRect(mRect + ToReferenceFrame(),
-                                mFrame->PresContext()->AppUnitsPerDevPixel(),
-                                *drawTarget);
+  Rect rect = NSRectToSnappedRect(mRect + ToReferenceFrame(),
+                                  mFrame->PresContext()->AppUnitsPerDevPixel(),
+                                  *drawTarget);
   ColorPattern color(ToDeviceColor(
                        mFrame->GetVisitedDependentColor(eCSSProperty_color)));
   drawTarget->FillRect(rect, color);
