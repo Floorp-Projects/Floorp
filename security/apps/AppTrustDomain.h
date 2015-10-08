91d4b45734c833fd11ab484c8a2a00e8c11df102
Jonathan Hao <jhao@mozilla.com>
2015-10-08 17:08:45 +0800

    Bug 1178448 - Use imported CA in developer mode. r=keeler,valentin

--- security/apps/AppTrustDomain.h
+++ security/apps/AppTrustDomain.h
@@ -8,6 +8,8 @@
 #define mozilla_psm_AppsTrustDomain_h
 
 #include "pkix/pkixtypes.h"
+#include "mozilla/StaticMutex.h"
+#include "nsAutoPtr.h"
 #include "nsDebug.h"
 #include "nsIX509CertDB.h"
 #include "ScopedNSSTypes.h"
@@ -69,6 +71,10 @@ private:
   void* mPinArg; // non-owning!
   ScopedCERTCertificate mTrustedRoot;
   unsigned int mMinRSABits;
+
+  static StaticMutex sMutex;
+  static nsAutoArrayPtr<unsigned char> sDevImportedDERData;
+  static unsigned int sDevImportedDERLen;
 };
 
 } } // namespace mozilla::psm
