e54df0718ba4faf64b9d97045c687bc44dc2cada
Jon Coppeard <jcoppeard@mozilla.com>
2015-10-08 17:48:53 +0100

    Bug 1212343 - Propagate OOM from SafepointWriter r=nbp

--- js/src/jit/CodeGenerator.cpp
+++ js/src/jit/CodeGenerator.cpp
@@ -8024,7 +8024,8 @@ CodeGenerator::link(JSContext* cx, CompilerConstraintList* constraints)
                            : FrameSizeClass::FromDepth(frameDepth_).frameSize();
 
     // We encode safepoints after the OSI-point offsets have been determined.
-    encodeSafepoints();
+    if (!encodeSafepoints())
+        return false;
 
     AutoDiscardIonCode discardIonCode(cx, &recompileInfo);
 
