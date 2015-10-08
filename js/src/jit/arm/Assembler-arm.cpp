c908c030758a56271dd3eb0a2559e7f16e6740d2
Wes Kocher <wkocher@mozilla.com>
2015-10-08 10:15:38 -0700

    Backed out changeset a4e0f0475b78 (bug 1212258) for SM(e) orange

--- js/src/jit/arm/Assembler-arm.cpp
+++ js/src/jit/arm/Assembler-arm.cpp
@@ -2416,11 +2416,6 @@ Assembler::as_b(Label* l, Condition c)
         BOffImm inv;
         ret = as_b(inv, c, l);
     }
-
-    // We might oom while adding more instructions.
-    if (!ret.assigned())
-        return ret;
-
     DebugOnly<int32_t> check = l->use(ret.getOffset());
     MOZ_ASSERT(check == old);
     return ret;
