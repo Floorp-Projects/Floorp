c05389afcc9d44bf1c3b0747a897f20012cbef57
Jim Chen <nchen@mozilla.com>
2015-10-08 01:12:57 -0400

    Bug 1200426 - Convert PROCESS_OBJECT GeckoEvent to native call; r=snorp The PROCESS_OBJECT GeckoEvent is used to set the layer client object in Gecko once Gecko is done loading. This patch converts it to a native call in GeckoView.Window.

--- widget/android/GeneratedJNIWrappers.cpp
+++ widget/android/GeneratedJNIWrappers.cpp
@@ -985,6 +985,9 @@ constexpr char GeckoView::Window::DisposeNative_t::signature[];
 constexpr char GeckoView::Window::Open_t::name[];
 constexpr char GeckoView::Window::Open_t::signature[];
 
+constexpr char GeckoView::Window::SetLayerClient_t::name[];
+constexpr char GeckoView::Window::SetLayerClient_t::signature[];
+
 constexpr char PrefsHelper::name[];
 
 constexpr char PrefsHelper::GetPrefsById_t::name[];
