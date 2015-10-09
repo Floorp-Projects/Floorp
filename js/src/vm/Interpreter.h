3dbb3a51aae35c98f4db3ce161cc5dd007ce8fdb
Eric Faust <efaustbmo@gmail.com>
2015-10-09 09:33:57 -0700

    Bug 1105463 - Implement default constructors for ES6 class definitions. (r=jorendorff)

--- js/src/vm/Interpreter.h
+++ js/src/vm/Interpreter.h
@@ -488,6 +488,12 @@ ReportRuntimeRedeclaration(JSContext* cx, HandlePropertyName name,
 bool
 ThrowUninitializedThis(JSContext* cx, AbstractFramePtr frame);
 
+bool
+DefaultClassConstructor(JSContext* cx, unsigned argc, Value* vp);
+
+bool
+DefaultDerivedClassConstructor(JSContext* cx, unsigned argc, Value* vp);
+
 }  /* namespace js */
 
 #endif /* vm_Interpreter_h */
