879ab27be51574e6de1d23fd864180960150bd74
Neil Deakin <neil@mozilla.com>
2015-10-09 08:50:13 -0400

    Bug 1173330, fix to use content tasks and BrowserTestUtils, and reenable browser_bug638292.js, r=felipe

--- toolkit/mozapps/extensions/test/xpinstall/browser_bug638292.js
+++ toolkit/mozapps/extensions/test/xpinstall/browser_bug638292.js
@@ -1,63 +1,40 @@
 // ----------------------------------------------------------------------------
 // Test whether an InstallTrigger.enabled is working
-function test() {
-  waitForExplicitFinish();
+add_task(function * ()
+{
+  let testtab = yield BrowserTestUtils.openNewForegroundTab(gBrowser, TESTROOT + "bug638292.html");
 
-  gBrowser.selectedTab = gBrowser.addTab();
-  gBrowser.selectedBrowser.addEventListener("load", function() {
-    gBrowser.selectedBrowser.removeEventListener("load", arguments.callee, true);
-    waitForFocus(page_loaded, gBrowser.contentWindow);
-  }, true);
-  gBrowser.loadURI(TESTROOT + "bug638292.html");
-}
+  function* verify(link, button)
+  {
+    info("Clicking " + link);
 
-function check_load(aCallback) {
-  gBrowser.addEventListener("load", function(aEvent) {
-    if (!gBrowser.browsers[2] ||
-        aEvent.target != gBrowser.browsers[2].contentDocument) {
-      // SeaMonkey tabbrowser needs to deal with additional loads.
-      if (navigator.userAgent.match(/ SeaMonkey\//))
-        info("Ignoring unrelated load on SeaMonkey. (Expected 2-3 times.)");
-      else
-        ok(false, "Ignoring unrelated load on Firefox. (Should never happen!)");
-      return;
+    let waitForNewTabPromise = BrowserTestUtils.waitForNewTab(gBrowser);
+
+    yield BrowserTestUtils.synthesizeMouseAtCenter("#" + link, { button: button },
+                                                   gBrowser.selectedBrowser);
+
+    let newtab = yield waitForNewTabPromise;
+
+    yield BrowserTestUtils.browserLoaded(newtab.linkedBrowser);
+
+    let result = yield ContentTask.spawn(newtab.linkedBrowser, { }, function* () {
+      return (content.document.getElementById("enabled").textContent == "true");
+    });
+
+    ok(result, "installTrigger for " + link + " should have been enabled");
+
+    // Focus the old tab (link3 is opened in the background)
+    if (link != "link3") {
+      yield BrowserTestUtils.switchTab(gBrowser, testtab);
     }
+    gBrowser.removeTab(newtab);
+  }
 
-    gBrowser.removeEventListener("load", arguments.callee, true);
+  yield* verify("link1", 0);
+  yield* verify("link2", 0);
+  yield* verify("link3", 1);
 
-    // Let the load handler complete
-    executeSoon(function() {
-      var doc = gBrowser.browsers[2].contentDocument;
-      is(doc.getElementById("enabled").textContent, "true", "installTrigger should have been enabled");
+  gBrowser.removeCurrentTab();
+});
 
-      // Focus the old tab
-      gBrowser.selectedTab = gBrowser.tabs[1];
-      waitForFocus(function() {
-        // Close the new tab
-        gBrowser.removeTab(gBrowser.tabs[2]);
-        aCallback();
-      }, gBrowser.contentWindow);
-    });
-  }, true);
-}
 
-function page_loaded() {
-  var doc = gBrowser.contentDocument;
-  info("Clicking link 1");
-  EventUtils.synthesizeMouseAtCenter(doc.getElementById("link1"), { }, gBrowser.contentWindow);
-
-  check_load(function() {
-    info("Clicking link 2");
-    EventUtils.synthesizeMouseAtCenter(doc.getElementById("link2"), { }, gBrowser.contentWindow);
-
-    check_load(function() {
-      info("Clicking link 3");
-      EventUtils.synthesizeMouseAtCenter(doc.getElementById("link3"), { button: 1 }, gBrowser.contentWindow);
-
-      check_load(function() {
-        gBrowser.removeCurrentTab();
-        finish();
-      });
-    });
-  });
-}
