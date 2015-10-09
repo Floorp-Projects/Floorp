7b48983873eb0621c168cdb759aeb512c84a805b
Gabor Krizsanits <gkrizsanits>
2015-10-09 06:06:00 +0200

    Bug 1196975 - part3: GetTabForContentWindowNoShim. r=billm

--- addon-sdk/source/lib/sdk/tabs/utils.js
+++ addon-sdk/source/lib/sdk/tabs/utils.js
@@ -11,10 +11,11 @@ module.metadata = {
 // NOTE: This file should only deal with xul/native tabs
 
 
-const { Ci } = require('chrome');
+const { Ci, Cu } = require('chrome');
 const { defer } = require("../lang/functional");
 const { windows, isBrowser } = require('../window/utils');
 const { isPrivateBrowsingSupported } = require('../self');
+const { ShimWaiver } = Cu.import("resource://gre/modules/ShimWaiver.jsm");
 
 // Bug 834961: ignore private windows when they are not supported
 function getWindows() {
@@ -261,6 +262,16 @@ function getTabForContentWindow(window) {
   return getTabs().find(tab => getTabContentWindow(tab) === window.top) || null;
 }
 exports.getTabForContentWindow = getTabForContentWindow;
+
+// only sdk/selection.js is relying on shims
+function getTabForContentWindowNoShim(window) {
+  function getTabContentWindowNoShim(tab) {
+    let browser = getBrowserForTab(tab);
+    return ShimWaiver.getProperty(browser, "contentWindow");
+  }
+  return getTabs().find(tab => getTabContentWindowNoShim(tab) === window.top) || null;
+}
+exports.getTabForContentWindowNoShim = getTabForContentWindowNoShim;
 
 function getTabURL(tab) {
   return String(getBrowserForTab(tab).currentURI.spec);
