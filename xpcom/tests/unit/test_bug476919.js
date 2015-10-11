00b0491dd072512c68e698d94bba7e6d5b6e9b32
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2015-10-11 21:48:00 +0200

    Bug 1167627 - Part 3: Use mozinfo in xpcom/. r=bsmedberg

--- xpcom/tests/unit/test_bug476919.js
+++ xpcom/tests/unit/test_bug476919.js
@@ -3,8 +3,7 @@ var Ci = Components.interfaces;
 
 function run_test() {
   // skip this test on Windows
-  var isWindows = ("@mozilla.org/windows-registry-key;1" in Components.classes);
-  if (!isWindows) {
+  if (mozinfo.os != "win") {
     var testDir = __LOCATION__.parent;
     // create a test file, then symlink it, then check that we think it's a symlink
     var targetFile = testDir.clone();
