a02161d4e3f927f910dce54d0c8204b0d141f3de
Heiher <r@hev.cc>
2015-10-12 04:46:03 +0800

    Bug 1210322 - IonMonkey: MIPS: Rename BaseFloatRegister/s to FloatRegister/sMIPSShared. r=nbp
    ---
     js/src/jit/mips-shared/Architecture-mips-shared.h | 6 +++---
     js/src/jit/mips32/Architecture-mips32.h           | 4 ++--
     js/src/jit/mips64/Architecture-mips64.h           | 6 +++---
     3 files changed, 8 insertions(+), 8 deletions(-)

--- js/src/jit/mips64/Architecture-mips64.h
+++ js/src/jit/mips64/Architecture-mips64.h
@@ -41,7 +41,7 @@ static const uint32_t ShadowStackSpace = 0;
 
 // MIPS64 have 64 bit floating-point coprocessor. There are 32 double
 // precision register which can also be used as single precision registers.
-class FloatRegisters : public BaseFloatRegisters
+class FloatRegisters : public FloatRegistersMIPSShared
 {
   public:
     enum ContentType {
@@ -52,7 +52,7 @@ class FloatRegisters : public BaseFloatRegisters
 
     static const char* GetName(uint32_t i) {
         MOZ_ASSERT(i < TotalPhys);
-        return BaseFloatRegisters::GetName(Encoding(i));
+        return FloatRegistersMIPSShared::GetName(Encoding(i));
     }
 
     static Encoding FromName(const char* name);
@@ -109,7 +109,7 @@ class FloatRegisters : public BaseFloatRegisters
 template <typename T>
 class TypedRegisterSet;
 
-class FloatRegister : public BaseFloatRegister
+class FloatRegister : public FloatRegisterMIPSShared
 {
   public:
     typedef FloatRegisters Codes;
