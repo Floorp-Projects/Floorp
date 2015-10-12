7924ef22c9460cf1911ecc4eaf2e379c5dc936ff
David Rajchenbach-Teller <dteller@mozilla.com>
2015-10-12 09:30:23 +0200

    Bug 1213672 - Turning onWindowOpened into a variable; r=mak
    --HG--
    extra : amend_source : 4749a0f13895765ec3431c51405483543742fafc

--- browser/base/content/sanitize.js
+++ browser/base/content/sanitize.js
@@ -635,8 +635,9 @@ Sanitizer.prototype = {
         let newWindow = existingWindow.openDialog("chrome://browser/content/", "_blank",
                                                   features, defaultArgs);
 
+        let onFullScreen = null;
         if (AppConstants.platform == "macosx") {
-          let onFullScreen = function(e) {
+          onFullScreen = function(e) {
             newWindow.removeEventListener("fullscreen", onFullScreen);
             let docEl = newWindow.document.documentElement;
             let sizemode = docEl.getAttribute("sizemode");
@@ -658,7 +659,7 @@ Sanitizer.prototype = {
           // closes) and/or run too late (and not have a fully-formed window yet
           // in existence). See bug 1088137.
           let newWindowOpened = false;
-          function onWindowOpened(subject, topic, data) {
+          let onWindowOpened = function(subject, topic, data) {
             if (subject != newWindow)
               return;
 
@@ -675,7 +676,7 @@ Sanitizer.prototype = {
           }
 
           let numWindowsClosing = windowList.length;
-          function onWindowClosed() {
+          let onWindowClosed = function() {
             numWindowsClosing--;
             if (numWindowsClosing == 0) {
               Services.obs.removeObserver(onWindowClosed, "xul-window-destroyed");
@@ -686,9 +687,9 @@ Sanitizer.prototype = {
               }
             }
           }
+          Services.obs.addObserver(onWindowOpened, "browser-delayed-startup-finished", false);
+          Services.obs.addObserver(onWindowClosed, "xul-window-destroyed", false);
         });
-        Services.obs.addObserver(onWindowOpened, "browser-delayed-startup-finished", false);
-        Services.obs.addObserver(onWindowClosed, "xul-window-destroyed", false);
 
         // Start the process of closing windows
         while (windowList.length) {
