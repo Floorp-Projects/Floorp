66f2f4340e8f5f27811eda8609b39c316ed81a23
Patrick Brosset <pbrosset@mozilla.com>
2015-10-12 11:39:21 +0200

    Bug 1205681 - 3 - Tests for the timeline rewind button; r=tromey

--- devtools/client/animationinspector/test/browser_animation_timeline_pause_button.js
+++ devtools/client/animationinspector/test/browser_animation_timeline_pause_button.js
@@ -20,48 +20,16 @@ add_task(function*() {
      "The play/pause button is in its playing state");
 
   info("Click on the button to pause all timeline animations");
-  yield clickPlayPauseButton(panel);
+  yield clickTimelinePlayPauseButton(panel);
 
   ok(btn.classList.contains("paused"),
      "The play/pause button is in its paused state");
-  yield checkIfScrubberMoving(panel, false);
+  yield assertScrubberMoving(panel, false);
 
   info("Click again on the button to play all timeline animations");
-  yield clickPlayPauseButton(panel);
+  yield clickTimelinePlayPauseButton(panel);
 
   ok(!btn.classList.contains("paused"),
      "The play/pause button is in its playing state again");
-  yield checkIfScrubberMoving(panel, true);
+  yield assertScrubberMoving(panel, true);
 });
-
-function* clickPlayPauseButton(panel) {
-  let onUiUpdated = panel.once(panel.UI_UPDATED_EVENT);
-
-  let btn = panel.playTimelineButtonEl;
-  let win = btn.ownerDocument.defaultView;
-  EventUtils.sendMouseEvent({type: "click"}, btn, win);
-
-  yield onUiUpdated;
-  yield waitForAllAnimationTargets(panel);
-}
-
-function* checkIfScrubberMoving(panel, isMoving) {
-  let timeline = panel.animationsTimelineComponent;
-  let scrubberEl = timeline.scrubberEl;
-
-  if (isMoving) {
-    // If we expect the scrubber to move, just wait for a couple of
-    // timeline-data-changed events and compare times.
-    let {time: time1} = yield timeline.once("timeline-data-changed");
-    let {time: time2} = yield timeline.once("timeline-data-changed");
-    ok(time2 > time1, "The scrubber is moving");
-  } else {
-    // If instead we expect the scrubber to remain at its position, just wait
-    // for some time. A relatively long timeout is used because the test page
-    // has long running animations, so the scrubber doesn't move that quickly.
-    let startOffset = scrubberEl.offsetLeft;
-    yield new Promise(r => setTimeout(r, 2000));
-    let endOffset = scrubberEl.offsetLeft;
-    is(startOffset, endOffset, "The scrubber is not moving");
-  }
-}
