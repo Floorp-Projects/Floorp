d3549352799c10b2cdb8550cf8f3898a00305ab5
Mats Palmgren <mats@mozilla.com>
2015-10-12 13:52:56 +0200

    Bug 1212688 - Drill through any <optgroup> scroll frames to get at its <option> child frames when estimating the list's row size.  r=roc

--- layout/forms/nsListControlFrame.cpp
+++ layout/forms/nsListControlFrame.cpp
@@ -270,8 +270,8 @@ GetMaxOptionBSize(nsIFrame* aContainer, WritingMode aWM)
     nscoord optionBSize;
     if (nsCOMPtr<nsIDOMHTMLOptGroupElement>
         (do_QueryInterface(option->GetContent()))) {
-      // an optgroup
-      optionBSize = GetMaxOptionBSize(option, aWM);
+      // An optgroup; drill through any scroll frame and recurse.
+      optionBSize = GetMaxOptionBSize(option->GetContentInsertionFrame(), aWM);
     } else {
       // an option
       optionBSize = option->BSize(aWM);
