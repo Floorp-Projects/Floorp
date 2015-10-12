4a1f8d01b553605682561a45d10cc2cb22cc99de
Heiher <r@hev.cc>
2015-10-12 17:05:02 +0800

    Bug 1213532 - IonMonkey: MIPS32: Fix call FloatRegistersMIPSShared::GetName recursive. r=arai
    ---
     js/src/jit/mips32/Architecture-mips32.h | 2 +-
     1 file changed, 1 insertion(+), 1 deletion(-)

--- js/src/jit/mips32/Architecture-mips32.h
+++ js/src/jit/mips32/Architecture-mips32.h
@@ -64,7 +64,7 @@ class FloatRegisters : public FloatRegistersMIPSShared
   public:
     static const char* GetName(uint32_t i) {
         MOZ_ASSERT(i < Total);
-        return GetName(Code(i % 32));
+        return BaseFloatRegisters::GetName(Code(i % 32));
     }
 
     static Code FromName(const char* name);
