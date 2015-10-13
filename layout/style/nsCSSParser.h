9b82cb15843923ae2cd6ad8f3bff0da8240f17b6
Zack Weinberg <zackw@panix.com>
2015-10-13 17:43:16 -0400

    Bug 1035091 part 1: change CSS parser and loader APIs to distinguish UA, user, and author sheets instead of just UA vs everyone else. r=heycam
    --HG--
    extra : rebase_source : a8a5a27db01306d1de58974ebb4d06d29d4b72b0

--- layout/style/nsCSSParser.h
+++ layout/style/nsCSSParser.h
@@ -9,6 +9,7 @@
 #define nsCSSParser_h___
 
 #include "mozilla/Attributes.h"
+#include "mozilla/css/Loader.h"
 
 #include "nsCSSProperty.h"
 #include "nsCSSScanner.h"
@@ -32,8 +33,6 @@ class CSSVariableValues;
 namespace css {
 class Rule;
 class Declaration;
-class Loader;
-class LoaderReusableStyleSheets;
 class StyleRule;
 } // namespace css
 } // namespace mozilla
@@ -78,8 +77,7 @@ public:
    * @param aSheetPrincipal the principal of the stylesheet.  This must match
    *                        the principal of the sheet passed to SetStyleSheet.
    * @param aLineNumber the line number of the first line of the sheet.
-   * @param aAllowUnsafeRules see aEnableUnsafeRules in
-   *                          mozilla::css::Loader::LoadSheetSync
+   * @param aParsingMode  see SheetParsingMode in css/Loader.h
    * @param aReusableSheets style sheets that can be reused by an @import.
    *                        This can be nullptr.
    */
@@ -88,7 +86,7 @@ public:
                       nsIURI*          aBaseURI,
                       nsIPrincipal*    aSheetPrincipal,
                       uint32_t         aLineNumber,
-                      bool             aAllowUnsafeRules,
+                      mozilla::css::SheetParsingMode aParsingMode,
                       mozilla::css::LoaderReusableStyleSheets* aReusableSheets =
                         nullptr);
 
