8abc0664d2a7c06cc47fed08442e469b2c10ea2a
Blake Kaplan <mrbkap@gmail.com>
2015-10-14 13:41:26 -0700

    Bug 1165558 - Return valid dispositions even if there wasn't a header. r=bz

--- uriloader/exthandler/ExternalHelperAppParent.cpp
+++ uriloader/exthandler/ExternalHelperAppParent.cpp
@@ -396,7 +396,9 @@ ExternalHelperAppParent::SetContentCharset(const nsACString& aContentCharset)
 NS_IMETHODIMP
 ExternalHelperAppParent::GetContentDisposition(uint32_t *aContentDisposition)
 {
-  if (mContentDispositionHeader.IsEmpty())
+  // NB: mContentDisposition may or may not be set to a non UINT32_MAX value in
+  // nsExternalHelperAppService::DoContentContentProcessHelper
+  if (mContentDispositionHeader.IsEmpty() && mContentDisposition == UINT32_MAX)
     return NS_ERROR_NOT_AVAILABLE;
 
   *aContentDisposition = mContentDisposition;
