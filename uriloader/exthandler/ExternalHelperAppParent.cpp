7248df22951bc78398ce1d93d7b6d5d0ad56b30b
Phil Ringnalda <philringnalda@gmail.com>
2015-10-13 21:36:10 -0700

    Back out 5 changesets (bug 1159385, bug 1165558) for making browser_pdfjs_zoom.js frequently leak on Windows CLOSED TREE
    Backed out changeset 9c0a6b934c7a (bug 1159385)
    Backed out changeset 1dcf1d5a95ce (bug 1159385)
    Backed out changeset 50b20aa3fd46 (bug 1159385)
    Backed out changeset 863379c3fe31 (bug 1159385)
    Backed out changeset 8decc8f8fcb1 (bug 1165558)

--- uriloader/exthandler/ExternalHelperAppParent.cpp
+++ uriloader/exthandler/ExternalHelperAppParent.cpp
@@ -396,9 +396,7 @@ ExternalHelperAppParent::SetContentCharset(const nsACString& aContentCharset)
 NS_IMETHODIMP
 ExternalHelperAppParent::GetContentDisposition(uint32_t *aContentDisposition)
 {
-  // NB: mContentDisposition may or may not be set to a non UINT32_MAX value in
-  // nsExternalHelperAppService::DoContentContentProcessHelper
-  if (mContentDispositionHeader.IsEmpty() && mContentDisposition == UINT32_MAX)
+  if (mContentDispositionHeader.IsEmpty())
     return NS_ERROR_NOT_AVAILABLE;
 
   *aContentDisposition = mContentDisposition;
