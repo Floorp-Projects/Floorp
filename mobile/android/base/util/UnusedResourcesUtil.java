5e3c101647eb2226bb44caeacde715178509c2b4
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-10-14 15:51:14 +0200

    Backed out changeset 308e28cb1d94 (bug 1208410) for permanent rc1 test failures

--- mobile/android/base/util/UnusedResourcesUtil.java
+++ mobile/android/base/util/UnusedResourcesUtil.java
@@ -53,6 +53,7 @@ final class UnusedResourcesUtil {
 
     public static final int[] USED_IN_SUGGESTEDSITES = {
             R.drawable.suggestedsites_fxaddons,
+            R.drawable.suggestedsites_fxmarketplace,
             R.drawable.suggestedsites_fxsupport,
             R.drawable.suggestedsites_mozilla,
     };
