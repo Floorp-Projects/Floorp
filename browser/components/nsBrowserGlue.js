0c7dd07bd3ff9add3f85edc18a03a438ef428d2d
Fabrice Desré <fabrice@mozilla.com>
2015-10-14 16:31:08 -0700

    Bug 1214021 - Move ext-bookmarks to browser/ r=billm

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -645,6 +645,7 @@ BrowserGlue.prototype = {
     ExtensionManagement.registerScript("chrome://browser/content/ext-contextMenus.js");
     ExtensionManagement.registerScript("chrome://browser/content/ext-tabs.js");
     ExtensionManagement.registerScript("chrome://browser/content/ext-windows.js");
+    ExtensionManagement.registerScript("chrome://browser/content/ext-bookmarks.js");
 
     this._flashHangCount = 0;
   },
