56988b2afb1f7118c3dfd8c12f6e61ecb2e79361
Eric Rahm <erahm@mozilla.com>
2015-10-14 16:52:59 -0700

    Bug 1194555 - Part 6: Run reporters asynchronously. r=njn,jld,ted

--- toolkit/crashreporter/test/unit/crasher_subprocess_tail.js
+++ toolkit/crashreporter/test/unit/crasher_subprocess_tail.js
@@ -1,2 +1,15 @@
+// Let the event loop process a bit before crashing.
+if (shouldDelay) {
+  let shouldCrashNow = false;
+  let thr = Components.classes["@mozilla.org/thread-manager;1"]
+                          .getService().currentThread;
+  thr.dispatch({ run: () => { shouldCrashNow = true; } },
+               Components.interfaces.nsIThread.DISPATCH_NORMAL);
+
+  while (!shouldCrashNow) {
+    thr.processNextEvent(true);
+  }
+}
+
 // now actually crash
 CrashTestUtils.crash(crashType);
