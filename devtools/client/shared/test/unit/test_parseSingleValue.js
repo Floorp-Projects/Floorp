8b0410f69ebe506c01e8e444df565abb9ad3baf1
J. Ryan Stinnett <jryans@gmail.com>
2015-10-14 16:40:08 -0500

    Bug 1203159 - Clean up various incorrect paths. r=ochameau

--- devtools/client/shared/test/unit/test_parseSingleValue.js
+++ devtools/client/shared/test/unit/test_parseSingleValue.js
@@ -6,7 +6,7 @@
 "use strict";
 
 var Cu = Components.utils;
-const {require} = Cu.import("resource://devtools/Loader.jsm", {});
+const {require} = Cu.import("resource://devtools/shared/Loader.jsm", {});
 const {parseSingleValue} = require("devtools/client/shared/css-parsing-utils");
 
 const TEST_DATA = [
