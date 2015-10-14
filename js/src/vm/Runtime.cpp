f5b9c58d74660a0f543815bb0676af8a0932f4f0
Jan de Mooij <jdemooij@mozilla.com>
2015-10-14 15:13:21 +0200

    Bug 1208819 - Fix irregexp overrecursion check to ignore interrupts. r=bhackett

--- js/src/vm/Runtime.cpp
+++ js/src/vm/Runtime.cpp
@@ -130,6 +130,7 @@ JSRuntime::JSRuntime(JSRuntime* parentRuntime)
     jitJSContext(nullptr),
     jitActivation(nullptr),
     jitStackLimit_(0xbad),
+    jitStackLimitNoInterrupt_(0xbad),
     activation_(nullptr),
     profilingActivation_(nullptr),
     profilerSampleBufferGen_(0),
@@ -623,6 +624,7 @@ JSRuntime::resetJitStackLimit()
 #else
     jitStackLimit_ = mainThread.nativeStackLimit[StackForUntrustedScript];
 #endif
+    jitStackLimitNoInterrupt_ = jitStackLimit_;
 }
 
 void
