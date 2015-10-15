672513261008645f7b8103d5a3627fc4df3d525d
Nicolas Silva <nsilva@mozilla.com>
2015-10-15 17:53:33 +0200

    Bug 1200595 - Buffer TextureData implementation. r=mattwoodrow

--- gfx/layers/ImageDataSerializer.h
+++ gfx/layers/ImageDataSerializer.h
@@ -39,6 +39,8 @@ public:
   static uint32_t ComputeMinBufferSize(gfx::IntSize aSize,
                                        gfx::SurfaceFormat aFormat);
 
+  size_t GetBufferSize() const { return mDataSize; }
+
 protected:
 
   ImageDataSerializerBase(uint8_t* aData, size_t aDataSize)
--- gfx/layers/ipc/SharedRGBImage.cpp
+++ gfx/layers/ipc/SharedRGBImage.cpp
@@ -8,9 +8,9 @@
 #include "gfx2DGlue.h"                  // for ImageFormatToSurfaceFormat, etc
 #include "gfxPlatform.h"                // for gfxPlatform, gfxImageFormat
 #include "mozilla/gfx/Point.h"          // for IntSIze
+#include "mozilla/layers/BufferTexture.h"
 #include "mozilla/layers/ISurfaceAllocator.h"  // for ISurfaceAllocator, etc
 #include "mozilla/layers/ImageClient.h"  // for ImageClient
-#include "mozilla/layers/ImageDataSerializer.h"  // for ImageDataSerializer
 #include "mozilla/layers/LayersSurfaces.h"  // for SurfaceDescriptor, etc
 #include "mozilla/layers/TextureClient.h"  // for BufferTextureClient, etc
 #include "mozilla/layers/ImageBridgeChild.h"  // for ImageBridgeChild
@@ -87,25 +87,17 @@ SharedRGBImage::Allocate(gfx::IntSize aSize, gfx::SurfaceFormat aFormat)
 uint8_t*
 SharedRGBImage::GetBuffer()
 {
-  if (!mTextureClient) {
-    return nullptr;
+  MappedTextureData mapped;
+  if (mTextureClient && mTextureClient->BorrowMappedData(mapped)) {
+    return mapped.data;
   }
-
-  ImageDataSerializer serializer(mTextureClient->GetBuffer(), mTextureClient->GetBufferSize());
-  return serializer.GetData();
+  return 0;
 }
 
 gfx::IntSize
 SharedRGBImage::GetSize()
 {
   return mSize;
-}
-
-size_t
-SharedRGBImage::GetBufferSize()
-{
-  return mTextureClient ? mTextureClient->GetBufferSize()
-                        : 0;
 }
 
 TextureClient*
