89cb263b54c0d2f5c3fc91514671db3490b4582e
Alexandre Poirot <poirot.alex@gmail.com>
2015-10-15 03:45:22 -0700

    Bug 1204812 - Keep Console.jsm in toolkit/modules/ r=jryans,Mossop
    --HG--
    rename : devtools/shared/Console.jsm => toolkit/modules/Console.jsm

--- devtools/shared/webconsole/test/common.js
+++ devtools/shared/webconsole/test/common.js
@@ -11,7 +11,7 @@ Cu.import("resource://gre/modules/Services.jsm");
 const {Task} = Cu.import("resource://gre/modules/Task.jsm", {});
 
 // This gives logging to stdout for tests
-var {console} = Cu.import("resource://gre/modules/devtools/shared/Console.jsm", {});
+var {console} = Cu.import("resource://gre/modules/Console.jsm", {});
 
 var {require} = Cu.import("resource://gre/modules/devtools/shared/Loader.jsm", {});
 var WebConsoleUtils = require("devtools/shared/webconsole/utils").Utils;
