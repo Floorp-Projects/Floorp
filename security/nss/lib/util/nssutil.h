87906e730382f35879800a36d90c74c5e9377d1d
Kai Engert <kaie@kuix.de>
2015-10-16 22:57:29 +0200

    Bug 1211586, NSPR_4_10_10_RC1 and NSS_3_19_4_RC0, a=sledru

--- security/nss/lib/util/nssutil.h
+++ security/nss/lib/util/nssutil.h
@@ -19,10 +19,10 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <Beta>]"
  */
-#define NSSUTIL_VERSION  "3.19.3"
+#define NSSUTIL_VERSION  "3.19.4"
 #define NSSUTIL_VMAJOR   3
 #define NSSUTIL_VMINOR   19
-#define NSSUTIL_VPATCH   3
+#define NSSUTIL_VPATCH   4
 #define NSSUTIL_VBUILD   0
 #define NSSUTIL_BETA     PR_FALSE
 
