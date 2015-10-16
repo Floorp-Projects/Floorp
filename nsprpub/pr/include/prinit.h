901fd97b93584703e215f1581e442f9f93a2246d
Kai Engert <kaie@kuix.de>
2015-10-16 22:48:40 +0200

    Bug 1211587, NSPR_4_10_10_RC1 and NSS_3_20_1_RC0, a=sledru

--- nsprpub/pr/include/prinit.h
+++ nsprpub/pr/include/prinit.h
@@ -31,10 +31,10 @@ PR_BEGIN_EXTERN_C
 ** The format of the version string is
 **     "<major version>.<minor version>[.<patch level>] [<Beta>]"
 */
-#define PR_VERSION  "4.10.9"
+#define PR_VERSION  "4.10.10"
 #define PR_VMAJOR   4
 #define PR_VMINOR   10
-#define PR_VPATCH   9
+#define PR_VPATCH   10
 #define PR_BETA     PR_FALSE
 
 /*
--- security/nss/lib/softoken/softkver.h
+++ security/nss/lib/softoken/softkver.h
@@ -25,10 +25,10 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define SOFTOKEN_VERSION  "3.20" SOFTOKEN_ECC_STRING 
+#define SOFTOKEN_VERSION  "3.20.1" SOFTOKEN_ECC_STRING
 #define SOFTOKEN_VMAJOR   3
 #define SOFTOKEN_VMINOR   20
-#define SOFTOKEN_VPATCH   0
+#define SOFTOKEN_VPATCH   1
 #define SOFTOKEN_VBUILD   0
 #define SOFTOKEN_BETA     PR_FALSE
 
