0d4794a52d82171a25884fc1ab1d1256eb95c9d0
Phil Ringnalda <philringnalda@gmail.com>
2015-10-16 20:32:21 -0700

    Back out 2 changesets (bug 1215336, bug 1215337) in hopes it will appease the angry gods, who will then stop hurling !phaseStartTimes[phase] assertion failures at our heads CLOSED TREE
    Backed out changeset f41fd6c7d522 (bug 1215337)
    Backed out changeset 1998b272540e (bug 1215336)

--- js/src/gc/Marking.cpp
+++ js/src/gc/Marking.cpp
@@ -2475,11 +2475,18 @@ struct UnmarkGrayTracer : public JS::CallbackTracer
      * We set weakMapAction to DoNotTraceWeakMaps because the cycle collector
      * will fix up any color mismatches involving weakmaps when it runs.
      */
-    explicit UnmarkGrayTracer(JSRuntime *rt, bool tracingShape = false)
-      : JS::CallbackTracer(rt, DoNotTraceWeakMaps)
-      , tracingShape(tracingShape)
-      , previousShape(nullptr)
-      , unmarkedAny(false)
+    explicit UnmarkGrayTracer(JSRuntime* rt)
+      : JS::CallbackTracer(rt, DoNotTraceWeakMaps),
+        tracingShape(false),
+        previousShape(nullptr),
+        unmarkedAny(false)
+    {}
+
+    UnmarkGrayTracer(JSTracer* trc, bool tracingShape)
+      : JS::CallbackTracer(trc->runtime(), DoNotTraceWeakMaps),
+        tracingShape(tracingShape),
+        previousShape(nullptr),
+        unmarkedAny(false)
     {}
 
     void onChild(const JS::GCCellPtr& thing) override;
@@ -2565,7 +2572,7 @@ UnmarkGrayTracer::onChild(const JS::GCCellPtr& thing)
     // The parent will later trace |tenured|. This is done to avoid increasing
     // the stack depth during shape tracing. It is safe to do because a shape
     // can only have one child that is a shape.
-    UnmarkGrayTracer childTracer(runtime(), thing.kind() == JS::TraceKind::Shape);
+    UnmarkGrayTracer childTracer(this, thing.kind() == JS::TraceKind::Shape);
 
     if (thing.kind() != JS::TraceKind::Shape) {
         TraceChildren(&childTracer, &tenured, thing.kind());
@@ -2610,8 +2617,6 @@ TypedUnmarkGrayCellRecursively(T* t)
     }
 
     UnmarkGrayTracer trc(rt);
-    gcstats::AutoPhase outerPhase(rt->gc.stats, gcstats::PHASE_BARRIER);
-    gcstats::AutoPhase innerPhase(rt->gc.stats, gcstats::PHASE_UNMARK_GRAY);
     t->traceChildren(&trc);
 
     return unmarkedArg || trc.unmarkedAny;
