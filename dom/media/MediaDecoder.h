42d9b0626207af918de0e42440ba3c821cfe1a41
JW Wang <jwwang@mozilla.com>
2015-10-19 13:55:26 +0800

    Bug 1215023. Part 1 - make MediaDecoder::mOwner a const member. We will check mShuttingDown before calling functions of mOwner. r=kinetik.

--- dom/media/MediaDecoder.h
+++ dom/media/MediaDecoder.h
@@ -868,7 +868,7 @@ protected:
   // This should only ever be accessed from the main thread.
   // It is set in Init and cleared in Shutdown when the element goes away.
   // The decoder does not add a reference the element.
-  MediaDecoderOwner* mOwner;
+  MediaDecoderOwner* const mOwner;
 
   // Counters related to decode and presentation of frames.
   FrameStatistics mFrameStats;
