eaa7a6f79838cb952597c43724f4279e0d345dbc
Cameron McCormack <cam@mcc.id.au>
2015-10-20 10:16:20 +1100

    Bug 1216041 - Add ranged iterator support to mozilla::{Array,RangedArray,EnumerationArray}. r=froydnj

--- mfbt/EnumeratedRange.h
+++ mfbt/EnumeratedRange.h
@@ -195,12 +195,14 @@ MakeEnumeratedRange(EnumType aBegin, EnumType aEnd)
 // Since the enumeration starts at EnumType(0), we know for sure that the values
 // will be in range of our deduced IntType.
 template<typename EnumType>
-inline detail::EnumeratedRange<UnsignedStdintTypeForSize<sizeof(EnumType)>::Type,
-                               EnumType>
+inline detail::EnumeratedRange<
+  typename UnsignedStdintTypeForSize<sizeof(EnumType)>::Type,
+  EnumType>
 MakeEnumeratedRange(EnumType aEnd)
 {
-  return MakeEnumeratedRange<UnsignedStdintTypeForSize<sizeof(EnumType)>::Type>(
-      EnumType(0), aEnd);
+  return MakeEnumeratedRange<
+    typename UnsignedStdintTypeForSize<sizeof(EnumType)>::Type>(EnumType(0),
+                                                                aEnd);
 }
 
 #ifdef __GNUC__
