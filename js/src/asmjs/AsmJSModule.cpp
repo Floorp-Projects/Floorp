c0c6e3dedb1bb65b7452b0e822e30d646276cb92
Benjamin Bouvier <benj@benj.me>
2015-10-19 16:03:12 +0200

    Bug 1181612: Rename AsmJSFunctionLabels to make them easier to understand; r=luke
    --HG--
    extra : commitid : IZMCWrFp9Vl
    extra : rebase_source : c3395e05dc8fd8ac23e305d6458446f60d36c112

--- js/src/asmjs/AsmJSModule.cpp
+++ js/src/asmjs/AsmJSModule.cpp
@@ -1364,19 +1364,19 @@ AsmJSModule::CodeRange::CodeRange(uint32_t nameIndex, uint32_t lineNumber,
                                   const AsmJSFunctionLabels& l)
   : nameIndex_(nameIndex),
     lineNumber_(lineNumber),
-    begin_(l.begin.offset()),
+    begin_(l.profilingEntry.offset()),
     profilingReturn_(l.profilingReturn.offset()),
-    end_(l.end.offset())
+    end_(l.endAfterOOL.offset())
 {
     PodZero(&u);  // zero padding for Valgrind
     u.kind_ = Function;
-    setDeltas(l.entry.offset(), l.profilingJump.offset(), l.profilingEpilogue.offset());
+    setDeltas(l.nonProfilingEntry.offset(), l.profilingJump.offset(), l.profilingEpilogue.offset());
 
-    MOZ_ASSERT(l.begin.offset() < l.entry.offset());
-    MOZ_ASSERT(l.entry.offset() < l.profilingJump.offset());
+    MOZ_ASSERT(l.profilingEntry.offset() < l.nonProfilingEntry.offset());
+    MOZ_ASSERT(l.nonProfilingEntry.offset() < l.profilingJump.offset());
     MOZ_ASSERT(l.profilingJump.offset() < l.profilingEpilogue.offset());
     MOZ_ASSERT(l.profilingEpilogue.offset() < l.profilingReturn.offset());
-    MOZ_ASSERT(l.profilingReturn.offset() < l.end.offset());
+    MOZ_ASSERT(l.profilingReturn.offset() < l.endAfterOOL.offset());
 }
 
 void
