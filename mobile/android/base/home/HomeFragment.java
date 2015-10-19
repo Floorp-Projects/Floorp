bad71aff9d4d42d16140ea329a44b2aacc02a5e8
Mark Finkle <mfinkle@mozilla.com>
2015-10-19 17:42:50 -0400

    Bug 1216265 - Remove any UI telemetry probes that reveal a user has entered private browsing r=margaret

--- mobile/android/base/home/HomeFragment.java
+++ mobile/android/base/home/HomeFragment.java
@@ -36,6 +36,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -179,7 +180,12 @@ public abstract class HomeFragment extends Fragment {
 
         // Track the menu action. We don't know much about the context, but we can use this to determine
         // the frequency of use for various actions.
-        Telemetry.sendUIEvent(TelemetryContract.Event.ACTION, TelemetryContract.Method.CONTEXT_MENU, getResources().getResourceEntryName(itemId));
+        String extras = getResources().getResourceEntryName(itemId);
+        if (TextUtils.equals(extras, "home_open_private_tab")) {
+            // Mask private browsing
+            extras = "home_open_new_tab";
+        }
+        Telemetry.sendUIEvent(TelemetryContract.Event.ACTION, TelemetryContract.Method.CONTEXT_MENU, extras);
 
         if (itemId == R.id.home_copyurl) {
             if (info.url == null) {
