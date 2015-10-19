c16431ed782bf654388b30173c5d7c1c89c69f08
Jan de Mooij <jdemooij@mozilla.com>
2015-10-19 16:37:52 +0200

    Backout ce75fb8d5a8b (bug 1187233) for B2G xpcshell failures.

--- js/src/jsdate.cpp
+++ js/src/jsdate.cpp
@@ -3030,26 +3030,11 @@ DateOneArgument(JSContext* cx, const CallArgs& args)
     MOZ_ASSERT(args.length() == 1);
 
     if (args.isConstructing()) {
-        if (args[0].isObject()) {
-            RootedObject obj(cx, &args[0].toObject());
-
-            ESClassValue cls;
-            if (!GetBuiltinClass(cx, obj, &cls))
-                return false;
-
-            if (cls == ESClass_Date) {
-                RootedValue unboxed(cx);
-                if (!Unbox(cx, obj, &unboxed))
-                    return false;
-
-                return NewDateObject(cx, args, TimeClip(unboxed.toNumber()));
-            }
-        }
+        ClippedTime t;
 
         if (!ToPrimitive(cx, args[0]))
             return false;
 
-        ClippedTime t;
         if (args[0].isString()) {
             JSLinearString* linearStr = args[0].toString()->ensureLinear(cx);
             if (!linearStr)
