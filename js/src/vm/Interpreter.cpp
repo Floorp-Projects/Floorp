021332a53b2b6881fa505d20f0990f909104345a
Jason Orendorff <jorendorff@mozilla.com>
2015-10-19 17:08:28 -0500

    Bug 1217110 - Remove unnecessary opcode JSOP_BINDINTRINSIC. r=shu.
    --HG--
    extra : commitid : KvPvP0Ki9Dv
    extra : rebase_source : 5e789e5a02817b45976313aea0f21b050ce91ff8

--- js/src/vm/Interpreter.cpp
+++ js/src/vm/Interpreter.cpp
@@ -1799,6 +1799,7 @@ CASE(EnableInterruptsPseudoOpcode)
 CASE(JSOP_NOP)
 CASE(JSOP_UNUSED14)
 CASE(JSOP_BACKPATCH)
+CASE(JSOP_UNUSED145)
 CASE(JSOP_UNUSED171)
 CASE(JSOP_UNUSED172)
 CASE(JSOP_UNUSED173)
@@ -2138,16 +2139,6 @@ CASE(JSOP_PICK)
     REGS.sp[-1] = lval;
 }
 END_CASE(JSOP_PICK)
-
-CASE(JSOP_BINDINTRINSIC)
-{
-    NativeObject* holder = GlobalObject::getIntrinsicsHolder(cx, cx->global());
-    if (!holder)
-        goto error;
-
-    PUSH_OBJECT(*holder);
-}
-END_CASE(JSOP_BINDINTRINSIC)
 
 CASE(JSOP_BINDGNAME)
 CASE(JSOP_BINDNAME)
@@ -2572,9 +2563,6 @@ CASE(JSOP_SETINTRINSIC)
 
     if (!SetIntrinsicOperation(cx, script, REGS.pc, value))
         goto error;
-
-    REGS.sp[-2] = REGS.sp[-1];
-    REGS.sp--;
 }
 END_CASE(JSOP_SETINTRINSIC)
 
