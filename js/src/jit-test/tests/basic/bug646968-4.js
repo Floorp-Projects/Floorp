38d16faf668632015cc9b66592d4d5b62918a1e2
Jason Orendorff <jorendorff@mozilla.com>
2015-10-20 11:52:01 -0500

    Bug 1216623 - Part 2: In `for (let ...)` loops, evaluate initializers in the scope of the variables being initialized. r=Waldo.
    --HG--
    extra : commitid : 7hDOQwrCy29
    extra : rebase_source : 83dfe46c9e0109d50601ff3c3b9da2e0df2fe844
    extra : amend_source : 6bdc771ff717ad82f251e2b58236edceeb6b0157

--- js/src/jit-test/tests/basic/bug646968-4.js
+++ js/src/jit-test/tests/basic/bug646968-4.js
@@ -1,4 +1,8 @@
-var s = '', x = {a: 1, b: 2, c: 3};
+// Scoping: `x` in the head of a `for (let x...)` loop refers to the loop variable.
+
+// For now, this means it evaluates to undefined. It ought to throw a
+// ReferenceError instead, but the TDZ isn't implemented here (bug 1069480).
+var s = "", x = {a: 1, b: 2, c: 3};
 for (let x in eval('x'))
     s += x;
-assertEq(s, 'abc');
+assertEq(s, "");
