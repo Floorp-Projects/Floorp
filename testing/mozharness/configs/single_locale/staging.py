d6b6f69ffa9a305606bd8c8df1a0d8849c0a3208
Nick Thomas <nthomas@mozilla.com>
2015-10-21 09:32:56 +1300

    Bug 1213721 - Tracking bug for migration from ftp.m.o to S3, r=rail
    --HG--
    rename : testing/mozharness/configs/multi_locale/mozilla-central_android-x86.json => testing/mozharness/configs/multi_locale/ash_android-x86.json
    rename : testing/mozharness/configs/multi_locale/mozilla-central_android.json => testing/mozharness/configs/multi_locale/ash_android.json
    rename : testing/mozharness/configs/single_locale/mozilla-central_android-api-11.py => testing/mozharness/configs/single_locale/ash_android-api-11.py
    rename : testing/mozharness/configs/single_locale/mozilla-central_android-api-9.py => testing/mozharness/configs/single_locale/ash_android-api-9.py
    extra : transplant_source : %C7%CE%DB%B6%E3%FBFbi%8CX%7B%FA%D9%80%D1%114%85%FF

--- testing/mozharness/configs/single_locale/staging.py
+++ testing/mozharness/configs/single_locale/staging.py
@@ -5,7 +5,7 @@ config = {
         # except when mock is in use, in this case, ssh_key_dir is
         # /home/mock_mozilla/.ssh
         "UPLOAD_SSH_KEY": "%(ssh_key_dir)s/stage-ffxbld_rsa",
-        "UPLOAD_HOST": "dev-stage01.srv.releng.scl3.mozilla.com",
+        "UPLOAD_HOST": "upload.ffxbld.productdelivery.stage.mozaws.net",
         "POST_UPLOAD_CMD": "post_upload.py -b %(branch)s-l10n -p %(stage_product)s -i %(buildid)s --release-to-latest --release-to-dated",
         "UPLOAD_TO_TEMP": "1"
     },
