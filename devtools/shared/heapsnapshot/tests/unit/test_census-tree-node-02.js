9815337bfc2aa5ddbcf7ab119bb31d0d61fb3a23
Nick Fitzgerald <fitzgen@gmail.com>
2015-10-20 10:21:33 -0700

    Bug 1201542 - Implement `allocationSite` breakdown for CensusTreeNode; r=jsantell

--- devtools/shared/heapsnapshot/tests/unit/test_census-tree-node-02.js
+++ devtools/shared/heapsnapshot/tests/unit/test_census-tree-node-02.js
@@ -5,16 +5,13 @@
  * Tests CensusTreeNode with `coarseType` breakdown.
  */
 
-function run_test() {
-  compareCensusViewData(BREAKDOWN, REPORT, EXPECTED, `${JSON.stringify(BREAKDOWN)} has correct results.`);
-}
-
 const countBreakdown = { by: "count", count: true, bytes: true };
 
 const BREAKDOWN = {
   by: "coarseType",
   objects: { by: "objectClass", then: countBreakdown },
   strings: countBreakdown,
+  scripts: countBreakdown,
   other: { by: "internalType", then: countBreakdown },
 };
 
@@ -24,6 +21,7 @@ const REPORT = {
     "Array": { bytes: 20, count: 2 },
   },
   "strings": { bytes: 10, count: 1 },
+  "scripts": { bytes: 1, count: 1 },
   "other": {
     "js::Shape": { bytes: 30, count: 3 },
     "js::Shape2": { bytes: 40, count: 4 }
@@ -31,15 +29,43 @@ const REPORT = {
 };
 
 const EXPECTED = {
+  name: null,
+  bytes: undefined,
+  count: undefined,
   children: [
-    { name: "strings", bytes: 10, count: 1, },
-    { name: "objects", children: [
-      { name: "Array", bytes: 20, count: 2, },
-      { name: "Function", bytes: 10, count: 1, },
-    ]},
-    { name: "other", children: [
-      { name: "js::Shape2", bytes: 40, count: 4, },
-      { name: "js::Shape", bytes: 30, count: 3, },
-    ]},
+    {
+      name: "strings",
+      count: 1,
+      bytes: 10,
+      children: undefined
+    },
+    {
+      name: "scripts",
+      count: 1,
+      bytes: 1,
+      children: undefined
+    },
+    {
+      name: "objects",
+      count: undefined,
+      bytes: undefined,
+      children: [
+        { name: "Array", bytes: 20, count: 2, children: undefined },
+        { name: "Function", bytes: 10, count: 1, children: undefined },
+      ]
+    },
+    {
+      name: "other",
+      count: undefined,
+      bytes: undefined,
+      children: [
+        { name: "js::Shape2", bytes: 40, count: 4, children: undefined },
+        { name: "js::Shape", bytes: 30, count: 3, children: undefined },
+      ]
+    },
   ]
 };
+
+function run_test() {
+  compareCensusViewData(BREAKDOWN, REPORT, EXPECTED);
+}
