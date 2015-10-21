18d13a2ff43fdaade7c0201876699a6e23a4bede
Andreas Tolfsen <ato@mozilla.com>
2015-10-21 14:21:27 +0100

    Bug 1216967: Bump Marionette Python packages
    Prepare release of new Python packages for Marionette, with the following
    versions:
    
      marionette-client 1.1.0
      marionette-driver 1.1.0
      marionette-transport 1.0.0
    
    r=dburns

--- testing/marionette/transport/marionette_transport/__init__.py
+++ testing/marionette/transport/marionette_transport/__init__.py
@@ -2,6 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-__version__ = '0.7.1'
+__version__ = '1.0.0'
 
 from transport import *
