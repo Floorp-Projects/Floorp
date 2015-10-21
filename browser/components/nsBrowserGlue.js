eee83aa5d8bbd0fea691f5bbfbffb0e96b8a8e27
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2015-10-20 18:51:25 -0700

    Bug 1204983 - Deprecate NewTabURL and forward calls to AboutNewTabService (r=florian,olivier)
    --HG--
    rename : browser/components/newtab/tests/xpcshell/test_NewTabURL.js => browser/components/newtab/tests/xpcshell/test_AboutNewTabService.js

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -2492,6 +2492,9 @@ AboutNewTabService.prototype = {
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIAboutNewTabService]),
 
   get newTabURL() {
+    if (Services.prefs.getBoolPref("browser.newtabpage.remote")) {
+      return "about:remote-newtab";
+    }
     return this._newTabURL;
   },
 
