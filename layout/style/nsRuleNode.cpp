1e1200c0468fe5a4007510396065df1c740c700f
L. David Baron <dbaron@dbaron.org>
2015-10-23 08:57:35 +0900

    Bug 1216431 patch 3 - Cache structs that are stored with conditions on the rule node all the time, rather than only when freshly computed.  r=heycam
    This is another case similar to the problem fixed in bug 1209603 patch 9.
    
    This should make things faster by caching structs on the style context
    more reliably.

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -66,6 +66,30 @@ using std::min;
 using namespace mozilla;
 using namespace mozilla::dom;
 
+void*
+nsConditionalResetStyleData::GetConditionalStyleData(nsStyleStructID aSID,
+                               nsStyleContext* aStyleContext) const
+{
+  Entry* e = static_cast<Entry*>(mEntries[aSID]);
+  MOZ_ASSERT(e, "if mConditionalBits bit is set, we must have at least one "
+                "conditional style struct");
+  do {
+    if (e->mConditions.Matches(aStyleContext)) {
+      void* data = e->mStyleStruct;
+
+      // For reset structs with conditions, we cache the data on the
+      // style context.
+      // Tell the style context that it doesn't own the data
+      aStyleContext->AddStyleBit(GetBitForSID(aSID));
+      aStyleContext->SetStyle(aSID, data);
+
+      return data;
+    }
+    e = e->mNext;
+  } while (e);
+  return nullptr;
+}
+
 #define NS_SET_IMAGE_REQUEST(method_, context_, request_)                     \
   if ((context_)->PresContext()->IsDynamic()) {                               \
     method_(request_);                                                        \
