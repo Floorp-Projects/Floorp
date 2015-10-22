2f9536cefef3f370d4f8648db4d446917a712aa4
Cameron McCormack <cam@mcc.id.au>
2015-10-22 19:22:37 +1100

    Bug 1208951 - Part 3: Expose nsCSSValue -> nsTimingFunction computation function. r=birtles

--- layout/style/nsRuleNode.h
+++ layout/style/nsRuleNode.h
@@ -1071,6 +1071,9 @@ public:
 
   static bool ParentHasPseudoElementData(nsStyleContext* aContext);
 
+  static void ComputeTimingFunction(const nsCSSValue& aValue,
+                                    nsTimingFunction& aResult);
+
 private:
 #ifdef DEBUG
   // non-inline helper function to allow assertions without incomplete
