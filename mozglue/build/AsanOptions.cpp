14123ecfc088d9b6e413b493ca9c759c56b57742
Andrew McCreight <continuation@gmail.com>
2015-10-22 09:33:43 -0700

    Bug 1216354 - Disable LSan leak checking by default in mozglue. r=glandium

--- mozglue/build/AsanOptions.cpp
+++ mozglue/build/AsanOptions.cpp
@@ -7,9 +7,10 @@
 
 #ifndef _MSC_VER // Not supported by clang-cl yet
 
-// When running with AddressSanitizer, we need to explicitely set some
+// When running with AddressSanitizer, we need to explicitly set some
 // options specific to our codebase to prevent errors during runtime.
-// 
+// To override these, set the ASAN_OPTIONS environment variable.
+//
 // Currently, these are:
 //
 //   allow_user_segv_handler=1 - Tell ASan to allow our code to use its
@@ -20,9 +21,15 @@
 //   operators that are backed by malloc/free. If one of them gets inlined
 //   while the other doesn't, ASan will report false positives.
 //
+//   detect_leaks=0 - Disable LeakSanitizer. This is required because
+//   otherwise leak checking will be enabled for various building and
+//   testing executables where we don't care much about leaks. Enabling
+//   this will also likely require setting LSAN_OPTIONS with a suppression
+//   file, as in build/sanitizers/lsan_suppressions.txt.
+//
 extern "C" MOZ_ASAN_BLACKLIST
 const char* __asan_default_options() {
-    return "allow_user_segv_handler=1:alloc_dealloc_mismatch=0";
+    return "allow_user_segv_handler=1:alloc_dealloc_mismatch=0:detect_leaks=0";
 }
 
 #endif
