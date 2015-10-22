86bfe16cf09b0a8b943d4a39902acc11f87c7778
Bobby Holley <bobbyholley@gmail.com>
2015-10-22 15:30:04 -0700

    Bug 1217614 - Remove XPIDL mozSetDataAt and refactor associated code. r=bz

--- dom/events/ClipboardEvent.cpp
+++ dom/events/ClipboardEvent.cpp
@@ -80,7 +80,8 @@ ClipboardEvent::Constructor(const GlobalObject& aGlobal,
       // support other types of events, make sure that read/write privileges are
       // checked properly within DataTransfer.
       clipboardData = new DataTransfer(ToSupports(e), eCopy, false, -1);
-      clipboardData->SetData(aParam.mDataType, aParam.mData);
+      clipboardData->SetData(aParam.mDataType, aParam.mData, aRv);
+      NS_ENSURE_TRUE(!aRv.Failed(), nullptr);
     }
   }
 
