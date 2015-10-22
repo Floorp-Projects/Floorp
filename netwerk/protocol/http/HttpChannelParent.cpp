69713f28b429cfc5caee9edc5faed9c82ea31b09
Henry <hchang@mozilla.com>
2015-10-22 05:44:00 +0200

    Bug 1180088 - Use origin-based permission check on parent side for signed packaged web app. r=kanru.

--- netwerk/protocol/http/HttpChannelParent.cpp
+++ netwerk/protocol/http/HttpChannelParent.cpp
@@ -1057,7 +1057,7 @@ NS_IMETHODIMP
 HttpChannelParent::OnStartSignedPackageRequest(const nsACString& aPackageId)
 {
   if (mTabParent) {
-    mTabParent->OnStartSignedPackageRequest(mChannel);
+    mTabParent->OnStartSignedPackageRequest(mChannel, aPackageId);
   }
   return NS_OK;
 }
