98b2be08f77899940c89141557d43df543c39b58
Andrew Comminos <andrew@comminos.com>
2015-10-23 21:35:16 -0700

    Bug 1203135 - Terminate linking if maximum vertex attribute count is exceeded on Mesa. r=jgilbert, a=ritu

--- dom/canvas/WebGLShaderValidator.h
+++ dom/canvas/WebGLShaderValidator.h
@@ -43,6 +43,7 @@ public:
     void GetOutput(nsACString* out) const;
     bool CanLinkTo(const ShaderValidator* prev, nsCString* const out_log) const;
     size_t CalcNumSamplerUniforms() const;
+    size_t NumAttributes() const;
 
     bool FindAttribUserNameByMappedName(const std::string& mappedName,
                                         const std::string** const out_userName) const;
