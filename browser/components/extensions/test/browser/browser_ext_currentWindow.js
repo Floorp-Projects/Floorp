a9764f9ff5c660af555ca9c27d405349c8cc5222
Luca Greco <luca.greco@alcacoop.it>
2015-10-26 04:54:00 +0100

    Bug 1213975 - add WebExtension tabs.onUpdated test case. r=billm

--- browser/components/extensions/test/browser/browser_ext_currentWindow.js
+++ browser/components/extensions/test/browser/browser_ext_currentWindow.js
@@ -1,21 +1,3 @@
-function* focusWindow(win)
-{
-  let fm = Cc["@mozilla.org/focus-manager;1"].getService(Ci.nsIFocusManager);
-  if (fm.activeWindow == win) {
-    return;
-  }
-
-  let promise = new Promise(resolve => {
-    win.addEventListener("focus", function listener() {
-      win.removeEventListener("focus", listener, true);
-      resolve();
-    }, true);
-  });
-
-  win.focus();
-  yield promise;
-}
-
 function genericChecker()
 {
   var kind = "background";
