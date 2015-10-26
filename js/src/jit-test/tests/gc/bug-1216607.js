32e19e35f713bedd43c8243cc17de461b583d326
Jon Coppeard <jcoppeard@mozilla.com>
2015-10-26 10:16:51 +0000

    Bug 1216607 - Fix assertion failure ARM assembler on OOM r=jandem

---
+++ js/src/jit-test/tests/gc/bug-1216607.js
@@ -0,0 +1,16 @@
+enableSPSProfilingWithSlowAssertions();
+try {
+(function() {
+   while (n--) {
+   }
+})();
+} catch(exc1) {}
+function arrayProtoOutOfRange() {
+    function f(obj) {}
+    function test() {
+        for (var i = 0; i < 1000; i++)
+            var r = f(i % 2 ? a : b);
+    }
+    test();
+}
+oomTest(arrayProtoOutOfRange);
