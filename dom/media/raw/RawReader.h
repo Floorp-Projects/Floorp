924432c2eedc9cb4f78a9c7220d169406aa0baa5
JW Wang <jwwang@mozilla.com>
2015-10-26 14:10:29 +0800

    Bug 1217714 - Remove some unused functions from MediaDecoderReader. r=jya.

--- dom/media/raw/RawReader.h
+++ dom/media/raw/RawReader.h
@@ -26,16 +26,6 @@ public:
   virtual bool DecodeVideoFrame(bool &aKeyframeSkip,
                                   int64_t aTimeThreshold) override;
 
-  virtual bool HasAudio() override
-  {
-    return false;
-  }
-
-  virtual bool HasVideo() override
-  {
-    return true;
-  }
-
   virtual nsresult ReadMetadata(MediaInfo* aInfo,
                                 MetadataTags** aTags) override;
   virtual RefPtr<SeekPromise>
