4e6d1736a6f22c372c521a1493b952e33656606d
Shu-yu Guo <shu@rfrn.org>
2015-10-26 14:13:25 -0700

    Bug 1167029 - Remove support for let blocks. (r=jorendorff)

--- js/src/tests/js1_8_1/regress/regress-522123.js
+++ js/src/tests/js1_8_1/regress/regress-522123.js
@@ -25,7 +25,8 @@ function test()
   expect = 1;
 
   evil=eval;
-  let (x=2) {
+  {
+    let x=2;
     actual = evil("x");
   };
 
--- js/src/tests/js1_8_5/regress/regress-541255-3.js
+++ js/src/tests/js1_8_5/regress/regress-541255-3.js
@@ -5,7 +5,7 @@
  */
 
 function f(y) {
-    eval("let (z=2, w=y) { (function () { w.p = 7; })(); }");
+    eval("{ let z=2, w=y; (function () { w.p = 7; })(); }");
 }
 var x = {};
 f(x);
