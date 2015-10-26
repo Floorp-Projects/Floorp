6dd6e00ec57976c255e09b2e6801f3074d1821ec
dimi <dlee@mozilla.com>
2015-10-26 11:03:47 +0800

    Bug 1207727 - Add WPT tests for service worker update algorithm. r=bkelly

---
+++ testing/web-platform/mozilla/tests/service-workers/service-worker/resources/update-nocookie-worker.py
@@ -0,0 +1,14 @@
+import time
+
+def main(request, response):
+    # no-cache itself to ensure the user agent finds a new version for each update.
+    headers = [('Cache-Control', 'no-cache, must-revalidate'),
+               ('Pragma', 'no-cache')]
+
+    # Set a normal mimetype.
+    content_type = 'application/javascript'
+
+    headers.append(('Content-Type', content_type))
+    # Return a different script for each access.
+    return headers, '// %s' % (time.time())
+
