6884827a14c438994ebf8ac67395a4c6fdb52c8e
Luca Greco <luca.greco@alcacoop.it>
2015-10-26 04:56:00 +0100

    Bug 1213975 - fix WebExtension tabs.onUpdated cleanup on context close. r=billm

--- browser/components/extensions/ext-tabs.js
+++ browser/components/extensions/ext-tabs.js
@@ -219,11 +219,12 @@ extensions.registerAPI((extension, context) => {
         AllWindowEvents.addListener("TabAttrModified", listener);
         AllWindowEvents.addListener("TabPinned", listener);
         AllWindowEvents.addListener("TabUnpinned", listener);
+
         return () => {
           AllWindowEvents.removeListener("progress", progressListener);
-          AllWindowEvents.addListener("TabAttrModified", listener);
-          AllWindowEvents.addListener("TabPinned", listener);
-          AllWindowEvents.addListener("TabUnpinned", listener);
+          AllWindowEvents.removeListener("TabAttrModified", listener);
+          AllWindowEvents.removeListener("TabPinned", listener);
+          AllWindowEvents.removeListener("TabUnpinned", listener);
         };
       }).api(),
 
