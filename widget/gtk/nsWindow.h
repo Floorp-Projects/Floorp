1ffd3b157e38af44dd60f678e277dfb76229cafb
Bill McCloskey <billm@mozilla.com>
2015-10-26 13:33:13 -0700

    Bug 1218552 - Fix GTK drag-and-drop coordinate scaling on HiDPI displays (r=karlt) a=lizzard

--- widget/gtk/nsWindow.h
+++ widget/gtk/nsWindow.h
@@ -269,7 +269,7 @@ public:
     bool               IsDestroyed() { return mIsDestroyed; }
 
     void               DispatchDragEvent(mozilla::EventMessage aMsg,
-                                         const nsIntPoint& aRefPoint,
+                                         const mozilla::LayoutDeviceIntPoint& aRefPoint,
                                          guint aTime);
     static void        UpdateDragStatus (GdkDragContext *aDragContext,
                                          nsIDragService *aDragService);
