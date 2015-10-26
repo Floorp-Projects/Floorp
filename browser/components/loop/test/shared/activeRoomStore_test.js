fab371f35f1a0df6dedde70dfd7d8f8c78bb34f5
Mark Banner <standard8@mozilla.com>
2015-10-26 16:31:19 +0000

    Bug 1216551 - Fix an issue with Loop's unit tests failing when no devices are installed, due to bad stubbing. r=Mardak

--- browser/components/loop/test/shared/activeRoomStore_test.js
+++ browser/components/loop/test/shared/activeRoomStore_test.js
@@ -821,8 +821,11 @@ describe("loop.store.ActiveRoomStore", function() {
   });
 
   describe("#joinRoom", function() {
+    var hasDevicesStub;
+
     beforeEach(function() {
       store.setStoreState({ roomState: ROOM_STATES.READY });
+      hasDevicesStub = sandbox.stub(loop.shared.utils, "hasAudioOrVideoDevices");
     });
 
     it("should reset failureReason", function() {
@@ -845,7 +848,7 @@ describe("loop.store.ActiveRoomStore", function() {
       });
 
       it("should set the state to MEDIA_WAIT if media devices are present", function() {
-        sandbox.stub(loop.shared.utils, "hasAudioOrVideoDevices").callsArgWith(0, true);
+        hasDevicesStub.callsArgWith(0, true);
 
         store.joinRoom();
 
@@ -853,7 +856,7 @@ describe("loop.store.ActiveRoomStore", function() {
       });
 
       it("should not set the state to MEDIA_WAIT if no media devices are present", function() {
-        sandbox.stub(loop.shared.utils, "hasAudioOrVideoDevices").callsArgWith(0, false);
+        hasDevicesStub.callsArgWith(0, false);
 
         store.joinRoom();
 
@@ -861,7 +864,7 @@ describe("loop.store.ActiveRoomStore", function() {
       });
 
       it("should dispatch `ConnectionFailure` if no media devices are present", function() {
-        sandbox.stub(loop.shared.utils, "hasAudioOrVideoDevices").callsArgWith(0, false);
+        hasDevicesStub.callsArgWith(0, false);
 
         store.joinRoom();
 
