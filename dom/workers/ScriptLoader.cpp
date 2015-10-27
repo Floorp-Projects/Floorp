e791c25d6b69bc8c8910064c7308d235603a54d4
Andrea Marchesini <amarchesini@mozilla.com>
2015-10-27 10:17:17 -0700

    Bug 1211967 - Fix how we report errors when loading a worker from a data url, r=bz, a=lizzard
    --HG--
    extra : commitid : 3W1uEyuJmIK

--- dom/workers/ScriptLoader.cpp
+++ dom/workers/ScriptLoader.cpp
@@ -976,7 +976,13 @@ private:
       principal = parentWorker->GetPrincipal();
     }
 
-    aLoadInfo.mMutedErrorFlag.emplace(!principal->Subsumes(channelPrincipal));
+    // We don't mute the main worker script becase we've already done
+    // same-origin checks on them so we should be able to see their errors.
+    // Note that for data: url, where we allow it through the same-origin check
+    // but then give it a different origin.
+    aLoadInfo.mMutedErrorFlag.emplace(IsMainWorkerScript()
+                                        ? false 
+                                        : !principal->Subsumes(channelPrincipal));
 
     // Make sure we're not seeing the result of a 404 or something by checking
     // the 'requestSucceeded' attribute on the http channel.
