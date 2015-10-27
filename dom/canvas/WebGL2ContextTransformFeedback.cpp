3c41cd4c26258b1c2db025210189b69dcabd9b22
Jeff Muizelaar <jmuizelaar@mozilla.com>
2015-10-27 18:01:29 -0400

    Bug 1219027. Fix unbinding transformfeedback when deleting it. r=djg
    I believe this was the original intention of the code.
    This fixes deletion conformance tests.

--- dom/canvas/WebGL2ContextTransformFeedback.cpp
+++ dom/canvas/WebGL2ContextTransformFeedback.cpp
@@ -41,7 +41,7 @@ WebGL2Context::DeleteTransformFeedback(WebGLTransformFeedback* tf)
         return;
 
     if (mBoundTransformFeedback == tf)
-        BindTransformFeedback(LOCAL_GL_TRANSFORM_FEEDBACK, tf);
+        BindTransformFeedback(LOCAL_GL_TRANSFORM_FEEDBACK, nullptr);
 
     tf->RequestDelete();
 }
