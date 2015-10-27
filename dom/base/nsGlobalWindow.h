617ba49520f6758f7dc72919f9c5627b3d370705
Neil Deakin <neil@mozilla.com>
2015-10-27 08:53:00 -0400

    Bug 1214293, initialize show focus rings state properly in all child frames, r=smaug

--- dom/base/nsGlobalWindow.h
+++ dom/base/nsGlobalWindow.h
@@ -1589,6 +1589,8 @@ protected:
 
   inline int32_t DOMMinTimeoutValue() const;
 
+  void InitializeShowFocusRings();
+
   // Clear the document-dependent slots on our JS wrapper.  Inner windows only.
   void ClearDocumentDependentSlots(JSContext* aCx);
 
