ffc9b08ae4fc3d0fac5a26570c10ace47a7c5903
Jakob Olesen <jolesen@mozilla.com>
2015-10-26 17:06:09 -0700

    Bug 1207827 - Remove ARM temporary offset buffers. r=nbp
    The ARM assembler no longer needs to keep track of code offsets for
    later translation to 'actual' offsets. The AssemblerBuffer offsets
    are directly usable now.
    
    Remove tmpDataRelocations_, tmpPreBarriers_, and tmpJumpRelocations_.

--- js/src/jit/arm/Assembler-arm.cpp
+++ js/src/jit/arm/Assembler-arm.cpp
@@ -631,21 +631,6 @@ Assembler::finish()
     flush();
     MOZ_ASSERT(!isFinished);
     isFinished = true;
-
-    for (unsigned int i = 0; i < tmpDataRelocations_.length(); i++) {
-        size_t offset = tmpDataRelocations_[i].getOffset();
-        dataRelocations_.writeUnsigned(offset);
-    }
-
-    for (unsigned int i = 0; i < tmpJumpRelocations_.length(); i++) {
-        size_t offset = tmpJumpRelocations_[i].getOffset();
-        jumpRelocations_.writeUnsigned(offset);
-    }
-
-    for (unsigned int i = 0; i < tmpPreBarriers_.length(); i++) {
-        size_t offset = tmpPreBarriers_[i].getOffset();
-        preBarriers_.writeUnsigned(offset);
-    }
 }
 
 void
@@ -893,12 +878,11 @@ TraceDataRelocations(JSTracer* trc, uint8_t* buffer, CompactBufferReader& reader
 }
 
 static void
-TraceDataRelocations(JSTracer* trc, ARMBuffer* buffer,
-                     Vector<BufferOffset, 0, SystemAllocPolicy>* locs)
+TraceDataRelocations(JSTracer* trc, ARMBuffer* buffer, CompactBufferReader& reader)
 {
-    for (unsigned int idx = 0; idx < locs->length(); idx++) {
-        BufferOffset bo = (*locs)[idx];
-        ARMBuffer::AssemblerBufferInstIterator iter(bo, buffer);
+    while (reader.more()) {
+        BufferOffset offset(reader.readUnsigned());
+        ARMBuffer::AssemblerBufferInstIterator iter(offset, buffer);
         TraceOneDataRelocation(trc, &iter);
     }
 }
@@ -942,8 +926,10 @@ Assembler::trace(JSTracer* trc)
         }
     }
 
-    if (tmpDataRelocations_.length())
-        ::TraceDataRelocations(trc, &m_buffer, &tmpDataRelocations_);
+    if (dataRelocations_.length()) {
+        CompactBufferReader reader(dataRelocations_);
+        ::TraceDataRelocations(trc, &m_buffer, reader);
+    }
 }
 
 void
@@ -951,7 +937,7 @@ Assembler::processCodeLabels(uint8_t* rawCode)
 {
     for (size_t i = 0; i < codeLabels_.length(); i++) {
         CodeLabel label = codeLabels_[i];
-        Bind(rawCode, label.dest(), rawCode + actualOffset(label.src()->offset()));
+        Bind(rawCode, label.dest(), rawCode + label.src()->offset());
     }
 }
 
@@ -973,8 +959,7 @@ void
 Assembler::Bind(uint8_t* rawCode, AbsoluteLabel* label, const void* address)
 {
     // See writeCodePointer comment.
-    uint32_t off = actualOffset(label->offset());
-    *reinterpret_cast<const void**>(rawCode + off) = address;
+    *reinterpret_cast<const void**>(rawCode + label->offset()) = address;
 }
 
 Assembler::Condition
