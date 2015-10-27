a85c0310804ce304316e0835bb2d8dc1cfe27f79
Patrick Brosset <pbrosset@mozilla.com>
2015-10-27 11:21:46 +0100

    Bug 1218409 - Eslint rule that checks for balanced listeners. r=miker

--- testing/eslint-plugin-mozilla/lib/index.js
+++ testing/eslint-plugin-mozilla/lib/index.js
@@ -13,12 +13,14 @@
 
 module.exports = {
   rules: {
+    "balanced-listeners": require("../lib/rules/balanced-listeners"),
     "components-imports": require("../lib/rules/components-imports"),
     "import-headjs-globals": require("../lib/rules/import-headjs-globals"),
     "mark-test-function-used": require("../lib/rules/mark-test-function-used"),
     "var-only-at-top-level": require("../lib/rules/var-only-at-top-level")
   },
   rulesConfig: {
+    "balanced-listeners": 0,
     "components-imports": 0,
     "import-headjs-globals": 0,
     "mark-test-function-used": 0,
