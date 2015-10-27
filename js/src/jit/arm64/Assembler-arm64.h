ebcd10e003c6a033b66ff24c76f26e06e923512f
Jakob Olesen <jolesen@mozilla.com>
2015-10-26 17:06:09 -0700

    Bug 1207827 - Remove ARM64 temporary offset buffers. r=nbp
    The ARM64 assembler no longer needs to keep track of code offsets for
    later translation to 'final' offsets. The AssemblerBuffer offsets are
    directly usable now.
    
    Remove tmpDataRelocations_, tmpPreBarriers_, tmpJumpRelocations_, and
    the finalOffset() method.

--- js/src/jit/arm64/Assembler-arm64.h
+++ js/src/jit/arm64/Assembler-arm64.h
@@ -342,11 +342,6 @@ class Assembler : public vixl::Assembler
     static void FixupNurseryObjects(JSContext* cx, JitCode* code, CompactBufferReader& reader,
                                     const ObjectVector& nurseryObjects);
 
-    // Convert a BufferOffset to a final byte offset from the start of the code buffer.
-    size_t toFinalOffset(BufferOffset offset) {
-        return size_t(offset.getOffset());
-    }
-
   public:
     // A Jump table entry is 2 instructions, with 8 bytes of raw data
     static const size_t SizeOfJumpTableEntry = 16;
@@ -401,13 +396,6 @@ class Assembler : public vixl::Assembler
           : jump(jump), extendedTableIndex(extendedTableIndex)
         { }
     };
-
-    // Because ARM and A64 use a code buffer that allows for constant pool insertion,
-    // the actual offset of each jump cannot be known until finalization.
-    // These vectors store the WIP offsets.
-    js::Vector<BufferOffset, 0, SystemAllocPolicy> tmpDataRelocations_;
-    js::Vector<BufferOffset, 0, SystemAllocPolicy> tmpPreBarriers_;
-    js::Vector<JumpRelocation, 0, SystemAllocPolicy> tmpJumpRelocations_;
 
     // Structure for fixing up pc-relative loads/jumps when the machine
     // code gets moved (executable copy, gc, etc.).
