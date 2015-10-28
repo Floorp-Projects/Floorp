ea830e8a7b95248f838c4e0cefdf23719706e928
Bill McCloskey <billm@mozilla.com>
2015-10-28 16:50:12 -0700

    Bug 1219504 - BrowserTestUtils changes (r=mconley)

--- testing/mochitest/browser-test.js
+++ testing/mochitest/browser-test.js
@@ -552,6 +552,7 @@ Tester.prototype = {
         this.SimpleTest[m] = this.SimpleTestOriginal[m];
       });
 
+      this.ContentTask.setTestScope(null);
       testScope.destroy();
       this.currentTest.scope = null;
     }
@@ -705,6 +706,8 @@ Tester.prototype = {
       }
       currentTest.addResult(res);
     });
+
+    this.ContentTask.setTestScope(currentScope);
 
     // Allow Assert.jsm methods to be tacked to the current scope.
     this.currentTest.scope.export_assertions = function() {
