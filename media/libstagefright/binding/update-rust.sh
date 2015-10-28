5ffe01b5a7040ca43a0267ee77968a3f38d1f127
Ralph Giles <giles@mozilla.com>
2015-10-28 14:55:45 -0700

    Bug 1219452 - Update script for rust mp4parser. r=kinetik
    Automate update and patching of the rust mp4 parser from the
    upstream repo. A rev for mp4parse-rust can be passed on the
    command line, but the byteorder crate's version is hardcoded.
    
    Differences to adapt to the gecko build system are applied as
    patches like we do for other media code.
    
    Unfortunately cargo isn't much help here. It can download
    crates for us, and we can set CARGO_HOME to force it to
    use a specific directory, but it doesn't return enough
    information to get the versions, etc. without some guessing/scraping
    to find the packaged source.

---
+++ media/libstagefright/binding/update-rust.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Script to update mp4parse-rust sources to latest upstream
+
+# Default version.
+VER=v0.1.2
+
+# Accept version or commit from the command line.
+if test -n "$1"; then
+  VER=$1
+fi
+
+echo "Fetching sources..."
+rm -rf _upstream
+git clone https://github.com/mozilla/mp4parse-rust _upstream/mp4parse
+pushd _upstream/mp4parse
+git checkout ${VER}
+popd
+cp _upstream/mp4parse/src/lib.rs MP4Metadata.rs
+cp _upstream/mp4parse/src/capi.rs .
+
+# TODO: download deps from crates.io.
+
+git clone https://github.com/BurntSushi/byteorder _upstream/byteorder
+pushd _upstream/byteorder
+git checkout 0.3.13
+popd
+cp _upstream/byteorder/src/lib.rs byteorder/mod.rs
+cp _upstream/byteorder/src/new.rs byteorder/new.rs
+
+echo "Applying patches..."
+patch -p4 < byteorder-mod.patch
+patch -p4 < mp4parse-mod.patch
+
+echo "Cleaning up..."
+rm -rf _upstream
+
+echo "Updated to ${VER}."
