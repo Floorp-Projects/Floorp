ec2e30835a7a721f286f499225e49255b89a66ae
Markus Stange <mstange@themasta.com>
2015-10-28 22:52:15 +0100

    Bug 1201327 - Let DLBI detect background-position changes. r=mattwoodrow
    --HG--
    extra : commitid : CDTUZNG0PYF
    extra : rebase_source : 0f5db6e4a5cde5fe8faeb77e15f95d12f6753fac

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -2576,6 +2576,11 @@ public:
   nsRect GetPositioningArea();
 
   /**
+   * Return the destination area of one instance of the image.
+   */
+  nsRect GetDestArea() const { return mDestArea; }
+
+  /**
    * Returns true if existing rendered pixels of this display item may need
    * to be redrawn if the positioning area size changes but its position does
    * not.
@@ -2615,6 +2620,7 @@ protected:
                                   gfxRect* aDestRect);
   bool IsNonEmptyFixedImage() const;
   nsRect GetBoundsInternal(nsDisplayListBuilder* aBuilder);
+  nsRect GetDestAreaInternal(nsDisplayListBuilder* aBuilder);
 
   void PaintInternal(nsDisplayListBuilder* aBuilder, nsRenderingContext* aCtx,
                      const nsRect& aBounds, nsRect* aClipRect);
@@ -2637,6 +2643,7 @@ protected:
   LayoutDeviceRect mImageLayerDestRect;
   /* Bounds of this display item */
   nsRect mBounds;
+  nsRect mDestArea;
   uint32_t mLayer;
 };
 
