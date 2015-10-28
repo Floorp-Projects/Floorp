e5b08c22865254cc333c9c2827cf09c8ce8c7bb0
Ehsan Akhgari <ehsan@mozilla.com>
2015-10-28 16:01:07 -0400

    Bug 1202085 - Part 6: Clear the entries in the image cache belonging to a controlled document when it gets destroyed; r=seth

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -70,7 +70,8 @@
 #include "mozilla/dom/TreeWalker.h"
 
 #include "nsIServiceManager.h"
-#include "nsIServiceWorkerManager.h"
+#include "mozilla/dom/workers/ServiceWorkerManager.h"
+#include "imgLoader.h"
 
 #include "nsCanvasFrame.h"
 #include "nsContentCID.h"
@@ -8893,8 +8894,13 @@ nsDocument::Destroy()
 
   mRegistry = nullptr;
 
-  nsCOMPtr<nsIServiceWorkerManager> swm = mozilla::services::GetServiceWorkerManager();
+  using mozilla::dom::workers::ServiceWorkerManager;
+  RefPtr<ServiceWorkerManager> swm = ServiceWorkerManager::GetInstance();
   if (swm) {
+    ErrorResult error;
+    if (swm->IsControlled(this, error)) {
+      nsContentUtils::GetImgLoaderForDocument(this)->ClearCacheForControlledDocument(this);
+    }
     swm->MaybeStopControlling(this);
   }
 
