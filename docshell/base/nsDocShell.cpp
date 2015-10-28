daec939f2317fff36497244c1a599505722dfd48
Henry <hchang@mozilla.com>
2015-10-29 00:52:40 +0800

    Bug 1178526 - Create docshell with packageId from TabContext. r=sicking.

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -13714,6 +13714,13 @@ nsDocShell::SetIsBrowserInsideApp(uint32_t aContainingAppId)
   return NS_OK;
 }
 
+NS_IMETHODIMP
+nsDocShell::SetIsSignedPackage(const nsAString& aSignedPkg)
+{
+  mSignedPkg = aSignedPkg;
+  return NS_OK;
+}
+
 /* [infallible] */ NS_IMETHODIMP
 nsDocShell::GetIsBrowserElement(bool* aIsBrowser)
 {
@@ -13820,6 +13827,9 @@ nsDocShell::GetOriginAttributes()
   if (mFrameType == eFrameTypeBrowser) {
     attrs.mInBrowser = true;
   }
+
+  // Bug 1209162 will address the inheritance of each attributes.
+  attrs.mSignedPkg = mSignedPkg;
 
   return attrs;
 }
