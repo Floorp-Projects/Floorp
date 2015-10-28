68cc239e9da4639aecde250ff6eff68206184edb
Seth Fowler <mark.seth.fowler@gmail.com>
2015-10-28 16:40:42 -0700

    Bug 1207355 (Part 1) - Stop requesting decodes in nsDocument. r=tn

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4152,9 +4152,6 @@ pref("image.multithreaded_decoding.limit", -1);
 // cache.
 pref("canvas.image.cache.limit", 0);
 
-// How many images to eagerly decode on a given page. 0 means "no limit".
-pref("image.onload.decode.limit", 0);
-
 // WebGL prefs
 #ifdef ANDROID
 // Disable MSAA on mobile.
