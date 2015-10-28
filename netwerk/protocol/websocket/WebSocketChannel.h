4d8e79e96986bc67d50ba298c3bb1b2b43d2ccec
Andrea Marchesini <amarchesini@mozilla.com>
2015-10-28 11:41:24 +0000

    Bug 1215092 - WebSocketEventService and WebSocket discovering - part 1 - Renaming WebSocketFrameService, r=michal
    --HG--
    rename : netwerk/protocol/websocket/PWebSocketFrameListener.ipdl => netwerk/protocol/websocket/PWebSocketEventListener.ipdl
    rename : netwerk/protocol/websocket/WebSocketFrameListenerChild.cpp => netwerk/protocol/websocket/WebSocketEventListenerChild.cpp
    rename : netwerk/protocol/websocket/WebSocketFrameListenerChild.h => netwerk/protocol/websocket/WebSocketEventListenerChild.h
    rename : netwerk/protocol/websocket/WebSocketFrameListenerParent.cpp => netwerk/protocol/websocket/WebSocketEventListenerParent.cpp
    rename : netwerk/protocol/websocket/WebSocketFrameListenerParent.h => netwerk/protocol/websocket/WebSocketEventListenerParent.h
    rename : netwerk/protocol/websocket/WebSocketFrameService.cpp => netwerk/protocol/websocket/WebSocketEventService.cpp
    rename : netwerk/protocol/websocket/WebSocketFrameService.h => netwerk/protocol/websocket/WebSocketEventService.h
    rename : netwerk/protocol/websocket/nsIWebSocketFrameService.idl => netwerk/protocol/websocket/nsIWebSocketEventService.idl

--- netwerk/protocol/websocket/WebSocketChannel.h
+++ netwerk/protocol/websocket/WebSocketChannel.h
@@ -50,7 +50,7 @@ class CallOnMessageAvailable;
 class CallOnStop;
 class CallOnServerClose;
 class CallAcknowledge;
-class WebSocketFrameService;
+class WebSocketEventService;
 
 // Used to enforce "1 connecting websocket per host" rule, and reconnect delays
 enum wsConnectingState {
@@ -227,7 +227,7 @@ private:
   const static int32_t            kLingeringCloseTimeout =   1000;
   const static int32_t            kLingeringCloseThreshold = 50;
 
-  RefPtr<WebSocketFrameService>   mFrameService;
+  RefPtr<WebSocketEventService>   mService;
 
   int32_t                         mMaxConcurrentConnections;
 
