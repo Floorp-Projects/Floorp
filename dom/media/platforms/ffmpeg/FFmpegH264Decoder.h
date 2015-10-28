bab472bc4c4653fa3e9ae8d5eddd365682917ab2
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-10-28 21:56:22 +1100

    Bug 1219134: P2. Use LibAV/FFmpeg logic to detect invalid pts. r=edwin
    The logic was extracted from LibAV cmdutils.c. FFmpeg provides an API for that (av_frame_get_best_effort_timestamp()) unfortunately this isn't provided by LibAV.
    So copy the logic instead in order to keep compatibility with the two forks.

--- dom/media/platforms/ffmpeg/FFmpegH264Decoder.h
+++ dom/media/platforms/ffmpeg/FFmpegH264Decoder.h
@@ -39,6 +39,7 @@ public:
   RefPtr<InitPromise> Init() override;
   nsresult Input(MediaRawData* aSample) override;
   void ProcessDrain() override;
+  void ProcessFlush() override;
   static AVCodecID GetCodecId(const nsACString& aMimeType);
 
 private:
@@ -59,13 +60,27 @@ private:
 
   static int AllocateBufferCb(AVCodecContext* aCodecContext, AVFrame* aFrame);
   static void ReleaseBufferCb(AVCodecContext* aCodecContext, AVFrame* aFrame);
-  int64_t GetPts(const AVPacket& packet);
 
   RefPtr<ImageContainer> mImageContainer;
   uint32_t mPictureWidth;
   uint32_t mPictureHeight;
   uint32_t mDisplayWidth;
   uint32_t mDisplayHeight;
+
+  class PtsCorrectionContext {
+  public:
+    PtsCorrectionContext();
+    int64_t GuessCorrectPts(int64_t aPts, int64_t aDts);
+    void Reset();
+
+  private:
+    int64_t mNumFaultyPts; /// Number of incorrect PTS values so far
+    int64_t mNumFaultyDts; /// Number of incorrect DTS values so far
+    int64_t mLastPts;       /// PTS of the last frame
+    int64_t mLastDts;       /// DTS of the last frame
+  };
+
+  PtsCorrectionContext mPtsContext;
 };
 
 } // namespace mozilla
