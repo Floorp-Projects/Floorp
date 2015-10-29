fbb9c9118d7b3d4c5ad66673cefd06c8d7051008
Bob Clary <bclary@bclary.com>
2015-10-28 23:21:47 -0700

    Bug 1218997 - [mozdevice] adb_android.py ADBDevice.power_on() requires root, r=gbrown.

--- testing/mozbase/mozdevice/mozdevice/adb_android.py
+++ testing/mozbase/mozdevice/mozdevice/adb_android.py
@@ -137,7 +137,9 @@ class ADBAndroid(ADBDevice):
                  * ADBError
         """
         try:
-            self.shell_output('svc power stayon true', timeout=timeout)
+            self.shell_output('svc power stayon true',
+                              timeout=timeout,
+                              root=True)
         except ADBError, e:
             # Executing this via adb shell errors, but not interactively.
             # Any other exitcode is a real error.
