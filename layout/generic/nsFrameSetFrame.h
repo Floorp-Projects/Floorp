4c783e236c9ff6b887947a479607a81727662a8d
Nathan Froyd <froydnj@mozilla.com>
2015-10-29 16:16:13 -0400

    Bug 1219903 - use UniquePtr<T[]> instead of delete[] calls in layout/generic/; r=dholbert

--- layout/generic/nsFrameSetFrame.h
+++ layout/generic/nsFrameSetFrame.h
@@ -9,6 +9,7 @@
 #define nsHTMLFrameset_h___
 
 #include "mozilla/Attributes.h"
+#include "mozilla/UniquePtr.h"
 #include "nsContainerFrame.h"
 #include "nsColor.h"
 
@@ -187,16 +188,19 @@ protected:
   
   void SetBorderResize(nsHTMLFramesetBorderFrame* aBorderFrame);
 
+  template<typename T, class D = mozilla::DefaultDelete<T>>
+  using UniquePtr = mozilla::UniquePtr<T, D>;
+
   nsFramesetDrag   mDrag;
   nsBorderColor    mEdgeColors;
   nsHTMLFramesetBorderFrame* mDragger;
   nsHTMLFramesetFrame* mTopLevelFrameset;
-  nsHTMLFramesetBorderFrame** mVerBorders;  // vertical borders
-  nsHTMLFramesetBorderFrame** mHorBorders;  // horizontal borders
-  nsFrameborder*   mChildFrameborder; // the frameborder attr of children
-  nsBorderColor*   mChildBorderColors;
-  nscoord*         mRowSizes;  // currently computed row sizes
-  nscoord*         mColSizes;  // currently computed col sizes
+  UniquePtr<nsHTMLFramesetBorderFrame*[]> mVerBorders;  // vertical borders
+  UniquePtr<nsHTMLFramesetBorderFrame*[]> mHorBorders;  // horizontal borders
+  UniquePtr<nsFrameborder[]> mChildFrameborder; // the frameborder attr of children
+  UniquePtr<nsBorderColor[]> mChildBorderColors;
+  UniquePtr<nscoord[]> mRowSizes;  // currently computed row sizes
+  UniquePtr<nscoord[]> mColSizes;  // currently computed col sizes
   mozilla::LayoutDeviceIntPoint mFirstDragPoint;
   int32_t          mNumRows;
   int32_t          mNumCols;
