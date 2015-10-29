8b2abf4370bc5dd1940302bcefd9ac1f7224a4b4
Heiher <r@hev.cc>
2015-10-30 01:44:15 +0800

    Bug 1217873 - IonMonkey: MIPS64: Simplify BailoutStack for MIPS64. r=lth
    ---
     js/src/jit/mips64/Bailouts-mips64.cpp      |  2 +-
     js/src/jit/mips64/Bailouts-mips64.h        | 47 +++++-------------------------
     js/src/jit/mips64/CodeGenerator-mips64.cpp |  4 +--
     js/src/jit/mips64/Trampoline-mips64.cpp    | 24 +++++++--------
     4 files changed, 21 insertions(+), 56 deletions(-)

--- js/src/jit/mips64/Bailouts-mips64.h
+++ js/src/jit/mips64/Bailouts-mips64.h
@@ -15,59 +15,26 @@ namespace jit {
 
 class BailoutStack
 {
-    uintptr_t frameClassId_;
-    // This is pushed in the bailout handler. Both entry points into the
-    // handler inserts their own value int lr, which is then placed onto the
-    // stack along with frameClassId_ above. This should be migrated to ip.
-  public:
-    union {
-        uintptr_t frameSize_;
-        uintptr_t tableOffset_;
-    };
-
-  protected:
     RegisterDump::FPUArray fpregs_;
     RegisterDump::GPRArray regs_;
-
+    uintptr_t frameSize_;
     uintptr_t snapshotOffset_;
-    uintptr_t padding_;
 
   public:
-    FrameSizeClass frameClass() const {
-        return FrameSizeClass::FromClass(frameClassId_);
-    }
-    uintptr_t tableOffset() const {
-        MOZ_ASSERT(frameClass() != FrameSizeClass::None());
-        return tableOffset_;
-    }
-    uint32_t frameSize() const {
-        if (frameClass() == FrameSizeClass::None())
-            return frameSize_;
-        return frameClass().frameSize();
-    }
-    MachineState machine() {
+    MachineState machineState() {
         return MachineState::FromBailout(regs_, fpregs_);
     }
-    SnapshotOffset snapshotOffset() const {
-        MOZ_ASSERT(frameClass() == FrameSizeClass::None());
+    uint32_t snapshotOffset() const {
         return snapshotOffset_;
     }
-    uint8_t* parentStackPointer() const {
-        if (frameClass() == FrameSizeClass::None())
-            return (uint8_t*)this + sizeof(BailoutStack);
-        return (uint8_t*)this + offsetof(BailoutStack, snapshotOffset_);
+    uint32_t frameSize() const {
+        return frameSize_;
     }
-    static size_t offsetOfFrameClass() {
-        return offsetof(BailoutStack, frameClassId_);
+    uint8_t* parentStackPointer() {
+        return (uint8_t*)this + sizeof(BailoutStack);
     }
     static size_t offsetOfFrameSize() {
         return offsetof(BailoutStack, frameSize_);
-    }
-    static size_t offsetOfFpRegs() {
-        return offsetof(BailoutStack, fpregs_);
-    }
-    static size_t offsetOfRegs() {
-        return offsetof(BailoutStack, regs_);
     }
 };
 
