c5a2e6fbfce2a13849cf610d7aa2ddf093ab6e7e
Brian Grinstead <bgrinstead@mozilla.com>
2015-10-29 16:20:47 -0700

    Bug 1219872 - Fix animationinspector permafail on beta by turning on dom.animations-api.core.enabled;r=birtles,a=bustage on CLOSED TREE

--- browser/devtools/animationinspector/test/browser_animation_timeline_takes_rate_into_account.js
+++ browser/devtools/animationinspector/test/browser_animation_timeline_takes_rate_into_account.js
@@ -12,6 +12,12 @@
 // rate changed have a delay = delay/rate and a duration = duration/rate.
 
 add_task(function*() {
+  yield new Promise(resolve => {
+    SpecialPowers.pushPrefEnv({"set": [
+      ["dom.animations-api.core.enabled", true]
+    ]}, resolve);
+  });
+
   yield addTab(TEST_URL_ROOT + "doc_modify_playbackRate.html");
 
   let {panel} = yield openAnimationInspector();
