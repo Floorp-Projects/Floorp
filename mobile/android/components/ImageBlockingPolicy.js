3b93da9d48c41d9f489b4399fc257d8146278756
Jonathan Almeida [:jonalmeida] <jonalmeida942@gmail.com>
2015-10-29 02:15:41 -0400

    Bug 1208167 - Telemetry probe for click-to-play images r=mfinkle,ally
    - A telemetry probe for when a user clicks to unblock an image.
    - A second probe for sending the size of the image being unblocked.
     Since we don't have information on the image size the best option
     was to make make an XHR HEAD request. We make an approximation
     of the image size from the content length which we send in kB.
    
    --HG--
    extra : commitid : G0ixOr8xmju
    extra : rebase_source : cfb7d0d5777f6d0c657f6aafa18c58ec2b882789

--- mobile/android/components/ImageBlockingPolicy.js
+++ mobile/android/components/ImageBlockingPolicy.js
@@ -8,17 +8,29 @@ Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Timer.jsm");
 
+////////////////////////////////////////////////////////////////////////////////
+//// Constants
+
+//// SVG placeholder image for blocked image content
+const PLACEHOLDER_IMG = "chrome://browser/skin/images/placeholder_image.svg";
+
+//// Telemetry
+const TELEMETRY_TAP_TO_LOAD_ENABLED = "TAP_TO_LOAD_ENABLED";
+const TELEMETRY_SHOW_IMAGE_SIZE = "TAP_TO_LOAD_IMAGE_SIZE";
+const TOPIC_GATHER_TELEMETRY = "gather-telemetry";
+
+//// Gecko preference
+const PREF_IMAGEBLOCKING_ENABLED = "browser.image_blocking.enabled"
+
 /**
  * Content policy for blocking images
  */
-
-// SVG placeholder image for blocked image content
-var PLACEHOLDER_IMG = "chrome://browser/skin/images/placeholder_image.svg";
-
-function ImageBlockingPolicy() {}
+function ImageBlockingPolicy() {
+  Services.obs.addObserver(this, TOPIC_GATHER_TELEMETRY, false);
+}
 
 ImageBlockingPolicy.prototype = {
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPolicy]),
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPolicy, Ci.nsIObserver]),
   classDescription: "Click-To-Play Image",
   classID: Components.ID("{f55f77f9-d33d-4759-82fc-60db3ee0bb91}"),
   contractID: "@mozilla.org/browser/blockimages-policy;1",
@@ -37,6 +49,7 @@ ImageBlockingPolicy.prototype = {
         if (node instanceof Ci.nsIDOMHTMLImageElement) {
           // Accept if the user has asked to view the image
           if (node.getAttribute("data-ctv-show") == "true") {
+            sendImageSizeTelemetry(node.getAttribute("data-ctv-src"));
             return Ci.nsIContentPolicy.ACCEPT;
           }
 
@@ -76,6 +89,35 @@ ImageBlockingPolicy.prototype = {
     return Services.prefs.getIntPref("browser.image_blocking");
   },
 
+  observe : function (subject, topic, data) {
+    if (topic == TOPIC_GATHER_TELEMETRY) {
+      Services.telemetry.getHistogramById(TELEMETRY_TAP_TO_LOAD_ENABLED).add(getEnabled());
+    }
+  },
 };
+
+function getEnabled() {
+  return Services.prefs.getBoolPref(PREF_IMAGEBLOCKING_ENABLED);
+}
+
+function sendImageSizeTelemetry(imageURL) {
+  let xhr = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
+  xhr.open("HEAD", imageURL, true);
+  xhr.onreadystatechange = function (e) {
+    if (xhr.readyState != 4) {
+      return;
+    }
+    if (xhr.status != 200) {
+      return;
+    }
+    let contentLength = xhr.getResponseHeader("Content-Length");
+    if (!contentLength) {
+      return;
+    }
+    let imageSize = contentLength / 1024;
+    Services.telemetry.getHistogramById(TELEMETRY_SHOW_IMAGE_SIZE).add(imageSize);
+  };
+  xhr.send(null);
+}
 
 this.NSGetFactory = XPCOMUtils.generateNSGetFactory([ImageBlockingPolicy]);
