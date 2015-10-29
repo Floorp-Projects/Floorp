e7df0d8c2e3fbaf1e6e3d669eb59cf84c83dc89c
Mark Finkle <mfinkle@mozilla.com>
2015-10-29 15:14:01 -0400

    Bug 1219447 - Add a UI Telemetry probe for the 'Add to Reading List' web context menu. r=margaret, a=sylvestre

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -721,6 +721,9 @@ var BrowserApp = {
     NativeWindow.contextmenus.add(stringGetter("contextmenu.addToReadingList"),
       NativeWindow.contextmenus.linkOpenableContext,
       function(aTarget) {
+        UITelemetry.addEvent("action.1", "contextmenu", null, "web_reading_list");
+        UITelemetry.addEvent("save.1", "contextmenu", null, "reading_list");
+
         let url = NativeWindow.contextmenus._getLinkURL(aTarget);
         Messaging.sendRequestForResult({
             type: "Reader:AddToList",
