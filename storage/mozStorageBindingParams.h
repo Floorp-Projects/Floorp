23e7f150360de8692569628c342e7b9620104b5b
Nicholas Nethercote <nnethercote@mozilla.com>
2015-10-29 16:04:41 -0700

    Bug 1186791 (part 1) - Replace nsBaseHashtable::EnumerateRead() calls in storage/ with iterators. r=mak.
    --HG--
    extra : rebase_source : 5781d353bec6bef4b96a6f4f6c734e10594b5643

--- storage/mozStorageBindingParams.h
+++ storage/mozStorageBindingParams.h
@@ -105,17 +105,6 @@ public:
 
 private:
   nsInterfaceHashtable<nsCStringHashKey, nsIVariant> mNamedParameters;
-
-  struct NamedParameterIterationClosureThunk
-  {
-    AsyncBindingParams *self;
-    sqlite3_stmt *statement;
-    nsCOMPtr<mozIStorageError> err;
-  };
-
-  static PLDHashOperator iterateOverNamedParameters(const nsACString &aName,
-                                                    nsIVariant *aValue,
-                                                    void *voidClosureThunk);
 };
 
 } // namespace storage
