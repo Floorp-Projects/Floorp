71f165d08bc7dcfcd7a7b80d20873be8bb2248f2
Mike Conley <mconley@mozilla.com>
2015-10-29 13:05:23 -0400

    Bug 1218594 - r=smaug
    --HG--
    extra : rebase_source : f35e3600b2eda4d6b5c6c18868f10b2d2d9310f4

--- dom/ipc/TabParent.cpp
+++ dom/ipc/TabParent.cpp
@@ -789,10 +789,10 @@ TabParent::RecvCreateWindow(PBrowserParent* aNewTab,
                             const bool& aCalledFromJS,
                             const bool& aPositionSpecified,
                             const bool& aSizeSpecified,
-                            const nsString& aURI,
+                            const nsCString& aURI,
                             const nsString& aName,
                             const nsCString& aFeatures,
-                            const nsString& aBaseURI,
+                            const nsCString& aBaseURI,
                             nsresult* aResult,
                             bool* aWindowIsNew,
                             InfallibleTArray<FrameScriptInfo>* aFrameScripts,
@@ -873,7 +873,7 @@ TabParent::RecvCreateWindow(PBrowserParent* aNewTab,
     loadContext->GetUsePrivateBrowsing(&isPrivate);
 
     nsCOMPtr<nsIOpenURIInFrameParams> params = new nsOpenURIInFrameParams();
-    params->SetReferrer(aBaseURI);
+    params->SetReferrer(NS_ConvertUTF8toUTF16(aBaseURI));
     params->SetIsPrivate(isPrivate);
 
     AutoUseNewTab aunt(newTab, aWindowIsNew, aURLToLoad);
@@ -906,7 +906,7 @@ TabParent::RecvCreateWindow(PBrowserParent* aNewTab,
   nsAutoCString finalURIString;
   if (!aURI.IsEmpty()) {
     nsCOMPtr<nsIURI> finalURI;
-    *aResult = NS_NewURI(getter_AddRefs(finalURI), NS_ConvertUTF16toUTF8(aURI).get(), baseURI);
+    *aResult = NS_NewURI(getter_AddRefs(finalURI), aURI.get(), baseURI);
 
     if (NS_WARN_IF(NS_FAILED(*aResult)))
       return true;
@@ -918,11 +918,15 @@ TabParent::RecvCreateWindow(PBrowserParent* aNewTab,
 
   AutoUseNewTab aunt(newTab, aWindowIsNew, aURLToLoad);
 
-  const char* features = aFeatures.Length() ? aFeatures.get() : nullptr;
+  // If nsWindowWatcher::OpenWindowInternal was passed a nullptr for the URI
+  // to open in the content process, we indicate that by sending up a voided
+  // nsString (since primitives are not nullable). If we detect the voided
+  // nsString, we know that we need to send OpenWindow2 a nullptr for the URI.
+  const char* uri = aURI.IsVoid() ? nullptr : finalURIString.get();
+  const char* name = aName.IsVoid() ? nullptr : NS_ConvertUTF16toUTF8(aName).get();
+  const char* features = aFeatures.IsVoid() ? nullptr : aFeatures.get();
 
-  *aResult = pwwatch->OpenWindow2(parent, finalURIString.get(),
-                                  NS_ConvertUTF16toUTF8(aName).get(),
-                                  features, aCalledFromJS,
+  *aResult = pwwatch->OpenWindow2(parent, uri, name, features, aCalledFromJS,
                                   false, false, this, nullptr, getter_AddRefs(window));
 
   if (NS_WARN_IF(NS_FAILED(*aResult)))
