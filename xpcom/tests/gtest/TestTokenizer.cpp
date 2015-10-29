36cd45a252b49b6915849b55a674efe92e754917
Honza Bambas <honzab.moz@firemni.cz>
2015-10-29 05:58:00 +0100

    Bug 1219726 - Nit: Tokenizer.ShortcutChecks gtest should use nsDependentCSubstring. r=nfroyd

--- xpcom/tests/gtest/TestTokenizer.cpp
+++ xpcom/tests/gtest/TestTokenizer.cpp
@@ -385,7 +385,7 @@ TEST(Tokenizer, ShortcutChecks)
   Tokenizer p("test1 test2,123");
 
   nsAutoCString test1;
-  nsDependentCString test2;
+  nsDependentCSubstring test2;
   char comma;
   uint32_t integer;
 
