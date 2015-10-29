6c1d1c39b23c4ce8fc26263b7b6832fb69b086fd
Mark Finkle <mfinkle@mozilla.com>
2015-10-29 18:44:44 -0400

    Bug 1219895 - Cleanup the 'share.1' telemetry probes by adding extra context. r=margaret, a=sylvestre
    --HG--
    extra : source : 35d014ddd2a47fc9f9827c27cb5890517bbb350b

--- mobile/android/base/widget/GeckoActionProvider.java
+++ mobile/android/base/widget/GeckoActionProvider.java
@@ -251,7 +251,7 @@ public class GeckoActionProvider {
             chooseActivity(item.getItemId());
 
             // Context: Sharing via chrome mainmenu list (no explicit session is active)
-            Telemetry.sendUIEvent(TelemetryContract.Event.SHARE, TelemetryContract.Method.LIST);
+            Telemetry.sendUIEvent(TelemetryContract.Event.SHARE, TelemetryContract.Method.LIST, "actionprovider");
             return true;
         }
 
@@ -261,7 +261,7 @@ public class GeckoActionProvider {
             chooseActivity(index);
 
             // Context: Sharing via chrome mainmenu and content contextmenu quickshare (no explicit session is active)
-            Telemetry.sendUIEvent(TelemetryContract.Event.SHARE, TelemetryContract.Method.BUTTON);
+            Telemetry.sendUIEvent(TelemetryContract.Event.SHARE, TelemetryContract.Method.BUTTON, "actionprovider");
         }
     }
 
