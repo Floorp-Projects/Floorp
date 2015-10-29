82e1790b97a1b1803f89e925b8bbc2fdaf32b20c
Yury Delendik <ydelendik@mozilla.com>
2015-10-29 16:05:58 -0500

    Bug 1016737 - Wait for PDF Viewer to release resources during testing. r=bdahl

--- browser/extensions/pdfjs/test/browser_pdfjs_main.js
+++ browser/extensions/pdfjs/test/browser_pdfjs_main.js
@@ -32,7 +32,9 @@ function test() {
 
     // Runs tests after all 'load' event handlers have fired off
     window.addEventListener("documentload", function() {
-      runTests(document, window, tab, finish);
+      runTests(document, window, tab, function () {
+        closePDFViewer(window, finish);
+      });
     }, false, true);
   }, true);
 }
@@ -45,6 +47,8 @@ function runTests(document, window, tab, callback) {
   //
   ok(document.querySelector('div#viewer'), "document content has viewer UI");
   ok('PDFJS' in window.wrappedJSObject, "window content has PDFJS object");
+  ok('PDFViewerApplication' in window.wrappedJSObject,
+     "window content has viewer object");
 
   //
   // Browser Find
@@ -83,4 +87,12 @@ function runTests(document, window, tab, callback) {
   ok(viewBookmark.href.length > 0, 'viewBookmark button has href');
 
   callback();
+}
+
+/**
+ * Destroys PDF.js viewer opened document.
+ */
+function closePDFViewer(window, callback) {
+  var viewer = window.wrappedJSObject.PDFViewerApplication;
+  viewer.close().then(callback);
 }
