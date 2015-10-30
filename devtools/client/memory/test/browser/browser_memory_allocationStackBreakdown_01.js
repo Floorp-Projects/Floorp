50027194c9f80dcd6afe6ca08651d3a942f8e6b0
Nick Fitzgerald <fitzgen@gmail.com>
2015-10-30 13:31:40 -0700

    Bug 1219385 - Small fixes for heap view's tree items; r=jsantell, a=blanket-sylvestre, l10n=blanket-sylvestre CLOSED TREE
    * Display function display name when breaking down by allocation stack.
    * Properly check or uncheck the "Invert tree" checkbox depending on if we are
      inverting the heap view's tree or not.

--- devtools/client/memory/test/browser/browser_memory_allocationStackBreakdown_01.js
+++ devtools/client/memory/test/browser/browser_memory_allocationStackBreakdown_01.js
@@ -10,7 +10,7 @@ const { breakdowns } = require("devtools/client/memory/constants");
 const { toggleRecordingAllocationStacks } = require("devtools/client/memory/actions/allocations");
 const { takeSnapshotAndCensus } = require("devtools/client/memory/actions/snapshot");
 const breakdownActions = require("devtools/client/memory/actions/breakdown");
-const { toggleInverted } = require("devtools/client/memory/actions/inverted");
+const { toggleInvertedAndRefresh } = require("devtools/client/memory/actions/inverted");
 
 const TEST_URL = "http://example.com/browser/devtools/client/memory/test/browser/doc_steady_allocation.html";
 
@@ -18,9 +18,12 @@ this.test = makeMemoryTest(TEST_URL, function* ({ tab, panel }) {
   const heapWorker = panel.panelWin.gHeapAnalysesClient;
   const front = panel.panelWin.gFront;
   const { getState, dispatch } = panel.panelWin.gStore;
+  const doc = panel.panelWin.document;
 
-  dispatch(toggleInverted());
+  yield dispatch(toggleInvertedAndRefresh(heapWorker));
   ok(getState().inverted, true);
+  ok(doc.getElementById("invert-tree-checkbox").checked,
+     "invert-tree-checkbox should be checked");
 
   dispatch(breakdownActions.setBreakdown(breakdowns.allocationStack.breakdown));
   is(getState().breakdown.by, "allocationStack");
@@ -33,7 +36,8 @@ this.test = makeMemoryTest(TEST_URL, function* ({ tab, panel }) {
 
   yield dispatch(takeSnapshotAndCensus(front, heapWorker));
 
-  const doc = panel.panelWin.document;
-  ok(doc.querySelector(".frame-link-function-display-name"),
-     "Should have rendered some allocation stack tree items");
+  const names = [...doc.querySelectorAll(".frame-link-function-display-name")];
+  ok(names.length, "Should have rendered some allocation stack tree items");
+  ok(names.some(e => !!e.textContent.trim()),
+     "And at least some of them should have functionDisplayNames");
 });
