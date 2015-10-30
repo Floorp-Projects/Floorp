72c9dbedd27f4fff859ecc01e1e2c2a6a893604a
Aaron Klotz <aklotz@mozilla.com>
2015-10-30 11:46:04 -0600

    Bug 1220010: Temporarily disable asyncInit; r=me, a=bustage CLOSED TREE

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2556,13 +2556,7 @@ pref("dom.ipc.plugins.reportCrashURL", true);
 // Defaults to 30 seconds.
 pref("dom.ipc.plugins.unloadTimeoutSecs", 30);
 
-// Asynchronous plugin initialization should only be enabled on non-e10s
-// channels until some remaining bugs are resolved.
-#ifdef E10S_TESTING_ONLY
 pref("dom.ipc.plugins.asyncInit.enabled", false);
-#else
-pref("dom.ipc.plugins.asyncInit.enabled", true);
-#endif
 
 pref("dom.ipc.processCount", 1);
 
