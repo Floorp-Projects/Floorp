732809d0108e4dd99f3f0a42dabb0ba1ccbfb5a1
Nathan Froyd <froydnj@mozilla.com>
2015-10-30 11:45:39 -0400

    Bug 1220190 - use UniquePtr<T[]> instead of delete[] calls in layout/xul/; r=dholbert

--- layout/xul/grid/nsGrid.h
+++ layout/xul/grid/nsGrid.h
@@ -10,6 +10,7 @@
 #include "nsStackLayout.h"
 #include "nsIGridPart.h"
 #include "nsCOMPtr.h"
+#include "mozilla/UniquePtr.h"
 
 class nsBoxLayoutState;
 class nsGridCell;
@@ -84,8 +85,9 @@ private:
 
   void FreeMap();
   void FindRowsAndColumns(nsIFrame** aRows, nsIFrame** aColumns);
-  void BuildRows(nsIFrame* aBox, int32_t aSize, nsGridRow** aColumnsRows, bool aIsHorizontal = true);
-  nsGridCell* BuildCellMap(int32_t aRows, int32_t aColumns);
+  mozilla::UniquePtr<nsGridRow[]> BuildRows(nsIFrame* aBox, int32_t aSize,
+                                            bool aIsHorizontal = true);
+  mozilla::UniquePtr<nsGridCell[]> BuildCellMap(int32_t aRows, int32_t aColumns);
   void PopulateCellMap(nsGridRow* aRows, nsGridRow* aColumns, int32_t aRowCount, int32_t aColumnCount, bool aIsHorizontal = true);
   void CountRowsColumns(nsIFrame* aBox, int32_t& aRowCount, int32_t& aComputedColumnCount);
   void SetLargestSize(nsSize& aSize, nscoord aHeight, bool aIsHorizontal = true);
@@ -96,10 +98,10 @@ private:
   nsIFrame* mBox;
 
   // an array of row object
-  nsGridRow* mRows;
+  mozilla::UniquePtr<nsGridRow[]> mRows;
 
   // an array of columns objects.
-  nsGridRow* mColumns;
+  mozilla::UniquePtr<nsGridRow[]> mColumns;
 
   // the first in the <grid> that implements the <rows> tag.
   nsIFrame* mRowsBox;
@@ -120,7 +122,7 @@ private:
   int32_t mExtraColumnCount;
 
   // x,y array of cells in the rows and columns
-  nsGridCell* mCellMap;
+  mozilla::UniquePtr<nsGridCell[]> mCellMap;
 
   // a flag that when true suppresses all other MarkDirties. This
   // prevents lots of extra work being done.
