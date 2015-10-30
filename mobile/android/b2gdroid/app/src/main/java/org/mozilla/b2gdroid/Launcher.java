0d484996a61054dc84e73f0ab2c6ccfd7ce420ab
sgiles <sgiles@mozilla.com>
2015-10-30 16:25:16 +0000

    Bug 1202752 - Prevent the android utility tray from opening r=fabrice

--- mobile/android/b2gdroid/app/src/main/java/org/mozilla/b2gdroid/Launcher.java
+++ mobile/android/b2gdroid/app/src/main/java/org/mozilla/b2gdroid/Launcher.java
@@ -9,12 +9,17 @@ import java.util.Date;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.PixelFormat;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 
 import org.json.JSONObject;
 import org.json.JSONException;
@@ -46,6 +51,8 @@ public class Launcher extends FragmentActivity
     private SettingsMapper        mSettings;
     private GeckoEventReceiver    mGeckoEventReceiver;
     private RemoteGeckoEventProxy mGeckoEventProxy;
+
+    private View mDisableStatusBarView = null;
 
     private static final long   kHomeRepeat = 2;
     private static final long   kHomeDelay  = 500; // delay in ms to tap kHomeRepeat times.
@@ -110,6 +117,46 @@ public class Launcher extends FragmentActivity
         });
     }
 
+    private View getStatusBarOverlay() {
+        if (mDisableStatusBarView != null) {
+            return mDisableStatusBarView;
+        }
+
+        mDisableStatusBarView = new View(this);
+
+        mDisableStatusBarView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent ev) {
+                // Pass the touch event down to the GeckoView
+                Launcher.this.findViewById(R.id.gecko_view).dispatchTouchEvent(ev);
+                return true;
+            }
+        });
+
+        WindowManager.LayoutParams handleParams = new WindowManager.LayoutParams(
+                // Fill the width of the screen
+                WindowManager.LayoutParams.FILL_PARENT,
+                // Arbitrary value to cover the edge of the top of the screen to interrupt the gesture.
+                // This value was found through trial and error on a large screen L and a Z3C on KK
+                25,
+                // display over everything
+                WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
+                // Prevent events from capturing in other views beneath this
+                // one
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+                // Allows the View to receive touch events so we can pass them
+                // to Gecko
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                // Draw over status bar area
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+                // Draw transparent
+                PixelFormat.TRANSPARENT);
+
+        handleParams.gravity = Gravity.TOP;
+        WindowManager manager = ((WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE));
+        manager.addView(mDisableStatusBarView, handleParams);
+        return mDisableStatusBarView;
+    }
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -138,6 +185,9 @@ public class Launcher extends FragmentActivity
         // service, so we need to forward them to the remote service
         EventDispatcher.getInstance().registerGeckoThreadListener(mGeckoEventProxy,
             "Android:NotificationOpened");
+
+        // Initialize status bar overlay
+        getStatusBarOverlay();
 
         setContentView(R.layout.launcher);
 
@@ -228,11 +278,14 @@ public class Launcher extends FragmentActivity
 
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
+            getStatusBarOverlay().setVisibility(View.VISIBLE);
             findViewById(R.id.main_layout).setSystemUiVisibility(
                      View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     );
+        } else {
+            getStatusBarOverlay().setVisibility(View.INVISIBLE);
         }
     }
 
