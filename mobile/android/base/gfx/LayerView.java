a9e5c5692433830ae80777a61a50a9752547f355
Jim Chen <nchen@mozilla.com>
2015-10-30 08:30:00 +0100

    Bug 1213888 - Block LayerView descendants from interfering with focus. r=kats, a=ritu
    Using a hardware keyboard makes Android leave "touch mode", and when
    leaving touch mode from a ViewGroup that prefers descendant
    focusability, which is what a ScrollView is, Android will shift the
    focus to the next available View, and cause our LayerView to lose focus.
    The fix is to set the descendant focusability to blocking.

--- mobile/android/base/gfx/LayerView.java
+++ mobile/android/base/gfx/LayerView.java
@@ -267,6 +267,10 @@ public class LayerView extends ScrollView implements Tabs.OnTabsChangedListener 
 
     @Override
     protected void onAttachedToWindow() {
+        // We are adding descendants to this LayerView, but we don't want the
+        // descendants to affect the way LayerView retains its focus.
+        setDescendantFocusability(FOCUS_BLOCK_DESCENDANTS);
+
         // This check should not be done before the view is attached to a window
         // as hardware acceleration will not be enabled at that point.
         // We must create and add the SurfaceView instance before the view tree
