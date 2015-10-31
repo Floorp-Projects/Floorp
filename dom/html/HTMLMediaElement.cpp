ae9778a6b2f6715a95b4398f7c9e734644d46912
Andrea Marchesini <amarchesini@mozilla.com>
2015-10-31 15:21:40 +0000

    Bug 1207546 - Integrate WebRTC with audio channels, r=roc
    --HG--
    rename : dom/base/test/test_audioNotification.html => dom/base/test/test_audioNotificationStream.html

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -4770,6 +4770,11 @@ HTMLMediaElement::IsPlayingThroughTheAudioChannel() const
     return true;
   }
 
+  // If we are playing an external stream.
+  if (mSrcAttrStream) {
+    return true;
+  }
+
   return false;
 }
 
