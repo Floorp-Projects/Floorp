cd0dd2fe66ccf736334a592796237cc87aded48e
Ehsan Akhgari <ehsan@mozilla.com>
2015-11-02 11:27:00 -0500

    Bug 1214305 - Part 5: Use the secure upgraded channel URI in FetchEventRunnable::Init; r=jdm
    This ensures that FetchEvent.request.url will have the correct value
    for secure upgraded channels.

--- dom/workers/ServiceWorkerPrivate.cpp
+++ dom/workers/ServiceWorkerPrivate.cpp
@@ -974,7 +974,7 @@ public:
     NS_ENSURE_SUCCESS(rv, rv);
 
     nsCOMPtr<nsIURI> uri;
-    rv = channel->GetURI(getter_AddRefs(uri));
+    rv = mInterceptedChannel->GetSecureUpgradedChannelURI(getter_AddRefs(uri));
     NS_ENSURE_SUCCESS(rv, rv);
 
     rv = uri->GetSpec(mSpec);
