6a00383dfd0bf7b2de7edeb390466a8257a8b2f6
Karl Tomlinson <karlt+@karlt.net>
2015-11-03 14:14:40 +1300

    bug 1221831 remove now-unnecessary m_preDelayBuffer r=padenot

--- dom/media/webaudio/blink/ReverbConvolverStage.cpp
+++ dom/media/webaudio/blink/ReverbConvolverStage.cpp
@@ -70,16 +70,7 @@ ReverbConvolverStage::ReverbConvolverStage(const float* impulseResponse, size_t,
             totalDelay -= halfSize;
     }
 
-    // We divide up the total delay, into pre and post delay sections so that we can schedule at exactly the moment when the FFT will happen.
-    m_preDelayLength = 0;
-    m_postDelayLength = totalDelay - m_preDelayLength;
-    m_preReadWriteIndex = 0;
-    m_framesProcessed = 0; // total frames processed so far
-
-    size_t delayBufferSize = m_preDelayLength < fftSize ? fftSize : m_preDelayLength;
-    delayBufferSize = delayBufferSize < renderSliceSize ? renderSliceSize : delayBufferSize;
-    m_preDelayBuffer.SetLength(delayBufferSize);
-    PodZero(m_preDelayBuffer.Elements(), m_preDelayBuffer.Length());
+    m_postDelayLength = totalDelay;
 }
 
 size_t ReverbConvolverStage::sizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf) const
@@ -94,7 +85,6 @@ size_t ReverbConvolverStage::sizeOfIncludingThis(mozilla::MallocSizeOf aMallocSi
         amount += m_fftConvolver->sizeOfIncludingThis(aMallocSizeOf);
     }
 
-    amount += m_preDelayBuffer.ShallowSizeOfExcludingThis(aMallocSizeOf);
     amount += m_temporaryBuffer.ShallowSizeOfExcludingThis(aMallocSizeOf);
     amount += m_directKernel.ShallowSizeOfExcludingThis(aMallocSizeOf);
 
@@ -118,65 +108,29 @@ void ReverbConvolverStage::process(const float* source, size_t framesToProcess)
     if (!source)
         return;
     
-    // Deal with pre-delay stream : note special handling of zero delay.
-
-    const float* preDelayedSource;
-    float* preDelayedDestination;
     float* temporaryBuffer;
     bool isTemporaryBufferSafe = false;
-    if (m_preDelayLength > 0) {
-        // Handles both the read case (call to process() ) and the write case (memcpy() )
-        bool isPreDelaySafe = m_preReadWriteIndex + framesToProcess <= m_preDelayBuffer.Length();
-        MOZ_ASSERT(isPreDelaySafe);
-        if (!isPreDelaySafe)
-            return;
-
-        isTemporaryBufferSafe = framesToProcess <= m_temporaryBuffer.Length();
-
-        preDelayedDestination = m_preDelayBuffer.Elements() + m_preReadWriteIndex;
-        preDelayedSource = preDelayedDestination;
-        temporaryBuffer = m_temporaryBuffer.Elements();
-    } else {
-        // Zero delay
-        preDelayedDestination = 0;
-        preDelayedSource = source;
-        temporaryBuffer = m_preDelayBuffer.Elements();
-        
-        isTemporaryBufferSafe = framesToProcess <= m_preDelayBuffer.Length();
-    }
+    isTemporaryBufferSafe = framesToProcess <= m_temporaryBuffer.Length();
+    temporaryBuffer = m_temporaryBuffer.Elements();
     
     MOZ_ASSERT(isTemporaryBufferSafe);
     if (!isTemporaryBufferSafe)
         return;
 
-    if (m_framesProcessed < m_preDelayLength) {
-        // For the first m_preDelayLength frames don't process the convolver, instead simply buffer in the pre-delay.
-        // But while buffering the pre-delay, we still need to update our index.
-        m_accumulationBuffer->updateReadIndex(&m_accumulationReadIndex, framesToProcess);
-    } else {
-        // Now, run the convolution (into the delay buffer).
-        // An expensive FFT will happen every fftSize / 2 frames.
-        // We process in-place here...
-        if (!m_directMode)
-            m_fftConvolver->process(m_fftKernel, preDelayedSource, temporaryBuffer, framesToProcess);
-        else
-            m_directConvolver->process(&m_directKernel, preDelayedSource, temporaryBuffer, framesToProcess);
+    // Now, run the convolution (into the delay buffer).
+    // An expensive FFT will happen every fftSize / 2 frames.
+    // We process in-place here...
+    if (!m_directMode)
+        m_fftConvolver->process(m_fftKernel, source,
+                                temporaryBuffer, framesToProcess);
+    else
+        m_directConvolver->process(&m_directKernel, source,
+                                   temporaryBuffer, framesToProcess);
 
-        // Now accumulate into reverb's accumulation buffer.
-        m_accumulationBuffer->accumulate(temporaryBuffer, framesToProcess, &m_accumulationReadIndex, m_postDelayLength);
-    }
-
-    // Finally copy input to pre-delay.
-    if (m_preDelayLength > 0) {
-        memcpy(preDelayedDestination, source, sizeof(float) * framesToProcess);
-        m_preReadWriteIndex += framesToProcess;
-
-        MOZ_ASSERT(m_preReadWriteIndex <= m_preDelayLength);
-        if (m_preReadWriteIndex >= m_preDelayLength)
-            m_preReadWriteIndex = 0;
-    }
-
-    m_framesProcessed += framesToProcess;
+    // Now accumulate into reverb's accumulation buffer.
+    m_accumulationBuffer->accumulate(temporaryBuffer, framesToProcess,
+                                     &m_accumulationReadIndex,
+                                     m_postDelayLength);
 }
 
 } // namespace WebCore
