aa60675f7f82a8622879216f8f109de66456a670
Nigel Babu <nigelbabu@gmail.com>
2015-11-03 11:59:05 +0530

    Backed out changeset 19e021ef5ba1 (bug 1216288)

--- layout/ipc/RenderFrameParent.cpp
+++ layout/ipc/RenderFrameParent.cpp
@@ -388,9 +388,7 @@ RenderFrameParent::BuildLayer(nsDisplayListBuilder* aBuilder,
     // draw a manager's subtree.  The latter is bad bad bad, but the the
     // MOZ_ASSERT() above will flag it.  Returning nullptr here will just
     // cause the shadow subtree not to be rendered.
-    if (!aContainerParameters.mForEventsOnly) {
-      NS_WARNING("Remote iframe not rendered");
-    }
+    NS_WARNING("Remote iframe not rendered");
     return nullptr;
   }
 
