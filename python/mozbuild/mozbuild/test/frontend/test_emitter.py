762aba02cd6c78dcf3eccc180ae7b64632656ee4
Mike Hommey <mh+mozilla@glandium.org>
2015-11-04 14:20:20 +0900

    Bug 1221453 - Use ObjDirPaths for GENERATED_INCLUDES and merge with LOCAL_INCLUDES. r=gps

--- python/mozbuild/mozbuild/test/frontend/test_emitter.py
+++ python/mozbuild/mozbuild/test/frontend/test_emitter.py
@@ -18,7 +18,6 @@ from mozbuild.frontend.data import (
     DirectoryTraversal,
     Exports,
     GeneratedFile,
-    GeneratedInclude,
     GeneratedSources,
     HostDefines,
     HostSources,
@@ -661,10 +660,19 @@ class TestEmitterBasic(unittest.TestCase):
         reader = self.reader('generated_includes')
         objs = self.read_topsrcdir(reader)
 
-        generated_includes = [o.path for o in objs if isinstance(o, GeneratedInclude)]
+        generated_includes = [o.path for o in objs if isinstance(o, LocalInclude)]
         expected = [
-            '/bar/baz',
-            'foo',
+            '!/bar/baz',
+            '!foo',
+        ]
+
+        self.assertEqual(generated_includes, expected)
+
+        generated_includes = [o.path.full_path
+                              for o in objs if isinstance(o, LocalInclude)]
+        expected = [
+            mozpath.join(reader.config.topobjdir, 'bar/baz'),
+            mozpath.join(reader.config.topobjdir, 'foo'),
         ]
 
         self.assertEqual(generated_includes, expected)
