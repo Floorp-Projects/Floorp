02131c4fded4ebfaecd848fd0686d33f3e792731
Luis Miguel [:quicksaver] <quicksaver@gmail.com>
2015-11-04 15:26:40 +0000

    Bug 1218351 - (e10s) Don't lose initial typed characters when opening the findbar; r=mikedeboer Moved some of the routine from content to chrome process. The drawback is that now the content process must wait for a response on every keypress.

--- toolkit/content/browser-content.js
+++ toolkit/content/browser-content.js
@@ -580,15 +580,10 @@ var FindBar = {
   FIND_NORMAL: 0,
   FIND_TYPEAHEAD: 1,
   FIND_LINKS: 2,
-  FAYT_LINKS_KEY: "'".charCodeAt(0),
-  FAYT_TEXT_KEY: "/".charCodeAt(0),
 
   _findMode: 0,
-  _findAsYouType: false,
 
   init() {
-    this._findAsYouType =
-      Services.prefs.getBoolPref("accessibility.typeaheadfind");
     addMessageListener("Findbar:UpdateState", this);
     Services.els.addSystemEventListener(global, "keypress", this, false);
     Services.els.addSystemEventListener(global, "mouseup", this, false);
@@ -598,7 +593,6 @@ var FindBar = {
     switch (msg.name) {
       case "Findbar:UpdateState":
         this._findMode = msg.data.findMode;
-        this._findAsYouType = msg.data.findAsYouType;
         break;
     }
   },
@@ -632,16 +626,9 @@ var FindBar = {
     if (event.ctrlKey || event.altKey || event.metaKey || event.defaultPrevented) {
       return;
     }
-    // Not interested in random keypresses most of the time:
-    if (this._findMode == this.FIND_NORMAL && !this._findAsYouType &&
-        event.charCode != this.FAYT_LINKS_KEY && event.charCode != this.FAYT_TEXT_KEY) {
-      return;
-    }
 
     // Check the focused element etc.
-    if (!this._shouldFastFind()) {
-      return;
-    }
+    let shouldFastFind = this._shouldFastFind();
 
     let fakeEvent = {};
     for (let k in event) {
@@ -650,7 +637,10 @@ var FindBar = {
       }
     }
     // sendSyncMessage returns an array of the responses from all listeners
-    let rv = sendSyncMessage("Findbar:Keypress", fakeEvent);
+    let rv = sendSyncMessage("Findbar:Keypress", {
+      fakeEvent: fakeEvent,
+      shouldFastFind: shouldFastFind
+    });
     if (rv.indexOf(false) !== -1) {
       event.preventDefault();
       return false;
