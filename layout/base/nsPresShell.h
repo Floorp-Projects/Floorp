6cdc913990b55cb258b312cc176df3605a7b7cc9
Nicholas Nethercote <nnethercote@mozilla.com>
2015-11-04 20:55:16 -0800

    Bug 1059014 (part 2) - Remove support for reflow on zoom event pending. r=dbaron.
    This basically reverts part 2 of bug 836565.
    
    --HG--
    extra : rebase_source : 4b047c51b01936e079d9e785bb1653863eacc4cd

--- layout/base/nsPresShell.h
+++ layout/base/nsPresShell.h
@@ -436,10 +436,8 @@ protected:
    * Callback handler for whether reflow happened.
    *
    * @param aInterruptible Whether or not reflow interruption is allowed.
-   * @param aWasInterrupted Whether or not the reflow was interrupted earlier.
-   *
    */
-  void     DidDoReflow(bool aInterruptible, bool aWasInterrupted);
+  void     DidDoReflow(bool aInterruptible);
   // ProcessReflowCommands returns whether we processed all our dirty roots
   // without interruptions.
   bool     ProcessReflowCommands(bool aInterruptible);
