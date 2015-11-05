a1611908e7ba3f6e150497de6622355922141ee2
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-11-05 11:29:12 +0100

    Backed out changeset f6ec673b3b76 (bug 1190018) for W-e10s(2) test failures on a CLOSED TREE

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -31,7 +31,6 @@
 #include "BlobParent.h"
 #include "CrashReporterParent.h"
 #include "GMPServiceParent.h"
-#include "HandlerServiceParent.h"
 #include "IHistory.h"
 #include "imgIContainer.h"
 #include "mozIApplication.h"
@@ -3909,21 +3908,6 @@ ContentParent::DeallocPExternalHelperAppParent(PExternalHelperAppParent* aServic
 {
     ExternalHelperAppParent *parent = static_cast<ExternalHelperAppParent *>(aService);
     parent->Release();
-    return true;
-}
-
-PHandlerServiceParent*
-ContentParent::AllocPHandlerServiceParent()
-{
-    HandlerServiceParent* actor =  new HandlerServiceParent();
-    actor->AddRef();
-    return actor;
-}
-
-bool
-ContentParent::DeallocPHandlerServiceParent(PHandlerServiceParent* aHandlerServiceParent)
-{
-    static_cast<HandlerServiceParent*>(aHandlerServiceParent)->Release();
     return true;
 }
 
