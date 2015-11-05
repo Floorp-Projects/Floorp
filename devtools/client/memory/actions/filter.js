7d323eb6c71e50f61af19c39bd7c6ca18d273781
Nick Fitzgerald <fitzgen@gmail.com>
2015-11-04 16:12:31 -0800

    Bug 1221673 - Add support for filtering censuses to memory tool's frontend; r=jsantell,vporof

---
+++ devtools/client/memory/actions/filter.js
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const { clearTimeout, setTimeout } = require("sdk/timers");
+const { actions } = require("../constants");
+const { refreshSelectedCensus } = require("./snapshot");
+
+const setFilterString = exports.setFilterString = function (filterString) {
+  return {
+    type: actions.SET_FILTER_STRING,
+    filter: filterString
+  };
+};
+
+// The number of milliseconds we should wait before kicking off a new census
+// when the filter string is updated. This helps us avoid doing any work while
+// the user is still typing.
+const FILTER_INPUT_DEBOUNCE_MS = 250;
+
+// The timer id for the debounced census refresh.
+let timerId = null;
+
+exports.setFilterStringAndRefresh = function (filterString, heapWorker) {
+  return function* (dispatch, getState) {
+    dispatch(setFilterString(filterString));
+
+    if (timerId !== null) {
+      clearTimeout(timerId);
+    }
+
+    timerId = setTimeout(() => dispatch(refreshSelectedCensus(heapWorker)),
+                         FILTER_INPUT_DEBOUNCE_MS);
+  };
+};
