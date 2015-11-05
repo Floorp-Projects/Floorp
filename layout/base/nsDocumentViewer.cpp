cb29599441f40b5c09b3da348d7c8b5f9ace783a
Nicholas Nethercote <nnethercote@mozilla.com>
2015-11-04 20:25:01 -0800

    Bug 1059014 (part 1) - Remove support for max line box width. r=dbaron.
    This basically reverts bug 780258.

--- layout/base/nsDocumentViewer.cpp
+++ layout/base/nsDocumentViewer.cpp
@@ -2832,11 +2832,6 @@ nsDocumentViewer::CallChildren(CallChildFunc aFunc, void* aClosure)
   }
 }
 
-struct LineBoxInfo
-{
-  nscoord mMaxLineBoxWidth;
-};
-
 static void
 ChangeChildPaintingEnabled(nsIContentViewer* aChild, void* aClosure)
 {
@@ -2846,13 +2841,6 @@ ChangeChildPaintingEnabled(nsIContentViewer* aChild, void* aClosure)
   } else {
     aChild->PausePainting();
   }
-}
-
-static void
-ChangeChildMaxLineBoxWidth(nsIContentViewer* aChild, void* aClosure)
-{
-  struct LineBoxInfo* lbi = (struct LineBoxInfo*) aClosure;
-  aChild->ChangeMaxLineBoxWidth(lbi->mMaxLineBoxWidth);
 }
 
 struct ZoomInfo
@@ -3307,24 +3295,6 @@ nsDocumentViewer::ResumePainting()
   nsIPresShell* presShell = GetPresShell();
   if (presShell) {
     presShell->ResumePainting();
-  }
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsDocumentViewer::ChangeMaxLineBoxWidth(int32_t aMaxLineBoxWidth)
-{
-  // Change the max line box width for all children.
-  struct LineBoxInfo lbi = { aMaxLineBoxWidth };
-  CallChildren(ChangeChildMaxLineBoxWidth, &lbi);
-
-  // Now, change our max line box width.
-  // Convert to app units, since our input is in CSS pixels.
-  nscoord mlbw = nsPresContext::CSSPixelsToAppUnits(aMaxLineBoxWidth);
-  nsIPresShell* presShell = GetPresShell();
-  if (presShell) {
-    presShell->SetMaxLineBoxWidth(mlbw);
   }
 
   return NS_OK;
