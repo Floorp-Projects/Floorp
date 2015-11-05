db5d8a6747a68e6e310ddd68b07f8efcd9232b58
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-11-05 11:29:12 +0100

    Backed out changeset f6ec673b3b76 (bug 1190018) for W-e10s(2) test failures on a CLOSED TREE

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -18,7 +18,6 @@
 #include "CrashReporterChild.h"
 #include "GeckoProfiler.h"
 #include "TabChild.h"
-#include "HandlerServiceChild.h"
 
 #include "mozilla/Attributes.h"
 #include "mozilla/LookAndFeel.h"
@@ -1892,20 +1891,6 @@ ContentChild::DeallocPExternalHelperAppChild(PExternalHelperAppChild* aService)
 {
     ExternalHelperAppChild *child = static_cast<ExternalHelperAppChild*>(aService);
     child->Release();
-    return true;
-}
-
-PHandlerServiceChild*
-ContentChild::AllocPHandlerServiceChild()
-{
-    HandlerServiceChild* actor =  new HandlerServiceChild();
-    actor->AddRef();
-    return actor;
-}
-
-bool ContentChild::DeallocPHandlerServiceChild(PHandlerServiceChild* aHandlerServiceChild)
-{
-    static_cast<HandlerServiceChild*>(aHandlerServiceChild)->Release();
     return true;
 }
 
