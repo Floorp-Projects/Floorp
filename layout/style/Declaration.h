a497d9a85d2fadaca896153ca7bb4f6f939adbd6
L. David Baron <dbaron@dbaron.org>
2015-11-05 16:44:08 +0800

    Bug 978833 patch 3 - Make css::Declaration implement nsIStyleRule.  r=heycam
    We switch to using this implementation instead of the one in
    css::StyleRule in patch 12.
    
    (Yes, implementing QueryInterface for a CID is ugly, but it's the same
    thing StyleRule does.  Unfortunately now we'll need to have it in both
    places.)

--- layout/style/Declaration.h
+++ layout/style/Declaration.h
@@ -23,9 +23,15 @@
 #include "nsCSSDataBlock.h"
 #include "nsCSSProperty.h"
 #include "nsCSSProps.h"
+#include "nsIStyleRule.h"
 #include "nsStringFwd.h"
 #include "nsTArray.h"
 #include <stdio.h>
+
+// feec07b8-3fe6-491e-90d5-cc93f853e048
+#define NS_CSS_DECLARATION_IMPL_CID \
+{ 0xfeec07b8, 0x3fe6, 0x491e, \
+  { 0x90, 0xd5, 0xcc, 0x93, 0xf8, 0x53, 0xe0, 0x48 } }
 
 namespace mozilla {
 namespace css {
@@ -38,7 +44,7 @@ namespace css {
 // |RuleMatched| method is called; after that, it must be copied before
 // it can be modified, which is taken care of by |EnsureMutable|.
 
-class Declaration {
+class Declaration final : public nsIStyleRule {
 public:
   /**
    * Construct an |Declaration| that is in an invalid state (null
@@ -49,12 +55,21 @@ public:
 
   Declaration(const Declaration& aCopy);
 
-  NS_INLINE_DECL_REFCOUNTING(Declaration)
+  NS_DECLARE_STATIC_IID_ACCESSOR(NS_CSS_DECLARATION_IMPL_CID)
+
+  NS_DECL_ISUPPORTS
 
 private:
   ~Declaration();
 
 public:
+
+  // nsIStyleRule implementation
+  virtual void MapRuleInfoInto(nsRuleData *aRuleData) override;
+#ifdef DEBUG
+  virtual void List(FILE* out = stdout, int32_t aIndent = 0) const override;
+#endif
+
   /**
    * |ValueAppended| must be called to maintain this declaration's
    * |mOrder| whenever a property is parsed into an expanded data block
@@ -170,17 +185,6 @@ public:
     aExpandedData->Expand(mData.forget(), mImportantData.forget());
   }
 
-  /**
-   * Do what |nsIStyleRule::MapRuleInfoInto| needs to do for a style
-   * rule using this declaration for storage.
-   */
-  void MapNormalRuleInfoInto(nsRuleData *aRuleData) const {
-    MOZ_ASSERT(mData, "called while expanded");
-    mData->MapRuleInfoInto(aRuleData);
-    if (mVariables) {
-      mVariables->MapRuleInfoInto(aRuleData);
-    }
-  }
   void MapImportantRuleInfoInto(nsRuleData *aRuleData) const {
     MOZ_ASSERT(mData, "called while expanded");
     MOZ_ASSERT(mImportantData || mImportantVariables,
@@ -276,10 +280,6 @@ public:
     mVariableOrder.Clear();
   }
 
-#ifdef DEBUG
-  void List(FILE* out = stdout, int32_t aIndent = 0) const;
-#endif
-
 private:
   Declaration& operator=(const Declaration& aCopy) = delete;
   bool operator==(const Declaration& aCopy) const = delete;
@@ -359,6 +359,8 @@ private:
   // also by ToString (hence the 'mutable').
   mutable bool mImmutable;
 };
+
+NS_DEFINE_STATIC_IID_ACCESSOR(Declaration, NS_CSS_DECLARATION_IMPL_CID)
 
 } // namespace css
 } // namespace mozilla
