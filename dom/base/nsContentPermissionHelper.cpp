7c26aaa9474afb887fa3674560dc8831cd2264a3
Sotaro Ikeda <sotaro.ikeda.g@gmail.com>
2015-11-05 16:41:13 -0800

    Bug 1189195 - Fix PContentPermissionRequest shutdown r=fabrice

--- dom/base/nsContentPermissionHelper.cpp
+++ dom/base/nsContentPermissionHelper.cpp
@@ -127,6 +127,7 @@ class ContentPermissionRequestParent : public PContentPermissionRequestParent
  private:
   virtual bool Recvprompt();
   virtual bool RecvNotifyVisibility(const bool& aIsVisible);
+  virtual bool RecvDestroy();
   virtual void ActorDestroy(ActorDestroyReason why);
 };
 
@@ -164,6 +165,13 @@ ContentPermissionRequestParent::RecvNotifyVisibility(const bool& aIsVisible)
     return false;
   }
   mProxy->NotifyVisibility(aIsVisible);
+  return true;
+}
+
+bool
+ContentPermissionRequestParent::RecvDestroy()
+{
+  Unused << PContentPermissionRequestParent::Send__delete__(this);
   return true;
 }
 
@@ -609,7 +617,7 @@ nsContentPermissionRequestProxy::Cancel()
 
   nsTArray<PermissionChoice> emptyChoices;
 
-  Unused << ContentPermissionRequestParent::Send__delete__(mParent, false, emptyChoices);
+  Unused << mParent->SendNotifyResult(false, emptyChoices);
   mParent = nullptr;
   return NS_OK;
 }
@@ -678,7 +686,7 @@ nsContentPermissionRequestProxy::Allow(JS::HandleValue aChoices)
     return NS_ERROR_FAILURE;
   }
 
-  Unused << ContentPermissionRequestParent::Send__delete__(mParent, true, choices);
+  Unused << mParent->SendNotifyResult(true, choices);
   mParent = nullptr;
   return NS_OK;
 }
@@ -732,9 +740,10 @@ RemotePermissionRequest::DoAllow(JS::HandleValue aChoices)
 
 // PContentPermissionRequestChild
 bool
-RemotePermissionRequest::Recv__delete__(const bool& aAllow,
-                                        InfallibleTArray<PermissionChoice>&& aChoices)
+RemotePermissionRequest::RecvNotifyResult(const bool& aAllow,
+                                          InfallibleTArray<PermissionChoice>&& aChoices)
 {
+  Unused << this->SendDestroy();
   mListener->RemoveListener();
   mListener = nullptr;
 
