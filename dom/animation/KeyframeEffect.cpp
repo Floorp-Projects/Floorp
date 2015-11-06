6e809a002a12c351deda82ebc3b47d3d3cf88b4a
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2015-11-06 02:53:00 +0100

    Bug 1216030 - Part 15: Add KeyframeEffectReadOnly::GetCollection. r=bbirtles

--- dom/animation/KeyframeEffect.cpp
+++ dom/animation/KeyframeEffect.cpp
@@ -1784,6 +1784,12 @@ KeyframeEffectReadOnly::GetPresContext() const
   return shell->GetPresContext();
 }
 
+AnimationCollection *
+KeyframeEffectReadOnly::GetCollection() const
+{
+  return mAnimation ? mAnimation->GetCollection() : nullptr;
+}
+
 /* static */ bool
 KeyframeEffectReadOnly::IsGeometricProperty(
   const nsCSSProperty aProperty)
