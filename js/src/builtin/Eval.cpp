8e7e7dac654c576713543a5a65b3be7e630a8d20
Jan de Mooij <jdemooij@mozilla.com>
2015-11-06 19:03:52 +0100

    Bug 1125423 part 4 - Make GetThisValue infallible, stop invoking the Class hook. r=shu

--- js/src/builtin/Eval.cpp
+++ js/src/builtin/Eval.cpp
@@ -273,9 +273,8 @@ EvalKernel(JSContext* cx, const CallArgs& args, EvalType evalType, AbstractFrame
     } else {
         MOZ_ASSERT(args.callee().global() == scopeobj->as<ClonedBlockObject>().global());
 
-        // Use the global as 'this', modulo outerization.
-        if (!GetThisValue(cx, scopeobj, &thisv))
-            return false;
+        // Use the global as 'this' (or the WindowProxy if it's a Window).
+        thisv = GetThisValue(scopeobj);
     }
 
     RootedLinearString linearStr(cx, str->ensureLinear(cx));
@@ -516,9 +515,7 @@ js::ExecuteInGlobalAndReturnScope(JSContext* cx, HandleObject global, HandleScri
     if (!scope)
         return false;
 
-    RootedValue thisv(cx);
-    if (!GetThisValue(cx, global, &thisv))
-        return false;
+    RootedValue thisv(cx, GetThisValue(global));
 
     RootedValue rval(cx);
     if (!ExecuteKernel(cx, script, *scope, thisv, UndefinedValue(), EXECUTE_GLOBAL,
