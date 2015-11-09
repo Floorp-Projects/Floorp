aaf614a2380778a93f123b81ee12d8474618086b
Milan Sreckovic <milan@mozilla.com>
2015-11-09 07:28:00 +0100

    Bug 1222033 - Rename gfxCrash to gfxDevCrash. r=jrmuizel

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -311,11 +311,11 @@ CrashStatsLogForwarder::CrashAction(LogReason aReason)
 #ifndef RELEASE_BUILD
   // Non-release builds crash by default, but will use telemetry
   // if this environment variable is present.
-  static bool useTelemetry = gfxEnv::GfxCrashTelemetry();
+  static bool useTelemetry = gfxEnv::GfxDevCrashTelemetry();
 #else
   // Release builds use telemetry by default, but will crash instead
   // if this environment variable is present.
-  static bool useTelemetry = !gfxEnv::GfxCrashMozCrash();
+  static bool useTelemetry = !gfxEnv::GfxDevCrashMozCrash();
 #endif
 
   if (useTelemetry) {
