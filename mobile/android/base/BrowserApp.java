ad6867e19df7c8784edf0569f047cae97bacb002
Wes Kocher <wkocher@mozilla.com>
2015-11-09 11:28:13 -0800

    Backed out 3 changesets (bug 1107636) for android mochitest bustage CLOSED TREE
    Backed out changeset ced71b78683d (bug 1107636)
    Backed out changeset a1efec5e79de (bug 1107636)
    Backed out changeset 6a8430385299 (bug 1107636)
    
    --HG--
    extra : commitid : ErD0sqMmTe

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -1378,7 +1378,7 @@ public class BrowserApp extends GeckoApp
     }
 
     private void setToolbarMargin(int margin) {
-        ((ViewGroup.MarginLayoutParams) mGeckoLayout.getLayoutParams()).topMargin = margin;
+        ((RelativeLayout.LayoutParams) mGeckoLayout.getLayoutParams()).topMargin = margin;
         mGeckoLayout.requestLayout();
     }
 
