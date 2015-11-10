af37945060ba707aaea3a12280ab248379c3c0ce
Karl Tomlinson <karlt+@karlt.net>
2015-11-10 15:09:52 +1300

    Bug 726483 - remove unnecessary DispatchResized() parameters. r=roc, a=lizzard

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -514,13 +514,14 @@ nsWindow::DispatchDeactivateEvent(void)
 }
 
 void
-nsWindow::DispatchResized(int32_t aWidth, int32_t aHeight)
+nsWindow::DispatchResized()
 {
     if (mWidgetListener) {
-        mWidgetListener->WindowResized(this, aWidth, aHeight);
+        mWidgetListener->WindowResized(this, mBounds.width, mBounds.height);
     }
     if (mAttachedWidgetListener) {
-        mAttachedWidgetListener->WindowResized(this, aWidth, aHeight);
+        mAttachedWidgetListener->WindowResized(this,
+                                               mBounds.width, mBounds.height);
     }
 }
 
@@ -1072,7 +1073,7 @@ nsWindow::Resize(double aWidth, double aHeight, bool aRepaint)
 
     // send a resize notification if this is a toplevel
     if (mIsTopLevel || mListenForResizes) {
-        DispatchResized(width, height);
+        DispatchResized();
     }
 
     return NS_OK;
@@ -1103,7 +1104,7 @@ nsWindow::Resize(double aX, double aY, double aWidth, double aHeight,
     ResizePluginSocketWidget();
 
     if (mIsTopLevel || mListenForResizes) {
-        DispatchResized(width, height);
+        DispatchResized();
     }
 
     return NS_OK;
@@ -2404,7 +2405,7 @@ nsWindow::OnSizeAllocate(GtkAllocation *aAllocation)
     if (!mGdkWindow)
         return;
 
-    DispatchResized(size.width, size.height);
+    DispatchResized();
 }
 
 void
