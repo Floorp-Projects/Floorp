1678c2d9d46a3d757888de5e5fa3323cfd5b3260
Andrea Marchesini <amarchesini@mozilla.com>
2015-11-11 10:46:29 +0000

    Bug 1201498 - Service worker update should compare scriptURL to worker URL without fragment, r=bkelly

--- dom/workers/ServiceWorkerManager.cpp
+++ dom/workers/ServiceWorkerManager.cpp
@@ -1526,7 +1526,7 @@ ServiceWorkerManager::Register(nsIDOMWindow* aWindow,
   }
 
   nsAutoCString spec;
-  rv = aScriptURI->GetSpec(spec);
+  rv = aScriptURI->GetSpecIgnoringRef(spec);
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return rv;
   }
