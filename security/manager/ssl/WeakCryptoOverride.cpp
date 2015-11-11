a94cabf2879c13e1441651699df099866590cdfd
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2015-11-11 23:13:34 +0900

    Bug 1219088 - Clear the session cache when a weak crypto override is revoked. r=keeler

--- security/manager/ssl/WeakCryptoOverride.cpp
+++ security/manager/ssl/WeakCryptoOverride.cpp
@@ -8,6 +8,7 @@
 
 #include "MainThreadUtils.h"
 #include "SharedSSLState.h"
+#include "nss.h"
 
 using namespace mozilla;
 using namespace mozilla::psm;
@@ -57,6 +58,10 @@ WeakCryptoOverride::RemoveWeakCryptoOverride(const nsACString& aHostName,
   }
   const nsPromiseFlatCString& host = PromiseFlatCString(aHostName);
   sharedState->IOLayerHelpers().removeInsecureFallbackSite(host, aPort);
+
+  // Some servers will fail with SSL_ERROR_ILLEGAL_PARAMETER_ALERT
+  // unless the session cache is cleared.
+  SSL_ClearSessionCache();
 
   return NS_OK;
 }
