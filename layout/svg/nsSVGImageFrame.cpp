f1da1d192f55531a022cf31c91b7d4804261cecc
Bas Schouten <bschouten@mozilla.com>
2015-11-11 16:15:39 +0100

    Bug 1210560 - Part 2: Convert some simple users to use PushGroupForBlendBack. r=jrmuizel

--- layout/svg/nsSVGImageFrame.cpp
+++ layout/svg/nsSVGImageFrame.cpp
@@ -340,7 +340,7 @@ nsSVGImageFrame::PaintSVG(gfxContext& aContext,
     }
 
     if (opacity != 1.0f || StyleDisplay()->mMixBlendMode != NS_STYLE_BLEND_NORMAL) {
-      aContext.PushGroup(gfxContentType::COLOR_ALPHA);
+      aContext.PushGroupForBlendBack(gfxContentType::COLOR_ALPHA, opacity);
     }
 
     nscoord appUnitsPerDevPx = PresContext()->AppUnitsPerDevPixel();
@@ -404,9 +404,7 @@ nsSVGImageFrame::PaintSVG(gfxContext& aContext,
     }
 
     if (opacity != 1.0f || StyleDisplay()->mMixBlendMode != NS_STYLE_BLEND_NORMAL) {
-      aContext.PopGroupToSource();
-      aContext.SetOp(CompositionOp::OP_OVER);
-      aContext.Paint(opacity);
+      aContext.PopGroupAndBlend();
     }
     // gfxContextAutoSaveRestore goes out of scope & cleans up our gfxContext
   }
