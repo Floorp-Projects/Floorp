4e27b8899803e5128763a559f442197981efd049
Jan Odvarko <odvarko@gmail.com>
2015-11-11 14:41:05 +0100

    Bug 1221754 - Set bodySize to 0 in case of 304 responses; r=jryans

--- devtools/client/netmonitor/har/har-builder.js
+++ devtools/client/netmonitor/har/har-builder.js
@@ -298,7 +298,15 @@ HarBuilder.prototype = {
 
     response.redirectURL = findValue(headers, "Location");
     response.headersSize = headersSize;
-    response.bodySize = file.transferredSize || -1;
+
+    // 'bodySize' is size of the received response body in bytes.
+    // Set to zero in case of responses coming from the cache (304).
+    // Set to -1 if the info is not available.
+    if (typeof file.transferredSize != "number") {
+      response.bodySize = (response.status == 304) ? 0 : -1;
+    } else {
+      response.bodySize = file.transferredSize;
+    }
 
     return response;
   },
