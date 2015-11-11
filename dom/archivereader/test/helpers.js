7d0607404ebb4e20d0500332b31f56ef9ab92b4f
Andrew McCreight <continuation@gmail.com>
2015-11-11 14:10:26 -0800

    Bug 1223510, part 2 - Make ArchiveReader tests use pushPrefEnv. r=baku

--- dom/archivereader/test/helpers.js
+++ dom/archivereader/test/helpers.js
@@ -3,35 +3,21 @@
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
 
-var archiveReaderEnabled = false;
-
 var testGenerator = testSteps();
 
 function runTest()
 {
-  enableArchiveReader();
-
   SimpleTest.waitForExplicitFinish();
-  testGenerator.next();
+
+  SpecialPowers.pushPrefEnv({'set': [ ["dom.archivereader.enabled", true] ]}, function() {
+    return testGenerator.next();
+  });
 }
 
 function finishTest()
 {
-  resetArchiveReader();
-
-  SimpleTest.executeSoon(function() {
+  SpecialPowers.popPrefEnv(function() {
     testGenerator.close();
     SimpleTest.finish();
   });
-}
-
-function enableArchiveReader()
-{
-  archiveReaderEnabled = SpecialPowers.getBoolPref("dom.archivereader.enabled");
-  SpecialPowers.setBoolPref("dom.archivereader.enabled", true);
-}
-
-function resetArchiveReader()
-{
-  SpecialPowers.setBoolPref("dom.archivereader.enabled", archiveReaderEnabled);
 }
