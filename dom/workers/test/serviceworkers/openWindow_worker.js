e6bbef02e95f74836b5b71acb7fdfb37ef5b6173
Catalin Badea <catalin.badea392@gmail.com>
2015-11-12 04:02:34 +0200

    Bug 1172870 - Part 3 - Fix openWindow mochitest to work on e10s. r=smaug a=rkothari
    Conflicts:
    	dom/workers/test/serviceworkers/mochitest.ini

--- dom/workers/test/serviceworkers/openWindow_worker.js
+++ dom/workers/test/serviceworkers/openWindow_worker.js
@@ -1,7 +1,16 @@
 // the worker won't shut down between events because we increased
 // the timeout values.
 var client;
+var window_count = 0;
+var expected_window_count = 6;
+var resolve_got_all_windows = null;
+var got_all_windows = new Promise(function(res, rej) {
+  resolve_got_all_windows = res;
+});
 
+// |expected_window_count| needs to be updated for every new call that's
+// expected to actually open a new window regardless of what |clients.openWindow|
+// returns.
 function testForUrl(url, throwType, clientProperties, resultsArray) {
   return clients.openWindow(url)
     .then(function(e) {
@@ -43,48 +52,64 @@ function testForUrl(url, throwType, clientProperties, resultsArray) {
 }
 
 onmessage = function(event) {
-  client = event.source;
+  if (event.data == "testNoPopup") {
+    client = event.source;
 
-  var results = [];
-  var promises = [];
-  promises.push(testForUrl("about:blank", "TypeError", null, results));
-  promises.push(testForUrl("http://example.com", "InvalidAccessError", null, results));
-  promises.push(testForUrl("_._*`InvalidURL", "InvalidAccessError", null, results));
-  Promise.all(promises).then(function(e) {
-    client.postMessage(results);
-  });
+    var results = [];
+    var promises = [];
+    promises.push(testForUrl("about:blank", "TypeError", null, results));
+    promises.push(testForUrl("http://example.com", "InvalidAccessError", null, results));
+    promises.push(testForUrl("_._*`InvalidURL", "InvalidAccessError", null, results));
+    Promise.all(promises).then(function(e) {
+      client.postMessage(results);
+    });
+  }
+  if (event.data == "NEW_WINDOW") {
+    window_count += 1;
+    if (window_count == expected_window_count) {
+      resolve_got_all_windows();
+    }
+  }
+
+  if (event.data == "CHECK_NUMBER_OF_WINDOWS") {
+    got_all_windows.then(function() {
+      return clients.matchAll();
+    }).then(function(cl) {
+      event.source.postMessage({result: cl.length == expected_window_count,
+                                message: "The number of windows is correct."});
+      for (i = 0; i < cl.length; i++) {
+        cl[i].postMessage("CLOSE");
+      }
+    });
+  }
 }
 
 onnotificationclick = function(e) {
   var results = [];
   var promises = [];
 
+  var redirect = "http://mochi.test:8888/tests/dom/workers/test/serviceworkers/redirect.sjs?"
+  var redirect_xorigin = "http://example.com/tests/dom/workers/test/serviceworkers/redirect.sjs?"
+  var same_origin = "http://mochi.test:8888/tests/dom/workers/test/serviceworkers/open_window/client.html"
+  var different_origin = "http://example.com/tests/dom/workers/test/serviceworkers/open_window/client.html"
+
+
   promises.push(testForUrl("about:blank", "TypeError", null, results));
-  promises.push(testForUrl("http://example.com", null, null, results));
-  promises.push(testForUrl("http://mochi.test:8888/same_origin.html", null,
-                           {url: "http://mochi.test:8888/same_origin.html"}, results));
+  promises.push(testForUrl(different_origin, null, null, results));
+  promises.push(testForUrl(same_origin, null, {url: same_origin}, results));
 
   // redirect tests
-  var redirect = "http://mochi.test:8888/tests/dom/workers/test/serviceworkers/redirect.sjs?"
-  var baseURL = "http://mochi.test:8888/tests/dom/workers/test/serviceworkers/"
-  promises.push(testForUrl(redirect + "same_origin_redirect.html", null,
-			   {url: baseURL + "same_origin_redirect.html"}, results));
-  promises.push(testForUrl(redirect + "http://example.com/redirect_to_other_origin.html", null,
-			   null, results));
+  promises.push(testForUrl(redirect + "open_window/client.html", null,
+			   {url: same_origin}, results));
+  promises.push(testForUrl(redirect + different_origin, null, null, results));
 
-  var redirect_xorigin = "http://example.com/tests/dom/workers/test/serviceworkers/redirect.sjs?"
-  promises.push(testForUrl(redirect_xorigin + "xorigin_redirect.html", null,
+  promises.push(testForUrl(redirect_xorigin + "open_window/client.html", null,
 			   null, results));
-  promises.push(testForUrl(redirect_xorigin + "http://mochi.test:8888/xorigin_to_same_origin.html", null,
-			   {url: "http://mochi.test:8888/xorigin_to_same_origin.html"}, results));
+  promises.push(testForUrl(redirect_xorigin + same_origin, null,
+			   {url: same_origin}, results));
 
   Promise.all(promises).then(function(e) {
     client.postMessage(results);
   });
 }
 
-onfetch = function(e) {
-  if (e.request.url.indexOf(same_origin) >= 0) {
-    e.respondWith(new Response("same_origin_window"));
-  }
-}
