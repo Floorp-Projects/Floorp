f8809d4655b1a7b40de4260f3e1bf809fceb3dae
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-11-12 11:48:54 +0100

    Backed out changeset 6fc769997278 (bug 1220121)

--- dom/bluetooth/ipc/BluetoothServiceChildProcess.h
+++ dom/bluetooth/ipc/BluetoothServiceChildProcess.h
@@ -267,7 +267,7 @@ public:
   virtual void
   SendPlayStatus(int64_t aDuration,
                  int64_t aPosition,
-                 ControlPlayStatus aPlayStatus,
+                 const nsAString& aPlayStatus,
                  BluetoothReplyRunnable* aRunnable) override;
 
   virtual void
