9ec12aea497896652f85034d872d5b7b0ed015cc
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-11-12 11:23:35 +0100

    Bug 1220121: Convert IPDL of Bluetooth AVRCP API to |ControlPlayStatus|, r=shuang

--- dom/bluetooth/bluedroid/BluetoothServiceBluedroid.h
+++ dom/bluetooth/bluedroid/BluetoothServiceBluedroid.h
@@ -255,7 +255,7 @@ public:
   virtual void
   SendPlayStatus(int64_t aDuration,
                  int64_t aPosition,
-                 const nsAString& aPlayStatus,
+                 ControlPlayStatus aPlayStatus,
                  BluetoothReplyRunnable* aRunnable) override;
 
   virtual void
@@ -485,9 +485,6 @@ public:
 protected:
   static nsresult StartGonkBluetooth();
   static nsresult StopGonkBluetooth();
-
-  static ControlPlayStatus PlayStatusStringToControlPlayStatus(
-    const nsAString& aPlayStatus);
 
   static void ConnectDisconnect(bool aConnect,
                                 const BluetoothAddress& aDeviceAddress,
