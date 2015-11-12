4f10c25810ec48a932855271ceec29c768f2f840
James Willcox <snorp@snorp.net>
2015-11-12 14:52:42 -0600

    Bug 1221228 - Work around busted OpenSL causing hangs/reboots on Android. r=padenot, a=sylvestre
    --HG--
    extra : rebase_source : 0352f1a9f984e060fd7d7713a7954e0428c8fca6
    extra : source : 84c21e328c7433092e8b41683bf750697da04867

--- media/libcubeb/src/cubeb_opensl.c
+++ media/libcubeb/src/cubeb_opensl.c
@@ -641,6 +641,9 @@ opensl_stream_init(cubeb * ctx, cubeb_stream ** stream, char const * stream_name
     return CUBEB_ERROR;
   }
 
+  // Work around wilhelm/AudioTrack badness, bug 1221228
+  (*stm->play)->SetMarkerPosition(stm->play, (SLmillisecond)0);
+
   res = (*stm->play)->SetCallbackEventsMask(stm->play, (SLuint32)SL_PLAYEVENT_HEADATMARKER);
   if (res != SL_RESULT_SUCCESS) {
     opensl_stream_destroy(stm);
