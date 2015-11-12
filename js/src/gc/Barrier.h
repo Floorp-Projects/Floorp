cf984dfab11ef49556a73eb662d4021d0cee73d5
Terrence Cole <terrence@mozilla.com>
2015-11-12 13:44:06 -0800

    Bug 1223918 - Use stable hashing for WatchPointMap; r=jonco
    --HG--
    extra : rebase_source : 24f858eff637994394e6e9ec0f5ba01add2fbbf7

--- js/src/gc/Barrier.h
+++ js/src/gc/Barrier.h
@@ -807,6 +807,17 @@ struct MovableCellHasher
 };
 
 template <typename T>
+struct MovableCellHasher<PreBarriered<T>>
+{
+    using Key = PreBarriered<T>;
+    using Lookup = T;
+
+    static HashNumber hash(const Lookup& l) { return MovableCellHasher<T>::hash(l); }
+    static bool match(const Key& k, const Lookup& l) { return MovableCellHasher<T>::match(k, l); }
+    static void rekey(Key& k, const Key& newKey) { k.unsafeSet(newKey); }
+};
+
+template <typename T>
 struct MovableCellHasher<RelocatablePtr<T>>
 {
     using Key = RelocatablePtr<T>;
