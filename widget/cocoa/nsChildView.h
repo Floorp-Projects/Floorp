0ed27703dbc5e453027f3702c25c5fdbc87ae96c
Josh Aas <joshmoz@fastmail.com>
2015-11-13 15:38:03 -0600

    Bug 1223966: Don't claim we support NSTextInput interface. r=masayuki

--- widget/cocoa/nsChildView.h
+++ widget/cocoa/nsChildView.h
@@ -167,7 +167,7 @@ typedef NSInteger NSEventGestureAxis;
 #ifdef ACCESSIBILITY
                               mozAccessible,
 #endif
-                              mozView, NSTextInput, NSTextInputClient>
+                              mozView, NSTextInputClient>
 {
 @private
   // the nsChildView that created the view. It retains this NSView, so
