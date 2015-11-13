afb080ae1953b5068a3c6f806ed30ea267ec4df4
Andreas Tolfsen <ato@mozilla.com>
2015-11-13 13:35:22 +0000

    Bug 1223907: Refactor cookies in Marionette
    Moves most of the cookie implementation to a new file,
    testing/marionette/cookies.js.  The new Cookies class encapsulates all
    APIs for manipulating and querying cookies from content space.
    
    It communicates with chrome space, where the cookie manager lives, through
    a new SyncContentSender provided by testing/marionette/proxy.js.  This new
    interface provides synchronous and transparent communication from content
    to chrome, not dissimilar from how the original listener proxy works.
    
    r=dburns

--- testing/marionette/proxy.js
+++ testing/marionette/proxy.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-var {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 
 Cu.import("resource://gre/modules/Log.jsm");
 Cu.import("chrome://marionette/content/modal.js");
@@ -18,11 +18,22 @@ const MARIONETTE_ERROR = "Marionette:error";
 const logger = Log.repository.getLogger("Marionette");
 const uuidgen = Cc["@mozilla.org/uuid-generator;1"].getService(Ci.nsIUUIDGenerator);
 
+// Proxy handler that traps requests to get a property.  Will prioritise
+// properties that exist on the object's own prototype.
+var ownPriorityGetterTrap = {
+  get: (obj, prop) => {
+    if (obj.hasOwnProperty(prop)) {
+      return obj[prop];
+    }
+    return (...args) => obj.send(prop, args);
+  }
+};
+
 this.proxy = {};
 
 /**
  * Creates a transparent interface between the chrome- and content
- * processes.
+ * contexts.
  *
  * Calls to this object will be proxied via the message manager to the active
  * browsing context (content) and responses will be provided back as
@@ -39,20 +50,12 @@ this.proxy = {};
  *     Callback for sending async messages to the current listener.
  */
 proxy.toListener = function(mmFn, sendAsyncFn) {
-  let sender = new ContentSender(mmFn, sendAsyncFn);
-  let handler = {
-    get: (obj, prop) => {
-      if (obj.hasOwnProperty(prop)) {
-        return obj[prop];
-      }
-      return (...args) => obj.send(prop, args);
-    }
-  };
-  return new Proxy(sender, handler);
+  let sender = new AsyncContentSender(mmFn, sendAsyncFn);
+  return new Proxy(sender, ownPriorityGetterTrap);
 };
 
 /**
- * The ContentSender allows one to make synchronous calls to the
+ * The AsyncContentSender allows one to make synchronous calls to the
  * message listener of the content frame of the current browsing context.
  *
  * Presumptions about the responses from content space are made so we
@@ -62,103 +65,138 @@ proxy.toListener = function(mmFn, sendAsyncFn) {
  *
  * The promise is guaranteed not to resolve until the execution of the
  * command in content space is complete.
- *
- * @param {function(): (nsIMessageSender|nsIMessageBroadcaster)} mmFn
- *     Function returning the current message manager.
- * @param {function(string, Object, number)} sendAsyncFn
- *     Callback for sending async messages to the current listener.
  */
-var ContentSender = function(mmFn, sendAsyncFn) {
-  this.curId = null;
-  this.sendAsync = sendAsyncFn;
-  this.mmFn_ = mmFn;
-  this._listeners = [];
-};
-
-Object.defineProperty(ContentSender.prototype, "mm", {
-  get: function() { return this.mmFn_(); }
-});
-
-ContentSender.prototype.removeListeners = function () {
-  this._listeners.map(l => this.mm.removeMessageListener(l[0], l[1]));
-  this._listeners = [];
-}
-
-/**
- * Call registered function in the frame script environment of the
- * current browsing context's content frame.
- *
- * @param {string} name
- *     Function to call in the listener, e.g. for "Marionette:foo8",
- *     use "foo".
- * @param {Array} args
- *     Argument list to pass the function.  If args has a single entry
- *     that is an object, we assume it's an old style dispatch, and
- *     the object will passed literally.
- *
- * @return {Promise}
- *     A promise that resolves to the result of the command.
- */
-ContentSender.prototype.send = function(name, args) {
-  if (this._listeners[0]) {
-    // A prior (probably timed-out) request has left listeners behind.
-    // Remove them before proceeding.
-    logger.warn("A previous failed command left content listeners behind!");
-    this.removeListeners();
+this.AsyncContentSender = class {
+  constructor(mmFn, sendAsyncFn) {
+    this.curId = null;
+    this.sendAsync = sendAsyncFn;
+    this.mmFn_ = mmFn;
+    this._listeners = [];
   }
 
-  this.curId = uuidgen.generateUUID().toString();
+  get mm() {
+    return this.mmFn_();
+  }
 
-  let proxy = new Promise((resolve, reject) => {
-    let removeListeners = (n, fn) => {
-      let rmFn = msg => {
-        if (this.curId !== msg.json.command_id) {
-          logger.warn("Skipping out-of-sync response from listener: " +
-              `Expected response to ${name} with ID ${this.curId}, ` +
-              "but got: " + msg.name + msg.json.toSource());
-          return;
-        }
+  removeListeners() {
+    this._listeners.map(l => this.mm.removeMessageListener(l[0], l[1]));
+    this._listeners = [];
+  }
 
-        this.removeListeners();
-        modal.removeHandler(handleDialog);
-
-        fn(msg);
-        this.curId = null;
-      };
-
-      this._listeners.push([n, rmFn]);
-      return rmFn;
-    };
-
-    let okListener = () => resolve();
-    let valListener = msg => resolve(msg.json.value);
-    let errListener = msg => reject(msg.objects.error);
-
-    let handleDialog = (subject, topic) => {
-      this.removeListeners()
-      modal.removeHandler(handleDialog);
-      this.sendAsync("cancelRequest");
-      resolve();
-    };
-
-    // start content process listeners, and install observers for global-
-    // and tab modal dialogues
-    this.mm.addMessageListener(MARIONETTE_OK, removeListeners(MARIONETTE_OK, okListener));
-    this.mm.addMessageListener(MARIONETTE_DONE, removeListeners(MARIONETTE_DONE, valListener));
-    this.mm.addMessageListener(MARIONETTE_ERROR, removeListeners(MARIONETTE_ERROR, errListener));
-    modal.addHandler(handleDialog);
-
-    // new style dispatches are arrays of arguments, old style dispatches
-    // are key-value objects
-    let msg = args;
-    if (args.length == 1 && typeof args[0] == "object") {
-      msg = args[0];
+  /**
+   * Call registered function in the frame script environment of the
+   * current browsing context's content frame.
+   *
+   * @param {string} name
+   *     Function to call in the listener, e.g. for "Marionette:foo8",
+   *     use "foo".
+   * @param {Array} args
+   *     Argument list to pass the function.  If args has a single entry
+   *     that is an object, we assume it's an old style dispatch, and
+   *     the object will passed literally.
+   *
+   * @return {Promise}
+   *     A promise that resolves to the result of the command.
+   */
+  send(name, args) {
+    if (this._listeners[0]) {
+      // A prior (probably timed-out) request has left listeners behind.
+      // Remove them before proceeding.
+      logger.warn("A previous failed command left content listeners behind!");
+      this.removeListeners();
     }
 
-    this.sendAsync(name, msg, this.curId);
-  });
+    this.curId = uuidgen.generateUUID().toString();
 
-  return proxy;
+    let proxy = new Promise((resolve, reject) => {
+      let removeListeners = (n, fn) => {
+        let rmFn = msg => {
+          if (this.curId !== msg.json.command_id) {
+            logger.warn("Skipping out-of-sync response from listener: " +
+                `Expected response to ${name} with ID ${this.curId}, ` +
+                "but got: " + msg.name + msg.json.toSource());
+            return;
+          }
+
+          this.removeListeners();
+          modal.removeHandler(handleDialog);
+
+          fn(msg);
+          this.curId = null;
+        };
+
+        this._listeners.push([n, rmFn]);
+        return rmFn;
+      };
+
+      let okListener = () => resolve();
+      let valListener = msg => resolve(msg.json.value);
+      let errListener = msg => reject(msg.objects.error);
+
+      let handleDialog = (subject, topic) => {
+        this.removeListeners()
+        modal.removeHandler(handleDialog);
+        this.sendAsync("cancelRequest");
+        resolve();
+      };
+
+      // start content process listeners, and install observers for global-
+      // and tab modal dialogues
+      this.mm.addMessageListener(MARIONETTE_OK, removeListeners(MARIONETTE_OK, okListener));
+      this.mm.addMessageListener(MARIONETTE_DONE, removeListeners(MARIONETTE_DONE, valListener));
+      this.mm.addMessageListener(MARIONETTE_ERROR, removeListeners(MARIONETTE_ERROR, errListener));
+      modal.addHandler(handleDialog);
+
+      this.sendAsync(name, marshal(args), this.curId);
+    });
+
+    return proxy;
+  }
 };
 
-proxy.ContentSender = ContentSender;
+/**
+ * Creates a transparent interface from the content- to the chrome context.
+ *
+ * Calls to this object will be proxied via the frame's sendSyncMessage
+ * (nsISyncMessageSender) function.  Since the message is synchronous,
+ * the return value is presented as a return value.
+ *
+ * Example on how to use from a frame content script:
+ *
+ *     let chrome = proxy.toChrome(sendSyncMessage.bind(this));
+ *     let cookie = chrome.getCookie("foo");
+ *
+ * @param {nsISyncMessageSender} sendSyncMessageFn
+ *     The frame message manager's sendSyncMessage function.
+ */
+proxy.toChrome = function(sendSyncMessageFn) {
+  let sender = new SyncChromeSender(sendSyncMessageFn);
+  return new Proxy(sender, ownPriorityGetterTrap);
+};
+
+/**
+ * The SyncChromeSender sends synchronous RPC messages to the chrome
+ * context, using a frame's sendSyncMessage (nsISyncMessageSender) function.
+ *
+ * Example on how to use from a frame content script:
+ *
+ *     let sender = new SyncChromeSender(sendSyncMessage.bind(this));
+ *     let res = sender.send("addCookie", cookie);
+ */
+this.SyncChromeSender = class {
+  constructor(sendSyncMessage) {
+    this.sendSyncMessage_ = sendSyncMessage;
+  }
+
+  send(func, args) {
+    let name = "Marionette:" + func;
+    return this.sendSyncMessage_(name, marshal(args));
+  }
+};
+
+function marshal(args) {
+  if (args.length == 1 && typeof args[0] == "object") {
+    return args[0];
+  }
+  return args;
+}
