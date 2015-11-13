68f3cf27671bc95855978db4eb2577cf9ae0a14a
Ralph Giles <giles@mozilla.com>
2015-11-12 17:00:07 -0800

    Bug 1218124 - Add vpx_once patch to update script. r=gerald From 3fae7b20918a3a3c8a9dc6e7f92aef0d19fb20fa Mon Sep 17 00:00:00 2001

--- media/libvpx/update.py
+++ media/libvpx/update.py
@@ -592,6 +592,8 @@ def apply_patches():
     # Cherry pick https://chromium-review.googlesource.com/#/c/276889/
     # to fix crash on 32bit
     os.system("patch -p1 < vp9_filter_restore_aligment.patch")
+    # Patch win32 vpx_once.
+    os.system("patch -p3 < vpx_once.patch")
 
 def update_readme(commit):
     with open('README_MOZILLA') as f:
