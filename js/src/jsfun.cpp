d8e207bf0ee18b5a062703be93e3adfb1aa63e24
Eric Faust <efaustbmo@gmail.com>
2015-11-13 18:22:21 -0800

    Bug 1055472 - Part 5: Make the various Error constructors properly subclassable. (r=Waldo)

--- js/src/jsfun.cpp
+++ js/src/jsfun.cpp
@@ -1863,14 +1863,7 @@ FunctionConstructor(JSContext* cx, unsigned argc, Value* vp, GeneratorKind gener
         if (!proto)
             return false;
     } else {
-        RootedObject toTest(cx);
-        // If we are invoked without |new|, then just use Function.prototype
-        if (args.isConstructing())
-            toTest = &args.newTarget().toObject();
-        else
-            toTest = &args.callee();
-
-        if (!GetPrototypeFromConstructor(cx, toTest, &proto))
+        if (!GetPrototypeFromCallableConstructor(cx, args, &proto))
             return false;
     }
 
