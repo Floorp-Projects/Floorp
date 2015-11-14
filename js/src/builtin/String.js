feb7e51fddd9e6ede5bf9618cff7b88cf839eb52
Phil Ringnalda <philringnalda@gmail.com>
2015-11-13 20:48:05 -0800

    Back out 2 changesets (bug 1223990, bug 1223916) for massive mostly date-related bustage CLOSED TREE
    Backed out changeset ee68d52994d9 (bug 1223916)
    Backed out changeset 75f6ef3df3fa (bug 1223990)

--- js/src/builtin/String.js
+++ js/src/builtin/String.js
@@ -296,12 +296,12 @@ function String_static_fromCodePoint(codePoints) {
         // Step 5f.
         // Inlined UTF-16 Encoding
         if (nextCP <= 0xFFFF) {
-            callFunction(std_Array_push, elements, nextCP);
+            elements.push(nextCP);
             continue;
         }
 
-        callFunction(std_Array_push, elements, (((nextCP - 0x10000) / 0x400) | 0) + 0xD800);
-        callFunction(std_Array_push, elements, (nextCP - 0x10000) % 0x400 + 0xDC00);
+        elements.push((((nextCP - 0x10000) / 0x400) | 0) + 0xD800);
+        elements.push((nextCP - 0x10000) % 0x400 + 0xDC00);
     }
 
     // Step 6.
--- js/src/builtin/Utilities.js
+++ js/src/builtin/Utilities.js
@@ -53,7 +53,16 @@ var std_Map_iterator_next = MapIteratorNext;
 function List() {
     this.length = 0;
 }
-MakeConstructible(List, {__proto__: null});
+
+{
+  let ListProto = std_Object_create(null);
+  ListProto.indexOf = std_Array_indexOf;
+  ListProto.join = std_Array_join;
+  ListProto.push = std_Array_push;
+  ListProto.slice = std_Array_slice;
+  ListProto.sort = std_Array_sort;
+  MakeConstructible(List, ListProto);
+}
 
 
 /********** Record specification type **********/
