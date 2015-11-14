a8524154120d636aeabcd811b911589b70c6f4d7
Eric Faust <efaustbmo@gmail.com>
2015-11-13 18:26:00 -0800

    Bug 1223916 - Prohibit direct method calls at the parser level in self-hosted code. (r=till)

--- js/src/vm/Xdr.h
+++ js/src/vm/Xdr.h
@@ -29,11 +29,11 @@ namespace js {
  *
  *  https://developer.mozilla.org/en-US/docs/SpiderMonkey/Internals/Bytecode
  */
-static const uint32_t XDR_BYTECODE_VERSION_SUBTRAHEND = 320;
+static const uint32_t XDR_BYTECODE_VERSION_SUBTRAHEND = 321;
 static const uint32_t XDR_BYTECODE_VERSION =
     uint32_t(0xb973c0de - XDR_BYTECODE_VERSION_SUBTRAHEND);
 
-static_assert(JSErr_Limit == 419,
+static_assert(JSErr_Limit == 420,
               "GREETINGS, POTENTIAL SUBTRAHEND INCREMENTER! If you added or "
               "removed MSG_DEFs from js.msg, you should increment "
               "XDR_BYTECODE_VERSION_SUBTRAHEND and update this assertion's "
