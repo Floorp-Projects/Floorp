eee17ab43871fad161810d0e4854bceec86b0182
Mats Palmgren <mats@mozilla.com>
2015-11-16 17:32:39 +0100

    Bug 576927 - Use saturating multiply when calculating zoomed app units to avoid integer overflow.  r=roc

--- layout/base/nsPresContext.h
+++ layout/base/nsPresContext.h
@@ -529,6 +529,7 @@ public:
 
   float TextZoom() { return mTextZoom; }
   void SetTextZoom(float aZoom) {
+    MOZ_ASSERT(aZoom > 0.0f, "invalid zoom factor");
     if (aZoom == mTextZoom)
       return;
 
