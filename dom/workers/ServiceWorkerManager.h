b6998ae17aff43afb5920696a307dd03a732807e
Ben Kelly <ben@wanderview.com>
2015-11-16 08:04:11 -0800

    Bug 1217909 P2 Track registering documents as weak reference so SWM can report errors to them. r=catalinb

--- dom/workers/ServiceWorkerManager.h
+++ dom/workers/ServiceWorkerManager.h
@@ -329,6 +329,11 @@ public:
   // Set of all documents that may be controlled by a service worker.
   nsTHashtable<nsISupportsHashKey> mAllDocuments;
 
+  // Track all documents that have attempted to register a service worker for a
+  // given scope.
+  typedef nsTArray<nsCOMPtr<nsIWeakReference>> WeakDocumentList;
+  nsClassHashtable<nsCStringHashKey, WeakDocumentList> mRegisteringDocuments;
+
   bool
   IsAvailable(const OriginAttributes& aOriginAttributes, nsIURI* aURI);
 
@@ -620,6 +625,9 @@ private:
 
   void
   NotifyListenersOnUnregister(nsIServiceWorkerRegistrationInfo* aRegistration);
+
+  void
+  AddRegisteringDocument(const nsACString& aScope, nsIDocument* aDoc);
 };
 
 } // namespace workers
