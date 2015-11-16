e4aa9d36de0a0d0c833598f35fa88730df5d4b7c
Wes Kocher <wkocher@mozilla.com>
2015-11-16 11:00:32 -0800

    Backed out changeset cbc8ceae73a2 (bug 576927) for test_value_cloning.html bustage CLOSED TREE
    --HG--
    extra : commitid : KD1fFkxta16

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -200,16 +200,13 @@ nsChangeHint nsStyleFont::CalcDifference(const nsStyleFont& aOther) const
 /* static */ nscoord
 nsStyleFont::ZoomText(nsPresContext *aPresContext, nscoord aSize)
 {
-  MOZ_ASSERT(aSize >= 0, "negative font size");
-  return NSCoordSaturatingNonnegativeMultiply(aSize, aPresContext->TextZoom());
+  return nscoord(float(aSize) * aPresContext->TextZoom());
 }
 
 /* static */ nscoord
 nsStyleFont::UnZoomText(nsPresContext *aPresContext, nscoord aSize)
 {
-  MOZ_ASSERT(aSize >= 0, "negative font size");
-  return NSCoordSaturatingNonnegativeMultiply(aSize,
-                                              1.0 / aPresContext->TextZoom());
+  return nscoord(float(aSize) / aPresContext->TextZoom());
 }
 
 /* static */ already_AddRefed<nsIAtom>
