5219bd1cea7302fe33d27d7e6df0fccc7ea4d051
Ben Kelly <ben@wanderview.com>
2015-11-16 08:04:12 -0800

    Bug 1223116 P3 Extend the test to check console messages for more service worker conditions. r=bgrins

--- devtools/shared/webconsole/test/helper_serviceworker.js
+++ devtools/shared/webconsole/test/helper_serviceworker.js
@@ -1,2 +1,19 @@
+console.log('script evaluation');
 
-console.log("Hello from serviceworker");
+addEventListener('install', function(evt) {
+  console.log('install event');
+});
+
+addEventListener('activate', function(evt) {
+  console.log('activate event');
+});
+
+addEventListener('fetch', function(evt) {
+  console.log('fetch event: ' + evt.request.url);
+  evt.respondWith(new Response('Hello world'));
+});
+
+addEventListener('message', function(evt) {
+  console.log('message event: ' + evt.data.message);
+  evt.source.postMessage({ type: 'PONG' });
+});
