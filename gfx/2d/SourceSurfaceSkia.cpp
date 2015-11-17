55243861cf4444f8135c3fc839be75517c9f6a12
Lee Salzman <lsalzman@mozilla.com>
2015-11-17 11:18:36 -0500

    Bug 1225250 - fix stride on SourceSurfaceSkia when initialized from GPU texture. r=jmuizelaar, a=lizzard
    --HG--
    extra : source : 8d2541ca9d8be24b9ab4a2f6d0d08e2c9e853bc1

--- gfx/2d/SourceSurfaceSkia.cpp
+++ gfx/2d/SourceSurfaceSkia.cpp
@@ -114,8 +114,10 @@ SourceSurfaceSkia::InitFromTexture(DrawTargetSkia* aOwner,
   GrTexture *skiaTexture = aOwner->mGrContext->wrapBackendTexture(skiaTexGlue);
   SkImageInfo imgInfo = SkImageInfo::Make(aSize.width, aSize.height, GfxFormatToSkiaColorType(aFormat), kOpaque_SkAlphaType);
   SkGrPixelRef *texRef = new SkGrPixelRef(imgInfo, skiaTexture, false);
-  mBitmap.setInfo(imgInfo, aSize.width*aSize.height*4);
+  mBitmap.setInfo(imgInfo);
   mBitmap.setPixelRef(texRef);
+  mFormat = aFormat;
+  mStride = mBitmap.rowBytes();
 #endif
 
   mDrawTarget = aOwner;
