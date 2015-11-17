29a30fade0180dc69207ae6fa7c9211604f69cd3
Andrea Marchesini <amarchesini@mozilla.com>
2015-11-17 23:38:39 +0000

    Bug 1221852 - SharedWorker.port should be always not null, r=smaug, a=ritu

--- dom/workers/test/sharedWorker_ports.js
+++ dom/workers/test/sharedWorker_ports.js
@@ -17,6 +17,7 @@ onconnect = function(evt) {
                           test: (evtFromPort2.data.type == "connected"),
                           msg: "The original message received" });
         port.postMessage({type: "finish"});
+        close();
       }
     }
   }
