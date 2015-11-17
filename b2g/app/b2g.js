2b8bbd69b7b6d0488c9ed174ec41ecae699975be
Fabrice Desré <fabrice@mozilla.com>
2015-11-17 11:03:44 -0800

    Bug 1225279 - Enable safebrowsing on b2g by default r=gwagner

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -352,11 +352,11 @@ pref("dom.w3c_touch_events.safetyX", 0); // escape borders in units of 1/240"
 pref("dom.w3c_touch_events.safetyY", 120); // escape borders in units of 1/240"
 
 #ifdef MOZ_SAFE_BROWSING
-pref("browser.safebrowsing.enabled", false);
+pref("browser.safebrowsing.enabled", true);
 // Prevent loading of pages identified as malware
-pref("browser.safebrowsing.malware.enabled", false);
-pref("browser.safebrowsing.downloads.enabled", false);
-pref("browser.safebrowsing.downloads.remote.enabled", false);
+pref("browser.safebrowsing.malware.enabled", true);
+pref("browser.safebrowsing.downloads.enabled", true);
+pref("browser.safebrowsing.downloads.remote.enabled", true);
 pref("browser.safebrowsing.downloads.remote.timeout_ms", 10000);
 pref("browser.safebrowsing.debug", false);
 
