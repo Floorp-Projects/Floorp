1911545986e775ccc7fcbb2c58f9b112de79b558
J. Ryan Stinnett <jryans@gmail.com>
2015-11-17 11:48:46 -0800

    Bug 1207976 - Remove /themes from DevTools URLs. r=ochameau a=ritu
    --HG--
    extra : commitid : BZFWXF7H8Ri

--- devtools/client/sourceeditor/editor.js
+++ devtools/client/sourceeditor/editor.js
@@ -46,7 +46,7 @@ const { OS } = Services.appinfo;
 // order to initialize a CodeMirror instance.
 
 const CM_STYLES   = [
-  "chrome://devtools/skin/themes/common.css",
+  "chrome://devtools/skin/common.css",
   "chrome://devtools/content/sourceeditor/codemirror/codemirror.css",
   "chrome://devtools/content/sourceeditor/codemirror/dialog/dialog.css",
   "chrome://devtools/content/sourceeditor/codemirror/mozilla.css"
