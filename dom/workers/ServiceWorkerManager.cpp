9e64e8713b6e578c0e581176fa2643d15a8e3ee0
Ben Kelly <ben@wanderview.com>
2015-11-18 13:07:43 -0800

    Bug 1217909 P11 Only report errors to documents that are active and not in the bfcache. r=catalinb a=ritu

--- dom/workers/ServiceWorkerManager.cpp
+++ dom/workers/ServiceWorkerManager.cpp
@@ -2317,7 +2317,7 @@ ServiceWorkerManager::ReportToAllClients(const nsCString& aScope,
     }
 
     nsCOMPtr<nsIDocument> doc = do_QueryInterface(iter.Key());
-    if (!doc || !doc->GetWindow()) {
+    if (!doc || !doc->IsCurrentActiveDocument() || !doc->GetWindow()) {
       continue;
     }
 
@@ -2341,6 +2341,10 @@ ServiceWorkerManager::ReportToAllClients(const nsCString& aScope,
       nsCOMPtr<nsIDocument> doc = do_QueryReferent(regList->ElementAt(i));
       if (!doc) {
         regList->RemoveElementAt(i);
+        continue;
+      }
+
+      if (!doc->IsCurrentActiveDocument()) {
         continue;
       }
 
