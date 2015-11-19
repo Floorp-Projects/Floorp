a605690f75bb2b4cc152f63c4476e5c1f20ebb72
Mike Hommey <mh+mozilla@glandium.org>
2015-11-19 18:00:39 +0900

    Bug 1225682 - Don't use nsAuto{,C}String as class member variables in dom/media/. r=cpearce

--- dom/media/gmp/GMPStorageParent.cpp
+++ dom/media/gmp/GMPStorageParent.cpp
@@ -482,7 +482,7 @@ private:
 
   // Hash record name to record data.
   nsClassHashtable<nsCStringHashKey, Record> mRecords;
-  const nsAutoCString mNodeId;
+  const nsCString mNodeId;
   const nsString mGMPName;
 };
 
