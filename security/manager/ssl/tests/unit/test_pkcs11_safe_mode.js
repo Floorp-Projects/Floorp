e9ef220fdefb6010de63c7c5869824cbd718101d
David Keeler <dkeeler@mozilla.com>
2015-11-19 13:31:52 -0800

    bug 986956 - only ever initialize NSS once per process r=Cykesiopka r=mgoodwin
    As a consequence, if NSS is initialized when there is no profile directory, NSS
    will not persist changes. Other failures may occur (e.g. see bug 1216882).

--- security/manager/ssl/tests/unit/test_pkcs11_safe_mode.js
+++ security/manager/ssl/tests/unit/test_pkcs11_safe_mode.js
@@ -4,18 +4,17 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-// This test loads a testing PKCS #11 module. After simulating a shutdown and
-// startup, the test verifies that the module is automatically loaded again.
-// After simulating a shutdown and restart in safe mode, the test verifies that
-// the module is not automatically loaded again.
+// In safe mode, PKCS#11 modules should not be loaded. This test tests this by
+// simulating starting in safe mode and then attempting to load a module.
 
 var { XPCOMUtils } = Cu.import("resource://gre/modules/XPCOMUtils.jsm", {});
 
-// Registers an nsIXULRuntime so the test can control whether or not the
-// platform thinks it's in safe mode.
-function registerXULRuntime() {
+function run_test() {
+  do_get_profile();
+
+  // Simulate starting in safe mode.
   let xulRuntime = {
-    inSafeMode: false,
+    inSafeMode: true,
     logConsoleErrors: true,
     OS: "XPCShell",
     XPCOMABI: "noarch-spidermonkey",
@@ -39,62 +38,20 @@ function registerXULRuntime() {
   const XULRUNTIME_CID = Components.ID("{f0f0b230-5525-4127-98dc-7bca39059e70}");
   registrar.registerFactory(XULRUNTIME_CID, "XULRuntime", XULRUNTIME_CONTRACTID,
                             xulRuntimeFactory);
-  return xulRuntime;
-}
 
-// Loads the PKCS11 test module.
-function loadTestModule() {
+  // When starting in safe mode, the test module should fail to load.
   let pkcs11 = Cc["@mozilla.org/security/pkcs11;1"].getService(Ci.nsIPKCS11);
   let libraryName = ctypes.libraryName("pkcs11testmodule");
   let libraryFile = Services.dirsvc.get("CurWorkD", Ci.nsILocalFile);
   libraryFile.append("pkcs11testmodule");
   libraryFile.append(libraryName);
   ok(libraryFile.exists(), "The pkcs11testmodule file should exist");
-  pkcs11.addModule("PKCS11 Test Module", libraryFile.path, 0, 0);
-}
-
-// Tests finding the PKCS11 test module. 'shouldSucceed' is a boolean
-// indicating whether or not the test module is expected to be found.
-function testFindTestModule(shouldSucceed) {
-  let pkcs11ModuleDB = Cc["@mozilla.org/security/pkcs11moduledb;1"]
-                         .getService(Ci.nsIPKCS11ModuleDB);
+  let exceptionCaught = false;
   try {
-    let module = pkcs11ModuleDB.findModuleByName("PKCS11 Test Module");
-    ok(shouldSucceed, "Success expected: findModuleByName should not throw");
-    ok(module, "module should be non-null");
+    pkcs11.addModule("PKCS11 Test Module", libraryFile.path, 0, 0);
+    ok(false, "addModule should have thrown an exception");
   } catch (e) {
-    ok(!shouldSucceed, "Failure expected: findModuleByName should throw");
+    exceptionCaught = true;
   }
-}
-
-function simulateShutdown() {
-  let psmComponent = Cc["@mozilla.org/psm;1"].getService(Ci.nsIObserver);
-  psmComponent.observe(null, "profile-before-change", null);
-  psmComponent.observe(null, "xpcom-shutdown", null);
-}
-
-function simulateStartup() {
-  let psmComponent = Cc["@mozilla.org/psm;1"].getService(Ci.nsIObserver);
-  psmComponent.observe(null, "profile-do-change", null);
-}
-
-function run_test() {
-  do_get_profile();
-  let xulRuntime = registerXULRuntime();
-  loadTestModule();
-
-  // After having loaded the test module, it should be available.
-  testFindTestModule(true);
-  simulateShutdown();
-  simulateStartup();
-  // After shutting down and starting up, the test module should automatically
-  // be loaded again.
-  testFindTestModule(true);
-
-  simulateShutdown();
-  // Simulate starting in safe mode.
-  xulRuntime.inSafeMode = true;
-  simulateStartup();
-  // When starting in safe mode, the test module should not be loaded.
-  testFindTestModule(false);
+  ok(exceptionCaught, "addModule should have thrown an exception");
 }
