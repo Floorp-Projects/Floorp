0520cbc652ae374b08a4ead9b1ed9f9c104c86cd
Michael Ratcliffe <mratcliffe@mozilla.com>
2015-11-20 16:57:20 +0000

    Bug 1225289 - Make eslint plugin code conform to .eslintrc r=pbrosset
    Added use strict statements now that we are no longer set up for ES6 modules.
    
    --HG--
    extra : commitid : BjJFvuFTZXZ
    extra : rebase_source : 7781800bce61036a04734b858c8d7d13910377f1

--- testing/eslint-plugin-mozilla/lib/rules/import-headjs-globals.js
+++ testing/eslint-plugin-mozilla/lib/rules/import-headjs-globals.js
@@ -6,20 +6,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 "use strict";
 
-//------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 // Rule Definition
-//------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 var fs = require("fs");
 var path = require("path");
 var helpers = require("../helpers");
+var importRx = /^(?:Services\.scriptloader\.|loader)?loadSubScript\((.+)[",]/;
 
 module.exports = function(context) {
-  //--------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
   // Helpers
-  //--------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
 
   function checkFile(fileArray) {
     var filePath = fileArray.pop();
@@ -29,7 +31,7 @@ module.exports = function(context) {
 
       try {
         headText = fs.readFileSync(filePath, "utf8");
-      } catch(e) {
+      } catch (e) {
         // Couldn't find file, continue.
         filePath = fileArray.pop();
         continue;
@@ -39,8 +41,8 @@ module.exports = function(context) {
       var globalVars = helpers.getGlobals(ast);
 
       for (var i = 0; i < globalVars.length; i++) {
-        var name = globalVars[i];
-        helpers.addVarToScope(name, context);
+        var varName = globalVars[i];
+        helpers.addVarToScope(varName, context);
       }
 
       for (var index in ast.body) {
@@ -54,16 +56,18 @@ module.exports = function(context) {
         }
 
         // Scripts loaded using loadSubScript or loadHelperScript
-        var matches =
-          source.match(/^(?:Services\.scriptloader\.|loader)?loadSubScript\((.+)[",]/);
+        var matches = source.match(importRx);
+
         if (!matches) {
           matches = source.match(/^loadHelperScript\((.+)[",]/);
         }
+
         if (matches) {
           var cwd = process.cwd();
 
           filePath = matches[1];
-          filePath = filePath.replace("chrome://mochitests/content/browser", cwd + "/../../../..");
+          filePath = filePath.replace("chrome://mochitests/content/browser",
+                                      cwd + "/../../../..");
           filePath = filePath.replace(/testdir\s*\+\s*["']/gi, cwd + "/");
           filePath = filePath.replace(/test_dir\s*\+\s*["']/gi, cwd);
           filePath = filePath.replace(/["']/gi, "");
@@ -77,9 +81,9 @@ module.exports = function(context) {
     }
   }
 
-  //--------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
   // Public
-  //--------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
 
   return {
     Program: function() {
