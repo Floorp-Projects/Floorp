8b6534620511f51316dec02c2a43ae1112249b39
L. David Baron <dbaron@dbaron.org>
2015-11-20 14:34:12 -0800

    Bug 1226489 - Pass only a single rule to StyleRuleChanged.  r=bzbarsky
    --HG--
    extra : transplant_source : g%3B%E8%9F%11/%88%E9R%85%0Dp5%CC%A7T%BC%10%0D%1E

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -4559,8 +4559,7 @@ PresShell::StyleSheetApplicableStateChanged(nsIDocument* aDocument,
 void
 PresShell::StyleRuleChanged(nsIDocument* aDocument,
                             CSSStyleSheet* aStyleSheet,
-                            mozilla::css::Rule* aOldStyleRule,
-                            mozilla::css::Rule* aNewStyleRule)
+                            mozilla::css::Rule* aStyleRule)
 {
   RecordStyleSheetChange(aStyleSheet);
 }
