508f2256e57e389cc506eb256e591e0c0fc9a3be
Wes Kocher <wkocher@mozilla.com>
2015-11-20 12:00:13 -0800

    Backed out changeset 7fd167583034 (bug 1226291) for android m(16) failures CLOSED TREE

--- dom/contacts/tests/shared.js
+++ dom/contacts/tests/shared.js
@@ -1,7 +1,11 @@
 "use strict";
 
 // Fix the environment to run Contacts tests
-SpecialPowers.importInMainProcess("resource://gre/modules/ContactService.jsm");
+if (SpecialPowers.isMainProcess()) {
+  SpecialPowers.Cu.import("resource://gre/modules/ContactService.jsm");
+} else {
+  SpecialPowers.loadChromeScript(SimpleTest.getTestFileURL('contacts_chromescript.js'));
+}
 
 // Some helpful global vars
 var isAndroid = (navigator.userAgent.indexOf("Android") !== -1);
--- testing/mochitest/tests/Harness_sanity/importtesting_chromescript.js
+++
@@ -1,3 +0,0 @@
-addMessageListener("ImportTesting:IsModuleLoaded", function (msg) {
-  sendAsyncMessage("ImportTesting:IsModuleLoadedReply", Components.utils.isModuleLoaded(msg));
-});
