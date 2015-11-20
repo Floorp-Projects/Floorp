8f3d7aafd71104d50a6e9c7d0a4697f1731fff24
Patrick McManus <mcmanus@ducksong.com>
2015-11-20 15:33:04 -0500

    Bug 1174899 - fixup log format strings for spdy/h2 r=bagder, a=lizzard

--- netwerk/protocol/http/SpdyStream31.cpp
+++ netwerk/protocol/http/SpdyStream31.cpp
@@ -1536,14 +1536,14 @@ SpdyStream31::OnReadSegment(const char *buf,
     if (dataLength > mSession->RemoteSessionWindow())
       dataLength = static_cast<uint32_t>(mSession->RemoteSessionWindow());
 
-    LOG3(("SpdyStream31 this=%p id 0x%X remote window is stream %ld and "
-          "session %ld. Chunk is %d\n",
-          this, mStreamID, mRemoteWindow, mSession->RemoteSessionWindow(),
-          dataLength));
+    LOG3(("SpdyStream31 this=%p id 0x%X remote window is stream %" PRId64 " and "
+          "session %" PRId64". Chunk is %u\n",
+          this, mStreamID, mRemoteWindow,
+          mSession->RemoteSessionWindow(), dataLength));
     mRemoteWindow -= dataLength;
     mSession->DecrementRemoteSessionWindow(dataLength);
 
-    LOG3(("SpdyStream31 %p id %x request len remaining %u, "
+    LOG3(("SpdyStream31 %p id 0x%x request len remaining %" PRId64 ", "
           "count avail %u, chunk used %u",
           this, mStreamID, mRequestBodyLenRemaining, count, dataLength));
     if (!dataLength && mRequestBodyLenRemaining) {
