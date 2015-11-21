c86953ba2d9e1e3bddbc2cd2041838976e99d4b3
L. David Baron <dbaron@dbaron.org>
2015-11-20 22:30:54 -0800

    Bug 1221823 patch 2 - Use already_AddRefed<Declaration>&& as parameter to nsCSSKeyframeRule constructor to avoid extra reference count cycle.  r=xidorn

--- layout/style/nsCSSParser.cpp
+++ layout/style/nsCSSParser.cpp
@@ -4281,7 +4281,8 @@ CSSParserImpl::ParseKeyframeRule()
 
   // Takes ownership of declaration
   RefPtr<nsCSSKeyframeRule> rule =
-    new nsCSSKeyframeRule(Move(selectorList), declaration, linenum, colnum);
+    new nsCSSKeyframeRule(Move(selectorList), declaration.forget(),
+                          linenum, colnum);
   return rule.forget();
 }
 
