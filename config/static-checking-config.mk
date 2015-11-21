7bd16a4817b4993e1221a0bce1d0f67c37560d06
Mike Hommey <mh+mozilla@glandium.org>
2015-11-21 13:54:57 +0900

    Bug 1227388 - Finish removing dehydra support. r=mshal
    Dehydra/Treehydra is unmaintained, broken (iirc), and obsoleted by clang
    static analysis. We've removed parts of the build system support for it, but
    not all. This is meant to remove the remains.

--- config/static-checking-config.mk
+++ config/static-checking-config.mk
@@ -5,43 +5,6 @@
 # The entire tree should be subject to static analysis using the XPCOM
 # script. Additional scripts may be added by specific subdirectories.
 
-DEHYDRA_SCRIPT = $(topsrcdir)/config/static-checking.js
-
-ifndef JS_STANDALONE
-DEHYDRA_MODULES = \
-  $(topsrcdir)/xpcom/analysis/final.js \
-  $(topsrcdir)/xpcom/analysis/must-override.js \
-  $(NULL)
-
-TREEHYDRA_MODULES = \
-  $(topsrcdir)/xpcom/analysis/outparams.js \
-  $(topsrcdir)/xpcom/analysis/stack.js \
-  $(topsrcdir)/xpcom/analysis/flow.js \
-  $(topsrcdir)/xpcom/analysis/static-init.js \
-  $(topsrcdir)/layout/generic/frame-verify.js \
-  $(NULL)
-endif
-
-TREEHYDRA_MODULES += \
-  $(topsrcdir)/js/src/jsstack.js \
-  $(NULL)
-
-DEHYDRA_ARG_PREFIX=-fplugin-arg-gcc_treehydra-
-
-DEHYDRA_ARGS = \
-  $(DEHYDRA_ARG_PREFIX)script=$(DEHYDRA_SCRIPT) \
-  $(DEHYDRA_ARG_PREFIX)topsrcdir=$(topsrcdir) \
-  $(DEHYDRA_ARG_PREFIX)objdir=$(DEPTH) \
-  $(DEHYDRA_ARG_PREFIX)dehydra-modules=$(subst $(NULL) ,$(COMMA),$(strip $(DEHYDRA_MODULES))) \
-  $(DEHYDRA_ARG_PREFIX)treehydra-modules=$(subst $(NULL) ,$(COMMA),$(strip $(TREEHYDRA_MODULES))) \
-  $(NULL)
-
-DEHYDRA_FLAGS = -fplugin=$(DEHYDRA_PATH) $(DEHYDRA_ARGS)
-
-ifdef DEHYDRA_PATH
-OS_CXXFLAGS += $(DEHYDRA_FLAGS)
-endif
-
 ifdef ENABLE_CLANG_PLUGIN
 CLANG_PLUGIN := $(DEPTH)/build/clang-plugin/$(DLL_PREFIX)clang-plugin$(DLL_SUFFIX)
 OS_CXXFLAGS += -Xclang -load -Xclang $(CLANG_PLUGIN) -Xclang -add-plugin -Xclang moz-check
--- xpcom/tests/static-checker/TestStaticInitGlobalConst.cpp
+++
@@ -1,5 +0,0 @@
-int foo() {
-  return 0;
-}
- 
-const static int x = foo();
