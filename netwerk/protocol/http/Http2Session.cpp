fee43b064481ef56240b71dd95855f8bfeef6d27
Patrick McManus <mcmanus@ducksong.com>
2015-11-22 16:23:18 -0500

    Bug 1174899 - discarded spdy data with fin attributed to wrong stream. r=bagder, a=lizzard
    --HG--
    extra : source : 4ec0c590ef6672e50c75cdceea9758e5454c4abb

--- netwerk/protocol/http/Http2Session.cpp
+++ netwerk/protocol/http/Http2Session.cpp
@@ -599,6 +599,7 @@ Http2Session::ResetDownstreamState()
     mInputFrameDataStream->SetRecvdFin(true);
     MaybeDecrementConcurrent(mInputFrameDataStream);
   }
+  mInputFrameFinal = false;
   mInputFrameBufferUsed = 0;
   mInputFrameDataStream = nullptr;
 }
