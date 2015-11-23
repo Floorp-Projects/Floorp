a53c127de0ee3e624cc8752ebdb799d736cd6d82
Valentin Gosu <valentin.gosu@gmail.com>
2015-11-23 13:02:41 +0100

    Bug 1183781 - Small leak in DNS.cpp and nsHostResolver.cpp r=sworkman * makes AddrInfo extend nsISupports * uses RefPtr for managing AddrInfo and nsHostResolver in nsHostResolver.cpp

--- netwerk/dns/GetAddrInfo.cpp
+++ netwerk/dns/GetAddrInfo.cpp
@@ -362,14 +362,14 @@ _GetAddrInfo_Portable(const char* aCanonHost, uint16_t aAddressFamily,
   }
 
   bool filterNameCollision = !(aFlags & nsHostResolver::RES_ALLOW_NAME_COLLISION);
-  nsAutoPtr<AddrInfo> ai(new AddrInfo(aCanonHost, prai, disableIPv4,
-                                      filterNameCollision, canonName));
+  RefPtr<AddrInfo> ai(new AddrInfo(aCanonHost, prai, disableIPv4,
+                                   filterNameCollision, canonName));
   PR_FreeAddrInfo(prai);
   if (ai->mAddresses.isEmpty()) {
     return NS_ERROR_UNKNOWN_HOST;
   }
 
-  *aAddrInfo = ai.forget();
+  ai.forget(aAddrInfo);
 
   return NS_OK;
 }
