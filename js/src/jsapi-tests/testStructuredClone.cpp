2703bafaca4367349cddad32f4485043a0f20a14
Jakob Olesen <jolesen@mozilla.com>
2015-11-23 15:28:46 -0800

    Bug 1210554 - Fix unified build breakage. r=sfink

--- js/src/jsapi-tests/testStructuredClone.cpp
+++ js/src/jsapi-tests/testStructuredClone.cpp
@@ -2,9 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "builtin/TestingFunctions.h"
 #include "js/StructuredClone.h"
 
 #include "jsapi-tests/tests.h"
+
+using namespace js;
 
 BEGIN_TEST(testStructuredClone_object)
 {
