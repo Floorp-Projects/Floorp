22ad94bee57c7c73792c5dde9db8ea12700e1180
Francois Marier <francois@mozilla.com>
2015-11-23 13:34:31 -0800

    Bug 1226490 - Allow administrators to prevent users from ignoring Safe Browsing warnings. r=gcp,r=smaug

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2994,10 +2994,12 @@ var BrowserOnClick = {
         break;
 
       case "ignoreWarningButton":
-        if (sendTelemetry) {
-          secHistogram.add(nsISecTel[bucketName + "IGNORE_WARNING"]);
+        if (gPrefService.getBoolPref("browser.safebrowsing.allowOverride")) {
+          if (sendTelemetry) {
+            secHistogram.add(nsISecTel[bucketName + "IGNORE_WARNING"]);
+          }
+          this.ignoreWarningButton(reason);
         }
-        this.ignoreWarningButton(reason);
         break;
     }
   },
