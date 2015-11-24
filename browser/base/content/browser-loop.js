91f1fbaf3b8bc9fadf478af48b86dab0488a6e48
Manuel Casas <manuel.casasbarrado@gmail.com>
2015-11-24 18:46:05 +0000

    Bug 1225189 - Disable Hello for e10s windows. r=mikedeboer

--- browser/base/content/browser-loop.js
+++ browser/base/content/browser-loop.js
@@ -171,11 +171,14 @@ var LoopUI;
           this.LoopAPI.initialize();
 
           let anchor = event ? event.target : this.toolbarButton.anchor;
-
+          let setHeight = 410;
+          if (gBrowser.selectedBrowser.getAttribute("remote") === "true") {
+            setHeight = 262;
+          }
           this.PanelFrame.showPopup(window, anchor,
             "loop", null, "about:looppanel",
             // Loop wants a fixed size for the panel. This also stops it dynamically resizing.
-            { width: 330, height: 410 },
+            { width: 330, height: setHeight },
             callback);
         });
       });
