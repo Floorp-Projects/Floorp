76e3c19c9101dadf9628dafc47b9d1d86be9d61f
John Daggett <jdaggett@mozilla.com>
2015-11-25 15:30:45 +0900

    Bug 1173260 - support multiple families for generics. r=heycam, a=ritu

--- gfx/thebes/gfxPlatformFontList.h
+++ gfx/thebes/gfxPlatformFontList.h
@@ -120,7 +120,7 @@ public:
 
     void UpdateFontList();
 
-    void ClearLangGroupPrefFonts();
+    virtual void ClearLangGroupPrefFonts();
 
     virtual void GetFontFamilyList(nsTArray<RefPtr<gfxFontFamily> >& aFamilyArray);
 
@@ -313,6 +313,11 @@ protected:
                                 void* aUserArg);
 
     virtual void GetFontFamilyNames(nsTArray<nsString>& aFontFamilyNames);
+
+    // helper function to map lang to lang group
+    nsIAtom* GetLangGroup(nsIAtom* aLanguage);
+
+    static const char* GetGenericName(mozilla::FontFamilyType aGenericType);
 
     // gfxFontInfoLoader overrides, used to load in font cmaps
     virtual void InitLoader();
