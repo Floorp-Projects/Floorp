7a46114bcfa2c0478785be45daf69b099c69bb59
Nick Thomas <nthomas@mozilla.com>
2015-11-26 11:06:02 +1300

    Bug 1222227, upload public b2g files to S3 system, temporarily disables socorro and source manifest uploads, r=catlee

--- testing/mozharness/configs/b2g/releng-emulator.py
+++ testing/mozharness/configs/b2g/releng-emulator.py
@@ -11,8 +11,9 @@ config = {
         'make-updates',
         'prep-upload',
         'upload',
-        'make-socorro-json',
-        'upload-source-manifest',
+        # bug 1222227 - temporarily disable for S3 migration
+        # 'make-socorro-json',
+        # 'upload-source-manifest',
     ],
     "upload": {
         "default": {
@@ -28,7 +29,7 @@ config = {
         "public": {
             "ssh_key": os.path.expanduser("~/.ssh/ffxbld_rsa"),
             "ssh_user": "ffxbld",
-            "upload_remote_host": "stage.mozilla.org",
+            "upload_remote_host": "upload.ffxbld.productdelivery.prod.mozaws.net",
             "post_upload_cmd": "post_upload.py --tinderbox-builds-dir %(branch)s-%(target)s -p b2g -i %(buildid)s --revision %(revision)s --release-to-tinderbox-dated-builds",
             "post_upload_nightly_cmd": "post_upload.py --tinderbox-builds-dir %(branch)s-%(target)s -b %(branch)s-%(target)s -p b2g -i %(buildid)s --revision %(revision)s --release-to-tinderbox-dated-builds --release-to-latest --release-to-dated",
         },
