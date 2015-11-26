96f234e0ba1e08014045e6ae61ab7f8624b4cb22
Felipe Gomes <felipc@gmail.com>
2015-11-26 14:59:08 -0200

    Bug 1226487 - Don't use the blocklist for the e10s a11y check on beta; instead, block everything. r=jmathies a=lmandel

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -3351,7 +3351,7 @@ var E10SUINotification = {
 var E10SAccessibilityCheck = {
   init: function() {
     Services.obs.addObserver(this, "a11y-init-or-shutdown", true);
-    if (Services.appinfo.accessibilityIsBlacklistedForE10S) {
+    if (Services.appinfo.accessibilityEnabled) {
       this._showE10sAccessibilityWarning();
     }
   },
@@ -3360,8 +3360,7 @@ var E10SAccessibilityCheck = {
 
   observe: function(subject, topic, data) {
     if (topic == "a11y-init-or-shutdown"
-        && data == "1" &&
-        Services.appinfo.accessibilityIsBlacklistedForE10S) {
+        && data == "1") {
       this._showE10sAccessibilityWarning();
     }
   },
