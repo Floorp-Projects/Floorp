5329fd760cfa5bdf8e5595de7516304e63811f9c
Anup Kumar <allamsetty.anup@gmail.com>
2015-11-27 21:44:00 +0100

    Bug 1197307 - remove PR_snprintf calls in layout. r=froydnj r=dholbert

--- layout/style/CounterStyleManager.cpp
+++ layout/style/CounterStyleManager.cpp
@@ -18,7 +18,6 @@
 #include "nsTArray.h"
 #include "nsTHashtable.h"
 #include "nsUnicodeProperties.h"
-#include "prprf.h"
 
 namespace mozilla {
 
@@ -212,8 +211,7 @@ DecimalToText(CounterValue aOrdinal, nsSubstring& aResult)
 {
   // 3 for additional digit, negative sign, and null
   char cbuf[std::numeric_limits<CounterValue>::digits10 + 3];
-  PR_snprintf(cbuf, sizeof(cbuf), "%ld", aOrdinal);
-  aResult.AssignASCII(cbuf);
+  aResult.AppendInt(aOrdinal);
   return true;
 }
 
