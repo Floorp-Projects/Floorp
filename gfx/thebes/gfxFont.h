9da1a306407d65377372c810557b68e97d681411
Xidorn Quan <quanxunzhen@gmail.com>
2015-11-28 11:56:33 +1100

    Bug 1040668 part 6 - Add some specifier on gfxTextRun and gfxShapedWord so that compilers are able to reason out certain optimizations. r=jfkthame

--- gfx/thebes/gfxFont.h
+++ gfx/thebes/gfxFont.h
@@ -1141,7 +1141,7 @@ protected:
  * The glyph data is copied into gfxTextRuns as needed from the cache of
  * ShapedWords associated with each gfxFont instance.
  */
-class gfxShapedWord : public gfxShapedText
+class gfxShapedWord final : public gfxShapedText
 {
 public:
     // Create a ShapedWord that can hold glyphs for aLength characters,
