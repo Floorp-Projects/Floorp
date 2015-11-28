2828d223d36076831f010507279154371a190c2d
Phil Ringnalda <philringnalda@gmail.com>
2015-11-27 22:40:54 -0800

    Back out 4 changesets (bug 1209184) for Android/B2G xpcshell bustage CLOSED TREE
    Backed out changeset 8e692344588a (bug 1209184)
    Backed out changeset f9ab76689610 (bug 1209184)
    Backed out changeset 9c63ffd499eb (bug 1209184)
    Backed out changeset e75f9f24d0dc (bug 1209184)

--- toolkit/components/utils/simpleServices.js
+++ toolkit/components/utils/simpleServices.js
@@ -12,16 +12,10 @@
 
 "use strict";
 
-const Cc = Components.classes;
 const Cu = Components.utils;
 const Ci = Components.interfaces;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-
-XPCOMUtils.defineLazyModuleGetter(this, "NetUtil",
-                                  "resource://gre/modules/NetUtil.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "Services",
-                                  "resource://gre/modules/Services.jsm");
 
 function RemoteTagServiceService()
 {
@@ -54,7 +48,6 @@ function AddonPolicyService()
 {
   this.wrappedJSObject = this;
   this.mayLoadURICallbacks = new Map();
-  this.localizeCallbacks = new Map();
 }
 
 AddonPolicyService.prototype = {
@@ -69,17 +62,8 @@ AddonPolicyService.prototype = {
    * @see nsIAddonPolicyService.addonMayLoadURI
    */
   addonMayLoadURI(aAddonId, aURI) {
-    let cb = this.mayLoadURICallbacks.get(aAddonId);
+    let cb = this.mayLoadURICallbacks[aAddonId];
     return cb ? cb(aURI) : false;
-  },
-
-  /*
-   * Invokes a callback (if any) associated with the addon to loclaize a
-   * resource belonging to that add-on.
-   */
-  localizeAddonString(aAddonId, aString) {
-    let cb = this.localizeCallbacks.get(aAddonId);
-    return cb ? cb(aString) : aString;
   },
 
   /*
@@ -121,21 +105,9 @@ AddonPolicyService.prototype = {
    */
   setAddonLoadURICallback(aAddonId, aCallback) {
     if (aCallback) {
-      this.mayLoadURICallbacks.set(aAddonId, aCallback);
+      this.mayLoadURICallbacks[aAddonId] = aCallback;
     } else {
-      delete this.mayLoadURICallbacks.delete(aAddonId);
-    }
-  },
-
-  /*
-   * Sets the callbacks used by the stream converter service to localize
-   * add-on resources.
-   */
-  setAddonLocalizeCallback(aAddonId, aCallback) {
-    if (aCallback) {
-      this.localizeCallbacks.set(aAddonId, aCallback);
-    } else {
-      delete this.localizeCallbacks.delete(aAddonId);
+      delete this.mayLoadURICallbacks[aAddonId];
     }
   },
 
@@ -162,95 +134,4 @@ AddonPolicyService.prototype = {
   }
 };
 
-/*
- * This class provides a stream filter for locale messages in CSS files served
- * by the moz-extension: protocol handler.
- *
- * See SubstituteChannel in netwerk/protocol/res/ExtensionProtocolHandler.cpp
- * for usage.
- */
-function AddonLocalizationConverter()
-{
-  this.aps = Cc["@mozilla.org/addons/policy-service;1"].getService(Ci.nsIAddonPolicyService)
-    .wrappedJSObject;
-}
-
-AddonLocalizationConverter.prototype = {
-  classID: Components.ID("{ded150e3-c92e-4077-a396-0dba9953e39f}"),
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIStreamConverter]),
-
-  FROM_TYPE: "application/vnd.mozilla.webext.unlocalized",
-  TO_TYPE: "text/css",
-
-  checkTypes(aFromType, aToType) {
-    if (aFromType != this.FROM_TYPE) {
-      throw Components.Exception("Invalid aFromType value", Cr.NS_ERROR_INVALID_ARG,
-                                 Components.stack.caller.caller);
-    }
-    if (aToType != this.TO_TYPE) {
-      throw Components.Exception("Invalid aToType value", Cr.NS_ERROR_INVALID_ARG,
-                                 Components.stack.caller.caller);
-    }
-  },
-
-  // aContext must be a nsIURI object for a valid moz-extension: URL.
-  getAddonId(aContext) {
-    // In this case, we want the add-on ID even if the URL is web accessible,
-    // so check the root rather than the exact path.
-    let uri = Services.io.newURI("/", null, aContext);
-
-    let id = this.aps.extensionURIToAddonId(uri);
-    if (id == undefined) {
-      throw new Components.Exception("Invalid context", Cr.NS_ERROR_INVALID_ARG);
-    }
-    return id;
-  },
-
-  convertToStream(aAddonId, aString) {
-    let stream = Cc["@mozilla.org/io/string-input-stream;1"]
-      .createInstance(Ci.nsIStringInputStream);
-
-    stream.data = this.aps.localizeAddonString(aAddonId, aString);
-    return stream;
-  },
-
-  convert(aStream, aFromType, aToType, aContext) {
-    this.checkTypes(aFromType, aToType);
-    let addonId = this.getAddonId(aContext);
-
-    let string = NetUtil.readInputStreamToString(aStream, aStream.available());
-    return this.convertToStream(addonId, string);
-  },
-
-  asyncConvertData(aFromType, aToType, aListener, aContext) {
-    this.checkTypes(aFromType, aToType);
-    this.addonId = this.getAddonId(aContext);
-    this.listener = aListener;
-  },
-
-  onStartRequest(aRequest, aContext) {
-    this.parts = [];
-  },
-
-  onDataAvailable(aRequest, aContext, aInputStream, aOffset, aCount) {
-    this.parts.push(NetUtil.readInputStreamToString(aInputStream, aCount));
-  },
-
-  onStopRequest(aRequest, aContext, aStatusCode) {
-    try {
-      this.listener.onStartRequest(aRequest, null);
-      if (Components.isSuccessCode(aStatusCode)) {
-        let string = this.parts.join("");
-        let stream = this.convertToStream(this.addonId, string);
-
-        this.listener.onDataAvailable(aRequest, null, stream, 0, stream.data.length);
-      }
-    } catch (e) {
-      aStatusCode = e.result || Cr.NS_ERROR_FAILURE;
-    }
-    this.listener.onStopRequest(aRequest, null, aStatusCode);
-  },
-};
-
-this.NSGetFactory = XPCOMUtils.generateNSGetFactory([RemoteTagServiceService, AddonPolicyService,
-                                                     AddonLocalizationConverter]);
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([RemoteTagServiceService, AddonPolicyService]);
