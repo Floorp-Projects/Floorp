19befb55a508506d71b79803bc3a5e73c615c853
L. David Baron <dbaron@dbaron.org>
2015-11-30 14:02:26 -0800

    Bug 1228501 patch 5 - Remove NS_FONT_DECORATION_* constants.  r=jdaggett
    Decorations in Gecko are associated with the CSS 'text-decoration'
    property, so the constants should live in nsStyleConsts.h, as they
    largely already do (since the names defined there are the vast majority
    of the uses).

--- gfx/src/nsFont.h
+++ gfx/src/nsFont.h
@@ -143,9 +143,4 @@ protected:
 #define NS_FONT_VARIANT_NORMAL            0
 #define NS_FONT_VARIANT_SMALL_CAPS        1
 
-#define NS_FONT_DECORATION_NONE           0x0
-#define NS_FONT_DECORATION_UNDERLINE      0x1
-#define NS_FONT_DECORATION_OVERLINE       0x2
-#define NS_FONT_DECORATION_LINE_THROUGH   0x4
-
 #endif /* nsFont_h___ */
