5664cf90141c349de6c76255e57b12b6d3274a95
L. David Baron <dbaron@dbaron.org>
2015-11-30 14:02:25 -0800

    Bug 1228501 patch 2 - Remove nsFont::decorations member variable.  r=jdaggett
    The member variable was always set to 0 or to NS_FONT_DECORATION_NONE,
    which is 0.
    
    Although this does leave a gap in the structure, I don't see any
    opportunities for reordering members to actually shrink nsFont.
    
    --HG--
    extra : commitid : 34LA3wCkbdR

--- gfx/src/nsFont.cpp
+++ gfx/src/nsFont.cpp
@@ -48,7 +48,6 @@ nsFont::Init()
   sizeAdjust = -1.0f;
   kerning = NS_FONT_KERNING_AUTO;
   synthesis = NS_FONT_SYNTHESIS_WEIGHT | NS_FONT_SYNTHESIS_STYLE;
-  decorations = 0;
 
   variantAlternates = 0;
   variantCaps = NS_FONT_VARIANT_CAPS_NORMAL;
@@ -65,7 +64,6 @@ nsFont::nsFont(const nsFont& aOther)
   systemFont = aOther.systemFont;
   weight = aOther.weight;
   stretch = aOther.stretch;
-  decorations = aOther.decorations;
   smoothing = aOther.smoothing;
   size = aOther.size;
   sizeAdjust = aOther.sizeAdjust;
@@ -118,15 +116,6 @@ bool nsFont::BaseEquals(const nsFont& aOther) const
   return false;
 }
 
-bool nsFont::Equals(const nsFont& aOther) const
-{
-  if (BaseEquals(aOther) &&
-      (decorations == aOther.decorations)) {
-    return true;
-  }
-  return false;
-}
-
 nsFont& nsFont::operator=(const nsFont& aOther)
 {
   fontlist = aOther.fontlist;
@@ -134,7 +123,6 @@ nsFont& nsFont::operator=(const nsFont& aOther)
   systemFont = aOther.systemFont;
   weight = aOther.weight;
   stretch = aOther.stretch;
-  decorations = aOther.decorations;
   smoothing = aOther.smoothing;
   size = aOther.size;
   sizeAdjust = aOther.sizeAdjust;
