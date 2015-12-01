06af4d3da6ee421f1401fe5b27ea0b0936fcaab9
Wes Kocher <wkocher@mozilla.com>
2015-12-01 15:55:26 -0800

    Backed out changeset d821c903bd2b (bug 1171863) for frequent failures in browser_animation_timeline_pause_button.js

--- devtools/client/animationinspector/test/browser_animation_timeline_scrubber_movable.js
+++ devtools/client/animationinspector/test/browser_animation_timeline_scrubber_movable.js
@@ -23,14 +23,16 @@ add_task(function*() {
 
   info("Mousedown in the header to move the scrubber");
   yield synthesizeInHeaderAndWaitForChange(timeline, 50, 1, "mousedown");
-  checkScrubberIsAt(scrubberEl, timeHeaderEl, 50);
+  let newPos = parseInt(scrubberEl.style.left, 10);
+  is(newPos, 50, "The scrubber moved on mousedown");
 
   ok(playTimelineButtonEl.classList.contains("paused"),
      "The timeline play button is in its paused state after mousedown");
 
   info("Continue moving the mouse and verify that the scrubber tracks it");
   yield synthesizeInHeaderAndWaitForChange(timeline, 100, 1, "mousemove");
-  checkScrubberIsAt(scrubberEl, timeHeaderEl, 100);
+  newPos = parseInt(scrubberEl.style.left, 10);
+  is(newPos, 100, "The scrubber followed the mouse");
 
   ok(playTimelineButtonEl.classList.contains("paused"),
      "The timeline play button is in its paused state after mousemove");
@@ -38,7 +40,8 @@ add_task(function*() {
   info("Release the mouse and move again and verify that the scrubber stays");
   EventUtils.synthesizeMouse(timeHeaderEl, 100, 1, {type: "mouseup"}, win);
   EventUtils.synthesizeMouse(timeHeaderEl, 200, 1, {type: "mousemove"}, win);
-  checkScrubberIsAt(scrubberEl, timeHeaderEl, 100);
+  newPos = parseInt(scrubberEl.style.left, 10);
+  is(newPos, 100, "The scrubber stopped following the mouse");
 
   info("Try to drag the scrubber handle and check that the scrubber moves");
   let onDataChanged = timeline.once("timeline-data-changed");
@@ -47,22 +50,12 @@ add_task(function*() {
   EventUtils.synthesizeMouse(timeHeaderEl, 0, 0, {type: "mouseup"}, win);
   yield onDataChanged;
 
-  checkScrubberIsAt(scrubberEl, timeHeaderEl, 0);
+  newPos = parseInt(scrubberEl.style.left, 10);
+  is(newPos, 0, "The scrubber stopped following the mouse");
 });
 
 function* synthesizeInHeaderAndWaitForChange(timeline, x, y, type) {
   let onDataChanged = timeline.once("timeline-data-changed");
   EventUtils.synthesizeMouse(timeline.timeHeaderEl, x, y, {type}, timeline.win);
   yield onDataChanged;
-}
-
-function getPositionPercentage(pos, headerEl) {
-  return pos * 100 / headerEl.offsetWidth;
-}
-
-function checkScrubberIsAt(scrubberEl, timeHeaderEl, pos) {
-  let newPos = Math.round(parseFloat(scrubberEl.style.left));
-  let expectedPos = Math.round(getPositionPercentage(pos, timeHeaderEl));
-  is(newPos, expectedPos,
-     `The scrubber is at position ${pos} (${expectedPos}%)`);
 }
