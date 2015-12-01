60e3907027521fbefdab68ddedd199d821539913
Mike de Boer <mdeboer@mozilla.com>
2015-12-01 12:17:28 +0100

    Bug 1225832: partially fix UITour for Loop without navigator.mozLoop present. r=Standard8

--- browser/extensions/loop/content/panels/js/panel.js
+++ browser/extensions/loop/content/panels/js/panel.js
@@ -20,7 +20,7 @@ loop.panel = (function(_, mozL10n) {
       loop.requestMulti(
         ["OpenGettingStartedTour", "getting-started"],
         ["SetLoopPref", "gettingStarted.seen", true]);
-      var event = new CustomEvent("GettingStartedSeen");
+      var event = new CustomEvent("GettingStartedSeen", { detail: true });
       window.dispatchEvent(event);
       this.closeWindow();
     },
@@ -94,21 +94,9 @@ loop.panel = (function(_, mozL10n) {
 
     getInitialState: function() {
       return {
-        terms_of_use_url: null,
-        privacy_notice_url: null
+        terms_of_use_url: loop.getStoredRequest(["GetLoopPref", "legal.ToS_url"]),
+        privacy_notice_url: loop.getStoredRequest(["GetLoopPref", "legal.privacy_url"])
       };
-    },
-
-    componentWillMount: function() {
-      loop.requestMulti(
-        ["GetLoopPref", "legal.ToS_url"],
-        ["GetLoopPref", "legal.privacy_url"]
-      ).then(function(results) {
-        this.setState({
-          terms_of_use_url: results[0],
-          privacy_notice_url: results[1]
-        });
-      }.bind(this));
     },
 
     handleLinkClick: function(event) {
@@ -197,34 +185,26 @@ loop.panel = (function(_, mozL10n) {
 
     getInitialState: function() {
       return {
-        doNotDisturb: false,
-        fxAEnabled: false,
-        signedIn: false
+        signedIn: !!loop.getStoredRequest(["GetUserProfile"]),
+        fxAEnabled: loop.getStoredRequest(["GetFxAEnabled"]),
+        doNotDisturb: loop.getStoredRequest(["GetDoNotDisturb"])
       };
-    },
-
-    componentWillMount: function() {
-      this._updateState();
     },
 
     componentWillUpdate: function(nextProps, nextState) {
       if (nextState.showMenu !== this.state.showMenu) {
-        this._updateState();
+        loop.requestMulti(
+          ["GetUserProfile"],
+          ["GetFxAEnabled"],
+          ["GetDoNotDisturb"]
+        ).then(function(results) {
+          this.setState({
+            signedIn: !!results[0],
+            fxAEnabled: results[1],
+            doNotDisturb: results[2]
+          });
+        }.bind(this));
       }
-    },
-
-    _updateState: function() {
-      loop.requestMulti(
-        ["GetUserProfile"],
-        ["GetFxAEnabled"],
-        ["GetDoNotDisturb"]
-      ).then(function(results) {
-        this.setState({
-          signedIn: !!results[0],
-          fxAEnabled: results[1],
-          doNotDisturb: results[2]
-        });
-      }.bind(this));
     },
 
     handleClickSettingsEntry: function() {
@@ -922,11 +902,11 @@ loop.panel = (function(_, mozL10n) {
 
     getInitialState: function() {
       return {
-        fxAEnabled: true,
-        hasEncryptionKey: false,
-        userProfile: null,
-        gettingStartedSeen: true,
-        multiProcessEnabled: false
+        fxAEnabled: loop.getStoredRequest(["GetFxAEnabled"]),
+        hasEncryptionKey: loop.getStoredRequest(["GetHasEncryptionKey"]),
+        userProfile: loop.getStoredRequest(["GetUserProfile"]),
+        gettingStartedSeen: loop.getStoredRequest(["GetLoopPref", "gettingStarted.seen"]),
+        multiProcessEnabled: loop.getStoredRequest(["IsMultiProcessEnabled"])
       };
     },
 
@@ -983,32 +963,12 @@ loop.panel = (function(_, mozL10n) {
       }.bind(this));
     },
 
-    _gettingStartedSeen: function() {
-      loop.request("GetLoopPref", "gettingStarted.seen").then(function(result) {
-        this.setState({
-          gettingStartedSeen: result
-        });
-      }.bind(this));
+    _gettingStartedSeen: function(e) {
+      this.setState({ gettingStartedSeen: !!e.detail });
     },
 
     componentWillMount: function() {
       this.updateServiceErrors();
-
-      loop.requestMulti(
-        ["GetFxAEnabled"],
-        ["GetHasEncryptionKey"],
-        ["GetUserProfile"],
-        ["GetLoopPref", "gettingStarted.seen"],
-        ["IsMultiProcessEnabled"]
-      ).then(function(results) {
-        this.setState({
-          fxAEnabled: results[0],
-          hasEncryptionKey: results[1],
-          userProfile: results[2],
-          gettingStartedSeen: results[3],
-          multiProcessEnabled: results[4]
-        });
-      }.bind(this));
     },
 
     componentDidMount: function() {
@@ -1084,18 +1044,33 @@ loop.panel = (function(_, mozL10n) {
    * Panel initialisation.
    */
   function init() {
-    return loop.requestMulti(
+    var requests = [
       ["GetAllConstants"],
       ["GetAllStrings"],
       ["GetLocale"],
       ["GetPluralRule"]
-    ).then(function(results) {
-      var constants = results[0];
+    ];
+    var prefetch = [
+      ["GetLoopPref", "gettingStarted.seen"],
+      ["GetLoopPref", "legal.ToS_url"],
+      ["GetLoopPref", "legal.privacy_url"],
+      ["GetUserProfile"],
+      ["GetFxAEnabled"],
+      ["GetDoNotDisturb"],
+      ["GetHasEncryptionKey"],
+      ["IsMultiProcessEnabled"]
+    ];
+
+    return loop.requestMulti.apply(null, requests.concat(prefetch)).then(function(results) {
+      // `requestIdx` is keyed off the order of the `requests` and `prefetch`
+      // arrays. Be careful to update both when making changes.
+      var requestIdx = 0;
+      var constants = results[requestIdx];
       // Do the initial L10n setup, we do this before anything
       // else to ensure the L10n environment is setup correctly.
-      var stringBundle = results[1];
-      var locale = results[2];
-      var pluralRule = results[3];
+      var stringBundle = results[++requestIdx];
+      var locale = results[++requestIdx];
+      var pluralRule = results[++requestIdx];
       mozL10n.initialize({
         locale: locale,
         pluralRule: pluralRule,
@@ -1107,6 +1082,11 @@ loop.panel = (function(_, mozL10n) {
 
           return JSON.stringify({ textContent: stringBundle[key] });
         }
+      });
+
+      prefetch.forEach(function(req) {
+        req.shift();
+        loop.storeRequest(req, results[++requestIdx]);
       });
 
       var notifications = new sharedModels.NotificationCollection();
