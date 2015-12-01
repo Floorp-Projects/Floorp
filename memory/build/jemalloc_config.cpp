979487623ca1bc21509b7adbc9fb9200ad22f8a1
Jan Beich <jbeich@FreeBSD.org>
2015-12-01 16:54:44 +0000

    Bug 1229395 - Part 2 - Rely on MALLOC_H to provide function prototypes for MOZ_NATIVE_JEMALLOC. r=glandium, a=lizzard on a CLOSED TREE

--- memory/build/jemalloc_config.cpp
+++ memory/build/jemalloc_config.cpp
@@ -12,12 +12,7 @@
 #include "mozilla/Types.h"
 
 #if defined(MOZ_NATIVE_JEMALLOC)
-MOZ_IMPORT_API int
-je_(mallctl)(const char*, void*, size_t*, void*, size_t);
-MOZ_IMPORT_API int
-je_(mallctlnametomib)(const char *name, size_t *mibp, size_t *miblenp);
-MOZ_IMPORT_API int
-je_(mallctlbymib)(const size_t *mib, size_t miblen, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
+#include MALLOC_H
 #else
 #define DLLEXPORT
 #include "jemalloc/jemalloc.h"
