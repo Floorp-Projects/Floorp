46bf106373b99b2fec2fc80d243edddf4c0de8a0
Tooru Fujisawa <arai_a@mac.com>
2015-12-01 17:03:23 +0900

    Bug 1229057 - Part 10: Move MacroAssembler::mul32 into generic macro assembler. r=sstangl

--- js/src/jit/arm64/MacroAssembler-arm64-inl.h
+++ js/src/jit/arm64/MacroAssembler-arm64-inl.h
@@ -308,6 +308,21 @@ MacroAssembler::subDouble(FloatRegister src, FloatRegister dest)
     fsub(ARMFPRegister(dest, 64), ARMFPRegister(dest, 64), ARMFPRegister(src, 64));
 }
 
+void
+MacroAssembler::mul32(Register src1, Register src2, Register dest, Label* onOver, Label* onZero)
+{
+    Smull(ARMRegister(dest, 64), ARMRegister(src1, 32), ARMRegister(src2, 32));
+    if (onOver) {
+        Cmp(ARMRegister(dest, 64), Operand(ARMRegister(dest, 32), vixl::SXTW));
+        B(onOver, NotEqual);
+    }
+    if (onZero)
+        Cbz(ARMRegister(dest, 32), onZero);
+
+    // Clear upper 32 bits.
+    Mov(ARMRegister(dest, 32), ARMRegister(dest, 32));
+}
+
 // ===============================================================
 // Shift functions
 
