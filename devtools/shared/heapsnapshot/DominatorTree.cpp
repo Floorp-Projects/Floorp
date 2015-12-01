83602ae35cd4bfecc81c9ec851fd2864d8d565b8
Nick Fitzgerald <fitzgen@gmail.com>
2015-11-30 17:38:06 -0800

    Bug 1226416 - Expose a method to get a node's set of immediately dominated nodes in the dominator tree; r=bz,sfink
    This adds the `getImmediatelyDominated` method to `DominatorTree` which takes a
    node id and returns the set of each node ids for every node that is immediately
    dominated by the node with the given id. The results are sorted by greatest to
    least retained size. In conjunction with the `root` attribute, this can be used
    to traverse the whole dominator tree.

--- devtools/shared/heapsnapshot/DominatorTree.cpp
+++ devtools/shared/heapsnapshot/DominatorTree.cpp
@@ -4,13 +4,24 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/devtools/DominatorTree.h"
-
 #include "js/Debug.h"
 #include "mozilla/CycleCollectedJSRuntime.h"
 #include "mozilla/dom/DominatorTreeBinding.h"
 
 namespace mozilla {
 namespace devtools {
+
+static MallocSizeOf
+getCurrentThreadDebuggerMallocSizeOf()
+{
+  auto ccrt = CycleCollectedJSRuntime::Get();
+  MOZ_ASSERT(ccrt);
+  auto rt = ccrt->Runtime();
+  MOZ_ASSERT(rt);
+  auto mallocSizeOf = JS::dbg::GetDebuggerMallocSizeOf(rt);
+  MOZ_ASSERT(mallocSizeOf);
+  return mallocSizeOf;
+}
 
 dom::Nullable<uint64_t>
 DominatorTree::GetRetainedSize(uint64_t aNodeId, ErrorResult& aRv)
@@ -20,13 +31,7 @@ DominatorTree::GetRetainedSize(uint64_t aNodeId, ErrorResult& aRv)
   if (node.isNothing())
     return dom::Nullable<uint64_t>();
 
-  auto ccrt = CycleCollectedJSRuntime::Get();
-  MOZ_ASSERT(ccrt);
-  auto rt = ccrt->Runtime();
-  MOZ_ASSERT(rt);
-  auto mallocSizeOf = JS::dbg::GetDebuggerMallocSizeOf(rt);
-  MOZ_ASSERT(mallocSizeOf);
-
+  auto mallocSizeOf = getCurrentThreadDebuggerMallocSizeOf();
   JS::ubi::Node::Size size = 0;
   if (!mDominatorTree.getRetainedSize(*node, mallocSizeOf, size)) {
     aRv.Throw(NS_ERROR_OUT_OF_MEMORY);
@@ -36,6 +41,79 @@ DominatorTree::GetRetainedSize(uint64_t aNodeId, ErrorResult& aRv)
   MOZ_ASSERT(size != 0,
              "The node should not have been unknown since we got it from the heap snapshot.");
   return dom::Nullable<uint64_t>(size);
+}
+
+struct NodeAndRetainedSize
+{
+  JS::ubi::Node mNode;
+  JS::ubi::Node::Size mSize;
+
+  NodeAndRetainedSize(const JS::ubi::Node& aNode, JS::ubi::Node::Size aSize)
+    : mNode(aNode)
+    , mSize(aSize)
+  { }
+
+  struct Comparator
+  {
+    static bool
+    Equals(const NodeAndRetainedSize& aLhs, const NodeAndRetainedSize& aRhs)
+    {
+      return aLhs.mSize == aRhs.mSize;
+    }
+
+    static bool
+    LessThan(const NodeAndRetainedSize& aLhs, const NodeAndRetainedSize& aRhs)
+    {
+      // Use > because we want to sort from greatest to least retained size.
+      return aLhs.mSize > aRhs.mSize;
+    }
+  };
+};
+
+void
+DominatorTree::GetImmediatelyDominated(uint64_t aNodeId,
+                                       dom::Nullable<nsTArray<uint64_t>>& aOutResult,
+                                       ErrorResult& aRv)
+{
+  MOZ_ASSERT(aOutResult.IsNull());
+
+  JS::ubi::Node::Id id(aNodeId);
+  Maybe<JS::ubi::Node> node = mHeapSnapshot->getNodeById(id);
+  if (node.isNothing())
+    return;
+
+  // Get all immediately dominated nodes and their retained sizes.
+  MallocSizeOf mallocSizeOf = getCurrentThreadDebuggerMallocSizeOf();
+  Maybe<JS::ubi::DominatorTree::DominatedSetRange> range = mDominatorTree.getDominatedSet(*node);
+  MOZ_ASSERT(range.isSome(), "The node should be known, since we got it from the heap snapshot.");
+  size_t length = range->length();
+  nsTArray<NodeAndRetainedSize> dominatedNodes(length);
+  for (const JS::ubi::Node& dominatedNode : *range) {
+    JS::ubi::Node::Size retainedSize = 0;
+    if (NS_WARN_IF(!mDominatorTree.getRetainedSize(dominatedNode, mallocSizeOf, retainedSize))) {
+      aRv.Throw(NS_ERROR_OUT_OF_MEMORY);
+      return;
+    }
+    MOZ_ASSERT(retainedSize != 0,
+               "retainedSize should not be zero since we know the node is in the dominator tree.");
+
+    dominatedNodes.AppendElement(NodeAndRetainedSize(dominatedNode, retainedSize));
+  }
+
+  // Sort them by retained size.
+  NodeAndRetainedSize::Comparator comparator;
+  dominatedNodes.Sort(comparator);
+
+  // Fill the result with the nodes' ids.
+  JS::ubi::Node root = mDominatorTree.root();
+  aOutResult.SetValue(nsTArray<uint64_t>(length));
+  for (const NodeAndRetainedSize& entry : dominatedNodes) {
+    // The root dominates itself, but we don't want to expose that to JS.
+    if (entry.mNode == root)
+      continue;
+
+    aOutResult.Value().AppendElement(entry.mNode.identifier());
+  }
 }
 
 
