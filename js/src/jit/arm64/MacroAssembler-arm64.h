a1db36e3ac5e100d2c38453157095ed0c5893a0e
Tooru Fujisawa <arai_a@mac.com>
2015-12-01 17:03:15 +0900

    Bug 1229057 - Part 9: Move MacroAssembler::subDouble into generic macro assembler. r=lth

--- js/src/jit/arm64/MacroAssembler-arm64.h
+++ js/src/jit/arm64/MacroAssembler-arm64.h
@@ -1236,9 +1236,6 @@ class MacroAssemblerCompat : public vixl::MacroAssembler
     void negateFloat(FloatRegister reg) {
         fneg(ARMFPRegister(reg, 32), ARMFPRegister(reg, 32));
     }
-    void subDouble(FloatRegister src, FloatRegister dest) {
-        fsub(ARMFPRegister(dest, 64), ARMFPRegister(dest, 64), ARMFPRegister(src, 64));
-    }
     void mulDouble(FloatRegister src, FloatRegister dest) {
         fmul(ARMFPRegister(dest, 64), ARMFPRegister(dest, 64), ARMFPRegister(src, 64));
     }
