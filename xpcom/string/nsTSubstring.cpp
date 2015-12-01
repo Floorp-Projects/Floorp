09ea98f4807871d48eb1801e366809cde3fb0fa8
Nicholas Nethercote <nnethercote@mozilla.com>
2015-12-01 15:36:26 -0800

    Bug 1229458 - Remove SizeOfIncludingThisMustBeUnshared() from string classes. r=mccr8.
    The patch changes all uses of SizeOfIncludingThisMustBeUnshared() to
    SizeOfIncludingThisIfUnshared(). This incurs the (tiny) cost of an unnecessary
    IsReadonly() check for guaranteed-unshared strings, but avoids the possible
    assertion failures that would occur when MustBeUnshared() was used incorrectly
    on shared strings, which is an easy mistake to make.

--- xpcom/string/nsTSubstring.cpp
+++ xpcom/string/nsTSubstring.cpp
@@ -1001,12 +1001,12 @@ nsTSubstring_CharT::AppendFloat(double aFloat)
 }
 
 size_t
-nsTSubstring_CharT::SizeOfExcludingThisMustBeUnshared(
+nsTSubstring_CharT::SizeOfExcludingThisIfUnshared(
     mozilla::MallocSizeOf aMallocSizeOf) const
 {
   if (mFlags & F_SHARED) {
     return nsStringBuffer::FromData(mData)->
-      SizeOfIncludingThisMustBeUnshared(aMallocSizeOf);
+      SizeOfIncludingThisIfUnshared(aMallocSizeOf);
   }
   if (mFlags & F_OWNED) {
     return aMallocSizeOf(mData);
@@ -1024,26 +1024,10 @@ nsTSubstring_CharT::SizeOfExcludingThisMustBeUnshared(
 }
 
 size_t
-nsTSubstring_CharT::SizeOfExcludingThisIfUnshared(
-    mozilla::MallocSizeOf aMallocSizeOf) const
-{
-  // This is identical to SizeOfExcludingThisMustBeUnshared except for the
-  // F_SHARED case.
-  if (mFlags & F_SHARED) {
-    return nsStringBuffer::FromData(mData)->
-           SizeOfIncludingThisIfUnshared(aMallocSizeOf);
-  }
-  if (mFlags & F_OWNED) {
-    return aMallocSizeOf(mData);
-  }
-  return 0;
-}
-
-size_t
 nsTSubstring_CharT::SizeOfExcludingThisEvenIfShared(
     mozilla::MallocSizeOf aMallocSizeOf) const
 {
-  // This is identical to SizeOfExcludingThisMustBeUnshared except for the
+  // This is identical to SizeOfExcludingThisIfUnshared except for the
   // F_SHARED case.
   if (mFlags & F_SHARED) {
     return nsStringBuffer::FromData(mData)->
@@ -1053,14 +1037,6 @@ nsTSubstring_CharT::SizeOfExcludingThisEvenIfShared(
     return aMallocSizeOf(mData);
   }
   return 0;
-}
-
-size_t
-nsTSubstring_CharT::SizeOfIncludingThisMustBeUnshared(
-    mozilla::MallocSizeOf aMallocSizeOf) const
-{
-  return aMallocSizeOf(this) +
-         SizeOfExcludingThisMustBeUnshared(aMallocSizeOf);
 }
 
 size_t
