02efc4a4c4fdb51ad381f4d2ed90a13c43feedda
Ted Mielczarek <ted@mielczarek.org>
2015-12-01 09:53:16 -0500

    bug 1160185 - support GENERATED_FILES in EXPORTS. r=glandium
    This change allows specifying objdir-relative paths in EXPORTS to enable
    exporting entries from GENERATED_FILES. Objdir paths in EXPORTS that are
    not in GENERATED_FILES will raise an exception.
    
    Example:
    ```
    EXPORTS += ['!g.h', 'f.h']
    GENERATED_FILES += ['g.h']
    ```
    
    Given the implementation, this should also work for FINAL_TARGET_FILES,
    FINAL_TARGET_PP_FILES, and TESTING_FILES, but those are not well-tested.
    
    This patch also renames the install manifest for '_tests' to match the
    directory name for convenience in some code I refactored.

--- python/mozbuild/mozbuild/frontend/data.py
+++ python/mozbuild/mozbuild/frontend/data.py
@@ -194,21 +194,6 @@ class Defines(BaseDefines):
 class HostDefines(BaseDefines):
     pass
 
-class Exports(ContextDerived):
-    """Context derived container object for EXPORTS, which is a
-    HierarchicalStringList.
-
-    We need an object derived from ContextDerived for use in the backend, so
-    this object fills that role. It just has a reference to the underlying
-    HierarchicalStringList, which is created when parsing EXPORTS.
-    """
-    __slots__ = ('exports', 'dist_install')
-
-    def __init__(self, context, exports, dist_install=True):
-        ContextDerived.__init__(self, context)
-        self.exports = exports
-        self.dist_install = dist_install
-
 class TestHarnessFiles(ContextDerived):
     """Sandbox container object for TEST_HARNESS_FILES,
     which is a HierarchicalStringList.
@@ -830,6 +815,19 @@ class TestingFiles(FinalTargetFiles):
     @property
     def install_target(self):
         return '_tests'
+
+
+class Exports(FinalTargetFiles):
+    """Context derived container object for EXPORTS, which is a
+    HierarchicalStringList.
+
+    We need an object derived from ContextDerived for use in the backend, so
+    this object fills that role. It just has a reference to the underlying
+    HierarchicalStringList, which is created when parsing EXPORTS.
+    """
+    @property
+    def install_target(self):
+        return 'dist/include'
 
 
 class GeneratedFile(ContextDerived):
