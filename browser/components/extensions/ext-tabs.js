de2565650406da6d477a022e0de8e0891b85ef82
Kris Maglione <maglione.k@gmail.com>
2015-12-01 20:37:41 -0800

    Bug 1190688: Part 1 - [webext] Implement the activeTab permission. r=billm
    --HG--
    extra : commitid : 3fCvT7O5Oax
    extra : rebase_source : 6df43947daa552eb318bf74a3c71792889328618

--- browser/components/extensions/ext-tabs.js
+++ browser/components/extensions/ext-tabs.js
@@ -457,13 +457,8 @@ extensions.registerAPI((extension, context) => {
         }
 
         let result = [];
-        let e = Services.wm.getEnumerator("navigator:browser");
-        while (e.hasMoreElements()) {
-          let window = e.getNext();
-          if (window.document.readyState != "complete") {
-            continue;
-          }
-          let tabs = TabManager.getTabs(extension, window);
+        for (let window of WindowListManager.browserWindows()) {
+          let tabs = TabManager.for(extension).getTabs(window);
           for (let tab of tabs) {
             if (matches(window, tab)) {
               result.push(tab);
@@ -489,9 +484,15 @@ extensions.registerAPI((extension, context) => {
           // window IDs and insufficient permissions need to result in a
           // callback with |lastError| set.
           innerWindowID: tab.linkedBrowser.innerWindowID,
-
-          matchesHost: extension.whiteListedHosts.serialize(),
         };
+
+        if (TabManager.for(extension).hasActiveTabPermission(tab)) {
+          // If we have the "activeTab" permission for this tab, ignore
+          // the host whitelist.
+          options.matchesHost = ["<all_urls>"];
+        } else {
+          options.matchesHost = extension.whiteListedHosts.serialize();
+        }
 
         if (details.code) {
           options[kind + 'Code'] = details.code;
