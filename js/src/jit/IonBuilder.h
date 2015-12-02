22d622d322ed02be9381edbfd5c5f8c1f18a3cf1
Jon Coppeard <jcoppeard@mozilla.com>
2015-12-02 10:10:26 +0000

    Bug 1227567 - Optimise module namespace imports in Ion where we have type information r=shu

--- js/src/jit/IonBuilder.h
+++ js/src/jit/IonBuilder.h
@@ -432,6 +432,8 @@ class IonBuilder
     bool getPropTryConstant(bool* emitted, MDefinition* obj, jsid id, TemporaryTypeSet* types);
     bool getPropTryDefiniteSlot(bool* emitted, MDefinition* obj, PropertyName* name,
                                 BarrierKind barrier, TemporaryTypeSet* types);
+    bool getPropTryModuleNamespace(bool* emitted, MDefinition* obj, PropertyName* name,
+                                   BarrierKind barrier, TemporaryTypeSet* types);
     bool getPropTryUnboxed(bool* emitted, MDefinition* obj, PropertyName* name,
                            BarrierKind barrier, TemporaryTypeSet* types);
     bool getPropTryCommonGetter(bool* emitted, MDefinition* obj, PropertyName* name,
