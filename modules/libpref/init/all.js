b2545f9af068e5cef17389cbbf961aab76b3a7eb
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-12-03 21:07:10 +1100

    Bug 1230054: Remove unused WebMReader. r=kinetik

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -502,9 +502,6 @@ pref("media.mediasource.webm.enabled", true);
 #endif
 pref("media.mediasource.webm.audio.enabled", true);
 
-// Enable new MediaFormatReader architecture for plain webm.
-pref("media.format-reader.webm", true);
-
 #ifdef MOZ_WEBSPEECH
 pref("media.webspeech.recognition.enable", false);
 pref("media.webspeech.synth.enabled", false);
