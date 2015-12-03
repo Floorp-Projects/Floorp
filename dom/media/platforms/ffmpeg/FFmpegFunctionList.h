49fb7fdb964e834b086476c3dd3d0a4da308731f
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-12-04 02:13:12 +1100

    Bug 1232268: [ffmpeg] P2. Remove unused AVFormat headers and unused functions. r=kentuckyfriedtakahe, a=sylvestre
    Additionaly, do not attempt to resolve unused symbols.
    
    MozReview-Commit-ID: CyGFFNkJOKi

--- dom/media/platforms/ffmpeg/FFmpegFunctionList.h
+++ dom/media/platforms/ffmpeg/FFmpegFunctionList.h
@@ -3,27 +3,23 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /* libavcodec */
-AV_FUNC(avcodec_align_dimensions2, AV_FUNC_AVCODEC_ALL)
-AV_FUNC(avcodec_get_frame_defaults, (AV_FUNC_53 | AV_FUNC_54))
+AV_FUNC(avcodec_alloc_context3, AV_FUNC_AVCODEC_ALL)
 AV_FUNC(avcodec_close, AV_FUNC_AVCODEC_ALL)
 AV_FUNC(avcodec_decode_audio4, AV_FUNC_AVCODEC_ALL)
 AV_FUNC(avcodec_decode_video2, AV_FUNC_AVCODEC_ALL)
-AV_FUNC(avcodec_default_get_buffer, AV_FUNC_AVCODEC_ALL)
-AV_FUNC(avcodec_default_release_buffer, AV_FUNC_AVCODEC_ALL)
 AV_FUNC(avcodec_find_decoder, AV_FUNC_AVCODEC_ALL)
 AV_FUNC(avcodec_flush_buffers, AV_FUNC_AVCODEC_ALL)
-AV_FUNC(avcodec_alloc_context3, AV_FUNC_AVCODEC_ALL)
-AV_FUNC(avcodec_get_edge_width, AV_FUNC_AVCODEC_ALL)
 AV_FUNC(avcodec_open2, AV_FUNC_AVCODEC_ALL)
+AV_FUNC(avcodec_register_all, AV_FUNC_AVCODEC_ALL)
 AV_FUNC(av_init_packet, AV_FUNC_AVCODEC_ALL)
 AV_FUNC(av_parser_init, AV_FUNC_AVCODEC_ALL)
 AV_FUNC(av_parser_close, AV_FUNC_AVCODEC_ALL)
 AV_FUNC(av_parser_parse2, AV_FUNC_AVCODEC_ALL)
-AV_FUNC(avcodec_register_all, AV_FUNC_AVCODEC_ALL)
+#if LIBAVCODEC_VERSION_MAJOR <= 54 || defined(LIBAVCODEC_ALLVERSION)
+AV_FUNC(avcodec_get_frame_defaults, (AV_FUNC_53 | AV_FUNC_54))
+#endif
 
 /* libavutil */
-AV_FUNC(av_image_fill_linesizes, AV_FUNC_AVUTIL_ALL)
-AV_FUNC(av_image_fill_pointers, AV_FUNC_AVUTIL_ALL)
 AV_FUNC(av_log_set_level, AV_FUNC_AVUTIL_ALL)
 AV_FUNC(av_malloc, AV_FUNC_AVUTIL_ALL)
 AV_FUNC(av_freep, AV_FUNC_AVUTIL_ALL)
