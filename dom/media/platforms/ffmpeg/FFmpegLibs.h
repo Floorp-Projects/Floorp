fc43f42cdc8b0aef0239b43bc6331cecaa5ba2e0
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-12-04 02:13:12 +1100

    Bug 1232268: [ffmpeg] P2. Remove unused AVFormat headers and unused functions. r=kentuckyfriedtakahe, a=sylvestre
    Additionaly, do not attempt to resolve unused symbols.
    
    MozReview-Commit-ID: CyGFFNkJOKi

--- dom/media/platforms/ffmpeg/FFmpegLibs.h
+++ dom/media/platforms/ffmpeg/FFmpegLibs.h
@@ -9,9 +9,9 @@
 
 extern "C" {
 #pragma GCC visibility push(default)
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavutil/imgutils.h>
+#include "libavcodec/avcodec.h"
+#include "libavutil/avutil.h"
+#include "libavutil/mem.h"
 #pragma GCC visibility pop
 }
 
@@ -25,7 +25,7 @@ extern "C" {
 typedef CodecID AVCodecID;
 #endif
 
-enum { LIBAV_VER = LIBAVFORMAT_VERSION_MAJOR };
+enum { LIBAV_VER = LIBAVCODEC_VERSION_MAJOR };
 
 namespace mozilla {
 
