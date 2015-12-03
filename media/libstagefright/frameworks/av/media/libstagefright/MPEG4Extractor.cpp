cd23dd4dfa354c6064bd9854127d4c6bac0ad543
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-12-03 14:58:55 -0800

    Bug 1206211: P1. Ensure operation can't overflow. r=kentuckyfriedtakahe a=lizzard
    --HG--
    extra : commitid : 1wYfRVSfTL
    extra : rebase_source : eeaaad7cb92990a4ffcc861ea8eee5513ec2a128
    extra : histedit_source : 154092fb854544b38f451b71647b9c4d495b5c01

--- media/libstagefright/frameworks/av/media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/frameworks/av/media/libstagefright/MPEG4Extractor.cpp
@@ -507,9 +507,12 @@ status_t MPEG4Extractor::readMetaData() {
     CHECK_NE(err, (status_t)NO_INIT);
 
     // copy pssh data into file metadata
-    int psshsize = 0;
+    uint64_t psshsize = 0;
     for (size_t i = 0; i < mPssh.Length(); i++) {
         psshsize += 20 + mPssh[i].datalen;
+    }
+    if (psshsize > kMAX_ALLOCATION) {
+        return ERROR_MALFORMED;
     }
     if (psshsize) {
         char *buf = (char*)malloc(psshsize);
