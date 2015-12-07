7ecb975c7f3bbce986d5f48116e5d203170b85f5
L. David Baron <dbaron@dbaron.org>
2015-12-07 18:39:41 -0500

    Bug 1230701 - Make will-change:position create a containing block for absolutely-positioned elements.  r=dholbert

--- layout/style/nsCSSProps.h
+++ layout/style/nsCSSProps.h
@@ -262,6 +262,15 @@ static_assert((CSS_PROPERTY_PARSE_PROPERTY_MASK &
 // nsStyleDisplay::IsFixedPosContainingBlock.
 #define CSS_PROPERTY_FIXPOS_CB                    (1<<29)
 
+// This property has values that can establish a containing block for
+// absolutely positioned elements.
+// This should be set for any properties that can cause an element to be
+// such a containing block, as implemented in
+// nsStyleDisplay::IsAbsPosContainingBlock.
+// It does not need to be set for properties that also have
+// CSS_PROPERTY_FIXPOS_CB set.
+#define CSS_PROPERTY_ABSPOS_CB                    (1<<30)
+
 /**
  * Types of animatable values.
  */
