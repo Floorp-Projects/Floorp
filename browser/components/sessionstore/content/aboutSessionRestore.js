d4ef772f823240baee72c2ca8f2c0752a083f2e6
Luis Miguel [:quicksaver] <quicksaver@gmail.com>
2015-12-08 12:35:28 +0000

    Bug 1228980 - Display favicons of tab entries in aboutSessionRestore-based pages. r=ttaubert

--- browser/components/sessionstore/content/aboutSessionRestore.js
+++ browser/components/sessionstore/content/aboutSessionRestore.js
@@ -77,7 +77,7 @@ function initTreeView() {
     };
     winState.tabs = aWinData.tabs.map(function(aTabData) {
       var entry = aTabData.entries[aTabData.index - 1] || { url: "about:blank" };
-      var iconURL = aTabData.attributes && aTabData.attributes.image || null;
+      var iconURL = aTabData.image || null;
       // don't initiate a connection just to fetch a favicon (see bug 462863)
       if (/^https?:/.test(iconURL))
         iconURL = "moz-anno:favicon:" + iconURL;
