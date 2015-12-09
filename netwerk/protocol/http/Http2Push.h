c63f74e5599dd636eb64de6c7b5600f4c2e08be8
Nicholas Hurley <hurley@todesschaf.org>
2015-12-09 10:11:04 -0800

    Bug 1228822 - Ensure we send out the window update when matching a pushed stream with a pull stream. r=mcmanus

--- netwerk/protocol/http/Http2Push.h
+++ netwerk/protocol/http/Http2Push.h
@@ -46,6 +46,7 @@ public:
   // override of Http2Stream
   nsresult ReadSegments(nsAHttpSegmentReader *,  uint32_t, uint32_t *) override;
   nsresult WriteSegments(nsAHttpSegmentWriter *, uint32_t, uint32_t *) override;
+  void AdjustInitialWindow() override;
 
   nsISchedulingContext *SchedulingContext() override { return mSchedulingContext; };
   void ConnectPushedStream(Http2Stream *consumer);
