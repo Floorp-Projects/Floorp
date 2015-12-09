467f2c9a3a36c5668d96ab620291b571790245c2
Shu-yu Guo <shu@rfrn.org>
2015-12-09 07:52:58 -0800

    Bug 1071646 - Make functions block-scoped in JS and implement Annex B semantics for compatibility. (r=jorendorff)

---
+++ js/src/tests/ecma_6/LexicalEnvironment/block-scoped-functions-annex-b-if.js
@@ -0,0 +1,33 @@
+var log = "";
+
+function f(x) {
+  if (x)
+    function g() { return "g0"; }
+  else
+    function g() { return "g1"; }
+
+  log += g();
+
+  if (x)
+    function g() { return "g2"; }
+  else {
+  }
+
+  log += g();
+
+  if (x) {
+  } else
+    function g() { return "g3"; }
+
+  log += g();
+
+  if (x)
+    function g() { return "g4"; }
+
+  log += g();
+}
+
+f(true);
+f(false);
+
+reportCompare(log, "g0g2g2g4g1g1g3g3");
