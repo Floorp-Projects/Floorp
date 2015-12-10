ee882dcb66fc192f50533109e83899e6661046a5
Sebastian Kaspari <s.kaspari@gmail.com>
2015-12-10 11:01:59 -0500

    Bug 1229153 - GeckoApp: Unregister from Snackbar events in onDestroy(). r=jchen a=ritu

--- mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
@@ -2155,6 +2155,7 @@ public abstract class GeckoApp
             "PrivateBrowsing:Data",
             "Session:StatePurged",
             "Share:Text",
+            "Snackbar:Show",
             "SystemUI:Visibility",
             "ToggleChrome:Focus",
             "ToggleChrome:Hide",
