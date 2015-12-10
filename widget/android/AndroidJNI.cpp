ef12ba95b059130d13e7e8568aa994d7acc35da5
Kartikaya Gupta <kgupta@mozilla.com>
2015-12-10 17:23:07 -0500

    Bug 1230522 - Make some unimplemented functions not be native. r=rbarker

--- widget/android/AndroidJNI.cpp
+++ widget/android/AndroidJNI.cpp
@@ -403,20 +403,6 @@ Java_org_mozilla_gecko_gfx_NativePanZoomController_getRedrawHint(JNIEnv* env, jo
 }
 
 NS_EXPORT void JNICALL
-Java_org_mozilla_gecko_gfx_NativePanZoomController_setOverScrollMode(JNIEnv* env, jobject instance, jint overscrollMode)
-{
-    // FIXME implement this
-}
-
-NS_EXPORT jint JNICALL
-Java_org_mozilla_gecko_gfx_NativePanZoomController_getOverScrollMode(JNIEnv* env, jobject instance)
-{
-    // FIXME implement this
-    return 0;
-}
-
-
-NS_EXPORT void JNICALL
 Java_org_mozilla_gecko_gfx_NativePanZoomController_setIsLongpressEnabled(JNIEnv* env, jobject instance, jboolean isLongpressEnabled)
 {
     APZCTreeManager::SetLongTapEnabled(isLongpressEnabled);
