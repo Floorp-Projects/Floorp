a38324015d46803cf4c45eee8056d69d788794df
Blake Kaplan <mrbkap@gmail.com>
2015-12-10 15:31:17 -0500

    Bug 1225829 - Use a temporary string to avoid string type confusion via references. r=jimm

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -7509,7 +7509,9 @@ nsContentUtils::TransferableToIPCTransferable(nsITransferable* aTransferable,
               if (IsFileImage(file, type)) {
                 IPCDataTransferItem* item = aIPCDataTransfer->items().AppendElement();
                 item->flavor() = type;
-                SlurpFileToString(file, item->data());
+                nsAutoCString data;
+                SlurpFileToString(file, data);
+                item->data() = data;
               }
 
               continue;
