f2afd327477ac67a529d6e3d58ac2766c0d9024d
J. Ryan Stinnett <jryans@gmail.com>
2015-12-11 12:17:58 -0500

    Bug 1233888 - Convert to Promise.jsm in app projects. r=ochameau

--- devtools/client/webide/modules/app-projects.js
+++ devtools/client/webide/modules/app-projects.js
@@ -4,7 +4,7 @@
 
 const {Cc,Ci,Cu,Cr} = require("chrome");
 const ObservableObject = require("devtools/client/shared/observable-object");
-const promise = require("devtools/shared/deprecated-sync-thenables");
+const promise = require("promise");
 
 const {EventEmitter} = Cu.import("resource://devtools/shared/event-emitter.js", {});
 const {generateUUID} = Cc['@mozilla.org/uuid-generator;1'].getService(Ci.nsIUUIDGenerator);
