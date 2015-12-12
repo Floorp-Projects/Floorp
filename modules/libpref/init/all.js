8e2e83d0f5bc00b3552230df6c82c8b9eecb597f
Phil Ringnalda <philringnalda@gmail.com>
2015-12-12 13:05:26 -0800

    Back out 908f57c5f6af (bug 1231485) for mochitest failures in test_inherit_computation.html, test_transitions_per_property.html, etc.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2428,7 +2428,7 @@ pref("layout.css.control-characters.visible", true);
 #endif
 
 // Is support for text-emphasis enabled?
-pref("layout.css.text-emphasis.enabled", true);
+pref("layout.css.text-emphasis.enabled", false);
 
 // pref for which side vertical scrollbars should be on
 // 0 = end-side in UI direction
