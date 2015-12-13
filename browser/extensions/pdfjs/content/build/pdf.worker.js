9fefd487d5c01d504465a5cf61841ddb39847cf5
Ryan VanderMeulen <ryanvm@gmail.com>
2015-12-13 11:15:26 -0500

    Bug 1232196 - Update pdf.js to version 1.3.76. r=bdahl

--- browser/extensions/pdfjs/content/build/pdf.worker.js
+++ browser/extensions/pdfjs/content/build/pdf.worker.js
@@ -20,8 +20,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.3.72';
-PDFJS.build = '4d6f3c8';
+PDFJS.version = '1.3.76';
+PDFJS.build = 'f7ec866';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
