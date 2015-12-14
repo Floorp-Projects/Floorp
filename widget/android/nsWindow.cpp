fff2d43c6bcb12d9d6469c92e49eb438389114c4
Mark Capella <markcapella@twcny.rr.com>
2015-12-14 18:53:26 -0500

    Bug 1230617 - Editing text in an input field causes actionbar jank, r=snorp

--- widget/android/nsWindow.cpp
+++ widget/android/nsWindow.cpp
@@ -2363,7 +2363,6 @@ nsWindow::Natives::OnImeReplaceText(int32_t aStart, int32_t aEnd,
             event.mOffset = uint32_t(aStart);
             event.mLength = uint32_t(aEnd - aStart);
             event.mExpandToClusterBoundary = false;
-            event.mReason = nsISelectionListener::IME_REASON;
             window.DispatchEvent(&event);
         }
 
@@ -2492,7 +2491,6 @@ nsWindow::Natives::OnImeUpdateComposition(int32_t aStart, int32_t aEnd)
         selEvent.mLength = std::max(aStart, aEnd) - selEvent.mOffset;
         selEvent.mReversed = aStart > aEnd;
         selEvent.mExpandToClusterBoundary = false;
-        selEvent.mReason = nsISelectionListener::IME_REASON;
 
         window.DispatchEvent(&selEvent);
         return;
