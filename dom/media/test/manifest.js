005c16d1978eefcedba37e68e13cb38be8873ef9
Louis Christie <lchristie@mozilla.com>
2015-12-14 15:30:57 +1300

    Bug 864780 - Changed the format chunk reader to skip any extention. r=cpearce

--- dom/media/test/manifest.js
+++ dom/media/test/manifest.js
@@ -154,6 +154,8 @@ var gPlayTests = [
   { name:"r11025_s16_c1_trailing.wav", type:"audio/x-wav", duration:1.0 },
   // file with list chunk
   { name:"r16000_u8_c1_list.wav", type:"audio/x-wav", duration:4.2 },
+  // file with 2 extra bytes of metadata
+  { name:"16bit_wave_extrametadata.wav" type:"audio/x-wav", duration:1.0 },
   // 24-bit samples
   { name:"wavedata_s24.wav", type:"audio/x-wav", duration:1.0 },
 
