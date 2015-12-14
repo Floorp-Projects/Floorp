4436ea9ce09b0eef7cf4b71e5dfce065792a473b
Shu-yu Guo <shu@rfrn.org>
2015-12-14 14:48:17 -0800

    No bug - Bump XDR version after backout of bug 1231758. (r=me)

--- js/src/vm/Xdr.h
+++ js/src/vm/Xdr.h
@@ -29,7 +29,7 @@ namespace js {
  *
  *  https://developer.mozilla.org/en-US/docs/SpiderMonkey/Internals/Bytecode
  */
-static const uint32_t XDR_BYTECODE_VERSION_SUBTRAHEND = 329;
+static const uint32_t XDR_BYTECODE_VERSION_SUBTRAHEND = 330;
 static const uint32_t XDR_BYTECODE_VERSION =
     uint32_t(0xb973c0de - XDR_BYTECODE_VERSION_SUBTRAHEND);
 
