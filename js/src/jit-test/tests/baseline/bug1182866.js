bd64ebea9820882836dfd97635edba97cbb68d71
Shu-yu Guo <shu@rfrn.org>
2015-12-15 14:31:34 -0800

    Bug 1182866 - Fix Baseline GETNAME stubs to check for uninitialized lexicals. (r=jandem)

---
+++ js/src/jit-test/tests/baseline/bug1182866.js
@@ -0,0 +1,17 @@
+// |jit-test| error: ReferenceError
+
+with(7) {
+    function f() {
+        if (i == 15) {
+            g();
+        }
+        const x = 42;
+        function g() {
+            return x;
+        }
+        return g;
+    }
+}
+for (var i = 0; i < 99; i++) {
+    assertEq(f()(), 42);
+}
