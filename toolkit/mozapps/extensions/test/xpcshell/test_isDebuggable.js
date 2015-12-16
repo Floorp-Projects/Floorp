1944acb4ef9ef2fe9054c8a79ba49af22cbe4917
Carsten "Tomcat" Book <cbook@mozilla.com>
2015-12-16 14:44:20 +0100

    Backed out changeset 7a5367ac58a4 (bug 1209344) for various test failures
    G: changed toolkit/mozapps/extensions/test/xpcshell/xpcshell-shared.ini

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_isDebuggable.js
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+var ADDONS = [
+  "test_bootstrap2_1", // restartless addon
+  "test_bootstrap1_4", // old-school addon
+  "test_jetpack"       // sdk addon
+];
+
+var IDS = [
+  "bootstrap1@tests.mozilla.org",
+  "bootstrap2@tests.mozilla.org",
+  "jetpack@tests.mozilla.org"
+];
+
+function run_test() {
+  do_test_pending();
+  
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "2", "2");
+
+  startupManager();
+  AddonManager.checkCompatibility = false;
+
+  installAllFiles(ADDONS.map(do_get_addon), function () {
+    restartManager();
+
+    AddonManager.getAddonsByIDs(IDS, function([a1, a2, a3]) {
+      do_check_eq(a1.isDebuggable, false);
+      do_check_eq(a2.isDebuggable, true);
+      do_check_eq(a3.isDebuggable, true);
+      do_test_finished();
+    });
+  }, true);
+}
