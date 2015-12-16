70e9ae8f315f91a10b598f4b735990ac92efdd11
Avi Halachmi <avihpit@yahoo.com>
2015-12-16 20:06:40 +0200

    Bug 1228147: part 2: Add telemetry probe for synchronous scroll. r=kats a=ritu

--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -441,8 +441,11 @@ private:
     #ifndef ANDROID  /* bug 1142079 */
       if (XRE_IsParentProcess()) {
         TimeDuration vsyncLatency = TimeStamp::Now() - aVsyncTimestamp;
+        uint32_t sample = (uint32_t)vsyncLatency.ToMilliseconds();
         Telemetry::Accumulate(Telemetry::FX_REFRESH_DRIVER_CHROME_FRAME_DELAY_MS,
-                              vsyncLatency.ToMilliseconds());
+                              sample);
+        Telemetry::Accumulate(Telemetry::FX_REFRESH_DRIVER_SYNC_SCROLL_FRAME_DELAY_MS,
+                              sample);
       } else if (mVsyncRate != TimeDuration::Forever()) {
         TimeDuration contentDelay = (TimeStamp::Now() - mLastChildTick) - mVsyncRate;
         if (contentDelay.ToMilliseconds() < 0 ){
@@ -450,8 +453,11 @@ private:
           // the reported hardware rate. In those cases, consider that we have 0 delay.
           contentDelay = TimeDuration::FromMilliseconds(0);
         }
+        uint32_t sample = (uint32_t)contentDelay.ToMilliseconds();
         Telemetry::Accumulate(Telemetry::FX_REFRESH_DRIVER_CONTENT_FRAME_DELAY_MS,
-                              contentDelay.ToMilliseconds());
+                              sample);
+        Telemetry::Accumulate(Telemetry::FX_REFRESH_DRIVER_SYNC_SCROLL_FRAME_DELAY_MS,
+                              sample);
       } else {
         // Request the vsync rate from the parent process. Might be a few vsyncs
         // until the parent responds.
