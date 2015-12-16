754e639fe53e2317baba5262d2685fa83f65b817
Tooru Fujisawa <arai_a@mac.com>
2015-12-17 01:01:16 +0900

    Bug 1229057 - Part 1 followup: Move addToStackPtr and dependent things to -inl.h. r=nbp

--- js/src/jit/BaselineCompiler.cpp
+++ js/src/jit/BaselineCompiler.cpp
@@ -26,6 +26,7 @@
 
 #include "jsscriptinlines.h"
 
+#include "jit/BaselineFrameInfo-inl.h"
 #include "jit/MacroAssembler-inl.h"
 #include "vm/Interpreter-inl.h"
 #include "vm/NativeObject-inl.h"
--- js/src/jit/MacroAssembler-inl.h
+++ js/src/jit/MacroAssembler-inl.h
@@ -341,6 +341,22 @@ MacroAssembler::branchFunctionKind(Condition cond, JSFunction::FunctionKind kind
     branch32(cond, scratch, Imm32(bit), label);
 }
 
+#ifndef JS_CODEGEN_ARM64
+
+template <typename T> void
+MacroAssembler::addToStackPtr(T t)
+{
+    addPtr(t, getStackPointer());
+}
+
+template <typename T> void
+MacroAssembler::addStackPtrTo(T t)
+{
+    addPtr(getStackPointer(), t);
+}
+
+#endif // !JS_CODEGEN_ARM64
+
 } // namespace jit
 } // namespace js
 
