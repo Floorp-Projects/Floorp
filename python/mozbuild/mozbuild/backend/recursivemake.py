dc1355e42ff3f48ce23df29685782e39fedf648a
Mike Hommey <mh+mozilla@glandium.org>
2015-12-18 17:17:33 +0900

    Bug 1233282 - Add generated FinalTargetFiles as optional_exists in install manifests. r=gps
    Otherwise there's a possibility of race condition in some cases.

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -1304,10 +1304,12 @@ INSTALL_TARGETS += %(prefix)s
                           if path else target).replace('/', '_')
             have_objdir_files = False
             for f in files:
+                dest = mozpath.join(reltarget, path,
+                                    mozpath.basename(f.full_path))
                 if not isinstance(f, ObjDirPath):
-                    dest = mozpath.join(reltarget, path, mozpath.basename(f))
                     install_manifest.add_symlink(f.full_path, dest)
                 else:
+                    install_manifest.add_optional_exists(dest)
                     backend_file.write('%s_FILES += %s\n' % (
                         target_var, self._pretty_path(f, backend_file)))
                     have_objdir_files = True
