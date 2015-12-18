498af0c11863592e2d4df52f37fb21feebfbffb0
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2015-12-18 15:23:36 -0800

    Bug 1233760 - Revert test_spdy.js to use AsyncOpen2 (r=mcmanus)

--- netwerk/test/unit/test_spdy.js
+++ netwerk/test/unit/test_spdy.js
@@ -1,6 +1,6 @@
 // test spdy/3.1
 
-Cu.import("resource://gre/modules/NetUtil.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
 
 // Generate a small and a large post with known pre-calculated md5 sums
 function generateContent(size) {
@@ -184,15 +184,24 @@ SpdyPostListener.prototype.onDataAvailable = function(request, ctx, stream, off,
 };
 
 function makeChan(url) {
-  return NetUtil.newChannel({uri: url, loadUsingSystemPrincipal: true})
-                .QueryInterface(Ci.nsIHttpChannel);
+  var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+  var chan = ios.newChannel2(url,
+                             null,
+                             null,
+                             null,      // aLoadingNode
+                             Services.scriptSecurityManager.getSystemPrincipal(),
+                             null,      // aTriggeringPrincipal
+                             Ci.nsILoadInfo.SEC_NORMAL,
+                             Ci.nsIContentPolicy.TYPE_OTHER).QueryInterface(Ci.nsIHttpChannel);
+
+  return chan;
 }
 
 // Make sure we make a spdy connection and both us and the server mark it as such
 function test_spdy_basic() {
   var chan = makeChan("https://localhost:" + serverPort + "/");
   var listener = new SpdyCheckListener();
-  chan.asyncOpen2(listener);
+  chan.asyncOpen(listener, null);
 }
 
 // Support for making sure XHR works over SPDY
@@ -240,7 +249,7 @@ function test_spdy_concurrent() {
   for (var i = 0; i < concurrent_listener.target; i++) {
     concurrent_channels[i] = makeChan("https://localhost:" + serverPort + "/750ms");
     concurrent_channels[i].loadFlags = Ci.nsIRequest.LOAD_BYPASS_CACHE;
-    concurrent_channels[i].asyncOpen2(concurrent_listener);
+    concurrent_channels[i].asyncOpen(concurrent_listener, null);
   }
 }
 
@@ -250,8 +259,8 @@ function test_spdy_multiplex() {
   var chan2 = makeChan("https://localhost:" + serverPort + "/multiplex2");
   var listener1 = new SpdyMultiplexListener();
   var listener2 = new SpdyMultiplexListener();
-  chan1.asyncOpen2(listener1);
-  chan2.asyncOpen2(listener2);
+  chan1.asyncOpen(listener1, null);
+  chan2.asyncOpen(listener2, null);
 }
 
 // Test to make sure we gateway non-standard headers properly
@@ -260,42 +269,42 @@ function test_spdy_header() {
   var hvalue = "Headers are fun";
   var listener = new SpdyHeaderListener(hvalue);
   chan.setRequestHeader("X-Test-Header", hvalue, false);
-  chan.asyncOpen2(listener);
+  chan.asyncOpen(listener, null);
 }
 
 function test_spdy_push1() {
   var chan = makeChan("https://localhost:" + serverPort + "/push");
   chan.loadGroup = loadGroup;
   var listener = new SpdyPushListener();
-  chan.asyncOpen2(listener);
+  chan.asyncOpen(listener, chan);
 }
 
 function test_spdy_push2() {
   var chan = makeChan("https://localhost:" + serverPort + "/push.js");
   chan.loadGroup = loadGroup;
   var listener = new SpdyPushListener();
-  chan.asyncOpen2(listener);
+  chan.asyncOpen(listener, chan);
 }
 
 function test_spdy_push3() {
   var chan = makeChan("https://localhost:" + serverPort + "/push2");
   chan.loadGroup = loadGroup;
   var listener = new SpdyPushListener();
-  chan.asyncOpen2(listener);
+  chan.asyncOpen(listener, chan);
 }
 
 function test_spdy_push4() {
   var chan = makeChan("https://localhost:" + serverPort + "/push2.js");
   chan.loadGroup = loadGroup;
   var listener = new SpdyPushListener();
-  chan.asyncOpen2(listener);
+  chan.asyncOpen(listener, chan);
 }
 
 // Make sure we handle GETs that cover more than 2 frames properly
 function test_spdy_big() {
   var chan = makeChan("https://localhost:" + serverPort + "/big");
   var listener = new SpdyBigListener();
-  chan.asyncOpen2(listener);
+  chan.asyncOpen(listener, null);
 }
 
 // Support for doing a POST
@@ -309,7 +318,7 @@ function do_post(content, chan, listener) {
 
   chan.requestMethod = "POST";
 
-  chan.asyncOpen2(listener);
+  chan.asyncOpen(listener, null);
 }
 
 // Make sure we can do a simple POST
