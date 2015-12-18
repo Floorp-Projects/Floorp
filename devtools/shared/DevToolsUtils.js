e68e1f18e60564aba2c4de11e8e1a46848d05e47
J. Ryan Stinnett <jryans@gmail.com>
2015-12-18 17:02:00 -0600

    Bug 1233865 - Remove deprecated dbg_assert. r=fitzgen

--- devtools/shared/DevToolsUtils.js
+++ devtools/shared/DevToolsUtils.js
@@ -461,24 +461,6 @@ exports.defineLazyGetter = function defineLazyGetter(aObject, aName, aLambda) {
   });
 };
 
-// DEPRECATED: use DevToolsUtils.assert(condition, message) instead!
-let haveLoggedDeprecationMessage = false;
-exports.dbg_assert = function dbg_assert(cond, e) {
-  if (!haveLoggedDeprecationMessage) {
-    haveLoggedDeprecationMessage = true;
-    const deprecationMessage = "DevToolsUtils.dbg_assert is deprecated! Use DevToolsUtils.assert instead!\n"
-          + Error().stack;
-    dump(deprecationMessage);
-    if (typeof console === "object" && console && console.warn) {
-      console.warn(deprecationMessage);
-    }
-  }
-
-  if (!cond) {
-    return e;
-  }
-};
-
 exports.defineLazyGetter(this, "AppConstants", () => {
   if (isWorker) {
     return {};
