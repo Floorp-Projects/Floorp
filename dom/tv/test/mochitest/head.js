d3e51f82f668cae6b01594473fbfa6530bd3e751
Sean Lin <selin@mozilla.com>
2015-12-18 16:33:16 +0800

    Bug 1263799 - Support IPC for TV service between B2G and content processes. Part 3 - Enhance TV mochitests. r=schien
    MozReview-Commit-ID: IsCVCxtSzEn

--- dom/tv/test/mochitest/head.js
+++ dom/tv/test/mochitest/head.js
@@ -1,40 +1,45 @@
 "use strict";
 
-function setupPrefsAndPermissions(callback) {
+function setupPrefsAndPermissions(aCallback) {
   setupPrefs(function() {
       SpecialPowers.pushPermissions([
         {"type":"tv", "allow":1, "context":document}
-      ], callback);
+      ], aCallback);
   });
 }
 
-function setupPrefs(callback) {
-  let xhr = new XMLHttpRequest;
-  let data;
-
-  xhr.open("GET", "./mock_data.json", false);
-  xhr.send(null);
-  if (xhr.status == 200) {
-    data = xhr.responseText;
-  }
-
+function setupPrefs(aCallback) {
   SpecialPowers.pushPrefEnv({"set": [
                               ["dom.tv.enabled", true],
                               ["dom.ignore_webidl_scope_checks", true],
-                              ["dom.testing.tv_mock_data", data]
                             ]}, function() {
-    callback();
+    aCallback();
   });
 }
 
-function removePrefsAndPermissions(callback) {
+function removePrefsAndPermissions(aCallback) {
   SpecialPowers.popPrefEnv(function() {
-    SpecialPowers.popPermissions(callback);
+    SpecialPowers.popPermissions(aCallback);
   });
 }
 
-function prepareTest(callback) {
+function prepareTest(aCallback) {
   removePrefsAndPermissions(function() {
-    setupPrefsAndPermissions(callback);
+    setupPrefsAndPermissions(function() {
+      initMockedData(aCallback);
+    });
   });
+}
+
+function initMockedData(aCallback) {
+  let xhr = new XMLHttpRequest;
+  xhr.open("GET", "./mock_data.json", false);
+  xhr.send(null);
+  if (xhr.status == 200) {
+    var gScript = SpecialPowers.loadChromeScript(SimpleTest.getTestFileURL('tv_chrome_script.js'));
+
+    gScript.addMessageListener('init-mocked-data-complete', aCallback);
+
+    gScript.sendAsyncMessage('init-mocked-data', xhr.responseText);
+  }
 }
