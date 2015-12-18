909db7e64a1b5922469f27f39ebea738e1839bf2
Wes Kocher <wkocher@mozilla.com>
2015-12-17 16:38:49 -0800

    Backed out changeset 0b616948a0f3 (bug 1214261) for 379461-3-container-xhtml.html failures CLOSED TREE

--- layout/generic/nsGfxScrollFrame.h
+++ layout/generic/nsGfxScrollFrame.h
@@ -78,6 +78,7 @@ public:
   void AppendScrollPartsTo(nsDisplayListBuilder*   aBuilder,
                            const nsRect&           aDirtyRect,
                            const nsDisplayListSet& aLists,
+                           bool                    aUsingDisplayPort,
                            bool                    aCreateLayer,
                            bool                    aPositioned);
 
