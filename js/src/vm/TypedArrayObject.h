80cad7cc552fa0686177a84806aab612f03aa0de
Tooru Fujisawa <arai_a@mac.com>
2015-12-20 19:14:44 +0900

    Bug 1165053 - Part 2: Implement %TypedArray%[@@species] getter and ArrayBuffer[@@species] getter. r=evilpie

--- js/src/vm/TypedArrayObject.h
+++ js/src/vm/TypedArrayObject.h
@@ -256,6 +256,7 @@ class TypedArrayObject : public NativeObject
     static const JSFunctionSpec protoFunctions[];
     static const JSPropertySpec protoAccessors[];
     static const JSFunctionSpec staticFunctions[];
+    static const JSPropertySpec staticProperties[];
 
     /* Accessors and functions */
 
