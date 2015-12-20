3078067bcb40372e460d6301fbadcf79bb510932
Tooru Fujisawa <arai_a@mac.com>
2015-12-20 19:14:44 +0900

    Bug 1165053 - Part 2: Implement %TypedArray%[@@species] getter and ArrayBuffer[@@species] getter. r=evilpie

--- js/src/vm/TypedArrayObject.cpp
+++ js/src/vm/TypedArrayObject.cpp
@@ -861,6 +861,12 @@ TypedArrayObject::staticFunctions[] = {
     JS_FS_END
 };
 
+/* static */ const JSPropertySpec
+TypedArrayObject::staticProperties[] = {
+    JS_SELF_HOSTED_SYM_GET(species, "TypedArraySpecies", 0),
+    JS_PS_END
+};
+
 /* static */ const Class
 TypedArrayObject::sharedTypedArrayPrototypeClass = {
     // Actually ({}).toString.call(%TypedArray%.prototype) should throw,
@@ -887,7 +893,7 @@ TypedArrayObject::sharedTypedArrayPrototypeClass = {
         GenericCreateConstructor<TypedArrayConstructor, 3, gc::AllocKind::FUNCTION>,
         GenericCreatePrototype,
         TypedArrayObject::staticFunctions,
-        nullptr,
+        TypedArrayObject::staticProperties,
         TypedArrayObject::protoFunctions,
         TypedArrayObject::protoAccessors,
         nullptr,
