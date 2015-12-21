7a04ea65a1e8d7e8eca9abdb97b9ca1aa9f7d741
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2015-12-21 13:06:46 -0800

    Bug 1233843 - Convert 30 tests within netwerk/test to use AsyncOpen2 (r=mcmanus)

--- netwerk/test/unit/test_bug369787.js
+++ netwerk/test/unit/test_bug369787.js
@@ -1,5 +1,5 @@
 Cu.import("resource://testing-common/httpd.js");
-Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/NetUtil.jsm");
 
 const BUGID = "369787";
 var server = null;
@@ -55,21 +55,12 @@ function run_test() {
   server.start(-1);
 
   // make request
-  channel =
-      Components.classes["@mozilla.org/network/io-service;1"].
-      getService(Components.interfaces.nsIIOService).
-      newChannel2("http://localhost:" +
-                  server.identity.primaryPort + "/bug" + BUGID,
-                  null,
-                  null,
-                  null,      // aLoadingNode
-                  Services.scriptSecurityManager.getSystemPrincipal(),
-                  null,      // aTriggeringPrincipal
-                  Ci.nsILoadInfo.SEC_NORMAL,
-                  Ci.nsIContentPolicy.TYPE_OTHER);
-
+  channel = NetUtil.newChannel({
+    uri: "http://localhost:" + server.identity.primaryPort + "/bug" + BUGID,
+    loadUsingSystemPrincipal: true
+  });
   channel.QueryInterface(Components.interfaces.nsIHttpChannel);
-  channel.asyncOpen(new TestListener(), null);
+  channel.asyncOpen2(new TestListener());
 
   do_test_pending();
 }
