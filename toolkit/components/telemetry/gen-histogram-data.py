af825059bad7b74d60874db34f8f06a1868a0377
Anup Kumar <allamsetty.anup@gmail.com>
2015-12-21 08:31:00 +0100

    Bug 1201492 - Remove extended_statistics_ok from Telemetry histograms. r=dexter

--- toolkit/components/telemetry/gen-histogram-data.py
+++ toolkit/components/telemetry/gen-histogram-data.py
@@ -60,11 +60,10 @@ def print_array_entry(output, histogram, name_index, exp_index):
     cpp_guard = histogram.cpp_guard()
     if cpp_guard:
         print("#if defined(%s)" % cpp_guard, file=output)
-    print("  { %s, %s, %s, %s, %d, %d, %s, %s, %s }," \
+    print("  { %s, %s, %s, %s, %d, %d, %s, %s }," \
         % (histogram.low(), histogram.high(),
            histogram.n_buckets(), histogram.nsITelemetry_kind(),
            name_index, exp_index, histogram.dataset(),
-           "true" if histogram.extended_statistics_ok() else "false",
            "true" if histogram.keyed() else "false"), file=output)
     if cpp_guard:
         print("#endif", file=output)
