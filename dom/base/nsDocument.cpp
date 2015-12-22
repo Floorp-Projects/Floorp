86cc5b91747d5d4624d67c0ea76c1016d49511ee
Brian Birtles <birtles@gmail.com>
2015-12-22 10:08:19 +0900

    Bug 1232829 - Detach obsolete DocumentTimeline from refresh driver when the document is reset; r=smaug a=sylvestre
    --HG--
    extra : commitid : 9ZwE23a2Wwc

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -2114,7 +2114,15 @@ nsDocument::Reset(nsIChannel* aChannel, nsILoadGroup* aLoadGroup)
   // Note that, since mTiming does not change during a reset, the
   // navigationStart time remains unchanged and therefore any future new
   // timeline will have the same global clock time as the old one.
-  mDocumentTimeline = nullptr;
+  if (mDocumentTimeline) {
+    nsRefreshDriver* rd = mPresShell && mPresShell->GetPresContext() ?
+                          mPresShell->GetPresContext()->RefreshDriver() :
+                          nullptr;
+    if (rd) {
+      mDocumentTimeline->NotifyRefreshDriverDestroying(rd);
+    }
+    mDocumentTimeline = nullptr;
+  }
 
   nsCOMPtr<nsIPropertyBag2> bag = do_QueryInterface(aChannel);
   if (bag) {
