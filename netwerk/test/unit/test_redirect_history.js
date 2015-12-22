6c003e830eaa982c2bb8baa4f616984c0d5f016e
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2015-12-21 16:29:26 -0800

    Bug 1234369 - Convert 25 tests within netwerk/test to use AsyncOpen2 (r=mcmanus)

--- netwerk/test/unit/test_redirect_history.js
+++ netwerk/test/unit/test_redirect_history.js
@@ -1,5 +1,5 @@
 Cu.import("resource://testing-common/httpd.js");
-Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/NetUtil.jsm");
 
 XPCOMUtils.defineLazyGetter(this, "URL", function() {
   return "http://localhost:" + httpServer.identity.primaryPort;
@@ -11,16 +11,7 @@ var redirects = [];
 const numRedirects = 10;
 
 function make_channel(url, callback, ctx) {
-  var ios = Cc["@mozilla.org/network/io-service;1"].
-            getService(Ci.nsIIOService);
-  return ios.newChannel2(url,
-                         "",
-                         null,
-                         null,      // aLoadingNode
-                         Services.scriptSecurityManager.getSystemPrincipal(),
-                         null,      // aTriggeringPrincipal
-                         Ci.nsILoadInfo.SEC_NORMAL,
-                         Ci.nsIContentPolicy.TYPE_OTHER);
+  return NetUtil.newChannel({uri: url, loadUsingSystemPrincipal: true});
 }
 
 const responseBody = "response body";
@@ -68,6 +59,6 @@ function run_test()
   httpServer.start(-1);
 
   var chan = make_channel(URL + redirects[0]);
-  chan.asyncOpen(new ChannelListener(finish_test, null), null);
+  chan.asyncOpen2(new ChannelListener(finish_test, null));
   do_test_pending();
 }
