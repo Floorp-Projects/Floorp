33b2ccdb8aac87f291647a30c8d701004f665400
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2015-12-23 11:45:26 -0800

    Bug 1234813 - Tests for: sendBeacon should not throw if blocked by Content Policy. r=rbarnes

--- dom/security/test/csp/file_main.js
+++ dom/security/test/csp/file_main.js
@@ -11,10 +11,7 @@ doXHR("http://example.com/tests/dom/security/test/csp/file_CSP.sjs?testid=xhr_ba
 fetch("http://mochi.test:8888/tests/dom/security/test/csp/file_CSP.sjs?testid=fetch_good");
 fetch("http://example.com/tests/dom/security/test/csp/file_CSP.sjs?testid=fetch_bad");
 navigator.sendBeacon("http://mochi.test:8888/tests/dom/security/test/csp/file_CSP.sjs?testid=beacon_good");
-try {
-  navigator.sendBeacon("http://example.com/tests/dom/security/test/csp/file_CSP.sjs?testid=beacon_bad");
-} catch(ex) {}
-
+navigator.sendBeacon("http://example.com/tests/dom/security/test/csp/file_CSP.sjs?testid=beacon_bad");
 
 new Worker("file_main_worker.js").postMessage({inherited : false});
 
