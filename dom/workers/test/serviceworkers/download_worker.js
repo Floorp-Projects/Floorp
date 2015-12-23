3131be1c3faba82612c86fe65e9c9c49daa1d94c
Ben Kelly <ben@wanderview.com>
2015-12-23 12:20:53 -0800

    Bug 1220681 - P6 Use clients.claim() in browser_download.js to avoid worker unregister race. r=jdm, a=sylvestre
    --HG--
    extra : source : ccc0cb0b69991861253569b822454eeb1c61e325

--- dom/workers/test/serviceworkers/download_worker.js
+++ dom/workers/test/serviceworkers/download_worker.js
@@ -1,5 +1,23 @@
+addEventListener('install', function(evt) {
+  evt.waitUntil(self.skipWaiting());
+});
+
+addEventListener('activate', function(evt) {
+  // We claim the current clients in order to ensure that we have an
+  // active client when we call unregister in the fetch handler.  Otherwise
+  // the unregister() can kill the current worker before returning a
+  // response.
+  evt.waitUntil(clients.claim());
+});
+
 addEventListener('fetch', function(evt) {
-  // We should only get a single fetch event.  Automatically unregister.
+  // This worker may live long enough to receive a fetch event from the next
+  // test.  Just pass such requests through to the network.
+  if (evt.request.url.indexOf('fake_download') === -1) {
+    return;
+  }
+
+  // We should only get a single download fetch event. Automatically unregister.
   evt.respondWith(registration.unregister().then(function() {
     return new Response('service worker generated download', {
       headers: {
