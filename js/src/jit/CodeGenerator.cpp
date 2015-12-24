7613b3d5d69524daf0fa42ca446443569268620d
Benjamin Bouvier <benj@benj.me>
2015-12-24 09:51:30 +0100

    Bug 1234280 - Handle oom in CodeGeneratorShared::allocateData. r=jandem, a=sledru
    --HG--
    extra : source : fcc73febb20cb72bb6716b48757b18c3f9044297
    extra : intermediate-source : 32808687f47165f5e84d060e550063039e3dc8d7

--- js/src/jit/CodeGenerator.cpp
+++ js/src/jit/CodeGenerator.cpp
@@ -8539,7 +8539,9 @@ CodeGenerator::visitGetPropertyIC(OutOfLineUpdateCache* ool, DataPtr<GetProperty
     if (ic->idempotent()) {
         size_t numLocs;
         CacheLocationList& cacheLocs = lir->mirRaw()->toGetPropertyCache()->location();
-        size_t locationBase = addCacheLocations(cacheLocs, &numLocs);
+        size_t locationBase;
+        if (!addCacheLocations(cacheLocs, &numLocs, &locationBase))
+            return;
         ic->setLocationInfo(locationBase, numLocs);
     }
 
