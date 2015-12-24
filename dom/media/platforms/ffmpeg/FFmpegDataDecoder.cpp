cf09fd969fa4829a051bde99c6f10042cecfe993
Jean-Yves Avenard <jyavenard@mozilla.com>
2015-12-24 13:09:09 +1100

    Bug 1234553: Always request 16 bits audio for libavcodec 53. r=kentuckyfriedtakahe
    LibAV 0.8 produces rubbish floating point data. We had restricted LibAV 0.8 to produce 16 bits audio instead, unfortunately some ubuntu version appears to have bumped the minor version.

--- dom/media/platforms/ffmpeg/FFmpegDataDecoder.cpp
+++ dom/media/platforms/ffmpeg/FFmpegDataDecoder.cpp
@@ -90,8 +90,8 @@ FFmpegDataDecoder<LIBAV_VER>::InitDecoder()
   // FFmpeg takes this as a suggestion for what format to use for audio samples.
   uint32_t major, minor;
   FFmpegRuntimeLinker::GetVersion(major, minor);
-  // LibAV 0.8 produces rubbish float interlaved samples, request 16 bits audio.
-  mCodecContext->request_sample_fmt = major == 53 && minor <= 34 ?
+  // LibAV 0.8 produces rubbish float interleaved samples, request 16 bits audio.
+  mCodecContext->request_sample_fmt = major == 53 ?
     AV_SAMPLE_FMT_S16 : AV_SAMPLE_FMT_FLT;
 
   // FFmpeg will call back to this to negotiate a video pixel format.
