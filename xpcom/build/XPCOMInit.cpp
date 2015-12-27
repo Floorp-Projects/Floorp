5a0c92b3317c9501ded6abe993a7f681ddb35b3b
Sebastian Hengst <archaeopteryx@coole-files.de>
2015-12-27 23:33:02 +0100

    Backed out changeset 6daddfb64d72 (bug 1228437) for timing out in test_NuwaProcessCreation.html on B2G ICS emulator debug. r=backout

--- xpcom/build/XPCOMInit.cpp
+++ xpcom/build/XPCOMInit.cpp
@@ -532,16 +532,12 @@ NS_InitXPCOM2(nsIServiceManager** aResult,
     sExitManager = new AtExitManager();
   }
 
-  MessageLoop* messageLoop = MessageLoop::current();
-  if (!messageLoop) {
+  if (!MessageLoop::current()) {
     sMessageLoop = new MessageLoopForUI(MessageLoop::TYPE_MOZILLA_UI);
     sMessageLoop->set_thread_name("Gecko");
     // Set experimental values for main thread hangs:
     // 128ms for transient hangs and 8192ms for permanent hangs
     sMessageLoop->set_hang_timeouts(128, 8192);
-  } else if (messageLoop->type() == MessageLoop::TYPE_MOZILLA_CHILD) {
-    messageLoop->set_thread_name("Gecko_Child");
-    messageLoop->set_hang_timeouts(128, 8192);
   }
 
   if (XRE_IsParentProcess() &&
