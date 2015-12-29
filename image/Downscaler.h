6603bc97f2e02efce5a5dfb8aadfc34dca538944
Edwin Flores <eflores@mozilla.com>
2015-12-29 18:19:27 -0500

    Bug 1229825 - Make GIF deinterlacer respect the frame rect bounds. r=tn, a=sylvestre

--- image/Downscaler.h
+++ image/Downscaler.h
@@ -58,6 +58,7 @@ public:
 
   const nsIntSize& OriginalSize() const { return mOriginalSize; }
   const nsIntSize& TargetSize() const { return mTargetSize; }
+  const nsIntSize FrameSize() const { return nsIntSize(mFrameRect.width, mFrameRect.height); }
   const gfxSize& Scale() const { return mScale; }
 
   /**
