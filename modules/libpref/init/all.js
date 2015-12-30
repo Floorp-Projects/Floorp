b253053ba3bc703b36cb3319ccbba5dd1cbb26bd
Phil Ringnalda <philringnalda@gmail.com>
2015-12-29 19:32:45 -0800

    Back out changeset 3769091b5a3e (bug 1218482) for frequent "Assertion failure: mCompositorThreadHolder" on b2g

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4675,12 +4675,8 @@ pref("dom.browserElement.maxScreenshotDelayMS", 2000);
 // Whether we should show the placeholder when the element is focused but empty.
 pref("dom.placeholder.show_on_focus", true);
 
-// VR is disabled by default in release and enabled for nightly and aurora
-#ifdef RELEASE_BUILD
+// VR is disabled by default
 pref("dom.vr.enabled", false);
-#else
-pref("dom.vr.enabled", true);
-#endif
 // Oculus > 0.5
 pref("dom.vr.oculus.enabled", true);
 // Oculus <= 0.5; will only trigger if > 0.5 is not used or found
@@ -4688,7 +4684,7 @@ pref("dom.vr.oculus050.enabled", true);
 // Cardboard VR device is disabled by default
 pref("dom.vr.cardboard.enabled", false);
 // 0 = never; 1 = only if real devices aren't there; 2 = always
-pref("dom.vr.add-test-devices", 0);
+pref("dom.vr.add-test-devices", 1);
 // true = show the VR textures in our compositing output; false = don't.
 // true might have performance impact
 pref("gfx.vr.mirror-textures", false);
