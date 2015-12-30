82e6d4397964f3cc67aeaa288f4cb94b83ad41e7
Lee Salzman <lsalzman@mozilla.com>
2015-12-30 13:39:13 -0500

    Bug 1229983 - verify that paths are finite in DrawTargetSkia::Stroke/Fill to handle Canvas paths containing infs. r=jmuizelaar

--- gfx/2d/DrawTargetSkia.cpp
+++ gfx/2d/DrawTargetSkia.cpp
@@ -493,6 +493,10 @@ DrawTargetSkia::Stroke(const Path *aPath,
     return;
   }
 
+  if (!skiaPath->GetPath().isFinite()) {
+    return;
+  }
+
   mCanvas->drawPath(skiaPath->GetPath(), paint.mPaint);
 }
 
@@ -542,6 +546,10 @@ DrawTargetSkia::Fill(const Path *aPath,
   const PathSkia *skiaPath = static_cast<const PathSkia*>(aPath);
 
   AutoPaintSetup paint(mCanvas.get(), aOptions, aPattern);
+
+  if (!skiaPath->GetPath().isFinite()) {
+    return;
+  }
 
   mCanvas->drawPath(skiaPath->GetPath(), paint.mPaint);
 }
