9a4392bbd5e047e5113e54d9571a3e053de76c26
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2015-12-30 09:31:18 +0000

    Bug 1233747 and bug 1235709 - deal better with host-less URIs in the checkbox handling for dialogs in non-selected tabs, r=Aryx
    --HG--
    extra : commitid : CibBoGVKQ3m
    extra : rebase_source : 72805a635195ab07923139e0854da9ac56fc8d76
    extra : histedit_source : 1865f7fe745c288ba3ccae7949bfc00f1334f84b

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -7923,13 +7923,17 @@ TabModalPromptBox.prototype = {
     delete this._allowTabFocusByPromptPrincipal;
 
     let allowFocusCheckbox; // Define outside the if block so we can bind it into the callback.
-    if (principalToAllowFocusFor) {
+    let hostForAllowFocusCheckbox = "";
+    try {
+      hostForAllowFocusCheckbox = principalToAllowFocusFor.URI.host;
+    } catch (ex) { /* Ignore exceptions for host-less URIs */ }
+    if (hostForAllowFocusCheckbox) {
       let allowFocusRow = document.createElementNS(XUL_NS, "row");
       allowFocusCheckbox = document.createElementNS(XUL_NS, "checkbox");
       let spacer = document.createElementNS(XUL_NS, "spacer");
       allowFocusRow.appendChild(spacer);
       let label = gBrowser.mStringBundle.getFormattedString("tabs.allowTabFocusByPromptForSite",
-                                                            [principalToAllowFocusFor.URI.host]);
+                                                            [hostForAllowFocusCheckbox]);
       allowFocusCheckbox.setAttribute("label", label);
       allowFocusRow.appendChild(allowFocusCheckbox);
       newPrompt.appendChild(allowFocusRow);
