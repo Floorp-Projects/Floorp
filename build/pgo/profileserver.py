905fbc8db2b856d9dc99c149902f2f5104a745d2
Mike Hommey <mh+mozilla@glandium.org>
2015-12-30 15:59:40 +0900

    Bug 1229731 - Run Firefox a first time to fill its profile before doing the actual profiling run for PGO. r=ted

--- build/pgo/profileserver.py
+++ build/pgo/profileserver.py
@@ -63,6 +63,14 @@ if __name__ == '__main__':
       if os.path.exists(vc12dir):
         env["PATH"] = vc12dir + ";" + env["PATH"]
 
+    # Run Firefox a first time to initialize its profile
+    runner = FirefoxRunner(profile=profile,
+                           binary=build.get_binary_path(where="staged-package"),
+                           cmdargs=['javascript:Quitter.quit()'],
+                           env=env)
+    runner.start()
+    runner.wait()
+
     jarlog = os.getenv("JARLOG_FILE")
     if jarlog:
       env["MOZ_JAR_LOG_FILE"] = os.path.abspath(jarlog)
