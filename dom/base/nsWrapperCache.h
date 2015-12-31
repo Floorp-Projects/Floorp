792ca148253b400c1a48d9105911bc76c56debc2
Wes Kocher <wkocher@mozilla.com>
2015-12-30 16:34:18 -0800

    Backed out changeset 0d55a6e4e98e (bug 1120016) for 980 b2g hazards
    --HG--
    extra : commitid : 27OIHVsa17S

--- dom/base/nsWrapperCache.h
+++ dom/base/nsWrapperCache.h
@@ -258,15 +258,14 @@ protected:
   void TraceWrapper(JSTracer* aTrc, const char* name)
   {
     if (mWrapper) {
-      JS_CallUnbarrieredObjectTracer(aTrc, &mWrapper, name);
+      JS_CallObjectTracer(aTrc, &mWrapper, name);
     }
   }
 
   void PoisonWrapper()
   {
     if (mWrapper) {
-      // See setToCrashOnTouch() in RootingAPI.h
-      mWrapper = reinterpret_cast<JSObject*>(1);
+      mWrapper.setToCrashOnTouch();
     }
   }
 
@@ -288,7 +287,13 @@ private:
     return mWrapper;
   }
 
-  void SetWrapperJSObject(JSObject* aWrapper);
+  void SetWrapperJSObject(JSObject* aWrapper)
+  {
+    mWrapper = aWrapper;
+    UnsetWrapperFlags(kWrapperFlagsMask & ~WRAPPER_IS_NOT_DOM_BINDING);
+  }
+
+  void TraceWrapperJSObject(JSTracer* aTrc, const char* aName);
 
   FlagsType GetWrapperFlags() const
   {
@@ -313,8 +318,8 @@ private:
     mFlags &= ~aFlagsToUnset;
   }
 
-  void HoldJSObjects(void* aScriptObjectHolder,
-                     nsScriptObjectTracer* aTracer);
+  static void HoldJSObjects(void* aScriptObjectHolder,
+                            nsScriptObjectTracer* aTracer);
 
 #ifdef DEBUG
 public:
@@ -344,8 +349,8 @@ private:
 
   enum { kWrapperFlagsMask = (WRAPPER_BIT_PRESERVED | WRAPPER_IS_NOT_DOM_BINDING) };
 
-  JSObject* mWrapper;
-  FlagsType mFlags;
+  JS::Heap<JSObject*> mWrapper;
+  FlagsType           mFlags;
 };
 
 enum { WRAPPER_CACHE_FLAGS_BITS_USED = 2 };
