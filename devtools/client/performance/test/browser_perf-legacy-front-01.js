cbbde2733e1028af0f5e8f7449f53d216a303f56
Ehsan Akhgari <ehsan@mozilla.com>
2015-12-30 20:44:06 -0500

    Bug 1232074 - Increase the timeout of browser_perf-legacy-front-01.js

--- devtools/client/performance/test/browser_perf-legacy-front-01.js
+++ devtools/client/performance/test/browser_perf-legacy-front-01.js
@@ -1,6 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+requestLongerTimeout(2);
+
 /**
  * Tests that when setting recording features in the UI (like enabling framerate or memory),
  * if the target does not support these features, then the target's support overrides
