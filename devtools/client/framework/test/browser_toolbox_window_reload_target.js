5e006e5976d92d5d20980f08d865957c846ca105
Ehsan Akhgari <ehsan@mozilla.com>
2015-12-30 20:45:00 -0500

    Bug 1027416 - Increase the timeout of browser_toolbox_window_reload_target.js a=test-only

--- devtools/client/framework/test/browser_toolbox_window_reload_target.js
+++ devtools/client/framework/test/browser_toolbox_window_reload_target.js
@@ -1,6 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+requestLongerTimeout(2);
+
 const TEST_URL = "data:text/html;charset=utf-8,"+
                  "<html><head><title>Test reload</title></head>"+
                  "<body><h1>Testing reload from devtools</h1></body></html>";
