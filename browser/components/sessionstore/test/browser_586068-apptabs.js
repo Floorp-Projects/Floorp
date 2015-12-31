f0d2d4993984a5dd8683f059e6fe574e4519e54b
Ehsan Akhgari <ehsan@mozilla.com>
2015-12-31 17:41:25 -0500

    Bug 1170881 - Increase the timeout of browser_586068-apptabs.js a=test-only

--- browser/components/sessionstore/test/browser_586068-apptabs.js
+++ browser/components/sessionstore/test/browser_586068-apptabs.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+requestLongerTimeout(2);
+
 const PREF_RESTORE_ON_DEMAND = "browser.sessionstore.restore_on_demand";
 
 add_task(function* test() {
