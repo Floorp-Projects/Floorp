f882971116b9f9411858c1eb4e7bed167e0b6dda
Ehsan Akhgari <ehsan@mozilla.com>
2015-12-31 17:30:22 -0500

    Bug 1221413 - Increase the timeout of browser_tab_drag_drop_perwindow.js

--- browser/base/content/test/general/browser_tab_drag_drop_perwindow.js
+++ browser/base/content/test/general/browser_tab_drag_drop_perwindow.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+requestLongerTimeout(2);
+
 const CHROMEUTILS_URL = "chrome://mochikit/content/tests/SimpleTest/ChromeUtils.js";
 var ChromeUtils = {};
 
