c80c0b95a00553df2c39278b844a0252bf41343c
Ehsan Akhgari <ehsan@mozilla.com>
2015-12-31 17:31:12 -0500

    Bug 1218007 - Increase the timeout of browser_perf-private-browsing.js a=test-only

--- devtools/client/performance/test/browser_perf-private-browsing.js
+++ devtools/client/performance/test/browser_perf-private-browsing.js
@@ -1,6 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+requestLongerTimeout(2);
+
 /**
  * Tests that disables the frontend when in private browsing mode.
  */
