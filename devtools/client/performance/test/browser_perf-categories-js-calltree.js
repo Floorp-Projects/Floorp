8ae1b5815bb2ca43e12d3acbec6d3a5e87fd0d29
Ehsan Akhgari <ehsan@mozilla.com>
2015-12-30 20:46:02 -0500

    Bug 1202628 - Increase the timeout of browser_perf-categories-js-calltree.js a=test-only

--- devtools/client/performance/test/browser_perf-categories-js-calltree.js
+++ devtools/client/performance/test/browser_perf-categories-js-calltree.js
@@ -1,6 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+requestLongerTimeout(2);
+
 /**
  * Tests that the categories are shown in the js call tree when platform data
  * is enabled.
