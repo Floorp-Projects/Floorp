b4c77a75b278207c7ef6c246b4ddf7da1717a801
Ehsan Akhgari <ehsan@mozilla.com>
2015-12-30 20:33:14 -0500

    Bug 1235691 - Increase the timeout of browser_perf-details-calltree-render.js a=test-only

--- devtools/client/performance/test/browser_perf-details-calltree-render.js
+++ devtools/client/performance/test/browser_perf-details-calltree-render.js
@@ -1,6 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+requestLongerTimeout(2);
+
 /**
  * Tests that the call tree view renders content after recording.
  */
