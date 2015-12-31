07f768986d067383e688d06d76e58ca681346130
Ehsan Akhgari <ehsan@mozilla.com>
2015-12-30 21:45:46 -0500

    Bug 1235817 - Increase the timeout of browser_perf-options-show-idle-blocks-01.js

--- devtools/client/performance/test/browser_perf-options-show-idle-blocks-01.js
+++ devtools/client/performance/test/browser_perf-options-show-idle-blocks-01.js
@@ -1,6 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+requestLongerTimeout(2);
+
 /**
  * Tests that the js flamegraphs get rerendered when toggling `show-idle-blocks`
  */
