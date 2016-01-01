b72d994e0e79fd3cc16e1369f2aa95257e648d0b
Tooru Fujisawa <arai_a@mac.com>
2016-01-01 14:19:20 +0900

    Bug 1153978 - Part 1: Separate buildIdOp from AsmJSCacheOps. r=jandem,bz

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -6371,8 +6371,7 @@ static const JS::AsmJSCacheOps asmJSCacheOps = {
     ShellOpenAsmJSCacheEntryForRead,
     ShellCloseAsmJSCacheEntryForRead,
     ShellOpenAsmJSCacheEntryForWrite,
-    ShellCloseAsmJSCacheEntryForWrite,
-    ShellBuildId
+    ShellCloseAsmJSCacheEntryForWrite
 };
 
 static JSContext*
@@ -7268,6 +7267,7 @@ main(int argc, char** argv, char** envp)
     JS_InitDestroyPrincipalsCallback(rt, ShellPrincipals::destroy);
 
     JS_SetInterruptCallback(rt, ShellInterruptCallback);
+    JS::SetBuildIdOp(rt, ShellBuildId);
     JS::SetAsmJSCacheOps(rt, &asmJSCacheOps);
 
     JS_SetNativeStackQuota(rt, gMaxStackSize);
