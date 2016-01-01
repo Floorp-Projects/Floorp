29bbc52ac70d5a210d152dd5cc81a73fbb0af987
Gregory Szorc <gps@mozilla.com>
2015-12-31 16:01:48 -0800

    Bug 1235921 - Update modern Mercurial version to 3.5.2; r=me
    3.5.2 is what is listed in `mach mercurial-setup`. These should match.
    Add a comment to each file saying to change both.
    
    --HG--
    extra : commitid : FebjTovmqGk
    extra : rebase_source : 50490c1896a4c402f27cf4154b155932614da558
    extra : amend_source : 73ae0ddc9f2770351d2ee2aaf5121656fb7e5750

--- tools/mercurial/hgsetup/wizard.py
+++ tools/mercurial/hgsetup/wizard.py
@@ -42,6 +42,8 @@ are up to date and you won't have to do anything.
 To begin, press the enter/return key.
 '''.strip()
 
+# This should match MODERN_MERCURIAL_VERSION in
+# python/mozboot/mozboot/base.py.
 OLDEST_NON_LEGACY_VERSION = LooseVersion('3.5.2')
 LEGACY_MERCURIAL = '''
 You are running an out of date Mercurial client (%s).
