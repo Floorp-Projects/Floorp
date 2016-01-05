8d496705db9f36ddd919d77b89517bb2fc04d30a
Eric Rahm <erahm@mozilla.com>
2016-01-05 13:05:19 -0800

    Bug 1174972 - Add basic mozilla logging backend. r=froydnj,r=bwc,r=jduell

--- media/webrtc/signaling/test/jsep_session_unittest.cpp
+++ media/webrtc/signaling/test/jsep_session_unittest.cpp
@@ -20,6 +20,7 @@
 
 #include "FakeMediaStreams.h"
 #include "FakeMediaStreamsImpl.h"
+#include "FakeLogging.h"
 
 #include "signaling/src/sdp/SdpMediaSection.h"
 #include "signaling/src/sdp/SipccSdpParser.h"
