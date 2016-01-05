5f9d005f4bb2132c4add8eb9b1f0630d70fc36e7
Luke Wagner <luke@mozilla.com>
2016-01-05 10:56:17 -0600

    Bug 1236541 - Odin: when enabling profiling, only patch actual callsites (r=bbouvier)

---
+++ js/src/jit-test/tests/asm.js/testBug1236541.js
@@ -0,0 +1,15 @@
+enableSPSProfiling();
+
+function mod() {
+    "use asm";
+    function f(i0) {
+        i0 = i0 | 0
+        switch (0) {
+            case 0:
+        }
+    }
+    return f;
+}
+
+mod()();
+mod()();
