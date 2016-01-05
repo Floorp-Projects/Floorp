5b29198d9d5ed5e45f897f42b6e59e32470d7e40
Steve Singer <steve@ssinger.info>
2016-01-05 09:26:58 -0500

    Bug 1232150 - Add atomics for ppc/ppc64. r=lth
    --HG--
    extra : rebase_source : 87b1a27b663075006ef8b6e3e62d1b168bc3a6f5

--- js/src/jit/AtomicOperations.h
+++ js/src/jit/AtomicOperations.h
@@ -312,6 +312,10 @@ AtomicOperations::isLockfree(int32_t size)
 # include "jit/arm64/AtomicOperations-arm64.h"
 #elif defined(JS_CODEGEN_MIPS32) || defined(JS_CODEGEN_MIPS64)
 # include "jit/mips-shared/AtomicOperations-mips-shared.h"
+#elif defined(__ppc64__) || defined(__PPC64_)       \
+    || defined(__ppc64le__) || defined(__PPC64LE__) \
+    || defined(__ppc__) || defined(__PPC__)
+# include "jit/none/AtomicOperations-ppc.h"
 #elif defined(JS_CODEGEN_NONE)
 # include "jit/none/AtomicOperations-none.h"
 #elif defined(JS_CODEGEN_X86) || defined(JS_CODEGEN_X64)
