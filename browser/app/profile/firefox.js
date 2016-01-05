c4655e768a3cd585131152aaa9bca9e056b86568
Kit Cambridge <kcambridge@mozilla.com>
2016-01-05 15:53:43 -0700

    Bug 1234054 - Only enable service worker notifications on Desktop and non-release B2G/Android. r=ehsan; a=sylvestre

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1623,6 +1623,7 @@ pref("view_source.tab", true);
 pref("dom.serviceWorkers.enabled", true);
 pref("dom.serviceWorkers.interception.enabled", true);
 pref("dom.serviceWorkers.openWindow.enabled", true);
+pref("dom.webnotifications.serviceworker.enabled", true);
 
 #ifndef RELEASE_BUILD
 // Enable Push API.
