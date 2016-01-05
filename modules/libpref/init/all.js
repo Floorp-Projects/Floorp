d7abc46375df4aad8a44971299ba26af69232ffe
Bill McCloskey <billm@mozilla.com>
2016-01-05 15:33:34 -0800

    Bug 1236754 - Enable add-on interposition unconditionally. r=mconley, a=sylvestre

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -206,11 +206,7 @@ pref("dom.url.getters_decode_hash", false);
 // Whether to run add-on code in different compartments from browser code. This
 // causes a separate compartment for each (addon, global) combination, which may
 // significantly increase the number of compartments in the system.
-#ifdef E10S_TESTING_ONLY
 pref("dom.compartment_per_addon", true);
-#else
-pref("dom.compartment_per_addon", false);
-#endif
 
 // Fastback caching - if this pref is negative, then we calculate the number
 // of content viewers to cache based on the amount of available memory.
