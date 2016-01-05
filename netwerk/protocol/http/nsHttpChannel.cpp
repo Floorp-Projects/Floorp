ddf76b50b9a1db9913f3ee59947e9d0d7ada0357
Ehsan Akhgari <ehsan@mozilla.com>
2016-01-04 19:21:42 -0500

    Bug 1229369 - Intercept redirected network fetches that have their request mode set to manual; r=jdm
    In the non-e10s case, this is done by simply avoiding to set the
    LOAD_BYPASS_SERVICE_WORKER flag when we detect a non-internal redirect
    in the manual redirect mode.
    
    The e10s solution is a bit more complicated.  Only the child process
    knows whether a URI needs to be intercepted, so we piggy back on the
    code written to support the |event.respondWith(Response.redirect())|
    case where we know in the child that the target of the redirect needs to
    be intercepted.
    
    This means that we need to check the same condition as in the non-e10s
    case, but we also need to check whether the target of the redirection
    needs to be intercepted, which means we need to properly take secure
    upgrades into account as well.  This is done by computing both whether
    we should intercept and whether we should do a secure upgrade in
    HttpChannelChild::SetupRedirect() and saving the information on the
    HttpChannelChild for later usage in HttpChannelChild::AsyncOpen().

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -4516,14 +4516,19 @@ nsHttpChannel::SetupReplacementChannel(nsIURI       *newURI,
 
     if (!(redirectFlags & nsIChannelEventSink::REDIRECT_STS_UPGRADE) &&
         mInterceptCache != INTERCEPTED) {
-        // Ensure that internally-redirected channels cannot be intercepted, which would look
-        // like two separate requests to the nsINetworkInterceptController.
-        nsLoadFlags loadFlags = nsIRequest::LOAD_NORMAL;
-        rv = newChannel->GetLoadFlags(&loadFlags);
-        NS_ENSURE_SUCCESS(rv, rv);
-        loadFlags |= nsIChannel::LOAD_BYPASS_SERVICE_WORKER;
-        rv = newChannel->SetLoadFlags(loadFlags);
-        NS_ENSURE_SUCCESS(rv, rv);
+        // Ensure that internally-redirected channels, or loads with manual
+        // redirect mode cannot be intercepted, which would look like two
+        // separate requests to the nsINetworkInterceptController.
+        if (mRedirectMode != nsIHttpChannelInternal::REDIRECT_MODE_MANUAL ||
+            (redirectFlags & (nsIChannelEventSink::REDIRECT_TEMPORARY |
+                              nsIChannelEventSink::REDIRECT_PERMANENT)) == 0) {
+            nsLoadFlags loadFlags = nsIRequest::LOAD_NORMAL;
+            rv = newChannel->GetLoadFlags(&loadFlags);
+            NS_ENSURE_SUCCESS(rv, rv);
+            loadFlags |= nsIChannel::LOAD_BYPASS_SERVICE_WORKER;
+            rv = newChannel->SetLoadFlags(loadFlags);
+            NS_ENSURE_SUCCESS(rv, rv);
+        }
     }
 
     return NS_OK;
