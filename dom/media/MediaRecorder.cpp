7c4fabc445cb87ec6821cbc67eb8f88cb72ab55f
Andreas Pehrson <pehrsons@gmail.com>
2016-01-05 10:16:21 +0800

    Bug 1208371 - Move OnTracksAvailableCallback out of DOMMediaStream. r=roc So it can be forward declared.
    MozReview-Commit-ID: 4RH7p8AzO84

--- dom/media/MediaRecorder.cpp
+++ dom/media/MediaRecorder.cpp
@@ -288,7 +288,7 @@ class MediaRecorder::Session: public nsIObserver
   };
 
   // For Ensure recorder has tracks to record.
-  class TracksAvailableCallback : public DOMMediaStream::OnTracksAvailableCallback
+  class TracksAvailableCallback : public OnTracksAvailableCallback
   {
   public:
     explicit TracksAvailableCallback(Session *aSession)
