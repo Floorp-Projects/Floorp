82b5f8d44d4788fdd593b6bb670bfbb46ff948b8
Jon Coppeard <jcoppeard@mozilla.com>
2016-01-05 15:07:58 +0000

    Bug 1233109 - Check for duplicate exported names using the data in the ModuleBuilder and remove ModuleBox::exportNames r=shu

--- js/src/frontend/SharedContext.h
+++ js/src/frontend/SharedContext.h
@@ -420,7 +420,6 @@ class ModuleBox : public ObjectBox, public SharedContext
   public:
     Bindings bindings;
     ModuleBuilder& builder;
-    TraceableVector<JSAtom*> exportNames;
 
     template <typename ParseHandler>
     ModuleBox(ExclusiveContext* cx, ObjectBox* traceListHead, ModuleObject* module,
