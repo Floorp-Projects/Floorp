f93fb95aab5f9a0528b1816e38e9d86900169df8
Bill McCloskey <billm@mozilla.com>
2016-01-05 15:33:34 -0800

    Bug 1236754 - Enable add-on interposition unconditionally (r=mconley)

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1576,11 +1576,9 @@ pref("browser.tabs.crashReporting.email", "");
 pref("layers.async-pan-zoom.enabled", true);
 #endif
 
-#ifdef E10S_TESTING_ONLY
 // Enable e10s add-on interposition by default.
 pref("extensions.interposition.enabled", true);
 pref("extensions.interposition.prefetching", true);
-#endif
 
 pref("browser.defaultbrowser.notificationbar", false);
 
