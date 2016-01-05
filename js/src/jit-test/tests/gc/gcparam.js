ba32e6eac51d081e500241d6a16d55538a2ab75a
Jon Coppeard <jcoppeard@mozilla.com>
2016-01-05 15:07:58 +0000

    Bug 1236564 - Fix various minor issues with getting/setting GC parameters r=terrence

---
+++ js/src/jit-test/tests/gc/gcparam.js
@@ -0,0 +1,47 @@
+function testGetParam(key) {
+    gcparam(key);
+}
+
+function testChangeParam(key) {
+    let prev = gcparam(key);
+    let value = prev - 1;
+    gcparam(key, value);
+    assertEq(gcparam(key), value);
+    gcparam(key, prev);
+}
+
+function testLargeParamValue(key) {
+    let prev = gcparam(key);
+    const value = 1000000;
+    gcparam(key, value);
+    assertEq(gcparam(key), value);
+    gcparam(key, prev);
+}
+
+testGetParam("gcBytes");
+testGetParam("gcNumber");
+testGetParam("unusedChunks");
+testGetParam("totalChunks");
+
+testChangeParam("maxBytes");
+testChangeParam("maxMallocBytes");
+testChangeParam("mode");
+testChangeParam("sliceTimeBudget");
+testChangeParam("markStackLimit");
+testChangeParam("highFrequencyTimeLimit");
+testChangeParam("highFrequencyLowLimit");
+testChangeParam("highFrequencyHighLimit");
+testChangeParam("highFrequencyHeapGrowthMax");
+testChangeParam("highFrequencyHeapGrowthMin");
+testChangeParam("lowFrequencyHeapGrowth");
+testChangeParam("dynamicHeapGrowth");
+testChangeParam("dynamicMarkSlice");
+testChangeParam("allocationThreshold");
+testChangeParam("decommitThreshold");
+testChangeParam("minEmptyChunkCount");
+testChangeParam("maxEmptyChunkCount");
+testChangeParam("compactingEnabled");
+
+testLargeParamValue("highFrequencyLowLimit");
+testLargeParamValue("highFrequencyHighLimit");
+testLargeParamValue("decommitThreshold");
