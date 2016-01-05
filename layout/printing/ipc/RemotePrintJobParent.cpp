4f71744c7eead28467637272126b46986b11e1a4
Bob Owen <bobowencode@gmail.com>
2016-01-05 10:08:57 +0000

    Bug 1156742 Part 14: Complete RemotePrintJob using PrintTranslator. r=mconley

--- layout/printing/ipc/RemotePrintJobParent.cpp
+++ layout/printing/ipc/RemotePrintJobParent.cpp
@@ -6,7 +6,16 @@
 
 #include "RemotePrintJobParent.h"
 
+#include <istream>
+
+#include "gfxContext.h"
+#include "mozilla/Attributes.h"
+#include "mozilla/unused.h"
+#include "nsComponentManagerUtils.h"
+#include "nsDeviceContext.h"
+#include "nsIDeviceContextSpec.h"
 #include "nsIPrintSettings.h"
+#include "PrintTranslator.h"
 
 namespace mozilla {
 namespace layout {
@@ -23,29 +32,123 @@ RemotePrintJobParent::RecvInitializePrint(const nsString& aDocumentTitle,
                                           const int32_t& aStartPage,
                                           const int32_t& aEndPage)
 {
-  NS_NOTREACHED("RemotePrintJobParent::RecvInitializePrint not implemented!");
-  return false;
+  nsresult rv = InitializePrintDevice(aDocumentTitle, aPrintToFile, aStartPage,
+                                      aEndPage);
+  if (NS_FAILED(rv)) {
+    Unused << SendPrintInitializationResult(rv);
+    Unused << Send__delete__(this);
+    return true;
+  }
+
+  mPrintTranslator.reset(new PrintTranslator(mPrintDeviceContext));
+  Unused << SendPrintInitializationResult(NS_OK);
+
+  return true;
+}
+
+nsresult
+RemotePrintJobParent::InitializePrintDevice(const nsString& aDocumentTitle,
+                                            const nsString& aPrintToFile,
+                                            const int32_t& aStartPage,
+                                            const int32_t& aEndPage)
+{
+  nsresult rv;
+  nsCOMPtr<nsIDeviceContextSpec> deviceContextSpec =
+  do_CreateInstance("@mozilla.org/gfx/devicecontextspec;1", &rv);
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
+  }
+
+  rv = deviceContextSpec->Init(nullptr, mPrintSettings, false);
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
+  }
+
+  mPrintDeviceContext = new nsDeviceContext();
+  rv = mPrintDeviceContext->InitForPrinting(deviceContextSpec);
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
+  }
+
+  rv = mPrintDeviceContext->BeginDocument(aDocumentTitle, aPrintToFile,
+                                          aStartPage, aEndPage);
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
+  }
+
+  return NS_OK;
 }
 
 bool
 RemotePrintJobParent::RecvProcessPage(Shmem&& aStoredPage)
 {
-  NS_NOTREACHED("PrintingParent::RecvProcessPage not implemented!");
-  return false;
+  nsresult rv = PrintPage(aStoredPage);
+
+  // Always deallocate the shared memory no matter what the result.
+  if (!DeallocShmem(aStoredPage)) {
+    NS_WARNING("Failed to deallocated shared memory, remote print will abort.");
+    rv = NS_ERROR_FAILURE;
+  }
+
+  if (NS_FAILED(rv)) {
+    Unused << SendAbortPrint(rv);
+  } else {
+    Unused << SendPageProcessed();
+  }
+
+  return true;
+}
+
+nsresult
+RemotePrintJobParent::PrintPage(const Shmem& aStoredPage)
+{
+  MOZ_ASSERT(mPrintDeviceContext);
+
+  nsresult rv = mPrintDeviceContext->BeginPage();
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
+  }
+
+  std::istringstream recording(std::string(aStoredPage.get<char>(),
+                                           aStoredPage.Size<char>()));
+  mPrintTranslator->TranslateRecording(recording);
+
+  rv = mPrintDeviceContext->EndPage();
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
+  }
+
+  mPrintTranslator->ClearSavedFonts();
+
+  return NS_OK;
 }
 
 bool
 RemotePrintJobParent::RecvFinalizePrint()
 {
-  NS_NOTREACHED("PrintingParent::RecvFinalizePrint not implemented!");
-  return false;
+  // EndDocument is sometimes called in the child even when BeginDocument has
+  // not been called. See bug 1223332.
+  if (mPrintDeviceContext) {
+    nsresult rv = mPrintDeviceContext->EndDocument();
+
+    // Too late to abort the child just log.
+    NS_WARN_IF(NS_FAILED(rv));
+  }
+
+
+  Unused << Send__delete__(this);
+  return true;
 }
 
 bool
 RemotePrintJobParent::RecvAbortPrint(const nsresult& aRv)
 {
-  NS_NOTREACHED("PrintingParent::RecvAbortPrint not implemented!");
-  return false;
+  if (mPrintDeviceContext) {
+    Unused << mPrintDeviceContext->AbortDocument();
+  }
+
+  Unused << Send__delete__(this);
+  return true;
 }
 
 RemotePrintJobParent::~RemotePrintJobParent()
