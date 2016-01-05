3288db6c90d8acd4592fac90d2dd0353b33c0191
Richard Barnes <rbarnes@mozilla.com>
2016-01-05 12:36:17 -0500

    Bug 1236975 - Re-enable SHA-1 certificates r=keeler,rbarnes a=lizzard

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1407,8 +1407,8 @@ pref("security.insecure_password.ui.enabled", false);
 // 1 = allow MITM for certificate pinning checks.
 pref("security.cert_pinning.enforcement_level", 1);
 
-// 2 = allow SHA-1 only before 2016-01-01
-pref("security.pki.sha1_enforcement_level", 2);
+// 0 = allow SHA-1
+pref("security.pki.sha1_enforcement_level", 0);
 
 // Required blocklist freshness for OneCRL OCSP bypass
 // (default is 1.25x extensions.blocklist.interval, or 30 hours)
