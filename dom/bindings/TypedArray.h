4f7e0fc34ce40cc2a2ef9e1ffc4762b4ff9bca3b
Terrence Cole <terrence@mozilla.com>
2016-01-06 12:05:06 -0800

    Backout 7621c3ea95cb11e1c376a9f705accc0f90057c0a (bug 1235923) for build bustage on a CLOSED TREE.

--- dom/bindings/TypedArray.h
+++ dom/bindings/TypedArray.h
@@ -45,10 +45,10 @@ public:
   inline void TraceSelf(JSTracer* trc)
   {
     if (mTypedObj) {
-      JS::UnsafeTraceRoot(trc, &mTypedObj, "TypedArray.mTypedObj");
+      JS_CallUnbarrieredObjectTracer(trc, &mTypedObj, "TypedArray.mTypedObj");
     }
     if (mWrappedObj) {
-      JS::UnsafeTraceRoot(trc, &mTypedObj, "TypedArray.mWrappedObj");
+      JS_CallUnbarrieredObjectTracer(trc, &mTypedObj, "TypedArray.mWrappedObj");
     }
   }
 
