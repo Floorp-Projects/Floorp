1c47bee22a8ece884c02944b72f413bf6ec37d77
Bogdan Postelnicu <bogdan.postelnicu@softvision.ro>
2016-01-06 04:32:00 -0500

    Bug 1237227 - Check the return of context->GetDisplayRootPresContext() for validity. r=roc

--- view/nsView.cpp
+++ view/nsView.cpp
@@ -1087,7 +1087,9 @@ nsView::DidCompositeWindow(const TimeStamp& aCompositeStart,
     nsAutoScriptBlocker scriptBlocker;
 
     nsPresContext* context = presShell->GetPresContext();
-    context->GetDisplayRootPresContext()->GetRootPresContext()->NotifyDidPaintForSubtree(nsIPresShell::PAINT_COMPOSITE);
+    nsRootPresContext* rootContext = context->GetDisplayRootPresContext();
+    MOZ_ASSERT(rootContext, "rootContext must be valid.");
+    rootContext->GetRootPresContext()->NotifyDidPaintForSubtree(nsIPresShell::PAINT_COMPOSITE);
 
     // If the two timestamps are identical, this was likely a fake composite
     // event which wouldn't be terribly useful to display.
