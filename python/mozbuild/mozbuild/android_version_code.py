2254883bc903bf208e9aabb4a966fe09474b40b5
Richard Newman <rnewman@mozilla.com>
2016-01-06 10:43:46 -0800

    Bug 1237342 - Drop support for Android API 14 from Firefox builds. r=nalexander

--- python/mozbuild/mozbuild/android_version_code.py
+++ python/mozbuild/mozbuild/android_version_code.py
@@ -18,12 +18,12 @@ def android_version_code_v0(buildid, cpu_arch=None, min_sdk=0, max_sdk=0):
     # None is interpreted as arm.
     if not cpu_arch or cpu_arch in ['armeabi', 'armeabi-v7a']:
         # Increment by MIN_SDK_VERSION -- this adds 9 to every build ID as a
-        # minimum.  Our split APK starts at 14.
+        # minimum.  Our split APK starts at 15.
         return base + min_sdk + 0
     elif cpu_arch in ['x86']:
         # Increment the version code by 3 for x86 builds so they are offered to
         # x86 phones that have ARM emulators, beating the 2-point advantage that
-        # the v14+ ARMv7 APK has.  If we change our splits in the future, we'll
+        # the v15+ ARMv7 APK has.  If we change our splits in the future, we'll
         # need to do this further still.
         return base + min_sdk + 3
     else:
@@ -55,11 +55,11 @@ def android_version_code_v1(buildid, cpu_arch=None, min_sdk=0, max_sdk=0):
 
     The bit labelled 'p' is a placeholder that is always 0 (for now).
 
-    The bit labelled 'g' is 1 if the build is an ARM build targeting API 14+
+    The bit labelled 'g' is 1 if the build is an ARM build targeting API 15+
     and 0 otherwise, which means the build targets Android API 9-10 (Gingerbread).
 
     Fennec no longer supports Android API 8 or earlier. After Bug 1155801 it
-    no longer supports API 11-13. API 9 is still supported due to significant usage.
+    no longer supports API 11-14. API 9 is still supported due to significant usage.
 
     We throw an explanatory exception when we are within one calendar year of
     running out of build events.  This gives lots of time to update the version
@@ -109,9 +109,9 @@ def android_version_code_v1(buildid, cpu_arch=None, min_sdk=0, max_sdk=0):
         # 0 is interpreted as SDK 9.
         if not min_sdk or min_sdk == 9:
             pass
-        # This used to compare to 11. The 14+ APK directly supersedes 11+, so
+        # This used to compare to 11. The 15+ APK directly supersedes 11+, so
         # we reuse this check.
-        elif min_sdk == 14:
+        elif min_sdk == 15:
             version |= 1 << 0
         else:
             raise ValueError("Don't know how to compute android:versionCode "
