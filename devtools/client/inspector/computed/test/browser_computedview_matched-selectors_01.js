c195030adcfaac04c218b468f9370fbc9b954c2f
Gabriel Luong <gabriel.luong@gmail.com>
2016-01-05 21:05:49 -0800

    Bug 1235178 - Part 2: Move the Computed Styles Inspector tests into the Inspector directory r=pbro

--- devtools/client/inspector/computed/test/browser_computedview_matched-selectors_01.js
+++ devtools/client/inspector/computed/test/browser_computedview_matched-selectors_01.js
@@ -6,7 +6,8 @@
 
 // Checking selector counts, matched rules and titles in the computed-view.
 
-const {PropertyView} = require("devtools/client/styleinspector/computed-view");
+const {PropertyView} =
+      require("devtools/client/inspector/computed/computed");
 const TEST_URI = TEST_URL_ROOT + "doc_matched_selectors.html";
 
 add_task(function*() {
