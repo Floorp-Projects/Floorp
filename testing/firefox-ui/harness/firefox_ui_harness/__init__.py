824d43d7b057bff5f54369b3060a740930161adc
Henrik Skupin <mail@hskupin.info>
2016-01-06 22:17:42 +0100

    Bug 1237322 - Mirror latest changes of firefox-ui-tests packages. r=maja_zf

--- testing/firefox-ui/harness/firefox_ui_harness/__init__.py
+++ testing/firefox-ui/harness/firefox_ui_harness/__init__.py
@@ -2,8 +2,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from firefox_ui_harness.cli_functional import cli_functional
-from firefox_ui_harness.cli_update import cli_update
-
-
-__version__ = '1.0.0'
+__version__ = '1.1.2'
