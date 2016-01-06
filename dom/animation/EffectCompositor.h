5d5fb26344d6e3cd6335d04dddb18cc8910089eb
Brian Birtles <birtles@gmail.com>
2016-01-06 11:04:04 +0900

    Bug 1228229 part 2 - Add a helper to get the appropriate (pseudo-)element for a frame; r=dbaron
    We will use similar logic later in this patch series so we separate it out into
    a separate helper function here.
    
    --HG--
    extra : rebase_source : 00cb49e7829bdef7a6084059b31fe2ef4b921af5

--- dom/animation/EffectCompositor.h
+++ dom/animation/EffectCompositor.h
@@ -7,13 +7,17 @@
 #ifndef mozilla_EffectCompositor_h
 #define mozilla_EffectCompositor_h
 
+#include "mozilla/Maybe.h"
+#include "mozilla/Pair.h"
 #include "mozilla/RefPtr.h"
+#include "nsCSSPseudoElements.h"
 #include "nsTArray.h"
 
 namespace mozilla {
 
 namespace dom {
 class Animation;
+class Element;
 }
 
 class EffectCompositor
@@ -25,6 +29,18 @@ public:
   static nsTArray<RefPtr<dom::Animation>>
   GetAnimationsForCompositor(const nsIFrame* aFrame,
                              nsCSSProperty aProperty);
+
+  // Helper to fetch the corresponding element and pseudo-type from a frame.
+  //
+  // For frames corresponding to pseudo-elements, the returned element is the
+  // element on which we store the animations (i.e. the EffectSet and/or
+  // AnimationCollection), *not* the generated content.
+  //
+  // Returns an empty result when a suitable element cannot be found including
+  // when the frame represents a pseudo-element on which we do not support
+  // animations.
+  static Maybe<Pair<dom::Element*, nsCSSPseudoElements::Type>>
+  GetAnimationElementAndPseudoForFrame(const nsIFrame* aFrame);
 };
 
 } // namespace mozilla
