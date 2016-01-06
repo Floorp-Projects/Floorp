5b40bb36a692f0f805fb26958030428209a6214d
Philip Chee <philip.chee@gmail.com>
2016-01-06 22:10:33 +0800

    Bug 1233997 - about:debugging should be available to all applications that ship the Devtools client (and not just Firefox) r=jryans
    --HG--
    extra : transplant_source : %A3%1C%AB%E5Z%D4w%28%98%E8%A3%0EcB%C3J%1FBD%91

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -105,9 +105,6 @@ static RedirEntry kRedirMap[] = {
     nsIAboutModule::ALLOW_SCRIPT },
   { "customizing", "chrome://browser/content/customizableui/aboutCustomizing.xul",
     nsIAboutModule::ALLOW_SCRIPT },
-  {
-    "debugging", "chrome://devtools/content/aboutdebugging/aboutdebugging.xhtml",
-    nsIAboutModule::ALLOW_SCRIPT },
   { "loopconversation", "chrome://loop/content/panels/conversation.html",
     nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
     nsIAboutModule::ALLOW_SCRIPT |
