789ccd15e0f3033154424abc60fd8b3590836b8b
Gabriel Luong <gabriel.luong@gmail.com>
2016-01-06 22:26:52 -0800

    Bug 1235172 - Part 1: Extract TextProperty to a separate module r=pbro

--- devtools/client/inspector/rules/rules.js
+++ devtools/client/inspector/rules/rules.js
@@ -17,6 +17,8 @@ const {ELEMENT_STYLE} = require("devtools/server/actors/styles");
 const {OutputParser} = require("devtools/client/shared/output-parser");
 const {PrefObserver, PREF_ORIG_SOURCES} =
       require("devtools/client/styleeditor/utils");
+const {TextProperty} =
+      require("devtools/client/inspector/rules/models/text-property");
 const {
   createChild,
   appendText,
@@ -26,7 +28,6 @@ const {
   throttle
 } = require("devtools/client/styleinspector/utils");
 const {
-  escapeCSSComment,
   parseDeclarations,
   parseSingleValue,
   parsePseudoClassesAndAttributes,
@@ -34,6 +35,7 @@ const {
   SELECTOR_ELEMENT,
   SELECTOR_PSEUDO_CLASS
 } = require("devtools/client/shared/css-parsing-utils");
+
 loader.lazyRequireGetter(this, "overlays",
   "devtools/client/styleinspector/style-inspector-overlays");
 loader.lazyRequireGetter(this, "EventEmitter",
@@ -1088,193 +1090,6 @@ Rule.prototype = {
       }
     }
     return false;
-  }
-};
-
-/**
- * A single property in a rule's authoredText.
- *
- * @param {Rule} rule
- *        The rule this TextProperty came from.
- * @param {String} name
- *        The text property name (such as "background" or "border-top").
- * @param {String} value
- *        The property's value (not including priority).
- * @param {String} priority
- *        The property's priority (either "important" or an empty string).
- * @param {Boolean} enabled
- *        Whether the property is enabled.
- * @param {Boolean} invisible
- *        Whether the property is invisible.  An invisible property
- *        does not show up in the UI; these are needed so that the
- *        index of a property in Rule.textProps is the same as the index
- *        coming from parseDeclarations.
- */
-function TextProperty(rule, name, value, priority, enabled = true,
-                      invisible = false) {
-  this.rule = rule;
-  this.name = name;
-  this.value = value;
-  this.priority = priority;
-  this.enabled = !!enabled;
-  this.invisible = invisible;
-  this.updateComputed();
-}
-
-TextProperty.prototype = {
-  /**
-   * Update the editor associated with this text property,
-   * if any.
-   */
-  updateEditor: function() {
-    if (this.editor) {
-      this.editor.update();
-    }
-  },
-
-  /**
-   * Update the list of computed properties for this text property.
-   */
-  updateComputed: function() {
-    if (!this.name) {
-      return;
-    }
-
-    // This is a bit funky.  To get the list of computed properties
-    // for this text property, we'll set the property on a dummy element
-    // and see what the computed style looks like.
-    let dummyElement = this.rule.elementStyle.dummyElement;
-    let dummyStyle = dummyElement.style;
-    dummyStyle.cssText = "";
-    dummyStyle.setProperty(this.name, this.value, this.priority);
-
-    this.computed = [];
-
-    try {
-      // Manually get all the properties that are set when setting a value on
-      // this.name and check the computed style on dummyElement for each one.
-      // If we just read dummyStyle, it would skip properties when value === "".
-      let subProps = domUtils.getSubpropertiesForCSSProperty(this.name);
-
-      for (let prop of subProps) {
-        this.computed.push({
-          textProp: this,
-          name: prop,
-          value: dummyStyle.getPropertyValue(prop),
-          priority: dummyStyle.getPropertyPriority(prop),
-        });
-      }
-    } catch (e) {
-      // This is a partial property name, probably from cutting and pasting
-      // text. At this point don't check for computed properties.
-    }
-  },
-
-  /**
-   * Set all the values from another TextProperty instance into
-   * this TextProperty instance.
-   *
-   * @param {TextProperty} prop
-   *        The other TextProperty instance.
-   */
-  set: function(prop) {
-    let changed = false;
-    for (let item of ["name", "value", "priority", "enabled"]) {
-      if (this[item] !== prop[item]) {
-        this[item] = prop[item];
-        changed = true;
-      }
-    }
-
-    if (changed) {
-      this.updateEditor();
-    }
-  },
-
-  setValue: function(value, priority, force = false) {
-    let store = this.rule.elementStyle.store;
-
-    if (this.editor && value !== this.editor.committed.value || force) {
-      store.userProperties.setProperty(this.rule.style, this.name, value);
-    }
-
-    this.rule.setPropertyValue(this, value, priority);
-    this.updateEditor();
-  },
-
-  /**
-   * Called when the property's value has been updated externally, and
-   * the property and editor should update.
-   */
-  noticeNewValue: function(value) {
-    if (value !== this.value) {
-      this.value = value;
-      this.updateEditor();
-    }
-  },
-
-  setName: function(name) {
-    let store = this.rule.elementStyle.store;
-
-    if (name !== this.name) {
-      store.userProperties.setProperty(this.rule.style, name,
-                                       this.editor.committed.value);
-    }
-
-    this.rule.setPropertyName(this, name);
-    this.updateEditor();
-  },
-
-  setEnabled: function(value) {
-    this.rule.setPropertyEnabled(this, value);
-    this.updateEditor();
-  },
-
-  remove: function() {
-    this.rule.removeProperty(this);
-  },
-
-  /**
-   * Return a string representation of the rule property.
-   */
-  stringifyProperty: function() {
-    // Get the displayed property value
-    let declaration = this.name + ": " + this.editor.valueSpan.textContent +
-      ";";
-
-    // Comment out property declarations that are not enabled
-    if (!this.enabled) {
-      declaration = "/* " + escapeCSSComment(declaration) + " */";
-    }
-
-    return declaration;
-  },
-
-  /**
-   * See whether this property's name is known.
-   *
-   * @return {Boolean} true if the property name is known, false otherwise.
-   */
-  isKnownProperty: function() {
-    try {
-      // If the property name is invalid, the cssPropertyIsShorthand
-      // will throw an exception.  But if it is valid, no exception will
-      // be thrown; so we just ignore the return value.
-      domUtils.cssPropertyIsShorthand(this.name);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  },
-
-  /**
-   * Validate this property. Does it make sense for this value to be assigned
-   * to this property name? This does not apply the property value
-   *
-   * @return {Boolean} true if the property value is valid, false otherwise.
-   */
-  isValid: function() {
-    return domUtils.cssPropertyIsValid(this.name, this.value);
   }
 };
 
