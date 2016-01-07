fda7cd1817309f69fb59f5108146f930d37c3d88
Tooru Fujisawa <arai_a@mac.com>
2016-01-07 12:30:26 +0900

    Bug 933257 - Part 1: Add a script to import and update fdlibm from FreeBSD. r=jwalden

---
+++ modules/fdlibm/update.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Script to update the mozilla in-tree copy of the fdlibm library.
+# Run this within the /modules/fdlibm directory of the source tree.
+
+set -e
+
+API_BASE_URL=https://api.github.com/repos/freebsd/freebsd
+
+function get_commit {
+    curl -s "${API_BASE_URL}/commits?path=lib/msun/src&per_page=1" \
+        | python -c 'import json, sys; print(json.loads(sys.stdin.read())[0]["sha"])'
+}
+
+rm -rf src
+BEFORE_COMMIT=$(get_commit)
+sh ./import.sh
+COMMIT=$(get_commit)
+if [ ${BEFORE_COMMIT} != ${COMMIT} ]; then
+    echo "Latest commit is changed during import.  Please run again."
+    exit 1
+fi
+for FILE in $(ls patches/*.patch | sort); do
+    patch -p3 < ${FILE}
+done
+hg add src
+
+perl -p -i -e "s/\[commit [0-9a-f]{40}\]/[commit ${COMMIT}]/" README.mozilla
+
+echo "###"
+echo "### Updated fdlibm/src to ${COMMIT}."
+echo "### Remember to verify and commit the changes to source control!"
+echo "###"
