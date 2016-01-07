80d04564e9886c72888bac0e42e008998b163131
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-01-07 19:22:50 +1100

    Bug 1232268: [ffmpeg] P4. Add support for libavcodec 57. r=kentuckyfriedtakahe, a=sylvestre
    At this stage only the original FFmpeg project is supported.
    
    MozReview-Commit-ID: 5r3tRFNcwJM

--- dom/media/platforms/ffmpeg/FFmpegFunctionList.h
+++ dom/media/platforms/ffmpeg/FFmpegFunctionList.h
@@ -31,9 +31,9 @@ AV_FUNC(avcodec_alloc_frame, AV_FUNC_AVUTIL_54)
 AV_FUNC(avcodec_free_frame, AV_FUNC_AVUTIL_54)
 #endif
 #if LIBAVCODEC_VERSION_MAJOR >= 55 || defined(LIBAVCODEC_ALLVERSION)
-/* libavutil v55 only */
-AV_FUNC(av_frame_alloc, AV_FUNC_AVUTIL_55)
-AV_FUNC(av_frame_free, AV_FUNC_AVUTIL_55)
-AV_FUNC(av_frame_unref, AV_FUNC_AVUTIL_55)
+/* libavutil v55 and later only */
+AV_FUNC(av_frame_alloc, (AV_FUNC_AVUTIL_55 | AV_FUNC_AVUTIL_56 | AV_FUNC_AVUTIL_57))
+AV_FUNC(av_frame_free, (AV_FUNC_AVUTIL_55 | AV_FUNC_AVUTIL_56 | AV_FUNC_AVUTIL_57))
+AV_FUNC(av_frame_unref, (AV_FUNC_AVUTIL_55 | AV_FUNC_AVUTIL_56 | AV_FUNC_AVUTIL_57))
 #endif
 #endif
