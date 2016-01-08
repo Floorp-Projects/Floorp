aff410cc337f37d067d4b3a4a5be8c774bc57511
Dragana Damjanovic <dd.mozilla@gmail.com>
2016-01-08 10:20:00 -0500

    Bug 1238017 - Remove ClosingService. r=mcmanus

--- netwerk/base/nsSocketTransport2.cpp
+++ netwerk/base/nsSocketTransport2.cpp
@@ -16,7 +16,6 @@
 #include "nsProxyInfo.h"
 #include "nsNetCID.h"
 #include "nsNetUtil.h"
-#include "ClosingService.h"
 #include "nsAutoPtr.h"
 #include "nsCOMPtr.h"
 #include "plstr.h"
@@ -1324,9 +1323,6 @@ nsSocketTransport::InitiateSocket()
 
     // Attach network activity monitor
     mozilla::net::NetworkActivityMonitor::AttachIOLayer(fd);
-
-    // Attach closing service.
-    ClosingService::AttachIOLayer(fd);
 
     PRStatus status;
 
