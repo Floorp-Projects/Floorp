a5fc4cd03f79701534f411d6b007c43caa960799
Chris Pearce <cpearce@mozilla.com>
2016-01-08 15:43:27 +1300

    Bug 1237836 - Backout 3bae9aee9d8a for test failures. r=backout

--- dom/media/MediaInfo.h
+++ dom/media/MediaInfo.h
@@ -23,17 +23,6 @@ class AudioInfo;
 class VideoInfo;
 class TextInfo;
 
-class MetadataTag {
-public:
-  MetadataTag(const nsACString& aKey,
-              const nsACString& aValue)
-    : mKey(aKey)
-    , mValue(aValue)
-  {}
-  nsCString mKey;
-  nsCString mValue;
-};
-
 class TrackInfo {
 public:
   enum TrackType {
@@ -90,8 +79,6 @@ public:
   int64_t mDuration;
   int64_t mMediaTime;
   CryptoTrack mCrypto;
-
-  nsTArray<MetadataTag> mTags;
 
   // True if the track is gonna be (decrypted)/decoded and
   // rendered directly by non-gecko components.
