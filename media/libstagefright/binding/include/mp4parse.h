483076eb115fd9932c2971409ee59e2791497268
Matthew Gregan <kinetik@flim.org>
2016-01-11 11:02:00 +1300

    Bug 1238420 - Update mp4parse-rust to 0.1.6. r=kinetik Bump default version in the update script and run it against the tagged v0.1.6 upstream repo.

--- media/libstagefright/binding/include/mp4parse.h
+++ media/libstagefright/binding/include/mp4parse.h
@@ -11,6 +11,14 @@ extern "C" {
 
 struct mp4parse_state;
 
+#define MP4PARSE_OK 0
+#define MP4PARSE_ERROR_BADARG -1      // Argument validation failure
+#define MP4PARSE_ERROR_INVALID -2     // Error::InvalidData
+#define MP4PARSE_ERROR_UNSUPPORTED -3 // Error::Unsupported
+#define MP4PARSE_ERROR_EOF -4         // Error::UnexpectedEOF
+#define MP4PARSE_ASSERT -5            // Error::AssertCaught
+#define MP4PARSE_ERROR_IO -6          // Error::Io(_)
+
 #define MP4PARSE_TRACK_TYPE_H264 0 // "video/avc"
 #define MP4PARSE_TRACK_TYPE_AAC  1 // "audio/mp4a-latm"
 
@@ -18,12 +26,6 @@ struct mp4parse_track_audio_info {
   uint16_t channels;
   uint16_t bit_depth;
   uint32_t sample_rate;
-  //int32_t profile;
-  //int32_t extended_profile; // check types
-
-  // TODO(kinetik):
-  // extra_data
-  // codec_specific_config
 };
 
 struct mp4parse_track_video_info {
@@ -31,10 +33,6 @@ struct mp4parse_track_video_info {
   uint32_t display_height;
   uint16_t image_width;
   uint16_t image_height;
-
-  // TODO(kinetik):
-  // extra_data
-  // codec_specific_config
 };
 
 struct mp4parse_track_info {
@@ -42,7 +40,6 @@ struct mp4parse_track_info {
   uint32_t track_id;
   uint64_t duration;
   int64_t media_time;
-  // TODO(kinetik): crypto guff
 };
 
 struct mp4parse_state* mp4parse_new(void);
@@ -50,11 +47,13 @@ void mp4parse_free(struct mp4parse_state* state);
 
 int32_t mp4parse_read(struct mp4parse_state* state, uint8_t *buffer, size_t size);
 
-int32_t mp4parse_get_track_info(struct mp4parse_state* state, int32_t track, struct mp4parse_track_info* track_info);
+uint32_t mp4parse_get_track_count(struct mp4parse_state* state);
 
-int32_t mp4parse_get_track_audio_info(struct mp4parse_state* state, int32_t track, struct mp4parse_track_audio_info* track_info);
+int32_t mp4parse_get_track_info(struct mp4parse_state* state, uint32_t track, struct mp4parse_track_info* track_info);
 
-int32_t mp4parse_get_track_video_info(struct mp4parse_state* state, int32_t track, struct mp4parse_track_video_info* track_info);
+int32_t mp4parse_get_track_audio_info(struct mp4parse_state* state, uint32_t track, struct mp4parse_track_audio_info* track_info);
+
+int32_t mp4parse_get_track_video_info(struct mp4parse_state* state, uint32_t track, struct mp4parse_track_video_info* track_info);
 
 #ifdef __cplusplus
 }
