25c5aadcce7d17253a409f9984ce921bde737ca5
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2016-01-11 12:16:35 -0800

    Bug 1232783 - Convert JS callsites to use open2 within modules/ (r=sicking)

--- modules/libjar/test/unit/test_bug597702.js
+++ modules/libjar/test/unit/test_bug597702.js
@@ -5,7 +5,7 @@
 var Cc = Components.classes;
 var Ci = Components.interfaces;
 var Cu = Components.utils;
-Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/NetUtil.jsm");
 
 // Check that reading non existant inner jars results in the right error
 
@@ -16,31 +16,17 @@ function run_test() {
   var outerJarBase = "jar:" + ios.newFileURI(file).spec + "!/";
   var goodSpec = "jar:" + outerJarBase + "inner.jar!/hello";
   var badSpec = "jar:" + outerJarBase + "jar_that_isnt_in_the.jar!/hello";
-  var goodChannel = ios.newChannel2(goodSpec,
-                                    null,
-                                    null,
-                                    null,      // aLoadingNode
-                                    Services.scriptSecurityManager.getSystemPrincipal(),
-                                    null,      // aTriggeringPrincipal
-                                    Ci.nsILoadInfo.SEC_NORMAL,
-                                    Ci.nsIContentPolicy.TYPE_OTHER);
-  var badChannel = ios.newChannel2(badSpec,
-                                   null,
-                                   null,
-                                   null,      // aLoadingNode
-                                   Services.scriptSecurityManager.getSystemPrincipal(),
-                                   null,      // aTriggeringPrincipal
-                                   Ci.nsILoadInfo.SEC_NORMAL,
-                                   Ci.nsIContentPolicy.TYPE_OTHER);
+  var goodChannel = NetUtil.newChannel({uri: goodSpec, loadUsingSystemPrincipal: true});
+  var badChannel = NetUtil.newChannel({uri: badSpec, loadUsingSystemPrincipal: true});
 
   try {
-    instr = goodChannel.open();
+    instr = goodChannel.open2();
   } catch (e) {
     do_throw("Failed to open file in inner jar");
   }
 
   try {
-    instr = badChannel.open();
+    instr = badChannel.open2();
     do_throw("Failed to report that file doesn't exist");
   } catch (e) {
     do_check_true(e.name == "NS_ERROR_FILE_NOT_FOUND");
--- modules/libjar/test/unit/test_not_found.js
+++ modules/libjar/test/unit/test_not_found.js
@@ -4,23 +4,19 @@ var Cc = Components.classes;
 var Ci = Components.interfaces;
 var Cu = Components.utils;
 
-Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/NetUtil.jsm");
 const path = "data/test_bug333423.zip";
 
 function run_test() {
   var ios = Cc["@mozilla.org/network/io-service;1"].
             getService(Ci.nsIIOService);
   var spec = "jar:" + ios.newFileURI(do_get_file(path)).spec + "!/";
-  var channel = ios.newChannel2(spec + "file_that_isnt_in.archive",
-                                null,
-                                null,
-                                null,      // aLoadingNode
-                                Services.scriptSecurityManager.getSystemPrincipal(),
-                                null,      // aTriggeringPrincipal
-                                Ci.nsILoadInfo.SEC_NORMAL,
-                                Ci.nsIContentPolicy.TYPE_OTHER);
+  var channel = NetUtil.newChannel({
+    uri: spec + "file_that_isnt_in.archive",
+    loadUsingSystemPrincipal: true
+  });
   try {
-    instr = channel.open();
+    instr = channel.open2();
     do_throw("Failed to report that file doesn't exist")
   } catch (e) {
       do_check_true(e.name == "NS_ERROR_FILE_NOT_FOUND")
