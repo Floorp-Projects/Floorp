b98670be1223d1a2b0fdd88a928d5c06f446e4f4
Edgar Chen <echen@mozilla.com>
2016-01-11 22:17:18 +0800

    Bug 1238469 - Part 4: Remove custom mozharness configs from docker image; r=ahal

--- testing/docker/tester/mozharness_configs/remove_executables.py
+++
@@ -1,6 +0,0 @@
-config = {
-    # We bake this directly into the tester image now...
-    "download_minidump_stackwalk": False,
-    "minidump_stackwalk_path": "/usr/local/bin/linux64-minidump_stackwalk",
-    "exes": {}
-}
