fc9cf6f9d5dbada6d102dbc80370d703e24c5f2e
Nicholas Nethercote <nnethercote@mozilla.com>
2016-01-11 16:42:09 -0800

    Bug 1231888 (follow-up) - Simplify CurrentSurface(). r=jrmuizel.
    The previous patch in this bug removed two calls to CurrentSurface(), leaving
    only one, and that one doesn't need the dx and dy arguments.
    
    --HG--
    extra : rebase_source : 52cc3d7a1317ea9f239f59c24a58810ce6669c9f

--- gfx/thebes/gfxContext.h
+++ gfx/thebes/gfxContext.h
@@ -75,15 +75,10 @@ public:
     static already_AddRefed<gfxContext> ContextForDrawTarget(mozilla::gfx::DrawTarget* aTarget);
 
     /**
-     * Return the current transparency group target, if any, along
-     * with its device offsets from the top.  If no group is
-     * active, returns the surface the gfxContext was created with,
-     * and 0,0 in dx,dy.
+     * Return the current transparency group target, if any. If no group is
+     * active, returns the surface the gfxContext was created with.
      */
-    already_AddRefed<gfxASurface> CurrentSurface(gfxFloat *dx, gfxFloat *dy);
-    already_AddRefed<gfxASurface> CurrentSurface() {
-        return CurrentSurface(nullptr, nullptr);
-    }
+    already_AddRefed<gfxASurface> CurrentSurface();
 
     mozilla::gfx::DrawTarget *GetDrawTarget() { return mDT; }
 
