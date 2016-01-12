f569fa65f9d3c0aa8d64f008c226ef487ba277c1
Xidorn Quan <quanxunzhen@gmail.com>
2016-01-12 15:18:47 +1100

    Bug 1238404 - Use 'using' directive instead of having separate Dispatch impl in subclasses of nsIEventTarget. r=froydnj
    --HG--
    extra : source : 76255b192a7d6dae74f19c68653537d5f8f39775

--- netwerk/base/nsStreamTransportService.h
+++ netwerk/base/nsStreamTransportService.h
@@ -21,10 +21,7 @@ public:
     NS_DECL_NSISTREAMTRANSPORTSERVICE
     NS_DECL_NSIEVENTTARGET
     NS_DECL_NSIOBSERVER
-    // missing from NS_DECL_NSIEVENTTARGET because MSVC
-    nsresult Dispatch(nsIRunnable* aEvent, uint32_t aFlags) {
-      return Dispatch(nsCOMPtr<nsIRunnable>(aEvent).forget(), aFlags);
-    }
+    using nsIEventTarget::Dispatch;
 
     nsresult Init();
 
