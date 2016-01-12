7a8bb45336d52b71df4ebf6ec54c8388cf7e884b
Brian Birtles <birtles@gmail.com>
2016-01-13 07:54:55 +0900

    Bug 1232577 part 15 - Drop LastStyleUpdateForAllAnimations flag from pres context; r=heycam
    nsPresContext contains a mLastStyleUpdateForAllAnimations flag which is simply
    used to prevent unnecessarily posting restyles when throttled animations are
    already up to date. Since part 13 we now accurately record whether we have
    posted a restyle for each throttled animation and only post a restyle if we
    have not done so already. As a result, this flag is no longer needed since
    calling PostRestyleForThrottledAnimations is effectively a noop when throttled
    animations are up-to-date.

--- layout/base/nsPresContext.cpp
+++ layout/base/nsPresContext.cpp
@@ -1050,9 +1050,6 @@ nsPresContext::Init(nsDeviceContext* aDeviceContext)
     }
   }
 
-  // Initialise refresh tick counters for OMTA
-  mLastStyleUpdateForAllAnimations = mRefreshDriver->MostRecentRefresh();
-
   // Initialize restyle manager after initializing the refresh driver.
   // Since RestyleManager is also the name of a method of nsPresContext,
   // it is necessary to prefix the class with the mozilla namespace
@@ -1760,24 +1757,6 @@ nsPresContext::Detach()
   if (mShell) {
     mShell->CancelInvalidatePresShellIfHidden();
   }
-}
-
-bool
-nsPresContext::StyleUpdateForAllAnimationsIsUpToDate() const
-{
-  return mLastStyleUpdateForAllAnimations == mRefreshDriver->MostRecentRefresh();
-}
-
-void
-nsPresContext::TickLastStyleUpdateForAllAnimations()
-{
-  mLastStyleUpdateForAllAnimations = mRefreshDriver->MostRecentRefresh();
-}
-
-void
-nsPresContext::ClearLastStyleUpdateForAllAnimations()
-{
-  mLastStyleUpdateForAllAnimations = TimeStamp();
 }
 
 bool
