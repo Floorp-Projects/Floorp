f4865c7977d9705dca910fef92f4bc794aa6b1bf
Alexandre Poirot <poirot.alex@gmail.com>
2016-01-12 01:14:00 -0800

    Bug 1227978 - Simplify usages of Debuggerclient APIs now that all requests returns promises. r=jlongster

--- devtools/client/debugger/test/mochitest/browser_dbg_listtabs-03.js
+++ devtools/client/debugger/test/mochitest/browser_dbg_listtabs-03.js
@@ -9,22 +9,6 @@ const TAB1_URL = EXAMPLE_URL + "doc_empty-tab-01.html";
 
 var gTab1, gTab1Actor, gTab2, gTab2Actor, gClient;
 
-function listTabs() {
-  let deferred = promise.defer();
-
-  gClient.listTabs(aResponse => {
-    deferred.resolve(aResponse.tabs);
-  });
-
-  return deferred.promise;
-}
-
-function request(params) {
-  let deferred = promise.defer();
-  gClient.request(params, deferred.resolve);
-  return deferred.promise;
-}
-
 function test() {
   if (!DebuggerServer.initialized) {
     DebuggerServer.init();
@@ -38,25 +22,26 @@ function test() {
       "Root actor should identify itself as a browser.");
     let tab = yield addTab(TAB1_URL);
 
-    let tabs = yield listTabs();
+    let { tabs } = yield gClient.listTabs();
     is(tabs.length, 2, "Should be two tabs");
     let tabGrip = tabs.filter(a => a.url ==TAB1_URL).pop();
     ok(tabGrip, "Should have an actor for the tab");
 
-    let response = yield request({ to: tabGrip.actor, type: "attach" });
+    let response = yield gClient.request({ to: tabGrip.actor, type: "attach" });
     is(response.type, "tabAttached", "Should have attached");
 
-    tabs = yield listTabs();
+    response = yield gClient.listTabs();
+    tabs = response.tabs;
 
-    response = yield request({ to: tabGrip.actor, type: "detach" });
+    response = yield gClient.request({ to: tabGrip.actor, type: "detach" });
     is(response.type, "detached", "Should have detached");
 
     let newGrip = tabs.filter(a => a.url ==TAB1_URL).pop();
     is(newGrip.actor, tabGrip.actor, "Should have the same actor for the same tab");
 
-    response = yield request({ to: tabGrip.actor, type: "attach" });
+    response = yield gClient.request({ to: tabGrip.actor, type: "attach" });
     is(response.type, "tabAttached", "Should have attached");
-    response = yield request({ to: tabGrip.actor, type: "detach" });
+    response = yield gClient.request({ to: tabGrip.actor, type: "detach" });
     is(response.type, "detached", "Should have detached");
 
     yield removeTab(tab);
