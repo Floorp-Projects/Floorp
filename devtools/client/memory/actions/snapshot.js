07140c59524c0ea41ffcceb276598e0edadb3ca1
Nick Fitzgerald <fitzgen@gmail.com>
2016-01-13 12:27:30 -0800

    Bug 1229229 - Display dominator trees in the memory tool's UI; r=jsantell
    This patch overhauls the memory tool's frontend from being in a state of viewing
    a snapshot's census (the implicit default) or viewing a census diff (a special
    cased kind of thing) to one of three recognized view states: (1) census, (2)
    diffing, or (3) dominator tree. The logic surrounding our current view is more
    explicit now. View option (3) is the new one, whose introduction required those
    clean ups.
    
    Dominator trees are too large to render in full, all at once. Instead, we
    eagerly load and render a few levels deep and then incrementally fetch further
    subtrees as their parents are expanded in the UI. This means that we get new
    Tree component instances across incremental fetches. Before this commit, the
    Tree component stored a bunch of state internally (in this.state rather than
    this.props) and we would lose focus and expansion state across incremental
    fetches. This internal state has been pulled out and made as explicit props,
    which are now managed by actions and reducers just like the rest of the state.

--- devtools/client/memory/actions/snapshot.js
+++ devtools/client/memory/actions/snapshot.js
@@ -4,9 +4,16 @@
 "use strict";
 
 const { assert, reportException } = require("devtools/shared/DevToolsUtils");
-const { censusIsUpToDate, getSnapshot, breakdownEquals, createSnapshot } = require("../utils");
-const { actions, snapshotState: states } = require("../constants");
-const { toggleDiffing } = require("./diffing");
+const {
+  censusIsUpToDate,
+  getSnapshot,
+  breakdownEquals,
+  createSnapshot,
+  dominatorTreeIsComputed,
+} = require("../utils");
+const { actions, snapshotState: states, viewState, dominatorTreeState } = require("../constants");
+const view = require("./view");
+const refresh = require("./refresh");
 
 /**
  * A series of actions are fired from this task to save, read and generate the
@@ -26,6 +33,10 @@ const takeSnapshotAndCensus = exports.takeSnapshotAndCensus = function (front, h
     yield dispatch(readSnapshot(heapWorker, id));
     if (getSnapshot(getState(), id).state === states.READ) {
       yield dispatch(takeCensus(heapWorker, id));
+
+      if (getState().view === viewState.DOMINATOR_TREE) {
+        yield dispatch(computeAndFetchDominatorTree(heapWorker, id));
+      }
     }
   };
 };
@@ -40,11 +51,11 @@ const takeSnapshotAndCensus = exports.takeSnapshotAndCensus = function (front, h
 const selectSnapshotAndRefresh = exports.selectSnapshotAndRefresh = function (heapWorker, id) {
   return function *(dispatch, getState) {
     if (getState().diffing) {
-      dispatch(toggleDiffing());
+      dispatch(view.changeView(viewState.CENSUS));
     }
 
     dispatch(selectSnapshot(id));
-    yield dispatch(refreshSelectedCensus(heapWorker));
+    yield dispatch(refresh.refresh(heapWorker));
   };
 };
 
@@ -57,10 +68,10 @@ const selectSnapshotAndRefresh = exports.selectSnapshotAndRefresh = function (he
 const takeSnapshot = exports.takeSnapshot = function (front) {
   return function *(dispatch, getState) {
     if (getState().diffing) {
-      dispatch(toggleDiffing());
+      dispatch(view.changeView(viewState.CENSUS));
     }
 
-    const snapshot = createSnapshot();
+    const snapshot = createSnapshot(getState());
     const id = snapshot.id;
     dispatch({ type: actions.TAKE_SNAPSHOT_START, snapshot });
     dispatch(selectSnapshot(id));
@@ -197,6 +208,192 @@ const refreshSelectedCensus = exports.refreshSelectedCensus = function (heapWork
 };
 
 /**
+ * Request that the `HeapAnalysesWorker` compute the dominator tree for the
+ * snapshot with the given `id`.
+ *
+ * @param {HeapAnalysesClient} heapWorker
+ * @param {SnapshotId} id
+ *
+ * @returns {Promise<DominatorTreeId>}
+ */
+const computeDominatorTree = exports.computeDominatorTree = function (heapWorker, id) {
+  return function*(dispatch, getState) {
+    const snapshot = getSnapshot(getState(), id);
+    assert(!(snapshot.dominatorTree && snapshot.dominatorTree.dominatorTreeId),
+           "Should not re-compute dominator trees");
+
+    dispatch({ type: actions.COMPUTE_DOMINATOR_TREE_START, id });
+
+    let dominatorTreeId;
+    try {
+      dominatorTreeId = yield heapWorker.computeDominatorTree(snapshot.path);
+    } catch (error) {
+      reportException("actions/snapshot/computeDominatorTree", error);
+      dispatch({ type: actions.DOMINATOR_TREE_ERROR, id, error });
+      return null;
+    }
+
+    dispatch({ type: actions.COMPUTE_DOMINATOR_TREE_END, id, dominatorTreeId });
+    return dominatorTreeId;
+  };
+};
+
+/**
+ * Get the partial subtree, starting from the root, of the
+ * snapshot-with-the-given-id's dominator tree.
+ *
+ * @param {HeapAnalysesClient} heapWorker
+ * @param {SnapshotId} id
+ *
+ * @returns {Promise<DominatorTreeNode>}
+ */
+const fetchDominatorTree = exports.fetchDominatorTree = function (heapWorker, id) {
+  return function*(dispatch, getState) {
+    const snapshot = getSnapshot(getState(), id);
+    assert(dominatorTreeIsComputed(snapshot),
+           "Should have dominator tree model and it should be computed");
+
+    let breakdown;
+    let root;
+    do {
+      breakdown = getState().dominatorTreeBreakdown;
+      assert(breakdown, "Should have a breakdown to describe nodes with.");
+
+      dispatch({ type: actions.FETCH_DOMINATOR_TREE_START, id, breakdown });
+
+      try {
+        root = yield heapWorker.getDominatorTree({
+          dominatorTreeId: snapshot.dominatorTree.dominatorTreeId,
+          breakdown,
+        });
+      } catch (error) {
+        reportException("actions/snapshot/fetchDominatorTree", error);
+        dispatch({ type: actions.DOMINATOR_TREE_ERROR, id, error });
+        return null;
+      }
+    }
+    while (!breakdownEquals(breakdown, getState().dominatorTreeBreakdown));
+
+    dispatch({ type: actions.FETCH_DOMINATOR_TREE_END, id, root });
+    return root;
+  };
+};
+
+/**
+ * Fetch the immediately dominated children represented by the placeholder
+ * `lazyChildren` from snapshot-with-the-given-id's dominator tree.
+ *
+ * @param {HeapAnalysesClient} heapWorker
+ * @param {SnapshotId} id
+ * @param {DominatorTreeLazyChildren} lazyChildren
+ */
+const fetchImmediatelyDominated = exports.fetchImmediatelyDominated = function (heapWorker, id, lazyChildren) {
+  return function*(dispatch, getState) {
+    const snapshot = getSnapshot(getState(), id);
+    assert(snapshot.dominatorTree, "Should have dominator tree model");
+    assert(snapshot.dominatorTree.state === dominatorTreeState.LOADED ||
+           snapshot.dominatorTree.state === dominatorTreeState.INCREMENTAL_FETCHING,
+           "Cannot fetch immediately dominated nodes in a dominator tree unless " +
+           " the dominator tree has already been computed");
+
+    let breakdown;
+    let response;
+    do {
+      breakdown = getState().dominatorTreeBreakdown;
+      assert(breakdown, "Should have a breakdown to describe nodes with.");
+
+      dispatch({ type: actions.FETCH_IMMEDIATELY_DOMINATED_START, id });
+
+      try {
+        response = yield heapWorker.getImmediatelyDominated({
+          dominatorTreeId: snapshot.dominatorTree.dominatorTreeId,
+          breakdown,
+          nodeId: lazyChildren.parentNodeId(),
+          startIndex: lazyChildren.siblingIndex(),
+        });
+      } catch (error) {
+        reportException("actions/snapshot/fetchImmediatelyDominated", error);
+        dispatch({ type: actions.DOMINATOR_TREE_ERROR, id, error });
+        return null;
+      }
+    }
+    while (!breakdownEquals(breakdown, getState().dominatorTreeBreakdown));
+
+    dispatch({
+      type: actions.FETCH_IMMEDIATELY_DOMINATED_END,
+      id,
+      path: response.path,
+      nodes: response.nodes,
+      moreChildrenAvailable: response.moreChildrenAvailable,
+    });
+  };
+};
+
+/**
+ * Compute and then fetch the dominator tree of the snapshot with the given
+ * `id`.
+ *
+ * @param {HeapAnalysesClient} heapWorker
+ * @param {SnapshotId} id
+ *
+ * @returns {Promise<DominatorTreeNode>}
+ */
+const computeAndFetchDominatorTree = exports.computeAndFetchDominatorTree = function (heapWorker, id) {
+  return function*(dispatch, getState) {
+    const dominatorTreeId = yield dispatch(computeDominatorTree(heapWorker, id));
+    if (dominatorTreeId === null) {
+      return null;
+    }
+
+    const root = yield dispatch(fetchDominatorTree(heapWorker, id));
+    if (!root) {
+      return null;
+    }
+
+    return root;
+  };
+};
+
+/**
+ * Update the currently selected snapshot's dominator tree.
+ *
+ * @param {HeapAnalysesClient} heapWorker
+ */
+const refreshSelectedDominatorTree = exports.refreshSelectedDominatorTree = function (heapWorker) {
+  return function*(dispatch, getState) {
+    let snapshot = getState().snapshots.find(s => s.selected);
+    if (!snapshot) {
+      return;
+    }
+
+    if (snapshot.dominatorTree &&
+        !(snapshot.dominatorTree.state === dominatorTreeState.COMPUTED ||
+          snapshot.dominatorTree.state === dominatorTreeState.LOADED ||
+          snapshot.dominatorTree.state === dominatorTreeState.INCREMENTAL_FETCHING)) {
+      return;
+    }
+
+    switch (snapshot.state) {
+      case states.READ:
+      case states.SAVING_CENSUS:
+      case states.SAVED_CENSUS:
+        if (snapshot.dominatorTree) {
+          yield dispatch(fetchDominatorTree(heapWorker, snapshot.id));
+        } else {
+          yield dispatch(computeAndFetchDominatorTree(heapWorker, snapshot.id));
+        }
+        return;
+
+      default:
+        // If there was an error, we can't continue. If we are still saving or
+        // reading the snapshot, then takeSnapshotAndCensus will finish the job
+        // for us.
+        return;
+    }
+  };
+};
+
+/**
  * Select the snapshot with the given id.
  *
  * @param {snapshotId} id
@@ -206,5 +403,85 @@ const selectSnapshot = exports.selectSnapshot = function (id) {
   return {
     type: actions.SELECT_SNAPSHOT,
     id
+  };
+};
+
+/**
+ * Expand the given node in the snapshot's census report.
+ *
+ * @param {CensusTreeNode} node
+ */
+const expandCensusNode = exports.expandCensusNode = function (id, node) {
+  return {
+    type: actions.EXPAND_CENSUS_NODE,
+    id,
+    node,
+  };
+};
+
+/**
+ * Collapse the given node in the snapshot's census report.
+ *
+ * @param {CensusTreeNode} node
+ */
+const collapseCensusNode = exports.collapseCensusNode = function (id, node) {
+  return {
+    type: actions.COLLAPSE_CENSUS_NODE,
+    id,
+    node,
+  };
+};
+
+/**
+ * Focus the given node in the snapshot's census's report.
+ *
+ * @param {SnapshotId} id
+ * @param {DominatorTreeNode} node
+ */
+const focusCensusNode = exports.focusCensusNode = function (id, node) {
+  return {
+    type: actions.FOCUS_CENSUS_NODE,
+    id,
+    node,
+  };
+};
+
+/**
+ * Expand the given node in the snapshot's dominator tree.
+ *
+ * @param {DominatorTreeTreeNode} node
+ */
+const expandDominatorTreeNode = exports.expandDominatorTreeNode = function (id, node) {
+  return {
+    type: actions.EXPAND_DOMINATOR_TREE_NODE,
+    id,
+    node,
+  };
+};
+
+/**
+ * Collapse the given node in the snapshot's dominator tree.
+ *
+ * @param {DominatorTreeTreeNode} node
+ */
+const collapseDominatorTreeNode = exports.collapseDominatorTreeNode = function (id, node) {
+  return {
+    type: actions.COLLAPSE_DOMINATOR_TREE_NODE,
+    id,
+    node,
+  };
+};
+
+/**
+ * Focus the given node in the snapshot's dominator tree.
+ *
+ * @param {SnapshotId} id
+ * @param {DominatorTreeNode} node
+ */
+const focusDominatorTreeNode = exports.focusDominatorTreeNode = function (id, node) {
+  return {
+    type: actions.FOCUS_DOMINATOR_TREE_NODE,
+    id,
+    node,
   };
 };
