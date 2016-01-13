c3cb1295939b09d1431ed25338cc4a83818825ba
Andrea Marchesini <amarchesini@mozilla.com>
2016-01-13 17:30:04 +0000

    Bug 1239280 - HTMLInputElement::GetFiles should work also if 'directory' attribute is set, r=smaug

--- dom/html/HTMLInputElement.cpp
+++ dom/html/HTMLInputElement.cpp
@@ -2487,7 +2487,8 @@ HTMLInputElement::GetFiles()
     return nullptr;
   }
 
-  if (HasAttr(kNameSpaceID_None, nsGkAtoms::directory)) {
+  if (Preferences::GetBool("dom.input.dirpicker", false) &&
+      HasAttr(kNameSpaceID_None, nsGkAtoms::directory)) {
     return nullptr;
   }
 
