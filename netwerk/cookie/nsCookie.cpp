3d7521308f9f3f7e0216b5936c1fcf6d2905ef45
Andrea Marchesini <amarchesini@mozilla.com>
2016-01-14 08:03:50 +0000

    Bug 1199466 - part 1 - Expose originAttributes in nsICookie, r=jduell

--- netwerk/cookie/nsCookie.cpp
+++ netwerk/cookie/nsCookie.cpp
@@ -3,10 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/dom/ToJSValue.h"
+#include "nsAutoPtr.h"
 #include "nsCookie.h"
 #include "nsUTF8ConverterService.h"
 #include <stdlib.h>
-#include "nsAutoPtr.h"
 
 static const int64_t kCookieStaleThreshold = 60 * PR_USEC_PER_SEC; // 1 minute in microseconds
 
@@ -79,7 +80,8 @@ nsCookie::Create(const nsACString &aName,
                  int64_t           aCreationTime,
                  bool              aIsSession,
                  bool              aIsSecure,
-                 bool              aIsHttpOnly)
+                 bool              aIsHttpOnly,
+                 const OriginAttributes& aOriginAttributes)
 {
   // Ensure mValue contains a valid UTF-8 sequence. Otherwise XPConnect will
   // truncate the string after the first invalid octet.
@@ -111,7 +113,8 @@ nsCookie::Create(const nsACString &aName,
   // construct the cookie. placement new, oh yeah!
   return new (place) nsCookie(name, value, host, path, end,
                               aExpiry, aLastAccessed, aCreationTime,
-                              aIsSession, aIsSecure, aIsHttpOnly);
+                              aIsSession, aIsSecure, aIsHttpOnly,
+                              aOriginAttributes);
 }
 
 size_t
@@ -150,6 +153,15 @@ NS_IMETHODIMP nsCookie::GetStatus(nsCookieStatus *aStatus) { *aStatus = 0;      
 NS_IMETHODIMP nsCookie::GetPolicy(nsCookiePolicy *aPolicy) { *aPolicy = 0;              return NS_OK; }
 NS_IMETHODIMP nsCookie::GetCreationTime(int64_t *aCreation){ *aCreation = CreationTime(); return NS_OK; }
 NS_IMETHODIMP nsCookie::GetLastAccessed(int64_t *aTime)    { *aTime = LastAccessed();   return NS_OK; }
+
+NS_IMETHODIMP
+nsCookie::GetOriginAttributes(JSContext *aCx, JS::MutableHandle<JS::Value> aVal)
+{
+  if (NS_WARN_IF(!ToJSValue(aCx, mOriginAttributes, aVal))) {
+    return NS_ERROR_FAILURE;
+  }
+  return NS_OK;
+}
 
 // compatibility method, for use with the legacy nsICookie interface.
 // here, expires == 0 denotes a session cookie.
