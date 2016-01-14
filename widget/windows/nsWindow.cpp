25dae1d7ee39e54375dc9d5b9dddf89b19e5a4b0
Ryan VanderMeulen <ryanvm@gmail.com>
2016-01-14 11:41:29 -0500

    Bug 1239683 - Add nsIFrame.h include to windows/nsWindow.cpp to fix --disable-accessibility bustage. r=jfkthame

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -133,6 +133,7 @@
 #include "mozilla/WindowsVersion.h"
 #include "mozilla/TextEvents.h" // For WidgetKeyboardEvent
 #include "nsThemeConstants.h"
+#include "nsIFrame.h"
 
 #include "nsIGfxInfo.h"
 #include "nsUXThemeConstants.h"
