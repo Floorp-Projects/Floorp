d601c93e4229460c64086031e19db7e52c141b71
David Rajchenbach-Teller <dteller@mozilla.com>
2016-01-15 11:38:03 +0100

    Bug 1224374 - Profiler labels for the top 26-100 chrome hangs;r=BenWa

--- xpcom/io/FileUtilsWin.cpp
+++ xpcom/io/FileUtilsWin.cpp
@@ -36,6 +36,8 @@ bool
 HandleToFilename(HANDLE aHandle, const LARGE_INTEGER& aOffset,
                  nsAString& aFilename)
 {
+  PROFILER_LABEL_FUNC(js::ProfileEntry::Category::NETWORK);
+
   aFilename.Truncate();
   // This implementation is nice because it uses fully documented APIs that
   // are available on all Windows versions that we support.
