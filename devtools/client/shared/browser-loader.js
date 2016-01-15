e64b60457f73138c1e4419015cf410fb44b80fed
Jan Odvarko <odvarko@gmail.com>
2016-01-15 14:15:28 +0100

    Bug 1237253 - Support define in BrowserLoader; r=jlongster r=jryans

--- devtools/client/shared/browser-loader.js
+++ devtools/client/shared/browser-loader.js
@@ -1,9 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
 var { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 
 const loaders = Cu.import("resource://gre/modules/commonjs/toolkit/loader.js", {});
 const { devtools, DevToolsLoader } = Cu.import("resource://devtools/shared/Loader.jsm", {});
 const { joinURI } = devtools.require("devtools/shared/path");
 const BROWSER_BASED_DIRS = [
+  "resource://devtools/client/jsonview",
   "resource://devtools/client/shared/vendor",
   "resource://devtools/client/shared/components",
   "resource://devtools/client/shared/redux"
@@ -54,6 +60,13 @@ function BrowserLoader(baseURI, window) {
       }
 
       return require(uri);
+    },
+    globals: {
+      // Make sure 'define' function exists. This allows reusing AMD modules.
+      define: function(callback) {
+        callback(this.require, this.exports, this.module);
+        return this.exports;
+      }
     }
   };
 
