91fd7e1b5c453d69e312b6e20c9d598d063d87f1
Jonathan Kew <jkew@mozilla.com>
2016-01-18 13:28:26 +0000

    Bug 1240180 - Optimize native theme scaling for the single-monitor case. r=emk

--- widget/windows/nsNativeThemeWin.cpp
+++ widget/windows/nsNativeThemeWin.cpp
@@ -1550,13 +1550,18 @@ AssumeThemePartAndStateAreTransparent(int32_t aPart, int32_t aState)
 // with a different DPI setting from the system's primary monitor, we need to
 // apply scaling to native-themed elements as the Windows theme APIs assume
 // the primary monitor's resolution.
-static double
+static inline double
 GetThemeDpiScaleFactor(nsIFrame* aFrame)
 {
-  double primaryScale =
-    WinUtils::LogToPhysFactor(WinUtils::GetPrimaryMonitor());
-  nsIWidget* rootWidget = aFrame->PresContext()->GetRootWidget();
-  return rootWidget ? rootWidget->GetDefaultScale().scale / primaryScale : 1.0;
+  if (WinUtils::IsPerMonitorDPIAware() && GetSystemMetrics(SM_CMONITORS) > 1) {
+    nsIWidget* rootWidget = aFrame->PresContext()->GetRootWidget();
+    if (rootWidget) {
+      double primaryScale =
+        WinUtils::LogToPhysFactor(WinUtils::GetPrimaryMonitor());
+      return rootWidget->GetDefaultScale().scale / primaryScale;
+    }
+  }
+  return 1.0;
 }
 
 NS_IMETHODIMP
