bdbccf4acab3d3c278d0f7692bc38710b00b073e
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-01-18 14:46:10 +1100

    Bug 1240411: P8. Clean up MediaSource headers. r=cpearce
    Remove redundant virtual keyword and add missing override if any.

--- dom/media/mediasource/MediaSourceDecoder.h
+++ dom/media/mediasource/MediaSourceDecoder.h
@@ -36,10 +36,10 @@ class MediaSourceDecoder : public MediaDecoder
 public:
   explicit MediaSourceDecoder(dom::HTMLMediaElement* aElement);
 
-  virtual MediaDecoder* Clone(MediaDecoderOwner* aOwner) override;
-  virtual MediaDecoderStateMachine* CreateStateMachine() override;
-  virtual nsresult Load(nsIStreamListener**) override;
-  virtual media::TimeIntervals GetSeekable() override;
+  MediaDecoder* Clone(MediaDecoderOwner* aOwner) override;
+  MediaDecoderStateMachine* CreateStateMachine() override;
+  nsresult Load(nsIStreamListener**) override;
+  media::TimeIntervals GetSeekable() override;
   media::TimeIntervals GetBuffered() override;
 
   // We can't do this in the constructor because we don't know what type of
@@ -50,7 +50,7 @@ public:
     mDormantSupported = aSupported;
   }
 
-  virtual RefPtr<ShutdownPromise> Shutdown() override;
+  RefPtr<ShutdownPromise> Shutdown() override;
 
   static already_AddRefed<MediaResource> CreateResource(nsIPrincipal* aPrincipal = nullptr);
 
@@ -60,7 +60,7 @@ public:
   void Ended(bool aEnded);
 
   // Return the duration of the video in seconds.
-  virtual double GetDuration() override;
+  double GetDuration() override;
 
   void SetInitialDuration(int64_t aDuration);
   void SetMediaSourceDuration(double aDuration, MSRangeRemovalAction aAction);
