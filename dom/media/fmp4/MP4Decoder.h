fcaa51310656c70c89dad1c787b04d5aa95956b9
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-01-18 14:33:44 +1100

    Bug 1240411: P4. Clean up readers headers. r=cpearce
    Remove redundant virtual keyword and add missing override if any.

--- dom/media/fmp4/MP4Decoder.h
+++ dom/media/fmp4/MP4Decoder.h
@@ -16,14 +16,14 @@ class MP4Decoder : public MediaDecoder
 public:
   explicit MP4Decoder(MediaDecoderOwner* aOwner);
 
-  virtual MediaDecoder* Clone(MediaDecoderOwner* aOwner) override {
+  MediaDecoder* Clone(MediaDecoderOwner* aOwner) override {
     if (!IsEnabled()) {
       return nullptr;
     }
     return new MP4Decoder(aOwner);
   }
 
-  virtual MediaDecoderStateMachine* CreateStateMachine() override;
+  MediaDecoderStateMachine* CreateStateMachine() override;
 
   // Returns true if aMIMEType is a type that we think we can render with the
   // a MP4 platform decoder backend. If aCodecs is non emtpy, it is filled
