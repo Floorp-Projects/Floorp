21a425a852550ba3e83b69b37d225e2c8e5385d3
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-01-18 13:07:55 +0100

    Backed out changeset a6ce2264d8f9 (bug 1201778)

--- dom/system/gonk/GeolocationUtil.h
+++ dom/system/gonk/GeolocationUtil.h
@@ -7,23 +7,7 @@
 #ifndef GEOLOCATIONUTIL_H
 #define GEOLOCATIONUTIL_H
 
-#include "nsPrintfCString.h"
-#include "mozilla/UniquePtr.h"
-
-namespace mozilla {
-/**
- * NI data encoding scheme
- */
-typedef int GpsNiEncodingType;
-#define GPS_ENC_NONE                   0
-#define GPS_ENC_SUPL_GSM_DEFAULT       1
-#define GPS_ENC_SUPL_UTF8              2
-#define GPS_ENC_SUPL_UCS2              3
-#define GPS_ENC_UNKNOWN                -1
-
 double CalculateDeltaInMeter(double aLat, double aLon, double aLastLat, double aLastLon);
-bool DecodeGsmDefaultToString(UniquePtr<char[]> &aByteArray, nsString &aWideStr);
-nsString DecodeNIString(const char* aMessage, GpsNiEncodingType aEncodingType);
-}
 
 #endif
+
