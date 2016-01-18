e63f018b58aa2b458bec514a767cd6ca8c85334d
Alexandre Poirot <poirot.alex@gmail.com>
2016-01-18 03:10:42 -0800

    Bug 950936 - Add disable autohide button in browser toolbox to help debug panel/popups. r=bgrins

--- devtools/client/framework/toolbox-options.js
+++ devtools/client/framework/toolbox-options.js
@@ -184,6 +184,9 @@ OptionsPanel.prototype = {
       if (this.toolbox.target.isMultiProcess && tool.id === "command-button-tilt") {
         continue;
       }
+      if (!tool.isTargetSupported(this.toolbox.target)) {
+        continue;
+      }
 
       enabledToolbarButtonsBox.appendChild(createCommandCheckbox(tool));
     }
