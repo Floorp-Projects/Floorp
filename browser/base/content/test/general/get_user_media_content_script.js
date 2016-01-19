879b7f7db2b69403ad6ea68a7381c175dde32d85
Florian Quèze <florian@queze.net>
2016-01-19 18:23:48 +0100

    Bug 1239827 - factor out the code that's common between browser_devices_get_user_media.js and browser_devices_get_user_media_in_frame.js, r=felipe.

---
+++ browser/base/content/test/general/get_user_media_content_script.js
@@ -0,0 +1,79 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+XPCOMUtils.defineLazyServiceGetter(this, "MediaManagerService",
+                                   "@mozilla.org/mediaManagerService;1",
+                                   "nsIMediaManagerService");
+
+const kObservedTopics = [
+  "getUserMedia:response:allow",
+  "getUserMedia:revoke",
+  "getUserMedia:response:deny",
+  "getUserMedia:request",
+  "recording-device-events",
+  "recording-window-ended"
+];
+
+var gObservedTopics = {};
+function observer(aSubject, aTopic, aData) {
+  if (!(aTopic in gObservedTopics))
+    gObservedTopics[aTopic] = 1;
+  else
+    ++gObservedTopics[aTopic];
+}
+
+kObservedTopics.forEach(topic => {
+  Services.obs.addObserver(observer, topic, false);
+});
+
+addMessageListener("Test:ExpectObserverCalled", ({data}) => {
+  sendAsyncMessage("Test:ExpectObserverCalled:Reply",
+                   {count: gObservedTopics[data]});
+  if (data in gObservedTopics)
+    --gObservedTopics[data];
+});
+
+addMessageListener("Test:TodoObserverNotCalled", ({data}) => {
+  sendAsyncMessage("Test:TodoObserverNotCalled:Reply",
+                   {count: gObservedTopics[data]});
+  if (gObservedTopics[data] == 1)
+    gObservedTopics[data] = 0;
+});
+
+addMessageListener("Test:ExpectNoObserverCalled", data => {
+  sendAsyncMessage("Test:ExpectNoObserverCalled:Reply", gObservedTopics);
+  gObservedTopics = {};
+});
+
+function _getMediaCaptureState() {
+  let hasVideo = {};
+  let hasAudio = {};
+  MediaManagerService.mediaCaptureWindowState(content, hasVideo, hasAudio);
+  if (hasVideo.value && hasAudio.value)
+    return "CameraAndMicrophone";
+  if (hasVideo.value)
+    return "Camera";
+  if (hasAudio.value)
+    return "Microphone";
+  return "none";
+}
+
+addMessageListener("Test:GetMediaCaptureState", data => {
+  sendAsyncMessage("Test:MediaCaptureState", _getMediaCaptureState());
+});
+
+addMessageListener("Test:WaitForObserverCall", ({data}) => {
+  let topic = data;
+  Services.obs.addObserver(function observer() {
+    sendAsyncMessage("Test:ObserverCalled", topic);
+    Services.obs.removeObserver(observer, topic);
+
+    if (kObservedTopics.indexOf(topic) != -1) {
+      if (!(topic in gObservedTopics))
+        gObservedTopics[topic] = -1;
+      else
+        --gObservedTopics[topic];
+    }
+  }, topic, false);
+});
