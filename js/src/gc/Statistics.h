aba60926fdb6c1661919a515c4a1e6498585de0f
Nick Fitzgerald <fitzgen@gmail.com>
2016-01-19 12:48:22 -0800

    Bug 1233831 - Part 1: Call the callback on nursery collections; r=terrence
    This commit adds the `js::gcstats::Statistics::{begin,end}NurseryCollection`
    methods which calls the nursery collection callback, if present, and
    additionally handles counting minor GCs (which used to be inline in
    js::Nursery::collect).

--- js/src/gc/Statistics.h
+++ js/src/gc/Statistics.h
@@ -191,6 +191,23 @@ struct Statistics
         counts[s]++;
     }
 
+    void beginNurseryCollection(JS::gcreason::Reason reason) {
+        count(STAT_MINOR_GC);
+        if (nurseryCollectionCallback) {
+            (*nurseryCollectionCallback)(runtime,
+                                         JS::GCNurseryProgress::GC_NURSERY_COLLECTION_START,
+                                         reason);
+        }
+    }
+
+    void endNurseryCollection(JS::gcreason::Reason reason) {
+        if (nurseryCollectionCallback) {
+            (*nurseryCollectionCallback)(runtime,
+                                         JS::GCNurseryProgress::GC_NURSERY_COLLECTION_END,
+                                         reason);
+        }
+    }
+
     int64_t beginSCC();
     void endSCC(unsigned scc, int64_t start);
 
