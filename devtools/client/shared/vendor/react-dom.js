8b55567bdc172c62ef62e4aa84c4d45efe5809b4
James Long <longster@gmail.com>
2016-01-19 16:55:06 -0500

    Bug 1224765 - add production version of React for devtools and use it in release modes r=fitzgen

--- devtools/client/shared/vendor/react-dom.js
+++ devtools/client/shared/vendor/react-dom.js
@@ -1,3 +1,14 @@
+/**
+ * ReactDOM v0.14.1
+ *
+ * Copyright 2013-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
 // Based off https://github.com/ForbesLindesay/umd/blob/master/template.js
 ;(function(f) {
   // CommonJS
