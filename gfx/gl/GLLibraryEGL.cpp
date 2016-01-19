26bc832c554aeb89786ebf273745a2b3da600c10
Nicolas Silva <nsilva@mozilla.com>
2016-01-19 20:14:27 +0100

    Bug 1240708 - Various trivial coverity warning fixes (part 2). r=kats

--- gfx/gl/GLLibraryEGL.cpp
+++ gfx/gl/GLLibraryEGL.cpp
@@ -8,6 +8,7 @@
 #include "gfxUtils.h"
 #include "mozilla/Preferences.h"
 #include "mozilla/Assertions.h"
+#include "mozilla/unused.h"
 #include "nsDirectoryServiceDefs.h"
 #include "nsDirectoryServiceUtils.h"
 #include "nsIGfxInfo.h"
@@ -306,8 +307,8 @@ GLLibraryEGL::EnsureInitialized(bool forceAccel)
     };
 
     // Do not warn about the failure to load this - see bug 1092191
-    GLLibraryLoader::LoadSymbols(mEGLLibrary, &optionalSymbols[0], nullptr, nullptr,
-                                 false);
+    Unused << GLLibraryLoader::LoadSymbols(mEGLLibrary, &optionalSymbols[0],
+                                           nullptr, nullptr, false);
 
 #if defined(MOZ_WIDGET_GONK) && ANDROID_VERSION >= 18
     MOZ_RELEASE_ASSERT(mSymbols.fQueryStringImplementationANDROID,
