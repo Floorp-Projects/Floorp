d6ddf19cc73f35fe01f62c8be901582ca98510cf
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-01-21 02:26:49 +1100

    Bug 1240201: [vorbis] P1. Properly determine sample duration and time in webm demuxer. r=kinetik

--- dom/media/webm/WebMDemuxer.h
+++ dom/media/webm/WebMDemuxer.h
@@ -9,8 +9,10 @@
 #include "nsTArray.h"
 #include "MediaDataDemuxer.h"
 #include "NesteggPacketHolder.h"
+#include "mozilla/Maybe.h"
 #include "mozilla/Move.h"
 
+#include "VorbisDecoder.h"
 typedef struct nestegg nestegg;
 
 namespace mozilla {
@@ -199,6 +201,8 @@ private:
   // as nestegg only performs 1-byte read at a time.
   int64_t mLastWebMBlockOffset;
   const bool mIsMediaSource;
+
+  VorbisPacketSampleCounter mVorbisCounter;
 };
 
 class WebMTrackDemuxer : public MediaTrackDemuxer
