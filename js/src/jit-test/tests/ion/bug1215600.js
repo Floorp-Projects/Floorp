b540ee46ba57cba184d4790b1f567fdd47e7f478
Jon Coppeard <jcoppeard@mozilla.com>
2016-01-20 10:13:14 +0000

    Bug 1215600 - Make IonBuilder::testGlobalLexicalBinding() work if there's no type information r=shu

---
+++ js/src/jit-test/tests/ion/bug1215600.js
@@ -0,0 +1,23 @@
+lfcode = Array()
+lfcode.push("5")
+lfcode.push("")
+lfcode.push("3")
+lfcode.push("oomTest(()=>{gc()})")
+for (let i = 0; i < 10; i++) {
+    file = lfcode.shift()
+    loadFile(file)
+}
+function loadFile(lfVarx) {
+    try {
+        if (lfVarx.length != 1)
+            switch (lfRunTypeId) {
+                case 3:
+                    function newFunc(x) Function(x)()
+                    newFunc(lfVarx)
+                case 5:
+                    for (lfLocal in this);
+            }
+        isNaN();
+        lfRunTypeId = parseInt(lfVarx);
+    } catch (lfVare) {}
+}
