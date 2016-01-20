d1ca3ec1827df840f2109c04f64053344bb3917a
Andrea Marchesini <amarchesini@mozilla.com>
2016-01-20 17:25:03 +0000

    Bug 1162658 - Update FormData to match latest spec, r=smaug

--- dom/html/test/formData_test.js
+++ dom/html/test/formData_test.js
@@ -81,7 +81,7 @@ function testFilename() {
   var f = new FormData();
   // Spec says if a Blob (which is not a File) is added, the name parameter is set to "blob".
   f.append("blob", new Blob(["hi"]));
-  is(f.get("blob").name, "blob", "Blob's filename should be blob.");
+  ok(f.get("blob") instanceof Blob, "We should have a blob back.");
 
   // If a filename is passed, that should replace the original.
   f.append("blob2", new Blob(["hi"]), "blob2.txt");
