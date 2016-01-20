2c64459ef85edb63dd76b74fb3641e208996b702
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2016-01-19 22:40:34 -0800

    Bug 1169179 - Run mozscreenshots as a mochitest-browser-chrome test. r=felipe,glandium

---
+++ browser/tools/mozscreenshots/mozscreenshots/extension/bootstrap.js
@@ -0,0 +1,72 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*
+#if 0
+Workaround a build system bug where this file doesn't get packaged if not pre-processed.
+#endif
+*/
+
+"use strict";
+
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+const env = Cc["@mozilla.org/process/environment;1"].getService(Ci.nsIEnvironment);
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/AddonManager.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/Timer.jsm");
+
+XPCOMUtils.defineLazyModuleGetter(this, "TestRunner",
+                                  "chrome://mozscreenshots/content/TestRunner.jsm");
+
+function install(data, reason) {
+  if (!isAppSupported()) {
+    uninstallExtension(data);
+    return;
+  }
+
+  AddonManager.getAddonByID(data.id, function(addon) {
+    // Enable on install in case the user disabled a prior version
+    if (addon) {
+      addon.userDisabled = false;
+    }
+  });
+}
+
+function startup(data, reason) {
+  if (!isAppSupported()) {
+    uninstallExtension(data);
+    return;
+  }
+
+  AddonManager.getAddonByID(data.id, function(addon) {
+    let extensionPath = addon.getResourceURI();
+    TestRunner.init(extensionPath);
+  });
+}
+
+function shutdown(data, reason) { }
+
+function uninstall(data, reason) { }
+
+/**
+ * @return boolean whether the test suite applies to the application.
+ */
+function isAppSupported() {
+  return true;
+}
+
+function uninstallExtension(data) {
+  AddonManager.getAddonByID(data.id, function(addon) {
+    addon.uninstall();
+  });
+}
+
+function startRun() {
+  let env = Cc["@mozilla.org/process/environment;1"]
+              .getService(Ci.nsIEnvironment);
+  let setsEnv = env.get("MOZSCREENSHOTS_SETS");
+  let sets = setsEnv ? setsEnv.split(",") : null;
+  TestRunner.start(sets);
+}
---
+++ browser/tools/mozscreenshots/mozscreenshots/extension/configurations/Toolbars.jsm
@@ -0,0 +1,56 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+this.EXPORTED_SYMBOLS = ["Toolbars"];
+
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/Task.jsm");
+
+this.Toolbars = {
+  init(libDir) {},
+
+  configurations: {
+    onlyNavBar: {
+      applyConfig: Task.async(function*() {
+        let browserWindow = Services.wm.getMostRecentWindow("navigator:browser");
+        let personalToolbar = browserWindow.document.getElementById("PersonalToolbar");
+        browserWindow.setToolbarVisibility(personalToolbar, false);
+        toggleMenubarIfNecessary(false);
+      }),
+    },
+
+    allToolbars: {
+      applyConfig: Task.async(function*() { // Boookmarks and menubar
+        let browserWindow = Services.wm.getMostRecentWindow("navigator:browser");
+        let personalToolbar = browserWindow.document.getElementById("PersonalToolbar");
+        browserWindow.setToolbarVisibility(personalToolbar, true);
+        toggleMenubarIfNecessary(true);
+      }),
+
+      verifyConfig: Task.async(function*() {
+        let browserWindow = Services.wm.getMostRecentWindow("navigator:browser");
+        if (browserWindow.fullScreen) {
+          return Promise.reject("The bookmark toolbar and menubar are not shown in fullscreen.");
+        }
+      }),
+    },
+
+  },
+};
+
+
+///// helpers /////
+
+function toggleMenubarIfNecessary(visible) {
+  let browserWindow = Services.wm.getMostRecentWindow("navigator:browser");
+  // The menubar is not shown on OS X or while in fullScreen
+  if (Services.appinfo.OS != "Darwin" /*&& !browserWindow.fullScreen*/) {
+    let menubar = browserWindow.document.getElementById("toolbar-menubar");
+    browserWindow.setToolbarVisibility(menubar, visible);
+  }
+}
