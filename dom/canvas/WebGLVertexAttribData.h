75ef7d9bcf299bf512fe339f97905dcfd4767f30
Brad Kotsopoulos <bkotsopoulos@mozilla.com>
2016-01-20 11:04:00 -0500

    Bug 1239488 - Add int/uint to vertex attrib data type. r=jgilbert
    --HG--
    extra : amend_source : 0c05e1aebdceec45cd242c6b6fe30f6b6e631feb

--- dom/canvas/WebGLVertexAttribData.h
+++ dom/canvas/WebGLVertexAttribData.h
@@ -52,12 +52,18 @@ struct WebGLVertexAttribData
         case LOCAL_GL_UNSIGNED_SHORT:
             return sizeof(GLushort);
 
+        case LOCAL_GL_INT:
+            return sizeof(GLint);
+
+        case LOCAL_GL_UNSIGNED_INT:
+            return sizeof(GLuint);
+
         // case LOCAL_GL_FIXED:
         case LOCAL_GL_FLOAT:
             return sizeof(GLfloat);
 
         default:
-            NS_ERROR("Should never get here!");
+            MOZ_ASSERT(false, "Should never get here!");
             return 0;
         }
     }
