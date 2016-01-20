b8e3b9e52b0b3b5677a9852e06f9364da4ad2138
Ting-Yu Lin <tlin@mozilla.com>
2016-01-20 23:20:40 +0800

    Bug 591737 - Add HTMLDetailsElement and webidl interface. r=bz
    --HG--
    extra : commitid : y0Jiyg8dom
    extra : rebase_source : 0945c6144e4a84ed3542c90b1cbd637d2b9d2b14

---
+++ dom/html/HTMLDetailsElement.cpp
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/HTMLDetailsElement.h"
+
+#include "mozilla/dom/HTMLDetailsElementBinding.h"
+
+NS_IMPL_NS_NEW_HTML_ELEMENT(Details)
+
+namespace mozilla {
+namespace dom {
+
+HTMLDetailsElement::~HTMLDetailsElement()
+{
+}
+
+NS_IMPL_ELEMENT_CLONE(HTMLDetailsElement)
+
+nsChangeHint
+HTMLDetailsElement::GetAttributeChangeHint(const nsIAtom* aAttribute,
+                                           int32_t aModType) const
+{
+  nsChangeHint hint =
+    nsGenericHTMLElement::GetAttributeChangeHint(aAttribute, aModType);
+  if (aAttribute == nsGkAtoms::open) {
+    NS_UpdateHint(hint, nsChangeHint_ReconstructFrame);
+  }
+  return hint;
+}
+
+JSObject*
+HTMLDetailsElement::WrapNode(JSContext* aCx, JS::Handle<JSObject*> aGivenProto)
+{
+  return HTMLDetailsElementBinding::Wrap(aCx, this, aGivenProto);
+}
+
+} // namespace dom
+} // namespace mozilla
