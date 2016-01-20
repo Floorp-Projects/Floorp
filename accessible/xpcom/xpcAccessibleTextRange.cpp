356a8b6d75cd57dce785dee63062e1f5165322f1
Alexander Surkov <surkov.alexander@gmail.com>
2016-01-20 12:53:03 -0500

    Bug 1233118 - implement IAccessible2_3::selectionRanges, r=yzen

--- accessible/xpcom/xpcAccessibleTextRange.cpp
+++ accessible/xpcom/xpcAccessibleTextRange.cpp
@@ -6,7 +6,7 @@
 
 #include "xpcAccessibleTextRange.h"
 
-#include "TextRange.h"
+#include "TextRange-inl.h"
 #include "xpcAccessibleDocument.h"
 
 #include "nsIMutableArray.h"
@@ -167,6 +167,16 @@ xpcAccessibleTextRange::MoveEnd(uint32_t aUnit, int32_t aCount)
 NS_IMETHODIMP
 xpcAccessibleTextRange::Normalize(uint32_t aUnit)
 {
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+xpcAccessibleTextRange::Crop(nsIAccessible* aContainer, bool* aSuccess)
+{
+  Accessible* container = aContainer->ToInternalAccessible();
+  NS_ENSURE_TRUE(container, NS_ERROR_INVALID_ARG);
+
+  *aSuccess = mRange.Crop(container);
   return NS_OK;
 }
 
