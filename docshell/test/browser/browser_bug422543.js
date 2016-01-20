83748b1873d8245e3685d0ab86dacf2a4f2f5105
Blake Kaplan <mrbkap@gmail.com>
2016-01-20 14:14:00 +0100

    Bug 1239122 - Make docshell browser-chrome tests e10s compatible. r=billm

--- docshell/test/browser/browser_bug422543.js
+++ docshell/test/browser/browser_bug422543.js
@@ -1,174 +1,116 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-
-function SHistoryListener() {
-}
-
-SHistoryListener.prototype = {
-  retval: true,
-  last: "initial",
-
-  OnHistoryNewEntry: function (aNewURI) {
-    this.last = "newentry";
-  },
-
-  OnHistoryGoBack: function (aBackURI) {
-    this.last = "goback";
-    return this.retval;
-  },
-
-  OnHistoryGoForward: function (aForwardURI) {
-    this.last = "goforward";
-    return this.retval;
-  },
-
-  OnHistoryGotoIndex: function (aIndex, aGotoURI) {
-    this.last = "gotoindex";
-    return this.retval;
-  },
-
-  OnHistoryPurge: function (aNumEntries) {
-    this.last = "purge";
-    return this.retval;
-  },
-
-  OnHistoryReload: function (aReloadURI, aReloadFlags) {
-    this.last = "reload";
-    return this.retval;
-  },
-
-  OnHistoryReplaceEntry: function (aIndex) {},
-
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsISHistoryListener,
-                                         Ci.nsISupportsWeakReference])
-};
-
-var gFirstListener = new SHistoryListener();
-var gSecondListener = new SHistoryListener();
-
-function test() {
-  TestRunner.run();
-}
-
-function runTests() {
+add_task(function* runTests() {
   yield setup();
   let browser = gBrowser.selectedBrowser;
-  checkListeners("initial", "listeners initialized");
+  // Now that we're set up, initialize our frame script.
+  yield checkListeners("initial", "listeners initialized");
 
   // Check if all history listeners are always notified.
   info("# part 1");
-  browser.loadURI("http://www.example.com/");
-  yield whenPageShown(browser);
-  checkListeners("newentry", "shistory has a new entry");
+  yield whenPageShown(browser, () => browser.loadURI("http://www.example.com/"));
+  yield checkListeners("newentry", "shistory has a new entry");
   ok(browser.canGoBack, "we can go back");
 
-  browser.goBack();
-  yield whenPageShown(browser);
-  checkListeners("goback", "back to the first shentry");
+  yield whenPageShown(browser, () => browser.goBack());
+  yield checkListeners("goback", "back to the first shentry");
   ok(browser.canGoForward, "we can go forward");
 
-  browser.goForward();
-  yield whenPageShown(browser);
-  checkListeners("goforward", "forward to the second shentry");
+  yield whenPageShown(browser, () => browser.goForward());
+  yield checkListeners("goforward", "forward to the second shentry");
 
-  browser.reload();
-  yield whenPageShown(browser);
-  checkListeners("reload", "current shentry reloaded");
+  yield whenPageShown(browser, () => browser.reload());
+  yield checkListeners("reload", "current shentry reloaded");
 
-  browser.gotoIndex(0);
-  yield whenPageShown(browser);
-  checkListeners("gotoindex", "back to the first index");
+  yield whenPageShown(browser, () => browser.gotoIndex(0));
+  yield checkListeners("gotoindex", "back to the first index");
 
   // Check nsISHistoryInternal.notifyOnHistoryReload
   info("# part 2");
-  ok(notifyReload(), "reloading has not been canceled");
-  checkListeners("reload", "saw the reload notification");
+  ok((yield notifyReload()), "reloading has not been canceled");
+  yield checkListeners("reload", "saw the reload notification");
 
   // Let the first listener cancel the reload action.
   info("# part 3");
-  resetListeners();
-  gFirstListener.retval = false;
-  ok(!notifyReload(), "reloading has been canceled");
-  checkListeners("reload", "saw the reload notification");
+  yield resetListeners();
+  yield setListenerRetval(0, false);
+  ok(!(yield notifyReload()), "reloading has been canceled");
+  yield checkListeners("reload", "saw the reload notification");
 
   // Let both listeners cancel the reload action.
   info("# part 4");
-  resetListeners();
-  gSecondListener.retval = false;
-  ok(!notifyReload(), "reloading has been canceled");
-  checkListeners("reload", "saw the reload notification");
+  yield resetListeners();
+  yield setListenerRetval(1, false);
+  ok(!(yield notifyReload()), "reloading has been canceled");
+  yield checkListeners("reload", "saw the reload notification");
 
   // Let the second listener cancel the reload action.
   info("# part 5");
-  resetListeners();
-  gFirstListener.retval = true;
-  ok(!notifyReload(), "reloading has been canceled");
-  checkListeners("reload", "saw the reload notification");
-}
+  yield resetListeners();
+  yield setListenerRetval(0, true);
+  ok(!(yield notifyReload()), "reloading has been canceled");
+  yield checkListeners("reload", "saw the reload notification");
+});
 
-function checkListeners(aLast, aMessage) {
-  is(gFirstListener.last, aLast, aMessage);
-  is(gSecondListener.last, aLast, aMessage);
-}
-
-function resetListeners() {
-  gFirstListener.last = gSecondListener.last = "initial";
-}
-
-function notifyReload() {
-  let browser = gBrowser.selectedBrowser;
-  let shistory = browser.docShell.sessionHistory;
-  shistory.QueryInterface(Ci.nsISHistoryInternal);
-  return shistory.notifyOnHistoryReload(browser.currentURI, 0);
-}
-
-function setup(aCallback) {
-  let tab = gBrowser.selectedTab = gBrowser.addTab("about:mozilla");
-  let browser = tab.linkedBrowser;
-  registerCleanupFunction(function () { gBrowser.removeTab(tab); });
-
-  whenPageShown(browser, function () {
-    gFirstListener = new SHistoryListener();
-    gSecondListener = new SHistoryListener();
-
-    let shistory = browser.docShell.sessionHistory;
-    shistory.addSHistoryListener(gFirstListener);
-    shistory.addSHistoryListener(gSecondListener);
-
-    registerCleanupFunction(function () {
-      shistory.removeSHistoryListener(gFirstListener);
-      shistory.removeSHistoryListener(gSecondListener);
+function listenOnce(message, arg = {}) {
+  return new Promise(resolve => {
+    let mm = gBrowser.selectedBrowser.messageManager;
+    mm.addMessageListener(message + ":return", function listener(msg) {
+      mm.removeMessageListener(message + ":return", listener);
+      resolve(msg.data);
     });
 
-    (aCallback || TestRunner.next)();
+    mm.sendAsyncMessage(message, arg);
   });
 }
 
-function whenPageShown(aBrowser, aCallback) {
-  aBrowser.addEventListener("pageshow", function onLoad() {
-    aBrowser.removeEventListener("pageshow", onLoad, true);
-    executeSoon(aCallback || TestRunner.next);
-  }, true);
+function checkListeners(aLast, aMessage) {
+  return listenOnce("bug422543:getListenerStatus").then((listenerStatuses) => {
+    is(listenerStatuses[0], aLast, aMessage);
+    is(listenerStatuses[1], aLast, aMessage);
+  });
 }
 
-var TestRunner = {
-  run: function () {
-    waitForExplicitFinish();
-    this._iter = runTests();
-    this.next();
-  },
+function resetListeners() {
+  return listenOnce("bug422543:resetListeners");
+}
 
-  next: function () {
-    try {
-      TestRunner._iter.next();
-    } catch (e if e instanceof StopIteration) {
-      TestRunner.finish();
-    }
-  },
+function notifyReload() {
+  return listenOnce("bug422543:notifyReload").then(({ rval }) => {
+    return rval;
+  });
+}
 
-  finish: function () {
-    finish();
-  }
-};
+function setListenerRetval(num, val) {
+  return listenOnce("bug422543:setRetval", { num, val });
+}
+
+function setup() {
+  return BrowserTestUtils.openNewForegroundTab(gBrowser,
+                                               "http://mochi.test:8888")
+                         .then(function (tab) {
+    let browser = tab.linkedBrowser;
+    registerCleanupFunction(function* () {
+      yield listenOnce("bug422543:cleanup");
+      gBrowser.removeTab(tab);
+    });
+
+    browser.messageManager
+           .loadFrameScript(getRootDirectory(gTestPath) + "file_bug422543_script.js", false);
+  });
+}
+
+function whenPageShown(aBrowser, aNavigation) {
+  let listener = ContentTask.spawn(aBrowser, null, function () {
+    return new Promise(resolve => {
+      addEventListener("pageshow", function onLoad() {
+        removeEventListener("pageshow", onLoad, true);
+        resolve();
+      }, true);
+    });
+  });
+
+  aNavigation();
+  return listener;
+}
