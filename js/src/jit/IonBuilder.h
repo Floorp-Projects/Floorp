ac69269f0026a564cec2588b538fbd42f0f2f2b7
Jakob Stoklund Olesen <jolesen@mozilla.com>
2016-01-20 12:50:07 -0800

    Bug 1238679 - Implement main SIMD inlining dispatch. r=bbouvier
    Use a shared inlineSimd() dispatch function for all the SIMD types supported by
    Ion. This function can dispatch on the .nativeOp stored in the JSJitInfo struct,
    and we avoid comparing native function pointers that way.
    
    Delete the old per-type dispatch functions.

--- js/src/jit/IonBuilder.h
+++ js/src/jit/IonBuilder.h
@@ -854,7 +854,7 @@ class IonBuilder
     // SIMD intrinsics and natives.
     InliningStatus inlineConstructSimdObject(CallInfo& callInfo, SimdTypeDescr* target);
 
-    // SIMD helpers
+    // SIMD helpers.
     static MIRType SimdTypeDescrToMIRType(SimdTypeDescr::Type type);
     bool canInlineSimd(CallInfo& callInfo, JSNative native, unsigned numArgs,
                        InlineTypedObject** templateObj);
@@ -862,9 +862,7 @@ class IonBuilder
                                        InlineTypedObject* templateObj);
     MDefinition* convertToBooleanSimdLane(MDefinition* scalar);
 
-    InliningStatus inlineSimdInt32x4(CallInfo& callInfo, JSNative native);
-    InliningStatus inlineSimdFloat32x4(CallInfo& callInfo, JSNative native);
-    InliningStatus inlineSimdBool32x4(CallInfo& callInfo, JSNative native);
+    InliningStatus inlineSimd(CallInfo& callInfo, JSFunction* target, MIRType simdType);
 
     template <typename T>
     InliningStatus inlineSimdBinary(CallInfo& callInfo, JSNative native,
