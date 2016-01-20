2c2030d252cf2549484509861d53bdcb19b64e32
Julian Descottes <jdescottes@mozilla.com>
2016-01-20 23:21:54 +0100

    Bug 1241070 - refresh dominatortreeitem if expand state changed;r=fitzgen
    In DominatorTreeItem check props.expanded in shouldComponentUpdate.
    Added integration/sanity test to check that a node can be collapsed
    and expanded using mouse events.
    Added a DominatorTree component unit test to check the expanded nodes
    are correctly updated.
    
    --HG--
    extra : rebase_source : 35eeca14296f81b6c8deec1d375b2a98467a460f

--- devtools/client/memory/test/chrome/head.js
+++ devtools/client/memory/test/chrome/head.js
@@ -174,12 +174,18 @@ function onNextAnimationFrame(fn) {
       requestAnimationFrame(fn));
 }
 
-function renderComponent(component, container) {
+/**
+ * Render the provided ReactElement in the provided HTML container.
+ * Returns a Promise that will resolve the rendered element as a React
+ * component.
+ */
+function renderComponent(element, container) {
   return new Promise(resolve => {
-    ReactDOM.render(component, container, onNextAnimationFrame(() => {
-      dumpn("Rendered = " + container.innerHTML);
-      resolve();
-    }));
+    let component = ReactDOM.render(element, container,
+      onNextAnimationFrame(() => {
+        dumpn("Rendered = " + container.innerHTML);
+        resolve(component);
+      }));
   });
 }
 
