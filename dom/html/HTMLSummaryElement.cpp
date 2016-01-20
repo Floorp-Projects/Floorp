f18f7f5e1581147d195a5f990b9f4c7249ad7caf
Ting-Yu Lin <tlin@mozilla.com>
2016-01-20 23:20:40 +0800

    Bug 591737 - Add HTMLSummaryElement. r=bz

---
+++ dom/html/HTMLSummaryElement.cpp
@@ -0,0 +1,28 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/HTMLSummaryElement.h"
+
+#include "mozilla/dom/HTMLElementBinding.h"
+
+NS_IMPL_NS_NEW_HTML_ELEMENT(Summary)
+
+namespace mozilla {
+namespace dom {
+
+HTMLSummaryElement::~HTMLSummaryElement()
+{
+}
+
+NS_IMPL_ELEMENT_CLONE(HTMLSummaryElement)
+
+JSObject*
+HTMLSummaryElement::WrapNode(JSContext* aCx, JS::Handle<JSObject*> aGivenProto)
+{
+  return HTMLElementBinding::Wrap(aCx, this, aGivenProto);
+}
+
+} // namespace dom
+} // namespace mozilla
