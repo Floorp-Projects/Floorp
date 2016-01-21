5b18d8ad8cac648a900820a51d1ba5b75b557ff4
David Burns <dburns@mozilla.com>
2016-01-21 15:49:40 +0000

    Bug 1241067: Handle Invalid Selector values when finding elements r=ato
    This allows us to now handle errors and return the appropriate
    errors to the clients
    
    --HG--
    extra : rebase_source : 948dc82a6d36d97b69ef2f0ef43b8390c35616aa

--- testing/marionette/elements.js
+++ testing/marionette/elements.js
@@ -344,8 +344,16 @@ ElementManager.prototype = {
     if (this.elementStrategies.indexOf(values.using) < 0) {
       throw new InvalidSelectorError("No such strategy: " + values.using);
     }
-    let found = all ? this.findElements(values.using, values.value, rootNode, startNode) :
+    if (values.value == null) {
+      throw new InvalidSelectorError("Invalid selector value of null was used.");
+    }
+    let found;
+    try {
+      found = all ? this.findElements(values.using, values.value, rootNode, startNode) :
                       this.findElement(values.using, values.value, rootNode, startNode);
+    } catch (e) {
+      throw new InvalidSelectorError(`Given ${values.using} expression "${values.value}" is invalid`);
+    }
     let type = Object.prototype.toString.call(found);
     let isArrayLike = ((type == '[object Array]') || (type == '[object HTMLCollection]') || (type == '[object NodeList]'));
     if (found == null || (isArrayLike && found.length <= 0)) {
