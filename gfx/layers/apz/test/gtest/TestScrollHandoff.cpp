0f5f481663c2c6059f9ce60b0dda4ed2f2f9c6b2
Botond Ballo <botond@mozilla.com>
2016-01-20 20:12:41 -0500

    Bug 1240202 - Change input event generation utilities used by APZ gtests to take points instead of loose coordinates. r=kats
    --HG--
    extra : source : e255bee429c46aca8269d9021bac8a16ba1e4e2a

--- gfx/layers/apz/test/gtest/TestScrollHandoff.cpp
+++ gfx/layers/apz/test/gtest/TestScrollHandoff.cpp
@@ -283,7 +283,7 @@ TEST_F(APZScrollHandoffTester, PartialFlingHandoff) {
   // Fling up and to the left. The child APZC has room to scroll up, but not
   // to the left, so the horizontal component of the fling should be handed
   // off to the parent APZC.
-  Pan(manager, mcc, ScreenPoint(90, 90), ScreenPoint(55, 55));
+  Pan(manager, mcc, ScreenIntPoint(90, 90), ScreenIntPoint(55, 55));
 
   RefPtr<TestAsyncPanZoomController> parent = ApzcOf(root);
   RefPtr<TestAsyncPanZoomController> child = ApzcOf(layers[1]);
