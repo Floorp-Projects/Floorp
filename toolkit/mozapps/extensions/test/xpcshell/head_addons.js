0fbb54b5d882a73bb46257bfbb51cc9d10d9458b
Dave Townsend <dtownsend@oxymoronical.com>
2016-01-21 11:52:01 -0800

    Bug 1237820: Track whether a user has been offered a sideloaded add-on or not. r=rhelmer
    Previously we just checked every newly sideloaded add-on to decide whether to
    offer it to the user for opt-in. This adds a new "seen" property (naming could
    be better if you have other suggestions) which tracks whether we've ever shown
    the opt-in UI for the add-on. It defaults to true for all add-ons and is only
    set to false for sideloaded add-ons that default to being disabled on install.
    The seen flag can be set to true through the API but cannot be reset to false
    as that would allow add-ons to forcibly re-present themselves to the user when
    disabled.
    
    The opt-in UI sets the seen flag to true only when it has focus which fixes a
    long-standing bug where if you accept the first add-on you see and restart the
    other tabs might not show up.
    
    The one slight downside of this approach is that it now requires loading the
    full add-ons database on every startup in order to check the seen flag for all
    installed add-ons. There are hacky ways we might get around this but they all
    involve overloading prefs with even more object data. The good thing is that
    we do the load and check asynchronously after most of startup is complete and
    the UI is fully loaded so there shouldn't be any percieved impact to startup
    time. I've run multiple talos runs to verify that none of the numbers appear to
    regress.
    
    --HG--
    extra : rebase_source : 4fceb4d9af17c4401aa8a06a18f4d8ba11d932bb

--- toolkit/mozapps/extensions/test/xpcshell/head_addons.js
+++ toolkit/mozapps/extensions/test/xpcshell/head_addons.js
@@ -1939,9 +1939,11 @@ function do_exception_wrap(func) {
 /**
  * Change the schema version of the JSON extensions database
  */
-function changeXPIDBVersion(aNewVersion) {
+function changeXPIDBVersion(aNewVersion, aMutator = undefined) {
   let jData = loadJSON(gExtensionsJSON);
   jData.schemaVersion = aNewVersion;
+  if (aMutator)
+    aMutator(jData);
   saveJSON(jData, gExtensionsJSON);
 }
 
