ec34a64efef91138e39fc7715ce08c5bfb040aa5
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-01-21 12:25:20 +0100

    Backed out changeset 8737105685f1 (bug 1003860)

--- devtools/client/storage/ui.js
+++ devtools/client/storage/ui.js
@@ -341,16 +341,11 @@ StorageUI.prototype = {
   populateStorageTree: function(storageTypes) {
     this.storageTypes = {};
     for (let type in storageTypes) {
-      // Ignore `from` field, which is just a protocol.js implementation artifact
       if (type === "from") {
         continue;
       }
-      let typeLabel = type;
-      try {
-        typeLabel = L10N.getStr("tree.labels." + type);
-      } catch(e) {
-        console.error("Unable to localize tree label type:" + type);
-      }
+
+      let typeLabel = L10N.getStr("tree.labels." + type);
       this.tree.add([{id: type, label: typeLabel, type: "store"}]);
       if (!storageTypes[type].hosts) {
         continue;
@@ -573,12 +568,7 @@ StorageUI.prototype = {
       if (!uniqueKey) {
         this.table.uniqueId = uniqueKey = key;
       }
-      columns[key] = key;
-      try {
-        columns[key] = L10N.getStr("table.headers." + type + "." + key);
-      } catch(e) {
-        console.error("Unable to localize table header type:" + type + " key:" + key);
-      }
+      columns[key] = L10N.getStr("table.headers." + type + "." + key);
     }
     this.table.setColumns(columns, null, HIDDEN_COLUMNS);
     this.shouldResetColumns = false;
