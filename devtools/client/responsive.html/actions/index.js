c321276a73a378acc170034efc6bf97b99733f6a
J. Ryan Stinnett <jryans@gmail.com>
2016-01-22 16:17:26 -0600

    Bug 1239437 - Stir in React and Redux. r=pbrosset,jlongster

---
+++ devtools/client/responsive.html/actions/index.js
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// This file lists all of the actions available in responsive design.  This
+// central list of constants makes it easy to see all possible action names at
+// a glance.  Please add a comment with each new action type.
+
+createEnum([
+
+  // The location of the page has changed.  This may be triggered by the user
+  // directly entering a new URL, navigating with links, etc.
+  "CHANGE_LOCATION",
+
+  // Add an additional viewport to display the document.
+  "ADD_VIEWPORT",
+
+], module.exports);
+
+/**
+ * Create a simple enum-like object with keys mirrored to values from an array.
+ * This makes comparison to a specfic value simpler without having to repeat and
+ * mis-type the value.
+ */
+function createEnum(array, target) {
+  for (let key of array) {
+    target[key] = key;
+  }
+  return target;
+}
---
+++ devtools/client/responsive.html/app.js
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { createClass, createFactory, PropTypes } =
+  require("devtools/client/shared/vendor/react");
+const { connect } = require("devtools/client/shared/vendor/react-redux");
+
+const Types = require("./types");
+const Viewports = createFactory(require("./components/viewports"));
+
+let App = createClass({
+
+  displayName: "App",
+
+  propTypes: {
+    location: Types.location.isRequired,
+    viewports: PropTypes.arrayOf(PropTypes.shape(Types.viewport)).isRequired,
+  },
+
+  render() {
+    let {
+      location,
+      viewports,
+    } = this.props;
+
+    // For the moment, the app is just the viewports.  This seems likely to
+    // change assuming we add a global toolbar or something similar.
+    return Viewports({
+      location,
+      viewports,
+    });
+  },
+
+});
+
+module.exports = connect(state => state)(App);
