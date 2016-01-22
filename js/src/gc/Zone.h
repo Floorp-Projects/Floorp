8f351d9a621790fc208c8f7c51812cd7fd669bf4
Terrence Cole <terrence@mozilla.com>
2016-01-22 10:41:39 -0800

    Bug 1224038 - Part 1: Add infallible versions of uid and hashcode getters; r=sfink

--- js/src/gc/Zone.h
+++ js/src/gc/Zone.h
@@ -360,12 +360,16 @@ struct Zone : public JS::shadow::Zone,
     unsigned gcLastZoneGroupIndex;
 #endif
 
+    static js::HashNumber UniqueIdToHash(uint64_t uid) {
+        return js::HashNumber(uid >> 32) ^ js::HashNumber(uid & 0xFFFFFFFF);
+    }
+
     // Creates a HashNumber based on getUniqueId. Returns false on OOM.
     MOZ_WARN_UNUSED_RESULT bool getHashCode(js::gc::Cell* cell, js::HashNumber* hashp) {
         uint64_t uid;
         if (!getUniqueId(cell, &uid))
             return false;
-        *hashp = js::HashNumber(uid >> 32) ^ js::HashNumber(uid & 0xFFFFFFFF);
+        *hashp = UniqueIdToHash(uid);
         return true;
     }
 
@@ -390,10 +394,19 @@ struct Zone : public JS::shadow::Zone,
         // If the cell was in the nursery, hopefully unlikely, then we need to
         // tell the nursery about it so that it can sweep the uid if the thing
         // does not get tenured.
+        return runtimeFromAnyThread()->gc.nursery.addedUniqueIdToCell(cell);
+    }
+
+    js::HashNumber getHashCodeInfallible(js::gc::Cell* cell) {
+        return UniqueIdToHash(getUniqueIdInfallible(cell));
+    }
+
+    uint64_t getUniqueIdInfallible(js::gc::Cell* cell) {
+        uint64_t uid;
         js::AutoEnterOOMUnsafeRegion oomUnsafe;
-        if (!runtimeFromAnyThread()->gc.nursery.addedUniqueIdToCell(cell))
-            oomUnsafe.crash("failed to allocate tracking data for a nursery uid");
-        return true;
+        if (!getUniqueId(cell, &uid))
+            oomUnsafe.crash("failed to allocate uid");
+        return uid;
     }
 
     // Return true if this cell has a UID associated with it.
