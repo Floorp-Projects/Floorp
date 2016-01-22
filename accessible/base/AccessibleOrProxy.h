8d47572098bc2439a3d53a3c3b1cca61ea0a968e
Trevor Saunders <tbsaunde@tbsaunde.org>
2016-01-22 04:04:08 -0500

    bug 1243077 - add AccessibleOrProxy::ChildCount() r=davidb

--- accessible/base/AccessibleOrProxy.h
+++ accessible/base/AccessibleOrProxy.h
@@ -50,6 +50,15 @@ public:
 
   bool IsNull() const { return mBits == 0; }
 
+  uint32_t ChildCount() const
+  {
+    if (IsProxy()) {
+      return AsProxy()->ChildrenCount();
+    }
+
+    return AsAccessible()->ChildCount();
+  }
+
   // XXX these are implementation details that ideally would not be exposed.
   uintptr_t Bits() const { return mBits; }
   void SetBits(uintptr_t aBits) { mBits = aBits; }
