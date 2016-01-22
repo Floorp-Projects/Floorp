a4aa15ddff4cdc91e09f053a815183af3b2337dc
Terrence Cole <terrence@mozilla.com>
2016-01-22 10:41:43 -0800

    Bug 1224038 - Part 2: Use stable hashing for NewTable; r=sfink
    --HG--
    extra : rebase_source : 03157a3dcd4b2f9eb26fd33cb7bdb9805bbcb5fb

--- js/src/vm/TaggedProto.h
+++ js/src/vm/TaggedProto.h
@@ -44,6 +44,9 @@ class TaggedProto
     bool operator ==(const TaggedProto& other) const { return proto == other.proto; }
     bool operator !=(const TaggedProto& other) const { return proto != other.proto; }
 
+    HashNumber hashCode() const;
+    uint64_t uniqueId() const;
+
     void trace(JSTracer* trc) {
         if (isObject())
             TraceManuallyBarrieredEdge(trc, &proto, "TaggedProto");
@@ -83,6 +86,8 @@ class TaggedProtoOperations
     inline JSObject* toObject() const { return value().toObject(); }
     inline JSObject* toObjectOrNull() const { return value().toObjectOrNull(); }
     JSObject* raw() const { return value().raw(); }
+    HashNumber hashCode() const { return value().hashCode(); }
+    uint64_t uniqueId() const { return value().uniqueId(); }
 };
 
 template <>
