588cfe72db76bbd542dd78bb44dfde3d58729ad8
Terrence Cole <terrence@mozilla.com>
2016-01-22 10:41:43 -0800

    Bug 1224038 - Part 2: Use stable hashing for NewTable; r=sfink

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -650,7 +650,8 @@ class GCRuntime
 
     uint64_t nextCellUniqueId() {
         MOZ_ASSERT(nextCellUniqueId_ > 0);
-        return nextCellUniqueId_++;
+        uint64_t uid = ++nextCellUniqueId_;
+        return uid;
     }
 
   public:
@@ -1030,7 +1031,7 @@ class GCRuntime
     size_t maxMallocBytes;
 
     // An incrementing id used to assign unique ids to cells that require one.
-    uint64_t nextCellUniqueId_;
+    mozilla::Atomic<uint64_t, mozilla::SequentiallyConsistent> nextCellUniqueId_;
 
     /*
      * Number of the committed arenas in all GC chunks including empty chunks.
