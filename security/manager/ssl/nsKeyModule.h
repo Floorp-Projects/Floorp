c3335ee00b32b3f554245ba0b59f78b4bde3b128
David Keeler <dkeeler@mozilla.com>
2016-01-22 14:49:39 -0800

    bug 1239609 - audit nsNSSShutDownObject destructors for correctness r=Cykesiopka,sworkman, a=lizzard
    --HG--
    extra : source : 9a1977d9b21b1a4ba1fc5fdaff4240af83088271

--- security/manager/ssl/nsKeyModule.h
+++ security/manager/ssl/nsKeyModule.h
@@ -51,7 +51,7 @@ public:
   NS_DECL_NSIKEYOBJECTFACTORY
 
 private:
-  ~nsKeyObjectFactory() {}
+  ~nsKeyObjectFactory();
 
   // Disallow copy constructor
   nsKeyObjectFactory(nsKeyObjectFactory&);
