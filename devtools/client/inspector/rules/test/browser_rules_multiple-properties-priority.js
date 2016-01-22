c7bbf8673a0b147e673c33ea0f3a068ae06d2d2e
Patrick Brosset <pbrosset@mozilla.com>
2016-01-22 16:52:51 +0100

    Bug 1241527 - 4 - Use ruleview-changed event to avoid pending requests in browser_rules_multiple* tests; r=gl

--- devtools/client/inspector/rules/test/browser_rules_multiple-properties-priority.js
+++ devtools/client/inspector/rules/test/browser_rules_multiple-properties-priority.js
@@ -13,15 +13,17 @@ add_task(function*() {
   yield addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
   let {inspector, view} = yield openRuleView();
   yield selectNode("div", inspector);
-  yield testCreateNewMultiPriority(inspector, view);
-});
 
-function* testCreateNewMultiPriority(inspector, view) {
   let ruleEditor = getRuleViewRuleEditor(view, 0);
+  // Note that we wait for a markup mutation here because this new rule will end
+  // up creating a style attribute on the node shown in the markup-view.
+  // (we also wait for the rule-view to refresh).
   let onMutation = inspector.once("markupmutation");
+  let onRuleViewChanged = view.once("ruleview-changed");
   yield createNewRuleViewProperty(ruleEditor,
     "color:red;width:100px;height: 100px;");
   yield onMutation;
+  yield onRuleViewChanged;
 
   is(ruleEditor.rule.textProps.length, 3,
     "Should have created new text properties.");
@@ -42,4 +44,4 @@ function* testCreateNewMultiPriority(inspector, view) {
     "Should have correct property name");
   is(ruleEditor.rule.textProps[2].value, "100px",
     "Should have correct property value");
-}
+});
