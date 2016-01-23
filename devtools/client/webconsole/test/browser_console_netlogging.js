8cd3dfc275329c553786e430ca22cbe4ab78402b
Lin Clark <lclark@mozilla.com>
2016-01-22 16:44:45 -0800

    Bug 1234287 - Refactor webconsole netlogging tests. r=bgrins

--- devtools/client/webconsole/test/browser_console_netlogging.js
+++ devtools/client/webconsole/test/browser_console_netlogging.js
@@ -7,44 +7,29 @@
 
 "use strict";
 
-const TEST_URI = "data:text/html;charset=utf-8,Web Console network " +
-                 "logging tests";
-
 const TEST_NETWORK_REQUEST_URI =
   "http://example.com/browser/devtools/client/webconsole/test/" +
   "test-network-request.html";
 
-var hud;
+add_task(function* () {
+  let finishedRequest = waitForFinishedRequest();
+  const hud = yield loadPageAndGetHud(TEST_NETWORK_REQUEST_URI,
+                                      "browserConsole");
+  let request = yield finishedRequest;
 
-function test() {
-  loadTab(TEST_URI).then((tab) => {
-    HUDService.openBrowserConsoleOrFocus().then(aHud => {
-      hud = aHud;
-      HUDService.lastFinishedRequest.callback = testResponse;
-      BrowserTestUtils.loadURI(gBrowser.selectedBrowser,
-                               TEST_NETWORK_REQUEST_URI);
-    });
-  });
-}
+  ok(request, "Page load was logged");
 
-function testResponse(request) {
-  hud.ui.webConsoleClient.getResponseContent(request.actor,
-    function(contentPacket) {
-      hud.ui.webConsoleClient.getRequestPostData(request.actor,
-        function(postDataPacket) {
-          // Check if page load was logged correctly.
-          ok(request, "Page load was logged");
+  let client = hud.ui.webConsoleClient;
+  let args = [request.actor];
+  const postData = yield getPacket(client, "getRequestPostData", args);
+  const responseContent = yield getPacket(client, "getResponseContent", args);
 
-          is(request.request.url, TEST_NETWORK_REQUEST_URI,
-            "Logged network entry is page load");
-          is(request.request.method, "GET", "Method is correct");
-          ok(!postDataPacket.postData.text, "No request body was stored");
-          ok(postDataPacket.postDataDiscarded, "Request body was discarded");
-          ok(!contentPacket.content.text, "No response body was stored");
-          ok(contentPacket.contentDiscarded || request.fromCache,
-             "Response body was discarded or response came from the cache");
-
-          executeSoon(finishTest);
-        });
-    });
-}
+  is(request.request.url, TEST_NETWORK_REQUEST_URI,
+    "Logged network entry is page load");
+  is(request.request.method, "GET", "Method is correct");
+  ok(!postData.postData.text, "No request body was stored");
+  ok(postData.postDataDiscarded, "Request body was discarded");
+  ok(!responseContent.content.text, "No response body was stored");
+  ok(responseContent.contentDiscarded || request.fromCache,
+     "Response body was discarded or response came from the cache");
+});
