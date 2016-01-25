9735cc5d436924f6fb869ebb6d2a8c352a3f66ef
Lin Clark <lclark@mozilla.com>
2016-01-25 11:19:58 -0800

    Bug 1241956 - Use standard license boilerplate in webconsole js files. r=bgrins

--- devtools/shared/webconsole/worker-utils.js
+++ devtools/shared/webconsole/worker-utils.js
@@ -1,3 +1,9 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft= javascript ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // XXXworkers This file is loaded on the server side for worker debugging.
 // Since the server is running in the worker thread, it doesn't
 // have access to Services / Components.  This functionality
