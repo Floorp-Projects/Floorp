57488c1b7acd713f93f6e14433d5bda7bafc5436
dimi <dlee@mozilla.com>
2016-01-25 14:18:29 +0800

    Bug 1188822 - Make service-workers/service-worker/fetch-request-resources.https.html pass. r=bkelly a=sylvestre

--- dom/fetch/InternalRequest.h
+++ dom/fetch/InternalRequest.h
@@ -375,6 +375,9 @@ public:
   static RequestMode
   MapChannelToRequestMode(nsIChannel* aChannel);
 
+  static RequestCredentials
+  MapChannelToRequestCredentials(nsIChannel* aChannel);
+
 private:
   // Does not copy mBodyStream.  Use fallible Clone() for complete copy.
   explicit InternalRequest(const InternalRequest& aOther);
