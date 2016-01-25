6a17a3ff1e7cc8b82de31db8281ee645022b08cb
Andrew Halberstadt <ahalberstadt@mozilla.com>
2016-01-25 09:55:57 -0500

    Bug 1231784 - Install specialpowers and mochikit extensions at runtime via AddonManager.loadTemporaryAddon(), r=jgriffin

---
+++ testing/mochitest/start_desktop.js
@@ -0,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const flavor  = __marionetteParams[0]
+const url = __marionetteParams[1]
+
+let wm = Cc["@mozilla.org/appshell/window-mediator;1"]
+          .getService(Ci.nsIWindowMediator);
+let win = wm.getMostRecentWindow("navigator:browser");
+
+// mochikit's bootstrap.js has set up a listener for this event. It's
+// used so bootstrap.js knows which flavor and url to load.
+let ev = new CustomEvent('mochitest-load', {'detail': [flavor, url]});
+win.dispatchEvent(ev);
