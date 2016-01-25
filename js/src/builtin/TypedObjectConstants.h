1c8dd64a805969e5b4fdb47ca31a24f7080adcd9
Jeff Walden <jwalden@mit.edu>
2016-01-24 23:41:39 -0800

    Bug 1079844 - Rename JS_ARRAYBUFFER_NEUTERED_FLAG to use detachment terminology.  r=sfink

--- js/src/builtin/TypedObjectConstants.h
+++ js/src/builtin/TypedObjectConstants.h
@@ -28,7 +28,7 @@
 
 #define JS_ARRAYBUFFER_FLAGS_SLOT            3
 
-#define JS_ARRAYBUFFER_NEUTERED_FLAG 0x4
+#define JS_ARRAYBUFFER_DETACHED_FLAG 0x4
 
 ///////////////////////////////////////////////////////////////////////////
 // Slots for typed prototypes
