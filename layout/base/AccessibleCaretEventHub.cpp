e8f13d9768a1353cdfa09f1ec5e2ace1047effba
Ting-Yu Lin <tlin@mozilla.com>
2016-01-25 15:50:28 +0800

    Bug 1121468 - Go to NoActionState after receiving release on LongTapState. r=roc
    This makes the state change match the user action. No functionality
    changes is expected.

--- layout/base/AccessibleCaretEventHub.cpp
+++ layout/base/AccessibleCaretEventHub.cpp
@@ -327,9 +327,16 @@ public:
       rv = nsEventStatus_eConsumeNoDefault;
     }
 
+    return rv;
+  }
+
+  virtual nsEventStatus OnRelease(AccessibleCaretEventHub* aContext) override
+  {
     aContext->SetState(aContext->NoActionState());
 
-    return rv;
+    // Do not consume the release since the press is not consumed in
+    // PressNoCaretState either.
+    return nsEventStatus_eIgnore;
   }
 
   virtual void OnReflow(AccessibleCaretEventHub* aContext) override
