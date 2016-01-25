989d599b4c8d0f8fce78b938eb8ff11ee60b7609
Patrick Brosset <pbrosset@mozilla.com>
2016-01-25 16:22:07 +0100

    Bug 1241527 - 5 - Use ruleview-changed event to fix remaining pending requests in tests; r=gl
    --HG--
    extra : commitid : 9A6MyrgAkgH

--- devtools/client/inspector/rules/test/browser_rules_edit-property_08.js
+++ devtools/client/inspector/rules/test/browser_rules_edit-property_08.js
@@ -19,25 +19,27 @@ add_task(function*() {
   yield addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
   let {inspector, view} = yield openRuleView();
   yield selectNode("#testid", inspector);
-  yield testRenameProperty(inspector, view);
-});
 
-function* testRenameProperty(inspector, view) {
+  info("Get the color property editor");
   let ruleEditor = getRuleViewRuleEditor(view, 0);
   let propEditor = ruleEditor.rule.textProps[0].editor;
-
   is(ruleEditor.rule.textProps[0].name, "color");
 
-  info("Focus on property name");
-  let editor = yield focusEditableField(ruleEditor.ruleView,
-    propEditor.nameSpan, 32, 1);
+  info("Focus the property name field");
+  yield focusEditableField(ruleEditor.ruleView, propEditor.nameSpan, 32, 1);
 
-  info("Renaming the property");
+  info("Rename the property to background-color");
+  // Expect 3 events: the value editor being focused, the ruleview-changed event
+  // which signals that the new value has been previewed (fires once when the
+  // value gets focused), and the markupmutation event since we're modifying an
+  // inline style.
   let onValueFocus = once(ruleEditor.element, "focus", true);
-  let onModifications = ruleEditor.ruleView.once("ruleview-changed");
+  let onRuleViewChanged = ruleEditor.ruleView.once("ruleview-changed");
+  let onMutation = inspector.once("markupmutation");
   EventUtils.sendString("background-color:", ruleEditor.doc.defaultView);
   yield onValueFocus;
-  yield onModifications;
+  yield onRuleViewChanged;
+  yield onMutation;
 
   is(ruleEditor.rule.textProps[0].name, "background-color");
   yield waitForComputedStyleProperty("#testid", null, "background-color",
@@ -45,4 +47,11 @@ function* testRenameProperty(inspector, view) {
 
   is((yield getComputedStyleProperty("#testid", null, "color")),
     "rgb(255, 255, 255)", "color is white");
-}
+
+  // The value field is still focused. Blur it now and wait for the
+  // ruleview-changed event to avoid pending requests.
+  onRuleViewChanged = view.once("ruleview-changed");
+  EventUtils.synthesizeKey("VK_ESCAPE", {});
+  yield onRuleViewChanged;
+});
+
