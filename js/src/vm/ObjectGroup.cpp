f4ed10f0f720e3b56a0ddbe935939b89b3c6fcb3
Terrence Cole <terrence@mozilla.com>
2016-01-25 08:43:53 -0800

    Bug 1234862 - Part 3: Always use DefaultGCPolicy for GCHashTables; r=sfink

--- js/src/vm/ObjectGroup.cpp
+++ js/src/vm/ObjectGroup.cpp
@@ -1772,10 +1772,10 @@ ObjectGroupCompartment::clearTables()
 }
 
 /* static */ bool
-ObjectGroupCompartment::PlainObjectGCPolicy::needsSweep(PlainObjectKey* key,
-                                                        PlainObjectEntry* entry)
+ObjectGroupCompartment::PlainObjectTableSweepPolicy::needsSweep(PlainObjectKey* key,
+                                                                PlainObjectEntry* entry)
 {
-    if (!(KeyPolicy::needsSweep(key) || entry->needsSweep(key->nproperties)))
+    if (!(DefaultGCPolicy<PlainObjectKey>::needsSweep(key) || entry->needsSweep(key->nproperties)))
         return false;
     js_free(key->properties);
     js_free(entry->types);
