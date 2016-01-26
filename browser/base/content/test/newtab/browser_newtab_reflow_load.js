ba2d05ea2f9f8c66b05ef1fb3b967012a6780726
Neil Deakin <neil@mozilla.com>
2016-01-26 08:16:11 -0500

    Bug 1240014, enable test browser_newtab_reflow_load in e10s, r=adw a=lizzard

--- browser/base/content/test/newtab/browser_newtab_reflow_load.js
+++ browser/base/content/test/newtab/browser_newtab_reflow_load.js
@@ -9,32 +9,29 @@ const ADDITIONAL_WAIT_MS = 2000;
 /*
  * Ensure that loading about:newtab doesn't cause uninterruptible reflows.
  */
-function runTests() {
-  gBrowser.selectedTab = gBrowser.addTab("about:blank", {animate: false});
-  let browser = gBrowser.selectedBrowser;
-  yield whenBrowserLoaded(browser);
+add_task(function* () {
+  yield BrowserTestUtils.openNewForegroundTab(gBrowser, () => {
+    return gBrowser.selectedTab = gBrowser.addTab("about:blank", {animate: false});
+  }, false);
 
+  let browser = gBrowser.selectedBrowser;
   let mm = browser.messageManager;
   mm.loadFrameScript(FRAME_SCRIPT, true);
   mm.addMessageListener("newtab-reflow", ({data: stack}) => {
     ok(false, `unexpected uninterruptible reflow ${stack}`);
   });
 
+  let browserLoadedPromise = BrowserTestUtils.waitForEvent(browser, "load", true);
   browser.loadURI("about:newtab");
-  yield whenBrowserLoaded(browser);
+  yield browserLoadedPromise;
 
   // Wait some more to catch sync reflows after the page has loaded.
-  yield setTimeout(TestRunner.next, ADDITIONAL_WAIT_MS);
+  yield new Promise(resolve => {
+    setTimeout(resolve, ADDITIONAL_WAIT_MS);
+  });
 
   // Clean up.
   gBrowser.removeCurrentTab({animate: false});
 
   ok(true, "Each test requires at least one pass, fail or todo so here is a pass.");
-}
-
-function whenBrowserLoaded(browser) {
-  browser.addEventListener("load", function onLoad() {
-    browser.removeEventListener("load", onLoad, true);
-    executeSoon(TestRunner.next);
-  }, true);
-}
+});
