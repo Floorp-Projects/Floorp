fa2eaf528a83d9a496b9581b03181cec9010e940
Kaku Kuo <tkuo@mozilla.com>
2016-01-26 10:53:46 +0800

    Bug 1239752 - Clean up test files. r=roc
    MozReview-Commit-ID: S7mYwGDKk5
    
    --HG--
    extra : rebase_source : 28235c83cc1b416bdb034a818acddfa9e025f6f8

--- dom/canvas/test/imagebitmap_structuredclone.js
+++ dom/canvas/test/imagebitmap_structuredclone.js
@@ -4,7 +4,7 @@ function ok(expect, msg) {
 
 onmessage = function(event) {
   ok(!!event.data.bitmap1, "Get the 1st ImageBitmap from the main script.");
-  ok(!!event.data.bitmap2, "Get the 2st ImageBitmap from the main script.");
+  ok(!!event.data.bitmap2, "Get the 2nd ImageBitmap from the main script.");
 
   // send the first original ImageBitmap back to the main-thread
   postMessage({"type":"bitmap1",
