db9a26bf346c3e5abd0d74be5d1e7fb794bbce60
Gabriel Luong <gabriel.luong@gmail.com>
2016-01-26 11:17:48 -0500

    Bug 1238133 - Part 4: Make the computed view live inside the inspector panel frame r=bgrins

--- devtools/client/inspector/shared/test/browser_styleinspector_refresh_when_active.js
+++ devtools/client/inspector/shared/test/browser_styleinspector_refresh_when_active.js
@@ -19,8 +19,7 @@ add_task(function*() {
   is(getRuleViewPropertyValue(view, "element", "color"), "red",
     "The rule-view shows the properties for test node one");
 
-  let cView = inspector.sidebar.getWindowForTab("computedview")
-    .computedview.view;
+  let cView = inspector.computedview.view;
   let prop = getComputedViewProperty(cView, "color");
   ok(!prop, "The computed-view doesn't show the properties for test node one");
 
