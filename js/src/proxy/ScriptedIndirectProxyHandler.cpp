8d1c3ce37464b986b22a65c25257b45bd6abe8a4
Tom Schuster <evilpies@gmail.com>
2016-01-26 19:38:52 +0100

    Bug 1242072 - Continue using getPropertyDescriptor for get in ScriptedIndirectProxyHandler. r=jorendorff

--- js/src/proxy/ScriptedIndirectProxyHandler.cpp
+++ js/src/proxy/ScriptedIndirectProxyHandler.cpp
@@ -299,8 +299,40 @@ ScriptedIndirectProxyHandler::get(JSContext* cx, HandleObject proxy, HandleValue
     if (!GetDerivedTrap(cx, handler, cx->names().get, &fval))
         return false;
     if (!IsCallable(fval))
-        return BaseProxyHandler::get(cx, proxy, receiver, id, vp);
+        return derivedGet(cx, proxy, receiver, id, vp);
     return Trap(cx, handler, fval, 2, argv.begin(), vp);
+}
+
+bool
+ScriptedIndirectProxyHandler::derivedGet(JSContext* cx, HandleObject proxy, HandleValue receiver,
+                                         HandleId id, MutableHandleValue vp) const
+{
+    // This uses getPropertyDescriptor for backward compatibility reasons.
+
+    Rooted<PropertyDescriptor> desc(cx);
+    if (!getPropertyDescriptor(cx, proxy, id, &desc))
+        return false;
+    desc.assertCompleteIfFound();
+
+    if (!desc.object()) {
+        vp.setUndefined();
+        return true;
+    }
+
+    if (desc.isDataDescriptor()) {
+        vp.set(desc.value());
+        return true;
+    }
+
+    MOZ_ASSERT(desc.isAccessorDescriptor());
+    RootedObject getter(cx, desc.getterObject());
+
+    if (!getter) {
+        vp.setUndefined();
+        return true;
+    }
+
+    return InvokeGetter(cx, receiver, ObjectValue(*getter), vp);
 }
 
 bool
