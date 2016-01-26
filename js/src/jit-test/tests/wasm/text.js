2d9f3dd3f72a50de44ae9f7bbc9415b345602f5b
Luke Wagner <luke@mozilla.com>
2016-01-26 13:05:01 -0600

    Bug 1242772 - Baldr: fix unterminated string literal (r=bbouvier)

--- js/src/jit-test/tests/wasm/text.js
+++ js/src/jit-test/tests/wasm/text.js
@@ -5,11 +5,12 @@ if (!this.wasmEval)
 
 var parsingError = /parsing wasm text at/;
 
-assertErrorMessage(() => wasmEvalText(""), Error, parsingError);
-assertErrorMessage(() => wasmEvalText("("), Error, parsingError);
-assertErrorMessage(() => wasmEvalText("(m"), Error, parsingError);
-assertErrorMessage(() => wasmEvalText("(module"), Error, parsingError);
-assertErrorMessage(() => wasmEvalText("(moduler"), Error, parsingError);
+assertErrorMessage(() => wasmEvalText(''), Error, parsingError);
+assertErrorMessage(() => wasmEvalText('('), Error, parsingError);
+assertErrorMessage(() => wasmEvalText('(m'), Error, parsingError);
+assertErrorMessage(() => wasmEvalText('(module'), Error, parsingError);
+assertErrorMessage(() => wasmEvalText('(moduler'), Error, parsingError);
+assertErrorMessage(() => wasmEvalText('(module (func) (export "a'), Error, parsingError);
 
 // Note: the s-expression text format is temporary, this file is mostly just to
 // hold basic error smoke tests.
