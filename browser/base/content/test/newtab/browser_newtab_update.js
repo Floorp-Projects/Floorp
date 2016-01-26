81c0b041d8e6fae7ccd57893f52ed01a258ecd6d
Neil Deakin <neil@mozilla.com>
2016-01-26 08:16:10 -0500

    Bug 1240014, enable test browser_newtab_update in e10s, r=adw a=lizzard

--- browser/base/content/test/newtab/browser_newtab_update.js
+++ browser/base/content/test/newtab/browser_newtab_update.js
@@ -6,42 +6,41 @@
 /**
  * Checks that newtab is updated as its links change.
  */
-function runTests() {
+add_task(function* () {
   // First, start with an empty page.  setLinks will trigger a hidden page
   // update because it calls clearHistory.  We need to wait for that update to
   // happen so that the next time we wait for a page update below, we catch the
   // right update and not the one triggered by setLinks.
-  yield whenPagesUpdatedAnd(resolve => setLinks([], resolve));
+  let updatedPromise = whenPagesUpdated();
+  let setLinksPromise = setLinks([]);
+  yield Promise.all([updatedPromise, setLinksPromise]);
 
   // Strategy: Add some visits, open a new page, check the grid, repeat.
   yield fillHistoryAndWaitForPageUpdate([1]);
-  yield addNewTabPageTab();
-  checkGrid("1,,,,,,,,");
+  yield* addNewTabPageTab();
+  yield* checkGrid("1,,,,,,,,");
 
   yield fillHistoryAndWaitForPageUpdate([2]);
-  yield addNewTabPageTab();
-  checkGrid("2,1,,,,,,,");
+  yield* addNewTabPageTab();
+  yield* checkGrid("2,1,,,,,,,");
 
   yield fillHistoryAndWaitForPageUpdate([1]);
-  yield addNewTabPageTab();
-  checkGrid("1,2,,,,,,,");
+  yield* addNewTabPageTab();
+  yield* checkGrid("1,2,,,,,,,");
 
   yield fillHistoryAndWaitForPageUpdate([2, 3, 4]);
-  yield addNewTabPageTab();
-  checkGrid("2,1,3,4,,,,,");
+  yield* addNewTabPageTab();
+  yield* checkGrid("2,1,3,4,,,,,");
 
   // Make sure these added links have the right type
-  is(getCell(1).site.link.type, "history", "added link is history");
-}
+  let type = yield performOnCell(1, cell => { return cell.site.link.type });
+  is(type, "history", "added link is history");
+});
 
 function fillHistoryAndWaitForPageUpdate(links) {
-  return whenPagesUpdatedAnd(resolve => fillHistory(links.map(link), resolve));
-}
-
-function whenPagesUpdatedAnd(promiseConstructor) {
-  let promise1 = new Promise(whenPagesUpdated);
-  let promise2 = new Promise(promiseConstructor);
-  return Promise.all([promise1, promise2]).then(TestRunner.next);
+  let updatedPromise = whenPagesUpdated;
+  let fillHistoryPromise = fillHistory(links.map(link));
+  return Promise.all([updatedPromise, fillHistoryPromise]);
 }
 
 function link(id) {
