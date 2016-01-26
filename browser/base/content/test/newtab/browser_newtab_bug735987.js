9fa9f797ebfa0cb114d08e263f294b0a00c2e532
Neil Deakin <neil@mozilla.com>
2016-01-26 08:16:11 -0500

    Bug 1240014, enable test browser_newtab_bug735987 in e10s, r=adw a=lizzard

--- browser/base/content/test/newtab/browser_newtab_bug735987.js
+++ browser/base/content/test/newtab/browser_newtab_bug735987.js
@@ -1,21 +1,21 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-function runTests() {
+add_task(function* () {
   yield setLinks("0,1,2,3,4,5,6,7,8");
   setPinnedLinks("");
 
-  yield addNewTabPageTab();
-  checkGrid("0,1,2,3,4,5,6,7,8");
+  yield* addNewTabPageTab();
+  yield* checkGrid("0,1,2,3,4,5,6,7,8");
 
-  yield simulateExternalDrop(1);
-  checkGrid("0,99p,1,2,3,4,5,6,7");
+  yield* simulateExternalDrop(1);
+  yield* checkGrid("0,99p,1,2,3,4,5,6,7");
 
   yield blockCell(1);
-  checkGrid("0,1,2,3,4,5,6,7,8");
+  yield* checkGrid("0,1,2,3,4,5,6,7,8");
 
-  yield simulateExternalDrop(1);
-  checkGrid("0,99p,1,2,3,4,5,6,7");
+  yield* simulateExternalDrop(1);
+  yield* checkGrid("0,99p,1,2,3,4,5,6,7");
 
   // Simulate a restart and force the next about:newtab
   // instance to read its data from the storage again.
@@ -24,9 +24,9 @@ function runTests() {
   // Update all open pages, e.g. preloaded ones.
   NewTabUtils.allPages.update();
 
-  yield addNewTabPageTab();
-  checkGrid("0,99p,1,2,3,4,5,6,7");
+  yield* addNewTabPageTab();
+  yield* checkGrid("0,99p,1,2,3,4,5,6,7");
 
   yield blockCell(1);
-  checkGrid("0,1,2,3,4,5,6,7,8");
-}
+  yield* checkGrid("0,1,2,3,4,5,6,7,8");
+});
