9df772f7132f8e51e4f1dec353ec7efef2640c40
Neil Deakin <neil@mozilla.com>
2016-01-26 08:16:11 -0500

    Bug 1240014, enable test browser_newtab_drop_preview in e10s, r=adw a=lizzard

--- browser/base/content/test/newtab/browser_newtab_drop_preview.js
+++ browser/base/content/test/newtab/browser_newtab_drop_preview.js
@@ -5,20 +5,37 @@
  * These tests ensure that the drop preview correctly arranges sites when
  * dragging them around.
  */
-function runTests() {
-  yield addNewTabPageTab();
+add_task(function* () {
+  yield* addNewTabPageTab();
 
   // the first three sites are pinned - make sure they're re-arranged correctly
   yield setLinks("0,1,2,3,4,5,6,7,8");
   setPinnedLinks("0,1,2,,,5");
 
-  yield addNewTabPageTab();
-  checkGrid("0p,1p,2p,3,4,5p,6,7,8");
+  yield* addNewTabPageTab();
+  yield* checkGrid("0p,1p,2p,3,4,5p,6,7,8");
 
-  let cw = getContentWindow();
-  cw.gDrag._draggedSite = getCell(0).site;
-  let sites = cw.gDropPreview.rearrange(getCell(4));
-  cw.gDrag._draggedSite = null;
+  let foundSites = yield ContentTask.spawn(gWindow.gBrowser.selectedBrowser, {}, function*() {
+    let cells = content.gGrid.cells;
+    content.gDrag._draggedSite = cells[0].site;
+    let sites = content.gDropPreview.rearrange(cells[4]);
+    content.gDrag._draggedSite = null;
 
-  checkGrid("3,1p,2p,4,0p,5p,6,7,8", sites);
-}
+    sites = sites.slice(0, 9);
+    return sites.map(function (aSite) {
+      if (!aSite)
+        return "";
+
+      let pinned = aSite.isPinned();
+      if (pinned != aSite.node.hasAttribute("pinned")) {
+        ok(false, "invalid state (site.isPinned() != site[pinned])");
+      }
+
+      return aSite.url.replace(/^http:\/\/example(\d+)\.com\/$/, "$1") + (pinned ? "p" : "");
+    });
+  });
+
+  let expectedSites = "3,1p,2p,4,0p,5p,6,7,8"
+  is(foundSites, expectedSites, "grid status = " + expectedSites);
+});
+
