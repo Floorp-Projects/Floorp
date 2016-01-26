ff04fbf532760d4771e47974afe634d7d8954eaa
Andrea Marchesini <amarchesini@mozilla.com>
2016-01-26 17:15:52 +0000

    Bug 1241171 - FormData should not force 'blob' as filename, r=smaug, a=lizzard

--- dom/tests/mochitest/fetch/test_fetch_basic_http.js
+++ dom/tests/mochitest/fetch/test_fetch_basic_http.js
@@ -169,7 +169,7 @@ function testFormDataSend() {
       }
 
       is(response[1].headers['Content-Disposition'],
-          'form-data; name="empty"; filename="blob"');
+          'form-data; name="empty"; filename=""');
 
       is(response[2].headers['Content-Disposition'],
           'form-data; name="explicit"; filename="explicit-file-name"');
