95c082c6fc6b612c7c4bdce720f1d516350176d9
Neil Deakin <neil@mozilla.com>
2016-01-26 08:16:10 -0500

    Bug 1240014, enable test browser_newtab_bug752841 in e10s, r=adw a=lizzard

--- browser/base/content/test/newtab/browser_newtab_bug752841.js
+++ browser/base/content/test/newtab/browser_newtab_bug752841.js
@@ -4,7 +4,14 @@
 const PREF_NEWTAB_ROWS = "browser.newtabpage.rows";
 const PREF_NEWTAB_COLUMNS = "browser.newtabpage.columns";
 
-function runTests() {
+function getCellsCount()
+{
+  return ContentTask.spawn(gBrowser.selectedBrowser, {}, function* () {
+    return content.gGrid.cells.length;
+  });
+}
+
+add_task(function* () {
   let testValues = [
     {row: 0, column: 0},
     {row: -1, column: -1},
@@ -20,38 +27,30 @@ function runTests() {
    // Values before setting new pref values (15 is the default value -> 5 x 3)
   let previousValues = [15, 1, 1, 1, 1, 8];
 
-  let existingTab, existingTabGridLength, newTab, newTabGridLength;
-  yield addNewTabPageTab();
-  existingTab = gBrowser.selectedTab;
+  yield* addNewTabPageTab();
+  let existingTab = gBrowser.selectedTab;
 
   for (let i = 0; i < expectedValues.length; i++) {
-    gBrowser.selectedTab = existingTab;
-    existingTabGridLength = getGrid().cells.length;
+    let existingTabGridLength = yield getCellsCount();
     is(existingTabGridLength, previousValues[i],
       "Grid length of existing page before update is correctly.");
 
-    Services.prefs.setIntPref(PREF_NEWTAB_ROWS, testValues[i].row);
-    Services.prefs.setIntPref(PREF_NEWTAB_COLUMNS, testValues[i].column);
+    yield pushPrefs([PREF_NEWTAB_ROWS, testValues[i].row]);
+    yield pushPrefs([PREF_NEWTAB_COLUMNS, testValues[i].column]);
 
-    existingTabGridLength = getGrid().cells.length;
+    existingTabGridLength = yield getCellsCount();
     is(existingTabGridLength, expectedValues[i],
       "Existing page grid is updated correctly.");
 
-    yield addNewTabPageTab();
-    newTab = gBrowser.selectedTab;
-    newTabGridLength = getGrid().cells.length;
+    yield* addNewTabPageTab();
+    let newTab = gBrowser.selectedTab;
+    let newTabGridLength = yield getCellsCount();
     is(newTabGridLength, expectedValues[i],
       "New page grid is updated correctly.");
 
-    gBrowser.removeTab(newTab);
-
-    // Wait until the original tab is visible again.
-    let doc = existingTab.linkedBrowser.contentDocument;
-    yield waitForCondition(() => !doc.hidden).then(TestRunner.next);
+    yield BrowserTestUtils.removeTab(newTab);
   }
 
   gBrowser.removeTab(existingTab);
+});
 
-  Services.prefs.clearUserPref(PREF_NEWTAB_ROWS);
-  Services.prefs.clearUserPref(PREF_NEWTAB_COLUMNS);
-}
