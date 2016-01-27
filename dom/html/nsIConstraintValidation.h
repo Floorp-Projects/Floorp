c3a8905b0b7a0b65aed4669897799d0c62ab4361
Jonathan Watt <jwatt@jwatt.org>
2016-01-27 16:14:16 +0000

    Bug 893332, part 1 - Rename misnamed SetValidityState/GetValidityState arguments. r=baku

--- dom/html/nsIConstraintValidation.h
+++ dom/html/nsIConstraintValidation.h
@@ -64,8 +64,8 @@ public:
     VALIDITY_STATE_CUSTOM_ERROR     = 0x1 <<  9,
   };
 
-  void SetValidityState(ValidityStateType mState,
-                        bool mValue);
+  void SetValidityState(ValidityStateType aState,
+                        bool aValue);
 
   // Web IDL binding methods
   bool WillValidate() const {
@@ -83,9 +83,10 @@ protected:
   nsresult CheckValidity(bool* aValidity);
   void     SetCustomValidity(const nsAString& aError);
 
-  bool GetValidityState(ValidityStateType mState) const {
-         return mValidityBitField & mState;
-       }
+  bool GetValidityState(ValidityStateType aState) const
+  {
+    return mValidityBitField & aState;
+  }
 
   void SetBarredFromConstraintValidation(bool aBarred);
 
