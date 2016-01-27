75626aef002635833c17e3c1e1184b2081482aaf
Lin Clark <lclark@mozilla.com>
2016-01-27 14:46:00 +0100

    Bug 1243243 - Use standard license boilerplate in debugger js files. r=jlongster

--- devtools/client/debugger/test/mochitest/head.js
+++ devtools/client/debugger/test/mochitest/head.js
@@ -1,5 +1,8 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
 /* Any copyright is dedicated to the Public Domain.
-   http://creativecommons.org/publicdomain/zero/1.0/ */
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
 "use strict";
 
 // shared-head.js handles imports, constants, and utility functions
