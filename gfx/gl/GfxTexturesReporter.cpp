9e56091f86c356a2f650d282cfc0e64c5591ee58
James Willcox <snorp@snorp.net>
2016-01-27 09:37:44 -0600

    Bug 1251065 - Add prefs for logging the current and peak texture usage r=nical

--- gfx/gl/GfxTexturesReporter.cpp
+++ gfx/gl/GfxTexturesReporter.cpp
@@ -4,7 +4,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <string>
+#include <sstream>
 #include "GfxTexturesReporter.h"
+#include "gfxPrefs.h"
 
 #ifdef MOZ_CRASHREPORTER
 #include "nsExceptionHandler.h"
@@ -19,16 +22,54 @@ Atomic<size_t> GfxTexturesReporter::sAmount(0);
 Atomic<size_t> GfxTexturesReporter::sPeakAmount(0);
 Atomic<size_t> GfxTexturesReporter::sTileWasteAmount(0);
 
+std::string
+FormatBytes(size_t amount)
+{
+    std::stringstream stream;
+    int depth = 0;
+    double val = amount;
+    while (val > 1024) {
+        val /= 1024;
+        depth++;
+    }
+
+    const char* unit;
+    switch(depth) {
+      case 0:
+      unit = "bytes";
+      break;
+      case 1:
+      unit = "KB";
+      break;
+      case 2:
+      unit = "MB";
+      break;
+      case 3:
+      unit = "GB";
+      break;
+  }
+
+  stream << val << " " << unit;
+  return stream.str();
+}
+
 /* static */ void
 GfxTexturesReporter::UpdateAmount(MemoryUse action, size_t amount)
 {
     if (action == MemoryFreed) {
         MOZ_RELEASE_ASSERT(amount <= sAmount);
         sAmount -= amount;
+
+        if (gfxPrefs::GfxLoggingTextureUsageEnabled()) {
+            printf_stderr("Current texture usage: %s\n", FormatBytes(sAmount).c_str());
+        }
     } else {
         sAmount += amount;
         if (sAmount > sPeakAmount) {
             sPeakAmount.exchange(sAmount);
+            if (gfxPrefs::GfxLoggingPeakTextureUsageEnabled()) {
+                printf_stderr("Peak texture usage: %s\n", FormatBytes(sPeakAmount).c_str());
+            }
         }
     }
 
