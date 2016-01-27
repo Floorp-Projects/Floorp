88f7d573278d92228f0e5139e58430714e1e7124
Lin Clark <lclark@mozilla.com>
2016-01-27 18:55:00 +0100

    Bug 1243567 - Revert license change on webconsole tests. r=bgrins

--- devtools/client/webconsole/test/browser_cached_messages.js
+++ devtools/client/webconsole/test/browser_cached_messages.js
@@ -1,8 +1,7 @@
 /* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
 /* vim: set ft=javascript ts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 // Test to see if the cached messages are displayed when the console UI is
 // opened.
