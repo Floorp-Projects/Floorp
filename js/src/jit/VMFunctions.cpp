5a6177ac6a0e17675a25f4609aed29bd5cadf7c3
Eric Faust <efaustbmo@gmail.com>
2016-01-27 11:26:52 -0800

    Bug 1238461 - Fix differential output involving JSOP_TOID. (r=jandem)

--- js/src/jit/VMFunctions.cpp
+++ js/src/jit/VMFunctions.cpp
@@ -1295,7 +1295,7 @@ bool
 ThrowObjectCoercible(JSContext* cx, HandleValue v)
 {
     MOZ_ASSERT(v.isUndefined() || v.isNull());
-    MOZ_ALWAYS_FALSE(ToObjectSlow(cx, v, false));
+    MOZ_ALWAYS_FALSE(ToObjectSlow(cx, v, true));
     return false;
 }
 
