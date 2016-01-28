0067cc1e04020f5dac27cbe6ec551c783aa7ab74
Hector Zhao <bzhao@mozilla.com>
2016-01-28 15:42:16 +0800

    Bug 1242965 - Make services/common eslintable. r=rnewman

--- services/common/observers.js
+++ services/common/observers.js
@@ -2,16 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef MERGED_COMPARTMENT
-
 this.EXPORTED_SYMBOLS = ["Observers"];
 
 var Cc = Components.classes;
 var Ci = Components.interfaces;
 var Cr = Components.results;
 var Cu = Components.utils;
-
-#endif
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
