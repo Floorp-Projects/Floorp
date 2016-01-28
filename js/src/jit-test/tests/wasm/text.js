ee766ec556dc3436f25db65cf22bd25c28f5d99a
Luke Wagner <luke@mozilla.com>
2016-01-28 09:20:18 -0600

    Bug 1243632 - Baldr: check TypeError vs. SyntaxError (r=bbouvier)
    --HG--
    extra : commitid : 35dU67Jeld7
    extra : rebase_source : fc085a9151894ff8ae6e42e201f875dcd2b6cdaa

--- js/src/jit-test/tests/wasm/text.js
+++ js/src/jit-test/tests/wasm/text.js
@@ -5,12 +5,12 @@ if (!wasmIsSupported())
 
 var parsingError = /parsing wasm text at/;
 
-assertErrorMessage(() => wasmEvalText(''), Error, parsingError);
-assertErrorMessage(() => wasmEvalText('('), Error, parsingError);
-assertErrorMessage(() => wasmEvalText('(m'), Error, parsingError);
-assertErrorMessage(() => wasmEvalText('(module'), Error, parsingError);
-assertErrorMessage(() => wasmEvalText('(moduler'), Error, parsingError);
-assertErrorMessage(() => wasmEvalText('(module (func) (export "a'), Error, parsingError);
+assertErrorMessage(() => wasmEvalText(''), SyntaxError, parsingError);
+assertErrorMessage(() => wasmEvalText('('), SyntaxError, parsingError);
+assertErrorMessage(() => wasmEvalText('(m'), SyntaxError, parsingError);
+assertErrorMessage(() => wasmEvalText('(module'), SyntaxError, parsingError);
+assertErrorMessage(() => wasmEvalText('(moduler'), SyntaxError, parsingError);
+assertErrorMessage(() => wasmEvalText('(module (func) (export "a'), SyntaxError, parsingError);
 
 // Note: the s-expression text format is temporary, this file is mostly just to
 // hold basic error smoke tests.
