411b9f0011cd65fb26be53ea629ac49dc17f3217
Chenxia Liu <liuche@mozilla.com>
2016-01-28 11:38:39 -0800

    Bug 1242467 - Give new onboarding experiments new names. r=margaret, a=lizzard

--- mobile/android/base/java/org/mozilla/gecko/util/Experiments.java
+++ mobile/android/base/java/org/mozilla/gecko/util/Experiments.java
@@ -11,6 +11,12 @@ package org.mozilla.gecko.util;
 public class Experiments {
     // Display History and Bookmarks in 3-dot menu.
     public static final String BOOKMARKS_HISTORY_MENU = "bookmark-history-menu";
+
+    // Onboarding: "Features and Story"
+    public static final String ONBOARDING2_A = "onboarding2-a"; // Control: Single (blue) welcome screen
+    public static final String ONBOARDING2_B = "onboarding2-b"; // 4 static Feature slides
+    public static final String ONBOARDING2_C = "onboarding2-c"; // 4 static + 1 clickable (Data saving) Feature slides
+
     // Show search mode (instead of home panels) when tapping on urlbar if there is a search term in the urlbar.
     public static final String SEARCH_TERM = "search-term";
 
