29467e693f13363c9f1224c62a0463b67da48853
Chenxia Liu <liuche@mozilla.com>
2016-01-27 16:40:55 -0800

    Bug 1240549 - Put re-enter search mode functionality behind a switchboard flag. r=margaret, a=lizzard

--- mobile/android/base/java/org/mozilla/gecko/util/Experiments.java
+++ mobile/android/base/java/org/mozilla/gecko/util/Experiments.java
@@ -9,5 +9,9 @@ package org.mozilla.gecko.util;
  * https://github.com/mozilla-services/switchboard-experiments
  */
 public class Experiments {
+    // Display History and Bookmarks in 3-dot menu.
     public static final String BOOKMARKS_HISTORY_MENU = "bookmark-history-menu";
+    // Show search mode (instead of home panels) when tapping on urlbar if there is a search term in the urlbar.
+    public static final String SEARCH_TERM = "search-term";
+
 }
