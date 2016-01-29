81bc64d6eca20d3823e1655b0a6a216fa97239d7
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2016-01-29 09:24:25 -0800

    Bug 1243924 - Convert remaining callsites within dom/ to use channel.open2() (r=sicking)

--- dom/base/test/unit/head_utilities.js
+++ dom/base/test/unit/head_utilities.js
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 Components.utils.import("resource://testing-common/httpd.js");
-Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://gre/modules/NetUtil.jsm");
 
 const nsIDocumentEncoder = Components.interfaces.nsIDocumentEncoder;
 const replacementChar = Components.interfaces.nsIConverterInputStream.DEFAULT_REPLACEMENT_CHARACTER;
@@ -17,23 +17,21 @@ function loadContentFile(aFile, aCharset) {
     var file = do_get_file(aFile);
     var ios = Components.classes['@mozilla.org/network/io-service;1']
             .getService(Components.interfaces.nsIIOService);
-    var chann = ios.newChannelFromURI2(ios.newFileURI(file),
-                                       null,      // aLoadingNode
-                                       Services.scriptSecurityManager.getSystemPrincipal(),
-                                       null,      // aTriggeringPrincipal
-                                       Components.interfaces.nsILoadInfo.SEC_NORMAL,
-                                       Components.interfaces.nsIContentPolicy.TYPE_OTHER);
+    var chann = NetUtil.newChannel({
+      uri: ios.newFileURI(file),
+      loadUsingSystemPrincipal: true
+    });
     chann.contentCharset = aCharset;
 
     /*var inputStream = Components.classes["@mozilla.org/scriptableinputstream;1"]
                         .createInstance(Components.interfaces.nsIScriptableInputStream);
-    inputStream.init(chann.open());
+    inputStream.init(chann.open2());
     return inputStream.read(file.fileSize);
     */
 
     var inputStream = Components.classes["@mozilla.org/intl/converter-input-stream;1"]
                        .createInstance(Components.interfaces.nsIConverterInputStream);
-    inputStream.init(chann.open(), aCharset, 1024, replacementChar);
+    inputStream.init(chann.open2(), aCharset, 1024, replacementChar);
     var str = {}, content = '';
     while (inputStream.readString(4096, str) != 0) {
         content += str.value;
