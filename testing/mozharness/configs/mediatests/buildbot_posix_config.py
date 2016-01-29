be898d03269ba282bea19d90643adaf09ec21de2
Syd Polk <spolk@mozilla.com>
2016-01-29 16:12:30 -0600

    Bug 1238648 - Use external-media-tests in tree instead of github for tests. r=maja_zf, r=whimboo
    --HG--
    extra : commitid : 9alr2iRFKaf
    extra : rebase_source : 6a46414396a317bb1d5e493be1fa30e2f62e409d

--- testing/mozharness/configs/mediatests/buildbot_posix_config.py
+++ testing/mozharness/configs/mediatests/buildbot_posix_config.py
@@ -12,7 +12,6 @@ config = {
         'python': '/tools/buildbot/bin/python',
         'virtualenv': ['/tools/buildbot/bin/python', '/tools/misc-python/virtualenv.py'],
         'tooltool.py': "/tools/tooltool.py",
-        'gittool.py': os.path.join(external_tools_path, 'gittool.py'),
     },
 
     "find_links": [
@@ -26,7 +25,6 @@ config = {
     "default_actions": [
         'clobber',
         'read-buildbot-config',
-        'checkout',
         'download-and-extract',
         'create-virtualenv',
         'install',
@@ -38,10 +36,6 @@ config = {
     "blob_uploader_auth_file" : os.path.join(os.getcwd(), "oauth.txt"),
     "download_minidump_stackwalk": True,
     "download_symbols": "ondemand",
-
-    "firefox_media_repo": 'https://github.com/mjzffr/firefox-media-tests.git',
-    "firefox_media_branch": 'master',
-    "firefox_media_rev": '0830e972e4b95fef3507207fc6bce028da27f2d3',
 
     "suite_definitions": {
         "media-tests": {
