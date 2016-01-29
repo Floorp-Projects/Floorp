b6fcf96be24899d9fa84f8cab9055ed4e7ae0738
Andreas Tolfsen <ato@mozilla.com>
2016-01-29 12:57:46 +0000

    Bug 1243704 - Serialise errors sent over IPC; r=automatedtester
    This fixes an instance of passing an Error prototype over the message
    manager as a CPOW.  We solve this by marshaling the error, which is
    now done automatically by the new AsyncMessageChannel.  It allows us to
    create an (almost) transparent promise-based interface between chrome-
    and content contexts.
    
    The patch also makes AsyncMessageChannel reusable on both sides of the
    message listener, but it's currently not used at its maximum potential
    because of the way the listener is architected.
    
    --HG--
    extra : commitid : EQd5E4Digdv
    extra : rebase_source : 8e0c36960759d25c73bbbc0f4f0bcb453c1028f4
    extra : amend_source : 6bb6a76a8b30a5524639f8236291980b16402e6a

--- testing/marionette/error.js
+++ testing/marionette/error.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-var {interfaces: Ci, utils: Cu} = Components;
+const {interfaces: Ci, utils: Cu} = Components;
 
 const errors = [
   "ElementNotAccessibleError",
@@ -90,21 +90,49 @@ error.stringify = function(err) {
 };
 
 /**
- * Marshal an Error to a JSON structure.
+ * Marshal a WebDriverError prototype to a JSON dictionary.
  *
- * @param {Error} err
- *     The Error to serialise.
+ * @param {WebDriverError} err
+ *     Error to serialise.
  *
  * @return {Object.<string, Object>}
- *     JSON structure with the keys "error", "message", and "stacktrace".
+ *     JSON dictionary with the keys "error", "message", and "stacktrace".
+ * @throws {TypeError}
+ *     If error type is not serialisable.
  */
 error.toJson = function(err) {
+  if (!error.isWebDriverError(err)) {
+    throw new TypeError(`Unserialisable error type: ${err}`);
+  }
+
   let json = {
     error: err.status,
     message: err.message || null,
     stacktrace: err.stack || null,
   };
   return json;
+};
+
+/**
+ * Unmarshal a JSON dictionary to a WebDriverError prototype.
+ *
+ * @param {Object.<string, string>} json
+ *     JSON dictionary with the keys "error", "message", and "stacktrace".
+ *
+ * @return {WebDriverError}
+ *     Deserialised error prototype.
+ */
+error.fromJson = function(json) {
+  if (!statusLookup.has(json.error)) {
+    throw new TypeError(`Undeserialisable error type: ${json.error}`);
+  }
+
+  let errCls = statusLookup.get(json.error);
+  let err = new errCls(json.message);
+  if ("stacktrace" in json) {
+    err.stack = json.stacktrace;
+  }
+  return err;
 };
 
 /**
@@ -297,3 +325,12 @@ this.UnsupportedOperationError = function(msg) {
   this.status = "unsupported operation";
 };
 UnsupportedOperationError.prototype = Object.create(WebDriverError.prototype);
+
+const nameLookup = new Map();
+const statusLookup = new Map();
+for (let s of errors) {
+  let cls = this[s];
+  let inst = new cls();
+  nameLookup.set(inst.name, cls);
+  statusLookup.set(inst.status, cls);
+};
