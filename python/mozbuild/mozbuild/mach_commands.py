4250455fa4004ba85d2b81f180826f8f18f93a7a
Dan Minor <dminor@mozilla.com>
2016-02-01 14:41:13 -0500

    Bug 1244143 - Record whether or not an artifact build was used in build telemetry data r=gps
    This adds a substs field and cherrypicks the MOZ_ARTIFACT_BUILDS field so
    we can determine whether or not an artifact build occurred.
    
    --HG--
    extra : rebase_source : 8878751b0bf5159cb8baedc68c9ab88cf8563628

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -495,8 +495,17 @@ class Build(MachCommandBase):
 
             telemetry_handler = getattr(self._mach_context,
                                         'telemetry_handler', None)
-            usage = monitor.record_resource_usage()
-            telemetry_handler(self._mach_context, usage)
+            telemetry_data = monitor.record_resource_usage()
+
+            # Record build configuration data. For now, we cherry pick
+            # items we need rather than grabbing everything, in order
+            # to avoid accidentally disclosing PII.
+            telemetry_data['substs'] = {
+                'MOZ_ARTIFACT_BUILDS': self.substs.get('MOZ_ARTIFACT_BUILDS',
+                                                       False)
+            }
+
+            telemetry_handler(self._mach_context, telemetry_data)
 
         # Only for full builds because incremental builders likely don't
         # need to be burdened with this.
