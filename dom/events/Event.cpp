7f510b2c2d03bc4549c349d49c704077026d03c1
Andrea Marchesini <amarchesini@mozilla.com>
2016-02-01 21:48:04 +0000

    Bug 1243881 - patch 2 - unship performance.translateTime, r=bz, a=sylvestre

--- dom/events/Event.cpp
+++ dom/events/Event.cpp
@@ -1116,7 +1116,7 @@ Event::TimeStamp() const
   MOZ_ASSERT(workerPrivate);
 
   TimeDuration duration =
-    mEvent->timeStamp - workerPrivate->CreationTimeStamp();
+    mEvent->timeStamp - workerPrivate->NowBaseTimeStamp();
   return duration.ToMilliseconds();
 }
 
--- dom/workers/WorkerPrivate.h
+++ dom/workers/WorkerPrivate.h
@@ -192,6 +192,8 @@ private:
   WorkerType mWorkerType;
   TimeStamp mCreationTimeStamp;
   DOMHighResTimeStamp mCreationTimeHighRes;
+  TimeStamp mNowBaseTimeStamp;
+  DOMHighResTimeStamp mNowBaseTimeHighRes;
 
 protected:
   // The worker is owned by its thread, which is represented here.  This is set
@@ -551,6 +553,16 @@ public:
   DOMHighResTimeStamp CreationTime() const
   {
     return mCreationTimeHighRes;
+  }
+
+  TimeStamp NowBaseTimeStamp() const
+  {
+    return mNowBaseTimeStamp;
+  }
+
+  DOMHighResTimeStamp NowBaseTime() const
+  {
+    return mNowBaseTimeHighRes;
   }
 
   nsIPrincipal*
