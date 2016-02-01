20ebda5141a0d12cbab94a8d796a37df854bd230
Jakob Stoklund Olesen <jolesen@mozilla.com>
2016-02-01 14:55:06 -0800

    Bug 1240796 - Detemplatize getOrCreateSimdTypeDescr(). r=bbouvier
    This saves some code size in a cold function, and it makes it possible to pass
    in the SIMD type as a dynamic argument.
    
    Also detemplatize the static CreateSimdType() to save some code size.
    
    Replace all of the Get*TypeRepr() self-hosting functions with a single
    GetSimdTypeDescr() which takes one of the JS_SIMDTYPEREPR_* constants as an
    argument instead.
    
    Total code shrink ~ 32 KB.

--- js/src/vm/GlobalObject.h
+++ js/src/vm/GlobalObject.h
@@ -31,6 +31,7 @@ class StaticBlockScope;
 class ClonedBlockObject;
 
 class SimdTypeDescr;
+enum class SimdType : uint8_t;
 
 /*
  * Global object slots are reserved as follows:
@@ -444,22 +445,11 @@ class GlobalObject : public NativeObject
         return getOrCreateObject(cx, APPLICATION_SLOTS + JSProto_SIMD, initSimdObject);
     }
 
-    template<class /* SimdTypeDescriptor (cf SIMD.h) */ T>
-    static SimdTypeDescr*
-    getOrCreateSimdTypeDescr(JSContext* cx, Handle<GlobalObject*> global) {
-        RootedObject globalSimdObject(cx, global->getOrCreateSimdGlobalObject(cx));
-        if (!globalSimdObject)
-            return nullptr;
-        uint32_t typeSlotIndex = uint32_t(T::type);
-        if (globalSimdObject->as<NativeObject>().getReservedSlot(typeSlotIndex).isUndefined() &&
-            !GlobalObject::initSimdType(cx, global, typeSlotIndex))
-        {
-            return nullptr;
-        }
-        const Value& slot = globalSimdObject->as<NativeObject>().getReservedSlot(typeSlotIndex);
-        MOZ_ASSERT(slot.isObject());
-        return &slot.toObject().as<SimdTypeDescr>();
-    }
+    // Get the type descriptor for one of the SIMD types.
+    // simdType is one of the JS_SIMDTYPEREPR_* constants.
+    // Implemented in builtin/SIMD.cpp.
+    static SimdTypeDescr* getOrCreateSimdTypeDescr(JSContext* cx, Handle<GlobalObject*> global,
+                                                   SimdType simdType);
 
     TypedObjectModuleObject& getTypedObjectModule() const;
 
@@ -732,9 +722,9 @@ class GlobalObject : public NativeObject
     // Implemented in builtin/TypedObject.cpp
     static bool initTypedObjectModule(JSContext* cx, Handle<GlobalObject*> global);
 
-    // Implemented in builtim/SIMD.cpp
+    // Implemented in builtin/SIMD.cpp
     static bool initSimdObject(JSContext* cx, Handle<GlobalObject*> global);
-    static bool initSimdType(JSContext* cx, Handle<GlobalObject*> global, uint32_t simdTypeDescrType);
+    static bool initSimdType(JSContext* cx, Handle<GlobalObject*> global, SimdType simdType);
 
     static bool initStandardClasses(JSContext* cx, Handle<GlobalObject*> global);
     static bool initSelfHostingBuiltins(JSContext* cx, Handle<GlobalObject*> global,
