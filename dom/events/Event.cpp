b79222f6c166199102bf44957a3bc5d040bfcb6f
Andrea Marchesini <amarchesini@mozilla.com>
2016-02-01 21:48:04 +0000

    Bug 1243881 - patch 2 - unship performance.translateTime, r=bz, a=sylvestre

--- dom/events/Event.cpp
+++ dom/events/Event.cpp
@@ -1119,7 +1119,7 @@ Event::TimeStamp() const
   MOZ_ASSERT(workerPrivate);
 
   TimeDuration duration =
-    mEvent->timeStamp - workerPrivate->CreationTimeStamp();
+    mEvent->timeStamp - workerPrivate->NowBaseTimeStamp();
   return duration.ToMilliseconds();
 }
 
