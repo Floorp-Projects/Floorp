cd20962f914b75a1810e6ff752d19078518b13bb
Andrzej Hunt <ahunt@mozilla.com>
2016-02-01 15:36:38 -0800

    Bug 1243307 - Part 2: Ensure homescreen shortcut creation happens on background thread r=liuche, a=lizzard

--- mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
+++ mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
@@ -1157,7 +1157,12 @@ public class BrowserApp extends GeckoApp
                     final String title = tab.getDisplayTitle();
 
                     if (url != null && title != null) {
-                        GeckoAppShell.createShortcut(title, url);
+                        ThreadUtils.postToBackgroundThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                GeckoAppShell.createShortcut(title, url);
+                            }
+                        });
                     }
                 }
             }
