31fb709fc328018c9a05ec99bde476e58e2cc907
Blake Kaplan <mrbkap@gmail.com>
2016-02-02 13:44:11 -0800

    Bug 1243928 - Make these tests work in e10s. r=felipe

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_zoomrestore.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_zoomrestore.js
@@ -5,76 +5,60 @@
 // This test makes sure that about:privatebrowsing does not appear zoomed in
 // if there is already a zoom site pref for about:blank (bug 487656).
 
-function test() {
+add_task(function* test() {
   // initialization
-  waitForExplicitFinish();
   let windowsToClose = [];
   let windowsToReset = [];
 
-  function doTestWhenReady(aIsZoomedWindow, aWindow, aCallback) {
+  function promiseLocationChange() {
+    return new Promise(resolve => {
+      Services.obs.addObserver(function onLocationChange(subj, topic, data) {
+        Services.obs.removeObserver(onLocationChange, topic);
+        resolve();
+      }, "browser-fullZoom:location-change", false);
+    });
+  }
+
+  function promiseTestReady(aIsZoomedWindow, aWindow) {
     // Need to wait on two things, the ordering of which is not guaranteed:
     // (1) the page load, and (2) FullZoom's update to the new page's zoom
     // level.  FullZoom broadcasts "browser-fullZoom:location-change" when its
     // update is done.  (See bug 856366 for details.)
 
-    let n = 0;
 
     let browser = aWindow.gBrowser.selectedBrowser;
-    browser.addEventListener("load", function onLoad() {
-      browser.removeEventListener("load", onLoad, true);
-      if (++n == 2)
-        doTest(aIsZoomedWindow, aWindow, aCallback);
-    }, true);
-
-    Services.obs.addObserver(function onLocationChange(subj, topic, data) {
-      Services.obs.removeObserver(onLocationChange, topic);
-      if (++n == 2)
-        doTest(aIsZoomedWindow, aWindow, aCallback);
-    }, "browser-fullZoom:location-change", false);
-
-    browser.loadURI("about:blank");
+    return BrowserTestUtils.loadURI(browser, "about:blank").then(() => {
+      return Promise.all([ BrowserTestUtils.browserLoaded(browser),
+                           promiseLocationChange() ]);
+    }).then(() => doTest(aIsZoomedWindow, aWindow));
   }
 
-  function doTest(aIsZoomedWindow, aWindow, aCallback) {
+  function doTest(aIsZoomedWindow, aWindow) {
     if (aIsZoomedWindow) {
       is(aWindow.ZoomManager.zoom, 1,
          "Zoom level for freshly loaded about:blank should be 1");
       // change the zoom on the blank page
       aWindow.FullZoom.enlarge();
       isnot(aWindow.ZoomManager.zoom, 1, "Zoom level for about:blank should be changed");
-      aCallback();
       return;
     }
+
     // make sure the zoom level is set to 1
     is(aWindow.ZoomManager.zoom, 1, "Zoom level for about:privatebrowsing should be reset");
-    aCallback();
-  }
-
-  function finishTest() {
-    // cleanup
-    windowsToReset.forEach(function(win) {
-      win.FullZoom.reset();
-    });
-    windowsToClose.forEach(function(win) {
-      win.close();
-    });
-    finish();
   }
 
   function testOnWindow(options, callback) {
-    let win = whenNewWindowLoaded(options,
-      function(win) {
-        windowsToClose.push(win);
-        windowsToReset.push(win);
-        executeSoon(function() { callback(win); });
-      });
-  };
-
-  testOnWindow({}, function(win) {
-    doTestWhenReady(true, win, function() {
-      testOnWindow({private: true}, function(win) {
-        doTestWhenReady(false, win, finishTest);
-      });
+    return BrowserTestUtils.openNewBrowserWindow(options).then((win) => {
+      windowsToClose.push(win);
+      windowsToReset.push(win);
+      return win;
     });
-  });
-}
+  }
+
+  yield testOnWindow({}).then(win => promiseTestReady(true, win));
+  yield testOnWindow({private: true}).then(win => promiseTestReady(false, win));
+
+  // cleanup
+  windowsToReset.forEach((win) => win.FullZoom.reset());
+  yield Promise.all(windowsToClose.map(win => BrowserTestUtils.closeWindow(win)));
+});
