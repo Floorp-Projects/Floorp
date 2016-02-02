01aa65f585d2598ac6a9624ee5dc3dce905c5530
Mike de Boer <mdeboer@mozilla.com>
2016-02-02 15:52:54 +0100

    Bug 1154277 - imported patch bug-1154277-socialTests. r=Standard8, a=lizzard

--- browser/base/content/test/chat/browser_focus.js
+++ browser/base/content/test/chat/browser_focus.js
@@ -51,26 +51,38 @@ add_chat_task(function* testDefaultFocus() {
 });
 
 // Test default focus via user input.
-add_chat_task(function* testDefaultFocus() {
+add_chat_task(function* testDefaultFocusUserInput() {
+  todo(false, "BrowserTestUtils.synthesizeMouseAtCenter doesn't move the user " +
+    "focus to the chat window, even though we're recording a click correctly.");
+  return;
+
   yield setUp();
-  let tab = gBrowser.selectedTab;
+  let browser = gBrowser.selectedTab.linkedBrowser;
+  let mm = browser.messageManager;
+
   let deferred = Promise.defer();
-  let button = tab.linkedBrowser.contentDocument.getElementById("chat-opener");
-  button.addEventListener("click", function onclick() {
-    button.removeEventListener("click", onclick);
-    promiseOpenChat("http://example.com").then(
-      chat => deferred.resolve(chat)
-    );
-  })
+  mm.addMessageListener("ChatOpenerClicked", function handler() {
+    mm.removeMessageListener("ChatOpenerClicked", handler);
+    promiseOpenChat("http://example.com").then(chat => deferred.resolve(chat));
+  });
+
+  yield ContentTask.spawn(browser, null, function* () {
+    let button = content.document.getElementById("chat-opener");
+    button.addEventListener("click", function onclick() {
+      button.removeEventListener("click", onclick);
+      sendAsyncMessage("ChatOpenerClicked");
+    });
+  });
   // Note we must use synthesizeMouseAtCenter() rather than calling
   // .click() directly as this causes nsIDOMWindowUtils.isHandlingUserInput
   // to be true.
-  EventUtils.synthesizeMouseAtCenter(button, {}, button.ownerDocument.defaultView);
+  yield BrowserTestUtils.synthesizeMouseAtCenter("#chat-opener", {}, browser);
   let chat = yield deferred.promise;
 
   // we use the default focus behaviour but the chat was opened via user input,
   // so the chat should be focused.
   Assert.equal(numChatsInWindow(window), 1, "should be 1 chat open");
+  yield promiseWaitForCondition(() => !isTabFocused());
   Assert.ok(!isTabFocused(), "the tab should have lost focus.");
   Assert.ok(isChatFocused(chat), "the chat should have got focus.");
 });
@@ -82,6 +94,7 @@ add_chat_task(function* testExplicitFocus() {
   // we use the default focus behaviour, which means that because this was
   // not the direct result of user action the chat should not be focused.
   Assert.equal(numChatsInWindow(window), 1, "should be 1 chat open");
+  yield promiseWaitForCondition(() => !isTabFocused());
   Assert.ok(!isTabFocused(), "the tab should have lost focus.");
   Assert.ok(isChatFocused(chat), "the chat should have got focus.");
 });
@@ -114,7 +127,7 @@ add_chat_task(function* testFocusOnExplicitRestore() {
   Assert.ok(isTabFocused(), "tab should still be focused");
   Assert.ok(!isChatFocused(chat), "the chat should not be focused.");
 
-  let promise = promiseOneEvent(chat.contentWindow, "focus");
+  let promise = promiseOneMessage(chat.content, "Social:FocusEnsured");
   // pretend we clicked on the titlebar
   chat.onTitlebarClick({button: 0});
   yield promise; // wait for focus event.
@@ -131,13 +144,18 @@ add_chat_task(function* testMinimizeFocused() {
   let chat2 = yield promiseOpenChat("http://example.com#2");
   Assert.equal(numChatsInWindow(window), 2, "2 chats open");
   Assert.strictEqual(chatbar.selectedChat, chat2, "chat2 is selected");
-  let promise = promiseOneEvent(chat1.contentWindow, "focus");
+  let promise = promiseOneMessage(chat1.content, "Social:FocusEnsured");
   chatbar.selectedChat = chat1;
   chatbar.focus();
   yield promise; // wait for chat1 to get focus.
   Assert.strictEqual(chat1, chatbar.selectedChat, "chat1 is marked selected");
   Assert.notStrictEqual(chat2, chatbar.selectedChat, "chat2 is not marked selected");
-  promise = promiseOneEvent(chat2.contentWindow, "focus");
+
+  todo(false, "Bug XXX should re-enable the test below to have a chat window " +
+    "re-gain focus when another chat window is minimized.");
+  return;
+
+  promise = promiseOneMessage(chat2.content, "Social:FocusEnsured");
   chat1.minimized = true;
   yield promise; // wait for chat2 to get focus.
   Assert.notStrictEqual(chat1, chatbar.selectedChat, "chat1 is not marked selected");
@@ -151,37 +169,42 @@ add_chat_task(function* testTab() {
   yield setUp();
 
   function sendTabAndWaitForFocus(chat, eltid) {
-    let doc = chat.contentDocument;
     EventUtils.sendKey("tab");
-    // ideally we would use the 'focus' event here, but that doesn't work
-    // as expected for the iframe - the iframe itself never gets the focus
-    // event (apparently the sub-document etc does.)
-    // So just poll for the correct element getting focus...
-    let deferred = Promise.defer();
-    let tries = 0;
-    let interval = setInterval(function() {
-      if (tries >= 30) {
-        clearInterval(interval);
-        deferred.reject("never got focus");
-        return;
-      }
-      tries ++;
-      let elt = eltid ? doc.getElementById(eltid) : doc.documentElement;
-      if (doc.activeElement == elt) {
-        clearInterval(interval);
-        deferred.resolve();
-      }
-      info("retrying wait for focus: " + tries);
-      info("(the active element is " + doc.activeElement + "/" + doc.activeElement.getAttribute("id") + ")");
-    }, 100);
-    info("waiting for element " + eltid + " to get focus");
-    return deferred.promise;
+
+    return ContentTask.spawn(chat.content, { eltid: eltid }, function* (args) {
+      let doc = content.document;
+
+      // ideally we would use the 'focus' event here, but that doesn't work
+      // as expected for the iframe - the iframe itself never gets the focus
+      // event (apparently the sub-document etc does.)
+      // So just poll for the correct element getting focus...
+      yield new Promise(function(resolve, reject) {
+        let tries = 0;
+        let interval = setInterval(function() {
+          if (tries >= 30) {
+            clearInterval(interval);
+            reject("never got focus");
+            return;
+          }
+          tries++;
+          let elt = args.eltid ? doc.getElementById(args.eltid) : doc.documentElement;
+          if (doc.activeElement == elt) {
+            clearInterval(interval);
+            resolve();
+          }
+          info("retrying wait for focus: " + tries);
+          info("(the active element is " + doc.activeElement + "/" +
+            doc.activeElement.getAttribute("id") + ")");
+        }, 100);
+        info("waiting for element " + args.eltid + " to get focus");
+      });
+    });
   }
 
   let chat1 = yield promiseOpenChat(CHAT_URL + "#1");
   let chat2 = yield promiseOpenChat(CHAT_URL + "#2");
   chatbar.selectedChat = chat2;
-  let promise = promiseOneEvent(chat2.contentWindow, "focus");
+  let promise = promiseOneMessage(chat2.content, "Social:FocusEnsured");
   chatbar.focus();
   info("waiting for second chat to get focus");
   yield promise;
@@ -189,19 +212,13 @@ add_chat_task(function* testTab() {
   // Our chats have 3 focusable elements, so it takes 4 TABs to move
   // to the new chat.
   yield sendTabAndWaitForFocus(chat2, "input1");
-  Assert.equal(chat2.contentDocument.activeElement.getAttribute("id"), "input1",
-               "first input field has focus");
   Assert.ok(isChatFocused(chat2), "new chat still focused after first tab");
 
   yield sendTabAndWaitForFocus(chat2, "input2");
   Assert.ok(isChatFocused(chat2), "new chat still focused after tab");
-  Assert.equal(chat2.contentDocument.activeElement.getAttribute("id"), "input2",
-               "second input field has focus");
 
   yield sendTabAndWaitForFocus(chat2, "iframe");
   Assert.ok(isChatFocused(chat2), "new chat still focused after tab");
-  Assert.equal(chat2.contentDocument.activeElement.getAttribute("id"), "iframe",
-               "iframe has focus");
 
   // this tab now should move to the next chat, but focus the
   // document element itself (hence the null eltid)
@@ -218,7 +235,9 @@ add_chat_task(function* testFocusedElement() {
   // open a chat with focus requested.
   let chat = yield promiseOpenChat(CHAT_URL, undefined, true);
 
-  chat.contentDocument.getElementById("input2").focus();
+  yield ContentTask.spawn(chat.content, null, function* () {
+    content.document.getElementById("input2").focus();
+  });
 
   // set focus to the tab.
   let tabb = gBrowser.getBrowserForTab(gBrowser.selectedTab);
@@ -226,10 +245,12 @@ add_chat_task(function* testFocusedElement() {
   Services.focus.moveFocus(tabb.contentWindow, null, Services.focus.MOVEFOCUS_ROOT, 0);
   yield promise;
 
-  promise = promiseOneEvent(chat.contentWindow, "focus");
+  promise = promiseOneMessage(chat.content, "Social:FocusEnsured");
   chatbar.focus();
   yield promise;
 
-  Assert.equal(chat.contentDocument.activeElement.getAttribute("id"), "input2",
-               "correct input field still has focus");
+  yield ContentTask.spawn(chat.content, null, function* () {
+    is(content.document.activeElement.getAttribute("id"), "input2",
+      "correct input field still has focus");
+  });
 });
