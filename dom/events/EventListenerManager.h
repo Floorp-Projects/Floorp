90452e08a2f3b6b655556994ce157d33da8c9faf
Daniel Holbert <dholbert@cs.stanford.edu>
2016-02-02 10:03:49 -0800

    Bug 1236979 part 3: If there are no listeners for a transition or animation event, check listeners again using a webkit-prefixed event name. r=smaug, a=lizzard
    --HG--
    extra : source : 23c492f3f6dba14756b0b59b4b3c5fc5484a3571

--- dom/events/EventListenerManager.h
+++ dom/events/EventListenerManager.h
@@ -458,6 +458,13 @@ protected:
                               nsIDOMEvent* aDOMEvent,
                               dom::EventTarget* aCurrentTarget);
 
+  /**
+   * If the given EventMessage has a legacy version that we support, then this
+   * function returns that legacy version. Otherwise, this function simply
+   * returns the passed-in EventMessage.
+   */
+  EventMessage GetLegacyEventMessage(EventMessage aEventMessage) const;
+
   nsIDocShell* GetDocShellForTarget();
 
   /**
@@ -571,7 +578,9 @@ protected:
   nsPIDOMWindow* GetInnerWindowForTarget();
   already_AddRefed<nsPIDOMWindow> GetTargetAsInnerWindow() const;
 
-  bool ListenerCanHandle(Listener* aListener, WidgetEvent* aEvent);
+  bool ListenerCanHandle(const Listener* aListener,
+                         const WidgetEvent* aEvent,
+                         EventMessage aEventMessage) const;
 
   // BE AWARE, a lot of instances of EventListenerManager will be created.
   // Therefor, we need to keep this class compact.  When you add integer
