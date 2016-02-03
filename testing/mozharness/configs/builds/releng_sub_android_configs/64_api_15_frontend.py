1c95416d9a07396853d08b763152584f61570e51
Nick Alexander <nalexander@mozilla.com>
2016-02-02 18:09:44 -0800

    Bug 1247375 - Part 1: Add TaskCluster job building Fennec with --disable-compile-environment. r=dustin
    MozReview-Commit-ID: 4FDevhlYnUC
    
    --HG--
    extra : rebase_source : 72c9458ec310432b8c8caf99f797c7dd35a070c0
    extra : amend_source : 4ff552fb4c877aa2388434fa46b4bb779616a9ac

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_api_15_frontend.py
@@ -0,0 +1,8 @@
+config = {
+    'base_name': 'Android armv7 API 15+ frontend %(branch)s',
+    'stage_platform': 'android-api-15-frontend',
+    'build_type': 'api-15-opt',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-15-frontend/nightly',
+    'tooltool_manifest_src': 'mobile/android/config/tooltool-manifests/android-frontend/releng.manifest',
+    'multi_locale_config_platform': 'android',
+}
--- testing/mozharness/mozharness/mozilla/building/buildbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildbase.py
@@ -357,6 +357,7 @@ class BuildOptionParser(object):
         'source': 'builds/releng_sub_%s_configs/%s_source.py',
         'api-9': 'builds/releng_sub_%s_configs/%s_api_9.py',
         'api-11': 'builds/releng_sub_%s_configs/%s_api_11.py',
+        'api-15-frontend': 'builds/releng_sub_%s_configs/%s_api_15_frontend.py',
         'api-15': 'builds/releng_sub_%s_configs/%s_api_15.py',
         'api-9-debug': 'builds/releng_sub_%s_configs/%s_api_9_debug.py',
         'api-11-debug': 'builds/releng_sub_%s_configs/%s_api_11_debug.py',
