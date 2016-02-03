fd41760c1788d3d2b21744827070bc576ba77fe9
Kyle Machulis <kyle@nonpolynomial.com>
2016-02-03 10:42:08 -0800

    Bug 1171182 - CSS Zoom Query for NPAPI; r=jaas r=bsmedberg

--- layout/generic/nsPluginFrame.cpp
+++ layout/generic/nsPluginFrame.cpp
@@ -1218,8 +1218,9 @@ nsPluginFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
 #endif
 
   if (aBuilder->IsForPainting() && mInstanceOwner) {
-#ifdef XP_MACOSX
+    // Update plugin frame for both content scaling and full zoom changes.
     mInstanceOwner->ResolutionMayHaveChanged();
+#ifdef XP_MACOSX
     mInstanceOwner->WindowFocusMayHaveChanged();
 #endif
     if (mInstanceOwner->UseAsyncRendering()) {
