defefe7ec361215d9ed5f706f937f23093bcc67b
Aaron Klotz <aklotz@mozilla.com>
2016-02-03 16:21:17 -0700

    Bug 1240848: Adds additional instructions to x64 detour patcher and prevents register clobbering in jmp from trampoline; r=ehsan

--- toolkit/xre/test/win/TestDllInterceptor.cpp
+++ toolkit/xre/test/win/TestDllInterceptor.cpp
@@ -159,6 +159,7 @@ int main()
       TestHook("gdi32.dll", "CreateDIBSection") &&
       TestHook("kernel32.dll", "CreateFileW") &&
 #endif
+      TestHook("user32.dll", "CreateWindowExW") &&
       TestHook("imm32.dll", "ImmGetContext") &&
       TestHook("imm32.dll", "ImmGetCompositionStringW") &&
       TestHook("imm32.dll", "ImmSetCandidateWindow") &&
