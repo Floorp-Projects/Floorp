628cee03d9cbbfc8b7adebb2fd054a218f78afba
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-02-04 16:42:01 +0100

    Backed out changeset 3cd839e515b9 (bug 1245420) for timeouts in browser_responsiveruleview.js

--- devtools/client/framework/test/browser_toolbox_textbox_context_menu.js
+++ devtools/client/framework/test/browser_toolbox_textbox_context_menu.js
@@ -4,7 +4,8 @@
 const URL = "data:text/html;charset=utf8,test for textbox context menu";
 
 add_task(function*() {
-  let toolbox = yield openNewTabAndToolbox(URL, "inspector");
+  let tab = yield addTab(URL);
+  let toolbox = yield gDevTools.showToolbox(TargetFactory.forTab(tab));
   let textboxContextMenu = toolbox.textboxContextMenuPopup;
 
   ok(textboxContextMenu, "The textbox context menu is loaded in the toolbox");
