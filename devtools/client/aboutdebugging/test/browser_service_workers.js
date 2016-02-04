6adfe3364960b0bb05a8d73be395034bbd6dc34f
Joe Whitfield-Seed <joeseed86@gmail.com>
2016-02-04 20:10:08 +0000

    Bug 1245496 - Fix eslint errors in about:debugging. r=janx

--- devtools/client/aboutdebugging/test/browser_service_workers.js
+++ devtools/client/aboutdebugging/test/browser_service_workers.js
@@ -1,5 +1,9 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/* eslint-disable mozilla/no-cpows-in-tests */
+/* global sendAsyncMessage */
+
 "use strict";
 
 // Service workers can't be loaded from chrome://,
@@ -9,11 +13,11 @@ const HTTP_ROOT = CHROME_ROOT.replace("chrome://mochitests/content/",
 const SERVICE_WORKER = HTTP_ROOT + "service-workers/empty-sw.js";
 const TAB_URL = HTTP_ROOT + "service-workers/empty-sw.html";
 
-add_task(function *() {
+add_task(function* () {
   yield new Promise(done => {
     let options = {"set": [
-                    ["dom.serviceWorkers.testing.enabled", true],
-                  ]};
+      ["dom.serviceWorkers.testing.enabled", true],
+    ]};
     SpecialPowers.pushPrefEnv(options, done);
   });
 
@@ -27,21 +31,22 @@ add_task(function *() {
   // Check that the service worker appears in the UI
   let names = [...document.querySelectorAll("#service-workers .target-name")];
   names = names.map(element => element.textContent);
-  ok(names.includes(SERVICE_WORKER), "The service worker url appears in the list: " + names);
+  ok(names.includes(SERVICE_WORKER),
+    "The service worker url appears in the list: " + names);
 
   // Finally, unregister the service worker itself
   let aboutDebuggingUpdate = waitForMutation(serviceWorkersElement,
     { childList: true });
 
   // Use message manager to work with e10s
-  let frameScript = function () {
+  let frameScript = function() {
     // Retrieve the `sw` promise created in the html page
     let { sw } = content.wrappedJSObject;
-    sw.then(function (registration) {
-      registration.unregister().then(function (success) {
+    sw.then(function(registration) {
+      registration.unregister().then(function() {
         sendAsyncMessage("sw-unregistered");
       },
-      function (e) {
+      function(e) {
         dump("SW not unregistered; " + e + "\n");
       });
     });
@@ -62,7 +67,8 @@ add_task(function *() {
   // Check that the service worker disappeared from the UI
   names = [...document.querySelectorAll("#service-workers .target-name")];
   names = names.map(element => element.textContent);
-  ok(!names.includes(SERVICE_WORKER), "The service worker url is no longer in the list: " + names);
+  ok(!names.includes(SERVICE_WORKER),
+    "The service worker url is no longer in the list: " + names);
 
   yield removeTab(swTab);
   yield closeAboutDebugging(tab);
