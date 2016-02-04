78f7c1fc6b9d4b92c515bdcb9473addd61e7b92f
Mike de Boer <mdeboer@mozilla.com>
2016-02-04 13:42:10 +0100

    Bug 1154277: Part 3 - implement test changes to work in e10s mode as well. r=mixedpuppy,Standard8, a=lizzard

--- browser/base/content/test/chat/head.js
+++ browser/base/content/test/chat/head.js
@@ -15,18 +15,12 @@ function promiseOpenChat(url, mode, focus, buttonSet = null) {
   // we just through a few hoops to ensure the content document is fully
   // loaded, otherwise tests that rely on that content may intermittently fail.
   let callback = function(chatbox) {
-    if (chatbox.contentDocument.readyState == "complete") {
-      // already loaded.
+    let mm = chatbox.content.messageManager;
+    mm.sendAsyncMessage("WaitForDOMContentLoaded");
+    mm.addMessageListener("DOMContentLoaded", function cb() {
+      mm.removeMessageListener("DOMContentLoaded", cb);
       deferred.resolve(chatbox);
-      return;
-    }
-    chatbox.addEventListener("load", function onload(event) {
-      if (event.target != chatbox.contentDocument || chatbox.contentDocument.location.href == "about:blank") {
-        return;
-      }
-      chatbox.removeEventListener("load", onload, true);
-      deferred.resolve(chatbox);
-    }, true);
+    });
   }
   let chatbox = Chat.open(null, {
     origin: origin,
@@ -68,6 +62,16 @@ function promiseOneEvent(target, eventName, capture) {
   return deferred.promise;
 }
 
+function promiseOneMessage(target, messageName) {
+  return new Promise(resolve => {
+    let mm = target.messageManager;
+    mm.addMessageListener(messageName, function handler() {
+      mm.removeMessageListener(messageName, handler);
+      resolve();
+    });
+  });
+}
+
 // Return the number of chats in a browser window.
 function numChatsInWindow(win) {
   let chatbar = win.document.getElementById("pinnedchats");
@@ -102,5 +106,33 @@ function add_chat_task(genFunction) {
         win.close();
       }
     }
+  });
+}
+
+function waitForCondition(condition, nextTest, errorMsg) {
+  var tries = 0;
+  var interval = setInterval(function() {
+    if (tries >= 100) {
+      ok(false, errorMsg);
+      moveOn();
+    }
+    var conditionPassed;
+    try {
+      conditionPassed = condition();
+    } catch (e) {
+      ok(false, e + "\n" + e.stack);
+      conditionPassed = false;
+    }
+    if (conditionPassed) {
+      moveOn();
+    }
+    tries++;
+  }, 100);
+  var moveOn = function() { clearInterval(interval); nextTest(); };
+}
+
+function promiseWaitForCondition(aConditionFn) {
+  return new Promise((resolve, reject) => {
+    waitForCondition(aConditionFn, resolve, "Condition didn't pass.");
   });
 }
