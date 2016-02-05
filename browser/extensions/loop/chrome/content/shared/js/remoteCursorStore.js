773448dc0c5426950d4a381bb4df20e31f77c96f
Mark Banner <standard8@mozilla.com>
2016-02-05 09:20:30 +0000

    Bug 1245912 - Update Loop system add-on to version 0.3.0. r=Standard8 for already reviewed code,a=lizzard

--- browser/extensions/loop/chrome/content/shared/js/remoteCursorStore.js
+++ browser/extensions/loop/chrome/content/shared/js/remoteCursorStore.js
@@ -16,7 +16,8 @@ loop.store.RemoteCursorStore = (function() {
   var RemoteCursorStore = loop.store.createStore({
     actions: [
       "receivedCursorData",
-      "videoDimensionsChanged"
+      "videoDimensionsChanged",
+      "videoScreenStreamChanged"
     ],
 
     /**
@@ -98,6 +99,21 @@ loop.store.RemoteCursorStore = (function() {
           height: actionData.dimensions.height,
           width: actionData.dimensions.width
         }
+      });
+    },
+
+    /**
+     * Listen to screen stream changes.
+     *
+     * @param {sharedActions.VideoScreenStreamChanged} actionData
+     */
+    videoScreenStreamChanged: function(actionData) {
+      if (actionData.hasVideo) {
+        return;
+      }
+
+      this.setStoreState({
+        remoteCursorPosition: null
       });
     }
   });
