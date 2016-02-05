a941690bae3e1d38f67258cf99a82f4778ba322b
Margaret Leibovic <margaret.leibovic@gmail.com>
2016-02-05 12:56:23 -0500

    Bug 1225842 - Properly send message to update open search state. r=liuche, a=lizzard
    MozReview-Commit-ID: 77s1zWo9vrr

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -4098,7 +4098,7 @@ Tab.prototype = {
     }
   },
 
-  makeOpenSearchMessage: function(eventTarget) {
+  sendOpenSearchMessage: function(eventTarget) {
     let type = eventTarget.type && eventTarget.type.toLowerCase();
     // Replace all starting or trailing spaces or spaces before "*;" globally w/ "".
     type = type.replace(/^\s+|\s*(?:;.*)?$/g, "");
@@ -4144,11 +4144,11 @@ Tab.prototype = {
           return null;
 
         // Broadcast message that this tab contains search engines that should be visible.
-        return {
+        Messaging.sendRequest({
           type: "Link:OpenSearch",
           tabID: this.id,
           visible: true
-        };
+        });
       });
     }
   },
@@ -4273,7 +4273,7 @@ Tab.prototype = {
 
           jsonMessage = this.makeFeedMessage(target, type);
         } else if (list.indexOf("[search]" != -1) && aEvent.type == "DOMLinkAdded") {
-          jsonMessage = this.makeOpenSearchMessage(target);
+          this.sendOpenSearchMessage(target);
         }
         if (!jsonMessage)
          return;
