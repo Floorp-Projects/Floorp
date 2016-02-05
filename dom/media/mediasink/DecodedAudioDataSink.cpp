deba97f0ffffe7e6d7a8d0adcb68185895d39600
JW Wang <jwwang@mozilla.com>
2016-02-05 10:19:12 +0800

    Bug 1245542 - I suspect AudioData::mAudioData/mFrames are poisoned when sample format doesn't match the metadata. Let's ignore these samples to see if crash volume can be reduced. r=kinetik. a=lizzard
    --HG--
    extra : commitid : LnzVgSV1dFd
    extra : source : ffdf364dd29511ad1a0eb517b0b085719050a8f4

--- dom/media/mediasink/DecodedAudioDataSink.cpp
+++ dom/media/mediasink/DecodedAudioDataSink.cpp
@@ -4,6 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "nsPrintfCString.h"
 #include "MediaQueue.h"
 #include "DecodedAudioDataSink.h"
 #include "VideoUtils.h"
@@ -208,8 +209,19 @@ DecodedAudioDataSink::PopFrames(uint32_t aFrames)
       return MakeUnique<Chunk>();
     }
 
+    AudioData* a = AudioQueue().PeekFront()->As<AudioData>();
+
     // Ignore the element with 0 frames and try next.
-    if (AudioQueue().PeekFront()->mFrames == 0) {
+    if (a->mFrames == 0) {
+      RefPtr<MediaData> releaseMe = AudioQueue().PopFront();
+      continue;
+    }
+
+    // Ignore invalid samples.
+    if (a->mRate != mInfo.mRate || a->mChannels != mInfo.mChannels) {
+      NS_WARNING(nsPrintfCString(
+        "mismatched sample format, data=%p rate=%u channels=%u frames=%u",
+        a->mAudioData.get(), a->mRate, a->mChannels, a->mFrames).get());
       RefPtr<MediaData> releaseMe = AudioQueue().PopFront();
       continue;
     }
