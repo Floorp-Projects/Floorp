4c4d432d3a7562463992fe6fe41a61f4515791a8
Andreas Tolfsen <ato@mozilla.com>
2016-02-05 14:14:15 +0000

    Bug 1245153 - Use EventUtils.js from mochikit; r=pbrosset
    testing/marionette/EventUtils.js has been converted to a JS module in
    testing/marionette/event.js and its API has changed.  It was originally
    a copy of testing/mochitest/tests/SimpleTest/EventUtils.js, and it should
    be fine to use the original instead.
    
    MozReview-Commit-ID: Exi9d5rEeOz
    
    --HG--
    extra : histedit_source : 999974c56edeecb1313d2b1050c8911473c02292
    extra : rebase_source : 77656e161e67423cd44d2071e4118e824cc64363
    extra : commitid : 5SGljZj56xm
    extra : source : 18d54b8d4ae8348d62070b57574858f0824474ad
    extra : amend_source : 968c4d6c4959f5ba502b846294aabe40296d6541

--- devtools/client/debugger/test/mochitest/code_frame-script.js
+++ devtools/client/debugger/test/mochitest/code_frame-script.js
@@ -4,8 +4,17 @@ var { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 const { loadSubScript } = Cc['@mozilla.org/moz/jssubscript-loader;1'].
                           getService(Ci.mozIJSSubScriptLoader);
 
-const EventUtils = {};
-loadSubScript("chrome://marionette/content/EventUtils.js", EventUtils);
+// Set up a dummy environment so that EventUtils works. We need to be careful to
+// pass a window object into each EventUtils method we call rather than having
+// it rely on the |window| global.
+let EventUtils = {};
+EventUtils.window = content;
+EventUtils.parent = EventUtils.window;
+EventUtils._EU_Ci = Components.interfaces;
+EventUtils._EU_Cc = Components.classes;
+EventUtils.navigator = content.navigator;
+EventUtils.KeyboardEvent = content.KeyboardEvent;
+loadSubScript("chrome://mochikit/content/tests/SimpleTest/EventUtils.js", EventUtils);
 
 dump("Frame script loaded.\n");
 
