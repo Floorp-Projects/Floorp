d07b3125f892ee32f6a839ad28091b22719b344d
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-02-05 14:18:19 +0100

    Backed out 16 changesets (bug 1245153) for mochitest test-bustage on a CLOSED TREE
    Backed out changeset 30c8ec933737 (bug 1245153)
    Backed out changeset 4c2b1902d7cd (bug 1245153)
    Backed out changeset 1be5f60393a0 (bug 1245153)
    Backed out changeset 22321e6b65e9 (bug 1245153)
    Backed out changeset c1e0abbfa66e (bug 1245153)
    Backed out changeset 80ae953819c8 (bug 1245153)
    Backed out changeset 3edb67388ad6 (bug 1245153)
    Backed out changeset 55f64197f6b5 (bug 1245153)
    Backed out changeset 6bb93562a576 (bug 1245153)
    Backed out changeset 5da7628c3767 (bug 1245153)
    Backed out changeset bd41e4ab829d (bug 1245153)
    Backed out changeset ee7ee24cc65e (bug 1245153)
    Backed out changeset 4b2a5ee7199e (bug 1245153)
    Backed out changeset d75ad1397656 (bug 1245153)
    Backed out changeset 1f5e37f8e446 (bug 1245153)
    Backed out changeset aec0a0166685 (bug 1245153)
    
    --HG--
    rename : testing/marionette/event.js => testing/marionette/EventUtils.js
    rename : testing/marionette/action.js => testing/marionette/actions.js
    rename : testing/marionette/atom.js => testing/marionette/atoms/atoms.js
    rename : testing/marionette/element.js => testing/marionette/elements.js
    rename : testing/marionette/frame.js => testing/marionette/frame-manager.js
    rename : testing/marionette/interaction.js => testing/marionette/interactions.js
    extra : rebase_source : 11bba1116e87ed79fa72f24ff41216a6e3fb00d6

---
+++ testing/marionette/ChromeUtils.js
@@ -0,0 +1,291 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * ChromeUtils.js is a set of mochitest utilities that are used to 
+ * synthesize events in the browser.  These are only used by 
+ * mochitest-chrome and browser-chrome tests.  Originally these functions were in 
+ * EventUtils.js, but when porting to specialPowers, we didn't want
+ * to move unnecessary functions.
+ *
+ * ChromeUtils.js depends on EventUtils.js being loaded.
+ *
+ */
+
+/**
+ * Synthesize a query text content event.
+ *
+ * @param aOffset  The character offset.  0 means the first character in the
+ *                 selection root.
+ * @param aLength  The length of getting text.  If the length is too long,
+ *                 the extra length is ignored.
+ * @param aWindow  Optional (If null, current |window| will be used)
+ * @return         An nsIQueryContentEventResult object.  If this failed,
+ *                 the result might be null.
+ */
+function synthesizeQueryTextContent(aOffset, aLength, aWindow)
+{
+  var utils = _getDOMWindowUtils(aWindow);
+  if (!utils) {
+    return nullptr;
+  }
+  return utils.sendQueryContentEvent(utils.QUERY_TEXT_CONTENT,
+                                     aOffset, aLength, 0, 0);
+}
+
+/**
+ * Synthesize a query caret rect event.
+ *
+ * @param aOffset  The caret offset.  0 means left side of the first character
+ *                 in the selection root.
+ * @param aWindow  Optional (If null, current |window| will be used)
+ * @return         An nsIQueryContentEventResult object.  If this failed,
+ *                 the result might be null.
+ */
+function synthesizeQueryCaretRect(aOffset, aWindow)
+{
+  var utils = _getDOMWindowUtils(aWindow);
+  if (!utils) {
+    return nullptr;
+  }
+  return utils.sendQueryContentEvent(utils.QUERY_CARET_RECT,
+                                     aOffset, 0, 0, 0);
+}
+
+/**
+ * Synthesize a query text rect event.
+ *
+ * @param aOffset  The character offset.  0 means the first character in the
+ *                 selection root.
+ * @param aLength  The length of the text.  If the length is too long,
+ *                 the extra length is ignored.
+ * @param aWindow  Optional (If null, current |window| will be used)
+ * @return         An nsIQueryContentEventResult object.  If this failed,
+ *                 the result might be null.
+ */
+function synthesizeQueryTextRect(aOffset, aLength, aWindow)
+{
+  var utils = _getDOMWindowUtils(aWindow);
+  if (!utils) {
+    return nullptr;
+  }
+  return utils.sendQueryContentEvent(utils.QUERY_TEXT_RECT,
+                                     aOffset, aLength, 0, 0);
+}
+
+/**
+ * Synthesize a query editor rect event.
+ *
+ * @param aWindow  Optional (If null, current |window| will be used)
+ * @return         An nsIQueryContentEventResult object.  If this failed,
+ *                 the result might be null.
+ */
+function synthesizeQueryEditorRect(aWindow)
+{
+  var utils = _getDOMWindowUtils(aWindow);
+  if (!utils) {
+    return nullptr;
+  }
+  return utils.sendQueryContentEvent(utils.QUERY_EDITOR_RECT, 0, 0, 0, 0);
+}
+
+/**
+ * Synthesize a character at point event.
+ *
+ * @param aX, aY   The offset in the client area of the DOM window.
+ * @param aWindow  Optional (If null, current |window| will be used)
+ * @return         An nsIQueryContentEventResult object.  If this failed,
+ *                 the result might be null.
+ */
+function synthesizeCharAtPoint(aX, aY, aWindow)
+{
+  var utils = _getDOMWindowUtils(aWindow);
+  if (!utils) {
+    return nullptr;
+  }
+  return utils.sendQueryContentEvent(utils.QUERY_CHARACTER_AT_POINT,
+                                     0, 0, aX, aY);
+}
+
+/**
+ * Emulate a dragstart event.
+ *  element - element to fire the dragstart event on
+ *  expectedDragData - the data you expect the data transfer to contain afterwards
+ *                      This data is in the format:
+ *                         [ [ {type: value, data: value, test: function}, ... ], ... ]
+ *                     can be null
+ *  aWindow - optional; defaults to the current window object.
+ *  x - optional; initial x coordinate
+ *  y - optional; initial y coordinate
+ * Returns null if data matches.
+ * Returns the event.dataTransfer if data does not match
+ *
+ * eqTest is an optional function if comparison can't be done with x == y;
+ *   function (actualData, expectedData) {return boolean}
+ *   @param actualData from dataTransfer
+ *   @param expectedData from expectedDragData
+ * see bug 462172 for example of use
+ *
+ */
+function synthesizeDragStart(element, expectedDragData, aWindow, x, y)
+{
+  if (!aWindow)
+    aWindow = window;
+  x = x || 2;
+  y = y || 2;
+  const step = 9;
+
+  var result = "trapDrag was not called";
+  var trapDrag = function(event) {
+    try {
+      var dataTransfer = event.dataTransfer;
+      result = null;
+      if (!dataTransfer)
+        throw "no dataTransfer";
+      if (expectedDragData == null ||
+          dataTransfer.mozItemCount != expectedDragData.length)
+        throw dataTransfer;
+      for (var i = 0; i < dataTransfer.mozItemCount; i++) {
+        var dtTypes = dataTransfer.mozTypesAt(i);
+        if (dtTypes.length != expectedDragData[i].length)
+          throw dataTransfer;
+        for (var j = 0; j < dtTypes.length; j++) {
+          if (dtTypes[j] != expectedDragData[i][j].type)
+            throw dataTransfer;
+          var dtData = dataTransfer.mozGetDataAt(dtTypes[j],i);
+          if (expectedDragData[i][j].eqTest) {
+            if (!expectedDragData[i][j].eqTest(dtData, expectedDragData[i][j].data))
+              throw dataTransfer;
+          }
+          else if (expectedDragData[i][j].data != dtData)
+            throw dataTransfer;
+        }
+      }
+    } catch(ex) {
+      result = ex;
+    }
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  aWindow.addEventListener("dragstart", trapDrag, false);
+  synthesizeMouse(element, x, y, { type: "mousedown" }, aWindow);
+  x += step; y += step;
+  synthesizeMouse(element, x, y, { type: "mousemove" }, aWindow);
+  x += step; y += step;
+  synthesizeMouse(element, x, y, { type: "mousemove" }, aWindow);
+  aWindow.removeEventListener("dragstart", trapDrag, false);
+  synthesizeMouse(element, x, y, { type: "mouseup" }, aWindow);
+  return result;
+}
+
+/**
+ * Emulate a drop by emulating a dragstart and firing events dragenter, dragover, and drop.
+ *  srcElement - the element to use to start the drag, usually the same as destElement
+ *               but if destElement isn't suitable to start a drag on pass a suitable
+ *               element for srcElement
+ *  destElement - the element to fire the dragover, dragleave and drop events
+ *  dragData - the data to supply for the data transfer
+ *                     This data is in the format:
+ *                       [ [ {type: value, data: value}, ...], ... ]
+ *  dropEffect - the drop effect to set during the dragstart event, or 'move' if null
+ *  aWindow - optional; defaults to the current window object.
+ *  eventUtils - optional; allows you to pass in a reference to EventUtils.js. 
+ *               If the eventUtils parameter is not passed in, we assume EventUtils.js is 
+ *               in the scope. Used by browser-chrome tests.
+ *
+ * Returns the drop effect that was desired.
+ */
+function synthesizeDrop(srcElement, destElement, dragData, dropEffect, aWindow, eventUtils)
+{
+  if (!aWindow)
+    aWindow = window;
+
+  var synthesizeMouseAtCenter = (eventUtils || window).synthesizeMouseAtCenter;
+  var synthesizeMouse = (eventUtils || window).synthesizeMouse;
+
+  var gWindowUtils  = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor).
+                             getInterface(Components.interfaces.nsIDOMWindowUtils);
+  var ds = Components.classes["@mozilla.org/widget/dragservice;1"].
+           getService(Components.interfaces.nsIDragService);
+
+  var dataTransfer;
+  var trapDrag = function(event) {
+    dataTransfer = event.dataTransfer;
+    for (var i = 0; i < dragData.length; i++) {
+      var item = dragData[i];
+      for (var j = 0; j < item.length; j++) {
+        dataTransfer.mozSetDataAt(item[j].type, item[j].data, i);
+      }
+    }
+    dataTransfer.dropEffect = dropEffect || "move";
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  ds.startDragSession();
+
+  try {
+    // need to use real mouse action
+    aWindow.addEventListener("dragstart", trapDrag, true);
+    synthesizeMouseAtCenter(srcElement, { type: "mousedown" }, aWindow);
+
+    var rect = srcElement.getBoundingClientRect();
+    var x = rect.width / 2;
+    var y = rect.height / 2;
+    synthesizeMouse(srcElement, x, y, { type: "mousemove" }, aWindow);
+    synthesizeMouse(srcElement, x+10, y+10, { type: "mousemove" }, aWindow);
+    aWindow.removeEventListener("dragstart", trapDrag, true);
+
+    event = aWindow.document.createEvent("DragEvents");
+    event.initDragEvent("dragenter", true, true, aWindow, 0, 0, 0, 0, 0, false, false, false, false, 0, null, dataTransfer);
+    gWindowUtils.dispatchDOMEventViaPresShell(destElement, event, true);
+
+    var event = aWindow.document.createEvent("DragEvents");
+    event.initDragEvent("dragover", true, true, aWindow, 0, 0, 0, 0, 0, false, false, false, false, 0, null, dataTransfer);
+    if (gWindowUtils.dispatchDOMEventViaPresShell(destElement, event, true)) {
+      synthesizeMouseAtCenter(destElement, { type: "mouseup" }, aWindow);
+      return "none";
+    }
+
+    if (dataTransfer.dropEffect != "none") {
+      event = aWindow.document.createEvent("DragEvents");
+      event.initDragEvent("drop", true, true, aWindow, 0, 0, 0, 0, 0, false, false, false, false, 0, null, dataTransfer);
+      gWindowUtils.dispatchDOMEventViaPresShell(destElement, event, true);
+    }
+
+    synthesizeMouseAtCenter(destElement, { type: "mouseup" }, aWindow);
+
+    return dataTransfer.dropEffect;
+  } finally {
+    ds.endDragSession(true);
+  }
+};
+
+var PluginUtils =
+{
+  withTestPlugin : function(callback)
+  {
+    if (typeof Components == "undefined")
+    {
+      todo(false, "Not a Mozilla-based browser");
+      return false;
+    }
+
+    var ph = Components.classes["@mozilla.org/plugin/host;1"]
+                       .getService(Components.interfaces.nsIPluginHost);
+    var tags = ph.getPluginTags();
+
+    // Find the test plugin
+    for (var i = 0; i < tags.length; i++)
+    {
+      if (tags[i].name == "Test Plug-in")
+      {
+        callback(tags[i]);
+        return true;
+      }
+    }
+    todo(false, "Need a test plugin on this platform");
+    return false;
+  }
+};
---
+++ testing/marionette/frame-manager.js
@@ -0,0 +1,230 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+
+this.EXPORTED_SYMBOLS = ["FrameManager"];
+
+var FRAME_SCRIPT = "chrome://marionette/content/listener.js";
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+var loader = Cc["@mozilla.org/moz/jssubscript-loader;1"]
+               .getService(Ci.mozIJSSubScriptLoader);
+
+//list of OOP frames that has the frame script loaded
+var remoteFrames = [];
+
+/**
+ * An object representing a frame that Marionette has loaded a
+ * frame script in.
+ */
+function MarionetteRemoteFrame(windowId, frameId) {
+  this.windowId = windowId; //outerWindowId relative to main process
+  this.frameId = frameId; //actual frame relative to windowId's frames list
+  this.targetFrameId = this.frameId; //assigned FrameId, used for messaging
+};
+
+/**
+ * The FrameManager will maintain the list of Out Of Process (OOP) frames and will handle
+ * frame switching between them.
+ * It handles explicit frame switching (switchToFrame), and implicit frame switching, which
+ * occurs when a modal dialog is triggered in B2G.
+ *
+ */
+this.FrameManager = function FrameManager(server) {
+  //messageManager maintains the messageManager for the current process' chrome frame or the global message manager
+  this.currentRemoteFrame = null; //holds a member of remoteFrames (for an OOP frame) or null (for the main process)
+  this.previousRemoteFrame = null; //frame we'll need to restore once interrupt is gone
+  this.handledModal = false; //set to true when we have been interrupted by a modal
+  this.server = server; // a reference to the marionette server
+};
+
+FrameManager.prototype = {
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIMessageListener,
+                                         Ci.nsISupportsWeakReference]),
+
+  /**
+   * Receives all messages from content messageManager
+   */
+  receiveMessage: function FM_receiveMessage(message) {
+    switch (message.name) {
+      case "MarionetteFrame:getInterruptedState":
+        // This will return true if the calling frame was interrupted by a modal dialog
+        if (this.previousRemoteFrame) {
+          let interruptedFrame = Services.wm.getOuterWindowWithId(this.previousRemoteFrame.windowId);//get the frame window of the interrupted frame
+          if (this.previousRemoteFrame.frameId != null) {
+            interruptedFrame = interruptedFrame.document.getElementsByTagName("iframe")[this.previousRemoteFrame.frameId]; //find the OOP frame
+          }
+          //check if the interrupted frame is the same as the calling frame
+          if (interruptedFrame.src == message.target.src) {
+            return {value: this.handledModal};
+          }
+        }
+        else if (this.currentRemoteFrame == null) {
+          // we get here if previousRemoteFrame and currentRemoteFrame are null, ie: if we're in a non-OOP process, or we haven't switched into an OOP frame, in which case, handledModal can't be set to true.
+          return {value: this.handledModal};
+        }
+        return {value: false};
+      case "MarionetteFrame:handleModal":
+        /*
+         * handleModal is called when we need to switch frames to the main process due to a modal dialog interrupt.
+         */
+        // If previousRemoteFrame was set, that means we switched into a remote frame.
+        // If this is the case, then we want to switch back into the system frame.
+        // If it isn't the case, then we're in a non-OOP environment, so we don't need to handle remote frames
+        let isLocal = true;
+        if (this.currentRemoteFrame != null) {
+          isLocal = false;
+          this.removeMessageManagerListeners(this.currentRemoteFrame.messageManager.get());
+          //store the previous frame so we can switch back to it when the modal is dismissed
+          this.previousRemoteFrame = this.currentRemoteFrame;
+          //by setting currentRemoteFrame to null, it signifies we're in the main process
+          this.currentRemoteFrame = null;
+          this.server.messageManager = Cc["@mozilla.org/globalmessagemanager;1"]
+                                       .getService(Ci.nsIMessageBroadcaster);
+        }
+        this.handledModal = true;
+        this.server.sendOk(this.server.command_id);
+        return {value: isLocal};
+      case "MarionetteFrame:getCurrentFrameId":
+        if (this.currentRemoteFrame != null) {
+          return this.currentRemoteFrame.frameId;
+        }
+    }
+  },
+
+  getOopFrame: function FM_getOopFrame(winId, frameId) {
+    // get original frame window
+    let outerWin = Services.wm.getOuterWindowWithId(winId);
+    // find the OOP frame
+    let f = outerWin.document.getElementsByTagName("iframe")[frameId];
+    return f;
+  },
+
+  getFrameMM: function FM_getFrameMM(winId, frameId) {
+    let oopFrame = this.getOopFrame(winId, frameId);
+    let mm = oopFrame.QueryInterface(Ci.nsIFrameLoaderOwner)
+        .frameLoader.messageManager;
+    return mm;
+  },
+
+  /**
+   * Switch to OOP frame.  We're handling this here
+   * so we can maintain a list of remote frames.
+   */
+  switchToFrame: function FM_switchToFrame(winId, frameId) {
+    let oopFrame = this.getOopFrame(winId, frameId);
+    let mm = this.getFrameMM(winId, frameId);
+
+    // See if this frame already has our frame script loaded in it;
+    // if so, just wake it up.
+    for (let i = 0; i < remoteFrames.length; i++) {
+      let frame = remoteFrames[i];
+      let frameMessageManager = frame.messageManager.get();
+      try {
+        frameMessageManager.sendAsyncMessage("aliveCheck", {});
+      } catch (e) {
+        if (e.result ==  Components.results.NS_ERROR_NOT_INITIALIZED) {
+          remoteFrames.splice(i--, 1);
+          continue;
+        }
+      }
+      if (frameMessageManager == mm) {
+        this.currentRemoteFrame = frame;
+        this.addMessageManagerListeners(mm);
+
+        mm.sendAsyncMessage("Marionette:restart");
+        return oopFrame.id;
+      }
+    }
+
+    // If we get here, then we need to load the frame script in this frame,
+    // and set the frame's ChromeMessageSender as the active message manager
+    // the server will listen to.
+    this.addMessageManagerListeners(mm);
+    let aFrame = new MarionetteRemoteFrame(winId, frameId);
+    aFrame.messageManager = Cu.getWeakReference(mm);
+    remoteFrames.push(aFrame);
+    this.currentRemoteFrame = aFrame;
+
+    mm.loadFrameScript(FRAME_SCRIPT, true, true);
+
+    return oopFrame.id;
+  },
+
+  /*
+   * This function handles switching back to the frame that was interrupted by the modal dialog.
+   * This function gets called by the interrupted frame once the dialog is dismissed and the frame resumes its process
+   */
+  switchToModalOrigin: function FM_switchToModalOrigin() {
+    //only handle this if we indeed switched out of the modal's originating frame
+    if (this.previousRemoteFrame != null) {
+      this.currentRemoteFrame = this.previousRemoteFrame;
+      this.addMessageManagerListeners(this.currentRemoteFrame.messageManager.get());
+    }
+    this.handledModal = false;
+  },
+
+  /**
+   * Adds message listeners to the server, 
+   * listening for messages from content frame scripts.
+   * It also adds a MarionetteFrame:getInterruptedState
+   * message listener to the FrameManager,
+   * so the frame manager's state can be checked by the frame.
+   *
+   * @param {nsIMessageListenerManager} mm
+   *     The message manager object, typically
+   *     ChromeMessageBroadcaster or ChromeMessageSender.
+   */
+  addMessageManagerListeners: function FM_addMessageManagerListeners(mm) {
+    mm.addWeakMessageListener("Marionette:ok", this.server);
+    mm.addWeakMessageListener("Marionette:done", this.server);
+    mm.addWeakMessageListener("Marionette:error", this.server);
+    mm.addWeakMessageListener("Marionette:emitTouchEvent", this.server);
+    mm.addWeakMessageListener("Marionette:log", this.server);
+    mm.addWeakMessageListener("Marionette:runEmulatorCmd", this.server.emulator);
+    mm.addWeakMessageListener("Marionette:runEmulatorShell", this.server.emulator);
+    mm.addWeakMessageListener("Marionette:shareData", this.server);
+    mm.addWeakMessageListener("Marionette:switchToModalOrigin", this.server);
+    mm.addWeakMessageListener("Marionette:switchedToFrame", this.server);
+    mm.addWeakMessageListener("Marionette:getVisibleCookies", this.server);
+    mm.addWeakMessageListener("Marionette:register", this.server);
+    mm.addWeakMessageListener("Marionette:listenersAttached", this.server);
+    mm.addWeakMessageListener("Marionette:getFiles", this.server);
+    mm.addWeakMessageListener("MarionetteFrame:handleModal", this);
+    mm.addWeakMessageListener("MarionetteFrame:getCurrentFrameId", this);
+    mm.addWeakMessageListener("MarionetteFrame:getInterruptedState", this);
+  },
+
+  /**
+   * Removes listeners for messages from content frame scripts.
+   * We do not remove the MarionetteFrame:getInterruptedState
+   * or the Marionette:switchToModalOrigin message listener,
+   * because we want to allow all known frames to contact the frame manager
+   * so that it can check if it was interrupted, and if so,
+   * it will call switchToModalOrigin when its process gets resumed.
+   *
+   * @param {nsIMessageListenerManager} mm
+   *     The message manager object, typically
+   *     ChromeMessageBroadcaster or ChromeMessageSender.
+   */
+  removeMessageManagerListeners: function FM_removeMessageManagerListeners(mm) {
+    mm.removeWeakMessageListener("Marionette:ok", this.server);
+    mm.removeWeakMessageListener("Marionette:done", this.server);
+    mm.removeWeakMessageListener("Marionette:error", this.server);
+    mm.removeWeakMessageListener("Marionette:log", this.server);
+    mm.removeWeakMessageListener("Marionette:shareData", this.server);
+    mm.removeWeakMessageListener("Marionette:runEmulatorCmd", this.server.emulator);
+    mm.removeWeakMessageListener("Marionette:runEmulatorShell", this.server.emulator);
+    mm.removeWeakMessageListener("Marionette:switchedToFrame", this.server);
+    mm.removeWeakMessageListener("Marionette:getVisibleCookies", this.server);
+    mm.removeWeakMessageListener("Marionette:listenersAttached", this.server);
+    mm.removeWeakMessageListener("Marionette:register", this.server);
+    mm.removeWeakMessageListener("Marionette:getFiles", this.server);
+    mm.removeWeakMessageListener("MarionetteFrame:handleModal", this);
+    mm.removeWeakMessageListener("MarionetteFrame:getCurrentFrameId", this);
+  }
+};
