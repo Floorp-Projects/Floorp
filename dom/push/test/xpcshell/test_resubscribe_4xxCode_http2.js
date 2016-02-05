ef1ebadbae6ac70c31431ccff3a1c09e911bdabe
Kit Cambridge <kcambridge@mozilla.com>
2016-02-04 20:17:07 -0800

    Bug 1246073 - Fix unique constraint errors in the H2 backend when resubscribing. r=dragana
    MozReview-Commit-ID: DciQIx9T99U

--- dom/push/test/xpcshell/test_resubscribe_4xxCode_http2.js
+++ dom/push/test/xpcshell/test_resubscribe_4xxCode_http2.js
@@ -14,15 +14,18 @@ XPCOMUtils.defineLazyGetter(this, "serverPort", function() {
   return httpServer.identity.primaryPort;
 });
 
+var handlerDone;
+var handlerPromise = new Promise(r => handlerDone = after(3, r));
+
 function listen4xxCodeHandler(metadata, response) {
   ok(true, "Listener point error")
-  do_test_finished();
+  handlerDone();
   response.setStatusLine(metadata.httpVersion, 410, "GONE");
 }
 
 function resubscribeHandler(metadata, response) {
   ok(true, "Ask for new subscription");
-  do_test_finished();
+  handlerDone();
   response.setHeader("Location",
                   'http://localhost:' + serverPort + '/newSubscription')
   response.setHeader("Link",
@@ -33,7 +36,7 @@ function resubscribeHandler(metadata, response) {
 
 function listenSuccessHandler(metadata, response) {
   do_check_true(true, "New listener point");
-  httpServer.stop(do_test_finished);
+  httpServer.stop(handlerDone);
   response.setStatusLine(metadata.httpVersion, 204, "Try again");
 }
 
@@ -61,10 +64,6 @@ add_task(function* test1() {
     return db.drop().then(_ => db.close());
   });
 
-  do_test_pending();
-  do_test_pending();
-  do_test_pending();
-
   var serverURL = "http://localhost:" + httpServer.identity.primaryPort;
 
   let records = [{
@@ -84,5 +83,18 @@ add_task(function* test1() {
     serverURI: serverURL + "/subscribe",
     db
   });
+
+  yield handlerPromise;
+
+  let record = yield db.getByIdentifiers({
+    scope: 'https://example.com/page',
+    originAttributes: '',
+  });
+  equal(record.keyID, serverURL + '/newSubscription',
+    'Should update subscription URL');
+  equal(record.pushEndpoint, serverURL + '/newPushEndpoint',
+    'Should update push endpoint');
+  equal(record.pushReceiptEndpoint, serverURL + '/newReceiptPushEndpoint',
+    'Should update push receipt endpoint');
 
 });
