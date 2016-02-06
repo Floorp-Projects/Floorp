0e74ba573b8f0b3a069fc461261222632634205c
Wes Kocher <wkocher@mozilla.com>
2016-02-05 16:29:00 -0800

    Backed out 2 changesets (bug 1232222) for xpcshell bustage
    Backed out changeset 06bc151231a5 (bug 1232222)
    Backed out changeset 38ba7c48145e (bug 1232222)
    
    --HG--
    extra : commitid : 4HsqRVO9R13

--- toolkit/mozapps/extensions/test/xpcshell/test_webextension.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_webextension.js
@@ -54,7 +54,6 @@ add_task(function*() {
   do_check_true(addon.isCompatible);
   do_check_false(addon.appDisabled);
   do_check_true(addon.isActive);
-  do_check_false(addon.isSystem);
   do_check_eq(addon.type, "extension");
   do_check_eq(addon.signedState, mozinfo.addon_signing ? AddonManager.SIGNEDSTATE_MISSING : AddonManager.SIGNEDSTATE_NOT_REQUIRED);
 
@@ -82,7 +81,6 @@ add_task(function*() {
   do_check_true(addon.isCompatible);
   do_check_false(addon.appDisabled);
   do_check_true(addon.isActive);
-  do_check_false(addon.isSystem);
   do_check_eq(addon.type, "extension");
   do_check_eq(addon.signedState, mozinfo.addon_signing ? AddonManager.SIGNEDSTATE_MISSING : AddonManager.SIGNEDSTATE_NOT_REQUIRED);
 
@@ -136,7 +134,6 @@ add_task(function*() {
   do_check_true(addon.isCompatible);
   do_check_false(addon.appDisabled);
   do_check_true(addon.isActive);
-  do_check_false(addon.isSystem);
   do_check_eq(addon.type, "extension");
   do_check_eq(addon.signedState, mozinfo.addon_signing ? AddonManager.SIGNEDSTATE_MISSING : AddonManager.SIGNEDSTATE_NOT_REQUIRED);
 
@@ -255,7 +252,6 @@ add_task(function*() {
   do_check_neq(first_addon, null);
   do_check_false(first_addon.appDisabled);
   do_check_true(first_addon.isActive);
-  do_check_false(first_addon.isSystem);
 
   let manifestjson_id= "last-webextension2@tests.mozilla.org";
   let last_addon = yield promiseAddonByID(manifestjson_id);
