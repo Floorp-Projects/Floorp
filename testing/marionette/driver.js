4bed55973f293072c4f15256bc0fee6057f24c5c
Andreas Tolfsen <ato@mozilla.com>
2016-02-06 18:20:07 +0000

    Bug 1246411 - Constructors must use new keyword; r=automatedtester a=test-only
    MozReview-Commit-ID: 4fl0Cl3mf5j

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -1703,7 +1703,7 @@ GeckoDriver.prototype.switchToFrame = function*(cmd, resp) {
         let frames = curWindow.document.getElementsByTagName("iframe");
         let numFrames = frames.length;
         for (let i = 0; i < numFrames; i++) {
-          if (XPCNativeWrapper(frames[i]) == XPCNativeWrapper(wantedFrame)) {
+          if (new XPCNativeWrapper(frames[i]) == new XPCNativeWrapper(wantedFrame)) {
             curWindow = frames[i].contentWindow;
             this.curFrame = curWindow;
             if (focus) {
