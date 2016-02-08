1496ca402620b25c5e9f74436dbd60d29c43bda0
Shu-yu Guo <shu@rfrn.org>
2016-02-08 14:48:46 +0100

    Bug 1243793 - Fix handling of labels when emitting hoisted function definitions. r=jorendorff, a=lizzard
    --HG--
    extra : rebase_source : 75e59bca96e1aacec65b11ceed9144d91e1ac727

--- js/src/tests/ecma_6/LexicalEnvironment/block-scoped-functions-deprecated-redecl.js
+++ js/src/tests/ecma_6/LexicalEnvironment/block-scoped-functions-deprecated-redecl.js
@@ -9,6 +9,18 @@
 // Annex B still works.
 assertEq(f(), 4);
 
+// The same thing with labels.
+{
+  assertEq(f(), 4);
+  function f() { return 3; }
+  assertEq(f(), 4);
+  l: function f() { return 4; }
+  assertEq(f(), 4);
+}
+
+// Annex B still works.
+assertEq(f(), 4);
+
 function test() {
   {
     assertEq(f(), 2);
