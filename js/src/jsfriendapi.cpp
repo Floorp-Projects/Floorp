ad763bf2b0c5b01bad6a64d2f80aea7a3d1f5d53
Jan de Mooij <jdemooij@mozilla.com>
2016-02-08 15:13:37 +0100

    Bug 1122581 - Fix FormatFrame to not assert on Ion frames in some cases. r=shu

--- js/src/jsfriendapi.cpp
+++ js/src/jsfriendapi.cpp
@@ -759,13 +759,13 @@ FormatFrame(JSContext* cx, const ScriptFrameIter& iter, char* buf, int num,
                         break;
                     }
                 }
-            } else if (script->argsObjAliasesFormals() && iter.hasArgsObj()) {
-                arg = iter.argsObj().arg(i);
-            } else {
-                if (iter.hasUsableAbstractFramePtr())
-                    arg = iter.unaliasedActual(i, DONT_CHECK_ALIASING);
+            } else if (iter.hasUsableAbstractFramePtr()) {
+                if (script->argsObjAliasesFormals() && iter.hasArgsObj())
+                    arg = iter.argsObj().arg(i);
                 else
-                    arg = MagicValue(JS_OPTIMIZED_OUT);
+                    arg = iter.unaliasedActual(i, DONT_CHECK_ALIASING);
+            } else {
+                arg = MagicValue(JS_OPTIMIZED_OUT);
             }
 
             JSAutoByteString valueBytes;
