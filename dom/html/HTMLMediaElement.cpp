cebc399c284b4bc3567d8335748cc29a3a10095d
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-02-08 15:27:14 +1100

    Bug 1246521: P1. Add MediaDecoderOwner::HasError method. r=gerald, a=sylvestre

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -3495,6 +3495,11 @@ void HTMLMediaElement::DecodeError()
   }
 }
 
+bool HTMLMediaElement::HasError() const
+{
+  return GetError();
+}
+
 void HTMLMediaElement::LoadAborted()
 {
   Error(nsIDOMMediaError::MEDIA_ERR_ABORTED);
