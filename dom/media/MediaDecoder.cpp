53c49526d6397871c81a4a2cac6869a08d5f5a87
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-02-08 15:28:15 +1100

    Bug 1246521: P2. Add MediaDecoder::OwnerHasError method. r=gerald

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -1043,6 +1043,13 @@ MediaDecoder::IsEndedOrShutdown() const
 }
 
 bool
+MediaDecoder::OwnerHasError() const
+{
+  MOZ_ASSERT(NS_IsMainThread());
+  return mShuttingDown || mOwner->HasError();
+}
+
+bool
 MediaDecoder::IsEnded() const
 {
   MOZ_ASSERT(NS_IsMainThread());
