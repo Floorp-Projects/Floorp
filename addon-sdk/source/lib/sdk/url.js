0567005cc3e0bea4648e82fdbda2299a0a308deb
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2016-02-08 11:12:37 -0800

    Bug 1246725 - Remove SEC_NORMAL from addon-sdk (r=sicking)

--- addon-sdk/source/lib/sdk/url.js
+++ addon-sdk/source/lib/sdk/url.js
@@ -23,7 +23,7 @@ var resProt = ios.getProtocolHandler("resource")
 var URLParser = Cc["@mozilla.org/network/url-parser;1?auth=no"]
                 .getService(Ci.nsIURLParser);
 
-const { Services } = Cu.import("resource://gre/modules/Services.jsm");
+const { NetUtil } = Cu.import("resource://gre/modules/NetUtil.jsm");
 
 function newURI(uriStr, base) {
   try {
@@ -66,12 +66,11 @@ var toFilename = exports.toFilename = function toFilename(url) {
   if (uri.scheme == "resource")
     uri = newURI(resolveResourceURI(uri));
   if (uri.scheme == "chrome") {
-    var channel = ios.newChannelFromURI2(uri,
-                                         null,      // aLoadingNode
-                                         Services.scriptSecurityManager.getSystemPrincipal(),
-                                         null,      // aTriggeringPrincipal
-                                         Ci.nsILoadInfo.SEC_NORMAL,
-                                         Ci.nsIContentPolicy.TYPE_OTHER);
+    var channel = NetUtil.newChannel({
+      uri: uri,
+      loadUsingSystemPrincipal: true
+    });
+
     try {
       channel = channel.QueryInterface(Ci.nsIFileChannel);
       return channel.file.path;
