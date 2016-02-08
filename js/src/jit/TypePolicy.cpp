0b5d2f4d168e6d656cd340e8931309ee63a9a6c9
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2016-02-08 17:38:27 +0000

    Bug 1244828 - Ensure enough ballast space in CallPolicy::adjustInputs. r=bbouvier

--- js/src/jit/TypePolicy.cpp
+++ js/src/jit/TypePolicy.cpp
@@ -913,8 +913,11 @@ CallPolicy::adjustInputs(TempAllocator& alloc, MInstruction* ins)
             return false;
     }
 
-    for (uint32_t i = 0; i < call->numStackArgs(); i++)
+    for (uint32_t i = 0; i < call->numStackArgs(); i++) {
+        if (!alloc.ensureBallast())
+            return false;
         EnsureOperandNotFloat32(alloc, call, MCall::IndexOfStackArg(i));
+    }
 
     return true;
 }
