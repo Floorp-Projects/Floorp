de72ce5dbe9979f6efe148b3d1523adf7dda0cb3
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2016-02-08 20:21:20 +0900

    Bug 1246574 - Store sandbox level to nsPluginTag for e10s. r=jimm
    When turned on e10s, plugin process creates from chrome process.  So content
    process doesn't know current sandbox level.  To rewrite wmode attribute on
    contnet process by sandbox level >= 2, we should store sandbox level to
    nsPluginTag.
    
    MozReview-Commit-ID: DCQ5g7uCbJF
    
    --HG--
    extra : rebase_source : 7064f469e873ea1301e28faeab0bbb27613978db

--- dom/plugins/base/nsPluginTags.h
+++ dom/plugins/base/nsPluginTags.h
@@ -130,7 +130,8 @@ public:
               bool aIsFlashPlugin,
               bool aSupportsAsyncInit,
               int64_t aLastModifiedTime,
-              bool aFromExtension);
+              bool aFromExtension,
+              int32_t aSandboxLevel);
 
   void TryUnloadPlugin(bool inShutdown);
 
@@ -170,6 +171,7 @@ public:
   nsCString     mFullPath; // UTF-8
   int64_t       mLastModifiedTime;
   nsCOMPtr<nsITimer> mUnloadTimer;
+  int32_t       mSandboxLevel;
 
   void          InvalidateBlocklistState();
 
@@ -185,6 +187,7 @@ private:
                 const char* const* aMimeDescriptions,
                 const char* const* aExtensions,
                 uint32_t aVariantCount);
+  void InitSandboxLevel();
   nsresult EnsureMembersAreUTF8();
   void FixupVersion();
 
