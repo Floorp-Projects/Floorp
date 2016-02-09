5493538baaea672abbdadaef24b50691780fe774
Jan Odvarko <odvarko@gmail.com>
2016-02-09 23:23:29 +0100

    Bug 1201475 - Implement DOM Panel; r=linclark, bgrins, helenvholmes
    MozReview-Commit-ID: IMYSW7ObOSh

---
+++ devtools/client/dom/content/reducers/index.js
@@ -0,0 +1,14 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const { grips } = require("./grips");
+const { filter } = require("./filter");
+
+exports.reducers = {
+  grips,
+  filter,
+};
---
+++ devtools/client/shared/components/reps/window.js
@@ -0,0 +1,71 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+// Make this available to both AMD and CJS environments
+define(function(require, exports, module) {
+  // ReactJS
+  const React = require("devtools/client/shared/vendor/react");
+
+  // Reps
+  const { createFactories, isGrip } = require("./rep-utils");
+  const { ObjectBox } = createFactories(require("./object-box"));
+  const { cropString } = require("./string");
+
+  // Shortcuts
+  const DOM = React.DOM;
+
+  /**
+   * Renders a grip representing a window.
+   */
+  let Window = React.createClass({
+    propTypes: {
+      object: React.PropTypes.object.isRequired,
+    },
+
+    displayName: "Window",
+
+    getLocation: function(grip) {
+      return cropString(grip.preview.url);
+    },
+
+    getTitle: function(grip, context) {
+      return grip.class;
+    },
+
+    getTooltip: function(grip) {
+      return grip.preview.url;
+    },
+
+    render: function() {
+      let grip = this.props.object;
+
+      return (
+        ObjectBox({className: "Window"},
+          DOM.span({className: "objectPropValue"},
+            this.getLocation(grip)
+          )
+        )
+      );
+    },
+  });
+
+  // Registration
+
+  function supportsObject(object, type) {
+    if (!isGrip(object)) {
+      return false;
+    }
+
+    return (object.preview && type == "Window");
+  }
+
+  // Exports from this module
+  exports.Window = {
+    rep: Window,
+    supportsObject: supportsObject
+  };
+});
