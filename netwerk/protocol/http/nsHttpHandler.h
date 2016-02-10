807175f2a6ca4c7471734fd9184768f641e3d822
Wes Kocher <wkocher@mozilla.com>
2016-02-09 16:58:54 -0800

    Backed out 2 changesets (bug 1148544) for failures in test_user_agent_overrides.html
    Backed out changeset 5073d841f77d (bug 1148544)
    Backed out changeset 64a2c41c6f20 (bug 1148544)
    
    --HG--
    extra : commitid : 3oP1hrXASC1

--- netwerk/protocol/http/nsHttpHandler.h
+++ netwerk/protocol/http/nsHttpHandler.h
@@ -275,12 +275,6 @@ public:
         NotifyObservers(chan, NS_HTTP_ON_MODIFY_REQUEST_TOPIC);
     }
 
-    // Called by the channel and cached in the loadGroup
-    void OnUserAgentRequest(nsIHttpChannel *chan)
-    {
-      NotifyObservers(chan, NS_HTTP_ON_USERAGENT_REQUEST_TOPIC);
-    }
-
     // Called by the channel once headers are available
     void OnExamineResponse(nsIHttpChannel *chan)
     {
