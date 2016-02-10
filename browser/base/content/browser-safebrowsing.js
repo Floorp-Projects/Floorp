fe7b8eaff0a74a34389b078a7fbf6412956246d5
Mike Conley <mconley@mozilla.com>
2016-02-10 15:49:50 -0500

    Bug 1246115 - Make gSafeBrowsing set the phishing menu item correctly. r=Gijs, a=lizzard
    Unfortunately, when onLocationChange is fired for an attack site for
    the about:blocked error page that we display, content.document has not
    been updated with the loaded error document, so
    content.document.documentURI will appear to be the previous page that
    had been loaded. In this patch, we update the parent's cache of
    documentURI in onStateChange as well, since this seems to be fired
    after the error page has been loaded.
    
    MozReview-Commit-ID: 1yLAw0JTEC6

--- browser/base/content/browser-safebrowsing.js
+++ browser/base/content/browser-safebrowsing.js
@@ -8,9 +8,14 @@
 var gSafeBrowsing = {
 
   setReportPhishingMenu: function() {
-    // A phishing page will have a specific about:blocked content documentURI
-    var uri = gBrowser.currentURI;
-    var isPhishingPage = uri && uri.spec.startsWith("about:blocked?e=phishingBlocked");
+    // In order to detect whether or not we're at the phishing warning
+    // page, we have to check the documentURI instead of the currentURI.
+    // This is because when the DocShell loads an error page, the
+    // currentURI stays at the original target, while the documentURI
+    // will point to the internal error page we loaded instead.
+    var docURI = gBrowser.selectedBrowser.documentURI;
+    var isPhishingPage =
+      docURI && docURI.spec.startsWith("about:blocked?e=phishingBlocked");
 
     // Show/hide the appropriate menu item.
     document.getElementById("menu_HelpPopup_reportPhishingtoolmenu")
@@ -26,6 +31,9 @@ var gSafeBrowsing = {
     if (!broadcaster)
       return;
 
+    // Now look at the currentURI to learn which page we were trying
+    // to browse to.
+    let uri = gBrowser.currentURI;
     if (uri && (uri.schemeIs("http") || uri.schemeIs("https")))
       broadcaster.removeAttribute("disabled");
     else
