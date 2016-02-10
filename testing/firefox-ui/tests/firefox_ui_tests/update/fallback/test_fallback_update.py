f3a212f11b9ade4a664b7a445502a10301355942
Henrik Skupin <mail@hskupin.info>
2016-02-10 01:48:00 +0100

    Bug 1207038 - Backout workaround of bug 1156475 for Marionette shutdown hang. r=maja_zf, a=test-only MozReview-Commit-ID: 8ZZGDA09ojo

--- testing/firefox-ui/tests/firefox_ui_tests/update/fallback/test_fallback_update.py
+++ testing/firefox-ui/tests/firefox_ui_tests/update/fallback/test_fallback_update.py
@@ -16,18 +16,7 @@ class TestFallbackUpdate(UpdateTestCase):
         finally:
             UpdateTestCase.tearDown(self)
 
-    def _test_update(self):
-        self.download_and_apply_available_update(force_fallback=True)
-
-        self.download_and_apply_forced_update()
-
-        self.check_update_applied()
-
     def test_update(self):
-        try:
-            self._test_update()
-        except:
-            # Switch context to the main browser window before embarking
-            # down the failure code path to work around bug 1141519.
-            self.browser.switch_to()
-            raise
+        self.download_and_apply_available_update(force_fallback=True)
+        self.download_and_apply_forced_update()
+        self.check_update_applied()
