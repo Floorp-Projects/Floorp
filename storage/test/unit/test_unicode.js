9e8ca5250ba479a35a3d31e8aa7eee3b0530daae
Jeffrey Tran <tranj23@gmail.com>
2016-02-10 15:30:34 +0100

    Bug 1230685 - Replace function declarations with add_task statements in test_storage_value_array.js and test_unicode.js. r=mak

--- storage/test/unit/test_unicode.js
+++ storage/test/unit/test_unicode.js
@@ -7,8 +7,7 @@
 const LATIN1_AE = "\xc6"; // "Æ"
 const LATIN1_ae = "\xe6";  // "æ"
 
-function setup()
-{
+add_task(function* setup() {
   getOpenedDatabase().createTable("test", "id INTEGER PRIMARY KEY, name TEXT");
 
   var stmt = createStatement("INSERT INTO test (name, id) VALUES (?1, ?2)");
@@ -25,20 +24,20 @@ function setup()
   stmt.bindByIndex(1, 4);
   stmt.execute();
   stmt.finalize();
-}
 
-function test_upper_ascii()
-{
+  do_register_cleanup(cleanup);
+});
+
+add_task(function* test_upper_ascii() {
   var stmt = createStatement("SELECT name, id FROM test WHERE name = upper('a')");
   do_check_true(stmt.executeStep());
   do_check_eq("A", stmt.getString(0));
   do_check_eq(2, stmt.getInt32(1));
   stmt.reset();
   stmt.finalize();
-}
+});
 
-function test_upper_non_ascii()
-{
+add_task(function* test_upper_non_ascii() {
   var stmt = createStatement("SELECT name, id FROM test WHERE name = upper(?1)");
   stmt.bindByIndex(0, LATIN1_ae);
   do_check_true(stmt.executeStep());
@@ -46,20 +45,18 @@ function test_upper_non_ascii()
   do_check_eq(1, stmt.getInt32(1));
   stmt.reset();
   stmt.finalize();
-}
+});
 
-function test_lower_ascii()
-{
+add_task(function* test_lower_ascii() {
   var stmt = createStatement("SELECT name, id FROM test WHERE name = lower('B')");
   do_check_true(stmt.executeStep());
   do_check_eq("b", stmt.getString(0));
   do_check_eq(3, stmt.getInt32(1));
   stmt.reset();
   stmt.finalize();
-}
+});
 
-function test_lower_non_ascii()
-{
+add_task(function* test_lower_non_ascii() {
   var stmt = createStatement("SELECT name, id FROM test WHERE name = lower(?1)");
   stmt.bindByIndex(0, LATIN1_AE);
   do_check_true(stmt.executeStep());
@@ -67,38 +64,20 @@ function test_lower_non_ascii()
   do_check_eq(4, stmt.getInt32(1));
   stmt.reset();
   stmt.finalize();
-}
+});
 
-function test_like_search_different()
-{
+add_task(function* test_like_search_different() {
   var stmt = createStatement("SELECT COUNT(*) FROM test WHERE name LIKE ?1");
   stmt.bindByIndex(0, LATIN1_AE);
   do_check_true(stmt.executeStep());
   do_check_eq(2, stmt.getInt32(0));
   stmt.finalize();
-}
+});
 
-function test_like_search_same()
-{
+add_task(function* test_like_search_same() {
   var stmt = createStatement("SELECT COUNT(*) FROM test WHERE name LIKE ?1");
   stmt.bindByIndex(0, LATIN1_ae);
   do_check_true(stmt.executeStep());
   do_check_eq(2, stmt.getInt32(0));
   stmt.finalize();
-}
-
-var tests = [test_upper_ascii, test_upper_non_ascii, test_lower_ascii,
-             test_lower_non_ascii, test_like_search_different,
-             test_like_search_same];
-
-function run_test()
-{
-  setup();
-
-  for (var i = 0; i < tests.length; i++) {
-    tests[i]();
-  }
-
-  cleanup();
-}
-
+});
