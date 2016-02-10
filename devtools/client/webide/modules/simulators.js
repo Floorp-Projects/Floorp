0eaf95a6a9f396cdbfe394d1623c423fd76230f3
J. Ryan Stinnett <jryans@gmail.com>
2016-02-10 13:49:15 -0600

    Bug 1244226 - Fix promise error in simulator test. r=ochameau
    MozReview-Commit-ID: B6UpRTlJYDM

--- devtools/client/webide/modules/simulators.js
+++ devtools/client/webide/modules/simulators.js
@@ -44,13 +44,13 @@ var Simulators = {
 
           // If the simulator had a reference to an addon, fix it.
           if (options.addonID) {
-            let job = promise.defer();
+            let deferred = promise.defer();
             AddonManager.getAddonByID(options.addonID, addon => {
               simulator.addon = addon;
               delete simulator.options.addonID;
-              job.resolve();
+              deferred.resolve();
             });
-            jobs.push(job);
+            jobs.push(deferred.promise);
           }
         });
       }
@@ -232,7 +232,7 @@ var Simulators = {
   },
 
   emitUpdated() {
-    this.emit("updated");
+    this.emit("updated", { length: this._simulators.length });
     this._simulators.sort(LocaleCompare);
     this._save();
   },
