55351409afc5d767930887ac1e022e7456f7dc88
Andrew McCreight <continuation@gmail.com>
2016-02-10 10:29:20 -0800

    Bug 1245673 - Remove useless NS_INT32_TO_PTR from DumpSerialNumbers. r=froydnj

--- xpcom/base/nsTraceRefcnt.cpp
+++ xpcom/base/nsTraceRefcnt.cpp
@@ -470,14 +470,14 @@ DumpSerialNumbers(PLHashEntry* aHashEntry, int aIndex, void* aClosure)
   fprintf(outputFile, "%" PRIdPTR
           " @%p (%d references; %d from COMPtrs)\n",
           record->serialNumber,
-          NS_INT32_TO_PTR(aHashEntry->key),
+          aHashEntry->key,
           record->refCount,
           record->COMPtrCount);
 #else
   fprintf(outputFile, "%" PRIdPTR
           " @%p (%d references)\n",
           record->serialNumber,
-          NS_INT32_TO_PTR(aHashEntry->key),
+          aHashEntry->key,
           record->refCount);
 #endif
 #ifdef MOZ_STACKWALKING
