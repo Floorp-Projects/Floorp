8570d9d24436ba3b2ce0dc95765078d83e18b480
Kyle Huey <khuey@kylehuey.com>
2016-02-10 10:53:26 -0800

    Bug 1246828: Push QuotaManager shutdown back to profile-before-change2

--- testing/xpcshell/head.js
+++ testing/xpcshell/head.js
@@ -614,6 +614,7 @@ function _execute_test() {
     obs.notifyObservers(null, "profile-change-net-teardown", null);
     obs.notifyObservers(null, "profile-change-teardown", null);
     obs.notifyObservers(null, "profile-before-change", null);
+    obs.notifyObservers(null, "profile-before-change2", null);
 
     _profileInitialized = false;
   }
