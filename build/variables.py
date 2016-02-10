1d537257e2661c546b0a4d2ef9d75eee5e92898c
Mike Hommey <mh+mozilla@glandium.org>
2016-02-10 13:38:31 +0900

    Bug 1247162 - Generate a header defining MOZ_SOURCE_*. r=mshal
    The behavior is not entirely idempotent (most notably for
    buildconfig.html), but this can be improved later if necessary.
    It is idempotent where it matters.
    
    This allows to get rid of config/makefiles/rcs.mk and its uses.

--- build/variables.py
+++ build/variables.py
@@ -5,6 +5,7 @@
 from __future__ import print_function, unicode_literals
 
 import os
+import subprocess
 import sys
 from datetime import datetime
 
@@ -17,6 +18,53 @@ def buildid_header(output):
     if not buildid:
         buildid = datetime.now().strftime('%Y%m%d%H%M%S')
     output.write("#define MOZ_BUILDID %s\n" % buildid)
+
+
+def get_program_output(*command):
+    try:
+        with open(os.devnull) as stderr:
+            return subprocess.check_output(command, stderr=stderr)
+    except:
+        return ''
+
+
+def get_hg_info(workdir):
+    repo = get_program_output('hg', '-R', workdir, 'path', 'default')
+    if repo:
+        repo = repo.strip()
+        if repo.startswith('ssh://'):
+            repo = 'https://' + repo[6:]
+        repo = repo.rstrip('/')
+
+    changeset = get_program_output(
+        'hg', '-R', workdir, 'parent', '--template={node}')
+
+    return repo, changeset
+
+
+def source_repo_header(output):
+    # We allow the source repo and changeset to be specified via the
+    # environment (see configure)
+    import buildconfig
+    repo = buildconfig.substs.get('MOZ_SOURCE_REPO')
+    changeset = buildconfig.substs.get('MOZ_SOURCE_CHANGESET')
+    source = ''
+
+    if bool(repo) != bool(changeset):
+        raise Exception('MOZ_SOURCE_REPO and MOZ_SOURCE_CHANGESET both must '
+                        'be set (or not set).')
+
+    if not repo:
+        if os.path.exists(os.path.join(buildconfig.topsrcdir, '.hg')):
+            repo, changeset = get_hg_info(buildconfig.topsrcdir)
+
+    if changeset:
+        output.write('#define MOZ_SOURCE_STAMP %s\n' % changeset)
+
+    if repo and buildconfig.substs.get('MOZ_INCLUDE_SOURCE_INFO'):
+        source = '%s/rev/%s' % (repo, changeset)
+        output.write('#define MOZ_SOURCE_REPO %s\n' % repo)
+        output.write('#define MOZ_SOURCE_URL %s\n' % source)
 
 
 def main(args):
