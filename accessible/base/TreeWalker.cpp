15d2ff2065ad94bf7f780cb09be5051b997ae651
Alexander Surkov <surkov.alexander@gmail.com>
2016-02-11 08:24:38 -0500

    Bug 1247364 - use AllChildrenIterator::Seek by a11y tree walker, r=davidb

--- accessible/base/TreeWalker.cpp
+++ accessible/base/TreeWalker.cpp
@@ -80,11 +80,9 @@ TreeWalker::NextChild()
 
     nsIContent* parent = parentNode->AsElement();
     top = PushState(parent);
-    while (nsIContent* childNode = Next(top)) {
-      if (childNode == mAnchorNode) {
-        mAnchorNode = parent;
-        return NextChild();
-      }
+    if (top->mDOMIter.Seek(mAnchorNode)) {
+      mAnchorNode = parent;
+      return NextChild();
     }
 
     // XXX We really should never get here, it means we're trying to find an
