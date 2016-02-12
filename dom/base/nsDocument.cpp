0a82d3db4b909b76c4286f6b5eda876e71354c32
Christoph Kerschbaumer <mozilla@christophkerschbaumer.com>
2016-02-12 14:51:07 -0800

    Bug 1247807 - Fix Mixed Content UI when using upgrade-insecure-requests (r=bz) a=sylvestre

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -2661,6 +2661,9 @@ nsDocument::ApplySettingsFromCSP(bool aSpeculative)
         rv = csp->GetUpgradeInsecureRequests(&mUpgradeInsecureRequests);
         NS_ENSURE_SUCCESS_VOID(rv);
       }
+      if (!mUpgradeInsecurePreloads) {
+        mUpgradeInsecurePreloads = mUpgradeInsecureRequests;
+      }
     }
     return;
   }
