e1a0953a970426bfc437f1a180d23da9201fb34e
Jonathan Kew <jkew@mozilla.com>
2016-02-12 09:19:12 +0000

    Backout cset f4bd1fc36f91 (bug 1239855) from aurora-46 (followup to bug 890156, which is being backed out). a=backout

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -1086,9 +1086,10 @@ nsWindow::Show(bool aState)
 NS_IMETHODIMP
 nsWindow::Resize(double aWidth, double aHeight, bool aRepaint)
 {
-    double scale = BoundsUseDesktopPixels() ? GetDesktopToDeviceScale().scale : 1.0;
-    int32_t width = NSToIntRound(scale * aWidth);
-    int32_t height = NSToIntRound(scale * aHeight);
+    CSSToLayoutDeviceScale scale = BoundsUseDesktopPixels() ? GetDefaultScale()
+                                    : CSSToLayoutDeviceScale(1.0);
+    int32_t width = NSToIntRound(scale.scale * aWidth);
+    int32_t height = NSToIntRound(scale.scale * aHeight);
     ConstrainSize(&width, &height);
 
     // For top-level windows, aWidth and aHeight should possibly be
@@ -1117,13 +1118,14 @@ NS_IMETHODIMP
 nsWindow::Resize(double aX, double aY, double aWidth, double aHeight,
                  bool aRepaint)
 {
-    double scale = BoundsUseDesktopPixels() ? GetDesktopToDeviceScale().scale : 1.0;
-    int32_t width = NSToIntRound(scale * aWidth);
-    int32_t height = NSToIntRound(scale * aHeight);
+    CSSToLayoutDeviceScale scale = BoundsUseDesktopPixels() ? GetDefaultScale()
+                                    : CSSToLayoutDeviceScale(1.0);
+    int32_t width = NSToIntRound(scale.scale * aWidth);
+    int32_t height = NSToIntRound(scale.scale * aHeight);
     ConstrainSize(&width, &height);
 
-    int32_t x = NSToIntRound(scale * aX);
-    int32_t y = NSToIntRound(scale * aY);
+    int32_t x = NSToIntRound(scale.scale * aX);
+    int32_t y = NSToIntRound(scale.scale * aY);
     mBounds.x = x;
     mBounds.y = y;
     mBounds.SizeTo(width, height);
@@ -1182,9 +1184,10 @@ nsWindow::Move(double aX, double aY)
     LOG(("nsWindow::Move [%p] %f %f\n", (void *)this,
          aX, aY));
 
-    double scale = BoundsUseDesktopPixels() ? GetDesktopToDeviceScale().scale : 1.0;
-    int32_t x = NSToIntRound(aX * scale);
-    int32_t y = NSToIntRound(aY * scale);
+    CSSToLayoutDeviceScale scale = BoundsUseDesktopPixels() ? GetDefaultScale()
+                                   : CSSToLayoutDeviceScale(1.0);
+    int32_t x = NSToIntRound(aX * scale.scale);
+    int32_t y = NSToIntRound(aY * scale.scale);
 
     if (mWindowType == eWindowType_toplevel ||
         mWindowType == eWindowType_dialog) {
