f6f6e4ae25f67b4802667127efedf5e55924e9a6
Luke Wagner <luke@mozilla.com>
2016-02-11 23:28:13 -0600

    Bug 1247755 - Baldr: disallow duplicate signature table entries (r=sunfish)
    MozReview-Commit-ID: 2feiq81dBKH

--- js/src/asmjs/WasmTypes.h
+++ js/src/asmjs/WasmTypes.h
@@ -245,6 +245,13 @@ class Sig
     }
 };
 
+struct SigHashPolicy
+{
+    typedef const Sig& Lookup;
+    static HashNumber hash(Lookup sig) { return sig.hash(); }
+    static bool match(const Sig* lhs, Lookup rhs) { return *lhs == rhs; }
+};
+
 // A "declared" signature is a Sig object that is created and owned by the
 // ModuleGenerator. These signature objects are read-only and have the same
 // lifetime as the ModuleGenerator. This type is useful since some uses of Sig
