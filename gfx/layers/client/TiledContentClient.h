ef43abbc2b1f1028180d4e48388414a2797cc092
Kartikaya Gupta <kgupta@mozilla.com>
2016-02-12 08:29:25 -0500

    Bug 1243589 - Use SingleTiledContentClient even for scrollable layers if the layer is smaller than a single tile. r=mattwoodrow, a=lizzard
    MozReview-Commit-ID: 6eYFmoOVL6b

--- gfx/layers/client/TiledContentClient.h
+++ gfx/layers/client/TiledContentClient.h
@@ -627,9 +627,6 @@ public:
   };
   virtual void UpdatedBuffer(TiledBufferType aType) = 0;
 
-  virtual bool SupportsLayerSize(const gfx::IntSize& aSize, ClientLayerManager* aManager) const
-  { return true; }
-
 private:
   const char* mName;
 };
