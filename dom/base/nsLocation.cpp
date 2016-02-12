edf945d077ee9115952ab49e9f877ccce1d910c2
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-02-12 11:21:01 +0000

    Bug 1245264, r=bz a=ritu
    MozReview-Commit-ID: I0sVdritpD3

--- dom/base/nsLocation.cpp
+++ dom/base/nsLocation.cpp
@@ -696,6 +696,16 @@ nsLocation::SetProtocol(const nsAString& aProtocol)
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return rv;
   }
+  nsAutoCString newSpec;
+  rv = uri->GetSpec(newSpec);
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+  // We may want a new URI class for the new URI, so recreate it:
+  rv = NS_NewURI(getter_AddRefs(uri), newSpec);
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
 
   return SetURI(uri);
 }
