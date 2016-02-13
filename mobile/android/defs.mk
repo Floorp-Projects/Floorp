943ac18c2be40d9a84f8729011b4e7d474780d1e
Nick Alexander <nalexander@mozilla.com>
2016-02-13 10:00:58 -0800

    Bug 1248160 - Part 1: Remove MOZ_ANDROID_SHARED_ACCOUNT_TYPE. r=ted.mielczarek
    This is cleaning up after Bug 1220906, which removed Old Sync.
    
    MozReview-Commit-ID: EmP4RTMIZ9

--- mobile/android/defs.mk
+++ mobile/android/defs.mk
@@ -1,7 +1,4 @@
 MOZ_ANDROID_SHARED_ID = $(ANDROID_PACKAGE_NAME).sharedID
-# Android Sync Account types are not per-package: release and beta
-# share an Account type, and aurora and nightly share an Account type.
-MOZ_ANDROID_SHARED_ACCOUNT_TYPE = $(ANDROID_PACKAGE_NAME)_sync
 # Firefox Accounts Account types are per-package.
 MOZ_ANDROID_SHARED_FXACCOUNT_TYPE = $(ANDROID_PACKAGE_NAME)_fxaccount
 
@@ -9,14 +6,10 @@ MOZ_ANDROID_SHARED_FXACCOUNT_TYPE = $(ANDROID_PACKAGE_NAME)_fxaccount
 # keep them consistent.
 ifeq (org.mozilla.firefox,$(ANDROID_PACKAGE_NAME))
 MOZ_ANDROID_SHARED_ID = org.mozilla.firefox.sharedID
-MOZ_ANDROID_SHARED_ACCOUNT_TYPE = org.mozilla.firefox_sync
 else ifeq (org.mozilla.firefox_beta,$(ANDROID_PACKAGE_NAME))
 MOZ_ANDROID_SHARED_ID = org.mozilla.firefox.sharedID
-MOZ_ANDROID_SHARED_ACCOUNT_TYPE = org.mozilla.firefox_sync
 else ifeq (org.mozilla.fennec_aurora,$(ANDROID_PACKAGE_NAME))
 MOZ_ANDROID_SHARED_ID = org.mozilla.fennec.sharedID
-MOZ_ANDROID_SHARED_ACCOUNT_TYPE = org.mozilla.fennec_sync
 else ifeq (org.mozilla.fennec,$(ANDROID_PACKAGE_NAME))
 MOZ_ANDROID_SHARED_ID = org.mozilla.fennec.sharedID
-MOZ_ANDROID_SHARED_ACCOUNT_TYPE = org.mozilla.fennec_sync
 endif
