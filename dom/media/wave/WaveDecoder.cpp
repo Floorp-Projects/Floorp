0591b9bc6dd5630c726f4f6fb62c761c955113dc
Louis Christie <lchristie@mozilla.com>
2016-02-15 12:10:48 +1300

    Bug 1231793: Part 5 - Added Wave Data Demuxer and Decoder. r=jya

--- dom/media/wave/WaveDecoder.cpp
+++ dom/media/wave/WaveDecoder.cpp
@@ -3,15 +3,58 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "WaveDemuxer.h"
+#include "mozilla/Preferences.h"
 #include "MediaDecoderStateMachine.h"
 #include "WaveReader.h"
 #include "WaveDecoder.h"
+#include "MediaFormatReader.h"
+#include "PDMFactory.h"
 
 namespace mozilla {
 
-MediaDecoderStateMachine* WaveDecoder::CreateStateMachine()
+MediaDecoder*
+WaveDecoder::Clone(MediaDecoderOwner* aOwner)
 {
-  return new MediaDecoderStateMachine(this, new WaveReader(this));
+  if (!IsEnabled())
+    return nullptr;
+
+  return new WaveDecoder(aOwner);
+}
+
+MediaDecoderStateMachine*
+WaveDecoder::CreateStateMachine()
+{
+  if (Preferences::GetBool("media.wave.decoder.enabled")) {
+    RefPtr<MediaDecoderReader> reader =
+        new MediaFormatReader(this, new WAVDemuxer(GetResource()));
+    return new MediaDecoderStateMachine(this, reader);
+  } else {
+    return new MediaDecoderStateMachine(this, new WaveReader(this));
+  }
+}
+
+/* static */
+bool
+WaveDecoder::IsEnabled()
+{
+  PDMFactory::Init();
+  RefPtr<PDMFactory> platform = new PDMFactory();
+  return platform->SupportsMimeType(NS_LITERAL_CSTRING("audio/x-wav"));
+}
+
+/* static */
+bool
+WaveDecoder::CanHandleMediaType(const nsACString& aType,
+                               const nsAString& aCodecs)
+{
+  if (aType.EqualsASCII("audio/wave") || aType.EqualsASCII("audio/x-wav") ||
+      aType.EqualsASCII("audio/wav")  || aType.EqualsASCII("audio/x-pn-wav")) {
+    return IsEnabled() && (aCodecs.IsEmpty() || aCodecs.EqualsASCII("1"));
+  }
+
+  return false;
 }
 
 } // namespace mozilla
