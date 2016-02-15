e8a2383bf8c126220dc461b177eb75dd2ce10924
Tooru Fujisawa <arai_a@mac.com>
2016-02-16 00:23:24 +0900

    Backed out changeset f8d78e70a6fc (bug 1245112)

--- js/src/jit/mips32/MacroAssembler-mips32.h
+++ js/src/jit/mips32/MacroAssembler-mips32.h
@@ -1074,6 +1074,9 @@ class MacroAssemblerMIPSCompat : public MacroAssemblerMIPS
 
     void breakpoint();
 
+    void branchDouble(DoubleCondition cond, FloatRegister lhs, FloatRegister rhs,
+                      Label* label);
+
     void checkStackAlignment();
 
     void alignStackPointer();
