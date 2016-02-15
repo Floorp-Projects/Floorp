5063a08ff9193cf842155037bdf6c539a34c4af1
Phil Ringnalda <philringnalda@gmail.com>
2016-02-15 13:12:06 -0800

    Bug 1245937 - requestLongerTimeout for browser_chatwindow.js and browser_focus.js since 1154277 slowed them down, a=testonly CLOSED TREE

--- browser/base/content/test/chat/browser_focus.js
+++ browser/base/content/test/chat/browser_focus.js
@@ -7,6 +7,8 @@
 Components.utils.import("resource://gre/modules/Promise.jsm", this);
 const CHAT_URL = "https://example.com/browser/browser/base/content/test/chat/chat.html";
 
+requestLongerTimeout(2);
+
 // Is the currently opened tab focused?
 function isTabFocused() {
   let tabb = gBrowser.getBrowserForTab(gBrowser.selectedTab);
