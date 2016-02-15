d99f2dd2cd74a9cfcf4b88e3f6cff288604c30fb
Mantaroh Yoshinaga <mantaroh@gmail.com>
2016-02-15 07:33:00 +0100

    Bug 1248339 - fix header sort order of hal. r=gsvelto

--- hal/WindowIdentifier.cpp
+++ hal/WindowIdentifier.cpp
@@ -4,8 +4,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "mozilla/dom/ContentChild.h"
 #include "WindowIdentifier.h"
+
+#include "mozilla/dom/ContentChild.h"
 #include "nsPIDOMWindow.h"
 
 namespace mozilla {
