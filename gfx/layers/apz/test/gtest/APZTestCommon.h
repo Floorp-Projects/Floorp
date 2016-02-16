84bb4cebe00b41fde41c09a39828b0741378245f
Kartikaya Gupta <kgupta@mozilla.com>
2016-02-16 17:25:54 -0500

    Bug 1246056 - Ensure that the MockContentController's timestamp is always >= GetStartupTime(). r=botond, a=test-only
    MozReview-Commit-ID: KfkX1ixpKaI

--- gfx/layers/apz/test/gtest/APZTestCommon.h
+++ gfx/layers/apz/test/gtest/APZTestCommon.h
@@ -68,6 +68,11 @@ private:
     &(gfxPrefs::Set##prefBase), \
     prefValue)
 
+static TimeStamp GetStartupTime() {
+  static TimeStamp sStartupTime = TimeStamp::Now();
+  return sStartupTime;
+}
+
 class MockContentController : public GeckoContentController {
 public:
   MOCK_METHOD1(RequestContentRepaint, void(const FrameMetrics&));
@@ -84,7 +89,7 @@ public:
 class MockContentControllerDelayed : public MockContentController {
 public:
   MockContentControllerDelayed()
-    : mTime(TimeStamp::Now())
+    : mTime(GetStartupTime())
   {
   }
 
@@ -256,11 +261,6 @@ public:
     mWaitForMainThread = true;
   }
 
-  static TimeStamp GetStartupTime() {
-    static TimeStamp sStartupTime = TimeStamp::Now();
-    return sStartupTime;
-  }
-
 private:
   bool mWaitForMainThread;
   MockContentControllerDelayed* mcc;
@@ -291,7 +291,7 @@ TestFrameMetrics()
 uint32_t
 MillisecondsSinceStartup(TimeStamp aTime)
 {
-  return (aTime - TestAsyncPanZoomController::GetStartupTime()).ToMilliseconds();
+  return (aTime - GetStartupTime()).ToMilliseconds();
 }
 
 #endif // mozilla_layers_APZTestCommon_h
