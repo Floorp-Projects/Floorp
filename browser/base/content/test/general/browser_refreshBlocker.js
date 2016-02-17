c4cf5c1a773b403ae75e5fe93d3f0b18b49f587a
Mike Conley <mconley@mozilla.com>
2016-02-17 15:57:14 -0500

    Bug 1246291 - Beef up RefreshBlocker regression tests. r=mossop, a=lizzard
    MozReview-Commit-ID: 99gakrP6vKh

--- browser/base/content/test/general/browser_refreshBlocker.js
+++ browser/base/content/test/general/browser_refreshBlocker.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const PAGE = "http://example.org/browser/browser/base/content/test/general/dummy_page.html";
+const META_PAGE = "http://example.org/browser/browser/base/content/test/general/refresh_meta.sjs"
+const HEADER_PAGE = "http://example.org/browser/browser/base/content/test/general/refresh_header.sjs"
+const TARGET_PAGE = "http://example.org/browser/browser/base/content/test/general/dummy_page.html";
 const PREF = "accessibility.blockautorefresh";
 
 /**
@@ -14,7 +16,7 @@ const PREF = "accessibility.blockautorefresh";
  *        Whether or not we expect the refresh attempt to succeed.
  * @returns Promise
  */
-function* attemptRefresh(browser, expectRefresh) {
+function* attemptFakeRefresh(browser, expectRefresh) {
   yield ContentTask.spawn(browser, expectRefresh, function*(expectRefresh) {
     let URI = docShell.QueryInterface(Ci.nsIWebNavigation).currentURI;
     let refresher = docShell.QueryInterface(Ci.nsIRefreshURI);
@@ -27,6 +29,11 @@ function* attemptRefresh(browser, expectRefresh) {
       // Cancel the pending refresh
       refresher.cancelRefreshURITimers();
     }
+
+    // The RefreshBlocker will wait until onLocationChange has
+    // been fired before it will show any notifications (see bug
+    // 1246291), so we cause this to occur manually here.
+    content.location = URI.spec + "#foo";
   });
 }
 
@@ -38,10 +45,10 @@ function* attemptRefresh(browser, expectRefresh) {
 add_task(function* test_can_enable_and_block() {
   yield BrowserTestUtils.withNewTab({
     gBrowser,
-    url: PAGE,
+    url: TARGET_PAGE,
   }, function*(browser) {
     // By default, we should be able to reload the page.
-    yield attemptRefresh(browser, true);
+    yield attemptFakeRefresh(browser, true);
 
     yield pushPrefs(["accessibility.blockautorefresh", true]);
 
@@ -49,37 +56,54 @@ add_task(function* test_can_enable_and_block() {
       BrowserTestUtils.waitForNotificationBar(gBrowser, browser,
                                               "refresh-blocked");
 
-    yield attemptRefresh(browser, false);
+    yield attemptFakeRefresh(browser, false);
 
     yield notificationPromise;
 
     yield pushPrefs(["accessibility.blockautorefresh", false]);
 
     // Page reloads should go through again.
-    yield attemptRefresh(browser, true);
+    yield attemptFakeRefresh(browser, true);
   });
 });
 
 /**
- * Tests that when a refresh is blocked that we show a notification
- * bar, and that when we click on the "Allow" button, the refresh
- * can then go through.
+ * Attempts a "real" refresh by opening a tab, and then sending it to
+ * an SJS page that will attempt to cause a refresh. This will also pass
+ * a delay amount to the SJS page. The refresh should be blocked, and
+ * the notification should be shown. Once shown, the "Allow" button will
+ * be clicked, and the refresh will go through. Finally, the helper will
+ * close the tab and resolve the Promise.
+ *
+ * @param refreshPage (string)
+ *        The SJS page to use. Use META_PAGE for the <meta> tag refresh
+ *        case. Use HEADER_PAGE for the HTTP header case.
+ * @param delay (int)
+ *        The amount, in ms, for the page to wait before attempting the
+ *        refresh.
+ *
+ * @returns Promise
  */
-add_task(function* test_can_allow_refresh() {
+function* testRealRefresh(refreshPage, delay) {
   yield BrowserTestUtils.withNewTab({
     gBrowser,
-    url: PAGE,
+    url: "about:blank",
   }, function*(browser) {
     yield pushPrefs(["accessibility.blockautorefresh", true]);
 
-    let notificationPromise =
-      BrowserTestUtils.waitForNotificationBar(gBrowser, browser,
-                                              "refresh-blocked");
+    browser.loadURI(refreshPage + "?p=" + TARGET_PAGE + "&d=" + delay);
+    yield BrowserTestUtils.browserLoaded(browser);
 
-    yield attemptRefresh(browser, false);
-    let notification = yield notificationPromise;
+    // Once browserLoaded resolves, all nsIWebProgressListener callbacks
+    // should have fired, so the notification should be visible.
+    let notificationBox = gBrowser.getNotificationBox(browser);
+    let notification = notificationBox.currentNotification;
 
-    // Then click the button to submit the crash report.
+    ok(notification, "Notification should be visible");
+    is(notification.value, "refresh-blocked",
+       "Should be showing the right notification");
+
+    // Then click the button to allow the refresh.
     let buttons = notification.querySelectorAll(".notification-button");
     is(buttons.length, 1, "Should have one button.");
 
@@ -89,4 +113,23 @@ add_task(function* test_can_allow_refresh() {
 
     yield refreshPromise;
   });
+}
+
+/**
+ * Tests the meta-tag case for both short and longer delay times.
+ */
+add_task(function* test_can_allow_refresh() {
+  yield testRealRefresh(META_PAGE, 0);
+  yield testRealRefresh(META_PAGE, 100);
+  yield testRealRefresh(META_PAGE, 500);
+});
+
+/**
+ * Tests that when a HTTP header case for both short and longer
+ * delay times.
+ */
+add_task(function* test_can_block_refresh_from_header() {
+  yield testRealRefresh(HEADER_PAGE, 0);
+  yield testRealRefresh(HEADER_PAGE, 100);
+  yield testRealRefresh(HEADER_PAGE, 500);
 });
