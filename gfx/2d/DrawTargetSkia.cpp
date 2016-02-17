eac60ec3866157340000e90e23a542ceb7bf18e1
Mason Chang <mchang@mozilla.com>
2016-02-17 13:19:56 -0800

    Backout 2c97c2a5dad7 bug 1249033. r=me

--- gfx/2d/DrawTargetSkia.cpp
+++ gfx/2d/DrawTargetSkia.cpp
@@ -554,8 +554,6 @@ DrawTargetSkia::ShouldLCDRenderText(FontType aFontType, AntialiasMode aAntialias
   if (aAntialiasMode == AntialiasMode::DEFAULT) {
     switch (aFontType) {
       case FontType::MAC:
-      case FontType::GDI:
-      case FontType::DWRITE:
         return true;
       default:
         // TODO: Figure out what to do for the other platforms.
