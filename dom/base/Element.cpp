7f068cc774cf8427c5453adc8bfcb05c8a59b54a
Xidorn Quan <quanxunzhen@gmail.com>
2016-02-17 08:47:11 +0800

    Bug 743198 part 11 - Update locales string to the new spelling as well as unprefixed API. r=smaug
    The main motivation of this patch is to fix the prefixed function name
    used in one string. But updated string should have different a different
    identifier, otherwise it might be ignored. Since we should eventually
    prefer using word "fullscreen" over "full-screen", it is easier to just
    change all of them together.
    
    --HG--
    extra : source : 1e9ab6a7503102b5562475784510581720bd1bad

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -1744,7 +1744,7 @@ Element::UnbindFromTree(bool aDeep, bool aNullParent)
       nsContentUtils::ReportToConsole(nsIScriptError::warningFlag,
                                       NS_LITERAL_CSTRING("DOM"), OwnerDoc(),
                                       nsContentUtils::eDOM_PROPERTIES,
-                                      "RemovedFullScreenElement");
+                                      "RemovedFullscreenElement");
       // Fully exit full-screen.
       nsIDocument::ExitFullscreenInDocTree(OwnerDoc());
     }
@@ -3241,7 +3241,7 @@ GetFullScreenError(nsIDocument* aDoc)
   }
 
   if (!nsContentUtils::IsRequestFullScreenAllowed()) {
-    return "FullScreenDeniedNotInputDriven";
+    return "FullscreenDeniedNotInputDriven";
   }
 
   return nullptr;
