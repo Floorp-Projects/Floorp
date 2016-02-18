e61d3cd13fcf725dfded6f0e62a48bb9f78e60fa
Andrew Halberstadt <ahalberstadt@mozilla.com>
2016-02-18 13:47:59 -0500

    Bug 1231784 - Also override the startup.homepage_welcome_url.additional pref to avoid non-local connections on Beta/Release. r=jgriffin, a=test-only

--- testing/profiles/prefs_general.js
+++ testing/profiles/prefs_general.js
@@ -349,3 +349,4 @@ user_pref("dom.audiochannel.mutedByDefault", false);
 
 user_pref("webextensions.tests", true);
 user_pref("startup.homepage_welcome_url", "about:blank");
+user_pref("startup.homepage_welcome_url.additional", "");
