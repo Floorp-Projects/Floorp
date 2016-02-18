19c5f39ab165e52a962effc2d0156c987fd9a0ae
Milan Sreckovic <milan@mozilla.com>
2016-02-18 14:00:53 -0500

    Bug 1249312: UseAcceleratedCanvas on Mac should really be UseAcceleratedSkiaCanvas. r=mchang, a=lizzard

--- gfx/thebes/gfxPlatformMac.cpp
+++ gfx/thebes/gfxPlatformMac.cpp
@@ -427,10 +427,10 @@ gfxPlatformMac::ReadAntiAliasingThreshold()
 }
 
 bool
-gfxPlatformMac::UseAcceleratedCanvas()
+gfxPlatformMac::UseAcceleratedSkiaCanvas()
 {
   // Lion or later is required
-  return nsCocoaFeatures::OnLionOrLater() && Preferences::GetBool("gfx.canvas.azure.accelerated", false);
+  return nsCocoaFeatures::OnLionOrLater() && gfxPlatform::UseAcceleratedSkiaCanvas();
 }
 
 bool
