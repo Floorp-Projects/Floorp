50802e775a679a1414773eefd602b50923b6ca57
Byron Campen [:bwc] <docfaraday@gmail.com>
2016-02-18 14:13:35 -0600

    Bug 1249098: Support maxplaybackrate for opus. r=jesup
    MozReview-Commit-ID: 7BKVFkbPgV2
    
    --HG--
    extra : rebase_source : 3ef372486a8c608c780e864412942e8819cc3f88

--- media/webrtc/signaling/src/sdp/sipcc/sdp_main.c
+++ media/webrtc/signaling/src/sdp/sipcc/sdp_main.c
@@ -430,7 +430,8 @@ const sdp_namearray_t sdp_fmtp_codec_param[SDP_MAX_FMTP_PARAM] =
     {"useinbandfec",                    sizeof("useinbandfec")}, /* 46 */
     {"maxcodedaudiobandwidth",          sizeof("maxcodedaudiobandwidth")}, /* 47 */
     {"cbr",                             sizeof("cbr")}, /* 48 */
-    {"max-fr",                          sizeof("max-fr")} /* 49 */
+    {"max-fr",                          sizeof("max-fr")}, /* 49 */
+    {"maxplaybackrate",                 sizeof("maxplaybackrate")} /* 50 */
 } ;
 
 /* Note: These *must* be in the same order as the enum type. */
