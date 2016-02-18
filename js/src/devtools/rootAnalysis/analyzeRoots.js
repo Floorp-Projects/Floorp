9ab433b4cdd6d6d2a46177e9989de8c1515a8f43
Steve Fink <sfink@mozilla.com>
2016-02-17 16:42:58 -0800

    No bug. Make it easier to work on specific functions in the XDB files
    --HG--
    extra : rebase_source : 539dd822b7db02e1d31fc811dd687f42fff06b2f

--- js/src/devtools/rootAnalysis/analyzeRoots.js
+++ js/src/devtools/rootAnalysis/analyzeRoots.js
@@ -687,13 +687,6 @@ var each = Math.floor(N/numBatches);
 var start = minStream + each * (batch - 1);
 var end = Math.min(minStream + each * batch - 1, maxStream);
 
-// For debugging: Set this variable to the function name you're interested in
-// debugging and run once. That will print out the nameIndex of that function.
-// Insert that into the following statement to go directly to just that
-// function. Add your debugging printouts or debugger; statements or whatever.
-var theFunctionNameToFind;
-// var start = end = 12345;
-
 function process(name, json) {
     functionName = name;
     functionBodies = JSON.parse(json);
