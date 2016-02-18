ed3b511cb2eab652f42c69db2e7e3bce6a8c2dd4
Felipe Gomes <felipc@gmail.com>
2016-02-18 00:53:48 -0200

    Bug 1248796 - Store on Telemetry whether the e10s blocking code successfully ran. r=gftrizsche data-r=bsmedberg a=sledru

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -4818,6 +4818,9 @@ mozilla::BrowserTabsRemoteAutostart()
     mozilla::Telemetry::Accumulate(mozilla::Telemetry::E10S_BLOCKED_FROM_RUNNING,
                                     !gBrowserTabsRemoteAutostart);
   }
+  if (Preferences::HasUserValue("extensions.e10sBlockedByAddons")) {
+    mozilla::Telemetry::Accumulate(mozilla::Telemetry::E10S_ADDONS_BLOCKER_RAN, true);
+  }
   return gBrowserTabsRemoteAutostart;
 }
 
