5e4623997bdfc67fec1d2b180140e92264733e3d
Jakob Stoklund Olesen <jolesen@mozilla.com>
2016-02-18 11:38:15 -0800

    Bug 1244889 - Disallow unsigned SIMD types for global variables. r=luke
    Treat global variables the same as function arguments and return values: No
    unsigned SIMD types allowed. This simplifies interoperation with WebAssembly.
    
    Add a Type::isGloblVarType() which can be used to check for valid types.
    
    Fold CheckGlobalVariableImportExpr into its single caller.
    
    Remove Type::var(ValType) and Type::ret(ExprType). These backwards conversions
    were only used for diagnostics, and we can use the existing ToCString() function
    for that. Move this function form Wasm.cpp to WasmTypes.h so it can be used
    everywhere.
    
    MozReview-Commit-ID: FolGlCTVmgZ

--- js/src/asmjs/Wasm.cpp
+++ js/src/asmjs/Wasm.cpp
@@ -83,23 +83,6 @@ class FunctionDecoder
     }
 };
 
-static const char*
-ToCString(ExprType type)
-{
-    switch (type) {
-      case ExprType::Void:  return "void";
-      case ExprType::I32:   return "i32";
-      case ExprType::I64:   return "i64";
-      case ExprType::F32:   return "f32";
-      case ExprType::F64:   return "f64";
-      case ExprType::I32x4: return "i32x4";
-      case ExprType::F32x4: return "f32x4";
-      case ExprType::B32x4: return "b32x4";
-      case ExprType::Limit:;
-    }
-    MOZ_CRASH("bad expression type");
-}
-
 static bool
 CheckType(FunctionDecoder& f, ExprType actual, ExprType expected)
 {
