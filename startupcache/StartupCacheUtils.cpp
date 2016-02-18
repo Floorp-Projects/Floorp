0adb53017799954c691a7f026833bf85e22e4a1c
Nathan Froyd <froydnj@mozilla.com>
2016-02-18 11:14:02 -0500

    Bug 1249389 - part 1 - change NewObjectInputStreamFromBuffer to take a UniquePtr argument; r=erahm
    Because NewObjectInputStreamFromBuffer takes a raw pointer as input, the
    typical coding pattern to use it is:
    
      nsAutoArrayPtr<char> buf;
      // assign something to buf
      nsresult rv = NewObjectInputStreamFromBuffer(buf, ...);
      if (NS_FAILED(rv)) {
        ...
        return rv;
      }
      buf.forget();
    
    Which is clumsy, error-prone, and obscures the ownership transfer of the
    pointer into the stream returned by NewObjectInputStreamFromBuffer.
    Let's address all of these concerns by changing the argument to a
    UniquePtr<char[]>.

--- startupcache/StartupCacheUtils.cpp
+++ startupcache/StartupCacheUtils.cpp
@@ -19,7 +19,7 @@ namespace mozilla {
 namespace scache {
 
 NS_EXPORT nsresult
-NewObjectInputStreamFromBuffer(char* buffer, uint32_t len, 
+NewObjectInputStreamFromBuffer(UniquePtr<char[]> buffer, uint32_t len, 
                                nsIObjectInputStream** stream)
 {
   nsCOMPtr<nsIStringInputStream> stringStream
@@ -27,7 +27,7 @@ NewObjectInputStreamFromBuffer(char* buffer, uint32_t len,
   nsCOMPtr<nsIObjectInputStream> objectInput 
     = do_CreateInstance("@mozilla.org/binaryinputstream;1");
   
-  stringStream->AdoptData(buffer, len);
+  stringStream->AdoptData(buffer.release(), len);
   objectInput->SetInputStream(stringStream);
   
   objectInput.forget(stream);
