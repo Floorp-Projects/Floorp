4c8a1f54f510e83812f97b962eb1978570c0e88c
Dão Gottwald <dao@mozilla.com>
2016-02-19 00:50:17 +0100

    Bug 1014185 - Remove about:customizing and use about:blank for customize mode instead. r=jaws

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -58,9 +58,6 @@ XPCOMUtils.defineLazyModuleGetter(this, "WebappManager",
 XPCOMUtils.defineLazyModuleGetter(this, "PageThumbs",
                                   "resource://gre/modules/PageThumbs.jsm");
 
-XPCOMUtils.defineLazyModuleGetter(this, "CustomizationTabPreloader",
-                                  "resource:///modules/CustomizationTabPreloader.jsm");
-
 XPCOMUtils.defineLazyModuleGetter(this, "PdfJs",
                                   "resource://pdf.js/PdfJs.jsm");
 
@@ -1072,7 +1069,6 @@ BrowserGlue.prototype = {
 
     SelfSupportBackend.uninit();
 
-    CustomizationTabPreloader.uninit();
     WebappManager.uninit();
 
     NewTabPrefsProvider.prefs.uninit();
