e0a3c2981bfbc767f77ca89f5f0e8e72c4debc69
David Rajchenbach-Teller <dteller@mozilla.com>
2016-02-19 12:51:04 +0100

    Bug 1249590 - Bullet-proofing AsyncShutdown wrt exceptions;r=froydnj
    While investigating bug 1248489, we discovered that some code paths in
    AsyncShutdown could possibly be sensitive to exceptions being thrown
    in unexpected places. This patch attempts to make AsyncShutdown more
    robust to such exceptions.
    
    MozReview-Commit-ID: 5ImL9YNVgQr

--- netwerk/protocol/http/nsHttpConnectionMgr.cpp
+++ netwerk/protocol/http/nsHttpConnectionMgr.cpp
@@ -188,6 +188,7 @@ nsHttpConnectionMgr::Shutdown()
 
     // wait for shutdown event to complete
     while (!shutdownWrapper->mBool) {
+        fprintf(stderr, "nsHttpConnectionMgr::Shutdown() ProcessNextEvent\n");
         NS_ProcessNextEvent(NS_GetCurrentThread());
     }
 
