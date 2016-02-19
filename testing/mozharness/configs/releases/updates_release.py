2811e35ef0af413bb579d000ba1fdf9eb3038dd2
Rail Aliiev <rail@mozilla.com>
2016-02-19 12:53:03 -0800

    Bug 1210539 - Add "updates" builder to release promotion task r=jlund DONTBUILD a=release
    MozReview-Commit-ID: ANFJ6g4CT05
    
    --HG--
    rename : testing/mozharness/mozharness/mozilla/merge.py => testing/mozharness/mozharness/mozilla/repo_manupulation.py
    extra : source : 93654b236653d2628b7d49ebe2b825bc85d4c754

---
+++ testing/mozharness/configs/releases/updates_release.py
@@ -0,0 +1,43 @@
+
+config = {
+    "log_name": "updates_date",
+    "repo": {
+        "repo": "https://hg.mozilla.org/build/tools",
+        "revision": "default",
+        "dest": "tools",
+        "vcs": "hg",
+    },
+    "push_dest": "ssh://hg.mozilla.org/build/tools",
+    "shipped-locales-url": "https://hg.mozilla.org/releases/mozilla-release/raw-file/{revision}/browser/locales/shipped-locales",
+    "ignore_no_changes": True,
+    "ssh_user": "ffxbld",
+    "ssh_key": "~/.ssh/ffxbld_rsa",
+    "archive_domain": "archive.mozilla.org",
+    "archive_prefix": "https://archive.mozilla.org",
+    "previous_archive_prefix": "https://archive.mozilla.org",
+    "download_domain": "download.mozilla.org",
+    "balrog_url": "https://aus5.mozilla.org",
+    "balrog_username": "ffxbld",
+    "update_channels": {
+        "beta": {
+            "version_regex": r"^(\d+\.\d+(b\d+)?)$",
+            "requires_mirrors": False,
+            "patcher_config": "moBeta-branch-patcher2.cfg",
+            "update_verify_channel": "beta-localtest",
+            "mar_channel_ids": [
+                "firefox-mozilla-beta", "firefox-mozilla-beta",
+            ],
+            "channel_names": ["beta", "beta-localtest", "beta-cdntest"],
+            "rules_to_update": ["firefox-beta-cdntest", "firefox-beta-localtest"],
+        },
+        "release": {
+            "version_regex": r"^\d+\.\d+(\.\d+)?$",
+            "requires_mirrors": True,
+            "patcher_config": "mozRelease-branch-patcher2.cfg",
+            "update_verify_channel": "release-localtest",
+            "mar_channel_ids": [],
+            "channel_names": ["release", "release-localtest", "release-cdntest"],
+            "rules_to_update": ["firefox-release-cdntest", "firefox-release-localtest"],
+        },
+    },
+}
