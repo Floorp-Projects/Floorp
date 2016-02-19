0a72c5e9e37f01a421f08e613625aba5f99558fd
Andrea Marchesini <amarchesini@mozilla.com>
2016-02-19 14:49:50 +0100

    Bug 1245184 - CookieManager should remove cookies only if they match the userContextId, r=sicking

--- browser/base/content/sanitize.js
+++ browser/base/content/sanitize.js
@@ -241,7 +241,8 @@ Sanitizer.prototype = {
 
               if (cookie.creationTime > range[0]) {
                 // This cookie was created after our cutoff, clear it
-                cookieMgr.remove(cookie.host, cookie.name, cookie.path, false);
+                cookieMgr.remove(cookie.host, cookie.name, cookie.path,
+                                 cookie.originAttributes, false);
 
                 if (++yieldCounter % YIELD_PERIOD == 0) {
                   yield new Promise(resolve => setTimeout(resolve, 0)); // Don't block the main thread too long
