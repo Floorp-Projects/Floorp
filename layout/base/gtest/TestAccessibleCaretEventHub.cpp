b6ca4c24ee866003c6aefa2858cf65cc3dc19eb1
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-02-19 09:38:59 +0100

    Backed out changeset a27cd3e26cc1 (bug 1248847) for bustage

--- layout/base/gtest/TestAccessibleCaretEventHub.cpp
+++ layout/base/gtest/TestAccessibleCaretEventHub.cpp
@@ -35,7 +35,7 @@ namespace mozilla
 class MockAccessibleCaretManager : public AccessibleCaretManager
 {
 public:
-  MockAccessibleCaretManager()
+  explicit MockAccessibleCaretManager()
     : AccessibleCaretManager(nullptr)
   {
   }
@@ -63,7 +63,7 @@ public:
   using AccessibleCaretEventHub::LongTapState;
   using AccessibleCaretEventHub::FireScrollEnd;
 
-  MockAccessibleCaretEventHub()
+  explicit MockAccessibleCaretEventHub()
     : AccessibleCaretEventHub(nullptr)
   {
     mManager = MakeUnique<MockAccessibleCaretManager>();
@@ -101,20 +101,10 @@ public:
 class AccessibleCaretEventHubTester : public ::testing::Test
 {
 public:
-  AccessibleCaretEventHubTester()
+  explicit AccessibleCaretEventHubTester()
   {
     DefaultValue<nsresult>::Set(NS_OK);
     EXPECT_EQ(mHub->GetState(), MockAccessibleCaretEventHub::NoActionState());
-
-    // AccessibleCaretEventHub requires the caller to hold a ref to it. We just
-    // add ref here for the sake of convenience.
-    mHub->AddRef();
-  }
-
-  ~AccessibleCaretEventHubTester()
-  {
-    // Release the ref added in the constructor.
-    mHub->Release();
   }
 
   static UniquePtr<WidgetEvent> CreateMouseEvent(EventMessage aMessage,
