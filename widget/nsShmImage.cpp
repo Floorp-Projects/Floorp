a2c24d8100c73979f72e119ee7a8bbc65e7825b4
Lee Salzman <lsalzman@mozilla.com>
2016-02-19 16:14:02 -0500

    Bug 1239152 - skip memset since XShm is already initialized to zero. r=mchang

--- widget/nsShmImage.cpp
+++ widget/nsShmImage.cpp
@@ -146,21 +146,19 @@ nsShmImage::CreateImage(const LayoutDeviceIntSize& aSize,
         (mImage->green_mask == 0xff00) &&
         (mImage->blue_mask == 0xff)) {
       mFormat = SurfaceFormat::B8G8R8A8;
-      memset(mImage->data, 0, mImage->bytes_per_line * mImage->height);
     }
     break;
   case 24:
-    // Only xRGB is supported.
+    // Only support the BGRX layout, and report it as BGRA to the compositor.
+    // The alpha channel will be discarded when we put the image.
     if ((mImage->red_mask == 0xff0000) &&
         (mImage->green_mask == 0xff00) &&
         (mImage->blue_mask == 0xff)) {
-      mFormat = SurfaceFormat::B8G8R8X8;
-      memset(mImage->data, 0xFF, mImage->bytes_per_line * mImage->height);
+      mFormat = SurfaceFormat::B8G8R8A8;
     }
     break;
   case 16:
     mFormat = SurfaceFormat::R5G6B5_UINT16;
-    memset(mImage->data, 0, mImage->bytes_per_line * mImage->height);
     break;
   }
 
