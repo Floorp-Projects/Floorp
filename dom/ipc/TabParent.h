6247df7a6bceb47bc33534539acd6b7cc3e7a739
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2016-02-19 21:46:31 -0500

    Bug 1249280 - Fix coordinates of APZ-detected gestures when crossing process boundaries. r=kats
    MozReview-Commit-ID: 9xPcG9uERB0

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -496,6 +496,7 @@ public:
   }
 
   LayoutDeviceIntPoint GetChildProcessOffset();
+  CSSPoint AdjustTapToChildWidget(const CSSPoint& aPoint);
 
   /**
    * Native widget remoting protocol for use with windowed plugins with e10s.
@@ -611,8 +612,6 @@ private:
   void TryCacheDPIAndScale();
 
   nsresult UpdatePosition();
-
-  CSSPoint AdjustTapToChildWidget(const CSSPoint& aPoint);
 
   bool AsyncPanZoomEnabled() const;
 
