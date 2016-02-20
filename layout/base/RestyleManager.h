90a1d902d1d49b993e2d97c8719861732b90f072
Cameron McCormack <cam@mcc.id.au>
2016-02-20 13:20:24 +1100

    Bug 1133615 - Don't assert about the contents of RestyleData::mDescendants when in a full style rebuild. r=dbaron

--- layout/base/RestyleManager.h
+++ layout/base/RestyleManager.h
@@ -442,6 +442,10 @@ public:
   void PostRebuildAllStyleDataEvent(nsChangeHint aExtraHint,
                                     nsRestyleHint aRestyleHint);
 
+#ifdef DEBUG
+  bool InRebuildAllStyleData() const { return mInRebuildAllStyleData; }
+#endif
+
 #ifdef RESTYLE_LOGGING
   /**
    * Returns whether a restyle event currently being processed by this
