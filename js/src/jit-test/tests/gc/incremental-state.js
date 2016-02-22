c8822258ef4579e501e5511903b151a8c2407155
Terrence Cole <terrence@mozilla.com>
2016-02-22 08:51:50 -0800

    Bug 1249367 - Make background finalization a GC phase (and clean up Zones properly); r=jonco

--- js/src/jit-test/tests/gc/incremental-state.js
+++ js/src/jit-test/tests/gc/incremental-state.js
@@ -1,49 +1,58 @@
-/*
- * Test expected state changes during collection.
- */
-
+// Test expected state changes during collection.
 if (!("gcstate" in this))
     quit();
 
 gczeal(0);
 
-/* Non-incremental GC. */
+// Non-incremental GC.
 gc();
 assertEq(gcstate(), "none");
 
-/* Incremental GC in one slice. */
+// Incremental GC in minimal slice. Note that finalization always uses zero-
+// sized slices while background finalization is on-going, so we need to loop.
 gcslice(1000000);
+while (gcstate() == "finalize") { gcslice(1); }
 assertEq(gcstate(), "none");
 
-/* 
- * Incremental GC in multiple slices: if marking takes more than one slice,
- * we yield before we start sweeping.
- */
+// Incremental GC in multiple slices: if marking takes more than one slice,
+// we yield before we start sweeping.
 gczeal(0);
 gcslice(1);
 assertEq(gcstate(), "mark");
 gcslice(1000000);
 assertEq(gcstate(), "mark");
 gcslice(1000000);
+while (gcstate() == "finalize") { gcslice(1); }
 assertEq(gcstate(), "none");
 
-/* Zeal mode 8: Incremental GC in two slices: 1) mark roots 2) finish collection. */
+// Zeal mode 8: Incremental GC in two main slices:
+//   1) mark roots
+//   2) mark and sweep
+//   *) finalize.
 gczeal(8);
 gcslice(1);
 assertEq(gcstate(), "mark");
 gcslice(1);
+while (gcstate() == "finalize") { gcslice(1); }
 assertEq(gcstate(), "none");
 
-/* Zeal mode 9: Incremental GC in two slices: 1) mark all 2) new marking and finish. */
+// Zeal mode 9: Incremental GC in two main slices:
+//   1) mark roots and marking
+//   2) new marking and sweeping
+//   *) finalize.
 gczeal(9);
 gcslice(1);
 assertEq(gcstate(), "mark");
 gcslice(1);
+while (gcstate() == "finalize") { gcslice(1); }
 assertEq(gcstate(), "none");
 
-/* Zeal mode 10: Incremental GC in multiple slices (always yeilds before sweeping). */
+// Zeal mode 10: Incremental GC in multiple slices (always yeilds before
+// sweeping). This test uses long slices to prove that this zeal mode yields
+// in sweeping, where normal IGC (above) does not.
 gczeal(10);
 gcslice(1000000);
 assertEq(gcstate(), "sweep");
 gcslice(1000000);
+while (gcstate() == "finalize") { gcslice(1); }
 assertEq(gcstate(), "none");
