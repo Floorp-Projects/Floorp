155f4fbb87766002a678ac7f9ad8451c2e2318b5
Alexander Surkov <surkov.alexander@gmail.com>
2016-02-22 12:32:03 -0500

    Bug 1249927 - devirtualize CanHavaAnonymousChildren, r=davdib

--- accessible/xul/XULListboxAccessible.cpp
+++ accessible/xul/XULListboxAccessible.cpp
@@ -544,6 +544,10 @@ XULListitemAccessible::
                                       nsGkAtoms::checkbox,
                                       eCaseMatters);
   mType = eXULListItemType;
+
+  // Walk XBL anonymous children for list items. Overrides the flag value from
+  // base XULMenuitemAccessible class.
+  mStateFlags &= ~eNoXBLKids;
 }
 
 XULListitemAccessible::~XULListitemAccessible()
@@ -666,13 +670,6 @@ XULListitemAccessible::ActionNameAt(uint8_t aIndex, nsAString& aName)
     else
       aName.AssignLiteral("check");
   }
-}
-
-bool
-XULListitemAccessible::CanHaveAnonChildren()
-{
-  // That indicates we should walk anonymous children for listitems
-  return true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
