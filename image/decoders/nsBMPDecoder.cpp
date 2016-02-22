f3940f33bed976788b0791d2ceb4c87bb9d05477
Nicholas Nethercote <nnethercote@mozilla.com>
2016-02-22 13:38:58 +1100

    Bug 1249550 (part 1) - Flip BMPs with negative height when downscaling. r=tn, a=sylvestre

--- image/decoders/nsBMPDecoder.cpp
+++ image/decoders/nsBMPDecoder.cpp
@@ -681,10 +681,11 @@ nsBMPDecoder::ReadBitfields(const char* aData, size_t aLength)
 
   if (mDownscaler) {
     // BMPs store their rows in reverse order, so the downscaler needs to
-    // reverse them again when writing its output.
+    // reverse them again when writing its output. Unless the height is
+    // negative!
     rv = mDownscaler->BeginFrame(GetSize(), Nothing(),
                                  mImageData, mMayHaveTransparency,
-                                 /* aFlipVertically = */ true);
+                                 /* aFlipVertically = */ mH.mHeight >= 0);
     if (NS_FAILED(rv)) {
       return Transition::TerminateFailure();
     }
