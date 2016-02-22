307cf5f00b606fb5f219e5536cc41386b44175f7
Kartikaya Gupta <kgupta@mozilla.com>
2016-02-22 14:14:20 -0500

    Bug 1243267 - Guard against reentrancy into the dispatch of a scroll event. r=mats a=lizzard
    MozReview-Commit-ID: 5saxPwxOuc8

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -4257,13 +4257,17 @@ ScrollFrameHelper::ScrollEvent::ScrollEvent(ScrollFrameHelper* aHelper)
 
 ScrollFrameHelper::ScrollEvent::~ScrollEvent()
 {
-  mDriver->RemoveRefreshObserver(this, Flush_Style);
-  mDriver = nullptr;
+  if (mDriver) {
+    mDriver->RemoveRefreshObserver(this, Flush_Style);
+    mDriver = nullptr;
+  }
 }
 
 void
 ScrollFrameHelper::ScrollEvent::WillRefresh(mozilla::TimeStamp aTime)
 {
+  mDriver->RemoveRefreshObserver(this, Flush_Style);
+  mDriver = nullptr;
   mHelper->FireScrollEvent();
 }
 
