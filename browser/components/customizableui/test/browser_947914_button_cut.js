4ae24adba6680bd7a2246d9c37d21c100277aaae
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-02-23 10:32:21 +0000

    Bug 1091561 - fix tab bookkeeping for cut/copy/paste tests to make them work on e10s, rs=trivial
    MozReview-Commit-ID: 4b1ZMZ6q3Yh

--- browser/components/customizableui/test/browser_947914_button_cut.js
+++ browser/components/customizableui/test/browser_947914_button_cut.js
@@ -8,55 +8,50 @@ var initialLocation = gBrowser.currentURI.spec;
 var globalClipboard;
 
 add_task(function*() {
-  info("Check cut button existence and functionality");
+  yield BrowserTestUtils.withNewTab({gBrowser, url: "about:blank"}, function*() {
+    info("Check cut button existence and functionality");
 
-  let testText = "cut text test";
+    let testText = "cut text test";
 
-  gURLBar.focus();
-  yield PanelUI.show();
-  info("Menu panel was opened");
+    gURLBar.focus();
+    yield PanelUI.show();
+    info("Menu panel was opened");
 
-  let cutButton = document.getElementById("cut-button");
-  ok(cutButton, "Cut button exists in Panel Menu");
-  ok(cutButton.hasAttribute("disabled"), "Cut button is disabled");
+    let cutButton = document.getElementById("cut-button");
+    ok(cutButton, "Cut button exists in Panel Menu");
+    ok(cutButton.hasAttribute("disabled"), "Cut button is disabled");
 
-  // cut text from URL bar
-  gURLBar.value = testText;
-  gURLBar.focus();
-  gURLBar.select();
-  yield PanelUI.show();
-  info("Menu panel was opened");
+    // cut text from URL bar
+    gURLBar.value = testText;
+    gURLBar.focus();
+    gURLBar.select();
+    yield PanelUI.show();
+    info("Menu panel was opened");
 
-  ok(!cutButton.hasAttribute("disabled"), "Cut button is enabled when selecting");
-  cutButton.click();
-  is(gURLBar.value, "", "Selected text is removed from source when clicking on cut");
+    ok(!cutButton.hasAttribute("disabled"), "Cut button is enabled when selecting");
+    cutButton.click();
+    is(gURLBar.value, "", "Selected text is removed from source when clicking on cut");
 
-  // check that the text was added to the clipboard
-  let clipboard = Services.clipboard;
-  let transferable = Cc["@mozilla.org/widget/transferable;1"].createInstance(Ci.nsITransferable);
-  globalClipboard = clipboard.kGlobalClipboard;
+    // check that the text was added to the clipboard
+    let clipboard = Services.clipboard;
+    let transferable = Cc["@mozilla.org/widget/transferable;1"].createInstance(Ci.nsITransferable);
+    globalClipboard = clipboard.kGlobalClipboard;
 
-  transferable.init(null);
-  transferable.addDataFlavor("text/unicode");
-  clipboard.getData(transferable, globalClipboard);
-  let str = {}, strLength = {};
-  transferable.getTransferData("text/unicode", str, strLength);
-  let clipboardValue = "";
+    transferable.init(null);
+    transferable.addDataFlavor("text/unicode");
+    clipboard.getData(transferable, globalClipboard);
+    let str = {}, strLength = {};
+    transferable.getTransferData("text/unicode", str, strLength);
+    let clipboardValue = "";
 
-  if (str.value) {
-    str.value.QueryInterface(Ci.nsISupportsString);
-    clipboardValue = str.value.data;
-  }
-  is(clipboardValue, testText, "Data was copied to the clipboard.");
+    if (str.value) {
+      str.value.QueryInterface(Ci.nsISupportsString);
+      clipboardValue = str.value.data;
+    }
+    is(clipboardValue, testText, "Data was copied to the clipboard.");
+  });
 });
 
-add_task(function* asyncCleanup() {
-  // clear the clipboard
+registerCleanupFunction(function cleanup() {
   Services.clipboard.emptyClipboard(globalClipboard);
-  info("Clipboard was cleared");
-
-  // restore the tab as it was at the begining of the test
-  gBrowser.addTab(initialLocation);
-  gBrowser.removeTab(gBrowser.selectedTab);
-  info("Tabs were restored");
 });
