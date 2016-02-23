be768c0a8d0daa6e6b9dbe5878b522081b75d51c
Joel Maher <jmaher@mozilla.com>
2016-02-23 12:04:27 -0800

    Bug 1250635 - remove ENABLE_TESTS from dom/media cpp files. r=esawin
    MozReview-Commit-ID: D6T1253vNwP
    
    --HG--
    extra : rebase_source : a247b10446bf8aaa03f0aad55d4c451aad3c2fe2

--- dom/media/ADTSDemuxer.cpp
+++ dom/media/ADTSDemuxer.cpp
@@ -193,11 +193,6 @@ public:
   const Frame& CurrentFrame() const { return mFrame; }
 
 
-#ifdef ENABLE_TESTS
-  // Returns the previously parsed frame. Reset via Reset.
-  const Frame& PrevFrame() const { return mPrevFrame; }
-#endif
-
   // Returns the first parsed frame. Reset via Reset.
   const Frame& FirstFrame() const { return mFirstFrame; }
 
@@ -212,9 +207,6 @@ public:
   // - resets the CurrentFrame
   // - resets ID3Header if no valid header was parsed yet
   void EndFrameSession() {
-#ifdef ENABLE_TESTS
-    mPrevFrame = mFrame;
-#endif
     mFrame.Reset();
   }
 
@@ -237,9 +229,6 @@ private:
   // previously parsed frame for debugging and the currently parsed frame.
   Frame mFirstFrame;
   Frame mFrame;
-#ifdef ENABLE_TESTS
-  Frame mPrevFrame;
-#endif
 };
 
 
@@ -435,26 +424,6 @@ ADTSTrackDemuxer::Init()
 
   return mSamplesPerSecond && mChannels;
 }
-
-#ifdef ENABLE_TESTS
-const adts::Frame&
-ADTSTrackDemuxer::LastFrame() const
-{
-  return mParser->PrevFrame();
-}
-
-RefPtr<MediaRawData>
-ADTSTrackDemuxer::DemuxSample()
-{
-  return GetNextFrame(FindNextFrame());
-}
-
-media::TimeUnit
-ADTSTrackDemuxer::SeekPosition() const
-{
-  return Duration(mFrameIndex);
-}
-#endif
 
 UniquePtr<TrackInfo>
 ADTSTrackDemuxer::GetInfo() const
--- dom/media/MP3Demuxer.h
+++ dom/media/MP3Demuxer.h
@@ -297,10 +297,8 @@ public:
   // Returns the currently parsed frame. Reset via Reset or EndFrameSession.
   const Frame& CurrentFrame() const;
 
-#ifdef ENABLE_TESTS
   // Returns the previously parsed frame. Reset via Reset.
   const Frame& PrevFrame() const;
-#endif
 
   // Returns the first parsed frame. Reset via Reset.
   const Frame& FirstFrame() const;
@@ -343,9 +341,7 @@ private:
   // previously parsed frame for debugging and the currently parsed frame.
   Frame mFirstFrame;
   Frame mFrame;
-#ifdef ENABLE_TESTS
   Frame mPrevFrame;
-#endif
 };
 
 // The MP3 demuxer used to extract MPEG frames and side information out of
@@ -372,10 +368,8 @@ public:
   // Returns the estimated current seek position time.
   media::TimeUnit SeekPosition() const;
 
-#ifdef ENABLE_TESTS
   const FrameParser::Frame& LastFrame() const;
   RefPtr<MediaRawData> DemuxSample();
-#endif
 
   const ID3Parser::ID3Header& ID3Header() const;
   const FrameParser::VBRHeader& VBRInfo() const;
