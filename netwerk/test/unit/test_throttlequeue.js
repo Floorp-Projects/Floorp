90d29d8ac8967d38d0b79c00f4c43cc265bb3e29
Tom Tromey <tom@tromey.com>
2016-02-23 14:26:45 -0700

    Bug 1244227 - Add nsIThrottledInputChannel.idl and implement. r=mcmanus
    MozReview-Commit-ID: JVIjxEO901W
    
    --HG--
    extra : transplant_source : %B5%5Cb%DC%23%CFyEz%D0a%21Y%C0%18%9E%09%9ENZ

---
+++ netwerk/test/unit/test_throttlequeue.js
@@ -0,0 +1,23 @@
+// Test ThrottleQueue initialization.
+
+function init(tq, mean, max) {
+  let threw = false;
+  try {
+    tq.init(mean, max);
+  } catch (e) {
+    threw = true;
+  }
+  return !threw;
+}
+
+function run_test() {
+  let tq = Cc["@mozilla.org/network/throttlequeue;1"]
+      .createInstance(Ci.nsIInputChannelThrottleQueue);
+
+  ok(!init(tq, 0, 50), "mean bytes cannot be 0");
+  ok(!init(tq, 50, 0), "max bytes cannot be 0");
+  ok(!init(tq, 0, 0), "mean and max bytes cannot be 0");
+  ok(!init(tq, 70, 20), "max cannot be less than mean");
+
+  ok(init(tq, 2, 2), "valid initialization");
+}
