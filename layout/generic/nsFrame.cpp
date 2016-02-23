c218f8cd277f76798a22793ef278024c68b34d0c
Markus Stange <mstange@themasta.com>
2016-02-23 18:22:21 +0100

    Bug 1238564 - When building a fixed/sticky display item, don't restore the clip until we're ready to build that item so that inner items aren't unnecessarily clipped. r=roc
    This isn't really about regular clips, it's about scroll clips. If the inner
    item has an unnecessary scroll clip (one that's already on the parent), this can
    cause the inner item's bounds to be larger than necessary because, after the
    first patch in bug 1238564, it will include the whole bounds of async-scrollable
    scroll frames.
    Also, if e.g. the inner item is an opacity item, and it has a scroll clip that's
    not just the innermost ancestor scroll clip of all of its children, then
    FrameLayerBuilder's mContainerBounds == mAccumulatedChildBounds assertion can
    fail if the opacity item gets flattened away, because the child bounds won't
    have been enlarged for the scroll clip.
    
    There must be a better way to do the clip resetting in
    nsFrame::BuildDisplayListForStackingContext - the code is not pretty at all.
    But I'd rather get the tests passing first before we figure out how to clean it
    up.
    
    MozReview-Commit-ID: E1DdpN546va

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -2196,7 +2196,7 @@ nsIFrame::BuildDisplayListForStackingContext(nsDisplayListBuilder* aBuilder,
                                              containerItemScrollClip));
   }
 
-  if (!isTransformed) {
+  if (!isTransformed && !useFixedPosition && !useStickyPosition) {
     // Restore saved clip state now so that any display items we create below
     // are clipped properly.
     clipState.Restore();
@@ -2278,7 +2278,7 @@ nsIFrame::BuildDisplayListForStackingContext(nsDisplayListBuilder* aBuilder,
     }
 
     // Restore clip state now so nsDisplayTransform is clipped properly.
-    if (!HasPerspective()) {
+    if (!HasPerspective() && !useFixedPosition && !useStickyPosition) {
       clipState.Restore();
       if (didResetClip) {
         containerItemScrollClip = aBuilder->ClipState().GetCurrentInnermostScrollClip();
@@ -2303,9 +2303,11 @@ nsIFrame::BuildDisplayListForStackingContext(nsDisplayListBuilder* aBuilder,
     resultList.AppendNewToTop(transformItem);
 
     if (HasPerspective()) {
-      clipState.Restore();
-      if (didResetClip) {
-        containerItemScrollClip = aBuilder->ClipState().GetCurrentInnermostScrollClip();
+      if (!useFixedPosition && !useStickyPosition) {
+        clipState.Restore();
+        if (didResetClip) {
+          containerItemScrollClip = aBuilder->ClipState().GetCurrentInnermostScrollClip();
+        }
       }
       resultList.AppendNewToTop(
         new (aBuilder) nsDisplayPerspective(
@@ -2319,6 +2321,13 @@ nsIFrame::BuildDisplayListForStackingContext(nsDisplayListBuilder* aBuilder,
     if (useOpacity && !usingSVGEffects) {
       CreateOpacityItem(aBuilder, this, resultList, opacityItemForEventsOnly,
                         containerItemScrollClip);
+    }
+  }
+
+  if (useFixedPosition || useStickyPosition) {
+    clipState.Restore();
+    if (didResetClip) {
+      containerItemScrollClip = aBuilder->ClipState().GetCurrentInnermostScrollClip();
     }
   }
 
