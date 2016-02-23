a193c3220e559c04a11c23b2bcf8b50e794e1f8d
Brendan Dahl <bdahl@mozilla.com>
2016-02-23 17:10:00 +0100

    Bug 1104916 - Implement CSS media query display-mode. r=cam

--- xpfe/appshell/nsWebShellWindow.cpp
+++ xpfe/appshell/nsWebShellWindow.cpp
@@ -365,6 +365,11 @@ nsWebShellWindow::SizeModeChanged(nsSizeMode sizeMode)
     ourWindow->DispatchCustomEvent(NS_LITERAL_STRING("sizemodechange"));
   }
 
+  nsIPresShell* presShell;
+  if ((presShell = GetPresShell())) {
+    presShell->GetPresContext()->SizeModeChanged(sizeMode);
+  }
+
   // Note the current implementation of SetSizeMode just stores
   // the new state; it doesn't actually resize. So here we store
   // the state and pass the event on to the OS. The day is coming
