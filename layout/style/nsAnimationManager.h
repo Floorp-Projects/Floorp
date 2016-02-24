2b1e865f3d6e09d7f0a4014176e547a5f09627a4
Brian Birtles <birtles@gmail.com>
2016-02-24 13:41:16 +0900

    Bug 1239945 part 1 - Drop a number of animation manager/collection-related methods that are unused (and in some cases undefined); r=dholbert

--- layout/style/nsAnimationManager.h
+++ layout/style/nsAnimationManager.h
@@ -320,10 +320,6 @@ public:
   void StopAnimationsForElement(mozilla::dom::Element* aElement,
                                 mozilla::CSSPseudoElementType aPseudoType);
 
-  bool IsAnimationManager() override {
-    return true;
-  }
-
 protected:
   virtual ~nsAnimationManager() {}
 
