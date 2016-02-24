c7d4c0bc70d9ff4e6e3548520039caad428f86f4
Mark Hammond <mhammond@skippinet.com.au>
2016-02-24 12:38:30 +1100

    Bug 1244929 - Allow UITour's showFirefoxAccounts command to accept additional URL params. r=MattN

--- browser/components/uitour/test/browser_UITour_sync.js
+++ browser/components/uitour/test/browser_UITour_sync.js
@@ -22,8 +22,50 @@ add_UITour_task(function* test_checkSyncSetup_enabled() {
 });
 
 // The showFirefoxAccounts API is sync related, so we test that here too...
-add_UITour_task(function* test_firefoxAccounts() {
+add_UITour_task(function* test_firefoxAccountsNoParams() {
   yield gContentAPI.showFirefoxAccounts();
   yield BrowserTestUtils.browserLoaded(gTestTab.linkedBrowser, false,
                                        "about:accounts?action=signup&entrypoint=uitour");
+});
+
+add_UITour_task(function* test_firefoxAccountsValidParams() {
+  yield gContentAPI.showFirefoxAccounts({ utm_foo: "foo", utm_bar: "bar" });
+  yield BrowserTestUtils.browserLoaded(gTestTab.linkedBrowser, false,
+                                       "about:accounts?action=signup&entrypoint=uitour&utm_foo=foo&utm_bar=bar");
+});
+
+// A helper to check the request was ignored due to invalid params.
+function* checkAboutAccountsNotLoaded() {
+  try {
+    yield waitForConditionPromise(() => {
+      return gBrowser.selectedBrowser.currentURI.spec.startsWith("about:accounts");
+    }, "Check if about:accounts opened");
+    ok(false, "No about:accounts tab should have opened");
+  } catch (ex) {
+    ok(true, "No about:accounts tab opened");
+  }
+}
+
+add_UITour_task(function* test_firefoxAccountsNonObject() {
+  // non-string should be rejected.
+  yield gContentAPI.showFirefoxAccounts(99);
+  yield checkAboutAccountsNotLoaded();
+});
+
+add_UITour_task(function* test_firefoxAccountsNonUtmPrefix() {
+  // Any non "utm_" name should should be rejected.
+  yield gContentAPI.showFirefoxAccounts({ utm_foo: "foo", bar: "bar" });
+  yield checkAboutAccountsNotLoaded();
+});
+
+add_UITour_task(function* test_firefoxAccountsNonAlphaName() {
+  // Any "utm_" name which includes non-alpha chars should be rejected.
+  yield gContentAPI.showFirefoxAccounts({ utm_foo: "foo", "utm_bar=": "bar" });
+  yield checkAboutAccountsNotLoaded();
+});
+
+add_UITour_task(function* test_firefoxAccountsNonAlphaValue() {
+  // Any non-alpha value should be rejected.
+  yield gContentAPI.showFirefoxAccounts({ utm_foo: "foo&" });
+  yield checkAboutAccountsNotLoaded();
 });
