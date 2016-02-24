46718c1cde7577574c59b2cf9c8864d17e832b28
Bas Schouten <bschouten@mozilla.com>
2016-02-24 03:50:09 +0000

    Bug 1232042: Execute an additional present for D3D11 on the compositor thread when a WM_PAINT event has returned. r=jrmuizel

--- gfx/layers/Compositor.h
+++ gfx/layers/Compositor.h
@@ -450,6 +450,8 @@ public:
    */
   virtual bool Ready() { return true; }
 
+  virtual void ForcePresent() { }
+
   // XXX I expect we will want to move mWidget into this class and implement
   // these methods properly.
   virtual nsIWidget* GetWidget() const { return nullptr; }
