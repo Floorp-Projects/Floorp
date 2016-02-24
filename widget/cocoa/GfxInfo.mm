eb4af6a608685b66437330231deeb0569e4f0e37
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-02-24 19:01:50 +0100

    Backed out changeset 8151f4102646 (bug 1249640)

--- widget/cocoa/GfxInfo.mm
+++ widget/cocoa/GfxInfo.mm
@@ -348,10 +348,6 @@ GfxInfo::GetFeatureStatusImpl(int32_t aFeature,
         *aStatus = nsIGfxInfo::FEATURE_STATUS_OK;
         return NS_OK;
       }
-    } else if (aFeature == nsIGfxInfo::FEATURE_CANVAS2D_ACCELERATION) {
-      // See bug 1249659
-      *aStatus = (os > DRIVER_OS_OS_X_10_7) ? nsIGfxInfo::FEATURE_STATUS_OK : nsIGfxInfo::FEATURE_BLOCKED_OS_VERSION;
-      return NS_OK;
     }
   }
 
