f57b2437ab28eadaf7dae6d1cc175ceb56c9b26c
L. David Baron <dbaron@dbaron.org>
2016-02-24 10:40:30 -0800

    Bug 1250342 patch 3:  Rename preference layout.css.text-align-true-value.enabled to layout.css.text-align-unsafe-value.enabled .  r=mats
    MozReview-Commit-ID: 6IKnuvxZQcI

--- layout/base/nsLayoutUtils.h
+++ layout/base/nsLayoutUtils.h
@@ -2291,7 +2291,7 @@ public:
    * Checks whether support for the CSS text-align (and -moz-text-align-last)
    * 'true' value is enabled.
    */
-  static bool IsTextAlignTrueValueEnabled();
+  static bool IsTextAlignUnsafeValueEnabled();
 
   /**
    * Checks if CSS variables are currently enabled.
