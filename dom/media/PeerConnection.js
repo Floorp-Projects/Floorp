93d9eae63025efc9c4022fa72131465d4cfb2a1b
Jan-Ivar Bruaroey <jib@mozilla.com>
2016-02-24 19:14:57 -0500

    Bug 1247622 - Make RTCRtpSender.setParameters return a promise. r=jesup,smaug
    MozReview-Commit-ID: 6lfmqpKevQl

--- dom/media/PeerConnection.js
+++ dom/media/PeerConnection.js
@@ -1593,7 +1593,8 @@ RTCRtpSender.prototype = {
   },
 
   setParameters: function(parameters) {
-    return this._pc._setParameters(this, parameters);
+    return this._pc._win.Promise.resolve()
+      .then(() => this._pc._setParameters(this, parameters));
   },
 
   getParameters: function() {
