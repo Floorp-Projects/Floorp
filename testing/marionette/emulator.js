5506aabaca7a3b6534535f9702059c14a0a1bbfb
Andreas Tolfsen <ato@mozilla.com>
2016-02-26 14:36:39 +0000

    Bug 1123506 - Evaluate scripts in content with lasting side-effects; r=automatedtester a=test-only
    In order to achieve WebDriver parity, Marionette needs the ability to
    evaluate scripts in content space with lasting side-effects.  This means
    that state modifications should affect behaviour and state of the browsing
    context, and such transgress the boundaries of the sandbox.
    
    This patch brings a new script evaluation module that is shared between
    code in chrome- and content space.  This brings the number of unique
    script evaluation implementations in Marionette down from six to one.
    
    evaluate.sandbox provides the main entry-point for execution.  It is
    compatible with existing Marionette uses of Execute Script and Execute
    Async Script commands in Mozilla clients, but also provides a new stateful
    sandbox for evaluation that should have lasting side-effects.
    
    It is not expected that Mozilla clients, such as testing/marionette/client
    and the Node.js client in Gaia, should have to change as a consequence
    of this change.
    
    A substantial change to the script's runtime environment is that many
    globals that previously existed are now only exposed whenever needed.
    This means for example that Simple Test harness functionality (waitFor,
    ok, isnot, is, &c.) is only available when using a sandbox augmented
    with a Simple Test harness adapter.
    
    Conversely, this patch does not expose marionetteScriptFinished as a
    callback to asynchronous scripts for sandboxes which sandboxName parameter
    is undefined, because this is what determines if the script should be
    evaluated under WebDriver conformance constraints.  In all other cases
    where sandboxName _is_ defined, the traditional marionetteScriptFinished
    et al. runtime environment is preserved.
    
    MozReview-Commit-ID: 8FZ6rNVImuC

--- testing/marionette/emulator.js
+++ testing/marionette/emulator.js
@@ -6,106 +6,161 @@
 
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 
+Cu.import("chrome://marionette/content/error.js");
+
 Cu.import("resource://gre/modules/Log.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Task.jsm");
 
 const logger = Log.repository.getLogger("Marionette");
 
-this.EXPORTED_SYMBOLS = ["Emulator"];
+this.EXPORTED_SYMBOLS = ["emulator"];
+
+this.emulator = {};
 
 /**
- * Represents the connection between Marionette and the emulator it's
- * running on.
- *
- * When injected scripts call the JS routines {@code runEmulatorCmd} or
- * {@code runEmulatorShell}, the second argument to those is a callback
- * which is stored in cbs.  They are later retreived by their unique ID
- * using popCallback.
- *
- * @param {function(Object)} sendToEmulatorFn
- *     Callback function that sends a message to the emulator.
- * @param {function(Object)} sendToEmulatorFn
- *     Callback function that sends a message asynchronously to the
- *     current listener.
+ * Provides a service for instructing the emulator attached to the
+ * currently connected client to perform shell- and command instructions.
  */
-this.Emulator = function(sendToEmulatorFn) {
-  this.sendToEmulator = sendToEmulatorFn;
-};
+emulator.EmulatorService = class {
+  /*
+   * @param {function(Object)} sendToEmulatorFn
+   *     Callback function that sends a message to the emulator.
+   */
+  constructor(sendToEmulatorFn) {
+    this.sendToEmulator = sendToEmulatorFn;
+  }
 
-/**
- * Instruct the client to run an Android emulator command.
- *
- * @param {string} cmd
- *     The command to run.
- * @param {function(?)} resCb
- *     Callback on a result response from the emulator.
- * @param {function(?)} errCb
- *     Callback on an error in running the command.
- */
-Emulator.prototype.command = function(cmd, resCb, errCb) {
-  assertDefined(cmd, "runEmulatorCmd");
-  this.sendToEmulator(
-      "runEmulatorCmd", {emulator_cmd: cmd}, resCb, errCb);
-};
+  /**
+   * Instruct the client to run an Android emulator command.
+   *
+   * @param {string} cmd
+   *     The command to run.
+   * @param {function(?)} resCb
+   *     Callback on a result response from the emulator.
+   * @param {function(?)} errCb
+   *     Callback on an error in running the command.
+   */
+  command(cmd, resCb, errCb) {
+    if (arguments.length < 1) {
+      throw new ValueError("Not enough arguments");
+    }
+    this.sendToEmulator(
+        "runEmulatorCmd", {emulator_cmd: cmd}, resCb, errCb);
+  }
 
-/**
- * Instruct the client to execute Android emulator shell arguments.
- *
- * @param {Array.<string>} args
- *     The shell instruction for the emulator to execute.
- * @param {function(?)} resCb
- *     Callback on a result response from the emulator.
- * @param {function(?)} errCb
- *     Callback on an error in executing the shell arguments.
- */
-Emulator.prototype.shell = function(args, resCb, errCb) {
-  assertDefined(args, "runEmulatorShell");
-  this.sendToEmulator(
-      "runEmulatorShell", {emulator_shell: args}, resCb, errCb);
-};
+  /**
+   * Instruct the client to execute Android emulator shell arguments.
+   *
+   * @param {Array.<string>} args
+   *     The shell instruction for the emulator to execute.
+   * @param {function(?)} resCb
+   *     Callback on a result response from the emulator.
+   * @param {function(?)} errCb
+   *     Callback on an error in executing the shell arguments.
+   */
+  shell(args, resCb, errCb) {
+    if (arguments.length < 1) {
+      throw new ValueError("Not enough arguments");
+    }
+    this.sendToEmulator(
+        "runEmulatorShell", {emulator_shell: args}, resCb, errCb);
+  }
 
-Emulator.prototype.processMessage = function(msg) {
-  let resCb = this.resultCallback(msg.json.id);
-  let errCb = this.errorCallback(msg.json.id);
+  processMessage(msg) {
+    let resCb = this.resultCallback(msg.json.id);
+    let errCb = this.errorCallback(msg.json.id);
 
-  switch (msg.name) {
-    case "Marionette:runEmulatorCmd":
-      this.command(msg.json.command, resCb, errCb);
-      break;
+    switch (msg.name) {
+      case "Marionette:runEmulatorCmd":
+        this.command(msg.json.arguments, resCb, errCb);
+        break;
 
-    case "Marionette:runEmulatorShell":
-      this.shell(msg.json.arguments, resCb, errCb);
-      break;
+      case "Marionette:runEmulatorShell":
+        this.shell(msg.json.arguments, resCb, errCb);
+        break;
+    }
+  }
+
+  resultCallback(uuid) {
+    return res => this.sendResult({value: res, id: uuid});
+  }
+
+  errorCallback(uuid) {
+    return err => this.sendResult({error: err, id: uuid});
+  }
+
+  sendResult(msg) {
+    // sendToListener set explicitly in GeckoDriver's ctor
+    this.sendToListener("listenerResponse", msg);
+  }
+
+  /** Receives IPC messages from the listener. */
+  // TODO(ato): The idea of services in chrome space
+  // can be generalised at some later time.
+  receiveMessage(msg) {
+    let uuid = msg.json.id;
+    try {
+      this.processMessage(msg);
+    } catch (e) {
+      this.sendResult({error: `${e.name}: ${e.message}`, id: uuid});
+    }
   }
 };
 
-Emulator.prototype.resultCallback = function(msgId) {
-  return res => this.sendResult({result: res, id: msgId});
-};
+emulator.EmulatorService.prototype.QueryInterface =
+    XPCOMUtils.generateQI([
+      Ci.nsIMessageListener,
+      Ci.nsISupportsWeakReference
+    ]);
 
-Emulator.prototype.errorCallback = function(msgId) {
-  return err => this.sendResult({error: err, id: msgId});
-};
+emulator.EmulatorServiceClient = class {
+  constructor(chromeProxy) {
+    this.chrome = chromeProxy;
+  }
 
-Emulator.prototype.sendResult = function(msg) {
-  // sendToListener set explicitly in GeckoDriver's ctor
-  this.sendToListener("emulatorCmdResult", msg);
-};
+  *command(cmd, cb) {
+    let res = yield this.chrome.runEmulatorCmd(cmd);
+    if (cb) {
+      cb(res);
+    }
+  }
 
-/** Receives IPC messages from the listener. */
-Emulator.prototype.receiveMessage = function(msg) {
-  try {
-    this.processMessage(msg);
-  } catch (e) {
-    this.sendResult({error: `${e.name}: ${e.message}`, id: msg.json.id});
+  *shell(args, cb) {
+    let res = yield this.chrome.runEmulatorShell(args);
+    if (cb) {
+      cb(res);
+    }
   }
 };
 
-Emulator.prototype.QueryInterface = XPCOMUtils.generateQI(
-    [Ci.nsIMessageListener, Ci.nsISupportsWeakReference]);
-
-function assertDefined(arg, action) {
-  if (typeof arg == "undefined") {
-    throw new TypeError("Not enough arguments to " + action);
+/**
+ * Adapts EmulatorService for use with sandboxes that scripts are
+ * evaluated in.  Is consumed by sandbox.augment.
+ */
+emulator.Adapter = class {
+  constructor(emulator) {
+    this.emulator = emulator;
   }
-}
+
+  get exports() {
+    return new Map([
+      ["runEmulatorCmd", this.runEmulatorCmd.bind(this)],
+      ["runEmulatorShell", this.runEmulatorShell.bind(this)],
+    ]);
+  }
+
+  runEmulatorCmd(cmd, cb) {
+    this.yield(this.emulator.command(cmd, cb));
+  }
+
+  runEmulatorShell(args, cb) {
+    this.yield(this.emulator.shell(args, cb));
+  }
+
+  yield(promise) {
+    Task.spawn(function() {
+      yield promise;
+    });
+  }
+};
