338aa50bfa17db45602dca01b5756af841699af4
Markus Stange <mstange@themasta.com>
2016-02-26 13:38:12 +0100

    Bug 1238564 - Allow constructing nsDisplayWrapList with a given scroll clip. r=roc a=lizzard
    MozReview-Commit-ID: 59FqPndU9mr

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -1321,6 +1321,8 @@ public:
   // This is never instantiated directly (it has pure virtual methods), so no
   // need to count constructors and destructors.
   nsDisplayItem(nsDisplayListBuilder* aBuilder, nsIFrame* aFrame);
+  nsDisplayItem(nsDisplayListBuilder* aBuilder, nsIFrame* aFrame,
+                const DisplayItemScrollClip* aScrollClip);
   /**
    * This constructor is only used in rare cases when we need to construct
    * temporary items.
@@ -3187,6 +3189,9 @@ public:
    */
   nsDisplayWrapList(nsDisplayListBuilder* aBuilder, nsIFrame* aFrame,
                     nsDisplayList* aList);
+  nsDisplayWrapList(nsDisplayListBuilder* aBuilder, nsIFrame* aFrame,
+                    nsDisplayList* aList,
+                    const DisplayItemScrollClip* aScrollClip);
   nsDisplayWrapList(nsDisplayListBuilder* aBuilder, nsIFrame* aFrame,
                     nsDisplayItem* aItem);
   nsDisplayWrapList(nsDisplayListBuilder* aBuilder, nsIFrame* aFrame)
