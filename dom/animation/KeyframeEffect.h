b3828100fd47e35170a3b1a979251da9d8158993
Ryo Motozawa <motozawa@mozilla-japan.org>
2016-02-27 06:39:30 +0900

    Bug 1244641 - Part 1: Let AnimationEffectTiming have an effect. r=hiro
    MozReview-Commit-ID: REfKyGs4HK

--- dom/animation/KeyframeEffect.h
+++ dom/animation/KeyframeEffect.h
@@ -19,7 +19,6 @@
 #include "mozilla/StyleAnimationValue.h"
 #include "mozilla/TimeStamp.h"
 #include "mozilla/dom/AnimationEffectReadOnly.h"
-#include "mozilla/dom/AnimationEffectTiming.h"
 #include "mozilla/dom/AnimationEffectTimingReadOnly.h" // TimingParams
 #include "mozilla/dom/Element.h"
 #include "mozilla/dom/KeyframeBinding.h"
@@ -428,6 +427,9 @@ public:
     return ConstructKeyframeEffect<KeyframeEffect>(aGlobal, aTarget, aFrames,
                                                    aTiming, aRv);
   }
+
+protected:
+  ~KeyframeEffect() override;
 };
 
 } // namespace dom
