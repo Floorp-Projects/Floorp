c279cb263c5b66ce9ac1257b069d3d30629d3dc7
Phil Ringnalda <philringnalda@gmail.com>
2016-02-25 19:15:16 -0800

    Back out 2 changesets (bug 1245992) for several browser-chrome failures
    Backed out changeset 6d9614f87daf (bug 1245992)
    Backed out changeset 34e6eea401e3 (bug 1245992)

--- browser/base/content/browser-safebrowsing.js
+++ browser/base/content/browser-safebrowsing.js
@@ -15,7 +15,7 @@ var gSafeBrowsing = {
     // will point to the internal error page we loaded instead.
     var docURI = gBrowser.selectedBrowser.documentURI;
     var isPhishingPage =
-      docURI && docURI.spec.startsWith("about:blocked?e=deceptiveBlocked");
+      docURI && docURI.spec.startsWith("about:blocked?e=phishingBlocked");
 
     // Show/hide the appropriate menu item.
     document.getElementById("menu_HelpPopup_reportPhishingtoolmenu")
