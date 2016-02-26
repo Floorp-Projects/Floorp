b700af6eddae924c68e0b3332f64426283b17583
Bogdan Postelnicu <bogdan.postelnicu@softvision.ro>
2016-02-26 10:30:12 +0200

    Bug 1234540 - verify the validity of hashEntry, if it's null return with NS_ERROR_FAILURE. r=michal

--- netwerk/cache/nsCacheEntry.cpp
+++ netwerk/cache/nsCacheEntry.cpp
@@ -436,6 +436,9 @@ nsCacheEntryHashTable::AddEntry( nsCacheEntry *cacheEntry)
     if (!cacheEntry)   return NS_ERROR_NULL_POINTER;
 
     hashEntry = table.Add(&(cacheEntry->mKey), fallible);
+
+    if (!hashEntry)
+        return NS_ERROR_FAILURE;
 #ifndef DEBUG_dougt
     NS_ASSERTION(((nsCacheEntryHashTableEntry *)hashEntry)->cacheEntry == 0,
                  "### nsCacheEntryHashTable::AddEntry - entry already used");
