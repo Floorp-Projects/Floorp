8cfae2d7579b77b8ac61a460341cc4a21fb19f99
Tom Tromey <tom@tromey.com>
2016-03-01 11:13:41 -0700

    Bug 1244227 - Add an API to enable throttling. r=Honza
    MozReview-Commit-ID: BirjFHVSZN7

---
+++ devtools/client/netmonitor/test/browser_net_throttle.js
@@ -0,0 +1,60 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Network throttling integration test.
+
+"use strict";
+
+add_task(function* () {
+  yield throttleTest(true);
+  yield throttleTest(false);
+});
+
+function* throttleTest(actuallyThrottle) {
+  requestLongerTimeout(2);
+
+  let [, , monitor] = yield initNetMonitor(SIMPLE_URL);
+  const {ACTIVITY_TYPE, NetMonitorController, NetMonitorView} =
+        monitor.panelWin;
+
+  info("Starting test... (actuallyThrottle = " + actuallyThrottle + ")");
+
+  // When throttling, must be smaller than the length of the content
+  // of SIMPLE_URL in bytes.
+  const size = actuallyThrottle ? 200 : 0;
+
+  const request = {
+    "NetworkMonitor.throttleData": {
+      roundTripTimeMean: 0,
+      roundTripTimeMax: 0,
+      downloadBPSMean: size,
+      downloadBPSMax: size,
+      uploadBPSMean: 10000,
+      uploadBPSMax: 10000,
+    },
+  };
+  let client = monitor._controller.webConsoleClient;
+
+  info("sending throttle request");
+  let deferred = promise.defer();
+  client.setPreferences(request, response => {
+    deferred.resolve(response);
+  });
+  yield deferred.promise;
+
+  let eventPromise =
+      monitor.panelWin.once(monitor.panelWin.EVENTS.RECEIVED_EVENT_TIMINGS);
+  yield NetMonitorController
+    .triggerActivity(ACTIVITY_TYPE.RELOAD.WITH_CACHE_DISABLED);
+
+  yield eventPromise;
+  let requestItem = NetMonitorView.RequestsMenu.getItemAtIndex(0);
+  const reportedOneSecond = requestItem.attachment.eventTimings.timings.receive > 1000;
+  if (actuallyThrottle) {
+    ok(reportedOneSecond, "download reported as taking more than one second");
+  } else {
+    ok(!reportedOneSecond, "download reported as taking less than one second");
+  }
+
+  yield teardown(monitor);
+}
