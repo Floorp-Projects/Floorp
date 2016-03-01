017bf0f2b6d65b56b4f46aa77d008fca5ba72f03
Ehsan Akhgari <ehsan@mozilla.com>
2016-03-01 11:42:52 -0500

    Bug 1120715 - Part 6: Remove the dom.requestcache.enabled pref; r=bkelly

--- dom/tests/mochitest/fetch/fetch_test_framework.js
+++ dom/tests/mochitest/fetch/fetch_test_framework.js
@@ -7,8 +7,7 @@ function testScript(script) {
   function setupPrefs() {
     return new Promise(function(resolve, reject) {
       SpecialPowers.pushPrefEnv({
-        "set": [["dom.requestcache.enabled", true],
-                ["dom.requestcontext.enabled", true],
+        "set": [["dom.requestcontext.enabled", true],
                 ["dom.serviceWorkers.enabled", true],
                 ["dom.serviceWorkers.testing.enabled", true],
                 ["dom.serviceWorkers.exemptFromPerDomainMax", true]]
