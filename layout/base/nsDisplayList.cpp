5ed62f8c34d1c62c4232dfa183c3b38e86ef235f
Nicholas Nethercote <nnethercote@mozilla.com>
2016-03-01 16:48:31 +1100

    Bug 1252374 - Increase the chunk size of the displayListArena. r=roc.
    It reduces the number of malloc/free calls and may reduce malloc heap
    fragmentation.

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -641,7 +641,7 @@ nsDisplayListBuilder::nsDisplayListBuilder(nsIFrame* aReferenceFrame,
       mAsyncPanZoomEnabled(nsLayoutUtils::AsyncPanZoomEnabled(aReferenceFrame))
 {
   MOZ_COUNT_CTOR(nsDisplayListBuilder);
-  PL_InitArenaPool(&mPool, "displayListArena", 1024,
+  PL_InitArenaPool(&mPool, "displayListArena", 4096,
                    std::max(NS_ALIGNMENT_OF(void*),NS_ALIGNMENT_OF(double))-1);
 
   nsPresContext* pc = aReferenceFrame->PresContext();
