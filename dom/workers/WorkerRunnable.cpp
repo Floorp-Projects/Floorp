8f73921c4fd7a377c69a5e33d491567ccf8f7015
Boris Zbarsky <bzbarsky@mit.edu>
2016-03-01 16:52:26 -0500

    Bug 1072144 part 3.  Hoist the exception reporting out of WorkerRunnable::PostRun into WorkerRunnable::Run and make it unconditional.  r=khuey

--- dom/workers/WorkerRunnable.cpp
+++ dom/workers/WorkerRunnable.cpp
@@ -184,10 +184,6 @@ WorkerRunnable::PostRun(JSContext* aCx, WorkerPrivate* aWorkerPrivate,
   if (mBehavior == WorkerThreadModifyBusyCount) {
     aWorkerPrivate->ModifyBusyCountFromWorker(false);
   }
-
-  if (!aRunResult) {
-    JS_ReportPendingException(aCx);
-  }
 }
 
 // static
@@ -349,6 +345,8 @@ WorkerRunnable::Run()
   }
 
   bool result = WorkerRun(cx, mWorkerPrivate);
+  MOZ_ASSERT_IF(result, !JS_IsExceptionPending(cx));
+  JS_ReportPendingException(cx);
 
   // In the case of CompileScriptRunnnable, WorkerRun above can cause us to
   // lazily create a global, so we construct aes here before calling PostRun.
