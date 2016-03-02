74fbbb6d1126710bd4428cb98f22c1de9c85dc7b
Randall Barker <rbarker@mozilla.com>
2016-03-02 14:13:26 -0800

    Bug 1253426 - When Fennec is brought to the foreground, soft keyboard needs to be restored if it was previously visible r=snorp

--- mobile/android/base/java/org/mozilla/gecko/InputConnectionListener.java
+++ mobile/android/base/java/org/mozilla/gecko/InputConnectionListener.java
@@ -21,5 +21,6 @@ interface InputConnectionListener
     boolean onKeyLongPress(int keyCode, KeyEvent event);
     boolean onKeyMultiple(int keyCode, int repeatCount, KeyEvent event);
     boolean onKeyUp(int keyCode, KeyEvent event);
+    void onWindowVisibilityChanged (int visibility);
     boolean isIMEEnabled();
 }
