f522096a404e9a8e972937083a02285465302707
Felipe Gomes <felipc@gmail.com>
2016-03-02 13:50:55 -0300

    Bug 1250744 - Disable e10s add-ons blocking on testing profiles. r=jmaher

--- testing/mozharness/configs/unittests/mac_unittest.py
+++ testing/mozharness/configs/unittests/mac_unittest.py
@@ -195,6 +195,7 @@ config = {
             'options': ['--suite=reftest',
                         '--setpref=browser.tabs.remote=true',
                         '--setpref=browser.tabs.remote.autostart=true',
+                        '--setpref=extensions.e10sBlocksEnabling=false',
                         '--setpref=layers.async-pan-zoom.enabled=true'],
             'tests': ['tests/reftest/tests/layout/reftests/reftest-sanity/reftest.list']
         },
@@ -202,6 +203,7 @@ config = {
             'options': ['--suite=crashtest',
                         '--setpref=browser.tabs.remote=true',
                         '--setpref=browser.tabs.remote.autostart=true',
+                        '--setpref=extensions.e10sBlocksEnabling=false',
                         '--setpref=layers.async-pan-zoom.enabled=true'],
             'tests': ['tests/reftest/tests/testing/crashtest/crashtests.list']
         },
