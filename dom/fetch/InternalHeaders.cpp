ea3b20fae48a9236384ec03ad5d585fade245472
Ehsan Akhgari <ehsan@mozilla.com>
2016-03-02 18:09:30 -0500

    Bug 1120715 - Part 5: Treat a default cache mode Request with a revalidation header as no-store; r=bkelly

--- dom/fetch/InternalHeaders.cpp
+++ dom/fetch/InternalHeaders.cpp
@@ -174,6 +174,17 @@ InternalHeaders::IsSimpleHeader(const nsACString& aName, const nsACString& aValu
           nsContentUtils::IsAllowedNonCorsContentType(aValue));
 }
 
+// static
+bool
+InternalHeaders::IsRevalidationHeader(const nsACString& aName)
+{
+  return aName.EqualsLiteral("if-modified-since") ||
+         aName.EqualsLiteral("if-none-match") ||
+         aName.EqualsLiteral("if-unmodified-since") ||
+         aName.EqualsLiteral("if-match") ||
+         aName.EqualsLiteral("if-range");
+}
+
 //static
 bool
 InternalHeaders::IsInvalidName(const nsACString& aName, ErrorResult& aRv)
@@ -281,6 +292,18 @@ InternalHeaders::HasOnlySimpleHeaders() const
   }
 
   return true;
+}
+
+bool
+InternalHeaders::HasRevalidationHeaders() const
+{
+  for (uint32_t i = 0; i < mList.Length(); ++i) {
+    if (IsRevalidationHeader(mList[i].mName)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 // static
