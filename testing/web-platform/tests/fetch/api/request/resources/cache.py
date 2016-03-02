827c509d38f9780a0dde177a625809508627a8ed
Ehsan Akhgari <ehsan@mozilla.com>
2016-03-02 18:09:30 -0500

    Bug 1120715 - Part 5: Treat a default cache mode Request with a revalidation header as no-store; r=bkelly

--- testing/web-platform/tests/fetch/api/request/resources/cache.py
+++ testing/web-platform/tests/fetch/api/request/resources/cache.py
@@ -14,19 +14,21 @@ def main(request, response):
     ims = request.headers.get("If-Modified-Since", None)
     pragma = request.headers.get("Pragma", None)
     cache_control = request.headers.get("Cache-Control", None)
+    ignore = "ignore" in request.GET
 
     server_state = request.server.stash.take(token)
     if not server_state:
         server_state = []
     state = dict()
-    if inm:
-        state["If-None-Match"] = inm
-    if ims:
-        state["If-Modified-Since"] = ims
-    if pragma:
-        state["Pragma"] = pragma
-    if cache_control:
-        state["Cache-Control"] = cache_control
+    if not ignore:
+        if inm:
+            state["If-None-Match"] = inm
+        if ims:
+            state["If-Modified-Since"] = ims
+        if pragma:
+            state["Pragma"] = pragma
+        if cache_control:
+            state["Cache-Control"] = cache_control
     server_state.append(state)
     request.server.stash.put(token, server_state)
 
