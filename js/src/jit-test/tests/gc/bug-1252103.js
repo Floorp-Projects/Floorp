7d0bb301c9867c8bd5ad599db3142fec3664402a
Jim Blandy <jimb@mozilla.com>
2016-03-02 16:09:00 -0800

    Bug 1252154: Delay allocation metadata collection for inline typed objects. r=sfink

---
+++ js/src/jit-test/tests/gc/bug-1252103.js
@@ -0,0 +1,19 @@
+// Bug 1252103: Inline typed array objects need delayed metadata collection.
+// Shouldn't crash.
+
+function foo() {
+    enableTrackAllocations();
+    gczeal(2, 10);
+    TO = TypedObject;
+    PointType = new TO.StructType({
+        y: TO.float64,
+        name: TO.string
+    })
+    LineType = new TO.StructType({
+        PointType
+    })
+    function testBasic() new LineType;
+    testBasic();
+}
+evaluate("foo()");
+
