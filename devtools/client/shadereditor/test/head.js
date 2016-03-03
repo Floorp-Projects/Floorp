4db6ed1c942304101b095d64c569a3a1130e36eb
Alexandre Poirot <poirot.alex@gmail.com>
2016-03-03 07:55:36 -0800

    Bug 1245875 - Add missing createCanvas function in webgl-utils to fix mochitest-dt5 failures. r=me

--- devtools/client/shadereditor/test/head.js
+++ devtools/client/shadereditor/test/head.js
@@ -118,7 +118,7 @@ function ifWebGLUnsupported() {
 }
 
 function test() {
-  let generator = isWebGLSupported() ? ifWebGLSupported : ifWebGLUnsupported;
+  let generator = isWebGLSupported(document) ? ifWebGLSupported : ifWebGLUnsupported;
   Task.spawn(generator).then(null, handleError);
 }
 
