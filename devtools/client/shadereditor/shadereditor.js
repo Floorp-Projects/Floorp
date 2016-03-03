c943bbac07402609e9813c64dc4b0d069e62e77d
J. Ryan Stinnett <jryans@gmail.com>
2016-03-02 20:25:37 -0600

    Bug 1253125 - Stop duplicating telemetry from inside tools. r=mratcliffe
    MozReview-Commit-ID: IRidYhLsJpy

--- devtools/client/shadereditor/shadereditor.js
+++ devtools/client/shadereditor/shadereditor.js
@@ -17,8 +17,6 @@ const Services = require("Services");
 const EventEmitter = require("devtools/shared/event-emitter");
 const {Tooltip} = require("devtools/client/shared/widgets/Tooltip");
 const Editor = require("devtools/client/sourceeditor/editor");
-const Telemetry = require("devtools/client/shared/telemetry");
-const telemetry = new Telemetry();
 
 // The panel's window global is an EventEmitter firing the following events:
 const EVENTS = {
@@ -87,7 +85,6 @@ var EventsHandler = {
    * Listen for events emitted by the current tab target.
    */
   initialize: function() {
-    telemetry.toolOpened("shadereditor");
     this._onHostChanged = this._onHostChanged.bind(this);
     this._onTabNavigated = this._onTabNavigated.bind(this);
     this._onProgramLinked = this._onProgramLinked.bind(this);
@@ -104,7 +101,6 @@ var EventsHandler = {
    * Remove events emitted by the current tab target.
    */
   destroy: function() {
-    telemetry.toolClosed("shadereditor");
     gToolbox.off("host-changed", this._onHostChanged);
     gTarget.off("will-navigate", this._onTabNavigated);
     gTarget.off("navigate", this._onTabNavigated);
