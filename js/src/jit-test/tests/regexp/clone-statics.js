6e95ef9161c95d03cb330cb68bccdf9a30f6f7c4
Jeff Walden <jwalden@mit.edu>
2016-03-02 17:52:09 -0800

    Bug 1242810 - Test.  r=jandem

---
+++ js/src/jit-test/tests/regexp/clone-statics.js
@@ -0,0 +1,21 @@
+if (helperThreadCount() === 0)
+  quit(0);
+
+offThreadCompileScript(`
+  function foo(x, {}) {
+    do {
+      re = /erwe/;
+      if (x === 1)
+        re.x = 1;
+      else
+        re.x = "a";
+      assertEq(re.x.length, (x === 1) ? undefined : 1);
+    } while (!inIon());
+  }
+
+  foo(0, 0);
+  RegExp.multiline = 1;
+  foo(1, 0);
+`);
+
+runOffThreadScript();
