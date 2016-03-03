4a93a11cd7e5ec29434700eea4d32b3f56a51d4f
Alexander Surkov <surkov.alexander@gmail.com>
2016-03-03 07:45:47 -0500

    Bug 1251941 - aria::GetRoleMap should take element, r=davidb

--- accessible/base/ARIAMap.h
+++ accessible/base/ARIAMap.h
@@ -210,7 +210,7 @@ extern nsRoleMapEntry gEmptyRoleMap;
  * @return        a pointer to the role map entry for the ARIA role, or nullptr
  *                if none
  */
-nsRoleMapEntry* GetRoleMap(nsINode* aNode);
+nsRoleMapEntry* GetRoleMap(dom::Element* aEl);
 
 /**
  * Return accessible state from ARIA universal states applied to the given
