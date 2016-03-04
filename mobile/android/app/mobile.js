58145966b8ae8d2b69aae8aec84ecaca6c528509
Nick Alexander <nalexander@mozilla.com>
2016-03-04 15:56:36 -0800

    Bug 1252666 - Part 3: Enable DOM Push API in Fennec Nightly. r=margaret
    MozReview-Commit-ID: 19pThtRMN6r

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -956,6 +956,15 @@ pref("browser.tabs.showAudioPlayingIcon", true);
 
 pref("dom.serviceWorkers.enabled", true);
 
+pref("dom.push.debug", false);
+// The upstream autopush endpoint must have the Google API key corresponding to
+// the App's sender ID; we bake this assumption directly into the URL.
+pref("dom.push.serverURL", "https://updates-autopush.stage.mozaws.net/v1/gcm/@MOZ_ANDROID_GCM_SENDERID@");
+
+#ifdef MOZ_ANDROID_GCM
+pref("dom.push.enabled", true);
+#endif
+
 // The remote content URL where FxAccountsWebChannel messages originate.  Must use HTTPS.
 pref("identity.fxaccounts.remote.webchannel.uri", "https://accounts.firefox.com");
 
