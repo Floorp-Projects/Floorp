091e5334685353409ed0fe2b15591045e2145214
Dan Gohman <sunfish@mozilla.com>
2016-03-04 12:36:57 +0100

    Bug 1246116 - BaldrMonkey: Wasm validation for block and loop. r=luke
    MozReview-Commit-ID: KSfRmwPH1Im

--- js/src/asmjs/WasmText.cpp
+++ js/src/asmjs/WasmText.cpp
@@ -3464,7 +3464,7 @@ EncodeExpr(Encoder& e, WasmAstExpr& expr);
 static bool
 EncodeBlock(Encoder& e, WasmAstBlock& b)
 {
-    if (!e.writeExpr(Expr::Block))
+    if (!e.writeExpr(b.expr()))
         return false;
 
     size_t numExprs = b.exprs().length();
