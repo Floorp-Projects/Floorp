cabe8b041e25f3ef5b0285c8a8c1590c1d11934b
Andrea Marchesini <amarchesini@mozilla.com>
2016-03-04 20:38:12 +0100

    Bug 1237807 - Split test_websocket.html - patch 1, r=ehsan

---
+++ dom/base/test/websocket_helpers.js
@@ -0,0 +1,58 @@
+var current_test = 0;
+
+function shouldNotOpen(e) {
+  var ws = e.target;
+  ok(false, "onopen shouldn't be called on test " + ws._testNumber + "!");
+}
+
+function shouldCloseCleanly(e) {
+  var ws = e.target;
+  ok(e.wasClean, "the ws connection in test " + ws._testNumber + " should be closed cleanly");
+}
+
+function shouldCloseNotCleanly(e) {
+  var ws = e.target;
+  ok(!e.wasClean, "the ws connection in test " + ws._testNumber + " shouldn't be closed cleanly");
+}
+
+function CreateTestWS(ws_location, ws_protocol) {
+  var ws;
+
+  try {
+    if (ws_protocol == undefined) {
+      ws = new WebSocket(ws_location);
+    } else {
+      ws = new WebSocket(ws_location, ws_protocol);
+    }
+
+    ws._testNumber = current_test;
+    ws._receivedCloseEvent = false;
+    ok(true, "Created websocket for test " + ws._testNumber +"\n");
+
+    ws.onerror = function(e) {
+      ok(false, "onerror called on test " + e.target._testNumber + "!");
+    }
+
+    ws.addEventListener("close", function(e) {
+      ws._receivedCloseEvent = true;
+    }, false);
+  } catch (e) {
+    throw e;
+  }
+
+  return ws;
+}
+
+function doTest() {
+  if (current_test >= tests.length) {
+    SimpleTest.finish();
+    return;
+  }
+
+  $("feedback").innerHTML = "executing test: " + (current_test+1) + " of " + tests.length + " tests.";
+  tests[current_test++]().then(doTest);
+}
+
+SimpleTest.requestFlakyTimeout("The web socket tests are really fragile, but avoiding timeouts might be hard, since it's testing stuff on the network. " +
+                               "Expect all sorts of flakiness in this test...");
+SimpleTest.waitForExplicitFinish();
