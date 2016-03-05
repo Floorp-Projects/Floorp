bf32983adb16717f58423ba808e6c36c3462f2e3
Luke Wagner <luke@mozilla.com>
2016-03-04 22:31:28 -0600

    Bug 1246116 - Baldr: remove two timeout tests until there is a good way to make them not fail on platforms without wasm (arm64) (r=red)
    MozReview-Commit-ID: JM9Iav7kMpE

--- js/src/jit-test/tests/wasm/timeout-no-signals.js
+++
@@ -1,7 +0,0 @@
-// |jit-test| exitstatus: 6;
-load(libdir + "wasm.js");
-
-setJitCompilerOption("signals.enable", 0);
-timeout(1);
-wasmEvalText('(module (func (loop (br 0))) (export "" 0))')();
-assertEq(true, false);
