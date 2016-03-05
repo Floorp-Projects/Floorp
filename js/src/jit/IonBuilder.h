9603464303f3758bd7701349383762f63d48d9cf
Tooru Fujisawa <arai_a@mac.com>
2016-03-05 18:56:37 +0900

    Bug 1165053 - Part 0.3: Inline ArrayBufferByteLength self-hosting intrinsic. r=jwalden

--- js/src/jit/IonBuilder.h
+++ js/src/jit/IonBuilder.h
@@ -847,6 +847,7 @@ class IonBuilder
     InliningStatus inlineGetNextMapEntryForIterator(CallInfo& callInfo);
 
     // ArrayBuffer intrinsics.
+    InliningStatus inlineArrayBufferByteLength(CallInfo& callInfo);
     InliningStatus inlinePossiblyWrappedArrayBufferByteLength(CallInfo& callInfo);
 
     // TypedArray intrinsics.
