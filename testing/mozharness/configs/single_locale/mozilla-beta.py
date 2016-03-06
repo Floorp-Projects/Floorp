e483020823549ed2f1bafcec209434c39aff40e9
Rail Aliiev <rail@mozilla.com>
2016-03-05 19:38:08 -0500

    Bug 1253749 - Branch specifics for relpro on beta r=jlund a=testing DONTBUILD
    MozReview-Commit-ID: WK1XuVHw2n
    
    --HG--
    extra : source : d4bbcd3b6e7ed5e36500dc3a114765e60327c2e7

---
+++ testing/mozharness/configs/single_locale/mozilla-beta.py
@@ -0,0 +1,44 @@
+config = {
+    "nightly_build": True,
+    "branch": "mozilla-beta",
+    "en_us_binary_url": "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-beta/",
+    "update_channel": "beta",
+    "latest_mar_dir": '/pub/mozilla.org/firefox/nightly/latest-mozilla-beta-l10n',
+
+    # l10n
+    "hg_l10n_base": "https://hg.mozilla.org/releases/l10n/mozilla-beta",
+
+    # mar
+    # No nightly on beta, using aurora mar tools
+    "mar_tools_url": "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/mar-tools/%(platform)s",
+
+    # repositories
+    "mozilla_dir": "mozilla-beta",
+    "repos": [{
+        "vcs": "hg",
+        "repo": "https://hg.mozilla.org/build/tools",
+        "revision": "default",
+        "dest": "tools",
+    }, {
+        "vcs": "hgtool",
+        "repo": "https://hg.mozilla.org/releases/mozilla-beta",
+        "revision": "default",
+        "dest": "mozilla-beta",
+    }, {
+        "vcs": "hgtool",
+        "repo": "https://hg.mozilla.org/build/compare-locales",
+        "revision": "RELEASE_AUTOMATION"
+    }],
+    # purge options
+    'purge_minsize': 12,
+    'is_automation': True,
+    'default_actions': [
+        "clobber",
+        "pull",
+        "list-locales",
+        "setup",
+        "repack",
+        "taskcluster-upload",
+        "summary",
+    ],
+}
