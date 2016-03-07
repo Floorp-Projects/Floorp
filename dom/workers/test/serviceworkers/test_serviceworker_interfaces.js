6015855de568ee8b053f905c5a2f305124a9b7d7
Nick Alexander <nalexander@mozilla.com>
2016-03-07 15:14:06 -0800

    Bug 1252666 - Part 1: Mark Push* exposed in Fennec Nightly. r=ehsan
    MozReview-Commit-ID: GM2YxeAZuVJ

--- dom/workers/test/serviceworkers/test_serviceworker_interfaces.js
+++ dom/workers/test/serviceworkers/test_serviceworker_interfaces.js
@@ -173,13 +173,13 @@ var interfaceNamesInGlobalScope =
 // IMPORTANT: Do not change this list without review from a DOM peer!
     "Promise",
 // IMPORTANT: Do not change this list without review from a DOM peer!
-    { name: "PushEvent", b2g: false, android: false },
+    { name: "PushEvent", b2g: false, android: false, nightlyAndroid: true },
 // IMPORTANT: Do not change this list without review from a DOM peer!
-    { name: "PushManager", b2g: false, android: false },
+    { name: "PushManager", b2g: false, android: false, nightlyAndroid: true },
 // IMPORTANT: Do not change this list without review from a DOM peer!
-    { name: "PushMessageData", b2g: false, android: false },
+    { name: "PushMessageData", b2g: false, android: false, nightlyAndroid: true },
 // IMPORTANT: Do not change this list without review from a DOM peer!
-    { name: "PushSubscription", b2g: false, android: false },
+    { name: "PushSubscription", b2g: false, android: false, nightlyAndroid: true },
 // IMPORTANT: Do not change this list without review from a DOM peer!
     "Request",
 // IMPORTANT: Do not change this list without review from a DOM peer!
@@ -228,10 +228,11 @@ function createInterfaceMap(permissionMap, version, userAgent, isB2G) {
       } else {
         ok(!("pref" in entry), "Bogus pref annotation for " + entry.name);
         if ((entry.nightly === !isNightly) ||
+            (entry.nightlyAndroid === !(isAndroid && isNightly) && isAndroid) ||
             (entry.nonReleaseB2G === !(isB2G && !isRelease) && isB2G) ||
             (entry.nonReleaseAndroid === !(isAndroid && !isRelease) && isAndroid) ||
             (entry.desktop === !isDesktop) ||
-            (entry.android === !isAndroid && !entry.nonReleaseAndroid) ||
+            (entry.android === !isAndroid && !entry.nonReleaseAndroid && !entry.nightlyAndroid) ||
             (entry.b2g === !isB2G && !entry.nonReleaseB2G) ||
             (entry.release === !isRelease) ||
             (entry.permission && !permissionMap[entry.permission])) {
