5253a1b8e61a98a3c895cfc83e1a42812f98b825
Myk Melez <myk@mykzilla.org>
2016-03-07 13:32:26 -0800

    Bug 1238079 - remove the desktop web runtime; r=fabrice,mossop,gps,jryans,jmaher,marco

--- testing/mochitest/mach_commands.py
+++ testing/mochitest/mach_commands.py
@@ -125,22 +125,6 @@ ALL_FLAVORS = {
             'a11y': True,
         }
     },
-    'webapprt-chrome': {
-        'suite': 'webapprt-chrome',
-        'aliases': ('webapprt-chrome', 'mochitest-webapprt-chrome'),
-        'enabled_apps': ('firefox',),
-        'extra_args': {
-            'webapprtChrome': True,
-        }
-    },
-    'webapprt-content': {
-        'suite': 'webapprt-content',
-        'aliases': ('webapprt-content', 'mochitest-webapprt-content'),
-        'enabled_apps': ('firefox',),
-        'extra_args': {
-            'webapprtContent': True,
-        }
-    },
 }
 
 SUPPORTED_APPS = ['firefox', 'b2g', 'android', 'mulet']
@@ -155,44 +139,6 @@ class MochitestRunner(MozbuildObject):
     This currently contains just the basics for running mochitests. We may want
     to hook up result parsing, etc.
     """
-
-    def get_webapp_runtime_path(self):
-        import mozinfo
-        app_name = 'webapprt-stub' + mozinfo.info.get('bin_suffix', '')
-        app_path = os.path.join(self.distdir, 'bin', app_name)
-        if sys.platform.startswith('darwin'):
-            # On Mac, we copy the stub from the dist dir to the test app bundle,
-            # since we have to run it from a bundle for its windows to appear.
-            # Ideally, the build system would do this for us, and we should find
-            # a way for it to do that.
-            mac_dir_name = os.path.join(
-                self.mochitest_dir,
-                'webapprtChrome',
-                'webapprt',
-                'test',
-                'chrome',
-                'TestApp.app',
-                'Contents',
-                'MacOS')
-            mac_app_name = 'webapprt' + mozinfo.info.get('bin_suffix', '')
-            mac_app_path = os.path.join(mac_dir_name, mac_app_name)
-            shutil.copy(app_path, mac_app_path)
-            return mac_app_path
-        return app_path
-
-    # On Mac, the app invoked by runtests.py is in a different app bundle
-    # (as determined by get_webapp_runtime_path above), but the XRE path should
-    # still point to the browser's app bundle, so we set it here explicitly.
-    def get_webapp_runtime_xre_path(self):
-        if sys.platform.startswith('darwin'):
-            xre_path = os.path.join(
-                self.distdir,
-                self.substs['MOZ_MACBUNDLE_NAME'],
-                'Contents',
-                'Resources')
-        else:
-            xre_path = os.path.join(self.distdir, 'bin')
-        return xre_path
 
     def __init__(self, *args, **kwargs):
         MozbuildObject.__init__(self, *args, **kwargs)
@@ -257,8 +203,7 @@ class MochitestRunner(MozbuildObject):
         """Runs a mochitest.
 
         suite is the type of mochitest to run. It can be one of ('plain',
-        'chrome', 'browser', 'a11y', 'jetpack-package', 'jetpack-addon',
-        'webapprt-chrome', 'webapprt-content').
+        'chrome', 'browser', 'a11y', 'jetpack-package', 'jetpack-addon').
         """
         # runtests.py is ambiguous, so we load the file/module manually.
         if 'mochitest' not in sys.modules:
@@ -281,16 +226,6 @@ class MochitestRunner(MozbuildObject):
             logging.getLogger().removeHandler(handler)
 
         options = Namespace(**kwargs)
-
-        if suite == 'webapprt-content':
-            if not options.app or options.app == self.get_binary_path():
-                options.app = self.get_webapp_runtime_path()
-            options.xrePath = self.get_webapp_runtime_xre_path()
-        elif suite == 'webapprt-chrome':
-            options.browserArgs.append("-test-mode")
-            if not options.app or options.app == self.get_binary_path():
-                options.app = self.get_webapp_runtime_path()
-            options.xrePath = self.get_webapp_runtime_xre_path()
 
         from manifestparser import TestManifest
         if tests:
@@ -665,12 +600,4 @@ class DeprecatedCommands(MachCommandBase):
 
     @Command('jetpack-package', category='testing', conditions=[REMOVED])
     def jetpack_package(self):
-        pass
-
-    @Command('webapprt-test-chrome', category='testing', conditions=[REMOVED])
-    def webapprt_chrome(self):
-        pass
-
-    @Command('webapprt-test-content', category='testing', conditions=[REMOVED])
-    def webapprt_content(self):
         pass
