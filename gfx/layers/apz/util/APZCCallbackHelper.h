7a65557850fc1f47159461a1293cce3fecf406e4
Kartikaya Gupta <kgupta@mozilla.com>
2016-03-07 16:27:44 -0500

    Bug 1192910 - Ensure we flush paints on the main thread during an APZ flush. r=mstange. reland because backout didn't fix the issue
    MozReview-Commit-ID: 61dyeqxxtVP

--- gfx/layers/apz/util/APZCCallbackHelper.h
+++ gfx/layers/apz/util/APZCCallbackHelper.h
@@ -161,7 +161,7 @@ public:
     static void NotifyMozMouseScrollEvent(const FrameMetrics::ViewID& aScrollId, const nsString& aEvent);
 
     /* Notify content that the repaint flush is complete. */
-    static void NotifyFlushComplete();
+    static void NotifyFlushComplete(nsIPresShell* aShell);
 
     /* Temporarily ignore the Displayport for better paint performance. */
     static void SuppressDisplayport(const bool& aEnabled);
