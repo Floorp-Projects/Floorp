0215f5f4345f8467cda3e856f33435c378b6b441
Aaron Klotz <aklotz@mozilla.com>
2016-03-07 11:26:35 -0800

    Bug 1236108 - Add temp directory for sandboxed content processes to directory service. r=bsmedberg, a=lizzard
    This is needed so that chrome processes know where sandboxed content
    processes will be writing their temp files, and so that content processes know
    where to write.
    
    MozReview-Commit-ID: BK9bTxFGvZO
    
    --HG--
    extra : source : a6ad577af025e503a4f0fb96345264b92018acee

--- toolkit/xre/nsXREDirProvider.h
+++ toolkit/xre/nsXREDirProvider.h
@@ -121,6 +121,11 @@ protected:
   // delimiters.
   static inline nsresult AppendProfileString(nsIFile* aFile, const char* aPath);
 
+#if (defined(XP_WIN) || defined(XP_MACOSX)) && defined(MOZ_CONTENT_SANDBOX)
+  // Load the temp directory for sandboxed content processes
+  nsresult LoadContentProcessTempDir();
+#endif
+
   // Calculate and register extension and theme bundle directories.
   void LoadExtensionBundleDirectories();
 
@@ -141,6 +146,9 @@ protected:
   nsCOMPtr<nsIFile>      mProfileDir;
   nsCOMPtr<nsIFile>      mProfileLocalDir;
   bool                   mProfileNotified;
+#if (defined(XP_WIN) || defined(XP_MACOSX)) && defined(MOZ_CONTENT_SANDBOX)
+  nsCOMPtr<nsIFile>      mContentTempDir;
+#endif
   nsCOMArray<nsIFile>    mAppBundleDirectories;
   nsCOMArray<nsIFile>    mExtensionDirectories;
   nsCOMArray<nsIFile>    mThemeDirectories;
