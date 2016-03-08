0e6e8d20006e3582b321d7091b351accdad4585d
Luke Wagner <luke@mozilla.com>
2016-03-08 13:15:09 -0600

    Bug 1253137 - Baldr: fold if_else into if to match ml-proto (r=sunfish)
    MozReview-Commit-ID: JdGRFVzGWbN

--- js/src/jit-test/tests/wasm/basic-control-flow.js
+++ js/src/jit-test/tests/wasm/basic-control-flow.js
@@ -1,34 +1,34 @@
 load(libdir + "wasm.js");
 
 // ----------------------------------------------------------------------------
-// if_else
+// if
 
 // Condition is an int32
 assertErrorMessage(() => wasmEvalText('(module (func (local f32) (if (get_local 0) (i32.const 1))))'), TypeError, mismatchError("f32", "i32"));
-assertErrorMessage(() => wasmEvalText('(module (func (local f32) (if_else (get_local 0) (i32.const 1) (i32.const 0))))'), TypeError, mismatchError("f32", "i32"));
-assertErrorMessage(() => wasmEvalText('(module (func (local f64) (if_else (get_local 0) (i32.const 1) (i32.const 0))))'), TypeError, mismatchError("f64", "i32"));
+assertErrorMessage(() => wasmEvalText('(module (func (local f32) (if (get_local 0) (i32.const 1) (i32.const 0))))'), TypeError, mismatchError("f32", "i32"));
+assertErrorMessage(() => wasmEvalText('(module (func (local f64) (if (get_local 0) (i32.const 1) (i32.const 0))))'), TypeError, mismatchError("f64", "i32"));
 wasmEvalText('(module (func (local i32) (if (get_local 0) (nop))) (export "" 0))');
-wasmEvalText('(module (func (local i32) (if_else (get_local 0) (nop) (nop))) (export "" 0))');
+wasmEvalText('(module (func (local i32) (if (get_local 0) (nop) (nop))) (export "" 0))');
 
 // Expression values types are consistent
-assertErrorMessage(() => wasmEvalText('(module (func (result i32) (local f32) (if_else (i32.const 42) (get_local 0) (i32.const 0))))'), TypeError, mismatchError("void", "i32"));
-assertErrorMessage(() => wasmEvalText('(module (func (result i32) (local f64) (if_else (i32.const 42) (i32.const 0) (get_local 0))))'), TypeError, mismatchError("void", "i32"));
-assertEq(wasmEvalText('(module (func (result i32) (if_else (i32.const 42) (i32.const 1) (i32.const 2))) (export "" 0))')(), 1);
-assertEq(wasmEvalText('(module (func (result i32) (if_else (i32.const 0) (i32.const 1) (i32.const 2))) (export "" 0))')(), 2);
+assertErrorMessage(() => wasmEvalText('(module (func (result i32) (local f32) (if (i32.const 42) (get_local 0) (i32.const 0))))'), TypeError, mismatchError("void", "i32"));
+assertErrorMessage(() => wasmEvalText('(module (func (result i32) (local f64) (if (i32.const 42) (i32.const 0) (get_local 0))))'), TypeError, mismatchError("void", "i32"));
+assertEq(wasmEvalText('(module (func (result i32) (if (i32.const 42) (i32.const 1) (i32.const 2))) (export "" 0))')(), 1);
+assertEq(wasmEvalText('(module (func (result i32) (if (i32.const 0) (i32.const 1) (i32.const 2))) (export "" 0))')(), 2);
 
 // If we don't yield, sub expressions types don't have to match
-assertEq(wasmEvalText('(module (func (if_else (i32.const 42) (i32.const 1) (i32.const 0))) (export "" 0))')(), undefined);
-assertEq(wasmEvalText('(module (func (param f32) (if_else (i32.const 42) (i32.const 1) (get_local 0))) (export "" 0))')(13.37), undefined);
+assertEq(wasmEvalText('(module (func (if (i32.const 42) (i32.const 1) (i32.const 0))) (export "" 0))')(), undefined);
+assertEq(wasmEvalText('(module (func (param f32) (if (i32.const 42) (i32.const 1) (get_local 0))) (export "" 0))')(13.37), undefined);
 
 // Sub-expression values are returned
 assertEq(wasmEvalText(`(module
     (func
         (result i32)
-        (if_else
+        (if
             (i32.const 42)
             (block
                 (
-                    if_else
+                    if
                     (block
                         (i32.const 3)
                         (i32.const 5)
@@ -51,7 +51,7 @@ assertEq(wasmEvalText(`(module
     (import "inc" "" (result i32))
     (func
         (result i32)
-        (if_else
+        (if
             (i32.const 42)
             (i32.const 1)
             (call_import 0)
@@ -65,7 +65,7 @@ assertEq(wasmEvalText(`(module
     (import "inc" "" (result i32))
     (func
         (result i32)
-        (if_else
+        (if
             (i32.const 0)
             (call_import 0)
             (i32.const 1)
@@ -99,19 +99,19 @@ assertEq(wasmEvalText(`(module
 )`, imports)(), undefined);
 assertEq(counter, 1);
 
-// One can chain if_else with if/if_else
+// One can chain if with if/if
 counter = 0;
 assertEq(wasmEvalText(`(module
     (import "inc" "" (result i32))
     (func
         (result i32)
-        (if_else
+        (if
             (i32.const 1)
-            (if_else
+            (if
                 (i32.const 2)
-                (if_else
+                (if
                     (i32.const 3)
-                    (if_else
+                    (if
                         (i32.const 0)
                         (call_import 0)
                         (i32.const 42)
@@ -129,8 +129,8 @@ assertEq(counter, 0);
 
 // "if" doesn't return an expression value
 assertErrorMessage(() => wasmEvalText('(module (func (result i32) (if (i32.const 42) (i32.const 0))))'), TypeError, mismatchError("void", "i32"));
-assertErrorMessage(() => wasmEvalText('(module (func (result i32) (if_else (i32.const 1) (i32.const 0) (if (i32.const 1) (i32.const 1)))))'), TypeError, mismatchError("void", "i32"));
-wasmEvalText('(module (func (if_else (i32.const 1) (i32.const 0) (if (i32.const 1) (i32.const 1)))))');
+assertErrorMessage(() => wasmEvalText('(module (func (result i32) (if (i32.const 1) (i32.const 0) (if (i32.const 1) (i32.const 1)))))'), TypeError, mismatchError("void", "i32"));
+wasmEvalText('(module (func (if (i32.const 1) (i32.const 0) (if (i32.const 1) (i32.const 1)))))');
 
 // ----------------------------------------------------------------------------
 // return
@@ -164,7 +164,7 @@ assertErrorMessage(() => wasmEvalText('(module (func (loop (br_if 2 (i32.const 0
 
 assertErrorMessage(() => wasmEvalText(`(module (func (result i32)
   (block
-    (if_else
+    (if
       (br 0)
       (i32.const 0)
       (i32.const 2)
@@ -244,7 +244,7 @@ assertEq(wasmEvalText(`(module (func
 
 assertEq(wasmEvalText(`(module (func (result i32)
   (block
-    (if_else
+    (if
       (i32.const 1)
       (br 0)
       (return (i32.const 0))
