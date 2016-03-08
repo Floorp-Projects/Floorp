2b31b34d352e7f2fce55940b435d541c82ff3a2a
Olivier Yiptong <olivier@olivieryiptong.com>
2016-03-08 14:20:51 -0500

    Bug 1239119 - BackgroundPageThumbs captureIfMissing promise resolves when task is truly done r=ursula
    MozReview-Commit-ID: LmN9Phdrspv

--- toolkit/components/thumbnails/test/browser_thumbnails_bg_basic.js
+++ toolkit/components/thumbnails/test/browser_thumbnails_bg_basic.js
@@ -5,8 +5,15 @@ function* runTests() {
   let url = "http://www.example.com/";
   ok(!thumbnailExists(url), "Thumbnail should not be cached yet.");
 
+  let capturePromise = new Promise(resolve => {
+    bgAddPageThumbObserver(url).then(() => {
+      ok(true, `page-thumbnail created for ${url}`);
+      resolve();
+    });
+  });
   let capturedURL = yield bgCapture(url);
   is(capturedURL, url, "Captured URL should be URL passed to capture");
+  yield capturePromise;
 
   ok(thumbnailExists(url), "Thumbnail should be cached after capture");
   removeThumbnail(url);
