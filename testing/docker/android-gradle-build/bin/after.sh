8729d54d762fbde787940b7110d07a91fd91f2d8
Nick Alexander <nalexander@mozilla.com>
2016-03-08 11:42:56 -0800

    Bug 1252928 - Part 2: Turn android-gradle-build into a desktop-build clone. r=dustin
    This container formerly defined a special Gradle project for fetching
    dependencies.  This patch lays the ground-work to use the in-tree
    Gradle project instead.  Using the in-tree project looks like first
    starting a local Nexus repository to collect downloaded dependencies
    (before.sh); then running a build which populates that repository; and
    then packaging up the downloaded dependencies (after.sh).  The patch
    after this will define the build which populates the repository.
    
    Sadly there's no easy way to *inherit* from desktop-build, so this is
    a copy-paste-modify clone.
    
    MozReview-Commit-ID: Dd5Hj8hkJVk

--- testing/docker/android-gradle-build/bin/after.sh
+++ testing/docker/android-gradle-build/bin/after.sh
@@ -7,23 +7,6 @@ set -x -e
 
 set -v
 
-# Frowned upon, but simplest.
-RUN_AS_USER=root NEXUS_WORK=${WORKSPACE}/nexus /opt/sonatype/nexus/bin/nexus start
-
-# Wait "a while" for Nexus to actually start.  Don't fail if this fails.
-wget --quiet --retry-connrefused --waitretry=2 --tries=100 \
-  http://localhost:8081/nexus/service/local/status || true
-rm -rf status
-
-# Verify Nexus has actually started.  Fail if this fails.
-curl --fail --silent --location http://localhost:8081/nexus/service/local/status | grep '<state>STARTED</state>'
-
-export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk
-
-pushd /project
-./gradlew tasks
-popd
-
 # Package everything up.
 pushd ${WORKSPACE}
 # Not yet.  See notes on tooltool below.
@@ -34,26 +17,26 @@ cp -R /workspace/nexus/storage/central jcentral
 tar cJf jcentral.tar.xz jcentral
 
 # The Gradle wrapper will have downloaded and verified the hash of exactly one
-# Gradle distribution.  It will be located at
+# Gradle distribution.  It will be located in $GRADLE_USER_HOME, like
 # ~/.gradle/wrapper/dists/gradle-2.7-all/$PROJECT_HASH/gradle-2.7-all.zip.  We
-# want to remove the version from the internal directory for use via tooltool
-# in a mozconfig.
-cp ~/.gradle/wrapper/dists/gradle-${GRADLE_VERSION}-all/*/gradle-${GRADLE_VERSION}-all.zip gradle-${GRADLE_VERSION}-all.zip
+# want to remove the version from the internal directory for use via tooltool in
+# a mozconfig.
+cp $GRADLE_USER_HOME/wrapper/dists/gradle-${GRADLE_VERSION}-all/*/gradle-${GRADLE_VERSION}-all.zip gradle-${GRADLE_VERSION}-all.zip
 unzip -q gradle-${GRADLE_VERSION}-all.zip
 mv gradle-${GRADLE_VERSION} gradle
 tar cJf gradle.tar.xz gradle
 
-mkdir -p /artifacts
+mkdir -p /home/worker/artifacts
 # We can't redistribute the Android SDK publicly just yet.  We'll
 # upload to (internal) tooltool eventually.  mv
-# android-sdk-linux.tar.xz /artifacts
-mv jcentral.tar.xz /artifacts
-mv gradle.tar.xz /artifacts
+# android-sdk-linux.tar.xz /home/worker/artifacts
+mv jcentral.tar.xz /home/worker/artifacts
+mv gradle.tar.xz /home/worker/artifacts
 popd
 
 # Bug 1245170: at some point in the future, we'll be able to upload
 # things directly to tooltool.
-# pushd /artifacts
+# pushd /home/worker/artifacts
 # /build/tooltool.py add --visibility=public jcentral.tar.xz
 # /build/tooltool.py add --visibility=public gradle.tar.xz
 # /build/tooltool.py add --visibility=internal android-sdk-linux.tar.xz
