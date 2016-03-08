7186dc89653a2e3733c9c9839606f0345249f250
Mason Chang <mchang@mozilla.com>
2016-03-08 07:37:16 -0800

    Bug 1249600 - Lookup font and font family from font face when requesting SkTypeface. r=bas r=ritu

--- gfx/2d/ScaledFontDWrite.h
+++ gfx/2d/ScaledFontDWrite.h
@@ -46,8 +46,10 @@ public:
 
 #ifdef USE_SKIA
   virtual SkTypeface* GetSkTypeface();
+  void GetFontDataFromSystemFonts(IDWriteFactory* aFactory);
 #endif
 
+  // The font and font family are only used with Skia
   RefPtr<IDWriteFont> mFont;
   RefPtr<IDWriteFontFamily> mFontFamily;
   RefPtr<IDWriteFontFace> mFontFace;
