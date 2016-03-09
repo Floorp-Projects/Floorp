1f3e9d9ff9d627a3556adff03a8fa48423dd15ea
Neil Deakin <neil@mozilla.com>
2016-03-09 09:11:46 -0500

    Bug 1246654, enable browser_bug1008941_dismissGeolocationHanger.js in e10s, r=mconley

--- dom/tests/browser/browser_bug1008941_dismissGeolocationHanger.js
+++ dom/tests/browser/browser_bug1008941_dismissGeolocationHanger.js
@@ -10,50 +10,19 @@ const TEST_URI = "http://example.com/" +
 add_task(function testDismissHanger() {
   info("Check that location is not shared when dismissing the geolocation hanger");
 
-  let promisePanelShown = waitForPanelShow();
-
-  gBrowser.selectedTab = gBrowser.addTab(TEST_URI);
-  yield waitForPageLoad(gBrowser.selectedTab);
-  info("Page was loaded");
-
+  let promisePanelShown = BrowserTestUtils.waitForEvent(PopupNotifications.panel, "popupshown", true);
+  yield BrowserTestUtils.openNewForegroundTab(gBrowser, TEST_URI);
   yield promisePanelShown;
-  info("Panel is shown");
 
   // click outside the Geolocation hanger to dismiss it
   window.document.getElementById("nav-bar").click();
   info("Clicked outside the Geolocation panel to dismiss it");
 
-  let result = gBrowser.getBrowserForTab(gBrowser.selectedTab)
-                       .contentDocument.body.innerHTML;
-  ok(result.includes("location..."), "Location is not shared");
-});
-
-add_task(function asyncCleanup() {
-  // close the tab
-  gBrowser.removeTab(gBrowser.selectedTab);
-  info("Cleanup: Closed the tab");
-});
-
-function waitForPageLoad(aTab) {
-  return new Promise(resolve => {
-    function onTabLoad(event) {
-      aTab.linkedBrowser.removeEventListener("load", onTabLoad, true);
-      info("Load tab event received");
-      resolve();
-    };
-
-    aTab.linkedBrowser.addEventListener("load", onTabLoad, true, true);
+  let hasLocation = yield ContentTask.spawn(gBrowser.selectedBrowser, {}, function*() {
+    return content.document.body.innerHTML.includes("location...");
   });
-}
 
-function waitForPanelShow(aPanel) {
-  return new Promise(resolve => {
-    function onPopupShown(event) {
-      PopupNotifications.panel.removeEventListener("popupshown", onPopupShown, true);
-      info("Popup shown event received");
-      resolve();
-    }
+  ok(hasLocation, "Location is not shared");
 
-    PopupNotifications.panel.addEventListener("popupshown", onPopupShown, true, true);
-  });
-}
+  yield BrowserTestUtils.removeTab(gBrowser.selectedTab);
+});
