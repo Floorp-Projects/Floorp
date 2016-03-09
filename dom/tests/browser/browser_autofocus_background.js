3dbbdbad697fe380ae12eaaecdd65db63a1eccdf
Neil Deakin <neil@mozilla.com>
2016-03-09 09:11:12 -0500

    Bug 1246635, enable browser_autofocus_background.js in e10s, r=mrbkap

--- dom/tests/browser/browser_autofocus_background.js
+++ dom/tests/browser/browser_autofocus_background.js
@@ -1,9 +1,4 @@
-function test() {
-  waitForExplicitFinish();
-
-  let fm = Components.classes["@mozilla.org/focus-manager;1"]
-                     .getService(Components.interfaces.nsIFocusManager);
-
+add_task(function* () {
   let tabs = [ gBrowser.selectedTab, gBrowser.addTab() ];
 
   // The first tab has an autofocused element.
@@ -13,43 +8,30 @@ function test() {
       tagName: "INPUT"},
   ];
 
-  function runTest() {
-    // Set the focus to the first tab.
-    tabs[0].linkedBrowser.focus();
+  // Set the focus to the first tab.
+  tabs[0].linkedBrowser.focus();
 
-    // Load the first tab on background.
-    tabs[1].linkedBrowser.addEventListener("load", onLoadBackgroundTab, true);
-    tabs[1].linkedBrowser.loadURI(testingList[0].uri);
+  // Load the second tab in the background.
+  let loadedPromise = BrowserTestUtils.browserLoaded(tabs[1].linkedBrowser);
+  tabs[1].linkedBrowser.loadURI(testingList[0].uri);
+  yield loadedPromise;
+
+  for (var i = 0; i < testingList.length; ++i) {
+    // Get active element in the tab.
+    let tagName = yield ContentTask.spawn(tabs[i + 1].linkedBrowser, null, function* () {
+      return content.document.activeElement.tagName;
+    });
+
+    is(tagName, testingList[i].tagName,
+       "The background tab's focused element should be " + testingList[i].tagName);
   }
 
-  function onLoadBackgroundTab() {
-    tabs[1].linkedBrowser.removeEventListener("load", onLoadBackgroundTab, true);
+  is(document.activeElement, tabs[0].linkedBrowser,
+     "The background tab's focused element should not cause the tab to be focused");
 
-    // The focus event (from autofocus) has been sent,
-    // let's test with executeSoon so we are sure the test is done
-    // after the focus event is processed.
-    executeSoon(doTest);
+  // Cleaning up.
+  for (let i = 1; i < tabs.length; i++) {
+    yield BrowserTestUtils.removeTab(tabs[i]);
   }
+});
 
-  function doTest() {
-    for (var i=0; i<testingList.length; ++i) {
-      // Get active element in the tab.
-      var e = tabs[i+1].linkedBrowser.contentDocument.activeElement;
-
-      is(e.tagName, testingList[i].tagName,
-         "The background tab's focused element should be " +
-         testingList[i].tagName);
-      isnot(fm.focusedElement, e,
-            "The background tab's focused element should not be the focus " +
-            "manager focused element");
-    }
-
-    // Cleaning up.
-    for (let i = 1; i < tabs.length; i++) {
-      gBrowser.removeTab(tabs[i]);
-    }
-    finish();
-  }
-
-  runTest();
-}
