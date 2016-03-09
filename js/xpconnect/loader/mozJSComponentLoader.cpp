4d8d19d5b801500ac070cf45adbf859162d33335
Boris Zbarsky <bzbarsky@mit.edu>
2016-03-09 15:28:26 -0500

    Bug 1254846.  Add an AutoEntryScript constructor that takes a JSObject instead of an nsIGlobalObject, for convenience.  r=bholley

--- js/xpconnect/loader/mozJSComponentLoader.cpp
+++ js/xpconnect/loader/mozJSComponentLoader.cpp
@@ -636,7 +636,7 @@ mozJSComponentLoader::PrepareObjectForLocation(JSContext* aCx,
     if (createdNewGlobal) {
         // AutoEntryScript required to invoke debugger hook, which is a
         // Gecko-specific concept at present.
-        dom::AutoEntryScript aes(NativeGlobal(holder->GetJSObject()),
+        dom::AutoEntryScript aes(holder->GetJSObject(),
                                  "component loader report global");
         RootedObject global(aes.cx(), holder->GetJSObject());
         JS_FireOnNewGlobalObject(aes.cx(), global);
