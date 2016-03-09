5fce379d37f6983ca79e7972741bc81d9651c101
Matt Woodrow <mwoodrow@mozilla.com>
2016-03-09 17:35:48 +1300

    Bug 1251527 - Don't override visible regions for background-attachment:fixed. r=mstange

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -2730,8 +2730,6 @@ public:
 
   virtual bool ShouldFixToViewport(nsDisplayListBuilder* aBuilder) override;
 
-  void MarkBoundsAsVisible(nsDisplayListBuilder* aBuilder);
-
 protected:
   typedef class mozilla::layers::ImageContainer ImageContainer;
   typedef class mozilla::layers::ImageLayer ImageLayer;
