d35ce49cd88af1fd1d3701a1c14b2ac9f88785f6
Jon Coppeard <jcoppeard@mozilla.com>
2016-03-09 10:15:10 +0000

    Bug 1254108 - Delay updating of GC trigger threshold until end of GC r=terrence

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -947,6 +947,7 @@ class GCRuntime
     IncrementalProgress sweepPhase(SliceBudget& sliceBudget);
     void endSweepPhase(bool lastGC);
     void sweepZones(FreeOp* fop, bool lastGC);
+    void endFinalizePhase(bool destroyingRuntime);
     void decommitAllWithoutUnlocking(const AutoLockGC& lock);
     void decommitArenas(AutoLockGC& lock);
     void expireChunksAndArenas(bool shouldShrink, AutoLockGC& lock);
