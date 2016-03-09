df5434db04ba7b924cacb368666dfea891ddb4c2
Kyle Machulis <kyle@nonpolynomial.com>
2016-03-09 14:42:42 -0800

    Backout 6397f46b0628 and c6be87916507 (Bug 1171182) due to windows mochitest failures

--- dom/plugins/ipc/PluginInstanceChild.h
+++ dom/plugins/ipc/PluginInstanceChild.h
@@ -89,8 +89,6 @@ protected:
     AnswerNPP_SetValue_NPNVprivateModeBool(const bool& value, NPError* result) override;
     virtual bool
     AnswerNPP_SetValue_NPNVmuteAudioBool(const bool& value, NPError* result) override;
-    virtual bool
-    AnswerNPP_SetValue_NPNVCSSZoomFactor(const double& value, NPError* result) override;
 
     virtual bool
     AnswerNPP_HandleEvent(const NPRemoteEvent& event, int16_t* handled) override;
@@ -405,7 +403,6 @@ private:
 #if defined(XP_DARWIN)
     double mContentsScaleFactor;
 #endif
-    double mCSSZoomFactor;
     int16_t               mDrawingModel;
 
     NPAsyncSurface* mCurrentDirectSurface;
