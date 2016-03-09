ad6f93666e06ea8f4f92c877d880af793583004d
Boris Zbarsky <bzbarsky@mit.edu>
2016-03-09 15:28:45 -0500

    Bug 1254855.  Switch AutoEntryScripts in nsDirectoryViewer to take ownership of error reporting.  r=bholley
    These are both in things that get called from the event loop, so there is no one
    expecting to see the pending exception.

--- xpfe/components/directory/nsDirectoryViewer.cpp
+++ xpfe/components/directory/nsDirectoryViewer.cpp
@@ -157,6 +157,7 @@ nsHTTPIndex::OnFTPControlLog(bool server, const char *msg)
     // AutoEntryScript. This is Gecko specific and not in any spec.
     dom::AutoEntryScript aes(globalObject,
                              "nsHTTPIndex OnFTPControlLog");
+    aes.TakeOwnershipOfErrorReporting();
     JSContext* cx = aes.cx();
 
     JS::Rooted<JSObject*> global(cx, JS::CurrentGlobalOrNull(cx));
@@ -231,6 +232,7 @@ nsHTTPIndex::OnStartRequest(nsIRequest *request, nsISupports* aContext)
     // This is Gecko specific and not in any spec.
     dom::AutoEntryScript aes(globalObject,
                              "nsHTTPIndex set HTTPIndex property");
+    aes.TakeOwnershipOfErrorReporting();
     JSContext* cx = aes.cx();
 
     JS::Rooted<JSObject*> global(cx, JS::CurrentGlobalOrNull(cx));
