fd1f001812dfae0dd7647acf9311269b3c753357
Jeff Walden <jwalden@mit.edu>
2016-03-09 00:37:20 -0800

    Bug 1246061. r=jandem, r=bz, r=luke, r=froydnj, a=lizzard

--- js/src/jit/arm/Assembler-arm.h
+++ js/src/jit/arm/Assembler-arm.h
@@ -1136,6 +1136,18 @@ class Operand
     }
 };
 
+inline Imm32
+Imm64::firstHalf() const
+{
+    return low();
+}
+
+inline Imm32
+Imm64::secondHalf() const
+{
+    return hi();
+}
+
 void
 PatchJump(CodeLocationJump& jump_, CodeLocationLabel label,
           ReprotectCode reprotect = DontReprotect);
