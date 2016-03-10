2ef7b2345bc86c2bc555a1bc0f6815402845ab7d
Margaret Leibovic <margaret.leibovic@gmail.com>
2016-03-10 15:31:30 -0500

    Bug 1253598 - Put offline cache feature behind a switchboard flag. r=sebastian, a=lizzard
    MozReview-Commit-ID: 2g0JKBopIDB

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -6801,6 +6801,9 @@ var Experiments = {
   // Enable malware download protection (bug 936041)
   MALWARE_DOWNLOAD_PROTECTION: "malware-download-protection",
 
+  // Try to load pages from disk cache when network is offline (bug 935190)
+  OFFLINE_CACHE: "offline-cache",
+
   init() {
     Messaging.sendRequestForResult({
       type: "Experiments:GetActive"
@@ -6815,6 +6818,12 @@ var Experiments = {
             let defaults = Services.prefs.getDefaultBranch(null);
             defaults.setBoolPref("browser.safebrowsing.downloads.enabled", true);
             defaults.setBoolPref("browser.safebrowsing.downloads.remote.enabled", true);
+            continue;
+          }
+
+          case this.OFFLINE_CACHE: {
+            let defaults = Services.prefs.getDefaultBranch(null);
+            defaults.setBoolPref("browser.tabs.useCache", true);
             continue;
           }
         }
