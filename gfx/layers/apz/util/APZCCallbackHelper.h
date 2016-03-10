8792f6fbea438edabbec3efe56615f0944ab14bc
Kartikaya Gupta <kgupta@mozilla.com>
2016-03-10 18:25:48 -0500

    Bug 1242690 - Ensure that mouse events have the callback transform applied. r=botond
    MozReview-Commit-ID: 9V7xXPT8WHk

--- gfx/layers/apz/util/APZCCallbackHelper.h
+++ gfx/layers/apz/util/APZCCallbackHelper.h
@@ -99,9 +99,9 @@ public:
                            const ScrollableLayerGuid& aGuid,
                            const CSSToLayoutDeviceScale& aScale);
 
-    /* Convenience function for applying a callback transform to all touch
-     * points of a touch event. */
-    static void ApplyCallbackTransform(WidgetTouchEvent& aEvent,
+    /* Convenience function for applying a callback transform to all refpoints
+     * in the input event. */
+    static void ApplyCallbackTransform(WidgetEvent& aEvent,
                                        const ScrollableLayerGuid& aGuid,
                                        const CSSToLayoutDeviceScale& aScale);
 
