f5a4d687559b0d5cad1ba0e8023f55dd68f68e20
Jim Chen <nchen@mozilla.com>
2016-03-11 13:47:22 -0500

    Bug 1254629 - Let query events fail when content root is wrong; r=masayuki Make query events fail (including when caching selection) if the queried content root is different from what we expected.
    Also, introduce a fix-up to the selection fix in test_imestate.html.

--- dom/events/IMEContentObserver.cpp
+++ dom/events/IMEContentObserver.cpp
@@ -740,10 +740,10 @@ IMEContentObserver::HandleQueryContentEvent(WidgetQueryContentEvent* aEvent)
   mIsHandlingQueryContentEvent = true;
   ContentEventHandler handler(GetPresContext());
   nsresult rv = handler.HandleQueryContentEvent(aEvent);
-  if (aEvent->mSucceeded) {
-    // We need to guarantee that mRootContent should be always same value for
-    // the observing editor.
-    aEvent->mReply.mContentsRoot = mRootContent;
+
+  if (NS_WARN_IF(aEvent->mReply.mContentsRoot != mRootContent)) {
+    // Focus has changed unexpectedly, so make the query fail.
+    aEvent->mSucceeded = false;
   }
   return rv;
 }
@@ -1261,7 +1261,8 @@ IMEContentObserver::UpdateSelectionCache()
   WidgetQueryContentEvent selection(true, eQuerySelectedText, mWidget);
   ContentEventHandler handler(GetPresContext());
   handler.OnQuerySelectedText(&selection);
-  if (NS_WARN_IF(!selection.mSucceeded)) {
+  if (NS_WARN_IF(!selection.mSucceeded) ||
+      NS_WARN_IF(selection.mReply.mContentsRoot != mRootContent)) {
     return false;
   }
 
