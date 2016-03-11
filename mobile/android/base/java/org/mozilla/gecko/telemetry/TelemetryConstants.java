f9f1d82391eb65b84a3af5877d048f01f9aac314
Michael Comella <michael.l.comella@gmail.com>
2016-03-10 16:00:15 -0800

    Bug 1249288 - Add default search engine to core ping. r=rnewman a=ritu
    The default search engine attribute may be null in the core ping if we haven't
    been able to retrieve the value yet. It's unclear when this might be, but the
    possibility is in the javadoc of `SearchEngineManager.getEngine`.
    
    MozReview-Commit-ID: IrJB6GyjyTO

--- mobile/android/base/java/org/mozilla/gecko/telemetry/TelemetryConstants.java
+++ mobile/android/base/java/org/mozilla/gecko/telemetry/TelemetryConstants.java
@@ -16,6 +16,7 @@ public class TelemetryConstants {
             "Firefox-Android-Telemetry/" + AppConstants.MOZ_APP_VERSION + " (" + AppConstants.MOZ_APP_UA_NAME + ")";
 
     public static final String ACTION_UPLOAD_CORE = "uploadCore";
+    public static final String EXTRA_DEFAULT_SEARCH_ENGINE = "defaultSearchEngine";
     public static final String EXTRA_DOC_ID = "docId";
     public static final String EXTRA_PROFILE_NAME = "geckoProfileName";
     public static final String EXTRA_PROFILE_PATH = "geckoProfilePath";
@@ -28,11 +29,12 @@ public class TelemetryConstants {
         private CorePing() { /* To prevent instantiation */ }
 
         public static final String NAME = "core";
-        public static final int VERSION_VALUE = 1;
+        public static final int VERSION_VALUE = 2; // For version history, see toolkit/components/telemetry/docs/core-ping.rst
         public static final String OS_VALUE = "Android";
 
         public static final String ARCHITECTURE = "arch";
         public static final String CLIENT_ID = "clientId";
+        public static final String DEFAULT_SEARCH_ENGINE = "defaultSearch";
         public static final String DEVICE = "device";
         public static final String EXPERIMENTS = "experiments";
         public static final String LOCALE = "locale";
