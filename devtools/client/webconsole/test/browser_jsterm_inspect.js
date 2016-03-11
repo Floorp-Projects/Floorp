65e076156d581143c4476e04c23bb40974ba486c
Jarda Snajdr <jsnajdr@gmail.com>
2016-03-11 01:33:00 -0500

    Bug 1255734 - DevTools: Inspecting a primitive value in console throws a TypeError. r=vporof
    --HG--
    extra : rebase_source : c10241573f3ca5f5ecbbb040a24f551d0a848f0d

--- devtools/client/webconsole/test/browser_jsterm_inspect.js
+++ devtools/client/webconsole/test/browser_jsterm_inspect.js
@@ -15,16 +15,33 @@ add_task(function* () {
   let hud = yield openConsole();
   let jsterm = hud.jsterm;
 
+  /* Check that the window object is inspected */
   jsterm.execute("testProp = 'testValue'");
 
-  let fetched = jsterm.once("variablesview-fetched");
+  let updated = jsterm.once("variablesview-updated");
   jsterm.execute("inspect(window)");
-  let variable = yield fetched;
+  let view = yield updated;
+  ok(view, "variables view object");
 
-  ok(variable._variablesView, "variables view object");
+  // The single variable view contains a scope with the variable name
+  // and unnamed subitem that contains the properties
+  let variable = view.getScopeAtIndex(0).get("");
+  ok(variable, "variable object");
 
   yield findVariableViewProperties(variable, [
     { name: "testProp", value: "testValue" },
     { name: "document", value: /HTMLDocument \u2192 data:/ },
   ], { webconsole: hud });
+
+  /* Check that a primitive value can be inspected, too */
+  let updated2 = jsterm.once("variablesview-updated");
+  jsterm.execute("inspect(1)");
+  let view2 = yield updated2;
+  ok(view2, "variables view object");
+
+  // Check the label of the scope - it should contain the value
+  let scope = view.getScopeAtIndex(0);
+  ok(scope, "variable object");
+
+  is(scope.name, "1", "The value of the primitive var is correct");
 });
