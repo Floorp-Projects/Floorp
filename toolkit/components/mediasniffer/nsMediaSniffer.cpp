4a997c4ede27dbadafb52360ba21adc49b8eed87
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-03-11 22:32:30 +1100

    Bug 1251184 - [quicktime] P1. Report video/quicktime mimetype when sniffing. r=cpearce, a=ritu
    MozReview-Commit-ID: 9EJQVYt76Mh

--- toolkit/components/mediasniffer/nsMediaSniffer.cpp
+++ toolkit/components/mediasniffer/nsMediaSniffer.cpp
@@ -43,7 +43,7 @@ nsMediaSnifferEntry sFtypEntries[] = {
   PATTERN_ENTRY("\xFF\xFF\xFF", "3gp", VIDEO_3GPP), // Could be 3gp4, 3gp5, ...
   PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "M4A ", AUDIO_MP4),
   PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "M4P ", AUDIO_MP4),
-  PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "qt  ", VIDEO_MP4),
+  PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "qt  ", VIDEO_QUICKTIME),
 };
 
 static bool MatchesBrands(const uint8_t aData[4], nsACString& aSniffedType)
