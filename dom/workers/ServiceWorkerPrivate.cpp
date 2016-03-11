b3f9947be18f1ecde49fb0d3138972e3bf4e9428
Boris Zbarsky <bzbarsky@mit.edu>
2016-03-11 16:43:30 -0500

    Bug 1255706 part 2.  Remove JSContext argument from WorkerPrivate::Terminate.  r=khuey

--- dom/workers/ServiceWorkerPrivate.cpp
+++ dom/workers/ServiceWorkerPrivate.cpp
@@ -1523,9 +1523,7 @@ ServiceWorkerPrivate::TerminateWorker()
       }
     }
 
-    AutoJSAPI jsapi;
-    jsapi.Init();
-    NS_WARN_IF(!mWorkerPrivate->Terminate(jsapi.cx()));
+    NS_WARN_IF(!mWorkerPrivate->Terminate());
     mWorkerPrivate = nullptr;
     mSupportsArray.Clear();
 
