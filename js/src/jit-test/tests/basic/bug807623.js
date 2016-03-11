31cba2f91aa5779700319f4ebdd2f62f2d6f1460
Tom Schuster <evilpies@gmail.com>
2016-03-11 13:32:26 +0100

    Bug 892903 - Remove Proxy.create from jit-tests. r=efaust

--- js/src/jit-test/tests/basic/bug807623.js
+++ js/src/jit-test/tests/basic/bug807623.js
@@ -1,9 +1,14 @@
-var objectProxy = Proxy.create({});
-var functionProxy = Proxy.createFunction({}, function() {}, function() {});
+var objectProxy = new Proxy({}, {});
+var functionProxy = new Proxy(function() {}, {});
 
 assertEq(Object.prototype.toString.call(objectProxy), '[object Object]');
 assertEq(Object.prototype.toString.call(functionProxy), '[object Function]');
-assertEq(Function.prototype.toString.call(functionProxy), 'function () {}');
+try {
+  Function.prototype.toString.call(functionProxy);
+  assertEq(true, false);
+} catch (e) {
+  assertEq(!!/incompatible/.exec(e), true);
+}
 try {
   Function.prototype.toString.call(objectProxy);
   assertEq(true, false);
