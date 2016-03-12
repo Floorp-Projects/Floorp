913587d999010c820ad7db3ac07577fc55f8a3f8
Tooru Fujisawa <arai_a@mac.com>
2016-03-13 04:54:30 +0900

    Backed out changeset 13e045fff28d (bug 891107)

--- js/src/jit-test/tests/ctypes/incompatible-abi.js
+++
@@ -1,9 +0,0 @@
-load(libdir + 'asserts.js');
-
-function test() {
-  assertTypeErrorMessage(() => { ctypes.default_abi.toSource.call(1); },
-                         "ABI.prototype.toSource called on incompatible Number");
-}
-
-if (typeof ctypes === "object")
-  test();
