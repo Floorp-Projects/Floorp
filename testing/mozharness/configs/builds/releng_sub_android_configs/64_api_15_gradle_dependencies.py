4ccbf54dd10e86d2ccd585dc9bd326f6125e92ec
Nick Alexander <nalexander@mozilla.com>
2016-03-12 16:43:42 -0800

    Bug 1258452 - Pre: Assemble app and tests when collecting dependencies. r=me
    In the wild, the Robotium Maven dependency is not being fetched.
    Let's see if this forces it to be downloaded.
    
    MozReview-Commit-ID: 3Fhm4Aj40Jp

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_api_15_gradle_dependencies.py
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_api_15_gradle_dependencies.py
@@ -6,6 +6,10 @@ config = {
     'tooltool_manifest_src': 'mobile/android/config/tooltool-manifests/android-gradle-dependencies/releng.manifest',
     'multi_locale_config_platform': 'android',
     'postflight_build_mach_commands': [
-        ['gradle', 'resolveDependencies'],
+        ['gradle',
+         'assembleAutomationRelease',
+         'assembleAutomationDebug',
+         'assembleAutomationDebugAndroidTest',
+        ],
     ],
 }
