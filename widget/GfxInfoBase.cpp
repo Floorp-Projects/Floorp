e08a09dafa20fda3f8ee9ffe4b0dba50ab66fb3b
Andrew McCreight <continuation@gmail.com>
2016-03-14 10:27:00 +0100

    Bug 1242084 - Fix GfxInfoBase nsStringBuffer leak. r=dvander

--- widget/GfxInfoBase.cpp
+++ widget/GfxInfoBase.cpp
@@ -1151,10 +1151,11 @@ nsresult GfxInfoBase::GetInfo(JSContext* aCx, JS::MutableHandle<JS::Value> aResu
   return NS_OK;
 }
 
+nsAutoCString gBaseAppVersion;
+
 const nsCString&
 GfxInfoBase::GetApplicationVersion()
 {
-  static nsCString version;
   static bool versionInitialized = false;
   if (!versionInitialized) {
     // If we fail to get the version, we will not try again.
@@ -1163,10 +1164,10 @@ GfxInfoBase::GetApplicationVersion()
     // Get the version from xpcom/system/nsIXULAppInfo.idl
     nsCOMPtr<nsIXULAppInfo> app = do_GetService("@mozilla.org/xre/app-info;1");
     if (app) {
-      app->GetVersion(version);
+      app->GetVersion(gBaseAppVersion);
     }
   }
-  return version;
+  return gBaseAppVersion;
 }
 
 void
