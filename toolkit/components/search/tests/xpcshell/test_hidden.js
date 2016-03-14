77612f26acdd37e99ce584ed7c6d4bb952c66415
Florian Quèze <florian@queze.net>
2016-03-14 18:45:33 +0100

    Bug 1255605 - search.json.mozlz4 should always contain search engines. r=adw, a=sledru

--- toolkit/components/search/tests/xpcshell/test_hidden.js
+++ toolkit/components/search/tests/xpcshell/test_hidden.js
@@ -45,13 +45,12 @@ add_task(function* async_init() {
 });
 
 add_task(function* sync_init() {
+  let unInitPromise = waitForSearchNotification("uninit-complete");
   let reInitPromise = asyncReInit();
-  // Synchronously check the current default engine, to force a sync init.
-  // XXX For some reason forcing a sync init while already asynchronously
-  // reinitializing causes a shutdown warning related to engineMetadataService's
-  // finalize method having already been called. Seems harmless for the purpose
-  // of this test.
+  yield unInitPromise;
   do_check_false(Services.search.isInitialized);
+
+  // Synchronously check the current default engine, to force a sync init.
   do_check_eq(Services.search.currentEngine.name, "hidden");
   do_check_true(Services.search.isInitialized);
 
@@ -67,7 +66,6 @@ add_task(function* sync_init() {
   do_check_neq(engine, null);
 
   yield reInitPromise;
-  yield promiseAfterCache();
 });
 
 add_task(function* invalid_engine() {
