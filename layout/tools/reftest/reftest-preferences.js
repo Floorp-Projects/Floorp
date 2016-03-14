98115883ad8ab029e61a2a2b8b58da2bd3848c53
Andrew Halberstadt <ahalberstadt@mozilla.com>
2016-03-14 17:00:40 -0400

    Bug 1245092 - Attempt to fix non-local network osx opt reftest bustage, r=me, a=test-only
    MozReview-Commit-ID: 1N6lsrbMNyI
    
    --HG--
    extra : rebase_source : 09ae6302553636f7014934aa9a537d924cbea1d9

--- layout/tools/reftest/reftest-preferences.js
+++ layout/tools/reftest/reftest-preferences.js
@@ -107,6 +107,7 @@ user_pref("browser.tabs.remote.autostart.1", false);
 user_pref("browser.tabs.remote.autostart.2", false);
 
 user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
 user_pref("startup.homepage_override_url", "");
 
 user_pref("media.gmp-manager.url.override", "http://localhost/dummy-gmp-manager.xml");
