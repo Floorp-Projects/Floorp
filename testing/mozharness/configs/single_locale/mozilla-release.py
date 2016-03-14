7f2d8db7eb5cb62f58f1518ee11b332e9bf09df5
Rail Aliiev <rail@mozilla.com>
2016-03-14 08:26:54 -0400

    Bug 1253756 - Branch specifics for relpro on release r=jlund a=release DONTBUILD
    MozReview-Commit-ID: Fva2LLVlsRV

---
+++ testing/mozharness/configs/single_locale/mozilla-release.py
@@ -0,0 +1,40 @@
+config = {
+    "nightly_build": True,
+    "branch": "mozilla-release",
+    "en_us_binary_url": "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-release/",
+    "update_channel": "release",
+    "latest_mar_dir": '/pub/mozilla.org/firefox/nightly/latest-mozilla-release-l10n',
+
+    # l10n
+    "hg_l10n_base": "https://hg.mozilla.org/releases/l10n/mozilla-release",
+
+    # repositories
+    "mozilla_dir": "mozilla-release",
+    "repos": [{
+        "vcs": "hg",
+        "repo": "https://hg.mozilla.org/build/tools",
+        "revision": "default",
+        "dest": "tools",
+    }, {
+        "vcs": "hgtool",
+        "repo": "https://hg.mozilla.org/releases/mozilla-release",
+        "revision": "default",
+        "dest": "mozilla-release",
+    }, {
+        "vcs": "hgtool",
+        "repo": "https://hg.mozilla.org/build/compare-locales",
+        "revision": "RELEASE_AUTOMATION"
+    }],
+    # purge options
+    'purge_minsize': 12,
+    'is_automation': True,
+    'default_actions': [
+        "clobber",
+        "pull",
+        "list-locales",
+        "setup",
+        "repack",
+        "taskcluster-upload",
+        "summary",
+    ],
+}
