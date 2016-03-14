1c18167e41a936ca4d8ecd73f8604aa1e7c04e59
Maurya Talisetti <maurya1985@yahoo.com>
2016-03-14 01:25:00 -0400

    Bug 1241857 - Add calls to super implementation to address lint warnings. r=mcomella

--- mobile/android/base/java/org/mozilla/gecko/gfx/LayerView.java
+++ mobile/android/base/java/org/mozilla/gecko/gfx/LayerView.java
@@ -282,6 +282,8 @@ public class LayerView extends ScrollView implements Tabs.OnTabsChangedListener 
 
     @Override
     protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
         // We are adding descendants to this LayerView, but we don't want the
         // descendants to affect the way LayerView retains its focus.
         setDescendantFocusability(FOCUS_BLOCK_DESCENDANTS);
