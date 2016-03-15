bf626f29a18b35c4d918e307c3ad45dabfebe855
Nihanth Subramanya <nhnt11@gmail.com>
2016-03-15 02:36:21 -0700

    Bug 1240594 - Update tests that touch about:certerror. r=Gijs
    MozReview-Commit-ID: 6dUWaq6eNbZ

--- browser/base/content/test/general/browser_bug633691.js
+++ browser/base/content/test/general/browser_bug633691.js
@@ -4,7 +4,7 @@
 
 function test() {
   waitForExplicitFinish();
-  gBrowser.selectedTab = gBrowser.addTab("data:text/html,<iframe width='700' height='700' src='about:certerror'></iframe>");
+  gBrowser.selectedTab = gBrowser.addTab("data:text/html,<iframe width='700' height='700' src='about:certerror?e=nssBadCert&u='></iframe>");
   // Open a html page with about:certerror in an iframe
   BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser).then(testIframeCert);
 }
@@ -12,7 +12,7 @@ function test() {
 function testIframeCert(e) {
   // Confirm that the expert section is hidden
   var doc = gBrowser.contentDocument.getElementsByTagName('iframe')[0].contentDocument;
-  var aP = doc.getElementById("advancedPanel");
+  var aP = doc.getElementById("badCertAdvancedPanel");
   ok(aP, "Advanced content should exist");
   is_element_hidden(aP, "Advanced content should not be visible by default")
 
