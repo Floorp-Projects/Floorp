b5600c6b0ec988b59d3fc911936ebd1eb9c61066
Daniel Holbert <dholbert@cs.stanford.edu>
2016-03-15 15:51:56 -0700

    backout d85b18faba72 (bug 1187270) for build bustage.

--- toolkit/crashreporter/nsExceptionHandler.h
+++ toolkit/crashreporter/nsExceptionHandler.h
@@ -51,7 +51,6 @@ void SetUserAppDataDirectory(nsIFile* aDir);
 void SetProfileDirectory(nsIFile* aDir);
 void UpdateCrashEventsDir();
 void SetMemoryReportFile(nsIFile* aFile);
-void SetTelemetrySessionId(const nsACString& id);
 
 /**
  * Get the path where crash event files should be written.
