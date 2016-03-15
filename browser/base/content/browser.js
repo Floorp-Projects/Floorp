f41773051497838baf4e00217fbb6f5dae0a71db
Mike Conley <mconley@mozilla.com>
2016-03-15 14:45:38 -0400

    Bug 1255511 - Skip beforeunload prompts once nsIAppStartup shuttingDown returns true. r=Gijs, a=ritu
    When the application is asked to quit, all windows are checked to see if
    they can close (which will spawn the permit unload dialogs). Once the
    user grants the quit via the permit unload dialogs, nsIAppStartup
    returns true for the shuttingDown attribute, and we know that all
    windows allowed the shutdown to occur. This means that we don't
    have to show the permitUnload dialogs again when SessionStore
    attempts to close the windows when it does its final async window
    flush during the quit-application-granted observer topic.
    
    MozReview-Commit-ID: DOy8ljngnTd

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6124,7 +6124,7 @@ function CanCloseWindow()
 {
   // Avoid redundant calls to canClose from showing multiple
   // PermitUnload dialogs.
-  if (window.skipNextCanClose) {
+  if (Services.startup.shuttingDown || window.skipNextCanClose) {
     return true;
   }
 
