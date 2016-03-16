6c9fd32ed1859aaaf8eeffa965527f5116fb5b53
Brian Birtles <birtles@gmail.com>
2016-03-16 15:05:10 +0800

    Bug 1245260 - Ignore redundant calls to RestyleManager::IncrementAnimationGeneration; r=dbaron a=lizzard
    While processing restyles and starting transitions, we may trigger
    a call to EffectCompositor::UpdateCascadeResults which may, in turn, call
    EffectCompositor::RequestRestyle with RestyleType::Layer, which ultimately
    results in a call to RestyleManager::IncrementAnimationGeneration().
    
    Typically, nsTransitionManager::StyleContextChanged compares the animation
    generation on its collection with that of the restyle manager and uses this
    to ignore the restyle that it generates. However, given the sequence of events
    above, that check may no longer help since the restyle manager's animation
    generation will be out of step. As a result,
    nsTransitionManager::StyleContextChanged will fail to ignore a subsequent
    and redundant restyle. With certain combinations of content, this can mean that
    restyles are posted in such a manner than an infinite cycle of restyles ensues.
    
    This patch causes RestyleManager to ignore calls to IncrementAnimationGeneration
    when it is already processing restyles such that the animation generation is
    only ever updated once per restyle. This makes the check for a matching
    animation generation in nsTransitionManager::StyleContextChanged work as
    expected, preventing us from generating needless transitions which can produce
    this endless loop.
    
    MozReview-Commit-ID: 9HYDrknKPAI

--- layout/base/RestyleManager.cpp
+++ layout/base/RestyleManager.cpp
@@ -95,9 +95,7 @@ RestyleManager::RestyleManager(nsPresContext* aPresContext)
   , mPendingRestyles(ELEMENT_HAS_PENDING_RESTYLE |
                      ELEMENT_IS_POTENTIAL_RESTYLE_ROOT |
                      ELEMENT_IS_CONDITIONAL_RESTYLE_ANCESTOR)
-#ifdef DEBUG
   , mIsProcessingRestyles(false)
-#endif
 #ifdef RESTYLE_LOGGING
   , mLoggingDepth(0)
 #endif
@@ -1731,9 +1729,7 @@ RestyleManager::ProcessPendingRestyles()
   // Process non-animation restyles...
   MOZ_ASSERT(!mIsProcessingRestyles,
              "Nesting calls to ProcessPendingRestyles?");
-#ifdef DEBUG
   mIsProcessingRestyles = true;
-#endif
 
   // Before we process any restyles, we need to ensure that style
   // resulting from any animations is up-to-date, so that if any style
@@ -1742,7 +1738,7 @@ RestyleManager::ProcessPendingRestyles()
   bool haveNonAnimation =
     mHavePendingNonAnimationRestyles || mDoRebuildAllStyleData;
   if (haveNonAnimation) {
-    IncrementAnimationGeneration();
+    ++mAnimationGeneration;
     UpdateOnlyAnimationStyles();
   } else {
     // If we don't have non-animation style updates, then we have queued
@@ -1775,9 +1771,7 @@ RestyleManager::ProcessPendingRestyles()
     mPresContext->TransitionManager()->SetInAnimationOnlyStyleUpdate(false);
   }
 
-#ifdef DEBUG
   mIsProcessingRestyles = false;
-#endif
 
   NS_ASSERTION(haveNonAnimation || !mHavePendingNonAnimationRestyles,
                "should not have added restyles");
