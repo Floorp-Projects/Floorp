ed08942bfede78e9859f0867ef0875b21bf4928f
Andrew McCreight <continuation@gmail.com>
2016-03-16 11:48:00 +0100

    Bug 1256061 - Actually check if the underlying referent still exists in nsGeolocationRequest::TimerCallbackHolder::Notify(). r=jdm

--- dom/geolocation/nsGeolocation.cpp
+++ dom/geolocation/nsGeolocation.cpp
@@ -775,7 +775,7 @@ NS_IMPL_ISUPPORTS(nsGeolocationRequest::TimerCallbackHolder, nsISupports, nsITim
 NS_IMETHODIMP
 nsGeolocationRequest::TimerCallbackHolder::Notify(nsITimer*)
 {
-  if (mRequest) {
+  if (mRequest.get()) {
     mRequest->Notify();
   }
   return NS_OK;
