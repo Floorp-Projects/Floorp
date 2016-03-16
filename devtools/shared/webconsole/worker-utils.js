c0dd7eb1a43fa684ea7e5538acbe12ded7e538dd
Tom Tromey <tom@tromey.com>
2016-03-16 08:16:00 +0100

    Bug 1255799 - make devtools/client/webconsole eslint-clean; r=odvarko
    MozReview-Commit-ID: AnaXFFHlNLM

--- devtools/shared/webconsole/worker-utils.js
+++ devtools/shared/webconsole/worker-utils.js
@@ -4,6 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+"use strict";
+
 // XXXworkers This file is loaded on the server side for worker debugging.
 // Since the server is running in the worker thread, it doesn't
 // have access to Services / Components.  This functionality
