2cbae1481bd0a3b6f4c70cc849804a889d965455
Tooru Fujisawa <arai_a@mac.com>
2016-03-16 16:58:29 +0900

    Bug 1256088 - Merge mock app-info implementation into AppInfo.jsm. r=gps

--- browser/components/places/tests/unit/head_bookmarks.js
+++ browser/components/places/tests/unit/head_bookmarks.js
@@ -29,36 +29,13 @@ const ORGANIZER_FOLDER_ANNO = "PlacesOrganizer/OrganizerFolder";
 const ORGANIZER_QUERY_ANNO = "PlacesOrganizer/OrganizerQuery";
 
 // Needed by some test that relies on having an app registered.
-var XULAppInfo = {
-  vendor: "Mozilla",
+Cu.import("resource://testing-common/AppInfo.jsm", this);
+updateAppInfo({
   name: "PlacesTest",
   ID: "{230de50e-4cd1-11dc-8314-0800200c9a66}",
   version: "1",
-  appBuildID: "2007010101",
   platformVersion: "",
-  platformBuildID: "2007010101",
-  inSafeMode: false,
-  logConsoleErrors: true,
-  OS: "XPCShell",
-  XPCOMABI: "noarch-spidermonkey",
-
-  QueryInterface: XPCOMUtils.generateQI([
-    Ci.nsIXULAppInfo,
-    Ci.nsIXULRuntime,
-  ])
-};
-
-var XULAppInfoFactory = {
-  createInstance: function (outer, iid) {
-    if (outer != null)
-      throw Cr.NS_ERROR_NO_AGGREGATION;
-    return XULAppInfo.QueryInterface(iid);
-  }
-};
-var registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
-registrar.registerFactory(Components.ID("{fbfae60b-64a4-44ef-a911-08ceb70b9f31}"),
-                          "XULAppInfo", "@mozilla.org/xre/app-info;1",
-                          XULAppInfoFactory);
+});
 
 // Smart bookmarks constants.
 const SMART_BOOKMARKS_VERSION = 7;
--- chrome/test/unit/test_abi.js
+++ chrome/test/unit/test_abi.js
@@ -1,41 +1,10 @@
-const XULAppInfo = {
-  vendor: "Mozilla",
+Components.utils.import("resource://testing-common/AppInfo.jsm", this);
+updateAppInfo({
   name: "XPCShell",
   ID: "{39885e5f-f6b4-4e2a-87e5-6259ecf79011}",
   version: "5",
-  appBuildID: "2007010101",
   platformVersion: "1.9",
-  platformBuildID: "2007010101",
-  inSafeMode: false,
-  logConsoleErrors: true,
-  OS: "XPCShell",
-  XPCOMABI: "noarch-spidermonkey",
-  
-  QueryInterface: function QueryInterface(iid) {
-    if (iid.equals(Ci.nsIXULAppInfo)
-     || iid.equals(Ci.nsIXULRuntime)
-     || iid.equals(Ci.nsISupports))
-      return this;
-  
-    throw Components.results.NS_ERROR_NO_INTERFACE;
-  }
-};
-
-const XULAppInfoFactory = {
-  // These two are used when we register all our factories (and unregister)
-  CID: XULAPPINFO_CID,
-  scheme: "XULAppInfo",
-  contractID: XULAPPINFO_CONTRACTID,
-  createInstance: function (outer, iid) {
-    if (outer != null)
-      throw Cr.NS_ERROR_NO_AGGREGATION;
-    return XULAppInfo.QueryInterface(iid);
-  }
-};
-
-var registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
-registrar.registerFactory(XULAPPINFO_CID, "XULAppInfo",
-                          XULAPPINFO_CONTRACTID, XULAppInfoFactory);
+});
 
 registerManifests([do_get_file("data/test_abi.manifest")]);
 
