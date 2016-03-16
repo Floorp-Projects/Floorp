3436420e379fb67c86a76e90b5ba174b672d2728
Andrew Halberstadt <ahalberstadt@mozilla.com>
2016-03-16 14:55:21 -0400

    Bug 1230962 - Add python/mozlint for running several linters at once, r=smacleod
    Mozlint provides two main benefits:
    1. A common system for defining lints across multiple languages
    2. A common interface and result format for running them
    
    This commit only adds the core library, it does not add any consumers of mozlint just yet.
    
    MozReview-Commit-ID: CSQzq5del5k

---
+++ python/mozlint/mozlint/parser.py
@@ -0,0 +1,85 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import imp
+import os
+import sys
+import uuid
+
+from .types import supported_types
+from .errors import LinterNotFound, LinterParseError
+
+
+class Parser(object):
+    """Reads and validates `.lint` files."""
+    required_attributes = (
+        'name',
+        'description',
+        'type',
+        'payload',
+    )
+
+    def __call__(self, path):
+        return self.parse(path)
+
+    def _load_linter(self, path):
+        # Ensure parent module is present otherwise we'll (likely) get
+        # an error due to unknown parent.
+        parent_module = 'mozlint.linters'
+        if parent_module not in sys.modules:
+            mod = imp.new_module(parent_module)
+            sys.modules[parent_module] = mod
+
+        write_bytecode = sys.dont_write_bytecode
+        sys.dont_write_bytecode = True
+
+        module_name = '{}.{}'.format(parent_module, uuid.uuid1().get_hex())
+        imp.load_source(module_name, path)
+
+        sys.dont_write_bytecode = write_bytecode
+
+        mod = sys.modules[module_name]
+
+        if not hasattr(mod, 'LINTER'):
+            raise LinterParseError(path, "No LINTER definition found!")
+
+        definition = mod.LINTER
+        definition['path'] = path
+        return definition
+
+    def _validate(self, linter):
+        missing_attrs = []
+        for attr in self.required_attributes:
+            if attr not in linter:
+                missing_attrs.append(attr)
+
+        if missing_attrs:
+            raise LinterParseError(linter['path'], "Missing required attribute(s): "
+                                                   "{}".format(','.join(missing_attrs)))
+
+        if linter['type'] not in supported_types:
+            raise LinterParseError(linter['path'], "Invalid type '{}'".format(linter['type']))
+
+        for attr in ('include', 'exclude'):
+            if attr in linter and (not isinstance(linter[attr], list) or
+                                   not all(isinstance(a, basestring) for a in linter[attr])):
+                raise LinterParseError(linter['path'], "The {} directive must be a "
+                                                       "list of strings!".format(attr))
+
+    def parse(self, path):
+        """Read a linter and return its LINTER definition.
+
+        :param path: Path to the linter.
+        :returns: Linter definition (dict)
+        :raises: LinterNotFound, LinterParseError
+        """
+        if not os.path.isfile(path):
+            raise LinterNotFound(path)
+
+        if not path.endswith('.lint'):
+            raise LinterParseError(path, "Invalid filename, linters must end with '.lint'!")
+
+        linter = self._load_linter(path)
+        self._validate(linter)
+        return linter
---
+++ python/mozlint/test/test_formatters.py
@@ -0,0 +1,84 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import unicode_literals
+
+import json
+import os
+from collections import defaultdict
+from unittest import TestCase
+
+from mozunit import main
+
+from mozlint import ResultContainer
+from mozlint import formatters
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class TestFormatters(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+
+        containers = (
+            ResultContainer(
+                linter='foo',
+                path='a/b/c.txt',
+                message="oh no foo",
+                lineno=1,
+            ),
+            ResultContainer(
+                linter='bar',
+                path='d/e/f.txt',
+                message="oh no bar",
+                hint="try baz instead",
+                level='warning',
+                lineno=4,
+                column=2,
+                rule="bar-not-allowed",
+            ),
+            ResultContainer(
+                linter='baz',
+                path='a/b/c.txt',
+                message="oh no baz",
+                lineno=4,
+                source="if baz:",
+            ),
+        )
+
+        self.results = defaultdict(list)
+        for c in containers:
+            self.results[c.path].append(c)
+
+    def test_stylish_formatter(self):
+        expected = """
+a/b/c.txt
+  1:1  error  oh no foo  (foo)
+  4:1  error  oh no baz  (baz)
+
+d/e/f.txt
+  4:2  warning  oh no bar  bar-not-allowed (bar)
+
+\u2716 3 problems (2 errors, 1 warning)
+""".strip()
+
+        fmt = formatters.get('stylish', disable_colors=True)
+        self.assertEqual(expected, fmt(self.results))
+
+    def test_json_formatter(self):
+        fmt = formatters.get('json')
+        formatted = json.loads(fmt(self.results))
+
+        self.assertEqual(set(formatted.keys()), set(self.results.keys()))
+
+        slots = ResultContainer.__slots__
+        for errors in formatted.values():
+            for err in errors:
+                self.assertTrue(all(s in err for s in slots))
+
+
+if __name__ == '__main__':
+    main()
