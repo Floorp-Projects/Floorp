b08b1415ba26cf57e39999f193a1fc6fb7a62e40
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-03-17 17:00:54 +0100

    Backed out changeset 6d3033d429e5 (bug 1252713) for bustage

--- js/src/jsapi-tests/testForceLexicalInitialization.cpp
+++ js/src/jsapi-tests/testForceLexicalInitialization.cpp
@@ -13,24 +13,24 @@ BEGIN_TEST(testForceLexicalInitialization)
 {
     // Attach an uninitialized lexical to a scope and ensure that it's
     // set to undefined
-    js::RootedGlobalObject g(cx, cx->global());
-    JS::Rooted<js::ClonedBlockObject*> scope(cx, js::ClonedBlockObject::createGlobal(cx, g));
+    RootedGlobalObject g(cx, cx->global());
+    Rooted<ClonedBlockObject*> scope(cx, ClonedBlockObject::createGlobal(cx, g));
 
-    JS::RootedValue uninitialized(cx, JS::MagicValue(JS_UNINITIALIZED_LEXICAL));
-    js::RootedPropertyName name(cx, Atomize(cx, "foopi", 4)->asPropertyName());
-    JS::RootedId id(cx, NameToId(name));
+    RootedValue uninitialized(cx, MagicValue(JS_UNINITIALIZED_LEXICAL));
+    RootedPropertyName name(cx, Atomize(cx, "foopi", 4)->asPropertyName());
+    RootedId id(cx, NameToId(name));
     unsigned attrs = JSPROP_ENUMERATE | JSPROP_PERMANENT;
 
     NativeDefineProperty(cx, scope, id, uninitialized, nullptr, nullptr, attrs);
 
     // Verify that "foopi" is uninitialized
-    const JS::Value v = scope->getSlot(scope->lookup(cx, id)->slot());
+    const Value v = scope->getSlot(scope->lookup(cx, id)->slot());
     CHECK(v.isMagic(JS_UNINITIALIZED_LEXICAL));
 
     ForceLexicalInitialization(cx, scope);
 
     // Verify that "foopi" has been initialized to undefined
-    const JS::Value v2 = scope->getSlot(scope->lookup(cx, id)->slot());
+    const Value v2 = scope->getSlot(scope->lookup(cx, id)->slot());
     CHECK(v2.isUndefined());
 
     return true;
