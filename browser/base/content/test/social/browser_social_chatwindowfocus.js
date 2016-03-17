c3a25096a7b42f363a83495adbdb3179fa418e7b
Shane Caraveo <scaraveo@mozilla.com>
2016-03-17 16:46:41 -0700

    Bug 898706 remove frameworker and all associated code and tests, r=markh

--- browser/base/content/test/social/browser_social_chatwindowfocus.js
+++ browser/base/content/test/social/browser_social_chatwindowfocus.js
@@ -6,71 +6,10 @@ function isChatFocused(chat) {
   return getChatBar()._isChatFocused(chat);
 }
 
-function openChatViaSidebarMessage(port, data, callback) {
-  port.onmessage = function (e) {
-    if (e.data.topic == "chatbox-opened")
-      callback();
-  }
-  port.postMessage({topic: "test-chatbox-open", data: data});
-}
-
-function openChatViaWorkerMessage(port, data, callback) {
-  // sadly there is no message coming back to tell us when the chat has
-  // been opened, so we wait until one appears.
-  let chatbar = getChatBar();
-  let numExpected = chatbar.childElementCount + 1;
-  port.postMessage({topic: "test-worker-chat", data: data});
-  waitForCondition(() => chatbar.childElementCount == numExpected,
-                   function() {
-                      // so the child has been added, but we don't know if it
-                      // has been intialized - re-request it and the callback
-                      // means it's done.  Minimized, same as the worker.
-                      chatbar.openChat({
-                        origin: SocialSidebar.provider.origin,
-                        title: SocialSidebar.provider.name,
-                        url: data,
-                        mode: "minimized"
-                      }, function() { callback(); });
-                   },
-                   "No new chat appeared");
-}
-
-
-var isSidebarLoaded = false;
-
-function startTestAndWaitForSidebar(callback) {
-  let doneCallback;
-  let port = SocialSidebar.provider.getWorkerPort();
-  function maybeCallback() {
-    if (!doneCallback)
-      callback(port);
-    doneCallback = true;
-  }
-  port.onmessage = function(e) {
-    let topic = e.data.topic;
-    switch (topic) {
-      case "got-sidebar-message":
-        // if sidebar loaded too fast, we need a backup ping
-      case "got-isVisible-response":
-        isSidebarLoaded = true;
-        maybeCallback();
-        break;
-      case "test-init-done":
-        if (isSidebarLoaded)
-          maybeCallback();
-        else
-          port.postMessage({topic: "test-isVisible"});
-        break;
-    }
-  }
-  port.postMessage({topic: "test-init"});
-}
-
 var manifest = { // normal provider
   name: "provider 1",
   origin: "https://example.com",
   sidebarURL: "https://example.com/browser/browser/base/content/test/social/social_sidebar.html",
-  workerURL: "https://example.com/browser/browser/base/content/test/social/social_worker.js",
   iconURL: "https://example.com/browser/browser/base/content/test/general/moz.png"
 };
 
@@ -103,58 +42,17 @@ function test() {
     runSocialTestWithProvider(manifest, function (finishcb) {
       SocialSidebar.show();
       runSocialTests(tests, preSubTest, postSubTest, function () {
-        finishcb();
+        ensureBrowserTabClosed(tab).then(finishcb);
       });
     });
   }, true);
-  registerCleanupFunction(function() {
-    gBrowser.removeTab(tab);
-  });
-
 }
 
 var tests = {
-  // In this test the worker asks the sidebar to open a chat.  As that means
-  // we aren't handling user-input we will not focus the chatbar.
-  // Then we do it again - should still not be focused.
-  // Then we perform a user-initiated request - it should get focus.
-  testNoFocusWhenViaWorker: function(next) {
-    let chatbar = getChatBar();
-    startTestAndWaitForSidebar(function(port) {
-      openChatViaSidebarMessage(port, {stealFocus: 1}, function() {
-        ok(true, "got chatbox message");
-        is(chatbar.childElementCount, 1, "exactly 1 chat open");
-
-        let browser = gBrowser.selectedTab.linkedBrowser;
-        ContentTask.spawn(browser, null, function* () {
-          is(Services.focus.focusedWindow, content, "tab should still be focused");
-        }).then(() => {
-          // re-request the same chat via a message.
-          openChatViaSidebarMessage(port, {stealFocus: 1}, function() {
-            is(chatbar.childElementCount, 1, "still exactly 1 chat open");
-
-            ContentTask.spawn(browser, null, function* () {
-              is(Services.focus.focusedWindow, content, "tab should still be focused");
-            }).then(() => {
-              // re-request the same chat via user event.
-              openChatViaUser();
-              waitForCondition(() => isChatFocused(chatbar.selectedChat), function() {
-                is(chatbar.childElementCount, 1, "still exactly 1 chat open");
-                is(chatbar.selectedChat, chatbar.firstElementChild,
-                  "chat should be selected");
-                next();
-              }, "chat should be focused");
-            });
-          });
-        });
-      });
-    });
-  },
-
   // In this test we arrange for the sidebar to open the chat via a simulated
   // click.  This should cause the new chat to be opened and focused.
   testFocusWhenViaUser: function(next) {
-    startTestAndWaitForSidebar(function(port) {
+    ensureFrameLoaded(document.getElementById("social-sidebar-browser")).then(() => {
       let chatbar = getChatBar();
       openChatViaUser();
       ok(chatbar.firstElementChild, "chat opened");
