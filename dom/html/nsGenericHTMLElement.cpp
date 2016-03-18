a224a91384596f750de91ea541cdabb1871acb49
Ting-Yu Lin <tlin@mozilla.com>
2016-03-18 13:47:50 +0800

    Bug 1249556 - Rename override to disallowOverridingFocusability. r=bz
    Rename 'override' to 'disallowOverridingFocusability' in
    nsGenericHTMLElement::IsHTMLFocusable() to make the implementation
    reflect the semantic that the subclass is disallow to override the value
    returned in aIsFocusable described in nsGenericHTMLElement.h.
    
    Also 'override' is a new specifier since C++11. Rename it make the
    syntax highlight looks right.
    
    MozReview-Commit-ID: CShdChjBv7j

--- dom/html/nsGenericHTMLElement.cpp
+++ dom/html/nsGenericHTMLElement.cpp
@@ -2722,11 +2722,12 @@ nsGenericHTMLElement::IsHTMLFocusable(bool aWithMouse,
   }
 
   int32_t tabIndex = TabIndex();
+  bool disabled = false;
+  bool disallowOverridingFocusability = true;
 
-  bool override, disabled = false;
   if (IsEditableRoot()) {
     // Editable roots should always be focusable.
-    override = true;
+    disallowOverridingFocusability = true;
 
     // Ignore the disabled attribute in editable contentEditable/designMode
     // roots.
@@ -2737,7 +2738,7 @@ nsGenericHTMLElement::IsHTMLFocusable(bool aWithMouse,
     }
   }
   else {
-    override = false;
+    disallowOverridingFocusability = false;
 
     // Just check for disabled attribute on form controls
     disabled = IsDisabled();
@@ -2751,10 +2752,10 @@ nsGenericHTMLElement::IsHTMLFocusable(bool aWithMouse,
   }
 
   // If a tabindex is specified at all, or the default tabindex is 0, we're focusable
-  *aIsFocusable = 
+  *aIsFocusable =
     (tabIndex >= 0 || (!disabled && HasAttr(kNameSpaceID_None, nsGkAtoms::tabindex)));
 
-  return override;
+  return disallowOverridingFocusability;
 }
 
 void
