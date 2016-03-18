5b5e0151d62aa88d193ca73bea1d8476c5b02477
Alexandre Poirot <poirot.alex@gmail.com>
2016-03-18 02:44:22 -0700

    Bug 1249447 - Wait correctly for all storage creation/deletion in browser_storage_dynamic_windows.js. r=mratcliffe

--- devtools/server/tests/browser/browser_storage_dynamic_windows.js
+++ devtools/server/tests/browser/browser_storage_dynamic_windows.js
@@ -148,6 +148,9 @@ function testAddIframe(front) {
         // empty because indexed db creation happens after the page load, so at
         // the time of window-ready, there was no indexed db present.
         "https://sectest1.example.org": []
+      },
+      Cache: {
+        "https://sectest1.example.org":[]
       }
     };
 
@@ -209,6 +212,12 @@ function testRemoveIframe(front) {
         "http://sectest1.example.org": []
       },
       sessionStorage: {
+        "http://sectest1.example.org": []
+      },
+      Cache: {
+        "http://sectest1.example.org": []
+      },
+      indexedDB: {
         "http://sectest1.example.org": []
       }
     };
