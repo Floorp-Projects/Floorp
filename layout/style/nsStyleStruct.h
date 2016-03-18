8d3fbe017791146099c5b83a79f4e1db268e6e35
Wes Kocher <wkocher@mozilla.com>
2016-03-18 15:48:00 -0700

    Backed out 3 changesets (bug 1209273) because something from this push appears to have broken Windows mochitests CLOSED TREE
    Backed out changeset 9c6650c7e237 (bug 1209273)
    Backed out changeset fb8b3ab41c82 (bug 1209273)
    Backed out changeset 6e2e6b0c7d8e (bug 1209273)
    
    MozReview-Commit-ID: J9WSL2CjoGJ

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -2063,8 +2063,7 @@ struct nsStyleVisibility
 
   nsChangeHint CalcDifference(const nsStyleVisibility& aOther) const;
   static nsChangeHint MaxDifference() {
-    return NS_STYLE_HINT_FRAMECHANGE |
-           nsChangeHint_NeutralChange;
+    return NS_STYLE_HINT_FRAMECHANGE;
   }
   static nsChangeHint DifferenceAlwaysHandledForDescendants() {
     // CalcDifference never returns the reflow hints that are sometimes
@@ -2080,7 +2079,6 @@ struct nsStyleVisibility
   uint8_t mPointerEvents;              // [inherited] see nsStyleConsts.h
   uint8_t mWritingMode;                // [inherited] see nsStyleConsts.h
   uint8_t mTextOrientation;            // [inherited] see nsStyleConsts.h
-  uint8_t mColorAdjust;                // [inherited] see nsStyleConsts.h
 
   bool IsVisible() const {
     return (mVisible == NS_STYLE_VISIBILITY_VISIBLE);
