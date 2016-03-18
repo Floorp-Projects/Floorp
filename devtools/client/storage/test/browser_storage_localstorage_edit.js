59c75885f7b849d40e5769bb0fb685279ededc7d
Michael Ratcliffe <mratcliffe@mozilla.com>
2016-03-18 17:09:52 +0000

    Bug 1231155 - Make localstorage entry rows editable via double-click in storage inspector r=pbro
    MozReview-Commit-ID: 9SjhR1n5AYG
    
    --HG--
    extra : rebase_source : 1ca3cb5f79a3420a3d0735ca4558e1f90eeff6b2

---
+++ devtools/client/storage/test/browser_storage_localstorage_edit.js
@@ -0,0 +1,25 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Basic test to check the editing of localStorage.
+
+"use strict";
+
+add_task(function*() {
+  yield openTabAndSetupStorage(MAIN_DOMAIN + "storage-localstorage.html");
+
+  yield selectTreeItem(["localStorage", "http://test1.example.org"]);
+  yield gUI.table.once(TableWidget.EVENTS.FIELDS_EDITABLE);
+
+  yield editCell("TestLS1", "name", "newTestLS1");
+  yield editCell("newTestLS1", "value", "newValueLS1");
+
+  yield editCell("TestLS3", "name", "newTestLS3");
+  yield editCell("newTestLS3", "value", "newValueLS3");
+
+  yield editCell("TestLS5", "name", "newTestLS5");
+  yield editCell("newTestLS5", "value", "newValueLS5");
+
+  yield finishTests();
+});
