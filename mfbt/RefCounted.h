dc4836b340b98b505b069617260f387f4a507349
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-03-18 07:59:17 +0100

    Backed out changeset 516d8328dcb6 (bug 1251593) for test tree bustage on a CLOSED TREE

--- mfbt/RefCounted.h
+++ mfbt/RefCounted.h
@@ -26,15 +26,6 @@
 #define MOZ_REFCOUNTED_LEAK_CHECKING
 #endif
 
-#if defined(MOZILLA_INTERNAL_API) && \
-    defined(DEBUG)
-#define MOZ_REFCOUNTED_THREAD_CHECKING
-#endif
-
-#ifdef MOZ_REFCOUNTED_THREAD_CHECKING
-#include "nsISupports.h"
-#endif
-
 namespace mozilla {
 
 /**
@@ -106,10 +97,6 @@ public:
   // Compatibility with nsRefPtr.
   void AddRef() const
   {
-#ifdef MOZ_REFCOUNTED_THREAD_CHECKING
-    RefCountThreadCheck(IntegralConstant<RefCountAtomicity, Atomicity>());
-#endif
-
     // Note: this method must be thread safe for AtomicRefCounted.
     MOZ_ASSERT(int32_t(mRefCnt) >= 0);
 #ifndef MOZ_REFCOUNTED_LEAK_CHECKING
@@ -125,10 +112,6 @@ public:
 
   void Release() const
   {
-#ifdef MOZ_REFCOUNTED_THREAD_CHECKING
-    RefCountThreadCheck(IntegralConstant<RefCountAtomicity, Atomicity>());
-#endif
-
     // Note: this method must be thread safe for AtomicRefCounted.
     MOZ_ASSERT(int32_t(mRefCnt) > 0);
 #ifndef MOZ_REFCOUNTED_LEAK_CHECKING
@@ -167,20 +150,6 @@ private:
   mutable typename Conditional<Atomicity == AtomicRefCount,
                                Atomic<MozRefCountType>,
                                MozRefCountType>::Type mRefCnt;
-
-#ifdef MOZ_REFCOUNTED_THREAD_CHECKING
-  void RefCountThreadCheck(IntegralConstant<RefCountAtomicity, AtomicRefCount>) const
-  {
-    // no need to check the thread if we use AtomicRefCount.
-  }
-
-  void RefCountThreadCheck(IntegralConstant<RefCountAtomicity, NonAtomicRefCount>) const
-  {
-    NS_ASSERT_OWNINGTHREAD(RefCounted);
-  }
-
-  NS_DECL_OWNINGTHREAD;
-#endif
 };
 
 #ifdef MOZ_REFCOUNTED_LEAK_CHECKING
