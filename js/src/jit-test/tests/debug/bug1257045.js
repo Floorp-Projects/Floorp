3854be2e30c31fad96f0d4d73990b7a1cde93ba5
Shu-yu Guo <shu@rfrn.org>
2016-03-17 18:53:04 -0700

    Bug 1257045 - Give Debugger wrappers of wasm code their own CCW key kinds. (r=jimb)

---
+++ js/src/jit-test/tests/debug/bug1257045.js
@@ -0,0 +1,8 @@
+fullcompartmentchecks(true);
+var g = newGlobal();
+var dbg = new Debugger(g);
+dbg.onNewScript = (function(script) {
+    s = script;
+})
+g.eval(`Wasm.instantiateModule(wasmTextToBinary('(module (func) (export "" 0))'));`);
+s.source;
