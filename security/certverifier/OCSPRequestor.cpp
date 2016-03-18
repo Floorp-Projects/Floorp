4d51669eb3bb9f1126d969b212c8692af04d5be5
Wes Kocher <wkocher@mozilla.com>
2016-03-18 11:07:55 -0700

    Backed out 3 changesets (bug 1004149) for Windows build failures in CertVerifier.cpp
    Backed out changeset e14e69dd0ff6 (bug 1004149)
    Backed out changeset 7c6888880270 (bug 1004149)
    Backed out changeset f164ff25450e (bug 1004149)
    
    MozReview-Commit-ID: GIbe3devuSe

--- security/certverifier/OCSPRequestor.cpp
+++ security/certverifier/OCSPRequestor.cpp
@@ -70,23 +70,25 @@ AppendEscapedBase64Item(const SECItem* encodedRequest, nsACString& path)
   return NS_OK;
 }
 
-Result
+SECItem*
 DoOCSPRequest(PLArenaPool* arena, const char* url,
               const SECItem* encodedRequest, PRIntervalTime timeout,
-              bool useGET,
-      /*out*/ SECItem*& encodedResponse)
+              bool useGET)
 {
   if (!arena || !url || !encodedRequest || !encodedRequest->data) {
-    return Result::FATAL_ERROR_INVALID_ARGS;
+    PR_SetError(SEC_ERROR_INVALID_ARGS, 0);
+    return nullptr;
   }
   uint32_t urlLen = PL_strlen(url);
   if (urlLen > static_cast<uint32_t>(std::numeric_limits<int32_t>::max())) {
-    return Result::FATAL_ERROR_INVALID_ARGS;
+    PR_SetError(SEC_ERROR_INVALID_ARGS, 0);
+    return nullptr;
   }
 
   nsCOMPtr<nsIURLParser> urlParser = do_GetService(NS_STDURLPARSER_CONTRACTID);
   if (!urlParser) {
-    return Result::FATAL_ERROR_LIBRARY_FAILURE;
+    PR_SetError(SEC_ERROR_LIBRARY_FAILURE, 0);
+    return nullptr;
   }
 
   uint32_t schemePos;
@@ -95,52 +97,58 @@ DoOCSPRequest(PLArenaPool* arena, const char* url,
   int32_t authorityLen;
   uint32_t pathPos;
   int32_t pathLen;
-  nsresult nsrv = urlParser->ParseURL(url, static_cast<int32_t>(urlLen),
-                                      &schemePos, &schemeLen,
-                                      &authorityPos, &authorityLen,
-                                      &pathPos, &pathLen);
-  if (NS_FAILED(nsrv)) {
-    return Result::ERROR_CERT_BAD_ACCESS_LOCATION;
+  nsresult rv = urlParser->ParseURL(url, static_cast<int32_t>(urlLen),
+                                    &schemePos, &schemeLen,
+                                    &authorityPos, &authorityLen,
+                                    &pathPos, &pathLen);
+  if (NS_FAILED(rv)) {
+    PR_SetError(SEC_ERROR_CERT_BAD_ACCESS_LOCATION, 0);
+    return nullptr;
   }
   if (schemeLen < 0 || authorityLen < 0) {
-    return Result::ERROR_CERT_BAD_ACCESS_LOCATION;
+    PR_SetError(SEC_ERROR_CERT_BAD_ACCESS_LOCATION, 0);
+    return nullptr;
   }
   nsAutoCString scheme(url + schemePos,
                        static_cast<nsAutoCString::size_type>(schemeLen));
   if (!scheme.LowerCaseEqualsLiteral("http")) {
-    // We don't support HTTPS to avoid loops. See Bug 92923.
-    // We also in general only support HTTP.
-    return Result::ERROR_CERT_BAD_ACCESS_LOCATION;
+    // We dont support https:// to avoid loops see Bug 92923
+    PR_SetError(SEC_ERROR_CERT_BAD_ACCESS_LOCATION, 0);
+    return nullptr;
   }
 
   uint32_t hostnamePos;
   int32_t hostnameLen;
   int32_t port;
-  // We ignore user:password sections: if one is present, we send an OCSP
-  // request to the URL as normal without sending the username or password.
-  nsrv = urlParser->ParseAuthority(url + authorityPos, authorityLen,
-                                   nullptr, nullptr, nullptr, nullptr,
-                                   &hostnamePos, &hostnameLen, &port);
-  if (NS_FAILED(nsrv)) {
-    return Result::ERROR_CERT_BAD_ACCESS_LOCATION;
+  // We do not support urls with user@pass sections in the URL,
+  // In cas we find them we will ignore and try to connect with
+  rv = urlParser->ParseAuthority(url + authorityPos, authorityLen,
+                                 nullptr, nullptr, nullptr, nullptr,
+                                 &hostnamePos, &hostnameLen, &port);
+  if (NS_FAILED(rv)) {
+    PR_SetError(SEC_ERROR_CERT_BAD_ACCESS_LOCATION, 0);
+    return nullptr;
   }
   if (hostnameLen < 0) {
-    return Result::ERROR_CERT_BAD_ACCESS_LOCATION;
+    PR_SetError(SEC_ERROR_CERT_BAD_ACCESS_LOCATION, 0);
+    return nullptr;
   }
   if (port == -1) {
     port = 80;
   } else if (port < 0 || port > 0xffff) {
-    return Result::ERROR_CERT_BAD_ACCESS_LOCATION;
+    PR_SetError(SEC_ERROR_CERT_BAD_ACCESS_LOCATION, 0);
+    return nullptr;
   }
   nsAutoCString
     hostname(url + authorityPos + hostnamePos,
              static_cast<nsACString_internal::size_type>(hostnameLen));
 
   SEC_HTTP_SERVER_SESSION serverSessionPtr = nullptr;
-  Result rv = nsNSSHttpInterface::createSessionFcn(
-    hostname.BeginReading(), static_cast<uint16_t>(port), &serverSessionPtr);
-  if (rv != Success) {
-    return rv;
+  if (nsNSSHttpInterface::createSessionFcn(hostname.BeginReading(),
+                                           static_cast<uint16_t>(port),
+                                           &serverSessionPtr) != SECSuccess) {
+    PR_SetError(SEC_ERROR_NO_MEMORY, 0);
+    return nullptr;
   }
   ScopedHTTPServerSession serverSession(
     reinterpret_cast<nsNSSHttpServerSession*>(serverSessionPtr));
@@ -160,53 +168,59 @@ DoOCSPRequest(PLArenaPool* arena, const char* url,
     if (!StringEndsWith(path, NS_LITERAL_CSTRING("/"))) {
       path.Append("/");
     }
-    nsresult nsrv = AppendEscapedBase64Item(encodedRequest, path);
-    if (NS_WARN_IF(NS_FAILED(nsrv))) {
-      return Result::FATAL_ERROR_LIBRARY_FAILURE;
+    nsresult rv = AppendEscapedBase64Item(encodedRequest, path);
+    if (NS_WARN_IF(NS_FAILED(rv))) {
+      return nullptr;
     }
   }
 
   SEC_HTTP_REQUEST_SESSION requestSessionPtr;
-  rv = nsNSSHttpInterface::createFcn(serverSession.get(), "http", path.get(),
-                                     method.get(), timeout, &requestSessionPtr);
-  if (rv != Success) {
-    return rv;
+  if (nsNSSHttpInterface::createFcn(serverSession.get(), "http",
+                                    path.get(), method.get(),
+                                    timeout, &requestSessionPtr)
+        != SECSuccess) {
+    PR_SetError(SEC_ERROR_NO_MEMORY, 0);
+    return nullptr;
   }
 
   ScopedHTTPRequestSession requestSession(
     reinterpret_cast<nsNSSHttpRequestSession*>(requestSessionPtr));
 
   if (!useGET) {
-    rv = nsNSSHttpInterface::setPostDataFcn(
-      requestSession.get(), reinterpret_cast<char*>(encodedRequest->data),
-      encodedRequest->len, "application/ocsp-request");
-    if (rv != Success) {
-      return rv;
+    if (nsNSSHttpInterface::setPostDataFcn(requestSession.get(),
+          reinterpret_cast<char*>(encodedRequest->data), encodedRequest->len,
+          "application/ocsp-request") != SECSuccess) {
+      PR_SetError(SEC_ERROR_NO_MEMORY, 0);
+      return nullptr;
     }
   }
 
   uint16_t httpResponseCode;
   const char* httpResponseData;
   uint32_t httpResponseDataLen = 0; // 0 means any response size is acceptable
-  rv = nsNSSHttpInterface::trySendAndReceiveFcn(requestSession.get(), nullptr,
-                                                &httpResponseCode, nullptr,
-                                                nullptr, &httpResponseData,
-                                                &httpResponseDataLen);
-  if (rv != Success) {
-    return rv;
+  if (nsNSSHttpInterface::trySendAndReceiveFcn(requestSession.get(), nullptr,
+                                               &httpResponseCode, nullptr,
+                                               nullptr, &httpResponseData,
+                                               &httpResponseDataLen)
+        != SECSuccess) {
+    PR_SetError(SEC_ERROR_OCSP_SERVER_ERROR, 0);
+    return nullptr;
   }
 
   if (httpResponseCode != 200) {
-    return Result::ERROR_OCSP_SERVER_ERROR;
+    PR_SetError(SEC_ERROR_OCSP_SERVER_ERROR, 0);
+    return nullptr;
   }
 
-  encodedResponse = SECITEM_AllocItem(arena, nullptr, httpResponseDataLen);
+  SECItem* encodedResponse = SECITEM_AllocItem(arena, nullptr,
+                                               httpResponseDataLen);
   if (!encodedResponse) {
-    return Result::FATAL_ERROR_NO_MEMORY;
+    PR_SetError(SEC_ERROR_NO_MEMORY, 0);
+    return nullptr;
   }
 
   memcpy(encodedResponse->data, httpResponseData, httpResponseDataLen);
-  return Success;
+  return encodedResponse;
 }
 
 } } // namespace mozilla::psm
--- security/manager/ssl/nsNSSCallbacks.h
+++ security/manager/ssl/nsNSSCallbacks.h
@@ -4,21 +4,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef nsNSSCallbacks_h
-#define nsNSSCallbacks_h
+#ifndef _NSNSSCALLBACKS_H_
+#define _NSNSSCALLBACKS_H_
 
-#include "mozilla/Attributes.h"
-#include "mozilla/CondVar.h"
-#include "mozilla/Mutex.h"
 #include "nsAutoPtr.h"
 #include "nsCOMPtr.h"
-#include "nsIStreamLoader.h"
-#include "nspr.h"
-#include "nsString.h"
 #include "pk11func.h"
-#include "pkix/pkixtypes.h"
-
-#include "ocspt.h" // Must be included after pk11func.h.
+#include "nspr.h"
+#include "ocspt.h"
+#include "nsIStreamLoader.h"
+#include "mozilla/CondVar.h"
+#include "mozilla/Mutex.h"
+#include "mozilla/Attributes.h"
+#include "nsString.h"
 
 class nsILoadGroup;
 
@@ -28,8 +26,6 @@ PK11PasswordPrompt(PK11SlotInfo *slot, PRBool retry, void* arg);
 void HandshakeCallback(PRFileDesc *fd, void *client_data);
 SECStatus CanFalseStartCallback(PRFileDesc* fd, void* client_data,
                                 PRBool *canFalseStart);
-
-typedef mozilla::pkix::Result Result;
 
 class nsHTTPListener final : public nsIStreamLoaderObserver
 {
@@ -59,11 +55,11 @@ public:
 
   const uint8_t* mResultData; // allocated in loader, but owned by listener
   uint32_t mResultLen;
-
+  
   mozilla::Mutex mLock;
   mozilla::CondVar mCondition;
   volatile bool mWaitFlag;
-
+  
   bool mResponsibleForDoneSignal;
   void send_done_signal();
 
@@ -80,11 +76,11 @@ class nsNSSHttpServerSession
 {
 public:
   nsCString mHost;
-  uint16_t mPort;
+  uint16_t mPort;  
 
-  static Result createSessionFcn(const char* host,
-                                 uint16_t portnum,
-                                 SEC_HTTP_SERVER_SESSION* pSession);
+  static SECStatus createSessionFcn(const char *host,
+                                    uint16_t portnum,
+                                    SEC_HTTP_SERVER_SESSION *pSession);
 };
 
 class nsNSSHttpRequestSession
@@ -93,96 +89,137 @@ protected:
   mozilla::ThreadSafeAutoRefCnt mRefCount;
 
 public:
-  static Result createFcn(SEC_HTTP_SERVER_SESSION session,
-                          const char* httpProtocolVariant,
-                          const char* pathAndQueryString,
-                          const char* httpRequestMethod,
-                          const PRIntervalTime timeout,
-                          SEC_HTTP_REQUEST_SESSION* pRequest);
+  static SECStatus createFcn(SEC_HTTP_SERVER_SESSION session,
+                             const char *http_protocol_variant,
+                             const char *path_and_query_string,
+                             const char *http_request_method, 
+                             const PRIntervalTime timeout, 
+                             SEC_HTTP_REQUEST_SESSION *pRequest);
 
-  Result setPostDataFcn(const char* httpData,
-                        const uint32_t httpDataLen,
-                        const char* httpContentType);
+  SECStatus setPostDataFcn(const char *http_data, 
+                           const uint32_t http_data_len,
+                           const char *http_content_type);
 
-  Result trySendAndReceiveFcn(PRPollDesc** pPollDesc,
-                              uint16_t* httpResponseCode,
-                              const char** httpResponseContentType,
-                              const char** httpResponseHeaders,
-                              const char** httpResponseData,
-                              uint32_t* httpResponseDataLen);
+  SECStatus addHeaderFcn(const char *http_header_name, 
+                         const char *http_header_value);
+
+  SECStatus trySendAndReceiveFcn(PRPollDesc **pPollDesc,
+                                 uint16_t *http_response_code, 
+                                 const char **http_response_content_type, 
+                                 const char **http_response_headers, 
+                                 const char **http_response_data, 
+                                 uint32_t *http_response_data_len);
+
+  SECStatus cancelFcn();
+  SECStatus freeFcn();
 
   void AddRef();
   void Release();
 
   nsCString mURL;
   nsCString mRequestMethod;
-
+  
   bool mHasPostData;
   nsCString mPostData;
   nsCString mPostContentType;
-
+  
   PRIntervalTime mTimeoutInterval;
-
+  
   RefPtr<nsHTTPListener> mListener;
-
+  
 protected:
   nsNSSHttpRequestSession();
   ~nsNSSHttpRequestSession();
 
-  Result internal_send_receive_attempt(bool& retryableError,
-                                       PRPollDesc** pPollDesc,
-                                       uint16_t* httpResponseCode,
-                                       const char** httpResponseContentType,
-                                       const char** httpResponseHeaders,
-                                       const char** httpResponseData,
-                                       uint32_t* httpResponseDataLen);
+  SECStatus internal_send_receive_attempt(bool &retryable_error,
+                                          PRPollDesc **pPollDesc,
+                                          uint16_t *http_response_code,
+                                          const char **http_response_content_type,
+                                          const char **http_response_headers,
+                                          const char **http_response_data,
+                                          uint32_t *http_response_data_len);
 };
 
 class nsNSSHttpInterface
 {
 public:
-  static Result createSessionFcn(const char* host,
-                                 uint16_t portnum,
-                                 SEC_HTTP_SERVER_SESSION* pSession)
+  static SECStatus createSessionFcn(const char *host,
+                                    uint16_t portnum,
+                                    SEC_HTTP_SERVER_SESSION *pSession)
   {
     return nsNSSHttpServerSession::createSessionFcn(host, portnum, pSession);
   }
 
-  static Result createFcn(SEC_HTTP_SERVER_SESSION session,
-                          const char* httpProtocolVariant,
-                          const char* pathAndQueryString,
-                          const char* httpRequestMethod,
-                          const PRIntervalTime timeout,
-                          SEC_HTTP_REQUEST_SESSION* pRequest)
+  static SECStatus keepAliveFcn(SEC_HTTP_SERVER_SESSION session,
+                                PRPollDesc **pPollDesc)
   {
-    return nsNSSHttpRequestSession::createFcn(session, httpProtocolVariant,
-                                              pathAndQueryString,
-                                              httpRequestMethod, timeout,
-                                              pRequest);
+    // Not yet implemented, however, Necko does transparent keep-alive 
+    // anyway, when enabled in Necko's prefs.
+    return SECSuccess;
   }
 
-  static Result setPostDataFcn(SEC_HTTP_REQUEST_SESSION request,
-                               const char* httpData,
-                               const uint32_t httpDataLen,
-                               const char* httpContentType)
+  static SECStatus freeSessionFcn(SEC_HTTP_SERVER_SESSION session)
   {
-    return static_cast<nsNSSHttpRequestSession*>(request)
-      ->setPostDataFcn(httpData, httpDataLen, httpContentType);
+    delete static_cast<nsNSSHttpServerSession*>(session);
+    return SECSuccess;
   }
 
-  static Result trySendAndReceiveFcn(SEC_HTTP_REQUEST_SESSION request,
-                                     PRPollDesc** pPollDesc,
-                                     uint16_t* httpResponseCode,
-                                     const char** httpResponseContentType,
-                                     const char** httpResponseHeaders,
-                                     const char** httpResponseData,
-                                     uint32_t* httpResponseDataLen)
+  static SECStatus createFcn(SEC_HTTP_SERVER_SESSION session,
+                             const char *http_protocol_variant,
+                             const char *path_and_query_string,
+                             const char *http_request_method, 
+                             const PRIntervalTime timeout, 
+                             SEC_HTTP_REQUEST_SESSION *pRequest)
+  {
+    return nsNSSHttpRequestSession::createFcn(session, http_protocol_variant,
+                                     path_and_query_string, http_request_method, 
+                                     timeout, pRequest);
+  }
+
+  static SECStatus setPostDataFcn(SEC_HTTP_REQUEST_SESSION request, 
+                                  const char *http_data, 
+                                  const uint32_t http_data_len,
+                                  const char *http_content_type)
   {
     return static_cast<nsNSSHttpRequestSession*>(request)
-      ->trySendAndReceiveFcn(pPollDesc, httpResponseCode,
-                             httpResponseContentType, httpResponseHeaders,
-                             httpResponseData, httpResponseDataLen);
+            ->setPostDataFcn(http_data, http_data_len, http_content_type);
   }
+
+  static SECStatus addHeaderFcn(SEC_HTTP_REQUEST_SESSION request,
+                                const char *http_header_name, 
+                                const char *http_header_value)
+  {
+    return static_cast<nsNSSHttpRequestSession*>(request)
+            ->addHeaderFcn(http_header_name, http_header_value);
+  }
+
+  static SECStatus trySendAndReceiveFcn(SEC_HTTP_REQUEST_SESSION request, 
+                                        PRPollDesc **pPollDesc,
+                                        uint16_t *http_response_code, 
+                                        const char **http_response_content_type, 
+                                        const char **http_response_headers, 
+                                        const char **http_response_data, 
+                                        uint32_t *http_response_data_len)
+  {
+    return static_cast<nsNSSHttpRequestSession*>(request)
+            ->trySendAndReceiveFcn(pPollDesc, http_response_code, http_response_content_type, 
+                     http_response_headers, http_response_data, http_response_data_len);
+  }
+
+  static SECStatus cancelFcn(SEC_HTTP_REQUEST_SESSION request)
+  {
+    return static_cast<nsNSSHttpRequestSession*>(request)
+            ->cancelFcn();
+  }
+
+  static SECStatus freeFcn(SEC_HTTP_REQUEST_SESSION request)
+  {
+    return static_cast<nsNSSHttpRequestSession*>(request)
+            ->freeFcn();
+  }
+
+  static void initTable();
+  static SEC_HttpClientFcn sNSSInterfaceTable;
 };
 
-#endif // nsNSSCallbacks_h
+#endif // _NSNSSCALLBACKS_H_
