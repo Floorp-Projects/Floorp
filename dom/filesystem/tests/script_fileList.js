13a558b1317d5ad72019219075df0348622d2139
Andrea Marchesini <amarchesini@mozilla.com>
2016-03-19 22:30:24 +0100

    Bug 1258137 - OSFileSystem should not be kept alive by more than 1 Directory, r=smaug

--- dom/filesystem/tests/script_fileList.js
+++ dom/filesystem/tests/script_fileList.js
@@ -1,18 +1,6 @@
 var { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 Cu.importGlobalProperties(["File"]);
 
-addMessageListener("file.open", function () {
-  var testFile = Cc["@mozilla.org/file/directory_service;1"]
-                   .getService(Ci.nsIDirectoryService)
-                   .QueryInterface(Ci.nsIProperties)
-                   .get("ProfD", Ci.nsIFile);
-  testFile.append("prefs.js");
-
-  sendAsyncMessage("file.opened", {
-    file: new File(testFile)
-  });
-});
-
 addMessageListener("dir.open", function () {
   var testFile = Cc["@mozilla.org/file/directory_service;1"]
                    .getService(Ci.nsIDirectoryService)
