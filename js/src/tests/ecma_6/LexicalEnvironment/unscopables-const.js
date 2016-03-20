79d19216820a094694e8f48c34a719c799f1dd51
Shu-yu Guo <shu@rfrn.org>
2016-03-19 19:18:12 -0700

    Backed out changesets b61921a307e9 and e9e74f6bd12a (bug 1054759) for breaking web compat by implementing Symbol.unscopables without Array.prototype[@@unscopables].

--- js/src/tests/ecma_6/LexicalEnvironment/unscopables-const.js
+++
@@ -1,8 +0,0 @@
-// @@unscopables prevents a property from having any effect on assigning to a
-// const binding (which is an error).
-
-const x = 1;
-with ({x: 1, [Symbol.unscopables]: {x: true}})
-    assertThrowsInstanceOf(() => {x = 2;}, TypeError);
-
-reportCompare(0, 0);
