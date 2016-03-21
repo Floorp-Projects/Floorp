57a397ec81583cf9c6355522d9ba323fae04df1b
Tom Schuster <evilpies@gmail.com>
2016-03-21 10:44:35 -0500

    Bug 875433 - Implement Array.prototype.values. r=jorendorff.

---
+++ js/src/tests/ecma_6/Array/values.js
@@ -0,0 +1,18 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/licenses/publicdomain/ */
+
+assertEq(Array.prototype.values, Array.prototype[Symbol.iterator]);
+assertEq(Array.prototype.values.name, "values");
+assertEq(Array.prototype.values.length, 0);
+
+function valuesUnscopeable() {
+    var values = "foo";
+    with ([1, 2, 3]) {
+        assertEq(indexOf, Array.prototype.indexOf);
+        assertEq(values, "foo");
+    }
+}
+valuesUnscopeable();
+
+if (typeof reportCompare === 'function')
+    reportCompare(0, 0);
