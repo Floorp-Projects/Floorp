75335c5e4b25a1049704c64eae11c022fe432901
Jan Odvarko <odvarko@gmail.com>
2016-03-21 13:29:17 +0100

    Bug 1256757 - Support AMD in tree modules. r=jryans

--- devtools/client/shared/components/tree/object-provider.js
+++ devtools/client/shared/components/tree/object-provider.js
@@ -5,83 +5,86 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-/**
- * Implementation of the default data provider. A provider is state less
- * object responsible for transformation data (usually a state) to
- * a structure that can be directly consumed by the tree-view component.
- */
-var ObjectProvider = {
-  getChildren: function(object) {
-    let children = [];
+// Make this available to both AMD and CJS environments
+define(function(require, exports, module) {
+  /**
+   * Implementation of the default data provider. A provider is state less
+   * object responsible for transformation data (usually a state) to
+   * a structure that can be directly consumed by the tree-view component.
+   */
+  let ObjectProvider = {
+    getChildren: function(object) {
+      let children = [];
 
-    if (object instanceof ObjectProperty) {
-      object = object.value;
-    }
-
-    if (!object) {
-      return [];
-    }
-
-    if (typeof (object) == "string") {
-      return [];
-    }
-
-    for (let prop in object) {
-      try {
-        children.push(new ObjectProperty(prop, object[prop]));
-      } catch (e) {
-        console.error(e);
+      if (object instanceof ObjectProperty) {
+        object = object.value;
       }
+
+      if (!object) {
+        return [];
+      }
+
+      if (typeof (object) == "string") {
+        return [];
+      }
+
+      for (let prop in object) {
+        try {
+          children.push(new ObjectProperty(prop, object[prop]));
+        } catch (e) {
+          console.error(e);
+        }
+      }
+      return children;
+    },
+
+    hasChildren: function(object) {
+      if (object instanceof ObjectProperty) {
+        object = object.value;
+      }
+
+      if (!object) {
+        return false;
+      }
+
+      if (typeof object == "string") {
+        return false;
+      }
+
+      if (typeof object !== "object") {
+        return false;
+      }
+
+      return Object.keys(object).length > 1;
+    },
+
+    getLabel: function(object) {
+      return (object instanceof ObjectProperty) ?
+        object.name : null;
+    },
+
+    getValue: function(object) {
+      return (object instanceof ObjectProperty) ?
+        object.value : null;
+    },
+
+    getKey: function(object) {
+      return (object instanceof ObjectProperty) ?
+        object.name : null;
+    },
+
+    getType: function(object) {
+      return (object instanceof ObjectProperty) ?
+        typeof object.value : typeof object;
     }
-    return children;
-  },
+  };
 
-  hasChildren: function(object) {
-    if (object instanceof ObjectProperty) {
-      object = object.value;
-    }
-
-    if (!object) {
-      return false;
-    }
-
-    if (typeof object == "string") {
-      return false;
-    }
-
-    if (typeof object !== "object") {
-      return false;
-    }
-
-    return Object.keys(object).length > 1;
-  },
-
-  getLabel: function(object) {
-    return (object instanceof ObjectProperty) ?
-      object.name : null;
-  },
-
-  getValue: function(object) {
-    return (object instanceof ObjectProperty) ?
-      object.value : null;
-  },
-
-  getKey: function(object) {
-    return (object instanceof ObjectProperty) ?
-      object.name : null;
-  },
-
-  getType: function(object) {
-    return (object instanceof ObjectProperty) ?
-      typeof object.value : typeof object;
+  function ObjectProperty(name, value) {
+    this.name = name;
+    this.value = value;
   }
-};
 
-function ObjectProperty(name, value) {
-  this.name = name;
-  this.value = value;
-}
-
-// Exports from this module
-exports.ObjectProperty = ObjectProperty;
-exports.ObjectProvider = ObjectProvider;
+  // Exports from this module
+  exports.ObjectProperty = ObjectProperty;
+  exports.ObjectProvider = ObjectProvider;
+});
