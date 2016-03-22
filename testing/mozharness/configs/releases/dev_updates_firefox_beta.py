4e6b33f7000a41a05983473cffe119ea21e04ec2
Jordan Lund <jlund@mozilla.com>
2016-03-22 10:15:11 -0700

    Bug 1258059 - generify dev configs and env for relpro by removing date strings and allow multiple products, CLOSED TREE DONTBUILD a=testing r=rail
    stub installers with beta-dev, add staging beta single locale, r?rail
    
    MozReview-Commit-ID: 6h1TtzVO1Jp
    MozReview-Commit-ID: Gtf4cJKsAPu
    
    --HG--
    rename : testing/mozharness/configs/releases/postrelease_date.py => testing/mozharness/configs/releases/dev_postrelease_firefox_beta.py
    rename : testing/mozharness/configs/releases/updates_date.py => testing/mozharness/configs/releases/dev_updates_firefox_beta.py
    rename : testing/mozharness/configs/releases/postrelease_beta.py => testing/mozharness/configs/releases/postrelease_firefox_beta.py
    rename : testing/mozharness/configs/releases/updates_beta.py => testing/mozharness/configs/releases/updates_firefox_beta.py
    extra : source : 5440170659bba0f09c915a1af676b93c376100db

--- testing/mozharness/configs/releases/dev_updates_firefox_beta.py
+++ testing/mozharness/configs/releases/dev_updates_firefox_beta.py
@@ -1,6 +1,6 @@
 
 config = {
-    "log_name": "bump_date",
+    "log_name": "bump_beta_dev",
     # TODO: use real repo
     "repo": {
         "repo": "https://hg.mozilla.org/users/raliiev_mozilla.com/tools",
@@ -10,6 +10,7 @@ config = {
     },
     # TODO: use real repo
     "push_dest": "ssh://hg.mozilla.org/users/raliiev_mozilla.com/tools",
+    # date repo used for staging beta
     "shipped-locales-url": "https://hg.mozilla.org/projects/date/raw-file/{revision}/browser/locales/shipped-locales",
     "ignore_no_changes": True,
     "ssh_user": "ffxbld",
@@ -21,14 +22,15 @@ config = {
     "balrog_url": "http://ec2-54-241-39-23.us-west-1.compute.amazonaws.com",
     "balrog_username": "stage-ffxbld",
     "update_channels": {
-        "date": {
+        "beta-dev": {
             "version_regex": r"^(\d+\.\d+(b\d+)?)$",
             "requires_mirrors": True,
+            # TODO - when we use a real repo, rename this file # s/MozDate/MozBeta-dev/
             "patcher_config": "mozDate-branch-patcher2.cfg",
-            "update_verify_channel": "date-localtest",
+            "update_verify_channel": "beta-dev-localtest",
             "mar_channel_ids": [],
-            "channel_names": ["date", "date-localtest", "date-cdntest"],
-            "rules_to_update": ["firefox-date-cdntest", "firefox-date-localtest"],
+            "channel_names": ["beta-dev", "beta-dev-localtest", "beta-dev-cdntest"],
+            "rules_to_update": ["firefox-beta-dev-cdntest", "firefox-beta-dev-localtest"],
         }
     },
     "balrog_use_dummy_suffix": False,
