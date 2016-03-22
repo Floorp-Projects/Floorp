c77ea3284294ed3cd9db79b07c85dc3ec7f43b70
Boris Zbarsky <bzbarsky@mit.edu>
2016-03-22 13:50:31 -0400

    Bug 1257919 part 6.  Make the filename getter on JSStackFrame take an explicit JSContext.  r=khuey

--- security/sandbox/linux/glue/SandboxCrash.cpp
+++ security/sandbox/linux/glue/SandboxCrash.cpp
@@ -45,13 +45,17 @@ SandboxLogJSStack(void)
     return;
   }
   nsCOMPtr<nsIStackFrame> frame = dom::GetCurrentJSStack();
+  // If we got a stack, we must have a current JSContext.  This is icky.  :(
+  // Would be better if GetCurrentJSStack() handed out the JSContext it ended up
+  // using or something.
+  JSContext* cx = frame ? nsContentUtils::GetCurrentJSContext() : nullptr;
   for (int i = 0; frame != nullptr; ++i) {
     nsAutoString fileName, funName;
     int32_t lineNumber;
 
     // Don't stop unwinding if an attribute can't be read.
     fileName.SetIsVoid(true);
-    Unused << frame->GetFilename(fileName);
+    Unused << frame->GetFilename(cx, fileName);
     lineNumber = 0;
     Unused << frame->GetLineNumber(&lineNumber);
     funName.SetIsVoid(true);
