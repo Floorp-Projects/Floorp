33c8db03239cf5271017f0dc754166d06de2b4b9
Daisuke Akatsuka <daisuke@mozilla-japan.org>
2016-03-22 09:33:05 +0900

    Bug 1254761 - Part 2: Removes extra whitespaces. r=birtles

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -1460,7 +1460,7 @@ nsIDocument::nsIDocument()
 {
   SetInDocument();
 
-  PR_INIT_CLIST(&mDOMMediaQueryLists);  
+  PR_INIT_CLIST(&mDOMMediaQueryLists);
 }
 
 // NOTE! nsDocument::operator new() zeroes out all members, so don't
@@ -13184,7 +13184,7 @@ nsDocument::ReportUseCounters()
     for (int32_t c = 0;
          c < eUseCounter_Count; ++c) {
       UseCounter uc = static_cast<UseCounter>(c);
-      
+
       Telemetry::ID id =
         static_cast<Telemetry::ID>(Telemetry::HistogramFirstUseCounter + uc * 2);
       bool value = GetUseCounter(uc);
