992b9146a788bee47c20af971676c6d81db4e43f
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2016-03-22 02:01:33 -0400

    Bug 1258607 - Return promises from some satchel test helpers which use callbacks. r=dolske
    MozReview-Commit-ID: 2glKz8R1XQo

--- toolkit/components/satchel/test/parent_utils.js
+++ toolkit/components/satchel/test/parent_utils.js
@@ -1,4 +1,4 @@
-var { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 
 Cu.import("resource://gre/modules/FormHistory.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
