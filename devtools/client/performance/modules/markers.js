62c6c1746c0da036025a749c29e25c5cab5870c7
Victor Porof <vporof@mozilla.com>
2016-03-22 10:17:21 +0100

    Bug 1258416 - Cleanup marker.js and marker-formatters.js by adding missing l10n strings, removing dead code and updating comments, r=jsantell

--- devtools/client/performance/modules/markers.js
+++ devtools/client/performance/modules/markers.js
@@ -4,7 +4,7 @@
 "use strict";
 
 const { L10N } = require("devtools/client/performance/modules/global");
-const { Formatters } = require("devtools/client/performance/modules/logic/marker-formatters");
+const { Formatters, labelForProperty } = require("devtools/client/performance/modules/marker-formatters");
 
 /**
  * A simple schema for mapping markers to the timeline UI. The keys correspond
@@ -14,42 +14,39 @@ const { Formatters } = require("devtools/client/performance/modules/logic/marker
  *          can be added on the same row. @see <overview.js/buildGraphImage>
  * - label: The label used in the waterfall to identify the marker. Can be a
  *          string or just a function that accepts the marker and returns a
- *          string, if you want to use a dynamic property for the main label.
+ *          string (if you want to use a dynamic property for the main label).
  *          If you use a function for a label, it *must* handle the case where
- *          no marker is provided for a main label to describe all markers of
- *          this type.
+ *          no marker is provided, to get a generic label used to describe
+ *          all markers of this type.
  * - colorName: The label of the DevTools color used for this marker. If
  *              adding a new color, be sure to check that there's an entry
- *              for `.marker-details-bullet.{COLORNAME}` for the equivilent
- *              entry in ./devtools/client/themes/performance.inc.css
+ *              for `.marker-color-graphs-{COLORNAME}` for the equivilent
+ *              entry in "./devtools/client/themes/performance.css"
  *              https://developer.mozilla.org/en-US/docs/Tools/DevToolsColors
  * - collapsible: Whether or not this marker can contain other markers it
- *                eclipses, and becomes collapsible to reveal its nestable children.
- *                Defaults to true.
+ *                eclipses, and becomes collapsible to reveal its nestable
+ *                children. Defaults to true.
  * - nestable: Whether or not this marker can be nested inside an eclipsing
  *             collapsible marker. Defaults to true.
- * - fields: An optional array of marker properties you wish to display in the
- *           marker details view. For example, a field in the array such as
- *           { property: "aCauseName", label: "Cause" } would render a string
- *           like `Cause: ${marker.aCauseName}` in the marker details view.
- *           Each `field` item may take the following properties:
+ * - fields: An optional array of fields you wish to display in the marker
+ *           details view. For example, a field in the array such as
+ *           { label: "Cause", property: "causeName" } would render a string
+ *           like `Cause: ${marker.causeName}` in the marker details view.
+ *           Each field may take the following properties:
+ *           - label: The name of the property that should be displayed.
  *           - property: The property that must exist on the marker to render,
  *                       and the value of the property will be displayed.
- *           - label: The name of the property that should be displayed.
- *           - formatter: If a formatter is provided, instead of directly using
- *                        the `property` property on the marker, the marker is
- *                        passed into the formatter function to determine the
- *                        displayed value.
- *            Can also be a function that returns an object. Each key in the object
- *            will be rendered as a field, with its value rendering as the value.
+ *           Alternatively, this also be a function that returns an object.
+ *           Each key in that object will be rendered as one field's label,
+ *           with its value rendering as one field's value.
  *
  * Whenever this is changed, browser_timeline_waterfall-styles.js *must* be
  * updated as well.
  */
 const TIMELINE_BLUEPRINT = {
-  /* Default definition used for markers that occur but
-   * are not defined here. Should ultimately be defined, but this gives
-   * us room to work on the front end separately from the platform. */
+  /* Default definition used for markers that occur but are not defined here.
+   * Should ultimately be defined, but this gives us room to work on the
+   * front end separately from the platform. */
   "UNKNOWN": {
     group: 2,
     colorName: "graphs-grey",
@@ -57,6 +54,7 @@ const TIMELINE_BLUEPRINT = {
   },
 
   /* Group 0 - Reflow and Rendering pipeline */
+
   "Styles": {
     group: 0,
     colorName: "graphs-purple",
@@ -85,6 +83,7 @@ const TIMELINE_BLUEPRINT = {
   },
 
   /* Group 1 - JS */
+
   "DOMEvent": {
     group: 1,
     colorName: "graphs-yellow",
@@ -155,38 +154,23 @@ const TIMELINE_BLUEPRINT = {
   },
 
   /* Group 2 - User Controlled */
+
   "ConsoleTime": {
     group: 2,
     colorName: "graphs-blue",
-    label: sublabelForProperty(L10N.getStr("marker.label.consoleTime"), "causeName"),
-    fields: [{
-      property: "causeName",
-      label: L10N.getStr("marker.field.consoleTimerName")
-    }],
+    label: Formatters.labelForProperty(L10N.getStr("marker.label.consoleTime"), "causeName"),
+    fields: Formatters.ConsoleTimeFields,
     nestable: false,
     collapsible: false,
   },
   "TimeStamp": {
     group: 2,
     colorName: "graphs-blue",
-    label: sublabelForProperty(L10N.getStr("marker.label.timestamp"), "causeName"),
-    fields: [{
-      property: "causeName",
-      label: "Label:"
-    }],
+    label: Formatters.labelForProperty(L10N.getStr("marker.label.timestamp"), "causeName"),
+    fields: Formatters.TimeStampFields,
     collapsible: false,
   },
 };
-
-/**
- * Takes a main label (like "Timestamp") and a property,
- * and returns a marker that will print out the property
- * value for a marker if it exists ("Timestamp (rendering)"),
- * or just the main label if it does not.
- */
-function sublabelForProperty (mainLabel, prop) {
-  return (marker={}) => marker[prop] ? `${mainLabel} (${marker[prop]})` : mainLabel;
-}
 
 // Exported symbols.
 exports.TIMELINE_BLUEPRINT = TIMELINE_BLUEPRINT;
