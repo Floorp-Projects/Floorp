77eb4067618d573558a29abc12f377755a867514
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-03-22 21:09:32 +0100

    Backed out changeset ff81c52375ba (bug 1257919)

--- dom/base/DOMException.cpp
+++ dom/base/DOMException.cpp
@@ -508,10 +508,10 @@ Exception::GetData() const
 }
 
 void
-Exception::GetStack(JSContext* aCx, nsAString& aStack, ErrorResult& aRv) const
+Exception::GetStack(nsAString& aStack, ErrorResult& aRv) const
 {
   if (mLocation) {
-    aRv = mLocation->GetFormattedStack(aCx, aStack);
+    aRv = mLocation->GetFormattedStack(aStack);
   }
 }
 
