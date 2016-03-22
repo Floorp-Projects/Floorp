47045c326651952e23485dfb84c13008bd1cc5f3
George Wright <george@mozilla.com>
2016-03-22 14:46:10 -0400

    Bug 1114647 - Rename "plugin-container" to "firefox-webcontent" and create a new executable target for Win32 called "firefox-plugin-container" r=ted,jhamer

--- ipc/app/MozillaRuntimeMain.cpp
+++ ipc/app/MozillaRuntimeMain.cpp
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "../contentproc/plugin-container.cpp"
+#include "../contentproc/firefox-webcontent.cpp"
  
 int
 main(int argc, char *argv[]) {
