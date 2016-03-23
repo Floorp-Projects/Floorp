51c6b28c0f331d6d410bd8d880151224f8faade4
Trevor Saunders <tbsaunde@tbsaunde.org>
2016-03-23 16:44:50 -0400

    bug 1255009 - make AccShowEvent store the insertion index of the new child r=davidb

--- accessible/base/AccEvent.h
+++ accessible/base/AccEvent.h
@@ -287,6 +287,11 @@ public:
   {
     return AccMutationEvent::GetEventGroups() | (1U << eShowEvent);
   }
+
+  uint32_t InsertionIndex() const { return mInsertionIndex; }
+
+private:
+  uint32_t mInsertionIndex;
 };
 
 
