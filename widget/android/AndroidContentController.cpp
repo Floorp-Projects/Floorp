bea17b967ee053773343abd2f74e0db683edd169
Jim Chen <nchen@mozilla.com>
2016-03-22 22:24:31 -0400

    Bug 1257319 - Don't use broadcast event in AndroidContentController; r=rbarker We should use observer service directly instead of broadcast event.

--- widget/android/AndroidContentController.cpp
+++ widget/android/AndroidContentController.cpp
@@ -70,9 +70,17 @@ AndroidContentController::HandleSingleTap(const CSSPoint& aPoint,
         }
 
         CSSIntPoint rounded = RoundedToInt(point);
-        nsCString data = nsPrintfCString("{ \"x\": %d, \"y\": %d }", rounded.x, rounded.y);
-        nsAppShell::PostEvent(AndroidGeckoEvent::MakeBroadcastEvent(
-                NS_LITERAL_CSTRING("Gesture:SingleTap"), data));
+        nsAppShell::PostEvent([rounded] {
+            nsCOMPtr<nsIObserverService> obsServ =
+                mozilla::services::GetObserverService();
+            if (!obsServ) {
+                return;
+            }
+
+            nsPrintfCString data("{\"x\":%d,\"y\":%d}", rounded.x, rounded.y);
+            obsServ->NotifyObservers(nullptr, "Gesture:SingleTap",
+                                     NS_ConvertASCIItoUTF16(data).get());
+        });
     }
 
     ChromeProcessController::HandleSingleTap(aPoint, aModifiers, aGuid);
