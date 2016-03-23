3c24d634759d3c5941577ec80fdd59dd8c254655
Brendan Dahl <bdahl@mozilla.com>
2016-03-23 17:56:00 -0400

    Bug 1256084 - Don't force reflow on size mode change. r=heycam

--- layout/base/nsPresContext.cpp
+++ layout/base/nsPresContext.cpp
@@ -2209,8 +2209,7 @@ nsPresContext::SizeModeChanged(nsSizeMode aSizeMode)
     nsContentUtils::CallOnAllRemoteChildren(mDocument->GetWindow(),
                                             NotifyTabSizeModeChanged,
                                             &aSizeMode);
-    MediaFeatureValuesChangedAllDocuments(eRestyle_Subtree,
-                                          NS_STYLE_HINT_REFLOW);
+    MediaFeatureValuesChangedAllDocuments(nsRestyleHint(0));
   }
 }
 
