ce9d6a3259ebf8bf1fa2d9dcff5dc4cb3bfe2dc1
Jonathan Kingston <jkingston@mozilla.com>
2016-03-23 11:26:04 +0000

    Bug 1244022 - Change the tooltip of the "i" icon to "Show site information". r=paolo
    MozReview-Commit-ID: oBtO1tuqOS

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6758,6 +6758,7 @@ var gIdentityHandler = {
 
     // Push the appropriate strings out to the UI
     this._identityBox.tooltipText = tooltip;
+    this._identityIcon.tooltipText = gNavigatorBundle.getString("identity.icon.tooltip");
     this._identityIconLabel.value = icon_label;
     this._identityIconCountryLabel.value = icon_country_label;
     // Set cropping and direction
