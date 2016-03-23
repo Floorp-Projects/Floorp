8876ae99286fd0b46f17166f1176f391cccd3e2c
Jon Coppeard <jcoppeard@mozilla.com>
2016-03-23 02:30:00 -0400

    Bug 1258436 - Remove GC suppression in JSFunction::createScriptForLazilyInterpretedFunction. r=sfink

--- js/src/jsscriptinlines.h
+++ js/src/jsscriptinlines.h
@@ -87,9 +87,10 @@ SetFrameArgumentsObject(JSContext* cx, AbstractFramePtr frame,
 inline JSFunction*
 LazyScript::functionDelazifying(JSContext* cx) const
 {
-    if (function_ && !function_->getOrCreateScript(cx))
+    Rooted<const LazyScript*> self(cx, this);
+    if (self->function_ && !self->function_->getOrCreateScript(cx))
         return nullptr;
-    return function_;
+    return self->function_;
 }
 
 } // namespace js
