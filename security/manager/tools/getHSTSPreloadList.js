e1d8b92ec610df71692976455b1346d410ec20af
Nathan Froyd <froydnj.com>
2016-03-24 15:09:28 -0400

    Bug 1255425 - part 2 - pack kSTSPreloadList into a more efficient format; r=keeler
    Entries in kSTSPreloadList currently look like:
    
    class nsSTSPreload
    {
      public:
        const char *mHost;
        const bool mIncludeSubdomains;
    };
    
    This is inefficient for a couple of reasons:
    
    * The structure has a bunch of wasted space: it takes 8 bytes on 32-bit
      platforms and 16 bytes on 64-bit platforms, even though it only uses 5
      and 9 bytes, respectively.
    
    * The |const char*| requires additional space in the form of relocations
      (at least on Linux/Android), which doubles the space cost of
      individual entries.  (The space cost of the relocations is mitigated
      somewhat on Linux and Android because of elfhack, but there's still
      extra cost in the on-disk format and during the load of libxul to
      process those relocations.)
    
    * The relocations the structure requires means that the data in it can't
      be shared between processes, which is important for e10s with multiple
      content processes.
    
    We can make it more efficient by structuring it like so:
    
    static const char kSTSPreloadHosts[] = {
      // One giant character array containing the hosts, in order:
      //   "example.com\0example.org\0example.test\0..."
      // Use an array rather than a literal string due to compiler limitations.
    };
    
    struct nsSTSPreload
    {
      // An index into kSTSPreloadHosts for the hostname.
      uint32_t mHostIndex: 31;
      // We use the same datatype for both members so that MSVC will pack
      // the bitfields into a single uint32_t.
      uint32_t mIncludeSubdomains: 1;
    };
    
    nsSTSPreload now has no wasted space and is significantly smaller,
    especially on 64-bit platforms (saves ~29K on 32-bit platforms and ~85K
    on 64-bit platforms).  This organization does add a couple extra
    operations to searching for preload list entries, depending on your
    platform, but the space savings make it worth it.

--- security/manager/tools/getHSTSPreloadList.js
+++ security/manager/tools/getHSTSPreloadList.js
@@ -42,16 +42,6 @@ const HEADER = "/* This Source Code Form is subject to the terms of the Mozilla 
 "/*****************************************************************************/\n" +
 "\n" +
 "#include <stdint.h>\n";
-const PREFIX = "\n" +
-"class nsSTSPreload\n" +
-"{\n" +
-"  public:\n" +
-"    const char *mHost;\n" +
-"    const bool mIncludeSubdomains;\n" +
-"};\n" +
-"\n" +
-"static const nsSTSPreload kSTSPreloadList[] = {\n";
-const POSTFIX =  "};\n";
 
 function download() {
   var req = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"]
@@ -241,12 +231,9 @@ function errorToString(status) {
           : status.error);
 }
 
-function writeEntry(status, outputStream) {
-  let incSubdomainsBool = (status.forceInclude && status.error != ERROR_NONE
-                           ? status.originalIncludeSubdomains
-                           : status.includeSubdomains);
-  let includeSubdomains = (incSubdomainsBool ? "true" : "false");
-  writeTo("  { \"" + status.name + "\", " + includeSubdomains + " },\n",
+function writeEntry(status, indices, outputStream) {
+  let includeSubdomains = (status.finalIncludeSubdomains ? "true" : "false");
+  writeTo("  { " + indices[status.name] + ", " + includeSubdomains + " },\n",
           outputStream);
 }
 
@@ -258,7 +245,6 @@ function output(sortedStatuses, currentList) {
     var eos = FileUtils.openSafeFileOutputStream(errorFile);
     writeTo(HEADER, fos);
     writeTo(getExpirationTimeString(), fos);
-    writeTo(PREFIX, fos);
 
     for (let status in sortedStatuses) {
       // If we've encountered an error for this entry (other than the site not
@@ -291,8 +277,56 @@ function output(sortedStatuses, currentList) {
       return true;
     });
 
-    for (var status of includedStatuses) {
-      writeEntry(status, fos);
+    // Resolve whether we should include subdomains for each entry.  We could
+    // do this while writing out entries, but separating out that decision is
+    // clearer.  Making that decision here also means we can write the choices
+    // in the comments in the static string table, which makes parsing the
+    // current list significantly easier when we go to update the list.
+    for (let status of includedStatuses) {
+      let incSubdomainsBool = (status.forceInclude && status.error != ERROR_NONE
+                               ? status.originalIncludeSubdomains
+                               : status.includeSubdomains);
+      status.finalIncludeSubdomains = incSubdomainsBool;
+    }
+
+    writeTo("\nstatic const char kSTSHostTable[] = {\n", fos);
+    var indices = {};
+    var currentIndex = 0;
+    for (let status of includedStatuses) {
+      indices[status.name] = currentIndex;
+      // Add 1 for the null terminator in C.
+      currentIndex += status.name.length + 1;
+      // Rebuilding the preload list requires reading the previous preload
+      // list.  Write out a comment describing each host prior to writing out
+      // the string for the host.
+      writeTo("  /* \"" + status.name + "\", " +
+              (status.finalIncludeSubdomains ? "true" : "false") + " */ ",
+              fos);
+      // Write out the string itself as individual characters, including the
+      // null terminator.  We do it this way rather than using C's string
+      // concatentation because some compilers have hardcoded limits on the
+      // lengths of string literals, and the preload list is large enough
+      // that it runs into said limits.
+      for (let c of status.name) {
+	writeTo("'" + c + "', ", fos);
+      }
+      writeTo("'\\0',\n", fos);
+    }
+    writeTo("};\n", fos);
+
+    const PREFIX = "\n" +
+      "struct nsSTSPreload\n" +
+      "{\n" +
+      "  const uint32_t mHostIndex : 31;\n" +
+      "  const uint32_t mIncludeSubdomains : 1;\n" +
+      "};\n" +
+      "\n" +
+      "static const nsSTSPreload kSTSPreloadList[] = {\n";
+    const POSTFIX = "};\n";
+
+    writeTo(PREFIX, fos);
+    for (let status of includedStatuses) {
+      writeEntry(status, indices, fos);
     }
     writeTo(POSTFIX, fos);
     FileUtils.closeSafeFileOutputStream(fos);
@@ -358,9 +392,17 @@ function readCurrentList(filename) {
               .createInstance(Ci.nsILineInputStream);
   fis.init(file, -1, -1, Ci.nsIFileInputStream.CLOSE_ON_EOF);
   var line = {};
-  var entryRegex = /  { "([^"]*)", (true|false) },/;
+  // While we generate entries matching the version 2 format (see bug 1255425
+  // for details), we still need to be able to read entries in the version 1
+  // format for bootstrapping a version 2 preload list from a version 1
+  // preload list.  Hence these two regexes.
+  var v1EntryRegex = /  { "([^"]*)", (true|false) },/;
+  var v2EntryRegex = /  \/\* "([^"]*)", (true|false) \*\//;
   while (fis.readLine(line)) {
-    var match = entryRegex.exec(line.value);
+    var match = v1EntryRegex.exec(line.value);
+    if (!match) {
+      match = v2EntryRegex.exec(line.value);
+    }
     if (match) {
       currentHosts[match[1]] = (match[2] == "true");
     }
