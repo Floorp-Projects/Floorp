227bab9ee8b443736f3483819c3f62cf484050d3
Olivier Yiptong <olivier@olivieryiptong.com>
2016-03-24 16:28:16 -0400

    Bug 1258728 - keep remote newtab version in a pref r=ursula
    MozReview-Commit-ID: GNk74kkxHcV

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1385,6 +1385,9 @@ pref("browser.newtabpage.directory.ping", "https://tiles.services.mozilla.com/v3
 // activates the remote-hosted newtab page
 pref("browser.newtabpage.remote", false);
 
+// remote newtab version targeted
+pref("browser.newtabpage.remote.version", "1");
+
 // Toggles endpoints allowed for remote newtab communications
 pref("browser.newtabpage.remote.mode", "production");
 
