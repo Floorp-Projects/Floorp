84d98ed01d1bc4d5b0f5cdae8b93a429a12fe00f
Michael Comella <michael.l.comella@gmail.com>
2016-03-24 11:18:58 -0700

    Bug 1249288 - review: Correct concurrency issues with searchEngineManager. r=sebastian
    Additionally, added WeakReferences to the SEM in its callbacks so we can
    GC ASAP if the Activity (and thus the SEM) gets GC'd. This is important
    since we hold a reference to Context which can be a rather large object.
    
    Furthermore, I added some related thread annotations where I felt they
    were useful.
    
    MozReview-Commit-ID: KaWlw14uOoN

--- mobile/android/base/java/org/mozilla/gecko/distribution/Distribution.java
+++ mobile/android/base/java/org/mozilla/gecko/distribution/Distribution.java
@@ -51,6 +51,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.SystemClock;
+import android.support.annotation.WorkerThread;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
@@ -126,6 +127,7 @@ public class Distribution {
      * {@link org.mozilla.gecko.distribution.Distribution#exists()} will return
      * false. In the other two callbacks, it will return true.
      */
+    @WorkerThread
     public interface ReadyCallback {
         void distributionNotFound();
         void distributionFound(Distribution distribution);
@@ -951,6 +953,7 @@ public class Distribution {
 
     private void invokeCallbackDelayed(final ReadyCallback callback) {
         ThreadUtils.postToBackgroundThread(new Runnable() {
+            @WorkerThread
             @Override
             public void run() {
                 switch (state) {
