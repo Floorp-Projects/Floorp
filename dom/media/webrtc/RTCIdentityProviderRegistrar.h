fd64e52ef9c02da3cd3f85c0dfdf839eb5db036f
Phil Ringnalda <philringnalda@gmail.com>
2016-03-23 19:25:39 -0700

    Back out 1726498035c1 (bug 1258299) for xpcshell failures in test_rtcIdentityProvider.js CLOSED TREE

--- dom/media/webrtc/RTCIdentityProviderRegistrar.h
+++ dom/media/webrtc/RTCIdentityProviderRegistrar.h
@@ -14,12 +14,11 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/dom/Promise.h"
 #include "mozilla/dom/BindingDeclarations.h"
-#include "mozilla/dom/RTCIdentityProviderBinding.h"
 
 namespace mozilla {
 namespace dom {
 
-struct RTCIdentityProvider;
+class RTCIdentityProvider;
 
 class RTCIdentityProviderRegistrar final : public nsISupports,
                                            public nsWrapperCache
@@ -34,9 +33,9 @@ public:
   nsIGlobalObject* GetParentObject() const;
   virtual JSObject* WrapObject(JSContext* aCx, JS::Handle<JSObject*> aGivenProto) override;
 
-  // setter and checker
-  void Register(const RTCIdentityProvider& aIdp);
-  bool HasIdp() const;
+  // setter and getter
+  void Register(RTCIdentityProvider& aIdp);
+  already_AddRefed<RTCIdentityProvider> GetIdp();
 
   already_AddRefed<Promise>
   GenerateAssertion(const nsAString& aContents, const nsAString& aOrigin,
@@ -49,8 +48,7 @@ private:
   ~RTCIdentityProviderRegistrar();
 
   nsCOMPtr<nsIGlobalObject> mGlobal;
-  RefPtr<GenerateAssertionCallback> mGenerateAssertionCallback;
-  RefPtr<ValidateAssertionCallback> mValidateAssertionCallback;
+  RefPtr<RTCIdentityProvider> mIdp;
 };
 
 } // namespace dom
