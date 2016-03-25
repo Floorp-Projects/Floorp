34f151aafd0383ad53eb58f4f0a1422080761941
David Anderson <danderson@mozilla.com>
2016-03-25 01:36:17 -0700

    Disable device access on textures created against stale layers. (bug 1256517 part 3, r=nical, a=rkothari)

--- gfx/layers/d3d11/TextureD3D11.cpp
+++ gfx/layers/d3d11/TextureD3D11.cpp
@@ -629,6 +629,10 @@ DXGITextureHostD3D11::OpenSharedHandle()
 ID3D11Device*
 DXGITextureHostD3D11::GetDevice()
 {
+  if (mFlags & TextureFlags::INVALID_COMPOSITOR) {
+    return nullptr;
+  }
+
   return gfxWindowsPlatform::GetPlatform()->GetD3D11Device();
 }
 
@@ -733,6 +737,10 @@ DXGIYCbCrTextureHostD3D11::OpenSharedHandle()
 ID3D11Device*
 DXGIYCbCrTextureHostD3D11::GetDevice()
 {
+  if (mFlags & TextureFlags::INVALID_COMPOSITOR) {
+    return nullptr;
+  }
+
   return gfxWindowsPlatform::GetPlatform()->GetD3D11Device();
 }
 
