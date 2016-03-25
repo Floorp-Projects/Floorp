22cf197d5df002fa551af053612e674a7819ecee
Bobby Holley <bobbyholley@gmail.com>
2016-03-24 18:26:46 -0700

    Bug 1258017 - Use an nsCOMPtr to hold onto the nsIStyleRule. r=dbaron

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -1527,10 +1527,6 @@ nsRuleNode::nsRuleNode(nsPresContext* aContext, nsRuleNode* aParent,
   mChildren.asVoid = nullptr;
   MOZ_COUNT_CTOR(nsRuleNode);
 
-  if (mRule) {
-    mRule->AddRef();
-  }
-
   NS_ASSERTION(IsRoot() || GetLevel() == aLevel, "not enough bits");
   NS_ASSERTION(IsRoot() || IsImportantRule() == aIsImportant, "yikes");
   /* If IsRoot(), then aContext->StyleSet() is typically null at this
@@ -1556,9 +1552,6 @@ nsRuleNode::~nsRuleNode()
   MOZ_COUNT_DTOR(nsRuleNode);
   if (mStyleData.mResetData || mStyleData.mInheritedData)
     mStyleData.Destroy(mDependentBits, mPresContext);
-  if (mRule) {
-    mRule->Release();
-  }
 }
 
 nsRuleNode*
