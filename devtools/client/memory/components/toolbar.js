2b44ecc7577c0e939853cd81997998381f71284f
Jarda Snajdr <jsnajdr@gmail.com>
2016-03-25 10:01:12 -0700

    Bug 1220208 - Move clear button to top left corner in toolbars r=bgrins
    MozReview-Commit-ID: 5xIjd2TkvP6

--- devtools/client/memory/components/toolbar.js
+++ devtools/client/memory/components/toolbar.js
@@ -179,6 +179,13 @@ module.exports = createClass({
           },
 
           dom.button({
+            id: "clear-snapshots",
+            className: "clear-snapshots devtools-button",
+            onClick: onClearSnapshotsClick,
+            title: L10N.getStr("clear-snapshots.tooltip")
+          }),
+
+          dom.button({
             id: "take-snapshot",
             className: "take-snapshot devtools-button",
             onClick: onTakeSnapshotClick,
@@ -204,14 +211,7 @@ module.exports = createClass({
               "data-text-only": true,
             },
             L10N.getStr("import-snapshot")
-          ),
-
-          dom.button({
-            id: "clear-snapshots",
-            className: "clear-snapshots devtools-button",
-            onClick: onClearSnapshotsClick,
-            title: L10N.getStr("clear-snapshots.tooltip")
-          })
+          )
         ),
 
         dom.label(
