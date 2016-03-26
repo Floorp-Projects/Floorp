9488ced949c3f3edb422f5f4020462cda8199ea9
Steve Melia <steve.j.melia@gmail.com>
2016-03-26 15:21:23 +0000

    Bug 1256658 - Make inspector search next/previous based on ctrl-g by default, and cmd-g on osx;r=bgrins
    MozReview-Commit-ID: F5NsNGmoeL8

--- devtools/client/inspector/inspector-search.js
+++ devtools/client/inspector/inspector-search.js
@@ -7,12 +7,13 @@
 const {Cu, Ci} = require("chrome");
 
 const promise = require("promise");
+
+loader.lazyGetter(this, "system", () => require("devtools/shared/system"));
 loader.lazyGetter(this, "EventEmitter", () => require("devtools/shared/event-emitter"));
 loader.lazyGetter(this, "AutocompletePopup", () => require("devtools/client/shared/autocomplete-popup").AutocompletePopup);
 
 // Maximum number of selector suggestions shown in the panel.
 const MAX_SUGGESTIONS = 15;
-
 
 /**
  * Converts any input field into a document search box.
@@ -108,7 +109,11 @@ InspectorSearch.prototype = {
     }
     if (event.keyCode === event.DOM_VK_RETURN) {
       this._onSearch(event.shiftKey);
-    } if (event.keyCode === Ci.nsIDOMKeyEvent.DOM_VK_G && event.metaKey) {
+    }
+
+    const modifierKey = system.constants.platform === "macosx" ? event.metaKey :
+event.ctrlKey;
+    if (event.keyCode === Ci.nsIDOMKeyEvent.DOM_VK_G && modifierKey) {
       this._onSearch(event.shiftKey);
       event.preventDefault();
     }
