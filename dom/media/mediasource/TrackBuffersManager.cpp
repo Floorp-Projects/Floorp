f2bfa841c10f659f88da786613921b755059f99a
Gerald Squelart <gsquelart@mozilla.com>
2016-03-27 19:57:25 +1100

    Bug 1259985 - Add missing return after null-check - r=jya
    MozReview-Commit-ID: 138NM9xTmVA
    
    --HG--
    extra : rebase_source : 6f594e905b1d8d8f623a02ea8fb78b7dc3b846f0

--- dom/media/mediasource/TrackBuffersManager.cpp
+++ dom/media/mediasource/TrackBuffersManager.cpp
@@ -891,6 +891,7 @@ TrackBuffersManager::OnDemuxerInitDone(nsresult)
     // request was being processed. See bug 1239983.
     NS_ASSERTION(false, "mInputDemuxer has been destroyed");
     RejectAppend(NS_ERROR_ABORT, __func__);
+    return;
   }
 
   MediaInfo info;
