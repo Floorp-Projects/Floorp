16270a556ae7a29db27ad012f30f520d0587efac
Tooru Fujisawa <arai_a@mac.com>
2016-03-28 06:49:58 +0900

    Backed out changeset 71ef5f8009ec (bug 1165053)

--- js/src/jsobj.h
+++ js/src/jsobj.h
@@ -1334,9 +1334,6 @@ FreezeObject(JSContext* cx, HandleObject obj)
 extern bool
 TestIntegrityLevel(JSContext* cx, HandleObject obj, IntegrityLevel level, bool* resultp);
 
-extern bool
-SpeciesConstructor(JSContext* cx, HandleObject obj, HandleValue defaultCtor, MutableHandleValue pctor);
-
 }  /* namespace js */
 
 #endif /* jsobj_h */
