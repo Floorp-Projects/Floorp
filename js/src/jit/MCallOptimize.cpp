eaeb3a5245b1369b6b20ce5e670b40c1a2584977
Tooru Fujisawa <arai_a@mac.com>
2016-03-28 06:49:58 +0900

    Backed out changeset f3195319f23d (bug 1165053)

--- js/src/jit/MCallOptimize.cpp
+++ js/src/jit/MCallOptimize.cpp
@@ -265,8 +265,6 @@ IonBuilder::inlineNativeCall(CallInfo& callInfo, JSFunction* target)
         return inlineGetNextMapEntryForIterator(callInfo);
 
       // ArrayBuffer intrinsics.
-      case InlinableNative::IntrinsicArrayBufferByteLength:
-        return inlineArrayBufferByteLength(callInfo);
       case InlinableNative::IntrinsicPossiblyWrappedArrayBufferByteLength:
         return inlinePossiblyWrappedArrayBufferByteLength(callInfo);
 
@@ -2250,25 +2248,6 @@ IsArrayBufferObject(CompilerConstraintList* constraints, MDefinition* def)
         return false;
 
     return types->getKnownClass(constraints) == &ArrayBufferObject::class_;
-}
-
-IonBuilder::InliningStatus
-IonBuilder::inlineArrayBufferByteLength(CallInfo& callInfo)
-{
-    MOZ_ASSERT(!callInfo.constructing());
-    MOZ_ASSERT(callInfo.argc() == 1);
-
-    MDefinition* objArg = callInfo.getArg(0);
-    if (objArg->type() != MIRType_Object)
-        return InliningStatus_NotInlined;
-    if (getInlineReturnType() != MIRType_Int32)
-        return InliningStatus_NotInlined;
-
-    MInstruction* ins = addArrayBufferByteLength(objArg);
-    current->push(ins);
-
-    callInfo.setImplicitlyUsedUnchecked();
-    return InliningStatus_Inlined;
 }
 
 IonBuilder::InliningStatus
