36a4d72c93aca71bd509fdd81863f9308e87d423
Lee Salzman <lsalzman@mozilla.com>
2016-03-28 12:53:42 -0400

    Bug 1259810 - check that D2D1 device creation succeeds and otherwise fall to software backend. r=bas.schouten

--- gfx/thebes/gfxWindowsPlatform.cpp
+++ gfx/thebes/gfxWindowsPlatform.cpp
@@ -2517,8 +2517,11 @@ gfxWindowsPlatform::InitializeD2D()
     return;
   }
 
-  mD2D1Status = FeatureStatus::Available;
-  Factory::SetDirect3D11Device(mD3D11ContentDevice);
+  // Verify that Direct2D device creation succeeded.
+  if (!Factory::SetDirect3D11Device(mD3D11ContentDevice)) {
+    mD2D1Status = FeatureStatus::Failed;
+    return;
+  }
 
   d2d1_1.SetSuccessful();
 
