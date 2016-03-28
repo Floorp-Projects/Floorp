6c06967b3730840d1382dd9e08c9fe887904d3be
Victor Porof <vporof@mozilla.com>
2016-03-28 13:50:02 +0200

    Bug 1258645 - Update all occurences of MarkerUtils to use the new module, r=jsantell

--- devtools/client/performance/test/browser_timeline-waterfall-sidebar.js
+++ devtools/client/performance/test/browser_timeline-waterfall-sidebar.js
@@ -9,7 +9,7 @@ function* spawnTest() {
   let { target, panel } = yield initPerformance(SIMPLE_URL);
   let { $, $$, PerformanceController, WaterfallView } = panel.panelWin;
   let { L10N } = require("devtools/client/performance/modules/global");
-  let { getMarkerLabel } = require("devtools/client/performance/modules/logic/marker-utils");
+  let { MarkerBlueprintUtils } = require("devtools/client/performance/modules/marker-blueprint-utils");
 
   // Hijack the markers massaging part of creating the waterfall view,
   // to prevent collapsing markers and allowing this test to verify
@@ -60,7 +60,7 @@ function* spawnTest() {
     info("Current marker data: " + mkr.toSource());
     info("Current marker output: " + $("#waterfall-details").innerHTML);
 
-    is(type, getMarkerLabel(mkr), "Sidebar title matches markers name.");
+    is(type, MarkerBlueprintUtils.getMarkerLabel(mkr), "Sidebar title matches markers name.");
 
     // Values are rounded. We don't use a strict equality.
     is(toMs(mkr.end - mkr.start), duration, "Sidebar duration is valid.");
