18c5c296efa4f97a9fe49917953bca78e777779c
Kit Cambridge <kcambridge@mozilla.com>
2016-03-28 11:40:58 -0700

    Bug 1246341 - Add a test for push event error reporting. r=dragana
    MozReview-Commit-ID: LABOJnYtpD5
    
    --HG--
    extra : rebase_source : 86c27b8a2deb388cdb34a82aebd12b042477acb5

--- dom/push/PushNotifier.h
+++ dom/push/PushNotifier.h
@@ -29,9 +29,8 @@ namespace dom {
  * forwards incoming push messages to service workers running in the content
  * process, and emits XPCOM observer notifications for system subscriptions.
  *
- * The XPCOM service can only be used from the main process. Callers running
- * in the content process should use
- * `ServiceWorkerManager::SendPush{SubscriptionChange}Event` directly.
+ * This service exists solely to support `PushService.jsm`. Other callers
+ * should use `ServiceWorkerManager` directly.
  */
 class PushNotifier final : public nsIPushNotifier
 {
