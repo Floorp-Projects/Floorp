f0ca386120273ca8bb0b9c38ce10445277ac1d77
dimi <dlee@mozilla.com>
2016-03-28 15:10:03 +0800

    Bug 1212600 - No upper limit on digest256 list file size. r=francois
    MozReview-Commit-ID: b1aQbF92as

--- toolkit/components/url-classifier/nsUrlClassifierStreamUpdater.cpp
+++ toolkit/components/url-classifier/nsUrlClassifierStreamUpdater.cpp
@@ -25,6 +25,9 @@
 
 static const char* gQuitApplicationMessage = "quit-application";
 
+// Limit the list file size to 32mb
+const uint32_t MAX_FILE_SIZE = (32 * 1024 * 1024);
+
 #undef LOG
 
 // NSPR_LOG_MODULES=UrlClassifierStreamUpdater:5
@@ -643,6 +646,11 @@ nsUrlClassifierStreamUpdater::OnDataAvailable(nsIRequest *request,
     return NS_ERROR_NOT_INITIALIZED;
 
   LOG(("OnDataAvailable (%d bytes)", aLength));
+
+  if (aSourceOffset > MAX_FILE_SIZE) {
+    LOG(("OnDataAvailable::Abort because exceeded the maximum file size(%lld)", aSourceOffset));
+    return NS_ERROR_FILE_TOO_BIG;
+  }
 
   nsresult rv;
 
