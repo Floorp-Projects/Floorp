b0c66f28e5ce633fbe2cdfea2f83c32df4c76319
Jordan Lund <jlund@mozilla.com>
2016-03-29 11:15:04 -0700

    Bug 1259525 - release promotion - add staging mozilla-release configs to mozharness, DONTBUILD a=testing r=rail
    MozReview-Commit-ID: 6VfvxSaaNO0
    
    MozReview-Commit-ID: 7aJJcWgEdeh

--- testing/mozharness/configs/releases/dev_updates_firefox_release.py
+++ testing/mozharness/configs/releases/dev_updates_firefox_release.py
@@ -1,6 +1,6 @@
 
 config = {
-    "log_name": "bump_beta_dev",
+    "log_name": "updates_release_dev",
     # TODO: use real repo
     "repo": {
         "repo": "https://hg.mozilla.org/users/raliiev_mozilla.com/tools",
@@ -10,8 +10,8 @@ config = {
     },
     # TODO: use real repo
     "push_dest": "ssh://hg.mozilla.org/users/raliiev_mozilla.com/tools",
-    # date repo used for staging beta
-    "shipped-locales-url": "https://hg.mozilla.org/projects/date/raw-file/{revision}/browser/locales/shipped-locales",
+    # jamun  repo used for staging release
+    "shipped-locales-url": "https://hg.mozilla.org/projects/jamun/raw-file/{revision}/browser/locales/shipped-locales",
     "ignore_no_changes": True,
     "ssh_user": "ffxbld",
     "ssh_key": "~/.ssh/ffxbld_rsa",
@@ -24,14 +24,24 @@ config = {
     "update_channels": {
         "beta-dev": {
             "version_regex": r"^(\d+\.\d+(b\d+)?)$",
-            "requires_mirrors": True,
-            # TODO - when we use a real repo, rename this file # s/MozDate/MozBeta-dev/
+            "requires_mirrors": False,
             "patcher_config": "mozDate-branch-patcher2.cfg",
             "update_verify_channel": "beta-dev-localtest",
-            "mar_channel_ids": [],
+            "mar_channel_ids": [
+                "firefox-mozilla-beta-dev", "firefox-mozilla-release-dev",
+            ],
             "channel_names": ["beta-dev", "beta-dev-localtest", "beta-dev-cdntest"],
             "rules_to_update": ["firefox-beta-dev-cdntest", "firefox-beta-dev-localtest"],
-        }
+        },
+        "release-dev": {
+            "version_regex": r"^\d+\.\d+(\.\d+)?$",
+            "requires_mirrors": True,
+            "patcher_config": "mozJamun-branch-patcher2.cfg",
+            "update_verify_channel": "release-dev-localtest",
+            "mar_channel_ids": [],
+            "channel_names": ["release-dev", "release-dev-localtest", "release-dev-cdntest"],
+            "rules_to_update": ["firefox-release-dev-cdntest", "firefox-release-dev-localtest"],
+        },
     },
     "balrog_use_dummy_suffix": False,
 }
