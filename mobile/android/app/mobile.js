5720908e6f325d1c55d242dacc1f1dd6920a7135
Wes Kocher <wkocher@mozilla.com>
2016-03-29 16:38:18 -0700

    Backed out changeset 3ff2b12ffedc (bug 1254667) for upsetting the test_ocsp_caching.js gods on android CLOSED TREE
    MozReview-Commit-ID: JaJXHxKEAvu

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -520,8 +520,8 @@ pref("security.cert_pinning.enforcement_level", 1);
 
 // NB: Changes to this pref affect CERT_CHAIN_SHA1_POLICY_STATUS telemetry.
 // See the comment in CertVerifier.cpp.
-// 3 = allow SHA-1 for certificates issued before 2016 or by an imported root.
-pref("security.pki.sha1_enforcement_level", 3);
+// Allow SHA-1 certificates
+pref("security.pki.sha1_enforcement_level", 0);
 
 // Required blocklist freshness for OneCRL OCSP bypass
 // (default is 1.25x extensions.blocklist.interval, or 30 hours)
