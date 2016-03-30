6208a6d628afa5362cbf6edeb41c45a872be02d2
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-03-30 14:37:54 +0200

    Backed out changeset 671ce80172a0 (bug 1220928)

--- mobile/android/base/java/org/mozilla/gecko/home/CombinedHistoryAdapter.java
+++ mobile/android/base/java/org/mozilla/gecko/home/CombinedHistoryAdapter.java
@@ -9,17 +9,13 @@ import android.support.v7.widget.RecyclerView;
 import android.content.Context;
 import android.database.Cursor;
 import android.util.Log;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import org.json.JSONArray;
 import org.mozilla.gecko.R;
-import org.mozilla.gecko.db.BrowserContract;
 import org.mozilla.gecko.db.RemoteClient;
 import org.mozilla.gecko.db.RemoteTab;
-import org.mozilla.gecko.home.CombinedHistoryPanel.SectionHeader;
 
 import java.util.Collections;
 import java.util.ArrayList;
@@ -29,7 +25,7 @@ public class CombinedHistoryAdapter extends RecyclerView.Adapter<CombinedHistory
     private static final String LOGTAG = "GeckoCombinedHistAdapt";
 
     public enum ItemType {
-        CLIENT, SECTION_HEADER, HISTORY, NAVIGATION_BACK, CHILD;
+        CLIENT, HISTORY, NAVIGATION_BACK, CHILD;
 
         public static ItemType viewTypeToItemType(int viewType) {
             if (viewType >= ItemType.values().length) {
@@ -46,10 +42,6 @@ public class CombinedHistoryAdapter extends RecyclerView.Adapter<CombinedHistory
     private List<RemoteClient> remoteClients = Collections.emptyList();
     private List<RemoteTab> clientChildren;
     private Cursor historyCursor;
-
-    // We use a sparse array to store each section header's position in the panel [more cheaply than a HashMap].
-    private final SparseArray<CombinedHistoryPanel.SectionHeader> sectionHeaders;
-
     private final Context context;
 
     private boolean inChildView = false;
@@ -57,7 +49,6 @@ public class CombinedHistoryAdapter extends RecyclerView.Adapter<CombinedHistory
     public CombinedHistoryAdapter(Context context) {
         super();
         this.context = context;
-        sectionHeaders = new SparseArray<>();
     }
 
     public void setClients(List<RemoteClient> clients) {
@@ -67,7 +58,6 @@ public class CombinedHistoryAdapter extends RecyclerView.Adapter<CombinedHistory
 
     public void setHistory(Cursor history) {
         historyCursor = history;
-        populateSectionHeaders(historyCursor, sectionHeaders);
         notifyDataSetChanged();
     }
 
@@ -102,10 +92,8 @@ public class CombinedHistoryAdapter extends RecyclerView.Adapter<CombinedHistory
     private int transformPosition(ItemType type, int position) {
         if (type == ItemType.CLIENT) {
             return position;
-        } else if (type == ItemType.SECTION_HEADER) {
-            return position - remoteClients.size();
         } else if (type == ItemType.HISTORY){
-            return position - remoteClients.size() - getHeadersBefore(position);
+            return position - remoteClients.size();
         } else {
             return position;
         }
@@ -127,10 +115,6 @@ public class CombinedHistoryAdapter extends RecyclerView.Adapter<CombinedHistory
                 view = inflater.inflate(R.layout.home_combined_back_item, viewGroup, false);
                 return new CombinedHistoryItem.HistoryItem(view);
 
-            case SECTION_HEADER:
-                view = inflater.inflate(R.layout.home_header_row, viewGroup, false);
-                return new CombinedHistoryItem.SectionItem(view);
-
             case CHILD:
             case HISTORY:
                 view = inflater.inflate(R.layout.home_item_row, viewGroup, false);
@@ -145,65 +129,26 @@ public class CombinedHistoryAdapter extends RecyclerView.Adapter<CombinedHistory
         if (inChildView) {
             if (position == 0) {
                 return ItemType.itemTypeToViewType(ItemType.NAVIGATION_BACK);
+            } else {
+                return ItemType.itemTypeToViewType(ItemType.CHILD);
             }
-            return ItemType.itemTypeToViewType(ItemType.CHILD);
         } else {
             final int numClients = remoteClients.size();
-            if (position < numClients) {
-                return ItemType.itemTypeToViewType(ItemType.CLIENT);
-            }
-
-            final int sectionPosition = transformPosition(ItemType.SECTION_HEADER, position);
-            if (sectionHeaders.get(sectionPosition) != null) {
-                return ItemType.itemTypeToViewType(ItemType.SECTION_HEADER);
-            }
-
-            return ItemType.itemTypeToViewType(ItemType.HISTORY);
+            return (position < numClients) ? ItemType.itemTypeToViewType(ItemType.CLIENT) : ItemType.itemTypeToViewType(ItemType.HISTORY);
         }
     }
 
     @Override
     public int getItemCount() {
+
         if (inChildView) {
             return (clientChildren == null) ? 0 : clientChildren.size();
         } else {
             final int remoteSize = remoteClients.size();
             final int historySize = historyCursor == null ? 0 : historyCursor.getCount();
-            return remoteSize + historySize + sectionHeaders.size();
+            return remoteSize + historySize;
         }
     }
-
-    /**
-     * Add only the SectionHeaders that have history items within their range to a SparseArray, where the
-     * array index is the position of the header in the history-only (no clients) ordering.
-     * @param c data Cursor
-     * @param sparseArray SparseArray to populate
-     */
-    private static void populateSectionHeaders(Cursor c, SparseArray<SectionHeader> sparseArray) {
-        sparseArray.clear();
-
-        if (c == null || !c.moveToFirst()) {
-            return;
-        }
-
-        SectionHeader section = null;
-
-        do {
-            final int historyPosition = c.getPosition();
-            final long visitTime = c.getLong(c.getColumnIndexOrThrow(BrowserContract.History.DATE_LAST_VISITED));
-            final SectionHeader itemSection = CombinedHistoryPanel.getSectionFromTime(visitTime);
-
-            if (section != itemSection) {
-                section = itemSection;
-                sparseArray.append(historyPosition + sparseArray.size(), section);
-            }
-
-            if (section == SectionHeader.OLDER_THAN_SIX_MONTHS) {
-                break;
-            }
-        } while (c.moveToNext());
-    }
-
 
     public boolean containsHistory() {
         if (historyCursor == null) {
@@ -229,10 +174,6 @@ public class CombinedHistoryAdapter extends RecyclerView.Adapter<CombinedHistory
                 ((CombinedHistoryItem.HistoryItem) viewHolder).bind(remoteTab);
                 break;
 
-            case SECTION_HEADER:
-                ((TextView) viewHolder.itemView).setText(CombinedHistoryPanel.getSectionHeaderTitle(sectionHeaders.get(localPosition)));
-                break;
-
             case HISTORY:
                 if (historyCursor == null || !historyCursor.moveToPosition(localPosition)) {
                     throw new IllegalStateException("Couldn't move cursor to position " + localPosition);
@@ -240,22 +181,5 @@ public class CombinedHistoryAdapter extends RecyclerView.Adapter<CombinedHistory
                 ((CombinedHistoryItem.HistoryItem) viewHolder).bind(historyCursor);
                 break;
         }
-    }
-
-    /**
-     * Returns the number of section headers before the given history item at the adapter position.
-     * @param position position in the adapter
-     */
-    private int getHeadersBefore(int position) {
-        final int adjustedPosition = position - remoteClients.size();
-        // Skip the first header case because there will always be a header.
-        for (int i = 1; i < sectionHeaders.size(); i++) {
-            // If the position of the header is greater than the history position,
-            // return the number of headers tested.
-            if (sectionHeaders.keyAt(i) > adjustedPosition) {
-                return i;
-            }
-        }
-        return sectionHeaders.size();
     }
 }
