1faf7f7ecd7998d0d5f20aaf43c92460ca59b7be
Eugen Sawin <esawin@mozilla.com>
2016-03-31 23:28:27 +0200

    Bug 1257304 - [1.3] Cache screen size and update only on orientation change. r=jchen

--- mobile/android/base/java/org/mozilla/gecko/GeckoAppShell.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoAppShell.java
@@ -265,6 +265,7 @@ public class GeckoAppShell
     public static native void onFullScreenPluginHidden(View view);
 
     private static LayerView sLayerView;
+    private static Rect sScreenSize;
 
     public static void setLayerView(LayerView lv) {
         if (sLayerView == lv) {
@@ -1283,7 +1284,7 @@ public class GeckoAppShell
                 return true;
             }
         };
-            
+
         EnumerateGeckoProcesses(visitor);
     }
 
@@ -1305,7 +1306,7 @@ public class GeckoAppShell
 
             // figure out the column offsets.  We only care about the pid and user fields
             StringTokenizer st = new StringTokenizer(headerOutput);
-            
+
             int tokenSoFar = 0;
             while (st.hasMoreTokens()) {
                 String next = st.nextToken();
@@ -1438,7 +1439,7 @@ public class GeckoAppShell
         final MimeTypeMap mtm = MimeTypeMap.getSingleton();
         return mtm.getMimeTypeFromExtension(ext);
     }
-    
+
     private static Drawable getDrawableForExtension(PackageManager pm, String aExt) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         final String mimeType = getMimeTypeFromExtension(aExt);
@@ -2210,7 +2211,7 @@ public class GeckoAppShell
         if (Proxy.NO_PROXY.equals(proxy)) {
             return "DIRECT";
         }
-        
+
         switch (proxy.type()) {
             case HTTP:
                 return "PROXY " + proxy.address().toString();
@@ -2375,11 +2376,18 @@ public class GeckoAppShell
         return 0;
     }
 
+    public static synchronized void resetScreenSize() {
+        sScreenSize = null;
+    }
+
     @WrapForJNI
-    static Rect getScreenSize() {
-        final WindowManager wm = (WindowManager)
-                getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
-        final Display disp = wm.getDefaultDisplay();
-        return new Rect(0, 0, disp.getWidth(), disp.getHeight());
+    public static synchronized Rect getScreenSize() {
+        if (sScreenSize == null) {
+            final WindowManager wm = (WindowManager)
+                    getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+            final Display disp = wm.getDefaultDisplay();
+            sScreenSize = new Rect(0, 0, disp.getWidth(), disp.getHeight());
+        }
+        return sScreenSize;
     }
 }
