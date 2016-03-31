549ba2f7832dcb47c796619c207a402bc1fba636
Michael Froman <mfroman@mozilla.com>
2016-03-31 09:16:23 -0500

    Bug 906986 - Disable TestSrflxCandPairingFilter until bug 1226838 is fixed. r=bwc, r=drno
    MozReview-Commit-ID: 5QFUeqDG4dh

--- media/mtransport/test/ice_unittest.cpp
+++ media/mtransport/test/ice_unittest.cpp
@@ -3130,7 +3130,8 @@ TEST_F(WebRtcIceConnectTest, TestHostCandPairingFilter) {
   }
 }
 
-TEST_F(WebRtcIceConnectTest, TestSrflxCandPairingFilter) {
+// TODO Bug 1226838 - See Comment 2 - this test can't work as written
+TEST_F(WebRtcIceConnectTest, DISABLED_TestSrflxCandPairingFilter) {
   if (stun_server_address_.empty()) {
     return;
   }
