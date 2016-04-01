a76351a171c75a7d4a1fbc9641c7d37649d9c45f
Alexander Surkov <surkov.alexander@gmail.com>
2016-03-31 20:46:58 -0400

    Bug 1260237 - remove InvalidateChildren, r=yzen

--- accessible/html/HTMLSelectAccessible.cpp
+++ accessible/html/HTMLSelectAccessible.cpp
@@ -348,15 +348,6 @@ HTMLComboboxAccessible::NativeRole()
   return roles::COMBOBOX;
 }
 
-void
-HTMLComboboxAccessible::InvalidateChildren()
-{
-  AccessibleWrap::InvalidateChildren();
-
-  if (mListAccessible)
-    mListAccessible->InvalidateChildren();
-}
-
 bool
 HTMLComboboxAccessible::RemoveChild(Accessible* aChild)
 {
@@ -397,7 +388,10 @@ void
 HTMLComboboxAccessible::Shutdown()
 {
   MOZ_ASSERT(mDoc->IsDefunct() || !mListAccessible);
-  mListAccessible = nullptr;
+  if (mListAccessible) {
+    mListAccessible->Shutdown();
+    mListAccessible = nullptr;
+  }
 
   AccessibleWrap::Shutdown();
 }
