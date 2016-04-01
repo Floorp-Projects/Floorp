60dd65ca10f3fb6e5f140411bf4341d2d81e559f
Matt Woodrow <mwoodrow@mozilla.com>
2016-04-01 14:36:46 +1300

    Bug 1258843 - Don't build SVG display items if their visibility is hidden. r=dholbert, a=ritu
    --HG--
    extra : source : 2344e8a7c4b7849778c4df45bc67a510aae8c218

--- layout/svg/nsSVGOuterSVGFrame.cpp
+++ layout/svg/nsSVGOuterSVGFrame.cpp
@@ -746,7 +746,7 @@ nsSVGOuterSVGFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
     nsDisplayListSet set(contentList, contentList, contentList,
                          contentList, contentList, contentList);
     BuildDisplayListForNonBlockChildren(aBuilder, aDirtyRect, set);
-  } else {
+  } else if (IsVisibleForPainting(aBuilder) || !aBuilder->IsForPainting()) {
     aLists.Content()->AppendNewToTop(
       new (aBuilder) nsDisplayOuterSVG(aBuilder, this));
   }
