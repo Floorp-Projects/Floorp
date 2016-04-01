bc6d04759437c5f171230c3f0f88a90cad752301
Nick Alexander <nalexander@mozilla.com>
2016-04-01 14:28:42 -0700

    Bug 1261438 - Generalize Android artifact package names. r=chmanchester DONTBUILD NPOTB
    If the target triple is noisy, let's just accept -arm and -i386
    instead of being strict.  We can be more strict when things have
    settled.
    
    MozReview-Commit-ID: FDNJ3TuY51d
    
    --HG--
    extra : rebase_source : 3db70abe6818fbe0360406f20de899867232df02
    extra : amend_source : d324c229b1713adb844407078d6cc4d00d84b56d

--- python/mozbuild/mozbuild/artifacts.py
+++ python/mozbuild/mozbuild/artifacts.py
@@ -386,10 +386,9 @@ class WinArtifactJob(ArtifactJob):
 # https://tools.taskcluster.net/index/artifacts/#buildbot.branches.mozilla-central/buildbot.branches.mozilla-central.
 # The values correpsond to a pair of (<package regex>, <test archive regex>).
 JOB_DETAILS = {
-    # 'android-api-9': (AndroidArtifactJob, 'public/build/fennec-(.*)\.android-arm\.apk'),
-    'android-api-15': (AndroidArtifactJob, ('public/build/fennec-(.*)\.android-arm\.apk',
+    'android-api-15': (AndroidArtifactJob, ('public/build/fennec-(.*)-arm\.apk',
                                             None)),
-    'android-x86': (AndroidArtifactJob, ('public/build/fennec-(.*)\.android-i386\.apk',
+    'android-x86': (AndroidArtifactJob, ('public/build/fennec-(.*)-i386\.apk',
                                          None)),
     'linux': (LinuxArtifactJob, ('public/build/firefox-(.*)\.linux-i686\.tar\.bz2',
                                  'public/build/firefox-(.*)\.common\.tests\.zip')),
