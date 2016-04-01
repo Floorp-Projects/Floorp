d29072074adb4b3c008dcc44c0163d36d2b7d267
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-04-01 11:57:56 +0200

    Backed out changeset 11f8ba2d1e85 (bug 1209273)

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -1985,10 +1985,7 @@ nsCSSRendering::DetermineBackgroundColor(nsPresContext* aPresContext,
   aDrawBackgroundImage = true;
   aDrawBackgroundColor = true;
 
-  const nsStyleVisibility* visibility = aStyleContext->StyleVisibility();
-
-  if (visibility->mColorAdjust != NS_STYLE_COLOR_ADJUST_EXACT &&
-      aFrame->HonorPrintBackgroundSettings()) {
+  if (aFrame->HonorPrintBackgroundSettings()) {
     aDrawBackgroundImage = aPresContext->GetBackgroundImageDraw();
     aDrawBackgroundColor = aPresContext->GetBackgroundColorDraw();
   }
