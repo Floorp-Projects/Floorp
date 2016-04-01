a4e929c4c2fd112a190ab46514798c21ee967969
Mark Banner <standard8@mozilla.com>
2016-04-01 13:54:08 +0100

    Bug 1257154 - Switch to getting DOM title updates via a frame script to work better with e10s. r=mikedeboer, a=ritu
    --HG--
    extra : source : 1725b460c3e0de97590cb8764df9ace9115b813e

---
+++ browser/extensions/loop/chrome/content/modules/tabFrame.js
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+/* global sendAsyncMessage */
+
+/**
+ * This script runs in the content process and is attached to browsers when
+ * they are created.
+ */
+
+// Listen for when the title is changed and send a message back to the chrome
+// process.
+addEventListener("DOMTitleChanged", ({ target }) => {
+  sendAsyncMessage("loop@mozilla.org:DOMTitleChanged", {
+    details: "titleChanged"
+  }, {
+     target: target
+  });
+});
