0e81e69ccf0ca6b631375f9eda38848fdd54b470
Benjamin Smedberg <benjamin@smedbergs.us>
2016-04-01 10:42:50 -0400

    Bug 1229252 followup - normalize to lowercase, r=aklotz, a=lizzard
    MozReview-Commit-ID: 90Sz1b8V6L6
    
    --HG--
    extra : source : 6bd70447477358d8d2e9452d2f29c6bdddba9aab
    extra : amend_source : d0d7b11c6139a97e81161aec432301575a7e26e7

--- mozglue/build/WindowsDllBlocklist.cpp
+++ mozglue/build/WindowsDllBlocklist.cpp
@@ -197,7 +197,7 @@ static DllBlockInfo sWindowsDllBlocklist[] = {
   { "grabkernel.dll", MAKE_VERSION(1, 0, 0, 1) },
 
   // ESET, bug 1229252
-  { "eOppMonitor.dll", ALL_VERSIONS },
+  { "eoppmonitor.dll", ALL_VERSIONS },
 
   { nullptr, 0 }
 };
