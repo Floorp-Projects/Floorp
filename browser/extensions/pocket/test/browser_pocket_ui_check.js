b8ee05276e049fc3b6b471d5f1e581b27c0b39bb
Shane Caraveo <scaraveo@mozilla.com>
2016-04-01 08:39:48 -0700

    Bug 1259729 turn on pocket during pocket test, r=Gijs

--- browser/extensions/pocket/test/browser_pocket_ui_check.js
+++ browser/extensions/pocket/test/browser_pocket_ui_check.js
@@ -1,21 +1,54 @@
 "use strict";
 
-add_task(function*() {
-  let pocketAddon = yield new Promise(resolve => {
-    AddonManager.getAddonByID("firefox@getpocket.com", resolve);
-  });
-  if (!pocketAddon) {
-    ok(true, "Pocket is not installed");
-    return;
+function checkWindowProperties(expectPresent, l) {
+  for (let name of l) {
+    is(!!window.hasOwnProperty(name), expectPresent, "property " + name + (expectPresent ? " is" : " is not") + " present");
   }
-  if (!Services.prefs.getBoolPref("extensions.pocket.enabled")) {
-    ok(true, "Pocket add-on is not enabled");
-    return;
+}
+function checkElements(expectPresent, l) {
+  for (let id of l) {
+    is(!!document.getElementById(id), expectPresent, "element " + id + (expectPresent ? " is" : " is not") + " present");
   }
+}
 
-  for (let id of ["panelMenu_pocket", "menu_pocket", "BMB_pocket",
-                  "panelMenu_pocketSeparator", "menu_pocketSeparator",
-                  "BMB_pocketSeparator"]) {
-    ok(document.getElementById(id), "Should see element with id " + id);
-  }
+add_task(function*() {
+  let enabledOnStartup = yield promisePocketEnabled();
+  registerCleanupFunction(() => {
+    // Extra insurance that this is disabled again, but it should have been set
+    // in promisePocketReset.
+    Services.prefs.setBoolPref("extensions.pocket.enabled", enabledOnStartup);
+  });
+
+  checkWindowProperties(true, ["Pocket", "pktUI", "pktUIMessaging"]);
+  checkElements(true, ["pocket-button", "panelMenu_pocket", "menu_pocket", "BMB_pocket",
+                       "panelMenu_pocketSeparator", "menu_pocketSeparator",
+                       "BMB_pocketSeparator"]);
+
+  // check context menu exists
+  info("checking content context menu");
+  let tab = yield BrowserTestUtils.openNewForegroundTab(gBrowser, "https://example.com/browser/browser/extensions/pocket/test/test.html");
+
+  let contextMenu = document.getElementById("contentAreaContextMenu");
+  let popupShown = BrowserTestUtils.waitForEvent(contextMenu, "popupshown");
+  let popupHidden = BrowserTestUtils.waitForEvent(contextMenu, "popuphidden");
+  yield BrowserTestUtils.synthesizeMouseAtCenter("body", {
+    type: "contextmenu",
+    button: 2
+  }, tab.linkedBrowser);
+  yield popupShown;
+
+  checkElements(true, ["context-pocket", "context-savelinktopocket"]);
+
+  contextMenu.hidePopup();
+  yield popupHidden;
+  yield BrowserTestUtils.removeTab(tab);
+
+  yield promisePocketDisabled();
+
+  checkWindowProperties(false, ["Pocket", "pktUI", "pktUIMessaging"]);
+  checkElements(false, ["pocket-button", "panelMenu_pocket", "menu_pocket", "BMB_pocket",
+                       "panelMenu_pocketSeparator", "menu_pocketSeparator",
+                       "BMB_pocketSeparator", "context-pocket", "context-savelinktopocket"]);
+
+  yield promisePocketReset();
 });
