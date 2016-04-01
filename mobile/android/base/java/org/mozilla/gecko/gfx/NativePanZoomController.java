f7ff8546952787a7dd540f5e0ae5ed1608dc12fe
Wes Kocher <wkocher@mozilla.com>
2016-04-01 16:05:07 -0700

    Backed out changeset 95e61ede373d (bug 1257269) because it caused merge conflicts with m-c/fx-team
    MozReview-Commit-ID: 2Zt4WJqmlzu

--- mobile/android/base/java/org/mozilla/gecko/gfx/NativePanZoomController.java
+++ mobile/android/base/java/org/mozilla/gecko/gfx/NativePanZoomController.java
@@ -324,11 +324,6 @@ class NativePanZoomController extends JNIObject implements PanZoomController {
         }
     }
 
-    @WrapForJNI
-    private void setScrollingRootContent(final boolean isRootContent) {
-        mTarget.setScrollingRootContent(isRootContent);
-    }
-
     /**
      * Active SelectionCaretDrag requires DynamicToolbarAnimator to be pinned
      * to avoid unwanted scroll interactions.
