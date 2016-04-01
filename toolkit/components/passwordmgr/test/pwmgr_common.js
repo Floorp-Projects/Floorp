b17375a2f886d50866e1817e9a2178db068cda55
Timothy Guan-tin Chien <timdream@gmail.com>
2016-04-01 14:25:14 +0800

    Bug 1243722 - Introduce notUsernameSelector in passwordmgr recipes. r=MattN
    Additionally, add a recipe for Facebook.
    
    MozReview-Commit-ID: 2l60v7R0TPP

--- toolkit/components/passwordmgr/test/pwmgr_common.js
+++ toolkit/components/passwordmgr/test/pwmgr_common.js
@@ -282,6 +282,26 @@ function promiseFormsProcessed(expectedCount = 1) {
   });
 }
 
+function loadRecipes(recipes) {
+  return new Promise(resolve => {
+    chromeScript.addMessageListener("loadedRecipes", function loaded() {
+      chromeScript.removeMessageListener("loadedRecipes", loaded);
+      resolve(recipes);
+    });
+    chromeScript.sendAsyncMessage("loadRecipes", recipes);
+  });
+}
+
+function resetRecipes() {
+  return new Promise(resolve => {
+    chromeScript.addMessageListener("recipesReset", function reset() {
+      chromeScript.removeMessageListener("recipesReset", reset);
+      resolve();
+    });
+    chromeScript.sendAsyncMessage("resetRecipes");
+  });
+}
+
 /**
  * Run a function synchronously in the parent process and destroy it in the test cleanup function.
  * @param {Function|String} aFunctionOrURL - either a function that will be stringified and run
@@ -310,6 +330,7 @@ function runChecksAfterCommonInit(aFunction = null) {
     pwmgrCommonScript.addMessageListener("registerRunTests", () => registerRunTests());
   }
   pwmgrCommonScript.sendSyncMessage("setupParent");
+  return pwmgrCommonScript;
 }
 
 // Code to run when loaded as a chrome script in tests via loadChromeScript
@@ -332,6 +353,13 @@ if (this.addMessageListener) {
     var recipeParent = yield LoginManagerParent.recipeParentPromise;
     yield recipeParent.load(recipes);
     sendAsyncMessage("loadedRecipes", recipes);
+  }));
+
+  addMessageListener("resetRecipes", Task.async(function* resetRecipes() {
+    let { LoginManagerParent } = Cu.import("resource://gre/modules/LoginManagerParent.jsm", {});
+    let recipeParent = yield LoginManagerParent.recipeParentPromise;
+    yield recipeParent.reset();
+    sendAsyncMessage("recipesReset");
   }));
 
   var globalMM = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIMessageListenerManager);
