2fd4a808bdd5b2e2459dd0cab9bf67b4301775aa
Margaret Leibovic <margaret.leibovic@gmail.com>
2016-04-03 18:24:29 -0400

    Bug 1261713 - (Part 3) Refactor and add tests for extra experiment values support. r=sebastian
    MozReview-Commit-ID: A1enTZO7f9X
    
    --HG--
    extra : rebase_source : f920ca1f172fd6f01af932c3fc1291e093f864a3

--- mobile/android/tests/background/junit4/src/com/keepsafe/switchboard/TestSwitchboard.java
+++ mobile/android/tests/background/junit4/src/com/keepsafe/switchboard/TestSwitchboard.java
@@ -2,6 +2,8 @@ package com.keepsafe.switchboard;
 
 import android.content.Context;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +18,7 @@ import static org.junit.Assert.*;
 @RunWith(TestRunner.class)
 public class TestSwitchboard {
 
-    private static final String TEST_JSON = "{\"active-experiment\":{\"isActive\":true,\"values\":null},\"inactive-experiment\":{\"isActive\":false,\"values\":null}}";
+    private static final String TEST_JSON = "{\"active-experiment\":{\"isActive\":true,\"values\":{\"foo\": true}},\"inactive-experiment\":{\"isActive\":false,\"values\":null}}";
 
     @Before
     public void setUp() throws IOException {
@@ -38,8 +40,19 @@ public class TestSwitchboard {
     @Test
     public void testIsInExperiment() {
         final Context c = RuntimeEnvironment.application;
-        assertTrue("Active experiment is active", SwitchBoard.isInExperiment(c, "active-experiment"));
-        assertFalse("Inactive experiment is inactive", SwitchBoard.isInExperiment(c, "inactive-experiment"));
+        assertTrue("active-experiment is active", SwitchBoard.isInExperiment(c, "active-experiment"));
+        assertFalse("inactive-experiment is inactive", SwitchBoard.isInExperiment(c, "inactive-experiment"));
+    }
+
+    @Test
+    public void testExperimentValues() throws JSONException {
+        final Context c = RuntimeEnvironment.application;
+        assertTrue("active-experiment has values", SwitchBoard.hasExperimentValues(c, "active-experiment"));
+        assertFalse("inactive-experiment doesn't have values", SwitchBoard.hasExperimentValues(c, "inactive-experiment"));
+
+        final JSONObject values = SwitchBoard.getExperimentValuesFromJson(c, "active-experiment");
+        assertNotNull("active-experiment values are not null", values);
+        assertTrue("\"foo\" extra value is true", values.getBoolean("foo"));
     }
 
 }
