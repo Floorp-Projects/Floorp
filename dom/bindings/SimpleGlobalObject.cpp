c380dbce701a6c9cd1efde7a2282d03ea6c7f3dd
Boris Zbarsky <bzbarsky@mit.edu>
2016-04-04 12:29:16 -0400

    Bug 1246153 part 2.  Create a way to ask for a clean new global that works on both mainthread and workers.  r=bholley

---
+++ dom/bindings/SimpleGlobalObject.cpp
@@ -0,0 +1,169 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/SimpleGlobalObject.h"
+
+#include "jsapi.h"
+#include "js/Class.h"
+
+#include "nsContentUtils.h"
+#include "nsJSPrincipals.h"
+#include "nsNullPrincipal.h"
+#include "nsThreadUtils.h"
+
+#include "xpcprivate.h"
+
+namespace mozilla {
+namespace dom {
+
+NS_IMPL_CYCLE_COLLECTION_CLASS(SimpleGlobalObject)
+
+NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(SimpleGlobalObject)
+  NS_IMPL_CYCLE_COLLECTION_UNLINK_PRESERVED_WRAPPER
+  tmp->UnlinkHostObjectURIs();
+NS_IMPL_CYCLE_COLLECTION_UNLINK_END
+
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN(SimpleGlobalObject)
+
+  NS_IMPL_CYCLE_COLLECTION_TRAVERSE_SCRIPT_OBJECTS
+  tmp->TraverseHostObjectURIs(cb);
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
+
+NS_IMPL_CYCLE_COLLECTION_TRACE_WRAPPERCACHE(SimpleGlobalObject)
+
+NS_IMPL_CYCLE_COLLECTING_ADDREF(SimpleGlobalObject)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(SimpleGlobalObject)
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(SimpleGlobalObject)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsIGlobalObject)
+NS_INTERFACE_MAP_END
+
+static bool
+SimpleGlobal_enumerate(JSContext *cx, JS::Handle<JSObject *> obj)
+{
+  return JS_EnumerateStandardClasses(cx, obj);
+}
+
+static bool
+SimpleGlobal_resolve(JSContext *cx, JS::Handle<JSObject *> obj,
+                    JS::Handle<jsid> id, bool *resolvedp)
+{
+  return JS_ResolveStandardClass(cx, obj, id, resolvedp);
+}
+
+static void
+SimpleGlobal_finalize(js::FreeOp *fop, JSObject *obj)
+{
+  SimpleGlobalObject* globalObject =
+    static_cast<SimpleGlobalObject*>(JS_GetPrivate(obj));
+  NS_RELEASE(globalObject);
+}
+
+static void
+SimpleGlobal_moved(JSObject *obj, const JSObject *old)
+{
+  SimpleGlobalObject* globalObject =
+    static_cast<SimpleGlobalObject*>(JS_GetPrivate(obj));
+  globalObject->UpdateWrapper(obj, old);
+}
+
+const js::Class SimpleGlobalClass = {
+    "",
+    JSCLASS_GLOBAL_FLAGS | JSCLASS_HAS_PRIVATE | JSCLASS_PRIVATE_IS_NSISUPPORTS,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    SimpleGlobal_enumerate,
+    SimpleGlobal_resolve,
+    nullptr,
+    SimpleGlobal_finalize,
+    nullptr,
+    nullptr,
+    nullptr,
+    JS_GlobalObjectTraceHook,
+    JS_NULL_CLASS_SPEC, {
+      false,
+      nullptr,
+      SimpleGlobal_moved
+    }, JS_NULL_OBJECT_OPS
+};
+
+// static
+JSObject*
+SimpleGlobalObject::Create(GlobalType globalType, JS::Handle<JS::Value> proto)
+{
+  JSContext* cx = nsContentUtils::GetDefaultJSContextForThread();
+  JSAutoRequest ar(cx);
+
+  JS::CompartmentOptions options;
+  options.creationOptions().setInvisibleToDebugger(true);
+
+  nsCOMPtr<nsIPrincipal> principal;
+  if (NS_IsMainThread()) {
+    principal = nsNullPrincipal::Create();
+    if (!principal) {
+      return nullptr;
+    }
+  }
+
+  JS::Rooted<JSObject*> global(cx,
+    JS_NewGlobalObject(cx, js::Jsvalify(&SimpleGlobalClass),
+                       nsJSPrincipals::get(principal),
+                       JS::DontFireOnNewGlobalHook, options));
+
+  if (!global) {
+    JS_ClearPendingException(cx);
+    return nullptr;
+  }
+
+  JSAutoCompartment ac(cx, global);
+
+  // It's important to create the nsIGlobalObject for our new global before we
+  // start trying to wrap things like the prototype into its compartment,
+  // because the wrap operation relies on the global having its nsIGlobalObject
+  // already.
+  RefPtr<SimpleGlobalObject> globalObject =
+    new SimpleGlobalObject(global, globalType);
+
+  // Pass on ownership of globalObject to |global|.
+  JS_SetPrivate(global, globalObject.forget().take());
+
+  if (proto.isObjectOrNull()) {
+    JS::Rooted<JSObject*> protoObj(cx, proto.toObjectOrNull());
+    if (!JS_WrapObject(cx, &protoObj)) {
+      JS_ClearPendingException(cx);
+      return nullptr;
+    }
+
+    if (!JS_SetPrototype(cx, global, protoObj)) {
+      JS_ClearPendingException(cx);
+      return nullptr;
+    }
+  } else if (!proto.isUndefined()) {
+    // Bogus proto.
+    return nullptr;
+  }
+
+  JS_FireOnNewGlobalObject(cx, global);
+  return global;
+}
+
+// static
+SimpleGlobalObject::GlobalType
+SimpleGlobalObject::SimpleGlobalType(JSObject* obj)
+{
+  if (js::GetObjectClass(obj) != &SimpleGlobalClass) {
+    return SimpleGlobalObject::GlobalType::NotSimpleGlobal;
+  }
+
+  SimpleGlobalObject* globalObject =
+    static_cast<SimpleGlobalObject*>(JS_GetPrivate(obj));
+  return globalObject->Type();
+}
+
+} // namespace mozilla
+} // namespace dom
--- dom/workers/WorkerScope.cpp
+++ dom/workers/WorkerScope.cpp
@@ -19,6 +19,7 @@
 #include "mozilla/dom/PromiseWorkerProxy.h"
 #include "mozilla/dom/ServiceWorkerGlobalScopeBinding.h"
 #include "mozilla/dom/SharedWorkerGlobalScopeBinding.h"
+#include "mozilla/dom/SimpleGlobalObject.h"
 #include "mozilla/dom/WorkerDebuggerGlobalScopeBinding.h"
 #include "mozilla/dom/WorkerGlobalScopeBinding.h"
 #include "mozilla/dom/WorkerLocation.h"
@@ -710,109 +711,6 @@ WorkerDebuggerGlobalScope::GetGlobal(JSContext* aCx,
   aGlobal.set(scope->GetWrapper());
 }
 
-class WorkerDebuggerSandboxPrivate : public nsIGlobalObject,
-                                     public nsWrapperCache
-{
-public:
-  explicit WorkerDebuggerSandboxPrivate(JSObject *global)
-  {
-    SetWrapper(global);
-  }
-
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
-  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS_AMBIGUOUS(WorkerDebuggerSandboxPrivate,
-                                                         nsIGlobalObject)
-
-  virtual JSObject *GetGlobalJSObject() override
-  {
-    return GetWrapper();
-  }
-
-  virtual JSObject* WrapObject(JSContext* cx,
-                               JS::Handle<JSObject*> aGivenProto) override
-  {
-    MOZ_CRASH("WorkerDebuggerSandboxPrivate doesn't use DOM bindings!");
-  }
-
-private:
-  virtual ~WorkerDebuggerSandboxPrivate()
-  {
-    ClearWrapper();
-  }
-};
-
-NS_IMPL_CYCLE_COLLECTION_CLASS(WorkerDebuggerSandboxPrivate)
-
-NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(WorkerDebuggerSandboxPrivate)
-  NS_IMPL_CYCLE_COLLECTION_UNLINK_PRESERVED_WRAPPER
-  tmp->UnlinkHostObjectURIs();
-NS_IMPL_CYCLE_COLLECTION_UNLINK_END
-
-NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN(WorkerDebuggerSandboxPrivate)
-
-  NS_IMPL_CYCLE_COLLECTION_TRAVERSE_SCRIPT_OBJECTS
-  tmp->TraverseHostObjectURIs(cb);
-NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
-
-NS_IMPL_CYCLE_COLLECTION_TRACE_WRAPPERCACHE(WorkerDebuggerSandboxPrivate)
-
-NS_IMPL_CYCLE_COLLECTING_ADDREF(WorkerDebuggerSandboxPrivate)
-NS_IMPL_CYCLE_COLLECTING_RELEASE(WorkerDebuggerSandboxPrivate)
-NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(WorkerDebuggerSandboxPrivate)
-  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
-  NS_INTERFACE_MAP_ENTRY(nsIGlobalObject)
-NS_INTERFACE_MAP_END
-
-static bool
-workerdebuggersandbox_enumerate(JSContext *cx, JS::Handle<JSObject *> obj)
-{
-  return JS_EnumerateStandardClasses(cx, obj);
-}
-
-static bool
-workerdebuggersandbox_resolve(JSContext *cx, JS::Handle<JSObject *> obj,
-                              JS::Handle<jsid> id, bool *resolvedp)
-{
-  return JS_ResolveStandardClass(cx, obj, id, resolvedp);
-}
-
-static void
-workerdebuggersandbox_finalize(js::FreeOp *fop, JSObject *obj)
-{
-  WorkerDebuggerSandboxPrivate *sandboxPrivate =
-    static_cast<WorkerDebuggerSandboxPrivate *>(JS_GetPrivate(obj));
-  NS_RELEASE(sandboxPrivate);
-}
-
-static void
-workerdebuggersandbox_moved(JSObject *obj, const JSObject *old)
-{
-  WorkerDebuggerSandboxPrivate *sandboxPrivate =
-    static_cast<WorkerDebuggerSandboxPrivate *>(JS_GetPrivate(obj));
-  sandboxPrivate->UpdateWrapper(obj, old);
-}
-
-const js::Class workerdebuggersandbox_class = {
-    "workerdebuggersandbox",
-    JSCLASS_GLOBAL_FLAGS | JSCLASS_HAS_PRIVATE | JSCLASS_PRIVATE_IS_NSISUPPORTS,
-    nullptr,
-    nullptr,
-    nullptr,
-    nullptr,
-    workerdebuggersandbox_enumerate,
-    workerdebuggersandbox_resolve,
-    nullptr, /* mayResolve */
-    workerdebuggersandbox_finalize,
-    nullptr,
-    nullptr,
-    nullptr,
-    JS_GlobalObjectTraceHook,
-    JS_NULL_CLASS_SPEC, {
-      false,
-      nullptr,
-      workerdebuggersandbox_moved
-    }, JS_NULL_OBJECT_OPS
-};
 
 void
 WorkerDebuggerGlobalScope::CreateSandbox(JSContext* aCx, const nsAString& aName,
@@ -824,39 +722,16 @@ WorkerDebuggerGlobalScope::CreateSandbox(JSContext* aCx, const nsAString& aName,
 
   aResult.set(nullptr);
 
-  JS::CompartmentOptions options;
-  options.creationOptions().setInvisibleToDebugger(true);
-
+  JS::Rooted<JS::Value> protoVal(aCx);
+  protoVal.setObjectOrNull(aPrototype);
   JS::Rooted<JSObject*> sandbox(aCx,
-    JS_NewGlobalObject(aCx, js::Jsvalify(&workerdebuggersandbox_class), nullptr,
-                       JS::DontFireOnNewGlobalHook, options));
+    SimpleGlobalObject::Create(SimpleGlobalObject::GlobalType::WorkerDebuggerSandbox,
+                               protoVal));
+
   if (!sandbox) {
-    aRv.NoteJSContextException(aCx);
+    aRv.Throw(NS_ERROR_OUT_OF_MEMORY);
     return;
   }
-
-  {
-    JSAutoCompartment ac(aCx, sandbox);
-
-    JS::Rooted<JSObject*> prototype(aCx, aPrototype);
-    if (!JS_WrapObject(aCx, &prototype)) {
-      aRv.NoteJSContextException(aCx);
-      return;
-    }
-
-    if (!JS_SetPrototype(aCx, sandbox, prototype)) {
-      aRv.NoteJSContextException(aCx);
-      return;
-    }
-
-    RefPtr<WorkerDebuggerSandboxPrivate> sandboxPrivate =
-      new WorkerDebuggerSandboxPrivate(sandbox);
-
-    // Pass on ownership of sandboxPrivate to |sandbox|.
-    JS_SetPrivate(sandbox, sandboxPrivate.forget().take());
-  }
-
-  JS_FireOnNewGlobalObject(aCx, sandbox);
 
   if (!JS_WrapObject(aCx, &sandbox)) {
     aRv.NoteJSContextException(aCx);
@@ -1000,7 +875,7 @@ GetGlobalObjectForGlobal(JSObject* global)
 
     if (!globalObject) {
       MOZ_ASSERT(IsDebuggerSandbox(global));
-      globalObject = static_cast<WorkerDebuggerSandboxPrivate*>(JS_GetPrivate(global));
+      globalObject = static_cast<SimpleGlobalObject*>(JS_GetPrivate(global));
 
       MOZ_ASSERT(globalObject);
     }
@@ -1028,7 +903,8 @@ IsDebuggerGlobal(JSObject* object)
 bool
 IsDebuggerSandbox(JSObject* object)
 {
-  return js::GetObjectClass(object) == &workerdebuggersandbox_class;
+  return SimpleGlobalObject::SimpleGlobalType(object) ==
+    SimpleGlobalObject::GlobalType::WorkerDebuggerSandbox;
 }
 
 bool
