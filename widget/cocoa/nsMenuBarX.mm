b8403645d520875c21d3937b2754ef22f25dcdaa
Stephen A Pohl <spohl.mozilla.bugs@gmail.com>
2016-04-05 10:42:00 +0200

    Bug 1199547 - Backout workaround from bug 722676 as it causes issues on at least OSX 10.10 and 10.11. r=mstange

--- widget/cocoa/nsMenuBarX.mm
+++ widget/cocoa/nsMenuBarX.mm
@@ -30,8 +30,7 @@
 #include "nsToolkitCompsCID.h"
 
 NativeMenuItemTarget* nsMenuBarX::sNativeEventTarget = nil;
-nsMenuBarX* nsMenuBarX::sLastGeckoMenuBarPainted = nullptr; // Weak
-nsMenuBarX* nsMenuBarX::sCurrentPaintDelayedMenuBar = nullptr; // Weak
+nsMenuBarX* nsMenuBarX::sLastGeckoMenuBarPainted = nullptr;
 NSMenu* sApplicationMenu = nil;
 BOOL sApplicationMenuIsFallback = NO;
 BOOL gSomeMenuBarPainted = NO;
@@ -58,11 +57,11 @@ NS_IMETHODIMP nsNativeMenuServiceX::CreateNativeMenuBar(nsIWidget* aParent, nsIC
 }
 
 nsMenuBarX::nsMenuBarX()
-: nsMenuGroupOwnerX(), mParentWindow(nullptr), mAwaitingDelayedPaint(false)
+: nsMenuGroupOwnerX(), mParentWindow(nullptr)
 {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK;
 
-  mNativeMenu = [[GeckoNSMenu alloc] initWithTitle:@"MainMenuBar" andMenuBarOwner:this];
+  mNativeMenu = [[GeckoNSMenu alloc] initWithTitle:@"MainMenuBar"];
 
   NS_OBJC_END_TRY_ABORT_BLOCK;
 }
@@ -94,7 +93,6 @@ nsMenuBarX::~nsMenuBarX()
   // before the registration hash table is destroyed.
   mMenuArray.Clear();
 
-  [mNativeMenu resetMenuBarOwner];
   [mNativeMenu release];
 
   NS_OBJC_END_TRY_ABORT_BLOCK;
@@ -397,14 +395,9 @@ void nsMenuBarX::SetSystemHelpMenu()
   }
 }
 
-nsresult nsMenuBarX::Paint(bool aDelayed)
+nsresult nsMenuBarX::Paint()
 {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT;
-
-  if (!aDelayed && mAwaitingDelayedPaint) {
-    return NS_OK;
-  }
-  mAwaitingDelayedPaint = false;
 
   // Don't try to optimize anything in this painting by checking
   // sLastGeckoMenuBarPainted because the menubar can be manipulated by
@@ -415,36 +408,13 @@ nsresult nsMenuBarX::Paint(bool aDelayed)
   NSMenu* outgoingMenu = [NSApp mainMenu];
   NS_ASSERTION([outgoingMenu numberOfItems] > 0, "Main menu does not have any items, something is terribly wrong!");
 
-  // To work around bug 722676, we sometimes need to delay making mNativeMenu
-  // the main menu.  This is an Apple bug that sometimes causes a top-level
-  // menu item to remain highlighted after pressing a Cmd+key combination that
-  // opens a new window, then closing the window.  The OS temporarily
-  // highlights the appropriate top-level menu item whenever you press the
-  // Cmd+key combination for one of its submenus.  (It does this by setting a
-  // "pressed" attribute on it.)  The OS then uses a timer to remove this
-  // "pressed" attribute.  But without our workaround we sometimes change the
-  // main menu before the timer has fired, so when it fires the menu item it
-  // was intended to unhighlight is no longer present in the main menu.  This
-  // causes the item to remain semi-permanently highlighted (until you quit
-  // Firefox or navigate the main menu by hand).
-  if ((outgoingMenu != mNativeMenu) &&
-      [outgoingMenu isKindOfClass:[GeckoNSMenu class]]) {
-    if (aDelayed) {
-      [(GeckoNSMenu *)outgoingMenu setDelayResignMainMenu:false];
-    } else if ([(GeckoNSMenu *)outgoingMenu delayResignMainMenu]) {
-      PaintMenuBarAfterDelay();
-      return NS_OK;
-    }
-  }
+  NSMenuItem* appMenuItem = [[outgoingMenu itemAtIndex:0] retain];
+  [outgoingMenu removeItemAtIndex:0];
+  [mNativeMenu insertItem:appMenuItem atIndex:0];
+  [appMenuItem release];
 
-  if (outgoingMenu != mNativeMenu) {
-    NSMenuItem* appMenuItem = [[outgoingMenu itemAtIndex:0] retain];
-    [outgoingMenu removeItemAtIndex:0];
-    [mNativeMenu insertItem:appMenuItem atIndex:0];
-    [appMenuItem release];
-    // Set menu bar and event target.
-    [NSApp setMainMenu:mNativeMenu];
-  }
+  // Set menu bar and event target.
+  [NSApp setMainMenu:mNativeMenu];
   SetSystemHelpMenu();
   nsMenuBarX::sLastGeckoMenuBarPainted = this;
 
@@ -453,19 +423,6 @@ nsresult nsMenuBarX::Paint(bool aDelayed)
   return NS_OK;
 
   NS_OBJC_END_TRY_ABORT_BLOCK_NSRESULT;
-}
-
-// Used to delay a call to nsMenuBarX::Paint().  Needed to work around
-// bug 722676.
-void nsMenuBarX::PaintMenuBarAfterDelay()
-{
-  mAwaitingDelayedPaint = true;
-  nsMenuBarX::sCurrentPaintDelayedMenuBar = this;
-  [mNativeMenu retain];
-  // The delay for Apple's unhighlight timer is 0.1f, so we make ours a bit longer.
-  [mNativeMenu performSelector:@selector(delayedPaintMenuBar:)
-                    withObject:nil
-                    afterDelay:0.15f];
 }
 
 // Returns the 'key' attribute of the 'shortcutID' object (if any) in the
@@ -816,66 +773,6 @@ void nsMenuBarX::SetParent(nsIWidget* aParent)
 static BOOL gMenuItemsExecuteCommands = YES;
 
 @implementation GeckoNSMenu
-
-- (id)initWithTitle:(NSString *)aTitle
-{
-  if ((self = [super initWithTitle:aTitle])) {
-    mMenuBarOwner = nullptr;
-    mDelayResignMainMenu = false;
-  }
-  return self;
-}
-
-- (id)initWithTitle:(NSString *)aTitle andMenuBarOwner:(nsMenuBarX *)aMenuBarOwner
-{
-  if ((self = [super initWithTitle:aTitle])) {
-    mMenuBarOwner = aMenuBarOwner;
-    mDelayResignMainMenu = false;
-  }
-  return self;
-}
-
-- (void)resetMenuBarOwner
-{
-  mMenuBarOwner = nil;
-}
-
-- (bool)delayResignMainMenu
-{
-  return mDelayResignMainMenu;
-}
-
-- (void)setDelayResignMainMenu:(bool)aShouldDelay
-{
-  mDelayResignMainMenu = aShouldDelay;
-}
-
-// Used to delay a call to nsMenuBarX::Paint().  Needed to work around
-// bug 722676.
-- (void)delayedPaintMenuBar:(id)unused
-{
-  if (mMenuBarOwner) {
-    if (mMenuBarOwner == nsMenuBarX::sCurrentPaintDelayedMenuBar) {
-      mMenuBarOwner->Paint(true);
-      nsMenuBarX::sCurrentPaintDelayedMenuBar = nullptr;
-    } else {
-      mMenuBarOwner->ResetAwaitingDelayedPaint();
-    }
-  }
-  [self release];
-}
-
-// Undocumented method, present unchanged since OS X 10.6, used to temporarily
-// highlight a top-level menu item when an appropriate Cmd+key combination is
-// pressed.
-- (void)_performActionWithHighlightingForItemAtIndex:(NSInteger)index
-{
-  NSMenu *mainMenu = [NSApp mainMenu];
-  if ([mainMenu isKindOfClass:[GeckoNSMenu class]]) {
-    [(GeckoNSMenu *)mainMenu setDelayResignMainMenu:true];
-  }
-  [super _performActionWithHighlightingForItemAtIndex:index];
-}
 
 // Keyboard commands should not cause menu items to invoke their
 // commands when there is a key window because we'd rather send
