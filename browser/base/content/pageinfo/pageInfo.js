c231fd6585573aae0696130946abf508c65314b7
Mike Conley <mconley@mozilla.com>
2016-04-05 12:39:24 -0400

    Bug 1255773 - Account for multiple media elements per node for Page Info. r=florian, a=lizzard
    MozReview-Commit-ID: GwzIYVsLgue

--- browser/base/content/pageinfo/pageInfo.js
+++ browser/base/content/pageinfo/pageInfo.js
@@ -405,7 +405,10 @@ function loadPageInfo(frameOuterWindowID, imageElement)
       return;
     }
 
-    addImage(message.data.imageViewRow);
+    for (let item of message.data.mediaItems) {
+      addImage(item);
+    }
+
     selectImage();
   });
 
