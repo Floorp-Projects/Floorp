35deaa95eecd7886ce2f821a309eee0c9abcb13b
Seth Fowler <mark.seth.fowler@gmail.com>
2016-04-05 01:11:14 -0700

    Bug 1261553 - Don't return from OnVisibilityChanged implementations without calling the superclass implementation. r=mstange

--- layout/generic/nsImageFrame.cpp
+++ layout/generic/nsImageFrame.cpp
@@ -2151,6 +2151,7 @@ nsImageFrame::OnVisibilityChange(Visibility aNewVisibility,
   nsCOMPtr<nsIImageLoadingContent> imageLoader = do_QueryInterface(mContent);
   if (!imageLoader) {
     MOZ_ASSERT_UNREACHABLE("Should have an nsIImageLoadingContent");
+    ImageFrameSuper::OnVisibilityChange(aNewVisibility, aNonvisibleAction);
     return;
   }
 
