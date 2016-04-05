56a10504076ba777a0f1a139e15d7f87f830de99
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-04-05 08:54:01 +0200

    Backed out changeset 864320b75e76 (bug 1259466) for suspicion this caused bc 7 test failures on m-c

--- testing/mozharness/configs/unittests/win_unittest.py
+++ testing/mozharness/configs/unittests/win_unittest.py
@@ -196,6 +196,11 @@ config = {
                         "--setpref=layers.acceleration.disabled=true"],
             "tests": ["tests/reftest/tests/layout/reftests/reftest.list"]
         },
+        "reftest-omtc": {
+            "options": ["--suite=reftest",
+                        "--setpref=layers.offmainthreadcomposition.enabled=true"],
+            "tests": ["tests/reftest/tests/layout/reftests/reftest.list"]
+        },
         "crashtest-ipc": {
             "options": ["--suite=crashtest",
                         '--setpref=browser.tabs.remote=true',
