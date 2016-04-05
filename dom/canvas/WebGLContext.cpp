c4eb2702ab6ca4b936e0d87cc9443f45741721aa
Wes Kocher <wkocher@mozilla.com>
2016-04-05 13:56:54 -0700

    Backed out changeset 7725b6550258 (bug 1261179) for breaking android mochitest(gl) tests CLOSED TREE
    MozReview-Commit-ID: EVFF5STXkLl

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -695,9 +695,6 @@ WebGLContext::CreateAndInitGL(bool forceEnabled)
     if (!disableANGLE) {
         if (CreateAndInitGLWith(CreateGLWithANGLE, baseCaps, flags))
             return true;
-
-        // Don't fallback to WGL.
-        return false;
     }
 
     MOZ_ASSERT(!gl);
