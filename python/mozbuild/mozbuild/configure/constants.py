2be15171f80c0209de6c91f8f6a6671a735ad3c0
Mike Hommey <mh+mozilla@glandium.org>
2016-04-06 09:56:35 +0900

    Bug 1264482 - Use the EnumString type for the different values we get out of split_triplet. r=ted

--- python/mozbuild/mozbuild/configure/constants.py
+++ python/mozbuild/mozbuild/configure/constants.py
@@ -13,3 +13,49 @@ CompilerType = EnumString.subclass(
     'gcc',
     'msvc',
 )
+
+OS = EnumString.subclass(
+    'Android',
+    'DragonFly',
+    'FreeBSD',
+    'GNU',
+    'iOS',
+    'NetBSD',
+    'OpenBSD',
+    'OSX',
+    'WINNT',
+)
+
+Kernel = EnumString.subclass(
+    'Darwin',
+    'DragonFly',
+    'FreeBSD',
+    'kFreeBSD',
+    'Linux',
+    'NetBSD',
+    'OpenBSD',
+    'WINNT',
+)
+
+CPU = EnumString.subclass(
+    'aarch64',
+    'Alpha',
+    'arm',
+    'hppa',
+    'ia64',
+    'mips32',
+    'mips64',
+    'ppc',
+    'ppc64',
+    's390',
+    's390x',
+    'sparc',
+    'sparc64',
+    'x86',
+    'x86_64',
+)
+
+Endianness = EnumString.subclass(
+    'big',
+    'little',
+)
