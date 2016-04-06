e62e2c820b2120eec9819311956b9e9070006a7e
Mike Hommey <mh+mozilla@glandium.org>
2016-04-06 09:35:58 +0900

    Bug 1264482 - Use the EnumString type for the compiler type. r=ted

---
+++ python/mozbuild/mozbuild/configure/constants.py
@@ -0,0 +1,15 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from mozbuild.util import EnumString
+
+
+CompilerType = EnumString.subclass(
+    'clang',
+    'clang-cl',
+    'gcc',
+    'msvc',
+)
