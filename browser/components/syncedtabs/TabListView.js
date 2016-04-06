b9d8f2d3719d702ac3870bf2241a719fbbe6d992
Kit Cambridge <kcambridge@mozilla.com>
2016-04-05 17:06:00 -0700

    Bug 1262310 - Fix opening synced tabs from the sidebar. r=markh
    MozReview-Commit-ID: 8VHbEqsyuF2
    
    --HG--
    extra : rebase_source : 04924f295134834cf66c0d9c4a31d43c614b3abf

--- browser/components/syncedtabs/TabListView.js
+++ browser/components/syncedtabs/TabListView.js
@@ -255,7 +255,7 @@ TabListView.prototype = {
     if (itemNode.classList.contains("tab")) {
       let url = itemNode.dataset.url;
       if (url) {
-        this.props.onOpenTab(url, event);
+        this.onOpenSelected(url, event);
       }
     }
 
@@ -285,7 +285,7 @@ TabListView.prototype = {
     } else if (event.keyCode == this._window.KeyEvent.DOM_VK_RETURN) {
       let selectedNode = this.container.querySelector('.item.selected');
       if (selectedNode.dataset.url) {
-        this.props.onOpenTab(selectedNode.dataset.url, event);
+        this.onOpenSelected(selectedNode.dataset.url, event);
       } else if (selectedNode) {
         this.props.onToggleBranch(selectedNode.dataset.id);
       }
@@ -307,7 +307,12 @@ TabListView.prototype = {
     }
   },
 
-  onOpenSelected(event) {
+  onOpenSelected(url, event) {
+    let where = getChromeWindow(this._window).whereToOpenLink(event);
+    this.props.onOpenTab(url, where, {});
+  },
+
+  onOpenSelectedFromContextMenu(event) {
     let item = this._getSelectedTabNode();
     if (item) {
       let where = event.target.getAttribute("where");
@@ -422,7 +427,7 @@ TabListView.prototype = {
       case "syncedTabsOpenSelectedInTab":
       case "syncedTabsOpenSelectedInWindow":
       case "syncedTabsOpenSelectedInPrivateWindow":
-        this.onOpenSelected(event);
+        this.onOpenSelectedFromContextMenu(event);
         break;
       case "syncedTabsBookmarkSelected":
         this.onBookmarkTab();
