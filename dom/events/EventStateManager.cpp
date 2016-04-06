c17191a112c99f40112976fc249f4d39f6150cea
Neil Deakin <neil@mozilla.com>
2016-04-06 10:35:39 -0400

    Bug 1256952, send a dragexit at remote process when leaving the remote frame, r=smaug

--- dom/events/EventStateManager.cpp
+++ dom/events/EventStateManager.cpp
@@ -4517,6 +4517,13 @@ EventStateManager::FireDragEnterOrExit(nsPresContext* aPresContext,
   // Finally dispatch the event to the frame
   if (aTargetFrame)
     aTargetFrame->HandleEvent(aPresContext, &event, &status);
+
+  if (aMessage == eDragExit && IsRemoteTarget(aTargetContent)) {
+    nsEventStatus status = nsEventStatus_eIgnore;
+    WidgetDragEvent remoteEvent(aDragEvent->IsTrusted(), aMessage, aDragEvent->widget);
+    remoteEvent.AssignDragEventData(*aDragEvent, true);
+    HandleCrossProcessEvent(&remoteEvent, &status);
+  }
 }
 
 void
