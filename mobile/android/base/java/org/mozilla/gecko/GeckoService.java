a7ba74e1e03247b35e19388a985722b193780e4b
Jim Chen <nchen@mozilla.com>
2016-04-05 21:43:40 -0400

    Bug 1259653 - Respect the profile when starting GeckoService; r=snorp In GeckoService, wait until we get an Intent before starting GeckoThread using the profile information contained in the Intent.

--- mobile/android/base/java/org/mozilla/gecko/GeckoService.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoService.java
@@ -6,25 +6,36 @@
 package org.mozilla.gecko;
 
 import android.app.AlarmManager;
-import android.app.IntentService;
+import android.app.Service;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.IBinder;
 import android.util.Log;
+
+import java.io.File;
 
 import org.mozilla.gecko.util.NativeEventListener;
 import org.mozilla.gecko.util.NativeJSObject;
 import org.mozilla.gecko.util.EventCallback;
 
-public class GeckoService extends IntentService {
+public class GeckoService extends Service {
 
     private static final String LOGTAG = "GeckoService";
     private static final boolean DEBUG = false;
+
+    private static final String INTENT_PROFILE_NAME = "org.mozilla.gecko.intent.PROFILE_NAME";
+    private static final String INTENT_PROFILE_DIR = "org.mozilla.gecko.intent.PROFILE_DIR";
 
     private static class EventListener implements NativeEventListener {
 
         private PendingIntent getIntentForAction(final Context context, final String action) {
             final Intent intent = new Intent(action, /* uri */ null, context, GeckoService.class);
+            final GeckoProfile profile = GeckoThread.getActiveProfile();
+            if (profile != null) {
+                intent.putExtra(INTENT_PROFILE_NAME, profile.getName());
+                intent.putExtra(INTENT_PROFILE_DIR, profile.getDir().getAbsolutePath());
+            }
             return PendingIntent.getService(context, /* requestCode */ 0, intent,
                                             PendingIntent.FLAG_CANCEL_CURRENT);
         }
@@ -57,10 +68,6 @@ public class GeckoService extends IntentService {
 
     private static final EventListener EVENT_LISTENER = new EventListener();
 
-    public GeckoService() {
-        super("GeckoService");
-    }
-
     public static void register() {
         if (DEBUG) {
             Log.d(LOGTAG, "Registered listener");
@@ -87,9 +94,6 @@ public class GeckoService extends IntentService {
         if (DEBUG) {
             Log.d(LOGTAG, "Created");
         }
-        GeckoThread.ensureInit(/* profile */ null, /* args */ null,
-                               /* action */ null, /* debugging */ false);
-        GeckoThread.launch();
     }
 
     @Override // Service
@@ -103,17 +107,33 @@ public class GeckoService extends IntentService {
         super.onDestroy();
     }
 
-    @Override // IntentService
-    protected void onHandleIntent(final Intent intent) {
-
-        if (intent == null) {
-            return;
-        }
-
-        // We are on the intent service worker thread.
+    private int handleIntent(final Intent intent, final int startId) {
         if (DEBUG) {
             Log.d(LOGTAG, "Handling " + intent.getAction());
         }
+
+        final String profileName = intent.getStringExtra(INTENT_PROFILE_NAME);
+        final String profileDir = intent.getStringExtra(INTENT_PROFILE_DIR);
+
+        if (profileName == null || profileDir == null) {
+            throw new IllegalArgumentException("Intent must specify profile.");
+        }
+
+        if (!GeckoThread.initWithProfile(profileName != null ? profileName : "",
+                                         new File(profileDir))) {
+            Log.w(LOGTAG, "Ignoring due to profile mismatch: " +
+                          profileName + " [" + profileDir + ']');
+
+            final GeckoProfile profile = GeckoThread.getActiveProfile();
+            if (profile != null) {
+                Log.w(LOGTAG, "Current profile is " + profile.getName() +
+                              " [" + profile.getDir().getAbsolutePath() + ']');
+            }
+            stopSelf(startId);
+            return Service.START_NOT_STICKY;
+        }
+
+        GeckoThread.launch();
 
         switch (intent.getAction()) {
         case "update-addons":
@@ -124,5 +144,26 @@ public class GeckoService extends IntentService {
         default:
             Log.w(LOGTAG, "Unknown request: " + intent);
         }
+
+        stopSelf(startId);
+        return Service.START_NOT_STICKY;
+    }
+
+    @Override // Service
+    public int onStartCommand(final Intent intent, final int flags, final int startId) {
+        if (intent == null) {
+            return Service.START_NOT_STICKY;
+        }
+        try {
+            return handleIntent(intent, startId);
+        } catch (final Throwable e) {
+            Log.e(LOGTAG, "Cannot handle intent: " + intent, e);
+            return Service.START_NOT_STICKY;
+        }
+    }
+
+    @Override // Service
+    public IBinder onBind(final Intent intent) {
+        return null;
     }
 }
