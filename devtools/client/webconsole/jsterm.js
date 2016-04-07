ca7d4e05b13f98fd6dc0689cf0a8e9098ef1bf08
gasolin <gasolin@gmail.com>
2016-04-07 14:43:39 +0800

    Bug 1256772 - Fix ESLint issues in devtools/client/webconsole/jsterm.js; r=linclark
    MozReview-Commit-ID: 4fS6PsBGORI

--- devtools/client/webconsole/jsterm.js
+++ devtools/client/webconsole/jsterm.js
@@ -6,9 +6,9 @@
 
 "use strict";
 
-const {Cc, Ci, Cu} = require("chrome");
+const {Ci, Cu} = require("chrome");
 
-const {Utils: WebConsoleUtils, CONSOLE_WORKER_IDS} =
+const {Utils: WebConsoleUtils} =
   require("devtools/shared/webconsole/utils");
 const promise = require("promise");
 const Debugger = require("Debugger");
@@ -315,7 +315,7 @@ JSTerm.prototype = {
       errorMessage += " ";
       errorDocLink = this.hud.document.createElementNS(XHTML_NS, "a");
       errorDocLink.className = "learn-more-link webconsole-learn-more-link";
-      errorDocLink.textContent = "[" + l10n.getStr("webConsoleMoreInfoLabel") + "]";
+      errorDocLink.textContent = `[${l10n.getStr("webConsoleMoreInfoLabel")}]`;
       errorDocLink.title = errorDocURL;
       errorDocLink.href = "#";
       errorDocLink.draggable = false;
@@ -326,7 +326,7 @@ JSTerm.prototype = {
 
     // Wrap thrown strings in Error objects, so `throw "foo"` outputs
     // "Error: foo"
-    if (typeof(response.exception) === "string") {
+    if (typeof response.exception === "string") {
       errorMessage = new Error(errorMessage).toString();
     }
     let result = response.result;
@@ -372,7 +372,8 @@ JSTerm.prototype = {
       return;
     }
 
-    let msg = new Messages.JavaScriptEvalOutput(response, errorMessage, errorDocLink);
+    let msg = new Messages.JavaScriptEvalOutput(response,
+                                                errorMessage, errorDocLink);
     this.hud.output.addMessage(msg);
 
     if (callback) {
@@ -424,7 +425,7 @@ JSTerm.prototype = {
     // attempt to execute the content of the inputNode
     executeString = executeString || this.getInputValue();
     if (!executeString) {
-      return;
+      return null;
     }
 
     let selectedNodeActor = null;
@@ -1022,6 +1023,7 @@ JSTerm.prototype = {
     }
   },
 
+  /* eslint-disable complexity */
   /**
    * The inputNode "keypress" event handler.
    *
@@ -1213,7 +1215,7 @@ JSTerm.prototype = {
         }
         break;
 
-      case Ci.nsIDOMKeyEvent.DOM_VK_RIGHT: {
+      case Ci.nsIDOMKeyEvent.DOM_VK_RIGHT:
         let cursorAtTheEnd = this.inputNode.selectionStart ==
                              this.inputNode.selectionEnd &&
                              this.inputNode.selectionStart ==
@@ -1231,7 +1233,7 @@ JSTerm.prototype = {
           this.clearCompletion();
         }
         break;
-      }
+
       case Ci.nsIDOMKeyEvent.DOM_VK_TAB:
         // Generate a completion and accept the first proposed value.
         if (this.complete(this.COMPLETE_HINT_ONLY) &&
@@ -1247,6 +1249,7 @@ JSTerm.prototype = {
         break;
     }
   },
+  /* eslint-enable complexity */
 
   /**
    * The inputNode "focus" event handler.
