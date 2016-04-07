ac5be5fb457ef7c8de82703f280853b097f97d64
Chris Manchester <cmanchester@mozilla.com>
2016-04-07 08:59:42 -0700

    Bug 1262961 - Add the 'generate-build-stats' step to several debug mozharness configs. r=mshal
    MozReview-Commit-ID: KvWV1hiUEc

--- testing/mozharness/configs/builds/releng_sub_mac_configs/64_debug.py
+++ testing/mozharness/configs/builds/releng_sub_mac_configs/64_debug.py
@@ -12,7 +12,7 @@ config = {
         'upload-files',
         'sendchange',
         'check-test',
-        # 'generate-build-stats',
+        'generate-build-stats',
         'update',  # decided by query_is_nightly()
     ],
     'stage_platform': 'macosx64-debug',
