14b85fefb02ded3e4b6cdf0c07bdcc5401b12892
Joel Maher <jmaher@mozilla.com>
2016-04-08 14:46:25 -0400

    Bug 1262954 - Explicitly set the 'layers.acceleration.disabled' pref to true on Windows XP e10s test runs. r=ahal
    MozReview-Commit-ID: 2KsBZDNvsdU

--- testing/talos/talos/run_tests.py
+++ testing/talos/talos/run_tests.py
@@ -113,12 +113,7 @@ def run_tests(config, browser_config):
         browser_config['preferences']['xpinstall.signatures.required'] = False
 
     # set defaults
-    title = config.get('title', '')
     testdate = config.get('testdate', '')
-
-    if browser_config['e10s'] and not title.endswith(".e"):
-        # we are running in e10s mode
-        title = "%s.e" % (title,)
 
     # get the process name from the path to the browser
     if not browser_config['process']:
