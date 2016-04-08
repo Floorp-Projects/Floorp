6543a7a57b8e147b1b0dce38b0e5b8ab98ca07fc
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-04-08 11:14:31 +0200

    Backed out 11 changesets (bug 1248861) for gtest test failures
    Backed out changeset be02aaa8b2e8 (bug 1248861)
    Backed out changeset 48718147d79e (bug 1248861)
    Backed out changeset 904a19b9cdd0 (bug 1248861)
    Backed out changeset b326f17035c1 (bug 1248861)
    Backed out changeset ddff88a6db07 (bug 1248861)
    Backed out changeset 0565edbead7f (bug 1248861)
    Backed out changeset d71b93bc6cc7 (bug 1248861)
    Backed out changeset 6e917e5d29c0 (bug 1248861)
    Backed out changeset c17c2fde0e89 (bug 1248861)
    Backed out changeset 56f769e95df2 (bug 1248861)
    Backed out changeset cb8a49f92cba (bug 1248861)

--- dom/media/ogg/OggReader.cpp
+++ dom/media/ogg/OggReader.cpp
@@ -528,10 +528,7 @@ nsresult OggReader::DecodeVorbis(ogg_packet* aPacket) {
   ogg_int64_t endFrame = aPacket->granulepos;
   while ((frames = vorbis_synthesis_pcmout(&mVorbisState->mDsp, &pcm)) > 0) {
     mVorbisState->ValidateVorbisPacketSamples(aPacket, frames);
-    AlignedAudioBuffer buffer(frames * channels);
-    if (!buffer) {
-      return NS_ERROR_OUT_OF_MEMORY;
-    }
+    auto buffer = MakeUnique<AudioDataValue[]>(frames * channels);
     for (uint32_t j = 0; j < channels; ++j) {
       VorbisPCMValue* channel = pcm[j];
       for (uint32_t i = 0; i < uint32_t(frames); ++i) {
@@ -580,10 +577,7 @@ nsresult OggReader::DecodeOpus(ogg_packet* aPacket) {
   if (frames < 120 || frames > 5760)
     return NS_ERROR_FAILURE;
   uint32_t channels = mOpusState->mChannels;
-  AlignedAudioBuffer buffer(frames * channels);
-  if (!buffer) {
-    return NS_ERROR_OUT_OF_MEMORY;
-  }
+  auto buffer = MakeUnique<AudioDataValue[]>(frames * channels);
 
   // Decode to the appropriate sample type.
 #ifdef MOZ_SAMPLE_TYPE_FLOAT32
@@ -623,10 +617,7 @@ nsresult OggReader::DecodeOpus(ogg_packet* aPacket) {
     }
     int32_t keepFrames = frames - skipFrames;
     int samples = keepFrames * channels;
-    AlignedAudioBuffer trimBuffer(samples);
-    if (!trimBuffer) {
-      return NS_ERROR_OUT_OF_MEMORY;
-    }
+    auto trimBuffer = MakeUnique<AudioDataValue[]>(samples);
     for (int i = 0; i < samples; i++)
       trimBuffer[i] = buffer[skipFrames*channels + i];
 
