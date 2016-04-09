3402ddb0659cb845c584e6e3af586b957836df7e
Andrew McCreight <continuation@gmail.com>
2016-04-09 06:50:59 -0700

    Bug 1263235, part 4 - Make PBrowserStream::Write's data argument last. r=jimm
    This will reduce internal memory fragmentation for the IPC::Message used to make this call.

--- dom/plugins/ipc/BrowserStreamChild.h
+++ dom/plugins/ipc/BrowserStreamChild.h
@@ -34,8 +34,8 @@ public:
             uint16_t* stype);
 
   virtual bool RecvWrite(const int32_t& offset,
-                         const Buffer& data,
-                         const uint32_t& newsize) override;
+                         const uint32_t& newsize,
+                         const Buffer& data) override;
   virtual bool RecvNPP_StreamAsFile(const nsCString& fname) override;
   virtual bool RecvNPP_DestroyStream(const NPReason& reason) override;
   virtual bool Recv__delete__() override;
