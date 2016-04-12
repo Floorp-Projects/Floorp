2902ab6ff3168a4149c9beef051adc7c8a554cf6
Jacek Caban <jacek@codeweavers.com>
2016-04-12 12:37:37 +0200

    Bug 1263657 - Fixed WindowsDllBlocklist.cpp if MOZ_MEMORY is not defined. r=glandium

--- mozglue/build/WindowsDllBlocklist.cpp
+++ mozglue/build/WindowsDllBlocklist.cpp
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifdef MOZ_MEMORY
 #define MOZ_MEMORY_IMPL
 #include "mozmemory_wrap.h"
 #define MALLOC_FUNCS MALLOC_FUNCS_MALLOC
@@ -11,6 +12,7 @@
 #define MALLOC_DECL(name, return_type, ...) \
   extern "C" MOZ_MEMORY_API return_type name ## _impl(__VA_ARGS__);
 #include "malloc_decls.h"
+#endif
 
 #include <windows.h>
 #include <winternl.h>
