c8432edc2155ba5c2b2a48b7eeb6c01b94cc1a8e
Andrzej Hunt <ahunt@mozilla.com>
2016-04-12 14:47:24 -0700

    Bug 1246243 - Use book icon for reading list folder r=liuche
    This approach is extensible and would allow easy addition of special icons for e.g. the
    screenshots folder.
    
    MozReview-Commit-ID: 44yWq85x2HG

--- mobile/android/base/java/org/mozilla/gecko/home/BookmarkFolderView.java
+++ mobile/android/base/java/org/mozilla/gecko/home/BookmarkFolderView.java
@@ -8,47 +8,65 @@ package org.mozilla.gecko.home;
 import org.mozilla.gecko.R;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 public class BookmarkFolderView extends TextView {
-    private static final int[] STATE_OPEN = { R.attr.state_open };
+    public enum FolderState {
+        /**
+         * A standard folder, i.e. a folder in a list of bookmarks and folders.
+         */
+        FOLDER(0),
 
-    private boolean mIsOpen;
+        /**
+         * The parent folder: this indicates that you are able to return to the previous
+         * folder ("Back to {name}").
+         */
+        PARENT(R.attr.parent),
+
+        /**
+         * The reading list smartfolder: this displays a reading list icon instead of the
+         * normal folder icon.
+         */
+        READING_LIST(R.attr.reading_list);
+
+        public final int state;
+
+        FolderState(final int state) { this.state = state; }
+    }
+
+    private FolderState mState;
 
     public BookmarkFolderView(Context context) {
         super(context);
+        mState = FolderState.FOLDER;
     }
 
     public BookmarkFolderView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mState = FolderState.FOLDER;
     }
 
     public BookmarkFolderView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mState = FolderState.FOLDER;
     }
 
     @Override
     public int[] onCreateDrawableState(int extraSpace) {
         final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
 
-        if (mIsOpen) {
-            mergeDrawableStates(drawableState, STATE_OPEN);
+        if (mState != null && mState != FolderState.FOLDER) {
+            mergeDrawableStates(drawableState, new int[] {  mState.state });
         }
 
         return drawableState;
     }
 
-    public void open() {
-        if (!mIsOpen) {
-            mIsOpen = true;
-            refreshDrawableState();
-        }
-    }
-
-    public void close() {
-        if (mIsOpen) {
-            mIsOpen = false;
+    public void setState(@NonNull FolderState state) {
+        if (state != mState) {
+            mState = state;
             refreshDrawableState();
         }
     }
