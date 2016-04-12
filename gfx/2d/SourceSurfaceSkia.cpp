16eeb095e5a0c9c3f0f0db454166b4d210ca2e81
Lee Salzman <lsalzman@mozilla.com>
2016-04-12 01:28:31 -0400

    Bug 1240177 - fix SourceSurfaceSkia::DrawTargetWillChange deepCopyTo usage. r=mattwoodrow, a=ritu
    --HG--
    extra : source : 12f6413904b1a3e16e3dcebb58546b54b79a42f5

--- gfx/2d/SourceSurfaceSkia.cpp
+++ gfx/2d/SourceSurfaceSkia.cpp
@@ -135,7 +135,7 @@ SourceSurfaceSkia::DrawTargetWillChange()
 
     // First try a deep copy to avoid a readback from the GPU.
     // If that fails, try the CPU copy.
-    if (!mBitmap.deepCopyTo(&mBitmap) ||
+    if (!mBitmap.deepCopyTo(&mBitmap) &&
         !mBitmap.copyTo(&mBitmap)) {
       mBitmap.reset();
     }
