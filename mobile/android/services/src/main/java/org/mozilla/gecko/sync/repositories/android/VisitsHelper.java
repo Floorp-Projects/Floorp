5e157bade929f3f3de128aed123b09f457b7a4e7
Grigory Kruglov <gkruglov@mozilla.com>
2016-04-13 16:33:36 -0700

    Bug 1046709 - Part 4: Sync changes r=nalexander,rnewman
    - insert/merge visits on "sync down"
    - ensure to attach visits on "sync up"
    - tests
    
    MozReview-Commit-ID: 4OmyQS5JSm7

---
+++ mobile/android/services/src/main/java/org/mozilla/gecko/sync/repositories/android/VisitsHelper.java
@@ -0,0 +1,130 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.sync.repositories.android;
+
+import android.content.ContentProviderClient;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.RemoteException;
+import android.support.annotation.NonNull;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.mozilla.gecko.db.BrowserContract.Visits;
+
+/**
+ * This class is used by History Sync code (see <code>AndroidBrowserHistoryDataAccessor</code> and <code>AndroidBrowserHistoryRepositorySession</code>,
+ * and provides utility functions for working with history visits. Primarily we're either inserting visits
+ * into local database based on data received from Sync, or we're preparing local visits for upload into Sync.
+ */
+public class VisitsHelper {
+    public static final boolean DEFAULT_IS_LOCAL_VALUE = false;
+    public static final String SYNC_TYPE_KEY = "type";
+    public static final String SYNC_DATE_KEY = "date";
+
+    /**
+     * Returns a list of ContentValues of visits ready for insertion for a provided History GUID.
+     * Visits must have data and type. See <code>getVisitContentValues</code>.
+     *
+     * @param guid History GUID to use when inserting visit records
+     * @param visits <code>JSONArray</code> list of (date, type) tuples for visits
+     * @return visits ready for insertion
+     */
+    public static ContentValues[] getVisitsContentValues(@NonNull String guid, @NonNull JSONArray visits) {
+        final ContentValues[] visitsToStore = new ContentValues[visits.size()];
+        final int visitCount = visits.size();
+
+        if (visitCount == 0) {
+            return visitsToStore;
+        }
+
+        for (int i = 0; i < visitCount; i++) {
+            visitsToStore[i] = getVisitContentValues(
+                    guid, (JSONObject) visits.get(i), DEFAULT_IS_LOCAL_VALUE);
+        }
+        return visitsToStore;
+    }
+
+    /**
+     * Maps up to <code>limit</code> visits for a given history GUID to an array of JSONObjects with "date" and "type" keys
+     *
+     * @param contentClient <code>ContentProviderClient</code> to use for querying Visits table
+     * @param guid History GUID for which to return visits
+     * @param limit Will return at most this number of visits
+     * @return <code>JSONArray</code> of all visits found for given History GUID
+     */
+    public static JSONArray getRecentHistoryVisitsForGUID(@NonNull ContentProviderClient contentClient,
+                                                          @NonNull String guid, int limit) throws RemoteException {
+        final JSONArray visits = new JSONArray();
+
+        final Cursor cursor = contentClient.query(
+                visitsUriWithLimit(limit),
+                new String[] {Visits.VISIT_TYPE, Visits.DATE_VISITED},
+                Visits.HISTORY_GUID + " = ?",
+                new String[] {guid}, null);
+        if (cursor == null) {
+            return visits;
+        }
+        try {
+            if (!cursor.moveToFirst()) {
+                return visits;
+            }
+
+            final int dateVisitedCol = cursor.getColumnIndexOrThrow(Visits.DATE_VISITED);
+            final int visitTypeCol = cursor.getColumnIndexOrThrow(Visits.VISIT_TYPE);
+
+            while (!cursor.isAfterLast()) {
+                insertTupleIntoVisitsUnchecked(visits,
+                        cursor.getInt(visitTypeCol),
+                        cursor.getLong(dateVisitedCol)
+                );
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+
+        return visits;
+    }
+
+    /**
+     * Constructs <code>ContentValues</code> object for a visit based on passed in parameters.
+     *
+     * @param visit <code>JSONObject</code> containing visit type and visit date keys for the visit
+     * @param guid History GUID with with to associate this visit
+     * @param isLocal Whether or not to mark this visit as local
+     * @return <code>ContentValues</code> with all visit values necessary for database insertion
+     * @throws IllegalArgumentException if visit object is missing date or type keys
+     */
+    public static ContentValues getVisitContentValues(@NonNull String guid, @NonNull JSONObject visit, boolean isLocal) {
+        if (!visit.containsKey(SYNC_DATE_KEY) || !visit.containsKey(SYNC_TYPE_KEY)) {
+            throw new IllegalArgumentException("Visit missing required keys");
+        }
+
+        final ContentValues cv = new ContentValues();
+        cv.put(Visits.HISTORY_GUID, guid);
+        cv.put(Visits.IS_LOCAL, isLocal ? 1 : 0);
+        cv.put(Visits.VISIT_TYPE, (Long) visit.get(SYNC_TYPE_KEY));
+        cv.put(Visits.DATE_VISITED, (Long) visit.get(SYNC_DATE_KEY));
+
+        return cv;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void insertTupleIntoVisitsUnchecked(JSONArray visits, Integer type, Long date) {
+        final JSONObject visit = new JSONObject();
+        visit.put(SYNC_TYPE_KEY, type);
+        visit.put(SYNC_DATE_KEY, date);
+        visits.add(visit);
+    }
+
+    private static Uri visitsUriWithLimit(int limit) {
+        return BrowserContractHelpers.VISITS_CONTENT_URI
+                .buildUpon()
+                .appendQueryParameter("limit", Integer.toString(limit))
+                .build();
+    }
+}
---
+++ mobile/android/tests/background/junit4/src/org/mozilla/gecko/sync/repositories/android/VisitsHelperTest.java
@@ -0,0 +1,139 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+package org.mozilla.gecko.sync.repositories.android;
+
+import android.content.ContentProviderClient;
+import android.content.ContentValues;
+import android.net.Uri;
+
+import junit.framework.Assert;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mozilla.gecko.background.testhelpers.TestRunner;
+import org.mozilla.gecko.db.BrowserContract;
+import org.mozilla.gecko.db.BrowserProvider;
+import org.robolectric.shadows.ShadowContentResolver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(TestRunner.class)
+public class VisitsHelperTest {
+    @Test
+    public void testBulkInsertRemoteVisits() throws Exception {
+        JSONArray toInsert = new JSONArray();
+        Assert.assertEquals(0, VisitsHelper.getVisitsContentValues("testGUID", toInsert).length);
+
+        JSONObject visit = new JSONObject();
+        Long date = Long.valueOf(123432552344l);
+        visit.put("date", date);
+        visit.put("type", 2l);
+        toInsert.add(visit);
+
+        JSONObject visit2 = new JSONObject();
+        visit2.put("date", date + 1000);
+        visit2.put("type", 5l);
+        toInsert.add(visit2);
+
+        ContentValues[] cvs = VisitsHelper.getVisitsContentValues("testGUID", toInsert);
+        Assert.assertEquals(2, cvs.length);
+        ContentValues cv1 = cvs[0];
+        ContentValues cv2 = cvs[1];
+        Assert.assertEquals(Integer.valueOf(2), cv1.getAsInteger(BrowserContract.Visits.VISIT_TYPE));
+        Assert.assertEquals(Integer.valueOf(5), cv2.getAsInteger(BrowserContract.Visits.VISIT_TYPE));
+
+        Assert.assertEquals(date, cv1.getAsLong("date"));
+        Assert.assertEquals(Long.valueOf(date + 1000), cv2.getAsLong(BrowserContract.Visits.DATE_VISITED));
+    }
+
+    @Test
+    public void testGetRecentHistoryVisitsForGUID() throws Exception {
+        Uri historyTestUri = testUri(BrowserContract.History.CONTENT_URI);
+        Uri visitsTestUri = testUri(BrowserContract.Visits.CONTENT_URI);
+
+        BrowserProvider provider = new BrowserProvider();
+        provider.onCreate();
+        ShadowContentResolver.registerProvider(BrowserContract.AUTHORITY_URI.toString(), provider);
+
+        final ShadowContentResolver cr = new ShadowContentResolver();
+        ContentProviderClient historyClient = cr.acquireContentProviderClient(BrowserContractHelpers.HISTORY_CONTENT_URI);
+        ContentProviderClient visitsClient = cr.acquireContentProviderClient(BrowserContractHelpers.VISITS_CONTENT_URI);
+
+        ContentValues historyItem = new ContentValues();
+        historyItem.put(BrowserContract.History.URL, "https://www.mozilla.org");
+        historyItem.put(BrowserContract.History.GUID, "testGUID");
+        historyClient.insert(historyTestUri, historyItem);
+
+        Long baseDate = System.currentTimeMillis();
+        for (int i = 0; i < 30; i++) {
+            ContentValues visitItem = new ContentValues();
+            visitItem.put(BrowserContract.Visits.HISTORY_GUID, "testGUID");
+            visitItem.put(BrowserContract.Visits.DATE_VISITED, baseDate - i * 100);
+            visitItem.put(BrowserContract.Visits.VISIT_TYPE, 1);
+            visitItem.put(BrowserContract.Visits.IS_LOCAL, 1);
+            visitsClient.insert(visitsTestUri, visitItem);
+        }
+
+        // test that limit worked, that sorting is correct, and that both date and type are present
+        JSONArray recentVisits = VisitsHelper.getRecentHistoryVisitsForGUID(visitsClient, "testGUID", 10);
+        Assert.assertEquals(10, recentVisits.size());
+        for (int i = 0; i < recentVisits.size(); i++) {
+            JSONObject v = (JSONObject) recentVisits.get(i);
+            Long date = (Long) v.get("date");
+            Integer type = (Integer) v.get("type");
+            Assert.assertEquals(Long.valueOf(baseDate - i * 100), date);
+            Assert.assertEquals(Integer.valueOf(1), type);
+        }
+    }
+
+    @Test
+    public void testGetVisitContentValues() throws Exception {
+        JSONObject visit = new JSONObject();
+        Long date = Long.valueOf(123432552344l);
+        visit.put("date", date);
+        visit.put("type", Long.valueOf(2));
+
+        ContentValues cv = VisitsHelper.getVisitContentValues("testGUID", visit, true);
+        assertTrue(cv.containsKey(BrowserContract.Visits.VISIT_TYPE));
+        assertTrue(cv.containsKey(BrowserContract.Visits.DATE_VISITED));
+        assertTrue(cv.containsKey(BrowserContract.Visits.HISTORY_GUID));
+        assertTrue(cv.containsKey(BrowserContract.Visits.IS_LOCAL));
+        assertEquals(4, cv.size());
+
+        assertEquals(date, cv.getAsLong(BrowserContract.Visits.DATE_VISITED));
+        assertEquals(Long.valueOf(2), cv.getAsLong(BrowserContract.Visits.VISIT_TYPE));
+        assertEquals("testGUID", cv.getAsString(BrowserContract.Visits.HISTORY_GUID));
+        assertEquals(Integer.valueOf(1), cv.getAsInteger(BrowserContract.Visits.IS_LOCAL));
+
+        cv = VisitsHelper.getVisitContentValues("testGUID", visit, false);
+        assertEquals(Integer.valueOf(0), cv.getAsInteger(BrowserContract.Visits.IS_LOCAL));
+
+        try {
+            JSONObject visit2 = new JSONObject();
+            visit.put("date", date);
+            VisitsHelper.getVisitContentValues("testGUID", visit2, false);
+            assertTrue("Must check that visit type key is present", false);
+        } catch (IllegalArgumentException e) {}
+
+        try {
+            JSONObject visit3 = new JSONObject();
+            visit.put("type", Long.valueOf(2));
+            VisitsHelper.getVisitContentValues("testGUID", visit3, false);
+            assertTrue("Must check that visit date key is present", false);
+        } catch (IllegalArgumentException e) {}
+
+        try {
+            JSONObject visit4 = new JSONObject();
+            VisitsHelper.getVisitContentValues("testGUID", visit4, false);
+            assertTrue("Must check that visit type and date keys are present", false);
+        } catch (IllegalArgumentException e) {}
+    }
+
+    private Uri testUri(Uri baseUri) {
+        return baseUri.buildUpon().appendQueryParameter(BrowserContract.PARAM_IS_TEST, "1").build();
+    }
+}
