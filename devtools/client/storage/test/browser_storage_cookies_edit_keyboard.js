664c4374969c2eafa393666c36637e14ac9503b4
Jarda Snajdr <jsnajdr@gmail.com>
2016-04-13 04:01:00 -0400

    Bug 1261785 - Part 1: Fix promise rejections in storage inspector tests. r=mratcliffe

--- devtools/client/storage/test/browser_storage_cookies_edit_keyboard.js
+++ devtools/client/storage/test/browser_storage_cookies_edit_keyboard.js
@@ -8,8 +8,6 @@
 
 add_task(function* () {
   yield openTabAndSetupStorage(MAIN_DOMAIN + "storage-cookies.html");
-  yield gUI.table.once(TableWidget.EVENTS.FIELDS_EDITABLE);
-
   showAllColumns(true);
 
   yield startCellEdit("test4", "name");
