f97b9ae311644f9eadf115821d52d04a0d250c61
J.C. Jones <jjones@mozilla.com>
2016-04-13 10:20:37 -0700

    Bug 1244960 - FIDO u2f NSSToken (Part 3): Review updates. r=keeler
    MozReview-Commit-ID: FkPHy9GGarU

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -749,20 +749,20 @@ private:
   virtual bool
   DeallocPCrashReporterParent(PCrashReporterParent* crashreporter) override;
 
-  virtual bool RecvNSSU2FTokenIsCompatibleVersion(const nsString& version,
-                                                  bool* isCompatible) override;
+  virtual bool RecvNSSU2FTokenIsCompatibleVersion(const nsString& aVersion,
+                                                  bool* aIsCompatible) override;
 
-  virtual bool RecvNSSU2FTokenIsRegistered(nsTArray<uint8_t>&& keyHandle,
-                                           bool* isValidKeyHandle) override;
+  virtual bool RecvNSSU2FTokenIsRegistered(nsTArray<uint8_t>&& aKeyHandle,
+                                           bool* aIsValidKeyHandle) override;
 
-  virtual bool RecvNSSU2FTokenRegister(nsTArray<uint8_t>&& application,
-                                       nsTArray<uint8_t>&& challenge,
-                                       nsTArray<uint8_t>* registration) override;
+  virtual bool RecvNSSU2FTokenRegister(nsTArray<uint8_t>&& aApplication,
+                                       nsTArray<uint8_t>&& aChallenge,
+                                       nsTArray<uint8_t>* aRegistration) override;
 
-  virtual bool RecvNSSU2FTokenSign(nsTArray<uint8_t>&& application,
-                                   nsTArray<uint8_t>&& challenge,
-                                   nsTArray<uint8_t>&& keyHandle,
-                                   nsTArray<uint8_t>* signature) override;
+  virtual bool RecvNSSU2FTokenSign(nsTArray<uint8_t>&& aApplication,
+                                   nsTArray<uint8_t>&& aChallenge,
+                                   nsTArray<uint8_t>&& aKeyHandle,
+                                   nsTArray<uint8_t>* aSignature) override;
 
   virtual bool RecvIsSecureURI(const uint32_t& aType, const URIParams& aURI,
                                const uint32_t& aFlags, bool* aIsSecureURI) override;
