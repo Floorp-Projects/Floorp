b799dfaa460362bc5dacee3230bf6c52b577f3f2
Karl Tomlinson <karlt+@karlt.net>
2016-04-14 17:47:20 +1200

    bug 1234158 rename GtkThemeWidgetType enum to WidgetNodeType as it will differentiate GTK CSS nodes r=Stránský
    --HG--
    extra : transplant_source : %11-%01%D8%F3%D6%00%E9%0C%D8%84z%A2%98%7C%F3%B5I%CF%5E
    extra : histedit_source : deb6e194d8ea4466976347e24a741ee354dcc331

--- widget/gtk/gtkdrawing.h
+++ widget/gtk/gtkdrawing.h
@@ -192,7 +192,7 @@ typedef enum {
   MOZ_GTK_WINDOW,
   /* Paints a GtkInfoBar, for notifications. */
   MOZ_GTK_INFO_BAR
-} GtkThemeWidgetType;
+} WidgetNodeType;
 
 /*** General library functions ***/
 /**
@@ -238,17 +238,17 @@ GdkColormap* moz_gtk_widget_get_colormap();
  * rect:      the bounding rectangle for the widget
  * cliprect:  a clipprect rectangle for this painting operation
  * state:     the state of the widget.  ignored for some widgets.
- * flags:     widget-dependant flags; see the GtkThemeWidgetType definition.
+ * flags:     widget-dependant flags; see the WidgetNodeType definition.
  * direction: the text direction, to draw the widget correctly LTR and RTL.
  */
 gint
-moz_gtk_widget_paint(GtkThemeWidgetType widget, GdkDrawable* drawable,
+moz_gtk_widget_paint(WidgetNodeType widget, GdkDrawable* drawable,
                      GdkRectangle* rect, GdkRectangle* cliprect,
                      GtkWidgetState* state, gint flags,
                      GtkTextDirection direction);
 #else
 gint
-moz_gtk_widget_paint(GtkThemeWidgetType widget, cairo_t *cr,
+moz_gtk_widget_paint(WidgetNodeType widget, cairo_t *cr,
                      GdkRectangle* rect,
                      GtkWidgetState* state, gint flags,
                      GtkTextDirection direction);
@@ -266,7 +266,7 @@ moz_gtk_widget_paint(GtkThemeWidgetType widget, cairo_t *cr,
  *
  * returns:    MOZ_GTK_SUCCESS if there was no error, an error code otherwise
  */
-gint moz_gtk_get_widget_border(GtkThemeWidgetType widget, gint* left, gint* top, 
+gint moz_gtk_get_widget_border(WidgetNodeType widget, gint* left, gint* top,
                                gint* right, gint* bottom, GtkTextDirection direction,
                                gboolean inhtml);
 
@@ -397,7 +397,7 @@ gint moz_gtk_get_tab_scroll_arrow_size(gint* width, gint* height);
  * height:     [OUT] the desired height
  */
 void
-moz_gtk_get_arrow_size(GtkThemeWidgetType widgetType,
+moz_gtk_get_arrow_size(WidgetNodeType widgetType,
                        gint* width, gint* height);
 
 /**
