251a87be0bcc323262b8b10eeab7e33f1048687c
Tooru Fujisawa <arai_a@mac.com>
2016-04-14 16:41:37 +0900

    Bug 1264085 - Use not-non-release-only warning in js/src/tests/shell/warning.js. r=shu

--- js/src/tests/shell/warning.js
+++ js/src/tests/shell/warning.js
@@ -9,14 +9,14 @@ print(BUGNUMBER + ": " + summary);
 
 enableLastWarning();
 
-eval(`(function() "This is an expression closure.")`);
+eval(`{ function f() {} function f() {} }`);
 
 var warning = getLastWarning();
 assertEq(warning !== null, true);
 assertEq(warning.name, "None");
-assertEq(warning.message.includes("expression closures are deprecated"), true);
+assertEq(warning.message.includes("deprecated"), true);
 assertEq(warning.lineNumber, 1);
-assertEq(warning.columnNumber, 12);
+assertEq(warning.columnNumber, 27);
 
 // Clear last warning.
 
