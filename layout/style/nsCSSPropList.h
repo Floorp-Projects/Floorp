5e1f61012e9a9d3b1d5824e23940415f106468cc
CJKu <cku@mozilla.com>
2016-04-14 16:28:06 +0800

    Bug 759568 - Part 1. Parse background-clip:text; r=dholbert r=heycam
    MozReview-Commit-ID: BPuQjWYvAuj

--- layout/style/nsCSSPropList.h
+++ layout/style/nsCSSPropList.h
@@ -522,7 +522,7 @@ CSS_PROP_BACKGROUND(
         CSS_PROPERTY_VALUE_LIST_USES_COMMAS,
     "",
     VARIANT_KEYWORD, // used by list parsing
-    kImageLayerOriginKTable,
+    kBackgroundClipKTable,
     CSS_PROP_NO_OFFSET,
     eStyleAnimType_None)
 CSS_PROP_BACKGROUND(
