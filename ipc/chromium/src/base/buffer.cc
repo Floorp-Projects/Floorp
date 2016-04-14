9fca5b9dec24a03612a933c1114b4004dc3f40b8
Bill McCloskey <billm@mozilla.com>
2016-04-13 18:43:36 -0700

    Bug 1263292 - Handle calling realloc(0) (r=jld)

--- ipc/chromium/src/base/buffer.cc
+++ ipc/chromium/src/base/buffer.cc
@@ -51,7 +51,7 @@ void
 Buffer::try_realloc(size_t newlength)
 {
   char* buffer = (char*)realloc(mBuffer, newlength);
-  if (buffer) {
+  if (buffer || !newlength) {
     mBuffer = buffer;
     mReserved = newlength;
     return;
@@ -107,6 +107,8 @@ Buffer::reserve(size_t size)
 char*
 Buffer::trade_bytes(size_t count)
 {
+  MOZ_RELEASE_ASSERT(count);
+
   char* result = mBuffer;
   mSize = mReserved = mSize - count;
   mBuffer = mReserved ? (char*)malloc(mReserved) : nullptr;
