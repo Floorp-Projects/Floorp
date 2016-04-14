5728cb1467fdff2ac7a0ddd7e45dc0b9cc1b1225
David Anderson <danderson@mozilla.com>
2016-04-14 15:16:33 -0400

    Fix aboutSupport.js eslint bustage, bug 1263849 r=orange

--- toolkit/content/aboutSupport.js
+++ toolkit/content/aboutSupport.js
@@ -262,7 +262,7 @@ var snapshotFormatters = {
     // @where    The name in "graphics-<name>-tbody", of the element to append to.
     // @trs      Array of row elements.
     function addRows(where, trs) {
-      $.append($('graphics-' + where + '-tbody'), trs);
+      $.append($("graphics-" + where + "-tbody"), trs);
     }
 
     // Build and append a row.
@@ -408,7 +408,7 @@ var snapshotFormatters = {
       }
       addRow(id, "active", strings.GetStringFromName(active));
       addRows(id, trs);
-    };
+    }
     showGpu("gpu-1", "");
     showGpu("gpu-2", "2");
 
