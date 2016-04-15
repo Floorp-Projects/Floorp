662687953b0a7a961eac5a555557a1d6fb08ee3d
Julian Descottes <jdescottes@mozilla.com>
2016-04-15 12:35:58 +0200

    Bug 1264101 - Force dom.animations-api.core.enabled pref to true before animation inspector tests. r=pbro
    WebAnimations API is not enabled by default in all release channels yet.
    It is behing the pref dom.animations-api.core.enabled.
    
    Turn the preference on before any test of the animationinspector, and
    clear it after the test.
    
    MozReview-Commit-ID: K8UXEthLhnH

--- devtools/client/animationinspector/test/browser_animation_timeline_takes_rate_into_account.js
+++ devtools/client/animationinspector/test/browser_animation_timeline_takes_rate_into_account.js
@@ -12,12 +12,6 @@
 // rate changed have a delay = delay/rate and a duration = duration/rate.
 
 add_task(function* () {
-  yield new Promise(resolve => {
-    SpecialPowers.pushPrefEnv({"set": [
-      ["dom.animations-api.core.enabled", true]
-    ]}, resolve);
-  });
-
   yield addTab(URL_ROOT + "doc_modify_playbackRate.html");
 
   let {panel} = yield openAnimationInspector();
