ed5ce2394b04fc9173c9aeea89e52b03f0ba6753
Brian Birtles <birtles@gmail.com>
2016-04-15 15:26:27 +0900

    Bug 1247004 part 1 - Drop orphaned declaration AnimationUtils::ParseEasing; r=heycam
    MozReview-Commit-ID: 6FSXOXeXomG

--- dom/animation/AnimationUtils.h
+++ dom/animation/AnimationUtils.h
@@ -50,14 +50,6 @@ public:
                                        const nsIContent* aContent = nullptr);
 
   /**
-   * Parses a CSS <single-transition-timing-function> value from
-   * aEasing into a ComputedTimingFunction.  If parsing fails, Nothing() will
-   * be returned.
-   */
-  static Maybe<ComputedTimingFunction>
-  ParseEasing(const nsAString& aEasing, nsIDocument* aDocument);
-
-  /**
    * Get the document from the JS context to use when parsing CSS properties.
    */
   static nsIDocument*
