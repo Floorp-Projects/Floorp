e8e585ea06057749257918619e9573d859cd0f24
Rail Aliiev <rail@mozilla.com>
2016-04-14 21:44:21 -0400

    Bug 1264810 - Change merge day configs to bump esr45 version r=jlund DONTBUILD
    MozReview-Commit-ID: BBEftRTVZiI

--- testing/mozharness/configs/merge_day/bump_esr.py
+++ testing/mozharness/configs/merge_day/bump_esr.py
@@ -2,13 +2,12 @@ config = {
     "log_name": "bump_esr",
     "version_files": [
         {"file": "browser/config/version.txt", "suffix": ""},
-        # TODO: add the following line when ESR is 45
-        # {"file": "browser/config/version_display.txt", "suffix": ""},
+        {"file": "browser/config/version_display.txt", "suffix": ""},
         {"file": "config/milestone.txt", "suffix": ""},
     ],
     "tools_repo_url": "https://hg.mozilla.org/build/tools",
     "tools_repo_revision": "default",
-    "to_repo_url": "ssh://hg.mozilla.org/releases/mozilla-esr38",
+    "to_repo_url": "ssh://hg.mozilla.org/releases/mozilla-esr45",
 
     "migration_behavior": "bump_second_digit",
     "require_remove_locales": False,
