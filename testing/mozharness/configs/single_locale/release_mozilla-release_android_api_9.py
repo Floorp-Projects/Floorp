79cecda815da1d0031ca39dd609a6dbd106d4ae6
Nick Thomas <nthomas@mozilla.com>
2016-04-19 09:28:07 +1200

    Bug 1262000, fix Android single-locale repacks on release, r=jlund, a=release

--- testing/mozharness/configs/single_locale/release_mozilla-release_android_api_9.py
+++ testing/mozharness/configs/single_locale/release_mozilla-release_android_api_9.py
@@ -26,7 +26,7 @@ config = {
         "output_dir": "%(abs_work_dir)s/" + MOZILLA_DIR,
     },
     "exes": {
-        'tooltool.py': '/tools/tooltool.py',
+        'tooltool.py': '/builds/tooltool.py',
     },
     "repos": [{
         "repo": "https://hg.mozilla.org/releases/mozilla-release",
@@ -92,6 +92,9 @@ config = {
                       ],
     "mock_files": [
         ("/home/cltbld/.ssh", "/home/mock_mozilla/.ssh"),
+        ('/home/cltbld/.hgrc', '/builds/.hgrc'),
+        ('/builds/relengapi.tok', '/builds/relengapi.tok'),
+        ('/tools/tooltool.py', '/builds/tooltool.py'),
         ('/usr/local/lib/hgext', '/usr/local/lib/hgext'),
     ],
 }
