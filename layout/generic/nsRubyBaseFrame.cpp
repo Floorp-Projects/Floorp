e4d939017ba1ba8bafd9e27e93ebc5e176a9ea07
Ting-Yu Lin <tlin@mozilla.com>
2016-04-18 14:04:03 +0800

    Bug 1264837 Part 9 - Remove nsRubyBaseFrameSuper. r=dholbert
    MozReview-Commit-ID: 5o0wQcUX9uU

--- layout/generic/nsRubyBaseFrame.cpp
+++ layout/generic/nsRubyBaseFrame.cpp
@@ -22,7 +22,7 @@ using namespace mozilla;
 
 NS_QUERYFRAME_HEAD(nsRubyBaseFrame)
   NS_QUERYFRAME_ENTRY(nsRubyBaseFrame)
-NS_QUERYFRAME_TAIL_INHERITING(nsRubyBaseFrameSuper)
+NS_QUERYFRAME_TAIL_INHERITING(nsRubyContentFrame)
 
 NS_IMPL_FRAMEARENA_HELPERS(nsRubyBaseFrame)
 
