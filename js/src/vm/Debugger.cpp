a64e1e384714f033825a0450a7bf8c749729d4bf
Lars T Hansen <lhansen@mozilla.com>
2016-04-18 15:28:40 +0200

    Bug 1263902 - check return value from JS_smprintf. r=bbouvier, r=shu
    --HG--
    extra : rebase_source : c212beece2b24dfdf9888546289ed1649aa1af77
    extra : amend_source : 65592b062fd6e97896f141feb67bdd6f29362f91

--- js/src/vm/Debugger.cpp
+++ js/src/vm/Debugger.cpp
@@ -6464,6 +6464,8 @@ class DebuggerSourceGetURLMatcher
         // end to prevent them from being blacklisted by devtools by having
         // the same value as a source mapped URL.
         char* buf = JS_smprintf("%s > wasm", wasmModule->module().filename());
+        if (!buf)
+            return Nothing();
         JSString* str = NewStringCopyZ<CanGC>(cx_, buf);
         JS_smprintf_free(buf);
         return Some(str);
