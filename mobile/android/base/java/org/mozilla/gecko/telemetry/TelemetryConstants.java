5719700e81c9eea02481c135d77d295cb78d01c0
Michael Comella <michael.l.comella@gmail.com>
2016-04-19 08:49:07 -0700

    Bug 1243585 - Move Intent constants into UploadService. r=sebastian
    I'm thinking it's better to have constants with the classes they're most
    closely associated with rather than one giant constants file because it becomes
    hard to find anything in a large constants file.
    
    MozReview-Commit-ID: D3SCkW3vbRM

--- mobile/android/base/java/org/mozilla/gecko/telemetry/TelemetryConstants.java
+++ mobile/android/base/java/org/mozilla/gecko/telemetry/TelemetryConstants.java
@@ -15,13 +15,6 @@ public class TelemetryConstants {
     public static final String USER_AGENT =
             "Firefox-Android-Telemetry/" + AppConstants.MOZ_APP_VERSION + " (" + AppConstants.MOZ_APP_UA_NAME + ")";
 
-    public static final String ACTION_UPLOAD_CORE = "uploadCore";
-    public static final String EXTRA_DEFAULT_SEARCH_ENGINE = "defaultSearchEngine";
-    public static final String EXTRA_DOC_ID = "docId";
-    public static final String EXTRA_PROFILE_NAME = "geckoProfileName";
-    public static final String EXTRA_PROFILE_PATH = "geckoProfilePath";
-    public static final String EXTRA_SEQ = "seq";
-
     public static final String PREF_SERVER_URL = "telemetry-serverUrl";
     public static final String PREF_SEQ_COUNT = "telemetry-seqCount";
 }
