3a905a98f784a5f1d6e41c53bec4817d99709643
Wes Kocher <wkocher@mozilla.com>
2016-04-19 16:09:49 -0700

    Backed out changeset bb60c7a0b0c5 (bug 1264761) for build failures in nsNSSCertHelper CLOSED TREE
    MozReview-Commit-ID: KwFHe6X2WCE

--- security/manager/ssl/nsNSSCertHelper.cpp
+++ security/manager/ssl/nsNSSCertHelper.cpp
@@ -6,20 +6,21 @@
 
 #include <algorithm>
 
-#include "ScopedNSSTypes.h"
 #include "mozilla/Snprintf.h"
 #include "mozilla/UniquePtr.h"
-#include "nsCOMPtr.h"
 #include "nsComponentManagerUtils.h"
+#include "nsCOMPtr.h"
 #include "nsDateTimeFormatCID.h"
 #include "nsIDateTimeFormat.h"
 #include "nsNSSASN1Object.h"
+#include "nsNSSCertificate.h"
 #include "nsNSSCertTrust.h"
 #include "nsNSSCertValidity.h"
-#include "nsNSSCertificate.h"
 #include "nsNSSComponent.h"
+#include "nsIDateTimeFormat.h"
 #include "nsServiceManagerUtils.h"
 #include "prerror.h"
+#include "ScopedNSSTypes.h"
 #include "secder.h"
 
 using namespace mozilla;
@@ -83,46 +84,53 @@ GetIntValue(SECItem *versionItem,
 }
 
 static nsresult
-ProcessVersion(SECItem* versionItem, nsINSSComponent* nssComponent,
-               nsIASN1PrintableItem** retItem)
+ProcessVersion(SECItem         *versionItem,
+               nsINSSComponent *nssComponent,
+               nsIASN1PrintableItem **retItem)
 {
+  nsresult rv;
   nsAutoString text;
-  nssComponent->GetPIPNSSBundleString("CertDumpVersion", text);
   nsCOMPtr<nsIASN1PrintableItem> printableItem = new nsNSSASN1PrintableItem();
-  nsresult rv = printableItem->SetDisplayName(text);
-  if (NS_FAILED(rv)) {
+ 
+  nssComponent->GetPIPNSSBundleString("CertDumpVersion", text);
+  rv = printableItem->SetDisplayName(text);
+  if (NS_FAILED(rv))
     return rv;
-  }
 
   // Now to figure out what version this certificate is.
   unsigned long version;
+
   if (versionItem->data) {
     rv = GetIntValue(versionItem, &version);
-    if (NS_FAILED(rv)) {
+    if (NS_FAILED(rv))
       return rv;
-    }
   } else {
-    // If there is no version present in the cert, then RFC 5280 says we
-    // default to v1 (0).
+    // If there is no version present in the cert, then rfc2459
+    // says we default to v1 (0)
     version = 0;
   }
 
-  // A value of n actually corresponds to version n + 1
-  nsAutoString versionString;
-  versionString.AppendInt(version + 1);
-  const char16_t* params[1] = { versionString.get() };
-  rv = nssComponent->PIPBundleFormatStringFromName("CertDumpVersionValue",
-                                                   params,
-                                                   MOZ_ARRAY_LENGTH(params),
-                                                   text);
-  if (NS_FAILED(rv)) {
-    return rv;
+  switch (version){
+  case 0:
+    rv = nssComponent->GetPIPNSSBundleString("CertDumpVersion1", text);
+    break;
+  case 1:
+    rv = nssComponent->GetPIPNSSBundleString("CertDumpVersion2", text);
+    break;
+  case 2:
+    rv = nssComponent->GetPIPNSSBundleString("CertDumpVersion3", text);
+    break;
+  default:
+    NS_ERROR("Bad value for cert version");
+    rv = NS_ERROR_FAILURE;
   }
+    
+  if (NS_FAILED(rv))
+    return rv;
 
   rv = printableItem->SetDisplayValue(text);
-  if (NS_FAILED(rv)) {
+  if (NS_FAILED(rv))
     return rv;
-  }
 
   printableItem.forget(retItem);
   return NS_OK;
