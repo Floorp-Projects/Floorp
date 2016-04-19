0982c581eab998f0a35b8cc368d51cbb73897028
Wes Kocher <wkocher@mozilla.com>
2016-04-19 16:37:08 -0700

    Backed out changeset 00bb04b8c9a0 (bug 1255040) for failures in browser_webapi.js
    MozReview-Commit-ID: 1qKiUTNw4fV

--- toolkit/mozapps/extensions/amWebAPI.js
+++ toolkit/mozapps/extensions/amWebAPI.js
@@ -55,16 +55,11 @@ const APIBroker = {
 
 APIBroker.init();
 
-function Addon(win, properties) {
+function Addon(properties) {
   // We trust the webidl binding to broker access to our properties.
   for (let key of Object.keys(properties)) {
     this[key] = properties[key];
   }
-
-  this.uninstall = function() {
-    let err = new win.Error("not yet implemented");
-    return win.Promise.reject(err);
-  };
 }
 
 /**
@@ -102,13 +97,8 @@ WebAPI.prototype = {
 
   getAddonByID: WebAPITask(function*(id) {
     let addonInfo = yield APIBroker.sendRequest("getAddonByID", id);
-    return addonInfo ? new Addon(this.window, addonInfo) : null;
+    return addonInfo ? new Addon(addonInfo) : null;
   }),
-
-  createInstall() {
-    let err = new this.window.Error("not yet implemented");
-    return this.window.Promise.reject(err);
-  },
 
   classID: Components.ID("{8866d8e3-4ea5-48b7-a891-13ba0ac15235}"),
   contractID: "@mozilla.org/addon-web-api/manager;1",
