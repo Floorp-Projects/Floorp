8c4b176965d607d2465acbbfa9fad92179f44ef9
James Graham <james@hoppipolla.co.uk>
2016-04-20 16:34:27 +0100

    Bug 1270508 - Update web-platform-tests to revision 26663baa852c0b42b91e7e72d3aa940e43371571, a=testonly
    MozReview-Commit-ID: JLzeheMYLlz

--- testing/web-platform/tests/service-workers/service-worker/resources/invalid-chunked-encoding-with-flush.py
+++ testing/web-platform/tests/service-workers/service-worker/resources/invalid-chunked-encoding-with-flush.py
@@ -1,6 +1,5 @@
 import time
 def main(request, response):
-    print dir(response)
     response.headers.set("Content-Type", "application/javascript")
     response.headers.set("Transfer-encoding", "chunked")
     response.write_status_headers()
