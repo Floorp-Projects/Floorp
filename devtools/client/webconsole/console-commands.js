be000854e6a52be70df82bfeded4f5fcbea776b3
gasolin <gasolin@gmail.com>
2016-04-20 18:06:01 +0800

    Bug 1256767 - Fix ESLint errors/warnings in devtools/client/webconsole/console-commands.js; r=linclark
    MozReview-Commit-ID: A5L5DqtHjGe

--- devtools/client/webconsole/console-commands.js
+++ devtools/client/webconsole/console-commands.js
@@ -46,12 +46,11 @@ exports.items = [
       let toolbox = gDevTools.getToolbox(target);
 
       if (!toolbox) {
-        return gDevTools.showToolbox(target, "inspector").then((toolbox) => {
-          toolbox.toggleSplitConsole();
+        return gDevTools.showToolbox(target, "inspector").then((newToolbox) => {
+          newToolbox.toggleSplitConsole();
         });
-      } else {
-        toolbox.toggleSplitConsole();
       }
+      return toolbox.toggleSplitConsole();
     }
   },
   {
@@ -67,12 +66,12 @@ exports.items = [
     exec: function (args, context) {
       let toolbox = gDevTools.getToolbox(context.environment.target);
       if (toolbox == null) {
-        return;
+        return null;
       }
 
       let panel = toolbox.getPanel("webconsole");
       if (panel == null) {
-        return;
+        return null;
       }
 
       let onceMessagesCleared = panel.hud.jsterm.once("messages-cleared");
@@ -86,8 +85,8 @@ exports.items = [
     name: "console close",
     description: l10n.lookup("consolecloseDesc"),
     exec: function (args, context) {
-      return gDevTools.closeToolbox(context.environment.target)
-                      .then(() => {}); // Don't return a value to GCLI
+      // Don't return a value to GCLI
+      return gDevTools.closeToolbox(context.environment.target).then(() => {});
     }
   },
   {
@@ -97,8 +96,8 @@ exports.items = [
     description: l10n.lookup("consoleopenDesc"),
     exec: function (args, context) {
       const target = context.environment.target;
-      return gDevTools.showToolbox(target, "webconsole")
-                      .then(() => {}); // Don't return a value to GCLI
+      // Don't return a value to GCLI
+      return gDevTools.showToolbox(target, "webconsole").then(() => {});
     }
   }
 ];
