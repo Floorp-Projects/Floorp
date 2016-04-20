7491ace1b65286072be0dedd7d7a4a4fabccc1d4
James Graham <james@hoppipolla.co.uk>
2016-04-20 16:34:27 +0100

    Bug 1270508 - Update web-platform-tests to revision 26663baa852c0b42b91e7e72d3aa940e43371571, a=testonly
    MozReview-Commit-ID: JLzeheMYLlz

--- testing/web-platform/tests/service-workers/service-worker/resources/worker-load-interceptor.js
+++ testing/web-platform/tests/service-workers/service-worker/resources/worker-load-interceptor.js
@@ -6,9 +6,8 @@ var response_script = "postMessage(\"This load was successfully intercepted.\");
 self.onfetch = function(event) {
     var url = event.request.url;
     if (url.indexOf("synthesized-response.txt") != -1) {
-	event.respondWith(new Response(response_text));
+        event.respondWith(new Response(response_text));
     } else if (url.indexOf("synthesized-response.js") != -1) {
-	event.respondWith(new Response(response_script));
+        event.respondWith(new Response(response_script));
     }
 };
-
