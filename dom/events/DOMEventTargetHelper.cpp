4091ff654946f694c7d5dd30bd29b13d96689733
Boris Zbarsky <bzbarsky@mit.edu>
2016-04-20 18:04:36 -0400

    Bug 1265072 part 3.  Get rid of the use of GetDocumentFromScriptContext in DOMEventTargetHelper.  r=smaug

--- dom/events/DOMEventTargetHelper.cpp
+++ dom/events/DOMEventTargetHelper.cpp
@@ -377,11 +377,10 @@ DOMEventTargetHelper::GetContextForEventHandlers(nsresult* aRv)
 nsresult
 DOMEventTargetHelper::WantsUntrusted(bool* aRetVal)
 {
-  nsresult rv;
-  nsIScriptContext* context = GetContextForEventHandlers(&rv);
+  nsresult rv = CheckInnerWindowCorrectness();
   NS_ENSURE_SUCCESS(rv, rv);
-  nsCOMPtr<nsIDocument> doc =
-    nsContentUtils::GetDocumentFromScriptContext(context);
+  
+  nsCOMPtr<nsIDocument> doc = GetDocumentIfCurrent();
   // We can let listeners on workers to always handle all the events.
   *aRetVal = (doc && !nsContentUtils::IsChromeDoc(doc)) || !NS_IsMainThread();
   return rv;
