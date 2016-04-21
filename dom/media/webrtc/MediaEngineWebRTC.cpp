7d950772fc2fd335e1ce0436e6b539148f2d62b9
Randell Jesup <rjesup@jesup.org>
2016-04-21 01:42:50 -0400

    Bug 1266285: Don't include UNPLUGGED cubeb devices in getUserMedia list, and use correct default r=kinetik
    MozReview-Commit-ID: 1hBNLCAu2rW

--- dom/media/webrtc/MediaEngineWebRTC.cpp
+++ dom/media/webrtc/MediaEngineWebRTC.cpp
@@ -46,6 +46,7 @@ namespace mozilla {
 
 // statics from AudioInputCubeb
 nsTArray<int>* AudioInputCubeb::mDeviceIndexes;
+int AudioInputCubeb::mDefaultDevice = -1;
 nsTArray<nsCString>* AudioInputCubeb::mDeviceNames;
 cubeb_device_collection* AudioInputCubeb::mDevices = nullptr;
 bool AudioInputCubeb::mAnyInUse = false;
@@ -74,10 +75,10 @@ void AudioInputCubeb::UpdateDeviceList()
   // stashed indexes.
   // For some reason the "fake" device for automation is marked as DISABLED,
   // so white-list it.
+  mDefaultDevice = -1;
   for (uint32_t i = 0; i < devices->count; i++) {
     if (devices->device[i]->type == CUBEB_DEVICE_TYPE_INPUT && // paranoia
         (devices->device[i]->state == CUBEB_DEVICE_STATE_ENABLED ||
-         devices->device[i]->state == CUBEB_DEVICE_STATE_UNPLUGGED ||
          (devices->device[i]->state == CUBEB_DEVICE_STATE_DISABLED &&
           devices->device[i]->friendly_name &&
           strcmp(devices->device[i]->friendly_name, "Sine source at 440 Hz") == 0)))
@@ -90,6 +91,11 @@ void AudioInputCubeb::UpdateDeviceList()
         // new device, add to the array
         mDeviceIndexes->AppendElement(i);
         mDeviceNames->AppendElement(devices->device[i]->device_id);
+      }
+      if (devices->device[i]->preferred & CUBEB_DEVICE_PREF_VOICE) {
+        // There can be only one... we hope
+        NS_ASSERTION(mDefaultDevice == -1, "multiple default cubeb input devices!");
+        mDefaultDevice = i;
       }
     }
   }
