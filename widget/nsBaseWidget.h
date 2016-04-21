8295d64db5ee6fc6c9fd518b0952ec6fe194553c
Nicolas Silva <nsilva@mozilla.com>
2016-04-21 12:20:31 +0200

    Bug 1264365 - Disable hardware acceleration for small popup widgets. r=dvander

--- widget/nsBaseWidget.h
+++ widget/nsBaseWidget.h
@@ -293,6 +293,10 @@ public:
   mozilla::a11y::Accessible* GetRootAccessible();
 #endif
 
+  // Return true if this is a simple widget (that is typically not worth
+  // accelerating)
+  bool IsSmallPopup() const;
+
   nsPopupLevel PopupLevel() { return mPopupLevel; }
 
   virtual LayoutDeviceIntSize
