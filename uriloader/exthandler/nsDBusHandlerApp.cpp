ec0b5f67ca7cdced0f2346361c7da27e04168ba9
Thomas Zimmermann <tdz@users.sourceforge.net>
2016-04-21 11:24:23 +0200

    Bug 1266033: Fix white-space errors in |nsDBusHandlerApp|, r=bz

--- uriloader/exthandler/nsDBusHandlerApp.cpp
+++ uriloader/exthandler/nsDBusHandlerApp.cpp
@@ -11,7 +11,7 @@
 #include "nsCOMPtr.h"
 #include "nsCExternalHandlerService.h"
 
-// XXX why does nsMIMEInfoImpl have a threadsafe nsISupports?  do we need one 
+// XXX why does nsMIMEInfoImpl have a threadsafe nsISupports?  do we need one
 // here too?
 NS_IMPL_CLASSINFO(nsDBusHandlerApp, nullptr, 0, NS_DBUSHANDLERAPP_CID)
 NS_IMPL_ISUPPORTS_CI(nsDBusHandlerApp, nsIDBusHandlerApp, nsIHandlerApp)
@@ -41,7 +41,7 @@ NS_IMETHODIMP nsDBusHandlerApp::SetDetailedDescription(const nsAString & aDescri
 NS_IMETHODIMP nsDBusHandlerApp::GetDetailedDescription(nsAString& aDescription)
 {
   aDescription.Assign(mDetailedDescription);
-  
+
   return NS_OK;
 }
 
@@ -49,7 +49,7 @@ NS_IMETHODIMP
 nsDBusHandlerApp::Equals(nsIHandlerApp *aHandlerApp, bool *_retval)
 {
   NS_ENSURE_ARG_POINTER(aHandlerApp);
-  
+
   // If the handler app isn't a dbus handler app, then it's not the same app.
   nsCOMPtr<nsIDBusHandlerApp> dbusHandlerApp = do_QueryInterface(aHandlerApp);
   if (!dbusHandlerApp) {
@@ -58,7 +58,7 @@ nsDBusHandlerApp::Equals(nsIHandlerApp *aHandlerApp, bool *_retval)
   }
   nsAutoCString service;
   nsAutoCString method;
-  
+
   nsresult rv = dbusHandlerApp->GetService(service);
   if (NS_FAILED(rv)) {
     *_retval = false;
@@ -69,7 +69,7 @@ nsDBusHandlerApp::Equals(nsIHandlerApp *aHandlerApp, bool *_retval)
     *_retval = false;
     return NS_OK;
   }
-  
+
   *_retval = service.Equals(mService) && method.Equals(mMethod);
   return NS_OK;
 }
@@ -81,37 +81,37 @@ nsDBusHandlerApp::LaunchWithURI(nsIURI *aURI,
   nsAutoCString spec;
   nsresult rv = aURI->GetAsciiSpec(spec);
   NS_ENSURE_SUCCESS(rv,rv);
-  const char* uri = spec.get(); 
-  
+  const char* uri = spec.get();
+
   DBusError err;
   dbus_error_init(&err);
-  
+
   DBusConnection  *connection;
   connection = dbus_bus_get(DBUS_BUS_SESSION, &err);
-  if (dbus_error_is_set(&err)) { 
-    dbus_error_free(&err); 
+  if (dbus_error_is_set(&err)) {
+    dbus_error_free(&err);
     return NS_ERROR_FAILURE;
   }
-  if (nullptr == connection) { 
-    return NS_ERROR_FAILURE; 
+  if (nullptr == connection) {
+    return NS_ERROR_FAILURE;
   }
   dbus_connection_set_exit_on_disconnect(connection,false);
-  
+
   DBusMessage* msg;
-  msg = dbus_message_new_method_call(mService.get(), 
-                                     mObjpath.get(), 
-                                     mInterface.get(), 
+  msg = dbus_message_new_method_call(mService.get(),
+                                     mObjpath.get(),
+                                     mInterface.get(),
                                      mMethod.get());
-  
+
   if (!msg) {
     return NS_ERROR_FAILURE;
   }
   dbus_message_set_no_reply(msg, true);
-  
+
   DBusMessageIter iter;
   dbus_message_iter_init_append(msg, &iter);
   dbus_message_iter_append_basic(&iter, DBUS_TYPE_STRING, &uri);
-  
+
   if (dbus_connection_send(connection, msg, nullptr)) {
     dbus_connection_flush(connection);
     dbus_message_unref(msg);
@@ -120,7 +120,7 @@ nsDBusHandlerApp::LaunchWithURI(nsIURI *aURI,
     return NS_ERROR_FAILURE;
   }
   return NS_OK;
-  
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
