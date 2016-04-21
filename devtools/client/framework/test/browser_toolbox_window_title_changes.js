cc510a79aea6989d34e698ecca908cd454d949b6
Jan Odvarko <odvarko@gmail.com>
2016-04-21 23:23:48 +0200

    Bug 1201475 - Fix test failures; r=jryans
    MozReview-Commit-ID: L2C6LdChJz8

--- devtools/client/framework/test/browser_toolbox_window_title_changes.js
+++ devtools/client/framework/test/browser_toolbox_window_title_changes.js
@@ -3,6 +3,8 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+requestLongerTimeout(5);
+
 var {Toolbox} = require("devtools/client/framework/toolbox");
 
 function test() {
