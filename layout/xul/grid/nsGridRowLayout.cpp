ea114a38d6177a01a8640c0666a458c6ba9814a3
L. David Baron <dbaron@dbaron.org>
2016-04-20 21:28:33 -0700

    Bug 1053986 - Rename nsIFrame::GetLayoutManager to GetXULLayoutManager.  r=dholbert
    Written purely with sed, over .h and .cpp files in layout/.
    
    MozReview-Commit-ID: LbOjEpXmcH4

--- layout/xul/grid/nsGridRowLayout.cpp
+++ layout/xul/grid/nsGridRowLayout.cpp
@@ -100,7 +100,7 @@ nsGridRowLayout::GetGrid(nsIFrame* aBox, int32_t* aIndex, nsGridRowLayout* aRequ
      // if there is a scrollframe walk inside it to its child
      nsIFrame* childBox = nsGrid::GetScrolledBox(child);
 
-     nsBoxLayout* layout = childBox->GetLayoutManager();
+     nsBoxLayout* layout = childBox->GetXULLayoutManager();
      nsIGridPart* gridRow = nsGrid::GetPartFromBox(childBox);
      if (gridRow) 
      {
