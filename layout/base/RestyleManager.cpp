68af7e645706d29db307e767604d4f613e93cb59
Xidorn Quan <quanxunzhen@gmail.com>
2016-04-22 09:18:41 +1000

    Bug 1097499 part 4 - Adjust computed value of writing-mode on text frames when text-combine-upright is used. r=heycam
    MozReview-Commit-ID: KNRe0ZpzxGr
    
    --HG--
    extra : source : dfd24ab49704b6542207aaab5b1283a4db355e60

--- layout/base/RestyleManager.cpp
+++ layout/base/RestyleManager.cpp
@@ -3668,6 +3668,14 @@ ElementRestyler::ComputeRestyleResultFromNewContext(nsIFrame* aSelf,
     aCanStopWithStyleChange = false;
     return;
   }
+
+  if (oldContext->IsTextCombined() != aNewContext->IsTextCombined()) {
+    LOG_RESTYLE_CONTINUE("NS_STYLE_IS_TEXT_COMBINED differs between "
+                         "old and new style contexts");
+    aRestyleResult = eRestyleResult_Continue;
+    aCanStopWithStyleChange = false;
+    return;
+  }
 }
 
 bool
