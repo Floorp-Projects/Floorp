3882751a3de19f4c41cc5fc777d93d0df46fcdcf
Matt Woodrow <mwoodrow@mozilla.com>
2016-04-22 15:39:56 +1200

    Bug 1260611 - Part 1: Opt-in to waiting for compositor recycling for TextureClientRecycleAllocator. r=sotaro

--- gfx/layers/client/TextureClient.cpp
+++ gfx/layers/client/TextureClient.cpp
@@ -963,6 +963,7 @@ TextureClient::TextureClient(TextureData* aData, TextureFlags aFlags, ClientIPCA
 , mExpectedDtRefs(0)
 #endif
 , mIsLocked(false)
+, mInUse(false)
 , mAddedToCompositableClient(false)
 , mWorkaroundAnnoyingSharedSurfaceLifetimeIssues(false)
 , mWorkaroundAnnoyingSharedSurfaceOwnershipIssues(false)
