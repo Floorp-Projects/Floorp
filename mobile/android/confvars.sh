e95b6181260cfacf014d6dc0d550927f6226bc0f
Chris Manchester <cmanchester@mozilla.com>
2016-04-22 13:01:21 -0700

    Bug 1257958 - Move MOZ_ANDROID_GCM to Python configure. r=nalexander
    MozReview-Commit-ID: 90vbYKB8rHJ

--- mobile/android/confvars.sh
+++ mobile/android/confvars.sh
@@ -86,11 +86,6 @@ MOZ_ANDROID_PACKAGE_INSTALL_BOUNCER=1
 # Use the low-memory GC tuning.
 export JS_GC_SMALL_CHUNK_SIZE=1
 
-# Enable GCM registration on Nightly builds only.
-if test "$NIGHTLY_BUILD"; then
-  MOZ_ANDROID_GCM=1
-fi
-
 # Enable C++ APZ.
 MOZ_ANDROID_APZ=1
 
