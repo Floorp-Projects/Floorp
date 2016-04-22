b2467ff7920ee58234fb5b9064e482c5335823ba
Edwin Flores <eflores@mozilla.com>
2016-04-22 14:08:25 +0100

    Bug 1253995 - Display stale image in nsImageFrame if we have a new src but haven't decoded it yet - r=seth a=ritu
    --HG--
    extra : source : 3e12df7cefe4cf2e0b8dc11867e1008b21aa135a

--- layout/generic/nsImageFrame.h
+++ layout/generic/nsImageFrame.h
@@ -326,6 +326,7 @@ private:
   nsCOMPtr<imgINotificationObserver> mListener;
 
   nsCOMPtr<imgIContainer> mImage;
+  nsCOMPtr<imgIContainer> mPrevImage;
   nsSize mComputedSize;
   mozilla::IntrinsicSize mIntrinsicSize;
   nsSize mIntrinsicRatio;
@@ -406,8 +407,11 @@ public:
   typedef mozilla::layers::LayerManager LayerManager;
 
   nsDisplayImage(nsDisplayListBuilder* aBuilder, nsImageFrame* aFrame,
-                 imgIContainer* aImage)
-    : nsDisplayImageContainer(aBuilder, aFrame), mImage(aImage) {
+                 imgIContainer* aImage, imgIContainer* aPrevImage)
+    : nsDisplayImageContainer(aBuilder, aFrame)
+    , mImage(aImage)
+    , mPrevImage(aPrevImage)
+  {
     MOZ_COUNT_CTOR(nsDisplayImage);
   }
   virtual ~nsDisplayImage() {
@@ -471,6 +475,7 @@ public:
   NS_DISPLAY_DECL_NAME("Image", TYPE_IMAGE)
 private:
   nsCOMPtr<imgIContainer> mImage;
+  nsCOMPtr<imgIContainer> mPrevImage;
 };
 
 #endif /* nsImageFrame_h___ */
