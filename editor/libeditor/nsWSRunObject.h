39f7d711a1e85e2f323a023473a7a871f75df4fb
Aryeh Gregor <ayg@aryeh.name>
2016-04-23 18:31:55 +0900

    Bug 1156062 part 5 - Make CreateBR return regular pointer; r=ehsan

--- editor/libeditor/nsWSRunObject.h
+++ editor/libeditor/nsWSRunObject.h
@@ -203,9 +203,9 @@ class MOZ_STACK_CLASS nsWSRunObject
     // and makes any needed adjustments to ws around that point.
     // example of fixup: normalws after {aInOutParent,aInOutOffset}
     //                   needs to begin with nbsp.
-    already_AddRefed<mozilla::dom::Element>
-      InsertBreak(nsCOMPtr<nsINode>* aInOutParent, int32_t* aInOutOffset,
-                  nsIEditor::EDirection aSelect);
+    mozilla::dom::Element* InsertBreak(nsCOMPtr<nsINode>* aInOutParent,
+                                       int32_t* aInOutOffset,
+                                       nsIEditor::EDirection aSelect);
 
     // InsertText inserts a string at {aInOutParent,aInOutOffset} and makes any
     // needed adjustments to ws around that point.  Example of fixup:
