cc1c10dbaee5de6712bb3d64717e4666c1512386
Kris Maglione <maglione.k@gmail.com>
2016-04-23 20:41:14 -0700

    Bug 1254194: Add a validator for custom add-on content security policies. r=billm f=aswan
    MozReview-Commit-ID: LtBbXBCFc32
    
    --HG--
    extra : rebase_source : 1da81c92a1ffb75df071d1b32ff04b7d1a9b905a

---
+++ toolkit/components/extensions/test/xpcshell/test_csp_validator.js
@@ -0,0 +1,85 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+"use strict";
+
+const cps = Cc["@mozilla.org/addons/content-policy;1"].getService(Ci.nsIAddonContentPolicy);
+
+add_task(function* test_csp_validator() {
+  let checkPolicy = (policy, expectedResult, message = null) => {
+    do_print(`Checking policy: ${policy}`);
+
+    let result = cps.validateAddonCSP(policy);
+    equal(result, expectedResult);
+  };
+
+  checkPolicy("script-src 'self'; object-src 'self';",
+              null);
+
+  let hash = "'sha256-NjZhMDQ1YjQ1MjEwMmM1OWQ4NDBlYzA5N2Q1OWQ5NDY3ZTEzYTNmMzRmNjQ5NGU1MzlmZmQzMmMxYmIzNWYxOCAgLQo='";
+
+  checkPolicy(`script-src 'self' https://com https://*.example.com moz-extension://09abcdef blob: filesystem: ${hash} 'unsafe-eval'; ` +
+              `object-src 'self' https://com https://*.example.com moz-extension://09abcdef blob: filesystem: ${hash}`,
+              null);
+
+  checkPolicy("",
+              "Policy is missing a required 'script-src' directive");
+
+  checkPolicy("object-src 'none';",
+              "Policy is missing a required 'script-src' directive");
+
+
+  checkPolicy("default-src 'self'", null,
+              "A valid default-src should count as a valid script-src or object-src");
+
+  checkPolicy("default-src 'self'; script-src 'self'", null,
+              "A valid default-src should count as a valid script-src or object-src");
+
+  checkPolicy("default-src 'self'; object-src 'self'", null,
+              "A valid default-src should count as a valid script-src or object-src");
+
+
+  checkPolicy("default-src 'self'; script-src http://example.com",
+              "'script-src' directive contains a forbidden http: protocol source",
+              "A valid default-src should not allow an invalid script-src directive");
+
+  checkPolicy("default-src 'self'; object-src http://example.com",
+              "'object-src' directive contains a forbidden http: protocol source",
+              "A valid default-src should not allow an invalid object-src directive");
+
+
+  checkPolicy("script-src 'self';",
+              "Policy is missing a required 'object-src' directive");
+
+  checkPolicy("script-src 'none'; object-src 'none'",
+              "'script-src' must include the source 'self'");
+
+  checkPolicy("script-src 'self'; object-src 'none';",
+              null);
+
+  checkPolicy("script-src 'self' 'unsafe-inline'; object-src 'self';",
+              "'script-src' directive contains a forbidden 'unsafe-inline' keyword");
+
+
+  let directives = ["script-src", "object-src"];
+
+  for (let [directive, other] of [directives, directives.slice().reverse()]) {
+    for (let src of ["https://*", "https://*.blogspot.com", "https://*"]) {
+      checkPolicy(`${directive} 'self' ${src}; ${other} 'self';`,
+                  `https: wildcard sources in '${directive}' directives must include at least one non-generic sub-domain (e.g., *.example.com rather than *.com)`);
+    }
+
+    checkPolicy(`${directive} 'self' https:; ${other} 'self';`,
+                `https: protocol requires a host in '${directive}' directives`);
+
+    checkPolicy(`${directive} 'self' http://example.com; ${other} 'self';`,
+                `'${directive}' directive contains a forbidden http: protocol source`);
+
+    for (let protocol of ["http", "ftp", "meh"]) {
+      checkPolicy(`${directive} 'self' ${protocol}:; ${other} 'self';`,
+                  `'${directive}' directive contains a forbidden ${protocol}: protocol source`);
+    }
+
+    checkPolicy(`${directive} 'self' 'nonce-01234'; ${other} 'self';`,
+                `'${directive}' directive contains a forbidden 'nonce-*' keyword`);
+  }
+});
