b20273ce98611efe3eb44018a19e162ae8c2b58b
Jeff Walden <jwalden@mit.edu>
2016-04-23 17:51:16 -0700

    Bug 1280362 - Move assertEqArray into shell.js's IIFE, and delete a duplicate of it elsewhere.  r=arai
    --HG--
    extra : rebase_source : d6dadef576fc4efd172dd064feae819369faa8ad

--- js/src/tests/Intl/shell.js
+++
@@ -1,9 +0,0 @@
-if (typeof assertShallowArray === 'undefined') {
-  var assertShallowArray = function assertShallowArray(actual, expected) {
-    var len = actual.length;
-    assertEq(len, expected.length, "mismatching array lengths");
-
-    for (var i = 0; i < len; i++)
-      assertEq(actual[i], expected[i], "mismatch at element " + i);
-  }
-}
