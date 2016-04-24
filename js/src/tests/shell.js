e43495a4cd67126b5d4179a9589266c7e8ec5ee5
Jeff Walden <jwalden@mit.edu>
2016-04-23 17:51:15 -0700

    Bug 1280362 - Move assertThrowsInstanceOf into shell.js's IIFE.  r=arai

--- js/src/tests/shell.js
+++ js/src/tests/shell.js
@@ -55,6 +55,29 @@
   }
   global.assertThrows = assertThrows;
 
+  function assertThrowsInstanceOf(f, ctor, msg) {
+    var fullmsg;
+    try {
+      f();
+    } catch (exc) {
+      if (exc instanceof ctor)
+        return;
+      fullmsg =
+        "Assertion failed: expected exception " + ctor.name + ", got " + exc;
+    }
+
+    if (fullmsg === undefined) {
+      fullmsg =
+        "Assertion failed: expected exception " + ctor.name + ", " +
+        "no exception thrown";
+    }
+    if (msg !== undefined)
+      fullmsg += " - " + msg;
+
+    throw new Error(fullmsg);
+  }
+  global.assertThrowsInstanceOf = assertThrowsInstanceOf;
+
   /****************************
    * UTILITY FUNCTION EXPORTS *
    ****************************/
@@ -936,19 +959,3 @@ function assertEqArray(a1, a2) {
     }
   }
 }
-
-function assertThrowsInstanceOf(f, ctor, msg) {
-  var fullmsg;
-  try {
-    f();
-  } catch (exc) {
-    if (exc instanceof ctor)
-      return;
-    fullmsg = "Assertion failed: expected exception " + ctor.name + ", got " + exc;
-  }
-  if (fullmsg === undefined)
-    fullmsg = "Assertion failed: expected exception " + ctor.name + ", no exception thrown";
-  if (msg !== undefined)
-    fullmsg += " - " + msg;
-  throw new Error(fullmsg);
-};
