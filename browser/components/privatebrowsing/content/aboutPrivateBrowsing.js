bdb9281e48414c37995b6dc50d15fbfd064d7422
Ricky Chien <rchien@mozilla.com>
2016-04-24 15:52:58 +0300

    Bug 1259340 - New Private Browsing Window Update. r=past,Gijs
    MozReview-Commit-ID: 5NbLribECvC

--- browser/components/privatebrowsing/content/aboutPrivateBrowsing.js
+++ browser/components/privatebrowsing/content/aboutPrivateBrowsing.js
@@ -8,85 +8,92 @@ Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-const FAVICON_QUESTION = "chrome://global/skin/icons/question-16.png";
-const FAVICON_PRIVACY = "chrome://browser/skin/Privacy-16.png";
+const FAVICON_QUESTION = "chrome://global/skin/icons/question-32.png";
+const FAVICON_PRIVACY = "chrome://browser/skin/privatebrowsing/favicon.svg";
 
 var stringBundle = Services.strings.createBundle(
-                     "chrome://browser/locale/aboutPrivateBrowsing.properties");
+                    "chrome://browser/locale/aboutPrivateBrowsing.properties");
 
 var prefBranch = Services.prefs.getBranch("privacy.trackingprotection.");
 var prefObserver = {
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver,
-                                         Ci.nsISupportsWeakReference]),
-  observe: function () {
-    if (prefBranch.getBoolPref("enabled")) {
-      document.body.setAttribute("globalTpEnabled", "true");
-    } else {
-      document.body.removeAttribute("globalTpEnabled");
-    }
-    if (prefBranch.getBoolPref("pbmode.enabled") ||
-        prefBranch.getBoolPref("enabled")) {
-      document.body.setAttribute("tpEnabled", "true");
-    } else {
-      document.body.removeAttribute("tpEnabled");
-    }
-  },
+ QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver,
+                                        Ci.nsISupportsWeakReference]),
+ observe: function () {
+   let tpSubHeader = document.getElementById("tpSubHeader");
+   let tpToggle = document.getElementById("tpToggle");
+   let tpButton = document.getElementById("tpButton");
+   let title = document.getElementById("title");
+   let titleTracking = document.getElementById("titleTracking");
+   let globalTrackingEnabled = prefBranch.getBoolPref("enabled");
+   let trackingEnabled = globalTrackingEnabled ||
+                         prefBranch.getBoolPref("pbmode.enabled");
+
+   tpButton.classList.toggle("hide", globalTrackingEnabled);
+   tpToggle.checked = trackingEnabled;
+   title.classList.toggle("hide", trackingEnabled);
+   titleTracking.classList.toggle("hide", !trackingEnabled);
+   tpSubHeader.classList.toggle("tp-off", !trackingEnabled);
+ }
 };
 prefBranch.addObserver("pbmode.enabled", prefObserver, true);
 prefBranch.addObserver("enabled", prefObserver, true);
 
 function setFavIcon(url) {
-  document.getElementById("favicon").setAttribute("href", url);
+ document.getElementById("favicon").setAttribute("href", url);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (!PrivateBrowsingUtils.isContentWindowPrivate(window)) {
-    document.body.setAttribute("class", "normal");
-    document.title = stringBundle.GetStringFromName("title.normal");
-    document.getElementById("favicon")
-            .setAttribute("href", FAVICON_QUESTION);
-    document.getElementById("startPrivateBrowsing")
-            .addEventListener("command", openPrivateWindow);
-    return;
-  }
+ if (!PrivateBrowsingUtils.isContentWindowPrivate(window)) {
+   document.body.classList.remove("private");
+   document.body.classList.add("normal");
+   document.title = stringBundle.GetStringFromName("title.normal");
+   document.getElementById("favicon")
+           .setAttribute("href", FAVICON_QUESTION);
+   document.getElementById("startPrivateBrowsing")
+           .addEventListener("command", openPrivateWindow);
+   return;
+ }
 
-  document.title = stringBundle.GetStringFromName("title");
-  document.getElementById("favicon")
-          .setAttribute("href", FAVICON_PRIVACY);
-  document.getElementById("enableTrackingProtection")
-          .addEventListener("click", toggleTrackingProtection);
-  document.getElementById("disableTrackingProtection")
-          .addEventListener("click", toggleTrackingProtection);
-  document.getElementById("startTour")
-          .addEventListener("click", dontShowIntroPanelAgain);
+ let tpToggle = document.getElementById("tpToggle");
+ document.getElementById("tpButton").addEventListener('click', () => {
+   tpToggle.checked = !tpToggle.checked;
+ });
 
-  let formatURLPref = Cc["@mozilla.org/toolkit/URLFormatterService;1"]
-                        .getService(Ci.nsIURLFormatter).formatURLPref;
-  document.getElementById("startTour").setAttribute("href",
-                     formatURLPref("privacy.trackingprotection.introURL"));
-  document.getElementById("learnMore").setAttribute("href",
-                     formatURLPref("app.support.baseURL") + "private-browsing");
+ document.title = stringBundle.GetStringFromName("title.head");
+ document.getElementById("favicon")
+         .setAttribute("href", FAVICON_PRIVACY);
+ document.getElementById("tpButton")
+         .addEventListener("click", toggleTrackingProtection);
+ document.getElementById("startTour")
+         .addEventListener("click", dontShowIntroPanelAgain);
 
-  // Update state that depends on preferences.
-  prefObserver.observe();
+ let formatURLPref = Cc["@mozilla.org/toolkit/URLFormatterService;1"]
+                       .getService(Ci.nsIURLFormatter).formatURLPref;
+ document.getElementById("startTour").setAttribute("href",
+                    formatURLPref("privacy.trackingprotection.introURL"));
+ document.getElementById("learnMore").setAttribute("href",
+                    formatURLPref("app.support.baseURL") + "private-browsing");
+
+ // Update state that depends on preferences.
+ prefObserver.observe();
 }, false);
 
 function openPrivateWindow() {
-  // Ask chrome to open a private window
-  document.dispatchEvent(
-    new CustomEvent("AboutPrivateBrowsingOpenWindow", {bubbles:true}));
+ // Ask chrome to open a private window
+ document.dispatchEvent(
+   new CustomEvent("AboutPrivateBrowsingOpenWindow", {bubbles:true}));
 }
 
 function toggleTrackingProtection() {
-  // Ask chrome to enable tracking protection
-  document.dispatchEvent(
-    new CustomEvent("AboutPrivateBrowsingToggleTrackingProtection",
-                    {bubbles:true}));
+ // Ask chrome to enable tracking protection
+ document.dispatchEvent(
+   new CustomEvent("AboutPrivateBrowsingToggleTrackingProtection",
+                   {bubbles: true}));
 }
 
 function dontShowIntroPanelAgain() {
-  // Ask chrome to disable the doorhanger
-  document.dispatchEvent(
-    new CustomEvent("AboutPrivateBrowsingDontShowIntroPanelAgain",
-                    {bubbles:true}));
+ // Ask chrome to disable the doorhanger
+ document.dispatchEvent(
+   new CustomEvent("AboutPrivateBrowsingDontShowIntroPanelAgain",
+                   {bubbles: true}));
 }
