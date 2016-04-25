2947931e1bd593c85adc584e2854d0bdc35e4ab7
Botond Ballo <botond@mozilla.com>
2016-04-25 17:20:13 -0400

    Bug 735857 - Treat background-attachment:fixed as background-attachment:scroll if it's on a non-root element affected by a transform. r=mstange
    MozReview-Commit-ID: 1lnQoD98xv3

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -2721,6 +2721,8 @@ protected:
                                   gfxRect* aDestRect);
   bool IsNonEmptyFixedImage() const;
   nsRect GetBoundsInternal(nsDisplayListBuilder* aBuilder);
+  bool ShouldTreatAsFixed() const;
+  bool ComputeShouldTreatAsFixed(bool isTransformedFixed) const;
 
   void PaintInternal(nsDisplayListBuilder* aBuilder, nsRenderingContext* aCtx,
                      const nsRect& aBounds, nsRect* aClipRect);
@@ -2745,6 +2747,8 @@ protected:
   nsRect mBounds;
   uint32_t mLayer;
   bool mIsRasterImage;
+  /* Whether the image should be treated as fixed to the viewport. */
+  bool mShouldTreatAsFixed;
 };
 
 
