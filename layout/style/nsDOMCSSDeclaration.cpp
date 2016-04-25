161986900bbb6585d609a7f62325ee13cf90ca4f
Markus Stange <mstange@themasta.com>
2016-04-25 18:56:47 -0400

    Bug 550426 - Use background-position-x/y when detecting scroll-linked effects. r=dbaron
    MozReview-Commit-ID: 6qDFcaEBYEF
    
    --HG--
    extra : rebase_source : 1bee538433c7cc5ca7cfef6fd73db44632cf8bed

--- layout/style/nsDOMCSSDeclaration.cpp
+++ layout/style/nsDOMCSSDeclaration.cpp
@@ -80,6 +80,8 @@ nsDOMCSSDeclaration::SetPropertyValue(const nsCSSProperty aPropID,
 {
   switch (aPropID) {
     case eCSSProperty_background_position:
+    case eCSSProperty_background_position_x:
+    case eCSSProperty_background_position_y:
     case eCSSProperty_transform:
     case eCSSProperty_top:
     case eCSSProperty_left:
