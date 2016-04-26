8e028efc83f63a1aa782ea27b5f9da375380f955
Nils Ohlmeier [:drno] <drno@ohlmeier.org>
2016-04-26 13:11:25 -0700

    Bug 929977: Add support for RFC 7675 ICE consent freshness. r=bwc,mt
    MozReview-Commit-ID: HGRM10L0R3M

--- media/mtransport/test_nr_socket.cpp
+++ media/mtransport/test_nr_socket.cpp
@@ -84,6 +84,7 @@ extern "C" {
 #include "stun_msg.h" // for NR_STUN_MAX_MESSAGE_SIZE
 #include "nr_api.h"
 #include "async_wait.h"
+#include "async_timer.h"
 #include "nr_socket.h"
 #include "nr_socket_local.h"
 #include "stun_hint.h"
@@ -199,7 +200,8 @@ int TestNat::create_socket_factory(nr_socket_factory **factorypp) {
 }
 
 TestNrSocket::TestNrSocket(TestNat *nat)
-  : nat_(nat) {
+  : nat_(nat),
+    timer_handle_(nullptr) {
   nat_->insert_socket(this);
 }
 
@@ -252,6 +254,10 @@ int TestNrSocket::getaddr(nr_transport_addr *addrp) {
 }
 
 void TestNrSocket::close() {
+  if (timer_handle_) {
+    NR_async_timer_cancel(timer_handle_);
+    timer_handle_ = 0;
+  }
   internal_socket_->close();
   for (RefPtr<PortMapping>& port_mapping : port_mappings_) {
     port_mapping->external_socket_->close();
@@ -282,11 +288,39 @@ int TestNrSocket::accept(nr_transport_addr *addrp, nr_socket **sockp) {
   return 0;
 }
 
+void TestNrSocket::process_delayed_cb(NR_SOCKET s, int how, void *cb_arg) {
+  DeferredPacket *op = static_cast<DeferredPacket *>(cb_arg);
+  op->socket_->timer_handle_ = nullptr;
+  r_log(LOG_GENERIC, LOG_DEBUG,
+        "TestNrSocket %s sending delayed STUN response",
+        op->internal_socket_->my_addr().as_string);
+  op->internal_socket_->sendto(op->buffer_.data(), op->buffer_.len(),
+                               op->flags_, &op->to_);
+
+  delete op;
+}
+
 int TestNrSocket::sendto(const void *msg, size_t len,
                          int flags, nr_transport_addr *to) {
   MOZ_ASSERT(internal_socket_->my_addr().protocol != IPPROTO_TCP);
 
+  UCHAR *buf = static_cast<UCHAR*>(const_cast<void*>(msg));
+  if (nat_->block_stun_ &&
+      nr_is_stun_message(buf, len)) {
+    return 0;
+  }
+
+  /* TODO: improve the functionality of this in bug 1253657 */
   if (!nat_->enabled_ || nat_->is_an_internal_tuple(*to)) {
+    if (nat_->delay_stun_resp_ms_ &&
+        nr_is_stun_response_message(buf, len)) {
+      NR_ASYNC_TIMER_SET(nat_->delay_stun_resp_ms_,
+                         process_delayed_cb,
+                         new DeferredPacket(this, msg, len, flags, to,
+                                            internal_socket_),
+                         &timer_handle_);
+      return 0;
+    }
     return internal_socket_->sendto(msg, len, flags, to);
   }
 
