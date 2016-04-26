d1f5595380f3a4765f3829ee161de0e0402473f6
Tom Schuster <evilpies@gmail.com>
2016-04-26 17:42:02 +0200

    Bug 1255925 - Change JSFunction::name to return a JSAtom. r=efaust

--- js/src/vm/Interpreter.cpp
+++ js/src/vm/Interpreter.cpp
@@ -4219,7 +4219,7 @@ js::DefFunOperation(JSContext* cx, HandleScript script, HandleObject scopeChain,
         parent = parent->enclosingScope();
 
     /* ES5 10.5 (NB: with subsequent errata). */
-    RootedPropertyName name(cx, fun->atom()->asPropertyName());
+    RootedPropertyName name(cx, fun->name()->asPropertyName());
 
     RootedShape shape(cx);
     RootedObject pobj(cx);
@@ -4931,8 +4931,8 @@ js::ThrowUninitializedThis(JSContext* cx, AbstractFramePtr frame)
     if (fun->isDerivedClassConstructor()) {
         const char* name = "anonymous";
         JSAutoByteString str;
-        if (fun->atom()) {
-            if (!AtomToPrintableString(cx, fun->atom(), &str))
+        if (fun->name()) {
+            if (!AtomToPrintableString(cx, fun->name(), &str))
                 return false;
             name = str.ptr();
         }
