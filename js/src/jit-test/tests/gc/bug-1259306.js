cfb8edbad9ad93a9a2c7d0c2da7b4b6f872f0378
Jon Coppeard <jcoppeard@mozilla.com>
2016-04-27 10:41:05 +0100

    Bug 1264420 - Speed up test bug-1259306.js which sometimes times out r=terrence

--- js/src/jit-test/tests/gc/bug-1259306.js
+++ js/src/jit-test/tests/gc/bug-1259306.js
@@ -1,15 +1,19 @@
 if (!('oomTest' in this))
     quit();
 
+let runCount = 0;
 oomTest(() => {
-    var lfGlobal = newGlobal();
-    var lfVarx = `
-        gczeal(8, 1);
-        try {
-            (5).replace(r, () => {});
-        } catch (x) {}
-        gczeal(0);
-    `;
-    lfGlobal.offThreadCompileScript(lfVarx);
-    lfGlobal.runOffThreadScript();
+    if (runCount < 5) {
+        let lfGlobal = newGlobal();
+        var lfVarx = `
+            gczeal(8, 1);
+            try {
+                (5).replace(r, () => {});
+            } catch (x) {}
+            gczeal(0);
+        `;
+        lfGlobal.offThreadCompileScript(lfVarx);
+        lfGlobal.runOffThreadScript();
+        runCount++;
+    }
 });
