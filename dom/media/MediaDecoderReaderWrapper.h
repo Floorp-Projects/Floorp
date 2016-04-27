27a6bdec4394f537634a1742433a65875cba94d1
Kaku Kuo <tkuo@mozilla.com>
2016-04-27 14:50:23 +0800

    Bug 1266027 part 1 - make the MediaDecoderReaderWrapper as a proxy of requesting media data; r=jwwang
    MozReview-Commit-ID: CgTBPmtbNfh

--- dom/media/MediaDecoderReaderWrapper.h
+++ dom/media/MediaDecoderReaderWrapper.h
@@ -12,6 +12,7 @@
 #include "nsISupportsImpl.h"
 
 #include "MediaDecoderReader.h"
+#include "MediaCallbackID.h"
 
 namespace mozilla {
 
@@ -33,6 +34,135 @@ class MediaDecoderReaderWrapper {
   typedef MediaDecoderReader::BufferedUpdatePromise BufferedUpdatePromise;
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(MediaDecoderReaderWrapper);
 
+  /*
+   * Type 1: void(MediaData*)
+   *         void(RefPtr<MediaData>)
+   */
+  template <typename T>
+  class ArgType1CheckHelper {
+    template<typename C, typename... Ts>
+    static TrueType
+    test(void(C::*aMethod)(Ts...),
+         decltype((DeclVal<C>().*aMethod)(DeclVal<MediaData*>()), 0));
+
+    template <typename F>
+    static TrueType
+    test(F&&, decltype(DeclVal<F>()(DeclVal<MediaData*>()), 0));
+
+    static FalseType test(...);
+  public:
+    typedef decltype(test(DeclVal<T>(), 0)) Type;
+  };
+
+  template <typename T>
+  struct ArgType1Check : public ArgType1CheckHelper<T>::Type {};
+
+  /*
+   * Type 2: void(MediaData*, TimeStamp)
+   *         void(RefPtr<MediaData>, TimeStamp)
+   *         void(MediaData*, TimeStamp&)
+   *         void(RefPtr<MediaData>, const TimeStamp&&)
+   */
+  template <typename T>
+  class ArgType2CheckHelper {
+
+    template<typename C, typename... Ts>
+    static TrueType
+    test(void(C::*aMethod)(Ts...),
+         decltype((DeclVal<C>().*aMethod)(DeclVal<MediaData*>(), DeclVal<TimeStamp>()), 0));
+
+    template <typename F>
+    static TrueType
+    test(F&&, decltype(DeclVal<F>()(DeclVal<MediaData*>(), DeclVal<TimeStamp>()), 0));
+
+    static FalseType test(...);
+  public:
+    typedef decltype(test(DeclVal<T>(), 0)) Type;
+  };
+
+  template <typename T>
+  struct ArgType2Check : public ArgType2CheckHelper<T>::Type {};
+
+  struct CallbackBase
+  {
+    virtual ~CallbackBase() {}
+    virtual void OnResolved(MediaData*, TimeStamp) = 0;
+    virtual void OnRejected(MediaDecoderReader::NotDecodedReason) = 0;
+  };
+
+  template<typename ThisType, typename ResolveMethodType, typename RejectMethodType>
+  struct MethodCallback : public CallbackBase
+  {
+    MethodCallback(ThisType* aThis, ResolveMethodType aResolveMethod, RejectMethodType aRejectMethod)
+      : mThis(aThis), mResolveMethod(aResolveMethod), mRejectMethod(aRejectMethod)
+    {
+    }
+
+    template<typename F>
+    typename EnableIf<ArgType1Check<F>::value, void>::Type
+    CallHelper(MediaData* aSample, TimeStamp)
+    {
+      (mThis->*mResolveMethod)(aSample);
+    }
+
+    template<typename F>
+    typename EnableIf<ArgType2Check<F>::value, void>::Type
+    CallHelper(MediaData* aSample, TimeStamp aDecodeStartTime)
+    {
+      (mThis->*mResolveMethod)(aSample, aDecodeStartTime);
+    }
+
+    void OnResolved(MediaData* aSample, TimeStamp aDecodeStartTime) override
+    {
+      CallHelper<ResolveMethodType>(aSample, aDecodeStartTime);
+    }
+
+    void OnRejected(MediaDecoderReader::NotDecodedReason aReason) override
+    {
+      (mThis->*mRejectMethod)(aReason);
+    }
+
+    RefPtr<ThisType> mThis;
+    ResolveMethodType mResolveMethod;
+    RejectMethodType mRejectMethod;
+  };
+
+  template<typename ResolveFunctionType, typename RejectFunctionType>
+  struct FunctionCallback : public CallbackBase
+  {
+    FunctionCallback(ResolveFunctionType&& aResolveFuntion, RejectFunctionType&& aRejectFunction)
+      : mResolveFuntion(Move(aResolveFuntion)), mRejectFunction(Move(aRejectFunction))
+    {
+    }
+
+    template<typename F>
+    typename EnableIf<ArgType1Check<F>::value, void>::Type
+    CallHelper(MediaData* aSample, TimeStamp)
+    {
+      mResolveFuntion(aSample);
+    }
+
+    template<typename F>
+    typename EnableIf<ArgType2Check<F>::value, void>::Type
+    CallHelper(MediaData* aSample, TimeStamp aDecodeStartTime)
+    {
+      mResolveFuntion(aSample, aDecodeStartTime);
+    }
+
+    void OnResolved(MediaData* aSample, TimeStamp aDecodeStartTime) override
+    {
+      CallHelper<ResolveFunctionType>(aSample, aDecodeStartTime);
+    }
+
+    void OnRejected(MediaDecoderReader::NotDecodedReason aReason) override
+    {
+      mRejectFunction(aReason);
+    }
+
+    ResolveFunctionType mResolveFuntion;
+    RejectFunctionType mRejectFunction;
+  };
+
 public:
   MediaDecoderReaderWrapper(bool aIsRealTime,
                             AbstractThread* aOwnerThread,
@@ -41,9 +171,83 @@ public:
   media::TimeUnit StartTime() const;
   RefPtr<MetadataPromise> ReadMetadata();
   RefPtr<HaveStartTimePromise> AwaitStartTime();
-  RefPtr<MediaDataPromise> RequestAudioData();
-  RefPtr<MediaDataPromise> RequestVideoData(bool aSkipToNextKeyframe,
-                                            media::TimeUnit aTimeThreshold);
+
+  template<typename ThisType, typename ResolveMethodType, typename RejectMethodType>
+  CallbackID
+  SetAudioCallback(ThisType* aThisVal,
+                   ResolveMethodType aResolveMethod,
+                   RejectMethodType aRejectMethod)
+  {
+    MOZ_ASSERT(mOwnerThread->IsCurrentThreadIn());
+    MOZ_ASSERT(!mRequestAudioDataCB,
+               "Please cancel the original callback before setting a new one.");
+
+    mRequestAudioDataCB.reset(
+      new MethodCallback<ThisType, ResolveMethodType, RejectMethodType>(
+            aThisVal, aResolveMethod, aRejectMethod));
+
+    return mAudioCallbackID;
+  }
+
+  template<typename ResolveFunction, typename RejectFunction>
+  CallbackID
+  SetAudioCallback(ResolveFunction&& aResolveFunction,
+                   RejectFunction&& aRejectFunction)
+  {
+    MOZ_ASSERT(mOwnerThread->IsCurrentThreadIn());
+    MOZ_ASSERT(!mRequestAudioDataCB,
+               "Please cancel the original callback before setting a new one.");
+
+    mRequestAudioDataCB.reset(
+      new FunctionCallback<ResolveFunction, RejectFunction>(
+            Move(aResolveFunction), Move(aRejectFunction)));
+
+    return mAudioCallbackID;
+  }
+
+  template<typename ThisType, typename ResolveMethodType, typename RejectMethodType>
+  CallbackID
+  SetVideoCallback(ThisType* aThisVal,
+                   ResolveMethodType aResolveMethod,
+                   RejectMethodType aRejectMethod)
+  {
+    MOZ_ASSERT(mOwnerThread->IsCurrentThreadIn());
+    MOZ_ASSERT(!mRequestVideoDataCB,
+               "Please cancel the original callback before setting a new one.");
+
+    mRequestVideoDataCB.reset(
+      new MethodCallback<ThisType, ResolveMethodType, RejectMethodType>(
+            aThisVal, aResolveMethod, aRejectMethod));
+
+    return mVideoCallbackID;
+  }
+
+  template<typename ResolveFunction, typename RejectFunction>
+  CallbackID
+  SetVideoCallback(ResolveFunction&& aResolveFunction,
+                   RejectFunction&& aRejectFunction)
+  {
+    MOZ_ASSERT(mOwnerThread->IsCurrentThreadIn());
+    MOZ_ASSERT(!mRequestVideoDataCB,
+               "Please cancel the original callback before setting a new one.");
+
+    mRequestVideoDataCB.reset(
+      new FunctionCallback<ResolveFunction, RejectFunction>(
+            Move(aResolveFunction), Move(aRejectFunction)));
+
+    return mVideoCallbackID;
+  }
+
+  void CancelAudioCallback(CallbackID aID);
+  void CancelVideoCallback(CallbackID aID);
+
+  // NOTE: please set callbacks before requesting audio/video data!
+  void RequestAudioData();
+  void RequestVideoData(bool aSkipToNextKeyframe, media::TimeUnit aTimeThreshold);
+
+  bool IsRequestingAudioData() const;
+  bool IsRequestingVidoeData() const;
+
   RefPtr<SeekPromise> Seek(SeekTarget aTarget, media::TimeUnit aEndTime);
   RefPtr<WaitForDataPromise> WaitForData(MediaData::Type aType);
   RefPtr<BufferedUpdatePromise> UpdateBufferedWithPromise();
@@ -96,8 +300,10 @@ private:
 
   void OnMetadataRead(MetadataHolder* aMetadata);
   void OnMetadataNotRead() {}
-  void OnSampleDecoded(MediaData* aSample);
-  void OnNotDecoded() {}
+  void OnSampleDecoded(CallbackBase* aCallback, MediaData* aSample,
+                       TimeStamp aVideoDecodeStartTime);
+  void OnNotDecoded(CallbackBase* aCallback,
+                    MediaDecoderReader::NotDecodedReason aReason);
 
   const bool mForceZeroStartTime;
   const RefPtr<AbstractThread> mOwnerThread;
@@ -105,6 +311,17 @@ private:
 
   bool mShutdown = false;
   RefPtr<StartTimeRendezvous> mStartTimeRendezvous;
+
+  UniquePtr<CallbackBase> mRequestAudioDataCB;
+  UniquePtr<CallbackBase> mRequestVideoDataCB;
+  MozPromiseRequestHolder<MediaDataPromise> mAudioDataRequest;
+  MozPromiseRequestHolder<MediaDataPromise> mVideoDataRequest;
+
+  /*
+   * These callback ids are used to prevent mis-canceling callback.
+   */
+  CallbackID mAudioCallbackID;
+  CallbackID mVideoCallbackID;
 };
 
 } // namespace mozilla
