c76afc329cafed52ce61278cb08f0a7bdad49824
Jan de Mooij <jdemooij@mozilla.com>
2016-04-28 13:38:05 +0200

    Bug 1267557 part 0 - Move JS poison constants to jsutil.h. r=jonco

--- js/src/jsutil.h
+++ js/src/jsutil.h
@@ -314,6 +314,22 @@ PodSet(T* aDst, T aSrc, size_t aNElem)
 
 } /* namespace mozilla */
 
+/*
+ * Patterns used by SpiderMonkey to overwrite unused memory. If you are
+ * accessing an object with one of these pattern, you probably have a dangling
+ * pointer.
+ *
+ * Note: new patterns should also be added to the array in IsThingPoisoned!
+ */
+#define JS_FRESH_NURSERY_PATTERN 0x2F
+#define JS_SWEPT_NURSERY_PATTERN 0x2B
+#define JS_ALLOCATED_NURSERY_PATTERN 0x2D
+#define JS_FRESH_TENURED_PATTERN 0x4F
+#define JS_MOVED_TENURED_PATTERN 0x49
+#define JS_SWEPT_TENURED_PATTERN 0x4B
+#define JS_ALLOCATED_TENURED_PATTERN 0x4D
+#define JS_SWEPT_CODE_PATTERN 0x3B
+
 static inline void*
 Poison(void* ptr, uint8_t value, size_t num)
 {
