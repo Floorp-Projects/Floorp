add91d42484c762c69e5158b58eb4dfd538ef82a
Kyle Huey <khuey@kylehuey.com>
2016-04-28 14:08:24 -0700

    Bug 1268313: Part 4 - Replace NewCancelableRunnableMethod with NS_NewCancelableRunnableMethod. r=froydnj

--- gfx/thebes/SoftwareVsyncSource.cpp
+++ gfx/thebes/SoftwareVsyncSource.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -128,14 +128,15 @@ SoftwareDisplay::ScheduleNextVsync(mozilla::TimeStamp aVsyncTimestamp)
     nextVsync = mozilla::TimeStamp::Now();
   }
 
-  mCurrentVsyncTask = NewCancelableRunnableMethod(this,
-      &SoftwareDisplay::NotifyVsync,
-      nextVsync);
+  mCurrentVsyncTask =
+    NS_NewCancelableRunnableMethodWithArgs<mozilla::TimeStamp>(this,
+                                                               &SoftwareDisplay::NotifyVsync,
+                                                               nextVsync);
 
   RefPtr<mozilla::Runnable> addrefedTask = mCurrentVsyncTask;
   mVsyncThread->message_loop()->PostDelayedTask(
-      addrefedTask.forget(),
-      delay.ToMilliseconds());
+    addrefedTask.forget(),
+    delay.ToMilliseconds());
 }
 
 void
