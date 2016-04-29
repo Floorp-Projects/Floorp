69a6b685dd57104d830e10dfe8d165ec7c13c607
Mason Chang <mchang@mozilla.com>
2016-04-29 10:23:49 -0400

    Bug 1267180. Don't draw emojis as paths when they are too big. r=lsalzman

--- gfx/skia/skia/src/core/SkDraw.cpp
+++ gfx/skia/skia/src/core/SkDraw.cpp
@@ -1403,6 +1403,11 @@ bool SkDraw::ShouldDrawTextAsPaths(const SkPaint& paint, const SkMatrix& ctm) {
         return true;
     }
 
+    // Glyphs like Emojis can't be rendered as a path.
+    if (paint.getTypeface()->hasColorGlyphs()) {
+      return false;
+    }
+
     SkMatrix textM;
     return SkPaint::TooBigToUseCache(ctm, *paint.setTextMatrix(&textM));
 }
