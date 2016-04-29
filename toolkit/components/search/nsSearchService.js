c8b09e285ea32d95eef3be940e3c3ab4caaeeb9f
Florian Quèze <florian@queze.net>
2016-04-29 17:08:55 +0200

    Bug 1266783 - prevent the search cache file from being flushed twice in a row after a re-initialization, r=adw. a=ritu
    MozReview-Commit-ID: 4sftx4M1WSy

--- toolkit/components/search/nsSearchService.js
+++ toolkit/components/search/nsSearchService.js
@@ -2831,6 +2831,9 @@ SearchService.prototype = {
   },
 
   _buildCache: function SRCH_SVC__buildCache() {
+    if (this._batchTask)
+      this._batchTask.disarm();
+
     TelemetryStopwatch.start("SEARCH_SERVICE_BUILD_CACHE_MS");
     let cache = {};
     let locale = getLocale();
