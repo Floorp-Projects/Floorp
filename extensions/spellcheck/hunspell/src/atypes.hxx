16eb033c6f33f72c555f31a364b7eb814bafed99
Ryan VanderMeulen <ryanvm@gmail.com>
2016-05-02 16:58:03 -0400

    Bug 1257902 - Update Hunspell to version 1.4.0. r=ehsan

--- extensions/spellcheck/hunspell/src/atypes.hxx
+++ extensions/spellcheck/hunspell/src/atypes.hxx
@@ -1,3 +1,43 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Hunspell, based on MySpell.
+ *
+ * The Initial Developers of the Original Code are
+ * Kevin Hendricks (MySpell) and Németh László (Hunspell).
+ * Portions created by the Initial Developers are Copyright (C) 2002-2005
+ * the Initial Developers. All Rights Reserved.
+ *
+ * Contributor(s): David Einstein, Davide Prina, Giuseppe Modugno,
+ * Gianluca Turconi, Simon Brouwer, Noll János, Bíró Árpád,
+ * Goldman Eleonóra, Sarlós Tamás, Bencsáth Boldizsár, Halácsy Péter,
+ * Dvornik László, Gefferth András, Nagy Viktor, Varga Dániel, Chris Halls,
+ * Rene Engelhard, Bram Moolenaar, Dafydd Jones, Harri Pitkänen
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 #ifndef _ATYPES_HXX_
 #define _ATYPES_HXX_
 
@@ -6,8 +46,9 @@
 #ifdef HUNSPELL_WARNING_ON
 #define HUNSPELL_WARNING fprintf
 #else
-// empty inline function to switch off warnings (instead of the C99 standard variadic macros)
-static inline void HUNSPELL_WARNING(FILE *, const char *, ...) {}
+// empty inline function to switch off warnings (instead of the C99 standard
+// variadic macros)
+static inline void HUNSPELL_WARNING(FILE*, const char*, ...) {}
 #endif
 #endif
 
@@ -16,90 +57,87 @@ static inline void HUNSPELL_WARNING(FILE *, const char *, ...) {}
 
 #include "hashmgr.hxx"
 #include "w_char.hxx"
+#include <algorithm>
+#include <string>
 
-#define SETSIZE         256
-#define CONTSIZE        65536
-#define MAXWORDLEN      100
-#define MAXWORDUTF8LEN  256
+#define SETSIZE 256
+#define CONTSIZE 65536
 
 // affentry options
-#define aeXPRODUCT      (1 << 0)
-#define aeUTF8          (1 << 1)
-#define aeALIASF        (1 << 2)
-#define aeALIASM        (1 << 3)
-#define aeLONGCOND      (1 << 4)
+#define aeXPRODUCT (1 << 0)
+#define aeUTF8 (1 << 1)
+#define aeALIASF (1 << 2)
+#define aeALIASM (1 << 3)
+#define aeLONGCOND (1 << 4)
 
 // compound options
-#define IN_CPD_NOT   0
+#define IN_CPD_NOT 0
 #define IN_CPD_BEGIN 1
-#define IN_CPD_END   2
+#define IN_CPD_END 2
 #define IN_CPD_OTHER 3
 
 // info options
-#define  SPELL_COMPOUND  (1 << 0)
-#define  SPELL_FORBIDDEN (1 << 1)
-#define  SPELL_ALLCAP    (1 << 2)
-#define  SPELL_NOCAP     (1 << 3)
-#define  SPELL_INITCAP   (1 << 4)
-#define  SPELL_ORIGCAP   (1 << 5)
-#define  SPELL_WARN      (1 << 6)
+#define SPELL_COMPOUND (1 << 0)
+#define SPELL_FORBIDDEN (1 << 1)
+#define SPELL_ALLCAP (1 << 2)
+#define SPELL_NOCAP (1 << 3)
+#define SPELL_INITCAP (1 << 4)
+#define SPELL_ORIGCAP (1 << 5)
+#define SPELL_WARN (1 << 6)
 
-#define MAXLNLEN        8192
+#define MAXLNLEN 8192
 
-#define MINCPDLEN       3
-#define MAXCOMPOUND     10
-#define MAXCONDLEN      20
-#define MAXCONDLEN_1    (MAXCONDLEN - sizeof(char *))
+#define MINCPDLEN 3
+#define MAXCOMPOUND 10
+#define MAXCONDLEN 20
+#define MAXCONDLEN_1 (MAXCONDLEN - sizeof(char*))
 
-#define MAXACC          1000
+#define MAXACC 1000
 
 #define FLAG unsigned short
 #define FLAG_NULL 0x00
 #define FREE_FLAG(a) a = 0
 
-#define TESTAFF( a, b , c ) (flag_bsearch((unsigned short *) a, (unsigned short) b, c))
+#define TESTAFF(a, b, c) (std::binary_search(a, a + c, b))
 
-struct affentry
-{
-   char * strip;
-   char * appnd;
-   unsigned char stripl;
-   unsigned char appndl;
-   char  numconds;
-   char  opts;
-   unsigned short aflag;
-   unsigned short * contclass;
-   short        contclasslen;
-   union {
-     char conds[MAXCONDLEN];
-     struct {
-       char conds1[MAXCONDLEN_1];
-       char * conds2;
-     } l;
-   } c;
-   char *       morphcode;
+struct affentry {
+  std::string strip;
+  std::string appnd;
+  char numconds;
+  char opts;
+  unsigned short aflag;
+  unsigned short* contclass;
+  short contclasslen;
+  union {
+    char conds[MAXCONDLEN];
+    struct {
+      char conds1[MAXCONDLEN_1];
+      char* conds2;
+    } l;
+  } c;
+  char* morphcode;
 };
 
 struct guessword {
-  char * word;
+  char* word;
   bool allow;
-  char * orig;
+  char* orig;
 };
 
 struct mapentry {
-  char ** set;
+  char** set;
   int len;
 };
 
 struct flagentry {
-  FLAG * def;
+  FLAG* def;
   int len;
 };
 
 struct patentry {
-  char * pattern;
-  char * pattern2;
-  char * pattern3;
+  char* pattern;
+  char* pattern2;
+  char* pattern3;
   FLAG cond;
   FLAG cond2;
 };
--- extensions/spellcheck/hunspell/src/replist.cxx
+++ extensions/spellcheck/hunspell/src/replist.cxx
@@ -1,87 +1,193 @@
-#include "license.hunspell"
-#include "license.myspell"
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Hunspell, based on MySpell.
+ *
+ * The Initial Developers of the Original Code are
+ * Kevin Hendricks (MySpell) and Németh László (Hunspell).
+ * Portions created by the Initial Developers are Copyright (C) 2002-2005
+ * the Initial Developers. All Rights Reserved.
+ *
+ * Contributor(s): David Einstein, Davide Prina, Giuseppe Modugno,
+ * Gianluca Turconi, Simon Brouwer, Noll János, Bíró Árpád,
+ * Goldman Eleonóra, Sarlós Tamás, Bencsáth Boldizsár, Halácsy Péter,
+ * Dvornik László, Gefferth András, Nagy Viktor, Varga Dániel, Chris Halls,
+ * Rene Engelhard, Bram Moolenaar, Dafydd Jones, Harri Pitkänen
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+/*
+ * Copyright 2002 Kevin B. Hendricks, Stratford, Ontario, Canada
+ * And Contributors.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. All modifications to the source code must be clearly marked as
+ *    such.  Binary redistributions based on modified source code
+ *    must be clearly marked as modified versions in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY KEVIN B. HENDRICKS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * KEVIN B. HENDRICKS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <limits>
 
 #include "replist.hxx"
 #include "csutil.hxx"
 
 RepList::RepList(int n) {
-    dat = (replentry **) malloc(sizeof(replentry *) * n);
-    if (dat == 0) size = 0; else size = n;
-    pos = 0;
+  dat = (replentry**)malloc(sizeof(replentry*) * n);
+  if (dat == 0)
+    size = 0;
+  else
+    size = n;
+  pos = 0;
 }
 
-RepList::~RepList()
-{
-    for (int i = 0; i < pos; i++) {
-        free(dat[i]->pattern);
-        free(dat[i]->pattern2);
-        free(dat[i]);
-    }
-    free(dat);
+RepList::~RepList() {
+  for (int i = 0; i < pos; i++) {
+    free(dat[i]->pattern);
+    free(dat[i]->pattern2);
+    free(dat[i]);
+  }
+  free(dat);
 }
 
 int RepList::get_pos() {
-    return pos;
+  return pos;
 }
 
-replentry * RepList::item(int n) {
-    return dat[n];
+replentry* RepList::item(int n) {
+  return dat[n];
 }
 
-int RepList::near(const char * word) {
-    int p1 = 0;
-    int p2 = pos;
-    while ((p2 - p1) > 1) {
-      int m = (p1 + p2) / 2;
-      int c = strcmp(word, dat[m]->pattern);
-      if (c <= 0) {
-        if (c < 0) p2 = m; else p1 = p2 = m;
-      } else p1 = m;
+int RepList::near(const char* word) {
+  int p1 = 0;
+  int p2 = pos;
+  while ((p2 - p1) > 1) {
+    int m = (p1 + p2) / 2;
+    int c = strcmp(word, dat[m]->pattern);
+    if (c <= 0) {
+      if (c < 0)
+        p2 = m;
+      else
+        p1 = p2 = m;
+    } else
+      p1 = m;
+  }
+  return p1;
+}
+
+int RepList::match(const char* word, int n) {
+  if (strncmp(word, dat[n]->pattern, strlen(dat[n]->pattern)) == 0)
+    return strlen(dat[n]->pattern);
+  return 0;
+}
+
+int RepList::add(char* pat1, char* pat2) {
+  if (pos >= size || pat1 == NULL || pat2 == NULL)
+    return 1;
+  replentry* r = (replentry*)malloc(sizeof(replentry));
+  if (r == NULL)
+    return 1;
+  r->pattern = mystrrep(pat1, "_", " ");
+  r->pattern2 = mystrrep(pat2, "_", " ");
+  r->start = false;
+  r->end = false;
+  dat[pos++] = r;
+  for (int i = pos - 1; i > 0; i--) {
+    r = dat[i];
+    if (strcmp(r->pattern, dat[i - 1]->pattern) < 0) {
+      dat[i] = dat[i - 1];
+      dat[i - 1] = r;
+    } else
+      break;
+  }
+  return 0;
+}
+
+int RepList::conv(const char* word, char* dest, size_t destsize) {
+  size_t stl = 0;
+  int change = 0;
+  for (size_t i = 0; i < strlen(word); i++) {
+    int n = near(word + i);
+    int l = match(word + i, n);
+    if (l) {
+      size_t replen = strlen(dat[n]->pattern2);
+      if (stl + replen >= destsize)
+        return -1;
+      strcpy(dest + stl, dat[n]->pattern2);
+      stl += replen;
+      i += l - 1;
+      change = 1;
+    } else {
+      if (stl + 1 >= destsize)
+        return -1;
+      dest[stl++] = word[i];
     }
-    return p1;
+  }
+  dest[stl] = '\0';
+  return change;
 }
 
-int RepList::match(const char * word, int n) {
-    if (strncmp(word, dat[n]->pattern, strlen(dat[n]->pattern)) == 0) return strlen(dat[n]->pattern);
-    return 0;
-}
+bool RepList::conv(const char* word, std::string& dest) {
+  dest.clear();
 
-int RepList::add(char * pat1, char * pat2) {
-    if (pos >= size || pat1 == NULL || pat2 == NULL) return 1;
-    replentry * r = (replentry *) malloc(sizeof(replentry));
-    if (r == NULL) return 1;
-    r->pattern = mystrrep(pat1, "_", " ");
-    r->pattern2 = mystrrep(pat2, "_", " ");
-    r->start = false;
-    r->end = false;
-    dat[pos++] = r;
-    for (int i = pos - 1; i > 0; i--) {
-      r = dat[i];
-      if (strcmp(r->pattern, dat[i - 1]->pattern) < 0) {
-          dat[i] = dat[i - 1];
-          dat[i - 1] = r;
-      } else break;
+  bool change = false;
+  for (size_t i = 0; i < strlen(word); i++) {
+    int n = near(word + i);
+    int l = match(word + i, n);
+    if (l) {
+      dest.append(dat[n]->pattern2);
+      i += l - 1;
+      change = true;
+    } else {
+      dest.push_back(word[i]);
     }
-    return 0;
-}
-
-int RepList::conv(const char * word, char * dest) {
-    int stl = 0;
-    int change = 0;
-    for (size_t i = 0; i < strlen(word); i++) {
-        int n = near(word + i);
-        int l = match(word + i, n);
-        if (l) {
-          strcpy(dest + stl, dat[n]->pattern2);
-          stl += strlen(dat[n]->pattern2);
-          i += l - 1;
-          change = 1;
-        } else dest[stl++] = word[i];
-    }
-    dest[stl] = '\0';
-    return change;
+  }
+  return change;
 }
