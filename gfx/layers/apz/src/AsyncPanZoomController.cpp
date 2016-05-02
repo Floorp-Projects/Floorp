975b7c9493abde42c1607839f48d9a27ce7f3146
Botond Ballo <botond@mozilla.com>
2016-05-02 13:29:33 -0400

    Bug 1219296 - Remove StartSmoothScroll()'s argument, which is no longer used. r=kats
    MozReview-Commit-ID: DlnaEQLCvlf
    
    --HG--
    extra : rebase_source : 95cc626bb6260fc11efa71db4e1e66a01d770508

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -708,7 +708,6 @@ private:
 class SmoothScrollAnimation : public AsyncPanZoomAnimation {
 public:
   SmoothScrollAnimation(AsyncPanZoomController& aApzc,
-                        ScrollSource aSource,
                         const nsPoint &aInitialPosition,
                         const nsPoint &aInitialVelocity,
                         const nsPoint& aDestination, double aSpringConstant,
@@ -2561,7 +2560,7 @@ void AsyncPanZoomController::HandleSmoothScrollOverscroll(const ParentLayerPoint
   HandleFlingOverscroll(aVelocity, BuildOverscrollHandoffChain(), nullptr);
 }
 
-void AsyncPanZoomController::StartSmoothScroll(ScrollSource aSource) {
+void AsyncPanZoomController::StartSmoothScroll() {
   SetState(SMOOTH_SCROLL);
   nsPoint initialPosition = CSSPoint::ToAppUnits(mFrameMetrics.GetScrollOffset());
   // Cast velocity from ParentLayerPoints/ms to CSSPoints/ms then convert to
@@ -2571,7 +2570,6 @@ void AsyncPanZoomController::StartSmoothScroll(ScrollSource aSource) {
   nsPoint destination = CSSPoint::ToAppUnits(mFrameMetrics.GetSmoothScrollOffset());
 
   StartAnimation(new SmoothScrollAnimation(*this,
-                                           aSource,
                                            initialPosition, initialVelocity,
                                            destination,
                                            gfxPrefs::ScrollBehaviorSpringConstant(),
@@ -3454,7 +3452,7 @@ void AsyncPanZoomController::NotifyLayersUpdated(const ScrollMetadata& aScrollMe
         CSSPoint::ToAppUnits(aLayerMetrics.GetSmoothScrollOffset()));
     } else {
       CancelAnimation();
-      StartSmoothScroll(ScrollSource::DOM);
+      StartSmoothScroll();
     }
   }
 
