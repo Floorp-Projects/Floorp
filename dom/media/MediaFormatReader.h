b27131e3c229e46d208b93544247d98250cbefef
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-05-02 10:53:31 +1000

    Bug 1269178: P2. Ensure that no skip to next keyframe logic is activated while there's a pending seek. r=gerald
    While a seek is pending, the demuxer has been reset meaning that ShouldSkip() would almost always incorrectly return true and would corrupt the seeking state.
    
    MozReview-Commit-ID: 6R912Fu6Nul

--- dom/media/MediaFormatReader.h
+++ dom/media/MediaFormatReader.h
@@ -330,6 +330,7 @@ private:
     {
       // Clear demuxer related data.
       mDemuxRequest.DisconnectIfExists();
+      mSeekRequest.DisconnectIfExists();
       mTrackDemuxer->Reset();
     }
 
@@ -354,6 +355,11 @@ private:
       mNumSamplesOutput = 0;
       mSizeOfQueue = 0;
       mNextStreamSourceID.reset();
+    }
+
+    bool HasInternalSeekPending() const
+    {
+      return mTimeThreshold && !mTimeThreshold.ref().mHasSeeked;
     }
 
     // Used by the MDSM for logging purposes.
