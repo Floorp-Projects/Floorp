4ab42252f43c7f7b2b267704fbfb8f47acf1a3f8
Ehsan Akhgari <ehsan@mozilla.com>
2016-05-02 15:43:36 -0400

    Bug 1261011 - Remove Inter-App Communication API; r=fabrice

--- dom/apps/tests/iac/makezips.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm publisher/publisher.zip
-rm subscriber/subscriber.zip
-cd publisher
-zip publisher.zip `cat publisher.list`
-cd ../subscriber
-zip subscriber.zip `cat subscriber.list`
-cd ..
