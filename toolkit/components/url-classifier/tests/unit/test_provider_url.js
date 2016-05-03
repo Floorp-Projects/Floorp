ccde104ea45f0d3e50485a8ce61b81057434b7f4
Yoshi Huang <allstars.chh@mozilla.com>
2016-05-03 17:29:21 +0800

    Bug 1077874 - Only expose Major version of Firefox in SafeBrowsing. r=francois

---
+++ toolkit/components/url-classifier/tests/unit/test_provider_url.js
@@ -0,0 +1,34 @@
+Cu.import("resource://testing-common/AppInfo.jsm", this);
+Cu.import("resource://gre/modules/Services.jsm");
+
+function updateVersion(version) {
+  updateAppInfo({ version });
+}
+
+add_test(function test_provider_url() {
+  let urls = [
+    "browser.safebrowsing.provider.google.updateURL",
+    "browser.safebrowsing.provider.google.gethashURL",
+    "browser.safebrowsing.provider.mozilla.updateURL",
+    "browser.safebrowsing.provider.mozilla.gethashURL"
+  ];
+
+  let versions = [
+    "49.0",
+    "49.0.1",
+    "49.0a1",
+    "49.0b1",
+    "49.0esr",
+    "49.0.1esr"
+  ];
+
+  for (let version of versions) {
+    for (let url of urls) {
+      updateVersion(version);
+      let value = Services.urlFormatter.formatURLPref(url);
+      Assert.notEqual(value.indexOf("&appver=49.0&"), -1);
+    }
+  }
+
+  run_next_test();
+});
