15b89607c6736d65b70df49d4844c533bfc56afb
Tom Tung <ttung@mozilla.com>
2016-05-03 09:48:40 +0800

    Bug 1243791 - P1 implement response.redirected - Fetch API & Cache API & ServiceWorker changed. r=bkelly, r=baku.

--- dom/fetch/Request.cpp
+++ dom/fetch/Request.cpp
@@ -285,18 +285,8 @@ Request::Constructor(const GlobalObject& aGlobal,
     request = inputReq->GetInternalRequest();
 
   } else {
-    request = new InternalRequest();
-  }
-
-  request = request->GetRequestConstructorCopy(global, aRv);
-  if (NS_WARN_IF(aRv.Failed())) {
-    return nullptr;
-  }
-
-  RequestMode fallbackMode = RequestMode::EndGuard_;
-  RequestCredentials fallbackCredentials = RequestCredentials::EndGuard_;
-  RequestCache fallbackCache = RequestCache::EndGuard_;
-  if (aInput.IsUSVString()) {
+    // aInput is USVString.
+    // We need to get url before we create a InternalRequest.
     nsAutoString input;
     input.Assign(aInput.GetAsUSVString());
 
@@ -317,7 +307,18 @@ Request::Constructor(const GlobalObject& aGlobal,
       return nullptr;
     }
 
-    request->SetURL(NS_ConvertUTF16toUTF8(requestURL));
+    request = new InternalRequest(NS_ConvertUTF16toUTF8(requestURL));
+  }
+
+  request = request->GetRequestConstructorCopy(global, aRv);
+  if (NS_WARN_IF(aRv.Failed())) {
+    return nullptr;
+  }
+
+  RequestMode fallbackMode = RequestMode::EndGuard_;
+  RequestCredentials fallbackCredentials = RequestCredentials::EndGuard_;
+  RequestCache fallbackCache = RequestCache::EndGuard_;
+  if (aInput.IsUSVString()) {
     fallbackMode = RequestMode::Cors;
     fallbackCredentials = RequestCredentials::Omit;
     fallbackCache = RequestCache::Default;
