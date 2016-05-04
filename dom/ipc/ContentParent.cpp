0f3eef2ae3879b37b79d3545392a6081d14d25c6
Kyle Huey <khuey@kylehuey.com>
2016-05-03 22:37:54 -0700

    Bug 1268341: Suppress gdk deprecation warning in ContentParent. r=karlt

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -1060,7 +1060,10 @@ ContentParent::RecvUngrabPointer(const uint32_t& aTime)
   NS_RUNTIMEABORT("This message only makes sense on GTK platforms");
   return false;
 #else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
   gdk_pointer_ungrab(aTime);
+#pragma GCC diagnostic pop
   return true;
 #endif
 }
