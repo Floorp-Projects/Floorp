85576afa6e500ed633e3e1d81fc4e21d5ceae282
David Anderson <danderson@mozilla.com>
2016-05-03 17:37:34 -0700

    Restrict SetDispAcquireFence's nsIWidget access to Gonk. (bug 1269653 part 2, r=kats)

--- gfx/layers/composite/LayerManagerComposite.cpp
+++ gfx/layers/composite/LayerManagerComposite.cpp
@@ -945,7 +945,7 @@ LayerManagerComposite::Render(const nsIntRegion& aInvalidRegion, const nsIntRegi
     mCompositor->EndFrame();
 
     // Call after EndFrame()
-    mCompositor->SetDispAcquireFence(mRoot, mCompositor->GetWidget()->RealWidget());
+    mCompositor->SetDispAcquireFence(mRoot);
   }
 
   if (composer2D) {
