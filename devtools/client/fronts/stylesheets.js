53f074e7962f39a74f6b114e222ec425c46121f1
Eddy Bruel <ejpbruel@mozilla.com>
2016-05-04 16:46:41 +0200

    Bug 1268461 - Decouple MediaRuleFront from MediaRuleActor;r=jryans

--- devtools/client/fronts/stylesheets.js
+++ devtools/client/fronts/stylesheets.js
@@ -4,7 +4,11 @@
 "use strict";
 
 const { Front, FrontClassWithSpec } = require("devtools/server/protocol.js");
-const { originalSourceSpec } = require("devtools/shared/specs/stylesheets.js");
+const {
+  originalSourceSpec,
+  mediaRuleSpec
+} = require("devtools/shared/specs/stylesheets.js");
+const events = require("sdk/event/core.js");
 
 /**
  * The client-side counterpart for an OriginalSourceActor.
@@ -34,3 +38,49 @@ const OriginalSourceFront = FrontClassWithSpec(originalSourceSpec, {
 });
 
 exports.OriginalSourceFront = OriginalSourceFront;
+
+/**
+ * Corresponding client-side front for a MediaRuleActor.
+ */
+const MediaRuleFront = FrontClassWithSpec(mediaRuleSpec, {
+  initialize: function (client, form) {
+    Front.prototype.initialize.call(this, client, form);
+
+    this._onMatchesChange = this._onMatchesChange.bind(this);
+    events.on(this, "matches-change", this._onMatchesChange);
+  },
+
+  _onMatchesChange: function (matches) {
+    this._form.matches = matches;
+  },
+
+  form: function (form, detail) {
+    if (detail === "actorid") {
+      this.actorID = form;
+      return;
+    }
+    this.actorID = form.actor;
+    this._form = form;
+  },
+
+  get mediaText() {
+    return this._form.mediaText;
+  },
+  get conditionText() {
+    return this._form.conditionText;
+  },
+  get matches() {
+    return this._form.matches;
+  },
+  get line() {
+    return this._form.line || -1;
+  },
+  get column() {
+    return this._form.column || -1;
+  },
+  get parentStyleSheet() {
+    return this.conn.getActor(this._form.parentStyleSheet);
+  }
+});
+
+exports.MediaRuleFront = MediaRuleFront;
