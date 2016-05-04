27964600fcc1423528c47a5efeb996d8711453f7
David Anderson <danderson@mozilla.com>
2016-05-03 17:40:19 -0700

    Remove unused null widget checks. (bug 1269037 part 2, r=mchang)

--- widget/CompositorWidgetProxy.cpp
+++ widget/CompositorWidgetProxy.cpp
@@ -70,18 +70,12 @@ CompositorWidgetProxyWrapper::CompositorWidgetProxyWrapper(nsBaseWidget* aWidget
 bool
 CompositorWidgetProxyWrapper::PreRender(layers::LayerManagerComposite* aManager)
 {
-  if (!mWidget) {
-    return false;
-  }
   return mWidget->PreRender(aManager);
 }
 
 void
 CompositorWidgetProxyWrapper::PostRender(layers::LayerManagerComposite* aManager)
 {
-  if (!mWidget) {
-    return;
-  }
   mWidget->PostRender(aManager);
 }
 
@@ -89,9 +83,6 @@ void
 CompositorWidgetProxyWrapper::DrawWindowUnderlay(layers::LayerManagerComposite* aManager,
                                                  LayoutDeviceIntRect aRect)
 {
-  if (!mWidget) {
-    return;
-  }
   mWidget->DrawWindowUnderlay(aManager, aRect);
 }
 
@@ -99,18 +90,12 @@ void
 CompositorWidgetProxyWrapper::DrawWindowOverlay(layers::LayerManagerComposite* aManager,
                                                 LayoutDeviceIntRect aRect)
 {
-  if (!mWidget) {
-    return;
-  }
   mWidget->DrawWindowOverlay(aManager, aRect);
 }
 
 already_AddRefed<gfx::DrawTarget>
 CompositorWidgetProxyWrapper::StartRemoteDrawing()
 {
-  if (!mWidget) {
-    return nullptr;
-  }
   return mWidget->StartRemoteDrawing();
 }
 
@@ -118,18 +103,12 @@ already_AddRefed<gfx::DrawTarget>
 CompositorWidgetProxyWrapper::StartRemoteDrawingInRegion(LayoutDeviceIntRegion& aInvalidRegion,
                                                          layers::BufferMode* aBufferMode)
 {
-  if (!mWidget) {
-    return nullptr;
-  }
   return mWidget->StartRemoteDrawingInRegion(aInvalidRegion, aBufferMode);
 }
 
 void
 CompositorWidgetProxyWrapper::EndRemoteDrawing()
 {
-  if (!mWidget) {
-    return;
-  }
   mWidget->EndRemoteDrawing();
 }
 
@@ -137,63 +116,42 @@ void
 CompositorWidgetProxyWrapper::EndRemoteDrawingInRegion(gfx::DrawTarget* aDrawTarget,
                                                        LayoutDeviceIntRegion& aInvalidRegion)
 {
-  if (!mWidget) {
-    return;
-  }
   mWidget->EndRemoteDrawingInRegion(aDrawTarget, aInvalidRegion);
 }
 
 void
 CompositorWidgetProxyWrapper::CleanupRemoteDrawing()
 {
-  if (mWidget) {
-    mWidget->CleanupRemoteDrawing();
-  }
-  CompositorWidgetProxy::CleanupRemoteDrawing();
+  mWidget->CleanupRemoteDrawing();
 }
 
 void
 CompositorWidgetProxyWrapper::CleanupWindowEffects()
 {
-  if (!mWidget) {
-    return;
-  }
   mWidget->CleanupWindowEffects();
 }
 
 bool
 CompositorWidgetProxyWrapper::InitCompositor(layers::Compositor* aCompositor)
 {
-  if (!mWidget) {
-    return false;
-  }
   return mWidget->InitCompositor(aCompositor);
 }
 
 LayoutDeviceIntSize
 CompositorWidgetProxyWrapper::GetClientSize()
 {
-  if (!mWidget) {
-    return LayoutDeviceIntSize();
-  }
   return mWidget->GetClientSize();
 }
 
 uint32_t
 CompositorWidgetProxyWrapper::GetGLFrameBufferFormat()
 {
-  if (!mWidget) {
-    return CompositorWidgetProxy::GetGLFrameBufferFormat();
-  }
   return mWidget->GetGLFrameBufferFormat();
 }
 
 layers::Composer2D*
 CompositorWidgetProxyWrapper::GetComposer2D()
 {
-  if (!mWidget) {
-    return nullptr;
-  }
   return mWidget->GetComposer2D();
 }
 
@@ -206,9 +164,6 @@ CompositorWidgetProxyWrapper::RealWidget()
 already_AddRefed<CompositorVsyncDispatcher>
 CompositorWidgetProxyWrapper::GetCompositorVsyncDispatcher()
 {
-  if (!mWidget) {
-    return nullptr;
-  }
   RefPtr<CompositorVsyncDispatcher> cvd = mWidget->GetCompositorVsyncDispatcher();
   return cvd.forget();
 }
