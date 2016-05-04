d0634d3b7d55338e2cad3224faa97c7d4cd29bc2
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-05-04 13:27:56 +1000

    Backed out changeset d14dc334734adccc6741093c388e13c7099a8a38 (Bug 1268868: [MSE] P1. Re-enable gap detection within a media segment. r=gerald)
    MozReview-Commit-ID: IxFqoMce57F

--- dom/media/mediasource/TrackBuffersManager.cpp
+++ dom/media/mediasource/TrackBuffersManager.cpp
@@ -1397,6 +1397,11 @@ TrackBuffersManager::ProcessFrames(TrackBuffer& aSamples, TrackData& aTrackData)
   TrackBuffer samples; // array that will contain the frames to be added
                        // to our track buffer.
 
+  // We assume that no frames are contiguous within a media segment and as such
+  // don't need to check for discontinuity except for the first frame and should
+  // a frame be ignored due to the target window.
+  bool needDiscontinuityCheck = true;
+
   if (aSamples.Length()) {
     aTrackData.mLastParsedEndTime = TimeUnit();
   }
@@ -1461,7 +1466,7 @@ TrackBuffersManager::ProcessFrames(TrackBuffer& aSamples, TrackData& aTrackData)
     // OR
     // If last decode timestamp for track buffer is set and the difference between decode timestamp and last decode timestamp is greater than 2 times last frame duration:
 
-    if (trackBuffer.mLastDecodeTimestamp.isSome() &&
+    if (needDiscontinuityCheck && trackBuffer.mLastDecodeTimestamp.isSome() &&
         (decodeTimestamp < trackBuffer.mLastDecodeTimestamp.ref() ||
          (decodeTimestamp - trackBuffer.mLastDecodeTimestamp.ref()
           > 2 * trackBuffer.mLongestFrameDuration))) {
@@ -1512,6 +1517,7 @@ TrackBuffersManager::ProcessFrames(TrackBuffer& aSamples, TrackData& aTrackData)
             : timestampOffset + sampleTimecode;
       }
       trackBuffer.mNeedRandomAccessPoint = false;
+      needDiscontinuityCheck = false;
     }
 
     // 7. Let frame end timestamp equal the sum of presentation timestamp and frame duration.
@@ -1530,6 +1536,7 @@ TrackBuffersManager::ProcessFrames(TrackBuffer& aSamples, TrackData& aTrackData)
         sizeNewSamples = 0;
       }
       trackBuffer.mNeedRandomAccessPoint = true;
+      needDiscontinuityCheck = true;
       continue;
     }
 
