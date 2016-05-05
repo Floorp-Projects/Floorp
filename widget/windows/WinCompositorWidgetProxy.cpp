927db0d41ae0f312f0bf852b1e00ac3ada8140cf
David Anderson <danderson@mozilla.com>
2016-05-04 22:00:13 -0700

    Add WinCompositorWidgetProxy. (bug 1265975 part 1, r=jimm)

---
+++ widget/windows/WinCompositorWidgetProxy.cpp
@@ -0,0 +1,65 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "WinCompositorWidgetProxy.h"
+#include "nsWindow.h"
+#include "VsyncDispatcher.h"
+
+namespace mozilla {
+namespace widget {
+
+WinCompositorWidgetProxy::WinCompositorWidgetProxy(nsWindow* aWindow)
+ : mWindow(aWindow)
+{
+  MOZ_ASSERT(aWindow);
+  MOZ_ASSERT(!aWindow->Destroyed());
+}
+
+bool
+WinCompositorWidgetProxy::PreRender(layers::LayerManagerComposite* aManager)
+{
+  return mWindow->PreRender(aManager);
+}
+
+void
+WinCompositorWidgetProxy::PostRender(layers::LayerManagerComposite* aManager)
+{
+  mWindow->PostRender(aManager);
+}
+
+void
+WinCompositorWidgetProxy::EndRemoteDrawing()
+{
+  mWindow->EndRemoteDrawing();
+}
+
+nsIWidget*
+WinCompositorWidgetProxy::RealWidget()
+{
+  return mWindow;
+}
+
+LayoutDeviceIntSize
+WinCompositorWidgetProxy::GetClientSize()
+{
+  return mWindow->GetClientSize();
+}
+
+already_AddRefed<gfx::DrawTarget>
+WinCompositorWidgetProxy::StartRemoteDrawing()
+{
+  return mWindow->StartRemoteDrawing();
+}
+
+already_AddRefed<CompositorVsyncDispatcher>
+WinCompositorWidgetProxy::GetCompositorVsyncDispatcher()
+{
+  RefPtr<CompositorVsyncDispatcher> cvd = mWindow->GetCompositorVsyncDispatcher();
+  return cvd.forget();
+}
+
+
+} // namespace widget
+} // namespace mozilla
