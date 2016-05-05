0c3f8a3e7d82ecde5915b2b8636113df19a24ac7
Botond Ballo <botond@mozilla.com>
2016-05-04 20:07:42 -0400

    Bug 1267438 - Remove the (no longer used) isClipFixed=false annotation. r=mstange
    MozReview-Commit-ID: LicG7zFHnX3
    
    --HG--
    extra : rebase_source : 1b39fcb34403dfa92f19100b3a59c3f550d52ff1
    extra : source : 2bf1247f574319f27f570bb372a71d6a826ab1fb
    extra : histedit_source : cc75a6e848225552adb9b3148ca4e2bb87b94f80

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -1931,8 +1931,7 @@ nsLayoutUtils::SetFixedPositionLayerData(Layer* aLayer,
                                          const nsRect& aAnchorRect,
                                          const nsIFrame* aFixedPosFrame,
                                          nsPresContext* aPresContext,
-                                         const ContainerLayerParameters& aContainerParameters,
-                                         bool aIsClipFixed) {
+                                         const ContainerLayerParameters& aContainerParameters) {
   // Find out the rect of the viewport frame relative to the reference frame.
   // This, in conjunction with the container scale, will correspond to the
   // coordinate-space of the built layer.
@@ -1991,7 +1990,7 @@ nsLayoutUtils::SetFixedPositionLayerData(Layer* aLayer,
       id = FindOrCreateIDFor(content);
     }
   }
-  aLayer->SetFixedPositionData(id, anchor, sides, aIsClipFixed);
+  aLayer->SetFixedPositionData(id, anchor, sides);
 }
 
 bool
