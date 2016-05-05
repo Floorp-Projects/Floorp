025b67248c166b3fce90ff2df91d573af0c80e53
Boris Zbarsky <bzbarsky@mit.edu>
2016-05-04 23:41:24 -0400

    Bug 1257849 part 2.  Pass in the right set of supported tokens to the sandbox tokenlist implementation.  r=bkelly

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -3094,7 +3094,8 @@ Element::HTMLSVGPropertiesToTraverseAndUnlink()
 }
 
 nsDOMTokenList*
-Element::GetTokenList(nsIAtom* aAtom)
+Element::GetTokenList(nsIAtom* aAtom,
+                      const DOMTokenListSupportedTokenArray aSupportedTokens)
 {
 #ifdef DEBUG
   nsIAtom*** props =
@@ -3114,7 +3115,7 @@ Element::GetTokenList(nsIAtom* aAtom)
     list = static_cast<nsDOMTokenList*>(GetProperty(aAtom));
   }
   if (!list) {
-    list = new nsDOMTokenList(this, aAtom);
+    list = new nsDOMTokenList(this, aAtom, aSupportedTokens);
     NS_ADDREF(list);
     SetProperty(aAtom, list, nsDOMTokenListPropertyDestructor);
   }
