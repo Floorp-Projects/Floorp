55917eeb3b97a7885dca14feaeb94fa83a3d130f
Mihai Tabara <mtabara@mozilla.com>
2016-05-05 13:11:06 -0400

    Bug 1214487 - Implement uptake monitoring on slaves/workers r=rail a=release DONTBUILD
    MozReview-Commit-ID: 7VPMJo6yA56

--- testing/mozharness/configs/releases/bouncer_thunderbird.py
+++ testing/mozharness/configs/releases/bouncer_thunderbird.py
@@ -4,6 +4,7 @@ config = {
     "products": {
         "installer": {
             "product-name": "Thunderbird-%(version)s",
+            "check_uptake": True,
             "alias": "thunderbird-latest",
             "ssl-only": False,
             "add-locales": True,
@@ -44,6 +45,7 @@ config = {
         },
         "installer-ssl": {
             "product-name": "Thunderbird-%(version)s-SSL",
+            "check_uptake": True,
             "ssl-only": True,
             "add-locales": True,
             "paths": {
@@ -83,6 +85,7 @@ config = {
         },
         "complete-mar": {
             "product-name": "Thunderbird-%(version)s-Complete",
+            "check_uptake": True,
             "ssl-only": False,
             "add-locales": True,
             "paths": {
@@ -124,6 +127,7 @@ config = {
     "partials": {
         "releases-dir": {
             "product-name": "Thunderbird-%(version)s-Partial-%(prev_version)s",
+            "check_uptake": True,
             "ssl-only": False,
             "add-locales": True,
             "paths": {
