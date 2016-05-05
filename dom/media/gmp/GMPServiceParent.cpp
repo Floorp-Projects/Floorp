faee3cba4f3e8bd6b743555de3685fc656a5a3ea
Chris Pearce <cpearce@mozilla.com>
2016-05-05 22:35:44 +1200

    Bug 1268984 - Prefer to re-use a GMPParent with the requested nodeId rather than clone. r=jesup
    If you request a GMPParent with a nodeId, you should get any already running
    instances with the same nodeId in preference to cloning an existing GMP and
    assigning it the nodeId.
    
    This is ensures that EME GMP actors that are same-origin run in the same GMP
    instance.
    
    The GMP gtests are failing because of the cross-origin checks in
    GeckoMediaPluginServiceParent::SelectPluginForAPI(). The loop there selects the
    first GMPParent that can be used from the nodeId passed in. We previously
    assumed a GMPParent can be used from a nodeId if the GMPParent has the same
    nodeId, or if it has not loaded its process and it has no nodeId. The problem
    with assuming that, is if an in-use GMPParent with the target nodeId lies in
    the GeckoMediaPluginServiceParent::mPlugins list after a GMPParent with no
    nodeId, we'll end up using the first GMPParent (the one with no nodeId) rather
    than the one with the target nodeId.
    
    The solution is to change GeckoMediaPluginServiceParent::SelectPluginForAPI()
    so that effectively if we have a target nodeId, we'll select the first
    GMPParent that has the same nodeId, or we'll clone the first which supported
    all the requested capabilities/tags.
    
    This means when you request a GMPParent with a given nodeId, you'll get the one
    with the same nodeId (origin) by preference.
    
    MozReview-Commit-ID: 4yVnrO8B1Pg
    
    --HG--
    extra : rebase_source : 5db7c6ef5f360df7d2244ca828b68b340c4e9a41

--- dom/media/gmp/GMPServiceParent.cpp
+++ dom/media/gmp/GMPServiceParent.cpp
@@ -990,8 +990,6 @@ GeckoMediaPluginServiceParent::SelectPluginForAPI(const nsACString& aNodeId,
           return gmp.forget();
         }
       } else if (gmp->CanBeUsedFrom(aNodeId)) {
-        MOZ_ASSERT(!aNodeId.IsEmpty());
-        gmp->SetNodeId(aNodeId);
         return gmp.forget();
       }
 
