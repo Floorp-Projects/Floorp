86a6547a9798dafa75ae018e80be362180851c6a
JW Wang <jwwang@mozilla.com>
2016-05-05 11:54:44 +0800

    Bug 1270350 - per comment 0, use SyncRunnable to repalce the boilerplate code. r=jya. a=sylvestre
    MozReview-Commit-ID: 9hqmUnNC3Vm

--- dom/media/platforms/apple/AppleVTDecoder.cpp
+++ dom/media/platforms/apple/AppleVTDecoder.cpp
@@ -122,9 +122,6 @@ AppleVTDecoder::ProcessFlush()
         "with error:%d.", rv);
   }
   ClearReorderedFrames();
-  MonitorAutoLock mon(mMonitor);
-  mIsFlushing = false;
-  mon.NotifyAll();
 }
 
 void
