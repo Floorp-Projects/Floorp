45985f5409c94e55ab9853508fb489d38eb1564d
Chris Pearce <cpearce@mozilla.com>
2016-05-04 20:32:00 -0400

    Bug 1266286 - Ensure crash reports work for GMP used by EME code. r=mconley
    Crash reporting for GMPs being used from the EME call site are not generating
    crash reports because they depend on the MediaKeys object calling
    GMPService::AddPluginCrashHandler() to associate a window to which the
    PluginCrashedEvent is fired. This doesn't work with e10s enabled because the
    GMPParent which causes the plugin crash handlers to run is in the chrome
    process, but the MediaKeys which adds the handler is in the child process. So
    the crash handler is on the GMPServiceChild, but we only run the crash handlers
    that were added to the GMPServiceParent in the chrome/parent process.
    
    The solution is to broadcast a message from the chrome process to all the
    content processes when a GMP has crashed that causes the GMPServiceChild to
    also run its crash handlers.
    
    MozReview-Commit-ID: 8Lek16G9ZGb

--- toolkit/content/process-content.js
+++ toolkit/content/process-content.js
@@ -10,3 +10,11 @@ var { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 // and for those that match URLs provided by the parent process will set up
 // a dedicated message port and notify the parent process.
 Cu.import("resource://gre/modules/RemotePageManager.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+Services.cpmm.addMessageListener("gmp-plugin-crash", msg => {
+  let gmpservice = Cc["@mozilla.org/gecko-media-plugin-service;1"]
+                     .getService(Ci.mozIGeckoMediaPluginService);
+
+  gmpservice.RunPluginCrashCallbacks(msg.data.pluginID, msg.data.pluginName);
+});
