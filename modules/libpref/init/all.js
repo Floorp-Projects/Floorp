12200e7da5a1cb12d65b50857dd11d54aa7a7f41
Chris Pearce <cpearce@mozilla.com>
2016-05-07 09:19:15 +1200

    Bug 1270968 - Add mechanism to clear GMP storage when its version changes. r=gerald,a=ritu,ba=cpearce
    In bug 1264497 we discovered that Netflix was broken because we'd made a change
    to not send the Adobe GMP the device bound nodeId, which it stored in GMP
    storage. When the GMP was comparing the nodeId it had stored against what it was
    passed (nothing) the comparison was failing. Users could have worked around this
    problem by clearing their GMP storage, whereupon the Adobe GMP will have stored
    "nothing" as its nodeId.
    
    When bug 1264497 was fixed, users who'd cleared their storage will see Netflix
    fail again, as their stored nodeId of "nothing" will again not match what we
    pass in. So to fix Netflix for these users, we need to clear GMP storage.
    
    This is another instance of a more general problem that we have occasionally
    encountered, namely that sometimes GMP storage becomes incompatible, and we
    need to clear it. Having a general mechanism that we can use to clear storage
    remotely will be helpful, so this patch adds one, and triggers it to fire.
    
    This mechanism is pref controlled, so that we can issue a hotfix if necessary
    to clear GMP storage.
    
    
    MozReview-Commit-ID: GzSyBj0P2JG

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -357,6 +357,13 @@ pref("media.mp3.enabled", true);
 pref("media.apple.mp3.enabled", true);
 pref("media.apple.mp4.enabled", true);
 #endif
+// GMP storage version number. At startup we check the version against
+// media.gmp.storage.version.observed, and if the versions don't match,
+// we clear storage and set media.gmp.storage.version.observed=expected.
+// This provides a mechanism to clear GMP storage when non-compatible
+// changes are made.
+pref("media.gmp.storage.version.expected", 1);
+
 #ifdef MOZ_WEBRTC
 pref("media.navigator.enabled", true);
 pref("media.navigator.video.enabled", true);
