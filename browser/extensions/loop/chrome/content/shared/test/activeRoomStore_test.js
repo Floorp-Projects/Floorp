040f250c226532b153a767ae5c0ad97548e6a616
Thom Chiovoloni <tchiovoloni@mozilla.com>
2016-05-06 12:23:42 -0400

    Bug 1238128 - Ensure that the details passed to WebChannelMessageToChrome is a string, with a whitelist for messages from existing users r=Margaret,markh,MattN
    MozReview-Commit-ID: DpdJ5bUcBdQ

--- browser/extensions/loop/chrome/content/shared/test/activeRoomStore_test.js
+++ browser/extensions/loop/chrome/content/shared/test/activeRoomStore_test.js
@@ -928,11 +928,11 @@ describe("loop.store.ActiveRoomStore", function () {
         sinon.assert.calledOnce(window.dispatchEvent);
         sinon.assert.calledWithExactly(window.dispatchEvent, new window.CustomEvent(
         "WebChannelMessageToChrome", { 
-          detail: { 
+          detail: JSON.stringify({ 
             id: "loop-link-clicker", 
             message: { 
               command: "openRoom", 
-              roomToken: "fakeToken" } } }));});
+              roomToken: "fakeToken" } }) }));});
 
 
 
