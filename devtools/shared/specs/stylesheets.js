197654cbbc54bb8ede266f6265a28e1e9dc89ade
J. Ryan Stinnett <jryans@gmail.com>
2016-05-06 21:42:34 -0500

    Bug 1270619 - Remove .js from module IDs. r=pbro
    MozReview-Commit-ID: x8zcFH2VjO

--- devtools/shared/specs/stylesheets.js
+++ devtools/shared/specs/stylesheets.js
@@ -8,7 +8,7 @@ const {
   RetVal,
   generateActorSpec,
   types
-} = require("devtools/shared/protocol.js");
+} = require("devtools/shared/protocol");
 
 const originalSourceSpec = generateActorSpec({
   typeName: "originalsource",
