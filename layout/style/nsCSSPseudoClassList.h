76ddb77f53aa26af51665b5cc382f97a2cce127d
Xidorn Quan <quanxunzhen@gmail.com>
2016-05-09 09:42:52 +1000

    Bug 1268749 part 3 - Hide :fullscreen pseudo-class from content when unprefixed API is disabled. r=heycam a=lizzard
    MozReview-Commit-ID: 78hlOGCDKn0

--- layout/style/nsCSSPseudoClassList.h
+++ layout/style/nsCSSPseudoClassList.h
@@ -161,7 +161,10 @@ CSS_STATE_PSEUDO_CLASS(mozDevtoolsHighlighted, ":-moz-devtools-highlighted", 0, 
 
 // Matches the element which is being displayed full-screen, and
 // any containing frames.
-CSS_STATE_PSEUDO_CLASS(fullscreen, ":fullscreen", 0, "", NS_EVENT_STATE_FULL_SCREEN)
+CSS_STATE_PSEUDO_CLASS(fullscreen, ":fullscreen",
+                       CSS_PSEUDO_CLASS_ENABLED_IN_UA_SHEETS_AND_CHROME,
+                       "full-screen-api.unprefix.enabled",
+                       NS_EVENT_STATE_FULL_SCREEN)
 CSS_STATE_PSEUDO_CLASS(mozFullScreen, ":-moz-full-screen", 0, "", NS_EVENT_STATE_FULL_SCREEN)
 
 // Matches any element which is an ancestor of the DOM full-screen element,
