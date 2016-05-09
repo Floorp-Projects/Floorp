4f33f19dca551967651a013953144a3133cb30e2
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-05-09 14:35:54 +0100

    Bug 1271049 - Fix showing alternative styles from <style> elements and make the test actually test things. r=mconley, a=sledru
    MozReview-Commit-ID: BD7eQl5iNaJ

--- browser/base/content/test/general/browser_page_style_menu.js
+++ browser/base/content/test/general/browser_page_style_menu.js
@@ -1,19 +1,41 @@
-function test() {
-  waitForExplicitFinish();
+"use strict";
 
-  var tab = gBrowser.addTab();
-  gBrowser.selectedTab = tab;
-  tab.linkedBrowser.addEventListener("pageshow", function () {
-    tab.linkedBrowser.removeEventListener("pageshow", arguments.callee, true);
-    checkPageStyleMenu();
-  }, true);
-  let rootDir = getRootDirectory(gTestPath);
-  content.location = rootDir + "page_style_sample.html";
+/**
+ * Stylesheets are updated for a browser after the pageshow event.
+ * This helper function returns a Promise that waits for that pageshow
+ * event, and then resolves on the next tick to ensure that gPageStyleMenu
+ * has had a chance to update the stylesheets.
+ *
+ * @param browser
+ *        The <xul:browser> to wait for.
+ * @return Promise
+ */
+function promiseStylesheetsUpdated(browser) {
+  return ContentTask.spawn(browser, { PAGE }, function*(args) {
+    return new Promise((resolve) => {
+      addEventListener("pageshow", function onPageShow(e) {
+        if (e.target.location == args.PAGE) {
+          removeEventListener("pageshow", onPageShow);
+          content.setTimeout(resolve, 0);
+        }
+      });
+    })
+  });
 }
 
-function checkPageStyleMenu() {
-  var menupopup = document.getElementById("pageStyleMenu")
-                          .getElementsByTagName("menupopup")[0];
+const PAGE = "http://example.com/browser/browser/base/content/test/general/page_style_sample.html";
+
+/*
+ * Test that the right stylesheets do (and others don't) show up
+ * in the page style menu.
+ */
+add_task(function*() {
+  let tab = yield BrowserTestUtils.openNewForegroundTab(gBrowser, "about:blank", false);
+  let browser = tab.linkedBrowser;
+  yield BrowserTestUtils.loadURI(browser, PAGE);
+  yield promiseStylesheetsUpdated(browser);
+
+  let menupopup = document.getElementById("pageStyleMenu").menupopup;
   gPageStyleMenu.fillPopup(menupopup);
 
   var items = [];
@@ -23,45 +45,53 @@ function checkPageStyleMenu() {
     items.push(current);
   }
 
-  var validLinks = 0;
-  Array.forEach(content.document.getElementsByTagName("link"), function (link) {
-    var title = link.getAttribute("title");
-    var rel = link.getAttribute("rel");
-    var media = link.getAttribute("media");
-    var idstring = "link " + (title ? title : "without title and") +
-                   " with rel=\"" + rel + "\"" +
-                   (media ? " and media=\"" + media + "\"" : "");
+  items = items.map(el => ({
+    label: el.getAttribute("label"),
+    checked: el.getAttribute("checked") == "true",
+  }));
 
-    var item = items.filter(item => item.getAttribute("label") == title);
-    var found = item.length == 1;
-    var checked = found && (item[0].getAttribute("checked") == "true");
+  let validLinks = yield ContentTask.spawn(gBrowser.selectedBrowser, items, function(items) {
+    let validLinks = 0;
+    Array.forEach(content.document.querySelectorAll("link, style"), function (el) {
+      var title = el.getAttribute("title");
+      var rel = el.getAttribute("rel");
+      var media = el.getAttribute("media");
+      var idstring = el.nodeName + " " + (title ? title : "without title and") +
+                     " with rel=\"" + rel + "\"" +
+                     (media ? " and media=\"" + media + "\"" : "");
 
-    switch (link.getAttribute("data-state")) {
-      case "0":
-        ok(!found, idstring + " does not show up in page style menu");
-        break;
-      case "0-todo":
-        validLinks++;
-        todo(!found, idstring + " should not show up in page style menu");
-        ok(!checked, idstring + " is not selected");
-        break;
-      case "1":
-        validLinks++;
-        ok(found, idstring + " shows up in page style menu");
-        ok(!checked, idstring + " is not selected");
-        break;
-      case "2":
-        validLinks++;
-        ok(found, idstring + " shows up in page style menu");
-        ok(checked, idstring + " is selected");
-        break;
-      default:
-        throw "data-state attribute is missing or has invalid value";
-    }
+      var item = items.filter(item => item.label == title);
+      var found = item.length == 1;
+      var checked = found && item[0].checked;
+
+      switch (el.getAttribute("data-state")) {
+        case "0":
+          ok(!found, idstring + " should not show up in page style menu");
+          break;
+        case "0-todo":
+          validLinks++;
+          todo(!found, idstring + " should not show up in page style menu");
+          ok(!checked, idstring + " should not be selected");
+          break;
+        case "1":
+          validLinks++;
+          ok(found, idstring + " should show up in page style menu");
+          ok(!checked, idstring + " should not be selected");
+          break;
+        case "2":
+          validLinks++;
+          ok(found, idstring + " should show up in page style menu");
+          ok(checked, idstring + " should be selected");
+          break;
+        default:
+          throw "data-state attribute is missing or has invalid value";
+      }
+    });
+    return validLinks;
   });
 
-  is(validLinks, items.length, "all valid links found");
+  ok(items.length, "At least one item in the menu");
+  is(items.length, validLinks, "all valid links found");
 
-  gBrowser.removeCurrentTab();
-  finish();
-}
+  yield BrowserTestUtils.removeTab(tab);
+});
