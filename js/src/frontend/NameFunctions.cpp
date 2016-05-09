83da9895b9a2029615eeeba697be7769a75957a1
Morgan Phillips <winter2718@gmail.com>
2016-05-09 00:42:45 -0700

    Bug 1266255 - Implement ES6 function "name" property semantics for simple assignments and methods; r=jorendorff

--- js/src/frontend/NameFunctions.cpp
+++ js/src/frontend/NameFunctions.cpp
@@ -183,7 +183,9 @@ class NameResolver
     /*
      * Resolve the name of a function. If the function already has a name
      * listed, then it is skipped. Otherwise an intelligent name is guessed to
-     * assign to the function's displayAtom field
+     * assign to the function's displayAtom field. Note: the format used to
+     * describe guessed names is relied upon for generation of es6 compatible
+     * [function] names (see: FunctionNameFromDisplayName in jsfun.cpp).
      */
     bool resolveFun(ParseNode* pn, HandleAtom prefix, MutableHandleAtom retAtom) {
         MOZ_ASSERT(pn != nullptr);
