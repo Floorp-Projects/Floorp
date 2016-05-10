3dbc8b187f928e77407ae4d910481c1a8149c121
Kit Cambridge <kcambridge@mozilla.com>
2016-05-09 20:54:05 -0700

    Bug 1206424 - Ensure `daysElapsed` is always non-negative when updating the push quota. r=wchen

--- dom/push/test/xpcshell/test_quota_with_notification.js
+++ dom/push/test/xpcshell/test_quota_with_notification.js
@@ -10,6 +10,8 @@ Cu.import("resource://gre/modules/Task.jsm");
 const userAgentID = 'aaabf1f8-2f68-44f1-a920-b88e9e7d7559';
 const nsIPushQuotaManager = Components.interfaces.nsIPushQuotaManager;
 
+const MS_IN_ONE_DAY = 1 * 24 * 60 * 60 * 1000;
+
 function run_test() {
   do_get_profile();
   setPrefs({
@@ -41,16 +43,34 @@ add_task(function* test_expiration_origin_threshold() {
     originAttributes: '',
     quota: 16,
   });
+  yield db.put({
+    channelID: 'last-visit-future',
+    pushEndpoint: 'https://example.org/push/2',
+    scope: 'https://example.info/~marty',
+    pushCount: 0,
+    lastPush: 0,
+    version: null,
+    originAttributes: '',
+    quota: 8,
+  });
 
   // A visit one day ago should provide a quota of 8 messages.
   yield PlacesTestUtils.addVisits({
     uri: 'https://example.com/login',
     title: 'Sign in to see your auctions',
-    visitDate: (Date.now() - 1 * 24 * 60 * 60 * 1000) * 1000,
+    visitDate: (Date.now() - MS_IN_ONE_DAY) * 1000,
     transition: Ci.nsINavHistoryService.TRANSITION_LINK
   });
+  // Make sure we calculate the quota correctly for visit dates in the
+  // future (bug 1206424).
+  yield PlacesTestUtils.addVisits({
+    uri: 'https://example.info/~marty/flux-capacitor.pdf',
+    title: 'Flux Capacitor Design',
+    visitDate: (Date.now() + MS_IN_ONE_DAY) * 1000,
+    transition: Ci.nsINavHistoryService.TRANSITION_TYPED
+  });
 
-  let numMessages = 10;
+  let numMessages = 11;
 
   let updates = 0;
   let notifyPromise = promiseObserverNotification(PushServiceComponent.pushTopic, (subject, data) => {
@@ -70,7 +90,7 @@ add_task(function* test_expiration_origin_threshold() {
     let quotaUpdateCount = 0;
     PushService._updateQuotaTestCallback = function() {
       quotaUpdateCount++;
-      if (quotaUpdateCount == 10) {
+      if (quotaUpdateCount == numMessages) {
         resolve();
       }
     };
@@ -99,6 +119,13 @@ add_task(function* test_expiration_origin_threshold() {
               }],
             }));
           }
+          this.serverSendMsg(JSON.stringify({
+            messageType: 'notification',
+            updates: [{
+              channelID: 'last-visit-future',
+              version: 1,
+            }],
+          }));
         },
         onUnregister(request) {
           ok(false, "Channel should not be unregistered.");
@@ -117,4 +144,8 @@ add_task(function* test_expiration_origin_threshold() {
 
   let expiredRecord = yield db.getByKeyID('f56645a9-1f32-4655-92ad-ddc37f6d54fb');
   notStrictEqual(expiredRecord.quota, 0, 'Expired record not updated');
+
+  let futureRecord = yield db.getByKeyID('last-visit-future');
+  equal(futureRecord.quota, 15,
+    'Should reset and reduce the quota for visits in the future');
 });
