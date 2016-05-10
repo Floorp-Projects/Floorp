52e7064618f76646527c87e4063ef202ec6efb73
Gerald Squelart <gsquelart@mozilla.com>
2016-05-10 18:13:14 +1000

    Bug 1271593 - NS_NewRunnableFunction should forward its arg - r=froydnj
    By perfect-forwarding its argument, we can automatically gain move semantics
    optimization when storing the given function object into nsRunnableFunction.
    Also it allows movable-only function objects.
    
    MozReview-Commit-ID: 9EZK84ZhMvR
    
    --HG--
    extra : rebase_source : 9ec11f728d69d4b032d9ba4263241832030b383a

--- xpcom/glue/nsThreadUtils.h
+++ xpcom/glue/nsThreadUtils.h
@@ -20,6 +20,7 @@
 #include "mozilla/Atomics.h"
 #include "mozilla/IndexSequence.h"
 #include "mozilla/Likely.h"
+#include "mozilla/Move.h"
 #include "mozilla/Tuple.h"
 #include "mozilla/TypeTraits.h"
 
@@ -266,8 +267,9 @@ template<typename Function>
 class nsRunnableFunction : public mozilla::Runnable
 {
 public:
-  explicit nsRunnableFunction(const Function& aFunction)
-    : mFunction(aFunction)
+  template <typename F>
+  explicit nsRunnableFunction(F&& aFunction)
+    : mFunction(mozilla::Forward<F>(aFunction))
   { }
 
   NS_IMETHOD Run() {
@@ -281,9 +283,9 @@ private:
 };
 
 template<typename Function>
-nsRunnableFunction<Function>* NS_NewRunnableFunction(const Function& aFunction)
+nsRunnableFunction<Function>* NS_NewRunnableFunction(Function&& aFunction)
 {
-  return new nsRunnableFunction<Function>(aFunction);
+  return new nsRunnableFunction<Function>(mozilla::Forward<Function>(aFunction));
 }
 
 // An event that can be used to call a method on a class.  The class type must
