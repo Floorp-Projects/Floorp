682cef8ac218ea97876002f7d4e93f4e3e74ee3c
Jakob Olesen <jolesen@mozilla.com>
2016-05-10 15:53:38 -0700

    Bug 1136226 - Unify Bailout_NonSimd*Input. r=nbp
    We have a lot of SIMD types in Ion now, and there is little benefit in
    maintaining a separate bailout kind for each type. Instead, use a single
    Bailout_NonSimd bailout kind to indicate that SimdUnbox didn't get the SIMD
    type it was expecting.

--- js/src/jit/IonTypes.h
+++ js/src/jit/IonTypes.h
@@ -104,9 +104,7 @@ enum BailoutKind
     Bailout_NonSymbolInput,
 
     // SIMD Unbox expects a given type, bails out if it doesn't match.
-    Bailout_NonSimdBool32x4Input,
-    Bailout_NonSimdInt32x4Input,
-    Bailout_NonSimdFloat32x4Input,
+    Bailout_UnexpectedSimdInput,
 
     // Atomic operations require shared memory, bail out if the typed array
     // maps unshared memory.
@@ -213,12 +211,8 @@ BailoutKindString(BailoutKind kind)
         return "Bailout_NonStringInput";
       case Bailout_NonSymbolInput:
         return "Bailout_NonSymbolInput";
-      case Bailout_NonSimdBool32x4Input:
-        return "Bailout_NonSimdBool32x4Input";
-      case Bailout_NonSimdInt32x4Input:
-        return "Bailout_NonSimdInt32x4Input";
-      case Bailout_NonSimdFloat32x4Input:
-        return "Bailout_NonSimdFloat32x4Input";
+      case Bailout_UnexpectedSimdInput:
+        return "Bailout_UnexpectedSimdInput";
       case Bailout_NonSharedTypedArrayInput:
         return "Bailout_NonSharedTypedArrayInput";
       case Bailout_Debugger:
