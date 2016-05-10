79b62712ae84eb12ecb99bf50e782e9c459f6b25
Benjamin Bouvier <benj@benj.me>
2016-05-10 16:11:51 +0200

    Bug 1271010: Remove AsmJSInternalCallee; r=luke
    MozReview-Commit-ID: KHmDlCFflm

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -481,7 +481,7 @@ class MacroAssembler : public MacroAssemblerSpecific
     void call(JitCode* c) PER_SHARED_ARCH;
 
     inline void call(const wasm::CallSiteDesc& desc, const Register reg);
-    inline void call(const wasm::CallSiteDesc& desc, AsmJSInternalCallee callee);
+    inline void call(const wasm::CallSiteDesc& desc, uint32_t callee);
 
     CodeOffset callWithPatch() PER_SHARED_ARCH;
     void patchCall(uint32_t callerOffset, uint32_t calleeOffset) PER_SHARED_ARCH;
