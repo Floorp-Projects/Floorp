8096c6e0e8c46ac1af3bf2ebb9bdcf37706691dc
James Long <longster@gmail.com>
2016-05-11 11:17:38 -0400

    Bug 1260042 - add a test to make sure auto pretty-printing works in case of error in debugger r=me

---
+++ devtools/client/debugger/test/mochitest/browser_dbg_auto-pretty-print-03.js
@@ -0,0 +1,56 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * If auto pretty-printing it enabled, make sure that if
+ * pretty-printing fails that it still properly shows the original
+ * source.
+ */
+
+const TAB_URL = EXAMPLE_URL + "doc_auto-pretty-print-02.html";
+
+var FIRST_SOURCE = "code_ugly-6.js";
+var SECOND_SOURCE = "code_ugly-7.js";
+
+function test(){
+  initDebugger(TAB_URL).then(([aTab, aDebuggee, aPanel]) => {
+    const gTab = aTab;
+    const gPanel = aPanel;
+    const gDebugger = gPanel.panelWin;
+
+    const gController = gDebugger.DebuggerController;
+    const gEditor = gDebugger.DebuggerView.editor;
+    const constants = gDebugger.require('./content/constants');
+    const queries = gDebugger.require('./content/queries');
+    const actions = bindActionCreators(gPanel);
+
+    Task.spawn(function*() {
+      yield waitForSourceShown(gPanel, FIRST_SOURCE);
+
+      const secondSource = queries.getSourceByURL(gController.getState(), EXAMPLE_URL + SECOND_SOURCE);
+      actions.selectSource(secondSource);
+
+      // It should be showing the loading text
+      is(gEditor.getText(), gDebugger.DebuggerView._loadingText,
+        "The editor loading text is shown");
+
+      gController.dispatch({
+        type: constants.TOGGLE_PRETTY_PRINT,
+        status: "error",
+        source: secondSource,
+      });
+
+      is(gEditor.getText(), gDebugger.DebuggerView._loadingText,
+        "The editor loading text is shown");
+
+      yield waitForSourceShown(gPanel, SECOND_SOURCE);
+
+      ok(gEditor.getText().includes("function foo"),
+         "The second source is shown");
+
+      yield closeDebuggerAndFinish(gPanel);
+    });
+  });
+}
