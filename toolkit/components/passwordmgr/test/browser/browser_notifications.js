2c9a87102aa0b8fedfac7615429df25b8451edec
gasolin <gasolin@gmail.com>
2016-05-11 13:32:15 -0700

    Bug 1217134 - Replace show password placeholder with conventional show password checkbox; r=MattN
    MozReview-Commit-ID: 26bja1C8vl2

--- toolkit/components/passwordmgr/test/browser/browser_notifications.js
+++ toolkit/components/passwordmgr/test/browser/browser_notifications.js
@@ -1,4 +1,3 @@
-Cu.import("resource://testing-common/ContentTaskUtils.jsm", this);
 /**
  * Test that the doorhanger notification for password saving is populated with
  * the correct values in various password capture cases.
@@ -49,10 +48,11 @@ add_task(function* test_save_change() {
         });
       yield promiseShown;
 
+      let notificationElement = PopupNotifications.panel.childNodes[0];
       // Check the actual content of the popup notification.
-      Assert.equal(document.getElementById("password-notification-username")
+      Assert.equal(notificationElement.querySelector("#password-notification-username")
                            .getAttribute("value"), username);
-      Assert.equal(document.getElementById("password-notification-password")
+      Assert.equal(notificationElement.querySelector("#password-notification-password")
                            .getAttribute("value"), password);
 
       // Simulate the action on the notification to request the login to be
@@ -61,7 +61,6 @@ add_task(function* test_save_change() {
       let expectedNotification = oldPassword ? "modifyLogin" : "addLogin";
       let promiseLogin = TestUtils.topicObserved("passwordmgr-storage-changed",
                          (_, data) => data == expectedNotification);
-      let notificationElement = PopupNotifications.panel.childNodes[0];
       notificationElement.button.doCommand();
       let [result] = yield promiseLogin;
 
@@ -131,6 +130,7 @@ add_task(function* test_edit_username() {
         password: "old password",
       }));
     }
+
     if (testCase.usernameChangedToExists) {
       Services.logins.addLogin(LoginTestUtils.testData.formLogin({
         hostname: "https://example.com",
@@ -158,9 +158,10 @@ add_task(function* test_edit_username() {
         });
       yield promiseShown;
 
+      let notificationElement = PopupNotifications.panel.childNodes[0];
       // Modify the username in the dialog if requested.
       if (testCase.usernameChangedTo) {
-        document.getElementById("password-notification-username")
+        notificationElement.querySelector("#password-notification-username")
                 .setAttribute("value", testCase.usernameChangedTo);
       }
 
@@ -176,7 +177,6 @@ add_task(function* test_edit_username() {
       let expectedNotification = expectModifyLogin ? "modifyLogin" : "addLogin";
       let promiseLogin = TestUtils.topicObserved("passwordmgr-storage-changed",
                          (_, data) => data == expectedNotification);
-      let notificationElement = PopupNotifications.panel.childNodes[0];
       notificationElement.button.doCommand();
       let [result] = yield promiseLogin;
 
@@ -200,8 +200,8 @@ add_task(function* test_edit_username() {
  * We check the following cases:
  *   - Editing the password of a new login.
  *   - Editing the password of an existing login.
- *   - Changing the username and password to an existing login with different password.
- *   - Changing the username and password to an existing login with the same password;
+ *   - Changing both username and password to an existing login.
+ *   - Changing the username to an existing login.
  *   - Editing username to an empty one and a new password.
  *
  * If both the username and password matches an already existing login, we should not
@@ -241,7 +241,7 @@ add_task(function* test_edit_password() {
     usernameChangedToExists: true,
     passwordInPage: "password",
     passwordChangedTo: "newPassword",
-    timesUsed: 1,
+    timesUsed: 2,
   }];
 
   for (let testCase of testCases) {
@@ -256,6 +256,7 @@ add_task(function* test_edit_password() {
         password: testCase.passwordInStorage,
       }));
     }
+
     if (testCase.usernameChangedToExists) {
       Services.logins.addLogin(LoginTestUtils.testData.formLogin({
         hostname: "https://example.com",
@@ -283,21 +284,22 @@ add_task(function* test_edit_password() {
         });
       yield promiseShown;
 
+      let notificationElement = PopupNotifications.panel.childNodes[0];
       // Modify the username in the dialog if requested.
       if (testCase.usernameChangedTo) {
-        document.getElementById("password-notification-username")
+        notificationElement.querySelector("#password-notification-username")
                 .setAttribute("value", testCase.usernameChangedTo);
       }
 
       // Modify the password in the dialog if requested.
       if (testCase.passwordChangedTo) {
-        document.getElementById("password-notification-password")
+        notificationElement.querySelector("#password-notification-password")
                 .setAttribute("value", testCase.passwordChangedTo);
       }
 
       // We expect a modifyLogin notification if the final username used by the
       // dialog exists in the logins database, otherwise an addLogin one.
-      let expectModifyLogin = testCase.usernameChangedTo
+      let expectModifyLogin = typeof testCase.usernameChangedTo !== "undefined"
                               ? testCase.usernameChangedToExists
                               : testCase.usernameInPageExists;
 
@@ -307,7 +309,6 @@ add_task(function* test_edit_password() {
       let expectedNotification = expectModifyLogin ? "modifyLogin" : "addLogin";
       let promiseLogin = TestUtils.topicObserved("passwordmgr-storage-changed",
                          (_, data) => data == expectedNotification);
-      let notificationElement = PopupNotifications.panel.childNodes[0];
       notificationElement.button.doCommand();
       let [result] = yield promiseLogin;
 
@@ -334,118 +335,4 @@ add_task(function* test_edit_password() {
     // Clean up the database before the next test case is executed.
     Services.logins.removeAllLogins();
   }
-});
-
-/**
- * Test that the doorhanger main action button is disabled
- * when the password field is empty.
- *
- * Also checks that submiting an empty password throws an error.
- *
- * We skip Linux for now because focusing elements on the doorhanger
- * doesn't work as expected (Bug 1182296)
- */
-add_task(function* test_empty_password() {
-  if (Services.appinfo.OS == "Linux") {
-    Assert.ok(true, "Skipping test on Linux.");
-    return;
-  }
-  yield BrowserTestUtils.withNewTab({
-      gBrowser,
-      url: "https://example.com/browser/toolkit/components/" +
-           "passwordmgr/test/browser/form_basic.html",
-    }, function* (browser) {
-      // Submit the form in the content page with the credentials from the test
-      // case. This will cause the doorhanger notification to be displayed.
-      let promiseShown = BrowserTestUtils.waitForEvent(PopupNotifications.panel,
-                                                       "popupshown");
-      yield ContentTask.spawn(browser, null,
-        function* () {
-          let doc = content.document;
-          doc.getElementById("form-basic-username").value = "username";
-          doc.getElementById("form-basic-password").value = "p";
-          doc.getElementById("form-basic").submit();
-        });
-      yield promiseShown;
-
-      let notificationElement = PopupNotifications.panel.childNodes[0];
-      let passwordTextbox = notificationElement.querySelector("#password-notification-password");
-
-      // Focus the password textbox
-      let focusPassword = BrowserTestUtils.waitForEvent(passwordTextbox, "focus");
-      passwordTextbox.focus();
-      yield focusPassword;
-
-      // Wait for the textbox type to change
-      yield ContentTaskUtils.waitForCondition(() => passwordTextbox.type == "", "Password textbox changed type");
-
-      // Synthesize input to empty the field
-      EventUtils.synthesizeKey("VK_RIGHT", {});
-      yield EventUtils.synthesizeKey("VK_BACK_SPACE", {});
-
-      let mainActionButton = document.getAnonymousElementByAttribute(notificationElement.button, "anonid", "button");
-
-      // Wait for main button to get disabled
-      yield ContentTaskUtils.waitForCondition(() => mainActionButton.disabled, "Main action button is disabled");
-
-      // Makes sure submiting an empty password throws an error
-      Assert.throws(notificationElement.button.doCommand(),
-                    "Can't add a login with a null or empty password.",
-                    "Should fail for an empty password");
-    });
-});
-
-/**
- * Checks that clicking at the doorhanger pane takes the focus
- * out of the password field.
- *
- * We skip Linux for now because focusing elements on the doorhanger
- * doesn't work as expected (Bug 1182296)
- */
-add_task(function* test_unfocus_click() {
-  if (Services.appinfo.OS == "Linux") {
-    Assert.ok(true, "Skipping test on Linux.");
-    return;
-  }
-  yield BrowserTestUtils.withNewTab({
-      gBrowser,
-      url: "https://example.com/browser/toolkit/components/" +
-           "passwordmgr/test/browser/form_basic.html",
-    }, function* (browser) {
-      // Submit the form in the content page with the credentials from the test
-      // case. This will cause the doorhanger notification to be displayed.
-
-      let promiseShown = BrowserTestUtils.waitForEvent(PopupNotifications.panel,
-                                                       "popupshown");
-      yield ContentTask.spawn(browser, null,
-        function* () {
-          let doc = content.document;
-          doc.getElementById("form-basic-username").value = "username";
-          doc.getElementById("form-basic-password").value = "password";
-          doc.getElementById("form-basic").submit();
-        });
-      yield promiseShown;
-
-      let notificationElement = PopupNotifications.panel.childNodes[0];
-      let passwordTextbox = notificationElement.querySelector("#password-notification-password");
-
-      // Focus the password textbox
-      let focusPassword = BrowserTestUtils.waitForEvent(passwordTextbox, "focus");
-      passwordTextbox.focus();
-      yield focusPassword;
-
-      // Wait for the textbox type to change
-      yield ContentTaskUtils.waitForCondition(() => passwordTextbox.type == "",
-                                              "Password textbox changed type");
-
-      let notificationIcon = document.getAnonymousElementByAttribute(notificationElement,
-                                                                     "class",
-                                                                     "popup-notification-icon");
-
-      yield EventUtils.synthesizeMouseAtCenter(notificationIcon, {});
-
-      // Wait for the textbox type to change back
-      yield ContentTaskUtils.waitForCondition(() => passwordTextbox.type == "password",
-                                              "Password textbox changed type back to password");
-    });
 });
