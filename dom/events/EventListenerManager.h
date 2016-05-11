288d78702a9ab47e79117eb36a2d3fb2fbbfc8a2
Wes Kocher <wkocher@mozilla.com>
2016-05-11 09:36:20 -0700

    Backed out 6 changesets (bug 1268749, bug 1268750) until a fix can be landed for the rc2 permafail a=backout
    Backed out changeset f6940851d82d (bug 1268749)
    Backed out changeset 268f715c7ae0 (bug 1268749)
    Backed out changeset fce30e08f966 (bug 1268750)
    Backed out changeset 1ce9d2db7389 (bug 1268749)
    Backed out changeset b0b9aae25b53 (bug 1268749)
    Backed out changeset 93227ba07158 (bug 1268750)

--- dom/events/EventListenerManager.h
+++ dom/events/EventListenerManager.h
@@ -195,7 +195,6 @@ public:
     bool mListenerIsHandler : 1;
     bool mHandlerIsString : 1;
     bool mAllEvents : 1;
-    bool mIsChrome : 1;
 
     EventListenerFlags mFlags;
 
--- layout/style/nsCSSParser.cpp
+++ layout/style/nsCSSParser.cpp
@@ -5872,14 +5872,19 @@ CSSParserImpl::ParsePseudoSelector(int32_t&       aDataMask,
   CSSPseudoElementType pseudoElementType =
     nsCSSPseudoElements::GetPseudoType(pseudo);
   nsCSSPseudoClasses::Type pseudoClassType =
-    nsCSSPseudoClasses::GetPseudoType(pseudo, AgentRulesEnabled(),
-                                      ChromeRulesEnabled());
+    nsCSSPseudoClasses::GetPseudoType(pseudo);
   bool pseudoClassIsUserAction =
     nsCSSPseudoClasses::IsUserActionPseudoClass(pseudoClassType);
 
-  if (pseudoElementType < CSSPseudoElementType::Count && !AgentRulesEnabled() &&
-      nsCSSPseudoElements::PseudoElementIsUASheetOnly(pseudoElementType)) {
-    // This pseudo-element is not exposed to content.
+  if (!AgentRulesEnabled() &&
+      ((pseudoElementType < CSSPseudoElementType::Count &&
+        nsCSSPseudoElements::PseudoElementIsUASheetOnly(pseudoElementType)) ||
+       (pseudoClassType != nsCSSPseudoClasses::ePseudoClass_NotPseudoClass &&
+        nsCSSPseudoClasses::PseudoClassIsUASheetOnly(pseudoClassType)) ||
+       (!ChromeRulesEnabled() &&
+        (pseudoClassType != nsCSSPseudoClasses::ePseudoClass_NotPseudoClass &&
+         nsCSSPseudoClasses::PseudoClassIsUASheetAndChromeOnly(pseudoClassType))))) {
+    // This pseudo-element or pseudo-class is not exposed to content.
     REPORT_UNEXPECTED_TOKEN(PEPseudoSelUnknown);
     UngetToken();
     return eSelectorParsingStatus_Error;
