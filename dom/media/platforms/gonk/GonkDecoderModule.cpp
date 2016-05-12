c6f3e15c339385a8245ca36e2f30270d4d159624
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-05-12 19:06:14 +1000

    Bug 1271491: P7. Remove unused members. r=alfredo
    MozReview-Commit-ID: FEY5j7PWmJy

--- dom/media/platforms/gonk/GonkDecoderModule.cpp
+++ dom/media/platforms/gonk/GonkDecoderModule.cpp
@@ -18,13 +18,6 @@ GonkDecoderModule::~GonkDecoderModule()
 {
 }
 
-/* static */
-void
-GonkDecoderModule::Init()
-{
-  MOZ_ASSERT(NS_IsMainThread(), "Must be on main thread.");
-}
-
 already_AddRefed<MediaDataDecoder>
 GonkDecoderModule::CreateVideoDecoder(const VideoInfo& aConfig,
                                      mozilla::layers::LayersBackend aLayersBackend,
