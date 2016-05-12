6121628b211ca3f0eb7e9ea2fec59b97f3a01048
CJKu <cku@mozilla.com>
2016-05-13 00:09:05 +0800

    Bug 1269971 - Part 4. Add text stroke into text mask; r=jfkthame
    MozReview-Commit-ID: IkaLMEX5q0T

--- layout/generic/nsTextFrame.cpp
+++ layout/generic/nsTextFrame.cpp
@@ -6613,7 +6613,9 @@ nsTextFrame::PaintText(const PaintTextParams& aParams,
     foregroundColor = gfxColor.ToABGR();
   }
 
-  nscolor textStrokeColor = textPaintStyle.GetWebkitTextStrokeColor();
+  nscolor textStrokeColor = aParams.generateTextMask
+                            ? NS_RGBA(0, 0, 0, 255)
+                            : textPaintStyle.GetWebkitTextStrokeColor();
   if (aOpacity != 1.0f) {
     gfx::Color gfxColor = gfx::Color::FromABGR(textStrokeColor);
     gfxColor.a *= aOpacity;
