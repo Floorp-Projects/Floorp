fdb3066641d58a5381670634741386433e0e0385
Mats Palmgren <matspal@gmail.com>
2016-05-12 12:07:45 +1200

    Bug 881832 - Make inner document reflow asynchronous. r=tnikkel

--- dom/base/nsFrameLoader.cpp
+++ dom/base/nsFrameLoader.cpp
@@ -2368,7 +2368,8 @@ nsFrameLoader::UpdateBaseWindowPositionAndSize(nsSubDocumentFrame *aIFrame)
 
     ScreenIntSize size = aIFrame->GetSubdocumentSize();
 
-    baseWindow->SetPositionAndSize(x, y, size.width, size.height, false);
+    baseWindow->SetPositionAndSize(x, y, size.width, size.height,
+                                   nsIBaseWindow::eDelayResize);
   }
 }
 
