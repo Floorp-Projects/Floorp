fc82b1bd89823f918cd23ce4cf5f96c6c42201f6
Jim Mathies <jmathies@mozilla.com>
2016-05-12 14:29:05 -0500

    Bug 1263951 - avoid trying to initialize new GMP instances once the browser enters shutdown. r=cpearce a=ritu
    --HG--
    extra : rebase_source : 87785c40aef3d03fdbb5a3dafae55c96e658c4df

--- dom/media/gmp/GMPServiceParent.cpp
+++ dom/media/gmp/GMPServiceParent.cpp
@@ -704,6 +704,13 @@ GeckoMediaPluginServiceParent::NotifySyncShutdownComplete()
   mWaitingForPluginsSyncShutdown = false;
 }
 
+bool
+GeckoMediaPluginServiceParent::IsShuttingDown()
+{
+  MOZ_ASSERT(NS_GetCurrentThread() == mGMPThread);
+  return mShuttingDownOnGMPThread;
+}
+
 void
 GeckoMediaPluginServiceParent::UnloadPlugins()
 {
@@ -1840,8 +1847,15 @@ GMPServiceParent::RecvLoadGMP(const nsCString& aNodeId,
                               nsTArray<ProcessId>&& aAlreadyBridgedTo,
                               ProcessId* aId,
                               nsCString* aDisplayName,
-                              uint32_t* aPluginId)
+                              uint32_t* aPluginId,
+                              nsresult* aRv)
 {
+  *aRv = NS_OK;
+  if (mService->IsShuttingDown()) {
+    *aRv = NS_ERROR_ILLEGAL_DURING_SHUTDOWN;
+    return true;
+  }
+
   RefPtr<GMPParent> gmp = mService->SelectPluginForAPI(aNodeId, aAPI, aTags);
 
   nsCString api = aTags[0];
