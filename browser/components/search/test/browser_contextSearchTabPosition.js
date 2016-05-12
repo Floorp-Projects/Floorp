c305f65b81ba165a3087664d86028bde2d594924
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-05-12 17:26:46 +0100

    Bug 1272294 - only collect non-default search engine names if the user opted into telemetry, r=florian
    MozReview-Commit-ID: 2mHHKBJyI6P
    
    --HG--
    extra : rebase_source : cbcb441527282891a2b3b6dcaa8f2da5e8543ccc

--- browser/components/search/test/browser_contextSearchTabPosition.js
+++ browser/components/search/test/browser_contextSearchTabPosition.js
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 add_task(function* test() {
+  yield SpecialPowers.pushPrefEnv({set: [["toolkit.telemetry.enabled", true]]});
   let engine = yield promiseNewEngine("testEngine.xml");
   let histogramKey = "other-" + engine.name + ".contextmenu";
   let numSearchesBefore = 0;
