b6302fec49b24a92f7787cb84c76152f0eb82f62
Jon Coppeard <jcoppeard@mozilla.com>
2016-05-12 10:00:43 +0100

    Bug 1271110 - Evict the nursery when discarding JIT code outside of a GC r=sfink a=abillings
    --HG--
    extra : source : 80d226fddf93f53eede17d694f8277ca2c485be7

---
+++ js/src/jit-test/tests/gc/bug-1271110.js
@@ -0,0 +1,37 @@
+if (!('oomTest' in this))
+    quit();
+
+var x1 = [];
+var x2 = [];
+var x3 = [];
+var x4 = [];
+(function() {
+    var gns = Object.getOwnPropertyNames(this);
+    for (var i = 0; i < 49; ++i) {
+        var gn = gns[i];
+        var g = this[gn];
+        if (typeof g == "function") {
+            var hns = Object.getOwnPropertyNames(gn);
+            for (var j = 0; j < hns.length; ++j) {
+                x1.push("");
+                x1.push("");
+                x2.push("");
+                x2.push("");
+                x3.push("");
+                x3.push("");
+                x4.push("");
+                x4.push("");
+            }
+        }
+    }
+})();
+try {
+    __proto__ = function(){};
+} catch (e) {
+    "" + e;
+}
+startgc(9222);
+Function("\
+    (function() {})();\
+    oomTest(Debugger.Script);\
+")();
