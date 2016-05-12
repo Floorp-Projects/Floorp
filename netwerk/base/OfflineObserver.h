1eaf608a83161e3b30be96186ac1b92983dd725b
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-05-12 14:35:39 +0200

    Backed out changeset 4e86f3f05b07 (bug 1148161) for causing assertions

--- netwerk/base/OfflineObserver.h
+++ netwerk/base/OfflineObserver.h
@@ -33,9 +33,6 @@ public:
 
   // OfflineDisconnect cancels all existing connections in the parent when
   // the app becomes offline.
-  // Since the offline observer holds a mutex while calling this,
-  // the implementation must make sure it doesn't call back into OfflineObserver
-  // or issue a notification for the "network:app-offline-status-changed" topic
   virtual void     OfflineDisconnect() { }
 };
 
@@ -67,11 +64,7 @@ private:
   void RemoveOfflineObserverMainThread();
 private:
   virtual ~OfflineObserver() { }
-  // This needs to be a raw pointer, or else the parent's destructor
-  // will not get called
   DisconnectableParent * mParent;
-  // We need to lock this mutex when accessing the value of mParent
-  mozilla::Mutex mLock;
 };
 
 } // namespace net
