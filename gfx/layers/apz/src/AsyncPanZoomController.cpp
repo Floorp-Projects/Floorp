6086e0f55a2f846cde2176dd3f2e699060a0c120
Botond Ballo <botond@mozilla.com>
2016-05-11 20:40:46 -0400

    Bug 1272165 - Move OverscrollAnimation into a separate file Overscroll.h. r=kats
    MozReview-Commit-ID: DaDJEZJ7EBJ
    
    --HG--
    extra : rebase_source : dc585521e917a2afbbace88ee3b6dc5f21346528

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -18,6 +18,7 @@
 #include "InputData.h"                  // for MultiTouchInput, etc
 #include "InputBlockState.h"            // for InputBlockState, TouchBlockState
 #include "InputQueue.h"                 // for InputQueue
+#include "Overscroll.h"                 // for OverscrollAnimation
 #include "OverscrollHandoffState.h"     // for OverscrollHandoffState
 #include "Units.h"                      // for CSSRect, CSSPoint, etc
 #include "UnitTransforms.h"             // for TransformTo
@@ -680,51 +681,6 @@ private:
   CSSToParentLayerScale2D mEndZoom;
 };
 
-class OverscrollAnimation: public AsyncPanZoomAnimation {
-public:
-  explicit OverscrollAnimation(AsyncPanZoomController& aApzc, const ParentLayerPoint& aVelocity)
-    : mApzc(aApzc)
-  {
-    mApzc.mX.StartOverscrollAnimation(aVelocity.x);
-    mApzc.mY.StartOverscrollAnimation(aVelocity.y);
-  }
-  ~OverscrollAnimation()
-  {
-    mApzc.mX.EndOverscrollAnimation();
-    mApzc.mY.EndOverscrollAnimation();
-  }
-
-  virtual bool DoSample(FrameMetrics& aFrameMetrics,
-                        const TimeDuration& aDelta) override
-  {
-    // Can't inline these variables due to short-circuit evaluation.
-    bool continueX = mApzc.mX.SampleOverscrollAnimation(aDelta);
-    bool continueY = mApzc.mY.SampleOverscrollAnimation(aDelta);
-    if (!continueX && !continueY) {
-      // If we got into overscroll from a fling, that fling did not request a
-      // fling snap to avoid a resulting scrollTo from cancelling the overscroll
-      // animation too early. We do still want to request a fling snap, though,
-      // in case the end of the axis at which we're overscrolled is not a valid
-      // snap point, so we request one now. If there are no snap points, this will
-      // do nothing. If there are snap points, we'll get a scrollTo that snaps us
-      // back to the nearest valid snap point.
-      // The scroll snapping is done in a deferred task, otherwise the state
-      // change to NOTHING caused by the overscroll animation ending would
-      // clobber a possible state change to SMOOTH_SCROLL in ScrollSnap().
-      mDeferredTasks.AppendElement(NewRunnableMethod(&mApzc, &AsyncPanZoomController::ScrollSnap));
-      return false;
-    }
-    return true;
-  }
-
-  virtual bool WantsRepaints() override
-  {
-    return false;
-  }
-
-private:
-  AsyncPanZoomController& mApzc;
-};
 
 class SmoothScrollAnimation : public AsyncPanZoomAnimation {
 public:
