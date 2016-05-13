0fb24349a2b3991618cc51d033eefaf4cea10072
Boris Chiou <boris.chiou@gmail.com>
2016-05-13 16:38:25 +0800

    Bug 1244590 - Part 3: Parse spacing. r=birtles
    We only support spacing mode from Web Animations API, so add a simple parser and
    use it only from the API. In this patch, parse spacing mode from the constructor
    and throw TypeError if we have any parse error.
    
    MozReview-Commit-ID: 9H7g80IcZfZ

--- dom/animation/KeyframeUtils.cpp
+++ dom/animation/KeyframeUtils.cpp
@@ -352,9 +352,6 @@ GetPropertyValuesPairs(JSContext* aCx,
                        nsTArray<PropertyValuesPair>& aResult);
 
 static bool
-IsAnimatableProperty(nsCSSProperty aProperty);
-
-static bool
 AppendStringOrStringSequenceToArray(JSContext* aCx,
                                     JS::Handle<JS::Value> aValue,
                                     ListAllowance aAllowLists,
@@ -563,6 +560,26 @@ KeyframeUtils::GetAnimationPropertiesFromKeyframes(
   return result;
 }
 
+/* static */ bool
+KeyframeUtils::IsAnimatableProperty(nsCSSProperty aProperty)
+{
+  if (aProperty == eCSSProperty_UNKNOWN) {
+    return false;
+  }
+
+  if (!nsCSSProps::IsShorthand(aProperty)) {
+    return nsCSSProps::kAnimTypeTable[aProperty] != eStyleAnimType_None;
+  }
+
+  CSSPROPS_FOR_SHORTHAND_SUBPROPERTIES(subprop, aProperty,
+                                       CSSEnabledState::eForAllContent) {
+    if (nsCSSProps::kAnimTypeTable[*subprop] != eStyleAnimType_None) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 // ------------------------------------------------------------------
 //
@@ -739,7 +756,7 @@ GetPropertyValuesPairs(JSContext* aCx,
     nsCSSProperty property =
       nsCSSProps::LookupPropertyByIDLName(propName,
                                           CSSEnabledState::eForAllContent);
-    if (IsAnimatableProperty(property)) {
+    if (KeyframeUtils::IsAnimatableProperty(property)) {
       AdditionalProperty* p = properties.AppendElement();
       p->mProperty = property;
       p->mJsidIndex = i;
@@ -766,31 +783,6 @@ GetPropertyValuesPairs(JSContext* aCx,
   }
 
   return true;
-}
-
-/**
- * Returns true if |aProperty| or, for shorthands, one or more of
- * |aProperty|'s subproperties, is animatable.
- */
-static bool
-IsAnimatableProperty(nsCSSProperty aProperty)
-{
-  if (aProperty == eCSSProperty_UNKNOWN) {
-    return false;
-  }
-
-  if (!nsCSSProps::IsShorthand(aProperty)) {
-    return nsCSSProps::kAnimTypeTable[aProperty] != eStyleAnimType_None;
-  }
-
-  CSSPROPS_FOR_SHORTHAND_SUBPROPERTIES(subprop, aProperty,
-                                       CSSEnabledState::eForAllContent) {
-    if (nsCSSProps::kAnimTypeTable[*subprop] != eStyleAnimType_None) {
-      return true;
-    }
-  }
-
-  return false;
 }
 
 /**
