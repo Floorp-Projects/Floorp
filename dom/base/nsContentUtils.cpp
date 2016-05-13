34a404bad2550e7a2331e1f6fb2d05a2d14fa1af
Xidorn Quan <quanxunzhen@gmail.com>
2016-05-13 21:17:53 +1000

    Bug 1268750 part 4 - Hide unprefixed Fullscreen API from content when disabled. r=smaug a=ritu
    MozReview-Commit-ID: KpjNFYYyCWh
    
    --HG--
    extra : source : 36023e241c8abd6baf7adcc16a277d985e567600
    extra : intermediate-source : fce30e08f96691abfa809b0821c9e4bb10395e8c

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -263,6 +263,7 @@ nsString* nsContentUtils::sModifierSeparator = nullptr;
 
 bool nsContentUtils::sInitialized = false;
 bool nsContentUtils::sIsFullScreenApiEnabled = false;
+bool nsContentUtils::sIsUnprefixedFullscreenApiEnabled = false;
 bool nsContentUtils::sTrustedFullScreenOnly = true;
 bool nsContentUtils::sIsCutCopyAllowed = true;
 bool nsContentUtils::sIsFrameTimingPrefEnabled = false;
@@ -537,6 +538,9 @@ nsContentUtils::Init()
 
   Preferences::AddBoolVarCache(&sIsFullScreenApiEnabled,
                                "full-screen-api.enabled");
+
+  Preferences::AddBoolVarCache(&sIsUnprefixedFullscreenApiEnabled,
+                               "full-screen-api.unprefix.enabled");
 
   Preferences::AddBoolVarCache(&sTrustedFullScreenOnly,
                                "full-screen-api.allow-trusted-requests-only");
