d42be8183cf137e9991025370a39666872671a73
Patrick Brosset <pbrosset@mozilla.com>
2016-05-13 13:09:17 +0200

    Bug 1260630 - ESLint cleanup of devtools/client/inspector/computed/; r=bgrins
    MozReview-Commit-ID: BreSVf4bPf

--- devtools/client/inspector/computed/test/browser_computed_style-editor-link.js
+++ devtools/client/inspector/computed/test/browser_computed_style-editor-link.js
@@ -4,53 +4,48 @@
 
 "use strict";
 
-///////////////////
-//
 // Whitelisting this test.
 // As part of bug 1077403, the leaking uncaught rejection should be fixed.
-//
 thisTestLeaksUncaughtRejectionsAndShouldBeFixed("Error: Unknown sheet source");
 
 // Tests the links from the computed view to the style editor.
 
 const STYLESHEET_URL = "data:text/css," + encodeURIComponent(
-  [".highlight {",
-   "color: blue",
-   "}"].join("\n"));
+  ".highlight {color: blue}");
 
 const DOCUMENT_URL = "data:text/html;charset=utf-8," + encodeURIComponent(
-  ['<html>' +
-   '<head>' +
-   '<title>Computed view style editor link test</title>',
-   '<style type="text/css"> ',
-   'html { color: #000000; } ',
-   'span { font-variant: small-caps; color: #000000; } ',
-   '.nomatches {color: #ff0000;}</style> <div id="first" style="margin: 10em; ',
-   'font-size: 14pt; font-family: helvetica, sans-serif; color: #AAA">',
-   '</style>',
-   '<style>',
-   'div { color: #f06; }',
-   '</style>',
-   '<link rel="stylesheet" type="text/css" href="' + STYLESHEET_URL + '">',
-   '</head>',
-   '<body>',
-   '<h1>Some header text</h1>',
-   '<p id="salutation" style="font-size: 12pt">hi.</p>',
-   '<p id="body" style="font-size: 12pt">I am a test-case. This text exists ',
-   'solely to provide some things to ',
-   '<span style="color: yellow" class="highlight">',
-   'highlight</span> and <span style="font-weight: bold">count</span> ',
-   'style list-items in the box at right. If you are reading this, ',
-   'you should go do something else instead. Maybe read a book. Or better ',
-   'yet, write some test-cases for another bit of code. ',
-   '<span style="font-style: italic">some text</span></p>',
-   '<p id="closing">more text</p>',
-   '<p>even more text</p>',
-   '</div>',
-   '</body>',
-   '</html>'].join("\n"));
+  `<html>
+   <head>
+   <title>Computed view style editor link test</title>
+   <style type="text/css">
+   html { color: #000000; }
+   span { font-variant: small-caps; color: #000000; }
+   .nomatches {color: #ff0000;}</style> <div id="first" style="margin: 10em;
+   font-size: 14pt; font-family: helvetica, sans-serif; color: #AAA">
+   </style>
+   <style>
+   div { color: #f06; }
+   </style>
+   <link rel="stylesheet" type="text/css" href="${STYLESHEET_URL}">
+   </head>
+   <body>
+   <h1>Some header text</h1>
+   <p id="salutation" style="font-size: 12pt">hi.</p>
+   <p id="body" style="font-size: 12pt">I am a test-case. This text exists
+   solely to provide some things to
+   <span style="color: yellow" class="highlight">
+   highlight</span> and <span style="font-weight: bold">count</span>
+   style list-items in the box at right. If you are reading this,
+   you should go do something else instead. Maybe read a book. Or better
+   yet, write some test-cases for another bit of code.
+   <span style="font-style: italic">some text</span></p>
+   <p id="closing">more text</p>
+   <p>even more text</p>
+   </div>
+   </body>
+   </html>`);
 
-add_task(function*() {
+add_task(function* () {
   yield addTab(DOCUMENT_URL);
   let {toolbox, inspector, view, testActor} = yield openComputedView();
   yield selectNode("span", inspector);
