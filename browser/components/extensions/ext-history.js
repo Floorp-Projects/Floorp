79a85728e08a4ac094aa0cfbea233fa44550c0da
Bob Silverberg <bsilverberg@mozilla.com>
2016-05-13 10:32:48 -0400

    Bug 1265834 - Part 2: Implement browser.history.search. r=aswan, r=mak
    Requesting review from mak for the changes to PlacesUtils.jsm.
    Note that one of these changes (toPRTime) is also present in the patch for bug 1265836, but I anticipate that this patch may land before that bug.
    
    MozReview-Commit-ID: Kg1XX40A4FW
    
    --HG--
    extra : transplant_source : %05%FAB%CENp%BE%A3%A1C%C3%B8%E6%3E%939%9E%E8%A1%5C

--- browser/components/extensions/ext-history.js
+++ browser/components/extensions/ext-history.js
@@ -9,6 +9,42 @@ XPCOMUtils.defineLazyGetter(this, "History", () => {
   return PlacesUtils.history;
 });
 
+Cu.import("resource://gre/modules/ExtensionUtils.jsm");
+const {
+  normalizeTime,
+} = ExtensionUtils;
+
+/*
+ * Converts a nsINavHistoryResultNode into a plain object
+ *
+ * https://developer.mozilla.org/en-US/docs/XPCOM_Interface_Reference/nsINavHistoryResultNode
+ */
+function convertNavHistoryResultNode(node) {
+  return {
+    id: node.pageGuid,
+    url: node.uri,
+    title: node.title,
+    lastVisitTime: PlacesUtils.toTime(node.time),
+    visitCount: node.accessCount,
+  };
+}
+
+/*
+ * Converts a nsINavHistoryContainerResultNode into an array of objects
+ *
+ * https://developer.mozilla.org/en-US/docs/XPCOM_Interface_Reference/nsINavHistoryContainerResultNode
+ */
+function convertNavHistoryContainerResultNode(container) {
+  let results = [];
+  container.containerOpen = true;
+  for (let i = 0; i < container.childCount; i++) {
+    let node = container.getChild(i);
+    results.push(convertNavHistoryResultNode(node));
+  }
+  container.containerOpen = false;
+  return results;
+}
+
 extensions.registerSchemaAPI("history", "history", (extension, context) => {
   return {
     history: {
@@ -27,6 +63,29 @@ extensions.registerSchemaAPI("history", "history", (extension, context) => {
         let url = details.url;
         // History.remove returns a boolean, but our API should return nothing
         return History.remove(url).then(() => undefined);
+      },
+      search: function(query) {
+        let beginTime = (query.startTime == null) ?
+                          PlacesUtils.toPRTime(Date.now() - 24 * 60 * 60 * 1000) :
+                          PlacesUtils.toPRTime(normalizeTime(query.startTime));
+        let endTime = (query.endTime == null) ?
+                        Number.MAX_VALUE :
+                        PlacesUtils.toPRTime(normalizeTime(query.endTime));
+        if (beginTime > endTime) {
+          return Promise.reject({message: "The startTime cannot be after the endTime"});
+        }
+
+        let options = History.getNewQueryOptions();
+        options.sortingMode = options.SORT_BY_DATE_DESCENDING;
+        options.maxResults = query.maxResults || 100;
+
+        let historyQuery = History.getNewQuery();
+        historyQuery.searchTerms = query.text;
+        historyQuery.beginTime = beginTime;
+        historyQuery.endTime = endTime;
+        let queryResult = History.executeQuery(historyQuery, options).root;
+        let results = convertNavHistoryContainerResultNode(queryResult);
+        return Promise.resolve(results);
       },
     },
   };
