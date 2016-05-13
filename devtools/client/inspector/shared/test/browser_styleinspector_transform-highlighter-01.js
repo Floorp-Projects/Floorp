db875053b855b8ca2159251f48896faf4c977fb1
Patrick Brosset <pbrosset@mozilla.com>
2016-05-13 10:16:45 +0200

    Bug 1267015 - ESLint cleanup of devtools/client/styleinspector/shared/test; r=bgrins
    MozReview-Commit-ID: GKnRmlqIBxb

--- devtools/client/inspector/shared/test/browser_styleinspector_transform-highlighter-01.js
+++ devtools/client/inspector/shared/test/browser_styleinspector_transform-highlighter-01.js
@@ -17,7 +17,7 @@ const TEST_URI = `
 
 const TYPE = "CssTransformHighlighter";
 
-add_task(function*() {
+add_task(function* () {
   yield addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
   let {inspector, view} = yield openRuleView();
 
