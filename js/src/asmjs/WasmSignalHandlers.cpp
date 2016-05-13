a31bc4c2e70b5ed33e3af51765d57fd362764f7d
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-05-13 07:00:55 +0200

    Backed out changeset c71165b9f120 (bug 956899)

--- js/src/asmjs/WasmSignalHandlers.cpp
+++ js/src/asmjs/WasmSignalHandlers.cpp
@@ -958,8 +958,9 @@ static const mach_msg_id_t sExceptionId = 2405;
 static const mach_msg_id_t sQuitId = 42;
 
 static void
-MachExceptionHandlerThread(JSRuntime* rt)
+MachExceptionHandlerThread(void* threadArg)
 {
+    JSRuntime* rt = reinterpret_cast<JSRuntime*>(threadArg);
     mach_port_t port = rt->wasmMachExceptionHandler.port();
     kern_return_t kret;
 
@@ -1011,6 +1012,7 @@ MachExceptionHandlerThread(JSRuntime* rt)
 
 MachExceptionHandler::MachExceptionHandler()
   : installed_(false),
+    thread_(nullptr),
     port_(MACH_PORT_NULL)
 {}
 
@@ -1029,7 +1031,7 @@ MachExceptionHandler::uninstall()
             MOZ_CRASH();
         installed_ = false;
     }
-    if (thread_.joinable()) {
+    if (thread_ != nullptr) {
         // Break the handler thread out of the mach_msg loop.
         mach_msg_header_t msg;
         msg.msgh_bits = MACH_MSGH_BITS(MACH_MSG_TYPE_COPY_SEND, 0);
@@ -1046,7 +1048,8 @@ MachExceptionHandler::uninstall()
         }
 
         // Wait for the handler thread to complete before deallocating the port.
-        thread_.join();
+        PR_JoinThread(thread_);
+        thread_ = nullptr;
     }
     if (port_ != MACH_PORT_NULL) {
         DebugOnly<kern_return_t> kret = mach_port_destroy(mach_task_self(), port_);
@@ -1071,7 +1074,9 @@ MachExceptionHandler::install(JSRuntime* rt)
         goto error;
 
     // Create a thread to block on reading port_.
-    if (!thread_.init(MachExceptionHandlerThread, rt))
+    thread_ = PR_CreateThread(PR_USER_THREAD, MachExceptionHandlerThread, rt,
+                              PR_PRIORITY_NORMAL, PR_GLOBAL_THREAD, PR_JOINABLE_THREAD, 0);
+    if (!thread_)
         goto error;
 
     // Direct exceptions on this thread to port_ (and thus our handler thread).
