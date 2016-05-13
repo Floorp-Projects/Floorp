7aa62a11c9b4579404c0b5127d5d0d5fd0b0fd95
Andreas Tolfsen <ato@mozilla.com>
2016-05-13 13:57:35 +0100

    Bug 1272642 - Fix failing execute script tests; r=jgriffin a=test-only
    The failing tests were introduced in bug 1123506 but was masqued by
    bug 1272109. They are not relevant for chrome context and this patch
    disables them.
    
    MozReview-Commit-ID: AgSq2GKhEIL

--- testing/marionette/harness/marionette/tests/unit/test_execute_script.py
+++ testing/marionette/harness/marionette/tests/unit/test_execute_script.py
@@ -236,7 +236,7 @@ class TestExecuteContent(MarionetteTestCase):
 @skip_if_b2g
 class TestExecuteChrome(TestExecuteContent):
     def setUp(self):
-        super(TestExecuteChrome, self).setUp()
+        TestExecuteContent.setUp(self)
         self.win = self.marionette.current_window_handle
         self.marionette.set_context("chrome")
         self.marionette.execute_script(
@@ -250,7 +250,7 @@ class TestExecuteChrome(TestExecuteContent):
         self.assertEqual(self.win, self.marionette.current_window_handle)
         super(TestExecuteChrome, self).tearDown()
 
-    def test_execute_permission(self):
+    def test_permission(self):
         self.assertEqual(1, self.marionette.execute_script(
             "var c = Components.classes; return 1;"))
 
@@ -277,3 +277,15 @@ class TestExecuteChrome(TestExecuteContent):
             self.marionette.execute_script("""
                 return true;
             """)
+
+    def test_lasting_side_effects(self):
+        pass
+
+    def test_return_web_element(self):
+        pass
+
+    def test_return_web_element_array(self):
+        pass
+
+    def test_return_web_element_nodelist(self):
+        pass
