43944667cb55314499f7a398822b24e9922cf8bb
Brian Birtles <birtles@gmail.com>
2016-05-13 09:40:52 +0900

    Bug 1271904 - Rename KeyframeEffectReadOnly.getFrames() and KeyframeEffect.setFrames() to getKeyframes()/setKeyframes(); r=hiro, r=smaug
    MozReview-Commit-ID: GwLLY39l1KE

--- layout/style/nsTransitionManager.h
+++ layout/style/nsTransitionManager.h
@@ -52,13 +52,13 @@ struct ElementPropertyTransition : public dom::KeyframeEffectReadOnly
   }
 
   nsCSSProperty TransitionProperty() const {
-    MOZ_ASSERT(mFrames.Length() == 2,
-               "Transitions should have exactly two animation frames. "
+    MOZ_ASSERT(mKeyframes.Length() == 2,
+               "Transitions should have exactly two animation keyframes. "
                "Perhaps we are using an un-initialized transition?");
-    MOZ_ASSERT(mFrames[0].mPropertyValues.Length() == 1,
+    MOZ_ASSERT(mKeyframes[0].mPropertyValues.Length() == 1,
                "Transitions should have exactly one property in their first "
                "frame");
-    return mFrames[0].mPropertyValues[0].mProperty;
+    return mKeyframes[0].mPropertyValues[0].mProperty;
   }
 
   StyleAnimationValue ToValue() const {
