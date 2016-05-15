68e68f43a0fb8c338aff07380c46dacfac6a4994
Jesper Kristensen <mail@jesperkristensen.dk>
2016-05-15 12:18:13 +0200

    Bug 1254221 - Support private browsing cookies in WebExtensions r=kmag
    MozReview-Commit-ID: 29ci8wbnMra

--- browser/components/extensions/ext-utils.js
+++ browser/components/extensions/ext-utils.js
@@ -706,6 +706,9 @@ ExtensionTabManager.prototype = {
       audible: tab.soundPlaying,
       mutedInfo,
     };
+    if (this.extension.hasPermission("cookies")) {
+      result.cookieStoreId = getCookieStoreIdForTab(result);
+    }
 
     if (this.hasTabPermission(tab)) {
       result.url = browser.currentURI.spec;
