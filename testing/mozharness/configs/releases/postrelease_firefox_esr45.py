291ca2c65a8d975b42e0a3dea33e58470ee0bd9a
Rail Aliiev <rail@mozilla.com>
2016-05-15 19:23:06 -0400

    Bug 1253757 - Branch specifics for relpro on esr45 r=jlund a=release DONTBUILD
    MozReview-Commit-ID: 8PExudPpYxj
    
    --HG--
    extra : source : afda2f94c0c145b50dedab928d9bdef4641ee7e0

---
+++ testing/mozharness/configs/releases/postrelease_firefox_esr45.py
@@ -0,0 +1,18 @@
+config = {
+    "log_name": "bump_esr45",
+    "version_files": [
+        {"file": "browser/config/version.txt"},
+        {"file": "browser/config/version_display.txt"},
+        {"file": "config/milestone.txt"},
+    ],
+    "repo": {
+        "repo": "https://hg.mozilla.org/releases/mozilla-esr45",
+        "revision": "default",
+        "dest": "mozilla-esr45",
+        "vcs": "hg",
+    },
+    "push_dest": "ssh://hg.mozilla.org/releases/mozilla-esr45",
+    "ignore_no_changes": True,
+    "ssh_user": "ffxbld",
+    "ssh_key": "~/.ssh/ffxbld_rsa",
+}
