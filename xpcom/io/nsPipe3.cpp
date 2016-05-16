4f4ac0dec0b563c550955fa348fced6db21ad41b
Ben Kelly <ben@wanderview.com>
2016-05-16 03:31:48 -0700

    Bug 1093357 P4 Follow-on to nsPipe3.cpp to address review feedback missing in P0 patch. r=me

--- xpcom/io/nsPipe3.cpp
+++ xpcom/io/nsPipe3.cpp
@@ -1125,16 +1125,6 @@ nsPipeEvents::~nsPipeEvents()
 NS_IMPL_ADDREF(nsPipeInputStream);
 NS_IMPL_RELEASE(nsPipeInputStream);
 
-/*
-NS_IMPL_QUERY_INTERFACE(nsPipeInputStream,
-                        nsIInputStream,
-                        nsIAsyncInputStream,
-                        nsISeekableStream,
-                        nsISearchableInputStream,
-                        nsICloneableInputStream,
-                        nsIBufferedInputStream,
-                        nsIClassInfo)
-*/
 NS_INTERFACE_TABLE_HEAD(nsPipeInputStream)
   NS_INTERFACE_TABLE_BEGIN
     NS_INTERFACE_TABLE_ENTRY(nsPipeInputStream, nsIAsyncInputStream)
