e07a01ca67c2afdfc37c412d14533015dad95969
Henrik Skupin <mail@hskupin.info>
2016-05-17 13:43:24 +0200

    Bug 1270953 - Enhance notification class for Firefox Puppeteer. r=maja_zf
    MozReview-Commit-ID: KE117HzhiXM
    
    --HG--
    extra : rebase_source : eefe2b6c7141784516bddf9e913162f3744e0b7e

--- testing/firefox-ui/tests/puppeteer/test_notifications.py
+++ testing/firefox-ui/tests/puppeteer/test_notifications.py
@@ -2,56 +2,42 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import os
-
 from marionette_driver import By
 from marionette_driver.errors import TimeoutException
 
 from firefox_ui_harness.testcases import FirefoxTestCase
 from firefox_puppeteer.ui.browser.notifications import (
-    AddOnInstallFailedNotification)
-
-here = os.path.abspath(os.path.dirname(__file__))
+    AddOnInstallFailedNotification,
+)
 
 
 class TestNotifications(FirefoxTestCase):
 
     def setUp(self):
         FirefoxTestCase.setUp(self)
-        # setting this pref prevents a notification from being displayed
-        # when e10s is enabled. see http://mzl.la/1TVNAXh
-        self.prefs.set_pref('browser.tabs.remote.force-enable', True)
+
+        self.prefs.set_pref('extensions.install.requireSecureOrigin', False)
+
+        self.addons_url = self.marionette.absolute_url('addons/extensions/')
+        self.utils.permissions.add(self.marionette.baseurl, 'install')
 
     def tearDown(self):
         try:
-            if self.browser.notification is not None:
-                self.browser.notification.close()
+            self.utils.permissions.remove(self.addons_url, 'install')
+
+            if self.browser.notification:
+                self.browser.notification.close(force=True)
         finally:
             FirefoxTestCase.tearDown(self)
 
-    def test_no_notification(self):
-        """No initial notifications are shown"""
-        self.assertIsNone(self.browser.notification)
-
-    def test_notification_with_origin(self):
-        """Trigger a notification with an origin"""
-        self.trigger_add_on_notification('mn-restartless-unsigned.xpi')
-        self.assertIsNotNone(self.browser.notification.origin)
-        self.assertIsNotNone(self.browser.notification.label)
-
-    def test_close_notification(self):
+    def test_open_close_notification(self):
         """Trigger and dismiss a notification"""
-        self.trigger_add_on_notification('mn-restartless-unsigned.xpi')
+        self.assertIsNone(self.browser.notification)
+        self.trigger_addon_notification('restartless_addon_unsigned.xpi')
         self.browser.notification.close()
         self.assertIsNone(self.browser.notification)
 
-    def test_add_on_failed_notification(self):
-        """Trigger add-on failed notification using an unsigned add-on"""
-        self.trigger_add_on_notification('mn-restartless-unsigned.xpi')
-        self.assertIsInstance(self.browser.notification,
-                              AddOnInstallFailedNotification)
-
-    def test_wait_for_any_notification_timeout(self):
+    def test_wait_for_notification_timeout(self):
         """Wait for a notification when one is not shown"""
         message = 'No notification was shown'
         with self.assertRaisesRegexp(TimeoutException, message):
@@ -66,12 +52,29 @@ class TestNotifications(FirefoxTestCase):
     def test_wait_for_no_notification_timeout(self):
         """Wait for no notification when one is shown"""
         message = 'Unexpected notification shown'
-        self.trigger_add_on_notification('mn-restartless-unsigned.xpi')
+        self.trigger_addon_notification('restartless_addon_unsigned.xpi')
         with self.assertRaisesRegexp(TimeoutException, message):
             self.browser.wait_for_notification(None)
 
-    def trigger_add_on_notification(self, add_on):
+    def test_notification_with_origin(self):
+        """Trigger a notification with an origin"""
+        self.trigger_addon_notification('restartless_addon_signed.xpi')
+        self.assertIn(self.browser.notification.origin, self.marionette.baseurl)
+        self.assertIsNotNone(self.browser.notification.label)
+        self.browser.notification.close()
+
+    def test_addon_install_failed_notification(self):
+        """Trigger add-on blocked notification using an unsigned add-on"""
+        # Ensure that installing unsigned extensions will fail
+        self.prefs.set_pref('xpinstall.signatures.required', True)
+
+        self.trigger_addon_notification('restartless_addon_unsigned.xpi')
+        self.assertIsInstance(self.browser.notification,
+                              AddOnInstallFailedNotification)
+        self.browser.notification.close()
+
+    def trigger_addon_notification(self, addon):
         with self.marionette.using_context('content'):
-            self.marionette.navigate('file://{0}'.format(here))
-            self.marionette.find_element(By.LINK_TEXT, add_on).click()
+            self.marionette.navigate(self.addons_url)
+            self.marionette.find_element(By.LINK_TEXT, addon).click()
         self.browser.wait_for_notification()
