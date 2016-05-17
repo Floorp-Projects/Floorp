f10f24429e6a25a72c753b1eb4488d75af044ac4
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-05-17 13:40:44 +0200

    Backed out changeset 613c6e5d56a2 (bug 1272109) for failure in test_execute_script.py. r=backout on a CLOSED TREE

--- testing/marionette/harness/marionette/tests/unit/test_pagesource.py
+++ testing/marionette/harness/marionette/tests/unit/test_pagesource.py
@@ -2,8 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette import MarionetteTestCase
-
+from marionette import MarionetteTestCase, skip_if_b2g
 
 class TestPageSource(MarionetteTestCase):
     def testShouldReturnTheSourceOfAPage(self):
