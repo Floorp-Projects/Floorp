2addb968ed9356e2c61cfb64c7feb4838bea489d
Xidorn Quan <me@upsuper.org>
2016-05-18 09:08:12 +1000

    Bug 1270648 part 5 - Setup the sandboxed fullscreen flag and make it work properly. r=smaug
    MozReview-Commit-ID: 3EKRbOyXwNi
    
    --HG--
    extra : source : 03b0ee1a3ba13b252cb3276adc098266979c3741

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -2502,6 +2502,10 @@ nsDocShell::GetFullscreenAllowed(bool* aFullscreenAllowed)
   // Assume false until we determine otherwise...
   *aFullscreenAllowed = false;
 
+  // If it is sandboxed, fullscreen is not allowed.
+  if (mSandboxFlags & SANDBOXED_FULLSCREEN) {
+    return NS_OK;
+  }
   nsCOMPtr<nsPIDOMWindowOuter> win = GetWindow();
   if (!win) {
     return NS_OK;
