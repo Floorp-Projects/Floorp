45ab564f44e07409c9d1fc6148cb8364fffc03f1
Shih-Chiang Chien <schien@mozilla.com>
2016-05-17 11:18:16 +0800

    Bug 1234128 - Part 2, use mozpresentation to identify presented content. r=smaug.

--- dom/presentation/PresentationReceiver.cpp
+++ dom/presentation/PresentationReceiver.cpp
@@ -37,11 +37,10 @@ NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION_INHERITED(PresentationReceiver)
 NS_INTERFACE_MAP_END_INHERITING(DOMEventTargetHelper)
 
 /* static */ already_AddRefed<PresentationReceiver>
-PresentationReceiver::Create(nsPIDOMWindowInner* aWindow,
-                             const nsAString& aSessionId)
+PresentationReceiver::Create(nsPIDOMWindowInner* aWindow)
 {
   RefPtr<PresentationReceiver> receiver = new PresentationReceiver(aWindow);
-  return NS_WARN_IF(!receiver->Init(aSessionId)) ? nullptr : receiver.forget();
+  return NS_WARN_IF(!receiver->Init()) ? nullptr : receiver.forget();
 }
 
 PresentationReceiver::PresentationReceiver(nsPIDOMWindowInner* aWindow)
@@ -55,28 +54,36 @@ PresentationReceiver::~PresentationReceiver()
 }
 
 bool
-PresentationReceiver::Init(const nsAString& aSessionId)
+PresentationReceiver::Init()
 {
   if (NS_WARN_IF(!GetOwner())) {
     return false;
   }
   mWindowId = GetOwner()->WindowID();
 
-  if (!aSessionId.IsEmpty()) {
-    nsresult rv = NotifySessionConnect(mWindowId, aSessionId);
-    if (NS_WARN_IF(NS_FAILED(rv))) {
-      return false;
-    }
-  }
-
-  // Register listener for incoming sessions.
   nsCOMPtr<nsIPresentationService> service =
     do_GetService(PRESENTATION_SERVICE_CONTRACTID);
   if (NS_WARN_IF(!service)) {
     return false;
   }
 
-  nsresult rv = service->RegisterRespondingListener(mWindowId, this);
+  // A session may already be connecting before the web content
+  // request for access in a receiving browsing context.
+  nsAutoString sessionId;
+  nsresult rv = service->GetExistentSessionIdAtLaunch(mWindowId, sessionId);
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return false;
+  }
+
+  if (!sessionId.IsEmpty()) {
+    rv = NotifySessionConnect(mWindowId, sessionId);
+    if (NS_WARN_IF(NS_FAILED(rv))) {
+      return false;
+    }
+  }
+
+  // Register listener for incoming sessions.
+  rv = service->RegisterRespondingListener(mWindowId, this);
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return false;
   }
