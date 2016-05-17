e4fbe1d9ac12bc1a998975d68b2566084e76c279
Stephanie Ouillon <stephouillon@mozilla.com>
2016-05-17 15:34:53 +0200

    Bug 1247459 - Meta and header CSP are merged without a semicolon. r=ckerschb

--- devtools/client/webconsole/test/browser_webconsole_bug_1247459_violation.js
+++ devtools/client/webconsole/test/browser_webconsole_bug_1247459_violation.js
@@ -3,16 +3,17 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-// Tests that the Web Console CSP messages are displayed
+// Tests that the Web Console CSP messages for two META policies
+// are correctly displayed.
 
 "use strict";
 
 const TEST_URI = "data:text/html;charset=utf8,Web Console CSP violation test";
 const TEST_VIOLATION = "https://example.com/browser/devtools/client/" +
-                       "webconsole/test/test_bug_770099_violation.html";
+                       "webconsole/test/test_bug_1247459_violation.html";
 const CSP_VIOLATION_MSG = "Content Security Policy: The page\u2019s settings " +
                           "blocked the loading of a resource at " +
-                          "http://some.example.com/test.png (\u201cdefault-src " +
+                          "http://some.example.com/test.png (\u201cimg-src " +
                           "https://example.com\u201d).";
 
 add_task(function* () {
@@ -23,13 +24,17 @@ add_task(function* () {
   hud.jsterm.clearOutput();
 
   let loaded = loadBrowser(browser);
-  content.location = TEST_VIOLATION;
+  BrowserTestUtils.loadURI(browser, TEST_VIOLATION);
   yield loaded;
 
-  yield waitForSuccess({
-    name: "CSP policy URI warning displayed successfully",
-    validator: function () {
-      return hud.outputNode.textContent.indexOf(CSP_VIOLATION_MSG) > -1;
-    }
+  yield waitForMessages({
+    webconsole: hud,
+    messages: [
+      {
+        name: "CSP policy URI warning displayed successfully",
+        text: CSP_VIOLATION_MSG,
+        repeats: 2
+      }
+    ]
   });
 });
