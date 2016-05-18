fd1f8a8b4ff51f3c13ee67c6efcf24dfc5a03248
Mike Hommey <mh+mozilla@glandium.org>
2016-05-18 16:37:19 +0900

    Bug 1269171 - Change how mozalloc.h is hooked in STL wrappers. r=froydnj
    Since the introduction of the STL wrappers, they have included
    mozalloc.h, and multiple times, we've hit header reentrancy problems,
    and worked around them as best as we could.
    
    Taking a step back, all mozalloc.h does is:
    - declare moz_* allocator functions.
    - define inline implementations of various operator new/delete variants.
    
    The first only requires the functions to be declared before they are used,
    so mozalloc.h only needs to be included before anything that would use
    those functions.
    
    The second doesn't actually require a specific order, as long as the
    declaration for those functions comes before their use, and they are
    either declared in <new> or implicitly by the C++ compiler.
    
    So all in all, it doesn't matter that mozalloc.h is included before the
    wrapped STL headers. What matters is that it's included when STL headers
    are included. So arrange things such that mozalloc.h is included after
    the first wrapped STL header is fully preprocessed (and all its includes
    have been included).

--- config/gcc-stl-wrapper.template.h
+++ config/gcc-stl-wrapper.template.h
@@ -17,32 +17,6 @@
 // Silence "warning: #include_next is a GCC extension"
 #pragma GCC system_header
 
-// Don't include mozalloc for cstdlib. See bug 1245076.
-#ifndef moz_dont_include_mozalloc_for_cstdlib
-#  define moz_dont_include_mozalloc_for_cstdlib
-#endif
-#ifndef moz_dont_include_mozalloc_for_${HEADER}
-// mozalloc.h wants <new>; break the cycle by always explicitly
-// including <new> here.  NB: this is a tad sneaky.  Sez the gcc docs:
-//
-//    `#include_next' does not distinguish between <file> and "file"
-//    inclusion, nor does it check that the file you specify has the
-//    same name as the current file. It simply looks for the file
-//    named, starting with the directory in the search path after the
-//    one where the current file was found.
-#  include_next <new>
-
-// See if we're in code that can use mozalloc.  NB: this duplicates
-// code in nscore.h because nscore.h pulls in prtypes.h, and chromium
-// can't build with that being included before base/basictypes.h.
-#  if !defined(XPCOM_GLUE) && !defined(NS_NO_XPCOM) && !defined(MOZ_NO_MOZALLOC)
-#    include "mozilla/mozalloc.h"
-#  else
-#    error "STL code can only be used with infallible ::operator new()"
-#  endif
-
-#endif
-
 #if defined(DEBUG) && !defined(_GLIBCXX_DEBUG)
 // Enable checked iterators and other goodies
 //
@@ -53,9 +27,33 @@
 //  # define _GLIBCXX_DEBUG 1
 #endif
 
+// Don't include mozalloc for cstdlib. See bug 1245076.
+#ifndef moz_dont_include_mozalloc_for_cstdlib
+#  define moz_dont_include_mozalloc_for_cstdlib
+#endif
+
+// Include mozalloc after the STL header and all other headers it includes
+// have been preprocessed.
+#if !defined(MOZ_INCLUDE_MOZALLOC_H) && \
+    !defined(moz_dont_include_mozalloc_for_${HEADER})
+#  define MOZ_INCLUDE_MOZALLOC_H
+#  define MOZ_INCLUDE_MOZALLOC_H_FROM_${HEADER}
+#endif
+
 #pragma GCC visibility push(default)
 #include_next <${HEADER}>
 #pragma GCC visibility pop
+
+#ifdef MOZ_INCLUDE_MOZALLOC_H_FROM_${HEADER}
+// See if we're in code that can use mozalloc.  NB: this duplicates
+// code in nscore.h because nscore.h pulls in prtypes.h, and chromium
+// can't build with that being included before base/basictypes.h.
+#  if !defined(XPCOM_GLUE) && !defined(NS_NO_XPCOM) && !defined(MOZ_NO_MOZALLOC)
+#    include "mozilla/mozalloc.h"
+#  else
+#    error "STL code can only be used with infallible ::operator new()"
+#  endif
+#endif
 
 // gcc calls a __throw_*() function from bits/functexcept.h when it
 // wants to "throw an exception".  functexcept exists nominally to
