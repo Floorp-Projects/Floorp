eeb195d4597e10638a06bcc04d9bd11f28d9e29f
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-05-18 15:25:00 +0100

    Bug 1126559 - don't allow resetting when that is not possible, r=MattN, a=ritu
    MozReview-Commit-ID: F0m70XKQtT7

--- browser/components/uitour/test/browser_UITour_resetProfile.js
+++ browser/components/uitour/test/browser_UITour_resetProfile.js
@@ -6,8 +6,32 @@ var gContentWindow;
 
 add_task(setup_UITourTest);
 
+let formerSelectedProfile, formerDefaultProfile;
+registerCleanupFunction(function() {
+  let profileService = Cc["@mozilla.org/toolkit/profile-service;1"].
+                       getService(Ci.nsIToolkitProfileService);
+  if (formerSelectedProfile) {
+    info("Restoring selected profile");
+    profileService.selectedProfile = formerSelectedProfile;
+  }
+  if (formerDefaultProfile) {
+    info("Restoring default profile");
+    profileService.defaultProfile = formerDefaultProfile;
+  }
+});
+
 // Test that a reset profile dialog appears when "resetFirefox" event is triggered
 add_UITour_task(function* test_resetFirefox() {
+  let profileService = Cc["@mozilla.org/toolkit/profile-service;1"].
+                       getService(Ci.nsIToolkitProfileService);
+  try {
+    formerSelectedProfile = profileService.selectedProfile;
+  } catch (ex) {}
+  try {
+    formerDefaultProfile = profileService.defaultProfile;
+  } catch (ex) {}
+  let canReset = yield getConfigurationPromise("canReset");
+  ok(!canReset, "Shouldn't be able to reset from mochitest's temporary profile.");
   let dialogPromise = new Promise((resolve) => {
     let winWatcher = Cc["@mozilla.org/embedcomp/window-watcher;1"].
                      getService(Ci.nsIWindowWatcher);
@@ -28,7 +52,18 @@ add_UITour_task(function* test_resetFirefox() {
       }
     });
   });
+
+  // make reset possible.
+  let currentProfileDir = Services.dirsvc.get("ProfD", Ci.nsIFile);
+  let profileName = "mochitest-test-profile-temp-" + Date.now();
+  let tempProfile = profileService.createProfile(currentProfileDir, profileName);
+  profileService.defaultProfile = profileService.selectedProfile = tempProfile;
+  canReset = yield getConfigurationPromise("canReset");
+  ok(canReset, "Should be able to reset from mochitest's temporary profile once it's in the profile manager.");
   yield gContentAPI.resetFirefox();
   yield dialogPromise;
+  tempProfile.remove(false);
+  canReset = yield getConfigurationPromise("canReset");
+  ok(!canReset, "Shouldn't be able to reset from mochitest's temporary profile once removed from the profile manager.");
 });
 
