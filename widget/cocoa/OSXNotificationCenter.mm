e1ac9527d2b617f627b66af86783b2cc9a2fea89
Jonathan Watt <jwatt@jwatt.org>
2016-05-18 10:19:19 +0100

    Bug 1274881 - Get rid of imgLoader::GetInstance. r=tnikkel

--- widget/cocoa/OSXNotificationCenter.mm
+++ widget/cocoa/OSXNotificationCenter.mm
@@ -391,7 +391,8 @@ OSXNotificationCenter::ShowAlertWithIconData(nsIAlertNotification* aAlert,
   } else {
     mPendingAlerts.AppendElement(osxni);
     osxni->mPendingNotifiction = notification;
-    RefPtr<imgLoader> il = imgLoader::GetInstance();
+    imgLoader* il =
+      inPrivateBrowsing ? imgLoader::PBSingleton() : imgLoader::Singleton();
     if (il) {
       nsCOMPtr<nsIURI> imageUri;
       NS_NewURI(getter_AddRefs(imageUri), imageUrl);
