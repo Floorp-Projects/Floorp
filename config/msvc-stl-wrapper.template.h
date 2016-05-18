314515f9595e8d9d92f9e19ab4ed89a2fcd6f52d
Mike Hommey <mh+mozilla@glandium.org>
2016-05-18 16:37:19 +0900

    Bug 1269171 - Change how mozalloc.h is hooked in STL wrappers. r=froydnj
    Since the introduction of the STL wrappers, they have included
    mozalloc.h, and multiple times, we've hit header reentrancy problems,
    and worked around them as best as we could.
    
    Taking a step back, all mozalloc.h does is:
    - declare moz_* allocator functions.
    - define inline implementations of various operator new/delete variants.
    
    The first only requires the functions to be declared before they are used,
    so mozalloc.h only needs to be included before anything that would use
    those functions.
    
    The second doesn't actually require a specific order, as long as the
    declaration for those functions comes before their use, and they are
    either declared in <new> or implicitly by the C++ compiler.
    
    So all in all, it doesn't matter that mozalloc.h is included before the
    wrapped STL headers. What matters is that it's included when STL headers
    are included. So arrange things such that mozalloc.h is included after
    the first wrapped STL header is fully preprocessed (and all its includes
    have been included).

--- config/msvc-stl-wrapper.template.h
+++ config/msvc-stl-wrapper.template.h
@@ -8,11 +8,15 @@
 #ifndef mozilla_${HEADER}_h
 #define mozilla_${HEADER}_h
 
-#ifndef MOZ_HAVE_INCLUDED_ALLOC
-#define MOZ_HAVE_INCLUDED_ALLOC
-
 #if _HAS_EXCEPTIONS
 #  error "STL code can only be used with -fno-exceptions"
+#endif
+
+// Include mozalloc after the STL header and all other headers it includes
+// have been preprocessed.
+#if !defined(MOZ_INCLUDE_MOZALLOC_H)
+#  define MOZ_INCLUDE_MOZALLOC_H
+#  define MOZ_INCLUDE_MOZALLOC_H_FROM_${HEADER}
 #endif
 
 // Code built with !_HAS_EXCEPTIONS calls std::_Throw(), but the win2k
@@ -20,22 +24,6 @@
 #ifndef mozilla_Throw_h
 #  include "mozilla/throw_msvc.h"
 #endif
-
-// Code might include <new> before other wrapped headers, but <new>
-// includes <exception> and so we want to wrap it.  But mozalloc.h
-// wants <new> also, so we break the cycle by always explicitly
-// including <new> here.
-#include <${NEW_HEADER_PATH}>
-
-// See if we're in code that can use mozalloc.  NB: this duplicates
-// code in nscore.h because nscore.h pulls in prtypes.h, and chromium
-// can't build with that being included before base/basictypes.h.
-#if !defined(XPCOM_GLUE) && !defined(NS_NO_XPCOM) && !defined(MOZ_NO_MOZALLOC)
-#  include "mozilla/mozalloc.h"
-#else
-#  error "STL code can only be used with infallible ::operator new()"
-#endif
-#endif /* MOZ_HAVE_INCLUDED_ALLOC */
 
 #ifdef _DEBUG
 // From
@@ -74,5 +62,16 @@
 #include <${HEADER_PATH}>
 
 #pragma warning( pop )
+
+#ifdef MOZ_INCLUDE_MOZALLOC_H_FROM_${HEADER}
+// See if we're in code that can use mozalloc.  NB: this duplicates
+// code in nscore.h because nscore.h pulls in prtypes.h, and chromium
+// can't build with that being included before base/basictypes.h.
+#  if !defined(XPCOM_GLUE) && !defined(NS_NO_XPCOM) && !defined(MOZ_NO_MOZALLOC)
+#    include "mozilla/mozalloc.h"
+#  else
+#    error "STL code can only be used with infallible ::operator new()"
+#  endif
+#endif
 
 #endif  // if mozilla_${HEADER}_h
