7137368692ec23f7e19a7b54df1c8aa9af7645f6
Mats Palmgren <mats@mozilla.com>
2016-05-18 13:49:32 +0200

    Bug 1273729 - Add some missing #includes and 'using' declarations to fix unified build errors.  r=bbirtles

--- dom/base/nsDOMMutationObserver.cpp
+++ dom/base/nsDOMMutationObserver.cpp
@@ -6,12 +6,15 @@
 
 #include "nsDOMMutationObserver.h"
 
+#include "mozilla/AnimationTarget.h"
+#include "mozilla/Maybe.h"
 #include "mozilla/OwningNonNull.h"
 
 #include "mozilla/dom/Animation.h"
 #include "mozilla/dom/KeyframeEffect.h"
 
 #include "nsContentUtils.h"
+#include "nsCSSPseudoElements.h"
 #include "nsError.h"
 #include "nsIDOMMutationEvent.h"
 #include "nsIScriptGlobalObject.h"
@@ -19,8 +22,11 @@
 #include "nsTextFragment.h"
 #include "nsThreadUtils.h"
 
+using mozilla::Maybe;
+using mozilla::NonOwningAnimationTarget;
 using mozilla::dom::TreeOrderComparator;
 using mozilla::dom::Animation;
+using mozilla::dom::Element;
 
 AutoTArray<RefPtr<nsDOMMutationObserver>, 4>*
   nsDOMMutationObserver::sScheduledMutationObservers = nullptr;
@@ -393,14 +399,14 @@ nsAnimationReceiver::RecordAnimationMutation(Animation* aAnimation,
     return;
   }
 
-  dom::Element* elem = animationTarget->mElement;
+  Element* elem = animationTarget->mElement;
   if (!Animations() || !(Subtree() || elem == Target()) ||
       elem->ChromeOnlyAccess()) {
     return;
   }
 
   // Record animations targeting to a pseudo element only when subtree is true.
-  if (animationTarget->mPseudoType != CSSPseudoElementType::NotPseudo &&
+  if (animationTarget->mPseudoType != mozilla::CSSPseudoElementType::NotPseudo &&
       !Subtree()) {
     return;
   }
