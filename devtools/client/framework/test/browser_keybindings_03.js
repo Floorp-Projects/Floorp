1915a59e3c7db8e7f7a53b037a3799a30173bae8
Alexandre Poirot <poirot.alex@gmail.com>
2016-05-19 16:13:00 -0400

    Bug 1268450 - Convert toolbox key shortcut to stop using XUL. r=jryans

--- devtools/client/framework/test/browser_keybindings_03.js
+++ devtools/client/framework/test/browser_keybindings_03.js
@@ -11,6 +11,8 @@
 const URL = "data:text/html;charset=utf8,test page for toolbox switching";
 
 var {Toolbox} = require("devtools/client/framework/toolbox");
+var strings = Services.strings.createBundle(
+  "chrome://devtools/locale/toolbox.properties");
 
 add_task(function* () {
   info("Create a test tab and open the toolbox");
@@ -18,18 +20,18 @@ add_task(function* () {
   let target = TargetFactory.forTab(tab);
   let toolbox = yield gDevTools.showToolbox(target, "webconsole");
 
-  let keyElement = toolbox.doc.getElementById("toolbox-toggle-host-key");
+  let shortcut = strings.GetStringFromName("toolbox.toggleHost.key");
 
   let {SIDE, BOTTOM, WINDOW} = Toolbox.HostType;
   checkHostType(toolbox, BOTTOM, SIDE);
 
   info("Switching from bottom to side");
-  synthesizeKeyElement(keyElement);
+  synthesizeKeyShortcut(shortcut, toolbox.win);
   yield toolbox.once("host-changed");
   checkHostType(toolbox, SIDE, BOTTOM);
 
   info("Switching from side to bottom");
-  synthesizeKeyElement(keyElement);
+  synthesizeKeyShortcut(shortcut, toolbox.win);
   yield toolbox.once("host-changed");
   checkHostType(toolbox, BOTTOM, SIDE);
 
@@ -38,7 +40,7 @@ add_task(function* () {
   checkHostType(toolbox, WINDOW, BOTTOM);
 
   info("Switching from window to bottom");
-  synthesizeKeyElement(keyElement);
+  synthesizeKeyShortcut(shortcut, toolbox.win);
   yield toolbox.once("host-changed");
   checkHostType(toolbox, BOTTOM, WINDOW);
 
