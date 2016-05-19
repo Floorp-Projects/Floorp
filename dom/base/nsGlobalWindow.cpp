cc80c3b08b231ca81e6bbbd06dbfe8a2dc5ec52a
Xidorn Quan <quanxunzhen@gmail.com>
2016-05-19 17:19:51 +1000

    Bug 1271160 - Add telemetry items for fullscreen transition. r=smaug data-review=bsmedberg r=ritu
    MozReview-Commit-ID: EvmLvE5VWOu
    
    --HG--
    extra : source : ad09f13328a7455e5b3ae9610fb1b4c9b1baa4bf
    extra : intermediate-source : c41ee7132426780f38bbffc58435548d92d2b327

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -5927,6 +5927,7 @@ private:
   nsCOMPtr<nsITimer> mTimer;
   nsCOMPtr<nsISupports> mTransitionData;
 
+  TimeStamp mFullscreenChangeStartTime;
   FullscreenTransitionDuration mDuration;
   Stage mStage;
   bool mFullscreen;
@@ -5951,6 +5952,7 @@ FullscreenTransitionTask::Run()
                                          mDuration.mFadeIn, mTransitionData,
                                          this);
   } else if (stage == eToggleFullscreen) {
+    mFullscreenChangeStartTime = TimeStamp::Now();
     if (MOZ_UNLIKELY(mWindow->mFullScreen != mFullscreen)) {
       // This could happen in theory if several fullscreen requests in
       // different direction happen continuously in a short time. We
@@ -5987,6 +5989,8 @@ FullscreenTransitionTask::Run()
       Preferences::GetUint("full-screen-api.transition.timeout", 500);
     mTimer->Init(observer, timeout, nsITimer::TYPE_ONE_SHOT);
   } else if (stage == eAfterToggle) {
+    Telemetry::AccumulateTimeDelta(Telemetry::FULLSCREEN_TRANSITION_BLACK_MS,
+                                   mFullscreenChangeStartTime);
     mWidget->PerformFullscreenTransition(nsIWidget::eAfterFullscreenToggle,
                                          mDuration.mFadeOut, mTransitionData,
                                          this);
