c4fdc1031105e6a530fd43074d9dc8bd71abbbf9
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-05-19 11:01:03 +0100

    Bug 1274007 - Firefox Refresh shouldn't show the 'Firefox on Windows 10' start page when you refresh on Windows 10, r=MattN
    MozReview-Commit-ID: 68CaJR5IsE1

--- browser/components/migration/FirefoxProfileMigrator.js
+++ browser/components/migration/FirefoxProfileMigrator.js
@@ -29,6 +29,8 @@ XPCOMUtils.defineLazyModuleGetter(this, "FileUtils",
                                   "resource://gre/modules/FileUtils.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "ProfileAge",
                                   "resource://gre/modules/ProfileAge.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "AppConstants",
+                                  "resource://gre/modules/AppConstants.jsm");
 
 
 function FirefoxProfileMigrator() {
@@ -148,6 +150,11 @@ FirefoxProfileMigrator.prototype._getResourcesInternal = function(sourceProfileD
           // session with the "what's new" page:
           Services.prefs.setCharPref("browser.startup.homepage_override.mstone", mstone);
           Services.prefs.setCharPref("browser.startup.homepage_override.buildID", buildID);
+          // Also set the Windows 10 pref to avoid the win10 intro page to show up
+          // on startup.
+          if (AppConstants.isPlatformAndVersionAtLeast("win", "10")) {
+            Services.prefs.setBoolPref("browser.usedOnWindows10", true);
+          }
           // It's too early in startup for the pref service to have a profile directory,
           // so we have to manually tell it where to save the prefs file.
           let newPrefsFile = currentProfileDir.clone();
