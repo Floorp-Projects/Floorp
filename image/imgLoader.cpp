71b02a10b069d5bcb0b9851804d2527eb3bc12c1
Jonathan Watt <jwatt@jwatt.org>
2016-05-19 13:31:15 +0100

    Bug 1275229 - Stop using do_CreateInstance("@mozilla.org/image/loader;1") to create gecko internal imgLoader instances. r=tikkel

--- image/imgLoader.cpp
+++ image/imgLoader.cpp
@@ -11,6 +11,7 @@
  #include "mozilla/ChaosMode.h"
 
 #include "ImageLogging.h"
+#include "nsImageModule.h"
 #include "nsPrintfCString.h"
 #include "imgLoader.h"
 #include "imgRequestProxy.h"
@@ -1136,11 +1137,25 @@ NS_IMPL_ISUPPORTS(imgLoader, imgILoader, nsIContentSniffer, imgICache,
 static imgLoader* gSingleton = nullptr;
 static imgLoader* gPBSingleton = nullptr;
 
+/* static */ already_AddRefed<imgLoader>
+imgLoader::CreateImageLoader()
+{
+  // In some cases, such as xpctests, XPCOM modules are not automatically
+  // initialized.  We need to make sure that our module is initialized before
+  // we hand out imgLoader instances and code starts using them.
+  mozilla::image::EnsureModuleInitialized();
+
+  RefPtr<imgLoader> loader = new imgLoader();
+  loader->Init();
+
+  return loader.forget();
+}
+
 imgLoader*
 imgLoader::Singleton()
 {
   if (!gSingleton) {
-    gSingleton = imgLoader::Create().take();
+    gSingleton = CreateImageLoader().take();
   }
   return gSingleton;
 }
@@ -1149,7 +1164,7 @@ imgLoader*
 imgLoader::PBSingleton()
 {
   if (!gPBSingleton) {
-    gPBSingleton = imgLoader::Create().take();
+    gPBSingleton = CreateImageLoader().take();
     gPBSingleton->RespectPrivacyNotifications();
   }
   return gPBSingleton;
