090e2324c663af39f10e6a7a215b0fbc05bc0759
Marco Bonardo <mbonardo@mozilla.com>
2016-05-19 23:50:27 +0200

    Bug 1275878 - Part 2: Replace places-will-close-connection notification with a shutdown blocker. r=adw
    MozReview-Commit-ID: A2sn2OreX4K

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_placesTitleNoUpdate.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_placesTitleNoUpdate.js
@@ -6,67 +6,40 @@
 // private browsing mode.
 "use strict";
 
-Cu.import("resource://gre/modules/Services.jsm");
-Cu.import("resource://gre/modules/PlacesUtils.jsm");
-
 add_task(async function test() {
   const TEST_URL = "http://mochi.test:8888/browser/browser/components/privatebrowsing/test/browser/browser_privatebrowsing_placesTitleNoUpdate.html"
-  const TEST_URI = Services.io.newURI(TEST_URL);
   const TITLE_1 = "Title 1";
   const TITLE_2 = "Title 2";
 
-  function waitForTitleChanged() {
-    return new Promise(resolve => {
-      let historyObserver = {
-        onTitleChanged(uri, pageTitle) {
-          PlacesUtils.history.removeObserver(historyObserver, false);
-          resolve({uri, pageTitle});
-        },
-        onBeginUpdateBatch() {},
-        onEndUpdateBatch() {},
-        onVisit() {},
-        onDeleteURI() {},
-        onClearHistory() {},
-        onPageChanged() {},
-        onDeleteVisits() {},
-        QueryInterface: XPCOMUtils.generateQI([Ci.nsINavHistoryObserver])
-      };
+  await PlacesUtils.history.clear();
 
-      PlacesUtils.history.addObserver(historyObserver);
-    });
-  }
-
-  await PlacesTestUtils.clearHistory();
-
-  let tabToClose = gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser, TEST_URL);
-  await waitForTitleChanged();
-  is(PlacesUtils.history.getPageTitle(TEST_URI), TITLE_1, "The title matches the orignal title after first visit");
-
-  let place = {
-    uri: TEST_URI,
-    title: TITLE_2,
-    visits: [{
-      visitDate: Date.now() * 1000,
-      transitionType: Ci.nsINavHistoryService.TRANSITION_LINK
-    }]
-  };
-  PlacesUtils.asyncHistory.updatePlaces(place, {
-    handleError: () => ok(false, "Unexpected error in adding visit."),
-    handleResult() { },
-    handleCompletion() {}
+  let promiseTitleChanged = PlacesTestUtils.waitForNotification(
+    "onTitleChanged", (uri, title) => uri.spec == TEST_URL, "history");
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, TEST_URL);
+  registerCleanupFunction(async () => {
+    await BrowserTestUtils.removeTab(tab);
   });
+  info("Wait for a title change notification.");
+  await promiseTitleChanged;
+  is((await PlacesUtils.history.fetch(TEST_URL)).title, TITLE_1,
+     "The title matches the orignal title after first visit");
 
-  await waitForTitleChanged();
-  is(PlacesUtils.history.getPageTitle(TEST_URI), TITLE_2, "The title matches the updated title after updating visit");
+  promiseTitleChanged = PlacesTestUtils.waitForNotification(
+    "onTitleChanged", (uri, title) => uri.spec == TEST_URL, "history");
+  await PlacesTestUtils.addVisits({ uri: TEST_URL, title: TITLE_2 });
+  info("Wait for a title change notification.");
+  await promiseTitleChanged;
+  is((await PlacesUtils.history.fetch(TEST_URL)).title, TITLE_2,
+     "The title matches the orignal title after updating visit");
 
   let privateWin = await BrowserTestUtils.openNewBrowserWindow({private: true});
-  await BrowserTestUtils.browserLoaded(privateWin.gBrowser.addTab(TEST_URL).linkedBrowser);
-
-  is(PlacesUtils.history.getPageTitle(TEST_URI), TITLE_2, "The title remains the same after visiting in private window");
-  await PlacesTestUtils.clearHistory();
-
-  // Cleanup
-  BrowserTestUtils.closeWindow(privateWin);
-  gBrowser.removeTab(tabToClose);
+  registerCleanupFunction(async () => {
+    await BrowserTestUtils.closeWindow(privateWin);
+  });
+  await BrowserTestUtils.openNewForegroundTab(privateWin.gBrowser, TEST_URL);
+  // Wait long enough to be sure history didn't set a title.
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  is((await PlacesUtils.history.fetch(TEST_URL)).title, TITLE_2,
+     "The title remains the same after visiting in private window");
 });
 
