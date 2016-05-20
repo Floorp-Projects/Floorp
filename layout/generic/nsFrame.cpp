27ac4d1ec6ae2891d4706ef0b2f3de18f2e9296c
Daniel Holbert <dholbert@cs.stanford.edu>
2016-05-20 15:05:31 -0700

    Bug 1272983 part 1: If GetUsed{Margin,Padding} get called for a value that can't be resolved & isn't cached, assert & return 0-sized nsMargin. r=dbaron
    (This restores the behavior we had before bug 1269901 landed, for this scenario.)
    
    MozReview-Commit-ID: Ir1TNM1CgwU

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -967,7 +967,11 @@ nsIFrame::GetUsedMargin() const
   if (m) {
     margin = *m;
   } else {
-    StyleMargin()->GetMarginNoPercentage(margin);
+    if (!StyleMargin()->GetMargin(margin)) {
+      // If we get here, our caller probably shouldn't be calling us...
+      NS_ERROR("Returning bogus 0-sized margin, because this margin "
+               "depends on layout & isn't cached!");
+    }
   }
   return margin;
 }
@@ -1041,7 +1045,11 @@ nsIFrame::GetUsedPadding() const
   if (p) {
     padding = *p;
   } else {
-    StylePadding()->GetPaddingNoPercentage(padding);
+    if (!StylePadding()->GetPadding(padding)) {
+      // If we get here, our caller probably shouldn't be calling us...
+      NS_ERROR("Returning bogus 0-sized padding, because this padding "
+               "depends on layout & isn't cached!");
+    }
   }
   return padding;
 }
