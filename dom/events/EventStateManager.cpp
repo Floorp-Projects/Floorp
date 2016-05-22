51163cb57694af3b972188a2e2622fa7689dc38a
Xidorn Quan <me@upsuper.org>
2016-05-23 09:03:32 +1000

    Bug 931445 part 3 - Reset synth centering pointer when unlocking pointer. r=smaug
    MozReview-Commit-ID: LcStCmm4iRV
    
    --HG--
    extra : source : e827730f9b70078e275da1163adc6746fe792399

--- dom/events/EventStateManager.cpp
+++ dom/events/EventStateManager.cpp
@@ -4398,6 +4398,9 @@ EventStateManager::SetPointerLock(nsIWidget* aWidget,
     // pre-pointerlock position, so that the synthetic mouse event reports
     // no movement.
     sLastRefPoint = mPreLockPoint;
+    // Reset SynthCenteringPoint to invalid so that next time we start
+    // locking pointer, it has its initial value.
+    sSynthCenteringPoint = kInvalidRefPoint;
     if (aWidget) {
       aWidget->SynthesizeNativeMouseMove(
         mPreLockPoint + aWidget->WidgetToScreenOffset(), nullptr);
