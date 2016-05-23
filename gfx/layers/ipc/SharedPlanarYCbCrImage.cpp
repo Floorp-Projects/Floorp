679443aab09c807fc7a621b761f9a5e334daa105
Sotaro Ikeda <sotaro.ikeda.g@gmail.com>
2016-05-22 17:37:43 -0700

    Bug 1274178 - Deliver picture origin r=nical

--- gfx/layers/ipc/SharedPlanarYCbCrImage.cpp
+++ gfx/layers/ipc/SharedPlanarYCbCrImage.cpp
@@ -151,6 +151,7 @@ SharedPlanarYCbCrImage::AdoptData(const Data &aData)
   }
   mData = aData;
   mSize = aData.mPicSize;
+  mOrigin = gfx::IntPoint(aData.mPicX, aData.mPicY);
 
   uint8_t *base = GetBuffer();
   uint32_t yOffset = aData.mYChannel - base;
@@ -221,6 +222,7 @@ SharedPlanarYCbCrImage::Allocate(PlanarYCbCrData& aData)
   // shmem.
   mBufferSize = ImageDataSerializer::ComputeYCbCrBufferSize(mData.mYSize, mData.mCbCrSize);
   mSize = mData.mPicSize;
+  mOrigin = gfx::IntPoint(aData.mPicX, aData.mPicY);
 
   mTextureClient->Unlock();
 
