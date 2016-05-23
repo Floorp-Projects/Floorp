5a87aaf54ec4a06a5f80a19f5b9e13ce88a82b82
David Anderson <danderson@mozilla.com>
2016-05-23 00:28:03 -0700

    Remove TabParent use of CompositorBridgeParent. (bug 1274149 part 4, r=mattwoodrow)

--- gfx/ipc/GPUProcessManager.cpp
+++ gfx/ipc/GPUProcessManager.cpp
@@ -78,5 +78,23 @@ GPUProcessManager::DeallocateLayerTreeId(uint64_t aLayersId)
   CompositorBridgeParent::DeallocateLayerTreeId(aLayersId);
 }
 
+void
+GPUProcessManager::RequestNotifyLayerTreeReady(uint64_t aLayersId, CompositorUpdateObserver* aObserver)
+{
+  CompositorBridgeParent::RequestNotifyLayerTreeReady(aLayersId, aObserver);
+}
+
+void
+GPUProcessManager::RequestNotifyLayerTreeCleared(uint64_t aLayersId, CompositorUpdateObserver* aObserver)
+{
+  CompositorBridgeParent::RequestNotifyLayerTreeCleared(aLayersId, aObserver);
+}
+
+void
+GPUProcessManager::SwapLayerTreeObservers(uint64_t aLayer, uint64_t aOtherLayer)
+{
+  CompositorBridgeParent::SwapLayerTreeObservers(aLayer, aOtherLayer);
+}
+
 } // namespace gfx
 } // namespace mozilla
