86005a71f89948266888aa1a9fe2ee2b6fd329cd
Andrzej Hunt <ahunt@mozilla.com>
2016-05-23 13:29:15 -0700

    Bug 932335 - Pre 3: Make getPreferredSizeFaviconForPage private r=me
    We only use this in getPreferredIconForHomeScreenShortcut, we don't
    need to expose this elsewhere.
    
    It might be nice to move it directly into getPreferredIconForHomeScreenShortcut,
    but the separation allows for some more useful documentation.
    
    MozReview-Commit-ID: E92aLMgp2tl

--- mobile/android/base/java/org/mozilla/gecko/favicons/Favicons.java
+++ mobile/android/base/java/org/mozilla/gecko/favicons/Favicons.java
@@ -595,7 +595,7 @@ public class Favicons {
      * @param url page URL to get a large favicon image for.
      * @param onFaviconLoadedListener listener to call back with the result.
      */
-    public static void getPreferredSizeFaviconForPage(Context context, String url, String iconURL, OnFaviconLoadedListener onFaviconLoadedListener) {
+    private static void getPreferredSizeFaviconForPage(Context context, String url, String iconURL, OnFaviconLoadedListener onFaviconLoadedListener) {
         int preferredSize = GeckoAppShell.getPreferredIconSize();
         loadUncachedFavicon(context, url, iconURL, LoadFaviconTask.FLAG_BYPASS_CACHE_WHEN_DOWNLOADING_ICONS, preferredSize, onFaviconLoadedListener);
     }
