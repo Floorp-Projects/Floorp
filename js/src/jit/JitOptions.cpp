92bd2f2d95e64499a26b6afee40d9711cab66ba4
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2016-05-23 14:27:10 +0000

    Bug 1273955 - Bump the bytecode size limit of Ion to account for increased size of JSScript code length. r=h4writer

--- js/src/jit/JitOptions.cpp
+++ js/src/jit/JitOptions.cpp
@@ -166,7 +166,7 @@ DefaultJitOptions::DefaultJitOptions()
     SET_DEFAULT(osrPcMismatchesBeforeRecompile, 6000);
 
     // The bytecode length limit for small function.
-    SET_DEFAULT(smallFunctionMaxBytecodeLength_, 120);
+    SET_DEFAULT(smallFunctionMaxBytecodeLength_, 130);
 
     // An artificial testing limit for the maximum supported offset of
     // pc-relative jump and call instructions.
