02e5be40231f390e0cceb42983ffd470f5035650
billel <billel.nazir@gmail.com>
2016-05-23 09:56:04 +0200

    Bug 1271577 - FloatingToolbarTextSelection: Remove Log.w() call. r=sebastian
    MozReview-Commit-ID: CQPeFsjJmfk
    
    --HG--
    extra : amend_source : 899352c7894660d068f746b3d64affd5167d14a1

--- mobile/android/base/java/org/mozilla/gecko/text/FloatingToolbarTextSelection.java
+++ mobile/android/base/java/org/mozilla/gecko/text/FloatingToolbarTextSelection.java
@@ -104,8 +104,6 @@ public class FloatingToolbarTextSelection implements TextSelection, GeckoEventLi
 
     @Override
     public void handleMessage(final String event, final JSONObject message) {
-        Log.w("SKDBG", "Received event " + event + " with message: " + message.toString());
-
         ThreadUtils.postToUiThread(new Runnable() {
             @Override
             public void run() {
