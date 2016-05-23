ecf249bd9a24a60750664e086aa550290a924d1e
David Anderson <danderson@mozilla.com>
2016-05-23 00:27:01 -0700

    Add GPUProcessManager to manage access to CompositorBridgeParent. (bug 1274149 part 1, r=mattwoodrow)

---
+++ gfx/ipc/GPUProcessManager.h
@@ -0,0 +1,52 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=99: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef _include_mozilla_gfx_ipc_GPUProcessManager_h_
+#define _include_mozilla_gfx_ipc_GPUProcessManager_h_
+
+#include "base/basictypes.h"
+#include "Units.h"
+
+namespace mozilla {
+namespace layers {
+class CompositorSession;
+class ClientLayerManager;
+} // namespace layers
+namespace widget {
+class CompositorWidgetProxy;
+} // namespace widget
+namespace gfx {
+
+// The GPUProcessManager is a singleton responsible for creating GPU-bound
+// objects that may live in another process. Currently, it provides access
+// to the compositor via CompositorBridgeParent.
+class GPUProcessManager final
+{
+public:
+  static void Initialize();
+  static void Shutdown();
+  static GPUProcessManager* Get();
+
+  ~GPUProcessManager();
+
+  already_AddRefed<layers::CompositorSession> CreateTopLevelCompositor(
+    widget::CompositorWidgetProxy* aProxy,
+    layers::ClientLayerManager* aLayerManager,
+    CSSToLayoutDeviceScale aScale,
+    bool aUseAPZ,
+    bool aUseExternalSurfaceSize,
+    int aSurfaceWidth,
+    int aSurfaceHeight);
+
+private:
+  GPUProcessManager();
+
+  DISALLOW_COPY_AND_ASSIGN(GPUProcessManager);
+};
+
+} // namespace gfx
+} // namespace mozilla
+
+#endif // _include_mozilla_gfx_ipc_GPUProcessManager_h_
