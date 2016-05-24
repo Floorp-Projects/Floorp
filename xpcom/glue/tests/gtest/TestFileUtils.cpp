f91c2dad20227ac84dceb37b2330bc394f0999a9
Andrew McCreight <continuation@gmail.com>
2016-05-24 14:45:44 -0700

    Bug 1273190, part 1 - Fix indentation and mode lines for various xpcom/ files. r=froydnj

--- xpcom/glue/tests/gtest/TestFileUtils.cpp
+++ xpcom/glue/tests/gtest/TestFileUtils.cpp
@@ -1,4 +1,5 @@
-/* vim:set ts=2 sw=2 sts=2 et: */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
