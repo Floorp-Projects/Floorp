e9c91bddf6aceb69d98658e10ddc86b9067a8ff3
Andreas Pehrson <pehrsons@gmail.com>
2016-05-24 13:23:50 +0200

    Bug 1274221 - Ensure MediaStreamListeners are always notified of created and ended tracks. r=ctai, r=jesup
    This means that when a MediaStreamListener is added to a stream, we'll call
    NotifyQueuedTrackChanges with TRACK_EVENT_CREATE for all tracks that already
    exist.
    
    Likewise, we'll call NotifyQueuedTrackChanges with TRACK_EVENT_ENDED for all
    tracks that exist and have ended.
    
    This fixes potential race conditions where a track was created and/or ended
    before the listener was asynchronously added.
    
    MozReview-Commit-ID: G3juhfiZMtg
    
    --HG--
    extra : rebase_source : 6f4fe68b888b6dd0d9ee370b6a390ff17da5f1de
    extra : source : 70e8c5a785a9866e6205b54bd1c45faaf834717d

--- dom/media/MediaStreamGraph.h
+++ dom/media/MediaStreamGraph.h
@@ -1439,6 +1439,8 @@ public:
   {
     return mInputs.Length();
   }
+  virtual MediaStream* GetInputStreamFor(TrackID aTrackID) { return nullptr; }
+  virtual TrackID GetInputTrackIDFor(TrackID aTrackID) { return TRACK_NONE; }
   void DestroyImpl() override;
   /**
    * This gets called after we've computed the blocking states for all
