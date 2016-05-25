5ba478e546438bd67f0aa9c020dd17be0625c446
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-05-25 10:07:42 +0200

    Backed out changeset 8b76fb7a3e31 (bug 1270686) for bustage on a CLOSED TREE

--- toolkit/crashreporter/nsExceptionHandler.cpp
+++ toolkit/crashreporter/nsExceptionHandler.cpp
@@ -1437,17 +1437,6 @@ ChildFilter(void* context)
   return result;
 }
 
-#ifdef NIGHTLY_BUILD
-static void DllLoadCallback(DllLoadInfo& info)
-{
-  nsAutoCString note;
-  note.AppendPrintf("%s-%p-%x\n", info.name, info.lpBaseOfDll, info.SizeOfImage);
-
-  CrashReporter::AppendAppNotesToCrashReport(note);
-}
-#endif
-
-
 nsresult SetExceptionHandler(nsIFile* aXREDirectory,
                              bool force/*=false*/)
 {
@@ -1684,10 +1673,6 @@ nsresult SetExceptionHandler(nsIFile* aXREDirectory,
 #endif
 
   mozalloc_set_oom_abort_handler(AnnotateOOMAllocationSize);
-
-#if defined(XP_WIN) && defined(NIGHTLY_BUILD)
-  RegisterDllLoadCallback(DllLoadCallback);
-#endif
 
   return NS_OK;
 }
