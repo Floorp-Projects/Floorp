40afbc460a3bf78b7da3356d688a4d62acacee74
Yoshi Huang <allstars.chh@mozilla.com>
2016-05-25 16:19:57 +0800

    Bug 1274461 - Part 1: undoCloseTab should preserve userContextId. r=mikedeboer

--- browser/components/sessionstore/test/browser_sessionStoreContainer.js
+++ browser/components/sessionstore/test/browser_sessionStoreContainer.js
@@ -4,7 +4,7 @@
 
 add_task(function* () {
   for (let i = 0; i < 3; ++i) {
-    let tab = gBrowser.addTab("http://example.com/", {userContextId: i});
+    let tab = gBrowser.addTab("http://example.com/", { userContextId: i });
     let browser = tab.linkedBrowser;
 
     yield promiseBrowserLoaded(browser);
@@ -26,7 +26,7 @@ add_task(function* () {
 });
 
 add_task(function* () {
-  let tab = gBrowser.addTab("http://example.com/", {userContextId: 1});
+  let tab = gBrowser.addTab("http://example.com/", { userContextId: 1 });
   let browser = tab.linkedBrowser;
 
   yield promiseBrowserLoaded(browser);
@@ -37,7 +37,7 @@ add_task(function* () {
   let browser2 = tab2.linkedBrowser;
   yield promiseTabRestored(tab2)
 
-  yield ContentTask.spawn(browser2, { expectedId: 1}, function* (args) {
+  yield ContentTask.spawn(browser2, { expectedId: 1 }, function* (args) {
     Assert.equal(docShell.getOriginAttributes().userContextId,
                  args.expectedId,
                  "The docShell has the correct userContextId");
@@ -47,3 +47,22 @@ add_task(function* () {
   yield promiseRemoveTab(tab2);
 });
 
+add_task(function* () {
+  let tab = gBrowser.addTab("http://example.com/", { userContextId: 1 });
+  let browser = tab.linkedBrowser;
+
+  yield promiseBrowserLoaded(browser);
+
+  gBrowser.removeTab(tab);
+
+  let tab2 = ss.undoCloseTab(window, 0);
+  Assert.equal(tab2.getAttribute("usercontextid"), 1);
+  yield promiseTabRestored(tab2);
+  yield ContentTask.spawn(tab2.linkedBrowser, { expectedId: 1 }, function* (args) {
+    Assert.equal(docShell.getOriginAttributes().userContextId,
+                 args.expectedId,
+                 "The docShell has the correct userContextId");
+  });
+
+  yield promiseRemoveTab(tab2);
+});
