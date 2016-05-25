80e7c64b97ebe12b5fc3b947849cfec34885fc47
Alexander Surkov <surkov.alexander@gmail.com>
2016-05-25 15:48:45 -0400

    Bug 1268069 - Embeded object collection may be not updated on removals. r=yzen, a=ritu

--- accessible/base/EventTree.cpp
+++ accessible/base/EventTree.cpp
@@ -157,10 +157,7 @@ TreeMutation::Done()
     mParent->mChildren[idx]->mStateFlags |= Accessible::eGroupInfoDirty;
   }
 
-  if (mStartIdx < mParent->mChildren.Length() - 1) {
-    mParent->mEmbeddedObjCollector = nullptr;
-  }
-
+  mParent->mEmbeddedObjCollector = nullptr;
   mParent->mStateFlags |= mStateFlagsCopy & Accessible::eKidsMutating;
 
 #ifdef DEBUG
