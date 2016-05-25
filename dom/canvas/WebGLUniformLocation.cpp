c3843ecebdaefbc87703cfce65a3fd2d11af0fc7
JerryShih <hshih@mozilla.com>
2016-05-25 16:27:41 +0200

    Bug 1248580 - strip the uploading element num according to the uniform array size. r=jgilbert a=ritu
    --HG--
    extra : source : 7e26c1d13046e6e69de1a47d7e9026ff645a0f5e
    extra : intermediate-source : ccd86521d8457aa09673b22b85f2d4f2c6e1feca

--- dom/canvas/WebGLUniformLocation.cpp
+++ dom/canvas/WebGLUniformLocation.cpp
@@ -16,10 +16,13 @@ namespace mozilla {
 
 WebGLUniformLocation::WebGLUniformLocation(WebGLContext* webgl,
                                            const webgl::LinkedProgramInfo* linkInfo,
-                                           GLuint loc, const WebGLActiveInfo* activeInfo)
+                                           GLuint loc,
+                                           size_t arrayIndex,
+                                           const WebGLActiveInfo* activeInfo)
     : WebGLContextBoundObject(webgl)
     , mLinkInfo(linkInfo)
     , mLoc(loc)
+    , mArrayIndex(arrayIndex)
     , mActiveInfo(activeInfo)
 { }
 
