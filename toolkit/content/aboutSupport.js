cbfaaa6b4bfdb4ae8f5312264b181f0c01ba2ac3
Felipe Gomes <felipc@gmail.com>
2016-05-25 18:51:51 -0300

    Bug 1275040 - Block e10s from being activated in OS X 10.6 - 10.8 in all channels. r=jimm
    MozReview-Commit-ID: HOXJgZ4b10x

--- toolkit/content/aboutSupport.js
+++ toolkit/content/aboutSupport.js
@@ -65,7 +65,7 @@ var snapshotFormatters = {
         break;
 
       case 10:
-        statusText = "Windows XP";
+        statusText = (Services.appinfo.OS == "Darwin" ? "OS X 10.6 - 10.8" : "Windows XP");
         break;
     }
 
