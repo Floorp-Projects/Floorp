eff051d1b97540090a56d9f1e870b10a6bb04f1c
Jan Henning <jh+bugzilla@buttercookie.de>
2016-05-26 17:40:19 +0200

    Bug 1251362 - Part 19 - Remove code and resources for the old Recent Tabs panel. r=liuche
    MozReview-Commit-ID: FQJ7j8YlV6E

--- mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
+++ mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
@@ -10,6 +10,7 @@ import android.app.DownloadManager;
 import android.os.Environment;
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
+
 import org.json.JSONArray;
 import org.mozilla.gecko.adjust.AdjustHelperInterface;
 import org.mozilla.gecko.annotation.RobocopTarget;
@@ -2528,7 +2529,7 @@ public class BrowserApp extends GeckoApp
                 // history, and loading new pages, see Bug 1268887
                 panelId = tab.getMostRecentHomePanel();
                 panelRestoreData = tab.getMostRecentHomePanelData();
-            } else if (panelId.equals(HomeConfig.getIdForBuiltinPanelType(PanelType.RECENT_TABS))) {
+            } else if (panelId.equals(HomeConfig.getIdForBuiltinPanelType(PanelType.DEPRECATED_RECENT_TABS))) {
                 // Redirect to the Combined History panel.
                 panelId = HomeConfig.getIdForBuiltinPanelType(PanelType.COMBINED_HISTORY);
                 panelRestoreData = new Bundle();
