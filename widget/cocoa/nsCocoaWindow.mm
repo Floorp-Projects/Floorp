28f3f25f0a39743d942ee14567ed63ee2fad13b2
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2016-05-27 00:05:10 +0900

    Backed out changeset d89f93a40f78 (bug 1212527) due to static analysis bustage
    MozReview-Commit-ID: 79SduJl9zcs

--- widget/cocoa/nsCocoaWindow.mm
+++ widget/cocoa/nsCocoaWindow.mm
@@ -3401,16 +3401,6 @@ static const NSString* kStateCollectionBehavior = @"collectionBehavior";
   return mBackgroundColor;
 }
 
-- (void)setTemporaryBackgroundColor
-{
-  [super setBackgroundColor:[NSColor whiteColor]];
-}
-
-- (void)restoreBackgroundColor
-{
-  [super setBackgroundColor:mBackgroundColor];
-}
-
 - (void)setTitlebarNeedsDisplayInRect:(NSRect)aRect
 {
   [self setTitlebarNeedsDisplayInRect:aRect sync:NO];
