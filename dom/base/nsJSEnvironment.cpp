5a2a55c6ccc6c0e50bb1b39eacbaecfbbe332637
Boris Zbarsky <bzbarsky@mit.edu>
2016-05-26 19:39:03 -0400

    Bug 1275704 part 3.  Remove the nsXPConnect::ReportAllJSExceptions mechanism, since it's dead code now.  r=bholley

--- dom/base/nsJSEnvironment.cpp
+++ dom/base/nsJSEnvironment.cpp
@@ -2384,13 +2384,6 @@ mozilla::dom::StartupJSEnvironment()
 }
 
 static void
-ReportAllJSExceptionsPrefChangedCallback(const char* aPrefName, void* aClosure)
-{
-  bool reportAll = Preferences::GetBool(aPrefName, false);
-  nsContentUtils::XPConnect()->SetReportAllJSExceptions(reportAll);
-}
-
-static void
 SetMemoryHighWaterMarkPrefChangedCallback(const char* aPrefName, void* aClosure)
 {
   int32_t highwatermark = Preferences::GetInt(aPrefName, 128);
@@ -2543,9 +2536,6 @@ nsJSContext::EnsureStatics()
   JS::SetAsmJSCacheOps(sRuntime, &asmJSCacheOps);
 
   // Set these global xpconnect options...
-  Preferences::RegisterCallbackAndCall(ReportAllJSExceptionsPrefChangedCallback,
-                                       "dom.report_all_js_exceptions");
-
   Preferences::RegisterCallbackAndCall(SetMemoryHighWaterMarkPrefChangedCallback,
                                        "javascript.options.mem.high_water_mark");
 
