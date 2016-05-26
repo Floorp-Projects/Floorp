9ccf578d90353f00e0ce1688df1574b670772bab
Chenxia Liu <liuche@mozilla.com>
2016-05-26 14:38:40 -0700

    Bug 1270641 - Choose winner of expanded-firstrun. r=ahunt, a=ritu
    MozReview-Commit-ID: 7Ay3YDR4KgU

--- mobile/android/base/java/org/mozilla/gecko/util/Experiments.java
+++ mobile/android/base/java/org/mozilla/gecko/util/Experiments.java
@@ -35,10 +35,6 @@ public class Experiments {
     public static final String CONTENT_NOTIFICATIONS_8AM = "content-notifications-8am";
     public static final String CONTENT_NOTIFICATIONS_5PM = "content-notifications-5pm";
 
-    // Onboarding: "Features and Story". These experiments are determined
-    // on the client, they are not part of the server config.
-    public static final String ONBOARDING2_A = "onboarding2-a"; // Control: Single (blue) welcome screen
-    public static final String ONBOARDING2_B = "onboarding2-b"; // 4 static Feature slides
     public static final String ONBOARDING2_C = "onboarding2-c"; // 4 static + 1 clickable (Data saving) Feature slides
 
     // Synchronizing the catalog of downloadable content from Kinto
@@ -92,15 +88,7 @@ public class Experiments {
      * @return returns value for experiment or false if experiment does not exist.
      */
     public static boolean isInExperimentLocal(Context context, String experiment) {
-        if (SwitchBoard.isInBucket(context, 0, 33)) {
-            return Experiments.ONBOARDING2_A.equals(experiment);
-        } else if (SwitchBoard.isInBucket(context, 33, 66)) {
-            return Experiments.ONBOARDING2_B.equals(experiment);
-        } else if (SwitchBoard.isInBucket(context, 66, 100)) {
-            return Experiments.ONBOARDING2_C.equals(experiment);
-        } else {
-            return false;
-        }
+        return Experiments.ONBOARDING2_C.equals(experiment);
     }
 
     /**
