cb6d29b0b767531d1c89bae267e26d3af5d94bf6
Julian Hector <julian.r.hector@gmail.com>
2016-05-27 15:58:51 +0200

    Bug 1275785 - Add sys_bind to seccomp whitelist. r=jld
    --HG--
    extra : rebase_source : 90d403a3b21547ff7f280b2bff7746f4b8e32fe3

--- security/sandbox/linux/SandboxFilter.cpp
+++ security/sandbox/linux/SandboxFilter.cpp
@@ -438,6 +438,7 @@ public:
     case SYS_SOCKET: // DANGEROUS
     case SYS_CONNECT: // DANGEROUS
     case SYS_ACCEPT:
+    case SYS_BIND:
     case SYS_SETSOCKOPT:
     case SYS_GETSOCKNAME:
     case SYS_GETPEERNAME:
