99f100736a60a628c3bdefcdf4e993b51ac23e21
Boris Zbarsky <bzbarsky@mit.edu>
2016-05-27 13:28:14 -0400

    Bug 1276138.  Remove the current/safe JSContext getters from nsScriptSecurityManager.  r=mrbkap

--- caps/nsScriptSecurityManager.h
+++ caps/nsScriptSecurityManager.h
@@ -58,10 +58,6 @@ public:
     static nsSystemPrincipal*
     SystemPrincipalSingletonConstructor();
 
-    JSContext* GetCurrentJSContext();
-
-    JSContext* GetSafeJSContext();
-
     /**
      * Utility method for comparing two URIs.  For security purposes, two URIs
      * are equivalent if their schemes, hosts, and ports (if any) match.  This
