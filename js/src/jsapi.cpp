e6f1c5655a6a061fbda19cc11b127b9e1026fb08
Jan de Mooij <jdemooij@mozilla.com>
2016-05-28 10:33:32 +0200

    Bug 1276162 part 1 - Stop skipping contexts in GetScriptedCallerActivationFast. r=bz

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -6455,9 +6455,6 @@ GetScriptedCallerActivationFast(JSContext* cx, Activation** activation)
 {
     ActivationIterator activationIter(cx->runtime());
 
-    while (!activationIter.done() && activationIter->cx() != cx)
-        ++activationIter;
-
     if (activationIter.done()) {
         *activation = nullptr;
         return true;
