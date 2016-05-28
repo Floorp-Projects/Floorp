6fa361a0a3a0819ea95cff830d9ee1544c1ca3dd
Lee Salzman <lsalzman@mozilla.com>
2016-05-27 21:12:25 -0400

    Bug 1255062 - disallow multiple moveTos in SkPath::isRectContour. r=ethlin

--- gfx/skia/skia/src/core/SkPath.cpp
+++ gfx/skia/skia/src/core/SkPath.cpp
@@ -504,6 +504,9 @@ bool SkPath::isRectContour(bool allowPartial, int* currVerb, const SkPoint** pts
                     *currVerb -= 1;  // try move again afterwards
                     goto addMissingClose;
                 }
+                if (pts != *ptsPtr) {
+                    return false;
+                }
                 last = *pts++;
                 closedOrMoved = true;
                 break;
