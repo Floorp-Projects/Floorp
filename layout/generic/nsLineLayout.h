92767500551fc700e4b5eca7ad0278208de63f71
Xidorn Quan <me@upsuper.org>
2016-05-30 09:24:18 +1000

    Bug 1268342 part 1 - Remove useless constructor of PerFrameData. r=jfkthame
    MozReview-Commit-ID: IL3iq4FFEaY

--- layout/generic/nsLineLayout.h
+++ layout/generic/nsLineLayout.h
@@ -423,13 +423,6 @@ protected:
   friend struct PerFrameData;
   struct PerFrameData
   {
-    explicit PerFrameData(mozilla::WritingMode aWritingMode)
-      : mBounds(aWritingMode)
-      , mMargin(aWritingMode)
-      , mBorderPadding(aWritingMode)
-      , mOffsets(aWritingMode)
-    {}
-
     // link to next/prev frame in same span
     PerFrameData* mNext;
     PerFrameData* mPrev;
