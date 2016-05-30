27d64c914e0291db45a576fe48f2cd1982db57f4
Jonathan Watt <jwatt@jwatt.org>
2016-05-30 16:26:30 +0100

    Bug 1278424 - Remove some code from gfxQuartzSurface. r=eflores
    --HG--
    extra : rebase_source : b382286844a47ef6e5ffdf83435cd0c36b767f31

--- gfx/thebes/gfxQuartzSurface.h
+++ gfx/thebes/gfxQuartzSurface.h
@@ -17,11 +17,9 @@ class gfxImageSurface;
 
 class gfxQuartzSurface : public gfxASurface {
 public:
-    gfxQuartzSurface(const gfxSize& size, gfxImageFormat format);
-    gfxQuartzSurface(CGContextRef context, const gfxSize& size);
+    gfxQuartzSurface(const mozilla::gfx::IntSize&, gfxImageFormat format);
     gfxQuartzSurface(CGContextRef context, const mozilla::gfx::IntSize& size);
     gfxQuartzSurface(cairo_surface_t *csurf, const mozilla::gfx::IntSize& aSize);
-    gfxQuartzSurface(unsigned char *data, const gfxSize& size, long stride, gfxImageFormat format);
     gfxQuartzSurface(unsigned char *data, const mozilla::gfx::IntSize& size, long stride, gfxImageFormat format);
 
     virtual ~gfxQuartzSurface();
@@ -39,7 +37,7 @@ protected:
     void MakeInvalid();
 
     CGContextRef mCGContext;
-    gfxSize      mSize;
+    mozilla::gfx::IntSize mSize;
 };
 
 #endif /* GFX_QUARTZSURFACE_H */
