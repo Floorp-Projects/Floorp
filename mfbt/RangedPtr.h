2e9a830904a355ae7cc6154307fe5c9bd45efb72
Boris Chiou <boris.chiou@gmail.com>
2016-05-30 13:58:13 +0800

    Bug 1276573 - Add a new constructor for Range<T>. r=Waldo
    These new constructor accepts two RangedPtr<T> arguments.
    
    MozReview-Commit-ID: 8a3bYserLMr

--- mfbt/RangedPtr.h
+++ mfbt/RangedPtr.h
@@ -117,6 +117,12 @@ public:
 
   explicit operator bool() const { return mPtr != nullptr; }
 
+  void checkIdenticalRange(const RangedPtr<T>& aOther) const
+  {
+    MOZ_ASSERT(mRangeStart == aOther.mRangeStart);
+    MOZ_ASSERT(mRangeEnd == aOther.mRangeEnd);
+  }
+
   /*
    * You can only assign one RangedPtr into another if the two pointers have
    * the same valid range:
@@ -129,8 +135,7 @@ public:
    */
   RangedPtr<T>& operator=(const RangedPtr<T>& aOther)
   {
-    MOZ_ASSERT(mRangeStart == aOther.mRangeStart);
-    MOZ_ASSERT(mRangeEnd == aOther.mRangeEnd);
+    checkIdenticalRange(aOther);
     mPtr = aOther.mPtr;
     checkSanity();
     return *this;
