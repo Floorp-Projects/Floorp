642cd22f807f1b53825047940519872918ab75aa
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-05-30 12:54:56 +0100

    Bug 1276117 - part 2: fix the URL bar state when loading about:home from a new tab page, r=mikedeboer, a=gchang
    MozReview-Commit-ID: FADnX8bymNE

--- browser/base/content/test/urlbar/browser_urlbarAboutHomeLoading.js
+++ browser/base/content/test/urlbar/browser_urlbarAboutHomeLoading.js
@@ -41,3 +41,26 @@ add_task(function* clearURLBarAfterParentProcessURLInExistingTab() {
   yield BrowserTestUtils.removeTab(tab);
 });
 
+/**
+ * Load about:home directly from an about:newtab page. Because it is an
+ * 'initial' page, we need to treat this specially if the user actually
+ * loads a page like this from the URL bar.
+ */
+add_task(function* clearURLBarAfterManuallyLoadingAboutHome() {
+  let promiseTabOpenedAndSwitchedTo = BrowserTestUtils.switchTab(gBrowser, () => {});
+  // This opens about:newtab:
+  BrowserOpenTab();
+  let tab = yield promiseTabOpenedAndSwitchedTo;
+  is(gURLBar.value, "", "URL bar should be empty");
+  is(tab.linkedBrowser.userTypedValue, null, "userTypedValue should be null");
+
+  gURLBar.value = "about:home";
+  gURLBar.select();
+  let aboutHomeLoaded = BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false, "about:home");
+  EventUtils.sendKey("return");
+  yield aboutHomeLoaded;
+
+  is(gURLBar.value, "", "URL bar should be empty");
+  is(tab.linkedBrowser.userTypedValue, null, "userTypedValue should be null");
+  yield BrowserTestUtils.removeTab(tab);
+});
