9f259a7f7a26d2601131de2c1be880837cbb919a
Dan Minor <dminor@mozilla.com>
2016-05-30 05:36:11 -0400

    Bug 1265408 - Avoid subnormals in IIRFilter; r=karlt
    MozReview-Commit-ID: F4NUE8834tM

--- dom/media/webaudio/blink/IIRFilter.cpp
+++ dom/media/webaudio/blink/IIRFilter.cpp
@@ -98,7 +98,13 @@ void IIRFilter::process(const float* sourceP, float* destP, size_t framesToProce
 
         m_bufferIndex = (m_bufferIndex + 1) & (kBufferLength - 1);
 
-        destP[n] = yn;
+        // Avoid introducing a stream of subnormals
+        // TODO: Remove this code when Bug 1157635 is fixed.
+        if (fabs(yn) >= FLT_MIN) {
+            destP[n] = yn;
+        } else {
+            destP[n] = 0.0;
+        }
     }
 }
 
