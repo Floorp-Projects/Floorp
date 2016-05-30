790ba2e9a62ab0ceeaed4590dbdaf67cad7ada4a
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-05-30 15:05:02 +0200

    Backed out changeset 1cc3aabd11e8 (bug 694570)

--- widget/gtk/compat/gdk/gdkx.h
+++ widget/gtk/compat/gdk/gdkx.h
@@ -5,14 +5,9 @@
 #ifndef GDKX_WRAPPER_H
 #define GDKX_WRAPPER_H
 
-#include <gtk/gtkversion.h>
-
 #define gdk_x11_window_foreign_new_for_display gdk_x11_window_foreign_new_for_display_
 #define gdk_x11_window_lookup_for_display gdk_x11_window_lookup_for_display_
 #define gdk_x11_window_get_xid gdk_x11_window_get_xid_
-#if !GTK_CHECK_VERSION(2,24,0)
-#define gdk_x11_set_sm_client_id gdk_x11_set_sm_client_id_
-#endif
 #include_next <gdk/gdkx.h>
 #undef gdk_x11_window_foreign_new_for_display
 #undef gdk_x11_window_lookup_for_display
@@ -40,12 +35,4 @@ gdk_x11_window_get_xid(GdkWindow *window)
 #define GDK_IS_X11_DISPLAY(a)   (true)
 #endif
 
-#if !GTK_CHECK_VERSION(2,24,0)
-#undef gdk_x11_set_sm_client_id
-static inline void
-gdk_x11_set_sm_client_id (const gchar *sm_client_id)
-{
-  gdk_set_sm_client_id(sm_client_id);
-}
-#endif
 #endif /* GDKX_WRAPPER_H */
