cb5c3c046be328da46dd0356e74d91310482a793
Jakob Stoklund Olesen <jolesen@mozilla.com>
2016-05-31 09:00:17 -0700

    Bug 1136226 - Provide shared NYI implementations of SIMD visitors. r=bbouvier
    Since most targets don't implement SIMD and don't enable the generation of SIMD
    instructions, it makes sense to provide a default implementation of the SIMD
    visitor functions that simply crashes with an NYI error.
    
    Remove the many identical copies of these visitors from the non-SIMD targets.

--- js/src/jit/arm/Lowering-arm.h
+++ js/src/jit/arm/Lowering-arm.h
@@ -104,10 +104,6 @@ class LIRGeneratorARM : public LIRGeneratorShared
     void visitAsmJSAtomicExchangeHeap(MAsmJSAtomicExchangeHeap* ins);
     void visitAsmJSAtomicBinopHeap(MAsmJSAtomicBinopHeap* ins);
     void visitStoreTypedArrayElementStatic(MStoreTypedArrayElementStatic* ins);
-    void visitSimdBinaryArith(MSimdBinaryArith* ins);
-    void visitSimdSelect(MSimdSelect* ins);
-    void visitSimdSplat(MSimdSplat* ins);
-    void visitSimdValueX4(MSimdValueX4* ins);
     void visitCompareExchangeTypedArrayElement(MCompareExchangeTypedArrayElement* ins);
     void visitAtomicExchangeTypedArrayElement(MAtomicExchangeTypedArrayElement* ins);
     void visitAtomicTypedArrayElementBinop(MAtomicTypedArrayElementBinop* ins);
