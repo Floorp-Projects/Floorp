c07cb6835cfeab3504551e19a1b692eaf3fe8abb
Alexandre Poirot <poirot.alex@gmail.com>
2016-05-31 03:54:05 -0700

    Bug 1273311 - Prevent DevToolsUtils.fetch from throwing when no policy is given. r=ejpbruel

--- devtools/shared/DevToolsUtils.js
+++ devtools/shared/DevToolsUtils.js
@@ -536,11 +536,14 @@ function newChannelForURL(url, { policy, window, principal }) {
 
   let channelOptions = {
     contentPolicyType: policy,
-    loadUsingSystemPrincipal: true,
     securityFlags: securityFlags,
     uri: url
   };
   if (principal) {
+    // contentPolicyType is required when loading with a custom principal
+    if (!channelOptions.contentPolicyType) {
+      channelOptions.contentPolicyType = Ci.nsIContentPolicy.TYPE_OTHER;
+    }
     channelOptions.loadingPrincipal = principal;
   } else {
     channelOptions.loadUsingSystemPrincipal = true;
