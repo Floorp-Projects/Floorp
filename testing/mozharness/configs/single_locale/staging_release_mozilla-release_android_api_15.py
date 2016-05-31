6e6cfe1c695fe39a691cc87b7b5c92de01640f2b
Gregory Szorc <gps@mozilla.com>
2016-05-31 14:40:04 -0700

    Bug 1275623 - Use "branch" instead of "revision" to pass symbolic revisions; r=jlund
    robustcheckout barfs on symbolic revisions when using "revision."
    
    MozReview-Commit-ID: B7YXqbWG0G1

--- testing/mozharness/configs/single_locale/staging_release_mozilla-release_android_api_15.py
+++ testing/mozharness/configs/single_locale/staging_release_mozilla-release_android_api_15.py
@@ -32,15 +32,15 @@ config = {
     },
     "repos": [{
         "repo": "https://hg.mozilla.org/%(user_repo_override)s/mozilla-release",
-        "revision": "default",
+        "branch": "default",
         "dest": MOZILLA_DIR,
     }, {
         "repo": "https://hg.mozilla.org/%(user_repo_override)s/buildbot-configs",
-        "revision": "default",
+        "branch": "default",
         "dest": "buildbot-configs"
     }, {
         "repo": "https://hg.mozilla.org/%(user_repo_override)s/tools",
-        "revision": "default",
+        "branch": "default",
         "dest": "tools"
     }, {
         "repo": "https://hg.mozilla.org/%(user_repo_override)s/compare-locales",
