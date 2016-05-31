44deb08f5c852284bbccfe70af9b74f57ad2f700
Bevis Tseng <btseng@mozilla.com>
2016-05-31 18:08:20 +0800

    Bug 1151017 - Support the 'close' Event on Databases. r=khuey
    Outline of this patch:
    
    1. Define a new ipdl message called |CloseAfterInvalidationComplete| to trigger the close event after all transactions are complete only if the database is invalidated by the user agent.
    2. Make sure the following event sequence is consistent during invalidation according to the steps in |5.2. Closing a database| by the following 2 solutions:
         IDBRequest.onerror -> IDBTransaction.onerror -> IDBTransaction.onabort -> IDBDatabase.onclose.
       2.1. In parent process, do not force to abort the transactions after invalidation but wait for all the transactions in its child process are complete.
       2.2. In child process, make sure that each IDBTransaction will notify its completion to the parent after all its pending IDBRequests are finished.
    3. Add test_database_onclose.js to test the close event especially when read/write operation is ongoing.
    4. Add test_database_close_without_onclose.js as a XPCShell test because setTimeout() is not preferred in Mochitest to ensure that the IDBDatabase.onclose event won't be sent after closed normally.

--- dom/indexedDB/IDBTransaction.cpp
+++ dom/indexedDB/IDBTransaction.cpp
@@ -376,7 +376,7 @@ IDBTransaction::OnRequestFinished(bool aActorDestroyedNormally)
 
   --mPendingRequestCount;
 
-  if (!mPendingRequestCount && !mDatabase->IsInvalidated()) {
+  if (!mPendingRequestCount) {
     mReadyState = COMMITTING;
 
     if (aActorDestroyedNormally) {
@@ -640,15 +640,6 @@ IDBTransaction::AbortInternal(nsresult aAbortCode,
   const bool isVersionChange = mMode == VERSION_CHANGE;
   const bool isInvalidated = mDatabase->IsInvalidated();
   bool needToSendAbort = mReadyState == INITIAL && !isInvalidated;
-
-  if (isInvalidated) {
-#ifdef DEBUG
-    mSentCommitOrAbort = true;
-#endif
-    // Increment the serial number counter here to account for the aborted
-    // transaction and keep the parent in sync.
-    IDBRequest::NextSerialNumber();
-  }
 
   mAbortCode = aAbortCode;
   mReadyState = DONE;
