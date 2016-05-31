5f6a0468d75afef0138252735590ba107aa99900
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-05-31 18:54:41 +0200

    Backed out changeset 7f61a6bd8a3d (bug 882718)

--- dom/html/HTMLMediaElement.h
+++ dom/html/HTMLMediaElement.h
@@ -699,11 +699,6 @@ public:
       mTextTrackManager->AddCue(aCue);
     }
   }
-  void NotifyCueRemoved(TextTrackCue& aCue) {
-    if (mTextTrackManager) {
-      mTextTrackManager->NotifyCueRemoved(aCue);
-    }
-  }
 
   /**
    * A public wrapper for FinishDecoderSetup()
