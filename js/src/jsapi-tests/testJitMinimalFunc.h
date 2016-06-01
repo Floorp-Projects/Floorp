9f09b1358839aea0c55a3d51f5000c09540aeea1
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2016-06-01 14:43:32 +0000

    Bug 1264948 - Weaken infallible allocator assertion for jsapi-tests. r=sunfish

--- js/src/jsapi-tests/testJitMinimalFunc.h
+++ js/src/jsapi-tests/testJitMinimalFunc.h
@@ -20,10 +20,15 @@ struct MinimalAlloc {
     LifoAlloc lifo;
     TempAllocator alloc;
 
+    // We are not testing the fallible allocator in these test cases, thus make
+    // the lifo alloc chunk extremely large for our test cases.
     MinimalAlloc()
-      : lifo(4096),
+      : lifo(128 * 1024),
         alloc(&lifo)
-    { }
+    {
+        if (!alloc.ensureBallast())
+            MOZ_CRASH("[OOM] Not enough RAM for the test.");
+    }
 };
 
 struct MinimalFunc : MinimalAlloc
