8a753f232ed20ebbf4fff8fe104aedc166163753
Eddy Bruel <ejpbruel@mozilla.com>
2016-06-01 12:09:51 +0200

    Bug 1271653 - Quick fix for an unhelpful error message;r=me

--- js/src/vm/Debugger.cpp
+++ js/src/vm/Debugger.cpp
@@ -412,7 +412,7 @@ ValueToStableChars(JSContext* cx, const char *fnname, HandleValue value,
 {
     if (!value.isString()) {
         JS_ReportErrorNumber(cx, GetErrorMessage, nullptr, JSMSG_NOT_EXPECTED_TYPE,
-                             fnname, "string", InformalValueType(value));
+                             fnname, "string", InformalValueTypeName(value));
         return false;
     }
     RootedLinearString linear(cx, value.toString()->ensureLinear(cx));
