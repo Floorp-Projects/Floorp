7be2fb4c82546fa9579ceef4ccae5f963d56fcc6
Grigory Kruglov <gkruglov@mozilla.com>
2016-06-01 15:02:46 -0700

    Bug 1274029 - Part 3: Update aggregates during syncing r=sebastian
    MozReview-Commit-ID: 9WyBE0Lk3Tp

--- mobile/android/base/java/org/mozilla/gecko/db/BrowserContract.java
+++ mobile/android/base/java/org/mozilla/gecko/db/BrowserContract.java
@@ -50,6 +50,7 @@ public class BrowserContract {
     public static final String PARAM_IS_TEST = "test";
     public static final String PARAM_INSERT_IF_NEEDED = "insert_if_needed";
     public static final String PARAM_INCREMENT_VISITS = "increment_visits";
+    public static final String PARAM_INCREMENT_REMOTE_AGGREGATES = "increment_remote_aggregates";
     public static final String PARAM_EXPIRE_PRIORITY = "priority";
     public static final String PARAM_DATASET_ID = "dataset_id";
     public static final String PARAM_GROUP_BY = "group_by";
