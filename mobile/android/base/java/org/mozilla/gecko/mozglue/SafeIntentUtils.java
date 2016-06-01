99aeaa07ec45122eddc4dafd7e547925a6a6d7b4
Michael Comella <michael.l.comella@gmail.com>
2016-06-01 15:27:41 -0700

    Bug 1277214 - Move SafeIntent to its own class. r=grisha
    This gets used often enough that it's annoying to do full class name imports.
    
    MozReview-Commit-ID: 7Yhp1NCgwQw

--- mobile/android/base/java/org/mozilla/gecko/mozglue/SafeIntentUtils.java
+++ mobile/android/base/java/org/mozilla/gecko/mozglue/SafeIntentUtils.java
@@ -5,12 +5,10 @@
 package org.mozilla.gecko.mozglue;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import org.mozilla.gecko.util.SafeIntent;
 
 public class SafeIntentUtils {
-    private static final String LOGTAG = "GeckoContextUtils";
 
     public static Bundle getBundleExtra(final Intent intent, final String name) {
         return new SafeIntent(intent).getBundleExtra(name);
@@ -24,84 +22,4 @@ public class SafeIntentUtils {
         return new SafeIntent(intent).getBooleanExtra(name, defaultValue);
     }
 
-    /**
-     * External applications can pass values into Intents that can cause us to crash: in defense,
-     * we wrap {@link Intent} and catch the exceptions they may force us to throw. See bug 1090385
-     * for more.
-     */
-    public static class SafeIntent {
-        private final Intent intent;
-
-        public SafeIntent(final Intent intent) {
-            this.intent = intent;
-        }
-
-        public boolean getBooleanExtra(final String name, final boolean defaultValue) {
-            try {
-                return intent.getBooleanExtra(name, defaultValue);
-            } catch (OutOfMemoryError e) {
-                Log.w(LOGTAG, "Couldn't get intent extras: OOM. Malformed?");
-                return defaultValue;
-            } catch (RuntimeException e) {
-                Log.w(LOGTAG, "Couldn't get intent extras.", e);
-                return defaultValue;
-            }
-        }
-
-        public String getStringExtra(final String name) {
-            try {
-                return intent.getStringExtra(name);
-            } catch (OutOfMemoryError e) {
-                Log.w(LOGTAG, "Couldn't get intent extras: OOM. Malformed?");
-                return null;
-            } catch (RuntimeException e) {
-                Log.w(LOGTAG, "Couldn't get intent extras.", e);
-                return null;
-            }
-        }
-
-        public Bundle getBundleExtra(final String name) {
-            try {
-                return intent.getBundleExtra(name);
-            } catch (OutOfMemoryError e) {
-                Log.w(LOGTAG, "Couldn't get intent extras: OOM. Malformed?");
-                return null;
-            } catch (RuntimeException e) {
-                Log.w(LOGTAG, "Couldn't get intent extras.", e);
-                return null;
-            }
-        }
-
-        public String getAction() {
-            return intent.getAction();
-        }
-
-        public String getDataString() {
-            try {
-                return intent.getDataString();
-            } catch (OutOfMemoryError e) {
-                Log.w(LOGTAG, "Couldn't get intent data string: OOM. Malformed?");
-                return null;
-            } catch (RuntimeException e) {
-                Log.w(LOGTAG, "Couldn't get intent data string.", e);
-                return null;
-            }
-        }
-
-        public Uri getData() {
-            try {
-                return intent.getData();
-            } catch (OutOfMemoryError e) {
-                Log.w(LOGTAG, "Couldn't get intent data: OOM. Malformed?");
-                return null;
-            } catch (RuntimeException e) {
-                Log.w(LOGTAG, "Couldn't get intent data.", e);
-                return null;
-            }
-        }
-
-        public Intent getUnsafe() {
-            return intent;
-        }
-    }
 }
