ecaf9ad6603643c143a7a3fbb258e5b8a4f2c10f
Bill McCloskey <billm@mozilla.com>
2016-06-01 16:37:28 -0700

    Bug 1277705 - Remove child_process_host iter (r=dvander)

--- ipc/chromium/src/chrome/common/child_process_host.cc
+++ ipc/chromium/src/chrome/common/child_process_host.cc
@@ -21,23 +21,15 @@ typedef mozilla::ipc::BrowserProcessSubThread ChromeThread;
 
 using mozilla::ipc::FileDescriptor;
 
-namespace {
-typedef std::list<ChildProcessHost*> ChildProcessList;
-}  // namespace
-
-
-
 ChildProcessHost::ChildProcessHost(ProcessType type)
     :
       ChildProcessInfo(type),
       ALLOW_THIS_IN_INITIALIZER_LIST(listener_(this)),
       opening_channel_(false) {
-  Singleton<ChildProcessList>::get()->push_back(this);
 }
 
 
 ChildProcessHost::~ChildProcessHost() {
-  Singleton<ChildProcessList>::get()->remove(this);
 }
 
 bool ChildProcessHost::CreateChannel() {
@@ -104,34 +96,4 @@ void ChildProcessHost::ListenerHook::OnChannelError() {
 
 void ChildProcessHost::ListenerHook::GetQueuedMessages(std::queue<IPC::Message>& queue) {
   host_->GetQueuedMessages(queue);
-}
-
-ChildProcessHost::Iterator::Iterator() : all_(true) {
-  iterator_ = Singleton<ChildProcessList>::get()->begin();
-}
-
-ChildProcessHost::Iterator::Iterator(ProcessType type)
-    : all_(false), type_(type) {
-  iterator_ = Singleton<ChildProcessList>::get()->begin();
-  if (!Done() && (*iterator_)->type() != type_)
-    ++(*this);
-}
-
-ChildProcessHost* ChildProcessHost::Iterator::operator++() {
-  do {
-    ++iterator_;
-    if (Done())
-      break;
-
-    if (!all_ && (*iterator_)->type() != type_)
-      continue;
-
-    return *iterator_;
-  } while (true);
-
-  return NULL;
-}
-
-bool ChildProcessHost::Iterator::Done() {
-  return iterator_ == Singleton<ChildProcessList>::get()->end();
 }
