9ece31839d49203a2989fea9ebcb3009a75f1b13
Kartikaya Gupta <kgupta@mozilla.com>
2016-06-01 13:13:14 -0400

    Bug 1275604 - Make the touch-action pref live. r=botond
    As part of making the pref live, we need to ensure that if the pref is flipped
    while an input block is in the input queue, the behaviour that results is
    reasonable. In particular, if a touch block is created while touch-action is
    disabled, but the pref is enabled while that block is in the queue, it may
    suddenly go from IsReadyForHandling() returning true to IsReadyForHandling()
    returning false. This can result in the block getting stuck in the queue and
    preventing future touch blocks from being processed at all. To handle this
    case, we explicitly set the mAllowedTouchBehaviorSet flag to true if the block
    is created with touch-action disabled. This prevents IsReadyForHandling() from
    flipping to false in these cases.
    
    MozReview-Commit-ID: CJq9NtMF1Bq

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -401,7 +401,7 @@ private:
   DECL_GFX_PREF(Live, "layout.css.scroll-snap.prediction-max-velocity", ScrollSnapPredictionMaxVelocity, int32_t, 2000);
   DECL_GFX_PREF(Live, "layout.css.scroll-snap.prediction-sensitivity", ScrollSnapPredictionSensitivity, float, 0.750f);
   DECL_GFX_PREF(Live, "layout.css.scroll-snap.proximity-threshold", ScrollSnapProximityThreshold, int32_t, 200);
-  DECL_GFX_PREF(Once, "layout.css.touch_action.enabled",       TouchActionEnabled, bool, false);
+  DECL_GFX_PREF(Live, "layout.css.touch_action.enabled",       TouchActionEnabled, bool, false);
   DECL_GFX_PREF(Live, "layout.display-list.dump",              LayoutDumpDisplayList, bool, false);
   DECL_GFX_PREF(Live, "layout.display-list.dump-content",      LayoutDumpDisplayListContent, bool, false);
   DECL_GFX_PREF(Live, "layout.event-regions.enabled",          LayoutEventRegionsEnabledDoNotUseDirectly, bool, false);
