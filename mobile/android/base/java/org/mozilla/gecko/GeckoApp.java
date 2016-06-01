a227fd871cc71822fff68bb75d6dd35898b70689
Jan Henning <jh+bugzilla@buttercookie.de>
2016-06-01 22:04:59 +0200

    Bug 1190627 - Part 4 - Add telemetry probe for measuring how often we restore from the backup session copy. r=sebastian
    By comparing the rates for FENNEC_SESSIONSTORE_DAMAGED_SESSION_FILE and this new probe, we might be able to infer whether there is a failure mode for the session store where the main session store file is completely lost after an interrupted write and doesn't exist even as an empty file of 0 bytes size.
    
    The first run case will be excluded, because in that case both the main and the backup session file won't exist, so neither telemetry probe will be triggered.
    
    MozReview-Commit-ID: JTR54Vk18jW
    
    --HG--
    extra : rebase_source : dd7f34b4ea5c7fedb8c636ca600f954c1c1104f7

--- mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
@@ -1404,10 +1404,16 @@ public abstract class GeckoApp
                             }
                             try {
                                 restoreMessage = restoreSessionTabs(isExternalURL, true);
+                                Telemetry.addToHistogram("FENNEC_SESSIONSTORE_RESTORING_FROM_BACKUP", 1);
                             } catch (SessionRestoreException ex) {
-                                // If this fails, too, do a normal startup.
-                                Log.e(LOGTAG, "An error occurred during restore", ex);
-                                mShouldRestore = false;
+                                if (!mShouldRestore) {
+                                    // Restoring only "failed" because the backup copy was deliberately empty, too.
+                                    Telemetry.addToHistogram("FENNEC_SESSIONSTORE_RESTORING_FROM_BACKUP", 1);
+                                } else {
+                                    // Restoring the backup failed, too, so do a normal startup.
+                                    Log.e(LOGTAG, "An error occurred during restore", ex);
+                                    mShouldRestore = false;
+                                }
                             }
                         }
                     }
