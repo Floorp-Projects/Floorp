957d189d2f31e78c3460140f2daaf0fb55613960
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-06-02 16:59:00 +1000

    Bug 1276184: [MSE] P2. Remove the need to scan the track buffer when frames are being removed. r=kamidphish, a=gchang
    The index at which we are removing frames is always the one where we will be inserting the next ones.
    
    MozReview-Commit-ID: DHeJDmwiMS9

--- dom/media/mediasource/TrackBuffersManager.cpp
+++ dom/media/mediasource/TrackBuffersManager.cpp
@@ -1633,7 +1633,11 @@ TrackBuffersManager::InsertFrames(TrackBuffer& aSamples,
       // so the search for when to start our frames removal can be exhaustive.
       trackBuffer.mNextInsertionIndex.reset();
     }
-    RemoveFrames(aIntervals, trackBuffer, trackBuffer.mNextInsertionIndex.refOr(0));
+    size_t index =
+      RemoveFrames(aIntervals, trackBuffer, trackBuffer.mNextInsertionIndex.refOr(0));
+    if (index) {
+      trackBuffer.mNextInsertionIndex = Some(index);
+    }
   }
 
   // 16. Add the coded frame with the presentation timestamp, decode timestamp, and frame duration to the track buffer.
@@ -1668,7 +1672,7 @@ TrackBuffersManager::InsertFrames(TrackBuffer& aSamples,
   trackBuffer.mSanitizedBufferedRanges += range;
 }
 
-void
+size_t
 TrackBuffersManager::RemoveFrames(const TimeIntervals& aIntervals,
                                   TrackData& aTrackData,
                                   uint32_t aStartIndex)
@@ -1700,7 +1704,7 @@ TrackBuffersManager::RemoveFrames(const TimeIntervals& aIntervals,
   }
 
   if (firstRemovedIndex.isNothing()) {
-    return;
+    return 0;
   }
 
   // Remove decoding dependencies of the coded frames removed in the previous step:
@@ -1764,6 +1768,8 @@ TrackBuffersManager::RemoveFrames(const TimeIntervals& aIntervals,
 
   data.RemoveElementsAt(firstRemovedIndex.ref(),
                         lastRemovedIndex - firstRemovedIndex.ref() + 1);
+
+  return firstRemovedIndex.ref();
 }
 
 void
