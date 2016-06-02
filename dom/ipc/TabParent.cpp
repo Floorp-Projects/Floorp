f5dbedafaaf8c229e71b8c8e72421200a7f6872d
dimi <dlee@mozilla.com>
2016-06-02 10:12:39 +0800

    Bug 1258033 - Part 1: Add IsTrackingProtectionOn for nsILoadContext. r=smaug
    MozReview-Commit-ID: 2eQzO6KDLyX

--- dom/ipc/TabParent.cpp
+++ dom/ipc/TabParent.cpp
@@ -3186,6 +3186,7 @@ public:
   NS_IMETHOD GetOriginAttributes(JS::MutableHandleValue) NO_IMPL
   NS_IMETHOD GetUseRemoteTabs(bool*) NO_IMPL
   NS_IMETHOD SetRemoteTabs(bool) NO_IMPL
+  NS_IMETHOD IsTrackingProtectionOn(bool*) NO_IMPL
 #undef NO_IMPL
 
 protected:
