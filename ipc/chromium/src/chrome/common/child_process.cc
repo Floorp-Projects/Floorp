6bcfecd8d4c82010f3868593ab198c40d0cf682c
Bill McCloskey <billm@mozilla.com>
2016-06-01 20:38:34 -0700

    Bug 1277705 - Remove some child_process code (r=dvander)

--- ipc/chromium/src/chrome/common/child_process.cc
+++ ipc/chromium/src/chrome/common/child_process.cc
@@ -13,9 +13,7 @@
 ChildProcess* ChildProcess::child_process_;
 
 ChildProcess::ChildProcess(ChildThread* child_thread)
-    : child_thread_(child_thread),
-      ref_count_(0),
-      shutdown_event_(true, false) {
+    : child_thread_(child_thread) {
   DCHECK(!child_process_);
   child_process_ = this;
   if (child_thread_.get())  // null in unittests.
@@ -25,32 +23,8 @@ ChildProcess::ChildProcess(ChildThread* child_thread)
 ChildProcess::~ChildProcess() {
   DCHECK(child_process_ == this);
 
-  // Signal this event before destroying the child process.  That way all
-  // background threads can cleanup.
-  // For example, in the renderer the RenderThread instances will be able to
-  // notice shutdown before the render process begins waiting for them to exit.
-  shutdown_event_.Signal();
-
   if (child_thread_.get())
     child_thread_->Stop();
 
   child_process_ = NULL;
-}
-
-void ChildProcess::AddRefProcess() {
-  DCHECK(!child_thread_.get() ||  // null in unittests.
-         MessageLoop::current() == child_thread_->message_loop());
-  ref_count_++;
-}
-
-void ChildProcess::ReleaseProcess() {
-  DCHECK(!child_thread_.get() ||  // null in unittests.
-         MessageLoop::current() == child_thread_->message_loop());
-  DCHECK(ref_count_);
-  DCHECK(child_process_);
-  if (--ref_count_)
-    return;
-
-  if (child_thread_.get())  // null in unittests.
-    child_thread_->OnProcessFinalRelease();
 }
