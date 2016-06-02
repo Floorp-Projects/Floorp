eaf212bb70f634f753d44f4b61f89f746825bbcb
Boris Zbarsky <bzbarsky@mit.edu>
2016-06-02 10:34:39 -0400

    Bug 1276317 part 3.  Move handling of the mem log prefs from being per-nsJSContext to just happening once in nsJSContext::EnsureStatics.  r=peterv

--- dom/base/nsJSEnvironment.h
+++ dom/base/nsJSEnvironment.h
@@ -168,8 +168,6 @@ private:
   // context does. It is eventually collected by the cycle collector.
   nsCOMPtr<nsIScriptGlobalObject> mGlobalObjectRef;
 
-  static void JSOptionChangedCallback(const char *pref, void *data);
-
   static bool DOMOperationCallback(JSContext *cx);
 };
 
