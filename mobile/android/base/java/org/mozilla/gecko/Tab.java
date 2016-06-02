342b90bc1667f9c76595223f6369da474aa4b3a5
Andrzej Hunt <ahunt@mozilla.com>
2016-06-02 13:51:52 -0700

    Bug 1265708 - Pre: remove unused variable r=sebastian
    We already pass the URL inside the JSON object.
    
    MozReview-Commit-ID: J4RjxLAzdI
    
    --HG--
    extra : rebase_source : 9a4c365fbd2ce4321c84a994db680f8906c13bea

--- mobile/android/base/java/org/mozilla/gecko/Tab.java
+++ mobile/android/base/java/org/mozilla/gecko/Tab.java
@@ -354,7 +354,7 @@ public class Tab {
         ThreadUtils.postToBackgroundThread(new Runnable() {
             @Override
             public void run() {
-                urlMetadata.save(cr, mUrl, data);
+                urlMetadata.save(cr, data);
             }
         });
     }
