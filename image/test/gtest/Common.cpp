8ab72fa989258610c904d083a67baf136d8dafb8
Seth Fowler <mark.seth.fowler@gmail.com>
2016-06-01 23:01:50 -0700

    Bug 1276061 (Part 3) - Add ImageLib GTest utility functions for checking paletted image output. r=njn

--- image/test/gtest/Common.cpp
+++ image/test/gtest/Common.cpp
@@ -95,6 +95,13 @@ IsSolidColor(SourceSurface* aSurface,
 }
 
 bool
+IsSolidPalettedColor(Decoder* aDecoder, uint8_t aColor)
+{
+  RawAccessFrameRef currentFrame = aDecoder->GetCurrentFrameRef();
+  return PalettedRectIsSolidColor(aDecoder, currentFrame->GetRect(), aColor);
+}
+
+bool
 RowsAreSolidColor(SourceSurface* aSurface,
                   int32_t aStartRow,
                   int32_t aRowCount,
@@ -104,6 +111,18 @@ RowsAreSolidColor(SourceSurface* aSurface,
   IntSize size = aSurface->GetSize();
   return RectIsSolidColor(aSurface, IntRect(0, aStartRow, size.width, aRowCount),
                           aColor, aFuzz);
+}
+
+bool
+PalettedRowsAreSolidColor(Decoder* aDecoder,
+                          int32_t aStartRow,
+                          int32_t aRowCount,
+                          uint8_t aColor)
+{
+  RawAccessFrameRef currentFrame = aDecoder->GetCurrentFrameRef();
+  IntRect frameRect = currentFrame->GetRect();
+  IntRect solidColorRect(frameRect.x, aStartRow, frameRect.width, aRowCount);
+  return PalettedRectIsSolidColor(aDecoder, solidColorRect, aColor);
 }
 
 bool
@@ -149,6 +168,43 @@ RectIsSolidColor(SourceSurface* aSurface,
   return true;
 }
 
+bool
+PalettedRectIsSolidColor(Decoder* aDecoder, const IntRect& aRect, uint8_t aColor)
+{
+  RawAccessFrameRef currentFrame = aDecoder->GetCurrentFrameRef();
+  uint8_t* imageData;
+  uint32_t imageLength;
+  currentFrame->GetImageData(&imageData, &imageLength);
+  ASSERT_TRUE_OR_RETURN(imageData, false);
+
+  // Clamp to the frame rect. If any pixels outside the frame rect are included,
+  // we immediately fail, because such pixels don't have any "color" in the
+  // sense this function measures - they're transparent, and that doesn't
+  // necessarily correspond to any color palette index at all.
+  IntRect frameRect = currentFrame->GetRect();
+  ASSERT_EQ_OR_RETURN(imageLength, uint32_t(frameRect.Area()), false);
+  IntRect rect = aRect.Intersect(frameRect);
+  ASSERT_EQ_OR_RETURN(rect.Area(), aRect.Area(), false);
+
+  // Translate |rect| by |frameRect.TopLeft()| to reflect the fact that the
+  // frame rect's offset doesn't actually mean anything in terms of the
+  // in-memory representation of the surface. The image data starts at the upper
+  // left corner of the frame rect, in other words.
+  rect -= frameRect.TopLeft();
+
+  // Walk through the image data and make sure that the entire rect has the
+  // palette index |aColor|.
+  int32_t rowLength = frameRect.width;
+  for (int32_t row = rect.y; row < rect.YMost(); ++row) {
+    for (int32_t col = rect.x; col < rect.XMost(); ++col) {
+      int32_t i = row * rowLength + col;
+      ASSERT_EQ_OR_RETURN(aColor, imageData[i], false);
+    }
+  }
+
+  return true;
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // SurfacePipe Helpers
@@ -166,9 +222,10 @@ CreateTrivialDecoder()
   return decoder.forget();
 }
 
-void AssertCorrectPipelineFinalState(SurfaceFilter* aFilter,
-                                     const gfx::IntRect& aInputSpaceRect,
-                                     const gfx::IntRect& aOutputSpaceRect)
+void
+AssertCorrectPipelineFinalState(SurfaceFilter* aFilter,
+                                const gfx::IntRect& aInputSpaceRect,
+                                const gfx::IntRect& aOutputSpaceRect)
 {
   EXPECT_TRUE(aFilter->IsSurfaceFinished());
   Maybe<SurfaceInvalidRect> invalidRect = aFilter->TakeInvalidRect();
@@ -222,6 +279,50 @@ CheckGeneratedImage(Decoder* aDecoder,
   EXPECT_TRUE(RectIsSolidColor(surface,
                                IntRect(0, aRect.YMost(), surfaceSize.width, heightBelow),
                                BGRAColor::Transparent(), aFuzz));
+}
+
+void
+CheckGeneratedPalettedImage(Decoder* aDecoder, const IntRect& aRect)
+{
+  RawAccessFrameRef currentFrame = aDecoder->GetCurrentFrameRef();
+  IntSize imageSize = currentFrame->GetImageSize();
+
+  // This diagram shows how the surface is divided into regions that the code
+  // below tests for the correct content. The output rect is the bounds of the
+  // region labeled 'C'.
+  //
+  // +---------------------------+
+  // |             A             |
+  // +---------+--------+--------+
+  // |    B    |   C    |   D    |
+  // +---------+--------+--------+
+  // |             E             |
+  // +---------------------------+
+
+  // Check that the output rect itself is all 255's. (Region 'C'.)
+  EXPECT_TRUE(PalettedRectIsSolidColor(aDecoder, aRect, 255));
+
+  // Check that the area above the output rect is all 0's. (Region 'A'.)
+  EXPECT_TRUE(PalettedRectIsSolidColor(aDecoder,
+                                       IntRect(0, 0, imageSize.width, aRect.y),
+                                       0));
+
+  // Check that the area to the left of the output rect is all 0's. (Region 'B'.)
+  EXPECT_TRUE(PalettedRectIsSolidColor(aDecoder,
+                                       IntRect(0, aRect.y, aRect.x, aRect.YMost()),
+                                       0));
+
+  // Check that the area to the right of the output rect is all 0's. (Region 'D'.)
+  const int32_t widthOnRight = imageSize.width - aRect.XMost();
+  EXPECT_TRUE(PalettedRectIsSolidColor(aDecoder,
+                                       IntRect(aRect.XMost(), aRect.y, widthOnRight, aRect.YMost()),
+                                       0));
+
+  // Check that the area below the output rect is transparent. (Region 'E'.)
+  const int32_t heightBelow = imageSize.height - aRect.YMost();
+  EXPECT_TRUE(PalettedRectIsSolidColor(aDecoder,
+                                       IntRect(0, aRect.YMost(), imageSize.width, heightBelow),
+                                       0));
 }
 
 void
