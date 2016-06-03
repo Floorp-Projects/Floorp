e28d80abb0379add7f746d09ccd1e714883ffe53
Johann Hofmann <jhofmann@mozilla.com>
2016-06-03 12:26:28 +0200

    Bug 1277524 - Add a WebExtension test that moz-extension is considered a trustworthy origin. r=kmag
    MozReview-Commit-ID: 1LLNWwIU90

---
+++ toolkit/components/extensions/test/mochitest/chrome_head.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const {
+  classes: Cc,
+  interfaces: Ci,
+  utils: Cu,
+  results: Cr,
+} = Components;
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/NetUtil.jsm");
+
