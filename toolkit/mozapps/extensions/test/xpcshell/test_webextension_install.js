2242969a95b04bb2996cf3e5e7a4fefdf0a14b44
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-06-03 16:43:45 -0500

    Bug 1273229 - auto-generate IDs for temp installs from dir path. r=kmag
    MozReview-Commit-ID: JFuEVUZs2Ci

--- toolkit/mozapps/extensions/test/xpcshell/test_webextension_install.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_webextension_install.js
@@ -46,6 +46,71 @@ add_task(function* test_implicit_id_temp() {
   addon.uninstall();
 });
 
+// We should be able to temporarily install an unsigned web extension
+// that does not have an ID in its manifest.
+add_task(function* test_unsigned_no_id_temp_install() {
+  if (!TEST_UNPACKED) {
+    do_print("This test does not apply when using packed extensions");
+    return;
+  }
+  const manifest = {
+    name: "no ID",
+    description: "extension without an ID",
+    manifest_version: 2,
+    version: "1.0"
+  };
+
+  const addonDir = writeWebManifestForExtension(manifest, gTmpD,
+                                                "the-addon-sub-dir");
+  const addon = yield AddonManager.installTemporaryAddon(addonDir);
+  ok(addon.id, "ID should have been auto-generated");
+
+  // Install the same directory again, as if re-installing or reloading.
+  const secondAddon = yield AddonManager.installTemporaryAddon(addonDir);
+  // The IDs should be the same.
+  equal(secondAddon.id, addon.id);
+
+  secondAddon.uninstall();
+  addonDir.remove(true);
+});
+
+// We should be able to install two extensions from manifests without IDs
+// at different locations and get two unique extensions.
+add_task(function* test_multiple_no_id_extensions() {
+  if (!TEST_UNPACKED) {
+    do_print("This test does not apply when using packed extensions");
+    return;
+  }
+  const manifest = {
+    name: "no ID",
+    description: "extension without an ID",
+    manifest_version: 2,
+    version: "1.0"
+  };
+
+  const firstAddonDir = writeWebManifestForExtension(manifest, gTmpD,
+                                                     "addon-sub-dir-one");
+  const secondAddonDir = writeWebManifestForExtension(manifest, gTmpD,
+                                                      "addon-sub-dir-two");
+  const [firstAddon, secondAddon] = yield Promise.all([
+    AddonManager.installTemporaryAddon(firstAddonDir),
+    AddonManager.installTemporaryAddon(secondAddonDir)
+  ]);
+
+  const allAddons = yield new Promise(resolve => {
+    AddonManager.getAllAddons(addons => resolve(addons));
+  });
+  do_print(`Found these add-ons: ${allAddons.map(a => a.name).join(", ")}`);
+  const filtered = allAddons.filter(addon => addon.name === manifest.name);
+  // Make sure we have two add-ons by the same name.
+  equal(filtered.length, 2);
+
+  firstAddon.uninstall();
+  firstAddonDir.remove(true);
+  secondAddon.uninstall();
+  secondAddonDir.remove(true);
+});
+
 // Test that we can get the ID from browser_specific_settings
 add_task(function* test_bss_id() {
   const ID = "webext_bss_id@tests.mozilla.org";
