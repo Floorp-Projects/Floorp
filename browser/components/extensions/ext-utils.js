c5d392a9bc35e74e2c8c229cce89bdca2036cbfc
Kris Maglione <maglione.k@gmail.com>
2016-06-02 20:53:41 -0700

    Bug 1276025: Stop using injectInDocShell to tag docShells with types. r=billm
    MozReview-Commit-ID: 7h5PI2birY4
    
    --HG--
    extra : rebase_source : a8b8b9b49f9ef8304ee669f6c28c9290a1198ff3
    extra : amend_source : f868dc11e3da5f89852343e88e373db14f5dcbd3
    extra : histedit_source : 6ee59978a58828e22a9f2cbf651cb713ba28d25d

--- browser/components/extensions/ext-utils.js
+++ browser/components/extensions/ext-utils.js
@@ -191,12 +191,10 @@ class BasePopup {
 
       this.viewNode.removeEventListener(this.DESTROY_EVENT, this);
 
-      this.context.unload();
       this.browser.remove();
 
       this.browser = null;
       this.viewNode = null;
-      this.context = null;
     });
   }
 
@@ -252,6 +250,7 @@ class BasePopup {
     this.browser = document.createElementNS(XUL_NS, "browser");
     this.browser.setAttribute("type", "content");
     this.browser.setAttribute("disableglobalhistory", "true");
+    this.browser.setAttribute("webextension-view-type", "popup");
 
     // Note: When using noautohide panels, the popup manager will add width and
     // height attributes to the panel, breaking our resize code, if the browser
@@ -280,15 +279,7 @@ class BasePopup {
                    .getInterface(Ci.nsIDOMWindowUtils)
                    .allowScriptsToClose();
 
-      this.context = new ExtensionContext(this.extension, {
-        type: "popup",
-        contentWindow,
-        uri: popupURI,
-        docShell: this.browser.docShell,
-      });
-
-      GlobalManager.injectInDocShell(this.browser.docShell, this.extension, this.context);
-      this.browser.setAttribute("src", this.context.uri.spec);
+      this.browser.setAttribute("src", popupURI.spec);
 
       this.browser.addEventListener("DOMWindowCreated", this, true);
       this.browser.addEventListener("load", this, true);
