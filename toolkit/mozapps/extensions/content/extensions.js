ddc30797f1bcb875e32a288bd24466c98521c96c
Kris Maglione <maglione.k@gmail.com>
2016-06-02 19:06:42 -0700

    Bug 1269877: Fix timing issue in options browser resizing code. r=aswan
    MozReview-Commit-ID: 9aV8QH7w8R8

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -3472,6 +3472,10 @@ var gDetailView = {
       timeout = null;
 
       let doc = browser.contentDocument;
+      if (!doc) {
+        return;
+      }
+
       let body = doc.body || doc.documentElement;
 
       let docHeight = doc.documentElement.getBoundingClientRect().height;
