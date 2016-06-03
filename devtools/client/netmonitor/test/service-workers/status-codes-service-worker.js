3a5de9df0e48ea21e3f4f620b891694c78f0a36b
Wes Kocher <wkocher@mozilla.com>
2016-06-03 14:01:27 -0700

    Backed out 3 changesets (bug 1134073) because maybe it broke the clipboard tests
    Backed out changeset a43d99734390 (bug 1134073)
    Backed out changeset 572ebec612e8 (bug 1134073)
    Backed out changeset f12a69ba9122 (bug 1134073)

--- devtools/client/netmonitor/test/service-workers/status-codes-service-worker.js
+++ devtools/client/netmonitor/test/service-workers/status-codes-service-worker.js
@@ -2,14 +2,7 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-"use strict";
-
-self.addEventListener("activate", event => {
-  // start controlling the already loaded page
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", event => {
+addEventListener("fetch", function (event) {
   let response = new Response("Service worker response");
   event.respondWith(response);
 });
