203adab87e8fe7d58df48df9099dd911856bb22a
Nicholas Nethercote <nnethercote@mozilla.com>
2016-06-03 16:12:25 +1000

    Bug 1278752 (part 1) - Add a missing null check in PropertyTypeIncludes. r=bhackett.

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -6040,6 +6040,9 @@ PropertyTypeIncludes(TempAllocator& alloc, HeapTypeSetKey property,
             types = types->clone(alloc.lifoAlloc());
         else
             types = alloc.lifoAlloc()->new_<TemporaryTypeSet>();
+        if (!types) {
+            return false;
+        }
         types->addType(newType, alloc.lifoAlloc());
     }
 
