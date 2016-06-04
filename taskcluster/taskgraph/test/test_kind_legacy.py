2779596fe218a2945302859b0571a11c59c6523a
Dustin J. Mitchell <dustin@mozilla.com>
2016-06-04 21:11:59 +0000

    Bug 1275409: remove taskcluster_graph.build_task; r=wcosta
    MozReview-Commit-ID: IphYcikdlxX

--- taskcluster/taskgraph/test/test_kind_legacy.py
+++ taskcluster/taskgraph/test/test_kind_legacy.py
@@ -6,7 +6,12 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import unittest
 
-from ..kind.legacy import LegacyKind, TASKID_PLACEHOLDER
+from ..kind.legacy import (
+    LegacyKind,
+    TASKID_PLACEHOLDER,
+    validate_build_task,
+    BuildTaskValidationException
+)
 from ..types import Task
 from mozunit import main
 
@@ -18,6 +23,24 @@ class TestLegacyKind(unittest.TestCase):
 
     def setUp(self):
         self.kind = LegacyKind('/root', {})
+
+
+class TestValidateBuildTask(unittest.TestCase):
+
+    def test_validate_missing_extra(self):
+        with self.assertRaises(BuildTaskValidationException):
+            validate_build_task({})
+
+    def test_validate_valid(self):
+        with self.assertRaises(BuildTaskValidationException):
+            validate_build_task({
+                'extra': {
+                    'locations': {
+                        'build': '',
+                        'tests': ''
+                    }
+                }
+            })
 
 
 if __name__ == '__main__':
