05a7e2b273a6f07f8ae6385cfa6755b1aba51ca2
Tim Nguyen <ntim.bugs@gmail.com>
2016-06-06 23:46:32 +0200

    Bug 1278346 - Switch browser_net_statistics-01,02.js to add_task. r=ochameau
    MozReview-Commit-ID: 5QDts08x0rv

--- devtools/client/netmonitor/test/browser_net_statistics-02.js
+++ devtools/client/netmonitor/test/browser_net_statistics-02.js
@@ -1,43 +1,42 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+"use strict";
+
 /**
  * Tests if the network inspector view is shown when the target navigates
  * away while in the statistics view.
  */
 
-function test() {
-  initNetMonitor(STATISTICS_URL).then(([aTab, aDebuggee, aMonitor]) => {
-    info("Starting test... ");
+add_task(function* () {
+  let [, debuggee, monitor] = yield initNetMonitor(STATISTICS_URL);
+  info("Starting test... ");
 
-    let panel = aMonitor.panelWin;
-    let { document, EVENTS, NetMonitorView } = panel;
-
-    is(NetMonitorView.currentFrontendMode, "network-inspector-view",
+  let panel = monitor.panelWin;
+  let { EVENTS, NetMonitorView } = panel;
+  is(NetMonitorView.currentFrontendMode, "network-inspector-view",
       "The initial frontend mode is correct.");
 
-    promise.all([
-      waitFor(panel, EVENTS.PRIMED_CACHE_CHART_DISPLAYED),
-      waitFor(panel, EVENTS.EMPTY_CACHE_CHART_DISPLAYED)
-    ]).then(() => {
-      is(NetMonitorView.currentFrontendMode, "network-statistics-view",
+  let onChartDisplayed = Promise.all([
+    waitFor(panel, EVENTS.PRIMED_CACHE_CHART_DISPLAYED),
+    waitFor(panel, EVENTS.EMPTY_CACHE_CHART_DISPLAYED)
+  ]);
+
+  info("Displaying statistics view");
+  NetMonitorView.toggleFrontendMode();
+  yield onChartDisplayed;
+  is(NetMonitorView.currentFrontendMode, "network-statistics-view",
         "The frontend mode is currently in the statistics view.");
 
-      waitFor(panel, EVENTS.TARGET_WILL_NAVIGATE).then(() => {
-        is(NetMonitorView.currentFrontendMode, "network-inspector-view",
+  info("Reloading page");
+  let onWillNavigate = waitFor(panel, EVENTS.TARGET_WILL_NAVIGATE);
+  let onDidNavigate = waitFor(panel, EVENTS.TARGET_DID_NAVIGATE);
+  debuggee.location.reload();
+  yield onWillNavigate;
+  is(NetMonitorView.currentFrontendMode, "network-inspector-view",
           "The frontend mode switched back to the inspector view.");
-
-        waitFor(panel, EVENTS.TARGET_DID_NAVIGATE).then(() => {
-          is(NetMonitorView.currentFrontendMode, "network-inspector-view",
+  yield onDidNavigate;
+  is(NetMonitorView.currentFrontendMode, "network-inspector-view",
             "The frontend mode is still in the inspector view.");
-
-          teardown(aMonitor).then(finish);
-        });
-      });
-
-      aDebuggee.location.reload();
-    });
-
-    NetMonitorView.toggleFrontendMode();
-  });
-}
+  yield teardown(monitor);
+});
