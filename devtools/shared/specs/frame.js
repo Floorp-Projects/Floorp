7b78102018706ee7d45dcc01fae80e0dc79fa5a3
Nick Fitzgerald <fitzgen@gmail.com>
2016-06-06 10:24:34 -0700

    Bug 1277953 - Use ActorClassWithSpec for the FrameActor; r=ejpbruel

--- devtools/shared/specs/frame.js
+++ devtools/shared/specs/frame.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-const {Arg, RetVal, generateActorSpec} = require("devtools/shared/protocol");
+const {generateActorSpec} = require("devtools/shared/protocol");
 
 const frameSpec = generateActorSpec({
   typeName: "frame",
