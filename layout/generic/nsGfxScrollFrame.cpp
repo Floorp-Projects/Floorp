f5fc4c4717ac06d3d573e1ff53eda0b3dc56903d
Wes Kocher <wkocher@mozilla.com>
2016-06-06 12:16:52 -0700

    Backed out 6 changesets (bug 1276107) for windows apz test bustage CLOSED TREE
    Backed out changeset acd104ba0e8d (bug 1276107)
    Backed out changeset f15df83c29a6 (bug 1276107)
    Backed out changeset cd06b8e0f6dc (bug 1276107)
    Backed out changeset e8361d5c43d2 (bug 1276107)
    Backed out changeset 6f03149a7565 (bug 1276107)
    Backed out changeset 7658c369fd33 (bug 1276107)

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -2845,13 +2845,7 @@ MaxZIndexInListOfItemsContainedInFrame(nsDisplayList* aList, nsIFrame* aFrame)
 {
   int32_t maxZIndex = -1;
   for (nsDisplayItem* item = aList->GetBottom(); item; item = item->GetAbove()) {
-    nsIFrame* itemFrame = item->Frame();
-    // Perspective items return the scroll frame as their Frame(), so consider
-    // their TransformFrame() instead.
-    if (item->GetType() == nsDisplayItem::TYPE_PERSPECTIVE) {
-      itemFrame = static_cast<nsDisplayPerspective*>(item)->TransformFrame();
-    }
-    if (nsLayoutUtils::IsProperAncestorFrame(aFrame, itemFrame)) {
+    if (nsLayoutUtils::IsProperAncestorFrame(aFrame, item->Frame())) {
       maxZIndex = std::max(maxZIndex, item->ZIndex());
     }
   }
@@ -3463,7 +3457,6 @@ ScrollFrameHelper::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
         MaxZIndexInListOfItemsContainedInFrame(positionedDescendants, mOuter);
       if (zindex >= 0) {
         destinationList = positionedDescendants;
-        inactiveRegionItem->SetOverrideZIndex(zindex);
       } else {
         destinationList = scrolledContent.Outlines();
       }
