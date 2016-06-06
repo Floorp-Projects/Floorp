5acc9029132c3289ce269db4180fbfed81422c34
Jordan Lund <jlund@mozilla.com>
2016-06-06 15:38:13 +0100

    Bug 1278256 - drop api-9 beta release support, CLOSED TREE DONTBUILD a=testing r=kmoir
    MozReview-Commit-ID: KFlaQoeGPRc
    
    * I guess this has to be uplifted to aurora and included in the aurora->beta
      merge

--- testing/mozharness/configs/releases/bouncer_fennec.py
+++ testing/mozharness/configs/releases/bouncer_fennec.py
@@ -12,10 +12,6 @@ config = {
                     "path": "/mobile/releases/%(version)s/android-api-15/:lang/fennec-%(version)s.:lang.android-arm.apk",
                     "bouncer-platform": "android",
                 },
-                "android-api-9": {
-                    "path": "/mobile/releases/%(version)s/android-api-9/:lang/fennec-%(version)s.:lang.android-arm.apk",
-                    "bouncer-platform": "android-api-9",
-                },
                 "android-x86": {
                     "path": "/mobile/releases/%(version)s/android-x86/:lang/fennec-%(version)s.:lang.android-i386.apk",
                     "bouncer-platform": "android-x86",
