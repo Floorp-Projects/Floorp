da176c36e19a6fe6796dbbdd0e04986fb19933fe
Wes Kocher <wkocher@mozilla.com>
2016-06-06 14:43:24 -0700

    Backed out changeset 396b577b98e6 (bug 1171736) for android nightly bustage a=backout

--- testing/mozharness/scripts/desktop_l10n.py
+++ testing/mozharness/scripts/desktop_l10n.py
@@ -173,7 +173,6 @@ class DesktopSingleLocale(LocalesMixin, ReleaseMixin, MockMixin, BuildbotMixin,
             'all_actions': [
                 "clobber",
                 "pull",
-                "clone-locales",
                 "list-locales",
                 "setup",
                 "repack",
@@ -466,7 +465,7 @@ class DesktopSingleLocale(LocalesMixin, ReleaseMixin, MockMixin, BuildbotMixin,
     def _query_revision(self):
         """ Get the gecko revision in this order of precedence
               * cached value
-              * command line arg --revision   (development, taskcluster)
+              * command line arg --revision   (development)
               * buildbot properties           (try with buildbot forced build)
               * buildbot change               (try with buildbot scheduler)
               * from the en-US build          (m-c & m-a)
@@ -638,8 +637,6 @@ class DesktopSingleLocale(LocalesMixin, ReleaseMixin, MockMixin, BuildbotMixin,
         self.info("repositories: %s" % repos)
         self.vcs_checkout_repos(repos, parent_dir=dirs['abs_work_dir'],
                                 tag_override=config.get('tag_override'))
-
-    def clone_locales(self):
         self.pull_locale_source()
 
     def setup(self):
