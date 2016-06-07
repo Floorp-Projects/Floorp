c7bd442e3b61396c3690ef148bc122b33f4b4d2f
Michael Comella <michael.l.comella@gmail.com>
2016-06-07 15:19:20 -0700

    Bug 1261494 - Add assertions & logging to resetTest for sanity checking. r=grisha
    MozReview-Commit-ID: 2WDk0IJokL9

--- mobile/android/tests/browser/robocop/src/org/mozilla/gecko/tests/testUnifiedTelemetryClientId.java
+++ mobile/android/tests/browser/robocop/src/org/mozilla/gecko/tests/testUnifiedTelemetryClientId.java
@@ -6,6 +6,7 @@ package org.mozilla.gecko.tests;
 
 import static org.mozilla.gecko.tests.helpers.AssertionHelper.*;
 
+import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.mozilla.gecko.GeckoProfile;
@@ -23,11 +24,17 @@ public class testUnifiedTelemetryClientId extends JavascriptBridgeTest {
 
     private GeckoProfile profile;
     private File profileDir;
+    private File[] filesToDeleteOnReset;
 
     public void setUp() throws Exception {
         super.setUp();
         profile = getTestProfile();
         profileDir = profile.getDir(); // Assumes getDir is tested.
+        filesToDeleteOnReset = new File[] {
+                getClientIdFile(),
+                getFHRClientIdFile(),
+                getFHRClientIdParentDir(),
+        };
 
         // In local testing, it's possible to ^C out of the harness and not have tearDown called,
         // hence reset. We can't clear the cache because Gecko is not running yet.
@@ -41,12 +48,17 @@ public class testUnifiedTelemetryClientId extends JavascriptBridgeTest {
     }
 
     private void resetTest(final boolean resetJSCache) {
+        Log.d(LOGTAG, "resetTest: begin");
+
         if (resetJSCache) {
             resetJSCache();
         }
-        getClientIdFile().delete();
-        getFHRClientIdFile().delete();
-        getFHRClientIdParentDir().delete();
+        for (final File file : filesToDeleteOnReset) {
+            file.delete();
+            fAssertFalse("Deleted file in reset does not exist", file.exists()); // sanity check.
+        }
+
+        Log.d(LOGTAG, "resetTest: end");
     }
 
     public void testUnifiedTelemetryClientId() throws Exception {
