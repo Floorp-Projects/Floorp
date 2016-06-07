a9df8d4d3fbb6dd900fa7c7ea7f33b692f778c73
Kit Cambridge <kcambridge@mozilla.com>
2016-06-07 14:25:59 -0700

    Bug 1278851 - Enable service worker notifications everywhere except for non-release B2G. r=baku
    MozReview-Commit-ID: InG420vpNtA
    
    --HG--
    extra : rebase_source : 0523697466cb8f438b1af129c5d74a1b0c8df499

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1391,7 +1391,6 @@ pref("view_source.tab", true);
 
 pref("dom.serviceWorkers.enabled", true);
 pref("dom.serviceWorkers.openWindow.enabled", true);
-pref("dom.webnotifications.serviceworker.enabled", true);
 
 // Enable Push API.
 pref("dom.push.enabled", true);
