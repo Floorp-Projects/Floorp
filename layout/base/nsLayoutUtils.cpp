acd21d26c03bd753bd9bae6af0e5374188e9085c
Cameron McCormack <cam@mcc.id.au>
2016-06-07 12:20:06 +1000

    Bug 1285474: Decide style system backend type for documents earlier. r=bholley
    MozReview-Commit-ID: 7A4t3957CWe

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -9365,3 +9365,11 @@ nsLayoutUtils::GetCumulativeApzCallbackTransform(nsIFrame* aFrame)
   }
   return delta;
 }
+
+/* static */ bool
+nsLayoutUtils::SupportsServoStyleBackend(nsIDocument* aDocument)
+{
+  return nsPresContext::StyloEnabled() &&
+         aDocument->IsHTMLOrXHTML() &&
+         static_cast<nsDocument*>(aDocument)->IsContentDocument();
+}
