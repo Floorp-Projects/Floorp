f6f246d7098235a590740073caab27af05af98e7
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-06-08 12:08:53 +0200

    Backed out changeset 74a7d364f5a3 (bug 1214710) for bustage on a CLOSED TREE

--- dom/media/MediaDecoderReader.h
+++ dom/media/MediaDecoderReader.h
@@ -290,18 +290,6 @@ public:
   // Notified by the OggReader during playback when chained ogg is detected.
   MediaEventSource<void>& OnMediaNotSeekable() { return mOnMediaNotSeekable; }
 
-  bool IsSuspended() const
-  {
-    MOZ_ASSERT(OnTaskQueue());
-    return mIsSuspended;
-  }
-
-  void SetIsSuspended(bool aState)
-  {
-    MOZ_ASSERT(OnTaskQueue());
-    mIsSuspended = aState;
-  }
-
 protected:
   virtual ~MediaDecoderReader();
 
@@ -447,7 +435,6 @@ private:
   // "discontinuity" in the stream. For example after a seek.
   bool mAudioDiscontinuity;
   bool mVideoDiscontinuity;
-  bool mIsSuspended;
 
   MediaEventListener mDataArrivedListener;
 };
