778983e4c304abc5467c12c300505f9bcb78a31a
Mike Conley <mconley@mozilla.com>
2016-06-08 16:42:04 -0400

    Bug 1261842 - browser_bug495058.js no longer needs to wait for remoteness flip on initial browser of new window. r=felipe
    MozReview-Commit-ID: Fl1OIsluS56
    
    --HG--
    extra : rebase_source : ef4a22423721d4ae2bc6449c56a4d5a4a0b465c0

--- browser/base/content/test/general/browser_bug495058.js
+++ browser/base/content/test/general/browser_bug495058.js
@@ -28,16 +28,6 @@ add_task(function*() {
       TestUtils.topicObserved("browser-delayed-startup-finished",
                               subject => subject == win);
 
-    if (gMultiProcessBrowser &&
-        E10SUtils.canLoadURIInProcess(uri, Ci.nsIXULRuntime.PROCESS_TYPE_CONTENT)) {
-      // Until bug 1261842 is fixed, the initial browser is going to be
-      // non-remote. If we're transitioning to a URL that can be loaded
-      // remotely, we'll need to wait until that remoteness switch is done
-      // before we send any framescripts down to the browser.
-      yield BrowserTestUtils.waitForEvent(tab, "TabRemotenessChange");
-      contentPainted = BrowserTestUtils.contentPainted(tab.linkedBrowser);
-    }
-
     yield Promise.all([delayedStartup, contentPainted]);
 
     Assert.equal(win.gBrowser.currentURI.spec, uri, uri + ": uri loaded in detached tab");
