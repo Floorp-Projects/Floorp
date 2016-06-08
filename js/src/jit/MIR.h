44fe5c7070cc874e085c4284f7710711d54f0d3f
Jan de Mooij <jdemooij@mozilla.com>
2016-06-08 15:18:59 +0200

    Bug 1266768 - Give MBoundsCheck an explicit type policy. r=lth

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -9379,7 +9379,7 @@ class MNot
 // (unsigned comparisons may be used).
 class MBoundsCheck
   : public MBinaryInstruction,
-    public NoTypePolicy::Data
+    public MixPolicy<IntPolicy<0>, IntPolicy<1>>::Data
 {
     // Range over which to perform the bounds check, may be modified by GVN.
     int32_t minimum_;
@@ -9450,7 +9450,7 @@ class MBoundsCheck
 // Bailout if index < minimum.
 class MBoundsCheckLower
   : public MUnaryInstruction,
-    public NoTypePolicy::Data
+    public IntPolicy<0>::Data
 {
     int32_t minimum_;
     bool fallible_;
