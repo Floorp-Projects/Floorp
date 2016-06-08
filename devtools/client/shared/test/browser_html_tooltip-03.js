94777feb05099e8d003d64fe6ce5712c140b0399
Julian Descottes <jdescottes@mozilla.com>
2016-06-08 13:32:15 +0200

    Bug 1266450 - part7: fix html tooltip  autofocus behavior;r=bgrins
    For autofocus tooltips, we need to find a focusable item in order
    to call focus() now that the tooltip content lives in the same
    document as the toolbox. Updated the corresponding test and made
    some superficial changes to HTMLTooltip.js.
    
    MozReview-Commit-ID: L61eIxgFm3d
    
    --HG--
    extra : rebase_source : dae55ebb8c888638b902aec000cce2ef0631ca04
    extra : source : c3ef26c17be6ef971f834c38d9aa69edc767d10c

--- devtools/client/shared/test/browser_html_tooltip-03.js
+++ devtools/client/shared/test/browser_html_tooltip-03.js
@@ -42,14 +42,13 @@ add_task(function* () {
 
 function* testNoAutoFocus(doc) {
   yield focusNode(doc, "#box4-input");
-  is(getFocusedDocument(doc), doc, "Focus is in the XUL document");
+  ok(doc.activeElement.closest("#box4-input"), "Focus is in the #box4-input");
 
   info("Test a tooltip without autofocus will not take focus");
   let tooltip = yield createTooltip(doc, false);
 
   yield showTooltip(tooltip, doc.getElementById("box1"));
-  is(getFocusedDocument(doc), doc, "Focus is still in the XUL document");
-  ok(doc.activeElement.closest("#box4-input"), "Focus is in the #box4-input");
+  ok(doc.activeElement.closest("#box4-input"), "Focus is still in the #box4-input");
 
   yield hideTooltip(tooltip);
   yield blurNode(doc, "#box4-input");
@@ -57,18 +56,16 @@ function* testNoAutoFocus(doc) {
 
 function* testAutoFocus(doc) {
   yield focusNode(doc, "#box4-input");
-  is(getFocusedDocument(doc), doc, "Focus is in the XUL document");
+  ok(doc.activeElement.closest("#box4-input"), "Focus is in the #box4-input");
 
   info("Test autofocus tooltip takes focus when displayed, " +
     "and restores the focus when hidden");
   let tooltip = yield createTooltip(doc, true);
 
   yield showTooltip(tooltip, doc.getElementById("box1"));
-  is(getFocusedDocument(doc), tooltip.panel.ownerDocument,
-    "Focus is in the tooltip document");
+  ok(doc.activeElement.closest(".tooltip-content"), "Focus is in the tooltip");
 
   yield hideTooltip(tooltip);
-  is(getFocusedDocument(doc), doc, "Focus is back in the XUL document");
   ok(doc.activeElement.closest("#box4-input"), "Focus is in the #box4-input");
 
   info("Blur the textbox before moving to the next test to reset the state.");
@@ -77,36 +74,24 @@ function* testAutoFocus(doc) {
 
 function* testAutoFocusPreservesFocusChange(doc) {
   yield focusNode(doc, "#box4-input");
-  is(getFocusedDocument(doc), doc, "Focus is in the XUL document");
+  ok(doc.activeElement.closest("#box4-input"), "Focus is still in the #box3-input");
 
   info("Test autofocus tooltip takes focus when displayed, " +
     "but does not try to restore the active element if it is not focused when hidden");
   let tooltip = yield createTooltip(doc, true);
 
   yield showTooltip(tooltip, doc.getElementById("box1"));
-  is(getFocusedDocument(doc), tooltip.panel.ownerDocument,
-    "Focus is in the tooltip document");
+  ok(doc.activeElement.closest(".tooltip-content"), "Focus is in the tooltip");
 
   info("Move the focus to #box3-input while the tooltip is displayed");
   yield focusNode(doc, "#box3-input");
-  is(getFocusedDocument(doc), doc, "Focus is back in the XUL document");
-  ok(doc.activeElement.closest("#box3-input"), "Focus is in the #box3-input");
+  ok(doc.activeElement.closest("#box3-input"), "Focus moved to the #box3-input");
 
   yield hideTooltip(tooltip);
-  is(getFocusedDocument(doc), doc, "Focus is still in the XUL document");
-
   ok(doc.activeElement.closest("#box3-input"), "Focus is still in the #box3-input");
 
   info("Blur the textbox before moving to the next test to reset the state.");
   yield blurNode(doc, "#box3-input");
-}
-
-function getFocusedDocument(doc) {
-  let activeElement = doc.activeElement;
-  while (activeElement && activeElement.contentDocument) {
-    activeElement = activeElement.contentDocument.activeElement;
-  }
-  return activeElement.ownerDocument;
 }
 
 /**
@@ -143,7 +128,10 @@ function blurNode(doc, selector) {
 function* createTooltip(doc, autofocus) {
   let tooltip = new HTMLTooltip({doc}, {autofocus});
   let div = doc.createElementNS(HTML_NS, "div");
+  div.classList.add("tooltip-content");
   div.style.height = "50px";
+  div.innerHTML = '<input type="text"></input>';
+
   yield tooltip.setContent(div, 150, 50);
   return tooltip;
 }
