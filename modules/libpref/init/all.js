57489318831d7df22abdc45429d5c7ebeffa83de
Mike de Boer <mdeboer@mozilla.com>
2016-06-09 13:30:46 +0200

    Bug 384458 - part 2: change the matches count timeout to 100ms, up the limit to a 1000 counts and capture the state of 'Highlight All' in a pref which will be TRUE by default in the browser. r=jaws

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -858,8 +858,9 @@ pref("accessibility.typeaheadfind.prefillwithselection", false);
 #else
 pref("accessibility.typeaheadfind.prefillwithselection", true);
 #endif
-pref("accessibility.typeaheadfind.matchesCountTimeout", 250);
-pref("accessibility.typeaheadfind.matchesCountLimit", 100);
+pref("accessibility.typeaheadfind.matchesCountTimeout", 100);
+pref("accessibility.typeaheadfind.matchesCountLimit", 1000);
+pref("findbar.highlightAll", false);
 
 // use Mac OS X Appearance panel text smoothing setting when rendering text, disabled by default
 pref("gfx.use_text_smoothing_setting", false);
