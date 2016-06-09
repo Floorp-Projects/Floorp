42d73e42ca40f8bbf64b5945462e5ab5d2d01e2a
Andrea Marchesini <amarchesini@mozilla.com>
2016-06-09 19:05:16 +0200

    Bug 1278838 - move workers/performance tests in dom/performance/tests, r=smaug
    --HG--
    rename : dom/workers/test/sharedworker_performance_user_timing.js => dom/performance/tests/sharedworker_performance_user_timing.js
    rename : dom/workers/test/test_sharedWorker_performance_user_timing.html => dom/performance/tests/test_sharedWorker_performance_user_timing.html
    rename : dom/workers/test/test_performance_observer.html => dom/performance/tests/test_worker_observer.html
    rename : dom/workers/test/test_worker_performance_now.html => dom/performance/tests/test_worker_performance_now.html
    rename : dom/workers/test/test_worker_performance_now.js => dom/performance/tests/test_worker_performance_now.js
    rename : dom/workers/test/test_performance_user_timing.html => dom/performance/tests/test_worker_user_timing.html
    rename : dom/workers/test/performance_observer.html => dom/performance/tests/worker_performance_observer.html
    rename : dom/workers/test/worker_performance_observer.js => dom/performance/tests/worker_performance_observer.js
    rename : dom/workers/test/worker_performance_user_timing.js => dom/performance/tests/worker_performance_user_timing.js

--- dom/performance/tests/test_worker_performance_now.js
+++ dom/performance/tests/test_worker_performance_now.js
@@ -1,3 +1,25 @@
+function ok(a, msg) {
+  dump("OK: " + !!a + "  =>  " + a + ": " + msg + "\n");
+  postMessage({type: 'status', status: !!a, msg: a + ": " + msg });
+}
+
+function workerTestDone() {
+  postMessage({ type: 'finish' });
+}
+
+function workerTestGetOSCPU(cb) {
+  addEventListener('message', function workerTestGetOSCPUCB(e) {
+    if (e.data.type !== 'returnOSCPU') {
+      return;
+    }
+    removeEventListener('message', workerTestGetOSCPUCB);
+    cb(e.data.result);
+  });
+  postMessage({
+    type: 'getOSCPU'
+  });
+}
+
 ok(self.performance, "Performance object should exist.");
 ok(typeof self.performance.now == 'function', "Performance object should have a 'now' method.");
 var n = self.performance.now(), d = Date.now();
