2d871e19221d9b72740cd68a790ef8133e284738
Kit Cambridge <kcambridge@mozilla.com>
2016-06-09 17:13:39 -0700

    Bug 1279306 - Use the production push server URL on Android. r=margaret a=gchang
    MozReview-Commit-ID: Fy347g00Lx0

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -901,7 +901,7 @@ pref("dom.serviceWorkers.openWindow.enabled", true);
 pref("dom.push.debug", false);
 // The upstream autopush endpoint must have the Google API key corresponding to
 // the App's sender ID; we bake this assumption directly into the URL.
-pref("dom.push.serverURL", "https://updates-autopush.stage.mozaws.net/v1/gcm/@MOZ_ANDROID_GCM_SENDERID@");
+pref("dom.push.serverURL", "https://updates.push.services.mozilla.com/v1/gcm/@MOZ_ANDROID_GCM_SENDERID@");
 
 #ifdef MOZ_ANDROID_GCM
 pref("dom.push.enabled", true);
