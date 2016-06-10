d46f71b8c0790e8f70c6d023f62ac91354ed662e
Gregory Szorc <gps@mozilla.com>
2016-06-10 09:39:29 -0700

    Bug 1279564 - Return a 2-tuple from get_state_dir(); r=glandium
    This matches the implementation from mach_bootstrap.py.
    
    MozReview-Commit-ID: 8kZCKuIsAMQ
    
    --HG--
    extra : rebase_source : 59b1f3d595e1663603363bb712da9cb74c3ba0e0
    extra : amend_source : e95774ade1c7d28ba1d880b6b9ef1d64eaa618a8

--- python/mozboot/mozboot/util.py
+++ python/mozboot/mozboot/util.py
@@ -6,7 +6,15 @@ import os
 
 
 def get_state_dir():
-    """Obtain path to a directory to hold state."""
+    """Obtain path to a directory to hold state.
+
+    Returns a tuple of the path and a bool indicating whether the
+    value came from an environment variable.
+    """
     state_user_dir = os.path.expanduser('~/.mozbuild')
     state_env_dir = os.environ.get('MOZBUILD_STATE_PATH')
-    return state_env_dir or state_user_dir
+
+    if state_env_dir:
+        return state_env_dir, True
+    else:
+        return state_user_dir, False
