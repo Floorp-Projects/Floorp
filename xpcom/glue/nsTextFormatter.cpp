083b9ba519fc89a0c2869a35e32fffa18ee9d78f
Nomis101 <Nomis101@web.de>
2016-06-11 10:41:45 +0200

    Bug 1279734 - "Fix two comment typos in nsTextFormatter.cpp". r=nfroyd

--- xpcom/glue/nsTextFormatter.cpp
+++ xpcom/glue/nsTextFormatter.cpp
@@ -631,7 +631,7 @@ BuildArgArray(const char16_t* aFmt, va_list aAp, int* aRv,
       continue;
     }
     cn = 0;
-    /* should imporve error check later */
+    /* should improve error check later */
     while (c && c != '$') {
       cn = cn * 10 + c - '0';
       c = *p++;
@@ -881,7 +881,7 @@ dosprintf(SprintfState* aState, const char16_t* aFmt, va_list aAp)
     if (nas) {
       /* the aFmt contains the Numbered Arguments feature */
       i = 0;
-      /* should imporve error check later */
+      /* should improve error check later */
       while (c && c != '$') {
         i = (i * 10) + (c - '0');
         c = *aFmt++;
