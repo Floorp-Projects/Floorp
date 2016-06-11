61a8d4f0c19513a0eb3a8be729d26b37096baef7
Robert Strong <robert.bugzilla@gmail.com>
2016-06-11 08:31:10 -0700

    Bug 1168743 - client changes - Only use env vars in the test updater and general app update cleanup. r=mhowell

--- toolkit/mozapps/update/common/updatelogging.cpp
+++ toolkit/mozapps/update/common/updatelogging.cpp
@@ -19,59 +19,82 @@ UpdateLog::UpdateLog() : logFP(nullptr)
 }
 
 void UpdateLog::Init(NS_tchar* sourcePath,
-                     const NS_tchar* fileName,
-                     const NS_tchar* alternateFileName,
-                     bool append)
+                     const NS_tchar* fileName)
 {
-  if (logFP)
+  if (logFP) {
     return;
+  }
 
-#ifdef XP_WIN
-  GetTempFileNameW(sourcePath, L"log", 0, mTmpFilePath);
   int dstFilePathLen = NS_tsnprintf(mDstFilePath,
     sizeof(mDstFilePath)/sizeof(mDstFilePath[0]),
-    NS_T("%s/%s"), sourcePath, append ? alternateFileName : fileName);
-
+    NS_T("%s/%s"), sourcePath, fileName);
   // If the destination path was over the length limit,
   // disable logging by skipping opening the file and setting logFP.
   if ((dstFilePathLen > 0) &&
       (dstFilePathLen <
          static_cast<int>(sizeof(mDstFilePath)/sizeof(mDstFilePath[0])))) {
-    if (append) {
-      MoveFileExW(mDstFilePath, mTmpFilePath, MOVEFILE_REPLACE_EXISTING);
-    }
-    logFP = NS_tfopen(mTmpFilePath, append ? NS_T("a") : NS_T("w"));
+#ifdef XP_WIN
+    GetTempFileNameW(sourcePath, L"log", 0, mTmpFilePath);
+    logFP = NS_tfopen(mTmpFilePath, NS_T("w"));
 
     // Delete this file now so it is possible to tell from the unelevated
     // updater process if the elevated updater process has written the log.
     DeleteFileW(mDstFilePath);
-  }
+#elif XP_MACOSX
+    logFP = NS_tfopen(mDstFilePath, NS_T("w"));
 #else
-  int dstFilePathLen = 0;
-  if (alternateFileName && NS_taccess(mDstFilePath, F_OK)) {
-    dstFilePathLen = NS_tsnprintf(mDstFilePath,
-      sizeof(mDstFilePath)/sizeof(mDstFilePath[0]),
-      NS_T("%s/%s"), sourcePath, alternateFileName);
-  } else {
-    dstFilePathLen = NS_tsnprintf(mDstFilePath,
-      sizeof(mDstFilePath)/sizeof(mDstFilePath[0]),
-      NS_T("%s/%s"), sourcePath, fileName);
-  }
-
-  // If the destination path was over the length limit,
-  // disable logging by skipping opening the file and setting logFP.
-  if ((dstFilePathLen > 0) &&
-      (dstFilePathLen <
-         static_cast<int>(sizeof(mDstFilePath)/sizeof(mDstFilePath[0])))) {
-    logFP = NS_tfopen(mDstFilePath, append ? NS_T("a") : NS_T("w"));
-  }
+    // On platforms that have an updates directory in the installation directory
+    // (e.g. platforms other than Windows and Mac) the update log is written to
+    // a temporary file and then to the update log file. This is needed since
+    // the installation directory is moved during a replace request. This can be
+    // removed when the platform's updates directory is located outside of the
+    // installation directory.
+    logFP = tmpfile();
 #endif
+  }
 }
 
 void UpdateLog::Finish()
 {
-  if (!logFP)
+  if (!logFP) {
     return;
+  }
+
+#if !defined(XP_WIN) && !defined(XP_MACOSX)
+  const int blockSize = 1024;
+  char buffer[blockSize];
+  fflush(logFP);
+  rewind(logFP);
+
+  FILE *updateLogFP = NS_tfopen(mDstFilePath, NS_T("wb+"));
+  while (!feof(logFP)) {
+    size_t read = fread(buffer, 1, blockSize, logFP);
+    if (ferror(logFP)) {
+      fclose(logFP);
+      logFP = nullptr;
+      fclose(updateLogFP);
+      updateLogFP = nullptr;
+      return;
+    }
+
+    size_t written = 0;
+
+    while (written < read) {
+      size_t chunkWritten = fwrite(buffer, 1, read - written, updateLogFP);
+      if (chunkWritten <= 0) {
+        fclose(logFP);
+        logFP = nullptr;
+        fclose(updateLogFP);
+        updateLogFP = nullptr;
+        return;
+      }
+
+      written += chunkWritten;
+    }
+  }
+  fclose(updateLogFP);
+  updateLogFP = nullptr;
+#endif
 
   fclose(logFP);
   logFP = nullptr;
@@ -89,16 +112,18 @@ void UpdateLog::Finish()
 
 void UpdateLog::Flush()
 {
-  if (!logFP)
+  if (!logFP) {
     return;
+  }
 
   fflush(logFP);
 }
 
 void UpdateLog::Printf(const char *fmt, ... )
 {
-  if (!logFP)
+  if (!logFP) {
     return;
+  }
 
   va_list ap;
   va_start(ap, fmt);
@@ -109,8 +134,9 @@ void UpdateLog::Printf(const char *fmt, ... )
 
 void UpdateLog::WarnPrintf(const char *fmt, ... )
 {
-  if (!logFP)
+  if (!logFP) {
     return;
+  }
 
   va_list ap;
   va_start(ap, fmt);
