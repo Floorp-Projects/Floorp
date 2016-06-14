114cd0596743d570b37d21888f96a4b030b2638f
Jeff Gilbert <jgilbert@mozilla.com>
2016-06-14 09:01:43 -0700

    Bug 1250710 - Add stub. - r=ethlin
    MozReview-Commit-ID: B555z5b3pyx

--- dom/canvas/WebGL2Context.h
+++ dom/canvas/WebGL2Context.h
@@ -55,7 +55,16 @@ public:
                           const dom::Nullable<dom::ArrayBuffer>& maybeData);
     void GetBufferSubData(GLenum target, GLintptr offset,
                           const dom::SharedArrayBuffer& data);
+    void ReadPixels(GLint x, GLint y, GLsizei width, GLsizei height, GLenum format,
+                    GLenum type, WebGLsizeiptr offset, ErrorResult& out_error);
 
+    void ReadPixels(GLint x, GLint y, GLsizei width, GLsizei height,
+                    GLenum format, GLenum type,
+                    const dom::Nullable<dom::ArrayBufferView>& pixels,
+                    ErrorResult& out_error)
+    {
+        WebGLContext::ReadPixels(x, y, width, height, format, type, pixels, out_error);
+    }
 
     // -------------------------------------------------------------------------
     // Framebuffer objects - WebGL2ContextFramebuffers.cpp
