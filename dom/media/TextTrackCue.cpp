70ff14fcaf16ec63a8c0c59b74559ae36a266c56
Alastor Wu <alwu@mozilla.com>
2016-06-14 11:57:18 +0100

    Bug 1276831 - part1 : VTTCue's position should be double or auto keyword. r=rillian,smaug
    MozReview-Commit-ID: 459CjJUPpW9

--- dom/media/TextTrackCue.cpp
+++ dom/media/TextTrackCue.cpp
@@ -33,7 +33,7 @@ StaticRefPtr<nsIWebVTTParserWrapper> TextTrackCue::sParserWrapper;
 void
 TextTrackCue::SetDefaultCueSettings()
 {
-  mPosition = 50;
+  mPositionIsAutoKeyword = true;
   mPositionAlign = PositionAlignSetting::Center;
   mSize = 100.0;
   mPauseOnExit = false;
@@ -202,6 +202,20 @@ TextTrackCue::ComputedLine()
   }
 
   return (-1.0) * showingTracksNum;
+}
+
+double
+TextTrackCue::ComputedPosition()
+{
+  // See spec https://w3c.github.io/webvtt/#cue-computed-position
+  if (!mPositionIsAutoKeyword) {
+    return mPosition;
+  } else if (mAlign == AlignSetting::Left) {
+    return 0.0;
+  } else if (mAlign == AlignSetting::Right) {
+    return 100.0;
+  }
+  return 50.0;
 }
 
 PositionAlignSetting
