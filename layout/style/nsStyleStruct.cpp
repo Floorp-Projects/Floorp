c8b8cf7b073e479d2849dea4a0da814f773679b3
cku <cku@mozilla.com>
2016-06-14 12:36:01 +0100

    Bug 1273804 - Use nsChangeHint_RepaintFrame hint for position change of a mask layer r=heycam
    MozReview-Commit-ID: 4W9Qmk6MVri

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -1288,7 +1288,7 @@ nsStyleSVGReset::CalcDifference(const nsStyleSVGReset& aOther) const
     hint |= nsChangeHint_RepaintFrame;
   }
 
-  hint |= mMask.CalcDifference(aOther.mMask);
+  hint |= mMask.CalcDifference(aOther.mMask, nsChangeHint_RepaintFrame);
 
   return hint;
 }
@@ -2303,7 +2303,8 @@ nsStyleImageLayers::nsStyleImageLayers(const nsStyleImageLayers &aSource)
 }
 
 nsChangeHint
-nsStyleImageLayers::CalcDifference(const nsStyleImageLayers& aOther) const
+nsStyleImageLayers::CalcDifference(const nsStyleImageLayers& aOther,
+                                   nsChangeHint aPositionChangeHint) const
 {
   nsChangeHint hint = nsChangeHint(0);
 
@@ -2317,7 +2318,8 @@ nsStyleImageLayers::CalcDifference(const nsStyleImageLayers& aOther) const
   NS_FOR_VISIBLE_IMAGE_LAYERS_BACK_TO_FRONT(i, moreLayers) {
     if (i < lessLayers.mImageCount) {
       nsChangeHint layerDifference =
-        moreLayers.mLayers[i].CalcDifference(lessLayers.mLayers[i]);
+        moreLayers.mLayers[i].CalcDifference(lessLayers.mLayers[i],
+                                             aPositionChangeHint);
       hint |= layerDifference;
       if (layerDifference &&
           ((moreLayers.mLayers[i].mImage.GetType() == eStyleImageType_Element) ||
@@ -2592,7 +2594,8 @@ nsStyleImageLayers::Layer::operator==(const Layer& aOther) const
 }
 
 nsChangeHint
-nsStyleImageLayers::Layer::CalcDifference(const nsStyleImageLayers::Layer& aOther) const
+nsStyleImageLayers::Layer::CalcDifference(const nsStyleImageLayers::Layer& aOther,
+                                          nsChangeHint aPositionChangeHint) const
 {
   nsChangeHint hint = nsChangeHint(0);
   if (!EqualURIs(mSourceURI, aOther.mSourceURI)) {
@@ -2614,7 +2617,7 @@ nsStyleImageLayers::Layer::CalcDifference(const nsStyleImageLayers::Layer& aOthe
   }
 
   if (mPosition != aOther.mPosition) {
-    hint |= nsChangeHint_UpdateBackgroundPosition;
+    hint |= aPositionChangeHint;
   }
 
   return hint;
@@ -2662,7 +2665,8 @@ nsStyleBackground::CalcDifference(const nsStyleBackground& aOther) const
     hint |= nsChangeHint_RepaintFrame;
   }
 
-  hint |= mImage.CalcDifference(aOther.mImage);
+  hint |= mImage.CalcDifference(aOther.mImage,
+                                nsChangeHint_UpdateBackgroundPosition);
 
   return hint;
 }
