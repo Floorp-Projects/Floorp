917d610b4947a014ee6eeed27ac71423ea8f919f
Cervantes Yu <cyu@mozilla.com>
2016-06-16 00:57:46 +0800

    Bug 1275398 - Use shmem for sending image data in IPCDataTransfer. r=nical, a=lizzard
    MozReview-Commit-ID: 9XETnSBXxEW

--- widget/nsClipboardProxy.cpp
+++ widget/nsClipboardProxy.cpp
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/dom/ContentChild.h"
+#include "mozilla/unused.h"
 #include "nsClipboardProxy.h"
 #include "nsISupportsPrimitives.h"
 #include "nsCOMPtr.h"
@@ -80,15 +81,18 @@ nsClipboardProxy::GetData(nsITransferable *aTransferable, int32_t aWhichClipboar
       rv = aTransferable->SetTransferData(item.flavor().get(), dataWrapper,
                                           data.Length() * sizeof(char16_t));
       NS_ENSURE_SUCCESS(rv, rv);
-    } else if (item.data().type() == IPCDataTransferData::TnsCString) {
+    } else if (item.data().type() == IPCDataTransferData::TShmem) {
       // If this is an image, convert it into an nsIInputStream.
       nsCString flavor = item.flavor();
+      mozilla::ipc::Shmem data = item.data().get_Shmem();
       if (flavor.EqualsLiteral(kJPEGImageMime) ||
           flavor.EqualsLiteral(kJPGImageMime) ||
           flavor.EqualsLiteral(kPNGImageMime) ||
           flavor.EqualsLiteral(kGIFImageMime)) {
         nsCOMPtr<nsIInputStream> stream;
-        NS_NewCStringInputStream(getter_AddRefs(stream), item.data().get_nsCString());
+
+        NS_NewCStringInputStream(getter_AddRefs(stream),
+                                 nsDependentCString(data.get<char>(), data.Size<char>()));
 
         rv = aTransferable->SetTransferData(flavor.get(), stream, sizeof(nsISupports*));
         NS_ENSURE_SUCCESS(rv, rv);
@@ -99,14 +103,15 @@ nsClipboardProxy::GetData(nsITransferable *aTransferable, int32_t aWhichClipboar
           do_CreateInstance(NS_SUPPORTS_CSTRING_CONTRACTID, &rv);
         NS_ENSURE_SUCCESS(rv, rv);
 
-        nsCString data = item.data().get_nsCString();
-        rv = dataWrapper->SetData(data);
+        rv = dataWrapper->SetData(nsDependentCString(data.get<char>(), data.Size<char>()));
         NS_ENSURE_SUCCESS(rv, rv);
 
         rv = aTransferable->SetTransferData(item.flavor().get(), dataWrapper,
-                                            data.Length());
+                                            data.Size<char>());
         NS_ENSURE_SUCCESS(rv, rv);
       }
+
+      mozilla::Unused << ContentChild::GetSingleton()->DeallocShmem(data);
     }
   }
 
