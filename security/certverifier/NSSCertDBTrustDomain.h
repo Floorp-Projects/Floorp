28c3fcf9205fc9705f3c8d93db3519720093622f
Sergei Chernov <sergei.cv@ndivi.com>
2016-06-15 11:11:00 +0300

    Bug 1275238 - Certificate Transparency support in mozilla::pkix; r=keeler
    MozReview-Commit-ID: HZwzSgxarTw

--- security/certverifier/NSSCertDBTrustDomain.h
+++ security/certverifier/NSSCertDBTrustDomain.h
@@ -141,6 +141,10 @@ public:
   virtual Result IsChainValid(const mozilla::pkix::DERArray& certChain,
                               mozilla::pkix::Time time) override;
 
+  virtual void NoteAuxiliaryExtension(
+                   mozilla::pkix::AuxiliaryExtension extension,
+                   mozilla::pkix::Input extensionData) override;
+
   CertVerifier::OCSPStaplingStatus GetOCSPStaplingStatus() const
   {
     return mOCSPStaplingStatus;
