48c1906bd411fa975b37b37bf83a9f6dc7fa5fbf
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2016-06-15 16:27:18 +0000

    Bug 1264948 - MBasicBlock::addPredecessor, check for OOMs when allocating Phi nodes. r=h4writer

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -7548,6 +7548,9 @@ class MPhi final
     static MPhi* New(TempAllocator& alloc, MIRType resultType = MIRType::Value) {
         return new(alloc) MPhi(alloc, resultType);
     }
+    static MPhi* New(TempAllocator::Fallible alloc, MIRType resultType = MIRType::Value) {
+        return new(alloc) MPhi(alloc.alloc, resultType);
+    }
 
     void removeOperand(size_t index);
     void removeAllOperands();
