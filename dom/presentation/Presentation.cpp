98b363a638a3cc15bac2ff0d486e77e3519b621f
Thomas Zimmermann <tdz@users.sourceforge.net>
2016-06-15 10:59:48 +0100

    Bug 1276927: Fix DOM code to build with Android NDK, r=fabrice
    MozReview-Commit-ID: KwW9O9DLFuy

--- dom/presentation/Presentation.cpp
+++ dom/presentation/Presentation.cpp
@@ -4,6 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <ctype.h>
 #include "mozilla/dom/PresentationBinding.h"
 #include "mozilla/dom/Promise.h"
 #include "nsContentUtils.h"
