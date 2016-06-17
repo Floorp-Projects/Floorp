6c89aaf37eb1a45a8e4feed077b4057a566ca17c
Jan-Ivar Bruaroey <jib@mozilla.com>
2016-06-17 15:20:10 -0400

    Bug 1213517 - Normalize even more of the constraints code. r=padenot
    MozReview-Commit-ID: 1XjdHXKYOmP

--- dom/media/webrtc/MediaEngineWebRTC.h
+++ dom/media/webrtc/MediaEngineWebRTC.h
@@ -135,7 +135,7 @@ public:
     return NS_ERROR_NOT_IMPLEMENTED;
   }
   uint32_t GetBestFitnessDistance(
-    const nsTArray<const dom::MediaTrackConstraintSet*>& aConstraintSets,
+    const nsTArray<const NormalizedConstraintSet*>& aConstraintSets,
     const nsString& aDeviceId) override;
 
 protected:
@@ -502,7 +502,7 @@ public:
   }
 
   uint32_t GetBestFitnessDistance(
-      const nsTArray<const dom::MediaTrackConstraintSet*>& aConstraintSets,
+      const nsTArray<const NormalizedConstraintSet*>& aConstraintSets,
       const nsString& aDeviceId) override;
 
   // VoEMediaProcess.
