55e7a44550be98f268363f083fe4d4bd223a3e0e
Andrew Swan <aswan@mozilla.com>
2016-06-17 07:27:07 -0700

    Bug 1272522 Handle stderr of native app r=kmag
    MozReview-Commit-ID: 5qGw83uTYTu
    
    --HG--
    extra : rebase_source : 748b4d02defbdb412cb81709e2ded27347b1d797

--- toolkit/components/extensions/test/mochitest/head.js
+++ toolkit/components/extensions/test/mochitest/head.js
@@ -1,6 +1,8 @@
 "use strict";
 
-/* exported waitForLoad */
+Components.utils.import("resource://gre/modules/Task.jsm");
+
+/* exported waitForLoad, promiseConsoleOutput */
 
 function waitForLoad(win) {
   return new Promise(resolve => {
@@ -10,3 +12,31 @@ function waitForLoad(win) {
     }, true);
   });
 }
+
+var promiseConsoleOutput = Task.async(function* (task) {
+  const DONE = "=== extension test console listener done ===";
+
+  let listener;
+  let messages = [];
+  let awaitListener = new Promise(resolve => {
+    listener = msg => {
+      if (msg == DONE) {
+        resolve();
+      } else if (msg instanceof Ci.nsIConsoleMessage) {
+        messages.push(msg.message);
+      }
+    };
+  });
+
+  Services.console.registerListener(listener);
+  try {
+    let result = yield task();
+
+    Services.console.logStringMessage(DONE);
+    yield awaitListener;
+
+    return {messages, result};
+  } finally {
+    Services.console.unregisterListener(listener);
+  }
+});
