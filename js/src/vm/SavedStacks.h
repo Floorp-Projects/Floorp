031c841ecf66be75677dfc085e47d7eac8e1082f
Nick Fitzgerald <fitzgen@gmail.com>
2016-06-17 13:52:43 +0100

    Bug 1267551 - Stop trying to align method declarations in js/src/vm/SavedStacks.h; r=jimb
    It just makes unrelated changes mess up blame when they fix indentation, or it
    makes the indentation go bad to preserve blame. Neither is a good outcome...

--- js/src/vm/SavedStacks.h
+++ js/src/vm/SavedStacks.h
@@ -162,24 +162,24 @@ class SavedStacks {
         creatingSavedFrame(false)
     { }
 
-    MOZ_MUST_USE bool     init();
-    bool     initialized() const { return frames.initialized(); }
-    MOZ_MUST_USE bool     saveCurrentStack(JSContext* cx, MutableHandleSavedFrame frame,
-                                           unsigned maxFrameCount = 0);
-    MOZ_MUST_USE bool     copyAsyncStack(JSContext* cx, HandleObject asyncStack,
-                                         HandleString asyncCause,
-                                         MutableHandleSavedFrame adoptedStack,
-                                         unsigned maxFrameCount = 0);
-    void     sweep();
-    void     trace(JSTracer* trc);
+    MOZ_MUST_USE bool init();
+    bool initialized() const { return frames.initialized(); }
+    MOZ_MUST_USE bool saveCurrentStack(JSContext* cx, MutableHandleSavedFrame frame,
+                                       unsigned maxFrameCount = 0);
+    MOZ_MUST_USE bool copyAsyncStack(JSContext* cx, HandleObject asyncStack,
+                                     HandleString asyncCause,
+                                     MutableHandleSavedFrame adoptedStack,
+                                     unsigned maxFrameCount = 0);
+    void sweep();
+    void trace(JSTracer* trc);
     uint32_t count();
-    void     clear();
-    void     chooseSamplingProbability(JSCompartment*);
+    void clear();
+    void chooseSamplingProbability(JSCompartment*);
 
     // Set the sampling random number generator's state to |state0| and
     // |state1|. One or the other must be non-zero. See the comments for
     // mozilla::non_crypto::XorShift128PlusRNG::setState for details.
-    void     setRNGState(uint64_t state0, uint64_t state1) { bernoulli.setRandomState(state0, state1); }
+    void setRNGState(uint64_t state0, uint64_t state1) { bernoulli.setRandomState(state0, state1); }
 
     size_t sizeOfExcludingThis(mozilla::MallocSizeOf mallocSizeOf);
 
@@ -219,13 +219,13 @@ class SavedStacks {
         }
     };
 
-    MOZ_MUST_USE bool        insertFrames(JSContext* cx, FrameIter& iter,
-                                          MutableHandleSavedFrame frame,
-                                          unsigned maxFrameCount = 0);
-    MOZ_MUST_USE bool        adoptAsyncStack(JSContext* cx, HandleSavedFrame asyncStack,
-                                             HandleString asyncCause,
-                                             MutableHandleSavedFrame adoptedStack,
-                                             unsigned maxFrameCount);
+    MOZ_MUST_USE bool insertFrames(JSContext* cx, FrameIter& iter,
+                                   MutableHandleSavedFrame frame,
+                                   unsigned maxFrameCount = 0);
+    MOZ_MUST_USE bool adoptAsyncStack(JSContext* cx, HandleSavedFrame asyncStack,
+                                      HandleString asyncCause,
+                                      MutableHandleSavedFrame adoptedStack,
+                                      unsigned maxFrameCount);
     SavedFrame* getOrCreateSavedFrame(JSContext* cx, SavedFrame::HandleLookup lookup);
     SavedFrame* createFrameFromLookup(JSContext* cx, SavedFrame::HandleLookup lookup);
 
