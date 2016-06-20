a95b5bd95c148b2292ec8796b71b67a66f0d89d8
Bob Silverberg <bsilverberg@mozilla.com>
2016-06-20 16:50:39 +0100

    Bug 1277686 - For uplift to Aurora and Beta - Issue a warning for manifests with any "incognito" property other than "spanning", r=kmag a=sylvestre
    --HG--
    extra : rebase_source : 4bd4d3b68dc1bdb0c527d9844de39721ef73a55d
    extra : source : 153c74d40a43a648d9a60c91af8042f5112be46f

---
+++ toolkit/components/extensions/test/xpcshell/test_ext_manifest_incognito.js
@@ -0,0 +1,27 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+"use strict";
+
+
+add_task(function* test_manifest_incognito() {
+  let normalized = yield normalizeManifest({
+    "incognito": "spanning",
+  });
+
+  equal(normalized.error, undefined, "Should not have an error");
+  equal(normalized.errors.length, 0, "Should not have warnings");
+  equal(normalized.value.incognito,
+        "spanning",
+        "Should have the expected incognito string");
+
+  normalized = yield normalizeManifest({
+    "incognito": "split",
+  });
+
+  equal(normalized.error, undefined, "Should not have an error");
+  Assert.deepEqual(normalized.errors,
+                   ['Error processing incognito: Invalid enumeration value "split"'],
+                   "Should have the expected warning");
+  equal(normalized.value.incognito, null,
+        "Invalid incognito string should be omitted");
+});
