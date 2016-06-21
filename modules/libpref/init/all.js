22f2166c2f8554e7bab91abb686683b78b9db11e
Kartikaya Gupta <kgupta@mozilla.com>
2016-06-21 14:46:34 -0400

    Bug 1281192 - Set velocity bias to 0 on Fennec. r=rbarker, a=sylvestre
    MozReview-Commit-ID: HupL2wF6rFK

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -643,7 +643,7 @@ pref("apz.record_checkerboarding", false);
 pref("apz.test.logging_enabled", false);
 pref("apz.touch_start_tolerance", "0.1");
 pref("apz.touch_move_tolerance", "0.03");
-pref("apz.velocity_bias", "1.0");
+pref("apz.velocity_bias", "0.0");
 pref("apz.velocity_relevance_time_ms", 150);
 pref("apz.x_skate_highmem_adjust", "0.0");
 pref("apz.y_skate_highmem_adjust", "0.0");
