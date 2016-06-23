6344c957c13609446ba26d5d2aca21253f84b13e
Boris Zbarsky <bzbarsky@mit.edu>
2016-06-23 00:22:30 -0400

    Bug 1276310 part 3.  Get rid of the mException member of XPCContext.  r=bholley
    The member is only set by SetException(), which is called with non-null in
    precisely one place.  This place is always immediately followed by a call to
    nsXPCWrappedJSClass::CheckForException which proceeds to grab the exception from
    the XPCContext and call SetException(null).  So basically, it's all a weird
    out-of-band argument to CheckForException.

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -678,17 +678,6 @@ public:
     XPCJSRuntime* GetRuntime() const {return mRuntime;}
     JSContext* GetJSContext() const {return mJSContext;}
 
-    nsresult GetException(nsIException** e)
-        {
-            nsCOMPtr<nsIException> rval = mException;
-            rval.forget(e);
-            return NS_OK;
-        }
-    void SetException(nsIException* e)
-        {
-            mException = e;
-        }
-
     void DebugDump(int16_t depth);
 
     ~XPCContext();
@@ -702,7 +691,6 @@ private:
 private:
     XPCJSRuntime* mRuntime;
     JSContext*  mJSContext;
-    nsCOMPtr<nsIException> mException;
     bool mErrorUnreported;
 };
 
@@ -2235,10 +2223,14 @@ public:
                                               nsIVariant** aResult);
 
 private:
+    // aSyntheticException, if not null, is the exception we should be using.
+    // If null, look for an exception on the JSContext hanging off the
+    // XPCCallContext.
     static nsresult CheckForException(XPCCallContext & ccx,
                                       mozilla::dom::AutoEntryScript& aes,
                                       const char * aPropertyName,
-                                      const char * anInterfaceName);
+                                      const char * anInterfaceName,
+                                      nsIException* aSyntheticException = nullptr);
     virtual ~nsXPCWrappedJSClass();
 
     nsXPCWrappedJSClass();   // not implemented
