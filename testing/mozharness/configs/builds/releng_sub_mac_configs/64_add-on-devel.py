a31c0637efb11f1ba353661e5af4173f7b97a510
Kim Moir <kmoir>
2016-06-23 10:26:00 +0200

    Bug 1135781 - "generate builds per checkin on beta/release/esr that allow installation of unsigned addons". a=sylvestre

---
+++ testing/mozharness/configs/builds/releng_sub_mac_configs/64_add-on-devel.py
@@ -0,0 +1,44 @@
+import os
+
+config = {
+    'default_actions': [
+        'clobber',
+        'clone-tools',
+        'checkout-sources',
+#        'setup-mock',
+        'build',
+        'upload-files',
+#        'sendchange',
+        'check-test',
+#        'generate-build-stats',
+#        'update',
+    ],
+    'stage_platform': 'macosx64-add-on-devel',
+    'publish_nightly_en_US_routes': False,
+    'build_type': 'add-on-devel',
+    'platform_supports_post_upload_to_latest': False,
+    'objdir': 'obj-add-on-devel',
+    'enable_signing': False,
+    'enable_talos_sendchange': False,
+    #### 64 bit build specific #####
+    'env': {
+        'MOZBUILD_STATE_PATH': os.path.join(os.getcwd(), '.mozbuild'),
+        'MOZ_AUTOMATION': '1',
+        'HG_SHARE_BASE_DIR': '/builds/hg-shared',
+        'MOZ_OBJDIR': 'obj-add-on-devel',
+        'TINDERBOX_OUTPUT': '1',
+        'TOOLTOOL_CACHE': '/builds/tooltool_cache',
+        'TOOLTOOL_HOME': '/builds',
+        'MOZ_CRASHREPORTER_NO_REPORT': '1',
+        'CCACHE_DIR': '/builds/ccache',
+        'CCACHE_COMPRESS': '1',
+        'CCACHE_UMASK': '002',
+        'LC_ALL': 'C',
+        ## 64 bit specific
+        'PATH': '/tools/python/bin:/tools/buildbot/bin:/opt/local/bin:/usr/bin:'
+                '/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin',
+        ##
+    },
+    'src_mozconfig': 'browser/config/mozconfigs/macosx64/add-on-devel',
+    #######################
+}
