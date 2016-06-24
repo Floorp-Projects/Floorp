1c5ef54d8a3d0b22d47702204656ade3965c1f38
Jan Keromnes <janx@linux.com>
2016-06-24 18:06:34 +0000

    Bug 1188981 - Show Push Service subscription endpoint URL for Service Workers in about:debugging. r=jryans

---
+++ devtools/client/aboutdebugging/test/browser_service_workers_push_service.js
@@ -0,0 +1,111 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test that a Service Worker registration's Push Service subscription appears
+// in about:debugging if it exists, and disappears when unregistered.
+
+// Service workers can't be loaded from chrome://, but http:// is ok with
+// dom.serviceWorkers.testing.enabled turned on.
+const SERVICE_WORKER = URL_ROOT + "service-workers/push-sw.js";
+const TAB_URL = URL_ROOT + "service-workers/push-sw.html";
+
+const FAKE_ENDPOINT = "https://fake/endpoint";
+
+const PushService = Cc["@mozilla.org/push/Service;1"]
+  .getService(Ci.nsIPushService).wrappedJSObject;
+
+add_task(function* () {
+  info("Turn on workers via mochitest http.");
+  yield SpecialPowers.pushPrefEnv({
+    "set": [
+      // Accept workers from mochitest's http.
+      ["dom.serviceWorkers.testing.enabled", true],
+      // Enable the push service.
+      ["dom.push.connection.enabled", true],
+    ]
+  });
+
+  info("Mock the push service");
+  PushService.service = {
+    _registrations: new Map(),
+    _notify(scope) {
+      Services.obs.notifyObservers(
+        null,
+        PushService.subscriptionModifiedTopic,
+        scope);
+    },
+    init() {},
+    register(pageRecord) {
+      let registration = {
+        endpoint: FAKE_ENDPOINT
+      };
+      this._registrations.set(pageRecord.scope, registration);
+      this._notify(pageRecord.scope);
+      return Promise.resolve(registration);
+    },
+    registration(pageRecord) {
+      return Promise.resolve(this._registrations.get(pageRecord.scope));
+    },
+    unregister(pageRecord) {
+      let deleted = this._registrations.delete(pageRecord.scope);
+      if (deleted) {
+        this._notify(pageRecord.scope);
+      }
+      return Promise.resolve(deleted);
+    },
+  };
+
+  let { tab, document } = yield openAboutDebugging("workers");
+
+  // Listen for mutations in the service-workers list.
+  let serviceWorkersElement = document.getElementById("service-workers");
+  let onMutation = waitForMutation(serviceWorkersElement, { childList: true });
+
+  // Open a tab that registers a push service worker.
+  let swTab = yield addTab(TAB_URL);
+
+  // Wait for the service-workers list to update.
+  yield onMutation;
+
+  // Check that the service worker appears in the UI.
+  assertHasTarget(true, document, "service-workers", SERVICE_WORKER);
+
+  // Wait for the service worker details to update.
+  let names = [...document.querySelectorAll("#service-workers .target-name")];
+  let name = names.filter(element => element.textContent === SERVICE_WORKER)[0];
+  ok(name, "Found the service worker in the list");
+  let targetContainer = name.parentNode.parentNode;
+  let targetDetailsElement = targetContainer.querySelector(".target-details");
+  yield waitForMutation(targetDetailsElement, { childList: true });
+
+  // Retrieve the push subscription endpoint URL, and verify it looks good.
+  let pushURL = targetContainer.querySelector(".service-worker-push-url");
+  ok(pushURL, "Found the push service URL in the service worker details");
+  is(pushURL.textContent, FAKE_ENDPOINT, "The push service URL looks correct");
+
+  // Unsubscribe from the push service.
+  ContentTask.spawn(swTab.linkedBrowser, {}, function () {
+    let win = content.wrappedJSObject;
+    return win.sub.unsubscribe();
+  });
+
+  // Wait for the service worker details to update again.
+  yield waitForMutation(targetDetailsElement, { childList: true });
+  ok(!targetContainer.querySelector(".service-worker-push-url"),
+    "The push service URL should be removed");
+
+  // Finally, unregister the service worker itself.
+  yield unregisterServiceWorker(swTab).then(() => {
+    ok(true, "Service worker registration unregistered");
+  }).catch(function (e) {
+    ok(false, "Service worker not unregistered; " + e);
+  });
+
+  info("Unmock the push service");
+  PushService.service = null;
+
+  yield removeTab(swTab);
+  yield closeAboutDebugging(tab);
+});
