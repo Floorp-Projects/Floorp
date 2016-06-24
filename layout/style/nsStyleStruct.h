b1101f8897e2aa76b8baace0b4618378eda5046d
Ting-Yu Lin <tlin@mozilla.com>
2016-06-24 17:47:32 +0800

    Bug 1282029 Part 1 - Remove superfluous access modifiers. r=dbaron
    MozReview-Commit-ID: 5HRNtYECUjU
    
    --HG--
    extra : rebase_source : d0cc30bcd72168d1da345dcb72e9f41876977755

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -1242,8 +1242,6 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleBorder
 
 public:
   nsBorderColors** mBorderColors;        // [reset] composite (stripe) colors
-
-public:
   nsStyleCorners mBorderRadius;       // [reset] coord, percent
   nsStyleImage   mBorderImageSource;  // [reset]
   nsStyleSides   mBorderImageSlice;   // [reset] factor, percent
