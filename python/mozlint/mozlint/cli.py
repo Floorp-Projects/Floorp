60ee32c86dd817df876a5a507b2526db5dfc8efd
Andrew Halberstadt <ahalberstadt@mozilla.com>
2016-06-24 14:06:22 -0400

    Bug 1281899 - [mozlint] Create cli module and move logic from tools/lint/mach_commands.py there, r=smacleod
    There is currently no built-in user interface to mozlint. The only existing interface is the
    external cli provided by |mach lint|. However, in the future mozlint may need to be used in a
    context where mach isn't readily available (i.e version-control-tools). This patch basically
    just moves the cli logic out of mach_commands.py, and into mozlint core. That way it can be
    re-used in other places without needing to be re-implemented.
    
    The |mach lint setup| subcommand was removed because apparently subcommands don't work with
    the parser attribute. Nothing was using it yet anyway, so I removed it for now. It may get
    re-added in some form in the future.
    
    MozReview-Commit-ID: aOGp2Yrncs

---
+++ python/mozlint/mozlint/cli.py
@@ -0,0 +1,91 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import print_function, unicode_literals
+
+import os
+import subprocess
+import sys
+from argparse import ArgumentParser
+
+
+SEARCH_PATHS = []
+
+
+class MozlintParser(ArgumentParser):
+    arguments = [
+        [['paths'],
+         {'nargs': '*',
+          'default': None,
+          'help': "Paths to file or directories to lint, like "
+                  "'browser/components/loop' or 'mobile/android'. "
+                  "Defaults to the current directory if not given.",
+          }],
+        [['-l', '--linter'],
+         {'dest': 'linters',
+          'default': [],
+          'action': 'append',
+          'help': "Linters to run, e.g 'eslint'. By default all linters "
+                  "are run for all the appropriate files.",
+          }],
+        [['-f', '--format'],
+         {'dest': 'fmt',
+          'default': 'stylish',
+          'help': "Formatter to use. Defaults to 'stylish'.",
+          }],
+        [['-n', '--no-filter'],
+         {'dest': 'use_filters',
+          'default': True,
+          'action': 'store_false',
+          'help': "Ignore all filtering. This is useful for quickly "
+                  "testing a directory that otherwise wouldn't be run, "
+                  "without needing to modify the config file.",
+          }],
+    ]
+
+    def __init__(self, **kwargs):
+        ArgumentParser.__init__(self, usage=self.__doc__, **kwargs)
+
+        for cli, args in self.arguments:
+            self.add_argument(*cli, **args)
+
+
+def find_linters(self, linters=None):
+    lints = []
+    for search_path in SEARCH_PATHS:
+        if not os.path.isdir(search_path):
+            continue
+
+        files = os.listdir(search_path)
+        for f in files:
+            name, ext = os.path.splitext(f)
+            if ext != '.lint':
+                continue
+
+            if linters and name not in linters:
+                continue
+
+            lints.append(os.path.join(search_path, f))
+    return lints
+
+
+def run(paths, linters, fmt, **lintargs):
+    from mozlint import LintRoller, formatters
+    paths = paths or ['.']
+
+    lint = LintRoller(**lintargs)
+    lint.read(find_linters(linters))
+
+    # run all linters
+    results = lint.roll(paths)
+
+    formatter = formatters.get(fmt)
+    print(formatter(results))
+    return 1 if results else 0
+
+
+if __name__ == '__main__':
+    parser = MozlintParser()
+    args = vars(parser.parse_args())
+    sys.exit(run(**args))
