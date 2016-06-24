09a79c8ff4e7c402999863246d8b7b5820ac876f
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2016-06-24 10:36:42 -0400

    Bug 1127579 - Cleanup jsdoc comments in nsLoginManagerPrompter.js
    MozReview-Commit-ID: 5VzE9DyFZEb

--- toolkit/components/passwordmgr/nsLoginManagerPrompter.js
+++ toolkit/components/passwordmgr/nsLoginManagerPrompter.js
@@ -27,9 +27,7 @@ const PROMPT_ADD_OR_UPDATE = 1;
 const PROMPT_NOTNOW = 2;
 const PROMPT_NEVER = 3;
 
-/*
- * LoginManagerPromptFactory
- *
+/**
  * Implements nsIPromptFactory
  *
  * Invoked by [toolkit/components/prompts/src/nsPrompter.js]
@@ -186,9 +184,7 @@ XPCOMUtils.defineLazyGetter(this.LoginManagerPromptFactory.prototype, "log", () 
 
 
 
-/*
- * LoginManagerPrompter
- *
+/**
  * Implements interfaces for prompting the user to enter/save/change auth info.
  *
  * nsIAuthPrompt: Used by SeaMonkey, Thunderbird, but not Firefox.
@@ -279,9 +275,7 @@ LoginManagerPrompter.prototype = {
   /* ---------- nsIAuthPrompt prompts ---------- */
 
 
-  /*
-   * prompt
-   *
+  /**
    * Wrapper around the prompt service prompt. Saving random fields here
    * doesn't really make sense and therefore isn't implemented.
    */
@@ -302,9 +296,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * promptUsernameAndPassword
-   *
+  /**
    * Looks up a username and password in the database. Will prompt the user
    * with a dialog, even if a username and password are found.
    */
@@ -401,9 +393,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * promptPassword
-   *
+  /**
    * If a password is found in the database for the password realm, it is
    * returned straight away without displaying a dialog.
    *
@@ -508,14 +498,12 @@ LoginManagerPrompter.prototype = {
 
 
 
-  /*
-   * promptAuth
-   *
+  /**
    * Implementation of nsIAuthPrompt2.
    *
-   * nsIChannel aChannel
-   * int        aLevel
-   * nsIAuthInformation aAuthInfo
+   * @param {nsIChannel} aChannel
+   * @param {int}        aLevel
+   * @param {nsIAuthInformation} aAuthInfo
    */
   promptAuth : function (aChannel, aLevel, aAuthInfo) {
     var selectedLogin = null;
@@ -694,11 +682,6 @@ LoginManagerPrompter.prototype = {
 
 
 
-
-  /*
-   * init
-   *
-   */
   init : function (aWindow, aFactory) {
     this._window = aWindow;
     this._factory = aFactory || null;
@@ -715,11 +698,6 @@ LoginManagerPrompter.prototype = {
     this._opener = aOpener;
   },
 
-
-  /*
-   * promptToSavePassword
-   *
-   */
   promptToSavePassword : function (aLogin) {
     this.log("promptToSavePassword");
     var notifyObj = this._getPopupNote() || this._getNotifyBox();
@@ -729,12 +707,8 @@ LoginManagerPrompter.prototype = {
       this._showSaveLoginDialog(aLogin);
   },
 
-
-  /*
-   * _showLoginNotification
-   *
+  /**
    * Displays a notification bar.
-   *
    */
   _showLoginNotification : function (aNotifyBox, aName, aText, aButtons) {
     var oldBar = aNotifyBox.getNotificationWithValue(aName);
@@ -1087,11 +1061,6 @@ LoginManagerPrompter.prototype = {
     }
   },
 
-
-  /*
-   * _removeLoginNotifications
-   *
-   */
   _removeLoginNotifications : function () {
     var popupNote = this._getPopupNote();
     if (popupNote)
@@ -1116,12 +1085,9 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _showSaveLoginDialog
-   *
+  /**
    * Called when we detect a new login in a form submission,
    * asks the user what to do.
-   *
    */
   _showSaveLoginDialog : function (aLogin) {
     const buttonFlags = Ci.nsIPrompt.BUTTON_POS_1_DEFAULT +
@@ -1197,9 +1163,7 @@ LoginManagerPrompter.prototype = {
     }
   },
 
-  /*
-   * _showChangeLoginNotification
-   *
+  /**
    * Shows the Change Password notification bar or popup notification.
    *
    * @param aNotifyObj
@@ -1210,7 +1174,6 @@ LoginManagerPrompter.prototype = {
    *
    * @param aNewLogin
    *        The login object with the changes we want to make.
-   *
    */
   _showChangeLoginNotification(aNotifyObj, aOldLogin, aNewLogin) {
     var changeButtonText =
@@ -1269,11 +1232,8 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _showChangeLoginDialog
-   *
+  /**
    * Shows the Change Password dialog.
-   *
    */
   _showChangeLoginDialog(aOldLogin, aNewLogin) {
     const buttonFlags = Ci.nsIPrompt.STD_YES_NO_BUTTONS;
@@ -1302,9 +1262,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * promptToChangePasswordWithUsernames
-   *
+  /**
    * Called when we detect a password change in a form submission, but we
    * don't know which existing login (username) it's for. Asks the user
    * to select a username and confirm the password change.
@@ -1372,9 +1330,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _getChromeWindow
-   *
+  /**
    * Given a content DOM window, returns the chrome window it's in.
    */
   _getChromeWindow: function (aWindow) {
@@ -1389,9 +1345,6 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _getNotifyWindow
-   */
   _getNotifyWindow: function () {
 
     try {
@@ -1457,9 +1410,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _getPopupNote
-   *
+  /**
    * Returns the popup notification to this prompter,
    * or null if there isn't one available.
    */
@@ -1482,9 +1433,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _getNotifyBox
-   *
+  /**
    * Returns the notification box to this prompter, or null if there isn't
    * a notification box available.
    */
@@ -1507,9 +1456,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _repickSelectedLogin
-   *
+  /**
    * The user might enter a login that isn't the one we prefilled, but
    * is the same as some other existing login. So, pick a login with a
    * matching username, or return null.
@@ -1522,9 +1469,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _getLocalizedString
-   *
+  /**
    * Can be called as:
    *   _getLocalizedString("key1");
    *   _getLocalizedString("key2", ["arg1"]);
@@ -1544,9 +1489,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _sanitizeUsername
-   *
+  /**
    * Sanitizes the specified username, by stripping quotes and truncating if
    * it's too long. This helps prevent an evil site from messing with the
    * "save password?" prompt too much.
@@ -1578,9 +1521,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _getShortDisplayHost
-   *
+  /**
    * Converts a login's hostname field (a URL) to a short string for
    * prompting purposes. Eg, "http://foo.com" --> "foo.com", or
    * "ftp://www.site.co.uk" --> "site.co.uk".
@@ -1607,9 +1548,7 @@ LoginManagerPrompter.prototype = {
   },
 
 
-  /*
-   * _getAuthTarget
-   *
+  /**
    * Returns the hostname and realm for which authentication is being
    * requested, in the format expected to be used with nsILoginInfo.
    */
