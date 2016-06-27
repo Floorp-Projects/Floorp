d3028d3755684e3ce9c171dcf93cb0f8969f6d03
Andrea Marchesini <amarchesini@mozilla.com>
2016-06-27 19:13:40 +0200

    Bug 1269162 - part 2 - Move XHRUpload in workers to XMLHttpRequestUploadWorker, r=smaug
    --HG--
    rename : dom/workers/XMLHttpRequestUpload.cpp => dom/workers/XMLHttpRequestUploadWorker.cpp
    rename : dom/workers/XMLHttpRequestUpload.h => dom/workers/XMLHttpRequestUploadWorker.h

---
+++ dom/workers/XMLHttpRequestUploadWorker.cpp
@@ -0,0 +1,45 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "XMLHttpRequestUploadWorker.h"
+
+#include "XMLHttpRequestWorker.h"
+
+#include "mozilla/dom/XMLHttpRequestUploadBinding.h"
+
+USING_WORKERS_NAMESPACE
+
+XMLHttpRequestUploadWorker::XMLHttpRequestUploadWorker(XMLHttpRequestWorker* aXHR)
+: mXHR(aXHR)
+{
+}
+
+XMLHttpRequestUploadWorker::~XMLHttpRequestUploadWorker()
+{
+}
+
+NS_IMPL_ADDREF_INHERITED(XMLHttpRequestUploadWorker, nsXHREventTarget)
+NS_IMPL_RELEASE_INHERITED(XMLHttpRequestUploadWorker, nsXHREventTarget)
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION_INHERITED(XMLHttpRequestUploadWorker)
+NS_INTERFACE_MAP_END_INHERITING(nsXHREventTarget)
+
+NS_IMPL_CYCLE_COLLECTION_INHERITED(XMLHttpRequestUploadWorker, nsXHREventTarget,
+                                   mXHR)
+
+JSObject*
+XMLHttpRequestUploadWorker::WrapObject(JSContext* aCx, JS::Handle<JSObject*> aGivenProto)
+{
+  return XMLHttpRequestUploadBinding_workers::Wrap(aCx, this, aGivenProto);
+}
+
+// static
+already_AddRefed<XMLHttpRequestUploadWorker>
+XMLHttpRequestUploadWorker::Create(XMLHttpRequestWorker* aXHR)
+{
+  RefPtr<XMLHttpRequestUploadWorker> upload = new XMLHttpRequestUploadWorker(aXHR);
+  return upload.forget();
+}
