c2c58e120e049c8ef2479511247c2074cd8afb97
Chih-Yi Leu <cleu@mozilla.com>
2016-06-28 00:27:00 +0200

    Bug 1221730 - Update mochitest. r=qdot
    --HG--
    extra : rebase_source : 981798b3dc3020ad4c4fea717e6dba4ec67fe57f

--- dom/tests/mochitest/gamepad/mock_gamepad.js
+++ dom/tests/mochitest/gamepad/mock_gamepad.js
@@ -3,11 +3,10 @@
 
 var GamepadService;
 
-GamepadService = SpecialPowers.Cc["@mozilla.org/gamepad-test;1"].getService(SpecialPowers.Ci.nsIGamepadServiceTest);
-
-
-var addGamepad = function(name, mapping, buttons, axes) {
-  return new Promise((resolve, reject) => {
-    resolve(GamepadService.addGamepad(name, mapping, buttons, axes));
-  });
+function runGamepadTest (callback) {
+  SpecialPowers.pushPrefEnv({"set" : [["dom.gamepad.test.enabled", true]]},
+                      () => {
+                        GamepadService = navigator.requestGamepadServiceTest();
+                        callback();
+                      });
 }
