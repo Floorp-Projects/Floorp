9d123a2673e41ef769c51409046a19505c41a1de
Kim Moir <kmoir>
2016-06-27 06:15:00 +0200

    Bug 1135781 - "generate builds per checkin on beta/release/esr that allow installation of unsigned addons", a=sylvestre

---
+++ testing/mozharness/configs/builds/releng_sub_windows_configs/32_add-on-devel.py
@@ -0,0 +1,37 @@
+import os
+
+config = {
+    'default_actions': [
+        'clobber',
+        'clone-tools',
+        'checkout-sources',
+        # 'setup-mock', windows do not use mock
+        'build',
+        'upload-files',
+#        'sendchange',
+        'check-test',
+#        'generate-build-stats',
+#        'update',
+    ],
+    'stage_platform': 'win32-add-on-devel',
+    'enable_talos_sendchange': False,
+    #### 32 bit build specific #####
+    'env': {
+        'BINSCOPE': 'C:/Program Files (x86)/Microsoft/SDL BinScope/BinScope.exe',
+        'HG_SHARE_BASE_DIR': 'C:/builds/hg-shared',
+        'MOZBUILD_STATE_PATH': os.path.join(os.getcwd(), '.mozbuild'),
+        'MOZ_AUTOMATION': '1',
+        'MOZ_CRASHREPORTER_NO_REPORT': '1',
+        'MOZ_OBJDIR': 'obj-add-on-devel',
+        'PATH': 'C:/mozilla-build/nsis-3.0b1;C:/mozilla-build/python27;'
+                'C:/mozilla-build/buildbotve/scripts;'
+                '%s' % (os.environ.get('path')),
+        'PROPERTIES_FILE': os.path.join(os.getcwd(), 'buildprops.json'),
+        'TINDERBOX_OUTPUT': '1',
+        'XPCOM_DEBUG_BREAK': 'stack-and-abort',
+        'TOOLTOOL_CACHE': '/c/builds/tooltool_cache',
+        'TOOLTOOL_HOME': '/c/builds',
+    },
+    'src_mozconfig': 'browser/config/mozconfigs/win32/add-on-devel',
+    #######################
+}
