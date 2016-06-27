fd6484f0a52b25bd3ae0688a5408daa79751651e
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-06-27 15:48:44 +0200

    Backed out changeset 9bd3cdd0eaa3 (bug 1268077) for bustage

--- toolkit/mozapps/extensions/AddonManagerWebAPI.h
+++ toolkit/mozapps/extensions/AddonManagerWebAPI.h
@@ -4,9 +4,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef addonmanagerwebapi_h_
-#define addonmanagerwebapi_h_
-
 #include "nsPIDOMWindow.h"
 
 namespace mozilla {
@@ -20,5 +17,3 @@ private:
 };
 
 } // namespace mozilla
-
-#endif // addonmanagerwebapi_h_
