ae15d95829641e5ef2ffe73c7244400d8aa8acbf
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-06-28 20:28:22 +0200

    Backed out changeset 169b7053b22d (bug 1167235)

--- gfx/layers/ipc/CompositorBridgeChild.cpp
+++ gfx/layers/ipc/CompositorBridgeChild.cpp
@@ -113,15 +113,6 @@ CompositorBridgeChild::Destroy()
     layers->Destroy();
   }
 
-  const ManagedContainer<PTextureChild>& textures = ManagedPTextureChild();
-  for (auto iter = textures.ConstIter(); !iter.Done(); iter.Next()) {
-    RefPtr<TextureClient> texture = TextureClient::AsTextureClient(iter.Get()->GetKey());
-
-    if (texture) {
-      texture->Destroy();
-    }
-  }
-
   SendWillClose();
   mCanSend = false;
 
@@ -137,6 +128,16 @@ CompositorBridgeChild::Destroy()
   // From now on we can't send any message message.
   MessageLoop::current()->PostTask(
              NewRunnableFunction(DeferredDestroyCompositor, mCompositorBridgeParent, selfRef));
+
+  const ManagedContainer<PTextureChild>& textures = ManagedPTextureChild();
+  for (auto iter = textures.ConstIter(); !iter.Done(); iter.Next()) {
+    RefPtr<TextureClient> texture = TextureClient::AsTextureClient(iter.Get()->GetKey());
+
+    if (texture) {
+      texture->Destroy();
+    }
+  }
+
 }
 
 // static
