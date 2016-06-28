875e53018cb237ffa272c287d19f57a88596fa6d
Andrea Marchesini <amarchesini@mozilla.com>
2016-06-28 16:44:42 +0200

    Bug 1281793 - Remove some non-used telemetry IDs - part 2 - network/cache, r=gfritzsche

--- netwerk/cache/nsDiskCacheMap.cpp
+++ netwerk/cache/nsDiskCacheMap.cpp
@@ -34,8 +34,7 @@ using namespace mozilla;
 
 nsresult
 nsDiskCacheMap::Open(nsIFile *  cacheDirectory,
-                     nsDiskCache::CorruptCacheInfo *  corruptInfo,
-                     bool reportCacheCleanTelemetryData)
+                     nsDiskCache::CorruptCacheInfo *  corruptInfo)
 {
     NS_ENSURE_ARG_POINTER(corruptInfo);
 
@@ -66,8 +65,7 @@ nsDiskCacheMap::Open(nsIFile *  cacheDirectory,
     uint32_t mapSize = PR_Available(mMapFD);    
 
     if (NS_FAILED(InitCacheClean(cacheDirectory,
-                                 corruptInfo,
-                                 reportCacheCleanTelemetryData))) {
+                                 corruptInfo))) {
         // corruptInfo is set in the call to InitCacheClean
         goto error_exit;
     }
@@ -1238,8 +1236,7 @@ nsDiskCacheMap::SizeOfExcludingThis(MallocSizeOf aMallocSizeOf)
 
 nsresult
 nsDiskCacheMap::InitCacheClean(nsIFile *  cacheDirectory,
-                               nsDiskCache::CorruptCacheInfo *  corruptInfo,
-                               bool reportCacheCleanTelemetryData)
+                               nsDiskCache::CorruptCacheInfo *  corruptInfo)
 {
     // The _CACHE_CLEAN_ file will be used in the future to determine
     // if the cache is clean or not. 
@@ -1275,9 +1272,6 @@ nsDiskCacheMap::InitCacheClean(nsIFile *  cacheDirectory,
         int32_t bytesRead = PR_Read(mCleanFD, &clean, 1);
         if (bytesRead != 1) {
             NS_WARNING("Could not read _CACHE_CLEAN_ file contents");
-        } else if (reportCacheCleanTelemetryData) {
-            Telemetry::Accumulate(Telemetry::DISK_CACHE_REDUCTION_TRIAL,
-                                  clean == '1' ? 1 : 0);
         }
     }
 
@@ -1340,11 +1334,9 @@ nsDiskCacheMap::InvalidateCache()
     if (!mIsDirtyCacheFlushed) {
         rv = WriteCacheClean(false);
         if (NS_FAILED(rv)) {
-          Telemetry::Accumulate(Telemetry::DISK_CACHE_INVALIDATION_SUCCESS, 0);
           return rv;
         }
 
-        Telemetry::Accumulate(Telemetry::DISK_CACHE_INVALIDATION_SUCCESS, 1);
         mIsDirtyCacheFlushed = true;
     }
 
@@ -1414,7 +1406,6 @@ nsDiskCacheMap::RevalidateCache()
     nsresult rv;
 
     if (!IsCacheInSafeState()) {
-        Telemetry::Accumulate(Telemetry::DISK_CACHE_REVALIDATION_SAFE, 0);
         CACHE_LOG_DEBUG(("CACHE: Revalidation should not performed because "
                          "cache not in a safe state\n"));
         // Normally we would return an error here, but there is a bug where
@@ -1422,8 +1413,6 @@ nsDiskCacheMap::RevalidateCache()
         // until browser shutdown.  So we allow revalidation for the time being
         // to get proper telemetry data of how much the cache corruption plan
         // would help.
-    } else {
-        Telemetry::Accumulate(Telemetry::DISK_CACHE_REVALIDATION_SAFE, 1);
     }
 
     // We want this after the lock to prove that flushing a file isn't that expensive
@@ -1435,11 +1424,9 @@ nsDiskCacheMap::RevalidateCache()
     // Write out the _CACHE_CLEAN_ file with '1'
     rv = WriteCacheClean(true);
     if (NS_FAILED(rv)) {
-        Telemetry::Accumulate(Telemetry::DISK_CACHE_REVALIDATION_SUCCESS, 0);
         return rv;
     }
 
-    Telemetry::Accumulate(Telemetry::DISK_CACHE_REVALIDATION_SUCCESS, 1);
     mIsDirtyCacheFlushed = false;
 
     return NS_OK;
