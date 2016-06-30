2aa816d7e635f967fa8e108bce39240dcdd156d7
Jeff Gilbert <jgilbert@mozilla.com>
2016-06-30 12:29:49 -0700

    Bug 1281098 - Cache LOCAL_GL_TEXTURE_COMPARE_MODE since we need it for validation. - r=ethlin
    MozReview-Commit-ID: 6bboJTWuEP7

--- dom/canvas/WebGLTexture.cpp
+++ dom/canvas/WebGLTexture.cpp
@@ -1023,6 +1023,10 @@ WebGLTexture::TexParameter(TexTarget texTarget, GLenum pname, GLint* maybeIntPar
         mWrapT = intParam;
         break;
 
+    case LOCAL_GL_TEXTURE_COMPARE_MODE:
+        mTexCompareMode = intParam;
+        break;
+
     // We don't actually need to store the WRAP_R, since it doesn't change texture
     // completeness rules.
     }
