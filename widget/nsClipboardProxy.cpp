1b8a4fecf539622cc319e288db5b0bdeec07d825
Hector Zhao <bzhao@mozilla.com>
2016-06-30 15:12:56 +0800

    Bug 664717 - Part 2: Pass requestingPrincipal to parent process with nsITransferable. r=smaug
    MozReview-Commit-ID: B0k10UplydR

--- widget/nsClipboardProxy.cpp
+++ widget/nsClipboardProxy.cpp
@@ -34,7 +34,10 @@ nsClipboardProxy::SetData(nsITransferable *aTransferable,
 
   bool isPrivateData = false;
   aTransferable->GetIsPrivateData(&isPrivateData);
-  child->SendSetClipboard(ipcDataTransfer, isPrivateData, aWhichClipboard);
+  nsCOMPtr<nsIPrincipal> requestingPrincipal;
+  aTransferable->GetRequestingPrincipal(getter_AddRefs(requestingPrincipal));
+  child->SendSetClipboard(ipcDataTransfer, isPrivateData,
+                          IPC::Principal(requestingPrincipal), aWhichClipboard);
 
   return NS_OK;
 }
