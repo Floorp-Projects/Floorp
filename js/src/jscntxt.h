84fb3b9f360e0c1409de775b1c0c217392ee5ea1
Jan de Mooij <jdemooij@mozilla.com>
2016-06-30 09:13:11 +0200

    Bug 1281529 part 3 - Use static_cast instead of rt->context_. r=jwalden

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -764,4 +764,17 @@ class MOZ_RAII AutoLockForExclusiveAccess
 #pragma warning(pop)
 #endif
 
+inline JSContext*
+JSRuntime::unsafeContextFromAnyThread()
+{
+    return static_cast<JSContext*>(this);
+}
+
+inline JSContext*
+JSRuntime::contextFromMainThread()
+{
+    MOZ_ASSERT(CurrentThreadCanAccessRuntime(this));
+    return unsafeContextFromAnyThread();
+}
+
 #endif /* jscntxt_h */
