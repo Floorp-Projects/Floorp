1bac9b5fbef8905f93aaef76088f1ab0589d5941
JW Wang <jwwang@mozilla.com>
2016-07-01 17:49:25 +0800

    Bug 1283785. Part 1 - remove the IsVideoRequestPending() check from EnsureVideoDecodeTaskQueued() r=kaku
    1. EnsureVideoDecodeTaskQueued() is called from video callbaks where IsVideoRequestPending() is false.
    2. EnsureVideoDecodeTaskQueued() is called from Seek() which checks IsVideoRequestPending() is false.
    
    MozReview-Commit-ID: 59qu0Gh6nK7
    
    --HG--
    extra : rebase_source : 29941e0e2a450db572f84d22de9707eeb250d6e9

--- dom/media/NextFrameSeekTask.cpp
+++ dom/media/NextFrameSeekTask.cpp
@@ -117,7 +117,7 @@ NextFrameSeekTask::Seek(const media::TimeUnit&)
   });
 
   RefPtr<SeekTaskPromise> promise = mSeekTaskPromise.Ensure(__func__);
-  if (NeedMoreVideo()) {
+  if (!IsVideoRequestPending() && NeedMoreVideo()) {
     EnsureVideoDecodeTaskQueued();
   }
   MaybeFinishSeek(); // Might resolve mSeekTaskPromise and modify audio queue.
@@ -138,7 +138,7 @@ NextFrameSeekTask::EnsureVideoDecodeTaskQueued()
   SAMPLE_LOG("EnsureVideoDecodeTaskQueued isDecoding=%d status=%s",
              IsVideoDecoding(), VideoRequestStatus());
 
-  if (!IsVideoDecoding() || IsVideoRequestPending()) {
+  if (!IsVideoDecoding()) {
     return;
   }
 
