40a56b044ef385906d0f1e858d656a1c950a2410
Lin Clark <lclark@mozilla.com>
2016-07-01 11:21:08 -0400

    Bug 1283870 - New console frontend: Use reps to render responses from the backend. r=bgrins
    MozReview-Commit-ID: HYRdgpKDXF

--- devtools/client/webconsole/new-console-output/components/message-types/evaluation-result.js
+++ devtools/client/webconsole/new-console-output/components/message-types/evaluation-result.js
@@ -9,8 +9,11 @@
 // React & Redux
 const {
   createFactory,
+  DOM: dom,
   PropTypes
 } = require("devtools/client/shared/vendor/react");
+const GripMessageBody = createFactory(require("devtools/client/webconsole/new-console-output/components/grip-message-body").GripMessageBody);
+const MessageIcon = createFactory(require("devtools/client/webconsole/new-console-output/components/message-icon").MessageIcon);
 
 EvaluationResult.displayName = "EvaluationResult";
 
@@ -20,23 +23,27 @@ EvaluationResult.propTypes = {
 
 function EvaluationResult(props) {
   const { message } = props;
-  let PreviewComponent = getPreviewComponent(message.data);
+  const icon = MessageIcon({severity: message.severity});
 
-  return PreviewComponent({
-    data: message.data,
+  // @TODO Use of "is" is a temporary hack to get the category and severity
+  // attributes to be applied. There are targeted in webconsole's CSS rules,
+  // so if we remove this hack, we have to modify the CSS rules accordingly.
+  return dom.div({
+    class: "message cm-s-mozilla",
+    is: "fdt-message",
     category: message.category,
     severity: message.severity
-  });
-}
-
-function getPreviewComponent(data) {
-  if (typeof data.class != "undefined") {
-    switch (data.class) {
-      case "Date":
-        return createFactory(require("devtools/client/webconsole/new-console-output/components/message-types/date-preview").DatePreview);
-    }
-  }
-  return createFactory(require("devtools/client/webconsole/new-console-output/components/message-types/default-renderer").DefaultRenderer);
+  },
+    // @TODO add timestamp
+    // @TODO add indent if needed with console.group
+    icon,
+    dom.span(
+      {className: "message-body-wrapper message-body devtools-monospace"},
+      dom.span({},
+        GripMessageBody({grip: message.data})
+      )
+    )
+  );
 }
 
 module.exports.EvaluationResult = EvaluationResult;
