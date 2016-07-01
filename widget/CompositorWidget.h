8d0e8b8dda512b40730ed2348d67440b26f57194
David Anderson <danderson@mozilla.com>
2016-07-01 01:15:16 -0700

    Move InProcessCompositorWidget to its own file. (bug 1281998 part 3, r=jimm)

--- widget/CompositorWidget.h
+++ widget/CompositorWidget.h
@@ -211,42 +211,6 @@ protected:
   RefPtr<gfx::DrawTarget> mLastBackBuffer;
 };
 
-// This version of CompositorWidget implements a wrapper around
-// nsBaseWidget.
-class InProcessCompositorWidget : public CompositorWidget
-{
-public:
-  explicit InProcessCompositorWidget(nsBaseWidget* aWidget);
-
-  virtual bool PreRender(layers::LayerManagerComposite* aManager) override;
-  virtual void PostRender(layers::LayerManagerComposite* aManager) override;
-  virtual void DrawWindowUnderlay(layers::LayerManagerComposite* aManager,
-                                  LayoutDeviceIntRect aRect) override;
-  virtual void DrawWindowOverlay(layers::LayerManagerComposite* aManager,
-                                 LayoutDeviceIntRect aRect) override;
-  virtual already_AddRefed<gfx::DrawTarget> StartRemoteDrawing() override;
-  virtual already_AddRefed<gfx::DrawTarget>
-  StartRemoteDrawingInRegion(LayoutDeviceIntRegion& aInvalidRegion,
-                             layers::BufferMode* aBufferMode) override;
-  virtual void EndRemoteDrawing() override;
-  virtual void EndRemoteDrawingInRegion(gfx::DrawTarget* aDrawTarget,
-                                        LayoutDeviceIntRegion& aInvalidRegion) override;
-  virtual void CleanupRemoteDrawing() override;
-  virtual void CleanupWindowEffects() override;
-  virtual bool InitCompositor(layers::Compositor* aCompositor) override;
-  virtual LayoutDeviceIntSize GetClientSize() override;
-  virtual uint32_t GetGLFrameBufferFormat() override;
-  virtual layers::Composer2D* GetComposer2D() override;
-  virtual already_AddRefed<CompositorVsyncDispatcher> GetCompositorVsyncDispatcher() override;
-  virtual uintptr_t GetWidgetKey() override;
-
-  // If you can override this method, inherit from CompositorWidget instead.
-  nsIWidget* RealWidget() override;
-
-private:
-  nsBaseWidget* mWidget;
-};
-
 } // namespace widget
 } // namespace mozilla
 
