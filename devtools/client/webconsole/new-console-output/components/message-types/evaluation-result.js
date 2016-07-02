60ccb2c304e11ff3295c1ff9678561a184a3a0d4
Phil Ringnalda <philringnalda@gmail.com>
2016-07-01 18:26:52 -0700

    Back out 7d431089a2d2 (bug 1283870) for failures in test_evaluation-result.html

--- devtools/client/webconsole/new-console-output/components/message-types/evaluation-result.js
+++ devtools/client/webconsole/new-console-output/components/message-types/evaluation-result.js
@@ -9,11 +9,8 @@
 // React & Redux
 const {
   createFactory,
-  DOM: dom,
   PropTypes
 } = require("devtools/client/shared/vendor/react");
-const GripMessageBody = createFactory(require("devtools/client/webconsole/new-console-output/components/grip-message-body").GripMessageBody);
-const MessageIcon = createFactory(require("devtools/client/webconsole/new-console-output/components/message-icon").MessageIcon);
 
 EvaluationResult.displayName = "EvaluationResult";
 
@@ -23,27 +20,23 @@ EvaluationResult.propTypes = {
 
 function EvaluationResult(props) {
   const { message } = props;
-  const icon = MessageIcon({severity: message.severity});
+  let PreviewComponent = getPreviewComponent(message.data);
 
-  // @TODO Use of "is" is a temporary hack to get the category and severity
-  // attributes to be applied. There are targeted in webconsole's CSS rules,
-  // so if we remove this hack, we have to modify the CSS rules accordingly.
-  return dom.div({
-    class: "message cm-s-mozilla",
-    is: "fdt-message",
+  return PreviewComponent({
+    data: message.data,
     category: message.category,
     severity: message.severity
-  },
-    // @TODO add timestamp
-    // @TODO add indent if needed with console.group
-    icon,
-    dom.span(
-      {className: "message-body-wrapper message-body devtools-monospace"},
-      dom.span({},
-        GripMessageBody({grip: message.data})
-      )
-    )
-  );
+  });
+}
+
+function getPreviewComponent(data) {
+  if (typeof data.class != "undefined") {
+    switch (data.class) {
+      case "Date":
+        return createFactory(require("devtools/client/webconsole/new-console-output/components/message-types/date-preview").DatePreview);
+    }
+  }
+  return createFactory(require("devtools/client/webconsole/new-console-output/components/message-types/default-renderer").DefaultRenderer);
 }
 
 module.exports.EvaluationResult = EvaluationResult;
