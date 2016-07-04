5f4dc0118e89958a14066f82832ea225f4133681
Nicholas Rosbrook <nrosbrook@mail.smcvt.edu>
2016-07-04 16:56:07 -0400

    Bug 1280794 - Updated WebURLFinder to use new WebURL regular expression that supports new TLDs r=nalexander,ahunt
    MozReview-Commit-ID: 42dfexg5qCs

--- mobile/android/services/src/main/java/org/mozilla/gecko/sync/setup/activities/WebURLFinder.java
+++ mobile/android/services/src/main/java/org/mozilla/gecko/sync/setup/activities/WebURLFinder.java
@@ -11,10 +11,36 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
-
-import android.util.Patterns;
+import java.util.regex.Pattern;
 
 public class WebURLFinder {
+  /**
+   * These regular expressions are taken from Android's Patterns.java.
+   * We brought them in to standardize URL matching across Android versions, instead of relying
+   * on Android version-dependent built-ins that can vary across Android versions.
+   * The original code can be found here:
+   * http://androidxref.com/6.0.1_r10/xref/frameworks/base/core/java/android/util/Patterns.java
+   *
+   */
+  public static final String GOOD_IRI_CHAR = "a-zA-Z0-9\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF";
+  public static final String GOOD_GTLD_CHAR = "a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF";
+  public static final String IRI = "[" + GOOD_IRI_CHAR + "]([" + GOOD_IRI_CHAR + "\\-]{0,61}[" + GOOD_IRI_CHAR + "]){0,1}";
+  public static final String GTLD = "[" + GOOD_GTLD_CHAR + "]{2,63}";
+  public static final String HOST_NAME = "(" + IRI + "\\.)+" + GTLD;
+  public static final Pattern IP_ADDRESS = Pattern.compile("((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(25[0-5]|2[0-4]"
+          + "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]"
+          + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
+          + "|[1-9][0-9]|[0-9]))");
+  public static final Pattern DOMAIN_NAME = Pattern.compile("(" + HOST_NAME + "|" + IP_ADDRESS + ")");
+  public static final Pattern WEB_URL = Pattern.compile("((?:(http|https|Http|Https|rtsp|Rtsp):\\/\\/(?:(?:[a-zA-Z0-9\\$\\-\\_\\.\\+\\!\\*\\'\\(\\)"
+          + "\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,64}(?:\\:(?:[a-zA-Z0-9\\$\\-\\_"
+          + "\\.\\+\\!\\*\\'\\(\\)\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,25})?\\@)?)?"
+          + "(?:" + DOMAIN_NAME + ")"
+          + "(?:\\:\\d{1,5})?)"
+          + "(\\/(?:(?:[" + GOOD_IRI_CHAR + "\\;\\/\\?\\:\\@\\&\\=\\#\\~"
+          + "\\-\\.\\+\\!\\*\\'\\(\\)\\,\\_])|(?:\\%[a-fA-F0-9]{2}))*)?"
+          + "(?:\\b|$)");
+
   public final List<String> candidates;
 
   public WebURLFinder(String string) {
@@ -90,7 +116,7 @@ public class WebURLFinder {
   }
 
   protected static List<String> candidateWebURLs(String string) {
-    Matcher matcher = Patterns.WEB_URL.matcher(string);
+    Matcher matcher = WEB_URL.matcher(string);
     List<String> matches = new LinkedList<String>();
 
     while (matcher.find()) {
