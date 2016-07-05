ebd0148bc92434629fa4ca0d5aa30b4ec72f1ca5
Andrzej Hunt <ahunt@mozilla.com>
2016-07-05 15:29:55 -0700

    Bug 1284587 - Distinguish normal from reader view bookmarks in telemetry r=liuche, a=gchang
    We want to be able to detect when we've opened a reader view item. Note: this is
    independent of whether or not we're in the Reading List smartfolder: it's possible
    to open reader view bookmarks from any real bookmark folder, or the smartfolder.
    
    MozReview-Commit-ID: KhqclodWSji

--- mobile/android/base/java/org/mozilla/gecko/home/BookmarksListView.java
+++ mobile/android/base/java/org/mozilla/gecko/home/BookmarksListView.java
@@ -24,6 +24,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.HeaderViewListAdapter;
 import android.widget.ListAdapter;
+
+import org.mozilla.gecko.reader.SavedReaderViewHelper;
 import org.mozilla.gecko.util.NetworkUtils;
 
 /**
@@ -169,7 +171,16 @@ public class BookmarksListView extends HomeListView
             // Otherwise, just open the URL
             final String url = cursor.getString(cursor.getColumnIndexOrThrow(Bookmarks.URL));
 
-            Telemetry.sendUIEvent(TelemetryContract.Event.LOAD_URL, TelemetryContract.Method.LIST_ITEM, "bookmarks");
+            final SavedReaderViewHelper rvh = SavedReaderViewHelper.getSavedReaderViewHelper(getContext());
+
+            final String extra;
+            if (rvh.isURLCached(url)) {
+                extra = "bookmarks-reader";
+            } else {
+                extra = "bookmarks";
+            }
+
+            Telemetry.sendUIEvent(TelemetryContract.Event.LOAD_URL, TelemetryContract.Method.LIST_ITEM, extra);
             Telemetry.addToHistogram("FENNEC_LOAD_SAVED_PAGE", NetworkUtils.isConnected(getContext()) ? 2 : 3);
 
             // This item is a TwoLinePageRow, so we allow switch-to-tab.
