4aa108fbc3cc86786539325602ccad1d79ca5b17
Jim Chen <nchen@mozilla.com>
2016-07-05 14:34:57 -0400

    Bug 1280446 - Create PushService if needed; r=snorp, a=gchang
    If PushService has not been created when getInstance is called, create
    the PushService instead of throwing an error. This fixes a possible race
    condition between initializing PushService and receiving a push message,
    where we can receive a push message first.

--- mobile/android/base/java/org/mozilla/gecko/push/PushService.java
+++ mobile/android/base/java/org/mozilla/gecko/push/PushService.java
@@ -66,9 +66,9 @@ public class PushService implements BundleEventListener {
             "History:GetPrePathLastVisitedTimeMilliseconds",
     };
 
-    public static synchronized PushService getInstance() {
+    public static synchronized PushService getInstance(Context context) {
         if (sInstance == null) {
-            throw new IllegalStateException("PushService not yet created!");
+            onCreate(context);
         }
         return sInstance;
     }
