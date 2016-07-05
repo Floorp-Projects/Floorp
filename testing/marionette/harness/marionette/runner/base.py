2a94c9ca27fae8ea136bacbf3e7ed551048320df
Maja Frydrychowicz <mjzffr@gmail.com>
2016-07-05 18:35:51 -0400

    Bug 1276011 - Fix symbols_path arg not being passed on to runner class. r=automatedtester, a=test-only
    MozReview-Commit-ID: 4NHNONnDjSk
    
    --HG--
    extra : source : f564cb1eb300136eb6f6dfe70b924d99b668da34

--- testing/marionette/harness/marionette/runner/base.py
+++ testing/marionette/harness/marionette/runner/base.py
@@ -660,6 +660,7 @@ class BaseMarionetteTestRunner(object):
             'prefs': self.prefs,
             'startup_timeout': self.startup_timeout,
             'verbose': self.verbose,
+            'symbols_path': self.symbols_path,
         }
         if self.bin:
             kwargs.update({
