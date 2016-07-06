a5c82e6e8ba3f3ec200b9bcb0b9e22ce902edcfc
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-07-06 16:13:19 +0200

    Backed out changeset 74df8f458a42 (bug 1274516) for bustage

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -60,7 +60,6 @@
 #include "mozilla/media/MediaChild.h"
 #include "mozilla/BasePrincipal.h"
 #include "mozilla/WebBrowserPersistDocumentChild.h"
-#include "imgLoader.h"
 
 #if defined(MOZ_CONTENT_SANDBOX)
 #if defined(XP_WIN)
@@ -2239,16 +2238,6 @@ ContentChild::RecvRegisterChromeItem(const ChromeRegistryItem& item)
       return false;
   }
 
-  return true;
-}
-
-bool
-ContentChild::RecvClearImageCache(const bool& privateLoader, const bool& chrome)
-{
-  imgLoader* loader = privateLoader ? imgLoader::PrivateBrowsingLoader() :
-                                      imgLoader::NormalLoader();
-
-  loader->ClearCache(chrome);
   return true;
 }
 
