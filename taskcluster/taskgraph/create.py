311285806776be6cdab9ad652995c87a2e236f16
Kalpesh Krishna <kalpeshk2011@gmail.com>
2016-07-06 03:03:00 +0200

    Bug 1284005 - Replacing timestamps with relative timestamps. r=dustin

--- taskcluster/taskgraph/create.py
+++ taskcluster/taskgraph/create.py
@@ -12,6 +12,10 @@ import os
 import logging
 
 from slugid import nice as slugid
+from taskgraph.util.time import (
+    current_json_time,
+    json_time_from_now
+)
 
 logger = logging.getLogger(__name__)
 
@@ -39,7 +43,6 @@ def create_tasks(taskgraph, label_to_taskid):
         # that.
         for task_id in taskgraph.graph.visit_postorder():
             task_def = taskgraph.tasks[task_id].task
-
             # if this task has no dependencies, make it depend on this decision
             # task so that it does not start immediately; and so that if this loop
             # fails halfway through, none of the already-created tasks run.
@@ -65,6 +68,11 @@ def create_tasks(taskgraph, label_to_taskid):
 def _create_task(session, task_id, label, task_def):
     # create the task using 'http://taskcluster/queue', which is proxied to the queue service
     # with credentials appropriate to this job.
+
+    # Resolve timestamps
+    now = current_json_time(datetime_format=True)
+    task_def = resolve_timestamps(now, task_def)
+
     logger.debug("Creating task with taskId {} for {}".format(task_id, label))
     res = session.put('http://taskcluster/queue/v1/task/{}'.format(task_id),
                       data=json.dumps(task_def))
@@ -74,3 +82,17 @@ def _create_task(session, task_id, label, task_def):
         except:
             logger.error(res.text)
         res.raise_for_status()
+
+
+def resolve_timestamps(now, task_def):
+    def recurse(val):
+        if isinstance(val, list):
+            return [recurse(v) for v in val]
+        elif isinstance(val, dict):
+            if val.keys() == ['relative-datestamp']:
+                return json_time_from_now(val['relative-datestamp'], now)
+            else:
+                return {k: recurse(v) for k, v in val.iteritems()}
+        else:
+            return val
+    return recurse(task_def)
