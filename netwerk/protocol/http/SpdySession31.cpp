48e9ddd7b11526c301a9b1c6de913a620aaf4cde
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-07-06 16:39:35 +0200

    Backed out changeset 7132b115ab91 (bug 1275917) for causing timeouts in test_notification_http2.js

--- netwerk/protocol/http/SpdySession31.cpp
+++ netwerk/protocol/http/SpdySession31.cpp
@@ -338,17 +338,6 @@ SpdySession31::AddStream(nsAHttpTransaction *aHttpTransaction,
     mConnection = aHttpTransaction->Connection();
   }
 
-  if (mClosed || mShouldGoAway) {
-    nsHttpTransaction *trans = aHttpTransaction->QueryHttpTransaction();
-    if (trans) {
-      LOG3(("SpdySession31::AddStream %p atrans=%p trans=%p session unusable - resched.\n",
-            this, aHttpTransaction, trans));
-      aHttpTransaction->SetConnection(nullptr);
-      gHttpHandler->InitiateTransaction(trans, trans->Priority());
-      return true;
-    }
-  }
-
   aHttpTransaction->SetConnection(this);
 
   if (aUseTunnel) {
