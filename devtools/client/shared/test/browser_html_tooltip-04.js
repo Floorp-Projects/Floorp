34cb206f63873ce865dfc2c6dea4b43fb44f07fe
Julian Descottes <jdescottes@mozilla.com>
2016-07-06 14:50:44 +0200

    Bug 1267403 - HTMLTooltip: add useXulWrapper option when displayed in a XUL document;r=ochameau
    The HTMLTooltip supports an additional configuration parameter "useXulWrapper".
    When set to true, if the tooltip is displayed in a XUL document, a XUL panel
    will be used as an additional container for the tooltip.
    
    This allows the tooltip to be displayed anywhere on the screen and can be
    useful when displayed in small toolboxes.
    
    MozReview-Commit-ID: 63kv4vAeW5R

--- devtools/client/shared/test/browser_html_tooltip-04.js
+++ devtools/client/shared/test/browser_html_tooltip-04.js
@@ -40,7 +40,7 @@ add_task(function* () {
   let [,, doc] = yield createHost("bottom", TEST_URI);
 
   info("Create HTML tooltip");
-  let tooltip = new HTMLTooltip({doc}, {});
+  let tooltip = new HTMLTooltip({doc}, {useXulWrapper: false});
   let div = doc.createElementNS(HTML_NS, "div");
   div.style.height = "100%";
   tooltip.setContent(div, {width: TOOLTIP_WIDTH, height: TOOLTIP_HEIGHT});
