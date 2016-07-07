f54b3fc301a5fa522dd2ed522b2a6feeb4fb44f2
Masayuki Nakano <masayuki@d-toybox.com>
2016-07-07 16:16:12 +0900

    Bug 1260651 part.26 Rename mozilla::dom::JoinNodeTxn to mozilla::JoinNodeTransaction (and their files too) r=mccr8
    MozReview-Commit-ID: ChcoinM0sfU

--- editor/libeditor/JoinNodeTransaction.h
+++ editor/libeditor/JoinNodeTransaction.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef JoinNodeTxn_h__
-#define JoinNodeTxn_h__
+#ifndef JoinNodeTransaction_h
+#define JoinNodeTransaction_h
 
 #include "EditTxn.h"                    // for EditTxn, NS_DECL_EDITTXN
 #include "nsCOMPtr.h"                   // for nsCOMPtr
@@ -16,7 +16,6 @@ class nsEditor;
 class nsINode;
 
 namespace mozilla {
-namespace dom {
 
 /**
  * A transaction that joins two nodes E1 (left node) and E2 (right node) into a
@@ -24,19 +23,23 @@ namespace dom {
  * children of E2.  After DoTransaction() and RedoTransaction(), E1 is removed
  * from the content tree and E2 remains.
  */
-class JoinNodeTxn : public EditTxn
+class JoinNodeTransaction final : public EditTxn
 {
 public:
-  /** @param aEditor    the provider of core editing operations
-    * @param aLeftNode  the first of two nodes to join
-    * @param aRightNode the second of two nodes to join
-    */
-  JoinNodeTxn(nsEditor& aEditor, nsINode& aLeftNode, nsINode& aRightNode);
+  /**
+   * @param aEditor         The provider of core editing operations.
+   * @param aLeftNode       The first of two nodes to join.
+   * @param aRightNode      The second of two nodes to join.
+   */
+  JoinNodeTransaction(nsEditor& aEditor,
+                      nsINode& aLeftNode, nsINode& aRightNode);
 
-  /* Call this after constructing to ensure the inputs are correct */
+  /**
+   * Call this after constructing to ensure the inputs are correct.
+   */
   nsresult CheckValidity();
 
-  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(JoinNodeTxn, EditTxn)
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(JoinNodeTransaction, EditTxn)
   NS_IMETHOD QueryInterface(REFNSIID aIID, void** aInstancePtr) override;
 
   NS_DECL_EDITTXN
@@ -44,23 +47,20 @@ public:
 protected:
   nsEditor&  mEditor;
 
-  /** The nodes to operate upon.  After the merge, mRightNode remains and
-   * mLeftNode is removed from the content tree.
-    */
+  // The nodes to operate upon.  After the merge, mRightNode remains and
+  // mLeftNode is removed from the content tree.
   nsCOMPtr<nsINode> mLeftNode;
   nsCOMPtr<nsINode> mRightNode;
 
-  /** The offset into mNode where the children of mElement are split (for
-    * undo). mOffset is the index of the first child in the right node.  -1
-    * means the left node had no children.
-    */
+  // The offset into mNode where the children of mElement are split (for
+  // undo). mOffset is the index of the first child in the right node.  -1
+  // means the left node had no children.
   uint32_t  mOffset;
 
-  /** The parent node containing mLeftNode and mRightNode */
+  // The parent node containing mLeftNode and mRightNode.
   nsCOMPtr<nsINode> mParent;
 };
 
-} // namespace dom
 } // namespace mozilla
 
-#endif
+#endif // #ifndef JoinNodeTransaction_h
