5b0bc35750d6ae2ac6008b4afb200d8cb4444e6b
Boris Chiou <boris.chiou@gmail.com>
2016-07-07 11:44:25 +0800

    Bug 1245751 - Part 8: Allow href without xlink on SVG <textPath> elements. r=jwatt
    MozReview-Commit-ID: KObkvkctP4L

--- dom/svg/SVGTextPathElement.cpp
+++ dom/svg/SVGTextPathElement.cpp
@@ -61,8 +61,9 @@ nsSVGElement::EnumInfo SVGTextPathElement::sEnumInfo[3] =
   }
 };
 
-nsSVGElement::StringInfo SVGTextPathElement::sStringInfo[1] =
+nsSVGElement::StringInfo SVGTextPathElement::sStringInfo[2] =
 {
+  { &nsGkAtoms::href, kNameSpaceID_None, true },
   { &nsGkAtoms::href, kNameSpaceID_XLink, true }
 };
 
@@ -82,7 +83,9 @@ NS_IMPL_ELEMENT_CLONE_WITH_INIT(SVGTextPathElement)
 already_AddRefed<SVGAnimatedString>
 SVGTextPathElement::Href()
 {
-  return mStringAttributes[HREF].ToDOMAnimatedString(this);
+  return mStringAttributes[HREF].IsExplicitlySet()
+         ? mStringAttributes[HREF].ToDOMAnimatedString(this)
+         : mStringAttributes[XLINK_HREF].ToDOMAnimatedString(this);
 }
 
 //----------------------------------------------------------------------
