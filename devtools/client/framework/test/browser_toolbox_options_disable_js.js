997396e94997a6a48f4e126576a99db5549dc078
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-07-07 06:22:16 +0200

    Backed out changeset e178424c2e2a (bug 1261714) for landed already on m-i

--- devtools/client/framework/test/browser_toolbox_options_disable_js.js
+++ devtools/client/framework/test/browser_toolbox_options_disable_js.js
@@ -7,74 +7,59 @@
 
 const TEST_URI = URL_ROOT + "browser_toolbox_options_disable_js.html";
 
+var doc;
+var toolbox;
+
 function test() {
   gBrowser.selectedTab = gBrowser.addTab();
   let target = TargetFactory.forTab(gBrowser.selectedTab);
 
   gBrowser.selectedBrowser.addEventListener("load", function onLoad(evt) {
     gBrowser.selectedBrowser.removeEventListener(evt.type, onLoad, true);
+    doc = content.document;
     gDevTools.showToolbox(target).then(testSelectTool);
   }, true);
 
-  BrowserTestUtils.loadURI(gBrowser.selectedBrowser, TEST_URI);
+  content.location = TEST_URI;
 }
 
-function testSelectTool(toolbox) {
-  toolbox.once("options-selected", () => testToggleJS(toolbox));
+function testSelectTool(aToolbox) {
+  toolbox = aToolbox;
+  toolbox.once("options-selected", testJSEnabled);
   toolbox.selectTool("options");
 }
 
-let testToggleJS = Task.async(function* (toolbox) {
+function testJSEnabled(event, tool, secondPass) {
   ok(true, "Toolbox selected via selectTool method");
-
-  yield testJSEnabled();
-  yield testJSEnabledIframe();
-
-  // Disable JS.
-  yield toggleJS(toolbox);
-
-  yield testJSDisabled();
-  yield testJSDisabledIframe();
-
-  // Re-enable JS.
-  yield toggleJS(toolbox);
-
-  yield testJSEnabled();
-  yield testJSEnabledIframe();
-
-  finishUp(toolbox);
-});
-
-function* testJSEnabled() {
   info("Testing that JS is enabled");
 
-  // We use waitForTick here because switching docShell.allowJavascript to true
+  // We use executeSoon here because switching docSehll.allowJavascript to true
   // takes a while to become live.
-  yield waitForTick();
-
-  yield ContentTask.spawn(gBrowser.selectedBrowser, {}, function () {
-    let doc = content.document;
+  executeSoon(function () {
     let output = doc.getElementById("output");
     doc.querySelector("#logJSEnabled").click();
     is(output.textContent, "JavaScript Enabled", 'Output is "JavaScript Enabled"');
+    testJSEnabledIframe(secondPass);
   });
 }
 
-function* testJSEnabledIframe() {
+function testJSEnabledIframe(secondPass) {
   info("Testing that JS is enabled in the iframe");
 
-  yield ContentTask.spawn(gBrowser.selectedBrowser, {}, function () {
-    let doc = content.document;
-    let iframe = doc.querySelector("iframe");
-    let iframeDoc = iframe.contentDocument;
-    let output = iframeDoc.getElementById("output");
-    iframeDoc.querySelector("#logJSEnabled").click();
-    is(output.textContent, "JavaScript Enabled",
-                            'Output is "JavaScript Enabled" in iframe');
-  });
+  let iframe = doc.querySelector("iframe");
+  let iframeDoc = iframe.contentDocument;
+  let output = iframeDoc.getElementById("output");
+  iframeDoc.querySelector("#logJSEnabled").click();
+  is(output.textContent, "JavaScript Enabled",
+                          'Output is "JavaScript Enabled" in iframe');
+  if (secondPass) {
+    finishUp();
+  } else {
+    toggleJS().then(testJSDisabled);
+  }
 }
 
-function* toggleJS(toolbox) {
+let toggleJS = Task.async(function* () {
   let panel = toolbox.getCurrentPanel();
   let cbx = panel.panelDoc.getElementById("devtools-disable-javascript");
 
@@ -87,38 +72,38 @@ function* toggleJS(toolbox) {
   let browserLoaded = BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
   cbx.click();
   yield browserLoaded;
-}
+  doc = content.document;
+});
 
-function* testJSDisabled() {
+function testJSDisabled() {
   info("Testing that JS is disabled");
 
-  yield ContentTask.spawn(gBrowser.selectedBrowser, {}, function () {
-    let doc = content.document;
-    let output = doc.getElementById("output");
-    doc.querySelector("#logJSDisabled").click();
+  let output = doc.getElementById("output");
+  doc.querySelector("#logJSDisabled").click();
 
-    ok(output.textContent !== "JavaScript Disabled",
-       'output is not "JavaScript Disabled"');
-  });
+  ok(output.textContent !== "JavaScript Disabled",
+     'output is not "JavaScript Disabled"');
+  testJSDisabledIframe();
 }
 
-function* testJSDisabledIframe() {
+function testJSDisabledIframe() {
   info("Testing that JS is disabled in the iframe");
 
-  yield ContentTask.spawn(gBrowser.selectedBrowser, {}, function () {
-    let doc = content.document;
-    let iframe = doc.querySelector("iframe");
-    let iframeDoc = iframe.contentDocument;
-    let output = iframeDoc.getElementById("output");
-    iframeDoc.querySelector("#logJSDisabled").click();
-    ok(output.textContent !== "JavaScript Disabled",
-       'output is not "JavaScript Disabled" in iframe');
+  let iframe = doc.querySelector("iframe");
+  let iframeDoc = iframe.contentDocument;
+  let output = iframeDoc.getElementById("output");
+  iframeDoc.querySelector("#logJSDisabled").click();
+  ok(output.textContent !== "JavaScript Disabled",
+     'output is not "JavaScript Disabled" in iframe');
+  toggleJS().then(function () {
+    testJSEnabled(null, null, true);
   });
 }
 
-function finishUp(toolbox) {
+function finishUp() {
   toolbox.destroy().then(function () {
     gBrowser.removeCurrentTab();
+    toolbox = doc = null;
     finish();
   });
 }
