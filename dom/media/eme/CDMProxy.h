b7170a045411233a99b09651124b4450ba5d8d46
Chris Pearce <cpearce@mozilla.com>
2016-07-07 17:26:15 +1200

    Bug 1278198 - Pipe through distinctive identifier and persistent state allowed. r=gerald
    MozReview-Commit-ID: A92e0XGp5s4

--- dom/media/eme/CDMProxy.h
+++ dom/media/eme/CDMProxy.h
@@ -50,8 +50,14 @@ public:
   typedef MozPromise<DecryptResult, DecryptResult, /* IsExclusive = */ true> DecryptPromise;
 
   // Main thread only.
-  CDMProxy(dom::MediaKeys* aKeys, const nsAString& aKeySystem)
-  : mKeys(aKeys), mKeySystem(aKeySystem)
+  CDMProxy(dom::MediaKeys* aKeys,
+           const nsAString& aKeySystem,
+           bool aDistinctiveIdentifierRequired,
+           bool aPersistentStateRequired)
+    : mKeys(aKeys)
+    , mKeySystem(aKeySystem)
+    , mDistinctiveIdentifierRequired(aDistinctiveIdentifierRequired)
+    , mPersistentStateRequired(aPersistentStateRequired)
   {}
 
   // Main thread only.
@@ -229,6 +235,9 @@ protected:
   nsCString mNodeId;
 
   CDMCaps mCapabilites;
+
+  const bool mDistinctiveIdentifierRequired;
+  const bool mPersistentStateRequired;
 };
 
 
