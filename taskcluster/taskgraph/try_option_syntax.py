5ecaa89386551b0e98fdb0223877b0bc3b601118
Steve Fink <sfink@mozilla.com>
2016-07-08 13:51:11 -0700

    Bug 1280637 - Make sm-{asan,tsan,msan} try-only for now, r=garndt
    MozReview-Commit-ID: Dx58G6O3B2Y

--- taskcluster/taskgraph/try_option_syntax.py
+++ taskcluster/taskgraph/try_option_syntax.py
@@ -6,8 +6,11 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import argparse
 import copy
+import logging
 import re
 import shlex
+
+logger = logging.getLogger(__name__)
 
 TRY_DELIMITER = 'try:'
 
@@ -135,6 +138,9 @@ RIDEALONG_BUILDS = {
         'sm-compacting',
         'sm-rootanalysis',
         'sm-package',
+        'sm-tsan',
+        'sm-asan',
+        'sm-msan',
     ],
 }
 
@@ -235,6 +241,8 @@ class TryOptionSyntax(object):
             results.append(build)
             if build in RIDEALONG_BUILDS:
                 results.extend(RIDEALONG_BUILDS[build])
+                logger.info("platform %s triggers ridealong builds %s" %
+                            (build, RIDEALONG_BUILDS[build]))
 
         return results
 
