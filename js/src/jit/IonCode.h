aac6f76e6fb70e93621de7c37109295290281a5e
sk <sk.alvin.x@gmail.com>
2016-07-08 09:05:32 +0800

    Bug 1271565 - Add autoflush icache for aarch64 target. r=sstangl

--- js/src/jit/IonCode.h
+++ js/src/jit/IonCode.h
@@ -751,7 +751,7 @@ struct VMFunction;
 struct AutoFlushICache
 {
   private:
-#if defined(JS_CODEGEN_ARM) || defined(JS_CODEGEN_MIPS32) || defined(JS_CODEGEN_MIPS64)
+#if defined(JS_CODEGEN_ARM) || defined(JS_CODEGEN_ARM64) || defined(JS_CODEGEN_MIPS32) || defined(JS_CODEGEN_MIPS64)
     uintptr_t start_;
     uintptr_t stop_;
     const char* name_;
