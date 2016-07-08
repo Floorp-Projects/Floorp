ab86a80ea565c3204f7070fc032db808e62dfb90
Eric Rahm <erahm@mozilla.com>
2016-07-08 11:47:04 -0700

    Bug 1274913 - Use LazyLogModule directly for DirectShow logging. r=rillian, a=gchang
    This should help fix unitialized statics crashes on Windows in the DirectShow logging code.
    
    Additional changes are included to fix unified build issues.
    
    --HG--
    extra : source : 0fadabf932c25817f2b8f218532171b82ac96280
    extra : intermediate-source : 8ecdd5e5bc79c336bde0d40eebf67c8bbdeae394

--- dom/media/directshow/DirectShowReader.h
+++ dom/media/directshow/DirectShowReader.h
@@ -60,6 +60,8 @@ public:
   RefPtr<SeekPromise>
   Seek(SeekTarget aTarget, int64_t aEndTime) override;
 
+  static const GUID CLSID_MPEG_LAYER_3_DECODER_FILTER;
+
 private:
   // Notifies the filter graph that playback is complete. aStatus is
   // the code to send to the filter graph. Always returns false, so
