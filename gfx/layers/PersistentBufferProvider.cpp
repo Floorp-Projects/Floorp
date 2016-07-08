b51ed22a3fb89ad32234fdbd95282e8acf4db9a2
Nicolas Silva <nsilva@mozilla.com>
2016-07-08 19:26:48 +0200

    Bug 1284721 - Flush CanvasRenderingContext2D's DrawTarget before handing it off to the BufferProvider. r=lsalzman

--- gfx/layers/PersistentBufferProvider.cpp
+++ gfx/layers/PersistentBufferProvider.cpp
@@ -40,6 +40,11 @@ PersistentBufferProviderBasic::ReturnDrawTarget(already_AddRefed<gfx::DrawTarget
 {
   RefPtr<gfx::DrawTarget> dt(aDT);
   MOZ_ASSERT(mDrawTarget == dt);
+  if (dt) {
+    // Since SkiaGL default to storing drawing command until flush
+    // we have to flush it before present.
+    dt->Flush();
+  }
   return true;
 }
 
@@ -205,10 +210,12 @@ PersistentBufferProviderShared::ReturnDrawTarget(already_AddRefed<gfx::DrawTarge
 
   mBack->Unlock();
 
-  if (!mBuffer && mFront && !mFront->IsLocked()) {
-    mBuffer.swap(mFront);
+  if (mFront != mBack && !mBuffers.Contains(mFront)) {
+    mBuffers.AppendElement(mFront);
   }
 
+  // Make mFront point to the now realized back buffer. mFront is what the next
+  // transaction will pick up and send to the compositor.
   mFront = mBack;
 
   return true;
