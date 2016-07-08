2b0ce29d0c97e3c5f39536586b14b0d6dd7ae5af
Henrik Skupin <mail@hskupin.info>
2016-07-08 10:13:26 +0200

    Bug 1247596 - Fix reference to the update type which is part of the patch. r=maja_zf
    MozReview-Commit-ID: K0ee3sp428U

--- testing/firefox-ui/harness/firefox_ui_harness/testcases.py
+++ testing/firefox-ui/harness/firefox_ui_harness/testcases.py
@@ -158,20 +158,13 @@ class UpdateTestCase(FirefoxTestCase):
         return about_window.deck.selected_panel != about_window.deck.no_updates_found
 
     def check_update_applied(self):
-        self.updates[self.current_update_index]['build_post'] = self.software_update.build_info
+        """Check that the update has been applied correctly"""
+        update = self.updates[self.current_update_index]
+        update['build_post'] = self.software_update.build_info
 
         about_window = self.browser.open_about_window()
         try:
             update_available = self.check_for_updates(about_window)
-
-            # No further updates should be offered now with the same update type
-            if update_available:
-                self.download_update(about_window, wait_for_finish=False)
-                self.assertNotEqual(self.software_update.active_update.type,
-                                    self.updates[self.current_update_index].type)
-
-            # Check that the update has been applied correctly
-            update = self.updates[self.current_update_index]
 
             # The upgraded version should be identical with the version given by
             # the update and we shouldn't have run a downgrade
@@ -201,6 +194,17 @@ class UpdateTestCase(FirefoxTestCase):
             # Check that no application-wide add-ons have been disabled
             self.assertEqual(update['build_post']['disabled_addons'],
                              update['build_pre']['disabled_addons'])
+
+            # Bug 604364 - We do not support watershed releases yet.
+            if update_available:
+                self.download_update(about_window, wait_for_finish=False)
+                self.assertNotEqual(self.software_update.active_update.type,
+                                    update['patch']['type'],
+                                    'No further update of the same type gets offered: '
+                                    '{0} != {1}'.format(
+                                        self.software_update.active_update.type,
+                                        update['patch']['type']
+                                    ))
 
             update['success'] = True
 
