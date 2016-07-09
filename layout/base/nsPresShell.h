67873a54ae3b4938630d6a55f36d2220c6691825
Seth Fowler <mark.seth.fowler@gmail.com>
2016-07-08 17:55:03 -0700

    Bug 1284350 - Revert "Bug 1269934 - Handle visible frame sets more generically in PresShell. r=mstange". a=lizzard
    This reverts commit 95a35f5c34c35325dc3627173767e8448bc2ddec.

--- layout/base/nsPresShell.h
+++ layout/base/nsPresShell.h
@@ -38,6 +38,7 @@
 #include "mozilla/StyleSetHandle.h"
 #include "mozilla/UniquePtr.h"
 #include "MobileViewportManager.h"
+#include "Visibility.h"
 #include "ZoomConstraintsClient.h"
 
 class nsRange;
@@ -405,7 +406,7 @@ public:
   void RebuildApproximateFrameVisibility(nsRect* aRect = nullptr,
                                          bool aRemoveOnly = false) override;
 
-  void MarkFrameVisible(nsIFrame* aFrame, VisibilityCounter aCounter) override;
+  void MarkFrameVisibleInDisplayPort(nsIFrame* aFrame) override;
   void MarkFrameNonvisible(nsIFrame* aFrame) override;
 
   bool AssumeAllFramesVisible() override;
@@ -789,15 +790,6 @@ protected:
   nsRevocableEventPtr<nsRunnableMethod<PresShell>> mUpdateApproximateFrameVisibilityEvent;
   nsRevocableEventPtr<nsRunnableMethod<PresShell>> mNotifyCompositorOfVisibleRegionsChangeEvent;
 
-  VisibleFrames& VisibleFramesForCounter(VisibilityCounter aCounter)
-  {
-    switch (aCounter)
-    {
-      case VisibilityCounter::MAY_BECOME_VISIBLE: return mApproximatelyVisibleFrames;
-      case VisibilityCounter::IN_DISPLAYPORT:     return mInDisplayPortFrames;
-    }
-  }
-
   // A set of frames that were visible or could be visible soon at the time
   // that we last did an approximate frame visibility update.
   VisibleFrames mApproximatelyVisibleFrames;
@@ -807,15 +799,6 @@ protected:
 
   struct VisibleRegionsContainer
   {
-    VisibleRegions& ForCounter(VisibilityCounter aCounter)
-    {
-      switch (aCounter)
-      {
-        case VisibilityCounter::MAY_BECOME_VISIBLE: return mApproximate;
-        case VisibilityCounter::IN_DISPLAYPORT:     return mInDisplayPort;
-      }
-    }
-
     // The approximately visible regions calculated during the last update to
     // approximate frame visibility.
     VisibleRegions mApproximate;
--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -1513,7 +1513,7 @@ nsIFrame::UpdateVisibilitySynchronously()
   }
 
   if (presShell->AssumeAllFramesVisible()) {
-    presShell->MarkFrameVisible(this, VisibilityCounter::IN_DISPLAYPORT);
+    presShell->MarkFrameVisibleInDisplayPort(this);
     return;
   }
 
@@ -1554,7 +1554,7 @@ nsIFrame::UpdateVisibilitySynchronously()
   }
 
   if (visible) {
-    presShell->MarkFrameVisible(this, VisibilityCounter::IN_DISPLAYPORT);
+    presShell->MarkFrameVisibleInDisplayPort(this);
   } else {
     presShell->MarkFrameNonvisible(this);
   }
@@ -2756,7 +2756,7 @@ nsIFrame::BuildDisplayListForChild(nsDisplayListBuilder*   aBuilder,
   // within the displayport.
   if (aBuilder->IsPaintingToWindow() && child->TrackingVisibility()) {
     nsIPresShell* shell = child->PresContext()->PresShell();
-    shell->MarkFrameVisible(child, VisibilityCounter::IN_DISPLAYPORT);
+    shell->MarkFrameVisibleInDisplayPort(child);
   }
 
   // Child is composited if it's transformed, partially transparent, or has
