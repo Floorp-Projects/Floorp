c78e9f372e06f1cfe9011c04feb7db2023a19441
Jonathan Hao <jhao@mozilla.com>
2016-07-09 21:41:30 +0800

    Bug 1279568 - Modify thumbnails tests to make sure cookies can't be set by document.cookies or xhr. r=markh

--- toolkit/components/thumbnails/test/browser_thumbnails_bg_no_cookies_stored.js
+++ toolkit/components/thumbnails/test/browser_thumbnails_bg_no_cookies_stored.js
@@ -4,7 +4,11 @@
 // check that if a page captured in the background attempts to set a cookie,
 // that cookie is not saved for subsequent requests.
 function* runTests() {
-  let url = bgTestPageURL({ setRedCookie: true });
+  let url = bgTestPageURL({
+    setRedCookie: true,
+    iframe: bgTestPageURL({ setRedCookie: true}),
+    xhr: bgTestPageURL({ setRedCookie: true})
+  });
   ok(!thumbnailExists(url), "Thumbnail file should not exist before capture.");
   yield bgCapture(url);
   ok(thumbnailExists(url), "Thumbnail file should exist after capture.");
