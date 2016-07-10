31fcbe13dd2c5e38a8db56b78bec3bc7f53cd5dd
Jarda Snajdr <jsnajdr@gmail.com>
2016-07-10 10:37:00 +0200

    Bug 1285638 - Network monitor leaks window when a parent-side tab is closed. r=ochameau, a=lizzard
    --HG--
    extra : rebase_source : 2af475c2614d1eee82ef6896803a9a8f9d6bd341
    extra : source : a2f084333bf3fddaa15909660f39652af1def9ab

---
+++ devtools/client/netmonitor/test/browser_net_leak_on_tab_close.js
@@ -0,0 +1,18 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests that netmonitor doesn't leak windows on parent-side pages (bug 1285638)
+ */
+
+add_task(function* () {
+  // Tell initNetMonitor to enable cache. Otherwise it will assert that there were more
+  // than zero network requests during the page load. But when loading about:config,
+  // there are none.
+  let [,, monitor] = yield initNetMonitor("about:config", null, true);
+  ok(monitor, "The network monitor was opened");
+  yield teardown(monitor);
+  finish();
+});
