0e242f0ac778ed070446f07927de376ab42d79a7
Andrea Marchesini <amarchesini@mozilla.com>
2016-07-11 00:28:10 +0200

    Bug 1279503 - part 1 - removed a non-used variable in BrowserConfiguration, r=gabor

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -653,8 +653,7 @@ protected:
   virtual bool RecvAudioChannelActivityNotification(const uint32_t& aAudioChannel,
                                                     const bool& aActive) override;
 
-  bool InitBrowserConfiguration(const nsCString& aURI,
-                                BrowserConfiguration& aConfiguration);
+  bool InitBrowserConfiguration(BrowserConfiguration& aConfiguration);
 
   ContentCacheInParent mContentCache;
 
