f8c2f001464bc2b0f9b9badde481fe9786c66ac5
Andrea Marchesini <amarchesini@mozilla.com>
2016-07-11 16:04:04 +0200

    Bug 1285947 - Rename BrowserConfiguration to ServiceWorkerConfiguration, r=gabor, a=gchang
    --HG--
    rename : dom/ipc/BrowserConfiguration.ipdlh => dom/ipc/ServiceWorkerConfiguration.ipdlh

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -5639,7 +5639,7 @@ ContentParent::PermissionManagerRelease(const ContentParentId& aCpId,
 }
 
 bool
-ContentParent::RecvGetBrowserConfiguration(BrowserConfiguration* aConfig)
+ContentParent::RecvGetServiceWorkerConfiguration(ServiceWorkerConfiguration* aConfig)
 {
   MOZ_ASSERT(XRE_IsParentProcess());
 
