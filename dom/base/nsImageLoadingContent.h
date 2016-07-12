8b8d8e14ab270ab2d11c689eb9bbd4329402079d
Ben Tian <btian@mozilla.com>
2016-07-12 10:56:42 +0800

    Bug 1264769 - Part 2: Dispatch loadstart event for image loading. r=hsivonen

--- dom/base/nsImageLoadingContent.h
+++ dom/base/nsImageLoadingContent.h
@@ -133,13 +133,15 @@ protected:
    * @param aNotify If true, nsIDocumentObserver state change notifications
    *                will be sent as needed.
    * @param aImageLoadType The ImageLoadType for this request
+   * @param aLoadStart If true, dispatch "loadstart" event.
    * @param aDocument Optional parameter giving the document this node is in.
    *        This is purely a performance optimization.
    * @param aLoadFlags Optional parameter specifying load flags to use for
    *        the image load
    */
   nsresult LoadImage(nsIURI* aNewURI, bool aForce, bool aNotify,
-                     ImageLoadType aImageLoadType, nsIDocument* aDocument = nullptr,
+                     ImageLoadType aImageLoadType, bool aLoadStart = true,
+                     nsIDocument* aDocument = nullptr,
                      nsLoadFlags aLoadFlags = nsIRequest::LOAD_NORMAL);
 
   /**
