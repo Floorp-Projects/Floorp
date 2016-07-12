73029ecf1e9c11c732e090075ac55122c6533576
Julian Hector <julian.r.hector@gmail.com>
2016-07-12 18:24:54 +0200

    Bug 1286119 - Allow sys_mremap when jemalloc is disabled. r=gcp
    MozReview-Commit-ID: FRVorDIIUVR

--- security/sandbox/linux/SandboxFilter.cpp
+++ security/sandbox/linux/SandboxFilter.cpp
@@ -574,8 +574,8 @@ public:
     case __NR_mprotect:
     case __NR_brk:
     case __NR_madvise:
-#if defined(ANDROID) && !defined(MOZ_MEMORY)
-      // Android's libc's realloc uses mremap.
+#if !defined(MOZ_MEMORY)
+      // libc's realloc uses mremap (Bug 1286119).
     case __NR_mremap:
 #endif
       return Allow();
