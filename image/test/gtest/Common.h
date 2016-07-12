8e2ea1af00ce772317198fac75ee0baa576f08a7
Seth Fowler <mark.seth.fowler@gmail.com>
2016-07-12 00:23:30 -0700

    Bug 1286165 (Part 3) - Add StreamingLexer tests for the SourceBufferIterator version of Lex(). r=njn

--- image/test/gtest/Common.h
+++ image/test/gtest/Common.h
@@ -188,6 +188,37 @@ bool RowHasPixels(gfx::SourceSurface* aSurface,
                   int32_t aRow,
                   const std::vector<BGRAColor>& aPixels);
 
+// ExpectNoResume is an IResumable implementation for use by tests that expect
+// Resume() to never get called.
+class ExpectNoResume final : public IResumable
+{
+public:
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(ExpectNoResume, override)
+
+  void Resume() override { FAIL() << "Resume() should not get called"; }
+
+private:
+  ~ExpectNoResume() override { }
+};
+
+// CountResumes is an IResumable implementation for use by tests that expect
+// Resume() to get called a certain number of times.
+class CountResumes : public IResumable
+{
+public:
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(CountResumes, override)
+
+  CountResumes() : mCount(0) { }
+
+  void Resume() override { mCount++; }
+  uint32_t Count() const { return mCount; }
+
+private:
+  ~CountResumes() override { }
+
+  uint32_t mCount;
+};
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // SurfacePipe Helpers
