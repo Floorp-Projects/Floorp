43bbac13b34584cbd9a99517a185baf75359476e
Michal Novotny <michal.novotny@gmail.com>
2016-07-12 17:58:38 +0200

    Bug 1279246 - Hang due to CacheFileInputStream deadlock, r=honzab

--- netwerk/cache2/CacheFileOutputStream.cpp
+++ netwerk/cache2/CacheFileOutputStream.cpp
@@ -131,18 +131,19 @@ CacheFileOutputStream::Write(const char * aBuf, uint32_t aCount,
     uint32_t chunkOffset = mPos - (mPos / kChunkSize) * kChunkSize;
     uint32_t canWrite = kChunkSize - chunkOffset;
     uint32_t thisWrite = std::min(static_cast<uint32_t>(canWrite), aCount);
-    nsresult rv = mChunk->EnsureBufSize(chunkOffset + thisWrite);
-    if (NS_FAILED(rv)) {
-      CloseWithStatusLocked(rv);
-      return rv;
+
+    CacheFileChunkWriteHandle hnd = mChunk->GetWriteHandle(chunkOffset + thisWrite);
+    if (!hnd.Buf()) {
+      CloseWithStatusLocked(NS_ERROR_OUT_OF_MEMORY);
+      return NS_ERROR_OUT_OF_MEMORY;
     }
-    memcpy(mChunk->BufForWriting() + chunkOffset, aBuf, thisWrite);
+
+    memcpy(hnd.Buf() + chunkOffset, aBuf, thisWrite);
+    hnd.UpdateDataSize(chunkOffset, thisWrite);
 
     mPos += thisWrite;
     aBuf += thisWrite;
     aCount -= thisWrite;
-
-    mChunk->UpdateDataSize(chunkOffset, thisWrite, false);
   }
 
   EnsureCorrectChunk(true);
@@ -410,16 +411,15 @@ CacheFileOutputStream::FillHole()
   LOG(("CacheFileOutputStream::FillHole() - Zeroing hole in chunk %d, range "
        "%d-%d [this=%p]", mChunk->Index(), mChunk->DataSize(), pos - 1, this));
 
-  nsresult rv = mChunk->EnsureBufSize(pos);
-  if (NS_FAILED(rv)) {
-    CloseWithStatusLocked(rv);
+  CacheFileChunkWriteHandle hnd = mChunk->GetWriteHandle(pos);
+  if (!hnd.Buf()) {
+    CloseWithStatusLocked(NS_ERROR_OUT_OF_MEMORY);
     return;
   }
 
-  memset(mChunk->BufForWriting() + mChunk->DataSize(), 0,
-         pos - mChunk->DataSize());
-
-  mChunk->UpdateDataSize(mChunk->DataSize(), pos - mChunk->DataSize(), false);
+  uint32_t offset = hnd.DataSize();
+  memset(hnd.Buf() + offset, 0, pos - offset);
+  hnd.UpdateDataSize(offset, pos - offset);
 }
 
 void
