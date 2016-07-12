95dbcadd9ce982744e70b918b9b3317389ff9fda
JW Wang <jwwang@mozilla.com>
2016-07-12 13:57:10 +0800

    Bug 1287698 - Make MDSM::mPendingDormant a bool. r=jya
    MozReview-Commit-ID: 8WF0cxAFABS

--- dom/media/MediaDecoderStateMachine.h
+++ dom/media/MediaDecoderStateMachine.h
@@ -857,8 +857,10 @@ private:
   // True if all video frames are already rendered.
   Watchable<bool> mVideoCompleted;
 
-  // Set if MDSM receives dormant request during reading metadata.
-  Maybe<bool> mPendingDormant;
+  // True if we need to enter dormant state after reading metadata. Note that
+  // we can't enter dormant state until reading metadata is done for some
+  // limitations of the reader.
+  bool mPendingDormant = false;
 
   // Flag whether we notify metadata before decoding the first frame or after.
   //
