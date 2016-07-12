7af2c4c079f015a7ad95650e7dad3042b25f9b10
Michael Li <michael.li11702@gmail.com>
2016-07-12 15:34:13 -0400

    Bug 1285272 - Replace direct use of mRoleMapEntry with calls to ARIARoleMap(). r=yzen
    MozReview-Commit-ID: JEuMlNLxD8R

--- accessible/html/HTMLFormControlAccessible.cpp
+++ accessible/html/HTMLFormControlAccessible.cpp
@@ -308,7 +308,7 @@ HTMLTextFieldAccessible::NativeAttributes()
   nsAutoString type;
   if (mContent->GetAttr(kNameSpaceID_None, nsGkAtoms::type, type)) {
     nsAccUtils::SetAccAttr(attributes, nsGkAtoms::textInputType, type);
-    if (!mRoleMapEntry && type.EqualsLiteral("search")) {
+    if (!ARIARoleMap() && type.EqualsLiteral("search")) {
       nsAccUtils::SetAccAttr(attributes, nsGkAtoms::xmlroles,
                              NS_LITERAL_STRING("searchbox"));
     }
