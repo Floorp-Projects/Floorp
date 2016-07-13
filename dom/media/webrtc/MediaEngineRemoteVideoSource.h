1f7f1269f6dafa1bea665dbf1809694d28997a0d
Jan-Ivar Bruaroey <jib@mozilla.com>
2016-07-13 16:07:03 -0400

    Bug 1286096 - Move UpdateSingleSource pattern to MediaEngine base class for reuse. r=padenot
    MozReview-Commit-ID: IJjZayOSxp4
    
    --HG--
    extra : rebase_source : de44566213623492982653c90793be9cceee539b

--- dom/media/webrtc/MediaEngineRemoteVideoSource.h
+++ dom/media/webrtc/MediaEngineRemoteVideoSource.h
@@ -50,6 +50,7 @@ namespace mozilla {
 class MediaEngineRemoteVideoSource : public MediaEngineCameraVideoSource,
                                      public webrtc::ExternalRenderer
 {
+  typedef MediaEngineCameraVideoSource Super;
 public:
   NS_DECL_THREADSAFE_ISUPPORTS
 
@@ -103,7 +104,7 @@ public:
   void Shutdown() override;
 
 protected:
-  ~MediaEngineRemoteVideoSource() { Shutdown(); }
+  ~MediaEngineRemoteVideoSource() { }
 
 private:
   // Initialize the needed Video engine interfaces.
@@ -112,46 +113,18 @@ private:
   void GetCapability(size_t aIndex, webrtc::CaptureCapability& aOut) const override;
   void SetLastCapability(const webrtc::CaptureCapability& aCapability);
 
-  /* UpdateExisting - Centralized function to apply constraints and restart
-   * device as needed, considering all allocations and changes to one.
-   *
-   * aHandle           - New or existing handle, or null to update after removal.
-   * aNewConstraints   - Constraints to be applied to existing handle, or null.
-   * aPrefs            - As passed in (in case of changes in about:config).
-   * aDeviceId         - As passed in (origin dependent).
-   * aOutBadConstraint - Result: nonzero if failed to apply. Name of culprit.
-   */
-
   nsresult
-  UpdateExisting(AllocationHandle* aHandle,
-                 NormalizedConstraints* aNewConstraints,
-                 const MediaEnginePrefs& aPrefs,
-                 const nsString& aDeviceId,
-                 const char** aOutBadConstraint);
-
-  nsresult
-  UpdateNew(AllocationHandle* aHandle,
-            const MediaEnginePrefs& aPrefs,
-            const nsString& aDeviceId,
-            const char** aOutBadConstraint) {
-    return UpdateExisting(aHandle, nullptr, aPrefs, aDeviceId, aOutBadConstraint);
-  }
-
-  nsresult
-  UpdateRemove(const MediaEnginePrefs& aPrefs,
-               const nsString& aDeviceId,
-               const char** aOutBadConstraint) {
-    return UpdateExisting(nullptr, nullptr, aPrefs, aDeviceId, aOutBadConstraint);
-  }
+  UpdateSingleSource(const AllocationHandle* aHandle,
+                     const NormalizedConstraints& aNetConstraints,
+                     const MediaEnginePrefs& aPrefs,
+                     const nsString& aDeviceId,
+                     const char** aOutBadConstraint) override;
 
   dom::MediaSourceEnum mMediaSource; // source of media (camera | application | screen)
   mozilla::camera::CaptureEngine mCapEngine;
 
-  nsTArray<RefPtr<AllocationHandle>> mRegisteredHandles;
-
   // To only restart camera when needed, we keep track previous settings.
   webrtc::CaptureCapability mLastCapability;
-  bool mInShutdown;
 };
 
 }
