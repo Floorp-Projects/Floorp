035f0282b5d89c3421b0cb3cf5171a4d3392555d
Nick Fitzgerald <fitzgen@gmail.com>
2016-07-13 15:19:23 -0700

    Bug 1261869 - Fix leaks in devtools; r=ejpbruel a=gchang
    There are two leaks addressed in this commit:
    
    1. The thread actor's `_debuggerSourcesSeen` set was never cleared. This set
    exists only as a performance optimization to speed up `_addSource` in cases
    where we've already added the source. Unfortunately, this set wasn't getting
    cleared when we cleared debuggees out and it ended up keeping the
    `Debugger.Source`, its referent, and transitively its referent's global alive. I
    figured it was simpler to make it a `WeakSet` than to add it as a special case
    in `ThreadActor.prototype._clearDebuggees` and manage the lifetimes by hand. I
    think this fits well with its intended use as an ephemeral performance
    optimization.
    
    2. Due to a logic error, we were not clearing debuggees in the memory actor's
    `Debugger` instance on navigations. This isn't really a "proper" leak, in that
    if you forced a GC, the old debuggees would go away as `Debugger` holds them
    weakly, however if there was no GC between navigations, then you could still see
    the old windows (and everything they "retained") as roots in the snapshot. This
    issue is straightforward to fix once identified: ensure that `_clearDebuggees`
    is actually called on navigation.
    
    Finally, this commit adds a test that we don't leak Window objects when devtools
    are open and we keep refreshing a tab. When it fails, it prints out the leaking
    window's retaining paths.

---
+++ devtools/client/memory/test/browser/browser_memory_refresh_does_not_leak.js
@@ -0,0 +1,108 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Test that refreshing the page with devtools open does not leak the old
+// windows from previous navigations.
+//
+// IF THIS TEST STARTS FAILING, YOU ARE LEAKING EVERY WINDOW EVER NAVIGATED TO
+// WHILE DEVTOOLS ARE OPEN! THIS IS NOT SPECIFIC TO THE MEMORY TOOL ONLY!
+
+"use strict";
+
+const HeapSnapshotFileUtils = require("devtools/shared/heapsnapshot/HeapSnapshotFileUtils");
+const { getLabelAndShallowSize } = require("devtools/shared/heapsnapshot/DominatorTreeNode");
+
+const TEST_URL = "http://example.com/browser/devtools/client/memory/test/browser/doc_empty.html";
+
+function* getWindowsInSnapshot(front) {
+  dumpn("Taking snapshot.");
+  const path = yield front.saveHeapSnapshot();
+  dumpn("Took snapshot with path = " + path);
+  const snapshot = ChromeUtils.readHeapSnapshot(path);
+  dumpn("Read snapshot into memory, taking census.");
+  const report = snapshot.takeCensus({
+    breakdown: {
+      by: "objectClass",
+      then: { by: "bucket" },
+      other: { by: "count", count: true, bytes: false },
+    }
+  });
+  dumpn("Took census, window count = " + report.Window.count);
+  return report.Window;
+}
+
+const DESCRIPTION = {
+  by: "coarseType",
+  objects: {
+    by: "objectClass",
+    then: { by: "count", count: true, bytes: false },
+    other: { by: "count", count: true, bytes: false },
+  },
+  strings: { by: "count", count: true, bytes: false },
+  scripts: {
+    by: "internalType",
+    then: { by: "count", count: true, bytes: false },
+  },
+  other: {
+    by: "internalType",
+    then: { by: "count", count: true, bytes: false },
+  }
+};
+
+this.test = makeMemoryTest(TEST_URL, function* ({ tab, panel }) {
+  const heapWorker = panel.panelWin.gHeapAnalysesClient;
+  const front = panel.panelWin.gFront;
+  const store = panel.panelWin.gStore;
+  const { getState, dispatch } = store;
+  const doc = panel.panelWin.document;
+
+  const startWindows = yield getWindowsInSnapshot(front);
+  dumpn("Initial windows found = " + startWindows.map(w => "0x" + w.toString(16)).join(", "));
+  is(startWindows.length, 1);
+
+  yield refreshTab(tab);
+
+  const endWindows = yield getWindowsInSnapshot(front);
+  is(endWindows.length, 1);
+
+  if (endWindows.length === 1) {
+    return;
+  }
+
+  dumpn("Test failed, diagnosing leaking windows.");
+  dumpn("(This may fail if a moving GC has relocated the initial Window objects.)");
+
+  dumpn("Taking full runtime snapshot.");
+  const path = yield front.saveHeapSnapshot({ boundaries: { runtime: true } });
+  dumpn("Full runtime's snapshot path = " + path);
+
+  dumpn("Reading full runtime heap snapshot.");
+  const snapshot = ChromeUtils.readHeapSnapshot(path);
+  dumpn("Done reading full runtime heap snapshot.");
+
+  const dominatorTree = snapshot.computeDominatorTree();
+  const paths = snapshot.computeShortestPaths(dominatorTree.root, startWindows, 50);
+
+  for (let i = 0; i < startWindows.length; i++) {
+    dumpn("Shortest retaining paths for leaking Window 0x" + startWindows[i].toString(16) + " =========================");
+    let j = 0;
+    for (let retainingPath of paths.get(startWindows[i])) {
+      if (retainingPath.find(part => part.predecessor === startWindows[i])) {
+        // Skip paths that loop out from the target window and back to it again.
+        continue;
+      }
+
+      dumpn("    Path #" + (++j) + ": --------------------------------------------------------------------");
+      for (let part of retainingPath) {
+        const { label } = getLabelAndShallowSize(part.predecessor, snapshot, DESCRIPTION);
+        dumpn("        0x" + part.predecessor.toString(16) +
+              " (" + label.join(" > ") + ")");
+        dumpn("               |");
+        dumpn("              " + part.edge);
+        dumpn("               |");
+        dumpn("               V");
+      }
+      dumpn("        0x" + startWindows[i].toString(16) + " (objects > Window)");
+    }
+  }
+});
