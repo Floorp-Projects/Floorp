930579ded011f6f31b1e02a38d288a5bc1d8d0ad
Sotaro Ikeda <sotaro.ikeda.g@gmail.com>
2016-07-12 23:00:57 -0700

    Bug 1284803 part 8 - Disable AVX2 asm if the compiler/assembler don't support it r=jesup

--- media/libyuv/include/libyuv/row.h
+++ media/libyuv/include/libyuv/row.h
@@ -194,8 +194,8 @@ extern "C" {
 // The following are available on all x86 platforms, but
 // require VS2012, clang 3.4 or gcc 4.7.
 // The code supports NaCL but requires a new compiler and validator.
-#if !defined(LIBYUV_DISABLE_X86) && (defined(VISUALC_HAS_AVX2) || \
-    defined(CLANG_HAS_AVX2) || defined(GCC_HAS_AVX2))
+#if !defined(LIBYUV_DISABLE_AVX2) && !defined(LIBYUV_DISABLE_X86) &&    \
+    (defined(VISUALC_HAS_AVX2) || defined(CLANG_HAS_AVX2) || defined(GCC_HAS_AVX2))
 #define HAS_ARGBCOPYALPHAROW_AVX2
 #define HAS_ARGBCOPYYTOALPHAROW_AVX2
 #define HAS_ARGBMIRRORROW_AVX2
