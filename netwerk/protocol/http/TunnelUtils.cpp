8f6121d3e7adbaeb65a4bfae2eba796454fa227e
Patrick McManus <mcmanus@ducksong.com>
2016-07-13 16:52:51 -0400

    Bug 1286664 - TLSFilterTransaction::WriteSegments null Connection(). r=hurley, a=sylvestre
    --HG--
    extra : source : aeec04b71527b443c169099cc125288746cc0926

--- netwerk/protocol/http/TunnelUtils.cpp
+++ netwerk/protocol/http/TunnelUtils.cpp
@@ -351,7 +351,7 @@ TLSFilterTransaction::WriteSegments(nsAHttpSegmentWriter *aWriter,
   if (NS_SUCCEEDED(rv) && NS_FAILED(mFilterReadCode) && !(*outCountWritten)) {
     // nsPipe turns failures into silent OK.. undo that!
     rv = mFilterReadCode;
-    if (mFilterReadCode == NS_BASE_STREAM_WOULD_BLOCK) {
+    if (Connection() && (mFilterReadCode == NS_BASE_STREAM_WOULD_BLOCK)) {
       Connection()->ResumeRecv();
     }
   }
