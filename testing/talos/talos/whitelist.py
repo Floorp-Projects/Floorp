d7c7b221e37a9acda400205bc2a3f4d2c5c52f77
Tom Tromey <tom@tromey.com>
2016-07-14 10:16:42 -0600

    Bug 1286877 - do not set c-basic-offset for python-mode; r=gps
    This removes the unnecessary setting of c-basic-offset from all
    python-mode files.
    
    This was automatically generated using
    
        perl -pi -e 's/; *c-basic-offset: *[0-9]+//'
    
    ... on the affected files.
    
    The bulk of these files are moz.build files but there a few others as
    well.
    
    MozReview-Commit-ID: 2pPf3DEiZqx

--- testing/talos/talos/whitelist.py
+++ testing/talos/talos/whitelist.py
@@ -1,4 +1,4 @@
-# -*- Mode: python; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+# -*- Mode: python; tab-width: 8; indent-tabs-mode: nil -*-
 # vim: set ts=8 sts=4 et sw=4 tw=80:
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
