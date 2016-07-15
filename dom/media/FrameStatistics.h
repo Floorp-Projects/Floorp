56c993321543707965beb2339da5c18005c776ad
Gerald Squelart <gsquelart@mozilla.com>
2016-07-15 16:48:56 +1000

    Bug 1289668 - FrameStatistics style - r=kamidphish
    MozReview-Commit-ID: B9yEd7tqfAN
    
    --HG--
    extra : rebase_source : 3b1bfd11c604f9e57321c842f4893192ca9b77b5

--- dom/media/FrameStatistics.h
+++ dom/media/FrameStatistics.h
@@ -11,27 +11,31 @@ namespace mozilla {
 
 // Frame decoding/painting related performance counters.
 // Threadsafe.
-class FrameStatistics {
+class FrameStatistics
+{
 public:
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(FrameStatistics);
 
-  FrameStatistics() :
-      mReentrantMonitor("FrameStats"),
-      mParsedFrames(0),
-      mDecodedFrames(0),
-      mPresentedFrames(0),
-      mDroppedFrames(0) {}
+  FrameStatistics()
+    : mReentrantMonitor("FrameStats")
+    , mParsedFrames(0)
+    , mDecodedFrames(0)
+    , mPresentedFrames(0)
+    , mDroppedFrames(0)
+  {}
 
   // Returns number of frames which have been parsed from the media.
   // Can be called on any thread.
-  uint32_t GetParsedFrames() {
+  uint32_t GetParsedFrames() const
+  {
     ReentrantMonitorAutoEnter mon(mReentrantMonitor);
     return mParsedFrames;
   }
 
   // Returns the number of parsed frames which have been decoded.
   // Can be called on any thread.
-  uint32_t GetDecodedFrames() {
+  uint32_t GetDecodedFrames() const
+  {
     ReentrantMonitorAutoEnter mon(mReentrantMonitor);
     return mDecodedFrames;
   }
@@ -39,14 +43,16 @@ public:
   // Returns the number of decoded frames which have been sent to the rendering
   // pipeline for painting ("presented").
   // Can be called on any thread.
-  uint32_t GetPresentedFrames() {
+  uint32_t GetPresentedFrames() const
+  {
     ReentrantMonitorAutoEnter mon(mReentrantMonitor);
     return mPresentedFrames;
   }
 
-  // Number of frames that have been skipped because they have missed their
-  // compoisition deadline.
-  uint32_t GetDroppedFrames() {
+  // Returns the number of frames that have been skipped because they have
+  // missed their composition deadline.
+  uint32_t GetDroppedFrames() const
+  {
     ReentrantMonitorAutoEnter mon(mReentrantMonitor);
     return mDroppedFrames;
   }
@@ -54,9 +60,11 @@ public:
   // Increments the parsed and decoded frame counters by the passed in counts.
   // Can be called on any thread.
   void NotifyDecodedFrames(uint32_t aParsed, uint32_t aDecoded,
-                           uint32_t aDropped) {
-    if (aParsed == 0 && aDecoded == 0 && aDropped == 0)
+                           uint32_t aDropped)
+  {
+    if (aParsed == 0 && aDecoded == 0 && aDropped == 0) {
       return;
+    }
     ReentrantMonitorAutoEnter mon(mReentrantMonitor);
     mParsedFrames += aParsed;
     mDecodedFrames += aDecoded;
@@ -65,7 +73,8 @@ public:
 
   // Increments the presented frame counters.
   // Can be called on any thread.
-  void NotifyPresentedFrame() {
+  void NotifyPresentedFrame()
+  {
     ReentrantMonitorAutoEnter mon(mReentrantMonitor);
     ++mPresentedFrames;
   }
@@ -74,7 +83,7 @@ private:
   ~FrameStatistics() {}
 
   // ReentrantMonitor to protect access of playback statistics.
-  ReentrantMonitor mReentrantMonitor;
+  mutable ReentrantMonitor mReentrantMonitor;
 
   // Number of frames parsed and demuxed from media.
   // Access protected by mReentrantMonitor.
@@ -88,9 +97,11 @@ private:
   // pipeline to be painted ("presented"). Access protected by mReentrantMonitor.
   uint32_t mPresentedFrames;
 
+  // Number of frames that have been skipped because they have missed their
+  // composition deadline.
   uint32_t mDroppedFrames;
 };
 
 } // namespace mozilla
 
-#endif
+#endif // FrameStatistics_h_
