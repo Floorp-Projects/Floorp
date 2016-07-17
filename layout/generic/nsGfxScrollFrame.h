cfa6acaada5ca2b091f7c47a3139ffed7db7fff7
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-07-17 09:20:26 +0200

    Backed out changeset b98df554a6f0 (bug 1012752)

--- layout/generic/nsGfxScrollFrame.h
+++ layout/generic/nsGfxScrollFrame.h
@@ -629,6 +629,7 @@ protected:
   AsyncScrollEventType mAsyncScrollEvent;
   bool HasPluginFrames();
   bool HasPerspective() const;
+  uint8_t GetScrolledFrameDir() const;
 
   static void EnsureFrameVisPrefsCached();
   static bool sFrameVisPrefsCached;
