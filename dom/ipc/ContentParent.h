b8a8d4e047a7158e1b72c3cbbd07398d977576cf
Andrea Marchesini <amarchesini@mozilla.com>
2016-07-17 16:51:28 +0200

    Bug 1279186 - Blob URLs in multi-e10s - part 2 - Contentparent unregistes BlobURLs if the child crashes, r=gabor

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -1299,6 +1299,8 @@ private:
   // This hashtable is used to run GetFilesHelper objects in the parent process.
   // GetFilesHelper can be aborted by receiving RecvDeleteGetFilesRequest.
   nsRefPtrHashtable<nsIDHashKey, GetFilesHelper> mGetFilesPendingRequests;
+
+  nsTArray<nsCString> mBlobURLs;
 };
 
 } // namespace dom
