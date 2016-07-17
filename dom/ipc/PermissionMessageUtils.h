0c1a08fce66150e3eca8615efb91378941c60627
Andrea Marchesini <amarchesini@mozilla.com>
2016-07-17 16:50:50 +0200

    Bug 1279186 - Blob URLs in multi-e10s - part 1 - BlobURLs broadcasted, r=gabor

--- dom/ipc/PermissionMessageUtils.h
+++ dom/ipc/PermissionMessageUtils.h
@@ -28,9 +28,13 @@ public:
 
   operator nsIPrincipal*() const { return mPrincipal.get(); }
 
+  Principal& operator=(const Principal& aOther)
+  {
+    mPrincipal = aOther.mPrincipal;
+    return *this;
+  }
+
 private:
-  // Unimplemented
-  Principal& operator=(Principal&);
   nsCOMPtr<nsIPrincipal> mPrincipal;
 };
 
