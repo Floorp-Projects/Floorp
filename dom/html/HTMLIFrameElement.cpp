1422d3a5cf568cdf99cf293538d6c59d2f6e2a1f
Xidorn Quan <me@upsuper.org>
2016-07-18 09:04:58 +1000

    Bug 1283526 part 2 - Remove fullscreen sandbox flag. r=smaug a=lizzard
    MozReview-Commit-ID: 5qH4gZxvNQz
    
    --HG--
    extra : source : 64e752c0270c0dfebdf4004e8f7d19029807994d

--- dom/html/HTMLIFrameElement.cpp
+++ dom/html/HTMLIFrameElement.cpp
@@ -204,11 +204,7 @@ HTMLIFrameElement::AfterSetAttr(int32_t aNameSpaceID, nsIAtom* aName,
                                 const nsAttrValue* aValue,
                                 bool aNotify)
 {
-  if ((aName == nsGkAtoms::sandbox ||
-       // The allowfullscreen attribute affects the sandboxed fullscreen
-       // flag, thus we should also reapply it if that is changed.
-       aName == nsGkAtoms::allowfullscreen ||
-       aName == nsGkAtoms::mozallowfullscreen) &&
+  if (aName == nsGkAtoms::sandbox &&
       aNameSpaceID == kNameSpaceID_None && mFrameLoader) {
     // If we have an nsFrameLoader, apply the new sandbox flags.
     // Since this is called after the setter, the sandbox flags have
@@ -244,22 +240,7 @@ HTMLIFrameElement::GetSandboxFlags()
   if (!sandboxAttr) {
     return 0;
   }
-
-  //  Start off by setting all the restriction flags.
-  uint32_t out = SANDBOX_ALL_FLAGS;
-
-// Macro for updating the flag according to the keywords
-#define SANDBOX_KEYWORD(string, atom, flags)                             \
-  if (sandboxAttr->Contains(nsGkAtoms::atom, eIgnoreCase)) { out &= ~(flags); }
-#include "IframeSandboxKeywordList.h"
-#undef SANDBOX_KEYWORD
-
-  if (GetParsedAttr(nsGkAtoms::allowfullscreen) ||
-      GetParsedAttr(nsGkAtoms::mozallowfullscreen)) {
-    out &= ~SANDBOXED_FULLSCREEN;
-  }
-
-  return out;
+  return nsContentUtils::ParseSandboxAttributeToFlags(sandboxAttr);
 }
 
 JSObject*
