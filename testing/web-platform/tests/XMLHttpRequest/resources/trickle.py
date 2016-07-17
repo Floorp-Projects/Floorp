b8b5c17e716c9dc13dd8afc0182571911ed6ee29
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-07-17 12:55:38 +0200

    Backed out changeset f52c26d677b0 (bug 918703)

--- testing/web-platform/tests/XMLHttpRequest/resources/trickle.py
+++ testing/web-platform/tests/XMLHttpRequest/resources/trickle.py
@@ -1,15 +1,12 @@
 import time
 
 def main(request, response):
-    chunk = "TEST_TRICKLE\n"
     delay = float(request.GET.first("ms", 500)) / 1E3
     count = int(request.GET.first("count", 50))
-    if "specifylength" in request.GET:
-        response.headers.set("Content-Length", count * len(chunk))
     time.sleep(delay)
     response.headers.set("Content-type", "text/plain")
     response.write_status_headers()
     time.sleep(delay);
     for i in xrange(count):
-        response.writer.write_content(chunk)
+        response.writer.write_content("TEST_TRICKLE\n")
         time.sleep(delay)
