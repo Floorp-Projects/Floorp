bb6eb374b4fc979d05f3c82f4edce1f94ca912f2
Sebastian Kaspari <s.kaspari@gmail.com>
2016-07-18 17:26:24 +0200

    Backed out changeset 83088b5375ae (bug 1284754)
    MozReview-Commit-ID: F0AEFYIxgHI

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -5964,7 +5964,7 @@ HTMLMediaElement::OpenUnsupportedMediaWithExtenalAppIfNeeded()
   LOG(LogLevel::Debug, ("Open unsupported type \'%s\' with external apps.",
       mMimeType.get()));
   nsContentUtils::DispatchTrustedEvent(OwnerDoc(), static_cast<nsIContent*>(this),
-                                       NS_LITERAL_STRING("OpenMediaWithExternalApp"),
+                                       NS_LITERAL_STRING("OpenMediaWithExtenalApp"),
                                        true,
                                        true);
 }
