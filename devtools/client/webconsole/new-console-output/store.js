8c7f39fc1a6a7417dbade0a2e4a07312c5aba1a1
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2016-07-18 19:02:19 +0200

    Bug 1287508 - Part 1: New console frontend: Add filter bar. r=bgrins
    MozReview-Commit-ID: KoIEkQVPg2V

--- devtools/client/webconsole/new-console-output/store.js
+++ devtools/client/webconsole/new-console-output/store.js
@@ -3,17 +3,50 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-const { combineReducers, createStore } = require("devtools/client/shared/vendor/redux");
 const Immutable = require("devtools/client/shared/vendor/immutable");
-const { reducers } = require("./reducers/index");
 const Services = require("Services");
+
+const Filters = Immutable.Record({
+  error: true,
+  warn: true,
+  info: true,
+  log: true,
+  searchText: ""
+});
+
+const Prefs = Immutable.Record({
+  logLimit: 1000
+});
+
+const Ui = Immutable.Record({
+  configFilterBarVisible: false,
+  filteredMessageVisible: false
+});
+
+module.exports.Prefs = Prefs;
+module.exports.Filters = Filters;
+module.exports.Ui = Ui;
+
+const { combineReducers, createStore } = require("devtools/client/shared/vendor/redux");
+const { reducers } = require("./reducers/index");
 
 function storeFactory() {
   const initialState = {
     messages: Immutable.List(),
-    prefs: {
-      logLimit: Math.max(Services.prefs.getIntPref("devtools.hud.loglimit"), 1)
-    }
+    prefs: new Prefs({
+      logLimit: Math.max(Services.prefs.getIntPref("devtools.hud.loglimit"), 1),
+    }),
+    filters: new Filters({
+      error: Services.prefs.getBoolPref("devtools.webconsole.filter.error"),
+      warn: Services.prefs.getBoolPref("devtools.webconsole.filter.warn"),
+      info: Services.prefs.getBoolPref("devtools.webconsole.filter.info"),
+      log: Services.prefs.getBoolPref("devtools.webconsole.filter.log"),
+      searchText: ""
+    }),
+    ui: new Ui({
+      configFilterBarVisible: false,
+      filteredMessageVisible: false
+    })
   };
 
   return createStore(combineReducers(reducers), initialState);
