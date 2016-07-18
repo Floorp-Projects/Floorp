f242b8e13a95c837e6b7b14527931fc7fc28c226
Tom Klein <twointofive@gmail.com>
2016-07-18 18:50:47 -0500

    Bug 1031519 - Use immersive fullscreen when available. r=mcomella
    This is Deepthi Venkitaramanan's patch with feedback comments addressed.
    
    MozReview-Commit-ID: 7vs0ZgefOVy

--- mobile/android/base/java/org/mozilla/gecko/util/ActivityUtils.java
+++ mobile/android/base/java/org/mozilla/gecko/util/ActivityUtils.java
@@ -21,10 +21,18 @@ public class ActivityUtils {
         Window window = activity.getWindow();
 
         if (Versions.feature16Plus) {
-            final int newVis;
+            int newVis;
             if (fullscreen) {
-                newVis = View.SYSTEM_UI_FLAG_FULLSCREEN |
-                         View.SYSTEM_UI_FLAG_LOW_PROFILE;
+                newVis = View.SYSTEM_UI_FLAG_FULLSCREEN;
+                if (Versions.feature19Plus) {
+                    newVis |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                } else {
+                    newVis |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+                }
             } else {
                 newVis = View.SYSTEM_UI_FLAG_VISIBLE;
             }
