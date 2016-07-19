7f2813b0fe2373b306a0226a13949a76aeed9219
Steve Chung <schung@mozilla.com>
2016-07-19 16:17:22 +0800

    Bug 1284855 - Reps: match spacing and brace placement in nested objects/arrays. r=Honza
    MozReview-Commit-ID: 8SwCGEU0Zak
    
    --HG--
    extra : rebase_source : 05c57c7c5065b45b4a65b5319007b3977e1fd0b0

--- devtools/client/shared/components/reps/array.js
+++ devtools/client/shared/components/reps/array.js
@@ -122,13 +122,19 @@ define(function (require, exports, module) {
       let mode = this.props.mode || "short";
       let object = this.props.object;
       let items;
+      let brackets;
+      let needSpace = function (space) {
+        return space ? { left: "[ ", right: " ]"} : { left: "[", right: "]"};
+      };
 
       if (mode == "tiny") {
         let isEmpty = object.length === 0;
         items = DOM.span({className: "length"}, isEmpty ? "" : object.length);
+        brackets = needSpace(false);
       } else {
         let max = (mode == "short") ? 3 : 300;
         items = this.arrayIterator(object, max);
+        brackets = needSpace(items.length > 0);
       }
 
       let objectLink = this.props.objectLink || DOM.span;
@@ -140,13 +146,13 @@ define(function (require, exports, module) {
             className: "arrayLeftBracket",
             role: "presentation",
             object: object
-          }, "["),
+          }, brackets.left),
           items,
           objectLink({
             className: "arrayRightBracket",
             role: "presentation",
             object: object
-          }, "]"),
+          }, brackets.right),
           DOM.span({
             className: "arrayProperties",
             role: "group"}
