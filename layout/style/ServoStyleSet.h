253dfd8aad95d5a59443a036fb38838c97300cf2
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-07-18 18:02:55 -0700

    Bug 1287951: stylo: Add support for computing style hints from Servo. r=heycam
    MozReview-Commit-ID: ALuJxcfAMuL

--- layout/style/ServoStyleSet.h
+++ layout/style/ServoStyleSet.h
@@ -11,11 +11,12 @@
 #include "mozilla/EventStates.h"
 #include "mozilla/RefPtr.h"
 #include "mozilla/ServoBindingHelpers.h"
+#include "mozilla/ServoElementSnapshot.h"
 #include "mozilla/ServoStyleSheet.h"
 #include "mozilla/SheetType.h"
 #include "mozilla/UniquePtr.h"
-#include "nsChangeHint.h"
 #include "nsCSSPseudoElements.h"
+#include "nsChangeHint.h"
 #include "nsIAtom.h"
 #include "nsTArray.h"
 
@@ -113,10 +114,15 @@ public:
   // Test if style is dependent on content state
   nsRestyleHint HasStateDependentStyle(dom::Element* aElement,
                                        EventStates aStateMask);
-  nsRestyleHint HasStateDependentStyle(dom::Element* aElement,
-                                       mozilla::CSSPseudoElementType aPseudoType,
-                                       dom::Element* aPseudoElement,
-                                       EventStates aStateMask);
+  nsRestyleHint HasStateDependentStyle(
+    dom::Element* aElement, mozilla::CSSPseudoElementType aPseudoType,
+    dom::Element* aPseudoElement, EventStates aStateMask);
+
+  /**
+   * Computes a restyle hint given a element and a previous element snapshot.
+   */
+  nsRestyleHint ComputeRestyleHint(dom::Element* aElement,
+                                   ServoElementSnapshot* aSnapshot);
 
   /**
    * Restyles a whole subtree of nodes.
