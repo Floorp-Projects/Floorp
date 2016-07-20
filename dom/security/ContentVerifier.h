e2d991127305020884243830c86d55edecbb22cf
Wes Kocher <wkocher@mozilla.com>
2016-07-20 11:16:37 -0700

    Backed out changeset 21d8bb5af7b4 (bug 1263793) for leaks in various jobs CLOSED TREE

--- dom/security/ContentVerifier.h
+++ dom/security/ContentVerifier.h
@@ -7,11 +7,12 @@
 #define mozilla_dom_ContentVerifier_h
 
 #include "nsCOMPtr.h"
-#include "nsIContentSignatureVerifier.h"
 #include "nsIObserver.h"
 #include "nsIStreamListener.h"
+#include "nsNSSShutDown.h"
 #include "nsString.h"
 #include "nsTArray.h"
+#include "ScopedNSSTypes.h"
 
 /**
  * Mediator intercepting OnStartRequest in nsHttpChannel, blocks until all
@@ -21,44 +22,77 @@
  * NS_ERROR_INVALID_SIGNATURE is thrown.
  */
 class ContentVerifier : public nsIStreamListener
-                      , public nsIContentSignatureReceiverCallback
+                      , public nsNSSShutDownObject
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSISTREAMLISTENER
   NS_DECL_NSIREQUESTOBSERVER
-  NS_DECL_NSICONTENTSIGNATURERECEIVERCALLBACK
 
   explicit ContentVerifier(nsIStreamListener* aMediatedListener,
                            nsISupports* aMediatedContext)
     : mNextListener(aMediatedListener)
-    , mContextCreated(false)
-    , mContentRead(false) {}
+    , mContext(aMediatedContext)
+    , mCx(nullptr) {}
 
-  nsresult Init(const nsACString& aContentSignatureHeader, nsIRequest* aRequest,
-                nsISupports* aContext);
+  nsresult Init(const nsAString& aContentSignatureHeader);
+
+  // nsNSSShutDownObject
+  virtual void virtualDestroyNSSReference() override
+  {
+    destructorSafeDestroyNSSReference();
+  }
 
 protected:
-  virtual ~ContentVerifier() {}
+  virtual ~ContentVerifier()
+  {
+    nsNSSShutDownPreventionLock locker;
+    if (isAlreadyShutDown()) {
+      return;
+    }
+    destructorSafeDestroyNSSReference();
+    shutdown(calledFromObject);
+  }
+
+  void destructorSafeDestroyNSSReference()
+  {
+    mCx = nullptr;
+  }
 
 private:
-  void FinishSignature();
+  nsresult ParseContentSignatureHeader(const nsAString& aContentSignatureHeader);
+  nsresult GetVerificationKey(const nsAString& aKeyId);
 
-  // buffered content to verify
-  FallibleTArray<nsCString> mContent;
+  // utility function to parse input before put into verification functions
+  nsresult ParseInput(mozilla::ScopedSECKEYPublicKey& aPublicKeyOut,
+                      mozilla::ScopedSECItem& aSignatureItemOut,
+                      SECOidTag& aOidOut,
+                      const nsNSSShutDownPreventionLock&);
+
+  // create a verifier context and store it in mCx
+  nsresult CreateContext();
+
+  // Adds data to the context that was used to generate the signature.
+  nsresult Update(const nsACString& aData);
+
+  // Finalises the signature and returns the result of the signature
+  // verification.
+  nsresult End(bool* _retval);
+
   // content and next listener for nsIStreamListener
   nsCOMPtr<nsIStreamListener> mNextListener;
-  // the verifier
-  nsCOMPtr<nsIContentSignatureVerifier> mVerifier;
-  // holding a pointer to the content request and context to resume/cancel it
-  nsCOMPtr<nsIRequest> mContentRequest;
-  nsCOMPtr<nsISupports> mContentContext;
-  // Semaphors to indicate that the verifying context was created, the entire
-  // content was read resp. The context gets created by ContentSignatureVerifier
-  // and mContextCreated is set in the ContextCreated callback. The content is
-  // read, i.e. mContentRead is set, when the content OnStopRequest is called.
-  bool mContextCreated;
-  bool mContentRead;
+  nsCOMPtr<nsISupports> mContext;
+
+  // verifier context for incrementel verifications
+  mozilla::UniqueVFYContext mCx;
+  // buffered content to verify
+  FallibleTArray<nsCString> mContent;
+  // signature to verify
+  nsCString mSignature;
+  // verification key
+  nsCString mKey;
+  // verification key preference
+  nsString mVks;
 };
 
 #endif /* mozilla_dom_ContentVerifier_h */
--- security/manager/ssl/ContentSignatureVerifier.h
+++ security/manager/ssl/ContentSignatureVerifier.h
@@ -11,7 +11,6 @@
 #include "cert.h"
 #include "CSTrustDomain.h"
 #include "nsIContentSignatureVerifier.h"
-#include "nsIStreamListener.h"
 #include "nsNSSShutDown.h"
 #include "ScopedNSSTypes.h"
 
@@ -23,21 +22,15 @@
     "@mozilla.org/security/contentsignatureverifier;1"
 
 class ContentSignatureVerifier final : public nsIContentSignatureVerifier
-                                     , public nsIStreamListener
                                      , public nsNSSShutDownObject
-                                     , public nsIInterfaceRequestor
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSICONTENTSIGNATUREVERIFIER
-  NS_DECL_NSIINTERFACEREQUESTOR
-  NS_DECL_NSISTREAMLISTENER
-  NS_DECL_NSIREQUESTOBSERVER
 
   ContentSignatureVerifier()
     : mCx(nullptr)
-    , mInitialised(false)
-    , mHasCertChain(false)
+    , mMutex("CSVerifier::mMutex")
   {
   }
 
@@ -51,11 +44,8 @@ private:
   ~ContentSignatureVerifier();
 
   nsresult UpdateInternal(const nsACString& aData,
+                          MutexAutoLock& /*proofOfLock*/,
                           const nsNSSShutDownPreventionLock& /*proofOfLock*/);
-  nsresult DownloadCertChain();
-  nsresult CreateContextInternal(const nsACString& aData,
-                                 const nsACString& aCertChain,
-                                 const nsACString& aName);
 
   void destructorSafeDestroyNSSReference()
   {
@@ -67,26 +57,11 @@ private:
 
   // verifier context for incremental verifications
   mozilla::UniqueVFYContext mCx;
-  bool mInitialised;
-  // Indicates whether we hold a cert chain to verify the signature or not.
-  // It's set by default in CreateContext or when the channel created in
-  // DownloadCertChain finished. Update and End must only be called after
-  // mHashCertChain is set.
-  bool mHasCertChain;
   // signature to verify
   nsCString mSignature;
-  // x5u (X.509 URL) value pointing to pem cert chain
-  nsCString mCertChainURL;
-  // the downloaded cert chain to verify against
-  FallibleTArray<nsCString> mCertChain;
   // verification key
   mozilla::UniqueSECKEYPublicKey mKey;
-  // name of the verifying context
-  nsCString mName;
-  // callback to notify when finished
-  nsCOMPtr<nsIContentSignatureReceiverCallback> mCallback;
-  // channel to download the cert chain
-  nsCOMPtr<nsIChannel> mChannel;
+  mozilla::Mutex mMutex;
 };
 
 #endif // ContentSignatureVerifier_h
