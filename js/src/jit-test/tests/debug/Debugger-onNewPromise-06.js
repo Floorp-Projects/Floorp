5e2454596c59ce8f9e2687929ee9e20a8ddf0141
Till Schneidereit <till@tillschneidereit.net>
2016-07-21 00:44:16 +0200

    Bug 911216 - Part 30: Enable SpiderMonkey Promise implementation. r=bz,efaust,bholley,Paolo,tromey,shu
    Also contains folded version of the following patches that have to land at the same time with enabling the new implementation (or be backed out at the same time, if it comes to that):
    
    Add Promise checks to test_xrayToJS.xul. r=bholley
    Change Promise debugger hook tests to use Promise ctor instead of makeFakePromise. r=shu
    Change DOM interface tests to assume Promise is an ES builtin, not a DOM one. r=bz
    Remove some PromiseDebugging references. r=bz
    Adapt promise rejections test to new xray-unwrapping error. r=bz
    Fix expectations in browser_timelineMarkers tests. r=tromey

--- js/src/jit-test/tests/debug/Debugger-onNewPromise-06.js
+++ js/src/jit-test/tests/debug/Debugger-onNewPromise-06.js
@@ -1,4 +1,6 @@
 // Resumption values from onNewPromise handlers are disallowed.
+if (!('Promise' in this))
+    quit(0);
 
 load(libdir + 'asserts.js');
 
@@ -8,28 +10,28 @@ var log;
 
 dbg.onNewPromise = function (g) { log += 'n'; return undefined; };
 log = '';
-assertEq(typeof g.makeFakePromise(), "object");
+assertEq(typeof new g.Promise(function (){}), "object");
 assertEq(log, 'n');
 
 dbg.uncaughtExceptionHook = function (ex) { assertEq(/disallowed/.test(ex), true); log += 'u'; }
 dbg.onNewPromise = function (g) { log += 'n'; return { return: "snoo" }; };
 log = '';
-assertEq(typeof g.makeFakePromise(), "object");
+assertEq(typeof new g.Promise(function (){}), "object");
 assertEq(log, 'nu');
 
 dbg.onNewPromise = function (g) { log += 'n'; return { throw: "snoo" }; };
 log = '';
-assertEq(typeof g.makeFakePromise(), "object");
+assertEq(typeof new g.Promise(function (){}), "object");
 assertEq(log, 'nu');
 
 dbg.onNewPromise = function (g) { log += 'n'; return null; };
 log = '';
-assertEq(typeof g.makeFakePromise(), "object");
+assertEq(typeof new g.Promise(function (){}), "object");
 assertEq(log, 'nu');
 
 dbg.uncaughtExceptionHook = function (ex) { assertEq(/foopy/.test(ex), true); log += 'u'; }
 dbg.onNewPromise = function (g) { log += 'n'; throw "foopy"; };
 log = '';
-assertEq(typeof g.makeFakePromise(), "object");
+assertEq(typeof new g.Promise(function (){}), "object");
 assertEq(log, 'nu');
 
