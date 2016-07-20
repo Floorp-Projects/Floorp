7b6a6a19d39da4ac2e9414e208535caec058f9b3
Milan Sreckovic <milan@mozilla.com>
2016-07-20 10:26:02 -0400

    Bug 635464: Diagnostic crash in nightly and aurora, to see if we are asking basic content client for alpha. r=bas
    MozReview-Commit-ID: 2DBdJZs0hlh
    
    --HG--
    extra : rebase_source : dabafda100d50b8308e4dc008254a7a4c6d80549

--- gfx/layers/client/ContentClient.cpp
+++ gfx/layers/client/ContentClient.cpp
@@ -127,6 +127,9 @@ ContentClientBasic::CreateBuffer(ContentType aType,
                                  RefPtr<gfx::DrawTarget>* aWhiteDT)
 {
   MOZ_ASSERT(!(aFlags & BUFFER_COMPONENT_ALPHA));
+  if (aFlags & BUFFER_COMPONENT_ALPHA) {
+    gfxDevCrash(LogReason::AlphaWithBasicClient) << "Asking basic content client for component alpha";
+  }
 
   *aBlackDT = gfxPlatform::GetPlatform()->CreateOffscreenContentDrawTarget(
     IntSize(aRect.width, aRect.height),
