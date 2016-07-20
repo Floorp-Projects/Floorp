a2c42c612374eabfe63c8f15257a2976c5864e78
Thomas Wisniewski <wisniewskit@gmail.com>
2016-07-19 23:13:13 -0400

    Bug 843579 - Unprefix -moz-any-link. r=bz

--- devtools/client/inspector/rules/test/browser_rules_user-agent-styles.js
+++ devtools/client/inspector/rules/test/browser_rules_user-agent-styles.js
@@ -116,8 +116,8 @@ function* userAgentStylesVisible(inspector, view) {
   // These tests rely on the "a" selector being the last test in
   // TEST_DATA.
   ok(uaRules.some(rule => {
-    return rule.matchedSelectors.indexOf(":-moz-any-link") !== -1;
-  }), "There is a rule for :-moz-any-link");
+    return rule.matchedSelectors.indexOf(":any-link") !== -1;
+  }), "There is a rule for :any-link");
   ok(uaRules.some(rule => {
     return rule.matchedSelectors.indexOf("*|*:link") !== -1;
   }), "There is a rule for *|*:link");
