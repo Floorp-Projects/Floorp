03bf5be5b238e2d5f89c9039fa10dfdbae12f659
Jim Chen <nchen@mozilla.com>
2016-07-21 00:41:13 -0400

    Bug 1286663 - Add request thumbnail native method to ThumbnailHelper; r=snorp Add and use a request thumbnail native method call in ThumbnailHelper, instead of using the THUMBNAIL event in GeckoEvent.

--- mobile/android/base/java/org/mozilla/gecko/ThumbnailHelper.java
+++ mobile/android/base/java/org/mozilla/gecko/ThumbnailHelper.java
@@ -154,9 +154,11 @@ public final class ThumbnailHelper {
         }
 
         Log.d(LOGTAG, "Sending thumbnail event: " + mWidth + ", " + mHeight);
-        GeckoEvent e = GeckoEvent.createThumbnailEvent(tab.getId(), mWidth, mHeight, mBuffer);
-        GeckoAppShell.sendEventToGecko(e);
+        requestThumbnail(mBuffer, tab.getId(), mWidth, mHeight);
     }
+
+    @WrapForJNI
+    private static native void requestThumbnail(ByteBuffer data, int tabId, int width, int height);
 
     /* This method is invoked by JNI once the thumbnail data is ready. */
     @WrapForJNI(stubName = "SendThumbnail")
