519d3b40494d168c7508194bac679de982e089f6
Jeff Walden <jwalden@mit.edu>
2016-07-20 21:49:12 -0700

    Bug 1299284 - Change typed-object code's alignment, offset, and size concepts to use unsigned types.  r=sfink
    --HG--
    extra : rebase_source : a6354f2444d417e48a44b24c6fe4f1578faebfff

--- js/src/jit/IonAnalysis.h
+++ js/src/jit/IonAnalysis.h
@@ -153,7 +153,7 @@ class LinearSum
 
     // Unlike the above function, on failure this leaves the sum unchanged and
     // it can still be used.
-    MOZ_MUST_USE bool divide(int32_t scale);
+    MOZ_MUST_USE bool divide(uint32_t scale);
 
     int32_t constant() const { return constant_; }
     size_t numTerms() const { return terms_.length(); }
