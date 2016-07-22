407bc3c38cbc4a4165337d7dd8598a84ea7725fb
Jeff Gilbert <jgilbert@mozilla.com>
2016-07-21 23:25:41 -0700

    Bug 1288649 - Fix non-unified-build errors. - r=ethlin
    MozReview-Commit-ID: EI7FtmuEBl7

--- dom/canvas/WebGLContext.h
+++ dom/canvas/WebGLContext.h
@@ -1311,7 +1311,7 @@ private:
     // Context customization points
     virtual WebGLVertexArray* CreateVertexArrayImpl();
 
-    virtual bool ValidateAttribPointerType(bool integerMode, GLenum type, GLsizei* alignment, const char* info) = 0;
+    virtual bool ValidateAttribPointerType(bool integerMode, GLenum type, uint32_t* alignment, const char* info) = 0;
     virtual bool ValidateBufferTarget(GLenum target, const char* info) = 0;
     virtual bool ValidateBufferIndexedTarget(GLenum target, const char* info) = 0;
     virtual bool ValidateBufferForTarget(GLenum target, WebGLBuffer* buffer, const char* info);
